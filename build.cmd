192e3ad751d10cde3526b2b2328ffe44af6a9ea5
Avaer Kazmer <a@modules.io>
2020-03-23 06:59:57 -0400

    Add build files

---
+++ build.cmd
@@ -0,0 +1,9 @@
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+set GOOGLE_API_KEY "no"
+set GOOGLE_DEFAULT_CLIENT_ID "no"
+set GOOGLE_DEFAULT_CLIENT_SECRET "no"
+gn gen out\Release
+gn args out\Release
+copy /Y start.cmd out\Release\start.cmd
+autoninja -C out\Release mini_installer
+copy /Y out\Release\chrome.packed.7z .\metachromium.7z
