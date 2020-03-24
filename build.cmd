90c219bb714deb5b5267be678e178058f77ccb66
Avaer Kazmer <a@modules.io>
2020-03-24 15:02:13 -0400

    Add example.html to build

--- build.cmd
+++ build.cmd
@@ -5,5 +5,6 @@ set GOOGLE_DEFAULT_CLIENT_SECRET="no"
 gn gen out\Release
 gn args out\Release
 copy /Y metachromium.cmd out\Release\metachromium.cmd
+copy /Y example.html out\Release\example.html
 autoninja -C out\Release mini_installer
 copy /Y out\Release\chrome.packed.7z .\metachromium.7z
