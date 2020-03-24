6e8666390d74f5c1be65c9b5c4d612b8bac695b8
Avaer Kazmer <a@modules.io>
2020-03-24 13:30:25 -0400

    Build env vars cleanup

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
-set GOOGLE_API_KEY "no"
-set GOOGLE_DEFAULT_CLIENT_ID "no"
-set GOOGLE_DEFAULT_CLIENT_SECRET "no"
+set GOOGLE_API_KEY="no"
+set GOOGLE_DEFAULT_CLIENT_ID="no"
+set GOOGLE_DEFAULT_CLIENT_SECRET="no"
 gn gen out\Release
 gn args out\Release
 copy /Y start.cmd out\Release\start.cmd
