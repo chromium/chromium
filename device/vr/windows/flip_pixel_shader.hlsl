e7a4adc9b8eaf396a047ed26c0eeb800c220f71e
Avaer Kazmer <a@modules.io>
2020-04-28 02:00:06 -0400

    Clean up shaders

--- device/vr/windows/flip_pixel_shader.hlsl
+++ device/vr/windows/flip_pixel_shader.hlsl
@@ -24,8 +24,8 @@ struct PixelShaderInput
 
 cbuffer PS_CONSTANT_BUFFER : register(b0) {
   float4x4 lookRotation;
-  float halfFOVInRadiansX;
-  float halfFOVInRadiansY;
+  float fovScalarX;
+  float fovScalarY;
 }
 
 float4 flip_pixel(PixelShaderInput input) : SV_TARGET
@@ -33,21 +33,8 @@ float4 flip_pixel(PixelShaderInput input) : SV_TARGET
   const float PI = 3.141592;
   const float HALF_PI = 0.5 * PI;
   const float QUARTER_PI = 0.25 * PI;
-
-  float2 uv = float2(input.tex.x, input.tex.y);
-
-  // convert UV (0,0 upper right) (1, 1 lower left) to XY (0,0 lower left) (1, 1 upper right)
-  float2 xy = float2(uv.x, 1 - uv.y);
-
-  // convert to -1, -1 lower left, 1, 1 upper right
-  xy = float2(2.0 * xy - 1.0);
-
-  // (-pi, -pi_half) lower left, (pi, pi_half) upper right
-  xy *= float2(PI, HALF_PI);
-
-  // Convert from one equirect to 2 stacked equirects (left on top of right eye)
-  // scaling by two ([-.5, .5 pi] to [-pi. pi])
-  xy.y *= 2;
+  
+  float2 xy = float2(input.tex.x, input.tex.y);
 
   // then subtractingin half pi  for top, adding pi back for bot
   bool isTop = xy.y >= 0;
@@ -63,8 +50,8 @@ float4 flip_pixel(PixelShaderInput input) : SV_TARGET
   // both eyes go from -pi -> pi left to right and -pi/2 -> pi/2 bottom to top
 
   // Get scalar for modifying projection from cubemap (90 fov) to eye target fov
-  float fovScalarX = tan(halfFOVInRadiansX) / tan(QUARTER_PI);
-  float fovScalarY = tan(halfFOVInRadiansY) / tan(QUARTER_PI);
+  // float fovScalarX = tan(halfFOVInRadiansX) / tan(QUARTER_PI);
+  // float fovScalarY = tan(halfFOVInRadiansY) / tan(QUARTER_PI);
 
   // create vector looking out at equirect CubeMap
   float3 cubeMapLookupDirection = float3(sin(xy.x), 1.0, cos(xy.x)) * float3(cos(xy.y), sin(xy.y), cos(xy.y));
@@ -73,43 +60,33 @@ float4 flip_pixel(PixelShaderInput input) : SV_TARGET
   // this is a trick to prevent a full cube map render of the scene, the only valid
   // equirectangular projections will be near wahtever is treated as forward and backward traditionally
   cubeMapLookupDirection = mul(lookRotation, float4(cubeMapLookupDirection, 1)).xyz;
-
-  // project the vector onto the 2d texture
-  // this will be wrong everywhere that is not near the rotated forward plane of the cubeMap
-  // U = ((X/|Z|) + 1) / 2
-  // V = ((Y/|Z|) + 1) / 2
-  // always project the +Z axis of a cube map
-  // X/|Z|, -Y/|Z| places uv coords in -1, 1. + 1 / 2 shifts to 0 -> 1
-  // fovScalar scales U/V from 90 degrees into eye fov that was rendered with.
-  float projectLookOntoUAxis = ((cubeMapLookupDirection.x / abs(cubeMapLookupDirection.z) / fovScalarX) + 1) / 2;
-  float projectLookOntoVAxis = 1 - (((cubeMapLookupDirection.y / abs(cubeMapLookupDirection.z) / fovScalarY) + 1) / 2);
-
-  float2 eyeUV = float2(projectLookOntoUAxis, projectLookOntoVAxis);
+  cubeMapLookupDirection.x /= abs(cubeMapLookupDirection.z) * fovScalarX;
+  cubeMapLookupDirection.y /= abs(cubeMapLookupDirection.z) * fovScalarY;
   
-  /* eyeUV.x *= 1.2;
-  eyeUV.y *= 1.2; */
+  if (cubeMapLookupDirection.x >= -1.0 && cubeMapLookupDirection.x <= 1.0 && cubeMapLookupDirection.y >= -1.0 && cubeMapLookupDirection.y <= 1.0) {
+    // project the vector onto the 2d texture
+    // this will be wrong everywhere that is not near the rotated forward plane of the cubeMap
+    // U = ((X/|Z|) + 1) / 2
+    // V = ((Y/|Z|) + 1) / 2
+    // always project the +Z axis of a cube map
+    // X/|Z|, -Y/|Z| places uv coords in -1, 1. + 1 / 2 shifts to 0 -> 1
+    // fovScalar scales U/V from 90 degrees into eye fov that was rendered with.
+    float projectLookOntoUAxis = (cubeMapLookupDirection.x + 1) / 2;
+    float projectLookOntoVAxis = 1 - ((cubeMapLookupDirection.y + 1) / 2);
 
-  /* float b = 0;
-  if (eyeUV.x > 0.5 || eyeUV.y > 0.5) {
-    b = 0.5;
-  } else if (eyeUV.x < 0 || eyeUV.x > 1 || eyeUV.y < 0 || eyeUV.y > 1) {
-    b = 1;
+    float2 eyeUV = float2(projectLookOntoUAxis, projectLookOntoVAxis);
+
+    // copy color from the right eye texture
+    eyeUV.x *= 0.5;
+    if (isTop) {
+      eyeUV.x += 0.5;
+    }
+    float4 c = my_texture.Sample(my_sampler, eyeUV).rgba;
+    /* if (c.r == 0 && c.g == 0 && c.b == 0) {
+      c.a = 0;
+    } */
+    return c;
+  } else {
+    return float4(0.0, 0.0, 0.0, 0.0);
   }
-  return float4(eyeUV.x, eyeUV.y, b, 1); */
-
-  // copy color from the right eye texture
-  eyeUV.x *= 0.5;
-  if (isTop) {
-    eyeUV.x += 0.5;
-  }
-  float4 c = my_texture.Sample(my_sampler, eyeUV).rgba;
-  /* if (c.r == 0 && c.g == 0 && c.b == 0) {
-    c.a = 0;
-  } */
-  return c;
-
-  /* float2 texture_coords = float2(input.tex.x, input.tex.y);
-  return my_texture.Sample(my_sampler, texture_coords).rgba; */
-
-  // return float4(input.tex.x, input.tex.y, 0, 1);
 }
--- device/vr/windows/vertex_shader.hlsl
+++ device/vr/windows/vertex_shader.hlsl
@@ -17,12 +17,32 @@ struct PixelShaderInput
 
 PixelShaderInput vertex(VertexShaderInput input)
 {
+  const float PI = 3.141592;
+  const float HALF_PI = 0.5 * PI;
+  const float QUARTER_PI = 0.25 * PI;
+
   PixelShaderInput output;
   float4 pos = float4(input.pos, 1.0f, 1.0f);
 
   output.pos = pos;
-  output.tex = input.tex;
   output.idx = input.idx;
+  
+  float2 uv = float2(input.tex.x, input.tex.y);
+
+  // convert UV (0,0 upper right) (1, 1 lower left) to XY (0,0 lower left) (1, 1 upper right)
+  float2 xy = float2(uv.x, 1 - uv.y);
+
+  // convert to -1, -1 lower left, 1, 1 upper right
+  xy = float2(2.0 * xy - 1.0);
+
+  // (-pi, -pi_half) lower left, (pi, pi_half) upper right
+  xy *= float2(PI, HALF_PI);
+
+  // Convert from one equirect to 2 stacked equirects (left on top of right eye)
+  // scaling by two ([-.5, .5 pi] to [-pi. pi])
+  xy.y *= 2;
+  
+  output.tex = xy;
 
   return output;
 }
