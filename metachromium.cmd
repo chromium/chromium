a297987bf82ca80327e3c7723235dd76a8d2ab07
Avaer Kazmer <a@modules.io>
2020-05-07 20:48:31 -0400

    Update metachromium.cmd

--- metachromium.cmd
+++ metachromium.cmd
@@ -1,1 +1,2 @@
-chrome.exe --no-sandbox --test-type --disable-xr-device-consent-prompt-for-testing --use-fake-ui-for-media-stream "%cd%/example.html"
+chrome.exe --no-sandbox --disable-web-security --test-type --disable-xr-device-consent-prompt-for-testing --disable-gesture-requirement-for-presentation --use-fake-ui-for-media-stream --no-user-gesture-required --autoplay-policy=no-user-gesture-required "%cd%/example.html"
+
