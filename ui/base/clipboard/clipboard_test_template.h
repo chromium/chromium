3c3591cc9d157ed85ace935c1cc7f8db88de31e5
Sahel Sharify <sahel@chromium.org>
2020-08-26 19:44:17 +0000

    Revert "[ozone/wayland] fix 'interactive_ui_tests' build"
    This reverts commit c5a74e50a5d328399491b63d5b44c8c8b3ec9d35.
    
    Reason for revert: [Sheriff] Compile failure on mac:
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8870878190370395696/+/steps/compile/0/stdout
    
    
    Original change's description:
    > [ozone/wayland] fix 'interactive_ui_tests' build
    > 
    > In file included from ../../ui/base/clipboard/clipboard_unittest.cc:57:
    > ../../ui/base/clipboard/clipboard_test_template.h:177:7:
    >   error: use of undeclared identifier 'features'
    > 
    >   if (features::IsUsingOzonePlatform()) {
    >       ^
    > 1 error generated.
    > 
    > BUG=789065
    > R=​sky@chromium.org
    > 
    > Change-Id: I60a5322b70cf03e206ba0854dfe61115c71b0337
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376129
    > Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Auto-Submit: Antonio Gomes (GMT-4) <tonikitoo@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#801652}
    
    TBR=sky@chromium.org,tonikitoo@igalia.com,msisov@igalia.com
    
    Change-Id: Ie7236ce92e34a8f389d25e07b95e9b997f3abcc3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 789065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378393
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801886}

--- ui/base/clipboard/clipboard_test_template.h
+++ ui/base/clipboard/clipboard_test_template.h
@@ -44,7 +44,6 @@
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/base/clipboard/test/clipboard_test_util.h"
 #include "ui/base/clipboard/test/test_clipboard.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/half_float.h"
 #include "url/origin.h"
@@ -54,6 +53,7 @@
 #endif
 
 #if defined(USE_X11)
+#include "ui/base/ui_base_features.h"
 #include "ui/events/platform/platform_event_source.h"
 #endif
 
