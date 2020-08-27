f8b345739fc5889b44e9f71bf1cf516ad5ac17b6
Sahel Sharify <sahel@chromium.org>
2020-08-27 21:41:50 +0000

    [Payments] Download secure-payment-confirmation instrument icon.
    This cl downloads and encodes instrument icon for SPC before it is
    written to db.
    
    Bug: 1121021, 1121331
    Change-Id: Ifa875ad6a42380fb5f7a7790022f4dd17b835122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380142
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802372}

--- components/payments/content/payment_credential.h
+++ components/payments/content/payment_credential.h
@@ -6,15 +6,23 @@
 #define COMPONENTS_PAYMENTS_CONTENT_PAYMENT_CREDENTIAL_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/webdata/common/web_data_service_base.h"
 #include "components/webdata/common/web_data_service_consumer.h"
+#include "content/public/browser/global_routing_id.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/payments/payment_credential.mojom.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
 
 namespace payments {
 
@@ -25,9 +33,12 @@ class PaymentManifestWebDataService;
 // These can be retrieved later to authenticate during a PaymentRequest
 // that uses Secure Payment Confirmation.
 class PaymentCredential : public mojom::PaymentCredential,
-                          public WebDataServiceConsumer {
+                          public WebDataServiceConsumer,
+                          public content::WebContentsObserver {
  public:
   PaymentCredential(
+      content::WebContents* web_contents,
+      content::GlobalFrameRoutingId initiator_frame_routing_id,
       scoped_refptr<PaymentManifestWebDataService> web_data_sevice,
       mojo::PendingReceiver<mojom::PaymentCredential> receiver);
   ~PaymentCredential() override;
@@ -48,10 +59,24 @@ class PaymentCredential : public mojom::PaymentCredential,
       WebDataServiceBase::Handle h,
       std::unique_ptr<WDTypedResult> result) override;
 
+  void DidDownloadFavicon(
+      payments::mojom::PaymentCredentialInstrumentPtr instrument,
+      const std::vector<uint8_t>& credential_id,
+      const std::string& rp_id,
+      StorePaymentCredentialCallback callback,
+      int request_id,
+      int unused_http_status_code,
+      const GURL& image_url,
+      const std::vector<SkBitmap>& bitmaps,
+      const std::vector<gfx::Size>& unused_sizes);
+
+  const content::GlobalFrameRoutingId initiator_frame_routing_id_;
   scoped_refptr<PaymentManifestWebDataService> web_data_service_;
   std::map<WebDataServiceBase::Handle, StorePaymentCredentialCallback>
       callbacks_;
   mojo::Receiver<mojom::PaymentCredential> receiver_{this};
+  std::set<int> pending_icon_download_request_ids_;
+  base::WeakPtrFactory<PaymentCredential> weak_ptr_factory_{this};
 };
 
 }  // namespace payments
