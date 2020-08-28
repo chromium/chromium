dc8be51836accdf26718d1274889b3b90556a883
Yuki Shiino <yukishiino@chromium.org>
2020-08-28 06:12:13 +0000

    IDL compiler: Forbid nullable dictionary as argument and dict member
    IDL nullable dictionaries are forbidden as argument type and
    dictionary member type.  Adds a validator of the rule.
    
    Bug: 997708
    Change-Id: I4b3f922ac77c8c6de949b7098b5096759c83f00b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379337
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802541}

--- third_party/blink/renderer/bindings/scripts/web_idl/validator.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/validator.py
@@ -2,10 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import itertools
+
 from .ir_map import IRMap
 
 
 def validate_after_resolve_references(ir_map):
+    _validate_forbidden_nullable_dictionary_type(ir_map)
     _validate_literal_constant_type(ir_map)
 
 
@@ -14,8 +17,9 @@ def _all_constants(ir_map):
     irs = ir_map.irs_of_kinds(IRMap.IR.Kind.CALLBACK_INTERFACE,
                               IRMap.IR.Kind.INTERFACE, IRMap.IR.Kind.NAMESPACE)
     for ir in irs:
-        accumulated.extend(ir.constants)
-    return accumulated
+        if ir.constants:
+            accumulated.append(ir.constants)
+    return itertools.chain(*accumulated)
 
 
 def _all_function_likes(ir_map):
@@ -23,11 +27,34 @@ def _all_function_likes(ir_map):
     irs = ir_map.irs_of_kinds(IRMap.IR.Kind.CALLBACK_INTERFACE,
                               IRMap.IR.Kind.INTERFACE, IRMap.IR.Kind.NAMESPACE)
     for ir in irs:
-        accumulated.extend(ir.constructors)
-        accumulated.extend(ir.named_constructors)
-        accumulated.extend(ir.operations)
-    accumulated.extend(ir_map.irs_of_kinds(IRMap.IR.Kind.CALLBACK_FUNCTION))
-    return accumulated
+        if ir.constructors:
+            accumulated.append(ir.constructors)
+        if ir.named_constructors:
+            accumulated.append(ir.named_constructors)
+        if ir.operations:
+            accumulated.append(ir.operations)
+    accumulated.append(ir_map.irs_of_kinds(IRMap.IR.Kind.CALLBACK_FUNCTION))
+    return itertools.chain(*accumulated)
+
+
+def _validate_forbidden_nullable_dictionary_type(ir_map):
+    for function in _all_function_likes(ir_map):
+        for argument in function.arguments:
+            assert (not (argument.idl_type.is_nullable
+                         and argument.idl_type.unwrap().is_dictionary)
+                    ), ("{}: {}, {}: Nullable dictionary type is forbidden as "
+                        "an argument type.".format(
+                            function.debug_info.location, function.identifier,
+                            argument.identifier))
+
+    for dictionary in ir_map.irs_of_kind(IRMap.IR.Kind.DICTIONARY):
+        for dict_member in dictionary.own_members:
+            assert (not (dict_member.idl_type.is_nullable
+                         and dict_member.idl_type.unwrap().is_dictionary)
+                    ), ("{}: {}, {}: Nullable dictionary type is forbidden as "
+                        "a dictionary member type.".format(
+                            dict_member.debug_info.location,
+                            dictionary.identifier, dict_member.identifier))
 
 
 def _validate_literal_constant_type(ir_map):
