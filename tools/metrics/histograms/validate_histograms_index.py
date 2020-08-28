71c6d079a67e342bd857f87e33f98ca6a3d1420e
Weilun Shi <sweilun@chromium.org>
2020-08-28 21:19:42 +0000

    Normalize histogram paths before comparing them
    We should normalize histogram paths in the validate_histograms_index.py
    before checking whether they are the same.
    
    This is to fix the issue that the path separator is different between
    Windows and Linux.
    
    Change-Id: I5806886a192b3a15512f76631e324d9f1cc9bc46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381092
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802794}

--- tools/metrics/histograms/validate_histograms_index.py
+++ tools/metrics/histograms/validate_histograms_index.py
@@ -17,7 +17,7 @@ import path_util
 def main():
   exit_code = 0
   with open(histogram_paths.HISTOGRAMS_INDEX, 'r') as f:
-    histograms_paths = [line.strip() for line in f]
+    histograms_paths = [os.path.normpath(line.strip()) for line in f]
     if histograms_paths != histogram_paths.HISTOGRAMS_XMLS_RELATIVE:
       exit_code = 1
       logging.error(
