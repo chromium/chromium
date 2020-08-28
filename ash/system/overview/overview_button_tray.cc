ac6c324ab2ed3ed77cd8312bd87b9a653e0f2ea4
Yulun Wu <yulunwu@chromium.org>
2020-08-28 00:46:36 +0000

    Update overview button color for light mode.
    Bug: 1110757
    Change-Id: I415313e4f1b9279ac4df68243a6b65f12055c092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378801
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802458}

--- ash/system/overview/overview_button_tray.cc
+++ ash/system/overview/overview_button_tray.cc
@@ -10,6 +10,7 @@
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_provider.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_container.h"
 #include "ash/wm/mru_window_tracker.h"
@@ -36,7 +37,10 @@ OverviewButtonTray::OverviewButtonTray(Shelf* shelf)
       icon_(new views::ImageView()),
       scoped_session_observer_(this) {
   gfx::ImageSkia image = gfx::CreateVectorIcon(
-      kShelfOverviewIcon, ShelfConfig::Get()->shelf_icon_color());
+      kShelfOverviewIcon,
+      AshColorProvider::Get()->GetContentLayerColor(
+          AshColorProvider::ContentLayerType::kButtonIconColor,
+          AshColorProvider::AshColorMode::kDark));
   icon_->SetImage(image);
   const int vertical_padding = (kTrayItemSize - image.height()) / 2;
   const int horizontal_padding = (kTrayItemSize - image.width()) / 2;
