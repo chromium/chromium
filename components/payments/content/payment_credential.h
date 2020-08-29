a9045d4a106813c63f1cc70d36be2f89f28d1539
Rouslan Solomakhin <rouslan@chromium.org>
2020-08-29 18:05:35 +0000

    [M86][Web Payment] Store payment credential in SQLite database.
    Before this patch, payment credential was not being stored, so Web
    Payments could not invoke secure payment confirmation.
    
    This patch stores the payment credential in SQLite database.
    
    After this patch, Web Payments can invoke secure payment confirmation
    for the credential that is stored in the SQLite database.
    
    TBR=rouslan@chromium.org
    TBR=sahel@chromium.org
    
    (cherry picked from commit 3732b2fe3d0191b31ce15d7df84b83ab5da7a05f)
    
    Bug: 1121336
    Change-Id: Iffbb93a50095d961b5191b3b6c936ee052b5bf83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375892
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#802259}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2383626
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#242}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- components/payments/content/payment_credential.h
+++ components/payments/content/payment_credential.h
@@ -5,22 +5,30 @@
 #ifndef COMPONENTS_PAYMENTS_CONTENT_PAYMENT_CREDENTIAL_H_
 #define COMPONENTS_PAYMENTS_CONTENT_PAYMENT_CREDENTIAL_H_
 
+#include <map>
 #include <string>
 #include <vector>
 
+#include "base/memory/scoped_refptr.h"
+#include "components/webdata/common/web_data_service_base.h"
+#include "components/webdata/common/web_data_service_consumer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/payments/payment_credential.mojom.h"
 
 namespace payments {
 
+class PaymentManifestWebDataService;
+
 // Implementation of the mojom::PaymentCredential interface for storing
 // PaymentCredential instruments and their associated WebAuthn credential IDs.
 // These can be retrieved later to authenticate during a PaymentRequest
 // that uses Secure Payment Confirmation.
-class PaymentCredential : public mojom::PaymentCredential {
+class PaymentCredential : public mojom::PaymentCredential,
+                          public WebDataServiceConsumer {
  public:
-  explicit PaymentCredential(
+  PaymentCredential(
+      scoped_refptr<PaymentManifestWebDataService> web_data_sevice,
       mojo::PendingReceiver<mojom::PaymentCredential> receiver);
   ~PaymentCredential() override;
 
@@ -35,6 +43,14 @@ class PaymentCredential : public mojom::PaymentCredential {
       StorePaymentCredentialCallback callback) override;
 
  private:
+  // WebDataServiceConsumer:
+  void OnWebDataServiceRequestDone(
+      WebDataServiceBase::Handle h,
+      std::unique_ptr<WDTypedResult> result) override;
+
+  scoped_refptr<PaymentManifestWebDataService> web_data_service_;
+  std::map<WebDataServiceBase::Handle, StorePaymentCredentialCallback>
+      callbacks_;
   mojo::Receiver<mojom::PaymentCredential> receiver_{this};
 };
 
