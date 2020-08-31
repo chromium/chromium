f2e8fcfb3fc6a560a4a10b6fd145209882ef950a
Danyao Wang <danyao@chromium.org>
2020-08-31 15:58:44 +0000

    [M86][Web Payments] Create WebAuthn challenge for Secure Payment Confirmation.
    Teach the browser to generate a WebAuthn challenge that is a SHA-256
    hash over transaction data.
    
    Also added a test page that automates expectation updating for the unit
    test.
    
    TBR=danyao@chromium.org
    TBR=agl@chromium.org
    TBR=rouslan@chromium.org
    
    (cherry picked from commit 4c8f6737b133554b10305d0783c902be9e0ac866)
    
    Bug: 1121020
    Change-Id: Ic5515135eb8724eedc49d966f3986b0a48bbd519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381096
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Auto-Submit: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#802741}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2383294
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#279}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

---
+++ components/payments/content/secure_payment_confirmation_app_unittest.cc
@@ -0,0 +1,106 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/secure_payment_confirmation_app.h"
+
+#include "base/base64.h"
+#include "base/strings/string_piece.h"
+#include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/browser/payments/internal_authenticator.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/webauthn/authenticator.mojom.h"
+#include "url/origin.h"
+
+namespace payments {
+namespace {
+
+using ::testing::_;
+using ::testing::Eq;
+
+static constexpr char kNetworkDataBase64[] = "aaaa";
+static constexpr char kCredentialIdBase64[] = "cccc";
+
+class MockAuthenticator : public autofill::InternalAuthenticator {
+ public:
+  MOCK_METHOD1(SetEffectiveOrigin, void(const url::Origin&));
+  MOCK_METHOD2(
+      MakeCredential,
+      void(blink::mojom::PublicKeyCredentialCreationOptionsPtr options,
+           blink::mojom::Authenticator::MakeCredentialCallback callback));
+  MOCK_METHOD1(IsUserVerifyingPlatformAuthenticatorAvailable,
+               void(blink::mojom::Authenticator::
+                        IsUserVerifyingPlatformAuthenticatorAvailableCallback));
+  MOCK_METHOD0(Cancel, void());
+  MOCK_METHOD1(VerifyChallenge, void(const std::vector<uint8_t>&));
+
+  // Implements an autofill::InternalAuthenticator method to delegate fields of
+  // |options| to gmock methods for easier verification.
+  void GetAssertion(
+      blink::mojom::PublicKeyCredentialRequestOptionsPtr options,
+      blink::mojom::Authenticator::GetAssertionCallback callback) override {
+    VerifyChallenge(options->challenge);
+  }
+};
+
+class SecurePaymentConfirmationAppTest : public testing::Test {
+ protected:
+  SecurePaymentConfirmationAppTest()
+      : label_(base::ASCIIToUTF16("test instrument")),
+        total_(mojom::PaymentCurrencyAmount::New()) {
+    total_->currency = "USD";
+    total_->value = "1.25";
+  }
+
+  void SetUp() override {
+    ASSERT_TRUE(base::Base64Decode(kNetworkDataBase64, &network_data_bytes_));
+    ASSERT_TRUE(base::Base64Decode(kCredentialIdBase64, &credential_id_bytes_));
+  }
+
+  mojom::SecurePaymentConfirmationRequestPtr MakeRequest() {
+    auto request = mojom::SecurePaymentConfirmationRequest::New();
+    request->network_data = std::vector<uint8_t>(network_data_bytes_.begin(),
+                                                 network_data_bytes_.end());
+    return request;
+  }
+
+  base::string16 label_;
+  mojom::PaymentCurrencyAmountPtr total_;
+  std::string network_data_bytes_;
+  std::string credential_id_bytes_;
+};
+
+TEST_F(SecurePaymentConfirmationAppTest, Smoke) {
+  std::vector<uint8_t> credential_id(credential_id_bytes_.begin(),
+                                     credential_id_bytes_.end());
+
+  auto authenticator = std::make_unique<MockAuthenticator>();
+  MockAuthenticator* mock_authenticator = authenticator.get();
+
+  SecurePaymentConfirmationApp app(
+      "effective_rp.example",
+      /*icon=*/std::make_unique<SkBitmap>(), label_, std::move(credential_id),
+      url::Origin::Create(GURL("https://merchant.example")), total_,
+      MakeRequest(), std::move(authenticator));
+
+  EXPECT_CALL(*mock_authenticator, SetEffectiveOrigin(Eq(url::Origin::Create(
+                                       GURL("https://effective_rp.example")))));
+
+  // This is the SHA-256 hash of the serialized JSON string:
+  // {"merchantData":{"merchantOrigin":"https://merchant.example","total":
+  // {"currency":"USD","value":"1.25"}},"networkData":"aaaa"}
+  //
+  // To update the test expectation, open
+  // //components/test/data/payments/secure_payment_confirmation_debut.html in a
+  // browser and follow the instructions.
+  std::vector<uint8_t> expected_bytes = {
+      240, 123, 37,  51,  16,  34,  244, 220, 166, 179, 139,
+      85,  229, 152, 242, 133, 88,  44,  222, 133, 49,  97,
+      146, 20,  207, 119, 43,  142, 171, 239, 125, 250};
+  EXPECT_CALL(*mock_authenticator, VerifyChallenge(Eq(expected_bytes)));
+  app.InvokePaymentApp(/*delegate=*/nullptr);
+}
+
+}  // namespace
+}  // namespace payments
