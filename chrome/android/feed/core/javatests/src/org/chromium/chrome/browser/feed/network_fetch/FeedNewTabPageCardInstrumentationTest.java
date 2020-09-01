b7735567fce1fa4919e29818488ad23b14e0f44b
Arthur Wang <wuwang@google.com>
2020-09-01 22:30:13 +0000

    Change to use ChromeJUnit4ClassRunner to able to enable features.
    The ChromeJUnit4ClassRunner calls InstrumentationProcessor.applyFeatures.
    
    R=harringtond@chromium.org
    
    Bug: 1060399
    Change-Id: I747f6d926636d9ff7eae78fc8905bc5efd834324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388148
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803674}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
@@ -21,12 +21,12 @@ import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeActivityTestRule;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.WPRArchiveDirectory;
 import org.chromium.components.embedder_support.util.UrlConstants;
-import org.chromium.content_public.browser.test.ContentJUnit4ClassRunner;
 
 import java.io.IOException;
 
@@ -34,7 +34,7 @@ import java.io.IOException;
  * Tests for {@link FeedNewTabPage} using WPR record/replay infra to mock the backend.
  * Other tests could be found in {@link FeedNewTabPageCardRenderTest}.
  */
-@RunWith(ContentJUnit4ClassRunner.class)
+@RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
 @Features.EnableFeatures(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS)
 public final class FeedNewTabPageCardInstrumentationTest {
