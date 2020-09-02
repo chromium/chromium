e7bc8b040af4d12b3938a4b0ea5c64f016927483
Samuel Huang <huangs@chromium.org>
2020-09-02 21:09:00 +0000

    [Lacros] Size dashboard: Track total and total compressed sizes.
    This CL modifies lacros_resource_sizes.py to compute total files sizes
    and total compressed sizes. Compression relies on gzip as an external
    tool.
    
    Also add tracking for WidevineCdm/ directory.
    
    Bug: 1106626
    Change-Id: I84ac09b4f66f8f55bb1869e2917cdaa60ef4bf03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390877
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803974}

--- build/lacros/lacros_resource_sizes.py
+++ build/lacros/lacros_resource_sizes.py
@@ -8,11 +8,11 @@ More information at //docs/speed/binary_size/metrics.md.
 """
 
 import argparse
-import collections
 import contextlib
 import json
 import logging
 import os
+import subprocess
 import sys
 
 
@@ -55,29 +55,72 @@ _BASE_CHART = {
     'charts': {}
 }
 
-_Item = collections.namedtuple('_Item', ['paths', 'title'])
 
-# This list should be synched with chromeos-amd64-generic-lacros-rel builder
-# contents, specified in
-# //infra/config/subprojects/chromium/master-only/ci.star
-_TRACKED_ITEMS = [
-    _Item(paths=['chrome'], title='File: chrome'),
-    _Item(paths=['crashpad_handler'], title='File: crashpad_handler'),
-    _Item(paths=['icudtl.dat'], title='File: icudtl.dat'),
-    _Item(paths=['nacl_helper'], title='File: nacl_helper'),
-    _Item(paths=['nacl_irt_x86_64.nexe'], title='File: nacl_irt_x86_64.nexe'),
-    _Item(paths=['resources.pak'], title='File: resources.pak'),
-    _Item(paths=[
+class _Group:
+  """A group of build artifacts whose file sizes are summed and tracked.
+
+  Build artifacts for size tracking fall under these categories:
+  * File: A single file.
+  * Group: A collection of files.
+  * Dir: All files under a directory.
+
+  Attributes:
+    paths: A list of files or directories to be tracked together.
+    title: The display name of the group.
+    track_compressed: Whether to also track summed compressed sizes.
+  """
+
+  def __init__(self, paths, title, track_compressed=False):
+    self.paths = paths
+    self.title = title
+    self.track_compressed = track_compressed
+
+
+# List of disjoint build artifact groups for size tracking. This list should be
+# synched with chromeos-amd64-generic-lacros-rel builder contents (specified in
+# //infra/config/subprojects/chromium/ci.star) and
+# chromeos-amd64-generic-lacros-internal builder (specified in src-internal).
+_TRACKED_GROUPS = [
+    _Group(paths=['chrome'], title='File: chrome', track_compressed=True),
+    _Group(paths=['crashpad_handler'], title='File: crashpad_handler'),
+    _Group(paths=['icudtl.dat'], title='File: icudtl.dat'),
+    _Group(paths=['nacl_helper'], title='File: nacl_helper'),
+    _Group(paths=['nacl_irt_x86_64.nexe'], title='File: nacl_irt_x86_64.nexe'),
+    _Group(paths=['resources.pak'], title='File: resources.pak'),
+    _Group(paths=[
         'chrome_100_percent.pak', 'chrome_200_percent.pak', 'headless_lib.pak'
     ],
-          title='Group: Other PAKs'),
-    _Item(paths=['snapshot_blob.bin'], title='Group: Misc'),
-    _Item(paths=['locales/'], title='Dir: locales'),
-    _Item(paths=['swiftshader/'], title='Dir: swiftshader'),
+           title='Group: Other PAKs'),
+    _Group(paths=['snapshot_blob.bin'], title='Group: Misc'),
+    _Group(paths=['locales/'], title='Dir: locales'),
+    _Group(paths=['swiftshader/'], title='Dir: swiftshader'),
+    _Group(paths=['WidevineCdm/'], title='Dir: WidevineCdm'),
 ]
 
 
-def _get_single_filesize(filename):
+def _visit_paths(base_dir, paths):
+  """Itemizes files specified by a list of paths.
+
+  Args:
+    base_dir: Base directory for all elements in |paths|.
+    paths: A list of filenames or directory names to specify files whose sizes
+      to be counted. Directories are recursed. There's no de-duping effort.
+      Non-existing files or directories are ignored (with warning message).
+  """
+  for path in paths:
+    full_path = os.path.join(base_dir, path)
+    if os.path.exists(full_path):
+      if os.path.isdir(full_path):
+        for dirpath, _, filenames in os.walk(full_path):
+          for filename in filenames:
+            yield os.path.join(dirpath, filename)
+      else:  # Assume is file.
+        yield full_path
+    else:
+      logging.critical('Not found: %s', path)
+
+
+def _get_filesize(filename):
   """Returns the size of a file, or 0 if file is not found."""
   try:
     return os.path.getsize(filename)
@@ -86,28 +129,27 @@ def _get_single_filesize(filename):
   return 0
 
 
-def _get_total_pathsize(base_dir, paths):
-  """Computes total file sizes given by a list of paths, best-effort.
-
-  Args:
-    base_dir: Base directory for all elements in |paths|.
-    paths: A list of filenames or directory names to specify files whose sizes
-      to be counted. Directories are recursed. There's no de-duping effort.
-      Non-existing files or directories are ignored (with warning message).
-  """
-  total_size = 0
-  for path in paths:
-    full_path = os.path.join(base_dir, path)
-    if os.path.exists(full_path):
-      if os.path.isdir(full_path):
-        for dirpath, _, filenames in os.walk(full_path):
-          for filename in filenames:
-            total_size += _get_single_filesize(os.path.join(dirpath, filename))
-      else:  # Assume is file.
-        total_size += _get_single_filesize(full_path)
-    else:
-      logging.critical('Not found: %s', path)
-  return total_size
+def _get_gzipped_filesize(filename):
+  """Returns the gzipped size of a file, or 0 if file is not found."""
+  BUFFER_SIZE = 65536
+  if not os.path.isfile(filename):
+    return 0
+  try:
+    # Call gzip externally instead of using gzip package since it's > 2x faster.
+    cmd = ['gzip', '-c', filename]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    # Manually counting bytes instead of using len(p.communicate()[0]) to avoid
+    # buffering the entire compressed data (can be ~100 MB).
+    ret = 0
+    while True:
+      chunk = len(p.stdout.read(BUFFER_SIZE))
+      if chunk == 0:
+        break
+      ret += chunk
+    return ret
+  except OSError:
+    logging.critical('Failed to get gzipped size: %s', filename)
+  return 0
 
 
 def _dump_chart_json(output_dir, chartjson):
@@ -146,13 +188,43 @@ def _dump_chart_json(output_dir, chartjson):
 def _run_resource_sizes(args):
   """Main flow to extract and output size data."""
   chartjson = _BASE_CHART.copy()
-  for item in _TRACKED_ITEMS:
-    total_size = _get_total_pathsize(args.out_dir, item.paths)
-    perf_tests_results_helper.ReportPerfResult(chart_data=chartjson,
-                                               graph_title=item.title,
-                                               trace_title='size',
-                                               value=total_size,
-                                               units='bytes')
+  report_func = perf_tests_results_helper.ReportPerfResult
+  total_size = 0
+  total_gzipped = 0
+  for group in _TRACKED_GROUPS:
+    group_size = sum(map(_get_filesize, _visit_paths(args.out_dir,
+                                                     group.paths)))
+    group_gzipped = sum(
+        map(_get_gzipped_filesize, _visit_paths(args.out_dir, group.paths)))
+    report_func(chart_data=chartjson,
+                graph_title=group.title,
+                trace_title='size',
+                value=group_size,
+                units='bytes')
+    if group.track_compressed:
+      report_func(chart_data=chartjson,
+                  graph_title=group.title + ' (Gzipped)',
+                  trace_title='size',
+                  value=group_gzipped,
+                  units='bytes')
+    total_size += group_size
+    # Summing compressed size of separate groups (instead of concatanating
+    # first) to get a conservative estimate. File metadata and overheads are
+    # assumed to be negligible.
+    total_gzipped += group_gzipped
+
+  report_func(chart_data=chartjson,
+              graph_title='Total',
+              trace_title='size',
+              value=total_size,
+              units='bytes')
+
+  report_func(chart_data=chartjson,
+              graph_title='Total (Gzipped)',
+              trace_title='size',
+              value=total_gzipped,
+              units='bytes')
+
   _dump_chart_json(args.output_dir, chartjson)
 
 
