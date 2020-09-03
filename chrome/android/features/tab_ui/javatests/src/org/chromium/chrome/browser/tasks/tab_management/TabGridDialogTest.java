7af59db8c3227c271990fc71a46670b19e67f2b3
Yue Zhang <yuezhanggg@chromium.org>
2020-09-03 18:13:01 +0000

    [M86] Auto-select all texts when tab group title is focused
    (cherry picked from commit e544af06569ee72949f2144e1418cbfad5af8369)
    
    Bug: 1116644
    Change-Id: I2607ded4d277328067b79f5127664c7a457ba09e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2383092
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#803662}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2392739
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#425}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -594,7 +594,7 @@ public class TabGridDialogTest {
         openDialogFromTabSwitcherAndVerify(cta, 2,
                 cta.getResources().getQuantityString(
                         R.plurals.bottom_tab_grid_title_placeholder, 2, 2));
-        editDialogTitle(cta, CUSTOMIZED_TITLE1);
+        editDialogTitle(CUSTOMIZED_TITLE1);
 
         // Verify the title is updated in both tab switcher and dialog.
         clickScrimToExitDialog(cta);
@@ -605,7 +605,7 @@ public class TabGridDialogTest {
         // Modify title in dialog from tab strip.
         clickFirstTabInDialog(cta);
         openDialogFromStripAndVerify(cta, 2, CUSTOMIZED_TITLE1);
-        editDialogTitle(cta, CUSTOMIZED_TITLE2);
+        editDialogTitle(CUSTOMIZED_TITLE2);
 
         clickScrimToExitDialog(cta);
         waitForDialogHidingAnimation(cta);
@@ -770,7 +770,7 @@ public class TabGridDialogTest {
 
         // Content description should update with group title.
         openDialogFromTabSwitcherAndVerify(cta, 3, null);
-        editDialogTitle(cta, CUSTOMIZED_TITLE1);
+        editDialogTitle(CUSTOMIZED_TITLE1);
         clickScrimToExitDialog(cta);
         waitForDialogHidingAnimationInTabSwitcher(cta);
         verifyFirstCardTitle(CUSTOMIZED_TITLE1);
@@ -1070,9 +1070,16 @@ public class TabGridDialogTest {
                 });
     }
 
-    private void editDialogTitle(ChromeTabbedActivity cta, String title) {
+    private void editDialogTitle(String title) {
         onView(allOf(withParent(withId(R.id.main_content)), withId(R.id.title)))
-                .perform(click(), replaceText(title));
+                .perform(click())
+                .check((v, e) -> {
+                    // Verify all texts in the field are selected.
+                    EditText titleView = (EditText) v;
+                    assertEquals(titleView.getText().length(),
+                            titleView.getSelectionEnd() - titleView.getSelectionStart());
+                })
+                .perform(replaceText(title));
     }
 
     private void verifyFirstCardTitle(String title) {
