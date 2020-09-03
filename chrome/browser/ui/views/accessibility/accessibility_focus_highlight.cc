44749821de832fe8ee0d41a7df64f55f38a6ff85
Makoto Shimazu <shimazu@chromium.org>
2020-09-03 06:28:32 +0000

    Revert "Reland "Add pixel test for AccessibilityFocusHighlight based on FocusRingBrowserTest""
    This reverts commit 69caa3d02dbe309a25ff14225184a1a16e2533ea.
    
    Reason for revert: AccessibilityFocusHighlightBrowserTest.FocusAppearance looks failing. 
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.12%20Tests/37326
    
    Original change's description:
    > Reland "Add pixel test for AccessibilityFocusHighlight based on FocusRingBrowserTest"
    > 
    > This is a reland of b89144a43412d690f28af7a3df6f32257e8c94cf
    > 
    > Original change's description:
    > > Add pixel test for AccessibilityFocusHighlight based on FocusRingBrowserTest
    > >
    > > AX-Relnotes: n/a
    > >
    > > Bug: 1114959
    > > Change-Id: Ib6d9f51a04fa8059dc6bd6192d182b8e0244367d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352451
    > > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Reviewed-by: Joey Arhar <jarhar@chromium.org>
    > > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#803007}
    > 
    > TBR=ellyjones@chromium.org,masonfreed@chromium.org,jarhar@chromium.org
    > 
    > Bug: 1114959
    > Change-Id: I1997496df505691103568989a9cbfaa38e4e9b0e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388025
    > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#804034}
    
    TBR=ellyjones@chromium.org,dmazzoni@chromium.org,aboxhall@chromium.org,masonfreed@chromium.org,jarhar@chromium.org
    
    Change-Id: I28f6ebeabe7204a329ff6fd4f42b87312ff9829e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1114959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390586
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804129}

--- chrome/browser/ui/views/accessibility/accessibility_focus_highlight.cc
+++ chrome/browser/ui/views/accessibility/accessibility_focus_highlight.cc
@@ -77,9 +77,6 @@ bool AccessibilityFocusHighlight::skip_activation_check_for_testing_ = false;
 // static
 bool AccessibilityFocusHighlight::use_default_color_for_testing_ = false;
 
-// static
-bool AccessibilityFocusHighlight::no_fade_for_testing_ = false;
-
 AccessibilityFocusHighlight::AccessibilityFocusHighlight(
     BrowserView* browser_view)
     : browser_view_(browser_view) {
@@ -101,7 +98,7 @@ AccessibilityFocusHighlight::AccessibilityFocusHighlight(
     fade_in_time_ = kFadeInTime;
     persist_time_ = kHighlightPersistTime;
     fade_out_time_ = kFadeOutTime;
-    default_color_ = SkColorSetRGB(0x10, 0x10, 0x10);  // #101010
+    default_color_ = SkColorSetRGB(16, 16, 16);  // #101010
   }
 }
 
@@ -112,7 +109,9 @@ AccessibilityFocusHighlight::~AccessibilityFocusHighlight() {
 
 // static
 void AccessibilityFocusHighlight::SetNoFadeForTesting() {
-  no_fade_for_testing_ = true;
+  fade_in_time_ = base::TimeDelta();
+  persist_time_ = base::TimeDelta::FromHours(1);
+  fade_out_time_ = base::TimeDelta();
 }
 
 // static
@@ -125,17 +124,7 @@ void AccessibilityFocusHighlight::UseDefaultColorForTesting() {
   use_default_color_for_testing_ = true;
 }
 
-// static
-ui::Layer* AccessibilityFocusHighlight::GetLayerForTesting() {
-  return layer_.get();
-}
-
 SkColor AccessibilityFocusHighlight::GetHighlightColor() {
-#if !defined(OS_MAC)
-  // Match behaviour with renderer_preferences_util::UpdateFromSystemSettings
-  // setting prefs->focus_ring_color
-  return default_color_;
-#else
   ui::NativeTheme* native_theme = ui::NativeTheme::GetInstanceForWeb();
   SkColor theme_color = native_theme->GetSystemColor(
       ui::NativeTheme::kColorId_FocusedBorderColor);
@@ -144,7 +133,6 @@ SkColor AccessibilityFocusHighlight::GetHighlightColor() {
     return default_color_;
 
   return native_theme->FocusRingColorForBaseColor(theme_color);
-#endif
 }
 
 void AccessibilityFocusHighlight::CreateOrUpdateLayer(gfx::Rect node_bounds) {
@@ -205,9 +193,6 @@ void AccessibilityFocusHighlight::CreateOrUpdateLayer(gfx::Rect node_bounds) {
 }
 
 void AccessibilityFocusHighlight::RemoveLayer() {
-  if (no_fade_for_testing_)
-    return;
-
   layer_.reset();
   if (compositor_) {
     compositor_->RemoveAnimationObserver(this);
@@ -326,29 +311,6 @@ void AccessibilityFocusHighlight::OnPaintLayer(
   recorder.canvas()->DrawRoundRect(bounds, kBorderRadius, original_flags);
 }
 
-float AccessibilityFocusHighlight::ComputeOpacity(
-    base::TimeDelta time_since_layer_create,
-    base::TimeDelta time_since_focus_move) {
-  float opacity = 1.0f;
-
-  if (no_fade_for_testing_)
-    return opacity;
-
-  if (time_since_layer_create < fade_in_time_) {
-    // We're fading in.
-    opacity = time_since_layer_create / fade_in_time_;
-  }
-
-  if (time_since_focus_move > persist_time_) {
-    // Fading out.
-    base::TimeDelta time_since_began_fading =
-        time_since_focus_move - (fade_in_time_ + persist_time_);
-    opacity = 1.0f - (time_since_began_fading / fade_out_time_);
-  }
-
-  return base::ClampToRange(opacity, 0.0f, 1.0f);
-}
-
 void AccessibilityFocusHighlight::OnAnimationStep(base::TimeTicks timestamp) {
   if (!layer_)
     return;
@@ -376,8 +338,21 @@ void AccessibilityFocusHighlight::OnAnimationStep(base::TimeTicks timestamp) {
     return;
   }
 
-  float opacity =
-      ComputeOpacity(time_since_layer_create, time_since_focus_move);
+  // Compute the opacity based on the fade in and fade out times.
+  // TODO(aboxhall): figure out how to use cubic beziers
+  float opacity = 1.0f;
+  if (time_since_layer_create < fade_in_time_) {
+    // We're fading in.
+    opacity = time_since_layer_create / fade_in_time_;
+  } else if (time_since_focus_move > persist_time_) {
+    // Fading out.
+    base::TimeDelta time_since_began_fading =
+        time_since_focus_move - (fade_in_time_ + persist_time_);
+    opacity = 1.0f - (time_since_began_fading / fade_out_time_);
+  }
+
+  // Layer::SetOpacity will throw an error if we're not within 0...1.
+  opacity = base::ClampToRange(opacity, 0.0f, 1.0f);
   layer_->SetOpacity(opacity);
 }
 
