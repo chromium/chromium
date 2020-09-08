dc50a154b417f16a98f6d8159ffe51a143afa5d5
Tibor Goldschwendt <tiborg@chromium.org>
2020-09-08 22:00:06 +0000

    [ntp][modules] Add basic collapse and expand functionality
    Collapsing/expanding hides/shows the local module UI.
    
    Before: https://screenshot.googleplex.com/7YpsgDQZg6EQbjT
    After: https://screenshot.googleplex.com/3wv7qaoupMtPQj5
    
    Bug: 1124089
    Change-Id: I80d88b1c2311da6a4d047e2ffb4837b6d4158add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388977
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805099}

--- chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.js
+++ chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import {$$} from 'chrome://new-tab-page/new_tab_page.js';
+import {assertNotStyle, assertStyle} from 'chrome://test/new_tab_page/test_support.js';
 
 suite('NewTabPageModulesModuleWrapperTest', () => {
   /** @type {!ModuleWrapperElement} */
@@ -29,9 +30,47 @@ suite('NewTabPageModulesModuleWrapperTest', () => {
 
     // Assert.
     assertEquals('Foo Title', moduleWrapper.$.title.textContent);
-    assertEquals(' • Foo', moduleWrapper.$.name.textContent);
+    assertEquals('Foo', moduleWrapper.$.name.textContent);
     assertEquals(100, $$(moduleWrapper, '#moduleElement').offsetHeight);
     assertDeepEquals(
         moduleElement, $$(moduleWrapper, '#moduleElement').children[0]);
+  });
+
+  test('collapses and expands', () => {
+    // Arrange.
+    const moduleElement = document.createElement('div');
+    moduleWrapper.descriptor = {
+      id: 'foo',
+      name: 'Foo',
+      heightPx: 100,
+      title: 'Foo Title',
+      element: moduleElement,
+    };
+
+    // Act (collapse).
+    moduleWrapper.$.toggleButton.click();
+
+    // Assert (collapsed).
+    assertStyle(moduleWrapper.$.title, 'display', 'none');
+    assertStyle(moduleWrapper.$.dot, 'display', 'none');
+    assertStyle(moduleWrapper.$.moduleElement, 'display', 'none');
+    assertStyle(
+        moduleWrapper.$.toggleButton, '--cr-icon-image',
+        'url("chrome://resources/images/icon_expand_more.svg")');
+    assertEquals(
+        moduleWrapper.$.header.offsetHeight, moduleWrapper.offsetHeight);
+
+    // Act (expand).
+    moduleWrapper.$.toggleButton.click();
+
+    // Assert (expanded).
+    assertNotStyle(moduleWrapper.$.title, 'display', 'none');
+    assertNotStyle(moduleWrapper.$.dot, 'display', 'none');
+    assertNotStyle(moduleWrapper.$.moduleElement, 'display', 'none');
+    assertStyle(
+        moduleWrapper.$.toggleButton, '--cr-icon-image',
+        'url("chrome://resources/images/icon_expand_less.svg")');
+    assertEquals(
+        moduleWrapper.$.header.offsetHeight + 100, moduleWrapper.offsetHeight);
   });
 });
