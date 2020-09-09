4c8e589d572b5dd6b76cae305c26c290db53305a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-09-09 20:00:22 +0000

    Import wpt@0b2796c08317aa7279b8dca488d50ce2f674fc13
    Using wpt-import in Chromium dc952b0bf73832821cc1233656a0b00b704b1347.
    With Chromium commits locally applied on WPT:
    c07a387a7f "Make DefinitelyNewFormattingContext return true for SVG foreignObject"
    eadf70300a "Implement Animation::setTimeline"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=smcgruer@google.com
    
    No-Export: true
    Change-Id: Id849639ce9fa7f652eb9d8768b91f5d8e3b601c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401760
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#805450}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/stability.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/stability.py
@@ -347,7 +347,7 @@ def check_stability(logger, repeat_loop=10, repeat_restart=5, chaos_mode=True, m
     start_time = datetime.now()
     step_results = []
 
-    github_checks_outputter = get_gh_checks_outputter(kwargs)
+    github_checks_outputter = get_gh_checks_outputter(kwargs["github_checks_text_file"])
 
     for desc, step_func in steps:
         if max_time and datetime.now() - start_time > max_time:
