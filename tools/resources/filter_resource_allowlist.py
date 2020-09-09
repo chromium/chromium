835f0bc3da97aca97f22488d04aa6e92c3c39cb5
Nate Fischer <ntfschr@chromium.org>
2020-09-09 19:15:01 +0000

    Code inclusion: whitelist -> allowlist for repack and related code
    This renames "repack_whitelist" to "repack_allowlist" and swaps out
    related code (ex. variables in chrome/ layer). The new term is
    consistent with "enable_resource_allowlist_generation" and avoids
    non-inclusive terminology.
    
    Bug: 1097644
    Test: autoninja -C out/Official monochrome_public_apk (official build)
    Change-Id: I3e319c8b8bec9ab9df081b0b61ec02683d665af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399453
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805426}

--- tools/resources/filter_resource_allowlist.py
+++ tools/resources/filter_resource_allowlist.py
@@ -3,17 +3,17 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""filter_resource_whitelist.py [-h] [--input INPUT] [--filter FILTER]
+"""filter_resource_allowlist.py [-h] [--input INPUT] [--filter FILTER]
 [--output OUTPUT]
 
-INPUT specifies a resource whitelist file containing resource IDs that should
-be whitelisted, where each line of INPUT contains a single resource ID.
+INPUT specifies a resource allowlist file containing resource IDs that should
+be allowed, where each line of INPUT contains a single resource ID.
 
-FILTER specifies a resource whitelist file containing resource IDs that should
-not be whitelisted, where each line of FILTER contains a single resource ID.
+FILTER specifies a resource denylist file containing resource IDs that should
+not be allowed, where each line of FILTER contains a single resource ID.
 
-Filters a resource whitelist by removing resource IDs that are contained in a
-another resource whitelist.
+Filters a resource allowlist by removing resource IDs that are contained in a
+another resource allowlist.
 
 This script is used to generate Monochrome's locale paks.
 """
@@ -26,11 +26,11 @@ def main():
   parser = argparse.ArgumentParser(usage=__doc__)
   parser.add_argument(
       '--input', type=argparse.FileType('r'), required=True,
-      help='A resource whitelist where each line contains one resource ID. '
+      help='A resource allowlist where each line contains one resource ID. '
            'These IDs, excluding the ones in FILTER, are to be included.')
   parser.add_argument(
       '--filter', type=argparse.FileType('r'), required=True,
-      help='A resource whitelist where each line contains one resource ID. '
+      help='A resource allowlist where each line contains one resource ID. '
            'These IDs are to be excluded.')
   parser.add_argument(
       '--output', type=argparse.FileType('w'), default=sys.stdout,
