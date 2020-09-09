713cc2e0699baf455cd0ba0e27a954c47456cab7
Kramer Ge <fangzhoug@chromium.org>
2020-09-09 13:16:50 +0000

    [ozone/wayland] MockSurface should destroy its role upon destruction
    TestSubsurface has a ptr to MockSurface wl_resource which may incur
    use-after-free if MockSurface is destroyed first. Do the same thing we
    do for MockXdgSurface in MockSurface dtor to avoid it.
    
    Change-Id: Ia63c187485c39033f715f446cc724fcc4043b46a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391388
    Reviewed-by: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805289}

--- ui/ozone/platform/wayland/test/mock_surface.cc
+++ ui/ozone/platform/wayland/test/mock_surface.cc
@@ -87,6 +87,8 @@ MockSurface::MockSurface(wl_resource* resource) : ServerObject(resource) {}
 MockSurface::~MockSurface() {
   if (xdg_surface_ && xdg_surface_->resource())
     wl_resource_destroy(xdg_surface_->resource());
+  if (sub_surface_ && sub_surface_->resource())
+    wl_resource_destroy(sub_surface_->resource());
 }
 
 MockSurface* MockSurface::FromResource(wl_resource* resource) {
