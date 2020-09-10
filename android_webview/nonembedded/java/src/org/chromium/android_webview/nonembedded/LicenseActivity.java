aea155f43e44872a010fb6541e8130ab26a189d7
Torne (Richard Coles) <torne@google.com>
2020-09-10 21:56:26 +0000

    webview: grant URI permission in LicenseActivity.
    Android 11 package visibility rules mean that HTMLViewer cannot access
    LicenseContentProvider by default, even though the content provider does
    not require a permission, because that would reveal the existence of the
    package to it.
    
    To fix this, explicitly grant URI access permission in the intent used
    to invoke HTMLViewer - while actual permission is not necessary, this
    also implicitly grants HTMLViewer the right to "see" the WebView
    package.
    
    Note that in most circumstances this was already working without this
    change: when an app uses WebView, the platform grants the app
    visibility of the current WebView implementation package as a side
    effect (so that it can load the code), so as long as the
    LicenseActivity belonged to the current WebView there wasn't an issue.
    This change is only required for the case where you launch the
    LicenseActivity for a *different* WebView implementation.
    
    Change-Id: I7bab033c05d386039c55799328fe0163bc4906ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404364
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805917}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/LicenseActivity.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/LicenseActivity.java
@@ -32,6 +32,7 @@ public class LicenseActivity extends Activity {
                 String.format("content://%s.%s", packageName, LICENSES_URI_SUFFIX);
         intent.setDataAndType(Uri.parse(licenseUri), LICENSES_CONTENT_TYPE);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         // Resources are accessed via getIdentifier because resource ids in the standalone system
         // webview apk have incorrect package_ids. In order to access a resource by identifier, you
         // must first add it to //android_webview/aapt2.config. see https://crbug.com/894208
