83d52b3a0266c0a7f37f48956fd854a104cd4655
John Chen <johnchen@chromium.org>
2020-09-11 20:40:55 +0000

    [tools/perf] Reenable speedometer2-future unittest
    The test has been disabled for almost a year, and all crash dumps are gone.
    Reenable test to see if it is still flaky, and collect new dumps if needed.
    
    Bug: 1003776
    Change-Id: Iccd4bd23b102e1e3c6d1346c04443c4d2e540096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363360
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Auto-Submit: John Chen <johnchen@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#806315}

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -104,7 +104,6 @@ _BLACK_LIST_TEST_NAMES = [
     'tab_switching.typical_25',
     'UNSCHEDULED_oortonline_tbmv2',
     'webrtc',  # crbug.com/932036
-    'speedometer2-future',  # Flaky on mac, crbug.com/1003776
     'v8.runtime_stats.top_25'  # Fails in Windows, crbug.com/1043048
 ]
 
