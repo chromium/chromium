ea6cdd79adbb2283acd9b242c0ba5b7781046e59
Sylvain Defresne <sdefresne@chromium.org>
2020-09-15 08:49:08 +0000

    Replace leaky singleton with base::NoDestructor
    base::Singleton is deprecated in favor of base::NoDestructor. The
    conversion is straight-forward for this leaky singleton and this
    allow removing unnecessary scaffolding code.
    
    Bug: none
    Change-Id: I1db6f1fdd00b3532efbaea8367c8563cc5e28740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2409495
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806940}

--- crypto/mac_security_services_lock.cc
+++ crypto/mac_security_services_lock.cc
@@ -4,39 +4,14 @@
 
 #include "crypto/mac_security_services_lock.h"
 
-#include "base/macros.h"
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
-
-namespace {
-
-class SecurityServicesSingleton {
- public:
-  static SecurityServicesSingleton* GetInstance() {
-    return base::Singleton<
-        SecurityServicesSingleton,
-        base::LeakySingletonTraits<SecurityServicesSingleton>>::get();
-  }
-
-  base::Lock& lock() { return lock_; }
-
- private:
-  friend struct base::DefaultSingletonTraits<SecurityServicesSingleton>;
-
-  SecurityServicesSingleton() {}
-  ~SecurityServicesSingleton() {}
-
-  base::Lock lock_;
-
-  DISALLOW_COPY_AND_ASSIGN(SecurityServicesSingleton);
-};
-
-}  // namespace
 
 namespace crypto {
 
 base::Lock& GetMacSecurityServicesLock() {
-  return SecurityServicesSingleton::GetInstance()->lock();
+  static base::NoDestructor<base::Lock> lock;
+  return *lock;
 }
 
 }  // namespace crypto
