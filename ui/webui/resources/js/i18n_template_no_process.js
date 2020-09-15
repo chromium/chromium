d9304f6eec020f0ee773e5b022f927557b1c915f
rbpotter <rbpotter@chromium.org>
2020-09-15 22:20:00 +0000

    Add deprecation warnings to i18n_template files, remove i18n_template.js
    Bug: 1128151
    Change-Id: I6cc8bdc763ef6190437ef816b60692ed71c706cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410849
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807202}

--- ui/webui/resources/js/i18n_template_no_process.js
+++ ui/webui/resources/js/i18n_template_no_process.js
@@ -6,7 +6,13 @@
 let ProcessingRoot;
 
 /**
- * @fileoverview This is a simple template engine inspired by JsTemplates
+ * @fileoverview
+ *
+ * This file (and related i18n_template_process.js) are deprecated and
+ * existing usages should be removed (see https://crbug.com/1128151).
+ * Do not add any additional usages of this file.
+ *
+ * This is a simple template engine inspired by JsTemplates
  * optimized for i18n.
  *
  * It currently supports three handlers:
--- ui/webui/resources/js/i18n_template_process.js
+++ ui/webui/resources/js/i18n_template_process.js
@@ -2,4 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This file (and related i18n_template_no_process.js) are deprecated and
+// existing usages should be removed (see https://crbug.com/1128151).
+// Do not add any additional usages of this file.
+
 i18nTemplate.process(document, loadTimeData);
