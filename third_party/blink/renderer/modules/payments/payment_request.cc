13771712b0eca90b550ee3e9b187b29b83055eca
Glen Robertson <glenrob@chromium.org>
2020-09-16 04:41:46 +0000

    digitalgoods: Add origin_trial_feature_name and _os for DigitalGoods.
    Origin Trial on Android only for now.
    
    Bug: 1032423,1096428
    Change-Id: I8329c798138d800156bbe3f90de6d05f2d9662b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404284
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807337}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -598,7 +598,8 @@ void ValidateAndConvertPaymentDetailsUpdate(const PaymentDetailsUpdate* input,
   if (exception_state.HadException())
     return;
   if (input->hasTotal()) {
-    DCHECK(!RuntimeEnabledFeatures::DigitalGoodsEnabled() || !ignore_total);
+    DCHECK(!RuntimeEnabledFeatures::DigitalGoodsEnabled(&execution_context) ||
+           !ignore_total);
     if (ignore_total) {
       output->total =
           CreateTotalPlaceHolderForAppStoreBilling(execution_context);
@@ -1233,8 +1234,9 @@ PaymentRequest::PaymentRequest(
   if (exception_state.HadException())
     return;
 
-  ignore_total_ = RuntimeEnabledFeatures::DigitalGoodsEnabled() &&
-                  RequestingOnlyAppStoreBillingMethods(validated_method_data);
+  ignore_total_ =
+      RuntimeEnabledFeatures::DigitalGoodsEnabled(GetExecutionContext()) &&
+      RequestingOnlyAppStoreBillingMethods(validated_method_data);
   ValidateAndConvertPaymentDetailsInit(details, options_, validated_details,
                                        shipping_option_, ignore_total_,
                                        *GetExecutionContext(), exception_state);
