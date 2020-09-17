82aa15ff6d8c2fd9d05a73838bc3ef6ebafb7072
David Bertoni <dbertoni@chromium.org>
2020-09-17 22:49:30 +0000

    [Extensions] Add more Alarms API test coverage.
    Bug: 1093066
    Change-Id: I8298635bd7f1717c29f020a6901a362f72c7d9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404943
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808124}

--- chrome/test/data/extensions/api_test/alarms/spanning/background.js
+++ chrome/test/data/extensions/api_test/alarms/spanning/background.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 let inIncognito = chrome.extension.inIncognitoContext;
+let alarmName = inIncognito ? 'incognito' : 'normal';
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   chrome.test.assertEq(inIncognito ? 'incognito' : 'normal', alarm.name);
@@ -11,8 +12,27 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 
 chrome.test.runTests([
   // Creates an alarm with the name of the context it was created in.
-  function createAlarms() {
-    const alarmName = inIncognito ? 'incognito' : 'normal';
-    chrome.alarms.create(alarmName, {delayInMinutes: 0.001});
+  function createAlarm() {
+    chrome.alarms.create(alarmName, {delayInMinutes: 0.001,
+                                     periodInMinutes: 60});
+  },
+  function getAlarm() {
+    chrome.alarms.get(alarmName, function(alarm) {
+      chrome.test.assertEq(alarmName, alarm.name);
+      chrome.test.succeed();
+    });
+  },
+  function getAllAlarms() {
+    chrome.alarms.getAll(function(alarms) {
+      chrome.test.assertEq(1, alarms.length);
+      chrome.test.assertEq(alarmName, alarms[0].name);
+      chrome.test.succeed();
+    });
+  },
+  function clearAlarm() {
+    chrome.alarms.clear(alarmName, function(wasCleared) {
+      chrome.test.assertTrue(wasCleared);
+      chrome.test.succeed();
+    });
   }
 ]);
