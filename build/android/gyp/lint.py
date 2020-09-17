2c43385a51e189b4d36d1827e2d146bc38403f40
Clark DuVall <cduvall@chromium.org>
2020-09-17 05:46:44 +0000

    Reland "Add build flag for building //chrome in a DFM"
    This is a reland of eea2a6fbd8fb51a7c1ab57cc6c1c878789e21e79
    
    Original change's description:
    > Add build flag for building //chrome in a DFM
    >
    > This change adds much of the support needed for building //chrome in a
    > DFM. Some TODOs have been scattered around for follow up changes. A gn
    > flag enable_chrome_module has been added to toggle the DFM.
    >
    > The main pieces here are (when the gn flag is enabled):
    >   1. All //chrome Java code will be moved into a "chrome" DFM.
    >
    >   2. All DFM Java code will also be moved into the chrome DFM.
    >
    >   3. All //chrome-specific definitions in the <application> manifest are
    >      moved to the "chrome" DFM manifest. This was done by moving all of
    >      the definitions in the main AndroidManifest.xml into a jinja macro
    >      which can either be called in the main AndroidManifest.xml or in
    >      the DFM AndroidManifest.xml.
    >
    >   4. Resources needed in the base AndroidManfiest.xml are moved to a
    >      separate target so they can be included only in the base module.
    >
    >   5. Changes were added to lint.py to allow linting the chrome DFM
    >      manifest along with the base manifest to avoid unused resource
    >      errors.
    >
    > Still TODO in follow ups:
    >   - Address TODOs added in this change.
    >   - Make a similar change in //clank.
    >   - Deal with org.chromium.chrome.browser.ChromeBackupAgent which must
    >     be referenced in application tag of the base manifest, but will be
    >     defined in the chrome DFM.
    >   - Change the application class to allow loading with isolated splits.
    >   - Collect loadable modules from DFMs and put in base.
    >   - Probably more...
    >
    > Bug: 1126301
    > Change-Id: I3fac243101eb2fb1e4af21ee99c2349fb074677b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412553
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#807704}
    
    TBR=agrieve@chromium.org,mdjones@chromium.org
    
    Bug: 1126301
    Change-Id: Ia1af90628c17c3d538c2fea4d48584bcc09ae725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2415489
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807762}

--- build/android/gyp/lint.py
+++ build/android/gyp/lint.py
@@ -109,10 +109,18 @@ def _GenerateProjectFile(android_manifest,
   return project
 
 
-def _GenerateAndroidManifest(original_manifest_path, min_sdk_version,
-                             android_sdk_version):
+def _GenerateAndroidManifest(original_manifest_path, extra_manifest_paths,
+                             min_sdk_version, android_sdk_version):
   # Set minSdkVersion in the manifest to the correct value.
   doc, manifest, app_node = manifest_utils.ParseManifest(original_manifest_path)
+
+  # TODO(crbug.com/1126301): Should this be done using manifest merging?
+  # Add anything in the application node of the extra manifests to the main
+  # manifest to prevent unused resource errors.
+  for path in extra_manifest_paths:
+    _, _, extra_app_node = manifest_utils.ParseManifest(path)
+    for node in extra_app_node:
+      app_node.append(node)
 
   if app_node.find(
       '{%s}allowBackup' % manifest_utils.ANDROID_NAMESPACE) is None:
@@ -144,6 +152,7 @@ def _WriteXmlFile(root, path):
 def _RunLint(lint_binary_path,
              config_path,
              manifest_path,
+             extra_manifest_paths,
              sources,
              classpath,
              cache_dir,
@@ -178,6 +187,7 @@ def _RunLint(lint_binary_path,
 
   logging.info('Generating Android manifest file')
   android_manifest_tree = _GenerateAndroidManifest(manifest_path,
+                                                   extra_manifest_paths,
                                                    min_sdk_version,
                                                    android_sdk_version)
   # Include the rebased manifest_path in the lint generated path so that it is
@@ -310,6 +320,10 @@ def _ParseArgs(argv):
   parser.add_argument('--srcjars', help='GN list of included srcjars.')
   parser.add_argument('--manifest-path',
                       help='Path to original AndroidManifest.xml')
+  parser.add_argument('--extra-manifest-paths',
+                      action='append',
+                      help='GYP-list of manifest paths to merge into the '
+                      'original AndroidManifest.xml')
   parser.add_argument('--resource-sources',
                       default=[],
                       action='append',
@@ -330,6 +344,7 @@ def _ParseArgs(argv):
   args.java_sources = build_utils.ParseGnList(args.java_sources)
   args.srcjars = build_utils.ParseGnList(args.srcjars)
   args.resource_sources = build_utils.ParseGnList(args.resource_sources)
+  args.extra_manifest_paths = build_utils.ParseGnList(args.extra_manifest_paths)
   args.resource_zips = build_utils.ParseGnList(args.resource_zips)
   args.classpath = build_utils.ParseGnList(args.classpath)
   return args
@@ -356,6 +371,7 @@ def main():
   _RunLint(args.lint_binary_path,
            args.config_path,
            args.manifest_path,
+           args.extra_manifest_paths,
            sources,
            args.classpath,
            args.cache_dir,
