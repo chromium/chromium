ca982ce7c09116a0781a5a155996facf201fad8e
Dale Curtis <dalecurtis@chromium.org>
2020-09-17 00:01:44 +0000

    Disable flaky OverviewListLayoutTest#testCanSwipeClosed.
    TBR=twellington
    
    Bug: 1127280
    Change-Id: I5e2e70478193d7d4b539467d5fbf54f206b0c7cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2415216
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807722}

--- chrome/android/javatests/src/org/chromium/chrome/browser/accessibility_tab_switcher/OverviewListLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/accessibility_tab_switcher/OverviewListLayoutTest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.CommandLine;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -194,6 +195,7 @@ public class OverviewListLayoutTest {
     }
 
     @Test
+    @DisabledTest(message = "TODO(crbug.com/1127280): Fix flakiness.")
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     @MediumTest
     @Feature({"Accessibility"})
