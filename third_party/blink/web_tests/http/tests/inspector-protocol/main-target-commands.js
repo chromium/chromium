62c286d1bda3c26b838daef9ec0cfee05e08090d
Andrey Kosyakov <caseq@chromium.org>
2020-09-19 01:38:53 +0000

    Do not execute global commands on non-root targets
    Bug: 1116444
    Change-Id: Ic50dfc144f8024870131e7586b9dce2dff591e42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419712
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808635}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/main-target-commands.js
@@ -0,0 +1,25 @@
+(async function(testRunner) {
+    const {session, dp} = await testRunner.startBlank('Tests for certain commands are only available at the top-level target');
+
+    await dp.Page.enable();
+    await dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: true, flatten: true});
+    dp.Page.navigate({url: testRunner.url('./resources/site_per_process_main.html')});
+
+    const {params} = await dp.Target.onceAttachedToTarget();
+    const childSession = session.createChild(params.sessionId);
+    const dp2 = childSession.protocol;
+    dp2.Page.enable();
+
+    async function checkCommand(method, params) {
+      const result = await childSession.sendCommand(method, params);
+
+      testRunner.log(`${method}: ` + (result.error ? result.error.message : 'FAIL: no error'));
+    }
+
+    await checkCommand('Page.captureSnapshot', {});
+    await checkCommand('Page.captureScreenshot', {});
+    await checkCommand('Page.startScreencast', {});
+    await checkCommand('Page.setDownloadBehavior', { behavior: 'deny'});
+
+    testRunner.completeTest();
+  })
