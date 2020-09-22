be6a8a343c52c6fb1b2be67d962b109b342d294f
Sebastien Marchand <sebmarchand@chromium.org>
2020-09-22 23:09:50 +0000

    Fix the Mac coverage build
    Bug: 1131200
    Change-Id: I04b3fce954c66a2e7299d1e7cfa102754f1dc459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425126
    Auto-Submit: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809566}

--- content/browser/child_process_task_port_provider_mac_unittest.cc
+++ content/browser/child_process_task_port_provider_mac_unittest.cc
@@ -35,6 +35,8 @@ class MockChildProcess : public mojom::ChildProcess {
 #endif
 #if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
   MOCK_METHOD1(SetProfilingFile, void(base::File));
+  MOCK_METHOD1(WriteClangProfilingProfile,
+               void(WriteClangProfilingProfileCallback));
 #endif
   MOCK_METHOD1(GetBackgroundTracingAgentProvider,
                void(mojo::PendingReceiver<
