4e791c498720ca1dab2da7b62c8c32158520233f
Darwin Huang <huangdarwin@chromium.org>
2020-09-22 23:07:55 +0000

    Revert "[ntp][modules] Disable dummy modules in NTP for official build"
    This reverts commit a099d2dbc18d632d13e233bd117e0aa3640c9342.
    
    Reason for revert: NewTabPageModulesDummyModuleTest.All failing. bot failures on linux-chromeos-chrome: https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    
    First failure: https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/8711
    
    Original change's description:
    > [ntp][modules] Disable dummy modules in NTP for official build
    >
    > Bug: 1130814
    > Change-Id: If8d93fb7c134637f399735637ff031abbe31ebc2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2411671
    > Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809463}
    
    TBR=tiborg@chromium.org,aee@chromium.org,yuezhanggg@chromium.org
    
    Change-Id: I4537af0a34a5cc61a1f7b228a5047127862e2a0f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1130814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425207
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809564}

--- chrome/browser/resources/new_tab_page/modules/modules.js
+++ chrome/browser/resources/new_tab_page/modules/modules.js
@@ -8,9 +8,7 @@
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 
-// <if expr="not is_official_build">
 import {dummyDescriptor, dummyDescriptor2} from './dummy/module.js';
-// </if>
 import {kaleidoscopeDescriptor} from './kaleidoscope/module.js';
 import {ModuleDescriptor} from './module_descriptor.js';
 import {ModuleRegistry} from './module_registry.js';
@@ -22,9 +20,7 @@ if (loadTimeData.getBoolean('kaleidoscopeModuleEnabled')) {
   descriptors.push(kaleidoscopeDescriptor);
 }
 
-// <if expr="not is_official_build">
 descriptors.push(dummyDescriptor);
 descriptors.push(dummyDescriptor2);
-// </if>
 
 ModuleRegistry.getInstance().registerModules(descriptors);
