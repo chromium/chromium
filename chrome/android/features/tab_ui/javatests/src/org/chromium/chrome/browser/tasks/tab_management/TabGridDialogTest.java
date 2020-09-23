499623995faf9888e0d50c0f47d3440467aaed77
Brandon Wylie <wylieb@chromium.org>
2020-09-23 18:58:37 +0000

    Disable failing testUndoClosureInDialog_WithStartSurface on Android N
    Bug: 1131545
    Change-Id: I9617e2d26f7acd50b44a9151927aff6713cdab28
    
    TBR=wychen@chromium.org
    
    Change-Id: I9617e2d26f7acd50b44a9151927aff6713cdab28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426489
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809885}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -918,8 +918,10 @@ public class TabGridDialogTest {
     @MediumTest
     @Features.EnableFeatures({ChromeFeatureList.START_SURFACE_ANDROID + "<Study"})
     @CommandLineFlags.Add({"force-fieldtrials=Study/Group", START_SURFACE_BASE_PARAMS + "/single"})
-    @DisableIf.Build(supported_abis_includes = "x86", message = "https://crbug.com/1119899")
-    public void testUndoClosureInDialog_WithStartSurface() throws Exception {
+    @DisableIf.Build(supported_abis_includes = "x86", sdk_is_greater_than = 23,
+            sdk_is_less_than = 26, message = "crbug.com/1119899, crbug.com/1131545")
+    public void
+    testUndoClosureInDialog_WithStartSurface() throws Exception {
         // Create a tab group with 2 tabs.
         finishActivity(mActivityTestRule.getActivity());
         createThumbnailBitmapAndWriteToFile(0);
