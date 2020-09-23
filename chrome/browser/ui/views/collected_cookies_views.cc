2f7820e302eb0dca9b8c90d2d8b7955ef7e67a04
Allen Bauer <kylixrd@chromium.org>
2020-09-23 16:23:13 +0000

    Cleanup: Use single local for layout provider.
    Bug: <none>
    Change-Id: Idb6a8fe933c0e71ce4a02e7f4ac5de4241367725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424832
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809798}

--- chrome/browser/ui/views/collected_cookies_views.cc
+++ chrome/browser/ui/views/collected_cookies_views.cc
@@ -425,6 +425,8 @@ std::unique_ptr<views::View> CollectedCookiesViews::CreateAllowedPane() {
       content_settings::PageSpecificContentSettings::GetForFrame(
           web_contents_->GetMainFrame());
 
+  ChromeLayoutProvider* layout_provider = ChromeLayoutProvider::Get();
+
   // Create the controls that go into the pane.
   auto allowed_label = std::make_unique<views::Label>(
       l10n_util::GetStringUTF16(IDS_COLLECTED_COOKIES_ALLOWED_COOKIES_LABEL),
@@ -451,10 +453,9 @@ std::unique_ptr<views::View> CollectedCookiesViews::CreateAllowedPane() {
   views::GridLayout* layout =
       pane->SetLayoutManager(std::make_unique<views::GridLayout>());
 
-  pane->SetBorder(
-      views::CreateEmptyBorder(ChromeLayoutProvider::Get()->GetInsetsMetric(
-          views::INSETS_DIALOG_SUBSECTION)));
-  const int vertical_distance = ChromeLayoutProvider::Get()->GetDistanceMetric(
+  pane->SetBorder(views::CreateEmptyBorder(
+      layout_provider->GetInsetsMetric(views::INSETS_DIALOG_SUBSECTION)));
+  const int vertical_distance = layout_provider->GetDistanceMetric(
       views::DISTANCE_UNRELATED_CONTROL_VERTICAL);
 
   const int single_column_layout_id = 0;
