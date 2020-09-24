eaebfb4f7adc30cfab3d5279f4c935e725b3d20e
Yuki Shiino <yukishiino@chromium.org>
2020-09-24 23:44:24 +0000

    base/allocator: Remove an unused header
    base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.h
    is unused.  Removes it.
    
    Change-Id: I26e88508c972b3912849269845ca873e838d3425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428289
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810352}

--- base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.cc
+++ base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.h"
-
 #include <utility>
 
 #include "base/allocator/allocator_interception_mac.h"
