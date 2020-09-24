8f8eee4f47b55bd57cd5c0c960bf59f0c95ffde6
Aya ElAttar <ayaelattar@chromium.org>
2020-09-24 13:59:26 +0000

    Show toast on paste failure
    Changed EnterpriseClipboardDlpController to
    show toast notification when clipboard data
    access is blocked.
    
    Bug: 1096461, 1102332
    Change-Id: I8f3aaf066c04468ba3a1c4716da02bdf878b7beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416514
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810184}

--- chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.cc
+++ chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.cc
@@ -3,15 +3,81 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.h"
-#include "ui/base/clipboard/clipboard.h"
 
-namespace chromeos {
+#include "ash/public/cpp/toast_data.h"
+#include "ash/public/cpp/toast_manager.h"
+#include "base/optional.h"
+#include "base/strings/string16.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/crostini/crostini_util.h"
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/strings/grit/components_strings.h"
+#include "ui/base/clipboard/clipboard.h"
+#include "ui/base/clipboard/clipboard_data_endpoint.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace policy {
+
+namespace {
+
+const char kToastId[] = "clipboard_dlp_block";
+constexpr int kToastDurationMs = 2500;
+
+}  // namespace
 
 bool EnterpriseClipboardDlpController::IsDataReadAllowed(
     const ui::ClipboardDataEndpoint* const data_src,
     const ui::ClipboardDataEndpoint* const data_dst) const {
   // TODO(crbug.com/1102332): all the policy logic should be added later.
-  return true;
+
+  DlpRulesManager::Level level = DlpRulesManager::Level::kAllow;
+
+  if (level == DlpRulesManager::Level::kBlock) {
+    ShowBlockToast(GetToastText(data_dst));
+  }
+
+  return level == DlpRulesManager::Level::kAllow;
 }
 
-}  // namespace chromeos
+void EnterpriseClipboardDlpController::ShowBlockToast(
+    const base::string16& text) const {
+  ash::ToastData toast(kToastId, text, kToastDurationMs,
+                       base::Optional<base::string16>());
+
+  ash::ToastManager::Get()->Show(toast);
+}
+
+base::string16 EnterpriseClipboardDlpController::GetToastText(
+    const ui::ClipboardDataEndpoint* const data_dst) const {
+  if (data_dst && data_dst->type() == ui::EndpointType::kGuestOs) {
+    ProfileManager* profile_manager = g_browser_process->profile_manager();
+    Profile* profile =
+        profile_manager ? profile_manager->GetActiveUserProfile() : nullptr;
+
+    bool is_crostini_running = crostini::IsCrostiniRunning(profile);
+    bool is_plugin_vm_running = plugin_vm::IsPluginVmRunning(profile);
+
+    if (is_crostini_running && is_plugin_vm_running) {
+      return l10n_util::GetStringFUTF16(
+          IDS_POLICY_DLP_CLIPBOARD_BLOCKED_ON_COPY_TWO_VMS,
+          l10n_util::GetStringUTF16(IDS_CROSTINI_LINUX),
+          l10n_util::GetStringUTF16(IDS_PLUGIN_VM_APP_NAME));
+    } else if (is_crostini_running) {
+      return l10n_util::GetStringFUTF16(
+          IDS_POLICY_DLP_CLIPBOARD_BLOCKED_ON_COPY_VM,
+          l10n_util::GetStringUTF16(IDS_CROSTINI_LINUX));
+    } else if (is_plugin_vm_running) {
+      return l10n_util::GetStringFUTF16(
+          IDS_POLICY_DLP_CLIPBOARD_BLOCKED_ON_COPY_VM,
+          l10n_util::GetStringUTF16(IDS_PLUGIN_VM_APP_NAME));
+    } else {
+      NOTREACHED();
+    }
+  }
+  return l10n_util::GetStringUTF16(IDS_POLICY_DLP_CLIPBOARD_BLOCKED_ON_PASTE);
+}
+
+}  // namespace policy
--- chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.h
+++ chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.h
@@ -5,10 +5,14 @@
 #ifndef CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
 #define CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
 
-#include "ui/base/clipboard/clipboard_data_endpoint.h"
+#include "base/strings/string16.h"
 #include "ui/base/clipboard/clipboard_dlp_controller.h"
 
-namespace chromeos {
+namespace ui {
+class ClipboardDataEndpoint;
+}
+
+namespace policy {
 
 // EnterpriseClipboardDlpController is responsible for preventing leaks of
 // confidential clipboard data by controlling read operations according to the
@@ -22,12 +26,23 @@ class EnterpriseClipboardDlpController : public ui::ClipboardDlpController {
       delete;
   void operator=(const EnterpriseClipboardDlpController&) = delete;
 
-  // nullptr can be passed instead of |data_src| or |data_dst|.
+  // nullptr can be passed instead of |data_src| or |data_dst|. If clipboard
+  // data read is not allowed, this function will show a toast to the user.
   bool IsDataReadAllowed(
       const ui::ClipboardDataEndpoint* const data_src,
       const ui::ClipboardDataEndpoint* const data_dst) const override;
+
+ private:
+  // Shows toast in case the access to the clipboard data is blocked.
+  // TODO(crbug.com/1131670): Move `ShowBlockToast` to a separate util/helper.
+  void ShowBlockToast(const base::string16& text) const;
+
+  // The text will be different if the clipboard data is shared with Crostini
+  // or Parallels or both of them.
+  base::string16 GetToastText(
+      const ui::ClipboardDataEndpoint* const data_dst) const;
 };
 
-}  // namespace chromeos
+}  // namespace policy
 
 #endif  // CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
