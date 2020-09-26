111f72a2cfce1532d7cbd1fd1cdbe69b403685c2
Xiaocheng Hu <xiaochengh@chromium.org>
2020-09-26 01:29:42 +0000

    Enable CSSFontLoadingReducedLayoutInvalidations by default on trunk
    Finch experiments results on M85 [1] look good, so this patch enables
    it by default on trunk.
    
    A follow up patch will ship in all channels it via finch.
    
    [1] https://docs.google.com/document/d/1HenoksopNhJKvUfITMnfBPTllPSNcUVH06yCJB3asj4/edit?usp=sharing
    
    Bug: 441925
    Change-Id: Ibd4f308875ff89979af83cf7b9a5e74709b5fba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429514
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810909}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -528,7 +528,7 @@ const base::Feature kDawn2dCanvas{"Dawn2dCanvas",
 
 const base::Feature kCSSReducedFontLoadingLayoutInvalidations{
     "CSSReducedFontLoadingLayoutInvalidations",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // When enabled, frees up CachedMetadata after consumption by script resources
 // and modules. Needed for the experiment in http://crbug.com/1045052.
