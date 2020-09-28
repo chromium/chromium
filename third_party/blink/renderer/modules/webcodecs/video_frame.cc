3a4a9265079c7acce9c1ab2e676c6625eb1b9025
Khushal <khushalsagar@chromium.org>
2020-09-28 22:27:55 +0000

    Revert "media: Ensure sync token synchronization for VideoFrame images."
    This reverts commit 2dc7fd65a44b06cb8ef56c6dd1e365693d188ba8.
    
    Reason for revert: Merged to 86. Was temporary to support merge of a safer fix.
    
    Original change's description:
    > media: Ensure sync token synchronization for VideoFrame images.
    >
    > Instead of relying on the sync token returned by
    > AcceleratedStaticBitmapImage to ensure synchronization with the raster
    > stream used by skia, perform this when the mailbox is returned. This is
    > temporary to allow merging a safer change to 86 release branch.
    >
    > R=​dalecurtis@chromium.org
    >
    > Bug: 1131123
    > Change-Id: I639a0d864fae29ddb32182aeda5ba2e854b1f5ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2433049
    > Commit-Queue: Khushal <khushalsagar@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Auto-Submit: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810936}
    
    TBR=dalecurtis@chromium.org,sunnyps@chromium.org,khushalsagar@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1131123
    Change-Id: Ia49977cb40ab9caea536d7c3dfeab4a97181874a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435782
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811395}

--- third_party/blink/renderer/modules/webcodecs/video_frame.cc
+++ third_party/blink/renderer/modules/webcodecs/video_frame.cc
@@ -401,16 +401,10 @@ ScriptPromise VideoFrame::CreateImageBitmap(ScriptState* script_state,
           ->GenUnverifiedSyncTokenCHROMIUM(sync_token.GetData());
 
       auto release_callback = viz::SingleReleaseCallback::Create(base::BindOnce(
-          [](viz::RasterContextProvider* context, gpu::Mailbox mailbox,
-             const gpu::SyncToken& sync_token, bool is_lost) {
-            auto* ri = context->RasterInterface();
-            auto* sii = context->SharedImageInterface();
-            ri->WaitSyncTokenCHROMIUM(sync_token.GetConstData());
-            gpu::SyncToken ri_sync_token;
-            ri->GenUnverifiedSyncTokenCHROMIUM(ri_sync_token.GetData());
-            sii->DestroySharedImage(ri_sync_token, mailbox);
-          },
-          base::Unretained(raster_context_provider), dest_holder.mailbox));
+          [](gpu::SharedImageInterface* sii, gpu::Mailbox mailbox,
+             const gpu::SyncToken& sync_token,
+             bool is_lost) { sii->DestroySharedImage(sync_token, mailbox); },
+          base::Unretained(shared_image_interface), dest_holder.mailbox));
 
       const SkImageInfo sk_image_info =
           SkImageInfo::Make(codedWidth(), codedHeight(), kN32_SkColorType,
