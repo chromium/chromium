ca51f973508bad66e9a707350831c116967ea472
Alex Rudenko <alexrudenko@chromium.org>
2020-09-28 06:44:07 +0000

    CDP: Add additional tests for DOM.getNodesForSubtreeByStyle for shadow DOM with unslotted elements.
    This CL adds additional tests that help to reproduce the issue that
    was fixed in [1].
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/2426450
    
    Fixed: 1130920
    Change-Id: I45a79f5a88d50ccebe543391e47533511b9ae384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421820
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811130}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-get-nodes-for-subtree-by-style-with-pierce-details.js
@@ -0,0 +1,37 @@
+(async function(testRunner) {
+  const {dp} = await testRunner.startHTML(`
+    <details>
+      <summary>
+        Summary
+        <div class="in-summary" style="display: grid;"></div>
+      </summary>
+      Details
+      <div class="in-details" style="display: grid;"></div>
+    </details>
+    <div class="outside" style="display: grid;"></div>
+    `, 'Tests finding DOM nodes by computed styles on a page containing <details> elements.');
+
+  await dp.DOM.enable();
+  const response = await dp.DOM.getDocument();
+  const rootNodeId = response.result.root.nodeId;
+
+  const nodesResponse = await dp.DOM.getNodesForSubtreeByStyle({
+    nodeId: rootNodeId,
+    pierce: true,
+    computedStyles: [
+      { name: 'display', value: 'grid' },
+    ],
+  });
+
+  testRunner.log('Expected nodeIds length: 3');
+  testRunner.log('Actual nodeIds length: ' + nodesResponse.result.nodeIds.length);
+
+  testRunner.log('Nodes:');
+  for (const nodeId of nodesResponse.result.nodeIds) {
+    const nodeResponse = await dp.DOM.describeNode({ nodeId });
+    testRunner.log(nodeResponse.result);
+  }
+
+  testRunner.completeTest();
+})
+
---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-get-nodes-for-subtree-by-style-with-pierce-unslotted-elements.js
@@ -0,0 +1,57 @@
+(async function(testRunner) {
+  const {dp} = await testRunner.startHTML(`
+    <template id="my-element">
+      <style>
+        slot {
+          display: grid;
+        }
+      </style>
+      <div>
+        <slot class="default-slot" name="my-slot">
+          <span>default element</span>
+        </slot>
+      </div>
+    </template>
+    <script>
+      customElements.define('my-element',
+        class extends HTMLElement {
+          constructor() {
+            super();
+            const template = document.getElementById('my-element');
+            const shadowRoot = this
+              .attachShadow({mode: 'open'})
+              .appendChild(template.content.cloneNode(true));
+          }
+        }
+      );
+    </script>
+    <my-element>
+      <span class="inserted-slot" slot="my-slot" style="display: grid;">custom element</span>
+    </my-element>
+    <div class="grid-outside-shadow-dom" style="display: grid;"></div>
+    `, 'Tests finding DOM nodes by computed styles on a page containing a custom element with unslotted nodes.');
+
+  await dp.DOM.enable();
+  const response = await dp.DOM.getDocument();
+  const rootNodeId = response.result.root.nodeId;
+
+  const nodesResponse = await dp.DOM.getNodesForSubtreeByStyle({
+    nodeId: rootNodeId,
+    pierce: true,
+    computedStyles: [
+      { name: 'display', value: 'grid' },
+    ],
+  });
+
+  testRunner.log('Expected nodeIds length: 3');
+  testRunner.log('Actual nodeIds length: ' + nodesResponse.result.nodeIds.length);
+
+  testRunner.log('Nodes:');
+  for (const nodeId of nodesResponse.result.nodeIds) {
+    const nodeResponse = await dp.DOM.describeNode({ nodeId });
+    testRunner.log(nodeResponse.result);
+  }
+
+  testRunner.completeTest();
+})
+
