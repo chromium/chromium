b4ccd39208a19b445b6e40afcb3dff5df788b7ad
Roman Arora <romanarora@chromium.org>
2020-09-30 12:26:46 -0700

    Tab Search: Use 16dp icons
    Depends on CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2441333
    
    Bug: 1133575
    Change-Id: Id13dfa24226ec87a0940328df4777ed18928877b
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3303663
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819624}

--- chrome/browser/resources/tab_search_merge/tab_search_item.js
+++ chrome/browser/resources/tab_search_merge/tab_search_item.js
@@ -4,7 +4,7 @@
 
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/cr_icons_css.m.js';
-import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/cr_elements/mwb_shared_icons.js';
 import 'chrome://resources/cr_elements/mwb_shared_vars.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 
