1e522f97e4757f55439c7b43f9dcb19d42314bac
Michael Moss <mmoss@chromium.org>
2020-09-30 20:03:26 +0000

    Enable --force signing for Chromoting on mac-arm64.
    This fixes the signing failures like:
    remoting-me2me-host-mac/PrivilegedHelperTools/ChromeRemoteDesktopHost.app/Contents/MacOS//remoting_me2me_host_service: is already signed
    
    BUG=1117247
    
    Change-Id: I184bc8f6c0c0f4476cd174db0a5a693c03fca271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438206
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Michael Moss <mmoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812294}

--- remoting/host/installer/mac/do_signing.sh
+++ remoting/host/installer/mac/do_signing.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -111,6 +111,20 @@ verify_clean_dir() {
   fi
 }
 
+linker_signed_arm64_needs_force() {
+  major=$(sw_vers -productVersion | sed -Ee 's/^([0-9]+)\..*$/\1/')
+  if (( ${major} >= 11 )) ; then
+    return 1
+  fi
+  flags=$(codesign --display --verbose --arch=arm64 -- "${1}" 2>&1 |
+          sed -Ene 's/^CodeDirectory .* flags=(0x[0-9a-f]+)( |\().*$/\1/p')
+  if [[ -z "${flags}" ]]; then
+    return 1
+  fi
+  (( rv=${flags} & 0x20000 ))
+  return ${rv}
+}
+
 sign() {
   local name="${1}"
   local keychain="${2}"
@@ -128,6 +142,9 @@ sign() {
   local args=(-vv --sign "${id}")
   if [[ -n "${keychain}" ]]; then
       args+=(--keychain "${keychain}")
+  fi
+  if linker_signed_arm64_needs_force "${name}"; then
+      args+=(--force)
   fi
   args+=(--timestamp --options runtime "${name}")
   codesign "${args[@]}"
