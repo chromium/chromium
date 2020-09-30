b5205c23057d37a517915d4f9cb456cadaece444
Yuwei Huang <yuweih@chromium.org>
2020-09-30 20:57:45 +0000

    [remoting host] Disable HTTP proxy resolution on Windows 7 hosts
    Per discussion with Chrome network folks, network stack we are using
    appears to have an issue with proxy resolution such that it always
    fails no matter whether the host is using a proxy, and it blocks each
    HTTP request for 10-30 seconds, which is unreliable and sometimes
    triggers timeout error on the caller.
    
    This CL disables proxy resolution on Windows 7 hosts as a workaround,
    given we have never officially supported or tested proxied connections,
    and it will likely fail in later P2P connection stage.
    
    Bug: 1132978
    Change-Id: I9096557105c566075bd2991007fe7d984476bf27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436670
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@google.com>
    Cr-Commit-Position: refs/heads/master@{#812327}

--- remoting/base/url_request_context_getter.cc
+++ remoting/base/url_request_context_getter.cc
@@ -7,11 +7,17 @@
 #include <utility>
 
 #include "base/single_thread_task_runner.h"
+#include "build/build_config.h"
 #include "net/proxy_resolution/configured_proxy_resolution_service.h"
 #include "net/proxy_resolution/proxy_config_service.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
 #include "remoting/base/vlog_net_log.h"
+
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#include "net/log/net_log.h"
+#endif  // defined(OS_WIN)
 
 namespace remoting {
 
@@ -27,7 +33,23 @@ net::URLRequestContext* URLRequestContextGetter::GetURLRequestContext() {
     CreateVlogNetLogObserver();
     net::URLRequestContextBuilder builder;
     builder.DisableHttpCache();
-    builder.set_proxy_config_service(std::move(proxy_config_service_));
+
+#if defined(OS_WIN)
+    if (base::win::GetVersion() <= base::win::Version::WIN7) {
+      // The network stack of Windows 7 and older systems has a bug such that
+      // proxy resolution always fails and blocks each request for ~10-30
+      // seconds. We don't support proxied connection right now, so just disable
+      // it on Windows 7 HTTP requests.
+      auto proxy_resolution_service =
+          net::ConfiguredProxyResolutionService::CreateWithoutProxyResolver(
+              std::move(proxy_config_service_), net::NetLog::Get());
+      builder.set_proxy_resolution_service(std::move(proxy_resolution_service));
+    }
+#endif  // defined(OS_WIN)
+
+    if (proxy_config_service_) {
+      builder.set_proxy_config_service(std::move(proxy_config_service_));
+    }
     url_request_context_ = builder.Build();
   }
   return url_request_context_.get();
