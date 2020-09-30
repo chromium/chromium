481cf8caaf95b5059f671871f563fbe13f0a35cf
Michael Checo <michaelcheco@google.com>
2020-09-30 16:28:13 +0000

    Diagnostics: Support reset on the fake provider
    - Add reset method to fake provider
    - Move method resolvers and observables to class methods
    
    Tests are in a follow up CL
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Ib36194af7511da82991a012325f6a12018d12239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437027
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812121}

--- chromeos/components/diagnostics_ui/resources/fake_system_data_provider.js
+++ chromeos/components/diagnostics_ui/resources/fake_system_data_provider.js
@@ -19,16 +19,8 @@ export class FakeSystemDataProvider {
     /** @private {!FakeObservables} */
     this.observables_ = new FakeObservables();
 
-    // Setup method resolvers.
-    this.methods_.register('getSystemInfo');
-    this.methods_.register('getBatteryInfo');
-
-    // Setup observables.
-    this.observables_.register(
-        'BatteryChargeStatusObserver_onBatteryChargeStatusUpdated');
-    this.observables_.register('BatteryHealthObserver_onBatteryHealthUpdated');
-    this.observables_.register('CpuUsageObserver_onCpuUsageUpdated');
-    this.observables_.register('MemoryUsageObserver_onMemoryUsageUpdated');
+    this.registerMethods();
+    this.registerObservables();
   }
 
   /**
@@ -192,5 +184,37 @@ export class FakeSystemDataProvider {
    */
   stopTriggerIntervals() {
     this.observables_.stopAllTriggerIntervals();
+  }
+
+  /**
+   * Setup method resolvers.
+   */
+  registerMethods() {
+    this.methods_.register('getSystemInfo');
+    this.methods_.register('getBatteryInfo');
+  }
+
+  /**
+   * Setup observables.
+   */
+  registerObservables() {
+    this.observables_.register(
+    'BatteryChargeStatusObserver_onBatteryChargeStatusUpdated');
+    this.observables_.register('BatteryHealthObserver_onBatteryHealthUpdated');
+    this.observables_.register('CpuUsageObserver_onCpuUsageUpdated');
+    this.observables_.register('MemoryUsageObserver_onMemoryUsageUpdated');
+  }
+
+  /**
+   * Disables all observers and resets provider to its initial state.
+   */
+  reset() {
+    this.observables_.stopAllTriggerIntervals();
+
+    this.methods_ = new FakeMethodResolver();
+    this.observables_ = new FakeObservables();
+
+    this.registerMethods();
+    this.registerObservables();
   }
 }
