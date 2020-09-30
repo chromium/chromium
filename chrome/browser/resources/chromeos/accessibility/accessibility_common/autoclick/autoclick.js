2464cf893afb35cc3be77dd45900188e70f9109d
Anastasia Helfinstein <anastasi@google.com>
2020-09-30 22:21:55 +0000

    Use EventHandler in autoclick extension
    This is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: None.
    Change-Id: Ie49ab8152b5fdd7b876dbf07346015c15717740a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391605
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#812398}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/autoclick/autoclick.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/autoclick/autoclick.js
@@ -39,9 +39,11 @@ class Autoclick {
     this.scrollableBoundsListener_ = null;
 
     /**
-     * @private {?function(!chrome.automation.AutomationEvent)}
+     * @private {!EventHandler}
      */
-    this.hitTestListener_ = null;
+    this.hitTestHandler_ = new EventHandler(
+        [], chrome.automation.EventType.MOUSE_PRESSED,
+        this.onAutomationHitTestResult_.bind(this), {capture: true});
 
     this.init_();
   }
@@ -60,12 +62,7 @@ class Autoclick {
       this.scrollableBoundsListener_ = null;
     }
 
-    if (this.desktop_ && this.hitTestListener_) {
-      this.desktop_.removeEventListener(
-          chrome.automation.EventType.MOUSE_PRESSED, this.hitTestListener_,
-          true);
-      this.hitTestListener_ = null;
-    }
+    this.hitTestHandler_.stop();
   }
 
   /**
@@ -78,13 +75,11 @@ class Autoclick {
 
     chrome.automation.getDesktop((desktop) => {
       this.desktop_ = desktop;
-      this.hitTestListener_ = this.onAutomationHitTestResult_.bind(this);
 
       // We use a hit test at a point to determine what automation node is
       // at that point, in order to find the scrollable area.
-      this.desktop_.addEventListener(
-          chrome.automation.EventType.MOUSE_PRESSED, this.hitTestListener_,
-          true);
+      this.hitTestHandler_.setNodes(this.desktop_);
+      this.hitTestHandler_.start();
     });
 
     chrome.accessibilityPrivate.onScrollableBoundsForPointRequested.addListener(
