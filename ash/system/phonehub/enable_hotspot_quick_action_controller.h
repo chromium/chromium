2e1031ec577b81781b3a6cd7a9badce2ca300171
Andre Le <leandre@chromium.org>
2020-10-01 21:22:12 +0000

    [CrOS PhoneHub] Add enable hotspot and locate phone controller to quick actions UI.
    - Add enable hotspot and locate phone quick actions controllers.
    - Connect the controllers to quick actions UI.
    - Minor changes in fake tether controller for testing.
    
    BUG=1106937,1126208
    
    Change-Id: I1d4ace03120a4c51fcb351b7c19f33252b3fb81c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428735
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812915}

---
+++ ash/system/phonehub/enable_hotspot_quick_action_controller.h
@@ -0,0 +1,52 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_PHONEHUB_ENABLE_HOTSPOT_QUICK_ACTION_CONTROLLER_H_
+#define ASH_SYSTEM_PHONEHUB_ENABLE_HOTSPOT_QUICK_ACTION_CONTROLLER_H_
+
+#include "ash/system/phonehub/quick_action_controller_base.h"
+#include "chromeos/components/phonehub/tether_controller.h"
+
+namespace ash {
+
+// Controller of a quick action item that toggles Locate phone mode.
+class EnableHotspotQuickActionController
+    : public QuickActionControllerBase,
+      public chromeos::phonehub::TetherController::Observer {
+ public:
+  explicit EnableHotspotQuickActionController(
+      chromeos::phonehub::TetherController* tether_controller);
+  ~EnableHotspotQuickActionController() override;
+  EnableHotspotQuickActionController(EnableHotspotQuickActionController&) =
+      delete;
+  EnableHotspotQuickActionController operator=(
+      EnableHotspotQuickActionController&) = delete;
+
+  // QuickActionControllerBase:
+  QuickActionItem* CreateItem() override;
+  void OnButtonPressed(bool is_now_enabled) override;
+
+  // chromeos::phonehub::TetherController::Observer:
+  void OnTetherStatusChanged() override;
+
+ private:
+  // All the possible states that the enable hotspot button can be viewed. Each
+  // state has a corresponding icon, labels and tooltip view.
+  enum class ActionState {
+    kNotAvailable,
+    kNotConnected,
+    kConnecting,
+    kConnected
+  };
+
+  // Set the item (including icon, label and tooltips) to a certain state.
+  void SetState(ActionState state);
+
+  chromeos::phonehub::TetherController* tether_controller_ = nullptr;
+  QuickActionItem* item_ = nullptr;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_PHONEHUB_ENABLE_HOTSPOT_QUICK_ACTION_CONTROLLER_H_
