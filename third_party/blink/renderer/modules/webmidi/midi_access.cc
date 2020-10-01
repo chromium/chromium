130b49c4f7546260f246f3680f547442eb6d9c55
cfredric <cfredric@google.com>
2020-10-01 15:41:01 +0000

    Instrument calls to navigator.requestMIDIAccess.
    This will be used to analyze how much entropy leaked by this API can be
    used for cross-site tracking.
    
    This cl does not address entropy leaked by MIDIConnectionEvents. Given
    those events only occur when a device is connected or disconnected, it
    is unlikely that they would be useful to fingerprinting scripts.
    
    Bug: 973801
    Change-Id: I7b3ad5682126dbcbdb2196bdbac0fa985bfee404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432811
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@google.com>
    Cr-Commit-Position: refs/heads/master@{#812717}

--- third_party/blink/renderer/modules/webmidi/midi_access.cc
+++ third_party/blink/renderer/modules/webmidi/midi_access.cc
@@ -31,6 +31,10 @@
 #include "third_party/blink/renderer/modules/webmidi/midi_access.h"
 
 #include <memory>
+#include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_token_builder.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/loader/document_load_timing.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
@@ -42,6 +46,7 @@
 #include "third_party/blink/renderer/modules/webmidi/midi_output_map.h"
 #include "third_party/blink/renderer/modules/webmidi/midi_port.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
+#include "third_party/blink/renderer/platform/privacy_budget/identifiability_digest_helpers.h"
 
 namespace blink {
 
@@ -79,6 +84,22 @@ MIDIAccess::MIDIAccess(
           this, outputs_.size(), port.id, port.manufacturer, port.name,
           port.version, ToDeviceState(port.state)));
     }
+  }
+  constexpr IdentifiableSurface surface = IdentifiableSurface::FromTypeAndToken(
+      IdentifiableSurface::Type::kWebFeature,
+      WebFeature::kRequestMIDIAccess_ObscuredByFootprinting);
+  if (IdentifiabilityStudySettings::Get()->IsSurfaceAllowed(surface)) {
+    IdentifiableTokenBuilder builder;
+    for (const auto& port : ports) {
+      builder.AddToken(IdentifiabilityBenignStringToken(port.id));
+      builder.AddToken(IdentifiabilityBenignStringToken(port.name));
+      builder.AddToken(IdentifiabilityBenignStringToken(port.manufacturer));
+      builder.AddToken(IdentifiabilityBenignStringToken(port.version));
+      builder.AddToken(port.type);
+    }
+    IdentifiabilityMetricBuilder(execution_context->UkmSourceID())
+        .Set(surface, builder.GetToken())
+        .Record(execution_context->UkmRecorder());
   }
 }
 
