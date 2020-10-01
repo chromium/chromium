948d21bc7336a0d1301ebe8ba8d5edcd70e0c93b
cfredric <cfredric@google.com>
2020-10-01 17:44:18 +0000

    Gate canvas metric collection on the study being active.
    Bug: 973801
    Change-Id: I95164a1bfc21f2af9e3377228ffe5ec4b2fbd2c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438470
    Commit-Queue: Chris Fredrickson <cfredric@google.com>
    Auto-Submit: Chris Fredrickson <cfredric@google.com>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812797}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -38,6 +38,8 @@
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metrics.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/modules/v8/rendering_context.h"
@@ -680,12 +682,13 @@ ImageData* CanvasRenderingContext2D::getImageData(
     int sw,
     int sh,
     ExceptionState& exception_state) {
-  blink::IdentifiabilityMetricBuilder(ukm_source_id_)
-      .Set(blink::IdentifiableSurface::FromTypeAndToken(
-               blink::IdentifiableSurface::Type::kCanvasReadback,
-               GetContextType()),
-           0)
-      .Record(ukm_recorder_);
+  const IdentifiableSurface surface = IdentifiableSurface::FromTypeAndToken(
+      IdentifiableSurface::Type::kCanvasReadback, GetContextType());
+  if (IdentifiabilityStudySettings::Get()->IsSurfaceAllowed(surface)) {
+    blink::IdentifiabilityMetricBuilder(ukm_source_id_)
+        .Set(surface, 0)
+        .Record(ukm_recorder_);
+  }
   return BaseRenderingContext2D::getImageData(sx, sy, sw, sh, exception_state);
 }
 
