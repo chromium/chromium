7feca5657c133b847bc3d0068b67c25808c8b79f
Mark Mentovai <mark@chromium.org>
2020-10-02 16:32:48 +0000

    mac-arm64: Include libcodedirectory.dylib in Xcode 12.2b2 package
    There’s no version change, this is still the hermetic Xcode 12.2b2
    toolchain. The new package adds libcodedirectory.dylib, a new library
    that strip and other tools depend on. It needs to be included in the
    hermetic toolchain packages, otherwise, links that request stripping
    will produce errors such as:
    
    [9770/76120] LINK clang_x64/symupload
    FAILED: clang_x64/symupload clang_x64/symupload.dSYM/Contents/Info.plist clang_x64/symupload.dSYM/Contents/Resources/DWARF/symupload
    TOOL_VERSION=1601523428 ../../build/toolchain/mac/linker_driver.py -Wcrl,strippath,../../build/mac_files/xcode_binaries/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip ../../third_party/llvm-build/Release+Asserts/bin/clang++ -B ../../build/mac_files/xcode_binaries/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/  -Wcrl,dsym,clang_x64 -Wcrl,dsymutilpath,../../tools/clang/dsymutil/bin/dsymutil  -Wl,-fatal_warnings -stdlib=libc++ -arch x86_64 -Wl,-dead_strip -Wl,-no_data_in_code_info -Wl,-no_function_starts -nostdlib++ -isysroot ../../build/mac_files/xcode_binaries/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.0.sdk -mmacosx-version-min=10.10.0 -Wl,-ObjC -Wcrl,strip,-x,-S -Werror -o "clang_x64/symupload" -Wl,-filelist,"clang_x64/symupload.rsp" -framework Foundation
    dyld: Library not loaded: @rpath/libcodedirectory.dylib
      Referenced from: /opt/s/w/ir/cache/builder/src/out/Release/../../build/mac_files/xcode_binaries/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip
      Reason: image not found
    Traceback (most recent call last):
      File "../../build/toolchain/mac/linker_driver.py", line 287, in <module>
        Main(sys.argv)
      File "../../build/toolchain/mac/linker_driver.py", line 103, in Main
        linker_driver_outputs += linker_driver_actions[name](args)
      File "../../build/toolchain/mac/linker_driver.py", line 137, in <lambda>
        lambda full_args: action(sub_arg[len(name):], full_args))
      File "../../build/toolchain/mac/linker_driver.py", line 223, in RunStrip
        subprocess.check_call(strip_command)
      File "/opt/s/w/ir/cipd_bin_packages/cpython/lib/python2.7/subprocess.py", line 190, in check_call
        raise CalledProcessError(retcode, cmd)
    subprocess.CalledProcessError: Command '['../../build/mac_files/xcode_binaries/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip', '-x', '-S', 'clang_x64/symupload']' returned non-zero exit status -6
    
    This package was built using the build/xcode_binaries.yaml proposed at
    https://chromium-review.googlesource.com/c/2445909/.
    
    (cherry picked from commit aaa2ab2336c56394ac66f45984ce6f14f695ec45)
    
    Bug: 1134565
    Change-Id: I66e8d1d34d678b3600b5b90b213a382e53133bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2445949
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#813213}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446489
    Reviewed-by: Michael Moss <mmoss@chromium.org>
    Commit-Queue: Michael Moss <mmoss@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#5}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- build/mac_toolchain.py
+++ build/mac_toolchain.py
@@ -36,7 +36,7 @@ MAC_BINARIES_TAG = {
     'default': 'wXywrnOhzFxwLYlwO62UzRxVCjnu6DoSI2D2jrCd00gC',
     # This contains binaries from Xcode 12.2 beta 2, along with the
     # 11 SDK (aka 12B5025f).
-    'xcode_12_beta': '8BjeNPCdtYPajEJ0cPZXG5ATuuBCHPTBfYEuGrdE5KcC',
+    'xcode_12_beta': 'G4U9cOycs9JFzxLNSMaRuoS3NZwiB6RLwg0v4dh3Vw4C',
 }
 
 # The toolchain will not be downloaded if the minimum OS version is not met.
