4d1156e0f019101908baa54875888acaeb2ef8b3
Hidehiko Abe <hidehiko@chromium.org>
2020-10-05 17:27:47 +0000

    Expand OS_LINUX to OS_LINUX || OS_CHROMEOS.
    Currently OS_LINUX is defined on Chrome OS Build,
    but it will be unset. This CL is the preparation to keep
    the current behavior.
    
    Bug: 1110266
    Test: Built locally.
    Change-Id: Ifffd315c669fd8d0d38ab41c8f4f7bbda6c0cbb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450129
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813793}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -53,7 +53,7 @@ namespace {
 std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
     media::VideoCodecProfile profile,
     const media::VideoEncoder::Options& options) {
-#if defined(OS_MAC) || defined(OS_LINUX)
+#if defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS)
   // TODO(https://crbug.com/1110279) Flush() is not implemented on MacOS'
   // accelerated video encoder, so we can't use it yet.
   return nullptr;
@@ -97,7 +97,7 @@ std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
       media::AsyncDestroyVideoEncoder<media::VideoEncodeAcceleratorAdapter>>(
       std::make_unique<media::VideoEncodeAcceleratorAdapter>(
           gpu_factories, std::move(task_runner)));
-#endif  // defined(OS_MAC) || defined(OS_LINUX)
+#endif  // defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS)
 }
 
 std::unique_ptr<media::VideoEncoder> CreateVpxVideoEncoder() {
