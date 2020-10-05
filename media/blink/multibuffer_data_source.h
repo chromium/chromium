bc58153181d6566a111816133b750dc276afc632
Dale Curtis <dalecurtis@chromium.org>
2020-10-05 18:10:13 +0000

    Revert "Add callback to WebMediaPlayerImpl to notify when a redirect occurs"
    This reverts commit 88f263f401b4b2f67d96f74fa2c6ce8119fb78c2.
    
    Reason for revert: Breaks played out audio too.
    
    Original change's description:
    > Add callback to WebMediaPlayerImpl to notify when a redirect occurs
    >
    > (cherry picked from commit 8b18bcfd9aa8096c4551ec34c0225b6017cd211e)
    >
    > Bug: 1128657
    > Change-Id: I9548e1f3bfe5693871a56e23c3373f45147e52e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422091
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#809217}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425223
    > Commit-Queue: Will Cassella <cassew@google.com>
    > Cr-Commit-Position: refs/branch-heads/4240@{#993}
    > Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}
    
    TBR=dalecurtis@chromium.org,guidou@chromium.org,cassew@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1128657
    Change-Id: I0b2264a4078059a4aebd8117446e112565994f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450349
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#1148}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- media/blink/multibuffer_data_source.h
+++ media/blink/multibuffer_data_source.h
@@ -39,7 +39,6 @@ class MultiBufferReader;
 class MEDIA_BLINK_EXPORT MultibufferDataSource : public DataSource {
  public:
   using DownloadingCB = base::RepeatingCallback<void(bool)>;
-  using RedirectCB = base::RepeatingCallback<void()>;
 
   // Used to specify video preload states. They are "hints" to the browser about
   // how aggressively the browser should load and buffer data.
@@ -83,9 +82,6 @@ class MEDIA_BLINK_EXPORT MultibufferDataSource : public DataSource {
   // https://html.spec.whatwg.org/#cors-cross-origin
   // This must be called after the response arrives.
   bool IsCorsCrossOrigin() const;
-
-  // Provides a callback to be run when the underlying url is redirected.
-  void OnRedirect(RedirectCB callback);
 
   // Returns true if the response includes an Access-Control-Allow-Origin
   // header (that is not "null").
@@ -133,7 +129,7 @@ class MEDIA_BLINK_EXPORT MultibufferDataSource : public DataSource {
   bool cancel_on_defer_for_testing() const { return cancel_on_defer_; }
 
  protected:
-  void OnRedirected(const scoped_refptr<UrlData>& new_destination);
+  void OnRedirect(const scoped_refptr<UrlData>& destination);
 
   // A factory method to create a BufferedResourceLoader based on the read
   // parameters.
@@ -247,9 +243,6 @@ class MEDIA_BLINK_EXPORT MultibufferDataSource : public DataSource {
   // As we follow redirects, we set this variable to false if redirects
   // go between different origins.
   bool single_origin_;
-
-  // Callback used when a redirect occurs.
-  RedirectCB redirect_cb_;
 
   // Close the connection when we have enough data.
   bool cancel_on_defer_;
