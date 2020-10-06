e5bab9ba1d5cb7a658d293d69435eb523db13783
Ben Reich <benreich@chromium.org>
2020-10-06 01:02:12 +0000

    [Files App] Remove browser_test prefix and order test BUILD file
    Renames the browser_test_devtools_listener class to remove the
    browser_test prefix and updates the naming in the places that
    refer to it.
    
    Bug: 1113941
    Change-Id: I4fe0827f5c332c8650367bff39eb144725723b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449940
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814027}

--- chrome/browser/chromeos/file_manager/devtools_listener.cc
+++ chrome/browser/chromeos/file_manager/devtools_listener.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/file_manager/browser_test_devtools_listener.h"
+#include "chrome/browser/chromeos/file_manager/devtools_listener.h"
 
 #include <stddef.h>
 #include <map>
--- chrome/browser/chromeos/file_manager/devtools_listener.h
+++ chrome/browser/chromeos/file_manager/devtools_listener.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_FILE_MANAGER_BROWSER_TEST_DEVTOOLS_LISTENER_H_
-#define CHROME_BROWSER_CHROMEOS_FILE_MANAGER_BROWSER_TEST_DEVTOOLS_LISTENER_H_
+#ifndef CHROME_BROWSER_CHROMEOS_FILE_MANAGER_DEVTOOLS_LISTENER_H_
+#define CHROME_BROWSER_CHROMEOS_FILE_MANAGER_DEVTOOLS_LISTENER_H_
 
 #include <map>
 #include <memory>
@@ -91,4 +91,4 @@ class DevToolsListener : public content::DevToolsAgentHostClient {
 
 }  // namespace file_manager
 
-#endif  // CHROME_BROWSER_CHROMEOS_FILE_MANAGER_BROWSER_TEST_DEVTOOLS_LISTENER_H_
+#endif  // CHROME_BROWSER_CHROMEOS_FILE_MANAGER_DEVTOOLS_LISTENER_H_
