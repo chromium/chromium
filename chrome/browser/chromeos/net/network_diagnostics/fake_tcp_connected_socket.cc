a5be5e25293035c90a61f013b5a01f765d5337a9
Kartik Hegde <khegde@chromium.org>
2020-10-06 22:55:39 +0000

    network_diagnostics: Add FakeTCPConnectedSocket class
    Introduce the FakeTCPConnectedSocket resolver class, used by unit tests,
    to avoid duplication across multiple routines.
    
    BUG=chromium:956783
    TEST=none
    
    Change-Id: I1ab4161de6a23790f8b6f8b3bb652701c6ad422b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2448652
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814435}

---
+++ chrome/browser/chromeos/net/network_diagnostics/fake_tcp_connected_socket.cc
@@ -0,0 +1,63 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/net/network_diagnostics/fake_tcp_connected_socket.h"
+
+#include <utility>
+
+#include "base/callback.h"
+#include "base/logging.h"
+#include "base/optional.h"
+#include "mojo/public/cpp/system/data_pipe.h"
+
+FakeTCPConnectedSocket::FakeTCPConnectedSocket() = default;
+
+FakeTCPConnectedSocket::~FakeTCPConnectedSocket() = default;
+
+void FakeTCPConnectedSocket::UpgradeToTLS(
+    const net::HostPortPair& host_port_pair,
+    network::mojom::TLSClientSocketOptionsPtr socket_options,
+    const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
+    mojo::PendingReceiver<network::mojom::TLSClientSocket> receiver,
+    mojo::PendingRemote<network::mojom::SocketObserver> observer,
+    network::mojom::TCPConnectedSocket::UpgradeToTLSCallback callback) {
+  if (disconnect_) {
+    receiver_.reset();
+    return;
+  }
+  // Only during no disconnects will |callback| be invoked.
+  std::move(callback).Run(tls_upgrade_code_,
+                          mojo::ScopedDataPipeConsumerHandle(),
+                          mojo::ScopedDataPipeProducerHandle(),
+                          /*ssl_info=*/base::nullopt);
+}
+
+void FakeTCPConnectedSocket::SetSendBufferSize(
+    int32_t send_buffer_size,
+    SetSendBufferSizeCallback callback) {
+  std::move(callback).Run(0);
+}
+
+void FakeTCPConnectedSocket::SetReceiveBufferSize(
+    int32_t receive_buffer_size,
+    SetReceiveBufferSizeCallback callback) {
+  std::move(callback).Run(0);
+}
+
+void FakeTCPConnectedSocket::SetNoDelay(bool no_delay,
+                                        SetNoDelayCallback callback) {
+  std::move(callback).Run(false);
+}
+void FakeTCPConnectedSocket::SetKeepAlive(bool enable,
+                                          int32_t delay_secs,
+                                          SetKeepAliveCallback callback) {
+  std::move(callback).Run(0);
+}
+
+void FakeTCPConnectedSocket::BindReceiver(
+    mojo::PendingReceiver<network::mojom::TCPConnectedSocket> socket) {
+  DCHECK(!receiver_.is_bound());
+
+  receiver_.Bind(std::move(socket));
+}
