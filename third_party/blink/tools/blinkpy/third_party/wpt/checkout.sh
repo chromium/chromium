44093a32f4e0512833dbf7cb42c7fb2dfc45e279
Luke Zielinski <lpz@chromium.org>
2020-10-06 19:49:04 +0000

    Roll internal WPT tools
    This rolls up to 4b8a64638b4c06fb38eb562b754eae389b381ec0
    
    Assorted changes, including webkitgtk_minobrowser support and mojojs
    improvements
    
    Change-Id: I2a806f1a0040394541fbe52f749f3ab69d3820a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453794
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814352}

--- third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
+++ third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 TARGET_DIR=$DIR/wpt
 REMOTE_REPO="https://github.com/web-platform-tests/wpt.git"
-WPT_HEAD=9f6a7e3cfdae54795a0ca58e194b828537f9659e
+WPT_HEAD=4b8a64638b4c06fb38eb562b754eae389b381ec0
 
 function clone {
   # Remove existing repo if already exists.
