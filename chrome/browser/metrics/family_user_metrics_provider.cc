bb2e7e34be0a7fd5eb9d26a88e8fe57688bcb9e8
Toby Huang <tobyhuang@chromium.org>
2020-10-06 23:14:26 +0000

    [Metrics] Add FamilyUserMetricsProvider for family user types slicing
    The FamilyUserMetricsProvider will filter the Family Experiences
    team's metrics by family user type of interest. The categories
    represent various combinations of supervised and EDU user types as
    primary and secondary accounts.
    
    Bug: 1103077
    Change-Id: Id1f1aab04a17ce7cb42febc42bc1e971033e5e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417451
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814444}

---
+++ chrome/browser/metrics/family_user_metrics_provider.cc
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/metrics/family_user_metrics_provider.h"
+
+#include "base/feature_list.h"
+#include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/browser/chromeos/child_accounts/family_features.h"
+#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
+#include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "components/policy/proto/device_management_backend.pb.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/user_manager/user_manager.h"
+
+namespace {
+
+// Returns user's segment for metrics logging.
+enterprise_management::PolicyData::MetricsLogSegment GetMetricsLogSegment(
+    Profile* profile) {
+  const policy::UserCloudPolicyManagerChromeOS* user_cloud_policy_manager =
+      profile->GetUserCloudPolicyManagerChromeOS();
+  if (!user_cloud_policy_manager)
+    return enterprise_management::PolicyData::UNSPECIFIED;
+  const enterprise_management::PolicyData* policy =
+      user_cloud_policy_manager->core()->store()->policy();
+  if (!policy || !policy->has_metrics_log_segment())
+    return enterprise_management::PolicyData::UNSPECIFIED;
+  return policy->metrics_log_segment();
+}
+
+bool IsLoggedIn() {
+  return user_manager::UserManager::IsInitialized() &&
+         user_manager::UserManager::Get()->IsUserLoggedIn();
+}
+
+bool IsEnterpriseManaged() {
+  policy::BrowserPolicyConnectorChromeOS* connector =
+      g_browser_process->platform_part()->browser_policy_connector_chromeos();
+  return connector->IsEnterpriseManaged();
+}
+
+}  // namespace
+
+// static
+const char FamilyUserMetricsProvider::kFamilyUserLogSegmentHistogramName[] =
+    "ChromeOS.FamilyUser.LogSegment";
+
+FamilyUserMetricsProvider::FamilyUserMetricsProvider() = default;
+
+FamilyUserMetricsProvider::~FamilyUserMetricsProvider() = default;
+
+void FamilyUserMetricsProvider::ProvideCurrentSessionData(
+    metrics::ChromeUserMetricsExtension* uma_proto_unused) {
+  if (!base::FeatureList::IsEnabled(chromeos::kFamilyUserMetricsProvider))
+    return;
+  if (!IsLoggedIn())
+    return;
+  Profile* profile = ProfileManager::GetPrimaryUserProfile();
+  DCHECK(profile);
+  DCHECK(chromeos::ProfileHelper::IsRegularProfile(profile));
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile);
+  auto accounts_size = identity_manager->GetAccountsWithRefreshTokens().size();
+  DCHECK_GT(accounts_size, 0);
+
+  LogSegment log_segment = LogSegment::kOther;
+  if (profile->IsChild() && accounts_size == 1) {
+    log_segment = LogSegment::kSupervisedUser;
+  } else if (profile->IsChild() && accounts_size > 1) {
+    // If a supervised user has a secondary account, then the secondary
+    // account must be EDU.
+    log_segment = LogSegment::kSupervisedStudent;
+  } else if (!IsEnterpriseManaged() &&
+             GetMetricsLogSegment(profile) ==
+                 enterprise_management::PolicyData::K12) {
+    DCHECK(profile->GetProfilePolicyConnector()->IsManaged());
+    // This is a K-12 EDU user on an unmanaged ChromeOS device.
+    log_segment = LogSegment::kStudentAtHome;
+  }
+  base::UmaHistogramEnumeration(kFamilyUserLogSegmentHistogramName,
+                                log_segment);
+}
