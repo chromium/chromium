50016ad6182a59b59e521ea6b0eeb3b3ed5370dd
Clemens Arbesser <arbesser@google.com>
2020-10-06 15:07:49 +0000

    Reland "[Autofill Assistant] Added missing unit tests for new service deps."
    This is a reland of c66038fc7b83aa4e49daad4245f2523caf7c5d9d
    
    Original change's description:
    > [Autofill Assistant] Added missing unit tests for new service deps.
    >
    > Bug: b/158998456
    > Change-Id: Ib580385e201a1b68967dcace71af8cd8f2ed58e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450309
    > Commit-Queue: Clemens Arbesser <arbesser@google.com>
    > Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#814184}
    
    Bug: b/158998456
    Change-Id: Ie318ecb1aba3af9f8ee64d9e727460889b8734c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2452629
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#814211}

---
+++ components/autofill_assistant/browser/service/server_url_fetcher_unittest.cc
@@ -0,0 +1,43 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/service/server_url_fetcher.h"
+
+#include "base/command_line.h"
+#include "components/autofill_assistant/browser/switches.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace autofill_assistant {
+namespace {
+
+using ::testing::Eq;
+
+TEST(ServerUrlFetcherTest, GetDefaultServerUrl) {
+  base::CommandLine::ForCurrentProcess()->RemoveSwitch(
+      switches::kAutofillAssistantUrl);
+  EXPECT_THAT(ServerUrlFetcher::GetDefaultServerUrl(),
+              Eq(GURL("https://automate-pa.googleapis.com")));
+}
+
+TEST(ServerUrlFetcherTest, GetDefaultServerUrlCommandLineOverride) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kAutofillAssistantUrl, "https://www.example.com");
+  EXPECT_THAT(ServerUrlFetcher::GetDefaultServerUrl(),
+              Eq(GURL("https://www.example.com")));
+}
+
+TEST(ServerUrlFetcherTest, GetScriptsEndpoint) {
+  EXPECT_THAT(ServerUrlFetcher(GURL("https://www.example.com"))
+                  .GetSupportsScriptEndpoint(),
+              Eq(GURL("https://www.example.com/v1/supportsSite2")));
+}
+
+TEST(ServerUrlFetcherTest, GetActionsEndpoint) {
+  EXPECT_THAT(ServerUrlFetcher(GURL("https://www.example.com"))
+                  .GetNextActionsEndpoint(),
+              Eq(GURL("https://www.example.com/v1/actions2")));
+}
+
+}  // namespace
+}  // namespace autofill_assistant
