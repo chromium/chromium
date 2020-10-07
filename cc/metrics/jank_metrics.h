2dfeef4bcde56324fe53ae0650cfa153b40c1c09
Mingjing Zhang <mjzhang@chromium.org>
2020-10-07 17:36:14 +0000

    Reland "Exclude no-update frames for jank detection"
    This is a reland of bfeec8b79ee82b29b69236648e1971ff40b77c64.
    
    This CL fixes a use-of-uninitialized-value error in the original
    change.
    
    Original change's description:
    > Exclude no-update frames for jank detection
    >
    >
    > This CL improves the jank metrics by excluding the delays caused by
    > frames without intended updates and prevents certain outlier cases
    > when a high jank percent is reported on a page with very low percent
    > of dropped frames.
    >
    > Bug: 1133058
    > Change-Id: If32576e8b528e22b184a0b549161751fcfd976bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417211
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#812988}
    
    (cherry picked from commit 0241cc426322fc74d718fb843c83a5f514da9857)
    
    Bug: 1133058
    Change-Id: I270ca4ad044468e19136d3ed544c25de8ec3c916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446530
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#813377}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456293
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#82}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- cc/metrics/jank_metrics.h
+++ cc/metrics/jank_metrics.h
@@ -6,6 +6,8 @@
 #define CC_METRICS_JANK_METRICS_H_
 
 #include <memory>
+#include <queue>
+#include <utility>
 
 #include "cc/metrics/frame_sequence_metrics.h"
 
@@ -21,7 +23,8 @@ class CC_EXPORT JankMetrics {
 
   // Check if a jank occurs based on the timestamps of recent presentations.
   // If there is a jank, increment |jank_count_| and log a trace event.
-  void AddPresentedFrame(base::TimeTicks current_presentation_timestamp,
+  void AddPresentedFrame(uint32_t presented_frame_token,
+                         base::TimeTicks current_presentation_timestamp,
                          base::TimeDelta frame_interval);
 
   // Report the occurrence rate of janks as a UMA metric.
@@ -30,6 +33,10 @@ class CC_EXPORT JankMetrics {
   // Merge the current jank count with a previously unreported jank metrics.
   void Merge(std::unique_ptr<JankMetrics> jank_metrics);
 
+  void AddSubmitFrame(uint32_t frame_token, uint32_t sequence_number);
+
+  void AddFrameWithNoUpdate(uint32_t sequence_number,
+                            base::TimeDelta frame_interval);
   FrameSequenceMetrics::ThreadType thread_type() const {
     return effective_thread_;
   }
@@ -48,8 +55,19 @@ class CC_EXPORT JankMetrics {
   // The time when the last presentation occurs
   base::TimeTicks last_presentation_timestamp_;
 
+  // The sequence number associated with the last presented frame
+  uint32_t last_presentation_frame_id_ = 0u;
+
   // The interval before the previous frame presentation.
   base::TimeDelta prev_frame_delta_;
+
+  // A queue storing {frame token, sequence number} for all submitted
+  // frames, in ascending order of frame token.
+  std::queue<std::pair<uint32_t, uint32_t>> queue_frame_token_and_id_;
+
+  // A queue storing {sequence number, frame interval} of unprocessed no-update
+  // frames, in ascending order of sequence number.
+  std::queue<std::pair<uint32_t, base::TimeDelta>> queue_frame_id_and_interval_;
 };
 
 }  // namespace cc
