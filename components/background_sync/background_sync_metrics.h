79309c7885238c9861d1f0d8ef5bd2a7df108bcd
Mugdha Lakhani <nator@chromium.org>
2020-10-07 08:57:06 +0000

    [BackgroundSync] Componentize BackgroundSyncMetrics.
    Pre-work for this has been done in:
    https://chromium-review.googlesource.com/c/chromium/src/+/2443089
    
    This moves BackgroundSyncMetrics to components and updates references.
    
    Bug: 1087486
    Change-Id: I175de6d5ff224ba2e9f590234b98bd4a2de952e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2445874
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814598}

--- components/background_sync/background_sync_metrics.h
+++ components/background_sync/background_sync_metrics.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
-#define CHROME_BROWSER_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
+#ifndef COMPONENTS_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
+#define COMPONENTS_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
 
 #include "base/callback.h"
 #include "base/macros.h"
@@ -87,4 +87,4 @@ class BackgroundSyncMetrics {
   DISALLOW_COPY_AND_ASSIGN(BackgroundSyncMetrics);
 };
 
-#endif  // CHROME_BROWSER_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
+#endif  // COMPONENTS_BACKGROUND_SYNC_BACKGROUND_SYNC_METRICS_H_
