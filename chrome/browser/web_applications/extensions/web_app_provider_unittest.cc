267e25885b98ccdd2c2573cf4747b923b4ed648a
Mike Jackson <mjackson@microsoft.com>
2020-10-07 06:41:40 +0000

    dpwa: Remove parameterization for WebApp Provider Unit Test
    Removes the parameterization for all tests in the
    web_app_provider_unittest.cc file, now that
    kDesktopPWAsWithoutExtensions flag is always true.
    
    Bug: 1065748
    Change-Id: I1bebc1de80fc8cdb3f93d49f07acd938f5acfb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453352
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814570}

--- chrome/browser/web_applications/extensions/web_app_provider_unittest.cc
+++ chrome/browser/web_applications/extensions/web_app_provider_unittest.cc
@@ -4,27 +4,15 @@
 
 #include "chrome/browser/web_applications/web_app_provider.h"
 
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/testing_profile.h"
 
 namespace web_app {
 
-class WebAppProviderUnitTest
-    : public WebAppTest,
-      public ::testing::WithParamInterface<ProviderType> {
+class WebAppProviderUnitTest : public WebAppTest {
  public:
-  WebAppProviderUnitTest() {
-    if (GetParam() == ProviderType::kWebApps) {
-      scoped_feature_list_.InitAndEnableFeature(
-          features::kDesktopPWAsWithoutExtensions);
-    } else if (GetParam() == ProviderType::kBookmarkApps) {
-      scoped_feature_list_.InitAndDisableFeature(
-          features::kDesktopPWAsWithoutExtensions);
-    }
-  }
+  WebAppProviderUnitTest() = default;
   WebAppProviderUnitTest(const WebAppProviderUnitTest&) = delete;
   WebAppProviderUnitTest& operator=(const WebAppProviderUnitTest&) = delete;
   ~WebAppProviderUnitTest() override = default;
@@ -37,19 +25,12 @@ class WebAppProviderUnitTest
   WebAppProvider* provider() { return provider_; }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
   WebAppProvider* provider_;
-
 };
 
-TEST_P(WebAppProviderUnitTest, Registrar) {
+TEST_F(WebAppProviderUnitTest, Registrar) {
   AppRegistrar& registrar = provider()->registrar();
   EXPECT_FALSE(registrar.IsInstalled("unknown"));
 }
-
-INSTANTIATE_TEST_SUITE_P(All,
-                         WebAppProviderUnitTest,
-                         ::testing::ValuesIn({ProviderType::kBookmarkApps,
-                                              ProviderType::kWebApps}));
 
 }  // namespace web_app
