d687ea661de8576b01fe49f30e04cf0c7d75685d
Joe Mason <joenotcharles@google.com>
2020-10-07 18:46:03 +0000

    Update import_util.py to work from chrome/chrome_cleaner/internal
    This prepares for a pending commit to the internal chrome_cleaner repo
    which will delete the internal copy of import_util.py:
    
    * Add separate GetChromiumRootDirectory and GetInternalRootDirectory
    functions to clarify the ambiguous term "root directory" when called
    from the internal repo mounted at //chrome/chrome_cleaner/internal.
    
    * Move all the functions that depend on being called from inside the
    chromium source tree to a separate import_paths.py script. (All the
    current callers of these functions happen to be in
    //chrome/chrome_cleaner/internal.)
    
    * Delete code to find a local copy of find_depot_tools. It's no longer
    supported to run this script without calling gclient sync, which will
    update depot_tools.
    
    R=​proberge
    
    (cherry picked from commit 1673a55ce9dfd635d021fd5b238f8b147d3ccc70)
    
    Bug: 1004848
    Change-Id: I829851f332975c950f1dcde59735ee96f34f295e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451810
    Reviewed-by: proberge <proberge@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#814390}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2457551
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#1172}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- chrome/chrome_cleaner/tools/import_util.py
+++ chrome/chrome_cleaner/tools/import_util.py
@@ -1,38 +1,15 @@
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Utilities for importing python proto modules."""
+"""Utilities for importing python scripts and proto modules."""
 
 import os
 import sys
 
 
-_CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-_ROOT_DIRECTORY = os.path.join(_CURRENT_DIRECTORY, os.pardir, os.pardir)
-
-
-def _InSourceTree():
-  return os.path.basename(os.path.abspath(_ROOT_DIRECTORY)) == 'src'
-
-
 def AddImportPath(path):
   """Adds the given path to the front of the Python import path."""
   sys.path.insert(0, os.path.normpath(path))
-
-
-def GetBuildDirectory(build_configuration):
-  """Returns the path to the build directory relative to this file.
-
-  Args:
-    build_configuration: name of the build configuration whose directory
-        should be looked up, e.g. 'Debug' or 'Release'.
-
-  Returns:
-    Path to the build directory or None if used from outside the source tree.
-  """
-  if not _InSourceTree():
-    return None
-  return os.path.join(_ROOT_DIRECTORY, 'out', build_configuration)
 
 
 def AddProtosToPath(root_path):
@@ -55,24 +32,3 @@ def AddProtosToPath(root_path):
   AddImportPath(os.path.join(pyproto_dir, 'chrome', 'chrome_cleaner', 'proto'))
   AddImportPath(os.path.join(pyproto_dir, 'chrome', 'chrome_cleaner', 'logging',
                              'proto'))
-
-
-def AddDepotToolsToPath():
-  """Locates a depot_tools checkout and adds it to the Python import path.
-
-  Returns:
-    The path to depot_tools.
-  """
-
-  # Try to import the local copy of find_depot_tools from the import subdir, in
-  # case this is being run from a checkout that hasn't been synced.
-  AddImportPath(os.path.join(_CURRENT_DIRECTORY, 'import'))
-
-  # But prefer to use the copy synced into the build dir which might be more
-  # up-to-date. (This path will take priority over the path added above.)
-  if _InSourceTree():
-    AddImportPath(os.path.join(_ROOT_DIRECTORY, 'build'))
-
-  # Import the first copy of find_depot_tools found in the above paths.
-  import find_depot_tools
-  return find_depot_tools.add_depot_tools_to_path()
