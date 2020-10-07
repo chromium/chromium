ab12c6e39401f3837893b09005c5b10d3c7679c9
Wei Lee <wtlee@google.com>
2020-10-07 05:49:35 +0000

    [CCA WebUI] Don't capture navigation other than main page URL for CCA
    We have the following requirements:
    1. We prefer loading CCA UI in the app window rather than browser tab
      => Therefore we added "capture_navigation=true" for CCA
    2. We don't want to load resource URLs or invalid URL in the app window
      => Short term: Landing this CL to filter out unwanted URLs
    
      We decide not to support URL filtering feature for capture navigation
      since only camera app has such requirement at this moment.
      (crbug.com/1132151)
    
    Bug: 980846
    Test: Type CCA url other than chrome://camera-app/views/main.html and
    the page is loaded in browser tab
    
    Change-Id: I467aa1498aa9fe975d34bdf82adee7f6dff6a14c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440076
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814557}

--- chrome/browser/chromeos/web_applications/camera_app_integration_browsertest.cc
+++ chrome/browser/chromeos/web_applications/camera_app_integration_browsertest.cc
@@ -5,10 +5,13 @@
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/chromeos/web_applications/system_web_app_integration_test.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/web_applications/system_web_app_manager_browsertest.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/test_navigation_observer.h"
 
 class CameraAppIntegrationTest : public SystemWebAppIntegrationTest {
  public:
@@ -30,6 +33,43 @@ IN_PROC_BROWSER_TEST_P(CameraAppIntegrationTest, WindowNotResizeable) {
   BrowserView* const browser_view =
       BrowserView::GetBrowserViewForBrowser(browser);
   EXPECT_FALSE(browser_view->CanResize());
+}
+
+IN_PROC_BROWSER_TEST_P(CameraAppIntegrationTest, MainUrlNavigation) {
+  WaitForTestSystemAppInstall();
+
+  GURL main_camera_app_url("chrome://camera-app/views/main.html");
+  content::TestNavigationObserver navigation_observer(main_camera_app_url);
+  navigation_observer.StartWatchingNewWebContents();
+  ASSERT_EQ(1u, chrome::GetTotalBrowserCount());
+
+  ui_test_utils::SendToOmniboxAndSubmit(browser(), main_camera_app_url.spec());
+  navigation_observer.Wait();
+
+  // We now have two browsers, one for the chrome window, one for the Camera
+  // app.
+  EXPECT_EQ(2u, chrome::GetTotalBrowserCount());
+  EXPECT_EQ(main_camera_app_url, chrome::FindLastActive()
+                                     ->tab_strip_model()
+                                     ->GetActiveWebContents()
+                                     ->GetVisibleURL());
+}
+
+IN_PROC_BROWSER_TEST_P(CameraAppIntegrationTest, OtherPageUrlNavigation) {
+  WaitForTestSystemAppInstall();
+
+  // TODO(crbug.com/980846): Change it to test page once the corresponding CL is
+  // merged.
+  GURL other_page_camera_app_url("chrome://camera-app/js/main.js");
+  ASSERT_EQ(1u, chrome::GetTotalBrowserCount());
+
+  ui_test_utils::SendToOmniboxAndSubmit(browser(),
+                                        other_page_camera_app_url.spec());
+
+  EXPECT_EQ(1u, chrome::GetTotalBrowserCount());
+  EXPECT_EQ(
+      other_page_camera_app_url,
+      browser()->tab_strip_model()->GetActiveWebContents()->GetVisibleURL());
 }
 
 INSTANTIATE_SYSTEM_WEB_APP_MANAGER_TEST_SUITE_MANIFEST_INSTALL_P(
