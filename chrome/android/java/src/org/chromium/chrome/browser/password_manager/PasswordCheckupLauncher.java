a7f8f4defd55650243075473756ca0c0d7d8b7a6
Brandon Wylie <wylieb@chromium.org>
2020-10-07 00:17:59 +0000

    Remove ChromeActivity reference from PasswordCheckedLauncher
    Only used like a regular Android activity.
    
    Bug: 1123209
    Change-Id: I3d68e4a3b9e984fa565249c40f03ea239c850b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453797
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814468}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordCheckupLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordCheckupLauncher.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.AppHooks;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.password_check.PasswordCheckFactory;
 import org.chromium.chrome.browser.password_check.PasswordCheckReferrer;
@@ -23,8 +22,7 @@ public class PasswordCheckupLauncher {
     private static void launchCheckupInAccountWithWindowAndroid(
             String checkupUrl, WindowAndroid windowAndroid) {
         if (windowAndroid.getContext().get() == null) return; // Window not available yet/anymore.
-        ChromeActivity activity = (ChromeActivity) windowAndroid.getActivity().get();
-        launchCheckupInAccountWithActivity(checkupUrl, activity);
+        launchCheckupInAccountWithActivity(checkupUrl, windowAndroid.getActivity().get());
     }
 
     @CalledByNative
