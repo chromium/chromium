caf8c0d9110c6dfaf2eae77426b6dd106f7ea88b
Clark DuVall <cduvall@chromium.org>
2020-10-08 19:03:26 +0000

    Convert ChromeMinidumpUploadJobService to split compat
    Review from Patchset 1 to get the right diffs on the changes in
    ChromeMinidumpUploadJobServiceImpl.
    
    Bug: 1126301
    Change-Id: Id583822c3bfddba0bf594b6a0eb2805a8d788819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461983
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815260}

--- chrome/android/java/src/org/chromium/chrome/browser/crash/ChromeMinidumpUploadJobService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/ChromeMinidumpUploadJobService.java
@@ -1,23 +1,16 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 package org.chromium.chrome.browser.crash;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.os.PersistableBundle;
+import org.chromium.chrome.browser.base.SplitCompatMinidumpUploadJobService;
+import org.chromium.chrome.browser.base.SplitCompatUtils;
 
-import org.chromium.components.minidump_uploader.MinidumpUploadJob;
-import org.chromium.components.minidump_uploader.MinidumpUploadJobImpl;
-import org.chromium.components.minidump_uploader.MinidumpUploadJobService;
-
-/**
- * Class that interacts with the Android JobScheduler to upload minidumps at appropriate times.
- */
-@TargetApi(Build.VERSION_CODES.M)
-public class ChromeMinidumpUploadJobService extends MinidumpUploadJobService {
-    @Override
-    protected MinidumpUploadJob createMinidumpUploadJob(PersistableBundle permissions) {
-        return new MinidumpUploadJobImpl(new ChromeMinidumpUploaderDelegate(this, permissions));
+/** See {@link ChromeMinidumpUploadJobServiceImpl}. */
+public class ChromeMinidumpUploadJobService extends SplitCompatMinidumpUploadJobService {
+    public ChromeMinidumpUploadJobService() {
+        super(SplitCompatUtils.getIdentifierName(
+                "org.chromium.chrome.browser.crash.ChromeMinidumpUploadJobServiceImpl"));
     }
 }
---
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/ChromeMinidumpUploadJobServiceImpl.java
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.crash;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.PersistableBundle;
+
+import org.chromium.components.minidump_uploader.MinidumpUploadJob;
+import org.chromium.components.minidump_uploader.MinidumpUploadJobImpl;
+
+/**
+ * Class that interacts with the Android JobScheduler to upload minidumps at appropriate times.
+ */
+@TargetApi(Build.VERSION_CODES.M)
+public class ChromeMinidumpUploadJobServiceImpl extends ChromeMinidumpUploadJobService.Impl {
+    @Override
+    protected MinidumpUploadJob createMinidumpUploadJob(PersistableBundle permissions) {
+        return new MinidumpUploadJobImpl(
+                new ChromeMinidumpUploaderDelegate(getService(), permissions));
+    }
+}
