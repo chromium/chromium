d79672b41635a9d5f79d7d78e17c7e95c3c3b978
harrisonsean <harrisonsean@chromium.org>
2020-10-08 11:07:00 +0000

    (Merge): [iOS][Safety Check] Add mediator tests
    Added Tests:
    StartingCheckPutsChecksInRunningState
    CheckStartButtonDefaultUI
    ClickingCancelTakesChecksToPrevious
    CheckStartButtonCancelUI
    TimestampSetIfIssueFound
    SafeBrowsingEnabledReturnsSafeState
    SafeBrowsingSafeUI
    SafeBrowsingDisabledReturnsInfoState
    SafeBrowsingUnSafeUI
    SafeBrowsingManagedUI
    PasswordCheckSafeCheck
    PasswordCheckSafeUI
    PasswordCheckUnSafeCheck
    PasswordCheckUnSafeUI
    PasswordCheckErrorCheck
    PasswordCheckErrorUI
    OmahaRespondsUpToDate
    UpdateCheckUpToDateUI
    OmahaRespondsOutOfDateAndUpdatesInfobarTime
    UpdateCheckOutOfDateUI
    OmahaRespondsError
    UpdateCheckOmahaErrorUI
    UpdateCheckNetErrorUI
    UpdateCheckManagedUI
    UpdateCheckChannelUI
    
    (cherry picked from commit d7b3298d8b60ef8001d5da4a9f6021dcee727991)
    
    Bug: 1078782, 1135128
    Change-Id: I61d155817bc13b96fb581fa4a77426ac56deae6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436530
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#813001}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454074
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#138}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- ios/chrome/browser/ui/settings/safety_check/safety_check_constants.h
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_constants.h
@@ -29,4 +29,62 @@ extern NSString* const kSafeBrowsingSafetyCheckStringURL;
 // issue.
 extern NSString* const kTimestampOfLastIssueFoundKey;
 
+// Enum with all possible states of the update check.
+typedef NS_ENUM(NSInteger, UpdateCheckRowStates) {
+  // When the user is up to date.
+  UpdateCheckRowStateUpToDate,
+  // When the check has not been run yet.
+  UpdateCheckRowStateDefault,
+  // When the user is out of date.
+  UpdateCheckRowStateOutOfDate,
+  // When the user is managed.
+  UpdateCheckRowStateManaged,
+  // When the check is running.
+  UpdateCheckRowStateRunning,
+  // When Omaha encountered an error.
+  UpdateCheckRowStateOmahaError,
+  // When there is a connectivity issue.
+  UpdateCheckRowStateNetError,
+  // When the device is on a non-supported channel.
+  UpdateCheckRowStateChannel,
+};
+
+// Enum with all possible states of the password check.
+typedef NS_ENUM(NSInteger, PasswordCheckRowStates) {
+  // When no compromised passwords were detected.
+  PasswordCheckRowStateSafe,
+  // When user has compromised passwords.
+  PasswordCheckRowStateUnSafe,
+  // When check has not been run yet.
+  PasswordCheckRowStateDefault,
+  // When password check is running.
+  PasswordCheckRowStateRunning,
+  // When user has no passwords and check can't be performed.
+  PasswordCheckRowStateDisabled,
+  // When password check failed due to network issues, quota limit or others.
+  PasswordCheckRowStateError,
+};
+
+// Enum with all possible states of the Safe Browsing check.
+typedef NS_ENUM(NSInteger, SafeBrowsingCheckRowStates) {
+  // When check was not run yet.
+  SafeBrowsingCheckRowStateDefault,
+  // When Safe Browsing is managed by admin.
+  SafeBrowsingCheckRowStateManaged,
+  // When the Safe Browsing check is running.
+  SafeBrowsingCheckRowStateRunning,
+  // When Safe Browsing is enabled.
+  SafeBrowsingCheckRowStateSafe,
+  // When Safe Browsing is disabled.
+  SafeBrowsingCheckRowStateUnsafe,
+};
+
+// Enum with all possible states of the button to start the check.
+typedef NS_ENUM(NSInteger, CheckStartStates) {
+  // When the check is not running.
+  CheckStartStateDefault,
+  // When the check is running.
+  CheckStartStateCancel,
+};
+
 #endif  // IOS_CHROME_BROWSER_UI_SETTINGS_SAFETY_CHECK_SAFETY_CHECK_CONSTANTS_H_
