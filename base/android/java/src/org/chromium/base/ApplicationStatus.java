57ee92af6772083d0ff23d50fbe9219414b30147
Alexander Cooper <alcooper@chromium.org>
2020-10-08 18:36:22 +0000

    Add warning to ApplicationStatus about usage
    Adds a warning that ApplicationStatus may not work properly in WebView
    or WebLayer, and that it is intended to be moved out of //base. While
    this is enforced by DEPS (at least in //components), that isn't as
    discoverable until the end of prepping a patch. This adds a class-level
    comment to provide better visibility to warning users off of using this
    if they are trying to break other //chrome dependencies.
    
    Bug: 470582
    Change-Id: I3593a44b1075b51ab99a114074b8a54c80e916f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461333
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815252}

--- base/android/java/src/org/chromium/base/ApplicationStatus.java
+++ base/android/java/src/org/chromium/base/ApplicationStatus.java
@@ -36,6 +36,8 @@ import javax.annotation.concurrent.GuardedBy;
 /**
  * Provides information about the current activity's status, and a way
  * to register / unregister listeners for state changes.
+ * TODO(https://crbug.com/470582): ApplicationStatus will not work on WebView/WebLayer, and
+ * should be moved out of base and into //chrome. It should not be relied upon for //components.
  */
 @JNINamespace("base::android")
 public class ApplicationStatus {
