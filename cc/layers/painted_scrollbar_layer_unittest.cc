a383d2923ba0f69d58ddb6092c6238b7a2914244
Xianzhu Wang <wangxianzhu@chromium.org>
2020-10-08 18:12:06 +0000

    Reland "Limit minimum scale of PaintedScrollbarLayer to 1"
    This reverts commit 1745f190411fddd876cf21992e3ae22fed86373e.
    
    Reason for revert: Rebaseline the failing test.
    
    Original change's description:
    > Revert "Limit minimum scale of PaintedScrollbarLayer to 1"
    >
    > This reverts commit 6860e109ba86e794f021f8d6d10e66f3473d2680.
    >
    > Reason for revert: Speculative revert to fix fast/frames/iframe-scaling-with-scroll.html. https://crbug.com/1136335
    >
    > Original change's description:
    > > Limit minimum scale of PaintedScrollbarLayer to 1
    > >
    > > This is to avoid too low scale when animating scale above the
    > > scrollbar layer.
    > >
    > > This is not ideal. In the future we should move rasterization of
    > > scrollbar layers to impl-side to better handle scales
    > > (crbug.com/1009291).
    > >
    > > Bug: 1133997
    > > Change-Id: I3364754f530914816fe8526c627e6cf12e7942ec
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453436
    > > Reviewed-by: David Bokan <bokan@chromium.org>
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#814681}
    >
    > TBR=wangxianzhu@chromium.org,bokan@chromium.org
    >
    > Change-Id: I4554c428bcc5a3d5fef9033e6772f53932d0e256
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1133997, 1136335
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459087
    > Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    > Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#814987}
    
    TBR=wangxianzhu@chromium.org,bokan@chromium.org,shimazu@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1133997
    Bug: 1136335
    Change-Id: I7719f62f9b23f31f9c35094ac3c1de2a73dfdef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459137
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815240}

--- cc/layers/painted_scrollbar_layer_unittest.cc
+++ cc/layers/painted_scrollbar_layer_unittest.cc
@@ -4,9 +4,12 @@
 
 #include "cc/layers/painted_scrollbar_layer.h"
 
+#include <memory>
+
 #include "cc/animation/animation_host.h"
 #include "cc/test/fake_layer_tree_host.h"
 #include "cc/test/fake_layer_tree_host_client.h"
+#include "cc/test/fake_painted_scrollbar_layer.h"
 #include "cc/test/fake_scrollbar.h"
 #include "cc/test/layer_test_common.h"
 #include "cc/test/test_task_graph_runner.h"
@@ -18,6 +21,20 @@ using ::testing::_;
 namespace cc {
 
 namespace {
+
+class PaintedScrollbarLayerTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    animation_host_ = AnimationHost::CreateForTesting(ThreadInstance::MAIN);
+    layer_tree_host_ = FakeLayerTreeHost::Create(
+        &fake_client_, &task_graph_runner_, animation_host_.get());
+  }
+
+  FakeLayerTreeHostClient fake_client_;
+  TestTaskGraphRunner task_graph_runner_;
+  std::unique_ptr<AnimationHost> animation_host_;
+  std::unique_ptr<FakeLayerTreeHost> layer_tree_host_;
+};
 
 class MockScrollbar : public FakeScrollbar {
  public:
@@ -35,14 +52,7 @@ class MockScrollbar : public FakeScrollbar {
   ~MockScrollbar() override = default;
 };
 
-TEST(PaintedScrollbarLayerTest, NeedsPaint) {
-  FakeLayerTreeHostClient fake_client_;
-  TestTaskGraphRunner task_graph_runner_;
-
-  auto animation_host = AnimationHost::CreateForTesting(ThreadInstance::MAIN);
-  auto layer_tree_host = FakeLayerTreeHost::Create(
-      &fake_client_, &task_graph_runner_, animation_host.get());
-
+TEST_F(PaintedScrollbarLayerTest, NeedsPaint) {
   auto scrollbar = base::MakeRefCounted<MockScrollbar>();
   scoped_refptr<PaintedScrollbarLayer> scrollbar_layer =
       PaintedScrollbarLayer::Create(scrollbar);
@@ -50,10 +60,10 @@ TEST(PaintedScrollbarLayerTest, NeedsPaint) {
   scrollbar_layer->SetIsDrawable(true);
   scrollbar_layer->SetBounds(gfx::Size(100, 100));
 
-  layer_tree_host->SetRootLayer(scrollbar_layer);
-  UpdateDrawProperties(layer_tree_host.get());
+  layer_tree_host_->SetRootLayer(scrollbar_layer);
+  UpdateDrawProperties(layer_tree_host_.get());
 
-  EXPECT_EQ(scrollbar_layer->layer_tree_host(), layer_tree_host.get());
+  EXPECT_EQ(scrollbar_layer->layer_tree_host(), layer_tree_host_.get());
 
   // Request no paint, but expect them to be painted because they have not
   // yet been initialized.
@@ -93,6 +103,33 @@ TEST(PaintedScrollbarLayerTest, NeedsPaint) {
   scrollbar->set_needs_repaint_track(true);
   scrollbar_layer->Update();
   Mock::VerifyAndClearExpectations(scrollbar.get());
+}
+
+TEST_F(PaintedScrollbarLayerTest, InternalContentBounds) {
+  auto scrollbar = base::MakeRefCounted<FakeScrollbar>();
+  auto scrollbar_layer = PaintedScrollbarLayer::Create(scrollbar);
+  scrollbar_layer->SetIsDrawable(true);
+  scrollbar_layer->SetBounds(gfx::Size(10, 100));
+
+  layer_tree_host_->SetRootLayer(scrollbar_layer);
+  UpdateDrawProperties(layer_tree_host_.get());
+  EXPECT_EQ(scrollbar_layer->layer_tree_host(), layer_tree_host_.get());
+  scrollbar_layer->Update();
+  EXPECT_EQ(gfx::Size(10, 100), scrollbar_layer->internal_content_bounds());
+
+  layer_tree_host_->SetViewportRectAndScale(
+      layer_tree_host_->device_viewport_rect(), 2.0f,
+      layer_tree_host_->local_surface_id_from_parent());
+  UpdateDrawProperties(layer_tree_host_.get());
+  scrollbar_layer->Update();
+  EXPECT_EQ(gfx::Size(20, 200), scrollbar_layer->internal_content_bounds());
+
+  layer_tree_host_->SetViewportRectAndScale(
+      layer_tree_host_->device_viewport_rect(), 0.1f,
+      layer_tree_host_->local_surface_id_from_parent());
+  UpdateDrawProperties(layer_tree_host_.get());
+  scrollbar_layer->Update();
+  EXPECT_EQ(gfx::Size(10, 100), scrollbar_layer->internal_content_bounds());
 }
 
 }  // namespace
