6d04014c119e5d231f44d78852e21417235c35f5
Brian Salomon <bsalomon@google.com>
2020-10-09 21:35:29 +0000

    Use updated Skia GPU blur code with various tile mode fixes.
    Bug: skia:10818
    
    Change-Id: Iefc3a686b686da4b1287fa97c7684b541b2cfea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456291
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Cr-Commit-Position: refs/heads/master@{#815798}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -212,12 +212,6 @@ SK_API void SkDebugf_FileLine(const char* file,
 #define SK_DISABLE_REDUCE_OPLIST_SPLITTING
 #endif
 
-// Many layout tests and unit tests need to updated/rebased to move to less
-// buggy GPU blur.
-#ifndef SK_USE_LEGACY_GPU_BLUR
-#define SK_USE_LEGACY_GPU_BLUR
-#endif
-
 // Max. verb count for paths rendered by the edge-AA tessellating path renderer.
 #define GR_AA_TESSELLATOR_MAX_VERB_COUNT 100
 
