023b0f79c66458bad6e540aaf90fb234644311a5
Viktor Semeniuk <vsemeniuk@google.com>
2020-10-10 11:28:01 +0000

    [iOS][Password Check] Rewriting Passwords test with new details screen
    Reland of https://chromium-review.googlesource.com/c/chromium/src/+/2450409
    In this CL removed changes to PasswordViewControllerTestCase which
    caused flakiness.
    
    This change rewrites Passwords tests with new Password Details screen.
    Obsolete tests are deleted. Fixed flakiness for
    testSearchAndDeleteAllPasswords.
    
    Bug: 1075494
    Bug: 1129441
    Change-Id: I36fe982d496f641ce1855ac34c7d816a30e3fcad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456609
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#815935}

--- ios/chrome/test/app/password_test_util.mm
+++ ios/chrome/test/app/password_test_util.mm
@@ -6,6 +6,7 @@
 
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/settings/password/legacy_password_details_table_view_controller+testing.h"
+#import "ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/browser/ui/util/top_view_controller.h"
@@ -53,12 +54,11 @@ MockReauthenticationModule* SetUpAndReturnMockReauthenticationModule() {
   SettingsNavigationController* settings_navigation_controller =
       base::mac::ObjCCastStrict<SettingsNavigationController>(
           top_view_controller::TopPresentedViewController());
-  LegacyPasswordDetailsTableViewController*
-      password_details_table_view_controller =
-          base::mac::ObjCCastStrict<LegacyPasswordDetailsTableViewController>(
-              settings_navigation_controller.topViewController);
-  [password_details_table_view_controller
-      setReauthenticationModule:mock_reauthentication_module];
+  PasswordDetailsTableViewController* password_details_table_view_controller =
+      base::mac::ObjCCastStrict<PasswordDetailsTableViewController>(
+          settings_navigation_controller.topViewController);
+  password_details_table_view_controller.reauthModule =
+      mock_reauthentication_module;
   return mock_reauthentication_module;
 }
 
