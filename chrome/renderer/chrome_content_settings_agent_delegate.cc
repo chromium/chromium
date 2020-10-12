5528b209fff7ca67ca96edc768689c1fe832c98a
Darwin Huang <huangdarwin@chromium.org>
2020-10-12 10:09:22 +0000

    Clipboard: Add UMA metrics for content script reads without user activation. (M86).
    The proportion of content script reads without user activation will
    inform potential deprecation plan and messaging.
    
    (cherry picked from commit 7d1795d455c8bd7459e970cb13f44c79b04ecc3c)
    
    Bug: 1051198
    Change-Id: Ie9c76a6d26ee78219729ef54da7237eb061c98f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440116
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#813889}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464084
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#1207}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- chrome/renderer/chrome_content_settings_agent_delegate.cc
+++ chrome/renderer/chrome_content_settings_agent_delegate.cc
@@ -12,6 +12,7 @@
 #include "third_party/blink/public/web/web_local_frame.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "base/metrics/histogram_functions.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/permissions/api_permission.h"
@@ -73,6 +74,16 @@ ChromeContentSettingsAgentDelegate::AllowReadFromClipboard() {
       extension_dispatcher_->script_context_set().GetCurrent();
   if (current_context && current_context->HasAPIPermission(
                              extensions::APIPermission::kClipboardRead)) {
+    if (current_context->context_type() ==
+        extensions::Feature::CONTENT_SCRIPT_CONTEXT) {
+      bool has_user_activation =
+          render_frame_->GetWebFrame()->HasTransientUserActivation();
+      // TODO(https://crbug.com/1051198): Evaluate and deprecate content script
+      // read without user activation after enough data is gathered.
+      base::UmaHistogramBoolean(
+          "Clipboard.ExtensionContentScriptReadHasUserActivation",
+          has_user_activation);
+    }
     return true;
   }
 #endif
