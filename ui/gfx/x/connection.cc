686851ff2ea0ff5118664e440cb7277193224d29
Tom Anderson <thomasanderson@chromium.org>
2020-10-12 18:46:33 +0000

    [Merge to M87] [XProto] Temporarily use libXkb to translate key events
    > The current key translation code doesn't look at the XKB group encoded
    > in the modifiers.  This is causing an issue where accelerators don't
    > work in environments that use groups to support multiple layouts.
    >
    > This is a minimal fix so that it can be merged back to M87 and M86.  It
    > temporarily reintroduces an Xlib function.
    >
    > R=sky
    >
    > Bug: 1125886, 1136265, 1136248, 1136206
    > Change-Id: Ic6ac82e2918482652f79b35c5a046f56bf5da760
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462490
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#815900}
    
    TBR=sky
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    
    Bug: 1125886, 1136265, 1136248, 1136206
    Change-Id: I6b6a1f9e28dbe31ab35a18fca89ca9dafd2e18c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466161
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#250}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- ui/gfx/x/connection.cc
+++ ui/gfx/x/connection.cc
@@ -27,6 +27,33 @@
 #include "ui/gfx/x/xproto_internal.h"
 #include "ui/gfx/x/xproto_types.h"
 
+extern "C" {
+typedef struct {
+  int type;
+  unsigned long serial;
+  Bool send_event;
+  Display* display;
+  Window window;
+  Window root;
+  Window subwindow;
+  Time time;
+  int x, y;
+  int x_root, y_root;
+  unsigned int state;
+  unsigned int keycode;
+  Bool same_screen;
+} XKeyEvent;
+
+// This is temporarily required to fix XKB key event processing (bugs 1125886,
+// 1136265, 1136248, 1136206).  It should be removed and replaced with an
+// XProto equivalent.
+int XLookupString(XKeyEvent* event_struct,
+                  char* buffer_return,
+                  int bytes_buffer,
+                  ::KeySym* keysym_return,
+                  void* status_in_out);
+}
+
 namespace x11 {
 
 namespace {
@@ -406,8 +433,16 @@ KeyCode Connection::KeysymToKeycode(KeySym keysym) {
 
 KeySym Connection::KeycodeToKeysym(uint32_t keycode, unsigned int modifiers) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  auto sym = TranslateKey(keycode, modifiers);
-  return sym == static_cast<KeySym>(XK_VoidSymbol) ? kNoSymbol : sym;
+
+  XKeyEvent key_event{
+      .type = KeyEvent::Press,
+      .display = display_,
+      .state = modifiers,
+      .keycode = keycode,
+  };
+  ::KeySym keysym;
+  XLookupString(&key_event, nullptr, 0, &keysym, nullptr);
+  return static_cast<x11::KeySym>(keysym);
 }
 
 std::unique_ptr<Connection> Connection::Clone() const {
