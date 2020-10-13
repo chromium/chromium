650ccc13876da3c8e63cdfd06dc5abffdecf401d
Matthew Mourgos <mmourgos@chromium.org>
2020-10-13 00:35:23 +0000

    Multipaste: Add keyboard shortcut icon inline with text.
    This CL adds text to the nudge view and an inline icon to represent the
    key to press to activate the keyboard shortcut. The icon shows as either
    a search icon or a launcher icon, depending on what the device supports.
    If the launcher icon is to be shown, then the icon is changed depending
    on if assistant is available.
    
    Bug: 1105541
    Change-Id: Ieaad19c899af545f38410350ece2d30a48409a11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463869
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816375}

--- ash/clipboard/clipboard_nudge.cc
+++ ash/clipboard/clipboard_nudge.cc
@@ -9,13 +9,16 @@
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/events/keyboard_layout_util.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/wm/core/coordinate_conversion.h"
 
 namespace ash {
@@ -32,7 +35,7 @@ constexpr int kNudgeBlurRadius = 30;
 constexpr int kClipboardIconSize = 20;
 
 // The size of the keyboard shortcut icon.
-constexpr int kKeyboardShortcutIconSize = 16;
+constexpr int kKeyboardShortcutIconSize = 14;
 
 // The minimum width of the label.
 constexpr int kMinLabelWidth = 200;
@@ -45,6 +48,13 @@ constexpr int kIconLabelSpacing = 16;
 
 // The padding which separates the nudge's border with its inner contents.
 constexpr int kNudgePadding = 16;
+
+bool IsAssistantAvailable() {
+  AssistantStateBase* state = AssistantState::Get();
+  return state->allowed_state() ==
+             chromeos::assistant::AssistantAllowedState::ALLOWED &&
+         state->settings_enabled().value_or(false);
+}
 
 }  // namespace
 
@@ -69,25 +79,67 @@ class ClipboardNudge::ClipboardNudgeView : public views::View {
     clipboard_icon_->SetImage(
         gfx::CreateVectorIcon(kClipboardIcon, icon_color));
 
-    label_ = AddChildView(std::make_unique<views::Label>());
+    label_ = AddChildView(std::make_unique<views::StyledLabel>());
     label_->SetPaintToLayer();
     label_->layer()->SetFillsBoundsOpaquely(false);
-    label_->SetMultiLine(true);
     label_->SetPosition(gfx::Point(
         kNudgePadding + kClipboardIconSize + kIconLabelSpacing, kNudgePadding));
-    label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-    label_->SetVerticalAlignment(gfx::ALIGN_TOP);
-    label_->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
-        AshColorProvider::ContentLayerType::kTextColorPrimary));
-    label_->SetBackgroundColor(SK_ColorTRANSPARENT);
 
-    // TODO(mmourgos): Create and set text for |label_|.
-    label_->SetSize(gfx::Size(kMinLabelWidth, kKeyboardShortcutIconSize));
+    bool use_launcher_key = ui::DeviceUsesKeyboardLayout2();
+
+    // Set the keyboard shortcut icon depending on whether search button or
+    // launcher button is being used.
+    gfx::ImageSkia shortcut_icon;
+    if (use_launcher_key) {
+      if (IsAssistantAvailable()) {
+        shortcut_icon = gfx::CreateVectorIcon(gfx::IconDescription(
+            kClipboardLauncherOuterIcon, kKeyboardShortcutIconSize, icon_color,
+            &kClipboardLauncherInnerIcon));
+      } else {
+        shortcut_icon =
+            gfx::CreateVectorIcon(kClipboardLauncherNoAssistantIcon,
+                                  kKeyboardShortcutIconSize, icon_color);
+      }
+    } else {
+      shortcut_icon = gfx::CreateVectorIcon(
+          kClipboardSearchIcon, kKeyboardShortcutIconSize, icon_color);
+    }
+    std::unique_ptr<views::ImageView> keyboard_shortcut_icon;
+    keyboard_shortcut_icon = std::make_unique<views::ImageView>();
+    keyboard_shortcut_icon->SetImage(shortcut_icon);
+    keyboard_shortcut_icon->SetBorder(views::CreateEmptyBorder(2, 4, 0, -2));
+
+    // Set the text for |label_|.
+    base::string16 shortcut_key = l10n_util::GetStringUTF16(
+        use_launcher_key ? IDS_ASH_MULTIPASTE_CONTEXTUAL_NUDGE_LAUNCHER_KEY
+                         : IDS_ASH_MULTIPASTE_CONTEXTUAL_NUDGE_SEARCH_KEY);
+    size_t offset;
+    base::string16 label_text = l10n_util::GetStringFUTF16(
+        IDS_ASH_MULTIPASTE_CONTEXTUAL_NUDGE, shortcut_key, &offset);
+    offset = offset + shortcut_key.length();
+    label_->SetText(label_text);
+
+    // Set the color of the text surrounding the shortcut icon.
+    views::StyledLabel::RangeStyleInfo text_color;
+    text_color.override_color = AshColorProvider::Get()->GetContentLayerColor(
+        AshColorProvider::ContentLayerType::kTextColorPrimary);
+    label_->AddStyleRange(gfx::Range(0, offset), text_color);
+    label_->AddStyleRange(gfx::Range(offset + 1, label_text.length()),
+                          text_color);
+
+    // Add the shortcut icon to |label_|.
+    views::StyledLabel::RangeStyleInfo icon_style;
+    icon_style.custom_view = keyboard_shortcut_icon.get();
+    label_->AddCustomView(std::move(keyboard_shortcut_icon));
+    label_->AddStyleRange(gfx::Range(offset, offset + 1), icon_style);
+
+    label_->SizeToFit(kMinLabelWidth);
+    label_->SetDisplayedOnBackgroundColor(SK_ColorTRANSPARENT);
   }
 
   ~ClipboardNudgeView() override = default;
 
-  views::Label* label_ = nullptr;
+  views::StyledLabel* label_ = nullptr;
   views::ImageView* clipboard_icon_ = nullptr;
 };
 
