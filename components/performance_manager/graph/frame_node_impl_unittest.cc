9cdd5ba89ae79af952a3794790664af24e2e67bb
Fabio Tirelo <ftirelo@chromium.org>
2020-10-14 16:03:56 +0000

    Reland "Revert "[PM] Add FrameNode viewport intersection""
    This is a reland of 53d410bbd3cea8de9389ea2f454dcd2dc16b2745
    
    Original change's description:
    > Revert "[PM] Add FrameNode viewport intersection"
    >
    > This reverts commit cf2a74895ccabc09c1751504a28f995cf5020089.
    >
    > Reason for revert:
    > FrameNodeImplBrowserTest.ViewportIntersection_PartiallyVisible failing on CrOS
    >  - example build: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/41947
    >
    > TBR=pmonette@chromium.org
    >
    > Original change's description:
    > > [PM] Add FrameNode viewport intersection
    > >
    > > This CLs plumbs the viewport intersection to the FrameNode, which
    > > will allow the tracking of the visibility of each frame.
    > >
    > > Bug: 1077217
    > > Change-Id: Id2811b1c52e4fa3f3c449d1b32a1cb7084f82116
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399621
    > > Reviewed-by: François Doray <fdoray@chromium.org>
    > > Reviewed-by: danakj <danakj@chromium.org>
    > > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > > Reviewed-by: David Bokan <bokan@chromium.org>
    > > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#816728}
    >
    > TBR=danakj@chromium.org,nasko@chromium.org,bokan@chromium.org,fdoray@chromium.org,pmonette@chromium.org
    >
    > Change-Id: I4e1cb014232b82016e14fbd85352a6442323300f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1077217
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2470463
    > Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    > Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#817024}
    
    Bug: 1077217
    Change-Id: If7f061e7722fdd6ae4a3ce37a1f51a337481dd58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471261
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817031}

--- components/performance_manager/graph/frame_node_impl_unittest.cc
+++ components/performance_manager/graph/frame_node_impl_unittest.cc
@@ -150,7 +150,6 @@ class LenientMockObserver : public FrameNodeImpl::Observer {
                void(const FrameNode*, const PriorityAndReason& previous_value));
   MOCK_METHOD1(OnHadFormInteractionChanged, void(const FrameNode*));
   MOCK_METHOD1(OnIsAudibleChanged, void(const FrameNode*));
-  MOCK_METHOD1(OnViewportIntersectionChanged, void(const FrameNode*));
   MOCK_METHOD1(OnNonPersistentNotificationCreated, void(const FrameNode*));
   MOCK_METHOD2(OnFirstContentfulPaint, void(const FrameNode*, base::TimeDelta));
 
@@ -368,27 +367,6 @@ TEST_F(FrameNodeImplTest, IsAudible) {
   graph()->RemoveFrameNodeObserver(&obs);
 }
 
-TEST_F(FrameNodeImplTest, ViewportIntersection) {
-  auto process = CreateNode<ProcessNodeImpl>();
-  auto page = CreateNode<PageNodeImpl>();
-  // A child frame node is used because the main frame does not have a viewport
-  // intersection.
-  auto main_frame_node = CreateFrameNodeAutoId(process.get(), page.get());
-  auto child_frame_node =
-      CreateFrameNodeAutoId(process.get(), page.get(), main_frame_node.get());
-
-  MockObserver obs;
-  graph()->AddFrameNodeObserver(&obs);
-
-  EXPECT_CALL(obs, OnViewportIntersectionChanged(child_frame_node.get()));
-
-  gfx::Rect kViewportIntersection(25, 25, 100, 100);
-  child_frame_node->SetViewportIntersection(kViewportIntersection);
-  EXPECT_EQ(child_frame_node->viewport_intersection(), kViewportIntersection);
-
-  graph()->RemoveFrameNodeObserver(&obs);
-}
-
 TEST_F(FrameNodeImplTest, FirstContentfulPaint) {
   auto process = CreateNode<ProcessNodeImpl>();
   auto page = CreateNode<PageNodeImpl>();
@@ -408,10 +386,7 @@ TEST_F(FrameNodeImplTest, PublicInterface) {
   auto process = CreateNode<ProcessNodeImpl>();
   auto page = CreateNode<PageNodeImpl>();
   auto frame_node = CreateFrameNodeAutoId(process.get(), page.get());
-  auto child_frame_node =
-      CreateFrameNodeAutoId(process.get(), page.get(), frame_node.get());
   const FrameNode* public_frame_node = frame_node.get();
-  const FrameNode* public_child_frame_node = child_frame_node.get();
 
   // Simply test that the public interface impls yield the same result as their
   // private counterpart.
@@ -452,10 +427,6 @@ TEST_F(FrameNodeImplTest, PublicInterface) {
             public_frame_node->IsHoldingIndexedDBLock());
   EXPECT_EQ(frame_node->had_form_interaction(),
             public_frame_node->HadFormInteraction());
-  // Use the child frame node to test the viewport intersection because the
-  // viewport intersection of the main frame is not tracked.
-  EXPECT_EQ(child_frame_node->viewport_intersection(),
-            public_child_frame_node->GetViewportIntersection());
 }
 
 TEST_F(FrameNodeImplTest, VisitChildFrameNodes) {
