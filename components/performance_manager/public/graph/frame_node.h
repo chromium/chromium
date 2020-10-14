b0f13f4143937a31c6c86c6b025cde6d58cd0b52
Fabio Tirelo <ftirelo@chromium.org>
2020-10-14 16:17:22 +0000

    [Sheriff] Reverts many CLs that tried to fix the same issue
    This should reopen the tree:
    
    This reverts commit 53d410bbd3cea8de9389ea2f454dcd2dc16b2745.
    This reverts commit eab74a794178251b55da72984babfc59ec49e9c5.
    This reverts commit 9cdd5ba89ae79af952a3794790664af24e2e67bb.
    
    Change-Id: Ifa97474155de2803dc95f5eb73573d7171ef6cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471677
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817036}

--- components/performance_manager/public/graph/frame_node.h
+++ components/performance_manager/public/graph/frame_node.h
@@ -13,6 +13,7 @@
 #include "components/performance_manager/public/mojom/coordination_unit.mojom.h"
 #include "components/performance_manager/public/mojom/lifecycle.mojom.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
+#include "ui/gfx/geometry/rect.h"
 
 class GURL;
 
@@ -173,6 +174,9 @@ class FrameNode : public Node {
   // Returns true if the frame is audible, false otherwise.
   virtual bool IsAudible() const = 0;
 
+  // Returns the intersection of this frame with the viewport.
+  virtual const gfx::Rect& GetViewportIntersection() const = 0;
+
   // Returns a proxy to the RenderFrameHost associated with this node. The
   // proxy may only be dereferenced on the UI thread.
   virtual const RenderFrameHostProxy& GetRenderFrameHostProxy() const = 0;
@@ -240,6 +244,9 @@ class FrameNodeObserver {
   // Invoked when the IsAudible property changes.
   virtual void OnIsAudibleChanged(const FrameNode* frame_node) = 0;
 
+  // Invoked when a frame's intersection with the viewport changes
+  virtual void OnViewportIntersectionChanged(const FrameNode* frame_node) = 0;
+
   // Events with no property changes.
 
   // Invoked when a non-persistent notification has been issued by the frame.
@@ -287,6 +294,7 @@ class FrameNode::ObserverDefaultImpl : public FrameNodeObserver {
       const PriorityAndReason& previous_value) override {}
   void OnHadFormInteractionChanged(const FrameNode* frame_node) override {}
   void OnIsAudibleChanged(const FrameNode* frame_node) override {}
+  void OnViewportIntersectionChanged(const FrameNode* frame_node) override {}
   void OnNonPersistentNotificationCreated(
       const FrameNode* frame_node) override {}
   void OnFirstContentfulPaint(
