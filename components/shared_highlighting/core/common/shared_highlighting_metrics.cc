758d8fd89f3430283bf70a3d51aad5031d86759c
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2020-10-14 14:27:43 +0000

    [SH] Add common logging functions.
    Bug: 1131107
    Change-Id: Ib7b047409a42fe5a4ad5b33ecf10825214c9089d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456126
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817004}

---
+++ components/shared_highlighting/core/common/shared_highlighting_metrics.cc
@@ -0,0 +1,49 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/shared_highlighting/core/common/shared_highlighting_metrics.h"
+
+#include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
+#include "components/search_engines/search_engine_utils.h"
+
+namespace shared_highlighting {
+
+namespace {
+
+TextFragmentLinkOpenSource GetLinkSource(const GURL& referrer) {
+  bool from_search_engine = SearchEngineUtils::GetEngineType(referrer) > 0;
+  return from_search_engine ? TextFragmentLinkOpenSource::kSearchEngine
+                            : TextFragmentLinkOpenSource::kUnknown;
+}
+
+}  // namespace
+
+void LogTextFragmentAmbiguousMatch(bool ambiguous_match) {
+  base::UmaHistogramBoolean("TextFragmentAnchor.AmbiguousMatch",
+                            ambiguous_match);
+}
+
+void LogTextFragmentLinkOpenSource(const GURL& referrer) {
+  base::UmaHistogramEnumeration("TextFragmentAnchor.LinkOpenSource",
+                                GetLinkSource(referrer));
+}
+
+void LogTextFragmentMatchRate(int matches, int text_fragments) {
+  if (text_fragments == 0) {
+    NOTREACHED();
+    return;
+  }
+
+  const int match_rate_percent =
+      static_cast<int>(100 * ((matches + 0.0) / text_fragments));
+  base::UmaHistogramPercentage("TextFragmentAnchor.MatchRate",
+                               match_rate_percent);
+}
+
+void LogTextFragmentSelectorCount(int count) {
+  base::UmaHistogramCounts100("TextFragmentAnchor.SelectorCount", count);
+}
+
+}  // namespace shared_highlighting
