7bf83af8318c419da4754bc49ebf9d85a046b0a6
James Vecore <vecore@google.com>
2020-10-15 19:56:51 +0000

    [Nearby] Only use prod tachyon urls
    Fixed: 1134810
    Change-Id: Id46b76dc22328b826a3359344e8d33366661459d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476685
    Commit-Queue: James Vecore <vecore@google.com>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Auto-Submit: James Vecore <vecore@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817623}

--- chrome/browser/nearby_sharing/instantmessaging/constants.h
+++ chrome/browser/nearby_sharing/instantmessaging/constants.h
@@ -5,21 +5,11 @@
 #ifndef CHROME_BROWSER_NEARBY_SHARING_INSTANTMESSAGING_CONSTANTS_H_
 #define CHROME_BROWSER_NEARBY_SHARING_INSTANTMESSAGING_CONSTANTS_H_
 
-#if defined(NDEBUG)
-const char kInstantMessagingReceiveMessageAPI[] =
-    "https://tachyon-playground-autopush-grpc.sandbox.googleapis.com/v1/"
-    "messages:receiveExpress";
-
-const char kInstantMessagingSendMessageAPI[] =
-    "https://tachyon-playground-autopush-grpc.sandbox.googleapis.com/v1/"
-    "message:sendExpress";
-#else
 const char kInstantMessagingReceiveMessageAPI[] =
     "https://instantmessaging-pa.googleapis.com/v1/messages:receiveExpress";
 
 const char kInstantMessagingSendMessageAPI[] =
     "https://instantmessaging-pa.googleapis.com/v1/message:sendExpress";
-#endif  // defined(NDEBUG)
 
 // Template for optional OAuth2 authorization HTTP header.
 const char kAuthorizationHeaderFormat[] = "Authorization: Bearer %s";
