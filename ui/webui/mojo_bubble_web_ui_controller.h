6e3387db4f74e4d822b05e9e2df6ca8e1302863c
Thomas Lukaszewicz <tluk@chromium.org>
2020-10-15 16:37:01 +0000

    Views: Add WebBubbleDialogView and MojoBubbleWebUIController classes
    This CL adds a reusable bubble and corresponding WebUIController
    class to be used when implementing Secondary UI using WebUI.
    
    See the below doc for more details.
    
    https://docs.google.com/document/d/1UtAVhVpo8Si_XlZEdt-BH_AN4egBo1XppZEhF3_bthM/edit?usp=sharing
    
    Bug: 1099917
    Change-Id: I6b16997b05dc2bf2f208ca71dd5b3cfa11db00f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462223
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817521}

---
+++ ui/webui/mojo_bubble_web_ui_controller.h
@@ -0,0 +1,43 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_WEBUI_MOJO_BUBBLE_WEB_UI_CONTROLLER_H_
+#define UI_WEBUI_MOJO_BUBBLE_WEB_UI_CONTROLLER_H_
+
+#include "base/callback_forward.h"
+#include "base/memory/weak_ptr.h"
+#include "ui/webui/mojo_web_ui_controller.h"
+
+namespace content {
+class WebUI;
+}  // namespace content
+
+namespace ui {
+
+class MojoBubbleWebUIController : public MojoWebUIController {
+ public:
+  class Embedder {
+   public:
+    virtual void ShowUI() = 0;
+  };
+
+  // By default MojoBubbleWebUIController do not have normal WebUI bindings.
+  // Pass |enable_chrome_send| as true if these are needed.
+  explicit MojoBubbleWebUIController(content::WebUI* contents,
+                                     bool enable_chrome_send = false);
+  MojoBubbleWebUIController(const MojoBubbleWebUIController&) = delete;
+  MojoBubbleWebUIController& operator=(const MojoBubbleWebUIController&) =
+      delete;
+  ~MojoBubbleWebUIController() override;
+
+  void set_embedder(base::WeakPtr<Embedder> embedder) { embedder_ = embedder; }
+  base::WeakPtr<Embedder> embedder() { return embedder_; }
+
+ private:
+  base::WeakPtr<Embedder> embedder_;
+};
+
+}  // namespace ui
+
+#endif  // UI_WEBUI_MOJO_BUBBLE_WEB_UI_CONTROLLER_H_
