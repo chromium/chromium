e18b4558edf5e0c7448e2a071fa5ac402e6ea562
Yulun Wu <yulunwu@chromium.org>
2020-10-15 20:45:22 +0000

    Fix default search box colors.
    (cherry picked from commit c7a81956d29839e8acc0bd7201760ec45bc2dc21)
    
    Bug: 1134721
    Change-Id: I50136df0e71e647e4f6629457da13104a7431422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473140
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#817339}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2474673
    Cr-Commit-Position: refs/branch-heads/4280@{#428}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- ash/app_list/app_list_color_provider_impl.cc
+++ ash/app_list/app_list_color_provider_impl.cc
@@ -52,7 +52,7 @@ SkColor AppListColorProviderImpl::GetSearchBoxCardBackgroundColor() const {
 SkColor AppListColorProviderImpl::GetSearchBoxPlaceholderTextColor() const {
   return DeprecatedGetContentLayerColor(
       AshColorProvider::ContentLayerType::kTextColorSecondary,
-      /*default_color*/ gfx::kGoogleGrey200);
+      /*default_color*/ SkColorSetARGB(0xDE, 0x00, 0x00, 0x00));
 }
 
 SkColor AppListColorProviderImpl::GetSearchBoxTextColor(
