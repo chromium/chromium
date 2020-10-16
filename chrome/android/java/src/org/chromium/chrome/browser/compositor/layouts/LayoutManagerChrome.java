e05a888c4dd58fb672a822113b577cf535d6c3b4
spdonghao <spdonghao@chromium.org>
2020-10-16 16:50:56 +0000

    Use tabContentManagerSupplier which is guaranteed to be non-null.
    Bug: 1126823
    Change-Id: I32ec909f844c29433d1b60e0048ba725052718f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475575
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817966}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -105,7 +105,7 @@ public class LayoutManagerChrome
                 if (mOverviewLayout != null) {
                     mOverviewLayout.setTabContentManager(manager);
                 }
-                mTabContentManagerSupplier.removeObserver(this);
+                tabContentManagerSupplier.removeObserver(this);
             }
         });
 
