369bfd729a3b56634f26bd68e6d2b26ac12b4e09
Hidehiko Abe <hidehiko@chromium.org>
2020-10-16 07:43:49 +0000

    Replace OS_LINUX by OS_LINUX || OS_CHROMEOS.
    Currently OS_LINUX is defined on Chrome OS build,
    but it will be unset. This CL is the preparation to keep the
    current behavior.
    
    Bug: 1110266
    Test: Built.
    Change-Id: I99b6d32990ca189bf11ca62e473fbbb9e98e8c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2477702
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817831}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -909,12 +909,12 @@ error::Error WebGPUDecoderImpl::HandleRequestAdapter(
       static_cast<DawnRequestAdapterSerial>(c.request_adapter_serial);
 
   if (gr_context_type_ != GrContextType::kVulkan) {
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
     SendAdapterProperties(request_adapter_serial, -1, nullptr,
                           "WebGPU on Linux requires command-line flag "
                           "--enable-features=Vulkan,UseSkiaRenderer");
     return error::kNoError;
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
   }
 
   int32_t requested_adapter_index = GetPreferredAdapterIndex(power_preference);
