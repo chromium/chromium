55f62f27a4121702ed00aa5cce1c90b70fad39bd
Andre Le <leandre@chromium.org>
2020-10-20 23:46:05 +0000

    [CrOS PhoneHub] Disable Locate Phone button when Silence Phone is on.
    - Add an observer for state change in Silence Phone so that Locate Phone
    can be disable when Silence Phone is on.
    - Refactor InitQuickActionsItems() since Locate Phone and Silence Phone
    is no longer independent.
    
    BUG=1106937,1126208
    
    Change-Id: I6f015e868d6d00df6a40447de62f3efd0b2f2ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2480662
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819161}

---
+++ ash/system/phonehub/silence_phone_quick_action_controller_unittest.cc
@@ -0,0 +1,83 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/phonehub/silence_phone_quick_action_controller.h"
+
+#include "ash/test/ash_test_base.h"
+#include "chromeos/components/phonehub/fake_do_not_disturb_controller.h"
+
+namespace ash {
+
+class SilencePhoneQuickActionControllerTest
+    : public AshTestBase,
+      public SilencePhoneQuickActionController::Observer {
+ public:
+  SilencePhoneQuickActionControllerTest() = default;
+
+  ~SilencePhoneQuickActionControllerTest() override = default;
+
+  // AshTestBase:
+  void SetUp() override {
+    AshTestBase::SetUp();
+
+    dnd_controller_ =
+        std::make_unique<chromeos::phonehub::FakeDoNotDisturbController>();
+    controller_ = std::make_unique<SilencePhoneQuickActionController>(
+        dnd_controller_.get());
+
+    controller_->AddObserver(this);
+    controller_->CreateItem();
+  }
+
+  void TearDown() override {
+    controller_->RemoveObserver(this);
+    controller_.reset();
+    dnd_controller_.reset();
+    AshTestBase::TearDown();
+  }
+
+ protected:
+  // SilencePhoneQuickActionController::Observer:
+  void OnSilencePhoneItemStateChanged() override { ++num_calls_; }
+
+  SilencePhoneQuickActionController* controller() { return controller_.get(); }
+
+  chromeos::phonehub::FakeDoNotDisturbController* dnd_controller() {
+    return dnd_controller_.get();
+  }
+
+  size_t GetNumObserverCalls() { return num_calls_; }
+
+ private:
+  std::unique_ptr<SilencePhoneQuickActionController> controller_;
+  std::unique_ptr<chromeos::phonehub::FakeDoNotDisturbController>
+      dnd_controller_;
+  size_t num_calls_ = 0;
+};
+
+TEST_F(SilencePhoneQuickActionControllerTest, ItemStateChanged) {
+  // Set request to fail to avoid triggering state's changes by the model.
+  dnd_controller()->SetShouldRequestFail(true);
+
+  // Initially, there's one observer call during initiation.
+  EXPECT_EQ(1u, GetNumObserverCalls());
+
+  // Press the button to enabled state will trigger observer.
+  controller()->OnButtonPressed(false /* is_now_enabled */);
+  EXPECT_EQ(2u, GetNumObserverCalls());
+
+  // Item state changed to enabled.
+  EXPECT_TRUE(controller()->IsItemEnabled());
+
+  // Press the button to disabled state will trigger observer.
+  controller()->OnButtonPressed(true /* is_now_enabled */);
+  EXPECT_EQ(3u, GetNumObserverCalls());
+
+  // Item state changed to disabled.
+  EXPECT_FALSE(controller()->IsItemEnabled());
+
+  dnd_controller()->SetShouldRequestFail(false);
+}
+
+}  // namespace ash
