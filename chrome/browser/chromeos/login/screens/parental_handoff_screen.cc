dca1afe8097a5048e14c5aa96d3f0f66011dc23d
yilkal <yilkal@chromium.org>
2020-10-20 23:39:21 +0000

    Adds parental handoff login screen in oobe.
    The description and mock can be seen in the comments in the bug report.
    
    Bug: 1134567
    Change-Id: I2ecd7c72629dc96dfbc054f747c6cb2508210709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466618
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819158}

---
+++ chrome/browser/chromeos/login/screens/parental_handoff_screen.cc
@@ -0,0 +1,109 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/login/screens/parental_handoff_screen.h"
+
+#include <string>
+
+#include "base/strings/string16.h"
+#include "chrome/browser/chromeos/login/oobe_screen.h"
+#include "chrome/browser/chromeos/login/screen_manager.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/supervised_user/supervised_user_features.h"
+#include "chrome/browser/ui/webui/chromeos/login/parental_handoff_screen_handler.h"
+#include "chrome/grit/chromium_strings.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace chromeos {
+
+namespace {
+
+constexpr char kUserActionNext[] = "next";
+
+base::string16 GetActiveUserName() {
+  const user_manager::User* user =
+      user_manager::UserManager::Get()->GetActiveUser();
+  if (!user || !user->IsChild())
+    return base::string16();
+  return user->GetDisplayName();
+}
+
+}  // namespace
+
+// static
+ParentalHandoffScreen* ParentalHandoffScreen::Get(
+    ScreenManager* screen_manager) {
+  return static_cast<ParentalHandoffScreen*>(
+      screen_manager->GetScreen(ParentalHandoffScreenView::kScreenId));
+}
+
+// static
+std::string ParentalHandoffScreen::GetResultString(
+    ParentalHandoffScreen::Result result) {
+  switch (result) {
+    case ParentalHandoffScreen::Result::DONE:
+      return "Done";
+    case ParentalHandoffScreen::Result::SKIPPED:
+      return BaseScreen::kNotApplicable;
+  }
+}
+
+ParentalHandoffScreen::ParentalHandoffScreen(
+    ParentalHandoffScreenView* view,
+    const ScreenExitCallback& exit_callback)
+    : BaseScreen(ParentalHandoffScreenView::kScreenId,
+                 OobeScreenPriority::DEFAULT),
+      view_(view),
+      exit_callback_(exit_callback) {
+  if (view_)
+    view_->Bind(this);
+}
+
+ParentalHandoffScreen::~ParentalHandoffScreen() {
+  if (view_)
+    view_->Unbind();
+}
+
+void ParentalHandoffScreen::OnViewDestroyed(ParentalHandoffScreenView* view) {
+  if (view_ == view)
+    view_ = nullptr;
+}
+
+bool ParentalHandoffScreen::MaybeSkip(WizardContext* context) {
+  Profile* profile = ProfileManager::GetActiveUserProfile();
+  if (profile->IsChild() && supervised_users::IsEduCoexistenceFlowV2Enabled()) {
+    return false;
+  }
+
+  exit_callback_.Run(Result::SKIPPED);
+  return true;
+}
+
+void ParentalHandoffScreen::ShowImpl() {
+  if (!view_)
+    return;
+
+  base::string16 user_name = GetActiveUserName();
+
+  view_->Show(l10n_util::GetStringFUTF16(
+                  IDS_LOGIN_PARENTAL_HANDOFF_SCREEN_TITLE, user_name),
+              l10n_util::GetStringFUTF16(
+                  IDS_LOGIN_PARENTAL_HANDOFF_SCREEN_SUBTITLE, user_name));
+}
+void ParentalHandoffScreen::HideImpl() {}
+
+void ParentalHandoffScreen::OnUserAction(const std::string& action_id) {
+  if (action_id == kUserActionNext) {
+    exit_callback_.Run(Result::DONE);
+  } else {
+    BaseScreen::OnUserAction(action_id);
+  }
+}
+
+}  // namespace chromeos
