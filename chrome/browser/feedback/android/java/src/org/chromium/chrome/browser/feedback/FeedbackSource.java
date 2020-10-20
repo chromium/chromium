04d97b52268d8c3b5e15a32cda9c6d71f841f635
Xinghui Lu <xinghuilu@chromium.org>
2020-10-20 19:24:48 +0000

    Move generic interfaces to feedback module.
    Move AsyncFeedbackSource.java, FeedbackSource.java and
    ScreenshotSource.java from chrome/android to
    chrome/browser/feedback/android.
    
    More classes will be moved in a future CL.
    
    Bug: 1117343
    Change-Id: I618c2e0e46a265098e121a204f987bba97a674c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2477595
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819033}

--- chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/FeedbackSource.java
+++ chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/FeedbackSource.java
@@ -18,12 +18,9 @@ public interface FeedbackSource {
      * Called to get all relevant feedback for this source.
      * @return A map of all feedback reported by this source.
      */
-    // clang-format off
-    // TODO(crbug.com/781018): Clang isn't formatting this correctly.
     default @Nullable Map<String, String> getFeedback() {
         return null;
     }
-    // clang-format on
 
     /**
      * Returns a key-value pair of logs for this source.  It is appropriate to
@@ -33,10 +30,7 @@ public interface FeedbackSource {
      * @return A key-value pair representing the logs for this source and the
      *         identifier.
      */
-    // clang-format off
-    // TODO(crbug.com/781018): Clang isn't formatting this correctly.
     default @Nullable Pair<String, String> getLogs() {
         return null;
     }
-        // clang-format on
 }
