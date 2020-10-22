1f033c4eb366fbeeec61536ff3d6e734aed587a1
Jack Lynch <jalyn@microsoft.com>
2020-10-22 19:05:07 +0000

    DevTools: Increase maximum histogram enum size
    This CL increases the maximum enum size accepted by
    DevToolsUIBindings::RecordEnumeratedHistogram from 100 to 1000.
    
    Bug: 1140386
    Change-Id: I72ca6b48825cb4e3e681322cd6107d729a817578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488561
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#819926}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -1283,7 +1283,7 @@ void DevToolsUIBindings::RecordEnumeratedHistogram(const std::string& name,
                                                    int boundary_value) {
   if (!frontend_host_)
     return;
-  if (!(boundary_value >= 0 && boundary_value <= 100 && sample >= 0 &&
+  if (!(boundary_value >= 0 && boundary_value <= 1000 && sample >= 0 &&
         sample < boundary_value)) {
     // TODO(nick): Replace with chrome::bad_message::ReceivedBadMessage().
     frontend_host_->BadMessageReceived();
