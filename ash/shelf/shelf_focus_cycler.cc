0cb79e0e1612cb5506ab3e90e4a28f06890a316b
Danila Kuzmin <dkuzmin@google.com>
2020-10-22 13:33:08 +0000

    oobe: Fix reverse focus for screens without LoginShelfView buttons
    Bug: 1134822
    Change-Id: I8fdae2162564b787a9d484b58cb687cf6c8d5b1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2470878
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/master@{#819809}

--- ash/shelf/shelf_focus_cycler.cc
+++ ash/shelf/shelf_focus_cycler.cc
@@ -45,7 +45,9 @@ void ShelfFocusCycler::FocusOut(bool reverse, SourceView source_view) {
       //   if we're going in reverse; if we're going forward, let the system
       //   tray focus observers focus the lock/login view.
       if (shelf_->shelf_widget()->login_shelf_view()->GetVisible() &&
-          !reverse) {
+          (!reverse ||
+           (!shelf_->shelf_widget()->login_shelf_view()->IsFocusable() &&
+            reverse))) {
         // Login/lock screen or OOBE.
         Shell::Get()->system_tray_notifier()->NotifyFocusOut(reverse);
       } else if (reverse) {
