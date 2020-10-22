1c80c6874ed91dd2444543c85291e7a8109d4f68
Elly Fong-Jones <ellyjones@chromium.org>
2020-10-22 20:49:08 +0000

    cbui crostini: refactor away CrostiniAppRestartView
    This change removes CrostiniAppRestartView, replacing it with:
    * Construction and configuration of a bare View + DialogDelegate pair
    * A pair of public functions that create and show this dialog
    
    This change also adds some unit test coverage for this dialog to
    ensure that I didn't break it when doing this refactor.
    
    This change also fixes a bug in the dialog found by the new tests: the
    message used for the body was being given a parameter that was never
    used, which would have caused a crash if it had ever happened in a debug
    build.
    
    Bug: 1075649
    Change-Id: Ia29640e1e47bd96c2996eba6c39722c477ff4aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490345
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#819545}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490518
    Cr-Commit-Position: refs/heads/master@{#819979}

---
+++ chrome/browser/ui/views/crostini/crostini_app_restart_dialog_unittest.cc
@@ -0,0 +1,47 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/crostini/crostini_app_restart_dialog.h"
+
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/test/views/chrome_views_test_base.h"
+#include "ui/views/test/widget_test.h"
+#include "ui/views/widget/any_widget_observer.h"
+#include "ui/views/window/dialog_delegate.h"
+
+class CrostiniAppRestartDialogTest : public ChromeViewsTestBase {
+ public:
+  CrostiniAppRestartDialogTest() = default;
+  ~CrostiniAppRestartDialogTest() override = default;
+
+  views::test::WidgetTest::WidgetAutoclosePtr ShowDialog() {
+    // TODO(ellyjones): the class name being "View" is kind of awkward. It
+    // should be possible to set the class name even when there is no actual
+    // class...
+    views::NamedWidgetShownWaiter waiter(views::test::AnyWidgetTestPasskey{},
+                                         "View");
+    crostini::ShowAppRestartDialogForTesting(GetContext());
+    return views::test::WidgetTest::WidgetAutoclosePtr(
+        waiter.WaitIfNeededAndGet());
+  }
+};
+
+TEST_F(CrostiniAppRestartDialogTest, OnlyHasOkButton) {
+  auto widget = ShowDialog();
+  EXPECT_EQ(widget->widget_delegate()->AsDialogDelegate()->GetDialogButtons(),
+            ui::DIALOG_BUTTON_OK);
+}
+
+TEST_F(CrostiniAppRestartDialogTest, IsSystemModal) {
+  auto widget = ShowDialog();
+  EXPECT_EQ(widget->widget_delegate()->AsDialogDelegate()->GetModalType(),
+            ui::MODAL_TYPE_SYSTEM);
+}
+
+TEST_F(CrostiniAppRestartDialogTest, ContentsViewHasModalPreferredWidth) {
+  auto widget = ShowDialog();
+  EXPECT_EQ(widget->widget_delegate()->GetContentsView()->width(),
+            ChromeLayoutProvider::Get()->GetDistanceMetric(
+                views::DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH));
+}
