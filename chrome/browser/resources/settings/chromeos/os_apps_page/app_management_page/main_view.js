4e63842d8d60a17f9a45c8c362c9d6ddb80f2075
Michael Irani <michaelirani@chromium.org>
2020-10-23 23:29:47 +0000

    ChromeVox focus not properly shown when navigating: Apps page
    If you navigate to "Apps" page and then to an application,
    and then hit the back button the focus does not highlight the
    application that was chosen when in ChromeVox mode.
    
    Steps (in ChromeVox mode):
    1. Apps
    2. Chrome
    3. Back button
    
    Bug: 1029954
    Test: Manual
    Change-Id: I8c54f7fb88379392ebfc373f209b9386a84a345e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493826
    Commit-Queue: Michael Irani <michaelirani@chromium.org>
    Auto-Submit: Michael Irani <michaelirani@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820490}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/main_view.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/main_view.js
@@ -7,6 +7,7 @@ Polymer({
 
   behaviors: [
     app_management.StoreClient,
+    settings.RouteObserverBehavior,
   ],
 
   properties: {
@@ -38,6 +39,25 @@ Polymer({
   attached() {
     this.watch('apps_', state => state.apps);
     this.updateFromStore();
+  },
+
+  /**
+   * @param {!settings.Route} route
+   * @param {!settings.Route} oldRoute
+   */
+  currentRouteChanged(route, oldRoute) {
+    if (route === settings.routes.APP_MANAGEMENT) {
+      const appId = app_management.Store.getInstance().data.selectedAppId;
+
+      // Expect this to be false the first time the "Manage your apps" page
+      // is requested as no app has been selected yet.
+      if (appId) {
+        const button = this.$$(`#app-subpage-button-${appId}`);
+        if (button) {
+          cr.ui.focusWithoutInk(button);
+        }
+      }
+    }
   },
 
   /**
