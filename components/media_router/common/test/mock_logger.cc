59f231f026df73e46d4b9792c2f1acbeadcb0eea
Muyao Xu <muyaoxu@google.com>
2020-10-23 17:33:31 +0000

    Move mock_logger to components/media_router/common/test
    This CL moves mock_logger.* from chrome/browser/ to components/ so that
    it can be reused by mirroring service tests in components/ directory.
    Mirroring service tests will be changed in a later CL.
    
    Bug: b/165366974, 1117673
    Change-Id: I225de779b0caa476fd253b56e0ecc10f16e13688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2482092
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/master@{#820315}

--- components/media_router/common/test/mock_logger.cc
+++ components/media_router/common/test/mock_logger.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/media/router/test/mock_logger.h"
+#include "components/media_router/common/test/mock_logger.h"
 
 namespace media_router {
 
