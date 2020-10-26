9f64374dfc877cbfaa325709eb0224e7c0153a37
Patrick Noland <pnoland@chromium.org>
2020-10-26 17:29:03 +0000

    Revert "Roll GN from e002e68a..f5f465b5"
    This reverts commit 7f3e4855725d5fe29df807b22917631c7ce4aac0.
    
    Reason for revert: Breaks the build at _generate_buildfiles step
    
    Original change's description:
    > Roll GN from e002e68a..f5f465b5
    >
    > Generated by //tools/gn/roll_gn.py
    >
    > https://gn.googlesource.com/gn/+log/e002e68a..f5f465b5
    >
    >     f5f465b5 Fix runtime_deps for elided targets
    >     3d165b90 Correct the `gn format --dump-tree=json ...` command to write to stdout instead of stderr.
    >     ad0a7ada Remove support for set_sources_assignment_filter function
    >     e1ac69b1 Add a command line to define/overwrite "root" in ".gn"
    >     7b2a04d5 VS: Use most recently installed Windows 10 SDK.
    >     132588e1 Revert "gn recipe: Link GN with rpmalloc to speed it up."
    >     3281c669 Add "framework_dirs" and "frameworks" to syntax highlighting rules
    >     4323e701 gn recipe: Link GN with rpmalloc to speed it up.
    >     c808630c Move runtime_deps file for phony targets to /obj
    >     8855c522 Update Clang and Xcode SDK to 12B5025f
    >     76f9b6ad Use fetch instead of cherry-pick for Gerrit changes
    >     a3d69e87 Don't return if package is up-to-date
    >     36ff17cb Switch back to the older Clang version
    >     7bcc0eb3 Revert "Update Xcode SDK to 12B5025f"
    >     e5f1f587 Update Xcode SDK to 12B5025f
    >     6453c5de Support cross-compiling GN for other targets
    >     42d6de6e Don't classify Rust static libraries as final
    >     a9eaeb80 Switch remaining stamp targets to phony targets
    >     ff45cf2a Show config.visibility in `gn desc`
    >     de5a8348 [rust project] removing obsolete std libraries
    >     4c684398 Fix "gn outputs" for bundle data.
    >     99f72f88 Switch source_set stamp targets to phony targets
    >     5233fa13 Remove extra * in gn output docs
    >
    > As of 99f72f88 and a9eaeb80, GN's usage of stamp files for intermediate
    > steps in the build graph have been replaced with phony aliases. As a
    > result, this CL also updates the MB tool to look for ".runtime_deps"
    > files instead of ".stamp.runtime_deps".
    >
    > Change-Id: I4ab46ee211c7dd45bba655c946e24684b0cfcf9a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485444
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Commit-Queue: RJ Ascani <rjascani@google.com>
    > Cr-Commit-Position: refs/heads/master@{#820799}
    
    TBR=dpranke@google.com,rjascani@google.com
    
    Change-Id: Icbad43cbaf7f622fb15f8e47693e01fc9eb234d8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499364
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820806}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1162,7 +1162,7 @@ class MetaBuildWrapper(object):
     for target in ninja_targets:
       target_type = isolate_map[target]['type']
       label = isolate_map[target]['label']
-      runtime_deps = 'obj/%s.runtime_deps' % label.replace(':', '/')
+      stamp_runtime_deps = 'obj/%s.stamp.runtime_deps' % label.replace(':', '/')
       # TODO(https://crbug.com/876065): 'official_tests' use
       # type='additional_compile_target' to isolate tests. This is not the
       # intended use for 'additional_compile_target'.
@@ -1175,21 +1175,23 @@ class MetaBuildWrapper(object):
       elif fuchsia or ios or target_type == 'generated_script':
         # iOS and Fuchsia targets end up as groups.
         # generated_script targets are always actions.
-        rpaths = [runtime_deps]
+        rpaths = [stamp_runtime_deps]
       elif android:
         # Android targets may be either android_apk or executable. The former
-        # will result in runtime_deps associated with the target, while the
+        # will result in runtime_deps associated with the stamp file, while the
         # latter will result in runtime_deps associated with the executable.
         label = isolate_map[target]['label']
-        rpaths = [target + '.runtime_deps', runtime_deps]
+        rpaths = [
+            target + '.runtime_deps',
+            stamp_runtime_deps]
       elif (target_type == 'script'
             or isolate_map[target].get('label_type') == 'group'):
         # For script targets, the build target is usually a group,
-        # for which gn generates the runtime_deps in the build directory
+        # for which gn generates the runtime_deps next to the stamp file
         # for the label, which lives under the obj/ directory, but it may
         # also be an executable.
         label = isolate_map[target]['label']
-        rpaths = [runtime_deps]
+        rpaths = [stamp_runtime_deps]
         if win:
           rpaths += [ target + '.exe.runtime_deps' ]
         else:
