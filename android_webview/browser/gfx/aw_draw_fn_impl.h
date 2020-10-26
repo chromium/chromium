442f3e3ac4754715a4bc0fb584ee19da19a6dd09
Bo Liu <boliu@chromium.org>
2020-10-26 20:16:25 +0000

    aw: Factor out vulkan gl interop
    Keep comment code such as creating SkColorSpace or converting parameters
    in AwDrawFnImpl.
    
    Fixed: 1141685
    Change-Id: I0c7191b59fffe413259d7ee308eb2f4610424ba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495918
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820871}

--- android_webview/browser/gfx/aw_draw_fn_impl.h
+++ android_webview/browser/gfx/aw_draw_fn_impl.h
@@ -10,27 +10,14 @@
 #include "android_webview/browser/gfx/aw_vulkan_context_provider.h"
 #include "android_webview/browser/gfx/compositor_frame_consumer.h"
 #include "android_webview/browser/gfx/render_thread_manager.h"
+#include "android_webview/browser/gfx/vulkan_gl_interop.h"
 #include "android_webview/public/browser/draw_fn.h"
-#include "base/android/jni_weak_ref.h"
-#include "base/containers/queue.h"
-#include "base/files/scoped_file.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
 #include "base/optional.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
-#include "third_party/skia/include/gpu/vk/GrVkTypes.h"
-
-class GrVkSecondaryCBDrawContext;
-
-namespace gl {
-class GLImageAHardwareBuffer;
-}
-
-namespace gpu {
-class VulkanImage;
-}
 
 namespace android_webview {
-class GLNonOwnedCompatibilityContext;
 
 class AwDrawFnImpl {
  public:
@@ -55,35 +42,10 @@ class AwDrawFnImpl {
 
  private:
   // With direct mode, we will render frames with Vulkan API directly.
-  void DrawVkDirect(AwDrawFn_DrawVkParams* params);
+  void DrawVkDirect(sk_sp<GrVkSecondaryCBDrawContext> draw_context,
+                    sk_sp<SkColorSpace> color_space,
+                    HardwareRendererDrawParams* params);
   void PostDrawVkDirect(AwDrawFn_PostDrawVkParams* params);
-
-  // With interop mode, we will render frames on AHBs with GL api, and then draw
-  // AHBs with Vulkan API on the final target.
-  void DrawVkInterop(AwDrawFn_DrawVkParams* params);
-  void PostDrawVkInterop(AwDrawFn_PostDrawVkParams* params);
-
-  template <typename T>
-  void DrawInternal(T* params, SkColorSpace* color_space);
-
-  // Struct which represents one in-flight draw for the Vk interop path.
-  struct InFlightInteropDraw {
-    explicit InFlightInteropDraw(AwVulkanContextProvider* vk_context_provider);
-    ~InFlightInteropDraw();
-    sk_sp<GrVkSecondaryCBDrawContext> draw_context;
-    VkFence post_draw_fence = VK_NULL_HANDLE;
-    VkSemaphore post_draw_semaphore = VK_NULL_HANDLE;
-    base::ScopedFD sync_fd;
-    scoped_refptr<gl::GLImageAHardwareBuffer> ahb_image;
-    sk_sp<SkImage> ahb_skimage;
-    uint32_t texture_id = 0;
-    uint32_t framebuffer_id = 0;
-    std::unique_ptr<gpu::VulkanImage> vulkan_image;
-    GrVkImageInfo image_info;
-
-    // Used to clean up Vulkan objects.
-    AwVulkanContextProvider* vk_context_provider;
-  };
 
   CompositorFrameConsumer* GetCompositorFrameConsumer() {
     return &render_thread_manager_;
@@ -101,12 +63,7 @@ class AwDrawFnImpl {
   base::Optional<AwVulkanContextProvider::ScopedSecondaryCBDraw>
       scoped_secondary_cb_draw_;
 
-  // GL context used to draw via GL in Vk interop path.
-  scoped_refptr<GLNonOwnedCompatibilityContext> gl_context_;
-
-  // Queue of draw contexts pending cleanup.
-  base::queue<std::unique_ptr<InFlightInteropDraw>> in_flight_interop_draws_;
-  std::unique_ptr<InFlightInteropDraw> pending_draw_;
+  base::Optional<VulkanGLInterop> interop_;
 
   DISALLOW_COPY_AND_ASSIGN(AwDrawFnImpl);
 };
