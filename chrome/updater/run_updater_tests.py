b227bf9d57cf9830fc0553b34eb37b9f75f60b53
adoneria <adoneria@google.com>
2020-10-26 18:56:06 +0000

    [updater] pass build dir to tests.
    Also changes test case superclass to typ.TestCase.
    
    Bug: 1141577
    Change-Id: I0d9b4b38543d1cd904808d9f85b6aa27ab973c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2496180
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Anjali Doneria <adoneria@google.com>
    Cr-Commit-Position: refs/heads/master@{#820849}

--- chrome/updater/run_updater_tests.py
+++ chrome/updater/run_updater_tests.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
+import logging
 import sys
 
 from test.integration_tests.common import path_finder
@@ -9,12 +11,34 @@ path_finder.add_typ_dir_to_sys_path()
 
 import typ
 
+class Context(object):
+  def __init__(self, build_dir):
+    self.build_dir = build_dir
+
 
 def main():
-  return typ.main(
-      tests=[path_finder.get_integration_tests_dir()],
-  )
+  parser = typ.ArgumentParser()
+  parser.add_argument('--build-dir',
+                      help='Specifies chromium build directory.')
 
+  runner = typ.Runner()
+
+  # Set this when using context that will be passed to tests.
+  runner.win_multiprocessing = typ.WinMultiprocessing.importable
+
+  runner.parse_args(parser,
+                    argv=None,
+                    tests=[path_finder.get_integration_tests_dir()])
+
+  # setup logging level
+  if runner.args.verbose > 1:
+    level = logging.DEBUG
+  else:
+    level = logging.INFO
+  logging.basicConfig(level=level)
+
+  runner.context = Context(runner.args.build_dir)
+  return runner.run()[0]
 
 if __name__ == "__main__":
-    sys.exit(main())
+  sys.exit(main())
