bc749206229e14fdbf18724f054d46b3f13aa62f
Eugene Zemtsov <eugene@chromium.org>
2020-10-26 20:43:02 +0000

    [webcodecs] Enable hardware encoding on CrOS when available
    Change-Id: I6ff42f27427b4ba171b7cb345523f4b1b550a8d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495795
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820892}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -53,7 +53,7 @@ namespace {
 std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
     media::VideoCodecProfile profile,
     const media::VideoEncoder::Options& options) {
-#if defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if defined(OS_MAC) || defined(OS_LINUX)
   // TODO(https://crbug.com/1110279) Flush() is not implemented on MacOS'
   // accelerated video encoder, so we can't use it yet.
   return nullptr;
@@ -97,7 +97,7 @@ std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
       media::AsyncDestroyVideoEncoder<media::VideoEncodeAcceleratorAdapter>>(
       std::make_unique<media::VideoEncodeAcceleratorAdapter>(
           gpu_factories, std::move(task_runner)));
-#endif  // defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#endif  // defined(OS_MAC) || defined(OS_LINUX)
 }
 
 std::unique_ptr<media::VideoEncoder> CreateVpxVideoEncoder() {
