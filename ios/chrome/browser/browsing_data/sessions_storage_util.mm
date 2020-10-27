1977ed691a798ed285e569dcf8c950102e8a425d
Mohammad Refaat <mrefaat@chromium.org>
2020-10-27 02:33:16 +0000

    [Merge 87] Schedule discarded session cleanup on the app initialization.
    the didDiscardSceneSessions callback can be called when the browser
    state is not yet created, or it can be called when the browsing data
    remover service is not available. Once the callback is received mark
    the sessions for a later time removal by saving them on a plist on disk.
    And at the app initialization after the browser state its keyed services
    are initialized. schedule the cleanup for these sessions.
    
    (cherry picked from commit c9818bdf1b958300936ded1ec230416fdec1fe7c)
    
    Bug: 1136761
    Change-Id: Ia5190866d4af7a041f0e5507fb7a88ea0ace9901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490788
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#820022}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499941
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#795}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

---
+++ ios/chrome/browser/browsing_data/sessions_storage_util.mm
@@ -0,0 +1,50 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/browsing_data/sessions_storage_util.h"
+
+#include "base/files/file_path.h"
+#include "base/path_service.h"
+#include "base/strings/sys_string_conversions.h"
+#include "ios/chrome/browser/chrome_paths.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+
+// Returns the path of the list of sessions that are marked for removal.
+NSString* GetDiscardedSessionsFilePath() {
+  base::FilePath directory_path;
+  base::PathService::Get(ios::DIR_USER_DATA, &directory_path);
+  NSString* file_path = base::SysUTF8ToNSString(
+      directory_path.Append(FILE_PATH_LITERAL("DiscardedSessions"))
+          .AsUTF8Unsafe());
+  return file_path;
+}
+
+}  // namespace
+
+namespace sessions_storage_util {
+
+void MarkSessionsForRemoval(NSArray<NSString*>* session_ids) {
+  NSString* file_path = GetDiscardedSessionsFilePath();
+  NSMutableArray* sessions = [NSMutableArray arrayWithContentsOfFile:file_path];
+  if (!sessions)
+    sessions = [[NSMutableArray alloc] init];
+  [sessions addObjectsFromArray:session_ids];
+  [sessions writeToFile:file_path atomically:YES];
+}
+
+NSArray<NSString*>* GetDiscardedSessions() {
+  return [NSArray arrayWithContentsOfFile:GetDiscardedSessionsFilePath()];
+}
+
+void ResetDiscardedSessions() {
+  NSFileManager* file_manager = [[NSFileManager alloc] init];
+  [file_manager removeItemAtPath:GetDiscardedSessionsFilePath() error:nil];
+}
+
+}  // namespace sessions_storage_util
