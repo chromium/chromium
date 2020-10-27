a227fd7b25da40621e30e77cc9f1b48dfa83ab1a
Wojciech Bielawski <wbielawski@fb.com>
2020-10-27 13:04:10 +0000

    Improve jinja2 templates to support build on python3
    Summary: Python3 doesn't support comparison between NoneType objects. Jinja uses sort function within 'groupby' and that leads to
    "TypeError: '<' not supported between instances of 'NoneType' and 'NoneType'" when template tries to group by attribute that is empty.
    This commit adds 'stringifykeygroupby' which mimics jinja's groupby but converts keys to string during sort, so comparison always succeeds.
    
    Bug: 1138872
    Change-Id: If604b4b8c405b5d924c934a04ffc763132b72061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495062
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#821196}

--- third_party/blink/renderer/bindings/scripts/code_generator.py
+++ third_party/blink/renderer/bindings/scripts/code_generator.py
@@ -13,6 +13,7 @@ import re
 import sys
 
 from idl_types import set_ancestors, IdlType
+from itertools import groupby
 from v8_globals import includes
 from v8_interface import constant_filters
 from v8_types import set_component_dirs
@@ -43,6 +44,7 @@ TEMPLATES_DIR = os.path.normpath(
 # after path[0] == invoking script dir
 sys.path.insert(1, THIRD_PARTY_DIR)
 import jinja2
+from jinja2.filters import make_attrgetter, environmentfilter
 
 
 def generate_indented_conditional(code, conditional):
@@ -88,6 +90,13 @@ def runtime_enabled_if(code, name):
     return generate_indented_conditional(code, function)
 
 
+@environmentfilter
+def do_stringify_key_group_by(environment, value, attribute):
+    expr = make_attrgetter(environment, attribute)
+    key = lambda item: '' if expr(item) is None else str(expr(item))
+    return groupby(sorted(value, key=key), expr)
+
+
 def initialize_jinja_env(cache_dir):
     jinja_env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(TEMPLATES_DIR),
@@ -117,6 +126,7 @@ def initialize_jinja_env(cache_dir):
     })
     jinja_env.filters.update(constant_filters())
     jinja_env.filters.update(method_filters())
+    jinja_env.filters["stringifykeygroupby"] = do_stringify_key_group_by
     return jinja_env
 
 
