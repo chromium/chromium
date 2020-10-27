431fbac83ce69685f87ee146221e4ca0361647c0
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 15:44:59 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=corising@chromium.org
    
    Bug: 1142671
    Change-Id: I63425a4accecedcffa4da3e746a0ccf449296336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500611
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821263}

--- chrome/browser/ui/views/find_bar_host.cc
+++ chrome/browser/ui/views/find_bar_host.cc
@@ -116,7 +116,8 @@ bool FindBarHost::MaybeForwardKeyEventToWebpage(
   // input. Otherwise Up and Down arrow key strokes get eaten. "Nom Nom Nom".
   contents->ClearFocusedElement();
   NativeWebKeyboardEvent event(key_event);
-  contents->GetRenderViewHost()
+  contents->GetMainFrame()
+      ->GetRenderViewHost()
       ->GetWidget()
       ->ForwardKeyboardEventWithLatencyInfo(event, *key_event.latency());
   return true;
