3cedc9c7c30c3da71888ce26c94385b6c431aaf4
Lijin Shen <lazzzis@google.com>
2020-10-27 21:43:18 +0000

    Introduce a general swipe listener for android widgets
    Add a new general swipe listener, which is derived from
    SwipeRecognizer.
    
    Bug: 1123947, 1142475
    Change-Id: I1e1518300bd4dbf36b889970cb89981b0ff3a15c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493364
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821448}

---
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/gesture/SwipeGestureListener.java
@@ -0,0 +1,214 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.widget.gesture;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+
+import androidx.annotation.IntDef;
+import androidx.annotation.VisibleForTesting;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.components.browser_ui.widget.R;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Recognizes directional swipe gestures using supplied {@link MotionEvent}s.
+ * The {@link SwipeHandler} callbacks will notify users when a particular gesture
+ * has occurred, if the handler supports the particular direction of the swipe.
+ *
+ * To use this class:
+ * <ul>
+ *  <li>Create an instance of the {@link SwipeGestureListener} for your View
+ *  <li>In the View#onTouchEvent(MotionEvent) method ensure you call
+ *          {@link #onTouchEvent(MotionEvent)}. The methods defined in your callback
+ *          will be executed when the gestures occur.
+ *  <li>Before trying to recognize the gesture, the class will call
+ *          {@link #shouldRecognizeSwipe(MotionEvent, MotionEvent)}, which allows
+ *          ignoring swipe recognition based on the MotionEvents.
+ *  <li>Once a swipe gesture is detected, the class will check if the the direction
+ *          is supported by calling {@link SwipeHandler#isSwipeEnabled}.
+ * </ul>
+
+ * Internally, this class uses a {@link GestureDetector} to recognize swipe gestures.
+ * For convenience, this class also extends {@link SimpleOnGestureListener} which
+ * is passed to the {@link GestureDetector}. This means that this class can also be
+ * used to detect simple gestures defined in {@link GestureDetector}.
+ */
+public class SwipeGestureListener extends SimpleOnGestureListener {
+    @IntDef({ScrollDirection.LEFT, ScrollDirection.RIGHT, ScrollDirection.UP, ScrollDirection.DOWN})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ScrollDirection {
+        int UNKNOWN = 0;
+        int LEFT = 1;
+        int RIGHT = 2;
+        int UP = 3;
+        int DOWN = 4;
+    }
+
+    public interface SwipeHandler {
+        /**
+         * @param direction The {@link ScrollDirection} representing the swipe direction.
+         * @param ev The first down motion event triggering the swipe.
+         */
+        void onSwipeStarted(@ScrollDirection int direction, MotionEvent ev);
+
+        /**
+         * @param start The first down motion event triggering the swipe.
+         * @param current The move motion event triggering the current swipe.
+         */
+        void onSwipeUpdated(MotionEvent start, MotionEvent current);
+
+        /**
+         * @param end The last motion event canceling the swipe.
+         */
+        void onSwipeFinished(MotionEvent end);
+
+        /**
+         * @param direction The {@link ScrollDirection} representing the swipe direction.
+         * @param start The first down motion event triggering the swipe.
+         * @param end The last motion event canceling the swipe.
+         */
+        void onFling(@ScrollDirection int direction, MotionEvent start, MotionEvent end);
+
+        /**
+         * @param direction The direction of the on-going swipe.
+         * @return False if this direction should be ignored.
+         */
+        boolean isSwipeEnabled(@ScrollDirection int direction);
+    }
+
+    /**
+     * The internal {@link GestureDetector} used to recognize swipe gestures.
+     */
+    private final GestureDetector mGestureDetector;
+    @ScrollDirection
+    private int mDirection = ScrollDirection.UNKNOWN;
+    private final SwipeHandler mHandler;
+
+    /**
+     * The threshold for a vertical swipe gesture, in px.
+     */
+    private final int mSwipeVerticalDragThreshold;
+
+    /**
+     * The threshold for a horizontal swipe gesture, in px.
+     */
+    private final int mSwipeHorizontalDragThreshold;
+
+    /**
+     * @param context The {@link Context}.
+     * @param handler The {@link SwipeHandler} to handle the swipe events.
+     */
+    public SwipeGestureListener(Context context, SwipeHandler handler) {
+        mGestureDetector = new GestureDetector(context, this, ThreadUtils.getUiThreadHandler());
+        mSwipeVerticalDragThreshold = context.getResources().getDimensionPixelOffset(
+                R.dimen.swipe_vertical_drag_threshold);
+        mSwipeHorizontalDragThreshold = context.getResources().getDimensionPixelOffset(
+                R.dimen.swipe_horizontal_drag_threshold);
+        mHandler = handler;
+    }
+
+    @VisibleForTesting
+    SwipeGestureListener(
+            Context context, SwipeHandler handler, int verticalThreshold, int horizontalThreshold) {
+        mGestureDetector = new GestureDetector(context, this, ThreadUtils.getUiThreadHandler());
+        mSwipeVerticalDragThreshold = verticalThreshold;
+        mSwipeHorizontalDragThreshold = horizontalThreshold;
+        mHandler = handler;
+    }
+
+    /**
+     * Analyzes the given motion event by feeding it to a {@link GestureDetector}. Depending on the
+     * results from the onScroll() and onFling() methods, it triggers the appropriate callbacks
+     * on the {@link SwipeHandler} supplied.
+     *
+     * @param event The {@link MotionEvent}.
+     * @return Whether the event has been consumed.
+     */
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean consumed = mGestureDetector.onTouchEvent(event);
+
+        if (mHandler != null) {
+            final int action = event.getAction();
+            if ((action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL)
+                    && mDirection != ScrollDirection.UNKNOWN) {
+                mHandler.onSwipeFinished(event);
+                mDirection = ScrollDirection.UNKNOWN;
+                consumed = true;
+            }
+        }
+
+        return consumed;
+    }
+
+    /**
+     * Checks whether the swipe gestures should be recognized. If this method returns false,
+     * then the whole swipe recognition process will be ignored. By default this method returns
+     * true. If a more complex logic is needed, this method should be overridden.
+     *
+     * @param e1 The first {@link MotionEvent}.
+     * @param e2 The second {@link MotionEvent}.
+     * @return Whether the swipe gestures should be recognized
+     */
+    public boolean shouldRecognizeSwipe(MotionEvent e1, MotionEvent e2) {
+        return true;
+    }
+
+    // ============================================================================================
+    // Swipe Recognition Helpers
+    // ============================================================================================
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        if (mHandler == null || e1 == null || e2 == null) return false;
+
+        if (mDirection == ScrollDirection.UNKNOWN && shouldRecognizeSwipe(e1, e2)) {
+            float tx = (e2.getRawX() - e1.getRawX());
+            float ty = (e2.getRawY() - e1.getRawY());
+
+            @ScrollDirection
+            int direction = ScrollDirection.UNKNOWN;
+
+            if (Math.abs(tx) < mSwipeHorizontalDragThreshold
+                    && Math.abs(ty) < mSwipeVerticalDragThreshold) {
+                return false;
+            }
+            if (Math.abs(tx) > Math.abs(ty)) {
+                direction = tx > 0.f ? ScrollDirection.RIGHT : ScrollDirection.LEFT;
+            } else {
+                direction = ty > 0.f ? ScrollDirection.DOWN : ScrollDirection.UP;
+            }
+
+            if (direction != ScrollDirection.UNKNOWN && mHandler.isSwipeEnabled(direction)) {
+                mDirection = direction;
+                mHandler.onSwipeStarted(direction, e1);
+            }
+        }
+
+        if (mDirection != ScrollDirection.UNKNOWN) {
+            mHandler.onSwipeUpdated(e1, e2);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (mHandler == null) return false;
+
+        if (mDirection != ScrollDirection.UNKNOWN) {
+            mHandler.onFling(mDirection, e1, e2);
+            return true;
+        }
+
+        return false;
+    }
+}
