45a1ccf97a81d1b66bd9660ac1c404960158af13
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 04:08:43 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=sadrul@chromium.org
    
    Bug: 1142671
    Change-Id: Idf1a6a3c792f968c39a3f22eea057d58b35f6c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501126
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821067}

--- content/browser/renderer_host/input/event_latency_aura_browsertest.cc
+++ content/browser/renderer_host/input/event_latency_aura_browsertest.cc
@@ -28,8 +28,11 @@ class EventLatencyBrowserTest : public ContentBrowserTest {
 
  protected:
   RenderWidgetHostImpl* GetWidgetHost() {
-    return RenderWidgetHostImpl::From(
-        shell()->web_contents()->GetRenderViewHost()->GetWidget());
+    return RenderWidgetHostImpl::From(shell()
+                                          ->web_contents()
+                                          ->GetMainFrame()
+                                          ->GetRenderViewHost()
+                                          ->GetWidget());
   }
 
   // Starts the test server and navigates to the test page. Returns after the
