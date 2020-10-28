ec9ffdf55d7b6e104d17b7fe2be25302752b2185
Billy Zhao <billyzhao@google.com>
2020-10-28 17:04:28 +0000

    Enable kSystemBluetoothAdapterEnabled to be toggled by settingsPrivate
    It was not possible to change the login screen profile's default
    bluetooth preference through settingsPrivate. This cl enables that
    behavior.
    
    Bug: b:161902990
    Change-Id: Ib0f0f798994182a19ee01562d7d5899667b58525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500445
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Billy Zhao <billyzhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821759}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -571,6 +571,8 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_allowlist)[::ash::prefs::kUserBluetoothAdapterEnabled] =
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
+  (*s_allowlist)[::ash::prefs::kSystemBluetoothAdapterEnabled] =
+      settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_allowlist)[::ash::prefs::kVpnConfigAllowed] =
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_allowlist)[arc::prefs::kAlwaysOnVpnPackage] =
