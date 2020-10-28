1e85a0245ad1780ed953228e512f6a1e06a4f2ad
Gayane Petrosyan <gayane@chromium.org>
2020-10-28 21:51:34 +0000

    [Merge-87][SH] Don't call TextFragmentSelectorGenerator for iframes.
    TextFragmentSelectorGenerator gets initialized only for main frame
    as link to text is supported only for those. Therefore, clients
    shouldn't call TextFragmentSelectorGenerator for iframes.
    
    Also adding a DCHECK that selection happened on the same frame as remote
    interface is being bind to.
    
    (cherry picked from commit bc09299e356a2a5f3914dc7627a4e4c0ee9cd834)
    
    Bug: 1139864
    Change-Id: Ibe4b03e04ef995ebb000a0488ffaa64e378bf20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487703
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#821209}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506233
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280@{#871}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- chrome/browser/renderer_context_menu/copy_link_to_text_menu_observer_interactive_uitest.cc
+++ chrome/browser/renderer_context_menu/copy_link_to_text_menu_observer_interactive_uitest.cc
@@ -9,9 +9,12 @@
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/renderer_context_menu/mock_render_view_context_menu.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/test/browser_test.h"
+#include "net/dns/mock_host_resolver.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/clipboard/clipboard.h"
 
@@ -22,7 +25,16 @@ class CopyLinkToTextMenuObserverTest : public InProcessBrowserTest {
   CopyLinkToTextMenuObserverTest();
 
   void SetUp() override { InProcessBrowserTest::SetUp(); }
-  void SetUpOnMainThread() override { Reset(false); }
+  void SetUpOnMainThread() override {
+    Reset(false);
+
+    host_resolver()->AddRule("*", "127.0.0.1");
+
+    // Add content/test/data for cross_site_iframe_factory.html
+    embedded_test_server()->ServeFilesFromSourceDirectory("content/test/data");
+
+    ASSERT_TRUE(embedded_test_server()->Start());
+  }
   void TearDownOnMainThread() override {
     observer_.reset();
     menu_.reset();
@@ -112,4 +124,34 @@ IN_PROC_BROWSER_TEST_F(CopyLinkToTextMenuObserverTest, ReplacesRefInURL) {
   clipboard->ReadText(ui::ClipboardBuffer::kCopyPaste, nullptr, &text);
   EXPECT_EQ(base::UTF8ToUTF16("\"hello world\"\nhttp://foo.com/#:~:text=hello"),
             text);
+}
+
+// crbug.com/1139864
+IN_PROC_BROWSER_TEST_F(CopyLinkToTextMenuObserverTest,
+                       InvalidSelectorForIframe) {
+  GURL main_url(
+      embedded_test_server()->GetURL("a.com", "/page_with_iframe.html"));
+
+  ui_test_utils::NavigateToURL(browser(), main_url);
+
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  content::RenderFrameHost* main_frame_a = web_contents->GetMainFrame();
+  content::RenderFrameHost* child_frame_b = ChildFrameAt(main_frame_a, 0);
+  EXPECT_TRUE(ExecuteScript(child_frame_b, "window.focus();"));
+  EXPECT_EQ(child_frame_b, web_contents->GetFocusedFrame());
+
+  menu()->set_web_contents(web_contents);
+
+  content::BrowserTestClipboardScope test_clipboard_scope;
+  content::ContextMenuParams params;
+  params.page_url = main_url;
+  params.selection_text = base::UTF8ToUTF16("hello world");
+  InitMenu(params);
+  menu()->ExecuteCommand(IDC_CONTENT_CONTEXT_COPYLINKTOTEXT, 0);
+
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  base::string16 text;
+  clipboard->ReadText(ui::ClipboardBuffer::kCopyPaste, nullptr, &text);
+  EXPECT_EQ(base::UTF8ToUTF16("\"hello world\"\n" + main_url.spec()), text);
 }
