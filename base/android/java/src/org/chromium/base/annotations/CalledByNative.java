4d3bc92cf9b9d9b0249be3a18691261a9aaff8ee
Nate Fischer <ntfschr@chromium.org>
2020-10-28 16:11:20 +0000

    Docs: update javadoc for @CalledByNative/Unchecked
    No change to logic, only docs. This updates the Javadoc for
    @CalledByNative and @CalledByNativeUnchecked to clarify the difference
    and how each of them behave with respect to Java exceptions. This info
    is based on an offline conversation with torne@.
    
    Test: N/A
    Change-Id: I4fe7d14d10d5f35abb8db6d6fffe9b680613a2b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504406
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821737}

--- base/android/java/src/org/chromium/base/annotations/CalledByNative.java
+++ base/android/java/src/org/chromium/base/annotations/CalledByNative.java
@@ -10,8 +10,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @CalledByNative is used by the JNI generator to create the necessary JNI
- * bindings and expose this method to native code.
+ * Used by the JNI generator to create the necessary JNI bindings and expose this method to native
+ * code.
+ *
+ * <p>Any uncaught Java exceptions will crash the current process. This is generally the desired
+ * behavior, since most exceptions indicate an unexpected error. If your java method expects an
+ * exception, we recommend refactoring to catch exceptions and indicate errors with special return
+ * values instead. If this is not possible, see {@link CalledByNativeUnchecked} instead.
  */
 @Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
 @Retention(RetentionPolicy.CLASS)
