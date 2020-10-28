dca3ad119615ff27d4897c8154fb752546ac419b
Jeremy Roman <jbroman@chromium.org>
2020-10-28 00:31:09 +0000

    Support parsing Supports-Loading-Mode header from string and response headers.
    It must be a list of simple tokens from a known list, formatted as an
    HTTP structured header list. All unknown items are ignored, including
    parameterized items, inner lists, and non-tokens.
    
    Unit tests included.
    
    This is the "parser" described in:
      https://docs.google.com/document/d/1IpqYg1Y4BX9U9LBELNUAaKlygFvOKo6MAmS8nXMLVlE/edit
    
    Bug: 1141083
    Change-Id: I914220bf6110c789c921965b65100823b9a11df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2492882
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821522}

---
+++ services/network/public/cpp/supports_loading_mode/README.md
@@ -0,0 +1,2 @@
+This is a parser for the Supports-Loading-Mode header, described here:
+  https://github.com/jeremyroman/alternate-loading-modes/blob/gh-pages/opt-in.md
---
+++ services/network/public/cpp/supports_loading_mode/supports_loading_mode_parser.cc
@@ -0,0 +1,78 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/supports_loading_mode/supports_loading_mode_parser.h"
+
+#include "base/optional.h"
+#include "base/ranges/algorithm.h"
+#include "base/strings/string_piece.h"
+#include "net/http/http_response_headers.h"
+#include "net/http/structured_headers.h"
+#include "services/network/public/mojom/supports_loading_mode.mojom.h"
+
+namespace network {
+
+namespace {
+
+constexpr base::StringPiece kSupportsLoadingMode = "Supports-Loading-Mode";
+constexpr struct KnownLoadingMode {
+  base::StringPiece token;
+  mojom::LoadingMode enumerator;
+} kKnownLoadingModes[] = {
+    {"default", mojom::LoadingMode::kDefault},
+    {"uncredentialed-prefetch", mojom::LoadingMode::kUncredentialedPrefetch},
+    {"uncredentialed-prerender", mojom::LoadingMode::kUncredentialedPrerender},
+};
+
+}  // namespace
+
+mojom::SupportsLoadingModePtr ParseSupportsLoadingMode(
+    base::StringPiece header_value) {
+  // A parse error in the HTTP structured headers syntax is a parse error for
+  // the header value as a whole.
+  auto list = net::structured_headers::ParseList(header_value);
+  if (!list)
+    return nullptr;
+
+  // The default loading mode is assumed to be supported.
+  std::vector<mojom::LoadingMode> modes{mojom::LoadingMode::kDefault};
+
+  for (const net::structured_headers::ParameterizedMember& member : *list) {
+    // No supported mode currently is specified as an inner list or takes
+    // parameters.
+    if (member.member_is_inner_list || !member.params.empty())
+      continue;
+
+    // All supported modes are tokens.
+    const net::structured_headers::ParameterizedItem& item = member.member[0];
+    DCHECK(item.params.empty());
+    if (!item.item.is_token())
+      continue;
+
+    // Each supported token maps 1:1 to an enumerator.
+    const auto& token = item.item.GetString();
+    const auto* it =
+        base::ranges::find(kKnownLoadingModes, token, &KnownLoadingMode::token);
+    if (it == base::ranges::end(kKnownLoadingModes))
+      continue;
+
+    modes.push_back(it->enumerator);
+  }
+
+  // Order and repetition are not significant.
+  // Canonicalize by making the vector sorted and unique.
+  base::ranges::sort(modes);
+  modes.erase(base::ranges::unique(modes), modes.end());
+  return mojom::SupportsLoadingMode::New(std::move(modes));
+}
+
+mojom::SupportsLoadingModePtr ParseSupportsLoadingMode(
+    const net::HttpResponseHeaders& headers) {
+  std::string header_value;
+  if (!headers.GetNormalizedHeader(kSupportsLoadingMode, &header_value))
+    return nullptr;
+  return ParseSupportsLoadingMode(header_value);
+}
+
+}  // namespace network
