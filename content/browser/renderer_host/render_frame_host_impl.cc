be0013d425828aa8794925ca07b13e627ca138c9
arthursonzogni <arthursonzogni@chromium.org>
2020-10-28 19:52:49 +0000

    [OutOfBlinkSandbox] Reenable the DumpWithoutCrashing.
    Those DumpWithoutCrashing have been removed twice.
    
    A few potential issue have been solved recently:
    - [ignore-csp-sandbox-report-only]
    - [sandbox-vs-mhtml]
    - [non-optional-frame-policy]
    
    This patch adds them again. I would like to know if they would still be
    triggered into the wild.
    
    -------
    
    [ignore-csp-sandbox-report-only]:
    https://chromium-review.googlesource.com/c/chromium/src/+/2498605
    
    [sandbox-vs-mhtml]:
    https://docs.google.com/document/d/13jcj_mATRxKakPEGy72YYHZBTBkkNrAHzaNIIjVsC8I
    https://chromium-review.googlesource.com/c/chromium/src/+/2450270
    https://chromium-review.googlesource.com/c/chromium/src/+/2467917
    https://chromium-review.googlesource.com/c/chromium/src/+/2480083
    https://chromium-review.googlesource.com/c/chromium/src/+/2479365
    https://chromium-review.googlesource.com/c/chromium/src/+/2467917
    
    [non-optional-frame-policy]:
    https://chromium-review.googlesource.com/c/chromium/src/+/2460890
    
    Bug: chromium:1041376
    Change-Id: Ie503453ed163b01643a90122b61fa2567ec7660f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2502930
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821856}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9721,6 +9721,17 @@ void RenderFrameHostImpl::CheckSandboxFlags() {
     return;
 
   DCHECK(false);
+
+  base::debug::ScopedCrashKeyString scoped_url(
+      base::debug::AllocateCrashKeyString("url",
+                                          base::debug::CrashKeySize::Size256),
+      GetLastCommittedURL().possibly_invalid_spec());
+  base::debug::ScopedCrashKeyString scoped_sandbox(
+      base::debug::AllocateCrashKeyString("sandbox",
+                                          base::debug::CrashKeySize::Size256),
+      base::StringPrintf("%u, %u", uint32_t(active_sandbox_flags_),
+                         uint32_t(*active_sandbox_flags_control_)));
+  base::debug::DumpWithoutCrashing();
 }
 
 void RenderFrameHostImpl::SetEmbeddingToken(
