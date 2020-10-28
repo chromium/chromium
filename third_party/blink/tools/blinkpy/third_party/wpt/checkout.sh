4f5e92a4fec7ff1b56bc0881972b5ce21556efd4
Stephen McGruer <smcgruer@chromium.org>
2020-10-28 20:39:10 +0000

    [blinkpy] Roll WPT tools to 326f137f2c3d23cd72b7163707def783f0a1e15b
    Bug: None
    Change-Id: I35d4c865e69968d53ceb73d248ca1bbdf952a288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506112
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821877}

--- third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
+++ third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 TARGET_DIR=$DIR/wpt
 REMOTE_REPO="https://github.com/web-platform-tests/wpt.git"
-WPT_HEAD=8b1df3d520335fb42ef4bfd50b9121ac41995f8e
+WPT_HEAD=326f137f2c3d23cd72b7163707def783f0a1e15b
 
 function clone {
   # Remove existing repo if already exists.
