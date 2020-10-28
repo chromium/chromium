3a73a21fcc991d1249d59a2c9c3b362ffb5ed549
Gavin Williams <gavinwill@chromium.org>
2020-10-28 19:15:51 +0000

    scanning: Add browser proxy
    Add browser proxy to give Scanning UI access to ScanningHandler
    functions.
    
    Bug: 1059779
    Change-Id: Iad0f72305ae315c3079393ff077c784ec9b8f101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2497427
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821831}

---
+++ chromeos/components/scanning/resources/scanning_browser_proxy.js
@@ -0,0 +1,40 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview A helper object used by the Scanning App UI in chromeos/ to
+ * provide access to the ScanningHandler which invokes functions that only exist
+ * in chrome/.
+ */
+
+import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+
+/** @interface */
+class ScanningBrowserProxy {
+  /** Initialize ScanningHandler. */
+  initialize() {}
+
+  /**
+   * Requests the user to choose the directory to save scans.
+   * @return {!Promise<string>}
+   */
+  requestScanToLocation() {}
+}
+
+/** @implements {ScanningBrowserProxy} */
+export class ScanningBrowserProxyImpl {
+  /** @override */
+  initialize() {
+    chrome.send('initialize');
+  }
+
+  /** @override */
+  requestScanToLocation() {
+    return sendWithPromise('requestScanToLocation');
+  }
+}
+
+// The singleton instance_ can be replaced with a test version of this wrapper
+// during testing.
+addSingletonGetter(ScanningBrowserProxyImpl);
