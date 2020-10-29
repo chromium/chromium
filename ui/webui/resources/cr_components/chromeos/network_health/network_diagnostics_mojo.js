50f457b5d8c53012da7f95d8b6214776f7000faa
Trent Begin <tbegin@google.com>
2020-10-29 17:52:56 +0000

    Make network diagnostics mojo include compatible with polymer 3
    The network diagnostics mojo is now a separate html file instead of
    being included directly. This allows the polymer 3 modulizes to
    correctly convert the include into a module.
    
    Bug: chromium:1140659
    Change-Id: Idc4dc7c1a23b0a9897742289c311c07e0507fc91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490349
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822253}

---
+++ ui/webui/resources/cr_components/chromeos/network_health/network_diagnostics_mojo.js
@@ -0,0 +1,15 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// clang-format off
+// #import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+// #import 'chrome://resources/mojo/chromeos/services/network_health/public/mojom/network_diagnostics.mojom-lite.js';
+// clang-format on
+
+/**
+ * @fileoverview Wrapper around Network Diagnostics mojom file and associated
+ * utilities.
+ * TODO(crbug/1111852): Remove this wrapper once Polymer2 no longer needs to be
+ * supported. These imports can be used directly.
+ */
