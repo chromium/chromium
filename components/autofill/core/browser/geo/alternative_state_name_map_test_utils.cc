ab05003771b76cb05c15d67c0873864fc73432ed
Vidhan Jain <vidhanj@google.com>
2020-10-29 17:58:18 +0000

    Revert "[Autofill][States] Implemented StateNameMapUpdater"
    This reverts commit bd6beb55638a8d94521e7c622bafae1d7d0eb3b0.
    
    Reason for revert: Open comments on CL
    
    Original change's description:
    > [Autofill][States] Implemented StateNameMapUpdater
    >
    > StateNameMapUpdater is a helper class for StateNameMapUpdater
    > that reads the data from the state data files and loads them to the
    > map.
    >
    > Bug: 1111960
    > Change-Id: I8add7215a1ec450e0a31ef999b2adfa6dade138b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471337
    > Reviewed-by: Matthias Körber <koerber@google.com>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Commit-Queue: Vidhan Jain <vidhanj@google.com>
    > Cr-Commit-Position: refs/heads/master@{#822187}
    
    TBR=battre@chromium.org,koerber@google.com,schwering@google.com,vidhanj@google.com
    
    Change-Id: I5a47671505371abfccfd5ba5ebbe43002e2db4f3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1111960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507751
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/master@{#822257}

--- components/autofill/core/browser/geo/alternative_state_name_map_test_utils.cc
+++ components/autofill/core/browser/geo/alternative_state_name_map_test_utils.cc
@@ -11,8 +11,8 @@ namespace autofill {
 
 namespace test {
 
-void PopulateStateEntry(const TestStateEntry& test_state_entry,
-                        StateEntry* state_entry) {
+void CreateFakeStateEntry(const TestStateEntry& test_state_entry,
+                          StateEntry* state_entry) {
   state_entry->set_canonical_name(test_state_entry.canonical_name);
   for (const auto& abbr : test_state_entry.abbreviations)
     state_entry->add_abbreviations(abbr);
@@ -31,7 +31,7 @@ void PopulateAlternativeStateNameMapForTesting(
     std::vector<TestStateEntry> test_state_entries) {
   for (const auto& test_state_entry : test_state_entries) {
     StateEntry state_entry;
-    PopulateStateEntry(test_state_entry, &state_entry);
+    CreateFakeStateEntry(test_state_entry, &state_entry);
     std::vector<AlternativeStateNameMap::StateName> alternatives;
     AlternativeStateNameMap::CanonicalStateName canonical_state_name =
         AlternativeStateNameMap::CanonicalStateName(
@@ -50,17 +50,6 @@ void PopulateAlternativeStateNameMapForTesting(
         AlternativeStateNameMap::StateName(base::ASCIIToUTF16(key)),
         state_entry, alternatives, &canonical_state_name);
   }
-}
-
-std::string CreateStatesProtoAsString(std::string country_code) {
-  StatesInCountry states_data;
-  states_data.set_country_code(std::move(country_code));
-  StateEntry* entry = states_data.add_states();
-  PopulateStateEntry(TestStateEntry(), entry);
-
-  std::string serialized_output;
-  DCHECK(states_data.SerializeToString(&serialized_output));
-  return serialized_output;
 }
 
 }  // namespace test
