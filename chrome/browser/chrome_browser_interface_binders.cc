d9b6a42361787a21be68124958cc1e4987875de3
Bailey Berro <baileyberro@google.com>
2020-10-29 17:18:31 +0000

    Add interface binder for chromeos::diagnostics::mojom::SystemDataProvider
    Registers an a WebUIControllerInterfaceBinder for SystemDataProvider.
    
    
    Bug: 1128204
    Change-Id: I4a35ec87d4e6e841a6d0e94eca7bfdb5f61cf832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2496189
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822232}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -174,6 +174,8 @@
 #include "chrome/browser/ui/webui/settings/chromeos/search/user_action_recorder.mojom.h"
 #include "chromeos/components/camera_app_ui/camera_app_helper.mojom.h"
 #include "chromeos/components/camera_app_ui/camera_app_ui.h"
+#include "chromeos/components/diagnostics_ui/diagnostics_ui.h"
+#include "chromeos/components/diagnostics_ui/mojom/system_data_provider.mojom.h"
 #include "chromeos/components/help_app_ui/help_app_ui.h"
 #include "chromeos/components/help_app_ui/help_app_ui.mojom.h"
 #include "chromeos/components/local_search_service/mojom/local_search_service_proxy.mojom.h"
@@ -741,6 +743,10 @@ void PopulateChromeWebUIFrameBinders(
   RegisterWebUIControllerInterfaceBinder<
       chromeos::network_diagnostics::mojom::NetworkDiagnosticsRoutines,
       chromeos::NetworkUI>(map);
+
+  RegisterWebUIControllerInterfaceBinder<
+      chromeos::diagnostics::mojom::SystemDataProvider,
+      chromeos::DiagnosticsUI>(map);
 
   RegisterWebUIControllerInterfaceBinder<chromeos::scanning::mojom::ScanService,
                                          chromeos::ScanningUI>(map);
