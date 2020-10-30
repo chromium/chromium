9dd6ad341eb71146912c757c3eb6dbd463261e33
Salvador Guerrero <salg@google.com>
2020-10-30 19:11:23 +0000

    Disable flaky WebViewLayoutTests
    This CL disables the following tests:
    * WebViewLayoutTest#testWebViewIncludedStableInterfaces
    * WebViewLayoutTest#testGlobalInterfaceNoFail
    * WebViewLayoutTest#testWebViewExcludedInterfaces
    
    Because they are flaky on builder:
    * android-pie-x86-rel
    
    TBR=timvolodine@chromium.org
    
    Bug: 1144241
    Change-Id: I15402e2a0169488c9b7500e3bf938ccb66af9ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510909
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#822749}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -110,6 +110,7 @@ public class WebViewLayoutTest {
 
     // This is a non-failing test because it tends to require frequent rebaselines.
     @Test
+    @DisabledTest(message = "https://crbug.com/1144241")
     @MediumTest
     public void testGlobalInterfaceNoFail() throws Exception {
         runBlinkLayoutTest("webexposed/global-interface-listing.html",
@@ -147,6 +148,7 @@ public class WebViewLayoutTest {
     }
 
     @Test
+    @DisabledTest(message = "https://crbug.com/1144241")
     @MediumTest
     public void testWebViewExcludedInterfaces() throws Exception {
         ensureJsTestCopied();
@@ -193,6 +195,7 @@ public class WebViewLayoutTest {
     }
 
     @Test
+    @DisabledTest(message = "https://crbug.com/1144241")
     @MediumTest
     public void testWebViewIncludedStableInterfaces() throws Exception {
         ensureJsTestCopied();
