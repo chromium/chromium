1a7f6f24f0c40edc1968f74c2dcfe548afcacab3
Darin Fisher <darin@chromium.org>
2020-10-30 00:43:52 +0000

    [Lacros] Make scrollbar theme match Chrome OS behavior
    Bug: 1141261
    Change-Id: I73710b07c82ebd8bfe76bea23251df505c5cc300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508850
    Commit-Queue: Darin Fisher <darin@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822454}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_aura.cc
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_aura.cc
@@ -31,7 +31,6 @@
 #include "third_party/blink/renderer/core/scroll/scrollbar_theme_aura.h"
 
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "cc/input/scrollbar.h"
 #include "third_party/blink/public/common/input/web_mouse_event.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -138,7 +137,7 @@ bool ScrollbarThemeAura::SupportsDragSnapBack() const {
 // Disable snapback on desktop Linux to better integrate with the desktop
 // behavior. Typically, Linux apps do not implement scrollbar snapback (this
 // is true for at least GTK and QT apps).
-#if (defined(OS_LINUX) || BUILDFLAG(IS_LACROS))
+#if defined(OS_LINUX)
   return false;
 #else
   return true;
@@ -304,7 +303,7 @@ ScrollbarPart ScrollbarThemeAura::PartsToInvalidateOnThumbPositionChange(
 
 bool ScrollbarThemeAura::ShouldCenterOnThumb(const Scrollbar& scrollbar,
                                              const WebMouseEvent& event) {
-#if (defined(OS_LINUX) || BUILDFLAG(IS_LACROS))
+#if defined(OS_LINUX)
   if (event.button == WebPointerProperties::Button::kMiddle)
     return true;
 #endif
