7a88939a92f722fe52b38ec4c458b3ea57de810c
Joe Downing <joedow@chromium.org>
2020-10-30 23:15:10 +0000

    [Windows][Host][Logging] Adding EtwTraceController class
    This is the first CL needed to allow the Windows host to log w/o
    logman or sawbuck.  Each process on Windows create an ETW trace
    provider with a well-known GUID, this is what logman/sawbuck use
    to capture a trace.
    
    With this feature, we will update the Windows host so that its log
    output can be redirected (likely to the Windows event log, could
    also be a human readable file or ETW file).
    
    In this CL, I am adding the class which is used to enable/disable
    ETW tracing.  Future CLs will add the ETW consumer which listens
    for the events and the logic needed to parse and redirect the
    output to a new location.
    
    Bug: 1144185
    Change-Id: I01affb28c01d01bde8649faffaf59a17c8a9cd67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509948
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@google.com>
    Cr-Commit-Position: refs/heads/master@{#822868}

--- remoting/host/logging.h
+++ remoting/host/logging.h
@@ -6,8 +6,22 @@
 #define REMOTING_HOST_LOGGING_H_
 
 #include "base/logging.h"
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include <guiddef.h>
+#endif
 
 namespace remoting {
+
+#if defined(OS_WIN)
+// {2db51ca1-4fd8-4b88-b5a2-fb8606b66b02}
+constexpr GUID kRemotingHostLogProviderGuid = {
+    0x2db51ca1,
+    0x4fd8,
+    0x4b88,
+    {0xb5, 0xa2, 0xfb, 0x86, 0x06, 0xb6, 0x6b, 0x02}};
+#endif
 
 // Initializes host logging.
 extern void InitHostLogging();
--- remoting/host/logging_win.cc
+++ remoting/host/logging_win.cc
@@ -9,11 +9,6 @@
 #include "base/logging.h"
 #include "base/logging_win.h"
 
-// {2db51ca1-4fd8-4b88-b5a2-fb8606b66b02}
-const GUID kRemotingHostLogProvider =
-    { 0x2db51ca1, 0x4fd8, 0x4b88,
-        { 0xb5, 0xa2, 0xfb, 0x86, 0x06, 0xb6, 0x6b, 0x02 } };
-
 namespace remoting {
 
 void InitHostLogging() {
@@ -24,7 +19,7 @@ void InitHostLogging() {
   logging::InitLogging(settings);
 
   // Enable trace control and transport through event tracing for Windows.
-  logging::LogEventProvider::Initialize(kRemotingHostLogProvider);
+  logging::LogEventProvider::Initialize(kRemotingHostLogProviderGuid);
 }
 
 }  // namespace remoting
