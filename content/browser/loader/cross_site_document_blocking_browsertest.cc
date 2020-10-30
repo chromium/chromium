57f34d163318b0992b5ef671dad7249f2b8cdd44
Lukasz Anforowicz <lukasza@chromium.org>
2020-10-30 17:15:57 +0000

    Re-enabling previously-flaky CrossSiteDocument*.BlockForVariousTargets
    This CL re-enables CrossSiteDocumentBlockingTest.BlockForVariousTargets
    because we speculate (as explained in https://crbug.com/1075663#c15)
    that the root cause of the flakiness was unrelated to the test and has
    been fixed by r792326 which fixed raciness in NestedMessagePumpAndroid
    in July.
    
    Fixed: 1075663
    Change-Id: Ie370c65671d9d498056ffb95c4e75e8940e8d1a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508910
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822689}

--- content/browser/loader/cross_site_document_blocking_browsertest.cc
+++ content/browser/loader/cross_site_document_blocking_browsertest.cc
@@ -770,14 +770,7 @@ IN_PROC_BROWSER_TEST_P(CrossSiteDocumentBlockingTest, BackToAboutBlank) {
   }
 }
 
-#if defined(OS_ANDROID)
-// Test is flaky on Android, see crbug.com/1075663
-#define MAYBE_BlockForVariousTargets DISABLED_BlockForVariousTargets
-#else
-#define MAYBE_BlockForVariousTargets BlockForVariousTargets
-#endif
-IN_PROC_BROWSER_TEST_P(CrossSiteDocumentBlockingTest,
-                       MAYBE_BlockForVariousTargets) {
+IN_PROC_BROWSER_TEST_P(CrossSiteDocumentBlockingTest, BlockForVariousTargets) {
   // This webpage loads a cross-site HTML page in different targets such as
   // <img>,<link>,<embed>, etc. Since the requested document is blocked, and one
   // character string (' ') is returned instead, this tests that the renderer
