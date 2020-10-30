10f35fc5eafc7ec9854be64260e142589f430f20
Salvador Guerrero <salg@google.com>
2020-10-30 20:43:07 +0000

    Disabled flaky ToolbarButtonIphTest
    This CL Disables the following test:
    * ToolbarButtonIphTest#testTabSwitcherButtonIph
    
    Because it's flaky on the following builder:
    * android-pie-x86-rel
    
    TBR=mdjones@chromium.org
    
    Bug: 1144263
    Change-Id: I3d935f8e0ce12c09222f60e6ac28214c0c93cfca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510873
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#822801}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarButtonIphTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarButtonIphTest.java
@@ -34,6 +34,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
@@ -120,6 +121,7 @@ public class ToolbarButtonIphTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1144263")
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     public void testTabSwitcherButtonIph() {
         when(mTracker.shouldTriggerHelpUI(FeatureConstants.TAB_SWITCHER_BUTTON_FEATURE))
