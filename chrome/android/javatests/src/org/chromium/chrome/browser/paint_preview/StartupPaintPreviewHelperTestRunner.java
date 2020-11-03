7b05610696b1b459a4ade116d2abdaf683ac429d
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-11-03 18:43:28 +0000

    [Paint Preview] Add end-to-end browser integration tests
    This adds a suite of new tests that verify capture on hide and display
    on startup. These tests don't override any test only behavior and fully
    rely on the prod behavior.
    
    Bug: 1143722
    Change-Id: Iccde9b097b8cd0217bd74588599c66063c7cec21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514725
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823634}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/paint_preview/StartupPaintPreviewHelperTestRunner.java
@@ -0,0 +1,56 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.paint_preview;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.test.InstrumentationRegistry;
+import android.text.TextUtils;
+
+import org.junit.runners.model.InitializationError;
+
+import org.chromium.base.test.util.RestrictionSkipCheck;
+import org.chromium.base.test.util.SkipCheck;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Test runner class that handles a custom {@link Restriction} type.
+ */
+public class StartupPaintPreviewHelperTestRunner extends ChromeJUnit4ClassRunner {
+    /**
+     * The test is only valid if Settings.Global.ALWAYS_FINISH_ACTIVITIES on device settings is
+     * disabled.
+     */
+    public static final String RESTRICTION_TYPE_KEEP_ACTIVITIES = "Keep_Activities";
+
+    public StartupPaintPreviewHelperTestRunner(Class<?> klass) throws InitializationError {
+        super(klass);
+    }
+
+    @Override
+    protected List<SkipCheck> getSkipChecks() {
+        return addToList(super.getSkipChecks(),
+                new StartupPaintPreviewSkipCheck(InstrumentationRegistry.getTargetContext()));
+    }
+
+    private static class StartupPaintPreviewSkipCheck extends RestrictionSkipCheck {
+        public StartupPaintPreviewSkipCheck(Context targetContext) {
+            super(targetContext);
+        }
+
+        @Override
+        protected boolean restrictionApplies(String restriction) {
+            if (TextUtils.equals(restriction, RESTRICTION_TYPE_KEEP_ACTIVITIES)) {
+                int alwaysFinishActivities =
+                        Settings.System.getInt(getTargetContext().getContentResolver(),
+                                Settings.Global.ALWAYS_FINISH_ACTIVITIES, 0);
+                return alwaysFinishActivities != 0;
+            }
+            return super.restrictionApplies(restriction);
+        }
+    }
+}
