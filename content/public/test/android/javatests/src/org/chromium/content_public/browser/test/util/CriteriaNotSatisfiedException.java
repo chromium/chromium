d07436c22cfe6ffea8fae09aa182838819de50b1
Michael Thiessen <mthiesse@chromium.org>
2020-11-03 16:57:28 +0000

    Revert "Remove content/ CriteriaNotSatisfiedException"
    This reverts commit 9f35be80c4a33289ee807b809c935de572517d08.
    
    Reason for revert: clank rolls all failed, not sure why the clank bot I added didn't fail on my change.
    
    Original change's description:
    > Remove content/ CriteriaNotSatisfiedException
    >
    > Part 3 of a 3-sided change to move CriteriaNotSatisfiedException to
    > base/
    >
    > Bug: 1134178
    > Change-Id: Ie76869351d8a09bd92205e3aef3ffea65220c01a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515520
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#823553}
    
    TBR=yfriedman@chromium.org,mthiesse@chromium.org
    
    Change-Id: I0ae020b9af62053f500a9f304c8fb9f9552c657e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1134178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517382
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823575}

---
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/CriteriaNotSatisfiedException.java
@@ -0,0 +1,25 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.browser.test.util;
+
+/**
+ * Exception indicating that a Criteria did not match expectations.
+ */
+public class CriteriaNotSatisfiedException
+        extends org.chromium.base.test.util.CriteriaNotSatisfiedException {
+    /**
+     * @param msg The reason the criteria was not met.
+     */
+    public CriteriaNotSatisfiedException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * @param cause The underlying exception that prevented the Criteria.
+     */
+    public CriteriaNotSatisfiedException(Throwable cause) {
+        super(cause);
+    }
+}
