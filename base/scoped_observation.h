046f5c7965ef4cb6b822402affa446802ca50809
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-04 16:48:43 +0000

    Add base::ScopedObservation.
    This class is for observers that observe only one source.
    This is more readable and slightly more efficient than using
    base::ScopedObserver, where an observer has zero or one subscriptions.
    
    Change-Id: Iefd987ce006aa2106249cf2708f63d90326814ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518028
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824014}

---
+++ base/scoped_observation.h
@@ -0,0 +1,77 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_SCOPED_OBSERVATION_H_
+#define BASE_SCOPED_OBSERVATION_H_
+
+#include <stddef.h>
+
+#include "base/check_op.h"
+
+// ScopedObservation is used to keep track of a single observation.
+// When ScopedObservation is destroyed, it removes the registered observation,
+// if any. Basic example (as a member variable):
+//
+//   class MyFooObserver : public FooObserver {
+//     ...
+//    private:
+//     ScopedObservation<Foo, FooObserver> foo_observation_{this};
+//   };
+//
+// For cases with methods not named AddObserver/RemoveObserver:
+//
+//   class MyFooStateObserver : public FooStateObserver {
+//     ...
+//    private:
+//     ScopedObservation<Foo,
+//                    FooStateObserver,
+//                    &Foo::AddStateObserver,
+//                    &Foo::RemoveStateObserver>
+//       observed_foo_{this};
+//   };
+//
+// See also base::ScopedObserver to manage observations from multiple sources.
+template <class Source,
+          class Observer,
+          void (Source::*AddObsFn)(Observer*) = &Source::AddObserver,
+          void (Source::*RemoveObsFn)(Observer*) = &Source::RemoveObserver>
+class ScopedObservation {
+ public:
+  explicit ScopedObservation(Observer* observer) : observer_(observer) {}
+  ScopedObservation(const ScopedObservation&) = delete;
+  ScopedObservation& operator=(const ScopedObservation&) = delete;
+  ~ScopedObservation() {
+    if (IsObserving())
+      RemoveObservation();
+  }
+
+  // Adds the object passed to the constructor as an observer on |source|.
+  // IsObserving() must be false.
+  void Observe(Source* source) {
+    DCHECK_EQ(source_, nullptr);
+    source_ = source;
+    (source_->*AddObsFn)(observer_);
+  }
+
+  // Remove the object passed to the constructor as an observer from |source|.
+  void RemoveObservation() {
+    DCHECK_NE(source_, nullptr);
+    (source_->*RemoveObsFn)(observer_);
+    source_ = nullptr;
+  }
+
+  // Returns true if any source is being observed.
+  bool IsObserving() const { return source_ != nullptr; }
+
+  // Returns true if |source| is being observed.
+  bool IsObservingSource(Source* source) const { return source_ == source; }
+
+ private:
+  Observer* const observer_;
+
+  // The observed source, if any.
+  Source* source_ = nullptr;
+};
+
+#endif  // BASE_SCOPED_OBSERVATION_H_
