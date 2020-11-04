45c57b9f1e75b4659a4029add6b9acbb316ea20f
Hirokazu Honda <hiroh@chromium.org>
2020-11-04 08:18:46 +0000

    media/gpu/v4l2: Add Filler and SPSext to filtered NALUs
    Since MT8173 decoder doesn't support multiple NALUs, it is
    necessary to split them before feeding them to the driver.
    The procedure of splitting doesn't detect filler and SPS
    extension NALUs. Therefore, for example, PPS and filler NALUs are
    fed fed together to the driver. This fixes the issue by adding
    Filler and SPS extension NALUs as filtered NALUs in the pre
    processing.
    
    Bug: b:171846172
    Test: local loopback in https://appr.tc/?vsc=h264&vrc=h264 on elm
    Change-Id: I792261d8c349518f15232d1cd50d9bf6c27d464e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517348
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823920}

--- media/gpu/v4l2/v4l2_vda_helpers.cc
+++ media/gpu/v4l2/v4l2_vda_helpers.cc
@@ -231,6 +231,8 @@ bool H264InputBufferFragmentSplitter::AdvanceFrameFragment(const uint8_t* data,
       case H264NALU::kAUD:
       case H264NALU::kEOSeq:
       case H264NALU::kEOStream:
+      case H264NALU::kFiller:
+      case H264NALU::kSPSExt:
       case H264NALU::kReserved14:
       case H264NALU::kReserved15:
       case H264NALU::kReserved16:
