64e40a70b5f13c655289a68394e0da2de9bf80a1
Avi Drissman <avi@chromium.org>
2020-11-05 07:36:25 +0000

    Add Rosetta utilities.
    New macOS capabilities, more utilities in Chromium.
    
    Bug: 1145672, 1145792, 1142017
    Change-Id: Ibd10501429aba40aacb4db7c751385428115941d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518258
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824329}

---
+++ base/mac/rosetta.h
@@ -0,0 +1,47 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MAC_ROSETTA_H_
+#define BASE_MAC_ROSETTA_H_
+
+#include <vector>
+
+#include "base/base_export.h"
+#include "build/build_config.h"
+
+namespace base {
+
+class FilePath;
+
+namespace mac {
+
+#if defined(ARCH_CPU_X86_64)
+
+// Returns true if the current process is being translated by Rosetta.
+bool ProcessIsTranslated();
+
+#endif  // ARCH_CPU_X86_64
+
+#if defined(ARCH_CPU_ARM64)
+
+// Returns true if Rosetta is installed and available to translate x86_64 code.
+BASE_EXPORT bool IsRosettaInstalled();
+
+#endif  // ARCH_CPU_ARM64
+
+// Requests an ahead-of-time translation of the binaries with paths given in
+// `binaries`. Returns the success value (true == success, false == failure)
+// indicated by the underlying call.
+//
+// Observed behavior about Rosetta AOT translation:
+// - If a binary was already translated, it will not be translated again.
+// - The call blocks and waits for the completion of the translation. Do not
+//   call this on the main thread.
+BASE_EXPORT bool RequestRosettaAheadOfTimeTranslation(
+    const std::vector<FilePath>& binaries);
+
+}  // namespace mac
+}  // namespace base
+
+#endif  // BASE_MAC_ROSETTA_H_
