683608bf84b4f50020c8322624a2b010b56dcc8e
Yuheng Huang <yuhengh@chromium.org>
2020-11-05 22:00:02 +0000

    Tab Search: Implement performance benchmark
    Doc:
    https://docs.google.com/document/d/1-1ijT7wt05hlBZmSKjX_DaTCzVqpxbfTM1y-j7kYHlc
    Usage:
    tools/perf/run_benchmark run UNSCHEDULED_tab_search --browser-executable=out/Default/chrome --story-filter=tab_search:top10:2020
    tools/perf/run_benchmark run UNSCHEDULED_tab_search --browser-executable=out/Default/chrome --story-filter=tab_search:top50:2020
    tools/perf/run_benchmark run UNSCHEDULED_tab_search --browser-executable=out/Default/chrome --story-filter=tab_search:top100:2020
    
    Change-Id: I684024f9cf3264ac2ed4feca15c6059342a493c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461224
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824599}

---
+++ tools/perf/benchmarks/tab_search.py
@@ -0,0 +1,58 @@
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from core import perf_benchmark
+from core import platforms
+
+from telemetry import benchmark
+from telemetry import story
+from telemetry.timeline import chrome_trace_category_filter
+from telemetry.web_perf import timeline_based_measurement
+import page_sets
+
+TAB_SEARCH_BENCHMARK_UMA = [
+    'Tabs.TabSearch.CloseAction',
+    'Tabs.TabSearch.NumTabsClosedPerInstance',
+    'Tabs.TabSearch.NumTabsOnOpen',
+    'Tabs.TabSearch.NumWindowsOnOpen',
+    'Tabs.TabSearch.OpenAction',
+    'Tabs.TabSearch.PageHandlerConstructionDelay',
+    'Tabs.TabSearch.WebUI.InitialTabsRenderTime',
+    'Tabs.TabSearch.WebUI.LoadCompletedTime',
+    'Tabs.TabSearch.WebUI.LoadDocumentTime',
+    'Tabs.TabSearch.WebUI.TabListDataReceived',
+    'Tabs.TabSearch.WebUI.TabSwitchAction',
+    'Tabs.TabSearch.WindowDisplayedDuration2',
+    'Tabs.TabSearch.WindowTimeToShowCachedWebView',
+    'Tabs.TabSearch.WindowTimeToShowUncachedWebView',
+]
+
+
+@benchmark.Info(emails=[
+    'yuhengh@chromium.org', 'tluk@chromium.org', 'romanarora@chromium.org'
+],
+                component='UI>Browser>TabSearch')
+class TabSearch(perf_benchmark.PerfBenchmark):
+  """Tab Search Benchmark."""
+  PLATFORM = 'desktop'
+  SUPPORTED_PLATFORM_TAGS = [platforms.DESKTOP]
+  SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
+
+  def CreateStorySet(self, options):
+    return page_sets.TabSearchStorySet()
+
+  def CreateCoreTimelineBasedMeasurementOptions(self):
+    category_filter = chrome_trace_category_filter.ChromeTraceCategoryFilter(
+        filter_string='uma')
+    category_filter.AddIncludedCategory('browser')
+    category_filter.AddIncludedCategory('blink.user_timing')
+    options = timeline_based_measurement.Options(category_filter)
+    options.config.chrome_trace_config.EnableUMAHistograms(
+        *TAB_SEARCH_BENCHMARK_UMA)
+    options.SetTimelineBasedMetrics(['webuiMetric', 'umaMetric'])
+    return options
+
+  @classmethod
+  def Name(cls):
+    return 'UNSCHEDULED_tab_search'
---
+++ tools/perf/page_sets/tab_search/tab_search_story.py
@@ -0,0 +1,205 @@
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import py_utils
+from telemetry.page import page
+from telemetry.internal.actions.action_runner import ActionRunner
+
+TOP_URL = [
+    'google.com',
+    'youtube.com',
+    'amazon.com',
+    'facebook.com',
+    'zoom.us',
+    'yahoo.com',
+    'reddit.com',
+    'wikipedia.org',
+    'myshopify.com',
+    'ebay.com',
+    'instructure.com',
+    'office.com',
+    'netflix.com',
+    'bing.com',
+    'live.com',
+    'microsoft.com',
+    'espn.com',
+    'twitch.tv',
+    'blogger.com',
+    'instagram.com',
+    'mozilla.org',
+    'cnn.com',
+    'apple.com',
+    'zillow.com',
+    'etsy.com',
+    'chase.com',
+    'nytimes.com',
+    'linkedin.com',
+    'dropbox.com',
+    'adobe.com',
+    'okta.com',
+    'craigslist.org',
+    'twitter.com',
+    'walmart.com',
+    'aliexpress.com',
+    'github.com',
+    'vimeo.com',
+    'quizlet.com',
+    'tmall.com',
+    'imgur.com',
+    'wellsfargo.com',
+    'hulu.com',
+    'imdb.com',
+    'salesforce.com',
+    'homedepot.com',
+    'indeed.com',
+    'foxnews.com',
+    'msn.com',
+    'spotify.com',
+    'whatsapp.com',
+]
+
+
+class TabSearchStory(page.Page):
+  """Base class for tab search stories"""
+
+  def __init__(self, story_set, extra_browser_args=None):
+    super(TabSearchStory, self).__init__(url=self.URL,
+                                         name=self.NAME,
+                                         page_set=story_set,
+                                         extra_browser_args=extra_browser_args)
+
+  def RunNavigateSteps(self, action_runner):
+    url_list = self.URL_LIST
+    tabs = action_runner.tab.browser.tabs
+    tabs[0].Navigate('https://' + url_list[0])
+    for url in url_list[1:]:
+      new_tab = tabs.New()
+      new_tab.Navigate('https://' + url)
+    if self.WAIT_FOR_NETWORK_QUIESCENCE:
+      for i, url in enumerate(url_list):
+        try:
+          tabs[i].action_runner.WaitForNetworkQuiescence()
+        except py_utils.TimeoutException:
+          logging.warning('WaitForNetworkQuiescence() timeout, url[%d]: %s' %
+                          (i, url))
+
+  def RunPageInteractions(self, action_runner):
+    tabs = action_runner.tab.browser.tabs
+    tabs_len = len(tabs)
+
+    # Open Tab Search bubble.
+    action_runner.tab.browser.supports_inspecting_webui = True
+    action_runner.tab.browser.ExecuteBrowserCommand('openTabSearch')
+    # Wait for Tab Search bubble to be inspectable.
+    py_utils.WaitFor(lambda: len(tabs) > tabs_len, 10)
+
+    # Wait for Tab Search bubble to load.
+    tab = tabs[-1]
+    action_runner = ActionRunner(
+        tab)  # Recreate action_runner for Tab Search bubble.
+    tab.WaitForDocumentReadyStateToBeComplete()
+
+    # Send key navigation to Tab Search bubble.
+    self.InteractWithPage(action_runner)
+
+  def InteractWithPage(self, action_runner):
+    self.ScrollTabs(action_runner)
+    self.SearchTabs(action_runner)
+    self.CloseTab(action_runner)
+
+  def ScrollUp(self, action_runner):
+    action_runner.Wait(1)
+    # Scroll to the bottom of the list.
+    action_runner.PressKey('ArrowUp')
+    action_runner.Wait(1)
+
+  def SearchTabs(self, action_runner):
+    action_runner.Wait(1)
+    action_runner.EnterText('o')
+    action_runner.Wait(2)
+    action_runner.PressKey('Backspace')
+    action_runner.Wait(1)
+
+  def CloseTab(self, action_runner):
+    action_runner.Wait(1)
+    # Tab to the close button of the 2nd tab.
+    action_runner.PressKey('Tab', repeat_count=4, repeat_delay_ms=500)
+    action_runner.PressKey(' ')
+    action_runner.Wait(1)
+
+  def ScrollTabs(self, action_runner):
+    action_runner.Wait(1)
+    self.StartMeasuringFrameTime(action_runner, 'frame_time_on_scroll')
+    action_runner.ScrollElement(element_function=SCROLL_ELEMENT_FUNCTION)
+    self.StopMeasuringFrameTime(action_runner)
+    action_runner.Wait(1)
+
+  def CloseAndOpen(self, action_runner):
+    action_runner.Wait(1)
+    action_runner.tab.browser.ExecuteBrowserCommand('closeTabSearch')
+    action_runner.Wait(1)
+    action_runner.tab.browser.ExecuteBrowserCommand('openTabSearch')
+    action_runner.Wait(5)
+
+  def StartMeasuringFrameTime(self, action_runner, name):
+    action_runner.ExecuteJavaScript(MEASURE_FRAME_TIME_SCRIPT)
+    action_runner.ExecuteJavaScript(START_MEASURING_FRAME_TIME % name)
+
+  def StopMeasuringFrameTime(self, action_runner):
+    action_runner.ExecuteJavaScript(STOP_MEASURING_FRAME_TIME)
+
+
+class TabSearchStoryTop10(TabSearchStory):
+  NAME = 'tab_search:top10:2020'
+  URL_LIST = TOP_URL[:10]
+  URL = 'https://' + URL_LIST[0]
+  WAIT_FOR_NETWORK_QUIESCENCE = True
+
+
+class TabSearchStoryTop50(TabSearchStory):
+  NAME = 'tab_search:top50:2020'
+  URL_LIST = TOP_URL[:50]
+  URL = 'https://' + URL_LIST[0]
+  WAIT_FOR_NETWORK_QUIESCENCE = True
+
+
+class TabSearchStoryTop100(TabSearchStory):
+  NAME = 'tab_search:top100:2020'
+  URL_LIST = TOP_URL[:50] * 2
+  URL = 'https://' + URL_LIST[0]
+  WAIT_FOR_NETWORK_QUIESCENCE = True
+
+
+SCROLL_ELEMENT_FUNCTION = '''
+document.querySelector('tab-search-app').shadowRoot.querySelector('#tabs')
+'''
+
+MEASURE_FRAME_TIME_SCRIPT = '''
+window.__webui_startMeasuringFrameTime = function(name) {
+  if (!window.__webui_onRequestAnimationFrame) {
+    window.__webui_onRequestAnimationFrame = function() {
+      performance.mark(name + ':benchmark_end');
+      if (window.__webui_onRequestAnimationFrame) {
+        requestAnimationFrame(window.__webui_onRequestAnimationFrame);
+        performance.mark(name + ':benchmark_begin')
+      }
+    }
+    performance.mark(name + ':benchmark_begin')
+    requestAnimationFrame(window.__webui_onRequestAnimationFrame);
+  }
+}
+
+window.__webui_stopMeasuringFrameTime = function() {
+  window.__webui_onRequestAnimationFrame = null;
+}
+'''
+
+START_MEASURING_FRAME_TIME = '''
+window.__webui_startMeasuringFrameTime('%s')
+'''
+
+STOP_MEASURING_FRAME_TIME = '''
+window.__webui_stopMeasuringFrameTime()
+'''
