33f6d0f88f9acaecd829193a496456923443436d
Mei Liang <meiliang@chromium.org>
2020-11-10 22:51:33 +0000

    Deprecate the SceneChangeObserver interface
    This cl annotates the SceneChangeObserver interface as deprecated to
    avoid new usage.
    
    Bug: 110846
    Change-Id: I5b562b9100c4b674f4f6cd9197f2a41322d47c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529697
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825995}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/SceneChangeObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/SceneChangeObserver.java
@@ -4,20 +4,29 @@
 
 package org.chromium.chrome.browser.compositor.layouts;
 
+import org.chromium.chrome.browser.layouts.LayoutStateProvider.LayoutStateObserver;
+
 /**
  * An observer that listens for active {@link Layout} changes.
+ *
+ * DEPRECATED, please use {@link LayoutStateObserver} instead.
  */
+@Deprecated
 public interface SceneChangeObserver {
     /**
      * Called when a layout wants to hint that a new tab might be selected soon.  This is not called
      * every time a tab is selected.
      * @param tabId The id of the tab that might be selected soon.
+     *
+     * DEPRECATED, please use {@link LayoutStateObserver#onTabSelectionHinted(int)} instead.
      */
     void onTabSelectionHinted(int tabId);
 
     /**
      * Called when the active {@link Layout} changes.
      * @param layout The new active {@link Layout}.
+     *
+     * DEPRECATED, please use {@link LayoutStateObserver#onStartedShowing(int, boolean)} instead.
      */
     void onSceneChange(Layout layout);
 }
