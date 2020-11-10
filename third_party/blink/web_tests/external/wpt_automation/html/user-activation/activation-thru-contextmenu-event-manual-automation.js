ad6362a1cc8cbe02765813f202368cd439190ce3
Lan Wei <lanwei@chromium.org>
2020-11-10 21:37:46 +0000

    Simulate user inputs in activation-thru-contextmenu-event.html
    Use testdriver Action API to simulate mouse actions in
    html/user-activation/activation-thru-contextmenu-event.html.
    
    Bug: 1145677
    Change-Id: I8c287cd45d87e809cfa4b5967df38ed0b58d0d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2520453
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825963}

--- third_party/blink/web_tests/external/wpt_automation/html/user-activation/activation-thru-contextmenu-event-manual-automation.js
+++
@@ -1,7 +0,0 @@
-importAutomationScript('/pointerevents/pointerevent_common_input.js');
-
-function inject_input() {
-  return mouseClickInTarget('#target', undefined, /* right button */ 2).then(function() {
-    return mouseClickInTarget('#done');
-  });
-}
