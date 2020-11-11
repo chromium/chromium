904e7bbebdf648a11211de5cfb09579a402b9829
Jesse Schettler <jschettler@chromium.org>
2020-11-11 04:12:03 +0000

    scanning: Update string for helper text
    Replace the string for the helper text with the newest string and update
    the styling to match the spec.
    
    Bug: 1059779
    Change-Id: I7b2ba855f0a34107b5c62ca1cb9f7f659c2e96fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2530216
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826125}

--- chromeos/components/scanning/resources/scan_preview.js
+++ chromeos/components/scanning/resources/scan_preview.js
@@ -35,6 +35,16 @@ Polymer({
   },
 
   /**
+   * Returns the translated helper text string with the id attribute. The id is
+   * used to selectively style parts of the string.
+   * @return {string}
+   * @private
+   */
+  getHelperTextHtml_() {
+    return this.i18nAdvanced('scanPreviewHelperText', {attrs: ['id']});
+  },
+
+  /**
    * @return {string}
    * @private
    */
