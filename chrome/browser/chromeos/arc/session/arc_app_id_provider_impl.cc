c59b2ab8df413cf3abe745f0e6b336e1359d53ce
Yuichiro Hanada <yhanada@google.com>
2020-11-11 03:01:02 +0000

    Move arc::ArcAppIdProviderImpl into c/b/chromeos/arc/session.
    It's used only by arc::ArcSessionManager.
    
    Change-Id: I2bc0844ed29c12571f21cd8bc67e3ac81e3bd62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531153
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826099}

--- chrome/browser/chromeos/arc/session/arc_app_id_provider_impl.cc
+++ chrome/browser/chromeos/arc/session/arc_app_id_provider_impl.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/arc/arc_app_id_provider_impl.h"
+#include "chrome/browser/chromeos/arc/session/arc_app_id_provider_impl.h"
 
 #include "chrome/browser/chromeos/arc/session/arc_session_manager.h"
 #include "chrome/browser/profiles/profile.h"
