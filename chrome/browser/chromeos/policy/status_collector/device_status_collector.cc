4ef44b6c7a8a28b4a985f83854081e59ae836108
Mason Wilde <masonwilde@google.com>
2020-11-13 19:11:10 +0000

    ServiceConnection: Make vpd serial_number optional
    Making this field required led to issues with test devices. This changes
    it to optional like many other fields.
    
    Bug: chromium:1146143
    Change-Id: Ie0bdfe38462aef987021c42eec20749255d5ddf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536150
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mason Wilde <masonwilde@google.com>
    Cr-Commit-Position: refs/heads/master@{#827343}

--- chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
+++ chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
@@ -1240,8 +1240,10 @@ class DeviceStatusCollectorState : public StatusCollectorState {
               system_status_out->set_vpd_sku_number(
                   system_info->product_sku_number.value());
             }
-            system_status_out->set_vpd_serial_number(
-                system_info->product_serial_number);
+            if (system_info->product_serial_number.has_value()) {
+              system_status_out->set_vpd_serial_number(
+                  system_info->product_serial_number.value());
+            }
           }
           if (report_system_info) {
             system_status_out->set_marketing_name(system_info->marketing_name);
