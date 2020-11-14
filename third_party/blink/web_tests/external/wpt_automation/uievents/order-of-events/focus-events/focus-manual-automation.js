50b853d146397a05a4719a9b66ffbf0ae329c5d7
Lan Wei <lanwei@chromium.org>
2020-11-14 01:24:12 +0000

    Simulate user inputs in focus-events/focus.html, fix event order.
    Use testdriver Action API to simulate mouse actions in
    uievents/order-of-events/focus-events/focus.html.
    
    This test now matches major browser implementations that "focus"
    preceeds "focusin", and "blur" preceeds "focusout".  See:
        https://github.com/w3c/uievents/issues/276.
    
    Bug: 1145677
    Change-Id: I9e5965c90a18560cce560b73a3df88d0e840473d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519193
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827515}

--- third_party/blink/web_tests/external/wpt_automation/uievents/order-of-events/focus-events/focus-manual-automation.js
+++
@@ -1,9 +0,0 @@
-importAutomationScript('/pointerevents/pointerevent_common_input.js');
-
-function inject_input() {
-  return mouseClickInTarget('#a').then(function() {
-    return mouseClickInTarget('#b');
-  }).then(function() {
-    return mouseClickInTarget('#done');
-  });
-}
