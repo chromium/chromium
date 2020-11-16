3f25ba2f801be657158877ff146d583301c22f58
Glen Robertson <glenrob@chromium.org>
2020-11-16 05:58:54 +0000

    digitalgoods:Log error messages to JS console so devs can debug.
    Console errors are also logged to stderr so C++ devs can debug as
    before.
    
    Bug: 1149223
    Change-Id: I050ba47184740bb7a08abf1469838f79f616105c
    Fixed: 1149223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537277
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827697}

---
+++ third_party/blink/renderer/modules/payments/goods/util.cc
@@ -0,0 +1,29 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/payments/goods/util.h"
+
+#include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+namespace digital_goods_util {
+
+void LogConsoleError(ScriptState* script_state, const String& message) {
+  if (!script_state || !script_state->ContextIsValid()) {
+    VLOG(1) << message;
+    return;
+  }
+  auto* execution_context = ExecutionContext::From(script_state);
+  DCHECK(execution_context);
+  execution_context->AddConsoleMessage(
+      mojom::blink::ConsoleMessageSource::kJavaScript,
+      mojom::blink::ConsoleMessageLevel::kError, message,
+      /*discard_duplicates=*/true);
+}
+
+}  // namespace digital_goods_util
+}  // namespace blink
