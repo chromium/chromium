ccfc6cbc869056eff77674d7b515de1ee5d01c7b
Connie Wan <connily@chromium.org>
2020-11-17 17:40:35 +0000

    Disable Tab Groups extension API end-to-end tests on Linux
    Bug: 1148195, 1149950
    Change-Id: I658e01ad6601f7cdf145502918419f2637bda8ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543127
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Commit-Queue: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828250}

--- chrome/browser/extensions/api/tab_groups/tab_groups_api_apitest.cc
+++ chrome/browser/extensions/api/tab_groups/tab_groups_api_apitest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
 
@@ -12,6 +13,11 @@ namespace {
 using TabGroupsApiTest = ExtensionApiTest;
 
 IN_PROC_BROWSER_TEST_F(TabGroupsApiTest, TestTabGroupsWorks) {
+#if defined(OS_LINUX)
+  // TODO(crbug.com/1148195): Fix flakiness of this text on Linux.
+  return;
+#endif
+
   ASSERT_TRUE(RunExtensionTestWithFlags("tab_groups",
                                         kFlagIgnoreManifestWarnings, kFlagNone))
       << message_;
