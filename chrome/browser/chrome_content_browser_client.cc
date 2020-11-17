a06275dc38aa8a6854326664739595f3ab20f59a
Aaron Tagliaboschi <aarontag@chromium.org>
2020-11-17 17:55:04 +0000

    [UA-CH] Add field trial control of GREASEy strings
    Bug: 1149575, 1070922, 1091285
    Change-Id: If6118f86e0a7d7333513af52a01fbe8e61d83c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2541924
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828257}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1177,7 +1177,8 @@ bool IsErrorPageAutoReloadEnabled() {
 blink::UserAgentBrandList GenerateBrandVersionList(
     int seed,
     base::Optional<std::string> brand,
-    std::string major_version) {
+    std::string major_version,
+    base::Optional<std::string> maybe_greasey_brand) {
   DCHECK_GE(seed, 0);
   const int npermutations = 6;  // 3!
   int permutation = seed % npermutations;
@@ -1197,7 +1198,8 @@ blink::UserAgentBrandList GenerateBrandVersionList(
       base::StrCat({escaped_chars[order[0]], "Not", escaped_chars[order[1]],
                     "A", escaped_chars[order[2]], "Brand"});
 
-  blink::UserAgentBrandVersion greasey_bv = {greasey_brand, "99"};
+  blink::UserAgentBrandVersion greasey_bv = {
+      maybe_greasey_brand.value_or(greasey_brand), "99"};
   blink::UserAgentBrandVersion chromium_bv = {"Chromium", major_version};
 
   blink::UserAgentBrandList greased_brand_version_list(3);
@@ -1229,8 +1231,14 @@ const blink::UserAgentBrandList& GetBrandVersionList() {
 #if !BUILDFLAG(CHROMIUM_BRANDING)
         brand = version_info::GetProductName();
 #endif
+        base::Optional<std::string> maybe_param_override =
+            base::GetFieldTrialParamValueByFeature(features::kGreaseUACH,
+                                                   "brand_override");
+        if (maybe_param_override->empty())
+          maybe_param_override = base::nullopt;
+
         return GenerateBrandVersionList(major_version_number, brand,
-                                        major_version);
+                                        major_version, maybe_param_override);
       }());
   return *greased_brand_version_list;
 }
