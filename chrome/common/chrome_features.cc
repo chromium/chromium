d52e465bf1a3c8a133dae19e4a897e00c066ddc0
Melissa Zhang <melzhang@chromium.org>
2020-11-17 06:22:38 +0000

    Reland "[Sharesheet] Turn on flags to enable sharesheet by default."
    This reverts commit ebc8ccab9420f14dc4f8a79c5fd2939a3d012607.
    
    Reason for revert: Enabling flag for M89
    
    Original change's description:
    > Revert "[Sharesheet] Turn on flags to enable sharesheet by default."
    >
    > This reverts commit 2374eacf35e48d8c0ee714ae6764bf41cee4f4c5.
    >
    > Reason for revert: Punting sharesheet to 89 so flags need to be disabled.
    >
    > Original change's description:
    > > [Sharesheet] Turn on flags to enable sharesheet by default.
    > >
    > > This CL turns on the kIntentHandlingSharing and kSharesheet flags to
    > > enable sharesheet feature by default.
    > >
    > > BUG=1097623
    > >
    > > Change-Id: Idff4e37934fb730b7f83b86206d555a8c049f12e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2460206
    > > Commit-Queue: Maggie Cai <mxcai@chromium.org>
    > > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#816071}
    >
    > TBR=dominickn@chromium.org,mxcai@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 1097623
    > Change-Id: I4d4914ad4ec363c2a63fdaaf7c55163dd1205f99
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501161
    > Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#821103}
    
    TBR=dominickn@chromium.org,mxcai@chromium.org,melzhang@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1097623
    Change-Id: I47b8cbcc1f05a6092db13b0fe645aad6f06c0325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543530
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828092}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -525,7 +525,7 @@ const base::Feature kInstallableAmbientBadgeInfoBar{
 #if !defined(OS_ANDROID)
 // Support sharing in Chrome OS intent handling.
 const base::Feature kIntentHandlingSharing{"IntentHandlingSharing",
-                                           base::FEATURE_DISABLED_BY_DEFAULT};
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
 // Allow user to have preference for PWA in the intent picker.
 const base::Feature kIntentPickerPWAPersistence{
     "IntentPickerPWAPersistence", base::FEATURE_DISABLED_BY_DEFAULT};
@@ -736,8 +736,7 @@ const base::Feature kSecurityKeyAttestationPrompt{
     "SecurityKeyAttestationPrompt", base::FEATURE_ENABLED_BY_DEFAULT};
 
 #if defined(OS_CHROMEOS)
-const base::Feature kSharesheet{"Sharesheet",
-                                base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kSharesheet{"Sharesheet", base::FEATURE_ENABLED_BY_DEFAULT};
 #endif
 
 #if defined(OS_MAC)
