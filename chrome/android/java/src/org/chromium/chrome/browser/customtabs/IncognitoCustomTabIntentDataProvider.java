2b1d1eb2294a5e9604801678e56bb8b666e2376b
Side Yilmaz <sideyilmaz@chromium.org>
2020-11-18 13:29:35 +0000

    Add title visibility option to Incognito CCT.
    This CL gives ability to Incognito CCT to show title on toolbar.
    
    For screenshot:
    https://drive.google.com/file/d/1WQFhK3yhVsF-OazSSrUh9d8mfpNb1ZeK
    
    Bug: 1104180
    Change-Id: I317c79c807fead5f024d197d09a190cd79841e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545625
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828691}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/IncognitoCustomTabIntentDataProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/IncognitoCustomTabIntentDataProvider.java
@@ -54,6 +54,7 @@ public class IncognitoCustomTabIntentDataProvider extends BrowserServicesIntentD
     private final int mToolbarColor;
     private final int mBottomBarColor;
     private final int mNavigationBarColor;
+    private final int mTitleVisibilityState;
     private final Drawable mCloseButtonIcon;
     private final boolean mShowShareItem;
     private final List<Pair<String, PendingIntent>> mMenuEntries = new ArrayList<>();
@@ -89,6 +90,8 @@ public class IncognitoCustomTabIntentDataProvider extends BrowserServicesIntentD
         mCloseButtonIcon = TintedDrawable.constructTintedDrawable(context, R.drawable.btn_close);
         mShowShareItem = IntentUtils.safeGetBooleanExtra(
                 intent, CustomTabsIntent.EXTRA_DEFAULT_SHARE_MENU_ITEM, false);
+        mTitleVisibilityState = IntentUtils.safeGetIntExtra(
+                intent, CustomTabsIntent.EXTRA_TITLE_VISIBILITY_STATE, CustomTabsIntent.NO_TITLE);
 
         mUiType = getUiType(intent);
         updateExtraMenuItemsIfNecessary(intent);
@@ -253,6 +256,11 @@ public class IncognitoCustomTabIntentDataProvider extends BrowserServicesIntentD
     @Override
     public Integer getNavigationBarColor() {
         return mNavigationBarColor;
+    }
+
+    @Override
+    public int getTitleVisibilityState() {
+        return mTitleVisibilityState;
     }
 
     @Override
