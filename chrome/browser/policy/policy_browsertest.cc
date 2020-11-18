e8f664c6cd443d4479768adc06938fcbd82f0274
Swapnil <swapnilgupta@google.com>
2020-11-18 10:41:56 +0000

    Split browsertests for browsing history and SSL related policies
    There are several browsertest related to browsing history and
    SSL error overriding policy. This CL splits these tests to new file.
    
    Bug: 1148745
    Change-Id: I6d8eb679aa84289694cdcb534ebf836dbde4e7ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544944
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#828658}

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -65,7 +65,6 @@
 #include "chrome/browser/devtools/devtools_window_testing.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/extensions/api/chrome_extensions_api_client.h"
-#include "chrome/browser/interstitials/security_interstitial_page_test_utils.h"
 #include "chrome/browser/media/webrtc/webrtc_event_log_manager.h"
 #include "chrome/browser/net/prediction_options.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
@@ -100,7 +99,6 @@
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
-#include "components/browsing_data/core/pref_names.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/pref_names.h"
@@ -865,81 +863,6 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, NetworkPrediction) {
   EXPECT_TRUE(IsNetworkPredictionEnabled(prefs));
 }
 
-IN_PROC_BROWSER_TEST_F(PolicyTest, SavingBrowserHistoryDisabled) {
-  // Verifies that browsing history is not saved.
-  PolicyMap policies;
-  policies.Set(key::kSavingBrowserHistoryDisabled, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(true),
-               nullptr);
-  UpdateProviderPolicy(policies);
-  GURL url = ui_test_utils::GetTestUrl(
-      base::FilePath(base::FilePath::kCurrentDirectory),
-      base::FilePath(FILE_PATH_LITERAL("empty.html")));
-  ui_test_utils::NavigateToURL(browser(), url);
-  // Verify that the navigation wasn't saved in the history.
-  ui_test_utils::HistoryEnumerator enumerator1(browser()->profile());
-  EXPECT_EQ(0u, enumerator1.urls().size());
-
-  // Now flip the policy and try again.
-  policies.Set(key::kSavingBrowserHistoryDisabled, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
-               nullptr);
-  UpdateProviderPolicy(policies);
-  ui_test_utils::NavigateToURL(browser(), url);
-  // Verify that the navigation was saved in the history.
-  ui_test_utils::HistoryEnumerator enumerator2(browser()->profile());
-  ASSERT_EQ(1u, enumerator2.urls().size());
-  EXPECT_EQ(url, enumerator2.urls()[0]);
-}
-
-IN_PROC_BROWSER_TEST_F(PolicyTest, DeletingBrowsingHistoryDisabled) {
-  // Verifies that deleting the browsing history can be disabled.
-
-  PrefService* prefs = browser()->profile()->GetPrefs();
-  EXPECT_FALSE(prefs->IsManagedPreference(prefs::kAllowDeletingBrowserHistory));
-  EXPECT_TRUE(prefs->GetBoolean(prefs::kAllowDeletingBrowserHistory));
-
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistory));
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteDownloadHistory));
-  EXPECT_TRUE(
-      prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistoryBasic));
-
-  PolicyMap policies;
-  policies.Set(key::kAllowDeletingBrowserHistory, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(true),
-               nullptr);
-  UpdateProviderPolicy(policies);
-  EXPECT_TRUE(prefs->IsManagedPreference(prefs::kAllowDeletingBrowserHistory));
-  EXPECT_TRUE(prefs->GetBoolean(prefs::kAllowDeletingBrowserHistory));
-
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistory));
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteDownloadHistory));
-  EXPECT_TRUE(
-      prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistoryBasic));
-
-  policies.Set(key::kAllowDeletingBrowserHistory, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
-               nullptr);
-  UpdateProviderPolicy(policies);
-  EXPECT_TRUE(prefs->IsManagedPreference(prefs::kAllowDeletingBrowserHistory));
-  EXPECT_FALSE(prefs->GetBoolean(prefs::kAllowDeletingBrowserHistory));
-
-  EXPECT_FALSE(prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistory));
-  EXPECT_FALSE(prefs->GetBoolean(browsing_data::prefs::kDeleteDownloadHistory));
-  EXPECT_FALSE(
-      prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistoryBasic));
-
-  policies.Clear();
-  UpdateProviderPolicy(policies);
-  EXPECT_FALSE(prefs->IsManagedPreference(prefs::kAllowDeletingBrowserHistory));
-  EXPECT_TRUE(prefs->GetBoolean(prefs::kAllowDeletingBrowserHistory));
-
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistory));
-  EXPECT_TRUE(prefs->GetBoolean(browsing_data::prefs::kDeleteDownloadHistory));
-  EXPECT_TRUE(
-      prefs->GetBoolean(browsing_data::prefs::kDeleteBrowsingHistoryBasic));
-}
-
 IN_PROC_BROWSER_TEST_F(PolicyTest, UrlKeyedAnonymizedDataCollection) {
   PrefService* prefs = browser()->profile()->GetPrefs();
   prefs->SetBoolean(
@@ -1130,72 +1053,6 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, WaitForInitialUserActivitySatisfied) {
 }
 
 #endif  // defined(OS_CHROMEOS)
-
-// Test that when SSL error overriding is allowed by policy (default), the
-// proceed link appears on SSL blocking pages.
-IN_PROC_BROWSER_TEST_F(PolicyTest, SSLErrorOverridingAllowed) {
-  net::EmbeddedTestServer https_server_expired(
-      net::EmbeddedTestServer::TYPE_HTTPS);
-  https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
-  https_server_expired.ServeFilesFromSourceDirectory("chrome/test/data");
-  ASSERT_TRUE(https_server_expired.Start());
-
-  const PrefService* const prefs = browser()->profile()->GetPrefs();
-
-  // Policy should allow overriding by default.
-  EXPECT_TRUE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
-
-  // Policy allows overriding - navigate to an SSL error page and expect the
-  // proceed link.
-  ui_test_utils::NavigateToURL(browser(), https_server_expired.GetURL("/"));
-  content::WebContents* tab =
-      browser()->tab_strip_model()->GetActiveWebContents();
-  WaitForInterstitial(tab);
-
-  // The interstitial should display the proceed link.
-  EXPECT_TRUE(chrome_browser_interstitials::IsInterstitialDisplayingText(
-      tab->GetMainFrame(), "proceed-link"));
-}
-
-// Test that when SSL error overriding is disallowed by policy, the
-// proceed link does not appear on SSL blocking pages and users should not
-// be able to proceed.
-IN_PROC_BROWSER_TEST_F(PolicyTest, SSLErrorOverridingDisallowed) {
-  net::EmbeddedTestServer https_server_expired(
-      net::EmbeddedTestServer::TYPE_HTTPS);
-  https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
-  https_server_expired.ServeFilesFromSourceDirectory("chrome/test/data");
-  ASSERT_TRUE(https_server_expired.Start());
-
-  const PrefService* const prefs = browser()->profile()->GetPrefs();
-  EXPECT_TRUE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
-
-  // Disallowing the proceed link by setting the policy to |false|.
-  PolicyMap policies;
-  policies.Set(key::kSSLErrorOverrideAllowed, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
-               nullptr);
-  UpdateProviderPolicy(policies);
-
-  // Policy should not allow overriding anymore.
-  EXPECT_FALSE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
-
-  // Policy disallows overriding - navigate to an SSL error page and expect no
-  // proceed link.
-  ui_test_utils::NavigateToURL(browser(), https_server_expired.GetURL("/"));
-  content::WebContents* tab =
-      browser()->tab_strip_model()->GetActiveWebContents();
-  WaitForInterstitial(tab);
-
-  // The interstitial should not display the proceed link.
-  EXPECT_FALSE(chrome_browser_interstitials::IsInterstitialDisplayingText(
-      tab->GetMainFrame(), "proceed-link"));
-
-  // The interstitial should not proceed, even if the command is sent in
-  // some other way (e.g., via the keyboard shortcut).
-  SendInterstitialCommand(tab, security_interstitials::CMD_PROCEED);
-  EXPECT_TRUE(IsShowingInterstitial(tab));
-}
 
 // Test that TaskManagerInterface::IsEndProcessEnabled is controlled by
 // TaskManagerEndProcessEnabled policy
---
+++ chrome/browser/policy/ssl_error_overriding_allowed_policy_browsertest.cc
@@ -0,0 +1,90 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/interstitials/security_interstitial_page_test_utils.h"
+#include "chrome/browser/policy/policy_test_utils.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/common/pref_names.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/policy/core/common/policy_map.h"
+#include "components/policy/core/common/policy_types.h"
+#include "components/policy/policy_constants.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace policy {
+
+// Test that when SSL error overriding is allowed by policy (default), the
+// proceed link appears on SSL blocking pages.
+IN_PROC_BROWSER_TEST_F(PolicyTest, SSLErrorOverridingAllowed) {
+  net::EmbeddedTestServer https_server_expired(
+      net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
+  https_server_expired.ServeFilesFromSourceDirectory("chrome/test/data");
+  ASSERT_TRUE(https_server_expired.Start());
+
+  const PrefService* const prefs = browser()->profile()->GetPrefs();
+
+  // Policy should allow overriding by default.
+  EXPECT_TRUE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
+
+  // Policy allows overriding - navigate to an SSL error page and expect the
+  // proceed link.
+  ui_test_utils::NavigateToURL(browser(), https_server_expired.GetURL("/"));
+  content::WebContents* tab =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  WaitForInterstitial(tab);
+
+  // The interstitial should display the proceed link.
+  EXPECT_TRUE(chrome_browser_interstitials::IsInterstitialDisplayingText(
+      tab->GetMainFrame(), "proceed-link"));
+}
+
+// Test that when SSL error overriding is disallowed by policy, the
+// proceed link does not appear on SSL blocking pages and users should not
+// be able to proceed.
+IN_PROC_BROWSER_TEST_F(PolicyTest, SSLErrorOverridingDisallowed) {
+  net::EmbeddedTestServer https_server_expired(
+      net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
+  https_server_expired.ServeFilesFromSourceDirectory("chrome/test/data");
+  ASSERT_TRUE(https_server_expired.Start());
+
+  const PrefService* const prefs = browser()->profile()->GetPrefs();
+  EXPECT_TRUE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
+
+  // Disallowing the proceed link by setting the policy to |false|.
+  PolicyMap policies;
+  policies.Set(key::kSSLErrorOverrideAllowed, POLICY_LEVEL_MANDATORY,
+               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(false),
+               nullptr);
+  UpdateProviderPolicy(policies);
+
+  // Policy should not allow overriding anymore.
+  EXPECT_FALSE(prefs->GetBoolean(prefs::kSSLErrorOverrideAllowed));
+
+  // Policy disallows overriding - navigate to an SSL error page and expect no
+  // proceed link.
+  ui_test_utils::NavigateToURL(browser(), https_server_expired.GetURL("/"));
+  content::WebContents* tab =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  WaitForInterstitial(tab);
+
+  // The interstitial should not display the proceed link.
+  EXPECT_FALSE(chrome_browser_interstitials::IsInterstitialDisplayingText(
+      tab->GetMainFrame(), "proceed-link"));
+
+  // The interstitial should not proceed, even if the command is sent in
+  // some other way (e.g., via the keyboard shortcut).
+  SendInterstitialCommand(tab, security_interstitials::CMD_PROCEED);
+  EXPECT_TRUE(IsShowingInterstitial(tab));
+}
+
+}  // namespace policy
