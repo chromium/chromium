94737673aca3a0fa21452f6b0136be8e3899c028
Ahmed Fakhry <afakhry@chromium.org>
2020-11-18 05:20:35 +0000

    capture_mode: Events ending video recording 2
    In this CL, we handle:
    - Imminent suspends.
    - User switches in multi-profile sessions.
    
    BUG=1142994
    TEST=Added new tests
    
    Change-Id: I31da5c8cfb4f2648b2a34b206673eb59cce69f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538590
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828602}

--- ash/capture_mode/capture_mode_unittests.cc
+++ ash/capture_mode/capture_mode_unittests.cc
@@ -30,6 +30,9 @@
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/dbus/power/fake_power_manager_client.h"
+#include "chromeos/dbus/power_manager/suspend.pb.h"
+#include "components/account_id/account_id.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
@@ -271,6 +274,21 @@ class CaptureModeTest : public AshTestBase {
     base::RunLoop().RunUntilIdle();
   }
 
+  void SwitchToUser2() {
+    auto* session_controller = GetSessionControllerClient();
+    constexpr char kUserEmail[] = "user2@capture_mode";
+    session_controller->AddUserSession(kUserEmail);
+    session_controller->SwitchActiveUser(AccountId::FromUserEmail(kUserEmail));
+  }
+
+  void WaitForSeconds(int seconds) {
+    base::RunLoop loop;
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, base::BindLambdaForTesting([&]() { loop.Quit(); }),
+        base::TimeDelta::FromSeconds(seconds));
+    loop.Run();
+  }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 };
@@ -385,7 +403,7 @@ TEST_F(CaptureModeTest, ChangeTypeAndSourceFromUI) {
 // TODO(https://crbug.com/1141927): test is flakey.
 TEST_F(CaptureModeTest, DISABLED_VideoRecordingUiBehavior) {
   // We need a non-zero duration to avoid infinite loop on countdown.
-  ui::ScopedAnimationDurationScaleMode animatin_scale(
+  ui::ScopedAnimationDurationScaleMode animation_scale(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   auto* controller = CaptureModeController::Get();
@@ -1079,12 +1097,17 @@ TEST_F(CaptureModeTest, FullscreenCursorStates) {
   EXPECT_TRUE(cursor_manager->IsCursorVisible());
 
   // Enter tablet mode, the cursor should be hidden.
-  TabletModeControllerTestApi().EnterTabletMode();
+  TabletModeControllerTestApi tablet_mode_controller_test_api;
+  // To avoid flaky failures due to mouse devices blocking entering tablet mode,
+  // we detach all mouse devices. This shouldn't affect testing the cursor
+  // status.
+  tablet_mode_controller_test_api.DetachAllMice();
+  tablet_mode_controller_test_api.EnterTabletMode();
   EXPECT_FALSE(cursor_manager->IsCursorVisible());
   EXPECT_TRUE(cursor_manager->IsCursorLocked());
 
   // Exit tablet mode, the cursor should appear again.
-  TabletModeControllerTestApi().LeaveTabletMode();
+  tablet_mode_controller_test_api.LeaveTabletMode();
   EXPECT_TRUE(cursor_manager->IsCursorVisible());
   EXPECT_EQ(CursorType::kCustom, cursor_manager->GetCursor().type());
   EXPECT_TRUE(test_api.IsUsingCustomCursor(CaptureModeType::kVideo));
@@ -1155,12 +1178,17 @@ TEST_F(CaptureModeTest, WindowCursorStates) {
   EXPECT_TRUE(test_api.IsUsingCustomCursor(CaptureModeType::kVideo));
 
   // Enter tablet mode, the cursor should be hidden.
-  TabletModeControllerTestApi().EnterTabletMode();
+  TabletModeControllerTestApi tablet_mode_controller_test_api;
+  // To avoid flaky failures due to mouse devices blocking entering tablet mode,
+  // we detach all mouse devices. This shouldn't affect testing the cursor
+  // status.
+  tablet_mode_controller_test_api.DetachAllMice();
+  tablet_mode_controller_test_api.EnterTabletMode();
   EXPECT_FALSE(cursor_manager->IsCursorVisible());
   EXPECT_TRUE(cursor_manager->IsCursorLocked());
 
   // Exit tablet mode, the cursor should appear again.
-  TabletModeControllerTestApi().LeaveTabletMode();
+  tablet_mode_controller_test_api.LeaveTabletMode();
   EXPECT_TRUE(cursor_manager->IsCursorVisible());
   EXPECT_EQ(CursorType::kCustom, cursor_manager->GetCursor().type());
   EXPECT_TRUE(test_api.IsUsingCustomCursor(CaptureModeType::kVideo));
@@ -1223,7 +1251,7 @@ TEST_F(CaptureModeTest, RegionDragCursorCompositing) {
 // incoming input events.
 TEST_F(CaptureModeTest, DoNotHandleEventDuringCountDown) {
   // We need a non-zero duration to avoid infinite loop on countdown.
-  ui::ScopedAnimationDurationScaleMode animatin_scale(
+  ui::ScopedAnimationDurationScaleMode animation_scale(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
   // Create 2 windows that overlap with each other.
@@ -1380,6 +1408,67 @@ TEST_F(CaptureModeTest, DetachDisplayWhileWindowRecording) {
   EXPECT_TRUE(stop_recording_button->visible_preferred());
 }
 
+TEST_F(CaptureModeTest, SuspendWhileSessionIsActive) {
+  auto* controller = StartCaptureSession(CaptureModeSource::kFullscreen,
+                                         CaptureModeType::kVideo);
+  EXPECT_TRUE(controller->IsActive());
+  power_manager_client()->SendSuspendImminent(
+      power_manager::SuspendImminent::IDLE);
+  EXPECT_FALSE(controller->IsActive());
+}
+
+TEST_F(CaptureModeTest, SuspendAfterCountdownStarts) {
+  // User NORMAL_DURATION for the countdown animation so we can have predictable
+  // timings.
+  ui::ScopedAnimationDurationScaleMode animation_scale(
+      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+  auto* controller = StartCaptureSession(CaptureModeSource::kFullscreen,
+                                         CaptureModeType::kVideo);
+  // Hit Enter to begin recording, wait for 1 second, then suspend the device.
+  auto* event_generator = GetEventGenerator();
+  SendKey(ui::VKEY_RETURN, event_generator);
+  WaitForSeconds(1);
+  power_manager_client()->SendSuspendImminent(
+      power_manager::SuspendImminent::IDLE);
+  EXPECT_FALSE(controller->IsActive());
+  EXPECT_FALSE(controller->is_recording_in_progress());
+}
+
+TEST_F(CaptureModeTest, SuspendAfterRecordingStarts) {
+  auto* controller = StartCaptureSession(CaptureModeSource::kFullscreen,
+                                         CaptureModeType::kVideo);
+  controller->StartVideoRecordingImmediatelyForTesting();
+  EXPECT_TRUE(controller->is_recording_in_progress());
+  power_manager_client()->SendSuspendImminent(
+      power_manager::SuspendImminent::IDLE);
+  EXPECT_FALSE(controller->is_recording_in_progress());
+}
+
+TEST_F(CaptureModeTest, SwitchUsersWhileRecording) {
+  auto* controller = StartCaptureSession(CaptureModeSource::kFullscreen,
+                                         CaptureModeType::kVideo);
+  controller->StartVideoRecordingImmediatelyForTesting();
+  EXPECT_TRUE(controller->is_recording_in_progress());
+  SwitchToUser2();
+  EXPECT_FALSE(controller->is_recording_in_progress());
+}
+
+TEST_F(CaptureModeTest, SwitchUsersAfterCountdownStarts) {
+  // User NORMAL_DURATION for the countdown animation so we can have predictable
+  // timings.
+  ui::ScopedAnimationDurationScaleMode animation_scale(
+      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+  auto* controller = StartCaptureSession(CaptureModeSource::kFullscreen,
+                                         CaptureModeType::kVideo);
+  // Hit Enter to begin recording, wait for 1 second, then switch users.
+  auto* event_generator = GetEventGenerator();
+  SendKey(ui::VKEY_RETURN, event_generator);
+  WaitForSeconds(1);
+  SwitchToUser2();
+  EXPECT_FALSE(controller->IsActive());
+  EXPECT_FALSE(controller->is_recording_in_progress());
+}
+
 TEST_F(CaptureModeTest, ClosingDisplayBeingFullscreenRecorded) {
   UpdateDisplay("400x400,401+0-400x400");
   auto roots = Shell::GetAllRootWindows();
@@ -1516,18 +1605,14 @@ TEST_F(CaptureModeTest, CaptureSessionSwitchedModeMetric) {
 
 // Test that cancel recording during countdown won't cause crash.
 TEST_F(CaptureModeTest, CancelCaptureDuringCountDown) {
-  ui::ScopedAnimationDurationScaleMode animatin_scale(
+  ui::ScopedAnimationDurationScaleMode animation_scale(
       ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
   StartCaptureSession(CaptureModeSource::kFullscreen, CaptureModeType::kVideo);
   // Hit Enter to begin recording, Wait for 1 second, then press ESC while count
   // down is in progress.
   auto* event_generator = GetEventGenerator();
   SendKey(ui::VKEY_RETURN, event_generator);
-  base::RunLoop loop;
-  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-      FROM_HERE, base::BindLambdaForTesting([&]() { loop.Quit(); }),
-      base::TimeDelta::FromSeconds(1));
-  loop.Run();
+  WaitForSeconds(1);
   SendKey(ui::VKEY_ESCAPE, event_generator);
 }
 
@@ -1615,7 +1700,7 @@ TEST_F(CaptureModeTest, NumberOfCaptureRegionAdjustmentsHistogram) {
 }
 
 TEST_F(CaptureModeTest, FullscreenCapture) {
-  ui::ScopedAnimationDurationScaleMode animatin_scale(
+  ui::ScopedAnimationDurationScaleMode animation_scale(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
   CaptureModeController* controller = StartCaptureSession(
       CaptureModeSource::kFullscreen, CaptureModeType::kImage);
