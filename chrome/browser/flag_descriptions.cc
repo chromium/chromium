0ee5c9efb40e4077c0d3e1485743d81f57568e6b
Majid Valipour <majidvp@chromium.org>
2020-11-18 20:37:41 +0000

    [WebOTP] Update backend flag expiry date, owners, and desc
    We are planning to start the devtrial for the new backend in the next
    two milestones so updating the flag expiry to reflect this.
    
    
    Bug: 1101050
    Change-Id: Ife0ad995225169ec0e3705dfa66d1d5e3706880c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2546992
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Auto-Submit: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828871}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2422,8 +2422,7 @@ const char kWebBundlesDescription[] =
 const char kWebOtpBackendName[] = "Web OTP";
 const char kWebOtpBackendDescription[] =
     "Enables Web OTP API that uses the specified backend.";
-const char kWebOtpBackendSmsVerification[] =
-    "SMS Verification API (requires app-hash)";
+const char kWebOtpBackendSmsVerification[] = "Code Browser API";
 const char kWebOtpBackendUserConsent[] = "User Consent API";
 const char kWebOtpBackendAuto[] = "Automatically select the backend";
 
