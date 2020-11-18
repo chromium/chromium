1dcd0fac77b281b3afdc4ae0ba9daa8b49f9e39e
Curt Clemens <cclem@google.com>
2020-11-18 00:42:05 +0000

    [Nearby] Stop progress spinner on error
    Add new properties |progress| and |hasError| to nearby-progress, which
    allow setting a specific progress percentage and disabling the spinner
    during error states respectively.
    
    |progress| defaults to null. When null, a waiting animation is shown
    where the progress bar spins around the icon. |progress| will be used
    in future CLs to set specific progress percentages, but for now, only
    the default is used.
    
    |hasError|=true causes the progress bar to be red, icons to be grey,
    progress to be 100%, and stops any spinning animation.
    
    Bug: b/163036847
    Change-Id: I74d88a1e4efefb6a5e73ba1fd77880b3397bc353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542850
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#828502}

--- chrome/browser/resources/nearby_share/nearby_progress.js
+++ chrome/browser/resources/nearby_share/nearby_progress.js
@@ -5,8 +5,8 @@
 /**
  * @fileoverview The 'nearby-progress' component shows a progress indicator for
  * a Nearby Share transfer to a remote device. It shows device icon and name,
- * and a circular progress bar that only supports an indeterminate status for
- * now.
+ * and a circular progress bar that can show either progress as a percentage or
+ * an animation if the percentage is unknown.
  */
 
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
@@ -32,5 +32,50 @@ Polymer({
       type: Object,
       value: null,
     },
+
+    /**
+     * The progress percentage to display, expressed as a number between 0 and
+     * 100. If null, then an animation representing an indeterminate state is
+     * shown, unless |hasError| is true.
+     * @type {?number}
+     */
+    progress: {
+      type: Number,
+      value: null,
+      observer: 'updateProgress_',
+    },
+
+    /**
+     * If true, then set progress stroke to red, and if in indeterminate mode,
+     * stop the animation and show 100% instead.
+     * @type {boolean}
+     */
+    hasError: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * @return {string} The css class to be applied to the progress wheel.
+   */
+  getProgressWheelClass_() {
+    if (this.hasError) {
+      return 'has-error';
+    }
+    if (this.progress === null) {
+      return 'unknown-progress';
+    }
+    return '';
+  },
+
+  /**
+   * Updates the css to set the progress percentage displayed to |value|.
+   * @param {?number} value
+   */
+  updateProgress_(value) {
+    if (value !== null && value !== undefined) {
+      this.updateStyles({'--progress-percentage': value});
+    }
   },
 });
