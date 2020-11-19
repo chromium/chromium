e35022a35f212c99d1954e104d7f2bd6bf44efd9
Pavol Marko <pmarko@google.com>
2020-11-19 10:09:19 +0000

    Expose disable_idle_sockets_close_on_memory_pressure as feature
    Adds a chrome Feature to disable closing idle sockets on memory
    pressure. This only applies to Profile NetworkContexts.
    
    Bug: 1150526
    Test: new browser test
    Change-Id: I0e4f18d8faa4f365bdd7dc404003e147ca47aa8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547723
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829161}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -207,6 +207,15 @@ const base::Feature kCrostiniEnableDlc{"CrostiniEnableDlc",
 const base::Feature kCryptAuthV2DeviceActivityStatus{
     "CryptAuthV2DeviceActivityStatus", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Disable idle sockets closing on memory pressure for NetworkContexts that
+// belong to Profiles. It only applies to Profiles because the goal is to
+// improve perceived performance of web browsing within the Chrome OS user
+// session by avoiding re-estabshing TLS connections that require client
+// certificates.
+const base::Feature kDisableIdleSocketsCloseOnMemoryPressure{
+    "disable_idle_sockets_close_on_memory_pressure",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables or disables the CryptAuth v2 DeviceSync flow. Regardless of this
 // flag, v1 DeviceSync will continue to operate until it is disabled via the
 // feature flag kDisableCryptAuthV1DeviceSync.
