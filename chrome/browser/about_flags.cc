8802ee576e688282c12b59ddf235c38852758009
Jinsuk Kim <jinsukkim@chromium.org>
2020-11-20 04:42:22 +0000

    PreviewTab: Add 3 variations in opening the tab
    When first requested, preview tab now opens to 3 different ways (peek,
    half, full) via chrome://flags. This will also be controlled
    via field trial configuration for experiments.
    
    1) 'Open peek' variation: opens the sheet to the peek state. Users
       manually swipe up to open fully. Swiping down goes to peek or
       closes the sheet.
    2) 'Open half' variation: opens the sheet to the half state. Users
       manually swipe up to open fully. Swiping down close the sheet.
    3) 'Open full' variation: opens the sheet fully immediately.
       Swiping down closes the sheet.
    
    Bug: 1146295
    Change-Id: I4936c23f3ffa17117a7ef33c7730d18bac705f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545341
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829532}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1567,6 +1567,22 @@ const FeatureEntry::FeatureVariation
          base::size(kOverridePrefsForHrefTranslateForceAuto), nullptr}};
 
 #if defined(OS_ANDROID)
+const FeatureEntry::FeatureParam kEphemeralTabOpenPeek[] = {
+    {"ephemeral_tab_open_mode", "0"}};
+const FeatureEntry::FeatureParam kEphemeralTabOpenHalf[] = {
+    {"ephemeral_tab_open_mode", "1"}};
+const FeatureEntry::FeatureParam kEphemeralTabOpenFull[] = {
+    {"ephemeral_tab_open_mode", "2"}};
+const FeatureEntry::FeatureVariation kEphemeralTabOpenVariations[] = {
+    {"Open at peek state", kEphemeralTabOpenPeek,
+     base::size(kEphemeralTabOpenPeek), nullptr},
+    {"Open at half state", kEphemeralTabOpenHalf,
+     base::size(kEphemeralTabOpenHalf), nullptr},
+    {"Open at full state", kEphemeralTabOpenFull,
+     base::size(kEphemeralTabOpenFull), nullptr}};
+#endif
+
+#if defined(OS_ANDROID)
 const FeatureEntry::FeatureParam kExploreSitesExperimental = {
     chrome::android::explore_sites::kExploreSitesVariationParameterName,
     chrome::android::explore_sites::kExploreSitesVariationExperimental};
@@ -4946,7 +4962,10 @@ const FeatureEntry kFeatureEntries[] = {
     {"enable-ephemeral-tab-bottom-sheet",
      flag_descriptions::kEphemeralTabUsingBottomSheetName,
      flag_descriptions::kEphemeralTabUsingBottomSheetDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(chrome::android::kEphemeralTabUsingBottomSheet)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(
+         chrome::android::kEphemeralTabUsingBottomSheet,
+         kEphemeralTabOpenVariations,
+         "EphemeralTabOpenMode")},
 #endif  // defined(OS_ANDROID)
 
 #if defined(OS_CHROMEOS)
