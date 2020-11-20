78d6e65eea12e67f5130b54f44693444f96bdd48
Sebastien Lalancette <seblalancette@chromium.org>
2020-11-20 17:01:20 +0000

    [iOS] Create Shared Highlighting UKM Events and Helper Functions
    Adding two UKM events around the Shared Highlighting feature, which is
    implemented on iOS, Android and Desktop. These helper functions will be
    used in those platforms' implementations of the feature.
    
    UKM Privacy Review Doc:
    https://docs.google.com/document/d/15l87rw8kjFW4wnAeHAC0kF8pTGIWuDYUUNO1tVsCQDY
    
    Bug: 1148807
    Change-Id: Ic896ea339929c30a545ec82081157200276fd1e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547763
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829694}

--- components/shared_highlighting/core/common/shared_highlighting_metrics_unittest.cc
+++ components/shared_highlighting/core/common/shared_highlighting_metrics_unittest.cc
@@ -5,6 +5,10 @@
 #include "components/shared_highlighting/core/common/shared_highlighting_metrics.h"
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/task_environment.h"
+#include "components/ukm/test_ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -12,7 +16,36 @@ namespace shared_highlighting {
 
 namespace {
 
-TEST(SharedHighlightingMetricsTest, LogTextFragmentAmbiguousMatch) {
+const char kSearchEngineUrl[] = "https://google.com";
+
+const char kSourceUkmMetric[] = "Source";
+const char kSuccessUkmMetric[] = "Success";
+const char kErrorUkmMetric[] = "Error";
+
+class SharedHighlightingMetricsTest : public testing::Test {
+ protected:
+  SharedHighlightingMetricsTest() = default;
+  ~SharedHighlightingMetricsTest() override = default;
+
+  void ValidateLinkOpenedUkm(const ukm::TestAutoSetUkmRecorder& recorder,
+                             ukm::SourceId source_id,
+                             bool success,
+                             TextFragmentLinkOpenSource source) {
+    auto entries = recorder.GetEntriesByName(
+        ukm::builders::SharedHighlights_LinkOpened::kEntryName);
+    ASSERT_EQ(1u, entries.size());
+    const ukm::mojom::UkmEntry* entry = entries[0];
+    EXPECT_EQ(source_id, entry->source_id);
+    recorder.ExpectEntryMetric(entry, kSuccessUkmMetric, success);
+    recorder.ExpectEntryMetric(entry, kSourceUkmMetric,
+                               static_cast<int64_t>(source));
+  }
+
+ private:
+  base::test::SingleThreadTaskEnvironment task_environment_;
+};
+
+TEST_F(SharedHighlightingMetricsTest, LogTextFragmentAmbiguousMatch) {
   base::HistogramTester histogram_tester;
 
   LogTextFragmentAmbiguousMatch(true);
@@ -23,10 +56,10 @@ TEST(SharedHighlightingMetricsTest, LogTextFragmentAmbiguousMatch) {
   histogram_tester.ExpectTotalCount("TextFragmentAnchor.AmbiguousMatch", 2);
 }
 
-TEST(SharedHighlightingMetricsTest, LogTextFragmentLinkOpenSource) {
+TEST_F(SharedHighlightingMetricsTest, LogTextFragmentLinkOpenSource) {
   base::HistogramTester histogram_tester;
 
-  GURL search_engine_url("https://google.com");
+  GURL search_engine_url(kSearchEngineUrl);
   LogTextFragmentLinkOpenSource(search_engine_url);
   histogram_tester.ExpectBucketCount("TextFragmentAnchor.LinkOpenSource",
                                      TextFragmentLinkOpenSource::kSearchEngine,
@@ -45,7 +78,7 @@ TEST(SharedHighlightingMetricsTest, LogTextFragmentLinkOpenSource) {
   histogram_tester.ExpectTotalCount("TextFragmentAnchor.LinkOpenSource", 3);
 }
 
-TEST(SharedHighlightingMetricsTest, LogTextFragmentMatchRate) {
+TEST_F(SharedHighlightingMetricsTest, LogTextFragmentMatchRate) {
   base::HistogramTester histogram_tester;
 
   LogTextFragmentMatchRate(/*matches=*/2, /*nb_selectors=*/2);
@@ -56,7 +89,7 @@ TEST(SharedHighlightingMetricsTest, LogTextFragmentMatchRate) {
   histogram_tester.ExpectTotalCount("TextFragmentAnchor.MatchRate", 2);
 }
 
-TEST(SharedHighlightingMetricsTest, LogTextFragmentSelectorCount) {
+TEST_F(SharedHighlightingMetricsTest, LogTextFragmentSelectorCount) {
   base::HistogramTester histogram_tester;
 
   LogTextFragmentSelectorCount(1);
@@ -67,7 +100,7 @@ TEST(SharedHighlightingMetricsTest, LogTextFragmentSelectorCount) {
   histogram_tester.ExpectTotalCount("TextFragmentAnchor.SelectorCount", 2);
 }
 
-TEST(SharedHighlightingMetricsTest, LogLinkGenerationStatus) {
+TEST_F(SharedHighlightingMetricsTest, LogLinkGenerationStatus) {
   base::HistogramTester histogram_tester;
 
   LogLinkGenerationStatus(true);
@@ -80,7 +113,7 @@ TEST(SharedHighlightingMetricsTest, LogLinkGenerationStatus) {
   histogram_tester.ExpectTotalCount("SharedHighlights.LinkGenerated", 2);
 }
 
-TEST(SharedHighlightingMetricsTest, LogLinkGenerationErrorReason) {
+TEST_F(SharedHighlightingMetricsTest, LogLinkGenerationErrorReason) {
   base::HistogramTester histogram_tester;
 
   LogLinkGenerationErrorReason(LinkGenerationError::kIncorrectSelector);
@@ -94,7 +127,7 @@ TEST(SharedHighlightingMetricsTest, LogLinkGenerationErrorReason) {
   histogram_tester.ExpectTotalCount("SharedHighlights.LinkGenerated.Error", 2);
 }
 
-TEST(SharedHighlightingMetricsTest, LogAndroidLinkGenerationErrorReason) {
+TEST_F(SharedHighlightingMetricsTest, LogAndroidLinkGenerationErrorReason) {
   base::HistogramTester histogram_tester;
 
   LogGenerateErrorTabHidden();
@@ -110,6 +143,104 @@ TEST(SharedHighlightingMetricsTest, LogAndroidLinkGenerationErrorReason) {
   histogram_tester.ExpectBucketCount("SharedHighlights.LinkGenerated.Error",
                                      LinkGenerationError::kTabCrash, 1);
   histogram_tester.ExpectTotalCount("SharedHighlights.LinkGenerated.Error", 3);
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkOpenedUkm_Success_SearchEngine) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+  bool success = true;
+
+  LogLinkOpenedUkmEvent(source_id, GURL(kSearchEngineUrl), success);
+
+  ValidateLinkOpenedUkm(ukm_recorder, source_id, success,
+                        TextFragmentLinkOpenSource::kSearchEngine);
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkOpenedUkm_Fail_SearchEngine) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+  bool success = false;
+
+  LogLinkOpenedUkmEvent(source_id, GURL(kSearchEngineUrl), success);
+
+  ValidateLinkOpenedUkm(ukm_recorder, source_id, success,
+                        TextFragmentLinkOpenSource::kSearchEngine);
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkOpenedUkm_Success_UnknownSource) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+  bool success = true;
+
+  LogLinkOpenedUkmEvent(source_id, GURL(), success);
+
+  ValidateLinkOpenedUkm(ukm_recorder, source_id, success,
+                        TextFragmentLinkOpenSource::kUnknown);
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkOpenedUkm_Fail_UnknownSource) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+  bool success = false;
+
+  LogLinkOpenedUkmEvent(source_id, GURL(), success);
+
+  ValidateLinkOpenedUkm(ukm_recorder, source_id, success,
+                        TextFragmentLinkOpenSource::kUnknown);
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkOpenedUkm_InvalidSourceId) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+
+  LogLinkOpenedUkmEvent(ukm::kInvalidSourceId, GURL(kSearchEngineUrl),
+                        /*success=*/true);
+
+  auto entries = ukm_recorder.GetEntriesByName(
+      ukm::builders::SharedHighlights_LinkOpened::kEntryName);
+  EXPECT_EQ(0u, entries.size());
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkGeneratedUkm_Success) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+
+  LogLinkGeneratedSuccessUkmEvent(source_id);
+
+  auto entries = ukm_recorder.GetEntriesByName(
+      ukm::builders::SharedHighlights_LinkGenerated::kEntryName);
+  ASSERT_EQ(1u, entries.size());
+  const ukm::mojom::UkmEntry* entry = entries[0];
+  EXPECT_EQ(source_id, entry->source_id);
+  ukm_recorder.ExpectEntryMetric(entry, kSuccessUkmMetric, true);
+  EXPECT_FALSE(ukm_recorder.GetEntryMetric(entry, kErrorUkmMetric));
+}
+
+TEST_F(SharedHighlightingMetricsTest, LinkGeneratedUkm_Error) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+  ukm::SourceId source_id = 1;
+  LinkGenerationError error = LinkGenerationError::kEmptySelection;
+
+  LogLinkGeneratedErrorUkmEvent(source_id, error);
+
+  auto entries = ukm_recorder.GetEntriesByName(
+      ukm::builders::SharedHighlights_LinkGenerated::kEntryName);
+  ASSERT_EQ(1u, entries.size());
+  const ukm::mojom::UkmEntry* entry = entries[0];
+  EXPECT_EQ(source_id, entry->source_id);
+  ukm_recorder.ExpectEntryMetric(entry, kSuccessUkmMetric, false);
+  ukm_recorder.ExpectEntryMetric(entry, kErrorUkmMetric,
+                                 static_cast<int64_t>(error));
+}
+
+TEST_F(SharedHighlightingMetricsTest,
+       LinkGeneratedUkm_Success_InvalidSourceId) {
+  ukm::TestAutoSetUkmRecorder ukm_recorder;
+
+  LogLinkGeneratedSuccessUkmEvent(ukm::kInvalidSourceId);
+
+  auto entries = ukm_recorder.GetEntriesByName(
+      ukm::builders::SharedHighlights_LinkGenerated::kEntryName);
+  EXPECT_EQ(0u, entries.size());
 }
 
 }  // namespace
