b51db0e879589d9593ec18d1da51f7b0b712ef88
David Munro <davidmunro@google.com>
2020-11-23 01:51:08 +0000

    Copy DefaultTickClock's usage note to TickClock
    Per the note, you probably don't need to use TickClock or
    DefaultTickClock anymore. Stick that note in both TickClock and
    DefaultTickClockheaders instead of just DefaultTickClock.
    
    Bug: None
    Test: None
    Change-Id: I7cdaa5b198ad06b230e2b0406c51f49f769b0794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547749
    Commit-Queue: David Munro <davidmunro@google.com>
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830072}

--- base/time/default_tick_clock.h
+++ base/time/default_tick_clock.h
@@ -13,9 +13,9 @@ namespace base {
 // DefaultTickClock is a TickClock implementation that uses TimeTicks::Now().
 // This is typically used by components that expose a SetTickClockForTesting().
 // Note: Overriding Time/TimeTicks altogether via
-// TaskEnvironment::TimeSource::MOCK_TIME is now the preferred of
-// overriding time in unit tests. As such, there shouldn't be many new use cases
-// for TickClock/DefaultTickClock anymore.
+// TaskEnvironment::TimeSource::MOCK_TIME is now the preferred way of overriding
+// time in unit tests. As such, there shouldn't be many new use cases for
+// TickClock/DefaultTickClock anymore.
 class BASE_EXPORT DefaultTickClock : public TickClock {
  public:
   ~DefaultTickClock() override;
