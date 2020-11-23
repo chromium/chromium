7b1b71a5adbc354c0301e174ad1646cc301586bb
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-11-23 19:21:16 +0000

    Revert "[LayoutNG] Avoid computing overflow for positioned elements twice"
    This reverts commit 8ebfa3e76bc9f41a86c525b063ca17f01c99648b.
    
    Reason for revert: Caused https://bugs.chromium.org/p/chromium/issues/detail?id=1150774
    
    This has subsequently been fixed by moving the layout overflow calculations into NG, however if we need to disable this new pass I'd prefer to not break any content in M89/M88.
    
    Original change's description:
    > [LayoutNG] Avoid computing overflow for positioned elements twice
    >
    > In LayoutNG the overflow of positioned elements is already computed in
    > LayoutNGBlockFlowMixin<Base>::AddLayoutOverflowFromChildren(),
    > so we don't need to compute it again in the legacy code.
    >
    > Specifically positioned elements are already considered in
    > NGPhysicalBoxFragment::ScrollableOverflowFromChildren().
    >
    > BUG=1098231
    >
    > Change-Id: I8e527617ae31ef564eeea57eb823ca93465cf62b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391256
    > Commit-Queue: Manuel Rego <rego@igalia.com>
    > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809272}
    
    TBR=rego@igalia.com,ikilpatrick@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    (cherry picked from commit 2be34662c138cbafaf7dcc82cd290b2ae517e973)
    
    Bug: 1098231, 1150774
    Change-Id: I7d1345cdc12c63dd122e3daab929b9398702cdfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552296
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#829786}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553112
    Cr-Commit-Position: refs/branch-heads/4324@{#255}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- third_party/blink/renderer/core/layout/layout_block.cc
+++ third_party/blink/renderer/core/layout/layout_block.cc
@@ -595,9 +595,6 @@ void LayoutBlock::AddLayoutOverflowFromBlockChildren() {
 
 void LayoutBlock::AddLayoutOverflowFromPositionedObjects() {
   NOT_DESTROYED();
-  if (IsLayoutNGBlockFlow())
-    return;
-
   if (ChildLayoutBlockedByDisplayLock())
     return;
 
