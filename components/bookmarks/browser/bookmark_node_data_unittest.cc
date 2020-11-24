18d62250e62be4c91ce490e9440d79143d8f1557
Yi Gu <yigu@chromium.org>
2020-11-24 17:10:29 +0000

    Disable flaky test on LaCrOS
    The test BookmarkNodeDataTest.WriteToClipboardEmptyFolder has been flaky on LaCrOS:
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=components_unittests%20(with%20patch)&tests=BookmarkNodeDataTest.WriteToClipboardEmptyFolder
    
    TBR=erikchen@chromium.org
    Bug: 1010185
    
    Change-Id: Ic30748b2c9ea93d01f794554ff0b973f00d4c396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558163
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830607}

--- components/bookmarks/browser/bookmark_node_data_unittest.cc
+++ components/bookmarks/browser/bookmark_node_data_unittest.cc
@@ -330,7 +330,8 @@ TEST_F(BookmarkNodeDataTest, MAYBE_WriteToClipboardMultipleURLs) {
   EXPECT_EQ(combined_text, clipboard_result);
 }
 
-#if defined(OS_APPLE)
+// Test is flaky on LaCrOS: crbug.com/1010185
+#if defined(OS_APPLE) || BUILDFLAG(IS_CHROMEOS_LACROS)
 #define MAYBE_WriteToClipboardEmptyFolder DISABLED_WriteToClipboardEmptyFolder
 #else
 #define MAYBE_WriteToClipboardEmptyFolder WriteToClipboardEmptyFolder
