46c618aa01355f4bb1a227dc5aae1dcdc790528e
Eric Seckler <eseckler@chromium.org>
2020-11-25 11:27:52 +0000

    feed: Don't spin the spinner when its ancestors are hidden
    Currently, the spinner used in feed will only stop() its animation if
    the spinner view itself is hidden through setVisibility(). However, in
    reality, one of its ancestor views is hidden instead.
    
    This causes a continuous invisible animation to tick at the display's
    refresh rate (usually 60fps) if the spinner was ever shown, until it is
    destroyed (when the NTP views are eventually evicted).
    
    Instead, stop the animation also when any of the spinner's ancestors
    become hidden by listening for visibility changes and window attachment
    state changes.
    
    Bug: 1151391
    Change-Id: Ib7b7a8e56f083619420fd58f4340991253402535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552781
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830957}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/shared/ui/MaterialSpinnerViewTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/shared/ui/MaterialSpinnerViewTest.java
@@ -8,6 +8,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
@@ -16,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.LooperMode;
 
 import org.chromium.chrome.R;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
@@ -23,38 +25,52 @@ import org.chromium.testing.local.LocalRobolectricTestRunner;
 /** Tests for {@link MaterialSpinnerView}. */
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
+@LooperMode(LooperMode.Mode.PAUSED)
 public class MaterialSpinnerViewTest {
+    private FrameLayout mLayout;
     private MaterialSpinnerView mMaterialSpinnerView;
+    private CircularProgressDrawable mAnimationDrawable;
 
     @Before
     public void setUp() {
-        Activity context = Robolectric.buildActivity(Activity.class).get();
-        context.setTheme(R.style.Light);
-        mMaterialSpinnerView = new MaterialSpinnerView(context);
+        Activity activity = Robolectric.setupActivity(Activity.class);
+        activity.setTheme(R.style.Light);
+
+        // Attach the spinner inside a layout, so we can hide either the spinner
+        // or the parent view (ie. the layout) in the tests. Note that we
+        // require the looper to stay paused (LooperMode.Mode.PAUSED) for the
+        // duration of the tests. Otherwise, Robolectric will run through the
+        // animation and stop it before the tests get run. Because
+        // Robolectric.setupActivity() will run the looper until idle, we call
+        // setContentView() only after launching the activity above.
+        mMaterialSpinnerView = new MaterialSpinnerView(activity);
+        mAnimationDrawable = (CircularProgressDrawable) mMaterialSpinnerView.getDrawable();
+
+        mLayout = new FrameLayout(activity);
+        mLayout.addView(mMaterialSpinnerView);
+        activity.setContentView(mLayout);
     }
 
     @Test
     public void testInit_isVisible_spinnerStarted() {
         assertThat(mMaterialSpinnerView.getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(mMaterialSpinnerView.isShown()).isTrue();
 
-        assertThat(((CircularProgressDrawable) mMaterialSpinnerView.getDrawable()).isRunning())
-                .isTrue();
+        assertThat(mAnimationDrawable.isRunning()).isTrue();
     }
 
     @Test
     public void testSetVisibility_gone_stopsSpinner() {
         mMaterialSpinnerView.setVisibility(View.GONE);
 
-        assertThat(((CircularProgressDrawable) mMaterialSpinnerView.getDrawable()).isRunning())
-                .isFalse();
+        assertThat(mAnimationDrawable.isRunning()).isFalse();
     }
 
     @Test
     public void testSetVisibility_invisible_stopsSpinner() {
         mMaterialSpinnerView.setVisibility(View.INVISIBLE);
 
-        assertThat(((CircularProgressDrawable) mMaterialSpinnerView.getDrawable()).isRunning())
-                .isFalse();
+        assertThat(mAnimationDrawable.isRunning()).isFalse();
     }
 
     @Test
@@ -62,7 +78,43 @@ public class MaterialSpinnerViewTest {
         mMaterialSpinnerView.setVisibility(View.GONE);
         mMaterialSpinnerView.setVisibility(View.VISIBLE);
 
-        assertThat(((CircularProgressDrawable) mMaterialSpinnerView.getDrawable()).isRunning())
-                .isTrue();
+        assertThat(mAnimationDrawable.isRunning()).isTrue();
+    }
+
+    @Test
+    public void testContainerSetVisibility_gone_stopsSpinner() {
+        mLayout.setVisibility(View.GONE);
+
+        assertThat(mAnimationDrawable.isRunning()).isFalse();
+    }
+
+    @Test
+    public void testContainerSetVisibility_invisible_stopsSpinner() {
+        mLayout.setVisibility(View.INVISIBLE);
+
+        assertThat(mAnimationDrawable.isRunning()).isFalse();
+    }
+
+    @Test
+    public void testContainerSetVisibility_toTrue_startsSpinner() {
+        mLayout.setVisibility(View.GONE);
+        mLayout.setVisibility(View.VISIBLE);
+
+        assertThat(mAnimationDrawable.isRunning()).isTrue();
+    }
+
+    @Test
+    public void testDetachFromWindow_stopsSpinner() {
+        mLayout.removeView(mMaterialSpinnerView);
+
+        assertThat(mAnimationDrawable.isRunning()).isFalse();
+    }
+
+    @Test
+    public void testAttachToWindow_startsSpinner() {
+        mLayout.removeView(mMaterialSpinnerView);
+        mLayout.addView(mMaterialSpinnerView);
+
+        assertThat(mAnimationDrawable.isRunning()).isTrue();
     }
 }
