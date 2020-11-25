cb493a13b5f8d20eb40d9d3c8c9c74a667a55b6c
Christoph Schwering <schwering@google.com>
2020-11-25 01:09:30 +0000

    [Autofill] Made JSON transpiler Python 3 compatible.
    The script failed under Python 3 if the locale encoding wasn't UTF-8.
    
    Bug: 1112471, 1147608
    Change-Id: If8c96f514cd0eb78f140240ea4307d48673b1a99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2555420
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#830818}

--- components/autofill/core/browser/pattern_provider/transpile_default_regex_patterns.py
+++ components/autofill/core/browser/pattern_provider/transpile_default_regex_patterns.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
+import io
 import json
 import sys
 
@@ -54,55 +54,61 @@ def build_cpp_map_population(input):
   return lines
 
 
-def build_cpp_function(cpp, output):
-  output.write('// Copyright 2020 The Chromium Authors. All rights reserved.\n')
-  output.write('// Use of this source code is governed by a BSD-style license ')
-  output.write('that can be\n')
-  output.write('// found in the LICENSE file.\n')
-  output.write('\n')
-  output.write('#include "components/autofill/core/browser/pattern_provider/'\
+def build_cpp_function(cpp, output_handle):
+  def output(s):
+    # unicode() exists and is necessary only in Python 2, not in Python 3.
+    if sys.version_info[0] < 3:
+      s = unicode(s, 'utf-8')
+    output_handle.write(s)
+
+  output('// Copyright 2020 The Chromium Authors. All rights reserved.\n')
+  output('// Use of this source code is governed by a BSD-style license ')
+  output('that can be\n')
+  output('// found in the LICENSE file.\n')
+  output('\n')
+  output('#include "components/autofill/core/browser/pattern_provider/'\
                'default_regex_patterns.h"\n')
-  output.write('#include "components/autofill/core/common/language_code.h"\n')
-  output.write('\n')
-  output.write('namespace autofill {\n')
-  output.write('\n')
-  output.write('PatternProvider::Map CreateDefaultRegexPatterns() {\n')
-  output.write('  struct JsonPattern {\n')
-  output.write('    const char* name;\n')
-  output.write('    const char* language;\n')
-  output.write('    const char* positive_pattern;\n')
-  output.write('    const char* negative_pattern;\n')
-  output.write('    float positive_score;\n')
-  output.write('    uint8_t match_field_attributes;\n')
-  output.write('    uint16_t match_field_input_types;\n')
-  output.write('  };\n')
-  output.write('\n')
+  output('#include "components/autofill/core/common/language_code.h"\n')
+  output('\n')
+  output('namespace autofill {\n')
+  output('\n')
+  output('PatternProvider::Map CreateDefaultRegexPatterns() {\n')
+  output('  struct JsonPattern {\n')
+  output('    const char* name;\n')
+  output('    const char* language;\n')
+  output('    const char* positive_pattern;\n')
+  output('    const char* negative_pattern;\n')
+  output('    float positive_score;\n')
+  output('    uint8_t match_field_attributes;\n')
+  output('    uint16_t match_field_input_types;\n')
+  output('  };\n')
+  output('\n')
   for line in build_cpp_map_population(cpp):
-    output.write(line)
-    output.write('\n')
-  output.write('  PatternProvider::Map map;\n')
-  output.write('  size_t len = sizeof(patterns) / sizeof(patterns[0]);\n')
-  output.write('  for (size_t i = 0; i < len; ++i) {\n')
-  output.write('    const JsonPattern& p = patterns[i];\n')
-  output.write('    MatchingPattern mp;\n')
-  output.write('    mp.language = LanguageCode(p.language);\n')
-  output.write('    mp.positive_pattern = p.positive_pattern;\n')
-  output.write('    mp.negative_pattern = '
+    output(line)
+    output('\n')
+  output('  PatternProvider::Map map;\n')
+  output('  size_t len = sizeof(patterns) / sizeof(patterns[0]);\n')
+  output('  for (size_t i = 0; i < len; ++i) {\n')
+  output('    const JsonPattern& p = patterns[i];\n')
+  output('    MatchingPattern mp;\n')
+  output('    mp.language = LanguageCode(p.language);\n')
+  output('    mp.positive_pattern = p.positive_pattern;\n')
+  output('    mp.negative_pattern = '
                'p.negative_pattern ? p.negative_pattern : "";\n')
-  output.write('    mp.positive_score = p.positive_score;\n')
-  output.write('    mp.match_field_input_types = p.match_field_input_types;\n')
-  output.write('    mp.match_field_attributes = p.match_field_attributes;\n')
-  output.write('    map[p.name][LanguageCode(p.language)].push_back(mp);\n')
-  output.write('  }\n')
-  output.write('  return map;\n')
-  output.write('}\n')
-  output.write('\n')
-  output.write('}')
+  output('    mp.positive_score = p.positive_score;\n')
+  output('    mp.match_field_input_types = p.match_field_input_types;\n')
+  output('    mp.match_field_attributes = p.match_field_attributes;\n')
+  output('    map[p.name][LanguageCode(p.language)].push_back(mp);\n')
+  output('  }\n')
+  output('  return map;\n')
+  output('}\n')
+  output('\n')
+  output('}')
 
 if __name__ == '__main__':
   input_file = sys.argv[1]
   output_file = sys.argv[2]
-  with open(input_file, 'r') as handle:
-    input = json.loads(handle.read())
-    with open(output_file, 'w') as output:
-      build_cpp_function(input, output)
+  with io.open(input_file, 'r', encoding='utf-8') as input_handle:
+    input_json = json.load(input_handle)
+    with io.open(output_file, 'w', encoding='utf-8') as output_handle:
+      build_cpp_function(input_json, output_handle)
