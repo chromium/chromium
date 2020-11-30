893c7ed7c137a999ff0ee3e6d044a2569ac821ff
Robert Sesek <rsesek@chromium.org>
2020-11-30 22:54:08 +0000

    mac: Use NSWritingDirectionOverride instead of NSTextWritingDirectionOverride.
    The latter is deprecated, and they have the same constant value, so no
    change in behavior.
    
    Bug: 1153883
    Change-Id: I530981290fd5101995550e68d65a013f5722683e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2564395
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832049}

--- content/browser/renderer_host/webmenurunner_mac.mm
+++ content/browser/renderer_host/webmenurunner_mac.mm
@@ -66,8 +66,7 @@
                         item->type != blink::mojom::MenuItem::Type::kGroup)];
   [menuItem setTarget:self];
 
-  // Set various alignment/language attributes. Note that many (if not most) of
-  // these attributes are functional only on 10.6 and above.
+  // Set various alignment/language attributes.
   base::scoped_nsobject<NSMutableDictionary> attrs(
       [[NSMutableDictionary alloc] initWithCapacity:3]);
   base::scoped_nsobject<NSMutableParagraphStyle> paragraphStyle(
@@ -82,12 +81,10 @@
   [attrs setObject:paragraphStyle forKey:NSParagraphStyleAttributeName];
 
   if (item->has_text_direction_override) {
-    base::scoped_nsobject<NSNumber> directionValue(
-        [[NSNumber alloc] initWithInteger:
-            writingDirection + NSTextWritingDirectionOverride]);
-    base::scoped_nsobject<NSArray> directionArray(
-        [[NSArray alloc] initWithObjects:directionValue.get(), nil]);
-    [attrs setObject:directionArray forKey:NSWritingDirectionAttributeName];
+    if (@available(macOS 10.11, *)) {
+      [attrs setObject:@[ @(writingDirection | NSWritingDirectionOverride) ]
+                forKey:NSWritingDirectionAttributeName];
+    }
   }
 
   [attrs setObject:[NSFont menuFontOfSize:_fontSize]
