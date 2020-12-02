c5a972ef7d53c3b4b650f7286677da4d124be557
Clark DuVall <cduvall@chromium.org>
2020-12-02 01:37:50 +0000

    Add another @DoNotInline to autofill_assistant
    This was found by running the check from http://crrev.com/c/2567476.
    
    Bug: 1154061
    Change-Id: Ibcbdd2e8f2005b82cb784a2b16d34cbfc53bcb94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568223
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832611}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectAction.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectAction.java
@@ -4,11 +4,16 @@
 
 package org.chromium.chrome.browser.autofill_assistant;
 
+import org.chromium.base.annotations.DoNotInline;
+
 import java.util.List;
 
 /**
  * Interface for direct action containers.
  */
+// TODO(crbug.com/1145287): DexSplitter has issues with interfaces that have a single implementer
+// inside a DFM.
+@DoNotInline
 interface AutofillAssistantDirectAction {
     /** Returns a list of name and aliases for this direct action. */
     List<String> getNames();
