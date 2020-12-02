d4e2b4daf3d2b5765b062c82f3331b7027a651cc
Mikhail Khokhlov <khokhlov@google.com>
2020-12-02 11:53:41 +0000

    [tools/perf] Enable proxy power metric on Pixel 4a bots
    This CL supplies two command-line flags that are necessary to compute
    the proxy power metric (based on CPU usage and power profiles).
    
     * --fetch-power-profile downloads the power profile from the cloud.
    
     * --performance-mode=keep disables the Telemetry power management,
     making it possible to collect cpu_freq events. Note that there will
     likely be a drop in observed power rails metrics after this CL lands,
     because devices will use more efficient CPU scheduling policy.
    
    Change-Id: I9aeecd80cda1a883e1994ba70b0a439cd83f8749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566995
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#832770}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -527,9 +527,12 @@ BUILDERS = {
     },
     'android-pixel4a_power-perf': {
         'tests': [{
-            'isolate': 'performance_test_suite',
+            'isolate':
+            'performance_test_suite',
             'extra_args': [
                 '--experimental-tbmv3-metrics',
+                '--fetch-power-profile',
+                '--performance-mode=keep',
             ],
         }],
         'platform':
