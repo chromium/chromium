add649aa3a4ef3453f34017ad391f16ef19d4c7e
Abigail Klein <abigailbklein@google.com>
2020-12-02 19:30:51 +0000

    [Live Caption] Only install SODA on ChromeOS if the UseSodaForLiveCaption flag is enabled.
    Live Caption currently doesn't work on ChromeOS because it's trying to
    go through the (not yet ready) SODA pipeline rather than the Cloud
    pipeline. This CL fixes that bug by preventing the SODA pipeline from
    beginning to set up, thus preventing the error that ensues.
    
    Bug: 1055150
    Change-Id: I3827b0b7e262c3126980af777595c08b34f2cf7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568369
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#832914}

--- chrome/browser/chromeos/accessibility/soda_installer_impl_chromeos.cc
+++ chrome/browser/chromeos/accessibility/soda_installer_impl_chromeos.cc
@@ -5,8 +5,10 @@
 #include "chrome/browser/chromeos/accessibility/soda_installer_impl_chromeos.h"
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "chromeos/dbus/dlcservice/dlcservice_client.h"
+#include "media/base/media_switches.h"
 
 namespace {
 
@@ -27,6 +29,9 @@ SODAInstallerImplChromeOS::SODAInstallerImplChromeOS() = default;
 SODAInstallerImplChromeOS::~SODAInstallerImplChromeOS() = default;
 
 void SODAInstallerImplChromeOS::InstallSODA(PrefService* prefs) {
+  if (!base::FeatureList::IsEnabled(media::kUseSodaForLiveCaption))
+    return;
+
   // Install SODA DLC.
   chromeos::DlcserviceClient::Get()->Install(
       kSODADlcName,
