49cf3b16428de7d8163f0924f2ac071e944aea43
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-12-03 02:02:18 +0000

    [M88][Extensions] Support specifying specific frame IDs in chrome.scripting
    Add support for specifying specific frame IDs (one or multiple) in the
    chrome.scripting APIs (scripting.executeScript and scripting.insertCSS).
    Add tests for the same.
    
    Bug: 1148878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547432
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829593}
    
    (cherry picked from commit e6a6223105cf1e5b05bb4530b46157824e457b51)
    TBR=karandeepb@chromium.org
    
    Change-Id: I66085e0c8e7d5de4f7ea44f34d53f3f78bad9db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570044
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#542}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- chrome/browser/extensions/api/scripting/scripting_api.cc
+++ chrome/browser/extensions/api/scripting/scripting_api.cc
@@ -51,36 +51,9 @@ bool HasPermissionToInjectIntoFrame(const PermissionsData& permissions,
   return permissions.CanAccessPage(url, tab_id, error);
 }
 
-// Returns true if the `permissions` allow for injection into the given `tab`.
-// If false, populates `error`.
-bool HasPermissionToInject(const PermissionsData& permissions,
-                           int tab_id,
-                           content::WebContents* tab,
-                           std::string* error) {
-  // TODO(devlin): Support specifying multiple frames.
-  return HasPermissionToInjectIntoFrame(permissions, tab_id,
-                                        tab->GetMainFrame(), error);
-}
-
-void ExecuteScript(ScriptExecutor* script_executor,
-                   const std::string& code,
-                   const Extension& extension,
-                   ScriptExecutor::FrameScope frame_scope,
-                   bool user_gesture,
-                   ScriptExecutor::ScriptFinishedCallback callback) {
-  script_executor->ExecuteScript(
-      HostID(HostID::EXTENSIONS, extension.id()), UserScript::ADD_JAVASCRIPT,
-      code, frame_scope, {ExtensionApiFrameIdMap::kTopFrameId},
-      ScriptExecutor::MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
-      ScriptExecutor::DEFAULT_PROCESS,
-      /* webview_src */ GURL(), /* script_url */ GURL(), user_gesture,
-      base::nullopt, ScriptExecutor::JSON_SERIALIZED_RESULT,
-      std::move(callback));
-}
-
 // Returns true if the `target` can be accessed with the given `permissions`.
-// If the target can be accessed, populates `script_executor_out` with the
-// associated ScriptExecutor and `frame_scope_out` with the appropriate scope;
+// If the target can be accessed, populates `script_executor_out`,
+// `frame_scope_out`, and `frame_ids_out` with the appropriate values;
 // if the target cannot be accessed, populates `error_out`.
 bool CanAccessTarget(const PermissionsData& permissions,
                      const api::scripting::InjectionTarget& target,
@@ -88,6 +61,7 @@ bool CanAccessTarget(const PermissionsData& permissions,
                      bool include_incognito_information,
                      ScriptExecutor** script_executor_out,
                      ScriptExecutor::FrameScope* frame_scope_out,
+                     std::vector<int>* frame_ids_out,
                      std::string* error_out) {
   content::WebContents* tab = nullptr;
   TabHelper* tab_helper = nullptr;
@@ -99,6 +73,11 @@ bool CanAccessTarget(const PermissionsData& permissions,
     return false;
   }
 
+  if ((target.all_frames && *target.all_frames == true) && target.frame_ids) {
+    *error_out = "Cannot specify both 'allFrames' and 'frameIds'.";
+    return false;
+  }
+
   ScriptExecutor* script_executor = tab_helper->script_executor();
   DCHECK(script_executor);
 
@@ -107,17 +86,36 @@ bool CanAccessTarget(const PermissionsData& permissions,
           ? ScriptExecutor::INCLUDE_SUB_FRAMES
           : ScriptExecutor::SPECIFIED_FRAMES;
 
-  // TODO(devlin): It'd be best to do all the permission checks for the frames
-  // on the browser side, including child frames. Today, we only check the
-  // main frame, and then let the ScriptExecutor inject into all child frames
-  // (there's a permission check at the time of the injection in the renderer).
-  // TODO(devlin): We error out if the extension doesn't have access to the top
-  // frame, even if it may inject in child frames. This is inconsistent with
-  // content scripts (which can execute on child frames), but consistent with
-  // the old tabs.executeScript() API.
-  if (!HasPermissionToInject(permissions, target.tab_id, tab, error_out))
-    return false;
+  std::vector<int> frame_ids;
+  if (target.frame_ids) {
+    // NOTE: This creates a copy, but it's should always be very cheap, and it
+    // lets us keep |target| const.
+    frame_ids = *target.frame_ids;
+  } else {
+    frame_ids.push_back(ExtensionApiFrameIdMap::kTopFrameId);
+  }
 
+  // TODO(devlin): We error out if the extension doesn't have access to the top
+  // frame, even if it may inject in child frames if allFrames is true. This is
+  // inconsistent with content scripts (which can execute on child frames), but
+  // consistent with the old tabs.executeScript() API.
+  for (int frame_id : frame_ids) {
+    content::RenderFrameHost* frame =
+        ExtensionApiFrameIdMap::GetRenderFrameHostById(tab, frame_id);
+    if (!frame) {
+      *error_out = base::StringPrintf("No frame with id %d in tab with id %d",
+                                      frame_id, target.tab_id);
+      return false;
+    }
+
+    DCHECK_EQ(content::WebContents::FromRenderFrameHost(frame), tab);
+    if (!HasPermissionToInjectIntoFrame(permissions, target.tab_id, frame,
+                                        error_out)) {
+      return false;
+    }
+  }
+
+  *frame_ids_out = std::move(frame_ids);
   *frame_scope_out = frame_scope;
   *script_executor_out = script_executor;
   return true;
@@ -141,9 +139,10 @@ ExtensionFunction::ResponseAction ScriptingExecuteScriptFunction::Run() {
   std::string error;
   ScriptExecutor* script_executor = nullptr;
   ScriptExecutor::FrameScope frame_scope = ScriptExecutor::SPECIFIED_FRAMES;
+  std::vector<int> frame_ids;
   if (!CanAccessTarget(*extension()->permissions_data(), injection.target,
                        browser_context(), include_incognito_information(),
-                       &script_executor, &frame_scope, &error)) {
+                       &script_executor, &frame_scope, &frame_ids, &error)) {
     return RespondNow(Error(std::move(error)));
   }
 
@@ -155,9 +154,13 @@ ExtensionFunction::ResponseAction ScriptingExecuteScriptFunction::Run() {
   std::string code_to_execute =
       base::StringPrintf("(%s)()", injection.function->c_str());
 
-  ExecuteScript(
-      script_executor, code_to_execute, *extension(), frame_scope,
-      user_gesture(),
+  script_executor->ExecuteScript(
+      HostID(HostID::EXTENSIONS, extension()->id()), UserScript::ADD_JAVASCRIPT,
+      code_to_execute, frame_scope, frame_ids,
+      ScriptExecutor::MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
+      ScriptExecutor::DEFAULT_PROCESS,
+      /* webview_src */ GURL(), /* script_url */ GURL(), user_gesture(),
+      base::nullopt, ScriptExecutor::JSON_SERIALIZED_RESULT,
       base::BindOnce(&ScriptingExecuteScriptFunction::OnScriptExecuted, this));
 
   return RespondLater();
@@ -202,9 +205,10 @@ ExtensionFunction::ResponseAction ScriptingInsertCSSFunction::Run() {
   std::string error;
   ScriptExecutor* script_executor = nullptr;
   ScriptExecutor::FrameScope frame_scope = ScriptExecutor::SPECIFIED_FRAMES;
+  std::vector<int> frame_ids;
   if (!CanAccessTarget(*extension()->permissions_data(), injection.target,
                        browser_context(), include_incognito_information(),
-                       &script_executor, &frame_scope, &error)) {
+                       &script_executor, &frame_scope, &frame_ids, &error)) {
     return RespondNow(Error(std::move(error)));
   }
   DCHECK(script_executor);
@@ -230,9 +234,8 @@ ExtensionFunction::ResponseAction ScriptingInsertCSSFunction::Run() {
   constexpr UserScript::RunLocation kRunLocation = UserScript::DOCUMENT_START;
   script_executor->ExecuteScript(
       HostID(HostID::EXTENSIONS, extension()->id()), UserScript::ADD_CSS,
-      *injection.css, frame_scope, {ExtensionApiFrameIdMap::kTopFrameId},
-      ScriptExecutor::MATCH_ABOUT_BLANK, kRunLocation,
-      ScriptExecutor::DEFAULT_PROCESS,
+      *injection.css, frame_scope, frame_ids, ScriptExecutor::MATCH_ABOUT_BLANK,
+      kRunLocation, ScriptExecutor::DEFAULT_PROCESS,
       /* webview_src */ GURL(), /* script_url */ GURL(), user_gesture(), origin,
       ScriptExecutor::NO_RESULT,
       base::BindOnce(&ScriptingInsertCSSFunction::OnCSSInserted, this));
