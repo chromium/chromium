7672be058caa26d14678f8b0d4a06de56c155309
Aya Nader Elgendy‎ <ayag@chromium.org>
2020-12-07 12:29:06 +0000

    Revert "Fix a typo in pin dialog error message"
    This reverts commit 642148be74fff7c34f3f31938fa8cec7ce16d7b4.
    
    Reason for revert: M88 regression causing a wrong message
    to be displayed in the SAML PIN dialog.
    
    Bug: 1152911
    
    Original change's description:
    > Fix a typo in pin dialog error message
    >
    > Fix a typo in the pin dialog error message which read
    > "1 attempts left" instead of "1 attempt left" when the user has
    > attempted incorrect pins and there's only 1 attempt remaining.
    >
    > Fixed: 1060695
    > Change-Id: Ic9f51d03f87e8d4090cb6f93a5968e0436fdcdf8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487106
    > Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: Aya Nader Elgendy‎ <ayag@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#819389}
    
    TBR=emaxx@chromium.org,fabiansommer@chromium.org,ayag@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    (cherry picked from commit b448ab93cb8fece874b7dbdad859da9046ca8f86)
    
    Skip-Translation-Screenshots-Check: True
    Change-Id: Iab86aac19beaa44d0918950c576a36a742526fdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567781
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#832954}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573099
    Commit-Queue: Aya Nader Elgendy‎ <ayag@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#630}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- chrome/browser/chromeos/login/security_token_login_browsertest.cc
+++ chrome/browser/chromeos/login/security_token_login_browsertest.cc
@@ -67,8 +67,9 @@ constexpr char kPinDialogDefaultTitle[] = "Smart card PIN";
 constexpr char kPinDialogInvalidPinTitle[] = "Invalid PIN.";
 constexpr char kPinDialogInvalidPin2AttemptsTitle[] =
     "Invalid PIN. 2 attempts left";
+// TODO(crbug.com/1060695): Fix the incorrect plural in the message.
 constexpr char kPinDialogInvalidPin1AttemptTitle[] =
-    "Invalid PIN. 1 attempt left";
+    "Invalid PIN. 1 attempts left";
 constexpr char kPinDialogNoAttemptsLeftTitle[] =
     "Maximum allowed attempts exceeded.";
 
