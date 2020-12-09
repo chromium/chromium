3904e0fe3b6a83c4aa22c960a28558612178870f
Shu-yu Guo <syg@chromium.org>
2020-12-09 18:14:23 +0000

    Ship top-level await
    I2S with 3 LGTMs:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/nAkEvjsqQbE/m/ilxfKaQ2CAAJ
    
    Bug: 1022182
    Bug: 994315
    Bug: v8:9344
    
    Change-Id: I86a997cc8be009f415361cd394b63e21bf61fa24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568557
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835253}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -86,7 +86,7 @@ const base::Feature kForceSynchronousHTMLParsing{
 
 // Enables top-level await in modules.
 const base::Feature kTopLevelAwait{"TopLevelAwait",
-                                   base::FEATURE_DISABLED_BY_DEFAULT};
+                                   base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enable EditingNG by default. This feature is for a kill switch.
 const base::Feature kEditingNG{"EditingNG", base::FEATURE_ENABLED_BY_DEFAULT};
--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/evaluation-order-1-worker.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/evaluation-order-1-worker.js
@@ -1,7 +1,7 @@
 globalThis.expectedLog = [
   "step-1-1", "step-1-2",
-  "microtask",
   "global-error", "error",
+  "microtask",
 ];
 
 globalThis.test_load.step_timeout(() => globalThis.testDone(), 0);
