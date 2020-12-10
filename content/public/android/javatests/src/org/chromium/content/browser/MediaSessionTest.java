c3fd59435685d9c5947ee9f0d0fdbf4054a8d3db
Yue Zhang <yuezhanggg@chromium.org>
2020-12-10 18:05:00 +0000

    Sheriff: disable 3 flaky MediaSessionTest
    These tests are flaky on android-pie-x86-rel.
    
    TBR: mlamouri@chromium.org
    Bug: 1157494
    Change-Id: I80ae660c7bbe0e073de2e890bd65d86600ce03e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585126
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835733}

--- content/public/android/javatests/src/org/chromium/content/browser/MediaSessionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/MediaSessionTest.java
@@ -361,6 +361,7 @@ public class MediaSessionTest {
     @Test
     @MediumTest
     @Feature({"MediaSession"})
+    @DisabledTest(message = "crbug.com/1157494")
     public void testAudioStopsIfLostFocus() throws Exception {
         Assert.assertEquals(
                 AudioManager.AUDIOFOCUS_LOSS, mAudioFocusChangeListener.getAudioFocusState());
@@ -384,6 +385,7 @@ public class MediaSessionTest {
     @Test
     @SmallTest
     @Feature({"MediaSession"})
+    @DisabledTest(message = "crbug.com/1157494")
     public void testVideoStopsIfLostFocus() throws Exception {
         Assert.assertEquals(
                 AudioManager.AUDIOFOCUS_LOSS, mAudioFocusChangeListener.getAudioFocusState());
@@ -481,6 +483,7 @@ public class MediaSessionTest {
     @Test
     @MediumTest
     @Feature({"MediaSession"})
+    @DisabledTest(message = "crbug.com/1157494")
     public void testSessionSuspendedAfterFocusLossWhenPlaying() throws Exception {
         ArrayList<StateRecord> expectedStates = new ArrayList<StateRecord>();
         expectedStates.add(new StateRecord(true, false));
