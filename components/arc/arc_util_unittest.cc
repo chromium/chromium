a57c0aab048a500cf8962713ebd0e41cdfdeed43
Yusuke Sato <yusukes@chromium.org>
2020-12-10 18:15:58 +0000

    arc: Add unit tests for arc::ConfigureUpstartJobs
    Now that the function is in arc_util.h, it's better to have tests
    in arc_util_unittest.cc (rather than indirectly test the function
    in arc_vm_client_adapter_unittest.cc.)
    
    Bug: None
    Test: components_unittests
    Change-Id: I75ef434ee39f28cdb545342cec535337849382c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582996
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835746}

--- components/arc/arc_util_unittest.cc
+++ components/arc/arc_util_unittest.cc
@@ -6,6 +6,8 @@
 
 #include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "ash/public/cpp/app_types.h"
 #include "base/base_switches.h"
@@ -14,7 +16,11 @@
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/run_loop.h"
+#include "base/task/post_task.h"
+#include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/dbus/upstart/fake_upstart_client.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/fake_user_manager.h"
 #include "components/user_manager/scoped_user_manager.h"
@@ -64,7 +70,81 @@ class FakeUser : public user_manager::User {
   DISALLOW_COPY_AND_ASSIGN(FakeUser);
 };
 
-using ArcUtilTest = testing::Test;
+class ArcUtilTest : public testing::Test {
+ public:
+  ArcUtilTest() { chromeos::UpstartClient::InitializeFake(); }
+  ArcUtilTest(const ArcUtilTest&) = delete;
+  ArcUtilTest& operator=(const ArcUtilTest&) = delete;
+  ~ArcUtilTest() override = default;
+
+  void SetUp() override {
+    run_loop_ = std::make_unique<base::RunLoop>();
+    RemoveUpstartStartStopJobFailures();
+  }
+
+  void TearDown() override { run_loop_.reset(); }
+
+ protected:
+  void InjectUpstartStartJobFailure(const std::string& job_name_to_fail) {
+    auto* upstart_client = chromeos::FakeUpstartClient::Get();
+    upstart_client->set_start_job_cb(base::BindLambdaForTesting(
+        [job_name_to_fail](const std::string& job_name,
+                           const std::vector<std::string>& env) {
+          // Return success unless |job_name| is |job_name_to_fail|.
+          return job_name != job_name_to_fail;
+        }));
+  }
+
+  void InjectUpstartStopJobFailure(const std::string& job_name_to_fail) {
+    auto* upstart_client = chromeos::FakeUpstartClient::Get();
+    upstart_client->set_stop_job_cb(base::BindLambdaForTesting(
+        [job_name_to_fail](const std::string& job_name,
+                           const std::vector<std::string>& env) {
+          // Return success unless |job_name| is |job_name_to_fail|.
+          return job_name != job_name_to_fail;
+        }));
+  }
+
+  void StartRecordingUpstartOperations() {
+    auto* upstart_client = chromeos::FakeUpstartClient::Get();
+    upstart_client->set_start_job_cb(
+        base::BindLambdaForTesting([this](const std::string& job_name,
+                                          const std::vector<std::string>& env) {
+          upstart_operations_.emplace_back(job_name, true);
+          return true;
+        }));
+    upstart_client->set_stop_job_cb(
+        base::BindLambdaForTesting([this](const std::string& job_name,
+                                          const std::vector<std::string>& env) {
+          upstart_operations_.emplace_back(job_name, false);
+          return true;
+        }));
+  }
+
+  void RecreateRunLoop() { run_loop_ = std::make_unique<base::RunLoop>(); }
+
+  base::RunLoop* run_loop() { return run_loop_.get(); }
+
+  const std::vector<std::pair<std::string, bool>>& upstart_operations() const {
+    return upstart_operations_;
+  }
+
+ private:
+  void RemoveUpstartStartStopJobFailures() {
+    auto* upstart_client = chromeos::FakeUpstartClient::Get();
+    upstart_client->set_start_job_cb(
+        chromeos::FakeUpstartClient::StartStopJobCallback());
+    upstart_client->set_stop_job_cb(
+        chromeos::FakeUpstartClient::StartStopJobCallback());
+  }
+
+  std::unique_ptr<base::RunLoop> run_loop_;
+  base::test::TaskEnvironment task_environment_;
+
+  // List of upstart operations recorded. When it's "start" the boolean is set
+  // to true.
+  std::vector<std::pair<std::string, bool>> upstart_operations_;
+};
 
 TEST_F(ArcUtilTest, IsArcAvailable_None) {
   auto* command_line = base::CommandLine::ForCurrentProcess();
@@ -311,6 +391,94 @@ TEST_F(ArcUtilTest, GenerateFirstStageFstab) {
   EXPECT_TRUE(base::ReadFileToString(fstab, &content));
   EXPECT_EQ(std::string::npos, content.find(kFakeCombinedBuildPropPath));
   EXPECT_NE(std::string::npos, content.find(kAnotherFakeCombinedBuildPropPath));
+}
+
+TEST_F(ArcUtilTest, ConfigureUpstartJobs_Success) {
+  std::deque<JobDesc> jobs{
+      JobDesc{"Job_2dA", UpstartOperation::JOB_STOP, {}},
+      JobDesc{"Job_2dB", UpstartOperation::JOB_STOP_AND_START, {}},
+      JobDesc{"Job_2dC", UpstartOperation::JOB_START, {}},
+  };
+  bool result = false;
+  StartRecordingUpstartOperations();
+  ConfigureUpstartJobs(jobs,
+                       base::BindLambdaForTesting([&result, this](bool r) {
+                         result = r;
+                         run_loop()->Quit();
+                       }));
+  run_loop()->Run();
+  EXPECT_TRUE(result);
+
+  auto ops = upstart_operations();
+  ASSERT_EQ(4u, ops.size());
+  EXPECT_EQ(ops[0].first, "Job_2dA");
+  EXPECT_FALSE(ops[0].second);
+  EXPECT_EQ(ops[1].first, "Job_2dB");
+  EXPECT_FALSE(ops[1].second);
+  EXPECT_EQ(ops[2].first, "Job_2dB");
+  EXPECT_TRUE(ops[2].second);
+  EXPECT_EQ(ops[3].first, "Job_2dC");
+  EXPECT_TRUE(ops[3].second);
+}
+
+TEST_F(ArcUtilTest, ConfigureUpstartJobs_StopFail) {
+  std::deque<JobDesc> jobs{
+      JobDesc{"Job_2dA", UpstartOperation::JOB_STOP, {}},
+      JobDesc{"Job_2dB", UpstartOperation::JOB_STOP_AND_START, {}},
+      JobDesc{"Job_2dC", UpstartOperation::JOB_START, {}},
+  };
+  // Confirm that failing to stop a job is ignored.
+  bool result = false;
+  InjectUpstartStopJobFailure("Job_2dA");
+  ConfigureUpstartJobs(jobs,
+                       base::BindLambdaForTesting([&result, this](bool r) {
+                         result = r;
+                         run_loop()->Quit();
+                       }));
+  run_loop()->Run();
+  EXPECT_TRUE(result);
+
+  // Do the same for the second task.
+  RecreateRunLoop();
+  result = false;
+  InjectUpstartStopJobFailure("Job_2dB");
+  ConfigureUpstartJobs(jobs,
+                       base::BindLambdaForTesting([&result, this](bool r) {
+                         result = r;
+                         run_loop()->Quit();
+                       }));
+  run_loop()->Run();
+  EXPECT_TRUE(result);
+}
+
+TEST_F(ArcUtilTest, ConfigureUpstartJobs_StartFail) {
+  std::deque<JobDesc> jobs{
+      JobDesc{"Job_2dA", UpstartOperation::JOB_STOP, {}},
+      JobDesc{"Job_2dB", UpstartOperation::JOB_STOP_AND_START, {}},
+      JobDesc{"Job_2dC", UpstartOperation::JOB_START, {}},
+  };
+  // Confirm that failing to start a job is not ignored.
+  bool result = true;
+  InjectUpstartStartJobFailure("Job_2dB");
+  ConfigureUpstartJobs(jobs,
+                       base::BindLambdaForTesting([&result, this](bool r) {
+                         result = r;
+                         run_loop()->Quit();
+                       }));
+  run_loop()->Run();
+  EXPECT_FALSE(result);
+
+  // Do the same for the third task.
+  RecreateRunLoop();
+  result = true;
+  InjectUpstartStartJobFailure("Job_2dC");
+  ConfigureUpstartJobs(std::move(jobs),
+                       base::BindLambdaForTesting([&result, this](bool r) {
+                         result = r;
+                         run_loop()->Quit();
+                       }));
+  run_loop()->Run();
+  EXPECT_FALSE(result);
 }
 
 }  // namespace
