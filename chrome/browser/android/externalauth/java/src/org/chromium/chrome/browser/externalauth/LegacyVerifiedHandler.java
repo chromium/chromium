b8ec794ff654efb65539a042dd828b44bf161191
Nick Burris <nburris@chromium.org>
2020-12-10 19:49:27 +0000

    Create LegacyExternalAuthUtils
    * Create LegacyExternalAuthUtils and related classes, which
      are empty subclasses of the ExternalAuthUtils classes. These will be
      referenced downstream temporarily while ExternalAuthUtils is moved to
      //components.
    * Create empty new target //components/externalauth/android:java so we
      can add the downstream dependency before the migration.
    
    This is patch 2/6 for refactoring ExternalAuthUtils into //components.
    
    Bug: 1144858
    Change-Id: I928ee2998f7042c85e64d8e91e275d23b789fc79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568689
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835776}

---
+++ chrome/browser/android/externalauth/java/src/org/chromium/chrome/browser/externalauth/LegacyVerifiedHandler.java
@@ -0,0 +1,22 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.externalauth;
+
+import android.content.Context;
+
+/**
+ * Temporary class, identical to {@link VerifiedHandler}, for migration.
+ *
+ * This class will be referenced temporarily while VerifiedHandler is moved to components.
+ *
+ * TODO(crbug.com/1144858): Remove this class after downstream uses
+ * components.externalauth.VerifiedHandler.
+ */
+public class LegacyVerifiedHandler extends VerifiedHandler {
+    public LegacyVerifiedHandler(
+            Context context, LegacyExternalAuthUtils externalAuthUtils, int authRequirements) {
+        super(context, externalAuthUtils, authRequirements);
+    }
+}
