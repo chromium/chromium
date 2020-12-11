dfcd9c846ae625f64736d1ceb4a7bb6f944f2868
Ella Ge <eirage@chromium.org>
2020-12-11 17:25:40 +0000

    [Sheriff] Disable testAdjustBackGroundViewAccessibilityImportance
    Tbr: yuezhanggg@chromium.org
    No-try: true
    Bug: 1157518
    Change-Id: I4538feaa30f325565140df47b0c43aad2114cb00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585639
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836159}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -777,6 +777,7 @@ public class TabGridDialogTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1157518")
     public void testAdjustBackGroundViewAccessibilityImportance() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         createTabs(cta, false, 2);
