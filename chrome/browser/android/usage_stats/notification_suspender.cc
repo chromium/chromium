7d5214a6ed1fdc5a0960973fea1c99b33f2e977d
Maksim Ivanov <emaxx@chromium.org>
2020-12-11 00:38:17 +0000

    Fix potential use-after-move in //c/b/android/usage_stats/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: I34a773f1610ff151708534e644b5ddda4d27f9b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584964
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835922}

--- chrome/browser/android/usage_stats/notification_suspender.cc
+++ chrome/browser/android/usage_stats/notification_suspender.cc
@@ -99,9 +99,9 @@ static void JNI_NotificationSuspender_StoreNotificationResources(
     GURL origin(std::move(origin_strings[i]));
     if (!origin.is_valid() || !origin.SchemeIsHTTPOrHTTPS())
       continue;
-    resources_by_context[GetContext(profile, origin)].emplace_back(
-        NotificationResourceData{std::move(id_strings[i]), std::move(origin),
-                                 std::move(resources[i])});
+    PlatformNotificationContext* context = GetContext(profile, origin);
+    resources_by_context[context].emplace_back(NotificationResourceData{
+        std::move(id_strings[i]), std::move(origin), std::move(resources[i])});
   }
 
   // Store resources in each context.
