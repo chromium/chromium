4e45a84f23dbb3726200b835be77c18626ea48de
Hongchan Choi <hongchan@chromium.org>
2020-12-11 19:56:52 +0000

    Remove redundant comments from ScriptProcessorNode
    The comment doesn't seem actionable and not sure what the alternative is.
    
    Bug: 854229
    Change-Id: I87e1ac40e874303e73f043a87d9d56a90b7585e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577533
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836226}

--- third_party/blink/renderer/modules/webaudio/script_processor_node.cc
+++ third_party/blink/renderer/modules/webaudio/script_processor_node.cc
@@ -440,19 +440,15 @@ ScriptProcessorNode* ScriptProcessorNode::Create(
     case 0:
       // Choose an appropriate size.  For an AudioContext, we need to
       // choose an appropriate size based on the callback buffer size.
-      // For OfflineAudioContext, there's no callback buffer size, so
-      // just use the minimum valid buffer size.
       if (context.HasRealtimeConstraint()) {
-        // TODO(crbug.com/854229): Due to the incompatible constructor between
-        // AudioDestinationNode and RealtimeAudioDestinationNode, casting
-        // directly from |destination()| is impossible. This is a temporary
-        // workaround until the refactoring is completed.
         RealtimeAudioDestinationHandler& destination_handler =
             static_cast<RealtimeAudioDestinationHandler&>(
                 context.destination()->GetAudioDestinationHandler());
         buffer_size =
             ChooseBufferSize(destination_handler.GetCallbackBufferSize());
       } else {
+        // For OfflineAudioContext, there's no callback buffer size, so
+        // just use the minimum valid buffer size.
         buffer_size = 256;
       }
       break;
