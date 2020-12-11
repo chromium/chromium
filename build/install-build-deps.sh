5b17a8f1d34deab6fbd2508a0bc88317f765e506
Shane Fitzpatrick <shanefitz@google.com>
2020-12-11 00:01:30 +0000

    Updating build/install-build-deps.sh to allow libbrlapi0.8
    Bug: 1157630
    Change-Id: I0be59d1997832087f12c1373b4d4112240bb65a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586087
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#835904}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -498,7 +498,9 @@ else
   dev_list="${dev_list} libudev0"
   nacl_list="${nacl_list} libudev0:i386"
 fi
-if package_exists libbrlapi0.7; then
+if package_exists libbrlapi0.8; then
+  dev_list="${dev_list} libbrlapi0.8"
+elif package_exists libbrlapi0.7; then
   dev_list="${dev_list} libbrlapi0.7"
 elif package_exists libbrlapi0.6; then
   dev_list="${dev_list} libbrlapi0.6"
