f54c49bcf074e764586611382f5b491f6565361e
Xing Liu <xingliu@chromium.org>
2020-12-12 01:47:57 +0000

    Read later: Fix an issue that cached flag is not added correctly.
    Read later needs to be added to ChromeCachedFlags.
    
    TBR=dtrainor@chromium.org
    
    Bug: 1157113
    Change-Id: Ie6dc92314603df4e8e44cd684a74a9ad64f4f1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587622
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836369}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -78,7 +78,8 @@ public class ChromeCachedFlags {
                 ChromeFeatureList.TABBED_APP_OVERFLOW_MENU_ICONS,
                 ChromeFeatureList.TABBED_APP_OVERFLOW_MENU_REGROUP,
                 ChromeFeatureList.TABBED_APP_OVERFLOW_MENU_THREE_BUTTON_ACTIONBAR,
-                ChromeFeatureList.USE_CHIME_ANDROID_SDK);
+                ChromeFeatureList.USE_CHIME_ANDROID_SDK,
+                ChromeFeatureList.READ_LATER);
         // clang-format on
         CachedFeatureFlags.cacheNativeFlags(featuresToCache);
         CachedFeatureFlags.cacheAdditionalNativeFlags();
