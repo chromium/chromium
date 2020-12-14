14199559d982f8673506f6b935136e5682d9cc31
Jana Grill <janagrill@google.com>
2020-12-14 17:32:12 +0000

    Adjust powerwash requirements notification for FlexOrgs
    Adjust IDS_POWERWASH_REQUEST_MESSAGE_FOR_ARC and
    IDS_POWERWASH_REQUEST_MESSAGE_FOR_CROSTINI to support FlexOrgs by
    adjusting the domain placeholder names and descriptions to make it
    easier for the translators to understand the usage of the texts.
    
    Additionally replace a call to
    BrowserPolicyConnectorChromeOS::GetEnterpriseDisplayDomain with a call
    to BrowserPolicyConnectorChromeOS::GetEnterpriseDomainManager.
    
    Bug: b:172685729
    Test: Manual
    Change-Id: I7636b1b9123fa2714cb002d1561e0665628ca8ec
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587029
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Jana Grill <janagrill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836641}

--- chrome/browser/chromeos/policy/powerwash_requirements_checker.cc
+++ chrome/browser/chromeos/policy/powerwash_requirements_checker.cc
@@ -56,10 +56,10 @@ constexpr mc::SystemNotificationWarningLevel kNotificationLevel =
 const char kNotificationLearnMoreLink[] =
     "https://support.google.com/chromebook?p=factory_reset";
 
-base::string16 GetEnterpriseDisplayDomain() {
+base::string16 GetEnterpriseManager() {
   policy::BrowserPolicyConnectorChromeOS* connector =
       g_browser_process->platform_part()->browser_policy_connector_chromeos();
-  return base::UTF8ToUTF16(connector->GetEnterpriseDisplayDomain());
+  return base::UTF8ToUTF16(connector->GetEnterpriseDomainManager());
 }
 
 void OnNotificationClicked(Profile* profile) {
@@ -205,7 +205,7 @@ void PowerwashRequirementsChecker::ShowNotification() {
   auto notification = ash::CreateSystemNotification(
       mc::NOTIFICATION_TYPE_SIMPLE, notification_id,
       l10n_util::GetStringUTF16(IDS_POWERWASH_REQUEST_TITLE),
-      l10n_util::GetStringFUTF16(message_id, GetEnterpriseDisplayDomain()),
+      l10n_util::GetStringFUTF16(message_id, GetEnterpriseManager()),
       base::string16{}, GURL{},
       mc::NotifierId(mc::NotifierType::SYSTEM_COMPONENT, notification_id),
       std::move(rich_data), std::move(delegate), kNotificationIcon,
