2d6c0690d4df941f038e0722e7154fd93ca7133d
arthursonzogni <arthursonzogni@chromium.org>
2020-12-14 08:42:24 +0000

    CSP: Give actionnable error for plugin-type empty.
    Users wanting to prevent plugins to be used were providing an empty list
    of plugin type to the 'plugin-types' directive. As a result, they were
    getting a console error, despite getting the intended behavior. It was
    not clear to the user what was wrong.
    
    Users should use object-src: 'none' instead.
    This patch reflects this into the error message.
    
    Bug: 1111546
    Fixed: 1111546
    Change-Id: I64a266582e33fa3417293ebfdb2e6bf87e08578e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584943
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836552}

--- third_party/blink/renderer/core/frame/csp/content_security_policy.cc
+++ third_party/blink/renderer/core/frame/csp/content_security_policy.cc
@@ -1295,21 +1295,23 @@ void ContentSecurityPolicy::ReportDuplicateDirective(const String& name) {
 void ContentSecurityPolicy::ReportInvalidPluginTypes(
     const String& plugin_type) {
   String message;
-  if (plugin_type.IsNull())
+  if (plugin_type.IsNull()) {
     message =
         "'plugin-types' Content Security Policy directive is empty; all "
-        "plugins will be blocked.\n";
-  else if (plugin_type == "'none'")
+        "plugins will be blocked. To disallow all plugins, the \"object-src "
+        "'none'\" directive should be used instead.\n";
+  } else if (plugin_type == "'none'") {
     message =
         "Invalid plugin type in 'plugin-types' Content Security Policy "
         "directive: '" +
         plugin_type +
         "'. Did you mean to set the object-src directive to 'none'?\n";
-  else
+  } else {
     message =
         "Invalid plugin type in 'plugin-types' Content Security Policy "
         "directive: '" +
         plugin_type + "'.\n";
+  }
   LogToConsole(message);
 }
 
