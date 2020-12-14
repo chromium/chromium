0a4ad0d3306a9428daf53ed40d517fd55d3613b9
Fabrice de Gans-Riberi <fdegans@chromium.org>
2020-12-14 22:50:07 +0000

    Add a //net-based implementation of Open Screen UdpSocket
    This adds a //net-based implementation of the Open Screen UdpSocket
    interface. Its main use is for unit tests that do not have or require
    the network service.
    
    Bug: 1110490
    Change-Id: Ic542c3c29d05b5c58d36efb6fd31d0eeac3cefe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580908
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836822}

---
+++ components/openscreen_platform/net_udp_socket.h
@@ -0,0 +1,64 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPENSCREEN_PLATFORM_NET_UDP_SOCKET_H_
+#define COMPONENTS_OPENSCREEN_PLATFORM_NET_UDP_SOCKET_H_
+
+#include <memory>
+
+#include "net/base/io_buffer.h"
+#include "net/base/ip_endpoint.h"
+#include "net/socket/udp_socket.h"
+#include "third_party/openscreen/src/platform/api/udp_socket.h"
+
+namespace net {
+class IPEndPoint;
+}
+
+namespace openscreen_platform {
+
+class NetUdpSocket : public openscreen::UdpSocket {
+ public:
+  NetUdpSocket(Client* client, const openscreen::IPEndpoint& local_endpoint);
+  ~NetUdpSocket() final;
+
+  NetUdpSocket& operator=(const NetUdpSocket&) = delete;
+  NetUdpSocket& operator=(NetUdpSocket&&) = delete;
+
+ private:
+  void SendErrorToClient(openscreen::Error::Code openscreen_error,
+                         int net_error);
+  void DoRead();
+  bool HandleRecvFromResult(int result);
+  void OnRecvFromCompleted(int result);
+  void OnSendToCompleted(int result);
+
+  // openscreen::UdpSocket implementation.
+  bool IsIPv4() const final;
+  bool IsIPv6() const final;
+  openscreen::IPEndpoint GetLocalEndpoint() const final;
+  void Bind() final;
+  void SetMulticastOutboundInterface(
+      openscreen::NetworkInterfaceIndex ifindex) final;
+  void JoinMulticastGroup(const openscreen::IPAddress& address,
+                          openscreen::NetworkInterfaceIndex ifindex) final;
+  void SendMessage(const void* data,
+                   size_t length,
+                   const openscreen::IPEndpoint& dest) final;
+  void SetDscp(openscreen::UdpSocket::DscpMode state) final;
+
+  Client* const client_;
+
+  // The local endpoint can change as a result of Bind() calls.
+  openscreen::IPEndpoint local_endpoint_;
+  net::UDPSocket udp_socket_;
+
+  scoped_refptr<net::IOBuffer> read_buffer_;
+  net::IPEndPoint from_address_;
+  bool send_pending_ = false;
+};
+
+}  // namespace openscreen_platform
+
+#endif  // COMPONENTS_OPENSCREEN_PLATFORM_NET_UDP_SOCKET_H_
