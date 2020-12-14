b3a8c5f82cbca73fe8d6c5653b0346c86874e0d6
Hongchan Choi <hongchan@chromium.org>
2020-12-14 21:09:08 +0000

    Install a server-side feature switch for RealtimeAudioWorkletThread
    This patch introduces a new server-side setting that enables
    the real-time priority thread for the RealtimeAudioWorkletThread object.
    Without the server interaction, the object will fall back to the
    NORMAL priority.
    
    See also: https://critique-ng.corp.google.com/cl/332043086
    
    (cherry picked from commit 3397c26de7a55dd3c5b827b149898a89b881f14c)
    
    Bug: 1119403
    Change-Id: I1067ccd5a8da4be2fb82450028f5697fbbfcdb59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412221
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#836186}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588303
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#913}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- third_party/blink/renderer/modules/webaudio/realtime_audio_worklet_thread.cc
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_worklet_thread.cc
@@ -4,6 +4,8 @@
 
 #include "third_party/blink/renderer/modules/webaudio/realtime_audio_worklet_thread.h"
 
+#include "base/feature_list.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_worklet_global_scope.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
@@ -24,9 +26,15 @@ RealtimeAudioWorkletThread::RealtimeAudioWorkletThread(
   ThreadCreationParams params =
       ThreadCreationParams(ThreadType::kRealtimeAudioWorkletThread);
 
-  // TODO(crbug.com/1022888): The worklet thread priority is always NORMAL
-  // on OS_LINUX and OS_CHROMEOS regardless of the thread priority setting.
-  params.thread_priority = base::ThreadPriority::REALTIME_AUDIO;
+  // Use a higher priority thread only when it is allowed by Finch.
+  if (base::FeatureList::IsEnabled(
+          features::kAudioWorkletThreadRealtimePriority)) {
+    // TODO(crbug.com/1022888): The worklet thread priority is always NORMAL on
+    // Linux and Chrome OS regardless of this thread priority setting.
+    params.thread_priority = base::ThreadPriority::REALTIME_AUDIO;
+  } else {
+    params.thread_priority = base::ThreadPriority::NORMAL;
+  }
 
   if (++s_ref_count_ == 1)
     EnsureSharedBackingThread(params);
