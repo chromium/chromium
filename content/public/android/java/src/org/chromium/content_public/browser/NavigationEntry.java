d14b9fdec63bd31d258b3c56145b1da7b17cf757
Yaron Friedman <yfriedman@chromium.org>
2020-12-14 16:46:02 +0000

    [GURL] Prepare for NavigationEntry migration.
    Introduce a temporary helper to get internal repo off this function.
    BUG=783819
    
    Change-Id: Ifd650b1361b5ddd4617500c4448190ae75d64c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588250
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836627}

--- content/public/android/java/src/org/chromium/content_public/browser/NavigationEntry.java
+++ content/public/android/java/src/org/chromium/content_public/browser/NavigationEntry.java
@@ -53,6 +53,12 @@ public class NavigationEntry {
         return mUrl;
     }
 
+    // NOTE(yfriedman): Use |getUrl| instead. Added to faciliate moving internal repo.
+    @Deprecated
+    public String getUrlString() {
+        return mUrl;
+    }
+
     /**
      * @return The virtual URL, when nonempty, will override the actual URL of
      *         the page when we display it to the user. This allows us to have
