8c53a70b9cf21ab9eae7a7f1afb64ccff54f1873
Kyle Horimoto <khorimoto@chromium.org>
2020-12-15 03:04:38 +0000

    [CrOS PhoneHub] Create UserActionRecorder which logs 28DA usage
    This class logs metrics when core Phone Hub functionality is used,
    which will be used to log our 28DA user count.
    
    Note that this class is not actually instantiated; a follow-up CL
    creates an instance of this class and calls its instance functions.
    
    (cherry picked from commit ceb9035ff6e5d01c5597fbc482c954d2abd0e151)
    
    Bug: 1150634, 1106937
    Change-Id: I224a1dd25d5d7f48b601392843d1a31a72d6743c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576474
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#835664}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591876
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#935}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

---
+++ chromeos/components/phonehub/fake_user_action_recorder.h
@@ -0,0 +1,54 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_PHONEHUB_FAKE_USER_ACTION_RECORDER_H_
+#define CHROMEOS_COMPONENTS_PHONEHUB_FAKE_USER_ACTION_RECORDER_H_
+
+#include <stddef.h>
+
+#include "chromeos/components/phonehub/user_action_recorder.h"
+
+namespace chromeos {
+namespace phonehub {
+
+class FakeUserActionRecorder : public UserActionRecorder {
+ public:
+  FakeUserActionRecorder();
+  ~FakeUserActionRecorder() override;
+
+  size_t num_ui_opened_events() const { return num_ui_opened_events_; }
+  size_t num_tether_attempts() const { return num_tether_attempts_; }
+  size_t num_dnd_attempts() const { return num_dnd_attempts_; }
+  size_t num_find_my_device_attempts() const {
+    return num_find_my_device_attempts_;
+  }
+  size_t num_browser_tabs_opened() const { return num_browser_tabs_opened_; }
+  size_t num_notification_dismissals() const {
+    return num_notification_dismissals_;
+  }
+  size_t num_notification_replies() const { return num_notification_replies_; }
+
+ private:
+  // UserActionRecorder:
+  void RecordUiOpened() override;
+  void RecordTetherConnectionAttempt() override;
+  void RecordDndAttempt() override;
+  void RecordFindMyDeviceAttempt() override;
+  void RecordBrowserTabOpened() override;
+  void RecordNotificationDismissAttempt() override;
+  void RecordNotificationReplyAttempt() override;
+
+  size_t num_ui_opened_events_ = 0u;
+  size_t num_tether_attempts_ = 0u;
+  size_t num_dnd_attempts_ = 0u;
+  size_t num_find_my_device_attempts_ = 0u;
+  size_t num_browser_tabs_opened_ = 0u;
+  size_t num_notification_dismissals_ = 0u;
+  size_t num_notification_replies_ = 0u;
+};
+
+}  // namespace phonehub
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_PHONEHUB_FAKE_USER_ACTION_RECORDER_H_
