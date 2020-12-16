3f32fabe71dd8c0fcfdcea4612e9778b07dca538
Christian Biesinger <cbiesinger@chromium.org>
2020-12-16 20:12:40 +0000

    [AspectRatio] Correctly handle "auto m/n" with display: block
    The code previously effectively ignored the auto.
    
    flex-aspect-ratio-024 is now failing in legacy because the
    LayoutReplaced code, unlike ComputeLogicalWidthFromAspectRatio,
    does not take an override logical height into account.
    
    (cherry picked from commit 154de628d10b74e623bdddff40adaf642ee61580)
    
    Bug: 1157740
    Change-Id: I593674569bb6d6ae76b957a09908bbade60c8fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585641
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#836645}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595294
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#1007}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -4117,7 +4117,7 @@ void LayoutBox::ComputeLogicalWidth(
       ContainerWidthInInlineDirection();
   LayoutBlock* cb = ContainingBlock();
 
-  if (StyleRef().LogicalWidth().IsAuto() &&
+  if (StyleRef().LogicalWidth().IsAuto() && !treat_as_replaced &&
       ComputeLogicalWidthFromAspectRatio(&computed_values.extent_)) {
     /* we're good */
   } else if (treat_as_replaced) {
@@ -4637,12 +4637,13 @@ void LayoutBox::ComputeLogicalHeight(
     }
 
     bool check_min_max_height = false;
+    bool compute_size_as_replaced = ShouldComputeSizeAsReplaced();
 
     // The parent box is flexing us, so it has increased or decreased our
     // height. We have to grab our cached flexible height.
     if (HasOverrideLogicalHeight()) {
       h = Length::Fixed(OverrideLogicalHeight());
-    } else if (ShouldComputeSizeAsReplaced()) {
+    } else if (compute_size_as_replaced) {
       h = Length::Fixed(ComputeReplacedLogicalHeight() +
                         BorderAndPaddingLogicalHeight());
     } else {
@@ -4652,7 +4653,8 @@ void LayoutBox::ComputeLogicalHeight(
 
     LayoutUnit height_result;
     if (check_min_max_height) {
-      if (ShouldComputeLogicalHeightFromAspectRatio()) {
+      if (!compute_size_as_replaced &&
+          ShouldComputeLogicalHeightFromAspectRatio()) {
         NGBoxStrut border_padding(BorderStart() + ComputedCSSPaddingStart(),
                                   BorderEnd() + ComputedCSSPaddingEnd(),
                                   BorderBefore() + ComputedCSSPaddingBefore(),
