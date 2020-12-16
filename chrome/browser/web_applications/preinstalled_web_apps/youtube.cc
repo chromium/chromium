9d709ab0442a45a8efffe3e24bfece2aad5e7ed4
Alan Cutter <alancutter@chromium.org>
2020-12-16 08:51:48 +0000

    Make YouTube default web app open in a window by default for new Chrome OS users
    This CL updates the YouTube default web app to install as opening in a
    window.
    This only affects new users on Chrome OS.
    
    Before:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=481158&signed_aid=oxtHhds0ncHcPrMr76xdvA==&inline=1
    
    After:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=481159&signed_aid=B2AAZpgZRJqHpeXt61yyrw==&inline=1
    
    Bug: 1159306
    Change-Id: I59b3f83c801596520c37995d849a156e4c0fd89c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592076
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837488}

--- chrome/browser/web_applications/preinstalled_web_apps/youtube.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/youtube.cc
@@ -17,7 +17,11 @@ ExternalInstallOptions GetConfigForYouTube() {
   ExternalInstallOptions options(
       /*install_url=*/GURL(
           "https://www.youtube.com/s/notifications/manifest/cr_install.html"),
+#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+      /*user_display_mode=*/DisplayMode::kStandalone,
+#else
       /*user_display_mode=*/DisplayMode::kBrowser,
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
       /*install_source=*/ExternalInstallSource::kExternalDefault);
 
   options.user_type_allowlist = {"unmanaged", "managed"};
