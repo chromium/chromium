65adb6a6acbb3ae70bb3d56a323a304a8638f034
Alex Ilin <alexilin@chromium.org>
2020-12-17 13:26:06 +0000

    [sheriff] Disable UrlOverridingTest#testRedirectionFromIntentWarm test
    The test is failing on builder "test-n-phone"
    
    TBR: mthiesse@chromium.org
    Bug: 1159767
    Change-Id: I89b47f56c0d19315780b9158778c04a05d953c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595537
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838052}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -36,6 +36,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
@@ -451,6 +452,7 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1159767")
     public void testRedirectionFromIntentWarm() throws Exception {
         Context context = ContextUtils.getApplicationContext();
         // TODO(crbug.com/1153686): This test times out on M tablets.
