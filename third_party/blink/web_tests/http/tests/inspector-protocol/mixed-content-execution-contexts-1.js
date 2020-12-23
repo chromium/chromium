bbf439e4e27540642b4ed172269274d2889c9cd6
Andrey Kosyakov <caseq@chromium.org>
2020-12-23 02:05:41 +0000

    DevTools: prepare tests for introduction of ExecutionContextDescription.uniqueID
    This temporarily removes the newly added field from objects being dumped,
    so that the test expectations do not change when upstream CL lands.
    
    Bug: v8:11268, chromium:1101897
    Change-Id: I8caa2bcf4a5b71250fb0712129771215c3a215bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601547
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839032}

--- third_party/blink/web_tests/http/tests/inspector-protocol/mixed-content-execution-contexts-1.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/mixed-content-execution-contexts-1.js
@@ -4,6 +4,9 @@
   await dp.Runtime.enable();
   let count = 0;
   dp.Runtime.onExecutionContextCreated(event => {
+    // TODO(caseq): remove following v8 roll past https://chromium-review.googlesource.com/c/v8/v8/+/2594538
+    delete event.params.context.uniqueId;
+
     testRunner.log(event);
     if (++count === 2) // page context + frame context.
       testRunner.completeTest();
--- third_party/blink/web_tests/http/tests/inspector-protocol/mixed-content-execution-contexts-2.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/mixed-content-execution-contexts-2.js
@@ -3,6 +3,9 @@
       `Tests that execution contexts are reported for frames that were blocked due to mixed content when Runtime is enabled *after* navigation.`);
   await page.navigate('https://devtools.test:8443/inspector-protocol/resources/mixed-content-iframe.html');
   dp.Runtime.onExecutionContextCreated(event => {
+    // TODO(caseq): remove following v8 roll past https://chromium-review.googlesource.com/c/v8/v8/+/2594538
+    delete event.params.context.uniqueId;
+
     testRunner.log(event);
   });
   await dp.Runtime.enable();
