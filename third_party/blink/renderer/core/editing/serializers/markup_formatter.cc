4193ecf74963e69eb6b635d429ed5944bf30124a
Mason Freed <masonfreed@chromium.org>
2020-12-24 00:30:31 +0000

    Fix serialization for <noscript> contents when script is disabled
    Previous to this CL, serialization for <noscript> nodes in a
    document with (per spec) no execution context would still *not*
    escape (e.g. '<' --> '&lt;') the contents. This allowed a sanitizer
    bypass.
    
    I added a fairly comprehensive test of the many fragment parser entry
    points here. Before this CL (and on other browsers):
     - Chrome would never escape <noscript> contents. (This bug)
     - Firefox would never escape <noscript> contents. (Should raise a bug)
     - Safari *always* escapes <noscript> contents, *even* when it
       shouldn't, such as with div.innerHTML, div.insertAdjacentHTML,
       and document.write on the main document. (Should raise a bug)
    
    An additional set of differences showed up for the *parsing* escaping
    behavior:
     - Chrome would (and still does) de-escape <noscript> contents when
       parsing in a script-disabled context, such as DOMParser or
       createHTMLDocument.innerHTML. It does *not* de-escape <noscript>
       contents in script-enabled contexts, such as div.innerHTML and
       document.write. This all seems correct to me.
     - Safari behaves identically to Chrome here (correct).
     - Firefox de-escapes <noscript> contents in some cases (e.g.
       XMLHTTPRequest, document.write) but not others (e.g.
       template.innerHTML, createHTMLDocument.innerHTML, and
       createHTMLDocument.createContextualFragment).
    
    I looked for a spec issue, but didn't find one. If there really isn't
    a spec issue for this, we should raise one.
    
    Fixed: 1160635
    Change-Id: I361a72409093e69b074495c475e9d647c6c58300
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602447
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839214}

--- third_party/blink/renderer/core/editing/serializers/markup_formatter.cc
+++ third_party/blink/renderer/core/editing/serializers/markup_formatter.cc
@@ -385,18 +385,28 @@ EntityMask MarkupFormatter::EntityMaskForText(const Text& text) const {
   if (text.parentElement())
     parent_name = &(text.parentElement())->TagQName();
 
-  if (parent_name && (*parent_name == html_names::kScriptTag ||
-                      *parent_name == html_names::kStyleTag ||
-                      *parent_name == html_names::kXmpTag ||
-                      *parent_name == html_names::kIFrameTag ||
-                      *parent_name == html_names::kPlaintextTag ||
-                      *parent_name == html_names::kNoembedTag ||
-                      *parent_name == html_names::kNoframesTag ||
-                      (*parent_name == html_names::kNoscriptTag &&
-                       text.GetExecutionContext() &&
-                       text.GetExecutionContext()->CanExecuteScripts(
-                           kNotAboutToExecuteScript))))
-    return kEntityMaskInCDATA;
+  if (parent_name) {
+    // For a NOSCRIPT tag, escape the string unless there's an execution context
+    // and scripting is enabled. Note that some documents (e.g. the one created
+    // by DOMParser) are created with a script-enabled execution context, but no
+    // DOMWindow. But per spec [1], they should behave as if they have no
+    // execution context. So check for a DOMWindow here.
+    // [1] https://html.spec.whatwg.org/multipage/dynamic-markup-insertion.html
+    bool is_noscript_tag_with_script_enabled =
+        *parent_name == html_names::kNoscriptTag &&
+        text.GetExecutionContext() && text.GetDocument().domWindow() &&
+        text.GetExecutionContext()->CanExecuteScripts(kNotAboutToExecuteScript);
+    if (*parent_name == html_names::kScriptTag ||
+        *parent_name == html_names::kStyleTag ||
+        *parent_name == html_names::kXmpTag ||
+        *parent_name == html_names::kIFrameTag ||
+        *parent_name == html_names::kPlaintextTag ||
+        *parent_name == html_names::kNoembedTag ||
+        *parent_name == html_names::kNoframesTag ||
+        is_noscript_tag_with_script_enabled) {
+      return kEntityMaskInCDATA;
+    }
+  }
   return kEntityMaskInHTMLPCDATA;
 }
 
