0f22863a206b68aa86442fccfe8301562c3e697b
sczs <sczs@chromium.org>
2020-12-29 19:49:15 +0000

    [ios] Ignores newSuggestionsInCategory if Discover is enabled.
    The crash from 1145109 seems to happens due to an update from
    newSuggestionsInCategory triggering a layoutSubviews pass on
    Discover.
    
    Since ContentSuggestionsServiceObserver is used for the old content
    suggestions system (and not Discover's) this callback will now be
    ignored if the Discover feed is being used.
    
    TBR=sczs@chromium.org
    
    (cherry picked from commit f265708066f271cc521e9ed4bd18b9fe3050ec2a)
    
    Bug: 1145109
    Change-Id: Iec40e155028f6406e702147371d208dc69471efc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605284
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#839456}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605273
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#1310}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -161,6 +161,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
   if (self) {
     _contentSuggestionsEnabled =
         prefService->FindPreference(prefs::kArticlesForYouEnabled);
+    // TODO(crbug.com/1085419): Stop observing this Service once DiscoverFeed is
+    // launched.
     _suggestionBridge =
         std::make_unique<ContentSuggestionsServiceBridge>(self, contentService);
     _contentService = contentService;
@@ -438,6 +440,11 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 - (void)contentSuggestionsService:
             (ntp_snippets::ContentSuggestionsService*)suggestionsService
          newSuggestionsInCategory:(ntp_snippets::Category)category {
+  // Ignore newSuggestionsInCategory if the DiscoverFeed is enabled, if not
+  // these might cause some unecessary section updates and crashes.
+  if (IsDiscoverFeedEnabled())
+    return;
+
   ContentSuggestionsCategoryWrapper* wrapper =
       [ContentSuggestionsCategoryWrapper wrapperWithCategory:category];
   if (!self.sectionInformationByCategory[wrapper]) {
@@ -462,8 +469,6 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
                   statusChangedTo:(ntp_snippets::CategoryStatus)status {
   // Ignore all ContentSuggestionsService if the DiscoverFeed is enabled, if not
   // these might cause some unecessary section updates and crashes.
-  // TODO(crbug.com/1105624): Stop observing this Service once DiscoverFeed is
-  // launched.
   if (IsDiscoverFeedEnabled())
     return;
 
