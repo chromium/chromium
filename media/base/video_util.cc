aba6331a18cb9355a59900526a8f171fc2389a0e
Eugene Zemtsov <eugene@chromium.org>
2020-12-29 06:45:51 +0000

    Switch video frame scaling back to kFilterBox
    This reverts: https://crrev.com/670be8adae8396738fb2b9c3ba146b34ff578f38
    The overread bug in libyuv has been fixed.
    
    Bug: 1158178
    Change-Id: I3440b601fd0a49e5a8b21a7aeb11d04c4a40e471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605835
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839549}

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -503,10 +503,7 @@ bool I420CopyWithPadding(const VideoFrame& src_frame, VideoFrame* dst_frame) {
 Status ConvertAndScaleFrame(const VideoFrame& src_frame,
                             VideoFrame& dst_frame,
                             std::vector<uint8_t>& tmp_buf) {
-  // Can't use kFilterBox, because it can cause source buffer overreads while
-  // scaling.
-  // TODO(https://bugs.chromium.org/p/libyuv/issues/detail?id=875)
-  constexpr auto kDefaultFiltering = libyuv::kFilterBilinear;
+  constexpr auto kDefaultFiltering = libyuv::kFilterBox;
   if (!src_frame.IsMappable() || !dst_frame.IsMappable())
     return Status(StatusCode::kUnsupportedFrameFormatError);
 
