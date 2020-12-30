71b822ea22fec0556dc5dff30a6dd8010fdc280c
Austin Eng <enga@chromium.org>
2020-12-30 19:20:11 +0000

    Revert "Disable more failing GPU tests on Linux"
    This reverts commit 2cb1105fd9e44aa7ade955df0fe3bd1f0f335043.
    
    Reason for revert: Linux CFI suppression for relocated vulkan-loader dep has been added.
    
    Original change's description:
    > Disable more failing GPU tests on Linux
    >
    > Some tests were already disabled in https://crrev.com/c/2605403
    > But others are still failing:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20CFI/19334/overview
    >
    > TBR=jdarpinian@chromium.org
    >
    > Bug: 1162117
    > Change-Id: I615aaba11b736e81c9051dc867c2605ddcdd1615
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602435
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Commit-Queue: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#839567}
    
    TBR=jdarpinian@chromium.org,poromov@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    TBR=jdarpinian@chromium.org
    
    Bug: 1162117
    Change-Id: I52c9eae27d2402e26e861325a1013eb9397ce858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2607157
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839770}

--- gpu/command_buffer/service/webgpu_decoder_unittest.cc
+++ gpu/command_buffer/service/webgpu_decoder_unittest.cc
@@ -72,13 +72,7 @@ class WebGPUDecoderTest : public ::testing::Test {
   static const DawnDeviceClientID kDeviceClientID = 0u;
 };
 
-// TODO(crbug.com/1162117): gl_tests failing on Linux
-#if defined(OS_LINUX)
-#define MAYBE_DawnCommands DISABLED_DawnCommands
-#else
-#define MAYBE_DawnCommands DawnCommands
-#endif
-TEST_F(WebGPUDecoderTest, MAYBE_DawnCommands) {
+TEST_F(WebGPUDecoderTest, DawnCommands) {
   if (!WebGPUSupported()) {
     LOG(ERROR) << "Test skipped because WebGPU isn't supported";
     return;
