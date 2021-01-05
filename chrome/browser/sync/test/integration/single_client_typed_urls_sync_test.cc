28ec5b95138b02b460df693f88e761f4d83141df
Sahel Sharify <sahel@chromium.org>
2021-01-05 15:42:13 +0000

    [Sheriff] Reland "Disabled flaky SingleClientTypedUrlsSyncTest tests on Android"
    This reverts commit 075822e5141b9f0b869b814dc857e9fffb310854.
    
    Reason for revert: SingleClientTypedUrlsSyncTest.TwoVisits is still
    flaky on Android after the fix landed in crbug.com/1159404
    
    Flake portal:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyWgsSBUZsYWtlIk9jaHJvbWl1bUBhbmRyb2lkX3N5bmNfaW50ZWdyYXRpb25fdGVzdHNAU2luZ2xlQ2xpZW50VHlwZWRVcmxzU3luY1Rlc3QuVHdvVmlzaXRzDA
    
    Original change's description:
    > Revert "Disabled flaky SingleClientTypedUrlsSyncTest tests on Android"
    >
    > This reverts commit a23b68c36ea4af3d421bac3ef17d9b939a7fc2fc.
    >
    > Reason for revert: Tests are fixed in crbug.com/1159404
    >
    > Original change's description:
    > > Disabled flaky SingleClientTypedUrlsSyncTest tests on Android
    > >
    > > This CL disabled the following tests on Android.
    > >
    > > * SingleClientTypedUrlsSyncTest.DeleteNonTyped
    > > * SingleClientTypedUrlsSyncTest.TwoVisits
    > > * SingleClientTypedUrlsSyncTest.DeleteTyped
    > > * SingleClientTypedUrlsSyncTest.Sanity
    > >
    > > TBR=mastiz@chromium.org
    > >
    > > Bug: 1159479
    > > Change-Id: I49a93df85f6e48bbd661e77e41c467787c4e8b68
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596119
    > > Reviewed-by: Salvador Guerrero <salg@google.com>
    > > Commit-Queue: Salvador Guerrero <salg@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#837747}
    >
    > TBR=mastiz@chromium.org,salg@google.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    >
    > Change-Id: I24bcdac6b58843f9ca7a1909f9720152d43845be
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1159479
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597744
    > Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    > Reviewed-by: Salvador Guerrero <salg@google.com>
    > Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#838969}
    
    TBR=mastiz@chromium.org,salg@google.com,svenzheng@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1159479
    Change-Id: I8c67dbd430b00805a5b6fac702aaf1c6400d151a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2608930
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840162}

--- chrome/browser/sync/test/integration/single_client_typed_urls_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_typed_urls_sync_test.cc
@@ -35,7 +35,13 @@ class SingleClientTypedUrlsSyncTest : public SyncTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, Sanity) {
+// Flaky on android: https://crbug.com/1159479
+#if defined(OS_ANDROID)
+#define MAYBE_Sanity DISABLED_Sanity
+#else
+#define MAYBE_Sanity Sanity
+#endif
+IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, MAYBE_Sanity) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
   history::URLRows urls = GetTypedUrlsFromClient(0);
   ASSERT_EQ(0U, urls.size());
@@ -53,7 +59,13 @@ IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, Sanity) {
   ASSERT_TRUE(CheckAllProfilesHaveSameTypedURLs());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, TwoVisits) {
+// Flaky on android: https://crbug.com/1159479
+#if defined(OS_ANDROID)
+#define MAYBE_TwoVisits DISABLED_TwoVisits
+#else
+#define MAYBE_TwoVisits TwoVisits
+#endif
+IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, MAYBE_TwoVisits) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
   history::URLRows urls = GetTypedUrlsFromClient(0);
   ASSERT_EQ(0U, urls.size());
@@ -73,7 +85,13 @@ IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, TwoVisits) {
   ASSERT_TRUE(CheckAllProfilesHaveSameTypedURLs());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, DeleteTyped) {
+// Flaky on android: https://crbug.com/1159479
+#if defined(OS_ANDROID)
+#define MAYBE_DeleteTyped DISABLED_DeleteTyped
+#else
+#define MAYBE_DeleteTyped DeleteTyped
+#endif
+IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, MAYBE_DeleteTyped) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
   history::URLRows urls = GetTypedUrlsFromClient(0);
   ASSERT_EQ(0U, urls.size());
@@ -100,7 +118,13 @@ IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, DeleteTyped) {
   ASSERT_TRUE(CheckAllProfilesHaveSameTypedURLs());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, DeleteNonTyped) {
+// Flaky on android: https://crbug.com/1159479
+#if defined(OS_ANDROID)
+#define MAYBE_DeleteNonTyped DISABLED_DeleteNonTyped
+#else
+#define MAYBE_DeleteNonTyped DeleteNonTyped
+#endif
+IN_PROC_BROWSER_TEST_F(SingleClientTypedUrlsSyncTest, MAYBE_DeleteNonTyped) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
   history::URLRows urls = GetTypedUrlsFromClient(0);
   ASSERT_EQ(0U, urls.size());
