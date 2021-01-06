060fc3e9414f644ce5bad2eb00e9dfaef72a775d
Mihai Sardarescu <msarda@chromium.org>
2021-01-06 16:48:25 +0000

    Convert signin_profile_attributes_updater to OnPrimaryAccountChanged()
    Methods OnPrimaryAccountSet/Cleared() are deprecated, so this CL changes
    the signin_profile_attributes_updater.(h|ccc} to override the method
    OnPrimaryAccountChanged().
    
    Note that is also have a slight change of behavior by updating the auth
    info even when the unconsent primary account changed.
    
    Bug: 1163126
    Change-Id: I5f1c739787e3ead98c8dcedc7ca486eeda2bc216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611717
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840634}

--- chrome/browser/signin/signin_profile_attributes_updater.cc
+++ chrome/browser/signin/signin_profile_attributes_updater.cc
@@ -67,12 +67,14 @@ void SigninProfileAttributesUpdater::UpdateProfileAttributes() {
 
   if (clear_profile) {
     entry->SetLocalAuthCredentials(std::string());
-    entry->SetAuthInfo(std::string(), base::string16(), false);
+    entry->SetAuthInfo(std::string(), base::string16(),
+                       /*is_consented_primary_account=*/false);
     if (!signin_util::IsForceSigninEnabled())
       entry->SetIsSigninRequired(false);
   } else {
-    entry->SetAuthInfo(account_info.gaia, base::UTF8ToUTF16(account_info.email),
-                       identity_manager_->HasPrimaryAccount());
+    entry->SetAuthInfo(
+        account_info.gaia, base::UTF8ToUTF16(account_info.email),
+        identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSync));
   }
 }
 
@@ -86,20 +88,7 @@ void SigninProfileAttributesUpdater::OnErrorChanged() {
   entry->SetIsAuthError(signin_error_controller_->HasError());
 }
 
-void SigninProfileAttributesUpdater::OnPrimaryAccountSet(
-    const CoreAccountInfo& primary_account_info) {
-  UpdateProfileAttributes();
-}
-
-void SigninProfileAttributesUpdater::OnPrimaryAccountCleared(
-    const CoreAccountInfo& previous_primary_account_info) {
-  UpdateProfileAttributes();
-}
-
-void SigninProfileAttributesUpdater::OnUnconsentedPrimaryAccountChanged(
-    const CoreAccountInfo& unconsented_primary_account_info) {
-  if (identity_manager_->HasPrimaryAccount())
-    return;
-
+void SigninProfileAttributesUpdater::OnPrimaryAccountChanged(
+    const signin::PrimaryAccountChangeEvent& event) {
   UpdateProfileAttributes();
 }
