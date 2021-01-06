da4ef76657c1c33dc0c41e9a657143e843cec686
Jun Kokatsu <Jun.Kokatsu@microsoft.com>
2021-01-06 16:32:24 +0000

    Clean up the NoTrustedTypesViolation test
    This change clean up unnecessary test cases in the
    NoTrustedTypesViolation test. Also added few things that makes
    debugging of the test failure more easier.
    
    Bug: 1115643
    Change-Id: I12e77b3038046337f8002850f5c28806dd722acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610486
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Jun Kokatsu <Jun.Kokatsu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#840628}

--- chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string_piece.h"
 #include "build/build_config.h"
@@ -156,9 +157,15 @@ class ChromeURLDataManagerWebUITrustedTypesTest
 
     ASSERT_TRUE(embedded_test_server()->Start());
     ui_test_utils::NavigateToURL(browser(), GURL(url));
-    // We don't ASSERT_TRUE here because some WebUI pages are by design not
-    // PAGE_TYPE_NORMAL (e.g. chrome://interstitials/ssl).
-    content::WaitForLoadStop(content);
+
+    if (url == "chrome://network-error" || url == "chrome://dino") {
+      // We don't ASSERT_TRUE here because some WebUI pages are
+      // PAGE_TYPE_ERROR by design.
+      content::WaitForLoadStop(content);
+    } else {
+      ASSERT_TRUE(content::WaitForLoadStop(content));
+    }
+
     EXPECT_TRUE(console_observer.messages().empty());
   }
 };
@@ -166,6 +173,7 @@ class ChromeURLDataManagerWebUITrustedTypesTest
 // Verify that there's no Trusted Types violation in chrome://chrome-urls
 IN_PROC_BROWSER_TEST_P(ChromeURLDataManagerWebUITrustedTypesTest,
                        NoTrustedTypesViolation) {
+  LOG(INFO) << "Navigating to " << GetParam();
   CheckTrustedTypesViolation(GetParam());
 }
 
@@ -176,23 +184,18 @@ static constexpr const char* const kChromeUrls[] = {
     // TODO(crbug.com/1114074): DCHECK failure when opening
     // chrome://appcache-internals.
     // "chrome://appcache-internals",
-    "chrome://apps",
     "chrome://autofill-internals",
     "chrome://blob-internals",
     "chrome://bluetooth-internals",
     "chrome://bookmarks",
-    "chrome://browser-switch",
     "chrome://chrome-urls",
     "chrome://components",
-    "chrome://conflicts",
     "chrome://connection-help",
     "chrome://connection-monitoring-detected",
     "chrome://conversion-internals",
     "chrome://crashes",
     "chrome://credits",
     "chrome://device-log",
-    // TODO(crbug.com/1114062): Crash when closing chrome://devices.
-    // "chrome://devices",
     "chrome://dino",
     // TODO(crbug.com/1113446): Test failure due to excessive output.
     // "chrome://discards",
@@ -215,7 +218,6 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://invalidations",
     "chrome://local-state",
     "chrome://management",
-    "chrome://md-user-manager",
     "chrome://media-engagement",
     "chrome://media-feeds",
     "chrome://media-history",
@@ -229,7 +231,6 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://network-errors",
     "chrome://new-tab-page",
     "chrome://newtab",
-    "chrome://notifications-internals",
     "chrome://ntp-tiles-internals",
     "chrome://omnibox",
     "chrome://password-manager-internals",
@@ -247,10 +248,8 @@ static constexpr const char* const kChromeUrls[] = {
     // TODO(crbug.com/1115600): DCHECK failure when opening
     // chrome://signin-dice-web-intercept.
     // "chrome://signin-dice-web-intercept",
-    "chrome://signin-email-confirmation",
     "chrome://signin-internals",
     "chrome://site-engagement",
-    "chrome://snippets-internals",
     "chrome://suggestions",
     // TODO(crbug.com/1099564): Navigating to chrome://sync-confirmation and
     // quickly navigating away cause DCHECK failure.
@@ -270,12 +269,12 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://version",
     "chrome://webrtc-internals",
     "chrome://webrtc-logs",
-    "chrome://welcome",
 #if defined(OS_ANDROID)
     "chrome://explore-sites-internals",
     "chrome://internals/notifications",
     "chrome://internals/query-tiles",
     "chrome://offline-internals",
+    "chrome://snippets-internals",
     "chrome://webapks",
 #endif
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -295,7 +294,6 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://crostini-installer",
     "chrome://cryptohome",
     "chrome://drive-internals",
-    "chrome://first-run",
     "chrome://help-app",
     "chrome://internet-config-dialog",
     "chrome://internet-detail-dialog",
@@ -314,10 +312,16 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://smb-share-dialog",
     "chrome://supervised-user-internals",
     "chrome://sys-internals",
-    // TODO(crbug.com/1115643): DCHECK failure when opening
-    // chrome-untrusted://crosh.
-    // "chrome-untrusted://crosh",
     "chrome-untrusted://terminal",
+#endif
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+    "chrome://apps",
+    "chrome://browser-switch",
+    "chrome://md-user-manager",
+    "chrome://signin-email-confirmation",
+#endif
+#if defined(OS_WIN)
+    "chrome://conflicts",
 #endif
 };
 
