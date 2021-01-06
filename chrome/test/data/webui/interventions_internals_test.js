699ffd7f48c7d01e5bf5c569ab20bd1592cc59b8
dpapad <dpapad@chromium.org>
2021-01-06 10:12:09 +0000

    Add JS type checking for intervention-internals tests.
    Bug: 1000989
    Change-Id: I4ac4ab15f50e1cdd3decee13fbc2630875561ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2609848
    Commit-Queue: dpapad <dpapad@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840545}

--- chrome/test/data/webui/interventions_internals_test.js
+++ chrome/test/data/webui/interventions_internals_test.js
@@ -3,31 +3,44 @@
 // found in the LICENSE file.
 
 import {getTimeFormat, InterventionsInternalPageImpl, URL_THRESHOLD} from 'chrome://interventions-internals/index.js';
+import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
 import {$} from 'chrome://resources/js/util.m.js';
 
+import {assertEquals, assertFalse, assertGT} from './chai_assert.js';
 import {TestBrowserProxy} from './test_browser_proxy.m.js';
+
+/** @return {!PromiseResolver} */
+function getSetupFnResolver() {
+  return /** @type {{setupFnResolver: !PromiseResolver}} */ (window)
+      .setupFnResolver;
+}
 
 suite('InterventionsInternalsUITest', function() {
   /**
    * A stub class for the Mojo PageHandler.
+   * @implements {mojom.InterventionsInternalsPageInterface}
+   * @implements {mojom.InterventionsInternalsPageHandlerInterface}
    */
   class TestPageHandler extends TestBrowserProxy {
     constructor() {
       super(['getPreviewsEnabled', 'getPreviewsFlagsDetails']);
 
-      /** @private {!Map} */
-      this.previewsModeStatuses_ = new Map();
-      this.previewsFlagsStatuses_ = new Map();
+      /** @private {!Array<!mojom.PreviewsStatus>} */
+      this.previewsModeStatuses_ = [];
+
+      /** @private {!Array<!mojom.PreviewsFlag>} */
+      this.previewsFlagsStatuses_ = [];
     }
 
     /**
      * Setup testing map for getPreviewsEnabled.
-     * @param {!Map} map The testing status map.
+     * @param {!Array<!mojom.PreviewsStatus>} map The testing status map.
      */
     setTestingPreviewsModeMap(map) {
       this.previewsModeStatuses_ = map;
     }
 
+    /** @param {!Array<!mojom.PreviewsFlag>} map */
     setTestingPreviewsFlagsMap(map) {
       this.previewsFlagsStatuses_ = map;
     }
@@ -47,12 +60,30 @@ suite('InterventionsInternalsUITest', function() {
         flags: this.previewsFlagsStatuses_,
       });
     }
+
+    /** @override **/
+    logNewMessage() {}
+    /** @override **/
+    onBlocklistCleared() {}
+    /** @override **/
+    onBlocklistedHost() {}
+    /** @override **/
+    onIgnoreBlocklistDecisionStatusChanged() {}
+    /** @override **/
+    onUserBlocklistedStatusChange() {}
+    /** @override **/
+    setClientPage() {}
+    /** @override **/
+    setIgnorePreviewsBlocklistDecision() {}
+    /** @override **/
+    updateEffectiveConnectionType() {}
   }
 
   function getBlocklistedStatus(blocklisted) {
     return (blocklisted ? 'Blocklisted' : 'Not blocklisted');
   }
 
+  /** @type {?TestPageHandler} */
   let testPageHandler = null;
 
   setup(function() {
@@ -81,9 +112,10 @@ suite('InterventionsInternalsUITest', function() {
     ];
 
     testPageHandler.setTestingPreviewsModeMap(testArray);
-    window.setupFnResolver.resolve();
+    getSetupFnResolver().resolve();
 
-    return window.setupFnResolver.promise
+    return getSetupFnResolver()
+        .promise
         .then(() => {
           return testPageHandler.whenCalled('getPreviewsEnabled');
         })
@@ -92,7 +124,7 @@ suite('InterventionsInternalsUITest', function() {
             let expected = value.description + ': ' +
                 (value.enabled ? 'Enabled' : 'Disabled');
             let actual = document.querySelector('#' + value.htmlId).textContent;
-            expectEquals(expected, actual);
+            assertEquals(expected, actual);
           });
         });
   });
@@ -121,9 +153,10 @@ suite('InterventionsInternalsUITest', function() {
     ];
 
     testPageHandler.setTestingPreviewsFlagsMap(testArray);
-    window.setupFnResolver.resolve();
+    getSetupFnResolver().resolve();
 
-    return window.setupFnResolver.promise
+    return getSetupFnResolver()
+        .promise
         .then(() => {
           return testPageHandler.whenCalled('getPreviewsFlagsDetails');
         })
@@ -133,15 +166,15 @@ suite('InterventionsInternalsUITest', function() {
             let actualDescription =
                 document.querySelector('#' + key + 'Description');
             let actualValue = document.querySelector('#' + key + 'Value');
-            expectEquals(value.description, actualDescription.textContent);
-            expectEquals(value.link, actualDescription.getAttribute('href'));
-            expectEquals(value.value, actualValue.textContent);
+            assertEquals(value.description, actualDescription.textContent);
+            assertEquals(value.link, actualDescription.getAttribute('href'));
+            assertEquals(value.value, actualValue.textContent);
           });
         });
   });
 
   test('LogNewMessage', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let logs = [
       {
         type: 'Type_a',
@@ -171,7 +204,7 @@ suite('InterventionsInternalsUITest', function() {
     });
 
     let rows = $('message-logs-table').querySelectorAll('.log-message');
-    expectEquals(logs.length, rows.length);
+    assertEquals(logs.length, rows.length);
 
     logs.forEach((log, index) => {
       let row = rows[logs.length - index - 1];  // Expecting reversed order.
@@ -179,25 +212,25 @@ suite('InterventionsInternalsUITest', function() {
                                                 // appended to the top of the
                                                 // log table).
 
-      expectEquals(
+      assertEquals(
           getTimeFormat(log.time), row.querySelector('.log-time').textContent);
-      expectEquals(log.type, row.querySelector('.log-type').textContent);
-      expectEquals(
+      assertEquals(log.type, row.querySelector('.log-type').textContent);
+      assertEquals(
           log.description, row.querySelector('.log-description').textContent);
-      expectEquals(
+      assertEquals(
           log.url.url, row.querySelector('.log-url-value').textContent);
     });
   });
 
   test('LogNewMessageWithLongUrl', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
-    let log = {
+    let pageImpl = new InterventionsInternalPageImpl();
+    let log = /** @type {!mojom.MessageLog} */ ({
       type: 'Some type',
       url: {url: ''},
       description: 'Some description',
       time: 758675653000,  // Jan 15 1994 23:14:13 UTC
       pageId: 0,
-    };
+    });
     // Creating long url.
     for (let i = 0; i <= 2 * URL_THRESHOLD; i++) {
       log.url.url += 'a';
@@ -205,31 +238,31 @@ suite('InterventionsInternalsUITest', function() {
     let expectedUrl = log.url.url.substring(0, URL_THRESHOLD - 3) + '...';
 
     pageImpl.logNewMessage(log);
-    expectEquals(
+    assertEquals(
         expectedUrl, document.querySelector('div.log-url-value').textContent);
   });
 
 
   test('LogNewMessageWithNoUrl', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
-    let log = {
+    let pageImpl = new InterventionsInternalPageImpl();
+    let log = /** @type {!mojom.MessageLog} */ ({
       type: 'Some type',
       url: {url: ''},
       description: 'Some description',
       time: 758675653000,  // Jan 15 1994 23:14:13 UTC
       pageId: 0,
-    };
+    });
     pageImpl.logNewMessage(log);
     let actual = $('message-logs-table').rows[1];
     let expectedNoColumns = 3;
-    expectEquals(expectedNoColumns, actual.querySelectorAll('td').length);
-    assert(
-        !actual.querySelector('.log-url'),
+    assertEquals(expectedNoColumns, actual.querySelectorAll('td').length);
+    assertFalse(
+        !!actual.querySelector('.log-url'),
         'There should not be a log-url column for empty URL');
   });
 
   test('LogNewMessagePageIdZero', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let logs = [
       {
         type: 'Type_a',
@@ -254,23 +287,23 @@ suite('InterventionsInternalsUITest', function() {
     // Expect 2 different rows in logs table.
     let rows = $('message-logs-table').querySelectorAll('.log-message');
     let expectedNumberOfRows = 2;
-    expectEquals(expectedNumberOfRows, rows.length);
+    assertEquals(expectedNumberOfRows, rows.length);
 
     logs.forEach((log, index) => {
       let expectedTime = getTimeFormat(log.time);
       let row = rows[logs.length - index - 1];
 
-      expectEquals(expectedTime, row.querySelector('.log-time').textContent);
-      expectEquals(log.type, row.querySelector('.log-type').textContent);
-      expectEquals(
+      assertEquals(expectedTime, row.querySelector('.log-time').textContent);
+      assertEquals(log.type, row.querySelector('.log-type').textContent);
+      assertEquals(
           log.description, row.querySelector('.log-description').textContent);
-      expectEquals(
+      assertEquals(
           log.url.url, row.querySelector('.log-url-value').textContent);
     });
   });
 
   test('LogNewMessageNewPageId', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let logs = [
       {
         type: 'Type_a',
@@ -294,23 +327,23 @@ suite('InterventionsInternalsUITest', function() {
 
     // Expect 2 different rows in logs table.
     let rows = $('message-logs-table').querySelectorAll('.log-message');
-    expectEquals(2, rows.length);
+    assertEquals(2, rows.length);
 
     logs.forEach((log, index) => {
       let expectedTime = getTimeFormat(log.time);
       let row = rows[logs.length - index - 1];
 
-      expectEquals(expectedTime, row.querySelector('.log-time').textContent);
-      expectEquals(log.type, row.querySelector('.log-type').textContent);
-      expectEquals(
+      assertEquals(expectedTime, row.querySelector('.log-time').textContent);
+      assertEquals(log.type, row.querySelector('.log-type').textContent);
+      assertEquals(
           log.description, row.querySelector('.log-description').textContent);
-      expectEquals(
+      assertEquals(
           log.url.url, row.querySelector('.log-url-value').textContent);
     });
   });
 
   test('LogNewMessageExistedPageId', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let logs = [
       {
         type: 'Type_a',
@@ -333,35 +366,35 @@ suite('InterventionsInternalsUITest', function() {
     });
 
     let logTableRows = $('message-logs-table').querySelectorAll('.log-message');
-    expectEquals(1, logTableRows.length);
-    expectEquals(
+    assertEquals(1, logTableRows.length);
+    assertEquals(
         1, document.querySelector('.expansion-logs-table').rows.length);
 
     // Log table row.
     let row = $('message-logs-table').querySelector('.log-message');
     let expectedRowTime = getTimeFormat(logs[1].time);
-    expectEquals(expectedRowTime, row.querySelector('.log-time').textContent);
-    expectEquals(logs[1].type, row.querySelector('.log-type').textContent);
-    expectEquals(
+    assertEquals(expectedRowTime, row.querySelector('.log-time').textContent);
+    assertEquals(logs[1].type, row.querySelector('.log-type').textContent);
+    assertEquals(
         logs[1].description, row.querySelector('.log-description').textContent);
-    expectEquals(
+    assertEquals(
         logs[1].url.url, row.querySelector('.log-url-value').textContent);
 
     // Sub log table row.
     let subRow = document.querySelector('.expansion-logs-table').rows[0];
     let expectedSubTime = getTimeFormat(logs[0].time);
-    expectEquals(
+    assertEquals(
         expectedSubTime, subRow.querySelector('.log-time').textContent);
-    expectEquals(logs[0].type, subRow.querySelector('.log-type').textContent);
-    expectEquals(
+    assertEquals(logs[0].type, subRow.querySelector('.log-type').textContent);
+    assertEquals(
         logs[0].description,
         subRow.querySelector('.log-description').textContent);
-    expectEquals(
+    assertEquals(
         logs[0].url.url, subRow.querySelector('.log-url-value').textContent);
   });
 
   test('LogNewMessageExistedPageIdGroupToTopOfTable', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let logs = [
       {
         type: 'Type_a',
@@ -389,130 +422,130 @@ suite('InterventionsInternalsUITest', function() {
     pageImpl.logNewMessage(logs[0]);
     pageImpl.logNewMessage(logs[1]);
     let rows = $('message-logs-table').querySelectorAll('.log-message');
-    expectEquals(2, rows.length);
-    expectEquals(logs[1].type, rows[0].querySelector('.log-type').textContent);
-    expectEquals(logs[0].type, rows[1].querySelector('.log-type').textContent);
+    assertEquals(2, rows.length);
+    assertEquals(logs[1].type, rows[0].querySelector('.log-type').textContent);
+    assertEquals(logs[0].type, rows[1].querySelector('.log-type').textContent);
 
     // Existing group pushed to the top of the log table.
     pageImpl.logNewMessage(logs[2]);
     rows = $('message-logs-table').querySelectorAll('.log-message');
-    expectEquals(2, rows.length);
-    expectEquals(logs[2].type, rows[0].querySelector('.log-type').textContent);
-    expectEquals(logs[1].type, rows[1].querySelector('.log-type').textContent);
+    assertEquals(2, rows.length);
+    assertEquals(logs[2].type, rows[0].querySelector('.log-type').textContent);
+    assertEquals(logs[1].type, rows[1].querySelector('.log-type').textContent);
   });
 
   test('AddNewBlocklistedHost', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let time = 758675653000;  // Jan 15 1994 23:14:13 UTC
     let expectedHost = 'example.com';
-    pageImpl.onBlocklistedHost(expectedHost, time);
+    pageImpl.onBlocklistedHost(expectedHost, BigInt(time));
 
     let blocklistedTable = $('blocklisted-hosts-table');
     let row = blocklistedTable.querySelector('.blocklisted-host-row');
 
     let expectedTime = getTimeFormat(time);
 
-    expectEquals(
+    assertEquals(
         expectedHost, row.querySelector('.host-blocklisted').textContent);
-    expectEquals(
+    assertEquals(
         expectedTime, row.querySelector('.host-blocklisted-time').textContent);
   });
 
 
   test('HostAlreadyBlocklisted', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let time0 = 758675653000;   // Jan 15 1994 23:14:13 UTC
     let time1 = 1507221689240;  // Oct 05 2017 16:41:29 UTC
     let expectedHost = 'example.com';
 
-    pageImpl.onBlocklistedHost(expectedHost, time0);
+    pageImpl.onBlocklistedHost(expectedHost, BigInt(time0));
 
     let blocklistedTable = $('blocklisted-hosts-table');
     let row = blocklistedTable.querySelector('.blocklisted-host-row');
     let expectedTime = getTimeFormat(time0);
-    expectEquals(
+    assertEquals(
         expectedHost, row.querySelector('.host-blocklisted').textContent);
-    expectEquals(
+    assertEquals(
         expectedTime, row.querySelector('.host-blocklisted-time').textContent);
 
-    pageImpl.onBlocklistedHost(expectedHost, time1);
+    pageImpl.onBlocklistedHost(expectedHost, BigInt(time1));
 
     // The row remains the same.
-    expectEquals(
+    assertEquals(
         expectedHost, row.querySelector('.host-blocklisted').textContent);
-    expectEquals(
+    assertEquals(
         expectedTime, row.querySelector('.host-blocklisted-time').textContent);
   });
 
   test('UpdateUserBlocklisted', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let state = $('user-blocklisted-status-value');
 
     pageImpl.onUserBlocklistedStatusChange(true /* blocklisted */);
-    expectEquals(
+    assertEquals(
         getBlocklistedStatus(true /* blocklisted */), state.textContent);
 
     pageImpl.onUserBlocklistedStatusChange(false /* blocklisted */);
-    expectEquals(
+    assertEquals(
         getBlocklistedStatus(false /* blocklisted */), state.textContent);
   });
 
   test('OnBlocklistCleared', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let state = $('user-blocklisted-status-value');
     let time = 758675653000;  // Jan 15 1994 23:14:13 UTC
 
-    pageImpl.onBlocklistCleared(time);
+    pageImpl.onBlocklistCleared(BigInt(time));
     let actualClearedTime = $('blocklist-last-cleared-time').textContent;
-    expectEquals(getTimeFormat(time), actualClearedTime);
+    assertEquals(getTimeFormat(time), actualClearedTime);
     let blocklistedTable = $('blocklisted-hosts-table');
     let rows = blocklistedTable.querySelectorAll('.blocklisted-host-row');
-    expectEquals(0, rows.length);
+    assertEquals(0, rows.length);
   });
 
   test('ClearLogMessageOnBlocklistCleared', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let time = 758675653000;  // Jan 15 1994 23:14:13 UTC
-    let log = {
+    let log = /** @type {!mojom.MessageLog} */ ({
       type: 'Some type',
       url: {url: ''},
       description: 'Some description',
       time: 758675653000,  // Jan 15 1994 23:14:13 UTC
       pageId: 0,
-    };
+    });
 
     pageImpl.logNewMessage(log);
-    expectGT($('message-logs-table').rows.length, 1 /* header row */);
-    pageImpl.onBlocklistCleared(time);
+    assertGT($('message-logs-table').rows.length, 1 /* header row */);
+    pageImpl.onBlocklistCleared(BigInt(time));
     let expectedNumberOfRows = 2;  // header row and clear blocklist log.
     let rows = $('message-logs-table').rows;
-    expectEquals(expectedNumberOfRows, rows.length);
-    expectEquals(
+    assertEquals(expectedNumberOfRows, rows.length);
+    assertEquals(
         'Blocklist Cleared',
         rows[1].querySelector('.log-description').textContent);
   });
 
 
   test('OnECTChanged', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     let ectTypes = ['type1', 'type2', 'type3'];
     ectTypes.forEach((type) => {
       pageImpl.updateEffectiveConnectionType(type, 'max');
       let actual = $('nqe-type').textContent;
-      expectEquals(type, actual);
+      assertEquals(type, actual);
     });
   });
 
   test('OnBlocklistIgnoreChange', () => {
-    let pageImpl = new InterventionsInternalPageImpl(null);
+    let pageImpl = new InterventionsInternalPageImpl();
     pageImpl.onIgnoreBlocklistDecisionStatusChanged(true /* ignored */);
-    expectEquals('Enable Blocklist', $('ignore-blocklist-button').textContent);
-    expectEquals(
+    assertEquals('Enable Blocklist', $('ignore-blocklist-button').textContent);
+    assertEquals(
         'Blocklist decisions are ignored.',
         $('blocklist-ignored-status').textContent);
 
     pageImpl.onIgnoreBlocklistDecisionStatusChanged(false /* ignored */);
-    expectEquals('Ignore Blocklist', $('ignore-blocklist-button').textContent);
-    expectEquals('', $('blocklist-ignored-status').textContent);
+    assertEquals('Ignore Blocklist', $('ignore-blocklist-button').textContent);
+    assertEquals('', $('blocklist-ignored-status').textContent);
   });
 });
