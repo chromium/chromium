ac10443d4115aee386a070862b7f77c31a4a7358
Benoit Lize <lizeb@chromium.org>
2021-01-06 13:49:21 +0000

    [PartitionAlloc] Better freelist corruption detection.
    Some PartitionAlloc crashes are likely due to freelist
    corruption. PartitionAlloc has two types of freelists: in the central
    allocator, and in the thread cache. The central allocator one already
    has a DCHECK() to verify that subsequent entries belong the same
    superpage, which doesn't hold for the thread cache freelists.
    
    This CL:
    - Makes these crash lead to a NOINLINE function
    - Add an interity check to all freelists.
    
    This is not meant to be a security mitigation, but to prevent against
    accidental issues.
    
    Bug: 998048
    Change-Id: I21aedfe2b6363069362514a8edd6cd5bdea1acfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582218
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840587}

--- base/allocator/partition_allocator/partition_bucket.cc
+++ base/allocator/partition_allocator/partition_bucket.cc
@@ -416,6 +416,10 @@ ALWAYS_INLINE char* PartitionBucket<thread_safe>::ProvisionMoreSlotsAndAllocOne(
     slot_span->num_unprovisioned_slots--;
   }
 
+#if DCHECK_IS_ON()
+  slot_span->freelist_head->CheckFreeList();
+#endif
+
   return return_slot;
 }
 
@@ -628,7 +632,10 @@ void* PartitionBucket<thread_safe>::SlowPathAlloc(
     PartitionFreelistEntry* new_head = entry->GetNext();
     new_slot_span->SetFreelistHead(new_head);
     new_slot_span->num_allocated_slots++;
-    return entry;
+
+    // We likely set *is_already_zeroed to true above, make sure that the
+    // freelist entry doesn't contain data.
+    return entry->ClearForAllocation();
   }
 
   // Otherwise, we need to provision more slots by committing more pages. Build
