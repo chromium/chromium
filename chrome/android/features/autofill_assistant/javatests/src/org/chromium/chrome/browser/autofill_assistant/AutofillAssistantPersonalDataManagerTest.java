ee5bd12074bd0a568c4988c0e951c5a7b2ece4d6
Mei Liang <meiliang@chromium.org>
2021-01-07 01:16:13 +0000

    Disable flaky AutofillAssistantPersonalDataManagerTest
    This CL disables the testCreateShippingAddressAndCreditCard test.
    
    It is flaky on builder:
    * android-pie-x86-rel
    
    TBR=micantox@google.com
    
    Bug: 1163659
    Change-Id: I4431d9f180968a2548aed29c415ad143bdfd4c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612382
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840862}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPersonalDataManagerTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPersonalDataManagerTest.java
@@ -53,6 +53,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
@@ -828,6 +829,7 @@ public class AutofillAssistantPersonalDataManagerTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1163659")
     public void testCreateShippingAddressAndCreditCard() {
         ArrayList<ActionProto> list = new ArrayList<>();
         list.add((ActionProto) ActionProto.newBuilder()
