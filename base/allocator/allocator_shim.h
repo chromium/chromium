2c975aee77c991fd7b47091038a4fdf97d33b561
Sebastien Marchand <sebmarchand@chromium.org>
2021-01-07 23:53:24 +0000

    Experiment: Disable PartitionAlloc-Everyhwere (Reverse experiment)
    This combines 3 CLs:
    - https://chromium-review.googlesource.com/c/chromium/src/+/2569444
    - https://chromium-review.googlesource.com/c/chromium/src/+/2616460
    - https://chromium-review.googlesource.com/c/chromium/src/+/2615327
    
    Bug: 1121427
    Change-Id: I88a6112bae5995d247ed2ec08cbb7c036564ec1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616521
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4381@{#15}
    Cr-Branched-From: 6ad04657559cfec90163e6385584958498f87428-refs/heads/master@{#840845}

--- base/allocator/allocator_shim.h
+++ base/allocator/allocator_shim.h
@@ -146,7 +146,7 @@ BASE_EXPORT void InsertAllocatorDispatch(AllocatorDispatch* dispatch);
 // in malloc(), which we really don't want.
 BASE_EXPORT void RemoveAllocatorDispatchForTesting(AllocatorDispatch* dispatch);
 
-#if defined(OS_WIN)
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && defined(OS_WIN)
 // Configures the allocator for the caller's allocation domain. Allocations that
 // take place prior to this configuration step will succeed, but will not
 // benefit from its one-time mitigations. As such, this function must be called
