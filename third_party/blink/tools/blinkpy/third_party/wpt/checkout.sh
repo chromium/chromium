b77a27f49113af1dc05ea8ebd597d05910d1bff7
Stephen McGruer <smcgruer@chromium.org>
2021-01-07 15:00:56 +0000

    Roll wpt tooling.
    This rolls up to WPT SHA e04f27b9946175edab6c09421ab5ceaa14b84539
    
    This drops the cherry-pick of 0dd83ea79e8505d4f20a633d6750636ad9634300,
    as the upstream version of that change has now landed.
    
    Bug: 1160108
    Change-Id: I6fdbb8f1453d3b6d70743614e60eaf26f9e2d29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611551
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841054}

--- third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
+++ third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 TARGET_DIR=$DIR/wpt
 REMOTE_REPO="https://github.com/web-platform-tests/wpt.git"
-WPT_HEAD=d3ce095fcc6b5a85e6056fdebbf939caf2e2719f
+WPT_HEAD=e04f27b9946175edab6c09421ab5ceaa14b84539
 
 function clone {
   # Remove existing repo if already exists.
