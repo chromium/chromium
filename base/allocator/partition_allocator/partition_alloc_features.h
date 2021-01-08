227c0c073a0402cb1681ae7ba7d1b6b151b7e97e
Bartek Nowierski <bartekn@chromium.org>
2021-01-08 02:40:50 +0000

    PCScan: Disable in Dev branch to avoid interfering with PA-E experiment
    Change-Id: Iab22e197068a630b1b26de3619722fccb3afa616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617398
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4381@{#17}
    Cr-Branched-From: 6ad04657559cfec90163e6385584958498f87428-refs/heads/master@{#840845}

--- base/allocator/partition_allocator/partition_alloc_features.h
+++ base/allocator/partition_allocator/partition_alloc_features.h
@@ -73,11 +73,7 @@ ALWAYS_INLINE bool IsPartitionAllocPCScanEnabled() {
 
 #if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 ALWAYS_INLINE bool IsPartitionAllocPCScanBrowserOnlyEnabled() {
-#if defined(PA_HAS_64_BITS_POINTERS)
-  return FeatureList::IsEnabled(kPartitionAllocPCScanBrowserOnly);
-#else  // PA_HAS_64_BITS_POINTERS
   return false;
-#endif
 }
 #endif
 
