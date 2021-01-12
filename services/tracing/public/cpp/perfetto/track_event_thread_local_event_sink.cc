9def7f1bebcc55487bae20cf14485926a33ddb85
ssid <ssid@chromium.org>
2021-01-12 00:33:30 +0000

    Add network service thread in tracing
    Change-Id: I160ed2450bdc3b99e1ac7047a75e69928b8f679a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622893
    Commit-Queue: ssid <ssid@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Auto-Submit: ssid <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842277}

--- services/tracing/public/cpp/perfetto/track_event_thread_local_event_sink.cc
+++ services/tracing/public/cpp/perfetto/track_event_thread_local_event_sink.cc
@@ -133,6 +133,8 @@ ChromeThreadDescriptor::ThreadType GetThreadType(
     return ChromeThreadDescriptor::THREAD_MAIN;
   } else if (base::MatchPattern(thread_name, "Chrome*IOThread")) {
     return ChromeThreadDescriptor::THREAD_IO;
+  } else if (base::MatchPattern(thread_name, "NetworkService")) {
+    return ChromeThreadDescriptor::THREAD_NETWORK_SERVICE;
   } else if (base::MatchPattern(thread_name, "ThreadPoolForegroundWorker*")) {
     return ChromeThreadDescriptor::THREAD_POOL_FG_WORKER;
   } else if (base::MatchPattern(thread_name, "ThreadPoolBackgroundWorker*")) {
