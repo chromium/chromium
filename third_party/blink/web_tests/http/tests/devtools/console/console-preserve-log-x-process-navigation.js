756c4e7787bdd9899e2837d6ae430e42d2d5f867
Adithya Srinivasan <adithyas@chromium.org>
2021-01-12 14:32:56 +0000

    DevTools: Fix flaky test
    Call console.log in an html file instead of calling it directly using
    evaluateInPage.
    
    Bug: 1165324
    Change-Id: I3198139bd867b15f4f28724ded7d1767647b3197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622282
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842515}

--- third_party/blink/web_tests/http/tests/devtools/console/console-preserve-log-x-process-navigation.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-preserve-log-x-process-navigation.js
@@ -6,11 +6,11 @@
   TestRunner.addResult(`Tests that the console can preserve log messages across cross-process navigations.`);
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.showPanel('console');
-  await TestRunner.navigatePromise('http://devtools.oopif.test:8000/devtools/oopif/resources/empty.html')
+  await TestRunner.navigatePromise('http://devtools.oopif.test:8000/devtools/console/resources/log-message.html')
   Common.settingForTest('preserveConsoleLog').set(true);
-  await TestRunner.evaluateInPage(`console.log('before navigation')`);
-  await TestRunner.navigatePromise('http://127.0.0.1:8000/devtools/oopif/resources/empty.html')
-  await TestRunner.evaluateInPage(`console.log('after navigation')`);
+  await TestRunner.evaluateInPage(`logMessage('before navigation')`);
+  await TestRunner.navigatePromise('http://127.0.0.1:8000/devtools/console/resources/log-message.html')
+  await TestRunner.evaluateInPage(`logMessage('after navigation')`);
   await ConsoleTestRunner.dumpConsoleMessages();
   TestRunner.completeTest();
 })();
