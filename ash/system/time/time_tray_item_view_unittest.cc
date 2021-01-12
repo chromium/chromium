chromium/chromium

commit 5ac617b265b3381872e3c3964f6fadd4fbb1b448

Scalable Status Area: Show date in status area in large screen.
- Add a flag for the scalable status area feature.
- Add enum and helper function in SystemTrayModel to capture system tray
size.
- Add date to system tray when the screen is large.

Bug: 1161557
Change-Id: Id85a6bee9a30684c65b2253862ba30ab78583525
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600912
Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
Reviewed-by: Toni Baržić <tbarzic@chromium.org>
Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
Commit-Queue: Andre Le <leandre@chromium.org>
Cr-Commit-Position: refs/heads/master@{#841227}

ash/BUILD.gn
@@ -2252,6 +2252,7 @@ test("ash_unittests") {
     "system/session/session_limit_notification_controller_unittest.cc",
     "system/status_area_widget_unittest.cc",
     "system/supervised/supervised_notification_controller_unittest.cc",
+    "system/time/time_tray_item_view_unittest.cc",
     "system/time/time_view_unittest.cc",
     "system/toast/toast_manager_unittest.cc",
     "system/tracing_notification_controller_unittest.cc",

ash/ash_strings.grd
@@ -933,6 +933,9 @@ This file contains the strings for ash.
       <message name="IDS_ASH_STATUS_TRAY_DATE" desc="The date displayed on ash system bubble, Depending on launguage, please choose the best separator(eg ',') between abbreviated weekday and date">
         <ph name="short_weekday">$1<ex>Fri</ex></ph>, <ph name="date">$2<ex>Aug 31, 2012</ex></ph>
       </message>
+      <message name="IDS_ASH_STATUS_TRAY_DATE_TIME" desc="The time displayed on ash system tray, Depending on launguage, please choose the best separator(eg ',') between abbreviated date and time">
+        <ph name="date">$1<ex>Aug 31</ex></ph>, <ph name="time">$2<ex>03:00</ex></ph>
+      </message>
       <message name="IDS_ASH_STATUS_TRAY_REBOOT" desc="The accessible text for the reboot button.">
         Restart
       </message>

ash/ash_strings_grd/IDS_ASH_STATUS_TRAY_DATE_TIME.png.sha1
@@ -1,0 +1,1 @@
+56b9ecb9b85721cdee47f52a37df3dcd4f6867a5

ash/public/cpp/ash_features.cc
@@ -143,6 +143,9 @@ const base::Feature kTemporaryHoldingSpacePreviews{
 const base::Feature kDragUnpinnedAppToPin{"DragUnpinnedAppToPin",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kScalableStatusArea{"ScalableStatusArea",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kKeyboardBasedDisplayArrangementInSettings{
     "KeyboardBasedDisplayArrangementInSettings",
     base::FEATURE_ENABLED_BY_DEFAULT};
@@ -319,6 +322,10 @@ bool IsTemporaryHoldingSpacePreviewsEnabled() {
 
 bool IsDragUnpinnedAppToPinEnabled() {
   return base::FeatureList::IsEnabled(kDragUnpinnedAppToPin);
+}
+
+bool IsScalableStatusAreaEnabled() {
+  return base::FeatureList::IsEnabled(kScalableStatusArea);
 }
 
 namespace {

commit 7175de90c803c40217942b4cbe02d434feb4cd60

Reland "Scalable Status Area: Show date in status area in large screen."
This is a reland of 5ac617b265b3381872e3c3964f6fadd4fbb1b448, which caused a MSan issue since TimeTrayItemView instance was destructed after the UnifiedSystemTrayModel instance in UnifiedSystemTray. This reland fix the order of destructions.

Original change's description:
> Scalable Status Area: Show date in status area in large screen.
>
> - Add a flag for the scalable status area feature.
> - Add enum and helper function in SystemTrayModel to capture system tray
> size.
> - Add date to system tray when the screen is large.
>
> Bug: 1161557
> Change-Id: Id85a6bee9a30684c65b2253862ba30ab78583525
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600912
> Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
> Reviewed-by: Toni Baržić <tbarzic@chromium.org>
> Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
> Commit-Queue: Andre Le <leandre@chromium.org>
> Cr-Commit-Position: refs/heads/master@{#841227}

Bug: 1161557
Change-Id: I2894c68e6e59bef90c374dfe6d6c85ea74ef1850
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618445
Commit-Queue: Andre Le <leandre@chromium.org>
Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
Reviewed-by: Toni Baržić <tbarzic@chromium.org>
Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
Cr-Commit-Position: refs/heads/master@{#842460}

ash/BUILD.gn
@@ -2249,6 +2249,7 @@ test("ash_unittests") {
     "system/session/logout_confirmation_controller_unittest.cc",
     "system/session/session_limit_notification_controller_unittest.cc",
     "system/status_area_widget_unittest.cc",
+    "system/time/time_tray_item_view_unittest.cc",
     "system/time/time_view_unittest.cc",
     "system/toast/toast_manager_unittest.cc",
     "system/tracing_notification_controller_unittest.cc",

ash/ash_strings.grd
@@ -933,6 +933,9 @@ This file contains the strings for ash.
       <message name="IDS_ASH_STATUS_TRAY_DATE" desc="The date displayed on ash system bubble, Depending on launguage, please choose the best separator(eg ',') between abbreviated weekday and date">
         <ph name="short_weekday">$1<ex>Fri</ex></ph>, <ph name="date">$2<ex>Aug 31, 2012</ex></ph>
       </message>
+      <message name="IDS_ASH_STATUS_TRAY_DATE_TIME" desc="The time displayed on ash system tray, Depending on launguage, please choose the best separator(eg ',') between abbreviated date and time">
+        <ph name="date">$1<ex>Aug 31</ex></ph>, <ph name="time">$2<ex>03:00</ex></ph>
+      </message>
       <message name="IDS_ASH_STATUS_TRAY_REBOOT" desc="The accessible text for the reboot button.">
         Restart
       </message>

ash/ash_strings_grd/IDS_ASH_STATUS_TRAY_DATE_TIME.png.sha1
@@ -1,0 +1,1 @@
+56b9ecb9b85721cdee47f52a37df3dcd4f6867a5

ash/public/cpp/ash_features.cc
@@ -143,6 +143,9 @@ const base::Feature kTemporaryHoldingSpacePreviews{
 const base::Feature kDragUnpinnedAppToPin{"DragUnpinnedAppToPin",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kScalableStatusArea{"ScalableStatusArea",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kKeyboardBasedDisplayArrangementInSettings{
     "KeyboardBasedDisplayArrangementInSettings",
     base::FEATURE_ENABLED_BY_DEFAULT};
@@ -319,6 +322,10 @@ bool IsTemporaryHoldingSpacePreviewsEnabled() {
 
 bool IsDragUnpinnedAppToPinEnabled() {
   return base::FeatureList::IsEnabled(kDragUnpinnedAppToPin);
+}
+
+bool IsScalableStatusAreaEnabled() {
+  return base::FeatureList::IsEnabled(kScalableStatusArea);
 }
 
 namespace {

ash/public/cpp/ash_features.h
@@ -194,6 +194,9 @@ ASH_PUBLIC_EXPORT extern const base::Feature kTemporaryHoldingSpacePreviews;
 // Enables dragging an unpinned open app to pinned app side to pin.
 ASH_PUBLIC_EXPORT extern const base::Feature kDragUnpinnedAppToPin;
 
+// Enables the system tray to show more information in larger screen.
+ASH_PUBLIC_EXPORT extern const base::Feature kScalableStatusArea;
+
 ASH_PUBLIC_EXPORT bool IsAllowAmbientEQEnabled();
 
 ASH_PUBLIC_EXPORT bool IsAltTabLimitedToActiveDesk();
@@ -273,6 +276,8 @@ ASH_PUBLIC_EXPORT bool IsTemporaryHoldingSpaceEnabled();
 ASH_PUBLIC_EXPORT bool IsTemporaryHoldingSpacePreviewsEnabled();
 
 ASH_PUBLIC_EXPORT bool IsDragUnpinnedAppToPinEnabled();
+
+ASH_PUBLIC_EXPORT bool IsScalableStatusAreaEnabled();
 
 // These two functions are supposed to be temporary functions to set or get
 // whether "WebUITabStrip" feature is enabled from Chrome.

ash/system/time/time_tray_item_view.cc
@@ -4,6 +4,7 @@
 
 #include "ash/system/time/time_tray_item_view.h"
 
+#include "ash/public/cpp/ash_features.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
@@ -16,14 +17,18 @@ namespace ash {
 
 namespace tray {
 
-TimeTrayItemView::TimeTrayItemView(Shelf* shelf)
-    : TrayItemView(shelf), session_observer_(this) {
+TimeTrayItemView::TimeTrayItemView(Shelf* shelf, UnifiedSystemTrayModel* model)
+    : TrayItemView(shelf), model_(model), session_observer_(this) {
+  system_tray_model_observation_.Observe(model_);
+
   TimeView::ClockLayout clock_layout =
       shelf->IsHorizontalAlignment() ? TimeView::ClockLayout::HORIZONTAL_CLOCK
                                      : TimeView::ClockLayout::VERTICAL_CLOCK;
   time_view_ =
       new TimeView(clock_layout, Shell::Get()->system_tray_model()->clock());
   AddChildView(time_view_);
+
+  OnSystemTrayButtonSizeChanged(model_->GetSystemTrayButtonSize());
 }
 
 TimeTrayItemView::~TimeTrayItemView() = default;
@@ -42,6 +47,17 @@ void TimeTrayItemView::HandleLocaleChange() {
 void TimeTrayItemView::OnSessionStateChanged(
     session_manager::SessionState state) {
   time_view_->SetTextColor(TrayIconColor(state));
+}
+
+void TimeTrayItemView::OnSystemTrayButtonSizeChanged(
+    UnifiedSystemTrayModel::SystemTrayButtonSize system_tray_size) {
+  time_view_->SetShowDateWhenHorizontal(
+      features::IsScalableStatusAreaEnabled() &&
+      system_tray_size == UnifiedSystemTrayModel::SystemTrayButtonSize::kLarge);
+}
+
+void TimeTrayItemView::Reset() {
+  system_tray_model_observation_.Reset();
 }
 
 const char* TimeTrayItemView::GetClassName() const {

ash/system/time/time_tray_item_view.h
@@ -8,7 +8,9 @@
 #include "ash/ash_export.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/system/tray/tray_item_view.h"
+#include "ash/system/unified/unified_system_tray_model.h"
 #include "base/macros.h"
+#include "base/scoped_observation.h"
 
 namespace ash {
 class Shelf;
@@ -17,9 +19,11 @@ namespace tray {
 
 class TimeView;
 
-class TimeTrayItemView : public TrayItemView, public SessionObserver {
+class ASH_EXPORT TimeTrayItemView : public TrayItemView,
+                                    public SessionObserver,
+                                    public UnifiedSystemTrayModel::Observer {
  public:
-  explicit TimeTrayItemView(Shelf* shelf);
+  TimeTrayItemView(Shelf* shelf, UnifiedSystemTrayModel* model);
   ~TimeTrayItemView() override;
 
   void UpdateAlignmentForShelf(Shelf* shelf);
@@ -31,13 +35,26 @@ class TimeTrayItemView : public TrayItemView, public SessionObserver {
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
 
+  // UnifiedSystemTrayModel::Observer:
+  void OnSystemTrayButtonSizeChanged(
+      UnifiedSystemTrayModel::SystemTrayButtonSize system_tray_size) override;
+
+  // Reset the view by removing observer to |model_|.
+  void Reset();
+
   // views::View:
   const char* GetClassName() const override;
   void OnThemeChanged() override;
 
  private:
+  friend class TimeTrayItemViewTest;
+
+  UnifiedSystemTrayModel* model_ = nullptr;
   TimeView* time_view_ = nullptr;
   ScopedSessionObserver session_observer_;
+  base::ScopedObservation<UnifiedSystemTrayModel,
+                          UnifiedSystemTrayModel::Observer>
+      system_tray_model_observation_{this};
   DISALLOW_COPY_AND_ASSIGN(TimeTrayItemView);
 };
 

ash/system/time/time_view.cc
@@ -54,6 +54,12 @@ const int kVerticalClockMinutesTopOffset = -2;
 // when the shelf is vertically aligned.
 const int kClockLeadingPadding = 8;
 
+base::string16 FormatDate(const base::Time& time) {
+  // Use 'short' month format (e.g., "Oct") followed by non-padded day of
+  // month (e.g., "2", "10").
+  return base::TimeFormatWithPattern(time, "LLLd");
+}
+
 }  // namespace
 
 TimeView::TimeView(ClockLayout clock_layout, ClockModel* model)
@@ -110,6 +116,14 @@ void TimeView::SetTextShadowValues(const gfx::ShadowValues& shadows) {
 
   vertical_label_hours_->SetShadows(shadows);
   vertical_label_minutes_->SetShadows(shadows);
+}
+
+void TimeView::SetShowDateWhenHorizontal(bool show_date_when_horizontal) {
+  if (show_date_when_horizontal_ == show_date_when_horizontal)
+    return;
+  show_date_when_horizontal_ = show_date_when_horizontal;
+  UpdateText();
+  PreferredSizeChanged();
 }
 
 void TimeView::OnDateFormatChanged() {
@@ -189,7 +203,11 @@ void TimeView::UpdateTextInternal(const base::Time& now) {
 
   base::string16 current_time = base::TimeFormatTimeOfDayWithHourClockType(
       now, model_->hour_clock_type(), base::kDropAmPm);
-  horizontal_label_->SetText(current_time);
+  base::string16 current_date_time = l10n_util::GetStringFUTF16(
+      IDS_ASH_STATUS_TRAY_DATE_TIME, FormatDate(now), current_time);
+
+  horizontal_label_->SetText(show_date_when_horizontal_ ? current_date_time
+                                                        : current_time);
   horizontal_label_->SetTooltipText(base::TimeFormatFriendlyDate(now));
   horizontal_label_->NotifyAccessibilityEvent(ax::mojom::Event::kTextChanged,
                                               true);

ash/system/time/time_view.h
@@ -56,6 +56,9 @@ class ASH_EXPORT TimeView : public ActionableView, public ClockObserver {
   // Updates the time text shadow values.
   void SetTextShadowValues(const gfx::ShadowValues& shadows);
 
+  // Shows the date in horizontal view when |show_date_when_horizontal| is true.
+  void SetShowDateWhenHorizontal(bool show_date_when_horizontal);
+
   // ClockObserver:
   void OnDateFormatChanged() override;
   void OnSystemClockTimeUpdated() override;
@@ -69,6 +72,7 @@ class ASH_EXPORT TimeView : public ActionableView, public ClockObserver {
 
  private:
   friend class TimeViewTest;
+  friend class TimeTrayItemViewTest;
 
   // ActionableView:
   bool PerformAction(const ui::Event& event) override;
@@ -106,6 +110,9 @@ class ASH_EXPORT TimeView : public ActionableView, public ClockObserver {
   // The time label is split into two lines for the vertical shelf.
   views::Label* vertical_label_hours_;
   views::Label* vertical_label_minutes_;
+
+  // Indicates if date should be show in horizontal view.
+  bool show_date_when_horizontal_ = false;
 
   // Invokes UpdateText() when the displayed time should change.
   base::OneShotTimer timer_;

ash/system/unified/unified_system_tray.cc
@@ -20,7 +20,6 @@
 #include "ash/system/network/network_tray_view.h"
 #include "ash/system/power/tray_power.h"
 #include "ash/system/privacy_screen/privacy_screen_toast_controller.h"
-#include "ash/system/status_area_widget.h"
 #include "ash/system/time/time_tray_item_view.h"
 #include "ash/system/time/time_view.h"
 #include "ash/system/tray/tray_constants.h"
@@ -120,8 +119,7 @@ void UnifiedSystemTray::UiDelegate::HideMessageCenter() {}
 UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
     : TrayBackgroundView(shelf),
       ui_delegate_(std::make_unique<UiDelegate>(this)),
-      model_(std::make_unique<UnifiedSystemTrayModel>(
-          shelf->GetStatusAreaWidget()->GetRootView())),
+      model_(std::make_unique<UnifiedSystemTrayModel>(shelf)),
       slider_bubble_controller_(
           std::make_unique<UnifiedSliderBubbleController>(this)),
       privacy_screen_toast_controller_(
@@ -136,7 +134,7 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
           new CameraMicTrayItemView(shelf, CameraMicTrayItemView::Type::kMic)),
       notification_counter_item_(new NotificationCounterView(shelf)),
       quiet_mode_view_(new QuietModeView(shelf)),
-      time_view_(new tray::TimeTrayItemView(shelf)) {
+      time_view_(new tray::TimeTrayItemView(shelf, model())) {
   tray_container()->SetMargin(
       kUnifiedTrayContentPadding -
           ShelfConfig::Get()->status_area_hit_region_padding(),
@@ -175,6 +173,10 @@ UnifiedSystemTray::~UnifiedSystemTray() {
   if (bubble_)
     bubble_->CloseNow();
   bubble_.reset();
+
+  // Reset the view to remove its dependency from |model_|, since this view is
+  // destructed after |model_|.
+  time_view_->Reset();
 }
 
 bool UnifiedSystemTray::IsBubbleShown() const {

ash/system/unified/unified_system_tray_model.cc
@@ -5,10 +5,23 @@
 #include "ash/system/unified/unified_system_tray_model.h"
 
 #include "ash/accessibility/accessibility_controller_impl.h"
+#include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
+#include "ash/shell_observer.h"
 #include "ash/system/brightness_control_delegate.h"
+#include "ash/system/status_area_widget.h"
 #include "base/bind.h"
 #include "chromeos/dbus/power_manager/backlight.pb.h"
+
+namespace {
+
+// The minimum width for system tray with size of kMedium.
+constexpr int kMinWidthMediumSystemTray = 800;
+
+// The maximum width for system tray with size of kMedium.
+constexpr int kMaxWidthMediumSystemTray = 1280;
+
+}  // namespace
 
 namespace ash {
 
@@ -32,6 +45,31 @@ class UnifiedSystemTrayModel::DBusObserver
   base::WeakPtrFactory<DBusObserver> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(DBusObserver);
+};
+
+class UnifiedSystemTrayModel::SizeObserver : public display::DisplayObserver,
+                                             public ShellObserver {
+ public:
+  explicit SizeObserver(UnifiedSystemTrayModel* owner);
+  ~SizeObserver() override;
+  SizeObserver(const SizeObserver&) = delete;
+  SizeObserver& operator=(const SizeObserver&) = delete;
+
+ private:
+  // display::DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
+
+  // ShellObserver:
+  void OnShelfAlignmentChanged(aura::Window* root_window,
+                               ShelfAlignment old_alignment) override;
+
+  void Update();
+
+  UnifiedSystemTrayModel* const owner_;
+
+  // Keep track of current system tray size.
+  UnifiedSystemTrayModel::SystemTrayButtonSize system_tray_size_;
 };
 
 UnifiedSystemTrayModel::DBusObserver::DBusObserver(
@@ -72,9 +110,51 @@ void UnifiedSystemTrayModel::DBusObserver::KeyboardBrightnessChanged(
           power_manager::BacklightBrightnessChange_Cause_USER_REQUEST);
 }
 
-UnifiedSystemTrayModel::UnifiedSystemTrayModel(views::View* owner_view)
-    : dbus_observer_(std::make_unique<DBusObserver>(this)),
-      pagination_model_(std::make_unique<PaginationModel>(owner_view)) {}
+UnifiedSystemTrayModel::SizeObserver::SizeObserver(
+    UnifiedSystemTrayModel* owner)
+    : owner_(owner) {
+  display::Screen::GetScreen()->AddObserver(this);
+  Shell::Get()->AddShellObserver(this);
+  system_tray_size_ = owner_->GetSystemTrayButtonSize();
+}
+
+UnifiedSystemTrayModel::SizeObserver::~SizeObserver() {
+  display::Screen::GetScreen()->RemoveObserver(this);
+  Shell::Get()->RemoveShellObserver(this);
+}
+
+void UnifiedSystemTrayModel::SizeObserver::OnDisplayMetricsChanged(
+    const display::Display& display,
+    uint32_t changed_metrics) {
+  if (owner_->GetDisplay().id() != display.id())
+    return;
+  Update();
+}
+
+void UnifiedSystemTrayModel::SizeObserver::OnShelfAlignmentChanged(
+    aura::Window* root_window,
+    ShelfAlignment old_alignment) {
+  Update();
+}
+
+void UnifiedSystemTrayModel::SizeObserver::Update() {
+  UnifiedSystemTrayModel::SystemTrayButtonSize new_size =
+      owner_->GetSystemTrayButtonSize();
+  if (system_tray_size_ == new_size)
+    return;
+
+  system_tray_size_ = new_size;
+  owner_->SystemTrayButtonSizeChanged(system_tray_size_);
+}
+
+UnifiedSystemTrayModel::UnifiedSystemTrayModel(Shelf* shelf)
+    : shelf_(shelf),
+      dbus_observer_(std::make_unique<DBusObserver>(this)),
+      size_observer_(std::make_unique<SizeObserver>(this)) {
+  // |shelf_| might be null in unit tests.
+  pagination_model_ = std::make_unique<PaginationModel>(
+      shelf_ ? shelf_->GetStatusAreaWidget()->GetRootView() : nullptr);
+}
 
 UnifiedSystemTrayModel::~UnifiedSystemTrayModel() = default;
 
@@ -124,6 +204,23 @@ void UnifiedSystemTrayModel::ClearNotificationChanges() {
   notification_changes_.clear();
 }
 
+UnifiedSystemTrayModel::SystemTrayButtonSize
+UnifiedSystemTrayModel::GetSystemTrayButtonSize() const {
+  // |shelf_| might be null in unit tests, returns medium size as default.
+  if (!shelf_)
+    return SystemTrayButtonSize::kMedium;
+
+  int display_size = shelf_->IsHorizontalAlignment()
+                         ? GetDisplay().size().width()
+                         : GetDisplay().size().height();
+
+  if (display_size < kMinWidthMediumSystemTray)
+    return SystemTrayButtonSize::kSmall;
+  if (display_size <= kMaxWidthMediumSystemTray)
+    return SystemTrayButtonSize::kMedium;
+  return SystemTrayButtonSize::kLarge;
+}
+
 void UnifiedSystemTrayModel::DisplayBrightnessChanged(float brightness,
                                                       bool by_user) {
   display_brightness_ = brightness;
@@ -136,6 +233,24 @@ void UnifiedSystemTrayModel::KeyboardBrightnessChanged(float brightness,
   keyboard_brightness_ = brightness;
   for (auto& observer : observers_)
     observer.OnKeyboardBrightnessChanged(by_user);
+}
+
+void UnifiedSystemTrayModel::SystemTrayButtonSizeChanged(
+    SystemTrayButtonSize system_tray_size) {
+  for (auto& observer : observers_)
+    observer.OnSystemTrayButtonSizeChanged(system_tray_size);
+}
+
+const display::Display UnifiedSystemTrayModel::GetDisplay() const {
+  // |shelf_| might be null in unit tests, returns primary display as default.
+  if (!shelf_)
+    return display::Screen::GetScreen()->GetPrimaryDisplay();
+
+  return display::Screen::GetScreen()->GetDisplayNearestWindow(
+      shelf_->GetStatusAreaWidget()
+          ->GetRootView()
+          ->GetWidget()
+          ->GetNativeWindow());
 }
 
 }  // namespace ash

ash/system/unified/unified_system_tray_model.h
@@ -10,7 +10,13 @@
 #include "base/observer_list.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 
+namespace display {
+class Display;
+}  // namespace display
+
 namespace ash {
+
+class Shelf;
 
 // Model class that stores UnifiedSystemTray's UI specific variables. Owned by
 // UnifiedSystemTray status area button. Not to be confused with UI agnostic
@@ -36,6 +42,19 @@ class ASH_EXPORT UnifiedSystemTrayModel {
     NOTIFICATION_ID,
   };
 
+  // Enumeration of possible sizes of the system tray button. Larger screen will
+  // have larger tray button with additional information.
+  enum class SystemTrayButtonSize {
+    // Display wifi, battery, notification counter icons and time.
+    kSmall = 0,
+    // Display those in small unified system tray, plus important notification
+    // icons.
+    kMedium = 1,
+    // Display those in medium unified system tray, plus the current date.
+    kLarge = 2,
+    kMaxValue = kLarge,
+  };
+
   class Observer {
    public:
     virtual ~Observer() {}
@@ -43,9 +62,11 @@ class ASH_EXPORT UnifiedSystemTrayModel {
     // |by_user| is true when brightness is changed by user action.
     virtual void OnDisplayBrightnessChanged(bool by_user) {}
     virtual void OnKeyboardBrightnessChanged(bool by_user) {}
+    virtual void OnSystemTrayButtonSizeChanged(
+        SystemTrayButtonSize system_tray_size) {}
   };
 
-  explicit UnifiedSystemTrayModel(views::View* owner_view);
+  explicit UnifiedSystemTrayModel(Shelf* shelf);
   ~UnifiedSystemTrayModel();
 
   void AddObserver(Observer* observer);
@@ -78,6 +99,9 @@ class ASH_EXPORT UnifiedSystemTrayModel {
   // NOTIFICATION_ID.
   void SetTargetNotification(const std::string& notification_id);
 
+  // Get the size of the system tray depends on the size of the display screen.
+  SystemTrayButtonSize GetSystemTrayButtonSize() const;
+
   float display_brightness() const { return display_brightness_; }
   float keyboard_brightness() const { return keyboard_brightness_; }
 
@@ -102,8 +126,15 @@ class ASH_EXPORT UnifiedSystemTrayModel {
  private:
   class DBusObserver;
 
+  // Keeps track all the sources that can change the size of system tray button.
+  class SizeObserver;
+
   void DisplayBrightnessChanged(float brightness, bool by_user);
   void KeyboardBrightnessChanged(float brightness, bool by_user);
+  void SystemTrayButtonSizeChanged(SystemTrayButtonSize system_tray_size);
+
+  // Get the display that owns the tray.
+  const display::Display GetDisplay() const;
 
   // Target mode which is used to decide the scroll position of the message
   // center on opening. See the comment in |NotificationTargetMode|.
@@ -128,7 +159,11 @@ class ASH_EXPORT UnifiedSystemTrayModel {
   // <notification ID, if notification is manually expanded>
   std::map<std::string, bool> notification_changes_;
 
+  Shelf* const shelf_;
+
   std::unique_ptr<DBusObserver> dbus_observer_;
+
+  std::unique_ptr<SizeObserver> size_observer_;
 
   base::ObserverList<Observer>::Unchecked observers_;
 

ash/system/time/time_tray_item_view_unittest.cc
@@ -1,0 +1,97 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/time/time_tray_item_view.h"
+
+#include "ash/public/cpp/ash_features.h"
+#include "ash/shelf/shelf.h"
+#include "ash/system/time/time_view.h"
+#include "ash/system/unified/unified_system_tray_model.h"
+#include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
+
+namespace ash {
+namespace tray {
+
+class TimeTrayItemViewTest : public AshTestBase,
+                             public testing::WithParamInterface<bool> {
+ public:
+  TimeTrayItemViewTest() = default;
+  ~TimeTrayItemViewTest() override = default;
+
+  // AshTestBase:
+  void SetUp() override {
+    AshTestBase::SetUp();
+    scoped_feature_list_.InitWithFeatureState(
+        features::kScalableStatusArea, is_scalable_status_area_enabled());
+
+    model_ = std::make_unique<UnifiedSystemTrayModel>(GetPrimaryShelf());
+    time_tray_item_view_ =
+        std::make_unique<TimeTrayItemView>(GetPrimaryShelf(), model_.get());
+  }
+
+  void TearDown() override {
+    time_tray_item_view_.reset();
+    model_.reset();
+    AshTestBase::TearDown();
+  }
+
+  bool is_scalable_status_area_enabled() { return GetParam(); }
+
+  // Returns true if the time view is in horizontal layout, false if it is in
+  // vertical layout.
+  bool IsTimeViewInHorizontalLayout() {
+    // Time view is in horizontal layout if its subview is in use (it transfers
+    // the ownership to the views hierarchy and becomes nullptr),
+    return !time_tray_item_view_->time_view_->horizontal_view_;
+  }
+
+  bool ShouldShowDateInTimeView() {
+    return time_tray_item_view_->time_view_->show_date_when_horizontal_;
+  }
+
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
+  std::unique_ptr<UnifiedSystemTrayModel> model_;
+  std::unique_ptr<TimeTrayItemView> time_tray_item_view_;
+};
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         TimeTrayItemViewTest,
+                         testing::Bool() /* is_scalable_status_area_enabled */);
+
+TEST_P(TimeTrayItemViewTest, ShelfAlignment) {
+  // The tray should show time horizontal view when the shelf is bottom.
+  GetPrimaryShelf()->SetAlignment(ShelfAlignment::kBottom);
+  time_tray_item_view_->UpdateAlignmentForShelf(GetPrimaryShelf());
+  EXPECT_TRUE(IsTimeViewInHorizontalLayout());
+
+  // The tray should show time vertical view when the shelf is left or right.
+  GetPrimaryShelf()->SetAlignment(ShelfAlignment::kLeft);
+  time_tray_item_view_->UpdateAlignmentForShelf(GetPrimaryShelf());
+  EXPECT_FALSE(IsTimeViewInHorizontalLayout());
+
+  GetPrimaryShelf()->SetAlignment(ShelfAlignment::kRight);
+  time_tray_item_view_->UpdateAlignmentForShelf(GetPrimaryShelf());
+  EXPECT_FALSE(IsTimeViewInHorizontalLayout());
+
+  // The tray should show time horizontal view when switching back to bottom
+  // shelf.
+  GetPrimaryShelf()->SetAlignment(ShelfAlignment::kBottom);
+  time_tray_item_view_->UpdateAlignmentForShelf(GetPrimaryShelf());
+  EXPECT_TRUE(IsTimeViewInHorizontalLayout());
+}
+
+TEST_P(TimeTrayItemViewTest, DisplayChanged) {
+  UpdateDisplay("800x800");
+  EXPECT_FALSE(ShouldShowDateInTimeView());
+
+  // Date should be shown in large screen size (when scalable status area is
+  // enabled).
+  UpdateDisplay("1680x800");
+  EXPECT_EQ(is_scalable_status_area_enabled(), ShouldShowDateInTimeView());
+}
+
+}  // namespace tray
+}  // namespace ash

ash/system/time/time_view_unittest.cc
@@ -76,5 +76,29 @@ TEST_F(TimeViewTest, Basics) {
   EXPECT_FALSE(vertical_view()->parent());
 }
 
+// Test the show date mode in the time view.
+TEST_F(TimeViewTest, ShowDateMode) {
+  CreateTimeView(TimeView::ClockLayout::HORIZONTAL_CLOCK);
+  base::string16 time_text = horizontal_label()->GetText();
+
+  // When showing date, the text is expected to be longer since it's showing
+  // more content.
+  time_view()->SetShowDateWhenHorizontal(true /* show_date_when_horizontal */);
+  EXPECT_GT(horizontal_label()->GetText(), time_text);
+
+  // Resetting show date mode should show only the time.
+  time_view()->SetShowDateWhenHorizontal(false /* show_date_when_horizontal */);
+  EXPECT_EQ(time_text, horizontal_label()->GetText());
+
+  time_view()->UpdateClockLayout(TimeView::ClockLayout::VERTICAL_CLOCK);
+  base::string16 hours_text = vertical_label_hours()->GetText();
+  base::string16 minutes_text = vertical_label_minutes()->GetText();
+
+  // Show date mode should not affect vertical view.
+  time_view()->SetShowDateWhenHorizontal(true /* show_date_when_horizontal */);
+  EXPECT_EQ(hours_text, vertical_label_hours()->GetText());
+  EXPECT_EQ(minutes_text, vertical_label_minutes()->GetText());
+}
+
 }  // namespace tray
 }  // namespace ash