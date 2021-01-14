fb8e0241b2adf884d191fc5d626503e5952c1545
Reilly Grant <reillyg@chromium.org>
2021-01-14 11:56:52 +0000

    Re-enable BluetoothShellApiTest.ApiSanityCheck
    This change re-enables BluetoothShellApiTest.ApiSanityCheck on macOS
    and Chrome OS. To fix the flake observed on macOS the resource bundle
    for extensions_browsertest now includes the Bluetooth strings.
    
    Without these strings if a Bluetooth device is discovered there will
    be a DCHECK failure because the string needed to localize the device
    is missing from the resource bundle used for tests.
    
    Bug: 711484, 1165955
    Change-Id: I8636feaff5180fd536f254f841dab89b2d5cc3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628246
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843490}

--- extensions/browser/api/bluetooth/bluetooth_appshell_test.cc
+++ extensions/browser/api/bluetooth/bluetooth_appshell_test.cc
@@ -7,12 +7,6 @@
 
 using BluetoothShellApiTest = extensions::ShellApiTest;
 
-// TODO(crbug.com/1165955): this test flakes on Mac ASAN
-#if defined(OS_MAC)
-#define MAYBE_ApiSanityCheck DISABLED_ApiSanityCheck
-#else
-#define MAYBE_ApiSanityCheck ApiSanityCheck
-#endif
-IN_PROC_BROWSER_TEST_F(BluetoothShellApiTest, MAYBE_ApiSanityCheck) {
+IN_PROC_BROWSER_TEST_F(BluetoothShellApiTest, ApiSanityCheck) {
   ASSERT_TRUE(RunAppTest("api_test/bluetooth"));
 }
