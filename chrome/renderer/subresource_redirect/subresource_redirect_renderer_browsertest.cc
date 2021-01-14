42167dc9651a1f1b1211dc986fd12c4f6eb5162c
rajendrant <rajendrant@chromium.org>
2021-01-14 07:04:47 +0000

    Disable flaky test
    Bug: 1166280
    Change-Id: I9ce5d95e5d0eb68d20753ab2832e78f6af30f874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628187
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843421}

--- chrome/renderer/subresource_redirect/subresource_redirect_renderer_browsertest.cc
+++ chrome/renderer/subresource_redirect/subresource_redirect_renderer_browsertest.cc
@@ -104,10 +104,11 @@ class SubresourceRedirectLoggedInSitesBrowserTest
 #define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
 #endif
 
+// TODO(crbug.com/1166280): Enable the test after fixing the flake.
 // Verify that when image load gets canceled due to subsequent page load, the
 // subresource redirect for the image is canceled as well.
 IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
-                       DISABLE_ON_WIN_MAC_CHROMEOS(TestCancelBeforeImageLoad)) {
+                       DISABLED_TestCancelBeforeImageLoad) {
   robots_rules_server_.set_failure_mode(
       RobotsRulesTestServer::FailureMode::kTimeout);
   robots_rules_server_.AddRobotsRules(GetHttpsTestURL("/"),
@@ -141,11 +142,11 @@ IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
   image_compression_server_.VerifyRequestedImagePaths({});
 }
 
+// TODO(crbug.com/1166280): Enable the test after fixing the flake.
 // Verify that when image load gets canceled due to subsequent navigation to a
 // logged-in page, the subresource redirect for the image is disabled as well.
-IN_PROC_BROWSER_TEST_F(
-    SubresourceRedirectLoggedInSitesBrowserTest,
-    DISABLE_ON_WIN_MAC_CHROMEOS(TestCancelBeforeImageLoadForLoggedInSite)) {
+IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
+                       DISABLED_TestCancelBeforeImageLoadForLoggedInSite) {
   robots_rules_server_.set_failure_mode(
       RobotsRulesTestServer::FailureMode::kTimeout);
   robots_rules_server_.AddRobotsRules(GetHttpsTestURL("/"),
