9da2aae72f0bb4ed3e2215efee7c2ba6f7d512b2
Keishi Hattori <keishi@chromium.org>
2021-01-15 08:43:21 +0000

    Add use_backup_ref_ptr gn args flag
    Add use_backup_ref_ptr gn args flag which enables the ref count in PA and switches the CheckedPtr implementation to BackupRefPtr.
    
    Also turns off backup ref ptr support for NaCl as it is compiled as 32bit but address_pool_manager is not available, causing a link errors.
    https://source.chromium.org/chromium/chromium/src/+/master:base/BUILD.gn;l=1772
    
    Bug: 1073933
    Change-Id: I1e0ea1b1181623fa5783d39c6d9a379c1df2daf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626681
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843988}

--- base/allocator/partition_allocator/checked_ptr_support.h
+++ base/allocator/partition_allocator/checked_ptr_support.h
@@ -5,8 +5,17 @@
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_CHECKED_PTR_SUPPORT_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_CHECKED_PTR_SUPPORT_H_
 
+#include "base/partition_alloc_buildflags.h"
+#include "build/build_config.h"
+#include "build/buildflag.h"
+
+#if BUILDFLAG(USE_BACKUP_REF_PTR) && !defined(OS_NACL)
+#define ENABLE_REF_COUNT_FOR_BACKUP_REF_PTR 1
+#define DISABLE_REF_COUNT_IN_RENDERER 1
+#else
 #define ENABLE_REF_COUNT_FOR_BACKUP_REF_PTR 0
 #define DISABLE_REF_COUNT_IN_RENDERER 0
+#endif
 
 #if DISABLE_REF_COUNT_IN_RENDERER
 static_assert(ENABLE_REF_COUNT_FOR_BACKUP_REF_PTR,
--- base/memory/checked_ptr.h
+++ base/memory/checked_ptr.h
@@ -20,7 +20,12 @@
 #include "build/build_config.h"
 #include "build/buildflag.h"
 
+#if BUILDFLAG(USE_BACKUP_REF_PTR) && !defined(OS_NACL)
+#define ENABLE_BACKUP_REF_PTR_IMPL 1
+#else
 #define ENABLE_BACKUP_REF_PTR_IMPL 0
+#endif
+
 #if ENABLE_BACKUP_REF_PTR_IMPL
 static_assert(ENABLE_REF_COUNT_FOR_BACKUP_REF_PTR,
               "BackupRefPtrImpl can only by used if PartitionRefCount is "
