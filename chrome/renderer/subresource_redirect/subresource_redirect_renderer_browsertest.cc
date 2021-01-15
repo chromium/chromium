8e46b4209e0178797a177c6c5fca6d43ada0f857
rajendrant <rajendrant@chromium.org>
2021-01-15 06:44:35 +0000

    Revert "Fix flaky test"
    This reverts commit c08b43037e6f16ae2417cbe804e6f514e09afec2.
    
    Reason for revert: Revert due to test failures
    
    Original change's description:
    > Fix flaky test
    >
    > Fix the path used in test server.
    >
    > Bug: 1166280
    > Change-Id: I34fc06dc911c69d00f8dc4d193052d088f99816a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626022
    > Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    > Commit-Queue: rajendrant <rajendrant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#843864}
    
    TBR=rajendrant@chromium.org,mcrouse@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Ibfee8d1710479ea363bf41ea4cf02e8df400d964
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1166280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631913
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843971}

--- chrome/renderer/subresource_redirect/subresource_redirect_renderer_browsertest.cc
+++ chrome/renderer/subresource_redirect/subresource_redirect_renderer_browsertest.cc
@@ -105,10 +105,11 @@ class SubresourceRedirectLoggedInSitesBrowserTest
 #define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
 #endif
 
+// TODO(crbug.com/1166280): Enable the test after fixing the flake.
 // Verify that when image load gets canceled due to subsequent page load, the
 // subresource redirect for the image is canceled as well.
 IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
-                       DISABLE_ON_WIN_MAC_CHROMEOS(TestCancelBeforeImageLoad)) {
+                       DISABLED_TestCancelBeforeImageLoad) {
   robots_rules_server_.set_failure_mode(
       RobotsRulesTestServer::FailureMode::kTimeout);
   robots_rules_server_.AddRobotsRules(GetHttpsTestURL("/"),
@@ -122,7 +123,8 @@ IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
     base::RunLoop().RunUntilIdle();
   }
 
-  ui_test_utils::NavigateToURL(browser(), GetHttpsTestURL("/simple.html"));
+  ui_test_utils::NavigateToURL(browser(),
+                               GetHttpsTestURL("/load_image/simple.html"));
   FetchHistogramsFromChildProcesses();
 
   RetryForHistogramUntilCountReached(
@@ -141,11 +143,11 @@ IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
   image_compression_server_.VerifyRequestedImagePaths({});
 }
 
+// TODO(crbug.com/1166280): Enable the test after fixing the flake.
 // Verify that when image load gets canceled due to subsequent navigation to a
 // logged-in page, the subresource redirect for the image is disabled as well.
-IN_PROC_BROWSER_TEST_F(
-    SubresourceRedirectLoggedInSitesBrowserTest,
-    DISABLE_ON_WIN_MAC_CHROMEOS(TestCancelBeforeImageLoadForLoggedInSite)) {
+IN_PROC_BROWSER_TEST_F(SubresourceRedirectLoggedInSitesBrowserTest,
+                       DISABLED_TestCancelBeforeImageLoadForLoggedInSite) {
   robots_rules_server_.set_failure_mode(
       RobotsRulesTestServer::FailureMode::kTimeout);
   robots_rules_server_.AddRobotsRules(GetHttpsTestURL("/"),
@@ -160,7 +162,8 @@ IN_PROC_BROWSER_TEST_F(
   }
 
   ui_test_utils::NavigateToURL(
-      browser(), https_test_server_.GetURL("loggedin.com", "/simple.html"));
+      browser(),
+      https_test_server_.GetURL("loggedin.com", "/load_image/simple.html"));
   FetchHistogramsFromChildProcesses();
   histogram_tester_.ExpectBucketCount(
       "Login.PageLoad.DetectionType",
