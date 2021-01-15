af98eac824bcc23ff253765682f13a60d8340c0c
Frédéric Wang <fwang@igalia.com>
2021-01-15 11:35:08 +0000

    Rewrite SecurityOrigin::IsSecure() using GURL and url::Origin
    SecurityOrigin::IsSecure() is very similar to the concept of
    "potentially trustworthy URL" defined in [1] and implemented by
    network::IsUrlPotentiallyTrustworthy(), but it has historically been
    hard to compare them. After previous refactoring they are quite close.
    
    This CL is yet another iteration. SecurityOrigin::IsSecure() is
    rewritten to use a similar implementation based on GURL and url::Origin.
    Spec comments are also added to make the difference more obvious.
    
    IsSecure() currently returns true if the URL scheme is in the secure
    list [2] or possibly one wrapped in a blob: or filesystem:. The
    constructor of url::Origin automatically unwraps this kind of URLs
    but is also more restrictive about accepted schemes [3]. In particular,
    about: and data: URLs or URLs with schemes registered as "secure"
    but not "standard" will now generate invalid origins.
    
    Special cases from network::IsUrlPotentiallyTrustworthy() are added to
    preserve behavior with about: and data: schemes, although some weird
    edge cases like "about:about" or "blob:about" are no longer treated as
    secure. Behavior for all standard secure schemes (including https, wss,
    quic-transport) are preserved. All these changes makes IsSecure()
    closer to  the spec definition of "potentially trustworthy URL".
    
    The rest of the IsSecure() handles opaque and allowedlist origins, which
    is again just a subset of the work done by
    network::IsUrlPotentiallyTrustworthy() and can be rewritten accordingly.
    
    [1] https://w3c.github.io/webappsec-secure-contexts/#is-origin-trustworthy
    [2] https://source.chromium.org/chromium/chromium/src/+/master:url/url_util.cc;l=63;drc=65a062c4528790ff3651920ba9a3934df184e1bc
    [3] https://source.chromium.org/chromium/chromium/src/+/master:url/origin.cc;l=47;drc=721926d0cf09b42968ec8481792fcd6fa4edab7b
    
    Bug: 1153336
    Change-Id: I8f4a42f89a9ae13016a20a1e242c1efd48874162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617709
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#844034}

--- third_party/blink/renderer/platform/weborigin/security_origin_test.cc
+++ third_party/blink/renderer/platform/weborigin/security_origin_test.cc
@@ -231,9 +231,6 @@ TEST_F(SecurityOriginTest, IsSecure) {
     bool is_secure;
     const char* url;
   } inputs[] = {
-      // TODO(crbug.com/1153336): Should SecurityOrigin::IsSecure be aligned
-      // with network::IsURLPotentiallyTrustworthy?
-      // https://w3c.github.io/webappsec-secure-contexts/#is-url-trustworthy
       {false, "blob:ftp://evil:99/578223a1-8c13-17b3-84d5-eca045ae384a"},
       {false, "blob:http://example.com/578223a1-8c13-17b3-84d5-eca045ae384a"},
       {false, "file:///etc/passwd"},
@@ -245,14 +242,14 @@ TEST_F(SecurityOriginTest, IsSecure) {
       {true, "wss://example.com/"},
       {true, "about:blank"},
       {true, "about:srcdoc"},
-      {true, "about:about"},
+      {false, "about:about"},
       {true, "data:text/html,Hello"},
       {false,
        "filesystem:http://example.com/578223a1-8c13-17b3-84d5-eca045ae384a"},
       {true,
        "filesystem:https://example.com/578223a1-8c13-17b3-84d5-eca045ae384a"},
-      {true, "blob:data:text/html,Hello"},
-      {true, "blob:about:blank"},
+      {false, "blob:data:text/html,Hello"},
+      {false, "blob:about:blank"},
       {false, "filesystem:data:text/html,Hello"},
       {false, "filesystem:about:blank"},
       {false,
@@ -289,9 +286,7 @@ TEST_F(SecurityOriginTest, CustomScheme) {
   {
     url::ScopedSchemeRegistryForTests scoped_registry;
     url::AddSecureScheme(custom_scheme);
-    // TODO(crbug.com/1153336): Should SecurityOrigin::IsSecure always consider
-    // non-standard schemes as insecure?
-    EXPECT_TRUE(SecurityOrigin::IsSecure(url));
+    EXPECT_FALSE(SecurityOrigin::IsSecure(url));
     scoped_refptr<const SecurityOrigin> origin =
         SecurityOrigin::CreateFromString(custom_scheme_example);
     EXPECT_FALSE(origin->IsPotentiallyTrustworthy());
