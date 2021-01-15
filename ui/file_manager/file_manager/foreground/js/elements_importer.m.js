53b78f8ce7b999722cbdc03d24631967a176e444
Luciano Pacheco <lucmult@chromium.org>
2021-01-15 06:49:03 +0000

    Files app: Add JS module for foreground secondary surfaces
    Add JS modules for //ui/file_manager/file_manger/foreground/js/ :
    - element_importer.js
    - deferred_elements.m.js This replaces the elements_bundle.html.
    
    Bug: 1133186
    Change-Id: I372fbb5afe8bdd22483d0fcccda3b2dd240f447a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631812
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843972}

---
+++ ui/file_manager/file_manager/foreground/js/elements_importer.m.js
@@ -0,0 +1,26 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @return {!Promise<void>} */
+export function importElements() {
+  const startTime = Date.now();
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = './foreground/js/deferred_elements.m.js';
+
+    script.onload = () => {
+      console.log('Elements imported.');
+      chrome.metricsPrivate.recordTime(
+          'FileBrowser.Load.ImportElements', Date.now() - startTime);
+      resolve();
+    };
+    script.onerror = (error) => {
+      console.error(error);
+      reject(error);
+    };
+
+    document.head.appendChild(script);
+  });
+}
