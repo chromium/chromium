a5c10a7e391fd4947b6780492838f6b3158f7182
Fabrice de Gans-Riberi <fdegans@chromium.org>
2021-01-15 19:26:12 +0000

    [cast_streaming] End the session when both streams send no data for 15s
    Previously, the Cast Streaming Session would be ended after either the
    audio or video stream received no data for 15s. However, some Cast
    Streaming Senders sometimes do not send video data for longer than
    that when there is no video update to send, resulting in the session
    being prematurely ended.
    
    This changes the Cast Streaming Receiver behavior to only end the
    session if no audio and no video data is received for 15 seconds. As
    a result, the frame duration has been changed to 10 minutes to prevent
    a demuxer underflow in the Chrome media pipeline, resulting in delayed
    playback.
    
    TBR=sergeyu@chromium.org
    (cherry picked from commit bd4eb61775f9afb9daccd8a4e4ff14b5daf782da)
    
    Bug: 1166371, b/172139177
    Change-Id: I2f66ca009bdedb00ae79ac8e9dda8cc2ebcd16af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617363
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#843227}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633096
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#1777}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- fuchsia/cast_streaming/cast_streaming_session.cc
+++ fuchsia/cast_streaming/cast_streaming_session.cc
@@ -77,6 +77,9 @@ media::VideoDecoderConfig VideoCaptureConfigToVideoDecoderConfig(
       media::EmptyExtraData(), media::EncryptionScheme::kUnencrypted);
 }
 
+// Timeout to stop the Session when no data is received.
+constexpr base::TimeDelta kNoDataTimeout = base::TimeDelta::FromSeconds(15);
+
 bool CreateDataPipeForStreamType(media::DemuxerStream::Type type,
                                  mojo::ScopedDataPipeProducerHandle* producer,
                                  mojo::ScopedDataPipeConsumerHandle* consumer) {
@@ -169,8 +172,8 @@ class CastStreamingSession::Internal
         base::BindRepeating(
             &CastStreamingSession::Client::OnAudioBufferReceived,
             base::Unretained(client_)),
-        base::BindOnce(&CastStreamingSession::Internal::OnDataTimeout,
-                       base::Unretained(this)));
+        base::BindRepeating(&base::OneShotTimer::Reset,
+                            base::Unretained(&data_timeout_timer_)));
 
     return AudioStreamInfo{
         AudioCaptureConfigToAudioDecoderConfig(audio_capture_config),
@@ -195,13 +198,15 @@ class CastStreamingSession::Internal
 
     // We can use unretained pointers here because StreamConsumer is owned by
     // this object and |client_| is guaranteed to outlive this object.
+    // |data_timeout_timer_| is also owned by this object and will outlive both
+    // StreamConsumers.
     video_consumer_ = std::make_unique<StreamConsumer>(
         video_receiver, std::move(data_pipe_producer),
         base::BindRepeating(
             &CastStreamingSession::Client::OnVideoBufferReceived,
             base::Unretained(client_)),
-        base::BindOnce(&CastStreamingSession::Internal::OnDataTimeout,
-                       base::Unretained(this)));
+        base::BindRepeating(&base::OneShotTimer::Reset,
+                            base::Unretained(&data_timeout_timer_)));
 
     return VideoStreamInfo{
         VideoCaptureConfigToVideoDecoderConfig(video_capture_config),
@@ -280,6 +285,10 @@ class CastStreamingSession::Internal
     } else {
       client_->OnSessionInitialization(std::move(audio_stream_info),
                                        std::move(video_stream_info));
+      data_timeout_timer_.Start(
+          FROM_HERE, kNoDataTimeout,
+          base::BindOnce(&CastStreamingSession::Internal::OnDataTimeout,
+                         base::Unretained(this)));
     }
   }
 
@@ -320,6 +329,9 @@ class CastStreamingSession::Internal
   CastMessagePortImpl cast_message_port_impl_;
   std::unique_ptr<openscreen::cast::ReceiverSession> receiver_session_;
   base::OneShotTimer init_timeout_timer_;
+
+  // Timer to trigger connection closure if no data is received for 15 seconds.
+  base::OneShotTimer data_timeout_timer_;
 
   bool is_initialized_ = false;
   CastStreamingSession::Client* const client_;
