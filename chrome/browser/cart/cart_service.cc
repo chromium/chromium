447110011b586d54f55398276f6b911c0970b8e3
Yue Zhang <yuezhanggg@chromium.org>
2021-01-16 00:23:41 +0000

    [ChromeCart] Clear database when detected history deletion
    Bug: 1157892
    Change-Id: If79167aa5cb6af2a9e639a93596410f18d39a144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616974
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844302}

--- chrome/browser/cart/cart_service.cc
+++ chrome/browser/cart/cart_service.cc
@@ -3,12 +3,22 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/cart/cart_service.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/search/ntp_features.h"
 
 CartService::CartService(Profile* profile)
-    : profile_(profile), cart_db_(std::make_unique<CartDB>(profile_)) {}
+    : profile_(profile),
+      cart_db_(std::make_unique<CartDB>(profile_)),
+      history_service_(HistoryServiceFactory::GetForProfile(
+          profile_,
+          ServiceAccessType::EXPLICIT_ACCESS)) {
+  if (history_service_) {
+    history_service_->AddObserver(this);
+  }
+}
+
 CartService::~CartService() = default;
 
 void CartService::RegisterProfilePrefs(PrefRegistrySimple* registry) {
@@ -38,4 +48,26 @@ void CartService::RestoreRemoved() {
 
 bool CartService::IsRemoved() {
   return profile_->GetPrefs()->GetBoolean(prefs::kCartModuleRemoved);
+}
+
+void CartService::OnOperationFinished(bool success) {
+  DCHECK(success) << "database operation failed.";
+}
+
+void CartService::Shutdown() {
+  if (history_service_) {
+    history_service_->RemoveObserver(this);
+  }
+}
+
+void CartService::OnURLsDeleted(history::HistoryService* history_service,
+                                const history::DeletionInfo& deletion_info) {
+  // TODO(crbug.com/1157892): Add more fine-grained deletion of cart data when
+  // history deletion happens.
+  cart_db_->DeleteAllCarts(base::BindOnce(&CartService::OnOperationFinished,
+                                          weak_ptr_factory_.GetWeakPtr()));
+}
+
+CartDB* CartService::GetDB() {
+  return cart_db_.get();
 }
--- chrome/browser/cart/cart_service_unittest.cc
+++ chrome/browser/cart/cart_service_unittest.cc
@@ -3,10 +3,30 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/cart/cart_service.h"
+#include "chrome/browser/cart/cart_db_content.pb.h"
 #include "chrome/browser/cart/cart_service_factory.h"
+#include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/persisted_state_db/profile_proto_db.h"
+#include "chrome/browser/persisted_state_db/profile_proto_db_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+cart_db::ChromeCartContentProto BuildProto(const char* domain) {
+  cart_db::ChromeCartContentProto proto;
+  proto.set_key(domain);
+  proto.set_merchant_cart_url("www.foo.com");
+  return proto;
+}
+
+const char kMockMerchantA[] = "A_merchant";
+const cart_db::ChromeCartContentProto kMockProtoA = BuildProto(kMockMerchantA);
+const std::vector<ProfileProtoDB<cart_db::ChromeCartContentProto>::KeyAndValue>
+    kExpectedA = {{kMockMerchantA, kMockProtoA}};
+const std::vector<ProfileProtoDB<cart_db::ChromeCartContentProto>::KeyAndValue>
+    kEmptyExpected = {};
+}  // namespace
 
 class CartServiceTest : public testing::Test {
  public:
@@ -17,6 +37,30 @@ class CartServiceTest : public testing::Test {
     testing::Test::SetUp();
 
     service_ = CartServiceFactory::GetForProfile(&profile_);
+    DCHECK(profile_.CreateHistoryService());
+  }
+
+  void OperationEvaluation(base::OnceClosure closure,
+                           bool expected_success,
+                           bool actual_success) {
+    EXPECT_EQ(expected_success, actual_success);
+    std::move(closure).Run();
+  }
+
+  void GetEvaluationPersistedStateDB(
+      base::OnceClosure closure,
+      std::vector<ProfileProtoDB<cart_db::ChromeCartContentProto>::KeyAndValue>
+          expected,
+      bool result,
+      std::vector<ProfileProtoDB<cart_db::ChromeCartContentProto>::KeyAndValue>
+          found) {
+    EXPECT_EQ(found.size(), expected.size());
+    for (size_t i = 0; i < expected.size(); i++) {
+      EXPECT_EQ(found[i].first, expected[i].first);
+      EXPECT_EQ(found[i].second.merchant_cart_url(),
+                expected[i].second.merchant_cart_url());
+    }
+    std::move(closure).Run();
   }
 
   void TearDown() override {}
@@ -48,4 +92,35 @@ TEST_F(CartServiceTest, TestRemoveStatusChange) {
 
   service_->RestoreRemoved();
   ASSERT_FALSE(service_->IsRemoved());
+}
+
+// Verifies the database is cleared when detected history deletion.
+TEST_F(CartServiceTest, TestOnHistoryDeletion) {
+  CartDB* cart_db_ = service_->GetDB();
+  base::RunLoop run_loop[3];
+  cart_db_->AddCart(
+      kMockMerchantA, kMockProtoA,
+      base::BindOnce(&CartServiceTest::OperationEvaluation,
+                     base::Unretained(this), run_loop[0].QuitClosure(), true));
+  task_environment_.RunUntilIdle();
+  run_loop[0].Run();
+
+  cart_db_->LoadAllCarts(base::BindOnce(
+      &CartServiceTest::GetEvaluationPersistedStateDB, base::Unretained(this),
+      run_loop[1].QuitClosure(), kExpectedA));
+  task_environment_.RunUntilIdle();
+  run_loop[1].Run();
+
+  service_->OnURLsDeleted(
+      HistoryServiceFactory::GetForProfile(&profile_,
+                                           ServiceAccessType::EXPLICIT_ACCESS),
+      history::DeletionInfo(history::DeletionTimeRange::Invalid(), false,
+                            history::URLRows(), std::set<GURL>(),
+                            base::nullopt));
+
+  cart_db_->LoadAllCarts(base::BindOnce(
+      &CartServiceTest::GetEvaluationPersistedStateDB, base::Unretained(this),
+      run_loop[2].QuitClosure(), kEmptyExpected));
+  task_environment_.RunUntilIdle();
+  run_loop[2].Run();
 }
