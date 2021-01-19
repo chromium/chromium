c7ab1c5600ba1ed23a49d74836b654835ac83760
Adam Norberg <norberg@google.com>
2021-01-19 21:05:21 +0000

    ExternalConstantsBuilder - class to write override file for tests
    This class uses the Builder pattern to write the config file loaded by
    ExternalConstantsOverrider. "Builder" is not common in C++, compared to
    Java, but here it seems like the easiest way to express our knowledge
    about what fields are supported in this file type; it will create
    reasonably terse integration test code and can be extended as new fields
    are added to the set of overridable constants.
    
    Bug: 1154901
    Change-Id: Ia17280e5a6eaad8cd4e1d1f04f5fd0515ac2e3c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631185
    Commit-Queue: Adam Norberg <norberg@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844892}

---
+++ chrome/updater/external_constants_builder_unittest.cc
@@ -0,0 +1,197 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/logging.h"
+#include "chrome/updater/constants.h"
+#include "chrome/updater/external_constants.h"
+#include "chrome/updater/external_constants_builder.h"
+#include "chrome/updater/external_constants_override.h"
+#include "chrome/updater/updater_branding.h"
+#include "chrome/updater/util.h"
+#include "url/gurl.h"
+
+namespace updater {
+
+namespace {
+
+void DeleteOverridesFile() {
+  base::FilePath target;
+  if (!GetBaseDirectory(&target)) {
+    LOG(ERROR) << "Could not get base directory to clean out overrides file.";
+  }
+  if (!base::DeleteFile(target.AppendASCII(kDevOverrideFileName))) {
+    // Note: base::DeleteFile returns `true` if there is no such file, which
+    // is what we want; "file already doesn't exist" is not an error here.
+    LOG(ERROR) << "Could not delete override file.";
+  }
+}
+
+}  // namespace
+
+class ExternalConstantsBuilderTests : public ::testing::Test {
+ protected:
+  void SetUp() override;
+  void TearDown() override;
+};
+
+void ExternalConstantsBuilderTests::SetUp() {
+  DeleteOverridesFile();
+}
+
+void ExternalConstantsBuilderTests::TearDown() {
+  DeleteOverridesFile();
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestOverridingNothing) {
+  EXPECT_TRUE(ExternalConstantsBuilder().Overwrite());
+
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+
+  EXPECT_TRUE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 1ul);
+  EXPECT_EQ(urls[0], GURL(UPDATE_CHECK_URL));
+
+  EXPECT_EQ(verifier->InitialDelay(), kInitialDelay);
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestOverridingEverything) {
+  ExternalConstantsBuilder builder;
+  builder.SetUpdateURL(std::vector<std::string>{"https://www.example.com"})
+      .SetUseCUP(false)
+      .SetInitialDelay(123);
+  EXPECT_TRUE(builder.Overwrite());
+
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+
+  EXPECT_FALSE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 1ul);
+  EXPECT_EQ(urls[0], GURL("https://www.example.com"));
+
+  EXPECT_EQ(verifier->InitialDelay(), 123);
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestPartialOverrideWithMultipleURLs) {
+  ExternalConstantsBuilder builder;
+  EXPECT_TRUE(builder
+                  .SetUpdateURL(std::vector<std::string>{
+                      "https://www.google.com", "https://www.example.com"})
+                  .Overwrite());
+
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+
+  EXPECT_TRUE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 2ul);
+  EXPECT_EQ(urls[0], GURL("https://www.google.com"));
+  EXPECT_EQ(urls[1], GURL("https://www.example.com"));
+
+  EXPECT_EQ(verifier->InitialDelay(), kInitialDelay);
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestClearedEverything) {
+  ExternalConstantsBuilder builder;
+  EXPECT_TRUE(builder
+                  .SetUpdateURL(std::vector<std::string>{
+                      "https://www.google.com", "https://www.example.com"})
+                  .SetUseCUP(false)
+                  .SetInitialDelay(123)
+                  .ClearUpdateURL()
+                  .ClearUseCUP()
+                  .ClearInitialDelay()
+                  .Overwrite());
+
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+  EXPECT_TRUE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 1ul);
+  EXPECT_EQ(urls[0], GURL(UPDATE_CHECK_URL));
+
+  EXPECT_EQ(verifier->InitialDelay(), kInitialDelay);
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestOverSet) {
+  EXPECT_TRUE(
+      ExternalConstantsBuilder()
+          .SetUpdateURL(std::vector<std::string>{"https://www.google.com"})
+          .SetUseCUP(true)
+          .SetInitialDelay(123)
+          .SetUpdateURL(std::vector<std::string>{"https://www.example.com"})
+          .SetUseCUP(false)
+          .SetInitialDelay(937)
+          .Overwrite());
+
+  // Only the second set of values should be observed.
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+  EXPECT_FALSE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 1ul);
+  EXPECT_EQ(urls[0], GURL("https://www.example.com"));
+
+  EXPECT_EQ(verifier->InitialDelay(), 937);
+}
+
+TEST_F(ExternalConstantsBuilderTests, TestReuseBuilder) {
+  ExternalConstantsBuilder builder;
+  EXPECT_TRUE(
+      builder.SetUpdateURL(std::vector<std::string>{"https://www.google.com"})
+          .SetUseCUP(false)
+          .SetInitialDelay(123)
+          .SetUpdateURL(std::vector<std::string>{"https://www.example.com"})
+          .Overwrite());
+
+  std::unique_ptr<ExternalConstantsOverrider> verifier =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+
+  EXPECT_FALSE(verifier->UseCUP());
+
+  std::vector<GURL> urls = verifier->UpdateURL();
+  ASSERT_EQ(urls.size(), 1ul);
+  EXPECT_EQ(urls[0], GURL("https://www.example.com"));
+
+  EXPECT_EQ(verifier->InitialDelay(), 123);
+
+  // But now we can use the builder again:
+  EXPECT_TRUE(builder.SetInitialDelay(92).ClearUpdateURL().Overwrite());
+
+  // We need a new overrider to verify because it only loads once.
+  std::unique_ptr<ExternalConstantsOverrider> verifier2 =
+      ExternalConstantsOverrider::FromDefaultJSONFile(
+          CreateDefaultExternalConstantsForTesting());
+
+  EXPECT_FALSE(verifier2->UseCUP());  // Not updated, value should be retained.
+
+  std::vector<GURL> urls2 = verifier2->UpdateURL();
+  ASSERT_EQ(urls2.size(), 1ul);
+  EXPECT_EQ(urls2[0], GURL(UPDATE_CHECK_URL));  // Cleared; should be default.
+
+  EXPECT_EQ(verifier2->InitialDelay(), 92);  // Updated; update should be seen.
+}
+
+}  // namespace updater
