2d2177301ae88d98d0b8c76cc117bc816c873d9c
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-01-20 04:38:36 +0000

    Revert "Add [[clang::nomerge]] attribute to ~CheckError()."
    This reverts commit 8860253376c38c090d585bda4b20b801e3aa3ce3.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 844989 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzg4NjAyNTMzNzZjMzhjMDkwZDU4NWJkYTRiMjBiODAxZTNhYTNjZTMM
    
    Sample Failed Build: https://ci.chromium.org/b/8857616434338832304
    
    Sample Failed Step: compile
    
    Original change's description:
    > Add [[clang::nomerge]] attribute to ~CheckError().
    > 
    > To disable merging multiple ~CheckError() destructor for accurate crash logs.
    > 
    > Bug: 1153188
    > Change-Id: If6d153661667a63f13b645f6d284eb3d5ea3a300
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611331
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Auto-Submit: Zequan Wu <zequanwu@google.com>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#844989}
    
    
    Change-Id: I8909aed272fc0c480f07a6c607f1ca5f65343c9c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1153188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639533
    Cr-Commit-Position: refs/heads/master@{#845015}

--- base/check.h
+++ base/check.h
@@ -85,7 +85,7 @@ class BASE_EXPORT CheckError {
   // Stream for adding optional details to the error message.
   std::ostream& stream();
 
-  NOMERGE ~CheckError();
+  ~CheckError();
 
   CheckError(const CheckError& other) = delete;
   CheckError& operator=(const CheckError& other) = delete;
