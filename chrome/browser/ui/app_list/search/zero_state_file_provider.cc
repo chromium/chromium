6f1c34e43f37970cf9f6d016872f8546206e430c
tby <tby@chromium.org>
2021-01-20 23:19:17 +0000

    [Suggested files] Change suggested content toggle gating
    This CL does two things.
    
    1. The previous CL only checked the value of the content toggle at
    initialization, meaning a user wouldn't immediately stop seeing
    suggestions after disabling, or start seeing them after reenabling.
    This CL moves the check to just before display time instead.
    
    2. This also adds a content toggle check to the local file provider,
    rather than just Drive in the previous CL. The UX of having one but
    not the other wasn't very good.
    
    (cherry picked from commit 72bac5aba943d9e74eedcc40708a6edef4d86391)
    
    Bug: 1034842
    Change-Id: I4f408d8d1e40ef44e4a80906aa10ad7333a49eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628709
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#843856}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640002
    Cr-Commit-Position: refs/branch-heads/4389@{#76}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chrome/browser/ui/app_list/search/zero_state_file_provider.cc
+++ chrome/browser/ui/app_list/search/zero_state_file_provider.cc
@@ -20,6 +20,8 @@
 #include "chrome/browser/ui/app_list/search/file_chip_result.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/recurrence_ranker.h"
 #include "chrome/browser/ui/app_list/search/zero_state_file_result.h"
+#include "chromeos/constants/chromeos_pref_names.h"
+#include "components/prefs/pref_service.h"
 
 using file_manager::file_tasks::FileTasksObserver;
 
@@ -46,6 +48,11 @@ internal::ValidAndInvalidResults ValidateFiles(
       invalid.emplace_back(path);
   }
   return {valid, invalid};
+}
+
+bool IsSuggestedContentEnabled(Profile* profile) {
+  return profile->GetPrefs()->GetBoolean(
+      chromeos::prefs::kSuggestedContentEnabled);
 }
 
 }  // namespace
@@ -113,7 +120,8 @@ void ZeroStateFileProvider::SetSearchResults(
     new_results.emplace_back(std::make_unique<ZeroStateFileResult>(
         filepath_score.first, filepath_score.second, profile_));
     // Add suggestion chip file results
-    if (app_list_features::IsSuggestedFilesEnabled()) {
+    if (app_list_features::IsSuggestedFilesEnabled() &&
+        IsSuggestedContentEnabled(profile_)) {
       new_results.emplace_back(std::make_unique<FileChipResult>(
           filepath_score.first, filepath_score.second, profile_));
     }
