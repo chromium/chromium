dc38b14b329c553a917f42cc594a069d67b8667d
Ken Rockot <rockot@google.com>
2021-01-20 19:21:03 +0000

    Move webmidi tests to wpt_internal
    Minimal changes are made here to adapt the webmidi tests to WPT and move
    them into wpt_internal.
    
    Bug: 1004256
    Change-Id: I78fe61ca5885c9896f63472d2b564eae6ec68951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638773
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845277}

---
+++ third_party/blink/web_tests/wpt_internal/webmidi/resources/permissions-helper.js
@@ -0,0 +1,5 @@
+function setMidiPermission(options, state) {
+  const sysex = options.sysex ?? false;
+  return internals.setPermission(
+      {name: 'midi', sysex: sysex}, state, location.origin, location.origin);
+}
