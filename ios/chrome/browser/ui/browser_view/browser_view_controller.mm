f6529508043663371170b4ec42628ef0b61cbbe1
Nazerke <Nazerke@google.com>
2021-01-20 23:15:04 +0000

    [ios] Add constraints to the tabstrip when it's needed.
    The crash report shows that the crash of |addConstraintsToTabStrip| is
    on the iphones only.
    
    This CL removes the adding the constraints to the tabstrip from the
    viewDidLoad, as currently it's crashing on it  while it's trying to add
    the constraints to the tabstripview which is nil on the iphones.
    
    Now it is moved to the point when the tabstripview is added, which does
    not let it run on the iphones as it's not needed and causes crashes.
    
    (cherry picked from commit 74a190ebb774606641bc3f275d74c4885b11b5b5)
    
    Bug: 1152059,  1128249
    Change-Id: Ic161b0d687b828ec57889c455d171c4f6a5fcf42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611977
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#841411}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639160
    Reviewed-by: Bindu Suvarna <bindusuvarna@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Bindu Suvarna <bindusuvarna@google.com>
    Cr-Commit-Position: refs/branch-heads/4324@{#1869}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1449,7 +1449,6 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   [self installFakeStatusBar];
   [self buildToolbarAndTabStrip];
   [self setUpViewLayout:YES];
-  [self addConstraintsToTabStrip];
   [self addConstraintsToToolbar];
 
   // If the tab model and browser state are valid, finish initialization.
@@ -2278,6 +2277,7 @@ NSString* const kBrowserViewControllerSnackbarCategory =
         [self addChildViewController:self.tabStripCoordinator.viewController];
         self.tabStripView = self.tabStripCoordinator.view;
         [self.view addSubview:self.tabStripView];
+        [self addConstraintsToTabStrip];
       }
       [self.view insertSubview:primaryToolbarView
                   aboveSubview:self.tabStripView];
