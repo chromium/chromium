80acff0beb66c134728fe38e7d19f9836cd19205
Tim Sergeant <tsergeant@chromium.org>
2021-01-21 00:15:43 +0000

    [App Service] Move Intent/IntentFilter conversion functions to util file
    The Shadow APKs feature (see crrev.com/c/2548592) needs to convert
    between ARC++ and App Service types for IntentFilters, and will in
    the future need to do the same for Intents.
    
    This CL moves the conversion functions to intent_util.h, where they
    can be accessed by Shadow APKs code.
    
    BUG=1099134
    
    Change-Id: I291d10c8ce53ed1890453c0f676b94e4c39121cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639055
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845420}

--- chrome/browser/apps/app_service/intent_util.h
+++ chrome/browser/apps/app_service/intent_util.h
@@ -7,9 +7,14 @@
 
 #include <vector>
 
+#include "components/arc/mojom/intent_helper.mojom-forward.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 
 class Profile;
+
+namespace arc {
+class IntentFilter;
+}
 
 namespace base {
 class FilePath;
@@ -35,6 +40,19 @@ apps::mojom::IntentPtr CreateShareIntentFromFiles(
     const std::vector<std::string>& mime_types,
     const std::string& share_text,
     const std::string& share_title);
+
+// Convert between App Service and ARC IntentFilters.
+arc::IntentFilter CreateArcIntentFilter(
+    const std::string& package_name,
+    const apps::mojom::IntentFilterPtr& intent_filter);
+apps::mojom::IntentFilterPtr ConvertArcIntentFilter(
+    const arc::IntentFilter& arc_intent_filter);
+
+// Convert between App Service and ARC Intents.
+arc::mojom::IntentInfoPtr CreateArcIntent(const apps::mojom::IntentPtr& intent);
+base::flat_map<std::string, std::string> CreateArcIntentExtras(
+    const apps::mojom::IntentPtr& intent);
+
 }  // namespace apps_util
 
 #endif  // CHROME_BROWSER_APPS_APP_SERVICE_INTENT_UTIL_H_
