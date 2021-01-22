1c42fa0165adb718e815bacbe5c75a87c54114a1
Jérôme Lebel <jlebel@chromium.org>
2021-01-22 13:22:16 +0000

    [iOS] Adding signout dialog for Mice
    Adds signout confirmation dialog for Mice based on this design:
    https://docs.google.com/presentation/d/1mfVdeJXk2hLXjyzOp9TBIx0E7-7jmuoN1-4oB6dYd5I/edit#slide=id.gaea85d524f_2_0
    
    iPad:
    https://drive.google.com/file/d/1UAS0L-ANTaIDEN2FFh2mgAiwKe-ksQ5U
    https://drive.google.com/file/d/1kQJoi6Vqe-xEsbLokmePAlAWU5kNl99T
    https://drive.google.com/file/d/1DUou8tJ1UR54lQtI2Zzi5smJ3aoUbyXX
    
    iPhone:
    https://drive.google.com/file/d/13TPquf7Z8HbdkCDmQhXnyqwP0VQGxwTp
    https://drive.google.com/file/d/1UjLwwkEk4RKnkDfX-7BSLmfqo6Lj1Ewd
    https://drive.google.com/file/d/1aykwVlgT3npD3DQkMCPWJm3hYiMeJfLx
    
    Bug: 1166148
    Change-Id: Ica58a513ee216d79f4994d155a4941ea5fdbe3d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627401
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846086}

--- ios/chrome/browser/ui/authentication/authentication_ui_util.h
+++ ios/chrome/browser/ui/authentication/authentication_ui_util.h
@@ -8,9 +8,43 @@
 #import <UIKit/UIKit.h>
 
 #include "base/ios/block_types.h"
+#include "base/strings/string16.h"
 
 @class AlertCoordinator;
+@class ActionSheetCoordinator;
 class Browser;
+
+// Sign-out result, related to SignoutActionSheetCoordinator().
+typedef NS_ENUM(NSUInteger, SignoutActionSheetCoordinatorResult) {
+  // The user canceled the sign-out confirmation dialog.
+  SignoutActionSheetCoordinatorResultCanceled,
+  // The user chose to sign-out and clear their data from the device.
+  SignoutActionSheetCoordinatorResultClearFromDevice,
+  // The user chose to sign-out and keep their data on the device.
+  SignoutActionSheetCoordinatorResultKeepOnDevice,
+};
+
+// Sign-out completion block.
+using SignoutActionSheetCoordinatorCompletion =
+    void (^)(SignoutActionSheetCoordinatorResult result);
+
+// Returns the hosted domain for the primary account.
+base::string16 HostedDomainForPrimaryAccount(Browser* browser);
+
+// Returns ActionSheetCoordinator to ask the sign-out confirmation from the
+// user. The alert sheet dialog choices are based if the primary account is
+// managed account or not, and if the user turned on sync or not.
+// The caller is in charge to start the coordinator.
+// The caller is in charge to sign-out the user and wipe the data from the
+// device according to |signout_completion| value.
+// |view_controller| view controller to present the action sheet.
+// |view| to position the action sheet for iPad.
+// |signout_completion| invoked when the user confirms or cancels sign-out.
+ActionSheetCoordinator* SignoutActionSheetCoordinator(
+    UIViewController* view_controller,
+    Browser* browser,
+    UIView* view,
+    SignoutActionSheetCoordinatorCompletion signout_completion);
 
 // Returns the sign in alert coordinator for |error|. |dismissAction| is called
 // when the dialog is dismissed (the user taps on the Ok button) or cancelled
--- ios/chrome/browser/ui/authentication/authentication_ui_util.mm
+++ ios/chrome/browser/ui/authentication/authentication_ui_util.mm
@@ -4,16 +4,160 @@
 
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
 
-#include "base/check.h"
-#include "base/format_macros.h"
-#include "components/strings/grit/components_strings.h"
-#include "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
-#include "ios/chrome/grit/ios_strings.h"
-#include "ui/base/l10n/l10n_util.h"
+#import "base/check.h"
+#import "base/format_macros.h"
+#import "base/strings/utf_string_conversions.h"
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/signin/authentication_service.h"
+#import "ios/chrome/browser/signin/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/identity_manager_factory.h"
+#import "ios/chrome/browser/sync/sync_setup_service.h"
+#import "ios/chrome/browser/sync/sync_setup_service_factory.h"
+#import "ios/chrome/browser/ui/alert_coordinator/action_sheet_coordinator.h"
+#import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
+#import "ios/chrome/grit/ios_chromium_strings.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+// Enum to described all 4 cases for a user being signed-in. This enum is used
+// internaly by SignoutActionSheetCoordinator().
+typedef NS_ENUM(NSUInteger, SignedInUserState) {
+  // Sign-in with a managed account and sync is turned on.
+  SignedInUserStateWithManagedAccountAndSyncing,
+  // Sign-in with a managed account and sync is turned off.
+  SignedInUserStateWithManagedAccountAndNotSyncing,
+  // Sign-in with a regular account and sync is turned on.
+  SignedInUserStateWithNonManagedAccountAndSyncing,
+  // Sign-in with a regular account and sync is turned off.
+  SignedInUserStateWithNoneManagedAccountAndNotSyncing
+};
+
+base::string16 HostedDomainForPrimaryAccount(Browser* browser) {
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForBrowserState(browser->GetBrowserState());
+  base::Optional<AccountInfo> account_info =
+      identity_manager->FindExtendedAccountInfoForAccountWithRefreshToken(
+          identity_manager->GetPrimaryAccountInfo());
+  std::string hosted_domain = account_info.has_value()
+                                  ? account_info.value().hosted_domain
+                                  : std::string();
+  return base::UTF8ToUTF16(hosted_domain);
+}
+
+ActionSheetCoordinator* SignoutActionSheetCoordinator(
+    UIViewController* view_controller,
+    Browser* browser,
+    UIView* view,
+    SignoutActionSheetCoordinatorCompletion signout_completion) {
+  DCHECK(signout_completion);
+  AuthenticationService* authentication_service =
+      AuthenticationServiceFactory::GetForBrowserState(
+          browser->GetBrowserState());
+  NSString* title = nil;
+  NSString* message = nil;
+  SyncSetupService* sync_setup_service =
+      SyncSetupServiceFactory::GetForBrowserState(browser->GetBrowserState());
+  BOOL sync_enabled = sync_setup_service->IsFirstSetupComplete();
+  SignedInUserState signed_in_user_state;
+  if (authentication_service->IsAuthenticatedIdentityManaged()) {
+    signed_in_user_state =
+        sync_enabled ? SignedInUserStateWithManagedAccountAndSyncing
+                     : SignedInUserStateWithManagedAccountAndNotSyncing;
+  } else {
+    signed_in_user_state =
+        sync_enabled ? SignedInUserStateWithNonManagedAccountAndSyncing
+                     : SignedInUserStateWithNoneManagedAccountAndNotSyncing;
+  }
+  switch (signed_in_user_state) {
+    case SignedInUserStateWithManagedAccountAndSyncing:
+    case SignedInUserStateWithManagedAccountAndNotSyncing: {
+      title = l10n_util::GetNSString(
+          IDS_IOS_SIGNOUT_DIALOG_TITLE_WITH_MANAGED_ACCOUNT);
+      base::string16 hosted_domain = HostedDomainForPrimaryAccount(browser);
+      message = l10n_util::GetNSStringF(
+          IDS_IOS_SIGNOUT_DIALOG_MESSAGE_WITH_MANAGED_ACCOUNT, hosted_domain);
+      break;
+    }
+    case SignedInUserStateWithNonManagedAccountAndSyncing: {
+      title = l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_TITLE_WITH_SYNC);
+      message =
+          l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_MESSAGE_WITH_SYNC);
+      break;
+    }
+    case SignedInUserStateWithNoneManagedAccountAndNotSyncing: {
+      title = l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_TITLE_WITHOUT_SYNC);
+      break;
+    }
+  }
+  ActionSheetCoordinator* alertCoordinator =
+      [[ActionSheetCoordinator alloc] initWithBaseViewController:view_controller
+                                                         browser:browser
+                                                           title:title
+                                                         message:message
+                                                            rect:view.frame
+                                                            view:view];
+  switch (signed_in_user_state) {
+    case SignedInUserStateWithManagedAccountAndSyncing:
+    case SignedInUserStateWithManagedAccountAndNotSyncing: {
+      NSString* const clear_from_this_device =
+          l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_CLEAR_DATA_BUTTON);
+      [alertCoordinator
+          addItemWithTitle:clear_from_this_device
+                    action:^{
+                      signout_completion(
+                          SignoutActionSheetCoordinatorResultClearFromDevice);
+                    }
+                     style:UIAlertActionStyleDestructive];
+      break;
+    }
+    case SignedInUserStateWithNonManagedAccountAndSyncing: {
+      NSString* const clear_from_this_device =
+          l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_CLEAR_DATA_BUTTON);
+      NSString* const keep_on_this_device =
+          l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_KEEP_DATA_BUTTON);
+      [alertCoordinator
+          addItemWithTitle:clear_from_this_device
+                    action:^{
+                      signout_completion(
+                          SignoutActionSheetCoordinatorResultClearFromDevice);
+                    }
+                     style:UIAlertActionStyleDestructive];
+      [alertCoordinator
+          addItemWithTitle:keep_on_this_device
+                    action:^{
+                      signout_completion(
+                          SignoutActionSheetCoordinatorResultKeepOnDevice);
+                    }
+                     style:UIAlertActionStyleDefault];
+      break;
+    }
+    case SignedInUserStateWithNoneManagedAccountAndNotSyncing: {
+      NSString* const sign_out =
+          l10n_util::GetNSString(IDS_IOS_SIGNOUT_DIALOG_SIGN_OUT_BUTTON);
+      [alertCoordinator
+          addItemWithTitle:sign_out
+                    action:^{
+                      signout_completion(
+                          SignoutActionSheetCoordinatorResultKeepOnDevice);
+                    }
+                     style:UIAlertActionStyleDestructive];
+      break;
+    }
+  }
+  [alertCoordinator
+      addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
+                action:^{
+                  signout_completion(
+                      SignoutActionSheetCoordinatorResultCanceled);
+                }
+                 style:UIAlertActionStyleCancel];
+  return alertCoordinator;
+}
 
 AlertCoordinator* ErrorCoordinator(NSError* error,
                                    ProceduralBlock dismissAction,
