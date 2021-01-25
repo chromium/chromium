e10a3083ad50e39f3ee4f4ae6026d4568e6a1f7d
Dana Fried <dfried@chromium.org>
2021-01-25 21:07:19 +0000

    [Lacros, Experimental] Link up system with touch input controller
    Hopefully, this will connect the touch UI controller with the system-
    specific tablet mode.
    
    Change-Id: I7c51b2bc9de307768c55c4c9b8f18e6ee9221745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642736
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846878}

--- chromeos/ui/base/tablet_state.cc
+++ chromeos/ui/base/tablet_state.cc
@@ -5,7 +5,12 @@
 #include "chromeos/ui/base/tablet_state.h"
 
 #include "base/check_op.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/display/screen.h"
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "ui/base/pointer/touch_ui_controller.h"
+#endif
 
 namespace chromeos {
 
@@ -36,6 +41,22 @@ bool TabletState::InTabletMode() const {
 
 void TabletState::OnDisplayTabletStateChanged(display::TabletState state) {
   state_ = state;
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // TouchUIController is used by Chrome and other apps to determine whether
+  // the device is in either a primarily touch-input or primarily keyboard
+  // input mode, and to show different UI depending on which mode it's in.
+  //
+  // On ChromeOS this was previously hooked up through Ash code. On Lacros,
+  // however, TabletState is one of the few classes which receives the relevant
+  // events *and* can communicate safely with TouchUiController. The ozone/
+  // wayland code can't see ui/base, and TouchController can't listen for
+  // events on display::Screen because of order of instatiation (there is no
+  // Screen object when TouchUiController is created).
+  //
+  // TODO(crbug.com/1170013): consolidate all of the tablet/touch state logic
+  // into a single place on all platforms (likely display::Screen).
+  ui::TouchUiController::Get()->OnTabletModeToggled(InTabletMode());
+#endif
 }
 
 }  // namespace chromeos
