421027cce9541bddd3ef69c77879f7112cdb012d
Nicolás Peña Moreno <npm@chromium.org>
2021-01-25 20:37:41 +0000

    [M89][Abandonment] Flush metrics on FCP
    This CL adds a method that gets called when a frame reaches FCP. This
    allows us to aggressively flush the metrics from the renderer process
    to the browser process, which in turn means that our abandonment rates
    would be more accurate as a result, since the abandonment metric relies
    on whether FCP has been reached.
    
    (cherry picked from commit 636e47da3177a9b830cd8413bb8584d4f35533ce)
    
    Bug: 1048691
    Change-Id: I7d0a4e2886a41f5eb007c0eed3b19e59df4917f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642993
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#846376}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647495
    Auto-Submit: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4389@{#219}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- components/page_load_metrics/renderer/page_timing_metrics_sender.cc
+++ components/page_load_metrics/renderer/page_timing_metrics_sender.cc
@@ -280,9 +280,22 @@ void PageTimingMetricsSender::Update(
     return;
   }
 
+  // We want to force sending the metrics immediately when FCP is reached
+  // instead of using a timer. SendLatest() will force sending now since we
+  // already called EnsureSendTimer().
+  bool force_send_metrics =
+      (!last_timing_->paint_timing ||
+       !last_timing_->paint_timing->first_contentful_paint.has_value()) &&
+      timing->paint_timing &&
+      timing->paint_timing->first_contentful_paint.has_value();
+
   last_timing_ = std::move(timing);
   metadata_recorder_.UpdateMetadata(monotonic_timing);
   EnsureSendTimer();
+
+  if (force_send_metrics) {
+    SendLatest();
+  }
 }
 
 void PageTimingMetricsSender::SendLatest() {
