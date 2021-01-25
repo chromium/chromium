3d064ca19f49826b7088de01111a16d405b0d5b7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-25 22:01:06 +0000

    Import wpt@0d740156e3f84e4bdf8e38653b1ba39cd0655c0d
    Using wpt-import in Chromium 3597ca3e21bbb4eaa99e2533d62aded7780158b4.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=smcgruer@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I7b6afdbeaf5f6ff9ee346efe1f41066458902cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647845
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#846907}

--- third_party/blink/web_tests/external/wpt/resources/testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/testharness.js
@@ -15,7 +15,6 @@ policies and contribution forms [3].
 
 (function (global_scope)
 {
-    var debug = false;
     // default timeout is 10 seconds, test can override if needed
     var settings = {
         output:true,
@@ -24,7 +23,8 @@ policies and contribution forms [3].
             "long":60000
         },
         test_timeout:null,
-        message_events: ["start", "test_state", "result", "completion"]
+        message_events: ["start", "test_state", "result", "completion"],
+        debug: false,
     };
 
     var xhtml_ns = "http://www.w3.org/1999/xhtml";
@@ -132,11 +132,7 @@ policies and contribution forms [3].
                         if (has_selector) {
                             try {
                                 w[selector].apply(undefined, callback_args);
-                            } catch (e) {
-                                if (debug) {
-                                    throw e;
-                                }
-                            }
+                            } catch (e) {}
                         }
                     }
                     if (supports_post_message(w) && w !== self) {
@@ -1190,6 +1186,9 @@ policies and contribution forms [3].
             let status = Test.statuses.TIMEOUT;
             let stack = null;
             try {
+                if (settings.debug) {
+                    console.debug("ASSERT", name, tests.current_test.name, args);
+                }
                 if (settings.output) {
                     tests.set_assert(name, ...args);
                 }
@@ -2064,6 +2063,9 @@ policies and contribution forms [3].
             return;
         }
 
+        if (settings.debug && this.phase !== this.phases.STARTED) {
+            console.log("TEST START", this.name);
+        }
         this.phase = this.phases.STARTED;
         //If we don't get a result before the harness times out that will be a test timeout
         this.set_status(this.TIMEOUT, "Test timed out");
@@ -2080,6 +2082,10 @@ policies and contribution forms [3].
 
         if (arguments.length === 1) {
             this_obj = this;
+        }
+
+        if (settings.debug) {
+            console.debug("TEST STEP", this.name);
         }
 
         try {
@@ -2312,6 +2318,12 @@ policies and contribution forms [3].
 
         if (global_scope.clearTimeout) {
             clearTimeout(this.timeout_id);
+        }
+
+        if (settings.debug) {
+            console.log("TEST DONE",
+                        this.status,
+                        this.name,)
         }
 
         this.cleanup();
@@ -2629,7 +2641,7 @@ policies and contribution forms [3].
             var record = new AssertRecord();
             record.assert_name = assert.assert_name;
             record.args = assert.args;
-            record.test = this.tests[assert.test.index];
+            record.test = assert.test != null ? this.tests[assert.test.index] : null;
             record.status = assert.status;
             record.stack = assert.stack;
             tests.asserts_run.push(record);
