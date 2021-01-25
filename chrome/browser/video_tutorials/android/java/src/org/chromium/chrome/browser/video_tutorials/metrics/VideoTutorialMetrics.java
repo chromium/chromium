578471abd40208031d6869c70220749ccee1c62c
Shakti Sahu <shaktisahu@chromium.org>
2021-01-25 22:13:53 +0000

    Video Tutorials : Added missing metrics for IPH cards
    This CL adds
    - Impression metrics for video tutorials cards on NTP.
    - Metrics for tutorials played from video list page.
    - Language picker metrics such as language selected, and user actions
    on close, back, and watch buttons.
    - Plugged in individual tutorial IPHs to the feature engagement UMA
    histogram suffixes and actions as recommended in
    components/feature_engagement/README.md, original CL : crrev/cl/2367438
    - Added missing enums for share from crrev/c/2437785
    
    Bug: 1168654
    Change-Id: I6c2d432da26e45c67fbf863c823d76cad419277d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640898
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846917}

--- chrome/browser/video_tutorials/android/java/src/org/chromium/chrome/browser/video_tutorials/metrics/VideoTutorialMetrics.java
+++ chrome/browser/video_tutorials/android/java/src/org/chromium/chrome/browser/video_tutorials/metrics/VideoTutorialMetrics.java
@@ -16,8 +16,8 @@ import java.lang.annotation.RetentionPolicy;
  * Responsible for collecting metrics associated with video tutorials.
  */
 public class VideoTutorialMetrics {
-    // Please treat this list as append only and keep it in sync with
-    // VideoTutorials.WatchState in enums.xml.
+    // These values are persisted to logs. Entries should not be renumbered and numeric values
+    // should never be reused. Must be kept in sync with VideoTutorials.WatchState in enums.xml.
     @IntDef({WatchState.STARTED, WatchState.COMPLETED, WatchState.PAUSED, WatchState.RESUMED,
             WatchState.WATCHED})
     @Retention(RetentionPolicy.SOURCE)
@@ -30,11 +30,12 @@ public class VideoTutorialMetrics {
         int NUM_ENTRIES = 5;
     }
 
-    // Please treat this list as append only and keep it in sync with
-    // VideoTutorials.UserAction in enums.xml.
+    // These values are persisted to logs. Entries should not be renumbered and numeric values
+    // should never be reused. Must be kept in sync with VideoTutorials.UserAction in enums.xml.
     @IntDef({UserAction.CHANGE_LANGUAGE, UserAction.WATCH_NEXT_VIDEO, UserAction.TRY_NOW,
             UserAction.SHARE, UserAction.CLOSE, UserAction.BACK_PRESS_WHEN_SHOWING_VIDEO_PLAYER,
-            UserAction.OPEN_SHARED_VIDEO, UserAction.INVALID_SHARE_URL})
+            UserAction.OPEN_SHARED_VIDEO, UserAction.INVALID_SHARE_URL, UserAction.IPH_NTP_SHOWN,
+            UserAction.IPH_NTP_CLICKED, UserAction.IPH_NTP_DISMISSED, UserAction.PLAYED_FROM_RECAP})
     @Retention(RetentionPolicy.SOURCE)
     public @interface UserAction {
         int CHANGE_LANGUAGE = 0;
@@ -45,7 +46,24 @@ public class VideoTutorialMetrics {
         int BACK_PRESS_WHEN_SHOWING_VIDEO_PLAYER = 5;
         int OPEN_SHARED_VIDEO = 6;
         int INVALID_SHARE_URL = 7;
-        int NUM_ENTRIES = 8;
+        int IPH_NTP_SHOWN = 8;
+        int IPH_NTP_CLICKED = 9;
+        int IPH_NTP_DISMISSED = 10;
+        int PLAYED_FROM_RECAP = 11;
+        int NUM_ENTRIES = 12;
+    }
+
+    // These values are persisted to logs. Entries should not be renumbered and numeric values
+    // should never be reused. Must be kept in sync with VideoTutorials.LanguagePickerAction in
+    // enums.xml.
+    @IntDef({LanguagePickerAction.BACK_PRESS, LanguagePickerAction.CLOSE,
+            LanguagePickerAction.WATCH})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface LanguagePickerAction {
+        int BACK_PRESS = 0;
+        int CLOSE = 1;
+        int WATCH = 2;
+        int NUM_ENTRIES = 3;
     }
 
     /** Called to record various user actions on the video player. */
@@ -66,6 +84,22 @@ public class VideoTutorialMetrics {
         RecordHistogram.recordEnumeratedHistogram(
                 "VideoTutorials." + histogramSuffix + ".Player.Progress", state,
                 WatchState.NUM_ENTRIES);
+    }
+
+    /** Called to record various user actions on the language picker. */
+    public static void recordLanguagePickerAction(@LanguagePickerAction int action) {
+        RecordHistogram.recordEnumeratedHistogram(
+                "VideoTutorials.LanguagePicker.Action", action, LanguagePickerAction.NUM_ENTRIES);
+    }
+
+    /**
+     * Called when the user selects a language from the list of available languages. The position of
+     * the language in the list will be recorded.
+     * @param position The position of the language in the list.
+     */
+    public static void recordLanguageSelected(int position) {
+        RecordHistogram.recordLinearCountHistogram(
+                "VideoTutorials.LanguagePicker.LanguageSelected", position, 0, 20, 21);
     }
 
     private static String histogramSuffixFromFeatureType(@FeatureType int feature) {
