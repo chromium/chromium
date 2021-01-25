ce7a6d9be69990c54d041e024849ec391d20d1b3
Curt Clemens <cclem@google.com>
2021-01-25 19:23:24 +0000

    [Nearby] Report stale devices as lost in BluetoothClassicMedium
    Once a bluetooth device is paired, the bluetooth stack doesn't generate
    "DeviceRemoved" events, causing high visibility share targets to never
    be removed from the share target list after the first share. Instead of
    relying on "DeviceRemoved", this CL tracks the last time the device was
    added/changed and automatically removes devices that haven't been
    added/changed for a while using a timer.
    
    Fixed: 1166716
    Change-Id: Iac7709a47d148eb89812d17ce4f3d1c577d57b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638918
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846824}

--- chrome/services/sharing/nearby/platform/bluetooth_classic_medium.h
+++ chrome/services/sharing/nearby/platform/bluetooth_classic_medium.h
@@ -9,6 +9,8 @@
 #include <string>
 
 #include "base/optional.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chrome/services/sharing/nearby/platform/bluetooth_device.h"
 #include "device/bluetooth/public/mojom/adapter.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -55,6 +57,8 @@ class BluetoothClassicMedium : public api::BluetoothClassicMedium,
   void DeviceChanged(bluetooth::mojom::DeviceInfoPtr device) override;
   void DeviceRemoved(bluetooth::mojom::DeviceInfoPtr device) override;
 
+  void RemoveStaleBluetoothDevices();
+
   mojo::SharedRemote<bluetooth::mojom::Adapter> adapter_;
 
   // |adapter_observer_| is only set and bound during active discovery so that
@@ -65,8 +69,13 @@ class BluetoothClassicMedium : public api::BluetoothClassicMedium,
   base::Optional<DiscoveryCallback> discovery_callback_;
   mojo::Remote<bluetooth::mojom::DiscoverySession> discovery_session_;
 
+  // This is a mapping of MAC addresses to discovered Bluetooth devices.
   std::map<std::string, chrome::BluetoothDevice>
       discovered_bluetooth_devices_map_;
+
+  // Used to periodically remove devices that haven't produced any Bluetooth
+  // events for N minutes.
+  base::RepeatingTimer stale_bluetooth_device_timer_;
 };
 
 }  // namespace chrome
