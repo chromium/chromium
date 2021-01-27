80682e864faaa3d9bfda968ff1d44a0fde9a1198
Elly Fong-Jones <ellyjones@chromium.org>
2021-01-27 15:50:32 +0000

    cocoa: tolerate TSMProcessRawKeyCode during NSEvent.characters
    There is some speculation on the linked bug about exactly what can cause
    this to happen, but it has been happening at a low level for a long time
    and become more common as of macOS 11, so we should start allowing for
    this exception to happen.
    
    Bug: 1150495
    Change-Id: I7b7a1938b91cab2463a98ab60dff616473205f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649329
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847631}

--- chrome/browser/chrome_browser_application_mac.mm
+++ chrome/browser/chrome_browser_application_mac.mm
@@ -60,14 +60,24 @@ std::string DescriptionForNSEvent(NSEvent* event) {
     case NSEventTypeKeyDown:
     case NSEventTypeKeyUp: {
       // Some NSEvents return a string with NUL in event.characters, see
-      // <https://crbug.com/826908>.
-      std::string characters = base::SysNSStringToUTF8([event.characters
-          stringByReplacingOccurrencesOfString:@"\0"
-                                    withString:@"\\x00"]);
-      std::string unmodified_characters =
-          base::SysNSStringToUTF8([event.charactersIgnoringModifiers
-              stringByReplacingOccurrencesOfString:@"\0"
-                                        withString:@"\\x00"]);
+      // <https://crbug.com/826908>. To make matters worse, in rare cases,
+      // NSEvent.characters or NSEvent.charactersIgnoringModifiers can throw an
+      // NSException complaining that "TSMProcessRawKeyCode failed". Since we're
+      // trying to gather a crash key here, if that exception happens, just
+      // remark that it happened and continue rather than crashing the browser.
+      std::string characters, unmodified_characters;
+      @try {
+        characters = base::SysNSStringToUTF8([event.characters
+            stringByReplacingOccurrencesOfString:@"\0"
+                                      withString:@"\\x00"]);
+        unmodified_characters =
+            base::SysNSStringToUTF8([event.charactersIgnoringModifiers
+                stringByReplacingOccurrencesOfString:@"\0"
+                                          withString:@"\\x00"]);
+      } @catch (id exception) {
+        characters = "(exception)";
+        unmodified_characters = "(exception)";
+      }
       desc += base::StringPrintf(
           " keyCode=0x%d ARepeat=%d characters='%s' unmodifiedCharacters='%s'",
           event.keyCode, event.ARepeat, characters.c_str(),
