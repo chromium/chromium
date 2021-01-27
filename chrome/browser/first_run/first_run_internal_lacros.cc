717807ec86ce1b0d565b766c895849722fc8d251
James Cook <jamescook@chromium.org>
2021-01-27 17:39:49 +0000

    lacros: Clean up unused first_run metrics code
    All lacros browser clients going forward target chromeos M89 or later.
    We don't need the legacy first run metrics support any more. Lacros now
    shares metrics consent with the OS, so it doesn't need to do anything
    special on first run.
    
    Bug: 1155751
    Change-Id: I0c85986031f3e0ded0dd371c798e2ca4c48ceaaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650636
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847677}

--- chrome/browser/first_run/first_run_internal_lacros.cc
+++ chrome/browser/first_run/first_run_internal_lacros.cc
@@ -4,29 +4,11 @@
 
 #include "chrome/browser/first_run/first_run_internal.h"
 
-#include "chrome/browser/metrics/metrics_reporting_state.h"
-#include "chromeos/crosapi/mojom/crosapi.mojom.h"
-#include "chromeos/lacros/lacros_chrome_service_impl.h"
-
 namespace first_run {
 namespace internal {
 
 void DoPostImportPlatformSpecificTasks(Profile* profile) {
-  auto* lacros_service = chromeos::LacrosChromeServiceImpl::Get();
-
-  // The code below is only needed for legacy ash where the metrics reporting
-  // API is not available.
-  // TODO(https://crbug.com/1155751): Remove this check and the code below when
-  // all lacros clients are on OS 89 or later.
-  if (lacros_service->IsMetricsReportingAvailable())
-    return;
-
-  // Lacros skips the first run dialog because Chrome is the default browser on
-  // Chrome OS and metrics consent is chosen during the Chrome OS out of box
-  // setup experience. Lacros inherits first-run metrics consent from ash over
-  // mojo. After first-run lacros handles metrics consent via settings.
-  ChangeMetricsReportingState(
-      lacros_service->init_params()->ash_metrics_enabled);
+  // Nothing to do.
 }
 
 bool ShowPostInstallEULAIfNeeded(installer::InitialPreferences* install_prefs) {
