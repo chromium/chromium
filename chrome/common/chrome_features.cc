74c939e6b6f77cb679f746b706422e0bacf11924
Maggie Cai <mxcai@chromium.org>
2021-01-27 02:50:45 +0000

    [IntentHandling] Enable intent picker PWA persistence flag by default.
    This CL enables the intent picker PWA persistence flag by default. This
    allows user to store the user selection for PWA in the intent picker.
    Also the intent picker will auto pop up for PWA, and for preferred PWA
    the navigation will directly open in PWA.
    
    BUG=853604, 1160135
    
    Change-Id: I283e12428bf63af32f9baf95496dc808d4ad542f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501101
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847462}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -551,7 +551,12 @@ const base::Feature kIntentHandlingSharing{"IntentHandlingSharing",
                                            base::FEATURE_ENABLED_BY_DEFAULT};
 // Allow user to have preference for PWA in the intent picker.
 const base::Feature kIntentPickerPWAPersistence{
-    "IntentPickerPWAPersistence", base::FEATURE_DISABLED_BY_DEFAULT};
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  "IntentPickerPWAPersistence", base::FEATURE_ENABLED_BY_DEFAULT
+#else
+  "IntentPickerPWAPersistence", base::FEATURE_DISABLED_BY_DEFAULT
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+};
 #endif  // !defined(OS_ANDROID)
 
 // If enabled, CloudPolicyInvalidator and RemoteCommandInvalidator instances
