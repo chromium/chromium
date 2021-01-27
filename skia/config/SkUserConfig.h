5000e9c84e014ca5c9f1299da8be0cad6ff2583c
Mike Reed <reed@google.com>
2021-01-27 22:45:15 +0000

    Add flag to stage skia convexity change
    Related skia cls
    https://skia-review.googlesource.com/c/skia/+/360597
    https://skia-review.googlesource.com/c/skia/+/359916
    
    Bug: skia:11227
    Change-Id: I50965e1723402ee0fef005b3e02f0a65337c4212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653514
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#847832}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -238,6 +238,10 @@ SK_API void SkDebugf_FileLine(const char* file,
 // Staging for lowp::bilerp_clamp_8888, and for planned misc. others.
 #define SK_DISABLE_LOWP_BILERP_CLAMP_CLAMP_STAGE
 
+#ifndef SK_SUPPORT_LEGACY_CONVEXITY_DIRECTION_CHANGE
+#define SK_SUPPORT_LEGACY_CONVEXITY_DIRECTION_CHANGE
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
