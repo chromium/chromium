602f3064e17a73cb8e57a2a7bc351ba9bb1ff687
David Jean <djean@chromium.org>
2021-01-28 09:17:21 +0000

    [ios] Add multiwindow passphrase eg test
    This should fulfil TC 3/10 on the list of manual test cases.
    
    Bug: 1169687
    Change-Id: I3a528343ccdba3f80552bf0dfc9d8f61968162db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649221
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848032}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
@@ -18,11 +18,17 @@
 
 using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::NavigationBarCancelButton;
+using chrome_test_util::MatchInWindowWithNumber;
 
 @interface SyncEncryptionPassphraseTestCase : ChromeTestCase
 @end
 
 @implementation SyncEncryptionPassphraseTestCase
+
+- (void)tearDown {
+  [ChromeEarlGrey closeAllExtraWindows];
+  [super tearDown];
+}
 
 // Tests to open the sync passphrase view, and to close it.
 - (void)testShowSyncPassphraseAndDismiss {
@@ -38,6 +44,38 @@ using chrome_test_util::NavigationBarCancelButton;
       performAction:grey_tap()];
   // Wait until the settings is fully removed.
   [ChromeEarlGreyUI waitForAppToIdle];
+}
+
+// Tests opening the sync passphrase view, then a new window and check that
+// enter passphrase message appears.
+- (void)testShowSyncPassphraseInNewWindowAndDismiss {
+  if (![ChromeEarlGrey areMultipleWindowsSupported])
+    EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
+
+  [ChromeEarlGrey addBookmarkWithSyncPassphrase:@"hello"];
+  // Signin.
+  FakeChromeIdentity* fakeIdentity = [SigninEarlGrey fakeIdentity1];
+  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+
+  [ChromeEarlGrey openNewWindow];
+  [ChromeEarlGrey waitForForegroundWindowCount:2];
+
+  [[EarlGrey
+      selectElementWithMatcher:MatchInWindowWithNumber(
+                                   1, ButtonWithAccessibilityLabelId(
+                                          IDS_IOS_SYNC_ENTER_PASSPHRASE))]
+      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:MatchInWindowWithNumber(
+                                          1, NavigationBarCancelButton())]
+      performAction:grey_tap()];
+  // Wait until the settings is fully removed.
+  [ChromeEarlGreyUI waitForAppToIdle];
+
+  [ChromeEarlGrey closeWindowWithNumber:1];
+  [ChromeEarlGrey waitForForegroundWindowCount:1];
+
+  [SigninEarlGrey signOut];
+  [SigninEarlGrey verifySignedOut];
 }
 
 @end
