19d67635d83f0cbd5f17164874fd176228ceffa7
Reid Kleckner <rnk@google.com>
2021-01-28 17:04:20 +0000

    Make Foo complete when using unique_ptr<Foo> as a parameter, iOS edition
    The next version of libc++ will require Foo to be complete in this
    sample code:
    
      #include <memory>
      class Foo; // Forward declaration only
      void takeAFoo(std::unique_ptr<Foo> foo) {
        // No usage of foo.
      }
    
    This change adjusts ifdefs and adds includes as necessary to fix the
    build with an updated libc++.
    
    Previously, Foo was not required to be complete in this code due to C++
    implementation details. On non-Windows platforms, the call site of
    takeAFoo would be responsible for destroying the unique_ptr after the
    call returns.
    
    The next libc++ roll will apply the trivial_abi attribute to unique_ptr,
    which means it can be passed in registers. This makes it inconvenient
    for the caller to destroy the arguments, so the responsibility for
    destroying the object is moved into the callee. Calling the unique_ptr
    destructor calls ~Foo, which requires it to be complete.
    
    Bug: 1166740
    Change-Id: I580d570eb356b437069a7cd5c5bad264e7d67864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651609
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848140}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -8,6 +8,7 @@
 
 #include "components/metrics/metrics_provider.h"
 #import "ios/public/provider/chrome/browser/mailto/mailto_handler_provider.h"
+#import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #import "ios/public/provider/chrome/browser/text_zoom_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
