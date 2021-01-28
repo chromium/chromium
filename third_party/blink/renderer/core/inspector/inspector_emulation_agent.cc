c20f69d83173d2befc48f76f15982a1bf0634754
Maks Orlovich <morlovich@chromium.org>
2021-01-28 20:07:30 +0000

    Devtools UA-CH emulation: permit omitting brands + version to use
    ... and just let the target fill in with default. That should make it easier for devtools frontend to get proper version for full-version to be emulation when UA string is frozen; doing likewise for brands will permit removing the code to from the frontend that tries to match what Chrome does, including the GREASEing.
    
    Requires as prereq
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2653385
    to avoid trouble.
    
    Bug: 1163935
    Change-Id: I1645dd1247d45af0ec31039935a18af40f26f36a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653686
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848223}

--- third_party/blink/renderer/core/inspector/inspector_emulation_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_emulation_agent.cc
@@ -589,6 +589,9 @@ Response InspectorEmulationAgent::setUserAgentOverride(
   }
 
   if (ua_metadata_override.isJust()) {
+    blink::UserAgentMetadata default_ua_metadata =
+        Platform::Current()->UserAgentMetadata();
+
     if (user_agent.IsEmpty()) {
       ua_metadata_override_ = base::nullopt;
       serialized_ua_metadata_override_.Set(std::vector<uint8_t>());
@@ -598,15 +601,25 @@ Response InspectorEmulationAgent::setUserAgentOverride(
     std::unique_ptr<protocol::Emulation::UserAgentMetadata> ua_metadata =
         ua_metadata_override.takeJust();
     ua_metadata_override_.emplace();
-    if (ua_metadata->getBrands()) {
-      for (const auto& bv : *ua_metadata->getBrands()) {
+    if (ua_metadata->hasBrands()) {
+      for (const auto& bv : *ua_metadata->getBrands(nullptr)) {
         blink::UserAgentBrandVersion out_bv;
         out_bv.brand = bv->getBrand().Ascii();
         out_bv.major_version = bv->getVersion().Ascii();
         ua_metadata_override_->brand_version_list.push_back(std::move(out_bv));
       }
+    } else {
+      ua_metadata_override_->brand_version_list =
+          std::move(default_ua_metadata.brand_version_list);
     }
-    ua_metadata_override_->full_version = ua_metadata->getFullVersion().Ascii();
+
+    if (ua_metadata->hasFullVersion()) {
+      ua_metadata_override_->full_version =
+          ua_metadata->getFullVersion("").Ascii();
+    } else {
+      ua_metadata_override_->full_version =
+          std::move(default_ua_metadata.full_version);
+    }
     ua_metadata_override_->platform = ua_metadata->getPlatform().Ascii();
     ua_metadata_override_->platform_version =
         ua_metadata->getPlatformVersion().Ascii();
