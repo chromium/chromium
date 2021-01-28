1f15892d0becf58302be3eeeba72d81748e32d7a
Etienne Pierre-Doray <etiennep@chromium.org>
2021-01-28 20:34:12 +0000

    [battery]: Implement battery level provider on windows.
    This CL implements BatteryLevelProviderWin.
    The logic follows
    https://docs.microsoft.com/en-us/windows/win32/power/enumerating-battery-devices
    Follow-up:
    - Adapt BatteryState to better express failure cases
    - Cache battery tag (https://docs.microsoft.com/en-us/windows/win32/power/battery-information).
    
    
    Bug: 1153193
    Change-Id: I46bdc3ee4b7dfc4fa6614e4508f701a14cc2d37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623192
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Oliver Li <olivierli@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848234}

---
+++ chrome/browser/metrics/power/battery_level_provider_win.cc
@@ -0,0 +1,194 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/metrics/power/battery_level_provider.h"
+
+#define INITGUID
+#include <windows.h>  // Must be in front of other Windows header files.
+
+#include <devguid.h>
+#include <poclass.h>
+#include <setupapi.h>
+#include <winioctl.h>
+
+#include <vector>
+
+#include "base/threading/scoped_blocking_call.h"
+#include "base/win/scoped_devinfo.h"
+#include "base/win/scoped_handle.h"
+
+namespace {
+
+// Returns a handle to the battery interface identified by |interface_data|, or
+// nullopt if the request failed. |devices| is a device information set that
+// contains battery devices information, obtained with ::SetupDiGetClassDevs().
+base::win::ScopedHandle GetBatteryHandle(
+    HDEVINFO devices,
+    SP_DEVICE_INTERFACE_DATA* interface_data) {
+  // Query size required to hold |interface_detail|.
+  DWORD required_size = 0;
+  ::SetupDiGetDeviceInterfaceDetail(devices, interface_data, nullptr, 0,
+                                    &required_size, nullptr);
+  DWORD error = ::GetLastError();
+  if (error != ERROR_INSUFFICIENT_BUFFER)
+    return base::win::ScopedHandle();
+
+  // |interface_detail->DevicePath| is variable size.
+  std::vector<uint8_t> raw_buf(required_size);
+  auto* interface_detail =
+      reinterpret_cast<SP_DEVICE_INTERFACE_DETAIL_DATA*>(raw_buf.data());
+  interface_detail->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+
+  BOOL success = ::SetupDiGetDeviceInterfaceDetail(
+      devices, interface_data, interface_detail, required_size, nullptr,
+      nullptr);
+  if (!success)
+    return base::win::ScopedHandle();
+
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+  base::win::ScopedHandle battery(
+      ::CreateFile(interface_detail->DevicePath, GENERIC_READ | GENERIC_WRITE,
+                   FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING,
+                   FILE_ATTRIBUTE_NORMAL, nullptr));
+  return battery;
+}
+
+// Returns the current tag for |battery| handle, or nullopt if there is no
+// battery at this slot or the request failed. Each battery in a particular slot
+// is assigned a tag, which must be used for all queries for information. For
+// more details, see
+// https://docs.microsoft.com/en-us/windows/win32/power/battery-information
+base::Optional<uint64_t> GetBatteryTag(HANDLE battery) {
+  ULONG battery_tag = 0;
+  ULONG wait = 0;
+  DWORD bytes_returned = 0;
+  BOOL success = ::DeviceIoControl(
+      battery, IOCTL_BATTERY_QUERY_TAG, &wait, sizeof(wait), &battery_tag,
+      sizeof(battery_tag), &bytes_returned, nullptr);
+  if (!success)
+    return base::nullopt;
+  return battery_tag;
+}
+
+// Returns BATTERY_INFORMATION structure containing battery information, given
+// battery handle and tag, or nullopt if the request failed. Battery handle and
+// tag are obtained with GetBatteryHandle() and GetBatteryTag(), respectively.
+base::Optional<BATTERY_INFORMATION> GetBatteryInformation(
+    HANDLE battery,
+    uint64_t battery_tag) {
+  BATTERY_QUERY_INFORMATION query_information = {};
+  query_information.BatteryTag = battery_tag;
+  query_information.InformationLevel = BatteryInformation;
+  BATTERY_INFORMATION battery_information = {};
+  DWORD bytes_returned;
+  BOOL success = ::DeviceIoControl(
+      battery, IOCTL_BATTERY_QUERY_INFORMATION, &query_information,
+      sizeof(query_information), &battery_information,
+      sizeof(battery_information), &bytes_returned, nullptr);
+  if (!success)
+    return base::nullopt;
+  return battery_information;
+}
+
+// Returns BATTERY_STATUS structure containing battery state, given battery
+// handle and tag, or nullopt if the request failed. Battery handle and tag are
+// obtained with GetBatteryHandle() and GetBatteryTag(), respectively.
+base::Optional<BATTERY_STATUS> GetBatteryStatus(HANDLE battery,
+                                                uint64_t battery_tag) {
+  BATTERY_WAIT_STATUS wait_status = {};
+  wait_status.BatteryTag = battery_tag;
+  BATTERY_STATUS battery_status;
+  DWORD bytes_returned;
+  BOOL success = ::DeviceIoControl(
+      battery, IOCTL_BATTERY_QUERY_STATUS, &wait_status, sizeof(wait_status),
+      &battery_status, sizeof(battery_status), &bytes_returned, nullptr);
+  if (!success)
+    return base::nullopt;
+  return battery_status;
+}
+
+}  // namespace
+
+class BatteryLevelProviderWin : public BatteryLevelProvider {
+ public:
+  BatteryLevelProviderWin() = default;
+  ~BatteryLevelProviderWin() override = default;
+
+  base::Optional<BatteryState> GetBatteryState() override;
+};
+
+std::unique_ptr<BatteryLevelProvider> BatteryLevelProvider::Create() {
+  return std::make_unique<BatteryLevelProviderWin>();
+}
+
+base::Optional<BatteryLevelProvider::BatteryState>
+BatteryLevelProviderWin::GetBatteryState() {
+  const base::TimeTicks capture_time = base::TimeTicks::Now();
+
+  // Battery interfaces are enumerated at every sample to detect when a new
+  // interface is added, and avoid holding dangling handles when a battery is
+  // disconnected.
+  base::win::ScopedDevInfo devices(
+      ::SetupDiGetClassDevs(&GUID_DEVICE_BATTERY, nullptr, nullptr,
+                            DIGCF_PRESENT | DIGCF_DEVICEINTERFACE));
+  if (!devices.is_valid())
+    return base::nullopt;
+
+  uint64_t total_max_capacity = 0;
+  uint64_t total_current_capacity = 0;
+  bool external_connected = false;
+
+  // The algorithm to enumerate battery devices is taken from
+  // https://docs.microsoft.com/en-us/windows/win32/power/enumerating-battery-devices
+  // Limit search to 8 batteries max. A system may have several battery slots
+  // and each slot may hold an actual battery.
+  for (int device_index = 0; device_index < 8; ++device_index) {
+    SP_DEVICE_INTERFACE_DATA interface_data = {};
+    interface_data.cbSize = sizeof(interface_data);
+
+    BOOL success =
+        ::SetupDiEnumDeviceInterfaces(devices.get(), 0, &GUID_DEVCLASS_BATTERY,
+                                      device_index, &interface_data);
+    if (!success) {
+      // Exit condition.
+      if (ERROR_NO_MORE_ITEMS == ::GetLastError())
+        break;
+      continue;
+    }
+
+    base::win::ScopedHandle battery =
+        GetBatteryHandle(devices.get(), &interface_data);
+    if (!battery.IsValid())
+      continue;
+
+    base::Optional<uint64_t> battery_tag = GetBatteryTag(battery.Get());
+    if (!battery_tag)
+      continue;
+    auto battery_information =
+        GetBatteryInformation(battery.Get(), *battery_tag);
+    auto battery_status = GetBatteryStatus(battery.Get(), *battery_tag);
+    // If any of the values were not available.
+    if (!battery_information.has_value() || !battery_status.has_value())
+      continue;
+
+    // The state is set as connected if any battery has access to AC power.
+    external_connected = external_connected ||
+                         (battery_status->PowerState & BATTERY_POWER_ON_LINE);
+
+    // Total capacity is averaged across multiple batteries.
+    total_max_capacity += battery_information->FullChargedCapacity;
+    total_current_capacity += battery_status->Capacity;
+  }
+
+  // Avoid invalid division.
+  if (total_max_capacity == 0)
+    return base::nullopt;
+
+  DCHECK_LE(total_current_capacity, total_max_capacity);
+  double charge_level = static_cast<double>(total_current_capacity) /
+                        static_cast<double>(total_max_capacity);
+
+  return BatteryState{charge_level, !external_connected, capture_time};
+}
