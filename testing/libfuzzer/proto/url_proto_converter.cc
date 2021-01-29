479e05674f4158f1b2f97a826bb45fc991fbeaaa
cfredric <cfredric@chromium.org>
2021-01-29 21:46:22 +0000

    Copy Url proto->string conversion into library to promote sharing.
    This enables other fuzzers to use the URL-to-string conversion code.
    
    Change-Id: If25a4cf317773171be159cbfd6927e6ce263eb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653666
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848726}

--- testing/libfuzzer/proto/url_proto_converter.cc
+++ testing/libfuzzer/proto/url_proto_converter.cc
@@ -1,39 +1,18 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string>
 
-#include <iostream>
-
-// Includes copied from url_parse_fuzzer.cc
-#include "base/at_exit.h"
-#include "base/i18n/icu_util.h"
-#include "url/gurl.h"
-
-// Includes *not* copied from url_parse_fuzzer.cc
-// Contains DEFINE_BINARY_PROTO_FUZZER, a macro we use to define our target
-// function.
-#include "third_party/libprotobuf-mutator/src/src/libfuzzer/libfuzzer_macro.h"
-// Header information about the Protocol Buffer Url class.
 #include "testing/libfuzzer/proto/url.pb.h"
 
-// The code using TestCase is copied from url_parse_fuzzer.cc
-struct TestCase {
-  TestCase() {
-    CHECK(base::i18n::InitializeICU());
-  }
-  // used by ICU integration.
-  base::AtExitManager at_exit_manager;
-};
+namespace url_proto {
 
-TestCase* test_case = new TestCase();
+namespace {
 
-// Silence logging from the protobuf library.
-protobuf_mutator::protobuf::LogSilencer log_silencer;
-
-std::string Slash_to_string(int slash) {
+std::string SlashToString(int slash) {
   if (slash == url_proto::Url::NONE)
     return "";
   if (slash == url_proto::Url::FORWARD)
@@ -46,11 +25,9 @@ std::string Slash_to_string(int slash) {
   return "";
 }
 
-// Converts a URL in Protocol Buffer format to a url in string format.
-// Since protobuf is a relatively simple format, fuzzing targets that do not
-// accept protobufs (such as this one) will require code to convert from
-// protobuf to the accepted format (string in this case).
-std::string protobuf_to_string(const url_proto::Url& url) {
+}  // namespace
+
+std::string Convert(const url_proto::Url& url) {
   // Build url_string piece by piece from url and then return it.
   std::string url_string = std::string("");
 
@@ -63,12 +40,12 @@ std::string protobuf_to_string(const url_proto::Url& url) {
   // Just append the slashes without doing validation, since it would be too
   // complex. libFuzzer will hopefully figure out good values.
   for (const int slash : url.slashes())
-    url_string += Slash_to_string(slash);
+    url_string += SlashToString(slash);
 
   // Get host. This is simple since hosts are simply strings according to our
   // definition.
   if (url.has_host()) {
-    // Get userinfo if libFuzzer set it. Ensure that user is seperated
+    // Get userinfo if libFuzzer set it. Ensure that user is separated
     // from the password by ":" (if a password is included) and that userinfo is
     // separated from the host by "@".
     if (url.has_userinfo()) {
@@ -91,7 +68,7 @@ std::string protobuf_to_string(const url_proto::Url& url) {
   // Append the path segments to the url, with each segment separated by
   // the path_separator.
   bool first_segment = true;
-  std::string path_separator = Slash_to_string(url.path_separator());
+  std::string path_separator = SlashToString(url.path_separator());
   for (const std::string& path_segment : url.path()) {
     // There does not need to be a path, but if there is a path and a host,
     // ensure the path begins with "/".
@@ -103,7 +80,7 @@ std::string protobuf_to_string(const url_proto::Url& url) {
   }
 
   // Queries must be started by "?". If libFuzzer included a query in url,
-  // ensure that it is preceded by "?". Also Seperate query components with
+  // ensure that it is preceded by "?". Also separate query components with
   // ampersands as is the convention.
   bool first_component = true;
   for (const std::string& query_component : url.query()) {
@@ -122,17 +99,4 @@ std::string protobuf_to_string(const url_proto::Url& url) {
   return url_string;
 }
 
-// The target function. This is the equivalent of LLVMFuzzerTestOneInput in
-// typical libFuzzer based fuzzers. It is passed our Url protobuf object that
-// was mutated by libFuzzer, converts it to a string and then feeds it to url()
-// for fuzzing.
-DEFINE_BINARY_PROTO_FUZZER(const url_proto::Url& url_protobuf) {
-  std::string url_string = protobuf_to_string(url_protobuf);
-
-  // Allow native input to be retrieved easily.
-  // Note that there will be a trailing newline that is not part of url_string.
-  if (getenv("LPM_DUMP_NATIVE_INPUT"))
-    std::cout << url_string << std::endl;
-
-  GURL url(url_string);
-}
+}  // namespace url_proto
