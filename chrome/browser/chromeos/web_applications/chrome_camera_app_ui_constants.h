f23da9ec418a1972ba6bd1e2906a34306cea631e
Kuo Jen Wei <inker@google.com>
2021-01-29 08:56:17 +0000

    CCA: Use calculated default window size
    The calculation of default window size is based on fixing width to 764px
    in all preview aspect ratio cases(3:4, 4:3, 9:16, 16:9) and deriving
    height from that fixed width so that after preserve space for left,
    right, bottom icons(--xxx-line*2), the |#preview-box| can fit in preview
    aspect ratio as close as possible. After these, the new default window
    size under different preview aspect ratio will be: 3:4 -> 764x888; 4:3
    -> 764x552; 9:16 -> 764x1144; 16:9 -> 764x444. Also change the minimal
    window width x height to 486 x 444 accordingly.
    
    Bug: b/175324385
    Test: Manually verify default window size under 3:4, 4:3, 9:16, 16:9
    preview.
    Test: tast run <DUT> "camera.CCA*"
    
    (cherry picked from commit 67f5a18ca293e1ef142284a72d69918cce6a52bf)
    
    (cherry picked from commit 2c0a9606e2d7e4740b9ffe8fca447c0d1e7a99dc)
    
    Change-Id: Id77a4843cb264fbaecb839b6ab053778017a6d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632105
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Original-Original-Commit-Position: refs/heads/master@{#843935}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635242
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Cr-Original-Commit-Position: refs/branch-heads/4387@{#9}
    Cr-Original-Branched-From: 7a47a61b26f401e0c3bf5d1720626d636657d790-refs/heads/master@{#842744}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653410
    Cr-Commit-Position: refs/branch-heads/4389@{#387}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chrome/browser/chromeos/web_applications/chrome_camera_app_ui_constants.h
+++ chrome/browser/chromeos/web_applications/chrome_camera_app_ui_constants.h
@@ -6,9 +6,9 @@
 #define CHROME_BROWSER_CHROMEOS_WEB_APPLICATIONS_CHROME_CAMERA_APP_UI_CONSTANTS_H_
 
 // All window size number here are referred to inner size excluding top bar.
-constexpr int kChromeCameraAppDefaultWidth = 788;
-constexpr int kChromeCameraAppDefaultHeight = 428;
-constexpr int kChromeCameraAppMinimumWidth = 505;
-constexpr int kChromeCameraAppMinimumHeight = 428;
+constexpr int kChromeCameraAppDefaultWidth = 764;
+constexpr int kChromeCameraAppDefaultHeight = 412;
+constexpr int kChromeCameraAppMinimumWidth = 486;
+constexpr int kChromeCameraAppMinimumHeight = 412;
 
 #endif  // CHROME_BROWSER_CHROMEOS_WEB_APPLICATIONS_CHROME_CAMERA_APP_UI_CONSTANTS_H_
