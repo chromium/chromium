0b3b7eb8752b2c3c1614a6ab78e5bcbe47d4cccc
Rushan Suleymanov <rushans@google.com>
2021-01-29 09:19:37 +0000

    [Sync] Remove verifier from single client bookmark tests
    The verifier is replaced by the check of the expected hierarchy of
    bookmarks where appropriate.
    
    Bug: 1137720
    Change-Id: I786d314af5cfc3dfd076c97e9a5e1308c3763b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653207
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848449}

--- chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
@@ -42,7 +42,6 @@ using bookmarks_helper::AddFolder;
 using bookmarks_helper::AddURL;
 using bookmarks_helper::BookmarkFaviconLoadedChecker;
 using bookmarks_helper::BookmarksGUIDChecker;
-using bookmarks_helper::BookmarksMatchVerifierChecker;
 using bookmarks_helper::BookmarksTitleChecker;
 using bookmarks_helper::BookmarksUrlChecker;
 using bookmarks_helper::CheckHasNoFavicon;
@@ -60,7 +59,6 @@ using bookmarks_helper::GetUniqueNodeByURL;
 using bookmarks_helper::IsFolderWithTitle;
 using bookmarks_helper::IsFolderWithTitleAndChildrenAre;
 using bookmarks_helper::IsUrlBookmarkWithTitleAndUrl;
-using bookmarks_helper::ModelMatchesVerifier;
 using bookmarks_helper::Move;
 using bookmarks_helper::Remove;
 using bookmarks_helper::RemoveAll;
@@ -88,18 +86,6 @@ class SingleClientBookmarksSyncTest : public SyncTest {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SingleClientBookmarksSyncTest);
-};
-
-class SingleClientBookmarksSyncTestWithVerifier
-    : public SingleClientBookmarksSyncTest {
- public:
-  SingleClientBookmarksSyncTestWithVerifier() = default;
-  ~SingleClientBookmarksSyncTestWithVerifier() override = default;
-
-  bool UseVerifier() override {
-    // TODO(crbug.com/1137720): rewrite tests to not use verifier.
-    return true;
-  }
 };
 
 class SingleClientBookmarksSyncTestWithDisabledCommitWithoutFavicon
@@ -384,8 +370,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, Sanity) {
                       "Wynn", GURL("http://www.wynnlasvegas.com")))))));
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
-                       CommitLocalCreations) {
+IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, CommitLocalCreations) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   // Starting state:
@@ -397,8 +382,9 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
   //        -> http://www.facebook.com "tier1_a_url2"
   //      -> tier1_b
   //        -> http://www.nhl.com "tier1_b_url0"
-  const BookmarkNode* top = AddFolder(
-      kSingleProfileIndex, GetOtherNode(kSingleProfileIndex), 0, "top");
+  const BookmarkNode* other_node = GetOtherNode(kSingleProfileIndex);
+  const BookmarkNode* top =
+      AddFolder(kSingleProfileIndex, other_node, 0, "top");
   const BookmarkNode* tier1_a =
       AddFolder(kSingleProfileIndex, top, 0, "tier1_a");
   const BookmarkNode* tier1_b =
@@ -423,7 +409,20 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
   ASSERT_TRUE(
       UpdatedProgressMarkerChecker(GetSyncService(kSingleProfileIndex)).Wait());
-  EXPECT_TRUE(BookmarksMatchVerifierChecker().Wait());
+  EXPECT_THAT(other_node->children(),
+              ElementsAre(IsFolderWithTitleAndChildrenAre(
+                  "top",
+                  IsFolderWithTitleAndChildrenAre(
+                      "tier1_a",
+                      IsUrlBookmarkWithTitleAndUrl("tier1_a_url0",
+                                                   "http://mail.google.com"),
+                      IsUrlBookmarkWithTitleAndUrl("tier1_a_url1",
+                                                   "http://www.pandora.com"),
+                      IsUrlBookmarkWithTitleAndUrl("tier1_a_url2",
+                                                   "http://www.facebook.com")),
+                  IsFolderWithTitleAndChildrenAre(
+                      "tier1_b", IsUrlBookmarkWithTitleAndUrl(
+                                     "tier1_b_url0", "http://www.nhl.com")))));
 }
 
 IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, InjectedBookmark) {
@@ -546,7 +545,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
 // Test that a client doesn't mutate the favicon data in the process
 // of storing the favicon data from sync to the database or in the process
 // of requesting data from the database for sync.
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
+IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
                        SetFaviconHiDPIDifferentCodec) {
   // Set the supported scale factors to 1x and 2x such that
   // BookmarkModel::GetFavicon() requests both 1x and 2x.
@@ -557,7 +556,6 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
   ui::SetSupportedScaleFactors(supported_scale_factors);
 
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
 
   const GURL page_url("http://www.google.com");
   const GURL icon_url("http://www.google.com/favicon.ico");
@@ -575,7 +573,6 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
 
   ASSERT_TRUE(
       UpdatedProgressMarkerChecker(GetSyncService(kSingleProfileIndex)).Wait());
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
 
   scoped_refptr<base::RefCountedMemory> original_favicon_bytes =
       original_favicon.As1xPNGBytes();
@@ -591,10 +588,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
 
 // Test that a client deletes favicons from sync when they have been removed
 // from the local database.
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
-                       DeleteFaviconFromSync) {
+IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, DeleteFaviconFromSync) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
 
   const GURL page_url("http://www.google.com");
   const GURL icon_url("http://www.google.com/favicon.ico");
@@ -603,7 +598,6 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
              bookmarks_helper::FROM_UI);
   ASSERT_TRUE(
       UpdatedProgressMarkerChecker(GetSyncService(kSingleProfileIndex)).Wait());
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
 
   // Simulate receiving a favicon deletion from sync.
   DeleteFaviconMappings(kSingleProfileIndex, bookmark,
@@ -611,14 +605,15 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
 
   ASSERT_TRUE(
       UpdatedProgressMarkerChecker(GetSyncService(kSingleProfileIndex)).Wait());
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
+  ASSERT_THAT(GetBookmarkBarNode(kSingleProfileIndex)->children(),
+              ElementsAre(IsUrlBookmarkWithTitleAndUrl("title", page_url)));
 
   CheckHasNoFavicon(kSingleProfileIndex, page_url);
   EXPECT_TRUE(
       GetBookmarkModel(kSingleProfileIndex)->GetFavicon(bookmark).IsEmpty());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
+IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest,
                        BookmarkAllNodesRemovedEvent) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
   // Starting state:
@@ -664,9 +659,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
 
   // Set up sync, wait for its completion and verify that changes propagated.
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_TRUE(
-      UpdatedProgressMarkerChecker(GetSyncService(kSingleProfileIndex)).Wait());
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
+  ASSERT_EQ(2u, GetOtherNode(kSingleProfileIndex)->children().size());
+  ASSERT_EQ(3u, GetBookmarkBarNode(kSingleProfileIndex)->children().size());
 
   // Remove all bookmarks and wait for sync completion.
   RemoveAll(kSingleProfileIndex);
@@ -675,8 +669,6 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTestWithVerifier,
   // Verify other node has no children now.
   EXPECT_TRUE(GetOtherNode(kSingleProfileIndex)->children().empty());
   EXPECT_TRUE(GetBookmarkBarNode(kSingleProfileIndex)->children().empty());
-  // Verify model matches verifier.
-  ASSERT_TRUE(BookmarksMatchVerifierChecker().Wait());
 }
 
 IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, DownloadDeletedBookmark) {
