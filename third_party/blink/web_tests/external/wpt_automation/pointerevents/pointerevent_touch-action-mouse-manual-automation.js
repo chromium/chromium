5d5013bdcee053676f07f723e32c5a8371cbb905
Lan Wei <lanwei@chromium.org>
2021-01-29 05:12:29 +0000

    Simulate user inputs in pointerevents/pointerevent_touch-action-mouse.html
    Use testdriver Action API to simulate wheel scroll actions in
    pointerevents/pointerevent_touch-action-mouse.html.
    
    Bug: 1145677
    Change-Id: I56d99d22a325419865fdc18dfa4cf0a3ae55df51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2556760
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848421}

--- third_party/blink/web_tests/external/wpt_automation/pointerevents/pointerevent_touch-action-mouse-manual-automation.js
+++
@@ -1,8 +0,0 @@
-importAutomationScript('/pointerevents/pointerevent_common_input.js');
-
-function inject_input() {
-  return mouseWheelScroll('#target0', 'down').then(function() {
-    return mouseWheelScroll('#target0', 'right');
-  });
-}
-
