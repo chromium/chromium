e945a1da1220ae8e28e9ee41486e358c204562ba
Lijin Shen <lazzzis@google.com>
2021-02-01 20:09:36 +0000

    [Messages] Extend duration of message on screen when a11y is on
    20s is not long enough for users to locate the location of the message.
    Extend it to 30s.
    
    Bug: 1159553
    Change-Id: I7a80dc153719fbcaed2b2ce6adf485416b30cdd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659736
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849234}

--- components/messages/android/java/src/org/chromium/components/messages/SingleActionMessage.java
+++ components/messages/android/java/src/org/chromium/components/messages/SingleActionMessage.java
@@ -22,7 +22,7 @@ import org.chromium.ui.util.AccessibilityUtil;
  */
 public class SingleActionMessage implements MessageStateHandler {
     private static final long DURATION = 10 * DateUtils.SECOND_IN_MILLIS;
-    private static final long DURATION_ON_A11Y = 20 * DateUtils.SECOND_IN_MILLIS;
+    private static final long DURATION_ON_A11Y = 30 * DateUtils.SECOND_IN_MILLIS;
 
     private MessageBannerCoordinator mMessageBanner;
     private MessageBannerView mView;
