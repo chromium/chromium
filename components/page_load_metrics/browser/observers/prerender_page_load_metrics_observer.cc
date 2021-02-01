0e1106877e428b893041552ab085cada3e7ee5ff
Matt Falkenhagen <falken@chromium.org>
2021-02-01 03:57:26 +0000

    prerender: Add metrics for when Session Storage is used.
    We did this for Local Storage in r822078 and it will also be good to
    know about Session Storage.
    
    Bug: 1126305
    Change-Id: I7b38c911aef04a00b7872bcb2626413e914a86a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659144
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848993}

--- components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/prerender_page_load_metrics_observer.cc
@@ -8,6 +8,24 @@
 #include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 
+namespace {
+
+// The purpose of this observer is to estimate how many prerendering pages will
+// use certain features. The plan for prerendering is to delay loading of
+// cross-origin iframes, so we want to ignore feature uses inside cross-origin
+// iframes.  To do this, just check if the |url| is cross-origin to
+// |first_party_url|. This may not be an accurate count if a third-party
+// subframe embeds a first-party subframe, or if there is a way for a frame to
+// access cross-origin storage, but it's probably not significant.
+//
+// This check is somewhat heavy so it should be done after cheaper early
+// returns.
+bool IsFirstParty(const GURL& url, const GURL& first_party_url) {
+  return url::IsSameOriginWith(url, first_party_url);
+}
+
+}  // namespace
+
 void PrerenderPageLoadMetricsObserver::OnFirstContentfulPaintInPage(
     const page_load_metrics::mojom::PageLoadTiming& timing) {
   did_fcp_ = true;
@@ -18,24 +36,37 @@ void PrerenderPageLoadMetricsObserver::OnStorageAccessed(
     const GURL& first_party_url,
     bool blocked_by_policy,
     page_load_metrics::StorageType access_type) {
-  if (access_type != page_load_metrics::StorageType::kLocalStorage ||
-      did_local_storage_)
-    return;
+  // TODO(falken): Account for `blocked_by_policy`?
 
-  // The purpose of this observer is to estimate how many prerendering pages
-  // will use certain features. The plan for prerendering is to delay loading
-  // of cross-origin iframes, so we want to ignore feature uses inside
-  // cross-origin iframes. To do this, just check if the |url| is cross-origin
-  // to |first_party_url|. This may not be an accurate count if a third-party
-  // subframe embeds a first-party subframe, or if there is a way for a frame
-  // to access cross-origin storage, but it's probably not significant.
-  if (!url::IsSameOriginWith(url, first_party_url))
-    return;
+  switch (access_type) {
+    case page_load_metrics::StorageType::kLocalStorage:
+      if (did_local_storage_)
+        return;
+      if (!IsFirstParty(url, first_party_url))
+        return;
 
-  did_local_storage_ = true;
-  RecordFeatureUse(
-      did_fcp_ ? blink::mojom::WebFeature::kLocalStorageFirstUsedAfterFcp
-               : blink::mojom::WebFeature::kLocalStorageFirstUsedBeforeFcp);
+      did_local_storage_ = true;
+      RecordFeatureUse(
+          did_fcp_ ? blink::mojom::WebFeature::kLocalStorageFirstUsedAfterFcp
+                   : blink::mojom::WebFeature::kLocalStorageFirstUsedBeforeFcp);
+      break;
+    case page_load_metrics::StorageType::kSessionStorage:
+      if (did_session_storage_)
+        return;
+      if (!IsFirstParty(url, first_party_url))
+        return;
+
+      did_session_storage_ = true;
+      RecordFeatureUse(
+          did_fcp_
+              ? blink::mojom::WebFeature::kSessionStorageFirstUsedAfterFcp
+              : blink::mojom::WebFeature::kSessionStorageFirstUsedBeforeFcp);
+      break;
+    case page_load_metrics::StorageType::kFileSystem:
+    case page_load_metrics::StorageType::kIndexedDb:
+    case page_load_metrics::StorageType::kCacheStorage:
+      return;
+  }
 }
 
 void PrerenderPageLoadMetricsObserver::RecordFeatureUse(
