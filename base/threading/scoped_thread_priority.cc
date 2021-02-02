b27243a0aceaea3d51158c34f379fc1e551734d2
Gabriel Charette <gab@chromium.org>
2021-02-02 16:12:10 +0000

    Revert "[base] Add a Feature to perform an ablation study on 2020 priority inversion fixes"
    This reverts commit ab901a3b4566a741e0e02ba3bde8b4b00ed8fda1.
    
    Reason for revert: Experiment is over.
    
    Original change's description:
    > [base] Add a Feature to perform an ablation study on 2020 priority inversion fixes
    >
    > R=​etiennep@chromium.org, olivierli@chromium.org
    >
    > Bug: 1167247
    > Change-Id: I034b0b6e095b3b10c5cf4318c11e94f83424aba6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633365
    > Reviewed-by: Oliver Li <olivierli@google.com>
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#844578}
    
    Bug: 1167247
    Change-Id: I120ec7c9c30e48049f8b207b4bdba73aa43d0a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668308
    Reviewed-by: Oliver Li <olivierli@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849618}

--- base/threading/scoped_thread_priority.cc
+++ base/threading/scoped_thread_priority.cc
@@ -4,7 +4,6 @@
 
 #include "base/threading/scoped_thread_priority.h"
 
-#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/threading/platform_thread.h"
 #include "base/trace_event/base_tracing.h"
@@ -12,15 +11,6 @@
 
 namespace base {
 namespace internal {
-
-#if defined(OS_WIN)
-// This is an ablation study to verify the impact of introducing
-// ScopedMayLoadLibraryAtBackgroundPriority now that we have hang metrics in
-// place (and also to verify the metric is able to catch a regression in this
-// space).
-const Feature kFixLdrLockPriorityInversion = {"FixLdrLockPriorityInversion",
-                                              FEATURE_ENABLED_BY_DEFAULT};
-#endif  // defined(OS_WIN)
 
 ScopedMayLoadLibraryAtBackgroundPriority::
     ScopedMayLoadLibraryAtBackgroundPriority(const Location& from_here,
@@ -35,18 +25,6 @@ ScopedMayLoadLibraryAtBackgroundPriority::
 #if defined(OS_WIN)
   if (already_loaded_ && already_loaded_->load(std::memory_order_relaxed))
     return;
-
-  // Skip the experiment if the FeatureList is not ready. This avoids crashes on
-  // Canary if a DLL is loaded early. It's fine to skip the experiment in this
-  // case because FeatureList is expected to be loaded before any background
-  // threads are created anyways.
-  if (FeatureList::GetInstance() &&
-      !FeatureList::IsEnabled(kFixLdrLockPriorityInversion)) {
-    TRACE_EVENT0(
-        "base",
-        "ScopedMayLoadLibraryAtBackgroundPriority : experimentally ignored");
-    return;
-  }
 
   const base::ThreadPriority priority =
       PlatformThread::GetCurrentThreadPriority();
