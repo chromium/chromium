a601a0244d874ee981fb77181760e1f66956c7bb
Matt Menard <mattme@google.com>
2021-02-02 18:55:32 +0000

    Enable PrintServerScaling flag by default
    (cherry picked from commit b0d5697e472dea4f2fd37fba4a0ddb71e81a2082)
    
    Bug: b:168650771
    Change-Id: I767eb010ae65cf1b4f694f464ff70ab80612d05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650307
    Auto-Submit: Matt Menard <mattme@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Matt Menard <mattme@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#848714}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658890
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4389@{#562}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -539,7 +539,7 @@ const base::Feature kPrintSaveToDrive{"PrintSaveToDrive",
 
 // Allows print servers to be selected when beyond a specified limit.
 const base::Feature kPrintServerScaling{"PrintServerScaling",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
+                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Controls whether to enable quick answers.
 const base::Feature kQuickAnswers{"QuickAnswers",
