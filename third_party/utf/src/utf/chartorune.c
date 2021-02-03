a5fb7662f5a2c5b61c532d949678ce1c4a1dc665
Sophie Chang <sophiechang@chromium.org>
2021-02-03 05:12:48 +0000

    Add workaround for libphonenumber's UTF subset on Android
    Bug: 1173328
    Change-Id: Iacf02d336780b9077531dbf82b69c7d49090f0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670887
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849973}

--- third_party/utf/src/utf/chartorune.c
+++ third_party/utf/src/utf/chartorune.c
@@ -1,14 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include "utf.h"
 
-/* lookup table for the number of bytes expected in a sequence */
-const unsigned char utftab[64] = {
-	0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /* 1100xxxx */
-	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, /* 1101xxxx */
-	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, /* 1110xxxx */
-	4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 0, 0, /* 1111xxxx */
-};
-
 int
 charntorune(Rune *p, const char *s, size_t len)
 {
