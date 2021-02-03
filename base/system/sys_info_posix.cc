20a9faa93e32e27141e2e10782319f5aa8e56108
Camillo Bruni <cbruni@chromium.org>
2021-02-03 13:11:41 +0000

    [base] Check CPU affinity when counting CPUs
    On Linux, use sched_getaffinity to count usable CPUs, so that runners of
    Chromium can restrict cores with e.g. taskset.
    
    This is observable locally by running
    
        taskset ./chrome
    
    recording a trace with chrome://tracing, and checking the number of
    foreground worker pool threads created.
    
    Reland of https://crrev.com/c/2270183 with the additional change of
    excluding chrome os due to failures that are not yet understood,
    see revert https://crrev.com/c/2290471.
    
    Bug: 1173421
    Change-Id: I411dfc4c4306cf6c8fc34282992deb73feba36b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2655545
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850081}

--- base/system/sys_info_posix.cc
+++ base/system/sys_info_posix.cc
@@ -5,6 +5,7 @@
 #include "base/system/sys_info.h"
 
 #include <errno.h>
+#include <sched.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
@@ -57,7 +58,21 @@ int NumberOfProcessors() {
     return 1;
   }
 
-  return static_cast<int>(res);
+  int num_cpus = static_cast<int>(res);
+
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  // Restrict the CPU count based on the process's CPU affinity mask, if
+  // available.
+  cpu_set_t* cpu_set = CPU_ALLOC(num_cpus);
+  size_t cpu_set_size = CPU_ALLOC_SIZE(num_cpus);
+  int ret = sched_getaffinity(0, cpu_set_size, cpu_set);
+  if (ret == 0) {
+    num_cpus = CPU_COUNT_S(cpu_set_size, cpu_set);
+  }
+  CPU_FREE(cpu_set);
+#endif // defined(OS_LINUX) && !defined(OS_CHROMEOS
+
+  return num_cpus;
 }
 
 base::LazyInstance<base::internal::LazySysInfoValue<int, NumberOfProcessors>>::
