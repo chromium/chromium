31bc15eac931152b1add7e54db18a83948a357e3
Canon Mukai <canonmukai@google.com>
2021-02-03 06:45:03 +0000

    bindings: [ToV8Traits] Add a unittest
    This CL adds a unittest file for ToV8Traits to compile to_v8_traits.h.
    Here is only one TEST which is just using ToV8().
    I will add more unittests to this file.
    
    Bug: 1172074
    Change-Id: I74980e91ff56dd8a1c2ca2260d80299deb3f381f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670680
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Canon Mukai <canonmukai@google.com>
    Cr-Commit-Position: refs/heads/master@{#849991}

---
+++ third_party/blink/renderer/bindings/core/v8/to_v8_traits_test.cc
@@ -0,0 +1,22 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/core/geometry/dom_point.h"
+
+namespace blink {
+
+namespace {
+
+TEST(ToV8TraitsTest, JustUsingToV8) {
+  V8TestingScope scope;
+  DOMPoint* point = DOMPoint::Create(1, 2, 3, 4);
+  ToV8Traits<DOMPoint>::ToV8(scope.GetScriptState(), point);
+}
+
+}  // namespace
+
+}  // namespace blink
