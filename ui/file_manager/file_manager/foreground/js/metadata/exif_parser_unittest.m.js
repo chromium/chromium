70e3912ed66ea1fc591d81e810e36bcd9b3a0d6d
Luciano Pacheco <lucmult@chromium.org>
2021-02-03 05:56:28 +0000

    Gallery & Files app: Copy ByteWriter from Gallery to Files app
    Copy the code that is used in Files app to allow the removal of Gallery.
    
    This class is only used in one unittest, copied to there. Additionally
    converted the class to ES6 class and replaces var with const/let.
    
    Bug: b:172875001
    Fixed: 1149269
    Change-Id: Ic73ae09bb8a2543f3b7830af81dae9f9674a376d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670659
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: David Lei <dlei@google.com>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849981}

--- ui/file_manager/file_manager/foreground/js/metadata/exif_parser_unittest.m.js
+++ ui/file_manager/file_manager/foreground/js/metadata/exif_parser_unittest.m.js
@@ -3,13 +3,189 @@
 // found in the LICENSE file.
 
 import {assertEquals, assertTrue} from 'chrome://test/chai_assert.js';
-import {ByteWriter} from '../../../../gallery/js/image_editor/exif_encoder.m.js';
 import {ByteReader} from './byte_reader.m.js';
 import * as wrappedExif from './exif_constants.m.js';
 import {ExifParser} from './exif_parser.m.js';
 const {Exif} = wrappedExif;
 import {ExifEntry} from '../../../../externs/exif_entry.m.js';
 import {MetadataParserLogger} from '../../../../externs/metadata_worker_window.m.js';
+
+class ByteWriter {
+  /**
+   * @param {!ArrayBuffer} arrayBuffer Underlying buffer to use.
+   * @param {number} offset Offset at which to start writing.
+   * @param {number=} opt_length Maximum length to use.
+   */
+  constructor(arrayBuffer, offset, opt_length) {
+    const length = opt_length || (arrayBuffer.byteLength - offset);
+    this.view_ = new DataView(arrayBuffer, offset, length);
+    this.littleEndian_ = false;
+    this.pos_ = 0;
+    this.forwards_ = {};
+  }
+
+  /**
+   * If key is a number, format it in hex style.
+   * @param {!(string|Exif.Tag)} key A key.
+   * @return {string} Formatted representation.
+   */
+  static prettyKeyFormat(key) {
+    if (typeof key === 'number') {
+      return '0x' + key.toString(16);
+    } else {
+      return key;
+    }
+  }
+
+  /**
+   * Set the byte ordering for future writes.
+   * @param {ByteWriter.ByteOrder} order ByteOrder to use
+   *     {ByteWriter.LITTLE_ENDIAN} or {ByteWriter.BIG_ENDIAN}.
+   */
+  setByteOrder(order) {
+    this.littleEndian_ = (order === ByteWriter.ByteOrder.LITTLE_ENDIAN);
+  }
+
+  /**
+   * @return {number} the current write position.
+   */
+  tell() {
+    return this.pos_;
+  }
+
+  /**
+   * Skips desired amount of bytes in output stream.
+   * @param {number} count Byte count to skip.
+   */
+  skip(count) {
+    this.validateWrite(count);
+    this.pos_ += count;
+  }
+
+  /**
+   * Check if the buffer has enough room to read 'width' bytes. Throws an error
+   * if it has not.
+   * @param {number} width Amount of bytes to check.
+   */
+  validateWrite(width) {
+    if (this.pos_ + width > this.view_.byteLength) {
+      throw new Error('Writing past the end of the buffer');
+    }
+  }
+
+  /**
+   * Writes scalar value to output stream.
+   * @param {number} value Value to write.
+   * @param {number} width Desired width of written value.
+   * @param {boolean=} opt_signed True if value represents signed number.
+   */
+  writeScalar(value, width, opt_signed) {
+    let method;
+    // The below switch is so verbose for two reasons:
+    // 1. V8 is faster on method names which are 'symbols'.
+    // 2. Method names are discoverable by full text search.
+    switch (width) {
+      case 1:
+        method = opt_signed ? 'setInt8' : 'setUint8';
+        break;
+
+      case 2:
+        method = opt_signed ? 'setInt16' : 'setUint16';
+        break;
+
+      case 4:
+        method = opt_signed ? 'setInt32' : 'setUint32';
+        break;
+
+      case 8:
+        method = opt_signed ? 'setInt64' : 'setUint64';
+        break;
+
+      default:
+        throw new Error('Invalid width: ' + width);
+        break;
+    }
+
+    this.validateWrite(width);
+    this.view_[method](this.pos_, value, this.littleEndian_);
+    this.pos_ += width;
+  }
+
+  /**
+   * Writes string.
+   * @param {string} str String to write.
+   */
+  writeString(str) {
+    this.validateWrite(str.length);
+    for (let i = 0; i != str.length; i++) {
+      this.view_.setUint8(this.pos_++, str.charCodeAt(i));
+    }
+  }
+
+  /**
+   * Allocate the space for 'width' bytes for the value that will be set later.
+   * To be followed by a 'resolve' call with the same key.
+   * @param {(string|Exif.Tag)} key A key to identify the value.
+   * @param {number} width Width of the value in bytes.
+   */
+  forward(key, width) {
+    if (key in this.forwards_) {
+      throw new Error('Duplicate forward key ' + key);
+    }
+    this.validateWrite(width);
+    this.forwards_[key] = {
+      pos: this.pos_,
+      width: width,
+    };
+    this.pos_ += width;
+  }
+
+  /**
+   * Set the value previously allocated with a 'forward' call.
+   * @param {(string|Exif.Tag)} key A key to identify the value.
+   * @param {number} value value to write in pre-allocated space.
+   */
+  resolve(key, value) {
+    if (!(key in this.forwards_)) {
+      throw new Error('Undeclared forward key ' + key.toString(16));
+    }
+    const forward = this.forwards_[key];
+    const curPos = this.pos_;
+    this.pos_ = forward.pos;
+    this.writeScalar(value, forward.width);
+    this.pos_ = curPos;
+    delete this.forwards_[key];
+  }
+
+  /**
+   * A shortcut to resolve the value to the current write position.
+   * @param {(string|Exif.Tag)} key A key to identify pre-allocated position.
+   */
+  resolveOffset(key) {
+    this.resolve(key, this.tell());
+  }
+
+  /**
+   * Check if every forward has been resolved, throw and error if not.
+   */
+  checkResolved() {
+    for (const key in this.forwards_) {
+      throw new Error(
+          'Unresolved forward pointer ' + ByteWriter.prettyKeyFormat(key));
+    }
+  }
+}
+
+/**
+ * Byte order.
+ * @enum {number}
+ */
+ByteWriter.ByteOrder = {
+  // Little endian byte order.
+  LITTLE_ENDIAN: 0,
+  // Big endian byte order.
+  BIG_ENDIAN: 1
+};
 
 /**
  * Creates a directory with specified tag. This method only supports string
