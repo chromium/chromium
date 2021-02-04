7b8c849c886af63bcf43cc6a784cc56d40b82b45
Yuta Hijikata <ythjkt@google.com>
2021-02-04 02:35:39 +0000

    Lacros: Fixes mouse input freeze on lacros.
    This ensures that kUseSkiaRenderer will be consistent before and after
    macro flip of lacros-chrome.
    
    Without this patch lacros-chrome stops responding to mouse input,
    when built with -DOS_CHROMEOS instead of OS_LINUX.
    
    Bug: 1173230
    Change-Id: I36b1135b95d68edcf26c1c39cfb6039a093bc33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670661
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850418}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -32,9 +32,8 @@ const base::Feature kEnableOverlayPrioritization {
 // Use the SkiaRenderer.
 const base::Feature kUseSkiaRenderer {
   "UseSkiaRenderer",
-#if defined(OS_WIN) || defined(OS_ANDROID) || \
-    (defined(OS_LINUX) &&                     \
-     !(BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMECAST)))
+#if defined(OS_WIN) || defined(OS_ANDROID) || BUILDFLAG(IS_CHROMEOS_LACROS) || \
+    (defined(OS_LINUX) && !BUILDFLAG(IS_CHROMECAST))
       base::FEATURE_ENABLED_BY_DEFAULT
 #else
       base::FEATURE_DISABLED_BY_DEFAULT
