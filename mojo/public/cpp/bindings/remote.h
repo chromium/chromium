21839d9335d07791a72b3d76fdca8b51e65bd967
Daniel Cheng <dcheng@chromium.org>
2021-02-04 19:48:26 +0000

    [mojo] Disallow binding an already-bound Remote.
    Bug: 1174287
    Change-Id: Ia0fcd2e730773589df969ea961ece1125e24919f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674704
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#850718}

--- mojo/public/cpp/bindings/remote.h
+++ mojo/public/cpp/bindings/remote.h
@@ -230,6 +230,7 @@ class Remote {
   // default SequencedTaskRunner (i.e. base::SequencedTaskRunnerHandle::Get() at
   // the time of this call). Must only be called on an unbound Remote.
   PendingReceiver<Interface> BindNewPipeAndPassReceiver() WARN_UNUSED_RESULT {
+    DCHECK(!is_bound()) << "Remote is already bound";
     return BindNewPipeAndPassReceiver(nullptr);
   }
 
@@ -239,6 +240,7 @@ class Remote {
   // owns this Remote.
   PendingReceiver<Interface> BindNewPipeAndPassReceiver(
       scoped_refptr<base::SequencedTaskRunner> task_runner) WARN_UNUSED_RESULT {
+    DCHECK(!is_bound()) << "Remote is already bound";
     MessagePipe pipe;
     Bind(PendingRemote<Interface>(std::move(pipe.handle0), 0),
          std::move(task_runner));
@@ -251,6 +253,7 @@ class Remote {
   // base::SequencedTaskRunnerHandle::Get() at the time of this call). Must only
   // be called on an unbound Remote.
   void Bind(PendingRemote<Interface> pending_remote) {
+    DCHECK(!is_bound()) << "Remote is already bound";
     DCHECK(pending_remote.is_valid());
     Bind(std::move(pending_remote), nullptr);
   }
