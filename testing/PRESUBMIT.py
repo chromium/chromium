68e68117971be87f27c393ba7247de1aaacb18db
Ben Pastene <bpastene@chromium.org>
2021-02-04 00:20:36 +0000

    Replace use of 'black_list' in //testing/PRESUBMIT.py
    Should fix presubmit warnings.
    
    R=​dpranke
    
    (cherry picked from commit 4b514ab42662385efc1a62b802b2824424e10d95)
    
    Change-Id: I767511e997d23a4a43e1dc2e25be567fca804c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412895
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#807283}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674076
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#1538}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- testing/PRESUBMIT.py
+++ testing/PRESUBMIT.py
@@ -11,11 +11,10 @@ for more details on the presubmit API built into depot_tools.
 
 def CommonChecks(input_api, output_api):
   output = []
-  blacklist = [r'gmock.*', r'gtest.*']
   output.extend(input_api.canned_checks.RunUnitTestsInDirectory(
       input_api, output_api, '.', [r'^.+_unittest\.py$']))
   output.extend(input_api.canned_checks.RunPylint(
-      input_api, output_api, black_list=blacklist))
+      input_api, output_api, files_to_skip=[r'gmock.*', r'gtest.*']))
   return output
 
 
