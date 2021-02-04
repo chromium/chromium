1c52e2f80775ec51d66adad95dfdac9ea5db7267
Sanja Perisic <sanjaperisic@chromium.org>
2021-02-04 09:41:49 +0000

    Moved policy code from components/ to web_applications/
    We need to access SystemWebAppManager which is in web_applications/ from WebAppPolicymanager, so we need to move WebAppPolicyManager to web_applications/.
    
    Change-Id: Ic6ae4d7e7faec8e0ac09c5eec9b45505fd258234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665919
    Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850513}

--- chrome/browser/web_applications/policy/web_app_policy_manager.cc
+++ chrome/browser/web_applications/policy/web_app_policy_manager.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/web_applications/components/policy/web_app_policy_manager.h"
+#include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 
 #include <algorithm>
 #include <utility>
