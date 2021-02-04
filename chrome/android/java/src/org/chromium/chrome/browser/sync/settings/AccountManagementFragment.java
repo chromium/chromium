b152cf5b46bae11355188d0934486b0c6f0c4078
Tanmoy Mollik <triploblastic@chromium.org>
2021-02-04 09:35:18 +0000

    [Android] Stop showing divider line for child accounts in AccountManagementFragment
    When MOBILE_IDENTITY_CONSISTENCY is enabled double divider line is shown
    between the child account and parental settings. This cl fixes that
    issue.
    
    Bug: 1162310
    
    Change-Id: Ia09ce943fd7efa8e1acd8a1b9b340ce81334a433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623209
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850512}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/AccountManagementFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/AccountManagementFragment.java
@@ -79,7 +79,6 @@ public class AccountManagementFragment extends PreferenceFragmentCompat
     private static final String PREF_PARENTAL_SETTINGS = "parental_settings";
     private static final String PREF_PARENT_ACCOUNTS = "parent_accounts";
     private static final String PREF_CHILD_CONTENT = "child_content";
-    private static final String PREF_CHILD_CONTENT_DIVIDER = "child_content_divider";
     private static final String PREF_SIGN_OUT = "sign_out";
     private static final String PREF_SIGN_OUT_DIVIDER = "sign_out_divider";
 
@@ -272,7 +271,6 @@ public class AccountManagementFragment extends PreferenceFragmentCompat
             prefScreen.removePreference(findPreference(PREF_PARENTAL_SETTINGS));
             prefScreen.removePreference(parentAccounts);
             prefScreen.removePreference(childContent);
-            prefScreen.removePreference(findPreference(PREF_CHILD_CONTENT_DIVIDER));
         }
     }
 
