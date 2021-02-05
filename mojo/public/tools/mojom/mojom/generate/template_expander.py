5f54923221c65c720b993bdc1816de98f75bc651
Wojciech Bielawski <wbielawski@fb.com>
2021-02-05 21:26:43 +0000

    Let mojo to be built using python 3.
    All necessary corrections to build mojo using python 3 are already applied.
    
    Bug: 1112471
    Change-Id: I476ccf800632e1fac6de9e61f14f13f297f750f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671325
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851284}

--- mojo/public/tools/mojom/mojom/generate/template_expander.py
+++ mojo/public/tools/mojom/mojom/generate/template_expander.py
@@ -75,9 +75,9 @@ def PrecompileTemplates(generator_modules, output_dir):
                 os.path.dirname(module.__file__), generator.GetTemplatePrefix())
         ]))
     jinja_env.filters.update(generator.GetFilters())
-    jinja_env.compile_templates(
-        os.path.join(output_dir, "%s.zip" % generator.GetTemplatePrefix()),
-        extensions=["tmpl"],
-        zip="stored",
-        py_compile=True,
-        ignore_errors=False)
+    jinja_env.compile_templates(os.path.join(
+        output_dir, "%s.zip" % generator.GetTemplatePrefix()),
+                                extensions=["tmpl"],
+                                zip="stored",
+                                py_compile=sys.version_info.major < 3,
+                                ignore_errors=False)
