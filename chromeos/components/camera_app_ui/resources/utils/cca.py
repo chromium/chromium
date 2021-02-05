90217b5315e49ed7bbd6407be4151b1d45330930
Wei Lee <wtlee@google.com>
2021-02-05 05:05:07 +0000

    [CCA] Remove functions for platform app from cca.py
    Bug: b/172343409
    Test: cca.py deploy [BOARD] [DUT]
    Change-Id: I37bac00dc7b96df32f21f830810437e36014cc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675541
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850977}

--- chromeos/components/camera_app_ui/resources/utils/cca.py
+++ chromeos/components/camera_app_ui/resources/utils/cca.py
@@ -5,18 +5,14 @@
 
 import ast
 import argparse
-from distutils import dir_util
 import functools
-import json
 import logging
 import os
 import re
 import shlex
-import shutil
 import subprocess
 import sys
 import tempfile
-import time
 
 
 @functools.lru_cache(1)
@@ -42,22 +38,6 @@ def run_node(args):
     run([node, binary] + args[1:])
 
 
-def build_locale_strings():
-    grit_cmd = [
-        os.path.join(get_chromium_root(), 'tools/grit/grit.py'),
-        '-i',
-        'strings/camera_strings.grd',
-        'build',
-        '-E',
-        'out_camera_app_dir=platform',
-        '-E',
-        'gen_camera_app_dir=swa',
-        '-o',
-        'build/strings',
-    ]
-    run(grit_cmd)
-
-
 def build_preload_images_js(outdir):
     with open('images/images.gni') as f:
         in_app_assets = ast.literal_eval(
@@ -76,160 +56,7 @@ def build_preload_images_js(outdir):
         subprocess.check_call(cmd)
 
 
-def build_mojom_bindings(mojom_bindings):
-    pylib = os.path.join(get_chromium_root(), 'mojo/public/tools/mojom')
-    sys.path.insert(0, pylib)
-    # pylint: disable=import-error,import-outside-toplevel
-    from mojom.parse.parser import Parse
-
-    mojom_paths = set()
-
-    def add_path(mojom):
-        path = os.path.join(get_chromium_root(), mojom)
-        if path in mojom_paths:
-            return
-        mojom_paths.add(path)
-
-        with open(path) as f:
-            code = f.read()
-            ast = Parse(code, path)
-            for imp in ast.import_list:
-                add_path(imp.import_filename)
-
-    for binding in mojom_bindings:
-        mojom = re.sub('-lite.js$', '', binding)
-        add_path(mojom)
-
-    # It's needed for generating mojo_bindings_lite.js.
-    add_path(
-        'mojo/public/interfaces/bindings/interface_control_messages.mojom')
-
-    if not os.path.exists('build/mojo'):
-        os.makedirs('build/mojo')
-
-    generator = os.path.join(
-        get_chromium_root(),
-        'mojo/public/tools/bindings/mojom_bindings_generator.py')
-
-    precompile_cmd = [
-        generator,
-        '-o',
-        'build/mojo',
-        '--use_bundled_pylibs',
-        'precompile',
-    ]
-    run(precompile_cmd)
-
-    parser = os.path.join(get_chromium_root(),
-                          'mojo/public/tools/mojom/mojom_parser.py')
-
-    parse_cmd = [
-        parser,
-        '--output-root',
-        'build/mojo',
-        '--input-root',
-        get_chromium_root(),
-        '--mojoms',
-    ] + list(mojom_paths)
-    run(parse_cmd)
-
-    generate_cmd = [
-        generator,
-        '-o',
-        'build/mojo',
-        '--use_bundled_pylibs',
-        'generate',
-        '-d',
-        get_chromium_root(),
-        '-I',
-        get_chromium_root(),
-        '--bytecode_path',
-        'build/mojo',
-        '-g',
-        'javascript',
-        '--js_bindings_mode',
-        'new',
-    ] + list(mojom_paths)
-    run(generate_cmd)
-
-    generate_binding_lite_cmd = [
-        os.path.join(get_chromium_root(),
-                     ('mojo/public/tools/bindings/' +
-                      'concatenate_and_replace_closure_exports.py')),
-        os.path.join(get_chromium_root(),
-                     'mojo/public/js/mojo_internal_preamble.js.part'),
-        os.path.join(get_chromium_root(), 'mojo/public/js/bindings_lite.js'),
-        os.path.join(get_chromium_root(),
-                     'mojo/public/js/interface_support_preamble.js.part'),
-        os.path.join(get_chromium_root(),
-                     'mojo/public/js/interface_support.js'),
-        ('build/mojo/mojo/public/interfaces/' +
-         'bindings/interface_control_messages.mojom-lite.js'),
-        'build/mojo/mojo_bindings_lite.js',
-    ]
-    run(generate_binding_lite_cmd)
-
-
-def build_cca(overlay=None, key=None):
-    with open('BUILD.gn') as f:
-        mojom_bindings = re.findall(r'root_gen_dir/(.*mojom-lite\.js)',
-                                    f.read())
-    mojo_files = ['mojo_bindings_lite.js'] + mojom_bindings
-
-    # TODO(shik): Check mtime and rebuild them if the source is updated.
-    if not os.path.exists('build/strings/platform'):
-        build_locale_strings()
-    if any(not os.path.exists(os.path.join('build/mojo', f))
-           for f in mojo_files):
-        build_mojom_bindings(mojom_bindings)
-
-    shutil.rmtree('build/camera', ignore_errors=True)
-
-    dir_list = [src for src in os.listdir('.') if os.path.isdir(src)]
-    for d in dir_list:
-        if d in ['build', 'node_modules', 'strings']:
-            continue
-        dir_util.copy_tree(d, os.path.join('build/camera', d))
-    build_preload_images_js('build/camera/js')
-    shutil.copy2('manifest.json', 'build/camera/manifest.json')
-
-    for f in mojo_files:
-        shutil.copy2(os.path.join('build/mojo', f), 'build/camera/js/mojo')
-    dir_util.copy_tree('build/strings/platform', 'build/camera')
-
-    if overlay == 'dev':
-        dir_util.copy_tree('utils/dev', 'build/camera')
-
-        git_cmd = ['git', 'rev-parse', 'HEAD']
-        commit_hash = subprocess.check_output(git_cmd, text=True).strip()[:8]
-        timestamp = time.strftime("%F %T")
-
-        with open('manifest.json') as f:
-            manifest = json.load(f)
-        manifest['version_name'] = f'Dev {commit_hash} @ {timestamp}'
-        if key is not None:
-            manifest['key'] = key
-        with open('build/camera/manifest.json', 'w') as f:
-            json.dump(manifest, f, indent=2)
-
-
 def deploy(args):
-    build_cca()
-    cmd = [
-        'rsync',
-        '--archive',
-        '--checksum',
-        '--chown=chronos:chronos',
-        '--omit-dir-times',
-        '--perms',
-        '--verbose',
-        'build/camera/',
-        f'{args.device}:/opt/google/chrome/resources/chromeos/camera',
-    ]
-    run(cmd)
-
-
-def deploy_swa(args):
     cca_root = os.getcwd()
     target_dir = os.path.join(get_chromium_root(), f'out_{args.board}/Release')
 
@@ -329,20 +156,13 @@ def parse_args(args):
 
     deploy_parser = subparsers.add_parser('deploy',
                                           help='deploy to device',
-                                          description='Deploy CCA to device.')
-    deploy_parser.add_argument('device')
-    deploy_parser.set_defaults(func=deploy)
-
-    deploy_swa_parser = subparsers.add_parser(
-        'deploy-swa',
-        help='deploy CCA (SWA) to device',
-        description='''Deploy CCA (SWA) to device.
+                                          description='''Deploy CCA to device.
             This script only works if there is no file added/deleted.
             And please build Chrome at least once before running the command.'''
-    )
-    deploy_swa_parser.add_argument('board')
-    deploy_swa_parser.add_argument('device')
-    deploy_swa_parser.set_defaults(func=deploy_swa)
+                                          )
+    deploy_parser.add_argument('board')
+    deploy_parser.add_argument('device')
+    deploy_parser.set_defaults(func=deploy)
 
     test_parser = subparsers.add_parser('test',
                                         help='run tests',
