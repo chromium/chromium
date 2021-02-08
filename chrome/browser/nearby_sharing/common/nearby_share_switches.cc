8e3cf0cdadbdb16f5a6ac1cf00cf5adc9016a6a5
Curt Clemens <cclem@google.com>
2021-02-08 19:50:55 +0000

    [Nearby] Add switch to force verbose logging for Nearby
    Add a command line switch that allows setting the log level to
    verbose for Nearby Share and Nearby Connections to facilitate
    collecting logs during testing.
    
    Bug: 1166070
    Change-Id: I9e963f3d8587310e7c46cb43d6c43d9d6b8ee9e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669710
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#851836}

--- chrome/browser/nearby_sharing/common/nearby_share_switches.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_switches.cc
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/nearby_sharing/client/nearby_share_switches.h"
+#include "chrome/browser/nearby_sharing/common/nearby_share_switches.h"
 
 namespace switches {
 
 // Overrides the default URL for Google APIs (https://www.googleapis.com) used
 // by Nearby Share
 const char kNearbyShareHTTPHost[] = "nearbysharing-http-host";
+
+// Enables verbose logging level for Nearby Share.
+const char kNearbyShareVerboseLogging[] = "nearby-share-verbose-logging";
 
 }  // namespace switches
--- chrome/browser/nearby_sharing/common/nearby_share_switches.h
+++ chrome/browser/nearby_sharing/common/nearby_share_switches.h
@@ -2,15 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_NEARBY_SHARING_CLIENT_NEARBY_SHARE_SWITCHES_H_
-#define CHROME_BROWSER_NEARBY_SHARING_CLIENT_NEARBY_SHARE_SWITCHES_H_
+#ifndef CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_SWITCHES_H_
+#define CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_SWITCHES_H_
 
 namespace switches {
 
 // All switches in alphabetical order. The switches should be documented
 // alongside the definition of their values in the .cc file.
 extern const char kNearbyShareHTTPHost[];
+extern const char kNearbyShareVerboseLogging[];
 
 }  // namespace switches
 
-#endif  // CHROME_BROWSER_NEARBY_SHARING_CLIENT_NEARBY_SHARE_SWITCHES_H_
+#endif  // CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_SWITCHES_H_
