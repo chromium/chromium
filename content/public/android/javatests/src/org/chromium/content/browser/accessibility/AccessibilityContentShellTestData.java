1456afbf6e9cd59579737c309688f35b203cd626
Mark Schillaci <mschillaci@google.com>
2021-02-09 00:12:18 +0000

    Refactor WebContentsAccessibilityTest and improve flakiness and failures
    This CL is a refactor effort to improve the WebContentsAccessibilityTest
    file.
    
    The WebContentsAccessibilityTest is a set of content shell tests which
    test the WebContentsAccessibilityImpl as well as underlying logic to
    ensure the accessibility code is correctly creating virtual views and
    sending events correctly.
    
    This CL adds some abstractions and an AccessibilityContentShellTestUtils
    file which includes some basic methods and objects used throughout
    the test suite.
    
    This CL cleans up the test file, re-enables all tests, adds
    error messages for failing tests, updates to using polling throughout
    to eliminate flakiness, and in general cleans up the test file.
    
    The goal of this CL is to make unit tests easier to write going forward,
    and to ensure all our tests are enabled and not flaky.
    
    AX-Relnotes: N/A
    Bug: 1162479, 1162284
    Change-Id: I39afcfb6f51db3cc3a595c2e453c1d0265797545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669367
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#851959}

---
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/AccessibilityContentShellTestData.java
@@ -0,0 +1,78 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.accessibility;
+
+/**
+ * Simple POJO used for tracking accessibility data during content shell unit tests.
+ */
+public class AccessibilityContentShellTestData {
+    public int traverseFromIndex;
+    public int traverseToIndex;
+    public int selectionFromIndex;
+    public int selectionToIndex;
+    public String announcementText;
+    public int typeWindowContentChangedCount;
+
+    public AccessibilityContentShellTestData() {
+        traverseFromIndex = -1;
+        traverseToIndex = -1;
+        selectionFromIndex = -1;
+        selectionToIndex = -1;
+        announcementText = "";
+        typeWindowContentChangedCount = 0;
+    }
+
+    public int getTraverseFromIndex() {
+        return traverseFromIndex;
+    }
+
+    public void setTraverseFromIndex(int traverseFromIndex) {
+        this.traverseFromIndex = traverseFromIndex;
+    }
+
+    public int getTraverseToIndex() {
+        return traverseToIndex;
+    }
+
+    public void setTraverseToIndex(int traverseToIndex) {
+        this.traverseToIndex = traverseToIndex;
+    }
+
+    public int getSelectionFromIndex() {
+        return selectionFromIndex;
+    }
+
+    public void setSelectionFromIndex(int selectionFromIndex) {
+        this.selectionFromIndex = selectionFromIndex;
+    }
+
+    public int getSelectionToIndex() {
+        return selectionToIndex;
+    }
+
+    public void setSelectionToIndex(int selectionToIndex) {
+        this.selectionToIndex = selectionToIndex;
+    }
+
+    public String getAnnouncementText() {
+        return announcementText;
+    }
+
+    public void setAnnouncementText(String announcementText) {
+        this.announcementText = announcementText;
+    }
+
+    public int getTypeWindowContentChangedCount() {
+        return typeWindowContentChangedCount;
+    }
+
+    public void setTypeWindowContentChangedCount(int typeWindowContentChangedCount) {
+        this.typeWindowContentChangedCount = typeWindowContentChangedCount;
+    }
+
+    public void incrementWindowContentChangedCount() {
+        this.typeWindowContentChangedCount++;
+    }
+}
---
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/AccessibilityContentShellTestUtils.java
@@ -0,0 +1,121 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.accessibility;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+/**
+ * Helper class with various testing util methods for content shell accessibility tests.
+ */
+public class AccessibilityContentShellTestUtils {
+    /**
+     * Basic interface to define a way to match |AccessibilityNodeInfo| objects based on the
+     * expected value of a given element of type T.
+     * @param <T>               Generic type of the element for matching
+     */
+    public interface AccessibilityNodeInfoMatcher<T> {
+        /**
+         * Method used to check if a node matches the given element requirement.
+         *
+         * @param node              The node to test match with
+         * @param element           The element and type we are matching against
+         * @return                  True/false for whether or not the node is a match
+         */
+        boolean matches(AccessibilityNodeInfo node, T element);
+    }
+
+    // Helper methods for common matching conditions for AccessibilityNodeInfo objects.
+
+    static AccessibilityNodeInfoMatcher<Integer> sInputTypeMatcher =
+            (node, type) -> node.getInputType() == type;
+
+    static AccessibilityNodeInfoMatcher<String> sClassNameMatcher =
+            (node, className) -> node.getClassName().equals(className);
+
+    static AccessibilityNodeInfoMatcher<String> sTextMatcher = (node, text) -> {
+        if (node.getText() == null) return false;
+
+        return node.getText().toString().equals(text);
+    };
+
+    static AccessibilityNodeInfoMatcher<String> sTextOrContentDescriptionMatcher = (node, text) -> {
+        // If there is no content description, rely only on text
+        if (node.getContentDescription() == null) {
+            return text.equals(node.getText());
+        }
+
+        // If there is no text, rely only on content description
+        if (node.getText() == null) {
+            return text.equals(node.getContentDescription());
+        }
+
+        return text.equals(node.getText()) || text.equals(node.getContentDescription());
+    };
+
+    static AccessibilityNodeInfoMatcher<String> sTextVisibleToUserMatcher = (node, text) -> {
+        if (node.getText() == null) return false;
+
+        return node.getText().toString().equals(text) && node.isVisibleToUser();
+    };
+
+    // Helper methods for common AccessibilityEvent tracking conditions.
+
+    public static View.AccessibilityDelegate textIndicesDelegate(
+            AccessibilityContentShellTestData data) {
+        return new View.AccessibilityDelegate() {
+            @Override
+            public boolean onRequestSendAccessibilityEvent(
+                    ViewGroup host, View child, AccessibilityEvent event) {
+                if (event.getEventType()
+                        == AccessibilityEvent.TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY) {
+                    data.setTraverseFromIndex(event.getFromIndex());
+                    data.setTraverseToIndex(event.getToIndex());
+                } else if (event.getEventType()
+                        == AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED) {
+                    data.setSelectionFromIndex(event.getFromIndex());
+                    data.setSelectionToIndex(event.getToIndex());
+                }
+
+                // Return false so that an accessibility event is not actually sent.
+                return false;
+            }
+        };
+    }
+
+    public static View.AccessibilityDelegate announcementDelegate(
+            AccessibilityContentShellTestData data) {
+        return new View.AccessibilityDelegate() {
+            @Override
+            public boolean onRequestSendAccessibilityEvent(
+                    ViewGroup host, View child, AccessibilityEvent event) {
+                if (event.getEventType() == AccessibilityEvent.TYPE_ANNOUNCEMENT) {
+                    data.setAnnouncementText(event.getText().get(0).toString());
+                }
+
+                // Return false so that an accessibility event is not actually sent.
+                return false;
+            }
+        };
+    }
+
+    public static View.AccessibilityDelegate contentChangeDelegate(
+            AccessibilityContentShellTestData data) {
+        return new View.AccessibilityDelegate() {
+            @Override
+            public boolean onRequestSendAccessibilityEvent(
+                    ViewGroup host, View child, AccessibilityEvent event) {
+                if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
+                    data.incrementWindowContentChangedCount();
+                }
+
+                // Return false so that an accessibility event is not actually sent.
+                return false;
+            }
+        };
+    }
+}
