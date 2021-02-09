ac2b1ee6437e74392ea2cecb6c124b362627dce2
David Black <dmblack@google.com>
2021-02-09 03:52:59 +0000

    Deflake holding space welcome banner browsertest.
    The test waited for an element containing inline styles that may have
    already been removed due to a racy condition. The important assertion
    is that the inline styles are in fact removed.
    
    This CL removes the racy check but retains the important assertion.
    
    (cherry picked from commit 092321c34c322db2f0a6047558743be7bf2df143)
    
    Bug: 1172870
    Change-Id: I94a6bbc59566dcb58309c3ef954661cca3574499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679404
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#851410}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683478
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/branch-heads/4389@{#834}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- ui/file_manager/integration_tests/file_manager/holding_space.js
+++ ui/file_manager/integration_tests/file_manager/holding_space.js
@@ -23,12 +23,6 @@ testcase.holdingSpaceWelcomeBannerWithFeatureEnabled = async () => {
   // Open Files app on Downloads.
   const appId = await setupAndWaitUntilReady(RootPath.DOWNLOADS);
 
-  // Check: the holding space welcome banner should be hidden until dynamic
-  // styles are loaded. Note that inline styles are used until dynamic styles
-  // have finished loading.
-  await remoteCall.waitForElement(
-      appId, `.holding-space-welcome[style="height: 0; opacity: 0;"]`);
-
   // Check: the holding space welcome banner should appear. Note that inline
   // styles are removed once dynamic styles have finished loading.
   await remoteCall.waitForElement(
