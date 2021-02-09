5f1576402174237a2f40633bc0a9481b498dd279
Robert Ogden <robertogden@chromium.org>
2021-02-09 01:01:23 +0000

    Add OptGuide Renderer-Side Page Text Agent
    This agent will plug into the ChromeRenderFrameObserver to be passed
    the page text when it is captured.
    
    Bug: 1163244
    Change-Id: If5fa1bf521cb024dab508b0ee73887d47a644e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682738
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852002}

---
+++ components/optimization_guide/content/renderer/page_text_agent.h
@@ -0,0 +1,76 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_RENDERER_PAGE_TEXT_AGENT_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_RENDERER_PAGE_TEXT_AGENT_H_
+
+#include <stdint.h>
+#include <map>
+#include <utility>
+
+#include "base/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "base/strings/string16.h"
+#include "components/optimization_guide/content/mojom/page_text_service.mojom.h"
+#include "mojo/public/cpp/bindings/associated_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "third_party/blink/public/web/web_meaningful_layout.h"
+
+namespace content {
+class RenderFrame;
+}  // namespace content
+
+namespace optimization_guide {
+
+// PageTextAgent is the interface between ChromeRenderFrameObserver and
+// mojom::PageTextService. It currently supports requesting and getting text
+// dumps during |content::RenderFrameObserver::DidMeaningfulLayout|, but more
+// events will be added in the future.
+class PageTextAgent : public mojom::PageTextService {
+ public:
+  explicit PageTextAgent(content::RenderFrame* frame);
+  ~PageTextAgent() override;
+
+  // This should be called during |DidMeaningfulLayout| to determine whether
+  // this class would like to get a page dump. If so, the returned callback
+  // should be ran with the text, and |max_size| will be updated to a bigger
+  // value iff this class wants more text than that.
+  base::OnceCallback<void(const base::string16&)>
+  MaybeRequestTextDumpOnLayoutEvent(blink::WebMeaningfulLayout event,
+                                    uint64_t* max_size);
+
+  // mojom::PageTextService:
+  void RequestPageTextDump(
+      mojom::PageTextDumpRequestPtr request,
+      mojo::PendingRemote<mojom::PageTextConsumer> consumer) override;
+
+  PageTextAgent(const PageTextAgent&) = delete;
+  PageTextAgent& operator=(const PageTextAgent&) = delete;
+
+ private:
+  // Bind to mojo pipes.
+  void Bind(mojo::PendingAssociatedReceiver<mojom::PageTextService> receiver);
+
+  // Called when the text dump is done and it can be sent to |consumer|.
+  void OnPageTextDump(mojo::PendingRemote<mojom::PageTextConsumer> consumer,
+                      const base::string16& content);
+
+  // Keeps track of the text dump events that have been requested. Entries are
+  // only present between being added in |RequestPageTextDump| and
+  // |MaybeRequestTextDumpOnLayoutEvent| where relevant data to make the
+  // response is moved into the callback.
+  using RequestAndConsumer =
+      std::pair<mojom::PageTextDumpRequestPtr,
+                mojo::PendingRemote<mojom::PageTextConsumer>>;
+  std::map<mojom::TextDumpEvent, RequestAndConsumer> requests_by_event_;
+
+  mojo::AssociatedReceiver<mojom::PageTextService> receiver_{this};
+
+  base::WeakPtrFactory<PageTextAgent> weak_factory_{this};
+};
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_RENDERER_PAGE_TEXT_AGENT_H_
