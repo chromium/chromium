chromium/chromium

commit 7776b460208fc76214d387693f00a9d2970d7b21

[Extensions] Make CSSOrigin non-optional in ExecuteCode_Params
Instead of having CSSOrigin be optional down to the injection site (in
the renderer), have it be non-optional throughout the pipeline and set
at the instantiation in the API. This is a little more direct and makes
the default easier to see.

Bug: None
Change-Id: I9fff73c2c92518ea63eb186bc6a613f456913e61
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654069
Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
Reviewed-by: Dominick Ng <dominickn@chromium.org>
Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
Cr-Commit-Position: refs/heads/master@{#849901}

extensions/renderer/user_script_injector.cc
@@ -140,8 +140,8 @@ bool UserScriptInjector::ExpectsResults() const {
   return false;
 }
 
-base::Optional<CSSOrigin> UserScriptInjector::GetCssOrigin() const {
-  return base::nullopt;
+CSSOrigin UserScriptInjector::GetCssOrigin() const {
+  return CSS_ORIGIN_USER;
 }
 
 bool UserScriptInjector::IsRemovingCSS() const {

chrome/browser/extensions/api/scripting/scripting_api.cc
@@ -259,7 +259,7 @@ bool ScriptingExecuteScriptFunction::Execute(std::string code_to_execute,
       ScriptExecutor::MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
       ScriptExecutor::DEFAULT_PROCESS,
       /* webview_src */ GURL(), std::move(script_url), user_gesture(),
-      base::nullopt, ScriptExecutor::JSON_SERIALIZED_RESULT,
+      CSS_ORIGIN_AUTHOR, ScriptExecutor::JSON_SERIALIZED_RESULT,
       base::BindOnce(&ScriptingExecuteScriptFunction::OnScriptExecuted, this));
 
   return true;
@@ -369,18 +369,14 @@ bool ScriptingInsertCSSFunction::Execute(std::string code_to_execute,
   }
   DCHECK(script_executor);
 
-  // TODO(devlin): Pull the default argument for CSSOrigin up to here, and
-  // pass it through ScriptExecutor and friends, rather than having it
-  // defined in the renderer.
-  base::Optional<CSSOrigin> origin;
+  CSSOrigin origin = CSS_ORIGIN_AUTHOR;
   switch (injection_.origin) {
-    case api::scripting::STYLE_ORIGIN_USER:
-      origin = CSS_ORIGIN_USER;
-      break;
+    case api::scripting::STYLE_ORIGIN_NONE:
     case api::scripting::STYLE_ORIGIN_AUTHOR:
       origin = CSS_ORIGIN_AUTHOR;
       break;
-    case api::scripting::STYLE_ORIGIN_NONE:
+    case api::scripting::STYLE_ORIGIN_USER:
+      origin = CSS_ORIGIN_USER;
       break;
   }
 

chrome/browser/extensions/script_executor_browsertest.cc
@@ -129,9 +129,8 @@ IN_PROC_BROWSER_TEST_F(ScriptExecutorBrowserTest, MainFrameExecution) {
       {ExtensionApiFrameIdMap::kTopFrameId},
       ScriptExecutor::DONT_MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
       ScriptExecutor::DEFAULT_PROCESS, GURL() /* webview_src */,
-      GURL() /* script_url */, false /* user_gesture */,
-      base::nullopt /* css_origin */, ScriptExecutor::JSON_SERIALIZED_RESULT,
-      helper.GetCallback());
+      GURL() /* script_url */, false /* user_gesture */, CSS_ORIGIN_AUTHOR,
+      ScriptExecutor::JSON_SERIALIZED_RESULT, helper.GetCallback());
   helper.Wait();
   EXPECT_EQ("New Title", base::UTF16ToUTF8(web_contents->GetTitle()));
 
@@ -210,9 +209,8 @@ IN_PROC_BROWSER_TEST_F(ScriptExecutorBrowserTest, SpecifiedFrames) {
         kCode, ScriptExecutor::SPECIFIED_FRAMES, {frame1_id, frame2_id},
         ScriptExecutor::DONT_MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
         ScriptExecutor::DEFAULT_PROCESS, GURL() /* webview_src */,
-        GURL() /* script_url */, false /* user_gesture */,
-        base::nullopt /* css_origin */, ScriptExecutor::JSON_SERIALIZED_RESULT,
-        helper.GetCallback());
+        GURL() /* script_url */, false /* user_gesture */, CSS_ORIGIN_AUTHOR,
+        ScriptExecutor::JSON_SERIALIZED_RESULT, helper.GetCallback());
     helper.Wait();
 
     EXPECT_THAT(helper.results(),
@@ -230,9 +228,8 @@ IN_PROC_BROWSER_TEST_F(ScriptExecutorBrowserTest, SpecifiedFrames) {
         kCode, ScriptExecutor::INCLUDE_SUB_FRAMES, {frame1_id, frame2_id},
         ScriptExecutor::DONT_MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
         ScriptExecutor::DEFAULT_PROCESS, GURL() /* webview_src */,
-        GURL() /* script_url */, false /* user_gesture */,
-        base::nullopt /* css_origin */, ScriptExecutor::JSON_SERIALIZED_RESULT,
-        helper.GetCallback());
+        GURL() /* script_url */, false /* user_gesture */, CSS_ORIGIN_AUTHOR,
+        ScriptExecutor::JSON_SERIALIZED_RESULT, helper.GetCallback());
     helper.Wait();
 
     EXPECT_THAT(helper.results(),
@@ -260,9 +257,8 @@ IN_PROC_BROWSER_TEST_F(ScriptExecutorBrowserTest, SpecifiedFrames) {
         {frame1_id, frame2_id, kNonExistentFrameId},
         ScriptExecutor::DONT_MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
         ScriptExecutor::DEFAULT_PROCESS, GURL() /* webview_src */,
-        GURL() /* script_url */, false /* user_gesture */,
-        base::nullopt /* css_origin */, ScriptExecutor::JSON_SERIALIZED_RESULT,
-        helper.GetCallback());
+        GURL() /* script_url */, false /* user_gesture */, CSS_ORIGIN_AUTHOR,
+        ScriptExecutor::JSON_SERIALIZED_RESULT, helper.GetCallback());
     helper.Wait();
 
     base::Value frame1_result("Frame 1");
@@ -283,9 +279,8 @@ IN_PROC_BROWSER_TEST_F(ScriptExecutorBrowserTest, SpecifiedFrames) {
         kCode, ScriptExecutor::SPECIFIED_FRAMES, {kNonExistentFrameId},
         ScriptExecutor::DONT_MATCH_ABOUT_BLANK, UserScript::DOCUMENT_IDLE,
         ScriptExecutor::DEFAULT_PROCESS, GURL() /* webview_src */,
-        GURL() /* script_url */, false /* user_gesture */,
-        base::nullopt /* css_origin */, ScriptExecutor::JSON_SERIALIZED_RESULT,
-        helper.GetCallback());
+        GURL() /* script_url */, false /* user_gesture */, CSS_ORIGIN_AUTHOR,
+        ScriptExecutor::JSON_SERIALIZED_RESULT, helper.GetCallback());
     helper.Wait();
 
     EXPECT_THAT(helper.results(),

extensions/browser/api/execute_code_function.cc
@@ -113,11 +113,16 @@ bool ExecuteCodeFunction::Execute(const std::string& code_string,
   }
   CHECK_NE(UserScript::UNDEFINED, run_at);
 
-  base::Optional<CSSOrigin> css_origin;
-  if (details_->css_origin == api::extension_types::CSS_ORIGIN_USER)
-    css_origin = CSS_ORIGIN_USER;
-  else if (details_->css_origin == api::extension_types::CSS_ORIGIN_AUTHOR)
-    css_origin = CSS_ORIGIN_AUTHOR;
+  CSSOrigin css_origin = CSS_ORIGIN_AUTHOR;
+  switch (details_->css_origin) {
+    case api::extension_types::CSS_ORIGIN_NONE:
+    case api::extension_types::CSS_ORIGIN_AUTHOR:
+      css_origin = CSS_ORIGIN_AUTHOR;
+      break;
+    case api::extension_types::CSS_ORIGIN_USER:
+      css_origin = CSS_ORIGIN_USER;
+      break;
+  }
 
   executor->ExecuteScript(
       host_id_, action_type, code_string, frame_scope, {root_frame_id_},

extensions/browser/script_executor.cc
@@ -300,7 +300,7 @@ void ScriptExecutor::ExecuteScript(const HostID& host_id,
                                    const GURL& webview_src,
                                    const GURL& script_url,
                                    bool user_gesture,
-                                   base::Optional<CSSOrigin> css_origin,
+                                   CSSOrigin css_origin,
                                    ScriptExecutor::ResultType result_type,
                                    ScriptFinishedCallback callback) {
   if (host_id.type() == HostID::EXTENSIONS) {

extensions/browser/script_executor.h
@@ -116,7 +116,7 @@ class ScriptExecutor {
                      const GURL& webview_src,
                      const GURL& script_url,
                      bool user_gesture,
-                     base::Optional<CSSOrigin> css_origin,
+                     CSSOrigin css_origin,
                      ResultType result_type,
                      ScriptFinishedCallback callback);
 

extensions/common/extension_messages.h
@@ -199,7 +199,7 @@ IPC_STRUCT_BEGIN(ExtensionMsg_ExecuteCode_Params)
   IPC_STRUCT_MEMBER(bool, user_gesture)
 
   // The origin of the CSS.
-  IPC_STRUCT_MEMBER(base::Optional<extensions::CSSOrigin>, css_origin)
+  IPC_STRUCT_MEMBER(extensions::CSSOrigin, css_origin)
 
   // The autogenerated key for the CSS injection.
   IPC_STRUCT_MEMBER(base::Optional<std::string>, injection_key)

extensions/renderer/programmatic_script_injector.cc
@@ -44,7 +44,7 @@ bool ProgrammaticScriptInjector::IsUserGesture() const {
   return params_->user_gesture;
 }
 
-base::Optional<CSSOrigin> ProgrammaticScriptInjector::GetCssOrigin() const {
+CSSOrigin ProgrammaticScriptInjector::GetCssOrigin() const {
   return params_->css_origin;
 }
 

commit 4a3af14b05c9488b86e69d9048510ed008abbc99

Reland "[Extensions] Fix Style Origin for extension-injected stylesheets"
This reverts commit 1995425b8a67080ca28b109b2c25b8f81be27fdd.

Reason for revert: The original revert did not fix the failing test, thus relanding.

Original change's description:
> Revert "[Extensions] Fix Style Origin for extension-injected stylesheets"
>
> This reverts commit e2c4d3aa6deb39e337e4c17d460447e639f99a88.
>
> Reason for revert: Speculative revert as this might be causing consistent
> ExtensionsMenuViewBrowserTest.PinningDisabledInIncognito test failures.
> Sample first failed run:
> https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests/60440
>
> Will reland if the revert does not fix the bot.
>
> Original change's description:
> > [Extensions] Fix Style Origin for extension-injected stylesheets
> >
> > https://crrev.com/7776b460 inadvertently changed the style origin for
> > extension-injected stylesheets to be "user" rather than "author". This
> > meant that they would no longer override stylesheets specified by the
> > webpage if they had more specific rules.
> >
> > Fix this by setting the style origin back to "author" (as it was
> > previously), and adding a regression test.
> >
> > Bug: 1175506
> > Change-Id: Ic799eba52dc0981899b78bade6f4cb46c6ed610e
> > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683052
> > Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
> > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
> > Cr-Commit-Position: refs/heads/master@{#852434}
>
> TBR=rdevlin.cronin@chromium.org,karandeepb@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
>
> Change-Id: Ie6bdb4146e309d20230637100c666f8e54f0fad2
> No-Presubmit: true
> No-Tree-Checks: true
> No-Try: true
> Bug: 1175506
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687095
> Reviewed-by: Guido Urdaneta <guidou@chromium.org>
> Commit-Queue: Guido Urdaneta <guidou@chromium.org>

chrome/browser/extensions/content_script_apitest.cc
@@ -62,52 +62,6 @@ namespace {
 // A fake webstore domain.
 const char kWebstoreDomain[] = "cws.com";
 
-// Check whether or not style was injected, with |expected_injection| indicating
-// the expected result. Also ensure that no CSS was added to the
-// document.styleSheets array.
-testing::AssertionResult CheckStyleInjection(Browser* browser,
-                                             const GURL& url,
-                                             bool expected_injection) {
-  ui_test_utils::NavigateToURL(browser, url);
-
-  bool css_injected = false;
-  if (!content::ExecuteScriptAndExtractBool(
-          browser->tab_strip_model()->GetActiveWebContents(),
-          "window.domAutomationController.send("
-          "    document.defaultView.getComputedStyle(document.body, null)."
-          "        getPropertyValue('display') == 'none');",
-          &css_injected)) {
-    return testing::AssertionFailure()
-           << "Failed to execute script and extract bool for injection status.";
-  }
-
-  if (css_injected != expected_injection) {
-    std::string message;
-    if (css_injected)
-      message = "CSS injected when no injection was expected.";
-    else
-      message = "CSS not injected when injection was expected.";
-    return testing::AssertionFailure() << message;
-  }
-
-  bool css_doesnt_add_to_list = false;
-  if (!content::ExecuteScriptAndExtractBool(
-          browser->tab_strip_model()->GetActiveWebContents(),
-          "window.domAutomationController.send("
-          "    document.styleSheets.length == 0);",
-          &css_doesnt_add_to_list)) {
-    return testing::AssertionFailure()
-           << "Failed to execute script and extract bool for stylesheets "
-              "length.";
-  }
-  if (!css_doesnt_add_to_list) {
-    return testing::AssertionFailure()
-           << "CSS injection added to number of stylesheets.";
-  }
-
-  return testing::AssertionSuccess();
-}
-
 // Runs all pending tasks in the renderer associated with |web_contents|, and
 // then all pending tasks in the browser process.
 // Returns true on success.
@@ -429,19 +383,65 @@ class ContentScriptCssInjectionTest : public ExtensionApiTest {
 IN_PROC_BROWSER_TEST_F(ContentScriptCssInjectionTest,
                        ContentScriptInjectsStyles) {
   ASSERT_TRUE(StartEmbeddedTestServer());
-
   ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("content_scripts")
                                 .AppendASCII("css_injection")));
 
-  // CSS injection should be allowed on an aribitrary web page.
+  // Helper to get the active tab from the browser.
+  auto get_active_tab = [browser = browser()]() {
+    return browser->tab_strip_model()->GetActiveWebContents();
+  };
+  // Returns the background color for the element retrieved from the given
+  // `query_selector`.
+  auto get_element_color =
+      [&get_active_tab](const char* query_selector) -> std::string {
+    content::WebContents* web_contents = get_active_tab();
+    SCOPED_TRACE(base::StringPrintf(
+        "URL: %s; Selector: %s",
+        web_contents->GetLastCommittedURL().spec().c_str(), query_selector));
+    std::string color;
+    constexpr char kGetColor[] =
+        R"((function() {
+             let element = document.querySelector('%s');
+             style = window.getComputedStyle(element);
+             domAutomationController.send(style.backgroundColor);
+            })();)";
+    if (!content::ExecuteScriptAndExtractString(
+            get_active_tab(), base::StringPrintf(kGetColor, query_selector),
+            &color)) {
+      return "<Failed to execute>";
+    }
+
+    return color;
+  };
+  // Returns the number of stylesheets attached to the document.
+  auto get_style_sheet_count = [&get_active_tab]() {
+    int count = -1;
+    constexpr char kGetStyleSheetCount[] =
+        "domAutomationController.send(document.styleSheets.length);";
+    if (!content::ExecuteScriptAndExtractInt(get_active_tab(),
+                                             kGetStyleSheetCount, &count)) {
+      return -1;
+    }
+    return count;
+  };
+
+  // CSS injection should be allowed on an unprivileged web page that matches
+  // the patterns specified for the content script.
   GURL url =
       embedded_test_server()->GetURL("/extensions/test_file_with_body.html");
-  EXPECT_TRUE(CheckStyleInjection(browser(), url, true));
+  ui_test_utils::NavigateToURL(browser(), url);
+  constexpr char kInjectedBodyColor[] = "rgb(0, 0, 255)";  // Blue
+  EXPECT_EQ(kInjectedBodyColor, get_element_color("body"));
+  EXPECT_EQ(0, get_style_sheet_count())
+      << "Extension-injected content scripts should not be included in "
+      << "document.styleSheets.";
 
   // The loaded extension has an exclude match for "extensions/test_file.html",
   // so no CSS should be injected.
   url = embedded_test_server()->GetURL("/extensions/test_file.html");
-  EXPECT_TRUE(CheckStyleInjection(browser(), url, false));
+  ui_test_utils::NavigateToURL(browser(), url);
+  EXPECT_NE(kInjectedBodyColor, get_element_color("body"));
+  EXPECT_EQ(0, get_style_sheet_count());
 
   // We disallow all injection on the webstore.
   GURL::Replacements replacements;
@@ -449,7 +449,48 @@ IN_PROC_BROWSER_TEST_F(ContentScriptCssInjectionTest,
   url = embedded_test_server()
             ->GetURL("/extensions/test_file_with_body.html")
             .ReplaceComponents(replacements);
-  EXPECT_TRUE(CheckStyleInjection(browser(), url, false));
+  ui_test_utils::NavigateToURL(browser(), url);
+  EXPECT_NE(kInjectedBodyColor, get_element_color("body"));
+  EXPECT_EQ(0, get_style_sheet_count());
+
+  // Check extensions override page styles if they have more specific rules.
+  // Regression test for https://crbug.com/1175506.
+  // This page has four divs (with ids div1, div2, div3, and div4). The page
+  // specifies styles for them, but the extension has more specific styles for
+  // divs 1, 2, and 3.
+  // The extension styles should win by specificity, since they are in the same
+  // style origin ("author").
+  url = embedded_test_server()->GetURL("/extensions/test_file_with_style.html");
+  ui_test_utils::NavigateToURL(browser(), url);
+  constexpr char kInjectedDivColor[] = "rgb(0, 0, 255)";  // Blue
+  constexpr char kOriginalDivColor[] = "rgb(255, 0, 0)";  // Red
+  EXPECT_EQ(kInjectedDivColor, get_element_color("#div1"));
+  EXPECT_EQ(kInjectedDivColor, get_element_color("#div2"));
+  EXPECT_EQ(kInjectedDivColor, get_element_color("#div3"));
+  EXPECT_EQ(kOriginalDivColor, get_element_color("#div4"));
+  // There should be two style sheets on this website; one inline <style> tag
+  // and a second included as a <link>.
+  EXPECT_EQ(2, get_style_sheet_count());
+
+  // Load an additional stylesheet dynamically (ensuring it was added to the DOM
+  // later). div3 should still be styled by the extension (since that rule is
+  // more specific). This ensures that stylesheets that just happen to be added
+  // later don't override extension sheets of higher specificity.
+  constexpr char kLoadExtraStylesheet[] =
+      R"((function() {
+           let sheet = document.createElement('link');
+           sheet.type = 'text/css';
+           sheet.rel = 'stylesheet';
+           sheet.href = 'test_file_with_style2.css';
+           sheet.onload = () => { domAutomationController.send('success'); };
+           sheet.onerror = () => { domAutomationController.send('error'); };
+           document.head.appendChild(sheet);
+         })();)";
+  std::string result;
+  ASSERT_TRUE(content::ExecuteScriptAndExtractString(
+      get_active_tab(), kLoadExtraStylesheet, &result));
+  EXPECT_EQ("success", result);
+  EXPECT_EQ(kInjectedDivColor, get_element_color("#div3"));
 }
 
 IN_PROC_BROWSER_TEST_F(ContentScriptApiTest, ContentScriptCSSLocalization) {

extensions/renderer/user_script_injector.cc
@@ -141,7 +141,7 @@ bool UserScriptInjector::ExpectsResults() const {
 }
 
 CSSOrigin UserScriptInjector::GetCssOrigin() const {
-  return CSS_ORIGIN_USER;
+  return CSS_ORIGIN_AUTHOR;
 }
 
 bool UserScriptInjector::IsRemovingCSS() const {

chrome/test/data/extensions/api_test/content_scripts/css_injection/content_styles.css
@@ -3,5 +3,17 @@
  * found in the LICENSE file. */
 
 body {
-  display: none;
+  background-color: blue;
+}
+
+body #div1 {
+  background-color: blue;
+}
+
+body #div2 {
+  background-color: blue;
+}
+
+body #div3 {
+  background-color: blue;
 }

chrome/test/data/extensions/test_file_with_style.css
@@ -1,0 +1,7 @@
+/* Copyright 2021 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+#div2 {
+  background-color: red;
+}

chrome/test/data/extensions/test_file_with_style.html
@@ -1,0 +1,20 @@
+<!doctype html>
+<html>
+  <head>
+    <style>
+      div {
+        background-color: red;
+      }
+      #div1 {
+        background-color: red;
+      }
+    </style>
+    <link href="test_file_with_style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="div1">DIV 1</div>
+    <div id="div2">DIV 2</div>
+    <div id="div3">DIV 3</div>
+    <div id="div4">DIV 4</div>
+  </body>
+</html>

chrome/test/data/extensions/test_file_with_style2.css
@@ -1,0 +1,7 @@
+/* Copyright 2021 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+#div3 {
+  background-color: red;
+}