e6698f21a5ff109c619c5e9839af2b9e8d5aa2e8
Taylor Bergquist <tbergquist@chromium.org>
2021-02-10 07:27:00 +0000

    Fix RevertDragAt losing track of tabs in some cases.
    (cherry picked from commit c0b715ae52a2966f2baf49483b613a6c3c164246)
    
    Bug: 1175500
    Change-Id: I9addf8bd76c38d647a8009cd6805be9af48ba1b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682744
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#851880}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686099
    Auto-Submit: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4389@{#882}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -1707,7 +1707,7 @@ void TabDragController::RevertDragAt(size_t drag_index) {
             ++target_index;
         }
       }
-      source_context_->GetTabStripModel()->MoveWebContentsAt(
+      target_index = source_context_->GetTabStripModel()->MoveWebContentsAt(
           index, target_index, false);
     }
   } else {
