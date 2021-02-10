3b9dd5230093e08d65139cdcde3a6cc9272c86bb
Sophie Chang <sophiechang@chromium.org>
2021-02-10 17:17:54 +0000

    Add model execution library that can be used by feature teams that depend on the Optimization Guide for serving TFLite models
    This currently only contains the generic case and future cases around
    NL will come in subsequent CLs.
    
    Change-Id: I0277564ad6db4c347a7628ba3c797301e832292a
    Bug: 1173328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645639
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852685}

---
+++ components/optimization_guide/content/browser/optimization_target_model_executor.h
@@ -0,0 +1,159 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_OPTIMIZATION_TARGET_MODEL_EXECUTOR_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_OPTIMIZATION_TARGET_MODEL_EXECUTOR_H_
+
+#include "base/bind.h"
+#include "base/callback_forward.h"
+#include "base/files/memory_mapped_file.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "components/optimization_guide/content/browser/optimization_guide_decider.h"
+#include "components/optimization_guide/core/optimization_target_model_observer.h"
+#include "content/public/browser/browser_thread.h"
+#include "third_party/tflite-support/src/tensorflow_lite_support/cc/task/core/base_task_api.h"
+#include "third_party/tflite/src/tensorflow/lite/c/common.h"
+
+namespace optimization_guide {
+
+template <class OutputType, class... InputTypes>
+class OptimizationTargetModelExecutor : public OptimizationTargetModelObserver {
+ public:
+  using ModelExecutionTask =
+      tflite::task::core::BaseTaskApi<OutputType, InputTypes...>;
+
+  OptimizationTargetModelExecutor(
+      OptimizationGuideDecider* decider,
+      proto::OptimizationTarget optimization_target,
+      const base::Optional<proto::Any>& model_metadata,
+      const scoped_refptr<base::SequencedTaskRunner>&
+          model_execution_task_runner)
+      : decider_(decider),
+        optimization_target_(optimization_target),
+        model_execution_task_runner_(model_execution_task_runner) {
+    DCHECK(decider_);
+    DCHECK_NE(optimization_target_,
+              proto::OptimizationTarget::OPTIMIZATION_TARGET_UNKNOWN);
+
+    decider_->AddObserverForOptimizationTargetModel(optimization_target_,
+                                                    model_metadata, this);
+  }
+  ~OptimizationTargetModelExecutor() override {
+    decider_->RemoveObserverForOptimizationTargetModel(optimization_target_,
+                                                       this);
+  }
+  OptimizationTargetModelExecutor(const OptimizationTargetModelExecutor&) =
+      delete;
+  OptimizationTargetModelExecutor& operator=(
+      const OptimizationTargetModelExecutor&) = delete;
+
+  // Executes the model using |input| and invokes |callback| when completed.
+  // TODO(crbug/1173328): Add a way to surface errors.
+  using ExecutionCallback =
+      base::OnceCallback<void(base::Optional<OutputType>)>;
+  void ExecuteModelWithInput(ExecutionCallback callback, InputTypes... input) {
+    // base::Unretained is safe here since the execution will not run if
+    // |model_execution_task_runner_| gets destructed.
+    model_execution_task_runner_->PostTaskAndReplyWithResult(
+        FROM_HERE,
+        base::BindOnce(&OptimizationTargetModelExecutor::SendForExecution,
+                       base::Unretained(this), input...),
+        base::BindOnce(&OptimizationTargetModelExecutor::OnExecutionCompleted,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  // OptimizationTargetModelObserver:
+  void OnModelFileUpdated(proto::OptimizationTarget optimization_target,
+                          const base::Optional<proto::Any>& model_metadata,
+                          const base::FilePath& file_path) override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+    if (optimization_target_ != optimization_target)
+      return;
+
+    // base::Unretained is safe here since model loading will not run if
+    // |model_execution_task_runner_| gets destructed.
+    model_execution_task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(&OptimizationTargetModelExecutor::LoadModelFile,
+                       base::Unretained(this), model_metadata, file_path));
+  }
+
+  // Returns whether a model is currently loaded.
+  bool HasLoadedModel() const { return loaded_model_ != nullptr; }
+
+  // Returns the supported features for the loaded model, if the server provided
+  // any.
+  base::Optional<proto::Any> supported_features_for_loaded_model() const {
+    return supported_features_for_loaded_model_;
+  }
+
+ protected:
+  // Executes the model using |execution_task| on |args|.
+  virtual base::Optional<OutputType> Execute(ModelExecutionTask* execution_task,
+                                             InputTypes... args) = 0;
+
+  // Builds a model execution task using |model_file|.
+  virtual std::unique_ptr<ModelExecutionTask> BuildModelExecutionTask(
+      base::MemoryMappedFile* model_file) = 0;
+
+ private:
+  // Callback invoked when a model file for |optimization_target| has been
+  // loaded.
+  void LoadModelFile(const base::Optional<proto::Any>& model_metadata,
+                     const base::FilePath& file_path) {
+    DCHECK(model_execution_task_runner_->RunsTasksInCurrentSequence());
+
+    // We received a new model file. Reset any loaded models.
+    loaded_model_.reset();
+    model_fb_.reset();
+    supported_features_for_loaded_model_ = base::nullopt;
+
+    std::unique_ptr<base::MemoryMappedFile> model_fb =
+        std::make_unique<base::MemoryMappedFile>();
+    if (!model_fb->Initialize(file_path))
+      return;
+    model_fb_ = std::move(model_fb);
+
+    supported_features_for_loaded_model_ = model_metadata;
+
+    loaded_model_ = BuildModelExecutionTask(model_fb_.get());
+  }
+
+  base::Optional<OutputType> SendForExecution(InputTypes... args) {
+    DCHECK(model_execution_task_runner_->RunsTasksInCurrentSequence());
+
+    if (!loaded_model_)
+      return base::nullopt;
+
+    return Execute(loaded_model_.get(), args...);
+  }
+
+  void OnExecutionCompleted(ExecutionCallback callback,
+                            base::Optional<OutputType> output) {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+    std::move(callback).Run(output);
+  }
+
+  // Not owned. Guaranteed to outlive |this|.
+  OptimizationGuideDecider* decider_;
+
+  proto::OptimizationTarget optimization_target_;
+
+  scoped_refptr<base::SequencedTaskRunner> model_execution_task_runner_;
+
+  std::unique_ptr<base::MemoryMappedFile> model_fb_;
+  std::unique_ptr<ModelExecutionTask> loaded_model_;
+  base::Optional<proto::Any> supported_features_for_loaded_model_;
+
+  base::WeakPtrFactory<OptimizationTargetModelExecutor> weak_ptr_factory_{this};
+};
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_OPTIMIZATION_TARGET_MODEL_EXECUTOR_H_
---
+++ components/optimization_guide/content/browser/optimization_target_model_executor_unittest.cc
@@ -0,0 +1,231 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/content/browser/optimization_target_model_executor.h"
+
+#include "base/path_service.h"
+#include "components/optimization_guide/content/browser/base_model_executor.h"
+#include "components/optimization_guide/content/browser/test_optimization_guide_decider.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace optimization_guide {
+
+class TestModelExecutor
+    : public BaseModelExecutor<std::vector<float>, const std::vector<float>&> {
+ public:
+  // Feature team specifies their target and the features they support along
+  // with a background task runner that has the appropriate properties for
+  // executing models.
+  TestModelExecutor(OptimizationGuideDecider* decider,
+                    const scoped_refptr<base::SequencedTaskRunner>& task_runner)
+      : BaseModelExecutor<std::vector<float>, const std::vector<float>&>(
+            decider,
+            proto::OptimizationTarget::OPTIMIZATION_TARGET_PAINFUL_PAGE_LOAD,
+            /*model_metadata=*/base::nullopt,
+            task_runner) {}
+  ~TestModelExecutor() override = default;
+  TestModelExecutor(const TestModelExecutor&) = delete;
+  TestModelExecutor& operator=(const TestModelExecutor&) = delete;
+
+  // There is a method on the base class that exposes the returned supported
+  // features, if provided by the loaded model received from the server.
+  // base::Optional<proto::Any> supported_features_for_loaded_model();
+
+ protected:
+  void Preprocess(const std::vector<TfLiteTensor*>& input_tensors,
+                  const std::vector<float>& input) override {
+    // TODO(crbug/1176459): Use PopulateTensor instead.
+
+    float* tensor = reinterpret_cast<float*>(input_tensors[0]->data.raw);
+    size_t bytes = input.size() * sizeof(float);
+    memcpy(tensor, input.data(), bytes);
+  }
+
+  std::vector<float> Postprocess(
+      const std::vector<const TfLiteTensor*>& output_tensors) override {
+    // TODO(crbug/1176459): Use PopulateVector instead.
+
+    const TfLiteTensor* tensor = output_tensors[0];
+    const float* results = reinterpret_cast<const float*>(tensor->data.raw);
+    size_t num = tensor->bytes / sizeof(tensor->type);
+
+    std::vector<float> data;
+    data.reserve(num);
+    for (size_t i = 0; i < num; i++) {
+      data.emplace_back(results[i]);
+    }
+    return data;
+  }
+};
+
+class ModelObserverTracker : public TestOptimizationGuideDecider {
+ public:
+  void AddObserverForOptimizationTargetModel(
+      proto::OptimizationTarget target,
+      const base::Optional<proto::Any>& model_metadata,
+      OptimizationTargetModelObserver* observer) override {
+    // Make sure we send what is expected based on
+    // TestModelExecutor ctor.
+    if (target !=
+        proto::OptimizationTarget::OPTIMIZATION_TARGET_PAINFUL_PAGE_LOAD) {
+      return;
+    }
+    if (model_metadata != base::nullopt)
+      return;
+
+    add_observer_called_ = true;
+  }
+
+  bool add_observer_called() const { return add_observer_called_; }
+
+  void RemoveObserverForOptimizationTargetModel(
+      proto::OptimizationTarget target,
+      OptimizationTargetModelObserver* observer) override {
+    if (target !=
+        proto::OptimizationTarget::OPTIMIZATION_TARGET_PAINFUL_PAGE_LOAD) {
+      return;
+    }
+    remove_observer_called_ = true;
+  }
+
+  bool remove_observer_called() const { return remove_observer_called_; }
+
+ private:
+  bool add_observer_called_ = false;
+  bool remove_observer_called_ = false;
+};
+
+class OptimizationTargetModelExecutorTest : public testing::Test {
+ public:
+  OptimizationTargetModelExecutorTest() = default;
+  ~OptimizationTargetModelExecutorTest() override = default;
+
+  void SetUp() override {
+    base::FilePath source_root_dir;
+    base::PathService::Get(base::DIR_SOURCE_ROOT, &source_root_dir);
+    model_file_path_ = source_root_dir.AppendASCII("components")
+                           .AppendASCII("test")
+                           .AppendASCII("data")
+                           .AppendASCII("optimization_guide")
+                           .AppendASCII("simple_test.tflite");
+
+    model_observer_tracker_ = std::make_unique<ModelObserverTracker>();
+  }
+
+  void TearDown() override { model_executor_.reset(); }
+
+  void CreateModelExecutor() {
+    if (model_executor_)
+      model_executor_.reset();
+
+    model_executor_ = std::make_unique<TestModelExecutor>(
+        model_observer_tracker_.get(),
+        task_environment_.GetMainThreadTaskRunner());
+  }
+
+  void ResetModelExecutor() { model_executor_.reset(); }
+
+  void PushModelFileToModelExecutor(
+      proto::OptimizationTarget optimization_target) {
+    DCHECK(model_executor_);
+    model_executor_->OnModelFileUpdated(optimization_target, base::nullopt,
+                                        model_file_path_);
+    RunUntilIdle();
+  }
+
+  TestModelExecutor* model_executor() { return model_executor_.get(); }
+
+  ModelObserverTracker* model_observer_tracker() {
+    return model_observer_tracker_.get();
+  }
+
+  void RunUntilIdle() { task_environment_.RunUntilIdle(); }
+
+ private:
+  content::BrowserTaskEnvironment task_environment_;
+
+  base::FilePath model_file_path_;
+  std::unique_ptr<ModelObserverTracker> model_observer_tracker_;
+
+  std::unique_ptr<TestModelExecutor> model_executor_;
+};
+
+TEST_F(OptimizationTargetModelExecutorTest, ObserverIsAttachedCorrectly) {
+  CreateModelExecutor();
+  EXPECT_TRUE(model_observer_tracker()->add_observer_called());
+
+  ResetModelExecutor();
+  EXPECT_TRUE(model_observer_tracker()->remove_observer_called());
+}
+
+TEST_F(OptimizationTargetModelExecutorTest, ModelFileUpdatedWrongTarget) {
+  CreateModelExecutor();
+
+  PushModelFileToModelExecutor(
+      proto::OptimizationTarget::OPTIMIZATION_TARGET_LANGUAGE_DETECTION);
+
+  EXPECT_FALSE(model_executor()->HasLoadedModel());
+}
+
+TEST_F(OptimizationTargetModelExecutorTest, ModelFileUpdatedCorrectTarget) {
+  CreateModelExecutor();
+
+  PushModelFileToModelExecutor(
+      proto::OptimizationTarget::OPTIMIZATION_TARGET_PAINFUL_PAGE_LOAD);
+
+  EXPECT_TRUE(model_executor()->HasLoadedModel());
+}
+
+TEST_F(OptimizationTargetModelExecutorTest,
+       ExecuteReturnsImmediatelyIfNoModelLoaded) {
+  CreateModelExecutor();
+
+  std::unique_ptr<base::RunLoop> run_loop = std::make_unique<base::RunLoop>();
+  model_executor()->ExecuteModelWithInput(
+      base::BindOnce(
+          [](base::RunLoop* run_loop,
+             base::Optional<std::vector<float>> output) {
+            EXPECT_FALSE(output.has_value());
+            run_loop->Quit();
+          },
+          run_loop.get()),
+      std::vector<float>{1, 1, 1});
+  run_loop->Run();
+}
+
+TEST_F(OptimizationTargetModelExecutorTest, ExecuteWithLoadedModel) {
+  CreateModelExecutor();
+
+  PushModelFileToModelExecutor(
+      proto::OptimizationTarget::OPTIMIZATION_TARGET_PAINFUL_PAGE_LOAD);
+  EXPECT_TRUE(model_executor()->HasLoadedModel());
+
+  std::vector<float> input;
+  int expected_dims = 1 * 32 * 32 * 3;
+  input.reserve(expected_dims);
+  for (int i = 0; i < expected_dims; i++)
+    input.emplace_back(1);
+
+  std::unique_ptr<base::RunLoop> run_loop = std::make_unique<base::RunLoop>();
+  model_executor()->ExecuteModelWithInput(
+      base::BindOnce(
+          [](base::RunLoop* run_loop,
+             base::Optional<std::vector<float>> output) {
+            EXPECT_TRUE(output.has_value());
+
+            std::vector<float> expected_output = {
+                -0.4936581, -0.32497078, -0.1705023, -0.38193324, 0.36136785,
+                0.2177353,  0.32200375,  0.28686714, -0.21846706, -0.4200018};
+            for (size_t i = 0; i < expected_output.size(); i++)
+              EXPECT_NEAR(expected_output[i], output.value()[i], 1e-5);
+
+            run_loop->Quit();
+          },
+          run_loop.get()),
+      input);
+  run_loop->Run();
+}
+
+}  // namespace optimization_guide
