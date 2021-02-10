961642cc71df86d1838ec0468c941369fb10d9cc
Tom <tluk@chromium.org>
2021-02-10 05:41:30 +0000

    Add the BubbleContentsWrapper class
    BubbleContentsWrapper is a class that wraps a WebContents object that
    hosts a bubble WebUI, acting as the Embedder to the
    MojoBubbleWebUIController.
    
    BubbleContentsWrapper is as a delegate to the WebContents, passing
    through resize and keyboard events to its Host.
    
    The template class BubbleContentsWrapperT allows for compile
    time checking to ensure the target WebUIController subclasses
    MojoBubbleWebUIController when reloading the WebContents.
    
    This class has been introduced to replace the WebUIBubbleView class.
    WebUIBubbleView is a Views subclass and has strict ownership
    semantics when added to a Views hierarchy. BubbleContentsWrapper will
    allow sharing of the wrapped WebContents without passing over
    ownership to the embedding View when updating code to use profile
    peristence of the BubbleContentsWrapper.
    
    Follow up CLs will replace WebUIBubbleView with BubbleContentsWrapper
    and add support for persistence per profile.
    
    Bug: 1162282
    Change-Id: I9511fae7738b0d9ea4fa2ba27f17f10348449c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676341
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852498}

---
+++ chrome/browser/ui/views/bubble/bubble_contents_wrapper.cc
@@ -0,0 +1,116 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/bubble/bubble_contents_wrapper.h"
+
+#include "content/public/browser/keyboard_event_processing_result.h"
+#include "content/public/browser/native_web_keyboard_event.h"
+#include "content/public/browser/render_widget_host_view.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/widget/widget.h"
+
+namespace {
+
+bool IsEscapeEvent(const content::NativeWebKeyboardEvent& event) {
+  return event.GetType() ==
+             content::NativeWebKeyboardEvent::Type::kRawKeyDown &&
+         event.windows_key_code == ui::VKEY_ESCAPE;
+}
+
+}  // namespace
+
+bool BubbleContentsWrapper::Host::HandleKeyboardEvent(
+    content::WebContents* source,
+    const content::NativeWebKeyboardEvent& event) {
+  return false;
+}
+
+BubbleContentsWrapper::BubbleContentsWrapper(
+    content::BrowserContext* browser_context,
+    int task_manager_string_id,
+    bool enable_extension_apis)
+    : web_contents_(content::WebContents::Create(
+          content::WebContents::CreateParams(browser_context))) {
+  web_contents_->SetDelegate(this);
+  WebContentsObserver::Observe(web_contents_.get());
+
+  if (enable_extension_apis) {
+    // In order for the WebUI in the renderer to use extensions APIs we must
+    // add a ChromeExtensionWebContentsObserver to the WebView's WebContents.
+    extensions::ChromeExtensionWebContentsObserver::CreateForWebContents(
+        web_contents_.get());
+  }
+  task_manager::WebContentsTags::CreateForToolContents(web_contents_.get(),
+                                                       task_manager_string_id);
+}
+
+BubbleContentsWrapper::~BubbleContentsWrapper() {
+  WebContentsObserver::Observe(nullptr);
+}
+
+void BubbleContentsWrapper::ResizeDueToAutoResize(content::WebContents* source,
+                                                  const gfx::Size& new_size) {
+  DCHECK_EQ(web_contents(), source);
+  if (host_)
+    host_->ResizeDueToAutoResize(source, new_size);
+}
+
+content::KeyboardEventProcessingResult
+BubbleContentsWrapper::PreHandleKeyboardEvent(
+    content::WebContents* source,
+    const content::NativeWebKeyboardEvent& event) {
+  DCHECK_EQ(web_contents(), source);
+  // Close the bubble if an escape event is detected. Handle this here to
+  // prevent the renderer from capturing the event and not propagating it up.
+  if (host_ && IsEscapeEvent(event)) {
+    host_->CloseUI();
+    return content::KeyboardEventProcessingResult::HANDLED;
+  }
+  return content::KeyboardEventProcessingResult::NOT_HANDLED;
+}
+
+bool BubbleContentsWrapper::HandleKeyboardEvent(
+    content::WebContents* source,
+    const content::NativeWebKeyboardEvent& event) {
+  DCHECK_EQ(web_contents(), source);
+  return host_ ? host_->HandleKeyboardEvent(source, event) : false;
+}
+
+void BubbleContentsWrapper::RenderViewHostChanged(
+    content::RenderViewHost* old_host,
+    content::RenderViewHost* new_host) {
+  content::RenderWidgetHostView* render_widget_host_view =
+      web_contents_->GetRenderWidgetHostView();
+  if (!render_widget_host_view)
+    return;
+
+  render_widget_host_view->EnableAutoResize(gfx::Size(1, 1),
+                                            gfx::Size(INT_MAX, INT_MAX));
+}
+
+void BubbleContentsWrapper::ShowUI() {
+  if (host_)
+    host_->ShowUI();
+}
+
+void BubbleContentsWrapper::CloseUI() {
+  if (host_)
+    host_->CloseUI();
+}
+
+base::WeakPtr<BubbleContentsWrapper::Host> BubbleContentsWrapper::GetHost() {
+  return host_;
+}
+
+void BubbleContentsWrapper::SetHost(
+    base::WeakPtr<BubbleContentsWrapper::Host> host) {
+  host_ = std::move(host);
+}
+
+void BubbleContentsWrapper::SetWebContentsForTesting(
+    std::unique_ptr<content::WebContents> web_contents) {
+  web_contents_->SetDelegate(nullptr);
+  web_contents_ = std::move(web_contents);
+  web_contents_->SetDelegate(this);
+}
