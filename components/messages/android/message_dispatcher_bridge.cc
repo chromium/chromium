89cda8ef39ef590eb8388ea65c728952d53e956f
Pavel Yatsuk <pavely@google.com>
2021-02-11 22:35:08 +0000

    [Messages] Address a CHECK in MessageWrapper on activity destruction
    When WebContents is destroyed as part of the activity destruction,
    MessageWrapper hits a CHECK if a message is displayed. The reason for
    the check is that the message wasn't dismissed.
    
    This CL ensures that the SavePassword message is dismissed on the
    delegate destruction. Additionally this CL prevents native
    DismissMessage call from propagating to java when WebContents is being
    destroyed.
    
    BUG=1174656
    R=​lazzzis@chromium.org,twellington@chromium.org
    
    (cherry picked from commit 845ee02c1fb09f9d3d58c907e6eb667d2a0fccef)
    
    Change-Id: I87101659f0bbbf604f92ae3456e83bed1fdba1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2677321
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#850961}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689266
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4389@{#940}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- components/messages/android/message_dispatcher_bridge.cc
+++ components/messages/android/message_dispatcher_bridge.cc
@@ -31,8 +31,13 @@ void MessageDispatcherBridge::DismissMessage(
       message->GetJavaMessageWrapper());
   JNIEnv* env = base::android::AttachCurrentThread();
   message->HandleDismissCallback(env);
-  Java_MessageDispatcherBridge_dismissMessage(
-      env, jmessage, web_contents->GetJavaWebContents());
+  // DismissMessage can be called in the process of WebContents destruction.
+  // In this case WebContentsAndroid is already torn down. We shouldn't call
+  // GetJavaWebContents() because it recreates WebContentsAndroid.
+  if (!web_contents->IsBeingDestroyed()) {
+    Java_MessageDispatcherBridge_dismissMessage(
+        env, jmessage, web_contents->GetJavaWebContents());
+  }
 }
 
 }  // namespace messages
