69a300c761dffb7bf984b15e205d3aedffdefb78
Sophey Dong <sophey@chromium.org>
2021-02-11 23:01:05 +0000

    [WebFeed] Create ClickableTextBubble class.
    Also merge ImageTextBubble into TextBubble.
    
    Bug: 1152592
    Change-Id: I6e0ece6ccfaeb71a7315f60dd0e11a2b15fdb781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2688922
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853266}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/textbubble/ClickableTextBubble.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/textbubble/ClickableTextBubble.java
@@ -1,26 +1,27 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 package org.chromium.components.browser_ui.widget.textbubble;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.StringRes;
 
-import org.chromium.components.browser_ui.widget.R;
 import org.chromium.ui.widget.RectProvider;
 
 /**
- * UI component that handles showing a text callout bubble with a preceding image.
+ * UI component that handles showing a clickable text callout bubble.
+ *
+ * <p>This has special styling specific to clickable text bubbles.
+ * // TODO(sophey): add specific styling differences once implemented.
  */
-public class ImageTextBubble extends TextBubble {
+public class ClickableTextBubble extends TextBubble {
     /**
-     * Constructs a {@link ImageTextBubble} instance.
-     * @param context  Context to draw resources from.
+     * Constructs a {@link ClickableTextBubble} instance.
+     *
+     * @param context Context to draw resources from.
      * @param rootView The {@link View} to use for size calculations and for display.
      * @param stringId The id of the string resource for the text that should be shown.
      * @param accessibilityStringId The id of the string resource of the accessibility text.
@@ -28,22 +29,15 @@ public class ImageTextBubble extends TextBubble {
      * @param anchorRectProvider The {@link RectProvider} used to anchor the text bubble.
      * @param imageDrawableId The resource id of the image to show at the start of the text bubble.
      * @param isAccessibilityEnabled Whether accessibility mode is enabled. Used to determine bubble
-     *         text and dismiss UX.
+     * text and dismiss UX.
+     * @param onTouchListener The callback for all touch events being dispatched to the bubble.
      */
-    public ImageTextBubble(Context context, View rootView, @StringRes int stringId,
+    public ClickableTextBubble(Context context, View rootView, @StringRes int stringId,
             @StringRes int accessibilityStringId, boolean showArrow,
-            RectProvider anchorRectProvider, int imageDrawableId, boolean isAccessibilityEnabled) {
+            RectProvider anchorRectProvider, @DrawableRes int imageDrawableId,
+            boolean isAccessibilityEnabled, View.OnTouchListener onTouchListener) {
         super(context, rootView, stringId, accessibilityStringId, showArrow, anchorRectProvider,
-                isAccessibilityEnabled);
-
-        ((ImageView) mContentView.findViewById(R.id.image)).setImageResource(imageDrawableId);
-    }
-
-    @Override
-    protected View createContentView() {
-        View view =
-                LayoutInflater.from(mContext).inflate(R.layout.textbubble_text_with_image, null);
-        setText((TextView) view.findViewById(R.id.message));
-        return view;
+                imageDrawableId, isAccessibilityEnabled);
+        setTouchInterceptor(onTouchListener);
     }
 }
