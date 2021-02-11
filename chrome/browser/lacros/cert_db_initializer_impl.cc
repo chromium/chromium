5251df36b6ed15f77dbd11eadbd4b77d65b85dc5
Erik Chen <erikchen@chromium.org>
2021-02-11 19:22:09 +0000

    Lacros: Enable certs unconditionally.
    Change-Id: I4c87f6f9f1925a089d1e68e6b21925d760674f68
    Bug: 1145946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2688843
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853210}

--- chrome/browser/lacros/cert_db_initializer_impl.cc
+++ chrome/browser/lacros/cert_db_initializer_impl.cc
@@ -22,64 +22,7 @@
 #include "crypto/nss_util_internal.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
-// Includes for `IsEnabledForEarlyAccess()`.
-#include "base/containers/flat_set.h"
-#include "base/feature_list.h"
-#include "base/no_destructor.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
-
 namespace {
-
-// When this feature is enabled, certs are available to everyone.
-const base::Feature kLacrosEnableCerts{"LacrosEnableCerts",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
-
-// TODO(crbug.com/1145946): Enable certificate database initialization for
-// everyone when the policy stack is ready (expected to happen before Feb 2021).
-bool IsEnabledForEarlyAccess(Profile* profile) {
-  if (base::FeatureList::IsEnabled(kLacrosEnableCerts)) {
-    return true;
-  }
-
-  static base::NoDestructor<base::flat_set<std::string>> allowlist{
-      {"bartfab@google.com",       "darin@google.com",
-       "dhaddock@google.com",      "edcourtney@google.com",
-       "erikchen@google.com",      "fangzhoug@google.com",
-       "fukino@google.com",        "gianluca@google.com",
-       "heiserya@google.com",      "hidehiko@google.com",
-       "huangs@google.com",        "huanr@google.com",
-       "igorcov@google.com",       "jamescook@google.com",
-       "jennyz@google.com",        "jorgelo@google.com",
-       "ketakid@google.com",       "lakpamarthy@google.com",
-       "leolai@google.com",        "liaoyuke@google.com",
-       "maguschen@google.com",     "marinakz@google.com",
-       "miersh@google.com",        "mkarkada@google.com",
-       "okalitova@google.com",     "oshima@google.com",
-       "pmarko@google.com",        "pucchakayala@google.com",
-       "rbock@google.com",         "rjkroege@google.com",
-       "rogerta@google.com",       "satorux@google.com",
-       "sinhak@google.com",        "songsuk@google.com",
-       "srinivassista@google.com", "svenzheng@google.com",
-       "willmcleod@google.com",    "ythjkt@google.com",
-       "yusukes@google.com",       "nrpeter@google.com",
-       "miersh@managedchrome.com", "omrilio@google.com",
-       "thomasriedl@google.com",   "kuscher@google.com",
-       "andrewgray@google.com",    "acostinas@google.com",
-       "amraboelkher@google.com",  "anastasiian@google.com",
-       "anqing@google.com",        "apotapchuk@google.com",
-       "asumaneev@google.com",     "djmm@google.com",
-       "emaxx@google.com",         "hendrich@google.com",
-       "marcgrimme@google.com",    "maxkirsch@google.com",
-       "mslus@google.com",         "okalitova@google.com",
-       "omorsi@google.com",        "roddis@google.com"}};
-
-  signin::IdentityManager* identity_manager =
-      IdentityManagerFactory::GetForProfile(profile);
-  return base::Contains(*allowlist, identity_manager
-                                        ->GetPrimaryAccountInfo(
-                                            signin::ConsentLevel::kNotRequired)
-                                        .email);
-}
 
 bool InitializeCertDbOnWorkerThread(bool should_load_chaps,
                                     base::FilePath software_nss_db_path) {
@@ -203,12 +146,6 @@ void CertDbInitializerImpl::OnRefreshTokensLoaded() {
 
 void CertDbInitializerImpl::WaitForCertDbReady() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-
-  if (!IsEnabledForEarlyAccess(profile_)) {
-    LOG(WARNING) << "Certificate initialization is skipped.";
-    OnCertDbInitializationFinished(false);
-    return;
-  }
 
   if (!profile_->IsMainProfile()) {
     OnCertDbInitializationFinished(false);
