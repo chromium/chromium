b04692320ce7ad62759c3e8ca3433a0f63135ed6
Manuel Rego Casasnovas <rego@igalia.com>
2021-02-11 07:09:56 +0000

    Send FocusType::kMouse in MenuListSelectType::DefaultEventHandler()
    When you first click a <select> element it goes through
    MenuListSelectType::DefaultEventHandler(), which ends up calling
    Element::focus() with FocusType::kNone as parameter.
    That's not true as the <select> has been actually clicked,
    so it should pass FocusType::kMouse.
    
    This has no effects right now, as <select> always matches :focus-visible
    (HTMLSelectElement::MayTriggerVirtualKeyboard() is always true),
    but it might have effects if we ever change that, as the it won't know
    that it has been focused via mouse click.
    
    Change-Id: Id1068d0e205993bfb59e457da2a523f29951b2bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671502
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#853027}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -232,7 +232,7 @@ bool MenuListSelectType::DefaultEventHandler(const Event& event) {
             ->GetInputDeviceCapabilities()
             ->FiresTouchEvents(mouse_event->FromTouch());
     select_->focus(FocusParams(SelectionBehaviorOnFocus::kRestore,
-                               mojom::blink::FocusType::kNone,
+                               mojom::blink::FocusType::kMouse,
                                source_capabilities));
     if (select_->GetLayoutObject() && !will_be_destroyed_ &&
         !select_->IsDisabledFormControl()) {
