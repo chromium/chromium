94eaa28a0408e3fe29147cbbe6494b8e5101b134
Lukasz Anforowicz <lukasza@chromium.org>
2021-02-12 00:13:48 +0000

    Disabling flaky ...OpenedFromExternalAppTest#testNewTabWithNewTabExtra.
    Bug: 1176476
    Change-Id: I17165e2cf63e26b914f18a0090534884f084eb1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691427
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Owners-Override: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853302}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
@@ -29,6 +29,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.UrlUtils;
@@ -478,6 +479,7 @@ public class TabsOpenedFromExternalAppTest {
     @Test
     @LargeTest
     @Feature({"Navigation"})
+    @DisabledTest(message = "Flaky test - see: https://crbug.com/1176476")
     public void testNewTabWithNewTabExtra() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
 
