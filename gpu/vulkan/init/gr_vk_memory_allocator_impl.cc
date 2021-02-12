fe7be881e4526ff0c4224bc4dd5a8dab235002f8
Greg Daniel <egdaniel@google.com>
2021-02-12 14:23:57 +0000

    Fix uninitialized value in skia vk mem allocator.
    TBR=penghuang@chromium.org
    
    Change-Id: Id7d7c5e7592c802c4fe81fcd84fd5855b420c113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691075
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Auto-Submit: Greg Daniel <egdaniel@google.com>
    Cr-Commit-Position: refs/heads/master@{#853510}

--- gpu/vulkan/init/gr_vk_memory_allocator_impl.cc
+++ gpu/vulkan/init/gr_vk_memory_allocator_impl.cc
@@ -88,6 +88,7 @@ class GrVkMemoryAllocatorImpl : public GrVkMemoryAllocator {
       case BufferUsage::kTransfersFromCpuToGpu:
         info.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
                              VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+        info.preferredFlags = 0;
         break;
       case BufferUsage::kTransfersFromGpuToCpu:
         info.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
