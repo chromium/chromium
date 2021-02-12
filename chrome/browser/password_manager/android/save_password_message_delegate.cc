c3a5450759689471c4de70e0ceecceafb2acc6d1
Pavel Yatsuk <pavely@google.com>
2021-02-12 21:12:30 +0000

    [Messages] Address a CHECK in MessageWrapper on activity destruction
    When WebContents is destroyed as part of the activity destruction,
    MessageWrapper hits a CHECK if a message is displayed. The reason for
    the check is that the message wasn't dismissed.
    
    This CL ensures that the SavePassword message is dismissed on the
    delegate destruction. Additionally this CL prevents native
    DismissMessage call from propagating to java when WebContents is being
    destroyed.
    
    BUG=1174656
    R=​lazzzis@chromium.org,twellington@chromium.org
    
    (cherry picked from commit 845ee02c1fb09f9d3d58c907e6eb667d2a0fccef)
    
    Change-Id: I87101659f0bbbf604f92ae3456e83bed1fdba1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2677321
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#850961}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693450
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#2198}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- chrome/browser/password_manager/android/save_password_message_delegate.cc
+++ chrome/browser/password_manager/android/save_password_message_delegate.cc
@@ -19,7 +19,10 @@
 #include "url/origin.h"
 
 SavePasswordMessageDelegate::SavePasswordMessageDelegate() = default;
-SavePasswordMessageDelegate::~SavePasswordMessageDelegate() = default;
+
+SavePasswordMessageDelegate::~SavePasswordMessageDelegate() {
+  DismissSavePasswordPrompt();
+}
 
 void SavePasswordMessageDelegate::DisplaySavePasswordPrompt(
     content::WebContents* web_contents,
