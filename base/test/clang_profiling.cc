fe82551250627dd33c5357ea72af34e04e49f3cd
Chong Gu <chonggu@google.com>
2021-02-12 10:51:03 +0000

    [Fuchsia] Skip profiling dump for Fuchsia.
    The way profile runtime works on Fuchsia is that we setup a memory object during runtime initialization which is mapped to address space and shared with the data collection service. The program then directly updates counters inside this object as it runs rather than storing them separately as is the case on other system, so the data collection service always has the up-to-date view. Therefore, there's no point for functions like `__llvm_profile_dump` and we don't provide it at all.
    
    Bug: 1156292
    Change-Id: I63876f6173521f7aa83751b2501868f0a4e204da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580438
    Commit-Queue: Chong Gu <chonggu@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Auto-Submit: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/master@{#853465}

--- base/test/clang_profiling.cc
+++ base/test/clang_profiling.cc
@@ -6,6 +6,7 @@
 
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
+#include "build/build_config.h"
 
 extern "C" int __llvm_profile_dump(void);
 
@@ -20,7 +21,11 @@ void WriteClangProfilingProfile() {
   // profiling counters.
   static base::NoDestructor<base::Lock> lock;
   base::AutoLock auto_lock(*lock);
+
+// Fuchsia's profile runtime does not handle profile dumping.
+#if !defined(OS_FUCHSIA)
   __llvm_profile_dump();
+#endif  // !defined(OS_FUCHSIA)
 }
 
 }  // namespace base
