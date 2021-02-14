fd4e4ceff62558927b8c40b43a09e66a9ee20b61
Nico Weber <thakis@chromium.org>
2021-02-14 01:51:16 +0000

    Roll clang llvmorg-13-init-794-g83e2710e-1 : llvmorg-13-init-1559-g01b87444-1.
    Ran `./tools/clang/scripts/upload_revision.py 01b87444cb02c38147dccc7049b32675de860d47`.
    
    Bug: 1175159
    Change-Id: I2804d0e488039ed6ae38129816b5a288196e7234
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_angle_deqp_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:win-angle-deqp-rel-64
    Cq-Include-Trybots: chromium/try:win-asan,win7-rel,win-angle-deqp-rel-32
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,mac-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690149
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853876}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -38,7 +38,7 @@ import zipfile
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-13-init-794-g83e2710e'
+CLANG_REVISION = 'llvmorg-13-init-1559-g01b87444'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
