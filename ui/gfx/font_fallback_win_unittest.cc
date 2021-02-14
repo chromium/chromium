9686e4d6a333014a9440ec42003c4f0242ae8869
Peter Kasting <pkasting@chromium.org>
2021-02-14 01:53:56 +0000

    Get gfx_unittests to build with std::u16string.
    Bug: 911896
    Change-Id: I3449632d1d3ad4b950b1f97ea29c04c7206178c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693856
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853877}

--- ui/gfx/font_fallback_win_unittest.cc
+++ ui/gfx/font_fallback_win_unittest.cc
@@ -88,18 +88,18 @@ TEST_F(FontFallbackWinTest, NulTerminatedStringPiece) {
   Font base_font;
   Font fallback_font;
   // Multiple ending NUL characters.
-  const wchar_t kTest1[] = {0x0540, 0x0541, 0, 0, 0};
+  const base::char16 kTest1[] = {0x0540, 0x0541, 0, 0, 0};
   EXPECT_FALSE(GetFallbackFont(base_font, kDefaultApplicationLocale,
                                base::StringPiece16(kTest1, base::size(kTest1)),
                                &fallback_font));
   // No ending NUL character.
-  const wchar_t kTest2[] = {0x0540, 0x0541};
+  const base::char16 kTest2[] = {0x0540, 0x0541};
   EXPECT_TRUE(GetFallbackFont(base_font, kDefaultApplicationLocale,
                               base::StringPiece16(kTest2, base::size(kTest2)),
                               &fallback_font));
 
   // NUL only characters.
-  const wchar_t kTest3[] = {0, 0, 0};
+  const base::char16 kTest3[] = {0, 0, 0};
   EXPECT_FALSE(GetFallbackFont(base_font, kDefaultApplicationLocale,
                                base::StringPiece16(kTest3, base::size(kTest3)),
                                &fallback_font));
@@ -115,7 +115,7 @@ TEST_F(FontFallbackWinTest, CJKLocaleFallback) {
   // common feature of written Chinese (hanzi), Japanese (kanji), and Korean
   // (hanja). The same text will be rendered using a different font based on
   // locale.
-  const wchar_t kCJKTest[] = L"\u8AA4\u904E\u9AA8";
+  const base::char16 kCJKTest[] = STRING16_LITERAL("\u8AA4\u904E\u9AA8");
   Font base_font;
   Font fallback_font;
 
