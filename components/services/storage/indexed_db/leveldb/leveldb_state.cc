6f5092354f5afc8555d31782215ba1ea3297c252
Marcin Grzeskowiak <marcing@opera.com>
2021-02-16 19:57:55 +0000

    Fix cleanup of LevelDBState in tests
    IndexedDBBackingStoreTest sets up LevelDBState in a temporary directory,
    which is later deleted during test shutdown. On Windows this can only
    succeed if all files created inside the directory are closed.
    
    This commit reorders calls in the destructor of LevelDBState to ensure
    that it signals to the test that destruction is complete after closing
    LevelDB and its files.
    
    Change-Id: I87ad5cb6b42c3010a826aee82536b1472b5909cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693238
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#854401}

--- components/services/storage/indexed_db/leveldb/leveldb_state.cc
+++ components/services/storage/indexed_db/leveldb/leveldb_state.cc
@@ -58,14 +58,14 @@ void LevelDBState::RequestDestruction(
 }
 
 LevelDBState::~LevelDBState() {
+  if (db_) {
+    base::TimeTicks begin_time = base::TimeTicks::Now();
+    const_cast<std::unique_ptr<leveldb::DB>*>(&db_)->reset();
+    base::UmaHistogramMediumTimes("WebCore.IndexedDB.LevelDB.CloseTime",
+                                  base::TimeTicks::Now() - begin_time);
+  }
   if (signal_on_destruction_)
     signal_on_destruction_->Signal();
-  if (!db_)
-    return;
-  base::TimeTicks begin_time = base::TimeTicks::Now();
-  const_cast<std::unique_ptr<leveldb::DB>*>(&db_)->reset();
-  base::UmaHistogramMediumTimes("WebCore.IndexedDB.LevelDB.CloseTime",
-                                base::TimeTicks::Now() - begin_time);
 }
 
 }  // namespace content
