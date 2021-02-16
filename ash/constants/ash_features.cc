9f440c8103d71aa8f2fd101a58463551e4a3f640
Bailey Berro <baileyberro@google.com>
2021-02-16 17:20:27 +0000

    Enabled the Diagnostics App by default
    This change enables the kDiagnosticsApp feature flag by default.
    
    Bug: 1128204
    Change-Id: I3f22fe4e034735aa887fb7f071ebbf38fd9bb9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692840
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Auto-Submit: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854313}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -244,7 +244,7 @@ const base::Feature kCryptAuthV2Enrollment{"CryptAuthV2Enrollment",
 
 // Enables or disables the Diagnostics app.
 const base::Feature kDiagnosticsApp{"DiagnosticsApp",
-                                    base::FEATURE_DISABLED_BY_DEFAULT};
+                                    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Disables the CryptAuth v1 DeviceSync flow. Note: During the first phase
 // of the v2 DeviceSync rollout, v1 and v2 DeviceSync run in parallel. This flag
