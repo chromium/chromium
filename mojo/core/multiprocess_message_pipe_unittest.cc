6c7f133fa7c6e85a748ce28eeaaa40007eb6b0d5
Austin Sullivan <asully@chromium.org>
2021-02-16 18:57:34 +0000

    testing: Only instantiate MultiprocessMessagePipe test if defined
    googletest CL 315255779 causes test binaries to fail when they include
    test suites / parametrized tests that are not instantiated, and when
    they include empty test suites.
    
    MultiprocessMessagePipeTestWithPipeCount is only sometimes defined,
    but always instantiated.
    
    Bug: 1163396
    Change-Id: I9199ff113e8be28c49f2df0a91a9e657c6c71b98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692349
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854371}

--- mojo/core/multiprocess_message_pipe_unittest.cc
+++ mojo/core/multiprocess_message_pipe_unittest.cc
@@ -437,6 +437,7 @@ DEFINE_TEST_CLIENT_WITH_PIPE(CheckPlatformHandleFile,
   return 0;
 }
 
+#if !defined(OS_ANDROID)
 class MultiprocessMessagePipeTestWithPipeCount
     : public MultiprocessMessagePipeTest,
       public testing::WithParamInterface<size_t> {};
@@ -482,7 +483,6 @@ TEST_P(MultiprocessMessagePipeTestWithPipeCount, PlatformHandlePassing) {
 }
 
 // Android multi-process tests are not executing the new process. This is flaky.
-#if !defined(OS_ANDROID)
 INSTANTIATE_TEST_SUITE_P(PipeCount,
                          MultiprocessMessagePipeTestWithPipeCount,
                          // TODO(rockot): Enable the 128 and 250 pipe cases when
