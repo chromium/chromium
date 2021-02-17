f83b102bd0f99f7fdcad83d53737bb2ca13d1e2b
David Jean <djean@chromium.org>
2021-02-17 09:45:54 +0000

    [ios] Add reading list / history multiwindow sync eg test
    Part of 6/10 on the list of manual test cases.
    
    Bug: 1176180
    Change-Id: I4252083e77fae58f3088b6a07dabaf6b5bf789cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696571
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854695}

--- ios/chrome/browser/ui/history/history_ui_egtest.mm
+++ ios/chrome/browser/ui/history/history_ui_egtest.mm
@@ -36,6 +36,7 @@ using chrome_test_util::HistoryEntry;
 using chrome_test_util::NavigationBarDoneButton;
 using chrome_test_util::OpenLinkInNewWindowButton;
 using chrome_test_util::DeleteButton;
+using chrome_test_util::WindowWithNumber;
 
 namespace {
 char kURL1[] = "/firstURL";
@@ -139,6 +140,8 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 - (void)tearDown {
   // No-op if only one window presents.
   [ChromeEarlGrey closeAllExtraWindows];
+  [EarlGrey setRootMatcherForSubsequentInteractions:nil];
+
   NSError* error = nil;
   // Dismiss search bar by pressing cancel, if present. Passing error prevents
   // failure if the element is not found.
@@ -618,6 +621,66 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
     [[EarlGrey selectElementWithMatcher:EmptyTableViewBackground()]
         assertWithMatcher:grey_notNil()];
   }
+}
+
+#pragma mark Multiwindow
+
+- (void)testHistorySyncInMultiwindow {
+  if (![ChromeEarlGrey areMultipleWindowsSupported])
+    EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
+
+  // Create history in first window.
+  [self loadTestURLs];
+
+  // Open history panel in a second window
+  [ChromeEarlGrey openNewWindow];
+  [ChromeEarlGrey waitForForegroundWindowCount:2];
+
+  [EarlGrey setRootMatcherForSubsequentInteractions:WindowWithNumber(1)];
+  [self openHistoryPanel];
+
+  // Assert that three history elements are present in second window.
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL1.GetOrigin().spec(), kTitle1)]
+      assertWithMatcher:grey_notNil()];
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL2.GetOrigin().spec(), kTitle2)]
+      assertWithMatcher:grey_notNil()];
+  [[EarlGrey selectElementWithMatcher:HistoryEntry(_URL3.GetOrigin().spec(),
+                                                   _URL3.GetContent())]
+      assertWithMatcher:grey_notNil()];
+
+  // Open history panel in first window also.
+  [EarlGrey setRootMatcherForSubsequentInteractions:WindowWithNumber(0)];
+  [self openHistoryPanel];
+
+  // Assert that three history elements are present in first window.
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL1.GetOrigin().spec(), kTitle1)]
+      assertWithMatcher:grey_notNil()];
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL2.GetOrigin().spec(), kTitle2)]
+      assertWithMatcher:grey_notNil()];
+  [[EarlGrey selectElementWithMatcher:HistoryEntry(_URL3.GetOrigin().spec(),
+                                                   _URL3.GetContent())]
+      assertWithMatcher:grey_notNil()];
+
+  // Delete item 1 from first window.
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL1.GetOrigin().spec(), kTitle1)]
+      performAction:grey_longPress()];
+
+  [[EarlGrey selectElementWithMatcher:DeleteButton()] performAction:grey_tap()];
+
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL1.GetOrigin().spec(), kTitle1)]
+      assertWithMatcher:grey_nil()];
+
+  // And make sure it has disappeared from second window.
+  [EarlGrey setRootMatcherForSubsequentInteractions:WindowWithNumber(1)];
+  [[EarlGrey
+      selectElementWithMatcher:HistoryEntry(_URL1.GetOrigin().spec(), kTitle1)]
+      assertWithMatcher:grey_nil()];
 }
 
 #pragma mark Helper Methods
