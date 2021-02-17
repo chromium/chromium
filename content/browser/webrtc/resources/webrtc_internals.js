ae6dc7bd2c147a3a1861748486e804ac3f4a9983
rbpotter <rbpotter@chromium.org>
2021-02-17 18:40:22 +0000

    WebUI: Move webrtc files to webrtc directory
    Move files in content/browser/resources/media/ that are only used by
    being inlined in content/browser/webrtc/resources/webrtc_internals.js
    into content/browser/webrtc/resources.
    
    This is in preparation for migrating these two pages (media and
    webrtc internals) to JS modules.
    
    Bug: 1173575
    Change-Id: I8cfde3c3e9851aba0fa3e2cf7ac53f789df05cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698143
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854875}

--- content/browser/webrtc/resources/webrtc_internals.js
+++ content/browser/webrtc/resources/webrtc_internals.js
@@ -95,14 +95,14 @@ var PeerConnectionRecord = (function() {
 // will be shifted out when the buffer is full.
 var MAX_STATS_DATA_POINT_BUFFER_SIZE = 1000;
 
-// <include src="../../resources/media/tab_view.js">
-// <include src="../../resources/media/data_series.js">
-// <include src="../../resources/media/ssrc_info_manager.js">
-// <include src="../../resources/media/stats_graph_helper.js">
-// <include src="../../resources/media/stats_rates_calculator.js">
-// <include src="../../resources/media/stats_table.js">
-// <include src="../../resources/media/peer_connection_update_table.js">
-// <include src="../../resources/media/dump_creator.js">
+// <include src="tab_view.js">
+// <include src="data_series.js">
+// <include src="ssrc_info_manager.js">
+// <include src="stats_graph_helper.js">
+// <include src="stats_rates_calculator.js">
+// <include src="stats_table.js">
+// <include src="peer_connection_update_table.js">
+// <include src="dump_creator.js">
 
 
 function initialize() {
