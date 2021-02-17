f2d8f18af4192f67f771faa491d9a8ea706a60f2
kylechar <kylechar@chromium.org>
2021-02-17 14:37:49 +0000

    Convert components/sync to use base::BindPostTask()
    This CL switches components/sync to use base::BindPostTask() instead of
    reimplementing the same functionality. The major difference for the task
    runner trampoline is that BindPostTask() will ensure the callbacks are
    destroyed on the specified task runner unlike the existing
    implementation.
    
    Leave the helper function BindToCurrentSequence() as it don't exist in
    base/ but update it to forward the location of the caller.
    
    Bug: 1140582
    Change-Id: Ifc68b0f21405665d629746d09fe30e49c89c253a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698209
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854762}

--- components/sync/base/bind_to_task_runner.h
+++ components/sync/base/bind_to_task_runner.h
@@ -7,77 +7,29 @@
 
 #include <utility>
 
-#include "base/bind.h"
+#include "base/bind_post_task.h"
 #include "base/callback.h"
-#include "base/callback_helpers.h"
 #include "base/location.h"
-#include "base/memory/ref_counted.h"
-#include "base/task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 
-// This is a helper utility for Bind()ing callbacks to a given TaskRunner.
-// The typical use is when |a| (of class |A|) wants to hand a callback such as
-// base::BindOnce(&A::AMethod, a) to |b|, but needs to ensure that when |b|
-// executes the callback, it does so on a specific TaskRunner (for example,
-// |a|'s current MessageLoop).
-//
-// Typical usage: request to be called back on the current sequence:
-// other->StartAsyncProcessAndCallMeBack(BindToTaskRunner(
-//     my_task_runner_, base::BindOnce(&MyClass::MyMethod, this)));
-//
-// Note that like base::Bind{Once,Repeating}(), BindToTaskRunner() can't bind
-// non-constant references, and that *unlike* base::Bind{Once,Repeating}(),
-// BindToTaskRunner() makes copies of its arguments, and thus can't be used with
-// arrays. Note that the callback is always posted to the target TaskRunner.
-//
-// As a convenience, you can use BindToCurrentSequence() to bind to the
-// TaskRunner for the current sequence (i.e.
-// base::SequencedTaskRunnerHandle::Get()).
-
+// Helpers for using base::BindPostTask() with the TaskRunner for the current
+// sequence, ie. base::SequencedTaskRunnerHandle::Get().
 namespace syncer {
-namespace bind_helpers {
-
-template <typename Sig>
-struct BindToTaskRunnerTrampoline;
-
-template <typename... Args>
-struct BindToTaskRunnerTrampoline<void(Args...)> {
-  static void Run(const scoped_refptr<base::TaskRunner>& task_runner,
-                  base::OnceCallback<void(Args...)> cb,
-                  Args... args) {
-    task_runner->PostTask(
-        FROM_HERE, base::BindOnce(std::move(cb), std::forward<Args>(args)...));
-  }
-};
-
-}  // namespace bind_helpers
 
 template <typename T>
-base::OnceCallback<T> BindToTaskRunner(
-    const scoped_refptr<base::TaskRunner>& task_runner,
-    base::OnceCallback<T> cb) {
-  return base::BindOnce(&bind_helpers::BindToTaskRunnerTrampoline<T>::Run,
-                        task_runner, std::move(cb));
-}
-
-template <typename T>
-base::RepeatingCallback<T> BindToTaskRunner(
-    const scoped_refptr<base::TaskRunner>& task_runner,
-    const base::RepeatingCallback<T>& cb) {
-  return base::BindRepeating(&bind_helpers::BindToTaskRunnerTrampoline<T>::Run,
-                             task_runner, cb);
-}
-
-template <typename T>
-base::OnceCallback<T> BindToCurrentSequence(base::OnceCallback<T> cb) {
-  return BindToTaskRunner(base::SequencedTaskRunnerHandle::Get(),
-                          std::move(cb));
+base::OnceCallback<T> BindToCurrentSequence(
+    base::OnceCallback<T> callback,
+    const base::Location& location = FROM_HERE) {
+  return base::BindPostTask(base::SequencedTaskRunnerHandle::Get(),
+                            std::move(callback), location);
 }
 
 template <typename T>
 base::RepeatingCallback<T> BindToCurrentSequence(
-    const base::RepeatingCallback<T>& cb) {
-  return BindToTaskRunner(base::SequencedTaskRunnerHandle::Get(), cb);
+    base::RepeatingCallback<T> callback,
+    const base::Location& location = FROM_HERE) {
+  return base::BindPostTask(base::SequencedTaskRunnerHandle::Get(),
+                            std::move(callback), location);
 }
 
 }  // namespace syncer
