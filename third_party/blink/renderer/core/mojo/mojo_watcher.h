c5f602528690e14542a24df5d2492fa389df711a
Ken Rockot <rockot@google.com>
2021-02-17 08:17:37 +0000

    Fix UAP in Blink MojoWatcher
    The MojoWatcher implementation had incorrectly assumed it would always
    be alive when the underlying trap triggers, because we were relying on
    its ActiveScriptWrappable::HasPendingActivity impl always returning true
    as long as the trap is alive. However ASW cannot prevent the object
    from being GCed when its ExecutionContext is destroyed, and this could
    result in a UAP when Mojo attempts to signal the object.
    
    This ensures that MojoWatcher retains a persistent reference to itself
    as long as its underlying trap is active.
    
    Fixed: 1174373
    Change-Id: I47ebea231fbe96f5d5914379e63976ce6bb45375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690995
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854669}

--- third_party/blink/renderer/core/mojo/mojo_watcher.h
+++ third_party/blink/renderer/core/mojo/mojo_watcher.h
@@ -10,6 +10,7 @@
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/self_keep_alive.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -54,6 +55,7 @@ class MojoWatcher final : public ScriptWrappable,
   static void OnHandleReady(const MojoTrapEvent*);
   void RunReadyCallback(MojoResult);
 
+  SelfKeepAlive<MojoWatcher> keep_alive_{PERSISTENT_FROM_HERE};
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   Member<V8MojoWatchCallback> callback_;
   mojo::ScopedTrapHandle trap_handle_;
