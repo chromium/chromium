0fd80c477c584f104b490dc2f863106cad9d11f9
Peter Wen <wnwen@chromium.org>
2021-02-18 20:20:00 +0000

    Android: Add typing to benchmark.py
    Split out benchmark suites from the individual benchmarks made the
    return types a lot easier to understand.
    
    Bug: None
    Change-Id: I1648f6af3e5ceecc09c8457f57b2742bcc76268d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702391
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855398}

--- tools/android/build_speed/benchmark.py
+++ tools/android/build_speed/benchmark.py
@@ -23,6 +23,7 @@ Note: This tool will make edits on files in your local repo. It will revert the
 
 import argparse
 import contextlib
+import dataclasses
 import logging
 import os
 import pathlib
@@ -32,15 +33,15 @@ import sys
 import time
 import shutil
 
-USE_PYTHON_3 = f'This script will only run under python3.'
+from typing import Dict, Iterator, List, Tuple
+
+USE_PYTHON_3 = f'{__file__} will only run under python3.'
 
 _SRC_ROOT = os.path.normpath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
 sys.path.append(os.path.join(_SRC_ROOT, 'build', 'android'))
+# pylint: disable=import-error
 from pylib import constants
-
-# pylint: disable=line-too-long
-_URL_BAR = 'chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java'
 
 _COMMON_ARGS = [
     'target_os="android"',
@@ -57,69 +58,77 @@ _TARGETS = {
     'apk': 'chrome_public_apk',
 }
 
-_BENCHMARKS = {
-    'all_incremental': {
-        'suite': [
-            'chrome_java_nosig',
-            'chrome_java_sig',
-            'chrome_java_res',
-            'base_java_nosig',
-            'base_java_sig',
-        ],
-    },
-    'extra_incremental': {
-        'suite': [
-            'turbine_headers',
-            'compile_java',
-        ],
-    },
-    'chrome_java_nosig': {
+_SUITES = {
+    'all_incremental': [
+        'chrome_java_nosig',
+        'chrome_java_sig',
+        'chrome_java_res',
+        'base_java_nosig',
+        'base_java_sig',
+    ],
+    'extra_incremental': [
+        'turbine_headers',
+        'compile_java',
+    ],
+}
+
+
+@dataclasses.dataclass
+class Benchmark:
+    name: str
+    info: Dict[str, str]
+
+
+_BENCHMARKS = [
+    Benchmark('chrome_java_nosig', {
         'kind': 'incremental',
         'from_string': '"Url',
         'to_string': '"Url1',
-        'change_file': _URL_BAR,
-    },
-    'chrome_java_sig': {
+        # pylint: disable=line-too-long
+        'change_file': 'chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java',
+    }),
+    Benchmark('chrome_java_sig', {
         'kind': 'incremental',
         'from_string': 'UrlBar";',
         'to_string': 'UrlBar";public void NewInterfaceMethod(){}',
-        'change_file': _URL_BAR,
-    },
-    'chrome_java_res': {
+        # pylint: disable=line-too-long
+        'change_file': 'chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java',
+    }),
+    Benchmark('chrome_java_res', {
         'kind': 'incremental',
         'from_string': '14181C',
         'to_string': '14181D',
         'change_file': 'chrome/android/java/res/values/colors.xml',
-    },
-    'base_java_nosig': {
+    }),
+    Benchmark('base_java_nosig', {
         'kind': 'incremental',
         'from_string': '"SysUtil',
         'to_string': '"SysUtil1',
         'change_file': 'base/android/java/src/org/chromium/base/SysUtils.java',
-    },
-    'base_java_sig': {
+    }),
+    Benchmark('base_java_sig', {
         'kind': 'incremental',
         'from_string': 'SysUtils";',
         'to_string': 'SysUtils";public void NewInterfaceMethod(){}',
         'change_file': 'base/android/java/src/org/chromium/base/SysUtils.java',
-    },
-    'turbine_headers': {
+    }),
+    Benchmark('turbine_headers', {
         'kind': 'incremental',
         'from_string': '# found in the LICENSE file.',
         'to_string': '#temporary_edit_for_benchmark.py',
         'change_file': 'build/android/gyp/turbine.py',
-    },
-    'compile_java': {
+    }),
+    Benchmark('compile_java', {
         'kind': 'incremental',
         'from_string': '# found in the LICENSE file.',
         'to_string': '#temporary_edit_for_benchmark.py',
         'change_file': 'build/android/gyp/compile_java.py',
-    },
-}
+    }),
+]
 
 
 @contextlib.contextmanager
-def _backup_file(file_path):
+def _backup_file(file_path: str):
     file_backup_path = file_path + '.backup'
     logging.info('Creating %s for backup', file_backup_path)
     # Move the original file and copy back to preserve metadata.
@@ -131,7 +140,7 @@ def _backup_file(file_path):
         shutil.move(file_backup_path, file_path)
 
 
-def _run_and_time_cmd(cmd):
+def _run_and_time_cmd(cmd: List[str]) -> float:
     logging.debug('Running %s', cmd)
     start = time.time()
     try:
@@ -148,16 +157,17 @@ def _run_and_time_cmd(cmd):
     return time.time() - start
 
 
-def _run_gn_gen(out_dir):
+def _run_gn_gen(out_dir: str) -> float:
     return _run_and_time_cmd(['gn', 'gen', '-C', out_dir])
 
 
-def _run_autoninja(out_dir, *args):
+def _run_autoninja(out_dir: str, *args: str) -> float:
     return _run_and_time_cmd(['autoninja', '-C', out_dir] + list(args))
 
 
-def _run_incremental_benchmark(*, out_dir, target, from_string, to_string,
-                               change_file):
+def _run_incremental_benchmark(*, out_dir: str, target: str, from_string: str,
+                               to_string: str,
+                               change_file: str) -> Iterator[float]:
     # This ensures that the only change is the one that this script makes.
     prep_time = _run_autoninja(out_dir, target)
     logging.info(f'Took {prep_time:.1f}s to prep this test')
@@ -178,14 +188,14 @@ def _run_incremental_benchmark(*, out_dir, target, from_string, to_string,
     yield _run_autoninja(out_dir, target)
 
 
-def _run_benchmark(*, kind, **kwargs):
+def _run_benchmark(*, kind: str, **kwargs: str) -> Iterator[float]:
     if kind == 'incremental':
         return _run_incremental_benchmark(**kwargs)
     else:
         raise NotImplementedError(f'Benchmark type {kind} is not defined.')
 
 
-def _format_result(time_taken):
+def _format_result(time_taken: List[float]) -> str:
     avg_time = sum(time_taken) / len(time_taken)
     list_of_times = ', '.join(f'{t:.1f}s' for t in time_taken)
     result = f'{avg_time:.1f}s'
@@ -194,18 +204,24 @@ def _format_result(time_taken):
     return result
 
 
-def _parse_benchmarks(benchmarks):
-    for benchmark in benchmarks:
-        assert benchmark in _BENCHMARKS, (
-            f'{benchmark} is not a valid benchmark/suite.')
-        info = _BENCHMARKS[benchmark]
-        if 'suite' in info:
-            yield from _parse_benchmarks(info['suite'])
+def _get_benchmark_for_name(name: str) -> Benchmark:
+    for benchmark in _BENCHMARKS:
+        if benchmark.name == name:
+            return benchmark
+    assert False, f'{name} is not a valid name.'
+
+
+def _parse_benchmarks(benchmarks: List[str]) -> Iterator[Benchmark]:
+    for name in benchmarks:
+        if name in _SUITES:
+            yield from _parse_benchmarks(_SUITES[name])
         else:
-            yield benchmark, info
+            yield _get_benchmark_for_name(name)
 
 
-def run_benchmarks(benchmarks, gn_args, output_directory, target, repeat):
+def run_benchmarks(benchmarks: List[str], gn_args: List[str],
+                   output_directory: str, target: str,
+                   repeat: int) -> Iterator[Tuple[str, List[float]]]:
     out_dir = os.path.relpath(output_directory, _SRC_ROOT)
     args_gn_path = os.path.join(out_dir, 'args.gn')
     with _backup_file(args_gn_path):
@@ -213,25 +229,32 @@ def run_benchmarks(benchmarks, gn_args, output_directory, target, repeat):
             # Use newlines instead of spaces since autoninja.py uses regex to
             # determine whether use_goma is turned on or off.
             f.write('\n'.join(gn_args))
-        yield f'gn gen', [_run_gn_gen(out_dir)]
-        for name, info in _parse_benchmarks(benchmarks):
-            logging.info(f'Starting {name}...')
+        yield 'gn gen', [_run_gn_gen(out_dir)]
+        for benchmark in _parse_benchmarks(benchmarks):
+            logging.info(f'Starting {benchmark.name}...')
             time_taken = []
             for run_num in range(repeat):
                 logging.info(f'Run number: {run_num + 1}')
                 for elapsed in _run_benchmark(out_dir=out_dir,
                                               target=target,
-                                              **info):
+                                              **benchmark.info):
                     logging.info(f'Time: {elapsed:.1f}s')
                     time_taken.append(elapsed)
-            logging.info(f'Completed {name}')
+            logging.info(f'Completed {benchmark.name}')
             logging.info('Result: %s', _format_result(time_taken))
-            yield name, time_taken
+            yield benchmark.name, time_taken
 
 
-def _list_benchmarks():
-    strs = ['\nBenchmarks:']
-    for name in _BENCHMARKS.keys():
+def _all_benchmark_and_suite_names() -> Iterator[str]:
+    for key in _SUITES.keys():
+        yield key
+    for benchmark in _BENCHMARKS:
+        yield benchmark.name
+
+
+def _list_benchmarks() -> str:
+    strs = ['\nSuites and Individual Benchmarks:']
+    for name in _all_benchmark_and_suite_names():
         strs.append(f'    {name}')
     return '\n'.join(strs)
 
@@ -243,8 +266,8 @@ def main():
     parser.add_argument('benchmark',
                         nargs='+',
                         metavar='BENCHMARK',
-                        choices=_BENCHMARKS.keys(),
-                        help='Names of default benchmark(s) to run.')
+                        choices=list(_all_benchmark_and_suite_names()),
+                        help='Names of benchmark(s) or suites(s) to run.')
     parser.add_argument('-A',
                         '--args',
                         choices=_GN_ARG_PRESETS.keys(),
@@ -272,7 +295,7 @@ def main():
     if args.output_directory:
         constants.SetOutputDirectory(args.output_directory)
     constants.CheckOutputDirectory()
-    out_dir = constants.GetOutDirectory()
+    out_dir: str = constants.GetOutDirectory()
 
     if args.verbose >= 2:
         level = logging.DEBUG
