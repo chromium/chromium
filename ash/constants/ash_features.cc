2c625a9811ea09ae3c778d658c934497ee28f896
Kyle Horimoto <khorimoto@chromium.org>
2021-02-19 00:33:24 +0000

    [CrOS Settings] Enable Polymer3 flag by default
    Bug: 1045266
    Change-Id: I110d152eadccc86402c0bde1dda68d4652d80c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705938
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855544}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -496,7 +496,7 @@ const base::Feature kOsSettingsDeepLinking{"OsSettingsDeepLinking",
 
 // Flips chrome://os-settings to show Polymer 3 version
 const base::Feature kOsSettingsPolymer3{"OsSettingsPolymer3",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
+                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Provides a UI for users to view information about their Android phone
 // and perform phone-side actions within Chrome OS.
