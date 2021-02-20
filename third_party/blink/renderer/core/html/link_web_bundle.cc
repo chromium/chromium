0d34b992b36d114d6deb9f3f013edfd2b4e2104d
Hayato Ito <hayato@chromium.org>
2021-02-20 00:00:29 +0000

    Change request's mode to reflect the WebBundle explainer's change
    Reflect the change of the WebBundle subresource loading explainer in
    https://github.com/WICG/webpackage/pull/622.
    
    Request for a bundle will have its mode set to "cors" and its
    credentials mode set to "same-origin" unless a crossorigin attribute
    is specified.
    
    BUG=1149816, 1168449
    
    Change-Id: Ie836e77fd50524b4019283d5237aec70cc29433b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703299
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855997}

--- third_party/blink/renderer/core/html/link_web_bundle.cc
+++ third_party/blink/renderer/core/html/link_web_bundle.cc
@@ -46,20 +46,16 @@ class WebBundleLoader : public GarbageCollected<WebBundleLoader>,
     // https://html.spec.whatwg.org/multipage/semantics.html#fetch-and-process-the-linked-resource
     request.SetRequestContext(
         mojom::blink::RequestContextType::SUBRESOURCE_WEBBUNDLE);
-    // Set request's mode and credentials mode. See
-    // https://html.spec.whatwg.org/multipage/#cors-settings-attribute
+
+    // https://github.com/WICG/webpackage/blob/main/explainers/subresource-loading.md#requests-mode-and-credentials-mode
+    request.SetMode(network::mojom::blink::RequestMode::kCors);
     switch (cross_origin_attribute_value) {
       case kCrossOriginAttributeNotSet:
-        request.SetMode(network::mojom::blink::RequestMode::kNoCors);
-        request.SetCredentialsMode(network::mojom::CredentialsMode::kInclude);
-        break;
       case kCrossOriginAttributeAnonymous:
-        request.SetMode(network::mojom::blink::RequestMode::kCors);
         request.SetCredentialsMode(
             network::mojom::CredentialsMode::kSameOrigin);
         break;
       case kCrossOriginAttributeUseCredentials:
-        request.SetMode(network::mojom::blink::RequestMode::kCors);
         request.SetCredentialsMode(network::mojom::CredentialsMode::kInclude);
         break;
     }
