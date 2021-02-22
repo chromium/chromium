49be30cfc984d4f41acd2c644b95667140048b91
Sophey Dong <sophey@chromium.org>
2021-02-22 23:56:26 +0000

    [ShareCCT] Enable ShareByDefaultInCCT by default.
    Also remove field trial testing config.
    
    Bug: 1135311
    Change-Id: I8c60f99cce363b15d47e39f5e32df8263a59cac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704979
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856510}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -595,7 +595,7 @@ const base::Feature kShareButtonInTopToolbar{"ShareButtonInTopToolbar",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kShareByDefaultInCCT{"ShareByDefaultInCCT",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kShoppingAssist{"ShoppingAssist",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
