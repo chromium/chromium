01f15e41c911f073bf3282d703cafe06b9745aa2
bttk <bttk@chromium.org>
2021-02-24 23:02:05 +0000

    Reland "Prevent calling setSelection with negative values"
    This is a reland of 386cf57bcbdb5c719b5a70155b0a4ed2d1c08abd
    
    Restrict testDarkThemeColor to phones
    Looks like all tests using theme color are restricted to run only on
    phones.
    
    Original change's description:
    > Prevent calling setSelection with negative values
    >
    > TextView#getSelectionStart() and TextView#getSelectionEnd() will return
    > -1 when there is no selection. This change prevents using negative
    > values when calling setSelection.
    >
    > Bug: 1177259
    > Change-Id: I391766ab797d58216fbf84a1371b1ff1951aacdf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705243
    > Reviewed-by: Filip Gorski <fgorski@chromium.org>
    > Commit-Queue: bttk <bttk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#855899}
    
    (cherry picked from commit ea27bfd29c741a4de76e14f664bc3678c47f640f)
    
    Bug: 1177259
    Change-Id: I997aae77703b2e361ce34a57846eecc42541076c
    Fixed: 1181041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713217
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: bttk <bttk@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#856706}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718555
    Cr-Commit-Position: refs/branch-heads/4389@{#1353}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBarViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBarViewBinder.java
@@ -10,7 +10,7 @@ import android.text.Selection;
 import android.text.TextUtils;
 import android.view.ActionMode;
 
-import androidx.annotation.ColorRes;
+import androidx.annotation.ColorInt;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
@@ -83,6 +83,7 @@ class UrlBarViewBinder {
     }
 
     private static void updateTextColors(UrlBar view, boolean useDarkTextColors) {
+        @ColorInt
         int originalHighlightColor;
         Object highlightColorObj = view.getTag(R.id.highlight_color);
         if (highlightColorObj == null || !(highlightColorObj instanceof Integer)) {
@@ -93,8 +94,11 @@ class UrlBarViewBinder {
         }
 
         Resources resources = view.getResources();
+        @ColorInt
         int textColor;
+        @ColorInt
         int hintColor;
+        @ColorInt
         int highlightColor;
         if (useDarkTextColors) {
             textColor = ApiCompatibilityUtils.getColor(resources, R.color.default_text_color_dark);
@@ -114,30 +118,31 @@ class UrlBarViewBinder {
         view.setHighlightColor(highlightColor);
     }
 
-    private static void setHintTextColor(UrlBar view, @ColorRes int textColor) {
-        // Note: Setting the hint text color only takes effect if there is not text in the URL bar.
+    private static void setHintTextColor(UrlBar view, @ColorInt int textColor) {
+        // Note: Setting the hint text color only takes effect if there is no text in the URL bar.
         //       To get around this, set the URL to empty before setting the hint color and revert
         //       back to the previous text after.
-        boolean hasNonEmptyText = false;
-        int selectionStart = 0;
-        int selectionEnd = 0;
         Editable text = view.getText();
-        if (!TextUtils.isEmpty(text)) {
-            selectionStart = view.getSelectionStart();
-            selectionEnd = view.getSelectionEnd();
-            // Make sure the setText in this block does not affect the suggestions.
-            view.setIgnoreTextChangesForAutocomplete(true);
-            view.setText("");
-            hasNonEmptyText = true;
+        if (TextUtils.isEmpty(text)) {
+            view.setHintTextColor(textColor);
+            return;
         }
+
+        int selectionStart = view.getSelectionStart();
+        int selectionEnd = view.getSelectionEnd();
+
+        // Make sure the setText in this block does not affect the suggestions.
+        view.setIgnoreTextChangesForAutocomplete(true);
+        view.setText("");
         view.setHintTextColor(textColor);
-        if (hasNonEmptyText) {
-            view.setText(text);
-            if (view.hasFocus()) {
-                Selection.setSelection(view.getText(), selectionStart, selectionEnd);
-            }
-            view.setIgnoreTextChangesForAutocomplete(false);
+        view.setText(text);
+
+        // Restore the previous selection, if there was one.
+        if (selectionStart >= 0 && selectionEnd >= 0 && view.hasFocus()) {
+            Selection.setSelection(view.getText(), selectionStart, selectionEnd);
         }
+
+        view.setIgnoreTextChangesForAutocomplete(false);
     }
 
     private UrlBarViewBinder() {}
