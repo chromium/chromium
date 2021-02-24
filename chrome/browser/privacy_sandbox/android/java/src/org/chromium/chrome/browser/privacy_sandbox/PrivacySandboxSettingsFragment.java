8924fcb029b3db817310644f38b3204bcfcbf92f
Andrey Zaytsev <andzaytsev@google.com>
2021-02-24 15:51:45 +0000

    Privacy Sandbox Android: updated the link to the help website
    Bug: 1152351
    Change-Id: I99922acb864ea08e5c0aefb4485974ecd8624909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718223
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857186}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
@@ -34,8 +34,7 @@ import org.chromium.ui.widget.ChromeBulletSpan;
  */
 public class PrivacySandboxSettingsFragment
         extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
-    public static final String PRIVACY_SANDBOX_URL =
-            "https://www.chromium.org/Home/chromium-privacy/privacy-sandbox";
+    public static final String PRIVACY_SANDBOX_URL = "https://www.google.com/chrome/privacysandbox";
     // Key for the argument with which the PrivacySandbox fragment will be launched. The value for
     // this argument should be part of the PrivacySandboxReferrer enum, which contains all points of
     // entry to the Privacy Sandbox UI.
