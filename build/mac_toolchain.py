dc199c1f7e59da78782a59b85d9daad8eb29e55c
Nico Weber <thakis@chromium.org>
2021-02-24 17:11:36 +0000

    mac: Update hermetic Xcode to 12.4
    Bug: none
    Change-Id: Ia352902b31ce1e2d1586e7d3d7bee869a39e16a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715984
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857225}

--- build/mac_toolchain.py
+++ build/mac_toolchain.py
@@ -29,10 +29,10 @@ import shutil
 import subprocess
 import sys
 
-# This contains binaries from Xcode 12.3 12C33, along with the macOS 11 SDK.
+# This contains binaries from Xcode 12.4 12D4e, along with the macOS 11 SDK.
 # To build these packages, see comments in build/xcode_binaries.yaml
 MAC_BINARIES_LABEL = 'infra_internal/ios/xcode/xcode_binaries/mac-amd64'
-MAC_BINARIES_TAG = 'B3GggjMmDaPMaCzmzKR4fJWoMZlF0PR0Rf3ZTtPkU18C'
+MAC_BINARIES_TAG = 'Za4aUIwiTUjk8rnjRow4nXbth-j7ZoN5plyOSCLidcgC'
 
 # The toolchain will not be downloaded if the minimum OS version is not met. 19
 # is the major version number for macOS 10.15. 12B5044c (Xcode 12.2rc) only runs
