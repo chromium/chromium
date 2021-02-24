9d1094800519a3c43c303a800ad9d587b89fe766
Sergey Poromov <poromov@chromium.org>
2021-02-24 07:38:05 +0000

    Revert "Reland "Enable pseudolocales on non-official builds""
    This reverts commit d579e80e7818a2c51048d597a806014b5da33ab5.
    
    Reason for revert: Broke Win builds and lead to tree closure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/win-archive-rel/22015/overview
    
    Original change's description:
    > Reland "Enable pseudolocales on non-official builds"
    >
    > This reverts commit e108d13905e06f9c73d0f2e29c4c0db7a95aa2c9.
    >
    > Reason for revert: Bug was already fixed by reverting the other offending CL (crrev.com/c/2716583)
    >
    > Original change's description:
    > > Revert "Enable pseudolocales on non-official builds"
    > >
    > > This reverts commit f36074ea4d7522cbdfbdc550fa91cb24104ccb47.
    > >
    > > Reason for revert: Looks like this broke the build:
    > >
    > > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ozone-rel/145906/overview
    > >
    > > Original change's description:
    > > > Enable pseudolocales on non-official builds
    > > >
    > > > BUG=chromium:1173812
    > > > TEST=Built chrome, confirmed that pseudolocales work as expected.
    > > >
    > > > Change-Id: I3c4b04c295f4c7b705733749e331b9bbf133b38e
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672624
    > > > Reviewed-by: Sean Topping <seantopping@chromium.org>
    > > > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > > > Reviewed-by: Trevor  Perrier <perrier@chromium.org>
    > > > Commit-Queue: Matt Stark <msta@google.com>
    > > > Cr-Commit-Position: refs/heads/master@{#856953}
    > >
    > > Bug: chromium:1173812
    > > Change-Id: I4d826dc6259b76049078da88d255fc6b8287fb7d
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716549
    > > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#856958}
    >
    > Bug: chromium:1173812
    > Change-Id: I5bb9ce018a801f8f690bd8adfaca55a491c03880
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717025
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Matt Stark <msta@google.com>
    > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#857027}
    
    Bug: chromium:1173812
    Change-Id: Ie7b8f2e3e4dc569131fb79be000ea9e92a0d8430
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716564
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857049}

--- chromecast/app/verify_cast_locales.py
+++ chromecast/app/verify_cast_locales.py
@@ -17,7 +17,6 @@ CAST_LOCALES = [
     'pl', 'pt-BR', 'pt-PT', 'ro', 'ru', 'sk', 'sl', 'sr', 'sv', 'sw', 'ta',
     'te', 'th', 'tr', 'uk', 'vi', 'zh-CN', 'zh-TW'
 ]
-PSEUDOLOCALES = ['ar-XB', 'en-XA']
 
 SUCCESS_RETURN_CODE = 0
 FAILURE_RETURN_CODE = 1
@@ -40,15 +39,13 @@ def main():
                       help='The script will stamp this file if successful.')
   args = parser.parse_args()
 
-  # The lists do not match. Compute the difference and log it to the developer.
-  removed_locales = set(CAST_LOCALES) - set(args.locales)
-  # Pseudolocales may or may not be added to the build, depending on the
-  # enable_pseudolocales flag. Don't worry about whether they're here.
-  added_locales = set(args.locales) - set(CAST_LOCALES) - set(PSEUDOLOCALES)
-
-  if not removed_locales and not added_locales:
+  if set(CAST_LOCALES) == set(args.locales):
     open(args.stamp_file, 'w')
     return SUCCESS_RETURN_CODE
+
+  # The lists do not match. Compute the difference and log it to the developer.
+  removed_locales = set(CAST_LOCALES) - set(args.locales)
+  added_locales = set(args.locales) - set(CAST_LOCALES)
 
   print('CAST_LOCALES no longer matches the locales list from GN!')
   if removed_locales:
