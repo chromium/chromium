533b9c2d2f49d2e85b00e72fe59ae463ba69306d
Callistus <callistus@google.com>
2021-02-25 04:07:49 +0000

    Revert future notification messages from M90 onwards to original message.
    The current message is only meant for M89.
    
    Bug: b/177378199
    Change-Id: I5bcef1157ec4b21de74adae01cff95514f2e92ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717141
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#857530}

--- chrome/browser/chromeos/release_notes/release_notes_notification_unittest.cc
+++ chrome/browser/chromeos/release_notes/release_notes_notification_unittest.cc
@@ -97,7 +97,7 @@ TEST_F(ReleaseNotesNotificationTest, ShowReleaseNotesNotification) {
   EXPECT_EQ(ui::SubstituteChromeOSDeviceType(
                 IDS_RELEASE_NOTES_DEVICE_SPECIFIC_NOTIFICATION_TITLE),
             GetReleaseNotesNotification().title());
-  EXPECT_EQ("Work better across devices, do things faster, and more",
+  EXPECT_EQ("Get highlights from the latest update",
             base::UTF16ToASCII(GetReleaseNotesNotification().message()));
   EXPECT_EQ(1, notification_count_);
 }
