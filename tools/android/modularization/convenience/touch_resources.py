0aa1954f7ed49d9f012cce7a11b18c992406a0eb
ckitagawa <ckitagawa@chromium.org>
2021-02-25 21:33:58 +0000

    [Android] Make modularization util script BUILD.gn changes automatic
    This automates some of the BUILD.gn suggestions in touch_resources.py
    it handles a number of cases, but is likely imperfect and certainly
    unable to handle complex BUILD.gn files. It can however, suggest a
    diff which the user can then choose whether to apply.
    
    Bug: 1170804
    Change-Id: I4f70aed66ab0719ba311c0cbc82a62bc4a8c5eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687657
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857845}

--- tools/android/modularization/convenience/touch_resources.py
+++ tools/android/modularization/convenience/touch_resources.py
@@ -6,8 +6,7 @@ r"""Creates Android resources directories and boilerplate files for a module.
 
 This is a utility script for conveniently creating resources directories and
 values .xml files in modules prefilled with boilerplate and example usages. It
-prints out suggested changes to the BUILD.gn to include those resources but does
-not apply them automatically.
+prints out suggested changes to the BUILD.gn and will apply them if accepted.
 
 Examples:
 
@@ -32,8 +31,9 @@ Touch drawable directories in module foo for hdpi, xhdpi and xxdpi:
 import argparse
 import datetime
 import pathlib
-import subprocess
 from typing import List, Optional, Tuple
+
+import build_gn_editor
 
 _IGNORED_FILES_IN_RES = {'DIR_METADATA', 'OWNERS'}
 
@@ -156,8 +156,64 @@ def main():
 
   # Print out build target suggestions
   all_resources.sort(key=str)
-  if all_resources:
+  if not all_resources:
+    return
+
+  build_file = build_gn_editor.BuildFile(build_gn_path)
+  build_gn_changes_ok = _update_build_file(build_file, all_resources)
+
+  if not build_gn_changes_ok:
     _print_build_target_suggestions(build_gn_path, all_resources)
+    return
+
+  print('Final delta:')
+  print(build_file.get_diff())
+  apply_changes = _yes_or_no('Would you like to apply these changes?')
+  if not apply_changes:
+    return
+
+  build_file.write_content_to_file()
+
+
+def _yes_or_no(question: str) -> bool:
+  val = input(question + ' [(y)es/(N)o] ')
+  try:
+    y_or_n = val.lower().strip()
+    return y_or_n[0] == 'y'
+  except Exception:
+    print('Invalid input. Assuming No.')
+    return False
+
+
+def _determine_target_to_use(targets: List[str], target_type: str,
+                             default_name: str) -> Optional[str]:
+  num_targets = len(targets)
+  if not num_targets:
+    print(f'Found no existing {target_type} will create ":{default_name}".')
+    return default_name
+  elif num_targets == 1:
+    print(f'Found existing target {target_type}("{targets[0]}"), using it.')
+    return targets[0]
+  else:
+    print(f'Found multiple existing {target_type} targets, pick one: ')
+    return _enumerate_targets_and_ask(targets)
+
+
+def _enumerate_targets_and_ask(targets: List[str]) -> Optional[str]:
+  for i, target in enumerate(targets):
+    print(f'{i + 1}: {target}')
+
+  try:
+    val = int(
+        input('Enter the number corresponding the to target you want to '
+              'use: ')) - 1
+  except ValueError:
+    return None
+
+  if 0 <= val < len(targets):
+    return targets[val]
+
+  return None
 
 
 def _identify_module_structure(path_argument: str
@@ -294,43 +350,183 @@ def _touch_subdirectories(resources_path: pathlib.Path, subdirectory: str,
       print(f'{subdir_path} already exists.')
 
 
-def _print_build_target_suggestions(build_gn_path: pathlib.Path,
-                                    new_resources: List[pathlib.Path]) -> None:
+def _generate_resources_sources(build_gn_dir_path: pathlib.Path,
+                                new_resources: List[pathlib.Path]) -> List[str]:
+  return [f'"{str(r.relative_to(build_gn_dir_path))}"' for r in new_resources]
+
+
+def _list_to_lines(lines, indent):
+  spaces = ' ' * indent
+  return '\n'.join([f'{spaces}{line},' for line in lines])
+
+
+def _generate_suggested_resources_deps() -> List[str]:
+  return [f'# "{dep}"' for dep in _COMMON_RESOURCE_DEPS]
+
+
+def _generate_resources_content(build_gn_path: pathlib.Path,
+                                new_resources: List[pathlib.Path], *,
+                                include_comment: bool) -> str:
   build_gn_dir_path = build_gn_path.parent
-  new_resources_strs = [
-      str(r.relative_to(build_gn_dir_path)) for r in new_resources
-  ]
-  new_resources_lines = '\n'.join(
-      [f'    "{src}",' for src in new_resources_strs])
-  suggested_deps_lines = '\n'.join(
-      [f'    # "{dep}",' for dep in _COMMON_RESOURCE_DEPS])
-  resources_target_suggestion = f"""
-android_resources("java_resources") {{
-  sources = [
+  new_resources_lines = _list_to_lines(
+      _generate_resources_sources(build_gn_dir_path, new_resources), 4)
+  suggested_deps_lines = _list_to_lines(_generate_suggested_resources_deps(), 4)
+  comment = ''
+  if include_comment:
+    comment = ('\n    # Commonly required resources deps for convenience, ' +
+               'add other required deps and remove unnecessary ones.')
+  resources_content = f"""sources = [
 {new_resources_lines}
   ]
-  deps = [
-    # Commonly required resources deps for convenience, add other required deps and remove unnecessary ones.
-{suggested_deps_lines}
-  ]
-}}"""
-  print(f'Suggestion for {build_gn_path}:')
-  print(resources_target_suggestion)
 
+  deps = [{comment}
+{suggested_deps_lines}
+  ]"""
+  return resources_content
+
+
+def _generate_suggested_resources(build_gn_path: pathlib.Path,
+                                  new_resources: List[pathlib.Path]) -> str:
+  resources_content = _generate_resources_content(build_gn_path,
+                                                  new_resources,
+                                                  include_comment=True)
+  resources_target_suggestion = f"""
+android_resources("java_resources") {{
+  {resources_content}
+}}"""
+  return resources_target_suggestion
+
+
+def _generate_suggested_java_package(build_gn_path: pathlib.Path) -> str:
+  build_gn_dir_path = build_gn_path.parent
   parts_for_package = build_gn_dir_path.parts
   # internal, public or android subdirectories are not part of the Java package.
   while parts_for_package[-1] in ('internal', 'public', 'android'):
     parts_for_package = parts_for_package[:-1]
-  suggested_java_package = f'org.chromium.{".".join(parts_for_package)}'
+  return f'org.chromium.{".".join(parts_for_package)}'
+
+
+def _generate_library_content(build_gn_path: pathlib.Path,
+                              resources_target_name: str) -> str:
+  suggested_java_package = _generate_suggested_java_package(build_gn_path)
+  library_content = f"""deps = [
+     ":{resources_target_name}",
+  ]
+
+  resources_package = "{suggested_java_package}" """
+  return library_content
+
+
+def _generate_library_target(build_gn_path: pathlib.Path,
+                             resources_target_name: str) -> str:
+  library_content = _generate_library_content(build_gn_path,
+                                              resources_target_name)
   android_library_target_suggestion = f"""
 android_library("java") {{
-  [...]
-  deps = [
-     ":java_resources",
-     [...]
-  ]
-  resources_package = "{suggested_java_package}"
+  {library_content}
 }}"""
+  return android_library_target_suggestion
+
+
+def _create_or_update_variable_list(target: build_gn_editor.BuildTarget,
+                                    variable_name: str,
+                                    elements: List[str]) -> None:
+  variable = target.get_variable(variable_name)
+  if variable:
+    variable_list = variable.get_content_as_list()
+    if not variable_list:
+      raise build_gn_editor.BuildFileUpdateError(
+          f'{target.get_type()}("{target.get_name()}") '
+          f'{variable_name} is not a list.')
+
+    variable_list.add_elements(elements)
+    variable.set_content_from_list(variable_list)
+    target.replace_variable(variable)
+    return
+
+  variable = build_gn_editor.TargetVariable(variable_name, '')
+  variable_list = build_gn_editor.VariableContentList()
+  variable_list.add_elements(elements)
+  variable.set_content_from_list(variable_list)
+  target.add_variable(variable)
+
+
+def _update_build_file(build_file: build_gn_editor.BuildFile,
+                       all_resources: List[pathlib.Path]) -> bool:
+  libraries = build_file.get_target_names_of_type('android_library')
+  resources = build_file.get_target_names_of_type('android_resources')
+
+  library_target = _determine_target_to_use(libraries, 'android_library',
+                                            'java')
+  resources_target = _determine_target_to_use(resources, 'android_resources',
+                                              'java_resources')
+  if not library_target or not resources_target:
+    print('Invalid build target selections. Aborting BUILD.gn changes.')
+    return False
+
+  try:
+    _update_build_targets(build_file, all_resources, library_target,
+                          resources_target)
+  except build_gn_editor.BuildFileUpdateError as e:
+    print(f'Changes to build targets failed: {e}. Aborting BUILD.gn changes.')
+    return False
+
+  try:
+    build_file.format_content()
+  except build_gn_editor.BuildFileUpdateError as e:
+    print(f'Formatting BUILD gn failed: {e}\n Aborting BUILD.gn changes')
+    return False
+
+  return True
+
+
+def _update_build_targets(build_file: build_gn_editor.BuildFile,
+                          all_resources: List[pathlib.Path],
+                          library_target: str, resources_target: str) -> None:
+  resources = build_file.get_target('android_resources', resources_target)
+  if not resources:
+    resources = build_gn_editor.BuildTarget(
+        'android_resources', resources_target,
+        _generate_resources_content(build_file.get_path(),
+                                    all_resources,
+                                    include_comment=False))
+    build_file.add_target(resources)
+  else:
+    _create_or_update_variable_list(
+        resources, 'sources',
+        _generate_resources_sources(build_file.get_path().parent,
+                                    all_resources))
+    _create_or_update_variable_list(resources, 'deps',
+                                    _generate_suggested_resources_deps())
+    build_file.replace_target(resources)
+
+  library = build_file.get_target('android_library', library_target)
+  if not library:
+    library = build_gn_editor.BuildTarget(
+        'android_library', library_target,
+        _generate_library_content(build_file.get_path(), resources_target))
+    build_file.add_target(library)
+  else:
+    _create_or_update_variable_list(library, 'deps', [f'":{resources_target}"'])
+
+    resources_package = library.get_variable('resources_package')
+    if not resources_package:
+      resources_package_str = _generate_suggested_java_package(
+          build_file.get_path())
+      library.add_variable(
+          build_gn_editor.TargetVariable('resources_package',
+                                         f'"{resources_package_str}"'))
+    build_file.replace_target(library)
+
+
+def _print_build_target_suggestions(build_gn_path: pathlib.Path,
+                                    new_resources: List[pathlib.Path]) -> None:
+  resources_target_suggestion = _generate_suggested_resources(
+      build_gn_path, new_resources)
+  android_library_target_suggestion = _generate_library_target(
+      build_gn_path, 'java_resources')
+  print(f'Suggestion for {build_gn_path}:')
+  print(resources_target_suggestion)
   print(android_library_target_suggestion)
   print()
 
