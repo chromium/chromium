2f2e8f66a6857515da7e03462518a5283dafdae2
John Abd-El-Malek <jam@chromium.org>
2021-02-25 19:18:22 +0000

    Revert "Reland "[bfcache] Enable BackForwardCache by default on Android""
    This reverts commit f42eb8d1666e0d4e4f6b63b4106e2deccc2f96f6.
    
    Reason for revert: this inadvertently enabled it on WebView & WebLayer
    
    Original change's description:
    > Reland "[bfcache] Enable BackForwardCache by default on Android"
    >
    > This is a reland of 2dd3a9c3e2dd8e4f32337d99ad80933c6b7ab4ec
    > after allowing tests to enable BackForwardCache for all memory
    > devices (https://crrev.com/c/2671525).
    >
    > Original change's description:
    > > [bfcache] Enable BackForwardCache by default on Android
    > >
    > > After approval for launch, This CL enables
    > > BackForwardCache on the tip of the tree by default for Android.
    > >
    > > We also enable BackForwardCacheMemoryControl on Android to enable
    > > BackForwardCache only for higher memory devices (2GB+).
    > > The default value is set to 1700 MB to account for
    > > all 2GB+ devices which report lower RAM due to carveouts.
    > >
    > > BUG=1157426
    > >
    > > Change-Id: I9a80989507a88527b5e123141b48b18868d9d8dd
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581979
    > > Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    > > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#848296}
    >
    > Bug: 1157426
    > Change-Id: I1af6cda91b1b60c3c5607962013deff6fa5961a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671688
    > Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851032}
    
    Bug: 1157426
    Change-Id: Icc1061a4e952c20de0335733f249f226f596aa3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719093
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857685}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -68,20 +68,8 @@ const base::Feature kBackgroundFetch{"BackgroundFetch",
                                      base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enable using the BackForwardCache.
-// BackForwardCache is enabled only on Android for the moment, as some
-// desktop-specific features (including extensions) are not compatible with
-// bfcache yet. Tracking bug for enabling bfcache on desktop:
-// https://crbug.com/1171298.
-// Please note that while enabling BackForwardCache feature for tests, disable
-// BackForwardCacheMemoryControl to allow BackForwardCache for all devices
-// regardless of their memory.
-#if defined(OS_ANDROID)
-const base::Feature kBackForwardCache{"BackForwardCache",
-                                      base::FEATURE_ENABLED_BY_DEFAULT};
-#else
 const base::Feature kBackForwardCache{"BackForwardCache",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
-#endif
 
 // BackForwardCache is disabled on low memory devices. The threshold is defined
 // via a field trial param: "memory_threshold_for_back_forward_cache_in_mb"
