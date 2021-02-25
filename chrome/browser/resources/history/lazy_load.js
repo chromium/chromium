3ad437fbc1fa8d4979e13b03da81536fb32528f8
dpapad <dpapad@chromium.org>
2021-02-25 08:11:46 +0000

    WebUI: Remove cr_toolbar_selection_overlay P2, check-in Polymer3.
    Bug: 1173575
    Change-Id: I850cdbb2710fdb1e73e55d931bb04e5a9698177c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716981
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857583}

--- chrome/browser/resources/history/lazy_load.js
+++ chrome/browser/resources/history/lazy_load.js
@@ -9,4 +9,4 @@ import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
 import 'chrome://resources/cr_elements/cr_drawer/cr_drawer.m.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
-import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_selection_overlay.m.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_selection_overlay.js';
