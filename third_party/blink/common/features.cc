2cf8841b2c129c0f8ad57c34c66114a85f13d404
Guido Urdaneta <guidou@chromium.org>
2021-02-25 20:13:34 +0000

    Enable browser-side feature for getCurrentBrowsingContextMedia().
    This base::Feature is intended to be used as a kill switch on the
    browser process and should be enabled by default.
    The actual API feature is controlled by the
    "GetCurrentBrowsingContextMedia" Blink runtime enabled feature,
    which is disabled by default and on Origin Trial.
    
    Bug: 1136942
    Change-Id: I81948284342c8646b862ac9d6cb67ae503086459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720204
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857779}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -198,10 +198,10 @@ const base::Feature kInsertKeyToggleMode = {"InsertKeyToggleMode",
 const base::Feature kRawClipboard{"RawClipboard",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
 
-// Enables usage of getCurrentBrowsingContextMedia() that allows capturing of
+// Kill switch for getCurrentBrowsingContextMedia(), which allows capturing of
 // web content from the tab from which it is called. (crbug.com/1136940)
 const base::Feature kRTCGetCurrentBrowsingContextMedia{
-    "RTCGetCurrentBrowsingContextMedia", base::FEATURE_DISABLED_BY_DEFAULT};
+    "RTCGetCurrentBrowsingContextMedia", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Changes the default RTCPeerConnection constructor behavior to use Unified
 // Plan as the SDP semantics. When the feature is enabled, Unified Plan is used
