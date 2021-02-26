42421b1ed7ac3f9a17ab1d8b0bbd7efe9c26f5e4
John Lee <johntlee@chromium.org>
2021-02-26 19:55:22 +0000

    Side panel: Add tabs, style list items
    Bug: 1181931
    Change-Id: I27d0768e905ce9af8fea8c1c6777eca8253fa025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720602
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858230}

--- chrome/browser/resources/read_later/side_panel/side_panel.js
+++ chrome/browser/resources/read_later/side_panel/side_panel.js
@@ -18,24 +18,92 @@ class SidePanel extends PolymerElement {
 
   static get template() {
     return html`
-      <style include="mwb-shared-style"></style>
+      <style include="mwb-shared-style">
+        #tabs {
+          display: flex;
+          height: 48px;
+          position: relative;
+          width: 100%;
+        }
 
-      <h2>Bookmarks</h2>
-      <template is="dom-repeat" items="[[bookmarkFolders_]]">
-        <bookmark-folder folder="[[item]]"></bookmark-folder>
-      </template>
+        #tabs a {
+          cursor: pointer;
+          display: block;
+          flex: 1;
+          line-height: 48px;
+          text-align: center;
+        }
 
-      <h2>Unread items</h2>
-      <template is="dom-repeat" items="[[unreadItems_]]">
-        <read-later-item class="mwb-list-item" data="[[item]]">
-        </read-later-item>
-      </template>
+        #tabs span {
+          position: relative;
+        }
 
-      <h2>Read items</h2>
-      <template is="dom-repeat" items="[[readItems_]]">
-        <read-later-item class="mwb-list-item" data="[[item]]">
-        </read-later-item>
-      </template>
+        #tabs a[active] span::after {
+          content: '';
+          display: block;
+          background: #1A73E8;
+          bottom: -8px;
+          border-radius: 3px 3px 0 0;
+          height: 3px;
+          left: 2px;
+          position: absolute;
+          right: 2px;
+        }
+
+        #tabsBorder {
+          background: #dadce0;
+          bottom: 8px;
+          height: 1px;
+          position: absolute;
+          width: 100%;
+        }
+
+        .tab {
+          display: none;
+        }
+
+        .tab[active] {
+          display: block;
+        }
+
+        h2 {
+          font-size: 11px;
+          font-weight: normal;
+          padding: 16px 16px 8px 16px;
+          text-transform: uppercase;
+        }
+      </style>
+
+      <div id="tabs">
+        <template is="dom-repeat" items="[[tabs_]]">
+          <a on-click="activateTab_"
+              active$="[[isActiveTab_(item.id, activeTab_)]]">
+            <span>[[item.label]]</span>
+          </a>
+        </template>
+        <div id="tabsBorder"></div>
+      </div>
+
+      <div id="readingList" class="tab"
+          active$="[[isActiveTab_('readingList', activeTab_)]]">
+        <h2>Unread</h2>
+        <template is="dom-repeat" items="[[unreadItems_]]">
+          <read-later-item class="mwb-list-item" data="[[item]]">
+          </read-later-item>
+        </template>
+        <h2>Read</h2>
+        <template is="dom-repeat" items="[[readItems_]]">
+          <read-later-item class="mwb-list-item" data="[[item]]">
+          </read-later-item>
+        </template>
+      </div>
+
+      <div id="bookmarks" class="tab"
+          active$="[[isActiveTab_('bookmarks', activeTab_)]]">
+        <template is="dom-repeat" items="[[bookmarkFolders_]]">
+          <bookmark-folder folder="[[item]]"></bookmark-folder>
+        </template>
+      </div>
     `;
   }
 
@@ -53,11 +121,31 @@ class SidePanel extends PolymerElement {
         type: Array,
         value: () => [],
       },
+      tabs_: {
+        type: Array,
+        value: () => {
+          return [
+            {
+              id: 'readingList',
+              label: 'Reading list',
+            },
+            {
+              id: 'bookmarks',
+              label: 'Bookmarks',
+            },
+          ];
+        },
+      },
+      activeTab_: {
+        type: Object,
+        value: () => {},
+      },
     };
   }
 
   connectedCallback() {
     super.connectedCallback();
+    this.activeTab_ = this.tabs_[0];
     readLaterApi.getReadLaterEntries().then(({entries}) => {
       this.unreadItems_ = entries.unreadEntries;
       this.readItems_ = entries.readEntries;
@@ -65,6 +153,14 @@ class SidePanel extends PolymerElement {
     chrome.bookmarks.getTree(([{children}]) => {
       this.bookmarkFolders_ = children;
     });
+  }
+
+  activateTab_(event) {
+    this.activeTab_ = event.model.item;
+  }
+
+  isActiveTab_(tabId) {
+    return this.activeTab_.id === tabId;
   }
 }
 customElements.define(SidePanel.is, SidePanel);
@@ -76,9 +172,13 @@ class BookmarkFolder extends PolymerElement {
 
   static get template() {
     return html`
-      <div><b>[[folder.title]]</b></div>
       <template is="dom-repeat" items="[[folder.children]]">
-        <bookmark-item item="[[item]]"></bookmark-item>
+        <template is="dom-if" if="[[item.children]]">
+          <bookmark-folder folder="[[item]]"></bookmark-folder>
+        </template>
+        <template is="dom-if" if="[[!item.children]]">
+          <bookmark-item item="[[item]]"></bookmark-item>
+        </template>
       </template>
     `;
   }
@@ -101,8 +201,36 @@ class BookmarkItem extends PolymerElement {
 
   static get template() {
     return html`
-      <img src="chrome://favicon/[[item.url]]">
-      <div>[[item.title]]</div>
+      <style>
+        :host {
+          align-items: center;
+          cursor: pointer;
+          display: flex;
+          height: 40px;
+          line-height: 40px;
+          overflow: hidden;
+          padding: 0 16px;
+          white-space: nowrap;
+        }
+
+        :host(:hover) {
+          background: var(--mwb-list-item-hover-background-color);
+        }
+
+        #favicon {
+          width: 16px;
+          height: 16px;
+          margin-right: 16px;
+        }
+
+        #title {
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      </style>
+
+      <img id="favicon" src="chrome://favicon/[[item.url]]">
+      <div id="title">[[item.title]]</div>
     `;
   }
 
