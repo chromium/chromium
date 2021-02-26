b03fc4c7ebcc3dc1ad2330aed963fb371233ed25
Ray Park <pkoarmy@yahoo.co.kr>
2021-02-26 15:52:35 -0800

    Updated Yahoo! Search domains to use HTTPS

--- chrome/browser/search_engines/template_url_parser_unittest.cc
+++ chrome/browser/search_engines/template_url_parser_unittest.cc
@@ -206,14 +206,14 @@ TEST_F(TemplateURLParserTest, TestFirefoxYahoo) {
   ASSERT_TRUE(template_url_);
   EXPECT_EQ(ASCIIToUTF16("Yahoo"), template_url_->short_name());
   EXPECT_TRUE(template_url_->url_ref().SupportsReplacement(SearchTermsData()));
-  EXPECT_EQ("http://ff.search.yahoo.com/gossip?"
+  EXPECT_EQ("https://ff.search.yahoo.com/gossip?"
             "output=fxjson&command={searchTerms}",
             template_url_->suggestions_url());
-  EXPECT_EQ("http://search.yahoo.com/search?p={searchTerms}&ei=UTF-8",
+  EXPECT_EQ("https://search.yahoo.com/search?p={searchTerms}&ei=UTF-8",
             template_url_->url());
   ASSERT_EQ(1U, template_url_->input_encodings().size());
   EXPECT_EQ("UTF-8", template_url_->input_encodings()[0]);
-  EXPECT_EQ(GURL("http://search.yahoo.com/favicon.ico"),
+  EXPECT_EQ(GURL("https://search.yahoo.com/favicon.ico"),
             template_url_->favicon_url());
 }
 
@@ -228,11 +228,11 @@ TEST_F(TemplateURLParserTest, TestPostSuggestion) {
   EXPECT_EQ(ASCIIToUTF16("Yahoo"), template_url_->short_name());
   EXPECT_TRUE(template_url_->url_ref().SupportsReplacement(SearchTermsData()));
   EXPECT_TRUE(template_url_->suggestions_url().empty());
-  EXPECT_EQ("http://search.yahoo.com/search?p={searchTerms}&ei=UTF-8",
+  EXPECT_EQ("https://search.yahoo.com/search?p={searchTerms}&ei=UTF-8",
             template_url_->url());
   ASSERT_EQ(1U, template_url_->input_encodings().size());
   EXPECT_EQ("UTF-8", template_url_->input_encodings()[0]);
-  EXPECT_EQ(GURL("http://search.yahoo.com/favicon.ico"),
+  EXPECT_EQ(GURL("https://search.yahoo.com/favicon.ico"),
             template_url_->favicon_url());
 }
 
