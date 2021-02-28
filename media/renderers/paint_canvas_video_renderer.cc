78a630c48137704bacb0cb84ed67337f43d60c67
Dale Curtis <dalecurtis@chromium.org>
2021-02-28 01:59:34 +0000

    Delete shared images produced for YUV texture uploads.
    SharedImage backing isn't packed in a scoper, so just going out of
    scope is insufficient to release the shared image. Clients must
    call DestroySharedImage(). As such ~YUVTextureCache not doing that
    leads to a leak. The fix is to call YUVTextureCache::Reset().
    
    R=​sandersd
    
    (cherry picked from commit d34f6356a783b9d7f117fe15aaf3152bf860d23e)
    
    Fixed: 1182476
    Change-Id: I40c7530d5b3f2ee6b4d8f8e6af15613df81c4f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721105
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#858007}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726096
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#19}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -1947,7 +1947,9 @@ bool PaintCanvasVideoRenderer::PrepareVideoFrame(
 }
 
 PaintCanvasVideoRenderer::YUVTextureCache::YUVTextureCache() = default;
-PaintCanvasVideoRenderer::YUVTextureCache::~YUVTextureCache() = default;
+PaintCanvasVideoRenderer::YUVTextureCache::~YUVTextureCache() {
+  Reset();
+}
 
 void PaintCanvasVideoRenderer::YUVTextureCache::Reset() {
   if (mailbox.IsZero())
