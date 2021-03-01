2a3c70faa8d2e556a9a694751b833f20c5df8409
Alice Wang <aliceywang@chromium.org>
2021-03-01 16:21:15 +0000

    [Android] Remove redundant profile data update in SignInPreference
    This CL removes the redundant manual profile data update in
    SignInPreference since the profile data cache is populated
    automatically when the GmsProfileDataSource is not available.
    
    As ProfileDataCache#update() does nothing when GmsProfileDataSource is
    available, this CL changes nothing when there is a valid
    GmsProfileDataSource.
    
    Bug: 1183296
    Change-Id: Ia7fc03bb38543c8df457aa6664746e77d82ad7b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726900
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858568}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
@@ -38,7 +38,6 @@ import org.chromium.ui.base.ViewUtils;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.Collections;
 
 /**
  * A preference that displays "Sign in to Chrome" when the user is not sign in, and displays
@@ -203,7 +202,6 @@ public class SignInPreference
 
     private void setupSignedIn(String accountName) {
         mState = State.SIGNED_IN;
-        mProfileDataCache.update(Collections.singletonList(accountName));
         DisplayableProfileData profileData = mProfileDataCache.getProfileDataOrDefault(accountName);
 
         setTitle(profileData.getFullNameOrEmail());
