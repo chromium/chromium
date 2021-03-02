5c5a28be6e362c1c80ccfc60e00e9d1d7fb59246
Alice Wang <aliceywang@chromium.org>
2021-03-02 07:27:10 +0000

    [Signin][Android] Change AccountPickerFeatureUtils to package-private
    This CL changes the class AccountPickerFeatureUtils to package-private
    as the modularization is already finished for the account_picker
    package.
    
    Bug: 1155123
    Change-Id: I9233324ef9aa7b92b46aa56693b1e7cff9f69a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727256
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858918}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerFeatureUtils.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerFeatureUtils.java
@@ -9,17 +9,12 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 /**
  * This class is used to handle state of feature flags in the project
  * MobileIdentityConsistency.
- *
- * TODO(crbug.com/1155123): Change the class to package private after modularization
  */
-public class AccountPickerFeatureUtils {
+class AccountPickerFeatureUtils {
     private static final String DISMISS_BUTTON_PARAM = "dismiss_button";
     private static final String HIDE_DISMISS_BUTTON = "hide";
 
-    /**
-     * TODO(crbug.com/1155123): Change the method to package private after modularization
-     */
-    public static boolean shouldHideDismissButton() {
+    static boolean shouldHideDismissButton() {
         return HIDE_DISMISS_BUTTON.equals(ChromeFeatureList.getFieldTrialParamByFeature(
                 ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY_VAR, DISMISS_BUTTON_PARAM));
     }
