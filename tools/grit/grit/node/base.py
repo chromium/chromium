84ef659584d3beb83b44cc168d02244dbd6b8f87
Wojciech Bielawski <wbielawski@fb.com>
2021-03-02 17:46:16 +0000

    Make grit's target platform compatible with python 3.
    Since python 3.3 sys.platform doesn't contain the major version on linux.
    
    Bug: 1112471
    Change-Id: I29bad863bbb04f30eaa727263502a0650ba5785e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718153
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/master@{#859035}

--- tools/grit/grit/node/base.py
+++ tools/grit/grit/node/base.py
@@ -499,8 +499,8 @@ class Node(object):
       elif name == 'is_bsd':
         value = 'bsd' in target_platform
       elif name == 'is_posix':
-        value = (target_platform in ('darwin', 'linux2', 'linux3', 'sunos5',
-                                     'android', 'ios')
+        value = (target_platform.startswith('linux')
+                 or target_platform in ('darwin', 'sunos5', 'android', 'ios')
                  or 'bsd' in target_platform)
 
       elif name == 'pp_ifdef':
