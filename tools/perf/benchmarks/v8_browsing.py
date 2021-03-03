a49160549979d8beae0dd5a299b2d2ead0c56e0f
Camillo Bruni <cbruni@chromium.org>
2021-03-03 09:21:44 +0000

    [perf] Adding convenience v8.loading story
    - Move AugmentOptionsForV8BrowsingMetrics to v8_helper
    - Add v8_helper.V8PerfMixin for common V8 metrics
    - Add disabled v8.loading for perf investigations
    
    Change-Id: I30c7196c628d2ea70b4cc06456c183897fa7d96c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684363
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859303}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -2,66 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from core import perf_benchmark
-from core import platforms
+from core import platforms, perf_benchmark
 
 from telemetry import benchmark
 from telemetry import story
-from telemetry.timeline import chrome_trace_config
-from telemetry.web_perf import timeline_based_measurement
 import page_sets
+from benchmarks import v8_helper
 
 
-def AugmentOptionsForV8BrowsingMetrics(options, enable_runtime_call_stats=True):
-  categories = [
-      # Disable all categories by default.
-      '-*',
-      # Memory categories.
-      'disabled-by-default-memory-infra',
-      'toplevel',
-      # V8 categories.
-      'cppgc',
-      'disabled-by-default-cppgc',
-      'disabled-by-default-v8.gc',
-      'v8',
-      'v8.wasm',
-      'v8.console',
-      'webkit.console',
-      # Blink categories.
-      'blink_gc',
-      'partition_alloc',
-      # Needed for the metric reported by page.
-      'blink.user_timing'
-  ]
-
-  options.ExtendTraceCategoryFilter(categories)
-  if enable_runtime_call_stats:
-    options.AddTraceCategoryFilter('disabled-by-default-v8.runtime_stats')
-
-  options.config.enable_android_graphics_memtrack = True
-  # Trigger periodic light memory dumps every 1000 ms.
-  memory_dump_config = chrome_trace_config.MemoryDumpConfig()
-  memory_dump_config.AddTrigger('light', 1000)
-  options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
-
-  options.config.chrome_trace_config.SetTraceBufferSizeInKb(400 * 1024)
-
-  metrics = [
-      'blinkGcMetric',
-      'consoleErrorMetric',
-      'expectedQueueingTimeMetric',
-      'gcMetric',
-      'memoryMetric',
-      'pcscanMetric',
-      'reportedByPageMetric',
-      'wasmMetric',
-  ]
-  options.ExtendTimelineBasedMetric(metrics)
-  if enable_runtime_call_stats:
-    options.AddTimelineBasedMetric('runtimeStatsTotalMetric')
-  return options
-
-class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
+class _V8BrowsingBenchmark(v8_helper.V8PerfMixin, perf_benchmark.PerfBenchmark):
   """Base class for V8 browsing benchmarks that measure RuntimeStats,
   eqt, gc and memory metrics.
   See browsing_stories._BrowsingStory for workload description.
@@ -70,18 +19,14 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
   def CreateStorySet(self, options):
     return page_sets.SystemHealthStorySet(platform=self.PLATFORM, case='browse')
 
-  def CreateCoreTimelineBasedMeasurementOptions(self):
-    options = timeline_based_measurement.Options()
-    AugmentOptionsForV8BrowsingMetrics(options)
-    return options
-
 
 @benchmark.Info(emails=[
     'mythria@chromium.org', 'tmrts@chromium.org', 'almuthanna@chromium.org'
 ],
-                component='Blink>JavaScript')
-class V8DesktopBrowsingBenchmark(
-    _V8BrowsingBenchmark):
+                component='Blink>JavaScript',
+                documentation_url='https://bit.ly/system-health-v8-benchmarks')
+class V8DesktopBrowsingBenchmark(_V8BrowsingBenchmark):
+  """See _V8BrowsingBenchmark."""
   PLATFORM = 'desktop'
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
@@ -89,31 +34,23 @@ class V8DesktopBrowsingBenchmark(
   SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
   SUPPORTED_PLATFORM_TAGS = [platforms.DESKTOP]
 
-  def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(
-      '--enable-blink-features=BlinkRuntimeCallStats')
-    options.AppendExtraBrowserArgs(['--disable-popup-blocking'])
-
   @classmethod
   def Name(cls):
     return 'v8.browsing_desktop'
 
 
-@benchmark.Info(
-    emails=['mythria@chromium.org', 'tmrts@chromium.org'],
-    component='Blink>JavaScript')
+@benchmark.Info(emails=['mythria@chromium.org', 'tmrts@chromium.org'],
+                component='Blink>JavaScript',
+                documentation_url='https://bit.ly/system-health-v8-benchmarks')
 class V8MobileBrowsingBenchmark(
     _V8BrowsingBenchmark):
+  """See _V8BrowsingBenchmark."""
   PLATFORM = 'mobile'
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
   # from expectations.config in SUPPORTED_PLATFORM_TAGS.
   SUPPORTED_PLATFORMS = [story.expectations.ALL_MOBILE]
   SUPPORTED_PLATFORM_TAGS = [platforms.MOBILE]
-
-  def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(
-      '--enable-blink-features=BlinkRuntimeCallStats')
 
   @classmethod
   def Name(cls):
@@ -123,9 +60,10 @@ class V8MobileBrowsingBenchmark(
 @benchmark.Info(emails=[
     'mythria@chromium.org', 'tmrts@chromium.org', 'almuthanna@chromium.org'
 ],
-                component='Blink>JavaScript')
-class V8FutureDesktopBrowsingBenchmark(
-    _V8BrowsingBenchmark):
+                component='Blink>JavaScript',
+                documentation_url='https://bit.ly/system-health-v8-benchmarks')
+class V8FutureDesktopBrowsingBenchmark(_V8BrowsingBenchmark):
+  """See _V8BrowsingBenchmark."""
   PLATFORM = 'desktop'
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
@@ -134,8 +72,8 @@ class V8FutureDesktopBrowsingBenchmark(
   SUPPORTED_PLATFORM_TAGS = [platforms.DESKTOP]
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(
-      '--enable-blink-features=BlinkRuntimeCallStats')
+    super(V8FutureDesktopBrowsingBenchmark,
+          self).SetExtraBrowserOptions(options)
     options.AppendExtraBrowserArgs('--enable-features=V8VmFuture')
 
   @classmethod
@@ -143,11 +81,11 @@ class V8FutureDesktopBrowsingBenchmark(
     return 'v8.browsing_desktop-future'
 
 
-@benchmark.Info(
-    emails=['mythria@chromium.org', 'tmrts@chromium.org'],
-    component='Blink>JavaScript')
-class V8FutureMobileBrowsingBenchmark(
-    _V8BrowsingBenchmark):
+@benchmark.Info(emails=['mythria@chromium.org', 'tmrts@chromium.org'],
+                component='Blink>JavaScript',
+                documentation_url='https://bit.ly/system-health-v8-benchmarks')
+class V8FutureMobileBrowsingBenchmark(_V8BrowsingBenchmark):
+  """See _V8BrowsingBenchmark."""
   PLATFORM = 'mobile'
   # TODO(rmhasan): Remove the SUPPORTED_PLATFORMS lists.
   # SUPPORTED_PLATFORMS is deprecated, please put system specifier tags
@@ -156,8 +94,7 @@ class V8FutureMobileBrowsingBenchmark(
   SUPPORTED_PLATFORM_TAGS = [platforms.MOBILE]
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(
-      '--enable-blink-features=BlinkRuntimeCallStats')
+    super(V8FutureMobileBrowsingBenchmark, self).SetExtraBrowserOptions(options)
     options.AppendExtraBrowserArgs(
       '--enable-features=V8VmFuture')
 
--- tools/perf/benchmarks/v8_helper.py
+++ tools/perf/benchmarks/v8_helper.py
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry.web_perf import timeline_based_measurement
+from telemetry.timeline import chrome_trace_config
+
 _JS_FLAGS_SWITCH = '--js-flags='
 
 def AppendJSFlags(options, js_flags):
@@ -20,3 +23,69 @@ def AppendJSFlags(options, js_flags):
       # Add a new --js-flags which includes previous flags.
       '%s%s %s' %  (_JS_FLAGS_SWITCH, js_flags, existing_js_flags)
   ])
+
+
+def AugmentOptionsForV8Metrics(options, enable_runtime_call_stats=True):
+  categories = [
+      # Disable all categories by default.
+      '-*',
+      # Memory categories.
+      'disabled-by-default-memory-infra',
+      'toplevel',
+      # V8 categories.
+      'cppgc',
+      'disabled-by-default-cppgc',
+      'disabled-by-default-v8.gc',
+      'v8',
+      'v8.wasm',
+      'v8.console',
+      'webkit.console',
+      # Blink categories.
+      'blink_gc',
+      'partition_alloc',
+      # Needed for the metric reported by page.
+      'blink.user_timing'
+  ]
+
+  options.ExtendTraceCategoryFilter(categories)
+  if enable_runtime_call_stats:
+    options.AddTraceCategoryFilter('disabled-by-default-v8.runtime_stats')
+
+  options.config.enable_android_graphics_memtrack = True
+  # Trigger periodic light memory dumps every 1000 ms.
+  memory_dump_config = chrome_trace_config.MemoryDumpConfig()
+  memory_dump_config.AddTrigger('light', 1000)
+  options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
+
+  options.config.chrome_trace_config.SetTraceBufferSizeInKb(400 * 1024)
+
+  metrics = [
+      'blinkGcMetric',
+      'consoleErrorMetric',
+      'expectedQueueingTimeMetric',
+      'gcMetric',
+      'memoryMetric',
+      'pcscanMetric',
+      'reportedByPageMetric',
+      'wasmMetric',
+  ]
+  options.ExtendTimelineBasedMetric(metrics)
+  if enable_runtime_call_stats:
+    options.AddTimelineBasedMetric('runtimeStatsTotalMetric')
+  return options
+
+
+class V8PerfMixin(object):
+  """Base class for V8 benchmarks that measure RuntimeStats,
+  eqt, gc and memory metrics.
+  """
+
+  def CreateCoreTimelineBasedMeasurementOptions(self):
+    options = timeline_based_measurement.Options()
+    AugmentOptionsForV8Metrics(options)
+    return options
+
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs(
+        '--enable-blink-features=BlinkRuntimeCallStats')
+    options.AppendExtraBrowserArgs(['--disable-popup-blocking'])
