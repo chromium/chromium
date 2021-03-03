4c5b746de1466d5ada93babb84bc8181feaac5f4
David Bertoni <dbertoni@chromium.org>
2021-03-03 22:28:56 +0000

    [Extensions] Enable the Notifications API for SW-based extensions.
    This CL primarily updates extensions/renderer/resources/image_util.js
    so it's usable by Service Worker-based extensions, similar to changes
    made in crrev.com/5aa9f43a9bc423406349ea9aad85b6bdf484d608. It also
    enables 4 tests to run both as a legacy extension and a SW-based one.
    
    Unification of duplicate code in extensions/renderer/resources is
    tracked by https://crbug.com/1182365.
    
    Fixing and enabling more of the tests in NotificationsApiTest is
    tracked by https://crbug.com/1182305.
    
    Bug: 1168477
    Change-Id: I3db67783089a7eded632784f8f130f18085bc7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719286
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859562}

--- chrome/browser/extensions/api/notifications/notifications_apitest.cc
+++ chrome/browser/extensions/api/notifications/notifications_apitest.cc
@@ -46,6 +46,7 @@
 #include "base/mac/mac_util.h"
 #endif
 
+using ContextType = extensions::ExtensionBrowserTest::ContextType;
 using extensions::AppWindow;
 using extensions::AppWindowRegistry;
 using extensions::Extension;
@@ -218,21 +219,41 @@ class NotificationsApiTest : public extensions::ExtensionApiTest {
   std::unique_ptr<NotificationDisplayServiceTester> display_service_tester_;
 };
 
+// TODO(https://crbug.com/1182305): We should merge this class with the base
+// class once the issues mentioned in the bug are resolved.
+class NotificationsApiTestWithBackgroundType
+    : public NotificationsApiTest,
+      public testing::WithParamInterface<ContextType> {
+ protected:
+  bool RunTest(const char* name) {
+    return RunExtensionTest(
+        {.name = name},
+        {.load_as_service_worker = GetParam() == ContextType::kServiceWorker});
+  }
+};
+
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestBasicUsage) {
-  ASSERT_TRUE(RunExtensionTest("notifications/api/basic_usage")) << message_;
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         NotificationsApiTestWithBackgroundType,
+                         testing::Values(ContextType::kPersistentBackground));
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         NotificationsApiTestWithBackgroundType,
+                         testing::Values(ContextType::kServiceWorker));
+
+IN_PROC_BROWSER_TEST_P(NotificationsApiTestWithBackgroundType, TestBasicUsage) {
+  ASSERT_TRUE(RunTest("notifications/api/basic_usage")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestEvents) {
-  ASSERT_TRUE(RunExtensionTest("notifications/api/events")) << message_;
+IN_PROC_BROWSER_TEST_P(NotificationsApiTestWithBackgroundType, TestEvents) {
+  ASSERT_TRUE(RunTest("notifications/api/events")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestCSP) {
-  ASSERT_TRUE(RunExtensionTest("notifications/api/csp")) << message_;
+IN_PROC_BROWSER_TEST_P(NotificationsApiTestWithBackgroundType, TestCSP) {
+  ASSERT_TRUE(RunTest("notifications/api/csp")) << message_;
 }
 
-// Native notifications don't support (nor use) observers.
+// Native notifications don't support (or use) observers.
 #if !defined(OS_MAC)
 IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestByUser) {
   const extensions::Extension* extension =
@@ -274,8 +295,9 @@ IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestByUser) {
 }
 #endif  // !defined(OS_MAC)
 
-IN_PROC_BROWSER_TEST_F(NotificationsApiTest, TestPartialUpdate) {
-  ASSERT_TRUE(RunExtensionTest("notifications/api/partial_update")) << message_;
+IN_PROC_BROWSER_TEST_P(NotificationsApiTestWithBackgroundType,
+                       TestPartialUpdate) {
+  ASSERT_TRUE(RunTest("notifications/api/partial_update")) << message_;
   const extensions::Extension* extension = GetSingleLoadedExtension();
   ASSERT_TRUE(extension) << message_;
 
