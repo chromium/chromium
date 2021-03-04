39766f676ede443caa633f75f2c7d28b85e2b8c0
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-03-04 22:43:56 +0000

    [iOS][Biometric Auth] Disable forced passcode for autofill
    (cherry picked from commit 2d8d10906290dd843983e300d5d1ce88ead6334b)
    
    Bug: 1184617
    Change-Id: I34df01309056997c419ef96db1b7890314dad4da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2736935
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#859856}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737398
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4280_77@{#18}
    Cr-Branched-From: 16e8b32173a5eff313fcf2d3f42477080fb6fdbd-refs/branch-heads/4280@{#1524}
    Cr-Branched-From: ea420fb963f9658c9969b6513c56b8f47efa1a2a-refs/heads/master@{#812852}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_injection_handler.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_injection_handler.mm
@@ -165,7 +165,7 @@ const int64_t kJavaScriptExecutionTimeoutInSeconds = 1;
       } else {
         UmaHistogramEnumeration("IOS.Reauth.Password.ManualFallback",
                                 ReauthenticationEvent::kMissingPasscode);
-        [self.securityAlertHandler showSetPasscodeDialog];
+        [self fillLastSelectedFieldWithString:content];
       }
     }
   }
