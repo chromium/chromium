b40d1ed4f3c246be55721a1dc00aae6a1ebfa341
Zhaoyang Li <zhaoyangli@chromium.org>
2021-03-04 22:25:06 +0000

    [iOS][infra] Add an exceptional "TestCase" for sharding
    Made test runner not recognizing appConfigurationForTestCase as a test.
    
    Bug: 1183379
    Change-Id: Ib1d324ea6c482f4043449890c8f590ec9cbae693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727211
    Reviewed-by: Lindsay Pasricha <lindsayw@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859977}

--- ios/build/bots/scripts/shard_util.py
+++ ios/build/bots/scripts/shard_util.py
@@ -27,8 +27,11 @@ TEST_CLASS_RELEASE_APP_PATTERN = re.compile(
 TEST_NAME_RELEASE_APP_PATTERN = re.compile(
     r'name +0[xX]\w+ (?P<testCase>(?:DISABLED_|FLAKY_)?test[A-Za-z0-9_]+)\n')
 # 'ChromeTestCase' and 'BaseEarlGreyTestCase' are parent classes
-# of all EarlGrey/EarlGrey2 test classes. They have no real tests.
-IGNORED_CLASSES = ['BaseEarlGreyTestCase', 'ChromeTestCase']
+# of all EarlGrey/EarlGrey2 test classes. 'appConfigurationForTestCase' is a
+# class method. They have no real tests.
+IGNORED_CLASSES = [
+    'BaseEarlGreyTestCase', 'ChromeTestCase', 'appConfigurationForTestCase'
+]
 
 
 def determine_app_path(app, host_app=None, release=False):
