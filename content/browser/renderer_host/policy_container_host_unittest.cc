b54a8d9f41652c8adedd4b47cdcd311c7b216e7e
Antonio Sartori <antoniosartori@chromium.org>
2021-03-04 13:02:50 +0000

    Unit test PolicyContainerPolicies::Clone()
    This change adds a unit test for the PolicyContainerPolicies.
    
    Bug: 1149272
    Change-Id: I15d9a1395273e6ad4f26cbd285f0cc40eefa90a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727505
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859736}

--- content/browser/renderer_host/policy_container_host_unittest.cc
+++ content/browser/renderer_host/policy_container_host_unittest.cc
@@ -6,10 +6,55 @@
 
 #include "base/run_loop.h"
 #include "content/public/test/browser_task_environment.h"
+#include "services/network/public/mojom/content_security_policy.mojom.h"
+#include "services/network/public/mojom/ip_address_space.mojom-shared.h"
 #include "services/network/public/mojom/referrer_policy.mojom-shared.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
+
+using ::testing::ByRef;
+using ::testing::Eq;
+using ::testing::Pointee;
+
+namespace {
+
+struct SameSizeAsPolicyContainerPolicies {
+  network::mojom::ReferrerPolicy referrer_policy;
+  network::mojom::IPAddressSpace ip_address_space;
+  bool is_web_secure_context;
+  std::vector<network::mojom::ContentSecurityPolicyPtr>
+      content_security_policies;
+};
+
+}  // namespace
+
+// Asserts size of PolicyContainerPolicies, so that whenever a new element is
+// added to PolicyContainerPolicies, the assert will fail. When hitting this
+// assert failure, please ensure that the attribute is
+// - added to the PolicyContainerPolicies constructor
+// - copied correctly in PolicyContainerPolicies::Clone()
+// - checked correctly in PolicyContainerPolicies::operator==
+// - handled correctly in PolicyContainerPolicies::operator<<
+// - tested correctly in PolicyContainerHostTest.PolicyContainerPolicies below.
+static_assert(sizeof(PolicyContainerPolicies) ==
+                  sizeof(SameSizeAsPolicyContainerPolicies),
+              "PolicyContainerPolicies have been modified");
+
+TEST(PolicyContainerPoliciesTest, CloneIsEqual) {
+  std::vector<network::mojom::ContentSecurityPolicyPtr> csps;
+  auto csp = network::mojom::ContentSecurityPolicy::New();
+  csp->treat_as_public_address = true;
+  csps.push_back(std::move(csp));
+
+  auto policies = std::make_unique<PolicyContainerPolicies>(
+      network::mojom::ReferrerPolicy::kAlways,
+      network::mojom::IPAddressSpace::kUnknown,
+      /*is_web_secure_context=*/true, std::move(csps));
+
+  EXPECT_THAT(policies->Clone(), Pointee(Eq(ByRef(*policies))));
+}
 
 TEST(PolicyContainerHostTest, ReferrerPolicy) {
   scoped_refptr<PolicyContainerHost> policy_container =
