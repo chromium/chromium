2a610dd4e2617b1a202b7a55733afd8a0856e218
Maya Lekova <mslekova@chromium.org>
2021-03-04 14:51:38 +0000

    [v8] Upgrade deprecated ScriptOrigin usage
    The deprecation was done in
    https://chromium-review.googlesource.com/c/v8/v8/+/2726513.
    
    Change-Id: I2911adf884dde3081dd70f1e96da9d66de6ec868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732100
    Auto-Submit: Maya Lekova <mslekova@chromium.org>
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859766}

--- testing/libfuzzer/fuzzers/javascript_parser_proto_fuzzer.cc
+++ testing/libfuzzer/fuzzers/javascript_parser_proto_fuzzer.cc
@@ -78,8 +78,8 @@ DEFINE_BINARY_PROTO_FUZZER(
                                   v8::NewStringType::kNormal)
               .ToLocalChecked();
 
-      v8::ScriptOrigin origin(name, 0, 0, false, -1, v8::Local<v8::Value>(),
-                              false, false, true);
+      v8::ScriptOrigin origin(isolate, name, 0, 0, false, -1,
+                              v8::Local<v8::Value>(), false, false, true);
       v8::ScriptCompiler::Source source(source_v8_string, origin);
       v8::MaybeLocal<v8::Module> module =
           v8::ScriptCompiler::CompileModule(isolate, &source);
