9cb0294ded39376247ed59ed817cc8a7444b791b
Michael Bai <michaelbai@chromium.org>
2021-03-04 20:28:55 +0000

    Enable Browser metrics for WebView by default
    This patch enables Browser metrics (AKA live metrics) for WebView
    
    According WebView's metadata experiment, it is safe to launch
    this feature as it is.
    
    This patch will be merged to M90
    
    Test: Manually check the memory mapped file generated and uploaded.
    
    Bug: 1090682, 1093420
    Change-Id: Ib588808cb51347f51266dd2a4e853f83d9192346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2728516
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859909}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -11,11 +11,9 @@
 #include "components/metrics/persistent_histograms.h"
 
 void AwFieldTrials::SetupFieldTrials() {
-  // https://crbug.com/1093420: enable for persistent metrics.
   // Persistent histograms must be enabled as soon as possible.
   base::FilePath metrics_dir;
   if (base::PathService::Get(base::DIR_ANDROID_APP_DATA, &metrics_dir)) {
-    InstantiatePersistentHistograms(metrics_dir,
-                                    /*default_local_memory=*/true);
+    InstantiatePersistentHistograms(metrics_dir);
   }
 }
