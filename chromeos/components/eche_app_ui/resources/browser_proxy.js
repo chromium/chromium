50a00ff5e3f4ad0fabb23018a494cdeed08276d6
Guanru Lee <guanrulee@chromium.org>
2021-03-05 06:09:51 +0000

    Reland "browser_proxy.js hooks up to mojo interface"
    This is a reland of 5282dab3e788dfe7c139cf3e148e3c4e70a4d42e
    
    Original change's description:
    > browser_proxy.js hooks up to mojo interface
    >
    > Bug: b/181602857
    > Change-Id: I4d541b205805e48add686aa7216a5cd53e07b7f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725407
    > Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    > Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#859628}
    
    Bug: b/181602857
    Change-Id: Ib0e00e10ca93bfb696abf8abf05299017d132d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732211
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860141}

--- chromeos/components/eche_app_ui/resources/browser_proxy.js
+++ chromeos/components/eche_app_ui/resources/browser_proxy.js
@@ -2,14 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Returns a remote for SignalingMessageExchanger interface which sends messages
+// to the browser.
+const signalMessageExchanger =
+    chromeos.echeApp.mojom.SignalingMessageExchanger.getRemote();
+// An object which receives request messages for the SignalingMessageObserver
+// mojom interface and dispatches them as callbacks.
+const signalingMessageObserverRouter =
+    new chromeos.echeApp.mojom.SignalingMessageObserverCallbackRouter();
+// Set up a message pipe to talk to the browser process.
+signalMessageExchanger.setSignalingMessageObserver(
+    signalingMessageObserverRouter.$.bindNewPipeAndPassRemote());
+
 // The implementation of echeapi.d.ts
 const EcheApiBindingImpl = new class {
   onWebRtcSignalReceived(callback) {
-    // TODO(b/178379656): hook the callback to mojo interface.
+    signalingMessageObserverRouter.onReceivedSignalingMessage.addListener(
+        callback);
   }
 
   sendWebRtcSignal(signaling) {
-    // TODO(b/178379656): hook the sending function to mojo interface.
+    signalMessageExchanger.sendSignalingMessage(signaling);
   }
 };
 
@@ -17,7 +30,7 @@ const EcheApiBindingImpl = new class {
 const echeapi = {};
 echeapi.webrtc = {};
 echeapi.webrtc.sendSignal =
-EcheApiBindingImpl.sendWebRtcSignal.bind(EcheApiBindingImpl);
+    EcheApiBindingImpl.sendWebRtcSignal.bind(EcheApiBindingImpl);
 echeapi.webrtc.registerSignalReceiver =
-EcheApiBindingImpl.onWebRtcSignalReceived.bind(EcheApiBindingImpl);
+    EcheApiBindingImpl.onWebRtcSignalReceived.bind(EcheApiBindingImpl);
 window['echeapi'] = echeapi;
