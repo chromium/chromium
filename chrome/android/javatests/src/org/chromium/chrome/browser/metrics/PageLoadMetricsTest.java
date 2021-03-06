3e2f06d1812f16aec58e0853ef346d35c87ac7b4
Egor Pasko <pasko@chromium.org>
2021-03-06 23:27:41 +0000

    android: Re-enable 2 tests in PageLoadMetricsTest
    The bug lists evidence of this test timing out on Nexus 5 (running
    Android K). Bump the timeout and re-enable to see whether other devices
    would flake.
    
    Remove the getIsFirstNavigationInWebContents() checks because the
    setUp() loads about:blank, and the test does not have good ways to avoid
    reusing the same WebContents. Luckily about:blank does not generate
    Contentful paints.
    
    Also make sure all loaded URLs are different to mitigate another
    potential source of flakiness.
    
    Bug: 983804
    Change-Id: I96171e29ac6c86a3ffcb3f698081dd9228862fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2736519
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860565}

--- chrome/android/javatests/src/org/chromium/chrome/browser/metrics/PageLoadMetricsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/metrics/PageLoadMetricsTest.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
@@ -36,12 +35,19 @@ public class PageLoadMetricsTest {
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
-    private static final int PAGE_LOAD_METRICS_TIMEOUT_MS = 3000;
+    private static final int PAGE_LOAD_METRICS_TIMEOUT_MS = 6000;
+
+    // TODO(pasko): Replace the similar string constants with a single prefix and an array of
+    // server-provided URLs.
     private static final String TEST_PAGE = "/chrome/test/data/android/google.html";
     private static final String TEST_PAGE_2 = "/chrome/test/data/android/test.html";
+    private static final String TEST_PAGE_3 = "/chrome/test/data/android/google.html?q=3";
+    private static final String TEST_PAGE_4 = "/chrome/test/data/android/test.html?q=4";
 
     private String mTestPage;
     private String mTestPage2;
+    private String mTestPage3;
+    private String mTestPage4;
     private EmbeddedTestServer mTestServer;
 
     @Before
@@ -50,6 +56,8 @@ public class PageLoadMetricsTest {
         mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
         mTestPage = mTestServer.getURL(TEST_PAGE);
         mTestPage2 = mTestServer.getURL(TEST_PAGE_2);
+        mTestPage3 = mTestServer.getURL(TEST_PAGE_3);
+        mTestPage4 = mTestServer.getURL(TEST_PAGE_4);
     }
 
     @After
@@ -76,13 +84,11 @@ public class PageLoadMetricsTest {
         private final CountDownLatch mFirstContentfulPaintLatch = new CountDownLatch(1);
         private final CountDownLatch mLoadEventStartLatch = new CountDownLatch(1);
         private long mNavigationId = NO_NAVIGATION_ID;
-        private Boolean mIsFirstNavigationInWebContents;
 
         @Override
         public void onNewNavigation(WebContents webContents, long navigationId,
                 boolean isFirstNavigationInWebContents) {
             if (mNavigationId == NO_NAVIGATION_ID) mNavigationId = navigationId;
-            mIsFirstNavigationInWebContents = isFirstNavigationInWebContents;
         }
 
         @Override
@@ -123,15 +129,10 @@ public class PageLoadMetricsTest {
         public long getNavigationId() {
             return mNavigationId;
         }
-
-        public Boolean getIsFirstNavigationInWebContents() {
-            return mIsFirstNavigationInWebContents;
-        }
     }
 
     @Test
     @SmallTest
-    @FlakyTest(message = "crbug.com/983804")
     public void testPageLoadMetricEmitted() throws InterruptedException {
         Assert.assertFalse("Tab shouldn't be loading anything before we add observer",
                 mActivityTestRule.getActivity().getActivityTab().isLoading());
@@ -139,33 +140,27 @@ public class PageLoadMetricsTest {
         TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> PageLoadMetrics.addObserver(metricsObserver));
 
-        Assert.assertNull(metricsObserver.getIsFirstNavigationInWebContents());
-
         mActivityTestRule.loadUrl(mTestPage);
-        Assert.assertTrue(metricsObserver.getIsFirstNavigationInWebContents().booleanValue());
         assertMetricsEmitted(metricsObserver);
 
-        mActivityTestRule.loadUrl(mTestPage);
-        Assert.assertFalse(metricsObserver.getIsFirstNavigationInWebContents().booleanValue());
-
+        mActivityTestRule.loadUrl(mTestPage2);
         TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> PageLoadMetrics.removeObserver(metricsObserver));
     }
 
     @Test
     @SmallTest
-    @FlakyTest(message = "crbug.com/986025")
     public void testPageLoadMetricNavigationIdSetCorrectly() throws InterruptedException {
         PageLoadMetricsTestObserver metricsObserver = new PageLoadMetricsTestObserver();
         TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> PageLoadMetrics.addObserver(metricsObserver));
-        mActivityTestRule.loadUrl(mTestPage);
+        mActivityTestRule.loadUrl(mTestPage3);
         assertMetricsEmitted(metricsObserver);
 
         PageLoadMetricsTestObserver metricsObserver2 = new PageLoadMetricsTestObserver();
         TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> PageLoadMetrics.addObserver(metricsObserver2));
-        mActivityTestRule.loadUrl(mTestPage2);
+        mActivityTestRule.loadUrl(mTestPage4);
         assertMetricsEmitted(metricsObserver2);
 
         Assert.assertNotEquals("Subsequent navigations should have different navigation ids",
