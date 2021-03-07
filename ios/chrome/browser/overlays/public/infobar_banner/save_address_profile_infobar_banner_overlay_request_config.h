7034c31c1b21fc996a1d389038d131679d3c0a1f
Vidhan Jain <vidhanj@google.com>
2021-03-07 13:24:59 +0000

    [iOS][Autofill][SavePrompts] Add SaveAddressProfileBannerRequestConfig
    Bug: 1167062
    Change-Id: I87471d0f17679839f39354203b9082217bfc28ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732089
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860586}

---
+++ ios/chrome/browser/overlays/public/infobar_banner/save_address_profile_infobar_banner_overlay_request_config.h
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OVERLAYS_PUBLIC_INFOBAR_BANNER_SAVE_ADDRESS_PROFILE_INFOBAR_BANNER_OVERLAY_REQUEST_CONFIG_H_
+#define IOS_CHROME_BROWSER_OVERLAYS_PUBLIC_INFOBAR_BANNER_SAVE_ADDRESS_PROFILE_INFOBAR_BANNER_OVERLAY_REQUEST_CONFIG_H_
+
+#include "base/strings/string16.h"
+#include "ios/chrome/browser/overlays/public/overlay_request_config.h"
+#include "ios/chrome/browser/overlays/public/overlay_user_data.h"
+
+namespace infobars {
+class InfoBar;
+}
+
+namespace save_address_profile_infobar_overlays {
+
+// Configuration object for OverlayRequests for the banner UI for an InfoBar
+// with a AutofillSaveAddressProfileDelegateIOS.
+class SaveAddressProfileBannerRequestConfig
+    : public OverlayRequestConfig<SaveAddressProfileBannerRequestConfig> {
+ public:
+  ~SaveAddressProfileBannerRequestConfig() override;
+
+  // The message text.
+  base::string16 message_text() const { return message_text_; }
+
+  // The button label text.
+  base::string16 button_label_text() const { return button_label_text_; }
+
+  // The message sub text.
+  base::string16 message_sub_text() const { return message_sub_text_; }
+
+  // The name of the icon image.
+  NSString* icon_image_name() const { return icon_image_name_; }
+
+ private:
+  OVERLAY_USER_DATA_SETUP(SaveAddressProfileBannerRequestConfig);
+  explicit SaveAddressProfileBannerRequestConfig(infobars::InfoBar* infobar);
+
+  // OverlayUserData:
+  void CreateAuxiliaryData(base::SupportsUserData* user_data) override;
+
+  // The InfoBar causing this banner.
+  infobars::InfoBar* infobar_ = nullptr;
+  // Configuration data extracted from |infobar_|'s save address profile
+  // delegate.
+  base::string16 message_text_;
+  base::string16 message_sub_text_;
+  base::string16 button_label_text_;
+  NSString* icon_image_name_ = nil;
+};
+
+}  // namespace save_address_profile_infobar_overlays
+
+#endif  // IOS_CHROME_BROWSER_OVERLAYS_PUBLIC_INFOBAR_BANNER_SAVE_ADDRESS_PROFILE_INFOBAR_BANNER_OVERLAY_REQUEST_CONFIG_H_
