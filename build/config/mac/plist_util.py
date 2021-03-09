d2a4cee74018893f22f51def3d217aaf60b9d14d
Joshua Pawlicki <waffles@chromium.org>
2021-03-09 14:19:16 +0000

    Revert "[apple] Split //build/config/mac/base_rules.gni"
    This reverts commit 7b4a80dc4b49e613547feffe3c9db80a0ace75fe.
    
    Reason for revert: compile failure: https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-full-configs/30097 ; may be flaky due to missing dep.
    
    Original change's description:
    > [apple] Split //build/config/mac/base_rules.gni
    >
    > The templates defined in that file were used to build for both ios
    > and macOS OSes. Move each template to a separate file (named after
    > the template defined) and put the files in //build/apple (which is
    > the location of template shared between all Apple platforms).
    >
    > Bug: 635745
    > Change-Id: I7d816e50d8245dd5c097776a17832a0e9b50e034
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2740920
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#861123}
    
    Bug: 635745
    Change-Id: Ied0e3eef8849d81ae4528eb550aea896e1db1fa4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745534
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#861140}

--- build/config/mac/plist_util.py
+++ build/config/mac/plist_util.py
@@ -51,7 +51,6 @@ def InterpolateString(value, substitutions):
     value in |substitutions|. Raises SubstitutionError if a variable has no
     substitution.
   """
-
   def repl(match):
     variable = match.group('id')
     if variable not in substitutions:
@@ -67,7 +66,6 @@ def InterpolateString(value, substitutions):
       return INVALID_CHARACTER_REGEXP.sub('-', substitutions[variable])
     else:
       return substitutions[variable]
-
   for substitution_regexp in SUBSTITUTION_REGEXP_LIST:
     value = substitution_regexp.sub(repl, value)
   return value
@@ -178,20 +176,19 @@ class MergeAction(Action):
 
   @staticmethod
   def _Register(parser):
-    parser.add_argument('-o',
-                        '--output',
-                        required=True,
-                        help='path to the output plist file')
-    parser.add_argument('-f',
-                        '--format',
-                        required=True,
-                        choices=('xml1', 'binary1'),
-                        help='format of the plist file to generate')
+    parser.add_argument(
+        '-o', '--output', required=True,
+        help='path to the output plist file')
+    parser.add_argument(
+        '-f', '--format', required=True, choices=('xml1', 'binary1'),
+        help='format of the plist file to generate')
     parser.add_argument(
         '-x',
         '--xcode-version',
         help='version of Xcode, ignored (can be used to force rebuild)')
-    parser.add_argument('path', nargs="+", help='path to plist files to merge')
+    parser.add_argument(
+          'path', nargs="+",
+          help='path to plist files to merge')
 
   @staticmethod
   def _Execute(args):
@@ -209,24 +206,18 @@ class SubstituteAction(Action):
 
   @staticmethod
   def _Register(parser):
-    parser.add_argument('-o',
-                        '--output',
-                        required=True,
-                        help='path to the output plist file')
-    parser.add_argument('-t',
-                        '--template',
-                        required=True,
-                        help='path to the template file')
-    parser.add_argument('-s',
-                        '--substitution',
-                        action='append',
-                        default=[],
-                        help='substitution rule in the format key=value')
-    parser.add_argument('-f',
-                        '--format',
-                        required=True,
-                        choices=('xml1', 'binary1'),
-                        help='format of the plist file to generate')
+    parser.add_argument(
+        '-o', '--output', required=True,
+        help='path to the output plist file')
+    parser.add_argument(
+        '-t', '--template', required=True,
+        help='path to the template file')
+    parser.add_argument(
+        '-s', '--substitution', action='append', default=[],
+        help='substitution rule in the format key=value')
+    parser.add_argument(
+        '-f', '--format', required=True, choices=('xml1', 'binary1'),
+        help='format of the plist file to generate')
     parser.add_argument(
         '-x',
         '--xcode-version',
