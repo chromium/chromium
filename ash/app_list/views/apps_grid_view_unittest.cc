c1ff543435edd9fb7144267b02478eaea9103133
calamity <calamity@chromium.org>
2021-03-10 01:41:18 +0000

    Revert "chromeos: Enable app_list_unittests AppsGridViewTest.MouseDragFlipPage"
    This reverts commit 4297cfb29c22416ad193d4db751cf71a3b72e96f.
    
    Reason for revert: Still flaking. Example: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/22895/overview
    
    
    Original change's description:
    > chromeos: Enable app_list_unittests AppsGridViewTest.MouseDragFlipPage
    >
    > It was disabled for flake on the MSAN builders on Dec 8, 2020. I can
    > repro the flake locally when I sync back to revisions before it was
    > disabled, but I cannot repro flake on tip-of-trunk. Some of the code
    > paths exercised by this test have been refactored in the last 2 months,
    > so the issue may be fixed.
    >
    > For older code revisions, this will repro the problem on an MSAN build:
    > app_list_unittests --gtest_shuffle --gtest_repeat=100
    >
    > Note that the test (while disabled) was accidentally broken by this CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/2648268
    >
    > and fixed by this one:
    > https://chromium-review.googlesource.com/c/chromium/src/+/2739441
    >
    > Bug: 1156634
    > Change-Id: I085a07afc6fb982fae77daaf544cc0af475042cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2740072
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860466}
    
    Bug: 1156634
    Change-Id: I8a1ef1a3c410d2d059521fe04e5b608442a62c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2746777
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861383}

--- ash/app_list/views/apps_grid_view_unittest.cc
+++ ash/app_list/views/apps_grid_view_unittest.cc
@@ -1927,7 +1927,8 @@ TEST_F(AppsGridViewTest, ControlShiftArrowFolderLastItemOnPage) {
   EXPECT_TRUE(folder_item->FindChildItem(second_item_id));
 }
 
-TEST_P(AppsGridViewTest, MouseDragFlipPage) {
+// Flaky: crbug.com/1156634
+TEST_P(AppsGridViewTest, DISABLED_MouseDragFlipPage) {
   apps_grid_view_->set_page_flip_delay_for_testing(
       base::TimeDelta::FromMilliseconds(10));
   GetPaginationModel()->SetTransitionDurations(
