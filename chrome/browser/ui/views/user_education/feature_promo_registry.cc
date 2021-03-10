6bb4a8f881d650fe2094ecf1af77b731dad7e01f
Charlene Yan <cyan@chromium.org>
2021-03-10 09:49:28 +0000

    Reland "[Read Later] Implement IPH promo for first time adding to reading list."
    This reverts commit 61ab280ab967b203cd7cf5d99b09c20c61cd40c5.
    
    Reason for revert: Trying to reland
    
    Original change's description:
    > Revert "[Read Later] Implement IPH promo for first time adding to reading list."
    >
    > This reverts commit 168f5253c3b7d4fbf862bb343d16140e56fabe4d.
    >
    > Reason for revert: FeaturePromoDialogTest.InvokeUi_IPH_ReadingListDiscovery Consistently fails on linux-chromeos-chrome
    >
    > First failure: https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/12543
    >
    > Original change's description:
    > > [Read Later] Implement IPH promo for first time adding to reading list.
    > >
    > > Bug: 1175976
    > > Change-Id: Iab9b8ae89a0d2e1316c4577464ce1ee475f3aca1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723691
    > > Reviewed-by: Collin Baker <collinbaker@chromium.org>
    > > Reviewed-by: Dana Fried <dfried@chromium.org>
    > > Commit-Queue: Charlene Yan <cyan@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#859833}
    >
    > Bug: 1175976
    > Change-Id: I17825294c5a5f5534d03c567a7cbdcfcb1d59f8c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737155
    > Auto-Submit: Meredith Lane <meredithl@chromium.org>
    > Reviewed-by: Meredith Lane <meredithl@chromium.org>
    > Commit-Queue: Meredith Lane <meredithl@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#860016}
    
    Bug: 1175976
    Change-Id: Ia2c4ff37ae19393e15c05f8b74aebc781e0ecc5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739866
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861495}

--- chrome/browser/ui/views/user_education/feature_promo_registry.cc
+++ chrome/browser/ui/views/user_education/feature_promo_registry.cc
@@ -8,11 +8,13 @@
 #include "base/optional.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_controller.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
+#include "chrome/browser/ui/views/read_later/read_later_button.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/toolbar/browser_app_menu_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
@@ -48,6 +50,11 @@ views::View* GetTabGroupsAnchorView(BrowserView* browser_view) {
 // kIPHLiveCaptionFeature:
 views::View* GetMediaButton(BrowserView* browser_view) {
   return browser_view->toolbar()->media_button();
+}
+
+// kIPHReadingListDiscoveryFeature:
+views::View* GetReadingListButton(BrowserView* browser_view) {
+  return browser_view->bookmark_bar()->read_later_button();
 }
 
 // kIPHReopenTabFeature:
@@ -167,6 +174,16 @@ void FeaturePromoRegistry::RegisterKnownFeatures() {
 
     RegisterFeature(feature_engagement::kIPHLiveCaptionFeature, params,
                     base::BindRepeating(GetMediaButton));
+  }
+
+  {
+    // kReadingListDiscoveryFeature:
+    FeaturePromoBubbleParams params;
+    params.body_string_specifier = IDS_READING_LIST_DISCOVERY_PROMO;
+    params.arrow = views::BubbleBorder::Arrow::TOP_RIGHT;
+
+    RegisterFeature(feature_engagement::kIPHReadingListDiscoveryFeature, params,
+                    base::BindRepeating(GetReadingListButton));
   }
 
   {
