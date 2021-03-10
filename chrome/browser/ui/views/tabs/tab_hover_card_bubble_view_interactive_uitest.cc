fa944230242d587a3cfbd2f017b9f75f04f1475b
Dana Fried <dfried@chromium.org>
2021-03-10 22:19:57 +0000

    Make UI test more reliable
    Test was flaky, this should ensure that all signals sent to the window
    are processed completely before continuing.
    
    Bug: 1182759
    Change-Id: Ic2742cd879bbff8a45beb08c47e90abdccd7c450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749073
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861730}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_interactive_uitest.cc
@@ -36,6 +36,41 @@ class TabHoverCardBubbleViewInteractiveUiTest : public InProcessBrowserTest {
 };
 
 #if defined(USE_AURA)
+
+namespace {
+
+// Similar to views::test::WidgetDestroyedWaiter but waiting after the widget
+// has been closed is a no-op rather than an error.
+class SafeWidgetDestroyedWaiter : public views::WidgetObserver {
+ public:
+  explicit SafeWidgetDestroyedWaiter(views::Widget* widget) {
+    observation_.Observe(widget);
+  }
+
+  // views::WidgetObserver:
+  void OnWidgetDestroyed(Widget* widget) override {
+    observation_.Reset();
+    if (!quit_closure_.is_null())
+      std::move(quit_closure_).Run();
+  }
+
+  void Wait() {
+    if (!observation_.IsObserving())
+      return;
+    DCHECK(quit_closure_.is_null());
+    quit_closure_ = run_loop_.QuitClosure();
+    run_loop_.Run();
+  }
+
+ private:
+  base::RunLoop run_loop_;
+  base::OnceClosure quit_closure_;
+  base::ScopedObservation<views::Widget, views::WidgetObserver> observation_{
+      this};
+};
+
+}  // namespace
+
 // Verify that the hover card is not visible when any key is pressed.
 IN_PROC_BROWSER_TEST_F(TabHoverCardBubbleViewInteractiveUiTest,
                        HoverCardHidesOnAnyKeyPressInSameWindow) {
@@ -49,10 +84,17 @@ IN_PROC_BROWSER_TEST_F(TabHoverCardBubbleViewInteractiveUiTest,
   views::test::WidgetVisibleWaiter(widget).Wait();
   EXPECT_TRUE(widget->IsVisible());
 
+  // Verify that the hover card widget is destroyed sometime between now and
+  // when we check afterwards. Depending on platform, the destruction could be
+  // synchronous or asynchronous.
+  SafeWidgetDestroyedWaiter widget_destroyed_waiter(widget);
+
   EXPECT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_DOWN, false,
                                               false, false, false));
-  // Note, fade in/out animations are disabled for testing so there is no need
-  // to account for them here.
+
+  // Note, fade in/out animations are disabled for testing so this should be
+  // relatively quick.
+  widget_destroyed_waiter.Wait();
   EXPECT_EQ(nullptr, GetHoverCard(tab_strip));
 }
 #endif
