cc5265a80455322091c250ca435fe83bfe9fdae6
Jesse McKenna <jessemckenna@google.com>
2021-03-10 08:15:25 +0000

    Move RecentlyDestroyedHosts to a separate file
    This change moves RecentlyDestroyedHosts, the class that emits metric
    "SiteIsolation.ReusePendingOrCommittedSite.
    TimeSinceReusableProcessDestroyed", into its own file.
    
    This has the benefits of:
    1. preparing RecentlyDestroyedHosts to be used by crrev.com/c/2431587
    2. removing clutter from render_process_host_impl.cc
    
    Bug: 894253
    Change-Id: Id539c870ac64581265085ed54dfa993562a1a837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739942
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#861473}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -120,6 +120,7 @@
 #include "content/browser/renderer_host/media/media_stream_track_metrics_host.h"
 #include "content/browser/renderer_host/p2p/socket_dispatcher_host.h"
 #include "content/browser/renderer_host/plugin_registry_impl.h"
+#include "content/browser/renderer_host/recently_destroyed_hosts.h"
 #include "content/browser/renderer_host/render_message_filter.h"
 #include "content/browser/renderer_host/render_widget_helper.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
@@ -922,116 +923,6 @@ class SiteProcessCountTracker : public base::SupportsUserData::Data,
   using CountPerProcessPerSiteMap =
       std::map<SiteInfo, std::map<ProcessID, Count>>;
   CountPerProcessPerSiteMap map_;
-};
-
-// Maintains a list of recently destroyed processes to gather metrics on the
-// potential for process reuse (crbug.com/894253).
-const void* const kRecentlyDestroyedHostTrackerKey =
-    "RecentlyDestroyedHostTrackerKey";
-// Storage time for information about recently destroyed processes. Intended to
-// be long enough to capture a large portion of the process-reuse opportunity.
-static constexpr base::TimeDelta kRecentlyDestroyedTimeout =
-    base::TimeDelta::FromSeconds(10);
-// Sentinel value indicating that no recently destroyed process matches the
-// host currently seeking a process. Changing this invalidates the histogram.
-static constexpr base::TimeDelta kRecentlyDestroyedNotFoundSentinel =
-    base::TimeDelta::FromSeconds(20);
-class RecentlyDestroyedHosts : public base::SupportsUserData::Data {
- public:
-  RecentlyDestroyedHosts(const RecentlyDestroyedHosts& other) = delete;
-  RecentlyDestroyedHosts& operator=(const RecentlyDestroyedHosts& other) =
-      delete;
-
-  // If a host matching |process_lock| was recently destroyed, records the time
-  // between its destruction and |reusable_host_lookup_time|. If not, records a
-  // sentinel value.
-  static void RecordMetricIfReusableHostRecentlyDestroyed(
-      const base::TimeTicks& reusable_host_lookup_time,
-      const ProcessLock& process_lock,
-      BrowserContext* browser_context) {
-    auto* instance = GetInstance(browser_context);
-    instance->RemoveExpiredEntries();
-    const auto iter = instance->map_.find(process_lock);
-    if (iter != instance->map_.end()) {
-      instance->RecordMetric(reusable_host_lookup_time - iter->second);
-      return;
-    }
-    instance->RecordMetric(kRecentlyDestroyedNotFoundSentinel);
-  }
-
-  // Adds |host|'s process lock to the list of recently destroyed hosts, or
-  // updates its time if it's already present.
-  static void Add(RenderProcessHost* host,
-                  const base::TimeDelta& time_spent_in_delayed_shutdown,
-                  BrowserContext* browser_context) {
-    if (time_spent_in_delayed_shutdown > kRecentlyDestroyedTimeout)
-      return;
-
-    auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
-    ProcessLock process_lock = policy->GetProcessLock(host->GetID());
-
-    // Don't record sites with an empty process lock. This includes sites on
-    // Android that are not isolated, and some special cases on desktop (e.g.,
-    // chrome-extension://). These sites would not be affected by increased
-    // process reuse, so are irrelevant for the metric being recorded.
-    if (!process_lock.is_locked_to_site())
-      return;
-
-    // Record the time before |time_spent_in_delayed_shutdown| to exclude time
-    // spent running unload handlers from the metric. This makes it consistent
-    // across processes that were delayed by DelayProcessShutdownForUnload(),
-    // and those that weren't.
-    auto* instance = GetInstance(browser_context);
-    instance->map_[process_lock] =
-        base::TimeTicks::Now() - time_spent_in_delayed_shutdown;
-
-    // Clean up list of recently destroyed hosts if it's getting large. This is
-    // a fallback in case a subframe process hasn't been created in a long time
-    // (which would clean up |map_|), e.g., on low-memory Android where site
-    // isolation is not used.
-    if (instance->map_.size() > 20)
-      instance->RemoveExpiredEntries();
-  }
-
- private:
-  RecentlyDestroyedHosts() = default;
-
-  static RecentlyDestroyedHosts* GetInstance(BrowserContext* browser_context) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
-    RecentlyDestroyedHosts* recently_destroyed_hosts =
-        static_cast<RecentlyDestroyedHosts*>(
-            browser_context->GetUserData(kRecentlyDestroyedHostTrackerKey));
-    if (recently_destroyed_hosts)
-      return recently_destroyed_hosts;
-
-    recently_destroyed_hosts = new RecentlyDestroyedHosts;
-    browser_context->SetUserData(kRecentlyDestroyedHostTrackerKey,
-                                 base::WrapUnique(recently_destroyed_hosts));
-    return recently_destroyed_hosts;
-  }
-
-  void RecordMetric(base::TimeDelta value) {
-    UMA_HISTOGRAM_CUSTOM_TIMES(
-        "SiteIsolation.ReusePendingOrCommittedSite."
-        "TimeSinceReusableProcessDestroyed",
-        value, base::TimeDelta::FromMilliseconds(1),
-        kRecentlyDestroyedNotFoundSentinel, 50);
-  }
-
-  void RemoveExpiredEntries() {
-    const auto expired_cutoff_time =
-        base::TimeTicks::Now() - kRecentlyDestroyedTimeout;
-    for (auto iter = map_.begin(); iter != map_.end();) {
-      if (iter->second < expired_cutoff_time) {
-        iter = map_.erase(iter);
-      } else {
-        ++iter;
-      }
-    }
-  }
-
-  std::map<ProcessLock, base::TimeTicks> map_;
 };
 
 bool ShouldUseSiteProcessTracking(BrowserContext* browser_context,
