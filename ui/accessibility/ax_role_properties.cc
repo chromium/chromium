60f26f2b1273c73f74e50fcc6d08e7067af13e0e
Nektarios Paisios <nektar@chromium.org>
2021-03-10 16:16:28 +0000

    Cleanup: Alphabetizes roles in ax_role_properties::IsControlOnAndroid
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    AX-Relnotes: n/a.
    Change-Id: I342bf350026b347e486fb435dc29069150203f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747603
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861575}

--- ui/accessibility/ax_role_properties.cc
+++ ui/accessibility/ax_role_properties.cc
@@ -192,23 +192,23 @@ bool IsControlOnAndroid(const ax::mojom::Role role, bool isFocusable) {
   switch (role) {
     case ax::mojom::Role::kSplitter:
       return isFocusable;
-    case ax::mojom::Role::kTreeItem:
     case ax::mojom::Role::kDate:
     case ax::mojom::Role::kDateTime:
-    case ax::mojom::Role::kInputTime:
     case ax::mojom::Role::kDocBackLink:
     case ax::mojom::Role::kDocBiblioRef:
     case ax::mojom::Role::kDocGlossRef:
     case ax::mojom::Role::kDocNoteRef:
+    case ax::mojom::Role::kInputTime:
     case ax::mojom::Role::kLink:
+    case ax::mojom::Role::kTreeItem:
       return true;
+    case ax::mojom::Role::kAlert:
+    case ax::mojom::Role::kDialog:
     case ax::mojom::Role::kMenu:
     case ax::mojom::Role::kMenuBar:
     case ax::mojom::Role::kNone:
-    case ax::mojom::Role::kUnknown:
     case ax::mojom::Role::kTree:
-    case ax::mojom::Role::kDialog:
-    case ax::mojom::Role::kAlert:
+    case ax::mojom::Role::kUnknown:
       return false;
     default:
       return IsControl(role);
