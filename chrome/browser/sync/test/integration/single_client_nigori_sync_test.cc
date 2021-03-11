75b093769b3abac1a69c9a3080f4b0aadc8eca69
calamity <calamity@chromium.org>
2021-03-11 06:42:53 +0000

    Disable SingleClientNigoriSyncTestWithSecurityDomainsServer test
    Bug: 1186919
    Change-Id: I90986f6efb3c2a1c4610c081ea6dc9af25da97fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2750851
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Owners-Override: calamity <calamity@chromium.org>
    Auto-Submit: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861874}

--- chrome/browser/sync/test/integration/single_client_nigori_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_nigori_sync_test.cc
@@ -1116,8 +1116,9 @@ class SingleClientNigoriSyncTestWithSecurityDomainsServer : public SyncTest {
 // Device registration attempt should be taken upon sign in into primary
 // profile. It should be successful when security domain server allows device
 // registration with constant key.
+// Disabled due to flakiness. See crbug.com/1186919.
 IN_PROC_BROWSER_TEST_F(SingleClientNigoriSyncTestWithSecurityDomainsServer,
-                       ShouldRegisterDeviceWithConstantKey) {
+                       DISABLED_ShouldRegisterDeviceWithConstantKey) {
   ASSERT_TRUE(SetupSync());
   // TODO(crbug.com/1113599): consider checking member public key (requires
   // either ability to overload key generator in the test or exposing public key
