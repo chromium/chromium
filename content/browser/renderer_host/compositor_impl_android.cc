a9b158d00a1114d05fcfa8cc878838f8c37c3d5b
kylechar <kylechar@chromium.org>
2021-03-11 15:51:37 +0000

    Speculative fix for CacheBackBuffer() crash
    In CompositorImpl::CacheBackBufferForCurrentSurface() |display_private_|
    is used to guard against sending a request to cache back buffer if there
    is no RootCompositorFrameSinkImpl. There is still a crash where there is
    no RootCompositorFrameSinkImpl despite this check.
    
    There is also a sync IPC to destroy the RootCompositorFrameSinkImpl
    before |display_private_| is reset. If another sync IPC arrives while
    waiting for the DestroyCompositorFrameSink() IPC response and that other
    sync IPC triggers CacheBackBufferForCurrentSurface() then
    |display_private_| will not have been reset yet.
    
    The FrameSinkManager::CacheBackBuffer() IPC will arrive after the
    RootCompositorFrameSinkImpl has been destroyed, the map lookup in
    FrameSinkManagerImpl::CacheBackBuffer() will fail and an invalid
    iterator will be used.
    
    Bug: 1182123
    Change-Id: I3b587617ac8997704819bb6dd39bfad5f3654cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747656
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861983}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -487,8 +487,11 @@ void CompositorImpl::TearDownDisplayAndUnregisterRootFrameSink() {
     // execution of this call.
     display_private_->ForceImmediateDrawAndSwapIfPossible();
   }
-  GetHostFrameSinkManager()->InvalidateFrameSinkId(frame_sink_id_);
+  // Reset |display_private_| first since InvalidateFrameSinkId() will send a
+  // sync IPC. This guards against reentrant code using |display_private_|
+  // before it can be reset.
   display_private_.reset();
+  GetHostFrameSinkManager()->InvalidateFrameSinkId(frame_sink_id_);
 }
 
 void CompositorImpl::RegisterRootFrameSink() {
