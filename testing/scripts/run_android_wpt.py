a8f403b1abe3c4c6ee8ae483cb0fc22a9eb559cd
Xiaohan Wang <xhwang@chromium.org>
2021-03-11 21:42:40 +0000

    Revert "Switch run_android_wpt to py3"
    This reverts commit 7d961ecace9954f368e56782bffb1ac60fd5b3fc.
    
    Reason for revert: crbug.com/1187233
    
    Original change's description:
    > Switch run_android_wpt to py3
    >
    > Bug: 1186789
    > Change-Id: I3b669e4ecddda1eb24554fb28b4c14730ed80a72
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747271
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Reviewed-by: Luke Z <lpz@chromium.org>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Cr-Commit-Position: refs/heads/master@{#862070}
    
    Bug: 1186789
    Change-Id: I318622363824ae133d46598868887b1afd414afb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754211
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862140}

--- testing/scripts/run_android_wpt.py
+++ testing/scripts/run_android_wpt.py
@@ -120,10 +120,10 @@ class WPTAndroidAdapter(wpt_common.BaseWptScriptAdapter):
     rest_args.extend([self.options.wpt_path])
 
     # TODO(crbug.com/1166741): We should be running WPT under Python 3.
-    rest_args.extend(["--py3"])
+    rest_args.extend(["--py2"])
 
     # vpython has packages needed by wpt, so force it to skip the setup
-    rest_args.extend(["--venv=" + SRC_DIR, "--skip-venv-setup"])
+    rest_args.extend(["--venv=../../", "--skip-venv-setup"])
 
     rest_args.extend(["run",
       "--tests=" + wpt_common.EXTERNAL_WPT_TESTS_DIR,
@@ -177,7 +177,6 @@ class WPTAndroidAdapter(wpt_common.BaseWptScriptAdapter):
          self._metadata_dir,
          '--additional-expectations',
          ANDROID_DISABLED_TESTS,
-         "--no-process-baselines",
     ]
     metadata_builder_cmd.extend(self._extra_metadata_builder_args())
     return common.run_command(metadata_builder_cmd)
