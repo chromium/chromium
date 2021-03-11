dcc9227eb39da4996a4feebf8a44d104bdeb5f69
Aaron Leventhal <aleventhal@google.com>
2021-03-11 19:41:10 +0000

    Fix error in rebase script
    Use try/except to avoid script early exit from
    "AttributeError: 'NoneType' object has no attribute 'group'".
    
    Bug: None
    Change-Id: Ib6d1487013cb85330cdd23f530fd7d94ef9b2081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2750964
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862083}

--- tools/accessibility/rebase_dump_accessibility_tree_test.py
+++ tools/accessibility/rebase_dump_accessibility_tree_test.py
@@ -43,17 +43,15 @@ completed_files = set()
 
 def Fix(line):
   if line[:3] == '@@@':
-    try:
-      line = re.search('[^@]@([^@]*)@@@', line).group(1)
-    except:
-      pass
+    result = re.search('[^@]@([^@]*)@@@', line)
+    if result:
+      line = result.group(1)
   # For Android tests:
   if line[:2] == 'I ':
-    try:
-      line = re.search('I  \d+\.\d+s run_tests_on_device\([0-9a-f]+\)  (.*)',
-                       line).group(1)
-    except:
-      pass
+    result = re.search('I  \d+\.\d+s run_tests_on_device\([0-9a-f]+\)  (.*)',
+                       line)
+    if result:
+      line = group(1)
   return line
 
 def ParseLog(logdata):
@@ -66,13 +64,15 @@ def ParseLog(logdata):
   for i in range(len(lines)):
     line = Fix(lines[i])
     if line.find('Testing:') >= 0:
-      test_file = re.search(
-          'content.test.*accessibility.([^@]*)', line).group(1)
+      result = re.search('content.test.*accessibility.([^@]*)', line)
+      if result:
+        test_file = result.group(1)
       expected_file = None
       start = None
     if line.find('Expected output:') >= 0:
-      expected_file = re.search(
-          'content.test.*accessibility.([^@]*)', line).group(1)
+      result = re.search('content.test.*accessibility.([^@]*)', line)
+      if result:
+        expected_file = result.group(1)
     if line == 'Actual':
       start = i + 2
     if start and test_file and expected_file and line.find('End-of-file') >= 0:
