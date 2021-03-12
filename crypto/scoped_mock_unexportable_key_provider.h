979ee875cb9249d7e5fa20ee2875bfa0c6f9f21f
Adam Langley <agl@chromium.org>
2021-03-12 02:47:19 +0000

    crypto: add support for TPM-backed keys on Windows.
    BUG=1184938
    
    Change-Id: I6d37b9066b7fadf0d79f1b3c7288f1db953a6100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737527
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862266}

---
+++ crypto/scoped_mock_unexportable_key_provider.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRYPTO_SCOPED_MOCK_UNEXPORTABLE_KEY_PROVIDER_H_
+#define CRYPTO_SCOPED_MOCK_UNEXPORTABLE_KEY_PROVIDER_H_
+
+namespace crypto {
+
+// ScopedMockUnexportableKeyProvider causes
+// |GetUnexportableKeyProvider| to return a mock, software-based
+// implementation of |UnexportableKeyProvider| while it is in scope.
+//
+// This needs you to link against the test_support target.
+class ScopedMockUnexportableKeyProvider {
+ public:
+  ScopedMockUnexportableKeyProvider();
+  ~ScopedMockUnexportableKeyProvider();
+};
+
+}  // namespace crypto
+
+#endif  // CRYPTO_SCOPED_MOCK_UNEXPORTABLE_KEY_PROVIDER_H_
