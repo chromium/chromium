5c4af6aaa41480591feeef69390349a676cc7231
David Black <dmblack@google.com>
2021-03-13 02:41:12 +0000

    Reland "Support drag-and-drop of directories to pin."
    This is a reland of e80eb1039791c8e5c2db2c0133cfa84e78f2ed20
    
    Original change's description:
    > Support drag-and-drop of directories to pin.
    >
    > Previously we only inspect for files in `ui::OSExchangeData` at the
    > filenames storage location. As it turns out, the Files app populates both
    > the filenames storage location as well as at a file system sources
    > storage location in custom pickled data.
    >
    > In the case of directories being dragged from Files app, the latter
    > storage location includes the directory while the former does not.
    >
    > After this CL, Tote supports both storage locations but prefers the
    > Files app's file system sources storage location when available since it
    > contains the most data.
    >
    > Bug: 1186408
    > Change-Id: I79938f468dd47fc877fcd33c2838e1c59ffa965a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749616
    > Commit-Queue: David Black <dmblack@google.com>
    > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#862615}
    
    Bug: 1186408
    Change-Id: Icb9a86875322aaf2cd5feddc9f4482c2aaef3412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2758249
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#862643}

--- chrome/browser/ui/ash/holding_space/holding_space_ui_browsertest.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_ui_browsertest.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/ui/ash/holding_space/holding_space_browsertest_base.h"
 
+#include <unordered_map>
 #include <vector>
 
 #include "ash/public/cpp/ash_features.h"
@@ -23,10 +24,12 @@
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/holding_space/holding_space_util.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/aura/window.h"
+#include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
 #include "ui/base/dragdrop/os_exchange_data_provider_factory.h"
@@ -239,6 +242,10 @@ class MockHoldingSpaceClient : public HoldingSpaceClient {
               CopyImageToClipboard,
               (const HoldingSpaceItem& item, SuccessCallback callback),
               (override));
+  MOCK_METHOD(base::FilePath,
+              CrackFileSystemUrl,
+              (const GURL& file_system_url),
+              (const, override));
   MOCK_METHOD(void, OpenDownloads, (SuccessCallback callback), (override));
   MOCK_METHOD(void, OpenMyFiles, (SuccessCallback callback), (override));
   MOCK_METHOD(void,
@@ -293,24 +300,38 @@ class DropSenderView : public views::WidgetDelegateView,
     return new DropSenderView(context);
   }
 
-  void SetData(const std::vector<base::FilePath> file_paths) {
+  void ClearFilenamesData() { filenames_data_.reset(); }
+
+  void SetFilenamesData(const std::vector<base::FilePath> file_paths) {
     std::vector<ui::FileInfo> filenames;
     for (const base::FilePath& file_path : file_paths)
       filenames.emplace_back(file_path, /*display_name=*/base::FilePath());
-    data_provider_->SetFilenames(filenames);
+    filenames_data_.emplace(std::move(filenames));
+  }
+
+  void ClearFileSystemSourcesData() { file_system_sources_data_.reset(); }
+
+  void SetFileSystemSourcesData(const std::vector<GURL>& file_system_urls) {
+    constexpr char kFileSystemSourcesType[] = "fs/sources";
+
+    std::stringstream file_system_sources;
+    for (const GURL& file_system_url : file_system_urls)
+      file_system_sources << file_system_url.spec() << "\n";
+
+    base::Pickle pickle;
+    ui::WriteCustomDataToPickle(
+        std::unordered_map<std::u16string, std::u16string>(
+            {{base::UTF8ToUTF16(kFileSystemSourcesType),
+              base::UTF8ToUTF16(file_system_sources.str())}}),
+        &pickle);
+
+    file_system_sources_data_.emplace(std::move(pickle));
   }
 
  private:
   explicit DropSenderView(aura::Window* context) {
     InitWidget(context);
-
     set_drag_controller(this);
-    data_provider_ = ui::OSExchangeDataProviderFactory::CreateProvider();
-
-    // NOTE: Gesture drag is only enabled if a drag image is specified.
-    data_provider_->SetDragImage(
-        /*image=*/gfx::test::CreateImageSkia(/*width=*/10, /*height=*/10),
-        /*cursor_offset=*/gfx::Vector2d());
   }
 
   // views::DragController:
@@ -331,13 +352,19 @@ class DropSenderView : public views::WidgetDelegateView,
                             const gfx::Point& press_pt,
                             ui::OSExchangeData* data) override {
     // Drag image.
-    data->provider().SetDragImage(data_provider_->GetDragImage(),
-                                  data_provider_->GetDragImageOffset());
+    // NOTE: Gesture drag is only enabled if a drag image is specified.
+    data->provider().SetDragImage(
+        /*image=*/gfx::test::CreateImageSkia(/*width=*/10, /*height=*/10),
+        /*cursor_offset=*/gfx::Vector2d());
 
     // Payload.
-    std::vector<ui::FileInfo> filenames;
-    ASSERT_TRUE(data_provider_->GetFilenames(&filenames));
-    data->provider().SetFilenames(filenames);
+    if (filenames_data_)
+      data->provider().SetFilenames(filenames_data_.value());
+    if (file_system_sources_data_) {
+      data->provider().SetPickledData(
+          ui::ClipboardFormatType::GetWebCustomDataType(),
+          file_system_sources_data_.value());
+    }
   }
 
   void InitWidget(aura::Window* context) {
@@ -353,7 +380,8 @@ class DropSenderView : public views::WidgetDelegateView,
     widget->Init(std::move(params));
   }
 
-  std::unique_ptr<ui::OSExchangeDataProvider> data_provider_;
+  base::Optional<std::vector<ui::FileInfo>> filenames_data_;
+  base::Optional<base::Pickle> file_system_sources_data_;
 };
 
 // DropTargetView --------------------------------------------------------------
@@ -486,16 +514,28 @@ class HoldingSpaceUiBrowserTest : public HoldingSpaceBrowserTestBase {
 
 // Tests -----------------------------------------------------------------------
 
+using PerformDragAndDropCallback =
+    base::RepeatingCallback<void(const views::View* from,
+                                 const views::View* to,
+                                 DragUpdateCallback drag_update_callback,
+                                 base::OnceClosure before_release_callback,
+                                 base::OnceClosure after_release_callback)>;
+
+enum StorageLocationFlag : uint32_t {
+  kFilenames = 1 << 0,
+  kFileSystemSources = 1 << 1,
+};
+
+using StorageLocationFlags = uint32_t;
+
 // Base class for holding space UI browser tests that test drag-and-drop.
-// Parameterized by a callback to invoke to perform a drag-and-drop.
+// Parameterized by:
+//   [0] - callback to invoke to perform a drag-and-drop.
+//   [1] - storage location(s) on `ui::OSExchangeData` at which to store files.
 class HoldingSpaceUiDragAndDropBrowserTest
     : public HoldingSpaceUiBrowserTest,
-      public testing::WithParamInterface<base::RepeatingCallback<void(
-          const views::View* from,
-          const views::View* to,
-          DragUpdateCallback drag_update_callback,
-          base::OnceClosure before_release_callback,
-          base::OnceClosure after_release_callback)>> {
+      public testing::WithParamInterface<
+          std::tuple<PerformDragAndDropCallback, StorageLocationFlags>> {
  public:
   // Asserts expectations that the holding space tray is or isn't a drop target.
   void ExpectTrayIsDropTarget(bool is_drop_target) {
@@ -523,9 +563,30 @@ class HoldingSpaceUiDragAndDropBrowserTest
       DragUpdateCallback drag_update_callback = base::DoNothing(),
       base::OnceClosure before_release_callback = base::DoNothing(),
       base::OnceClosure after_release_callback = base::DoNothing()) {
-    GetParam().Run(from, to, std::move(drag_update_callback),
-                   std::move(before_release_callback),
-                   std::move(after_release_callback));
+    GetPerformDragAndDropCallback().Run(
+        from, to, std::move(drag_update_callback),
+        std::move(before_release_callback), std::move(after_release_callback));
+  }
+
+  // Sets data on `sender()` at the storage location specified by test params.
+  void SetSenderData(const std::vector<base::FilePath>& file_paths) {
+    if (ShouldStoreDataIn(StorageLocationFlag::kFilenames))
+      sender()->SetFilenamesData(file_paths);
+    else
+      sender()->ClearFilenamesData();
+
+    if (!ShouldStoreDataIn(StorageLocationFlag::kFileSystemSources)) {
+      sender()->ClearFileSystemSourcesData();
+      return;
+    }
+
+    std::vector<GURL> file_system_urls;
+    for (const base::FilePath& file_path : file_paths) {
+      file_system_urls.push_back(
+          holding_space_util::ResolveFileSystemUrl(GetProfile(), file_path));
+    }
+
+    sender()->SetFileSystemSourcesData(file_system_urls);
   }
 
   // Returns the view serving as the drop sender for tests.
@@ -554,6 +615,18 @@ class HoldingSpaceUiDragAndDropBrowserTest
     drop_sender_view_->GetWidget()->Close();
     drop_target_view_->GetWidget()->Close();
     HoldingSpaceUiBrowserTest::TearDownOnMainThread();
+  }
+
+  PerformDragAndDropCallback GetPerformDragAndDropCallback() {
+    return std::get<0>(GetParam());
+  }
+
+  StorageLocationFlags GetStorageLocationFlags() const {
+    return std::get<1>(GetParam());
+  }
+
+  bool ShouldStoreDataIn(StorageLocationFlag flag) const {
+    return GetStorageLocationFlags() & flag;
   }
 
   DropSenderView* drop_sender_view_ = nullptr;
@@ -637,7 +710,7 @@ IN_PROC_BROWSER_TEST_P(HoldingSpaceUiDragAndDropBrowserTest, DragAndDropToPin) {
   // Create a file to be dragged into the holding space.
   std::vector<base::FilePath> file_paths;
   file_paths.push_back(CreateFile());
-  sender()->SetData(file_paths);
+  SetSenderData(file_paths);
 
   // Expect no events have been recorded to histograms.
   base::HistogramTester histogram_tester;
@@ -685,7 +758,7 @@ IN_PROC_BROWSER_TEST_P(HoldingSpaceUiDragAndDropBrowserTest, DragAndDropToPin) {
   // Create a few more files to be dragged into the holding space.
   file_paths.push_back(CreateFile());
   file_paths.push_back(CreateFile());
-  sender()->SetData(file_paths);
+  SetSenderData(file_paths);
 
   {
     base::RunLoop run_loop;
@@ -768,12 +841,19 @@ IN_PROC_BROWSER_TEST_P(HoldingSpaceUiDragAndDropBrowserTest, DragAndDropToPin) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(All,
-                         HoldingSpaceUiDragAndDropBrowserTest,
-                         testing::ValuesIn({
-                             base::BindRepeating(&MouseDrag),
-                             base::BindRepeating(&GestureDrag),
-                         }));
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    HoldingSpaceUiDragAndDropBrowserTest,
+    testing::Combine(testing::ValuesIn({
+                         base::BindRepeating(&MouseDrag),
+                         base::BindRepeating(&GestureDrag),
+                     }),
+                     testing::ValuesIn(std::vector<StorageLocationFlags>({
+                         StorageLocationFlag::kFilenames,
+                         StorageLocationFlag::kFileSystemSources,
+                         StorageLocationFlag::kFilenames |
+                             StorageLocationFlag::kFileSystemSources,
+                     }))));
 
 // Verifies that the holding space tray does not appear on the lock screen.
 IN_PROC_BROWSER_TEST_F(HoldingSpaceUiBrowserTest, LockScreen) {
