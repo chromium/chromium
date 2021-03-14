7e1e44476bb361e95dc252d8428324a3feb89ecd
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-03-14 19:37:05 +0000

    [LSC] Remove base/strings/string16.h
    This change removes base/strings/string16.h and remaining references to
    it from the code base.
    
    Bug: 1184339
    Change-Id: Ia52688fac747cd2d70ea5d52be5d7c2dd0db4829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756251
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Owners-Override: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862685}

--- base/strings/string_piece.h
+++ base/strings/string_piece.h
@@ -568,9 +568,9 @@ BASE_EXPORT std::ostream& operator<<(std::ostream& o, WStringPiece piece);
 // We provide appropriate hash functions so StringPiece and StringPiece16 can
 // be used as keys in hash sets and maps.
 
-// This hash function is copied from base/strings/string16.h. We don't use the
-// ones already defined for string and std::u16string directly because it would
-// require the string constructors to be called, which we don't want.
+// This is a custom hash function. We don't use the ones already defined for
+// string and std::u16string directly because it would require the string
+// constructors to be called, which we don't want.
 
 template <typename StringPieceType>
 struct StringPieceHashImpl {
