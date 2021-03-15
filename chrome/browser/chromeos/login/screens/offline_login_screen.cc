91699bbc55f8a7d875b8357cabc5a6d89c21da9f
Danila Kuzmin <dkuzmin@google.com>
2021-03-15 13:08:45 +0000

    OOBE OfflineLogin: Correct login call to use internal authorization
    During screen rework in [1] the wrong login function was used.
    
    [1] - https://chromium-review.googlesource.com/c/chromium/src/+/2450314
    
    (cherry picked from commit 4a90b280bc0d8c94f8a5eb7eeb575dc90b4cf00c)
    
    Bug: 1187537
    Change-Id: Ifd92c446df17a92b35fdef77cfaf8cbbbbf7eb15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756115
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#862523}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2759705
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/branch-heads/4389@{#1545}
    Cr-Branched-From: 9251c5db2b6d5a59fe4eac7aafa5fed37c139bb7-refs/heads/master@{#843830}

--- chrome/browser/chromeos/login/screens/offline_login_screen.cc
+++ chrome/browser/chromeos/login/screens/offline_login_screen.cc
@@ -8,6 +8,7 @@
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/browser/chromeos/login/existing_user_controller.h"
 #include "chrome/browser/chromeos/login/screen_manager.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
 #include "chrome/browser/chromeos/login/wizard_context.h"
@@ -147,7 +148,14 @@ void OfflineLoginScreen::HandleCompleteAuth(const std::string& email,
         << user_context.GetUserType();
   }
   user_context.SetIsUsingOAuth(false);
-  LoginDisplayHost::default_host()->CompleteLogin(user_context);
+  // TODO(dkuzmin): call Login through delegate.
+  if (ExistingUserController::current_controller()) {
+    ExistingUserController::current_controller()->Login(user_context,
+                                                        SigninSpecifics());
+  } else {
+    LOG(ERROR) << "OfflineLoginScreen::HandleCompleteAuth: "
+               << "ExistingUserController not available.";
+  }
 }
 
 void OfflineLoginScreen::StartIdleDetection() {
