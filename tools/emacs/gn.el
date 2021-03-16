fa1cc81c7300af05ae60dc8513929ed4d450edc8
Henrique Ferreiro <hferreiro@igalia.com>
2021-03-16 22:19:54 +0000

    build: Allow using platform names as output dirs
    Now that sources_assignment_filter is gone, there's no reason to limit
    the name of the output directory, allowing the usage of out/linux or
    out/win, for example.
    
    Also, remove the last references to it.
    
    Bug: 1018739
    Change-Id: I5dbf8dcc0b0ea5b2d7f9780eae429246407def94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757191
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#863498}

--- tools/emacs/gn.el
+++ tools/emacs/gn.el
@@ -74,8 +74,8 @@ variable name or the '{{' and '}}' which surround it."
     "forward_variables_from" "get_label_info" "get_path_info"
     "get_target_outputs" "getenv" "import" "not_needed" "print"
     "process_file_template" "read_file" "rebase_path" "set_default_toolchain"
-    "set_defaults" "set_sources_assignment_filter" "split_list" "template"
-    "tool" "toolchain" "propagates_configs" "write_file"))
+    "set_defaults" "split_list" "template" "tool" "toolchain"
+    "propagates_configs" "write_file"))
 
 (defvar gn-font-lock-predefined-var-keywords
   '("current_cpu" "current_os" "current_toolchain" "default_toolchain"
