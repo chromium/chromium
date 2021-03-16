8775e162840833e9a66bd639f211dcb1c36357fd
Elizabeth Popova <lizapopova@google.com>
2021-03-16 10:55:43 +0000

    [Autofill] Confirm address save on Android only when Messages enabled
    Prior to this change, importing an address profile led to crash on
    Android when AutofillAddressProfileSavePrompt feature was enabled but
    MessagesForAndroidInfrastructure was disabled. Now, we fallback to the
    default behavior without the prompt when
    MessagesForAndroidInfrastructure is disabled.
    
    Bug: 1167061
    Change-Id: Ibe2205094054346bbdd7e4f632658bc2a40f0864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744050
    Owners-Override: Evan Stade <estade@chromium.org>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863223}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -90,6 +90,7 @@
 #include "components/autofill/core/browser/ui/payments/card_expiration_date_fix_flow_view.h"
 #include "components/autofill/core/browser/ui/payments/card_name_fix_flow_view.h"
 #include "components/infobars/core/infobar.h"
+#include "components/messages/android/messages_feature.h"
 #include "ui/android/window_android.h"
 #else  // !OS_ANDROID
 #include "chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.h"
@@ -531,8 +532,16 @@ void ChromeAutofillClient::ConfirmSaveAddressProfile(
     const AutofillProfile& profile,
     AddressProfileSavePromptCallback callback) {
 #if defined(OS_ANDROID)
-  save_address_profile_message_delegate_.DisplaySavePrompt(
-      web_contents(), profile, std::move(callback));
+  if (base::FeatureList::IsEnabled(
+          messages::kMessagesForAndroidInfrastructure)) {
+    save_address_profile_message_delegate_.DisplaySavePrompt(
+        web_contents(), profile, std::move(callback));
+  } else {
+    // Fallback to the default behavior without the prompt.
+    std::move(callback).Run(
+        AutofillClient::SaveAddressProfileOfferUserDecision::kAccepted,
+        profile);
+  }
 #else
   SaveAddressProfileBubbleControllerImpl::CreateForWebContents(web_contents());
   SaveAddressProfileBubbleControllerImpl* controller =
