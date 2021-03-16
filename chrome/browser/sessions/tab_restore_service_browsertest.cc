7eb6d93e427192fa4ae978f3088e844e75a6f6c3
Joel Hockey <joelhockey@chromium.org>
2021-03-16 01:23:27 +0000

    Restore last app tab to correct app window
    When the last app tab is closed, we receive CreateHistoricalTab() with
    the last tab and add a Tab Entry, then we receive BrowserClosing(), but
    since there are no tabs, we do not add any Window Entry. When the Entry
    is subsequently restored, it uses a normal browser window rather than
    the correct app window.
    
    This change modifies CreateHistoricalTab() to detect when the last tab
    for an app browser is closing and redirect to BrowserClosing().
    
    (cherry picked from commit 509791234c48610d1f80972db7d341dc01bc3093)
    
    Bug: 1186599
    Change-Id: Idc0f9985f56a6e120ecf31d60b17e4e60be787d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2751245
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#863054}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762479
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#486}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- chrome/browser/sessions/tab_restore_service_browsertest.cc
+++ chrome/browser/sessions/tab_restore_service_browsertest.cc
@@ -10,13 +10,27 @@
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
+#include "chrome/browser/web_applications/test/test_system_web_app_installation.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 
 using Window = sessions::TabRestoreService::Window;
 
-using TabRestoreServiceImplBrowserTest = InProcessBrowserTest;
+class TabRestoreServiceImplBrowserTest : public InProcessBrowserTest {
+ public:
+  TabRestoreServiceImplBrowserTest()
+      : test_system_web_app_installation_(
+            web_app::TestSystemWebAppInstallation::
+                SetUpTabbedMultiWindowApp()) {}
+
+ protected:
+  std::unique_ptr<web_app::TestSystemWebAppInstallation>
+      test_system_web_app_installation_;
+};
 
 IN_PROC_BROWSER_TEST_F(TabRestoreServiceImplBrowserTest, RestoreApp) {
   Profile* profile = browser()->profile();
@@ -36,6 +50,40 @@ IN_PROC_BROWSER_TEST_F(TabRestoreServiceImplBrowserTest, RestoreApp) {
   ASSERT_EQ(sessions::TabRestoreService::WINDOW, restored_entry->type);
   const Window* restored_window = static_cast<const Window*>(restored_entry);
   EXPECT_EQ(app_name, restored_window->app_name);
+}
+
+// Test that the last app browser tab saves the Window to ensure it will be
+// reopened in the correct app browser.
+IN_PROC_BROWSER_TEST_F(TabRestoreServiceImplBrowserTest,
+                       LastAppTabSavesWindow) {
+  Profile* profile = browser()->profile();
+  sessions::TabRestoreService* trs =
+      TabRestoreServiceFactory::GetForProfile(profile);
+
+  test_system_web_app_installation_->WaitForAppInstall();
+  Browser* app_browser = web_app::LaunchWebAppBrowser(
+      browser()->profile(), test_system_web_app_installation_->GetAppId());
+  GURL app_url = test_system_web_app_installation_->GetAppUrl();
+  ui_test_utils::NavigateToURL(app_browser, app_url);
+
+  // Create second tab and close it, TAB entry should be created.
+  chrome::NewTab(app_browser);
+  ui_test_utils::NavigateToURL(app_browser, app_url);
+  chrome::CloseTab(app_browser);
+  ASSERT_EQ(1U, trs->entries().size());
+  const sessions::TabRestoreService::Entry* tab_entry =
+      trs->entries().front().get();
+  EXPECT_EQ(sessions::TabRestoreService::TAB, tab_entry->type);
+
+  // Close last tab, WINDOW entry should be created.
+  chrome::CloseTab(app_browser);
+  EXPECT_EQ(2U, trs->entries().size());
+  const sessions::TabRestoreService::Entry* window_entry =
+      trs->entries().front().get();
+  ASSERT_EQ(sessions::TabRestoreService::WINDOW, window_entry->type);
+  const Window* restored_window = static_cast<const Window*>(window_entry);
+  EXPECT_EQ(app_browser->app_name(), restored_window->app_name);
+  EXPECT_EQ(1U, restored_window->tabs.size());
 }
 
 #endif  // defined(USE_AURA)
