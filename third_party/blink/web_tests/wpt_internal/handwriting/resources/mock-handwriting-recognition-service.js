a5b8acb44fdc7d91274331d420f62cc6736a749f
Honglin Yu <honglinyu@chromium.org>
2021-03-16 05:12:24 +0000

    Add web test for handwriting API.
    Bug: 1166910
    Test: ./third_party/blink/tools/run_web_tests.py
    Test: -t Default
    Test: wpt_internal/handwriting/handwriting-recognition-interface.https.html
    Change-Id: I202356972de18b08e4ab189e4b3f76add03eedde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2758532
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Honglin Yu <honglinyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863144}

---
+++ third_party/blink/web_tests/wpt_internal/handwriting/resources/mock-handwriting-recognition-service.js
@@ -0,0 +1,96 @@
+import {CreateHandwritingRecognizerResult, HandwritingRecognitionService, HandwritingRecognitionServiceReceiver, HandwritingRecognizerReceiver, HandwritingRecognizerRemote} from '/gen/third_party/blink/public/mojom/handwriting/handwriting.mojom.m.js';
+
+// Generates the prediction result based on strokes and hints.
+// The segmentation result is empty.
+function transformHandwritingMojoStroke(stroke) {
+  return stroke.points.map(point => ({
+    x: Math.round(point.location.x),
+    y: Math.round(point.location.y),
+    t: Math.round(Number(point.t.microseconds) / 1000)}));
+}
+
+function transformHandwritingIDLStroke(stroke) {
+  return stroke.getPoints().map(point => ({
+    x: Math.round(point.x),
+    y: Math.round(point.y),
+    t: Math.round(point.t)}));
+}
+
+// We need to export this function because we will verify whether the prediction
+// result is as expected.
+export function generateHandwritingPrediction(strokes, hints) {
+  const result = { strokes: [] };
+  for (let i = 0; i < strokes.length; i++) {
+    // Check which kind of stroke it is. Mojo Stroke should have a `points`
+    // member and IDL stroke does not.
+    // Note that `strokes[i] instanceof HandwritingStroke` does not work here.
+    if ('points' in strokes[i]) {
+      result.strokes.push(transformHandwritingMojoStroke(strokes[i]));
+    } else {
+      result.strokes.push(transformHandwritingIDLStroke(strokes[i]));
+    }
+  }
+  result.hints = hints;
+  return [{text: JSON.stringify(result), segmentationResult: []}];
+}
+
+class MockHandwritingRecognizer {
+  // In this mock impl, we ignore the `modelConstraint`.
+  constructor(modelConstraint) {}
+
+  bind(request) {
+    this.receiver_ = new HandwritingRecognizerReceiver(this);
+    this.receiver_.$.bindHandle(request.handle);
+  }
+
+  async getPrediction(strokes, hints) {
+    return {prediction: generateHandwritingPrediction(strokes, hints)};
+  }
+}
+
+let mockHandwritingRecognizer =
+    new MockHandwritingRecognizer({languages: ['en']});
+
+class MockHandwritingRecognitionService {
+  constructor() {
+    this.interceptor_ = new MojoInterfaceInterceptor(
+        HandwritingRecognitionService.$interfaceName);
+    this.interceptor_.oninterfacerequest = e => this.bind(e.handle);
+    this.receiver_ = new HandwritingRecognitionServiceReceiver(this);
+
+    this.interceptor_.start();
+  }
+
+  bind(handle) {
+    this.receiver_.$.bindHandle(handle);
+  }
+
+  async createHandwritingRecognizer(modelConstraint) {
+    const handwritingRecognizer = new HandwritingRecognizerRemote();
+    mockHandwritingRecognizer.bind(
+        handwritingRecognizer.$.bindNewPipeAndPassReceiver());
+
+    return {
+      result: CreateHandwritingRecognizerResult.kOk,
+      handwritingRecognizer: handwritingRecognizer,
+    };
+  }
+
+  async queryHandwritingRecognizerSupport(query) {
+    const support = {};
+    // In this mock class, we pretend we support all features.
+    if (query.languages.length !== 0) {
+      support.languages = true;
+    }
+    if (query.alternatives === true) {
+      support.alternatives = true;
+    }
+    if (query.segmentationResult === true) {
+      support.segmentationResult = true;
+    }
+
+    return {result: support};
+  }
+}
+
+let mockHandwritingRecognitionService = new MockHandwritingRecognitionService();
