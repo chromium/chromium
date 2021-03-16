0af2c390addba0386bdddad03c1d3850ece4f4a2
Gang Wu <gangwu@chromium.org>
2021-03-16 22:57:21 +0000

    Add new UKMs related to request desktop sites
    The UKM collection review:
    https://docs.google.com/document/d/1rpIChXhgxHaFsR5DFrZ_dUfeTnXUq5rl0lvQA8pcQZc/edit#
    
    (cherry picked from commit ff9d5827c13556b3d7d966953e8ede5f9392749a)
    
    Bug: 1186739
    Change-Id: Ia5f53ee666f010b9546cc90c5f44a0aa47f65ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749695
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#862988}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765509
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4430@{#526}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -143,6 +143,7 @@ import org.chromium.chrome.browser.share.ShareDelegateImpl;
 import org.chromium.chrome.browser.share.ShareDelegateSupplier;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.chrome.browser.tab.AccessibilityVisibilityHandler;
+import org.chromium.chrome.browser.tab.RequestDesktopUtils;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabHidingType;
 import org.chromium.chrome.browser.tab.TabLaunchType;
@@ -898,6 +899,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             }
         }
         VrModuleProvider.getDelegate().onActivityShown(this);
+
+        MultiWindowUtils.getInstance().recordMultiWindowStateUkm(this, tab);
     }
 
     private void onActivityHidden() {
@@ -1227,9 +1230,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
      * Actions that may be run at some point after startup for Android N multi-window mode. Should
      * be called from #onDeferredStartup() if the activity is in multi-window mode.
      */
-    protected void onDeferredStartupForMultiWindowMode() {
+    private void onDeferredStartupForMultiWindowMode() {
         // If the Activity was launched in multi-window mode, record a user action.
-        recordMultiWindowModeChangedUserAction(true);
+        recordMultiWindowModeChanged(
+                /* isInMultiWindowMode= */ true, /* isDeferredStartup= */ true);
     }
 
     /**
@@ -1267,6 +1271,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (mCompositorViewHolder != null) mCompositorViewHolder.onStart();
 
         mConfig = getResources().getConfiguration();
+
         mStarted = true;
     }
 
@@ -2022,6 +2027,12 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 return;
             }
         }
+
+        if (newConfig.orientation != mConfig.orientation) {
+            RequestDesktopUtils.recordScreenOrientationChangedUkm(
+                    newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE, getActivityTab());
+        }
+
         mConfig = newConfig;
     }
 
@@ -2051,7 +2062,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         // will be called in #onResumeWithNative(). Both of these methods require native to be
         // initialized, so do not call here to avoid crashing. See https://crbug.com/797921.
         if (mNativeInitialized) {
-            recordMultiWindowModeChangedUserAction(isInMultiWindowMode);
+            recordMultiWindowModeChanged(isInMultiWindowMode, /* isDeferredStartup= */ false);
 
             if (!isInMultiWindowMode
                     && ApplicationStatus.getStateForActivity(this) == ActivityState.RESUMED) {
@@ -2073,15 +2084,23 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     }
 
     /**
-     * Records user actions associated with entering and exiting Android N multi-window mode
+     * Records user actions and ukms associated with entering and exiting Android N multi-window
+     * mode.
      * @param isInMultiWindowMode True if the activity is in multi-window mode.
+     * @param isDeferredStartup True if the activity is deferred startup.
      */
-    protected void recordMultiWindowModeChangedUserAction(boolean isInMultiWindowMode) {
-        if (isInMultiWindowMode) {
-            RecordUserAction.record("Android.MultiWindowMode.Enter");
-        } else {
-            RecordUserAction.record("Android.MultiWindowMode.Exit");
-        }
+    private void recordMultiWindowModeChanged(
+            boolean isInMultiWindowMode, boolean isDeferredStartup) {
+        MultiWindowUtils.getInstance().recordMultiWindowModeChanged(
+                isInMultiWindowMode, isDeferredStartup, isFirstActivity(), getActivityTab());
+    }
+
+    /**
+     * This method serves to distinguish windows in multi-window mode.
+     * @return True if this activity is the first created activity.
+     */
+    protected boolean isFirstActivity() {
+        return true;
     }
 
     @Override
@@ -2343,9 +2362,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                     currentTab.getWebContents().getNavigationController().getUseDesktopUserAgent();
             usingDesktopUserAgent = !usingDesktopUserAgent;
             TabUtils.switchUserAgent(currentTab, usingDesktopUserAgent, /* forcedByUser */ true);
-            RecordUserAction.record("MobileMenuRequestDesktopSite");
-            RecordHistogram.recordBooleanHistogram(
-                    "Android.RequestDesktopSite.UserSwitchToDesktop", usingDesktopUserAgent);
+            RequestDesktopUtils.recordUserChangeUserAgent(usingDesktopUserAgent, getActivityTab());
             return true;
         }
 
---
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/RequestDesktopUtils.java
@@ -0,0 +1,66 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.tab;
+
+import androidx.annotation.IntDef;
+import androidx.annotation.Nullable;
+
+import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.browser.metrics.UkmRecorder;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Utilities for requesting desktop sites support.
+ */
+public class RequestDesktopUtils {
+    // Note: these values must match the UserAgentRequestType enum in enums.xml.
+    @IntDef({UserAgentRequestType.REQUEST_DESKTOP, UserAgentRequestType.REQUEST_MOBILE})
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface UserAgentRequestType {
+        int REQUEST_DESKTOP = 0;
+        int REQUEST_MOBILE = 1;
+    }
+
+    // Note: these values must match the DeviceOrientation2 enum in enums.xml.
+    @IntDef({DeviceOrientation2.LANDSCAPE, DeviceOrientation2.PORTRAIT})
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface DeviceOrientation2 {
+        int LANDSCAPE = 0;
+        int PORTRAIT = 1;
+    }
+
+    /**
+     * Records the metrics associated with changing the user agent by user agent.
+     * @param isDesktop True if the user agent is the desktop.
+     * @param tab The current activity {@link Tab}.
+     */
+    public static void recordUserChangeUserAgent(boolean isDesktop, @Nullable Tab tab) {
+        RecordUserAction.record("MobileMenuRequestDesktopSite");
+        RecordHistogram.recordBooleanHistogram(
+                "Android.RequestDesktopSite.UserSwitchToDesktop", isDesktop);
+
+        if (tab == null || tab.isIncognito() || tab.getWebContents() == null) return;
+
+        new UkmRecorder.Bridge().recordEventWithIntegerMetric(tab.getWebContents(),
+                "Android.UserRequestedUserAgentChange", "UserAgentType",
+                isDesktop ? UserAgentRequestType.REQUEST_DESKTOP
+                          : UserAgentRequestType.REQUEST_MOBILE);
+    }
+
+    /**
+     * Records the ukms associated with changing screen orientation.
+     * @param isLandscape True if the orientation is landscape.
+     * @param tab The current activity {@link Tab}.
+     */
+    public static void recordScreenOrientationChangedUkm(boolean isLandscape, @Nullable Tab tab) {
+        if (tab == null || tab.isIncognito() || tab.getWebContents() == null) return;
+
+        new UkmRecorder.Bridge().recordEventWithIntegerMetric(tab.getWebContents(),
+                "Android.ScreenRotation", "TargetDeviceOrientation",
+                isLandscape ? DeviceOrientation2.LANDSCAPE : DeviceOrientation2.PORTRAIT);
+    }
+}
