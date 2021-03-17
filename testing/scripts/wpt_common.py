5b0bcde32a2facd7c20b2879ead1ad13de7f2821
Luke Zielinski <lpz@chromium.org>
2021-03-17 16:14:14 +0000

    [wptrunner] Generate image diff for failing reftests
    Change-Id: I2afb2ef2fd0c208efd787b51bd80d22cccdee1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760937
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863787}

--- testing/scripts/wpt_common.py
+++ testing/scripts/wpt_common.py
@@ -14,6 +14,7 @@ import common
 BLINK_TOOLS_DIR = os.path.join(common.SRC_DIR, 'third_party', 'blink', 'tools')
 WEB_TESTS_DIR = os.path.join(BLINK_TOOLS_DIR, os.pardir, 'web_tests')
 EXTERNAL_WPT_TESTS_DIR = os.path.join(WEB_TESTS_DIR, 'external', 'wpt')
+LAYOUT_TEST_RESULTS_SUBDIR = 'layout-test-results'
 
 if BLINK_TOOLS_DIR not in sys.path:
     sys.path.append(BLINK_TOOLS_DIR)
@@ -60,7 +61,8 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
 
         # Move json results into layout-test-results directory
         results_dir = os.path.dirname(self.wpt_output)
-        layout_test_results = os.path.join(results_dir, 'layout-test-results')
+        layout_test_results = os.path.join(results_dir,
+                                           LAYOUT_TEST_RESULTS_SUBDIR)
         if self.fs.exists(layout_test_results):
             self.fs.rmtree(layout_test_results)
         self.fs.maybe_make_directory(layout_test_results)
@@ -320,7 +322,7 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
               to the |results_dir|.
         """
         log_artifact_sub_path = (
-            os.path.join("layout-test-results",
+            os.path.join(LAYOUT_TEST_RESULTS_SUBDIR,
                          self.port.output_filename(
                              test_name, suffix, extension))
         )
@@ -353,6 +355,9 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
              path of the file for that screenshot
         """
         result={}
+        # Remember the two images so we can diff them later.
+        actual_image_bytes = None
+        expected_image_bytes = None
         for screenshot_pair in screenshot_artifact:
             screenshot_split = screenshot_pair.split(":")
             url = screenshot_split[0]
@@ -368,8 +373,13 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
                 screenshot_key = "actual_image"
                 file_suffix = test_failures.FILENAME_SUFFIX_ACTUAL
 
+            if screenshot_key == "expected_image":
+                expected_image_bytes = image_bytes
+            else:
+                actual_image_bytes = image_bytes
+
             screenshot_sub_path = (
-                os.path.join("layout-test-results",
+                os.path.join(LAYOUT_TEST_RESULTS_SUBDIR,
                              self.port.output_filename(
                                  test_name, file_suffix, ".png"))
             )
@@ -381,4 +391,25 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
                     os.path.dirname(screenshot_full_path))
             # Note: we are writing raw bytes to this file
             self.fs.write_binary_file(screenshot_full_path, image_bytes)
+
+        # Diff the two images and output the diff file.
+        diff_bytes, error = self.port.diff_image(expected_image_bytes,
+                                                 actual_image_bytes)
+        if diff_bytes and not error:
+            diff_sub_path = (
+                os.path.join(LAYOUT_TEST_RESULTS_SUBDIR,
+                             self.port.output_filename(
+                                 test_name, test_failures.FILENAME_SUFFIX_DIFF,
+                                 ".png")))
+            result["image_diff"] = diff_sub_path
+            diff_full_path = os.path.join(results_dir, diff_sub_path)
+            if not self.fs.exists(os.path.dirname(diff_full_path)):
+                self.fs.maybe_make_directory(os.path.dirname(diff_full_path))
+            # Note: we are writing raw bytes to this file
+            self.fs.write_binary_file(diff_full_path, diff_bytes)
+        else:
+            print("Error creating diff image for test %s. "
+                  "Error=%s, diff_bytes is None? %s\n"
+                  % (test_name, error, diff_bytes is None))
+
         return result
