38477e51f17ab8f7c5e9596735ce518de320b9a9
Nancy Wang <nancylingwang@chromium.org>
2021-03-17 03:02:58 +0000

    Reland "Refactor FullRestoreReadHandler."
    This CL adds a public method RemoveAppRestoreData to prepare for the ARC
    window restoration related CLs. RemoveAppRestoreData will be used by
    ArcReadHandler in the follow up CLs, e.g. CL:2759809.
    
    Bug: 1146900
    
    Change-Id: I2667f44785badcc2d835581c893412a3777789c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764264
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863612}

--- components/full_restore/full_restore_read_handler.h
+++ components/full_restore/full_restore_read_handler.h
@@ -73,6 +73,12 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
   // from |profile_path_to_restore_data_| for |profile_path| .
   void RemoveApp(const base::FilePath& profile_path, const std::string& app_id);
 
+  // Removes AppRestoreData from |profile_path| for |app_id| and
+  // |restore_window_id|.
+  void RemoveAppRestoreData(const base::FilePath& profile_path,
+                            const std::string& app_id,
+                            int32_t restore_window_id);
+
   // Returns true if there is a window info for |restore_window_id| from the
   // full restore file. Otherwise, returns false. This interface can't be used
   // for Arc app windows.
@@ -131,7 +137,7 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
                         std::unique_ptr<RestoreData>);
 
   // Removes AppRestoreData for |restore_window_id|.
-  void RemoveAppRestoreData(int restore_window_id);
+  void RemoveAppRestoreData(int32_t restore_window_id);
 
   // The current active user profile path.
   base::FilePath active_profile_path_;
@@ -141,9 +147,9 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
       profile_path_to_restore_data_;
 
   // The map from the window id to the full restore file path and the
-  // app id. The window id id is saved in the window property
+  // app id. The window id is saved in the window property
   // |kRestoreWindowIdKey|. This map is used to find the file path and the app
-  // id when get the window info.
+  // id when get the window info. This map is not used for ARC app windows.
   std::map<int32_t, std::pair<base::FilePath, std::string>>
       window_id_to_app_restore_info_;
 
