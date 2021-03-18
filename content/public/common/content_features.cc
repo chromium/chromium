d58d5d8b627b4c51d5d96b81c7beb5730607daee
Christopher Cameron <ccameron@chromium.org>
2021-03-18 20:26:13 +0000

    Disable DesktopCaptureMacV2
    This broke ChromeCast, and it's too far past branch point to responsibly
    shove a fix in.
    
    (cherry picked from commit ca164d4e220ce439992521ad1ea1140eb96f296d)
    
    Bug: 1185388
    Change-Id: I8c7fd1f680aa6391fb828c7367807270b004210a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2755178
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#863367}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774199
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: ccameron <ccameron@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#564}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -975,7 +975,7 @@ const base::Feature kRetryGetVideoCaptureDeviceInfos{
     "RetryGetVideoCaptureDeviceInfos", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kDesktopCaptureMacV2{"DesktopCaptureMacV2",
-                                         base::FEATURE_ENABLED_BY_DEFAULT};
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
 #endif  // defined(OS_MAC)
 
