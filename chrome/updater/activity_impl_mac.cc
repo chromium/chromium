c14c40bcf56dbd7aafc476ff7283bfbb57b172e3
Joshua Pawlicki <waffles@chromium.org>
2021-03-19 15:05:02 +0000

    Updater: Only delete actives files that are owned by the expected user.
    In the system case, the activity service is running as root. To avoid
    a symlink attack, check that the actives file is owned by the same
    user as owns the home directory in which it allegedly resides.
    
    This CL does not fix a potential symlink attack in which a user uses
    the system updater to test for file existence; a similar change in
    GetActiveBit is needed for that.
    
    Bug: 1096654
    Change-Id: Ib64932afef7369e123b152982f882daa1462ab54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773919
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864651}

--- chrome/updater/activity_impl_mac.cc
+++ chrome/updater/activity_impl_mac.cc
@@ -4,10 +4,16 @@
 
 #include "chrome/updater/activity_impl.h"
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include <string>
 
 #include "base/files/file_util.h"
+#include "base/files/scoped_file.h"
 #include "base/logging.h"
+#include "base/posix/eintr_wrapper.h"
 #include "chrome/updater/mac/mac_util.h"
 #include "chrome/updater/updater_branding.h"
 #include "chrome/updater/updater_scope.h"
@@ -15,26 +21,50 @@
 namespace updater {
 namespace {
 
-base::FilePath GetActiveFile(UpdaterScope scope, const std::string& id) {
-  // TODO(crbug.com/1096654): Add support for UpdaterScope::kSystem.
-  return base::GetHomeDir()
-      .AppendASCII("Library")
+base::FilePath GetActiveFile(const base::FilePath& home_dir,
+                             const std::string& id) {
+  return home_dir.AppendASCII("Library")
       .AppendASCII(COMPANY_SHORTNAME_STRING)
       .AppendASCII(COMPANY_SHORTNAME_STRING "SoftwareUpdate")
       .AppendASCII("Actives")
       .AppendASCII(id);
 }
 
+void ClearActiveBit(const base::FilePath& home_dir, const std::string& id) {
+  struct stat home_buffer = {0};
+  if (stat(home_dir.value().c_str(), &home_buffer)) {
+    VPLOG(2) << "Failed to stat " << home_dir;
+    return;
+  }
+
+  const base::FilePath active_file_path = GetActiveFile(home_dir, id);
+  const base::ScopedFD dir_fd(
+      HANDLE_EINTR(open(active_file_path.DirName().value().c_str(), O_RDONLY)));
+  if (!dir_fd.is_valid()) {
+    return;
+  }
+  struct stat active_file_buffer = {0};
+  if (fstatat(dir_fd.get(), active_file_path.BaseName().value().c_str(),
+              &active_file_buffer, AT_SYMLINK_NOFOLLOW)) {
+    return;
+  }
+  if (active_file_buffer.st_uid != home_buffer.st_uid) {
+    return;
+  }
+  unlinkat(dir_fd.get(), active_file_path.BaseName().value().c_str(), 0);
+}
+
 }  // namespace
 
 bool GetActiveBit(UpdaterScope scope, const std::string& id) {
-  const base::FilePath path = GetActiveFile(scope, id);
+  // TODO(crbug.com/1096654): Add support for UpdaterScope::kSystem.
+  const base::FilePath path = GetActiveFile(base::GetHomeDir(), id);
   return base::PathExists(path) && base::PathIsWritable(path);
 }
 
 void ClearActiveBit(UpdaterScope scope, const std::string& id) {
-  if (!base::DeleteFile(GetActiveFile(scope, id)))
-    VLOG(2) << "Failed to clear activity bit at " << GetActiveFile(scope, id);
+  // TODO(crbug.com/1096654): Add support for UpdaterScope::kSystem.
+  ClearActiveBit(base::GetHomeDir(), id);
 }
 
 }  // namespace updater
