fc979eb80d1e1ad48a9bc903c0769f309566e246
spdonghao <spdonghao@chromium.org>
2021-03-19 20:55:01 +0000

    Make isInOverviewAndShowingOmnibox() more accurate.
    Before, isInOverviewAndShowingOmnibox() was only updated when the new
    Layout starts showing, rather than the current Layout starts hiding.
    In this CL, LocationBarModel is notified when
    LayoutStateObserver#onStartedShowing() and onStartedHiding()
    events are triggered, and it updates |mIsShowingTabSwitcher| which makes
    isInOverviewAndShowingOmnibox() updated.
    
    Also, notifyTitleChanged(), notifyUrlChanged(),
    notifyPrimaryColorChanged(), notifySecurityStateChanged() are called
    when |mIsShowingTabSwitcher| is changed to ensure they are correct.
    
    
    (cherry picked from commit c759cd9d8a1c3aebc8992781fbf78bc8132a00d0)
    
    Bug: 1177359, 1184155
    Change-Id: I647edff05018fd0d8953ab1b5cfdd55cd5d10d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765065
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#863992}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775713
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#593}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -778,6 +778,7 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
             @Override
             public void onStartedShowing(@LayoutType int layoutType, boolean showToolbar) {
                 if (layoutType == LayoutType.TAB_SWITCHER) {
+                    mLocationBarModel.setIsShowingTabSwitcher(true);
                     mToolbar.setTabSwitcherMode(true, showToolbar, false);
                     updateButtonStatus();
                     if (mLocationBarModel.shouldShowLocationBarInOverviewMode()) {
@@ -791,6 +792,7 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
             public void onStartedHiding(
                     @LayoutType int layoutType, boolean showToolbar, boolean delayAnimation) {
                 if (layoutType == LayoutType.TAB_SWITCHER) {
+                    mLocationBarModel.setIsShowingTabSwitcher(false);
                     mToolbar.setTabSwitcherMode(false, showToolbar, delayAnimation);
                     updateButtonStatus();
                 }
