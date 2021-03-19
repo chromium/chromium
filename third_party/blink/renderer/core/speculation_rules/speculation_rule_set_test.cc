8752332678fa236d3ceded672832178db81f7f44
Jeremy Roman <jbroman@chromium.org>
2021-03-19 17:00:40 +0000

    Add missing copyright header to speculation_rule_set_test.cc.
    Bug: 1173646
    Change-Id: I108e618409c21668ed5d0e26105b498a85cddcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774165
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864685}

--- third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rule_set_test.cc
@@ -1,3 +1,7 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "third_party/blink/renderer/core/speculation_rules/speculation_rule_set.h"
 
 #include "base/bind.h"
