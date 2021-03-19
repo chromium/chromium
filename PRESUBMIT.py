44feb233c6b3157e40106c5bd714976fe12d5651
Edward Lesmes <ehmaldonado@google.com>
2021-03-19 01:27:52 +0000

    PRESUBMIT: Don't skip OWNERS check on Bot-Commit.
    Bug: 1189777
    Change-Id: I8c0522abff51d973fc6b5bc0b501d01e2eb8d883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773624
    Auto-Submit: Edward Lesmes <ehmaldonado@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/master@{#864513}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -2326,17 +2326,12 @@ def CheckAddedDepsHaveTargetApprovals(input_api, output_api):
   # might not use Gerrit.
   if not input_api.gerrit:
     return []
-  if input_api.change.issue:
-    # Skip OWNERS check when Bot-Commit label is approved. This label is
-    # intended for commits made by trusted bots that don't require review nor
-    # owners approval.
-    if input_api.gerrit.IsBotCommitApproved(input_api.change.issue):
-      return []
+  if (input_api.change.issue and
+      input_api.gerrit.IsOwnersOverrideApproved(input_api.change.issue)):
     # Skip OWNERS check when Owners-Override label is approved. This is intended
     # for global owners, trusted bots, and on-call sheriffs. Review is still
     # required for these changes.
-    if input_api.gerrit.IsOwnersOverrideApproved(input_api.change.issue):
-      return []
+    return []
 
   virtual_depended_on_files = set()
 
