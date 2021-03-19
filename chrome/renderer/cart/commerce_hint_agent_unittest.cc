a0afa94ee33e689ae476bc983e60221667dbfbbb
Yue Zhang <yuezhanggg@chromium.org>
2021-03-19 21:03:40 +0000

    Reland "[ChromeCart] Improve checkout detection heuristics"
    This reland commit fcffaf9c38fc60fed54470dd56def81cef17d76d.
    
    Reason for revert: fix the test on Linux CFI builder.
    
    Original change's description:
    > [ChromeCart] Improve checkout detection heuristics
    >
    > This improves the checkout detection heuristics by upstreaming
    > heuristics from https://colab.corp.google.com/drive/1Z8xrIRYK5SX20mVDDdlvk9n4DLpJ67-o?usp=sharing.
    >
    > Bug: 1188827
    > Change-Id: I8c822adc749ad0ae9637b7e505b868ecb27c2d35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764933
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#864286}
    
    Bug: 1188827
    Change-Id: Id8efe5afbf038f936b092b01c319557fdd950ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776039
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864788}

--- chrome/renderer/cart/commerce_hint_agent_unittest.cc
+++ chrome/renderer/cart/commerce_hint_agent_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/renderer/cart/commerce_hint_agent.h"
 
+#include "base/cfi_buildflags.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "components/search/ntp_features.h"
@@ -68,15 +69,27 @@ const char* kNotVisitCart[] = {
 };
 
 const char* kVisitCheckout[] = {
-  "https://www.amazon.com/gp/cart/mobile/go-to-checkout.html/ref=ox_sc_proceed?proceedToCheckout=1",
-  "https://smile.amazon.com/gp/cart/mobile/go-to-checkout.html/ref=ox_sc_proceed?proceedToCheckout=1",
+  "https://www.amazon.com/gp/buy/spc/handlers/display.html?",
+  "https://smile.amazon.com/gp/buy/spc/handlers/display.html?",
+  "https://pay.ebay.com/rgxo?action=view&sessionid=1111111111",
+  "https://www.example.com/chkout/rc",
+  "https://www.example.com/final-checkout",
+  "https://www.example.com/Secure-Checkout",
+  "https://www.example.com/Checkout-Begin",
+  "https://www.example.com/checkout-start",
   "http://example.com/us/checkout/",
   "http://example.com/checkout/",
   "https://example.com/123/checkouts/456",
   "http://example.com/123/checkouts/456",
+  "http://example.com/123/SPcheckouts",
+  "https://www.example.com/intlcheckout"
 };
 
 const char* kNotVisitCheckout[] = {
+  "https://www.amazon.com/gp/spc/handlers/display.html?",
+  "https://www.amazon.com/gp/aw/c?ref_=navm_hdr_cart",
+  "https://cart.ebay.com/rgxo",
+  "https://pay.ebay.com/api/xo",
   "https://example.com/gp/cart/mobile/go-to-checkout.html/ref=ox_sc_proceed?proceedToCheckout=1",
   "http://example.com/checkoutput",
   "http://example.com/us/checkoutside/",
@@ -95,12 +108,26 @@ const char* kPurchaseText[] = {
   "PAY NOW",
   "PLACE ORDER",
   "Pay now",
+  "pay $24",
+  "pay $24.35",
+  "pay USD 24.35",
+  "make payment",
+  "confirm & pay",
+  "PURCHASE",
+  "MAKE THIS PURCHASE",
 };
 
 const char* kNotPurchaseText[] = {
   "I'd like to pay now",
   "replace order",
   "Pay nowadays",
+  "make payment later",
+  "confirm and continue"
+  "submit",
+  "Continue to Review Order",
+  "next step: review order"
+  "Process Order",
+  "",
 };
 
 const char* kSkipText[] = {
@@ -268,13 +295,17 @@ TEST(CommerceHintAgentTest, MAYBE_RegexBenchmark) {
     // With string copy, it would take at least 400us, assuming string length is
     // 1M chars, and running x86-64 Release build on Linux workstation. x86-64
     // Debug build is ~4X slower, Debug build on Pixel 2 is 3~10X slower, and
-    // Release build on Nexus 5 is ~10X slower.
+    // Release build on Nexus 5 is ~10X slower. Linux CFI build is ~4X slower.
     int slow_factor = 1;
 #if !defined(NDEBUG)
     slow_factor *= 4;
 #endif
 #if defined(OS_ANDROID)
     slow_factor *= 10;
+#endif
+#if BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
+    BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC) || BUILDFLAG(CFI_ENFORCEMENT_TRAP)
+    slow_factor *= 4;
 #endif
 
     float elapsed_us = BenchmarkIsAddToCart(str);
@@ -287,9 +318,9 @@ TEST(CommerceHintAgentTest, MAYBE_RegexBenchmark) {
     EXPECT_LT(elapsed_us, 50.0 * slow_factor);
 
     elapsed_us = BenchmarkIsVisitCheckout(url);
-    // Typical value is ~0.2us.
-    // Without capping the length, it would take at least 30us.
-    EXPECT_LT(elapsed_us, 2.0 * slow_factor);
+    // Typical value is ~10us.
+    // Without capping the length, it would take at least 2000us.
+    EXPECT_LT(elapsed_us, 50.0 * slow_factor);
 
     elapsed_us = BenchmarkIsPurchase(str);
     // Typical value is ~0.1us.
