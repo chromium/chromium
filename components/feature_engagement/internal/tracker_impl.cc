2991f4473c366deade3663bf709243c3d7a4cd04
Sky Malice <skym@chromium.org>
2021-03-19 17:22:04 +0000

    Immediately dismiss tracking_only help UI.
    The feature engagement system previously expected their client to always
    dismiss after ShouldTriggerHelpUI(). However, this wasn't reasonable for
    tracking only features, because false is always returned by
    ShouldTriggerHelpUI() and the client has no idea when they should
    dismiss.
    
    To work around this the TrackerImpl now always dismisses tracking only
    features to reset their condition validator. This isn't a perfect
    solution, as real help UI will almost never be dismissed in 0 seconds,
    but it should be a good enough approximation.
    
    Bug: 1188679
    Change-Id: I179b1fb4a97c752b5d9ed0a91fbb4c5e9d915def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773179
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864689}

--- components/feature_engagement/internal/tracker_impl.cc
+++ components/feature_engagement/internal/tracker_impl.cc
@@ -194,7 +194,19 @@ bool TrackerImpl::ShouldTriggerHelpUI(const base::Feature& feature) {
            << ": trigger=" << result.NoErrors()
            << " tracking_only=" << feature_config.tracking_only << " "
            << result;
-  return result.NoErrors() && !feature_config.tracking_only;
+
+  if (feature_config.tracking_only) {
+    if (result.NoErrors()) {
+      // Because tracking only features always return false to the client,
+      // clients do not have the context to correctly dismiss. The dismiss is
+      // needed because our condition validator thinks the feature is currently
+      // showing. See https://crbug.com/1188679 for more details.
+      Dismissed(feature);
+    }
+    return false;
+  } else {
+    return result.NoErrors();
+  }
 }
 
 bool TrackerImpl::WouldTriggerHelpUI(const base::Feature& feature) const {
