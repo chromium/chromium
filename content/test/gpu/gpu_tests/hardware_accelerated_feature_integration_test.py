07a5511d29c0d44d44e95a961075b29e21852c85
James Darpinian <jdarpinian@chromium.org>
2021-03-19 01:43:54 +0000

    Increase timeout for loading about:gpu
    We're seeing timeouts on this line in the FYI waterfall, for example here:
    https://ci.chromium.org/p/chromium/builders/ci/Mac%20FYI%20Retina%20Debug%20%28NVIDIA%29/19117
    
    We'll see if increasing the timeout helps.
    
    TBR=kbr@chromium.org
    
    Bug: 1189426
    Change-Id: I06ddf335ff534f1a1d247416e383ed2ece61e37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774807
    Reviewed-by: James Darpinian <jdarpinian@chromium.org>
    Auto-Submit: James Darpinian <jdarpinian@chromium.org>
    Commit-Queue: James Darpinian <jdarpinian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864519}

--- content/test/gpu/gpu_tests/hardware_accelerated_feature_integration_test.py
+++ content/test/gpu/gpu_tests/hardware_accelerated_feature_integration_test.py
@@ -64,7 +64,7 @@ class HardwareAcceleratedFeatureIntegrationTest(
     feature = args[0]
     self._Navigate(test_path)
     tab = self.tab
-    tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+    tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=30)
     if not tab.EvaluateJavaScript(
         'VerifyHardwareAccelerated({{ feature }})', feature=feature):
       print 'Test failed. Printing page contents:'
