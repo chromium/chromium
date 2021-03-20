5fb5b61e76a3ca0aa901df6d925be7128f3b5d5b
Adam Langley <agl@chromium.org>
2021-03-20 00:29:11 +0000

    webauthn: log caBLEv2 connect errors to logcat.
    FIDO_LOG(ERROR) seems to be dropped from official builds, at least it
    doesn't appear in logcat. The device-log is also dropped from official
    builds so you can't see any messages there either.
    
    Suzy is reporting seeing connection errors sometimes with Canary so, to
    help debug that, make this log message a LOG(ERROR), which I believe
    will appear in logcat, even in official builds.
    
    BUG=1002262
    
    Change-Id: I0d149a2d2b17abcae7e95dae565e208d029266b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776378
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#864881}

--- device/fido/cable/websocket_adapter.cc
+++ device/fido/cable/websocket_adapter.cc
@@ -4,6 +4,7 @@
 
 #include "device/fido/cable/websocket_adapter.h"
 
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "components/device_event_log/device_event_log.h"
@@ -61,8 +62,8 @@ void WebSocketAdapter::OnOpeningHandshakeStarted(
 void WebSocketAdapter::OnFailure(const std::string& message,
                                  int net_error,
                                  int response_code) {
-  FIDO_LOG(ERROR) << "Tunnel server connection failed: " << message << " "
-                  << net_error << " " << response_code;
+  LOG(ERROR) << "Tunnel server connection failed: " << message << " "
+             << net_error << " " << response_code;
 
   if (response_code != net::HTTP_GONE) {
     // The callback will be cleaned up when the pipe disconnects.
