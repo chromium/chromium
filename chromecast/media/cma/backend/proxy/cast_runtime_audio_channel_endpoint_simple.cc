1f445764e67c0ca5224da106bd6a21375c2b9683
Ryan Keane <rwkeane@google.com>
2021-03-22 19:28:13 +0000

    Add GrpcEndpointManager
    This CL adds the interface that needs to be implemented inside Eureka
    to allow for accessing the gRPC endpoint defined there
    
    Bug: internal b/174695894
    Change-Id: Ibc813b1cad27799eebfc57d967a6bc582c04cfd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779363
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865301}

---
+++ chromecast/media/cma/backend/proxy/cast_runtime_audio_channel_endpoint_simple.cc
@@ -0,0 +1,20 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/media/cma/backend/proxy/cast_runtime_audio_channel_endpoint_manager.h"
+
+#include "base/notreached.h"
+
+namespace chromecast {
+namespace media {
+
+// static
+CastRuntimeAudioChannelEndpointManager*
+CastRuntimeAudioChannelEndpointManager::Get() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace media
+}  // namespace chromecast
