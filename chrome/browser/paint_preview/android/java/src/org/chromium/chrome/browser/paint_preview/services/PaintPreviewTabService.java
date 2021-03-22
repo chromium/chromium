049224ddd48b967c1b73183501230ace6aa292d2
ckitagawa <ckitagawa@chromium.org>
2021-03-22 19:07:08 +0000

    [Paint Preview] Only capture if loaded
    Only initiate a capture if the Tab is not in the middle of loading.
    
    Change-Id: I723dcaa34985f30596ac0e7e3ad17ac5b8dcc128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778831
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865290}

--- chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
+++ chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
@@ -89,7 +89,7 @@ public class PaintPreviewTabService implements NativePaintPreviewServiceProvider
             String scheme = tab.getUrl().getScheme();
             boolean schemeAllowed = scheme.equals("http") || scheme.equals("https");
             return !tab.isIncognito() && !tab.isNativePage() && !tab.isShowingErrorPage()
-                    && tab.getWebContents() != null && schemeAllowed;
+                    && tab.getWebContents() != null && !tab.isLoading() && schemeAllowed;
         }
     }
 
