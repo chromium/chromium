193197fea4d2db853dd83f1bd166d5ff6037bc46
Simon Zünd <szuend@chromium.org>
2021-03-23 10:07:49 +0000

    [devtools] Add regression test for mixed content issues via redirect
    This CL adds an inspector-protocol test verifying that the
    AffectedRequest is included in Mixed Content Issues stemming from
    server redirects that originate from the renderer:
    
      1) Initiate script load from renderer (e.g. via document.write ing
         a <script> tag) from a HTTPS origin
      2) Server redirects to a HTTP origin
      3) Renderer tries to load script via HTTP origin
    
    The Mixed Content issue filed in step 3 does currently not include
    the requestId. The ResourceRequest created for redirecting, and that
    is used for the Mixed Content check is transparent to the network
    agent. The two "requestWillBeSent" Network events are filed with the
    same "requestId", namely the ResourceRequest created in step 1.
    
    Please note that this CL doesn't actually include a fix and the test
    needs to be rebaselined once the requestId is actually included.
    
    R=sigurds@chromium.org
    
    Bug: chromium:1190808
    Change-Id: Ic72d49ddf06d468cdb9639a25da7e024ed4b81ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780260
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865576}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/mixed-content-issue-renderer-redirect-js.js
@@ -0,0 +1,18 @@
+(async function(testRunner) {
+  const {page, dp} = await testRunner.startBlank(
+    `Verifies that the AffectedRequest is included in an mixed content issue created when a HTTPS url redirects to a HTTP script (crbug.com/1190808)\n`);
+
+  await dp.Audits.enable();
+  await dp.Network.enable();
+  await page.navigate('https://devtools.test:8443/inspector-protocol/resources/empty.html');
+
+  const issuePromise = dp.Audits.onceIssueAdded();
+  await page.loadHTML(`
+    <script src='https://devtools.test:8443/inspector-protocol/resources/redirect-mixed-content-script.php'></script>
+  `)
+  const issue = await issuePromise;
+  // TODO(chromium:1190808): The test doesn't currently output the requestId but it actually should.
+  //     Remove this TODO and rebaseline the test once the bug is fixed.
+  testRunner.log(issue.params, "Inspector issue: ", ["frameId", "requestId"]);
+  testRunner.completeTest();
+})
