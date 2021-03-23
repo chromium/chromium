9960b1e2b8dec5fd0419208d9e79e213ba2f2f8e
Ilya Sherman <isherman@chromium.org>
2021-03-23 16:09:26 +0000

    Disable variations layers when low entropy provider is null
    R=​ntfschr, oysteine
    
    (cherry picked from commit ed4ff89ff5e5a6d86477343714d1090314a15f14)
    
    Bug: 1191169
    Change-Id: I1fc6f45ca4ec582e0e5c9e32a68dd707de8100a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780882
    Auto-Submit: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#865635}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2781844
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4455@{#5}
    Cr-Branched-From: 4189daf81451bce980ed75a04281fc8d78f1f4d6-refs/heads/master@{#865012}

--- ios/chrome/browser/ios_chrome_field_trials.h
+++ ios/chrome/browser/ios_chrome_field_trials.h
@@ -19,7 +19,7 @@ class IOSChromeFieldTrials : public variations::PlatformFieldTrials {
   void SetupFieldTrials() override;
   void SetupFeatureControllingFieldTrials(
       bool has_seed,
-      const base::FieldTrial::EntropyProvider& low_entropy_provider,
+      const base::FieldTrial::EntropyProvider* low_entropy_provider,
       base::FeatureList* feature_list) override;
 
  private:
