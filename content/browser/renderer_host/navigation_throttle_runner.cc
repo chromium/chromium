fca22699fdb1aae35572c8197395d793c1f5526e
Clark DuVall <cduvall@chromium.org>
2021-03-23 01:41:21 +0000

    [Merge M90] Add histograms to track NavigationThrottle execution and defer times
    This should help determine if any navigation throttles are holding up
    navigation.
    
    TBR=jam@chromium.org
    
    (cherry picked from commit 841e262bd084edf46c3c250cf35b3d4f41d4ff63)
    
    Bug: 1187753
    Change-Id: I5647bb4e1655f96d4d44bd127b49ccc8b5401c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757171
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#863124}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780722
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#663}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- content/browser/renderer_host/navigation_throttle_runner.cc
+++ content/browser/renderer_host/navigation_throttle_runner.cc
@@ -4,6 +4,7 @@
 
 #include "content/browser/renderer_host/navigation_throttle_runner.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/portal/portal_navigation_throttle.h"
 #include "content/browser/renderer_host/ancestor_throttle.h"
@@ -56,6 +57,41 @@ const char* GetEventName(NavigationThrottleRunner::Event event) {
   return "";
 }
 
+const char* GetEventNameForHistogram(NavigationThrottleRunner::Event event) {
+  switch (event) {
+    case NavigationThrottleRunner::Event::WillStartRequest:
+      return "WillStartRequest";
+    case NavigationThrottleRunner::Event::WillRedirectRequest:
+      return "WillRedirectRequest";
+    case NavigationThrottleRunner::Event::WillFailRequest:
+      return "WillFailRequest";
+    case NavigationThrottleRunner::Event::WillProcessResponse:
+      return "WillProcessResponse";
+    default:
+      NOTREACHED();
+  }
+  return "";
+}
+
+void RecordHistogram(NavigationThrottleRunner::Event event,
+                     base::Time start,
+                     const std::string& metric_type) {
+  base::TimeDelta delta = base::Time::Now() - start;
+  base::UmaHistogramTimes(base::StrCat({"Navigation.Throttle", metric_type, ".",
+                                        GetEventNameForHistogram(event)}),
+                          delta);
+}
+
+void RecordDeferTimeHistogram(NavigationThrottleRunner::Event event,
+                              base::Time start) {
+  RecordHistogram(event, start, "DeferTime");
+}
+
+void RecordExecutionTimeHistogram(NavigationThrottleRunner::Event event,
+                                  base::Time start) {
+  RecordHistogram(event, start, "ExecutionTime");
+}
+
 }  // namespace
 
 NavigationThrottleRunner::NavigationThrottleRunner(Delegate* delegate,
@@ -74,6 +110,7 @@ void NavigationThrottleRunner::ProcessNavigationEvent(Event event) {
 void NavigationThrottleRunner::ResumeProcessingNavigationEvent(
     NavigationThrottle* deferring_throttle) {
   DCHECK_EQ(GetDeferringThrottle(), deferring_throttle);
+  RecordDeferTimeHistogram(current_event_, defer_start_time_);
   ProcessInternal();
 }
 
@@ -167,6 +204,7 @@ void NavigationThrottleRunner::ProcessInternal() {
         "navigation", GetEventName(current_event_), local_navigation_id,
         "throttle", throttles_[i]->GetNameForLogging());
 
+    base::Time start = base::Time::Now();
     NavigationThrottle::ThrottleCheckResult result =
         ExecuteNavigationEvent(throttles_[i].get(), current_event_);
     if (!weak_ref) {
@@ -176,6 +214,7 @@ void NavigationThrottleRunner::ProcessInternal() {
                                       "result", "deleted");
       return;
     }
+    RecordExecutionTimeHistogram(current_event_, start);
     TRACE_EVENT_NESTABLE_ASYNC_END1("navigation", GetEventName(current_event_),
                                     local_navigation_id, "result",
                                     result.action());
@@ -195,6 +234,7 @@ void NavigationThrottleRunner::ProcessInternal() {
 
       case NavigationThrottle::DEFER:
         next_index_ = i + 1;
+        defer_start_time_ = base::Time::Now();
         return;
     }
   }
