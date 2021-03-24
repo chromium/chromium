ea2f43f89c30201a4843aebcca3f16d25f1c4900
John L Chen <zuan@chromium.org>
2021-03-24 14:49:47 +0000

    Implement FakeInstallAttributesClient
    BUG=b:181787676
    TEST=Build OK && Unit test passes
    
    Change-Id: Ia9ca82ef565d7911669349a1277ddf0a62900139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780234
    Commit-Queue: John L Chen <zuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866113}

--- chromeos/dbus/userdataauth/fake_install_attributes_client.cc
+++ chromeos/dbus/userdataauth/fake_install_attributes_client.cc
@@ -4,13 +4,39 @@
 
 #include "chromeos/dbus/userdataauth/fake_install_attributes_client.h"
 
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/location.h"
 #include "base/notreached.h"
+#include "base/path_service.h"
+#include "base/threading/thread_restrictions.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "chromeos/dbus/constants/dbus_paths.h"
+#include "chromeos/dbus/cryptohome/rpc.pb.h"
+#include "components/policy/proto/install_attributes.pb.h"
 
 namespace chromeos {
 
-FakeInstallAttributesClient::FakeInstallAttributesClient() = default;
+namespace {
+// Used to track the fake instance, mirrors the instance in the base class.
+FakeInstallAttributesClient* g_instance = nullptr;
 
-FakeInstallAttributesClient::~FakeInstallAttributesClient() = default;
+}  // namespace
+
+FakeInstallAttributesClient::FakeInstallAttributesClient() {
+  DCHECK(!g_instance);
+  g_instance = this;
+}
+
+FakeInstallAttributesClient::~FakeInstallAttributesClient() {
+  DCHECK_EQ(this, g_instance);
+  g_instance = nullptr;
+}
+
+// static
+FakeInstallAttributesClient* FakeInstallAttributesClient::Get() {
+  return g_instance;
+}
 
 void FakeInstallAttributesClient::InstallAttributesGet(
     const ::user_data_auth::InstallAttributesGetRequest& request,
@@ -35,41 +61,124 @@ void FakeInstallAttributesClient::InstallAttributesGetStatus(
 void FakeInstallAttributesClient::RemoveFirmwareManagementParameters(
     const ::user_data_auth::RemoveFirmwareManagementParametersRequest& request,
     RemoveFirmwareManagementParametersCallback callback) {
-  NOTIMPLEMENTED();
+  remove_firmware_management_parameters_from_tpm_call_count_++;
+  ReturnProtobufMethodCallback(
+      ::user_data_auth::RemoveFirmwareManagementParametersReply(),
+      std::move(callback));
 }
 void FakeInstallAttributesClient::SetFirmwareManagementParameters(
     const ::user_data_auth::SetFirmwareManagementParametersRequest& request,
     SetFirmwareManagementParametersCallback callback) {
-  NOTIMPLEMENTED();
+  ReturnProtobufMethodCallback(
+      ::user_data_auth::SetFirmwareManagementParametersReply(),
+      std::move(callback));
 }
 base::Optional<::user_data_auth::InstallAttributesGetReply>
 FakeInstallAttributesClient::BlockingInstallAttributesGet(
     const ::user_data_auth::InstallAttributesGetRequest& request) {
-  NOTIMPLEMENTED();
-  return base::nullopt;
+  ::user_data_auth::InstallAttributesGetReply reply;
+  if (install_attrs_.find(request.name()) != install_attrs_.end()) {
+    reply.set_value(install_attrs_[request.name()]);
+  } else {
+    reply.set_error(::user_data_auth::CryptohomeErrorCode::
+                        CRYPTOHOME_ERROR_INSTALL_ATTRIBUTES_GET_FAILED);
+  }
+  return reply;
 }
 base::Optional<::user_data_auth::InstallAttributesSetReply>
 FakeInstallAttributesClient::BlockingInstallAttributesSet(
     const ::user_data_auth::InstallAttributesSetRequest& request) {
-  NOTIMPLEMENTED();
-  return base::nullopt;
+  ::user_data_auth::InstallAttributesSetReply reply;
+  install_attrs_[request.name()] = request.value();
+  return reply;
 }
 base::Optional<::user_data_auth::InstallAttributesFinalizeReply>
 FakeInstallAttributesClient::BlockingInstallAttributesFinalize(
     const ::user_data_auth::InstallAttributesFinalizeRequest& request) {
-  NOTIMPLEMENTED();
-  return base::nullopt;
+  locked_ = true;
+  ::user_data_auth::InstallAttributesFinalizeReply reply;
+
+  // Persist the install attributes so that they can be reloaded if the
+  // browser is restarted. This is used for ease of development when device
+  // enrollment is required.
+  base::FilePath cache_path;
+  if (!base::PathService::Get(dbus_paths::FILE_INSTALL_ATTRIBUTES,
+                              &cache_path)) {
+    reply.set_error(::user_data_auth::CryptohomeErrorCode::
+                        CRYPTOHOME_ERROR_INSTALL_ATTRIBUTES_FINALIZE_FAILED);
+    return reply;
+  }
+
+  cryptohome::SerializedInstallAttributes install_attrs_proto;
+  for (const auto& it : install_attrs_) {
+    const std::string& name = it.first;
+    const std::string& value = it.second;
+    cryptohome::SerializedInstallAttributes::Attribute* attr_entry =
+        install_attrs_proto.add_attributes();
+    attr_entry->set_name(name);
+    attr_entry->set_value(value);
+  }
+
+  std::string result;
+  install_attrs_proto.SerializeToString(&result);
+
+  // The real implementation does a blocking wait on the dbus call; the fake
+  // implementation must have this file written before returning.
+  base::ScopedAllowBlockingForTesting allow_io;
+  base::WriteFile(cache_path, result.data(), result.size());
+
+  return reply;
 }
 base::Optional<::user_data_auth::InstallAttributesGetStatusReply>
 FakeInstallAttributesClient::BlockingInstallAttributesGetStatus(
     const ::user_data_auth::InstallAttributesGetStatusRequest& request) {
-  NOTIMPLEMENTED();
-  return base::nullopt;
+  ::user_data_auth::InstallAttributesGetStatusReply reply;
+  if (locked_) {
+    reply.set_state(user_data_auth::InstallAttributesState::VALID);
+  } else {
+    reply.set_state(user_data_auth::InstallAttributesState::FIRST_INSTALL);
+  }
+  return reply;
 }
 
 void FakeInstallAttributesClient::WaitForServiceToBeAvailable(
     chromeos::WaitForServiceToBeAvailableCallback callback) {
-  NOTIMPLEMENTED();
+  if (service_is_available_ || service_reported_not_available_) {
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(std::move(callback), service_is_available_));
+  } else {
+    pending_wait_for_service_to_be_available_callbacks_.push_back(
+        std::move(callback));
+  }
+}
+
+void FakeInstallAttributesClient::SetServiceIsAvailable(bool is_available) {
+  service_is_available_ = is_available;
+  if (!is_available)
+    return;
+
+  std::vector<WaitForServiceToBeAvailableCallback> callbacks;
+  callbacks.swap(pending_wait_for_service_to_be_available_callbacks_);
+  for (auto& callback : callbacks)
+    std::move(callback).Run(true);
+}
+
+void FakeInstallAttributesClient::ReportServiceIsNotAvailable() {
+  DCHECK(!service_is_available_);
+  service_reported_not_available_ = true;
+
+  std::vector<WaitForServiceToBeAvailableCallback> callbacks;
+  callbacks.swap(pending_wait_for_service_to_be_available_callbacks_);
+  for (auto& callback : callbacks)
+    std::move(callback).Run(false);
+}
+
+template <typename ReplyType>
+void FakeInstallAttributesClient::ReturnProtobufMethodCallback(
+    const ReplyType& reply,
+    DBusMethodCallback<ReplyType> callback) {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), reply));
 }
 
 }  // namespace chromeos
--- chromeos/dbus/userdataauth/fake_install_attributes_client.h
+++ chromeos/dbus/userdataauth/fake_install_attributes_client.h
@@ -23,6 +23,10 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeInstallAttributesClient
   FakeInstallAttributesClient& operator=(const FakeInstallAttributesClient&) =
       delete;
 
+  // Checks that a FakeInstallAttributesClient instance was initialized and
+  // returns it.
+  static FakeInstallAttributesClient* Get();
+
   // InstallAttributesClient override:
   void WaitForServiceToBeAvailable(
       chromeos::WaitForServiceToBeAvailableCallback callback) override;
@@ -59,6 +63,57 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) FakeInstallAttributesClient
   BlockingInstallAttributesGetStatus(
       const ::user_data_auth::InstallAttributesGetStatusRequest& request)
       override;
+
+  // FWMP related:
+
+  // Return the number of times RemoveFirmwareManagementParameters is called.
+  int remove_firmware_management_parameters_from_tpm_call_count() const {
+    return remove_firmware_management_parameters_from_tpm_call_count_;
+  }
+
+  // WaitForServiceToBeAvailable() related:
+
+  // Changes the behavior of WaitForServiceToBeAvailable(). This method runs
+  // pending callbacks if is_available is true.
+  void SetServiceIsAvailable(bool is_available);
+
+  // Runs pending availability callbacks reporting that the service is
+  // unavailable. Expects service not to be available when called.
+  void ReportServiceIsNotAvailable();
+
+ private:
+  // Helper that returns the protobuf reply.
+  template <typename ReplyType>
+  void ReturnProtobufMethodCallback(const ReplyType& reply,
+                                    DBusMethodCallback<ReplyType> callback);
+
+  // FWMP related:
+
+  // Number of times RemoveFirmwareManagementParameters() is called.
+  int remove_firmware_management_parameters_from_tpm_call_count_ = 0;
+
+  // Install attributes related:
+
+  // A stub store for InstallAttributes, mapping an attribute name to the
+  // associated data blob. Used to implement InstallAttributesSet and -Get.
+  std::map<std::string, std::string> install_attrs_;
+
+  // Set to true if install attributes are finalized.
+  bool locked_;
+
+  // WaitForServiceToBeAvailable() related fields:
+
+  // If set, we tell callers that service is available.
+  bool service_is_available_ = true;
+
+  // If set, WaitForServiceToBeAvailable will run the callback, even if service
+  // is not available (instead of adding the callback to pending callback list).
+  bool service_reported_not_available_ = false;
+
+  // The list of callbacks passed to WaitForServiceToBeAvailable when the
+  // service wasn't available.
+  std::vector<WaitForServiceToBeAvailableCallback>
+      pending_wait_for_service_to_be_available_callbacks_;
 };
 
 }  // namespace chromeos
