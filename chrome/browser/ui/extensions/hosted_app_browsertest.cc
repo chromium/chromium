168e4cb0564578036a3eaf4290b64fe928ed6264
Stephen Chenney <schenney@chromium.org>
2021-03-24 17:02:39 +0000

    [Paint Holding] Address flaky All/HostedOrWebAppTest.CtrlClickLink/HostedApp
    Allow pre-commit-input.
    
    Also revert the previous speculative fix.
    
    Bug: 1191635
    Change-Id: I1562644c1164a7dccc1b3308eaa7a92ff65f620e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782725
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866165}

--- chrome/browser/ui/extensions/hosted_app_browsertest.cc
+++ chrome/browser/ui/extensions/hosted_app_browsertest.cc
@@ -85,6 +85,7 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
+#include "third_party/blink/public/common/switches.h"
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 
 using content::RenderFrameHost;
@@ -260,6 +261,9 @@ class HostedOrWebAppTest : public extensions::ExtensionBrowserTest,
     // Browser will both run and display insecure content.
     command_line->AppendSwitch(switches::kAllowRunningInsecureContent);
     cert_verifier_.SetUpCommandLine(command_line);
+    // Some builders are flaky due to slower loading interacting
+    // with deferred commits.
+    command_line->AppendSwitch(blink::switches::kAllowPreCommitInput);
   }
 
   void SetUpOnMainThread() override {
