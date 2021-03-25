771bdeaca841404a6bd0399f3ef642087c1ecbd7
Henrique Nakashima <hnakashima@chromium.org>
2021-03-25 21:15:15 +0000

    [Android] Remove reference from TabDelegate to ChromeActivity
    Use TabWindowManager to get a Tab from an id, and Tab#getContext() to
    get its activity instead of looking at TabModelSelectors of all existing
    ChromeActivities.
    
    Bug: 1155796
    Change-Id: I81f3492094e85f594b0ba8e5413f055c3b4d83cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785362
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866779}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.tabmodel.document;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Browser;
@@ -13,13 +14,12 @@ import android.provider.Browser;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.ServiceTabLauncher;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.app.tabmodel.AsyncTabParamsManagerSingleton;
+import org.chromium.chrome.browser.app.tabmodel.TabWindowManagerSingleton;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.multiwindow.MultiInstanceManager;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
@@ -55,13 +55,11 @@ public class TabDelegate extends AsyncTabCreator {
     private static Activity getActivityForTabId(int id) {
         if (id == Tab.INVALID_TAB_ID) return null;
 
-        for (Activity runningActivity : ApplicationStatus.getRunningActivities()) {
-            if (!(runningActivity instanceof ChromeActivity)) continue;
+        Tab tab = TabWindowManagerSingleton.getInstance().getTabById(id);
+        if (tab == null) return null;
 
-            ChromeActivity activity = (ChromeActivity) runningActivity;
-            if (activity.getTabModelSelector().getTabById(id) != null) return activity;
-        }
-        return null;
+        Context tabContext = tab.getContext();
+        return (tabContext instanceof Activity) ? (Activity) tabContext : null;
     }
 
     @Override
