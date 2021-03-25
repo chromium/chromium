83ed2a8a66b70e7d44d8294e983d40e87101f52a
rbpotter <rbpotter@chromium.org>
2021-03-25 19:43:41 +0000

    WebUI: Remove unnecessary non-module files except on ChromeOS
    The non-JS module versions of these files are no longer used on other
    platforms. Remove them from the build.
    
    Bug: 1173575
    Change-Id: I990931a7298423dce8f7ac30b2b04daeea927603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785976
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866746}

--- chrome/test/data/webui/webui_resource_browsertest.cc
+++ chrome/test/data/webui/webui_resource_browsertest.cc
@@ -48,15 +48,15 @@ class WebUIResourceBrowserTest : public InProcessBrowserTest {
   }
 };
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ArrayDataModelTest) {
   LoadTestUrl("js/cr/ui/array_data_model_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ArrayDataModelModuleTest) {
   LoadTestUrl("?module=js/cr/ui/array_data_model_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, CrTest) {
   LoadTestUrl("cr_test.html");
@@ -78,15 +78,17 @@ IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, I18nProcessCssTest) {
   LoadTestUrl("i18n_process_css_test.html");
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListTest) {
   LoadTestUrl("js/cr/ui/list_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListModuleTest) {
   LoadTestUrl("?module=js/cr/ui/list_test.m.js");
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, GridTest) {
   LoadTestUrl("js/cr/ui/grid_test.html");
 }
@@ -94,38 +96,34 @@ IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, GridTest) {
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, GridModuleTest) {
   LoadTestUrl("?module=js/cr/ui/grid_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListSelectionModelTest) {
   LoadTestUrl("js/cr/ui/list_selection_model_test.html");
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListSelectionModeModulelTest) {
+// This test is Chrome OS only as the utils file it imports relies on
+// list_single_selection_model, which is only included on Chrome OS.
+IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListSelectionModelModuleTest) {
   LoadTestUrl("?module=js/cr/ui/list_selection_model_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListSingleSelectionModelTest) {
   LoadTestUrl("js/cr/ui/list_single_selection_model_test.html");
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest,
                        ListSingleSelectionModelModuleTest) {
   LoadTestUrl("?module=js/cr/ui/list_single_selection_model_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, MenuTest) {
   LoadTestUrl("js/cr/ui/menu_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, MenuModuleTest) {
   LoadTestUrl("?module=js/cr/ui/menu_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, MockTimerTest) {
   LoadTestUrl("mock_timer_test.html");
@@ -135,55 +133,55 @@ IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ParseHtmlSubsetTest) {
   LoadTestUrl("parse_html_subset_test.html");
 }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, PositionUtilTest) {
   LoadTestUrl("js/cr/ui/position_util_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, PositionUtilModuleTest) {
   LoadTestUrl("?module=js/cr/ui/position_util_test.m.js");
 }
-#endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, CommandTest) {
   LoadTestUrl("js/cr/ui/command_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, CommandModuleTest) {
   LoadTestUrl("?module=js/cr/ui/command_test.m.js");
 }
-#endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ContextMenuHandlerTest) {
   LoadTestUrl("js/cr/ui/context_menu_handler_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ContextMenuHandlerModuleTest) {
   LoadTestUrl("?module=js/cr/ui/context_menu_handler_test.m.js");
 }
-#endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, MenuButtonTest) {
   LoadTestUrl("js/cr/ui/menu_button_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, MenuButtonModuleTest) {
   LoadTestUrl("?module=js/cr/ui/menu_button_test.m.js");
 }
-#endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, SplitterTest) {
   LoadTestUrl("js/cr/ui/splitter_test.html");
 }
+#endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, SplitterModuleTest) {
   LoadTestUrl("?module=js/cr/ui/splitter_test.m.js");
 }
-#endif
 
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, UtilTest) {
   LoadTestUrl("util_test.html");
