9da6a64b50c7d9fcfb6d7cdcab61bcbece502279
John L Chen <zuan@chromium.org>
2021-03-29 17:07:06 +0000

    Migrate UserAffiliationBrowserTest to UserDataAuth
    BUG=b:183409667
    TEST=Build OK && Unit test passes
    
    Change-Id: I8872460c00067d244518ab83389d056365b1fe42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787031
    Commit-Queue: John L Chen <zuan@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867249}

--- chrome/browser/chromeos/policy/user_affiliation_browsertest.cc
+++ chrome/browser/chromeos/policy/user_affiliation_browsertest.cc
@@ -21,9 +21,9 @@
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chromeos/cryptohome/cryptohome_parameters.h"
 #include "chromeos/dbus/authpolicy/fake_authpolicy_client.h"
-#include "chromeos/dbus/cryptohome/cryptohome_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/upstart/upstart_client.h"
+#include "chromeos/dbus/userdataauth/userdataauth_client.h"
 #include "chromeos/tpm/install_attributes.h"
 #include "components/account_id/account_id.h"
 #include "components/policy/core/common/cloud/device_management_service.h"
@@ -134,7 +134,8 @@ class UserAffiliationBrowserTest
                                       cryptohome_id.account_id());
       command_line->AppendSwitchASCII(
           chromeos::switches::kLoginProfile,
-          chromeos::CryptohomeClient::GetStubSanitizedUsername(cryptohome_id));
+          chromeos::UserDataAuthClient::GetStubSanitizedUsername(
+              cryptohome_id));
     }
   }
 
