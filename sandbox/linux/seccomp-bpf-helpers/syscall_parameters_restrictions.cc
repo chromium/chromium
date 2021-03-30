6e749e6521fc796d864c6d26190b245aa7c002a5
Peter Collingbourne <pcc@chromium.org>
2021-03-30 02:29:38 +0000

    Allow prctl(PR_PAC_RESET_KEYS) in the sandbox.
    https://r.android.com/1652994 proposes to change Bionic to call
    prctl(PR_PAC_RESET_KEYS) on each new thread. Since this syscall is
    not allowed by the Chromium sandbox, we would otherwise crash when
    creating a new thread. To work around the issue in current versions
    of Chromium, we avoid issuing the syscall if the target SDK level is
    less than S. This CL adds the prctl to the allowlist so that we will
    be able to upgrade our target SDK level.
    
    Bug: 919548
    Change-Id: If418fcfb827695250336f2889589aaaa62617cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787704
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867426}

--- sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
+++ sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
@@ -159,6 +159,10 @@ ResultExpr RestrictCloneToThreadsAndEPERMFork() {
       .Else(CrashSIGSYSClone());
 }
 
+#ifndef PR_PAC_RESET_KEYS
+#define PR_PAC_RESET_KEYS 54
+#endif
+
 ResultExpr RestrictPrctl() {
   // Will need to add seccomp compositing in the future. PR_SET_PTRACER is
   // used by breakpad but not needed anymore.
@@ -167,7 +171,7 @@ ResultExpr RestrictPrctl() {
       .CASES((PR_GET_NAME, PR_SET_NAME, PR_GET_DUMPABLE, PR_SET_DUMPABLE
 #if defined(OS_ANDROID)
               , PR_SET_VMA, PR_SET_PTRACER, PR_SET_TIMERSLACK
-              , PR_GET_NO_NEW_PRIVS
+              , PR_GET_NO_NEW_PRIVS, PR_PAC_RESET_KEYS
 
 // Enable PR_SET_TIMERSLACK_PID, an Android custom prctl which is used in:
 // https://android.googlesource.com/platform/system/core/+/lollipop-release/libcutils/sched_policy.c.
