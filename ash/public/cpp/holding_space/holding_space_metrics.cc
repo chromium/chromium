be9a19dc9b2a300d7bb2ab7fbca62dae943315a9
David Black <dmblack@google.com>
2021-03-30 01:57:20 +0000

    Add histograms to track holding space item actions by extension type.
    This gives further insight into the types of files being acted upon by
    holding space users.
    
    Note that this CL upgrades holding space histograms to use variants as
    histogram suffixes are now deprecated.
    
    Output of
      ./tools/metrics/histograms/print_histogram_names.py \
      --diff origin/master
    
      7 histograms added:
      HoldingSpace.Item.Action.Copy.Extension
      HoldingSpace.Item.Action.Drag.Extension
      HoldingSpace.Item.Action.Launch.Extension
      HoldingSpace.Item.Action.Pin.Extension
      HoldingSpace.Item.Action.Remove.Extension
      HoldingSpace.Item.Action.ShowInFolder.Extension
      HoldingSpace.Item.Action.Unpin.Extension
      2 histograms removed:
      HoldingSpace.Item.Action
      HoldingSpace.Item.Count
      0 histograms obsoleted:
    
    Note that the 2 removed histograms were base histograms generated by
    histogram_suffixes and were not used.
    
    Bug: 1191149
    Change-Id: I0e2bf4a537165c4598a047097c3fe7d9d42c17df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782417
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867409}

--- ash/public/cpp/holding_space/holding_space_metrics.cc
+++ ash/public/cpp/holding_space/holding_space_metrics.cc
@@ -9,6 +9,7 @@
 
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/strings/string_util.h"
 #include "base/time/time.h"
 
 namespace ash {
@@ -17,6 +18,45 @@ namespace holding_space_metrics {
 namespace {
 
 // Helpers ---------------------------------------------------------------------
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused. Note that 2, 3, and 4 are reserved in
+// case additional special values need to be added in the future.
+constexpr int kEmptyExtension = 0;
+constexpr int kOtherExtension = 1;
+constexpr int kFirstKnownExtension = 5;
+constexpr std::array<const char*, 72> kKnownExtensions = {
+    ".3ga",     ".3gp",  ".aac",        ".alac", ".arw",   ".asf",  ".avi",
+    ".bmp",     ".cr2",  ".crdownload", ".crx",  ".csv",   ".dmg",  ".dng",
+    ".doc",     ".docx", ".exe",        ".flac", ".gdoc",  ".gif",  ".gsheet",
+    ".gslides", ".htm",  ".html",       ".jar",  ".jpeg",  ".jpg",  ".log",
+    ".m3u",     ".m3u8", ".m4a",        ".m4v",  ".mhtml", ".mid",  ".mkv",
+    ".mov",     ".mp3",  ".mp4",        ".mpg",  ".nef",   ".nrw",  ".odf",
+    ".odp",     ".ods",  ".odt",        ".oga",  ".ogg",   ".ogv",  ".orf",
+    ".pdf",     ".png",  ".ppt",        ".pptx", ".ps",    ".ra",   ".raf",
+    ".ram",     ".rar",  ".rm",         ".rtf",  ".rw2",   ".tini", ".torrent",
+    ".txt",     ".wav",  ".webm",       ".webp", ".wma",   ".wmv",  ".xls",
+    ".xlsx",    ".zip",
+};
+
+// Returns the integer representation of the extension for the specified
+// `file_path`. Note that these values are persisted to histograms so should
+// remain unchanged.
+int FilePathToExtension(const base::FilePath& file_path) {
+  if (file_path.empty())
+    return kEmptyExtension;
+
+  const std::string extension = base::ToLowerASCII(file_path.Extension());
+  if (extension.empty())
+    return kEmptyExtension;
+
+  auto* const* it =
+      std::find(kKnownExtensions.begin(), kKnownExtensions.end(), extension);
+  if (it == kKnownExtensions.end())
+    return kOtherExtension;
+
+  return kFirstKnownExtension + std::distance(kKnownExtensions.begin(), it);
+}
 
 // Returns the string representation of the specified `action`. Note that these
 // values are persisted to histograms so should remain unchanged.
@@ -78,10 +118,16 @@ void RecordFilesAppChipAction(FilesAppChipAction action) {
 
 void RecordItemAction(const std::vector<const HoldingSpaceItem*>& items,
                       ItemAction action) {
+  const std::string action_string = ItemActionToString(action);
+  const int extensions_size = kFirstKnownExtension + kKnownExtensions.size();
+
   for (const HoldingSpaceItem* item : items) {
     base::UmaHistogramEnumeration("HoldingSpace.Item.Action.All", action);
-    base::UmaHistogramEnumeration(
-        "HoldingSpace.Item.Action." + ItemActionToString(action), item->type());
+    base::UmaHistogramEnumeration("HoldingSpace.Item.Action." + action_string,
+                                  item->type());
+    base::UmaHistogramExactLinear(
+        "HoldingSpace.Item.Action." + action_string + ".Extension",
+        FilePathToExtension(item->file_path()), extensions_size);
   }
 }
 
