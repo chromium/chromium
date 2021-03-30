ca51b6489eca6fdd2ceb0fc34e0cbda9b13ab2c1
Matt Falkenhagen <falken@chromium.org>
2021-03-30 15:25:26 +0000

    [M90] loader: Avoid crashing in WebResourceRequestSender::SendAsync.
    Adds a temporary early return until the root cause is discovered.
    
    (cherry picked from commit 8b615b447d070592bbdad64d75f6d398433f53fc)
    
    Bug: 1175286
    Change-Id: I342c423fe535fd4fc96f35026eeb55f7bac67e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789089
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Minggang Wang <minggang.wang@intel.com>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#867126}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793162
    Cr-Commit-Position: refs/branch-heads/4430@{#883}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/web_resource_request_sender.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/web_resource_request_sender.cc
@@ -257,6 +257,8 @@ int WebResourceRequestSender::SendAsync(
     std::unique_ptr<ResourceLoadInfoNotifierWrapper>
         resource_load_info_notifier_wrapper,
     WebBackForwardCacheLoaderHelper back_forward_cache_loader_helper) {
+  auto weak_this = weak_factory_.GetWeakPtr();
+
   CheckSchemeForReferrerPolicy(*request);
 
 #if defined(OS_ANDROID)
@@ -296,6 +298,11 @@ int WebResourceRequestSender::SendAsync(
           request_id, loader_options, request.get(), client.get(),
           traffic_annotation, std::move(loading_task_runner),
           base::make_optional(std_cors_exempt_header_list));
+  // TODO(https://crbug.com/1175286): Remove this mitigation when we understand
+  // why `this` or `request_info_` is being destroyed.
+  if (!weak_this || !request_info_)
+    return request_id;
+
   request_info_->url_loader = std::move(url_loader);
   request_info_->url_loader_client = std::move(client);
 
