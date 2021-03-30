836eee8dcca11631a275d3c8689ab33bdfc81944
Andres Calderon Jaramillo <andrescj@chromium.org>
2021-03-30 20:23:07 +0000

    viz: Tests for color space considerations in overlay processor.
    This CL is a follow-up to CL:2739035 to add unit tests for the new
    behavior added by that CL.
    
    Bug: b:181366246
    Test: None
    Change-Id: Ia54cb1fa514cf7deb89a17ca6019188ad9631d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774959
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867761}

--- components/viz/service/display/overlay_processor_ozone_unittest.cc
+++ components/viz/service/display/overlay_processor_ozone_unittest.cc
@@ -23,12 +23,15 @@ class FakeOverlayCandidatesOzone : public ui::OverlayCandidatesOzone {
  public:
   ~FakeOverlayCandidatesOzone() override = default;
 
-  // Mark every overlay candidate as handled since we don't really care about
-  // OverlayCandidatesOzone internals in this test suite.
+  // We don't really care about OverlayCandidatesOzone internals, but we do need
+  // to detect if the OverlayProcessor skipped a candidate. In that case,
+  // ui::OverlaySurfaceCandidate would be default constructed (except for the Z
+  // order). Therefore, we use the buffer size of the candidate to decide
+  // whether to mark the candidate as handled.
   void CheckOverlaySupport(
       std::vector<ui::OverlaySurfaceCandidate>* candidates) override {
     for (auto& candidate : *candidates) {
-      candidate.overlay_handled = true;
+      candidate.overlay_handled = !candidate.buffer_size.IsEmpty();
     }
   }
 };
@@ -151,6 +154,82 @@ TEST(OverlayProcessorOzoneTest, PrimaryPlaneFormatMismatch) {
   // Since the |OutputSurfaceOverlayPlane|'s format doesn't match that of the
   // primary plane's NativePixmap, the overlay candidate is NOT promoted.
   EXPECT_FALSE(candidates.at(0).overlay_handled);
+}
+
+TEST(OverlayProcessorOzoneTest, ColorSpaceMismatch) {
+  // Set up the primary plane.
+  gfx::Size size(128, 128);
+  OverlayProcessorInterface::OutputSurfaceOverlayPlane primary_plane;
+  primary_plane.resource_size = size;
+  primary_plane.format = gfx::BufferFormat::BGRA_8888;
+  primary_plane.mailbox = gpu::Mailbox::GenerateForSharedImage();
+
+  // Set up a dummy OverlayCandidate.
+  OverlayCandidate candidate;
+  candidate.resource_size_in_pixels = size;
+  candidate.format = gfx::BufferFormat::BGRA_8888;
+  candidate.mailbox = gpu::Mailbox::GenerateForSharedImage();
+  candidate.overlay_handled = false;
+  OverlayCandidateList candidates;
+  candidates.push_back(candidate);
+
+  // Initialize a MockSharedImageInterface that returns a NativePixmap with
+  // matching params to the primary plane.
+  std::unique_ptr<MockSharedImageInterface> sii =
+      std::make_unique<::testing::NiceMock<MockSharedImageInterface>>();
+  scoped_refptr<gfx::NativePixmap> primary_plane_pixmap =
+      base::MakeRefCounted<FakeNativePixmap>(size,
+                                             gfx::BufferFormat::BGRA_8888);
+  scoped_refptr<gfx::NativePixmap> candidate_pixmap =
+      base::MakeRefCounted<FakeNativePixmap>(size,
+                                             gfx::BufferFormat::BGRA_8888);
+  ON_CALL(*sii, GetNativePixmap(primary_plane.mailbox))
+      .WillByDefault(Return(primary_plane_pixmap));
+  ON_CALL(*sii, GetNativePixmap(candidate.mailbox))
+      .WillByDefault(Return(candidate_pixmap));
+  OverlayProcessorOzone processor(
+      std::make_unique<FakeOverlayCandidatesOzone>(), {}, sii.get());
+
+  // In Chrome OS, we don't allow the promotion of the candidate if the
+  // ContentColorUsage is different from the primary plane (e.g., SDR vs. HDR).
+  // In other platforms, this is not a restriction.
+  primary_plane.color_space = gfx::ColorSpace::CreateSRGB();
+  candidates[0].color_space = gfx::ColorSpace::CreateHDR10();
+  processor.CheckOverlaySupport(&primary_plane, &candidates);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  EXPECT_FALSE(candidates.at(0).overlay_handled);
+#else
+  EXPECT_TRUE(candidates.at(0).overlay_handled);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+  candidates[0] = candidate;
+
+  // We do allow color space mismatches as long as the ContentColorUsage is the
+  // same as the primary plane's (and this applies to all platforms).
+  primary_plane.color_space = gfx::ColorSpace::CreateHDR10();
+  candidates[0].color_space = gfx::ColorSpace::CreateHLG();
+  processor.CheckOverlaySupport(&primary_plane, &candidates);
+  EXPECT_TRUE(candidates.at(0).overlay_handled);
+
+  candidates[0] = candidate;
+
+  // Also, if the candidate requires an overlay, then it should be promoted
+  // regardless of the color space mismatch.
+  primary_plane.color_space = gfx::ColorSpace::CreateSRGB();
+  candidates[0].color_space = gfx::ColorSpace::CreateHDR10();
+  candidates[0].requires_overlay = true;
+  processor.CheckOverlaySupport(&primary_plane, &candidates);
+  EXPECT_TRUE(candidates.at(0).overlay_handled);
+
+  candidates[0] = candidate;
+
+  // And finally, if the candidate's color space is invalid, then it also should
+  // be promoted.
+  primary_plane.color_space = gfx::ColorSpace::CreateHDR10();
+  candidates[0].color_space = gfx::ColorSpace();
+  EXPECT_FALSE(candidates[0].color_space.IsValid());
+  processor.CheckOverlaySupport(&primary_plane, &candidates);
+  EXPECT_TRUE(candidates.at(0).overlay_handled);
 }
 #endif
 
