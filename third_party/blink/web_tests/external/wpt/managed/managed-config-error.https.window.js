d2a6ec2a0dbe27c7d27a168cc78ce51abc493ac8
Anatoliy Potapchuk <apotapchuk@chromium.org>
2021-03-31 00:17:15 +0000

    Add WPT tests for Managed Configuration API
    Bug: 1141849
    Change-Id: Idb685d8fc1e7dcf77f8dd7bf5be30c82ab60876c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792424
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867885}

---
+++ third_party/blink/web_tests/external/wpt/managed/managed-config-error.https.window.js
@@ -0,0 +1,17 @@
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+
+promise_test(t => {
+  return promise_rejects_js(
+      t, TypeError, navigator.managed.getManagedConfiguration(-1));
+}, 'Number instead of keys');
+
+promise_test(t => {
+  return promise_rejects_js(
+      t, TypeError, navigator.managed.getManagedConfiguration());
+}, 'Empty key list');
+
+promise_test(t => {
+  return promise_rejects_js(
+      t, TypeError, navigator.managed.getManagedConfiguration({'a': 2}));
+}, 'Dictionary instead of list');
