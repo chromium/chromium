c0276846f7726a37e6abada4dff2f302c31e151f
Joon Ahn <joonbug@chromium.org>
2021-03-31 00:08:59 +0000

    diagnostics: update p-links to point to exact section
    text link should also appear for memory routine
    
    (cherry picked from commit 554e44b29179549f1cf165991d15903df7346ef5)
    
    Bug: 1191620
    Change-Id: I7e3b7c827a65e309afb9e4ca7cc66d1a27ec6797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786212
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#867687}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795753
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4430@{#925}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- chromeos/components/diagnostics_ui/resources/routine_section.js
+++ chromeos/components/diagnostics_ui/resources/routine_section.js
@@ -125,6 +125,12 @@ Polymer({
       value: '',
     },
 
+    /** @type {string} */
+    learnMoreLinkSection: {
+      type: String,
+      value: '',
+    },
+
     /** @private {!BadgeType} */
     badgeType_: {
       type: String,
@@ -244,9 +250,13 @@ Polymer({
     this.$.collapse.toggle();
   },
 
-  /** @private */
+  /** @protected */
   onLearnMoreClicked_() {
-    window.open('https://support.google.com/chromebook?p=diagnostics_app');
+    const baseSupportUrl =
+        'https://support.google.com/chromebook?p=diagnostics_';
+    assert(this.learnMoreLinkSection);
+
+    window.open(baseSupportUrl + this.learnMoreLinkSection);
   },
 
   /** @protected */
@@ -257,7 +267,7 @@ Polymer({
 
   /** @protected */
   isLearnMoreHidden_() {
-    return this.isPowerRoutine === false || this.isLoggedIn_ === false ||
+    return !this.shouldHideReportList_() || !this.isLoggedIn_ ||
         this.executionStatus_ !== ExecutionProgress.kCompleted;
   },
 
