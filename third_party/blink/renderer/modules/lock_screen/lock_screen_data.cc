66456f269cc1d54b26f62c7ce76b720662d56f4c
Louise Brett <loubrett@google.com>
2021-03-31 07:44:52 +0000

    Add experimental stub idl for lock screen API.
    Discourse post:
    https://discourse.wicg.io/t/proposal-lock-screen-api/4372/2
    Explainer:
    https://github.com/WICG/lock-screen/blob/main/README.md
    Chrome Status:
    https://www.chromestatus.com/feature/6575595884380160
    Intent to prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/006tkVwj1yw
    
    Note: This is behind a runtime enabled feature flag, and so, does not
    expose any new APIs to the web. This is a very early prototype, and
    will likely not ship in its current form.
    
    Bug: 1194371
    
    Change-Id: Ia25fbbd677e2c6afe474f28e724f3d997e84f44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717433
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867997}

---
+++ third_party/blink/renderer/modules/lock_screen/lock_screen_data.cc
@@ -0,0 +1,80 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/lock_screen/lock_screen_data.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+
+namespace blink {
+
+const char LockScreenData::kSupplementName[] = "LockScreenData";
+
+LockScreenData::LockScreenData(LocalDOMWindow& window)
+    : Supplement<LocalDOMWindow>(window) {}
+
+LockScreenData::~LockScreenData() = default;
+
+ScriptPromise LockScreenData::getLockScreenData(ScriptState* script_state,
+                                                LocalDOMWindow& window) {
+  LockScreenData* supplement =
+      Supplement<LocalDOMWindow>::From<LockScreenData>(window);
+  if (!supplement) {
+    supplement = MakeGarbageCollected<LockScreenData>(window);
+    ProvideTo(window, supplement);
+  }
+  return supplement->GetLockScreenData(script_state);
+}
+
+ScriptPromise LockScreenData::GetLockScreenData(ScriptState* script_state) {
+  ScriptPromiseResolver* resolver =
+      MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  auto promise = resolver->Promise();
+
+  resolver->Resolve(this);
+  return promise;
+}
+
+ScriptPromise LockScreenData::getKeys(ScriptState* script_state) {
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+
+  // TODO(crbug.com/1006642): This should call out to a mojo service.
+
+  return resolver->Promise();
+}
+
+ScriptPromise LockScreenData::getData(ScriptState* script_state,
+                                      const String& key) {
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+
+  // TODO(crbug.com/1006642): This should call out to a mojo service.
+
+  return resolver->Promise();
+}
+
+ScriptPromise LockScreenData::setData(ScriptState* script_state,
+                                      const String& key,
+                                      const String& data) {
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+
+  // TODO(crbug.com/1006642): This should call out to a mojo service.
+
+  return resolver->Promise();
+}
+
+ScriptPromise LockScreenData::deleteData(ScriptState* script_state,
+                                         const String& key) {
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+
+  // TODO(crbug.com/1006642): This should call out to a mojo service.
+
+  return resolver->Promise();
+}
+
+void LockScreenData::Trace(Visitor* visitor) const {
+  ScriptWrappable::Trace(visitor);
+  Supplement<LocalDOMWindow>::Trace(visitor);
+}
+
+}  // namespace blink
