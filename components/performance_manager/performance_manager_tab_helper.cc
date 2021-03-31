2ca79fed006c557285f1638573ec32323a665b54
Sreeja Kamishetty <sreejakshetty@chromium.org>
2021-03-31 18:35:25 +0000

    Cleanup PerformanceManagerTabHelper::BindDocumentCoordinationUnit
    Bug: 987445
    Change-Id: I1815840ef1bd47e586a23239d33bfb72922039fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2796648
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868192}

--- components/performance_manager/performance_manager_tab_helper.cc
+++ components/performance_manager/performance_manager_tab_helper.cc
@@ -458,24 +458,8 @@ void PerformanceManagerTabHelper::DidUpdateFaviconURL(
 void PerformanceManagerTabHelper::BindDocumentCoordinationUnit(
     content::RenderFrameHost* render_frame_host,
     mojo::PendingReceiver<mojom::DocumentCoordinationUnit> receiver) {
-  // TODO(https://crbug.com/987445): Why else than due to speculative render
-  //     frame hosts would this happen? Is there a race between the RFH creation
-  //     notification and the mojo interface request?
   auto it = frames_.find(render_frame_host);
-  if (it == frames_.end()) {
-    if (render_frame_host->IsRenderFrameCreated()) {
-      // This must be a speculative render frame host, generate a creation event
-      // for it a this point
-      RenderFrameCreated(render_frame_host);
-
-      it = frames_.find(render_frame_host);
-      DCHECK(it != frames_.end());
-    } else {
-      // It would be nice to know what's up here, maybe there's a race between
-      // in-progress interface requests and the frame deletion?
-      return;
-    }
-  }
+  DCHECK(it != frames_.end());
 
   PerformanceManagerImpl::CallOnGraphImpl(
       FROM_HERE,
