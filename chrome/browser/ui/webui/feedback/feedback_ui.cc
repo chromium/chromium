dcfa6a5cdeafdd51d2020dbd893b3907cb639d21
Xiangdong Kong <Xiangdongkong@google.com>
2021-03-31 00:03:29 +0000

    [feedback] fixed the sysInfo string for arc play store enabled case
    This is to match the following logic:
    chrome/browser/extensions/api/feedback_private/
    chrome_feedback_private_delegate.cc;l=49-59
    
    Bug: 1167223
    Change-Id: I0cca3baade31f1127d1a25b3cacdc7b27795e5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794733
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/master@{#867879}

--- chrome/browser/ui/webui/feedback/feedback_ui.cc
+++ chrome/browser/ui/webui/feedback/feedback_ui.cc
@@ -13,7 +13,12 @@
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-void AddStringResources(content::WebUIDataSource* source) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/arc/arc_util.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+void AddStringResources(content::WebUIDataSource* source,
+                        const Profile* profile) {
   static constexpr webui::LocalizedString kStrings[] = {
     {"additionalInfo", IDS_FEEDBACK_ADDITIONAL_INFO_LABEL},
     {"anonymousUser", IDS_FEEDBACK_ANONYMOUS_EMAIL_OPTION},
@@ -37,11 +42,6 @@ void AddStringResources(content::WebUIDataSource* source) {
     {"screenshot", IDS_FEEDBACK_SCREENSHOT_LABEL},
     {"screenshotA11y", IDS_FEEDBACK_SCREENSHOT_A11Y_TEXT},
     {"sendReport", IDS_FEEDBACK_SEND_REPORT},
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-    {"sysInfo", IDS_FEEDBACK_INCLUDE_SYSTEM_INFORMATION_AND_METRICS_CHKBOX},
-#else
-    {"sysInfo", IDS_FEEDBACK_INCLUDE_SYSTEM_INFORMATION_CHKBOX},
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
     {"sysinfoPageCollapseAllBtn", IDS_ABOUT_SYS_COLLAPSE_ALL},
     {"sysinfoPageCollapseBtn", IDS_ABOUT_SYS_COLLAPSE},
     {"sysinfoPageDescription", IDS_ABOUT_SYS_DESC},
@@ -54,9 +54,19 @@ void AddStringResources(content::WebUIDataSource* source) {
   };
 
   source->AddLocalizedStrings(kStrings);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  source->AddLocalizedString(
+      "sysInfo",
+      arc::IsArcPlayStoreEnabledForProfile(profile)
+          ? IDS_FEEDBACK_INCLUDE_SYSTEM_INFORMATION_AND_METRICS_CHKBOX_ARC
+          : IDS_FEEDBACK_INCLUDE_SYSTEM_INFORMATION_AND_METRICS_CHKBOX);
+#else
+  source->AddLocalizedString("sysInfo",
+                             IDS_FEEDBACK_INCLUDE_SYSTEM_INFORMATION_CHKBOX);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
-content::WebUIDataSource* CreateFeedbackHTMLSource() {
+content::WebUIDataSource* CreateFeedbackHTMLSource(const Profile* profile) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUIFeedbackHost);
   source->AddResourcePaths(
@@ -64,14 +74,14 @@ content::WebUIDataSource* CreateFeedbackHTMLSource() {
   source->AddResourcePath("", IDR_FEEDBACK_DEFAULT_HTML);
   source->UseStringsJs();
 
-  AddStringResources(source);
+  AddStringResources(source, profile);
 
   return source;
 }
 
 FeedbackUI::FeedbackUI(content::WebUI* web_ui) : WebDialogUI(web_ui) {
   Profile* profile = Profile::FromWebUI(web_ui);
-  content::WebUIDataSource::Add(profile, CreateFeedbackHTMLSource());
+  content::WebUIDataSource::Add(profile, CreateFeedbackHTMLSource(profile));
 }
 
 FeedbackUI::~FeedbackUI() = default;
