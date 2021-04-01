7f914d45fe97264965aebbf2b4547d71a6d6a4a0
Noel Gordon <noel@chromium.org>
2021-04-01 06:45:01 +0000

    Revert "Relax constraints on scroll snapping for snap-type both."
    This reverts commit 9f51ed46d53717149e7496dbd7baf0eb372c046a.
    
    Reason for revert: 
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 868394 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzlmNTFlZDQ2ZDUzNzE3MTQ5ZTc0OTZkYmQ3YmFmMGViMzcyYzA0NmEM
    
    Original change's description:
    > Relax constraints on scroll snapping for snap-type both.
    >
    > Our scroll snapping logic confined the snap position to be within
    > the visible snapport if snapping in both the x and y direction; however,
    > snaps are allowed to be outside the "snapport's search corridor" as long
    > as they are not too far outside (left to the user agent to determine
    > what constitutes "too far").  The relaxed rules are not applied if
    > snapping in one direction only, or if scroll-snap-stop is set to
    > 'always'.
    >
    > In addressing the issue, two other tests required tweaking since they
    > had no allowance for a 2D snap outside the visible snapport either
    > implicitly (nested-scrollIntoView-snaps.html) or explicitly
    > (scroll_snap_data_unittest.cc).
    >
    > Bug: 626703
    > Change-Id: I3646ccc271d88e06f3e6655009e32ff2a9e08931
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785142
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Kevin Ellis <kevers@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#868394}
    
    Bug: 626703
    Change-Id: I760ad79c204f0f3dc7d73a5fd721b1bf91a2d827
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798148
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Noel Gordon <noel@chromium.org>
    Auto-Submit: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868436}

--- cc/input/scroll_snap_data.h
+++ cc/input/scroll_snap_data.h
@@ -275,9 +275,6 @@ class CC_EXPORT SnapContainerData {
       const SnapSelectionStrategy& strategy,
       const SnapSearchResult& cross_axis_snap_result) const;
 
-  bool FindSnapPositionForMutualSnap(const SnapSelectionStrategy& strategy,
-                                     gfx::ScrollOffset* snap_position) const;
-
   // Finds the snap area associated with the target snap area element id for the
   // given axis.
   base::Optional<SnapSearchResult> GetTargetSnapAreaSearchResult(
