chromium/chromium

commit e99ac7b11ab896a742525f72d42f75dd239bb8ce

mac: Give clang build script a flag to make it build an arm binary
We're going to build separate clang packages for mac/intel and mac/arm
(see bug for reasoning).

For now, we're cross-building the mac/arm clang package on an intel
machine. This means:
- pgo data is collected on the intel machine and then used to build
  the arm binary. Patch set 8 on the review uploaded such a PGO'd binary
  to http://commondatastorage.googleapis.com/chromium-browser-clang-staging/Mac/clang-llvmorg-13-init-4720-g7bafe336-2.tgz
  while patch set 9 uploaded a binary without PGO to
  clang-llvmorg-13-init-4720-g7bafe336-3.tgz
  The PGO'd binary builds LLVM"s "FileCheck" target from scratch in
  13.2s (comparable to Xcode's clang), while the non-PGO'd binary
  needs 14.8s (I did a few runs, deviations in build time < 0.1s).
  So the PGO data helps, but maybe less than when instrumenting and
  collecting profile on arm (didn't try, but speedup here is ~12%,
  while normally PGO speedup for clang itself is twice that).
  Building with PGO takes ~4.5h on the bots, ~3h without (again, see
  those patch sets).
- we don't run tests for the final stage
- also don't test the final arm compiler in any other way
  (but http://45.33.8.238/macm1/summary.html at least runs
  LLVM's and clang's tests on it). We _do_ test clang's arm codegen
  via building chrome/android and (especially) chrome/ios though,
  and we assume arm codegen for chrome/arm builds works, so assuming
  that the stage0 clang compiler produces a working clang/arm binary
  isn't that big an assumption. And we'll have more testing here when
  we have more hardware.
- clang takes its default triple from the default build machine, so
  the clang/arm binary will target mac/intel by default (!) This is
  arguably a good thing because it means both mac/intel clang and
  mac/arm clang have the same default triple. On the other hand, we
  pass an explicit target triple in the chrome build, so from a goma

tools/clang/scripts/package.py
@@ -156,14 +156,29 @@ def main():
   parser = argparse.ArgumentParser(description='build and package clang')
   parser.add_argument('--upload', action='store_true',
                       help='Upload the target archive to Google Cloud Storage.')
+  parser.add_argument('--build-mac-arm', action='store_true',
+                      help='Build arm binaries. Only valid on macOS.')
   args = parser.parse_args()
+
+  if args.build_mac_arm and sys.platform != 'darwin':
+    print('--build-mac-arm only valid on macOS')
+    return 1
 
   expected_stamp = GetExpectedStamp()
   pdir = 'clang-' + expected_stamp
   print(pdir)
 
   if sys.platform == 'darwin':
-    platform = 'Mac'
+    # When we need to run this script on an arm machine, we need to add a
+    # --build-mac-intel switch to pick which clang to build, pick the
+    # 'Mac_arm64' here when there's no flag and 'Mac' when --build-mac-intel is
+    # passed. Also update the build script to explicitly pass a default triple
+    # then.
+    assert platform.machine() != 'arm64'
+    if args.build_mac_arm:
+      platform = 'Mac_arm64'
+    else:
+      platform = 'Mac'
   elif sys.platform == 'win32':
     platform = 'Win'
   else:
@@ -182,6 +197,8 @@ def main():
         os.path.join(THIS_DIR, 'build.py'), '--bootstrap', '--disable-asserts',
         '--run-tests', '--pgo'
     ]
+    if args.build_mac_arm:
+      build_cmd.append('--build-mac-arm')
     if sys.platform != 'darwin':
       build_cmd.append('--thinlto')
 
@@ -219,8 +236,11 @@ def main():
       # Include libclang_rt.builtins.a for Fuchsia targets.
       'lib/clang/$V/lib/aarch64-fuchsia/libclang_rt.builtins.a',
       'lib/clang/$V/lib/x86_64-fuchsia/libclang_rt.builtins.a',
-      'lib/clang/$V/lib/x86_64-fuchsia/libclang_rt.profile.a',
     ])
+    if not args.build_mac_arm
+      # TODO(thakis): Figure out why this doesn't build in --build-mac-arm
+      # builds.
+      want.append('lib/clang/$V/lib/x86_64-fuchsia/libclang_rt.profile.a')
   if sys.platform == 'darwin':
     want.extend([
       # AddressSanitizer runtime.

tools/clang/scripts/build.py
@@ -412,6 +412,8 @@ def main():
   parser = argparse.ArgumentParser(description='Build Clang.')
   parser.add_argument('--bootstrap', action='store_true',
                       help='first build clang with CC, then with itself.')
+  parser.add_argument('--build-mac-arm', action='store_true',
+                      help='Build arm binaries. Only valid on macOS.')
   parser.add_argument('--disable-asserts', action='store_true',
                       help='build with asserts disabled')
   parser.add_argument('--gcc-toolchain', help='what gcc toolchain to use for '
@@ -478,6 +480,9 @@ def main():
     print('for general Fuchsia build instructions.')
     return 1
 
+  if args.build_mac_arm and sys.platform != 'darwin':
+    print('--build-mac-arm only valid on macOS')
+    return 1
 
   # Don't buffer stdout, so that print statements are immediately flushed.
   # LLVM tests print output without newlines, so with buffering they won't be
@@ -906,9 +911,13 @@ def main():
     cmake_args.append('-DLLVM_ENABLE_LTO=Thin')
   if sys.platform == 'win32':
     cmake_args.append('-DLLVM_ENABLE_ZLIB=FORCE_ON')
+
   if sys.platform == 'darwin':
     cmake_args += ['-DCOMPILER_RT_ENABLE_IOS=ON',
                    '-DSANITIZER_MIN_OSX_VERSION=10.7']
+    if args.build_mac_arm:
+      cmake_args += ['-DCMAKE_OSX_ARCHITECTURES=arm64',
+                     '-DLLVM_USE_HOST_TOOLS=ON']
 
   # TODO(crbug.com/962988): Use -DLLVM_EXTERNAL_PROJECTS instead.
   CreateChromeToolsShim()
@@ -926,8 +935,9 @@ def main():
     # If any Chromium tools were built, install those now.
     RunCommand(['ninja', 'cr-install'], msvc_arch='x64')
 
-  VerifyVersionOfBuiltClangMatchesVERSION()
-  VerifyZlibSupport()
+  if not args.build_mac_arm:
+    VerifyVersionOfBuiltClangMatchesVERSION()
+    VerifyZlibSupport()
 
   if sys.platform == 'win32':
     platform = 'windows'
@@ -1050,14 +1060,20 @@ def main():
         os.mkdir(os.path.join(build_dir))
       os.chdir(build_dir)
       target_spec = target_arch + '-fuchsia'
+      if args.build_mac_arm:
+        # Just-built clang can't run (it's an arm binary), so use the bootstrap
+        # compiler instead.
+        host_path = LLVM_BOOTSTRAP_INSTALL_DIR
+      else:
+        host_path = LLVM_BUILD_DIR
       # TODO(thakis): Might have to pass -B here once sysroot contains
       # binaries (e.g. gas for arm64?)
       fuchsia_args = base_cmake_args + [
-        '-DCMAKE_C_COMPILER=' + os.path.join(LLVM_BUILD_DIR, 'bin/clang'),
-        '-DCMAKE_CXX_COMPILER=' + os.path.join(LLVM_BUILD_DIR, 'bin/clang++'),
-        '-DCMAKE_LINKER=' + os.path.join(LLVM_BUILD_DIR, 'bin/clang'),
-        '-DCMAKE_AR=' + os.path.join(LLVM_BUILD_DIR, 'bin/llvm-ar'),
-        '-DLLVM_CONFIG_PATH=' + os.path.join(LLVM_BUILD_DIR, 'bin/llvm-config'),
+        '-DCMAKE_C_COMPILER=' + os.path.join(host_path, 'bin/clang'),
+        '-DCMAKE_CXX_COMPILER=' + os.path.join(host_path, 'bin/clang++'),
+        '-DCMAKE_LINKER=' + os.path.join(host_path, 'bin/clang'),
+        '-DCMAKE_AR=' + os.path.join(host_path, 'bin/llvm-ar'),
+        '-DLLVM_CONFIG_PATH=' + os.path.join(host_path, 'bin/llvm-config'),
         '-DCMAKE_SYSTEM_NAME=Fuchsia',
         '-DCMAKE_C_COMPILER_TARGET=%s-fuchsia' % target_arch,
         '-DCMAKE_ASM_COMPILER_TARGET=%s-fuchsia' % target_arch,
@@ -1095,7 +1111,9 @@ def main():
                fuchsia_lib_dst_dir)
 
       # Build the Fuchsia profile runtime.
-      if target_arch == 'x86_64':
+      # TODO(thakis): Figure out why this doesn't build with the stage0
+      # compiler in arm builds.
+      if target_arch == 'x86_64' and not args.build_mac_arm:
         fuchsia_args.extend([
             '-DCOMPILER_RT_BUILD_BUILTINS=OFF',
             '-DCOMPILER_RT_BUILD_PROFILE=ON',
@@ -1116,10 +1134,11 @@ def main():
                               fuchsia_lib_dst_dir)
 
   # Run tests.
-  if args.run_tests or args.llvm_force_head_revision:
+  if (not args.build_mac_arm and
+      (args.run_tests or args.llvm_force_head_revision)):
     RunCommand(['ninja', '-C', LLVM_BUILD_DIR, 'cr-check-all'], msvc_arch='x64')
 
-  if args.run_tests:
+  if not args.build_mac_arm and args.run_tests:
     test_targets = [ 'check-all' ]
     if sys.platform == 'darwin':
       # TODO(thakis): Run check-all on Darwin too, https://crbug.com/959361

commit 0a334e226413dfe48fefed80568e93b134ecb380

clang package script: fix more fallout from e99ac7b11ab89
Need to import the platform module, and need to not shadow it with
a local variable too.

Bug: 1103322
Change-Id: I613a77e7a5a9f23d24a25d458b32db4f952bad77
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800173
Reviewed-by: Hans Wennborg <hans@chromium.org>
Cr-Commit-Position: refs/heads/master@{#868587}

tools/clang/scripts/package.py
@@ -12,6 +12,7 @@ import argparse
 import fnmatch
 import itertools
 import os
+import platform
 import shutil
 import subprocess
 import sys
@@ -90,9 +91,11 @@ def RunGsutil(args):
   return subprocess.call([sys.executable, GetGsutilPath()] + args)
 
 
-def MaybeUpload(do_upload, filename, platform, extra_gsutil_args=[]):
-  gsutil_args = ['cp'] + extra_gsutil_args + ['-a', 'public-read', filename,
-      'gs://chromium-browser-clang-staging/%s/%s' % (platform, filename)]
+def MaybeUpload(do_upload, filename, gcs_platform, extra_gsutil_args=[]):
+  gsutil_args = ['cp'] + extra_gsutil_args + [
+      '-a', 'public-read', filename,
+      'gs://chromium-browser-clang-staging/%s/%s' % (gcs_platform, filename)
+  ]
   if do_upload:
     print('Uploading %s to Google Cloud Storage...' % filename)
     exit_code = RunGsutil(gsutil_args)
@@ -176,13 +179,13 @@ def main():
     # then.
     assert platform.machine() != 'arm64'
     if args.build_mac_arm:
-      platform = 'Mac_arm64'
+      gcs_platform = 'Mac_arm64'
     else:
-      platform = 'Mac'
+      gcs_platform = 'Mac'
   elif sys.platform == 'win32':
-    platform = 'Win'
+    gcs_platform = 'Win'
   else:
-    platform = 'Linux_x64'
+    gcs_platform = 'Linux_x64'
 
   with open('buildlog.txt', 'w') as log:
     Tee('Starting build\n', log)
@@ -430,11 +433,13 @@ def main():
   with tarfile.open(pdir + '.tgz', 'w:gz') as tar:
     for entry in tar_entries:
       tar.add(os.path.join(pdir, entry), arcname=entry, filter=PrintTarProgress)
-  MaybeUpload(args.upload, pdir + '.tgz', platform)
+  MaybeUpload(args.upload, pdir + '.tgz', gcs_platform)
 
   # Upload build log next to it.
   os.rename('buildlog.txt', pdir + '-buildlog.txt')
-  MaybeUpload(args.upload, pdir + '-buildlog.txt', platform,
+  MaybeUpload(args.upload,
+              pdir + '-buildlog.txt',
+              gcs_platform,
               extra_gsutil_args=['-z', 'txt'])
   os.remove(pdir + '-buildlog.txt')
 
@@ -448,7 +453,7 @@ def main():
   with tarfile.open(code_coverage_dir + '.tgz', 'w:gz') as tar:
     tar.add(os.path.join(code_coverage_dir, 'bin'), arcname='bin',
             filter=PrintTarProgress)
-  MaybeUpload(args.upload, code_coverage_dir + '.tgz', platform)
+  MaybeUpload(args.upload, code_coverage_dir + '.tgz', gcs_platform)
 
   # Zip up llvm-objdump and related tools for sanitizer coverage and Supersize.
   objdumpdir = 'llvmobjdump-' + stamp
@@ -470,7 +475,7 @@ def main():
             filter=PrintTarProgress)
     tar.add(llvmobjdump_stamp_file, arcname=llvmobjdump_stamp_file_base,
             filter=PrintTarProgress)
-  MaybeUpload(args.upload, objdumpdir + '.tgz', platform)
+  MaybeUpload(args.upload, objdumpdir + '.tgz', gcs_platform)
 
   # Zip up clang-tidy for users who opt into it, and Tricium.
   clang_tidy_dir = 'clang-tidy-' + stamp
@@ -481,7 +486,7 @@ def main():
   with tarfile.open(clang_tidy_dir + '.tgz', 'w:gz') as tar:
     tar.add(os.path.join(clang_tidy_dir, 'bin'), arcname='bin',
             filter=PrintTarProgress)
-  MaybeUpload(args.upload, clang_tidy_dir + '.tgz', platform)
+  MaybeUpload(args.upload, clang_tidy_dir + '.tgz', gcs_platform)
 
   # On Mac, lld isn't part of the main zip.  Upload it in a separate zip.
   if sys.platform == 'darwin':
@@ -499,7 +504,7 @@ def main():
     with tarfile.open(llddir + '.tgz', 'w:gz') as tar:
       tar.add(os.path.join(llddir, 'bin'), arcname='bin',
               filter=PrintTarProgress)
-    MaybeUpload(args.upload, llddir + '.tgz', platform)
+    MaybeUpload(args.upload, llddir + '.tgz', gcs_platform)
 
     # dsymutil isn't part of the main zip either, and it gets periodically
     # deployed to CIPD (manually, not as part of clang rolls) for use in the
@@ -512,7 +517,7 @@ def main():
     with tarfile.open(dsymdir + '.tgz', 'w:gz') as tar:
       tar.add(os.path.join(dsymdir, 'bin'), arcname='bin',
               filter=PrintTarProgress)
-    MaybeUpload(args.upload, dsymdir + '.tgz', platform)
+    MaybeUpload(args.upload, dsymdir + '.tgz', gcs_platform)
 
   # Zip up the translation_unit tool.
   translation_unit_dir = 'translation_unit-' + stamp
@@ -524,7 +529,7 @@ def main():
   with tarfile.open(translation_unit_dir + '.tgz', 'w:gz') as tar:
     tar.add(os.path.join(translation_unit_dir, 'bin'), arcname='bin',
             filter=PrintTarProgress)
-  MaybeUpload(args.upload, translation_unit_dir + '.tgz', platform)
+  MaybeUpload(args.upload, translation_unit_dir + '.tgz', gcs_platform)
 
   # Zip up the libclang binaries.
   libclang_dir = 'libclang-' + stamp
@@ -543,7 +548,7 @@ def main():
     for entry in tar_entries:
       tar.add(os.path.join(libclang_dir, entry), arcname=entry,
               filter=PrintTarProgress)
-  MaybeUpload(args.upload, libclang_dir + '.tgz', platform)
+  MaybeUpload(args.upload, libclang_dir + '.tgz', gcs_platform)
 
   if sys.platform == 'win32' and args.upload:
     binaries = [f for f in want if f.endswith('.exe') or f.endswith('.dll')]