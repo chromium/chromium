8a221e0b9717bd26d782e631e93ef172853919e1
Christopher Cameron <ccameron@chromium.org>
2021-04-02 00:13:17 +0000

    Enable WindowCaptureMacV2 by default
    Bug: 1179098
    Change-Id: Ibdced8d112508a43a6b1631c5d23bc657cc872fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800425
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868740}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -985,7 +985,7 @@ const base::Feature kDesktopCaptureMacV2{"DesktopCaptureMacV2",
                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kWindowCaptureMacV2{"WindowCaptureMacV2",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
+                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
 #endif  // defined(OS_MAC)
 
