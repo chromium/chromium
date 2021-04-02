3298ae4494e234a768bb7201ab810205502cd01b
David Tseng <dtseng@google.com>
2021-04-02 00:28:44 +0000

    Finalize ChromeVox turn off screen dialog text
    R=akihiroota@chromium.org
    
    Bug: 1190279
    Change-Id: I39b1397a113642317675eaa510166f00c437c06a
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785409
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868744}

--- chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
@@ -1275,7 +1275,9 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, DarkenScreenConfirmation) {
   sm_.ExpectSpeech("Button");
   sm_.ExpectSpeech("Turn off screen?");
   sm_.ExpectSpeech("Dialog");
-  sm_.ExpectSpeech("This turns off the screen.");
+  sm_.ExpectSpeech(
+      "This improves privacy by turning off your screen so it isn’t visible to "
+      "others.");
   sm_.ExpectSpeech(
       "You can always turn the screen back on by pressing Search plus "
       "Brightness up.");
