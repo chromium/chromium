4415a7d9389ef02afd39a4d5d6e83e1060b196dc
Chris Lu <thegreenfrog@chromium.org>
2021-04-05 19:00:32 +0000

    [ios] Add placeholder to Return to Recent Tab time string
    Bug: 1190979
    Change-Id: Ie94c113d1361aeaadf2c6199928c44f095b3e150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798284
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869225}

--- ios/chrome/browser/ui/start_surface/start_surface_util.mm
+++ ios/chrome/browser/ui/start_surface/start_surface_util.mm
@@ -3,9 +3,11 @@
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/start_surface/start_surface_util.h"
+#include "base/i18n/number_formatting.h"
+#include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
 #include "ios/chrome/grit/ios_strings.h"
-#import "ui/base/l10n/l10n_util_mac.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -62,15 +64,23 @@ NSString* GetRecentTabTileTimeLabelForSceneState(SceneState* sceneState) {
     return @"";
   }
   NSInteger time = (NSInteger)timeSinceOpen / 3600;
+  NSString* timeString = [NSString
+      stringWithFormat:@"%@",
+                       base::SysUTF16ToNSString(base::FormatNumber(time))];
   NSString* timeLabel =
-      l10n_util::GetNSString(IDS_IOS_RETURN_TO_RECENT_TAB_TIME_HOURS);
+      l10n_util::GetNSStringF(IDS_IOS_RETURN_TO_RECENT_TAB_TIME_HOURS,
+                              base::SysNSStringToUTF16(timeString));
   if (time > 24) {
     // If it has been at least a day since the most recent tab was opened,
     // then show days since instead of hours.
     time = time / 24;
-    timeLabel = l10n_util::GetNSString(IDS_IOS_RETURN_TO_RECENT_TAB_TIME_DAYS);
+    timeString = [NSString
+        stringWithFormat:@"%@",
+                         base::SysUTF16ToNSString(base::FormatNumber(time))];
+    timeLabel = l10n_util::GetNSStringF(IDS_IOS_RETURN_TO_RECENT_TAB_TIME_DAYS,
+                                        base::SysNSStringToUTF16(timeString));
   }
-  return [NSString stringWithFormat:@" · %ld%@", (long)time, timeLabel];
+  return [NSString stringWithFormat:@" · %@", timeLabel];
 }
 
 void SetStartSurfaceSessionObjectForSceneState(SceneState* sceneState) {
