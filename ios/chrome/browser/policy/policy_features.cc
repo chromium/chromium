bea8ac62e3d2666291d8fb28b04c4d24aa644099
Tina Wang <tinazwang@google.com>
2021-04-06 19:27:58 +0000

    [ios]🏢 Enable incognito mode availability flag by default
    Enable incognito mode availability flag by default.
    
    Bug: 1196347
    Change-Id: I034c5b3d31b80827ad729f365d909b03d416d1ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808044
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869663}

--- ios/chrome/browser/policy/policy_features.cc
+++ ios/chrome/browser/policy/policy_features.cc
@@ -13,7 +13,7 @@
 #include "ios/web/common/features.h"
 
 const base::Feature kEnableIncognitoModeAvailabilityIOS{
-    "EnableIncognitoModeAvailabilityIOS", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EnableIncognitoModeAvailabilityIOS", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kURLBlocklistIOS{"URLBlocklistIOS",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
