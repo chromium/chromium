2c95ef11b3f6cf1d3866f061c48a16f719da86ff
Yi Xu <yiyix@chromium.org>
2021-04-06 04:46:00 +0000

    Add options for adding ContextAttributes when calling getContext
    Adding a new tag "attributes" in Canvas Template file, so that users
    can do getContext('2d', attributes). I also added a test uses this
    function in this cl.
    
    Bug: 1178707
    
    Change-Id: I020f7d83831391895b8b937e623f30f28dbcbc5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2788735
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869392}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutils.py
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutils.py
@@ -385,6 +385,13 @@ def genTestUtils(TESTOUTPUTDIR, IMAGEOUTPUTDIR, TEMPLATEFILE, NAME2DIRFILE, ISOF
         desc = test.get('desc', '')
         escaped_desc = simpleEscapeJS(desc)
 
+        attributes = test.get('attributes', '')
+        if attributes:
+            context_args = "'2d', %s" % attributes.strip()
+            attributes = ', ' + attributes.strip()
+        else:
+            context_args = "'2d'"
+
         for (variant, extra_script) in script_variants:
             name_variant = '' if not variant else '.' + variant
 
@@ -397,7 +404,8 @@ def genTestUtils(TESTOUTPUTDIR, IMAGEOUTPUTDIR, TEMPLATEFILE, NAME2DIRFILE, ISOF
                 'fonts':fonts, 'fonthack':fonthack, 'timeout': timeout,
                 'canvas':canvas, 'expected':expectation_html, 'code':code,
                 'scripts':scripts + extra_script,
-                'fallback':fallback
+                'fallback':fallback, 'attributes':attributes,
+                'context_args': context_args
             }
 
             f = codecs.open('%s/%s%s.html' % (TESTOUTPUTDIR, mapped_name, name_variant), 'w', 'utf-8')
