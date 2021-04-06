2a4b900d61b497160e6fa73aab07d99897b2faa7
Austin Eng <enga@chromium.org>
2021-04-06 03:49:02 +0000

    Update regenerate_internal_cts_html.sh to work without additional deps
    Transpiling the CTS sources to a tmp directory and then running the
    generator script breaks the dependency on ts-node. This will allow
    the script to run on an autoroller.
    
    Bug: chromium:1186320
    Change-Id: Ib5cefc28dd3e494580ba0bf95443697b156dd56b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803571
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869383}

--- third_party/blink/web_tests/webgpu/regenerate_internal_cts_html.sh
+++ third_party/blink/web_tests/webgpu/regenerate_internal_cts_html.sh
@@ -7,7 +7,8 @@
 # checked-out version of the WebGPU CTS. It does the following:
 #
 #   - Extracts the "name" part of every expectation from WebGPUExpectations.
-#   - Prepares the WebGPU checkout to run the gen_wpt_cts_html tool.
+#   - Transpiles the WebGPU checkout into a tmp directory to run the
+#     gen_wpt_cts_html tool.
 #   - Runs gen_wpt_cts_html with the appropriate metadata and the extracted
 #     expectations names. This generates a cts.html file with the necessary
 #     variants to cover the expectations in WebGPUExpectations.
@@ -21,9 +22,29 @@ set -e
 cd "$(dirname "$0")"/../../../..  # cd to [chromium]/src/
 
 expectations=$(mktemp)
-trap "{ rm -f $expectations; }" EXIT
-echo $expectations
+out_dir=$(mktemp -d)
+out_cts_html=$(realpath $(dirname "$0")/../wpt_internal/webgpu/cts.html)
 
+echo "Expectations names: $expectations"
+echo "JS output dir: $out_dir"
+echo "Output CTS html: $out_cts_html"
+
+function check_output() {
+  if [ ! -f $out_cts_html ]; then
+    echo "Failed to generate $out_cts_html"
+    exit 1
+  fi
+}
+
+function cleanup() {
+  rm -f $expectations
+  rm -rf $out_dir
+  check_output
+}
+
+trap cleanup EXIT
+
+rm -f $out_cts_html
 
 echo 'Extracting expectation names...'
 third_party/webgpu-cts/scripts/extract_expectation_names_for_variant_generation.py \
@@ -33,16 +54,22 @@ cat third_party/blink/web_tests/webgpu/internal_cts_test_splits.txt >> $expectat
 
 pushd third_party/webgpu-cts/src > /dev/null
 
-  echo 'Updating node for webgpu-cts...'
-  npm install --frozen-lockfile
-  npx grunt run:generate-listings
+  echo 'Transpiling WebGPU CTS...'
+  ../scripts/tsc_ignore_errors.py \
+    --project node.tsconfig.json \
+    --outDir $out_dir \
+    --noEmit false \
+    --declaration false \
+    --sourceMap false
 
-  echo 'Regenerating expectations...'
-  npm run gen_wpt_cts_html \
-    ../../blink/web_tests/wpt_internal/webgpu/cts.html \
+  echo 'Regenerating cts.html...'
+  ../../node/node.py $out_dir/common/tools/gen_wpt_cts_html.js \
+    $out_cts_html \
     ../../blink/web_tests/webgpu/ctshtml-template.txt \
     ../../blink/web_tests/webgpu/argsprefixes.txt \
     $expectations \
     'wpt_internal/webgpu/cts.html' webgpu
 
 popd > /dev/null
+
+check_output
