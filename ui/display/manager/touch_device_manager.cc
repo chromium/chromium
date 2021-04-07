35a3d2b2e0cdb027a36cde1b09c2af49e989cff2
Pablo Ceballos <pceballos@google.com>
2021-04-07 18:05:29 +0000

    Move call to MakeAbsoluteFilePath
    The call to Base::MakeAbsoluteFilePath triggers a DCHECK since it's
    considered potentially blocking and this runs on the UI thread. Instead,
    move the call to a thread that can block.
    
    (cherry picked from commit 35159e73a9efb7f144d9c915b7eedf05003ab96c)
    
    (cherry picked from commit c3ab2dff911543211db00791b378d5b62d2d015f)
    
    Bug: 1181384
    Change-Id: I72cb048c43b5aacb53d55875bfd9521c1d1a10f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716337
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Pablo Ceballos <pceballos@chromium.org>
    Cr-Original-Original-Commit-Position: refs/heads/master@{#859234}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753548
    Auto-Submit: Rehan Ghori <rehang@chromium.org>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Cr-Original-Commit-Position: refs/branch-heads/4147@{#1140}
    Cr-Original-Branched-From: 16307825352720ae04d898f37efa5449ad68b606-refs/heads/master@{#768962}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806483
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Rehan Ghori <rehang@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4324@{#2291}
    Cr-Branched-From: c73b5a651d37a6c4d0b8e3262cc4015a5579c6c8-refs/heads/master@{#827102}

--- ui/display/manager/touch_device_manager.cc
+++ ui/display/manager/touch_device_manager.cc
@@ -29,39 +29,6 @@ using DeviceList = std::vector<ui::TouchscreenDevice>;
 constexpr char kFallbackTouchDeviceName[] = "fallback_touch_device_name";
 constexpr char kFallbackTouchDevicePhys[] = "fallback_touch_device_phys";
 
-base::FilePath GetDisplaySysPath(const ManagedDisplayInfo* display) {
-  std::vector<base::FilePath::StringType> components;
-  display->sys_path().GetComponents(&components);
-  base::FilePath path_thus_far;
-
-  for (const auto& component : components) {
-    if (path_thus_far.empty()) {
-      path_thus_far = base::FilePath(component);
-    } else {
-      path_thus_far = path_thus_far.Append(component);
-    }
-
-    // Newer versions of the EVDI kernel driver include a symlink to the USB
-    // device in the sysfs EVDI directory (e.g.
-    // /sys/devices/platform/evdi.0/device) for EVDI displays that are USB. If
-    // that symlink exists, read it, and use that path as the sysfs path for the
-    // display when calculating the association score to match it with a
-    // corresponding USB touch device. If the symlink doesn't exist, use the
-    // normal sysfs path.
-    if (base::StartsWith(component, "evdi", base::CompareCase::SENSITIVE)) {
-      base::FilePath usb_device_path;
-      if (base::ReadSymbolicLink(path_thus_far.Append("device"),
-                                 &usb_device_path)) {
-        return base::MakeAbsoluteFilePath(
-            path_thus_far.Append(usb_device_path));
-      }
-      break;
-    }
-  }
-
-  return display->sys_path();
-}
-
 // Returns true if |path| is likely a USB device.
 bool IsDeviceConnectedViaUsb(const base::FilePath& path) {
   std::vector<base::FilePath::StringType> components;
@@ -89,7 +56,7 @@ int GetUsbAssociationScore(const ManagedDisplayInfo* display,
                            const ui::TouchscreenDevice& device) {
   // If the devices are not both connected via USB, then there cannot be a USB
   // association score.
-  if (!IsDeviceConnectedViaUsb(GetDisplaySysPath(display)) ||
+  if (!IsDeviceConnectedViaUsb(display->sys_path()) ||
       !IsDeviceConnectedViaUsb(device.sys_path))
     return 0;
 
@@ -97,7 +64,7 @@ int GetUsbAssociationScore(const ManagedDisplayInfo* display,
   // sysfs paths have in common.
   std::vector<base::FilePath::StringType> display_components;
   std::vector<base::FilePath::StringType> device_components;
-  GetDisplaySysPath(display).GetComponents(&display_components);
+  display->sys_path().GetComponents(&display_components);
   device.sys_path.GetComponents(&device_components);
 
   std::size_t largest_idx = 0;
@@ -351,8 +318,7 @@ void TouchDeviceManager::AssociateTouchscreens(
     for (const ManagedDisplayInfo* display : displays) {
       VLOG(2) << "Received display " << display->name()
               << " (size: " << display->GetNativeModeSize().ToString() << ", "
-              << "sys_path: " << GetDisplaySysPath(display).LossyDisplayName()
-              << ")";
+              << "sys_path: " << display->sys_path().LossyDisplayName() << ")";
     }
     for (const ui::TouchscreenDevice& device : devices) {
       VLOG(2) << "Received device " << device.name
