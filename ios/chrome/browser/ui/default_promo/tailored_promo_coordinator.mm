d0e45098aaa6db065ed809a8ef3053c8b1939512
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-04-07 17:44:36 +0000

    [iOS][Default Promo] Add tailored coordinator & view controller
    Files forked from current browser modal fullscreen promo.
    
    Bug: 1191736, 1191727
    Change-Id: I408212710e1b0c1cc7b69fdc3fc906a0ea336452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810173
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870092}

---
+++ ios/chrome/browser/ui/default_promo/tailored_promo_coordinator.mm
@@ -0,0 +1,127 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/default_promo/tailored_promo_coordinator.h"
+
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
+#import "ios/chrome/browser/ui/default_promo/default_browser_string_util.h"
+#import "ios/chrome/browser/ui/default_promo/tailored_promo_view_controller.h"
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
+#import "ios/chrome/common/ui/elements/popover_label_view_controller.h"
+#include "ios/chrome/grit/ios_google_chrome_strings.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util_mac.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+using base::RecordAction;
+using base::UserMetricsAction;
+using l10n_util::GetNSString;
+
+@interface TailoredPromoCoordinator () <
+    ConfirmationAlertActionHandler,
+    UIAdaptivePresentationControllerDelegate>
+
+// The fullscreen confirmation modal promo view controller this coordinator
+// manages.
+@property(nonatomic, strong)
+    TailoredPromoViewController* tailoredPromoViewController;
+
+// Popover used to show learn more info, not nil when presented.
+@property(nonatomic, strong)
+    PopoverLabelViewController* learnMoreViewController;
+
+@end
+
+@implementation TailoredPromoCoordinator
+
+#pragma mark - Public Methods.
+
+- (void)start {
+  [super start];
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Appear"));
+  self.tailoredPromoViewController = [[TailoredPromoViewController alloc] init];
+
+  // TODO(crbug.com/1191733): Set up the correct data in the view controller.
+
+  self.tailoredPromoViewController.actionHandler = self;
+  self.tailoredPromoViewController.modalPresentationStyle =
+      UIModalPresentationFormSheet;
+  self.tailoredPromoViewController.presentationController.delegate = self;
+  [self.baseViewController
+      presentViewController:self.tailoredPromoViewController
+                   animated:YES
+                 completion:nil];
+}
+
+- (void)stop {
+  // Ensure that presentationControllerDidDismiss: is not called in response to
+  // a stop.
+  self.tailoredPromoViewController.presentationController.delegate = nil;
+  [self.tailoredPromoViewController.presentingViewController
+      dismissViewControllerAnimated:YES
+                         completion:nil];
+  self.tailoredPromoViewController = nil;
+  [super stop];
+}
+
+#pragma mark - UIAdaptivePresentationControllerDelegate
+
+- (void)presentationControllerDidDismiss:
+    (UIPresentationController*)presentationController {
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Dismiss"));
+  // This ensures that a modal swipe dismiss will also be logged.
+  // TODO(crbug.com/1191732): log interaction with promo.
+}
+
+#pragma mark - ConfirmationAlertActionHandler
+
+- (void)confirmationAlertDismissAction {
+  // There should be no cancel toolbar button for this UI.
+  NOTREACHED();
+}
+
+- (void)confirmationAlertPrimaryAction {
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Accepted"));
+  // TODO(crbug.com/1191732): log interaction with promo.
+  [[UIApplication sharedApplication]
+                openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]
+                options:{}
+      completionHandler:nil];
+
+  [self.handler hidePromo];
+}
+
+- (void)confirmationAlertSecondaryAction {
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Dismiss"));
+  // TODO(crbug.com/1191732): log interaction with promo.
+  [self.handler hidePromo];
+}
+
+- (void)confirmationAlertLearnMoreAction {
+  NSString* message =
+      GetNSString(IDS_IOS_DEFAULT_BROWSER_LEARN_MORE_INSTRUCTIONS_MESSAGE);
+  self.learnMoreViewController =
+      [[PopoverLabelViewController alloc] initWithMessage:message];
+
+  self.learnMoreViewController.popoverPresentationController.barButtonItem =
+      self.tailoredPromoViewController.helpButton;
+  self.learnMoreViewController.popoverPresentationController
+      .permittedArrowDirections = UIPopoverArrowDirectionUp;
+
+  [self.tailoredPromoViewController
+      presentViewController:self.learnMoreViewController
+                   animated:YES
+                 completion:nil];
+}
+
+@end
