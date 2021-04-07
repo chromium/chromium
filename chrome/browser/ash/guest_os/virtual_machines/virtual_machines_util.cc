805e640c4da57880bcca607beeed71b7d712f363
Yeunjoo Choi <ychoi@igalia.com>
2021-04-07 06:36:06 +0000

    Move //chrome/browser/{chromeos => ash/guest_os}/virtual_machines
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: Id63c5419eb95f4b1bf65b926173415896e9b765b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809715
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#869899}

--- chrome/browser/ash/guest_os/virtual_machines/virtual_machines_util.cc
+++ chrome/browser/ash/guest_os/virtual_machines/virtual_machines_util.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/virtual_machines/virtual_machines_util.h"
+#include "chrome/browser/ash/guest_os/virtual_machines/virtual_machines_util.h"
 
 #include "chrome/browser/ash/settings/cros_settings.h"
 #include "chromeos/settings/cros_settings_names.h"
