95933b351df87da82eb60ec488914c9b8c07ab6d
Gabriel Charette <gab@chromium.org>
2021-04-07 18:06:37 +0000

    Update PostTaskAndReply comment for the new implementation
    The comments around PostTaskAndReply are outdated after
    http://crrev.com/e5de8d551e80115b. This CL updates them for new impl.
    
    Cherry-picked from
    https://chromium-review.googlesource.com/c/chromium/src/+/603038
    
    R=fdoray@chromium.org
    
    Change-Id: I08209f16b0e37f4efa0a7c2dc88b8b6f575dedde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808856
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870100}

--- base/task_runner.h
+++ base/task_runner.h
@@ -73,14 +73,17 @@ class BASE_EXPORT TaskRunner
                                OnceClosure task,
                                base::TimeDelta delay) = 0;
 
-  // Posts |task| on the current TaskRunner.  On completion, |reply|
-  // is posted to the thread that called PostTaskAndReply().  Both
-  // |task| and |reply| are guaranteed to be deleted on the thread
-  // from which PostTaskAndReply() is invoked.  This allows objects
-  // that must be deleted on the originating thread to be bound into
-  // the |task| and |reply| OnceClosures.  In particular, it can be useful
-  // to use WeakPtr<> in the |reply| OnceClosure so that the reply
-  // operation can be canceled. See the following pseudo-code:
+  // Posts |task| on the current TaskRunner.  On completion, |reply| is posted
+  // to the sequence that called PostTaskAndReply().  On the success case,
+  // |task| is destroyed on the target sequence and |reply| is destroyed on the
+  // originating sequence immediately after their invocation.  If an error
+  // happened on the onward PostTask, both |task| and |reply| are destroyed on
+  // the originating sequence, and on an error on the backward PostTask, |reply|
+  // is leaked rather than being destroyed on the wrong sequence.  This allows
+  // objects that must be deleted on the originating sequence to be bound into
+  // the |reply| Closures.  In particular, it can be useful to use WeakPtr<> in
+  // the |reply| Closure so that the reply operation can be canceled. See the
+  // following pseudo-code:
   //
   // class DataBuffer : public RefCountedThreadSafe<DataBuffer> {
   //  public:
