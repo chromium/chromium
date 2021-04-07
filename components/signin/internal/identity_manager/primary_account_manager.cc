c8e08deb633ccaacb160d8a1ba841e32fea77521
Boris Sazonov <bsazonov@chromium.org>
2021-04-07 11:02:27 +0000

    Check for primary account with ConsentLevel::kSignin in LoadCredentials
    PrimaryAccountManager::OnRefreshTokensLoaded and
    LoadCredentials in ProfileOAuth2TokenService delegates have some special
    logic for the primary account (for example, the primary account isn't
    removed from AccountTrackerService even if it is not present in
    ProfileOAuth2TokenService delegate). However, in both places the primary
    account was obtained using ConsentLevel::kSync. This CL replaces this
    with ConsentLevel::kSignin for ChromeOS. Other platforms will be
    migrated in subsequent CLs.
    
    Bug: 1117472, 1196596
    Change-Id: I4ee0eea69c0664a7a150e7899934a5f0ef73b82e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809154
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869952}

--- components/signin/internal/identity_manager/primary_account_manager.cc
+++ components/signin/internal/identity_manager/primary_account_manager.cc
@@ -125,8 +125,13 @@ void PrimaryAccountManager::Initialize(PrefService* local_state) {
   // It is important to only load credentials after starting to observe the
   // token service.
   token_service_->AddObserver(this);
-  token_service_->LoadCredentials(
-      GetPrimaryAccountId(signin::ConsentLevel::kSync));
+  signin::ConsentLevel consent_level = signin::ConsentLevel::kSync;
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // TODO(https://crbug.com/1196596): Use kSignin on all platforms.
+  if (base::FeatureList::IsEnabled(switches::kUseAccountManagerFacade))
+    consent_level = signin::ConsentLevel::kSignin;
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+  token_service_->LoadCredentials(GetPrimaryAccountId(consent_level));
 }
 
 bool PrimaryAccountManager::IsInitialized() const {
@@ -398,8 +403,13 @@ void PrimaryAccountManager::OnRefreshTokensLoaded() {
   if (token_service_->HasLoadCredentialsFinishedWithNoErrors()) {
     std::vector<AccountInfo> accounts_in_tracker_service =
         account_tracker_service_->GetAccounts();
-    const CoreAccountId sync_account_id =
-        GetPrimaryAccountId(signin::ConsentLevel::kSync);
+    signin::ConsentLevel consent_level = signin::ConsentLevel::kSync;
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    // TODO(https://crbug.com/1196596): Use kSignin on all platforms.
+    if (base::FeatureList::IsEnabled(switches::kUseAccountManagerFacade))
+      consent_level = signin::ConsentLevel::kSignin;
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+    const CoreAccountId sync_account_id = GetPrimaryAccountId(consent_level);
     for (const auto& account : accounts_in_tracker_service) {
       if (sync_account_id != account.account_id &&
           !token_service_->RefreshTokenIsAvailable(account.account_id)) {
