56f96e537c828f04906c0bd8d8913f4c7141e9c4
Shengsong Tan <sstan@google.com>
2021-04-08 09:52:01 +0000

    exo: promo kZAuraShellVersion from 16 to 18
    The https://crrev.com/c/2789999 promo the aura shell to version 18,
    this change make it available.
    
    BUG=b:177405617
    TEST=build
    
    Change-Id: I17fa965b796673c04a233b47d530ad37b3f39ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812802
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@google.com>
    Cr-Commit-Position: refs/heads/master@{#870438}

--- components/exo/wayland/zaura_shell.h
+++ components/exo/wayland/zaura_shell.h
@@ -17,7 +17,7 @@ struct wl_resource;
 namespace exo {
 namespace wayland {
 
-constexpr uint32_t kZAuraShellVersion = 16;
+constexpr uint32_t kZAuraShellVersion = 18;
 
 // Adds bindings to the Aura Shell. Normally this implies Ash on ChromeOS
 // builds. On non-ChromeOS builds the protocol provides access to Aura windowing
