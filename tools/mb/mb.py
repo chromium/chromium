38b6a9a8e5901766613879b6976f207aa163588a
Takuto Ikuta <tikuta@chromium.org>
2021-04-08 05:27:28 +0000

    Revert "Reland #7 of "Force Python 3 to be used in build.""
    This reverts commit 0a70004d628b1e3113195d2208297ed2b2d35841.
    
    Reason for revert:
    https://crbug.com/1196930
    
    Original change's description:
    > Reland #7 of "Force Python 3 to be used in build."
    >
    > This is a reland of d9caee7e07aab47c71b9d84d7745e2a953708944.
    >
    > A number of fixes have landed in between then and now, and I
    > think we're ready for another round.
    >
    > Original change's description:
    > > Reland #6 of "Force Python 3 to be used in build."
    > >
    > > This reverts commit b2446a04bfe29b7a743bafd43dd0ad724a20e590
    > >
    > > Reason for revert: Trying again w/ build fixes.
    > >
    > > The two issues we saw yesterday with reland attempt #5
    > > were fixed in
    > >   https://crrev.com/c/2768998 (remoting linux installer)
    > >   https://crrev.com/c/2768998 (GLES test scripts)
    > >
    > > But the change wasn't in the tree for very long, so there may
    > > well be more. Let's try again to see.
    > >
    > > Bug: 1112471
    > > Cq-Include-Trybots: luci.chromium.try:linux-angle-rel
    > > Cq-Include-Trybots: luci.chrome.try:linux-chrome
    > > Change-Id: Ie5483d1f6838deeaf0f9b7cddc0bb57a82960c29
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2769361
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Commit-Queue: Dirk Pranke <dpranke@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#864372}
    >
    > Bug: 1112471
    > Change-Id: Iccef0aa5d92c28fee1ce67e446b9b15a7cc0745a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792623
    > Commit-Queue: Dirk Pranke <dpranke@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#868187}
    
    Bug: 1112471, 1196930
    Change-Id: I405a771ffc29ed40e74764008c9e0be1e4b9ef7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812719
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#870378}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1085,6 +1085,33 @@ class MetaBuildWrapper(object):
       self.WriteFile(gn_runtime_deps_path, '\n'.join(labels) + '\n')
       cmd.append('--runtime-deps-list-file=%s' % gn_runtime_deps_path)
 
+    # Detect if we are running in a vpython interpreter, and if so force GN to
+    # use the real python interpreter. crbug.com/1049421
+    # This ensures that ninja will only use the real python interpreter and not
+    # vpython, so that any python scripts in the build will only use python
+    # modules vendored into //third_party.
+    # This can be deleted when python 3 becomes the only supported interpreter,
+    # because in python 3 vpython will no longer have its current 'viral'
+    # qualities and will require explicit usage to opt in to.
+    prefix = getattr(sys, "real_prefix", sys.prefix)
+    python_exe = 'python.exe' if self.platform.startswith('win') else 'python'
+    # The value of prefix varies. Sometimes it extends to include the bin/
+    # directory of the python install such that prefix/python is the
+    # interpreter, and other times prefix/bin/python is the interpreter.
+    # Therefore we need to check both. Also, it is safer to check prefix/bin
+    # first because there have been previous installs where prefix/bin/python
+    # was the real binary and prefix/python was actually vpython-native.
+    possible_python_locations = [
+        os.path.join(prefix, 'bin', python_exe),
+        os.path.join(prefix, python_exe),
+    ]
+    for p in possible_python_locations:
+      if os.path.isfile(p):
+        cmd.append('--script-executable=%s' % p)
+        break
+    else:
+      self.Print('python interpreter not under %s' % prefix)
+
     ret, output, _ = self.Run(cmd)
     if ret != 0:
       if self.args.json_output:
