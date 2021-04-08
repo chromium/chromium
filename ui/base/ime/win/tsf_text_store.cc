8203295f4f7fce51856713178991c2ea4f7321e4
jongdeok.kim <jongdeok.kim@navercorp.com>
2021-04-08 06:15:57 +0000

    Reset composition state in TerminateComposition()
    This CL merges ResetCompositionState() into TerminateComposition().
    
    Change-Id: Ie63bf9e34268771c211085cb1f842a5d3cffd589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809927
    Commit-Queue: jongdeok.kim <jongdeok.kim@navercorp.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870387}

--- ui/base/ime/win/tsf_text_store.cc
+++ ui/base/ime/win/tsf_text_store.cc
@@ -1098,34 +1098,33 @@ bool TSFTextStore::GetCompositionStatus(
   return true;
 }
 
-void TSFTextStore::ResetCompositionState() {
+bool TSFTextStore::TerminateComposition() {
+  TRACE_EVENT0("ime", "TSFTextStore::TerminateComposition");
+  bool terminate_composition_called = false;
+  if (context_ && has_composition_range_) {
+    Microsoft::WRL::ComPtr<ITfContextOwnerCompositionServices> service;
+
+    if (SUCCEEDED(context_->QueryInterface(IID_PPV_ARGS(&service)))) {
+      service->TerminateComposition(nullptr);
+      terminate_composition_called = true;
+    }
+  }
+
   previous_composition_string_.clear();
   previous_composition_start_ = 0;
   previous_composition_selection_range_ = gfx::Range::InvalidRange();
   previous_text_spans_.clear();
 
   string_pending_insertion_.clear();
+  has_composition_range_ = false;
   composition_range_.set_start(0);
   composition_range_.set_end(0);
 
   selection_ = gfx::Range(composition_from_client_.end(),
                           composition_from_client_.end());
   composition_start_ = selection_.end();
-}
 
-bool TSFTextStore::TerminateComposition() {
-  TRACE_EVENT0("ime", "TSFTextStore::TerminateComposition");
-  if (context_ && has_composition_range_) {
-    Microsoft::WRL::ComPtr<ITfContextOwnerCompositionServices> service;
-
-    if (SUCCEEDED(context_->QueryInterface(IID_PPV_ARGS(&service)))) {
-      service->TerminateComposition(nullptr);
-      has_composition_range_ = false;
-      return true;
-    }
-  }
-
-  return false;
+  return terminate_composition_called;
 }
 
 void TSFTextStore::CalculateTextandSelectionDiffAndNotifyIfNeeded() {
@@ -1305,8 +1304,6 @@ bool TSFTextStore::CancelComposition() {
 
   TRACE_EVENT0("ime", "TSFTextStore::CancelComposition");
 
-  ResetCompositionState();
-
   return TerminateComposition();
 }
 
@@ -1320,8 +1317,6 @@ bool TSFTextStore::ConfirmComposition() {
 
   if (!text_input_client_)
     return false;
-
-  ResetCompositionState();
 
   return TerminateComposition();
 }
