51229538336b713f1fdf6ad682058b8b949b89f0
Johann <johannkoenig@google.com>
2021-04-08 00:19:39 +0000

    doc: reverse condition on |exit_code| test
    Terminate() only sets |exit_code| on Windows.
    
    Change-Id: I04d595f86871c67545d56b4687517fbf2c61c6db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810036
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#870289}

--- base/process/process_unittest.cc
+++ base/process/process_unittest.cc
@@ -183,8 +183,8 @@ TEST_F(ProcessTest, Terminate) {
 
   EXPECT_NE(TERMINATION_STATUS_STILL_RUNNING,
             GetTerminationStatus(process.Handle(), &exit_code));
-#if !defined(OS_POSIX) && !defined(OS_FUCHSIA)
-  // The POSIX & Fuchsia implementations actually ignore the exit_code.
+#if defined(OS_WIN)
+  // Only Windows propagates the |exit_code| set in Terminate().
   EXPECT_EQ(kExpectedExitCode, exit_code);
 #endif
 }
