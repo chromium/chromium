a44ab74b2fd62e9dadb3b36805aca27eb3bdf9ca
Shahbaz Youssefi <syoussefi@chromium.org>
2021-04-08 15:07:54 +0000

    Remove the remove_pow_with_constant_exponent workaround
    Only used by the validating command decoder on Linux, which is
    deprecated.
    
    Bug: 477306
    Change-Id: Iddaf4330b065875ea878a13262e0f6d5a94c2021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810879
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870523}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -4559,8 +4559,6 @@ bool GLES2DecoderImpl::InitializeShaderTranslator() {
     driver_bug_workarounds |= SH_SCALARIZE_VEC_AND_MAT_CONSTRUCTOR_ARGS;
   if (workarounds().regenerate_struct_names)
     driver_bug_workarounds |= SH_REGENERATE_STRUCT_NAMES;
-  if (workarounds().remove_pow_with_constant_exponent)
-    driver_bug_workarounds |= SH_REMOVE_POW_WITH_CONSTANT_EXPONENT;
   if (workarounds().emulate_abs_int_function)
     driver_bug_workarounds |= SH_EMULATE_ABS_INT_FUNCTION;
   if (workarounds().rewrite_texelfetchoffset_to_texelfetch)
