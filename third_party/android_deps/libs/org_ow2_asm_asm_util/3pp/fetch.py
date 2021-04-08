755471e305b5c8919725ae067b8f2269b640f04e
benjamin joyce <bjoyce@chromium.org>
2021-04-08 19:37:50 +0000

    Revert "Update 3pp files for robolectric 4.5.1"
    This reverts commit 8e53b43b2aa1e6b4f175054aa149c1fe9ae83e6e.
    
    Reason for revert: Just needed to get 3pp files uploaded to cipd. Am not able to easily land 4.5.1 update as third_party/robolectric uses ~6 manually downloaded and zipped files, so need to get 3pp to replicate that behavior first.
    
    Original change's description:
    > Update 3pp files for robolectric 4.5.1
    >
    > Adding 3pp files. Per Readme, adding 3pp file changes so that the
    > packages will be downloaded to cipd.
    > https://source.chromium.org/chromium/chromium/src/+/master:third_party/android_deps/README.md;drc=4ba6d0b8e522c195d6d337dcc0426c337f58b72b;l=25
    >
    > After that, can test and land the actual update to robolectric 4.5.1
    > and modify the version numbers correctly.
    > https://chromium-review.googlesource.com/c/chromium/src/+/2800427
    >
    > Bug: 1195173
    > Change-Id: I19b8462a5ccb70121e7ba65efc27651a6d433d6d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800914
    > Commit-Queue: benjamin joyce <bjoyce@chromium.org>
    > Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#869724}
    
    Bug: 1195173
    Change-Id: I23c75e6bb22c5d41df60f113f938fd33d5c13b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815579
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: benjamin joyce <bjoyce@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870652}

--- third_party/android_deps/libs/org_ow2_asm_asm_util/3pp/fetch.py
+++ third_party/android_deps/libs/org_ow2_asm_asm_util/3pp/fetch.py
@@ -12,9 +12,9 @@ import argparse
 import json
 import os
 
-_FILE_URL = 'https://repo.maven.apache.org/maven2/org/ow2/asm/asm-util/9.0/asm-util-9.0.jar'
-_FILE_NAME = 'asm-util-9.0.jar'
-_FILE_VERSION = '9.0'
+_FILE_URL = 'https://repo.maven.apache.org/maven2/org/ow2/asm/asm-util/7.0/asm-util-7.0.jar'
+_FILE_NAME = 'asm-util-7.0.jar'
+_FILE_VERSION = '7.0'
 
 
 def do_latest():
