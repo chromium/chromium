24f09fbd28d76cd45ac9303ee0690a891fad4dbc
Steven Bennetts <stevenjb@chromium.org>
2021-04-09 15:48:42 +0000

    CrOS Networking: Handle shill::kErrorSimLocked errors
    BUG=b:184776317
    TEST=Manual
    
    Change-Id: I65251cd2c7b4efd5e0b1cee7fd26fc769c5e59f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811652
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870979}

--- chrome/browser/chromeos/net/shill_error.cc
+++ chrome/browser/chromeos/net/shill_error.cc
@@ -22,6 +22,9 @@ const NetworkState* GetNetworkState(const std::string& network_id) {
       ->GetNetworkStateFromGuid(network_id);
 }
 
+// TODO(b:184776317): Use string from service_constants.h
+const char kErrorSimLocked[] = "sim-locked";
+
 }  // namespace
 
 namespace shill_error {
@@ -108,6 +111,9 @@ std::u16string GetShillErrorString(const std::string& error,
   }
   if (error == shill::kErrorNotAuthenticated) {
     return l10n_util::GetStringUTF16(IDS_CHROMEOS_NETWORK_ERROR_BAD_PASSPHRASE);
+  }
+  if (error == kErrorSimLocked) {
+    return l10n_util::GetStringUTF16(IDS_NETWORK_LIST_SIM_CARD_LOCKED);
   }
 
   if (base::ToLowerASCII(error) == base::ToLowerASCII(shill::kUnknownString)) {
