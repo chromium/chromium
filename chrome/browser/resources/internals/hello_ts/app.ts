8b0724cf446c837bb8b5d7c26652491dc3e0e215
dpapad <dpapad@chromium.org>
2021-04-09 08:54:34 +0000

    WebUI: Modify TypeScript demo page to use Polymer3.
    Specifically:
     - Defining a new ts-app element that resides in a TypeScript
       file and depends on polymer_bundled.min.js
     - Updating ts_library()'s default path mappings to correctly
       map polymer_bundled.min.js to a TS definition file.
    
    The following target
    chrome/browser/resources/internals/hello_ts:build
    
    builds successfully indicating that the TypeScript compiler
    understands the Polymer definition files.
    
    Bug: 1189595
    Change-Id: Ib699e5c24d1236f08412fd00eb845d3fca7fe622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811319
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870894}

---
+++ chrome/browser/resources/internals/hello_ts/app.ts
@@ -0,0 +1,70 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chrome://resources/js/assert.m.js';
+import {helloWorld} from 'chrome://resources/js/hello_world.js';
+import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
+import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export class TsAppElement extends PolymerElement {
+  static get is() {
+    return 'ts-app';
+  }
+
+  static get template() {
+    return html`<div>[[message]]</div>`;
+  }
+
+  static get properties() {
+    return {
+      message: String,
+    };
+  }
+
+  private message: string;
+
+  constructor() {
+    super();
+
+    // Try helloWorld().
+    this.message = helloWorld() + ' from TypeScript!';
+
+    // <if expr="chromeos">
+    this.message = helloWorld() + ' from CrOS TypeScript!';
+    // </if>
+
+    // Try assert, getDeepActiveElement.
+    assert(getDeepActiveElement() === document.body);
+
+    // Try PromiseResolver with a string.
+    const stringResolver = new PromiseResolver<string>();
+    stringResolver.promise.then((result: string) => {
+      assert(result.includes('done'));
+    });
+    stringResolver.resolve('done');
+
+    // Try PromiseResolver with a number.
+    const numberResolver = new PromiseResolver<number>();
+    numberResolver.promise.then((result: number) => {
+      assert(Math.min(result, 0) === 0);
+    });
+    numberResolver.resolve(5);
+  }
+
+  ready() {
+    super.ready();
+    console.log(this.message);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+}
+
+customElements.define(TsAppElement.is, TsAppElement);
