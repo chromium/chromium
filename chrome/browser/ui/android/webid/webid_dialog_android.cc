6fdcdf47a603e02d6f4dd4027d73adec34fd4d65
Majid Valipour <majidvp@chromium.org>
2021-04-09 20:49:25 +0000

    [WebID] Add a stub for Android UI
    The throttle now invokes the UI code. So simply enabling the flag and
    going to a site that triggers an OpenID request would now cause a crash
    on android.
    
    To prevent such crashes, this CL adds a very basic WebIU UI stub on
    android.
    
    Test: Manually go to stackoverflow.com and try to trigger OAuth login
          with WebID flag enabled.
    
    Bug: 1195764
    Change-Id: I41ddce8c71873eb214aebd012af004cb9d86b61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816018
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871102}

--- chrome/browser/ui/android/webid/webid_dialog_android.cc
+++ chrome/browser/ui/android/webid/webid_dialog_android.cc
@@ -2,16 +2,59 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/bind.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "chrome/browser/ui/webid/webid_dialog.h"
-
-// Stub implementations for Identity UI on Android.
 
 namespace content {
 class WebContents;
 }  // namespace content
 
+// A stub implementation for WebID Dialog for Android.
+//
+// The dialog simply accepts various permissions and shows no UI. This is here
+// to avoid crash when WebID is used on Android.
+class WebIdDialogAndroidStub : public WebIdDialog {
+ public:
+  explicit WebIdDialogAndroidStub(content::WebContents* rp_web_contents)
+      : WebIdDialog(rp_web_contents) {}
+
+  WebIdDialogAndroidStub(const WebIdDialogAndroidStub&) = delete;
+  WebIdDialogAndroidStub operator=(const WebIdDialogAndroidStub&) = delete;
+  ~WebIdDialogAndroidStub() override {}
+
+  void ShowInitialPermission(const std::u16string& idp_hostname,
+                             const std::u16string& rp_hostname,
+                             PermissionCallback callback) override {
+    // Post a task to run the callback because FederatedAuthNavigationThrottle
+    // currently assumes these callbacks are run asynchronously.
+    base::SequencedTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindOnce(std::move(callback), UserApproval::kApproved));
+  }
+  void ShowTokenExchangePermission(const std::u16string& idp_hostname,
+                                   const std::u16string& rp_hostname,
+                                   PermissionCallback callback) override {
+    // Post a task to run the callback because FederatedAuthNavigationThrottle
+    // currently assumes these callbacks are run asynchronously.
+    base::SequencedTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindOnce(std::move(callback), UserApproval::kApproved));
+    delete this;
+  }
+  void ShowSigninPage(content::WebContents* idp_web_contents,
+                      const GURL& idp_signin_url,
+                      CloseCallback callback) override {
+    close_callback_ = std::move(callback);
+  }
+  void CloseSigninPage() override { std::move(close_callback_).Run(); }
+
+ private:
+  CloseCallback close_callback_;
+};
+
 // static
 WebIdDialog* WebIdDialog::Create(content::WebContents* rp_web_contents) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  // This instance gets deleted once token exchange permission is called.
+  return new WebIdDialogAndroidStub(rp_web_contents);
 }
