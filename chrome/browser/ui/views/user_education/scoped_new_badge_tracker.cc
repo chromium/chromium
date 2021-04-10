23d2969beba110109c01c925ae3eb6b9ff9ee26e
Dana Fried <dfried@chromium.org>
2021-04-10 03:01:04 +0000

    Scoped feature_engagement::Tracker when using it with the New Badge.
    This somewhat simplifies using Feature Engagement to control when and
    how often the user sees a New Badge.
    
    Once the Feature Engagement feature for the badge has been configured,
    a ScopedNewBadgeTracker can be added to your menu model or dialog to
    gate the display of the badge and correctly record its usage and the
    user's engagement with the promoted feature.
    
    Change-Id: Icc1ea01133d4fec7f97ff16167d565eaf848f654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815649
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871216}

---
+++ chrome/browser/ui/views/user_education/scoped_new_badge_tracker.cc
@@ -0,0 +1,46 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/user_education/scoped_new_badge_tracker.h"
+
+#include "base/feature_list.h"
+#include "base/stl_util.h"
+#include "chrome/browser/feature_engagement/tracker_factory.h"
+#include "components/feature_engagement/public/tracker.h"
+
+ScopedNewBadgeTracker::ScopedNewBadgeTracker(content::BrowserContext* profile)
+    : tracker_(
+          feature_engagement::TrackerFactory::GetForBrowserContext(profile)) {}
+
+ScopedNewBadgeTracker::~ScopedNewBadgeTracker() {
+  for (const base::Feature* new_badge_feature : active_badge_features_)
+    tracker_->Dismissed(*new_badge_feature);
+}
+
+bool ScopedNewBadgeTracker::TryShowNewBadge(
+    const base::Feature& badge_feature,
+    const base::Feature* promoted_feature) {
+  // In the event of a submenu that the user could open multiple times while
+  // navigating the same top-level menu, and we don't want to count those as
+  // separate times the user sees the New Badge:
+  if (base::Contains(active_badge_features_, &badge_feature))
+    return true;
+
+  // If there is no tracker available or the feature being promoted is disabled,
+  // do not show the New Badge.
+  if (!tracker_)
+    return false;
+  if (promoted_feature && !base::FeatureList::IsEnabled(*promoted_feature))
+    return false;
+
+  const bool result = tracker_->ShouldTriggerHelpUI(badge_feature);
+  if (result)
+    active_badge_features_.insert(&badge_feature);
+  return result;
+}
+
+void ScopedNewBadgeTracker::ActionPerformed(const char* event_name) {
+  if (tracker_)
+    tracker_->NotifyEvent(event_name);
+}
