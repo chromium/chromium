9249238baa234626c76d93be35bdf70b798d9db0
Russ Hamilton <behamilton@google.com>
2021-04-10 01:04:27 +0000

    Add interest group persistent storage for FLEDGE.
    The interest group state is stored in an SQLite database. This database
    is persisted to disk in the user profile directory in the “InterestGroups”
    file.
    
    Bug: 1186444
    Change-Id: Iee8408a1776f85f996bd202be47c71b0f3587a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757425
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871191}

---
+++ content/browser/interest_group/interest_group_storage.h
@@ -0,0 +1,81 @@
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_INTEREST_GROUP_INTEREST_GROUP_STORAGE_H_
+#define CONTENT_BROWSER_INTEREST_GROUP_INTEREST_GROUP_STORAGE_H_
+
+#include "base/files/file_path.h"
+#include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
+#include "base/thread_annotations.h"
+#include "content/common/content_export.h"
+#include "content/services/auction_worklet/public/mojom/auction_worklet_service.mojom.h"
+#include "sql/database.h"
+#include "sql/statement.h"
+#include "url/gurl.h"
+#include "url/origin.h"
+
+namespace content {
+
+// InterestGroupStorage controls access to the Interest Group Database. All
+// public functions perform operations on the database and may block. This
+// implementation is not thread-safe so all functions should be called from
+// within the same sequence.
+class CONTENT_EXPORT InterestGroupStorage {
+ public:
+  // Constructs an interest group storage based on a SQLite database in the
+  // `path`/InterestGroups file. If the path passed in is empty, then the
+  // database is instead stored in memory and not persisted to disk.
+  explicit InterestGroupStorage(const base::FilePath& path);
+  InterestGroupStorage(const InterestGroupStorage& other) = delete;
+  InterestGroupStorage& operator=(const InterestGroupStorage& other) = delete;
+  ~InterestGroupStorage();
+
+  // Joins an interest group. If the interest group does not exist, a new one
+  // is created based on the provided group information. If the interest group
+  // exists, the existing interest group is overwritten. In either case a join
+  // record for this interest group is created.
+  void JoinInterestGroup(blink::mojom::InterestGroupPtr group);
+  // Remove the interest group if it exists.
+  void LeaveInterestGroup(const url::Origin& owner, const std::string& name);
+  // Updates the interest group of the same name based on the information in
+  // the provided group. This does not update the interest group expiration
+  // time or user bidding signals. Silently fails if the interest group does
+  // not exist.
+  void UpdateInterestGroup(blink::mojom::InterestGroupPtr group);
+  // Adds an entry to the bidding history for this interest group.
+  void RecordInterestGroupBid(const url::Origin& owner,
+                              const std::string& name);
+  // Adds an entry to the win history for this interest group. `ad_json` is a
+  // piece of opaque data to identify the winning ad.
+  void RecordInterestGroupWin(const url::Origin& owner,
+                              const std::string& name,
+                              const std::string& ad_json);
+  // Gets a list of all interest group owners. Each owner will only appear
+  // once.
+  std::vector<::url::Origin> GetAllInterestGroupOwners();
+  // Gets a list of all interest groups with their bidding information
+  // associated with the provided owner.
+  std::vector<auction_worklet::mojom::BiddingInterestGroupPtr>
+  GetInterestGroupsForOwner(const url::Origin& owner);
+
+  // Clear out storage for the specified owning origin. If the origin is opaque
+  // then apply to all origins.
+  void DeleteInterestGroupData(const url::Origin& owner);
+
+ private:
+  bool EnsureDBInitialized();
+  bool InitializeDB();
+  bool InitializeSchema();
+  void DatabaseErrorCallback(int extended_error, sql::Statement* stmt);
+
+  std::unique_ptr<sql::Database> db_ GUARDED_BY_CONTEXT(sequence_checker_);
+  const base::FilePath path_to_database_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_INTEREST_GROUP_INTEREST_GROUP_STORAGE_H_
---
+++ content/browser/interest_group/interest_group_storage_unittest.cc
@@ -0,0 +1,263 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/interest_group/interest_group_storage.h"
+
+#include <functional>
+#include <memory>
+
+#include "base/bind.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
+#include "base/test/simple_test_clock.h"
+#include "base/time/time.h"
+#include "sql/database.h"
+#include "sql/test/scoped_error_expecter.h"
+#include "sql/test/test_helpers.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/origin.h"
+
+namespace content {
+
+using ::auction_worklet::mojom::BiddingInterestGroupPtr;
+using ::blink::mojom::InterestGroupPtr;
+
+namespace {
+InterestGroupPtr NewInterestGroup(url::Origin owner, std::string name) {
+  InterestGroupPtr result = blink::mojom::InterestGroup::New();
+  result->owner = owner;
+  result->name = name;
+  return result;
+}
+}  // namespace
+
+class InterestGroupStorageTest : public testing::Test {
+ public:
+  InterestGroupStorageTest() = default;
+
+  void SetUp() override { ASSERT_TRUE(temp_directory_.CreateUniqueTempDir()); }
+
+  std::unique_ptr<InterestGroupStorage> CreateStorage() {
+    return std::make_unique<InterestGroupStorage>(temp_directory_.GetPath());
+  }
+
+  base::FilePath db_path() {
+    return temp_directory_.GetPath().Append(
+        FILE_PATH_LITERAL("InterestGroups"));
+  }
+
+ private:
+  base::ScopedTempDir temp_directory_;
+};
+
+TEST_F(InterestGroupStorageTest, DatabaseInitialized_CreateDatabase) {
+  base::HistogramTester histograms;
+
+  EXPECT_FALSE(base::PathExists(db_path()));
+
+  { std::unique_ptr<InterestGroupStorage> storage = CreateStorage(); }
+
+  // InterestGroupStorageSqlImpl opens the database lazily.
+  EXPECT_FALSE(base::PathExists(db_path()));
+
+  {
+    std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+    ::url::Origin test_origin =
+        ::url::Origin::Create(GURL("https://owner.example.com"));
+    storage->LeaveInterestGroup(test_origin, "example");
+  }
+
+  // InterestGroupStorage creates the database if it doesn't exist.
+  EXPECT_TRUE(base::PathExists(db_path()));
+
+  {
+    sql::Database raw_db;
+    EXPECT_TRUE(raw_db.Open(db_path()));
+
+    // [interest_groups], [join_history], [bid_history], [win_history], [meta].
+    EXPECT_EQ(4u, sql::test::CountSQLTables(&raw_db));
+  }
+}
+
+TEST_F(InterestGroupStorageTest, DatabaseJoin) {
+  url::Origin test_origin =
+      url::Origin::Create(GURL("https://owner.example.com"));
+  {
+    std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+    storage->JoinInterestGroup(NewInterestGroup(test_origin, "example"));
+  }
+  {
+    std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+    std::vector<url::Origin> origins = storage->GetAllInterestGroupOwners();
+    EXPECT_EQ(1u, origins.size());
+    EXPECT_EQ(test_origin, origins[0]);
+    std::vector<BiddingInterestGroupPtr> interest_groups =
+        storage->GetInterestGroupsForOwner(test_origin);
+    EXPECT_EQ(1u, interest_groups.size());
+    EXPECT_EQ(test_origin, interest_groups[0]->group->owner);
+    EXPECT_EQ("example", interest_groups[0]->group->name);
+    EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+    EXPECT_EQ(0, interest_groups[0]->signals->bid_count);
+  }
+}
+
+// Test that joining and interest group twice increments the counter.
+// Test that joining multiple interest groups with the same owner only creates a
+// single distinct owner. Test that leaving one interest group does not affect
+// membership of other interest groups by the same owner.
+TEST_F(InterestGroupStorageTest, JoinJoinLeave) {
+  url::Origin test_origin =
+      url::Origin::Create(GURL("https://owner.example.com"));
+  std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+
+  storage->JoinInterestGroup(NewInterestGroup(test_origin, "example"));
+  storage->JoinInterestGroup(NewInterestGroup(test_origin, "example"));
+
+  std::vector<url::Origin> origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(1u, origins.size());
+  EXPECT_EQ(test_origin, origins[0]);
+
+  std::vector<BiddingInterestGroupPtr> interest_groups =
+      storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(2, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(0, interest_groups[0]->signals->bid_count);
+
+  storage->JoinInterestGroup(NewInterestGroup(test_origin, "example2"));
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(2u, interest_groups.size());
+
+  origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(1u, origins.size());
+  EXPECT_EQ(test_origin, origins[0]);
+
+  storage->LeaveInterestGroup(test_origin, "example");
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example2", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(0, interest_groups[0]->signals->bid_count);
+
+  origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(1u, origins.size());
+  EXPECT_EQ(test_origin, origins[0]);
+}
+
+TEST_F(InterestGroupStorageTest, BidCount) {
+  url::Origin test_origin =
+      url::Origin::Create(GURL("https://owner.example.com"));
+  std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+
+  storage->JoinInterestGroup(NewInterestGroup(test_origin, "example"));
+
+  std::vector<url::Origin> origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(1u, origins.size());
+  EXPECT_EQ(test_origin, origins[0]);
+
+  std::vector<BiddingInterestGroupPtr> interest_groups =
+      storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(0, interest_groups[0]->signals->bid_count);
+
+  storage->RecordInterestGroupBid(test_origin, "example");
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(1, interest_groups[0]->signals->bid_count);
+
+  storage->RecordInterestGroupBid(test_origin, "example");
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(2, interest_groups[0]->signals->bid_count);
+}
+
+TEST_F(InterestGroupStorageTest, RecordsWins) {
+  url::Origin test_origin =
+      url::Origin::Create(GURL("https://owner.example.com"));
+  GURL ad1_url = GURL("http://owner.example.com/ad1");
+  GURL ad2_url = GURL("http://owner.example.com/ad2");
+  std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+
+  storage->JoinInterestGroup(NewInterestGroup(test_origin, "example"));
+
+  std::vector<url::Origin> origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(1u, origins.size());
+  EXPECT_EQ(test_origin, origins[0]);
+
+  std::vector<BiddingInterestGroupPtr> interest_groups =
+      storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(0, interest_groups[0]->signals->bid_count);
+
+  std::string ad1_json = "{url: '" + ad1_url.spec() + "'}";
+  storage->RecordInterestGroupBid(test_origin, "example");
+  storage->RecordInterestGroupWin(test_origin, "example", ad1_json);
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(1, interest_groups[0]->signals->bid_count);
+
+  std::string ad2_json = "{url: '" + ad2_url.spec() + "'}";
+  storage->RecordInterestGroupBid(test_origin, "example");
+  storage->RecordInterestGroupWin(test_origin, "example", ad2_json);
+
+  interest_groups = storage->GetInterestGroupsForOwner(test_origin);
+  EXPECT_EQ(1u, interest_groups.size());
+  EXPECT_EQ("example", interest_groups[0]->group->name);
+  EXPECT_EQ(1, interest_groups[0]->signals->join_count);
+  EXPECT_EQ(2, interest_groups[0]->signals->bid_count);
+  EXPECT_EQ(2u, interest_groups[0]->signals->prev_wins.size());
+  // Ad wins should be listed in reverse chronological order.
+  EXPECT_EQ(ad2_json, interest_groups[0]->signals->prev_wins[0]->ad_json);
+  EXPECT_EQ(ad1_json, interest_groups[0]->signals->prev_wins[1]->ad_json);
+
+  // Try delete
+  storage->DeleteInterestGroupData(test_origin);
+
+  origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(0u, origins.size());
+}
+
+TEST_F(InterestGroupStorageTest, DeleteOriginDeleteAll) {
+  std::vector<::url::Origin> test_origins = {
+      ::url::Origin::Create(GURL("https://owner.example.com")),
+      ::url::Origin::Create(GURL("https://owner2.example.com")),
+      ::url::Origin::Create(GURL("https://owner3.example.com")),
+  };
+  std::unique_ptr<InterestGroupStorage> storage = CreateStorage();
+  for (const auto& origin : test_origins)
+    storage->JoinInterestGroup(NewInterestGroup(origin, "example"));
+
+  std::vector<url::Origin> origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(3u, origins.size());
+
+  storage->DeleteInterestGroupData(test_origins[0]);
+
+  origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(2u, origins.size());
+
+  storage->DeleteInterestGroupData(::url::Origin());
+
+  origins = storage->GetAllInterestGroupOwners();
+  EXPECT_EQ(0u, origins.size());
+}
+
+}  // namespace content
