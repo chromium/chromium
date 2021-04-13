e7986e13e30ccd5ad365ae3ffafc518ad9a9e279
Camillo Bruni <cbruni@chromium.org>
2021-04-13 13:20:37 +0000

    [cluster-telemetry] Fix v8.loading_runtime_stats.cluster_telemetry
    https://crrev.com/c/2684363 moved a helper function that was used by
    the v8 cluster telemetry benchmarks.
    
    Change-Id: I0dcabf9c8fccc1da5196444bb192647dc057e8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822253
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Ravi Mistry <rmistry@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871917}

--- tools/perf/contrib/cluster_telemetry/v8_loading_ct.py
+++ tools/perf/contrib/cluster_telemetry/v8_loading_ct.py
@@ -1,9 +1,10 @@
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-from benchmarks import v8_browsing
+from benchmarks import v8_helper
 from contrib.cluster_telemetry import loading_base_ct
 from telemetry.web_perf import timeline_based_measurement
+
 
 # pylint: disable=protected-access
 class V8LoadingClusterTelemetry(loading_base_ct._LoadingBaseClusterTelemetry):
@@ -13,6 +14,6 @@ class V8LoadingClusterTelemetry(loading_base_ct._LoadingBaseClusterTelemetry):
 
   def CreateCoreTimelineBasedMeasurementOptions(self):
     options = timeline_based_measurement.Options()
-    v8_browsing.AugmentOptionsForV8BrowsingMetrics(options,
-        enable_runtime_call_stats=False)
+    v8_helper.AugmentOptionsForV8Metrics(options,
+                                         enable_runtime_call_stats=False)
     return options
