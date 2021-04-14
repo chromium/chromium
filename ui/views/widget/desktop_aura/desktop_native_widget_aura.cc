8d6dabf68c3b6f2cef942f3867195a48d02f78d1
Alexander Zhirov <a.zhirov@corp.mail.ru>
2021-04-14 04:57:13 +0000

    Fix OnDeviceScaleFactorChanged isn't called in views::View
    Added a missing function call for
    DesktopNativeWidgetAura::OnDeviceScaleFactorChanged.
    
    Bug: 1197225
    Change-Id: I253eee6cf3b1b3181562e38f193a6683b14411c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821824
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872282}

--- ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -1096,7 +1096,10 @@ void DesktopNativeWidgetAura::OnPaint(const ui::PaintContext& context) {
 
 void DesktopNativeWidgetAura::OnDeviceScaleFactorChanged(
     float old_device_scale_factor,
-    float new_device_scale_factor) {}
+    float new_device_scale_factor) {
+  GetWidget()->DeviceScaleFactorChanged(old_device_scale_factor,
+                                        new_device_scale_factor);
+}
 
 void DesktopNativeWidgetAura::OnWindowDestroying(aura::Window* window) {
   // Cleanup happens in OnHostClosed().
