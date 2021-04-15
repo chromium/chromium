15e4187faccc03982feb56123298999cb531bb90
Jim Van Verth <jvanverth@google.com>
2021-04-15 14:33:39 +0000

    Metal: disable test shader
    We have been running this test to diagnose browser startup time issues
    with Metal. There are some issues with it, so disable it for now.
    
    Bug: 1198385
    Change-Id: Id4b688843500c94d96ac8b076cf75e1221293aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826594
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Cr-Commit-Position: refs/heads/master@{#872826}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -185,33 +185,6 @@ class ContentSandboxHelper : public gpu::GpuSandboxHelper {
   DISALLOW_COPY_AND_ASSIGN(ContentSandboxHelper);
 };
 
-#if defined(OS_MAC)
-void TestShaderCallback(metal::TestShaderComponent component,
-                        metal::TestShaderResult result,
-                        const base::TimeDelta& callback_time) {
-  switch (result) {
-    case metal::TestShaderResult::kNotAttempted:
-    case metal::TestShaderResult::kFailed:
-      // Don't include data if no Metal device was created (e.g, due to hardware
-      // or macOS version reasons).
-      return;
-    case metal::TestShaderResult::kTimedOut:
-      break;
-    case metal::TestShaderResult::kSucceeded:
-      break;
-  }
-  switch (component) {
-    case metal::TestShaderComponent::kCompile:
-      UMA_HISTOGRAM_MEDIUM_TIMES("Gpu.Metal.TestShaderCompileTime",
-                                 callback_time);
-      break;
-    case metal::TestShaderComponent::kLink:
-      UMA_HISTOGRAM_MEDIUM_TIMES("Gpu.Metal.TestShaderLinkTime", callback_time);
-      break;
-  }
-}
-#endif
-
 }  // namespace
 
 // Main function for starting the Gpu process.
@@ -438,11 +411,6 @@ int GpuMain(const MainFunctionParams& parameters) {
   // process exits, it appears that the browser process is no longer considered
   // to be using the GPU, so it "succeeds" the 'wait'.
   metal::RegisterGracefulExitOnDeviceRemoval();
-
-  // Launch a test metal shader compile to see how long it takes to complete (if
-  // it ever completes).
-  // https://crbug.com/974219
-  metal::TestShader(base::BindOnce(TestShaderCallback));
 #endif
 
 #if defined(OS_ANDROID)
