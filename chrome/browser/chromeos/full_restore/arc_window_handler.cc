6ec219bcd6908e54254c3bd44f0f7350baeb848e
Shengsong Tan <sstan@google.com>
2021-04-15 07:10:22 +0000

    full_restore: Support bounds scale for ARC apps
    This CL is used only for single screen case, and only for ARC R and
    above.
    Will change to a general way after figuring out how to do it.
    
    BUG=b:177405617
    
    Change-Id: I160fafac18c793b5c8e3d624f162cbeee1694e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826002
    Commit-Queue: Shengsong Tan <sstan@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872746}

--- chrome/browser/chromeos/full_restore/arc_window_handler.cc
+++ chrome/browser/chromeos/full_restore/arc_window_handler.cc
@@ -33,8 +33,9 @@ apps::mojom::WindowInfoPtr ConvertToArcBounds(
     window_info->bounds.reset();
     return window_info;
   }
-  // TODO(sstan): Add position adjustment after specify how to conversion
-  // to ARC bounds.
+  // TODO(sstan): Only for single screen. Need to find a general way.
+  window_info->bounds->x *= scale_factor;
+  window_info->bounds->y *= scale_factor;
   window_info->bounds->width *= scale_factor;
   window_info->bounds->height *= scale_factor;
   return window_info;
