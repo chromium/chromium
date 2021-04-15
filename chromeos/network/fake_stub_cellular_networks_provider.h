d21bc639824c0e3bd9773faa43f23c61dbcbd1f5
Kyle Horimoto <khorimoto@chromium.org>
2021-04-15 23:44:18 +0000

    [CrOS Cellular] Add FakeStubCellularNetworksProvider
    Provides more flexibility for testing the adding/removal of stub
    cellular networks. Needed for follow-up CLs which require processing
    transitions to different network identifiers.
    
    This CL also updates the existing NetworkStateHandler test to use this
    class.
    
    (cherry picked from commit b01bc0a060468ce18abbbffe45eaf38a7e40c268)
    
    Bug: 1197315, 1198393
    Change-Id: I8fa3c43a99f17aa7a20c95492cb32afa12fb0d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822122
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#871935}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2828783
    Auto-Submit: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4472@{#130}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

---
+++ chromeos/network/fake_stub_cellular_networks_provider.h
@@ -0,0 +1,52 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_NETWORK_FAKE_STUB_CELLULAR_NETWORKS_PROVIDER_H_
+#define CHROMEOS_NETWORK_FAKE_STUB_CELLULAR_NETWORKS_PROVIDER_H_
+
+#include <string>
+#include <utility>
+
+#include "base/containers/flat_set.h"
+#include "chromeos/network/network_state_handler.h"
+
+namespace chromeos {
+
+// Fake StubCellularNetworksProvider implementation which allows clients to
+// specify ICCIDs of stub networks to be created.
+class COMPONENT_EXPORT(CHROMEOS_NETWORK) FakeStubCellularNetworksProvider
+    : public NetworkStateHandler::StubCellularNetworksProvider {
+ public:
+  FakeStubCellularNetworksProvider();
+  ~FakeStubCellularNetworksProvider() override;
+
+  // Adds a stub network with the provided ICCID and EID. This network will be
+  // used when a subsequent call to AddOrRemoveStubCellularNetworks() is made.
+  // Note that an empty EID refers to a pSIM network.
+  void AddStub(const std::string& stub_iccid,
+               const std::string& eid = std::string());
+
+  // Removes a stub network with the provided ICCID and EID, reversing a
+  // previous call to AddStub().
+  void RemoveStub(const std::string& stub_iccid,
+                  const std::string& eid = std::string());
+
+ private:
+  using IccidEidPair = std::pair<std::string, std::string>;
+
+  // NetworkStateHandler::StubCellularNetworksProvider:
+  bool AddOrRemoveStubCellularNetworks(
+      NetworkStateHandler::ManagedStateList& network_list,
+      NetworkStateHandler::ManagedStateList& new_stub_networks,
+      const DeviceState* device) override;
+
+  std::vector<IccidEidPair> GetStubsNotBackedByShill(
+      const NetworkStateHandler::ManagedStateList& network_list) const;
+
+  base::flat_set<IccidEidPair> stub_iccid_and_eid_pairs_;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_NETWORK_FAKE_STUB_CELLULAR_NETWORKS_PROVIDER_H_
