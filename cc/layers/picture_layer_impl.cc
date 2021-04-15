21722d52e27fe8f3edf253ea51096181cbdd7550
Oriol Brufau <obrufau@igalia.com>
2021-04-15 18:44:24 +0000

    [cc] Avoid rasterizing too often after r872117
    When adding support for 2D scales, r872117 did this change:
    
    -  ideal_source_scale_ =
    -      ideal_contents_scale_ / ideal_page_scale_ / ideal_device_scale_;
    +  ideal_source_scale_ = ideal_contents_scale_;
    +  ideal_source_scale_.Scale(1 / ideal_page_scale_ / ideal_device_scale_);
    
    Using Scale() was nice since it "divided" both components together, but
    due to precision limitations, multiplying by the inverse is not the same
    as dividing. Thus it would produce a slightly wrong ideal_source_scale_,
    which would end up causing PictureLayerImpl::ShouldAdjustRasterScale to
    return true more often.
    
    The solution is using a division for each component.
    
    Bug: 1198914, 1119996
    
    Change-Id: I3782a6637adbea4f3a30131be1a2d21e3d6fbd86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825807
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#872973}

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -1814,8 +1814,9 @@ void PictureLayerImpl::UpdateIdealScales() {
       gfx::Vector2dF(min_contents_scale, min_contents_scale));
   ideal_contents_scale_.SetToMin(
       gfx::Vector2dF(kMaxIdealContentsScale, kMaxIdealContentsScale));
-  ideal_source_scale_ = ideal_contents_scale_;
-  ideal_source_scale_.Scale(1 / ideal_page_scale_ / ideal_device_scale_);
+  ideal_source_scale_ = {
+      ideal_contents_scale_.x() / ideal_page_scale_ / ideal_device_scale_,
+      ideal_contents_scale_.y() / ideal_page_scale_ / ideal_device_scale_};
 }
 
 void PictureLayerImpl::GetDebugBorderProperties(
