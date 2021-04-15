a7da67b320051627328ab176d862e016439f3e75
Ryan Keane <rwkeane@google.com>
2021-04-15 18:32:59 +0000

    Add Interface to be implemented in internal Chromecast repo
    This CL defined the CastRuntimeService interface. It is to be used for
    building the Cast Runtime Service inside internal Chromecast code, and
    it will act as the border between public Chromium and this internal
    implementation to provide a mechanism through which internal constants
    and implementations of public Chromium types available during an
    internally-built executable's runtime may be accessed.
    
    NOTE: This interface is intended to wrap an existing internal class:
    go/castruntimeservicewrapper
    
    Change-Id: I1aeaf69c824fe491051ffc47dd21e027dced83f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827503
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/master@{#872966}

---
+++ chromecast/cast_core/cast_runtime_service.cc
@@ -0,0 +1,15 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/cast_core/cast_runtime_service.h"
+
+namespace chromecast {
+namespace media {
+
+CastRuntimeService::CastRuntimeService() = default;
+
+CastRuntimeService::~CastRuntimeService() = default;
+
+}  // namespace media
+}  // namespace chromecast
---
+++ chromecast/cast_core/cast_runtime_service.h
@@ -0,0 +1,49 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_CAST_CORE_CAST_RUNTIME_SERVICE_H_
+#define CHROMECAST_CAST_CORE_CAST_RUNTIME_SERVICE_H_
+
+#include <memory>
+
+#include "chromecast/media/cma/backend/proxy/cast_runtime_audio_channel_endpoint_manager.h"
+#include "chromecast/service/cast_service.h"
+
+namespace chromecast {
+
+class WebCryptoServer;
+
+namespace receiver {
+class MediaManager;
+}  // namespace receiver
+
+namespace media {
+
+// This interface is to be used for building the Cast Runtime Service and act as
+// the border between shared Chromium code and the specifics of that
+// implementation.
+//
+// NOTE: When adding a new interface to this class, first add it to all
+// implementations of this interface in downstream repos. Else, the roll of this
+// code into those repos will break.
+class CastRuntimeService : public CastService,
+                           public CastRuntimeAudioChannelEndpointManager {
+ public:
+  // Returns current instance of CastRuntimeService in the browser process.
+  // TODO(rwkeane): After dependent repos have implemented this interface,
+  // implement it in public Chromium as-well. Do not implement before then, to
+  // ensure that integration cross-repo remains relatively simple.
+  static CastRuntimeService* GetInstance();
+
+  CastRuntimeService();
+  ~CastRuntimeService() override;
+
+  virtual WebCryptoServer* GetWebCryptoServer() = 0;
+  virtual receiver::MediaManager* GetMediaManager() = 0;
+};
+
+}  // namespace media
+}  // namespace chromecast
+
+#endif  // CHROMECAST_CAST_CORE_CAST_RUNTIME_SERVICE_H_
