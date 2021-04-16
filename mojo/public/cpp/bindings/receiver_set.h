df981dc4d6068439085cfee5609e23d2b21940c0
Ken Rockot <rockot@google.com>
2021-04-16 19:00:43 +0000

    Never fail in ReceiverSet::Add
    Because of how UniqueReceiverSet is implemented and used, it is
    dangerous to allow Add() to fail: callers reasonably assume that added
    objects are still alive immediately after the Add() call.
    
    This changes ReceiverId to a uint64 and simply CHECK-fails on
    insert collision.
    
    This fundamentally increases binary size of 32-bit builds, because
    a widely used 32-bit data type is expanding to 64 bits for the sake
    of security and stability. It is effectively unavoidable for now, and
    also just barely above the tolerable threshold.
    
    A follow-up (but less backwards-mergeable) change should be able to
    reduce binary size beyond this increase by consolidating shared
    code among ReceiverSet template instantiations.
    
    (cherry picked from commit 668cf831e91210d4f23e815e07ff1421f3ee9747)
    
    Fixed: 1185732
    Change-Id: I9acf6aaaa36e10fdce5aa49a890173caddc13c52
    Binary-Size: Unavoidable (see above)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778871
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: Ken Rockot <rockot@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#865815}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822154
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Owners-Override: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4240@{#1610}
    Cr-Branched-From: f297677702651916bbf65e59c0d4bbd4ce57d1ee-refs/heads/master@{#800218}

--- mojo/public/cpp/bindings/receiver_set.h
+++ mojo/public/cpp/bindings/receiver_set.h
@@ -24,7 +24,7 @@
 
 namespace mojo {
 
-using ReceiverId = size_t;
+using ReceiverId = uint64_t;
 
 template <typename ReceiverType>
 struct ReceiverSetTraits;
@@ -359,11 +359,11 @@ class ReceiverSetBase {
                      Context context,
                      scoped_refptr<base::SequencedTaskRunner> task_runner) {
     ReceiverId id = next_receiver_id_++;
-    DCHECK_GE(next_receiver_id_, 0u);
     auto entry =
         std::make_unique<Entry>(std::move(impl), std::move(receiver), this, id,
                                 std::move(context), std::move(task_runner));
-    receivers_.insert(std::make_pair(id, std::move(entry)));
+    auto result = receivers_.insert(std::make_pair(id, std::move(entry)));
+    CHECK(result.second) << "ReceiverId overflow with collision";
     return id;
   }
 
