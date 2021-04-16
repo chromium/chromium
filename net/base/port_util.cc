d05d4c35dfa87a9da7cf376474dea7c458e129d3
Adam Rice <ricea@chromium.org>
2021-04-16 17:18:53 +0000

    Block port 10080
    See Fetch Standard issue https://github.com/whatwg/fetch/issues/1191
    for justification and background, and
    https://groups.google.com/a/chromium.org/g/blink-dev/c/CNpLNXx2wf0/m/Ehi3cx1YAQAJ
    for the intent-to-ship thread.
    
    BUG=1197299
    
    (cherry picked from commit d6e884b99ffa8ad5f85ae13c5aa78dd297572f19)
    
    Change-Id: Ie664726307fef0d8578ffbaea9793b74d722b58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814044
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#871865}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830519
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#151}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- net/base/port_util.cc
+++ net/base/port_util.cc
@@ -66,83 +66,84 @@ void LogSlipstreamRestrictedPort(int port) {
 // When adding a port to the list, consider also adding it to kAllowablePorts,
 // below.
 const int kRestrictedPorts[] = {
-    1,     // tcpmux
-    7,     // echo
-    9,     // discard
-    11,    // systat
-    13,    // daytime
-    15,    // netstat
-    17,    // qotd
-    19,    // chargen
-    20,    // ftp data
-    21,    // ftp access
-    22,    // ssh
-    23,    // telnet
-    25,    // smtp
-    37,    // time
-    42,    // name
-    43,    // nicname
-    53,    // domain
-    69,    // tftp
-    77,    // priv-rjs
-    79,    // finger
-    87,    // ttylink
-    95,    // supdup
-    101,   // hostriame
-    102,   // iso-tsap
-    103,   // gppitnp
-    104,   // acr-nema
-    109,   // pop2
-    110,   // pop3
-    111,   // sunrpc
-    113,   // auth
-    115,   // sftp
-    117,   // uucp-path
-    119,   // nntp
-    123,   // NTP
-    135,   // loc-srv /epmap
-    137,   // netbios
-    139,   // netbios
-    143,   // imap2
-    161,   // snmp
-    179,   // BGP
-    389,   // ldap
-    427,   // SLP (Also used by Apple Filing Protocol)
-    465,   // smtp+ssl
-    512,   // print / exec
-    513,   // login
-    514,   // shell
-    515,   // printer
-    526,   // tempo
-    530,   // courier
-    531,   // chat
-    532,   // netnews
-    540,   // uucp
-    548,   // AFP (Apple Filing Protocol)
-    554,   // rtsp
-    556,   // remotefs
-    563,   // nntp+ssl
-    587,   // smtp (rfc6409)
-    601,   // syslog-conn (rfc3195)
-    636,   // ldap+ssl
-    993,   // ldap+ssl
-    995,   // pop3+ssl
-    1719,  // h323gatestat
-    1720,  // h323hostcall
-    1723,  // pptp
-    2049,  // nfs
-    3659,  // apple-sasl / PasswordServer
-    4045,  // lockd
-    5060,  // sip
-    5061,  // sips
-    6000,  // X11
-    6566,  // sane-port
-    6665,  // Alternate IRC [Apple addition]
-    6666,  // Alternate IRC [Apple addition]
-    6667,  // Standard IRC [Apple addition]
-    6668,  // Alternate IRC [Apple addition]
-    6669,  // Alternate IRC [Apple addition]
-    6697,  // IRC + TLS
+    1,      // tcpmux
+    7,      // echo
+    9,      // discard
+    11,     // systat
+    13,     // daytime
+    15,     // netstat
+    17,     // qotd
+    19,     // chargen
+    20,     // ftp data
+    21,     // ftp access
+    22,     // ssh
+    23,     // telnet
+    25,     // smtp
+    37,     // time
+    42,     // name
+    43,     // nicname
+    53,     // domain
+    69,     // tftp
+    77,     // priv-rjs
+    79,     // finger
+    87,     // ttylink
+    95,     // supdup
+    101,    // hostriame
+    102,    // iso-tsap
+    103,    // gppitnp
+    104,    // acr-nema
+    109,    // pop2
+    110,    // pop3
+    111,    // sunrpc
+    113,    // auth
+    115,    // sftp
+    117,    // uucp-path
+    119,    // nntp
+    123,    // NTP
+    135,    // loc-srv /epmap
+    137,    // netbios
+    139,    // netbios
+    143,    // imap2
+    161,    // snmp
+    179,    // BGP
+    389,    // ldap
+    427,    // SLP (Also used by Apple Filing Protocol)
+    465,    // smtp+ssl
+    512,    // print / exec
+    513,    // login
+    514,    // shell
+    515,    // printer
+    526,    // tempo
+    530,    // courier
+    531,    // chat
+    532,    // netnews
+    540,    // uucp
+    548,    // AFP (Apple Filing Protocol)
+    554,    // rtsp
+    556,    // remotefs
+    563,    // nntp+ssl
+    587,    // smtp (rfc6409)
+    601,    // syslog-conn (rfc3195)
+    636,    // ldap+ssl
+    993,    // ldap+ssl
+    995,    // pop3+ssl
+    1719,   // h323gatestat
+    1720,   // h323hostcall
+    1723,   // pptp
+    2049,   // nfs
+    3659,   // apple-sasl / PasswordServer
+    4045,   // lockd
+    5060,   // sip
+    5061,   // sips
+    6000,   // X11
+    6566,   // sane-port
+    6665,   // Alternate IRC [Apple addition]
+    6666,   // Alternate IRC [Apple addition]
+    6667,   // Standard IRC [Apple addition]
+    6668,   // Alternate IRC [Apple addition]
+    6669,   // Alternate IRC [Apple addition]
+    6697,   // IRC + TLS
+    10080,  // Amanda
 };
 
 base::LazyInstance<std::multiset<int>>::Leaky g_explicitly_allowed_ports =
