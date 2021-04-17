60379e27604e2b901d00322932d8bde7e749921e
Pete Williamson <petewil@chromium.org>
2021-04-17 00:51:46 +0000

    Follow Management - display the sites we are following.
    In the Web Feed Follow Management view, we now get the list of sites
    that the user is following and display them in a recycler view.
    
    Bug: 1197286
    Change-Id: If4aafb0653ed78821f0e59aba692b75dcc4c5c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831074
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873553}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementCoordinator.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementCoordinator.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.chrome.browser.feed.webfeed.R;
@@ -25,18 +26,20 @@ public class FollowManagementCoordinator {
     public FollowManagementCoordinator(Context context) {
         ModelList listItems = new ModelList();
 
-        // Once the adapter is attached to the ListView, there is no need to hold a reference to it.
         SimpleRecyclerViewAdapter adapter = new SimpleRecyclerViewAdapter(listItems);
         adapter.registerType(FollowManagementItemProperties.DEFAULT_ITEM_TYPE,
                 new LayoutViewBuilder<FollowManagementItemView>(R.layout.follow_management_item),
                 FollowManagementItemViewBinder::bind);
 
-        // Inflate the XML.
+        // Inflate the XML for the activity.
         mView = LayoutInflater.from(context).inflate(R.layout.follow_management_activity, null);
         RecyclerView recyclerView = (RecyclerView) mView.findViewById(R.id.follow_management_list);
+        // With the recycler view, we need to explicitly set a layout manager.
+        LinearLayoutManager manager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
 
-        mMediator = new FollowManagementMediator(context, listItems);
+        mMediator = new FollowManagementMediator(context, listItems, adapter);
     }
 
     public View getView() {
--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementMediator.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementMediator.java
@@ -8,11 +8,16 @@ import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+
 import org.chromium.base.Log;
-import org.chromium.chrome.browser.feed.webfeed.R;
+import org.chromium.chrome.browser.feed.webfeed.WebFeedBridge;
+import org.chromium.chrome.browser.feed.webfeed.WebFeedBridge.WebFeedMetadata;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
-import org.chromium.ui.modelutil.ModelListAdapter;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
+
+import java.util.List;
 
 /**
  * The MVC pattern Mediator for the Follow Management activity.
@@ -22,26 +27,41 @@ import org.chromium.ui.modelutil.PropertyModel;
 class FollowManagementMediator {
     private static final String TAG = "FollowManagementMdtr";
     private ModelList mModelList;
+    private Context mContext;
+    private Adapter mAdapter;
 
-    FollowManagementMediator(Context context, ModelList modelList) {
+    FollowManagementMediator(Context context, ModelList modelList, Adapter adapter) {
         mModelList = modelList;
-        // TODO(petewil): Fill the list with data.  We use fake data for now until the service is
-        // ready.
-        PropertyModel activityModel = generateListItem(context, R.string.feed_manage_activity,
-                R.string.feed_manage_activity_description, this::handleClick);
-        // Add the menu items into the menu.
-        mModelList.add(new ModelListAdapter.ListItem(
-                FollowManagementItemProperties.DEFAULT_ITEM_TYPE, activityModel));
+        mContext = context;
+        mAdapter = adapter;
+
+        // TODO(petewil) Make the weebFeedBridge method static.
+        WebFeedBridge webFeedBridge = new WebFeedBridge();
+
+        webFeedBridge.getAllFollowedWebFeeds(this::followedPagesCallback);
+    }
+
+    private void followedPagesCallback(List<WebFeedMetadata> followedPages) {
+        // TODO(petewil) Remove when the feature is complete, for debugging.
+        Log.d(TAG, "followedPagesCallback size " + followedPages.size());
+        if (followedPages.size() < 1) return;
+
+        for (WebFeedMetadata page : followedPages) {
+            String title = page.title;
+            String url = page.visitUrl.getSpec();
+            PropertyModel pageModel = generateListItem(title, url, this::handleClick);
+            mModelList.add(new SimpleRecyclerViewAdapter.ListItem(
+                    FollowManagementItemProperties.DEFAULT_ITEM_TYPE, pageModel));
+        }
+
+        mAdapter.notifyDataSetChanged();
     }
 
     // Adapt for our data items.
-    private PropertyModel generateListItem(
-            Context context, int titleResource, int descriptionResource, OnClickListener listener) {
-        String title = context.getResources().getString(titleResource);
-        String description = context.getResources().getString(descriptionResource);
+    private PropertyModel generateListItem(String title, String url, OnClickListener listener) {
         return new PropertyModel.Builder(FollowManagementItemProperties.ALL_KEYS)
                 .with(FollowManagementItemProperties.TITLE_KEY, title)
-                .with(FollowManagementItemProperties.DESCRIPTION_KEY, description)
+                .with(FollowManagementItemProperties.DESCRIPTION_KEY, url)
                 .with(FollowManagementItemProperties.ON_CLICK_KEY, listener)
                 .build();
     }
