b195a93b28aa88fcdd3e213e6330a6b9569fdaf5
Minoru Chikamune <chikamune@chromium.org>
2021-04-19 04:35:18 +0000

    CodeHealth: Remove use of Value::GetAsInteger
    This change removes the use of Value::GetAsInteger and replaces with
    Value::is_int(), Value::GetInt(), Value::GetIfInt().
    
    Bug: 1187004
    Change-Id: I9e15a8df47d69a6765ba74dc02c179eebc98ccc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830780
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873684}

--- components/language/core/browser/url_language_histogram.cc
+++ components/language/core/browser/url_language_histogram.cc
@@ -26,11 +26,10 @@ const float kDiscountFactor = 0.75f;
 // Gets the sum of the counter for all languages in the histogram.
 int GetCountersSum(const base::DictionaryValue& dict) {
   int sum = 0;
-  int counter_value = 0;
   for (base::DictionaryValue::Iterator itr(dict); !itr.IsAtEnd();
        itr.Advance()) {
-    if (itr.value().GetAsInteger(&counter_value))
-      sum += counter_value;
+    if (itr.value().is_int())
+      sum += itr.value().GetInt();
   }
   return sum;
 }
@@ -39,18 +38,17 @@ int GetCountersSum(const base::DictionaryValue& dict) {
 void DiscountAndCleanCounters(base::DictionaryValue* dict) {
   std::set<std::string> remove_keys;
 
-  int counter_value = 0;
   for (base::DictionaryValue::Iterator itr(*dict); !itr.IsAtEnd();
        itr.Advance()) {
     // Remove languages with invalid or small values.
-    if (!itr.value().GetAsInteger(&counter_value) ||
-        counter_value < (kCutoffRatio * kMaxCountersSum)) {
+    if (!itr.value().is_int() ||
+        itr.value().GetInt() < (kCutoffRatio * kMaxCountersSum)) {
       remove_keys.insert(itr.key());
       continue;
     }
 
     // Discount the value.
-    dict->SetInteger(itr.key(), counter_value * kDiscountFactor);
+    dict->SetInteger(itr.key(), itr.value().GetInt() * kDiscountFactor);
   }
 
   for (const std::string& lang_to_remove : remove_keys)
@@ -67,13 +65,12 @@ std::vector<UrlLanguageHistogram::LanguageInfo> GetAllLanguages(
     return std::vector<UrlLanguageHistogram::LanguageInfo>();
 
   std::vector<UrlLanguageHistogram::LanguageInfo> top_languages;
-  int counter_value = 0;
   for (base::DictionaryValue::Iterator itr(dict); !itr.IsAtEnd();
        itr.Advance()) {
-    if (!itr.value().GetAsInteger(&counter_value))
+    if (!itr.value().is_int())
       continue;
     top_languages.emplace_back(
-        itr.key(), static_cast<float>(counter_value) / counters_sum);
+        itr.key(), static_cast<float>(itr.value().GetInt()) / counters_sum);
   }
   return top_languages;
 }
