6614aed4596ce3edfaf113467e2ad4ad8447b463
Keren Zhu <kerenzhu@chromium.org>
2021-04-19 19:43:50 +0000

    Reland "Handle non-clicker IPH with snooze or dismiss"
    This is a reland of 6e89c81e05bc56577e631942067efaa6ed54cb94
    
    The reverted CL added two pref data that didn't exist before and caused
    crashes. This reland fixes the issue and adds test.
    
    Adds finch iph parameter "x_iph_snooze_non_clicker_policy" which can be
    either of the two options:
    
    "dismiss": dismiss the ignored IPH permanently.
    "long_snooze": snooze the ignored IPH for 14 days.
    
    This is a per-IPH parameter, meaning that IPHs can have their own
    policies independently.
    
    
    Original change's description:
    > Handle non-clicker IPH with snooze or dismiss
    >
    > For a snoozable IPH that is dismissed without user clicking "Got It" or
    > "Remind Me Later" button, suppress it for 14 days (by default) or
    > dismiss it permanently. This can be controlled by finch.
    >
    > Bug: 1190270
    > Change-Id: I428f888ae37746eb6e744ba60cc2d74f55bafb06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2813829
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Reviewed-by: Collin Baker <collinbaker@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#871138}
    
    (cherry picked from commit 261717f2bb081a53c96d8ef900bd41cde7db00e8)
    
    Bug: 1190270
    Change-Id: I09cb33600e4d92e3bb5a59c9cf5ae78954c744d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821631
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#871740}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2836706
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#203}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/browser/ui/user_education/feature_promo_snooze_service.cc
+++ chrome/browser/ui/user_education/feature_promo_snooze_service.cc
@@ -24,25 +24,37 @@ namespace {
 // PrefService path.
 const char kIPHSnoozeDataPath[] = "in_product_help.snoozed_feature";
 
-// Path to the boolean indicates if an IPH is dismissed.
+// Path to the boolean indicates if an IPH was dismissed.
 // in_product_help.snoozed_feature.[iph_name].is_dismissed
-constexpr char kIPHIsFeatureDismissed[] = "is_dismissed";
-// Path to the timestamp an IPH is snoozed.
+constexpr char kIPHIsDismissedPath[] = "is_dismissed";
+// Path to the timestamp an IPH was last shown.
+// in_product_help.snoozed_feature.[iph_name].last_show_time
+constexpr char kIPHLastShowTimePath[] = "last_show_time";
+// Path to the timestamp an IPH was last snoozed.
 // in_product_help.snoozed_feature.[iph_name].last_snooze_time
-constexpr char kIPHSnoozedFeatureTime[] = "last_snooze_time";
+constexpr char kIPHLastSnoozeTimePath[] = "last_snooze_time";
 // Path to the duration of snooze.
 // in_product_help.snoozed_feature.[iph_name].last_snooze_duration
-constexpr char kIPHSnoozedFeatureDuration[] = "last_snooze_duration";
+constexpr char kIPHLastSnoozeDurationPath[] = "last_snooze_duration";
 // Path to the count of how many times this IPH has been snoozed.
 // in_product_help.snoozed_feature.[iph_name].snooze_count
-constexpr char kIPHSnoozedFeatureCount[] = "snooze_count";
+constexpr char kIPHSnoozeCountPath[] = "snooze_count";
+// Path to the count of how many times this IPH has been shown.
+// in_product_help.snoozed_feature.[iph_name].show_count
+constexpr char kIPHShowCountPath[] = "show_count";
 
 // Finch parameter to control the snooze duration.
 // If this parameter is not specified or is zero, the default duration at the
 // client side will be used.
 constexpr base::FeatureParam<base::TimeDelta> kOverriddenDuration{
     &feature_engagement::kIPHDesktopSnoozeFeature,
-    "x_iph_snooze_overridden_duration ", base::TimeDelta::FromHours(0)};
+    "x_iph_snooze_overridden_duration", base::TimeDelta::FromHours(0)};
+
+constexpr base::FeatureParam<FeaturePromoSnoozeService::NonClickerPolicy>::
+    Option kNonClickerPolicyOptions[] = {
+        {FeaturePromoSnoozeService::NonClickerPolicy::kDismiss, "dismiss"},
+        {FeaturePromoSnoozeService::NonClickerPolicy::kLongSnooze,
+         "long_snooze"}};
 
 // Used in UMA histogram to track if the user snoozes for once or more.
 enum class SnoozeType {
@@ -101,6 +113,18 @@ void FeaturePromoSnoozeService::OnUserDismiss(
       snooze_data->snooze_count, kUmaMaxSnoozeCount);
 }
 
+void FeaturePromoSnoozeService::OnPromoShown(const base::Feature& iph_feature) {
+  auto snooze_data = ReadSnoozeData(iph_feature);
+
+  if (!snooze_data)
+    snooze_data = SnoozeData();
+
+  snooze_data->last_show_time = base::Time::Now();
+  snooze_data->show_count++;
+
+  SaveSnoozeData(iph_feature, *snooze_data);
+}
+
 bool FeaturePromoSnoozeService::IsBlocked(const base::Feature& iph_feature) {
   auto snooze_data = ReadSnoozeData(iph_feature);
 
@@ -111,17 +135,37 @@ bool FeaturePromoSnoozeService::IsBlocked(const base::Feature& iph_feature) {
   if (snooze_data->is_dismissed)
     return true;
 
-  // This IPH has neither been dismissed nor snoozed.
-  if (snooze_data->snooze_count == 0)
+  // This IPH is shown for the first time.
+  if (snooze_data->show_count == 0)
     return false;
 
-  // Corruption: Snooze time is in the future.
-  if (snooze_data->last_snooze_time > base::Time::Now())
-    return true;
+  if (snooze_data->snooze_count > 0 &&
+      snooze_data->last_snooze_time >= snooze_data->last_show_time) {
+    // The IPH was snoozed on last display.
 
-  // This IPH is snoozed. Test if snooze period has expired.
-  return base::Time::Now() <
-         snooze_data->last_snooze_time + snooze_data->last_snooze_duration;
+    // Corruption: Snooze time is in the future.
+    if (snooze_data->last_snooze_time > base::Time::Now())
+      return true;
+
+    // This IPH is snoozed. Test if snooze period has expired.
+    return base::Time::Now() <
+           snooze_data->last_snooze_time + snooze_data->last_snooze_duration;
+  } else {
+    // The IPH was neither snoozed or dismissed on last display.
+    const base::FeatureParam<FeaturePromoSnoozeService::NonClickerPolicy>
+        kNonClickerPolicy{
+            &iph_feature, "x_iph_snooze_non_clicker_policy",
+            FeaturePromoSnoozeService::NonClickerPolicy::kLongSnooze,
+            &kNonClickerPolicyOptions};
+
+    NonClickerPolicy non_clicker_policy = kNonClickerPolicy.Get();
+
+    if (non_clicker_policy == NonClickerPolicy::kDismiss)
+      return true;
+
+    return base::Time::Now() <
+           snooze_data->last_show_time + base::TimeDelta::FromDays(14);
+  }
 }
 
 // static
@@ -149,13 +193,17 @@ FeaturePromoSnoozeService::ReadSnoozeData(const base::Feature& iph_feature) {
   const base::DictionaryValue* pref_data =
       profile_->GetPrefs()->GetDictionary(kIPHSnoozeDataPath);
   base::Optional<bool> is_dismissed =
-      pref_data->FindBoolPath(path_prefix + kIPHIsFeatureDismissed);
+      pref_data->FindBoolPath(path_prefix + kIPHIsDismissedPath);
+  base::Optional<base::Time> show_time = util::ValueToTime(
+      pref_data->FindPath(path_prefix + kIPHLastShowTimePath));
   base::Optional<base::Time> snooze_time = util::ValueToTime(
-      pref_data->FindPath(path_prefix + kIPHSnoozedFeatureTime));
-  base::Optional<int> snooze_count =
-      pref_data->FindIntPath(path_prefix + kIPHSnoozedFeatureCount);
+      pref_data->FindPath(path_prefix + kIPHLastSnoozeTimePath));
   base::Optional<base::TimeDelta> snooze_duration = util::ValueToTimeDelta(
-      pref_data->FindPath(path_prefix + kIPHSnoozedFeatureDuration));
+      pref_data->FindPath(path_prefix + kIPHLastSnoozeDurationPath));
+  base::Optional<int> snooze_count =
+      pref_data->FindIntPath(path_prefix + kIPHSnoozeCountPath);
+  base::Optional<int> show_count =
+      pref_data->FindIntPath(path_prefix + kIPHShowCountPath);
 
   base::Optional<SnoozeData> snooze_data;
 
@@ -168,11 +216,20 @@ FeaturePromoSnoozeService::ReadSnoozeData(const base::Feature& iph_feature) {
     return snooze_data;
   }
 
+  if (!show_time || !show_count) {
+    // This feature was shipped before without handling
+    // non-clickers. Assume previous displays were all snooozed.
+    show_time = *snooze_time - base::TimeDelta::FromSeconds(1);
+    show_count = *snooze_count;
+  }
+
   snooze_data = SnoozeData();
   snooze_data->is_dismissed = *is_dismissed;
+  snooze_data->last_show_time = *show_time;
   snooze_data->last_snooze_time = *snooze_time;
   snooze_data->last_snooze_duration = *snooze_duration;
   snooze_data->snooze_count = *snooze_count;
+  snooze_data->show_count = *show_count;
 
   return snooze_data;
 }
@@ -185,12 +242,16 @@ void FeaturePromoSnoozeService::SaveSnoozeData(
   DictionaryPrefUpdate update(profile_->GetPrefs(), kIPHSnoozeDataPath);
   base::DictionaryValue* pref_data = update.Get();
 
-  pref_data->SetBoolPath(path_prefix + kIPHIsFeatureDismissed,
+  pref_data->SetBoolPath(path_prefix + kIPHIsDismissedPath,
                          snooze_data.is_dismissed);
-  pref_data->SetPath(path_prefix + kIPHSnoozedFeatureTime,
+  pref_data->SetPath(path_prefix + kIPHLastShowTimePath,
+                     util::TimeToValue(snooze_data.last_show_time));
+  pref_data->SetPath(path_prefix + kIPHLastSnoozeTimePath,
                      util::TimeToValue(snooze_data.last_snooze_time));
-  pref_data->SetPath(path_prefix + kIPHSnoozedFeatureDuration,
+  pref_data->SetPath(path_prefix + kIPHLastSnoozeDurationPath,
                      util::TimeDeltaToValue(snooze_data.last_snooze_duration));
-  pref_data->SetIntPath(path_prefix + kIPHSnoozedFeatureCount,
+  pref_data->SetIntPath(path_prefix + kIPHSnoozeCountPath,
                         snooze_data.snooze_count);
+  pref_data->SetIntPath(path_prefix + kIPHShowCountPath,
+                        snooze_data.show_count);
 }
--- chrome/browser/ui/user_education/feature_promo_snooze_service.h
+++ chrome/browser/ui/user_education/feature_promo_snooze_service.h
@@ -17,14 +17,21 @@ struct Feature;
 class Profile;
 class PrefRegistrySimple;
 
-// This service manages snooze and dismiss of in-product help promo.
+// This service manages snooze and dismiss of snoozable in-product help promo.
 //
-// An IPH controller notifies this service when an IPH is snoozed or dismissed.
-// When the snooze period expires, this service will approve controller's
-// request to retrigger the IPH.
-// If an IPH is dismissed, this service will reject controller's request.
+// Before showing an IPH, the IPH controller should ask if the IPH is blocked.
+// The controller should also notify after the IPH is shown and after the user
+// clicks the snooze/dismiss button.
 class FeaturePromoSnoozeService {
  public:
+  // Policies to handler users who don't interact with the IPH.
+  enum class NonClickerPolicy {
+    // Permanently dismiss the IPH. Equivalent to clicking the dismiss button.
+    kDismiss = 0,
+    // Reshow the IPH later after at least 14 days.
+    kLongSnooze
+  };
+
   // Maximum count of snoozes to track in UMA histogram.
   // Snooze counts that are equal or larger than this value will be conflated.
   static const int kUmaMaxSnoozeCount;
@@ -48,6 +55,9 @@ class FeaturePromoSnoozeService {
   // after a fixed amount of time.
   void OnUserDismiss(const base::Feature& iph_feature);
 
+  // The IPH controller must call this method after an IPH is shown.
+  void OnPromoShown(const base::Feature& iph_feature);
+
   // The IPH controller must call this method to check if an IPH is blocked by
   // dismiss or snooze. An IPH will be approved if it is not snoozed or the
   // snoozing period has timed out.
@@ -70,9 +80,11 @@ class FeaturePromoSnoozeService {
   // in_product_help.snoozed_feature.[iph_name] in PerfService.
   struct SnoozeData {
     bool is_dismissed = false;
+    base::Time last_show_time = base::Time();
     base::Time last_snooze_time = base::Time();
     base::TimeDelta last_snooze_duration = base::TimeDelta();
     int snooze_count = 0;
+    int show_count = 0;
   };
 
   base::Optional<SnoozeData> ReadSnoozeData(const base::Feature& iph_feature);
