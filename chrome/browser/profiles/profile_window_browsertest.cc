488fe012ed5cb4682da94e714a5c75d57e31ac19
Ramin Halavati <rhalavati@chromium.org>
2021-04-19 09:27:09 +0000

    Enable ProfileWindowCountBrowserTest::CountProfileWindows for Linux.
    The flakinesss on Linux Debug seem to be related to
    |OpenDevToolsWindowSync| which is slow on Linux debug and results in
    time-out.
    
    Regular windows and DevTools section of the test are separated and the
    non-devtools section is re-enabled on linux-debug.
    
    Bug: 1186994
    Change-Id: Ia77e9636947eb23bf5f94aef6a36fa40d93bcba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831538
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873744}

--- chrome/browser/profiles/profile_window_browsertest.cc
+++ chrome/browser/profiles/profile_window_browsertest.cc
@@ -155,43 +155,50 @@ class ProfileWindowCountBrowserTest
   Profile* profile_ = nullptr;
 };
 
-// TODO(crbug.com/1186994): Test is flaky on Linux Dbg.
-#if defined(OS_LINUX) && !defined(NDEBUG)
-#define MAYBE_CountProfileWindows DISABLED_CountProfileWindows
-#else
-#define MAYBE_CountProfileWindows CountProfileWindows
-#endif
-IN_PROC_BROWSER_TEST_P(ProfileWindowCountBrowserTest,
-                       MAYBE_CountProfileWindows) {
-  DCHECK_EQ(0, GetWindowCount());
+IN_PROC_BROWSER_TEST_P(ProfileWindowCountBrowserTest, CountProfileWindows) {
+  EXPECT_EQ(0, GetWindowCount());
 
   // Create a browser and check the count.
   Browser* browser1 = CreateGuestOrIncognitoBrowser();
-  DCHECK_EQ(1, GetWindowCount());
+  EXPECT_EQ(1, GetWindowCount());
 
   // Create another browser and check the count.
   Browser* browser2 = CreateGuestOrIncognitoBrowser();
-  DCHECK_EQ(2, GetWindowCount());
-
-  // Open a docked DevTool window and count.
-  DevToolsWindow* devtools_window =
-      DevToolsWindowTesting::OpenDevToolsWindowSync(browser1, true);
-  DCHECK_EQ(2, GetWindowCount());
-  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools_window);
-
-  // Open a detached DevTool window and count.
-  devtools_window =
-      DevToolsWindowTesting::OpenDevToolsWindowSync(browser1, false);
-  DCHECK_EQ(2, GetWindowCount());
-  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools_window);
+  EXPECT_EQ(2, GetWindowCount());
 
   // Close one browser and count.
   CloseBrowserSynchronously(browser2);
-  DCHECK_EQ(1, GetWindowCount());
+  EXPECT_EQ(1, GetWindowCount());
 
   // Close another browser and count.
   CloseBrowserSynchronously(browser1);
-  DCHECK_EQ(0, GetWindowCount());
+  EXPECT_EQ(0, GetWindowCount());
+}
+
+// |OpenDevToolsWindowSync| is slow on Linux Debug and can result in flacky test
+// failure. See (crbug.com/1186994).
+#if defined(OS_LINUX) && !defined(NDEBUG)
+#define MAYBE_DevToolsWindowsNotCounted DISABLED_DevToolsWindowsNotCounted
+#else
+#define MAYBE_DevToolsWindowsNotCounted DevToolsWindowsNotCounted
+#endif
+IN_PROC_BROWSER_TEST_P(ProfileWindowCountBrowserTest,
+                       MAYBE_DevToolsWindowsNotCounted) {
+  Browser* browser = CreateGuestOrIncognitoBrowser();
+  EXPECT_EQ(1, GetWindowCount());
+
+  DevToolsWindow* devtools_window =
+      DevToolsWindowTesting::OpenDevToolsWindowSync(browser,
+                                                    /*is_docked=*/true);
+  EXPECT_EQ(1, GetWindowCount());
+  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools_window);
+
+  devtools_window = DevToolsWindowTesting::OpenDevToolsWindowSync(
+      browser, /*is_docked=*/false);
+  EXPECT_EQ(1, GetWindowCount());
+  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools_window);
+
+  EXPECT_EQ(1, GetWindowCount());
 }
 
 INSTANTIATE_TEST_SUITE_P(All,
