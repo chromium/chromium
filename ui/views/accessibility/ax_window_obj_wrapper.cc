472e0e3b518fc220e4866a90b211107fbb074c7b
Christos Froussios <cfroussios@chromium.org>
2021-04-20 08:57:01 +0000

    Revert "Adopt DesktopWindowTreeHostLinux::From in ax_window_obj_wrapper.cc"
    This reverts commit 05263b5990f23582d14ded9db712c56801f7eb64.
    
    Reason for revert: AXAuraObjCacheTest.ValidTree is failing on linux-lacros-tester-rel
    First failure https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/9021/overview
    
    Original change's description:
    > Adopt DesktopWindowTreeHostLinux::From in ax_window_obj_wrapper.cc
    >
    > This helper encapsulates the explicit static_cast, performing
    > some sanity checks.
    >
    > Follow up of https://crrev.com/c/871606.
    >
    > BUG=NONE
    > R=​sky@chromium.org
    >
    > Change-Id: Idaa19bf2c9fa0082ab30a127c62f16f3b4d0257b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837785
    > Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#873993}
    
    Bug: NONE
    Change-Id: Ifb956edbfb3cdfbf5474f823ee65808b91c11124
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2839605
    Auto-Submit: Christos Froussios <cfroussios@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Owners-Override: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874192}

--- ui/views/accessibility/ax_window_obj_wrapper.cc
+++ ui/views/accessibility/ax_window_obj_wrapper.cc
@@ -17,9 +17,9 @@
 #include "ui/accessibility/ax_tree_id.h"
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/aura/window_tree_host_platform.h"
 #include "ui/platform_window/platform_window.h"
 #include "ui/views/accessibility/ax_aura_obj_cache.h"
-#include "ui/views/widget/desktop_aura/desktop_window_tree_host_linux.h"
 #include "ui/views/widget/widget.h"
 
 namespace views {
@@ -141,12 +141,12 @@ void AXWindowObjWrapper::Serialize(ui::AXNodeData* out_node_data) {
     aura::WindowTreeHost* window_tree_host = window_->GetHost();
     if (window_tree_host) {
       // Lacros is based on Ozone/Wayland, which uses PlatformWindow and
-      // views::DesktopWindowTreeHostLinux.
-      auto* desktop_window_tree_host_linux =
-          views::DesktopWindowTreeHostLinux::From(window_tree_host);
+      // aura::WindowTreeHostPlatform.
+      aura::WindowTreeHostPlatform* window_tree_host_platform =
+          static_cast<aura::WindowTreeHostPlatform*>(window_tree_host);
+
       const std::string window_id =
-          desktop_window_tree_host_linux->platform_window()
-              ->GetWindowUniqueId();
+          window_tree_host_platform->platform_window()->GetWindowUniqueId();
 
       if (!window_id.empty())
         out_node_data->AddStringAttribute(
