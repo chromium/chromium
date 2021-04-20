e174c7a79bbcc08140982130812176e998204c2d
Matt Menke <mmenke@chromium.org>
2021-04-20 21:52:29 +0000

    Merge: FLEDGE: Add an AuctionURLoaderFactoryProxy class.
    The class sanitizes ResourceRequests made by the worklet process.
    
    It validates requested URLs against the URLs an auction may request,
    validates the Accept header, and throws away the rest of the original
    ResourceRequest parameters, creating its own.
    
    This minimizes the damage a compromised auction worklet process can do.
    
    This CL adds the class, but does not hook it up.
    
    (cherry picked from commit d844cb6f22cfbc3cfd8bbb72a5ea80f84ed833bd)
    
    Bug: 1186444, 1199852
    Change-Id: Ie16906a2dae0ff495f9f29ff87e0782d00b49db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818779
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#871151}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841063
    Cr-Commit-Position: refs/branch-heads/4472@{#270}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

---
+++ content/browser/interest_group/auction_url_loader_factory_proxy_unittest.cc
@@ -0,0 +1,307 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/interest_group/auction_url_loader_factory_proxy.h"
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "base/memory/ref_counted.h"
+#include "base/test/task_environment.h"
+#include "content/services/auction_worklet/public/mojom/auction_worklet_service.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "net/base/isolation_info.h"
+#include "net/cookies/site_for_cookies.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
+#include "services/network/test/test_url_loader_factory.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/interest_group/interest_group_types.mojom.h"
+#include "url/gurl.h"
+#include "url/origin.h"
+
+namespace content {
+
+// URL for the scoring worklet.
+const char kScoringWorkletUrl[] = "https://decision_logic_url.test/foo";
+
+// URLs for bidding worklets and their trusted bidding signals. Third worklet
+// has no trusted bidding signals URL.
+
+const char kBiddingWorkletUrl1[] = "https://bidding_url1.test/";
+const char kTrustedBiddingSignalsUrl1[] =
+    "https://trusted_bidding_signals_url1.test/";
+
+const char kBiddingWorkletUrl2[] = "https://bidding_url2.test/foo";
+const char kTrustedBiddingSignalsUrl2[] =
+    "https://trusted_bidding_signals_url2.test/bar";
+
+const char kBiddingWorkletUrl3[] = "https://bidding_url3.test/baz?foobar";
+
+// Values for the Accept header.
+const char kAcceptJavascript[] = "application/javascript";
+const char kAcceptJson[] = "application/json";
+
+class ActionUrlLoaderFactoryProxyTest : public testing::Test {
+ public:
+  ActionUrlLoaderFactoryProxyTest() {
+    blink::mojom::AuctionAdConfigPtr auction_config =
+        blink::mojom::AuctionAdConfig::New();
+    auction_config->decision_logic_url = GURL(kScoringWorkletUrl);
+    std::vector<auction_worklet::mojom::BiddingInterestGroupPtr> bidders;
+
+    bidders.emplace_back(auction_worklet::mojom::BiddingInterestGroup::New());
+    bidders.back()->group = blink::mojom::InterestGroup::New();
+    bidders.back()->group->bidding_url = GURL(kBiddingWorkletUrl1);
+    bidders.back()->group->trusted_bidding_signals_url =
+        GURL(kTrustedBiddingSignalsUrl1);
+
+    bidders.emplace_back(auction_worklet::mojom::BiddingInterestGroup::New());
+    bidders.back()->group = blink::mojom::InterestGroup::New();
+    bidders.back()->group->bidding_url = GURL(kBiddingWorkletUrl2);
+    bidders.back()->group->trusted_bidding_signals_url =
+        GURL(kTrustedBiddingSignalsUrl2);
+
+    bidders.emplace_back(auction_worklet::mojom::BiddingInterestGroup::New());
+    bidders.back()->group = blink::mojom::InterestGroup::New();
+    bidders.back()->group->bidding_url = GURL(kBiddingWorkletUrl3);
+
+    url_loader_factory_proxy_ = std::make_unique<AuctionURLLoaderFactoryProxy>(
+        url_loader_factory_.GetSafeWeakWrapper(), "foo.test", *auction_config,
+        bidders);
+  }
+
+  ~ActionUrlLoaderFactoryProxyTest() override = default;
+
+  // Attempts to make a request for `request`. Return true of the request is
+  // passed through AuctionURLLoaderFactoryProxy to the nested
+  // TestURLLoaderFactory.
+  bool TryMakeRequest(const network::ResourceRequest& request) {
+    int initial_num_requests = url_loader_factory_.NumPending();
+
+    // Try to send a request. Requests are never run to completion, instead,
+    // requests that make it to the nested `url_loader_factory_` are tracked in
+    // its vector of pending requests.
+    mojo::PendingRemote<network::mojom::URLLoader> receiver;
+    mojo::PendingReceiver<network::mojom::URLLoaderClient> client;
+    // Set a couple random options, which should be ignored.
+    int options = network::mojom::kURLLoadOptionSendSSLInfoWithResponse |
+                  network::mojom::kURLLoadOptionSniffMimeType;
+    url_loader_factory_proxy_->CreateLoaderAndStart(
+        receiver.InitWithNewPipeAndPassReceiver(), 0 /* request_id_ */, options,
+        request, client.InitWithNewPipeAndPassRemote(),
+        net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS));
+
+    // If there are just as many requests now as there were before a request was
+    // made, then the proxy blocked the request. Note that NumPending() spins
+    // the run loop, ensuring any async mojo calls have completed.
+    int final_num_requests = url_loader_factory_.NumPending();
+    if (initial_num_requests == final_num_requests)
+      return false;
+
+    // If there are more pending requests than before, there should only be one
+    // more pending request.
+    EXPECT_EQ(initial_num_requests + 1, final_num_requests);
+
+    const auto& pending_request =
+        url_loader_factory_.pending_requests()->back();
+
+    // These should always be the same for all requests.
+    EXPECT_EQ(0u, pending_request.options);
+    EXPECT_EQ(
+        net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS),
+        pending_request.traffic_annotation);
+
+    // Each request should be assigned a unique ID. These should actually be
+    // unique within the browser process, not just among requests using the
+    // AuctionURLLoaderFactoryProxy.
+    for (const auto& other_pending_request :
+         *url_loader_factory_.pending_requests()) {
+      if (&other_pending_request == &pending_request)
+        continue;
+      EXPECT_NE(other_pending_request.request_id, pending_request.request_id);
+    }
+
+    const auto& observed_request = pending_request.request;
+
+    // The URL should be unaltered.
+    EXPECT_EQ(request.url, observed_request.url);
+
+    // There should be an accept header, and it should be the same as before.
+    std::string original_accept_header;
+    std::string observed_accept_header;
+    EXPECT_TRUE(request.headers.GetHeader(net::HttpRequestHeaders::kAccept,
+                                          &original_accept_header));
+    EXPECT_TRUE(observed_request.headers.GetHeader(
+        net::HttpRequestHeaders::kAccept, &observed_accept_header));
+    EXPECT_EQ(original_accept_header, observed_accept_header);
+
+    // The accept header should be the only accept header.
+    EXPECT_EQ(1u, observed_request.headers.GetHeaderVector().size());
+
+    // The request should not include credentials and not follow redirects.
+    EXPECT_EQ(network::mojom::CredentialsMode::kOmit,
+              observed_request.credentials_mode);
+    EXPECT_EQ(network::mojom::RedirectMode::kError,
+              observed_request.redirect_mode);
+
+    // Validate `observed_request.trusted_params`.
+    EXPECT_TRUE(observed_request.trusted_params);
+    if (observed_request.trusted_params) {
+      EXPECT_FALSE(observed_request.trusted_params->disable_secure_dns);
+
+      const auto& observed_isolation_info =
+          observed_request.trusted_params->isolation_info;
+      EXPECT_EQ(net::IsolationInfo::RequestType::kOther,
+                observed_isolation_info.request_type());
+      url::Origin expected_origin = url::Origin::Create(request.url);
+      EXPECT_EQ(expected_origin, observed_isolation_info.top_frame_origin());
+      EXPECT_EQ(expected_origin, observed_isolation_info.frame_origin());
+      EXPECT_TRUE(observed_isolation_info.site_for_cookies().IsNull());
+    }
+
+    return true;
+  }
+
+  bool TryMakeRequest(const std::string& url,
+                      base::Optional<std::string> accept_value) {
+    network::ResourceRequest request;
+    request.url = GURL(url);
+    if (accept_value) {
+      request.headers.SetHeader(net::HttpRequestHeaders::kAccept,
+                                *accept_value);
+    }
+    return TryMakeRequest(request);
+  }
+
+ protected:
+  base::test::TaskEnvironment task_environment_;
+  network::TestURLLoaderFactory url_loader_factory_;
+  std::unique_ptr<AuctionURLLoaderFactoryProxy> url_loader_factory_proxy_;
+};
+
+// Test exact URL matches. Trusted bidding signals URLs should be rejected
+// unless that have a valid query string appended.
+TEST_F(ActionUrlLoaderFactoryProxyTest, ExactURLMatch) {
+  EXPECT_TRUE(TryMakeRequest(kScoringWorkletUrl, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kScoringWorkletUrl, kAcceptJson));
+  EXPECT_FALSE(TryMakeRequest(kScoringWorkletUrl, "Unknown/Unknown"));
+  EXPECT_FALSE(TryMakeRequest(kScoringWorkletUrl, base::nullopt));
+
+  EXPECT_TRUE(TryMakeRequest(kBiddingWorkletUrl1, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kBiddingWorkletUrl1, kAcceptJson));
+  EXPECT_FALSE(TryMakeRequest(kBiddingWorkletUrl1, "Unknown/Unknown"));
+  EXPECT_FALSE(TryMakeRequest(kBiddingWorkletUrl1, base::nullopt));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl1, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl1, kAcceptJson));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl1, "Unknown/Unknown"));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl1, base::nullopt));
+
+  EXPECT_TRUE(TryMakeRequest(kBiddingWorkletUrl2, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kBiddingWorkletUrl2, kAcceptJson));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl2, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kTrustedBiddingSignalsUrl2, kAcceptJson));
+
+  EXPECT_TRUE(TryMakeRequest(kBiddingWorkletUrl3, kAcceptJavascript));
+  EXPECT_FALSE(TryMakeRequest(kBiddingWorkletUrl3, kAcceptJson));
+}
+
+TEST_F(ActionUrlLoaderFactoryProxyTest, QueryStrings) {
+  const char* kValidBiddingSignalsQueryStrings[] = {
+      "?hostname=foo.test&keys=bar,baz",
+      "?hostname=foo.test&keys=bar",
+      // Escaped &.
+      "?hostname=foo.test&keys=bar%26",
+  };
+
+  const char* kInvalidBiddingSignalsQueryStrings[] = {
+      "?hostname=bar.test&keys=bar,baz",
+      "?hostname=foo.test&keys=bar&hats=foo",
+      "?hostname=foo.test&keys=bar&hats",
+      "?hostname=foo.test",
+      "?hostname=foo.test&keys",
+      "?bar=foo.test&keys=bar,baz",
+      "?keys=bar,baz",
+      "?",
+  };
+
+  for (std::string query_string : kValidBiddingSignalsQueryStrings) {
+    SCOPED_TRACE(query_string);
+    EXPECT_FALSE(
+        TryMakeRequest(kScoringWorkletUrl + query_string, kAcceptJavascript));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl1 + query_string, kAcceptJavascript));
+    EXPECT_TRUE(
+        TryMakeRequest(kTrustedBiddingSignalsUrl1 + query_string, kAcceptJson));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl2 + query_string, kAcceptJavascript));
+    EXPECT_TRUE(
+        TryMakeRequest(kTrustedBiddingSignalsUrl2 + query_string, kAcceptJson));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl3 + query_string, kAcceptJavascript));
+  }
+
+  for (std::string query_string : kInvalidBiddingSignalsQueryStrings) {
+    SCOPED_TRACE(query_string);
+    EXPECT_FALSE(
+        TryMakeRequest(kScoringWorkletUrl + query_string, kAcceptJavascript));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl1 + query_string, kAcceptJavascript));
+    EXPECT_FALSE(
+        TryMakeRequest(kTrustedBiddingSignalsUrl1 + query_string, kAcceptJson));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl2 + query_string, kAcceptJavascript));
+    EXPECT_FALSE(
+        TryMakeRequest(kTrustedBiddingSignalsUrl2 + query_string, kAcceptJson));
+
+    EXPECT_FALSE(
+        TryMakeRequest(kBiddingWorkletUrl3 + query_string, kAcceptJavascript));
+  }
+}
+
+// Set a number of extra parameters on the ResourceRequest, which the
+// AuctionURLLoaderFactoryProxy should ignore. This test relies heavily on the
+// validity checks in TryMakeRequest().
+TEST_F(ActionUrlLoaderFactoryProxyTest, ExtraParametersIgnored) {
+  network::ResourceRequest request;
+
+  request.credentials_mode = network::mojom::CredentialsMode::kInclude;
+  request.trusted_params = network::ResourceRequest::TrustedParams();
+
+  url::Origin other_origin =
+      url::Origin::Create(GURL("https://somewhere.else.text"));
+  request.trusted_params->isolation_info = net::IsolationInfo::Create(
+      net::IsolationInfo::RequestType::kMainFrame, other_origin, other_origin,
+      net::SiteForCookies::FromOrigin(other_origin));
+  request.trusted_params->disable_secure_dns = true;
+
+  request.headers.SetHeader("Flux-Capacitor", "Y");
+  request.headers.SetHeader("Host", "Fred");
+  request.headers.SetHeader(net::HttpRequestHeaders::kAccept,
+                            kAcceptJavascript);
+
+  request.url = GURL(kScoringWorkletUrl);
+  EXPECT_TRUE(TryMakeRequest(request));
+
+  request.url = GURL(kBiddingWorkletUrl1);
+  EXPECT_TRUE(TryMakeRequest(request));
+
+  request.url = GURL(std::string(kTrustedBiddingSignalsUrl1) +
+                     "?hostname=foo.test&keys=bar");
+  request.headers.SetHeader(net::HttpRequestHeaders::kAccept, kAcceptJson);
+  EXPECT_TRUE(TryMakeRequest(request));
+}
+
+}  // namespace content
