ac001b08f5a696a7bf882ad290401d9c12657367
Asami Doi <asamidoi@chromium.org>
2021-04-20 04:49:51 +0000

    Prerender: Move a browsertest for clients.matchAll() to wpt_internal/
    This CLs moves a browsertest
    'PrerenderBrowserTest.FeatureRestriction_ClientsMatchAll' to
    the wpt_internal/prerender directory.
    
    Exposing Clients API for prerendered pages is not defined at the spec
    and the issue is still open
    (https://github.com/jeremyroman/alternate-loading-modes/issues/44).
    The behavior can be changed in the future.
    
    Bug: 1166470
    Change-Id: Iaaa5e3449cbfef264d5968eac4d0f52548eef7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2828171
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874124}

--- third_party/blink/web_tests/wpt_internal/prerender/resources/clients-matchall-service-worker.js
+++ third_party/blink/web_tests/wpt_internal/prerender/resources/clients-matchall-service-worker.js
@@ -1,7 +1,3 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 onmessage = e => {
   // Collect all client URLs in this origin.
   const options = { includeUncontrolled: true, type: 'all' };
