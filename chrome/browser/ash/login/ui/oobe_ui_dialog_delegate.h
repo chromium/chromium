1165491e468faee2f4d7edde542f624cde1a76e6
Denis Kuznetsov <antrim@google.com>
2021-04-20 06:56:20 +0000

    [M-91] OOBE: use ScopedObservation for OobeWebDialogView
    (cherry picked from commit d88c1aac966e5cec57925b0050e32f4aa27ec9b3)
    
    Bug: 1187724
    Change-Id: I2e2eab2398a4cde40b8f9cac6a6293a266433f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2835828
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Auto-Submit: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#873807}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838166
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#229}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.h
+++ chrome/browser/ash/login/ui/oobe_ui_dialog_delegate.h
@@ -18,6 +18,7 @@
 #include "chrome/browser/ui/ash/login_screen_client.h"
 #include "chrome/browser/ui/chrome_web_modal_dialog_manager_delegate.h"
 #include "components/web_modal/web_contents_modal_dialog_host.h"
+#include "ui/views/view.h"
 #include "ui/views/view_observer.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 
@@ -30,7 +31,6 @@ class Accelerator;
 }
 
 namespace views {
-class View;
 class WebDialogView;
 class Widget;
 }  // namespace views
@@ -113,6 +113,7 @@ class OobeUIDialogDelegate : public ui::WebDialogDelegate,
 
   // views::ViewObserver:
   void OnViewBoundsChanged(views::View* observed_view) override;
+  void OnViewIsDeleting(views::View* observed_view) override;
 
   // ChromeKeyboardControllerClient::Observer:
   void OnKeyboardVisibilityChanged(bool visible) override;
@@ -136,6 +137,8 @@ class OobeUIDialogDelegate : public ui::WebDialogDelegate,
   // Reference to dialog view stored in widget_.
   OobeWebDialogView* dialog_view_ = nullptr;
 
+  base::ScopedObservation<views::View, views::ViewObserver> view_observer_{
+      this};
   base::ScopedObservation<ChromeKeyboardControllerClient,
                           ChromeKeyboardControllerClient::Observer>
       keyboard_observer_{this};
