0d01570dd904df573ecc40e688c5597804a92290
Dave Tapuska <dtapuska@chromium.org>
2021-04-21 21:21:44 +0000

    Fix CanAccessWindow bindings CHECK failing.
    In http://crrev.com/92e39c8 the WindowAgentFactory started handing out
    different Agents that may pass an origin check but fail a cluster
    agent key check. This will trigger a CHECK failure because those will
    still be in the same window agent factory. Allow Agents that have
    different ExplicitlyOriginKeyed values to pass the CHECK.
    
    A regression test is being worked on by dcheng@ but still needs some
    investigation.
    
    BUG=1199979
    
    (cherry picked from commit 8b52e802de509d31f94a1af98cb49bcb3e401de8)
    
    Change-Id: I5d62d23723f96e98b85ecedd981122f8fd207ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841784
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#874712}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844290
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Auto-Submit: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4484@{#10}
    Cr-Branched-From: c45ca6c817c5848943575712429c56a5a555671f-refs/heads/master@{#874500}

--- third_party/blink/renderer/bindings/core/v8/binding_security.cc
+++ third_party/blink/renderer/bindings/core/v8/binding_security.cc
@@ -35,6 +35,7 @@
 #include "third_party/blink/renderer/bindings/core/v8/v8_location.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_window.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/agent.h"
 #include "third_party/blink/renderer/core/frame/dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -151,9 +152,12 @@ bool CanAccessWindowInternal(
     if (detail == SecurityOrigin::AccessResultDomainDetail::
                       kDomainNotRelevantAgentClusterMismatch) {
       // Assert that because the agent clusters are different than the
-      // WindowAgentFactories must also be different.
+      // WindowAgentFactories must also be different unless they differ in
+      // being explicitly origin keyed.
       SECURITY_CHECK(
           !IsSameWindowAgentFactory(accessing_window, local_target_window) ||
+          (accessing_window->GetAgent()->IsExplicitlyOriginKeyed() !=
+           local_target_window->GetAgent()->IsExplicitlyOriginKeyed()) ||
           (WebTestSupport::IsRunningWebTest() &&
            local_target_window->GetFrame()->PagePopupOwner()));
 
