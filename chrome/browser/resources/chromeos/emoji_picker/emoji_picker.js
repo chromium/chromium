f42b6dcdd2968ef436a23ab0899146e14fa93f4d
John Palmer <jopalmer@chromium.org>
2021-04-21 06:38:05 +0000

    [Emoji picker] When clearing recent emoji, update active emoji group
    Minor UX tweak to ensure that correct group is highlighted in this case
    #SIMPLE_REVIEW
    
    Bug: b/185855792
    Change-Id: I0b9e1cd7c463b8ac7c9b41da56c9547a1dcdd3fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837208
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874590}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
@@ -238,6 +238,7 @@ export class EmojiPicker extends PolymerElement {
   clearRecentEmoji() {
     this.set(['history', 'emoji'], makeRecentlyUsed([]));
     this.recentEmojiStore.clearRecents();
+    afterNextRender(this, () => this.updateActiveGroup());
   }
 
   /**
