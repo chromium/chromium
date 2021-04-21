07401c8dae289951c51f7e65c82c1938f15223d0
Ayman Almadhoun <ayman@chromium.org>
2021-04-21 22:43:39 +0000

    Add support for SpannableString in Messages
    This CL adds the support for using Spannable in the description of the
    MessageBannerView by changing the PropertyModel to a CharSequence
    instead of a string.
    
    Bug: 1190530
    Change-Id: I37f966d59d8b14c7dc1134879a60cc7872f87c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842035
    Commit-Queue: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874906}

--- components/messages/android/internal/java/src/org/chromium/components/messages/MessageBannerView.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/MessageBannerView.java
@@ -65,7 +65,7 @@ public class MessageBannerView extends BoundedLinearLayout {
         if (mOnTitleChanged != null) mOnTitleChanged.run();
     }
 
-    void setDescription(String description) {
+    void setDescription(CharSequence description) {
         mDescription.setVisibility(VISIBLE);
         mDescription.setText(description);
     }
