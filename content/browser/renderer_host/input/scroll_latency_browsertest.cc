40d650c7b072707539fc51c027a9d03fec09863e
Dominik Röttsches <drott@chromium.org>
2021-04-22 14:36:19 +0000

    Disable MultipleWheelScrollOnMain on Linux debug
    Bug: 1201652
    Change-Id: I30a082367c667d9d2db48526907e3299cc202038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844599
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875154}

--- content/browser/renderer_host/input/scroll_latency_browsertest.cc
+++ content/browser/renderer_host/input/scroll_latency_browsertest.cc
@@ -215,7 +215,13 @@ IN_PROC_BROWSER_TEST_F(ScrollLatencyBrowserTest, MultipleWheelScroll) {
   RunMultipleWheelScroll();
 }
 
-IN_PROC_BROWSER_TEST_F(ScrollLatencyBrowserTest, MultipleWheelScrollOnMain) {
+#if !defined(NDEBUG) && defined(OS_LINUX)
+#define MAYBE_MultipleWheelScrollOnMain DISABLED_MultipleWheelScrollOnMain
+#else
+#define MAYBE_MultipleWheelScrollOnMain MultipleWheelScrollOnMain
+#endif
+IN_PROC_BROWSER_TEST_F(ScrollLatencyBrowserTest,
+                       MAYBE_MultipleWheelScrollOnMain) {
   disable_threaded_scrolling_ = true;
   LoadURL();
   RunMultipleWheelScroll();
