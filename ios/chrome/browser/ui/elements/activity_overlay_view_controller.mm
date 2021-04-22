6fcb47684b505e5955a5f153ae48de115f06a576
Gauthier Ambard <gambard@chromium.org>
2021-04-22 10:14:57 +0000

    [iOS]🐻[FRE] Create shared overlay view
    This CL creates an overlay view which can be shared among different UI.
    
    Bug: 1189836
    Change-Id: I4f14eb429b9e1a39d1a5c23a7fca1037f020ad1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840454
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875090}

--- ios/chrome/browser/ui/elements/activity_overlay_view_controller.mm
+++ ios/chrome/browser/ui/elements/activity_overlay_view_controller.mm
@@ -4,36 +4,16 @@
 
 #import "ios/chrome/browser/ui/elements/activity_overlay_view_controller.h"
 
-#import <MaterialComponents/MaterialActivityIndicator.h>
-
-#import "ios/chrome/browser/ui/material_components/activity_indicator.h"
-#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/browser/ui/elements/activity_overlay_view.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-namespace {
-// Size of the activity indicator.
-const CGFloat kActivityIndicatorSize = 48;
-// Alpha of the presented view's background.
-const CGFloat kBackgroundAlpha = 0.5;
-}
-
 @implementation ActivityOverlayViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  self.view.backgroundColor = [UIColor colorWithWhite:0 alpha:kBackgroundAlpha];
-
-  MDCActivityIndicator* activityIndicator = [[MDCActivityIndicator alloc]
-      initWithFrame:CGRectMake(0, 0, kActivityIndicatorSize,
-                               kActivityIndicatorSize)];
-  activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:activityIndicator];
-  AddSameCenterConstraints(self.view, activityIndicator);
-  activityIndicator.cycleColors = ActivityIndicatorBrandedCycleColors();
-  [activityIndicator startAnimating];
+- (void)loadView {
+  self.view = [[ActivityOverlayView alloc] init];
 }
 
 @end
--- ios/chrome/browser/ui/first_run/signin_screen_view_controller.mm
+++ ios/chrome/browser/ui/first_run/signin_screen_view_controller.mm
@@ -4,21 +4,14 @@
 
 #import "ios/chrome/browser/ui/first_run/signin_screen_view_controller.h"
 
-#import <MaterialComponents/MaterialActivityIndicator.h>
 
 #import "ios/chrome/browser/ui/authentication/views/identity_button_control.h"
-#import "ios/chrome/browser/ui/material_components/activity_indicator.h"
-#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/browser/ui/elements/activity_overlay_view.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-// Size of the activity indicator.
-const CGFloat kActivityIndicatorSize = 48;
-}  // namespace
 
 @interface SigninScreenViewController ()
 
@@ -26,9 +19,7 @@ const CGFloat kActivityIndicatorSize = 48;
 @property(nonatomic, strong) IdentityButtonControl* identityControl;
 
 // Scrim displayed above the view when the UI is disabled.
-@property(nonatomic, strong) UIView* scrimView;
-// Activity indicator for the scrim view.
-@property(nonatomic, strong) MDCActivityIndicator* activityIndicator;
+@property(nonatomic, strong) ActivityOverlayView* overlay;
 
 @end
 
@@ -57,21 +48,12 @@ const CGFloat kActivityIndicatorSize = 48;
 
 #pragma mark - Properties
 
-- (UIView*)scrimView {
-  if (!_scrimView) {
-    _scrimView = [[UIView alloc] initWithFrame:CGRectZero];
-    _scrimView.translatesAutoresizingMaskIntoConstraints = NO;
-    _scrimView.backgroundColor = [UIColor colorNamed:kScrimBackgroundColor];
-
-    self.activityIndicator = [[MDCActivityIndicator alloc]
-        initWithFrame:CGRectMake(0, 0, kActivityIndicatorSize,
-                                 kActivityIndicatorSize)];
-    self.activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:self.activityIndicator];
-    AddSameCenterConstraints(_scrimView, self.activityIndicator);
-    self.activityIndicator.cycleColors = ActivityIndicatorBrandedCycleColors();
+- (ActivityOverlayView*)overlay {
+  if (!_overlay) {
+    _overlay = [[ActivityOverlayView alloc] init];
+    _overlay.translatesAutoresizingMaskIntoConstraints = NO;
   }
-  return _scrimView;
+  return _overlay;
 }
 
 #pragma mark - SignInScreenConsumer
@@ -95,23 +77,22 @@ const CGFloat kActivityIndicatorSize = 48;
 
 - (void)setUIEnabled:(BOOL)UIEnabled {
   if (UIEnabled) {
-    [self.scrimView removeFromSuperview];
-    self.scrimView = nil;
+    [self.overlay removeFromSuperview];
   } else {
-    [self.view addSubview:self.scrimView];
-    AddSameConstraints(self.view, self.scrimView);
-    [self.activityIndicator startAnimating];
+    [self.view addSubview:self.overlay];
+    AddSameConstraints(self.view, self.overlay);
+    [self.overlay.indicator startAnimating];
   }
 }
 
 #pragma mark - AuthenticationFlowDelegate
 
 - (void)didPresentDialog {
-  [self.activityIndicator stopAnimating];
+  [self.overlay.indicator stopAnimating];
 }
 
 - (void)didDismissDialog {
-  [self.activityIndicator startAnimating];
+  [self.overlay.indicator startAnimating];
 }
 
 #pragma mark - Private
