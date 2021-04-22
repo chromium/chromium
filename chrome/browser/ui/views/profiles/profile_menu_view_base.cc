chromium/chromium

commit 55b9122d0209277cddab25597571b245b0b95cce

Avoid GetNativeTheme() access before ProfileMenuView is in a widget.
Bug: 1056916
Change-Id: I1ed120e655dc0cd780fb403d873bef5209ecb57e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827670
Auto-Submit: Peter Kasting <pkasting@chromium.org>
Reviewed-by: Allen Bauer <kylixrd@chromium.org>
Reviewed-by: Peter Boström <pbos@chromium.org>
Commit-Queue: Peter Boström <pbos@chromium.org>
Cr-Commit-Position: refs/heads/master@{#873442}

chrome/browser/ui/views/profiles/profile_menu_view_base.cc
@@ -44,6 +44,7 @@
 #include "ui/views/controls/link.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/controls/separator.h"
+#include "ui/views/image_model_utils.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/flex_layout_types.h"
@@ -154,13 +155,18 @@ const gfx::ImageSkia ImageForMenu(const gfx::VectorIcon& icon,
   return gfx::CanvasImageSource::CreatePadded(sized_icon, gfx::Insets(padding));
 }
 
-gfx::ImageSkia SizeImageModel(const ui::ImageModel& image_model,
-                              const ui::NativeTheme* native_theme,
-                              int size) {
-  return image_model.IsImage()
-             ? CropCircle(SizeImage(image_model.GetImage().AsImageSkia(), size))
-             : ui::ThemedVectorIcon(image_model.GetVectorIcon())
-                   .GetImageSkia(native_theme, size);
+ui::ImageModel SizeImageModel(const ui::ImageModel& image_model, int size) {
+  if (image_model.IsImage()) {
+    return ui::ImageModel::FromImageSkia(
+        CropCircle(SizeImage(image_model.GetImage().AsImageSkia(), size)));
+  }
+  const ui::VectorIconModel& model = image_model.GetVectorIcon();
+  if (model.has_color()) {
+    return ui::ImageModel::FromVectorIcon(*model.vector_icon(), model.color(),
+                                          size);
+  }
+  return ui::ImageModel::FromVectorIcon(*model.vector_icon(), model.color_id(),
+                                        size);
 }
 
 // TODO(crbug.com/1146998): Adjust button size to be 16x16.
@@ -282,9 +288,9 @@ class AvatarImageView : public views::ImageView {
     ImageView::OnThemeChanged();
     constexpr int kBadgePadding = 1;
     DCHECK(!avatar_image_.IsEmpty());
-    gfx::ImageSkia sized_avatar_image =
-        SizeImageModel(avatar_image_, GetNativeTheme(),
-                       ProfileMenuViewBase::kIdentityImageSize);
+    gfx::ImageSkia sized_avatar_image = views::GetImageSkiaFromImageModel(
+        SizeImageModel(avatar_image_, ProfileMenuViewBase::kIdentityImageSize),
+        GetNativeTheme());
     if (base::FeatureList::IsEnabled(features::kNewProfilePicker)) {
       sized_avatar_image =
           AddCircularBackground(sized_avatar_image, GetBackgroundColor(),
@@ -681,17 +687,13 @@ void ProfileMenuViewBase::SetSyncInfo(const SyncInfo& sync_info,
       ChromeLayoutProvider::Get()->GetDistanceMetric(
           views::DISTANCE_RELATED_LABEL_HORIZONTAL);
   constexpr int kInsidePadding = 12;
-  constexpr int kBorderThickness = 1;
-  const int kBorderCornerRadius =
-      views::LayoutProvider::Get()->GetCornerRadiusMetric(
-          views::Emphasis::kHigh);
 
   sync_background_state_ = sync_info.background_state;
-  UpdateSyncInfoContainerBackground();
 
   sync_info_container_->RemoveAllChildViews(/*delete_children=*/true);
   sync_info_container_->SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical, gfx::Insets(), kInsidePadding));
+      views::BoxLayout::Orientation::kVertical, gfx::Insets(kInsidePadding),
+      kInsidePadding));
 
   if (description.empty()) {
     sync_info_container_->AddChildView(std::make_unique<SyncButton>(
@@ -701,13 +703,6 @@ void ProfileMenuViewBase::SetSyncInfo(const SyncInfo& sync_info,
     return;
   }
 
-  const SkColor border_color = GetNativeTheme()->GetSystemColor(
-      ui::NativeTheme::kColorId_MenuSeparatorColor);
-  // Add padding, rounded border and margins.
-  sync_info_container_->SetBorder(views::CreatePaddedBorder(
-      views::CreateRoundedRectBorder(kBorderThickness, kBorderCornerRadius,
-                                     border_color),
-      gfx::Insets(kInsidePadding)));
   sync_info_container_->SetProperty(
       views::kMarginsKey, gfx::Insets(kDefaultMargin, kMenuEdgeMargin));
 
@@ -853,9 +848,8 @@ void ProfileMenuViewBase::AddSelectableProfile(
   }
 
   DCHECK(!image_model.IsEmpty());
-  gfx::ImageSkia sized_image = SizeImageModel(image_model, GetNativeTheme(),
-                                              profiles::kMenuAvatarIconSize);
-
+  ui::ImageModel sized_image =
+      SizeImageModel(image_model, profiles::kMenuAvatarIconSize);
   views::Button* button = selectable_profiles_container_->AddChildView(
       std::make_unique<HoverButton>(
           base::BindRepeating(&ProfileMenuViewBase::ButtonPressed,
@@ -1013,9 +1007,33 @@ void ProfileMenuViewBase::OnWindowClosing() {
 
 void ProfileMenuViewBase::OnThemeChanged() {
   views::BubbleDialogDelegateView::OnThemeChanged();
-  SetBackground(views::CreateSolidBackground(GetNativeTheme()->GetSystemColor(
+
+  const auto* const native_theme = GetNativeTheme();
+  SetBackground(views::CreateSolidBackground(native_theme->GetSystemColor(
       ui::NativeTheme::kColorId_DialogBackground)));
-  UpdateSyncInfoContainerBackground();
+
+  ui::NativeTheme::ColorId bg_color;
+  switch (sync_background_state_) {
+    case SyncInfoContainerBackgroundState::kNoError:
+      sync_info_container_->SetBackground(nullptr);
+      return;
+    case SyncInfoContainerBackgroundState::kPaused:
+      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerPaused;
+      break;
+    case SyncInfoContainerBackgroundState::kError:
+      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerError;
+      break;
+    case SyncInfoContainerBackgroundState::kNoPrimaryAccount:
+      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerNoPrimaryAccount;
+  }
+  const int radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
+      views::Emphasis::kHigh);
+  sync_info_container_->SetBackground(views::CreateRoundedRectBackground(
+      native_theme->GetSystemColor(bg_color), radius));
+  sync_info_container_->SetBorder(views::CreateRoundedRectBorder(
+      1, radius,
+      native_theme->GetSystemColor(
+          ui::NativeTheme::kColorId_MenuSeparatorColor)));
 }
 
 ax::mojom::Role ProfileMenuViewBase::GetAccessibleWindowRole() {
@@ -1037,27 +1055,6 @@ void ProfileMenuViewBase::ButtonPressed(base::RepeatingClosure action) {
   DCHECK(action);
   signin_ui_util::RecordProfileMenuClick(browser()->profile());
   action.Run();
-}
-
-void ProfileMenuViewBase::UpdateSyncInfoContainerBackground() {
-  ui::NativeTheme::ColorId bg_color;
-  switch (sync_background_state_) {
-    case SyncInfoContainerBackgroundState::kNoError:
-      sync_info_container_->SetBackground(nullptr);
-      return;
-    case SyncInfoContainerBackgroundState::kPaused:
-      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerPaused;
-      break;
-    case SyncInfoContainerBackgroundState::kError:
-      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerError;
-      break;
-    case SyncInfoContainerBackgroundState::kNoPrimaryAccount:
-      bg_color = ui::NativeTheme::kColorId_SyncInfoContainerNoPrimaryAccount;
-  }
-  sync_info_container_->SetBackground(views::CreateRoundedRectBackground(
-      GetNativeTheme()->GetSystemColor(bg_color),
-      views::LayoutProvider::Get()->GetCornerRadiusMetric(
-          views::Emphasis::kHigh)));
 }
 
 // Despite ProfileMenuViewBase being a dialog, we are enforcing it to behave

chrome/browser/ui/views/hover_button.cc
@@ -11,6 +11,7 @@
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/views/hover_button_controller.h"
+#include "ui/base/models/image_model.h"
 #include "ui/events/event_constants.h"
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/background.h"
@@ -102,10 +103,10 @@ HoverButton::HoverButton(PressedCallback callback, const std::u16string& text)
 }
 
 HoverButton::HoverButton(PressedCallback callback,
-                         const gfx::ImageSkia& icon,
+                         const ui::ImageModel& icon,
                          const std::u16string& text)
     : HoverButton(std::move(callback), text) {
-  SetImage(STATE_NORMAL, icon);
+  SetImageModel(STATE_NORMAL, icon);
 }
 
 HoverButton::HoverButton(PressedCallback callback,

chrome/browser/ui/views/hover_button.h
@@ -14,14 +14,14 @@
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/metadata/metadata_header_macros.h"
 
-namespace gfx {
-class ImageSkia;
-}  // namespace gfx
-
 namespace media_router {
 FORWARD_DECLARE_TEST(CastDialogSinkButtonTest, SetTitleLabel);
 FORWARD_DECLARE_TEST(CastDialogSinkButtonTest, SetStatusLabel);
 }  // namespace media_router
+
+namespace ui {
+class ImageModel;
+}
 
 namespace views {
 class Label;
@@ -44,7 +44,7 @@ class HoverButton : public views::LabelButton {
 
   // Creates a single line hover button with an icon.
   HoverButton(PressedCallback callback,
-              const gfx::ImageSkia& icon,
+              const ui::ImageModel& icon,
               const std::u16string& text);
 
   // Creates a HoverButton with custom subviews. |icon_view| replaces the

chrome/browser/ui/views/profiles/profile_menu_view_base.h
@@ -194,8 +194,6 @@ class ProfileMenuViewBase : public content::WebContentsDelegate,
 
   void ButtonPressed(base::RepeatingClosure action);
 
-  void UpdateSyncInfoContainerBackground();
-
   Browser* const browser_;
 
   views::Button* const anchor_button_;

ui/base/models/image_model.cc
@@ -106,7 +106,7 @@ bool ImageModel::IsImage() const {
 
 gfx::Size ImageModel::Size() const {
   if (IsVectorIcon()) {
-    const int icon_size = GetVectorIcon().icon_size();
+    const int icon_size = GetVectorIcon().icon_size_;
     return gfx::Size(icon_size, icon_size);
   }
   return IsImage() ? GetImage().Size() : gfx::Size();

ui/base/models/image_model.h
@@ -43,8 +43,13 @@ class COMPONENT_EXPORT(UI_BASE) VectorIconModel {
   bool operator==(const VectorIconModel& other) const;
   bool operator!=(const VectorIconModel& other) const;
 
+  const gfx::VectorIcon* vector_icon() const { return vector_icon_; }
+  int icon_size() const { return icon_size_; }
+  int color_id() const { return absl::get<int>(color_); }
+  SkColor color() const { return absl::get<SkColor>(color_); }
+  bool has_color() const { return absl::holds_alternative<SkColor>(color_); }
+
  private:
-  friend class ThemedVectorIcon;
   friend class ImageModel;
 
   VectorIconModel(const gfx::VectorIcon& vector_icon,
@@ -55,12 +60,6 @@ class COMPONENT_EXPORT(UI_BASE) VectorIconModel {
   VectorIconModel(const gfx::VectorIcon& vector_icon,
                   SkColor color,
                   int icon_size);
-
-  const gfx::VectorIcon* vector_icon() const { return vector_icon_; }
-  int icon_size() const { return icon_size_; }
-  int color_id() const { return absl::get<int>(color_); }
-  SkColor color() const { return absl::get<SkColor>(color_); }
-  bool has_color() const { return absl::holds_alternative<SkColor>(color_); }
 
   const gfx::VectorIcon* vector_icon_ = nullptr;
   int icon_size_ = 0;

commit 3c8b1597866e13e35414be16cba0760951340786

[Profile menu] Fix wrong padding for sync info
This CL fixes a glitch in the profile menu when the user is syncing,
introduced in CL 2827670.

Before: https://screenshot.googleplex.com/4DsPVcjWx5dCs4e
After: https://screenshot.googleplex.com/BtLDGTPfbzHor36

Fixed: 1200778
Change-Id: Ic7f2eaf0a24d3a545406519945af0e91059b134a
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844582
Auto-Submit: Jan Krcal <jkrcal@chromium.org>
Commit-Queue: Peter Kasting <pkasting@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Cr-Commit-Position: refs/heads/master@{#875193}

chrome/browser/ui/views/profiles/profile_menu_view_base.cc
@@ -83,6 +83,8 @@ constexpr int kMinimumScrollableContentHeight = 40;
 // the menu items.
 constexpr int kMenuEdgeMargin = 16;
 
+constexpr int kSyncInfoInsidePadding = 12;
+
 gfx::ImageSkia SizeImage(const gfx::ImageSkia& image, int size) {
   return gfx::ImageSkiaOperations::CreateResizedImage(
       image, skia::ImageOperations::RESIZE_BEST, gfx::Size(size, size));
@@ -686,14 +688,12 @@ void ProfileMenuViewBase::SetSyncInfo(const SyncInfo& sync_info,
   const int kDescriptionIconSpacing =
       ChromeLayoutProvider::Get()->GetDistanceMetric(
           views::DISTANCE_RELATED_LABEL_HORIZONTAL);
-  constexpr int kInsidePadding = 12;
-
   sync_background_state_ = sync_info.background_state;
 
   sync_info_container_->RemoveAllChildViews(/*delete_children=*/true);
   sync_info_container_->SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical, gfx::Insets(kInsidePadding),
-      kInsidePadding));
+      views::BoxLayout::Orientation::kVertical, gfx::Insets(),
+      kSyncInfoInsidePadding));
 
   if (description.empty()) {
     sync_info_container_->AddChildView(std::make_unique<SyncButton>(
@@ -1016,6 +1016,7 @@ void ProfileMenuViewBase::OnThemeChanged() {
   switch (sync_background_state_) {
     case SyncInfoContainerBackgroundState::kNoError:
       sync_info_container_->SetBackground(nullptr);
+      // Return early to avoid a visible padded border, set below.
       return;
     case SyncInfoContainerBackgroundState::kPaused:
       bg_color = ui::NativeTheme::kColorId_SyncInfoContainerPaused;
@@ -1030,10 +1031,12 @@ void ProfileMenuViewBase::OnThemeChanged() {
       views::Emphasis::kHigh);
   sync_info_container_->SetBackground(views::CreateRoundedRectBackground(
       native_theme->GetSystemColor(bg_color), radius));
-  sync_info_container_->SetBorder(views::CreateRoundedRectBorder(
-      1, radius,
-      native_theme->GetSystemColor(
-          ui::NativeTheme::kColorId_MenuSeparatorColor)));
+  sync_info_container_->SetBorder(views::CreatePaddedBorder(
+      views::CreateRoundedRectBorder(
+          1, radius,
+          native_theme->GetSystemColor(
+              ui::NativeTheme::kColorId_MenuSeparatorColor)),
+      gfx::Insets(kSyncInfoInsidePadding)));
 }
 
 ax::mojom::Role ProfileMenuViewBase::GetAccessibleWindowRole() {