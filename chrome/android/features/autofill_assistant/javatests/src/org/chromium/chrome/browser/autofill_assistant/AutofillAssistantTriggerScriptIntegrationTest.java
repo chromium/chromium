f728f6338fc3a3179002d08b054d20af6458d286
Andrey Zaytsev <andzaytsev@google.com>
2021-04-22 11:21:46 +0000

    Disable flaky AutofillAssistantTriggerScriptIntegrationTest#setReturningUserFlag
    Bug: 1199416
    Change-Id: Iafb635b838090802cef00ed80091a2853243a150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846504
    Owners-Override: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#875112}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantTriggerScriptIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantTriggerScriptIntegrationTest.java
@@ -43,6 +43,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipIcon;
@@ -209,8 +210,9 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
     @Test
     @MediumTest
     @Features.EnableFeatures(ChromeFeatureList.AUTOFILL_ASSISTANT_PROACTIVE_HELP)
-    @DisableIf.Build(message = "See https://crbug.com/1199849",
-            sdk_is_greater_than = VERSION_CODES.O_MR1)
+    @DisableIf.
+    Build(message = "See https://crbug.com/1199849", sdk_is_greater_than = VERSION_CODES.O_MR1)
+    @FlakyTest(message = "crbug.com/1199416")
     public void setReturningUserFlag() {
         TriggerScriptProto.Builder firstTimeTriggerScript =
                 TriggerScriptProto.newBuilder()
