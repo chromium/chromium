abf98485688e7d13432c680cdb2244738e0a1787
Bartek Nowierski <bartekn@chromium.org>
2021-04-22 02:35:31 +0000

    [PA] Stop disabling GigaCage on Windows <8.1
    Per crbug.com/1101421#c14 this will add 32MiB of commit charge per
    process on those systems (GigaCage size is now 16GiB).
    
    Bug: 1200702
    Change-Id: Ie09224db652083a43f3a713d984b950d811ed546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838410
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874986}

--- base/allocator/partition_allocator/partition_alloc_features.h
+++ base/allocator/partition_allocator/partition_alloc_features.h
@@ -7,22 +7,10 @@
 
 #include "base/allocator/buildflags.h"
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
-#include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/base_export.h"
 #include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
-#include "build/build_config.h"
-
-#if defined(OS_WIN)
-// VersionHelpers.h depends on Windows.h.
-#include <Windows.h>
-// For IsWindows8Point1OrGreater().
-#include <VersionHelpers.h>
-#endif
 
 namespace base {
-
-struct Feature;
 
 namespace features {
 
@@ -39,31 +27,8 @@ extern const BASE_EXPORT Feature kPartitionAllocPCScanMUAwareScheduler;
 
 extern const BASE_EXPORT Feature kPartitionAllocLazyCommit;
 
+// TODO(bartekn): Clean up.
 ALWAYS_INLINE bool IsPartitionAllocGigaCageEnabled() {
-#if defined(PA_HAS_64_BITS_POINTERS) && defined(OS_WIN)
-  // Lots of crashes (at PartitionAddressSpace::Init) occur
-  // when enabling GigaCage on Windows whose version is smaller than 8.1,
-  // because PTEs for reserved memory counts against commit limit. See
-  // https://crbug.com/1101421.
-  // TODO(tasak): this windows version check is the same as GetRandomPageBase()
-  // (address_space_randomization.cc). Refactor the code to avoid the
-  // duplication.
-  static bool is_windows_version_checked = false;
-  // Don't assign directly IsWindows8Point1OrGreater() to a static local
-  // variable, because the initial value is not trivial and the assignment needs
-  // thread-safe static-local initializer on Windows. (i.e. Init_thread_header)
-  // This causes issues when used on the allocation path (see
-  // crbug.com/1126432). As we don't use atomics here, this may end up querying
-  // the version multiple times, which is fine, as this operation is idempotent,
-  // with no side-effects.
-  static bool recent_enough_windows_version = false;
-  if (!is_windows_version_checked) {
-    recent_enough_windows_version = IsWindows8Point1OrGreater();
-    is_windows_version_checked = true;
-  }
-  if (!recent_enough_windows_version)
-    return false;
-#endif  // defined(PA_HAS_64_BITS_POINTERS) && defined(OS_WIN)
   return true;
 }
 
