313007824f14410e387560266bcaafb2c85f5fc8
My Nguyen <myy@chromium.org>
2021-04-23 05:32:38 +0000

    Revert "Remove LOG statements."
    This reverts commit 4b75f4b2ad38e18297df53fdd1a72dbeb3287efc.
    
    Reason for revert: Revert this to revert https://crrev.com/c/2724978
    
    Original change's description:
    > Remove LOG statements.
    >
    > Bug: 1157161
    > Change-Id: I16a1542c83e439d619ecaa318e3946f43d84d938
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847554
    > Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    > Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    > Auto-Submit: Simon Pelchat <spelchat@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#875503}
    
    Bug: 1201988
    Change-Id: Ie0014e67244303c731b6742b3dbc2f62fe4d1f82
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847832
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: My Nguyen <myy@chromium.org>
    Owners-Override: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875533}

--- chrome/browser/navigation_predictor/navigation_predictor.cc
+++ chrome/browser/navigation_predictor/navigation_predictor.cc
@@ -104,7 +104,10 @@ void NavigationPredictor::ReportNewAnchorElements(
   PageAnchorsMetricsObserver::AnchorsData* data =
       PageAnchorsMetricsObserver::AnchorsData::FromWebContents(web_contents);
   DCHECK(data);
+  // TODO: rm
+  LOG(ERROR) << "reporting new anchors:";
   for (auto& element : elements) {
+    LOG(ERROR) << element->target_url;
     data->number_of_anchors_++;
     if (element->contains_image) {
       data->number_of_anchors_contains_image_++;
@@ -137,6 +140,8 @@ void NavigationPredictor::ReportNewAnchorElements(
     }
     tracked_anchor_id_to_index_[anchor_id] = tracked_anchor_id_to_index_.size();
   }
+  LOG(ERROR) << "done reporting new anchors";
+  LOG(ERROR) << "";
 }
 
 void NavigationPredictor::ReportAnchorElementClick(
@@ -242,6 +247,13 @@ void NavigationPredictor::ReportAnchorElementsEnteredViewport(
   DCHECK(service);
   content::WebContents* web_contents =
       content::WebContents::FromRenderFrameHost(render_frame_host());
+  // TODO: rm
+  LOG(ERROR) << "reporting predictions:";
+  for (auto& pred : new_predictions) {
+    LOG(ERROR) << pred;
+  }
+  LOG(ERROR) << "done reporting predictions:";
+  LOG(ERROR) << "";
   service->OnPredictionUpdated(
       web_contents, document_url,
       NavigationPredictorKeyedService::PredictionSource::
