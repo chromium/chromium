00f7913c75945cc630cea046a3b0277c4d5a59b9
Mugdha Lakhani <nator@chromium.org>
2021-04-23 15:23:53 +0000

    [AW] Add Trust tokens flag to developer UI
    Expose the flag to enable Trust tokens on WebView via the developer
    UI. This also adds a GN target to generate Java counterparts for the
    native network service features.
    
    Bug: 1201780
    Change-Id: I1346e0adaccbbc42f71f3a9c7ded57850a9dda21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846518
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875675}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -11,6 +11,7 @@ import org.chromium.components.metrics.MetricsSwitches;
 import org.chromium.components.viz.common.VizFeatures;
 import org.chromium.gpu.config.GpuFeatures;
 import org.chromium.gpu.config.GpuSwitches;
+import org.chromium.services.network.NetworkServiceFeatures;
 import org.chromium.ui.base.UiFeatures;
 
 /**
@@ -126,5 +127,7 @@ public final class ProductionSupportedFlagList {
                     "Enables the Form Controls visual improvements and dark mode."),
             Flag.baseFeature(
                     BlinkFeatures.LAYOUT_NG_TABLE, "Enables Blink's next generation table layout."),
+            Flag.baseFeature(
+                    NetworkServiceFeatures.TRUST_TOKENS, "Enables the prototype Trust Tokens API."),
     };
 }
