7ce7c1173881761a7f6899f133e19f19f4fe3793
Jochen Eisinger <jochen@chromium.org>
2021-04-23 04:59:09 +0000

    Add missing microtasks scope to security interstitials
    Make it explicit that microtasks won't be executed when installing the
    security interstitial page controller.
    
    Bug: 728583
    Change-Id: I27070bed6970788ceb5063bf0966c2df93206307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847340
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875524}

--- components/security_interstitials/content/renderer/security_interstitial_page_controller.cc
+++ components/security_interstitials/content/renderer/security_interstitial_page_controller.cc
@@ -23,6 +23,8 @@ void SecurityInterstitialPageController::Install(
     content::RenderFrame* render_frame) {
   v8::Isolate* isolate = blink::MainThreadIsolate();
   v8::HandleScope handle_scope(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   v8::Local<v8::Context> context =
       render_frame->GetWebFrame()->MainWorldScriptContext();
   if (context.IsEmpty())
