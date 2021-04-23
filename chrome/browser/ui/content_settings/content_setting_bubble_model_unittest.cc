84d67a8e204f25c3951c7836af84c2002b61f21b
Mikel Astiz <mastiz@chromium.org>
2021-04-23 14:12:59 +0000

    Migrate tests away from TestingProfile::CreateHistoryService()
    This API is problematic and discouraged in favor of using
    TestingProfile::Builder to set up custom factories. This patch tackles
    /chrome/browser/ui/content_settings.
    
    This CL was uploaded by git cl split.
    
    R=andypaicu@chromium.org
    
    Bug: 1106699
    Change-Id: I2f4b0e9601b01cbe530a24c75e1920989fc577c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844594
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875652}

--- chrome/browser/ui/content_settings/content_setting_bubble_model_unittest.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model_unittest.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/content_settings/page_specific_content_settings_delegate.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/custom_handlers/test_protocol_handler_registry_delegate.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
@@ -68,6 +69,11 @@ class ContentSettingBubbleModelTest : public ChromeRenderViewHostTestHarness {
         std::make_unique<chrome::PageSpecificContentSettingsDelegate>(
             web_contents()));
     InfoBarService::CreateForWebContents(web_contents());
+  }
+
+  TestingProfile::TestingFactories GetTestingFactories() const override {
+    return {{HistoryServiceFactory::GetInstance(),
+             HistoryServiceFactory::GetDefaultFactory()}};
   }
 
   std::string GetDefaultAudioDevice() {
@@ -252,8 +258,6 @@ TEST_F(ContentSettingBubbleModelTest, BlockedMediastreamMicAndCamera) {
 // Tests whether a changed setting in the setting bubble is displayed again when
 // the bubble is re-opened.
 TEST_F(ContentSettingBubbleModelTest, MediastreamContentBubble) {
-  ASSERT_TRUE(profile()->CreateHistoryService());
-
   // Required to break dependency on BrowserMainLoop.
   MediaCaptureDevicesDispatcher::GetInstance()->
       DisableDeviceEnumerationForTesting();
@@ -735,8 +739,6 @@ class GeolocationContentSettingBubbleModelTest
 };
 
 TEST_F(GeolocationContentSettingBubbleModelTest, Geolocation) {
-  ASSERT_TRUE(profile()->CreateHistoryService());
-
 #if defined(OS_MAC)
   auto fake_geolocation_manager =
       std::make_unique<device::FakeGeolocationManager>();
