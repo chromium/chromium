d1d15d84564f7ad0ac8858bb34f63ee1b4085dcb
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-04-24 09:14:49 +0000

    [M91] Add menu minor icon color config
    This is just a cherry-pick from main to m91.
    Cherry-picking to m91 is approved in b:181614585.
    
    
    This CL adds the way to set the color of “minor icon”.
    If the color is not explicitly set, no behavior is changed by this CL (fallback to the default color).
    If it's set, the color is overridden with the specified color.
    
    TEST=MenuItemViewUnitTest.MinorIconColor
    BUG=b:181614585
    
    (cherry picked from commit ffefc50b04be5c2b0ca002c127fcd802dcd43081)
    
    Change-Id: I640dc6b3e3a2998c8f44944831bb004cabfc7b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786643
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#870833}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846166
    Cr-Commit-Position: refs/branch-heads/4472@{#385}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- ui/views/controls/menu/test_menu_item_view.h
+++ ui/views/controls/menu/test_menu_item_view.h
@@ -21,6 +21,8 @@ class TestMenuItemView : public MenuItemView {
 
   using MenuItemView::AddEmptyMenus;
 
+  using MenuItemView::GetMinorIconColor;
+
   void set_has_mnemonics(bool has_mnemonics) { has_mnemonics_ = has_mnemonics; }
 
   bool show_mnemonics() { return show_mnemonics_; }
