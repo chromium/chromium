d5b258d7e19379ddeb7b39ceb0002b0ac002db14
spdonghao <spdonghao@chromium.org>
2021-04-26 21:35:54 +0000

    [Sheriff] Disable TrustedWebActivityPreferencesUiTest#testSingleCategoryManagedBy on O.
    Bug: 1202711
    Change-Id: I6a621af625426f238b28041fcfc798b1ee0cbcd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2849186
    Reviewed-by: Hao Dong <spdonghao@google.com>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Owners-Override: Hao Dong <spdonghao@google.com>
    Cr-Commit-Position: refs/heads/master@{#876309}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPreferencesUiTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPreferencesUiTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.browserservices.permissiondelegation;
 
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 
 import androidx.test.filters.SmallTest;
@@ -16,6 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeApplicationImpl;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -64,7 +66,10 @@ public class TrustedWebActivityPreferencesUiTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    public void testSingleCategoryManagedBy() throws Exception {
+    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.P,
+            message = "This test is disabled on Android O because of https://crbug.com/1202711")
+    public void
+    testSingleCategoryManagedBy() throws Exception {
         final String site = "http://example.com";
         final Origin origin = Origin.create(site);
 
