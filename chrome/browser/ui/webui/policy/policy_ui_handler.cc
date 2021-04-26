ba3914fc2fd470593e8b8e318dee3d28cee40416
Zain Afzal <zafzal@google.com>
2021-04-26 09:12:20 +0000

    Fix usage of deprecated DeepCopy function
    This was causing compile failures due to crrev/c/2843358 which renamed
    DeepCopy to Clone. See bug for more information on build failures.
    
    Change-Id: I9f7b735b191139fa06c939dd074263342930b24f
    Bug: 1202542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850026
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Owners-Override: Zain Afzal <zafzal@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/master@{#876090}

--- chrome/browser/ui/webui/policy/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy/policy_ui_handler.cc
@@ -1072,7 +1072,8 @@ base::Value PolicyUIHandler::GetPolicyValues() {
   if (updater_policies_) {
     return policy::ArrayPolicyConversions(std::move(client))
         .EnableConvertValues(true)
-        .WithUpdaterPolicies(updater_policies_->DeepCopy())
+        .WithUpdaterPolicies(
+            std::make_unique<policy::PolicyMap>(updater_policies_->Clone()))
         .WithUpdaterPolicySchemas(GetGoogleUpdatePolicySchemas())
         .ToValue();
   }
