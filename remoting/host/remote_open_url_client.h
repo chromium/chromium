8a4231a68ff7598d73327525015c035feb1c6386
Yuwei Huang <yuweih@chromium.org>
2021-04-26 23:26:41 +0000

    Reland "[remoting host] remote_open_url binary implementation"
    This is a reland of aed144758c960978d36605dfdfb71a503a097615.
    
    Reland with fix: Factor out a remote_open_url_support source set and
    make remote_open_url depend on it instead of `:host` so that it doesn't
    cause funky link error on the win-archive-dbg builder when building
    some unused Windows-specific components.
    
    Original change's description:
    > [remoting host] remote_open_url binary implementation
    >
    > This CL implements a simple standalone binary that opens a URL on the
    > client by making an IPC to the host process, which will in turn forward
    > the request to the client via WebRTC data channel.
    >
    > Bug: b:183135000
    > Change-Id: I12a98dd661f7803ec08c037281057e06022c7ff3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842963
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#875914}
    
    Bug: b:183135000
    Change-Id: I04da0107712e9b368b959ec4d7c3bd6b35869f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851578
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876350}

---
+++ remoting/host/remote_open_url_client.h
@@ -0,0 +1,37 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_H_
+#define REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_H_
+
+#include "base/callback.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "remoting/host/mojom/remote_url_opener.mojom.h"
+
+namespace remoting {
+
+// A helper to allow the standalone open URL binary to open a URL remotely and
+// handle local fallback.
+class RemoteOpenUrlClient final {
+ public:
+  RemoteOpenUrlClient();
+  ~RemoteOpenUrlClient();
+
+  // Opens the URL and calls |done| when done.
+  void OpenUrl(const GURL& url, base::OnceClosure done);
+
+  RemoteOpenUrlClient(const RemoteOpenUrlClient&) = delete;
+  RemoteOpenUrlClient& operator=(const RemoteOpenUrlClient&) = delete;
+
+ private:
+  void OnOpenUrlResponse(mojom::OpenUrlResult result);
+
+  GURL url_;
+  base::OnceClosure done_;
+  mojo::Remote<mojom::RemoteUrlOpener> remote_;
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_H_
