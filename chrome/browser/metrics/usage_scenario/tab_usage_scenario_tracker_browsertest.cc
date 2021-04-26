b34c34f059822cc559036b0ac73fd231f93f237f
Peter Kasting <pkasting@chromium.org>
2021-04-26 20:09:01 +0000

    Tear down TabUsageScenarioTracker earlier in browsertest.
    This parallels where it's created, and prevents attempts to access
    freed objects if the message loop is spun later during test shutdown.
    
    Found as part of work on
    https://chromium-review.googlesource.com/c/chromium/src/+/2154163 .
    
    Bug: none
    Change-Id: I3459652722af4e300d7936b03098dcecf97021a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851100
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876290}

--- chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
+++ chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
@@ -126,6 +126,7 @@ class TabUsageScenarioTrackerBrowserTest : public InProcessBrowserTest {
 
   void TearDownOnMainThread() override {
     tab_stats_tracker_->RemoveObserver(tab_usage_scenario_tracker_.get());
+    tab_usage_scenario_tracker_.reset();
     InProcessBrowserTest::TearDownOnMainThread();
   }
 
