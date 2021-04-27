66cad230279ed343d845d63ce93d7b59cbe6ace8
Kent Tamura <tkent@chromium.org>
2021-04-27 11:01:55 +0000

    SVG Text NG: Fix two issues on dx/dy handling
    * If 'x'/'y' attribute value is specified to a character,
      shift.x/shift.y should be reset to 0 respectively.
      https://github.com/w3c/svgwg/issues/846
    
    * If 'x' and 'dx' or 'y' and 'dy' are specified to a character,
      do not ignore 'dx'/'dy' value.
      https://github.com/w3c/svgwg/issues/845
    
    Bug: 1179585
    Change-Id: I78db10e3cb5dc2c6d8fb03bad0ee0a0ff64b201b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852942
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876533}

--- third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.cc
@@ -246,6 +246,11 @@ void NGSVGTextLayoutAlgorithm::AdjustPositionsDxDy(
   ResolvedIterator iterator(inline_node_.SVGCharacterDataList());
   for (wtf_size_t i = 0; i < addressable_count_; ++i) {
     const NGSVGCharacterData& resolve = iterator.AdvanceTo(i);
+    // https://github.com/w3c/svgwg/issues/846
+    if (resolve.HasX())
+      shift.SetX(0.0f);
+    if (resolve.HasY())
+      shift.SetY(0.0f);
     // 2.1. If resolve_x[i] is unspecified, set it to 0. If resolve_y[i] is
     // unspecified, set it to 0.
     // https://github.com/w3c/svgwg/issues/271
@@ -513,12 +518,14 @@ void NGSVGTextLayoutAlgorithm::AdjustPositionsXY(
     const NGSVGCharacterData& resolve = iterator.AdvanceTo(i);
     // 3.1. If resolved_x[index] is set, then let
     // shift.x = resolved_x[index] − result.x[index].
+    // https://github.com/w3c/svgwg/issues/845
     if (resolve.HasX())
-      shift.SetX(resolve.x * scaling_factor - *result_[i].x);
+      shift.SetX(resolve.x * scaling_factor - css_positions_[i].X());
     // 3.2. If resolved_y[index] is set, then let
     // shift.y = resolved_y[index] − result.y[index].
+    // https://github.com/w3c/svgwg/issues/845
     if (resolve.HasY())
-      shift.SetY(resolve.y * scaling_factor - *result_[i].y);
+      shift.SetY(resolve.y * scaling_factor - css_positions_[i].Y());
     // 3.3. Let result.x[index] = result.x[index] + shift.x and
     // result.y[index] = result.y[index] + shift.y.
     result_[i].x = *result_[i].x + shift.X();
