fb58f83eb38734c533fed166802785ec1684bdd7
Dale Curtis <dalecurtis@chromium.org>
2021-04-27 08:32:22 +0000

    Merge M91: "[WebCodecs] Fix issues when reset() happens during flush()."
    reset() clears the active configuration, so we should not attempt
    to reinitialize if reset() was called during flush().
    
    R=​eugene
    
    (cherry picked from commit 6058decceddbe0b3a5023298b4914e61cfac4330)
    
    Fixed: 1200768, 1199245
    Change-Id: Iac34480ba859ab156d6a93c943a7ffce9304b1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842784
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#874546}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852885
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4472@{#453}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

---
+++ third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
@@ -0,0 +1,170 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_function.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_tester.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_audio_decoder_config.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_audio_decoder_init.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_video_decoder_config.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_video_decoder_init.h"
+#include "third_party/blink/renderer/modules/webcodecs/audio_decoder.h"
+#include "third_party/blink/renderer/modules/webcodecs/video_decoder.h"
+#include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
+
+namespace blink {
+
+namespace {
+
+class MockFunction : public ScriptFunction {
+ public:
+  static testing::StrictMock<MockFunction>* Create(ScriptState* script_state) {
+    return MakeGarbageCollected<testing::StrictMock<MockFunction>>(
+        script_state);
+  }
+
+  v8::Local<v8::Function> Bind() { return BindToV8Function(); }
+
+  MOCK_METHOD1(Call, ScriptValue(ScriptValue));
+
+ protected:
+  explicit MockFunction(ScriptState* script_state)
+      : ScriptFunction(script_state) {}
+};
+
+template <class T>
+class DecoderTemplateTest : public testing::Test {
+ public:
+  DecoderTemplateTest() = default;
+  ~DecoderTemplateTest() override = default;
+
+  typename T::ConfigType* CreateConfig();
+  typename T::InitType* CreateInit(MockFunction* output_callback,
+                                   MockFunction* error_callback);
+
+  T* CreateDecoder(ScriptState*, const typename T::InitType*, ExceptionState&);
+};
+
+template <>
+AudioDecoderConfig* DecoderTemplateTest<AudioDecoder>::CreateConfig() {
+  auto* config = MakeGarbageCollected<AudioDecoderConfig>();
+  config->setCodec("mp3");
+  config->setSampleRate(48000);
+  config->setNumberOfChannels(2);
+  return config;
+}
+
+template <>
+AudioDecoder* DecoderTemplateTest<AudioDecoder>::CreateDecoder(
+    ScriptState* script_state,
+    const AudioDecoderInit* init,
+    ExceptionState& exception_state) {
+  return MakeGarbageCollected<AudioDecoder>(script_state, init,
+                                            exception_state);
+}
+
+template <>
+AudioDecoderInit* DecoderTemplateTest<AudioDecoder>::CreateInit(
+    MockFunction* output_callback,
+    MockFunction* error_callback) {
+  auto* init = MakeGarbageCollected<AudioDecoderInit>();
+  init->setOutput(V8AudioFrameOutputCallback::Create(output_callback->Bind()));
+  init->setError(V8WebCodecsErrorCallback::Create(error_callback->Bind()));
+  return init;
+}
+
+template <>
+VideoDecoderConfig* DecoderTemplateTest<VideoDecoder>::CreateConfig() {
+  auto* config = MakeGarbageCollected<VideoDecoderConfig>();
+  config->setCodec("vp09.00.10.08");
+  return config;
+}
+
+template <>
+VideoDecoderInit* DecoderTemplateTest<VideoDecoder>::CreateInit(
+    MockFunction* output_callback,
+    MockFunction* error_callback) {
+  auto* init = MakeGarbageCollected<VideoDecoderInit>();
+  init->setOutput(V8VideoFrameOutputCallback::Create(output_callback->Bind()));
+  init->setError(V8WebCodecsErrorCallback::Create(error_callback->Bind()));
+  return init;
+}
+
+template <>
+VideoDecoder* DecoderTemplateTest<VideoDecoder>::CreateDecoder(
+    ScriptState* script_state,
+    const VideoDecoderInit* init,
+    ExceptionState& exception_state) {
+  return MakeGarbageCollected<VideoDecoder>(script_state, init,
+                                            exception_state);
+}
+
+using DecoderTemplateImplementations =
+    testing::Types<AudioDecoder, VideoDecoder>;
+
+TYPED_TEST_SUITE(DecoderTemplateTest, DecoderTemplateImplementations);
+
+TYPED_TEST(DecoderTemplateTest, BasicConstruction) {
+  V8TestingScope v8_scope;
+
+  auto* output_callback = MockFunction::Create(v8_scope.GetScriptState());
+  auto* error_callback = MockFunction::Create(v8_scope.GetScriptState());
+
+  auto* decoder =
+      this->CreateDecoder(v8_scope.GetScriptState(),
+                          this->CreateInit(output_callback, error_callback),
+                          v8_scope.GetExceptionState());
+  ASSERT_TRUE(decoder);
+  EXPECT_FALSE(v8_scope.GetExceptionState().HadException());
+}
+
+TYPED_TEST(DecoderTemplateTest, ResetDuringFlush) {
+  V8TestingScope v8_scope;
+
+  // Create a decoder.
+  auto* output_callback = MockFunction::Create(v8_scope.GetScriptState());
+  auto* error_callback = MockFunction::Create(v8_scope.GetScriptState());
+
+  auto* decoder =
+      this->CreateDecoder(v8_scope.GetScriptState(),
+                          this->CreateInit(output_callback, error_callback),
+                          v8_scope.GetExceptionState());
+  ASSERT_TRUE(decoder);
+  ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
+
+  // Configure the decoder.
+  decoder->configure(this->CreateConfig(), v8_scope.GetExceptionState());
+  ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
+
+  // flush() to ensure configure completes.
+  {
+    auto promise = decoder->flush(v8_scope.GetExceptionState());
+    ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
+
+    ScriptPromiseTester tester(v8_scope.GetScriptState(), promise);
+    tester.WaitUntilSettled();
+    ASSERT_TRUE(tester.IsFulfilled());
+  }
+
+  // flush() again but reset() before it gets started.
+  {
+    auto promise = decoder->flush(v8_scope.GetExceptionState());
+    ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
+    decoder->reset(v8_scope.GetExceptionState());
+    ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
+
+    ScriptPromiseTester tester(v8_scope.GetScriptState(), promise);
+    tester.WaitUntilSettled();
+    ASSERT_TRUE(tester.IsFulfilled());
+  }
+}
+
+// Note: AudioDecoder and VideoDecoder specific tests should be put in
+// audio_decoder_test.cc and video_decoder_test.cc respectively.
+
+}  // namespace
+
+}  // namespace blink
