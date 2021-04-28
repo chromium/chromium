cdf3816f9b5a7bc5539469092b4529d089ef19d9
Swapnil Gupta <swapnilgupta@google.com>
2021-04-28 12:49:47 +0000

    Revert "WebGPU CTS: Generate html files for ref tests"
    This reverts commit e91750afe4ae4bd5a69537453011d63af8e45695.
    
    Reason for revert: crbug.com/1203660
    
    Original change's description:
    > WebGPU CTS: Generate html files for ref tests
    >
    > Bug: 1083478
    > Change-Id: I10bdab7f54334bbcdac45b6e2c2cf1eb94529fd8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853827
    > Commit-Queue: Austin Eng <enga@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#876901}
    
    Bug: 1083478, 1203660
    Change-Id: I89455b98f9e1ac30f894996495d71029916005c4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853919
    Auto-Submit: Swapnil Gupta <swapnilgupta@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Owners-Override: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#877010}

--- third_party/webgpu-cts/scripts/regenerate_internal_cts_html.py
+++ third_party/webgpu-cts/scripts/regenerate_internal_cts_html.py
@@ -5,7 +5,6 @@
 
 import argparse
 import ast
-import errno
 import tempfile
 import os
 import shutil
@@ -18,23 +17,10 @@ third_party_dir = os.path.dirname(
 from compile_src import compile_src_for_node
 
 
-def check_or_write_file(filepath, content, check):
-    if check:
-        with open(filepath, 'rb') as dst:
-            if dst.read() != content:
-                raise RuntimeError(
-                    '%s is out of date. Please re-run //third_party/webgpu-cts/scripts/run_regenerate_internal_cts_html.py\n'
-                    % filepath)
-    else:
-        with open(filepath, 'wb') as dst:
-            dst.write(content)
-
-
-def generate_internal_cts_html(check):
+def generate_internal_cts_html():
     split_list_fd, split_list_fname = tempfile.mkstemp()
     cts_html_fd, cts_html_fname = tempfile.mkstemp()
     js_out_dir = tempfile.mkdtemp()
-    contents = None
     try:
         print('WebGPU CTS: Extracting expectation names...')
         old_sys_path = sys.path
@@ -76,7 +62,7 @@ def generate_internal_cts_html(check):
         finally:
             sys.path = old_sys_path
 
-        print('WebGPU CTS: Generating cts.html...')
+        print('WebGPU CTS: Generating cts.html contents...')
         cmd = [
             os.path.join(js_out_dir,
                          'common/tools/gen_wpt_cts_html.js'), cts_html_fname,
@@ -89,75 +75,12 @@ def generate_internal_cts_html(check):
         print(RunNode(cmd))
 
         with open(cts_html_fname, 'rb') as f:
-            contents = f.read()
+            return f.read()
 
     finally:
         os.close(split_list_fd)
         os.close(cts_html_fd)
         shutil.rmtree(js_out_dir)
-
-    out_cts_html = os.path.join(third_party_dir, 'blink', 'web_tests',
-                                'wpt_internal', 'webgpu', 'cts.html')
-
-    if not contents:
-        raise RuntimeError('Failed to generate %s' % out_cts_html)
-
-    check_or_write_file(out_cts_html, contents, check=check)
-
-
-def generate_reftest_html(check):
-    # Update this to add/remove subdirectories to check.
-    sub_dirs = ['web_platform']
-
-    for sub_dir in sub_dirs:
-        html_search_dir = os.path.join(third_party_dir, 'webgpu-cts', 'src',
-                                       'src', 'webgpu', sub_dir)
-        wpt_internal_dir = os.path.join(third_party_dir, 'blink', 'web_tests',
-                                        'wpt_internal', 'webgpu', sub_dir)
-
-        if not check:
-            shutil.rmtree(wpt_internal_dir)
-        print('WebGPU CTS: Generating HTML tests from %s...' % html_search_dir)
-        for root, dirnames, filenames in os.walk(html_search_dir):
-            for filename in filenames:
-                if filename.endswith('.html'):
-                    filepath = os.path.join(root, filename)
-                    relative_dir = os.path.relpath(os.path.dirname(filepath),
-                                                   html_search_dir)
-                    dst_dir = os.path.join(wpt_internal_dir, relative_dir)
-                    gen_base_dir = os.path.join(
-                        '/gen/third_party/webgpu-cts/src/webgpu', sub_dir,
-                        relative_dir)
-                    gen_base_dir = gen_base_dir.replace('\\', '/') + '/'
-
-                    try:
-                        os.makedirs(dst_dir)
-                    except OSError as e:
-                        if e.errno == errno.EEXIST and os.path.isdir(dst_dir):
-                            pass
-                        else:
-                            raise
-
-                    with open(filepath, 'rb') as src:
-                        src_content = src.read()
-
-                    # Find the starting html tag
-                    i = src_content.find('<html')
-                    assert i != -1
-
-                    # Then find the end of the starting html tag
-                    i = src_content.find('>', i)
-                    assert i != -1
-
-                    # Bump the index just past the starting <html> tag
-                    i = i + 1
-
-                    base_tag = b'\n  <base href="%s" />' % gen_base_dir
-                    dst_content = src_content[:i] + base_tag + src_content[i:]
-
-                    check_or_write_file(os.path.join(dst_dir, filename),
-                                        dst_content,
-                                        check=check)
 
 
 if __name__ == '__main__':
@@ -168,8 +91,22 @@ if __name__ == '__main__':
     parser.add_argument('--stamp', help='Stamp file to write after success.')
     args = parser.parse_args()
 
-    generate_internal_cts_html(check=args.check)
-    generate_reftest_html(check=args.check)
+    out_cts_html = os.path.join(third_party_dir, 'blink', 'web_tests',
+                                'wpt_internal', 'webgpu', 'cts.html')
+    contents = generate_internal_cts_html()
+    if not contents:
+        raise RuntimeError('Failed to generate %s' % out_cts_html)
+
+    if args.check:
+        with open(out_cts_html, 'rb') as f:
+            if f.read() != contents:
+                print(contents)
+                raise RuntimeError(
+                    '%s is out of date. Please re-run //third_party/webgpu-cts/scripts/regenerate_internal_cts_html.py\n'
+                    % out_cts_html)
+    else:
+        with open(out_cts_html, 'wb') as f:
+            f.write(contents)
 
     if args.stamp:
         with open(args.stamp, 'w') as f:
