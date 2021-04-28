2098dce12c1a5ab7d4cbbda375277a45855fb055
manukh <manukh@chromium.org>
2021-04-28 16:20:37 +0000

    Add `base/logging.h` include to `url_database.cc`.
    CL crrev.com/c/2854394 removed the 'base/logging.h' include from
    url_database.cc but it's used for `VLOG`.
    
    This CL returns the include.
    
    Change-Id: I002e937f44b3eff319345e6baa763154d94eb01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2856675
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877084}

--- components/history/core/browser/url_database.cc
+++ components/history/core/browser/url_database.cc
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "base/i18n/case_conversion.h"
+#include "base/logging.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
