17e45c63842180eb29c66d62e3eedf804a68c803
Robin Lewis <wrlewis@google.com>
2021-04-29 00:03:04 +0000

    Upload username and domain from GCPW extension upload device details.
    Bug: 1203777
    Change-Id: I4f348a9869b260ff94e1e2281d10aa05420efbff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857097
    Auto-Submit: Robin Lewis <wrlewis@google.com>
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Commit-Queue: Robin Lewis <wrlewis@google.com>
    Cr-Commit-Position: refs/heads/master@{#877301}

--- chrome/credential_provider/test/gcp_fakes.h
+++ chrome/credential_provider/test/gcp_fakes.h
@@ -146,6 +146,8 @@ class FakeOSUserManager : public OSUserManager {
     is_device_domain_joined_ = is_device_domain_joined;
   }
 
+  void FailFindUserBySID(const wchar_t* sid, int number_of_failures);
+
   struct UserInfo {
     UserInfo(const wchar_t* domain,
              const wchar_t* password,
@@ -216,6 +218,7 @@ class FakeOSUserManager : public OSUserManager {
   std::map<std::wstring, UserInfo> username_to_info_;
   bool is_device_domain_joined_ = false;
   std::map<FAILEDOPERATIONS, HRESULT> failure_reasons_;
+  std::map<std::wstring, int> to_be_failed_find_user_sids_;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
