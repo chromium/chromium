53713f6a1b1508a2b5c5c53536cff651be35007a
Brett Kilty <brettk@google.com>
2021-04-30 16:31:07 +0000

    Remove unsupported flag
    is_android_things is no longer supported. Remove from the
    codebase. Move a couple of instances to is_android_appliance
    where appropriate.
    
    Bug: 185175599
    Test: Build + internal patch.
    Change-Id: I96842da3b80019d13cf3816038ece7b851561b3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848759
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Commit-Queue: Brett Kilty <brettk@google.com>
    Cr-Commit-Position: refs/heads/master@{#877958}

--- chromecast/base/cast_sys_info_android_factory.cc
+++ chromecast/base/cast_sys_info_android_factory.cc
@@ -3,20 +3,13 @@
 // found in the LICENSE file.
 
 #include "chromecast/chromecast_buildflags.h"
-#if BUILDFLAG(IS_ANDROID_THINGS)
-#include "chromecast/base/cast_sys_info_android_things.h"
-#endif
 #include "chromecast/base/cast_sys_info_android.h"
 
 namespace chromecast {
 
 // static
 std::unique_ptr<CastSysInfo> CreateSysInfo() {
-#if BUILDFLAG(IS_ANDROID_THINGS)
-  return std::make_unique<CastSysInfoAndroidThings>();
-#else
   return std::make_unique<CastSysInfoAndroid>();
-#endif
 }
 
 }  // namespace chromecast
