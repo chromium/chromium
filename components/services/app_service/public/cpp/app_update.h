e0751a62cacfa13dd1f45f314cca45b6cc0817db
echo linker <ajlinker@chromium.org>
2021-04-30 02:51:55 +0000

    Adding the preferred app setting to the app struct.
    The app structure has been updated to include a setting that will
    store whether a particular app has been set as a preferred app.
    
    BUG=1204324
    
    Change-Id: Ic16ad274351c85470d670a4366b60c4536b6780d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852846
    Commit-Queue: Echo Linker <ajlinker@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877763}

--- components/services/app_service/public/cpp/app_update.h
+++ components/services/app_service/public/cpp/app_update.h
@@ -136,6 +136,9 @@ class COMPONENT_EXPORT(APP_UPDATE) AppUpdate {
   apps::mojom::OptionalBool ResizeLocked() const;
   bool ResizeLockedChanged() const;
 
+  apps::mojom::OptionalBool PreferredApp() const;
+  bool PreferredAppChanged() const;
+
   const ::AccountId& AccountId() const;
 
  private:
