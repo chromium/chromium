3efb03d98501f51705268d11ea728d032312eabb
Peter McNeeley <petermcneeley@chromium.org>
2021-05-01 19:43:08 +0000

    Introducing the viz remote visual debugger
    The Viz Remote Debugger is a debug only connection that allows an
    active chromium instance to send graphical and text debug information
    to a remote client for display.
    
    This CL contains all the c++ runtime code required to support the visual
    debugger. This includes the main visual debugger interface
    (|viz::VizDebugger|), the mojo/protocol required for communication,
    and unit tests for the new classes.
    
    The security for this viz debugger can be summarized as follows:
    1. The viz debugger communication simply extends the existing DevTools
    remote debugger and has the same security parameters.
    2. All visual debugging macros are defined as empty in official builds
    and as such induce no additional security concerns.
    
    Additional documentation can be found in the README.md
    Additional documentation will also be found in the remote debug app.
    
    Note on confusing name terminology: The project is the "visual debugger"
    but the its implementation is in "viz" which makes the current instance
    the "viz debugger".
    
    Bug: 1174540
    Change-Id: Idab66fcf257bcd2a233ff0d1e291054e467155c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544049
    Commit-Queue: Peter McNeeley <petermcneeley@google.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878256}

---
+++ content/browser/devtools/protocol/visual_debugger_handler_unittest.cc
@@ -0,0 +1,42 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <memory>
+#include <utility>
+
+#include "base/bind.h"
+#include "base/callback_helpers.h"
+#include "base/command_line.h"
+#include "base/json/json_reader.h"
+#include "base/system/sys_info.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/values.h"
+#include "build/build_config.h"
+#include "content/browser/devtools/protocol/devtools_protocol_test_support.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/common/url_constants.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "content/public/test/content_browser_test_utils.h"
+
+namespace content {
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, VisualDebuggerTest) {
+  set_agent_host_can_close();
+  GURL url = GURL("data:text/html,<body></body>");
+  NavigateToURLBlockUntilNavigationsComplete(shell(), url, 1);
+  Attach();
+  SendCommand("VisualDebugger.startStream", nullptr, true);
+  WaitForNotification("VisualDebugger.frameResponse", true);
+
+  std::unique_ptr<base::DictionaryValue> command_params =
+      std::make_unique<base::DictionaryValue>();
+  auto filter_param =
+      std::string(R"({"filters":[{"selector":{"anno":""},"active":true}]})");
+  command_params->SetString("json", filter_param);
+  SendCommand("VisualDebugger.filterStream", std::move(command_params), true);
+  SendCommand("VisualDebugger.stopStream", nullptr, true);
+}
+
+}  // namespace content
