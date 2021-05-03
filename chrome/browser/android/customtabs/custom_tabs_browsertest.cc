6176a2ea62b2542cd82f902b4380190a5ba71c22
Clark DuVall <cduvall@chromium.org>
2021-05-03 19:55:18 +0000

    [Merge M90] Ensure the X-CCT-Client-Data header is added for popups
    The X-CCT-Client-Data was not being added for popups, which was a bug.
    This adds that functionality and a regression test.
    
    (cherry picked from commit d422b094b278dda2a54374a3398b55c2ee68d8f0)
    
    Bug: 1201001
    Change-Id: I55833ea11d6cebc287c11c91a31b95976ffa7bcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842495
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#874688}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2861215
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4430@{#1376}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- chrome/browser/android/customtabs/custom_tabs_browsertest.cc
+++ chrome/browser/android/customtabs/custom_tabs_browsertest.cc
@@ -17,63 +17,84 @@
 #include "net/test/embedded_test_server/request_handler_util.h"
 
 namespace customtabs {
+namespace {
+
+constexpr char kHeaderValue[] = "TestApp";
 
 class CustomTabsHeader : public AndroidBrowserTest {
  public:
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
+
+    run_loop_ = std::make_unique<base::RunLoop>();
+    embedded_test_server()->RegisterRequestMonitor(base::BindLambdaForTesting(
+        [&](const net::test_server::HttpRequest& request) {
+          if (request.relative_url == "/favicon.ico")
+            return;
+
+          std::string value;
+          auto it = request.headers.find("X-CCT-Client-Data");
+          if (it != request.headers.end())
+            value = it->second;
+
+          std::string path = request.relative_url;
+          base::ReplaceFirstSubstringAfterOffset(&path, 0,
+                                                 "/android/customtabs/", "");
+          if (base::StartsWith(path, "cct_header.html",
+                               base::CompareCase::SENSITIVE)) {
+            path = "cct_header.html";
+          }
+
+          url_header_values_[path] = value;
+          if (url_header_values_.size() == 5)
+            run_loop_->Quit();
+        }));
+
+    ASSERT_TRUE(embedded_test_server()->Start());
+
+    auto* web_contents = chrome_test_utils::GetActiveWebContents(this);
+    ClientDataHeaderWebContentsObserver::CreateForWebContents(web_contents);
+    ClientDataHeaderWebContentsObserver::FromWebContents(web_contents)
+        ->SetHeader(kHeaderValue);
   }
+
+  GURL CreateURL() {
+    base::StringPairs replacements;
+    replacements.push_back(std::make_pair(
+        "REPLACE_WITH_HTTP_PORT",
+        base::NumberToString(embedded_test_server()->host_port_pair().port())));
+
+    std::string path = net::test_server::GetFilePathWithReplacements(
+        "/android/customtabs/cct_header.html", replacements);
+    return embedded_test_server()->GetURL("www.google.com", path);
+  }
+
+  void ExpectClientDataHeadersSet() {
+    run_loop_->Run();
+    EXPECT_EQ(url_header_values_["cct_header.html"], kHeaderValue);
+    EXPECT_EQ(url_header_values_["cct_header_frame.html"], kHeaderValue);
+    EXPECT_EQ(url_header_values_["google1.jpg"], kHeaderValue);
+    EXPECT_EQ(url_header_values_["google2.jpg"], kHeaderValue);
+    EXPECT_EQ(url_header_values_["non_google.jpg"], "");
+  }
+
+ private:
+  std::unique_ptr<base::RunLoop> run_loop_;
+  std::map<std::string, std::string> url_header_values_;
 };
 
 IN_PROC_BROWSER_TEST_F(CustomTabsHeader, Basic) {
-  base::RunLoop run_loop;
-  std::map<std::string, std::string> url_header_values;
-  embedded_test_server()->RegisterRequestMonitor(base::BindLambdaForTesting(
-      [&](const net::test_server::HttpRequest& request) {
-        if (request.relative_url == "/favicon.ico")
-          return;
-
-        std::string value;
-        auto it = request.headers.find("X-CCT-Client-Data");
-        if (it != request.headers.end())
-          value = it->second;
-
-        std::string path = request.relative_url;
-        base::ReplaceFirstSubstringAfterOffset(&path, 0, "/android/customtabs/",
-                                               "");
-        if (base::StartsWith(path, "cct_header.html",
-                             base::CompareCase::SENSITIVE)) {
-          path = "cct_header.html";
-        }
-
-        url_header_values[path] = value;
-        if (url_header_values.size() == 5)
-          run_loop.Quit();
-      }));
-
-  ASSERT_TRUE(embedded_test_server()->Start());
-
   auto* web_contents = chrome_test_utils::GetActiveWebContents(this);
-  ClientDataHeaderWebContentsObserver::CreateForWebContents(web_contents);
-  ClientDataHeaderWebContentsObserver::FromWebContents(web_contents)
-      ->SetHeader("TestApp");
-
-  base::StringPairs replacements;
-  replacements.push_back(std::make_pair(
-      "REPLACE_WITH_HTTP_PORT",
-      base::NumberToString(embedded_test_server()->host_port_pair().port())));
-
-  std::string path = net::test_server::GetFilePathWithReplacements(
-      "/android/customtabs/cct_header.html", replacements);
-  GURL url = embedded_test_server()->GetURL("www.google.com", path);
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
-  run_loop.Run();
-
-  EXPECT_EQ(url_header_values["cct_header.html"], "TestApp");
-  EXPECT_EQ(url_header_values["cct_header_frame.html"], "TestApp");
-  EXPECT_EQ(url_header_values["google1.jpg"], "TestApp");
-  EXPECT_EQ(url_header_values["google2.jpg"], "TestApp");
-  EXPECT_EQ(url_header_values["non_google.jpg"], "");
+  EXPECT_TRUE(content::NavigateToURL(web_contents, CreateURL()));
+  ExpectClientDataHeadersSet();
 }
 
+IN_PROC_BROWSER_TEST_F(CustomTabsHeader, Popup) {
+  auto* web_contents = chrome_test_utils::GetActiveWebContents(this);
+  EXPECT_TRUE(content::ExecJs(web_contents,
+                              "window.open('" + CreateURL().spec() + "')"));
+  ExpectClientDataHeadersSet();
+}
+
+}  // namespace
 }  // namespace customtabs
