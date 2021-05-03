02bc6b555c9eab86c514831248ab227683b1528f
Victor Costan <pwnall@chromium.org>
2021-05-03 19:19:24 +0000

    Reland "fonts: Use Core Text in macOS version of GetFontList_SlowBlocking()."
    This is a reland of 034be01755d06d8f5010bffe6e5cf2cf93600435
    
    This CL has the following changes from the original.
    
    * Add DCHECKs checking for null return values from macOS APIs. This
      will make it easier to debug build failures like the one that got the
      original CL reverted.
    * Remove the string copy in the FindFirstWithFamilyName() fallback
      case.
    
    Original change's description:
    > fonts: Use Core Text in macOS version of GetFontList_SlowBlocking().
    >
    > Bug: 1062925
    > Change-Id: I075bd135bf334f511480bcdccd04e91f99ad54ea
    > Tested: Compared old and new output on macOS 10.15.3, via DLOGs.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2104102
    > Commit-Queue: Victor Costan <pwnall@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#751919}
    
    Bug: 1062925
    Change-Id: I241740d317b70b0575d2e0dbd2eeb3e6b0e89390
    Cq-Include-Trybots: luci.chromium.try:mac_chromium_10.11_rel_ng,mac_chromium_10.12_rel_ng,mac_chromium_10.13_rel_ng,mac_chromium_10.14_rel_ng,mac_chromium_10.15_rel_ng
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112752
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878508}

--- content/common/font_list_mac.mm
+++ content/common/font_list_mac.mm
@@ -5,40 +5,231 @@
 #include "content/common/font_list.h"
 
 #import <Cocoa/Cocoa.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreText/CoreText.h>
 
 #include <utility>
 
+#include "base/logging.h"
+#include "base/mac/foundation_util.h"
+#include "base/mac/scoped_cftyperef.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
 
+// The code here is unusually skeptical about the macOS APIs returning non-null
+// values. An earlier version was reverted due to crashing tests on bots running
+// older macOS versions. The DCHECKs are there to expedite debugging similar
+// problems.
+
 namespace content {
+
+namespace {
+
+// Core Text-based localized family name lookup.
+//
+// This class is not thread-safe.
+//
+// This class caches some state for an efficient implementation of
+// [NSFontManager localizedNameForFamily:face:] using the Core Text API.
+class FontFamilyResolver {
+ public:
+  FontFamilyResolver() {
+    DCHECK(mandatory_attributes_ != nullptr);
+    DCHECK(font_descriptor_attributes_ != nullptr);
+  }
+  ~FontFamilyResolver() = default;
+
+  FontFamilyResolver(const FontFamilyResolver&) = delete;
+  FontFamilyResolver& operator=(const FontFamilyResolver&) = delete;
+
+  // Returns a localized font family name for the given family name.
+  base::ScopedCFTypeRef<CFStringRef> CopyLocalizedFamilyName(
+      CFStringRef family_name) {
+    DCHECK(family_name != nullptr);
+
+    CFDictionarySetValue(font_descriptor_attributes_.get(),
+                         kCTFontFamilyNameAttribute, family_name);
+    base::ScopedCFTypeRef<CTFontDescriptorRef> raw_descriptor(
+        CTFontDescriptorCreateWithAttributes(
+            font_descriptor_attributes_.get()));
+    DCHECK(raw_descriptor != nullptr)
+        << "CTFontDescriptorCreateWithAttributes returned null";
+
+    base::ScopedCFTypeRef<CFArrayRef> normalized_descriptors(
+        CTFontDescriptorCreateMatchingFontDescriptors(
+            raw_descriptor, mandatory_attributes_.get()));
+    DCHECK(normalized_descriptors != nullptr)
+        << "CTFontDescriptorCreateMatchingFontDescriptors returned null";
+    return CopyLocalizedFamilyNameFrom(family_name,
+                                       normalized_descriptors.get());
+  }
+
+  // True if the font should be hidden from Chrome.
+  //
+  // On macOS 10.15, CTFontManagerCopyAvailableFontFamilyNames() filters hidden
+  // fonts. This is not true on older version of macOS that Chrome still
+  // supports. The unittest FontTest.GetFontListDoesNotIncludeHiddenFonts can be
+  // used to determine when it's safe to slim down / remove this function.
+  static bool IsHiddenFontFamily(CFStringRef family_name) {
+    DCHECK(family_name != nullptr);
+    DCHECK_GT(CFStringGetLength(family_name), 0);
+
+    // macOS 10.13 includes names that start with . (period). These fonts should
+    // not be shown to users.
+    return CFStringGetCharacterAtIndex(family_name, 0) == '.';
+  }
+
+ private:
+  // Returns the first font descriptor matching the given family name.
+  //
+  // |descriptors| must be an array of CTFontDescriptors whose font family name
+  // attribute is populated.
+  //
+  // Returns null if none of the descriptors match.
+  static base::ScopedCFTypeRef<CTFontDescriptorRef> FindFirstWithFamilyName(
+      CFStringRef family_name,
+      CFArrayRef descriptors) {
+    DCHECK(family_name != nullptr);
+    DCHECK(descriptors != nullptr);
+
+    CFIndex normalized_descriptor_count = CFArrayGetCount(descriptors);
+    for (CFIndex i = 0; i < normalized_descriptor_count; ++i) {
+      CTFontDescriptorRef descriptor =
+          base::mac::CFCastStrict<CTFontDescriptorRef>(
+              CFArrayGetValueAtIndex(descriptors, i));
+      DCHECK(descriptor != nullptr)
+          << "The descriptors array has a null element.";
+
+      base::ScopedCFTypeRef<CFStringRef> descriptor_family_name(
+          base::mac::CFCastStrict<CFStringRef>(CTFontDescriptorCopyAttribute(
+              descriptor, kCTFontFamilyNameAttribute)));
+      if (CFStringCompare(family_name, descriptor_family_name,
+                          /*compareOptions=*/0) == kCFCompareEqualTo) {
+        return base::ScopedCFTypeRef<CTFontDescriptorRef>(
+            descriptor, base::scoped_policy::RETAIN);
+      }
+    }
+    return base::ScopedCFTypeRef<CTFontDescriptorRef>(nullptr);
+  }
+
+  // Returns a localized font family name for the given family name.
+  //
+  // |descriptors| must be an array of CTFontDescriptors representing all
+  // descriptors on the system matching the given family name.
+  //
+  // The given family name is returned as a fallback, if none of the descriptors
+  // match the desired font family name.
+  static base::ScopedCFTypeRef<CFStringRef> CopyLocalizedFamilyNameFrom(
+      CFStringRef family_name,
+      CFArrayRef descriptors) {
+    DCHECK(family_name != nullptr);
+    DCHECK(descriptors != nullptr);
+
+    base::ScopedCFTypeRef<CTFontDescriptorRef> descriptor =
+        FindFirstWithFamilyName(family_name, descriptors);
+    if (descriptor == nullptr) {
+      DLOG(WARNING) << "Will use non-localized family name for font family: "
+                    << family_name;
+      return base::ScopedCFTypeRef<CFStringRef>(family_name,
+                                                base::scoped_policy::RETAIN);
+    }
+
+    base::ScopedCFTypeRef<CFStringRef> localized_family_name(
+        base::mac::CFCastStrict<CFStringRef>(
+            CTFontDescriptorCopyLocalizedAttribute(descriptor,
+                                                   kCTFontFamilyNameAttribute,
+                                                   /*language=*/nullptr)));
+    DCHECK(localized_family_name != nullptr)
+        << "CTFontDescriptorCopyLocalizedAttribute returned null";
+    return localized_family_name;
+  }
+
+  // Creates the set stored in |mandatory_attributes_|.
+  static base::ScopedCFTypeRef<CFSetRef> CreateMandatoryAttributes() {
+    CFStringRef set_values[] = {kCTFontFamilyNameAttribute};
+    return base::ScopedCFTypeRef<CFSetRef>(CFSetCreate(
+        kCFAllocatorDefault, reinterpret_cast<const void**>(set_values),
+        base::size(set_values), &kCFTypeSetCallBacks));
+  }
+
+  // Creates the mutable dictionary stored in |font_descriptor_attributes_|.
+  static base::ScopedCFTypeRef<CFMutableDictionaryRef>
+  CreateFontDescriptorAttributes() {
+    return base::ScopedCFTypeRef<CFMutableDictionaryRef>(
+        CFDictionaryCreateMutable(kCFAllocatorDefault, /*capacity=*/1,
+                                  &kCFTypeDictionaryKeyCallBacks,
+                                  &kCFTypeDictionaryValueCallBacks));
+  }
+
+  // Used for all CTFontDescriptorCreateMatchingFontDescriptors() calls.
+  //
+  // Caching this dictionary saves one dictionary creation per lookup.
+  const base::ScopedCFTypeRef<CFSetRef> mandatory_attributes_ =
+      CreateMandatoryAttributes();
+
+  // Used for all CTFontDescriptorCreateMatchingFontDescriptors() calls.
+  //
+  // This dictionary has exactly one key, kCTFontFamilyNameAttribute. The value
+  // associated with the key is overwritten as needed.
+  //
+  // Caching this dictionary saves one dictionary creation per lookup.
+  const base::ScopedCFTypeRef<CFMutableDictionaryRef>
+      font_descriptor_attributes_ = CreateFontDescriptorAttributes();
+};
+
+}  // namespace
 
 std::unique_ptr<base::ListValue> GetFontList_SlowBlocking() {
   @autoreleasepool {
-    std::unique_ptr<base::ListValue> font_list(new base::ListValue);
-    NSFontManager* fontManager = [[[NSFontManager alloc] init] autorelease];
-    NSMutableDictionary* fonts_dict = [NSMutableDictionary dictionary];
-    NSArray* fonts = [fontManager availableFontFamilies];
+    FontFamilyResolver resolver;
 
-    for (NSString* family_name in fonts) {
-      NSString* localized_family_name =
-          [fontManager localizedNameForFamily:family_name face:nil];
-      fonts_dict[family_name] = localized_family_name;
+    base::ScopedCFTypeRef<CFArrayRef> cf_family_names(
+        CTFontManagerCopyAvailableFontFamilyNames());
+    DCHECK(cf_family_names != nullptr)
+        << "CTFontManagerCopyAvailableFontFamilyNames returned null";
+    NSArray* family_names = base::mac::CFToNSCast(cf_family_names.get());
+
+    // Maps localized font family names to non-localized names.
+    NSMutableDictionary* family_name_map = [NSMutableDictionary
+        dictionaryWithCapacity:CFArrayGetCount(cf_family_names)];
+    for (NSString* family_name in family_names) {
+      DCHECK(family_name != nullptr)
+          << "CTFontManagerCopyAvailableFontFamilyNames returned an array with "
+          << "a null element";
+
+      CFStringRef family_name_cf = base::mac::NSToCFCast(family_name);
+      if (FontFamilyResolver::IsHiddenFontFamily(family_name_cf))
+        continue;
+
+      base::ScopedCFTypeRef<CFStringRef> cf_normalized_family_name =
+          resolver.CopyLocalizedFamilyName(base::mac::NSToCFCast(family_name));
+      DCHECK(cf_normalized_family_name != nullptr)
+          << "FontFamilyResolver::CopyLocalizedFamilyName returned null";
+      family_name_map[base::mac::CFToNSCast(cf_normalized_family_name)] =
+          family_name;
     }
 
-    // Sort family names based on localized names.
-    NSArray* sortedFonts = [fonts_dict
+    // The Apple documentation for CTFontManagerCopyAvailableFontFamilyNames
+    // states that it returns family names sorted for user interface display.
+    // https://developer.apple.com/documentation/coretext/1499494-ctfontmanagercopyavailablefontfa
+    //
+    // This doesn't seem to be the case, at least on macOS 10.15.3.
+    NSArray* sorted_localized_family_names = [family_name_map
         keysSortedByValueUsingSelector:@selector(localizedStandardCompare:)];
 
-    for (NSString* family_name in sortedFonts) {
-      NSString* localized_family_name = fonts_dict[family_name];
-      auto font_item = std::make_unique<base::ListValue>();
-      font_item->AppendString(base::SysNSStringToUTF16(family_name));
-      font_item->AppendString(base::SysNSStringToUTF16(localized_family_name));
-      font_list->Append(std::move(font_item));
+    std::vector<base::Value> font_list;
+    for (NSString* localized_family_name in sorted_localized_family_names) {
+      NSString* family_name = family_name_map[localized_family_name];
+
+      std::vector<base::Value> font_list_item;
+      font_list_item.reserve(2);
+      font_list_item.emplace_back(base::SysNSStringToUTF8(family_name));
+      font_list_item.emplace_back(
+          base::SysNSStringToUTF8(localized_family_name));
+      font_list.emplace_back(std::move(font_list_item));
     }
 
-    return font_list;
+    return std::make_unique<base::ListValue>(base::Value(font_list).TakeList());
   }
 }
 
