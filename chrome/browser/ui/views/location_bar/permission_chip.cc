ad60c2d48a8456ca4523173eb9ee65099ed188e1
Illia Klimov <elklm@google.com>
2021-05-04 12:52:56 +0000

    Fix a crash that happens after a chip animation ends.
    (cherry picked from commit ffca1b0f235324df789771d8a4b1efdf265a07f5)
    
    Bug: 1203589
    Change-Id: Iaf811978730a8f33d0051201150a1e4670013cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2855479
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#877437}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871586
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#719}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/browser/ui/views/location_bar/permission_chip.cc
+++ chrome/browser/ui/views/location_bar/permission_chip.cc
@@ -122,6 +122,7 @@ void PermissionChip::DisplayRequest(
 
 void PermissionChip::FinalizeRequest() {
   SetVisible(false);
+  chip_button_->ResetAnimation();
   collapse_timer_.AbandonAndStop();
   dismiss_timer_.AbandonAndStop();
   delegate_ = nullptr;
