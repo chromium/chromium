8538c69aefa8f9e5f1a83d54e7d11febf72030d1
Hui Yingst <nigi@chromium.org>
2021-05-04 16:23:03 +0000

    Revert "[Device Trust] Generate challenge response"
    This reverts commit b0c754e18d8f53864b25ea0e19f3b94b22cda050.
    
    Reason for revert: Breaking build https://ci.chromium.org/p/chromium/builders/ci/mac-archive-rel/20778.
    attestation_ca.pb.h is missing.
    
    Original change's description:
    > [Device Trust] Generate challenge response
    >
    > Chrome gets the challenge from the Idp, then it create the challenge
    > response and send it back in the proper header.
    >
    > This CL includes
    > - Handle the HTTP response from the Idp which contains the challenge so
    > the client can generate the challenge response.
    > - Signing message method using the key pair created by client.
    > - Challenge response creation with the signature using the method
    > mentioned above.
    > - Use of VA protos for the handshake that were only part of Chrome OS.
    >
    >
    > This CL doesn't contain the full logic for the challenge response
    > creation, items that are missing:
    > - Encrypted key info in the challenge response.
    > - Verification of the challenge.
    >
    > Following CLs are going to be created to implement those logic. I
    > decided to separated since those items need lot of code for the crypto
    > part.
    >
    > Change-Id: Ie1d90f1007bb08ef48b0b168a200e22342530048
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827913
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    > Cr-Commit-Position: refs/heads/master@{#878847}
    
    Change-Id: I000386aefa2ee663d25ce1225fa4e5f80d624521
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872246
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878881}

--- chrome/browser/enterprise/connectors/device_trust/navigation_throttle_unittest.cc
+++ chrome/browser/enterprise/connectors/device_trust/navigation_throttle_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,8 +20,8 @@ using content::NavigationThrottle;
 
 namespace {
 
-const base::Value kOrigins[]{base::Value("https://www.example.com"),
-                             base::Value("example2.example.com")};
+const base::Value origins[]{base::Value("https://www.example.com"),
+                            base::Value("example2.example.com")};
 
 }  // namespace
 
@@ -47,7 +47,7 @@ class DeviceTrustNavigationThrottleTest
     Profile::FromBrowserContext(browser_context())
         ->GetPrefs()
         ->Set(kContextAwareAccessSignalsAllowlistPref,
-              base::ListValue(kOrigins));
+              std::move(base::ListValue(origins)));
   }
 
  private:
