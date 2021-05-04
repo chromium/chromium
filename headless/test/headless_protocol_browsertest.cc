aee664d7463785bc0c7b56ba00f205824bfbf72e
David Roger <droger@chromium.org>
2021-05-04 16:50:57 +0000

    [Sheriff] disable HeadlessProtocolBrowserTest.InputClipboardOps on mac
    The test is flaky.
    
    Bug: 1205451
    Change-Id: If626e1bef2692fe4b5d3fee8a0d58bdc2495f6fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871755
    Owners-Override: David Roger <droger@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878887}

--- headless/test/headless_protocol_browsertest.cc
+++ headless/test/headless_protocol_browsertest.cc
@@ -261,7 +261,13 @@ HEADLESS_PROTOCOL_TEST(Geolocation, "emulation/geolocation-crash.js")
 
 HEADLESS_PROTOCOL_TEST(DragStarted, "input/dragIntercepted.js")
 
-HEADLESS_PROTOCOL_TEST(InputClipboardOps, "input/input-clipboard-ops.js")
+// https://crbug.com/1205451
+#if defined(OS_MAC)
+#define MAYBE_InputClipboardOps DISABLED_InputClipboardOps
+#else
+#define MAYBE_InputClipboardOps InputClipboardOps
+#endif
+HEADLESS_PROTOCOL_TEST(MAYBE_InputClipboardOps, "input/input-clipboard-ops.js")
 
 HEADLESS_PROTOCOL_TEST(HeadlessSessionBasicsTest,
                        "sessions/headless-session-basics.js")
