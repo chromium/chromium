06d44e313c3f0448d12fd522fcdcc54c1c4542b1
Kristi Park <kristipark@chromium.org>
2021-05-04 18:04:27 +0000

    [SharingHub] Enable copy url to use in desktop sharing hub
    Enable IDC_COPY_URL to be used in the desktop sharing hub.
    
    Bug: 1186846
    Change-Id: I23f31e24cd246af0c3163a28ac039086511ad802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872250
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878961}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -29,6 +29,7 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
+#include "chrome/browser/sharing_hub/sharing_hub_features.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/signin/signin_promo.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
@@ -1063,7 +1064,11 @@ void BrowserCommandController::InitCommandState() {
 #endif
       web_app::AppBrowserController::IsWebApp(browser_);
   // Hosted app browser commands.
-  command_updater_.UpdateCommandEnabled(IDC_COPY_URL, is_web_app_or_custom_tab);
+  const bool enable_copy_url =
+      is_web_app_or_custom_tab ||
+      base::FeatureList::IsEnabled(sharing_hub::kSharingHubDesktopOmnibox) ||
+      base::FeatureList::IsEnabled(sharing_hub::kSharingHubDesktopAppMenu);
+  command_updater_.UpdateCommandEnabled(IDC_COPY_URL, enable_copy_url);
   command_updater_.UpdateCommandEnabled(IDC_OPEN_IN_CHROME,
                                         is_web_app_or_custom_tab);
   command_updater_.UpdateCommandEnabled(IDC_SITE_SETTINGS,
