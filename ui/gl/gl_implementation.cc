641d54d756baff01484214b16b00b4935a8be657
Alexis Hetu <sugoi@google.com>
2021-05-04 17:21:11 +0000

    Revert using SwANGLE with kOverrideUseSoftwareGLForTests
    kOverrideUseSoftwareGLForTests is unfortunately not only used for tests
    and ends up being used for headless mode in the released version of
    Chrome. To avoid issues, we'll remove it for now, merge in beta and
    revert this CL afterwards, in order to give us enough time to write a
    proper fix for this.
    
    Bug: chromium:1060139
    Change-Id: Id3b3e7cec755f9c4b9a4953bc3f69445e516af32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872065
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878941}

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -182,12 +182,7 @@ GLImplementationParts GetSoftwareGLImplementation() {
 }
 
 GLImplementationParts GetSoftwareGLForTestsImplementation() {
-#if defined(OS_WIN) || \
-    (defined(OS_LINUX) && !defined(OS_FUCHSIA) && !defined(USE_OZONE))
-  return GetSoftwareGLImplementation();
-#else
   return GetLegacySoftwareGLImplementation();
-#endif
 }
 
 bool IsSoftwareGLImplementation(GLImplementationParts implementation) {
