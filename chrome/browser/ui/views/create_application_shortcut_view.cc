chromium/chromium

commit b6c2b83b04a2675c478d2bfc399e5afc70cbeae2

cbuiv: remove overrides of GetModalType
All of these are simple - most return inlined constants, the remainder
return values that are set at constructor time and never changed.

This change also makes it safe to call a few overrides of
WidgetDelegate::GetWidget() before the widget has been created (in which
case it should simply return nullptr, rather than crashing).

Bug: 1075649
Change-Id: Id977744f36a1123f22f5479701dfd086a4efab74
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618303
Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Cr-Commit-Position: refs/heads/master@{#846932}

chrome/browser/ui/views/create_application_shortcut_view.cc
@@ -61,6 +61,7 @@ CreateChromeApplicationShortcutView::CreateChromeApplicationShortcutView(
     const extensions::Extension* app,
     base::OnceCallback<void(bool)> close_callback)
     : CreateChromeApplicationShortcutView(profile, std::move(close_callback)) {
+  SetModalType(ui::MODAL_TYPE_WINDOW);
   // Get shortcut and icon information; needed for creating the shortcut.
   web_app::GetShortcutInfoForApp(
       app, profile,
@@ -207,10 +208,6 @@ bool CreateChromeApplicationShortcutView::IsDialogButtonEnabled(
   return desktop_check_box_->GetChecked() ||
          (menu_check_box_ && menu_check_box_->GetChecked()) ||
          (quick_launch_check_box_ && quick_launch_check_box_->GetChecked());
-}
-
-ui::ModalType CreateChromeApplicationShortcutView::GetModalType() const {
-  return ui::MODAL_TYPE_WINDOW;
 }
 
 base::string16 CreateChromeApplicationShortcutView::GetWindowTitle() const {

commit 8375a80a78fde88bf2c398a83fd0adfb2c2bb125

Fix modality of PWA shortcut creation dialog.
The modality was not set, so the user could interact with the browser
behind the would-be modal dialog. Also, the code was DCHECKing.

This regressed in b6c2b83b04a2675c478d2bfc399e5afc70cbeae2
The modality must be set in the shared constructor, rather than the
ctor that's used for extension apps.

Bug: 1205929
Change-Id: I68d0517c3a71539e0326aac6f631f7810527d116
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875326
Auto-Submit: Evan Stade <estade@chromium.org>
Commit-Queue: Mike Wasserman <msw@chromium.org>
Reviewed-by: Mike Wasserman <msw@chromium.org>
Cr-Commit-Position: refs/heads/master@{#879473}

chrome/browser/ui/views/create_application_shortcut_view.cc
@@ -62,7 +62,6 @@ CreateChromeApplicationShortcutView::CreateChromeApplicationShortcutView(
     const extensions::Extension* app,
     base::OnceCallback<void(bool)> close_callback)
     : CreateChromeApplicationShortcutView(profile, std::move(close_callback)) {
-  SetModalType(ui::MODAL_TYPE_WINDOW);
   // Get shortcut and icon information; needed for creating the shortcut.
   web_app::GetShortcutInfoForApp(
       app, profile,
@@ -86,6 +85,7 @@ CreateChromeApplicationShortcutView::CreateChromeApplicationShortcutView(
     Profile* profile,
     base::OnceCallback<void(bool)> close_callback)
     : profile_(profile), close_callback_(std::move(close_callback)) {
+  SetModalType(ui::MODAL_TYPE_WINDOW);
   SetButtonLabel(ui::DIALOG_BUTTON_OK,
                  l10n_util::GetStringUTF16(IDS_CREATE_SHORTCUTS_COMMIT));
   set_margins(ChromeLayoutProvider::Get()->GetDialogInsetsForContentType(