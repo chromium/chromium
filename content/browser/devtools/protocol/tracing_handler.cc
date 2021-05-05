22b5923ec550222cf8f57591c970661234a6c2e9
Austin Sullivan <asully@chromium.org>
2021-05-05 16:28:26 +0000

    [Code Health] Remove uses of Value::CreateDeepCopy()
    This CL was uploaded by git cl split.
    
    R=sigurds@chromium.org
    
    Bug: 1187013
    Change-Id: I1770eb9453ea194a3706906f16dbf03af763656a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873614
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879392}

--- content/browser/devtools/protocol/tracing_handler.cc
+++ content/browser/devtools/protocol/tracing_handler.cc
@@ -106,7 +106,7 @@ std::unique_ptr<base::Value> ConvertDictKeyStyle(const base::Value& value) {
     return std::move(out_list);
   }
 
-  return value.CreateDeepCopy();
+  return base::Value::ToUniquePtrValue(value.Clone());
 }
 
 class DevToolsTraceEndpointProxy : public TracingController::TraceDataEndpoint {
