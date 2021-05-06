1b6cf0ec462c233bcaf7bf09cac402f73a8317b6
Colin Blundell <blundell@chromium.org>
2021-05-06 16:02:32 +0000

    [gn check] Enable //chrome/browser/touch_to_fill/android
    Most deps can be entered. The exception is a dep on //chrome/browser,
    as //chrome/browser transitively depends on this target. Instead this
    CL adds nogncheck for the one relevant include.
    
    Bug: 949535
    Change-Id: Idc952b1bc8300775a115290bf0e70c685659cd03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874885
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879853}

--- chrome/browser/touch_to_fill/android/touch_to_fill_view_impl.cc
+++ chrome/browser/touch_to_fill/android/touch_to_fill_view_impl.cc
@@ -13,7 +13,7 @@
 #include "base/time/time.h"
 #include "chrome/browser/touch_to_fill/android/jni_headers/Credential_jni.h"
 #include "chrome/browser/touch_to_fill/android/jni_headers/TouchToFillBridge_jni.h"
-#include "chrome/browser/touch_to_fill/touch_to_fill_controller.h"
+#include "chrome/browser/touch_to_fill/touch_to_fill_controller.h"  // nogncheck
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
 #include "components/password_manager/core/browser/origin_credential_store.h"
 #include "ui/android/view_android.h"
