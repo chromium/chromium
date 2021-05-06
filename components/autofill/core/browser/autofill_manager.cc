793a17eaa7fcb096185062316960ca4be2a0480b
Christoph Schwering <schwering@google.com>
2021-05-06 06:38:10 +0000

    [Autofill] Fixed refill of changed form.
    (cherry picked from commit 533bb3adcfe3499f90e2646fc60312f303b963ac)
    
    Bug: 1203667
    Change-Id: I2693a024531775e0e60cc330107d77d10558f466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867655
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#878824}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874611
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/branch-heads/4430@{#1403}
    Cr-Branched-From: e5ce7dc4f7518237b3d9bb93cccca35d25216cbe-refs/heads/master@{#857950}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1649,7 +1649,11 @@ void AutofillManager::FillOrPreviewDataModelForm(
   form_structure->RationalizePhoneNumbersInSection(autofill_field->section);
 
   FormData result = form;
-  DCHECK_EQ(form_structure->field_count(), form.fields.size());
+
+  // TODO(crbug/1203667#c9): Skip if the form has changed in the meantime, which
+  // may happen with refills.
+  if (form_structure->field_count() != form.fields.size())
+    return;
 
   if (action == AutofillDriver::FORM_DATA_ACTION_FILL && !is_refill) {
     SetFillingContext(
@@ -1690,8 +1694,10 @@ void AutofillManager::FillOrPreviewDataModelForm(
       continue;
     }
 
-    // The field order should be the same in |form_structure| and |result|.
-    DCHECK(form_structure->field(i)->SameFieldAs(result.fields[i]));
+    // TODO(crbug/1203667#c9): Skip if the form has changed in the meantime,
+    // which may happen with refills.
+    if (!form_structure->field(i)->SameFieldAs(result.fields[i]))
+      continue;
 
     AutofillField* cached_field = form_structure->field(i);
     FieldTypeGroup field_group_type = cached_field->Type().group();
