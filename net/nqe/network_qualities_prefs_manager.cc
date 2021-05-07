07c13f921ffe614089f238c6fe32762abe77423e
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-07 02:21:47 +0000

    CodeHealth: Remove use of DictionaryValue::size()
    This change removes the use of DictionaryValue::size()
    and replaces with Value::DictSize()
    
    Bug: 1187019
    Change-Id: I675268d865fc80afb55b02677f1bb19231d4cb5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875909
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#880213}

--- net/nqe/network_qualities_prefs_manager.cc
+++ net/nqe/network_qualities_prefs_manager.cc
@@ -32,7 +32,7 @@ constexpr size_t kMaxCacheSize = 20u;
 // Parses |value| into a map of NetworkIDs and CachedNetworkQualities,
 // and returns the map.
 ParsedPrefs ConvertDictionaryValueToMap(const base::DictionaryValue* value) {
-  DCHECK_GE(kMaxCacheSize, value->size());
+  DCHECK_GE(kMaxCacheSize, value->DictSize());
 
   ParsedPrefs read_prefs;
   for (const auto& it : value->DictItems()) {
@@ -63,7 +63,7 @@ NetworkQualitiesPrefsManager::NetworkQualitiesPrefsManager(
       prefs_(pref_delegate_->GetDictionaryValue()),
       network_quality_estimator_(nullptr) {
   DCHECK(pref_delegate_);
-  DCHECK_GE(kMaxCacheSize, prefs_->size());
+  DCHECK_GE(kMaxCacheSize, prefs_->DictSize());
 }
 
 NetworkQualitiesPrefsManager::~NetworkQualitiesPrefsManager() {
@@ -103,9 +103,9 @@ void NetworkQualitiesPrefsManager::ShutdownOnPrefSequence() {
 void NetworkQualitiesPrefsManager::ClearPrefs() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  LOCAL_HISTOGRAM_COUNTS_100("NQE.PrefsSizeOnClearing", prefs_->size());
+  LOCAL_HISTOGRAM_COUNTS_100("NQE.PrefsSizeOnClearing", prefs_->DictSize());
   prefs_->Clear();
-  DCHECK_EQ(0u, prefs_->size());
+  DCHECK_EQ(0u, prefs_->DictSize());
   pref_delegate_->SetDictionaryValue(*prefs_);
 }
 
@@ -113,7 +113,7 @@ void NetworkQualitiesPrefsManager::OnChangeInCachedNetworkQuality(
     const nqe::internal::NetworkID& network_id,
     const nqe::internal::CachedNetworkQuality& cached_network_quality) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK_GE(kMaxCacheSize, prefs_->size());
+  DCHECK_GE(kMaxCacheSize, prefs_->DictSize());
 
   std::string network_id_string = network_id.ToString();
 
@@ -126,10 +126,10 @@ void NetworkQualitiesPrefsManager::OnChangeInCachedNetworkQuality(
                     GetNameForEffectiveConnectionType(
                         cached_network_quality.effective_connection_type()));
 
-  if (prefs_->size() > kMaxCacheSize) {
+  if (prefs_->DictSize() > kMaxCacheSize) {
     // Delete one randomly selected value that has a key that is different from
     // |network_id|.
-    DCHECK_EQ(kMaxCacheSize + 1, prefs_->size());
+    DCHECK_EQ(kMaxCacheSize + 1, prefs_->DictSize());
     // Generate a random number in the range [0, |kMaxCacheSize| - 1] since the
     // number of network IDs in |prefs_| other than |network_id| is
     // |kMaxCacheSize|.
@@ -148,7 +148,7 @@ void NetworkQualitiesPrefsManager::OnChangeInCachedNetworkQuality(
       index_to_delete--;
     }
   }
-  DCHECK_GE(kMaxCacheSize, prefs_->size());
+  DCHECK_GE(kMaxCacheSize, prefs_->DictSize());
 
   // Notify the pref delegate so that it updates the prefs on the disk.
   pref_delegate_->SetDictionaryValue(*prefs_);
