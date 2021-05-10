fd09940e1208c9bb0d7781e32cd87825721dc465
Aleks Totic <atotic@chromium.org>
2021-05-10 17:26:25 +0000

    Reland [TablesNG] Width over percentage columns redistribution bugfix
    Column width redistribution was incorrect if:
    - table width was wider than max columns width
    - all columns were percentage columns
    - column min width was wider than intrinsic percent width
    
    Something like this would trigger it:
      <td style="width:1%">  <- small percentage size
        <div style=width:500px> <- large min width
    
    Its a bit more complicated than that because of reverse percentage
    width computation, you have to hit just the right ration of min/%.
    
    The correct fix matches FF/Legacy:
    - distributed size is min-width + cell%/total% * excess width.
    
    (cherry picked from commit 1267b9f5b22efb059479d781af009848760b3365)
    
    Bug: 1205135
    Change-Id: Iae53f3166f1e532a30c9e08c3d124bb194625658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875946
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#879655}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880884
    Cr-Commit-Position: refs/branch-heads/4472@{#902}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm_helpers.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm_helpers.cc
@@ -100,7 +100,9 @@ Vector<LayoutUnit> DistributeInlineSizeToComputedInlineSizeAuto(
       }
     } break;
     case kPercentageGuess: {
-      // Percent columns grow, auto/fixed get min inline size.
+      // Percent columns grow in proportion to difference between their
+      // percentage size and minimum size.
+      // Auto/Fixed columns get min inline size.
       LayoutUnit percent_inline_size_increases =
           guess_size_total_increases[kPercentageGuess];
       LayoutUnit distributable_inline_size =
@@ -294,8 +296,10 @@ Vector<LayoutUnit> DistributeInlineSizeToComputedInlineSizeAuto(
           *last_computed_size += rounding_error_inline_size;
         }
       } else if (percent_columns_count > 0) {
-        // All remaining columns are percent. Grow them.
-        LayoutUnit rounding_error_inline_size = target_inline_size;
+        // All remaining columns are percent.
+        // They grow to max(col minimum, %ge size) + additional size
+        // proportional to column percent.
+        LayoutUnit rounding_error_inline_size = distributable_inline_size;
         LayoutUnit* last_computed_size = nullptr;
         LayoutUnit* computed_size = computed_sizes.begin();
         for (const NGTableTypes::Column* column = start_column;
@@ -304,13 +308,18 @@ Vector<LayoutUnit> DistributeInlineSizeToComputedInlineSizeAuto(
             continue;
           DCHECK(column->percent);
           last_computed_size = computed_size;
-          if (total_percent > 0.0f) {
-            *computed_size = LayoutUnit(*column->percent / total_percent *
-                                        target_inline_size);
+          LayoutUnit percent_inline_size =
+              column->ResolvePercentInlineSize(target_inline_size);
+          LayoutUnit delta;
+          if (total_percent != 0.0f) {
+            delta = LayoutUnit(distributable_inline_size * *column->percent /
+                               total_percent);
           } else {
-            *computed_size = distributable_inline_size / percent_columns_count;
+            delta = LayoutUnit(distributable_inline_size.ToFloat() /
+                               percent_columns_count);
           }
-          rounding_error_inline_size -= *computed_size;
+          rounding_error_inline_size -= delta;
+          *computed_size = percent_inline_size + delta;
         }
         if (rounding_error_inline_size != LayoutUnit()) {
           DCHECK(last_computed_size);
