eb72ef022ade527f559d4f075470ffdb45daf465
Olivier Li <olivierli@chromium.org>
2021-05-11 19:52:27 +0000

    Update Memory doc URL in ProcessMetrics
    Change-Id: I3133d15b364aa591384e46db72b21628940e9e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880995
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881661}

--- base/process/process_metrics.h
+++ base/process/process_metrics.h
@@ -80,7 +80,7 @@ BASE_EXPORT int64_t TimeValToMicroseconds(const struct timeval& tv);
 // To obtain consistent memory metrics, use the memory_instrumentation service.
 //
 // For further documentation on memory, see
-// https://chromium.googlesource.com/chromium/src/+/HEAD/docs/README.md
+// https://chromium.googlesource.com/chromium/src/+/HEAD/docs/README.md#Memory
 class BASE_EXPORT ProcessMetrics {
  public:
   ~ProcessMetrics();
