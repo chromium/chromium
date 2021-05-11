3123e02fcb363f8acdc8941698e19c7a1acc4b2d
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-11 03:48:35 +0000

    CodeHealth: Remove use of DictionaryValue::size()
    This change removes the use of DictionaryValue::size()
    and replaces with Value::DictSize()
    
    Bug: 1187019
    Change-Id: Ide5f8100e4253ad21035ef2a9840c9e16bfcb18d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878152
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#881395}

--- remoting/host/it2me/it2me_native_messaging_host.cc
+++ remoting/host/it2me/it2me_native_messaging_host.cc
@@ -344,7 +344,7 @@ void It2MeNativeMessagingHost::ProcessConnect(
 
   std::unique_ptr<base::DictionaryValue> policies =
       policy_watcher_->GetEffectivePolicies();
-  if (policies->size() == 0) {
+  if (policies->DictSize() == 0) {
     // At this point policies have been read, so if there are none set then
     // it indicates an error. Since this can be fixed by end users it has a
     // dedicated message type rather than the generic "error" so that the
