79b623785c02db992b101ffb27f5b1585bdaf28c
Lei Zhang <thestig@chromium.org>
2021-05-11 17:40:51 +0000

    Fix includes and dependencies for custom_tabs_test_utils.cc.
    The file should include base/containers/contains.h, and the build target
    it is in should depend on //base.
    
    Change-Id: I3ea5f0c396fb92d808d491d99f492da63944a0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886838
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881592}

--- chrome/android/javatests/src/custom_tabs_test_utils.cc
+++ chrome/android/javatests/src/custom_tabs_test_utils.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/containers/contains.h"
 #include "chrome/android/chrome_test_util_jni_headers/CustomTabsTestUtils_jni.h"
 #include "components/variations/variations_ids_provider.h"
 
