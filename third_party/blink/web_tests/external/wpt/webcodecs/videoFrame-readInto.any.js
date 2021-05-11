5cc427ae0104a7e5a63a148fecdf704bd4b9f482
Dan Sanders <sandersd@chromium.org>
2021-05-11 01:26:04 +0000

    [webcodecs] WPT: validate VideoFrame.readInto() layouts
    Bug: 1176464
    Change-Id: Ide480fabb156b969a582b6f9fcf239b9634f4564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880837
    Auto-Submit: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881355}

--- third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-readInto.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-readInto.any.js
@@ -1,112 +1,171 @@
 // META: global=window,dedicatedworker
 
 function makeI420_4x2() {
-  let yData = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
-  let uData = new Uint8Array([9, 10]);
-  let vData = new Uint8Array([11, 12]);
-  let planes = [{src: yData, stride: 4},
-                {src: uData, stride: 2},
-                {src: vData, stride: 2}];
-  let init = {timestamp: 0,
-              codedWidth: 4,
-              codedHeight: 2};
+  const yData = new Uint8Array([
+      1, 2, 3, 4,
+      5, 6, 7, 8,
+  ]);
+  const uData = new Uint8Array([
+      9, 10,
+  ]);
+  const vData = new Uint8Array([
+      11, 12,
+  ]);
+  const planes = [
+      {data: yData, stride: 4},
+      {data: uData, stride: 2},
+      {data: vData, stride: 2},
+  ];
+  const init = {
+      timestamp: 0,
+      codedWidth: 4,
+      codedHeight: 2,
+  };
   return new VideoFrame('I420', planes, init);
 }
 
 function makeRGBA_2x2() {
-  let planes = [{src: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8,
-                                      9, 10, 11, 12, 13, 14, 15, 16]),
-                 stride: 8}];
-  let init = {timestamp: 0,
-              codedWidth: 2,
-              codedHeight: 2};
-  // TODO(sandersd): Should be RGBA but that's missing in the IDL right now.
+  const rgbaData = new Uint8Array([
+      1, 2, 3, 4,     5, 6, 7, 8,
+      9, 10, 11, 12,  13, 14, 15, 16,
+  ]);
+  const planes = [
+      {data: rgbaData, stride: 8},
+  ];
+  const init = {
+      timestamp: 0,
+      codedWidth: 2,
+      codedHeight: 2,
+  };
+  // TODO(sandersd): Should be RGBA but the IDL is reversed right now.
   return new VideoFrame('ABGR', planes, init);
 }
 
+function assert_buffer_equals(actual, expected) {
+  assert_true(actual instanceof Uint8Array, 'actual instanceof Uint8Array');
+  assert_true(expected instanceof Uint8Array, 'buffer instanceof Uint8Array');
+  assert_equals(actual.length, expected.length, 'buffer length');
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] != expected[i]) {
+      assert_equals(actual[i], expected[i], 'buffer contents at index ' + i);
+    }
+  }
+}
+
+function assert_layout_equals(actual, expected) {
+  assert_equals(actual.length, expected.length, 'layout planes');
+  for (let i = 0; i < actual.length; i++) {
+    assert_object_equals(actual[i], expected[i], 'plane ' + i + ' layout');
+  }
+}
+
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let expected = new Uint8Array([
-      1, 2, 3, 4, 5, 6, 7, 8,  // y
-      9, 10,                   // u
-      11, 12                   // v
+  const frame = makeI420_4x2();
+  const expectedLayout = [
+      {offset: 0, stride: 4},
+      {offset: 8, stride: 2},
+      {offset: 10, stride: 2},
+  ];
+  const expectedData = new Uint8Array([
+      1, 2, 3, 4,  // y
+      5, 6, 7, 8,
+      9, 10,       // u
+      11, 12       // v
   ]);
-  assert_equals(frame.allocationSize(), 12, 'allocationSize()');
-  await frame.readInto(buf);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data);
+  assert_layout_equals(layout, expectedLayout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test I420 frame.');
 
 promise_test(async t => {
-  let frame = makeRGBA_2x2();
-  let buf = new Uint8Array(16);
-  let expected = new Uint8Array([
-      1, 2, 3, 4, 5, 6, 7, 8,
-      9, 10, 11, 12, 13, 14, 15, 16
+  const frame = makeRGBA_2x2();
+  const expectedLayout = [
+      {offset: 0, stride: 8},
+  ];
+  const expectedData = new Uint8Array([
+      1, 2, 3, 4,     5, 6, 7, 8,
+      9, 10, 11, 12,  13, 14, 15, 16
   ]);
-  assert_equals(frame.allocationSize(), 16, 'allocationSize()');
-  await frame.readInto(buf);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data);
+  assert_layout_equals(layout, expectedLayout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test RGBA frame.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(11);
-  let expected = new Uint8Array([
-      1, 2, 3, 4, 5, 6, 7, 8,  // y
-      9, 10,                   // u
-      11, 12                   // v
-  ]);
-  assert_equals(frame.allocationSize(), 12, 'allocationSize()');
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf));
+  const frame = makeI420_4x2();
+  const data = new Uint8Array(11);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data));
 }, 'Test undersized buffer.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{}, {}, {}]};
-  let expected = new Uint8Array([
-      1, 2, 3, 4, 5, 6, 7, 8,  // y
-      9, 10,                   // u
-      11, 12                   // v
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [{}, {}, {}],
+  };
+  const expectedLayout = [
+      {offset: 0, stride: 4},
+      {offset: 8, stride: 2},
+      {offset: 10, stride: 2},
+  ];
+  const expectedData = new Uint8Array([
+      1, 2, 3, 4,  // y
+      5, 6, 7, 8,
+      9, 10,       // u
+      11, 12       // v
   ]);
-  assert_equals(frame.allocationSize(options), 12, 'allocationSize()');
-  await frame.readInto(buf, options);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(options), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data, options);
+  assert_layout_equals(layout, expectedLayout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test layout can be empty.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{}]};
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [{}],
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test incorrect plane count.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{offset: 4, stride: 4},
-                          {offset: 0, stride: 2},
-                          {offset: 2, stride: 2}]};
-  let expected = new Uint8Array([
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 4, stride: 4},
+          {offset: 0, stride: 2},
+          {offset: 2, stride: 2},
+      ],
+  };
+  const expectedData = new Uint8Array([
       9, 10,       // u
       11, 12,      // v
       1, 2, 3, 4,  // y
       5, 6, 7, 8,
   ]);
-  assert_equals(frame.allocationSize(options), 12, 'allocationSize()');
-  await frame.readInto(buf, options);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(options), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data, options);
+  assert_layout_equals(layout, options.layout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test stride and offset work.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(20);
-  let options = {layout: [{offset: 9, stride: 5},
-                          {offset: 1, stride: 3},
-                          {offset: 5, stride: 3}]};
-  let expected = new Uint8Array([
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 9, stride: 5},
+          {offset: 1, stride: 3},
+          {offset: 5, stride: 3},
+      ],
+  };
+  const expectedData = new Uint8Array([
       0,
       9, 10, 0,       // u
       0,
@@ -114,102 +173,142 @@ promise_test(async t => {
       0,
       1, 2, 3, 4, 0,  // y
       5, 6, 7, 8, 0,
-      0
   ]);
-  assert_equals(frame.allocationSize(options), 19, 'allocationSize()');
-  await frame.readInto(buf, options);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(options), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data, options);
+  assert_layout_equals(layout, options.layout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test stride and offset with padding.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{offset: 0, stride: 1},
-                          {offset: 8, stride: 2},
-                          {offset: 10, stride: 2}]};
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 0, stride: 1},
+          {offset: 8, stride: 2},
+          {offset: 10, stride: 2},
+      ],
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test invalid stride.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{offset: 0, stride: 4},
-                          {offset: 8, stride: 2},
-                          {offset: 10}]};
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 0, stride: 4},
+          {offset: 8, stride: 2},
+          {offset: 10},
+      ],
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test missing stride.');
 
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{offset: 0, stride: 4},
-                          {offset: 8, stride: 2},
-                          {stride: 2}]};
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 0, stride: 4},
+          {offset: 8, stride: 2},
+          {stride: 2},
+      ],
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test missing offset.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {layout: [{offset: 0, stride: 4},
-                          {offset: 8, stride: 2},
-                          {offset: 2 ** 32 - 2, stride: 2}]};
+  const frame = makeI420_4x2();
+  const options = {
+      layout: [
+          {offset: 0, stride: 4},
+          {offset: 8, stride: 2},
+          {offset: 2 ** 32 - 2, stride: 2},
+      ],
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test address overflow.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {region: frame.codedRegion};
-  let expected = new Uint8Array([
+  const frame = makeI420_4x2();
+  const options = {
+      region: frame.codedRegion,
+  };
+  const expectedLayout = [
+      {offset: 0, stride: 4},
+      {offset: 8, stride: 2},
+      {offset: 10, stride: 2},
+  ];
+  const expectedData = new Uint8Array([
       1, 2, 3, 4, 5, 6, 7, 8,  // y
       9, 10,                   // u
       11, 12                   // v
   ]);
-  assert_equals(frame.allocationSize(options), 12, 'allocationSize()');
-  await frame.readInto(buf, options);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(options), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data, options);
+  assert_layout_equals(layout, expectedLayout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test codedRegion.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {region: {left: 0, top: 0, width: 4, height: 0}};
+  const frame = makeI420_4x2();
+  const options = {
+      region: {left: 0, top: 0, width: 4, height: 0},
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test empty region.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {region: {left: 0, top: 0, width: 4, height: 1}};
+  const frame = makeI420_4x2();
+  const options = {
+      region: {left: 0, top: 0, width: 4, height: 1},
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test unaligned region.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(6);
-  let options = {region: {left: 2, top: 0, width: 2, height: 2}};
-  let expected = new Uint8Array([
-      3, 4, 7, 8,  // y
-      10,          // u
-      12           // v
+  const frame = makeI420_4x2();
+  const options = {
+      region: {left: 2, top: 0, width: 2, height: 2},
+  };
+  const expectedLayout = [
+      {offset: 0, stride: 2},
+      {offset: 4, stride: 1},
+      {offset: 5, stride: 1},
+  ];
+  const expectedData = new Uint8Array([
+      3, 4,  // y
+      7, 8,
+      10,    // u
+      12     // v
   ]);
-  assert_equals(frame.allocationSize(options), 6, 'allocationSize()');
-  await frame.readInto(buf, options);
-  assert_array_equals(buf, expected, 'destination buffer contents');
+  assert_equals(frame.allocationSize(options), expectedData.length, 'allocationSize()');
+  const data = new Uint8Array(expectedData.length);
+  const layout = await frame.readInto(data, options);
+  assert_layout_equals(layout, expectedLayout);
+  assert_buffer_equals(data, expectedData);
 }, 'Test left crop.');
 
 promise_test(async t => {
-  let frame = makeI420_4x2();
-  let buf = new Uint8Array(12);
-  let options = {region: {left: 0, top: 0, width: 4, height: 4}};
+  const frame = makeI420_4x2();
+  const options = {
+      region: {left: 0, top: 0, width: 4, height: 4},
+  };
   assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
-  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(buf, options));
+  const data = new Uint8Array(12);
+  await promise_rejects_dom(t, 'ConstraintError', frame.readInto(data, options));
 }, 'Test invalid region.');
