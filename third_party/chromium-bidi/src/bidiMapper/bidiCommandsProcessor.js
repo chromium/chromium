98781bfe9f1f7dc57115485cd87dba0f3e914f29
Maksim Sadym <sadym@chromium.org>
2021-05-12 10:49:16 +0000

    structure mapper

--- third_party/chromium-bidi/src/bidiMapper/bidiCommandsProcessor.js
+++ third_party/chromium-bidi/src/bidiMapper/bidiCommandsProcessor.js
@@ -1,61 +1,61 @@
 export async function runBidiCommandsProcessor(cdpClient, bidiClient, getCurrentTargetId) {
     const targets = {};
 
+    const getErrorResponse = (commandData, errorCode, errorMessage) => {
+        // TODO: this is bizarre per spec. We reparse the payload and
+        // extract the ID, regardless of what kind of value it was.
+        let commandId = undefined;
+        try {
+                commandId = commandData.id;
+        } catch { }
+
+        return {
+            id: commandId,
+            error: errorCode,
+            message: errorMessage,
+            // TODO: optional stacktrace field.
+        };
+    };
+
+    const respondWithError = (commandData, errorCode, errorMessage) => {
+        const errorResponse = getErrorResponse(commandData, errorCode, errorMessage);
+        bidiClient.sendBidiMessage(errorResponse);
+    };
+
+
     const targetToContext = t => ({
         context: t.targetId,
         parent: t.openerId ? t.openerId : null,
         url: t.url
     });
 
-    const process_browsingContext_getTree = async function (messageId) {
+    const process_browsingContext_getTree = async function (params) {
         const cdpTargets = await cdpClient.sendCdpCommand({ method: "Target.getTargets" });
         const contexts = cdpTargets.targetInfos
             // Don't expose any information about the tab with Mapper running.
             .filter(t => t.targetId !== getCurrentTargetId())
             .map(targetToContext);
-        bidiClient.sendBidiMessage({
-            id: messageId,
-            result: { contexts }
-        });
+        return { contexts };
     };
 
-    const process_PROTO_browsingContext_createContext = async function (message) {
+    const process_PROTO_browsingContext_createContext = async function (params) {
         const { targetId } = await cdpClient.sendCdpCommand({
             method: "Target.createTarget",
-            params: { url: message.params.url }
+            params: { url: params.url }
         });
-
-        bidiClient.sendBidiMessage({
-            id: message.id,
-            result: targetToContext(targets[targetId])
-        });
+        return targetToContext(targets[targetId]);
     };
 
-    const process_DEBUG_Page_close = async function (message) {
+    const process_DEBUG_Page_close = async function (params) {
         await cdpClient.sendCdpCommand({
             method: "Target.closeTarget",
-            params: { targetId: message.params.context }
+            params: { targetId: params.context }
         });
-
-        bidiClient.sendBidiMessage({
-            id: message.id,
-            result: {}
-        });
+        return {};
     };
 
-    const process_session_status = async function (messageId) {
-        bidiClient.sendBidiMessage({
-            id: messageId,
-            result: { "ready": true, message: "ready" }
-        });
-    };
-
-    const process_unknown_command = async function (messageId) {
-        bidiClient.sendBidiMessage({
-            id: messageId,
-            "error": "invalid argument",
-            "message": "not supported operation"
-        });
+    const process_session_status = async function (params) {
+        return { "ready": true, message: "ready" };
     };
 
     const handle_target_attachedToTarget = (message) => {
@@ -87,28 +87,39 @@ export async function runBidiCommandsProcessor(cdpClient, bidiClient, getCurrent
         }
     };
 
-    const onBidiMessage = function (message) {
-        const messageId = message.id;
-        switch (message.method) {
+    const processCommand = async (commandData) => {
+        const response = {};
+        response.id = commandData.id;
+
+        switch (commandData.method) {
             case 'session.status':
-                process_session_status(messageId);
-                return;
+                return await process_session_status(commandData.params);
             case 'browsingContext.getTree':
-                process_browsingContext_getTree(messageId);
-                return;
+                return await process_browsingContext_getTree(commandData.params);
 
             case 'PROTO.browsingContext.createContext':
-                process_PROTO_browsingContext_createContext(message);
-                return;
+                return await process_PROTO_browsingContext_createContext(commandData.params);
 
             case 'DEBUG.Page.close':
-                process_DEBUG_Page_close(message);
-                return;
+                return await process_DEBUG_Page_close(commandData.params);
 
             default:
-                process_unknown_command(messageId);
-                return;
+                throw new Error('unknown command');
         }
+    };
+
+    const onBidiMessage = async (message) => {
+        processCommand(message).then(result => {
+            const response = {
+                id: message.id,
+                result
+            };
+
+            bidiClient.sendBidiMessage(response);
+        }).catch(e => {
+            console.error(e);
+            respondWithError(message, "unknown error", e.message);
+        });
     }
 
     cdpClient.setCdpMessageHandler(onCdpMessage);
