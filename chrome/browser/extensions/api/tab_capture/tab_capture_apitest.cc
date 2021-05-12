9022964153ebfd5b220aaa79e381a6261492718f
Robert Kroeger <rjkroege@chromium.org>
2021-05-12 02:51:38 +0000

    Suppress TabCaptureApiPixelTest.EndToEndWithoutRemoting
    Test TabCaptureApiPixelTest.EndToEndWithoutRemoting is failing on the
    Windows 10 GPU bot. Suppressing.
    
    Bug: 1040894
    Change-Id: Ifa7fee603832a043f923ac3a973a25c8bceed442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2888985
    Auto-Submit: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881817}

--- chrome/browser/extensions/api/tab_capture/tab_capture_apitest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_apitest.cc
@@ -169,7 +169,7 @@ IN_PROC_BROWSER_TEST_F(TabCaptureApiTest, MaxOffscreenTabs) {
 
 // Tests that tab capture video frames can be received in a VIDEO element.
 // Disabled due to flakes on multiple platforms; see https://crbug.com/1040894.
-IN_PROC_BROWSER_TEST_F(TabCaptureApiPixelTest, EndToEndWithoutRemoting) {
+IN_PROC_BROWSER_TEST_F(TabCaptureApiPixelTest, DISABLED_EndToEndWithoutRemoting) {
   if (IsTooIntensiveForThisPlatform()) {
     LOG(WARNING) << "Skipping this CPU-intensive test on this platform/build.";
     return;
