fe59284a48bc5a3af8f058b048e3cc1fe260a1b9
David Bertoni <dbertoni@chromium.org>
2021-05-13 22:46:56 +0000

    [CodeHealth] Remove ListValue::begin()/end().
    This CL removes these deprecated member function.
    
    Bug: 1187107
    Change-Id: I854f318df9faeefaef63c14bfb5e3bc988a737dc
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868016
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882735}

--- base/values.h
+++ base/values.h
@@ -964,15 +964,16 @@ class BASE_EXPORT ListValue : public Value {
   void Swap(ListValue* other);
 
   // Iteration.
-  // DEPRECATED, use `GetList()::begin()` instead.
-  iterator begin() { return GetList().begin(); }
-  // DEPRECATED, use `GetList()::end()` instead.
-  iterator end() { return GetList().end(); }
-
-  // DEPRECATED, use `GetList()::begin()` instead.
-  const_iterator begin() const { return GetList().begin(); }
-  // DEPRECATED, use `GetList()::end()` instead.
-  const_iterator end() const { return GetList().end(); }
+  //
+  // ListValue no longer supports iteration. Instead, use GetList() to get the
+  // underlying list:
+  //
+  // for (const auto& entry : list_value.GetList()) {
+  //   ...
+  //
+  // for (auto it = list_value.GetList().begin();
+  //      it != list_value.GetList().end(); ++it) {
+  //   ...
 
   // DEPRECATED, use `Value::Clone()` instead.
   // TODO(crbug.com/646113): Delete this and migrate callsites.
