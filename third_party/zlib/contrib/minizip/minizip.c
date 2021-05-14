741c3736b5f1a015a6d34bd8393f3074313b4466
Noel Gordon <noel@chromium.org>
2021-05-14 02:25:04 +0000

    [zlib] Build minizip zip and unzip tools
    Minizip is a library provided by //third_party/zlib. Its zip and unzip
    tools can be built in a developer checkout for testing purposes with:
    
      autoninja -C out/Release minizip_bin
      autoninja -C out/Release miniunz_bin
    
    Add GN build rules for these files. Patch minizip and miniunz tools so
    they compile (all bots). Add a patch file and minizip.md for usage.
    
    Bug: 1207895
    Change-Id: Ia2e1a920445414fed78e14d9195e484f209cf089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2887385
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882824}

--- third_party/zlib/contrib/minizip/minizip.c
+++ third_party/zlib/contrib/minizip/minizip.c
@@ -28,7 +28,7 @@
         #endif
 #endif
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__Fuchsia__)
 // In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
 #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
 #define FTELLO_FUNC(stream) ftello(stream)
@@ -94,7 +94,7 @@ uLong filetime(f, tmzip, dt)
   return ret;
 }
 #else
-#ifdef unix || __APPLE__
+#if defined(unix) || defined(__APPLE__) || defined(__Fuchsia__)
 uLong filetime(f, tmzip, dt)
     char *f;               /* name of file to get info on */
     tm_zip *tmzip;         /* return value: access, modific. and creation times */
