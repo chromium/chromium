158e60777ee1b5dd6b1cd5f3ac74c11335e571dd
Chris Lu <thegreenfrog@chromium.org>
2021-05-14 00:37:22 +0000

    [ios] Only relayout overlay presentation context if necessary
    When an Infobar overlay banner is presented behind a fullscreen modal,
    the UI gets stuck in an infinite loop where a relayout slightly modifies
    the banner frame size from the assigned frame size. A layout pass is
    only necessary the first time to resize the presentation context
    to match the presented view frame and when a legitimate resizing of the
    presented view frame occurred.
    
    Bug: 1158309
    Change-Id: I62ca9ab4124d593694cb2f6aa28b7224d491d4ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895046
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882775}

--- ios/chrome/browser/ui/overlays/overlay_presentation_controller.mm
+++ ios/chrome/browser/ui/overlays/overlay_presentation_controller.mm
@@ -10,6 +10,18 @@
 
 @implementation OverlayPresentationController
 
+- (instancetype)
+    initWithPresentedViewController:(UIViewController*)presentedViewController
+           presentingViewController:
+               (nullable UIViewController*)presentingViewController {
+  self = [super initWithPresentedViewController:presentedViewController
+                       presentingViewController:presentingViewController];
+  if (self) {
+    _needsLayout = YES;
+  }
+  return self;
+}
+
 #pragma mark - Accessors
 
 - (BOOL)resizesPresentationContainer {
@@ -27,7 +39,10 @@
   // Trigger a layout pass for the presenting view controller.  This allows the
   // presentation context to resize itself to match the presented overlay UI if
   // |resizesPresentationContainer| is YES.
-  [self.presentingViewController.view setNeedsLayout];
+  if (self.needsLayout) {
+    [self.presentingViewController.view setNeedsLayout];
+    self.needsLayout = NO;
+  }
 }
 
 @end
