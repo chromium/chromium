e09e42d6be87bace75040c543db2f0795fe4d48f
Peng Huang <penghuang@chromium.org>
2021-05-14 20:51:49 +0000

    Enable SkiaRenderer on MacOS
    Bug: 1208173,894929
    Change-Id: I61604f0df01a0bcab825eb8f8839604eb20c3a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894509
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883114}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -44,6 +44,8 @@ const base::Feature kUseSkiaRenderer {
 #if defined(OS_WIN) || defined(OS_ANDROID) || BUILDFLAG(IS_CHROMEOS_LACROS) || \
     defined(OS_LINUX)
       base::FEATURE_ENABLED_BY_DEFAULT
+#elif defined(OS_MAC)
+      base::FEATURE_ENABLED_BY_DEFAULT
 #else
       base::FEATURE_DISABLED_BY_DEFAULT
 #endif
