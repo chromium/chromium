60cfa956f1c56467488e593186b0c619e73591b3
Yutaka Hirano <yhirano@chromium.org>
2021-05-17 05:05:58 +0000

    [XHR] Stop content type sniffing for testing
    xhr/event-error.sub.any.js checks whether the "loaded" and "total"
    properties of "error" and "loadend" events, but it has a hidden
    assumption: at least one progress events have already been dispatched.
    To ensure that the assumption holds, diable the XHR content type
    sniffing with adding some response headers.
    
    Bug: 402812
    Change-Id: I5eb0a50d675d5a9a112344f5fb0aedc8d79d9036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896659
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883369}

--- third_party/blink/web_tests/external/wpt/xhr/resources/bad-chunk-encoding.py
+++ third_party/blink/web_tests/external/wpt/xhr/resources/bad-chunk-encoding.py
@@ -5,6 +5,8 @@ def main(request, response):
     count = 5
     time.sleep(delay)
     response.headers.set(b"Transfer-Encoding", b"chunked")
+    response.headers.set(b"Content-Type", b"text/plain")
+    response.headers.set(b"X-Content-Type-Options", b"nosniff")
     response.close_connection = True
     response.write_status_headers()
     time.sleep(delay)
