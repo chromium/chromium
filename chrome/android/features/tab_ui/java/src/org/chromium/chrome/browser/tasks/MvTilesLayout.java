50fc7af1aedfefce2afad4bee132ce32ba466ff0
spdonghao <spdonghao@chromium.org>
2021-05-17 20:24:28 +0000

    [Start] Remove extra margin at the cut-off of the MV tiles.
    After:
    Portrait:
    https://drive.google.com/file/d/1meopOZM-sNotN2I0OhBa1mRMlZSMhSpm/view?usp=sharing&resourcekey=0-i8mgy7ehYMzggNPctMO5UQ
    https://drive.google.com/file/d/1uDDlZ_78Nv82g4W78KBM3OB-B7jt4DXn/view?usp=sharing&resourcekey=0-WrHh7SEBuZUgviAJpHqAGw
    Landscape:
    https://drive.google.com/file/d/1KgK72IqFIKioHrJt9BxXfmnwHHueASdM/view?usp=sharing&resourcekey=0-pMrjBTuhtkd40Oo9gz7Gtg
    https://drive.google.com/file/d/1v0JXzMUQvhY3b0tIB5CMqAwQzYZR9Ls9/view?usp=sharing&resourcekey=0-IRqnigTzYhVYX-GphCGArA
    
    Bug: 1203511
    Change-Id: Iafc5d9ce9660afecda5dafd623758afbd189adce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2892983
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883592}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/MvTilesLayout.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/MvTilesLayout.java
@@ -19,12 +19,15 @@ public class MvTilesLayout extends LinearLayout {
     private final Context mContext;
     private final int mTileViewPaddingPortrait;
     private final int mTileViewPaddingLandscape;
+    private final int mTileViewPaddingEdgePortrait;
 
     public MvTilesLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
 
         Resources resources = mContext.getResources();
+        mTileViewPaddingEdgePortrait = resources.getDimensionPixelSize(
+                org.chromium.chrome.R.dimen.tile_view_padding_edge_portrait);
         mTileViewPaddingPortrait = resources.getDimensionPixelSize(
                 org.chromium.chrome.R.dimen.tile_view_padding_portrait);
         mTileViewPaddingLandscape = resources.getDimensionPixelSize(
@@ -45,20 +48,34 @@ public class MvTilesLayout extends LinearLayout {
         int childCount = getChildCount();
         if (childCount == 0) return;
 
+        // The margins of the left side of the first tile view and the right side of the last tile
+        // view are larger than the tiles in the middle.
+        int newEdgeMargin = orientation == Configuration.ORIENTATION_PORTRAIT
+                ? mTileViewPaddingEdgePortrait
+                : mTileViewPaddingLandscape;
+        updateSingleTileViewLayout(
+                (TileView) getChildAt(0), newEdgeMargin, /* isSetStartMargin = */ true);
+        updateSingleTileViewLayout((TileView) getChildAt(childCount - 1), newEdgeMargin,
+                /* isSetStartMargin = */ false);
+
         int newMargin = orientation == Configuration.ORIENTATION_PORTRAIT
                 ? mTileViewPaddingPortrait
                 : mTileViewPaddingLandscape;
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 1; i < childCount; i++) {
             TileView tileView = (TileView) getChildAt(i);
-            updateSingleTileViewLayout(tileView, newMargin);
+            updateSingleTileViewLayout(tileView, newMargin, /* isSetStartMargin = */ true);
         }
     }
 
-    private void updateSingleTileViewLayout(TileView tileView, int newMarginStart) {
+    private void updateSingleTileViewLayout(
+            TileView tileView, int newMargin, boolean isSetStartMargin) {
         MarginLayoutParams layoutParams = (MarginLayoutParams) tileView.getLayoutParams();
 
-        if (newMarginStart != layoutParams.getMarginStart()) {
-            layoutParams.setMarginStart(newMarginStart);
+        if (isSetStartMargin && newMargin != layoutParams.getMarginStart()) {
+            layoutParams.setMarginStart(newMargin);
+            tileView.setLayoutParams(layoutParams);
+        } else if (!isSetStartMargin && newMargin != layoutParams.getMarginEnd()) {
+            layoutParams.setMarginEnd(newMargin);
             tileView.setLayoutParams(layoutParams);
         }
     }
