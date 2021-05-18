73fae5f9ab2ad620dd5b062a899463b08763fa7e
Kyle Horimoto <khorimoto@chromium.org>
2021-05-18 19:52:49 +0000

    [CrOS Cellular] Remove unactionable "Cellular" from OOBE
    When a cellular-capable device without a SIM card or any installed eSIM
    profiles starts OOBE, we show an unactionable row called "Cellular",
    which does not do anything. This is confusing and not useful to the
    user, so this CL removes it.
    
    Fixed: 1209244
    Change-Id: If6f612413b4546fcde73cb86acb7a6168f5565b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902219
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884101}

--- ui/webui/resources/cr_components/chromeos/network/network_select.js
+++ ui/webui/resources/cr_components/chromeos/network/network_select.js
@@ -86,12 +86,6 @@ Polymer({
       type: Boolean,
       value: false,
     },
-
-    /**
-     * The cellular DeviceState, or undefined if there is no Cellular device.
-     * @private {!OncMojo.DeviceStateProperties|undefined} deviceState
-     */
-    cellularDeviceState_: Object,
   },
 
   /** @type {!OncMojo.NetworkStateProperties|undefined} */
@@ -262,12 +256,6 @@ Polymer({
    * @private
    */
   onGetNetworkStateList_(deviceStates, networkStates) {
-    this.cellularDeviceState_ = deviceStates.find(function(device) {
-      return device.type === mojom.NetworkType.kCellular;
-    });
-    if (this.cellularDeviceState_) {
-      this.ensureCellularNetwork_(networkStates);
-    }
     this.networkStateList_ = networkStates;
     this.fire('network-list-changed', networkStates);
 
@@ -286,37 +274,6 @@ Polymer({
         undefined;
     // Note: event.detail will be {} if defaultNetwork is undefined.
     this.fire('default-network-changed', defaultNetwork);
-  },
-
-  /**
-   * Modifies |networkStates| to include a cellular network if one is required
-   * but does not exist.
-   * @param {!Array<!OncMojo.NetworkStateProperties>} networkStates
-   * @private
-   */
-  ensureCellularNetwork_(networkStates) {
-    if (networkStates.find(function(network) {
-          return network.type === mojom.NetworkType.kCellular;
-        })) {
-      return;
-    }
-    const deviceState = this.cellularDeviceState_.deviceState;
-    if (deviceState === mojom.DeviceStateType.kDisabled ||
-        deviceState === mojom.DeviceStateType.kProhibited) {
-      return;  // No Cellular network
-    }
-
-    // Note: the default connectionState is kNotConnected.
-    // TODO(khorimoto): Maybe set an 'initializing' CellularState property if
-    // the device state is initializing, see TODO in network_list_item.js.
-
-    // Insert the Cellular network after the Ethernet network if it exists.
-    const idx = (networkStates.length > 0 &&
-                 networkStates[0].type === mojom.NetworkType.kEthernet) ?
-        1 :
-        0;
-    networkStates.splice(
-        idx, 0, OncMojo.getDefaultNetworkState(mojom.NetworkType.kCellular));
   },
 
   /**
