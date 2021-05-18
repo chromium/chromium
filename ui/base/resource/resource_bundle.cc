d0160e6f0672c209f10edf94b57a29fd9e48d7cd
Etienne Bergeron <etienneb@chromium.org>
2021-05-18 19:08:57 +0000

    Add trace event for resource loading
    This CL is adding trace event to help tracking down a regression
    related to resource loading. Trace event will be removed after
    the bug is fixed.
    
    See crbug/1191597 for traces.
    
    Bug: 1191597
    Change-Id: I310ba93910010b7d1fa2b36cb116081d74889dcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902317
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884076}

--- ui/base/resource/resource_bundle.cc
+++ ui/base/resource/resource_bundle.cc
@@ -26,6 +26,7 @@
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/lock.h"
+#include "base/trace_event/base_tracing.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "net/filter/gzip_header.h"
@@ -576,6 +577,14 @@ base::RefCountedMemory* ResourceBundle::LoadDataResourceBytes(
 base::RefCountedMemory* ResourceBundle::LoadDataResourceBytesForScale(
     int resource_id,
     ScaleFactor scale_factor) const {
+  TRACE_EVENT("ui", "ResourceBundle::LoadDataResourceBytesForScale",
+              [&](perfetto::EventContext ctx) {
+                auto* event =
+                    ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+                auto* data = event->set_resource_bundle();
+                data->set_resource_id(resource_id);
+              });
+
   if (delegate_) {
     base::RefCountedMemory* bytes =
         delegate_->LoadDataResourceBytes(resource_id, scale_factor);
