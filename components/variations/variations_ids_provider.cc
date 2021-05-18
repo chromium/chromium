a88c7bab58f38ae95d4f9249fc248ddb2481888e
Henrique Nakashima <hnakashima@chromium.org>
2021-05-18 20:12:27 +0000

    Bump variation ID limit from 35 to 50
    The limit by the DCHECK is being hit, and the actual limit below is 50,
    so bump the DCHECK to match.
    
    Bug: 1204696
    Change-Id: I93ed72e75674c6e8922929a1cca2876aa3ea2dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862932
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884115}

--- components/variations/variations_ids_provider.cc
+++ components/variations/variations_ids_provider.cc
@@ -359,10 +359,10 @@ std::string VariationsIdsProvider::GenerateBase64EncodedProto(
   // This is the bottleneck for the creation of the header, so validate the size
   // here. Force a hard maximum on the ID count in case the Variations server
   // returns too many IDs and DOSs receiving servers with large requests.
-  DCHECK_LE(total_id_count, 35U);
+  DCHECK_LE(total_id_count, 50U);
   UMA_HISTOGRAM_COUNTS_100("Variations.Headers.ExperimentCount",
                            total_id_count);
-  if (total_id_count > 50)
+  if (total_id_count > 75)
     return std::string();
 
   std::string serialized;
