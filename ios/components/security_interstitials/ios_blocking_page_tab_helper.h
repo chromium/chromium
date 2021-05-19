76679254f4a9fd61d0a6793a3470c27d11e67a28
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-19 15:03:28 +0000

    Retire ScopedObserver in /ios/components/security_interstitials.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=ajuma@chromium.org
    
    Bug: 1145565
    Change-Id: Ic8758b0fd3d56571fb41a7971a599ed1bb75b2e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904173
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884479}

--- ios/components/security_interstitials/ios_blocking_page_tab_helper.h
+++ ios/components/security_interstitials/ios_blocking_page_tab_helper.h
@@ -5,7 +5,7 @@
 #ifndef IOS_COMPONENTS_SECURITY_INTERSTITIALS_IOS_BLOCKING_PAGE_TAB_HELPER_H_
 #define IOS_COMPONENTS_SECURITY_INTERSTITIALS_IOS_BLOCKING_PAGE_TAB_HELPER_H_
 
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "ios/components/security_interstitials/ios_security_interstitial_page.h"
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
@@ -74,7 +74,8 @@ class IOSBlockingPageTabHelper
     void WebStateDestroyed(web::WebState* web_state) override;
 
     IOSBlockingPageTabHelper* tab_helper_ = nullptr;
-    ScopedObserver<web::WebState, web::WebStateObserver> scoped_observer_{this};
+    base::ScopedObservation<web::WebState, web::WebStateObserver>
+        scoped_observation_{this};
   };
 
   // The navigation ID of the last committed navigation.  Used to associate
--- ios/components/security_interstitials/ios_blocking_page_tab_helper.mm
+++ ios/components/security_interstitials/ios_blocking_page_tab_helper.mm
@@ -91,7 +91,7 @@ IOSBlockingPageTabHelper::CommittedNavigationIDListener::
                                   IOSBlockingPageTabHelper* tab_helper)
     : tab_helper_(tab_helper) {
   DCHECK(tab_helper_);
-  scoped_observer_.Add(web_state);
+  scoped_observation_.Observe(web_state);
 }
 
 IOSBlockingPageTabHelper::CommittedNavigationIDListener::
@@ -113,7 +113,8 @@ void IOSBlockingPageTabHelper::CommittedNavigationIDListener::
 
 void IOSBlockingPageTabHelper::CommittedNavigationIDListener::WebStateDestroyed(
     web::WebState* web_state) {
-  scoped_observer_.Remove(web_state);
+  DCHECK(scoped_observation_.IsObservingSource(web_state));
+  scoped_observation_.Reset();
 }
 
 }  // namespace security_interstitials
