b54cd49f87aa7813c807ec28e0916b6876e940a7
Lei Zhang <thestig@chromium.org>
2021-05-19 22:46:01 +0000

    Revert "Turn DCHECKs into real checks in a PrintManager IPC handler."
    This reverts commit 500bdd7b8b42c386ccfc05b4ce790206d820a1e6.
    
    Reason for revert: Broke printing, as the new check, though correct,
    does not pass because the renderer's cookie values are out of sync.
    Fixing that is risky, so revert for now to stabilize the M92 branch. 
    
    Original change's description:
    > Turn DCHECKs into real checks in a PrintManager IPC handler.
    >
    > Do not trust the renderer to always send correct values.
    >
    > Change-Id: I1ec9ee76b78d59720bd1e7d9b9caea0f0fe422b8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893123
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#882289}
    
    Bug: 1209824
    Change-Id: Ia8a0ad67d16f015fc40e5d1b5136553c9597c9e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906784
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884729}

--- components/printing/browser/print_manager.cc
+++ components/printing/browser/print_manager.cc
@@ -24,9 +24,8 @@ void PrintManager::RenderFrameDeleted(
 
 void PrintManager::DidGetPrintedPagesCount(int32_t cookie,
                                            uint32_t number_pages) {
-  if (!IsValidCookie(cookie) || number_pages == 0)
-    return;
-
+  DCHECK_GT(cookie, 0);
+  DCHECK_GT(number_pages, 0u);
   number_pages_ = number_pages;
 }
 
