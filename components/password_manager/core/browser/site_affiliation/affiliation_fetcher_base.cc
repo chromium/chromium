d126755a873b086b4f9fa7c18c9b62905776e1c8
Maxim Kolosovskiy <kolos@chromium.org>
2021-05-20 19:01:02 +0000

    [Passwords] Append variations headers to Affiliation Service requests
    Affiliation Service is never accessed in Incognito mode.
    Currently, the service is used only for Signed-in users, but with
    #use-of-hash-affiliation-fetcher #filling-across-affiliated-websites
    requests are also sent for non-signed-in users.
    
    Bug: 1211239
    Change-Id: If5b380cf30473ec38aed45003e20aaeeaa78cfbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909532
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885141}

--- components/password_manager/core/browser/site_affiliation/affiliation_fetcher_base.cc
+++ components/password_manager/core/browser/site_affiliation/affiliation_fetcher_base.cc
@@ -7,6 +7,7 @@
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/password_manager/core/browser/android_affiliation/lookup_affiliation_response_parser.h"
+#include "components/variations/net/variations_http_headers.h"
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -69,6 +70,9 @@ void AffiliationFetcherBase::FinalizeRequest(
       net::LOAD_BYPASS_CACHE | net::LOAD_DISABLE_CACHE;
   resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->method = "POST";
+
+  variations::AppendVariationsHeaderUnknownSignedIn(
+      query_url, variations::InIncognito::kNo, resource_request.get());
 
   DCHECK(!simple_url_loader_);
   simple_url_loader_ = network::SimpleURLLoader::Create(
