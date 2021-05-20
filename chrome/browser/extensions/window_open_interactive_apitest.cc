e5d53e297cd6e1c5395182817cf1a1ac86abf17b
Katie Dektar <katie@chromium.org>
2021-05-20 18:30:42 +0000

    [Sheriff] Disable flaky test, ExtensionApiTest.WindowOpen, on Linux.
    Bug: 477691
    Change-Id: I25f584fc9457e05b68ed04b9c5a68194a23ef516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910814
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885120}

--- chrome/browser/extensions/window_open_interactive_apitest.cc
+++ chrome/browser/extensions/window_open_interactive_apitest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/test/result_catcher.h"
@@ -14,7 +15,13 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WindowOpenFocus) {
 
 // The test uses the chrome.browserAction.openPopup API, which requires that the
 // window can automatically be activated.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WindowOpen) {
+// Fails flakily on Linux. https://crbug.com/477691.
+#if defined(OS_LINUX)
+#define MAYBE_WindowOpen DISABLED_WindowOpen
+#else
+#define MAYBE_WindowOpen WindowOpen
+#endif
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_WindowOpen) {
   extensions::ResultCatcher catcher;
   ASSERT_TRUE(LoadExtension(
       test_data_dir_.AppendASCII("window_open").AppendASCII("spanning"),
