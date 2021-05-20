09f4fbe73ef31afbd6f4be47cce435a4f0fb778e
David Bertoni <dbertoni@chromium.org>
2021-05-20 15:35:23 +0000

    [Extensions] Disable a flaky test on Windows.
    ExtensionBackForwardCacheExtensionsDisabledBrowserTest.ScriptDisallowed
    is flaky on Windows.
    
    Bug: 1204751
    Change-Id: Id205d313a88c1596f3ee3b3d0a7604c730fbac43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897430
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885037}

--- chrome/browser/extensions/back_forward_cache_browsertest.cc
+++ chrome/browser/extensions/back_forward_cache_browsertest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/back_forward_cache/back_forward_cache_disable.h"
@@ -135,9 +136,15 @@ IN_PROC_BROWSER_TEST_F(ExtensionBackForwardCacheExtensionsDisabledBrowserTest,
 }
 
 // Test content script injection disallow the back forward cache.
+// TODO(https://crbug.com/1204751): Very flaky on Windows.
+#if defined(OS_WIN)
+#define MAYBE_ScriptDisallowed DISABLED_ScriptDisallowed
+#else
+#define MAYBE_ScriptDisallowed ScriptDisallowed
+#endif
 IN_PROC_BROWSER_TEST_F(
     ExtensionBackForwardCacheContentScriptDisabledBrowserTest,
-    ScriptDisallowed) {
+    MAYBE_ScriptDisallowed) {
   ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("back_forward_cache")
                                 .AppendASCII("content_script")));
 
