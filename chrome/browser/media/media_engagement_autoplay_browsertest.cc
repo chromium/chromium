cbc8a0bf45a33fddb1081459af21ff8b0d8d6ecc
Takuto Ikuta <tikuta@chromium.org>
2021-05-21 10:26:36 +0000

    Reland "chrome: run tools/media_engagement_preload/make_dafsa.py in python3"
    This reverts commit e30fcda2ebf55ecb2a6a4c3cbeb62028611fbdf9.
    
    Reason for revert:
    use python3 instead of vpython3 explicitly in
    chrome/browser/media/media_engagement_autoplay_browsertest.cc
    
    
    .vpython3 change is for PRESUBMIT.py which uses vpython3 by default.
    
    Original change's description:
    > Revert "chrome: run tools/media_engagement_preload/make_dafsa.py in python3"
    >
    > This reverts commit 07e05386c5d83fb0d428b3a847ee4ddac4b20bfd.
    >
    > Reason for revert: MediaEngagementAutoplayBrowserTest is failing on mac bots:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.11%20Tests/63046/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/43848/overview
    >
    > Original change's description:
    > > chrome: run tools/media_engagement_preload/make_dafsa.py in python3
    > >
    > > Fixed: 1210295
    > > Change-Id: I531f99ba4209086c25157854321e3addf1ce632e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903407
    > > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#884796}
    >
    > Change-Id: I4bb812fa3ddbfba0450574edf1e4b8e8d36c01eb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909213
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#884891}
    
    Change-Id: I5246afcacdf289da798240c60fafd530b8ef60ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908796
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885449}

--- chrome/browser/media/media_engagement_autoplay_browsertest.cc
+++ chrome/browser/media/media_engagement_autoplay_browsertest.cc
@@ -28,18 +28,8 @@
 namespace {
 
 base::FilePath GetPythonPath() {
-#if defined(OS_WIN)
-  // Windows bots do not have python installed and available on the PATH.
-  // Please see infra/doc/users/python.md
-  base::FilePath bot_path =
-      base::FilePath(FILE_PATH_LITERAL("c:/infra-system/bin/python.exe"));
-
-  if (base::PathExists(bot_path))
-    return bot_path;
-  return base::FilePath(FILE_PATH_LITERAL("python.exe"));
-#else
-  return base::FilePath(FILE_PATH_LITERAL("python"));
-#endif
+  // Every environment should have python3.
+  return base::FilePath(FILE_PATH_LITERAL("python3"));
 }
 
 const base::FilePath kTestDataPath = base::FilePath(
--- tools/media_engagement_preload/PRESUBMIT.py
+++ tools/media_engagement_preload/PRESUBMIT.py
@@ -5,6 +5,10 @@
 
 """Chromium presubmit script for src/tools/media_engagement_preload."""
 
+# This line is 'magic' in that git-cl looks for it to decide whether to
+# use Python3 instead of Python2 when running the code in this file.
+USE_PYTHON3 = True
+
 
 def _RunMakeDafsaTests(input_api, output_api):
   """Runs unittest for make_dafsa if any related file has been modified."""
@@ -12,16 +16,14 @@ def _RunMakeDafsaTests(input_api, output_api):
            'tools/media_engagement_preload/make_dafsa_unittest.py')
   if not any(f in input_api.LocalPaths() for f in files):
     return []
-  test_path = input_api.os_path.join(input_api.PresubmitLocalPath(),
-                                     'make_dafsa_unittest.py')
-  cmd_name = 'make_dafsa_unittest'
-  cmd = [input_api.python_executable, test_path]
-  test_cmd = input_api.Command(
-    name=cmd_name,
-    cmd=cmd,
-    kwargs={},
-    message=output_api.PresubmitPromptWarning)
-  return input_api.RunTests([test_cmd])
+
+  return input_api.RunTests(
+      input_api.canned_checks.RunUnitTestsInDirectory(
+          input_api,
+          output_api,
+          input_api.PresubmitLocalPath(),
+          files_to_check=['.*test\.py$'],
+          run_on_python2=False))
 
 
 def CheckChangeOnUpload(input_api, output_api):
