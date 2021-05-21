81ba1747508184170968c7efe2add2a1915644ae
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-05-21 00:25:24 +0000

    [iOS][Non Modal Promo] Add metrics
    Adds the following metrics to non modal promos:
    
    New Histograms
    IOS.DefaultBrowserPromo.NonModal.VisitPastedLink
    IOS.DefaultBrowserPromo.NonModal.Share
    IOS.DefaultBrowserPromo.NonModal.GrowthKit
    IOS.DefaultBrowserPromo.NonModal.OnScreenTime
    Histogram to track "accept, dismiss, timeout" along the # of impression
    
    New User Actions
    IOS.DefaultBrowserPromo.NonModal.Appear
    IOS.DefaultBrowserPromo.NonModal.Timeout
    IOS.DefaultBrowserPromo.NonModal.Dismiss
    
    Bug: 1191563
    Change-Id: Iea903ecbc84851b0544b3d052aaf2eb6c3b7a734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906480
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885303}

---
+++ ios/chrome/browser/ui/default_promo/default_browser_promo_non_modal_metrics_util.mm
@@ -0,0 +1,83 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/default_promo/default_browser_promo_non_modal_metrics_util.h"
+
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
+#include "base/notreached.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+using base::RecordAction;
+using base::UserMetricsAction;
+using base::UmaHistogramEnumeration;
+
+void LogNonModalPromoAction(NonModalPromoAction action,
+                            NonModalPromoTriggerType type,
+                            int impression_number) {
+  switch (action) {
+    case NonModalPromoAction::kAppear:
+      RecordAction(
+          UserMetricsAction("IOS.DefaultBrowserPromo.NonModal.Appear"));
+      break;
+    case NonModalPromoAction::kAccepted:
+      RecordAction(
+          UserMetricsAction("IOS.DefaultBrowserPromo.NonModal.Accepted"));
+      break;
+    case NonModalPromoAction::kDismiss:
+      RecordAction(
+          UserMetricsAction("IOS.DefaultBrowserPromo.NonModal.Dismiss"));
+      break;
+    case NonModalPromoAction::kTimeout:
+      RecordAction(
+          UserMetricsAction("IOS.DefaultBrowserPromo.NonModal.Timeout"));
+      break;
+    case NonModalPromoAction::kBackgroundCancel:
+      // No-op.
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+
+  if (impression_number == 0) {
+    UmaHistogramEnumeration("IOS.DefaultBrowserPromo.NonModal.FirstImpression",
+                            action);
+  } else if (impression_number == 1) {
+    UmaHistogramEnumeration("IOS.DefaultBrowserPromo.NonModal.SecondImpression",
+                            action);
+  }
+
+  switch (type) {
+    case NonModalPromoTriggerType::kPastedLink:
+      UmaHistogramEnumeration(
+          "IOS.DefaultBrowserPromo.NonModal.VisitPastedLink", action);
+
+      break;
+    case NonModalPromoTriggerType::kShare:
+      UmaHistogramEnumeration("IOS.DefaultBrowserPromo.NonModal.Share", action);
+
+      break;
+    case NonModalPromoTriggerType::kGrowthKitOpen:
+      UmaHistogramEnumeration("IOS.DefaultBrowserPromo.NonModal.GrowthKit",
+                              action);
+
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+}
+
+void LogNonModalTimeOnScreen(base::TimeTicks initial_time) {
+  if (initial_time.is_null()) {
+    return;
+  }
+  UmaHistogramMediumTimes("IOS.DefaultBrowserPromo.NonModal.OnScreenTime",
+                          base::TimeTicks::Now() - initial_time);
+}
