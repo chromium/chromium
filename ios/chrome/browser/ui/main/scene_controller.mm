91944635a2f9f8a1a4b3f35dca37f2683ec980e1
Vincent Boisselle <vincb@google.com>
2021-05-22 02:36:01 +0000

    Present FRE on the view controller of the current interface that is active
    This is needed to be able to show the FRE when the browser has the force
    incognito policy.
    
    Tests:
     * Incognito + FRE + Location Prompt: https://drive.google.com/file/d/1tcr4CVzp9X_15AxLQuo0offDHrhbU101/view?usp=sharing&resourcekey=0-Kk1vzRCwIbqtEGGAmGt-OA
     * Regular + FRE + Location Prompt: https://drive.google.com/file/d/15Waum9PuJA9GinXYELtCkcBNemiHnvNu/view?usp=sharing&resourcekey=0-YM5rXaR4LEKq9yDutlD06g
     * Incognito + FRE + settings + Location Prompt: https://drive.google.com/file/d/1QTwhC5b9kTRHJjA0tnqiMb1hOgGv0SR4/view?usp=sharing&resourcekey=0-Sb6PEPo6gCjknsfazpH4lQ
     * New window can open new incognito tab: https://drive.google.com/file/d/1gUcr6oxObw-G1sfByAYp8mx1UbgfK7Qy/view?usp=sharing&resourcekey=0-R-y_R6oQYTLXok_qtBm_3g
    
    (cherry picked from commit 8c1a258601c82b35123ba48481b0a206bb66de0c)
    
    Bug: 1210494
    Change-Id: I25a3680766d03d47c64f9d43fb27ab7026f5f85a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910576
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#885603}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909466
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Auto-Submit: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#1228}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -1133,7 +1133,7 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   WelcomeToChromeViewController* welcomeToChrome =
       [[WelcomeToChromeViewController alloc]
           initWithBrowser:browser
-                presenter:self.mainInterface.bvc
+                presenter:self.currentInterface.bvc
                dispatcher:welcomeHandler];
   self.welcomeToChromeController = welcomeToChrome;
   UINavigationController* navController =
@@ -1144,9 +1144,9 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   CGRect appFrame = [[UIScreen mainScreen] bounds];
   [[navController view] setFrame:appFrame];
   self.sceneState.presentingFirstRunUI = YES;
-  [self.mainInterface.viewController presentViewController:navController
-                                                  animated:NO
-                                                completion:nil];
+  [self.currentInterface.viewController presentViewController:navController
+                                                     animated:NO
+                                                   completion:nil];
 }
 
 // Sets a LocalState pref marking the TOS EULA as accepted.
