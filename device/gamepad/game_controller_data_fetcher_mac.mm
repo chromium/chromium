e99bf7a6e8ada940827a1408014e11be9c9a2d99
Matt Reynolds <mattreynolds@google.com>
2021-05-24 18:01:56 +0000

    gamepad: Block GameController double-enumeration
    macOS 11.3 started enumerating DualSense through the GameController
    framework when connected over Bluetooth. This causes DualSense to be
    enumerated twice, once through GameController and once through IOHID.
    Support for Bluetooth-connected Switch Pro controller and Switch
    Joy-Cons was added in macOS 11.2.
    
    This CL modifies GameControllerDataFetcherMac to ignore gamepads with
    productCategory "DualSense", "Switch Pro Controller", or
    "Nintendo Switch JoyCon (L/R)".
    
    11.3 also started enumerating Xbox Series X|S controller through
    GameController, but it uses productCategory "Xbox One" which is
    already blocked.
    
    Bug: 1176515, 1209272
    Change-Id: I90654721bce928ebd9dd6a4087e7b6356390cfdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910895
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885995}

--- device/gamepad/game_controller_data_fetcher_mac.mm
+++ device/gamepad/game_controller_data_fetcher_mac.mm
@@ -27,34 +27,20 @@ bool IsSupported(GCController* controller) {
   if (![controller extendedGamepad])
     return false;
 
-  // In OS X 10.15, Game Controller API added support for Xbox Wireless
-  // Controller and Dualshock 4. A productCategory property was added to
-  // GCController to allow applications to detect these new categories of
-  // devices.
-  //
-  // In Chrome for Mac, Xbox Wireless Controller and Dualshock 4 are
-  // enumerated by XboxDataFetcher and GamepadPlatformDataFetcherMac,
-  // respectively. If GameControllerDataFetcherMac also enumerates these
-  // devices, it will add duplicate gamepads to the gamepad list.
-  //
-  // On macOS 10.15 or later, use the productCategory property to distinguish
-  // Xbox One and Dualshock 4 and block them from being enumerated by this
-  // data fetcher. On 10.14 or earlier, compare the |vendor_name| against
-  // known values for these devices. Once Chrome no longer supports 10.14, the
-  // |vendor_name| path may be removed.
-  if (base::mac::IsAtLeastOS10_15()) {
-    NSString* product_category =
-        [controller performSelector:@selector(productCategory)];
-    if ([product_category isEqualToString:@"Xbox One"])
+  // In macOS 10.15, support for some console gamepads was added to the Game
+  // Controller framework and a productCategory property was added to enable
+  // applications to detect the new devices. These gamepads are already
+  // supported in Chrome through other data fetchers and must be blocked here to
+  // avoid double-enumeration.
+  if (@available(macOS 10.15, *)) {
+    NSString* product_category = [controller productCategory];
+    if ([product_category isEqualToString:@"Xbox One"] ||
+        [product_category isEqualToString:@"DualShock 4"] ||
+        [product_category isEqualToString:@"DualSense"] ||
+        [product_category isEqualToString:@"Switch Pro Controller"] ||
+        [product_category isEqualToString:@"Nintendo Switch JoyCon (L/R)"]) {
       return false;
-    if ([product_category isEqualToString:@"DualShock 4"])
-      return false;
-  } else {
-    NSString* vendor_name = [controller vendorName];
-    if ([vendor_name isEqualToString:@"Xbox Wireless Controller"])
-      return false;
-    if ([vendor_name isEqualToString:@"Wireless Controller"])
-      return false;
+    }
   }
 
   return true;
@@ -62,9 +48,9 @@ bool IsSupported(GCController* controller) {
 
 }  // namespace
 
-GameControllerDataFetcherMac::GameControllerDataFetcherMac() {}
+GameControllerDataFetcherMac::GameControllerDataFetcherMac() = default;
 
-GameControllerDataFetcherMac::~GameControllerDataFetcherMac() {}
+GameControllerDataFetcherMac::~GameControllerDataFetcherMac() = default;
 
 GamepadSource GameControllerDataFetcherMac::source() {
   return Factory::static_source();
