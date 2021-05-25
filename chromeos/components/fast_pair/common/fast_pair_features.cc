269aaf633de383056b08b7dec11e7c77b68721b4
Shane Fitzpatrick <shanefitz@google.com>
2021-05-25 22:27:23 +0000

    [FastPair] Add Fast Pair feature flag
    This CL adds a base::Feature flag for Fast Pair.
    
    We do not want to add this flag to the UI yet because the feature is still in early development.
    
    Change-Id: I402477bdf2fbe1ac3154a6bf2aaa8b5e15da0429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917995
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886487}

---
+++ chromeos/components/fast_pair/common/fast_pair_features.cc
@@ -0,0 +1,14 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/fast_pair/common/fast_pair_features.h"
+
+namespace chromeos {
+namespace features {
+
+// Enables Fast Pair functionality.
+const base::Feature kFastPair{"FastPair", base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
+}  // namespace chromeos
---
+++ chromeos/components/fast_pair/common/fast_pair_features.h
@@ -0,0 +1,18 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_FAST_PAIR_COMMON_FAST_PAIR_FEATURES_H_
+#define CHROMEOS_COMPONENTS_FAST_PAIR_COMMON_FAST_PAIR_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace chromeos {
+namespace features {
+
+extern const base::Feature kFastPair;
+
+}  // namespace features
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_FAST_PAIR_COMMON_FAST_PAIR_FEATURES_H_
