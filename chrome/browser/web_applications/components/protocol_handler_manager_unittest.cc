1e0421e09ec46553a387bce4288e37eee7dfd79a
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-25 06:09:42 +0000

    DPWA: Remove use of TestAppRegistrar
    This CL removes the use of TestAppRegistrar and replaces with
    TestWebAppRegistryController.
    
    Bug: 1212594
    Change-Id: Ib360cf81ea57cc8676ec995a738f833c6364f4ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909348
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#886182}

--- chrome/browser/web_applications/components/protocol_handler_manager_unittest.cc
+++ chrome/browser/web_applications/components/protocol_handler_manager_unittest.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/test/fake_protocol_handler_manager.h"
-#include "chrome/browser/web_applications/test/test_app_registrar.h"
+#include "chrome/browser/web_applications/test/test_web_app_registry_controller.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
+#include "chrome/browser/web_applications/web_app.h"
 #include "chrome/common/custom_handlers/protocol_handler.h"
 #include "net/base/escape.h"
 
@@ -15,33 +17,57 @@ class ProtocolHandlerManagerTest : public WebAppTest {
   void SetUp() override {
     WebAppTest::SetUp();
 
-    app_registrar_ = std::make_unique<TestAppRegistrar>();
+    test_registry_controller_ =
+        std::make_unique<TestWebAppRegistryController>();
+    test_registry_controller_->SetUp(profile());
     protocol_handler_manager_ =
         std::make_unique<FakeProtocolHandlerManager>(profile());
 
-    protocol_handler_manager_->SetSubsystems(app_registrar_.get());
+    protocol_handler_manager_->SetSubsystems(&app_registrar());
+
+    controller().Init();
+  }
+
+  std::unique_ptr<WebApp> CreateWebApp() {
+    const GURL app_url = GURL("https://example.com/path");
+    const AppId app_id = GenerateAppIdFromURL(app_url);
+
+    auto web_app = std::make_unique<WebApp>(app_id);
+    web_app->AddSource(Source::kSync);
+    web_app->SetDisplayMode(DisplayMode::kStandalone);
+    web_app->SetUserDisplayMode(DisplayMode::kStandalone);
+    web_app->SetName("Name");
+    web_app->SetStartUrl(app_url);
+
+    return web_app;
   }
 
   FakeProtocolHandlerManager& protocol_handler_manager() {
     return *protocol_handler_manager_.get();
   }
 
-  TestAppRegistrar& app_registrar() { return *app_registrar_.get(); }
+  TestWebAppRegistryController& controller() {
+    return *test_registry_controller_;
+  }
+
+  WebAppRegistrar& app_registrar() { return controller().registrar(); }
 
  private:
-  std::unique_ptr<TestAppRegistrar> app_registrar_;
+  std::unique_ptr<TestWebAppRegistryController> test_registry_controller_;
   std::unique_ptr<FakeProtocolHandlerManager> protocol_handler_manager_;
 };
 
 TEST_F(ProtocolHandlerManagerTest, TestGetHandlersFor) {
-  const AppId app_id = "app_id";
+  auto web_app = CreateWebApp();
+  const AppId app_id = web_app->app_id();
+
+  controller().RegisterApp(std::move(web_app));
 
   apps::ProtocolHandlerInfo protocol_handler1;
   const std::string protocol1 = "web+test";
   protocol_handler1.protocol = protocol1;
   protocol_handler1.url = GURL("http://example.com/test=%s");
   protocol_handler_manager().RegisterProtocolHandler(app_id, protocol_handler1);
-  app_registrar().AddExternalApp(app_id, {protocol_handler1.url});
   ProtocolHandler handler1 = ProtocolHandler::CreateWebAppProtocolHandler(
       protocol_handler1.protocol, GURL(protocol_handler1.url), app_id);
 
@@ -50,7 +76,6 @@ TEST_F(ProtocolHandlerManagerTest, TestGetHandlersFor) {
   protocol_handler2.protocol = protocol2;
   protocol_handler2.url = GURL("http://example.net/test=%s");
   protocol_handler_manager().RegisterProtocolHandler(app_id, protocol_handler2);
-  app_registrar().AddExternalApp(app_id, {protocol_handler2.url});
   ProtocolHandler handler2 = ProtocolHandler::CreateWebAppProtocolHandler(
       protocol_handler2.protocol, GURL(protocol_handler2.url), app_id);
 
