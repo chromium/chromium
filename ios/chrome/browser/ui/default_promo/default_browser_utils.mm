f707e57fa45398c2297cb94453453eb6b176a61c
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-05-25 22:34:15 +0000

    [iOS][Default Promo] Change lookback window to 21 days
    This will help to lower the chances of a repeat, irrelevant promo.
    
    This will change the definition of a default user to some that opened a
    system URL in the last 21 days instead of 7. This shift in the
    definition of "no longer default user" will increase the default count,
    though we don't think it is an artificial inflation.
    
    Bug: N/A
    Change-Id: I31386760fd17b681580d830e26a65c164313dcd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2916728
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886495}

--- ios/chrome/browser/ui/default_promo/default_browser_utils.mm
+++ ios/chrome/browser/ui/default_promo/default_browser_utils.mm
@@ -72,11 +72,11 @@ const char kDefaultBrowserFullscreenPromoExperimentChangeStringsGroupParam[] =
     "show_switch_description";
 
 // Time threshold before activity timestamps should be removed. Currently set to
-// seven days.
-const NSTimeInterval kUserActivityTimestampExpiration = 7 * 24 * 60 * 60;
+// 21 days.
+const NSTimeInterval kUserActivityTimestampExpiration = 21 * 24 * 60 * 60;
 // Time threshold for the last URL open before no URL opens likely indicates
 // Chrome is no longer the default browser.
-const NSTimeInterval kLatestURLOpenForDefaultBrowser = 7 * 24 * 60 * 60;
+const NSTimeInterval kLatestURLOpenForDefaultBrowser = 21 * 24 * 60 * 60;
 // Delay for the user to be reshown the fullscreen promo when the user taps on
 // the "Remind Me Later" button. 50 hours.
 const NSTimeInterval kRemindMeLaterPresentationDelay = 50 * 60 * 60;
@@ -84,7 +84,7 @@ const NSTimeInterval kRemindMeLaterPresentationDelay = 50 * 60 * 60;
 // Cool down between fullscreen promos. Currently set to 14 days.
 const NSTimeInterval kFullscreenPromoCoolDown = 14 * 24 * 60 * 60;
 
-// Helper function to clear all timestamps that occur later than 7 days ago and
+// Helper function to clear all timestamps that occur later than 21 days ago and
 // keep it only to 10 timestamps.
 NSMutableArray<NSDate*>* SanitizePastUserEvents(
     NSMutableArray<NSDate*>* pastUserEvents) {
