9e26f8880c96d56fa936e8e27846ca73ab8b0fb4
Stefan Zager <szager@chromium.org>
2021-05-26 22:02:36 +0000

    Send visual viewport resize event when layout size changes
    Bug: 1213418
    Change-Id: If06b175adff1aac6dcd5b49b52de7e1ca5adfa01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920791
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886918}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -4524,6 +4524,7 @@ void Document::LayoutViewportWasResized() {
   if (View()->DidFirstLayout() ||
       load_event_progress_ == kLoadEventInProgress) {
     EnqueueResizeEvent();
+    EnqueueVisualViewportResizeEvent();
   }
   if (!HasViewportUnits())
     return;
