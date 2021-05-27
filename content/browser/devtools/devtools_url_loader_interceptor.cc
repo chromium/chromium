3f2f8c0f8861056914db5f3b9c322520476c1098
Andrey Kosyakov <caseq@chromium.org>
2021-05-27 05:16:44 +0000

    [m92] Reland "Make mime type sniffing in DevToolsURLLoaderInterceptor consistent with real URLLoader"
    This is a reland of 918d412d5f83ccb98909ebe1478667a85c1737ae
    with the test (hopefully) deflaked.
    
    Original change's description:
    > Make mime type sniffing in DevToolsURLLoaderInterceptor consistent with real URLLoader
    >
    > - extract ShouldSniffContent() from url_loader.cc to header_util.{h,cc}
    > - reuse it in DevToolsURLLoaderInterceptor
    >
    > Bug: b/187745024
    > Change-Id: I0cd6904e7912c372aa744b0abb038d867c9195d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905075
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#884328}
    
    (cherry picked from commit adc1e03617adb0e735bd3ad45752e81feec21d2a)
    
    Bug: b/187745024, 1210841
    Change-Id: I498eb48815747f7e2627de1e57072b4ba80e225e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911256
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#885395}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919890
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#105}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- content/browser/devtools/devtools_url_loader_interceptor.cc
+++ content/browser/devtools/devtools_url_loader_interceptor.cc
@@ -35,6 +35,7 @@
 #include "net/url_request/redirect_util.h"
 #include "net/url_request/referrer_policy.h"
 #include "services/network/public/cpp/cors/cors.h"
+#include "services/network/public/cpp/header_util.h"
 #include "services/network/public/cpp/resource_request_body.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "third_party/blink/public/platform/resource_request_blocked_reason.h"
@@ -1037,15 +1038,22 @@ Response InterceptionJob::ProcessResponseOverride(
         base::MakeRefCounted<net::HttpResponseHeaders>(kDummyHeaders);
   }
   head->headers->GetMimeTypeAndCharset(&head->mime_type, &head->charset);
-  if (head->mime_type.empty() && body_size) {
-    size_t bytes_to_sniff =
-        std::min(body_size, static_cast<size_t>(net::kMaxBytesToSniff));
-    net::SniffMimeType(
-        base::StringPiece(body->front_as<const char>() + response_body_offset,
-                          bytes_to_sniff),
-        create_loader_params_->request.url, "",
-        net::ForceSniffFileUrlsForHtml::kDisabled, &head->mime_type);
-    head->did_mime_sniff = true;
+  const GURL& url = create_loader_params_->request.url;
+  if (create_loader_params_->options &
+      network::mojom::kURLLoadOptionSniffMimeType) {
+    if (body_size && network::ShouldSniffContent(url, *head)) {
+      size_t bytes_to_sniff =
+          std::min(body_size, static_cast<size_t>(net::kMaxBytesToSniff));
+      const std::string hint = head->mime_type;
+      net::SniffMimeType(
+          base::StringPiece(body->front_as<const char>() + response_body_offset,
+                            bytes_to_sniff),
+          url, hint, net::ForceSniffFileUrlsForHtml::kDisabled,
+          &head->mime_type);
+      head->did_mime_sniff = true;
+    } else if (head->mime_type.empty()) {
+      head->mime_type.assign("text/plain");
+    }
   }
   // TODO(caseq): we're cheating here a bit, raw_headers() have \0's
   // where real headers would have \r\n, but the sizes here
@@ -1067,7 +1075,7 @@ Response InterceptionJob::ProcessResponseOverride(
   base::OnceClosure continue_after_cookies_set;
   std::string location;
   if (head->headers->IsRedirect(&location)) {
-    GURL redirect_url = create_loader_params_->request.url.Resolve(location);
+    GURL redirect_url = url.Resolve(location);
     if (redirect_url.is_valid()) {
       continue_after_cookies_set =
           base::BindOnce(&InterceptionJob::ProcessRedirectByClient,
