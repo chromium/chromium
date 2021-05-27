3e58484575b91685dd4a8f29a45b226ce6973350
Andreas Haas <ahaas@chromium.org>
2021-05-27 23:29:56 +0000

    [ukm] Add kCrossOriginWasmModuleSharing to UKM-based UseCounter
    We want to deprecate cross-origin sharing of WebAssembly modules as part
    of our efforts to deprecate `document.domain`, see
    https://github.com/mikewest/deprecating-document-domain. We already
    introduced the UMA counter for
    WebFeature::kCrossOriginWasmModuleSharing, and indeed the usage seems
    very low. Next we would like to know which URLs use cross-origin sharing
    of WebAssembly modules. This data will guide our deprecation process.
    
    R=jwd@chromium.org
    
    Bug: chromium:1171334
    Change-Id: Ib928b33e9f4434eae00e296b9c5deae9bb74f816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919832
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887382}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -209,6 +209,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kSerialPortOpen,
           WebFeature::kHidRequestDevice,
           WebFeature::kHidDeviceOpen,
+          WebFeature::kCrossOriginWasmModuleSharing,
       }));
   return *opt_in_features;
 }
