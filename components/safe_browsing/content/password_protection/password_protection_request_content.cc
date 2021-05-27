0130c7bfaa142886972b3fb9fcfa5dc97f044396
Daniel Rubery <drubery@chromium.org>
2021-05-27 22:53:42 +0000

    Cherry pick [M92] - Add metrics for Android visual features
    This CL adds a few metrics to help diagnose why content_area_width and
    height are not populated in so many password protection pings.
    
    (cherry picked from commit 5660487b30e83be9fe383e85db29add9e549d2fe)
    
    Bug: 1211045
    Change-Id: I8782c6f25f117034b82e5567816d4f82e0c98107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907813
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#885594}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923230
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#134}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- components/safe_browsing/content/password_protection/password_protection_request_content.cc
+++ components/safe_browsing/content/password_protection/password_protection_request_content.cc
@@ -4,6 +4,7 @@
 
 #include "components/safe_browsing/content/password_protection/password_protection_request_content.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/thread_pool.h"
 #include "components/safe_browsing/content/password_protection/password_protection_navigation_throttle.h"
@@ -236,10 +237,23 @@ void PasswordProtectionRequestContent::MaybeCollectVisualFeatures() {
       !password_protection_service()->IsIncognito()) {
     content::RenderWidgetHostView* view =
         web_contents_ ? web_contents_->GetRenderWidgetHostView() : nullptr;
+    base::UmaHistogramBoolean(
+        "PasswordProtection.AndroidVisualFeaturesViewNull", (view == nullptr));
+    if (view) {
+      base::UmaHistogramBoolean(
+          "PasswordProtection.AndroidVisualFeaturesNativeViewNull",
+          (view->GetNativeView() == nullptr));
+    }
     if (view && view->GetNativeView()) {
       gfx::SizeF content_area_size = view->GetNativeView()->viewport_size();
       request_proto_->set_content_area_height(content_area_size.height());
       request_proto_->set_content_area_width(content_area_size.width());
+      base::UmaHistogramCounts10000(
+          "PasswordProtection.AndroidVisualFeaturesNativeViewWidth",
+          content_area_size.width());
+      base::UmaHistogramCounts10000(
+          "PasswordProtection.AndroidVisualFeaturesNativeViewHeight",
+          content_area_size.height());
     }
   }
 #endif
