276f276592edbed52db9fdabc93cbca7aec15918
Justin Novosad <junov@chromium.org>
2021-05-27 18:24:03 +0000

    Fix MacOS rendering glitch with OOP-R canvas
    This change ensures that the ANGLE_texture_rectangle extension
    is enabled when the YUV-to-RGB conversion shader is compiled
    inside a WebGL context (which has the extension disabled by
    default). This change also does some refactoring to share code
    with a similar us case in passthrough command decoder.
    
    Bug: 1205722
    Change-Id: I4366a974f202c656ccf17a5988b040a4431ba140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2918140
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887256}

--- ui/gl/yuv_to_rgb_converter.cc
+++ ui/gl/yuv_to_rgb_converter.cc
@@ -9,6 +9,7 @@
 #include "base/strings/stringprintf.h"
 #include "ui/gfx/color_transform.h"
 #include "ui/gl/gl_helper.h"
+#include "ui/gl/gl_utils.h"
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/scoped_binders.h"
 
@@ -158,17 +159,23 @@ YUVToRGBConverter::YUVToRGBConverter(const GLVersionInfo& gl_version_info,
 
   glGenFramebuffersEXT(1, &framebuffer_);
 
-  vertex_buffer_ = GLHelper::SetupQuadVertexBuffer();
-  vertex_shader_ = GLHelper::LoadShader(
-      GL_VERTEX_SHADER,
-      base::StringPrintf("%s\n%s", vertex_header, kVertexShader).c_str());
-  fragment_shader_ = GLHelper::LoadShader(
-      GL_FRAGMENT_SHADER,
-      base::StringPrintf("%s\n%s\n%s", fragment_header,
-                         do_color_conversion.c_str(),
-                         (is_rect ? kFragmentShaderRect : kFragmentShader2D))
-          .c_str());
-  program_ = GLHelper::SetupProgram(vertex_shader_, fragment_shader_);
+  {
+    ScopedEnableTextureRectangleInShaderCompiler enable(
+        g_current_gl_driver->ext.b_GL_ANGLE_texture_rectangle
+            ? g_current_gl_context
+            : nullptr);
+    vertex_buffer_ = GLHelper::SetupQuadVertexBuffer();
+    vertex_shader_ = GLHelper::LoadShader(
+        GL_VERTEX_SHADER,
+        base::StringPrintf("%s\n%s", vertex_header, kVertexShader).c_str());
+    fragment_shader_ = GLHelper::LoadShader(
+        GL_FRAGMENT_SHADER,
+        base::StringPrintf("%s\n%s\n%s", fragment_header,
+                           do_color_conversion.c_str(),
+                           (is_rect ? kFragmentShaderRect : kFragmentShader2D))
+            .c_str());
+    program_ = GLHelper::SetupProgram(vertex_shader_, fragment_shader_);
+  }
 
   ScopedUseProgram use_program(program_);
   size_location_ = glGetUniformLocation(program_, "a_texScale");
