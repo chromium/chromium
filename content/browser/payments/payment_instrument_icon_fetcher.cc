908e3e3a134cb3b9a741ea31aedb2f035696c2ca
Liquan (Max) Gu <maxlg@chromium.org>
2021-05-28 13:31:10 +0000

    Reland: PaymentInstrumentIconFetcher avoids using released WebContents
    * Original CL: crrev.com/c/2917455
    * Reverting CL: crrev.com/c/2920970
    * Reverting reason: The unique pointer frame_routing_ids was used after
      std::move.
    * Relanding change: Change frame_routing_ids to be a reference instead.
    
    Bug: 1212612
    Change-Id: Ic96718d25f70948bbf4bd7650649172395868fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920672
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887497}

--- content/browser/payments/payment_instrument_icon_fetcher.cc
+++ content/browser/payments/payment_instrument_icon_fetcher.cc
@@ -25,13 +25,19 @@ namespace content {
 namespace {
 
 void DownloadBestMatchingIcon(
-    WebContents* web_contents,
+    const GURL& scope,
+    std::unique_ptr<std::vector<GlobalFrameRoutingId>> frame_routing_ids,
     const std::vector<blink::Manifest::ImageResource>& icons,
     PaymentInstrumentIconFetcher::PaymentInstrumentIconFetcherCallback
         callback);
 
+WebContents* GetWebContentsFromFrameRoutingIds(
+    const GURL& scope,
+    const std::vector<GlobalFrameRoutingId>& frame_routing_ids);
+
 void OnIconFetched(
-    WebContents* web_contents,
+    const GURL& scope,
+    std::unique_ptr<std::vector<GlobalFrameRoutingId>> frame_routing_ids,
     const std::vector<blink::Manifest::ImageResource>& icons,
     PaymentInstrumentIconFetcher::PaymentInstrumentIconFetcherCallback callback,
     const SkBitmap& bitmap) {
@@ -46,7 +52,8 @@ void OnIconFetched(
     } else {
       // If could not download or decode the chosen image(e.g. not supported,
       // invalid), try it again with remaining icons.
-      DownloadBestMatchingIcon(web_contents, icons, std::move(callback));
+      DownloadBestMatchingIcon(scope, std::move(frame_routing_ids), icons,
+                               std::move(callback));
     }
     return;
   }
@@ -64,12 +71,15 @@ void OnIconFetched(
 }
 
 void DownloadBestMatchingIcon(
-    WebContents* web_contents,
+    const GURL& scope,
+    std::unique_ptr<std::vector<GlobalFrameRoutingId>> frame_routing_ids,
     const std::vector<blink::Manifest::ImageResource>& icons,
     PaymentInstrumentIconFetcher::PaymentInstrumentIconFetcherCallback
         callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
+  WebContents* web_contents =
+      GetWebContentsFromFrameRoutingIds(scope, *frame_routing_ids);
   if (web_contents == nullptr) {
     BrowserThread::GetTaskRunnerForThread(
         ServiceWorkerContext::GetCoreThreadId())
@@ -108,18 +118,18 @@ void DownloadBestMatchingIcon(
       payments::IconSizeCalculator::IdealIconHeight(native_view),
       payments::IconSizeCalculator::MinimumIconHeight(),
       /* maximum_icon_size_in_px= */ std::numeric_limits<int>::max(),
-      base::BindOnce(&OnIconFetched, web_contents, copy_icons,
-                     std::move(callback)),
+      base::BindOnce(&OnIconFetched, scope, std::move(frame_routing_ids),
+                     copy_icons, std::move(callback)),
       false /* square_only */);
   DCHECK(can_download_icon);
 }
 
 WebContents* GetWebContentsFromFrameRoutingIds(
     const GURL& scope,
-    std::unique_ptr<std::vector<GlobalFrameRoutingId>> frame_routing_ids) {
+    const std::vector<GlobalFrameRoutingId>& frame_routing_ids) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  for (const auto& ids : *frame_routing_ids) {
+  for (const auto& ids : frame_routing_ids) {
     RenderFrameHostImpl* render_frame_host =
         RenderFrameHostImpl::FromID(ids.child_id, ids.frame_routing_id);
     if (!render_frame_host)
@@ -145,9 +155,8 @@ void StartOnUI(
         callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  WebContents* web_contents =
-      GetWebContentsFromFrameRoutingIds(scope, std::move(frame_routing_ids));
-  DownloadBestMatchingIcon(web_contents, icons, std::move(callback));
+  DownloadBestMatchingIcon(scope, std::move(frame_routing_ids), icons,
+                           std::move(callback));
 }
 
 }  // namespace
