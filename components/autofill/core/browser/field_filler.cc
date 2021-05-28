624f194d9f2d988a7f8e450bc39f5f1120865312
Mohamed Amir Yosef <mamir@chromium.org>
2021-05-28 12:02:23 +0000

    [Autofill] Check the size of both contents and values in FormFieldData
    Details are in the linked bug.
    
    Bug: 1213313
    Change-Id: I9f018cd0d6a5820f9496a494dbb210445ae99f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923844
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#887493}

--- components/autofill/core/browser/field_filler.cc
+++ components/autofill/core/browser/field_filler.cc
@@ -53,7 +53,9 @@ bool SetSelectControlValue(const std::u16string& value,
 
   DCHECK_EQ(field->option_values.size(), field->option_contents.size());
   std::u16string best_match;
-  for (size_t i = 0; i < field->option_values.size(); ++i) {
+  size_t items_count =
+      std::min(field->option_contents.size(), field->option_values.size());
+  for (size_t i = 0; i < items_count; ++i) {
     if (value == field->option_values[i] ||
         value == field->option_contents[i]) {
       // An exact match, use it.
@@ -116,8 +118,9 @@ bool SetSelectControlValueTokenMatch(const std::u16string& value,
   std::vector<std::u16string> tokenized;
   DCHECK_EQ(field->option_values.size(), field->option_contents.size());
   l10n::CaseInsensitiveCompare compare;
-
-  for (size_t i = 0; i < field->option_values.size(); ++i) {
+  size_t items_count =
+      std::min(field->option_contents.size(), field->option_values.size());
+  for (size_t i = 0; i < items_count; ++i) {
     tokenized =
         base::SplitString(field->option_values[i], base::kWhitespaceASCIIAs16,
                           base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
@@ -230,7 +233,9 @@ bool FillNumericSelectControl(int value,
                               FormFieldData* field,
                               std::string* failure_to_fill) {
   DCHECK_EQ(field->option_values.size(), field->option_contents.size());
-  for (size_t i = 0; i < field->option_values.size(); ++i) {
+  size_t items_count =
+      std::min(field->option_contents.size(), field->option_values.size());
+  for (size_t i = 0; i < items_count; ++i) {
     int option;
     if ((StringToInt(field->option_values[i], &option) && option == value) ||
         (StringToInt(field->option_contents[i], &option) && option == value)) {
@@ -463,7 +468,9 @@ bool FillYearSelectControl(const std::u16string& value,
   }
 
   DCHECK_EQ(field->option_values.size(), field->option_contents.size());
-  for (size_t i = 0; i < field->option_values.size(); ++i) {
+  size_t items_count =
+      std::min(field->option_contents.size(), field->option_values.size());
+  for (size_t i = 0; i < items_count; ++i) {
     if (LastTwoDigitsMatch(value, field->option_values[i]) ||
         LastTwoDigitsMatch(value, field->option_contents[i])) {
       field->value = field->option_values[i];
@@ -979,8 +986,9 @@ int FieldFiller::FindShortestSubstringMatchInSelect(
       ignore_whitespace ? RemoveWhitespace(value) : value;
   base::i18n::FixedPatternStringSearchIgnoringCaseAndAccents searcher(
       value_stripped);
-
-  for (size_t i = 0; i < field->option_values.size(); ++i) {
+  size_t items_count =
+      std::min(field->option_contents.size(), field->option_values.size());
+  for (size_t i = 0; i < items_count; ++i) {
     std::u16string option_value =
         ignore_whitespace ? RemoveWhitespace(field->option_values[i])
                           : field->option_values[i];
