c781b6e601a1ece446530e2927db3e2ae44e6ba5
jlebel <jlebel@chromium.org>
2021-05-31 11:22:29 +0000

    [iOS] Update IdentityChooser background color
    Change the IdentityChooser background color to have only one color.
    
    Before:
    https://drive.google.com/file/d/1WBsAzRmkWxRCdX6WHadCmd2bnjdNHlZS/view?usp=sharing&resourcekey=0-zVf90H8pB0LNCmn9_hBGaQ
    After:
    https://drive.google.com/file/d/1QruWIgzaRHsC_905RDOx4LbTMoicQf2O/view?usp=sharing&resourcekey=0--fFWJWAPZ41nUqoZFL1y9A
    https://drive.google.com/file/d/1MNv6GreFJ35rCB27HSdUMVRBIYnHlPB9/view?usp=sharing&resourcekey=0-7w4FbeNRTSpedWain7uzvQ
    
    (cherry picked from commit 435cdb083b3aa87dbe55e2a7bec689716dac2e46)
    
    Fixed: 1213467
    Change-Id: I481845745be0f7ba79ea01e77c5e69704869d048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919735
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#886789}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927602
    Cr-Commit-Position: refs/branch-heads/4515@{#170}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_header_item.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_header_item.mm
@@ -5,6 +5,7 @@
 #import "ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_header_item.h"
 
 #import "ios/chrome/common/ui/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -37,6 +38,8 @@ CGFloat kLeadingMargin = 24.;
     label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     label.textColor = UIColor.cr_labelColor;
     [self.contentView addSubview:label];
+    self.contentView.backgroundColor =
+        [UIColor colorNamed:kGroupedSecondaryBackgroundColor];
     NSDictionary* views = @{
       @"label" : label,
     };
