a53ea3f89cecd2e85a4b511075612d825f40ceee
Ioana Pandele <ioanap@chromium.org>
2021-05-31 17:27:35 +0000

    Revert "[PwdEditAndroid]Don't handle text changes before the model is initialized"
    This reverts commit ef80ae82594fad3297011c281a28c5378a6b39f7.
    
    Reason for revert: This doesn't fix the issue. 
    
    Original change's description:
    > [PwdEditAndroid]Don't handle text changes before the model is initialized
    >
    > Sometimes text change events fire before the initial text is even set on the
    > field. This causes an empty string to be set on the model instead of the
    > password. Checking if the model is initialized before handling a text
    > change should fix the issue.
    >
    > Bug: 1213843
    > Change-Id: I2ec590ec0c37a9f7d36d6128daed8a311ca30071
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919759
    > Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#887156}
    
    Bug: 1213843
    Change-Id: Iefeedbb52f405eb1f43e8231da88d0e1d4e0e8f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927605
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887777}

--- chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditMediator.java
+++ chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditMediator.java
@@ -206,8 +206,6 @@ public class CredentialEditMediator implements UiActionHandler {
 
     @Override
     public void onUsernameTextChanged(String username) {
-        // Ignore events before the UI is fully initialized.
-        if (mModel.get(USERNAME) == null) return;
         mModel.set(USERNAME, username);
         boolean hasError =
                 !mOriginalUsername.equals(username) && mExistingUsernames.contains(username);
@@ -220,8 +218,6 @@ public class CredentialEditMediator implements UiActionHandler {
 
     @Override
     public void onPasswordTextChanged(String password) {
-        // Ignore events before the UI is fully initialized.
-        if (mModel.get(PASSWORD) == null) return;
         mModel.set(PASSWORD, password);
         mModel.set(EMPTY_PASSWORD_ERROR, password.isEmpty());
         if (password.isEmpty()) {
