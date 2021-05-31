b816209cb762d17018d264be000314134ae525af
Ella Ge <eirage@google.com>
2021-05-31 18:45:36 +0000

    Disable StartupTracingTest.TestEnableTracing on all bots
    failing on Linux and Android bots
    
    No-try: true
    Bug: 1213441
    Change-Id: Ibe44c774729bfcc9f7ac306845aa525ad84a7d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929137
    Owners-Override: Ella Ge <eirage@google.com>
    Auto-Submit: Ella Ge <eirage@google.com>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887804}

--- content/browser/tracing/startup_tracing_browsertest.cc
+++ content/browser/tracing/startup_tracing_browsertest.cc
@@ -311,12 +311,8 @@ INSTANTIATE_TEST_SUITE_P(
             OutputLocation::kDirectoryWithBasenameUpdatedBeforeStop)));
 
 // TODO(crbug.com/1197278): Failing on Windows 7 debug builds.
-#if defined(OS_WIN) && DCHECK_IS_ON()
-#define MAYBE_TestEnableTracing DISABLED_TestEnableTracing
-#else
-#define MAYBE_TestEnableTracing TestEnableTracing
-#endif
-IN_PROC_BROWSER_TEST_P(StartupTracingTest, MAYBE_TestEnableTracing) {
+// TODO(crbug.com/1213441): Failing on Linux and Android.
+IN_PROC_BROWSER_TEST_P(StartupTracingTest, DISABLED_TestEnableTracing) {
   EXPECT_TRUE(NavigateToURL(shell(), GetTestUrl("", "title1.html")));
 
   if (GetOutputLocation() ==
