1d8ef365cd1af02bc0742f93d88765132b849fae
Vidhan Jain <vidhanj@google.com>
2021-06-01 18:40:10 +0000

    [iOS][Autofill][SavePrompts] Prevent removal of fallback icon post save/update
    In the current scenario, the infobar is explicitly removed after the
    save/update as well as if the infobar banner is dismissed, resulting in
    the removal of the fallback icon from the omnibox.
    This change removes that logic such that there would always be a
    fallback icon even after save/update or when banner/modal is dismissed.
    Now, the fallback icon would be dismissed only when the user navigates
    away.
    
    Video:
    https://drive.google.com/file/d/1Wx7cSgpIpoFnXPM9lh7bRZvKVe1422hW/view?usp=sharing&resourcekey=0-KnqUWeNFYmX7PV83Zy1miA
    
    (cherry picked from commit 649d8ebefe46a913c025cd2f37150b52eb30df34)
    
    Bug: 1167062, 1213496
    Change-Id: I9817d18b8a7a39c6e49d68ba3ef3170133ba62e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919693
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#887183}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927600
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4515@{#204}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_banner_interaction_handler.h
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_banner_interaction_handler.h
@@ -9,10 +9,6 @@
 
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/common/infobar_banner_interaction_handler.h"
 
-namespace autofill {
-class AutofillSaveUpdateAddressProfileDelegateIOS;
-}
-
 // Helper object that updates the model layer for interaction events with the
 // SaveAddressProfile infobar banner UI.
 class SaveAddressProfileInfobarBannerInteractionHandler
@@ -22,17 +18,8 @@ class SaveAddressProfileInfobarBannerInteractionHandler
   ~SaveAddressProfileInfobarBannerInteractionHandler() override;
 
   // InfobarBannerInteractionHandler:
-  void BannerVisibilityChanged(InfoBarIOS* infobar, bool visible) override;
-  void ShowModalButtonTapped(InfoBarIOS* infobar,
-                             web::WebState* web_state) override;
-  // The delegate's InfoBarDismissed() would be called in
-  // BannerVisibilityChanged().
+  void BannerVisibilityChanged(InfoBarIOS* infobar, bool visible) override {}
   void BannerDismissedByUser(InfoBarIOS* infobar) override {}
-
- private:
-  // Returns the SaveAddressProfile delegate from |infobar|.
-  autofill::AutofillSaveUpdateAddressProfileDelegateIOS* GetInfobarDelegate(
-      InfoBarIOS* infobar);
 };
 
 #endif  // IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_AUTOFILL_ADDRESS_PROFILE_SAVE_ADDRESS_PROFILE_INFOBAR_BANNER_INTERACTION_HANDLER_H_
--- ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_banner_interaction_handler.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_banner_interaction_handler.mm
@@ -4,13 +4,8 @@
 
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_banner_interaction_handler.h"
 
-#include "base/check.h"
-#include "components/autofill/core/browser/autofill_save_update_address_profile_delegate_ios.h"
 #include "ios/chrome/browser/infobars/infobar_ios.h"
-#import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_cancel_handler.h"
-#import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_inserter.h"
 #import "ios/chrome/browser/overlays/public/infobar_banner/save_address_profile_infobar_banner_overlay_request_config.h"
-#include "ios/chrome/browser/overlays/public/overlay_request_queue.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -28,37 +23,3 @@ SaveAddressProfileInfobarBannerInteractionHandler::
 
 SaveAddressProfileInfobarBannerInteractionHandler::
     ~SaveAddressProfileInfobarBannerInteractionHandler() = default;
-
-void SaveAddressProfileInfobarBannerInteractionHandler::BannerVisibilityChanged(
-    InfoBarIOS* infobar,
-    bool visible) {
-  if (!visible && !infobar->accepted()) {
-    GetInfobarDelegate(infobar)->InfoBarDismissed();
-    if (!GetInfobarDelegate(infobar)->modal_was_shown()) {
-      // Remove the infobar to prevent showing the modal when the user has
-      // cancelled the banner.
-      infobar->RemoveSelf();
-    }
-  }
-}
-
-void SaveAddressProfileInfobarBannerInteractionHandler::ShowModalButtonTapped(
-    InfoBarIOS* infobar,
-    web::WebState* web_state) {
-  // Inform delegate that the modal is shown.
-  GetInfobarDelegate(infobar)->set_modal_was_shown_to_true();
-
-  InfobarBannerInteractionHandler::ShowModalButtonTapped(infobar, web_state);
-}
-
-#pragma mark - Private
-
-autofill::AutofillSaveUpdateAddressProfileDelegateIOS*
-SaveAddressProfileInfobarBannerInteractionHandler::GetInfobarDelegate(
-    InfoBarIOS* infobar) {
-  autofill::AutofillSaveUpdateAddressProfileDelegateIOS* delegate =
-      autofill::AutofillSaveUpdateAddressProfileDelegateIOS::
-          FromInfobarDelegate(infobar->delegate());
-  DCHECK(delegate);
-  return delegate;
-}
