8bfa91a169ea0c4ffc7c100c3922992482733dbf
Maksim Ivanov <emaxx@chromium.org>
2021-06-01 14:08:01 +0000

    Fix potential use-after-move in //components/os_crypt/
    Fix the potential scenario in which the already-moved-from variable
    could be used again in key_storage_linux.cc.
    
    The problem was found by the clang-tidy bugprone-use-after-move
    diagnostics:
    
      key_storage_linux.cc:186:
      'application_name' used after it was moved
      key_storage_linux.cc:173:
      move occurred here
    
    Bug: 1122844
    Change-Id: Idb6ad5ae353c6ddd5192ae05f750ff59255a46e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929176
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887953}

--- components/os_crypt/key_storage_linux.cc
+++ components/os_crypt/key_storage_linux.cc
@@ -170,7 +170,7 @@ std::unique_ptr<KeyStorageLinux> KeyStorageLinux::CreateServiceInternal(
 #if defined(USE_LIBSECRET)
   if (selected_backend == os_crypt::SelectedLinuxBackend::GNOME_ANY ||
       selected_backend == os_crypt::SelectedLinuxBackend::GNOME_LIBSECRET) {
-    key_storage = std::make_unique<KeyStorageLibsecret>(std::move(application_name));
+    key_storage = std::make_unique<KeyStorageLibsecret>(application_name);
     if (key_storage->WaitForInitOnTaskRunner()) {
       VLOG(1) << "OSCrypt using Libsecret as backend.";
       return key_storage;
@@ -183,7 +183,7 @@ std::unique_ptr<KeyStorageLinux> KeyStorageLinux::CreateServiceInternal(
   if (selected_backend == os_crypt::SelectedLinuxBackend::GNOME_ANY ||
       selected_backend == os_crypt::SelectedLinuxBackend::GNOME_KEYRING) {
     key_storage = std::make_unique<KeyStorageKeyring>(config.main_thread_runner,
-                                                      std::move(application_name));
+                                                      application_name);
     if (key_storage->WaitForInitOnTaskRunner()) {
       VLOG(1) << "OSCrypt using Keyring as backend.";
       return key_storage;
