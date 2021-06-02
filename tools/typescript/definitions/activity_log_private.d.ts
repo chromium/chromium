315d1bb1c5d5b5ffa5f2b0360df5ed41df6ef75d
dpapad <dpapad@chromium.org>
2021-06-02 22:12:46 +0000

    [Reland] TypeScript: Move ChromeEvent definition to its own file.
    Original CL was erroneously reverted, https://crbug.com/1215527.
    
    Need to change
    declare namespace chrome {...}
    
    to
    declare global { export namespace chrome {...} }
    
    in all files where ChromeEvent is imported, since once a file uses the
    "import" keyword, it is no longer considered an "ambient module" by
    tsc and no longer refers to the global scope by default.
    
    Bug: 1189595
    Change-Id: I908884572e7706cc3a13eea82362761c6eb2f2fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934178
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888629}

--- tools/typescript/definitions/activity_log_private.d.ts
+++ tools/typescript/definitions/activity_log_private.d.ts
@@ -5,87 +5,84 @@
 /** @fileoverview Definitions for chrome.activityLogPrivate API. */
 // TODO(crbug.com/1203307): Auto-generate this file.
 
-declare namespace chrome {
-  export namespace activityLogPrivate {
+import {ChromeEvent} from './chrome_event.js';
 
-    export enum ExtensionActivityType {
-      API_CALL = 'api_call',
-      API_EVENT = 'api_event',
-      CONTENT_SCRIPT = 'content_script',
-      DOM_ACCESS = 'dom_access',
-      DOM_EVENT = 'dom_event',
-      WEB_REQUEST = 'web_request',
+declare global {
+  export namespace chrome {
+    export namespace activityLogPrivate {
+
+      export enum ExtensionActivityType {
+        API_CALL = 'api_call',
+        API_EVENT = 'api_event',
+        CONTENT_SCRIPT = 'content_script',
+        DOM_ACCESS = 'dom_access',
+        DOM_EVENT = 'dom_event',
+        WEB_REQUEST = 'web_request',
+      }
+
+      export enum ExtensionActivityFilter {
+        API_CALL = 'api_call',
+        API_EVENT = 'api_event',
+        CONTENT_SCRIPT = 'content_script',
+        DOM_ACCESS = 'dom_access',
+        DOM_EVENT = 'dom_event',
+        WEB_REQUEST = 'web_request',
+        ANY = 'any',
+      }
+
+      export enum ExtensionActivityDomVerb {
+        GETTER = 'getter',
+        SETTER = 'setter',
+        METHOD = 'method',
+        INSERTED = 'inserted',
+        XHR = 'xhr',
+        WEBREQUEST = 'webrequest',
+        MODIFIED = 'modified',
+      }
+
+      export type ExtensionActivity = {
+        activityId?: string,
+        extensionId?: string, activityType: ExtensionActivityType,
+        time?: number,
+        apiCall?: string,
+        args?: string,
+        count?: number,
+        pageUrl?: string,
+        pageTitle?: string,
+        argUrl?: string,
+        other?: {
+          prerender?: boolean,
+          domVerb?: ExtensionActivityDomVerb,
+          webRequest?: string,
+          extra?: string,
+        },
+      };
+
+      export type Filter = {
+        extensionId?: string, activityType: ExtensionActivityFilter,
+        apiCall?: string,
+        pageUrl?: string,
+        argUrl?: string,
+        daysAgo?: number
+      };
+
+      export type ActivityResultSet = {
+        activities: chrome.activityLogPrivate.ExtensionActivity[],
+      };
+
+      type VoidCallback = () => void;
+
+      export function getExtensionActivities(
+          filter: Filter, callback: (result: ActivityResultSet) => void): void;
+      export function deleteActivities(
+          activityIds: string[], callback?: VoidCallback): void;
+      export function deleteActivitiesByExtension(
+          extensionId: string, callback?: VoidCallback): void;
+      export function deleteDatabase(): void;
+      export function deleteUrls(urls: string[]): void;
+
+      export const onExtensionActivity:
+          ChromeEvent<(activity: ExtensionActivity) => void>;
     }
-
-    export enum ExtensionActivityFilter {
-      API_CALL = 'api_call',
-      API_EVENT = 'api_event',
-      CONTENT_SCRIPT = 'content_script',
-      DOM_ACCESS = 'dom_access',
-      DOM_EVENT = 'dom_event',
-      WEB_REQUEST = 'web_request',
-      ANY = 'any',
-    }
-
-    export enum ExtensionActivityDomVerb {
-      GETTER = 'getter',
-      SETTER = 'setter',
-      METHOD = 'method',
-      INSERTED = 'inserted',
-      XHR = 'xhr',
-      WEBREQUEST = 'webrequest',
-      MODIFIED = 'modified',
-    }
-
-    export type ExtensionActivity = {
-      activityId?: string,
-      extensionId?: string, activityType: ExtensionActivityType,
-      time?: number,
-      apiCall?: string,
-      args?: string,
-      count?: number,
-      pageUrl?: string,
-      pageTitle?: string,
-      argUrl?: string,
-      other?: {
-        prerender?: boolean,
-        domVerb?: ExtensionActivityDomVerb,
-        webRequest?: string,
-        extra?: string,
-      },
-    };
-
-    export type Filter = {
-      extensionId?: string, activityType: ExtensionActivityFilter,
-      apiCall?: string,
-      pageUrl?: string,
-      argUrl?: string,
-      daysAgo?: number
-    };
-
-    export type ActivityResultSet = {
-      activities: chrome.activityLogPrivate.ExtensionActivity[],
-    };
-
-    type VoidCallback = () => void;
-
-    export function getExtensionActivities(
-        filter: Filter, callback: (result: ActivityResultSet) => void): void;
-    export function deleteActivities(
-        activityIds: string[], callback?: VoidCallback): void;
-    export function deleteActivitiesByExtension(
-        extensionId: string, callback?: VoidCallback): void;
-    export function deleteDatabase(): void;
-    export function deleteUrls(urls: string[]): void;
-
-    // This is a workaround for the fact that importing other .d.ts files does
-    // not work. ChromeEvent is also used elsewhere so should be in its own
-    // file.
-    export interface ChromeEvent<ListenerType> {
-      addListener(listener: ListenerType): void;
-      removeListener(listener: ListenerType): void;
-    }
-
-    export const onExtensionActivity: ChromeEvent<(activity: ExtensionActivity) => void>;
   }
 }
--- tools/typescript/definitions/bookmarks.d.ts
+++ tools/typescript/definitions/bookmarks.d.ts
@@ -5,123 +5,120 @@
 /** @fileoverview Definitions for chrome.bookmarks API. */
 // TODO(crbug.com/1203307): Auto-generate this file.
 
-declare namespace chrome {
-  export namespace bookmarks {
-    export enum BookmarkTreeNodeUnmodifiable {
-      MANAGED = 'managed',
+import {ChromeEvent} from './chrome_event.js';
+
+declare global {
+  export namespace chrome {
+    export namespace bookmarks {
+      export enum BookmarkTreeNodeUnmodifiable {
+        MANAGED = 'managed',
+      }
+
+      export interface BookmarkTreeNode {
+        id: string;
+        parentId?: string;
+        index?: number;
+        url?: string;
+        title: string;
+        dateAdded?: number;
+        dateGroupModified?: number;
+        unmodifiable?: BookmarkTreeNodeUnmodifiable;
+        children?: BookmarkTreeNode[];
+      }
+
+      export interface CreateDetails {
+        parentId?: string|null;
+        index?: number;
+        title?: string;
+        url?: string;
+      }
+
+      export const MAX_WRITE_OPERATIONS_PER_HOUR: number;
+      export const MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE: number;
+
+      export function get(
+          idOrIdList: string|string[],
+          callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function getChildren(
+          id: string, callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function getRecent(
+          numberOfItems: number,
+          callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function getTree(callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function getSubTree(
+          id: string, callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function search(
+          query: string|{
+            query: string | undefined,
+            url: string|undefined,
+            title: string|undefined
+          },
+          callback: (p1: BookmarkTreeNode[]) => void): void;
+
+      export function create(
+          bookmark: CreateDetails,
+          callback?: (p1: BookmarkTreeNode) => void): void;
+
+      export function move(
+          id: string,
+          destination: {parentId: string|undefined, index: number|undefined},
+          callback?: (p1: BookmarkTreeNode) => void): void;
+
+      export function update(
+          id: string, changes: {title?: string, url?: string},
+          callback?: (p1: BookmarkTreeNode) => void): void;
+
+      export function remove(id: string, callback?: () => void): void;
+      export function removeTree(id: string, callback?: () => void): void;
+      function _import(callback?: () => void): void;
+      function _export(callback?: () => void): void;
+      export { _import as import }
+      export { _export as export }
+
+      export const onCreated:
+          ChromeEvent<(id: string, bookmark: BookmarkTreeNode) => void>;
+
+      export interface ChangeInfo {
+        title: string;
+        url: string;
+      }
+
+      export const onChanged:
+          ChromeEvent<(id: string, changeInfo: ChangeInfo) => void>;
+
+      export interface ReorderInfo {
+        childIds: string[],
+      }
+
+      export const onChildrenReordered:
+          ChromeEvent<(id: string, reorderInfo: ReorderInfo) => void>;
+
+      export interface RemoveInfo {
+        index: number;
+        node: BookmarkTreeNode;
+        parentId: string;
+      }
+
+      export const onRemoved:
+          ChromeEvent<(id: string, removeInfo: RemoveInfo) => void>;
+
+      export interface MoveInfo {
+        index: number;
+        oldIndex: number;
+        oldParentId: string;
+        parentId: string;
+      }
+
+      export const onMoved:
+          ChromeEvent<(id: string, moveInfo: MoveInfo) => void>;
+
+      export const onImportEnded: ChromeEvent<() => void>;
+      export const onImportBegan: ChromeEvent<() => void>;
     }
-
-    export interface BookmarkTreeNode {
-      id: string;
-      parentId?: string;
-      index?: number;
-      url?: string;
-      title: string;
-      dateAdded?: number;
-      dateGroupModified?: number;
-      unmodifiable?: BookmarkTreeNodeUnmodifiable;
-      children?: BookmarkTreeNode[];
-    }
-
-    export interface CreateDetails {
-      parentId?: string;
-      index?: number;
-      title?: string;
-      url?: string;
-    }
-
-    export const MAX_WRITE_OPERATIONS_PER_HOUR: number;
-    export const MAX_SUSTAINED_WRITE_OPERATIONS_PER_MINUTE: number;
-
-    export function get(
-        idOrIdList: string|string[],
-        callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function getChildren(
-        id: string, callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function getRecent(
-        numberOfItems: number,
-        callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function getTree(callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function getSubTree(
-        id: string, callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function search(
-        query: string|{
-          query: string | undefined,
-          url: string|undefined,
-          title: string|undefined
-        },
-        callback: (p1: BookmarkTreeNode[]) => void): void;
-
-    export function create(
-        bookmark: CreateDetails,
-        callback?: (p1: BookmarkTreeNode) => void): void;
-
-    export function move(
-        id: string,
-        destination: {parentId: string|undefined, index: number|undefined},
-        callback?: (p1: BookmarkTreeNode) => void): void;
-
-    export function update(
-        id: string, changes: {title?: string, url?: string},
-        callback?: (p1: BookmarkTreeNode) => void): void;
-
-    export function remove(id: string, callback?: () => void): void;
-    export function removeTree(id: string, callback?: () => void): void;
-    function _import(callback?: () => void): void;
-    function _export(callback?: () => void): void;
-    export {_import as import}
-    export {_export as export}
-
-    // This is a workaround for the fact that importing other .d.ts files does
-    // not work. ChromeEvent is also used elsewhere so should be in its own
-    // file.
-    export interface ChromeEvent<ListenerType> {
-      addListener(listener: ListenerType): void;
-      removeListener(listener: ListenerType): void;
-    }
-
-    export const onCreated:
-        ChromeEvent<(id: string, bookmark: BookmarkTreeNode) => void>;
-
-    export interface ChangeInfo {
-      title: string,
-      url: string,
-    }
-
-    export const onChanged:
-        ChromeEvent<(id: string, changeInfo: ChangeInfo) => void>;
-
-    export interface ReorderInfo {
-      childIds: string[],
-    }
-
-    export const onChildrenReordered:
-        ChromeEvent<(id: string, reorderInfo: ReorderInfo) => void>;
-
-    export interface RemoveInfo {
-      index: number,
-      node: BookmarkTreeNode,
-      parentId: string,
-    }
-
-    export const onRemoved:
-        ChromeEvent<(id: string, removeInfo: RemoveInfo) => void>;
-
-    export interface MoveInfo {
-      index: number,
-      oldIndex: number,
-      oldParentId: string,
-      parentId: string,
-    }
-
-    export const onMoved: ChromeEvent<(id: string, moveInfo: MoveInfo) => void>;
-
-    export const onImportEnded: ChromeEvent<() => void>;
-    export const onImportBegan: ChromeEvent<() => void>;
   }
 }
