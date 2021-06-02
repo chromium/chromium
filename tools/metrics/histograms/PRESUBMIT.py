47ce74b26d0a18f2ad6cd0da19022f4c0394cf1c
Dan Harrington <harringtond@chromium.org>
2021-06-02 18:15:13 +0000

    Migrate PRESUBMIT.py to python3
    I tested the presubmit ran successfully when changing
    histograms.xml, enums.xml, and obsolete_histograms.xml files.
    
    Bug: 1212108
    Change-Id: I6b36d0acb054a59a733333058fef31ee59a2a2fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934363
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888528}

--- tools/metrics/histograms/PRESUBMIT.py
+++ tools/metrics/histograms/PRESUBMIT.py
@@ -7,6 +7,8 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into depot_tools.
 """
 
+USE_PYTHON3 = True
+
 
 def GetPrettyPrintErrors(input_api, output_api, cwd, rel_path, results):
   """Runs pretty-print command for specified file."""
