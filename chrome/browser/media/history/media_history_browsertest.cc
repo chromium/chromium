bd819fe67ee506fe5d0f6d583495efc521ca0bf6
Miyoung Shin <myid.shin@igalia.com>
2021-06-02 00:54:37 +0000

    Enable MediaHistoryForPrerenderBrowserTest.KeepRecordingMediaSession
    The test is no longer to load test_util.js that caused a flaky
    result, and this CL enables the TC.
    
    Bug: 1213333
    Change-Id: I70ef7709e2c3f31d0e28d3fd8d02ab16eaa22be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2925361
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#888234}

--- chrome/browser/media/history/media_history_browsertest.cc
+++ chrome/browser/media/history/media_history_browsertest.cc
@@ -1163,9 +1163,8 @@ class MediaHistoryForPrerenderBrowserTest : public MediaHistoryBrowserTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
-// Test is flaky: crbug.com/1213333.
 IN_PROC_BROWSER_TEST_F(MediaHistoryForPrerenderBrowserTest,
-                       DISABLED_KeepRecordingMediaSession) {
+                       KeepRecordingMediaSession) {
   // Start a page and wait for significant playback so we record watchtime.
   ASSERT_TRUE(SetupPageAndStartPlaying(browser(), GetTestURL()));
   EXPECT_TRUE(SetMediaMetadata(browser()));
