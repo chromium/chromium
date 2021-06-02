c99faec0b5678e3d1c7a3d48e925a8befeb61b76
Christopher Cameron <ccameron@chromium.org>
2021-06-02 17:05:01 +0000

    Add skia::BlitRGBAToYUVA
    Add a wrapper function that uses a SkColorFilter to apply the
    RGBA to YUVA conversion matrix, selecting out the appropriate
    planes.
    
    Leave the assumption of Rec601 baked in (because the code this is
    replacing has that assumption).
    
    Leave unimplemented (with a NOTREACHED) all formats except Y_UV,
    because they do not have tests coming yet.
    
    Bug: 1206168
    Change-Id: Ibe781233af17a645e18a92570b44b197d33e6d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2924216
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888505}

---
+++ skia/ext/rgba_to_yuva.cc
@@ -0,0 +1,81 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "skia/ext/rgba_to_yuva.h"
+
+#include "base/notreached.h"
+#include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkColorFilter.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/effects/SkColorMatrix.h"
+
+namespace skia {
+
+void BlitRGBAToYUVA(sk_sp<SkImage> src_image,
+                    const SkRect& src_rect,
+                    sk_sp<SkSurface> dst_surfaces[SkYUVAInfo::kMaxPlanes],
+                    const SkYUVAInfo& dst_yuva_info,
+                    const SkRect& dst_rect) {
+  // TODO(https://crbug.com/1206168): These color matrices are copied directly
+  // from kRGBtoYColorWeights et al in the gpu::GLHelperScaling::
+  // CreateI420Planerizer method this is code is replacing. This corresponds
+  // to hard-coding kRec601_Limited_SkYUVColorSpace. This should look up the
+  // matrix based on |dst_yuva_info|.
+  SkColorMatrix rgb_to_yuv(0.257f, 0.504f, 0.098f, 0.000f, 0.0625f,    //
+                           -0.148f, -0.291f, 0.439f, 0.000f, 0.5000f,  //
+                           0.439f, -0.368f, -0.071f, 0.000f, 0.5000f,  //
+                           0.000f, 0.000f, 0.000f, 0.000f, 1.0000f);
+
+  // Permutation matrices to select the appropriate YUVA channels for each
+  // output plane.
+  const SkColorMatrix xxxY(0, 0, 0, 0, 0,  //
+                           0, 0, 0, 0, 0,  //
+                           0, 0, 0, 0, 0,  //
+                           1, 0, 0, 0, 0);
+  const SkColorMatrix UVx1(0, 1, 0, 0, 0,  //
+                           0, 0, 1, 0, 0,  //
+                           0, 0, 0, 0, 0,  //
+                           0, 0, 0, 1, 0);
+
+  // Only Y_UV has been tested.
+  SkColorMatrix permutation_matrices[SkYUVAInfo::kMaxPlanes];
+  switch (dst_yuva_info.planeConfig()) {
+    case SkYUVAInfo::PlaneConfig::kY_UV:
+      permutation_matrices[0] = xxxY;
+      permutation_matrices[1] = UVx1;
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+
+  // Blit each plane.
+  for (int plane = 0; plane < dst_yuva_info.numPlanes(); ++plane) {
+    SkColorMatrix color_matrix = rgb_to_yuv;
+    color_matrix.postConcat(permutation_matrices[plane]);
+
+    SkSamplingOptions sampling_options(SkFilterMode::kLinear);
+
+    SkPaint paint;
+    paint.setColorFilter(SkColorFilters::Matrix(color_matrix));
+
+    float subsampling_factors[2] = {
+        static_cast<float>(dst_surfaces[plane]->width()) /
+            dst_yuva_info.dimensions().width(),
+        static_cast<float>(dst_surfaces[plane]->height()) /
+            dst_yuva_info.dimensions().height(),
+    };
+    SkRect plane_dst_rect =
+        SkRect::MakeXYWH(dst_rect.x() * subsampling_factors[0],
+                         dst_rect.y() * subsampling_factors[1],
+                         dst_rect.width() * subsampling_factors[0],
+                         dst_rect.height() * subsampling_factors[1]);
+
+    dst_surfaces[plane]->getCanvas()->drawImageRect(
+        src_image, src_rect, plane_dst_rect, sampling_options, &paint,
+        SkCanvas::kFast_SrcRectConstraint);
+  }
+}
+
+}  // namespace skia
---
+++ skia/ext/rgba_to_yuva.h
@@ -0,0 +1,26 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKIA_EXT_RGBA_TO_YUVA_H_
+#define SKIA_EXT_RGBA_TO_YUVA_H_
+
+#include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
+#include "third_party/skia/include/core/SkYUVAInfo.h"
+
+namespace skia {
+
+// Copy `src_image` from RGBA to the YUVA planes specified in `dst_surfaces`,
+// using the color space and plane configuration information specified in
+// `dst_yuva_info`.
+SK_API void BlitRGBAToYUVA(
+    sk_sp<SkImage> src_image,
+    const SkRect& src_rect,
+    sk_sp<SkSurface> dst_surfaces[SkYUVAInfo::kMaxPlanes],
+    const SkYUVAInfo& dst_yuva_info,
+    const SkRect& dst_rect);
+
+}  // namespace skia
+
+#endif  // SKIA_EXT_RGBA_TO_YUVA_H_
