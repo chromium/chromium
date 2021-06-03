77d4c336a8459cc1fcdfd059c2aad32f7708152c
Xiyuan Xia <xiyuan@google.com>
2021-06-03 05:41:03 +0000

    M91 "Move EncodeBitmap off UI thread for tabs.captureVisibleTab"
    > EncodeBitmap from tabs.captureVisibleTab causes janks on the
    > UI thread. This CL moves it to a worker thread.
    >
    > Bug: 1200553
    > Change-Id: I4763bdfe28d853af1b434e3ee19cbb3ae3552cfe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844958
    > Reviewed-by: Ben Wells <benwells@chromium.org>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#875168}
    
    (cherry picked from commit 8dac4883d00e6ea86b446734c0c0d9624134e08d)
    
    Bug: 1200553
    Change-Id: If4008d3024eccef3daf4215a598188c1a784b251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929463
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4472@{#1405}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/browser/extensions/api/tabs/tabs_api.h
+++ chrome/browser/extensions/api/tabs/tabs_api.h
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_refptr.h"
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 #include "chrome/common/extensions/api/tabs.h"
 #include "components/translate/core/browser/translate_driver.h"
@@ -24,6 +25,11 @@
 class GURL;
 class SkBitmap;
 class TabStripModel;
+
+namespace base {
+class TaskRunner;
+}
+
 namespace content {
 class WebContents;
 }
@@ -251,6 +257,11 @@ class TabsCaptureVisibleTabFunction
   bool ClientAllowsTransparency() override;
   void OnCaptureSuccess(const SkBitmap& bitmap) override;
   void OnCaptureFailure(CaptureResult result) override;
+
+  void EncodeBitmapOnWorkerThread(
+      scoped_refptr<base::TaskRunner> reply_task_runner,
+      const SkBitmap& bitmap);
+  void OnBitmapEncodedOnUIThread(bool success, std::string base64_result);
 
  private:
   DECLARE_EXTENSION_FUNCTION("tabs.captureVisibleTab", TABS_CAPTUREVISIBLETAB)
