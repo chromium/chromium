f61f1ef7292fda6aeadc6f02e7062e1368300370
Yi Gu <yigu@chromium.org>
2021-06-03 15:44:56 +0000

    [Sheriff] Disable InvokeUi_UninstallDialog_Cancel on LaCrOS
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=ExtensionsMenuViewBrowserTest.InvokeUi_UninstallDialog_Cancel&testType=browser_tests%20on%20Ubuntu-18.04
    
    Bug: 1173344
    Change-Id: I5caf889ae3508a387fd78d59ac6ce84c86255eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935924
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888872}

--- chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
@@ -752,7 +752,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   ShowAndVerifyUi();
 }
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
 // TODO(crbug.com/1173344): Flaky on Linux.
 #define MAYBE_InvokeUi_UninstallDialog_Cancel \
   DISABLED_InvokeUi_UninstallDialog_Cancel
