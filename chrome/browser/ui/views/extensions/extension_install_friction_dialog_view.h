500a93d2535a1f6c9eaea8dd278f5057e54afda0
Istiaque Ahmed <lazyboy@chromium.org>
2021-06-03 23:42:08 +0000

    [M91] Fix navigator access from ExtensionInstallFrictionDialogView
    This CL changes how PageNavigator is/was structured within
    ExtensionInstallFrictionDialogView: PageNavigator can go stale in
    certain circumstances (see bug) upon WebContents's destruction.
    ExtensionInstallFrictionDialogView now tracks the WC's lifetime so
    it can safely use it from OnLearnMoreLinkClicked() method.
    
    This CL also uses ScopedTabbedBrowserDisplayer from
    ExtensionInstallFrictionDialogView::OnLearnMoreLinkClicked when
    there is not WC available in it. This brings it into parity
    with ExtensionInstallDialogView::OnLinkClicked.
    
    This CL also adds a regression test for the bug:
    ExtensionInstallFrictionDialogViewTest.\
    TabClosureClearsWebContentsFromDialogView
    
    (cherry picked from commit 0486a5d1ced3701593746cd1e7428d5dc88c9086)
    
    Bug: 1201031
    Change-Id: I31a8aa3e3b92c73cd3ea236f75dcf0e4083d74c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909412
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#885217}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937290
    Cr-Commit-Position: refs/branch-heads/4472@{#1424}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/browser/ui/views/extensions/extension_install_friction_dialog_view.h
+++ chrome/browser/ui/views/extensions/extension_install_friction_dialog_view.h
@@ -12,12 +12,14 @@
 #include "ui/views/metadata/metadata_header_macros.h"
 
 namespace content {
-class PageNavigator;
+class WebContents;
 }
 
 namespace views {
 class StyledLabel;
 }
+
+class Profile;
 
 // Modal dialog shown to Enhanced Safe Browsing users before the extension
 // install dialog if the extension is not included in the Safe Browsing CRX
@@ -27,9 +29,9 @@ class ExtensionInstallFrictionDialogView
  public:
   METADATA_HEADER(ExtensionInstallFrictionDialogView);
 
-  // `navigator` is owned by the caller, `callback` will be invoked with `true
-  // if the user accepts or `false` if the user cancels.
-  ExtensionInstallFrictionDialogView(content::PageNavigator* navigator,
+  // `web_contents` ownership is not passed, `callback` will be invoked with
+  // `true` if the user accepts or `false` if the user cancels.
+  ExtensionInstallFrictionDialogView(content::WebContents* web_contents,
                                      base::OnceCallback<void(bool)> callback);
   ~ExtensionInstallFrictionDialogView() override;
   ExtensionInstallFrictionDialogView(
@@ -39,12 +41,24 @@ class ExtensionInstallFrictionDialogView
 
   gfx::ImageSkia GetWindowIcon() override;
 
+  // Returns the parent web contents for the dialog. Returns nullptr if the web
+  // contents have been destroyed.
+  content::WebContents* parent_web_contents() { return parent_web_contents_; }
+
+  void ClickLearnMoreLinkForTesting();
+
  private:
+  class WebContentsDestructionObserver;
+
   std::unique_ptr<views::StyledLabel> CreateWarningLabel();
   void OnLearnMoreLinkClicked();
 
-  content::PageNavigator* navigator_;
+  Profile* profile_ = nullptr;
+  content::WebContents* parent_web_contents_ = nullptr;
+  std::unique_ptr<WebContentsDestructionObserver>
+      web_contents_destruction_observer_;
   base::OnceCallback<void(bool)> callback_;
+
   bool accepted_ = false;
   bool learn_more_clicked_ = false;
 };
