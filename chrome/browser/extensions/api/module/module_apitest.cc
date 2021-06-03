f26c7e9707358211385f510d181dc8edbfdd355c
David Bertoni <dbertoni@chromium.org>
2021-06-03 00:57:34 +0000

    [Extensions] Add Service Worker tests for some chrome.extension APIs.
    This CL parameterizes the ExtensionModuleApiTest test fixture to
    run tests as both persistent background and Service Worker-based
    extensions.
    
    Bug: 1093066
    Change-Id: I0ca73987d73cfb77728ae2261a353928dded8758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934199
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888695}

--- chrome/browser/extensions/api/module/module_apitest.cc
+++ chrome/browser/extensions/api/module/module_apitest.cc
@@ -5,27 +5,58 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
 
-using ExtensionModuleApiTest = extensions::ExtensionApiTest;
+namespace extensions {
 
-IN_PROC_BROWSER_TEST_F(ExtensionModuleApiTest, CognitoFile) {
-  ASSERT_TRUE(RunExtensionTest("extension_module/cognito_file", {},
-                               {.allow_file_access = true}))
-      << message_;
-}
+namespace {
 
-IN_PROC_BROWSER_TEST_F(ExtensionModuleApiTest, IncognitoFile) {
+using ContextType = ExtensionBrowserTest::ContextType;
+
+class ExtensionModuleApiTest : public ExtensionApiTest,
+                               public testing::WithParamInterface<ContextType> {
+ public:
+  ExtensionModuleApiTest() = default;
+  ~ExtensionModuleApiTest() override = default;
+  ExtensionModuleApiTest(const ExtensionModuleApiTest&) = delete;
+  ExtensionModuleApiTest& operator=(const ExtensionModuleApiTest&) = delete;
+
+ protected:
+  bool RunTest(const char* name,
+               LoadOptions load_options = {}) WARN_UNUSED_RESULT {
+    load_options.load_as_service_worker =
+        GetParam() == ContextType::kServiceWorker;
+    return RunExtensionTest(name, {}, load_options);
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         ExtensionModuleApiTest,
+                         ::testing::Values(ContextType::kPersistentBackground));
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         ExtensionModuleApiTest,
+                         ::testing::Values(ContextType::kServiceWorker));
+
+}  // namespace
+
+IN_PROC_BROWSER_TEST_P(ExtensionModuleApiTest, CognitoFile) {
   ASSERT_TRUE(
-      RunExtensionTest("extension_module/incognito_file", {},
-                       {.allow_in_incognito = true, .allow_file_access = true}))
+      RunTest("extension_module/cognito_file", {.allow_file_access = true}))
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionModuleApiTest, CognitoNoFile) {
-  ASSERT_TRUE(RunExtensionTest("extension_module/cognito_nofile")) << message_;
-}
-
-IN_PROC_BROWSER_TEST_F(ExtensionModuleApiTest, IncognitoNoFile) {
-  ASSERT_TRUE(RunExtensionTest("extension_module/incognito_nofile", {},
-                               {.allow_in_incognito = true}))
+IN_PROC_BROWSER_TEST_P(ExtensionModuleApiTest, IncognitoFile) {
+  ASSERT_TRUE(RunTest("extension_module/incognito_file",
+                      {.allow_in_incognito = true, .allow_file_access = true}))
       << message_;
 }
+
+IN_PROC_BROWSER_TEST_P(ExtensionModuleApiTest, CognitoNoFile) {
+  ASSERT_TRUE(RunTest("extension_module/cognito_nofile")) << message_;
+}
+
+IN_PROC_BROWSER_TEST_P(ExtensionModuleApiTest, IncognitoNoFile) {
+  ASSERT_TRUE(RunTest("extension_module/incognito_nofile",
+                      {.allow_in_incognito = true}))
+      << message_;
+}
+
+}  // namespace extensions
