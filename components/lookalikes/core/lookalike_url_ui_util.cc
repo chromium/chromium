1fa1dae23d04b6a74a59d396d3c7fb0584d47123
Sylvain Defresne <sdefresne@chromium.org>
2021-06-03 18:59:18 +0000

    Convert //components/lookalikes to use base::Value
    The base::DictionaryValue class is deprecated in favor of simply
    using base::Value (and the API that internally check the stored
    value to be a dictionary).
    
    Bug: 1187061
    Change-Id: Id9798bc26286c34c83325c55149193191ce9e14c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2933956
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888954}

--- components/lookalikes/core/lookalike_url_ui_util.cc
+++ components/lookalikes/core/lookalike_url_ui_util.cc
@@ -41,19 +41,18 @@ void ReportUkmForLookalikeUrlBlockingPageIfNeeded(
   }
 }
 
-void PopulateLookalikeUrlBlockingPageStrings(
-    base::DictionaryValue* load_time_data,
-    const GURL& safe_url,
-    const GURL& request_url) {
+void PopulateLookalikeUrlBlockingPageStrings(base::Value* load_time_data,
+                                             const GURL& safe_url,
+                                             const GURL& request_url) {
   CHECK(load_time_data);
 
   PopulateStringsForSharedHTML(load_time_data);
-  load_time_data->SetString("tabTitle",
-                            l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_TITLE));
-  load_time_data->SetString(
+  load_time_data->SetStringKey(
+      "tabTitle", l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_TITLE));
+  load_time_data->SetStringKey(
       "optInLink",
       l10n_util::GetStringUTF16(IDS_SAFE_BROWSING_SCOUT_REPORTING_AGREE));
-  load_time_data->SetString(
+  load_time_data->SetStringKey(
       "enhancedProtectionMessage",
       l10n_util::GetStringUTF16(IDS_SAFE_BROWSING_ENHANCED_PROTECTION_MESSAGE));
 
@@ -61,60 +60,60 @@ void PopulateLookalikeUrlBlockingPageStrings(
     const std::u16string hostname =
         security_interstitials::common_string_util::GetFormattedHostName(
             safe_url);
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "heading",
         l10n_util::GetStringFUTF16(IDS_LOOKALIKE_URL_HEADING, hostname));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "primaryParagraph",
         l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_PRIMARY_PARAGRAPH));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "proceedButtonText",
         l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_IGNORE));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "primaryButtonText",
         l10n_util::GetStringFUTF16(IDS_LOOKALIKE_URL_CONTINUE, hostname));
   } else {
     // No safe URL available to suggest. This can happen when the navigated
     // domain fails IDN spoof checks but isn't a lookalike of a known domain.
     // TODO: Change to actual strings.
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "heading",
         l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_HEADING_NO_SUGGESTED_URL));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "primaryParagraph",
         l10n_util::GetStringUTF16(
             IDS_LOOKALIKE_URL_PRIMARY_PARAGRAPH_NO_SUGGESTED_URL));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "proceedButtonText",
         l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_IGNORE));
-    load_time_data->SetString(
+    load_time_data->SetStringKey(
         "primaryButtonText",
         l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_BACK_TO_SAFETY));
 #if defined(OS_IOS)
     // On iOS, offer to close the page instead of navigating to NTP when the
     // safe URL is empty or invalid, and unable to go back.
-    bool show_close_page = false;
-    load_time_data->GetBoolean("cant_go_back", &show_close_page);
-    if (show_close_page) {
-      load_time_data->SetString(
+    absl::optional<bool> maybe_cant_go_back =
+        load_time_data->FindBoolKey("cant_go_back");
+    if (maybe_cant_go_back && *maybe_cant_go_back) {
+      load_time_data->SetStringKey(
           "primaryButtonText",
           l10n_util::GetStringUTF16(IDS_LOOKALIKE_URL_CLOSE_PAGE));
     }
 #endif
   }
-  load_time_data->SetString("lookalikeRequestHostname", request_url.host());
+  load_time_data->SetStringKey("lookalikeRequestHostname", request_url.host());
 }
 
-void PopulateStringsForSharedHTML(base::DictionaryValue* load_time_data) {
-  load_time_data->SetBoolean("lookalike_url", true);
-  load_time_data->SetBoolean("overridable", false);
-  load_time_data->SetBoolean("hide_primary_button", false);
-  load_time_data->SetBoolean("show_recurrent_error_paragraph", false);
+void PopulateStringsForSharedHTML(base::Value* load_time_data) {
+  load_time_data->SetBoolKey("lookalike_url", true);
+  load_time_data->SetBoolKey("overridable", false);
+  load_time_data->SetBoolKey("hide_primary_button", false);
+  load_time_data->SetBoolKey("show_recurrent_error_paragraph", false);
 
-  load_time_data->SetString("recurrentErrorParagraph", "");
-  load_time_data->SetString("openDetails", "");
-  load_time_data->SetString("explanationParagraph", "");
-  load_time_data->SetString("finalParagraph", "");
+  load_time_data->SetStringKey("recurrentErrorParagraph", "");
+  load_time_data->SetStringKey("openDetails", "");
+  load_time_data->SetStringKey("explanationParagraph", "");
+  load_time_data->SetStringKey("finalParagraph", "");
 
-  load_time_data->SetString("type", "LOOKALIKE");
+  load_time_data->SetStringKey("type", "LOOKALIKE");
 }
--- components/lookalikes/core/lookalike_url_ui_util.h
+++ components/lookalikes/core/lookalike_url_ui_util.h
@@ -9,7 +9,7 @@
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace base {
-class DictionaryValue;
+class Value;
 }  // namespace base
 
 // Allow easier reporting of UKM when no interstitial is shown.
@@ -27,12 +27,11 @@ void ReportUkmForLookalikeUrlBlockingPageIfNeeded(
     bool triggered_by_initial_url);
 
 // Populates |load_time_data| for interstitial HTML.
-void PopulateLookalikeUrlBlockingPageStrings(
-    base::DictionaryValue* load_time_data,
-    const GURL& safe_url,
-    const GURL& request_url);
+void PopulateLookalikeUrlBlockingPageStrings(base::Value* load_time_data,
+                                             const GURL& safe_url,
+                                             const GURL& request_url);
 
 // Values added to get shared interstitial HTML to play nice.
-void PopulateStringsForSharedHTML(base::DictionaryValue* load_time_data);
+void PopulateStringsForSharedHTML(base::Value* load_time_data);
 
 #endif  // COMPONENTS_LOOKALIKES_CORE_LOOKALIKE_URL_UI_UTIL_H_
