708b2835556d1a7df06df857f2830e95d25964eb
Sky Malice <skym@chromium.org>
2021-06-04 21:47:19 +0000

    Initial material color roles.
    This sets the color roles that material widgets expect to have the
    existing modern color values. This should facilitate migrating widgets
    without causing any visual effect.
    
    Bug: 1210689
    Change-Id: I0ea4f326cb90075fab069103a32b939c26ef2ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927993
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#889470}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
@@ -17,8 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.base.SplitCompatUtils;
-import org.chromium.chrome.browser.flags.CachedFeatureFlags;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.language.GlobalAppLocaleController;
 import org.chromium.chrome.browser.night_mode.GlobalNightModeStateProviderHolder;
 import org.chromium.chrome.browser.night_mode.NightModeStateProvider;
@@ -54,9 +52,7 @@ public class ChromeBaseAppCompatActivity
 
         initializeNightModeStateProvider();
         mNightModeStateProvider.addObserver(this);
-        if (CachedFeatureFlags.isEnabled(ChromeFeatureList.THEME_REFACTOR_ANDROID)) {
-            setTheme(R.style.ThemeRefactorAppThemeOverlay);
-        }
+        setTheme(R.style.BaseColorOverlay);
         super.onCreate(savedInstanceState);
 
         // Activity level locale overrides must be done in onCreate.
