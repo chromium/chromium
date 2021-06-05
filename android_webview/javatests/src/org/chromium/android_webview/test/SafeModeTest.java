f32564536b467c5d10ffea2c909488391de29491
Nate Fischer <ntfschr@chromium.org>
2021-06-05 00:27:40 +0000

    AW SafeMode: introduce SafeModeAction framework
    No change to production logic. This introduces a framework for
    registering and executing SafeModeActions.
    
    This adds some initial tests for this functionality but does not yet
    wire this up in production code paths. This will be integrated into
    production once more of SafeMode has been implemented.
    
    Bug: 1215366
    Test: run_webview_instrumentation_test_apk -f SafeModeTest.*
    Change-Id: I03d7cc3df4ef2f999f1efb0574ed5b48e7c944db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928399
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889526}

--- android_webview/javatests/src/org/chromium/android_webview/test/SafeModeTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeModeTest.java
@@ -9,22 +9,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import androidx.annotation.NonNull;
 import androidx.test.filters.MediumTest;
+import androidx.test.filters.SmallTest;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.android_webview.common.SafeModeAction;
 import org.chromium.android_webview.common.SafeModeController;
 import org.chromium.android_webview.common.services.ISafeModeService;
 import org.chromium.android_webview.services.SafeModeService;
 import org.chromium.android_webview.test.services.ServiceConnectionHelper;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.Feature;
+import org.chromium.build.BuildConfig;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Test WebView SafeMode.
@@ -35,8 +44,15 @@ public class SafeModeTest {
     // provider.
     public static final String TEST_WEBVIEW_PACKAGE_NAME = "org.chromium.android_webview.shell";
 
+    private AtomicInteger mTestSafeModeActionExecutionCounter;
+
     @Rule
     public AwActivityTestRule mActivityTestRule = new AwActivityTestRule();
+
+    @Before
+    public void setUp() throws Throwable {
+        mTestSafeModeActionExecutionCounter = new AtomicInteger(0);
+    }
 
     @After
     public void tearDown() throws Throwable {
@@ -46,6 +62,8 @@ public class SafeModeTest {
                 TEST_WEBVIEW_PACKAGE_NAME, SafeModeController.SAFE_MODE_STATE_COMPONENT);
         context.getPackageManager().setComponentEnabledSetting(safeModeComponent,
                 PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+
+        SafeModeController.getInstance().unregisterActionsForTesting();
     }
 
     @Test
@@ -129,5 +147,170 @@ public class SafeModeTest {
 
         Assert.assertFalse("SafeMode should stay disabled because package name is wrong",
                 SafeModeController.getInstance().isSafeModeEnabled(TEST_WEBVIEW_PACKAGE_NAME));
+    }
+
+    private class TestSafeModeAction implements SafeModeAction {
+        private int mCallCount;
+        private int mExecutionOrder;
+        private final String mId;
+
+        TestSafeModeAction(String id) {
+            mId = id;
+        }
+
+        @Override
+        @NonNull
+        public String getId() {
+            return mId;
+        }
+
+        @Override
+        public void execute() {
+            mCallCount++;
+            mExecutionOrder = mTestSafeModeActionExecutionCounter.incrementAndGet();
+        }
+
+        public int getCallCount() {
+            return mCallCount;
+        }
+
+        public int getExecutionOrder() {
+            return mExecutionOrder;
+        }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_cannotRegisterActionsTwice() throws Throwable {
+        TestSafeModeAction testAction1 = new TestSafeModeAction("test1");
+        TestSafeModeAction testAction2 = new TestSafeModeAction("test2");
+        SafeModeController.getInstance().registerActions(new SafeModeAction[] {testAction1});
+        try {
+            SafeModeController.getInstance().registerActions(new SafeModeAction[] {testAction2});
+            Assert.fail("SafeModeController should have thrown an exception when "
+                    + "re-registering actions");
+        } catch (IllegalStateException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_cannotRegisterDuplicateActionId() throws Throwable {
+        Assume.assumeTrue("This behavior is only in debug builds for performance reasons",
+                BuildConfig.ENABLE_ASSERTS);
+        TestSafeModeAction testAction1 = new TestSafeModeAction("test1");
+        TestSafeModeAction testAction2 = new TestSafeModeAction("test1");
+        try {
+            SafeModeController.getInstance().registerActions(
+                    new SafeModeAction[] {testAction1, testAction2});
+            Assert.fail("SafeModeController should have thrown an exception for "
+                    + "a duplicate ID");
+        } catch (IllegalArgumentException e) {
+            // Expected
+        }
+    }
+
+    private static <T> Set<T> asSet(T... values) {
+        Set<T> set = new HashSet<>();
+        for (T value : values) {
+            set.add(value);
+        }
+        return set;
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_mustRegisterBeforeExecuting() throws Throwable {
+        try {
+            Set<String> actions = asSet("test");
+            SafeModeController.getInstance().executeActions(actions);
+            Assert.fail("SafeModeController should have thrown an exception when "
+                    + "executing without registering");
+        } catch (IllegalStateException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_executesRegisteredAction() throws Throwable {
+        TestSafeModeAction testAction = new TestSafeModeAction("test");
+        SafeModeController.getInstance().registerActions(new SafeModeAction[] {testAction});
+
+        Set<String> actions = asSet("test");
+        SafeModeController.getInstance().executeActions(actions);
+        Assert.assertEquals("TestSafeModeAction should have been executed exactly 1 time", 1,
+                testAction.getCallCount());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_doesNotExecuteUnregisteredActions() throws Throwable {
+        TestSafeModeAction testAction = new TestSafeModeAction("test");
+        SafeModeController.getInstance().registerActions(new SafeModeAction[] {testAction});
+
+        Set<String> actions = asSet("test", "unregistered1", "unregistered2");
+        SafeModeController.getInstance().executeActions(actions);
+        Assert.assertEquals("TestSafeModeAction should have been executed exactly 1 time", 1,
+                testAction.getCallCount());
+        // If we got this far without crashing, we assume SafeModeController correctly ignored the
+        // unregistered actions.
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_onlyExecutesSpecifiedActions() throws Throwable {
+        TestSafeModeAction testAction1 = new TestSafeModeAction("test1");
+        TestSafeModeAction testAction2 = new TestSafeModeAction("test2");
+        SafeModeController.getInstance().registerActions(
+                new SafeModeAction[] {testAction1, testAction2});
+
+        Set<String> actions = asSet("test1");
+        SafeModeController.getInstance().executeActions(actions);
+        Assert.assertEquals("testAction1 should have been executed exactly 1 time", 1,
+                testAction1.getCallCount());
+        Assert.assertEquals(
+                "testAction2 should not have been executed", 0, testAction2.getCallCount());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testSafeModeAction_executesActionsInOrder() throws Throwable {
+        TestSafeModeAction testAction1 = new TestSafeModeAction("test1");
+        TestSafeModeAction testAction2 = new TestSafeModeAction("test2");
+        TestSafeModeAction testAction3 = new TestSafeModeAction("test3");
+
+        Set<String> actions = asSet(testAction1.getId(), testAction2.getId(), testAction3.getId());
+
+        SafeModeController.getInstance().registerActions(
+                new SafeModeAction[] {testAction1, testAction2, testAction3});
+        SafeModeController.getInstance().executeActions(actions);
+        Assert.assertEquals(
+                "testAction1 should be executed first", 1, testAction1.getExecutionOrder());
+        Assert.assertEquals(
+                "testAction2 should be executed second", 2, testAction2.getExecutionOrder());
+        Assert.assertEquals(
+                "testAction3 should be executed third", 3, testAction3.getExecutionOrder());
+
+        // Unregister and re-register in the opposite order. Verify that they're executed in the new
+        // registration order.
+        SafeModeController.getInstance().unregisterActionsForTesting();
+        SafeModeController.getInstance().registerActions(
+                new SafeModeAction[] {testAction3, testAction2, testAction1});
+        SafeModeController.getInstance().executeActions(actions);
+        Assert.assertEquals("testAction3 should be executed first the next time", 4,
+                testAction3.getExecutionOrder());
+        Assert.assertEquals("testAction2 should be executed second the next time", 5,
+                testAction2.getExecutionOrder());
+        Assert.assertEquals("testAction1 should be executed third the next time", 6,
+                testAction1.getExecutionOrder());
     }
 }
