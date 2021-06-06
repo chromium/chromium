3fad3c290497c88fdc11a4b69022c9911a3ae07b
Yuki Shiino <yukishiino@chromium.org>
2021-06-06 17:47:39 +0000

    bind-gen: Refactor blink::DictionaryTest
    As preparation of migration to the new IDL dictionary
    implementation, refactor blink::DictionaryTest so that it works
    with both the old and new IDL dictionary implementations.
    
    Bug: 839389
    Change-Id: I0830447ebeef8b445b1f2d54dcb04e17c585f2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939533
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889617}

--- third_party/blink/renderer/core/testing/dictionary_test.cc
+++ third_party/blink/renderer/core/testing/dictionary_test.cc
@@ -4,264 +4,167 @@
 
 #include "third_party/blink/renderer/core/testing/dictionary_test.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/script_iterator.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_internal_dictionary.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_internal_dictionary_derived.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_internal_dictionary_derived_derived.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_object_builder.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/platform/bindings/script_state.h"
-#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
 
-DictionaryTest::DictionaryTest() : required_boolean_member_(false) {}
+DictionaryTest::DictionaryTest() = default;
 
 DictionaryTest::~DictionaryTest() = default;
 
-void DictionaryTest::set(const InternalDictionary* testing_dictionary) {
-  Reset();
-  if (testing_dictionary->hasLongMember())
-    long_member_ = testing_dictionary->longMember();
-  if (testing_dictionary->hasLongMemberWithClamp())
-    long_member_with_clamp_ = testing_dictionary->longMemberWithClamp();
-  if (testing_dictionary->hasLongMemberWithEnforceRange()) {
-    long_member_with_enforce_range_ =
-        testing_dictionary->longMemberWithEnforceRange();
+#define SAVE_DICT_MEMBER(camel, capital)                  \
+  if (input_dictionary->has##capital()) {                 \
+    dictionary_->set##capital(input_dictionary->camel()); \
+  } else {                                                \
   }
-  long_member_with_default_ = testing_dictionary->longMemberWithDefault();
-  if (testing_dictionary->hasLongOrNullMember())
-    long_or_null_member_ = testing_dictionary->longOrNullMember();
-  if (testing_dictionary->hasLongOrNullMemberWithDefault()) {
-    long_or_null_member_with_default_ =
-        testing_dictionary->longOrNullMemberWithDefault();
-  }
-  if (testing_dictionary->hasBooleanMember())
-    boolean_member_ = testing_dictionary->booleanMember();
-  if (testing_dictionary->hasDoubleMember())
-    double_member_ = testing_dictionary->doubleMember();
-  if (testing_dictionary->hasUnrestrictedDoubleMember()) {
-    unrestricted_double_member_ =
-        testing_dictionary->unrestrictedDoubleMember();
-  }
-  if (testing_dictionary->hasStringMember())
-    string_member_ = testing_dictionary->stringMember();
-  string_member_with_default_ = testing_dictionary->stringMemberWithDefault();
-  if (testing_dictionary->hasByteStringMember())
-    byte_string_member_ = testing_dictionary->byteStringMember();
-  if (testing_dictionary->hasUsvStringMember())
-    usv_string_member_ = testing_dictionary->usvStringMember();
-  if (testing_dictionary->hasStringSequenceMember())
-    string_sequence_member_ = testing_dictionary->stringSequenceMember();
-  string_sequence_member_with_default_ =
-      testing_dictionary->stringSequenceMemberWithDefault();
-  if (testing_dictionary->hasStringSequenceOrNullMember()) {
-    string_sequence_or_null_member_ =
-        testing_dictionary->stringSequenceOrNullMember();
-  }
-  if (testing_dictionary->hasEnumMember())
-    enum_member_ = testing_dictionary->enumMember();
-  enum_member_with_default_ = testing_dictionary->enumMemberWithDefault();
-  if (testing_dictionary->hasEnumOrNullMember())
-    enum_or_null_member_ = testing_dictionary->enumOrNullMember();
-  if (testing_dictionary->hasElementMember())
-    element_member_ = testing_dictionary->elementMember();
-  if (testing_dictionary->hasElementOrNullMember()) {
-    element_or_null_member_ = testing_dictionary->elementOrNullMember();
-    has_element_or_null_member_ = true;
-  }
-  if (testing_dictionary->hasObjectMember())
-    object_member_ = testing_dictionary->objectMember();
-  object_or_null_member_with_default_ =
-      testing_dictionary->objectOrNullMemberWithDefault();
-  if (testing_dictionary->hasDoubleOrStringMember())
-    double_or_string_member_ = testing_dictionary->doubleOrStringMember();
-  if (testing_dictionary->hasDoubleOrStringSequenceMember()) {
-    double_or_string_sequence_or_null_member_ =
-        MakeGarbageCollected<HeapVector<DoubleOrString>>();
-    *double_or_string_sequence_or_null_member_ =
-        testing_dictionary->doubleOrStringSequenceMember();
-  }
-  // eventTargetOrNullMember has a default null value.
-  event_target_or_null_member_ = testing_dictionary->eventTargetOrNullMember();
-  if (testing_dictionary->hasInternalEnumOrInternalEnumSequenceMember()) {
-    internal_enum_or_internal_enum_sequence_ =
-        testing_dictionary->internalEnumOrInternalEnumSequenceMember();
-  }
-  if (testing_dictionary->hasAnyMember())
-    any_member_ = testing_dictionary->anyMember();
-  if (testing_dictionary->hasCallbackFunctionMember())
-    callback_function_member_ = testing_dictionary->callbackFunctionMember();
+
+void DictionaryTest::set(v8::Isolate* isolate,
+                         const InternalDictionary* input_dictionary) {
+  dictionary_ = InternalDictionaryDerivedDerived::Create(isolate);
+
+  SAVE_DICT_MEMBER(longMember, LongMember);
+  SAVE_DICT_MEMBER(longMemberWithClamp, LongMemberWithClamp);
+  SAVE_DICT_MEMBER(longMemberWithEnforceRange, LongMemberWithEnforceRange);
+  SAVE_DICT_MEMBER(longMemberWithDefault, LongMemberWithDefault);
+  SAVE_DICT_MEMBER(longOrNullMember, LongOrNullMember);
+  SAVE_DICT_MEMBER(longOrNullMemberWithDefault, LongOrNullMemberWithDefault);
+  SAVE_DICT_MEMBER(booleanMember, BooleanMember);
+  SAVE_DICT_MEMBER(doubleMember, DoubleMember);
+  SAVE_DICT_MEMBER(unrestrictedDoubleMember, UnrestrictedDoubleMember);
+  SAVE_DICT_MEMBER(stringMember, StringMember);
+  SAVE_DICT_MEMBER(stringMemberWithDefault, StringMemberWithDefault);
+  SAVE_DICT_MEMBER(byteStringMember, ByteStringMember);
+  SAVE_DICT_MEMBER(usvStringMember, UsvStringMember);
+  SAVE_DICT_MEMBER(stringSequenceMember, StringSequenceMember);
+  SAVE_DICT_MEMBER(stringSequenceMemberWithDefault,
+                   StringSequenceMemberWithDefault);
+  SAVE_DICT_MEMBER(stringSequenceOrNullMember, StringSequenceOrNullMember);
+  SAVE_DICT_MEMBER(enumMember, EnumMember);
+  SAVE_DICT_MEMBER(enumMemberWithDefault, EnumMemberWithDefault);
+  SAVE_DICT_MEMBER(enumOrNullMember, EnumOrNullMember);
+  SAVE_DICT_MEMBER(elementMember, ElementMember);
+  SAVE_DICT_MEMBER(elementOrNullMember, ElementOrNullMember);
+  SAVE_DICT_MEMBER(objectMember, ObjectMember);
+  SAVE_DICT_MEMBER(objectOrNullMemberWithDefault,
+                   ObjectOrNullMemberWithDefault);
+  SAVE_DICT_MEMBER(doubleOrStringMember, DoubleOrStringMember);
+  SAVE_DICT_MEMBER(doubleOrStringSequenceMember, DoubleOrStringSequenceMember);
+  SAVE_DICT_MEMBER(eventTargetOrNullMember, EventTargetOrNullMember);
+  SAVE_DICT_MEMBER(internalEnumOrInternalEnumSequenceMember,
+                   InternalEnumOrInternalEnumSequenceMember);
+  SAVE_DICT_MEMBER(anyMember, AnyMember);
+  SAVE_DICT_MEMBER(callbackFunctionMember, CallbackFunctionMember);
 }
 
 InternalDictionary* DictionaryTest::get(v8::Isolate* isolate) {
-  InternalDictionary* result = InternalDictionary::Create(isolate);
-  GetInternals(result);
-  return result;
+  InternalDictionary* dictionary = InternalDictionary::Create(isolate);
+  RestoreInternalDictionary(dictionary);
+  return dictionary;
 }
 
-void DictionaryTest::setDerived(const InternalDictionaryDerived* derived) {
-  DCHECK(derived->hasRequiredBooleanMember());
-  set(derived);
-  if (derived->hasDerivedStringMember())
-    derived_string_member_ = derived->derivedStringMember();
-  derived_string_member_with_default_ =
-      derived->derivedStringMemberWithDefault();
-  required_boolean_member_ = derived->requiredBooleanMember();
+void DictionaryTest::setDerived(
+    v8::Isolate* isolate,
+    const InternalDictionaryDerived* input_dictionary) {
+  set(isolate, input_dictionary);
+
+  SAVE_DICT_MEMBER(derivedStringMember, DerivedStringMember);
+  SAVE_DICT_MEMBER(derivedStringMemberWithDefault,
+                   DerivedStringMemberWithDefault);
+  SAVE_DICT_MEMBER(requiredBooleanMember, RequiredBooleanMember);
 }
 
 InternalDictionaryDerived* DictionaryTest::getDerived(v8::Isolate* isolate) {
-  InternalDictionaryDerived* result =
+  InternalDictionaryDerived* dictionary =
       InternalDictionaryDerived::Create(isolate);
-  GetDerivedInternals(result);
-  return result;
+  RestoreInternalDictionaryDerived(dictionary);
+  return dictionary;
 }
 
 void DictionaryTest::setDerivedDerived(
-    const InternalDictionaryDerivedDerived* derived) {
-  setDerived(derived);
-  if (derived->hasDerivedDerivedStringMember())
-    derived_derived_string_member_ = derived->derivedDerivedStringMember();
+    v8::Isolate* isolate,
+    const InternalDictionaryDerivedDerived* input_dictionary) {
+  setDerived(isolate, input_dictionary);
+
+  SAVE_DICT_MEMBER(derivedDerivedStringMember, DerivedDerivedStringMember);
 }
 
 InternalDictionaryDerivedDerived* DictionaryTest::getDerivedDerived(
     v8::Isolate* isolate) {
-  InternalDictionaryDerivedDerived* result =
+  InternalDictionaryDerivedDerived* dictionary =
       InternalDictionaryDerivedDerived::Create(isolate);
-  GetDerivedDerivedInternals(result);
-  return result;
+  RestoreInternalDictionaryDerivedDerived(dictionary);
+  return dictionary;
 }
 
-void DictionaryTest::Reset() {
-  long_member_ = absl::nullopt;
-  long_member_with_clamp_ = absl::nullopt;
-  long_member_with_enforce_range_ = absl::nullopt;
-  long_member_with_default_ = -1;  // This value should not be returned.
-  long_or_null_member_ = absl::nullopt;
-  long_or_null_member_with_default_ = absl::nullopt;
-  boolean_member_ = absl::nullopt;
-  double_member_ = absl::nullopt;
-  unrestricted_double_member_ = absl::nullopt;
-  string_member_ = absl::nullopt;
-  string_member_with_default_ = String("Should not be returned");
-  byte_string_member_ = absl::nullopt;
-  usv_string_member_ = absl::nullopt;
-  string_sequence_member_ = absl::nullopt;
-  string_sequence_member_with_default_.Fill("Should not be returned", 1);
-  string_sequence_or_null_member_ = absl::nullopt;
-  enum_member_ = absl::nullopt;
-  enum_member_with_default_ = String();
-  enum_or_null_member_ = absl::nullopt;
-  element_member_ = nullptr;
-  element_or_null_member_.Clear();
-  has_element_or_null_member_ = false;
-  object_member_ = ScriptValue();
-  object_or_null_member_with_default_ = ScriptValue();
-  double_or_string_member_ = DoubleOrString();
-  double_or_string_sequence_or_null_member_ = nullptr;
-  event_target_or_null_member_ = nullptr;
-  derived_string_member_ = absl::nullopt;
-  derived_string_member_with_default_ = String();
-  derived_derived_string_member_ = absl::nullopt;
-  required_boolean_member_ = false;
-  dictionary_member_properties_ = absl::nullopt;
-  internal_enum_or_internal_enum_sequence_ =
-      InternalEnumOrInternalEnumSequence();
-  any_member_ = ScriptValue();
-  callback_function_member_ = nullptr;
-}
-
-void DictionaryTest::GetInternals(InternalDictionary* dict) {
-  DCHECK(dict);
-
-  if (long_member_)
-    dict->setLongMember(long_member_.value());
-  if (long_member_with_clamp_)
-    dict->setLongMemberWithClamp(long_member_with_clamp_.value());
-  if (long_member_with_enforce_range_) {
-    dict->setLongMemberWithEnforceRange(
-        long_member_with_enforce_range_.value());
-  }
-  dict->setLongMemberWithDefault(long_member_with_default_);
-  if (long_or_null_member_)
-    dict->setLongOrNullMember(long_or_null_member_.value());
-  if (long_or_null_member_with_default_) {
-    dict->setLongOrNullMemberWithDefault(
-        long_or_null_member_with_default_.value());
-  }
-  if (boolean_member_)
-    dict->setBooleanMember(boolean_member_.value());
-  if (double_member_)
-    dict->setDoubleMember(double_member_.value());
-  if (unrestricted_double_member_)
-    dict->setUnrestrictedDoubleMember(unrestricted_double_member_.value());
-  if (string_member_)
-    dict->setStringMember(string_member_.value());
-  dict->setStringMemberWithDefault(string_member_with_default_);
-  if (byte_string_member_)
-    dict->setByteStringMember(byte_string_member_.value());
-  if (usv_string_member_)
-    dict->setUsvStringMember(usv_string_member_.value());
-  if (string_sequence_member_)
-    dict->setStringSequenceMember(string_sequence_member_.value());
-  dict->setStringSequenceMemberWithDefault(
-      string_sequence_member_with_default_);
-  if (string_sequence_or_null_member_) {
-    dict->setStringSequenceOrNullMember(
-        string_sequence_or_null_member_.value());
-  }
-  if (enum_member_)
-    dict->setEnumMember(enum_member_.value());
-  dict->setEnumMemberWithDefault(enum_member_with_default_);
-  if (enum_or_null_member_)
-    dict->setEnumOrNullMember(enum_or_null_member_.value());
-  if (element_member_)
-    dict->setElementMember(element_member_);
-  if (has_element_or_null_member_)
-    dict->setElementOrNullMember(element_or_null_member_);
-  dict->setObjectMember(object_member_);
-  dict->setObjectOrNullMemberWithDefault(object_or_null_member_with_default_);
-  if (!double_or_string_member_.IsNull())
-    dict->setDoubleOrStringMember(double_or_string_member_);
-  if (double_or_string_sequence_or_null_member_) {
-    dict->setDoubleOrStringSequenceMember(
-        *double_or_string_sequence_or_null_member_);
-  }
-  dict->setEventTargetOrNullMember(event_target_or_null_member_);
-  dict->setInternalEnumOrInternalEnumSequenceMember(
-      internal_enum_or_internal_enum_sequence_);
-  dict->setAnyMember(any_member_);
-  if (callback_function_member_)
-    dict->setCallbackFunctionMember(callback_function_member_);
-}
-
-void DictionaryTest::GetDerivedInternals(InternalDictionaryDerived* dict) {
-  GetInternals(dict);
-
-  if (derived_string_member_)
-    dict->setDerivedStringMember(derived_string_member_.value());
-  dict->setDerivedStringMemberWithDefault(derived_string_member_with_default_);
-  dict->setRequiredBooleanMember(required_boolean_member_);
-}
-
-void DictionaryTest::GetDerivedDerivedInternals(
-    InternalDictionaryDerivedDerived* dict) {
-  GetDerivedInternals(dict);
-
-  if (derived_derived_string_member_)
-    dict->setDerivedDerivedStringMember(derived_derived_string_member_.value());
-}
+#undef SAVE_DICT_MEMBER
 
 void DictionaryTest::Trace(Visitor* visitor) const {
-  visitor->Trace(element_member_);
-  visitor->Trace(element_or_null_member_);
-  visitor->Trace(object_member_);
-  visitor->Trace(object_or_null_member_with_default_);
-  visitor->Trace(double_or_string_sequence_or_null_member_);
-  visitor->Trace(event_target_or_null_member_);
-  visitor->Trace(any_member_);
-  visitor->Trace(callback_function_member_);
   ScriptWrappable::Trace(visitor);
+  visitor->Trace(dictionary_);
 }
+
+#define RESTORE_DICT_MEMBER(camel, capital)                \
+  if (dictionary_->has##capital()) {                       \
+    output_dictionary->set##capital(dictionary_->camel()); \
+  } else {                                                 \
+  }
+
+void DictionaryTest::RestoreInternalDictionary(
+    InternalDictionary* output_dictionary) {
+  RESTORE_DICT_MEMBER(longMember, LongMember);
+  RESTORE_DICT_MEMBER(longMemberWithClamp, LongMemberWithClamp);
+  RESTORE_DICT_MEMBER(longMemberWithEnforceRange, LongMemberWithEnforceRange);
+  RESTORE_DICT_MEMBER(longMemberWithDefault, LongMemberWithDefault);
+  RESTORE_DICT_MEMBER(longOrNullMember, LongOrNullMember);
+  RESTORE_DICT_MEMBER(longOrNullMemberWithDefault, LongOrNullMemberWithDefault);
+  RESTORE_DICT_MEMBER(booleanMember, BooleanMember);
+  RESTORE_DICT_MEMBER(doubleMember, DoubleMember);
+  RESTORE_DICT_MEMBER(unrestrictedDoubleMember, UnrestrictedDoubleMember);
+  RESTORE_DICT_MEMBER(stringMember, StringMember);
+  RESTORE_DICT_MEMBER(stringMemberWithDefault, StringMemberWithDefault);
+  RESTORE_DICT_MEMBER(byteStringMember, ByteStringMember);
+  RESTORE_DICT_MEMBER(usvStringMember, UsvStringMember);
+  RESTORE_DICT_MEMBER(stringSequenceMember, StringSequenceMember);
+  RESTORE_DICT_MEMBER(stringSequenceMemberWithDefault,
+                      StringSequenceMemberWithDefault);
+  RESTORE_DICT_MEMBER(stringSequenceOrNullMember, StringSequenceOrNullMember);
+  RESTORE_DICT_MEMBER(enumMember, EnumMember);
+  RESTORE_DICT_MEMBER(enumMemberWithDefault, EnumMemberWithDefault);
+  RESTORE_DICT_MEMBER(enumOrNullMember, EnumOrNullMember);
+  RESTORE_DICT_MEMBER(elementMember, ElementMember);
+  RESTORE_DICT_MEMBER(elementOrNullMember, ElementOrNullMember);
+  RESTORE_DICT_MEMBER(objectMember, ObjectMember);
+  RESTORE_DICT_MEMBER(objectOrNullMemberWithDefault,
+                      ObjectOrNullMemberWithDefault);
+  RESTORE_DICT_MEMBER(doubleOrStringMember, DoubleOrStringMember);
+  RESTORE_DICT_MEMBER(doubleOrStringSequenceMember,
+                      DoubleOrStringSequenceMember);
+  RESTORE_DICT_MEMBER(eventTargetOrNullMember, EventTargetOrNullMember);
+  RESTORE_DICT_MEMBER(internalEnumOrInternalEnumSequenceMember,
+                      InternalEnumOrInternalEnumSequenceMember);
+  RESTORE_DICT_MEMBER(anyMember, AnyMember);
+  RESTORE_DICT_MEMBER(callbackFunctionMember, CallbackFunctionMember);
+}
+
+void DictionaryTest::RestoreInternalDictionaryDerived(
+    InternalDictionaryDerived* output_dictionary) {
+  RestoreInternalDictionary(output_dictionary);
+
+  RESTORE_DICT_MEMBER(derivedStringMember, DerivedStringMember);
+  RESTORE_DICT_MEMBER(derivedStringMemberWithDefault,
+                      DerivedStringMemberWithDefault);
+  RESTORE_DICT_MEMBER(requiredBooleanMember, RequiredBooleanMember);
+}
+
+void DictionaryTest::RestoreInternalDictionaryDerivedDerived(
+    InternalDictionaryDerivedDerived* output_dictionary) {
+  RestoreInternalDictionaryDerived(output_dictionary);
+
+  RESTORE_DICT_MEMBER(derivedDerivedStringMember, DerivedDerivedStringMember);
+}
+
+#undef RESTORE_DICT_MEMBER
 
 }  // namespace blink
--- third_party/blink/renderer/core/testing/dictionary_test.h
+++ third_party/blink/renderer/core/testing/dictionary_test.h
@@ -5,17 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_DICTIONARY_TEST_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_DICTIONARY_TEST_H_
 
-#include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/blink/renderer/bindings/core/v8/double_or_string.h"
-#include "third_party/blink/renderer/bindings/core/v8/internal_enum_or_internal_enum_sequence.h"
-#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_internal_enum.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_test_callback.h"
-#include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
-#include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/wtf/hash_map.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -31,74 +21,31 @@ class DictionaryTest : public ScriptWrappable {
   DictionaryTest();
   ~DictionaryTest() override;
 
-  // Stores all members into corresponding fields
-  void set(const InternalDictionary*);
-  // Sets each member of the given TestDictionary from fields
+  // Saves the given InternalDictionary in this object.
+  void set(v8::Isolate* isolate, const InternalDictionary* input_dictionary);
+  // Returns an InternalDictionary with filled with the saved dictionary
+  // members.
   InternalDictionary* get(v8::Isolate* isolate);
 
-  void setDerived(const InternalDictionaryDerived*);
+  void setDerived(v8::Isolate* isolate,
+                  const InternalDictionaryDerived* input_dictionary);
   InternalDictionaryDerived* getDerived(v8::Isolate* isolate);
 
-  void setDerivedDerived(const InternalDictionaryDerivedDerived*);
+  void setDerivedDerived(
+      v8::Isolate* isolate,
+      const InternalDictionaryDerivedDerived* input_dictionary);
   InternalDictionaryDerivedDerived* getDerivedDerived(v8::Isolate* isolate);
 
   void Trace(Visitor*) const override;
 
  private:
-  void Reset();
+  void RestoreInternalDictionary(InternalDictionary* output_dictionary);
+  void RestoreInternalDictionaryDerived(
+      InternalDictionaryDerived* output_dictionary);
+  void RestoreInternalDictionaryDerivedDerived(
+      InternalDictionaryDerivedDerived* output_dictionary);
 
-  void GetInternals(InternalDictionary*);
-  void GetDerivedInternals(InternalDictionaryDerived*);
-  void GetDerivedDerivedInternals(InternalDictionaryDerivedDerived*);
-
-  // The reason to use absl::optional<T> is convenience; we use
-  // absl::optional<T> here to record whether the member field is set or not.
-  // Some members are not wrapped with optional because:
-  //  - |longMemberWithDefault| has a non-null default value
-  //  - String and PtrTypes can express whether they are null
-  //  - absl::optional does not work with GarbageCollected types when used on
-  //  heap.
-  absl::optional<int> long_member_;
-  absl::optional<int> long_member_with_clamp_;
-  absl::optional<int> long_member_with_enforce_range_;
-  int long_member_with_default_;
-  absl::optional<int> long_or_null_member_;
-  absl::optional<int> long_or_null_member_with_default_;
-  absl::optional<bool> boolean_member_;
-  absl::optional<double> double_member_;
-  absl::optional<double> unrestricted_double_member_;
-  absl::optional<String> string_member_;
-  String string_member_with_default_;
-  absl::optional<String> byte_string_member_;
-  absl::optional<String> usv_string_member_;
-  absl::optional<Vector<String>> string_sequence_member_;
-  Vector<String> string_sequence_member_with_default_;
-  absl::optional<Vector<String>> string_sequence_or_null_member_;
-  absl::optional<String> enum_member_;
-  String enum_member_with_default_;
-#ifdef USE_BLINK_V8_BINDING_NEW_IDL_DICTIONARY
-  // The outer optional<> represents if the member is missing, and the inner
-  // optional<> represents if the member is a null value.
-  absl::optional<absl::optional<V8InternalEnum>> enum_or_null_member_;
-#else
-  absl::optional<String> enum_or_null_member_;
-#endif
-  Member<Element> element_member_;
-  Member<Element> element_or_null_member_;
-  bool has_element_or_null_member_ = false;
-  ScriptValue object_member_;
-  ScriptValue object_or_null_member_with_default_;
-  DoubleOrString double_or_string_member_;
-  Member<HeapVector<DoubleOrString>> double_or_string_sequence_or_null_member_;
-  Member<EventTarget> event_target_or_null_member_;
-  absl::optional<String> derived_string_member_;
-  String derived_string_member_with_default_;
-  absl::optional<String> derived_derived_string_member_;
-  bool required_boolean_member_;
-  absl::optional<HashMap<String, String>> dictionary_member_properties_;
-  InternalEnumOrInternalEnumSequence internal_enum_or_internal_enum_sequence_;
-  ScriptValue any_member_;
-  Member<V8TestCallback> callback_function_member_;
+  Member<InternalDictionaryDerivedDerived> dictionary_;
 };
 
 }  // namespace blink
