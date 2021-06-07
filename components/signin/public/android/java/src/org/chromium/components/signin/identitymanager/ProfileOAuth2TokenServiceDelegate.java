f72d313bf9ae947f7f7f67493fca05ad65b7eec3
Alice Wang <aliceywang@chromium.org>
2021-06-07 11:24:20 +0000

    [Android] Migrate tryGetGoogleAccounts() in PO2TS#getAccessTokenFromNative()
    This CL migrates the usage of the blocking tryGetGoogleAccounts()
    to the asynchronous version in PO2TS#getAccessTokenFromNative().
    
    Bug: 1211294
    Change-Id: I91afa31514b6f843cfa8763561d88d72e57fa5db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934907
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889717}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegate.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegate.java
@@ -83,28 +83,30 @@ final class ProfileOAuth2TokenServiceDelegate {
     private void getAccessTokenFromNative(
             String accountEmail, String scope, final long nativeCallback) {
         assert accountEmail != null : "Account email cannot be null!";
-        final Account account = AccountUtils.findAccountByName(
-                mAccountManagerFacade.tryGetGoogleAccounts(), accountEmail);
-        if (account == null) {
-            ThreadUtils.postOnUiThread(() -> {
-                ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(
-                        null, AccessTokenData.NO_KNOWN_EXPIRATION_TIME, false, nativeCallback);
-            });
-            return;
-        }
-        String oauth2Scope = OAUTH2_SCOPE_PREFIX + scope;
-        getAccessToken(account, oauth2Scope, new GetAccessTokenCallback() {
-            @Override
-            public void onGetTokenSuccess(AccessTokenData token) {
-                ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(
-                        token.getToken(), token.getExpirationTimeSecs(), false, nativeCallback);
+        mAccountManagerFacade.tryGetGoogleAccounts(accounts -> {
+            final Account account = AccountUtils.findAccountByName(accounts, accountEmail);
+            if (account == null) {
+                ThreadUtils.postOnUiThread(() -> {
+                    ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(
+                            null, AccessTokenData.NO_KNOWN_EXPIRATION_TIME, false, nativeCallback);
+                });
+                return;
             }
+            String oauth2Scope = OAUTH2_SCOPE_PREFIX + scope;
+            getAccessToken(account, oauth2Scope, new GetAccessTokenCallback() {
+                @Override
+                public void onGetTokenSuccess(AccessTokenData token) {
+                    ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(
+                            token.getToken(), token.getExpirationTimeSecs(), false, nativeCallback);
+                }
 
-            @Override
-            public void onGetTokenFailure(boolean isTransientError) {
-                ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(null,
-                        AccessTokenData.NO_KNOWN_EXPIRATION_TIME, isTransientError, nativeCallback);
-            }
+                @Override
+                public void onGetTokenFailure(boolean isTransientError) {
+                    ProfileOAuth2TokenServiceDelegateJni.get().onOAuth2TokenFetched(null,
+                            AccessTokenData.NO_KNOWN_EXPIRATION_TIME, isTransientError,
+                            nativeCallback);
+                }
+            });
         });
     }
 
