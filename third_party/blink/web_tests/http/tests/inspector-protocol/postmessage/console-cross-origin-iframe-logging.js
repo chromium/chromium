792b58d0e4bf96cf9ee61368ebfaab2e904e3f39
Sigurd Schneider <sigurds@chromium.org>
2021-06-07 07:50:18 +0000

    Rewrite devtools test as protocol inspector test
    This CL rewrites console-cross-origin-iframe-logging.js
    as a inspector protocol test and adds actual validation
    for two tests steps that weren't checked in the original
    version.
    
    Bug: chromium:718940
    Change-Id: I5b8ac0855dec4db66f3f7970690200e4dc69e98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940186
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889671}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/postmessage/console-cross-origin-iframe-logging.js
@@ -0,0 +1,42 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startURL(
+      'http://example.test:8000/inspector-protocol/resources/empty.html',
+      `Tests that postMessage origin mismatches are logged with source url and line number if both frames are in the same process.`);
+
+  const onEntryAddedHandler = event => {
+    testRunner.log(event.params.entry.stackTrace);
+    testRunner.log(event.params.entry.text);
+    testRunner.completeTest();
+  };
+  dp.Log.onEntryAdded(onEntryAddedHandler);
+  await dp.Log.enable();
+
+  await session.evaluateAsync(`
+    const frame = document.createElement('iframe');
+    frame.src =
+    'http://other.origin.example.test:8000/inspector-protocol/resources/empty.html';
+    document.body.appendChild(frame);
+    window.myframe = frame;
+    new Promise(f => frame.onload = f);
+  `);
+
+  testRunner.log(`Trying to access iframe's location`);
+  const {result: result1} = await dp.Runtime.evaluate(
+      {expression: 'window.myframe.contentWindow.location.host'});
+  testRunner.log(result1.exceptionDetails.exception.className);
+
+  testRunner.log(`Trying to reload iframe`);
+  const {result: result2} = await dp.Runtime.evaluate(
+      {expression: 'window.myframe.location.reload()'});
+  testRunner.log(result2.exceptionDetails.exception.className);
+
+  testRunner.log(`Trying to postMessage iframe`);
+  await session.evaluate(`
+    window.myframe.contentWindow.postMessage("fail",
+    "http://example.test:8000");
+  `);
+})
