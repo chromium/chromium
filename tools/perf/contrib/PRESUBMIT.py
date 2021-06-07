2c606267865f6664b12e5b3a1ef7d7cb20238133
John Chen <johnchen@chromium.org>
2021-06-07 19:48:26 +0000

    Fix presubmit to ignore __pycache__ directory
    Fix presubmit in tools/perf/contrib to ignore __pycache__ directory,
    which is automatically created by Python 3.
    
    Bug: 1199156
    
    Change-Id: I6826348ab83fea07e689969179cb96d37e8c028b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944746
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889904}

--- tools/perf/contrib/PRESUBMIT.py
+++ tools/perf/contrib/PRESUBMIT.py
@@ -41,6 +41,8 @@ def _CheckContribDir(input_api, output_api):
       invalid_contrib_files.append(file_path)
 
   for f in input_api.os_listdir(contrib_dir):
+    if f == '__pycache__':
+      continue
     path = input_api.os_path.join(contrib_dir, f)
     if input_api.os_path.isdir(path):
       results.extend(
