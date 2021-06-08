d62f241562092023a4ddeebaf3491b7f3fd06c37
Peter Boström <pbos@chromium.org>
2021-06-08 22:32:55 +0000

    Make Omnibox FocusRing match theme background
    This fixes the FocusRing color selection when the Omnibox theme
    background color mismatches the OS light/dark mode.
    
    Bug: 1213536
    Change-Id: Iaa2889630338b69f67a9caff5661b022e22b2b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946341
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890483}

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -12,6 +12,7 @@
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_row_view.h"
@@ -173,6 +174,9 @@ OmniboxPopupContentsView::OmniboxPopupContentsView(
             &OmniboxPopupContentsView::OnSuggestionGroupVisibilityUpdate,
             base::Unretained(this)));
   }
+
+  views::FocusRing::SetBackgroundColorIdForSubtree(
+      this, ThemeProperties::COLOR_OMNIBOX_RESULTS_BG);
 }
 
 OmniboxPopupContentsView::~OmniboxPopupContentsView() {
