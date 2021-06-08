2214b1fb69f9668cc58e65e399a52ce89a75ac01
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-06-08 10:04:36 +0000

    [iOS][Default Promo] Don't use cool down for remind me promo
    Fixes bug where the remind me later promo is capped by the fullscreen
    promo cool down, instead of the correct remind me later timing.
    
    (cherry picked from commit 6f2cd62dd7e43312bc9c96ce2ef954fac5cfa598)
    
    Bug: 1215318
    Change-Id: I3fe3fd29e5368a068351e1eee48d01733ae99270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927705
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#888893}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944716
    Auto-Submit: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#404}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -984,7 +984,7 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   if (!firstRun && self.sceneState.appState.initStage > InitStageSafeMode &&
       postOpeningAction == NO_ACTION &&
       !self.sceneState.appState.postCrashLaunch &&
-      !IsChromeLikelyDefaultBrowser() && !UserInPromoCooldown()) {
+      !IsChromeLikelyDefaultBrowser()) {
     // Show the Default Browser promo UI if the user's past behavior fits
     // the categorization of potentially interested users or if the user is
     // signed in. Do not show if it is determined that Chrome is already the
@@ -1013,7 +1013,7 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
         !HasUserInteractedWithTailoredFullscreenPromoBefore() &&
         (isMadeForIOSPromoEligible || isAllTabsPromoEligible ||
          isStaySafePromoEligible);
-    if (isTailoredPromoEligibleUser) {
+    if (isTailoredPromoEligibleUser && !UserInPromoCooldown()) {
       self.sceneState.appState.shouldShowDefaultBrowserPromo = YES;
       self.sceneState.appState.defaultBrowserPromoTypeToShow =
           MostRecentInterestDefaultPromoType(!isSignedIn);
@@ -1025,7 +1025,8 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
     BOOL isGeneralPromoEligibleUser =
         !HasUserInteractedWithFullscreenPromoBefore() &&
         (IsLikelyInterestedDefaultBrowserUser(DefaultPromoTypeGeneral) ||
-         isSignedIn);
+         isSignedIn) &&
+        !UserInPromoCooldown();
     if (isGeneralPromoEligibleUser ||
         ShouldShowRemindMeLaterDefaultBrowserFullscreenPromo()) {
       self.sceneState.appState.shouldShowDefaultBrowserPromo = YES;
