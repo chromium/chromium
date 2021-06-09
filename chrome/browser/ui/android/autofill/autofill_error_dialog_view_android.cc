a57bd36c288a80d556a2b3f3bf5fa9cab704c1a8
siashah <siashah@chromium.org>
2021-06-09 01:38:21 +0000

    [VirtualCards] Add implementation for autofill error dialog view
    Also added a build target at
    //chrome/browser/ui/android/autofill/internal as per modularization
    guidance. go/how-to-modularize-clank
    
    Currently the error dialog view is only used for Virtual Cards, however,
    it is intended to be generic enough to be used for other autofill flows
    in the future.
    
    Follow-up:
    * Add tests
    * Add wiring logic to trigger the dialog based on unmask response.
    
    Bug: 1196021
    Change-Id: I02b08b6a46ab3cf9f5ad2da38450fd6c28f5aa84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923325
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890551}

---
+++ chrome/browser/ui/android/autofill/autofill_error_dialog_view_android.cc
@@ -0,0 +1,71 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/android/autofill/autofill_error_dialog_view_android.h"
+
+#include <jni.h>
+#include <stddef.h>
+#include "chrome/browser/android/resource_mapper.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/android/scoped_java_ref.h"
+#include "base/compiler_specific.h"
+#include "base/macros.h"
+#include "chrome/browser/ui/android/autofill/internal/jni_headers/AutofillErrorDialogBridge_jni.h"
+#include "components/grit/components_scaled_resources.h"
+#include "ui/android/view_android.h"
+#include "ui/android/window_android.h"
+#include "ui/base/resource/resource_bundle.h"
+
+using base::android::ConvertUTF16ToJavaString;
+
+namespace autofill {
+
+AutofillErrorDialogViewAndroid::AutofillErrorDialogViewAndroid(
+    AutofillErrorDialogController* controller)
+    : controller_(controller) {}
+
+AutofillErrorDialogViewAndroid::~AutofillErrorDialogViewAndroid() = default;
+
+// static
+std::unique_ptr<AutofillErrorDialogView> AutofillErrorDialogView::Create(
+    AutofillErrorDialogController* controller) {
+  return std::make_unique<AutofillErrorDialogViewAndroid>(controller);
+}
+
+void AutofillErrorDialogViewAndroid::Show() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  ui::ViewAndroid* view_android =
+      controller_->GetWebContents()->GetNativeView();
+  DCHECK(view_android);
+  ui::WindowAndroid* window_android = view_android->GetWindowAndroid();
+  if (!window_android)
+    return;
+
+  java_object_.Reset(Java_AutofillErrorDialogBridge_create(
+      env, reinterpret_cast<intptr_t>(this), window_android->GetJavaObject()));
+
+  Java_AutofillErrorDialogBridge_show(
+      env, java_object_, ConvertUTF16ToJavaString(env, controller_->GetTitle()),
+      ConvertUTF16ToJavaString(env, controller_->GetDescription()),
+      ConvertUTF16ToJavaString(env, controller_->GetButtonLabel()),
+      ResourceMapper::MapToJavaDrawableId(
+          IDR_AUTOFILL_GOOGLE_PAY_WITH_DIVIDER));
+}
+
+void AutofillErrorDialogViewAndroid::Dismiss() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  if (!java_object_.is_null()) {
+    Java_AutofillErrorDialogBridge_dismiss(env, java_object_);
+  } else {
+    OnDismissed(env);
+  }
+}
+
+void AutofillErrorDialogViewAndroid::OnDismissed(JNIEnv* env) {
+  controller_->OnDismissed();
+}
+
+}  // namespace autofill
