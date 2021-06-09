a0a52b62cbedeb11607bf7ef3c1d3f5ef5fe774b
Justin Cohen <justincohen@google.com>
2021-06-09 10:22:55 +0000

    ios: Don't call setSoftwareKeyboardShownByTouch on iOS15.
    Bug: 1217878
    Change-Id: I1cf940e33dc67c71afce2306f35bf969ccd711f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947465
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890688}

--- base/test/test_support_ios.mm
+++ base/test/test_support_ios.mm
@@ -119,7 +119,10 @@ bool IsSceneStartupEnabled() {
   // calls override this behavior by ensuring that the software keyboard is
   // always shown.
   [[UIKeyboardImpl sharedInstance] setAutomaticMinimizationEnabled:NO];
-  [[UIKeyboardImpl sharedInstance] setSoftwareKeyboardShownByTouch:YES];
+  if (@available(iOS 15, *)) {
+  } else {
+    [[UIKeyboardImpl sharedInstance] setSoftwareKeyboardShownByTouch:YES];
+  }
 #endif  // TARGET_IPHONE_SIMULATOR
 
   if (!IsSceneStartupEnabled()) {
