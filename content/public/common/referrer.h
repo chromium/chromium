76c955f9f03220e2e59ad5217f5a5f137410fe13
David Van Cleve <davidvc@chromium.org>
2021-06-09 03:13:07 +0000

    content::Referrer: Remove two unused method declarations
    crrev.com/801398 moved these methods to blink::Referrer, except it seems the declarations stuck around in //content.
    
    Bug: 1016541, 860403
    Change-Id: Iff0b42fa1b393562dce842b80033ccf00b274b3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948183
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Auto-Submit: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890581}

--- content/public/common/referrer.h
+++ content/public/common/referrer.h
@@ -50,13 +50,6 @@ struct CONTENT_EXPORT Referrer {
   static net::ReferrerPolicy ReferrerPolicyForUrlRequest(
       network::mojom::ReferrerPolicy referrer_policy);
 
-  // Configures retaining the pre-M80 default referrer
-  // policy of no-referrer-when-downgrade.
-  // TODO(crbug.com/1016541): After M82, remove when the corresponding
-  // enterprise policy has been deleted.
-  static void SetForceLegacyDefaultReferrerPolicy(bool force);
-  static bool ShouldForceLegacyDefaultReferrerPolicy();
-
   // Validates |policy| to make sure it represents one of the valid
   // net::mojom::ReferrerPolicy enum values and returns it.  The relatively safe
   // |kNever| value is returned if |policy| is not a valid value.
