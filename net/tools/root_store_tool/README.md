caa2438f516fe141607e5f76dd10b03ccf2451e7
Hubert Chao <hchao@chromium.org>
2021-06-10 15:25:10 +0000

    Chrome root store: PEM files and skeleton of codegen tool
    Start of chrome root cert trust store; checking in PEM files for certs
    that are in https://g.co/chrome/root-store and the start of a tool for
    including this data in chromium.
    
    Change-Id: Id56f19d4524561f5a7b4ee950b654cde2df996c5
    Bug: 1216547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939655
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891233}

---
+++ net/tools/root_store_tool/README.md
@@ -0,0 +1,8 @@
+# Root store tool
+
+This directory contains tools for processing the
+[Chrome Root
+Store](https://www.chromium.org/Home/chromium-security/root-ca-policy) data in
+[`//net/data/ssl/chrome_root_store`](/net/data/ssl/chrome_root_store).
+
+TODO(https://crbug.com/1216547): document how to use these tools.
---
+++ net/tools/root_store_tool/root_store_tool.cc
@@ -0,0 +1,172 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+#include <string>
+
+#include "base/at_exit.h"
+#include "base/base_paths.h"
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/logging.h"
+#include "base/path_service.h"
+#include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
+#include "crypto/openssl_util.h"
+#include "net/tools/root_store_tool/root_store.pb.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/boringssl/src/include/openssl/bio.h"
+#include "third_party/boringssl/src/include/openssl/pem.h"
+#include "third_party/protobuf/src/google/protobuf/text_format.h"
+
+using chrome_root_store::RootStore;
+
+namespace {
+
+absl::optional<std::string> DecodePEM(base::StringPiece pem) {
+  // TODO(https://crbug.com/1216547): net/cert/pem.h has a much nicer API, but
+  // it would require some build refactoring to avoid a circular dependency.
+  // This is assuming that the chrome trust store code goes in
+  // net/cert/internal, which it may not.
+  bssl::UniquePtr<BIO> bio(BIO_new_mem_buf(pem.data(), pem.size()));
+  if (!bio) {
+    return absl::nullopt;
+  }
+  char* name;
+  char* header;
+  unsigned char* data;
+  long len;
+  if (!PEM_read_bio(bio.get(), &name, &header, &data, &len)) {
+    LOG(ERROR) << "Could not find PEM block.";
+    return absl::nullopt;
+  }
+  bssl::UniquePtr<char> scoped_name(name);
+  bssl::UniquePtr<char> scoped_header(header);
+  bssl::UniquePtr<unsigned char> scoped_data(data);
+  if (strcmp(name, "CERTIFICATE") != 0) {
+    LOG(ERROR) << "Found PEM block of type " << name
+               << " instead of CERTIFICATE";
+    return absl::nullopt;
+  }
+  return std::string(data, data + len);
+}
+
+absl::optional<RootStore> ReadTextRootStore(
+    const base::FilePath& root_store_dir) {
+  base::FilePath root_store_path =
+      root_store_dir.AppendASCII("root_store.textproto");
+  std::string root_store_text;
+  if (!base::ReadFileToString(root_store_path, &root_store_text)) {
+    LOG(ERROR) << "Could not read " << root_store_path;
+    return absl::nullopt;
+  }
+
+  RootStore root_store;
+  if (!google::protobuf::TextFormat::ParseFromString(root_store_text,
+                                                     &root_store)) {
+    LOG(ERROR) << "Could not parse " << root_store_path;
+    return absl::nullopt;
+  }
+
+  // Replace the filenames with the actual certificate contents.
+  base::FilePath certs_dir = root_store_dir.AppendASCII("certs");
+  for (auto& anchor : *root_store.mutable_trust_anchors()) {
+    base::FilePath pem_path = certs_dir.AppendASCII(anchor.filename());
+
+    if (!base::PathExists(pem_path)) {
+      LOG(ERROR) << "Error file does not exist: " << pem_path;
+      return absl::nullopt;
+    }
+
+    if (base::DirectoryExists(pem_path)) {
+      LOG(ERROR) << "Error path is a directory: " << pem_path;
+      return absl::nullopt;
+    }
+
+    if (!base::PathIsReadable(pem_path)) {
+      LOG(ERROR) << "Error file is not readable: " << pem_path;
+      return absl::nullopt;
+    }
+
+    std::string pem;
+    if (!base::ReadFileToString(pem_path, &pem)) {
+      LOG(ERROR) << "Error reading " << pem_path;
+      return absl::nullopt;
+    }
+    absl::optional<std::string> der = DecodePEM(pem);
+    if (!der) {
+      LOG(ERROR) << "Error decoding " << pem_path;
+      return absl::nullopt;
+    }
+    anchor.clear_filename();
+    anchor.set_der(*der);
+  }
+  return std::move(root_store);
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  base::AtExitManager at_exit_manager;
+  base::CommandLine::Init(argc, argv);
+
+  logging::LoggingSettings settings;
+  settings.logging_dest =
+      logging::LOG_TO_SYSTEM_DEBUG_LOG | logging::LOG_TO_STDERR;
+  logging::InitLogging(settings);
+
+  crypto::EnsureOpenSSLInit();
+
+  base::CommandLine& command_line = *base::CommandLine::ForCurrentProcess();
+  base::FilePath proto_path = command_line.GetSwitchValuePath("write-proto");
+  base::FilePath cpp_path = command_line.GetSwitchValuePath("write-cpp");
+  if ((proto_path.empty() && cpp_path.empty()) ||
+      command_line.HasSwitch("help")) {
+    std::cerr << "Usage: root_store_tool [--write-proto=PROTO_FILE] "
+                 "[--write-cpp=CPP_FILE]"
+              << std::endl;
+    return 1;
+  }
+
+  base::FilePath root_store_dir =
+      command_line.GetSwitchValuePath("root-store-dir");
+  if (root_store_dir.empty()) {
+    base::FilePath source_root;
+    CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &source_root));
+    root_store_dir = source_root.AppendASCII("net")
+                         .AppendASCII("data")
+                         .AppendASCII("ssl")
+                         .AppendASCII("chrome_root_store");
+  }
+
+  absl::optional<RootStore> root_store = ReadTextRootStore(root_store_dir);
+  if (!root_store) {
+    return 1;
+  }
+
+  // TODO(https://crbug.com/1216547): Figure out how to use the serialized
+  // proto to support component update.
+  // components/resources/ssl/ssl_error_assistant/push_proto.py
+  // does it through a GCS bucket (I think) so that might be an option.
+  if (!proto_path.empty()) {
+    std::string serialized;
+    if (!root_store->SerializeToString(&serialized)) {
+      LOG(ERROR) << "Error serializing root store proto"
+                 << root_store->DebugString();
+      return 1;
+    }
+    if (!base::WriteFile(proto_path, serialized)) {
+      LOG(ERROR) << "Error writing serialized proto root store";
+      return 1;
+    }
+  }
+
+  if (!cpp_path.empty()) {
+    // TODO(https://crbug.com/1216547): implement C++ generation.
+    LOG(ERROR) << "Error writing c++ root store; unimplemented!";
+  }
+
+  return 0;
+}
