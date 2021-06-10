d50f8fe909f2a0613a34f81a19222f74d148d917
Alexander Dunaev <adunaev@igalia.com>
2021-06-10 16:59:54 +0000

    Reland "[linux] Improved the wrapper for the D-Bus screensaver service."
    This is a reland of a7fa7364dde651afdc364562cb60ddf3674dc645
    
    The original change had a threading issue: in case when a D-Bus service
    was unavailable, and the wrapper was about to pick the next service from
    the list, it called the blocking Detach() method on the wrong thread.
    
    This is now fixed by posting the blocking call on the correct thread.
    
    Original change's description:
    > [linux] Improved the wrapper for the D-Bus screensaver service.
    >
    > This is a follow-up to [1].  Here the wrapper class is improved so it no
    > longer uses blocking calls to D-Bus.
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/2929877
    >
    > Bug: 1098202
    > Change-Id: I8a71e5f56a1dec4836010889dde1be7231ea9e7f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2931463
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#890086}
    
    Bug: 1098202
    Change-Id: Ie84b5263b526ba4dc75d30f3592dc9c771531567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950173
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#891268}

--- ui/base/idle/idle_linux.cc
+++ ui/base/idle/idle_linux.cc
@@ -2,25 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/notreached.h"
+#include "ui/base/idle/idle.h"
+#include "ui/base/idle/idle_internal.h"
+
+#if defined(USE_DBUS)
 #include "base/bind.h"
+#include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/no_destructor.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/task_runner.h"
-#include "ui/base/idle/idle.h"
-
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_path.h"
 #include "dbus/object_proxy.h"
-#include "ui/base/idle/idle_internal.h"
+#endif
 
 #if defined(USE_X11)
 #include "ui/base/x/x11_idle_query.h"
 #include "ui/base/x/x11_screensaver.h"
-#else
-#include "base/notreached.h"
 #endif
 
 #if defined(USE_OZONE)
@@ -30,16 +32,20 @@
 
 namespace ui {
 
+#if defined(USE_DBUS)
+
 namespace {
 
 const char kMethodName[] = "GetActive";
 const char kSignalName[] = "ActiveChanged";
 
+// Various names under which the service may be found in different Linux desktop
+// environments.
 struct {
   const char* service_name;
   const char* object_path;
   const char* interface;
-} constexpr kInterfaces[] = {
+} constexpr kServices[] = {
     // ksmserver, light-locker, etc.
     {"org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver",
      "org.freedesktop.ScreenSaver"},
@@ -54,23 +60,12 @@ struct {
     {"org.xfce.ScreenSaver", "/", "org.xfce.ScreenSaver"},
 };
 
-bool ServiceNameHasOwner(dbus::Bus* bus, const char* service_name) {
-  dbus::ObjectProxy* dbus_proxy =
-      bus->GetObjectProxy(DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS));
-  dbus::MethodCall name_has_owner_call(DBUS_INTERFACE_DBUS, "NameHasOwner");
-  dbus::MessageWriter writer(&name_has_owner_call);
-  writer.AppendString(service_name);
-  std::unique_ptr<dbus::Response> name_has_owner_response =
-      dbus_proxy->CallMethodAndBlock(&name_has_owner_call,
-                                     dbus::ObjectProxy::TIMEOUT_USE_DEFAULT);
-  dbus::MessageReader reader(name_has_owner_response.get());
-  bool owned = false;
-  return name_has_owner_response && reader.PopBool(&owned) && owned;
-}
+constexpr size_t kServiceCount = sizeof(kServices) / sizeof(kServices[0]);
 
-// This class checks for availability of various DBus screensaver interfaces
-// and listens for screensaver events on the first one found.
-class IdleLinuxImpl {
+// This class tries to find the name under which the ScreenSaver D-Bus service
+// is registered, and if found the one, connects to the service and subscribes
+// to its notifications.
+class DBusScreenSaverWatcher {
  public:
   enum class LockState {
     kUnknown,
@@ -78,59 +73,119 @@ class IdleLinuxImpl {
     kUnlocked,
   };
 
-  IdleLinuxImpl()
+  DBusScreenSaverWatcher()
       : task_runner_(
             base::ThreadPool::CreateSequencedTaskRunner(base::TaskTraits(
                 base::MayBlock(),
                 base::TaskPriority::USER_VISIBLE,
                 base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN))) {
-    task_runner_->PostTask(FROM_HERE,
-                           base::BindOnce(&IdleLinuxImpl::InitOnTaskRunner,
-                                          base::Unretained(this)));
-  }
-
-  LockState lock_state() const { return lock_state_; }
-
- private:
-  ~IdleLinuxImpl() = default;
-
-  void InitOnTaskRunner() {
     dbus::Bus::Options options;
     options.bus_type = dbus::Bus::SESSION;
     options.connection_type = dbus::Bus::PRIVATE;
     options.dbus_task_runner = task_runner_;
     bus_ = base::MakeRefCounted<dbus::Bus>(options);
 
-    for (const auto& interface : kInterfaces) {
-      // Calling methods on a non-existent service will lead to a timeout rather
-      // than an immediate error, so check for service existence first.
-      if (!ServiceNameHasOwner(bus_.get(), interface.service_name))
-        continue;
+    TryCurrentService();
+  }
 
-      // To avoid a race condition, start listenting for state changes before
-      // querying the state.
-      auto* proxy = bus_->GetObjectProxy(
-          interface.service_name, dbus::ObjectPath(interface.object_path));
-      if (!proxy->ConnectToSignalAndBlock(
-              interface.interface, kSignalName,
-              base::BindRepeating(&IdleLinuxImpl::OnActiveChanged,
-                                  base::Unretained(this)))) {
-        continue;
+  LockState lock_state() const { return lock_state_; }
+
+ private:
+  ~DBusScreenSaverWatcher() = default;
+
+  // Starts the initialisation sequence for the current service.  Failure at any
+  // step will increment the service counter and re-start the process.
+  void TryCurrentService() {
+    // Detach the proxy, if we have one from the previous attempt.
+    if (proxy_) {
+      task_runner_->PostTask(
+          FROM_HERE,
+          base::BindOnce(&dbus::ObjectProxy::Detach, base::Unretained(proxy_)));
+      proxy_ = nullptr;
+    }
+
+    if (current_service_ >= kServiceCount) {
+      if (current_service_ == kServiceCount) {
+        // Log the warning once.
+        LOG(WARNING)
+            << "None of the known D-Bus ScreenSaver services could be used.";
+        ++current_service_;
       }
+      return;
+    }
 
-      // Some service owners (eg. gsd-screensaver-proxy) advertise the correct
-      // methods on org.freedesktop.ScreenSaver, but calling them will result in
-      // a NotImplemented DBus error.  To ensure the service owner will send
-      // state change events, make an explicit method call and check that no
-      // error is returned.
-      dbus::MethodCall method_call(interface.interface, kMethodName);
-      auto response = proxy->CallMethodAndBlock(
-          &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT);
-      if (response && UpdateLockState(response.get()))
-        return;
+    // Calling methods on a non-existent service will lead to a timeout rather
+    // than an immediate error, so check for service existence first.
+    dbus::ObjectProxy* dbus_proxy = bus_->GetObjectProxy(
+        DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS));
+    dbus::MethodCall name_has_owner_call(DBUS_INTERFACE_DBUS, "NameHasOwner");
+    dbus::MessageWriter writer(&name_has_owner_call);
+    writer.AppendString(kServices[current_service_].service_name);
+    dbus_proxy->CallMethod(
+        &name_has_owner_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&DBusScreenSaverWatcher::OnServiceHasOwner,
+                       weak_factory_.GetWeakPtr()));
+  }
 
-      // Try the next interface.
-      proxy->Detach();
+  void OnServiceHasOwner(dbus::Response* response) {
+    DCHECK_LT(current_service_, kServiceCount);
+
+    dbus::MessageReader reader(response);
+    bool owned = false;
+    if (!response || !reader.PopBool(&owned) || !owned) {
+      VLOG(1) << kServices[current_service_].service_name
+              << " D-Bus service does not exist";
+      ++current_service_;
+      return TryCurrentService();
+    }
+
+    // Now connect the ActiveChanged signal.
+    proxy_ = bus_->GetObjectProxy(
+        kServices[current_service_].service_name,
+        dbus::ObjectPath(kServices[current_service_].object_path));
+    proxy_->ConnectToSignal(
+        kServices[current_service_].interface, kSignalName,
+        base::BindRepeating(&DBusScreenSaverWatcher::OnActiveChanged,
+                            weak_factory_.GetWeakPtr()),
+        base::BindOnce(&DBusScreenSaverWatcher::OnActiveChangedSignalConnected,
+                       weak_factory_.GetWeakPtr()));
+  }
+
+  void OnActiveChangedSignalConnected(const std::string& interface,
+                                      const std::string& signal,
+                                      bool succeeded) {
+    DCHECK_LT(current_service_, kServiceCount);
+    DCHECK_EQ(interface, kServices[current_service_].interface);
+    DCHECK_EQ(signal, kSignalName);
+
+    if (!succeeded) {
+      VLOG(1) << "Cannot connect to " << kSignalName << " signal of "
+              << interface << " D-Bus service";
+      ++current_service_;
+      return TryCurrentService();
+    }
+
+    // Some service owners (e.g., gsd-screensaver-proxy) advertise the correct
+    // methods on org.freedesktop.ScreenSaver, but calling them will result in
+    // a NotImplemented DBus error.  To ensure the service owner will send
+    // state change events, and to have the correct current state of the lock,
+    // make an explicit method call and check that no error is returned.
+    dbus::MethodCall method_call(kServices[current_service_].interface,
+                                 kMethodName);
+    proxy_->CallMethod(&method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+                       base::BindOnce(&DBusScreenSaverWatcher::OnGetActive,
+                                      weak_factory_.GetWeakPtr()));
+  }
+
+  void OnGetActive(dbus::Response* response) {
+    DCHECK_LT(current_service_, kServiceCount);
+
+    if (!response || !UpdateLockState(response)) {
+      VLOG(1)
+          << "Call to " << kMethodName << " method of "
+          << kServices[current_service_].interface << " D-Bus service failed";
+      ++current_service_;
+      return TryCurrentService();
     }
   }
 
@@ -145,22 +200,28 @@ class IdleLinuxImpl {
     return true;
   }
 
-  // Only accessed on the task runner.
+  LockState lock_state_ = LockState::kUnknown;
+
+  // Index of the service (in the kServices array) that is currently being
+  // initialised or used.  A value out of bounds means that no working service
+  // was found.
+  size_t current_service_ = 0;
+
   scoped_refptr<dbus::Bus> bus_;
-
-  // Only accessed on the main thread.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  dbus::ObjectProxy* proxy_ = nullptr;
 
-  // Read on the main thread and written on the task runner.
-  std::atomic<LockState> lock_state_{LockState::kUnknown};
+  base::WeakPtrFactory<DBusScreenSaverWatcher> weak_factory_{this};
 };
 
-IdleLinuxImpl* GetIdleLinuxImpl() {
-  static base::NoDestructor<IdleLinuxImpl> impl;
+DBusScreenSaverWatcher* GetDBusScreenSaverWatcher() {
+  static base::NoDestructor<DBusScreenSaverWatcher> impl;
   return impl.get();
 }
 
 }  // namespace
+
+#endif  // defined(USE_DBUS)
 
 int CalculateIdleTime() {
 #if defined(USE_OZONE)
@@ -185,9 +246,11 @@ bool CheckIdleStateIsLocked() {
   if (IdleStateForTesting().has_value())
     return IdleStateForTesting().value() == IDLE_STATE_LOCKED;
 
-  auto lock_state = GetIdleLinuxImpl()->lock_state();
-  if (lock_state != IdleLinuxImpl::LockState::kUnknown)
-    return lock_state == IdleLinuxImpl::LockState::kLocked;
+#if defined(USE_DBUS)
+  auto lock_state = GetDBusScreenSaverWatcher()->lock_state();
+  if (lock_state != DBusScreenSaverWatcher::LockState::kUnknown)
+    return lock_state == DBusScreenSaverWatcher::LockState::kLocked;
+#endif
 
 #if defined(USE_OZONE)
   if (features::IsUsingOzonePlatform()) {
