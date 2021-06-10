af2be0e5342445cf503671ad47f3f463fb76f4e0
Tommy Nyquist <nyquist@chromium.org>
2021-06-10 04:59:33 +0000

    [segmentation_platform] Add SignalKey implementation for SignalDatabase
    The SignalDatabase needs a way to identify specific records in the
    database. Some of these records have a single entry, and other have
    multiple. The same database also needs to be able to store multiple
    different types of data.
    
    It is important that on a single device, this representation never
    changes, so steps are taken to make sure it is safe to make assumptions
    about the memory layout of the key.
    
    The SignalKey is not meant to be used by external clients in any way,
    and should be considered as an internal implementation detail of the
    SignalDatabase.
    
    To be able to compare two binary keys lexicographically, big endian
    is used for the representation.
    
    The binary representation of the SignalKey is not human readable, so
    for debugging, a human readable format is also provided on this format:
    {kind=..., name_hash=..., range_start=..., range_start=...}
    
    SignalKey uses SignalKeyInternal for converting to and from binary keys.
    The field order of the comparable binary key is:
    kind, name_hash, range_end, range_start.
    
    See go/chrome-segmentation-storage-mvp for details on the key format.
    
    Bug: 1212931
    Change-Id: I97fc951d3f899f68c12ec51b0438f403c297ee23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941967
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891098}

---
+++ components/segmentation_platform/internal/database/signal_key_internal.cc
@@ -0,0 +1,92 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/segmentation_platform/internal/database/signal_key_internal.h"
+
+#include <stdint.h>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <utility>
+
+#include "base/big_endian.h"
+#include "base/check.h"
+#include "base/logging.h"
+
+namespace segmentation_platform {
+
+std::string SignalKeyInternalToBinary(const SignalKeyInternal& input) {
+  char output[sizeof(SignalKeyInternal)];
+  base::BigEndianWriter writer(output, sizeof(output));
+  writer.WriteBytes(&input.prefix.kind, sizeof(input.prefix.kind));
+  writer.WriteBytes(&input.prefix.padding, sizeof(input.prefix.padding));
+  writer.WriteU64(input.prefix.name_hash);
+  base::WriteBigEndian(writer.ptr(), input.time_range_end_sec);
+  writer.Skip(sizeof(input.time_range_end_sec));
+  base::WriteBigEndian(writer.ptr(), input.time_range_start_sec);
+  writer.Skip(sizeof(input.time_range_start_sec));
+  CHECK_EQ(0UL, writer.remaining());
+  return std::string(output, sizeof(output));
+}
+
+void SignalKeyInternalFromBinary(const std::string& input,
+                                 SignalKeyInternal* output) {
+  CHECK_EQ(input.size(), sizeof(SignalKeyInternal));
+  base::BigEndianReader reader(input.data(), input.size());
+  reader.ReadBytes(&output->prefix.kind, sizeof(output->prefix.kind));
+  reader.Skip(sizeof(SignalKeyInternal::Prefix::padding));
+  reader.ReadU64(&output->prefix.name_hash);
+  base::ReadBigEndian(reader.ptr(), &output->time_range_end_sec);
+  reader.Skip(sizeof(SignalKeyInternal::time_range_end_sec));
+  base::ReadBigEndian(reader.ptr(), &output->time_range_start_sec);
+  reader.Skip(sizeof(SignalKeyInternal::time_range_start_sec));
+  CHECK_EQ(0UL, reader.remaining());
+}
+
+std::string SignalKeyInternalToDebugString(const SignalKeyInternal& input) {
+  std::stringstream buffer;
+  buffer << input;
+  return buffer.str();
+}
+
+std::string SignalKeyInternalPrefixToBinary(
+    const SignalKeyInternal::Prefix& input) {
+  char output[sizeof(SignalKeyInternal::Prefix)];
+  base::BigEndianWriter writer(output, sizeof(output));
+  writer.WriteBytes(&input.kind, sizeof(input.kind));
+  writer.WriteBytes(&input.padding, sizeof(input.padding));
+  writer.WriteU64(input.name_hash);
+  CHECK_EQ(0UL, writer.remaining());
+  std::string output_str = std::string(output, sizeof(output));
+  return output_str;
+}
+
+void SignalKeyInternalPrefixFromBinary(const std::string& input,
+                                       SignalKeyInternal::Prefix* output) {
+  CHECK_EQ(input.size(), sizeof(SignalKeyInternal::Prefix));
+  base::BigEndianReader reader(input.data(), input.size());
+  reader.ReadBytes(&output->kind, sizeof(output->kind));
+  reader.Skip(sizeof(SignalKeyInternal::Prefix::padding));
+  reader.ReadU64(&output->name_hash);
+  CHECK_EQ(0UL, reader.remaining());
+}
+
+std::string SignalKeyInternalPrefixToDebugString(
+    const SignalKeyInternal::Prefix& input) {
+  std::stringstream buffer;
+  buffer << input;
+  return buffer.str();
+}
+
+std::ostream& operator<<(std::ostream& os,
+                         const SignalKeyInternal::Prefix& prefix) {
+  return os << "{" << prefix.kind << ":" << prefix.name_hash << "}";
+}
+
+std::ostream& operator<<(std::ostream& os, const SignalKeyInternal& key) {
+  return os << "{" << key.prefix << ":" << key.time_range_end_sec << ":"
+            << key.time_range_start_sec << "}";
+}
+
+}  // namespace segmentation_platform
