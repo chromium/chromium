56e2e66f1c00ac0aeed958235057125c1bc66490
Kent Tamura <tkent@chromium.org>
2021-06-10 09:43:28 +0000

    SVG Text NG: Fix letter-spacing and word-spacing with zoom
    FontDescription::LetterSpacing and WordSpacing have zoomed values, and
    |scaling_factor| also contains zoom factor. We should avoid to multiply
    zoom factor twice.
    
    Bug: 1179585
    Change-Id: I923cb2379725c0cc78543ab0ae5148b2af727a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951842
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891161}

--- third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.cc
@@ -462,10 +462,11 @@ void LayoutSVGInlineText::ComputeNewScaledFontForStyle(
 
   FontDescription font_description = unscaled_font_description;
   font_description.SetComputedSize(scaled_font_size);
+  const float zoom = style.EffectiveZoom();
   font_description.SetLetterSpacing(font_description.LetterSpacing() *
-                                    scaling_factor);
+                                    scaling_factor / zoom);
   font_description.SetWordSpacing(font_description.WordSpacing() *
-                                  scaling_factor);
+                                  scaling_factor / zoom);
 
   scaled_font =
       Font(font_description, document.GetStyleEngine().GetFontSelector());
