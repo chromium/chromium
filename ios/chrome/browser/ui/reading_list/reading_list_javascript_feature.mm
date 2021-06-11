5115c1111c91896709c3ad386fd7c4274498c7ff
Chris Lu <thegreenfrog@chromium.org>
2021-06-11 19:03:22 +0000

    [ios] Calculate distilability score from Reading List JavaScript
    This code takes the data returned from the JavaScript in
    crrev.com/c/2911296 and passes it to DistillablePageDetector
    to retrieve a distilability score for the page. Both the
    regular score and the "long page" score is calculated to log
    in histograms, but only the regular score is used to determine
    whether to show the Reading List banner.
    
    The execution time is also logged in a histogram to monitor
    the performance of the JavaScript. The estimated "time to read"
    is also logged as a histogram to gain insight on the spread
    of page lengths users are navigating to.
    
    Bug: 1195978
    Change-Id: I413693a7d751949eb60580569c46b265bd2403d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923027
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891740}

--- ios/chrome/browser/ui/reading_list/reading_list_javascript_feature.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_javascript_feature.mm
@@ -4,7 +4,13 @@
 
 #import "ios/chrome/browser/ui/reading_list/reading_list_javascript_feature.h"
 
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "base/values.h"
+#include "components/dom_distiller/core/distillable_page_detector.h"
+#include "components/dom_distiller/core/page_features.h"
+#import "ios/chrome/browser/chrome_url_util.h"
 #import "ios/web/public/js_messaging/java_script_feature_util.h"
 #import "ios/web/public/js_messaging/script_message.h"
 #import "ios/web/public/web_state.h"
@@ -16,6 +22,10 @@
 namespace {
 const char kScriptName[] = "distiller_js";
 const char kScriptHandlerName[] = "ReadingListDOMMessageHandler";
+// Heuristic for words per minute reading speed.
+const int kWordCountPerMinute = 275;
+// Minimum read time to show the Messages prompt.
+const double kReadTimeThreshold = 7.0;
 }  // namespace
 
 ReadingListJavaScriptFeature::ReadingListJavaScriptFeature()
@@ -43,6 +53,100 @@ void ReadingListJavaScriptFeature::ScriptMessageReceived(
     return;
   }
 
-  // TODO(crbug.com/1195978): pass result into
-  // dom_distiller::CalculateDerivedFeatures().
+  absl::optional<GURL> url = message.request_url();
+  if (!url.has_value() || UrlHasChromeScheme(url.value()) ||
+      url.value().SchemeIs(url::kAboutScheme)) {
+    // Ignore any Chrome-handled or NTP pages.
+    return;
+  }
+
+  absl::optional<double> opt_num_elements =
+      message.body()->FindDoubleKey("numElements");
+  double num_elements = 0.0;
+  if (opt_num_elements.has_value()) {
+    num_elements = opt_num_elements.value();
+  }
+
+  absl::optional<double> opt_num_anchors =
+      message.body()->FindDoubleKey("numAnchors");
+  double num_anchors = 0.0;
+  if (opt_num_anchors.has_value()) {
+    num_anchors = opt_num_anchors.value();
+  }
+
+  absl::optional<double> opt_num_forms =
+      message.body()->FindDoubleKey("numForms");
+  double num_forms = 0.0;
+  if (opt_num_forms.has_value()) {
+    num_forms = opt_num_forms.value();
+  }
+
+  absl::optional<double> opt_moz_score =
+      message.body()->FindDoubleKey("mozScore");
+  double moz_score = 0.0;
+  if (opt_moz_score.has_value()) {
+    moz_score = opt_moz_score.value();
+  }
+
+  absl::optional<double> opt_moz_sqrt =
+      message.body()->FindDoubleKey("mozScoreAllSqrt");
+  double moz_sqrt = 0.0;
+  if (opt_moz_sqrt.has_value()) {
+    moz_sqrt = opt_moz_sqrt.value();
+  }
+
+  absl::optional<double> opt_moz_linear =
+      message.body()->FindDoubleKey("mozScoreAllLinear");
+  double moz_linear = 0.0;
+  if (opt_moz_linear.has_value()) {
+    moz_linear = opt_moz_linear.value();
+  }
+
+  absl::optional<double> time = message.body()->FindDoubleKey("time");
+  if (time.has_value()) {
+    UMA_HISTOGRAM_TIMES("IOS.ReadingList.Javascript.ExecutionTime",
+                        base::TimeDelta::FromMillisecondsD(time.value()));
+  }
+
+  std::vector<double> result = dom_distiller::CalculateDerivedFeatures(
+      true, message.request_url().value(), num_elements, num_anchors, num_forms,
+      moz_score, moz_sqrt, moz_linear);
+
+  const dom_distiller::DistillablePageDetector* detector =
+      dom_distiller::DistillablePageDetector::GetNewModel();
+  // Equivalent of DistillablePageDetector::Classify().
+  double score = detector->Score(result) - detector->GetThreshold();
+  // Translate score by +1 to make histogram logging simpler by keeping all
+  // scores positive. Multiply by 100 to get granular scoring logging to the
+  // hundredths digit.
+  NSLog(@"score: %f", score);
+  base::UmaHistogramCustomCounts(
+      "IOS.ReadingList.Javascript.RegularDistillabilityScore",
+      (score + 1) * 100, 0, 400, 50);
+
+  const dom_distiller::DistillablePageDetector* long_detector =
+      dom_distiller::DistillablePageDetector::GetLongPageModel();
+  // Equivalent of DistillablePageDetector::Classify().
+  double long_score =
+      long_detector->Score(result) - long_detector->GetThreshold();
+  // Translate score by +1 to make histogram logging simpler by keeping all
+  // scores positive. Multiply by 100 to get granular scoring logging to the
+  // hundredths digit.
+  base::UmaHistogramCustomCounts(
+      "IOS.ReadingList.Javascript.LongPageDistillabilityScore",
+      (long_score + 1) * 100, 0, 400, 50);
+
+  // Calculate Time to Read
+  absl::optional<double> opt_word_count =
+      message.body()->FindDoubleKey("wordCount");
+  double estimated_read_time = 0.0;
+  if (opt_word_count.has_value()) {
+    estimated_read_time = opt_word_count.value() / kWordCountPerMinute;
+    base::UmaHistogramCounts100("IOS.ReadingList.Javascript.TimeToRead",
+                                estimated_read_time);
+  }
+
+  if (score > 0 && estimated_read_time > kReadTimeThreshold) {
+    // TODO(crbug.com/1195978): Insert Reading List Banner Overlay Request.
+  }
 }
