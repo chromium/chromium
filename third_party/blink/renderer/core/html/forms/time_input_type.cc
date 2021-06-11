9ebcac5a11bfeae8cda89c8d5ea0347d681342ec
Joey Arhar <jarhar@chromium.org>
2021-06-11 20:54:54 +0000

    <input type=time> always return seconds if visible
    This patch makes <input type=time> always include the seconds and
    milliseconds portions of the time if they are visible in the UI, even if they
    are zero.
    
    Firefox already has this behavior. However, it would seem that this
    behavior can only occur and be tested when actually typing into the UI.
    Assigning to input.value as well as using input.stepUp() and
    input.stepDown() already always return seconds/milliseconds when
    appropriate.
    
    Without this patch:
    - "12:34:00" in the UI will return a value of "12:34"
    - "12:34:56" in the UI will return a value of "12:34:56"
    - "12:34:56.000" in the UI will return a value of "12:34:56"
    - "12:34:56.789" in the UI will return a value of "12:34:56.789"
    With this patch:
    - "12:34:00" in the UI will return a value of "12:34:00"
    - "12:34:56" in the UI will return a value of "12:34:56"
    - "12:34:56.000" in the UI will return a value of "12:34:56.000"
    - "12:34:56.789" in the UI will return a value of "12:34:56.789"
    
    Fixed: 399185
    Change-Id: I71569249c61f7dfab6f0072c6192ff7797b3029e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2729168
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891785}

--- third_party/blink/renderer/core/html/forms/time_input_type.cc
+++ third_party/blink/renderer/core/html/forms/time_input_type.cc
@@ -124,8 +124,7 @@ String TimeInputType::FormatDateTimeFieldsState(
   if (!date_time_fields_state.HasHour() ||
       !date_time_fields_state.HasMinute() || !date_time_fields_state.HasAMPM())
     return g_empty_string;
-  if (date_time_fields_state.HasMillisecond() &&
-      date_time_fields_state.Millisecond()) {
+  if (date_time_fields_state.HasMillisecond()) {
     return String::Format(
         "%02u:%02u:%02u.%03u", date_time_fields_state.Hour23(),
         date_time_fields_state.Minute(),
@@ -133,7 +132,7 @@ String TimeInputType::FormatDateTimeFieldsState(
                                            : 0,
         date_time_fields_state.Millisecond());
   }
-  if (date_time_fields_state.HasSecond() && date_time_fields_state.Second()) {
+  if (date_time_fields_state.HasSecond()) {
     return String::Format("%02u:%02u:%02u", date_time_fields_state.Hour23(),
                           date_time_fields_state.Minute(),
                           date_time_fields_state.Second());
