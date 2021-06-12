1b130f3991a440a3004456de1050e0d38939fb8b
Josip Sokcevic <sokcevic@google.com>
2021-06-12 02:30:41 +0000

    [py3] Migrate mojo PRESUBMIT
    Bug: 1212046
    Change-Id: I1819a6b9e936a08d58f2819bb8770238361d10a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957338
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891871}

--- mojo/PRESUBMIT.py
+++ mojo/PRESUBMIT.py
@@ -10,6 +10,9 @@ for more details about the presubmit API built into depot_tools.
 
 import os.path
 
+USE_PYTHON3 = True
+
+
 def CheckChangeOnUpload(input_api, output_api):
   # Additional python module paths (we're in src/mojo/); not everyone needs
   # them, but it's easiest to add them to everyone's path.
