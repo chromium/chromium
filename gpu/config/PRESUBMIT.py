8c42e9d0032b7083592c0d8406ba83b8b7acda5d
Josip Sokcevic <sokcevic@google.com>
2021-06-14 03:30:28 +0000

    [py3] Migrate gpu/config PRESUBMIT
    Bug: 1212035
    Change-Id: I8b2ef744156d9aef75da99c258f8140c6e9cbcf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957344
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891964}

--- gpu/config/PRESUBMIT.py
+++ gpu/config/PRESUBMIT.py
@@ -10,6 +10,10 @@ for more details on the presubmit API built into depot_tools.
 
 import difflib
 import os.path
+import io
+
+USE_PYTHON3 = True
+
 
 def _CheckGPUWorkaroundListSorted(input_api, output_api):
     """Check: gpu_workaround_list.txt feature list sorted alphabetically.
@@ -17,7 +21,8 @@ def _CheckGPUWorkaroundListSorted(input_api, output_api):
     filename = os.path.join(input_api.PresubmitLocalPath(),
                             'gpu_workaround_list.txt')
 
-    workaround_list = [line.rstrip('\n') for line in open(filename)]
+    with io.open(filename, encoding='utf-8') as f:
+      workaround_list = [line.rstrip('\n') for line in f]
 
     workaround_list_sorted = sorted(workaround_list, key=lambda s: s.lower())
     if workaround_list == workaround_list_sorted:
