f419fbe765453e2ad21fe8365d279db07c288b24
Timothy Gu <timothygu@chromium.org>
2021-06-14 15:11:28 +0000

    Remove deprecated DISALLOW_ macros from /third_party/blink/renderer/core/origin_trials
    Macros such as DISALLOW_COPY_AND_ASSIGN() are deprecated since r711900
    per style arbitration [1]. This commit replaces the macros with deleted
    constructors and methods, which is preferred by the Google C++ Style
    Guide [2][3].
    
    Also remove unneeded "base/macros.h" #includes when possible.
    
    [1]: https://groups.google.com/a/chromium.org/g/cxx/c/qwH2hxaEjac/m/TUKq6eqfCwAJ
    [2]: https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++-dos-and-donts.md#explicitly-declare-class-copyability_movability
    [3]: https://google.github.io/styleguide/cppguide.html#Copyable_Movable_Types
    
    This CL was uploaded by git cl split.
    
    R=iclelland@chromium.org
    
    Bug: 1010217
    Change-Id: Id74ec382d9e6c046622e0074ecac74fd998b84e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957416
    Auto-Submit: Timothy Gu <timothygu@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892075}

--- third_party/blink/renderer/core/origin_trials/origin_trial_context_test.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context_test.cc
@@ -49,6 +49,8 @@ const char kTokenPlaceholder[] = "The token contents are not used";
 class MockTokenValidator : public TrialTokenValidator {
  public:
   MockTokenValidator() = default;
+  MockTokenValidator(const MockTokenValidator&) = delete;
+  MockTokenValidator& operator=(const MockTokenValidator&) = delete;
   ~MockTokenValidator() override = default;
 
   // blink::WebTrialTokenValidator implementation
@@ -96,8 +98,6 @@ class MockTokenValidator : public TrialTokenValidator {
   bool is_third_party_;
 
   mutable int call_count_ = 0;
-
-  DISALLOW_COPY_AND_ASSIGN(MockTokenValidator);
 };
 }  // namespace
 
