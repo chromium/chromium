cd131ca29b96f44e8d7f578ae4e7acf11f44cbef
John Abd-El-Malek <jam@chromium.org>
2021-06-14 23:47:22 +0000

    Revert "[Sheriff] Disable flaky WindowOpenFocus test on Mac"
    This reverts commit ef73aa3c2c7503b24e8cd241462f6a3820fa22b4.
    
    Reason for revert: r890327 landed which reverts the field trial testing config for ProcessHostOnUI for Mac.
    
    Original change's description:
    > [Sheriff] Disable flaky WindowOpenFocus test on Mac
    >
    > Bug: 1216102
    > Change-Id: Icc9977bf177aeeb2124d2f415d93d46c7a278fd4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937342
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Owners-Override: Yi Gu <yigu@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#889031}
    
    Bug: 1216102
    Change-Id: I8aa33fb3623457e13d225fd80c7103423c7a8812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946274
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892334}

--- chrome/browser/extensions/window_open_interactive_apitest.cc
+++ chrome/browser/extensions/window_open_interactive_apitest.cc
@@ -9,13 +9,7 @@
 
 namespace extensions {
 
-// Fails flakily on Mac. https://crbug.com/1216102
-#if defined(OS_MAC)
-#define MAYBE_WindowOpenFocus DISABLED_WindowOpenFocus
-#else
-#define MAYBE_WindowOpenFocus WindowOpenFocus
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_WindowOpenFocus) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WindowOpenFocus) {
   ASSERT_TRUE(RunExtensionTest("window_open/focus")) << message_;
 }
 
