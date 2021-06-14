91ccd9a3e0547bd4716b68ee6ba0f25dc58dd4bd
Peter Kasting <pkasting@chromium.org>
2021-06-14 11:20:25 +0000

    Various changes to prevent narrowing conversions: extensions/
    Bug: 1216696
    Change-Id: Ib877c1302360279162c6af860ba61f54b890b331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957546
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892030}

--- extensions/browser/api/alarms/alarms_api_constants.cc
+++ extensions/browser/api/alarms/alarms_api_constants.cc
@@ -10,7 +10,10 @@ namespace alarms_api_constants {
 // 0.016667 minutes  ~= 1s.
 const double kDevDelayMinimum = 0.016667;
 
-const int kReleaseDelayMinimum = 1;
+// Must use int for initializer so static_assert below will compile.  This can
+// all be made better once C++17 inline variables are allowed.
+constexpr int kReleaseDelayMinimumInitializer = 1;
+const double kReleaseDelayMinimum = kReleaseDelayMinimumInitializer;
 
 const char kWarningMinimumDevDelay[] =
     "Alarm delay is less than minimum of 1 minutes. In released .crx, alarm "
@@ -28,7 +31,8 @@ const char kWarningMinimumReleasePeriod[] =
     "Alarm period is less than minimum of 1 minutes. Alarm \"*\" will fire "
     "approximately every 1 minutes.";
 
-static_assert(kReleaseDelayMinimum == 1, "warning message must be updated");
+static_assert(kReleaseDelayMinimumInitializer == 1,
+              "warning message must be updated");
 
 }  // namespace alarms_api_constants
 }  // namespace extensions
