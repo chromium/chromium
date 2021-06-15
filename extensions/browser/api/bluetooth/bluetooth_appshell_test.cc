8be02e0e38d5f7adb35f07612e0b53d2f8d206f5
Fergal Daly <fergal@chromium.org>
2021-06-15 03:15:13 +0000

    Disable BluetoothShellApiTest.ApiSanityCheck it's failing on Mac.
    Bug: 1219861
    Change-Id: I0fdc87d4f29905acaf18b29a07932e3a08228927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960706
    Owners-Override: Fergal Daly <fergal@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892405}

--- extensions/browser/api/bluetooth/bluetooth_appshell_test.cc
+++ extensions/browser/api/bluetooth/bluetooth_appshell_test.cc
@@ -7,6 +7,12 @@
 
 using BluetoothShellApiTest = extensions::ShellApiTest;
 
-IN_PROC_BROWSER_TEST_F(BluetoothShellApiTest, ApiSanityCheck) {
+// TODO(crbug.com/1165955): this test flakes on Mac ASAN
+#if defined(OS_MAC)
+#define MAYBE_ApiSanityCheck DISABLED_ApiSanityCheck
+#else
+#define MAYBE_ApiSanityCheck ApiSanityCheck
+#endif
+IN_PROC_BROWSER_TEST_F(BluetoothShellApiTest, MAYBE_ApiSanityCheck) {
   ASSERT_TRUE(RunAppTest("api_test/bluetooth"));
 }
