49d79be765d572f9bda7e71de0d8b54a7f8bfb27
Elaine Chien <elainechien@chromium.org>
2021-06-15 19:25:48 +0000

    Python3Migration: components/autofill/PRESUBMIT
    Bug: 1211997
    Change-Id: Ie43f0d8b39938124585e7a43c8572753011b9522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964916
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892673}

--- components/autofill/PRESUBMIT.py
+++ components/autofill/PRESUBMIT.py
@@ -8,6 +8,8 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into depot_tools.
 """
 
+USE_PYTHON3 = True
+
 def _CheckNoBaseTimeCalls(input_api, output_api):
   """Checks that no files call base::Time::Now() or base::TimeTicks::Now()."""
   pattern = input_api.re.compile(
