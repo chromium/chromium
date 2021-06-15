369254712d22dd91af90f8c0a1a069daa7645ff8
Shane Fitzpatrick <shanefitz@google.com>
2021-06-15 01:40:26 +0000

    [FastPair] Add QP_LOG() logging macro
    This change adds a log MACRO for Quick Pair implementations (e.g. Fast
    Pair).
    
    The logic here matches the logic we've used for our other similar scenarios,
    (NS_LOG and PA_LOG). I've filed crbug/1219790 as a tech debt item to refactor
    this pattern into something that can be shared across all our features.
    
    Change-Id: I2cf2f32152cd604c3cac6af382dd59bb28cd4ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960724
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892375}

---
+++ chromeos/components/quick_pair/common/logging.h
@@ -0,0 +1,61 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_QUICK_PAIR_COMMON_LOGGING_H_
+#define CHROMEOS_COMPONENTS_QUICK_PAIR_COMMON_LOGGING_H_
+
+#include <sstream>
+
+#include "base/logging.h"
+
+namespace chromeos {
+namespace quick_pair {
+
+// Use the QP_LOG() macro for all logging related to Quick Pair implementations
+// (e.g. Fast Pair), so the system is aware of all logs related to this feature.
+// We display these logs in the debug WebUI (chrome://nearby-internals).
+//
+// QP_LOG() has the same interface as the standard LOG() macro and also creates
+// a normal log message of the same severity.
+// Examples:
+//   QP_LOG(INFO) << "Waiting for " << x << " pending requests.";
+//   QP_LOG(ERROR) << "Request failed: " << error_string;
+#define QP_LOG(severity)                                          \
+  chromeos::quick_pair::ScopedLogMessage(__FILE__, __LINE__,      \
+                                         logging::LOG_##severity) \
+      .stream()
+
+// Disables all logging while in scope. Intended to be called only from test
+// code, to clean up test output.
+class ScopedDisableLoggingForTesting {
+ public:
+  ScopedDisableLoggingForTesting();
+  ~ScopedDisableLoggingForTesting();
+};
+
+// An intermediate object used by the QP_LOG macro, wrapping a
+// logging::LogMessage instance. When this object is destroyed, the message will
+// be logged with the standard logging system and also added to Proximity Auth
+// specific log buffer.  You should use the QP_LOG() macro instead of this class
+// directly.
+class ScopedLogMessage {
+ public:
+  ScopedLogMessage(const char* file, int line, logging::LogSeverity severity);
+  ScopedLogMessage(const ScopedLogMessage&) = delete;
+  ScopedLogMessage& operator=(const ScopedLogMessage&) = delete;
+  ~ScopedLogMessage();
+
+  std::ostream& stream() { return stream_; }
+
+ private:
+  const char* file_;
+  int line_;
+  logging::LogSeverity severity_;
+  std::ostringstream stream_;
+};
+
+}  // namespace quick_pair
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_QUICK_PAIR_COMMON_LOGGING_H_
