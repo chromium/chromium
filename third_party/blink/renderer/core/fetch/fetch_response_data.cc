chromium/chromium

commit 22a2236d2969b1ff48ae3a642eaae241ffabdd28

[Credentialless] Block credentialled opaque response (CacheStorage)
Discussion here:
https://github.com/w3c/ServiceWorker/issues/1592

+ Plumb response's |request_include_credentials|
+ Modify CORP check to prevent vulnerable credentialled opaque responses
  from entering COEP:credentialless document via CacheStorage.

Change-Id: Ieaffd379da43904ba9b5dfe364489ef02ec20b2e
Bug: 1175099
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886899
Reviewed-by: Mike West <mkwst@chromium.org>
Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
Cr-Commit-Position: refs/heads/master@{#889719}

third_party/blink/renderer/core/fetch/fetch_response_data.cc
@@ -174,6 +174,11 @@ String FetchResponseData::InternalMIMEType() const {
   return mime_type_;
 }
 
+bool FetchResponseData::RequestIncludeCredentials() const {
+  return internal_response_ ? internal_response_->RequestIncludeCredentials()
+                            : request_include_credentials_;
+}
+
 void FetchResponseData::SetURLList(const Vector<KURL>& url_list) {
   url_list_ = url_list;
 }
@@ -208,6 +213,7 @@ FetchResponseData* FetchResponseData::Clone(ScriptState* script_state,
   new_response->alpn_negotiated_protocol_ = alpn_negotiated_protocol_;
   new_response->was_fetched_via_spdy_ = was_fetched_via_spdy_;
   new_response->has_range_requested_ = has_range_requested_;
+  new_response->request_include_credentials_ = request_include_credentials_;
   if (auth_challenge_info_) {
     new_response->auth_challenge_info_ =
         std::make_unique<net::AuthChallengeInfo>(*auth_challenge_info_);
@@ -286,6 +292,7 @@ mojom::blink::FetchAPIResponsePtr FetchResponseData::PopulateFetchAPIResponse(
   response->alpn_negotiated_protocol = alpn_negotiated_protocol_;
   response->was_fetched_via_spdy = was_fetched_via_spdy_;
   response->has_range_requested = has_range_requested_;
+  response->request_include_credentials = request_include_credentials_;
   for (const auto& header : HeaderList()->List())
     response->headers.insert(header.first, header.second);
   response->parsed_headers = ParseHeaders(
@@ -367,6 +374,7 @@ void FetchResponseData::InitFromResourceResponse(
   }
 
   SetAuthChallengeInfo(response.AuthChallengeInfo());
+  SetRequestIncludeCredentials(response.RequestIncludeCredentials());
 }
 
 FetchResponseData::FetchResponseData(Type type,
@@ -391,6 +399,12 @@ void FetchResponseData::SetAuthChallengeInfo(
     auth_challenge_info_ =
         std::make_unique<net::AuthChallengeInfo>(*auth_challenge_info);
   }
+}
+
+void FetchResponseData::SetRequestIncludeCredentials(
+    bool request_include_credentials) {
+  DCHECK(!internal_response_);
+  request_include_credentials_ = request_include_credentials;
 }
 
 void FetchResponseData::ReplaceBodyStreamBuffer(BodyStreamBuffer* buffer) {

third_party/blink/renderer/core/fetch/fetch_response_data.h
@@ -84,6 +84,7 @@ class CORE_EXPORT FetchResponseData final
     return cors_exposed_header_names_;
   }
   bool HasRangeRequested() const { return has_range_requested_; }
+  bool RequestIncludeCredentials() const;
 
   int64_t GetPadding() const { return padding_; }
   void SetPadding(int64_t padding) { padding_ = padding; }
@@ -126,6 +127,7 @@ class CORE_EXPORT FetchResponseData final
   }
   void SetAuthChallengeInfo(
       const absl::optional<net::AuthChallengeInfo>& auth_challenge_info);
+  void SetRequestIncludeCredentials(bool request_include_credentials);
 
   // If the type is Default, replaces |buffer_|.
   // If the type is Basic or CORS, replaces |buffer_| and
@@ -171,6 +173,11 @@ class CORE_EXPORT FetchResponseData final
   // |auth_challenge_info_| is a std::unique_ptr instead of absl::optional
   // |because this member is empty in most cases.
   std::unique_ptr<net::AuthChallengeInfo> auth_challenge_info_;
+
+  // The request's |includeCredentials| value from the "HTTP-network fetch"
+  // algorithm.
+  // See: https://fetch.spec.whatwg.org/#concept-http-network-fetch
+  bool request_include_credentials_ = true;
 
   DISALLOW_COPY_AND_ASSIGN(FetchResponseData);
 };

third_party/blink/renderer/platform/loader/fetch/resource_response.h
@@ -520,6 +520,13 @@ class PLATFORM_EXPORT ResourceResponse final {
     auth_challenge_info_ = value;
   }
 
+  bool RequestIncludeCredentials() const {
+    return request_include_credentials_;
+  }
+  void SetRequestIncludeCredentials(bool request_include_credentials) {
+    request_include_credentials_ = request_include_credentials;
+  }
+
  private:
   void UpdateHeaderParsedState(const AtomicString& name);
 
@@ -710,6 +717,11 @@ class PLATFORM_EXPORT ResourceResponse final {
   KURL web_bundle_url_;
 
   absl::optional<net::AuthChallengeInfo> auth_challenge_info_;
+
+  // The request's |includeCredentials| value from the "HTTP-network fetch"
+  // algorithm.
+  // See: https://fetch.spec.whatwg.org/#concept-http-network-fetch
+  bool request_include_credentials_ = true;
 };
 
 }  // namespace blink

content/browser/cache_storage/cache_storage.proto
@@ -58,6 +58,7 @@ message CacheResponse {
   optional string request_method = 14;
   optional int64 padding = 15;
   optional int64 side_data_padding = 16;
+  optional bool request_include_credentials = 17;
 }
 
 message CacheMetadata {

content/browser/cache_storage/cache_storage_cache_unittest.cc
@@ -680,7 +680,8 @@ class CacheStorageCacheTest : public testing::Test {
         net::HttpResponseInfo::CONNECTION_INFO_UNKNOWN,
         /*alpn_negotiated_protocol=*/"unknown",
         /*was_fetched_via_spdy=*/false, /*has_range_requested=*/false,
-        /*auth_challenge_info=*/absl::nullopt);
+        /*auth_challenge_info=*/absl::nullopt,
+        /*request_include_credentials=*/true);
   }
 
   void CopySideDataToResponse(const std::string& uuid,

content/browser/cache_storage/cache_storage_dispatcher_host.cc
@@ -152,7 +152,8 @@ bool ResponseBlockedByCrossOriginResourcePolicy(
              response->url_list.back(), response->url_list.front(),
              document_origin, corp_header_value, RequestMode::kNoCors,
              document_origin, network::mojom::RequestDestination::kEmpty,
-             document_coep, coep_reporter ? coep_reporter.get() : nullptr)
+             response->request_include_credentials, document_coep,
+             coep_reporter ? coep_reporter.get() : nullptr)
       .has_value();
 }
 

content/browser/cache_storage/cache_storage_manager_unittest.cc
@@ -653,7 +653,8 @@ class CacheStorageManagerTest : public testing::Test {
         net::HttpResponseInfo::CONNECTION_INFO_UNKNOWN,
         /*alpn_negotiated_protocol=*/"unknown",
         /*was_fetched_via_spdy=*/false, /*has_range_requested=*/false,
-        /*auth_challenge_info=*/absl::nullopt);
+        /*auth_challenge_info=*/absl::nullopt,
+        /*request_include_credentials=*/true);
 
     blink::mojom::BatchOperationPtr operation =
         blink::mojom::BatchOperation::New();

content/browser/cache_storage/legacy/legacy_cache_storage_cache.cc
@@ -441,6 +441,11 @@ blink::mojom::FetchAPIResponsePtr CreateResponse(
     padding = storage::ComputeRandomResponsePadding();
   }
 
+  bool request_include_credentials =
+      metadata.response().has_request_include_credentials()
+          ? metadata.response().request_include_credentials()
+          : true;
+
   // Note that |has_range_requested| can be safely set to false since it only
   // affects HTTP 206 (Partial) responses, which are blocked from cache storage.
   // See https://fetch.spec.whatwg.org/#main-fetch for usage of
@@ -462,7 +467,8 @@ blink::mojom::FetchAPIResponsePtr CreateResponse(
       static_cast<net::HttpResponseInfo::ConnectionInfo>(
           metadata.response().connection_info()),
       alpn_negotiated_protocol, metadata.response().was_fetched_via_spdy(),
-      /*has_range_requested=*/false, /*auth_challenge_info=*/absl::nullopt);
+      /*has_range_requested=*/false, /*auth_challenge_info=*/absl::nullopt,
+      request_include_credentials);
 }
 
 int64_t CalculateSideDataPadding(
@@ -1929,6 +1935,8 @@ void LegacyCacheStorageCache::PutDidCreateEntry(
         storage_key_, response_metadata, put_context->side_data_blob_size);
   }
   response_metadata->set_side_data_padding(side_data_padding);
+  response_metadata->set_request_include_credentials(
+      put_context->response->request_include_credentials);
 
   // Get a temporary copy of the entry pointer before passing it in base::Bind.
   disk_cache::Entry* temp_entry_ptr = put_context->cache_entry.get();

commit 21519b148d9c895b6678e59e277e825f31039181

Pack bools bits together to recover from memory regression.
The previous patch:
https://chromium.googlesource.com/chromium/src/+/22a2236d2969b1ff48ae3a642eaae241ffabdd28
caused a memory regression.

A memory regression caused by adding a simple bool? This is weird.
Let's pack it with other bool!

struct           | sizeof (before)| sizeof (after)
-----------------|----------------|---------------
ResourceResponse | 1080           | 1048
FetchResponseData| 208            | 200

This patch recovers from the regression.
On the benchmark: system_health.memory_mobile
It reduces by 11% the blink_gc:effective_size.

Pinpoint:
https://pinpoint-dot-chromeperf.appspot.com/job/13df762c320000

Bug: 1217266
Change-Id: I54f4a5626107724c2d6186d4eeb8e9265aff1e52
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947501
Reviewed-by: Egor Pasko <pasko@chromium.org>
Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
Cr-Commit-Position: refs/heads/master@{#892584}

third_party/blink/renderer/core/fetch/fetch_response_data.cc
@@ -391,7 +391,8 @@ FetchResponseData::FetchResponseData(Type type,
       connection_info_(net::HttpResponseInfo::CONNECTION_INFO_UNKNOWN),
       alpn_negotiated_protocol_("unknown"),
       was_fetched_via_spdy_(false),
-      has_range_requested_(false) {}
+      has_range_requested_(false),
+      request_include_credentials_(true) {}
 
 void FetchResponseData::SetAuthChallengeInfo(
     const absl::optional<net::AuthChallengeInfo>& auth_challenge_info) {

third_party/blink/renderer/core/fetch/fetch_response_data.h
@@ -169,16 +169,16 @@ class CORE_EXPORT FetchResponseData final
   HTTPHeaderSet cors_exposed_header_names_;
   net::HttpResponseInfo::ConnectionInfo connection_info_;
   AtomicString alpn_negotiated_protocol_;
-  bool was_fetched_via_spdy_;
-  bool has_range_requested_;
   // |auth_challenge_info_| is a std::unique_ptr instead of absl::optional
   // |because this member is empty in most cases.
   std::unique_ptr<net::AuthChallengeInfo> auth_challenge_info_;
 
+  bool was_fetched_via_spdy_ : 1;
+  bool has_range_requested_ : 1;
   // The request's |includeCredentials| value from the "HTTP-network fetch"
   // algorithm.
   // See: https://fetch.spec.whatwg.org/#concept-http-network-fetch
-  bool request_include_credentials_ = true;
+  bool request_include_credentials_ : 1;
 };
 
 }  // namespace blink

third_party/blink/renderer/platform/loader/fetch/resource_response.cc
@@ -82,10 +82,37 @@ ResourceResponse::SignedCertificateTimestamp::IsolatedCopy() const {
       signature_data_.IsolatedCopy());
 }
 
-ResourceResponse::ResourceResponse() : is_null_(true) {}
+ResourceResponse::ResourceResponse()
+    : was_cached_(false),
+      connection_reused_(false),
+      is_null_(true),
+      have_parsed_age_header_(false),
+      have_parsed_date_header_(false),
+      have_parsed_expires_header_(false),
+      have_parsed_last_modified_header_(false),
+      has_major_certificate_errors_(false),
+      is_legacy_tls_version_(false),
+      has_range_requested_(false),
+      timing_allow_passed_(false),
+      was_fetched_via_spdy_(false),
+      was_fetched_via_service_worker_(false),
+      did_service_worker_navigation_preload_(false),
+      async_revalidation_requested_(false),
+      is_signed_exchange_inner_response_(false),
+      was_in_prefetch_cache_(false),
+      was_cookie_in_request_(false),
+      network_accessed_(false),
+      from_archive_(false),
+      was_alternate_protocol_available_(false),
+      was_alpn_negotiated_(false),
+      has_authorization_covered_by_wildcard_on_preflight_(false),
+      is_validated_(false),
+      request_include_credentials_(true) {}
 
 ResourceResponse::ResourceResponse(const KURL& current_request_url)
-    : current_request_url_(current_request_url), is_null_(false) {}
+    : ResourceResponse() {
+  SetCurrentRequestUrl(current_request_url);
+}
 
 ResourceResponse::ResourceResponse(const ResourceResponse&) = default;
 ResourceResponse& ResourceResponse::operator=(const ResourceResponse&) =

third_party/blink/renderer/platform/loader/fetch/resource_response.h
@@ -553,92 +553,100 @@ class PLATFORM_EXPORT ResourceResponse final {
   network::mojom::IPAddressSpace address_space_ =
       network::mojom::IPAddressSpace::kUnknown;
 
-  bool was_cached_ = false;
-  bool connection_reused_ = false;
-  bool is_null_ = false;
-  mutable bool have_parsed_age_header_ = false;
-  mutable bool have_parsed_date_header_ = false;
-  mutable bool have_parsed_expires_header_ = false;
-  mutable bool have_parsed_last_modified_header_ = false;
-
-  // True if the resource was retrieved by the embedder in spite of
-  // certificate errors.
-  bool has_major_certificate_errors_ = false;
-
   // The Certificate Transparency policy compliance status of the resource.
   CTPolicyCompliance ct_policy_compliance_ =
       kCTPolicyComplianceDetailsNotAvailable;
 
+  bool was_cached_ : 1;
+  bool connection_reused_ : 1;
+  bool is_null_ : 1;
+  mutable bool have_parsed_age_header_ : 1;
+  mutable bool have_parsed_date_header_ : 1;
+  mutable bool have_parsed_expires_header_ : 1;
+  mutable bool have_parsed_last_modified_header_ : 1;
+
+  // True if the resource was retrieved by the embedder in spite of
+  // certificate errors.
+  bool has_major_certificate_errors_ : 1;
+
   // True if the response was sent over TLS 1.0 or 1.1, which are deprecated and
   // will be removed in the future.
-  bool is_legacy_tls_version_ = false;
+  bool is_legacy_tls_version_ : 1;
 
   // This corresponds to the range-requested flag in the Fetch spec:
   // https://fetch.spec.whatwg.org/#concept-response-range-requested-flag
-  bool has_range_requested_ = false;
+  bool has_range_requested_ : 1;
 
   // True if the Timing-Allow-Origin check passes.
   // https://fetch.spec.whatwg.org/#concept-response-timing-allow-passed
-  bool timing_allow_passed_ = false;
+  bool timing_allow_passed_ : 1;
+
+  // Was the resource fetched over SPDY.  See http://dev.chromium.org/spdy
+  bool was_fetched_via_spdy_ : 1;
+
+  // Was the resource fetched over a ServiceWorker.
+  bool was_fetched_via_service_worker_ : 1;
+
+  // True if service worker navigation preload was performed due to
+  // the request for this resource.
+  bool did_service_worker_navigation_preload_ : 1;
+
+  // True if this resource is stale and needs async revalidation. Will only
+  // possibly be set if the load_flags indicated SUPPORT_ASYNC_REVALIDATION.
+  bool async_revalidation_requested_ : 1;
+
+  // True if this resource is from an inner response of a signed exchange.
+  // https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html
+  bool is_signed_exchange_inner_response_ : 1;
+
+  // True if this resource is served from the prefetch cache.
+  bool was_in_prefetch_cache_ : 1;
+
+  // True if a cookie was sent in the request for this resource.
+  bool was_cookie_in_request_ : 1;
+
+  // True if this resource was loaded from the network.
+  bool network_accessed_ : 1;
+
+  // True if this resource was loaded from a MHTML archive.
+  bool from_archive_ : 1;
+
+  // True if response could use alternate protocol.
+  bool was_alternate_protocol_available_ : 1;
+
+  // True if the response was delivered after ALPN is negotiated.
+  bool was_alpn_negotiated_ : 1;
+
+  // True when there is an "authorization" header on the request and it is
+  // covered by the wildcard in the preflight response.
+  // TODO(crbug.com/1176753): Remove this once the investigation is done.
+  bool has_authorization_covered_by_wildcard_on_preflight_ : 1;
+
+  // Whether the resource came from the cache and validated over the network.
+  bool is_validated_ : 1;
+
+  // The request's |includeCredentials| value from the "HTTP-network fetch"
+  // algorithm.
+  // See: https://fetch.spec.whatwg.org/#concept-http-network-fetch
+  bool request_include_credentials_ : 1;
+
+  // Pre-computed padding.  This should only be non-zero if |response_type| is
+  // set to kOpaque.  In addition, it is only set if the response was provided
+  // by a service worker FetchEvent handler.
+  int64_t padding_ = 0;
 
   // The time at which the resource's certificate expires. Null if there was no
   // certificate.
   base::Time cert_validity_start_;
-
-  // Was the resource fetched over SPDY.  See http://dev.chromium.org/spdy
-  bool was_fetched_via_spdy_ = false;
-
-  // Was the resource fetched over a ServiceWorker.
-  bool was_fetched_via_service_worker_ = false;
 
   // The source of the resource, if it was fetched via ServiceWorker. This is
   // kUnspecified if |was_fetched_via_service_worker| is false.
   network::mojom::FetchResponseSource service_worker_response_source_ =
       network::mojom::FetchResponseSource::kUnspecified;
 
-  // True if service worker navigation preload was performed due to
-  // the request for this resource.
-  bool did_service_worker_navigation_preload_ = false;
-
-  // True if this resource is stale and needs async revalidation. Will only
-  // possibly be set if the load_flags indicated SUPPORT_ASYNC_REVALIDATION.
-  bool async_revalidation_requested_ = false;
-
-  // True if this resource is from an inner response of a signed exchange.
-  // https://wicg.github.io/webpackage/draft-yasskin-http-origin-signed-responses.html
-  bool is_signed_exchange_inner_response_ = false;
-
-  // True if this resource is served from the prefetch cache.
-  bool was_in_prefetch_cache_ = false;
-
-  // True if a cookie was sent in the request for this resource.
-  bool was_cookie_in_request_ = false;
-
-  // True if this resource was loaded from the network.
-  bool network_accessed_ = false;
-
-  // True if this resource was loaded from a MHTML archive.
-  bool from_archive_ = false;
-
-  // True if response could use alternate protocol.
-  bool was_alternate_protocol_available_ = false;
-
-  // True if the response was delivered after ALPN is negotiated.
-  bool was_alpn_negotiated_ = false;
-
-  // True when there is an "authorization" header on the request and it is
-  // covered by the wildcard in the preflight response.
-  // TODO(crbug.com/1176753): Remove this once the investigation is done.
-  bool has_authorization_covered_by_wildcard_on_preflight_ = false;
-
   // https://fetch.spec.whatwg.org/#concept-response-type
   network::mojom::FetchResponseType response_type_ =
       network::mojom::FetchResponseType::kDefault;
-
-  // Pre-computed padding.  This should only be non-zero if |response_type| is
-  // set to kOpaque.  In addition, it is only set if the response was provided
-  // by a service worker FetchEvent handler.
-  int64_t padding_ = 0;
 
   // HTTP version used in the response, if known.
   HTTPVersion http_version_ = kHTTPVersionUnknown;
@@ -695,9 +703,6 @@ class PLATFORM_EXPORT ResourceResponse final {
   net::HttpResponseInfo::ConnectionInfo connection_info_ =
       net::HttpResponseInfo::ConnectionInfo::CONNECTION_INFO_UNKNOWN;
 
-  // Whether the resource came from the cache and validated over the network.
-  bool is_validated_ = false;
-
   // Size of the response in bytes prior to decompression.
   int64_t encoded_data_length_ = 0;
 
@@ -723,11 +728,6 @@ class PLATFORM_EXPORT ResourceResponse final {
   KURL web_bundle_url_;
 
   absl::optional<net::AuthChallengeInfo> auth_challenge_info_;
-
-  // The request's |includeCredentials| value from the "HTTP-network fetch"
-  // algorithm.
-  // See: https://fetch.spec.whatwg.org/#concept-http-network-fetch
-  bool request_include_credentials_ = true;
 };
 
 }  // namespace blink