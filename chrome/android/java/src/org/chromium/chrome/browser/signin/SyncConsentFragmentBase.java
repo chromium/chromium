83f87e54ac100c2808ce5849974dffb939274d0b
Alice Wang <aliceywang@chromium.org>
2021-06-16 07:29:03 +0000

    [Android] Stop setting the dialog coordinator to null on sync consent screen
    This CL stops setting the dialog coordinator to null on the sync
    consent screen.
    
    Bug: 1217086
    Change-Id: I0b8f9cee9d823cd0297138a4baa0922e26139106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2962374
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892908}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
@@ -457,7 +457,6 @@ public abstract class SyncConsentFragmentBase
     public void onAccountSelected(String accountName, boolean isDefaultAccount) {
         selectAccount(accountName, isDefaultAccount);
         mAccountPickerDialogCoordinator.dismissDialog();
-        mAccountPickerDialogCoordinator = null;
     }
 
     @Override
@@ -484,10 +483,8 @@ public abstract class SyncConsentFragmentBase
             if (addedAccountName == null) return;
 
             // Found the account name, dismiss the dialog if it is shown
-            if (mAccountPickerDialogCoordinator != null) {
-                mAccountPickerDialogCoordinator.dismissDialog();
-                mAccountPickerDialogCoordinator = null;
-            }
+            mAccountPickerDialogCoordinator.dismissDialog();
+
             // Wait for the account cache to be updated and select newly-added account.
             mAccountSelectionPending = true;
             mRequestedAccountName = addedAccountName;
