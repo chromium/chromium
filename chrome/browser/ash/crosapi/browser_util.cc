9e869cd85733da2add1f2b342ac3cf5932348e28
Igor <igorcov@chromium.org>
2021-06-16 09:22:25 +0000

    Propagate Device DM Token to Lacros
    There are some device specific static data like device DM token, or
    serial_number that are needed in Lacros. In this CL, just the
    device DM token is passed. But the plan is to include in this
    structure other device data in the future.
    
    As a static data that is device specific, it's enough to pass it in
    BrowserInitParams. Then Lacros can use it anywhere.
    
    BUG=1201582
    TEST=None
    
    Change-Id: If6a1f3c3561e27ad7bdec7cb71074a895c4968f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960852
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892938}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -28,6 +28,7 @@
 #include "chrome/browser/ash/crosapi/idle_service_ash.h"
 #include "chrome/browser/ash/crosapi/native_theme_service_ash.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
@@ -201,6 +202,21 @@ absl::optional<std::vector<uint8_t>> GetDeviceAccountPolicy(
   std::string policy_data = environment_provider->GetDeviceAccountPolicy();
   return std::vector<uint8_t>(policy_data.begin(), policy_data.end());
 }
+
+// Returns the device specific data needed for Lacros.
+mojom::DevicePropertiesPtr GetDeviceProperties() {
+  mojom::DevicePropertiesPtr result = mojom::DeviceProperties::New();
+  if (ash::DeviceSettingsService::IsInitialized() &&
+      ash::DeviceSettingsService::Get()->policy_data() &&
+      ash::DeviceSettingsService::Get()->policy_data()->has_request_token()) {
+    result->device_dm_token =
+        ash::DeviceSettingsService::Get()->policy_data()->request_token();
+  } else {
+    result->device_dm_token = "";
+  }
+
+  return result;
+}  // namespace
 
 struct InterfaceVersionEntry {
   base::Token uuid;
@@ -597,6 +613,7 @@ mojom::BrowserInitParamsPtr GetBrowserInitParams(
   params->web_apps_enabled =
       base::FeatureList::IsEnabled(features::kWebAppsCrosapi);
   params->standalone_browser_is_primary = IsLacrosPrimaryBrowser();
+  params->device_properties = GetDeviceProperties();
 
   return params;
 }
