cb3fc3d6bb741f3aec2d5bf248cca49cc8fbcb80
Yue Zhang <yuezhanggg@chromium.org>
2021-06-16 04:34:59 +0000

    [ChromeCart] Fix AddToCart false positives for some sites
    (cherry picked from commit c5e03d26f7acf09848a6be1b346ec3913a5f55d4)
    
    Bug: 1212229
    Change-Id: I97ec89a3cad09ca8455575ec9ee770933852ee94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935805
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#889038}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965576
    Cr-Commit-Position: refs/branch-heads/4472@{#1488}
    Cr-Branched-From: 3d60439cfb36485e76a1c5bb7f513d3721b20da1-refs/heads/master@{#870763}

--- chrome/renderer/cart/commerce_hint_agent.cc
+++ chrome/renderer/cart/commerce_hint_agent.cc
@@ -53,6 +53,12 @@ std::string eTLDPlusOne(const GURL& url) {
       url, net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 }
 
+bool IsCartHeuristicsImprovementEnabled() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      ntp_features::kNtpChromeCartModule,
+      "NtpChromeCartModuleHeuristicsImprovementParam", false);
+}
+
 enum class CommerceEvent {
   kAddToCartByForm,
   kAddToCartByURL,
@@ -322,6 +328,29 @@ void DetectAddToCart(content::RenderFrame* render_frame,
     return;
   if (navigation_url.DomainIs("hsn.com") && url.DomainIs("granify.com"))
     return;
+
+  if (IsCartHeuristicsImprovementEnabled()) {
+    if (navigation_url.DomainIs("abebooks.com"))
+      return;
+    if (navigation_url.DomainIs("abercrombie.com"))
+      return;
+    if (navigation_url.DomainIs(kAmazonDomain) &&
+        url.host() != "fls-na.amazon.com")
+      return;
+    if (navigation_url.DomainIs("bestbuy.com"))
+      return;
+    if (navigation_url.DomainIs("containerstore.com"))
+      return;
+    if (navigation_url.DomainIs("gap.com") && url.DomainIs("granify.com"))
+      return;
+    if (navigation_url.DomainIs("kohls.com"))
+      return;
+    if (navigation_url.DomainIs("officedepot.com") &&
+        url.DomainIs("chatid.com"))
+      return;
+    if (navigation_url.DomainIs("pier1.com"))
+      return;
+  }
 
   blink::WebHTTPBody body = request.HttpBody();
   if (body.IsNull())
