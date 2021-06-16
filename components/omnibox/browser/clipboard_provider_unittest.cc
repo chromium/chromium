e0eaea1ae7a9ce7c2b843e607b6e48155c03e0b0
Gang Wu <gangwu@chromium.org>
2021-06-16 23:24:29 +0000

    Android: Enabled by default for ClipboardSuggestionContentHidden
    This is a feature related to Android S, so we need to be default enabled
    and merge back to M91 before the final system image drop, which will be
    happen at June/22nd.
    This is a privacy feature, so the experiment is only for sanity check.
    And the feature is approved for full launch in crbug.com/1198746
    
    (cherry picked from commit 8c86cdf54638ec9ee78ce2df8b2e83fbec71b387)
    
    Bug: 1211860
    Change-Id: I06009cc23014e880b408f4dca48bb1d6a9b84462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2959133
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#892741}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967055
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Commit-Queue: Ben Mason <benmason@chromium.org>
    Owners-Override: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#689}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- components/omnibox/browser/clipboard_provider_unittest.cc
+++ components/omnibox/browser/clipboard_provider_unittest.cc
@@ -153,6 +153,11 @@ TEST_F(ClipboardProviderTest, EmptyClipboard) {
   EXPECT_TRUE(provider_->matches().empty());
 }
 
+#if !defined(OS_ANDROID)
+// The following tests do not apply to Android.
+// On Android, the Omnibox won't access the content of the system clipboard
+// before users click the reveal button, so the clipboard suggestions will be
+// empty on start.
 TEST_F(ClipboardProviderTest, ClipboardIsCurrentURL) {
   auto template_url_service = std::make_unique<TemplateURLService>(
       /*initializers=*/nullptr, /*count=*/0);
@@ -211,6 +216,7 @@ TEST_F(ClipboardProviderTest, MatchesImage) {
   ASSERT_TRUE(matches_image_match_);
   EXPECT_EQ(AutocompleteMatchType::CLIPBOARD_IMAGE, matches_image_match_->type);
 }
+#endif  // !defined(OS_ANDROID)
 
 TEST_F(ClipboardProviderTest, DeleteMatch) {
   auto template_url_service = std::make_unique<TemplateURLService>(
