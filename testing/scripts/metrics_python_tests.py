4c9955b1c31b725372e61590c595522cb4e63451
Dirk Pranke <dpranke@google.com>
2021-06-16 21:13:49 +0000

    Move metrics_python_tests from isolated_script to script.
    This changes how we invoke metrics_python_tests, so that
    it is invoked as a 'script' test and run directly on the builder
    (machine doing the compile) rather than as an 'isolated_script'
    test under swarming.
    
    This fixes an issue where we'd try to run `dirmd`, which requires
    a checkout.
    
    Bug: 1201822
    Change-Id: I17c4f6c2341c70b688efc48aada88a5a51ef4a16
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2921324
    Auto-Submit: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893147}

---
+++ testing/scripts/metrics_python_tests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs //tools/metrics/metrics_python_tests.py as a 'script' test."""
+
+import json
+import os
+import sys
+
+
+import common
+
+
+def main_run(args):
+  with common.temporary_file() as tempfile_path:
+    rc = common.run_command(['vpython',
+        os.path.join(common.SRC_DIR, 'testing', 'test_env.py'),
+        os.path.join(common.SRC_DIR, 'tools', 'metrics',
+                     'metrics_python_tests.py'),
+        '--isolated-script-test-output', tempfile_path
+    ], cwd=os.path.join(common.SRC_DIR, 'out', args.build_config_fs))
+
+    with open(tempfile_path) as f:
+      isolated_results = json.load(f)
+
+  results = common.parse_common_test_results(isolated_results,
+                                             test_separator='.')
+
+  json.dump({
+      'valid': True,
+      'failures': ['%s: %s' % (k, v)
+                   for k, v in results['unexpected_failures'].items()]
+  }, args.output)
+
+  return rc
+
+
+def main_compile_targets(args):
+  json.dump([], args.output)
+
+
+if __name__ == '__main__':
+  funcs = {
+    'run': main_run,
+    'compile_targets': main_compile_targets,
+  }
+  sys.exit(common.run_script(sys.argv[1:], funcs))
--- tools/metrics/metrics_python_tests.py
+++ tools/metrics/metrics_python_tests.py
@@ -21,22 +21,33 @@ def resolve(*paths):
 
 
 sys.exit(
-    typ.main(tests=resolve(
-        'actions/extract_actions_test.py',
-        'histograms/expand_owners_unittest.py',
-        'histograms/extract_histograms_test.py',
-        'histograms/generate_expired_histograms_array_unittest.py',
-        'histograms/pretty_print_test.py',
-        'ukm/codegen_test.py',
-        'ukm/gen_builders_test.py',
-        'ukm/ukm_model_test.py',
-        'ukm/xml_validations_test.py',
-        '../json_comment_eater/json_comment_eater_test.py',
-        '../json_to_struct/element_generator_test.py',
-        '../json_to_struct/struct_generator_test.py',
-        '../variations/fieldtrial_to_struct_unittest.py',
-        '../variations/fieldtrial_util_unittest.py',
-        '../variations/split_variations_cmd_unittest.py',
-        '../../components/variations/service/'
-        'generate_ui_string_overrider_unittest.py',
-    )))
+    typ.main(
+        tests=resolve(
+            'actions/extract_actions_test.py',
+
+            # TODO(crbug.com/1220251) - the test ordering is very sensitive due to
+            # potential name collisions between ukm/pretty_print.py and
+            # histograms/pretty_print.py and the implementation in typ.
+            #
+            # Until this issue is fixed, best to ensure that the 'histograms' files
+            # show up *after* the 'ukm' files (in order for the histograms
+            # directory to be added to sys.path *before* ukm), and that we run the
+            # tests in a single process (jobs=1, below).
+            'ukm/codegen_test.py',
+            'ukm/gen_builders_test.py',
+            'ukm/ukm_model_test.py',
+            'ukm/xml_validations_test.py',
+            'histograms/expand_owners_unittest.py',
+            'histograms/extract_histograms_test.py',
+            'histograms/generate_expired_histograms_array_unittest.py',
+            'histograms/pretty_print_test.py',
+            '../json_comment_eater/json_comment_eater_test.py',
+            '../json_to_struct/element_generator_test.py',
+            '../json_to_struct/struct_generator_test.py',
+            '../variations/fieldtrial_to_struct_unittest.py',
+            '../variations/fieldtrial_util_unittest.py',
+            '../variations/split_variations_cmd_unittest.py',
+            '../../components/variations/service/'
+            'generate_ui_string_overrider_unittest.py',
+        ),
+        jobs=1))
