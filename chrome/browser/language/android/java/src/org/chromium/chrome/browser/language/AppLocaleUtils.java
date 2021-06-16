97e4e9a788f86da12ed337032f1e185cc61fc35f
Trevor Perrier <perrier@chromium.org>
2021-06-16 18:54:44 +0000

    [Android] fix isAvailableUiLanguage check
    This CL fixes a bug with LanguageItem.isAvailableUiLanguage. The issue
    is that we need to do two different types of availability checks:
    
     - Check that a locale exactly matches an available UI language.
     - Check that the base language is an available UI language.
    
    This CL splits the current isAvailableUiLanguage method into two and
    moves them to AppLocaleUtils.
    
    For an exact match "en" should not be an available language since the
    only languages we have strings for are "en-GB" and "en-US".  However,
    "fr" is an exact match since the languages we have strings for are "fr"
    and "fr-CA" (and in this case "fr-FR" would not match).
    
    For the base language match just needs to check if for a given locale
    Chrome on Android can be displayed in it. (We rely on Android to
    correctly select the strings in "en-GB" when the device locale is
    something like "en-AU" or "en-IN").
    
    Bug: 1218113
    Change-Id: I2ce6b3342a1200ab4ac9696fd56cfb1adc8bdae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953114
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893083}

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
@@ -9,8 +9,14 @@ import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import org.chromium.base.BundleUtils;
+import org.chromium.base.LocaleUtils;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
+import org.chromium.ui.base.ResourceBundle;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
 
 /**
  * Provides utility functions to assist with overriding the application language.
@@ -80,4 +86,44 @@ public class AppLocaleUtils {
             listener.onComplete(true);
         }
     }
+
+    /**
+     * Return true if the locale is an exact match for an available UI language.
+     * Note: "en" and "en-AU" will return false since the available locales are "en-GB" and "en-US".
+     * @param locale BCP-47 language tag representing a locale (e.g. "en-US")
+     */
+    public static boolean isAvailableExactUiLanguage(String locale) {
+        return isAvailableUiLanguage(locale, null);
+    }
+
+    /**
+     * Return true if this locale is available or has a reasonable fallback language that can be
+     * used for UI. For example we do not have language packs for "en" or "pt" but fallback to the
+     * reasonable alternatives "en-US" and "pt-BR". Similarly, we have no language pack for "es-MX"
+     * or "es-AR" but will use "es-419" for both. However, for languages with no translations
+     * (e.g. "yo", "cy", ect.) the fallback is "en-US" which is not reasonable.
+     * @param locale BCP-47 language tag representing a locale (e.g. "en-US")
+     */
+    public static boolean isSupportedUiLanguage(String locale) {
+        return isAvailableUiLanguage(locale, BASE_LANGUAGE_COMPARATOR);
+    }
+
+    private static boolean isAvailableUiLanguage(String locale, Comparator<String> comparator) {
+        if (Objects.equals(locale, AppLocaleUtils.SYSTEM_LANGUAGE_VALUE)) return true;
+        return Arrays.binarySearch(ResourceBundle.getAvailableLocales(), locale, comparator) >= 0;
+    }
+
+    /**
+     * Comparator that removes any country or script information from either language tag
+     * since they are not needed for locale availability checks.
+     * Example: "es-MX" and "es-ES" will evaluate as equal.
+     */
+    private static final Comparator<String> BASE_LANGUAGE_COMPARATOR = new Comparator<String>() {
+        @Override
+        public int compare(String a, String b) {
+            String langA = LocaleUtils.toLanguage(a);
+            String langB = LocaleUtils.toLanguage(b);
+            return langA.compareTo(langB);
+        }
+    };
 }
