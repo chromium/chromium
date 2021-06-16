9551005b78f35e35bc9b31936dc868e89022e250
David Bienvenu <davidbienvenu@chromium.org>
2021-06-16 16:03:43 +0000

    unit_tests: fix mock warnings in TileManagerTests
    Use NiceMock to silence warnings
    
    Change-Id: I389c457c223b1efcf21279d6b6c9f5b6e90fb8aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966758
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893026}

--- components/query_tiles/internal/tile_service_impl_unittest.cc
+++ components/query_tiles/internal/tile_service_impl_unittest.cc
@@ -27,6 +27,7 @@
 
 using testing::_;
 using ::testing::Invoke;
+using ::testing::NiceMock;
 
 namespace query_tiles {
 namespace {
@@ -83,7 +84,7 @@ class TileServiceImplTest : public testing::Test {
     auto tile_manager = std::make_unique<MockTileManager>();
     tile_manager_ = tile_manager.get();
     auto image_prefetcher = std::make_unique<MockImagePrefetcher>();
-    auto scheduler = std::make_unique<MockTileServiceScheduler>();
+    auto scheduler = std::make_unique<NiceMock<MockTileServiceScheduler>>();
     scheduler_ = scheduler.get();
     image_prefetcher_ = image_prefetcher.get();
     ON_CALL(*image_prefetcher_, Prefetch(_, _, _))
