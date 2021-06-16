55ee66a2e9fcd05a756eaefba07e1d6a7be68a9a
Elaine Chien <elainechien@chromium.org>
2021-06-16 20:05:48 +0000

    Python3Migration: content/browser/conversions/PRESUBMIT
    Bug: 1212024
    Change-Id: Ie1001e04c54766d5a80476f326602d68d175bfcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963891
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893122}

--- content/browser/conversions/PRESUBMIT.py
+++ content/browser/conversions/PRESUBMIT.py
@@ -8,6 +8,8 @@ See https://www.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
 
+USE_PYTHON3 = True
+
 def CheckConversionStorageSchemaModification(input_api, output_api):
   """ Checks the kCurrentVersionNumber is modified when necessary.
 
