96e9a1f3cb35353778269b43ca22972bb595ba64
Raymond Toy <rtoy@chromium.org>
2021-06-16 20:34:13 +0000

    Make HandleNaNValues work for Android arm32
    The intrinsic code for HandleNaNValues for Android has some serious
    errors.  It was converting floats to ints (vcvtq_u32_f32) instead of
    just casting the float to an int so we can get at the actual bits of
    the float.  Using a cast fixes this.
    
    Likewise we were converting an int to float instead of casting to a
    float type to express the int bits as a floating-point number.
    
    Bug: 1212851
    Change-Id: Ic3361998b6b9f6b06f6372c6fa73b94c299b4a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956471
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893131}

--- third_party/blink/renderer/modules/webaudio/audio_param.cc
+++ third_party/blink/renderer/modules/webaudio/audio_param.cc
@@ -267,17 +267,19 @@ static void HandleNaNValues(float* values,
   }
 #elif defined(CPU_ARM_NEON)
   if (number_of_values >= 4) {
-    uint32x4_t defaults = vcvtq_u32_f32(vdupq_n_f32(default_value));
+    uint32x4_t defaults =
+        reinterpret_cast<uint32x4_t>(vdupq_n_f32(default_value));
     for (k = 0; k < number_of_values; k += 4) {
       float32x4_t v = vld1q_f32(values + k);
       // Returns true (all ones) if v is not NaN
       uint32x4_t is_not_nan = vceqq_f32(v, v);
       // Get the parts that are not NaN
-      uint32x4_t result = vandq_u32(is_not_nan, vcvtq_u32_f32(v));
+      uint32x4_t result =
+          vandq_u32(is_not_nan, reinterpret_cast<uint32x4_t>(v));
       // Replace the parts that are NaN with the default and merge with previous
       // result.  (Note: vbic_u32(x, y) = x and not y)
       result = vorrq_u32(result, vbicq_u32(defaults, is_not_nan));
-      vst1q_f32(values + k, vcvtq_f32_u32(result));
+      vst1q_f32(values + k, reinterpret_cast<float32x4_t>(result));
     }
   }
 #endif
