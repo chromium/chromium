d8512f0c7f1dc2276d7c1be5da3d9c03350148a9
Hajime Hoshi <hajimehoshi@chromium.org>
2021-06-17 02:01:43 +0000

    Revert "assistant: Enable sandbox of Libassistant service"
    This reverts commit 5d6b07c080e80183fa3ae5907274d9a5f2536dcc.
    
    Reason for revert: crbug.com/1220954: test failures
    
    Original change's description:
    > assistant: Enable sandbox of Libassistant service
    >
    > This cl turns on the sandbox flag.
    >
    > Bug: b/155328340
    > Test: manual
    > Change-Id: I327abaafd7c412e724e50b09c3c9659b711b2419
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965655
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Commit-Queue: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#893146}
    
    Bug: b/155328340
    Change-Id: I84f57bf6e44656b5e1beb512a6fd0f69fd395270
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966704
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Owners-Override: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893273}

--- chromeos/services/assistant/public/cpp/features.cc
+++ chromeos/services/assistant/public/cpp/features.cc
@@ -55,7 +55,7 @@ const base::Feature kDisableVoiceMatch{"DisableVoiceMatch",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kEnableLibAssistantSandbox{
-    "LibAssistantSandbox", base::FEATURE_ENABLED_BY_DEFAULT};
+    "LibAssistantSandbox", base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsAppSupportEnabled() {
   return base::FeatureList::IsEnabled(
