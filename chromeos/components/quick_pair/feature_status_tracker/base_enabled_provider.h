b8d1548e41820251c4108352a4be2aa7255ae104
Shane Fitzpatrick <shanefitz@google.com>
2021-06-17 00:13:08 +0000

    [FastPair] Implement a common base class for feature enablement logic
    This change introduces BaseEnabledProvider and unit tests.
    
    This class will be used to share logic for the common scenario of
    querying and observing a "is_enabled()" boolean. Subclasses will
    implement the logic to determine that state.
    
    Change-Id: I01fa07d4f6b8f16cfc2b775b99d41476664438e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966766
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#893238}

---
+++ chromeos/components/quick_pair/feature_status_tracker/base_enabled_provider.h
@@ -0,0 +1,43 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_QUICK_PAIR_FEATURE_STATUS_TRACKER_BASE_ENABLED_PROVIDER_H_
+#define CHROMEOS_COMPONENTS_QUICK_PAIR_FEATURE_STATUS_TRACKER_BASE_ENABLED_PROVIDER_H_
+
+#include "base/callback.h"
+
+namespace chromeos {
+namespace quick_pair {
+
+// Common Base class to expose whether or not a feature is enabled.
+// Provides a |is_enabled()| method and a callback pattern to observe changes.
+//
+// Subclasses can focus on implementing the logic to determine the state of
+// |is_enabled()|, and this base class provides common functionality for
+// querying and observing that state.
+//
+// Example subclass: BluetoothEnabledProvider - Changes |is_enabled()| based on
+// the Bluetooth state on the device.
+class BaseEnabledProvider {
+ public:
+  BaseEnabledProvider();
+  BaseEnabledProvider(const BaseEnabledProvider&) = delete;
+  BaseEnabledProvider& operator=(const BaseEnabledProvider&) = delete;
+  virtual ~BaseEnabledProvider();
+
+  virtual bool is_enabled();
+  void SetCallback(base::RepeatingCallback<void(bool)> callback);
+
+ protected:
+  void SetEnabledAndInvokeCallback(bool new_value);
+
+ private:
+  bool is_enabled_;
+  base::RepeatingCallback<void(bool)> callback_;
+};
+
+}  // namespace quick_pair
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_QUICK_PAIR_FEATURE_STATUS_TRACKER_BASE_ENABLED_PROVIDER_H_
