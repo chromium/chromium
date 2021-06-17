3bf887a58d45e842cb8c67887ec3d090bd10826a
Chris Palmer <palmer@chromium.org>
2021-06-17 01:49:45 +0000

    [PartitionAlloc] Simplify `SecureMemset`.
    Including windows.h makes builds complex and slow, all for just 1
    lil function: `SecureZeroMemory`. (And we were including windows.h
    infectiously. This CL alleviates that.) It turns out we don't need
    it, and it was a bumpy special case anyway.
    
    This approach works as we intend in clang++, but we don't know if it
    works in VC++. Chromium supports and uses clang++, though.
    
    Bug: 1220098
    Change-Id: I31491a4dd0c51c56113be81eb454d605c3f09923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965266
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893271}

--- base/allocator/partition_allocator/partition_alloc-inl.h
+++ base/allocator/partition_allocator/partition_alloc-inl.h
@@ -12,10 +12,6 @@
 #include "base/allocator/partition_allocator/random.h"
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include <windows.h>
-#endif
-
 // Prefetch *x into memory.
 #if defined(__clang__) || defined(COMPILER_GCC)
 #define PA_PREFETCH(x) __builtin_prefetch(x)
@@ -30,12 +26,6 @@ namespace internal {
 // boringssl/src/crypto/mem.c. (Copying and pasting is bad, but //base can't
 // depend on //third_party, and this is small enough.)
 ALWAYS_INLINE void SecureMemset(void* ptr, uint8_t value, size_t size) {
-#if defined(OS_WIN)
-  if (value == 0) {
-    SecureZeroMemory(ptr, size);
-    return;
-  }
-#endif  // defined(OS_WIN)
   memset(ptr, value, size);
 
   // As best as we can tell, this is sufficient to break any optimisations that
