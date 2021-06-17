0e7303265a1399fcd951cd8e67706462d7a3a112
Benoit Lize <lizeb@chromium.org>
2021-06-17 14:02:59 +0000

    [shared_highlighting] Use fixed_flat_map for disabled sites.
    Change-Id: Ieff852b0a85b5721c1e36d5b2be2336d91e25d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963831
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893412}

--- components/shared_highlighting/core/common/disabled_sites.cc
+++ components/shared_highlighting/core/common/disabled_sites.cc
@@ -4,12 +4,10 @@
 
 #include "components/shared_highlighting/core/common/disabled_sites.h"
 
+#include "base/containers/fixed_flat_map.h"
 #include "base/feature_list.h"
 #include "components/shared_highlighting/core/common/shared_highlighting_features.h"
 #include "third_party/re2/src/re2/re2.h"
-
-#include <map>
-#include <utility>
 
 namespace shared_highlighting {
 
@@ -18,19 +16,19 @@ bool ShouldOfferLinkToText(const GURL& url) {
     // If a URL's host matches a key in this map, then the path will be tested
     // against the RE stored in the value. For example, {"foo.com", ".*"} means
     // any page on the foo.com domain.
-    const static std::map<std::string, std::string> kBlocklist = {
-        {"facebook.com", ".*"},
-        // TODO(crbug.com/1157981): special case this to cover other Google TLDs
-        {"google.com", "^\\/amp\\/.*"},
-        {"instagram.com", ".*"},
-        {"mail.google.com", ".*"},
-        {"outlook.live.com", ".*"},
-        {"reddit.com", ".*"},
-        {"twitter.com", ".*"},
-        {"web.whatsapp.com", ".*"},
-        {"youtube.com", ".*"},
-
-    };
+    static constexpr auto kBlocklist =
+        base::MakeFixedFlatMap<base::StringPiece, base::StringPiece>(
+            {{"facebook.com", ".*"},
+             // TODO(crbug.com/1157981): special case this to cover other Google
+             // TLDs
+             {"google.com", "^\\/amp\\/.*"},
+             {"instagram.com", ".*"},
+             {"mail.google.com", ".*"},
+             {"outlook.live.com", ".*"},
+             {"reddit.com", ".*"},
+             {"twitter.com", ".*"},
+             {"web.whatsapp.com", ".*"},
+             {"youtube.com", ".*"}});
 
     std::string domain = url.host();
     if (domain.compare(0, 4, "www.") == 0) {
@@ -41,9 +39,9 @@ bool ShouldOfferLinkToText(const GURL& url) {
       domain = domain.substr(7);
     }
 
-    auto it = kBlocklist.find(domain);
+    auto* it = kBlocklist.find(domain);
     if (it != kBlocklist.end()) {
-      return !re2::RE2::FullMatch(url.path(), it->second);
+      return !re2::RE2::FullMatch(url.path(), it->second.data());
     }
   }
   return true;
