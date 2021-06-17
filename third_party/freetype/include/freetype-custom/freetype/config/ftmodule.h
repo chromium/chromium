e6708a6e5bc0c4bfdcb1ab89e2d7f96535e59a45
Ben Wagner <bungeman@chromium.org>
2021-06-17 14:15:37 +0000

    Reorder FreeType modules
    FreeType scans modules in the order they are declared, so they should
    be ordered by frequency of use. In particular, place the smooth
    renderer module before the raster1 renderer module since more glyphs
    are rendered with antialiasing than not. Motivated by upstream change
    https://chromium.googlesource.com/chromium/src/third_party/freetype2/+/81852fbccc84d087de7cd2defdac3cb6b9d13ffe
    
    Change-Id: I86308b83893ebb1c916f85125f96ec4232eb0e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965381
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893420}

--- third_party/freetype/include/freetype-custom/freetype/config/ftmodule.h
+++ third_party/freetype/include/freetype-custom/freetype/config/ftmodule.h
@@ -15,15 +15,15 @@
 /*                                                                         */
 /***************************************************************************/
 
-FT_USE_MODULE( FT_Module_Class, autofit_module_class )
 FT_USE_MODULE( FT_Driver_ClassRec, tt_driver_class )
 FT_USE_MODULE( FT_Driver_ClassRec, cff_driver_class )
-FT_USE_MODULE( FT_Module_Class, psnames_module_class )
-FT_USE_MODULE( FT_Module_Class, pshinter_module_class )
+FT_USE_MODULE( FT_Renderer_Class, ft_smooth_renderer_class )
 FT_USE_MODULE( FT_Renderer_Class, ft_raster1_renderer_class )
 FT_USE_MODULE( FT_Module_Class, sfnt_module_class )
-FT_USE_MODULE( FT_Renderer_Class, ft_smooth_renderer_class )
+FT_USE_MODULE( FT_Module_Class, autofit_module_class )
 FT_USE_MODULE( FT_Module_Class, psaux_module_class )
+FT_USE_MODULE( FT_Module_Class, psnames_module_class )
+FT_USE_MODULE( FT_Module_Class, pshinter_module_class )
 
 #if defined(PDFIUM_REQUIRED_MODULES)
 FT_USE_MODULE( FT_Driver_ClassRec, t1_driver_class )
