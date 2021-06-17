8df1eecead920802a85f6b0ff1eaf66f02898f7f
Hajime Hoshi <hajimehoshi@chromium.org>
2021-06-17 07:09:21 +0000

    Disable FingerprintSetupTest.FingerprintEnrollLimit on Chrome OS
    TBR=achuith@chromium.org
    
    Bug: 1220967
    Change-Id: I8eb9d9ba2757d8c7d89c0ea73a078ec53d553179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966694
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893326}

--- chrome/browser/ash/login/screens/fingerprint_setup_browsertest.cc
+++ chrome/browser/ash/login/screens/fingerprint_setup_browsertest.cc
@@ -169,7 +169,13 @@ IN_PROC_BROWSER_TEST_F(FingerprintSetupTest, FingerprintEnrollFull) {
       ElementsAre(base::Bucket(static_cast<int>(UserAction::kSetupDone), 1)));
 }
 
-IN_PROC_BROWSER_TEST_F(FingerprintSetupTest, FingerprintEnrollLimit) {
+// Test failure on Chrome OS: crbug.com/1220967
+#if defined(OS_CHROMEOS)
+#define MAYBE_FingerprintEnrollLimit DISABLED_FingerprintEnrollLimit
+#else
+#define MAYBE_FingerprintEnrollLimit FingerprintEnrollLimit
+#endif
+IN_PROC_BROWSER_TEST_F(FingerprintSetupTest, MAYBE_FingerprintEnrollLimit) {
   ShowFingerprintScreen();
 
   for (int i = 0; i < kMaxAllowedFingerprints - 1; i++) {
