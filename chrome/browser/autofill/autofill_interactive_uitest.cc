2c07eb18c1737e154147d4cc744d33c52ba0e062
Christoph Schwering <schwering@google.com>
2021-06-21 11:53:07 +0000

    Revert "[Autofill] Re-enabled AutofillInteractiveTestWithHistogramTester.BasicFormFill."
    This reverts commit 584138c647bb2d29261e26b1d93ffca47facb3c9.
    
    Reason for revert: Apparently crrev.com/c/2965917 had *not*
    deflaked the test.
    
    Original change's description:
    > [Autofill] Re-enabled AutofillInteractiveTestWithHistogramTester.BasicFormFill.
    >
    > The CL crrev.com/c/2965917 may have deflaked the test
    > AutofillInteractiveTestWithHistogramTester.BasicFormFill.
    > This CL re-enables the test.
    >
    > Bug: 1045709
    > Change-Id: I7ae39d710095acf744e123bf9208eca21997f8ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964610
    > Commit-Queue: Dominic Battré <battre@chromium.org>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#893979}
    
    Bug: 1045709
    Change-Id: If65f6f2d3f1b291ca72b12169f6f9ff14f765da3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2975441
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#894207}

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -847,8 +847,15 @@ class AutofillInteractiveTestWithHistogramTester
   std::unique_ptr<URLLoaderInterceptor> url_loader_interceptor_;
 };
 
+// Test is flaky on Linux TSAN, see http://crbug.com/1045709.
+#if defined(THREAD_SANITIZER)
+#define MAYBE_BasicFormFill DISABLED_BasicFormFill
+#else
+#define MAYBE_BasicFormFill BasicFormFill
+#endif  // THREAD_SANITIZER
+// Test that basic form fill is working.
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTestWithHistogramTester,
-                       BasicFormFill) {
+                       MAYBE_BasicFormFill) {
   CreateTestProfile();
 
   // Load the test page.
