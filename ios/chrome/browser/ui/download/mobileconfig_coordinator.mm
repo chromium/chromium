0c3ef80869943bd1b00a30de9f6297c796f24a0a
Ewann <ewannpv@google.com>
2021-06-21 09:10:03 +0000

    [iOS][2/N] Adds .mobileconfig file support
    This CL displays an alert to the user with a warning message and
    displays a SFSafariViewController which is used to handle the download.
    
    Bug: 781770
    Change-Id: I3be834b153e655993a15c374ba9b98287ad18ae8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911420
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894181}

--- ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
+++ ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
@@ -4,15 +4,22 @@
 
 #import "ios/chrome/browser/ui/download/mobileconfig_coordinator.h"
 
+#import <SafariServices/SafariServices.h>
+
 #include <memory>
 
 #include "base/scoped_observation.h"
+#include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/download/mobileconfig_tab_helper.h"
 #import "ios/chrome/browser/download/mobileconfig_tab_helper_delegate.h"
 #import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/web_state_observer_bridge.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -20,6 +27,7 @@
 
 @interface MobileConfigCoordinator () <CRWWebStateObserver,
                                        MobileConfigTabHelperDelegate,
+                                       SFSafariViewControllerDelegate,
                                        WebStateListObserving> {
   // WebStateList observers.
   std::unique_ptr<WebStateListObserverBridge> _webStateListObserverBridge;
@@ -29,6 +37,14 @@
 
 // The WebStateList being observed.
 @property(nonatomic, readonly) WebStateList* webStateList;
+
+// Coordinator used to display modal alerts to the user.
+@property(nonatomic, strong) AlertCoordinator* alertCoordinator;
+
+// SFSafariViewController used to download .mobileconfig file. When a
+// mobileconfig is downloaded from a SFSafariViewController, it's directly send
+// to the Settings app.
+@property(nonatomic, strong) SFSafariViewController* safariViewController;
 
 @end
 
@@ -57,6 +73,8 @@
     web::WebState* webState = self.webStateList->GetWebStateAt(i);
     [self uninstallDelegatesForWebState:webState];
   }
+  self.safariViewController = nil;
+  [self.alertCoordinator stop];
 }
 
 #pragma mark - Private
@@ -73,6 +91,19 @@
   if (MobileConfigTabHelper::FromWebState(webState)) {
     MobileConfigTabHelper::FromWebState(webState)->set_delegate(nil);
   }
+}
+
+// Presents SFSafariViewController in order to download .mobileconfig file.
+- (void)presentSFSafariViewController:(NSURL*)fileURL {
+  self.safariViewController =
+      [[SFSafariViewController alloc] initWithURL:fileURL];
+  self.safariViewController.delegate = self;
+  self.safariViewController.preferredBarTintColor =
+      [UIColor colorNamed:kPrimaryBackgroundColor];
+
+  [self.baseViewController presentViewController:self.safariViewController
+                                        animated:YES
+                                      completion:nil];
 }
 
 #pragma mark - WebStateListObserving
@@ -101,7 +132,36 @@
 #pragma mark - MobileConfigTabHelperDelegate
 
 - (void)presentMobileConfigAlertFromURL:(NSURL*)fileURL {
-  // TODO(crbug.com/781770): Present the alert.
+  self.alertCoordinator = [[AlertCoordinator alloc]
+      initWithBaseViewController:self.baseViewController
+                         browser:self.browser
+                           title:
+                               l10n_util::GetNSString(
+                                   IDS_IOS_DOWNLOAD_MOBILECONFIG_FILE_WARNING_TITLE)
+                         message:
+                             l10n_util::GetNSString(
+                                 IDS_IOS_DOWNLOAD_MOBILECONFIG_FILE_WARNING_MESSAGE)];
+
+  [self.alertCoordinator addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
+                                   action:nil
+                                    style:UIAlertActionStyleCancel];
+
+  __weak MobileConfigCoordinator* weakSelf = self;
+  [self.alertCoordinator
+      addItemWithTitle:l10n_util::GetNSString(
+                           IDS_IOS_DOWNLOAD_MOBILECONFIG_CONTINUE)
+                action:^{
+                  [weakSelf presentSFSafariViewController:fileURL];
+                }
+                 style:UIAlertActionStyleDefault];
+
+  [self.alertCoordinator start];
+}
+
+#pragma mark - SFSafariViewControllerDelegate
+
+- (void)safariViewControllerDidFinish:(SFSafariViewController*)controller {
+  [self.baseViewController dismissViewControllerAnimated:true completion:nil];
 }
 
 @end
