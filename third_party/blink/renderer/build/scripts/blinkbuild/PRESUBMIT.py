b4211134644deb6c675f972dfc587946ee6fc0cf
Fabrice de Gans <fdegans@chromium.org>
2021-06-23 16:13:47 +0000

    [code-health] Migrate blinkbuild presubmit to python3
    Bug: 1212072
    Change-Id: I684f94efc194f2d2757b149e5d635c961887e479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2979466
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895165}

--- third_party/blink/renderer/build/scripts/blinkbuild/PRESUBMIT.py
+++ third_party/blink/renderer/build/scripts/blinkbuild/PRESUBMIT.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from __future__ import print_function
+USE_PYTHON3 = True
 
 
 def _RunBindingsTests(input_api, output_api):
