7a4c68a5a396ab22a793c402353b4fe85ee780bd
Fabrice de Gans <fdegans@chromium.org>
2021-06-23 18:40:03 +0000

    [code-health] Migrate //gpu/PRESUBMIT.py to python3
    Bug: 1212033
    Change-Id: I2fd7af0db6a355730d7130e62511095a45fb72b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976463
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895249}

--- gpu/PRESUBMIT.py
+++ gpu/PRESUBMIT.py
@@ -8,6 +8,10 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
 
+
+USE_PYTHON3 = True
+
+
 def CommonChecks(input_api, output_api):
   import sys
 
