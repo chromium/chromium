ec2acb3c02f65bcb608b13f9b48e4b3c9007ee36
Lei Zhang <thestig@chromium.org>
2021-06-23 22:55:02 +0000

    Disable flaky SyncFileSystemServiceTest.InitializeForAppWithError.
    Bug: 1222929
    Change-Id: Ice8bbdaed8d134841ce7f41002c32aded65b9a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983485
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895360}

--- chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
+++ chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
@@ -302,7 +302,8 @@ TEST_F(SyncFileSystemServiceTest, InitializeForAppWithNetworkFailure) {
       SYNC_STATUS_NETWORK_ERROR);
 }
 
-TEST_F(SyncFileSystemServiceTest, InitializeForAppWithError) {
+// Disabled due to flakiness: crbug.com/1222929
+TEST_F(SyncFileSystemServiceTest, DISABLED_InitializeForAppWithError) {
   std::vector<SyncServiceState> expected_states;
   expected_states.push_back(SYNC_SERVICE_DISABLED);
 
