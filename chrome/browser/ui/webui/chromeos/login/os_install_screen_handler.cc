7a41f89acf0d1350ae567b54db52c4f0bfee519e
Danila Kuzmin <dkuzmin@google.com>
2021-06-23 10:29:57 +0000

    OOBE: Add subtitle to loading state of os-install-element
    Bug: b/190503857
    Change-Id: I697ffd239e0dd2802c9376e8388117f5d9857ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978477
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/master@{#895079}

--- chrome/browser/ui/webui/chromeos/login/os_install_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/os_install_screen_handler.cc
@@ -57,6 +57,8 @@ void OsInstallScreenHandler::DeclareLocalizedValues(
 
   builder->Add("osInstallDialogInProgressTitle",
                IDS_OS_INSTALL_SCREEN_IN_PROGRESS_TITLE);
+  builder->Add("osInstallDialogInProgressSubtitle",
+               IDS_OS_INSTALL_SCREEN_IN_PROGRESS_SUBTITLE);
 
   builder->Add("osInstallDialogErrorTitle", IDS_OS_INSTALL_SCREEN_ERROR_TITLE);
   builder->Add("osInstallDialogSuccessTitle",
