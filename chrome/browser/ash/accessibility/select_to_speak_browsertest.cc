a17618ef6b080f2b8c214fee90267055b4f27074
Lei Zhang <thestig@chromium.org>
2021-06-23 21:09:35 +0000

    Disable SelectToSpeakTest.ContinuesReadingDuringResize on MSAN.
    The test seems to be timing out there.
    
    Bug: 1114854
    Change-Id: I3d0bcfea47d05275f6b8a485483dbd7cdba12cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983653
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895318}

--- chrome/browser/ash/accessibility/select_to_speak_browsertest.cc
+++ chrome/browser/ash/accessibility/select_to_speak_browsertest.cc
@@ -488,7 +488,13 @@ IN_PROC_BROWSER_TEST_F(SelectToSpeakTest, FocusRingMovesWithMouse) {
   EXPECT_EQ(focus_rings.size(), 0u);
 }
 
-IN_PROC_BROWSER_TEST_F(SelectToSpeakTest, ContinuesReadingDuringResize) {
+// crbug.com/1114854 - Times out on MSAN bots.
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_ContinuesReadingDuringResize DISABLED_ContinuesReadingDuringResize
+#else
+#define MAYBE_ContinuesReadingDuringResize ContinuesReadingDuringResize
+#endif
+IN_PROC_BROWSER_TEST_F(SelectToSpeakTest, MAYBE_ContinuesReadingDuringResize) {
   ActivateSelectToSpeakInWindowBounds(
       "data:text/html;charset=utf-8,<p>First paragraph</p>"
       "<div id='resize' style='width:300px; font-size: 1em'>"
