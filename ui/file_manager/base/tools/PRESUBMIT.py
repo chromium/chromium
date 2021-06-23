8be543efbdd9316e2735193290fcc945e29f2f80
Dan Harrington <harringtond@chromium.org>
2021-06-23 20:50:59 +0000

    Migrate ui/file_manager presubmits to python3
    Bug: 1212124,1212125
    Change-Id: I1b62134fba18b8d9928e76230424fdd8a6508719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928157
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895305}

--- ui/file_manager/base/tools/PRESUBMIT.py
+++ ui/file_manager/base/tools/PRESUBMIT.py
@@ -7,6 +7,7 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
 
+USE_PYTHON3 = True
 
 def RunTests(input_api, output_api):
     presubmit_path = input_api.PresubmitLocalPath()
