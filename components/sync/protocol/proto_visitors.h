f771153021d5f37c73ce65ec66da565aee91d2cf
Adam Langley <agl@chromium.org>
2021-06-23 19:38:55 +0000

    Add WebauthnCredentialSpecifics
    This Sync entity type will carry WebAuthn credentials between Android
    and Chrome OS devices.
    
    Change-Id: I8200045075a5acb379837bef7b6dbe8efe71b1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2970681
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895275}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -43,6 +43,7 @@
 #include "components/sync/protocol/user_consent_specifics.pb.h"
 #include "components/sync/protocol/user_event_specifics.pb.h"
 #include "components/sync/protocol/web_app_specifics.pb.h"
+#include "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #include "components/sync/protocol/workspace_desk_specifics.pb.h"
 
 // This file implements VisitProtoFields() functions for sync protos.
@@ -656,6 +657,20 @@ VISIT_PROTO_FIELDS(const sync_pb::NigoriSpecifics& proto) {
   VISIT(custom_passphrase_key_derivation_method);
   VISIT(custom_passphrase_key_derivation_salt);
   VISIT(trusted_vault_debug_info);
+}
+
+VISIT_PROTO_FIELDS(const sync_pb::WebauthnCredentialSpecifics& proto) {
+  VISIT_BYTES(sync_id);
+  VISIT_BYTES(credential_id);
+  VISIT(rp_id);
+  VISIT_BYTES(user_id);
+  VISIT_REP(newly_shadowed_credential_ids);
+  VISIT(creation_time);
+  VISIT(user_name);
+  VISIT(user_display_name);
+  // |private_key| is deliberately omitted to avoid including sensitive
+  // information in debugging output, which might be included in bug reports
+  // etc.
 }
 
 VISIT_PROTO_FIELDS(
