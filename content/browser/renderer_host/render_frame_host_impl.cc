b57368da0918c6200c71d75d098d062dfa6ce5ec
Lukasz Anforowicz <lukasza@chromium.org>
2021-06-24 00:02:32 +0000

    Removing DwoC from VerifyThatBrowserAndRenderer...OriginsToCommitMatch.
    This CL removes the DumpWithoutCrashing call that was added to
    VerifyThatBrowserAndRendererCalculatedOriginsToCommitMatch in
    https://crrev.com/c/2925695.  Keeping DumpWithoutCrashing is undesirable
    at this point - it won't provide more actionable data + the volume of
    reports is too high to ship to Beta.
    
    Bug: 1220238
    Change-Id: I913fdbc9c80253248b7a712f6be9b5fdd7511bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983341
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895388}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -855,19 +855,6 @@ void VerifyThatBrowserAndRendererCalculatedOriginsToCommitMatch(
   }
 #endif
 
-  // TODO(https://crbug.com/888079): Remove the DwoC below once we are sure that
-  // the `browser_side_origin` is always the same as the `renderer_side_origin`.
-  if (browser_side_origin != renderer_side_origin) {
-    NavigationRequest::ScopedCrashKeys navigation_request_crash_keys(
-        *navigation_request);
-    SCOPED_CRASH_KEY_STRING256("", "browser_side_origin",
-                               browser_side_origin.GetDebugString());
-    SCOPED_CRASH_KEY_STRING256("", "renderer_side_origin",
-                               renderer_side_origin.GetDebugString());
-    CaptureTraceForNavigationDebugScenario(
-        DebugScenario::kDebugBrowserVsRendererOriginToCommit);
-    base::debug::DumpWithoutCrashing();
-  }
   DCHECK_EQ(browser_side_origin, renderer_side_origin)
       << "; navigation_request->GetURL() = " << navigation_request->GetURL();
 }
