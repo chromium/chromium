bf9d97ca05dcc11c2187269ca09c068b7d6dac06
Piotr Tworek <ptworek@vewd.com>
2021-06-25 11:59:58 +0000

    Fix flaky TestMultipleObservers NetLogProxySink test.
    The test creates net_log_observer2 by posting a task to a thread pool.
    As the code comment states the intention is to create this observer on a
    different thread. The code does not ensure the posted callback is
    executed before first access to net_log_observer2. In most cases it is,
    but I did manage to hit cases where GetEntries methods gets called on
    nullptr net_log_observer2. So far this only happened to me when using
    qemu VM limited to 2 CPU cores.
    
    Fix this by explicitly waiting for net_log_observer2 to be created
    before proceeding with the rest of the testing procedure.
    
    Bug: 1223732
    Change-Id: Ia226b83cf17f30fe9ce044e24f398a70ab1c6b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988119
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896014}

--- services/network/net_log_proxy_sink_unittest.cc
+++ services/network/net_log_proxy_sink_unittest.cc
@@ -87,10 +87,16 @@ TEST(NetLogProxySink, TestMultipleObservers) {
       net_log_proxy_source.WaitForUpdateCaptureModes());
 
   std::unique_ptr<net::RecordingNetLogObserver> net_log_observer2;
-  // Create observer2 on a different thread.
-  base::ThreadPool::PostTask(FROM_HERE,
-                             base::BindOnce(&CreateObserver, &net_log_observer2,
-                                            net::NetLogCaptureMode::kDefault));
+  {
+    // Create observer2 on a different thread.
+    base::RunLoop run_loop;
+    base::ThreadPool::PostTaskAndReply(
+        FROM_HERE,
+        base::BindOnce(&CreateObserver, &net_log_observer2,
+                       net::NetLogCaptureMode::kDefault),
+        run_loop.QuitClosure());
+    run_loop.Run();
+  }
   EXPECT_EQ(
       net::NetLogCaptureModeToBit(net::NetLogCaptureMode::kIncludeSensitive) |
           net::NetLogCaptureModeToBit(net::NetLogCaptureMode::kDefault),
