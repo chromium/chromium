e971bb9a7bdc1947560b1c418619aa0fcbdc2ae5
Fabrice de Gans <fdegans@chromium.org>
2021-06-28 11:53:26 +0000

    [code-health] Migrate //third_party/android_platform/development/scripts/PRESUBMIT.py to python3
    Bug: 1212062
    Change-Id: I681ded502c3f84aaec2f63fc4c1005447de87dad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976550
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896485}

--- third_party/android_platform/development/scripts/PRESUBMIT.py
+++ third_party/android_platform/development/scripts/PRESUBMIT.py
@@ -8,6 +8,9 @@ for more details about the presubmit API built into depot_tools.
 """
 
 
+USE_PYTHON3 = True
+
+
 def CommonChecks(input_api, output_api):
   output = []
   output.extend(input_api.canned_checks.RunPylint(input_api, output_api))
