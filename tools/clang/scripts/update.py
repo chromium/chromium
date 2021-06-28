404441352104b8ae0b9e7540c9e6005cc965a1ec
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-06-28 11:04:57 +0000

    Revert "Roll clang llvmorg-13-init-13689-g93938943-1 : llvmorg-13-init-13925-g0f24ffcd-1"
    This reverts commit 5d4f0c22ed6a4a1f9614209d8dbcaf2c3eb2d5c9.
    
    Reason for revert:
    
    Note: It is reported that sheriffs cannot submit CL created by Findit
    (crbug.com/1187426). A workaround in the mean time is to abandon this
    CL and create another revert CL.
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 896384 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzVkNGYwYzIyZWQ2YTRhMWY5NjE0MjA5ZDhkYmNhZjJjM2ViMmQ1YzkM
    
    Sample Failed Build: https://ci.chromium.org/b/8843206127111465201
    
    Sample Failed Step: compile
    
    Original change's description:
    > Roll clang llvmorg-13-init-13689-g93938943-1 : llvmorg-13-init-13925-g0f24ffcd-1
    >
    > Ran `tools/clang/scripts/upload_revision.py 0f24ffcdfa9bb2b6380cedbb24f16d4ab9920dd7`.
    >
    > Bug: 1223784
    > Change-Id: I56505c75bbc0b0f0c296b664f27c61cd2f5a1735
    > Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    > Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    > Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    > Cq-Include-Trybots: chromium/try:linux_angle_deqp_rel_ng
    > Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    > Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    > Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    > Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    > Cq-Include-Trybots: chromium/try:mac-arm64-rel,mac_chromium_asan_rel_ng
    > Cq-Include-Trybots: chromium/try:win-asan,win7-rel
    > Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    > Cq-Include-Trybots: chromium/try:mac-official,linux-official
    > Cq-Include-Trybots: chromium/try:win-official,win32-official
    > Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    > Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    > Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,mac-chrome
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987865
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#896384}
    
    
    Change-Id: I65e8670b4cf83f23e6e11c403721600326076993
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1223784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992233
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Owners-Override: Richard Stotz <richardstotz@google.com>
    Cr-Commit-Position: refs/heads/master@{#896476}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -39,7 +39,7 @@ import zipfile
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-13-init-13925-g0f24ffcd'
+CLANG_REVISION = 'llvmorg-13-init-13689-g93938943'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
