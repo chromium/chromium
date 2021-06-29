c96dacdc5117f454dfb7451381f50321bb0c2570
Keren Zhu <kerenzhu@chromium.org>
2021-06-29 00:03:44 +0000

    Use python3 in presubmits previously blocked by canned_checks
    Bug: 1212100, 1212104, 1212111
    Change-Id: Icc318231b193fdd24f0b5dde6bdad744a1a3269a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992685
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896711}

--- tools/origin_trials/PRESUBMIT.py
+++ tools/origin_trials/PRESUBMIT.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+USE_PYTHON3 = True
+
 
 def _CommonChecks(input_api, output_api):
   results = []
