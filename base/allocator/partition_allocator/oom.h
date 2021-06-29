7e6a57aa6d33ecad2e1783df2cec6eb5498a6532
Bruce Dawson <brucedawson@chromium.org>
2021-06-29 02:26:58 +0000

    Move OnNoMemory to .cc file
    OnNoMemory was implemented as a NOINLINE anonymous namespace function,
    which can more efficiently/canonically be implemented in a separate
    translation unit. This makes this change, which allows oom.h to include
    far fewer files.
    
    Bug: 1220098
    Change-Id: I967074e99f0f18f10fa0aab29f3dd8c0a88f218a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981452
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896760}

--- base/allocator/partition_allocator/oom.h
+++ base/allocator/partition_allocator/oom.h
@@ -5,22 +5,15 @@
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_OOM_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_OOM_H_
 
-#include "base/allocator/partition_allocator/oom_callback.h"
+#include "base/base_export.h"
 #include "base/compiler_specific.h"
-#include "base/immediate_crash.h"
-#include "base/process/memory.h"
-#include "build/build_config.h"
 
-namespace {
+#include <stddef.h>
+
 // The crash is generated in a NOINLINE function so that we can classify the
 // crash as an OOM solely by analyzing the stack trace. It is tagged as
 // NOT_TAIL_CALLED to ensure that its parent function stays on the stack.
-[[noreturn]] NOINLINE void NOT_TAIL_CALLED OnNoMemory(size_t size) {
-  base::internal::RunPartitionAllocOomCallback();
-  base::TerminateBecauseOutOfMemory(size);
-  IMMEDIATE_CRASH();
-}
-}  // namespace
+[[noreturn]] BASE_EXPORT void NOT_TAIL_CALLED OnNoMemory(size_t size);
 
 // OOM_CRASH(size) - Specialization of IMMEDIATE_CRASH which will raise a custom
 // exception on Windows to signal this is OOM and not a normal assert.
