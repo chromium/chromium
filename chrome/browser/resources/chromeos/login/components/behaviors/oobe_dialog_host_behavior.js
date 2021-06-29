5deb6790a8cc5eb12dc90814ad4fdc0d83e085ba
Renato Silva <rrsilva@google.com>
2021-06-29 14:46:37 +0000

    OOBE - Modulize behaviours
    Generate modulized versions of the following elements for
    the ongoing Polymer3 migration:
    
        - oobe_focus_behavior
        - oobe_scrollable_behavior
        - oobe_dialog_host_behavior
    
    Bug: 1184731
    Change-Id: I4a9c443edbd3bad149d3800040c02d63cfe7a204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991740
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896915}

--- chrome/browser/resources/chromeos/login/components/behaviors/oobe_dialog_host_behavior.js
+++ chrome/browser/resources/chromeos/login/components/behaviors/oobe_dialog_host_behavior.js
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {dom, Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
 /**
  * @fileoverview
  * 'OobeDialogHostBehavior' is a behavior for oobe-dialog containers to
--- chrome/browser/resources/chromeos/login/components/behaviors/oobe_focus_behavior.js
+++ chrome/browser/resources/chromeos/login/components/behaviors/oobe_focus_behavior.js
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {afterNextRender} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
 /**
  * @fileoverview
  * 'OobeFocusBehavior' is a special behavior which supports focus transferring
