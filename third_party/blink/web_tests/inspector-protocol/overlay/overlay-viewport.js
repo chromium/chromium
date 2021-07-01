38ed17d5a3a205adc54c7bbcd5b064b2841b7a1c
Alex Rudenko <alexrudenko@chromium.org>
2021-07-01 12:19:28 +0000

    Add a test for Overlay.setShowViewportSizeOnResize
    Fixed: chromium:1224297
    Change-Id: I86f45b23d02c211940a2bacca796a6d782654c1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3000244
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897735}

---
+++ third_party/blink/web_tests/inspector-protocol/overlay/overlay-viewport.js
@@ -0,0 +1,69 @@
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startHTML(`
+    <div>Test page</div>
+  `, 'Verifies that Overlay.setShowViewportSizeOnResize works.');
+
+  await dp.DOM.enable();
+  await dp.Overlay.enable();
+  // Page domain is required by the viewport size overlay.
+  await dp.Page.enable();
+
+  await dp.Overlay.setInspectMode({
+    mode: 'searchForNode',
+    highlightConfig: {},
+  });
+
+  async function waitForAnimationFrame() {
+    await session.evaluateAsync(() => {
+      return new Promise(resolve => requestAnimationFrame(resolve));
+    });
+  }
+
+  async function getViewportResetCommands(count) {
+    await waitForAnimationFrame();
+    return await session.evaluate((count) => {
+      return internals.evaluateInInspectorOverlay(`(function () {
+        const commands = window.commands;
+        window.commands = [];
+        return JSON.stringify(commands.slice(0, ${count}), null, 2);
+      })()`);
+    }, count);
+  }
+
+  await session.evaluate(() => {
+    return internals.evaluateInInspectorOverlay(`(function () {
+      window.commands = [];
+      window.dispatch = ([name, data]) => {
+        window.commands.push({name, data});
+      };
+    })()`);
+  });
+
+  await dp.Emulation.setDeviceMetricsOverride({
+    width: 800,
+    height: 600,
+    deviceScaleFactor: 1,
+    mobile: false,
+    dontSetVisibleSize: false,
+  });
+
+  testRunner.log('Initial device metrics:');
+  testRunner.log(await getViewportResetCommands(1));
+
+  await dp.Overlay.setShowViewportSizeOnResize({
+    show: true,
+  });
+
+  await dp.Emulation.setDeviceMetricsOverride({
+    width: 500,
+    height: 500,
+    deviceScaleFactor: 1,
+    mobile: false,
+  });
+
+  testRunner.log('Device metrics with changed viewport:');
+  testRunner.log(await getViewportResetCommands(3));
+
+  testRunner.completeTest();
+});
+
