7be29afc9e12d9c6a2cf923e4743d991eb34cbee
Ali Beyad <abeyad@chromium.org>
2021-07-01 22:17:51 +0000

    Fix a spelling mistake
    Privileged was misspelled in the header comments.
    
    Change-Id: Id91a16c26daa6813f373cdfd75c9aa84b400e9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002222
    Auto-Submit: Ali Beyad <abeyad@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897922}

--- services/network/public/cpp/parsed_headers.h
+++ services/network/public/cpp/parsed_headers.h
@@ -19,7 +19,7 @@ namespace network {
 // This function must remain:
 // - As 'pure' as possible. See [pure-function].
 // - Callable from any processes.
-//   For security reasons, highly proviledged processes must never call it
+//   For security reasons, highly privileged processes must never call it
 //   directly against untrustworthy inputs. See [rule-of-2]. In this case, you
 //   can call it indirectly via NetworkContext.ParseHeaders().
 //
