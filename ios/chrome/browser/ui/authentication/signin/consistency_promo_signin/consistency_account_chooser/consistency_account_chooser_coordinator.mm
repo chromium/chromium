f6b722c729678c77a432cec3fbd1b10602068a15
Ewann <ewannpv@google.com>
2021-07-02 13:49:27 +0000

    [iOS] Uses ChromeAccountManagerService in ConsistencyAccountChooserCoordinator
    This CL uses APIs from ChromeAccountManagerService instead
    of ChromeIdentityService.
    
    Bug: 1220345
    Change-Id: Ic74910bce2c8e2a9de634a651e8b3de839ff2836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001887
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898155}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_coordinator.mm
@@ -7,13 +7,12 @@
 #import "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service.h"
 #import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_mediator.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_table_view_controller_action_delegate.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_view_controller.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
-#import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -67,10 +66,12 @@
 - (void)consistencyAccountChooserTableViewController:
             (ConsistencyAccountChooserTableViewController*)viewController
                          didSelectIdentityWithGaiaID:(NSString*)gaiaID {
-  ios::ChromeIdentityService* identityService =
-      ios::GetChromeBrowserProvider()->GetChromeIdentityService();
-  ChromeIdentity* identity =
-      identityService->GetIdentityWithGaiaID(base::SysNSStringToUTF8(gaiaID));
+  ChromeAccountManagerService* accountManagerService =
+      ChromeAccountManagerServiceFactory::GetForBrowserState(
+          self.browser->GetBrowserState());
+
+  ChromeIdentity* identity = accountManagerService->GetIdentityWithGaiaID(
+      base::SysNSStringToUTF8(gaiaID));
   DCHECK(identity);
   self.mediator.selectedIdentity = identity;
   [self.delegate
