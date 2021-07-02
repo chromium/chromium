72b741e19701ed499f3dcdeda1ff03a5c87bd83b
Joon Ahn <joonbug@chromium.org>
2021-07-02 22:23:53 +0000

    shimless: skip network selection page on detecting active network
    Bug: 1198187
    Change-Id: I73f554dfce8df1ff93dcc66cd66c358470c319c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999303
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898370}

--- ash/webui/shimless_rma/resources/onboarding_landing_page.js
+++ ash/webui/shimless_rma/resources/onboarding_landing_page.js
@@ -5,7 +5,11 @@
 import './shimless_rma_shared_css.js';
 import './base_page.js';
 
+import {OncMojo} from 'chrome://resources/cr_components/chromeos/network/onc_mojo.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getNetworkConfigService} from './mojo_interface_provider.js';
+import {NetworkConfigServiceInterface, RmadErrorCode, RmaState, StateResult} from './shimless_rma_types.js';
 
 /**
  * @fileoverview
@@ -21,9 +25,62 @@ export class OnboardingLandingPage extends PolymerElement {
     return html`{__html_template__}`;
   }
 
+  static get properties() {
+    return {
+      /** @private {?NetworkConfigServiceInterface} */
+      networkConfig_: {
+        type: Object,
+        value: null,
+      },
+
+      /** @private {boolean} */
+      networkConnected_: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+
   /** @override */
   ready() {
     super.ready();
+    this.networkConfig_ = getNetworkConfigService();
+    this.hasNetworkConnection_();
+  }
+
+  /** @private */
+  hasNetworkConnection_() {
+    const networkFilter = {
+      filter: chromeos.networkConfig.mojom.FilterType.kVisible,
+      networkType: chromeos.networkConfig.mojom.NetworkType.kAll,
+      limit: chromeos.networkConfig.mojom.NO_LIMIT,
+    };
+
+    this.networkConfig_.getNetworkStateList(networkFilter).then(res => {
+      const connectedNetworks = res.result.filter(
+          (network) =>
+              (network.connectionState ===
+               chromeos.networkConfig.mojom.ConnectionStateType.kOnline) &&
+              [
+                chromeos.networkConfig.mojom.NetworkType.kWiFi,
+                chromeos.networkConfig.mojom.NetworkType.kEthernet,
+                chromeos.networkConfig.mojom.NetworkType.kCellular,
+              ].includes(network.type));
+
+      this.networkConnected_ = connectedNetworks.length > 0;
+    });
+  }
+
+  /** @return {!Promise<StateResult>} */
+  onNextButtonClick() {
+    if (this.networkConnected_) {
+      // TODO(crbug.com/1218180): Replace with a state specific function e.g.
+      // ProvisioningComplete()
+      return Promise.resolve(
+          {state: RmaState.kUpdateChrome, error: RmadErrorCode.kOk});
+    } else {
+      return Promise.resolve();
+    }
   }
 };
 
--- chrome/test/data/webui/chromeos/shimless_rma/onboarding_landing_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/onboarding_landing_page_test.js
@@ -2,14 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {OncMojo} from 'chrome://resources/cr_components/chromeos/network/onc_mojo.m.js';
+import {setNetworkConfigServiceForTesting} from 'chrome://shimless-rma/mojo_interface_provider.js';
 import {OnboardingLandingPage} from 'chrome://shimless-rma/onboarding_landing_page.js';
+import {RmaState} from 'chrome://shimless-rma/shimless_rma_types.js';
 
-import {assertEquals, assertFalse, assertNotEquals, assertTrue} from '../../chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks} from '../../test_util.m.js';
+import {FakeNetworkConfig} from '../fake_network_config_mojom.m.js';
+
 
 export function onboardingLandingPageTest() {
   /** @type {?OnboardingLandingPage} */
   let component = null;
+
+  /** @type {?FakeNetworkConfig} */
+  let networkConfigService = null;
+
+  suiteSetup(() => {
+    networkConfigService = new FakeNetworkConfig();
+    setNetworkConfigServiceForTesting(
+        /** @type {!chromeos.networkConfig.mojom.CrosNetworkConfigInterface} */
+        (networkConfigService));
+  });
 
   setup(() => {
     document.body.innerHTML = '';
@@ -18,6 +33,7 @@ export function onboardingLandingPageTest() {
   teardown(() => {
     component.remove();
     component = null;
+    networkConfigService.resetForTest();
   });
 
   /**
@@ -40,5 +56,27 @@ export function onboardingLandingPageTest() {
 
     const basePage = component.shadowRoot.querySelector('base-page');
     assertTrue(!!basePage);
+  });
+
+  test('ConnectedNetworkNext', async () => {
+    networkConfigService.setNetworkConnectionStateForTest(
+        'eth0_guid', chromeos.networkConfig.mojom.ConnectionStateType.kOnline);
+    await initializeLandingPage();
+
+    let savedResult;
+    component.onNextButtonClick().then((result) => savedResult = result);
+    await flushTasks();
+
+    assertEquals(savedResult.state, RmaState.kUpdateChrome);
+  });
+
+  test('NoNetworkNext', async () => {
+    await initializeLandingPage();
+
+    let savedResult;
+    component.onNextButtonClick().then((result) => savedResult = result);
+    await flushTasks();
+
+    assertEquals(savedResult, undefined);
   });
 }
