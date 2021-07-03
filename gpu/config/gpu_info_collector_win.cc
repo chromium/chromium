878f42ef8aee42ccbed1996a39568eeb33c4ce3e
Maggie Chen <magchen@chromium.org>
2021-07-03 22:11:53 +0000

    Add D3D_FEATURE_LEVEL_12_2 to Windows GPU info collection
    The current info collection function can only query D3D12 feature
    level up to 12.1. This CL addes the new feature level 12.2 to the
    info collection function.
    
    This CL does not update the D3D11 info collection function. The
    support for D3D_FEATURE_LEVEL_12_2 on Windows has not been surfaced
    through D3D11 API. Calling a D3D11 function with
    D3D_FEATURE_LEVEL_12_2 will fail now.
    
    Bug: 1222429
    Change-Id: I2a3a2e3453c95f5698f21b3611b51ea6f5912bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004188
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898422}

--- gpu/config/gpu_info_collector_win.cc
+++ gpu/config/gpu_info_collector_win.cc
@@ -42,6 +42,18 @@ namespace gpu {
 
 namespace {
 
+// TODO(magchen@): Remove PFN_D3D12_CREATE_DEVICE_CHROMIUM and use
+// PFN_D3D12_CREATE_DEVICE from d3d12.h directly once the Windows toolchain is
+// updated.
+
+// Declaration for D3D12CreateDevice() with D3D_FEATURE_LEVEL_12_2 support in
+// D3D_FEATURE_LEVEL_CHROMIUM.
+typedef HRESULT(WINAPI* PFN_D3D12_CREATE_DEVICE_CHROMIUM)(
+    _In_opt_ IUnknown*,
+    D3D_FEATURE_LEVEL_CHROMIUM,
+    _In_ REFIID,
+    _COM_Outptr_opt_ void**);
+
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
 // This should match enum D3D12FeatureLevel in
@@ -52,7 +64,8 @@ enum class D3D12FeatureLevel {
   kD3DFeatureLevel_12_1 = 2,
   kD3DFeatureLevel_11_0 = 3,
   kD3DFeatureLevel_11_1 = 4,
-  kMaxValue = kD3DFeatureLevel_11_1,
+  kD3DFeatureLevel_12_2 = 5,
+  kMaxValue = kD3DFeatureLevel_12_2,
 };
 
 inline D3D12FeatureLevel ConvertToHistogramFeatureLevel(
@@ -60,13 +73,15 @@ inline D3D12FeatureLevel ConvertToHistogramFeatureLevel(
   switch (d3d_feature_level) {
     case 0:
       return D3D12FeatureLevel::kD3DFeatureLevelUnknown;
-    case D3D_FEATURE_LEVEL_12_0:
+    case D3D12_FEATURE_LEVEL_12_0:
       return D3D12FeatureLevel::kD3DFeatureLevel_12_0;
-    case D3D_FEATURE_LEVEL_12_1:
+    case D3D12_FEATURE_LEVEL_12_1:
       return D3D12FeatureLevel::kD3DFeatureLevel_12_1;
-    case D3D_FEATURE_LEVEL_11_0:
+    case D3D12_FEATURE_LEVEL_12_2:
+      return D3D12FeatureLevel::kD3DFeatureLevel_12_2;
+    case D3D12_FEATURE_LEVEL_11_0:
       return D3D12FeatureLevel::kD3DFeatureLevel_11_0;
-    case D3D_FEATURE_LEVEL_11_1:
+    case D3D12_FEATURE_LEVEL_11_1:
       return D3D12FeatureLevel::kD3DFeatureLevel_11_1;
     default:
       NOTREACHED();
@@ -245,12 +260,13 @@ uint32_t GetGpuSupportedD3D12Version() {
     return 0;
 
   // The order of feature levels to attempt to create in D3D CreateDevice
-  const D3D_FEATURE_LEVEL feature_levels[] = {
-      D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0, D3D_FEATURE_LEVEL_11_1,
-      D3D_FEATURE_LEVEL_11_0};
+  const D3D_FEATURE_LEVEL_CHROMIUM feature_levels[] = {
+      D3D12_FEATURE_LEVEL_12_2, D3D12_FEATURE_LEVEL_12_1,
+      D3D12_FEATURE_LEVEL_12_0, D3D12_FEATURE_LEVEL_11_1,
+      D3D12_FEATURE_LEVEL_11_0};
 
-  PFN_D3D12_CREATE_DEVICE D3D12CreateDevice =
-      reinterpret_cast<PFN_D3D12_CREATE_DEVICE>(
+  PFN_D3D12_CREATE_DEVICE_CHROMIUM D3D12CreateDevice =
+      reinterpret_cast<PFN_D3D12_CREATE_DEVICE_CHROMIUM>(
           d3d12_library.GetFunctionPointer("D3D12CreateDevice"));
   if (D3D12CreateDevice) {
     // For the default adapter only. (*pAdapter == nullptr)
