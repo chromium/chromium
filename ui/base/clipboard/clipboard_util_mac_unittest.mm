7de403ae3c1777a0ad89c9d51c019865d325b8fe
Avi Drissman <avi@chromium.org>
2021-07-06 19:03:59 +0000

    Fully unpack data for comparisons
    plists can be encoded multiple ways. If comparing data for equality,
    unpack the plists before the comparison, so that the comparison can
    happen on the actual data rather than encodings that may differ due to
    the whims of the OS.
    
    Fixed: 1222623
    Change-Id: Ifebe18d486237ebde820c2d8f2e59be6b10c8d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009995
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898926}

--- ui/base/clipboard/clipboard_util_mac_unittest.mm
+++ ui/base/clipboard/clipboard_util_mac_unittest.mm
@@ -19,13 +19,28 @@ class ClipboardUtilMacTest : public PlatformTest {
  public:
   ClipboardUtilMacTest() { }
 
-  NSDictionary* DictionaryFromPasteboard(NSPasteboard* pboard) {
-    NSArray* types = [pboard types];
-    NSMutableDictionary* data = [NSMutableDictionary dictionary];
-    for (NSString* type in types) {
-      data[type] = [pboard dataForType:type];
+  // Given a pasteboard, returns a dictionary of the contents of the pasteboard
+  // for use in deep comparisons. This fully unpacks any plist-encoded items.
+  NSDictionary* DictionaryFromPasteboardForDeepComparisons(
+      NSPasteboard* pboard) {
+    NSMutableDictionary* result = [NSMutableDictionary dictionary];
+    for (NSString* type in [pboard types]) {
+      NSData* data = [pboard dataForType:type];
+      // Try to unpack the data as a plist, and if it succeeds, use that in the
+      // resulting dictionary rather than the raw NSData. This is needed because
+      // plists have multiple encodings, and the comparison should be made on
+      // the underlying data rather than the specific encoding used by the OS.
+      NSDictionary* unpacked_data = [NSPropertyListSerialization
+          propertyListWithData:data
+                       options:NSPropertyListImmutable
+                        format:nil
+                         error:nil];
+      if (unpacked_data)
+        result[type] = unpacked_data;
+      else
+        result[type] = data;
     }
-    return data;
+    return result;
   }
 };
 
@@ -116,13 +131,7 @@ TEST_F(ClipboardUtilMacTest, CheckForLeak) {
   }
 }
 
-#if defined(ARCH_CPU_ARM64)
-// https://crbug.com/1222623
-#define MAYBE_CompareToWriteToPasteboard DISABLED_CompareToWriteToPasteboard
-#else
-#define MAYBE_CompareToWriteToPasteboard CompareToWriteToPasteboard
-#endif
-TEST_F(ClipboardUtilMacTest, MAYBE_CompareToWriteToPasteboard) {
+TEST_F(ClipboardUtilMacTest, CompareToWriteToPasteboard) {
   NSString* urlString = @"https://www.cnn.com/";
 
   base::scoped_nsobject<NSPasteboardItem> item(
@@ -133,8 +142,10 @@ TEST_F(ClipboardUtilMacTest, MAYBE_CompareToWriteToPasteboard) {
   scoped_refptr<UniquePasteboard> pboard = new UniquePasteboard;
   [pboard->get() setDataForURL:urlString title:urlString];
 
-  NSDictionary* data1 = DictionaryFromPasteboard(pasteboard->get());
-  NSDictionary* data2 = DictionaryFromPasteboard(pboard->get());
+  NSDictionary* data1 =
+      DictionaryFromPasteboardForDeepComparisons(pasteboard->get());
+  NSDictionary* data2 =
+      DictionaryFromPasteboardForDeepComparisons(pboard->get());
   EXPECT_NSEQ(data1, data2);
 }
 
