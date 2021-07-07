4dc43a81bf1d755dcd13e2a5502288db10bd84df
Etienne Pierre-doray <etiennep@chromium.org>
2021-07-07 16:36:19 +0000

    [throttler]: Remove unused includes.
    Bug: 1153139
    Change-Id: I0c9a60ad17dcaced106bf71a95cac3591cd3daa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009114
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899158}

--- third_party/blink/renderer/core/editing/finder/find_task_controller.h
+++ third_party/blink/renderer/core/editing/finder/find_task_controller.h
@@ -9,9 +9,6 @@
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/position.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool.h"
-#include "third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool_controller.h"
-#include "third_party/blink/renderer/platform/scheduler/common/throttling/cpu_time_budget_pool.h"
 #include "third_party/blink/renderer/platform/scheduler/common/tracing_helper.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
