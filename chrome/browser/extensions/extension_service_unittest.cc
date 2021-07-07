19b8db006ef8c10b7bddc32a638ce3f4a3e79ffe
Scott Little <sclittle@chromium.org>
2021-07-07 19:36:41 +0000

    Disable failing test: ExtensionServiceTest.ConcurrentExternalLocalFile
    This test is failing on a TSan builder. See crbug for more details.
    
    Bug: 1227104
    Change-Id: I977a6fb62299db8d48891b03472e638a08ac253b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011776
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Owners-Override: Scott Little <sclittle@chromium.org>
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899237}

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -6573,7 +6573,8 @@ TEST_F(ExtensionServiceTest, InstallPriorityExternalLocalFile) {
   EXPECT_TRUE(pending->IsIdPending(kGoodId));
 }
 
-TEST_F(ExtensionServiceTest, ConcurrentExternalLocalFile) {
+// TODO(crbug.com/1227104): This test fails when run by TSan buildbots.
+TEST_F(ExtensionServiceTest, DISABLED_ConcurrentExternalLocalFile) {
   base::Version kVersion123("1.2.3");
   base::Version kVersion124("1.2.4");
   base::Version kVersion125("1.2.5");
