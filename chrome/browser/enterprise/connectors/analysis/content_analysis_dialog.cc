fb4a6cb2fa941fe8ebfec2506e1d0a9cd98d885a
Owen Min <zmin@chromium.org>
2021-07-07 21:58:45 +0000

    Remove chrome/browser/ui/views dependency from enterprise folder
    chrome/browser/enterprise shouldn't rely on the implementation details
    of ui view, and it doesn't need to. Hence remove the include and DEPS
    
    Bug: 1227070
    Change-Id: Ic509154d0160ada66d3826c9d6c41d54b19a06c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010540
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899312}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_dialog.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_dialog.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,6 @@
 #include "base/bind.h"
 #include "cc/paint/paint_flags.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
-#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
