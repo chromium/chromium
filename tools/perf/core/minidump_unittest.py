a8d53f3f96e84c08244bf650185cda9eebd1fd46
Marijn Kruisselbrink <mek@chromium.org>
2021-07-08 19:02:18 +0000

    Disable two browser minidump tests on android.
    These tests are very flaky, so disable them.
    
    Bug: 1218560
    Change-Id: I6816ffb997e07429655016cfdf402f19ad9d8287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015312
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899646}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -48,6 +48,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
+      'android',  # https://crbug.com/1218560
       'chromeos-local',
       'win7'  # https://crbug.com/1084931
   )
@@ -93,6 +94,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
+      'android',  # https://crbug.com/1218560
       'chromeos-local',
       'win7'  # https://crbug.com/1084931
   )
