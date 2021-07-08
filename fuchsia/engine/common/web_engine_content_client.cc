0b5c0cb4d4d4164e2237264985152699a635444f
Mitsuru Oshima <oshima@chromium.org>
2021-07-08 19:35:49 +0000

    Rename ScaleFactor to ResourceScaleFactor in fuchsia/
    Bug: 1217405
    Change-Id: Ibd31bb164abcc653122d08fb8dfaf5d9a2798202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3000831
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Mitsuru Oshima (OOO back on July 12)) <oshima@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899661}

--- fuchsia/engine/common/web_engine_content_client.cc
+++ fuchsia/engine/common/web_engine_content_client.cc
@@ -20,7 +20,7 @@ std::u16string WebEngineContentClient::GetLocalizedString(int message_id) {
 
 base::StringPiece WebEngineContentClient::GetDataResource(
     int resource_id,
-    ui::ScaleFactor scale_factor) {
+    ui::ResourceScaleFactor scale_factor) {
   return ui::ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
       resource_id, scale_factor);
 }
--- fuchsia/engine/common/web_engine_content_client.h
+++ fuchsia/engine/common/web_engine_content_client.h
@@ -15,8 +15,9 @@ class WebEngineContentClient : public content::ContentClient {
 
   // content::ContentClient implementation.
   std::u16string GetLocalizedString(int message_id) override;
-  base::StringPiece GetDataResource(int resource_id,
-                                    ui::ScaleFactor scale_factor) override;
+  base::StringPiece GetDataResource(
+      int resource_id,
+      ui::ResourceScaleFactor scale_factor) override;
   base::RefCountedMemory* GetDataResourceBytes(int resource_id) override;
   gfx::Image& GetNativeImageNamed(int resource_id) override;
   blink::OriginTrialPolicy* GetOriginTrialPolicy() override;
