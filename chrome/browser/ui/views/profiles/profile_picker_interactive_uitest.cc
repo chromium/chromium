a03107ffd60280c0fa203da26bdc744211a1ca90
Pete Williamson <petewil@chromium.org>
2021-07-09 20:43:39 +0000

    Disable the ProfilePickerInteractiveUITest.CloseSigninWithKeyboard as flaky
    Bug: 1227800
    Change-Id: I08f1c412e9e10608e4d39369e716a2abafcc14e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018522
    Owners-Override: Peter Williamson <petewil@chromium.org>
    Owners-Override: Roger Tawa <rogerta@chromium.org>
    Auto-Submit: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900131}

--- chrome/browser/ui/views/profiles/profile_picker_interactive_uitest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_interactive_uitest.cc
@@ -183,8 +183,14 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerInteractiveUiTest, FullscreenWithKeyboard) {
 }
 
 // Checks that the signin web view is able to process keyboard events.
+// TODO(https://crbug.com/1227800): Flaky on linux.
+#if defined(OS_LINUX)
+#define MAYBE_CloseSigninWithKeyboard DISABLED_CloseSigninWithKeyboardd
+#else
+#define MAYBE_CloseSigninWithKeyboard CloseSigninWithKeyboard
+#endif
 IN_PROC_BROWSER_TEST_F(ProfilePickerInteractiveUiTest,
-                       CloseSigninWithKeyboard) {
+                       MAYBE_CloseSigninWithKeyboard) {
   ShowAndFocusPicker(ProfilePicker::EntryPoint::kProfileMenuAddNewProfile);
 
   // Simulate a click on the signin button.
