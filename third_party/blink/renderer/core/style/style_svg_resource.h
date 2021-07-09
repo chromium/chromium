37e8f7a06c92e912966d6ba666460010c7520bb1
Fredrik Söderqvist <fs@opera.com>
2021-07-09 17:48:28 +0000

    Shrink style_svg_resource.h
    Bug: 242216
    Change-Id: I5c8af73b473c2f649cb43e63ad956038e89d3e56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016641
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900067}

--- third_party/blink/renderer/core/style/style_svg_resource.h
+++ third_party/blink/renderer/core/style/style_svg_resource.h
@@ -6,12 +6,15 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_STYLE_SVG_RESOURCE_H_
 
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/core/svg/svg_resource.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
+
+class SVGResource;
+class SVGResourceClient;
 
 class StyleSVGResource : public RefCounted<StyleSVGResource> {
   USING_FAST_MALLOC(StyleSVGResource);
@@ -21,26 +24,20 @@ class StyleSVGResource : public RefCounted<StyleSVGResource> {
                                                 const AtomicString& url) {
     return base::AdoptRef(new StyleSVGResource(resource, url));
   }
+  CORE_EXPORT ~StyleSVGResource();
 
   bool operator==(const StyleSVGResource& other) const {
     return resource_.Get() == other.resource_.Get();
   }
 
-  void AddClient(SVGResourceClient& client) {
-    if (resource_)
-      resource_->AddClient(client);
-  }
-  void RemoveClient(SVGResourceClient& client) {
-    if (resource_)
-      resource_->RemoveClient(client);
-  }
+  void AddClient(SVGResourceClient& client);
+  void RemoveClient(SVGResourceClient& client);
 
   SVGResource* Resource() const { return resource_; }
   const AtomicString& Url() const { return url_; }
 
  private:
-  StyleSVGResource(SVGResource* resource, const AtomicString& url)
-      : resource_(resource), url_(url) {}
+  StyleSVGResource(SVGResource* resource, const AtomicString& url);
 
   Persistent<SVGResource> resource_;
   const AtomicString url_;
