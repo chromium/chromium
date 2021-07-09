79177263dd3d1cbf4a33175499c7239c58e9ded7
Sammie Quon <sammiequon@chromium.org>
2021-07-09 20:18:27 +0000

    [M92] cros: Disable *PageStateAfterForwardInCompetingFrames*.
    (cherry picked from commit af485715cc13480c0cb37fe5e0ec730227ad84ef)
    
    Bug: 1227088
    Change-Id: I38e15cd100d22be71e4b86d32c6589586295d19b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011873
    Auto-Submit: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#899416}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017494
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#1432}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- content/browser/renderer_host/navigation_controller_impl_browsertest.cc
+++ content/browser/renderer_host/navigation_controller_impl_browsertest.cc
@@ -8707,7 +8707,8 @@ IN_PROC_BROWSER_TEST_P(NavigationControllerBrowserTest, NavigateTo304) {
 // Ensure that we do not corrupt a NavigationEntry's PageState if two forward
 // navigations compete in different frames.  See https://crbug.com/623319.
 // Currently flaking on Android and Mac, see https://crubug.com/1101292.
-#if defined(OS_ANDROID) || defined(OS_MAC)
+// Currently flaking on ChromeOS, see https://crbug.com/1227088.
+#if defined(OS_ANDROID) || defined(OS_MAC) || defined(OS_CHROMEOS)
 #define MAYBE_PageStateAfterForwardInCompetingFrames \
   DISABLED_PageStateAfterForwardInCompetingFrames
 #else
