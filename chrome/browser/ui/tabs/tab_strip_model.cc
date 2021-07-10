5c38a303b8c9e8b2c23705214026dd3cba52d3fa
David Pennington <dpenning@chromium.org>
2021-07-10 19:41:29 +0000

    Fix calling TabStripModel::IsTabBlocked with kNoTab
    A fuzzer found a potential heap buffer overflow caused by the
    BrowserCommandController not checking that there is an active index in
    the tab strip model before making changes. That callsite is fixed by
    adding a check before using the active_index and a DCHECK is added
    to the function for IsTabBlocked that will trigger when using kNoTab.
    
    Bug: 1216898
    Change-Id: I17b49c4cd1c24a78d6bd044f27692c8320725c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017321
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900287}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -873,6 +873,7 @@ bool TabStripModel::IsGroupCollapsed(
 }
 
 bool TabStripModel::IsTabBlocked(int index) const {
+  DCHECK(ContainsIndex(index)) << index;
   return contents_data_[index]->blocked();
 }
 
