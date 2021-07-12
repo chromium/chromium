23e106a964edc518dcb16fc6c60924837fa842e5
Ryan Huckleberry <rhuckleberry@google.com>
2021-07-12 20:35:42 +0000

    Add a new tool to symbolize trace files
    The symbolize_trace binary provides support for symbolizing trace
    files from the tools/tracing directory. Currently, this only
    supports extracting metadata. Symbolization support will be added
    in future CLs.
    
    Bug: 1224717
    Change-Id: I6065144a849d820d8ba5d6061fb15b5a571814c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017396
    Commit-Queue: Ryan Huckleberry <rhuckleberry@google.com>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900620}

---
+++ tools/tracing/symbolize_trace.py
@@ -0,0 +1,66 @@
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Symbolizes a perfetto trace file.
+"""
+
+import os
+import sys
+import shutil
+import tempfile
+
+sys.path.insert(
+    0,
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'third_party',
+                 'catapult', 'systrace'))
+
+from systrace import util
+import metadata_extractor
+
+
+def SymbolizeTrace(trace_file,
+                   cloud_storage_bucket='chrome-unsigned',
+                   trace_processor_path=None,
+                   breakpad_output_dir=None):
+  """Symbolizes a perfetto trace file.
+
+  Args:
+    trace_file: path to proto trace file to symbolize.
+    cloud_storage_bucket: bucket in cloud storage where symbols reside.
+    trace_processor_path: path to the trace_processor executable. If not
+      specified, trace processor binary will be automatically downloaded.
+    breakpad_output_dir: empty local base directory of where to save
+      breakpad symbols.
+
+  Raises:
+    Exception: if breakpad_output_dir is not empty.
+  """
+  # Ensure valid |breakpad_output_dir|
+  need_cleanup = False
+  if breakpad_output_dir is None:
+    # Create a temp dir if output dir is not provided.
+    # Temp dir must be cleaned up later.
+    breakpad_output_dir = tempfile.mkdtemp()
+    need_cleanup = True
+  else:
+    if not os.path.isdir(breakpad_output_dir):
+      os.makedirs(breakpad_output_dir)
+    else:
+      # Assert breakpad_output_dir is empty
+      if os.listdir(breakpad_output_dir):
+        raise Exception('breakpad_output_dir is not empty.')
+
+  # Extract Metadata
+  print('Extracting proto trace metadata, please wait.')
+  trace_metadata = metadata_extractor.MetadataExtractor(trace_processor_path,
+                                                        trace_file)
+  trace_metadata.Initialize()
+
+  # TODO(rhuckleberry): Fetch trace breakpad symbols from GCS.
+
+  # TODO(rhuckleberry): use Perfetto's traceconv to symbolize.
+
+  # Cleanup
+  if need_cleanup:
+    shutil.rmtree(breakpad_output_dir)
