ee17c385e5338b78a660a7bab5b05b2b7214b115
Roman Sorokin <rsorokin@chromium.org>
2021-07-12 10:58:02 +0000

    [ash-chrome] Bail after HID detection check if another screen is shown
    Bug: 1224799
    Change-Id: I664ffac15ad16cbf3a68196631b845afab5e9f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3020821
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900410}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -1809,7 +1809,7 @@ void WizardController::OnHIDScreenNecessityCheck(bool screen_needed) {
 
   // Check for tests configurations.
   if (wizard_context_->skip_to_update_for_tests ||
-      wizard_context_->skip_to_login_for_tests) {
+      wizard_context_->skip_to_login_for_tests || current_screen_) {
     return;
   }
 
