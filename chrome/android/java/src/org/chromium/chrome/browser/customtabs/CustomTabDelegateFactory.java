39a9eee668c07d34d41deffda26c1391d80cf0d0
Peter E Conn <peconn@chromium.org>
2021-07-12 18:17:20 +0000

    ❗ Use MATCH_DEFAULT_ONLY when looking for default handlers.
    A change in Android S requires the PackageManager.MATCH_DEFAULT_ONLY
    flag to be used when searching for apps that can handle a browsable
    URL. Technically the old behavior was a bug, but still worked.
    
    Change-Id: I6c7c8bd8236f48a18707b30879246a6f6c005d85
    Bug: 1226468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021087
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900555}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabDelegateFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabDelegateFactory.java
@@ -11,6 +11,7 @@ import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.text.TextUtils;
 
@@ -166,7 +167,8 @@ public class CustomTabDelegateFactory implements TabDelegateFactory {
          *         default handler, this method will return false.
          */
         private boolean hasDefaultHandler(Intent intent) {
-            ResolveInfo info = PackageManagerUtils.resolveActivity(intent, 0);
+            ResolveInfo info =
+                    PackageManagerUtils.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
             if (info == null) return false;
 
             final String chromePackage = mApplicationContext.getPackageName();
