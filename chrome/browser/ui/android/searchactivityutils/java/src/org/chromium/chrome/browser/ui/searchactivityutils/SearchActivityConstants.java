c074e2688d550a5302c798bb48f78ea7d78d3cc2
Max Tauro <maxtauro@google.com>
2021-07-12 20:44:19 +0000

    [Shortcuts Widget]  Created a new gn target to define keys for intent extras
    We're introducing this as a separate CL in order to prevent build breaks, since the string we're replacing is used in both internal & external code.
    
    This helps us avoid introducing a circular dependency between the quickactionsearchwidget:java module and the chrome_java module (since this string is used in both modules).
    
    This string is used by by https://crrev.com/2998664 and https://crrev.com/i/3950609
    
    Bug: 1213541
    Change-Id: I246de1d05cd4c0a45cfd9e0ae2d402351c32e9b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003490
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Maxwell Tauro <maxtauro@google.com>
    Cr-Commit-Position: refs/heads/master@{#900625}

---
+++ chrome/browser/ui/android/searchactivityutils/java/src/org/chromium/chrome/browser/ui/searchactivityutils/SearchActivityConstants.java
@@ -0,0 +1,17 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.searchactivityutils;
+
+/**
+ * Constant Strings to be used by {@link SearchActivity}
+ */
+public class SearchActivityConstants {
+    /**
+     * Key for the boolean extra to be passed to {@link SearchActivity} to indicate whether voice
+     * search should be started or not.
+     */
+    public static final String EXTRA_SHOULD_START_VOICE_SEARCH =
+            "org.chromium.chrome.browser.ui.searchactivityutils.SHOULD_START_VOICE_SEARCH";
+}
