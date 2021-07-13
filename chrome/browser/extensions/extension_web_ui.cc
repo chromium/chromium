dacf1ebff693649c566301a9374ce53a4058c5e5
Maggie Cai <mxcai@google.com>
2021-07-13 02:51:15 +0000

    [CodeHealth] Remove the usage of ListValue::Insert() from ExtensionWebUI.
    This CL removes the deprecated ListValue::Insert() API from
    ExtensionWebUI. Also this CL removes some other deprecated
    DictionaryValue and ListValue usage that introduced by this removal.
    
    BUG=1187105
    
    Change-Id: Ic76883bed1259d77ce3923fbbac2cab81dd72f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016847
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900799}

--- chrome/browser/extensions/extension_web_ui.cc
+++ chrome/browser/extensions/extension_web_ui.cc
@@ -102,7 +102,7 @@ void InitializeOverridesList(base::ListValue* list) {
 
 // Adds |override| to |list|, or, if there's already an entry for the override,
 // marks it as active.
-void AddOverridesToList(base::ListValue* list, const GURL& override_url) {
+void AddOverridesToList(base::Value* list, const GURL& override_url) {
   const std::string& spec = override_url.spec();
   for (auto& val : list->GetList()) {
     std::string* entry = nullptr;
@@ -129,11 +129,11 @@ void AddOverridesToList(base::ListValue* list, const GURL& override_url) {
     }
   }
 
-  auto dict = std::make_unique<base::DictionaryValue>();
-  dict->SetString(kEntry, spec);
-  dict->SetBoolean(kActive, true);
+  base::Value dict(base::Value::Type::DICTIONARY);
+  dict.SetStringPath(kEntry, spec);
+  dict.SetBoolPath(kActive, true);
   // Add the entry to the front of the list.
-  list->Insert(0, std::move(dict));
+  list->Insert(list->GetList().begin(), std::move(dict));
 }
 
 // Validates that each entry in |list| contains a valid url and points to an
@@ -539,14 +539,13 @@ void ExtensionWebUI::RegisterOrActivateChromeURLOverrides(
     return;
   PrefService* prefs = profile->GetPrefs();
   DictionaryPrefUpdate update(prefs, kExtensionURLOverrides);
-  base::DictionaryValue* all_overrides = update.Get();
+  base::Value* all_overrides = update.Get();
   for (const auto& page_override_pair : overrides) {
-    base::ListValue* page_overrides_weak = nullptr;
-    if (!all_overrides->GetList(page_override_pair.first,
-                                &page_overrides_weak)) {
-      auto page_overrides = std::make_unique<base::ListValue>();
-      page_overrides_weak = page_overrides.get();
-      all_overrides->Set(page_override_pair.first, std::move(page_overrides));
+    base::Value* page_overrides_weak =
+        all_overrides->FindListPath(page_override_pair.first);
+    if (page_overrides_weak == nullptr) {
+      page_overrides_weak = all_overrides->SetPath(
+          page_override_pair.first, base::Value(base::Value::Type::LIST));
     }
     AddOverridesToList(page_overrides_weak, page_override_pair.second);
   }
