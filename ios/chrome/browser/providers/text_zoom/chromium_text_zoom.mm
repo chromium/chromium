f087c0a01aef735333b41ca59a13636b114ac3be
Sylvain Defresne <sdefresne@chromium.org>
2021-07-13 15:48:49 +0000

    [ios] Expose `text_zoom` API as free function
    Add two free functions exposing the `text_zoom` API with the goal
    to replace `TextZoomProvider` class. Do not convert the code to use
    the function as they have no internal implementation yet.
    
    Bug: 1201182
    Change-Id: Ib9a667dfb922e3fe86aa5c9c36b94da39dd9eefe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024085
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901028}

---
+++ ios/chrome/browser/providers/text_zoom/chromium_text_zoom.mm
@@ -0,0 +1,27 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/web/features.h"
+#import "ios/public/provider/chrome/browser/font_size_java_script_feature.h"
+#import "ios/public/provider/chrome/browser/text_zoom/text_zoom_api.h"
+#include "ui/base/device_form_factor.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ios {
+namespace provider {
+
+void SetTextZoomForWebState(web::WebState* web_state, int size) {
+  FontSizeJavaScriptFeature::GetInstance()->AdjustFontSize(web_state, size);
+}
+
+bool IsTextZoomEnabled() {
+  return ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_TABLET &&
+         base::FeatureList::IsEnabled(web::kWebPageTextAccessibility);
+}
+
+}  // namespace provider
+}  // namespace ios
