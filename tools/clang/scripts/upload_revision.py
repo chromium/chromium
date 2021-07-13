b320f1ff086e90fe308cd5eb5aa6425a8c4979a9
Nico Weber <thakis@chromium.org>
2021-07-13 10:37:03 +0000

    clang script: Add android-asan to roll trybots.
    Bug: 1228392
    Change-Id: I58dfc469dc36545c49acf2bf9a7f13f824c36b1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022201
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900931}

--- tools/clang/scripts/upload_revision.py
+++ tools/clang/scripts/upload_revision.py
@@ -31,6 +31,7 @@ COMMIT_FOOTER = \
 '''
 Bug: TODO. Remove the Tricium: line below when filling this in.
 Tricium: skip
+Cq-Include-Trybots: chromium/try:android-asan
 Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
 Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
 Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
