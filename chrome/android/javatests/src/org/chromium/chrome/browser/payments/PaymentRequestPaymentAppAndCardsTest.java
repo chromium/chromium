a178f05e40b68e2509cb6c5f158ad0d7554bf2cc
Darren Shen <shend@chromium.org>
2021-07-13 12:42:07 +0000

    sheriff: Disable flaky test.
    PaymentRequestPaymentAppAndCardsTest#
    testAddPaymentMethodAndCancelEditorShouldKeepExistingCardSelected
    has been flaky for a while now:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVytwELEgVGbGFrZSKrAWNocm9taXVtQGNocm9tZV9wdWJsaWNfdGVzdF9hcGtAb3JnLmNocm9taXVtLmNocm9tZS5icm93c2VyLnBheW1lbnRzLlBheW1lbnRSZXF1ZXN0UGF5bWVudEFwcEFuZENhcmRzVGVzdCN0ZXN0QWRkUGF5bWVudE1ldGhvZEFuZENhbmNlbEVkaXRvclNob3VsZEtlZXBFeGlzdGluZ0NhcmRTZWxlY3RlZAw
    
    Bug: 1222954
    Change-Id: Ica073378840777f4f90e6fb8407dcba67800c64a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023689
    Owners-Override: Darren Shen <shend@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#900965}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestPaymentAppAndCardsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestPaymentAppAndCardsTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
@@ -127,6 +128,7 @@ public class PaymentRequestPaymentAppAndCardsTest implements MainActivityStartCa
     /** Test that going into "add" flow editor and cancelling will leave existing row checked. */
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1222954")
     @Feature({"Payments"})
     public void testAddPaymentMethodAndCancelEditorShouldKeepExistingCardSelected()
             throws TimeoutException {
