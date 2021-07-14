228c6234594a0f8d2d84be1372112eaac4605caf
Chad Duffin <chadduffin@chromium.org>
2021-07-14 22:25:01 +0000

    [CrOS Cellular] Cond. change roaming pref default.
    This change updates the default value used for the cellular roaming
    preference when the feature flag to enable per-network cellular roaming
    configuration is on and the device in question is not managed by an
    enterprise policy. The default is now |true| since a non-enterprise
    policy managed device should always have the device
    Cellular.PolicyAllowRoaming Shill property be |true|.
    
    Bug: 1220691
    Test: Checked that the value can only be changed via enterprise policy.
    Change-Id: I674c3015103016a79e483a1c842933c3bfcbf155
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024502
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901670}

--- chrome/browser/ash/settings/device_settings_provider.cc
+++ chrome/browser/ash/settings/device_settings_provider.cc
@@ -15,6 +15,7 @@
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -481,13 +482,20 @@ void DecodeLoginPolicies(const em::ChromeDeviceSettingsProto& policy,
 
 void DecodeNetworkPolicies(const em::ChromeDeviceSettingsProto& policy,
                            PrefValueMap* new_values_cache) {
-  // kSignedDataRoamingEnabled has a default value of false.
-  new_values_cache->SetBoolean(
-      kSignedDataRoamingEnabled,
-      policy.has_data_roaming_enabled() &&
-          policy.data_roaming_enabled().has_data_roaming_enabled() &&
-          policy.data_roaming_enabled().data_roaming_enabled());
-
+  // Device-level cellular roaming should always be enabled for devices not
+  // enrolled in an enterprise policy when per-network cellular roaming
+  // configuration is enabled.
+  if (base::FeatureList::IsEnabled(
+          ash::features::kCellularAllowPerNetworkRoaming) &&
+      !chromeos::InstallAttributes::Get()->IsEnterpriseManaged()) {
+    new_values_cache->SetBoolean(kSignedDataRoamingEnabled, true);
+  } else {
+    new_values_cache->SetBoolean(
+        kSignedDataRoamingEnabled,
+        policy.has_data_roaming_enabled() &&
+            policy.data_roaming_enabled().has_data_roaming_enabled() &&
+            policy.data_roaming_enabled().data_roaming_enabled());
+  }
   if (policy.has_system_proxy_settings()) {
     const em::SystemProxySettingsProto& settings_proto(
         policy.system_proxy_settings());
