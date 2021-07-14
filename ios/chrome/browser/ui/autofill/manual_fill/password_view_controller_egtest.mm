a42b85c256707ced5d254b5197072a7152bc65fb
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-07-14 20:34:59 +0000

    [sheriff] Disable PasswordViewControllerTestCase/testPasswordControllerKeepsRightSize
    Bug: 1226894
    Change-Id: I8e28aa8b8fa6ed4eb99a40938829848405e37f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027765
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@google.com>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Cr-Commit-Position: refs/heads/master@{#901598}

--- ios/chrome/browser/ui/autofill/manual_fill/password_view_controller_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/password_view_controller_egtest.mm
@@ -100,10 +100,11 @@ id<GREYMatcher> CancelUsingOtherPasswordButton() {
     config.features_enabled.push_back(
         password_manager::features::kEnableManualPasswordGeneration);
   }
-  if ([self isRunningTest:@selector(testPasswordControllerKeepsRightSize)]) {
-    config.features_disabled.push_back(
-        password_manager::features::kEnableManualPasswordGeneration);
-  }
+  // TODO(crbug.com/1226894): Uncomment this once the test is fixed.
+  // if ([self isRunningTest:@selector(testPasswordControllerKeepsRightSize)]) {
+  //   config.features_disabled.push_back(
+  //       password_manager::features::kEnableManualPasswordGeneration);
+  // }
   return config;
 }
 
@@ -510,7 +511,8 @@ id<GREYMatcher> CancelUsingOtherPasswordButton() {
 
 // Tests that after switching fields the content size of the table view didn't
 // grow.
-- (void)testPasswordControllerKeepsRightSize {
+// TODO(crbug.com/1226894): Disabled due to flakiness.
+- (void)DISABLED_testPasswordControllerKeepsRightSize {
   // Bring up the keyboard.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
       performAction:TapWebElementWithId(kFormElementUsername)];
