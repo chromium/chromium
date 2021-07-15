208ae2abc2824cffac6f16b66356df92ed213b8e
Ionel Popescu <iopopesc@microsoft.com>
2021-07-15 20:44:06 +0000

    [EyeDropper API] Update implementation to match explainer.
    Explainer:
    https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/EyeDropper/explainer.md
    
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/cPTMI1i6d7I/
    
    The explainer was updated to address the tag review:
    - moved to a promise based design (EyeDropper.open() returns a
    Promise<ColorSelectionResult>)
    - renamed the selected color property to sRGBHex to make it clear that
    the color is a string and it is in sRGB (this allows room for future
    properties to be added)
    
    For now a manual test has been added, more test coverage is going to be
    added in follow-up CLs.
    
    Bug: 897309
    Change-Id: Ib3631b94ff43cd59ba1640f0c295e233f058383a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027321
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#902121}

--- third_party/blink/renderer/modules/eyedropper/eye_dropper.cc
+++ third_party/blink/renderer/modules/eyedropper/eye_dropper.cc
@@ -4,45 +4,101 @@
 
 #include "third_party/blink/renderer/modules/eyedropper/eye_dropper.h"
 
-#include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/modules/event_target_modules.h"
+#include "third_party/blink/public/common/browser_interface_broker_proxy.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_color_selection_result.h"
+#include "third_party/blink/renderer/core/dom/dom_exception.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/graphics/color.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
-EyeDropper::EyeDropper(ScriptState* script_state)
-    : ExecutionContextClient(ExecutionContext::From(script_state)),
-      opened_(false) {}
+EyeDropper::EyeDropper(ExecutionContext* context)
+    : eye_dropper_chooser_(context) {}
 
-EyeDropper* EyeDropper::Create(ScriptState* script_state) {
-  return MakeGarbageCollected<EyeDropper>(script_state);
+EyeDropper* EyeDropper::Create(ExecutionContext* context) {
+  return MakeGarbageCollected<EyeDropper>(context);
 }
 
-ExecutionContext* EyeDropper::GetExecutionContext() const {
-  return ExecutionContextClient::GetExecutionContext();
+ScriptPromise EyeDropper::open(ScriptState* script_state,
+                               ExceptionState& exception_state) {
+  DCHECK(RuntimeEnabledFeatures::EyeDropperAPIEnabled());
+
+  if (!script_state->ContextIsValid()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidStateError,
+        "The object is no longer associated with a window.");
+    return ScriptPromise();
+  }
+
+  LocalDOMWindow* window = LocalDOMWindow::From(script_state);
+  if (!LocalFrame::HasTransientUserActivation(window->GetFrame())) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidAccessError,
+        "EyeDropper::open() requires user gesture.");
+    return ScriptPromise();
+  }
+
+  if (eye_dropper_chooser_.is_bound()) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                      "EyeDropper is already open.");
+    return ScriptPromise();
+  }
+
+  auto* resolver_ = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  ScriptPromise promise = resolver_->Promise();
+
+  auto* frame = window->GetFrame();
+  frame->GetBrowserInterfaceBroker().GetInterface(
+      eye_dropper_chooser_.BindNewPipeAndPassReceiver(
+          frame->GetTaskRunner(TaskType::kUserInteraction)));
+  eye_dropper_chooser_.set_disconnect_handler(
+      WTF::Bind(&EyeDropper::EndChooser, WrapWeakPersistent(this)));
+  eye_dropper_chooser_->Choose(WTF::Bind(&EyeDropper::EyeDropperResponseHandler,
+                                         WrapPersistent(this),
+                                         WrapPersistent(resolver_)));
+
+  return promise;
 }
 
-const AtomicString& EyeDropper::InterfaceName() const {
-  return event_target_names::kEyeDropper;
+void EyeDropper::EyeDropperResponseHandler(ScriptPromiseResolver* resolver,
+                                           bool success,
+                                           uint32_t color) {
+  eye_dropper_chooser_.reset();
+
+  if (!resolver->GetExecutionContext() ||
+      resolver->GetExecutionContext()->IsContextDestroyed()) {
+    return;
+  }
+
+  if (success) {
+    ColorSelectionResult* result = ColorSelectionResult::Create();
+    result->setSRGBHex(Color(color).Serialized());
+    resolver->Resolve(result);
+  } else {
+    resolver->Reject(MakeGarbageCollected<DOMException>(
+        DOMExceptionCode::kOperationError, "Unable to select a color."));
+  }
 }
 
-EyeDropper::~EyeDropper() = default;
-
-bool EyeDropper::opened() const {
-  return opened_;
-}
-
-ScriptPromise EyeDropper::open() {
-  // TODO(iopopesc): Add support for open.
-  return ScriptPromise();
-}
-
-void EyeDropper::close() {
-  // TODO(iopopesc): Add support for close.
+void EyeDropper::EndChooser() {
+  eye_dropper_chooser_.reset();
+  if (resolver_) {
+    resolver_->Reject(
+        MakeGarbageCollected<DOMException>(DOMExceptionCode::kNotSupportedError,
+                                           "EyeDropper API is not available."));
+    resolver_ = nullptr;
+  }
 }
 
 void EyeDropper::Trace(Visitor* visitor) const {
-  EventTargetWithInlineData::Trace(visitor);
-  ExecutionContextClient::Trace(visitor);
+  visitor->Trace(eye_dropper_chooser_);
+  visitor->Trace(resolver_);
+  ScriptWrappable::Trace(visitor);
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/eyedropper/eye_dropper.h
+++ third_party/blink/renderer/modules/eyedropper/eye_dropper.h
@@ -6,12 +6,16 @@
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_EYEDROPPER_EYE_DROPPER_H_
 
 #include "base/macros.h"
+#include "third_party/blink/public/mojom/choosers/color_chooser.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
-#include "third_party/blink/renderer/core/dom/events/event_target.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 
 namespace blink {
+
+class ExceptionState;
+class ScriptPromise;
+class ScriptPromiseResolver;
 
 // The EyeDropper API enables developers to use a browser-supplied eyedropper
 // in their web applications. This feature is still
@@ -19,39 +23,28 @@ namespace blink {
 // by passing --enable-blink-features=EyeDropperAPI. See
 // https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/EyeDropper/explainer.md
 // for more details.
-class EyeDropper final : public EventTargetWithInlineData,
-                         public ExecutionContextClient {
+class EyeDropper final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  explicit EyeDropper(ScriptState*);
-  static EyeDropper* Create(ScriptState*);
+  explicit EyeDropper(ExecutionContext*);
+  static EyeDropper* Create(ExecutionContext*);
   EyeDropper(const EyeDropper&) = delete;
   EyeDropper& operator=(const EyeDropper&) = delete;
-  ~EyeDropper() override;
+  ~EyeDropper() override = default;
 
-  // EventTarget:
-  ExecutionContext* GetExecutionContext() const override;
-  const AtomicString& InterfaceName() const override;
-
-  // Opens the eyedropper and replaces the cursor with a browser-defined preview
-  // and sets opened boolean to true.
-  ScriptPromise open();
-
-  // Exits the eyedropper mode and the cursor returns to its regular
-  // functionality. Sets opened boolean to false.
-  void close();
-
-  // States if the eyedropper is opened and in use.
-  bool opened() const;
-
-  DEFINE_ATTRIBUTE_EVENT_LISTENER(colorselect, kColorselect)
-  DEFINE_ATTRIBUTE_EVENT_LISTENER(close, kClose)
+  // Opens the eyedropper and replaces the cursor with a browser-defined
+  // preview.
+  ScriptPromise open(ScriptState*, ExceptionState&);
 
   void Trace(Visitor*) const override;
 
  private:
-  bool opened_;
+  void EyeDropperResponseHandler(ScriptPromiseResolver*, bool, uint32_t);
+  void EndChooser();
+
+  HeapMojoRemote<mojom::blink::EyeDropperChooser> eye_dropper_chooser_;
+  Member<ScriptPromiseResolver> resolver_;
 };
 
 }  // namespace blink
