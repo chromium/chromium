607667ba5ea50d89bc9a06b12dfea2dc0eba241f
Joel Hockey <joelhockey@chromium.org>
2021-07-15 14:43:58 +0000

    Remove deprecated_is_app() call in MultiProfileSupport
    Replaced call to Browser::deprecated_is_app() by checking is_type_app()
    and is_type_app_popup().
    
    This is a change of logic since it is now ignoring the case of
    is_type_devtools(), however I don't expect a devtools browser should be
    affected.
    
    Bug: 990158
    Change-Id: Id9039241a41948ea772687958d82d1e65962b0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026658
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901945}

--- chrome/browser/ui/ash/multi_user/multi_profile_support.cc
+++ chrome/browser/ui/ash/multi_user/multi_profile_support.cc
@@ -52,7 +52,7 @@ void RecordUMAForTransferredWindowType(aura::Window* window) {
   if (browser) {
     if (browser->profile()->IsOffTheRecord()) {
       window_type = TELEPORT_WINDOW_INCOGNITO_BROWSER;
-    } else if (browser->deprecated_is_app()) {
+    } else if (browser->is_type_app() || browser->is_type_app_popup()) {
       window_type = TELEPORT_WINDOW_V1_APP;
     } else if (browser->is_type_popup()) {
       window_type = TELEPORT_WINDOW_POPUP;
