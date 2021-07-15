dbfa5ce833569d8603cc1b97d3a52731df5aa40a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-07-15 16:23:54 +0000

    Import wpt@b00170f86bf1bf4a368003b694f5506f011706c4
    Using wpt-import in Chromium 56ddd6d4aa136c7dcda78452402fb7cc080f8e68.
    With Chromium commits locally applied on WPT:
    ba1d78b8e7 "WindowPlacement: remove deprecated screen_enumeration APIs"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    foolip@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    jochen@chromium.org, estark@chromium.org:
      external/wpt/referrer-policy
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I34cb332255c266da299d1dc0f2b7c9a71b6038c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031421
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#901991}

--- third_party/blink/web_tests/external/wpt/infrastructure/assumptions/non-local-ports.sub.window.js
+++ third_party/blink/web_tests/external/wpt/infrastructure/assumptions/non-local-ports.sub.window.js
@@ -8,6 +8,13 @@
 //
 // More tests can be found in `fetch/private-network-access/`.
 
+const alternatePorts = {
+  httpPrivate:  "{{ports[http-private][0]}}",
+  httpsPrivate: "{{ports[https-private][0]}}",
+  httpPublic:   "{{ports[http-public][0]}}",
+  httpsPublic:  "{{ports[https-public][0]}}",
+};
+
 // Resolves a URL relative to the current location, returning an absolute URL.
 //
 // `url` specifies the relative URL, e.g. "foo.html" or "http://foo.example".
@@ -30,38 +37,75 @@ function resolveUrl(url, options) {
   return result;
 }
 
-promise_test(async () => {
-  const url = resolveUrl("/common/blank-with-cors.html", {
+const alternateOrigins = {
+  httpPrivate: {
+    protocol: "http:",
+    port: alternatePorts.httpPrivate,
+  },
+  httpsPrivate: {
     protocol: "https:",
-    port: "{{ports[https-private][0]}}"
-  });
+    port: alternatePorts.httpsPrivate,
+  },
+  httpPublic: {
+    protocol: "http:",
+    port: alternatePorts.httpPublic,
+  },
+  httpsPublic: {
+    protocol: "https:",
+    port: alternatePorts.httpsPublic,
+  },
+};
+
+promise_test(async () => {
+  const url =
+    resolveUrl("/common/blank-with-cors.html", alternateOrigins.httpsPrivate);
   const response = await fetch(url);
   assert_true(response.ok);
 }, "Fetch from https-private port works.");
 
 promise_test(async () => {
-  const url = resolveUrl("/common/blank-with-cors.html", {
-    protocol: "http:",
-    port: "{{ports[http-private][0]}}"
-  });
+  const url =
+    resolveUrl("/common/blank-with-cors.html", alternateOrigins.httpPrivate);
   const response = await fetch(url);
   assert_true(response.ok);
 }, "Fetch from http-private port works.");
 
 promise_test(async () => {
-  const url = resolveUrl("/common/blank-with-cors.html", {
-    protocol: "https:",
-    port: "{{ports[https-public][0]}}"
-  });
+  const url =
+    resolveUrl("/common/blank-with-cors.html", alternateOrigins.httpsPublic);
   const response = await fetch(url);
   assert_true(response.ok);
 }, "Fetch from https-public port works.");
 
 promise_test(async () => {
-  const url = resolveUrl("/common/blank-with-cors.html", {
-    protocol: "http:",
-    port: "{{ports[http-public][0]}}"
-  });
+  const url =
+    resolveUrl("/common/blank-with-cors.html", alternateOrigins.httpPublic);
   const response = await fetch(url);
   assert_true(response.ok);
 }, "Fetch from http-public port works.");
+
+promise_test(async (t) => {
+  const futureMessage = new Promise((resolve) => {
+    window.addEventListener("message", resolve);
+  });
+
+  const iframe = await new Promise((resolve, reject) => {
+    const iframe = document.createElement("iframe");
+    iframe.src = resolveUrl("resources/fetch-and-post-result.html",
+                            alternateOrigins.httpPublic);
+
+    iframe.onload = () => { resolve(iframe); };
+    iframe.onerror = reject;
+
+    document.body.appendChild(iframe);
+    t.add_cleanup(() => {
+      document.body.removeChild(iframe);
+    });
+  });
+
+  iframe.contentWindow.postMessage(
+    resolveUrl("/common/blank-with-cors.html").toString(), "*");
+
+  const evt = await futureMessage;
+  assert_equals(evt.data, "failure: error = TypeError");
+}, "Fetch from http-public to local http fails.");
