247e241cab884aeff272dc7573d647f182453554
Joe Mason <joenotcharles@google.com>
2021-07-15 19:27:43 +0000

    Re-enable SamplingHeapProfilerTest.ConcurrentStartStop on Mac
    SamplingHeapProfilerTest.StartStop is not flaking, so time to enable
    ConcurrentStartStop to see if it continues to flake, and gather logs if
    so.
    
    R=erikchen
    
    Bug: 1116543
    Change-Id: Idf2b077e62e34ac70cb4abf7ec7e7fca57278d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032008
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902069}

--- base/sampling_heap_profiler/sampling_heap_profiler_unittest.cc
+++ base/sampling_heap_profiler/sampling_heap_profiler_unittest.cc
@@ -289,19 +289,7 @@ TEST_F(SamplingHeapProfilerTest, StartStop) {
   EXPECT_EQ(0, GetRunningSessionsCount());
 }
 
-// TODO(crbug.com/1116543): When this was part of StartStop, the whole test was
-// flaky on Mac. If StartStop continues to flake, the problem is probably in
-// PoissonAllocationSampler::InstallAllocatorHooksOnce, which runs on the first
-// call to SamplingHeapProfiler::Start. Otherwise, try re-enabling this part
-// too to see if it's still flaky; if so, the problem is probably due to
-// `thread` and the main thread both calling RunStartStopLoop and contending
-// over some shared resource.
-#if defined(OS_MAC)
-#define MAYBE_ConcurrentStartStop DISABLED_ConcurrentStartStop
-#else
-#define MAYBE_ConcurrentStartStop ConcurrentStartStop
-#endif
-TEST_F(SamplingHeapProfilerTest, MAYBE_ConcurrentStartStop) {
+TEST_F(SamplingHeapProfilerTest, ConcurrentStartStop) {
   auto* profiler = SamplingHeapProfiler::Get();
   WaitableEvent event;
   StartStopThread thread(&event);
