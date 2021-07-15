cddb85f4a4da29937a401d6567f67e4d465fe2f9
Ilya Nikolaevskiy <ilnik@chromium.org>
2021-07-15 16:41:40 +0000

    Disable WebRtcBrowserTest.CanSetupVideoCallWith16To9AspectRatio on android
    Bug: webrtc:12984
    Change-Id: I3a6f66d929a2b698e8c126325608ac02daed6353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031186
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Auto-Submit: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901999}

--- content/browser/webrtc/webrtc_browsertest.cc
+++ content/browser/webrtc/webrtc_browsertest.cc
@@ -24,12 +24,18 @@
 
 namespace content {
 
-#if defined(OS_ANDROID) && defined(ADDRESS_SANITIZER)
+#if defined(OS_ANDROID)
+#if defined(ADDRESS_SANITIZER)
 // Renderer crashes under Android ASAN: https://crbug.com/408496.
 #define MAYBE_WebRtcBrowserTest DISABLED_WebRtcBrowserTest
 #else
+// WebRtcBrowserTest.CanSetupVideoCallWith16To9AspectRatio fails consistently
+// on android
+#define MAYBE_WebRtcBrowserTest DISABLED_WebRtcBrowserTest
+#endif  // defined(ADDRESS_SANITIZER)
+#else
 #define MAYBE_WebRtcBrowserTest WebRtcBrowserTest
-#endif
+#endif  // defined(OS_ANDROID)
 
 // This class tests the scenario when permission to access mic or camera is
 // granted.
