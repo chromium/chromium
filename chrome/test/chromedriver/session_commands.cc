53f9924abe20b0385b682f6f81951cc6b2e47d61
Maksim Sadym <sadym@chromium.org>
2021-07-16 07:18:53 +0000

    CodeHealth: Remove use of DictionaryValue::GetStringWithoutPathExpansion
    This change removes the use of DictionaryValue::GetStringWithoutPathExpansion and replaces with Value::FindStringKey().
    
    * chrome/test/chromedriver/session_commands.cc
    
    Bug: 1187046
    Change-Id: I3411034f0f35c8b5d7ee8e079e3de63346ef968f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031064
    Auto-Submit: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902348}

--- chrome/test/chromedriver/session_commands.cc
+++ chrome/test/chromedriver/session_commands.cc
@@ -394,25 +394,25 @@ Status ConfigureSession(Session* session,
 
 Status ConfigureHeadlessSession(Session* session,
                                 const Capabilities& capabilities) {
-  if (session->chrome->GetBrowserInfo()->is_headless) {
-    std::string download_directory;
-    if (capabilities.prefs &&
-        (capabilities.prefs->GetString("download.default_directory",
-                                       &download_directory) ||
-         capabilities.prefs->GetStringWithoutPathExpansion(
-             "download.default_directory", &download_directory)))
-      session->headless_download_directory =
-          std::make_unique<std::string>(download_directory);
-    else
-      session->headless_download_directory = std::make_unique<std::string>(".");
-    WebView* first_view;
-    session->chrome->GetWebViewById(session->window, &first_view);
-    Status status = first_view->OverrideDownloadDirectoryIfNeeded(
-        *session->headless_download_directory);
-    return status;
+  if (!session->chrome->GetBrowserInfo()->is_headless)
+    return Status(kOk);
+
+  const std::string* download_directory = nullptr;
+  if (capabilities.prefs) {
+    download_directory =
+        capabilities.prefs->FindStringPath("download.default_directory");
+    if (!download_directory) {
+      download_directory =
+          capabilities.prefs->FindStringKey("download.default_directory");
+    }
   }
-  // session is not headless
-  return Status(kOk);
+  session->headless_download_directory = std::make_unique<std::string>(
+      download_directory ? *download_directory : ".");
+
+  WebView* first_view;
+  session->chrome->GetWebViewById(session->window, &first_view);
+  return first_view->OverrideDownloadDirectoryIfNeeded(
+      *session->headless_download_directory);
 }
 
 }  // namespace internal
