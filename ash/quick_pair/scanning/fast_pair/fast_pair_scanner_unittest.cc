e79014e916efa2baae47601312ac871e3549f049
Juliet Levesque <julietlevesque@google.com>
2021-07-16 22:40:18 +0000

    [Fast Pair] Add tests to FastPairScanner.
    Add tests for device added, device lost, and device changed events in
    FastPairScanner.
    
    Fixed: 1227522
    Change-Id: I660074ffe3c65e5627c7eb5530009cbee3c999d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032501
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#902708}

--- ash/quick_pair/scanning/fast_pair/fast_pair_scanner_unittest.cc
+++ ash/quick_pair/scanning/fast_pair/fast_pair_scanner_unittest.cc
@@ -6,9 +6,13 @@
 
 #include <memory>
 
+#include "ash/quick_pair/common/constants.h"
+#include "base/memory/weak_ptr.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
+#include "device/bluetooth/bluetooth_low_energy_scan_filter.h"
 #include "device/bluetooth/test/mock_bluetooth_adapter.h"
 #include "device/bluetooth/test/mock_bluetooth_device.h"
+#include "device/bluetooth/test/mock_bluetooth_low_energy_scan_session.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -19,9 +23,14 @@ constexpr char kTestBleDeviceAddress[] = "11:12:13:14:15:16";
 constexpr char kTestBleDeviceName[] = "Test Device Name";
 
 std::unique_ptr<device::MockBluetoothDevice> CreateTestBluetoothDevice() {
-  return std::make_unique<testing::NiceMock<device::MockBluetoothDevice>>(
-      /*adapter=*/nullptr, /*bluetooth_class=*/0, kTestBleDeviceName,
-      kTestBleDeviceAddress, /*paired=*/true, /*connected=*/true);
+  auto mock_device =
+      std::make_unique<testing::NiceMock<device::MockBluetoothDevice>>(
+          /*adapter=*/nullptr, /*bluetooth_class=*/0, kTestBleDeviceName,
+          kTestBleDeviceAddress, /*paired=*/true, /*connected=*/true);
+  mock_device->AddUUID(ash::quick_pair::kFastPairBluetoothUuid);
+  mock_device->SetServiceDataForUUID(ash::quick_pair::kFastPairBluetoothUuid,
+                                     {1, 2, 3});
+  return mock_device;
 }
 
 }  // namespace
@@ -47,6 +56,16 @@ class FakeBluetoothAdapter
       observer.DeviceRemoved(this, mock_device.get());
   }
 
+  void ChangeDevice() {
+    auto mock_device = CreateTestBluetoothDevice();
+    auto* mock_device_ptr = mock_device.get();
+    mock_device->SetServiceDataForUUID(ash::quick_pair::kFastPairBluetoothUuid,
+                                       {4, 5, 6});
+
+    for (auto& observer : GetObservers())
+      observer.DeviceChanged(this, mock_device_ptr);
+  }
+
  private:
   ~FakeBluetoothAdapter() override = default;
 };
@@ -56,6 +75,7 @@ class FastPairScannerObserver : public FastPairScanner::Observer {
   // FastPairScanner::Observer overrides
   void OnDeviceFound(device::BluetoothDevice* device) override {
     device_addreses_.push_back(device->GetAddress());
+    on_device_found_count_++;
   }
 
   void OnDeviceLost(device::BluetoothDevice* device) override {
@@ -69,23 +89,25 @@ class FastPairScannerObserver : public FastPairScanner::Observer {
                      kTestBleDeviceAddress) != device_addreses_.end();
   }
 
+  int on_device_found_count() { return on_device_found_count_; }
+
  private:
   std::vector<std::string> device_addreses_;
+  int on_device_found_count_ = 0;
 };
 
 class FastPairScannerTest : public testing::Test {
  public:
   void SetUp() override {
     adapter_ = base::MakeRefCounted<FakeBluetoothAdapter>();
+    ON_CALL(*adapter_, StartLowEnergyScanSession(testing::_, testing::_))
+        .WillByDefault(
+            Invoke(this, &FastPairScannerTest::StartLowEnergyScanSession));
     device::BluetoothAdapterFactory::SetAdapterForTesting(adapter_);
     EXPECT_CALL(adapter(), AddObserver);
     scanner_ = std::make_unique<FastPairScannerImpl>();
     scanner_observer_ = std::make_unique<FastPairScannerObserver>();
     scanner().AddObserver(scanner_observer_.get());
-
-    // TODO(crbug.com/1227522) Add support for the Bluetooth LE Scanner here
-    // when available, as well as new tests to check for the correct behavior
-    // when a device is found and removed.
   }
 
   void TearDown() override {
@@ -93,7 +115,26 @@ class FastPairScannerTest : public testing::Test {
     scanner().RemoveObserver(scanner_observer_.get());
   }
 
+  std::unique_ptr<device::BluetoothLowEnergyScanSession>
+  StartLowEnergyScanSession(
+      std::unique_ptr<device::BluetoothLowEnergyScanFilter> filter,
+      base::WeakPtr<device::BluetoothLowEnergyScanSession::Delegate> delegate) {
+    auto mock_scan_session =
+        std::make_unique<device::MockBluetoothLowEnergyScanSession>(
+            base::BindOnce(&FastPairScannerTest::OnScanSessionDestroyed,
+                           weak_ptr_factory_.GetWeakPtr()));
+    mock_scan_session_ = mock_scan_session.get();
+    delegate_ = delegate;
+    return mock_scan_session;
+  }
+
+  void OnScanSessionDestroyed() { mock_scan_session_ = nullptr; }
+
   FakeBluetoothAdapter& adapter() { return *(adapter_.get()); }
+
+  device::MockBluetoothLowEnergyScanSession* scan_session() {
+    return mock_scan_session_;
+  }
 
   FastPairScannerObserver& scanner_observer() {
     return *(scanner_observer_.get());
@@ -101,11 +142,43 @@ class FastPairScannerTest : public testing::Test {
 
   FastPairScanner& scanner() { return *(scanner_.get()); }
 
+  void TriggerOnDeviceFound() {
+    delegate_->OnDeviceFound(mock_scan_session_,
+                             CreateTestBluetoothDevice().get());
+  }
+
+  void TriggerOnDeviceLost() {
+    delegate_->OnDeviceLost(mock_scan_session_,
+                            CreateTestBluetoothDevice().get());
+  }
+
  protected:
   scoped_refptr<FakeBluetoothAdapter> adapter_;
   std::unique_ptr<FastPairScannerImpl> scanner_;
+  device::MockBluetoothLowEnergyScanSession* mock_scan_session_ = nullptr;
   std::unique_ptr<FastPairScannerObserver> scanner_observer_;
+  base::WeakPtr<device::BluetoothLowEnergyScanSession::Delegate> delegate_;
+  base::WeakPtrFactory<FastPairScannerTest> weak_ptr_factory_{this};
 };
+
+TEST_F(FastPairScannerTest, DeviceAddedNotifiesObservers) {
+  TriggerOnDeviceFound();
+  EXPECT_TRUE(scanner_observer().DoesDeviceListContainTestDevice());
+}
+
+TEST_F(FastPairScannerTest, DeviceLostNotifiesObservers) {
+  TriggerOnDeviceFound();
+  EXPECT_TRUE(scanner_observer().DoesDeviceListContainTestDevice());
+  TriggerOnDeviceLost();
+  EXPECT_FALSE(scanner_observer().DoesDeviceListContainTestDevice());
+}
+
+TEST_F(FastPairScannerTest, DeviceChangedNotifiesObservors) {
+  TriggerOnDeviceFound();
+  EXPECT_EQ(scanner_observer().on_device_found_count(), 1);
+  adapter().ChangeDevice();
+  EXPECT_EQ(scanner_observer().on_device_found_count(), 2);
+}
 
 }  // namespace quick_pair
 }  // namespace ash
