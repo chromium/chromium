024c8ebdff6dc7ea0d8de01c0ea13ffff75e82bb
Daniel Libby <dlibby@microsoft.com>
2021-07-16 17:58:40 +0000

    Skip protobuf-based values for ETW serialization
    There is no serialization of protobuf values, and trying to serialize
    them DCHECKs so skip them.
    
    R=brucedawson@chromium.org, skyostil@chromium.org
    
    Fixed: 1229719
    Change-Id: I42627aee882936068bc2c670a192367d3b945db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032622
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#902522}

--- base/trace_event/trace_event_etw_export_win.cc
+++ base/trace_event/trace_event_etw_export_win.cc
@@ -361,13 +361,18 @@ void TraceEventETWExport::AddEvent(char phase,
   std::string arg_values_string[3];
   size_t num_args = args ? args->size() : 0;
   for (size_t i = 0; i < num_args; i++) {
-    if (args->types()[i] == TRACE_VALUE_TYPE_CONVERTABLE) {
-      // Temporarily do nothing here. This function consumes 1/3 to 1/2 of
-      // *total* process CPU time when ETW tracing, and many of the strings
-      // created exceed WPA's 4094 byte limit and are shown as:
-      // "Unable to parse data". See crbug.com/488257
+    const auto type = args->types()[i];
+    if (type == TRACE_VALUE_TYPE_CONVERTABLE ||
+        type == TRACE_VALUE_TYPE_PROTO) {
+      // For convertable types, temporarily do nothing here. This function
+      // consumes 1/3 to 1/2 of *total* process CPU time when ETW tracing, and
+      // many of the strings created exceed WPA's 4094 byte limit and are shown
+      // as "Unable to parse data". See crbug.com/488257.
+      //
+      // For protobuf-based values, there is no string serialization so skip
+      // those as well.
     } else {
-      args->values()[i].AppendAsString(args->types()[i], arg_values_string + i);
+      args->values()[i].AppendAsString(type, arg_values_string + i);
     }
   }
 
