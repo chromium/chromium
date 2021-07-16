6b588fb030eaf682833c0fea763d97326f7a7804
SebSG <sebsg@chromium.org>
2021-07-16 20:43:01 +0000

    [Publish] Set the correct scope for auth to access the server.
    Fixed: 1221522
    Change-Id: I71e0b0b8f172220ab9830a194f94d8d5ae079134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036101
    Auto-Submit: sebsg <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#902647}

--- components/content_creation/notes/core/server/notes_server_base.cc
+++ components/content_creation/notes/core/server/notes_server_base.cc
@@ -23,7 +23,7 @@ NotesServerBase::NotesServerBase(
 NotesServerBase::~NotesServerBase() {}
 
 signin::ScopeSet NotesServerBase::GetAuthScopes() {
-  return {GaiaConstants::kOAuth1LoginScope};
+  return {"https://www.googleapis.com/auth/googlenow"};
 }
 
 GURL NotesServerBase::GetNotesServerURL() {
