5ef5d87cd2c4cefa6391daa4be364104f34f4742
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-07-19 16:18:46 +0000

    Import wpt@d8899c277366e6960b0c564f76815f14dfd92cab
    Using wpt-import in Chromium 5d2e78c70d63ea003edd0dff3a17804f3f887d9f.
    With Chromium commits locally applied on WPT:
    ba1d78b8e7 "WindowPlacement: remove deprecated screen_enumeration APIs"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ia832e469af385a2e1a03013da96acd00786ea584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3038245
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#903026}

---
+++ third_party/blink/web_tests/external/wpt/html/canvas/offscreen/the-offscreen-canvas/2d-getcontext-options.any.js
@@ -0,0 +1,40 @@
+test(() => {
+  const expected = [
+    "alpha",
+    "colorSpace",
+    "colorSpace toString",
+    "desynchronized",
+    "willReadFrequently",
+  ];
+  const actual = [];
+  const options = {
+    get alpha() {
+      actual.push("alpha");
+      return true;
+    },
+    get willReadFrequently() {
+      actual.push("willReadFrequently");
+      return false;
+    },
+    get desynchronized() {
+      actual.push("desynchronized");
+      return false;
+    },
+    get colorSpace() {
+      actual.push("colorSpace");
+      return {
+        toString() {
+          actual.push("colorSpace toString");
+          return "srgb";
+        }
+      };
+    },
+  };
+
+  const canvas = new OffscreenCanvas(100, 50);
+  const context = canvas.getContext('2d', options);
+  assert_not_equals(context, null, "context");
+  assert_array_equals(actual, expected, "order of operations (creation)");
+  assert_equals(canvas.getContext('2d', options), context, "cached context");
+  assert_array_equals(actual, expected, "order of operations (caching)");
+});
