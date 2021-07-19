f5c6299e16cc704c02de989a6352e746086a35be
Greg Thompson <grt@chromium.org>
2021-07-19 16:03:15 +0000

    Run unit_tests in branded builds as-if in stable Chrome.
    Make chrome::GetChannel() and friends report stable channel by default
    in Google Chrome-branded unit_tests. Individual tests can use
    chrome::ScopedChannelOverride if they wish to test as-if they're running
    on a different channel.
    
    Bug: 1229586
    Change-Id: I92d260c689c3350c1a9e40256920df222aa54f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3030807
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903021}

--- chrome/test/base/run_all_unittests.cc
+++ chrome/test/base/run_all_unittests.cc
@@ -7,13 +7,19 @@
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_io_thread.h"
 #include "base/threading/platform_thread.h"
+#include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/test/base/chrome_unit_test_suite.h"
 #include "content/public/test/unittest_test_suite.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) &&           \
+    (defined(OS_WIN) || defined(OS_MAC) ||         \
+     (defined(OS_POSIX) && !defined(OS_ANDROID) && \
+      !BUILDFLAG(IS_CHROMEOS_ASH)))
+#include "chrome/test/base/scoped_channel_override.h"
+#elif defined(OS_WIN)
 #include "chrome/install_static/test/scoped_install_details.h"
 #endif
 
@@ -37,7 +43,17 @@ int main(int argc, char** argv) {
       test_io_thread.task_runner(),
       mojo::core::ScopedIPCSupport::ShutdownPolicy::FAST);
 
-#if defined(OS_WIN)
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) &&           \
+    (defined(OS_WIN) || defined(OS_MAC) ||         \
+     (defined(OS_POSIX) && !defined(OS_ANDROID) && \
+      !BUILDFLAG(IS_CHROMEOS_ASH)))
+  // Tests running in Google Chrome builds on Win/Mac/Lin/Lacros should present
+  // as stable channel by default.
+  chrome::ScopedChannelOverride scoped_channel_override(
+      chrome::ScopedChannelOverride::Channel::kStable);
+#elif defined(OS_WIN)
+  // Tests running in Chromium builds on Windows need basic InstallDetails even
+  // though there are no channels.
   install_static::ScopedInstallDetails scoped_install_details;
 #endif
 
