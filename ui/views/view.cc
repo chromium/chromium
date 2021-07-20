f900a6addd85b687b230b65200f7fdd9902ac015
Fergus Dall <sidereal@google.com>
2021-07-20 03:04:26 +0000

    Revert "[views] Add CHECK to prevent fallthrough to global NativeTheme"
    This reverts commit 93850eca7ee7369c0600f9558649d521b8f6d2eb.
    
    Reason for revert: Breaks linux-chromeos-chrome, see
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/15966/overview
    
    Original change's description:
    > [views] Add CHECK to prevent fallthrough to global NativeTheme
    >
    > This CL adds a CHECK to View::GetNativeTheme() to catch any
    > remaining fallthrough to the global NativeTheme instance.
    >
    > Bug: 1056756
    > Change-Id: I690ab0cfddc99495e9635f411feb17208f9e34d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3029188
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#903079}
    
    Bug: 1056756
    Change-Id: I72ec19311b189d9ed015acf8ab795bcf0db07d5f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040582
    Auto-Submit: Fergus Dall <sidereal@google.com>
    Owners-Override: Fergus Dall <sidereal@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#903319}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -1234,10 +1234,6 @@ const ui::NativeTheme* View::GetNativeTheme() const {
   if (widget)
     return widget->GetNativeTheme();
 
-  // CHECK here to ensure we catch fallthrough to the global NativeTheme
-  // instance on all Chromium builds (crbug.com/1056756).
-  CHECK(false);
-
   return ui::NativeTheme::GetInstanceForNativeUi();
 }
 
