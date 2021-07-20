d63429e4a4942e6c0bb42bfb5fbee24361e6ae9d
Benoit Lize <lizeb@chromium.org>
2021-07-20 08:38:16 +0000

    [PartitionAlloc] Enable the pre/post-fork() hooks on Chrome OS.
    (cherry picked from commit ac2ab77c21ca8b01d80f22ea425b644c78effa72)
    
    Bug: b/191316572, 1230401
    Change-Id: Icfbcd3bd37cac74e99806bff6b23c41df55de57b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034743
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#902485}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034886
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4577@{#27}
    Cr-Branched-From: 761ddde228655e313424edec06497d0c56b0f3c4-refs/heads/master@{#902210}

--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -27,7 +27,7 @@
 #include "wow64apiset.h"
 #endif
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
 #include <pthread.h>
 #endif
 
@@ -41,7 +41,7 @@ namespace base {
 
 namespace {
 
-#if defined(OS_APPLE) || defined(OS_LINUX)
+#if defined(OS_APPLE) || defined(OS_LINUX) || defined(OS_CHROMEOS)
 
 // NO_THREAD_SAFETY_ANALYSIS: acquires the lock and doesn't release it, by
 // design.
@@ -94,7 +94,7 @@ void AfterForkInChild() {
   internal::ThreadCacheRegistry::Instance()
       .ForcePurgeAllThreadAfterForkUnsafe();
 }
-#endif  // defined(OS_APPLE) || defined(OS_LINUX)
+#endif  // defined(OS_APPLE) || defined(OS_LINUX) || defined(OS_CHROMEOS)
 
 std::atomic<bool> g_global_init_called;
 void PartitionAllocMallocInitOnce() {
@@ -104,7 +104,7 @@ void PartitionAllocMallocInitOnce() {
   if (!g_global_init_called.compare_exchange_strong(expected, true))
     return;
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // When fork() is called, only the current thread continues to execute in the
   // child process. If the lock is held, but *not* by this thread when fork() is
   // called, we have a deadlock.
@@ -129,7 +129,7 @@ void PartitionAllocMallocInitOnce() {
   int err =
       pthread_atfork(BeforeForkInParent, AfterForkInParent, AfterForkInChild);
   PA_CHECK(err == 0);
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 }
 
 }  // namespace
