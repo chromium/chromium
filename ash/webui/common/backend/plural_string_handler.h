40da2d60748b18395bb0eab1e83b180146e14bd0
Michael Checo <michaelcheco@google.com>
2021-07-21 22:10:12 +0000

    Add plural_string_handler
    Test: ./out/Default/ash_webui_unittests --gtest_filter=*PluralStringHandlerTest*
    Change-Id: Iae7fcfd64b0565be506fc0edd7b80cd6e6df27dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039278
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904078}

---
+++ ash/webui/common/backend/plural_string_handler.h
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WEBUI_COMMON_BACKEND_PLURAL_STRING_HANDLER_H_
+#define ASH_WEBUI_COMMON_BACKEND_PLURAL_STRING_HANDLER_H_
+
+#include <map>
+#include <string>
+
+#include "content/public/browser/web_ui_message_handler.h"
+
+namespace base {
+class ListValue;
+}  // namespace base
+
+namespace ash {
+
+class PluralStringHandler : public content::WebUIMessageHandler {
+ public:
+  PluralStringHandler();
+
+  PluralStringHandler(const PluralStringHandler&) = delete;
+  PluralStringHandler& operator=(const PluralStringHandler&) = delete;
+
+  ~PluralStringHandler() override;
+
+  // WebUIMessageHandler:
+  void RegisterMessages() override;
+
+  // Adds to map of string IDs for pluralization.
+  void AddStringToPluralMap(const std::string& name, int id);
+
+  void SetWebUIForTest(content::WebUI* web_ui);
+
+ private:
+  // Returns a localized, pluralized string.
+  void HandleGetPluralString(const base::ListValue* args);
+
+  std::map<std::string, int> string_id_map_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_WEBUI_COMMON_BACKEND_PLURAL_STRING_HANDLER_H_
