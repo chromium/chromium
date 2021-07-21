5216c3c5282d927511e2b8810856d1fcf992a89a
Brian Ho <hob@chromium.org>
2021-07-21 22:52:29 +0000

    gpu: Don't require Vulkan GrContextType on CrOS for WebGPU
    Dawn uses Vulkan internally to fulfill WebGPU API calls on CrOS, but
    there is no requirement that Vulkan is enabled more generally [1] on
    Chrome (e.g. Vulkan for compositing or raster). CrOS has no plans to
    use Vulkan outside of WebGPU in the near future, so this CL removes
    the explicit check for Vulkan when requesting the WebGPU adapter.
    Unlike Linux which uses ExternalVkImage SharedImages, CrOS uses
    Ozone-backed SharedImages which do not use Vulkan at all.
    SharedImageBackingOzone interops between Dawn and the compositor via
    sync obj and dma-buf FDs and thus does not need a Vulkan
    GrContextType.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/config/gpu_finch_features.cc;l=246;drc=fbf6fd8bec28440a5cadc7d044ff06744daaa809
    
    BUG=b:172208313
    
    Change-Id: Iff0a88dca96ea0e51c012005958e4f60b8f7755e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043203
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Brian Ho <hob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904094}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -936,12 +936,12 @@ error::Error WebGPUDecoderImpl::HandleRequestAdapter(
       static_cast<DawnRequestAdapterSerial>(c.request_adapter_serial);
 
   if (gr_context_type_ != GrContextType::kVulkan) {
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
     SendAdapterProperties(request_adapter_serial, -1, nullptr,
                           "WebGPU on Linux requires command-line flag "
                           "--enable-features=Vulkan,UseSkiaRenderer");
     return error::kNoError;
-#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
+#endif  // defined(OS_LINUX)
   }
 
   int32_t requested_adapter_index = GetPreferredAdapterIndex(power_preference);
