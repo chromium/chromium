3f7cbc1f052e20756096fdd930df12033aceecd6
Corentin Wallez <cwallez@chromium.org>
2021-07-21 08:42:08 +0000

    libfuzzer/gen_fuzzer_owners: find closest OWNERS file for third_party
    Previously only OWNERS file in the shape of third_party/<libname>/OWNERS
    would be found. For recursive dependencies this would find only the
    OWNERS of the parent dependency and not the child one, leading to
    incorrect folks being CCed on fuzzer issues.
    
    Change-Id: Ibe76cd4110e4273a1b7a266a5d3dd29228486441
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039004
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903821}

--- testing/libfuzzer/gen_fuzzer_owners.py
+++ testing/libfuzzer/gen_fuzzer_owners.py
@@ -19,7 +19,8 @@ AUTHOR_REGEX = re.compile('author-mail <(.+)>')
 CHROMIUM_SRC_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 OWNERS_FILENAME = 'OWNERS'
-THIRD_PARTY_SEARCH_STRING = 'third_party' + os.sep
+THIRD_PARTY = 'third_party'
+THIRD_PARTY_SEARCH_STRING = THIRD_PARTY + os.path.sep
 
 
 def GetAuthorFromGitBlame(blame_output):
@@ -40,25 +41,28 @@ def GetGitCommand():
 
 
 def GetOwnersIfThirdParty(source):
-  """Return owners using OWNERS file if in third_party."""
+  """Return owners using the closest OWNERS file if in third_party."""
   match_index = source.find(THIRD_PARTY_SEARCH_STRING)
   if match_index == -1:
     # Not in third_party, skip.
     return None
 
-  match_index_with_library = source.find(
-      os.sep, match_index + len(THIRD_PARTY_SEARCH_STRING))
-  if match_index_with_library == -1:
-    # Unable to determine library name, skip.
-    return None
+  path_prefix = source[:match_index + len(THIRD_PARTY_SEARCH_STRING)]
+  path_after_third_party = source[len(path_prefix):].split(os.path.sep)
 
-  owners_file_path = os.path.join(source[:match_index_with_library],
-                                  OWNERS_FILENAME)
-  if not os.path.exists(owners_file_path):
-    return None
+  # Test all the paths after third_party/<libname>, making sure that we don't
+  # test third_party/OWNERS itself, otherwise we'd default to CCing them for
+  # all fuzzer issues without OWNERS, which wouldn't be nice.
+  while path_after_third_party:
+    owners_file_path = path_prefix + \
+        os.path.join(*(path_after_third_party + [OWNERS_FILENAME]))
 
-  return open(owners_file_path).read()
+    if os.path.exists(owners_file_path):
+      return open(owners_file_path).read()
 
+    path_after_third_party.pop()
+
+  return None
 
 def GetOwnersForFuzzer(sources):
   """Return owners given a list of sources as input."""
