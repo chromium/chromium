de22758c80c0265a0899c000694a63c764e7751b
dpapad <dpapad@chromium.org>
2021-07-21 10:06:27 +0000

    Profile Picker: Migrate to TypeScript compiler, still using JS.
    Bug: 1189595
    Change-Id: Ifb256b0b64e129cc2a4aa3bbaadf37eb5a040ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978233
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903843}

--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/local_profile_customization.js
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/local_profile_customization.js
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
-import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
-import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
+import 'chrome://resources/cr_elements/cr_profile_avatar_selector/cr_profile_avatar_selector.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 import 'chrome://resources/cr_components/customize_themes/customize_themes.js';
