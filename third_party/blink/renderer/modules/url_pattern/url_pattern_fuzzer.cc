1c3f10ff5f62660fa126c15cbd1498853a1b76ce
Ben Kelly <wanderview@chromium.org>
2021-07-22 19:26:37 +0000

    URLPattern: Force GC at end of the fuzzer.
    The fuzzer has been reporting oilpan objects as leaks.  Attempt to fix
    this by forcing GC at the end of the fuzzing routine.
    
    Bug: 1232096
    Change-Id: I52e08d304bcf94837354fa4b2fd83c887dcc0bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046087
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904437}

--- third_party/blink/renderer/modules/url_pattern/url_pattern_fuzzer.cc
+++ third_party/blink/renderer/modules/url_pattern/url_pattern_fuzzer.cc
@@ -5,6 +5,7 @@
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_urlpatterninit_usvstring.h"
 #include "third_party/blink/renderer/modules/url_pattern/url_pattern.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -16,6 +17,8 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   auto* input = MakeGarbageCollected<V8URLPatternInput>(
       String::FromUTF8(reinterpret_cast<const char*>(data), size));
   URLPattern::Create(input, exception_state);
+  V8PerIsolateData::MainThreadIsolate()->RequestGarbageCollectionForTesting(
+      v8::Isolate::kFullGarbageCollection);
   return 0;
 }
 
