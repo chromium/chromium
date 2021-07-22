615b2ca6e9e79c2ecbeb2a3fc6214d24b9f4d30c
Adam Langley <agl@chromium.org>
2021-07-22 22:38:57 +0000

    Temporarily enable CECPQ2 for everything.
    The linked bug notes a speed regression in mobile loading from expanding
    the CECPQ2 prefix letters in r903248. If the benchmark is loading 100%
    of its test pages from a single domain that we happened to cover with
    that change, then maybe that's ok. However, if we're going to make it
    nearly 10x worse by the time we've enabled everything then that might be
    a concern.
    
    Therefore flip on CECQP2 for everything so that the impact on that
    benchmark can be observed. If HRSS-SXY keygen is causing concerns on
    mobile then that would be valuable to learn.
    
    BUG=1231025,930812
    
    Change-Id: Ib526babb47e1c86e8110892f007ba278bf146068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046564
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904520}

--- net/base/features.cc
+++ net/base/features.cc
@@ -140,7 +140,7 @@ const base::Feature kTLS13KeyUpdate{"TLS13KeyUpdate",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kPostQuantumCECPQ2{"PostQuantumCECPQ2",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kPostQuantumCECPQ2SomeDomains{
     "PostQuantumCECPQ2SomeDomains", base::FEATURE_ENABLED_BY_DEFAULT};
 const base::FeatureParam<std::string> kPostQuantumCECPQ2InitialLetters(
