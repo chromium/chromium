fd8e60d8545747d2426a2ee9ec6aa2350af14d3a
Tommy Nyquist <nyquist@chromium.org>
2021-07-23 23:10:18 +0000

    [segmentation_platform] Add tracing for processor intensive tasks
    This CL adds initial tracing events for the parts of the platform that
    is expected to be most CPU intensive, such as model execution and
    sample lookup and parsing.
    
    To be able to follow along each execution of the model, we create
    a unique perfetto track per model execution. Initially, this is
    expected to be up to at most 3 unique models, but this could change
    over time. The track enables asynchrounously following along all parts
    of the execution, across DB invocations.
    
    To simplify looking at a trace event, some callback invocations are
    now posted instead of directly executed.
    
    Bug: 1212943
    Change-Id: Ideadf563b40e74a0fcba785b45f68d30369e38a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032919
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904935}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -138,6 +138,7 @@
   X("RLZ")                                                               \
   X("safe_browsing")                                                     \
   X("screenlock_monitor")                                                \
+  X("segmentation_platform")                                             \
   X("sequence_manager")                                                  \
   X("service_manager")                                                   \
   X("ServiceWorker")                                                     \
