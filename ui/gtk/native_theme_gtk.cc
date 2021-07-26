43e779809aca26130649a467c4f852a377c199fc
Alex Ilin <alexilin@chromium.org>
2021-07-26 20:50:44 +0000

    Respect --force-dark-mode in NativeThemeGtk
    NativeThemeGtk should always use the dark mode when --force-dark-mode
    is set.
    
    Bug: 1231283
    Change-Id: I97cd78d743abef43314ea7ff21d3e196efafe070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041584
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905407}

--- ui/gtk/native_theme_gtk.cc
+++ ui/gtk/native_theme_gtk.cc
@@ -187,8 +187,8 @@ void NativeThemeGtk::OnThemeChanged(GtkSettings* settings,
   // have a light variant and aren't affected by the setting.  Because of this,
   // experimentally check if the theme is dark by checking if the window
   // background color is dark.
-  set_use_dark_colors(
-      color_utils::IsDark(GetSystemColor(kColorId_WindowBackground)));
+  set_use_dark_colors(IsForcedDarkMode() || color_utils::IsDark(GetSystemColor(
+                                                kColorId_WindowBackground)));
   set_preferred_color_scheme(CalculatePreferredColorScheme());
 
   // GTK doesn't have a native high contrast setting.  Rather, it's implied by
