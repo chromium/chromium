fc1469c6b11e2860bad1fa9740193ad4df813bfb
Avi Drissman <avi@chromium.org>
2021-07-26 22:56:09 +0000

    Manually clamp on suspected hot paths
    https://crrev.com/c/3045786 seems to have caused a layout speed
    regression; manually clamp in two suspected hot paths.
    
    Bug: 1232666
    Change-Id: I08b0b24618f2a2c2e9b5b9f27b81b23ef5f7042b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054713
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905468}

--- third_party/blink/renderer/platform/fonts/font_selection_types.h
+++ third_party/blink/renderer/platform/fonts/font_selection_types.h
@@ -26,7 +26,8 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_FONT_SELECTION_TYPES_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_FONT_SELECTION_TYPES_H_
 
-#include "base/cxx17_backports.h"
+#include <algorithm>
+
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_table_deleted_value_type.h"
@@ -345,7 +346,9 @@ struct FontSelectionRange {
   }
 
   FontSelectionValue clampToRange(FontSelectionValue selection_value) const {
-    return base::clamp(selection_value, minimum, maximum);
+    // Use a local clamp for speed, as base::clamp has a CHECK and this is a hot
+    // path.
+    return std::min(std::max(selection_value, minimum), maximum);
   }
 
   FontSelectionValue minimum{FontSelectionValue(1)};
