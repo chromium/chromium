4aeda2cb7796229d85fd52d9bc800567660b5a06
Weizhong Xia <weizhong@google.com>
2021-07-26 20:29:28 +0000

    Run blink merge_results.py under py3
    By running merge_result.py under py3, we saved 44 seconds for Windows,
    12 seconds for Linux, 8 seconds for Mac.
    
    Will revert this change once all merge script are running under py3.
    
    Bug: 1056628
    Change-Id: I1b5c1f1d0524c46b14911636e04857dca53facf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3050540
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/master@{#905395}

--- third_party/blink/tools/merge_web_test_results.py
+++ third_party/blink/tools/merge_web_test_results.py
@@ -4,8 +4,22 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import sys
 
-from blinkpy.web_tests.merge_results import main
+from blinkpy.common.host import Host
 
-main(sys.argv[1:])
+BLINK_TOOLS_PATH = os.path.abspath(os.path.dirname(__file__))
+
+def main():
+    path_to_merge_script = os.path.join(BLINK_TOOLS_PATH,
+                                        'blinkpy',
+                                        'web_tests',
+                                        'merge_results.py')
+    command = ['python3', path_to_merge_script] + sys.argv[1:]
+    host = Host()
+    output = host.executive.run_command(command)
+    print("Output of merge_results.py:\n%s\n" % output)
+
+if __name__ == '__main__':
+    main()
