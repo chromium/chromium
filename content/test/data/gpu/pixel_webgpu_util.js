0597c5430e2952188791205e0ab0e2379a72c2fe
Corentin Wallez <cwallez@chromium.org>
2021-07-26 12:38:12 +0000

    Fix WebGPU pixel tests for interstage variable matching
    Bug: dawn:802
    Change-Id: Iae55473df19412500ed26bbf14584d8035cf75cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3052509
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Reviewed-by: Jiawei Shao <jiawei.shao@intel.com>
    Cr-Commit-Position: refs/heads/master@{#905216}

--- content/test/data/gpu/pixel_webgpu_util.js
+++ content/test/data/gpu/pixel_webgpu_util.js
@@ -45,7 +45,7 @@ fn main([[location(0)]] fragUV : vec2<f32>) -> [[location(0)]] vec4<f32> {
 
     fragmentClear: `
 [[stage(fragment)]]
-fn main() -> [[location(0)]] vec4<f32> {
+fn main([[location(0)]] fragUV : vec2<f32>) -> [[location(0)]] vec4<f32> {
   return vec4<f32>(1.0, 1.0, 1.0, 1.0);
 }
 `,
