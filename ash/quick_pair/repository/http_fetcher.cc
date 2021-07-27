cee2a93a78254412670277df5b373e2e6cebca42
Jon Mann <jonmann@chromium.org>
2021-07-27 16:31:33 +0000

    FastPair: Implement HttpFetcher.
    The HttpFetcher class is responsible for fetching
    data from a URL and returning the data to the caller.
    Unit testing will be possible once the delegate to
    retrieve a SharedURLLoaderFactory is available.
    
    Change-Id: I93fa430e8ce03bbc84c1bb094c29f2cef4cc0e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054861
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#905769}

---
+++ ash/quick_pair/repository/http_fetcher.cc
@@ -0,0 +1,79 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/quick_pair/repository/http_fetcher.h"
+
+#include "ash/quick_pair/common/logging.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
+
+namespace {
+
+// Max size set to 2MB.  This is well over the expected maximum for our
+// expected responses, however it can be increased if needed in the future.
+constexpr int kMaxDownloadBytes = 2 * 1024 * 1024;
+
+// Helper function to extract response code from |SimpleURLLoader|.
+int GetResponseCode(network::SimpleURLLoader* simple_loader) {
+  if (simple_loader->ResponseInfo() && simple_loader->ResponseInfo()->headers) {
+    return simple_loader->ResponseInfo()->headers->response_code();
+  }
+
+  return -1;
+}
+
+}  // namespace
+
+namespace ash {
+namespace quick_pair {
+
+HttpFetcher::HttpFetcher(
+    const net::NetworkTrafficAnnotationTag& traffic_annotation)
+    : traffic_annotation_(traffic_annotation) {}
+
+HttpFetcher::~HttpFetcher() = default;
+
+void HttpFetcher::ExecuteGetRequest(const GURL& url,
+                                    FetchCompleteCallback callback) {
+  QP_LOG(VERBOSE) << __func__ << " executing request to: " << url;
+
+  auto resource_request = std::make_unique<network::ResourceRequest>();
+  resource_request->url = url;
+  resource_request->method = "GET";
+  resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+
+  auto loader = network::SimpleURLLoader::Create(std::move(resource_request),
+                                                 traffic_annotation_);
+  loader->DownloadToString(
+      /*url_loader_factory=*/nullptr,
+      base::BindOnce(&HttpFetcher::OnComplete, weak_ptr_factory_.GetWeakPtr(),
+                     std::move(loader), std::move(callback)),
+      kMaxDownloadBytes);
+}
+
+void HttpFetcher::OnComplete(
+    std::unique_ptr<network::SimpleURLLoader> simple_loader,
+    FetchCompleteCallback callback,
+    std::unique_ptr<std::string> response_body) {
+  if (simple_loader->NetError() == net::OK && response_body) {
+    QP_LOG(VERBOSE) << "Successfully fetched "
+                    << simple_loader->GetContentSize() << " bytes from "
+                    << simple_loader->GetFinalURL();
+    std::move(callback).Run(std::move(response_body));
+    return;
+  }
+
+  QP_LOG(WARNING) << "Downloading to string from "
+                  << simple_loader->GetFinalURL() << " failed with error code: "
+                  << GetResponseCode(simple_loader.get())
+                  << " with network error: " << simple_loader->NetError();
+
+  // TODO(jonmann): Implement retries with back-off.
+  std::move(callback).Run(nullptr);
+}
+
+}  // namespace quick_pair
+}  // namespace ash
---
+++ ash/quick_pair/repository/http_fetcher.h
@@ -0,0 +1,51 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_QUICK_PAIR_REPOSITORY_HTTP_FETCHER_H_
+#define ASH_QUICK_PAIR_REPOSITORY_HTTP_FETCHER_H_
+
+#include "base/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
+#include "url/gurl.h"
+
+namespace network {
+class SimpleURLLoader;
+class SharedURLLoaderFactory;
+}  // namespace network
+
+namespace ash {
+namespace quick_pair {
+
+using FetchCompleteCallback =
+    base::OnceCallback<void(std::unique_ptr<std::string>)>;
+
+// Makes HTTP GET requests and returns the response.
+class HttpFetcher {
+ public:
+  explicit HttpFetcher(
+      const net::NetworkTrafficAnnotationTag& traffic_annotation);
+  HttpFetcher(const HttpFetcher&) = delete;
+  HttpFetcher& operator=(const HttpFetcher&) = delete;
+  virtual ~HttpFetcher();
+
+  // Performs a GET request to the desired URL and returns the response, if
+  // available, as a string to the provided |callback|.
+  void ExecuteGetRequest(const GURL& url, FetchCompleteCallback callback);
+
+ private:
+  void OnComplete(std::unique_ptr<network::SimpleURLLoader> simple_loader,
+                  FetchCompleteCallback success_callback,
+                  std::unique_ptr<std::string> response_body);
+  scoped_refptr<network::SharedURLLoaderFactory> GetURLLoaderFactory();
+
+  net::NetworkTrafficAnnotationTag traffic_annotation_;
+
+  base::WeakPtrFactory<HttpFetcher> weak_ptr_factory_{this};
+};
+
+}  // namespace quick_pair
+}  // namespace ash
+
+#endif  // ASH_QUICK_PAIR_REPOSITORY_HTTP_FETCHER_H_
