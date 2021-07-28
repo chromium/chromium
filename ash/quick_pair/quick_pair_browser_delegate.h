43b94f7b6fc24bcbddd466d09dcb539831f8df74
Tal Pressman <talp@google.com>
2021-07-28 02:15:09 +0000

    Reland "[FastPair] Browser delegate to access chrome/browser dependencies"
    This reverts commit e56c9f8781ef3a97df2774aa85564c745777622a.
    
    Reason for revert:
    Wrong culprit, should have reverted crrev.com/c/3048642.
    
    Original change's description:
    > Revert "[FastPair] Browser delegate to access chrome/browser dependencies"
    >
    > This reverts commit 3ce9e6c64a64dee61c32504015037deda6348257.
    >
    > Reason for revert:
    > Suspected to cause FastPairGattServiceClientTest.IgnoreNonFastPairServices to fail on "Linux ChromiumOS MSan Tests" bot.
    >
    > First failure:
    > https://crgo.dev/ci/Linux%20ChromiumOS%20MSan%20Tests/24392/test-results
    >
    > Original change's description:
    > > [FastPair] Browser delegate to access chrome/browser dependencies
    > >
    > > This change introduces QuickPairBrowserDelegate, a delegate
    > > implementation to enable the //ash/quick_pair code to access
    > > dependencies from chrome/browser.
    > >
    > > Initial implementation has an accessor for the active profile's
    > > SharedURLLoaderFactory.
    > >
    > > Change-Id: I3efa783cb6df996c2cf0887d8e362de30e5f7469
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055015
    > > Reviewed-by: James Cook <jamescook@chromium.org>
    > > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > > Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#905787}
    >
    > Change-Id: Ib5e7e8f78cc2fe5d0c9160db80854fddddc5ef53
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056114
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Tal Pressman <talp@google.com>
    > Owners-Override: Tal Pressman <talp@google.com>
    > Cr-Commit-Position: refs/heads/master@{#905980}
    
    Change-Id: I62a3ad63df15c729b2c7d376a5d81ee9f06f27a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056115
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tal Pressman <talp@google.com>
    Owners-Override: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#906014}

---
+++ ash/quick_pair/quick_pair_browser_delegate.h
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_QUICK_PAIR_QUICK_PAIR_BROWSER_DELEGATE_H_
+#define ASH_QUICK_PAIR_QUICK_PAIR_BROWSER_DELEGATE_H_
+
+#include "base/component_export.h"
+#include "base/memory/scoped_refptr.h"
+
+namespace network {
+class SharedURLLoaderFactory;
+}  // namespace network
+
+namespace ash {
+namespace quick_pair {
+
+// Interface for a class which provides browser dependences to classes within
+// ash::quick_pair. This allows us to retrieve dependencies (such as the active
+// user profile) which cannot directly be retrieved in ash.
+class COMPONENT_EXPORT(QUICK_PAIR) QuickPairBrowserDelegate {
+ public:
+  QuickPairBrowserDelegate();
+  QuickPairBrowserDelegate(const QuickPairBrowserDelegate&) = delete;
+  QuickPairBrowserDelegate& operator=(const QuickPairBrowserDelegate&) = delete;
+  virtual ~QuickPairBrowserDelegate();
+
+  static QuickPairBrowserDelegate* Get();
+
+  // Returns the URL loader factory associated with the active user's profile.
+  virtual scoped_refptr<network::SharedURLLoaderFactory>
+  GetURLLoaderFactory() = 0;
+};
+
+}  // namespace quick_pair
+}  // namespace ash
+
+#endif  // ASH_QUICK_PAIR_QUICK_PAIR_BROWSER_DELEGATE_H_
