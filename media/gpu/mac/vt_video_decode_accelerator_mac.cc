f29c41b8cb17ae522d42079a24bf831b77836a80
Christopher Cameron <ccameron@chromium.org>
2021-07-28 18:19:33 +0000

    VTVideoDecodeAccelerator: Remove legacy mailboxes for shared images
    When using shared images, there is no longer a need for creating a
    legacy mailbox.
    
    Bug: 1210994
    Change-Id: Ie55440d205e8704098092325d3ed00dd84cb733c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056952
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906278}

--- media/gpu/mac/vt_video_decode_accelerator_mac.cc
+++ media/gpu/mac/vt_video_decode_accelerator_mac.cc
@@ -1586,22 +1586,13 @@ bool VTVideoDecodeAccelerator::SendFrame(const Frame& frame) {
       gl_params.is_cleared = true;
       gpu::SharedImageBackingGLCommon::UnpackStateAttribs gl_attribs;
 
-      // A GL texture id is needed to create the legacy mailbox, which requires
-      // that the GL context be made current.
-      const bool kCreateLegacyMailbox = true;
-      if (!gl_client_.make_context_current.Run()) {
-        DLOG(ERROR) << "Failed to make context current";
-        NotifyError(PLATFORM_FAILURE, SFT_PLATFORM_ERROR);
-        return false;
-      }
-
       auto shared_image = std::make_unique<gpu::SharedImageBackingGLImage>(
           gl_image, mailbox, viz_resource_format, plane_size, color_space,
           kTopLeft_GrSurfaceOrigin, kOpaque_SkAlphaType, shared_image_usage,
           gl_params, gl_attribs, gl_client_.is_passthrough);
 
       const bool success = shared_image_stub->factory()->RegisterBacking(
-          std::move(shared_image), kCreateLegacyMailbox);
+          std::move(shared_image), /*allow_legacy_mailbox=*/false);
       if (!success) {
         DLOG(ERROR) << "Failed to register shared image";
         NotifyError(PLATFORM_FAILURE, SFT_PLATFORM_ERROR);
