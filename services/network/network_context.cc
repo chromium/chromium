26cd015dd2c6ee3642955aceea504ce78c99f864
Xiaohan Wang <xhwang@chromium.org>
2021-07-28 23:59:38 +0000

    net: Fix OS_LINUX check
    This CL fixes a typo. Instead of defined(IS_LINUX) it should be
    defined(OS_LINUX). This was introduced during the last round of OS
    check refactoring when we Replace defined(OS_CHROMEOS) with
    BUILDFLAG(IS_CHROMEOS_ASH):
    https://chromium-review.googlesource.com/c/chromium/src/+/2596898
    
    Bug: 1234043
    Change-Id: I930bc291e9cac1dec02fa1b8ebee2b9de2be69cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059552
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906461}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -2382,7 +2382,7 @@ NetworkContext::MakeSessionCleanupCookieStore() const {
 
   net::CookieCryptoDelegate* crypto_delegate = nullptr;
   if (params_->enable_encrypted_cookies) {
-#if (defined(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && \
+#if (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && \
     !BUILDFLAG(IS_CHROMECAST)
     DCHECK(network_service_->os_crypt_config_set())
         << "NetworkService::SetCryptConfig must be called before creating a "
