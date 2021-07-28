c3acf47060ca81ff4bbccedab2114f551d2b2e08
sandromaggi <sandromaggi@google.com>
2021-07-28 12:19:21 +0000

    [Autofill Assistant] Remove schema check from domain check
    This removes the schema check and puts it in its own helper.
    
    Bug: b/192998862
    Change-Id: I7bdce8551c73b5018c773a4512732f6d1f4d5450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055171
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#906154}

--- components/autofill_assistant/browser/url_utils.cc
+++ components/autofill_assistant/browser/url_utils.cc
@@ -55,12 +55,16 @@ bool IsSamePublicSuffixDomain(const GURL& url1, const GURL& url2) {
     return false;
   }
 
-  return url1.scheme() == url2.scheme() && domain1 == domain2;
+  return domain1 == domain2;
 }
 
 std::string GetOrganizationIdentifyingDomain(const GURL& url) {
   return net::registry_controlled_domains::GetDomainAndRegistry(
       url, net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
+}
+
+bool IsAllowedSchemaTransition(const GURL& from, const GURL& to) {
+  return from.scheme() == to.scheme() || to.scheme() == url::kHttpsScheme;
 }
 
 }  // namespace url_utils
