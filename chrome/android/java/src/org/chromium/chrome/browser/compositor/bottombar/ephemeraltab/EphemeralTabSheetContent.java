73f20c72be9f7b7f5798d63924708cb0450f94d0
Jinsuk Kim <jinsukkim@chromium.org>
2021-07-28 22:30:02 +0000

    PreviewTab: Disable Web Share feature and fix the crash
    ThinWebView backing PreviewTab manages its own WindowAndroid but
    its ShareDelegate/Supplier was not set. It caused the reported crash.
    This CL initializes the supplier but not set the Delegate object,
    in order to fix the crash but leave the feature disabled on Preview Tab.
    
    (cherry picked from commit f1f959054a553a535498c39d365b47e19afdf3eb)
    
    Bug: 1229516
    Change-Id: I355ae0e602988e8044bc6a7bf08ab51633345b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043069
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#904085}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056133
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#1889}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
@@ -19,7 +19,10 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.supplier.UnownedUserDataSupplier;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.share.ShareDelegate;
+import org.chromium.chrome.browser.share.ShareDelegateSupplier;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 import org.chromium.components.browser_ui.widget.FadingShadow;
 import org.chromium.components.browser_ui.widget.FadingShadowView;
@@ -32,6 +35,7 @@ import org.chromium.components.url_formatter.SchemeDisplay;
 import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.content_public.browser.RenderCoordinates;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.ui.base.WindowAndroid;
 import org.chromium.url.GURL;
 
 /**
@@ -54,6 +58,8 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
     private final Runnable mToolbarClickCallback;
     private final Runnable mCloseButtonCallback;
     private final int mToolbarHeightPx;
+    private final UnownedUserDataSupplier<ShareDelegate> mShareDelegateSupplier =
+            new ShareDelegateSupplier();
 
     private ViewGroup mToolbarView;
     private ViewGroup mSheetContentView;
@@ -100,6 +106,13 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
             ((ViewGroup) mWebContentView.getParent()).removeView(mWebContentView);
         }
         mThinWebView.attachWebContents(mWebContents, mWebContentView, delegate);
+
+        // Initialize the supplier of {@link ShareDelegate} for the WindowAndroid used by
+        // ThinWebView.  The {@link ShareDelegate} itself is not set by design in order to leave
+        // the share feature disabled on Preview Tab.
+        WindowAndroid window = mWebContents.getTopLevelNativeWindow();
+        assert window != null;
+        mShareDelegateSupplier.attach(window.getUnownedUserDataHost());
     }
 
     /**
@@ -241,6 +254,7 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
     @Override
     public void destroy() {
         mThinWebView.destroy();
+        mShareDelegateSupplier.destroy();
     }
 
     @Override
