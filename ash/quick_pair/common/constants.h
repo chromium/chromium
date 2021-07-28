e56c9f8781ef3a97df2774aa85564c745777622a
Tal Pressman <talp@google.com>
2021-07-28 00:48:45 +0000

    Revert "[FastPair] Browser delegate to access chrome/browser dependencies"
    This reverts commit 3ce9e6c64a64dee61c32504015037deda6348257.
    
    Reason for revert:
    Suspected to cause FastPairGattServiceClientTest.IgnoreNonFastPairServices to fail on "Linux ChromiumOS MSan Tests" bot.
    
    First failure:
    https://crgo.dev/ci/Linux%20ChromiumOS%20MSan%20Tests/24392/test-results
    
    Original change's description:
    > [FastPair] Browser delegate to access chrome/browser dependencies
    >
    > This change introduces QuickPairBrowserDelegate, a delegate
    > implementation to enable the //ash/quick_pair code to access
    > dependencies from chrome/browser.
    >
    > Initial implementation has an accessor for the active profile's
    > SharedURLLoaderFactory.
    >
    > Change-Id: I3efa783cb6df996c2cf0887d8e362de30e5f7469
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055015
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    > Cr-Commit-Position: refs/heads/master@{#905787}
    
    Change-Id: Ib5e7e8f78cc2fe5d0c9160db80854fddddc5ef53
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056114
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tal Pressman <talp@google.com>
    Owners-Override: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#905980}

--- ash/quick_pair/common/constants.h
+++ ash/quick_pair/common/constants.h
@@ -6,13 +6,12 @@
 #define ASH_QUICK_PAIR_COMMON_CONSTANTS_H_
 
 #include "base/component_export.h"
-#include "device/bluetooth/public/cpp/bluetooth_uuid.h"
 
 namespace ash {
 namespace quick_pair {
 
 COMPONENT_EXPORT(QUICK_PAIR_COMMON)
-extern const device::BluetoothUUID kFastPairBluetoothUuid;
+const device::BluetoothUUID kFastPairBluetoothUuid("0xFE2C");
 
 }  // namespace quick_pair
 }  // namespace ash
