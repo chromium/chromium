454eda340898f4eff566b25b99b27f97dc1d2d33
Hiroki Nakagawa <nhiroki@chromium.org>
2021-07-29 04:31:50 +0000

    Prerender: Add UseCounter for SpeculationRules-triggered prerender
    The counter will record a use of <script type="speculationrules"> with
    the "prerender" action regardless of its origin type (same-origin
    prerendering vs. cross-origin prerendering) and result (success vs.
    failure).
    
    Bug: 1227261
    Change-Id: I3f7e00163d9e189a41d27c3d8603def12c6b2cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3012436
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906567}

--- content/browser/speculation_rules/speculation_host_impl.cc
+++ content/browser/speculation_rules/speculation_host_impl.cc
@@ -12,6 +12,7 @@
 #include "content/public/common/content_client.h"
 #include "mojo/public/cpp/bindings/message.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "url/origin.h"
 
 namespace content {
@@ -120,13 +121,17 @@ void SpeculationHostImpl::ProcessCandidatesForPrerender(
 
   auto* rfhi = static_cast<RenderFrameHostImpl*>(render_frame_host());
   for (const auto& it : candidates) {
-    // TODO(crbug.com/1176054): Support cross-origin prerendering.
-    // TODO(crbug.com/1197133): Record the cancellation reason of no same-origin
-    // candidates via UMA.
-    if (it->action != blink::mojom::SpeculationAction::kPrerender ||
-        !origin().IsSameOriginWith(url::Origin::Create(it->url))) {
+    if (it->action != blink::mojom::SpeculationAction::kPrerender)
       continue;
-    }
+
+    GetContentClient()->browser()->LogWebFeatureForCurrentPage(
+        rfhi, blink::mojom::WebFeature::kSpeculationRulesPrerender);
+
+    // TODO(crbug.com/1176054): Support cross-origin prerendering.
+    // TODO(crbug.com/1197133): Record the cancellation reason of no
+    // same-origin candidates via UMA.
+    if (!origin().IsSameOriginWith(url::Origin::Create(it->url)))
+      continue;
 
     // TODO(https://crbug.com/1217903): Set up `attributes->size`.
     auto attributes = blink::mojom::PrerenderAttributes::New();
