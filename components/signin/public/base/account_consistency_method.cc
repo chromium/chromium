188c4d9da90e9a13d96b066736f300ac66f4c67e
Gauthier Ambard <gambard@chromium.org>
2021-07-30 12:44:12 +0000

    [iOS] Enable SimplifySignOutIOS and MobileIdentityConsistency
    This CL enables the two features by default. This is needed in order to
    launch the new FRE with built-in experimental groups.
    
    Bug: 1231018
    Change-Id: I8a425d9f486ff9362e7e46074a88a7a112f89ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054052
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907093}

--- components/signin/public/base/account_consistency_method.cc
+++ components/signin/public/base/account_consistency_method.cc
@@ -25,7 +25,7 @@ const base::Feature kMobileIdentityConsistencyPromos{
 
 #if defined(OS_IOS)
 const base::Feature kMobileIdentityConsistency{
-    "MobileIdentityConsistency", base::FEATURE_DISABLED_BY_DEFAULT};
+    "MobileIdentityConsistency", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMICEWebSignIn{"MICEWebSignIn",
                                    base::FEATURE_DISABLED_BY_DEFAULT};
