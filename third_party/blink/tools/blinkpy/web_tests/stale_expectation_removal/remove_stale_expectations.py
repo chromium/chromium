5fe53b8b85a36d0512c1361873ba08fd29163d69
Brian Sheedy <bsheedy@chromium.org>
2021-08-02 19:30:59 +0000

    Add web test stale expectation remover skeleton
    Adds some boilerplate code and OWNERS files for upcoming work on the
    web test unexpected pass finder/stale expectation remover. Additional
    code will be added in follow-up CLs to make the script actually
    functional.
    
    Bug: 1222827
    Change-Id: I7345d2ffc3eb19a4798d7c35ffaedcc6586b1e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3063987
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907659}

---
+++ third_party/blink/tools/blinkpy/web_tests/stale_expectation_removal/remove_stale_expectations.py
@@ -0,0 +1,85 @@
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import logging
+import sys
+
+assert sys.version_info[0] == 3
+
+
+def ParseArgs():
+    parser = argparse.ArgumentParser(description=(
+        'Script for finding cases of stale expectations that can be '
+        'removed/modified.'))
+    parser.add_argument('--large-query-mode',
+                        action='store_true',
+                        default=False,
+                        help=('Run the script in large query mode. This '
+                              'incurs a significant performance hit, but '
+                              'allows the use of larger sample sizes by '
+                              'working around a hard memory limit in '
+                              'BigQuery.'))
+    parser.add_argument('--project',
+                        default='chrome-unexpected-pass-data',
+                        help=('The billing project to use for BigQuery '
+                              'queries. Must have access to the ResultDB BQ '
+                              'tables, e.g. "chrome-luci-data.chromium.'
+                              'blink_web_testes_ci_results".'))
+    parser.add_argument('--num-samples',
+                        type=int,
+                        default=100,
+                        help='The number of recent builds to query.')
+    parser.add_argument('--output-format',
+                        choices=['html', 'print'],
+                        default='html',
+                        help='How to output script results.')
+    parser.add_argument('--remove-stale-expectations',
+                        action='store_true',
+                        default=False,
+                        help=('Automatically remove any expectations that are '
+                              'determined to be stale from the expectation '
+                              'files.'))
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='count',
+                        default=0,
+                        help=('Increase logging verbosity, can be passed '
+                              'multiple times.'))
+    parser.add_argument('-q',
+                        '--quiet',
+                        action='store_true',
+                        default=False,
+                        help='Disable logging for non-errors.')
+
+    args = parser.parse_args()
+    if args.quiet:
+        args.verbose = -1
+    SetLoggingVerbosity(args.verbose)
+
+    return args
+
+
+def SetLoggingVerbosity(verbosity_level):
+    if verbosity_level == -1:
+        level = logging.ERROR
+    elif verbosity_level == 0:
+        level = logging.WARNING
+    elif verbosity_level == 1:
+        level = logging.INFO
+    else:
+        level = logging.DEBUG
+    logging.getLogger().setLevel(level)
+
+
+def main():
+    args = ParseArgs()
+    raise RuntimeError(
+        'Script is still under active development and not currently functional'
+    )
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
