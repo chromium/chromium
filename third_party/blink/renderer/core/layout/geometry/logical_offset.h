bfa886405084fc5bdf88df18c6daa3e2b44dd173
Stephan Hartmann <stha09@googlemail.com>
2021-08-02 18:33:52 +0000

    IWYU: add tuple for std::tie in blink::LogicalOffset
    Bug: None
    Change-Id: Ia1832c61a00357b2e2e809d8b699cdff877b2b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064601
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#907630}

--- third_party/blink/renderer/core/layout/geometry/logical_offset.h
+++ third_party/blink/renderer/core/layout/geometry/logical_offset.h
@@ -5,6 +5,8 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_GEOMETRY_LOGICAL_OFFSET_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_GEOMETRY_LOGICAL_OFFSET_H_
 
+#include <tuple>
+
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
 #include "third_party/blink/renderer/platform/text/writing_direction_mode.h"
