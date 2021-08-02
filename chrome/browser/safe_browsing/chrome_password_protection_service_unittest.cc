84a0886f4cf06d25be680bb488db15f23ba3fd3a
Mohamed Amir Yosef <mamir@chromium.org>
2021-08-02 15:28:02 +0000

    Reland "[Passwords] Migrate ChromePasswordProtectionService to the interface"
    This is a reland of a0146ee827e34adbd10bcc9a6a10f15847f1f4a5
    
    Original change's description:
    > [Passwords] Migrate ChromePasswordProtectionService to the interface
    >
    > This CL moves ChromePasswordProtectionService to PasswordStoreInterface
    > together with its unittest.
    >
    > Bug: 1218413
    > Change-Id: Ic13cff882ff17b818bded008ba55ff3143d0b77f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062942
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#907132}
    
    Bug: 1218413
    Change-Id: I38f0e16063ee9c28846a8dcbf9bf521ebbbd5622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067341
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907562}

--- chrome/browser/safe_browsing/chrome_password_protection_service_unittest.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service_unittest.cc
@@ -31,7 +31,7 @@
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/password_manager/core/browser/hash_password_manager.h"
 #include "components/password_manager/core/browser/insecure_credentials_table.h"
-#include "components/password_manager/core/browser/mock_password_store.h"
+#include "components/password_manager/core/browser/mock_password_store_interface.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
 #include "components/password_manager/core/browser/password_reuse_detector.h"
@@ -236,26 +236,28 @@ class ChromePasswordProtectionServiceTest
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
 
-    password_store_ =
-        base::WrapRefCounted(static_cast<password_manager::MockPasswordStore*>(
+    password_store_ = base::WrapRefCounted(
+        static_cast<password_manager::MockPasswordStoreInterface*>(
             PasswordStoreFactory::GetInstance()
                 ->SetTestingFactoryAndUse(
                     profile(),
-                    base::BindRepeating(&password_manager::BuildPasswordStore<
-                                        content::BrowserContext,
-                                        password_manager::MockPasswordStore>))
+                    base::BindRepeating(
+                        &password_manager::BuildPasswordStoreInterface<
+                            content::BrowserContext,
+                            password_manager::MockPasswordStoreInterface>))
                 .get()));
 
     if (base::FeatureList::IsEnabled(
             password_manager::features::kEnablePasswordsAccountStorage)) {
       account_password_store_ = base::WrapRefCounted(
-          static_cast<password_manager::MockPasswordStore*>(
+          static_cast<password_manager::MockPasswordStoreInterface*>(
               AccountPasswordStoreFactory::GetInstance()
                   ->SetTestingFactoryAndUse(
                       profile(),
-                      base::BindRepeating(&password_manager::BuildPasswordStore<
-                                          content::BrowserContext,
-                                          password_manager::MockPasswordStore>))
+                      base::BindRepeating(
+                          &password_manager::BuildPasswordStoreInterface<
+                              content::BrowserContext,
+                              password_manager::MockPasswordStoreInterface>))
                   .get()));
     }
 
@@ -416,8 +418,9 @@ class ChromePasswordProtectionServiceTest
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_profile_adaptor_;
   MockSecurityEventRecorder* security_event_recorder_;
-  scoped_refptr<password_manager::MockPasswordStore> password_store_;
-  scoped_refptr<password_manager::MockPasswordStore> account_password_store_;
+  scoped_refptr<password_manager::MockPasswordStoreInterface> password_store_;
+  scoped_refptr<password_manager::MockPasswordStoreInterface>
+      account_password_store_;
   // Owned by KeyedServiceFactory.
   syncer::FakeUserEventService* fake_user_event_service_;
 #if !defined(OS_ANDROID)
