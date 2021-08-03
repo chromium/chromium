963fa9354063df85ef2df255a174f00e3c0bd16b
Alexey Baskakov <loyso@chromium.org>
2021-08-03 08:47:36 +0000

    dpwa: Fix badge icons accent color on ChromeOS.
    Some background for previous CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2871410
    
    Following the previous change, that gave us the following order of precedence:
      1. Chrome logo (default).
      2. Web app icon, when available.
      3. Notification-specific icon, when available.
    
    Fix: We shouldn't change the pre-2871410 behavior:
    Notification-specific `badge` icon should stay in gray color
    on ChromeOS (effectively, that's alpha-channel only mask)
    and it must stay full-color on WML OSes.
    
    Reason/review/approval: The ChromeOS UI review decision.
    
    In next CL: Add a browser test case in
    chrome/browser/web_applications/web_app_notifications_interactive_uitest.cc
    
    Bug: 1190648
    Change-Id: Ibdc65c4e0f30e381a9b691fd64988484c2ea8488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066729
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907887}

--- chrome/browser/notifications/platform_notification_service_impl.cc
+++ chrome/browser/notifications/platform_notification_service_impl.cc
@@ -437,8 +437,11 @@ PlatformNotificationServiceImpl::CreateNotificationFromData(
   if (base::FeatureList::IsEnabled(
           features::kDesktopPWAsNotificationIconAndTitle)) {
     web_app_icon_and_title = FindWebAppIconAndTitle(web_app_hint_url);
-    if (web_app_icon_and_title) {
+    if (web_app_icon_and_title && notification_resources.badge.isNull()) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+      // ChromeOS: Enables web app theme color only if monochrome web app icon
+      // has been specified. `badge` Notifications API icons must be masked with
+      // the accent color.
       optional_fields.ignore_accent_color_for_small_image = true;
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
     }
