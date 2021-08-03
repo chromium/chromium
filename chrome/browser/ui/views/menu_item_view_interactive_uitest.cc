bc50f9864dc07b0dd12f7eab1cb8629acb1cf7e2
Maksim Moskvitin <mmoskvitin@google.com>
2021-08-03 14:31:05 +0000

    [Sheriff] Disable test failing on Linux Ozone Wayland builder
    MenuItemViewTestInsertWithSubmenu1.InsertItemWithSubmenu1.
    
    Bug: 1236048, 1163646
    Change-Id: I341c892b1fd9c09322c68640630508f5be3b6144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067993
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Owners-Override: Maksim Moskvitin <mmoskvitin@google.com>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907970}

--- chrome/browser/ui/views/menu_item_view_interactive_uitest.cc
+++ chrome/browser/ui/views/menu_item_view_interactive_uitest.cc
@@ -218,7 +218,13 @@ using MenuItemViewTestInsertWithSubmenu1 = MenuItemViewTestInsertWithSubmenu<1>;
 VIEW_TEST(MenuItemViewTestInsertWithSubmenu0, InsertItemWithSubmenu0)
 
 // If this flakes, disable and log details in http://crbug.com/523255.
-VIEW_TEST(MenuItemViewTestInsertWithSubmenu1, InsertItemWithSubmenu1)
+// Failing on Linux Ozone Tester (Wayland) (See crbug.com/1236048).
+#if defined(USE_OZONE)
+#define MAYBE_InsertItemWithSubmenu1 DISABLED_InsertItemWithSubmenu1
+#else
+#define MAYBE_InsertItemWithSubmenu1 InsertItemWithSubmenu1
+#endif
+VIEW_TEST(MenuItemViewTestInsertWithSubmenu1, MAYBE_InsertItemWithSubmenu1)
 
 // Test class for removing a menu item while the menu is open.
 template <int REMOVE_INDEX, int SELECT_INDEX>
