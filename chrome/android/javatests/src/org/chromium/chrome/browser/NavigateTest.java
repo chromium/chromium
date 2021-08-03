ee336ccd2138d719b329aff6c3cac17935a3df32
Theresa Sullivan <twellington@chromium.org>
2021-08-03 16:14:49 +0000

    🤠 Fix NavigateTest#testIntentFallbackRedirection
    The test was previously failing on test-go-phone because it checked that
    the app was starting on the NTP rather than starting on the homepage
    (which on test-go-phone is not the NTP). This test only cares that the
    browser doesn't start on the page it will navigate to, so check for that
    instead.
    
    Bug: 1233114
    Change-Id: Id405fbe606dfa26b27cbe1b0ff61678b9294ca54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067828
    Auto-Submit: Theresa  <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907998}

--- chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
@@ -74,7 +74,6 @@ public class NavigateTest {
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
     private static final String HTTPS_SCHEME = "https://";
-    private static final String NEW_TAB_PAGE = "chrome-native://newtab/";
 
     private EmbeddedTestServer mTestServer;
 
@@ -391,14 +390,8 @@ public class NavigateTest {
      */
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1233114")
     @Feature({"Navigation"})
     public void testIntentFallbackRedirection() throws Exception {
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
-        Assert.assertEquals(NEW_TAB_PAGE,
-                ChromeTabUtils.getUrlStringOnUiThread(
-                        mActivityTestRule.getActivity().getActivityTab()));
-
         final String fallbackUrl =
                 mTestServer.getURL("/chrome/test/data/android/redirect/about.html");
         final String redirectUrl = "intent://non_existent/#Intent;scheme=non_existent;"
@@ -407,12 +400,19 @@ public class NavigateTest {
                 mTestServer.getURL("/chrome/test/data/android/redirect/js_redirect.html"
                         + "?replace_text="
                         + Base64.encodeToString(
-                                  ApiCompatibilityUtils.getBytesUtf8("PARAM_URL"), Base64.URL_SAFE)
+                                ApiCompatibilityUtils.getBytesUtf8("PARAM_URL"), Base64.URL_SAFE)
                         + ":"
-                        + Base64.encodeToString(ApiCompatibilityUtils.getBytesUtf8(redirectUrl),
-                                  Base64.URL_SAFE));
-        final String targetUrl =
-                mTestServer.getURL("/chrome/test/data/android/redirect/one.html");
+                        + Base64.encodeToString(
+                                ApiCompatibilityUtils.getBytesUtf8(redirectUrl), Base64.URL_SAFE));
+        final String targetUrl = mTestServer.getURL("/chrome/test/data/android/redirect/one.html");
+
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        // We should start on the homepage, which is something other than our test page.
+        String originalUrl = ChromeTabUtils.getUrlStringOnUiThread(
+                mActivityTestRule.getActivity().getActivityTab());
+        Criteria.checkThat(originalUrl, Matchers.not(targetUrl));
+
         typeInOmniboxAndNavigate(initialUrl, null);
 
         // Now intent fallback should be triggered assuming 'non_existent' scheme cannot be handled.
@@ -440,7 +440,7 @@ public class NavigateTest {
                                                .getEntryAtIndex(0)
                                                .getUrl()
                                                .getSpec();
-        Assert.assertEquals(NEW_TAB_PAGE, previousNavigationUrl);
+        Assert.assertEquals(originalUrl, previousNavigationUrl);
     }
 
     /**
