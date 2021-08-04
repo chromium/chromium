5bc57681793ef5008225cf26900492f62f640dd0
Christopher Cameron <ccameron@chromium.org>
2021-08-04 16:27:23 +0000

    Disable kDesktopCaptureMacV2
    Also move both kDesktopCaptureMacV2 and kWindowCaptureMacV2 to the
    location of their only use.
    
    Change-Id: I1add4f80786321f7d8e9f83f9bf9199d892b0766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069272
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908480}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -1054,12 +1054,6 @@ const base::Feature kMacSyscallSandbox{"MacSyscallSandbox",
 const base::Feature kRetryGetVideoCaptureDeviceInfos{
     "RetryGetVideoCaptureDeviceInfos", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kDesktopCaptureMacV2{"DesktopCaptureMacV2",
-                                         base::FEATURE_ENABLED_BY_DEFAULT};
-
-const base::Feature kWindowCaptureMacV2{"WindowCaptureMacV2",
-                                        base::FEATURE_ENABLED_BY_DEFAULT};
-
 #endif  // defined(OS_MAC)
 
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
