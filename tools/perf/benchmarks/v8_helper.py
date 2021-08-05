cd1e4a4c36175958cce3225388478e5aadc1c992
Mikhail Khokhlov <khokhlov@google.com>
2021-08-05 09:17:44 +0000

    Reland "[tools/perf] Reduce tracing buffer size on low-end devices"
    This is a reland of a2b9e17c30729acd7fc51ed34dce09c901e41927
    
    Relanding with a fix for platforms that don't have adb installed.
    
    Original change's description:
    > [tools/perf] Reduce tracing buffer size on low-end devices
    >
    > Devices with <1GB total memory might not be able to allocate
    > the 400MB tracing buffer, so we reduce the buffer size for them.
    > This might cause occasional problems with trace data loss but they
    > are much rarer in practice.
    >
    > Bug: 1218139
    > Change-Id: Icba72318e36d454f0b47b0990a7c0fe75521b26e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2989027
    > Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    > Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    > Cr-Commit-Position: refs/heads/master@{#902030}
    
    (cherry picked from commit b15b9577a597bc40e38c24a091a5f7f820ff4863)
    
    Bug: 1218139, 1229851
    Change-Id: I08b871afe14a09084e45f9eea4d4d3bdbae7fa98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032096
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Auto-Submit: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#902541}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071283
    Cr-Commit-Position: refs/branch-heads/4577@{#471}
    Cr-Branched-From: 761ddde228655e313424edec06497d0c56b0f3c4-refs/heads/master@{#902210}

--- tools/perf/benchmarks/v8_helper.py
+++ tools/perf/benchmarks/v8_helper.py
@@ -2,10 +2,27 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from devil import base_error as devil_error  # pylint: disable=import-error
+from devil.android import device_utils  # pylint: disable=import-error
 from telemetry.web_perf import timeline_based_measurement
 from telemetry.timeline import chrome_trace_config
 
 _JS_FLAGS_SWITCH = '--js-flags='
+LOW_END_DEVICE_MEMORY_KB = 1024 * 1024  # 1 GB
+
+
+def GetDeviceTotalMemory():
+  try:
+    devices = device_utils.DeviceUtils.HealthyDevices()
+    if not devices:
+      return None
+    mem_info = devices[0].ReadFile('/proc/meminfo')
+  except devil_error.BaseError:
+    return None
+  for line in mem_info.splitlines():
+    if line.startswith('MemTotal:'):
+      return int(line.split()[1])
+
 
 def AppendJSFlags(options, js_flags):
   existing_js_flags = ''
@@ -58,7 +75,13 @@ def AugmentOptionsForV8Metrics(options, enable_runtime_call_stats=True):
   memory_dump_config.AddTrigger('light', 1000)
   options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
 
-  options.config.chrome_trace_config.SetTraceBufferSizeInKb(400 * 1024)
+  # On low-end devices there's not enough memory to hold 400Mb buffer (See
+  # crbug.com/1218139).
+  device_memory = GetDeviceTotalMemory()
+  if device_memory and device_memory < LOW_END_DEVICE_MEMORY_KB:
+    options.config.chrome_trace_config.SetTraceBufferSizeInKb(200 * 1024)
+  else:
+    options.config.chrome_trace_config.SetTraceBufferSizeInKb(400 * 1024)
 
   metrics = [
       'blinkGcMetric',
