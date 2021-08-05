6002255d54fb2057749512fa4bf0a5e1648f256c
Rune Lillesveen <futhark@chromium.org>
2021-08-05 09:55:34 +0000

    Revert "Media Controls: Support live stream."
    This reverts commit 15ce14e336345e10f7366f4acb4b388c6b3952c3.
    
    Reason for revert: Test flaky on Mac10.14 crbug.com/1236920
    
    Original change's description:
    > Media Controls: Support live stream.
    >
    > This CL makes default media controls working for live stream.
    >
    > Bug: 771706
    > Change-Id: Ic4634313f7b8786e1e55f232c68310f5d8ed39ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042502
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#908237}
    
    Bug: 771706
    Change-Id: Iac4c0ac151934ceb42ec91629c9638459bb49471
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074119
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rune Lillesveen <futhark@google.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908813}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -1864,13 +1864,11 @@ void MediaControlsImpl::OnPlay() {
 void MediaControlsImpl::OnPlaying() {
   StartHideMediaControlsTimer();
   UpdateCSSClassFromState();
-  timeline_->OnMediaPlaying();
 }
 
 void MediaControlsImpl::OnPause() {
   UpdatePlayState();
   UpdateTimeIndicators();
-  timeline_->OnMediaStoppedPlaying();
   MakeOpaque();
 
   StopHideMediaControlsTimer();
@@ -1995,7 +1993,7 @@ void MediaControlsImpl::ElementSizeChangedTimerFired(TimerBase*) {
 }
 
 void MediaControlsImpl::OnLoadingProgress() {
-  timeline_->OnProgress();
+  timeline_->RenderBarSegments();
 }
 
 void MediaControlsImpl::ComputeWhichControlsFit() {
@@ -2195,7 +2193,6 @@ MediaControlOverflowMenuButtonElement& MediaControlsImpl::OverflowButton() {
 }
 
 void MediaControlsImpl::OnWaiting() {
-  timeline_->OnMediaStoppedPlaying();
   UpdateCSSClassFromState();
 }
 
