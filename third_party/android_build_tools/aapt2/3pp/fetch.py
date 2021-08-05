a98766c6e08f56e033de4b1ac68d9bf219e264d9
Mohamed Heikal <mheikal@chromium.org>
2021-08-05 20:13:51 +0000

    Migrate //third_party/android_build_tools/aapt2 to 3pp bot
    Bug: 1237059
    Change-Id: Ibb276d3cdab46a187ae6970f521324054a2e4645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074811
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909015}

---
+++ third_party/android_build_tools/aapt2/3pp/fetch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import print_function
+
+import argparse
+import json
+import os
+
+_FILE_URL = 'https://dl.google.com/dl/android/maven2/com/android/tools/build/aapt2/{0}/aapt2-{0}-linux.jar'
+_FILE_NAME = 'aapt2-{0}-linux.jar'
+_FILE_VERSION = '7.0.0-7396180'
+
+
+def do_latest():
+    print(_FILE_VERSION)
+
+
+def get_download_url(version):
+    partial_manifest = {
+        'url': [_FILE_URL.format(version)],
+        'name': [_FILE_NAME.format(version)],
+        'ext': '.jar',
+    }
+    print(json.dumps(partial_manifest))
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    sub = ap.add_subparsers()
+
+    latest = sub.add_parser("latest")
+    latest.set_defaults(func=lambda _opts: do_latest())
+
+    download = sub.add_parser("get_url")
+    download.set_defaults(
+        func=lambda _opts: get_download_url(os.environ['_3PP_VERSION']))
+
+    opts = ap.parse_args()
+    opts.func(opts)
+
+
+if __name__ == '__main__':
+    main()
