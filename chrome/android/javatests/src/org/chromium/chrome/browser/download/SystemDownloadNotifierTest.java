ad15f1e5a42c3dc052d89d506671cc9c2a3d0c5d
Min Qin <qinmin@chromium.org>
2021-08-05 19:48:49 +0000

    Fix flaky SystemDownloadNotifierTest
    Need to clear the notification throttler so that notifications already
    in the throttler won't impact a test.
    
    BUG=1236710
    
    Change-Id: I9cb5698a6df732c15ec0045b98606869cbf1cb60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3073062
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909003}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/SystemDownloadNotifierTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/SystemDownloadNotifierTest.java
@@ -9,6 +9,7 @@ import android.os.Looper;
 import androidx.test.filters.SmallTest;
 
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -22,6 +23,7 @@ import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
+import org.chromium.components.browser_ui.notifications.ThrottlingNotificationScheduler;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.OfflineItemSchedule;
 import org.chromium.components.offline_items_collection.PendingState;
@@ -53,6 +55,11 @@ public class SystemDownloadNotifierTest {
             mSystemDownloadNotifier.setDownloadNotificationService(
                     mMockDownloadNotificationService);
         });
+    }
+
+    @After
+    public void tearDown() {
+        ThrottlingNotificationScheduler.getInstance().clear();
     }
 
     private DownloadInfo getDownloadInfo(ContentId id) {
