523792216ad8b93bbfb14fe13f921af400070882
Ramin Halavati <rhalavati@chromium.org>
2021-08-06 10:27:28 +0000

    Revert "[ntp][modules][taskV2] Make new recipe Task module"
    This reverts commit 3b5862f88e84e7fb1796d47571d57049ffd9043f.
    
    Reason for revert: Many NTP tests are failing.
    Bug: 1237386
    
    Original change's description:
    > [ntp][modules][taskV2] Make new recipe Task module
    >
    > This is the second version of the Task module for recipes, implemented
    > according to the V2 mocks. We are creating a separate module to
    > implement V2 to ensure the original version of the module continues
    > to work and remains unchanged. (This is not a complete module. It's
    > a frame for a module, and we will add content to it in future CLs.)
    > When the modules-redesigned-enabled flag is set, the new V2 recipes
    > Task module will be displayed. Otherwise, the original version will be
    > displayed.
    >
    > * Create a new (recipes) Task module.
    > * When modules redesigned flag is enabled,
    >   display this new Task module.
    > * Add unit test to test if module appears on render.
    >
    > Fixed: 1237083
    > Change-Id: I4f1d59d75a882ebb9c27b87efa2c2e6ef5792883
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3075086
    > Commit-Queue: Deanna Oei <doei@google.com>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#909156}
    
    Change-Id: I2070bae2302c2d0b2ce28de9ad0078a8bf452a09
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    TBR: doei@google.com, mahmadi@chromium.org, tiborg@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077383
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Owners-Override: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909227}

--- chrome/browser/resources/new_tab_page/modules/module_descriptors.js
+++ chrome/browser/resources/new_tab_page/modules/module_descriptors.js
@@ -18,7 +18,6 @@ import {dummyDescriptor, dummyDescriptor2} from './dummy/module.js';
 import {ModuleDescriptor} from './module_descriptor.js';
 // <if expr="not is_official_build">
 import {photosDescriptor} from './photos/module.js';
-import {recipeTasksDescriptor as recipeTasksV2Descriptor} from './recipes_v2/module.js';
 // </if>
 import {recipeTasksDescriptor, shoppingTasksDescriptor} from './task_module/module.js';
 
@@ -30,11 +29,7 @@ if (loadTimeData.getBoolean('shoppingTasksModuleEnabled')) {
 }
 
 if (loadTimeData.getBoolean('recipeTasksModuleEnabled')) {
-  if (loadTimeData.getBoolean('modulesRedesignedEnabled')) {
-    descriptors.push(recipeTasksV2Descriptor);
-  } else {
-    descriptors.push(recipeTasksDescriptor);
-  }
+  descriptors.push(recipeTasksDescriptor);
 }
 
 if (loadTimeData.getBoolean('chromeCartModuleEnabled')) {
