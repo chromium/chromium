262fd2146f765e1f334c0b39f4e4a4ba38266e31
Brandon Jones <bajones@chromium.org>
2021-08-06 04:19:40 +0000

    Add newly defined WebGPU limits
    Includes all the newly added limits except for maxComputeWorkgroupSize,
    since that one can't be implemented as defined at this point and is
    awaiting a spec change.
    
    Bug: 1212868
    Change-Id: I2ffc16e00e32fabd862198e5ac40cfeab029c430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072602
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909172}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
@@ -4,41 +4,68 @@
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_supported_limits.h"
 
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_extent_3d_dict.h"
+
 #include <algorithm>
 
-#define SUPPORTED_LIMITS                                                      \
-  X(maxTextureDimension1D, max_texture_dimension_1d_)                         \
-  X(maxTextureDimension2D, max_texture_dimension_2d_)                         \
-  X(maxTextureDimension3D, max_texture_dimension_3d_)                         \
-  X(maxTextureArrayLayers, max_texture_array_layers_)                         \
-  X(maxBindGroups, max_bind_groups_)                                          \
-  X(maxDynamicUniformBuffersPerPipelineLayout,                                \
-    max_dynamic_uniform_buffers_per_pipeline_layout_)                         \
-  X(maxDynamicStorageBuffersPerPipelineLayout,                                \
-    max_dynamic_storage_buffers_per_pipeline_layout_)                         \
-  X(maxSampledTexturesPerShaderStage, max_sampled_textures_per_shader_stage_) \
-  X(maxSamplersPerShaderStage, max_samplers_per_shader_stage_)                \
-  X(maxStorageBuffersPerShaderStage, max_storage_buffers_per_shader_stage_)   \
-  X(maxStorageTexturesPerShaderStage, max_storage_textures_per_shader_stage_) \
-  X(maxUniformBuffersPerShaderStage, max_uniform_buffers_per_shader_stage_)   \
-  X(maxUniformBufferBindingSize, max_uniform_buffer_binding_size_)            \
-  X(maxStorageBufferBindingSize, max_storage_buffer_binding_size_)            \
-  X(maxVertexBuffers, max_vertex_buffers_)                                    \
-  X(maxVertexAttributes, max_vertex_attributes_)                              \
-  X(maxVertexBufferArrayStride, max_vertex_buffer_array_stride_)
+#define SUPPORTED_LIMITS                                                       \
+  MAX(unsigned, maxTextureDimension1D, max_texture_dimension_1d_)              \
+  MAX(unsigned, maxTextureDimension2D, max_texture_dimension_2d_)              \
+  MAX(unsigned, maxTextureDimension3D, max_texture_dimension_3d_)              \
+  MAX(unsigned, maxTextureArrayLayers, max_texture_array_layers_)              \
+  MAX(unsigned, maxBindGroups, max_bind_groups_)                               \
+  MAX(unsigned, maxDynamicUniformBuffersPerPipelineLayout,                     \
+      max_dynamic_uniform_buffers_per_pipeline_layout_)                        \
+  MAX(unsigned, maxDynamicStorageBuffersPerPipelineLayout,                     \
+      max_dynamic_storage_buffers_per_pipeline_layout_)                        \
+  MAX(unsigned, maxSampledTexturesPerShaderStage,                              \
+      max_sampled_textures_per_shader_stage_)                                  \
+  MAX(unsigned, maxSamplersPerShaderStage, max_samplers_per_shader_stage_)     \
+  MAX(unsigned, maxStorageBuffersPerShaderStage,                               \
+      max_storage_buffers_per_shader_stage_)                                   \
+  MAX(unsigned, maxStorageTexturesPerShaderStage,                              \
+      max_storage_textures_per_shader_stage_)                                  \
+  MAX(unsigned, maxUniformBuffersPerShaderStage,                               \
+      max_uniform_buffers_per_shader_stage_)                                   \
+  MAX(uint64_t, maxUniformBufferBindingSize, max_uniform_buffer_binding_size_) \
+  MAX(uint64_t, maxStorageBufferBindingSize, max_storage_buffer_binding_size_) \
+  MIN(unsigned, minUniformBufferOffsetAlignment,                               \
+      min_uniform_buffer_offset_alignment_)                                    \
+  MIN(unsigned, minStorageBufferOffsetAlignment,                               \
+      min_storage_buffer_offset_alignment_)                                    \
+  MAX(unsigned, maxVertexBuffers, max_vertex_buffers_)                         \
+  MAX(unsigned, maxVertexAttributes, max_vertex_attributes_)                   \
+  MAX(unsigned, maxVertexBufferArrayStride, max_vertex_buffer_array_stride_)   \
+  MAX(unsigned, maxInterStageShaderComponents,                                 \
+      max_inter_stage_shader_components_)                                      \
+  MAX(unsigned, maxComputeWorkgroupStorageSize,                                \
+      max_compute_workgroup_storage_size_)                                     \
+  MAX(unsigned, maxComputeWorkgroupInvocations,                                \
+      max_compute_workgroup_invocations_)                                      \
+  MAX(unsigned, maxComputeWorkgroupSizeX, max_compute_workgroup_size_x_)       \
+  MAX(unsigned, maxComputeWorkgroupSizeY, max_compute_workgroup_size_y_)       \
+  MAX(unsigned, maxComputeWorkgroupSizeZ, max_compute_workgroup_size_z_)       \
+  MAX(unsigned, maxComputePerDimensionDispatchSize,                            \
+      max_compute_per_dimension_dispatch_size_)
 
 namespace blink {
 
 GPUSupportedLimits::GPUSupportedLimits(
-    const Vector<std::pair<String, unsigned>>& limits) {
+    const Vector<std::pair<String, uint64_t>>& limits) {
   for (const auto& key_value : limits) {
-#define X(name, member)                          \
-  if (key_value.first == #name) {                \
-    member = std::max(key_value.second, member); \
-    continue;                                    \
+#define MAX(type, name, member)                                     \
+  if (key_value.first == #name) {                                   \
+    member = std::max(static_cast<type>(key_value.second), member); \
+    continue;                                                       \
+  }
+#define MIN(type, name, member)                                     \
+  if (key_value.first == #name) {                                   \
+    member = std::min(static_cast<type>(key_value.second), member); \
+    continue;                                                       \
   }
     SUPPORTED_LIMITS
-#undef X
+#undef MIN
+#undef MAX
 
     // Unrecognized keys should be rejected by validation prior to reaching
     // this point.
@@ -46,21 +73,29 @@ GPUSupportedLimits::GPUSupportedLimits(
   }
 }
 
-#define X(name, member) \
-  unsigned GPUSupportedLimits::name() const { return member; }
+#define MAX(type, name, member) \
+  type GPUSupportedLimits::name() const { return member; }
+#define MIN MAX
 SUPPORTED_LIMITS
-#undef X
+#undef MIN
+#undef MAX
 
 GPUSupportedLimits::ValidationResult GPUSupportedLimits::ValidateLimit(
     const String& name,
-    unsigned value) {
-#define X(limit_name, member)                                  \
+    uint64_t value) {
+#define MAX(type, limit_name, member)                          \
   if (name == #limit_name) {                                   \
     return value <= limit_name() ? ValidationResult::Valid     \
                                  : ValidationResult::BadValue; \
   }
+#define MIN(type, limit_name, member)                          \
+  if (name == #limit_name) {                                   \
+    return value >= limit_name() ? ValidationResult::Valid     \
+                                 : ValidationResult::BadValue; \
+  }
   SUPPORTED_LIMITS
-#undef X
+#undef MIN
+#undef MAX
 
   // Unknown limit name.
   return ValidationResult::BadName;
--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.h
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.h
@@ -22,12 +22,12 @@ class GPUSupportedLimits final : public ScriptWrappable {
   GPUSupportedLimits() = default;
 
   explicit GPUSupportedLimits(
-      const Vector<std::pair<String, unsigned>>& limits);
+      const Vector<std::pair<String, uint64_t>>& limits);
 
   GPUSupportedLimits(const GPUSupportedLimits&) = delete;
   GPUSupportedLimits& operator=(const GPUSupportedLimits&) = delete;
 
-  ValidationResult ValidateLimit(const String& name, unsigned value);
+  ValidationResult ValidateLimit(const String& name, uint64_t value);
 
   unsigned maxTextureDimension1D() const;
   unsigned maxTextureDimension2D() const;
@@ -41,11 +41,20 @@ class GPUSupportedLimits final : public ScriptWrappable {
   unsigned maxStorageBuffersPerShaderStage() const;
   unsigned maxStorageTexturesPerShaderStage() const;
   unsigned maxUniformBuffersPerShaderStage() const;
-  unsigned maxUniformBufferBindingSize() const;
-  unsigned maxStorageBufferBindingSize() const;
+  uint64_t maxUniformBufferBindingSize() const;
+  uint64_t maxStorageBufferBindingSize() const;
+  unsigned minUniformBufferOffsetAlignment() const;
+  unsigned minStorageBufferOffsetAlignment() const;
   unsigned maxVertexBuffers() const;
   unsigned maxVertexAttributes() const;
   unsigned maxVertexBufferArrayStride() const;
+  unsigned maxInterStageShaderComponents() const;
+  unsigned maxComputeWorkgroupStorageSize() const;
+  unsigned maxComputeWorkgroupInvocations() const;
+  unsigned maxComputeWorkgroupSizeX() const;
+  unsigned maxComputeWorkgroupSizeY() const;
+  unsigned maxComputeWorkgroupSizeZ() const;
+  unsigned maxComputePerDimensionDispatchSize() const;
 
  private:
   unsigned max_texture_dimension_1d_ = 8192;
@@ -60,11 +69,20 @@ class GPUSupportedLimits final : public ScriptWrappable {
   unsigned max_storage_buffers_per_shader_stage_ = 4;
   unsigned max_storage_textures_per_shader_stage_ = 4;
   unsigned max_uniform_buffers_per_shader_stage_ = 12;
-  unsigned max_uniform_buffer_binding_size_ = 16384;
-  unsigned max_storage_buffer_binding_size_ = 134217728;
+  uint64_t max_uniform_buffer_binding_size_ = 16384;
+  uint64_t max_storage_buffer_binding_size_ = 134217728;
+  unsigned min_uniform_buffer_offset_alignment_ = 256;
+  unsigned min_storage_buffer_offset_alignment_ = 256;
   unsigned max_vertex_buffers_ = 8;
   unsigned max_vertex_attributes_ = 16;
   unsigned max_vertex_buffer_array_stride_ = 2048;
+  unsigned max_inter_stage_shader_components_ = 60;
+  unsigned max_compute_workgroup_storage_size_ = 16352;
+  unsigned max_compute_workgroup_invocations_ = 256;
+  unsigned max_compute_workgroup_size_x_ = 256;
+  unsigned max_compute_workgroup_size_y_ = 256;
+  unsigned max_compute_workgroup_size_z_ = 64;
+  unsigned max_compute_per_dimension_dispatch_size_ = 65535;
 };
 
 }  // namespace blink
