8adf3b101f6b59944374b0f5dc164615cb91f6e3
Gordon Seto <gordonseto@google.com>
2021-08-06 19:56:16 +0000

    Reland "[CrOS Bluetooth] Fix paired device list not rendering items correctly."
    This reverts commit 1ba4215f06a3cba0f98dde661b9ca2636bf5abed.
    
    Reason for revert: Fix flakiness caused by CrScrollableBehavior using
    setInterval() before resizing the list. The test initially waited for
    this by using waitAfterNextRender(). However, this would cause flakiness
    because setInterval() was not guaranteed to fire before
    waitAfterNextRender() finished. This CL fixes this by waiting for the
    'iron-resize' event which is fired after the list is resized.
    
    Original change's description:
    > Revert "[CrOS Bluetooth] Fix paired device list not rendering items correctly."
    >
    > This reverts commit edb59223a517371734394172739cceef594c20b2.
    >
    > Reason for revert: https://crbug.com/1236907
    >
    > Original change's description:
    > > [CrOS Bluetooth] Fix paired device list not rendering items correctly.
    > >
    > > Add CrScrollableBehavior to <os-settings-paired-bluetooth-list> to fix
    > > list not rendering items correctly when updates with new devices.
    > >
    > > Bug: 1010321
    > > Change-Id: I5cb11b458e04aa0360348c268176aa0af8010a3b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072280
    > > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > > Commit-Queue: Gordon Seto <gordonseto@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#908587}
    >
    > Bug: 1010321, 1236907
    > Change-Id: Ib6edeefca80335abfc8b9167c28bbb8368e2ace8
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074742
    > Owners-Override: Rune Lillesveen <futhark@google.com>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#908863}
    
    Bug: 1010321, 1236907
    Change-Id: Ie5584f8e85b8ac9e4a1769213403a56614850d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077439
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#909429}

--- chrome/test/data/webui/settings/chromeos/os_paired_bluetooth_list_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_paired_bluetooth_list_tests.js
@@ -8,7 +8,8 @@
 // #import 'chrome://os-settings/strings.m.js';
 
 // #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-// #import {assertTrue} from '../../../chai_assert.js';
+// #import {assertTrue, assertEquals} from '../../../chai_assert.js';
+// #import {eventToPromise} from 'chrome://test/test_util.m.js';
 // clang-format on
 
 suite('OsPairedBluetoothListTest', function() {
@@ -22,8 +23,33 @@ suite('OsPairedBluetoothListTest', function() {
     Polymer.dom.flush();
   });
 
+  function flushAsync() {
+    Polymer.dom.flush();
+    return new Promise(resolve => setTimeout(resolve));
+  }
+
   test('Base Test', function() {
     const list = pairedBluetoothList.shadowRoot.querySelector('iron-list');
     assertTrue(!!list);
+  });
+
+  test('Device list change renders items correctly', async function() {
+    // TODO(crbug.com/1010321): Use real Device objects.
+    pairedBluetoothList.devices = [{}, {}, {}];
+    await flushAsync();
+
+    const getListItems = () => {
+      return pairedBluetoothList.shadowRoot.querySelectorAll(
+          'os-settings-paired-bluetooth-list-item');
+    };
+    assertEquals(getListItems().length, 3);
+
+    const ironResizePromise =
+        test_util.eventToPromise('iron-resize', pairedBluetoothList);
+    pairedBluetoothList.devices = [{}, {}, {}, {}, {}];
+
+    await ironResizePromise;
+    Polymer.dom.flush();
+    assertEquals(getListItems().length, 5);
   });
 });
