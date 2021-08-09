0ae99d7fc4c95f92ea95cd76d81c4bd7e3f05347
Tomasz Wiszkowski <ender@google.com>
2021-08-09 20:57:44 +0000

    Revert "Add logic tracking possible UAF for ACMatch."
    This reverts commit 524e52ac7a7dd90dc9ad5f6edd274bf99535064f.
    
    Reason for revert: Culprit found. See crbug.com/1217575 for more details.
    
    Original change's description:
    > Add logic tracking possible UAF for ACMatch.
    >
    > The logic here should help us track and report any instances where
    > ACMatch was used after being freed.
    >
    > This logic will track, report and prevent any native operations on
    > freed ACMatch objects including, but not limited to, match deletions
    > and navigations. Note that the navigation itself will still work, but
    > the change may prevent some data (such as AQS) from being reported
    > if the native match is already deleted.
    >
    > Assuming we have no instances of UAF, the change should be safe to
    > roll back as a whole, since the metric enum introduced here should
    > never be recorded - alternatively we can keep the enum value around.
    >
    > (cherry picked from commit c44b00f2e8e3780b9dc6ab54052755deb2e2ad27)
    >
    > Bug: 1217575
    > Change-Id: I6cdd88bd37cff8b882b3b952d9edff32f3019ca1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053416
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#905881}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058740
    > Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/branch-heads/4577@{#279}
    > Cr-Branched-From: 761ddde228655e313424edec06497d0c56b0f3c4-refs/heads/master@{#902210}
    
    Bug: 1217575
    Change-Id: Idfab7adc91aece6c31b3c1af258ac51ec65a4923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078926
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909968}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -9,7 +9,6 @@
 #include "base/check_op.h"
 #include "base/cxx17_backports.h"
 #include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/logging.h"
@@ -43,6 +42,7 @@
 #endif
 
 namespace {
+
 bool IsTrivialClassification(const ACMatchClassifications& classifications) {
   return classifications.empty() ||
       ((classifications.size() == 1) &&
@@ -225,11 +225,7 @@ AutocompleteMatch::AutocompleteMatch(const AutocompleteMatch& match)
       additional_info(match.additional_info),
       duplicate_matches(match.duplicate_matches),
       query_tiles(match.query_tiles),
-      navsuggest_tiles(match.navsuggest_tiles) {
-#if defined(OS_ANDROID)
-  magic_signature_ = match.magic_signature_;
-#endif
-}
+      navsuggest_tiles(match.navsuggest_tiles) {}
 
 AutocompleteMatch::AutocompleteMatch(AutocompleteMatch&& match) noexcept {
   *this = std::move(match);
@@ -290,8 +286,6 @@ AutocompleteMatch& AutocompleteMatch::operator=(
   DestroyJavaObject();
   std::swap(java_match_, match.java_match_);
   UpdateJavaObjectNativeRef();
-  magic_signature_ = match.magic_signature_;
-  match.magic_signature_ = 0;
 #endif
   return *this;
 }
@@ -299,7 +293,6 @@ AutocompleteMatch& AutocompleteMatch::operator=(
 AutocompleteMatch::~AutocompleteMatch() {
 #if defined(OS_ANDROID)
   DestroyJavaObject();
-  magic_signature_ = 0;
 #endif
 }
 
@@ -993,16 +986,6 @@ void AutocompleteMatch::RecordAdditionalInfo(const std::string& property,
 
 std::string AutocompleteMatch::GetAdditionalInfo(
     const std::string& property) const {
-#if defined(OS_ANDROID)
-  if (!CheckMatchAlive()) {
-    SCOPED_CRASH_KEY_STRING32("ACMatch", "bad-magic", "true");
-    DCHECK(false)
-        << "Please report this on crbug.com/1217575, and include both C++ and "
-        << "Java stack traces where possible.";
-    base::debug::DumpWithoutCrashing();
-    return "";
-  }
-#endif
   auto i(additional_info.find(property));
   return (i == additional_info.end()) ? std::string() : i->second;
 }
