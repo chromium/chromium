800c653b7fba6dfd7b00843a5159090c2a50bc7f
Mei Liang <meiliang@chromium.org>
2021-08-09 17:57:44 +0000

    Address NPE in TabGroupUtils
    (cherry picked from commit 0c5f3c3d32f15dfbc0016b2dbffba3a4e287a53c)
    
    Bug: 1229542
    Change-Id: I4a2914da8941a8fb12ca005c10c60b9c701be596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067665
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#908032}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074816
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4515@{#1994}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_groups/TabGroupUtils.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_groups/TabGroupUtils.java
@@ -46,15 +46,13 @@ public class TabGroupUtils {
 
     public static void maybeShowIPH(@FeatureConstants String featureName, View view,
             @Nullable BottomSheetController bottomSheetController) {
+        if (view == null) return;
         // For tab group, all three IPHs are valid. For conditional tab strip, the only valid IPH
         // below is TAB_GROUPS_TAP_TO_SEE_ANOTHER_TAB_FEATURE.
         if (!TabUiFeatureUtilities.isTabGroupsAndroidEnabled(view.getContext())
                 && !(TabUiFeatureUtilities.isConditionalTabStripEnabled()
                         && featureName.equals(
                                 FeatureConstants.TAB_GROUPS_TAP_TO_SEE_ANOTHER_TAB_FEATURE))) {
-            return;
-        }
-        if (TabUiFeatureUtilities.isLaunchPolishEnabled() && view == null) {
             return;
         }
 
