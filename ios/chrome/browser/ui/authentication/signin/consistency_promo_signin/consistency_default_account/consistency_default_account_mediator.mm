9b032ef6144added757a98ca0f676ea11d613bef
Ewann <ewannpv@google.com>
2021-08-09 09:23:13 +0000

    [iOS][7/N] Adds ChromeAccountManagerServiceObserver
    This CL adds ChromeAccountManagerServiceObserver to
    ConsistencyDefaultAccountMediator.
    
    Bug: 1220345
    Change-Id: Ie6d40d33754f3a2a78ac407a6ef94a91e2254884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077381
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909737}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_mediator.mm
@@ -4,23 +4,20 @@
 
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_mediator.h"
 
-#import "ios/chrome/browser/chrome_browser_provider_observer_bridge.h"
 #import "ios/chrome/browser/signin/chrome_account_manager_service.h"
-#import "ios/chrome/browser/signin/chrome_identity_service_observer_bridge.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service_observer_bridge.h"
 #import "ios/chrome/browser/ui/authentication/resized_avatar_cache.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_default_account/consistency_default_account_consumer.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
-#import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @interface ConsistencyDefaultAccountMediator () <
-    ChromeBrowserProviderObserver,
-    ChromeIdentityServiceObserver> {
-  std::unique_ptr<ChromeIdentityServiceObserverBridge> _identityServiceObserver;
-  std::unique_ptr<ChromeBrowserProviderObserverBridge> _browserProviderObserver;
+    ChromeAccountManagerServiceObserver> {
+  std::unique_ptr<ChromeAccountManagerServiceObserverBridge>
+      _accountManagerServiceObserver;
 }
 
 @property(nonatomic, strong) UIImage* avatar;
@@ -36,10 +33,9 @@
   if (self = [super init]) {
     DCHECK(accountManagerService);
     _accountManagerService = accountManagerService;
-    _identityServiceObserver =
-        std::make_unique<ChromeIdentityServiceObserverBridge>(self);
-    _browserProviderObserver =
-        std::make_unique<ChromeBrowserProviderObserverBridge>(self);
+    _accountManagerServiceObserver =
+        std::make_unique<ChromeAccountManagerServiceObserverBridge>(
+            self, _accountManagerService);
     _avatarCache = [[ResizedAvatarCache alloc] initWithDefaultLarge];
   }
   return self;
@@ -51,6 +47,7 @@
 
 - (void)disconnect {
   self.accountManagerService = nullptr;
+  _accountManagerServiceObserver.reset();
 }
 
 #pragma mark - Properties
@@ -100,32 +97,16 @@
   [self.consumer updateUserAvatar:avatar];
 }
 
-#pragma mark - ChromeBrowserProviderObserver
-
-- (void)chromeIdentityServiceDidChange:(ios::ChromeIdentityService*)identity {
-  DCHECK(!_identityServiceObserver.get());
-  _identityServiceObserver =
-      std::make_unique<ChromeIdentityServiceObserverBridge>(self);
-}
-
-- (void)chromeBrowserProviderWillBeDestroyed {
-  _browserProviderObserver.reset();
-}
-
-#pragma mark - ChromeIdentityServiceObserver
+#pragma mark - ChromeAccountManagerServiceObserver
 
 - (void)identityListChanged {
   [self selectSelectedIdentity];
 }
 
-- (void)profileUpdate:(ChromeIdentity*)identity {
+- (void)identityChanged:(ChromeIdentity*)identity {
   if ([self.selectedIdentity isEqual:identity]) {
     [self updateSelectedIdentityUI];
   }
-}
-
-- (void)chromeIdentityServiceWillBeDestroyed {
-  _identityServiceObserver.reset();
 }
 
 @end
