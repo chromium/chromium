91df03601eb5e7b34ced91d784a7db846733e532
Yuwei Huang <yuweih@chromium.org>
2021-08-09 17:45:07 +0000

    [remoting][windows host] Replace setup URL forwarder dialog with a task dialog
    The setup URL forwarder dialog was implemented using the DIALOGEX
    control, which has a quirk such that the dialog's message text cannot
    exceed 256 characters. This has lead to a translation merge revert in
    crrev.com/c/3075199.
    
    This CL fixes the issue by replacing the DIALOGEX control with a
    TASKDIALOG that is programmatically created and laid out, which doesn't
    have the 256 character limitation. The code is mostly modified from
    it2me_confirmation_dialog_win.cc, and it looks subjectively better than
    the previous dialog.
    
    This CL has been tested by reverting CL 3075199, which previous would
    cause a build failure, but no longer does it after the change.
    
    Screenshot: https://screenshot.googleplex.com/9n4M7aYBG8xub38
    
    Bug: b:183135000
    Change-Id: I3846b03906fdbfeebcfe11af0a25052bc6b896fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082307
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4602@{#4}
    Cr-Branched-From: 9627326cf03b5608d46e71bc484a8d60deccbe2c-refs/heads/master@{#909676}

--- remoting/host/it2me/it2me_confirmation_dialog_win.cc
+++ remoting/host/it2me/it2me_confirmation_dialog_win.cc
@@ -20,6 +20,7 @@
 #include "base/time/time.h"
 #include "remoting/host/it2me/it2me_confirmation_dialog.h"
 #include "remoting/host/win/core_resource.h"
+#include "remoting/host/win/simple_task_dialog.h"
 
 namespace remoting {
 
@@ -28,57 +29,22 @@ namespace {
 // Time to wait before closing the dialog and cancelling the connection.
 constexpr base::TimeDelta kDialogTimeout = base::TimeDelta::FromMinutes(1);
 
-const HRESULT kTimeoutErrorCode = E_ABORT;
-
-// Loads an embedded string resource from the specified module.
-bool LoadStringResource(HMODULE resource_module,
-                        int resource_id,
-                        std::wstring* string) {
-  DCHECK(resource_module);
-  DCHECK(string);
-
-  string->clear();
-
-  const wchar_t* string_resource = nullptr;
-  int string_length = LoadStringW(resource_module, resource_id,
-                                  reinterpret_cast<wchar_t*>(&string_resource),
-                                  /*nBufferMax=*/0);
-  if (string_length <= 0) {
-    PLOG(ERROR) << "LoadStringW() failed";
-    return false;
-  }
-
-  string->append(string_resource, string_length);
-  return true;
-}
-
 class It2MeConfirmationDialogWin : public It2MeConfirmationDialog {
  public:
   It2MeConfirmationDialogWin();
   ~It2MeConfirmationDialogWin() override;
-
-  static HRESULT CALLBACK TaskDialogCallbackProc(HWND hwnd,
-                                                 UINT notification,
-                                                 WPARAM w_param,
-                                                 LPARAM l_param,
-                                                 LONG_PTR ref_data);
 
   // It2MeConfirmationDialog implementation.
   void Show(const std::string& remote_user_email,
             ResultCallback callback) override;
 
  private:
-  // Tracks whether the dialog was in the foreground the last time we checked.
-  // Default to true so we will attempt to bring it back if it starts in the
-  // background for some reason.
-  bool is_foreground_window_ = true;
-
   DISALLOW_COPY_AND_ASSIGN(It2MeConfirmationDialogWin);
 };
 
-It2MeConfirmationDialogWin::It2MeConfirmationDialogWin() {}
+It2MeConfirmationDialogWin::It2MeConfirmationDialogWin() = default;
 
-It2MeConfirmationDialogWin::~It2MeConfirmationDialogWin() {}
+It2MeConfirmationDialogWin::~It2MeConfirmationDialogWin() = default;
 
 void It2MeConfirmationDialogWin::Show(const std::string& remote_user_email,
                                       ResultCallback callback) {
@@ -99,127 +65,49 @@ void It2MeConfirmationDialogWin::Show(const std::string& remote_user_email,
     return;
   }
 
-  std::wstring title_text;
-  if (!LoadStringResource(resource_module, IDS_PRODUCT_NAME, &title_text)) {
-    LOG(ERROR) << "Failed to load title text for confirmation dialog.";
+  SimpleTaskDialog task_dialog(resource_module);
+  if (!task_dialog.SetTitleTextWithStringId(IDS_PRODUCT_NAME) ||
+      !task_dialog.AppendButtonWithStringId(IDYES,
+                                            IDS_SHARE_CONFIRM_DIALOG_CONFIRM) ||
+      !task_dialog.AppendButtonWithStringId(IDNO,
+                                            IDS_SHARE_CONFIRM_DIALOG_DECLINE)) {
+    LOG(ERROR) << "Failed to load text for confirmation dialog.";
     std::move(callback).Run(result);
     return;
   }
 
-  std::wstring message_text;
-  if (!LoadStringResource(resource_module,
-                          IDS_SHARE_CONFIRM_DIALOG_MESSAGE_WITH_USERNAME,
-                          &message_text)) {
-    LOG(ERROR) << "Failed to load message text for confirmation dialog.";
+  const wchar_t* message_stringw = nullptr;
+  int string_length = LoadStringW(
+      resource_module, IDS_SHARE_CONFIRM_DIALOG_MESSAGE_WITH_USERNAME,
+      reinterpret_cast<wchar_t*>(&message_stringw),
+      /*cchBufferMax=*/0);
+  if (string_length <= 0) {
+    PLOG(ERROR) << "Failed to load message text for confirmation dialog.";
     std::move(callback).Run(result);
     return;
   }
-  message_text =
+  std::wstring message_text =
       base::AsWString(base::i18n::MessageFormatter::FormatWithNumberedArgs(
-          base::AsStringPiece16(message_text),
+          base::AsStringPiece16(
+              base::WStringPiece(message_stringw, string_length)),
           base::UTF8ToUTF16(remote_user_email)));
 
-  std::wstring share_button_text;
-  if (!LoadStringResource(resource_module, IDS_SHARE_CONFIRM_DIALOG_CONFIRM,
-                          &share_button_text)) {
-    LOG(ERROR) << "Failed to load share button text for confirmation dialog.";
+  task_dialog.set_message_text(message_text);
+  task_dialog.set_default_button(IDNO);
+  task_dialog.set_dialog_timeout(kDialogTimeout);
+  absl::optional<int> button_result = task_dialog.Show();
+
+  if (!button_result.has_value()) {
     std::move(callback).Run(result);
     return;
   }
 
-  std::wstring decline_button_text;
-  if (!LoadStringResource(resource_module, IDS_SHARE_CONFIRM_DIALOG_DECLINE,
-                          &decline_button_text)) {
-    LOG(ERROR) << "Failed to load decline button text for confirmation dialog.";
-    std::move(callback).Run(result);
-    return;
-  }
-
-  TASKDIALOG_BUTTON dialog_buttons[] = {
-      {IDYES, share_button_text.c_str()}, {IDNO, decline_button_text.c_str()},
-  };
-
-  TASKDIALOGCONFIG dialog_config = {0};
-  dialog_config.cbSize = sizeof(dialog_config);
-  dialog_config.hInstance = resource_module;
-  dialog_config.pszWindowTitle = title_text.c_str();
-  dialog_config.pszMainInstruction = message_text.c_str();
-  dialog_config.pszMainIcon = MAKEINTRESOURCE(IDI_CHROME_REMOTE_DESKTOP);
-  dialog_config.dwFlags = TDF_CALLBACK_TIMER;
-  dialog_config.pfCallback = &TaskDialogCallbackProc;
-  dialog_config.lpCallbackData = reinterpret_cast<LONG_PTR>(this);
-  dialog_config.cButtons = ARRAYSIZE(dialog_buttons);
-  dialog_config.pButtons = dialog_buttons;
-  dialog_config.nDefaultButton = IDNO;
-
-  int button_result = 0;
-  HRESULT hr = TaskDialogIndirect(&dialog_config, &button_result,
-                                  /*pnRadioButton=*/nullptr,
-                                  /*pfVerificationFlagChecked=*/nullptr);
-  if (FAILED(hr)) {
-    if (hr == kTimeoutErrorCode) {
-      LOG(INFO) << "TaskDialog timed out.";
-    } else {
-      LOG(ERROR) << "TaskDialogIndirect() Failed: 0x" << std::hex << hr;
-    }
-
-    std::move(callback).Run(result);
-    return;
-  }
-
-  if (button_result == IDYES) {
+  if (*button_result == IDYES) {
     // Only accept the connection if the user chose 'share'.
     result = Result::OK;
   }
 
   std::move(callback).Run(result);
-}
-
-HRESULT CALLBACK
-It2MeConfirmationDialogWin::TaskDialogCallbackProc(HWND hwnd,
-                                                   UINT notification,
-                                                   WPARAM w_param,
-                                                   LPARAM l_param,
-                                                   LONG_PTR ref_data) {
-  if (notification == TDN_TIMER) {
-    if (static_cast<int64_t>(w_param) >= kDialogTimeout.InMilliseconds()) {
-      // Close the dialog window if we have reached the timeout.
-      return kTimeoutErrorCode;
-    }
-
-    // Ensure the window is visible before checking if it is in the foreground.
-    if (!IsWindowVisible(hwnd)) {
-      ShowWindow(hwnd, SW_SHOWNORMAL);
-    }
-
-    // Attempt to bring the dialog window to the foreground if needed.  If the
-    // window is in the background and cannot be brought forward, this call will
-    // flash the placeholder on the taskbar.  Do not call SetForegroundWindow()
-    // multiple times as it will cause annoying flashing for the user.
-    It2MeConfirmationDialogWin* dialog =
-        reinterpret_cast<It2MeConfirmationDialogWin*>(ref_data);
-    if (hwnd == GetForegroundWindow()) {
-      dialog->is_foreground_window_ = true;
-    } else if (dialog->is_foreground_window_) {
-      SetForegroundWindow(hwnd);
-      dialog->is_foreground_window_ = false;
-    }
-
-    if (!dialog->is_foreground_window_) {
-      // Ensure the dialog is always at the top of the top-most window stack,
-      // even if it doesn't have focus, so the user can always see it.
-      BringWindowToTop(hwnd);
-    }
-  } else if (notification == TDN_CREATED) {
-    // After the dialog has been created, but before it is visible, set its
-    // z-order so it will be a top-most window and have always on top behavior.
-    if (!SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0,
-                      SWP_NOMOVE | SWP_NOSIZE)) {
-      PLOG(ERROR) << "SetWindowPos() failed";
-    }
-  }
-
-  return S_OK;
 }
 
 }  // namespace
