692c7be6b90b8e765c9d4778e9d067fa946a38bd
Roman Arora <romanarora@chromium.org>
2021-08-10 15:16:12 +0000

    Lacros: Blink Runtime enabled feature status parity presubmit check
    Adds a presubmit check to ensure that there is feature status parity
    across Lacros and ChromeOS feature status values
    
    Bug: 1235853
    Change-Id: I849b3785ce3671f1dbf39971d61d88d07c98f30b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076264
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910313}

--- third_party/blink/renderer/platform/PRESUBMIT.py
+++ third_party/blink/renderer/platform/PRESUBMIT.py
@@ -10,9 +10,42 @@ for more details about the presubmit API built into depot_tools.
 import difflib
 import os
 import re
+import sys
 
 USE_PYTHON3 = True
 RUNTIMEENABLED_NAME = re.compile(r'\s*name\s*:\s*"([^"]*)"')
+CHROMEOS_STATUS = "ChromeOS"
+LACROS_STATUS = "Lacros"
+
+# The ignore list will be removed once existing features adopt parity across
+# Lacros and ChromeOS.
+LACROS_CHROMEOS_FEATURE_STATUS_PARITY_IGNORE_LIST = [
+    'BarcodeDetector',  # crbug.com/1235855
+    'DigitalGoods',  # crbug.com/1235859
+    'ForceTallerSelectPopup',  # crbug.com/1235860
+    'NetInfoDownlinkMax',  # crbug.com/1235864
+    'WebBluetooth',  # crbug.com/1235867
+    'WebBluetoothManufacturerDataFilter',  # crbug.com/1235869
+    'WebBluetoothRemoteCharacteristicNewWriteValue',  # crbug.com/235870
+]
+
+
+# pyright: reportMissingImports=false
+def RuntimeEnabledFeatures(input_api, filename):
+    """Returns the features present in the specified features JSON5 file."""
+
+    # We need to wait until we have an input_api object and use this
+    # roundabout construct to import json5 because this file is
+    # eval-ed and thus doesn't have __file__.
+    try:
+        json5_path = input_api.os_path.join(input_api.PresubmitLocalPath(),
+                                            '..', '..', '..', 'pyjson5', 'src')
+        sys.path.append(json5_path)
+        import json5
+        return json5.load(open(filename, encoding='utf-8'))['data']
+    finally:
+        # Restore sys.path to what it was before.
+        sys.path.remove(json5_path)
 
 
 def RuntimeEnabledFeatureNames(filename):
@@ -50,10 +83,45 @@ def _CheckRuntimeEnabledFeaturesSorted(input_api, output_api):
     ]
 
 
+def _CheckLacrosChromeOSFeatureStatusParity(input_api, output_api):
+    """Check: runtime_enabled_features.json5 feature status parity across Lacros
+     and ChromeOS.
+    """
+
+    filename = os.path.join(input_api.PresubmitLocalPath(),
+                            'runtime_enabled_features.json5')
+    try:
+        features = RuntimeEnabledFeatures(input_api, filename)
+        # Check that all features with a status specified for ChromeOS have the
+        # same status specified for Lacros.
+        for feature in features:
+            if feature[
+                    'name'] in LACROS_CHROMEOS_FEATURE_STATUS_PARITY_IGNORE_LIST:
+                continue
+            if 'status' in feature and type(feature['status']) is dict:
+                status_dict = feature['status']
+                if (CHROMEOS_STATUS in status_dict
+                        or LACROS_STATUS in status_dict) and (
+                            status_dict.get(LACROS_STATUS) !=
+                            status_dict.get(CHROMEOS_STATUS)):
+                    return [output_api.PresubmitError('Feature {} does not have status parity '\
+                      'across Lacros and ChromeOS.'.format(feature['name']))]
+    except:
+        return [
+            output_api.PresubmitError(
+                'Failed to parse {} for checks'.format(filename))
+        ]
+
+    return []
+
+
 def _CommonChecks(input_api, output_api):
     """Checks common to both upload and commit."""
     results = []
     results.extend(_CheckRuntimeEnabledFeaturesSorted(input_api, output_api))
+    results.extend(
+        _CheckLacrosChromeOSFeatureStatusParity(input_api, output_api))
+
     return results
 
 
