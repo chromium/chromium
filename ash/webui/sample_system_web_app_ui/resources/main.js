a71e674fdfe9c052576070d87c45a07c3ccfb358
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2021-08-11 07:19:33 +0000

    sample-swa: Adds example Mojo interface to the Sample System Web App
    Based on: https://crrev.com/c/2538862
    
    This example shows how to:
    
      - Get a value from the browser process.
      - Set a value on the browser process.
      - Request the browser process to do something.
      - Have the browser notify the page about an event
    
    Change-Id: Ibcee09b7258a48a5c404a40d04f887f76bd1155a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960339
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#910696}

--- ash/webui/sample_system_web_app_ui/resources/main.js
+++ ash/webui/sample_system_web_app_ui/resources/main.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {pageHandler, callbackRouter} from './page_handler.js';
 
 const first = document.querySelector('#number1');
 const second = document.querySelector('#number2');
@@ -23,4 +24,34 @@ second.onchange = () => {
 myWorker.port.onmessage = (event) => {
   result.textContent = event.data[0];
   additional.value = event.data[1];
+};
+
+// Exposes the pageHandler to the user as a window's global variable for
+// testing.
+window.pageHandler = pageHandler;
+window.callbackRouter = callbackRouter;
+window.eventCount = new Map();
+
+// Example of adding an event listener for `OnEventOccurred`.
+callbackRouter.onEventOccurred.addListener((name) => {
+  document.querySelector('#mojo-event').value = name;
+  window.eventCount.set(name, 1 + (window.eventCount.get(name) || 0));
+});
+
+// Example of sending information to the browser process.
+pageHandler.send(`message at ${Date.now()}`);
+
+// Example of getting information from the browser process.
+(async () => {
+  // Mojo results get wrapped in a "response" object that contains
+  // a member for each of the Mojo callback's argument, in this case
+  // a `preferences` member.
+  const {preferences} = await pageHandler.getPreferences();
+  document.querySelector('#background').value = preferences.background;
+  document.querySelector('#foreground').value = preferences.foreground;
+})();
+
+const mojoButton = document.querySelector('#do-something');
+mojoButton.onclick = () => {
+  pageHandler.doSomething();
 };
--- ash/webui/sample_system_web_app_ui/sample_system_web_app_ui.h
+++ ash/webui/sample_system_web_app_ui/sample_system_web_app_ui.h
@@ -9,17 +9,46 @@
 #error Sample System Web App should only be included in unofficial builds.
 #endif
 
+#include <memory>
+
+#include "ash/webui/sample_system_web_app_ui/mojom/sample_system_web_app_ui.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 
 namespace ash {
 
 // The WebUI for chrome://sample-system-web-app/.
-class SampleSystemWebAppUI : public ui::MojoWebUIController {
+class SampleSystemWebAppUI : public ui::MojoWebUIController,
+                             public mojom::sample_swa::PageHandlerFactory {
  public:
   explicit SampleSystemWebAppUI(content::WebUI* web_ui);
   SampleSystemWebAppUI(const SampleSystemWebAppUI&) = delete;
   SampleSystemWebAppUI& operator=(const SampleSystemWebAppUI&) = delete;
   ~SampleSystemWebAppUI() override;
+
+  void BindInterface(
+      mojo::PendingReceiver<mojom::sample_swa::PageHandlerFactory> factory);
+
+ private:
+  // mojom::sample_swa::PageHandlerFactory:
+  void CreatePageHandler(
+      mojo::PendingReceiver<mojom::sample_swa::PageHandler> handler,
+      mojo::PendingRemote<mojom::sample_swa::Page> page) override;
+
+  mojo::Receiver<mojom::sample_swa::PageHandlerFactory> sample_page_factory_{
+      this};
+
+  // Handles requests from the user visible page. Created when the page calls
+  // PageHandlerFactory::CreatePageHandler(). Expected to live as long as
+  // the WebUIController. In most cases this matches the lifetime of the page.
+  // However, sometimes the WebUIController is re-used within same-origin
+  // navigations. Calling CreatePageHandler() multiple times will replace the
+  // existing sample_page_handler_.
+  std::unique_ptr<mojom::sample_swa::PageHandler> sample_page_handler_;
+
+  WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
 }  // namespace ash
