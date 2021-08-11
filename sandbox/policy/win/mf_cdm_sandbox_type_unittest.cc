449418d6b9297c79fa2feee38ed5950465b75b80
Dirk Pranke <dpranke@google.com>
2021-08-11 20:10:24 +0000

    Revert "Require valid sandbox string for utilities"
    This reverts commit 17104368d03c80e610d4a684aa034ed366d3c847.
    
    Reason for revert: Breaks chromeos bots, see crbug.com/1238952
    
    Original change's description:
    > Require valid sandbox string for utilities
    >
    > UtilitySandboxTypeFromString() did not have an exhaustive set of valid
    > utility sandbox types, so if a utility was started with a new type it
    > would be reported inside the utility as the default kUtility.
    >
    > For Linux this meant that the kUtility sandbox was applied, although
    > this is very close to kService on Linux.
    >
    > For Windows this was accidentally safe as no additional lockdown
    > would occur within the utility process, but for future sandbox
    > types this could cause a reduction in security.
    >
    > This CL removes the default and instead CHECKs, in combination with
    > the CHECK in SandboxTypeFromCommandLine() this should now cover all
    > paths.
    >
    > kService is now added as a valid service within a utility.
    >
    > Bug: 1238460
    > Change-Id: I509fbf4268536043562b72c42bdcf7504a6cc9a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086629
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Commit-Queue: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#910577}
    
    Bug: 1238460
    Change-Id: I5775c548b172dd72b456935175704ea55f82bc67
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089497
    Auto-Submit: Dirk Pranke <dpranke@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910943}

--- sandbox/policy/win/mf_cdm_sandbox_type_unittest.cc
+++ sandbox/policy/win/mf_cdm_sandbox_type_unittest.cc
@@ -11,10 +11,12 @@
 namespace media {
 
 TEST(SandboxTypeTest, Utility) {
-  // Setup to have '--type=utility' first (but no valid sandbox).
+  // Setup to have '--type=utility' first.
   base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
   command_line.AppendSwitchASCII(sandbox::policy::switches::kProcessType,
                                  sandbox::policy::switches::kUtilityProcess);
+  EXPECT_EQ(sandbox::policy::SandboxType::kUtility,
+            sandbox::policy::SandboxTypeFromCommandLine(command_line));
 
   base::CommandLine command_line2(command_line);
   SetCommandLineFlagsForSandboxType(
