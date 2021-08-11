2b6d1d2109a9b67fbc81314085ea51cb845cdb3a
Dan McArdle <dmcardle@chromium.org>
2021-08-11 00:51:22 +0000

    [WTF::HashMap Refactor] Use DeprecatedAtOrEmptyValue() in /third_party/blink/renderer/platform/audio
    This CL is a true behavior-preserving refactor. It will not affect
    performance because HashMap<>::DeprecatedAtOrEmptyValue() has an
    identical implementation to HashMap<>::at().
    
    In the near future, we will change the definition of HashMap<>::at() to
    CHECK that the map contains the given key.
    
    All occurrences of DeprecatedAtOrEmptyValue will need to be cleaned up.
    If the caller cannot know whether the map already contains the given
    key, one possible workaround is to use HashMap<>::find() instead.
    
    Design doc: https://docs.google.com/document/d/18JIiajErikZaBzCtZvl-wwAJShqZsg0jT9wVyPBhSdU/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=hongchan@chromium.org
    
    Bug: 1058527
    Change-Id: I6b2c9dd657d96d09d1d2d1e15d49975900b33709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081477
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910600}

--- third_party/blink/renderer/platform/audio/hrtf_database_loader.cc
+++ third_party/blink/renderer/platform/audio/hrtf_database_loader.cc
@@ -49,7 +49,8 @@ scoped_refptr<HRTFDatabaseLoader>
 HRTFDatabaseLoader::CreateAndLoadAsynchronouslyIfNecessary(float sample_rate) {
   DCHECK(IsMainThread());
 
-  scoped_refptr<HRTFDatabaseLoader> loader = GetLoaderMap().at(sample_rate);
+  scoped_refptr<HRTFDatabaseLoader> loader =
+      GetLoaderMap().DeprecatedAtOrEmptyValue(sample_rate);
   if (loader) {
     DCHECK_EQ(sample_rate, loader->DatabaseSampleRate());
     return loader;
