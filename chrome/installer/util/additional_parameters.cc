86b81f400aab0a2118a23e16b58f7ade67f619fd
Greg Thompson <grt@chromium.org>
2021-08-11 14:30:24 +0000

    Comment cleanups following r904903.
    Bug: 1229222
    Change-Id: Ia03420b20c0d54b25bd16c63f63d9d013a7408c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081181
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910800}

--- chrome/installer/util/additional_parameters.cc
+++ chrome/installer/util/additional_parameters.cc
@@ -136,7 +136,8 @@ ChannelParseState MakeChannelParseState(
     return {std::wstring(kExtendedChannel), channel_range, includes_arch};
   }
 
-  // Beta channel: /^1.1-.*$/ with an optional -arch_FOO.
+  // Beta channel: /^1.1-.*$/ with an optional -arch_FOO. Note that '.' is the
+  // regexp wildcard, not a literal period.
   if (ap.size() >= 4 && ap[0] == L'1' && ap[2] == L'1' && ap[3] == L'-') {
     base::WStringPiece channel_range =
         ap.substr(0, ap.find(kTokenSeparator, 4));
@@ -144,7 +145,8 @@ ChannelParseState MakeChannelParseState(
     return {L"beta", channel_range, includes_arch};
   }
 
-  // Dev channel: /^2.0-d.*$/ with an optional -arch_FOO.
+  // Dev channel: /^2.0-d.*$/ with an optional -arch_FOO. Note that '.' is the
+  // regexp wildcard, not a literal period.
   if (ap.size() >= 5 && ap[0] == L'2' && ap[2] == L'0' && ap[3] == L'-' &&
       ap[4] == L'd') {
     base::WStringPiece channel_range =
@@ -153,7 +155,7 @@ ChannelParseState MakeChannelParseState(
     return {L"dev", channel_range, includes_arch};
   }
 
-  // Middle ages channels.
+  // Older channels.
   static constexpr struct {
     base::WStringPiece literal;
     bool is_stable;  // if false, the channel name is embedded in `literal`.
