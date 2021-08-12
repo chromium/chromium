5302c49cff00d00d3b657fc89d9edc551eae9df5
Adam Langley <agl@chromium.org>
2021-08-12 00:38:04 +0000

    Disable CECPQ2 by default.
    (Merge to M92.)
    
    This moves from enabling CECPQ2 for domains starting with "a" to not
    enabling it at all. It can still be enabled for testing from
    chrome://flags.
    
    BUG=930812,1236802
    
    (cherry picked from commit 4d179ee619bdc8237e41492b291f04ba10f93d71)
    
    Change-Id: I3f7ef58da7797f1d025d80b977e3eba71dbd7f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076804
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#909447}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089618
    Commit-Queue: Adam Langley <agl@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4515@{#2034}
    Cr-Branched-From: 488fc70865ddaa05324ac00a54a6eb783b4bc41c-refs/heads/master@{#885287}

--- net/base/features.cc
+++ net/base/features.cc
@@ -123,7 +123,7 @@ const base::Feature kTLS13KeyUpdate{"TLS13KeyUpdate",
 const base::Feature kPostQuantumCECPQ2{"PostQuantumCECPQ2",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kPostQuantumCECPQ2SomeDomains{
-    "PostQuantumCECPQ2SomeDomains", base::FEATURE_ENABLED_BY_DEFAULT};
+    "PostQuantumCECPQ2SomeDomains", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::FeatureParam<std::string>
     kPostQuantumCECPQ2Prefix(&kPostQuantumCECPQ2SomeDomains, "prefix", "a");
 
