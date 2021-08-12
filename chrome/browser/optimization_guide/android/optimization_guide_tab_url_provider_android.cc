068e9b446b312848daecffc9e540fc13108b4223
Sophie Chang <sophiechang@chromium.org>
2021-08-12 16:50:06 +0000

    Fall back to TabAndroid URL if WebContents not attached to TabModel
    I mostly tested this manually - unfortunately, TabAndroid is un-testable
    via unit tests.
    
    Bug: 1239063
    Change-Id: I87fea1cedb05aff86ec38c090fe54f24fca88c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3090017
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911356}

--- chrome/browser/optimization_guide/android/optimization_guide_tab_url_provider_android.cc
+++ chrome/browser/optimization_guide/android/optimization_guide_tab_url_provider_android.cc
@@ -15,26 +15,59 @@ namespace android {
 
 OptimizationGuideTabUrlProviderAndroid::OptimizationGuideTabUrlProviderAndroid(
     Profile* profile)
-    : OptimizationGuideTabUrlProvider(profile) {}
+    : profile_(profile) {}
 OptimizationGuideTabUrlProviderAndroid::
     ~OptimizationGuideTabUrlProviderAndroid() = default;
 
-const std::vector<content::WebContents*>
-OptimizationGuideTabUrlProviderAndroid::GetAllWebContentsForProfile(
-    Profile* profile) {
-  std::vector<content::WebContents*> web_contents_list;
+const std::vector<GURL>
+OptimizationGuideTabUrlProviderAndroid::GetUrlsOfActiveTabs(
+    const base::TimeDelta& duration_since_last_shown) {
+  std::vector<std::pair<GURL, absl::optional<base::TimeTicks>>>
+      urls_and_active_time;
   for (const TabModel* tab_model : TabModelList::models()) {
-    if (tab_model->GetProfile() != profile)
+    if (tab_model->GetProfile() != profile_)
       continue;
 
     int tab_count = tab_model->GetTabCount();
     for (int i = 0; i < tab_count; i++) {
       content::WebContents* web_contents = tab_model->GetWebContentsAt(i);
-      if (web_contents)
-        web_contents_list.push_back(web_contents);
+      if (web_contents) {
+        if ((base::TimeTicks::Now() - web_contents->GetLastActiveTime()) <
+            duration_since_last_shown) {
+          urls_and_active_time.push_back(
+              std::make_pair(web_contents->GetLastCommittedURL(),
+                             web_contents->GetLastActiveTime()));
+        }
+        continue;
+      }
+
+      // Fall back to the tab if there isn't a WebContents created for the tab.
+      TabAndroid* tab = tab_model->GetTabAt(i);
+      if (tab) {
+        // Just push back the URL even though we have no idea if it was shown
+        // before. TabAndroid does not expose the last active time.
+        urls_and_active_time.push_back(
+            std::make_pair(tab->GetURL(), absl::nullopt));
+      }
     }
   }
-  return web_contents_list;
+  // Sort by descending active time.
+  std::sort(urls_and_active_time.begin(), urls_and_active_time.end(),
+            [](const std::pair<GURL, absl::optional<base::TimeTicks>>& a,
+               const std::pair<GURL, absl::optional<base::TimeTicks>>& b) {
+              if (a.second && b.second)
+                return *a.second > *b.second;
+              // If b.second has a value, then put that in front. Otherwise,
+              // leave the same order.
+              return !b.second.has_value();
+            });
+
+  std::vector<GURL> urls;
+  urls.reserve(urls_and_active_time.size());
+  for (const auto& url_and_active_time : urls_and_active_time) {
+    urls.emplace_back(url_and_active_time.first);
+  }
+  return urls;
 }
 
 }  // namespace android
