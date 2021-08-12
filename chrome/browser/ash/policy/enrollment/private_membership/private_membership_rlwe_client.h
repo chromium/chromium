1aa77917c22311e632f3d904735cd1f8d1e2ac02
amraboelkher <amraboelkher@chromium.org>
2021-08-12 22:52:17 +0000

    PSM: Add PSM RLWE client interface
    This interface is used to create two instances of it:
    1) One that will act as a delegate to communicate with the original
    third_party PSM RLWE client used in production code
    2) Another one will act as a delegate but will be created for testing
    purposes
    3) Last one, will be created to mock the RLWE client requests/responses
    
    That will be required in order not to test out the PSM RLWE client
    layer. For more information, see detailed design
    go/psm-integration-tests.
    
    This CL is doing the following:
    1) Adding the interface for RLWE client for all other classes
    2) Adding a folder for all private_membership related stuff
    3) Adding OWNERS file for that folder
    
    Bug: 1234777
    Change-Id: Iecb50a8a5931b94fb3bfedfd7da8a507492bc116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083823
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911533}

---
+++ chrome/browser/ash/policy/enrollment/private_membership/private_membership_rlwe_client.h
@@ -0,0 +1,64 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
+#define CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
+
+#include <memory>
+#include <vector>
+
+#include "third_party/private_membership/src/private_membership_rlwe.pb.h"
+#include "third_party/shell-encryption/src/statusor.h"
+
+namespace private_membership {
+namespace rlwe {
+class MembershipResponseMap;
+}  // namespace rlwe
+}  // namespace private_membership
+
+namespace policy {
+
+// Interface for the Private Membership RLWE Client, allowing to replace the
+// private membership RLWE client library with a fake for tests.
+class PrivateMembershipRlweClient {
+ public:
+  class Factory {
+   public:
+    virtual ~Factory() = default;
+    // Creates a client for the Private Membership RLWE protocol. It will be
+    // created for |plaintext_ids| with use case as |use_case|.
+    virtual ::rlwe::StatusOr<std::unique_ptr<PrivateMembershipRlweClient>>
+    Create(private_membership::rlwe::RlweUseCase use_case,
+           const std::vector<private_membership::rlwe::RlwePlaintextId>&
+               plaintext_ids) = 0;
+  };
+
+  virtual ~PrivateMembershipRlweClient() = default;
+
+  // Creates a request proto for the first phase of the protocol.
+  virtual ::rlwe::StatusOr<
+      private_membership::rlwe::PrivateMembershipRlweOprfRequest>
+  CreateOprfRequest() = 0;
+
+  // Creates a request proto for the second phase of the protocol.
+  virtual ::rlwe::StatusOr<
+      private_membership::rlwe::PrivateMembershipRlweQueryRequest>
+  CreateQueryRequest(
+      const private_membership::rlwe::PrivateMembershipRlweOprfResponse&
+          oprf_response) = 0;
+
+  // Processes the query response from the server and returns the membership
+  // response map.
+  //
+  // Keys of the returned map corresponds to the original plaintext ids supplied
+  // to the client when it was created.
+  virtual ::rlwe::StatusOr<private_membership::rlwe::MembershipResponseMap>
+  ProcessResponse(
+      const private_membership::rlwe::PrivateMembershipRlweQueryResponse&
+          query_response) = 0;
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
