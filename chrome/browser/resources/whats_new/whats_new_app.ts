277b4349f9d62ca5f3b8c17f06d711b3d0404f12
rbpotter <rbpotter@chromium.org>
2021-08-12 20:57:54 +0000

    What's New: Add a message listener for commands
    Bug: 1219381
    Change-Id: Ibfbc7972550b7cf2e867ac5284af390623399270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089690
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911479}

--- chrome/browser/resources/whats_new/whats_new_app.ts
+++ chrome/browser/resources/whats_new/whats_new_app.ts
@@ -6,11 +6,24 @@ import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import './whats_new_error_page.js';
 import './strings.m.js';
 
+import {EventTracker} from 'chrome://resources/js/event_tracker.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {html, microTask, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {ClickInfo, Command} from './promo_browser_command.mojom-webui.js';
 import {WhatsNewCommandProxy} from './whats_new_command_proxy.js';
 import {WhatsNewProxyImpl} from './whats_new_proxy.js';
+
+type CommandData = {
+  commandId: number,
+  clickInfo: ClickInfo,
+};
+
+// TODO (https://www.crbug.com/1219381): Add some additional parameters so
+// that we can filter the messages a bit better.
+type BrowserCommandMessageData = {
+  data: CommandData,
+};
 
 export class WhatsNewAppElement extends PolymerElement {
   static get is() {
@@ -26,6 +39,7 @@ export class WhatsNewAppElement extends PolymerElement {
 
   private showErrorPage_: boolean = false;
   private url_: string = '';
+  private eventTracker_: EventTracker = new EventTracker();
 
   connectedCallback() {
     super.connectedCallback();
@@ -39,6 +53,37 @@ export class WhatsNewAppElement extends PolymerElement {
       }
 
       this.url_ = isAutoOpen ? url.concat('?latest=true') : url;
+      this.eventTracker_.add(
+          window, 'message',
+          event => this.handleMessage_(event as MessageEvent));
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.eventTracker_.removeAll();
+  }
+
+  private handleMessage_(event: MessageEvent) {
+    const {data, origin} = event;
+    const iframeUrl = new URL(this.url_);
+    if (!data || origin !== iframeUrl.origin) {
+      return;
+    }
+
+    const commandData = (data as BrowserCommandMessageData).data;
+
+    const commandId = Object.values(Command).includes(commandData.commandId) ?
+        commandData.commandId :
+        Command.kUnknownCommand;
+
+    const handler = WhatsNewCommandProxy.getInstance().handler;
+    handler.canExecuteCommand(commandId).then(({canExecute}) => {
+      if (canExecute) {
+        handler.executeCommand(commandId, commandData.clickInfo);
+      } else {
+        console.warn('Received invalid command: ' + commandId);
+      }
     });
   }
 
