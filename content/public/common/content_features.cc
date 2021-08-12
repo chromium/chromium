15f400165860d2c0e320391b2a6f78d643b2d07e
Liquan (Max) Gu <maxlg@chromium.org>
2021-08-12 13:16:06 +0000

    [Web Payment] Enable the SPCv3 flag
    Enable the SPCv3 flag to enable the V3 features of
    Secure Payment Confirmation origin trial. These changes
    will be announced to blink-dev@ and communicated to origin
    trial partners.
    
    Bug: 1237430
    Change-Id: I82052c6160bf1511c34f994a98bfd7a8eba2b2dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3075485
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911282}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -651,7 +651,7 @@ const base::Feature kSecurePaymentConfirmation{
 // Used to enable API changes for Secure Payment Confirmation.
 // TODO(crbug.com/1228924): Enable by default in M94.
 const base::Feature kSecurePaymentConfirmationAPIV3{
-    "SecurePaymentConfirmationAPIV3", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SecurePaymentConfirmationAPIV3", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Used to control whether to remove the restriction that PaymentCredential in
 // WebAuthn and secure payment confirmation method in PaymentRequest API must
