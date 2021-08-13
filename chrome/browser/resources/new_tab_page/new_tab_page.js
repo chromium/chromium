0c43b4993f615e7c9c9a333ff70a85e8b9db460c
rbpotter <rbpotter@chromium.org>
2021-08-13 00:00:25 +0000

    Fix NTP Import
    Mistakenly moved into an // <if expr> block by git cl format by
    https://crrev.com/c/3084563.
    
    Also move to alphabetical ordering, so that this won't happen again next
    time someone runs git cl format on this file.
    
    Bug: 1219381
    Change-Id: I7682376ccba14cdffc88c6295e75e2f2098d7d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3092765
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911573}

--- chrome/browser/resources/new_tab_page/new_tab_page.js
+++ chrome/browser/resources/new_tab_page/new_tab_page.js
@@ -32,8 +32,8 @@ export {ModulesElement} from './modules/modules.js';
 // <if expr="not is_official_build">
 export {photosDescriptor} from './modules/photos/module.js';
 export {PhotosProxy} from './modules/photos/photos_module_proxy.js';
-export {recipeTasksDescriptor as recipeTasksV2Descriptor} from './modules/recipes_v2/module.js';
 // </if>
+export {recipeTasksDescriptor as recipeTasksV2Descriptor} from './modules/recipes_v2/module.js';
 export {recipeTasksDescriptor, shoppingTasksDescriptor} from './modules/task_module/module.js';
 export {TaskModuleHandlerProxy} from './modules/task_module/task_module_handler_proxy.js';
 export {NewTabPageProxy} from './new_tab_page_proxy.js';
