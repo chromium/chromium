1d1ec5b48c4e55c273d801aa82d57e9b1e24f239
Stephan Hartmann <stha09@googlemail.com>
2021-08-13 16:57:20 +0000

    IWYU: add memory for std::unique_ptr in blink::CustomSpaces
    Bug: None
    Change-Id: I415e68638d230d201f6cf72b109ad39da917a53b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094266
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#911787}

--- third_party/blink/renderer/platform/heap/v8_wrapper/custom_spaces.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/custom_spaces.h
@@ -5,6 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_V8_WRAPPER_CUSTOM_SPACES_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_V8_WRAPPER_CUSTOM_SPACES_H_
 
+#include <memory>
 #include <vector>
 
 #include "third_party/blink/renderer/platform/platform_export.h"
