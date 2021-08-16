873d5c0a53b472ace59a6c8fc5b6a17ba4a0d768
Daniel Andersson <dandersson@chromium.org>
2021-08-16 17:50:22 +0000

    Make use of InitParams::bounds when creating a new window.
    This fixes a bug with ash's feature to launch windows from a
    template. Without this change, the bounds may be present in InitParams
    are overridden by the logic in InitializeDefaultWindow.
    
    Bug: 1231195
    Change-Id: Ia9419204b56675c37ae7d8b31406f41154fa852d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3092713
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912243}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views.cc
@@ -130,12 +130,16 @@ void ChromeNativeAppWindowViews::InitializeDefaultWindow(
       create_params.visible_on_all_workspaces;
 
   OnBeforeWidgetInit(create_params, &init_params, widget());
+  gfx::Rect init_param_bounds = init_params.bounds;
   widget()->Init(std::move(init_params));
 
   // The frame insets are required to resolve the bounds specifications
   // correctly. So we set the window bounds and constraints now.
   gfx::Insets frame_insets = GetFrameInsets();
-  gfx::Rect window_bounds = create_params.GetInitialWindowBounds(frame_insets);
+  gfx::Rect window_bounds =
+      init_param_bounds.IsEmpty()
+          ? create_params.GetInitialWindowBounds(frame_insets)
+          : init_param_bounds;
   SetContentSizeConstraints(create_params.GetContentMinimumSize(frame_insets),
                             create_params.GetContentMaximumSize(frame_insets));
   if (!window_bounds.IsEmpty()) {
