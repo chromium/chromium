d2ca36d02ae0d38e5b5a12613861249408264bef
Alexei Svitkine <asvitkine@chromium.org>
2021-08-16 18:16:36 +0000

    Simplify logic in persistent histograms initialization.
    This simplifies the code and fixes some potential races:
      1. We don't need to check if the file exists before
         opening it, since we can ask the file object if it
         created it.
      2. We don't need to query the file size. Passing 0
         to FilePersistentMemoryAllocator ctor makes it use
         mmfile->length(), which does what we want already,
         either using file length or region size based on how
         it was initialized..
      3. Include build_config.h to satisfy a presubmit.
    
    Bug: 1183166
    Change-Id: I405807ad7e6ebc040730d3b8356a50020d8709d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093851
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912256}

--- base/metrics/persistent_histogram_allocator.cc
+++ base/metrics/persistent_histogram_allocator.cc
@@ -33,6 +33,7 @@
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/lock.h"
+#include "build/build_config.h"
 
 namespace base {
 
@@ -684,19 +685,17 @@ bool GlobalHistogramAllocator::CreateWithFile(
     size_t size,
     uint64_t id,
     StringPiece name) {
-  bool exists = PathExists(file_path);
   File file(
       file_path, File::FLAG_OPEN_ALWAYS | File::FLAG_SHARE_DELETE |
                  File::FLAG_READ | File::FLAG_WRITE);
 
   std::unique_ptr<MemoryMappedFile> mmfile(new MemoryMappedFile());
   bool success = false;
-  if (exists) {
-    size = saturated_cast<size_t>(file.GetLength());
-    success = mmfile->Initialize(std::move(file), MemoryMappedFile::READ_WRITE);
-  } else {
+  if (file.created()) {
     success = mmfile->Initialize(std::move(file), {0, size},
                                  MemoryMappedFile::READ_WRITE_EXTEND);
+  } else {
+    success = mmfile->Initialize(std::move(file), MemoryMappedFile::READ_WRITE);
   }
   if (!success ||
       !FilePersistentMemoryAllocator::IsFileAcceptable(*mmfile, true)) {
@@ -704,8 +703,8 @@ bool GlobalHistogramAllocator::CreateWithFile(
   }
 
   Set(WrapUnique(new GlobalHistogramAllocator(
-      std::make_unique<FilePersistentMemoryAllocator>(std::move(mmfile), size,
-                                                      id, name, false))));
+      std::make_unique<FilePersistentMemoryAllocator>(std::move(mmfile), 0, id,
+                                                      name, false))));
   Get()->SetPersistentLocation(file_path);
   return true;
 }
