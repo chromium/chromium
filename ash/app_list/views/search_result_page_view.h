d6345f713c282601b95564eccc59d66f3fdd7648
Yulun Wu <yulunwu@chromium.org>
2021-08-16 22:38:24 +0000

    Revert "Hide search result page view for zero state tablet mode."
    This reverts commit 61bacd0e845b6803d29b6a6d98e053b4216559da.
    
    Reason for revert: Breaking focus behavior in: crbug/1240123
    
    Original change's description:
    > Hide search result page view for zero state tablet mode.
    >
    > Zero state search should no longer be shown in tablet mode when the
    > kAppListBubble feature flag is turned on. Show/hide animation will be
    > implemented in a separate CL.
    >
    > Bug: 1216097
    > Change-Id: I8db07118a9fd5682294d65e8b356a1270381be29
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3052814
    > Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#907660}
    
    Bug: 1216097
    Change-Id: I284aca26c9c0954e4f4cda653103fec0dac6591b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097095
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912362}

--- ash/app_list/views/search_result_page_view.h
+++ ash/app_list/views/search_result_page_view.h
@@ -119,9 +119,6 @@ class ASH_EXPORT SearchResultPageView
   // Returns background color for the given state.
   SkColor GetBackgroundColorForState(AppListState state) const;
 
-  // Hide zero state search result view when AppListBubble is enabled.
-  bool ShouldShowSearchResultView() const;
-
   PrivacyContainerView* GetPrivacyContainerViewForTest();
   SearchResultTileItemListView* GetSearchResultTileItemListViewForTest();
   SearchResultListView* GetSearchResultListViewForTest();
