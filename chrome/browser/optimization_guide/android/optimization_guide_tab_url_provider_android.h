e503857612a79210a76a53cf8b9f4361ae188929
Sophie Chang <sophiechang@chromium.org>
2021-08-16 04:36:43 +0000

    Ensure sort in Android TabUrlProvider has strict ordering
    The unit test fails if the sort does not have a strict ordering so I'm
    confident this works
    
    (cherry picked from commit 58d50f0777ad51c1e4530f64a4e3b52f8cf0213a)
    
    Bug: 1239761
    Change-Id: I0a7e7fbf6134600115c847533a4bb746befff714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3095664
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#912059}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3096951
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Auto-Submit: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4609@{#3}
    Cr-Branched-From: 22f4a572daaf2891028a6104482dff6b6464c983-refs/heads/master@{#912034}

--- chrome/browser/optimization_guide/android/optimization_guide_tab_url_provider_android.h
+++ chrome/browser/optimization_guide/android/optimization_guide_tab_url_provider_android.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_OPTIMIZATION_GUIDE_ANDROID_OPTIMIZATION_GUIDE_TAB_URL_PROVIDER_ANDROID_H_
 
 #include "components/optimization_guide/core/tab_url_provider.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class Profile;
 
@@ -25,6 +26,31 @@ class OptimizationGuideTabUrlProviderAndroid
       const base::TimeDelta& duration_since_last_shown) override;
 
  private:
+  friend class OptimizationGuideTabUrlProviderAndroidTest;
+
+  struct TabRepresentation {
+    TabRepresentation();
+    ~TabRepresentation();
+    TabRepresentation(const TabRepresentation&);
+
+    // The URL displayed on the tab.
+    GURL url;
+
+    // The index of the tab model the tab is in.
+    size_t tab_model_index;
+    // The index of the tab within the tab model.
+    size_t tab_index;
+
+    // The time the tab was last active.
+    absl::optional<base::TimeTicks> last_active_time;
+  };
+
+  // Sorts |tabs|. Sorts by descending last active time (if present) and then by
+  // its position in the tab model.
+  //
+  // Exposed for testing purposes.
+  void SortTabs(std::vector<TabRepresentation>* tabs);
+
   // The profile associated with this tab URL provider.
   Profile* profile_;
 };
