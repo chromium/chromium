888e26ef6aad6de81a55304b0ea1c5110a719dab
Avi Drissman <avi@chromium.org>
2021-08-16 19:21:29 +0000

    Migrate uses of WebContentsObserver to WeakPtr in weblayer/
    Now that WebContents has weak pointer support, migrate uses of
    WebContentsObserver to use it.
    
    Bug: 952390
    Change-Id: I4378e053b2f6e9355ca66e6ee3e38cc3c5901347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094835
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912288}

--- weblayer/browser/file_select_helper.h
+++ weblayer/browser/file_select_helper.h
@@ -9,13 +9,15 @@
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/file_select_listener.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 
 namespace content {
 class FileSelectListener;
+class RenderFrameHost;
+class WebContents;
 }  // namespace content
 
 namespace ui {
@@ -34,8 +36,7 @@ namespace weblayer {
 class FileSelectHelper : public base::RefCountedThreadSafe<
                              FileSelectHelper,
                              content::BrowserThread::DeleteOnUIThread>,
-                         public ui::SelectFileDialog::Listener,
-                         public content::WebContentsObserver {
+                         public ui::SelectFileDialog::Listener {
  public:
   // Show the file chooser dialog.
   static void RunFileChooser(
@@ -80,9 +81,8 @@ class FileSelectHelper : public base::RefCountedThreadSafe<
   void ConvertToFileChooserFileInfoList(
       const std::vector<ui::SelectedFileInfo>& files);
 
-  // Calls RunFileChooserEnd() if the webcontents was destroyed. Returns true
-  // if the file chooser operation shouldn't proceed.
-  bool AbortIfWebContentsDestroyed();
+  // A weak pointer to the WebContents of the RenderFrameHost, for life checks.
+  base::WeakPtr<content::WebContents> web_contents_;
 
   // |listener_| receives the result of the FileSelectHelper.
   scoped_refptr<content::FileSelectListener> listener_;
