f48a03f0d16d53c735bd41d1fc2c089169b6ccb8
cammie <cammie@chromium.org>
2021-08-17 17:50:11 +0000

    Sheriff: Disable flaky TestBrokenHTTPSWithActiveInsecureContent Lacros.
    SSLUITest.TestBrokenHTTPSWithActiveInsecureContent was previously
    disabled on Mac and Linux due to flakes. This change also disables it
    on Lacros, where it has been consistently flaking as well.
    
    Bug: 1239347
    Change-Id: Ic00ec50c984986dd955b8e1fdba566345a033f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097071
    Reviewed-by: bttk <bttk@chromium.org>
    Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912639}

--- chrome/browser/ssl/ssl_browsertest.cc
+++ chrome/browser/ssl/ssl_browsertest.cc
@@ -1119,8 +1119,8 @@ IN_PROC_BROWSER_TEST_F(SSLUITest, TestBrokenHTTPSWithInsecureContent) {
 // Tests that the NavigationEntry gets marked as active mixed content,
 // even if there is a certificate error. Regression test for
 // https://crbug.com/593950.
-// TODO(crbug.com/1239347): Flaky on Mac and Linux bots.
-#if defined(OS_MAC) || defined(OS_LINUX)
+// TODO(crbug.com/1239347): Flaky on Mac, Linux, and Lacros bots.
+#if defined(OS_MAC) || defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
 #define MAYBE_TestBrokenHTTPSWithActiveInsecureContent \
   DISABLED_TestBrokenHTTPSWithActiveInsecureContent
 #else
