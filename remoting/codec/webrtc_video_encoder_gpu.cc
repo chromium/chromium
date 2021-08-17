19683f1b1652af75a802468d3dbeda4cd03137eb
Joe Downing <joedow@google.com>
2021-08-17 21:10:53 +0000

    Get H.264 encoder working with new video pipeline
    This is a simple CL to get the H.264 encoder working again with the new
    video pipeline. I haven't tested fallback or done any tuning, I would
    like to land it now for testing purposes to get some simple metrics
    before deciding whether to put more time into tuning it.
    
    Essentially this CL moves all of the VEA interactions onto a dedicated
    thread. The issue I am addressing is that with the new video pipeline,
    the WebrtcVideoEncoderGpu methods were being executed on a sequence
    but the VEA requires a dedicated thread.
    
    To simplify things, I use the threadpool to spin up a dedicated thread
    for the VEA and run all HW encoding operations on it.
    
    Bug: NO_BUG
    Change-Id: If8fc23eb4cc4d36fd967ec86cb91ca89d624e327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097094
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912740}

--- remoting/codec/webrtc_video_encoder_gpu.cc
+++ remoting/codec/webrtc_video_encoder_gpu.cc
@@ -8,19 +8,32 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "base/bind_post_task.h"
 #include "base/callback_helpers.h"
+#include "base/containers/flat_map.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/shared_memory_mapping.h"
 #include "base/memory/unsafe_shared_memory_region.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "base/threading/thread_checker.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_preferences.h"
 #include "media/base/video_frame.h"
 #include "media/gpu/gpu_video_encode_accelerator_factory.h"
+#include "media/video/video_encode_accelerator.h"
 #include "remoting/base/constants.h"
+#include "remoting/codec/encoder_bitrate_filter.h"
 #include "third_party/libyuv/include/libyuv/convert_from_argb.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
+
+#if defined(OS_WIN)
+#include "base/win/scoped_com_initializer.h"
+#endif
 
 namespace {
 
@@ -28,8 +41,8 @@ using media::VideoCodecProfile;
 using media::VideoFrame;
 using media::VideoPixelFormat;
 
-// Currently, the frame scheduler only encodes a single frame at a time. Thus,
-// there's no reason to have this set to anything greater than one.
+// Currently, the WebrtcVideoEncoderWrapper only encodes a single frame at a
+// time. Thus, there's no reason to have this set to anything greater than one.
 const int kWebrtcVideoEncoderGpuOutputBufferCount = 1;
 
 constexpr VideoCodecProfile kH264Profile = VideoCodecProfile::H264PROFILE_MAIN;
@@ -65,27 +78,152 @@ gpu::GpuDriverBugWorkarounds CreateGpuWorkarounds() {
   return gpu_workarounds;
 }
 
+struct OutputBuffer {
+  base::UnsafeSharedMemoryRegion region;
+  base::WritableSharedMemoryMapping mapping;
+
+  bool IsValid();
+};
+
+bool OutputBuffer::IsValid() {
+  return region.IsValid() && mapping.IsValid();
+}
+
 }  // namespace
 
 namespace remoting {
 
+// WebrtcVideoEncoderGpu::Core handles the initialization, usage, and teardown
+// of a VideoEncodeAccelerator object which is used to encode desktop frames for
+// presentation on the client.
+//
+// A brief explanation of how this class is initialized:
+// 1. An instance of WebrtcVideoEncoderGpu is created using the static
+//      CreateForH264() function. At this point its |core_| member (an instance
+//      of this class) is created with a state of UNINITIALIZED. After this
+//      point, WebrtcVideoEncoderGpu will forward all Encode calls to its
+//      |core_| member.
+// 2. On the first encode call, the incoming DesktopFrame's dimensions are
+//      stored and the Encode params are saved in |pending_encode_|. Before
+//      returning, BeginInitialization() is called.
+// 3. In BeginInitialization(), the Core instance constructs the
+//      VideoEncodeAccelerator using the saved dimensions from the DesktopFrame.
+//      If the VideoEncodeAccelerator is constructed successfully, the state is
+//      set to INITIALIZING. If not, the state isset to INIITALIZATION_ERROR.
+// 4. Some time later, the VideoEncodeAccelerator sets itself up and is ready
+//      to encode. At this point, it calls the Core instance's
+//      RequireBitstreamBuffers() method. Once bitstream buffers are allocated,
+//      the state is INITIALIZED.
+class WebrtcVideoEncoderGpu::Core
+    : public WebrtcVideoEncoder,
+      public media::VideoEncodeAccelerator::Client {
+ public:
+  explicit Core(media::VideoCodecProfile codec_profile);
+  Core(const Core&) = delete;
+  Core& operator=(const Core&) = delete;
+  ~Core() override;
+
+  // WebrtcVideoEncoder interface.
+  void Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
+              const FrameParams& params,
+              WebrtcVideoEncoder::EncodeCallback done) override;
+
+  // VideoEncodeAccelerator::Client interface.
+  void RequireBitstreamBuffers(unsigned int input_count,
+                               const gfx::Size& input_coded_size,
+                               size_t output_buffer_size) override;
+  void BitstreamBufferReady(
+      int32_t bitstream_buffer_id,
+      const media::BitstreamBufferMetadata& metadata) override;
+  void NotifyError(media::VideoEncodeAccelerator::Error error) override;
+
+ private:
+  enum State { UNINITIALIZED, INITIALIZING, INITIALIZED, INITIALIZATION_ERROR };
+
+  void BeginInitialization();
+
+  void UseOutputBitstreamBufferId(int32_t bitstream_buffer_id);
+
+  void RunAnyPendingEncode();
+
+#if defined(OS_WIN)
+  // This object is required by Chromium to ensure proper init/uninit of COM on
+  // this thread.  The guidance is to match the lifetime of this object to the
+  // lifetime of the thread if possible.
+  std::unique_ptr<base::win::ScopedCOMInitializer> scoped_com_initializer_;
+#endif
+
+  State state_ = UNINITIALIZED;
+
+  // Only after the first encode request do we know how large the incoming
+  // frames will be. Thus, we initialize after the first encode request,
+  // postponing the encode until the encoder has been initialized.
+  base::OnceClosure pending_encode_;
+
+  std::unique_ptr<media::VideoEncodeAccelerator> video_encode_accelerator_;
+
+  base::TimeDelta previous_timestamp_;
+
+  media::VideoCodecProfile codec_profile_;
+
+  // Shared memory with which the VEA transfers output to us.
+  std::vector<std::unique_ptr<OutputBuffer>> output_buffers_;
+
+  gfx::Size input_coded_size_;
+  gfx::Size input_visible_size_;
+
+  size_t output_buffer_size_;
+
+  base::flat_map<base::TimeDelta, WebrtcVideoEncoder::EncodeCallback>
+      callbacks_;
+
+  EncoderBitrateFilter bitrate_filter_{
+      kH264MinimumTargetBitrateKbpsPerMegapixel};
+
+  THREAD_CHECKER(thread_checker_);
+};
+
 WebrtcVideoEncoderGpu::WebrtcVideoEncoderGpu(VideoCodecProfile codec_profile)
-    : state_(UNINITIALIZED),
-      codec_profile_(codec_profile),
-      bitrate_filter_(kH264MinimumTargetBitrateKbpsPerMegapixel) {}
+    : core_(std::make_unique<WebrtcVideoEncoderGpu::Core>(codec_profile)),
+      hw_encode_task_runner_(base::ThreadPool::CreateSingleThreadTaskRunner(
+          {base::MayBlock(), base::WithBaseSyncPrimitives(),
+           base::TaskPriority::HIGHEST},
+          base::SingleThreadTaskRunnerThreadMode::DEDICATED)) {}
 
-WebrtcVideoEncoderGpu::~WebrtcVideoEncoderGpu() = default;
+WebrtcVideoEncoderGpu::~WebrtcVideoEncoderGpu() {
+  hw_encode_task_runner_->DeleteSoon(FROM_HERE, core_.release());
+}
 
-// TODO(gusss): Implement either a software fallback or some sort of delay if
-// the hardware encoder crashes.
-// Bug: crbug.com/751870
 void WebrtcVideoEncoderGpu::Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
                                    const FrameParams& params,
                                    WebrtcVideoEncoder::EncodeCallback done) {
+  DCHECK(core_);
   DCHECK(frame);
   DCHECK(done);
   DCHECK_GT(params.duration, base::TimeDelta::FromMilliseconds(0));
 
+  hw_encode_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&WebrtcVideoEncoderGpu::Core::Encode,
+                     base::Unretained(core_.get()), std::move(frame), params,
+                     base::BindPostTask(base::SequencedTaskRunnerHandle::Get(),
+                                        std::move(done))));
+}
+
+WebrtcVideoEncoderGpu::Core::Core(media::VideoCodecProfile codec_profile)
+    : codec_profile_(codec_profile) {
+  DETACH_FROM_THREAD(thread_checker_);
+}
+
+WebrtcVideoEncoderGpu::Core::~Core() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+}
+
+void WebrtcVideoEncoderGpu::Core::Encode(
+    std::unique_ptr<webrtc::DesktopFrame> frame,
+    const FrameParams& params,
+    WebrtcVideoEncoder::EncodeCallback done) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   bitrate_filter_.SetFrameSize(frame->size().width(), frame->size().height());
 
   if (state_ == INITIALIZATION_ERROR) {
@@ -100,22 +238,15 @@ void WebrtcVideoEncoderGpu::Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
     return;
   }
 
-  DVLOG(3) << __func__ << " bitrate = " << params.bitrate_kbps << ", "
-           << "duration = " << params.duration << ", "
-           << "key_frame = " << params.key_frame;
-
   if (state_ == UNINITIALIZED ||
       input_visible_size_.width() != frame->size().width() ||
       input_visible_size_.height() != frame->size().height()) {
-    DVLOG(3) << __func__ << " Currently not initialized for frame size "
-             << frame->size().width() << "x" << frame->size().height()
-             << ". Initializing.";
     input_visible_size_ =
         gfx::Size(frame->size().width(), frame->size().height());
 
-    pending_encode_ = base::BindOnce(&WebrtcVideoEncoderGpu::Encode,
-                                     weak_factory_.GetWeakPtr(),
-                                     std::move(frame), params, std::move(done));
+    pending_encode_ = base::BindOnce(&WebrtcVideoEncoderGpu::Core::Encode,
+                                     base::Unretained(this), std::move(frame),
+                                     params, std::move(done));
 
     BeginInitialization();
 
@@ -124,8 +255,8 @@ void WebrtcVideoEncoderGpu::Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
 
   // If we get to this point and state_ != INITIALIZED, we may be attempting to
   // have multiple outstanding encode requests, which is not currently
-  // supported. The current assumption is that the FrameScheduler will wait for
-  // an Encode to finish before attempting another.
+  // supported. The current assumption is that the WebrtcVideoEncoderWrapper
+  // will wait for an Encode to finish before attempting another.
   DCHECK_EQ(state_, INITIALIZED);
 
   scoped_refptr<VideoFrame> video_frame = VideoFrame::CreateFrame(
@@ -152,25 +283,19 @@ void WebrtcVideoEncoderGpu::Encode(std::unique_ptr<webrtc::DesktopFrame> frame,
   video_encode_accelerator_->Encode(video_frame, params.key_frame);
 }
 
-void WebrtcVideoEncoderGpu::RequireBitstreamBuffers(
+void WebrtcVideoEncoderGpu::Core::RequireBitstreamBuffers(
     unsigned int input_count,
     const gfx::Size& input_coded_size,
     size_t output_buffer_size) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(state_ == INITIALIZING);
 
-  DVLOG(3) << __func__ << ", "
-           << "input_count = " << input_count << ", "
-           << "input_coded_size = " << input_coded_size.width() << "x"
-           << input_coded_size.height() << ", "
-           << "output_buffer_size = " << output_buffer_size;
-
-  required_input_frame_count_ = input_count;
   input_coded_size_ = input_coded_size;
   output_buffer_size_ = output_buffer_size;
 
   output_buffers_.clear();
 
-  for (unsigned int i = 0; i < kWebrtcVideoEncoderGpuOutputBufferCount; ++i) {
+  for (unsigned int i = 0; i < kWebrtcVideoEncoderGpuOutputBufferCount; i++) {
     auto output_buffer = std::make_unique<OutputBuffer>();
     output_buffer->region =
         base::UnsafeSharedMemoryRegion::Create(output_buffer_size_);
@@ -180,7 +305,7 @@ void WebrtcVideoEncoderGpu::RequireBitstreamBuffers(
     output_buffers_.push_back(std::move(output_buffer));
   }
 
-  for (size_t i = 0; i < output_buffers_.size(); ++i) {
+  for (size_t i = 0; i < output_buffers_.size(); i++) {
     UseOutputBitstreamBufferId(i);
   }
 
@@ -188,14 +313,10 @@ void WebrtcVideoEncoderGpu::RequireBitstreamBuffers(
   RunAnyPendingEncode();
 }
 
-void WebrtcVideoEncoderGpu::BitstreamBufferReady(
+void WebrtcVideoEncoderGpu::Core::BitstreamBufferReady(
     int32_t bitstream_buffer_id,
     const media::BitstreamBufferMetadata& metadata) {
-  DVLOG(3) << __func__ << " bitstream_buffer_id = " << bitstream_buffer_id
-           << ", "
-           << "payload_size = " << metadata.payload_size_bytes << ", "
-           << "key_frame = " << metadata.key_frame << ", "
-           << "timestamp ms = " << metadata.timestamp.InMilliseconds();
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   std::unique_ptr<EncodedFrame> encoded_frame =
       std::make_unique<EncodedFrame>();
@@ -220,17 +341,21 @@ void WebrtcVideoEncoderGpu::BitstreamBufferReady(
   callbacks_.erase(metadata.timestamp);
 }
 
-void WebrtcVideoEncoderGpu::NotifyError(
+void WebrtcVideoEncoderGpu::Core::NotifyError(
     media::VideoEncodeAccelerator::Error error) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   LOG(ERROR) << __func__ << " error: " << error;
 }
 
-bool WebrtcVideoEncoderGpu::OutputBuffer::IsValid() {
-  return region.IsValid() && mapping.IsValid();
-}
+void WebrtcVideoEncoderGpu::Core::BeginInitialization() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-void WebrtcVideoEncoderGpu::BeginInitialization() {
-  DVLOG(3) << __func__;
+#if defined(OS_WIN)
+  if (!scoped_com_initializer_) {
+    scoped_com_initializer_ =
+        std::make_unique<base::win::ScopedCOMInitializer>();
+  }
+#endif
 
   VideoPixelFormat input_format = VideoPixelFormat::PIXEL_FORMAT_I420;
   // TODO(zijiehe): implement some logical way to set an initial bitrate.
@@ -255,24 +380,24 @@ void WebrtcVideoEncoderGpu::BeginInitialization() {
   state_ = INITIALIZING;
 }
 
-void WebrtcVideoEncoderGpu::UseOutputBitstreamBufferId(
+void WebrtcVideoEncoderGpu::Core::UseOutputBitstreamBufferId(
     int32_t bitstream_buffer_id) {
-  DVLOG(3) << __func__ << " id=" << bitstream_buffer_id;
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   video_encode_accelerator_->UseOutputBitstreamBuffer(media::BitstreamBuffer(
       bitstream_buffer_id,
       output_buffers_[bitstream_buffer_id]->region.Duplicate(),
       output_buffers_[bitstream_buffer_id]->region.GetSize()));
 }
 
-void WebrtcVideoEncoderGpu::RunAnyPendingEncode() {
-  if (pending_encode_)
+void WebrtcVideoEncoderGpu::Core::RunAnyPendingEncode() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (pending_encode_) {
     std::move(pending_encode_).Run();
+  }
 }
 
 // static
 std::unique_ptr<WebrtcVideoEncoder> WebrtcVideoEncoderGpu::CreateForH264() {
-  DVLOG(3) << __func__;
-
   LOG(WARNING) << "H264 video encoder is created.";
   // HIGH profile requires Windows 8 or upper. Considering encoding latency,
   // frame size and image quality, MAIN should be fine for us.
@@ -288,8 +413,6 @@ bool WebrtcVideoEncoderGpu::IsSupportedByH264(
   // lifetime of the thread if possible.  Since we are still experimenting with
   // H.264 and run the encoder on a different thread, we use a locally scoped
   // object for now.
-  // TODO(joedow): Use a COMscoped Autothread (or run in a separate process) if
-  // H.264 becomes a common use case for us.
   base::win::ScopedCOMInitializer scoped_com_initializer;
 #endif
 
