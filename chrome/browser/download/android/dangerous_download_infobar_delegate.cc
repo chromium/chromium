9d9d661215e49290b347ce77b948413b04cb7954
Min Qin <qinmin@chromium.org>
2021-08-17 20:45:29 +0000

    Add metrics for DangerousDownloadInfobarDelegate
    So we can compare the metrics with DangerousDownloadDialog
    
    BUG=1240699
    
    Change-Id: Idb0cf7fdbbe78d41c465eb5aad687f403292d81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100548
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912727}

--- chrome/browser/download/android/dangerous_download_infobar_delegate.cc
+++ chrome/browser/download/android/dangerous_download_infobar_delegate.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/android/android_theme_resources.h"
 #include "chrome/grit/generated_resources.h"
@@ -14,6 +15,28 @@
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar.h"
 #include "ui/base/l10n/l10n_util.h"
+
+namespace {
+// Records user interactions with the dangerous download infobar.
+// Used in UMA, do not remove, change or reuse existing entries.
+// Update histograms.xml and enums.xml when adding entries.
+enum class DangerousDownloadInfobarEvent {
+  // Infobar was shown.
+  kShown = 0,
+  // Accepted the dangerous download.
+  kAccepted = 1,
+  // Canceled the dangerous download.
+  kCanceled = 2,
+  // Dismissed the dangerous download.
+  kDismissed = 3,
+  kMaxValue = kDismissed
+};
+
+void RecordDangerousDownloadInfobarEvent(DangerousDownloadInfobarEvent event) {
+  base::UmaHistogramEnumeration("Download.Mobile.DangerousDownloadInfobarEvent",
+                                event);
+}
+}  // namespace
 
 // static
 void DangerousDownloadInfoBarDelegate::Create(
@@ -30,6 +53,7 @@ DangerousDownloadInfoBarDelegate::DangerousDownloadInfoBarDelegate(
   message_text_ = l10n_util::GetStringFUTF16(
       IDS_PROMPT_DANGEROUS_DOWNLOAD,
       base::UTF8ToUTF16(download_item_->GetFileNameToReportUser().value()));
+  RecordDangerousDownloadInfobarEvent(DangerousDownloadInfobarEvent::kShown);
 }
 
 DangerousDownloadInfoBarDelegate::~DangerousDownloadInfoBarDelegate() {
@@ -60,6 +84,8 @@ bool DangerousDownloadInfoBarDelegate::ShouldExpire(
 void DangerousDownloadInfoBarDelegate::InfoBarDismissed() {
   if (download_item_)
     download_item_->Remove();
+  RecordDangerousDownloadInfobarEvent(
+      DangerousDownloadInfobarEvent::kDismissed);
 }
 
 std::u16string DangerousDownloadInfoBarDelegate::GetMessageText() const {
@@ -69,11 +95,13 @@ std::u16string DangerousDownloadInfoBarDelegate::GetMessageText() const {
 bool DangerousDownloadInfoBarDelegate::Accept() {
   if (download_item_)
     download_item_->ValidateDangerousDownload();
+  RecordDangerousDownloadInfobarEvent(DangerousDownloadInfobarEvent::kAccepted);
   return true;
 }
 
 bool DangerousDownloadInfoBarDelegate::Cancel() {
   if (download_item_)
     download_item_->Remove();
+  RecordDangerousDownloadInfobarEvent(DangerousDownloadInfobarEvent::kCanceled);
   return true;
 }
