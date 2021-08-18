8453d96730832f6bb8e81bc8f34a3b20217e3ee2
Haiyang Pan <hypan@google.com>
2021-08-18 06:50:12 +0000

    M94: Revert "[PA] Improve and enable Lazy Commit for all platforms"
    This reverts commit eeaf875b34d7cd5ef50a5d698cbeb6581addb9f0.
    
    Reason for revert: Caused the test failure of ProcessReaderLinux.SelfModules in crashpad_tests, starting from https://ci.chromium.org/ui/p/chromium/builders/ci/android-lollipop-arm-rel/11165/overview.
    Also fails the test DebugRendezvous.Self in crashpad_tests on trybot and block a couple of CLs from landing. Example build: https://ci.chromium.org/ui/p/chromium/builders/try/android-lollipop-arm-rel/390699/overview
    
    Bisected with the following command:
    ./tools/mb/mb.py run -s --no-default-dimensions -d pool chromium.tests -d device_type hammerhead -d device_os LMY48M -j 500 out/Lollipop crashpad_tests
    
    Original change's description:
    > [PA] Improve and enable Lazy Commit for all platforms
    >
    > crrev.com/c/2569143 disabled Lazy Commit on non-Windows platforms due to
    > perf regressions. These happened, because the original implementation
    > would call expensive mprotect() even when not needed. This
    > implementation tracks how many pages of a slot span have been previously
    > committed, thus already having the right permissions, and doesn't call
    > mprotect() for those.
    >
    > One might wonder why we need Lazy Commit for non-Windows platform, which
    > don't incur costs of early commit. There are other benefits:
    > 1. Less platform-specific code.
    > 2. Less codepaths dealing with committing (once the reverse experiment
    >    paths are removed).
    > 3. Easier to implement returning nullptr on failure, without crashing.
    > 4. Pages in a slot span are invalid just a little longer, helping to
    >    catch overflows.
    >
    > This may come at a performance cost, mostly due to increased number
    > of calls to mprotect() under the lock. We are planning to observe perf
    > closely on benchmarks and via Finch.
    >
    > Change-Id: I9ad094d0ac601a2ca2daefa4e5f1be384fe75cd7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725896
    > Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#911074}
    
    (cherry picked from commit e4f0c42a5c076e2120574567d6ee9e791dcc9328)
    
    Bug: 1239342
    Change-Id: Ia213dad0d46f582f7eeee674d47663d93dfa0d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093002
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#911832}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101853
    Auto-Submit: Haiyang Pan <hypan@google.com>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4606@{#77}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

--- base/allocator/partition_allocator/partition_page.cc
+++ base/allocator/partition_allocator/partition_page.cc
@@ -133,10 +133,7 @@ SlotSpanMetadata<thread_safe>::get_sentinel_slot_span() {
 template <bool thread_safe>
 SlotSpanMetadata<thread_safe>::SlotSpanMetadata(
     PartitionBucket<thread_safe>* bucket)
-    : bucket(bucket),
-      can_store_raw_size(bucket->CanStoreRawSize()),
-      num_previously_committed_system_pages(0),
-      unused(0) {}
+    : bucket(bucket), can_store_raw_size(bucket->CanStoreRawSize()) {}
 
 template <bool thread_safe>
 void SlotSpanMetadata<thread_safe>::FreeSlowPath() {
