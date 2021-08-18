7443fb556c43cea7227bf14b8f54974fb3b90718
Jimmy Gong <jimmyxgong@google.com>
2021-08-18 00:47:54 +0000

    shortcuts: Add page-toolbar to main page
    The page-toolbar component currently only shows a title.
    
    screenshot: http://shortn/_21wOSMYha1
    
    Bug: 1060690
    Change-Id: Ib9870d91a2071aa03bf3dac88b485d20a5ac4c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089798
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912829}

--- ash/webui/shortcut_customization_ui/resources/shortcut_customization_app.js
+++ ash/webui/shortcut_customization_ui/resources/shortcut_customization_app.js
@@ -7,6 +7,7 @@ import './shortcut_input.js';
 import './shortcuts_page.js'
 import './shortcut_customization_fonts_css.js'
 import 'chrome://resources/ash/common/navigation_view_panel.js';
+import 'chrome://resources/ash/common/page_toolbar.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
