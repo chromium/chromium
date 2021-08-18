4389f61eb991378ca736450949c22438840ff5c5
Rakina Zata Amni <rakina@chromium.org>
2021-08-18 13:55:50 +0000

    Disable BFCache for AdTaggingBrowserTest.VerifySameOriginWithoutNavigate
    This test is flaky when BFCache is enabled because of a known bug with
    histogram recording if a cross-RFH navigation happened too fast.
    (See crbug.com/1229122).
    
    Bug: 1240525
    Change-Id: I15f73bf2599e917ff437eb38b140113951187261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099549
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912975}

--- chrome/browser/subresource_filter/ad_tagging_browsertest.cc
+++ chrome/browser/subresource_filter/ad_tagging_browsertest.cc
@@ -26,6 +26,7 @@
 #include "components/subresource_filter/core/common/test_ruleset_utils.h"
 #include "components/ukm/content/source_url_recorder.h"
 #include "components/ukm/test_ukm_recorder.h"
+#include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -394,6 +395,14 @@ const char kSubresourceFilterOriginStatusHistogram[] =
 const char kWindowOpenFromAdStateHistogram[] = "Blink.WindowOpen.FromAdState";
 
 IN_PROC_BROWSER_TEST_F(AdTaggingBrowserTest, VerifySameOriginWithoutNavigate) {
+  // The test assumes pages gets deleted after navigation, triggering histogram
+  // recording. Disable back/forward cache to ensure that pages don't get
+  // preserved in the cache.
+  // TODO(https://crbug.com/1229122): Investigate if this needs further fix.
+  content::DisableBackForwardCacheForTesting(
+      browser()->tab_strip_model()->GetActiveWebContents(),
+      content::BackForwardCache::TEST_ASSUMES_NO_CACHING);
+
   base::HistogramTester histogram_tester;
 
   // Main frame.
