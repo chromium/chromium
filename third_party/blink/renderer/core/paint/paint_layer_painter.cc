eb79a28b164a19704c7824d3801e02e61e5ac38c
Philip Rogers <pdr@chromium.org>
2021-08-19 00:24:43 +0000

    [CompositeAfterPaint] Paint content under animating non-invertible xform
    Before this patch, painting of content under non-invertible transforms
    was skipped in two places. This is problematic because a composited
    transform animation may become invertible later, but the compositor will
    not have content to show. This patch causes this content to paint.
    
    Another approach would be to not composite these animations, but the
    composited animation system already needs to handle non-invertible
    transforms (e.g., when animating scale(1) to scale(-1)) so these are
    compositable.
    
    (cherry picked from commit d30668754e28963f133f4b00238e05f783f953c2)
    
    Bug: 1227011
    Change-Id: Idd41abef0919404e40473117064e6ffe221511ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093707
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#911926}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105151
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4606@{#118}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

--- third_party/blink/renderer/core/paint/paint_layer_painter.cc
+++ third_party/blink/renderer/core/paint/paint_layer_painter.cc
@@ -98,8 +98,20 @@ PaintResult PaintLayerPainter::Paint(
     return kFullyPainted;
   }
 
-  // If the transform can't be inverted, then don't paint anything.
-  if (paint_layer_.PaintsWithTransform(painting_info.GetGlobalPaintFlags()) &&
+  // If the transform can't be inverted, don't paint anything. We still need
+  // to paint with CompositeAfterPaint if there are animations to ensure the
+  // animation can be setup to run on the compositor.
+  bool paint_non_invertible_transforms = false;
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled()) {
+    const auto* properties =
+        paint_layer_.GetLayoutObject().FirstFragment().PaintProperties();
+    if (properties && properties->Transform() &&
+        properties->Transform()->HasActiveTransformAnimation()) {
+      paint_non_invertible_transforms = true;
+    }
+  }
+  if (!paint_non_invertible_transforms &&
+      paint_layer_.PaintsWithTransform(painting_info.GetGlobalPaintFlags()) &&
       !paint_layer_.RenderableTransform(painting_info.GetGlobalPaintFlags())
            .IsInvertible()) {
     return kFullyPainted;
@@ -225,6 +237,14 @@ bool PaintLayerPainter::ShouldUseInfiniteCullRectInternal(
         // details.
         if (!transform->IsIdentityOr2DTranslation() &&
             transform->Matrix().HasPerspective()) {
+          return true;
+        }
+
+        // Ensure content under animating transforms is not culled out, even if
+        // the initial matrix is non-invertible.
+        if (transform->HasActiveTransformAnimation() &&
+            !transform->IsIdentityOr2DTranslation() &&
+            !transform->Matrix().IsInvertible()) {
           return true;
         }
 
