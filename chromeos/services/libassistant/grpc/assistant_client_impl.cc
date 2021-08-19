f863c828401822d5b47f1863c1ab3293062d4325
Roman Sorokin [CET] <rsorokin@chromium.org>
2021-08-19 12:10:43 +0000

    Revert "[LibassistantV2] Wire up V2 entry point |AssistantClientImpl|."
    This reverts commit 6679ebc53f634576ad2a36d84690ad76e3adc2ce.
    
    Reason for revert: suspecting as a culprit for chromeos-betty-pi-arc-cfi-thin-lto-chrome failures (https://crbug.com/1241330)
    
    Original change's description:
    > [LibassistantV2] Wire up V2 entry point |AssistantClientImpl|.
    >
    > - Wires up |AssistantClientImpl| with |ServiceController|, which manages
    >   the lifecycle of Libassistant.
    > - Guards the entry point behind a chromeos feature flag "LibAssistantV2"
    >   and we can switch to V2 with that feature flag enabled.
    >
    > Change-Id: I021cbab34249004faef70408be7a0f751ba88694
    > Bug: b/186687608
    > Test: run chromeos_unittests --gtest-filter=Assistant*
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954188
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Commit-Queue: Meilin Wang <meilinw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#913204}
    
    Bug: b/186687608
    Bug: 1241330
    Change-Id: Ic377fced30dac337800f7ad67e5efc4753bd5a5b
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106645
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913364}

--- chromeos/services/libassistant/grpc/assistant_client_impl.cc
+++ chromeos/services/libassistant/grpc/assistant_client_impl.cc
@@ -4,13 +4,10 @@
 
 #include "chromeos/services/libassistant/grpc/assistant_client_impl.h"
 
-#include <memory>
-
 #include "base/check.h"
 #include "base/containers/flat_set.h"
 #include "base/notreached.h"
 #include "base/threading/sequenced_task_runner_handle.h"
-#include "chromeos/assistant/internal/internal_constants.h"
 #include "chromeos/services/assistant/public/cpp/features.h"
 #include "chromeos/services/libassistant/grpc/assistant_client_v1.h"
 #include "chromeos/services/libassistant/grpc/grpc_libassistant_client.h"
@@ -24,20 +21,13 @@ AssistantClientImpl::AssistantClientImpl(
     assistant_client::AssistantManagerInternal* assistant_manager_internal,
     const std::string& libassistant_service_address,
     const std::string& assistant_service_address)
-    : AssistantClientV1(std::move(assistant_manager),
-                        assistant_manager_internal),
+    : AssistantClient(std::move(assistant_manager), assistant_manager_internal),
       grpc_services_(libassistant_service_address, assistant_service_address),
       client_(grpc_services_.GrpcLibassistantClient()),
       task_runner_(base::SequencedTaskRunnerHandle::Get()) {}
 
 AssistantClientImpl::~AssistantClientImpl() {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
-}
-
-void AssistantClientImpl::StartServices() {
-  StartGrpcServices();
-
-  AssistantClientV1::StartServices();
 }
 
 bool AssistantClientImpl::StartGrpcServices() {
@@ -47,26 +37,13 @@ bool AssistantClientImpl::StartGrpcServices() {
 void AssistantClientImpl::AddExperimentIds(
     const std::vector<std::string>& exp_ids) {}
 
-void AssistantClientImpl::SendVoicelessInteraction(
-    const ::assistant::api::Interaction& interaction,
-    const std::string& description,
-    const ::assistant::api::VoicelessOptions& options,
-    base::OnceCallback<void(bool)> on_done) {
-  NOTIMPLEMENTED();
-}
-
 // static
 std::unique_ptr<AssistantClient> AssistantClient::Create(
     std::unique_ptr<assistant_client::AssistantManager> assistant_manager,
     assistant_client::AssistantManagerInternal* assistant_manager_internal) {
   if (chromeos::assistant::features::IsLibAssistantV2Enabled()) {
-    // Note that we should *not* depend on |assistant_manager_internal| for V2,
-    // so |assistant_manager_internal| will be nullptr after the migration has
-    // done.
-    return std::make_unique<AssistantClientImpl>(
-        std::move(assistant_manager), assistant_manager_internal,
-        assistant::kLibassistantServiceAddress,
-        assistant::kAssistantServiceAddress);
+    // not supported yet
+    return nullptr;
   }
 
   return std::make_unique<AssistantClientV1>(std::move(assistant_manager),
