245a1ea03b698c589ba7b3f991f5394955e2f3be
amraboelkher <amraboelkher@chromium.org>
2021-08-20 11:05:53 +0000

    [M94] PSM: Add fake implementation to PSM RLWE client
    This mock implementation will be used for integration tests. For
    more detailed design, see go/psm-integration-tests.
    
    This CL is adding the fake implementation for PSM RLWE client.
    The usage for this client for testing purposes, will be in a later
    CL.
    
    (cherry picked from commit e7ca28b675db70aa9971b92928ce8bccb4b6701b)
    
    Bug: 1234777
    Change-Id: Iac06d3403e7f495b81177a58ffd0164d8b03e52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088855
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#911629}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3109827
    Auto-Submit: Amr Aboelkher <amraboelkher@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4606@{#163}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

---
+++ chrome/browser/ash/policy/enrollment/private_membership/fake_private_membership_rlwe_client.h
@@ -0,0 +1,76 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_FAKE_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
+#define CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_FAKE_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
+
+#include "chrome/browser/ash/policy/enrollment/private_membership/private_membership_rlwe_client.h"
+
+#include <string>
+#include <vector>
+
+#include "third_party/private_membership/src/private_membership_rlwe.pb.h"
+#include "third_party/shell-encryption/src/statusor.h"
+
+namespace private_membership {
+namespace rlwe {
+class MembershipResponseMap;
+}  // namespace rlwe
+}  // namespace private_membership
+
+namespace policy {
+
+class FakePrivateMembershipRlweClient : public PrivateMembershipRlweClient {
+ public:
+  // A factory that creates |FakePrivateMembershipRlweClient|s.
+  class FactoryImpl : public Factory {
+   public:
+    FactoryImpl() = default;
+
+    // FactoryImpl is neither copyable nor copy assignable.
+    FactoryImpl(const FactoryImpl&) = delete;
+    FactoryImpl& operator=(const FactoryImpl&) = delete;
+
+    ~FactoryImpl() override = default;
+
+    // Creates a fake PSM RLWE client for testing purposes.
+    ::rlwe::StatusOr<std::unique_ptr<PrivateMembershipRlweClient>> Create(
+        private_membership::rlwe::RlweUseCase use_case,
+        const std::vector<private_membership::rlwe::RlwePlaintextId>&
+            plaintext_ids) override;
+  };
+
+  // FakePrivateMembershipRlweClient is neither copyable nor copy assignable.
+  FakePrivateMembershipRlweClient(const FakePrivateMembershipRlweClient&) =
+      delete;
+  FakePrivateMembershipRlweClient& operator=(
+      const FakePrivateMembershipRlweClient&) = delete;
+
+  ~FakePrivateMembershipRlweClient() override;
+
+  // Mocks all function calls of PrivateMembershipRlweClient.
+
+  ::rlwe::StatusOr<private_membership::rlwe::PrivateMembershipRlweOprfRequest>
+  CreateOprfRequest() override;
+  ::rlwe::StatusOr<private_membership::rlwe::PrivateMembershipRlweQueryRequest>
+  CreateQueryRequest(
+      const private_membership::rlwe::PrivateMembershipRlweOprfResponse&
+          oprf_response) override;
+  ::rlwe::StatusOr<private_membership::rlwe::MembershipResponseMap>
+  ProcessResponse(
+      const private_membership::rlwe::PrivateMembershipRlweQueryResponse&
+          query_response) override;
+
+ private:
+  FakePrivateMembershipRlweClient(
+      private_membership::rlwe::RlweUseCase use_case,
+      std::vector<private_membership::rlwe::RlwePlaintextId> plaintext_ids);
+
+  const private_membership::rlwe::RlweUseCase use_case_;
+  const std::vector<private_membership::rlwe::RlwePlaintextId> plaintext_ids_;
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_ASH_POLICY_ENROLLMENT_PRIVATE_MEMBERSHIP_FAKE_PRIVATE_MEMBERSHIP_RLWE_CLIENT_H_
