53a7766a3ac6305bd18a2d254878982720365777
Zihan Chen <zihanchen@google.com>
2021-08-20 23:37:10 +0000

    improve Xbox Elite Wireless Controller Bluetooth support for mac
    - add support for a previously unsupported firmware version
    
    TESTED=all buttons and vibration works correctly
    
    Bug: 1009726
    Change-Id: I339a35b33e01e4354209146b494532203eed28cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062582
    Auto-Submit: Zihan Chen <zihanchen@google.com>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914044}

--- device/gamepad/gamepad_standard_mappings_mac.mm
+++ device/gamepad/gamepad_standard_mappings_mac.mm
@@ -604,6 +604,40 @@ void MapperXboxOneBluetooth(const Gamepad& input, Gamepad* mapped) {
   mapped->axes_length = AXIS_INDEX_COUNT;
 }
 
+void MapperXboxElite2Bluetooth(const Gamepad& input, Gamepad* mapped) {
+  *mapped = input;
+
+  mapped->buttons[BUTTON_INDEX_PRIMARY] = input.buttons[0];
+  mapped->buttons[BUTTON_INDEX_SECONDARY] = input.buttons[1];
+  mapped->buttons[BUTTON_INDEX_TERTIARY] = input.buttons[3];
+  mapped->buttons[BUTTON_INDEX_QUATERNARY] = input.buttons[4];
+  mapped->buttons[BUTTON_INDEX_LEFT_SHOULDER] = input.buttons[6];
+  mapped->buttons[BUTTON_INDEX_RIGHT_SHOULDER] = input.buttons[7];
+  // Firmware updates have changed the mappings in this controller. The newer
+  // firmware has an axes length of 10 and some new mappings. Since this seems
+  // to be the only way to tell which firmware version we have we are splitting
+  // the mapping based on the axes_length.
+  if (mapped->axes_length > 10) {
+    mapped->buttons[BUTTON_INDEX_LEFT_TRIGGER] = AxisToButton(input.axes[10]);
+    mapped->buttons[BUTTON_INDEX_RIGHT_TRIGGER] = AxisToButton(input.axes[11]);
+    mapped->buttons[BUTTON_INDEX_BACK_SELECT] = input.buttons[31];
+    mapped->buttons[BUTTON_INDEX_META] = input.buttons[30];
+  } else {
+    mapped->buttons[BUTTON_INDEX_LEFT_TRIGGER] = AxisToButton(input.axes[3]);
+    mapped->buttons[BUTTON_INDEX_RIGHT_TRIGGER] = AxisToButton(input.axes[4]);
+    mapped->buttons[BUTTON_INDEX_BACK_SELECT] = input.buttons[16];
+    mapped->buttons[BUTTON_INDEX_META] = input.buttons[12];
+  }
+  mapped->buttons[BUTTON_INDEX_START] = input.buttons[11];
+  mapped->buttons[BUTTON_INDEX_LEFT_THUMBSTICK] = input.buttons[13];
+  mapped->buttons[BUTTON_INDEX_RIGHT_THUMBSTICK] = input.buttons[14];
+  DpadFromAxis(mapped, input.axes[9]);
+  mapped->axes[AXIS_INDEX_RIGHT_STICK_Y] = input.axes[5];
+
+  mapped->buttons_length = BUTTON_INDEX_COUNT;
+  mapped->axes_length = AXIS_INDEX_COUNT;
+}
+
 void MapperSnakebyteIDroidCon(const Gamepad& input, Gamepad* mapped) {
   *mapped = input;
   mapped->buttons[BUTTON_INDEX_TERTIARY] = input.buttons[3];
@@ -689,7 +723,7 @@ constexpr struct MappingData {
     // Xbox One Elite 2 (USB)
     {GamepadId::kMicrosoftProduct0b00, MapperXbox360Gamepad},
     // Xbox One Elite 2 (Bluetooth)
-    {GamepadId::kMicrosoftProduct0b05, MapperXboxOneBluetooth},
+    {GamepadId::kMicrosoftProduct0b05, MapperXboxElite2Bluetooth},
     // Xbox Adaptive Controller (USB)
     {GamepadId::kMicrosoftProduct0b0a, MapperXbox360Gamepad},
     // Xbox Adaptive Controller (Bluetooth)
