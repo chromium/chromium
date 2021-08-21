f47d8ed565597dfb48e74acb7ec5af0853436edb
Wez <wez@chromium.org>
2021-08-21 10:19:30 +0000

    [fuchsia] Provide a BrowserDesktopWindowTreeHost stub implementation.
    This stub uses the views::DesktopWindowTreeHostPlatform, but will
    not actually display a window/View on-screen yet.
    
    Bug: 1234748
    Change-Id: I83812094e14cc23984f3e0e688caac9f1f1a2b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110348
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914111}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_fuchsia.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_fuchsia.cc
@@ -4,7 +4,40 @@
 
 #include "chrome/browser/ui/views/frame/browser_desktop_window_tree_host.h"
 
-#include "base/notreached.h"
+#include "ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h"
+
+namespace {
+
+class BrowserDesktopWindowTreeHostFuchsia
+    : public BrowserDesktopWindowTreeHost,
+      public views::DesktopWindowTreeHostPlatform {
+ public:
+  BrowserDesktopWindowTreeHostFuchsia(
+      views::internal::NativeWidgetDelegate* native_widget_delegate,
+      views::DesktopNativeWidgetAura* desktop_native_widget_aura,
+      BrowserView* browser_view,
+      BrowserFrame* browser_frame)
+      : views::DesktopWindowTreeHostPlatform(native_widget_delegate,
+                                             desktop_native_widget_aura) {
+    // TODO(crbug.com/1234748): Implement ViewProvider and connect it here,
+    // so that the DesktopWindowTreeHostPlatform can create an Ozone window
+    // with the necessary Scenic View token, ViewRef, etc.
+  }
+  ~BrowserDesktopWindowTreeHostFuchsia() override = default;
+
+  BrowserDesktopWindowTreeHostFuchsia(
+      const BrowserDesktopWindowTreeHostFuchsia&) = delete;
+  BrowserDesktopWindowTreeHostFuchsia& operator=(
+      const BrowserDesktopWindowTreeHostFuchsia&) = delete;
+
+ private:
+  // Overridden from BrowserDesktopWindowTreeHost:
+  DesktopWindowTreeHost* AsDesktopWindowTreeHost() override { return this; }
+  int GetMinimizeButtonOffset() const override { return 0; }
+  bool UsesNativeSystemMenu() const override { return false; }
+};
+
+}  // namespace
 
 BrowserDesktopWindowTreeHost*
 BrowserDesktopWindowTreeHost::CreateBrowserDesktopWindowTreeHost(
@@ -12,7 +45,7 @@ BrowserDesktopWindowTreeHost::CreateBrowserDesktopWindowTreeHost(
     views::DesktopNativeWidgetAura* desktop_native_widget_aura,
     BrowserView* browser_view,
     BrowserFrame* browser_frame) {
-  // TODO(crbug.com/1234748)
-  NOTIMPLEMENTED_LOG_ONCE();
-  return nullptr;
+  return new BrowserDesktopWindowTreeHostFuchsia(native_widget_delegate,
+                                                 desktop_native_widget_aura,
+                                                 browser_view, browser_frame);
 }
