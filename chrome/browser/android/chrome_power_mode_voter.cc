8aa62840f181b8bc51c62d580772eb25459f603a
Stephen Nusko <nuskos@chromium.org>
2021-08-23 16:40:46 +0000

    Add trace events to BrowserControlsManager and UMA histogram on android View onDraw.
    We've seen that there is a subtle connection between
    BrowserControlsManager observers and changes to android views. So by
    adding trace events we can more clearly see when an observer might be
    causing the views to need a new layout/draw.
    
    In addition we add a histogram that emits the amount of Android Views
    onDraw calls (of the RootWindow) in the last 30 seconds.
    
    Bug: 1235338
    Change-Id: I632abaaf4689f97733803a047f257dd947c492de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110445
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914384}

--- chrome/browser/android/chrome_power_mode_voter.cc
+++ chrome/browser/android/chrome_power_mode_voter.cc
@@ -6,11 +6,48 @@
 
 #include <memory>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "components/power_scheduler/power_mode.h"
 #include "components/power_scheduler/power_mode_arbiter.h"
 #include "components/power_scheduler/power_mode_voter.h"
+#include "content/public/browser/browser_thread.h"
+
+namespace {
+
+// Only accessed on the Android UI thread and only after native initialization
+// which means its well after C++ initialization order issues. We could make
+// these static variables inside recordViewTreeDrawsUMA() to prevent them being
+// global but it would add a thread safe access on a hot UI path so we avoid
+// that here.
+base::TimeTicks g_android_view_ondraw_emitted = base::TimeTicks();
+int g_android_view_ondraw_count = 0;
+
+void RecordOnViewTreeDrawUMA() {
+  DCHECK(!content::BrowserThread::IsThreadInitialized(
+             content::BrowserThread::UI) ||
+         content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  // This is the first onDraw we've seen so start counting from now.
+  if (g_android_view_ondraw_emitted.is_null()) {
+    g_android_view_ondraw_count = 1;
+    g_android_view_ondraw_emitted = base::TimeTicks::Now();
+    return;
+  }
+  base::TimeTicks curr = base::TimeTicks::Now();
+  if (curr - g_android_view_ondraw_emitted > base::TimeDelta::FromSeconds(30)) {
+    // a 144hz monitor refreshes 120 times a second so 3600 frames in 30
+    // seconds. So 10000 should be more than enough for now (famous last words).
+    UMA_HISTOGRAM_COUNTS_10000("Android.View.onDraw.30Seconds",
+                               g_android_view_ondraw_count);
+    g_android_view_ondraw_emitted = curr;
+    g_android_view_ondraw_count = 1;
+    return;
+  }
+  ++g_android_view_ondraw_count;
+}
+}  // namespace
 
 static void JNI_ChromePowerModeVoter_OnChromeActivityStateChange(
     JNIEnv* env,
@@ -26,6 +63,7 @@ static void JNI_ChromePowerModeVoter_OnViewTreeDraw(JNIEnv* env) {
   static base::NoDestructor<power_scheduler::DebouncedPowerModeVoter> voter(
       "PowerModeVoter.Animation.Java",
       power_scheduler::PowerModeVoter::kAnimationTimeout);
+  RecordOnViewTreeDrawUMA();
   voter->VoteFor(power_scheduler::PowerMode::kAnimation);
   voter->ResetVoteAfterTimeout();
 }
