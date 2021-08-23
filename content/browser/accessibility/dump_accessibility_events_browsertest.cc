2632b0c0043301b59db5b04ab0b3b9ef08cc2f30
Maxim Kolosovskiy <kolos@chromium.org>
2021-08-23 16:13:51 +0000

    [Sheriff] Disable flaky AccessibilityEventsMenuListExpand on Win Bug: 1232295
    Change-Id: I3d9c639822f646a0c268f836abeaa3bd0b123a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113925
    Owners-Override: Maxim Kolosovskiy  <kolos@chromium.org>
    Auto-Submit: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914364}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -749,8 +749,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("menulist-collapse-next.html"));
 }
 
-// TODO(crbug/1232295): Flaky on Linux.
-#if defined(OS_LINUX)
+// TODO(crbug/1232295): Flaky on Linux and Win.
+#if defined(OS_LINUX) || defined(OS_WIN)
 #define MAYBE_AccessibilityEventsMenuListExpand \
   DISABLED_AccessibilityEventsMenuListExpand
 #else
