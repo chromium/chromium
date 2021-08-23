c093cb8e53833128f4b204e255aa00e4ea0c98ea
Maxim Kolosovskiy <kolos@chromium.org>
2021-08-23 16:39:38 +0000

    [Sheriff] Disable AmbientAuthenticationTestWithPolicy.RegularOnly on Mac Bug: 1242432
    Change-Id: I10a3756b12d5029d65cef1f1eb210f9c692c0b66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114285
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Owners-Override: Maxim Kolosovskiy  <kolos@chromium.org>
    Auto-Submit: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914382}

--- chrome/browser/net/profile_network_context_service_browsertest.cc
+++ chrome/browser/net/profile_network_context_service_browsertest.cc
@@ -389,7 +389,13 @@ class AmbientAuthenticationTestWithPolicy : public policy::PolicyTest {
   policy::PolicyMap policies_;
 };
 
-IN_PROC_BROWSER_TEST_F(AmbientAuthenticationTestWithPolicy, RegularOnly) {
+// TODO(crbug/1242432): Flaky on Mac.
+#if defined(OS_MAC)
+#define MAYBE_RegularOnly DISABLED_RegularOnly
+#else
+#define MAYBE_RegularOnly RegularOnly
+#endif
+IN_PROC_BROWSER_TEST_F(AmbientAuthenticationTestWithPolicy, MAYBE_RegularOnly) {
   EnablePolicyWithValue(net::AmbientAuthAllowedProfileTypes::REGULAR_ONLY);
   IsAmbientAuthAllowedForProfilesTest();
 }
