b899b9b4a77fff0d609ebb200b93106887d24808
Xida Chen <xidachen@chromium.org>
2021-08-23 16:42:38 +0000

    [sheriff] Disable LoginOfflineManagedTest.BackButtonTest
    Bug: 1240620
    Change-Id: Ic23140f6f7153a2b6da5572e36e43599222a301e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114425
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Owners-Override: Xida Chen <xidachen@chromium.org>
    Auto-Submit: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914385}

--- chrome/browser/ash/login/login_browsertest.cc
+++ chrome/browser/ash/login/login_browsertest.cc
@@ -369,7 +369,13 @@ IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, FullEmailDontMatchProvided) {
   TestSystemTrayIsVisible();
 }
 
-IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, BackButtonTest) {
+// crbug.com/1240620: test is flaky on chromeos.
+#if defined(OS_CHROMEOS)
+#define MAYBE_BackButtonTest DISABLED_BackButtonTest
+#else
+#define MAYBE_BackButtonTest BackButtonTest
+#endif
+IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, MAYBE_BackButtonTest) {
   std::string domain = gaia::ExtractDomainName(managed_user_id_.GetUserEmail());
 
   ConfigurePolicy(domain);
