602ccb7de68619dd400975a80db20b5b16d66712
Ewann Pellé <ewannpv@chromium.org>
2021-08-24 09:18:32 +0000

    Revert "[iOS][MobileConfig] Updates alert style"
    This reverts commit 61b590bb4932361830c3341f665c5879c46f3310.
    
    Reason for revert: The Cancel action should be at the left.
    
    Original change's description:
    > [iOS][MobileConfig] Updates alert style
    >
    > This CL  makes the continue button bold instead of the cancel button.
    >
    > Bug: 1241798
    > Change-Id: I4eba273b3f7d19c0b34324e8ebefd8e4f00df504
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3109988
    > Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#914257}
    
    Bug: 1241798
    Change-Id: Id13a790e910d8b57686671bd0177e28530c1cfdc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114927
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914668}

--- ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
+++ ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
@@ -169,7 +169,7 @@ const char kUmaDownloadMobileConfigFileUI[] =
                       kUmaDownloadMobileConfigFileUI,
                       DownloadMobileConfigFileUI::KWarningAlertIsDismissed);
                 }
-                 style:UIAlertActionStyleDefault];
+                 style:UIAlertActionStyleCancel];
 
   __weak MobileConfigCoordinator* weakSelf = self;
   [self.alertCoordinator
@@ -178,7 +178,7 @@ const char kUmaDownloadMobileConfigFileUI[] =
                 action:^{
                   [weakSelf presentSFSafariViewController:fileURL];
                 }
-                 style:UIAlertActionStyleCancel];
+                 style:UIAlertActionStyleDefault];
 
   [self.alertCoordinator start];
 }
