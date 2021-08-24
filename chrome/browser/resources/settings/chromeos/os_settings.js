411598700b00c320a4babbc2286f592ebdbc396f
Yue Li <updowndota@google.com>
2021-08-24 09:19:35 +0000

    [CrOS Settings] Check in Polymer3 version of os_settings_menu
    Bug: 1180979
    Change-Id: I56188ac506256d52d68c30f042052332f3ae7137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115549
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914669}

--- chrome/browser/resources/settings/chromeos/os_settings.js
+++ chrome/browser/resources/settings/chromeos/os_settings.js
@@ -85,7 +85,7 @@ import './os_search_page/os_search_page.js';
 import './os_settings_main/os_settings_main.m.js';
 import './os_settings_page/os_settings_page.m.js';
 import './os_settings_page/settings_idle_load.m.js';
-import './os_settings_menu/os_settings_menu.m.js';
+import './os_settings_menu/os_settings_menu.js';
 import './os_settings_ui/os_settings_ui.m.js';
 import './os_settings_icons_css.m.js';
 import './os_settings_search_box/os_search_result_row.js';
--- chrome/browser/resources/settings/chromeos/os_settings_menu/os_settings_menu.js
+++ chrome/browser/resources/settings/chromeos/os_settings_menu/os_settings_menu.js
@@ -6,10 +6,26 @@
  * @fileoverview
  * 'os-settings-menu' shows a menu with a hardcoded set of pages and subpages.
  */
+import '//resources/cr_elements/cr_button/cr_button.m.js';
+import '//resources/cr_elements/icons.m.js';
+import '//resources/polymer/v3_0/iron-collapse/iron-collapse.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/iron-selector/iron-selector.js';
+import '../../settings_shared_css.js';
+import '../os_icons.m.js';
+
+import {assert, assertNotReached} from '//resources/js/assert.m.js';
+import {loadTimeData} from '//resources/js/load_time_data.m.js';
+import {afterNextRender, flush, html, Polymer, TemplateInstanceBase, Templatizer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {Route, RouteObserverBehavior, Router} from '../../router.js';
+import {routes} from '../os_route.m.js';
+
 Polymer({
+  _template: html`{__html_template__}`,
   is: 'os-settings-menu',
 
-  behaviors: [settings.RouteObserverBehavior],
+  behaviors: [RouteObserverBehavior],
 
   properties: {
     advancedOpened: {
@@ -49,22 +65,22 @@ Polymer({
     },
   },
 
-  /** @param {!settings.Route} newRoute */
+  /** @param {!Route} newRoute */
   currentRouteChanged(newRoute) {
     const urlSearchQuery =
-        settings.Router.getInstance().getQueryParameters().get('search');
+        Router.getInstance().getQueryParameters().get('search');
     // If the route navigated to by a search result is in the advanced
     // section, the advanced menu will expand.
-    if (urlSearchQuery && settings.routes.ADVANCED &&
-        settings.routes.ADVANCED.contains(newRoute)) {
+    if (urlSearchQuery && routes.ADVANCED &&
+        routes.ADVANCED.contains(newRoute)) {
       this.advancedOpened = true;
     }
 
     // Focus the initially selected path.
     const anchors = this.root.querySelectorAll('a');
     for (let i = 0; i < anchors.length; ++i) {
-      const anchorRoute = settings.Router.getInstance().getRouteForPath(
-          anchors[i].getAttribute('href'));
+      const anchorRoute =
+          Router.getInstance().getRouteForPath(anchors[i].getAttribute('href'));
       if (anchorRoute && anchorRoute.contains(newRoute)) {
         this.setSelectedUrl_(anchors[i].href);
         return;
