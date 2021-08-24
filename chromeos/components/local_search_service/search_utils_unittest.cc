d3c8343721c2399aae1dac27bc6bba3669f97a02
Toni Baržić <tbarzic@chromium.org>
2021-08-24 19:36:05 +0000

    Revert "Reland "[LSS] Tweak the fuzzy search parameters for the inverted index.""
    This reverts commit d913aa7a590f03f5e48c1190161a2e7f9075a631.
    
    Reason for revert: apps.SearchInHelpApp.stable failing again
    
    Original change's description:
    > Reland "[LSS] Tweak the fuzzy search parameters for the inverted index."
    >
    > This is a reland of 7bfdd6ebef59be553b574a66b43ef7fb908abe4f
    >
    > Original change's description:
    > > [LSS] Tweak the fuzzy search parameters for the inverted index.
    > >
    > > Inverted index search is currently used by the Help App client only.
    > > There are multiple examples of fuzzy matches appearing higher than exact
    > > matches. This CL introduces two tweaks to help address this:
    > > - The default fuzzy match threshold is raised from 0.6 to 0.7.
    > > - Scores are multiplied by their match score, which is lower for fuzzier
    > >   matches and higher for more exact matches. This penalises the fuzzy
    > >   matches which still meet the threshold.
    > >
    > > Also see http://go/showoff-launcher-search-qa#heading=h.nn72w5j8ngvo for
    > > examples of this change, by zufeng@.
    > >
    > > Bug: 1221017
    > > Change-Id: I569edaa77eb8972844933850178cf50a2bbf13f0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021625
    > > Commit-Queue: Rachel Wong <wrong@chromium.org>
    > > Reviewed-by: Zufeng Wang <zufeng@google.com>
    > > Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#902350}
    >
    > Bug: 1221017
    > Change-Id: If12cae191f94d4c7332aa87f0ac95253f1186f12
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114150
    > Reviewed-by: Zufeng Wang <zufeng@google.com>
    > Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    > Commit-Queue: Rachel Wong <wrong@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#914637}
    
    Bug: 1221017
    Change-Id: I191a605e564de3a5a9a613543946fd4f6b92fd64
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116495
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Owners-Override: Toni Baržić <tbarzic@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914870}

--- chromeos/components/local_search_service/search_utils_unittest.cc
+++ chromeos/components/local_search_service/search_utils_unittest.cc
@@ -4,8 +4,6 @@
 
 #include "chromeos/components/local_search_service/search_utils.h"
 
-#include <algorithm>
-
 #include "base/strings/utf_string_conversions.h"
 #include "chromeos/components/local_search_service/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -40,38 +38,25 @@ TEST(SearchUtilsTest, BlockMatch) {
   EXPECT_NEAR(BlockMatchScore(u"wifi", u"Wi-Fi"), 0.402, 0.001);
 }
 
-TEST(SearchUtilsTest, RelevanceCoefficient) {
+TEST(SearchUtilsTest, IsRelevantApproximately) {
   // Relevant because prefix requirement is met.
-  EXPECT_GT(RelevanceCoefficient(u"wifi", u"wi-fi", 0 /* prefix_threshold */,
-                                 0.99 /* block_threshold */),
-            0);
+  EXPECT_TRUE(IsRelevantApproximately(
+      u"wifi", u"wi-fi", 0 /* prefix_threshold */, 0.99 /* block_threshold */));
 
   // Relevant because prefix requirement is met.
-  EXPECT_GT(RelevanceCoefficient(u"musi", u"music", 0.8 /* prefix_threshold */,
-                                 0.999 /* block_threshold */),
-            0);
+  EXPECT_TRUE(IsRelevantApproximately(u"musi", u"music",
+                                      0.8 /* prefix_threshold */,
+                                      0.999 /* block_threshold */));
 
   // Relevant because block match requirement is met.
-  EXPECT_GT(RelevanceCoefficient(u"wifi", u"wi-fi", 0.2 /* prefix_threshold */,
-                                 0.001 /* block_threshold */),
-            0);
+  EXPECT_TRUE(IsRelevantApproximately(u"wifi", u"wi-fi",
+                                      0.2 /* prefix_threshold */,
+                                      0.001 /* block_threshold */));
 
   // Neither prefix nor block match requirement is met.
-  EXPECT_EQ(RelevanceCoefficient(u"wifi", u"wi-fi", 0.2 /* prefix_threshold */,
-                                 0.99 /* block_threshold */),
-            0);
-
-  // Return the higher score if both requirements are met.
-  EXPECT_NEAR(RelevanceCoefficient(u"wifi", u"wi-fi", 0 /* prefix_threshold */,
-                                   0 /* block_threshold */),
-              std::max(BlockMatchScore(u"wifi", u"wi-fi"),
-                       ExactPrefixMatchScore(u"wifi", u"wi-fi")),
-              0.001);
-  EXPECT_NEAR(RelevanceCoefficient(u"musi", u"music", 0 /* prefix_threshold */,
-                                   0 /* block_threshold */),
-              std::max(BlockMatchScore(u"musi", u"music"),
-                       ExactPrefixMatchScore(u"musi", u"music")),
-              0.001);
+  EXPECT_FALSE(IsRelevantApproximately(u"wifi", u"wi-fi",
+                                       0.2 /* prefix_threshold */,
+                                       0.99 /* block_threshold */));
 }
 
 }  // namespace local_search_service
