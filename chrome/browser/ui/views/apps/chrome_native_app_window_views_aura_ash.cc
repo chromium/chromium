68d4e144a71d0e508faec076ae5c11060e3045c4
ananubis <anqing@google.com>
2021-08-25 04:45:06 +0000

    Remove unnecessary Layout() call from SetFullscreen method
    The Layout() call in the SetFullscreen(..) method can be removed because
    the `fram_view` has been invalidated. It should be updated as expected
    afterwards.
    
    Change-Id: I63828041e0a415ec525f0a5f976dce07af0e2f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110608
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915054}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -330,7 +330,6 @@ void ChromeNativeAppWindowViewsAuraAsh::SetFullscreen(int fullscreen_types) {
   // Invalidate the frame to ensure that it is re-laid out (even if the bounds
   // don't change) so that the frame sets the bounds of the client view.
   widget()->non_client_view()->frame_view()->InvalidateLayout();
-  widget()->non_client_view()->Layout();
 }
 
 void ChromeNativeAppWindowViewsAuraAsh::SetActivateOnPointer(
