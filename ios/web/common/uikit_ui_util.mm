565c2d55a40d37a5bc4b7645eb299b2ea792fe7e
Nohemi Fernandez <fernandex@chromium.org>
2021-08-25 09:18:24 +0000

    [iOS] Update key window logic to use scene-level information in iOS 15.
    iOS 15 deprecates the key window concept at the application level,
    therefore Chrome should use information from scenes to determine the
    orientation of the device.
    
    Fixed: 1240313
    Change-Id: I5ee599952a9118cb2def7063cc1cfe3964f54ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099987
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915110}

--- ios/web/common/uikit_ui_util.mm
+++ ios/web/common/uikit_ui_util.mm
@@ -14,10 +14,24 @@ UIWindow* GetAnyKeyWindow() {
 #if !defined(__IPHONE_13_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_13_0
   return [UIApplication sharedApplication].keyWindow;
 #else
-  NSArray<UIWindow*>* windows = [UIApplication sharedApplication].windows;
-  for (UIWindow* window in windows) {
-    if (window.isKeyWindow)
-      return window;
+  // In iOS 15 and later key windows are a deprecated concept. Window state
+  // should be determined at the scene rather than the application level.
+  if (@available(iOS 15, *)) {
+    NSSet<UIScene*>* windowScenes =
+        [UIApplication sharedApplication].connectedScenes;
+    for (UIScene* scene : windowScenes) {
+      if ([scene.delegate
+              conformsToProtocol:@protocol(UIWindowSceneDelegate)]) {
+        return [(id<UIWindowSceneDelegate>)scene.delegate window];
+      }
+    }
+  } else {
+    NSArray<UIWindow*>* windows = [UIApplication sharedApplication].windows;
+    // Find a key window if it exists.
+    for (UIWindow* window in windows) {
+      if (window.isKeyWindow)
+        return window;
+    }
   }
   return nil;
 #endif
