7cd3e237083f9f700398bc1d3cc1add428059c13
Roman Sorokin <rsorokin@chromium.org>
2021-08-25 13:22:59 +0000

    [ash-chrome] Migrate UserImageSyncObserver to sync categorization
    Check kSyncSettingsCategorization instead of deprecated
    kSplitSettingsSync
    
    Bug: 1227693
    Change-Id: I6b31fb6a827d24d7fb9d191bb5e357cf5342885d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114678
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915161}

--- chrome/browser/ash/login/users/avatar/user_image_sync_observer.cc
+++ chrome/browser/ash/login/users/avatar/user_image_sync_observer.cc
@@ -77,7 +77,7 @@ void UserImageSyncObserver::OnProfileGained(Profile* profile) {
       kUserImageInfo,
       base::BindRepeating(&UserImageSyncObserver::OnPreferenceChanged,
                           base::Unretained(this)));
-  is_synced_ = chromeos::features::IsSplitSettingsSyncEnabled()
+  is_synced_ = chromeos::features::IsSyncSettingsCategorizationEnabled()
                    ? prefs_->AreOsPriorityPrefsSyncing()
                    : prefs_->IsPrioritySyncing();
   if (!is_synced_) {
@@ -127,7 +127,7 @@ void UserImageSyncObserver::OnUserImageChanged(const user_manager::User& user) {
 }
 
 void UserImageSyncObserver::OnIsSyncingChanged() {
-  is_synced_ = chromeos::features::IsSplitSettingsSyncEnabled()
+  is_synced_ = chromeos::features::IsSyncSettingsCategorizationEnabled()
                    ? prefs_->AreOsPriorityPrefsSyncing()
                    : prefs_->IsPrioritySyncing();
   if (is_synced_) {
