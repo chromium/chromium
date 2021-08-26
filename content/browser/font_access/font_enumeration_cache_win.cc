e2e739e6e3d562578af11704fb8a8118cfd5dbd2
Victor Costan <pwnall@chromium.org>
2021-08-26 14:42:25 +0000

    Font Access: Extract functions in Windows implementation.
    The main goal of this CL is to make ExtractNamesFromFamily() in
    //content/browser/font_access/font_enumeration_cache_win.cc smaller, and
    therefore easier to read.
    
    * COM interface function arguments that are only used during the
      function call are passed using raw pointers, instead of
      Microsoft::WRL::ComPtr smart pointers. This is similar to limiting the
      use of std::unique_ptr and scoped_refptr to arguments that are stored
      and used past the function call.
    
    * GetNativeString() is folded into GetLocalizedString(). A key
      observation here is that, before this CL, GetLocalizedString() was
      only used for `full_name` (lines 176-179), and the two calls
      effectively implemented the same fallback logic used in the old
      GetNativeString() implementation. So, GetLocalizedString() now
      implements the fallback logic, and is used everywhere.
    
    * For uniformity, GetLocalizedString() is updated to take a raw pointer
      to the IDWriteFont COM interface, instead of a Microsoft::WRL::ComPtr
      smart pointer. This is appropriate because GetLocalizedString() only
      uses the argument during the function call.
    
    * GetSystemFonts() contains the logic that used to live at lines
      257-272 before this CL.
    
    * GetFontInformation() contains logic that used to be duplicated at
      lines 143-151, 164-171, and 185-193 before this CL.
    
    * GetFamilyName() contains the logic that used to live at lines 106-114
      before this CL.
    
    * GetFontPostScriptName() contains the logic that used to live at lines
      134-154 before this CL.
    
    * GetFontFullName() contains the logic that used to live at lines
      162-180 before this CL.
    
    * GetFontStyleName() contains the logic that used to live at lines
      183-209 before this CL.
    
    * Lines 211-213 before this CL were inlined in lines 269-271.
    
    Bug: 1232812
    Change-Id: Ie0e5a001bf6faf676a0188b906bb60a8ab0102f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105730
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915573}

--- content/browser/font_access/font_enumeration_cache_win.cc
+++ content/browser/font_access/font_enumeration_cache_win.cc
@@ -26,24 +26,149 @@ namespace content {
 
 namespace {
 
-absl::optional<std::string> GetNativeString(
-    Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> names) {
-  // Retrieve the native name. Try the "en-us" locale and if it's
-  // not present, used the first available localized name.
-  absl::optional<std::string> native =
-      gfx::win::RetrieveLocalizedString(names.Get(), "en-us");
-  if (!native) {
-    native = gfx::win::RetrieveLocalizedString(names.Get(), "");
-  }
-  return native;
+// Retrieves a DirectWrite font collection for all fonts on the system.
+//
+// This operation may be expensive, and its result should be cached.
+//
+// Returns nullptr in case of failure.
+Microsoft::WRL::ComPtr<IDWriteFontCollection> GetSystemFonts() {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+  // Returned from all code paths, to enable return value optimization.
+  Microsoft::WRL::ComPtr<IDWriteFontCollection> collection = nullptr;
+
+  Microsoft::WRL::ComPtr<IDWriteFactory> factory;
+  gfx::win::CreateDWriteFactory(&factory);
+  if (!factory)
+    return collection;
+
+  HRESULT hr = factory->GetSystemFontCollection(&collection);
+  if (FAILED(hr))
+    collection = nullptr;
+
+  return collection;
 }
 
-absl::optional<std::string> GetLocalizedString(
-    Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> names,
-    const std::string& locale) {
+// Retrieves a string from a font's information table.
+//
+// Returns nullptr in case of failure.
+Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> GetFontInformation(
+    IDWriteFont* font,
+    DWRITE_INFORMATIONAL_STRING_ID string_id) {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+
+  // Returned from all code paths, to enable return value optimization.
+  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> localized_strings;
+  BOOL font_has_info;
+
+  HRESULT hr = font->GetInformationalStrings(string_id, &localized_strings,
+                                             &font_has_info);
+  if (FAILED(hr) || !font_has_info)
+    localized_strings = nullptr;
+
+  return localized_strings;
+}
+
+// Retrieves a string matching a locale from a DirectWrite string collection.
+//
+// If a string in the given locale does not exist, falls back to retrieving the
+// first string in the collection.
+//
+// Returns nullopt in case the string does not exist.
+absl::optional<std::string> GetLocalizedString(IDWriteLocalizedStrings* names,
+                                               const std::string& locale) {
   absl::optional<std::string> localized_name =
-      gfx::win::RetrieveLocalizedString(names.Get(), locale);
+      gfx::win::RetrieveLocalizedString(names, locale);
+  if (!localized_name.has_value()) {
+    // Fall back to returning the first string in the collection.
+    localized_name = gfx::win::RetrieveLocalizedString(names, std::string());
+  }
   return localized_name;
+}
+
+// Retrieves a font family name that can be reported by the Fonts Access API.
+//
+// Returns nullopt in case of failure.
+absl::optional<std::string> GetFamilyName(IDWriteFontFamily* family) {
+  absl::optional<std::string> family_name;
+
+  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> family_names;
+  HRESULT hr = family->GetFamilyNames(&family_names);
+  if (FAILED(hr))
+    return family_name;
+
+  family_name = GetLocalizedString(family_names.Get(), "en-us");
+  return family_name;
+}
+
+// Retrieves a font's PostScript name, to be reported by the Fonts Access API.
+//
+// Returns nullopt in case of failure.
+absl::optional<std::string> GetFontPostScriptName(IDWriteFont* font) {
+  absl::optional<std::string> postscript_name;
+
+  // DWRITE_INFORMATIONAL_STRING_POSTSCRIPT_NAME and
+  // DWRITE_INFORMATIONAL_STRING_FULL_NAME are only supported on Windows 7 with
+  // KB2670838 (https://support.microsoft.com/en-us/kb/2670838) installed. It is
+  // possible to use a fallback as can be observed in Firefox:
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=947812 However, this might not
+  // be worth the effort.
+
+  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> postscript_names =
+      GetFontInformation(font, DWRITE_INFORMATIONAL_STRING_POSTSCRIPT_NAME);
+  if (!postscript_names)
+    return postscript_name;
+
+  postscript_name = GetLocalizedString(postscript_names.Get(), "en-us");
+  return postscript_name;
+}
+
+// Retrieves a font's full name, to be reported by the Fonts Access API.
+//
+// Returns nullopt in case of failure.
+absl::optional<std::string> GetFontFullName(IDWriteFont* font,
+                                            const std::string& locale) {
+  absl::optional<std::string> full_name;
+
+  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> full_names =
+      GetFontInformation(font, DWRITE_INFORMATIONAL_STRING_FULL_NAME);
+  if (!full_names)
+    return full_name;
+
+  full_name = GetLocalizedString(full_names.Get(), locale);
+  return full_name;
+}
+
+// Returns a font's style name, to be reported by the Fonts Access API.
+//
+// Returns nullopt in case of failure.
+absl::optional<std::string> GetFontStyleName(IDWriteFont* font) {
+  absl::optional<std::string> style_name;
+
+  // All fonts should have a subfamily name compatible with Windows GDI,
+  // available as the string DWRITE_INFORMATIONAL_STRING_WIN32_SUBFAMILY_NAMES.
+  //
+  // In some cases, the family / sub-family names preferred by designer wouldn't
+  // be compatible with Windows GDI, and the desiner. In these cases, the
+  // designer-preferred subfamily name is availabe as the string
+  // DWRITE_INFORMATIONAL_STRING_PREFERRED_SUBFAMILY_NAMES.
+  //
+  // More details at
+  // https://docs.microsoft.com/en-us/windows/win32/api/dwrite/ne-dwrite-dwrite_informational_string_id
+
+  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> style_names =
+      GetFontInformation(font,
+                         DWRITE_INFORMATIONAL_STRING_PREFERRED_SUBFAMILY_NAMES);
+  if (!style_names) {
+    style_names = GetFontInformation(
+        font, DWRITE_INFORMATIONAL_STRING_WIN32_SUBFAMILY_NAMES);
+    if (!style_names)
+      return style_name;
+  }
+
+  style_name = GetLocalizedString(style_names.Get(), "en-us");
+  return style_name;
 }
 
 // Map DWRITE_FONT_STYLE to a boolean for italic/oblique.
@@ -103,20 +228,10 @@ ExtractNamesFromFamily(Microsoft::WRL::ComPtr<IDWriteFontCollection> collection,
   if (FAILED(hr))
     return family_result;
 
-  Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> family_names;
-  hr = family->GetFamilyNames(&family_names);
-  if (FAILED(hr))
-    return family_result;
-
-  absl::optional<std::string> native_family_name =
-      GetNativeString(family_names);
-  if (!native_family_name)
-    return family_result;
+  absl::optional<std::string> native_family_name = GetFamilyName(family.Get());
 
   uint32_t font_count = family->GetFontCount();
   for (uint32_t font_index = 0; font_index < font_count; ++font_index) {
-    BOOL exists = false;
-
     Microsoft::WRL::ComPtr<IDWriteFont> font;
     {
       base::ScopedBlockingCall scoped_blocking_call(
@@ -131,101 +246,36 @@ ExtractNamesFromFamily(Microsoft::WRL::ComPtr<IDWriteFontCollection> collection,
     if (font->GetSimulations() != DWRITE_FONT_SIMULATIONS_NONE)
       continue;
 
-    // DWRITE_INFORMATIONAL_STRING_POSTSCRIPT_NAME and
-    // DWRITE_INFORMATIONAL_STRING_FULL_NAME are only supported on Windows 7
-    // with KB2670838 (https://support.microsoft.com/en-us/kb/2670838)
-    // installed. It is possible to use a fallback as can be observed
-    // in Firefox: https://bugzilla.mozilla.org/show_bug.cgi?id=947812 However,
-    // this might not be worth the effort.
-
-    // Extracting the postscript name.
-    Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> postscript_name;
-    {
-      base::ScopedBlockingCall scoped_blocking_call(
-          FROM_HERE, base::BlockingType::MAY_BLOCK);
-      hr = font->GetInformationalStrings(
-          DWRITE_INFORMATIONAL_STRING_POSTSCRIPT_NAME, &postscript_name,
-          &exists);
-    }
-    if (FAILED(hr))
-      return family_result;
-
-    if (!exists)
-      return family_result;
-
     absl::optional<std::string> native_postscript_name =
-        GetNativeString(postscript_name);
-    if (!native_postscript_name) {
-      return family_result;
-    }
-
-    // Extracting the full name.
-    Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> full_name;
-    {
-      base::ScopedBlockingCall scoped_blocking_call(
-          FROM_HERE, base::BlockingType::MAY_BLOCK);
-      hr = font->GetInformationalStrings(DWRITE_INFORMATIONAL_STRING_FULL_NAME,
-                                         &full_name, &exists);
-    }
-    if (FAILED(hr))
-      return family_result;
-
-    if (!exists)
+        GetFontPostScriptName(font.Get());
+    if (!native_postscript_name)
       return family_result;
 
     absl::optional<std::string> localized_full_name =
-        GetLocalizedString(full_name, locale);
-    if (!localized_full_name)
-      localized_full_name = GetLocalizedString(full_name, "");
+        GetFontFullName(font.Get(), locale);
     if (!localized_full_name)
       localized_full_name = native_postscript_name;
 
-    // Extracting style name.
-    Microsoft::WRL::ComPtr<IDWriteLocalizedStrings> style_name;
-    {
-      base::ScopedBlockingCall scoped_blocking_call(
-          FROM_HERE, base::BlockingType::MAY_BLOCK);
-      hr = font->GetInformationalStrings(
-          DWRITE_INFORMATIONAL_STRING_PREFERRED_SUBFAMILY_NAMES, &style_name,
-          &exists);
-    }
-    if (FAILED(hr))
+    absl::optional<std::string> native_style_name =
+        GetFontStyleName(font.Get());
+    if (!native_style_name)
       return family_result;
-
-    if (!exists) {
-      {
-        base::ScopedBlockingCall scoped_blocking_call(
-            FROM_HERE, base::BlockingType::MAY_BLOCK);
-        hr = font->GetInformationalStrings(
-            DWRITE_INFORMATIONAL_STRING_WIN32_SUBFAMILY_NAMES, &style_name,
-            &exists);
-      }
-      if (FAILED(hr))
-        return family_result;
-    }
-    absl::optional<std::string> native_style_name;
-    if (exists) {
-      native_style_name = GetNativeString(style_name);
-    }
-
-    DWRITE_FONT_STRETCH stretch = font->GetStretch();
-    DWRITE_FONT_STYLE style = font->GetStyle();
-    DWRITE_FONT_WEIGHT weight = font->GetWeight();
 
     blink::FontEnumerationTable_FontMetadata metadata;
     metadata.set_postscript_name(native_postscript_name.value());
     metadata.set_full_name(localized_full_name.value());
     metadata.set_family(native_family_name.value());
     metadata.set_style(native_style_name ? native_style_name.value() : "");
-    metadata.set_italic(DWriteStyleToWebItalic(style));
-    metadata.set_weight(DWriteWeightToWebWeight(weight));
-    metadata.set_stretch(DWriteStretchToWebStretch(stretch));
+    metadata.set_italic(DWriteStyleToWebItalic(font->GetStyle()));
+    metadata.set_weight(DWriteWeightToWebWeight(font->GetWeight()));
+    metadata.set_stretch(DWriteStretchToWebStretch(font->GetStretch()));
 
     family_result->fonts.push_back(std::move(metadata));
   }
 
   return family_result;
 }
+
 }  // namespace
 
 // static
@@ -253,23 +303,7 @@ void FontEnumerationCacheWin::InitializeDirectWrite() {
     return;
 
   direct_write_initialized_ = true;
-
-  Microsoft::WRL::ComPtr<IDWriteFactory> factory;
-  gfx::win::CreateDWriteFactory(&factory);
-  if (factory == nullptr) {
-    // We won't be able to load fonts, but we should still return messages so
-    // renderers don't hang.
-    status_ = blink::mojom::FontEnumerationStatus::kUnexpectedError;
-    return;
-  }
-
-  HRESULT hr = factory->GetSystemFontCollection(&collection_);
-  DCHECK(SUCCEEDED(hr));
-
-  if (!collection_) {
-    status_ = blink::mojom::FontEnumerationStatus::kUnexpectedError;
-    return;
-  }
+  collection_ = GetSystemFonts();
 }
 
 void FontEnumerationCacheWin::SchedulePrepareFontEnumerationCache() {
