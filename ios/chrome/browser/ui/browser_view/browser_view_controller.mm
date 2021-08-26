6ecbfddb07666afa23b1fce73435351521c8176e
Justin Cohen <justincohen@chromium.org>
2021-08-26 18:23:57 +0000

    Revert "[iOS] Updates context menu image actions"
    This reverts commit 52ddfe5fb4bdb4d54090b254ee187bbdd32ae6ad.
    
    Reason for revert: Broke downstream tests
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/ipad-simulator/24209/test-results
    
    Original change's description:
    > [iOS] Updates context menu image actions
    >
    > This CL updates context menu image actions.
    >
    > screenshot: https://drive.google.com/file/d/1DMm71TZppyGCVx5JBqsIXnNtUpmwq27R/view?usp=sharing&resourcekey=0-3hRzObHRPhqT1kQXX5ywZg
    >
    > Bug: 1241012
    > Change-Id: I697944e8c805e3a9d3d407f47b9bf2e9a95e1b48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114933
    > Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#915577}
    
    Bug: 1241012
    Change-Id: Ia7ddbbbcc7008bd2e1a9f2b65de2c477b34e2a9d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122788
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Owners-Override: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915653}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -3870,11 +3870,8 @@ NSString* const kBrowserViewControllerSnackbarCategory =
         ios::TemplateURLServiceFactory::GetForBrowserState(self.browserState);
     if (search_engines::SupportsSearchByImage(service)) {
       const TemplateURL* defaultURL = service->GetDefaultSearchProvider();
-      NSString* title =
-          IsContextMenuActionsRefreshEnabled()
-              ? l10n_util::GetNSString(IDS_IOS_CONTEXT_MENU_SEARCHFORIMAGE)
-              : l10n_util::GetNSStringF(IDS_IOS_CONTEXT_MENU_SEARCHWEBFORIMAGE,
-                                        defaultURL->short_name());
+      NSString* title = l10n_util::GetNSStringF(
+          IDS_IOS_CONTEXT_MENU_SEARCHWEBFORIMAGE, defaultURL->short_name());
       UIAction* searchByImage = [actionFactory
           actionSearchImageWithTitle:title
                                Block:^{
