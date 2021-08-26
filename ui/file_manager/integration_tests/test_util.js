9b16413d883089aa7f586942212e4e2142408ba3
Luciano Pacheco <lucmult@chromium.org>
2021-08-26 03:01:33 +0000

    Files app: Remove unused test function
    Change-Id: Ie1745c5cab6251817525189e24daa12f6621efa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115717
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915456}

--- ui/file_manager/integration_tests/test_util.js
+++ ui/file_manager/integration_tests/test_util.js
@@ -219,25 +219,6 @@ export function waitForAppWindow(windowUrl) {
 }
 
 /**
- * Wait for the count of windows for app |appId| to equal |expectedCount|.
- * @param{string} appId ID of the app to count windows for.
- * @param{number} expectedCount Number of app windows to wait for.
- * @return {Promise} Promise to be fulfilled when the number of app windows
- *     equals |expectedCount|.
- */
-export function waitForAppWindowCount(appId, expectedCount) {
-  const caller = getCaller();
-  const command = {'name': 'countAppWindows', 'appId': appId};
-  return repeatUntil(async () => {
-    if (await sendTestMessage(command) != expectedCount) {
-      return pending(
-          caller, 'waitForAppWindowCount ' + appId + ' ' + expectedCount);
-    }
-    return true;
-  });
-}
-
-/**
  * Get all the browser windows.
  * @return {Object} Object returned from chrome.windows.getAll().
  */
