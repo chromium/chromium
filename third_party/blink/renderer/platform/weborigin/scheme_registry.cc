45328211652ef03134cc69260d073856954dbb48
Dan Sanders <sandersd@chromium.org>
2021-08-26 19:20:04 +0000

    [Code health] Remove DeprecatedAtOrEmptyValue in renderer/platform/weborigin
    Fixed: 1236750
    Change-Id: Id1a87286baa7c256db653ec2241a893517140089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119272
    Auto-Submit: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915676}

--- third_party/blink/renderer/platform/weborigin/scheme_registry.cc
+++ third_party/blink/renderer/platform/weborigin/scheme_registry.cc
@@ -415,12 +415,12 @@ bool SchemeRegistry::SchemeShouldBypassContentSecurityPolicy(
   if (scheme.IsEmpty() || policy_areas == kPolicyAreaNone)
     return false;
 
-  // get() returns 0 (PolicyAreaNone) if there is no entry in the map.
-  // Thus by default, schemes do not bypass CSP.
-  return (GetURLSchemesRegistry()
-              .content_security_policy_bypassing_schemes
-              .DeprecatedAtOrEmptyValue(scheme) &
-          policy_areas) == policy_areas;
+  const auto& bypassing_schemes =
+      GetURLSchemesRegistry().content_security_policy_bypassing_schemes;
+  const auto it = bypassing_schemes.find(scheme);
+  if (it == bypassing_schemes.end())
+    return false;
+  return (it->value & policy_areas) == policy_areas;
 }
 
 void SchemeRegistry::RegisterURLSchemeBypassingSecureContextCheck(
