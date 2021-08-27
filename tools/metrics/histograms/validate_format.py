ba954c6f36b062f1aa251cea08793e2b03af5a0e
Weilun Shi <sweilun@chromium.org>
2021-08-27 09:41:04 +0000

    Reland "All histograms with the same namespace should be put together"
    This cl was reverted because we already had a namespace that didn't
    satisfy the condition. After a few discussions, instead of being so
    inflexible, we decided to add an allowlist as an option for some
    developers to work around this restriction.
    
    The first patch is the pure reland, but the subsequent patches add the
    |_NAMESPACE_IN_MULTIPLE_FILES| allowlist to allow some namespaces to be
    stored in multiple files when necessary.
    
    Original change's description:
    > All histograms with the same namespace should be put together
    >
    > Add a PRESUBMIT check to make sure all histograms with the same
    > namespace should be put in the same file.
    >
    > This cl also moves some existing histograms that violate this new
    > requirement to where they should be in.
    >
    > The cl puts extra burden on the validate_format.py as well as the
    > presubmit stage but the performance impact is negligible.
    >
    > With CheckNamespace():
    > $ time python3 tools/metrics/histograms/validate_format.py
    >
    > real	0m18.639s
    > user	0m15.523s
    > sys	0m4.623s
    >
    > Without:
    > $ time python3 tools/metrics/histograms/validate_format.py
    >
    > real	0m17.599s
    > user	0m14.676s
    > sys	0m4.405s
    >
    >
    > Bug: 1240633
    > Change-Id: I7230f50d1b6f4ff89dc299742427df221444110c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105171
    > Auto-Submit: Weilun Shi <sweilun@chromium.org>
    > Commit-Queue: Weilun Shi <sweilun@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#913709}
    
    Bug: 1240633
    Change-Id: Ifb6b85b33d0ca2fef4c5bbe681bca6ac63890b15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119702
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915880}

--- tools/metrics/histograms/validate_format.py
+++ tools/metrics/histograms/validate_format.py
@@ -5,17 +5,53 @@
 
 """Verifies that the histograms XML file is well-formatted."""
 
-import os
+import logging
 import sys
+import xml.dom.minidom
 
 import extract_histograms
 import histogram_paths
 import merge_xml
 
+# The allowlist of namespaces that are split across multiple files.
+_NAMESPACES_IN_MULTIPLE_FILES = ['chromeos']
+
+
+def CheckNamespaces():
+  namespaces = {}
+  has_errors = False
+  # Iterate over HISTOGRAMS_XMLS rather than ALL_XMLS because it's fine for
+  # histogram namespaces in obsolete_histograms.xml to also appear in
+  # non-obsolete histograms.xml files.
+  for path in histogram_paths.HISTOGRAMS_XMLS:
+    tree = xml.dom.minidom.parse(path)
+
+    def _GetNamespace(node):
+      return node.getAttribute('name').lower().split('.')[0]
+
+    namespaces_in_file = set(
+        _GetNamespace(node) for node in extract_histograms.IterElementsWithTag(
+            tree, 'histogram', depth=3))
+    for namespace in namespaces_in_file:
+      if (namespace in namespaces
+          and namespace not in _NAMESPACES_IN_MULTIPLE_FILES):
+        logging.error(
+            'Namespace %s has already been used in %s. it\'s recommended to '
+            'put histograms with the same namespace in the same file. If you '
+            'intentionally want to split a namespace across multiple files, '
+            'please add the namespace to the |_NAMESPACES_IN_MULTIPLE_FILES| '
+            'in the validate_format.py.' % (namespace, namespaces[namespace]))
+        has_errors = True
+      namespaces[namespace] = path
+
+  return has_errors
+
+
 def main():
   doc = merge_xml.MergeFiles(histogram_paths.ALL_XMLS,
                              should_expand_owners=True)
   _, errors = extract_histograms.ExtractHistogramsFromDom(doc)
+  errors = errors or CheckNamespaces()
   sys.exit(errors)
 
 if __name__ == '__main__':
