4a3ae798de12a76b4c7f9072bb7a88d18b89fec3
K. Moon <kmoon@chromium.org>
2021-08-27 16:58:19 +0000

    Revert "Extensions: Flip kAllowSharedArrayBuffersUnconditionally feature."
    This reverts commit 4729ab5371a9e1a02531ad393256e4ea8409f5e4.
    
    Reason for revert: Broke PDFExtensionJSTestWithoutUnseasonedOverride.AnnotationsFeatureEnabled on linux-chromeos-chrome.
    
    Original change's description:
    > Extensions: Flip kAllowSharedArrayBuffersUnconditionally feature.
    >
    > Flip kAllowSharedArrayBuffersUnconditionally feature to be disabled by
    > default. Starting from M95, extensions will be required to opt into
    > cross origin isolation to use features like SharedArrayBuffers.
    >
    > Bug: 1199491
    > Change-Id: I37aa94706939b16938e0395d9a8ffeb647afe086
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116905
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#915748}
    
    Bug: 1199491, 1244094
    Change-Id: I7859197befa6a12a0654a6746ed0daf0087d27e9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123032
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@google.com>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Cr-Commit-Position: refs/heads/main@{#916006}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -63,9 +63,8 @@ const base::Feature kStrictExtensionIsolation{"StrictExtensionIsolation",
 
 // Whether extension contexts can use SharedArrayBuffers unconditionally (i.e.
 // without requiring cross origin isolation).
-// TODO(crbug.com/1199491): Remove the feature in M97 if everything goes well.
+// TODO(crbug.com/1184892): Flip this in M95.
 const base::Feature kAllowSharedArrayBuffersUnconditionally{
-    "AllowSharedArrayBuffersUnconditionally",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    "AllowSharedArrayBuffersUnconditionally", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace extensions_features
