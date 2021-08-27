90836c3e6046c5093ccbe0013659b4bc11fe25cc
Junbo Ke <juke@chromium.org>
2021-08-27 21:22:31 +0000

    [Chromecast] Introduce audio_output_service::OutputStreamConnection.
    OutputStreamConnection is used to establish connection to the audio
    output service and send audio buffers and proto packets over sockets.
    
    Bug: b:173250111
    Test: Fling YT on x86 cast_shell
    Change-Id: Ic24afa5f509fcd6f3fd33cc91bae1602b748f359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108353
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916117}

---
+++ chromecast/media/audio/audio_output_service/output_connection.cc
@@ -0,0 +1,99 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/media/audio/audio_output_service/output_connection.h"
+
+#include <string>
+#include <utility>
+
+#include "base/bind.h"
+#include "base/check.h"
+#include "base/location.h"
+#include "base/logging.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "base/time/time.h"
+#include "chromecast/media/audio/audio_output_service/constants.h"
+#include "chromecast/media/audio/mixer_service/audio_socket_service.h"
+#include "chromecast/media/audio/mixer_service/mixer_socket.h"
+#include "net/base/net_errors.h"
+#include "net/socket/stream_socket.h"
+
+namespace chromecast {
+namespace media {
+namespace audio_output_service {
+
+namespace {
+
+constexpr base::TimeDelta kConnectTimeout = base::TimeDelta::FromSeconds(1);
+
+}  // namespace
+
+OutputConnection::OutputConnection() = default;
+
+OutputConnection::~OutputConnection() = default;
+
+void OutputConnection::Connect() {
+  DCHECK(!connecting_socket_);
+
+  connecting_socket_ = AudioSocketService::Connect(
+      kDefaultAudioOutputServiceUnixDomainSocketPath,
+      kDefaultAudioOutputServiceTcpPort);
+  int result = connecting_socket_->Connect(base::BindOnce(
+      &OutputConnection::ConnectCallback, weak_factory_.GetWeakPtr()));
+  if (result != net::ERR_IO_PENDING) {
+    ConnectCallback(result);
+    return;
+  }
+
+  connection_timeout_.Start(FROM_HERE, kConnectTimeout, this,
+                            &OutputConnection::ConnectTimeout);
+}
+
+void OutputConnection::ConnectCallback(int result) {
+  DCHECK_NE(result, net::ERR_IO_PENDING);
+
+  connection_timeout_.Stop();
+  if (result == net::OK) {
+    LOG_IF(INFO, !log_timeout_) << "Now connected to audio output service.";
+    log_connection_failure_ = true;
+    log_timeout_ = true;
+    auto socket = std::make_unique<mixer_service::MixerSocket>(
+        std::move(connecting_socket_));
+    OnConnected(std::move(socket));
+    return;
+  }
+
+  base::TimeDelta delay = kConnectTimeout;
+  if (log_connection_failure_) {
+    LOG(ERROR) << "Failed to connect to audio output service: " << result;
+    log_connection_failure_ = false;
+    delay = base::TimeDelta();  // No reconnect delay on the first failure.
+  }
+  connecting_socket_.reset();
+
+  base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&OutputConnection::Connect, weak_factory_.GetWeakPtr()),
+      delay);
+}
+
+void OutputConnection::ConnectTimeout() {
+  if (!connecting_socket_) {
+    return;
+  }
+
+  if (log_timeout_) {
+    LOG(ERROR) << "Timed out connecting to audio output service";
+    log_timeout_ = false;
+  }
+  connecting_socket_.reset();
+
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&OutputConnection::Connect, weak_factory_.GetWeakPtr()));
+}
+
+}  // namespace audio_output_service
+}  // namespace media
+}  // namespace chromecast
---
+++ chromecast/media/audio/audio_output_service/output_connection.h
@@ -0,0 +1,60 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_AUDIO_AUDIO_OUTPUT_SERVICE_OUTPUT_CONNECTION_H_
+#define CHROMECAST_MEDIA_AUDIO_AUDIO_OUTPUT_SERVICE_OUTPUT_CONNECTION_H_
+
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
+
+namespace net {
+class StreamSocket;
+}  // namespace net
+
+namespace chromecast {
+namespace media {
+
+namespace mixer_service {
+class MixerSocket;
+}  // namespace mixer_service
+
+namespace audio_output_service {
+
+// Base class for connecting to the audio output service.
+class OutputConnection {
+ public:
+  OutputConnection();
+  OutputConnection(const OutputConnection&) = delete;
+  OutputConnection& operator=(const OutputConnection&) = delete;
+  virtual ~OutputConnection();
+
+  // Initiates connection to the audio output service. Will call OnConnected()
+  // when connection is successful.
+  void Connect();
+
+ protected:
+  // Called when a connection is established to the audio output service.
+  virtual void OnConnected(
+      std::unique_ptr<mixer_service::MixerSocket> socket) = 0;
+
+ private:
+  void ConnectCallback(int result);
+  void ConnectTimeout();
+
+  std::unique_ptr<net::StreamSocket> connecting_socket_;
+  base::OneShotTimer connection_timeout_;
+
+  bool log_connection_failure_ = true;
+  bool log_timeout_ = true;
+
+  base::WeakPtrFactory<OutputConnection> weak_factory_{this};
+};
+
+}  // namespace audio_output_service
+}  // namespace media
+}  // namespace chromecast
+
+#endif  // CHROMECAST_MEDIA_AUDIO_AUDIO_OUTPUT_SERVICE_OUTPUT_CONNECTION_H_
