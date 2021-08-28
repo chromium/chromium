60b3796a421c3cf04f1d7ca90889145c38cb6ac5
Jimmy Gong <jimmyxgong@google.com>
2021-08-28 02:31:25 +0000

    Settings: Disable flaky CupsPrinterPageTest
    Will re-enable once flakiness is fixed.
    
    Bug: 1240970
    Change-Id: I5d17a3e1c49cfbc8974e9ba8ee9d338b91c7e1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125470
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Auto-Submit: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916201}

--- chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
@@ -369,7 +369,8 @@ TEST_F('OSSettingsOsBluetoothDeviceDetailSubpageV3Test', 'AllJsTests', () => {
  ['CrostiniPage', 'crostini_page_test.m.js'],
  ['CupsPrinterEntry', 'cups_printer_entry_tests.m.js'],
  ['CupsPrinterLandingPage', 'cups_printer_landing_page_tests.m.js'],
- ['CupsPrinterPage', 'cups_printer_page_tests.m.js'],
+ // TODO(crbug/1240970): Re-enable once flakiness is fixed.
+ // ['CupsPrinterPage', 'cups_printer_page_tests.m.js'],
  ['DarkModeSubpage', 'dark_mode_subpage_tests.m.js'],
  ['DateTimePage', 'date_time_page_tests.m.js'],
  ['EsimInstallErrorDialog', 'esim_install_error_dialog_test.m.js'],
