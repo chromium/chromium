edaa02beb153bd80552e8587e413429d17bcc458
Ken Rockot <rockot@google.com>
2021-08-30 19:32:57 +0000

    Viz: Fix destruction order for GMB alloc helper
    GpuClient::CreateGpuMemoryBuffer delegates down to the
    GpuMemoryBufferManager, and when doing so temporarily passes ownership
    of its callback into the async task sequence.
    
    On shutdown, if the task runner is destroyed before the GpuClient, this
    means these callbacks can be destroyed before their originating Recever
    (or in this case ReceiverSet) is unbound. This is an invalid condition
    and it will DCHECK.
    
    This change retains ownership of pending CreateGpuMemoryBuffer callbacks
    within the GpuClient instance itself, ensuring proper destruction order
    with respect to the ReceiverSet.
    
    Bug: 1230054, 1241723
    Change-Id: I092a8da354f459d302e1a13f1f8ac4b6f74d7b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3130140
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#916484}

--- components/viz/host/gpu_client.h
+++ components/viz/host/gpu_client.h
@@ -5,6 +5,7 @@
 #ifndef COMPONENTS_VIZ_HOST_GPU_CLIENT_H_
 #define COMPONENTS_VIZ_HOST_GPU_CLIENT_H_
 
+#include <map>
 #include <memory>
 
 #include "base/callback_forward.h"
@@ -87,13 +88,20 @@ class VIZ_HOST_EXPORT GpuClient : public mojom::GpuMemoryBufferFactory,
                              const gpu::GPUInfo& gpu_info,
                              const gpu::GpuFeatureInfo& gpu_feature_info,
                              GpuHostImpl::EstablishChannelStatus status);
-  void OnCreateGpuMemoryBuffer(CreateGpuMemoryBufferCallback callback,
+  void OnCreateGpuMemoryBuffer(gfx::GpuMemoryBufferId id,
                                gfx::GpuMemoryBufferHandle handle);
   void ClearCallback();
 
   std::unique_ptr<GpuClientDelegate> delegate_;
   const int client_id_;
   const uint64_t client_tracing_id_;
+
+  // Note that this map is placed before the ReceiverSet below, because pending
+  // response callbacks cannot be destroyed while their originating connection
+  // is still active.
+  std::map<gfx::GpuMemoryBufferId, CreateGpuMemoryBufferCallback>
+      pending_create_callbacks_;
+
   mojo::ReceiverSet<mojom::GpuMemoryBufferFactory>
       gpu_memory_buffer_factory_receivers_;
   mojo::ReceiverSet<mojom::Gpu> gpu_receivers_;
