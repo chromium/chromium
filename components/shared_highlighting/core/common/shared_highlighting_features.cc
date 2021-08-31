9ce21a6d0797ac30e0adb47727c6f3dc38c0474c
Cheick Cisse <cheickcisse@google.com>
2021-08-31 22:14:42 +0000

    [SH] Fix text Fragment Selector Generator EOL node bug.
    This CL fixes a crash in TextFragmentSelectorGenerator due to EOL node deprived of layout object.
    
    (cherry picked from commit b018d0fc6018096d03ee0d40c102ed80afc9c44a)
    
    Bug: 1233762
    Change-Id: If322f3cf1754a4a3536ce567e2b3db54dde9c792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3104011
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#915775}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134020
    Cr-Commit-Position: refs/branch-heads/4606@{#575}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

--- components/shared_highlighting/core/common/shared_highlighting_features.cc
+++ components/shared_highlighting/core/common/shared_highlighting_features.cc
@@ -22,6 +22,9 @@ const base::Feature kSharedHighlightingV2{"SharedHighlightingV2",
 const base::Feature kSharedHighlightingAmp{"SharedHighlightingAmp",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kSharedHighlightingLayoutObjectFix{
+    "SharedHighlightingLayoutObjectFix", base::FEATURE_ENABLED_BY_DEFAULT};
+
 int GetPreemptiveLinkGenTimeoutLengthMs() {
   return kPreemptiveLinkGenTimeoutLengthMs.Get();
 }
