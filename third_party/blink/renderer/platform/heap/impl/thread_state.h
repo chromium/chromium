5f6c9ef060029c594147bd78d02e621bf4b0814f
Peter Varga <pvarga@inf.u-szeged.hu>
2021-08-31 16:41:46 +0000

    heap: Do not cast WTF::GetCurrentStackPosition() to uintptr_t
    It already returns uintptr_t.
    
    This is a fixup of https://crrev.com/c/2000726
    
    Bug: none
    Change-Id: I9dbe57468955c0fa54c04be3f479fc3fc648abc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3132923
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916842}

--- third_party/blink/renderer/platform/heap/impl/thread_state.h
+++ third_party/blink/renderer/platform/heap/impl/thread_state.h
@@ -344,8 +344,7 @@ class PLATFORM_EXPORT ThreadState final {
 
   ALWAYS_INLINE bool IsOnStack(Address address) const {
     return reinterpret_cast<Address>(start_of_stack_) >= address &&
-           address >= (reinterpret_cast<Address>(reinterpret_cast<uintptr_t>(
-                          WTF::GetCurrentStackPosition())));
+           address >= reinterpret_cast<Address>(WTF::GetCurrentStackPosition());
   }
 
   MarkingVisitor* CurrentVisitor() const {
