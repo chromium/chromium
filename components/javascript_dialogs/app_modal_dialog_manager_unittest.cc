38407234d1c8c7904e9bc0bfd2c02657e3b77d3e
Avi Drissman <avi@chromium.org>
2021-08-31 23:26:20 +0000

    Use GetLastCommittedOrigin in Javascript dialogs
    GetLastCommittedOrigin more accurately allows us to track the origins
    of the alerting frame and report it to the user.
    
    Bug: 1241497
    Change-Id: Ie62462d3bccd5de7c3d9881781082795f43e3fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107569
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917027}

--- components/javascript_dialogs/app_modal_dialog_manager_unittest.cc
+++ components/javascript_dialogs/app_modal_dialog_manager_unittest.cc
@@ -7,92 +7,105 @@
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/origin.h"
 
 namespace javascript_dialogs {
 
 TEST(AppModalDialogManagerTest, GetTitle) {
   struct Case {
-    const char* parent_url;
-    const char* alerting_url;
+    // The name of the test case.
+    const char* case_name;
+
+    // The URL of the main frame of the page.
+    const char* main_frame_url;
+
+    // Whether the main frame is alerting.
+    bool is_main_frame;
+
+    // If `is_main_frame` is false, the URL of the alerting frame of the page.
+    const char* alerting_frame_url;
+
+    // The expected title for the alert.
     const char* expected;
-    const char* expected_android;
   } cases[] = {
       // Standard main frame alert.
-      {"http://foo.com/", "http://foo.com/", "foo.com says", "foo.com says"},
+      {"standard", "http://foo.com/", true, "", "foo.com says"},
 
       // Subframe alert from the same origin.
-      {"http://foo.com/1", "http://foo.com/2", "foo.com says", "foo.com says"},
+      {"subframe same origin", "http://foo.com/1", false, "http://foo.com/2",
+       "foo.com says"},
       // Subframe alert from a different origin.
-      {"http://foo.com/", "http://bar.com/", "An embedded page at bar.com says",
+      {"subframe different origin", "http://foo.com/", false, "http://bar.com/",
        "An embedded page at bar.com says"},
 
       // file:
       // - main frame:
-      {"file:///path/to/page.html", "file:///path/to/page.html",
-       "This page says", "This page says"},
+      {"file main frame", "file:///path/to/page.html", true, "",
+       "This page says"},
       // - subframe:
-      {"http://foo.com/", "file:///path/to/page.html",
-       "An embedded page on this page says",
+      {"file subframe", "http://foo.com/", false, "file:///path/to/page.html",
        "An embedded page on this page says"},
 
-      // ftp:
-      // - main frame:
-      {"ftp://foo.com/path/to/page.html", "ftp://foo.com/path/to/page.html",
-       "foo.com says", "ftp://foo.com says"},
-      // - subframe:
-      {"http://foo.com/", "ftp://foo.com/path/to/page.html",
-       "An embedded page at foo.com says",
-       "An embedded page at ftp://foo.com says"},
-
       // data:
+      // /!\ NOTE that this is for data URLs entered directly in the omnibox.
+      // For pages that generate frames with data URLs, see the browsertest.
       // - main frame:
-      {"data:blahblah", "data:blahblah", "This page says", "This page says"},
+      {"data main frame", "data:blahblah", true, "", "This page says"},
       // - subframe:
-      {"http://foo.com/", "data:blahblah", "An embedded page on this page says",
+      {"data subframe", "http://foo.com/", false, "data:blahblah",
        "An embedded page on this page says"},
 
       // javascript:
+      // /!\ NOTE that this is for javascript URLs entered directly in the
+      // omnibox. For pages that generate frames with javascript URLs, see the
+      // browsertest.
       // - main frame:
-      {"javascript:abc", "javascript:abc", "This page says", "This page says"},
+      {"javascript main frame", "javascript:abc", true, "", "This page says"},
       // - subframe:
-      {"http://foo.com/", "javascript:abc",
-       "An embedded page on this page says",
+      {"javascript subframe", "http://foo.com/", false, "javascript:abc",
        "An embedded page on this page says"},
 
       // about:
+      // /!\ NOTE that this is for about:blank URLs entered directly in the
+      // omnibox. For pages that generate frames with about:blank URLs, see the
+      // browsertest.
       // - main frame:
-      {"about:blank", "about:blank", "This page says", "This page says"},
+      {"about main frame", "about:blank", true, "", "This page says"},
       // - subframe:
-      {"http://foo.com/", "about:blank", "An embedded page on this page says",
+      {"about subframe", "http://foo.com/", false, "about:blank",
        "An embedded page on this page says"},
 
       // blob:
       // - main frame:
-      {"blob:http://foo.com/66666666-6666-6666-6666-666666666666",
-       "blob:http://foo.com/66666666-6666-6666-6666-666666666666",
-       "foo.com says", "foo.com says"},
+      {"blob main frame",
+       "blob:http://foo.com/66666666-6666-6666-6666-666666666666", true, "",
+       "foo.com says"},
       // - subframe:
-      {"http://bar.com/",
+      {"blob subframe", "http://bar.com/", false,
        "blob:http://foo.com/66666666-6666-6666-6666-666666666666",
-       "An embedded page at foo.com says", "An embedded page at foo.com says"},
+       "An embedded page at foo.com says"},
 
       // filesystem:
       // - main frame:
-      {"filesystem:http://foo.com/bar.html",
-       "filesystem:http://foo.com/bar.html", "foo.com says", "foo.com says"},
+      {"filesystem main frame", "filesystem:http://foo.com/bar.html", true, "",
+       "foo.com says"},
       // - subframe:
-      {"http://bar.com/", "filesystem:http://foo.com/bar.html",
-       "An embedded page at foo.com says", "An embedded page at foo.com says"},
+      {"filesystem subframe", "http://bar.com/", false,
+       "filesystem:http://foo.com/bar.html",
+       "An embedded page at foo.com says"},
   };
 
   for (const auto& test_case : cases) {
+    SCOPED_TRACE(test_case.case_name);
+    url::Origin main_frame_origin =
+        url::Origin::Create(GURL(test_case.main_frame_url));
+    url::Origin alerting_frame_origin =
+        test_case.is_main_frame
+            ? main_frame_origin
+            : url::Origin::Create(GURL(test_case.alerting_frame_url));
     std::u16string result = AppModalDialogManager::GetTitleImpl(
-        GURL(test_case.parent_url), GURL(test_case.alerting_url));
-#if defined(OS_ANDROID)
-    EXPECT_EQ(test_case.expected_android, base::UTF16ToUTF8(result));
-#else
+        main_frame_origin, alerting_frame_origin);
     EXPECT_EQ(test_case.expected, base::UTF16ToUTF8(result));
-#endif
   }
 }
 
