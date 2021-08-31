nwjs/chromium.src

commit 716e18423192709f88bce7766532ef4963c66508

[Extensions] declarative API function -> UIThreadExtensionFunction
AsynExtensionFunction is deprecated and will soon be removed.

Bug: 829174
Change-Id: I1201a74737fff8846f0847e1a2ae1c3963d96af9
Reviewed-on: https://chromium-review.googlesource.com/997099
Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
Cr-Commit-Position: refs/heads/master@{#548506}

extensions/browser/api/declarative/declarative_api.cc
@@ -139,29 +139,23 @@ RulesFunction::RulesFunction() {}
 
 RulesFunction::~RulesFunction() {}
 
-bool RulesFunction::HasPermission() {
+ExtensionFunction::ResponseAction RulesFunction::Run() {
+  EXTENSION_FUNCTION_VALIDATE(CreateParams());
+
   std::string event_name;
   EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &event_name));
+
   int web_view_instance_id = 0;
   EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(1, &web_view_instance_id));
 
   // <webview> embedders use the declarativeWebRequest API via
   // <webview>.onRequest.
-  if (web_view_instance_id && extension_->permissions_data()->HasAPIPermission(
+  if (web_view_instance_id && !extension_->permissions_data()->HasAPIPermission(
                                   APIPermission::kWebView)) {
-    return true;
+    return RespondNow(Error("Missing webview permission"));
   }
-  return ExtensionFunction::HasPermission();
-}
 
-bool RulesFunction::RunAsync() {
-  std::string event_name;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &event_name));
-
-  int web_view_instance_id = 0;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(1, &web_view_instance_id));
   int embedder_process_id = render_frame_host()->GetProcess()->GetID();
-
   RecordUMA(event_name);
 
   bool from_web_view = web_view_instance_id != 0;
@@ -189,43 +183,50 @@ bool RulesFunction::RunAsync() {
   // there should never be a request for a nonexisting rules registry.
   EXTENSION_FUNCTION_VALIDATE(rules_registry_.get());
 
-  if (content::BrowserThread::CurrentlyOn(rules_registry_->owner_thread())) {
-    bool success = RunAsyncOnCorrectThread();
-    SendResponse(success);
-  } else {
-    scoped_refptr<base::SingleThreadTaskRunner> thread_task_runner =
-        content::BrowserThread::GetTaskRunnerForThread(
-            rules_registry_->owner_thread());
-    base::PostTaskAndReplyWithResult(
-        thread_task_runner.get(), FROM_HERE,
-        base::Bind(&RulesFunction::RunAsyncOnCorrectThread, this),
-        base::Bind(&RulesFunction::SendResponse, this));
-  }
+  if (content::BrowserThread::CurrentlyOn(rules_registry_->owner_thread()))
+    return RespondNow(RunAsyncOnCorrectThread());
 
-  return true;
+  scoped_refptr<base::SingleThreadTaskRunner> thread_task_runner =
+      content::BrowserThread::GetTaskRunnerForThread(
+          rules_registry_->owner_thread());
+  base::PostTaskAndReplyWithResult(
+      thread_task_runner.get(), FROM_HERE,
+      base::BindOnce(&RulesFunction::RunAsyncOnCorrectThread, this),
+      base::BindOnce(&RulesFunction::SendResponse, this));
+  return RespondLater();
 }
 
-bool EventsEventAddRulesFunction::RunAsyncOnCorrectThread() {
+void RulesFunction::SendResponse(ResponseValue response) {
+  Respond(std::move(response));
+}
+
+EventsEventAddRulesFunction::EventsEventAddRulesFunction() = default;
+
+EventsEventAddRulesFunction::~EventsEventAddRulesFunction() = default;
+
+bool EventsEventAddRulesFunction::CreateParams() {
+  params_ = AddRules::Params::Create(*args_);
+  return params_ != nullptr;
+}
+
+ExtensionFunction::ResponseValue
+EventsEventAddRulesFunction::RunAsyncOnCorrectThread() {
   ConvertBinaryListElementsToBase64(args_.get());
-  std::unique_ptr<AddRules::Params> params(AddRules::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
 
   // TODO(devlin): Remove the dependency on linked_ptr here.
   std::vector<linked_ptr<api::events::Rule>> linked_rules;
-  for (api::events::Rule& rule : params->rules) {
+  for (api::events::Rule& rule : params_->rules) {
     linked_rules.push_back(
         make_linked_ptr(new api::events::Rule(std::move(rule))));
   }
-  error_ = rules_registry_->AddRules(extension_id(), linked_rules);
+  std::string error = rules_registry_->AddRules(extension_id(), linked_rules);
+  if (!error.empty())
+    return Error(error);
 
-  if (error_.empty()) {
-    std::unique_ptr<base::ListValue> rules_value(new base::ListValue());
-    for (const auto& rule : linked_rules)
-      rules_value->Append(rule->ToValue());
-    SetResult(std::move(rules_value));
-  }
-
-  return error_.empty();
+  auto rules_value = std::make_unique<base::ListValue>();
+  for (const auto& rule : linked_rules)
+    rules_value->Append(rule->ToValue());
+  return OneArgument(std::move(rules_value));
 }
 
 void EventsEventAddRulesFunction::RecordUMA(
@@ -248,19 +249,26 @@ void EventsEventAddRulesFunction::RecordUMA(
   RecordUMAHelper(type);
 }
 
-bool EventsEventRemoveRulesFunction::RunAsyncOnCorrectThread() {
-  std::unique_ptr<RemoveRules::Params> params(
-      RemoveRules::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+EventsEventRemoveRulesFunction::EventsEventRemoveRulesFunction() = default;
 
-  if (params->rule_identifiers.get()) {
-    error_ = rules_registry_->RemoveRules(extension_id(),
-                                          *params->rule_identifiers);
+EventsEventRemoveRulesFunction::~EventsEventRemoveRulesFunction() = default;
+
+bool EventsEventRemoveRulesFunction::CreateParams() {
+  params_ = RemoveRules::Params::Create(*args_);
+  return params_ != nullptr;
+}
+
+ExtensionFunction::ResponseValue
+EventsEventRemoveRulesFunction::RunAsyncOnCorrectThread() {
+  std::string error;
+  if (params_->rule_identifiers.get()) {
+    error = rules_registry_->RemoveRules(extension_id(),
+                                         *params_->rule_identifiers);
   } else {
-    error_ = rules_registry_->RemoveAllRules(extension_id());
+    error = rules_registry_->RemoveAllRules(extension_id());
   }
 
-  return error_.empty();
+  return error.empty() ? NoArguments() : Error(error);
 }
 
 void EventsEventRemoveRulesFunction::RecordUMA(
@@ -283,24 +291,29 @@ void EventsEventRemoveRulesFunction::RecordUMA(
   RecordUMAHelper(type);
 }
 
-bool EventsEventGetRulesFunction::RunAsyncOnCorrectThread() {
-  std::unique_ptr<GetRules::Params> params(GetRules::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+EventsEventGetRulesFunction::EventsEventGetRulesFunction() = default;
 
+EventsEventGetRulesFunction::~EventsEventGetRulesFunction() = default;
+
+bool EventsEventGetRulesFunction::CreateParams() {
+  params_ = GetRules::Params::Create(*args_);
+  return params_ != nullptr;
+}
+
+ExtensionFunction::ResponseValue
+EventsEventGetRulesFunction::RunAsyncOnCorrectThread() {
   std::vector<linked_ptr<Rule> > rules;
-  if (params->rule_identifiers.get()) {
-    rules_registry_->GetRules(
-        extension_id(), *params->rule_identifiers, &rules);
+  if (params_->rule_identifiers.get()) {
+    rules_registry_->GetRules(extension_id(), *params_->rule_identifiers,
+                              &rules);
   } else {
     rules_registry_->GetAllRules(extension_id(), &rules);
   }
 
-  std::unique_ptr<base::ListValue> rules_value(new base::ListValue());
+  auto rules_value = std::make_unique<base::ListValue>();
   for (const auto& rule : rules)
     rules_value->Append(rule->ToValue());
-  SetResult(std::move(rules_value));
-
-  return true;
+  return OneArgument(std::move(rules_value));
 }
 
 void EventsEventGetRulesFunction::RecordUMA(

extensions/browser/api/declarative/declarative_api.h
@@ -13,7 +13,23 @@
 
 namespace extensions {
 
-class RulesFunction : public AsyncExtensionFunction {
+namespace api {
+namespace events {
+namespace Event {
+namespace AddRules {
+struct Params;
+}  // namespace AddRules
+namespace GetRules {
+struct Params;
+}  // namespace GetRules
+namespace RemoveRules {
+struct Params;
+}  // namespace RemoveRules
+}  // namespace Event
+}  // namespace events
+}  // namespace api
+
+class RulesFunction : public UIThreadExtensionFunction {
  public:
   RulesFunction();
 
@@ -21,54 +37,80 @@ class RulesFunction : public AsyncExtensionFunction {
   ~RulesFunction() override;
 
   // ExtensionFunction:
-  bool HasPermission() override;
-  bool RunAsync() override;
+  ResponseAction Run() override;
+
+  // Returns whether or not params creation succeeded, the result is used to
+  // validate params.
+  virtual bool CreateParams() = 0;
 
   // Concrete implementation of the RulesFunction that is being called
   // on the thread on which the respective RulesRegistry lives.
   // Returns false in case of errors.
-  virtual bool RunAsyncOnCorrectThread() = 0;
+  virtual ResponseValue RunAsyncOnCorrectThread() = 0;
 
   // Records UMA metrics for the kind of declarative API call.
   virtual void RecordUMA(const std::string& event_name) const = 0;
 
   scoped_refptr<RulesRegistry> rules_registry_;
+
+ private:
+  void SendResponse(ResponseValue response);
+
+  DISALLOW_COPY_AND_ASSIGN(RulesFunction);
 };
 
 class EventsEventAddRulesFunction : public RulesFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("events.addRules", EVENTS_ADDRULES)
 
+  EventsEventAddRulesFunction();
+
  protected:
-  ~EventsEventAddRulesFunction() override {}
+  ~EventsEventAddRulesFunction() override;
 
   // RulesFunction:
-  bool RunAsyncOnCorrectThread() override;
+  bool CreateParams() override;
+  ResponseValue RunAsyncOnCorrectThread() override;
   void RecordUMA(const std::string& event_name) const override;
+
+ private:
+  std::unique_ptr<api::events::Event::AddRules::Params> params_;
 };
 
 class EventsEventRemoveRulesFunction : public RulesFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("events.removeRules", EVENTS_REMOVERULES)
 
+  EventsEventRemoveRulesFunction();
+
  protected:
-  ~EventsEventRemoveRulesFunction() override {}
+  ~EventsEventRemoveRulesFunction() override;
 
   // RulesFunction:
-  bool RunAsyncOnCorrectThread() override;
+  bool CreateParams() override;
+  ResponseValue RunAsyncOnCorrectThread() override;
   void RecordUMA(const std::string& event_name) const override;
+
+ private:
+  std::unique_ptr<api::events::Event::RemoveRules::Params> params_;
 };
 
 class EventsEventGetRulesFunction : public RulesFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("events.getRules", EVENTS_GETRULES)
 
+  EventsEventGetRulesFunction();
+
  protected:
-  ~EventsEventGetRulesFunction() override {}
+  ~EventsEventGetRulesFunction() override;
 
   // RulesFunction:
-  bool RunAsyncOnCorrectThread() override;
+  bool CreateParams() override;
+  ResponseValue RunAsyncOnCorrectThread() override;
   void RecordUMA(const std::string& event_name) const override;
+
+ private:
+  std::unique_ptr<api::events::Event::GetRules::Params> params_;
 };
 
 }  // namespace extensions

chromium/chromium

commit bb5c126bd6d5b62f002d8631d7249d143bb2cf08

[Extensions] Fix param handling in declarativeContent.SetIcon.
Change 716e18423192709f88bce7766532ef4963c66508 mistakenly removed
ExtensionFunction::params_ to pass through
ConvertBinaryListElementsToBase64. This CL fixes that.

This CL also expands SetIconAPITest.Overview test to add
regression test for the bug.

Bug: 1231027
Change-Id: I324382b881e1e97c04b311a0f6ee1da31291b9b1
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3130647
Reviewed-by: David Bertoni <dbertoni@chromium.org>
Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
Cr-Commit-Position: refs/heads/main@{#916640}

chrome/browser/extensions/api/declarative_content/set_icon_apitest.cc
@@ -8,6 +8,8 @@
 #include "components/version_info/version_info.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/browser/api/declarative/rules_registry.h"
+#include "extensions/browser/api/declarative/rules_registry_service.h"
 #include "extensions/browser/extension_action_manager.h"
 #include "extensions/common/features/feature_channel.h"
 #include "extensions/test/extension_test_message_listener.h"
@@ -33,6 +35,10 @@ const char kDeclarativeContentManifest[] =
     "  ]\n"
     "}\n";
 
+constexpr char kOneByOneImageData[] =
+    "GAAAAAAAAAAQAAAAAAAAADAAAAAAAAAAKAAAAAAAAAACAAAAAQAAAAEAAAAAAAAAAAAAAAAAAA"
+    "AAAAAAAAAAAAwAAAAEAAAAAAAAAAAAAAA=";
+
 class SetIconAPITest : public ExtensionApiTest {
  public:
   SetIconAPITest()
@@ -57,7 +63,7 @@ IN_PROC_BROWSER_TEST_F(SetIconAPITest, Overview) {
       "\n"
       "var canvas = document.createElement(\'canvas\');\n"
       "var ctx = canvas.getContext(\"2d\");"
-      "var imageData = ctx.createImageData(19,19);\n"
+      "var imageData = ctx.createImageData(1, 1);\n"
       "\n"
       "var rule0 = {\n"
       "  conditions: [new PageStateMatcher({\n"
@@ -84,6 +90,30 @@ IN_PROC_BROWSER_TEST_F(SetIconAPITest, Overview) {
   ASSERT_TRUE(action);
 
   ASSERT_TRUE(ready.WaitUntilSatisfied());
+
+  // Regression test for crbug.com/1231027.
+  {
+    scoped_refptr<RulesRegistry> rules_registry =
+        extensions::RulesRegistryService::Get(browser()->profile())
+            ->GetRulesRegistry(RulesRegistryService::kDefaultRulesRegistryID,
+                               "declarativeContent.onPageChanged");
+    ASSERT_TRUE(rules_registry);
+
+    std::vector<const api::events::Rule*> rules;
+    rules_registry->GetAllRules(extension->id(), &rules);
+    ASSERT_EQ(1u, rules.size());
+    ASSERT_EQ(rules[0]->actions.size(), 1u);
+
+    base::Value& action = *rules[0]->actions[0];
+    base::Value* action_instance_type = action.FindPath("instanceType");
+    ASSERT_TRUE(action_instance_type);
+    EXPECT_EQ("declarativeContent.SetIcon", action_instance_type->GetString());
+
+    base::Value* image_data_value = action.FindPath({"imageData", "1"});
+    ASSERT_TRUE(image_data_value);
+    EXPECT_EQ(kOneByOneImageData, image_data_value->GetString());
+  }
+
   content::WebContents* const tab =
       browser()->tab_strip_model()->GetWebContentsAt(0);
   const int tab_id = ExtensionTabUtil::GetTabId(tab);

extensions/browser/api/declarative/declarative_api.cc
@@ -205,14 +205,13 @@ EventsEventAddRulesFunction::EventsEventAddRulesFunction() = default;
 EventsEventAddRulesFunction::~EventsEventAddRulesFunction() = default;
 
 bool EventsEventAddRulesFunction::CreateParams() {
+  ConvertBinaryListElementsToBase64(mutable_args());
   params_ = AddRules::Params::Create(args());
   return params_ != nullptr;
 }
 
 ExtensionFunction::ResponseValue
 EventsEventAddRulesFunction::RunAsyncOnCorrectThread() {
-  ConvertBinaryListElementsToBase64(mutable_args());
-
   std::vector<const api::events::Rule*> rules_out;
   std::string error = rules_registry_->AddRules(
       extension_id(), std::move(params_->rules), &rules_out);