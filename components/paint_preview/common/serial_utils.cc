544dc83e638b7fd49e346b1bf81b8b2383ccd250
yoshiki iguchi <yoshiki@google.com>
2021-08-31 15:54:20 +0000

    paint_preview::PictureSerializationContext: Add noexcept to align the declarations
    The declarations of the move ctor and operator have "noexcept",
    but their impls don't have noexcept. That causes a compile
    failure on C++17 mode.
    
    Bug: 752720
    Test: compile
    
    Change-Id: I40851af520cc7a01f8f4490619d00d289632745e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3128266
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916822}

--- components/paint_preview/common/serial_utils.cc
+++ components/paint_preview/common/serial_utils.cc
@@ -177,9 +177,9 @@ PictureSerializationContext::PictureSerializationContext() = default;
 PictureSerializationContext::~PictureSerializationContext() = default;
 
 PictureSerializationContext::PictureSerializationContext(
-    PictureSerializationContext&&) = default;
+    PictureSerializationContext&&) noexcept = default;
 PictureSerializationContext& PictureSerializationContext::operator=(
-    PictureSerializationContext&&) = default;
+    PictureSerializationContext&&) noexcept = default;
 
 TypefaceSerializationContext::TypefaceSerializationContext(
     TypefaceUsageMap* usage)
