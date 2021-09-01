b3dd231386161c56142615dbca4a7fbea0ff2503
Dan McArdle <dmcardle@chromium.org>
2021-09-01 23:30:30 +0000

    Delete DeprecatedAtOrEmptyValue
    Bug: 1058527
    Change-Id: Ib742c9c829e35b038f93603080c7a8dcbb13784f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138633
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917441}

--- third_party/blink/renderer/platform/wtf/hash_map.h
+++ third_party/blink/renderer/platform/wtf/hash_map.h
@@ -158,10 +158,6 @@ class HashMap {
   bool Contains(KeyPeekInType) const;
   // Returns a reference to the mapped value. Crashes if no mapped value exists.
   MappedPeekType at(KeyPeekInType) const;
-  // Deprecated variant of at(). Created for refactor described in
-  // https://crbug.com/1058527. Returns a reference to the mapped value or the
-  // empty value if no mapped value exists.
-  MappedPeekType DeprecatedAtOrEmptyValue(KeyPeekInType) const;
 
   // replaces value but not key if key is already present return value is a
   // pair of the iterator to the key location, and a boolean that's true if a
@@ -594,20 +590,6 @@ HashMap<T, U, V, W, X, Y>::at(KeyPeekInType key) const {
   const ValueType* entry = impl_.Lookup(key);
   CHECK(entry) << "HashMap::at found no value for the given key. See "
                   "https://crbug.com/1058527.";
-  return MappedTraits::Peek(entry->value);
-}
-
-template <typename T,
-          typename U,
-          typename V,
-          typename W,
-          typename X,
-          typename Y>
-typename HashMap<T, U, V, W, X, Y>::MappedPeekType
-HashMap<T, U, V, W, X, Y>::DeprecatedAtOrEmptyValue(KeyPeekInType key) const {
-  const ValueType* entry = impl_.Lookup(key);
-  if (!entry)
-    return MappedTraits::Peek(MappedTraits::EmptyValue());
   return MappedTraits::Peek(entry->value);
 }
 
