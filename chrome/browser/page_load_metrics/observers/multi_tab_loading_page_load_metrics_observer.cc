0fab6233e599766d69502d03f9e8623f0d61edea
Darren W <djw@chromium.org>
2021-09-01 13:40:55 +0000

    Add an include of build/build_config.h to multi_tab_loading_page_load_metrics_observer. A presubmit is complaining about this while I'm making an unrelated change.
    Change-Id: I389285625c940f5c79e36bd55d05917451da35fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135074
    Auto-Submit: Darren W <djw@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917220}

--- chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "build/build_config.h"
 #include "chrome/browser/page_load_metrics/observers/histogram_suffixes.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "content/public/browser/web_contents.h"
