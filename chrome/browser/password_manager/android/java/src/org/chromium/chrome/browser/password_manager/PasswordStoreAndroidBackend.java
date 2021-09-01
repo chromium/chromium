719c10d0fb48e9bdacc491f9e7afcd343903585c
Friedrich Horschig <fhorschig@chromium.org>
2021-09-01 09:49:07 +0000

    [Android][Passwords] Get Passwords from ChromeSync
    This CL forwards requests to a downstream implementation and forwards
    the responses to the caller of GetLogins(), e.g. in the old settings UI.
    
    To achieve that, an interface is defined and instantiate by an abstract
    factory whose implementation lives downstream. The tests also live there
    since the entire android-specific implementation is unused in public
    builds that keep using the cross-platform, local store backend.
    
    In order to test or run this CL, you will need to include the API
    changes from https://crrev.com/i/4086348 and build with:
    
      enable_chrome_android_internal = true  # to fetch the API SDK
      use_signing_keys = true                # to talk to the 1st party API
    
    Change-Id: I8c56f08728eed42c4379d01ca8fcbba212254a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704560
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917169}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreAndroidBackend.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreAndroidBackend.java
@@ -5,21 +5,18 @@
 package org.chromium.chrome.browser.password_manager;
 
 import org.chromium.base.Callback;
-import org.chromium.base.task.PostTask;
-import org.chromium.base.task.TaskTraits;
-import org.chromium.components.sync.protocol.ListPasswordsResult;
 
 /**
- * Java implementation of the backend. Uses a Google Mobile Services API to fulfill password store
+ * Interface to send backend requests to a downstream implementation to fulfill password store
  * tasks. All methods are expected to respond asynchronously to callbacks.
  */
-class PasswordStoreAndroidBackend {
-    PasswordStoreAndroidBackend() {}
-
-    public void getAllLogins(Callback<byte[]> loginsReply) {
-        // TODO(crbug.com/1229654): Call actual API.
-        PostTask.postTask(TaskTraits.THREAD_POOL_USER_VISIBLE, () -> {
-            loginsReply.onResult(ListPasswordsResult.getDefaultInstance().toByteArray());
-        });
-    }
+public interface PasswordStoreAndroidBackend {
+    /**
+     * Triggers an async list call to retrieve all logins.
+     *
+     * @param loginsReply Callback that is called on success with serialized {@link
+     *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     */
+    void getAllLogins(Callback<byte[]> loginsReply, Callback<Exception> failureCallback);
 }
