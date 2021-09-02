89c9b1dd00fc7e6ca9afc1b247656bd0449abf8f
Nan Lin <linnan@chromium.org>
2021-09-02 16:08:47 +0000

    Implement the AggregatableReportSender
    This CL implements the AggregatableReportSender which is responsible for
    sending the aggregatable report to the reporting endpoint.
    
    This code is part of a new service in Chromium that deserves it's own
    metrics namespace. More histograms will be added in the future.
    
    Bug: 1239371
    Change-Id: Ibc70ecb89d460d92bfebfc8ca01fbdbd8671ecc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071541
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917687}

---
+++ content/browser/aggregation_service/aggregatable_report_sender.h
@@ -0,0 +1,88 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_AGGREGATION_SERVICE_AGGREGATABLE_REPORT_SENDER_H_
+#define CONTENT_BROWSER_AGGREGATION_SERVICE_AGGREGATABLE_REPORT_SENDER_H_
+
+#include <list>
+#include <memory>
+
+#include "base/callback_forward.h"
+#include "base/memory/scoped_refptr.h"
+#include "content/common/content_export.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+
+class GURL;
+
+namespace base {
+class Value;
+}  // namespace base
+
+namespace net {
+class HttpResponseHeaders;
+}  // namespace net
+
+namespace content {
+
+class StoragePartition;
+
+// This class is responsible for sending aggregatable reports to the reporting
+// endpoints over the network.
+class CONTENT_EXPORT AggregatableReportSender {
+ public:
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  enum class RequestStatus {
+    kOk = 0,
+    // Corresponds to a non-zero NET_ERROR indicating error occurred reading or
+    // consuming the response body.
+    kNetworkError = 1,
+    // Corresponds to a non-200 HTTP response code from the reporting endpoint.
+    kServerError = 2,
+    kMaxValue = kServerError,
+  };
+
+  explicit AggregatableReportSender(StoragePartition* storage_partition);
+  AggregatableReportSender(const AggregatableReportSender&) = delete;
+  AggregatableReportSender& operator=(const AggregatableReportSender&) = delete;
+  ~AggregatableReportSender();
+
+  // Callback used to notify caller that the requested report has been sent.
+  using ReportSentCallback = base::OnceCallback<void(RequestStatus)>;
+
+  // Sends an aggregatable report to the reporting endpoint `url`. This should
+  // generate a secure POST request with no-credentials.
+  void SendReport(const GURL& url,
+                  const base::Value& contents,
+                  ReportSentCallback callback);
+
+  // Tests inject a TestURLLoaderFactory so they can mock the network response.
+  // Also called by the aggregation service tool to inject a
+  // network::mojom::URLLoaderFactory.
+  void SetURLLoaderFactoryForTesting(
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
+
+ private:
+  // This is a std::list so that iterators remain valid during modifications.
+  using UrlLoaderList = std::list<std::unique_ptr<network::SimpleURLLoader>>;
+
+  // Called when headers are available for a sent report.
+  void OnReportSent(UrlLoaderList::iterator it,
+                    ReportSentCallback callback,
+                    scoped_refptr<net::HttpResponseHeaders> headers);
+
+  // Reports that are actively being sent.
+  UrlLoaderList loaders_in_progress_;
+
+  // Must outlive `this`.
+  StoragePartition* storage_partition_;
+
+  // Lazily accessed URLLoaderFactory used for network requests.
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_AGGREGATION_SERVICE_AGGREGATABLE_REPORT_SENDER_H_
