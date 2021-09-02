8d476b96335e8788505438e8fd4cad492aa7923d
Mitsuru Oshima <oshima@chromium.org>
2021-09-02 17:17:42 +0000

    Log clients that keep CompositorAnimationObserver for too long (>15s)
    This is reland of crrev.com/c/3095070.
    It simply logs on sanitizer builds.
    
    Bug: 1237981
    Change-Id: Ibfd3906ca613250eb900c7f0375e4d9793bb0c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138387
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917722}

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -595,6 +595,7 @@ void Compositor::AddAnimationObserver(CompositorAnimationObserver* observer) {
     for (auto& obs : observer_list_)
       obs.OnFirstAnimationStarted(this);
   }
+  observer->Start();
   animation_observer_list_.AddObserver(observer);
   host_->SetNeedsAnimate();
 }
@@ -603,6 +604,10 @@ void Compositor::RemoveAnimationObserver(
     CompositorAnimationObserver* observer) {
   if (!animation_observer_list_.HasObserver(observer))
     return;
+
+  for (auto& aobs : animation_observer_list_)
+    aobs.Check();
+
   animation_observer_list_.RemoveObserver(observer);
   if (animation_observer_list_.empty()) {
     for (auto& obs : observer_list_)
