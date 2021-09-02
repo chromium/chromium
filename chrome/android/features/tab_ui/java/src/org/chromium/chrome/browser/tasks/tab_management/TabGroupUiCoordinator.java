8854278003004c32a5ef36b3ccb3f088d7c6867a
Mei Liang <meiliang@chromium.org>
2021-09-02 21:16:18 +0000

    Fix NPE in TabGroupUiCoordinator
    TabGridDialogCoordinator can be null before native is loaded, we need to do a null check in the #getTabGridDialogVisibilitySupplier.
    
    Bug: 1199273
    Change-Id: I947fefb592818599f0ea3349c1091d08a6e4b573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139190
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917830}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiCoordinator.java
@@ -187,7 +187,7 @@ public class TabGroupUiCoordinator implements TabGroupUiMediator.ResetHandler, T
      */
     @Override
     public Supplier<Boolean> getTabGridDialogVisibilitySupplier() {
-        return mTabGridDialogCoordinator::isVisible;
+        return () -> mTabGridDialogCoordinator != null && mTabGridDialogCoordinator.isVisible();
     }
 
     /**
