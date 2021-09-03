148e624799f8ea6ace533446e0f85b7195ed1a61
Harry Cutts <hcutts@chromium.org>
2021-09-03 20:13:12 +0000

    Diagnostics: Split diagnostics-card content and frame
    For the input diagnostics, we're going to be using "cards" containing
    device lists (see go/cros-input-diag for the mocks), where the layout of
    the card "body" is significantly different to the ones used in the
    system and network pages. The look of the card's header, chip, and frame
    are still the same, though, so splitting those out into a separate
    component will allow code sharing.
    
    Bug: 1207678
    Test: Load the diagnostics UI and check it looks the same as before
    
    Change-Id: Ia73b0f1f8cf03fa501e74bed606ab20e2be22abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140870
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918221}

--- ash/webui/diagnostics_ui/resources/diagnostics_card.js
+++ ash/webui/diagnostics_ui/resources/diagnostics_card.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './diagnostics_card_frame.js';
 import './diagnostics_fonts_css.js';
 import './diagnostics_shared_css.js';
 
