0b03d74e7530aef437536858cc7386bd7addcbed
Ben Reich <benreich@chromium.org>
2021-09-03 05:03:26 +0000

    [FilesBanner] Add TrashBanner
    The TrashBanner shows when the user navigates to the Trash directory in
    Files app. It should always show and not be dismissable.
    
    Bug: 1228128
    Test: Compile and deploy to eve device, enable Trash and navigate to dir
    Change-Id: I1ac3786414c4278285600e2676222102c97fe893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135019
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917970}

--- ui/file_manager/file_manager/foreground/js/banner_controller.js
+++ ui/file_manager/file_manager/foreground/js/banner_controller.js
@@ -13,6 +13,7 @@ import {VolumeManager} from '../../externs/volume_manager.js';
 import {DirectoryModel} from './directory_model.js';
 import {TAG_NAME as DriveWelcomeBannerTagName} from './ui/banners/drive_welcome_banner.js';
 import {TAG_NAME as LocalDiskLowSpaceBannerTagName} from './ui/banners/local_disk_low_space_banner.js';
+import {TAG_NAME as TrashBannerTagName} from './ui/banners/trash_banner.js';
 
 /**
  * Local storage key suffix for how many times a banner was shown.
@@ -184,6 +185,7 @@ export class BannerController extends EventTarget {
       // denotes the priority of the banner, 0th index is highest priority.
       this.setWarningBannersInOrder([LocalDiskLowSpaceBannerTagName]);
       this.setEducationalBannersInOrder([DriveWelcomeBannerTagName]);
+      this.setStateBannersInOrder([TrashBannerTagName]);
     }
 
     for (const banner of this.warningBanners_) {
