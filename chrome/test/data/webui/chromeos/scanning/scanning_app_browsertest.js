c3266343f90b4f427e82810129a9edfe04707f6e
Gavin Williams <gavinwill@chromium.org>
2021-09-03 01:27:58 +0000

    scanning: Re-enable scanning browser tests
    Reverted the offending CL https://chromium-review.googlesource.com/c/chromium/src/+/3140242
    
    Bug: 1245885
    Change-Id: If18e0fee6527f5c2f8e313fc9bea5873795cb7c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140447
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917923}

--- chrome/test/data/webui/chromeos/scanning/scanning_app_browsertest.js
+++ chrome/test/data/webui/chromeos/scanning/scanning_app_browsertest.js
@@ -54,8 +54,7 @@ const debug_suites_list = [
   'SourceSelect'
 ];
 
-// ScanningAppBrowserTest.All is flaky, see https://crbug.com/1245885
-TEST_F('ScanningAppBrowserTest', 'DISABLED_All', function() {
+TEST_F('ScanningAppBrowserTest', 'All', function() {
   assertDeepEquals(
       debug_suites_list, test_suites_list,
       'List of registered tests suites and debug suites do not match.\n' +
