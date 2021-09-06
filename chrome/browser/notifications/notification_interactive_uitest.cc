d5f6f1587ee3e20c12f457ffbaf11be81cf2accd
Richard Knoll <knollr@chromium.org>
2021-09-06 12:21:42 +0000

    Re-enable TestCrashRendererNotificationRemain
    This test got disabled on Linux / Chrome OS 8 years ago but does not
    seem to be flaky when run locally or on the bots. Enabling it again to
    see if we can still reproduce these flakes as a lot has changed in the
    notification stack since then.
    
    Bug: 248470
    Change-Id: If060852ba755c6d5a6cce58dd92412e157d62f06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3143773
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918565}

--- chrome/browser/notifications/notification_interactive_uitest.cc
+++ chrome/browser/notifications/notification_interactive_uitest.cc
@@ -460,17 +460,7 @@ IN_PROC_BROWSER_TEST_F(NotificationsTest, TestCloseTabWithPermissionRequestUI) {
   destroyed_watcher.Wait();
 }
 
-// See crbug.com/248470
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
-#define MAYBE_TestCrashRendererNotificationRemain \
-    DISABLED_TestCrashRendererNotificationRemain
-#else
-#define MAYBE_TestCrashRendererNotificationRemain \
-    TestCrashRendererNotificationRemain
-#endif
-
-IN_PROC_BROWSER_TEST_F(NotificationsTest,
-                       MAYBE_TestCrashRendererNotificationRemain) {
+IN_PROC_BROWSER_TEST_F(NotificationsTest, TestCrashRendererNotificationRemain) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Test crashing renderer does not close or crash notification.
