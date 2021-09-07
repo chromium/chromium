de8b74ee7939e4b28ccd13fffdec7ee38c973c4f
Sebastien Lalancette <seblalancette@chromium.org>
2021-09-07 14:07:20 +0000

    [CAS] Run DeviceTrustBrowserTests on Linux and Mac
    Added dummy implementation for SigningKeyPair on both Linux and Mac to
    enable running the browser tests there as well.
    
    Bug: 1241857
    Change-Id: Ibba23e75473bf887cfbf26e37bbb53551e033195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134280
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918783}

--- chrome/browser/enterprise/connectors/device_trust/attestation/desktop/signing_key_pair_linux.cc
+++ chrome/browser/enterprise/connectors/device_trust/attestation/desktop/signing_key_pair_linux.cc
@@ -4,12 +4,46 @@
 
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/desktop/signing_key_pair.h"
 
+#include "base/notreached.h"
+#include "crypto/unexportable_key.h"
+
+using BPKUR = enterprise_management::BrowserPublicKeyUploadRequest;
+
 namespace enterprise_connectors {
+
+namespace {
+class SigningKeyPairLinux : public SigningKeyPair {
+ public:
+  // SigningKeyPair:
+  std::unique_ptr<crypto::UnexportableKeyProvider> GetTpmBackedKeyProvider()
+      override;
+  bool StoreKeyPair(KeyTrustLevel trust_level,
+                    std::vector<uint8_t> wrapped) override;
+  KeyInfo LoadKeyPair() override;
+};
+
+std::unique_ptr<crypto::UnexportableKeyProvider>
+SigningKeyPairLinux::GetTpmBackedKeyProvider() {
+  return crypto::GetUnexportableKeyProvider();
+}
+
+bool SigningKeyPairLinux::StoreKeyPair(KeyTrustLevel trust_level,
+                                       std::vector<uint8_t> wrapped) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+SigningKeyPair::KeyInfo SigningKeyPairLinux::LoadKeyPair() {
+  NOTIMPLEMENTED();
+  return {BPKUR::KEY_TRUST_LEVEL_UNSPECIFIED, std::vector<uint8_t>()};
+}
+
+}  // namespace
 
 // static
 std::unique_ptr<SigningKeyPair> SigningKeyPair::CreatePlatformKeyPair() {
-  // TODO(b/194891140)
-  return nullptr;
+  // TODO(b/194891140): Take care of the NOTIMPLEMENTED above.
+  return std::make_unique<SigningKeyPairLinux>();
 }
 
 }  // namespace enterprise_connectors
