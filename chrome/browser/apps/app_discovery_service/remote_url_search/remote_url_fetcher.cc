4a40b5a5ce413624386daf1374a94988998bd7df
Dominick Ng <dominickn@chromium.org>
2021-09-07 06:30:47 +0000

    Revert "[Remote URL search] Scaffolding for internal classes"
    This reverts commit 1cade7b90437e24682e38c4baf22e510f78cc770.
    
    Reason for revert: contains an undefined method that will cause linker errors
    
    Original change's description:
    > [Remote URL search] Scaffolding for internal classes
    >
    > This adds skeleton classes for the internal pieces of the remote url
    > search system.
    >
    > Bug: 1244221
    > Change-Id: If3e4010bc14ea508ef53df74bbb717e4a8de89eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125556
    > Reviewed-by: Rachel Wong <wrong@chromium.org>
    > Commit-Queue: Tony Yeoman <tby@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#918479}
    
    Bug: 1244221
    Change-Id: Ia9589c08a9ae413c89f689f88bd953b7bd345b28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144252
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918696}

--- chrome/browser/apps/app_discovery_service/remote_url_search/remote_url_fetcher.cc
+++ chrome/browser/apps/app_discovery_service/remote_url_search/remote_url_fetcher.cc
@@ -6,35 +6,13 @@
 
 #include "base/values.h"
 #include "chrome/browser/apps/app_discovery_service/app_discovery_features.h"
-#include "chrome/browser/apps/app_discovery_service/remote_url_search/remote_url_client.h"
-#include "chrome/browser/profiles/profile.h"
 
 namespace apps {
-namespace {
 
-constexpr char kIndexStoragePath[] = "launcher/remote_url_index.json";
-
-}
-
-RemoteUrlFetcher::RemoteUrlFetcher(Profile* profile) {
-  GURL url;
-
-  // TODO(crbug.com/1244221): Enabled state should also depend on whether we can
-  // find a url.
-  enabled_ = IsRemoteUrlSearchEnabled();
-  if (enabled_) {
-    index_ = std::make_unique<RemoteUrlIndex>(
-        std::make_unique<RemoteUrlClient>(url),
-        profile->GetPath().AppendASCII(kIndexStoragePath));
-  }
-}
-
-RemoteUrlFetcher::~RemoteUrlFetcher() {}
+RemoteUrlFetcher::RemoteUrlFetcher(Profile* profile) {}
 
 void RemoteUrlFetcher::GetApps(ResultCallback callback) {
-  // TODO(crbug.com/1244221): When available, add error handling for when
-  // featured is disabled
-  if (!enabled_) {
+  if (!IsRemoteUrlSearchEnabled()) {
     std::move(callback).Run(base::Value());
     return;
   }
