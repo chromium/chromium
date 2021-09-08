e18f0dcaf7613225d457ff676aa92626dc8e1ec0
Yoav Weiss <yoavweiss@chromium.org>
2021-09-08 08:33:30 +0000

    [resource-timing] Refactor object-not-found-adds-entry
    Change-Id: I428ab78f469652b4ec07c9773bfe1a0d0ea510a6
    Bug: 1171767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145213
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Tom McKee <tommckee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919153}

--- third_party/blink/web_tests/external/wpt/resource-timing/resources/resource-loaders.js
+++ third_party/blink/web_tests/external/wpt/resource-timing/resources/resource-loaders.js
@@ -92,6 +92,23 @@ const load = {
     document.body.removeChild(script);
   },
 
+  // Returns a promise that settles once the given path has been fetched as an
+  // object.
+  object: async (path, type) => {
+    const object = document.createElement("object");
+    const loaded = new Promise(resolve => {
+      object.onload = object.onerror = resolve;
+    });
+    object.data = load.cache_bust(path);
+    if (type) {
+      object.type = type;
+    }
+    object.style = "width: 0px; height: 0px";
+    document.body.appendChild(object);
+    await loaded;
+    document.body.removeChild(object);
+  },
+
   // Returns a promise that settles once the given path has been fetched
   // through a synchronous XMLHttpRequest.
   xhr_sync: async (path, headers) => {
