032607460c3c1055a5595f797bd3a25187085189
Roman Sorokin <rsorokin@chromium.org>
2021-09-08 10:39:15 +0000

    [ash-chrome] Migrate sync_internals_util off SplitSettingsSync
    Migrate to SyncSettingsCategorization and SyncConsentOptionalEnabled
    
    Bug: 1227693
    Change-Id: Id458b5ad93a7047671e5ccd378dedf2222db6fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3147396
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919196}

--- components/sync/driver/sync_internals_util.cc
+++ components/sync/driver/sync_internals_util.cc
@@ -481,12 +481,16 @@ std::unique_ptr<base::DictionaryValue> ConstructAboutInformation(
   transport_state->Set(GetTransportStateString(service->GetTransportState()));
   disable_reasons->Set(GetDisableReasonsString(service->GetDisableReasons()));
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  if (!chromeos::features::IsSplitSettingsSyncEnabled())
+  if (!chromeos::features::IsSyncSettingsCategorizationEnabled()) {
     os_feature_state->Set("Flag disabled");
-  else if (service->GetUserSettings()->IsOsSyncFeatureEnabled())
+  } else if (!chromeos::features::IsSyncConsentOptionalEnabled()) {
+    DCHECK(service->GetUserSettings()->IsOsSyncFeatureEnabled());
+    os_feature_state->Set("Enforced Enabled");
+  } else if (service->GetUserSettings()->IsOsSyncFeatureEnabled()) {
     os_feature_state->Set("Enabled");
-  else
+  } else {
     os_feature_state->Set("Disabled");
+  }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   feature_enabled->Set(service->IsSyncFeatureEnabled());
   setup_in_progress->Set(service->IsSetupInProgress());
