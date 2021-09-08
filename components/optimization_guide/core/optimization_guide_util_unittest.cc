7dcdc4b22ce6c28007cb10c974f11bb828dfb35a
Hiroki Nakagawa <nhiroki@chromium.org>
2021-09-08 04:53:20 +0000

    OptimizationGuide: Remove proto::DelayAsyncScriptExecutionMetadata
    The experiment of the DelayAsyncScriptExecution optimization had ended.
    We have no plans to proceed with the optimization, so this CL removes
    the proto file used for the optimization.
    
    In addition, this CL replaces DelayAsyncScriptExecutionMetadata with
    LoadingPredictorMetadata in the unit tests for parsing metadata. The
    tests don't care about types of metadata, so the CL author randomly
    chose LoadingPredictorMetadata.
    
    Bug: 1236832
    Change-Id: I227a79edb9c6ba5e6ff2f6c91c93a93ceb8e52c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3143232
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919105}

--- components/optimization_guide/core/optimization_guide_util_unittest.cc
+++ components/optimization_guide/core/optimization_guide_util_unittest.cc
@@ -4,7 +4,7 @@
 
 #include "components/optimization_guide/core/optimization_guide_util.h"
 
-#include "components/optimization_guide/proto/delay_async_script_execution_metadata.pb.h"
+#include "components/optimization_guide/proto/loading_predictor_metadata.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace optimization_guide {
@@ -12,45 +12,49 @@ namespace optimization_guide {
 TEST(OptimizationGuideUtilTest, ParsedAnyMetadataMismatchedTypeTest) {
   proto::Any any_metadata;
   any_metadata.set_type_url("type.googleapis.com/com.foo.Whatever");
-  proto::DelayAsyncScriptExecutionMetadata dase_metadata;
-  dase_metadata.set_delay_type(proto::DELAY_TYPE_FINISHED_PARSING);
-  dase_metadata.SerializeToString(any_metadata.mutable_value());
+  proto::LoadingPredictorMetadata metadata;
+  proto::Resource* subresource = metadata.add_subresources();
+  subresource->set_url("https://example.com/");
+  subresource->set_resource_type(proto::ResourceType::RESOURCE_TYPE_CSS);
+  subresource->set_preconnect_only(true);
+  metadata.SerializeToString(any_metadata.mutable_value());
 
-  absl::optional<proto::DelayAsyncScriptExecutionMetadata>
-      parsed_dase_metadata =
-          ParsedAnyMetadata<proto::DelayAsyncScriptExecutionMetadata>(
-              any_metadata);
-  EXPECT_FALSE(parsed_dase_metadata.has_value());
+  absl::optional<proto::LoadingPredictorMetadata> parsed_metadata =
+      ParsedAnyMetadata<proto::LoadingPredictorMetadata>(any_metadata);
+  EXPECT_FALSE(parsed_metadata.has_value());
 }
 
 TEST(OptimizationGuideUtilTest, ParsedAnyMetadataNotSerializableTest) {
   proto::Any any_metadata;
   any_metadata.set_type_url(
-      "type.googleapis.com/com.foo.DelayAsyncScriptExecutionMetadata");
+      "type.googleapis.com/com.foo.LoadingPredictorMetadata");
   any_metadata.set_value("12345678garbage");
 
-  absl::optional<proto::DelayAsyncScriptExecutionMetadata>
-      parsed_dase_metadata =
-          ParsedAnyMetadata<proto::DelayAsyncScriptExecutionMetadata>(
-              any_metadata);
-  EXPECT_FALSE(parsed_dase_metadata.has_value());
+  absl::optional<proto::LoadingPredictorMetadata> parsed_metadata =
+      ParsedAnyMetadata<proto::LoadingPredictorMetadata>(any_metadata);
+  EXPECT_FALSE(parsed_metadata.has_value());
 }
 
 TEST(OptimizationGuideUtilTest, ParsedAnyMetadataTest) {
   proto::Any any_metadata;
   any_metadata.set_type_url(
-      "type.googleapis.com/com.foo.DelayAsyncScriptExecutionMetadata");
-  proto::DelayAsyncScriptExecutionMetadata dase_metadata;
-  dase_metadata.set_delay_type(proto::DELAY_TYPE_FINISHED_PARSING);
-  dase_metadata.SerializeToString(any_metadata.mutable_value());
+      "type.googleapis.com/com.foo.LoadingPredictorMetadata");
+  proto::LoadingPredictorMetadata metadata;
+  proto::Resource* subresource = metadata.add_subresources();
+  subresource->set_url("https://example.com/");
+  subresource->set_resource_type(proto::ResourceType::RESOURCE_TYPE_CSS);
+  subresource->set_preconnect_only(true);
+  metadata.SerializeToString(any_metadata.mutable_value());
 
-  absl::optional<proto::DelayAsyncScriptExecutionMetadata>
-      parsed_dase_metadata =
-          ParsedAnyMetadata<proto::DelayAsyncScriptExecutionMetadata>(
-              any_metadata);
-  EXPECT_TRUE(parsed_dase_metadata.has_value());
-  EXPECT_EQ(parsed_dase_metadata->delay_type(),
-            proto::DELAY_TYPE_FINISHED_PARSING);
+  absl::optional<proto::LoadingPredictorMetadata> parsed_metadata =
+      ParsedAnyMetadata<proto::LoadingPredictorMetadata>(any_metadata);
+  EXPECT_TRUE(parsed_metadata.has_value());
+  ASSERT_EQ(parsed_metadata->subresources_size(), 1);
+  const proto::Resource& parsed_subresource = parsed_metadata->subresources(0);
+  EXPECT_EQ(parsed_subresource.url(), "https://example.com/");
+  EXPECT_EQ(parsed_subresource.resource_type(),
+            proto::ResourceType::RESOURCE_TYPE_CSS);
+  EXPECT_TRUE(parsed_subresource.preconnect_only());
 }
 
 }  // namespace optimization_guide
