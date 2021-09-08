ebd4ba3eb8549332deb4172898027b11eb67800f
Dave Tapuska <dtapuska@chromium.org>
2021-09-08 18:30:53 +0000

    Remove auto injected comment for IsInMainFrame->IsInPrimaryMainFrame.
    Using the primary main frame is correct here. Input and popups are
    not allowed for prerender so testing this is not possible for
    prerendering.
    
    BUG=1218946
    
    Change-Id: I694cc9dd6d03266080dc05086c07b89cad105df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149910
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919349}

--- content/browser/android/gesture_listener_manager.cc
+++ content/browser/android/gesture_listener_manager.cc
@@ -252,9 +252,6 @@ void GestureListenerManager::UpdateRenderProcessConnection(
 
 void GestureListenerManager::OnNavigationFinished(
     NavigationHandle* navigation_handle) {
-  // TODO(https://crbug.com/1218946): With MPArch there may be multiple main
-  // frames. This caller was converted automatically to the primary main frame
-  // to preserve its semantics. Follow up to confirm correctness.
   if (navigation_handle->IsInPrimaryMainFrame() &&
       navigation_handle->HasCommitted() &&
       !navigation_handle->IsSameDocument()) {
