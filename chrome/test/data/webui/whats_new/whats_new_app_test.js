0546d45669bb5a78f6f141a6145d9365146bac62
rbpotter <rbpotter@chromium.org>
2021-09-08 00:29:00 +0000

    What's New: Add a test case for browser command
    Bug: 1219381
    Change-Id: Ib80e71a4cf66b1be53af6b93a534a2b2aad2fb60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139244
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919014}

--- chrome/test/data/webui/whats_new/whats_new_app_test.js
+++ chrome/test/data/webui/whats_new/whats_new_app_test.js
@@ -2,43 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {CommandHandlerRemote} from 'chrome://resources/js/browser_command/browser_command.mojom-webui.js';
+import {BrowserCommandProxy} from 'chrome://resources/js/browser_command/browser_command_proxy.js';
 import {WhatsNewAppElement} from 'chrome://whats-new/whats_new_app.js';
 import {WhatsNewProxyImpl} from 'chrome://whats-new/whats_new_proxy.js';
 
 import {assertFalse, assertTrue} from '../chai_assert.js';
 import {TestBrowserProxy} from '../test_browser_proxy.js';
-import {flushTasks} from '../test_util.js';
+import {eventToPromise, flushTasks} from '../test_util.js';
 
 class TestWhatsNewProxy extends TestBrowserProxy {
   /**
-   * @param {boolean=} shouldLoad Whether initialize() should load the test
-   *     URL. Defaults to true.
+   * @param {?string} url The URL to load in the iframe or null to simulate a
+   *     load failure.
    */
-  constructor(shouldLoad = true) {
+  constructor(url) {
     super([
       'initialize',
     ]);
 
-    /** @private {boolean} */
-    this.shouldLoad_ = shouldLoad;
+    /** @private {?string} */
+    this.url_ = url;
   }
 
   /** @override */
   initialize(isAuto) {
     this.methodCalled('initialize', isAuto);
-    return this.shouldLoad_ ?
-        Promise.resolve('chrome://test/whats_new/test.html') :
-        Promise.resolve();
+    return Promise.resolve(this.url_);
   }
 }
 
 suite('WhatsNewAppTest', function() {
+  const whatsNewURL = 'chrome://test/whats_new/test.html';
+  const whatsNewWithCommandURL =
+      'chrome://test/whats_new/test_with_command_3.html';
+
   setup(function() {
     document.body.innerHTML = '';
   });
 
   test('with query parameter', async () => {
-    const proxy = new TestWhatsNewProxy();
+    const proxy = new TestWhatsNewProxy(whatsNewURL);
     WhatsNewProxyImpl.setInstance(proxy);
     window.history.replaceState({}, '', '?auto=true');
     const whatsNewApp = document.createElement('whats-new-app');
@@ -50,14 +54,14 @@ suite('WhatsNewAppTest', function() {
     const iframe = whatsNewApp.shadowRoot.querySelector('#content');
     assertTrue(!!iframe);
     // iframe has latest=true URL query parameter.
-    assertEquals('chrome://test/whats_new/test.html?latest=true', iframe.src);
+    assertEquals(whatsNewURL + '?latest=true', iframe.src);
     const errorPage =
         whatsNewApp.shadowRoot.querySelector('whats-new-error-page');
     assertFalse(!!errorPage);
   });
 
   test('no query parameter', async () => {
-    const proxy = new TestWhatsNewProxy();
+    const proxy = new TestWhatsNewProxy(whatsNewURL);
     WhatsNewProxyImpl.setInstance(proxy);
     window.history.replaceState({}, '', '/');
     const whatsNewApp = document.createElement('whats-new-app');
@@ -68,7 +72,7 @@ suite('WhatsNewAppTest', function() {
 
     const iframe = whatsNewApp.shadowRoot.querySelector('#content');
     assertTrue(!!iframe);
-    assertEquals('chrome://test/whats_new/test.html', iframe.src);
+    assertEquals(whatsNewURL, iframe.src);
     const errorPage =
         whatsNewApp.shadowRoot.querySelector('whats-new-error-page');
     assertFalse(!!errorPage);
@@ -76,7 +80,7 @@ suite('WhatsNewAppTest', function() {
 
   test('no query parameter failure', async () => {
     // Simulate a failure to load the remote content.
-    const proxy = new TestWhatsNewProxy(false);
+    const proxy = new TestWhatsNewProxy(null);
     WhatsNewProxyImpl.setInstance(proxy);
     window.history.replaceState({}, '', '/');
     const whatsNewApp = document.createElement('whats-new-app');
@@ -91,5 +95,26 @@ suite('WhatsNewAppTest', function() {
     const errorPage =
         whatsNewApp.shadowRoot.querySelector('whats-new-error-page');
     assertTrue(!!errorPage);
+  });
+
+  test('with command', async () => {
+    const proxy = new TestWhatsNewProxy(whatsNewWithCommandURL);
+    WhatsNewProxyImpl.setInstance(proxy);
+    const browserCommandHandler =
+        TestBrowserProxy.fromClass(CommandHandlerRemote);
+    BrowserCommandProxy.getInstance().handler = browserCommandHandler;
+    browserCommandHandler.setResultFor(
+        'canExecuteCommand', Promise.resolve({canExecute: true}));
+    window.history.replaceState({}, '', '/');
+    const whatsNewApp = document.createElement('whats-new-app');
+    document.body.appendChild(whatsNewApp);
+
+    const whenMessage = eventToPromise('message', window);
+    const commandId =
+        await browserCommandHandler.whenCalled('canExecuteCommand');
+    assertEquals(3, commandId);
+
+    const {data} = await whenMessage;
+    assertEquals(3, data.data.commandId);
   });
 });
