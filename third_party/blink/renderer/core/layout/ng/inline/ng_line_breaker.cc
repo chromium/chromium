bd37dd442ff769e53ee330f732e391409b0de62e
Kent Tamura <tkent@chromium.org>
2021-09-09 00:47:16 +0000

    Revert "Fix hang of MinMax for "shy&; + space" when `hyphens: none`"
    This reverts commit 59a7f921c2774d895adfbfea8a1478cde5794315.
    
    Reason for revert: Ssupected to cause crashes in HyphenationMinikin
    
    Original change's description:
    > Fix hang of MinMax for "shy&; + space" when `hyphens: none`
    >
    > The trailing space logic decrements the text offset, while
    > the logic to disable soft hyphens increments the text offset.
    > This patch fixes infinite loop when these two are combined.
    >
    > Bug: 1246965
    > Change-Id: Iaebcf95f66437854a9450e19c85140790f08ddde
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3143832
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#918656}
    
    Bug: 1246965, 1247610
    Change-Id: Ia1fb2d9cabe7ace9906bce5ec2c63894ad2c9d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150397
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Prudhvi Kumar Bommana <pbommana@google.com>
    Auto-Submit: Prudhvi Kumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/4636@{#9}
    Cr-Branched-From: c7e95945a5e198023f3c9675db533f77ec0ade7f-refs/heads/main@{#918973}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
@@ -1297,15 +1297,15 @@ bool NGLineBreaker::HandleTextForFastMinContent(NGInlineItemResult* item_result,
              IsBreakableSpace(text[non_hangable_run_end - 1])) {
         --non_hangable_run_end;
       }
+      end_offset = non_hangable_run_end;
     }
 
-    if (non_hangable_run_end >= item.EndOffset())
+    if (end_offset >= item.EndOffset())
       break;
 
     // Ignore soft-hyphen opportunities if `hyphens: none`.
     bool has_hyphen = text[non_hangable_run_end - 1] == kSoftHyphenCharacter;
-    if (UNLIKELY(has_hyphen && !enable_soft_hyphen_ &&
-                 non_hangable_run_end == end_offset)) {
+    if (has_hyphen && !enable_soft_hyphen_) {
       ++end_offset;
       if (end_offset < item.EndOffset())
         continue;
@@ -1359,8 +1359,14 @@ bool NGLineBreaker::HandleTextForFastMinContent(NGInlineItemResult* item_result,
     }
 
     last_end_offset = non_hangable_run_end;
+    // TODO (jfernandez): I think that having the non_hangable_run_end
+    // would make this loop unnecessary/redudant.
     start_offset = end_offset;
-    ++end_offset;
+    while (start_offset < item.EndOffset() &&
+           IsBreakableSpace(text[start_offset])) {
+      ++start_offset;
+    }
+    end_offset = start_offset + 1;
   }
 
   if (saved_line_break_type.has_value())
