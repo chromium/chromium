99d73c8e7bdbaa09354f77acd8ccdfaaf80f9807
Xinghui Lu <xinghuilu@chromium.org>
2021-09-09 21:16:46 +0000

    Reformat s_b/content/resources/PRESUBMIT.py
    git cl format --python
    
    Bug: 1245463
    Change-Id: I6e9f6904f75b1428555a273048af467271bae33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151410
    Auto-Submit: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919965}

--- components/safe_browsing/content/resources/PRESUBMIT.py
+++ components/safe_browsing/content/resources/PRESUBMIT.py
@@ -8,32 +8,35 @@
 USE_PYTHON3 = True
 
 def CheckVersionUpdatedInDownloadFileTypeList(input_api, output_api):
-  def IsDownloadFileTypeList(x):
-    return (input_api.os_path.basename(x.LocalPath()) ==
-            'download_file_types.asciipb')
+    def IsDownloadFileTypeList(x):
+        return (input_api.os_path.basename(
+            x.LocalPath()) == 'download_file_types.asciipb')
 
-  download_file_types = input_api.AffectedFiles(
-      file_filter=IsDownloadFileTypeList)
-  if not download_file_types:
-    return []
+    download_file_types = input_api.AffectedFiles(
+        file_filter=IsDownloadFileTypeList)
+    if not download_file_types:
+        return []
 
-  for _, line in download_file_types[0].ChangedContents():
-    if line.strip().startswith('version_id: '):
-      return []
+    for _, line in download_file_types[0].ChangedContents():
+        if line.strip().startswith('version_id: '):
+            return []
 
-  # It's enticing to do something fancy like checking whether the ID was in fact
-  # incremented or whether this is a whitespace-only or comment-only change.
-  # However, currently deleted lines don't show up in ChangedContents() and
-  # attempting to parse the asciipb file any more than we are doing above is
-  # likely not worth the trouble.
-  #
-  # At worst, the submitter can skip the presubmit check on upload if it isn't
-  # correct.
-  return [output_api.PresubmitError(
-      'Increment |version_id| in download_file_types.asciipb if you are '
-      'updating the file types proto.')]
+    # It's enticing to do something fancy like checking whether the ID was in
+    # fact incremented or whether this is a whitespace-only or comment-only
+    # change. However, currently deleted lines don't show up in
+    # ChangedContents() and attempting to parse the asciipb file any more than
+    # we are doing above is likely not worth the trouble.
+    #
+    # At worst, the submitter can skip the presubmit check on upload if it isn't
+    # correct.
+    return [
+        output_api.PresubmitError(
+            'Increment |version_id| in download_file_types.asciipb if you are '
+            'updating the file types proto.')
+    ]
+
 
 def CheckChangeOnUpload(input_api, output_api):
-  # TODO(asanka): Add a PRESUBMIT check for verifying that the
-  # download_file_types.asciipb file is valid.
-  return CheckVersionUpdatedInDownloadFileTypeList(input_api, output_api)
+    # TODO(asanka): Add a PRESUBMIT check for verifying that the
+    # download_file_types.asciipb file is valid.
+    return CheckVersionUpdatedInDownloadFileTypeList(input_api, output_api)
