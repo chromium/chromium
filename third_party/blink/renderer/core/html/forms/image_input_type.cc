6fe0403ea305b71150b25465f08f8dbb9b2ec084
Joey Arhar <jarhar@chromium.org>
2021-09-09 03:15:13 +0000

    Reland "Force DisplayLocks in <input type=image> layout APIs"
    Original patch: http://crrev.com/919246
    Revert patch: http://crrev.com/919305
    
    The original patch's test was flaky because it didn't wait for the load
    event when loading the image.
    
    Unfortunately, it would seem that <input type=image> doesn't fire load
    events within c-v:hidden subtrees like <img> does, which means that we
    have no signal to wait for the image to load. I have filed a separate
    issue for this, and will mark the test as timing out for now.
    https://bugs.chromium.org/p/chromium/issues/detail?id=1247844
    
    Bug: 1247417, 1247844
    Fixed: 1247714
    Change-Id: Ic76777857774cfa67d974e370358a1a720fa7025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149403
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919597}

--- third_party/blink/renderer/core/html/forms/image_input_type.cc
+++ third_party/blink/renderer/core/html/forms/image_input_type.cc
@@ -180,8 +180,8 @@ unsigned ImageInputType::Height() const {
     }
   }
 
-  GetElement().GetDocument().UpdateStyleAndLayout(
-      DocumentUpdateReason::kJavaScript);
+  GetElement().GetDocument().UpdateStyleAndLayoutForNode(
+      &GetElement(), DocumentUpdateReason::kJavaScript);
 
   LayoutBox* box = GetElement().GetLayoutBox();
   return box ? AdjustForAbsoluteZoom::AdjustInt(box->ContentHeight().ToInt(),
@@ -206,8 +206,8 @@ unsigned ImageInputType::Width() const {
     }
   }
 
-  GetElement().GetDocument().UpdateStyleAndLayout(
-      DocumentUpdateReason::kJavaScript);
+  GetElement().GetDocument().UpdateStyleAndLayoutForNode(
+      &GetElement(), DocumentUpdateReason::kJavaScript);
 
   LayoutBox* box = GetElement().GetLayoutBox();
   return box ? AdjustForAbsoluteZoom::AdjustInt(box->ContentWidth().ToInt(),
