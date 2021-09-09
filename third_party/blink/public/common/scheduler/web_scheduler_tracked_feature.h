1766d433dbe6ff25fe9a1bf7788edfe383ba7e61
Hajime Hoshi <hajimehoshi@chromium.org>
2021-09-09 05:05:06 +0000

    Add WebSchedulerTrackedFeature::WebTransport to enum.xml
    This change also moves the notice comment at WebSchedulerTrackedFeature
    definition to the end of the value definition, so that developers will
    more likely be aware of the necessity of syncing with enum.xml.
    
    Change-Id: I34d4e9eb77778eaeb8162bf389b750c05c3e487b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146718
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919635}

--- third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h
+++ third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h
@@ -17,9 +17,6 @@ namespace scheduler {
 // A list of features which influence scheduling behaviour (throttling /
 // freezing / back-forward cache) and which might be sent to the browser process
 // for metrics-related purposes.
-//
-// Please keep in sync with WebSchedulerTrackedFeature in
-// tools/metrics/histograms/enums.xml. These values should not be renumbered.
 enum class WebSchedulerTrackedFeature : uint32_t {
   kMinValue = 0,
   kWebSocket = 0,
@@ -111,6 +108,9 @@ enum class WebSchedulerTrackedFeature : uint32_t {
   kWebTransport = 57,
   // This should be used only for testing.
   kDummy = 58,
+
+  // Please keep in sync with WebSchedulerTrackedFeature in
+  // tools/metrics/histograms/enums.xml. These values should not be renumbered.
 
   // NB: This enum is used in a bitmask, so kMaxValue must be less than 64.
   kMaxValue = kDummy,
