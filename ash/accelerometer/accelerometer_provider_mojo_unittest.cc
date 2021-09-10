2ef14b4224ac64a4f45b8cd22cf224c51e0282be
Harvey Yang <chenghaoyang@chromium.org>
2021-09-10 09:25:04 +0000

    ash/accelerometer: Support hot-pluggable devices
    As hot-pluggable devices may cause disconnections of SensorDevice, we
    shouldn't reset other devices and SensorService usages.
    
    BUG=b:189998208
    TEST=unit tests and run on drallion
    
    Change-Id: I233981b6645828f4006192b18f8d9f58363a0be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936436
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920166}

--- ash/accelerometer/accelerometer_provider_mojo_unittest.cc
+++ ash/accelerometer/accelerometer_provider_mojo_unittest.cc
@@ -101,6 +101,7 @@ class AccelerometerProviderMojoTest : public ::testing::Test {
       sensor_device->SetAttribute(chromeos::sensors::mojom::kLocation,
                                   location.value());
 
+    sensor_devices_.emplace(iio_device_id, sensor_device.get());
     sensor_hal_server_->GetSensorService()->SetDevice(
         iio_device_id, std::move(types), std::move(sensor_device));
   }
@@ -136,6 +137,8 @@ class AccelerometerProviderMojoTest : public ::testing::Test {
 
   FakeObserver observer_;
   std::unique_ptr<chromeos::sensors::FakeSensorHalServer> sensor_hal_server_;
+  std::map<int32_t, chromeos::sensors::FakeSensorDevice*> sensor_devices_;
+
   scoped_refptr<AccelerometerProviderMojo> provider_;
 
   base::test::TaskEnvironment task_environment{
@@ -229,7 +232,7 @@ TEST_F(AccelerometerProviderMojoTest, GetSamplesWithNoLidAngle) {
 
   // Simulate a disconnection of the accelerometer's mojo channel in IIO
   // Service.
-  AddLidAccelerometer();
+  sensor_devices_[kFakeLidAccelerometerId]->ClearReceivers();
 
   // Wait until the disconnection is done.
   base::RunLoop().RunUntilIdle();
@@ -435,6 +438,52 @@ TEST_F(AccelerometerProviderMojoTest, NoSamplesFromLidAngle) {
   TriggerSamples();
 
   EXPECT_EQ(provider_->GetInitializationStateForTesting(), MojoState::ANGL_LID);
+
+  EXPECT_TRUE(observer_.update_.has(ACCELEROMETER_SOURCE_SCREEN));
+  EXPECT_FALSE(observer_.update_.has(ACCELEROMETER_SOURCE_ATTACHED_KEYBOARD));
+}
+
+TEST_F(AccelerometerProviderMojoTest, ResetStatesWithNoLidAngle) {
+  AddLidAccelerometer();
+  AddDevice(kFakeBaseAccelerometerId,
+            chromeos::sensors::mojom::DeviceType::ACCEL,
+            base::NumberToString(kFakeScaleValue),
+            kLocationStrings[ACCELEROMETER_SOURCE_ATTACHED_KEYBOARD]);
+
+  chromeos::sensors::SensorHalDispatcher::GetInstance()->RegisterServer(
+      sensor_hal_server_->PassRemote());
+
+  // Wait until samples are received.
+  base::RunLoop().RunUntilIdle();
+
+  // Simulate timeout to check |ec_lid_angle_driver_status_|.
+  TriggerNewDevicesTimeout();
+
+  EXPECT_TRUE(observer_.is_supported_.has_value());
+  EXPECT_FALSE(observer_.is_supported_.value());
+  EXPECT_EQ(provider_->GetInitializationStateForTesting(), MojoState::LID_BASE);
+
+  EXPECT_TRUE(observer_.update_.has(ACCELEROMETER_SOURCE_SCREEN));
+  EXPECT_TRUE(observer_.update_.has(ACCELEROMETER_SOURCE_ATTACHED_KEYBOARD));
+
+  observer_.update_.Reset();
+
+  // Simulate a disconnection of the base accelerometer's mojo channel in IIO
+  // Service with the reason of DEVICE_REMOVED.
+  sensor_devices_[kFakeBaseAccelerometerId]->ClearReceiversWithReason(
+      chromeos::sensors::mojom::SensorDeviceDisconnectReason::DEVICE_REMOVED,
+      "Device was removed");
+  // Overwrite the base accelerometer in the iioservice.
+  AddDevice(kFakeBaseAccelerometerId,
+            chromeos::sensors::mojom::DeviceType::ANGLVEL, absl::nullopt,
+            absl::nullopt);
+
+  // Wait until the disconnection and the re-initialization are done.
+  base::RunLoop().RunUntilIdle();
+
+  EXPECT_TRUE(observer_.is_supported_.has_value());
+  EXPECT_FALSE(observer_.is_supported_.value());
+  EXPECT_EQ(provider_->GetInitializationStateForTesting(), MojoState::LID);
 
   EXPECT_TRUE(observer_.update_.has(ACCELEROMETER_SOURCE_SCREEN));
   EXPECT_FALSE(observer_.update_.has(ACCELEROMETER_SOURCE_ATTACHED_KEYBOARD));
