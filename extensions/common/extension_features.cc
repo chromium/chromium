a26a5ea1776c54486722b55043a931b4677df1dd
Xinghui Lu <xinghuilu@chromium.org>
2021-09-10 00:21:28 +0000

    Enable Disable{PV/PU}ExtensionsRemotely by default.
    Bug: 1180996
    Change-Id: Ib16f247c1e7c9273e4e505c334fcabfd12ca28ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150290
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920033}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -11,13 +11,13 @@ namespace extensions_features {
 // by the Omaha attribute.
 const base::Feature kDisablePolicyViolationExtensionsRemotely{
     "DisablePolicyViolationExtensionsRemotely",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Controls whether we disable extensions that are marked as potentially
 // unwanted by the Omaha attribute.
 const base::Feature kDisablePotentiallyUwsExtensionsRemotely{
     "DisablePotentiallyUwsExtensionsRemotely",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Controls whether we show an install friction dialog when an Enhanced Safe
 // Browsing user tries to install an extension that is not included in the
