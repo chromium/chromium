80d4c9fbf17f757c060543ea90d1d68adf8b9cb5
Thomas Guilbert <tguilbert@chromium.org>
2021-09-10 18:01:47 +0000

    Re-enable CodecReclamation test
    This CL tentatively fixes a data race issue with ScopedFeatureList, in
    the DecoderTemplateTest test suite.
    
    Bug: 1247967
    Change-Id: Iaa4cf988fbd2042d849d78e6d298a3cc0bd744ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153222
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920282}

--- third_party/blink/renderer/modules/webcodecs/reclaimable_codec.cc
+++ third_party/blink/renderer/modules/webcodecs/reclaimable_codec.cc
@@ -4,7 +4,6 @@
 
 #include "third_party/blink/renderer/modules/webcodecs/reclaimable_codec.h"
 
-#include "base/feature_list.h"
 #include "base/location.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
