f5499f36528ef51d2c52ef1a6ae61ce958c721e6
Michael Checo <michaelcheco@google.com>
2021-09-13 17:54:42 +0000

    [navigation_view] Add shared navigation css variables
    Bug: 1205539
    Change-Id: I38f795b10669def4126364b13c620f8bfd64e47e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154169
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#920816}

--- ash/webui/common/resources/navigation_view_panel.js
+++ ash/webui/common/resources/navigation_view_panel.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './navigation_shared_vars.js';
 import './page_toolbar.js';
 
 import {assert} from 'chrome://resources/js/assert.m.js';
