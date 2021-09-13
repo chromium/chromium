024aec1d6bb70f71f83454f58e68676632f0a8da
Ionel Popescu <iopopesc@microsoft.com>
2021-09-13 23:58:49 +0000

    [EyeDropper API] Add optional AbortSignal parameter to EyeDropper.open
    Per TAG review[1], this CL adds an optional AbortSignal parameter to
    EyeDropper.open to allow authors to dismiss the eyedropper with no
    selection in case another high-priority event occurs.
    
    [1]: https://github.com/w3ctag/design-reviews/issues/587#issuecomment-911951900
    
    Bug: 1248286
    Change-Id: I15580609ece85f947f019a724b23b5e4898e42f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152761
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920997}

--- third_party/blink/renderer/modules/eyedropper/eye_dropper.cc
+++ third_party/blink/renderer/modules/eyedropper/eye_dropper.cc
@@ -7,16 +7,22 @@
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_color_selection_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_color_selection_result.h"
+#include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/platform/bindings/exception_code.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "ui/base/ui_base_features.h"
 
 namespace blink {
+
+constexpr char kAbortMessage[] = "Color selection aborted.";
+constexpr char kNotAvailableMessage[] = "EyeDropper is not available.";
 
 EyeDropper::EyeDropper(ExecutionContext* context)
     : eye_dropper_chooser_(context) {}
@@ -26,6 +32,7 @@ EyeDropper* EyeDropper::Create(ExecutionContext* context) {
 }
 
 ScriptPromise EyeDropper::open(ScriptState* script_state,
+                               const ColorSelectionOptions* options,
                                ExceptionState& exception_state) {
   DCHECK(RuntimeEnabledFeatures::EyeDropperAPIEnabled());
 
@@ -46,7 +53,7 @@ ScriptPromise EyeDropper::open(ScriptState* script_state,
 
   if (!features::IsEyeDropperEnabled()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kOperationError,
-                                      "EyeDropper is not available.");
+                                      kNotAvailableMessage);
     return ScriptPromise();
   }
 
@@ -54,6 +61,16 @@ ScriptPromise EyeDropper::open(ScriptState* script_state,
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       "EyeDropper is already open.");
     return ScriptPromise();
+  }
+
+  if (options->hasSignal()) {
+    if (options->signal()->aborted()) {
+      exception_state.ThrowDOMException(DOMExceptionCode::kAbortError,
+                                        kAbortMessage);
+      return ScriptPromise();
+    }
+    options->signal()->AddAlgorithm(
+        WTF::Bind(&EyeDropper::Abort, WrapWeakPersistent(this)));
   }
 
   resolver_ = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
@@ -72,6 +89,10 @@ ScriptPromise EyeDropper::open(ScriptState* script_state,
   return promise;
 }
 
+void EyeDropper::Abort() {
+  RejectPromiseHelper(DOMExceptionCode::kAbortError, kAbortMessage);
+}
+
 void EyeDropper::EyeDropperResponseHandler(ScriptPromiseResolver* resolver,
                                            bool success,
                                            uint32_t color) {
@@ -87,16 +108,21 @@ void EyeDropper::EyeDropperResponseHandler(ScriptPromiseResolver* resolver,
     result->setSRGBHex(Color(color).Serialized());
     resolver->Resolve(result);
   } else {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kAbortError, "The user canceled the selection."));
+    RejectPromiseHelper(DOMExceptionCode::kAbortError,
+                        "The user canceled the selection.");
   }
 }
 
 void EyeDropper::EndChooser() {
+  RejectPromiseHelper(DOMExceptionCode::kOperationError, kNotAvailableMessage);
+}
+
+void EyeDropper::RejectPromiseHelper(DOMExceptionCode exception_code,
+                                     const WTF::String& message) {
   eye_dropper_chooser_.reset();
   if (resolver_) {
-    resolver_->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kOperationError, "EyeDropper is not available."));
+    resolver_->Reject(
+        MakeGarbageCollected<DOMException>(exception_code, message));
     resolver_ = nullptr;
   }
 }
