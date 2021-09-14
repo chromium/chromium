06f99f1ad2c962daf2a5ba0485ac26d18bf99e8f
Vladimir Levin <vmpstr@chromium.org>
2021-09-14 00:05:32 +0000

    content-visibility: Add a clipper fix for content-visibility.
    This patch adds a few checks in the svg painting code which may access
    a content-visibility locked element via an svg reference.
    
    R=​fs@opera.com,jarhar@chromium.org
    
    (cherry picked from commit e0d8a4f20bf98bbda2dc58199fca5caf0add1b00)
    
    Bug: 1247196
    Change-Id: I4dcb4ef298fb8d51aa0ec1a3b3bc130cfb560791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149811
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#920209}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157400
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4638@{#27}
    Cr-Branched-From: 159257cab5585bc8421abf347984bb32fdfe9eb9-refs/heads/main@{#920003}

--- third_party/blink/renderer/core/paint/svg_mask_painter.cc
+++ third_party/blink/renderer/core/paint/svg_mask_painter.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/paint/svg_mask_painter.h"
 
+#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_resource_masker.h"
 #include "third_party/blink/renderer/core/layout/svg/svg_resources.h"
 #include "third_party/blink/renderer/core/paint/object_paint_properties.h"
@@ -46,7 +47,9 @@ void SVGMaskPainter::Paint(GraphicsContext& context,
   auto* masker = GetSVGResourceAsType<LayoutSVGResourceMasker>(
       *client, style.MaskerResource());
   DCHECK(masker);
-  SECURITY_DCHECK(!masker->NeedsLayout());
+  if (DisplayLockUtilities::LockedAncestorPreventingLayout(*masker))
+    return;
+  SECURITY_DCHECK(!masker->SelfNeedsLayout());
   masker->ClearInvalidationMask();
 
   FloatRect reference_box = SVGResources::ReferenceBoxForEffects(layout_object);
