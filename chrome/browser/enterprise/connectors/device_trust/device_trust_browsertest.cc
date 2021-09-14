6f69cec860296ebcf3d80f5b9f710810aaa37cfe
Sebastien Lalancette <seblalancette@chromium.org>
2021-09-14 18:33:53 +0000

    [CAS] Re-enable DeviceTrust BrowserTests
    Re-enabling the tests after having removed the SignalsReporter class
    which caused the flaky crashes during test teardowns.
    
    Fixed: 1246597, 1246534
    Change-Id: I10610e29f6a2efcc054c6cc11d19c41d613f497e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159549
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921333}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_browsertest.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_browsertest.cc
@@ -202,8 +202,7 @@ class DeviceTrustBrowserTest : public InProcessBrowserTest {
 
 // Tests that the whole attestation flow occurs when navigating to an allowed
 // domain.
-// TODO(crbug.com/1246534) Flaky test.
-IN_PROC_BROWSER_TEST_F(DeviceTrustBrowserTest, DISABLED_AttestationFullFlow) {
+IN_PROC_BROWSER_TEST_F(DeviceTrustBrowserTest, AttestationFullFlow) {
   GURL redirect_url = GetRedirectUrl();
   TestNavigationManager first_navigation(web_contents(), redirect_url);
 
@@ -238,9 +237,7 @@ IN_PROC_BROWSER_TEST_F(DeviceTrustBrowserTest, DISABLED_AttestationFullFlow) {
 
 // Tests that the attestation flow does not get triggered when navigating to a
 // domain that is not part of the allow-list.
-// TODO(crbug.com/1246597) Flaky test
-IN_PROC_BROWSER_TEST_F(DeviceTrustBrowserTest,
-                       DISABLED_AttestationHostNotAllowed) {
+IN_PROC_BROWSER_TEST_F(DeviceTrustBrowserTest, AttestationHostNotAllowed) {
   GURL navigation_url = GetDisallowedUrl();
   TestNavigationManager navigation_manager(web_contents(), navigation_url);
 
