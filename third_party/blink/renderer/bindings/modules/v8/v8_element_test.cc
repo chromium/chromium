85cdb065393290752ca5621cc45084581e7a848f
Kevin Babbitt <kbabbitt@microsoft.com>
2021-09-14 18:17:13 +0000

    Reland "Add tests for string refcount thrashing in setAttribute and getAttribute"
    This is a reland of 9ad27c21b9724230faa8ee18185772dcd9df8d75
    
    The previous CL failed builds on some targets due to missing
    DCHECK_IS_ON scoping. This CL adds the missing scoping.
    
    Original change's description:
    > Add tests for string refcount thrashing in setAttribute and getAttribute
    >
    > These APIs were previously identified as hot paths that exhibited
    > regressions when attempting to make StringImpl's refcount atomic. We'll
    > use these tests to drive down refcount thrashing on these paths and
    > ensure it stays low.
    >
    > Bug: 1083392
    > Change-Id: I5e92687b49cbf8da12f1676609e3f7755965530c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3117006
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#916479}
    
    Bug: 1083392
    Change-Id: Ibb9705b0d57c96950545e38bc24ec59c8e6954e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131461
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#921321}

---
+++ third_party/blink/renderer/bindings/modules/v8/v8_element_test.cc
@@ -0,0 +1,104 @@
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/bindings/core/v8/script_source_code.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/core/script/classic_script.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string_table.h"
+
+namespace blink {
+
+class V8ElementTest : public BindingTestSupportingGC {
+ protected:
+  void SetUp() override {
+    // Precondition: test strings should not be in the AtomicStringTable yet.
+    DCHECK(AtomicStringTable::Instance().WeakFind("test-attribute").IsNull());
+    DCHECK(AtomicStringTable::Instance().WeakFind("test-value").IsNull());
+  }
+
+  void TearDown() override {
+    PreciselyCollectGarbage();
+
+    // Postcondition: test strings should have been released from the
+    // AtomicStringTable
+    DCHECK(AtomicStringTable::Instance().WeakFind("test-attribute").IsNull());
+    DCHECK(AtomicStringTable::Instance().WeakFind("test-value").IsNull());
+  }
+};
+
+v8::Local<v8::Value> Eval(const String& source, V8TestingScope& scope) {
+  return ClassicScript::CreateUnspecifiedScript(ScriptSourceCode(source))
+      ->RunScriptAndReturnValue(&scope.GetWindow());
+}
+
+TEST_F(V8ElementTest, SetAttributeOperationCallback) {
+  V8TestingScope scope;
+
+  Eval("document.body.setAttribute('test-attribute', 'test-value')", scope);
+  EXPECT_FALSE(
+      AtomicStringTable::Instance().WeakFind("test-attribute").IsNull());
+  EXPECT_FALSE(AtomicStringTable::Instance().WeakFind("test-value").IsNull());
+
+#if DCHECK_IS_ON()
+  AtomicString test_attribute("test-attribute");
+  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 27u);
+  AtomicString test_value("test-value");
+  EXPECT_EQ(test_value.Impl()->RefCountChangeCountForTesting(), 15u);
+#endif
+
+  // Trigger a low memory notification. This will signal V8 to clear its
+  // CompilationCache, which is needed because cached compiled code may be
+  // holding references to externalized AtomicStrings.
+  scope.GetIsolate()->LowMemoryNotification();
+}
+
+TEST_F(V8ElementTest, GetAttributeOperationCallback_NonExisting) {
+  V8TestingScope scope;
+
+  Eval("document.body.getAttribute('test-attribute')", scope);
+  EXPECT_FALSE(
+      AtomicStringTable::Instance().WeakFind("test-attribute").IsNull());
+  EXPECT_TRUE(AtomicStringTable::Instance().WeakFind("test-value").IsNull());
+
+#if DCHECK_IS_ON()
+  AtomicString test_attribute("test-attribute");
+  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 14u);
+#endif
+
+  // Trigger a low memory notification. This will signal V8 to clear its
+  // CompilationCache, which is needed because cached compiled code may be
+  // holding references to externalized AtomicStrings.
+  scope.GetIsolate()->LowMemoryNotification();
+}
+
+TEST_F(V8ElementTest, GetAttributeOperationCallback_Existing) {
+  V8TestingScope scope;
+
+  Eval("document.body.setAttribute('test-attribute', 'test-value')", scope);
+  EXPECT_FALSE(
+      AtomicStringTable::Instance().WeakFind("test-attribute").IsNull());
+  EXPECT_FALSE(AtomicStringTable::Instance().WeakFind("test-value").IsNull());
+
+#if DCHECK_IS_ON()
+  AtomicString test_attribute("test-attribute");
+  test_attribute.Impl()->ResetRefCountChangeCountForTesting();
+  AtomicString test_value("test-value");
+  test_value.Impl()->ResetRefCountChangeCountForTesting();
+#endif
+
+  Eval("document.body.getAttribute('test-attribute')", scope);
+
+#if DCHECK_IS_ON()
+  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 9u);
+  EXPECT_EQ(test_value.Impl()->RefCountChangeCountForTesting(), 6u);
+#endif
+
+  // Trigger a low memory notification. This will signal V8 to clear its
+  // CompilationCache, which is needed because cached compiled code may be
+  // holding references to externalized AtomicStrings.
+  scope.GetIsolate()->LowMemoryNotification();
+}
+
+}  // namespace blink
