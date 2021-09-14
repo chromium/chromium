afcecb59e1fcb3c5de966280e99001b92c98ede6
Mike Dougherty <michaeldo@chromium.org>
2021-09-14 16:28:11 +0000

    Ignore building add to menu if no browser exists
    (cherry picked from commit 4192164e8404493e8df809c18f706847649f3c8b)
    
    Bug: 1247290
    Change-Id: I680d4e53c2bf2b66672c9ff58c3f9e359cd74792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151418
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#920116}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158552
    Owners-Override: Harry Souders <harrysouders@google.com>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4606@{#1030}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
@@ -481,6 +481,10 @@ web::WebState* GetWebStateWithId(WebStateList* web_state_list,
 
 - (NSArray<UIMenuElement*>*)addToButtonMenuElementsForItems:
     (NSArray<NSString*>*)items {
+  if (!self.browser) {
+    return nil;
+  }
+
   ActionFactory* actionFactory =
       [[ActionFactory alloc] initWithScenario:MenuScenario::kTabGridAddTo];
 
