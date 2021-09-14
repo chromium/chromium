f9fabfe84d977f538d5c954d1f71b3acfb9c99ee
Jonathan Ross <jonross@chromium.org>
2021-09-14 13:34:08 +0000

    Add Id to LayerTreeHostSize Trace
    We've been trying to debug cases where Renderers appear to have two
    different LocalSurfaceId EmbedTokens. We want to know if they are due to
    a disconnect, or if there are two separate LayerTreeHosts in the same
    Renderer.
    
    This adds the LayerTreeHost::id_ to the trace to debug that.
    
    Bug: 1243801
    Change-Id: Id4f6db97caee87254d7e91fcca81352a541f65d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139315
    Reviewed-by: Behdadb <behdadb@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921181}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1284,8 +1284,8 @@ void LayerTreeHost::SetViewportRectAndScale(
       local_surface_id_from_parent_;
   SetLocalSurfaceIdFromParent(local_surface_id_from_parent);
 
-  TRACE_EVENT_NESTABLE_ASYNC_END0("cc", "LayerTreeHostSize",
-                                  TRACE_ID_LOCAL(this));
+  TRACE_EVENT_NESTABLE_ASYNC_END1("cc", "LayerTreeHostSize",
+                                  TRACE_ID_LOCAL(this), "id", id_);
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN2("cc", "LayerTreeHostSize",
                                     TRACE_ID_LOCAL(this), "size",
                                     device_viewport_rect.ToString(), "lsid",
