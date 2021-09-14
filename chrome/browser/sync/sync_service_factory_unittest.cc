018b1868c6d605e1860d352a887b34827ac2b443
Boris Sazonov <bsazonov@chromium.org>
2021-09-14 10:59:48 +0000

    Migrate SyncServiceFactoryTest to SyncSettingsCategorization
    Replaces SplitSettingsSync with SyncSettingsCategorization in
    SyncServiceFactoryTest.
    
    Bug: 1227690
    Change-Id: If5fdfbbddbddb00953eb00bbbf14859a213317af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160201
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921144}

--- chrome/browser/sync/sync_service_factory_unittest.cc
+++ chrome/browser/sync/sync_service_factory_unittest.cc
@@ -116,7 +116,7 @@ class SyncServiceFactoryTest : public testing::Test {
     datatypes.push_back(syncer::APP_LIST);
     if (arc::IsArcAllowedForProfile(profile()))
       datatypes.push_back(syncer::ARC_PACKAGE);
-    if (chromeos::features::IsSplitSettingsSyncEnabled()) {
+    if (chromeos::features::IsSyncSettingsCategorizationEnabled()) {
       datatypes.push_back(syncer::OS_PREFERENCES);
       datatypes.push_back(syncer::OS_PRIORITY_PREFERENCES);
     }
