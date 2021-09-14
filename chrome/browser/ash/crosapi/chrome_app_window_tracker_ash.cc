71913446771cf08890ea859328bbcf895494d54b
Maggie Cai <mxcai@chromium.org>
2021-09-14 06:00:14 +0000

    Revert "Add unit test for ChromeAppWindowTrackerAsh."
    This reverts commit 8e93b3bfe67e092bf80dacd084e6e8736474919e.
    
    Reason for revert: This CL is likely the culprit for build failure in Builder Linux ChromiumOS MSan Tests starting from https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/24885.
    Failed tests: ChromeAppWindowTrackerAsh.FirstWindowThenApp
    ChromeAppWindowTrackerAsh.FirstAppThenWindow
    
    Original change's description:
    > Add unit test for ChromeAppWindowTrackerAsh.
    >
    > Bug: 1225848
    > Change-Id: If165ef53b18be5dc1a280611c8362a761b4bd606
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154720
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#920956}
    
    Bug: 1225848
    Change-Id: I8ce39f39293f6181142ee97fad248e15123bdb33
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159846
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921080}

--- chrome/browser/ash/crosapi/chrome_app_window_tracker_ash.cc
+++ chrome/browser/ash/crosapi/chrome_app_window_tracker_ash.cc
@@ -62,8 +62,23 @@ void ChromeAppWindowTrackerAsh::OnWindowDestroying(aura::Window* window) {
   pending_window_ids_.erase(it);
 }
 
-void ChromeAppWindowTrackerAsh::UpdateShelf(const std::string& app_id,
-                                            aura::Window* window) {
+void ChromeAppWindowTrackerAsh::CheckWindowNoLongerPending(
+    const std::string& window_id) {
+  auto pending_window = pending_window_ids_.find(window_id);
+  if (pending_window == pending_window_ids_.end())
+    return;
+
+  // The window is still pending
+  if (pending_window->second.app_id.empty() || !pending_window->second.window)
+    return;
+
+  std::string app_id = std::move(pending_window->second.app_id);
+  aura::Window* window = pending_window->second.window;
+
+  // Now that both pieces of metadata are available, we can stop tracking the
+  // window.
+  pending_window_ids_.erase(pending_window);
+
   ash::ShelfID shelf_id(app_id);
   ash::ShelfItemDelegate* existing_delegate =
       ash::ShelfModel::Get()->GetShelfItemDelegate(shelf_id);
@@ -85,26 +100,6 @@ void ChromeAppWindowTrackerAsh::UpdateShelf(const std::string& app_id,
             shelf_id, window);
     ash::ShelfModel::Get()->Add(item, std::move(delegate));
   }
-}
-
-void ChromeAppWindowTrackerAsh::CheckWindowNoLongerPending(
-    const std::string& window_id) {
-  auto pending_window = pending_window_ids_.find(window_id);
-  if (pending_window == pending_window_ids_.end())
-    return;
-
-  // The window is still pending
-  if (pending_window->second.app_id.empty() || !pending_window->second.window)
-    return;
-
-  std::string app_id = std::move(pending_window->second.app_id);
-  aura::Window* window = pending_window->second.window;
-
-  // Now that both pieces of metadata are available, we can stop tracking the
-  // window.
-  pending_window_ids_.erase(pending_window);
-
-  UpdateShelf(app_id, window);
 }
 
 }  // namespace crosapi
