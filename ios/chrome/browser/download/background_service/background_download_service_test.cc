73823413ab0d86bd74028129359de772d8794bba
Xing Liu <xingliu@google.com>
2021-09-14 22:35:19 +0000

    Background downoad: Add more integration tests on iOS.
    Adds the integration test to cover download failure and file clean up
    use cases.
    
    Bug: NONE
    Change-Id: If3694048806db30f76ba573b15753cdce9e9ee21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158829
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921444}

--- ios/chrome/browser/download/background_service/background_download_service_test.cc
+++ ios/chrome/browser/download/background_service/background_download_service_test.cc
@@ -16,11 +16,15 @@
 #include "ios/chrome/browser/download/background_service/background_download_service_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 using ::testing::NiceMock;
 
 namespace {
 const char kGuid[] = "broccoli";
+const base::FilePath::CharType kDownloadServiceStorageDir[] =
+    FILE_PATH_LITERAL("Download Service");
+const base::FilePath::CharType kFilesStorageDir[] = FILE_PATH_LITERAL("Files");
 
 // A background download service client used by this test.
 class FakeClient : public download::test::MockClient {
@@ -35,6 +39,10 @@ class FakeClient : public download::test::MockClient {
 
   const download::CompletionInfo* completion_info() const {
     return completion_info_.get();
+  }
+
+  const absl::optional<FailureReason>& failure_reason() const {
+    return failure_reason_;
   }
 
  private:
@@ -55,10 +63,22 @@ class FakeClient : public download::test::MockClient {
     run_loop_->Quit();
   }
 
+  void OnDownloadFailed(const std::string& guid,
+                        const download::CompletionInfo& completion_info,
+                        FailureReason reason) override {
+    download_guid_ = guid;
+    completion_info_ =
+        std::make_unique<download::CompletionInfo>(completion_info);
+    DCHECK(run_loop_);
+    run_loop_->Quit();
+    failure_reason_ = reason;
+  }
+
   std::unique_ptr<base::RunLoop> run_loop_;
   std::vector<download::DownloadMetaData> metadata_;
   std::string download_guid_;
   std::unique_ptr<download::CompletionInfo> completion_info_;
+  absl::optional<FailureReason> failure_reason_;
 };
 
 }  // namespace
@@ -78,6 +98,16 @@ class BackgroundDownloadServiceTest
     TestChromeBrowserState::Builder builder;
     browser_state_ = builder.Build();
 
+    // Create a random file in root dir and an unknown file in downoad dir.
+    ASSERT_TRUE(base::CreateTemporaryFileInDir(browser_state_->GetStatePath(),
+                                               &temp_file_path_));
+    auto download_dir = browser_state_->GetStatePath()
+                            .Append(kDownloadServiceStorageDir)
+                            .Append(kFilesStorageDir);
+    base::CreateDirectoryAndGetError(download_dir, nullptr);
+    ASSERT_TRUE(base::CreateTemporaryFileInDir(download_dir,
+                                               &temp_file_path_to_delete_));
+
     // Inject a fake client through SetTestingFactory.
     BackgroundDownloadServiceFactory* factory =
         BackgroundDownloadServiceFactory::GetInstance();
@@ -96,29 +126,57 @@ class BackgroundDownloadServiceTest
         browser_state_.get());
   }
 
-  void Download() {
+  // Download a file from embedded test server. Use different |relative_url| for
+  // different http responses.
+  void Download(const std::string& relative_url) {
     download::DownloadParams params;
     params.guid = kGuid;
     params.client = download::DownloadClient::TEST;
     params.request_params.method = "GET";
-    params.request_params.url = server_.GetURL(/*relative_url=*/"/test");
+    params.request_params.url = server_.GetURL(relative_url);
     service_->StartDownload(std::move(params));
   }
 
   FakeClient* client() { return fake_client_; }
+  const base::FilePath& temp_file_path() const { return temp_file_path_; }
+  const base::FilePath& temp_file_path_to_delete() const {
+    return temp_file_path_to_delete_;
+  }
 
  private:
   std::unique_ptr<ChromeBrowserState> browser_state_;
   download::BackgroundDownloadService* service_;
   FakeClient* fake_client_;
+  base::FilePath temp_file_path_;
+  base::FilePath temp_file_path_to_delete_;
 };
 
 // Verifies download can be finished.
 TEST_F(BackgroundDownloadServiceTest, DownloadComplete) {
-  Download();
+  Download("/test");
   client()->WaitForDownload();
   std::string content;
   ASSERT_TRUE(
       base::ReadFileToString(client()->completion_info()->path, &content));
   EXPECT_EQ(BackgroundDownloadTestBase::kDefaultResponseContent, content);
+}
+
+// Verifies download will fail when the server returns http 404 status code.
+TEST_F(BackgroundDownloadServiceTest, DownloadFailed) {
+  Download("/notfound");
+  client()->WaitForDownload();
+  EXPECT_TRUE(client()->failure_reason().has_value());
+  EXPECT_EQ(FakeClient::FailureReason::UNKNOWN, client()->failure_reason());
+}
+
+// Verifies that unknown files under download directory will be deleted.
+TEST_F(BackgroundDownloadServiceTest, FileCleanUp) {
+  // Make sure the service is initialized.
+  Download("/notfound");
+  client()->WaitForDownload();
+
+  EXPECT_TRUE(base::PathExists(temp_file_path()))
+      << "Files under browser state directory should not be deleted.";
+  EXPECT_FALSE(base::PathExists(temp_file_path_to_delete()))
+      << "Unknown files under download files directory should be deleted.";
 }
