04fd1ee6d48608b666d7c8e47780bb0aed462cdc
Scott Violet <sky@chromium.org>
2021-09-15 20:12:28 +0000

    chromeos: disables OAuth2Test.MergeSession as it is flaky
    This test was disabled on linux, but it doesn't run on linux, rather
    it runs on chromeos.
    
    BUG=1249687
    TEST=test only change
    
    Change-Id: I094dc3455dee5b9b9ae30a3373f426d9f55e2dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163437
    Commit-Queue: Scott Violet <sky@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921813}

--- chrome/browser/ash/login/signin/oauth2_browsertest.cc
+++ chrome/browser/ash/login/signin/oauth2_browsertest.cc
@@ -566,8 +566,8 @@ IN_PROC_BROWSER_TEST_F(OAuth2Test, PRE_MergeSession) {
 // MergeSession test is attempting to merge session for an existing profile
 // that was generated in PRE_PRE_MergeSession test. This attempt should fail
 // since FakeGaia instance isn't configured to return relevant tokens/cookies.
-// TODO(crbug.com/1249863): Test is flaky on Linux
-#if defined(OS_LINUX)
+// TODO(crbug.com/1249863): Test is flaky on chromeos
+#if defined(OS_CHROMEOS)
 #define MAYBE_MergeSession DISABLED_MergeSession
 #else
 #define MAYBE_MergeSession MergeSession
