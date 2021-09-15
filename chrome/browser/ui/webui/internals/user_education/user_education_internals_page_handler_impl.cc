70ed941ba8e37005dca1282fb4c6e87e70994e19
David Pennington <dpenning@chromium.org>
2021-09-15 19:58:39 +0000

    Remove Feature Tutorials code.
    To prepare clean CLs for Tutorials work, first the old FeatureTutorials
    logic is removed. This has no effect on any currently working systems,
    so this change is functionally a no-op.
    
    Change-Id: Idecd490cf0da97f75dfb30d693762ca73228af07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116686
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921808}

--- chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.cc
+++ chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.cc
@@ -4,36 +4,22 @@
 
 #include "chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h"
 
-#include "chrome/browser/ui/user_education/feature_tutorial_service.h"
-#include "chrome/browser/ui/user_education/feature_tutorial_service_factory.h"
-#include "chrome/browser/ui/user_education/feature_tutorials.h"
 #include "chrome/grit/dev_ui_browser_resources.h"
 #include "ui/base/webui/resource_path.h"
 
 UserEducationInternalsPageHandlerImpl::UserEducationInternalsPageHandlerImpl(
-    Profile* profile)
-    : tutorial_service_(FeatureTutorialServiceFactory::GetForProfile(profile)) {
-}
+    Profile* profile) {}
 
 UserEducationInternalsPageHandlerImpl::
     ~UserEducationInternalsPageHandlerImpl() = default;
 
 void UserEducationInternalsPageHandlerImpl::GetTutorials(
     GetTutorialsCallback callback) {
-  std::vector<base::StringPiece> id_pieces = GetAllFeatureTutorialStringIds();
-
   std::vector<std::string> ids;
-  for (base::StringPiece piece : id_pieces)
-    ids.emplace_back(piece.data(), piece.size());
-
   std::move(callback).Run(std::move(ids));
 }
 
 void UserEducationInternalsPageHandlerImpl::StartTutorial(
     const std::string& tutorial_id) {
-  absl::optional<FeatureTutorial> tutorial =
-      GetFeatureTutorialFromStringId(tutorial_id);
-  if (!tutorial)
-    return;
-  tutorial_service_->StartTutorial(*tutorial);
+  return;
 }
--- chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
+++ chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
@@ -9,8 +9,6 @@
 #include "chrome/browser/ui/webui/internals/user_education/user_education_internals.mojom.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-class FeatureTutorialService;
-
 class UserEducationInternalsPageHandlerImpl
     : public mojom::user_education_internals::
           UserEducationInternalsPageHandler {
@@ -26,9 +24,6 @@ class UserEducationInternalsPageHandlerImpl
   // mojom::user_education_internals::UserEducationInternalsPageHandler:
   void GetTutorials(GetTutorialsCallback callback) override;
   void StartTutorial(const std::string& tutorial_id) override;
-
- private:
-  FeatureTutorialService* const tutorial_service_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_INTERNALS_USER_EDUCATION_USER_EDUCATION_INTERNALS_PAGE_HANDLER_IMPL_H_
