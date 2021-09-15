bf18ee0bd63e0453eca7bb8673bc784e57188a59
Palak Agarwal <agpalak@chromium.org>
2021-09-15 13:13:22 +0000

    Fixing PanTiltZoom tests to be focused while calling getUserMedia
    Bug: 1163972
    Change-Id: I8cd03a6fe3f2a841eb2462c243a0b6a127983080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113015
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#921594}

--- chrome/test/data/extensions/api_test/video_capture/camera_pan_tilt_zoom/runtest.js
+++ chrome/test/data/extensions/api_test/video_capture/camera_pan_tilt_zoom/runtest.js
@@ -2,23 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.test.runTests([
-  async function cameraMovePermissionGrantsAccessToPanTiltZoom() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {pan: true, tilt: true, zoom: true},
-    });
-    chrome.test.assertTrue(!!stream);
-
-    const [videoTrack] = stream.getVideoTracks();
-    const settings = videoTrack.getSettings();
-    chrome.test.assertEq(100, settings.pan);
-    chrome.test.assertEq(100, settings.tilt);
-    chrome.test.assertEq(100, settings.zoom);
-
-    await videoTrack.applyConstraints({advanced: [{pan: 200}]});
-    await videoTrack.applyConstraints({advanced: [{tilt: 200}]});
-    await videoTrack.applyConstraints({advanced: [{zoom: 200}]});
-
-    chrome.test.succeed();
-  },
-]);
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('embedder.html', {}, function() {});
+});
--- chrome/test/data/extensions/api_test/video_capture/camera_pan_tilt_zoom_no_kiosk_session/runtest.js
+++ chrome/test/data/extensions/api_test/video_capture/camera_pan_tilt_zoom_no_kiosk_session/runtest.js
@@ -2,21 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.test.runTests([
-  // Test that the videoCapture permission does not grant access to pan, tilt,
-  // and zoom settings when extension is not running in kiosk mode.
-  async function videoCapturePermissionDoesNotGrantAccessOutsideOfKiosk() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {pan: true, tilt: true, zoom: true},
-    });
-    chrome.test.assertTrue(!!stream);
-
-    const [videoTrack] = stream.getVideoTracks();
-    const settings = videoTrack.getSettings();
-    chrome.test.assertFalse('pan' in settings);
-    chrome.test.assertFalse('tilt' in settings);
-    chrome.test.assertFalse('zoom' in settings);
-
-    chrome.test.succeed();
-  },
-]);
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('embedder.html', {}, function() {});
+});
