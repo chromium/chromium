64298a6046a2eff3a6b76381d968d10ca1b207d3
Shuran Huang <shuuran@chromium.org>
2021-09-15 15:31:15 +0000

    Bail out of fuzzing on problematic inputs.
    We don't fuzz inputs that contain URLs that don't have a host or a path
    separator, and whose path will eventually contain "..", since these URLs
    are not realistic and will cause the fuzzers to fail due to issue
    unrelated with the fuzzing purpose.
    
    Bug: 1237025
    Change-Id: If9c21b37737df2153bac4712511811e9e6eb8a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160738
    Auto-Submit: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921672}

--- services/network/first_party_sets/test/first_party_set_parser_fuzzer.cc
+++ services/network/first_party_sets/test/first_party_set_parser_fuzzer.cc
@@ -15,10 +15,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   base::StringPiece string_input(reinterpret_cast<const char*>(data), size);
   FirstPartySetParser::ParseSetsFromComponentUpdater(string_input);
 
+  // We deserialize -> serialize -> deserialize the input and make sure the
+  // outcomes from the two deserialization matches.
   base::flat_map<net::SchemefulSite, net::SchemefulSite> deserialized =
       FirstPartySetParser::DeserializeFirstPartySets(string_input);
   std::string serialized_input =
       FirstPartySetParser::SerializeFirstPartySets(deserialized);
+  // The inputs that have hosts contain more than one "." will cause
+  // SchemefulSite to consider the registrable domain to start with the last
+  // "." during the first deserialization; those hosts that start with '.'
+  // are serialized again and then result in empty registrable domain during the
+  // second deserialization.
+  //
+  // We don't run the fuzzer on inputs that are lossy due to URL parsing instead
+  // of the FirstPartySetsParser.
+  for (const auto& pair : deserialized) {
+    if (base::StartsWith(pair.first.GetInternalOriginForTesting().host(),
+                         ".") ||
+        base::StartsWith(pair.second.GetInternalOriginForTesting().host(),
+                         ".")) {
+      return 0;
+    }
+  }
   CHECK(deserialized ==
         FirstPartySetParser::DeserializeFirstPartySets(serialized_input));
 
