5d011bbd0e9710836347c750a88c2b5e02ef3b3e
Siyu An <siyua@chromium.org>
2021-09-15 18:44:04 +0000

    [VCN] Add CardUnmaskChallengeOption struct
    This struct will be used by Autofill Components to manage data related
    to a challenge option. The challenge option will be first parsed to a
    base::Value and then converted to this.
    
    
    Bug: 1243475
    Change-Id: Ic85eeb47164b3208717d24b662e1c54c0b19ec47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159198
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#921771}

---
+++ components/autofill/core/browser/payments/card_unmask_challenge_option.h
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_CARD_UNMASK_CHALLENGE_OPTION_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_CARD_UNMASK_CHALLENGE_OPTION_H_
+
+namespace autofill {
+
+// Indicates the type of challenge option used in card unmasking.
+enum class CardUnmaskChallengeOptionType {
+  // Default value, should never be used.
+  kUnknownType = 0,
+  // SMS OTP authentication.
+  kSmsOtp = 1,
+  kMaxValue = kSmsOtp,
+};
+
+// The struct used by Autofill components to represent a card unmask challenge
+// option.
+struct CardUnmaskChallengeOption {
+  CardUnmaskChallengeOption() = default;
+  ~CardUnmaskChallengeOption() = default;
+
+  // The unique identifier for the challenge option.
+  std::string id;
+
+  // The type of the challenge option.
+  CardUnmaskChallengeOptionType type;
+
+  // The user-facing text providing additional information for the challenge
+  // option, such as the masked phone number that will receive an SMS, etc.
+  std::u16string challenge_info;
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_CARD_UNMASK_CHALLENGE_OPTION_H_
