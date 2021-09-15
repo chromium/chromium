552a7ae2311322a4876ec330ad08ab14b9cb8e99
Dana Fried <dfried@chromium.org>
2021-09-15 20:27:37 +0000

    Show IPH close button in-line with text.
    This allows for the display of an IPH close button that does not force
    the text of the IPH bubble down by one line. Previously, it was assumed
    that bubbles with close buttons also had progress indicators, so space
    was allocated for the progress area if either progress or close button
    was present.
    
    Follow-up work:
     - determine if close button display should be contingent on the
       presence of other buttons rather than an independent configuration
       option
    
    Change-Id: I31f3785d80f8ff6c384f5519638b9f7227a37028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161325
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921827}

--- chrome/browser/ui/views/user_education/feature_promo_bubble_view.cc
+++ chrome/browser/ui/views/user_education/feature_promo_bubble_view.cc
@@ -4,7 +4,9 @@
 
 #include "chrome/browser/ui/views/user_education/feature_promo_bubble_view.h"
 
+#include <initializer_list>
 #include <memory>
+#include <numeric>
 #include <utility>
 
 #include "base/bind.h"
@@ -167,10 +169,6 @@ class CloseTutorialButton : public views::ImageButton {
         this,
         std::make_unique<views::CircleHighlightPathGenerator>(gfx::Insets()));
     views::InkDrop::Get(this)->SetBaseColor(kBubbleButtonHighlightColor);
-    SetProperty(views::kFlexBehaviorKey,
-                views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
-                                         views::MaximumFlexSizeRule::kUnbounded)
-                    .WithAlignment(views::LayoutAlignment::kEnd));
     SetAccessibleName(l10n_util::GetStringUTF16(IDS_CLOSE_TUTORIAL));
   }
 };
@@ -281,14 +279,11 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
   const SkColor text_color = theme_provider->GetColor(
       ThemeProperties::COLOR_FEATURE_PROMO_BUBBLE_TEXT);
 
-  // Add child views.
+  // Add progress indicator (optional) and its container.
   views::View* top_row_container = nullptr;
-  if (params.tutorial_progress_current || params.has_close_button)
-    top_row_container = AddChildView(std::make_unique<views::View>());
-
-  // Add progress indicator.
   if (params.tutorial_progress_current) {
     DCHECK(params.tutorial_progress_max);
+    top_row_container = AddChildView(std::make_unique<views::View>());
     // TODO(crbug.com/1197208): surface progress information in a11y tree
     for (int i = 0; i < params.tutorial_progress_max; ++i) {
       SkColor fill_color = i < params.tutorial_progress_current
@@ -300,6 +295,31 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
     }
   }
 
+  // Add body container views. If there is only text in the body, we won't
+  // bother adding these at all.
+
+  // The bubble body container is horizontal and contains the icon, the text,
+  // and the close button.
+  views::View* bubble_body_container = nullptr;
+  // The text container is vertical and sits inside the bubble body container.
+  // It contains the title and body text of the bubble.
+  views::View* text_container = nullptr;
+  if (params.body_icon || (params.has_close_button && !top_row_container)) {
+    bubble_body_container = AddChildView(std::make_unique<views::View>());
+    text_container =
+        bubble_body_container->AddChildView(std::make_unique<views::View>());
+  }
+
+  // Add the body icon (optional).
+  views::ImageView* icon_view = nullptr;
+  constexpr int kBodyIconSize = 24;
+  if (params.body_icon) {
+    icon_view = bubble_body_container->AddChildView(
+        std::make_unique<views::ImageView>(ui::ImageModel::FromVectorIcon(
+            *params.body_icon, text_color, kBodyIconSize)));
+  }
+
+  // This callback is used by both the close button and additional buttons.
   auto close_bubble_and_run_callback = [](FeaturePromoBubbleView* view,
                                           base::RepeatingClosure callback,
                                           const ui::Event& event) {
@@ -307,34 +327,20 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
     callback.Run();
   };
 
-  // Add close button.
+  // Add close button (optional).
+  CloseTutorialButton* close_button = nullptr;
   if (params.has_close_button) {
-    // Close button should only be used for tutorial use cases.
-    DCHECK(params.tutorial_progress_current);
-    top_row_container->AddChildView(
-        std::make_unique<CloseTutorialButton>(base::BindRepeating(
-            close_bubble_and_run_callback, base::Unretained(this),
-            params.dismiss_callback.has_value()
-                ? std::move(params.dismiss_callback.value())
-                : base::DoNothing())));
+    close_button =
+        (top_row_container ? top_row_container : bubble_body_container)
+            ->AddChildView(
+                std::make_unique<CloseTutorialButton>(base::BindRepeating(
+                    close_bubble_and_run_callback, base::Unretained(this),
+                    params.dismiss_callback.has_value()
+                        ? std::move(params.dismiss_callback.value())
+                        : base::DoNothing())));
   }
 
-  // Add body icon (optional) and associated containers.
-  views::View* outer_label_container = nullptr;
-  views::View* inner_label_container = nullptr;
-  views::ImageView* icon_view = nullptr;
-  constexpr int kBodyIconSize = 24;
-  if (params.body_icon) {
-    outer_label_container = AddChildView(std::make_unique<views::View>());
-    icon_view = outer_label_container->AddChildView(
-        std::make_unique<views::ImageView>(ui::ImageModel::FromVectorIcon(
-            *params.body_icon, text_color, kBodyIconSize)));
-    inner_label_container =
-        outer_label_container->AddChildView(std::make_unique<views::View>());
-  }
-
-  views::View* const label_parent =
-      inner_label_container ? inner_label_container : this;
+  views::View* const label_parent = text_container ? text_container : this;
 
   // Add title label.
   views::Label* title_label = nullptr;
@@ -358,7 +364,7 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
   body_label->SetMultiLine(true);
   body_label->SetElideBehavior(gfx::NO_ELIDE);
 
-  // Add buttons.
+  // Add other buttons.
   views::View* button_container = nullptr;
   if (!params.buttons.empty()) {
     button_container = AddChildView(std::make_unique<views::View>());
@@ -408,6 +414,24 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
         views::FlexSpecification(top_layout.GetDefaultFlexRule()));
   }
 
+  // Close button should float right in whatever container it's in.
+  if (close_button) {
+    close_button->SetProperty(
+        views::kFlexBehaviorKey,
+        views::FlexSpecification(views::LayoutOrientation::kHorizontal,
+                                 views::MinimumFlexSizeRule::kPreferred,
+                                 views::MaximumFlexSizeRule::kUnbounded)
+            .WithAlignment(views::LayoutAlignment::kEnd));
+    close_button->SetProperty(views::kMarginsKey,
+                              gfx::Insets(0, default_spacing, 0, 0));
+  }
+
+  // Icon view should have padding between it and the title or body label.
+  if (icon_view) {
+    icon_view->SetProperty(views::kMarginsKey,
+                           gfx::Insets(0, 0, 0, default_spacing));
+  }
+
   // Set label flex properties. This ensures that if the width of the bubble
   // maxes out the text will shrink on the cross-axis and grow to multiple
   // lines without getting cut off.
@@ -421,41 +445,27 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(CreateParams params)
   if (title_label)
     title_label->SetProperty(views::kFlexBehaviorKey, text_flex);
 
-  if (outer_label_container) {
+  if (bubble_body_container) {
     auto& outer_layout =
-        outer_label_container
+        bubble_body_container
             ->SetLayoutManager(std::make_unique<views::FlexLayout>())
             ->SetOrientation(views::LayoutOrientation::kHorizontal)
             .SetCrossAxisAlignment(views::LayoutAlignment::kStart)
             .SetIgnoreDefaultMainAxisMargins(true);
-    outer_label_container->SetProperty(
+    bubble_body_container->SetProperty(
         views::kFlexBehaviorKey,
         views::FlexSpecification(outer_layout.GetDefaultFlexRule()));
 
     auto& inner_layout =
-        inner_label_container
-            ->SetLayoutManager(std::make_unique<views::FlexLayout>())
+        text_container->SetLayoutManager(std::make_unique<views::FlexLayout>())
             ->SetOrientation(views::LayoutOrientation::kVertical)
             .SetCollapseMargins(true)
             .SetDefault(views::kMarginsKey,
                         gfx::Insets(0, 0, default_spacing, 0))
             .SetIgnoreDefaultMainAxisMargins(true);
-    inner_label_container->SetProperty(
+    text_container->SetProperty(
         views::kFlexBehaviorKey,
         views::FlexSpecification(inner_layout.GetDefaultFlexRule()));
-
-    // Align the midpoint of the first line of text with the icon.
-    const gfx::FontList& first_line_font =
-        title_label ? title_label->font_list() : body_label->font_list();
-    const int to_text_midpoint =
-        first_line_font.GetBaseline() - first_line_font.GetCapHeight() / 2;
-    const int icon_top_margin = to_text_midpoint - kBodyIconSize / 2;
-    icon_view->SetProperty(
-        views::kMarginsKey,
-        gfx::Insets(std::max(0, icon_top_margin), 0, 0, default_spacing));
-    inner_label_container->SetProperty(
-        views::kMarginsKey,
-        gfx::Insets(std::max(0, -icon_top_margin), 0, 0, 0));
   }
 
   // Set up button container layout.
