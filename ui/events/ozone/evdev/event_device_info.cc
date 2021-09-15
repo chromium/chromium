16a8bf4bbc83a15080f942c43edde40f98943dc7
Ting Shen <phoenixshen@google.com>
2021-09-15 13:14:29 +0000

    ozone: evdev: Add quirk to identify 0x18d1:0x5056/0x5057 as internal
    This base should be treated as an internal device.
    
    Bug: b:198583261
    Change-Id: Ia78ca11ea129da7ebb61f0d53a9eaa606bdab599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161867
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921596}

--- ui/events/ozone/evdev/event_device_info.cc
+++ ui/events/ozone/evdev/event_device_info.cc
@@ -618,6 +618,8 @@ ui::InputDeviceType EventDeviceInfo::GetInputDeviceTypeFromId(input_id id) {
       {0x18d1, 0x504c},  // Google, Zed PID (coachz)
       {0x18d1, 0x5050},  // Google, Don PID (katsu)
       {0x18d1, 0x5052},  // Google, Star PID (homestar)
+      {0x18d1, 0x5056},  // Google, bland PID (mrbland)
+      {0x18d1, 0x5057},  // Google, eel PID (wormdingler)
       {0x1fd2, 0x8103},  // LG, Internal TouchScreen PID
   };
 
