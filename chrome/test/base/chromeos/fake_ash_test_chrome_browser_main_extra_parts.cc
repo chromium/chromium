9445db61f50c836a9a86c9bddf61944895ffaa2a
Sven Zheng <svenzheng@chromium.org>
2021-09-16 04:07:33 +0000

    Ash is ready file in test_ash_chrome
    Adds a cmdline flag to pass in a file path. When ash
    is ready for testing, the file will be created.
    This cl needs to be merged to branches. Wait for a
    few days and then submit test_runner changes in
    crrev.com/c/3163782.
    
    Bug: 1107966
    Change-Id: I4736a0dd13cb5dfb9650ee80092e96b785668f93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163432
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921981}

--- chrome/test/base/chromeos/fake_ash_test_chrome_browser_main_extra_parts.cc
+++ chrome/test/base/chromeos/fake_ash_test_chrome_browser_main_extra_parts.cc
@@ -3,15 +3,41 @@
 // found in the LICENSE file.
 
 #include "fake_ash_test_chrome_browser_main_extra_parts.h"
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "chromeos/services/machine_learning/public/cpp/fake_service_connection.h"
 
 namespace test {
+
+// The file path to indicate if ash is ready for testing.
+// The file should not be on the file system initially. After
+// ash is ready for testing, the file will be created.
+constexpr char kAshReadyFilePathFlag[] = "ash-ready-file-path";
 
 FakeAshTestChromeBrowserMainExtraParts::
     FakeAshTestChromeBrowserMainExtraParts() = default;
 
 FakeAshTestChromeBrowserMainExtraParts::
     ~FakeAshTestChromeBrowserMainExtraParts() = default;
+
+// Create a file so test_runner know ash is ready for testing.
+void AshIsReadyForTesting() {
+  // TODO(crbug.com/1107966) Remove this early return after
+  // test_runner make related changes.
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          kAshReadyFilePathFlag)) {
+    return;
+  }
+
+  CHECK(
+      base::CommandLine::ForCurrentProcess()->HasSwitch(kAshReadyFilePathFlag));
+  base::FilePath path =
+      base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+          kAshReadyFilePathFlag);
+  CHECK(!base::PathExists(path));
+  CHECK(base::WriteFile(path, "ash is ready"));
+}
 
 void FakeAshTestChromeBrowserMainExtraParts::PostBrowserStart() {
   // Fake ML service is needed because ml service client library
@@ -22,6 +48,9 @@ void FakeAshTestChromeBrowserMainExtraParts::PostBrowserStart() {
   fake_service_connection->Initialize();
   chromeos::machine_learning::ServiceConnection::
       UseFakeServiceConnectionForTesting(fake_service_connection);
+
+  // Call this at the end of PostBrowserStart().
+  AshIsReadyForTesting();
 }
 
 }  // namespace test
