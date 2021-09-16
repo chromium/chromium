ea2f0ca1f367d50c2f1587f34ab0d2f7dd4dd89d
Pete Williamson <petewil@chromium.org>
2021-09-16 23:25:41 +0000

    Web Feed - Fix problem where status line was disappearing.
    If the status line was empty, we marked the control invisible.  However,
    that was persisting even if we reused the view inside the recycler view.
    The fix was for the code that removed it to put it back if it was not empty.
    
    Bug: b:199550940
    Change-Id: Ie30e643104cc9f6efc122f497c0f394d92b0b605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163424
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922354}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementItemView.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/followmanagement/FollowManagementItemView.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.feed.followmanagement;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.CheckBox;
@@ -41,7 +42,11 @@ public class FollowManagementItemView extends LinearLayout {
 
     public void setStatus(String status) {
         mStatus.setText(status);
-        if (status != null && status.isEmpty()) mStatus.setVisibility(View.GONE);
+        if (TextUtils.isEmpty(status)) {
+            mStatus.setVisibility(View.GONE);
+        } else {
+            mStatus.setVisibility(View.VISIBLE);
+        }
     }
 
     public void setFavicon(Bitmap favicon) {
