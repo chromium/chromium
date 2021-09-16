061595dd73f0fc22e8e2d18be0d007d6a0ccc17f
Mahmoud Gawad <mgawad@google.com>
2021-09-16 16:49:01 +0000

    Reland "[Telemetry] add features chromeos_system_extension"
    This is a reland of 4671685ce96a840c51e5ccf11b2a8dbfc6302cdf
    
    Original change's description:
    > [Telemetry] add features chromeos_system_extension
    >
    > Enable chromeos_system_extension type of extensions to define the
    > following manifest key: externally_connectable.
    >
    > Also allow the extension type to access the api: chrome.runtime.*.
    >
    > Bug: b:188897824, 1229585
    > Test: added browser_test in telemetry_extension_capabilities_browser_test.cc
    > Test: browser_tests --gtest_filter="*CanReceiveMessageExternal*" Green
    >
    > Change-Id: Ie2127565bb6d7084b5d5ff1ff27818381380ef21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114300
    > Commit-Queue: Mahmoud Gawad <mgawad@google.com>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Reviewed-by: Oleh Lamzin <lamzin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#920394}
    
    Bug: b:188897824, 1229585
    Test: browser_tests --gtest_filter="*CanReceiveMessageExternal*"
    Test: browser_tests --gtest_filter="*TelemetryExtension*ApiBrowserTest*"
    
    Change-Id: Ia356c06529874d02f35c73ebdc6c75b86954633f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158051
    Commit-Queue: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922177}

--- chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.h
+++ chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.h
@@ -9,6 +9,10 @@
 
 #include "chrome/browser/extensions/extension_browsertest.h"
 
+namespace extensions {
+class Extension;
+}  // namespace extensions
+
 namespace chromeos {
 
 class BaseTelemetryExtensionBrowserTest
@@ -26,6 +30,11 @@ class BaseTelemetryExtensionBrowserTest
   void SetUpOnMainThread() override;
 
  protected:
+  const extensions::Extension* LoadExtensionWithManifestAndServiceWorker(
+      const std::string& manifest_content,
+      const std::string& service_worker_content);
+  const extensions::Extension* LoadExtensionWithServiceWorker(
+      const std::string& service_worker_content);
   void CreateExtensionAndRunServiceWorker(
       const std::string& service_worker_content);
 };
---
+++ chrome/browser/chromeos/extensions/telemetry/api/telemetry_extension_capabilities_browser_test.cc
@@ -0,0 +1,94 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/stringprintf.h"
+#include "chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.h"
+#include "content/public/test/browser_test.h"
+#include "extensions/test/extension_test_message_listener.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace extensions {
+class Extension;
+}  // namespace extensions
+
+namespace chromeos {
+
+namespace {
+
+constexpr char kChromeOSSystemExtensionId[] =
+    "gogonhoemckpdpadfnjnpgbjpbjnodgc";
+constexpr char kNormalExtensionId[] = "adjjaddigjeahomddneilolepekgojjk";
+
+}  // namespace
+
+using TelemetryExtensionBrowserTest = BaseTelemetryExtensionBrowserTest;
+
+// Tests that chromeos_system_extension is able to define externally_connectable
+// manifest key and receive messages from another extension.
+IN_PROC_BROWSER_TEST_F(TelemetryExtensionBrowserTest,
+                       CanReceiveMessageExternal) {
+  // Start listening on the receiving extension.
+  ExtensionTestMessageListener listener(/*will_reply=*/false);
+  listener.set_extension_id(kChromeOSSystemExtensionId);
+
+  // Load and run the receiving extenion (chromeos_system_extension).
+  const extensions::Extension* receiver = LoadExtensionWithServiceWorker(
+    base::StringPrintf(R"(
+      chrome.test.runTests([
+        function runtimeOnMessageExternal() {
+          chrome.runtime.onMessageExternal.addListener(
+            (message, sender, sendResponse) => {
+              chrome.test.assertEq("%s", sender.id);
+              chrome.test.assertEq("ping", message);
+              sendResponse("pong");
+          });
+          chrome.test.sendMessage('ready');
+        }
+      ]);
+    )", kNormalExtensionId));
+  ASSERT_TRUE(receiver);
+
+  // Make sure the receiving extension is loaded first before loading the
+  // sending extension.
+  ASSERT_TRUE(listener.WaitUntilSatisfied());
+  EXPECT_EQ("ready", listener.message());
+
+  // Start listening on the sending extension.
+  listener.Reset();
+  listener.set_extension_id(kNormalExtensionId);
+
+  // Load and run the sending extension (normal extension).
+  const extensions::Extension* sender =
+      LoadExtensionWithManifestAndServiceWorker(R"(
+        {
+          // normal extension (id=adjjaddigjeahomddneilolepekgojjk)
+          "key": "MMIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2CwI94nqAQzLTBHSIwtkMlkoRyhu27rmkDsBneMprscOzl4524Y0bEA+0RSjNZB+kZgP6M8QAZQJHCpAzULXa49MooDDIdzzmqQswswguAALm2FS7XP2N0p2UYQneQce4Wehq/C5Yr65mxasAgjXgDWlYBwV3mgiISDPXI/5WG94TM2Z3PDQePJ91msDAjN08jdBme3hAN976yH3Q44M7cP1r+OWRkZGwMA6TSQjeESEuBbkimaLgPIyzlJp2k6jwuorG5ujmbAGFiTQoSDFBFCjwPEtywUMLKcZjH4fD76pcIQIdkuuzRQCVyuImsGzm1cmGosJ/Z4iyb80c1ytwIDAQAB",
+          "name": "Test Extension",
+          "version": "1",
+          "manifest_version": 3,
+          "background": {
+            "service_worker": "sw.js"
+          }
+        }
+      )",
+      base::StringPrintf(R"(
+        chrome.test.runTests([
+          function runtimeSendMessage() {
+            // send a message to chromeos_system_extension
+            chrome.runtime.sendMessage("%s", "ping",
+              (result) => {
+                chrome.test.assertEq("pong", result);
+                chrome.test.sendMessage('success');
+            });
+          }
+        ]);
+      )", kChromeOSSystemExtensionId));
+  ASSERT_TRUE(sender);
+
+  // Now wait until the sending extension receives a reply.
+  ASSERT_TRUE(listener.WaitUntilSatisfied());
+  EXPECT_EQ("success", listener.message());
+}
+
+}  // namespace chromeos
