50ac10eb3f5bcf9331492405ebfa52c5cba76b03
Ben Reich <benreich@chromium.org>
2021-09-16 09:29:11 +0000

    [FilesBanner] Migrate Crostini / PluginVM banners to new framework
    When a folder is shared with Crostini, PluginVM or both a banner appears
    highlighting the fact. These banners exist in the crostini_controller.js
    and this migrates them into the centralised framework.
    
    This allows for banner reconciliation to only show 1 banner at a time
    with the Crostini / PluginVM banners being shown as the lower priority
    (i.e. if no other banner has appeared).
    
    Bug: 1228128
    Test: ui/file_managaer:closure_compile
    Test: browser_tests --gtest_filter=FileManagerJsTest.Banner*
    Test: browser_tests --gtest_filter=*FilesApp*
    Test: Compile and flash DUT and share a folder with Linux.
    Change-Id: I6e5e368adba34702da1c14068b97f06f4470185d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144634
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922057}

--- ui/file_manager/file_manager/foreground/js/banner_controller.js
+++ ui/file_manager/file_manager/foreground/js/banner_controller.js
@@ -8,15 +8,18 @@ import {VolumeManagerCommon} from '../../common/js/volume_manager_types.js';
 import {xfm} from '../../common/js/xfm.js';
 import {Crostini} from '../../externs/background/crostini.js';
 import {Banner} from '../../externs/banner.js';
+import {FakeEntry, FilesAppDirEntry} from '../../externs/files_app_entry_interfaces.js';
 import {VolumeInfo} from '../../externs/volume_info.js';
 import {VolumeManager} from '../../externs/volume_manager.js';
 
+import {constants} from './constants.js';
 import {DirectoryModel} from './directory_model.js';
 import {TAG_NAME as DriveOfflinePinningBannerTagName} from './ui/banners/drive_offline_pinning_banner.js';
 import {TAG_NAME as DriveWelcomeBannerTagName} from './ui/banners/drive_welcome_banner.js';
 import {TAG_NAME as HoldingSpaceWelcomeBannerTagName} from './ui/banners/holding_space_welcome_banner.js';
 import {TAG_NAME as LocalDiskLowSpaceBannerTagName} from './ui/banners/local_disk_low_space_banner.js';
 import {TAG_NAME as PhotosWelcomeBannerTagName} from './ui/banners/photos_welcome_banner.js';
+import {TAG_NAME as SharedWithCrostiniPluginVmBanner} from './ui/banners/shared_with_crostini_pluginvm_banner.js';
 import {TAG_NAME as TrashBannerTagName} from './ui/banners/trash_banner.js';
 
 /**
@@ -119,6 +122,13 @@ export class BannerController extends EventTarget {
     this.currentRootType_ = null;
 
     /**
+     * Maintains the currently navigated directory entry. This is updated when
+     * a reconcile event is called.
+     * @private {?DirectoryEntry|?FakeEntry|?FilesAppDirEntry}
+     */
+    this.currentEntry_ = null;
+
+    /**
      * Maintains a cache of the current size for all observed volumes. If a
      * banner requests to observe a volumeType on initialization, the volume
      * size is cached here, keyed by volumeId.
@@ -218,7 +228,33 @@ export class BannerController extends EventTarget {
         DriveOfflinePinningBannerTagName,
         PhotosWelcomeBannerTagName,
       ]);
-      this.setStateBannersInOrder([TrashBannerTagName]);
+      this.setStateBannersInOrder([
+        SharedWithCrostiniPluginVmBanner,
+        TrashBannerTagName,
+      ]);
+
+      // Register custom filters that verify whether the currently navigated
+      // path is shared with Crostini, PluginVM or both.
+      this.registerCustomBannerFilter_(SharedWithCrostiniPluginVmBanner, {
+        shouldShow: () =>
+            isPathSharedWithVm(
+                this.crostini_, this.currentEntry_,
+                constants.DEFAULT_CROSTINI_VM) &&
+            isPathSharedWithVm(
+                this.crostini_, this.currentEntry_, constants.PLUGIN_VM),
+        context: () =>
+            ({type: constants.PLUGIN_VM + constants.DEFAULT_CROSTINI_VM}),
+      });
+      this.registerCustomBannerFilter_(SharedWithCrostiniPluginVmBanner, {
+        shouldShow: () => isPathSharedWithVm(
+            this.crostini_, this.currentEntry_, constants.DEFAULT_CROSTINI_VM),
+        context: () => ({type: constants.DEFAULT_CROSTINI_VM}),
+      });
+      this.registerCustomBannerFilter_(SharedWithCrostiniPluginVmBanner, {
+        shouldShow: () => isPathSharedWithVm(
+            this.crostini_, this.currentEntry_, constants.PLUGIN_VM),
+        context: () => ({type: constants.PLUGIN_VM}),
+      });
     }
 
     for (const banner of this.warningBanners_) {
@@ -266,8 +302,9 @@ export class BannerController extends EventTarget {
    */
   async reconcile() {
     const previousVolume = this.currentVolume_;
-    this.currentVolume_ = this.directoryModel_.getCurrentVolumeInfo();
+    this.currentEntry_ = this.directoryModel_.getCurrentDirEntry();
     this.currentRootType_ = this.directoryModel_.getCurrentRootType();
+    this.currentVolume_ = this.directoryModel_.getCurrentVolumeInfo();
 
     // When navigating to a different volume, refresh the volume size stats
     // when first navigating. A listener will keep this in sync.
@@ -807,4 +844,22 @@ async function getSizeStats(volumeId) {
   return new Promise((resolve) => {
     chrome.fileManagerPrivate.getSizeStats(volumeId, resolve);
   });
+}
+
+/**
+ * Identifies if a supplied Entry is shared with a particularly VM. Returns a
+ * curried function that takes the vm type.
+ * @param {!Crostini} crostini
+ * @param {?DirectoryEntry|?FakeEntry|?FilesAppDirEntry} entry
+ * @param {string} vmType
+ * @returns {boolean}
+ */
+function isPathSharedWithVm(crostini, entry, vmType) {
+  if (!crostini.isEnabled(vmType)) {
+    return false;
+  }
+  if (!entry) {
+    return false;
+  }
+  return crostini.isPathShared(vmType, /** @type {!Entry} */ (entry));
 }
