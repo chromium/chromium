2d537eb5e99e5d75c54e55e2372c1099fc5dc1d9
Glen Robertson <glenrob@chromium.org>
2021-09-17 06:09:35 +0000

    Change update_histogram_enum.py to use io.open for Py2/3 compatibility.
    Change-Id: I5d50ee766c3e94970a333a26d7522ba746e1d4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162562
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#922440}

--- tools/metrics/histograms/update_histogram_enum.py
+++ tools/metrics/histograms/update_histogram_enum.py
@@ -7,6 +7,7 @@
 If the file was pretty-printed, the updated version is pretty-printed too.
 """
 
+import io
 import logging
 import os
 import re
@@ -65,7 +66,7 @@ def ReadHistogramValues(filename, start_marker, end_marker, strip_k_prefix):
       DuplicatedValue: An error when two enum labels share the same value.
   """
   # Read the file as a list of lines
-  with open(path_util.GetInputFile(filename)) as f:
+  with io.open(path_util.GetInputFile(filename)) as f:
     content = f.readlines()
 
   START_REGEX = re.compile(start_marker)
@@ -216,7 +217,7 @@ def _GetOldAndUpdatedXml(histogram_enum_name, source_enum_values,
   and returns both in XML format.
   """
   Log('Reading existing histograms from "{0}".'.format(ENUMS_PATH))
-  with open(ENUMS_PATH, 'r', encoding='utf-8') as f:
+  with io.open(ENUMS_PATH, 'r', encoding='utf-8') as f:
     histograms_doc = minidom.parse(f)
     f.seek(0)
     xml = f.read()
@@ -300,7 +301,7 @@ def UpdateHistogramFromDict(histogram_enum_name, source_enum_values,
     Log('Cancelled.')
     return
 
-  with open(ENUMS_PATH, 'w', encoding='utf-8') as f:
+  with io.open(ENUMS_PATH, 'w', encoding='utf-8') as f:
     f.write(new_xml)
 
   Log('Done.')
