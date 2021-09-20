ac8f229e4a76ed82248e9a47507618f59d1ea302
Xiaocheng Hu <xiaochengh@chromium.org>
2021-09-20 21:19:38 +0000

    [@layer] Allow casting StyleRuleLayerBlock to StyleRuleGroup
    This patch fixes down cast traits of StyleRuleGroup, so that a layer
    block rule can be casted into StyleRuleGroup. This fixes a DCHECK hit
    in web test layer-stylesheet-sharing.html and also clusterfuzz.
    
    Note that the WPT test still fails for a different reason, but it
    doesn't crash any more.
    
    Fixed: 1250775
    Change-Id: I7aa3af41368a509589fce074d5e38750c00f7dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169931
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923059}

--- third_party/blink/renderer/core/css/style_rule.h
+++ third_party/blink/renderer/core/css/style_rule.h
@@ -464,7 +464,7 @@ template <>
 struct DowncastTraits<StyleRuleGroup> {
   static bool AllowFrom(const StyleRuleBase& rule) {
     return rule.IsMediaRule() || rule.IsSupportsRule() ||
-           rule.IsContainerRule();
+           rule.IsContainerRule() || rule.IsLayerBlockRule();
   }
 };
 
