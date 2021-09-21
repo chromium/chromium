73f14957d4c22796a29739c6c2ecda8aad1f437d
David Bertoni <dbertoni@chromium.org>
2021-09-21 19:00:32 +0000

    [Extensions] Re-enable ProcessManagementTest.ProcessOverflow on Windows.
    This test was disabled many years ago on Windows for timeouts. It
    passed 3000 iterations on the Windows bot with no issues.
    
    Bug: 173137
    Change-Id: Iab2b982e586ae3a9ec830d41b14d71349a33ea64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171920
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923504}

--- chrome/browser/extensions/process_management_browsertest.cc
+++ chrome/browser/extensions/process_management_browsertest.cc
@@ -119,17 +119,10 @@ class ChromeWebStoreInIsolatedOriginTest : public ChromeWebStoreProcessTest {
 }  // namespace
 
 
-// TODO(nasko): crbug.com/173137
-#if defined(OS_WIN)
-#define MAYBE_ProcessOverflow DISABLED_ProcessOverflow
-#else
-#define MAYBE_ProcessOverflow ProcessOverflow
-#endif
-
 // Ensure that an isolated app never shares a process with WebUIs, non-isolated
 // extensions, and normal webpages.  None of these should ever comingle
 // RenderProcessHosts even if we hit the process limit.
-IN_PROC_BROWSER_TEST_F(ProcessManagementTest, MAYBE_ProcessOverflow) {
+IN_PROC_BROWSER_TEST_F(ProcessManagementTest, ProcessOverflow) {
   // Set max renderers to 1 to force running out of processes.
   content::RenderProcessHost::SetMaxRendererProcessCount(1);
 
