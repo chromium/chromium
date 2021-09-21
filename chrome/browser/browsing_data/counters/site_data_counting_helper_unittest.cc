80f8174d857dddfacee7e88f682828312389be8b
David Bienvenu <davidbienvenu@chromium.org>
2021-09-21 20:49:49 +0000

    Revert "unit_tests: Fix SiteDataCountingHelperTest flakiness"
    This reverts commit 0e5308e7eac035f17d0da1ff45c8fb654ea6812a.
    
    Reason for revert: <there is a workaround for crbug.com/1182630>
    
    Original change's description:
    > unit_tests: Fix SiteDataCountingHelperTest flakiness
    >
    > Make sure QuotaManager is finished initializing before starting test,
    > so that we don't crash while initializing it while pumping events
    > during the test, or during test shutdown.
    >
    > This should be rolled back once crbug.com/1182630 is fixed.
    >
    > Bug: 956719
    > Change-Id: I1357cb46a2edf3a9b95b098262e8a3953cb86aaf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747258
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#861586}
    
    Bug: 956719
    Change-Id: Ibcec554b02aa4322a6a73f9c8c94f0dda8fa9093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173090
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923572}

--- chrome/browser/browsing_data/counters/site_data_counting_helper_unittest.cc
+++ chrome/browser/browsing_data/counters/site_data_counting_helper_unittest.cc
@@ -33,11 +33,6 @@ class SiteDataCountingHelperTest : public testing::Test {
 
   void SetUp() override {
     profile_ = std::make_unique<TestingProfile>();
-    // Let the storage system finish setting up, to avoid test flakiness caused
-    // by the quota storage system shutting down at test end, while still being
-    // set up. TODO(crbug.com/1182630) Remove when crbug.com/1182630 is fixed.
-    profile()->GetDefaultStoragePartition();
-    task_environment_.RunUntilIdle();
   }
 
   void TearDown() override {
