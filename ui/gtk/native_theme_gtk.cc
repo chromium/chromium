3761c84f7777e2ecb7badbe929482a43cb7c4b70
tom <tluk@chromium.org>
2021-09-21 21:40:40 +0000

    Reland "[gtk] Use GTK color values to set the |use_dark_colors| bit"
    This is a reland of 7597f9f46ed0129ddd2730618c3dc2fa6fd26150
    
    Original CL was incorrectly using a NativeTheme::ColorId rather
    than ColorProvider::ColorId enum for SkColorFromColorId().
    
    See diff vs patchset 1.
    
    Original change's description:
    > [gtk] Use GTK color values to set the |use_dark_colors| bit
    >
    > This CL updates the call to set_use_dark_colors() in
    > NativeThemeGtk::OnThemeChanged() to use SkColorFromColorId()
    > instead of the GetSystemColor() method (currently being deprecated).
    >
    > Previously, if no color was supplied by the GTK theme,
    > GetSystemColor() would fallthrough to the default GetSystemColor()
    > implementation which would redundantly return the light mode window
    > background color (and hence the NativeTheme instance would always
    > be set to light mode in this case).
    >
    > Bug: 1251006
    > Change-Id: Ib758ae300a6c07db8f1aee81488c5e7bb060a93f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3170892
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#923083}
    
    Bug: 1251006
    Change-Id: Ic3ec1a1dd82f55c55247284c8bffec7850e9dda3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173088
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923596}

--- ui/gtk/native_theme_gtk.cc
+++ ui/gtk/native_theme_gtk.cc
@@ -5,6 +5,7 @@
 #include "ui/gtk/native_theme_gtk.h"
 
 #include "base/strings/strcat.h"
+#include "ui/color/color_id.h"
 #include "ui/color/color_provider_manager.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
@@ -189,8 +190,10 @@ void NativeThemeGtk::OnThemeChanged(GtkSettings* settings,
   // have a light variant and aren't affected by the setting.  Because of this,
   // experimentally check if the theme is dark by checking if the window
   // background color is dark.
-  set_use_dark_colors(IsForcedDarkMode() || color_utils::IsDark(GetSystemColor(
-                                                kColorId_WindowBackground)));
+  const auto window_bg_color = SkColorFromColorId(ui::kColorWindowBackground);
+  set_use_dark_colors(
+      IsForcedDarkMode() ||
+      (window_bg_color && color_utils::IsDark(window_bg_color.value())));
   set_preferred_color_scheme(CalculatePreferredColorScheme());
 
   // GTK doesn't have a native high contrast setting.  Rather, it's implied by
