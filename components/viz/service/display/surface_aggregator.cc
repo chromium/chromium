cc9fcde02a7a8d5103d9591f6f851a48b6e127fa
Peter McNeeley <petermcneeley@chromium.org>
2021-09-21 01:57:30 +0000

    Add checks on surface damage list to narrow cause of bug
    Under some very specific case the damage area computed in 'AssignDamage'
    is much larger than an int32. This change introduces checks to help
    narrow down the reason for why this might be occurring.
    
    
    Bug: b/191414141
    Change-Id: I78e7301cdd82497f2973ac789e73a455aacada62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169120
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923180}

--- components/viz/service/display/surface_aggregator.cc
+++ components/viz/service/display/surface_aggregator.cc
@@ -399,6 +399,8 @@ void SurfaceAggregator::AddSurfaceDamageToDamageList(
     damage_rect_in_root_target_space.Intersect(root_clip_rect);
   }
 
+  // TODO(petermcneeley) : Check is temporary to isolate b/191414141.
+  CHECK(damage_rect_in_root_target_space.size().GetCheckedArea().IsValid());
   surface_damage_rect_list_->push_back(damage_rect_in_root_target_space);
 }
 
@@ -463,6 +465,8 @@ const DrawQuad* SurfaceAggregator::FindQuadWithOverlayDamage(
     surface_damage_rect_list_->push_back(gfx::Rect());
   }
 
+  // TODO(petermcneeley) : Check is temporary to isolate b/191414141.
+  CHECK(!surface_damage_rect_list_->empty());
   // The latest surface damage rect.
   *overlay_damage_index = surface_damage_rect_list_->size() - 1;
 
@@ -1192,6 +1196,8 @@ void SurfaceAggregator::CopyQuadsToPass(
           AddSurfaceDamageToDamageList(damage_rect_in_target_space.value(),
                                        target_transform, {}, dest_pass,
                                        /*resolved_frame=*/nullptr);
+          // TODO(petermcneeley) : Check is temporary to isolate b/191414141.
+          CHECK_GE(surface_damage_rect_list_->size(), 1u);
         } else if (quad == quad_with_overlay_damage_index) {
           dest_shared_quad_state->overlay_damage_index = overlay_damage_index;
         }
