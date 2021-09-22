978e43de50d1fa42196fb5863d4d18a1aa55243d
Yulun Wu <yulunwu@chromium.org>
2021-09-22 17:58:23 +0000

    Enable categorical search if app list bubble is enabled.
    Bug: 1249340
    Change-Id: If36209178d5d90ce685b5ec205c9803e867d05bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163857
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923940}

--- ash/public/cpp/app_list/app_list_features.cc
+++ ash/public/cpp/app_list/app_list_features.cc
@@ -4,6 +4,7 @@
 
 #include "ash/public/cpp/app_list/app_list_features.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/app_list_switches.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
@@ -131,7 +132,9 @@ bool IsAppListLaunchRecordingEnabled() {
 }
 
 bool IsCategoricalSearchEnabled() {
-  return base::FeatureList::IsEnabled(kCategoricalSearch);
+  // Force categorical search for the latest version of the launcher.
+  return ash::features::IsAppListBubbleEnabled() ||
+         base::FeatureList::IsEnabled(kCategoricalSearch);
 }
 
 bool IsLauncherQueryHighlightingEnabled() {
