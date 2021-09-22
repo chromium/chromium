3c76fd64989187f57228aba54bab3b3993fde529
Asami Doi <asamidoi@chromium.org>
2021-09-22 05:08:32 +0000

    Sheriff: disable IndexedDBDispatcherHostTest.DatabaseOperationSequencing on ChromeOS and Android
    Bug: 1189512
    Change-Id: I54f3c877e7f77e48216b5df26c689c31914da753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173039
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923731}

--- content/browser/indexed_db/indexed_db_dispatcher_host_unittest.cc
+++ content/browser/indexed_db/indexed_db_dispatcher_host_unittest.cc
@@ -1411,7 +1411,8 @@ TEST_F(IndexedDBDispatcherHostTest, DISABLED_NotifyIndexedDBContentChanged) {
 }
 
 // Flaky on Mac and Linux ASAN builds. See: crbug.com/1189512.
-#if defined(OS_MAC) || (defined(OS_LINUX) && defined(ADDRESS_SANITIZER))
+#if defined(OS_MAC) || (defined(OS_LINUX) && defined(ADDRESS_SANITIZER)) || \
+    defined(OS_CHROMEOS) || defined(OS_ANDROID)
 #define MAYBE_DatabaseOperationSequencing DISABLED_DatabaseOperationSequencing
 #else
 #define MAYBE_DatabaseOperationSequencing DatabaseOperationSequencing
