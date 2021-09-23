b386b31cbc58f0e4a63c6b67fefd905069ed9878
Sammie Quon <sammiequon@chromium.org>
2021-09-23 02:12:37 +0000

    Reland "desks: Add a flag with tentative improvements for swiping desks."
    This is a reland of 4e822c94e5a7dd7744a2209bcc9418884932fdca
    
    PS1 contains the original patch. The issue was that we were assuming
    that by going sync in the test, a fast swipe would be registered. This
    is not the case on the much slower MSAN test, so this patch fixes that
    by forcing a fast swipe in the test.
    
    Original change's description:
    > desks: Add a flag with tentative improvements for swiping desks.
    >
    > Tested locally and like the improvements, but want feedback from more
    > people and more devices (namely UX), so introduced a feature flag to
    > tentatively hide these improvements.
    >
    > The main issue we are seeing is that "fast swipes" are not working as
    > user intends. Fast swipes can fail due to:
    > - starting screenshot not taken
    > - starting screenshot taken but ending screenshot not taken
    > - user has not seen 10% of the next desk
    >
    > This patch attempts to solve the latter two. If feedback is positive we
    > could explore solving the former, but that is a bit more tricky. In
    > addition to removing the 10% seen threshold, we let the ending screenshot
    > callback finish and start an animation when it is, just like keyboard
    > release.
    >
    > Test: manual, added
    > Bug: 1191545
    > Change-Id: I86a86d6e5f51670bff192132fe6a0a0685f8e7b1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169038
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#923671}
    
    Bug: 1191545
    Change-Id: I4b4fbb42cb22aad22f050a1815241502a68951c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176313
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924149}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -412,6 +412,12 @@ const base::Feature kEmojiSuggestAddition{"EmojiSuggestAddition",
 const base::Feature kEnableBackgroundBlur{"EnableBackgroundBlur",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
+// Enables some trials aimed at improving user experiencing when using the
+// trackpad to switch desks.
+// TODO(https://crbug.com/1191545): Remove this after the feature is launched.
+const base::Feature kEnableDesksTrackpadSwipeImprovements{
+    "EnableDesksTrackpadSwipeImprovements", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables the DNS proxy service providing support split and secure DNS
 // for Chrome OS.
 const base::Feature kEnableDnsProxy{"EnableDnsProxy",
@@ -1193,6 +1199,10 @@ bool AreContextualNudgesEnabled() {
 
 bool AreDesksTemplatesEnabled() {
   return base::FeatureList::IsEnabled(kDesksTemplates);
+}
+
+bool AreDesksTrackpadSwipeImprovementsEnabled() {
+  return base::FeatureList::IsEnabled(kEnableDesksTrackpadSwipeImprovements);
 }
 
 bool AreImprovedScreenCaptureSettingsEnabled() {
