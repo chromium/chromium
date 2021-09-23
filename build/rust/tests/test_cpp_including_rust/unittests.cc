8524e35677a3632631c95158a87f3939efb8bf9b
Lukasz Anforowicz <lukasza@chromium.org>
2021-09-23 14:12:26 +0000

    [rust] Basic GTest coverage: simple FFI + PartitionAlloc usage.
    Bug: 1069271
    Change-Id: I9e10ff301d0ec2f8ff16a027b3bf6ad89dc31a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011805
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924288}

---
+++ build/rust/tests/test_cpp_including_rust/unittests.cc
@@ -0,0 +1,29 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <memory>
+
+#include "base/allocator/buildflags.h"
+#include "base/allocator/partition_allocator/partition_address_space.h"
+#include "build/build_config.h"
+#include "build/buildflag.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+extern "C" int32_t add_two_ints_via_rust(int32_t x, int32_t y);
+TEST(RustTest, CppCallingIntoRust_BasicFFI) {
+  EXPECT_EQ(7, add_two_ints_via_rust(3, 4));
+}
+
+extern "C" void* allocate_via_rust();
+extern "C" void deallocate_via_rust(void* p);
+TEST(RustTest, RustComponentUsesPartitionAlloc) {
+  // Verify that PartitionAlloc is consistently used in C++ and Rust.
+  auto cpp_allocated_int = std::make_unique<int>();
+  void* rust_allocated_ptr = allocate_via_rust();
+  EXPECT_EQ(base::IsManagedByPartitionAlloc(rust_allocated_ptr),
+            base::IsManagedByPartitionAlloc(cpp_allocated_int.get()));
+  deallocate_via_rust(rust_allocated_ptr);
+}
--- build/rust/tests/test_rust_source_set/main.rs
+++ build/rust/tests/test_rust_source_set/main.rs
@@ -8,14 +8,32 @@ pub extern "C" fn say_hello_from_cpp() {
 }
 
 pub fn say_hello() {
-    println!("Hello, world - from a Rust library. Calculations suggest that 3+4={}", do_maths());
-}
-
-fn do_maths() -> i32 {
-    3 + 4
+    println!(
+        "Hello, world - from a Rust library. Calculations suggest that 3+4={}",
+        add_two_ints_via_rust(3, 4));
 }
 
 #[test]
 fn test_hello() {
-    assert_eq!(7, do_maths());
+    assert_eq!(7, add_two_ints_via_rust(3, 4));
+}
+
+// The extern "C" functions below are used by GTest tests from the same C++ test
+// executable which is why for now they're all in this same file.
+
+// The next 2 functions are used from the
+// AllocatorTest.RustComponentUsesPartitionAlloc unit test.
+#[no_mangle]
+pub extern "C" fn allocate_via_rust() -> *mut i32 {
+    Box::into_raw(Box::new(123_i32))
+}
+#[no_mangle]
+pub unsafe extern "C" fn deallocate_via_rust(ptr: *mut i32) {
+    Box::from_raw(ptr);
+}
+
+// The next function is used from FfiTest.CppCallingIntoRust unit test.
+#[no_mangle]
+pub extern "C" fn add_two_ints_via_rust(x: i32, y: i32) -> i32 {
+    x + y
 }
