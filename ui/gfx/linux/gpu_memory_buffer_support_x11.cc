ecf918ed107f43ca1e7ddc0b2354941105b83762
Saifuddin Hitawala <hitawala@chromium.org>
2021-09-24 22:02:51 +0000

    Choose OzoneBacking over GLImageBacking for CrOS
    This change completes a TODO where the OzoneFactory is chosen over
    GLImage since it supports creating shared images with both GMBs and
    pixel upload support. It also adds a check for device creation failures
    for x11 in ozone and fallbacks to GPU_READ when SCANOUT is not
    supported for creating pixmaps.
    
    Bug: 1241537
    Change-Id: I9a475d58d13e680b33dfa849e23f6678525f7dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162777
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924935}

--- ui/gfx/linux/gpu_memory_buffer_support_x11.cc
+++ ui/gfx/linux/gpu_memory_buffer_support_x11.cc
@@ -11,6 +11,7 @@
 
 #include "base/containers/contains.h"
 #include "base/debug/crash_logging.h"
+#include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/buffer_types.h"
@@ -118,7 +119,10 @@ std::unique_ptr<GbmBuffer> GpuMemoryBufferSupportX11::CreateBuffer(
     gfx::BufferFormat format,
     const gfx::Size& size,
     gfx::BufferUsage usage) {
-  DCHECK(device_);
+  if (!device_) {
+    LOG(ERROR) << "Device could not be created.";
+    return nullptr;
+  }
   DCHECK(base::Contains(supported_configs_,
                         gfx::BufferUsageAndFormat(usage, format)));
 
