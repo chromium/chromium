db47da8977a2c68aec916f1c6efbe566d670c192
Prakhar Shrivastav <p.shri@samsung.com>
2021-09-24 01:07:49 +0000

    Notification:  Simplify threading in PlatformNotificationServiceProxy
    The "service worker core thread" is just the UI thread,
    so remove the switch to get task runner and change the DCHECK to
    BrowserThread::UI
    
    Bug: 1138155
    Change-Id: I89c92b47869dea268300bafefd573250c1a14051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174953
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924582}

--- content/browser/notifications/platform_notification_service_proxy.cc
+++ content/browser/notifications/platform_notification_service_proxy.cc
@@ -61,7 +61,7 @@ void PlatformNotificationServiceProxy::VerifyServiceWorkerScope(
     DisplayResultCallback callback,
     blink::ServiceWorkerStatusCode status,
     scoped_refptr<ServiceWorkerRegistration> registration) {
-  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   base::OnceClosure task;
 
   if (status == blink::ServiceWorkerStatusCode::kOk &&
@@ -84,27 +84,17 @@ void PlatformNotificationServiceProxy::DisplayNotification(
     return;
   }
 
-  scoped_refptr<base::SingleThreadTaskRunner> core_thread_task_runner;
-  constexpr BrowserTaskTraits traits = {base::TaskPriority::USER_VISIBLE};
-  switch (ServiceWorkerContext::GetCoreThreadId()) {
-    case BrowserThread::UI:
-      core_thread_task_runner = GetUIThreadTaskRunner(traits);
-      break;
-    case BrowserThread::IO:
-      core_thread_task_runner = GetIOThreadTaskRunner(traits);
-      break;
-    case BrowserThread::ID_COUNT:
-      NOTREACHED();
-  }
-  core_thread_task_runner->PostTask(
-      FROM_HERE,
-      base::BindOnce(
-          &ServiceWorkerContextWrapper::FindReadyRegistrationForId,
-          service_worker_context_, data.service_worker_registration_id,
-          blink::StorageKey(url::Origin::Create(data.origin)),
+  GetUIThreadTaskRunner({base::TaskPriority::USER_VISIBLE})
+      ->PostTask(
+          FROM_HERE,
           base::BindOnce(
-              &PlatformNotificationServiceProxy::VerifyServiceWorkerScope,
-              weak_ptr_factory_io_.GetWeakPtr(), data, std::move(callback))));
+              &ServiceWorkerContextWrapper::FindReadyRegistrationForId,
+              service_worker_context_, data.service_worker_registration_id,
+              blink::StorageKey(url::Origin::Create(data.origin)),
+              base::BindOnce(
+                  &PlatformNotificationServiceProxy::VerifyServiceWorkerScope,
+                  weak_ptr_factory_io_.GetWeakPtr(), data,
+                  std::move(callback))));
 }
 
 void PlatformNotificationServiceProxy::CloseNotifications(
