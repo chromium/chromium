a00d67f933955b011431e8d7566f4f8d4c9ba5bf
David Bertoni <dbertoni@chromium.org>
2021-09-24 19:42:51 +0000

    [Extensions] Re-enable ExtensionApiNewTabTest.Tabs.
    This test was disabled for flakiness and wasn't re-enabled when the
    underlying issue was fixed. Testing shows that it is not flaky on any
    of the ChromeOS bots.
    
    Bug: 870322
    Change-Id: If5067fd3ccde11e8b6623af9ac964eafb7f4bc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180388
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924867}

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -71,13 +71,7 @@ class ExtensionApiNewTabTest : public ExtensionApiTabTest {
   }
 };
 
-// Flaky on chromeos: http://crbug.com/870322
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#define MAYBE_Tabs DISABLED_Tabs
-#else
-#define MAYBE_Tabs Tabs
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiNewTabTest, MAYBE_Tabs) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiNewTabTest, Tabs) {
   // The test creates a tab and checks that the URL of the new tab
   // is that of the new tab page.  Make sure the pref that controls
   // this is set.
