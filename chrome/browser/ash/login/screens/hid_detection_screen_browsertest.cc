08c030bfbcc535733dc096a28fe82c211aad72ce
Roman Sorokin <rsorokin@chromium.org>
2021-09-25 15:59:29 +0000

    [M94][ash-chrome] Restore HIDDetectionScreenDisabledAfterRestartTest(s)
    Also tweaked the logic - store the flag right after the HID detection
    screen was skipped. Instead of SkipToUpdateForTesting.
    The tests were removed in crrev.com/c/2910614 which caused autoupdate
    tests to fail.
    
    BUG=b:199755300
    
    (cherry picked from commit 254396eda3485260656eb3a6c6f2207f1c2177a2)
    
    Change-Id: I0ba1a23029a388d640825494d1ce9197af16faea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169715
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#923338}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179041
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4606@{#1219}
    Cr-Branched-From: 35b0d5a9dc8362adfd44e2614f0d5b7402ef63d0-refs/heads/master@{#911515}

--- chrome/browser/ash/login/screens/hid_detection_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/hid_detection_screen_browsertest.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/ash/login/test/device_state_mixin.h"
 #include "chrome/browser/ash/login/test/hid_controller_mixin.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
+#include "chrome/browser/ash/login/test/local_state_mixin.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/ash/login/test/oobe_screens_utils.h"
@@ -251,10 +252,11 @@ IN_PROC_BROWSER_TEST_F(HIDDetectionOobeCompletedUnowned, ShowScreen) {
 }
 
 class HIDDetectionScreenDisabledAfterRestartTest
-    : public HIDDetectionScreenChromeboxTest {
+    : public HIDDetectionScreenChromeboxTest,
+      public LocalStateMixin::Delegate {
  public:
   HIDDetectionScreenDisabledAfterRestartTest() = default;
-  // HidDetectionTest:
+  // HIDDetectionScreenChromeboxTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     HIDDetectionScreenChromeboxTest::SetUpCommandLine(command_line);
     // Emulating Chrome restart without the flag.
@@ -263,7 +265,33 @@ class HIDDetectionScreenDisabledAfterRestartTest
           switches::kDisableHIDDetectionOnOOBEForTesting);
     }
   }
+  // We need to check local state flag before welcome screen is shown.
+  void SetUpLocalState() override {
+    if (content::IsPreTest()) {
+      // Pref should be false by default.
+      EXPECT_FALSE(StartupUtils::IsHIDDetectionScreenDisabledForTests());
+    }
+  }
+
+ private:
+  LocalStateMixin local_state_mixin_{&mixin_host_, this};
 };
+
+IN_PROC_BROWSER_TEST_F(HIDDetectionScreenDisabledAfterRestartTest,
+                       PRE_SkipToUpdate) {
+  OobeScreenWaiter(chromeos::WelcomeView::kScreenId).Wait();
+
+  EXPECT_TRUE(StartupUtils::IsHIDDetectionScreenDisabledForTests());
+  EXPECT_EQ(GetExitResult(), HIDDetectionScreen::Result::SKIPPED_FOR_TESTS);
+}
+
+IN_PROC_BROWSER_TEST_F(HIDDetectionScreenDisabledAfterRestartTest,
+                       SkipToUpdate) {
+  OobeScreenWaiter(chromeos::WelcomeView::kScreenId).Wait();
+  // The pref should persist restart.
+  EXPECT_TRUE(StartupUtils::IsHIDDetectionScreenDisabledForTests());
+  EXPECT_EQ(GetExitResult(), HIDDetectionScreen::Result::SKIPPED_FOR_TESTS);
+}
 
 class HIDDetectionScreenChromebookTest : public OobeBaseTest {
  private:
