857559398cd4901ce3f262a9797e9dd9cac79fb8
Maria Kazinova <kazinova@google.com>
2021-09-27 18:07:27 +0000

    Migrate ios/chrome/browser/passwords classes to PasswordStoreInterface.
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore
    
    Bug: 1218413
    Change-Id: I90d827e1666a52d079af6be6cfcba7bcfec86859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3186951
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925322}

--- ios/chrome/browser/passwords/ios_chrome_password_reuse_manager_factory.cc
+++ ios/chrome/browser/passwords/ios_chrome_password_reuse_manager_factory.cc
@@ -9,7 +9,7 @@
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/password_manager/core/browser/password_reuse_manager_impl.h"
-#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -55,10 +55,11 @@ IOSChromePasswordReuseManagerFactory::BuildServiceInstanceFor(
   std::unique_ptr<password_manager::PasswordReuseManager> reuse_manager =
       std::make_unique<password_manager::PasswordReuseManagerImpl>();
 
-  reuse_manager->Init(ChromeBrowserState::FromBrowserState(context)->GetPrefs(),
-                      IOSChromePasswordStoreFactory::GetForBrowserState(
-                          browser_state, ServiceAccessType::EXPLICIT_ACCESS)
-                          .get());
+  reuse_manager->Init(
+      ChromeBrowserState::FromBrowserState(context)->GetPrefs(),
+      IOSChromePasswordStoreFactory::GetInterfaceForBrowserState(
+          browser_state, ServiceAccessType::EXPLICIT_ACCESS)
+          .get());
   return reuse_manager;
 }
 
