c69019f7d6dbd364d77c97dc339491bbcf3b3ad0
Matt Falkenhagen <falken@chromium.org>
2021-09-27 07:16:33 +0000

    service worker: Remove expired "PageTransition" histogram.
    This removes code for emitting the
    PageLoad.Clients.ServiceWorker2.PageTransition histogram, which expired
    in October 2020.
    
    Bug: 1089209
    Change-Id: I23d4eca9bb7f260efab8ce35a8d3190ae1bb49c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3185020
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925166}

--- chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.cc
@@ -15,9 +15,6 @@
 
 namespace internal {
 
-const char kHistogramServiceWorkerPageTransition[] =
-    "PageLoad.Clients.ServiceWorker2.PageTransition";
-
 const char kHistogramServiceWorkerParseStart[] =
     "PageLoad.Clients.ServiceWorker2.ParseTiming.NavigationToParseStart";
 const char kHistogramServiceWorkerParseStartForwardBack[] =
@@ -264,16 +261,6 @@ void ServiceWorkerPageLoadMetricsObserver::OnParseStart(
     const page_load_metrics::mojom::PageLoadTiming& timing) {
   if (!IsServiceWorkerControlled())
     return;
-
-  // TODO(falken): It may be cleaner to record page transition in OnCommit() but
-  // at that point we don't yet know if the page is controlled by a service
-  // worker. It should be possible to plumb the information there since the
-  // browser process already sends the controller service worker in the
-  // navigation commit IPC.
-  UMA_HISTOGRAM_ENUMERATION(
-      internal::kHistogramServiceWorkerPageTransition,
-      static_cast<int>(ui::PageTransitionStripQualifier(transition_)),
-      static_cast<int>(ui::PAGE_TRANSITION_LAST_CORE) + 1);
 
   if (page_load_metrics::WasStartedInForegroundOptionalEventInForeground(
           timing.parse_timing->parse_start, GetDelegate())) {
