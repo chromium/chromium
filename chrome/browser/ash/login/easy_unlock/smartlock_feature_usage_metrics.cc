f2619c412a72394375bb0b9072006ae2af2542d8
Brittany Hartmire <bhartmire@google.com>
2021-09-28 20:54:19 +0000

    [SmartLock]: SFUL SmartLockFeatureUsageMetrics class
    This CL creates the SmartLockFeatureUsageMetrics class to implement
    Standard Feature Usage Logging (SFUL) for the SmartLock feature.
    The SmartLock feature presents unique complications for implementing
    SFUL due to its possible active state outside of any user session.
    A design doc detailing these unique challenges and decisions around the
    SmartLockFeatureUsageMetrics class can be found at go/cros-smartlock-sful.
    
    Bug: 1233614
    Change-Id: Ic53063bb5bf17bcbc149d6e71261433dd8b5e161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179843
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#925929}

---
+++ chrome/browser/ash/login/easy_unlock/smartlock_feature_usage_metrics.cc
@@ -0,0 +1,79 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/login/easy_unlock/smartlock_feature_usage_metrics.h"
+
+#include "chromeos/components/feature_usage/feature_usage_metrics.h"
+#include "chromeos/services/multidevice_setup/public/cpp/multidevice_setup_client.h"
+
+namespace ash {
+namespace {
+using chromeos::multidevice_setup::mojom::Feature;
+using chromeos::multidevice_setup::mojom::FeatureState;
+
+const char kFeatureName[] = "SmartLock";
+}  // namespace
+
+SmartLockFeatureUsageMetrics::SmartLockFeatureUsageMetrics(
+    chromeos::multidevice_setup::MultiDeviceSetupClient*
+        multi_device_setup_client)
+    : multi_device_setup_client_(multi_device_setup_client),
+      feature_usage_metrics_(kFeatureName, this) {}
+
+void SmartLockFeatureUsageMetrics::RecordUsage(bool success) {
+  feature_usage_metrics_.RecordUsage(success);
+}
+
+bool SmartLockFeatureUsageMetrics::IsEligible() const {
+  if (!multi_device_setup_client_) {
+    // EasyUnlockServiceSignin cannot determine Eligible/Enabled values, so
+    // it injects a null MultiDeviceSetupClient reference. Therefore, a null
+    // MultiDeviceSetupClient reference implies that Eligible/Enabled are always
+    // true.
+    return true;
+  }
+
+  switch (multi_device_setup_client_->GetFeatureState(Feature::kSmartLock)) {
+    case FeatureState::kUnavailableNoVerifiedHost:
+      FALLTHROUGH;
+    case FeatureState::kNotSupportedByChromebook:
+      FALLTHROUGH;
+    case FeatureState::kNotSupportedByPhone:
+      return false;
+
+    case FeatureState::kProhibitedByPolicy:
+      FALLTHROUGH;
+    case FeatureState::kDisabledByUser:
+      FALLTHROUGH;
+    case FeatureState::kEnabledByUser:
+      FALLTHROUGH;
+    case FeatureState::kUnavailableInsufficientSecurity:
+      FALLTHROUGH;
+    case FeatureState::kUnavailableSuiteDisabled:
+      FALLTHROUGH;
+    case FeatureState::kFurtherSetupRequired:
+      FALLTHROUGH;
+    case FeatureState::kUnavailableTopLevelFeatureDisabled:
+      return true;
+  }
+}
+
+bool SmartLockFeatureUsageMetrics::IsEnabled() const {
+  if (!multi_device_setup_client_) {
+    // EasyUnlockServiceSignin cannot determine Eligible/Enabled values, so
+    // it injects a null MultiDeviceSetupClient reference. Therefore, a null
+    // MultiDeviceSetupClient reference implies that Eligible/Enabled are always
+    // true.
+    return true;
+  }
+
+  if (multi_device_setup_client_->GetFeatureState(Feature::kSmartLock) ==
+      FeatureState::kEnabledByUser) {
+    return true;
+  }
+
+  return false;
+}
+
+}  // namespace ash
---
+++ chrome/browser/ash/login/easy_unlock/smartlock_feature_usage_metrics.h
@@ -0,0 +1,43 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_LOGIN_EASY_UNLOCK_SMARTLOCK_FEATURE_USAGE_METRICS_H_
+#define CHROME_BROWSER_ASH_LOGIN_EASY_UNLOCK_SMARTLOCK_FEATURE_USAGE_METRICS_H_
+
+#include "chromeos/components/feature_usage/feature_usage_metrics.h"
+
+namespace chromeos {
+namespace multidevice_setup {
+class MultiDeviceSetupClient;
+}  // namespace multidevice_setup
+}  // namespace chromeos
+
+namespace ash {
+
+// Tracks Smart Lock feature usage for the Standard Feature Usage Logging
+// (SFUL) framework.
+class SmartLockFeatureUsageMetrics
+    : public feature_usage::FeatureUsageMetrics::Delegate {
+ public:
+  explicit SmartLockFeatureUsageMetrics(
+      chromeos::multidevice_setup::MultiDeviceSetupClient*
+          multi_device_setup_client);
+
+  // To be called by the owner of this SmartLockFeatureUsageMetrics
+  // instance when the user uses the feature.
+  void RecordUsage(bool success);
+
+ private:
+  // feature_usage::FeatureUsageMetrics::Delegate:
+  bool IsEligible() const override;
+  bool IsEnabled() const override;
+
+  chromeos::multidevice_setup::MultiDeviceSetupClient*
+      multi_device_setup_client_;
+  feature_usage::FeatureUsageMetrics feature_usage_metrics_;
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_LOGIN_EASY_UNLOCK_SMARTLOCK_FEATURE_USAGE_METRICS_H_
