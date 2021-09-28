2ec645e1d0d4b7404bcdec7c0f7693708fce98c8
Dominic Farolino <dom@chromium.org>
2021-09-28 21:23:52 +0000

    Add braces to multi-line conditional in RenderFrameProxyHost
    This CL brings a conditional in RenderFrameProxyHost::RouteMessageEvent
    up to conformance with the Google C++ Style Guide on conditionals [1].
    
    [1]: https://google.github.io/styleguide/cppguide.html#Conditionals
    
    Change-Id: I1148f1a0fec80e82fb493b8694b732a34feb59cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3184671
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925946}

--- content/browser/renderer_host/render_frame_proxy_host.cc
+++ content/browser/renderer_host/render_frame_proxy_host.cc
@@ -552,8 +552,9 @@ void RenderFrameProxyHost::RouteMessageEvent(
   SiteInstance* target_site_instance = target_rfh->GetSiteInstance();
   if (!target_site_instance->IsRelatedSiteInstance(GetSiteInstance()) &&
       !target_rfh->delegate()->ShouldRouteMessageEvent(target_rfh,
-                                                       GetSiteInstance()))
+                                                       GetSiteInstance())) {
     return;
+  }
 
   // If there is a |source_frame_token|, translate it to the frame token of the
   // equivalent RenderFrameProxyHost in the target process.
