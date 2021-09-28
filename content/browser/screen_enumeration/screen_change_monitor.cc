5fdceeda09e303102155e755d30d4906ecb3ad4e
Adrienne Walker <enne@chromium.org>
2021-09-28 01:50:37 +0000

    Remove ScreenChangeMonitor Fuchsia #ifdefs
    Various cleanups seem to have made this #ifdef unnecessary.
    After other changes landed that shored up Screen assumptions on
    Fuchsia, I checked back on other #ifdefs and thought it was worth
    a try to see if this passed now.
    
    Bug: 1071233
    Change-Id: Iaff4349057a5c0419ffa228e71b2ec08c8032f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188573
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925572}

--- content/browser/screen_enumeration/screen_change_monitor.cc
+++ content/browser/screen_enumeration/screen_change_monitor.cc
@@ -4,7 +4,6 @@
 
 #include "content/browser/screen_enumeration/screen_change_monitor.h"
 
-#include "build/build_config.h"
 #include "ui/display/screen.h"
 
 namespace content {
@@ -12,11 +11,8 @@ namespace content {
 ScreenChangeMonitor::ScreenChangeMonitor(
     base::RepeatingCallback<void(bool)> callback)
     : callback_(callback) {
-// TODO(crbug.com/1071233): Investigate test failures (crashes?) on Fuchsia.
-#if !defined(OS_FUCHSIA)
   if (display::Screen* screen = display::Screen::GetScreen())
     cached_displays_ = screen->GetAllDisplays();
-#endif  // !OS_FUCHSIA
 }
 
 ScreenChangeMonitor::~ScreenChangeMonitor() = default;
