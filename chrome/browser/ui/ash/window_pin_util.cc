67cb0a4b6022632be46d8b18a4d10662f017f1e3
Sean Kau <skau@chromium.org>
2021-09-28 23:23:46 +0000

    Consolidate users of WindowPinType
    These utility functions make it easier to transition users of
    WindowPinType to rely on WindowState instead (which contains the
    same information).
    
    Bug: 1249678
    Change-Id: I892860b397939c3d018648bce6fe0ae1f4092621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188744
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926002}

---
+++ chrome/browser/ui/ash/window_pin_util.cc
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/ash/window_pin_util.h"
+
+#include "ash/wm/window_state.h"
+#include "chromeos/ui/base/window_pin_type.h"
+#include "chromeos/ui/base/window_properties.h"
+#include "ui/aura/window.h"
+
+void PinWindow(aura::Window* window, bool trusted) {
+  DCHECK(window);
+  // TODO(crbug.com/1249678): Transition away from property once callers are
+  // consolidated.
+  window->SetProperty(chromeos::kWindowPinTypeKey,
+                      trusted ? chromeos::WindowPinType::kTrustedPinned
+                              : chromeos::WindowPinType::kPinned);
+}
+
+void UnpinWindow(aura::Window* window) {
+  DCHECK(window);
+  // TODO(crbug.com/1249678): Transition away from property once callers are
+  // consolidated.
+  window->SetProperty(chromeos::kWindowPinTypeKey,
+                      chromeos::WindowPinType::kNone);
+}
+
+chromeos::WindowPinType GetWindowPinType(const aura::Window* window) {
+  DCHECK(window);
+  return window->GetProperty(chromeos::kWindowPinTypeKey);
+}
