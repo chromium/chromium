94adaae85e4db1d1a366aee2175148eea4915fe0
Erik Chen <erikchen@chromium.org>
2021-09-29 21:16:30 +0000

    [Merge to M95] Prevents non-browser processes from requesting memory dumps.
    This CL makes several changes:
    
    (1) Causes the browser to reset non-browser
    mojo::PendingReceiver<Coordinator>. This means that non-browser
    processes will never be able to use the Coordinator interface.
    
    (2) Add CHECKs to existing code to prevent non-browser processes from
    attempting to use the Coordinator interface.
    
    A code audit shows that all Coordinator usages should already only be
    from the browser process.
    
    Note that (2) is important since attempting to use an unbound interface
    will trigger a nullptr dereference, which is undefined behavior.
    
    (cherry picked from commit d9cc471e122e9a2391a68fa7cd72ea50587d8d97)
    
    (cherry picked from commit 6fdb79cefd125eef4b07ff51c8d881dd12a020d5)
    
    Bug: 1251787
    Change-Id: Ifbe9610cc0e373edaaa60fad46b447e8bdb3ec04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174305
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Original-Original-Commit-Position: refs/heads/main@{#923693}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174563
    Reviewed-by: Prudhvi Kumar Bommana <pbommana@google.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Auto-Submit: Krishna Govind <govind@chromium.org>
    Cr-Original-Commit-Position: refs/branch-heads/4650@{#1}
    Cr-Original-Branched-From: 9a88bfa543d7a681c0f4a0a9ce8708effb3179bf-refs/heads/main@{#923661}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194773
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4638@{#463}
    Cr-Branched-From: 159257cab5585bc8421abf347984bb32fdfe9eb9-refs/heads/main@{#920003}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2803,6 +2803,9 @@ void RenderProcessHostImpl::RegisterCoordinatorClient(
     mojo::PendingReceiver<memory_instrumentation::mojom::Coordinator> receiver,
     mojo::PendingRemote<memory_instrumentation::mojom::ClientProcess>
         client_process) {
+  // Intentionally disallow non-browser processes from getting a Coordinator.
+  receiver.reset();
+
   if (!GetProcess().IsValid()) {
     // If the process dies before we get this message. we have no valid PID
     // and there's nothing to register.
