8aa145ada3923e14992f966fed0eebb1964689e3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-09-29 08:52:32 +0000

    Import wpt@ecbffb9a9a4d457a3a7a90aa814b7d42fb63a6a7
    Using wpt-import in Chromium c207320cf58adf40db23e5e992b03d880663bd77.
    With Chromium commits locally applied on WPT:
    e2dc11a997 "Fix flaky coop reporting tests."
    c482ebfa21 "Reland "[CSP] Fix wasm-eval check from workers""
    04601bb016 "Added WPT tests for wasm-unsafe-eval"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I5dd4c024667c291353fab3170e97c41582564e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192931
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#926177}

--- third_party/blink/web_tests/external/wpt/fetch/api/cors/cors-preflight.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/api/cors/cors-preflight.any.js
@@ -12,8 +12,12 @@ corsPreflight("CORS [PUT], server allows, check preflight has user agent", corsU
 corsPreflight("CORS [PUT], server refuses", corsUrl, "PUT", false);
 corsPreflight("CORS [PATCH], server allows", corsUrl, "PATCH", true);
 corsPreflight("CORS [PATCH], server refuses", corsUrl, "PATCH", false);
+corsPreflight("CORS [patcH], server allows", corsUrl, "patcH", true);
+corsPreflight("CORS [patcH], server refuses", corsUrl, "patcH", false);
 corsPreflight("CORS [NEW], server allows", corsUrl, "NEW", true);
 corsPreflight("CORS [NEW], server refuses", corsUrl, "NEW", false);
+corsPreflight("CORS [chicken], server allows", corsUrl, "chicken", true);
+corsPreflight("CORS [chicken], server refuses", corsUrl, "chicken", false);
 
 corsPreflight("CORS [GET] [x-test-header: allowed], server allows", corsUrl, "GET", true, [["x-test-header1", "allowed"]]);
 corsPreflight("CORS [GET] [x-test-header: refused], server refuses", corsUrl, "GET", false, [["x-test-header1", "refused"]]);
