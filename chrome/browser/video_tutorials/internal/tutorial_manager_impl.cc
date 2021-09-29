50ae3a08bb85f36686c80acc7091b3273126217b
Shakti Sahu <shaktisahu@chromium.org>
2021-09-29 18:06:23 +0000

    [Video Tutorials] Fetch fresh data if accept languages have changed
    This CL adds support for re-requesting fresh data from server if the
    accept languages have changed. This is checked at startup and anytime
    after startup whenever the language changes. The new data fetched won't
    be available to use until next chrome session. Also restructured the
    manager class a bit to allow caching of the calls before initialization.
    
    Bug: 1254109
    Change-Id: I9c73a5ada4d7532a431b7250e2a3148f2c96d044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194110
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926371}

--- chrome/browser/video_tutorials/internal/tutorial_manager_impl.cc
+++ chrome/browser/video_tutorials/internal/tutorial_manager_impl.cc
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "base/bind.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "components/prefs/pref_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -53,19 +54,8 @@ TutorialManagerImpl::TutorialManagerImpl(std::unique_ptr<TutorialStore> store,
 
 TutorialManagerImpl::~TutorialManagerImpl() = default;
 
-void TutorialManagerImpl::GetTutorials(MultipleItemCallback callback) {
-  // Find the data from cache.
-  if (tutorial_groups_) {
-    auto preferred_locale = tutorial_groups_->preferred_locale();
-    proto::VideoTutorialGroup active_group;
-    FindTutorialGroupInPreferredLanguageOrDefault(
-        *tutorial_groups_, preferred_locale, active_group);
-    auto tutorials = TutorialsFromProto(&active_group);
-
-    FilterOutSummaryCard(tutorials);
-    std::move(callback).Run(tutorials);
-    return;
-  }
+void TutorialManagerImpl::Initialize(SuccessCallback callback) {
+  DCHECK(!initialized_);
 
   // The data doesn't exist in the cache. Load it from the DB.
   store_->InitAndLoad(base::BindOnce(&TutorialManagerImpl::OnDataLoaded,
@@ -73,8 +63,30 @@ void TutorialManagerImpl::GetTutorials(MultipleItemCallback callback) {
                                      std::move(callback)));
 }
 
+bool TutorialManagerImpl::IsInitialized() {
+  return initialized_;
+}
+
+void TutorialManagerImpl::GetTutorials(MultipleItemCallback callback) {
+  DCHECK(initialized_);
+  if (!tutorial_groups_) {
+    std::move(callback).Run(std::vector<Tutorial>());
+    return;
+  }
+
+  auto preferred_locale = tutorial_groups_->preferred_locale();
+  proto::VideoTutorialGroup active_group;
+  FindTutorialGroupInPreferredLanguageOrDefault(*tutorial_groups_,
+                                                preferred_locale, active_group);
+  auto tutorials = TutorialsFromProto(&active_group);
+
+  FilterOutSummaryCard(tutorials);
+  std::move(callback).Run(tutorials);
+}
+
 void TutorialManagerImpl::GetTutorial(FeatureType feature_type,
                                       SingleItemCallback callback) {
+  DCHECK(initialized_);
   if (!tutorial_groups_) {
     std::move(callback).Run(absl::nullopt);
     return;
@@ -96,6 +108,8 @@ void TutorialManagerImpl::GetTutorial(FeatureType feature_type,
 }
 
 std::vector<std::string> TutorialManagerImpl::GetSupportedLanguages() {
+  DCHECK(initialized_);
+
   std::vector<std::string> supported_languages;
   if (!tutorial_groups_)
     return supported_languages;
@@ -111,10 +125,12 @@ std::vector<std::string> TutorialManagerImpl::GetSupportedLanguages() {
 const std::vector<std::string>&
 TutorialManagerImpl::GetAvailableLanguagesForTutorial(
     FeatureType feature_type) {
+  DCHECK(initialized_);
   return languages_for_tutorials_[feature_type];
 }
 
 absl::optional<std::string> TutorialManagerImpl::GetPreferredLocale() {
+  DCHECK(initialized_);
   std::string preferred_locale;
   if (tutorial_groups_)
     preferred_locale = tutorial_groups_->preferred_locale();
@@ -123,52 +139,51 @@ absl::optional<std::string> TutorialManagerImpl::GetPreferredLocale() {
 }
 
 void TutorialManagerImpl::SetPreferredLocale(const std::string& locale) {
+  DCHECK(initialized_);
   if (!tutorial_groups_)
     return;
+
   tutorial_groups_->set_preferred_locale(locale);
   store_->Update(*tutorial_groups_,
                  base::BindOnce(&TutorialManagerImpl::OnGroupsSaved,
                                 weak_ptr_factory_.GetWeakPtr()));
 }
 
+std::string TutorialManagerImpl::GetTextLocale() {
+  DCHECK(initialized_);
+  return tutorial_groups_ ? tutorial_groups_->text_locale() : std::string();
+}
+
 void TutorialManagerImpl::OnDataLoaded(
-    MultipleItemCallback callback,
+    SuccessCallback callback,
     bool success,
     std::unique_ptr<proto::VideoTutorialGroups> loaded_data) {
   tutorial_groups_ = std::move(loaded_data);
-  if (!success || !tutorial_groups_) {
-    std::move(callback).Run(std::vector<Tutorial>());
-    return;
-  }
 
-  languages_for_tutorials_.clear();
-  for (const auto& group : tutorial_groups_->tutorial_groups()) {
-    for (const auto& tutorial : group.tutorials()) {
-      auto feature = ToFeatureType(tutorial.feature());
-      languages_for_tutorials_[feature].emplace_back(group.language());
+  if (tutorial_groups_) {
+    languages_for_tutorials_.clear();
+    for (const auto& group : tutorial_groups_->tutorial_groups()) {
+      for (const auto& tutorial : group.tutorials()) {
+        auto feature = ToFeatureType(tutorial.feature());
+        languages_for_tutorials_[feature].emplace_back(group.language());
+      }
     }
   }
 
-  proto::VideoTutorialGroup active_group;
-  auto preferred_locale = tutorial_groups_->preferred_locale();
-  FindTutorialGroupInPreferredLanguageOrDefault(*tutorial_groups_,
-                                                preferred_locale, active_group);
-  auto tutorials = TutorialsFromProto(&active_group);
-  FilterOutSummaryCard(tutorials);
-  std::move(callback).Run(tutorials);
+  initialized_ = true;
+  std::move(callback).Run(success);
 }
 
 void TutorialManagerImpl::SaveGroups(
     std::unique_ptr<proto::VideoTutorialGroups> groups) {
+  DCHECK(initialized_);
   if (!groups) {
     OnGroupsSaved(true);
     return;
   }
 
-  if (tutorial_groups_) {
+  if (tutorial_groups_)
     groups->set_preferred_locale(tutorial_groups_->preferred_locale());
-    tutorial_groups_.reset();
-  }
 
   store_->Update(*groups, base::BindOnce(&TutorialManagerImpl::OnGroupsSaved,
                                          weak_ptr_factory_.GetWeakPtr()));
