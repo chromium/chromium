33d7b9337b202e6dc0377529187a4502605ff3dc
John Abd-El-Malek <jam@chromium.org>
2021-09-30 19:29:36 +0000

    Stop IntentHelper.sendEmail from sending the email to the current user if there is no email specified.
    This was originally added in https://chromiumcodereview.appspot.com/10809061 but it didn't look like there were consumers then. Currently the only consumer is chrome://net-export, in which sending the email to oneself doesn't seem useful.
    
    The motivation for this is me wanting to move this to components to reuse in WebLayer. I first thought I'd move this dependency (see https://chromium-review.googlesource.com/c/chromium/src/+/3195531) but that had a non-trivial size impact. Then I was going to keep the dependency in Chrome, but looking closer it didn't look like we need this.
    
    Bug: 1254558
    Change-Id: I1b392dcbebc4a8779b4c6ab02fc5a2c1cbf38c64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197833
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926880}

--- chrome/android/java/src/org/chromium/chrome/browser/IntentHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/IntentHelper.java
@@ -4,21 +4,16 @@
 
 package org.chromium.chrome.browser;
 
-import android.accounts.Account;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.Html;
 import android.text.TextUtils;
-import android.util.Patterns;
 
 import org.chromium.base.ContentUriUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
-import org.chromium.components.signin.AccountManagerFacadeProvider;
-import org.chromium.components.signin.AccountUtils;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Helper for issuing intents to the android framework.
@@ -29,8 +24,7 @@ public abstract class IntentHelper {
 
     /**
      * Triggers a send email intent.  If no application has registered to receive these intents,
-     * this will fail silently.  If an email is not specified and the device has exactly one
-     * account and the account name matches the email format, the email is set to the account name.
+     * this will fail silently.
      *  @param email The email address to send to.
      * @param subject The subject of the email.
      * @param body The body of the email.
@@ -41,15 +35,6 @@ public abstract class IntentHelper {
     @CalledByNative
     static void sendEmail(
             String email, String subject, String body, String chooserTitle, String fileToAttach) {
-        if (TextUtils.isEmpty(email)) {
-            List<Account> accounts = AccountUtils.getAccountsIfFulfilledOrEmpty(
-                    AccountManagerFacadeProvider.getInstance().getAccounts());
-            if (accounts.size() == 1
-                    && Patterns.EMAIL_ADDRESS.matcher(accounts.get(0).name).matches()) {
-                email = accounts.get(0).name;
-            }
-        }
-
         Intent send = new Intent(Intent.ACTION_SEND);
         send.setType("message/rfc822");
         if (!TextUtils.isEmpty(email)) send.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
