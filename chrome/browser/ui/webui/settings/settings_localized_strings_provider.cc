52b5ffad646fb4ec2038ec5e044b9ad2338d70d9
Sophey <sophey@chromium.org>
2021-09-30 22:18:42 +0000

    Revert "[fuchsia] Update strings provider to allow access to about:settings"
    This reverts commit f9364b0d0ef7e08ec36f802eb767737783bd1dee.
    
    Reason for revert: causes build failures on fuchsia after https://crrev.com/c/3195954 landed
    
    Original change's description:
    > [fuchsia] Update strings provider to allow access to about:settings
    >
    > about:settings is crashing on fuchsia because some localized strings are
    > not available. This CL provide these strings on Fuchsia the same way
    > they are provided on Linux
    >
    > Test: Browse about:settings with chromium on Fuchsia
    > Bug: 1241824
    > R=​wez@chromium.org
    >
    > Change-Id: I4fc408c6806028f0b90a5a39a339dfbe7f34f56e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3196110
    > Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926787}
    
    Bug: 1241824
    Change-Id: Iccfa450696b6886fe27064c578e528f7dd48a3ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198479
    Reviewed-by: Travis Skare <skare@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Sophey <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926957}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -360,14 +360,16 @@ void AddAppearanceStrings(content::WebUIDataSource* html_source,
     {"huge", IDS_SETTINGS_HUGE_FONT_SIZE},
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if defined(OS_FUCHSIA) || (defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS))
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
     {"systemTheme", IDS_SETTINGS_SYSTEM_THEME},
     {"useSystemTheme", IDS_SETTINGS_USE_SYSTEM_THEME},
     {"classicTheme", IDS_SETTINGS_CLASSIC_THEME},
     {"useClassicTheme", IDS_SETTINGS_USE_CLASSIC_THEME},
-    {"showWindowDecorations", IDS_SHOW_WINDOW_DECORATIONS},
 #else
     {"resetToDefaultTheme", IDS_SETTINGS_RESET_TO_DEFAULT_THEME},
+#endif
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
+    {"showWindowDecorations", IDS_SHOW_WINDOW_DECORATIONS},
 #endif
 #if defined(OS_MAC)
     {"tabsToLinks", IDS_SETTINGS_TABS_TO_LINKS_PREF},
