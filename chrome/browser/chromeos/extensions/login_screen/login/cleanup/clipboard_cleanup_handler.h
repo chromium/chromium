7d3cf0ac2f6fd40a880443f4e148072b835036a7
Jit Yao Yap <jityao@google.com>
2021-09-30 16:14:29 +0000

    Add ClipboardCleanupHandler
    This CL adds the clipboard cleanup handler for the
    chrome.login.endSession API.
    
    Bug: b:186737512
    
    Change-Id: Idf87c46b84899a979354a0888553d1aab0e2190e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3196132
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Cr-Commit-Position: refs/heads/main@{#926788}

---
+++ chrome/browser/chromeos/extensions/login_screen/login/cleanup/clipboard_cleanup_handler.h
@@ -0,0 +1,25 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_EXTENSIONS_LOGIN_SCREEN_LOGIN_CLEANUP_CLIPBOARD_CLEANUP_HANDLER_H_
+#define CHROME_BROWSER_CHROMEOS_EXTENSIONS_LOGIN_SCREEN_LOGIN_CLEANUP_CLIPBOARD_CLEANUP_HANDLER_H_
+
+#include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/cleanup_handler.h"
+
+namespace chromeos {
+
+// A cleanup handler which clears the profile's clipboard.
+// TODO(jityao, b:200678974) Add browser tests.
+class ClipboardCleanupHandler : public CleanupHandler {
+ public:
+  ClipboardCleanupHandler();
+  ~ClipboardCleanupHandler() override;
+
+  // CleanupHandler:
+  void Cleanup(CleanupHandlerCallback callback) override;
+};
+
+}  // namespace chromeos
+
+#endif  //  CHROME_BROWSER_CHROMEOS_EXTENSIONS_LOGIN_SCREEN_LOGIN_CLEANUP_CLIPBOARD_CLEANUP_HANDLER_H_
