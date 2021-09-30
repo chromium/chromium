18012c7f80641864ff6ea1cd76092fa9c510bb1e
Shakti Sahu <shaktisahu@chromium.org>
2021-09-30 04:41:35 +0000

    Download progress message UI : Fixed description text
    Changed description text for completed download progress message to a
    single line.
    
    Bug: 1248517
    Change-Id: I7790d26b27049f0ecc76a475c6ef8ee4e669ab4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194244
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926596}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadMessageUiControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadMessageUiControllerTest.java
@@ -55,7 +55,7 @@ public class DownloadMessageUiControllerTest {
     private static final String MESSAGE_TWO_DOWNLOAD_SCHEDULED = "2 downloads scheduled";
 
     private static final String DESCRIPTION_DOWNLOADING = "See notification for download status";
-    private static final String DESCRIPTION_DOWNLOAD_COMPLETE = "(0.01 KB)\nexample.com";
+    private static final String DESCRIPTION_DOWNLOAD_COMPLETE = "(0.01 KB) example.com";
     private static final String DESCRIPTION_DOWNLOAD_SCHEDULED =
             "Download will start when on Wi-Fi";
 
