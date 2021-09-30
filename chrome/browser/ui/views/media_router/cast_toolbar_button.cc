da74de2d1b950a4919fc0496684d690f4b8b3f04
tom <tluk@chromium.org>
2021-09-30 22:38:39 +0000

    Avoid fallthrough to the global NativeTheme in CastToolbarButton
    This CL ensures that calls to GetNativeTheme() in CastToolbarButton
    are only made when the View is present in a Widget hierarchy.
    
    This avoids GetNativeTheme() falling through to the global
    NativeTheme instance and instead gets the correct instance from the
    hosting Widget.
    
    This has the added benefit of ensuring that colors are correctly
    updated between theme changes.
    
    Bug: 1239676
    Change-Id: I41cd5d0645971699d8067d7391396bac522bf922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094937
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926977}

--- chrome/browser/ui/views/media_router/cast_toolbar_button.cc
+++ chrome/browser/ui/views/media_router/cast_toolbar_button.cc
@@ -17,6 +17,7 @@
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/models/image_model.h"
 #include "ui/base/models/menu_model.h"
 #include "ui/base/pointer/touch_ui_controller.h"
 #include "ui/base/theme_provider.h"
