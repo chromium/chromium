7d7024885297ff8e32f424c8685cc6f698170fd6
Austin Sullivan <asully@chromium.org>
2021-09-30 18:45:29 +0000

    FSA: Shorten test name to unblock wpt import
    This file generates extremely long -expected.txt files, which is
    blocking the landing of https://crrev.com/c/3173559.
    
    Bug: N/A
    Change-Id: Ic49a8d26858e8acc0c8226bb4b3881209cbd000d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180159
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926857}

--- third_party/blink/web_tests/external/wpt/file-system-access/sandboxed_FileSystemFileHandle-create-sync-access-handle.https.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/file-system-access/sandboxed_FileSystemFileHandle-create-sync-access-handle.https.tentative.window.js
@@ -1,7 +1,7 @@
 // META: script=resources/test-helpers.js
 // META: script=resources/sandboxed-fs-test-helpers.js
 // META: script=resources/messaging-helpers.js
-// META: script=script-tests/FileSystemFileHandle-create-sync-access-handle-dedicated-worker.js
+// META: script=script-tests/FileSystemFileHandle-create-sync-access-handle.js
 
 // This variable allows the test to differentiate between local and sandboxed
 // file systems, since createSyncAccessHandle() behavior is different each one.
