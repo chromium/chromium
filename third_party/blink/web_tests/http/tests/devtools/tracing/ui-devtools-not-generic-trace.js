e56d55b3d7c5c3d70a5dcdf0346702c820734919
Mingjing Zhang <mjzhang@chromium.org>
2021-10-01 19:52:57 +0000

    [DevTools] Fix test for vsync-aligned frame visualization
    This CL adds BeginFrame events and the frameSeqId argument in the
    ui-devtools-not-generic-trace test to reflect the change regarding frame
    alignment in DevTools frontend (https://crrev.com/c/3083645).
    
    Bug: 1192637
    Change-Id: Ifbc52308a0cb9742090e2a1726c062e314c12f95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192790
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927345}

--- third_party/blink/web_tests/http/tests/devtools/tracing/ui-devtools-not-generic-trace.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing/ui-devtools-not-generic-trace.js
@@ -41,10 +41,12 @@
     {"pid":10000,"tid":210657,"ts":382830364662,"ph":"X","cat":"viz,benchmark","name":"DirectRenderer::DrawFrame","dur":895,"tdur":895,"tts":1341607,"args":{}},
     {"pid":10000,"tid":210683,"ts":382830893114,"ph":"B","cat":"cc,disabled-by-default-devtools.timeline","name":"RasterTask","tts":28550,"args":{"tileData":{"tileId":{"id_ref":"0x298e7bb83210"},"tileResolution":"HIGH_RESOLUTION","sourceFrameNumber":37,"layerId":82}}},
     {"pid":10000,"tid":10000,"ts":100500,"ph":"I","cat":"disabled-by-default-devtools.timeline","name":"SetLayerTreeId","s":"t","tts":606560,"args":{"data":{"frame":"ui_devtools_browser_frame","layerTreeId":1}}},
-    {"pid":10000,"tid":10000,"ts":101000,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"BeginFrame","s":"t","tts":606660,"args":{"layerTreeId":1}},
-    {"pid":10000,"tid":10000,"ts":101200,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":606760,"args":{"layerTreeId":1}},
-    {"pid":10000,"tid":10000,"ts":101300,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":606860,"args":{"layerTreeId":1}},
-    {"pid":10000,"tid":10000,"ts":101400,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":606960,"args":{"layerTreeId":1}},
+    {"pid":10000,"tid":10000,"ts":101000,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"BeginFrame","s":"t","tts":606660,"args":{"layerTreeId":1,"frameSeqId":100}},
+    {"pid":10000,"tid":10000,"ts":101100,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"BeginFrame","s":"t","tts":606760,"args":{"layerTreeId":1,"frameSeqId":101}},
+    {"pid":10000,"tid":10000,"ts":101200,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":606860,"args":{"layerTreeId":1,"frameSeqId":100}},
+    {"pid":10000,"tid":10000,"ts":101200,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"BeginFrame","s":"t","tts":606860,"args":{"layerTreeId":1,"frameSeqId":102}},
+    {"pid":10000,"tid":10000,"ts":101300,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":606960,"args":{"layerTreeId":1,"frameSeqId":101}},
+    {"pid":10000,"tid":10000,"ts":101400,"ph":"I","cat":"disabled-by-default-devtools.timeline.frame","name":"DrawFrame","s":"t","tts":607060,"args":{"layerTreeId":1,"frameSeqId":102}},
   ];
 
   const timeline = UI.panels.timeline;
