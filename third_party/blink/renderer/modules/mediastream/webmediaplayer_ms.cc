d4b5ce0e6db1cdef52f89be05a8bcec16c4fe3a7
Sergey Ulanov <sergeyu@google.com>
2021-10-01 01:45:58 +0000

    Free video frame buffers when MediaStream renderer is suspended
    Previously GpuMemoryBufferVideoFramePool was kept even when the stream
    is not being rendered on the screen. As result it was still consuming
    significant amount of memory for these buffers. Updated WebMediaPlayerMS
    to free the pool when the renderer is suspended.
    
    Bug: 1180323
    Change-Id: I9a543d22e66e6258e6b981bf20aeba23e0691068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191071
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927045}

--- third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
+++ third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
@@ -153,15 +153,12 @@ class WebMediaPlayerMS::FrameDeliverer {
       : main_task_runner_(base::ThreadTaskRunnerHandle::Get()),
         player_(player),
         enqueue_frame_cb_(std::move(enqueue_frame_cb)),
-        media_task_runner_(media_task_runner) {
+        media_task_runner_(media_task_runner),
+        worker_task_runner_(worker_task_runner),
+        gpu_factories_(gpu_factories) {
     DETACH_FROM_THREAD(io_thread_checker_);
 
-    if (gpu_factories && gpu_factories->ShouldUseGpuMemoryBuffersForVideoFrames(
-                             true /* for_media_stream */)) {
-      gpu_memory_buffer_pool_ =
-          std::make_unique<media::GpuMemoryBufferVideoFramePool>(
-              media_task_runner, worker_task_runner, gpu_factories);
-    }
+    CreateGpuMemoryBufferPoolIfNecessary();
   }
 
   FrameDeliverer(const FrameDeliverer&) = delete;
@@ -169,11 +166,7 @@ class WebMediaPlayerMS::FrameDeliverer {
 
   ~FrameDeliverer() {
     DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
-    if (gpu_memory_buffer_pool_) {
-      DropCurrentPoolTasks();
-      media_task_runner_->DeleteSoon(FROM_HERE,
-                                     gpu_memory_buffer_pool_.release());
-    }
+    FreeGpuMemoryBufferPool();
   }
 
   void OnVideoFrame(scoped_refptr<media::VideoFrame> frame) {
@@ -236,6 +229,12 @@ class WebMediaPlayerMS::FrameDeliverer {
   void SetRenderFrameSuspended(bool render_frame_suspended) {
     DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
     render_frame_suspended_ = render_frame_suspended;
+    if (render_frame_suspended_) {
+      // Drop GpuMemoryBuffer pool to free memory.
+      FreeGpuMemoryBufferPool();
+    } else {
+      CreateGpuMemoryBufferPoolIfNecessary();
+    }
   }
 
   WTF::CrossThreadRepeatingFunction<
@@ -247,6 +246,26 @@ class WebMediaPlayerMS::FrameDeliverer {
 
  private:
   friend class WebMediaPlayerMS;
+
+  void CreateGpuMemoryBufferPoolIfNecessary() {
+    if (!gpu_memory_buffer_pool_ && gpu_factories_ &&
+        gpu_factories_->ShouldUseGpuMemoryBuffersForVideoFrames(
+            true /* for_media_stream */)) {
+      gpu_memory_buffer_pool_ =
+          std::make_unique<media::GpuMemoryBufferVideoFramePool>(
+              media_task_runner_, worker_task_runner_, gpu_factories_);
+    }
+  }
+
+  void FreeGpuMemoryBufferPool() {
+    DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
+
+    if (gpu_memory_buffer_pool_) {
+      DropCurrentPoolTasks();
+      media_task_runner_->DeleteSoon(FROM_HERE,
+                                     gpu_memory_buffer_pool_.release());
+    }
+  }
 
   void EnqueueFrame(int original_frame_id,
                     scoped_refptr<media::VideoFrame> frame) {
@@ -296,6 +315,9 @@ class WebMediaPlayerMS::FrameDeliverer {
   // Pool of GpuMemoryBuffers and resources used to create hardware frames.
   std::unique_ptr<media::GpuMemoryBufferVideoFramePool> gpu_memory_buffer_pool_;
   const scoped_refptr<base::SingleThreadTaskRunner> media_task_runner_;
+  const scoped_refptr<base::TaskRunner> worker_task_runner_;
+
+  media::GpuVideoAcceleratorFactories* const gpu_factories_;
 
   // Used for DCHECKs to ensure method calls are executed on the correct thread.
   THREAD_CHECKER(io_thread_checker_);
