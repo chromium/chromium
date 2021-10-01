bd5d8fb908bcae6bce5584290c3ca0b47a3e8d12
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-10-01 13:26:52 +0000

    Revert "[RSearches] Cancel Bar height animation on close"
    This reverts commit f3b9fd98283f3fa3f796776b794c0460d82f94e7.
    
    Reason for revert: Causes failed test crbug.com/1255084.
    
    Original change's description:
    > [RSearches] Cancel Bar height animation on close
    >
    > When the panel is closed there's a bug causing it to show again
    > and be stuck open when an animation is running. This is because
    > the animation sets the panel height and that implicitly sets whether
    > it's showing or not.
    >
    > The fix is to cancel animation when the Bar is destroyed.
    >
    > An additional safeguard is added to make sure we never get stuck in the
    > IDLE state with the panel showing.
    >
    > Also updates the test for the Internal State Controller to Robolectric
    > and adds test cases for the safeguard.
    >
    > BUG=1251774
    >
    > Change-Id: I476168086d9905675c3abaafb5e14dbd13350718
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177499
    > Auto-Submit: Donn Denman <donnd@chromium.org>
    > Reviewed-by: Matt Jones <mdjones@chromium.org>
    > Commit-Queue: Donn Denman <donnd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926944}
    
    Bug: 1251774, 1255084
    Change-Id: Iab78eeef6faeb74964b486028fc503e729f4a4f4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199912
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ehimare Okoyomon <eokoyomon@google.com>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927190}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchBarControl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchBarControl.java
@@ -122,9 +122,6 @@ public class ContextualSearchBarControl {
     /** The max height of the Related Searches section of the Bar, used for shrink animation. */
     private float mInBarRelatedSearchesMaxHeightForShrinkAnimation;
 
-    /** A way to notify tests when the in-bar animation changes. */
-    private Runnable mInBarAnimationTestNotifier;
-
     /**
      * Constructs a new bottom bar control container by inflating views from XML.
      *
@@ -185,11 +182,6 @@ public class ContextualSearchBarControl {
      * Removes the bottom bar views from the parent container.
      */
     public void destroy() {
-        // Make sure animations are canceled otherwise setting the height can put it into an
-        // inconsistent state.
-        if (mInBarRelatedSearchesAnimation != null) {
-            mInBarRelatedSearchesAnimation.cancel();
-        }
         mContextControl.destroy();
         mSearchTermControl.destroy();
         mCaptionControl.destroy();
@@ -591,7 +583,6 @@ public class ContextualSearchBarControl {
         if (mInBarRelatedSearchesAnimation == null || mInBarRelatedSearchesAnimation.hasEnded()) {
             clearCacheMaxHeightForShrinkAnimation();
         }
-        if (mInBarAnimationTestNotifier != null) mInBarAnimationTestNotifier.run();
     }
 
     /** Returns the maximum height of the Related Searches UI that we show right in the Bar. */
@@ -620,11 +611,5 @@ public class ContextualSearchBarControl {
      */
     float getInBarRelatedSearchesAnimatedHeightDps() {
         return mInBarRelatedSearchesAnimatedHeightDps;
-    }
-
-    @VisibleForTesting
-    public void setInBarAnimationTestNotifier(Runnable runnable) {
-        assert mInBarAnimationTestNotifier == null;
-        mInBarAnimationTestNotifier = runnable;
     }
 }
