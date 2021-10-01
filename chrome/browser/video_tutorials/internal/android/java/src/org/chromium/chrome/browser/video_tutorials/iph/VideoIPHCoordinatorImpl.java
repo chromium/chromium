5b673bc87fc9ffa307e23d40e9e4011dc9c21021
Shakti Sahu <shaktisahu@chromium.org>
2021-10-01 04:37:42 +0000

    Video Tutorials: Show static image for summary card
    As per UX spec, we will mostly use a static thumbnail for the summary
    card unlike other videos where we show an animated GIF. This CL adds
    support for a finch supplied boolean to control that.
    
    (cherry picked from commit b913243f7ca33ae8b466afd456b5b8d4b8529fb1)
    
    Bug: 1254071
    Change-Id: Idd949d301c270a1f5dba07690eeddde39a940962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192031
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#925976}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199451
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4638@{#496}
    Cr-Branched-From: 159257cab5585bc8421abf347984bb32fdfe9eb9-refs/heads/main@{#920003}

--- chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/iph/VideoIPHCoordinatorImpl.java
+++ chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/iph/VideoIPHCoordinatorImpl.java
@@ -12,6 +12,7 @@ import android.view.ViewStub;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.video_tutorials.FeatureType;
 import org.chromium.chrome.browser.video_tutorials.Tutorial;
 import org.chromium.chrome.browser.video_tutorials.VideoTutorialUtils;
 import org.chromium.components.image_fetcher.ImageFetcher;
@@ -26,6 +27,8 @@ import jp.tomorrowkey.android.gifplayer.BaseGifDrawable;
  */
 public class VideoIPHCoordinatorImpl implements VideoIPHCoordinator {
     private static final String VARIATION_USE_ANIMATED_GIF_URL = "use_animated_gif_url";
+    private static final String VARIATION_USE_ANIMATED_GIF_URL_FOR_SUMMARY_CARD =
+            "use_animated_gif_url_for_summary_card";
 
     private final Context mContext;
     private final PropertyModel mModel;
@@ -77,8 +80,12 @@ public class VideoIPHCoordinatorImpl implements VideoIPHCoordinator {
 
     private void fetchImage(
             Callback<Drawable> consumer, int widthPx, int heightPx, Tutorial tutorial) {
+        boolean isSummaryCard = tutorial.featureType == FeatureType.SUMMARY;
         boolean useAnimatedGifUrl = ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
-                ChromeFeatureList.VIDEO_TUTORIALS, VARIATION_USE_ANIMATED_GIF_URL, true);
+                ChromeFeatureList.VIDEO_TUTORIALS,
+                isSummaryCard ? VARIATION_USE_ANIMATED_GIF_URL_FOR_SUMMARY_CARD
+                              : VARIATION_USE_ANIMATED_GIF_URL,
+                !isSummaryCard);
         ImageFetcher.Params params = ImageFetcher.Params.create(
                 useAnimatedGifUrl ? tutorial.animatedGifUrl : tutorial.thumbnailUrl,
                 ImageFetcher.VIDEO_TUTORIALS_IPH_UMA_CLIENT_NAME, widthPx, heightPx);
