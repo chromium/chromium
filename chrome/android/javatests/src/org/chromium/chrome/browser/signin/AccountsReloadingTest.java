177b0af64c45b7b6d205170f75efc6a08e40cbf5
Peter Kotwicz <pkotwicz@chromium.org>
2021-10-02 00:51:40 +0000

    Disable testRefreshTokenUpdateWhenSignedInAndSyncUserAddsNewAccount on L
    This CL disables
    testRefreshTokenUpdateWhenSignedInAndSyncUserAddsNewAccount() on
    Lollipop.
    
    Bug: 1254427
    
    Change-Id: Ifea06ef9befbbafc8441c3fe2ea1dde7dc1cdd91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197494
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927478}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountsReloadingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountsReloadingTest.java
@@ -20,6 +20,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
@@ -194,8 +195,7 @@ public class AccountsReloadingTest {
 
     @Test
     @MediumTest
-    @DisableIf.
-    Build(sdk_is_greater_than = Build.VERSION_CODES.M, message = "http://crbug.com/1254427")
+    @DisabledTest(message = "http://crbug.com/1254427")
     public void testRefreshTokenUpdateWhenSignedInAndSyncUserAddsNewAccount() {
         final CoreAccountInfo account1 =
                 mAccountManagerTestRule.addTestAccountThenSigninAndEnableSync();
