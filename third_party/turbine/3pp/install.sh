7cbf6f5fc57189899771ef5c1bbbfb8c6f398677
Peter Wen <wnwen@chromium.org>
2021-10-04 13:45:01 +0000

    Android: Fix 3pp turbine install.sh maven version
    Maven updated from 3.8.1 to 3.8.3 so the original files are no longer
    available.
    
    Bug: None
    Change-Id: I99d028c440cb3f24b3a29f0100f222b453225605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202111
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927662}

--- third_party/turbine/3pp/install.sh
+++ third_party/turbine/3pp/install.sh
@@ -11,16 +11,17 @@ PREFIX="$1"
 DEPS_PREFIX="$2"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+MAVEN_VERSION="3.8.3"
 
-curl -O https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
-tar xzf apache-maven-3.8.1-bin.tar.gz
-mv apache-maven-3.8.1 $SCRIPT_DIR/
+curl -O https://downloads.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+tar xzf apache-maven-$MAVEN_VERSION-bin.tar.gz
+mv apache-maven-$MAVEN_VERSION $SCRIPT_DIR/
 
 # Verify mvn works
-JAVA_HOME=$DEPS_PREFIX/current PATH=$SCRIPT_DIR/apache-maven-3.8.1/bin:$PATH mvn -v
+JAVA_HOME=$DEPS_PREFIX/current PATH=$SCRIPT_DIR/apache-maven-$MAVEN_VERSION/bin:$PATH mvn -v
 
 # Build
-JAVA_HOME=$DEPS_PREFIX/current PATH=$SCRIPT_DIR/apache-maven-3.8.1/bin:$PATH mvn package -DskipTests=true -q -f pom.xml
+JAVA_HOME=$DEPS_PREFIX/current PATH=$SCRIPT_DIR/apache-maven-$MAVEN_VERSION/bin:$PATH mvn package -DskipTests=true -q -f pom.xml
 cp target/turbine-HEAD-SNAPSHOT-all-deps.jar turbine.jar
 
 mkdir -p $PREFIX/
