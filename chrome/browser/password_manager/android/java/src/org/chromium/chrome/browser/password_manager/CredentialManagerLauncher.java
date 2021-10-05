ee6048c51a8f929ab9c82e6d4fdafd58ab937ab3
Ioana Pandele <ioanap@chromium.org>
2021-10-05 15:20:55 +0000

    [CredManager] Add interfaces for the credential manager launcher
    This is part 1 of a series of 3 changes:
    1. Add interfaces for the credential manager launcher (upstream)
    2. Add implementation for the credential manager launcher (downstream)
    3. Add public implementation for the credential manager launcher and
    exclude implementations from the jar so that upstream and downstream
    can explicitly provide their own version of the implementation (upstream)
    
    Bug: 1255038
    Change-Id: I697d82c269bf64d82835a97c5b1f0a250199c3a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205199
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928160}

---
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/CredentialManagerLauncher.java
@@ -0,0 +1,13 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.password_manager;
+/**
+ * Interface for the launcher responsible for opening the Credential Manager.
+ */
+public interface CredentialManagerLauncher {
+    /**
+     * Launches the UI surface allowing users to manage their saved passwords.
+     */
+    void launchCredentialManager();
+}
