5467af18e1c8e4fec7f86d46e488b80b04092196
Peter Wen <wnwen@chromium.org>
2021-10-05 17:53:18 +0000

    Android: Re-enable androidx custom lint checks (reland)
    Androidx custom lint checks were disabled due to using newer APIs than
    the lint version we were using allowed. Now that we have updated to lint
    version 7.0.0, these checks are working again.
    
    The new --remove-fixed flag is also turned on in lint.py so that
    baseline.xml files do not keep growing but get trimmed when fixes are
    merged.
    
    Original CL: https://crrev.com/c/3200345
    
    Fixed:
    - Removed --remove-fixed flag. Context in: crbug.com/1256477#c5
    
    Bug: 1111416, 1225326, 1256477
    Fixed: 1111416, 1225326, 1256477
    Cq-Include-Trybots: luci.chrome.try:android-internal-rel
    Change-Id: I332509d0a23c28f8bca5dcebcba2fdd2e414e8e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205911
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928240}

--- build/android/gyp/lint.py
+++ build/android/gyp/lint.py
@@ -218,6 +218,11 @@ def _RunLint(lint_binary_path,
 
   cmd = [
       lint_binary_path,
+      # Uncomment to update baseline files during lint upgrades.
+      #'--update-baseline',
+      # Uncomment to easily remove fixed lint errors. This is not turned on by
+      # default due to: https://crbug.com/1256477#c5
+      #'--remove-fixed',
       '--quiet',  # Silences lint's "." progress updates.
       '--disable',
       ','.join(_DISABLED_ALWAYS),
@@ -265,10 +270,6 @@ def _RunLint(lint_binary_path,
   custom_annotation_zips = []
   if aars:
     for aar in aars:
-      # androidx custom lint checks require a newer version of lint. Disable
-      # until we update see https://crbug.com/1225326
-      if 'androidx' in aar:
-        continue
       # Use relative source for aar files since they are not generated.
       aar_dir = os.path.join(aar_root_dir,
                              os.path.splitext(_SrcRelative(aar))[0])
