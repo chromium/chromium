fafa8f21b503fc462e88a53463ba953b90b02254
Fabrice de Gans <fdegans@chromium.org>
2021-10-05 22:17:38 +0000

    [fuchsia] Fix the local-sdk script
    fvm_image_size is no longer used by Fuchsia.
    
    Change-Id: I69f1be3aee3d5ffb9df1548f03a40f2aab14d7af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206957
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928376}

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -34,16 +34,9 @@ def EnsureEmptyDir(path):
 
 
 def BuildForArch(arch):
-  Run(
-      'scripts/fx',
-      '--dir',
-      'out/release-{}'.format(arch),
-      'set',
-      'terminal.qemu-{}'.format(arch),
-      '--args=is_debug=false',
-      '--args=build_sdk_archives=true',
-      # Increase the size of the image to allow multiple test runs.
-      '--args=fvm_image_size={}'.format(512 * 1024 * 1024))
+  Run('scripts/fx', '--dir', 'out/release-{}'.format(arch), 'set',
+      'terminal.qemu-{}'.format(arch), '--args=is_debug=false',
+      '--args=build_sdk_archives=true')
   Run('scripts/fx', 'build', 'sdk', 'build/images')
 
 
