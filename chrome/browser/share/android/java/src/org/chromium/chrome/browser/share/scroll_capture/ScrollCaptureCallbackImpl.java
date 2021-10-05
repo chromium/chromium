9f22076a909dd3e77855abf22619748aed5c2c27
Yashar Dabiran <yashard@chromium.org>
2021-10-05 21:43:55 +0000

    [M95][Scroll Capture] Avoid sending transparent captures
    The color White is drawn to Canvas before the capture bitmap draw to
    ensure we send opaque captures to the OS.
    
    (cherry picked from commit 0b1b3ac1325f570c6b2989ca7c368ccd0804a538)
    
    Bug: 1254844
    Change-Id: Id3f97327c7b1430a131cd990529ec6cdbae8868e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199503
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#927423}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205801
    Cr-Commit-Position: refs/branch-heads/4638@{#622}
    Cr-Branched-From: 159257cab5585bc8421abf347984bb32fdfe9eb9-refs/heads/main@{#920003}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/scroll_capture/ScrollCaptureCallbackImpl.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/scroll_capture/ScrollCaptureCallbackImpl.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.share.scroll_capture;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Build.VERSION_CODES;
 import android.os.CancellationSignal;
@@ -165,6 +166,7 @@ public class ScrollCaptureCallbackImpl implements ScrollCaptureCallback {
 
             Rect destRect = new Rect(0, 0, captureArea.width(), captureArea.height());
             Canvas canvas = session.getSurface().lockCanvas(destRect);
+            canvas.drawColor(Color.WHITE);
             canvas.drawBitmap(bitmap, null, destRect, null);
             session.getSurface().unlockCanvasAndPost(canvas);
             // Translate the captureArea Rect back to its original coordinates.
