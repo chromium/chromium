ad666a27273b94315ce2885848a1f5ba5dae286a
Dominic Farolino <dom@chromium.org>
2021-10-05 23:00:31 +0000

    Fenced Frames: Restrict return value of location.ancestorOrigins
    This CL modifies `Location::ancestorOrigin` to honor a fenced frame's
    ShadowDOM fence, via the modifications to FrameTree::Parent() in [1].
    
    It also adds tests for:
     - Top-level fenced frames
     - Iframes nested in fenced frames
     - Nested fenced frames
    
    This work is done as a part of the Fenced Frame + ShadowDOM work in [2].
    
    [1]: https://crrev.com/c/3199540
    [2]: https://docs.google.com/document/d/1ijTZJT3DHQ1ljp4QQe4E4XCCRaYAxmInNzN1SzeJM8s/edit#bookmark=id.kixaimljfrt
    
    R=dcheng@chromium.org, shivanisha@chromium.org
    
    Bug: 1123606
    Change-Id: If57253e3ae99f67afbda973c15357c57cc595e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197997
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928395}

--- third_party/blink/web_tests/wpt_internal/fenced_frame/resources/utils.js
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/utils.js
@@ -12,17 +12,20 @@ const STORE_URL = '/wpt_internal/fenced_frame/resources/key-value-store.py';
 // otherwise interfere with each other's server state.
 const KEYS = {
   // This key is only used to test that the server-side stash works properly.
-  "dummy"                 : "00000000-0000-0000-0000-000000000000",
+  "dummy"                        : "00000000-0000-0000-0000-000000000000",
 
-  "document.referrer"     : "00000000-0000-0000-0000-000000000001",
-  "document.referrer ACK" : "00000000-0000-0000-0000-000000000002",
+  "document.referrer"            : "00000000-0000-0000-0000-000000000001",
+  "document.referrer ACK"        : "00000000-0000-0000-0000-000000000002",
 
-  "window.top"            : "00000000-0000-0000-0000-000000000003",
-  "window.top ACK"        : "00000000-0000-0000-0000-000000000004",
+  "window.top"                   : "00000000-0000-0000-0000-000000000003",
+  "window.top ACK"               : "00000000-0000-0000-0000-000000000004",
 
-  "window.parent"         : "00000000-0000-0000-0000-000000000005",
-  "window.parent ACK"     : "00000000-0000-0000-0000-000000000006",
-  // Add keys below this list, incrementing the key UUID in hexadecimal.
+  "window.parent"                : "00000000-0000-0000-0000-000000000005",
+  "window.parent ACK"            : "00000000-0000-0000-0000-000000000006",
+
+  "location.ancestorOrigins"     : "00000000-0000-0000-0000-000000000007",
+  "location.ancestorOrigins ACK" : "00000000-0000-0000-0000-000000000008",
+  // Add keys below this list, incrementing the key UUID in hexadecimal:
 }
 
 function attachFencedFrame(url) {
