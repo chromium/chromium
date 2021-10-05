64d636617895bed938872136972692cb6d9a64ca
dpapad <dpapad@chromium.org>
2021-10-05 23:06:13 +0000

    WebUI: Add explicit Rollup dependency, update version 1.32.1 -> 2.58.0.
    Also adding missing JS imports in
    c/b/r/new_tab_page/.../chrome_cart_proxy.js, to fix runtime errors
    occurring with the new Rollup version.
    
    Bug: 1255075
    Change-Id: Id80b87ff309ef7891682422baf381527e83d51c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197722
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928403}

--- chrome/browser/resources/new_tab_page/modules/cart/chrome_cart_proxy.js
+++ chrome/browser/resources/new_tab_page/modules/cart/chrome_cart_proxy.js
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(crbug.com/1179821): Migrate to JS module Mojo bindings.
+import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://resources/mojo/url/mojom/url.mojom-lite.js';
 import './chrome_cart.mojom-lite.js';
 
 /**
