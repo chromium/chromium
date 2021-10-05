d5c5f6cbd62d979e491c3baeea9ef2dba0691faa
Robert Sesek <rsesek@chromium.org>
2021-10-05 23:03:57 +0000

    Reland "mac: Refactor the linker_driver.py to not use global variables."
    This is a reland of 38803034c5e507e668f620161e810e1efd823855
    
    Original change's description:
    > mac: Refactor the linker_driver.py to not use global variables.
    >
    > Bug: 1255223
    > Change-Id: I15f28c39da3bc856cec0a4514443bf3fe566b89e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200450
    > Commit-Queue: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#927836}
    
    Bug: 1255223
    Change-Id: I05e5eddb8d348994962128e1c3af98ca040faef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205812
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928401}

--- build/toolchain/apple/linker_driver.py
+++ build/toolchain/apple/linker_driver.py
@@ -10,14 +10,8 @@ import shutil
 import subprocess
 import sys
 
-# On mac, the values of these globals are modified when parsing -Wcrl, flags. On
-# ios, the script uses the defaults.
-DSYMUTIL_INVOKE = ['xcrun', 'dsymutil']
-STRIP_INVOKE = ['xcrun', 'strip']
-
-# Setting this flag will emit a deterministic binary by stripping dates from the
-# N_OSO field.
-DETERMINISTIC_FLAG = '--deterministic'
+# Prefix for all custom linker driver arguments.
+LINKER_DRIVER_ARG_PREFIX = '-Wcrl,'
 
 # The linker_driver.py is responsible for forwarding a linker invocation to
 # the compiler driver, while processing special arguments itself.
@@ -33,242 +27,245 @@ DETERMINISTIC_FLAG = '--deterministic'
 # removal of the special driver arguments, described below). Then the driver
 # performs additional actions, based on these arguments:
 #
-#   -Wcrl,dsym,<dsym_path_prefix>
-#       After invoking the linker, this will run `dsymutil` on the linker's
-#       output, producing a dSYM bundle, stored at dsym_path_prefix. As an
-#       example, if the linker driver were invoked with:
-#         "... -o out/gn/obj/foo/libbar.dylib ... -Wcrl,dsym,out/gn ..."
-#       The resulting dSYM would be out/gn/libbar.dylib.dSYM/.
+# -Wcrl,dsym,<dsym_path_prefix>
+#    After invoking the linker, this will run `dsymutil` on the linker's
+#    output, producing a dSYM bundle, stored at dsym_path_prefix. As an
+#    example, if the linker driver were invoked with:
+#        "... -o out/gn/obj/foo/libbar.dylib ... -Wcrl,dsym,out/gn ..."
+#    The resulting dSYM would be out/gn/libbar.dylib.dSYM/.
 #
-#   -Wcrl,dsymutilpath,<dsymutil_path>
-#       Sets the path to the dsymutil to run with -Wcrl,dsym, in which case
-#       `xcrun` is not used to invoke it.
+# -Wcrl,dsymutilpath,<dsymutil_path>
+#    Sets the path to the dsymutil to run with -Wcrl,dsym, in which case
+#    `xcrun` is not used to invoke it.
 #
-#   -Wcrl,unstripped,<unstripped_path_prefix>
-#       After invoking the linker, and before strip, this will save a copy of
-#       the unstripped linker output in the directory unstripped_path_prefix.
+# -Wcrl,unstripped,<unstripped_path_prefix>
+#    After invoking the linker, and before strip, this will save a copy of
+#    the unstripped linker output in the directory unstripped_path_prefix.
 #
-#   -Wcrl,strip,<strip_arguments>
-#       After invoking the linker, and optionally dsymutil, this will run
-#       the strip command on the linker's output. strip_arguments are
-#       comma-separated arguments to be passed to the strip command.
+# -Wcrl,strip,<strip_arguments>
+#    After invoking the linker, and optionally dsymutil, this will run
+#    the strip command on the linker's output. strip_arguments are
+#    comma-separated arguments to be passed to the strip command.
 #
-#   -Wcrl,strippath,<strip_path>
-#       Sets the path to the strip to run with -Wcrl,strip, in which case
-#       `xcrun` is not used to invoke it.
+# -Wcrl,strippath,<strip_path>
+#    Sets the path to the strip to run with -Wcrl,strip, in which case
+#    `xcrun` is not used to invoke it.
 
 
-def main(args):
-    """Main function for the linker driver. Separates out the arguments for
-    the main compiler driver and the linker driver, then invokes all the
-    required tools.
+class LinkerDriver(object):
+    def __init__(self, args):
+        """Creates a new linker driver.
 
-    Args:
-      args: list of string, Arguments to the script.
-    """
+        Args:
+            args: list of string, Arguments to the script.
+        """
+        if len(args) < 2:
+            raise RuntimeError("Usage: linker_driver.py [linker-invocation]")
+        self._args = args
 
-    if len(args) < 2:
-        raise RuntimeError("Usage: linker_driver.py [linker-invocation]")
+        # List of linker driver actions. **The sort order of this list affects
+        # the order in which the actions are invoked.**
+        # The first item in the tuple is the argument's -Wcrl,<sub_argument>
+        # and the second is the function to invoke.
+        self._actions = [
+            ('dsymutilpath,', self.set_dsymutil_path),
+            ('dsym,', self.run_dsymutil),
+            ('unstripped,', self.run_save_unstripped),
+            ('strippath,', self.set_strip_path),
+            ('strip,', self.run_strip),
+        ]
 
-    # Collect arguments to the linker driver (this script) and remove them from
-    # the arguments being passed to the compiler driver.
-    linker_driver_actions = {}
-    compiler_driver_args = []
-    deterministic = False
-    for arg in args[1:]:
-        if arg.startswith(_LINKER_DRIVER_ARG_PREFIX):
-            # Convert driver actions into a map of name => lambda to invoke.
-            driver_action = process_linker_driver_arg(arg)
-            assert driver_action[0] not in linker_driver_actions
-            linker_driver_actions[driver_action[0]] = driver_action[1]
-        elif arg == DETERMINISTIC_FLAG:
-            deterministic = True
-        else:
-            compiler_driver_args.append(arg)
+        # Linker driver actions can modify the these values.
+        self._dsymutil_cmd = ['xcrun', 'dsymutil']
+        self._strip_cmd = ['xcrun', 'strip']
 
-    linker_driver_outputs = [_find_linker_output(compiler_driver_args)]
+        # The linker output file, lazily computed in self._get_linker_output().
+        self._linker_output = None
 
-    try:
-        # Zero the mtime in OSO fields for deterministic builds.
-        # https://crbug.com/330262.
-        env = os.environ.copy()
-        if deterministic:
+    def run(self):
+        """Runs the linker driver, separating out the main compiler driver's
+        arguments from the ones handled by this class. It then invokes the
+        required tools, starting with the compiler driver to produce the linker
+        output.
+        """
+        # Collect arguments to the linker driver (this script) and remove them
+        # from the arguments being passed to the compiler driver.
+        linker_driver_actions = {}
+        compiler_driver_args = []
+        for index, arg in enumerate(self._args[1:]):
+            if arg.startswith(LINKER_DRIVER_ARG_PREFIX):
+                # Convert driver actions into a map of name => lambda to invoke.
+                driver_action = self._process_driver_arg(arg)
+                assert driver_action[0] not in linker_driver_actions
+                linker_driver_actions[driver_action[0]] = driver_action[1]
+            else:
+                compiler_driver_args.append(arg)
+
+        if self._get_linker_output() is None:
+            raise ValueError(
+                'Could not find path to linker output (-o or --output)')
+
+        linker_driver_outputs = [self._get_linker_output()]
+
+        try:
+            # Zero the mtime in OSO fields for deterministic builds.
+            # https://crbug.com/330262.
+            env = os.environ.copy()
             env['ZERO_AR_DATE'] = '1'
-        # Run the linker by invoking the compiler driver.
-        subprocess.check_call(compiler_driver_args, env=env)
+            # Run the linker by invoking the compiler driver.
+            subprocess.check_call(compiler_driver_args, env=env)
 
-        # Run the linker driver actions, in the order specified by the actions
-        # list.
-        for action in _LINKER_DRIVER_ACTIONS:
-            name = action[0]
-            if name in linker_driver_actions:
-                linker_driver_outputs += linker_driver_actions[name](args)
-    except:
-        # If a linker driver action failed, remove all the outputs to make the
-        # build step atomic.
-        map(_remove_path, linker_driver_outputs)
+            # Run the linker driver actions, in the order specified by the
+            # actions list.
+            for action in self._actions:
+                name = action[0]
+                if name in linker_driver_actions:
+                    linker_driver_outputs += linker_driver_actions[name]()
+        except:
+            # If a linker driver action failed, remove all the outputs to make
+            # the build step atomic.
+            map(_remove_path, linker_driver_outputs)
 
-        # Re-report the original failure.
-        raise
+            # Re-report the original failure.
+            raise
 
+    def _get_linker_output(self):
+        """Returns the value of the output argument to the linker."""
+        if not self._linker_output:
+            for index, arg in enumerate(self._args):
+                if arg in ('-o', '-output', '--output'):
+                    self._linker_output = self._args[index + 1]
+                    break
+        return self._linker_output
 
-def process_linker_driver_arg(arg):
-    """Processes a linker driver argument and returns a tuple containing the
-    name and unary lambda to invoke for that linker driver action.
+    def _process_driver_arg(self, arg):
+        """Processes a linker driver argument and returns a tuple containing the
+        name and unary lambda to invoke for that linker driver action.
 
-    Args:
-      arg: string, The linker driver argument.
+        Args:
+            arg: string, The linker driver argument.
 
-    Returns:
-      A 2-tuple:
-        0: The driver action name, as in _LINKER_DRIVER_ACTIONS.
-        1: An 1-ary lambda that takes the full list of arguments passed to
-           Main(). The lambda should call the linker driver action that
-           corresponds to the argument and return a list of outputs from the
-           action.
-    """
-    if not arg.startswith(_LINKER_DRIVER_ARG_PREFIX):
-        raise ValueError('%s is not a linker driver argument' % (arg, ))
+        Returns:
+            A 2-tuple:
+                0: The driver action name, as in |self._actions|.
+                1: A lambda that calls the linker driver action with its direct
+                   argument and returns a list of outputs from the action.
+        """
+        if not arg.startswith(LINKER_DRIVER_ARG_PREFIX):
+            raise ValueError('%s is not a linker driver argument' % (arg, ))
 
-    sub_arg = arg[len(_LINKER_DRIVER_ARG_PREFIX):]
+        sub_arg = arg[len(LINKER_DRIVER_ARG_PREFIX):]
 
-    for driver_action in _LINKER_DRIVER_ACTIONS:
-        (name, action) = driver_action
-        if sub_arg.startswith(name):
-            return (name,
-                    lambda full_args: action(sub_arg[len(name):], full_args))
+        for driver_action in self._actions:
+            (name, action) = driver_action
+            if sub_arg.startswith(name):
+                return (name, lambda: action(sub_arg[len(name):]))
 
-    raise ValueError('Unknown linker driver argument: %s' % (arg, ))
+        raise ValueError('Unknown linker driver argument: %s' % (arg, ))
 
+    def run_dsymutil(self, dsym_path_prefix):
+        """Linker driver action for -Wcrl,dsym,<dsym-path-prefix>. Invokes
+        dsymutil on the linker's output and produces a dsym file at |dsym_file|
+        path.
 
-def run_dsym_util(dsym_path_prefix, full_args):
-    """Linker driver action for -Wcrl,dsym,<dsym-path-prefix>. Invokes dsymutil
-    on the linker's output and produces a dsym file at |dsym_file| path.
+        Args:
+            dsym_path_prefix: string, The path at which the dsymutil output
+                should be located.
 
-    Args:
-      dsym_path_prefix: string, The path at which the dsymutil output should be
-          located.
-      full_args: list of string, Full argument list for the linker driver.
+        Returns:
+            list of string, Build step outputs.
+        """
+        if not len(dsym_path_prefix):
+            raise ValueError('Unspecified dSYM output file')
 
-    Returns:
-        list of string, Build step outputs.
-    """
-    if not len(dsym_path_prefix):
-        raise ValueError('Unspecified dSYM output file')
+        linker_output = self._get_linker_output()
+        base = os.path.basename(linker_output)
+        dsym_out = os.path.join(dsym_path_prefix, base + '.dSYM')
 
-    linker_out = _find_linker_output(full_args)
-    base = os.path.basename(linker_out)
-    dsym_out = os.path.join(dsym_path_prefix, base + '.dSYM')
+        # Remove old dSYMs before invoking dsymutil.
+        _remove_path(dsym_out)
 
-    # Remove old dSYMs before invoking dsymutil.
-    _remove_path(dsym_out)
+        tools_paths = _find_tools_paths(self._args)
+        if os.environ.get('PATH'):
+            tools_paths.append(os.environ['PATH'])
+        dsymutil_env = os.environ.copy()
+        dsymutil_env['PATH'] = ':'.join(tools_paths)
+        subprocess.check_call(self._dsymutil_cmd +
+                              ['-o', dsym_out, linker_output],
+                              env=dsymutil_env)
+        return [dsym_out]
 
-    tools_paths = _find_tools_path(full_args)
-    if os.environ.get('PATH'):
-        tools_paths.append(os.environ['PATH'])
-    dsymutil_env = os.environ.copy()
-    dsymutil_env['PATH'] = ':'.join(tools_paths)
-    subprocess.check_call(DSYMUTIL_INVOKE + ['-o', dsym_out, linker_out],
-                          env=dsymutil_env)
-    return [dsym_out]
+    def set_dsymutil_path(self, dsymutil_path):
+        """Linker driver action for -Wcrl,dsymutilpath,<dsymutil_path>.
 
+        Sets the invocation command for dsymutil, which allows the caller to
+        specify an alternate dsymutil. This action is always processed before
+        the RunDsymUtil action.
 
-def set_dsymutil_path(dsymutil_path, full_args):
-    """Linker driver action for -Wcrl,dsymutilpath,<dsymutil_path>.
+        Args:
+            dsymutil_path: string, The path to the dsymutil binary to run
 
-    Sets the invocation command for dsymutil, which allows the caller to specify
-    an alternate dsymutil. This action is always processed before the
-    run_dsym_util action.
+        Returns:
+            No output - this step is run purely for its side-effect.
+        """
+        self._dsymutil_cmd = [dsymutil_path]
+        return []
 
-    Args:
-      dsymutil_path: string, The path to the dsymutil binary to run
-      full_args: list of string, Full argument list for the linker driver.
+    def run_save_unstripped(unstripped_path_prefix):
+        """Linker driver action for -Wcrl,unstripped,<unstripped_path_prefix>.
+        Copies the linker output to |unstripped_path_prefix| before stripping.
 
-    Returns:
-      No output - this step is run purely for its side-effect.
-    """
-    global DSYMUTIL_INVOKE
-    DSYMUTIL_INVOKE = [dsymutil_path]
-    return []
+        Args:
+            unstripped_path_prefix: string, The path at which the unstripped
+                output should be located.
 
+        Returns:
+            list of string, Build step outputs.
+        """
+        if not len(unstripped_path_prefix):
+            raise ValueError('Unspecified unstripped output file')
 
-def run_save_unstripped(unstripped_path_prefix, full_args):
-    """Linker driver action for -Wcrl,unstripped,<unstripped_path_prefix>.
-    Copies the linker output to |unstripped_path_prefix| before stripping.
+        base = os.path.basename(self._get_linker_output())
+        unstripped_out = os.path.join(unstripped_path_prefix,
+                                      base + '.unstripped')
 
-    Args:
-      unstripped_path_prefix: string, The path at which the unstripped output
-          should be located.
-      full_args: list of string, Full argument list for the linker driver.
+        shutil.copyfile(linker_out, unstripped_out)
+        return [unstripped_out]
 
-    Returns:
-      list of string, Build step outputs.
-    """
-    if not len(unstripped_path_prefix):
-        raise ValueError('Unspecified unstripped output file')
+    def run_strip(self, strip_args_string):
+        """Linker driver action for -Wcrl,strip,<strip_arguments>.
 
-    linker_out = _find_linker_output(full_args)
-    base = os.path.basename(linker_out)
-    unstripped_out = os.path.join(unstripped_path_prefix, base + '.unstripped')
+        Args:
+            strip_args_string: string, Comma-separated arguments for `strip`.
 
-    shutil.copyfile(linker_out, unstripped_out)
-    return [unstripped_out]
+        Returns:
+            list of string, Build step outputs.
+        """
+        strip_command = list(self._strip_cmd)
+        if len(strip_args_string) > 0:
+            strip_command += strip_args_string.split(',')
+        strip_command.append(self._get_linker_output())
+        subprocess.check_call(strip_command)
+        return []
 
+    def set_strip_path(self, strip_path):
+        """Linker driver action for -Wcrl,strippath,<strip_path>.
 
-def run_strip(strip_args_string, full_args):
-    """Linker driver action for -Wcrl,strip,<strip_arguments>.
+        Sets the invocation command for strip, which allows the caller to
+        specify an alternate strip. This action is always processed before the
+        RunStrip action.
 
-    Args:
-        strip_args_string: string, Comma-separated arguments for `strip`.
-        full_args: list of string, Full arguments for the linker driver.
+        Args:
+            strip_path: string, The path to the strip binary to run
 
-    Returns:
-        list of string, Build step outputs.
-    """
-    strip_command = list(STRIP_INVOKE)
-    if len(strip_args_string) > 0:
-        strip_command += strip_args_string.split(',')
-    strip_command.append(_find_linker_output(full_args))
-    subprocess.check_call(strip_command)
-    return []
-
-
-def set_strip_path(strip_path, full_args):
-    """Linker driver action for -Wcrl,strippath,<strip_path>.
-
-    Sets the invocation command for strip, which allows the caller to specify
-    an alternate strip. This action is always processed before the run_strip
-    action.
-
-    Args:
-      strip_path: string, The path to the strip binary to run
-      full_args: list of string, Full argument list for the linker driver.
-
-    Returns:
-      No output - this step is run purely for its side-effect.
-    """
-    global STRIP_INVOKE
-    STRIP_INVOKE = [strip_path]
-    return []
+        Returns:
+            No output - this step is run purely for its side-effect.
+        """
+        self._strip_cmd = [strip_path]
+        return []
 
 
-def _find_linker_output(full_args):
-    """Finds the output of the linker by looking for the output flag in its
-    argument list. As this is a required linker argument, raises an error if it
-    cannot be found.
-    """
-    # The linker_driver.py script may be used to wrap either the compiler linker
-    # (uses -o to configure the output) or lipo (uses -output to configure the
-    # output). Since wrapping the compiler linker is the most likely possibility
-    # use try/except and fallback to checking for -output if -o is not found.
-    try:
-        output_flag_index = full_args.index('-o')
-    except ValueError:
-        output_flag_index = full_args.index('-output')
-    return full_args[output_flag_index + 1]
-
-
-def _find_tools_path(full_args):
+def _find_tools_paths(full_args):
     """Finds all paths where the script should look for additional tools."""
     paths = []
     for idx, arg in enumerate(full_args):
@@ -290,19 +287,6 @@ def _remove_path(path):
             os.unlink(path)
 
 
-_LINKER_DRIVER_ARG_PREFIX = '-Wcrl,'
-"""List of linker driver actions. The sort order of this list affects the
-order in which the actions are invoked. The first item in the tuple is the
-argument's -Wcrl,<sub_argument> and the second is the function to invoke.
-"""
-_LINKER_DRIVER_ACTIONS = [
-    ('dsymutilpath,', set_dsymutil_path),
-    ('dsym,', run_dsym_util),
-    ('unstripped,', run_save_unstripped),
-    ('strippath,', set_strip_path),
-    ('strip,', run_strip),
-]
-
 if __name__ == '__main__':
-    main(sys.argv)
+    LinkerDriver(sys.argv).run()
     sys.exit(0)
