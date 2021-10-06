d5d6526c815c0dbf0a3b14f69d38816873eeea75
Andrew Shulaev <ddrone@google.com>
2021-10-06 12:47:13 +0000

    Clean up trace events for scroll jank UMA metric
    Bug: b/185991751
    Change-Id: Id572af4a56d33abd262d423df40a3a722f8df237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159535
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Auto-Submit: Andrew Shulaev <ddrone@google.com>
    Commit-Queue: Andrew Shulaev <ddrone@google.com>
    Cr-Commit-Position: refs/heads/main@{#928636}

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -170,21 +170,6 @@ void LatencyTracker::ReportJankyFrame(base::TimeTicks original_timestamp,
   CONFIRM_EVENT_TIMES_EXIST(original_timestamp, gpu_swap_end_timestamp);
   base::TimeDelta dur = gpu_swap_end_timestamp - original_timestamp;
 
-  uint64_t id = base::trace_event::GetNextGlobalTraceId();
-  uint64_t flow_id = base::trace_event::GetNextGlobalTraceId();
-
-  // Temporary tracing events used to log individual UMA jank metric values
-  // TODO(b/185991751): Delete after the discrepancy is fixed.
-  TRACE_EVENT_BEGIN(
-      "latencyInfo", "InputLatency::ReportJankyFrame", perfetto::Track(id),
-      original_timestamp, "trace_id", latency.trace_id(),
-      [flow_id](perfetto::EventContext ctx) {
-        tracing::FillFlowEvent(
-            ctx, perfetto::protos::pbzero::TrackEvent_LegacyEvent::FLOW_OUT,
-            flow_id);
-      });
-  TRACE_EVENT_END("latencyInfo", perfetto::Track(id), gpu_swap_end_timestamp);
-
   if (first_frame) {
     if (jank_state_.total_update_events_ > 0) {
       // If we have some data from previous scroll, report it to UMA.
@@ -235,14 +220,6 @@ void LatencyTracker::ReportJankyFrame(base::TimeTicks original_timestamp,
 
       if (IsJankyComparison(prev_frames_taken, frames_taken)) {
         UMA_HISTOGRAM_BOOLEAN("Event.Latency.ScrollJank", true);
-        TRACE_EVENT(
-            "latencyInfo", "InputLatency::ConfirmJankyFrame",
-            [this](perfetto::EventContext ctx) {
-              tracing::FillFlowEvent(
-                  ctx,
-                  perfetto::protos::pbzero::TrackEvent_LegacyEvent::FLOW_IN,
-                  prev_flow_id_);
-            });
         jank_state_.janky_update_events_++;
         jank_state_.janky_update_duration_ += jank_state_.prev_duration_;
       } else {
@@ -253,13 +230,6 @@ void LatencyTracker::ReportJankyFrame(base::TimeTicks original_timestamp,
     // The current GestureScrollUpdate is janky compared to the previous one.
     if (IsJankyComparison(frames_taken, prev_frames_taken)) {
       UMA_HISTOGRAM_BOOLEAN("Event.Latency.ScrollJank", true);
-      TRACE_EVENT(
-          "latencyInfo", "InputLatency::ConfirmJankyFrame",
-          [flow_id](perfetto::EventContext ctx) {
-            tracing::FillFlowEvent(
-                ctx, perfetto::protos::pbzero::TrackEvent_LegacyEvent::FLOW_IN,
-                flow_id);
-          });
       jank_state_.janky_update_events_++;
       jank_state_.janky_update_duration_ += dur;
 
@@ -276,7 +246,6 @@ void LatencyTracker::ReportJankyFrame(base::TimeTicks original_timestamp,
   }
 
   jank_state_.prev_duration_ = dur;
-  prev_flow_id_ = flow_id;
 }
 
 void LatencyTracker::ComputeEndToEndLatencyHistograms(
