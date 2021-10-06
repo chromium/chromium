6c9b466f3b439874ab3cc4732ac191525fb7bde6
Tom <tluk@chromium.org>
2021-10-06 18:08:26 +0000

    [tab search] Add test config entries for entrypoint update
    This CL also makes the following changes to ensure tests function
    as expected
      - TabSearchButtonBrowserTest ensures the tab search caption button
        flag is disabled since it is not present in the tab strip when
        enabled.
      - SadTabViewInteractiveUITest has been updated to take into account
        the tab search caption button during focus traversal if present.
      - The width of the browser window in the
        DraggingRightExpandsTabStripSize test was increased. Previously
        this was arbitrarily set to TabStyle::GetStandardWidth() * 4
        however the tab search caption button made the tab strip area
        too small for the tab strip to grow as expected and this has been
        increased to TabStyle::GetStandardWidth() * 5.
      - The FocusTraversal test has been updated to ensure focus
        traversal takes into account the tab search caption button if
        present.
    
    Bug: None
    Change-Id: I18ef438d70b82d0d7fedb4998d97a0ab4d6f5d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206499
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928767}

--- chrome/browser/ui/views/sad_tab_view_interactive_uitest.cc
+++ chrome/browser/ui/views/sad_tab_view_interactive_uitest.cc
@@ -9,6 +9,8 @@
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/sad_tab.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
+#include "chrome/browser/ui/ui_features.h"
+#include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -155,10 +157,26 @@ IN_PROC_BROWSER_TEST_F(SadTabViewInteractiveUITest,
   ASSERT_TRUE(IsFocusedViewInsideSadTab());
   ASSERT_FALSE(IsFocusedViewInsideBrowserToolbar());
 
-  // Pressing the Tab key should cycle focus back to the toolbar.
+  const bool tab_search_caption_button_enabled =
+#if defined(OS_WIN)
+      base::FeatureList::IsEnabled(features::kWin10TabSearchCaptionButton);
+#else
+      false;
+#endif  // defined(OS_WIN)
+
+  // Pressing the Tab key should cycle focus back to the toolbar or the browser
+  // frame if the tab search caption button is enabled.
   PressTab();
-  ASSERT_FALSE(IsFocusedViewInsideSadTab());
-  ASSERT_TRUE(IsFocusedViewInsideBrowserToolbar());
+  if (tab_search_caption_button_enabled) {
+    const auto* frame_view = BrowserView::GetBrowserViewForBrowser(browser())
+                                 ->frame()
+                                 ->GetFrameView();
+    ASSERT_FALSE(IsFocusedViewInsideSadTab());
+    ASSERT_TRUE(frame_view->Contains(GetFocusedView()));
+  } else {
+    ASSERT_FALSE(IsFocusedViewInsideSadTab());
+    ASSERT_TRUE(IsFocusedViewInsideBrowserToolbar());
+  }
 
   // Keep pressing the Tab key and make sure we make it back to the sad tab.
   while (!IsFocusedViewInsideSadTab())
