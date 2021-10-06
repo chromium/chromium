e54d246db2e920fa4cc0ab3fcdb474c97c8f19a6
Chad Duffin <chadduffin@chromium.org>
2021-10-06 15:35:36 +0000

    Reland "[CrOS Bluetooth] Implement BluetoothDeviceListItemView."
    This reverts commit f1255ff29e7edb80de72f5b404f82b89f911471c.
    
    Reason for revert: Relanding with fixed
    BluetoothDeviceListItemViewTest.HasCorrectIcon. Previously, equality of
    the ImageSkia instances was determined by comparing the objects backing
    them which may not actually be the same when building debug
    configurations. Instead, we generate images from the ImageSkia instances
    and compare the results.
    
    Original change's description:
    > Revert "[CrOS Bluetooth] Implement BluetoothDeviceListItemView."
    >
    > This reverts commit a0d0889eba2a047b130bb3db1713834a9b0e5935.
    >
    > Reason for revert: BluetoothDeviceListItemViewTest.HasCorrectIcon fails under debug configuration.
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/25752/overview
    >
    > Original change's description:
    > > [CrOS Bluetooth] Implement BluetoothDeviceListItemView.
    > >
    > > This change implements the class responsible for managing the view for a
    > > specific device within the device list of the Bluetooth detailed view
    > > within the Quick Settings.
    > >
    > > Examples of devices:
    > >  - https://screenshot.googleplex.com/AMi4rsiJ59Dus5D.png
    > >  - https://screenshot.googleplex.com/Ae5S7qigGf7KBA6.png
    > >
    > > Bug: 1010321
    > > Change-Id: I938e70cb4d5ab184accff82f438831bc3dba3484
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171493
    > > Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    > > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > > Reviewed-by: Gordon Seto <gordonseto@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#927728}
    >
    > Bug: 1010321
    > Change-Id: I826dbfe9287a7f08dcb748114ceac02771575af2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202537
    > Reviewed-by: Gordon Seto <gordonseto@google.com>
    > Reviewed-by: Roman Arora <romanarora@google.com>
    > Owners-Override: Roman Arora <romanarora@google.com>
    > Commit-Queue: Roman Arora <romanarora@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Chad Duffin <chadduffin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#927797}
    
    Bug: 1010321
    Change-Id: Ie67157a327c239764ef2973a94b254e71aac3061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202348
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928679}

--- ash/system/bluetooth/bluetooth_device_list_item_view.cc
+++ ash/system/bluetooth/bluetooth_device_list_item_view.cc
@@ -5,12 +5,50 @@
 #include "ash/system/bluetooth/bluetooth_device_list_item_view.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/style/ash_color_provider.h"
+#include "ash/system/tray/tray_utils.h"
 #include "base/check.h"
+#include "chromeos/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.h"
 #include "mojo/public/cpp/bindings/clone_traits.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/gfx/vector_icon_types.h"
+#include "ui/views/controls/label.h"
 
 namespace ash {
 namespace {
+
+using chromeos::bluetooth_config::GetPairedDeviceName;
+using chromeos::bluetooth_config::mojom::DeviceConnectionState;
+using chromeos::bluetooth_config::mojom::DeviceType;
 using chromeos::bluetooth_config::mojom::PairedBluetoothDevicePropertiesPtr;
+
+// Returns the icon corresponding to the provided |device_type| and
+// |connection_state|.
+const gfx::VectorIcon& GetDeviceIcon(const DeviceType device_type) {
+  switch (device_type) {
+    case DeviceType::kComputer:
+      return ash::kSystemMenuComputerIcon;
+    case DeviceType::kPhone:
+      return ash::kSystemMenuPhoneIcon;
+    case DeviceType::kHeadset:
+      return ash::kSystemMenuHeadsetIcon;
+    case DeviceType::kVideoCamera:
+      return ash::kSystemMenuVideocamIcon;
+    case DeviceType::kGameController:
+      return ash::kSystemMenuGamepadIcon;
+    case DeviceType::kKeyboard:
+      return ash::kSystemMenuKeyboardIcon;
+    case DeviceType::kMouse:
+      return ash::kSystemMenuMouseIcon;
+    case DeviceType::kTablet:
+      return ash::kSystemMenuTabletIcon;
+    case DeviceType::kUnknown:
+      return ash::kSystemMenuBluetoothIcon;
+  }
+}
+
 }  // namespace
 
 BluetoothDeviceListItemView::BluetoothDeviceListItemView(
@@ -24,6 +62,31 @@ BluetoothDeviceListItemView::~BluetoothDeviceListItemView() = default;
 void BluetoothDeviceListItemView::UpdateDeviceProperties(
     const PairedBluetoothDevicePropertiesPtr& device_properties) {
   device_properties_ = mojo::Clone(device_properties);
+
+  // We can only add an icon and label if the view has not already been
+  // populated with one or both of these views. For simplicity, instead of
+  // trying to determine which views exist and modifying them, and creating the
+  // missing views, we instead clear all of the views and recreate them.
+  if (is_populated())
+    Reset();
+
+  const DeviceType& device_type =
+      device_properties_->device_properties->device_type;
+
+  AddIconAndLabel(
+      gfx::CreateVectorIcon(
+          GetDeviceIcon(device_type),
+          AshColorProvider::Get()->GetContentLayerColor(
+              AshColorProvider::ContentLayerType::kIconColorPrimary)),
+      GetPairedDeviceName(device_properties_.get()));
+
+  const DeviceConnectionState& connection_state =
+      device_properties_->device_properties->connection_state;
+
+  // Adds a sub-label to show that the device is in the process of connecting.
+  if (connection_state == DeviceConnectionState::kConnecting) {
+    SetupConnectingScrollListItem(this);
+  }
 }
 
 const char* BluetoothDeviceListItemView::GetClassName() const {
