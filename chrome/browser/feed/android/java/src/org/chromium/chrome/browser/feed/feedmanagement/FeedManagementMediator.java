0021a72f981b00797b9a99fecbff5964071514d8
Pete Williamson <petewil@chromium.org>
2021-10-07 23:59:23 +0000

    Web Channels - Add unit tests for the Feed Management Interstitial activity.
    Change-Id: Iaf30e73932f8e88027a18cf4ed142e18dd6a1508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208920
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929501}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementMediator.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementMediator.java
@@ -14,9 +14,9 @@ import android.provider.Browser;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.browser.customtabs.CustomTabsIntent;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.compat.ApiHelperForM;
 import org.chromium.chrome.browser.feed.FeedServiceBridge;
@@ -113,12 +113,11 @@ public class FeedManagementMediator {
         intent.setClassName(mContext, "org.chromium.chrome.browser.customtabs.CustomTabActivity");
 
         // Do the things that createCustomTabActivityIntent does:
-        intent.setPackage(mContext.getPackageName());
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Needed for pre-N versions of android.
         intent.putExtra(Browser.EXTRA_APPLICATION_ID, mContext.getPackageName());
 
         // Adding trusted extras lets us know that the intent came from Chrome.
-        intent.setPackage(ContextUtils.getApplicationContext().getPackageName());
+        intent.setPackage(mContext.getPackageName());
         intent.putExtra(TRUSTED_APPLICATION_CODE_EXTRA, getAuthenticationToken());
         mContext.startActivity(intent);
         // TODO(https://crbug.com/1195209): Record uma by calling ReportOtherUserAction
@@ -126,40 +125,43 @@ public class FeedManagementMediator {
     }
 
     // Copied from IntentHandler, which is in chrome_java, so we can't call it directly.
-    private static PendingIntent getAuthenticationToken() {
+    private PendingIntent getAuthenticationToken() {
         Intent fakeIntent = new Intent();
-        Context appContext = ContextUtils.getApplicationContext();
-        ComponentName fakeComponentName =
-                new ComponentName(appContext.getPackageName(), "FakeClass");
+        ComponentName fakeComponentName = new ComponentName(mContext.getPackageName(), "FakeClass");
         fakeIntent.setComponent(fakeComponentName);
         int mutabililtyFlag = 0;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             mutabililtyFlag = ApiHelperForM.getPendingIntentImmutableFlag();
         }
-        return PendingIntent.getActivity(appContext, 0, fakeIntent, mutabililtyFlag);
+        return PendingIntent.getActivity(mContext, 0, fakeIntent, mutabililtyFlag);
     }
 
-    private void handleActivityClick(View view) {
+    @VisibleForTesting
+    void handleActivityClick(View view) {
         Log.d(TAG, "Activity click caught.");
         launchUriActivity("https://myactivity.google.com/myactivity?product=50");
     }
 
-    private void handleInterestsClick(View view) {
+    @VisibleForTesting
+    void handleInterestsClick(View view) {
         Log.d(TAG, "Interests click caught.");
         launchUriActivity("https://www.google.com/preferences/interests/yourinterests?sh=n");
     }
 
-    private void handleHiddenClick(View view) {
+    @VisibleForTesting
+    void handleHiddenClick(View view) {
         Log.d(TAG, "Hidden click caught.");
         launchUriActivity("https://www.google.com/preferences/interests/hidden?sh=n");
     }
 
-    private void handleAutoplayClick(View view) {
+    @VisibleForTesting
+    void handleAutoplayClick(View view) {
         Log.d(TAG, "Autoplay click caught.");
         mAutoplayManagementLauncher.launchAutoplayManagement(mContext);
     }
 
-    private void handleFollowingClick(View view) {
+    @VisibleForTesting
+    void handleFollowingClick(View view) {
         Log.d(TAG, "Following click caught.");
         FeedServiceBridge.reportOtherUserAction(FeedUserActionType.TAPPED_MANAGE_FOLLOWING);
         mFollowManagementLauncher.launchFollowManagement(mContext);
