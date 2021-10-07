2ef8d464759f8e56b703154ae566cda276d6391a
Patrick Monette <pmonette@chromium.org>
2021-10-07 18:21:47 +0000

    Add a temporary header for each file moving to base/task/
    A multitude of task-related headers currently live in the general
    base/ folder. To improve the organisation of the files in the base
    library, those files will be moved to base/task/.
    
    Those temporary headers, whose purpose is to redirect the include to
    the real location of the header, will facilitate the mass-migration of
    all the references to the moved files.
    
    Bug: 1255932
    Change-Id: I397021ae29a8042d6ef1b336404864d08497752f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209027
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929305}

---
+++ base/task/post_task_and_reply_with_result_internal_forward.h
@@ -0,0 +1,10 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_TASK_POST_TASK_AND_REPLY_WITH_RESULT_INTERNAL_FORWARD_H_
+#define BASE_TASK_POST_TASK_AND_REPLY_WITH_RESULT_INTERNAL_FORWARD_H_
+
+#include "base/post_task_and_reply_with_result_internal.h"
+
+#endif  // BASE_TASK_POST_TASK_AND_REPLY_WITH_RESULT_INTERNAL_FORWARD_H_
