16a51a1729334b98f4f085106cb8f9c463fa960b
Michael Martis <martis@chromium.org>
2021-10-07 23:41:53 +0000

    [HPS Notify] Use new DBus signal.
    In the upstream CL, Chrome was made a client of the Chrome OS HPS
    daemon. This CL listens for the "HPS notify changed" signal from the
    daemon and shows/hides the HPS notify icon as appropriate.
    
    See go/cros-hps-notify-ui-impl for feature details, and go/cros-hps-oss
    for naming conventions.
    
    BUG=chromium:1241704, chromium:1251832
    TEST=Manual
    
    Change-Id: I24653f51db0156a6980b31d892984dc8e1429297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168985
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929495}

--- ash/system/unified/hps_notify_view.cc
+++ ash/system/unified/hps_notify_view.cc
@@ -11,6 +11,10 @@
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_utils.h"
+#include "base/bind.h"
+#include "base/callback.h"
+#include "components/session_manager/session_manager_types.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -18,51 +22,85 @@
 #include "ui/views/controls/image_view.h"
 
 namespace ash {
-namespace {
-
-gfx::ImageSkia MakeIcon(SkColor color) {
-  return gfx::CreateVectorIcon(gfx::IconDescription(
-      kSystemTrayHpsNotifyIcon, kUnifiedTrayIconSize, color));
-}
-
-}  // namespace
 
 HpsNotifyView::HpsNotifyView(Shelf* shelf)
     : TrayItemView(shelf),
-      icon_color_(
-          TrayIconColor(Shell::Get()->session_controller()->GetSessionState())),
-      observation_(this) {
+      hps_state_(false),
+      is_oobe_(false),
+      first_signal_received_(false),
+      session_observation_(this),
+      hps_dbus_observation_(this),
+      weak_ptr_factory_(this) {
   CreateImageView();
-  image_view()->SetImage(MakeIcon(icon_color_));
-  observation_.Observe(Shell::Get()->session_controller());
+  UpdateIconColor(Shell::Get()->session_controller()->GetSessionState());
+  SetVisible(false);
+
+  session_observation_.Observe(Shell::Get()->session_controller());
+  hps_dbus_observation_.Observe(chromeos::HpsDBusClient::Get());
+
+  chromeos::HpsDBusClient::Get()->GetResultHpsNotify(base::BindOnce(
+      &HpsNotifyView::OnHpsPollResult, weak_ptr_factory_.GetWeakPtr()));
 }
 
 HpsNotifyView::~HpsNotifyView() = default;
 
 void HpsNotifyView::HandleLocaleChange() {}
 
-void HpsNotifyView::OnSessionStateChanged(session_manager::SessionState state) {
-  // Update color of icon if necessary.
-  UpdateIcon();
+void HpsNotifyView::OnSessionStateChanged(
+    session_manager::SessionState session_state) {
+  UpdateIconColor(session_state);
+  UpdateIconVisibility(session_state == session_manager::SessionState::OOBE,
+                       hps_state_);
+}
 
-  // Don't display during the OOBE flow.
-  SetVisible(state != session_manager::SessionState::OOBE);
+void HpsNotifyView::OnHpsNotifyChanged(bool hps_state) {
+  first_signal_received_ = true;
+  UpdateIconVisibility(is_oobe_, hps_state);
 }
 
 void HpsNotifyView::OnThemeChanged() {
   TrayItemView::OnThemeChanged();
-  UpdateIcon();
+  UpdateIconColor(Shell::Get()->session_controller()->GetSessionState());
 }
 
 const char* HpsNotifyView::GetClassName() const {
   return "HpsNotifyView";
 }
 
-void HpsNotifyView::UpdateIcon() {
-  const SkColor new_color =
-      TrayIconColor(Shell::Get()->session_controller()->GetSessionState());
-  if (icon_color_ != new_color)
-    image_view()->SetImage(MakeIcon(new_color));
+void HpsNotifyView::UpdateIconColor(
+    session_manager::SessionState session_state) {
+  const SkColor new_color = TrayIconColor(session_state);
+  const gfx::ImageSkia new_icon = gfx::CreateVectorIcon(gfx::IconDescription(
+      kSystemTrayHpsNotifyIcon, kUnifiedTrayIconSize, new_color));
+  image_view()->SetImage(new_icon);
+}
+
+void HpsNotifyView::UpdateIconVisibility(bool is_oobe, bool hps_state) {
+  if (is_oobe_ == is_oobe && hps_state_ == hps_state)
+    return;
+
+  is_oobe_ = is_oobe;
+  hps_state_ = hps_state;
+  SetVisible(!is_oobe_ && hps_state_);
+}
+
+void HpsNotifyView::OnHpsPollResult(absl::optional<bool> result) {
+  // A race: we have received a signal before we had a chance to establish our
+  // initial state (from the result of our DBus method call). The signal could
+  // have originated from either before our DBus method call, or after.
+  //
+  // If the two states are the same, there is no problem. If they are different,
+  // we should always ignore the DBus method response because:
+  //   a) If the signal originated before the DBus method call, the state of the
+  //      daemon has changed since the signal was sent and an updated signal
+  //      will be forthcoming.
+  //   b) If the signal originated after the DBus method call, then the signal
+  //      is newer and reflects the current state of the daemon.
+  if (first_signal_received_ || !result.has_value()) {
+    return;
+  }
+
+  UpdateIconVisibility(is_oobe_, *result);
 }
 
 }  // namespace ash
--- ash/system/unified/hps_notify_view.h
+++ ash/system/unified/hps_notify_view.h
@@ -9,14 +9,17 @@
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/system/tray/tray_item_view.h"
 #include "base/scoped_observation.h"
+#include "chromeos/dbus/hps/hps_dbus_client.h"
+#include "components/session_manager/session_manager_types.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace ash {
 
 // The current HPS notify state in the system tray.
-//
-// As a very first prototype, simply displays a static icon in the system tray.
-class HpsNotifyView : public TrayItemView, public SessionObserver {
+class HpsNotifyView : public TrayItemView,
+                      public SessionObserver,
+                      public chromeos::HpsDBusClient::Observer {
  public:
   explicit HpsNotifyView(Shelf* shelf);
   HpsNotifyView(const HpsNotifyView&) = delete;
@@ -31,11 +34,37 @@ class HpsNotifyView : public TrayItemView, public SessionObserver {
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
 
- private:
-  void UpdateIcon();
+  // chromeos::HpsDBusClient::Observer:
+  void OnHpsNotifyChanged(bool state) override;
 
-  SkColor icon_color_;
-  base::ScopedObservation<SessionController, SessionObserver> observation_;
+ private:
+  // Updates the system tray icon to use the color corresponding to the current
+  // session state (e.g. darker during OOBE).
+  void UpdateIconColor(session_manager::SessionState session_state);
+
+  // Shows or hides the system tray icon as appropriate given the signal and
+  // session state. Also updates the current cached states.
+  void UpdateIconVisibility(bool is_oobe, bool hps_state);
+
+  // A callback to set our initial state by polling HPS. If we have received a
+  // signal before the callback is executed, this becomes a no-op.
+  void OnHpsPollResult(absl::optional<bool> result);
+
+  // Used to track whether a signal should actually trigger a visibility change.
+  bool hps_state_;
+  bool is_oobe_;
+
+  // Used to avoid a race between polling and being signaled by the HPS service.
+  bool first_signal_received_;
+
+  base::ScopedObservation<SessionController, SessionObserver>
+      session_observation_;
+  base::ScopedObservation<chromeos::HpsDBusClient,
+                          chromeos::HpsDBusClient::Observer>
+      hps_dbus_observation_;
+
+  // Must be last.
+  base::WeakPtrFactory<HpsNotifyView> weak_ptr_factory_;
 };
 
 }  // namespace ash
