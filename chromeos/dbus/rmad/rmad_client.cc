21ed550a1f0a9c719b445a2215b821b53d3f9f25
Gavin Dodd <gavindodd@google.com>
2021-10-07 22:42:42 +0000

    shimless: Set rmad_client observers to be checked.
    Set flags on ObserverList so that the destructor checks list is empty to
    confirm that observers are removed.
    
    Bug: 1198187
    Change-Id: Icb956cc09483b6c5bab95e5e5572158cea9be8ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208922
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#929454}

--- chromeos/dbus/rmad/rmad_client.cc
+++ chromeos/dbus/rmad/rmad_client.cc
@@ -63,7 +63,7 @@ class RmadClientImpl : public RmadClient {
                        bool success);
 
   dbus::ObjectProxy* rmad_proxy_ = nullptr;
-  base::ObserverList<Observer>::Unchecked observers_;
+  base::ObserverList<Observer, true, false>::Unchecked observers_;
 
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
