9336ab6af097a0f61245ed4b71f4e67bd02eb146
Peter McNeeley <petermcneeley@chromium.org>
2021-10-07 13:52:52 +0000

    Clamping device scale in exo-surface
    The damage issue we are seeing in CtsWindowManagerDeviceTestCases
    failures do trigger the previous CHECK we installed. At this stage
    there are only two possibilities. Either |content_size_| is invalid or
    |device_scale_factor| is an unexpected value.
    
    Bug: b/191414141
    Change-Id: Ica031327ed0127307b0bfcbd24cd1558335b87d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3204730
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929178}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -1246,22 +1246,43 @@ void Surface::AppendContentsToFrame(const gfx::Point& origin,
     damage_rect.Inset(-1, -1);
     damage_rect += origin.OffsetFromOrigin();
     damage_rect.Intersect(output_rect);
-    if (device_scale_factor <= 1) {
+
+    // TODO(b/191414141) : Clamp is temporary to isolate damage issue.
+    float device_scale_factor_clamped = device_scale_factor;
+    constexpr float kMaxExpectedScale = 50.0f;
+    constexpr float kMinExpectedScale = 0.1f;
+    if (device_scale_factor_clamped > kMaxExpectedScale) {
+      LOG(ERROR) << "Scale error: device_scale_factor="
+                 << device_scale_factor_clamped << " was clamped to "
+                 << kMaxExpectedScale;
+      device_scale_factor_clamped = kMaxExpectedScale;
+    } else if (kMinExpectedScale > device_scale_factor_clamped) {
+      LOG(ERROR) << "Scale error: device_scale_factor="
+                 << device_scale_factor_clamped << " was clamped to "
+                 << kMinExpectedScale;
+      device_scale_factor_clamped = kMinExpectedScale;
+    }
+
+    if (device_scale_factor_clamped <= 1) {
       damage_rect = gfx::ToEnclosingRect(
-          gfx::ConvertRectToPixels(damage_rect, device_scale_factor));
+          gfx::ConvertRectToPixels(damage_rect, device_scale_factor_clamped));
     } else {
       // The damage will eventually be rescaled by 1/device_scale_factor. Since
       // that scale factor is <1, taking the enclosed rect here means that that
       // rescaled RectF is <1px smaller than |damage_rect| in each dimension,
       // which makes the enclosing rect equal to |damage_rect|.
       gfx::RectF scaled_damage(damage_rect);
-      scaled_damage.Scale(device_scale_factor);
+      scaled_damage.Scale(device_scale_factor_clamped);
       damage_rect = gfx::ToEnclosedRect(scaled_damage);
     }
   }
 
   // TODO(b/191414141) : Check is temporary to isolate damage issue.
-  CHECK(damage_rect.size().GetCheckedArea().IsValid());
+  CHECK(damage_rect.size().GetCheckedArea().IsValid())
+      << " original=" << state_.damage.ToString()
+      << ", dsf=" << device_scale_factor
+      << ", result=" << damage_rect.ToString()
+      << ", output_rect=" << output_rect.ToString();
 
   state_.damage.Clear();
 
