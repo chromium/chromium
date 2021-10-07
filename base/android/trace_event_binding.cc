88ea018cf480dc53a1b89b37458fe9fb3011947e
Mikhail Khokhlov <khokhlov@google.com>
2021-10-07 10:23:59 +0000

    [Tracing] Fix base build when enable_base_tracing=false
    Moves the view hierarchy dump functions under the ENABLE_BASE_TRACING
    build flag.
    
    Change-Id: I9cc46285de2d22008644070618a107f7c15af403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208015
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#929094}

--- base/android/trace_event_binding.cc
+++ base/android/trace_event_binding.cc
@@ -75,6 +75,54 @@ static void JNI_TraceEvent_SetupATraceStartupTrace(
       category_filter_utf8);
 }
 
+static jboolean JNI_TraceEvent_ViewHierarchyDumpEnabled(JNIEnv* env) {
+  static const unsigned char* enabled =
+      TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(
+          kAndroidViewHierarchyTraceCategory);
+  return *enabled;
+}
+
+static void JNI_TraceEvent_InitViewHierarchyDump(JNIEnv* env) {
+  SCOPED_UMA_HISTOGRAM_TIMER("Tracing.ViewHierarchyDump.DumpDuration");
+  TRACE_EVENT_INSTANT(
+      kAndroidViewHierarchyTraceCategory, kAndroidViewHierarchyEventName,
+      perfetto::Track::Global(0), [&](perfetto::EventContext ctx) {
+        auto* event = ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+        auto* dump = event->set_android_view_dump();
+        Java_TraceEvent_dumpViewHierarchy(env, reinterpret_cast<jlong>(dump));
+      });
+}
+
+static jlong JNI_TraceEvent_StartActivityDump(JNIEnv* env,
+                                              const JavaParamRef<jstring>& name,
+                                              jlong dump_proto_ptr) {
+  auto* dump = reinterpret_cast<perfetto::protos::pbzero::AndroidViewDump*>(
+      dump_proto_ptr);
+  auto* activity = dump->add_activity();
+  activity->set_name(ConvertJavaStringToUTF8(env, name));
+  return reinterpret_cast<jlong>(activity);
+}
+
+static void JNI_TraceEvent_AddViewDump(
+    JNIEnv* env,
+    jint id,
+    jint parent_id,
+    jboolean is_shown,
+    jboolean is_dirty,
+    const JavaParamRef<jstring>& class_name,
+    const JavaParamRef<jstring>& resource_name,
+    jlong activity_proto_ptr) {
+  auto* activity = reinterpret_cast<perfetto::protos::pbzero::AndroidActivity*>(
+      activity_proto_ptr);
+  auto* view = activity->add_view();
+  view->set_id(id);
+  view->set_parent_id(parent_id);
+  view->set_is_shown(is_shown);
+  view->set_is_dirty(is_dirty);
+  view->set_class_name(ConvertJavaStringToUTF8(env, class_name));
+  view->set_resource_name(ConvertJavaStringToUTF8(env, resource_name));
+}
+
 #else  // BUILDFLAG(ENABLE_BASE_TRACING)
 
 // Empty implementations when TraceLog isn't available.
@@ -87,6 +135,29 @@ static void JNI_TraceEvent_StopATrace(JNIEnv* env) {}
 static void JNI_TraceEvent_SetupATraceStartupTrace(
     JNIEnv* env,
     const JavaParamRef<jstring>&) {}
+static jboolean JNI_TraceEvent_ViewHierarchyDumpEnabled(JNIEnv* env) {
+  return false;
+}
+static void JNI_TraceEvent_InitViewHierarchyDump(JNIEnv* env) {
+  DCHECK(false);
+  // This code should not be reached when base tracing is disabled. Calling
+  // dumpViewHierarchy to avoid "unused function" warning.
+  Java_TraceEvent_dumpViewHierarchy(env, 0);
+}
+static jlong JNI_TraceEvent_StartActivityDump(JNIEnv* env,
+                                              const JavaParamRef<jstring>& name,
+                                              jlong dump_proto_ptr) {
+  return 0;
+}
+static void JNI_TraceEvent_AddViewDump(
+    JNIEnv* env,
+    jint id,
+    jint parent_id,
+    jboolean is_shown,
+    jboolean is_dirty,
+    const JavaParamRef<jstring>& class_name,
+    const JavaParamRef<jstring>& resource_name,
+    jlong activity_proto_ptr) {}
 
 #endif  // BUILDFLAG(ENABLE_BASE_TRACING)
 
@@ -200,54 +271,6 @@ static void JNI_TraceEvent_FinishAsync(JNIEnv* env,
   TRACE_EVENT_NESTABLE_ASYNC_END_WITH_FLAGS0(
       internal::kJavaTraceCategory, converter.name(), TRACE_ID_LOCAL(jid),
       TRACE_EVENT_FLAG_JAVA_STRING_LITERALS | TRACE_EVENT_FLAG_COPY);
-}
-
-static jboolean JNI_TraceEvent_ViewHierarchyDumpEnabled(JNIEnv* env) {
-  static const unsigned char* enabled =
-      TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(
-          kAndroidViewHierarchyTraceCategory);
-  return *enabled;
-}
-
-static void JNI_TraceEvent_InitViewHierarchyDump(JNIEnv* env) {
-  SCOPED_UMA_HISTOGRAM_TIMER("Tracing.ViewHierarchyDump.DumpDuration");
-  TRACE_EVENT_INSTANT(
-      kAndroidViewHierarchyTraceCategory, kAndroidViewHierarchyEventName,
-      perfetto::Track::Global(0), [&](perfetto::EventContext ctx) {
-        auto* event = ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
-        auto* dump = event->set_android_view_dump();
-        Java_TraceEvent_dumpViewHierarchy(env, reinterpret_cast<jlong>(dump));
-      });
-}
-
-static jlong JNI_TraceEvent_StartActivityDump(JNIEnv* env,
-                                              const JavaParamRef<jstring>& name,
-                                              jlong dump_proto_ptr) {
-  auto* dump = reinterpret_cast<perfetto::protos::pbzero::AndroidViewDump*>(
-      dump_proto_ptr);
-  auto* activity = dump->add_activity();
-  activity->set_name(ConvertJavaStringToUTF8(env, name));
-  return reinterpret_cast<jlong>(activity);
-}
-
-static void JNI_TraceEvent_AddViewDump(
-    JNIEnv* env,
-    jint id,
-    jint parent_id,
-    jboolean is_shown,
-    jboolean is_dirty,
-    const JavaParamRef<jstring>& class_name,
-    const JavaParamRef<jstring>& resource_name,
-    jlong activity_proto_ptr) {
-  auto* activity = reinterpret_cast<perfetto::protos::pbzero::AndroidActivity*>(
-      activity_proto_ptr);
-  auto* view = activity->add_view();
-  view->set_id(id);
-  view->set_parent_id(parent_id);
-  view->set_is_shown(is_shown);
-  view->set_is_dirty(is_dirty);
-  view->set_class_name(ConvertJavaStringToUTF8(env, class_name));
-  view->set_resource_name(ConvertJavaStringToUTF8(env, resource_name));
 }
 
 }  // namespace android
