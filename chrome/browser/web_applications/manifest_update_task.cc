d6003a25d3f8447fdccb479338617ca674d5269c
Finnur Thorarinsson <finnur@chromium.org>
2021-10-08 00:04:02 +0000

    [AppIdentity]: Allow update only if images can be loaded.
    This also updates the test expectation for the test
    CheckDoesNotUpdateGeneratedIcons_SyncFailure
    ... because the AppIdentityDialog currently doesn't support
    multiple icons updating at the same time.
    
    Bug: 1253357
    Change-Id: I84d8fd57e00a4f0431d9503f92768c086ab08c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194078
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929504}

--- chrome/browser/web_applications/manifest_update_task.cc
+++ chrome/browser/web_applications/manifest_update_task.cc
@@ -417,7 +417,8 @@ void ManifestUpdateTask::OnAllIconsRead(IconsMap downloaded_icons_map,
       }
     }
 
-    if (before_icon != nullptr && after_icon != nullptr) {
+    if (before_icon != nullptr && after_icon != nullptr &&
+        !before_icon->drawsNothing() && !after_icon->drawsNothing()) {
       ui_manager_.ShowWebAppIdentityUpdateDialog(
           app_id_, title_change, icon_diff.changes_detected, old_title,
           new_title, *before_icon, *after_icon, web_contents,
