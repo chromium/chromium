833cda0a8fd7626713ddca408833149381a30e11
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2021-10-11 17:09:54 +0000

    sensors: Use base::ranges::any_of() in PlatformSensorFusion's constructor.
    Simplify the code a little bit by using the ranges version of any_of().
    While here, stop unnecessarily making Factory a friend class and fix the
    grammar in the class' description.
    
    Change-Id: If48acaf2b6ca6cb5d66ab74b60eb42415a6a627e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217450
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930189}

--- services/device/generic_sensor/platform_sensor_fusion.h
+++ services/device/generic_sensor/platform_sensor_fusion.h
@@ -24,8 +24,8 @@ class PlatformSensorFusionAlgorithm;
 //
 // This class implements the generic concept of sensor fusion. It implements
 // a new sensor using data from one or more existing sensors. For example,
-// it can implements a *_EULER_ANGLES orientation sensor using a
-// *_QUATERNION orientation sensor, or vice a versa.
+// it can implement a *_EULER_ANGLES orientation sensor using a
+// *_QUATERNION orientation sensor, or vice-versa.
 //
 // It can also implement an orientation sensor using an ACCELEROMETER, etc.
 class PlatformSensorFusion : public PlatformSensor,
@@ -59,11 +59,8 @@ class PlatformSensorFusion : public PlatformSensor,
 
  protected:
   class Factory;
-  friend class Factory;
   using SourcesMap =
       base::flat_map<mojom::SensorType, scoped_refptr<PlatformSensor>>;
-  using SourcesMapEntry =
-      std::pair<mojom::SensorType, scoped_refptr<PlatformSensor>>;
   PlatformSensorFusion(
       SensorReadingSharedBuffer* reading_buffer,
       PlatformSensorProvider* provider,
