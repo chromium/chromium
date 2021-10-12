84d667f73d202f1ddeb22622b8ccb0a753a2530f
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2021-10-12 16:48:15 +0000

    Fix UMA Commerce.Carts.ExtractionExecutionTime
    https://crrev.com/c/3205509 broke UMA
    Commerce.Carts.ExtractionExecutionTime because it didn't count the
    time spent on waiting for Promise().
    
    After the fix, it should be slightly longer than
    Commerce.Carts.ExtractionElapsedTime.
    
    (cherry picked from commit 1455c155369d6c2434948e34fe57fb472001a4f8)
    
    Bug: 1255627, 1255628, 1255629
    Change-Id: Ia134a0e5f69332265ae0c708967356b9525aff87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214221
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Auto-Submit: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#929905}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218303
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4664@{#52}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- chrome/renderer/cart/commerce_hint_agent.cc
+++ chrome/renderer/cart/commerce_hint_agent.cc
@@ -750,12 +750,6 @@ void CommerceHintAgent::JavaScriptRequest::WillExecute() {
 
 void CommerceHintAgent::JavaScriptRequest::Completed(
     const blink::WebVector<v8::Local<v8::Value>>& result) {
-  // Only record when the start time is correctly captured.
-  DCHECK(!start_time_.is_null());
-  if (!start_time_.is_null()) {
-    base::UmaHistogramTimes("Commerce.Carts.ExtractionExecutionTime",
-                            base::TimeTicks::Now() - start_time_);
-  }
   if (!agent_)
     return;
   blink::WebLocalFrame* main_frame = agent_->render_frame()->GetWebFrame();
@@ -789,6 +783,12 @@ void CommerceHintAgent::JavaScriptRequest::Completed(
 
 void CommerceHintAgent::JavaScriptRequest::HandlePromiseResults(
     const v8::FunctionCallbackInfo<v8::Value>& info) {
+  // Only record when the start time is correctly captured.
+  DCHECK(!start_time_.is_null());
+  if (!start_time_.is_null()) {
+    base::UmaHistogramTimes("Commerce.Carts.ExtractionExecutionTime",
+                            base::TimeTicks::Now() - start_time_);
+  }
   if (!agent_)
     return;
   blink::WebLocalFrame* main_frame = agent_->render_frame()->GetWebFrame();
