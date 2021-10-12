0b9cc9112c2928d799643754ab26aa22b0e71bad
Cathy Li <chili@chromium.org>
2021-10-12 16:34:15 +0000

    Prevent call-after-destroy in FeedSurfaceCoordinator.
    (cherry picked from commit 5f8356c681c8578587170cde6e115c9d457e2375)
    
    Bug: 1247675, 1229179
    Change-Id: I03e273b8fe62c01ca7432028e1b6b11028cc5bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3207377
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#928697}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218337
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4638@{#796}
    Cr-Branched-From: 159257cab5585bc8421abf347984bb32fdfe9eb9-refs/heads/main@{#920003}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -378,7 +378,6 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider, FeedBubbleDe
             mSwipeRefreshLayout = null;
         }
         stopBubbleTriggering();
-        mMediator.destroy();
         if (mFeedSurfaceLifecycleManager != null) mFeedSurfaceLifecycleManager.destroy();
         mFeedSurfaceLifecycleManager = null;
         if (mEnhancedProtectionPromoController != null) {
@@ -390,6 +389,9 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider, FeedBubbleDe
             mSectionHeaderModel.get(SectionHeaderListProperties.SECTION_HEADERS_KEY)
                     .removeObserver(mSectionHeaderListModelChangeProcessor);
         }
+        // Destroy mediator after all other related controller/processors are destroyed.
+        mMediator.destroy();
+
         maybeClearImageMemoryCache();
         FeedSurfaceTracker.getInstance().untrackSurface(this);
         if (mHybridListRenderer != null) {
