3162fa3a0f45f0a84763c120a94d4d06f0aca796
Avi Drissman <avi@chromium.org>
2021-10-13 19:47:15 +0000

    Switch to using Origin calls rather than converting URLs
    If the "last committed origin" is needed, get that directly rather
    than getting the "last committed URL" and converting that into an
    origin.
    
    This CL was uploaded by git cl split.
    
    R=sandersd@chromium.org
    
    Bug: 1259711
    Change-Id: Ie8b027a342d5ac43d3f429fbc7f7b8b829c4d0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221026
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931189}

--- chrome/browser/media/unified_autoplay_browsertest.cc
+++ chrome/browser/media/unified_autoplay_browsertest.cc
@@ -144,7 +144,7 @@ class UnifiedAutoplayBrowserTest : public InProcessBrowserTest {
         is_renderer_initiated, from_context_menu);
 
     open_url_params.initiator_origin =
-        url::Origin::Create(active_contents->GetLastCommittedURL());
+        active_contents->GetMainFrame()->GetLastCommittedOrigin();
     open_url_params.source_render_process_id =
         active_contents->GetMainFrame()->GetProcess()->GetID();
     open_url_params.source_render_frame_id =
