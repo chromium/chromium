fd8de18204e7291b0fb8187c5bafdd8dbca07579
Jared Saul <jsaul@google.com>
2021-10-13 16:32:50 +0000

    [AF] Add hovered/pressed and dark mode colors to PromoCodeLabelButton
    Also moves the button setup from ctor to AddedToWidget() in order for
    GetCascadingColor(~) to work properly.
    
    Screenshots of [default, hovered, pressed] buttons for [light, dark]
    themes below. Note that my gLinux themes differ a little from general
    Mac/Windows themes so it's greyer/darker than mocks, but these are the
    requested color sets.
    https://screenshot.googleplex.com/C9FZbbamRnvzj7k.png
    https://screenshot.googleplex.com/xM27VyJkKWNtGBF.png
    https://screenshot.googleplex.com/7Vr2BUixwfqpuBS.png
    https://screenshot.googleplex.com/SivDgLWnemFMv3e.png
    https://screenshot.googleplex.com/6jKdYXuFYJG4Nyu.png
    https://screenshot.googleplex.com/AoArdAhjf6utBpt.png
    
    Bug: 1248523
    Change-Id: I90f3880eae1271b674e42786d0813c7311ab86d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218385
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#931082}

--- chrome/browser/ui/views/autofill/payments/promo_code_label_button.cc
+++ chrome/browser/ui/views/autofill/payments/promo_code_label_button.cc
@@ -9,14 +9,24 @@
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
+#include "ui/views/animation/ink_drop.h"
+#include "ui/views/animation/ink_drop_host_view.h"
 #include "ui/views/background.h"
 
 namespace autofill {
 
-// TODO(crbug.com/1248523): Add in hovered and pressed background colors
-//     specified by Chrome UX, including dark mode colors when UX provides them.
-const SkColor kBackgroundColor = gfx::kGoogleGreen050;
-const SkColor kTextColor = gfx::kGoogleGreen800;
+// TODO(crbug.com/1248523): Move these to theme constants.
+const SkColor kLightModeBackgroundBaseColor = gfx::kGoogleGreen050;
+const SkColor kLightModeBackgroundInkDropColor = gfx::kGoogleGreen600;
+const SkColor kLightModeTextDefaultColor = gfx::kGoogleGreen800;
+const SkColor kLightModeTextHoveredColor = gfx::kGoogleGreen900;
+const SkColor kLightModeTextPressedColor = gfx::kGoogleGreen900;
+const SkColor kDarkModeBackgroundBaseColor = SkColorSetRGB(0x2C, 0x35, 0x32);
+const SkColor kDarkModeBackgroundInkDropColor = gfx::kGoogleGreen300;
+const SkColor kDarkModeTextDefaultColor = gfx::kGoogleGreen300;
+const SkColor kDarkModeTextHoveredColor = gfx::kGoogleGreen200;
+const SkColor kDarkModeTextPressedColor = gfx::kGoogleGreen200;
 
 const int kContentCopyIconSizePx = 24;
 const int kFontSizeIncreasePx = 3;
@@ -24,25 +34,54 @@ const int kHorizontalPaddingPx = 10;
 
 PromoCodeLabelButton::PromoCodeLabelButton(PressedCallback callback,
                                            const std::u16string& text)
-    : views::LabelButton(std::move(callback), text) {
-  SetTextColor(views::Button::STATE_NORMAL, kTextColor);
-  SetTextColor(views::Button::STATE_HOVERED, kTextColor);
-  SetTextColor(views::Button::STATE_PRESSED, kTextColor);
+    : views::LabelButton(std::move(callback), text) {}
+
+PromoCodeLabelButton::~PromoCodeLabelButton() = default;
+
+void PromoCodeLabelButton::OnThemeChanged() {
+  Button::OnThemeChanged();
+
+  bool use_dark_mode = color_utils::IsDark(GetCascadingBackgroundColor(this));
+
+  SetTextColor(views::Button::STATE_NORMAL, use_dark_mode
+                                                ? kDarkModeTextDefaultColor
+                                                : kLightModeTextDefaultColor);
+  SetTextColor(views::Button::STATE_HOVERED, use_dark_mode
+                                                 ? kDarkModeTextHoveredColor
+                                                 : kLightModeTextHoveredColor);
+  SetTextColor(views::Button::STATE_PRESSED, use_dark_mode
+                                                 ? kDarkModeTextPressedColor
+                                                 : kLightModeTextPressedColor);
   SetImageModel(
       views::Button::STATE_NORMAL,
-      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon, kTextColor,
+      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon,
+                                     use_dark_mode ? kDarkModeTextDefaultColor
+                                                   : kLightModeTextDefaultColor,
                                      kContentCopyIconSizePx));
   SetImageModel(
       views::Button::STATE_HOVERED,
-      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon, kTextColor,
+      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon,
+                                     use_dark_mode ? kDarkModeTextHoveredColor
+                                                   : kLightModeTextHoveredColor,
                                      kContentCopyIconSizePx));
   SetImageModel(
       views::Button::STATE_PRESSED,
-      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon, kTextColor,
+      ui::ImageModel::FromVectorIcon(vector_icons::kContentCopyIcon,
+                                     use_dark_mode ? kDarkModeTextPressedColor
+                                                   : kLightModeTextPressedColor,
                                      kContentCopyIconSizePx));
   SetBackground(views::CreateRoundedRectBackground(
-      kBackgroundColor, ChromeLayoutProvider::Get()->GetCornerRadiusMetric(
-                            views::Emphasis::kMedium)));
+      use_dark_mode ? kDarkModeBackgroundBaseColor
+                    : kLightModeBackgroundBaseColor,
+      ChromeLayoutProvider::Get()->GetCornerRadiusMetric(
+          views::Emphasis::kMedium)));
+
+  views::InkDrop::Get(this)->SetMode(views::InkDropHost::InkDropMode::ON);
+  views::InkDrop::Get(this)->SetBaseColor(
+      use_dark_mode ? kDarkModeBackgroundInkDropColor
+                    : kLightModeBackgroundInkDropColor);
+  SetHasInkDropActionOnClick(true);
+
   SetHorizontalAlignment(gfx::ALIGN_CENTER);
   label()->SetFontList(views::Label::GetDefaultFontList().Derive(
       kFontSizeIncreasePx, gfx::Font::NORMAL, gfx::Font::Weight::MEDIUM));
@@ -53,8 +92,6 @@ PromoCodeLabelButton::PromoCodeLabelButton(PressedCallback callback,
   insets.set_right(kHorizontalPaddingPx);
   SetBorder(views::CreateEmptyBorder(insets));
 }
-
-PromoCodeLabelButton::~PromoCodeLabelButton() = default;
 
 BEGIN_METADATA(PromoCodeLabelButton, views::LabelButton)
 END_METADATA
