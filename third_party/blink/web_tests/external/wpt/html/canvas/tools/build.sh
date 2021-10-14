49b0cc808e8bd2ed6c47e76ef14967d4cd54f9f2
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-14 20:00:52 +0000

    Import wpt@4ebe2e21eac5062eec9cf9e13f688b1de804319a
    Using wpt-import in Chromium b75d8b8f8740a26aee3019b59c3665cc9eee4c38.
    With Chromium commits locally applied on WPT:
    e4386fcd66 "webrtc wpt: fix invalid unicode indexOf method call"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=weizhong@google.com
    
    No-Export: true
    Change-Id: I3a8bd6003bf5c92c01a46439e9ea420ed5736a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222786
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#931669}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/build.sh
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/build.sh
@@ -3,5 +3,5 @@ set -ex
 
 cd "${0%/*}"
 virtualenv -p python .virtualenv
-.virtualenv/bin/pip install pyyaml cairocffi
+.virtualenv/bin/pip install pyyaml==5.4.1 cairocffi==1.3.0
 .virtualenv/bin/python gentest.py
