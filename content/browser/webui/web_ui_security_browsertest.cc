60ff8e7c12d95d666abbcb8d1414d029c417fc4c
Jiewei Qian <qjw@chromium.org>
2021-10-14 03:10:37 +0000

    webui: test shared resources can't be accessed from Web
    This CL adds a test to verify shared resources can't be access from Web,
    even if renderer side checks are disabled.
    
    Bug: 1247169
    Change-Id: I608436e064da5dba00f14395ced07826cb65c1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193939
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931360}

--- content/browser/webui/web_ui_security_browsertest.cc
+++ content/browser/webui/web_ui_security_browsertest.cc
@@ -24,6 +24,7 @@
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_paths.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -888,6 +889,49 @@ IN_PROC_BROWSER_TEST_F(WebUISecurityTest,
   EXPECT_EQ(console_observer.GetMessageAt(0),
             base::StringPrintf("Not allowed to load local resource: %s",
                                chrome_url.spec().c_str()));
+}
+
+class WebUIBrowserSideSecurityTest : public WebUISecurityTest {
+ public:
+  WebUIBrowserSideSecurityTest() = default;
+
+ protected:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // Disable Web Security to skip renderer-side checks so that we can test
+    // browser-side checks.
+    command_line->AppendSwitch(switches::kDisableWebSecurity);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(WebUIBrowserSideSecurityTest,
+                       DenyWebAccessToSharedResources) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  ASSERT_TRUE(
+      NavigateToURL(shell(), embedded_test_server()->GetURL("/title2.html")));
+
+  const char kLoadResourceScript[] =
+      "new Promise((resolve) => {"
+      "  const script = document.createElement('script');"
+      "  script.onload = () => {"
+      "    resolve('Script load should have failed');"
+      "  };"
+      "  script.onerror = (e) => {"
+      "    resolve('Load failed');"
+      "  };"
+      "  script.src = $1;"
+      "  document.body.appendChild(script);"
+      "});";
+
+  DevToolsInspectorLogWatcher log_watcher(shell()->web_contents());
+
+  GURL shared_resource_url =
+      GURL("chrome-untrusted://resources/mojo/mojo/public/js/bindings.js");
+  EXPECT_EQ("Load failed", EvalJs(shell(), JsReplace(kLoadResourceScript,
+                                                     shared_resource_url)));
+  log_watcher.FlushAndStopWatching();
+
+  EXPECT_EQ(log_watcher.last_message(),
+            "Failed to load resource: net::ERR_UNKNOWN_URL_SCHEME");
 }
 
 }  // namespace content
