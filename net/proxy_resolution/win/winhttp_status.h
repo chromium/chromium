90c6ff8f327a83e4e68b36f05dfcc3e8f0f218e0
Nicolas Arciniega <niarci@microsoft.com>
2021-10-15 19:51:58 +0000

    Add proxy_resolver_win Mojo
    This CL defines the mojo interface that will be used by the new
    proxy_resolver_win service. Callers wishing to interact with the service
    need only supply a URL to receive their proxy resolution in the mojo
    callback.
    
    This also adds the mojo version of WinHttpStatus, which was defined in
    /net code in the previous CL. This includes the traits necessary to
    convert between the two equivalent enums.
    
    Bug: 1032820
    Change-Id: I68ebc8ded0e0dd5afd077402701fbb0662af5bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206954
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#932171}

--- net/proxy_resolution/win/winhttp_status.h
+++ net/proxy_resolution/win/winhttp_status.h
@@ -7,46 +7,46 @@
 
 namespace net {
 
-// TODO(niarci): Add an equivalent enum in the proxy_resolver_win mojo
-//
 // This describes the full set of failure points that could occur when calling
 // into the proxy_resolver_win service. Further detail is additionally provided
 // by the Windows error code, which will be supplied alongside this enum.
+//
+// Keep in sync with proxy_resolver_win.mojom.WinHttpStatus.
 enum class WinHttpStatus {
-  // No Error
+  // No Error.
   kOk,
 
-  // Aborted by caller
+  // Aborted by caller.
   kAborted,
 
-  // WinHttp binary failed to load
+  // WinHttp binary failed to load.
   kFunctionsNotLoaded,
 
-  // WinHttpOpen() API failed
+  // WinHttpOpen() API failed.
   kWinHttpOpenFailed,
 
-  // WinHttpSetTimeouts() API failed
+  // WinHttpSetTimeouts() API failed.
   kWinHttpSetTimeoutsFailed,
 
-  // WinHttpSetStatusCallback() API failed
+  // WinHttpSetStatusCallback() API failed.
   kWinHttpSetStatusCallbackFailed,
 
-  // WinHttpGetIEProxyConfigForCurrentUser() API failed
+  // WinHttpGetIEProxyConfigForCurrentUser() API failed.
   kWinHttpGetIEProxyConfigForCurrentUserFailed,
 
-  // WinHttpCreateProxyResolver() API failed
+  // WinHttpCreateProxyResolver() API failed.
   kWinHttpCreateProxyResolverFailed,
 
-  // WinHttpGetProxyForURLEx() failed
+  // WinHttpGetProxyForURLEx() API failed.
   kWinHttpGetProxyForURLExFailed,
 
-  // Proxy resolution callback returned an error
+  // Proxy resolution callback returned an error.
   kStatusCallbackFailed,
 
-  // WinHttpGetProxyResult() API failed
+  // WinHttpGetProxyResult() API failed.
   kWinHttpGetProxyResultFailed,
 
-  // WinHttpGetProxyResult() API unexpectedly returned an empty list
+  // WinHttpGetProxyResult() API unexpectedly returned an empty list.
   kEmptyProxyList,
 };
 
