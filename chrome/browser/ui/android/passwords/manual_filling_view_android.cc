1b7d37113aa6153be5a03723d19e09ff215411cc
Avi Drissman <avi@chromium.org>
2021-10-15 08:50:17 +0000

    Switch to using Origin calls rather than converting URLs
    If the "last committed origin" is needed, get that directly rather
    than getting the "last committed URL" and converting that into an
    origin.
    
    This CL was uploaded by git cl split.
    
    R=mamir@chromium.org
    
    Bug: 1259711
    Change-Id: I14c4ae0ccf1d008fbeb43d17523e70ba7377b388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221613
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931931}

--- chrome/browser/ui/android/passwords/manual_filling_view_android.cc
+++ chrome/browser/ui/android/passwords/manual_filling_view_android.cc
@@ -262,7 +262,7 @@ void JNI_ManualFillingComponentBridge_CachePasswordSheetDataForTesting(
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(j_web_contents);
 
-  url::Origin origin = url::Origin::Create(web_contents->GetLastCommittedURL());
+  url::Origin origin = web_contents->GetMainFrame()->GetLastCommittedOrigin();
   std::vector<std::string> usernames;
   std::vector<std::string> passwords;
   base::android::AppendJavaStringArrayToStringVector(env, j_usernames,
