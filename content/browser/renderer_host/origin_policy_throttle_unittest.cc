1e7525f3b9f8601dbb082ad6a82df62822bbbd43
Robbie McElrath <rmcelrath@chromium.org>
2021-10-15 19:32:02 +0000

    Don't show Origin-Policy interstitial in iframes.
    This CL makes OriginPolicyThrottle show a regular net error instead of
    the custom Origin-Policy interstitial when the error is in an iframe or
    a non-primary page. This makes the interstitial match the behavior of
    the TLS interstitial.
    
    Bug: 1212263
    Change-Id: I9158ac9192d4512719804afa577c415f99c9f943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218488
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932164}

--- content/browser/renderer_host/origin_policy_throttle_unittest.cc
+++ content/browser/renderer_host/origin_policy_throttle_unittest.cc
@@ -15,11 +15,14 @@
 #include "content/browser/storage_partition_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/storage_partition.h"
+#include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/mock_navigation_handle.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
+#include "content/test/test_render_frame_host.h"
 #include "net/http/http_util.h"
 #include "services/network/public/cpp/origin_policy.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -27,6 +30,21 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
+
+namespace {
+
+const char kInterstitialContent[] = "error page content";
+
+class OriginPolicyErrorPageContentBrowserClient : public ContentBrowserClient {
+ public:
+  absl::optional<std::string> GetOriginPolicyErrorPage(
+      network::OriginPolicyState error_reason,
+      content::NavigationHandle* navigation_handle) override {
+    return kInterstitialContent;
+  }
+};
+
+}  // namespace
 
 class OriginPolicyThrottleTest : public RenderViewHostTestHarness,
                                  public testing::WithParamInterface<bool> {
@@ -38,10 +56,13 @@ class OriginPolicyThrottleTest : public RenderViewHostTestHarness,
     // Hence, we run this test in both variants.
     features_.InitWithFeatureState(features::kOriginPolicy, GetParam());
 
+    old_client_ = SetBrowserClientForTesting(&test_client_);
+
     RenderViewHostTestHarness::SetUp();
   }
   void TearDown() override {
     nav_handle_.reset();
+    SetBrowserClientForTesting(old_client_);
     RenderViewHostTestHarness::TearDown();
   }
   bool enabled() {
@@ -56,6 +77,8 @@ class OriginPolicyThrottleTest : public RenderViewHostTestHarness,
  protected:
   std::unique_ptr<MockNavigationHandle> nav_handle_;
   base::test::ScopedFeatureList features_;
+  OriginPolicyErrorPageContentBrowserClient test_client_;
+  ContentBrowserClient* old_client_;
 };
 
 INSTANTIATE_TEST_SUITE_P(OriginPolicyThrottleTests,
@@ -99,15 +122,35 @@ TEST_P(OriginPolicyThrottleTest, WillProcessResponse) {
     network::OriginPolicyState state;
     // The expected navigation thottle action.
     NavigationThrottle::ThrottleAction expected_action;
+    // The expected network error.
+    net::Error expected_net_error_code;
+    // The expected custom error message.
+    absl::optional<std::string> expected_error_page_content;
   } test_cases[] = {
-      {network::OriginPolicyState::kLoaded,
-       NavigationThrottle::ThrottleAction::PROCEED},
-      {network::OriginPolicyState::kNoPolicyApplies,
-       NavigationThrottle::ThrottleAction::PROCEED},
-      {network::OriginPolicyState::kCannotLoadPolicy,
-       NavigationThrottle::ThrottleAction::CANCEL},
-      {network::OriginPolicyState::kCannotParseHeader,
-       NavigationThrottle::ThrottleAction::CANCEL},
+      {
+          network::OriginPolicyState::kLoaded,
+          NavigationThrottle::ThrottleAction::PROCEED,
+          net::OK,
+          absl::nullopt,
+      },
+      {
+          network::OriginPolicyState::kNoPolicyApplies,
+          NavigationThrottle::ThrottleAction::PROCEED,
+          net::OK,
+          absl::nullopt,
+      },
+      {
+          network::OriginPolicyState::kCannotLoadPolicy,
+          NavigationThrottle::ThrottleAction::CANCEL,
+          net::ERR_BLOCKED_BY_CLIENT,
+          kInterstitialContent,
+      },
+      {
+          network::OriginPolicyState::kCannotParseHeader,
+          NavigationThrottle::ThrottleAction::CANCEL,
+          net::ERR_BLOCKED_BY_CLIENT,
+          kInterstitialContent,
+      },
   };
 
   for (const auto& test : test_cases) {
@@ -124,8 +167,40 @@ TEST_P(OriginPolicyThrottleTest, WillProcessResponse) {
 
     EXPECT_EQ(test.expected_action,
               navigation->GetLastThrottleCheckResult().action());
+    EXPECT_EQ(test.expected_net_error_code,
+              navigation->GetLastThrottleCheckResult().net_error_code());
+    EXPECT_EQ(test.expected_error_page_content,
+              navigation->GetLastThrottleCheckResult().error_page_content());
     OriginPolicyThrottle::ResetOriginPolicyForTesting();
   }
+}
+
+TEST_P(OriginPolicyThrottleTest, Iframe) {
+  if (!enabled())
+    return;
+
+  NavigationSimulator::NavigateAndCommitFromBrowser(
+      web_contents(), GURL("https://example.org/main"));
+  RenderFrameHost* iframe_rfh = static_cast<TestRenderFrameHost*>(main_rfh())
+                                    ->AppendChild("child_iframe");
+
+  network::OriginPolicy result;
+  result.state = network::OriginPolicyState::kCannotLoadPolicy;
+  OriginPolicyThrottle::SetOriginPolicyForTesting(result);
+
+  auto navigation = NavigationSimulator::CreateRendererInitiated(
+      GURL("https://example.org/iframe"), iframe_rfh);
+  navigation->Start();
+  navigation->ReadyToCommit();
+
+  EXPECT_EQ(NavigationThrottle::ThrottleAction::CANCEL,
+            navigation->GetLastThrottleCheckResult().action());
+  EXPECT_EQ(net::ERR_BLOCKED_BY_CLIENT,
+            navigation->GetLastThrottleCheckResult().net_error_code());
+  EXPECT_FALSE(navigation->GetLastThrottleCheckResult()
+                   .error_page_content()
+                   .has_value());
+  OriginPolicyThrottle::ResetOriginPolicyForTesting();
 }
 
 }  // namespace content
