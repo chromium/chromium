0fdc493aa517c91020b78bead98b103df5d8251c
Alice Wang <aliceywang@chromium.org>
2021-10-15 09:39:26 +0000

    [Sheriff] Disable flaky AccountsReloadingTest on Android L and M
    Bug: 1254405
    Change-Id: I2594a8fb75fee7c3e960db693cea0abcc31a729f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226060
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931944}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountsReloadingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountsReloadingTest.java
@@ -4,6 +4,8 @@
 
 package org.chromium.chrome.browser.signin;
 
+import android.os.Build;
+
 import androidx.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -17,6 +19,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -171,6 +174,7 @@ public class AccountsReloadingTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "crbug/1254405")
     public void testRefreshTokenUpdateWhenSignedInUserAddsNewAccount() {
         final CoreAccountInfo account1 = mAccountManagerTestRule.addTestAccountThenSignin();
         CriteriaHelper.pollUiThread(() -> mObserver.mCallCount == 1);
