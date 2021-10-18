fa5894bbe33e0ca3684e7f31e3b67f2ebedba937
Andrew Williams <awillia@google.com>
2021-10-18 15:20:57 +0000

    BroadcastChannel: Add WPTs for service worker, cross origin, frames
    Adds new WPTs to test that:
     - messages can't be delivered to frames in a different origin
     - service workers can communicate via BroadcastChannels
     - ordering of messages is correct even with multiple frames
    
    Bug: 1239274
    Change-Id: I2768c38ba1645a8c9b5387007d568191ab08762b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220860
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Andrew Williams <awillia@google.com>
    Cr-Commit-Position: refs/heads/main@{#932526}

---
+++ third_party/blink/web_tests/external/wpt/webmessaging/broadcastchannel/resources/service-worker.js
@@ -0,0 +1,14 @@
+let promise_func = null;
+let promise = new Promise(resolve => promise_func = resolve);
+
+const SERVICE_WORKER_TEST_CHANNEL_NAME = 'service worker';
+const bc3 = new BroadcastChannel(SERVICE_WORKER_TEST_CHANNEL_NAME);
+bc3.onmessage = e => {
+  bc3.postMessage('done');
+  promise_func();
+};
+bc3.postMessage('from worker');
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(promise);
+});
