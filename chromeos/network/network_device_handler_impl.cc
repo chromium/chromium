9f1e277ff425fe51536f28e5260c5320a61f9728
Chad Duffin <chadduffin@chromium.org>
2021-10-19 18:05:33 +0000

    [CrOS Cellular] Only set AllowRoaming when flag is disabled.
    This change updates the code responsible for propagating values for
    Device.AllowRoaming and Device.PolicyAllowRoaming such that it only
    propagates update values for Device.AllowRoaming when the per-network
    cellular roaming feature flag is disabled.
    
    This change was confirmed to fix the associated issue by:
     - Disabling the feature flag.
     - Allowing/disallowing cellular roaming via policy.
     - Removing the pSIM.
     - Clearing the Shill cache.
     - Enabling the feature flag.
     - Inserting the pSIM.
     - Checking that cellular roaming enabled state for the pSIM defaulted
       to the state from when the feature flag was disabled.
    
    Additionally (eSIM):
     - Disabling the feature flag.
     - Disallowing cellular roaming via policy.
     - Clearing the Shill cache.
     - Enabling the feature flag.
     - Allowing cellular roaming via policy.
     - Installed new eSIM profile.
     - Checked that roaming was disabled for new eSIM.
    
    Additionally (powerwash):
     - Removing the pSIM.
     - Powerwashing the device via admin console.
     - Enrolling the device.
     - Allowing cellular roaming via policy.
     - Inserting the pSIM.
     - Checking that roaming was disabled for new pSIM.
    
    (cherry picked from commit aca31f64cccbd2889d54d863586730be73133ac2)
    
    Fixed: 1258227
    Test: See above, and stepped through applicable CUJs.
    Change-Id: Ifa20c7d8a6a22cfaac90efc29b17cc4d09b18b32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222147
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#932583}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3230221
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4664@{#217}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- chromeos/network/network_device_handler_impl.cc
+++ chromeos/network/network_device_handler_impl.cc
@@ -293,10 +293,19 @@ void NetworkDeviceHandlerImpl::ApplyCellularAllowRoamingToShill() {
   for (NetworkStateHandler::DeviceStateList::const_iterator it = list.begin();
        it != list.end(); ++it) {
     const DeviceState* device_state = *it;
-    SetDevicePropertyInternal(
-        device_state->path(), shill::kCellularAllowRoamingProperty,
-        base::Value(cellular_allow_roaming_), base::DoNothing(),
-        network_handler::ErrorCallback());
+
+    // We no longer want to set the Device.AllowRoaming Shill property once
+    // per-network cellular roaming is enabled since we want to use this value
+    // as the default for newly added networks until we finish migrating to
+    // using only the Service.AllowRoaming and Device.PolicyAllowRoaming Shill
+    // properties. For more details see crbug.com/1258227.
+    if (!base::FeatureList::IsEnabled(
+            ash::features::kCellularAllowPerNetworkRoaming)) {
+      SetDevicePropertyInternal(
+          device_state->path(), shill::kCellularAllowRoamingProperty,
+          base::Value(cellular_allow_roaming_), base::DoNothing(),
+          network_handler::ErrorCallback());
+    }
     SetDevicePropertyInternal(
         device_state->path(), shill::kCellularPolicyAllowRoamingProperty,
         base::Value(cellular_policy_allow_roaming_), base::DoNothing(),
