16a5f539f6730963bed515b6f20fe481885cc25d
Caitlin Fischer <caitlinfischer@google.com>
2021-10-19 19:33:57 +0000

    Replace DLOG(WARNING) with LOG(ERROR).
    DLOGs were added in crrev.com/c/3227104. However, in order to see such
    output, it must be a debug build.
    
    Bug: 1259550
    Change-Id: Ib3777efeb9c4c7100e9eb813acc8d76ea5ae35b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232017
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933128}

--- chrome/browser/metrics/variations/variations_safe_mode_browsertest.cc
+++ chrome/browser/metrics/variations/variations_safe_mode_browsertest.cc
@@ -249,8 +249,8 @@ class FieldTrialTest : public ::testing::Test {
     EXPECT_TRUE(IsCrashingSubTestOutput(output))
         << "Did not find crash signals in output:\n"
         << output;
-    // TODO(crbug/1259550): Remove DLOG once resolved.
-    DLOG(WARNING) << "output: " << output;
+    // TODO(crbug/1259550): Remove LOG once resolved.
+    LOG(ERROR) << "output: " << output;
   }
 
   std::unique_ptr<PrefService> LoadLocalState(const base::FilePath& path) {
