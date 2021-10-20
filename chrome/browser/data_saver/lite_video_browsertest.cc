52dbfad7964080d60b21fcae2d94a7a938253219
Xida Chen <xidachen@chromium.org>
2021-10-20 14:18:40 +0000

    [sheriff] Mark LiteVideoStopThrottlingOnPlaybackSeekBrowserTest.StopsThrottlingOnPlaybackSeek flaky on Win
    Bug: 1248927
    Change-Id: I3a132e73377ce1dcd95821a8a99abeab8c5a2aff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233094
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Owners-Override: Xida Chen <xidachen@chromium.org>
    Auto-Submit: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933453}

--- chrome/browser/data_saver/lite_video_browsertest.cc
+++ chrome/browser/data_saver/lite_video_browsertest.cc
@@ -352,8 +352,15 @@ INSTANTIATE_TEST_SUITE_P(,
                          ::testing::Bool());
 
 // When video is seeked throttling is stopped.
+// crbug.com/1248927: flaky on win
+#if defined(OS_WIN)
+#define MAYBE_StopsThrottlingOnPlaybackSeek \
+  DISABLED_StopsThrottlingOnPlaybackSeek
+#else
+#define MAYBE_StopsThrottlingOnPlaybackSeek StopsThrottlingOnPlaybackSeek
+#endif
 IN_PROC_BROWSER_TEST_P(LiteVideoStopThrottlingOnPlaybackSeekBrowserTest,
-                       StopsThrottlingOnPlaybackSeek) {
+                       MAYBE_StopsThrottlingOnPlaybackSeek) {
   ukm::TestAutoSetUkmRecorder ukm_recorder;
   TestMSEPlayback("bear-vp9.webm", "2000", "2000", false);
 
