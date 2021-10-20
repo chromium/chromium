fa7e7a804ea156eb22b962d31aeb1a0cd7976a22
Peter Kasting <pkasting@chromium.org>
2021-10-20 15:56:28 +0000

    Add base::apply(), a backport of C++17's std::apply().
    This was requested by kylixrd@ for some Views infrastructure work.
    
    Bug: none
    Change-Id: I83aa639b4dc88da6c5b74cb152bd1e5ab55f679d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234656
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933486}

--- base/cxx17_backports.h
+++ base/cxx17_backports.h
@@ -10,8 +10,12 @@
 #include <initializer_list>
 #include <memory>
 #include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 
 #include "base/check.h"
+#include "base/functional/invoke.h"
 
 namespace base {
 
@@ -106,6 +110,26 @@ constexpr const T& clamp(const T& v, const T& lo, const T& hi, Compare comp) {
 template <typename T>
 constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
   return base::clamp(v, lo, hi, std::less<T>{});
+}
+
+// C++14 implementation of C++17's std::apply():
+// https://en.cppreference.com/w/cpp/utility/apply
+namespace internal {
+template <class F, class Tuple, std::size_t... I>
+constexpr decltype(auto) apply_impl(F&& f,
+                                    Tuple&& t,
+                                    std::index_sequence<I...>) {
+  return base::invoke(std::forward<F>(f),
+                      std::get<I>(std::forward<Tuple>(t))...);
+}
+}  // namespace internal
+
+template <class F, class Tuple>
+constexpr decltype(auto) apply(F&& f, Tuple&& t) {
+  return internal::apply_impl(
+      std::forward<F>(f), std::forward<Tuple>(t),
+      std::make_index_sequence<
+          std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
 }
 
 }  // namespace base
