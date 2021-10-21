ce6c41ceef6891abac6d8b55dd64c6c31e097924
Gavin Dodd <gavindodd@google.com>
2021-10-21 01:47:26 +0000

    Improve stub RequestUpdateCheckWithoutApplying
    Make
    UpdateEngineClientStubImpl::RequestUpdateCheckWithoutApplying
    only report states up to UPDATE_AVAILABLE and then return to IDLE,
    which is closer to real behavior
    
    BUG=b:197793656
    TEST= build local chrome and confirm sub behavior
    
    Change-Id: I9b24796c7b59af205d2919848ca2048343547c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233600
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#933742}

--- chromeos/dbus/update_engine/update_engine_client.cc
+++ chromeos/dbus/update_engine/update_engine_client.cc
@@ -556,7 +556,8 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
     return observers_.HasObserver(observer);
   }
 
-  void RequestUpdateCheck(UpdateCheckCallback callback) override {
+  void RequestUpdateCheckInternal(UpdateCheckCallback callback,
+                                  bool apply_update) {
     if (last_status_.current_operation() != update_engine::Operation::IDLE) {
       std::move(callback).Run(UPDATE_RESULT_FAILED);
       return;
@@ -572,13 +573,17 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
     base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
         FROM_HERE,
         base::BindOnce(&UpdateEngineClientStubImpl::StateTransition,
-                       weak_factory_.GetWeakPtr()),
+                       weak_factory_.GetWeakPtr(), apply_update),
         base::Milliseconds(kStateTransitionDefaultDelayMs));
+  }
+
+  void RequestUpdateCheck(UpdateCheckCallback callback) override {
+    RequestUpdateCheckInternal(std::move(callback), true);
   }
 
   void RequestUpdateCheckWithoutApplying(
       UpdateCheckCallback callback) override {
-    RequestUpdateCheck(std::move(callback));
+    RequestUpdateCheckInternal(std::move(callback), false);
   }
 
   void RebootAfterUpdate() override {}
@@ -628,7 +633,7 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
   }
 
  private:
-  void StateTransition() {
+  void StateTransition(bool apply_update) {
     update_engine::Operation next_operation = update_engine::Operation::ERROR;
     int delay_ms = kStateTransitionDefaultDelayMs;
     switch (last_status_.current_operation()) {
@@ -644,7 +649,8 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
         next_operation = update_engine::Operation::UPDATE_AVAILABLE;
         break;
       case update_engine::Operation::UPDATE_AVAILABLE:
-        next_operation = update_engine::Operation::DOWNLOADING;
+        next_operation = apply_update ? update_engine::Operation::DOWNLOADING
+                                      : update_engine::Operation::IDLE;
         break;
       case update_engine::Operation::DOWNLOADING:
         if (last_status_.progress() >= 1.0) {
@@ -673,7 +679,7 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
       base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
           FROM_HERE,
           base::BindOnce(&UpdateEngineClientStubImpl::StateTransition,
-                         weak_factory_.GetWeakPtr()),
+                         weak_factory_.GetWeakPtr(), apply_update),
           base::Milliseconds(delay_ms));
     }
   }
