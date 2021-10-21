19d503e2d77b452d3322a47c6d328c5e47476228
K. Moon <kmoon@chromium.org>
2021-10-21 06:15:42 +0000

    [unseasoned-pdf] Don't disable internal PDF plugin
    Only disables the external PDF plugin when the
    "plugins.always_open_pdf_externally" preference is set. We don't
    actually want to disable the internal PDF plugin (which is also used by
    Print Preview) based on this preference, only the PDF viewer.
    
    Adds extra security checks dropped from crrev.com/c/3233319.
    
    Fixed: 1261758
    Change-Id: Ieba7089d4b825c92e5b92d6d6699689c4685fd1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233761
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933794}

--- components/pdf/renderer/internal_plugin_renderer_helpers.cc
+++ components/pdf/renderer/internal_plugin_renderer_helpers.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "base/check.h"
 #include "base/feature_list.h"
 #include "components/pdf/renderer/pdf_internal_plugin_delegate.h"
 #include "components/pdf/renderer/pdf_view_web_plugin_client.h"
@@ -46,14 +47,22 @@ blink::WebPlugin* CreateInternalPlugin(
     return render_frame->CreatePlugin(info, params);
   }
 
-  // The in-process plugin should only be created if the parent frame has an
-  // allowed origin.
-  blink::WebFrame* parent_frame = render_frame->GetWebFrame()->Parent();
-  if (!parent_frame)
+  // The in-process plugin should only be created if the parent frame's origin
+  // was allowed to (externally) embed the internal plugin.
+  blink::WebFrame* frame = render_frame->GetWebFrame();
+  blink::WebFrame* parent_frame = frame->Parent();
+  if (!parent_frame ||
+      !delegate->IsAllowedOrigin(parent_frame->GetSecurityOrigin())) {
     return nullptr;
+  }
 
-  if (!delegate->IsAllowedOrigin(parent_frame->GetSecurityOrigin()))
-    return nullptr;
+  // Origins allowed to embed the internal plugin are trusted (the PDF viewer
+  // and Print Preview), and should never directly create the in-process plugin.
+  // Likewise, they should not share a process with this frame.
+  //
+  // See crbug.com/1259635 and crbug.com/1261758 for examples of previous bugs.
+  CHECK(!delegate->IsAllowedOrigin(frame->GetSecurityOrigin()));
+  CHECK(parent_frame->IsWebRemoteFrame());
 
   mojo::AssociatedRemote<pdf::mojom::PdfService> pdf_service_remote;
   render_frame->GetRemoteAssociatedInterfaces()->GetInterface(
