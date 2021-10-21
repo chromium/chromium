aa19bba58acd632e0dd5d28c6af06525bbb26b05
Adrian Taylor <adetaylor@chromium.org>
2021-10-21 18:56:34 +0000

    [rust] Unit test executable improvements
    This CL has two changes to rust_source_set:
    
    1. It respects crate_root for unit tests.
    2. It allows a custom unit test executable name.
    
    This latter thing has proven necessary where we needed a crate
    called (say) 'mojo' and existing code relied on it being called
    'mojo', yet the unit tests would therefore previously have been
    called 'mojo_unittests' which conflicts with an existing target.
    
    Fuchsia's advice is to allow custom unit test executable names,
    so that's what this change does.
    
    Bug: 1069271
    Change-Id: I8860e6a6ff98e6ad07ff0199d3515f23c204429e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232188
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933971}

--- build/rust/tests/test_rust_exe/main.rs
+++ build/rust/tests/test_rust_exe/main.rs
@@ -4,8 +4,10 @@
 
 use test_mixed_source_set_rs::say_hello_from_a_cpp_callback_from_rust;
 use test_rust_source_set::say_hello;
+use test_rust_source_set_non_standard_arrangement::say_foo;
 
 fn main() {
     say_hello();
+    say_foo();
     say_hello_from_a_cpp_callback_from_rust();
 }
