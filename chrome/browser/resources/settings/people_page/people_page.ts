830c0dcf2287d0f376f0a85338fc6a3d2ad6dbf8
Samuel Huang <huangs@chromium.org>
2021-10-21 15:47:50 +0000

    Revert "[Lacros] Remove "import data" settings button"
    This reverts commit 1b710fc7ed68e8fdcfee9e4218ebea60fca992a1.
    
    Reason for revert: Causes failure on clean Lacros builds for target chrome/browser/resources/settings:build_ts.
    
    Original change's description:
    > [Lacros] Remove "import data" settings button
    >
    > This is button is only supported on desktop platforms.
    >
    > Bug: 1226072
    > Change-Id: I7ff41d85217daceb9ca4e2a38f18f07315b9aa01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3231817
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#933836}
    
    Bug: 1226072
    Change-Id: I2798bdaa1a9ed6ba1d178a4c6ef87a61dbf3d4a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237108
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Owners-Override: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933896}

--- chrome/browser/resources/settings/people_page/people_page.ts
+++ chrome/browser/resources/settings/people_page/people_page.ts
@@ -366,7 +366,7 @@ class SettingsPeoplePageElement extends SettingsPeoplePageElementBase {
     Router.getInstance().navigateTo(routes.SYNC);
   }
 
-  // <if expr="not chromeos and not lacros">
+  // <if expr="not chromeos">
   private onImportDataTap_() {
     Router.getInstance().navigateTo(routes.IMPORT_DATA);
   }
