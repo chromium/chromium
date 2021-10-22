78c2081d1d0de162a81413def5e68be3feed37f5
David Bertoni <dbertoni@chromium.org>
2021-10-22 06:29:23 +0000

    [Extensions] Add tests for chrome.idle.queryState().
    This CL adds additional tests for chrome.idle.queryState(), along
    with parameterizing all of the tests to run as both persistent
    background-based and service worker-based extensions.
    
    Bug: 1093066
    Change-Id: I494bbf57b1c31d76314ae8000d35f3644be3b4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3236026
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim Judkins <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934156}

--- chrome/browser/extensions/api/idle/idle_apitest.cc
+++ chrome/browser/extensions/api/idle/idle_apitest.cc
@@ -2,18 +2,80 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/browser/api/idle/idle_manager_factory.h"
+#include "extensions/browser/api/idle/test_idle_provider.h"
 
 namespace extensions {
 
+using ContextType = ExtensionBrowserTest::ContextType;
+
+class IdleApiTest : public ExtensionApiTest,
+                    public testing::WithParamInterface<ContextType> {
+ public:
+  IdleApiTest() : ExtensionApiTest(GetParam()) {}
+  ~IdleApiTest() override = default;
+  IdleApiTest(const IdleApiTest& other) = delete;
+  IdleApiTest& operator=(const IdleApiTest& other) = delete;
+
+ protected:
+  void SetTestIdleProvider(int idle_time, bool locked) {
+    auto idle_provider = std::make_unique<TestIdleProvider>();
+    idle_provider->set_idle_time(idle_time);
+    idle_provider->set_locked(locked);
+    IdleManagerFactory::GetForBrowserContext(profile())
+        ->SetIdleTimeProviderForTest(std::move(idle_provider));
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         IdleApiTest,
+                         ::testing::Values(ContextType::kPersistentBackground));
+
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         IdleApiTest,
+                         ::testing::Values(ContextType::kServiceWorker));
+
+IN_PROC_BROWSER_TEST_P(IdleApiTest, QueryStateActive) {
+  // Set up a test IdleProvider in the active state.
+  SetTestIdleProvider(/*idle_time=*/0, /*locked=*/false);
+  ASSERT_TRUE(
+      RunExtensionTest("idle/query_state", {.custom_arg = "queryStateActive"}));
+}
+
+IN_PROC_BROWSER_TEST_P(IdleApiTest, QueryStateIdle) {
+  // Set up a test IdleProvider in the idle state. The JS test uses the
+  // value 15 for its "intervalInSeconds" value.
+  SetTestIdleProvider(/*idle_time=*/15, /*locked=*/false);
+  ASSERT_TRUE(
+      RunExtensionTest("idle/query_state", {.custom_arg = "queryStateIdle"}));
+}
+
+IN_PROC_BROWSER_TEST_P(IdleApiTest, QueryStateAlmostIdle) {
+  // Set up a test IdleProvider in the active state, just about to transition
+  // to idle. The JS test uses the value 15 for its "intervalInSeconds" value.
+  SetTestIdleProvider(/*idle_time=*/14, /*locked=*/false);
+  ASSERT_TRUE(
+      RunExtensionTest("idle/query_state", {.custom_arg = "queryStateActive"}));
+}
+
+IN_PROC_BROWSER_TEST_P(IdleApiTest, QueryStateLocked) {
+  // Set up a test IdleProvider in the locked state.
+  SetTestIdleProvider(/*idle_time=*/0, /*locked=*/true);
+  ASSERT_TRUE(
+      RunExtensionTest("idle/query_state", {.custom_arg = "queryStateLocked"}));
+}
+
 #if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, IdleGetAutoLockDelay) {
+IN_PROC_BROWSER_TEST_P(IdleApiTest, IdleGetAutoLockDelay) {
   ASSERT_TRUE(RunExtensionTest("idle/get_auto_lock_delay")) << message_;
 }
 #else
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, UnsupportedIdleGetAutoLockDelay) {
+IN_PROC_BROWSER_TEST_P(IdleApiTest, UnsupportedIdleGetAutoLockDelay) {
   ASSERT_TRUE(RunExtensionTest("idle/unsupported_get_auto_lock_delay"));
 }
 #endif
