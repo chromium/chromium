ce40d39a95dc937869161409fc85a4aaa50040fa
Morten Stenshorne <mstensho@chromium.org>
2021-10-25 09:32:30 +0000

    Revert "Use `notifyCurrentFile` API to update window icon and title."
    This reverts commit 825642e4f3b82a279477ae2e3a078c387116a30f.
    
    Reason for revert: Suspecting apps.LaunchGallery.stable failure
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-arc-r-chrome/9945/overview
    
    Original change's description:
    > Use `notifyCurrentFile` API to update window icon and title.
    >
    > MediaApp now supports multiple windows, which show up in the shelf
    > window menu as "Gallery Gallery Gallery". This isn't useful, and makes
    > it hard for users to find files that they have open.
    >
    > This CL uses the `notifyCurrentFile` API to listen to current file
    > changes and update the window icon and title to refelct the current
    > file. Specifically, this means the title becomes the filename and the
    > favicon becomes the file type.
    >
    > Bug: b/200898614, b/202214335
    > Change-Id: I336d45d03cc1d24d960292b20b58529c80e2be86
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3230995
    > Commit-Queue: Mitchell McDermott <mcdermottm@google.com>
    > Reviewed-by: dstockwell <dstockwell@google.com>
    > Cr-Commit-Position: refs/heads/main@{#934382}
    
    Bug: b/200898614, b/202214335
    Change-Id: I79da6a22b90a0a9c4d6d91ea8af72ba9e8e1db17
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3240923
    Owners-Override: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#934462}

--- ash/webui/media_app_ui/test/guest_query_receiver.js
+++ ash/webui/media_app_ui/test/guest_query_receiver.js
@@ -121,9 +121,6 @@ const SIMPLE_TEST_QUERIES = {
     Object.assign(resultData, assertLastReceivedFileArray().map(flattenFile));
     return `${resultData.length}`;
   },
-  notifyCurrentFile: (data, resultData) => {
-    DELEGATE.notifyCurrentFile(data.simpleArgs.name, data.simpleArgs.type);
-  },
   openFileAtIndex: async (data, resultData) => {
     const handle = assertLastReceivedFileList().item(data.simpleArgs.index);
     const domFile = await handle.openFile();
