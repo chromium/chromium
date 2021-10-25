1274702cc92c1a8e52db983ce9efe13c4a405f8e
Ben joyce <bjoyce@chromium.org>
2021-10-25 13:21:00 +0000

    Revert "Update 3pp files for robolectric 4.6.1"
    This reverts commit 71c899442ff71052a7986c3e295635dd759504fc.
    
    Reason for revert: Planned revert now that 3pp has downloaded the files.
    
    Original change's description:
    > Update 3pp files for robolectric 4.6.1
    >
    > 3pp leaves us in a weird state where we can't actually compile suites with
    > robolectric 4.6.1 until the files are downloaded into cipd by 3pp. So we
    > need to add these files first to get the binaries downloaded into cipd.
    > This CL will be reverted once 3pp has downloaded the files into the cipd
    > repo while the numerous 4.6.1 changes are made, such as converting test
    > cases to using shadow loopers. The reverting of this CL will still leave
    > the files in the CIPD repo for use.
    >
    > Bug: 1195173
    > Change-Id: I525ef3ae200ff7d8be149ee3cdaf37451cf3e14c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234391
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Ben joyce <bjoyce@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#933924}
    
    Bug: 1195173
    Change-Id: Ia1469cf1049a83d1d09d4eadc1a9f72225127361
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237869
    Auto-Submit: Ben joyce <bjoyce@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934506}

--- third_party/android_deps/libs/org_robolectric_plugins_maven_dependency_resolver/3pp/fetch.py
+++ third_party/android_deps/libs/org_robolectric_plugins_maven_dependency_resolver/3pp/fetch.py
@@ -12,9 +12,9 @@ import argparse
 import json
 import os
 
-_FILE_URL = 'https://repo.maven.apache.org/maven2/org/robolectric/plugins-maven-dependency-resolver/4.6.1/plugins-maven-dependency-resolver-4.6.1.jar'
-_FILE_NAME = 'plugins-maven-dependency-resolver-4.6.1.jar'
-_FILE_VERSION = '4.6.1'
+_FILE_URL = 'https://repo.maven.apache.org/maven2/org/robolectric/plugins-maven-dependency-resolver/4.3.1/plugins-maven-dependency-resolver-4.3.1.jar'
+_FILE_NAME = 'plugins-maven-dependency-resolver-4.3.1.jar'
+_FILE_VERSION = '4.3.1'
 
 
 def do_latest():
