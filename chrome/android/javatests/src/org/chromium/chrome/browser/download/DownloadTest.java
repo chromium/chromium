ce66fbd19b471a67a0a8abbfde372d9811a98285
Min Qin <qinmin@chromium.org>
2021-10-26 23:33:30 +0000

    Add field trial testing config for dangerous download dialog
    Change-Id: Ic5ab22237108f9d85d389c9675a1b66b39949d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245637
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935197}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
@@ -75,7 +75,8 @@ import java.util.List;
 @RunWith(ParameterizedRunner.class)
 @ParameterAnnotations.UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
-        "disable-features=" + ChromeFeatureList.ENABLE_DUPLICATE_DOWNLOAD_DIALOG})
+        "disable-features=" + ChromeFeatureList.ENABLE_DUPLICATE_DOWNLOAD_DIALOG + ","
+                + ChromeFeatureList.ENABLE_DANGEROUS_DOWNLOAD_DIALOG})
 public class DownloadTest implements CustomMainActivityStart {
     @ParameterAnnotations.ClassParameter
     private static List<ParameterSet> sClassParams = Arrays.asList(
