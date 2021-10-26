25670a1e88c77aaacc5fd21aeca813fb6843f057
Jimmy Gong <jimmyxgong@google.com>
2021-10-26 22:12:24 +0000

    shortcuts: Add AcceleratorConfiguration interface
    AcceleratorConfiguration is an interface to be implemented by
    all relevant accelerator sources e.g. Browser, Ash, Android. This
    interface streamlines accelerator fetching and mutating functions.
    
    Created in ash/public/cpp so that it is available to all sources.
    
    Bug: 1060690
    Change-Id: I29688e3237cc317e6720e3757a2498ad88869081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219849
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935160}

---
+++ ash/public/cpp/accelerator_configuration.cc
@@ -0,0 +1,37 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/accelerator_configuration.h"
+
+#include "base/ranges/algorithm.h"
+
+namespace ash {
+
+AcceleratorConfiguration::AcceleratorConfiguration(AcceleratorSource source)
+    : source_(source) {}
+
+AcceleratorConfiguration::~AcceleratorConfiguration() = default;
+
+void AcceleratorConfiguration::AddAcceleratorsUpdatedCallback(
+    AcceleratorsUpdatedCallback callback) {
+  callbacks_.push_back(callback);
+}
+
+void AcceleratorConfiguration::RemoveAcceleratorsUpdatedCallback(
+    AcceleratorsUpdatedCallback callback) {
+  const auto it = base::ranges::find_if(
+      callbacks_, [callback](const auto& o) { return o == callback; });
+  if (it == callbacks_.end())
+    return;
+
+  callbacks_.erase(it);
+}
+
+void AcceleratorConfiguration::NotifyAcceleratorsUpdated(
+    const std::multimap<AcceleratorAction, AcceleratorInfo>& accelerators) {
+  for (auto& cb : callbacks_) {
+    cb.Run(source_, accelerators);
+  }
+}
+}  // namespace ash
