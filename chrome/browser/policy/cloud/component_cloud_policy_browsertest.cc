4357890b7eba2bb5d118d348d4fb7a07595f8de9
Maksim Ivanov <emaxx@chromium.org>
2021-10-26 06:38:47 +0000

    Revert "Disable flaky test ComponentCloudPolicyTest.SignOutAndBackIn"
    This reverts commit f6d885fd6cd215d6b8f9842f182708dcae43f667.
    
    Reason for revert: The presumed fix for the flakiness landed in
    r934718.
    
    Original change's description:
    > Disable flaky test ComponentCloudPolicyTest.SignOutAndBackIn
    >
    > This test regularly flakes on all platforms.
    >
    > Tbr: mstensho@chromium.org
    > Bug: 1205239
    > Change-Id: Ifb560d74455cf130c8a23885f643c8f8ab080364
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3241450
    > Owners-Override: Robert Flack <flackr@chromium.org>
    > Auto-Submit: Robert Flack <flackr@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    > Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#934529}
    
    Bug: 1205239
    Change-Id: I758931d96f005c01c8c9a931de409fcda654d4d8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244082
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934849}

--- chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
+++ chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
@@ -332,8 +332,7 @@ IN_PROC_BROWSER_TEST_F(ComponentCloudPolicyTest, MAYBE_InstallNewExtension) {
 // get policy for components working again.
 // Signing out on Lacros is not possible.
 #if !defined(OS_CHROMEOS)
-// Flaky, see https://crbug.com/1205239
-IN_PROC_BROWSER_TEST_F(ComponentCloudPolicyTest, DISABLED_SignOutAndBackIn) {
+IN_PROC_BROWSER_TEST_F(ComponentCloudPolicyTest, SignOutAndBackIn) {
   // Read the initial policy.
   ExtensionTestMessageListener initial_policy_listener(kTestPolicyJSON, true);
   event_listener_->Reply("get-policy-Name");
