d7145e1f223584856773ddd881cad3b079778a63
Elly Fong-Jones <ellyjones@chromium.org>
2021-10-26 22:27:24 +0000

    sheriff: Revert "[ZT] Load SigningKeyPair in Background Thread"
    This reverts commit 354d58c602219b57484aaa18b3d4f2bdbacc89b9.
    
    Reason for revert: Crashes on Win7 bots
    
    Specifically, after this change:
    
    Received fatal exception EXCEPTION_ACCESS_VIOLATION
    [6188:9572:1026/142614.761:ERROR:chrome_browser_main_extra_parts_metrics.cc(235)] START: GetDefaultBrowser(). If you don't see the END: message, this is crbug.com/1216328.
    [6188:9572:1026/142614.824:ERROR:chrome_browser_main_extra_parts_metrics.cc(239)] END: GetDefaultBrowser()
    Backtrace:
    	enterprise_connectors::SigningKeyPair::Create [0x00000001431C1F94+120] (o:\chrome\browser\enterprise\connectors\device_trust\key_management\core\signing_key_pair.cc:34)
    	enterprise_connectors::DesktopAttestationService::TryLoadKeyPair [0x00000001486440FB+45] (o:\chrome\browser\enterprise\connectors\device_trust\attestation\desktop\desktop_attestation_service.cc:254)
    	base::TaskAnnotator::RunTask [0x0000000144722F86+358] (o:\base\task\common\task_annotator.cc:178)
    	base::internal::TaskTracker::RunSkipOnShutdown [0x0000000145E232B7+23] (o:\base\task\thread_pool\task_tracker.cc:661)
    	base::internal::TaskTracker::RunTask [0x0000000145E22DF8+760] (o:\base\task\thread_pool\task_tracker.cc:529)
    	base::test::TaskEnvironment::TestTaskTracker::RunTask [0x00000001447AA776+390] (o:\base\test\task_environment.cc:810)
    	base::internal::TaskTracker::RunAndPopNextTask [0x0000000145E22A15+533] (o:\base\task\thread_pool\task_tracker.cc:433)
    	base::internal::WorkerThread::RunWorker [0x0000000148604C0E+734] (o:\base\task\thread_pool\worker_thread.cc:379)
    	base::internal::WorkerThread::RunPooledWorker [0x0000000148604808+24] (o:\base\task\thread_pool\worker_thread.cc:267)
    	base::`anonymous namespace'::ThreadFunc [0x000000014475FC0D+253] (o:\base\threading\platform_thread_win.cc:113)
    	BaseThreadInitThunk [0x00000000779559CD+13]
    	RtlUserThreadStart [0x0000000077A8A561+33]
    
    Original change's description:
    > [ZT] Load SigningKeyPair in Background Thread
    >
    > - Ensure that the signing key pair is loaded on a background thread since it is being read from device storage (IO),
    > - Use a ThreadPool SequencedTaskRunner to ensure that the constructor's key loading actually completes before any attestation flow attempts,
    > - Added logic to retry loading the key pair during attestation flows if it is missing.
    >
    > Unit tests were also updated and added to improve the path coverage.
    >
    > Bug: b/203694133
    > Change-Id: I1dbad4caca561ce45a3ee7b0456faf20221a1117
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244799
    > Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#935107}
    
    Bug: b/203694133
    Change-Id: I69896f3d72693bab042c96eda0e320f1a918e8ff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245647
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Owners-Override: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935168}

--- chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service.cc
@@ -10,7 +10,6 @@
 #include "base/check.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/values.h"
@@ -28,8 +27,18 @@ namespace {
 // Size of nonce for challenge response.
 const size_t kChallengResponseNonceBytesSize = 32;
 
-// Verify challenge comes from Verify Access.
-bool ChallengeComesFromVerifiedAccess(
+}  // namespace
+
+DesktopAttestationService::DesktopAttestationService(
+    std::unique_ptr<KeyPersistenceDelegate> key_persistence_delegate)
+    : key_persistence_delegate_(std::move(key_persistence_delegate)) {
+  DCHECK(key_persistence_delegate_);
+  key_pair_ = SigningKeyPair::Create(key_persistence_delegate_.get());
+}
+
+DesktopAttestationService::~DesktopAttestationService() = default;
+
+bool DesktopAttestationService::ChallengeComesFromVerifiedAccess(
     const std::string& serialized_signed_data,
     const std::string& public_key_modulus_hex) {
   SignedData signed_challenge;
@@ -40,29 +49,19 @@ bool ChallengeComesFromVerifiedAccess(
       signed_challenge.signature());
 }
 
-}  // namespace
-
-DesktopAttestationService::DesktopAttestationService(
-    std::unique_ptr<KeyPersistenceDelegate> key_persistence_delegate)
-    : key_persistence_delegate_(std::move(key_persistence_delegate)),
-      background_task_runner_(base::ThreadPool::CreateSequencedTaskRunner(
-          {base::MayBlock(), base::TaskPriority::USER_BLOCKING,
-           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})) {
-  DCHECK(key_persistence_delegate_);
-
-  // Try to pre-load the key pair.
-  background_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&DesktopAttestationService::InitialLoadKey,
-                                weak_factory_.GetWeakPtr()));
+std::string DesktopAttestationService::ExportPublicKey() {
+  if (!key_pair_ || !key_pair_->key()) {
+    return std::string();
+  }
+  auto public_key_info = key_pair_->key()->GetSubjectPublicKeyInfo();
+  return std::string(public_key_info.begin(), public_key_info.end());
 }
-
-DesktopAttestationService::~DesktopAttestationService() = default;
 
 void DesktopAttestationService::BuildChallengeResponseForVAChallenge(
     const std::string& challenge,
     std::unique_ptr<DeviceTrustSignals> signals,
     AttestationCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(!ExportPublicKey().empty());
   DCHECK(signals);
   DCHECK(signals->has_device_id() && !signals->device_id().empty());
   DCHECK(signals->has_obfuscated_customer_id() &&
@@ -71,9 +70,8 @@ void DesktopAttestationService::BuildChallengeResponseForVAChallenge(
   AttestationCallback reply = base::BindOnce(
       &DesktopAttestationService::ParseChallengeResponseAndRunCallback,
       weak_factory_.GetWeakPtr(), challenge, std::move(callback));
-
-  background_task_runner_->PostTaskAndReplyWithResult(
-      FROM_HERE,
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
       base::BindOnce(
           &DesktopAttestationService::
               VerifyChallengeAndMaybeCreateChallengeResponse,
@@ -83,27 +81,11 @@ void DesktopAttestationService::BuildChallengeResponseForVAChallenge(
       std::move(reply));
 }
 
-std::string DesktopAttestationService::ExportPublicKey() {
-  if (!key_pair_ || !key_pair_->key()) {
-    return std::string();
-  }
-  auto public_key_info = key_pair_->key()->GetSubjectPublicKeyInfo();
-  return std::string(public_key_info.begin(), public_key_info.end());
-}
-
 std::string
 DesktopAttestationService::VerifyChallengeAndMaybeCreateChallengeResponse(
     const std::string& serialized_signed_data,
     const std::string& public_key_modulus_hex,
     std::unique_ptr<DeviceTrustSignals> signals) {
-  // Ensure that a signing key pair was loaded (else retry loading it), as
-  // otherwise the attestation flow cannot be completed.
-  if (!key_pair_ && !TryLoadKeyPair()) {
-    LOG(ERROR)
-        << "No signing key available to do the device trust attestation flow.";
-    return std::string();
-  }
-
   if (!ChallengeComesFromVerifiedAccess(serialized_signed_data,
                                         public_key_modulus_hex)) {
     LOG(ERROR) << "Challenge signature verification did not succeed.";
@@ -123,7 +105,6 @@ void DesktopAttestationService::ParseChallengeResponseAndRunCallback(
     const std::string& challenge,
     AttestationCallback callback,
     const std::string& challenge_response_proto) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (challenge_response_proto != std::string()) {
     // Return to callback (throttle with the challenge response) with empty
     // challenge response.
@@ -148,6 +129,7 @@ void DesktopAttestationService::SignEnterpriseChallenge(
   }
   KeyInfo key_info;
   // Fill `key_info` out for Chrome Browser.
+  // TODO(crbug.com/1241870): Remove public key from signals.
   key_info.set_key_type(CBCM);
   key_info.set_browser_instance_public_key(ExportPublicKey());
   key_info.set_device_id(signals->device_id());
@@ -212,10 +194,6 @@ bool DesktopAttestationService::EncryptEnterpriseKeyInfo(
 
 bool DesktopAttestationService::SignChallengeData(const std::string& data,
                                                   std::string* response) {
-  // Signing is an expensive operation that needs to be performed in a
-  // background thread.
-  DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
-
   SignedData signed_data;
   signed_data.set_data(data);
 
@@ -240,19 +218,6 @@ bool DesktopAttestationService::SignChallengeData(const std::string& data,
     return false;
   }
   return true;
-}
-
-void DesktopAttestationService::InitialLoadKey() {
-  // TODO(seblalancette): Add success/failure metrics.
-  TryLoadKeyPair();
-}
-
-bool DesktopAttestationService::TryLoadKeyPair() {
-  // Signing Key Pair creation involves IO, and therefore should always be done
-  // using the thread pool task runner.
-  DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
-  key_pair_ = SigningKeyPair::Create(key_persistence_delegate_.get());
-  return !ExportPublicKey().empty();
 }
 
 }  // namespace enterprise_connectors
--- chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service_unittest.cc
+++ chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service_unittest.cc
@@ -5,25 +5,17 @@
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/desktop/desktop_attestation_service.h"
 
 #include <memory>
-#include <string>
 
-#include "base/base64.h"
-#include "base/json/json_reader.h"
-#include "base/test/bind.h"
-#include "base/test/task_environment.h"
-#include "base/values.h"
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/common/attestation_utils.h"
 #include "chrome/browser/enterprise/connectors/device_trust/attestation/common/proto/device_trust_attestation_ca.pb.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/key_persistence_delegate_factory.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/mock_key_persistence_delegate.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/scoped_key_persistence_delegate_factory.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/signing_key_pair.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
-
-using testing::Return;
-
-namespace enterprise_connectors {
 
 namespace {
 
@@ -42,200 +34,65 @@ constexpr char kJsonChallenge[] =
     "u3W4CMboCswxIxNYRCGrIIVPElE3Yb4QS65mKrg=\""
     "}";
 
-constexpr char kDeviceId[] = "device-id";
-constexpr char kObfuscatedCustomerId[] = "customer-id";
-
-absl::optional<SignedData> ParseDataFromResponse(const std::string& response) {
-  absl::optional<base::Value> data = base::JSONReader::Read(
-      response, base::JSONParserOptions::JSON_ALLOW_TRAILING_COMMAS);
-
-  // If json is malformed or it doesn't include the needed field return
-  // an empty string.
-  if (!data || !data.value().FindPath("challengeResponse"))
-    return absl::nullopt;
-
-  std::string serialized_signed_data;
-  if (!base::Base64Decode(
-          data.value().FindPath("challengeResponse")->GetString(),
-          &serialized_signed_data)) {
-    return absl::nullopt;
-  }
-
-  SignedData signed_data;
-  if (!signed_data.ParseFromString(serialized_signed_data)) {
-    return absl::nullopt;
-  }
-  return signed_data;
-}
-
-enterprise_connectors::test::MockKeyPersistenceDelegate::KeyInfo
-CreateEmptyKey() {
-  return {enterprise_management::BrowserPublicKeyUploadRequest::
-              KEY_TRUST_LEVEL_UNSPECIFIED,
-          std::vector<uint8_t>()};
-}
-
 }  // namespace
+
+namespace enterprise_connectors {
 
 class DesktopAttestationServiceTest : public testing::Test {
  protected:
-  std::unique_ptr<DesktopAttestationService> CreateServiceWithLoadedKey() {
+  DesktopAttestationServiceTest() = default;
+
+  void SetUp() override {
+    testing::Test::SetUp();
+
     // ScopedKeyPersistenceDelegateFactory creates mocked persistence delegates
     // that already mimic the existence of a TPM key provider and stored key.
     auto mock_persistence_delegate =
         persistence_delegate_factory_.CreateMockedDelegate();
-    EXPECT_CALL(*mock_persistence_delegate, LoadKeyPair());
-    EXPECT_CALL(*mock_persistence_delegate, GetTpmBackedKeyProvider());
+    mock_persistence_delegate_ = mock_persistence_delegate.get();
+    EXPECT_CALL(*mock_persistence_delegate_, LoadKeyPair());
+    EXPECT_CALL(*mock_persistence_delegate_, GetTpmBackedKeyProvider());
 
-    auto service = std::make_unique<DesktopAttestationService>(
+    attestation_service_ = std::make_unique<DesktopAttestationService>(
         std::move(mock_persistence_delegate));
-    RunUntilIdle();
-    return service;
   }
 
-  std::unique_ptr<DeviceTrustSignals> CreateSignals() {
-    auto signals = std::make_unique<DeviceTrustSignals>();
-    signals->set_device_id(kDeviceId);
-    signals->set_obfuscated_customer_id(kObfuscatedCustomerId);
-    return signals;
-  }
-
-  void RunUntilIdle() { task_environment_.RunUntilIdle(); }
-
-  test::ScopedKeyPersistenceDelegateFactory* persistence_delegate_factory() {
-    return &persistence_delegate_factory_;
+  DesktopAttestationService* attestation_service() {
+    return attestation_service_.get();
   }
 
  private:
-  base::test::TaskEnvironment task_environment_{
-      base::test::TaskEnvironment::TimeSource::MOCK_TIME,
-      base::test::TaskEnvironment::ThreadPoolExecutionMode::QUEUED};
+  base::test::TaskEnvironment task_environment_;
+  std::unique_ptr<DesktopAttestationService> attestation_service_;
   test::ScopedKeyPersistenceDelegateFactory persistence_delegate_factory_;
+  test::MockKeyPersistenceDelegate* mock_persistence_delegate_;
 };
 
-// Tests the challenge-response building flow with the signing key pair being
-// available at service construction time.
-TEST_F(DesktopAttestationServiceTest, BuildChallengeResponse_InitialKeyLoaded) {
+TEST_F(DesktopAttestationServiceTest, BuildChallengeResponse) {
+  SignEnterpriseChallengeRequest request;
+  SignEnterpriseChallengeReply result;
+
   // TODO(crbug.com/1208881): Add signals and validate they effectively get
   // added to the signed data.
-  auto signals = CreateSignals();
+  auto signals = std::make_unique<DeviceTrustSignals>();
 
-  std::string captured_response;
-  auto callback = base::BindLambdaForTesting(
-      [&captured_response](const std::string& response) {
-        captured_response = response;
-      });
+  // Get the challenge from the SignedData json and create request.
+  request.set_challenge(JsonChallengeToProtobufChallenge(kJsonChallenge));
+  // If challenge is equal to empty string, then
+  // `JsonChallengeToProtobufChallenge()` failed.
+  EXPECT_NE(request.challenge(), std::string());
 
-  auto attestation_service = CreateServiceWithLoadedKey();
-  attestation_service->BuildChallengeResponseForVAChallenge(
-      kJsonChallenge, std::move(signals), std::move(callback));
-  RunUntilIdle();
+  attestation_service()->SignEnterpriseChallenge(request, std::move(signals),
+                                                 &result);
+  // If challenge is equal to empty string, then
+  // `JsonChallengeToProtobufChallenge()` failed.
+  EXPECT_NE(result.challenge_response(), std::string());
 
-  ASSERT_NE(std::string(), captured_response);
+  SignedData signed_data;
+  EXPECT_TRUE(signed_data.ParseFromString(result.challenge_response()));
 
-  auto signed_data = ParseDataFromResponse(captured_response);
-  ASSERT_TRUE(signed_data);
-  EXPECT_NE(signed_data->data(), std::string());
-  EXPECT_NE(signed_data->signature(), std::string());
-}
-
-// Tests the challenge-response building flow with the signing key pair not
-// being available at service construction time, but loadable during the
-// attestation flow.
-TEST_F(DesktopAttestationServiceTest, BuildChallengeResponse_DelayKeyLoaded) {
-  auto signals = CreateSignals();
-
-  std::string captured_response;
-  auto callback = base::BindLambdaForTesting(
-      [&captured_response](const std::string& response) {
-        captured_response = response;
-      });
-
-  // ScopedKeyPersistenceDelegateFactory creates mocked persistence delegates
-  // that already mimic the existence of a TPM key provider and stored key.
-  auto mock_persistence_delegate =
-      persistence_delegate_factory()->CreateMockedDelegate();
-  auto* mock_persistence_delegate_ptr = mock_persistence_delegate.get();
-  EXPECT_CALL(*mock_persistence_delegate_ptr, LoadKeyPair())
-      .WillOnce(Return(CreateEmptyKey()));
-
-  auto attestation_service = std::make_unique<DesktopAttestationService>(
-      std::move(mock_persistence_delegate));
-  RunUntilIdle();
-
-  // Now update the mock to expect a key actually getting loaded. The
-  // persistence mock created by the factory already has some default TPM key
-  // values in it, so simply expecting to get called once is good enough.
-  EXPECT_CALL(*mock_persistence_delegate_ptr, LoadKeyPair());
-  EXPECT_CALL(*mock_persistence_delegate_ptr, GetTpmBackedKeyProvider());
-
-  attestation_service->BuildChallengeResponseForVAChallenge(
-      kJsonChallenge, std::move(signals), std::move(callback));
-  RunUntilIdle();
-
-  ASSERT_NE(std::string(), captured_response);
-
-  auto signed_data = ParseDataFromResponse(captured_response);
-  ASSERT_TRUE(signed_data);
-  EXPECT_NE(signed_data->data(), std::string());
-  EXPECT_NE(signed_data->signature(), std::string());
-}
-
-// Tests the case where no key ever gets loaded, which results in the challenge
-// response validation failing.
-TEST_F(DesktopAttestationServiceTest, BuildChallengeResponse_NoKey_Fails) {
-  auto signals = CreateSignals();
-
-  bool callback_called;
-  std::string captured_response;
-  auto callback = base::BindLambdaForTesting(
-      [&callback_called, &captured_response](const std::string& response) {
-        callback_called = true;
-        captured_response = response;
-      });
-
-  // Mimic that no key ever gets loaded.
-  auto mock_persistence_delegate =
-      persistence_delegate_factory()->CreateMockedDelegate();
-  EXPECT_CALL(*mock_persistence_delegate, LoadKeyPair())
-      .Times(2)
-      .WillRepeatedly(Return(CreateEmptyKey()));
-
-  auto attestation_service = std::make_unique<DesktopAttestationService>(
-      std::move(mock_persistence_delegate));
-  RunUntilIdle();
-
-  attestation_service->BuildChallengeResponseForVAChallenge(
-      kJsonChallenge, std::move(signals), std::move(callback));
-  RunUntilIdle();
-
-  // Attestation flow failure is represented as getting an empty string as
-  // challenge response.
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(std::string(), captured_response);
-}
-
-// Tests the case where a valid key is loaded but a bad challenge is given.
-TEST_F(DesktopAttestationServiceTest, BuildChallengeResponse_BadChallenge) {
-  auto signals = CreateSignals();
-
-  bool callback_called;
-  std::string captured_response;
-  auto callback = base::BindLambdaForTesting(
-      [&callback_called, &captured_response](const std::string& response) {
-        callback_called = true;
-        captured_response = response;
-      });
-
-  auto attestation_service = CreateServiceWithLoadedKey();
-  attestation_service->BuildChallengeResponseForVAChallenge(
-      "A bad challenge for sure", std::move(signals), std::move(callback));
-  RunUntilIdle();
-
-  // Attestation flow failure is represented as getting an empty string as
-  // challenge response.
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(std::string(), captured_response);
+  EXPECT_NE(signed_data.data(), std::string());
+  EXPECT_NE(signed_data.signature(), std::string());
 }
 
 }  // namespace enterprise_connectors
