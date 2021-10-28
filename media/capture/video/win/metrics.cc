5c4b4e4b5ed9ebc97ac2b64ba064030f833da14f
Frank Enderman <90335574+FrankEnderman@users.noreply.github.com>
2021-10-28 21:10:58 +0530

    further increased width

--- media/capture/video/win/metrics.cc
+++ media/capture/video/win/metrics.cc
@@ -28,7 +28,7 @@ std::string VideoCaptureWinBackendEnumToString(
 }  // anonymous namespace
 
 bool IsHighResolution(const VideoCaptureFormat& format) {
-  return format.frame_size.width() > 1940;
+  return format.frame_size.width() > 1946;
 }
 
 void LogVideoCaptureWinBackendUsed(VideoCaptureWinBackendUsed value) {
