7586330657150f9d21fdb84d430cdd2250a446d5
Nancy Wang <nancylingwang@chromium.org>
2021-10-29 04:00:57 +0000

    Implement ArcApps::LoadIcon for the non-mojom publisher interface.
    BUG=1253250
    
    Change-Id: Id8181875e41abd01410bc1a8daf0e502ca93e1aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251803
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936259}

--- chrome/browser/apps/app_service/publishers/arc_apps.cc
+++ chrome/browser/apps/app_service/publishers/arc_apps.cc
@@ -647,8 +647,39 @@ void ArcApps::LoadIcon(const std::string& app_id,
                        int32_t size_hint_in_dip,
                        bool allow_placeholder_icon,
                        apps::LoadIconCallback callback) {
-  // TODO(crbug.com/1253250): Implement icon loading.
-  NOTIMPLEMENTED();
+  if (icon_type == IconType::kUnknown) {
+    std::move(callback).Run(std::make_unique<IconValue>());
+    return;
+  }
+  IconEffects icon_effects = static_cast<IconEffects>(icon_key.icon_effects);
+
+  // Treat the Play Store as a special case, loading an icon defined by a
+  // resource instead of asking the Android VM (or the cache of previous
+  // responses from the Android VM). Presumably this is for bootstrapping:
+  // the Play Store icon (the UI for enabling and installing Android apps)
+  // should be showable even before the user has installed their first
+  // Android app and before bringing up an Android VM for the first time.
+  if (app_id == arc::kPlayStoreAppId) {
+    LoadPlayStoreIcon(icon_type, size_hint_in_dip, icon_effects,
+                      std::move(callback));
+  } else {
+    const ArcAppListPrefs* arc_prefs = ArcAppListPrefs::Get(profile_);
+    DCHECK(arc_prefs);
+
+    // If the app has been removed, immediately terminate the icon request since
+    // it can't possibly succeed.
+    std::unique_ptr<ArcAppListPrefs::AppInfo> app_info =
+        arc_prefs->GetApp(app_id);
+    if (!app_info) {
+      std::move(callback).Run(std::make_unique<IconValue>());
+      return;
+    }
+
+    arc_icon_once_loader_.LoadIcon(
+        app_id, size_hint_in_dip, icon_type,
+        base::BindOnce(&OnArcAppIconCompletelyLoaded, icon_type,
+                       size_hint_in_dip, icon_effects, std::move(callback)));
+  }
 }
 
 void ArcApps::Connect(
