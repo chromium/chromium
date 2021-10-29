792467f29adbe1434b92487cd32c3ca499f1ae18
Adrian Taylor <adetaylor@chromium.org>
2021-10-29 20:18:30 +0000

    [rust] Add cargo_crate template.
    cargo crates are third party packages of Rust code distributed
    using from crates.io using cargo tooling. To a large degree,
    they're a single compilation unit of Rust code producing
    an executable, rlib or proc_macro, and thus correspond closely
    to our existing 'rust_target' gn template.
    
    However, cargo crates may have a build script, being some Rust
    code run at build time.
    
    Build scripts may do arbitrary things. Commonly, they do one
    of four things:
    * do rustc version checks, and refuse to build on older versions
    * emit --cfg flags to be passed to the Rust compiler depending
      on the rustc version and/or the target platform
    * dynamically generate .rs code
    * link in some C++ code
    
    Such build scripts rely on environment variables set by
    cargo, and can emit some directives on stdout which cargo then
    respects (e.g. by arranging to pass --cfg flags to rustc).
    
    Notably, build scripts cannot do these two things:
    * they cannot alter the dependencies of the crate.
    * they cannot detect arbitrary facts about the target
      environment other than via the preset environment variables,
      since in any cross-compilation environment they won't actually
      be running on the target. (Obviously there are no guarantees,
      but in practice crates are not seen to make this mistake
      often).
    
    This CL arranges for limited support for build scripts.
    It supports --cfg flags. It can easily be extended in future
    to support generation of .rs code.
    
    Build scripts are not necessarily desirable in a build system
    like gn for two reasons:
    * running arbitrary code on build machines requires a thorough
      audit of each build script and its dependencies;
    * no downstream steps can run until the build script is linked
      and run, which stalls the pipeline in a massively parallel
      build system like gn.
    
    However, the alternative of not supporting build scripts is
    worse:
    * we would have to fork a large number of third party crates
    * we would have to manually craft build rules corresponding
      to the static output of the build script
    * we would likely have to adjust those rules each time we
      changed the rustc version.
    
    See: 
    https: //docs.google.com/document/d/1QS96GAXZ-Q8rGujRoPwBbuJ8PnLp61hAl0nGgyARgzk/edit#heading=h.u3hxshup6xm8
    Change-Id: Iec4b99d7adc256d4e41b8afb1494e76b9244cfc3
    Bug: 1069271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227461
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936530}

--- build/rust/tests/test_rust_exe/main.rs
+++ build/rust/tests/test_rust_exe/main.rs
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 use test_mixed_source_set_rs::say_hello_from_a_cpp_callback_from_rust;
+use test_rlib_crate::say_hello_from_crate;
 use test_rust_source_set::say_hello;
 use test_rust_source_set_non_standard_arrangement::say_foo;
 
@@ -10,4 +11,5 @@ fn main() {
     say_hello();
     say_foo();
     say_hello_from_a_cpp_callback_from_rust();
+    say_hello_from_crate();
 }
