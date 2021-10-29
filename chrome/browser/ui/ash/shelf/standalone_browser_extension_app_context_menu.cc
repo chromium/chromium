56cf15fa3b759e3cf72133f0eb60bd5ad6a4d2dd
Erik Chen <erikchen@chromium.org>
2021-10-29 17:31:53 +0000

    lacros: Hide some chrome app context menu items.
    Context menus items for chrome apps will sometimes be hidden because of
    policy or manifest. This CL hooks up these settings for lacros chrome
    apps.
    
    Note that: what an app can be shown in management comes from extension
    manifest, which must come from Lacros. The two remaining policies
    technically could come from either Lacros or Ash as user policy is
    duplicated between the two. We choose to propagate allow_uninstall from
    Lacros as that semanticaly makes sense. Likewise for propagating
    allow_pin_unpin from ash,
    
    Bug: 1225848
    Change-Id: Ib602ebb1f8f730e03cf562b63787cff6dfdfd858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3230179
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936452}

--- chrome/browser/ui/ash/shelf/standalone_browser_extension_app_context_menu.cc
+++ chrome/browser/ui/ash/shelf/standalone_browser_extension_app_context_menu.cc
@@ -17,7 +17,9 @@
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
+#include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
+#include "chrome/browser/ui/ash/shelf/chrome_shelf_prefs.h"
 #include "chrome/browser/ui/ash/shelf/standalone_browser_extension_app_shelf_item_controller.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/webui/settings/chromeos/app_management/app_management_uma.h"
@@ -122,10 +124,25 @@ void StandaloneBrowserExtensionAppContextMenu::ExecuteCommand(int command_id,
 
 void StandaloneBrowserExtensionAppContextMenu::OnGetMenuModel(
     GetMenuModelCallback callback) {
-  // TODO(https://crbug.com/1225848): These values should come from Lacros.
-  bool lacros_allows_pin_unpin = true;
-  bool allows_uninstall = true;
-  bool allow_app_info = true;
+  bool allow_uninstall = false;
+  bool allow_app_info = false;
+  apps::AppServiceProxyChromeOs* proxy =
+      apps::AppServiceProxyFactory::GetForProfile(
+          ProfileManager::GetPrimaryUserProfile());
+  proxy->AppRegistryCache().ForOneApp(
+      app_id_,
+      [&allow_app_info, &allow_uninstall](const apps::AppUpdate& update) {
+        allow_app_info =
+            update.ShowInManagement() == apps::mojom::OptionalBool::kTrue;
+        allow_uninstall =
+            update.AllowUninstall() == apps::mojom::OptionalBool::kTrue;
+      });
+
+  std::string sync_id =
+      ChromeShelfController::instance()->shelf_prefs()->GetSyncId(app_id_);
+  bool allow_pin_unpin =
+      GetPinnableForAppID(sync_id, ProfileManager::GetPrimaryUserProfile()) ==
+      AppListControllerDelegate::PIN_EDITABLE;
 
   auto menu_model = std::make_unique<ui::SimpleMenuModel>(this);
 
@@ -151,7 +168,7 @@ void StandaloneBrowserExtensionAppContextMenu::OnGetMenuModel(
     }
   }
 
-  if (lacros_allows_pin_unpin) {
+  if (allow_pin_unpin) {
     // This context menu is used by both the shelf and app list. We choose to
     // use the app list IDS string since it's clearer.
     bool currently_pinned = item_in_shelf && model->IsAppPinned(app_id_);
@@ -179,7 +196,7 @@ void StandaloneBrowserExtensionAppContextMenu::OnGetMenuModel(
   }
 
   // Add an uninstall menu item.
-  if (allows_uninstall) {
+  if (allow_uninstall) {
     menu_model->AddItemWithStringIdAndIcon(
         ash::UNINSTALL, IDS_APP_LIST_UNINSTALL_ITEM,
         GetMenuItemIcon(views::kUninstallIcon));
