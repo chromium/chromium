44d5d7903a657f516a2c1d9c396b6fa823b53968
dpapad <dpapad@chromium.org>
2021-10-29 08:35:58 +0000

    ChromeOS: Fork pre-JS modules fake_chrome_event.js.
    This pre-JS modlues file is only used in SelectToSpeak a11y tests and is
    blocking migrating WebUI tests to TypeScript.
    
    Bug: 1263610
    Change-Id: Ic4158c41d1024cd38aac03da327d0d2bb4b21434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248441
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936311}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_prefs_test.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_prefs_test.js
@@ -4,7 +4,7 @@
 
 GEN_INCLUDE(['select_to_speak_e2e_test_base.js']);
 GEN_INCLUDE(['../../../../../../ui/webui/resources/js/cr.js']);
-GEN_INCLUDE(['../../../../../test/data/webui/fake_chrome_event.js']);
+GEN_INCLUDE(['fake_chrome_event.js']);
 GEN_INCLUDE(['fake_settings_private.js']);
 GEN_INCLUDE(['mock_storage.js']);
 
