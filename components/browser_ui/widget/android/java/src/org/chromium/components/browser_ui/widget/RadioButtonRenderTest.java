b38219f1894db76cd535ecf0f995209b4b93ea33
Sky Malice <skym@chromium.org>
2021-10-29 22:36:59 +0000

    Delete DummyUiComponentsActivityTestCase
    This mechanism is obsolete now that material color attributes have been
    moved into a base theme that DummyUiActivity shares.
    
    Bug: 1260396
    Change-Id: I577240abcbd3475f41fa1db01cd5825c0da9ca17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253513
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#936587}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonRenderTest.java
@@ -22,9 +22,9 @@ import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.Feature;
-import org.chromium.components.browser_ui.test.DummyUiComponentsActivityTestCase;
 import org.chromium.components.browser_ui.widget.test.R;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.ui.test.util.DummyUiActivityTestCase;
 import org.chromium.ui.test.util.NightModeTestUtils;
 import org.chromium.ui.test.util.RenderTestRule;
 
@@ -37,7 +37,7 @@ import java.util.List;
  */
 @RunWith(ParameterizedRunner.class)
 @UseRunnerDelegate(BaseJUnit4RunnerDelegate.class)
-public class RadioButtonRenderTest extends DummyUiComponentsActivityTestCase {
+public class RadioButtonRenderTest extends DummyUiActivityTestCase {
     @ClassParameter
     private static List<ParameterSet> sClassParams =
             new NightModeTestUtils.NightModeParams().getParameters();
