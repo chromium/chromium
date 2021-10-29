b95b96fb4c807c1f8f6459013c66aaaab7619e2c
Gustaf Ullberg <gustaf@chromium.org>
2021-10-29 14:48:31 +0000

    Mixing Graph for Chrome-wide echo cancellation.
    Builds a graph of audio converters to combine all output streams
    into a single output. The graph is constructed to minimize the
    use of resampling (a computationally complex operation), while still
    allowing for adding and removing inputs on the fly.
    
    Bug: 1224845
    Change-Id: I61f5c99b81d6539b3eed6e2709b2a25370bf8ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226390
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Gustaf Ullberg <gustaf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936384}

--- services/audio/mixing_graph.cc
+++ services/audio/mixing_graph.cc
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 #include "services/audio/mixing_graph.h"
+#include "services/audio/mixing_graph_impl.h"
 
-#include "base/notreached.h"
 namespace audio {
 
 // static
@@ -12,8 +12,7 @@ std::unique_ptr<MixingGraph> MixingGraph::Create(
     const media::AudioParameters& output_params,
     OnMoreDataCallback on_more_data_cb,
     OnErrorCallback on_error_cb) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return std::make_unique<MixingGraphImpl>(
+      output_params, std::move(on_more_data_cb), std::move(on_error_cb));
 }
-
 }  // namespace audio
