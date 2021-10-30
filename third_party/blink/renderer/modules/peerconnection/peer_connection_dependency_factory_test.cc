a7b16d9ca05c71f959cf039ec7b7af6ab9eece0f
Henrik Boström <hbos@chromium.org>
2021-10-30 07:46:21 +0000

    PeerConnectionDependencyFactory: Use a context-wide metronome provider.
    Part of go/metronome-task-queue-implementation.
    
    PeerConnectionDependencyFactory manages the lifetime of the WebRTC
    MetronomeSource. In this CL, the PeerConnectionDependencyFactory is
    updated to make use of a context-wide MetronomeProvider. This will allow
    anyone with an ExecutionContext to create a WebRtcTimer (or make use of
    the metronome in other ways).
    
    In order for the context-wide MetronomeProvider to be accessible in both
    core/ and modules/, the ownership of the MetronomeProvider is given to
    ExecutionContextMetronomeProvider in core/. This is because modules/ can
    depend on core/ but core/ cannot depend on modules/. (HTMLMediaElement
    which lives in core/ will want to be able to create WebRtcTimers.)
    
    Bug: chromium:1253787
    Change-Id: I2b475d7144c9d02772b27ad6f2abe46c917a9973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233100
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936678}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory_test.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory_test.cc
@@ -8,38 +8,39 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/core/peerconnection/execution_context_metronome_provider.h"
 #include "third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h"
 #include "third_party/blink/renderer/modules/peerconnection/mock_rtc_peer_connection_handler_client.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h"
+#include "third_party/webrtc_overrides/metronome_provider.h"
 #include "third_party/webrtc_overrides/metronome_task_queue_factory.h"
 
 namespace blink {
 
 namespace {
 
-class UseMetronomeSourceListenerImpl : public UseMetronomeSourceListener {
+class FakeMetronomeProviderListener : public MetronomeProviderListener {
  public:
   size_t start_count() const { return start_count_; }
   size_t stop_count() const { return stop_count_; }
 
-  void OnCanUseMetronome(
-      scoped_refptr<MetronomeSource> metronome_source) override {
-    DCHECK(!metronome_source_);
-    metronome_source_ = metronome_source;
+  // MetronomeProviderListener:
+  void OnStartUsingMetronome(
+      scoped_refptr<MetronomeSource> metronome) override {
+    DCHECK(!metronome_);
+    metronome_ = metronome;
     ++start_count_;
   }
-
-  void OnStopUsingMetronome(
-      scoped_refptr<MetronomeSource> metronome_source) override {
-    DCHECK(metronome_source_ == metronome_source);
-    metronome_source_ = nullptr;
+  void OnStopUsingMetronome() override {
+    DCHECK(metronome_);
+    metronome_ = nullptr;
     ++stop_count_;
   }
 
  private:
+  scoped_refptr<MetronomeSource> metronome_;
   size_t start_count_ = 0;
   size_t stop_count_ = 0;
-  scoped_refptr<MetronomeSource> metronome_source_;
 };
 
 }  // namespace
@@ -109,14 +110,20 @@ TEST_F(PeerConnectionDependencyFactoryTest,
   V8TestingScope scope;
   EnsureDependencyFactory(*scope.GetExecutionContext());
 
-  UseMetronomeSourceListenerImpl use_metronome_listener;
-  dependency_factory_->AddUseMetronomeSourceListener(&use_metronome_listener);
+  FakeMetronomeProviderListener fake_metronome_listener;
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->AddListener(&fake_metronome_listener);
 
   // Start using WebRTC. Nothing should happen to the metronome use counters.
   std::unique_ptr<RTCPeerConnectionHandler> pc1 =
       CreateRTCPeerConnectionHandler();
-  EXPECT_EQ(use_metronome_listener.start_count(), 0u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 0u);
+
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->RemoveListener(&fake_metronome_listener);
 }
 
 TEST_F(PeerConnectionDependencyFactoryTest,
@@ -126,59 +133,65 @@ TEST_F(PeerConnectionDependencyFactoryTest,
   V8TestingScope scope;
   EnsureDependencyFactory(*scope.GetExecutionContext());
 
-  UseMetronomeSourceListenerImpl use_metronome_listener;
-  dependency_factory_->AddUseMetronomeSourceListener(&use_metronome_listener);
+  FakeMetronomeProviderListener fake_metronome_listener;
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->AddListener(&fake_metronome_listener);
 
-  EXPECT_EQ(use_metronome_listener.start_count(), 0u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 0u);
 
   // Create a peer connection, the start count should increment.
   std::unique_ptr<RTCPeerConnectionHandler> pc1 =
       CreateRTCPeerConnectionHandler();
-  EXPECT_EQ(use_metronome_listener.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 0u);
 
   // Create another peer connection. The start count remains the same.
   std::unique_ptr<RTCPeerConnectionHandler> pc2 =
       CreateRTCPeerConnectionHandler();
-  EXPECT_EQ(use_metronome_listener.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 0u);
 
   // When the metronome is already in use, adding another listener should
   // immediately inform it that there is a metronome that may be used.
-  UseMetronomeSourceListenerImpl use_metronome_listener2;
-  dependency_factory_->AddUseMetronomeSourceListener(&use_metronome_listener2);
-  EXPECT_EQ(use_metronome_listener2.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener2.stop_count(), 0u);
-  dependency_factory_->RemoveUseMetronomeSourceListener(
-      &use_metronome_listener2);
+  FakeMetronomeProviderListener fake_metronome_listener2;
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->AddListener(&fake_metronome_listener2);
+  EXPECT_EQ(fake_metronome_listener2.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener2.stop_count(), 0u);
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->RemoveListener(&fake_metronome_listener2);
 
   // Stop a peer connection. The stop count does not increase because there is
   // one more non-stopped peer connection.
   pc1->Stop();
-  EXPECT_EQ(use_metronome_listener.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 0u);
 
   // The stop count increases when there are no more peer connections.
   pc2->Stop();
-  EXPECT_EQ(use_metronome_listener.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 1u);
 
   // The previously removed listener was not informed.
-  EXPECT_EQ(use_metronome_listener2.start_count(), 1u);
-  EXPECT_EQ(use_metronome_listener2.stop_count(), 0u);
+  EXPECT_EQ(fake_metronome_listener2.start_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener2.stop_count(), 0u);
 
   // Creating another peer connection should start it again.
   std::unique_ptr<RTCPeerConnectionHandler> pc3 =
       CreateRTCPeerConnectionHandler();
-  EXPECT_EQ(use_metronome_listener.start_count(), 2u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 1u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 2u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 1u);
   pc3->Stop();
-  EXPECT_EQ(use_metronome_listener.start_count(), 2u);
-  EXPECT_EQ(use_metronome_listener.stop_count(), 2u);
+  EXPECT_EQ(fake_metronome_listener.start_count(), 2u);
+  EXPECT_EQ(fake_metronome_listener.stop_count(), 2u);
 
-  dependency_factory_->RemoveUseMetronomeSourceListener(
-      &use_metronome_listener);
+  ExecutionContextMetronomeProvider::From(*scope.GetExecutionContext())
+      .metronome_provider()
+      ->RemoveListener(&fake_metronome_listener);
 }
 
 }  // namespace blink
