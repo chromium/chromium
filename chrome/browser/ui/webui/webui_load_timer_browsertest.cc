9c43c7c181831173a17831d97107b5b8fed38562
Yeunjoo Choi <ychoi@igalia.com>
2021-11-01 04:14:25 +0000

    [mparch] Add a prerendering test for WebuiLoadTimer
    This CL is part of MPArch tasks and add a browsertest to ensure
    WebuiLoadTimer doesn't report page load times on prerender navigation.
    
    Bug: 1218946
    Change-Id: Ief05568efdde229660de94a55f330cc5db92f72e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247402
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#936808}

--- chrome/browser/ui/webui/webui_load_timer_browsertest.cc
+++ chrome/browser/ui/webui/webui_load_timer_browsertest.cc
@@ -12,6 +12,7 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/prerender_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -36,4 +37,50 @@ IN_PROC_BROWSER_TEST_F(WebuiLoadTimerTest, Timers) {
 
   histogram_tester.ExpectTotalCount(kDocumentInitialLoadUmaId, 1);
   histogram_tester.ExpectTotalCount(kDocumentLoadCompletedUmaId, 1);
+}
+
+class WebuiLoadTimerPrerenderTest : public WebuiLoadTimerTest {
+ public:
+  WebuiLoadTimerPrerenderTest()
+      : prerender_helper_(
+            base::BindRepeating(&WebuiLoadTimerPrerenderTest::web_contents,
+                                base::Unretained(this))) {}
+
+  void SetUp() override {
+    prerender_helper_.SetUp(embedded_test_server());
+    ASSERT_TRUE(embedded_test_server()->Start());
+    InProcessBrowserTest::SetUp();
+  }
+
+  content::WebContents* web_contents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+
+ protected:
+  content::test::PrerenderTestHelper prerender_helper_;
+};
+
+IN_PROC_BROWSER_TEST_F(WebuiLoadTimerPrerenderTest,
+                       TimersOnPrerenderNavigation) {
+  base::HistogramTester histogram_tester;
+  WebuiLoadTimer timer(web_contents(), kDocumentInitialLoadUmaId,
+                       kDocumentLoadCompletedUmaId);
+
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), embedded_test_server()->GetURL("/simple.html")));
+  histogram_tester.ExpectTotalCount(kDocumentInitialLoadUmaId, 1);
+  histogram_tester.ExpectTotalCount(kDocumentLoadCompletedUmaId, 1);
+
+  // Start a prerender.
+  GURL prerender_url = embedded_test_server()->GetURL("/title1.html");
+  prerender_helper_.AddPrerender(prerender_url);
+  // Ensure prerender navigation doesn't update WebuiLoadTimer.
+  histogram_tester.ExpectTotalCount(kDocumentInitialLoadUmaId, 1);
+  histogram_tester.ExpectTotalCount(kDocumentLoadCompletedUmaId, 1);
+
+  // Activate a prerendered page.
+  prerender_helper_.NavigatePrimaryPage(prerender_url);
+  // Ensure WebuiLoadTimer reports page load times.
+  histogram_tester.ExpectTotalCount(kDocumentInitialLoadUmaId, 2);
+  histogram_tester.ExpectTotalCount(kDocumentLoadCompletedUmaId, 2);
 }
