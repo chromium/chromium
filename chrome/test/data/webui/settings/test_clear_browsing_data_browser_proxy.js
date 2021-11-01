4c1b9278b086365f04b90d4f802936966afca7eb
dpapad <dpapad@chromium.org>
2021-11-01 19:46:34 +0000

    Settings: Pass generated tests to ts_library().
    Starting with tests that are already generated (preprocessed), and
    planning to extend to more test files in follow up CLs.
    
    This is in preparation of migrating tests to TypeScript.
    
    Bug: 1263610
    Change-Id: I4cc9bc316f10122a2d3d166edeea75fdb7b3d77a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246166
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937000}

--- chrome/test/data/webui/settings/test_clear_browsing_data_browser_proxy.js
+++ chrome/test/data/webui/settings/test_clear_browsing_data_browser_proxy.js
@@ -5,7 +5,7 @@
 // clang-format off
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 
-import {TestBrowserProxy} from '../test_browser_proxy.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 // clang-format on
 
