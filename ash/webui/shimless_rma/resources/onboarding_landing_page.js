c9a3def2fe8b45ec22b3332e7c92afe062ad4184
Gavin Dodd <gavindodd@google.com>
2021-11-01 18:05:43 +0000

    shimless: Wait for verification on welcome screen
    Screenshots:
     - Waiting https://screenshot.googleplex.com/AqFmaaDeE6Q7VMb.png
     - Success https://screenshot.googleplex.com/6Cd5kHHcG3PmveG.png
     - Fail https://screenshot.googleplex.com/5nEG3qw6X62CgCZ.png
    
    Wait for the hardware verification result before enabling the 'Next'
    button, and display the appropriate message for validation state
    
            Run closure compile for shimless rma and tests
            Run browser tests for Shimless*
    
    Bug: 1198187
    Test: Run local and on device and check the welcome page behavior
    Change-Id: Iace9e62d09aaff1363c563eefe42c5511d42c506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252252
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936954}

--- ash/webui/shimless_rma/resources/onboarding_landing_page.js
+++ ash/webui/shimless_rma/resources/onboarding_landing_page.js
@@ -11,7 +11,7 @@ import './icons.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getShimlessRmaService} from './mojo_interface_provider.js';
-import {ShimlessRmaServiceInterface, StateResult} from './shimless_rma_types.js';
+import {HardwareVerificationStatusObserverInterface, HardwareVerificationStatusObserverReceiver, ShimlessRmaServiceInterface, StateResult} from './shimless_rma_types.js';
 
 /**
  * @fileoverview
@@ -27,24 +27,103 @@ export class OnboardingLandingPage extends PolymerElement {
     return html`{__html_template__}`;
   }
 
+  static get properties() {
+    return {
+      /** @protected */
+      verificationMessage_: {
+        type: String,
+        // TODO(gavindodd): i18n string
+        value: 'Validating components...',
+      },
+
+      /**
+       * Error code from rmad service, not i18n.
+       * @protected
+       */
+      errorMessage_: {
+        type: String,
+        value: '',
+      },
+
+      /** @protected */
+      verificationInProgress_: {
+        type: Boolean,
+        value: true,
+      },
+
+      /**
+       * isCompliant_ is not valid until verificationInProgress_ is false.
+       * @protected
+       */
+      isCompliant_: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+
   constructor() {
     super();
     /** @private {ShimlessRmaServiceInterface} */
     this.shimlessRmaService_ = getShimlessRmaService();
+    /** @protected {?HardwareVerificationStatusObserverReceiver} */
+    this.hwVerificationObserverReceiver_ =
+        new HardwareVerificationStatusObserverReceiver(
+            /**
+             * @type {!HardwareVerificationStatusObserverInterface}
+             */
+            (this));
+
+    this.shimlessRmaService_.observeHardwareVerificationStatus(
+        this.hwVerificationObserverReceiver_.$.bindNewPipeAndPassRemote());
   }
 
   /** @override */
   ready() {
     super.ready();
-    this.dispatchEvent(new CustomEvent(
-        'disable-next-button',
-        {bubbles: true, composed: true, detail: false},
-        ));
   }
 
   /** @return {!Promise<StateResult>} */
   onNextButtonClick() {
-    return this.shimlessRmaService_.beginFinalization();
+    if (!this.verificationInProgress_) {
+      return this.shimlessRmaService_.beginFinalization();
+    }
+
+    return Promise.reject(new Error('Hardware verification is not complete.'));
+  }
+
+  /**
+   * @protected
+   * @return {string}
+   */
+  getVerificationIcon_() {
+    // TODO(gavindodd): Replace with check and warning icons.
+    return this.isCompliant_ ? 'shimless-icon:wifi' : 'shimless-icon:info';
+  }
+
+  /**
+   * Implements
+   * HardwareVerificationStatusObserver.onHardwareVerificationResult()
+   * @param {boolean} isCompliant
+   * @param {string} errorMessage
+   */
+  onHardwareVerificationResult(isCompliant, errorMessage) {
+    // TODO(gavindodd): i18n strings
+    if (isCompliant) {
+      this.verificationMessage_ = 'Components are installed correctly.';
+    } else {
+      this.verificationMessage_ =
+          'Components could not be validated. If you are certain you are ' +
+          'using a qualified component updating Chrome OS may resolve this ' +
+          'issue.';
+      this.errorMessage_ = errorMessage;
+    }
+    this.isCompliant_ = isCompliant;
+    this.verificationInProgress_ = false;
+    this.dispatchEvent(new CustomEvent(
+        'disable-next-button',
+        {bubbles: true, composed: true, detail: false},
+        ));
   }
 }
 
