d7db31b5c0a97180a74ea6dcdfb722ba7a31d6dd
Daniel Vogelheim <vogelheim@chromium.org>
2021-11-02 12:39:53 +0000

    [Sanitizer] Fix edge cases with <template> element.
    If .sanitizeFor is called, the newly created root needs to have the
    special processing done, but doesn't need to be checked against
    the configuration.
    
    Bug: 1257558
    Spec: https://wicg.github.io/sanitizer-api/#sanitizefor
    Spec: https://wicg.github.io/sanitizer-api/#handle-funky-elements
    
    Change-Id: Ie8f899578c00af65fe5d9ec7b53cf08e8479d449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251166
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937296}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.cc
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.cc
@@ -154,6 +154,16 @@ Element* Sanitizer::sanitizeFor(ScriptState* script_state,
     exception_state.ClearException();
     return nullptr;
   }
+  // Edge case: The template element treatment also applies to the newly created
+  // element in .sanitizeFor.
+  if (IsA<HTMLTemplateElement>(element)) {
+    DoSanitizing(To<HTMLTemplateElement>(element)->content(), window,
+                 exception_state);
+    if (exception_state.HadException()) {
+      exception_state.ClearException();
+      return nullptr;
+    }
+  }
   return element;
 }
 
