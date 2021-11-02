4b271958cb599f89502546d2f4a6f58106937536
Etienne Pierre-doray <etiennep@chromium.org>
2021-11-02 15:19:30 +0000

    [battery]: User supplied sample-interval in power tool.
    And a few other goodies:
    - Redirect logging to std::err.
    - Print help for wrong usage
    
    Bug: 1254332
    Change-Id: I672be7a242dd19cb969c1b59c5158103ff323dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250742
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937338}

--- tools/mac/power/power_sampler/power_sampler_main.cc
+++ tools/mac/power/power_sampler/power_sampler_main.cc
@@ -3,8 +3,12 @@
 // found in the LICENSE file.
 
 #include <cstdio>
+#include <iostream>
 
+#include "base/command_line.h"
+#include "base/logging.h"
 #include "base/run_loop.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/single_thread_task_executor.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -15,7 +19,61 @@
 #include "tools/mac/power/power_sampler/sampling_controller.h"
 #include "tools/mac/power/power_sampler/user_idle_level_sampler.h"
 
+namespace {
+
+void InitLogging() {
+  logging::LoggingSettings settings;
+  settings.logging_dest =
+      logging::LOG_TO_SYSTEM_DEBUG_LOG | logging::LOG_TO_STDERR;
+  settings.log_file_path = nullptr;
+  settings.lock_log = logging::DONT_LOCK_LOG_FILE;
+  settings.delete_old = logging::APPEND_TO_OLD_LOG_FILE;
+  bool logging_res = logging::InitLogging(settings);
+  CHECK(logging_res);
+}
+
+const char kSwitchHelp[] = "h";
+const char kSwitchSampleInterval[] = "sample-interval";
+
+// Prints main usage text.
+void PrintUsage(std::ostream& err) {
+  err << "Usage: powermetrics [-sample-interval=sample_interval]" << std::endl;
+}
+
+}  // namespace
+
+// Status code, which can also be used as process exit code. Therefore
+// success is explicitly 0.
+enum StatusCode {
+  kStatusSuccess = 0,
+  kStatusUsage = 1,
+  kStatusInvalidParam = 2,
+};
+
 int main(int argc, char** argv) {
+  // Initialize infrastructure from base.
+  base::CommandLine::Init(argc, argv);
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
+  InitLogging();
+
+  if (command_line.HasSwitch(kSwitchHelp)) {
+    PrintUsage(std::cerr);
+    return kStatusUsage;
+  }
+
+  base::TimeDelta sampling_interval = base::Seconds(60);
+  if (command_line.HasSwitch(kSwitchSampleInterval)) {
+    std::string interval_seconds_switch =
+        command_line.GetSwitchValueASCII(kSwitchSampleInterval);
+    uint64_t interval_seconds = 0;
+    if (!base::StringToUint64(interval_seconds_switch, &interval_seconds)) {
+      PrintUsage(std::cerr);
+      return kStatusInvalidParam;
+    }
+    sampling_interval = base::Seconds(interval_seconds);
+  }
+
   base::SingleThreadTaskExecutor executor(base::MessagePumpType::DEFAULT);
   power_sampler::SamplingController controller;
 
@@ -38,9 +96,6 @@ int main(int argc, char** argv) {
 
   controller.StartSession();
 
-  // TODO(etiennep): Take sampling interval from command line.
-  base::TimeDelta sampling_interval = base::Seconds(10);
-
   base::RunLoop run_loop;
   // TODO(siggi): Support battery notifications to drive OnSamplingEvent().
   base::RepeatingTimer timer(
@@ -61,5 +116,5 @@ int main(int argc, char** argv) {
 
   // TODO(etiennep): Optionally export to a file.
 
-  return 0;
+  return kStatusSuccess;
 }
