f99f8e3d57c0355e387253cd6b7f1ca5d39a1ca0
Chad Duffin <chadduffin@chromium.org>
2021-11-02 17:47:40 +0000

    Reland "[CrOS Network] Implement NetworkFeaturePodButton."
    This reverts commit 61eec6b85518de1c16c5a4fc365ef38d7e55ed6d.
    
    Reason for revert: Relanding since the change that had caused the
    break,
    https://chromium-review.googlesource.com/c/chromium/src/+/3255346, has
    been fixed and relanded.
    
    Original change's description:
    > Revert "[CrOS Network] Implement NetworkFeaturePodButton."
    >
    > This reverts commit e6f157634be3bde143621261de9c8efd4cda2126.
    >
    > Reason for revert: This CL is blocking uprev as it breaks a tast test
    > checking for the network pod.
    >
    > crbug/1264078
    >
    > Original change's description:
    > > [CrOS Network] Implement NetworkFeaturePodButton.
    > >
    > > This change implements the NetworkFeaturePodButton class responsible for
    > > notifying the NetworkFeaturePodButton when any theme associated with the
    > > view has changed.
    > >
    > > Bug: 1169479
    > > Test: Ran unit tests locally.
    > > Change-Id: Ie4651c3b4339b7ce61e400694c3e06b2df3a6d87
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243106
    > > Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    > > Reviewed-by: Gordon Seto <gordonseto@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#935172}
    >
    > Bug: 1169479
    > Change-Id: I98eb9608680acd3b683df2b5ca80036097b8a54c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251699
    > Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    > Reviewed-by: Gordon Seto <gordonseto@google.com>
    > Owners-Override: Yulun Wu <yulunwu@chromium.org>
    > Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    > Commit-Queue: Gordon Seto <gordonseto@google.com>
    > Cr-Commit-Position: refs/heads/main@{#936196}
    
    Bug: 1169479
    Change-Id: I87de7c9f2e76286c08547d0ccc5e61a0e6820fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257907
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937438}

---
+++ ash/system/network/network_feature_pod_button.cc
@@ -0,0 +1,29 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/network/network_feature_pod_button.h"
+
+#include "base/check.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+
+namespace ash {
+
+NetworkFeaturePodButton::NetworkFeaturePodButton(
+    FeaturePodControllerBase* controller,
+    Delegate* delegate)
+    : FeaturePodButton(controller), delegate_(delegate) {
+  DCHECK(delegate);
+}
+
+NetworkFeaturePodButton::~NetworkFeaturePodButton() = default;
+
+void NetworkFeaturePodButton::OnThemeChanged() {
+  FeaturePodButton::OnThemeChanged();
+  delegate_->OnFeaturePodButtonThemeChanged();
+}
+
+BEGIN_METADATA(NetworkFeaturePodButton, FeaturePodButton)
+END_METADATA
+
+}  // namespace ash
---
+++ ash/system/network/network_feature_pod_button.h
@@ -0,0 +1,44 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_NETWORK_NETWORK_FEATURE_POD_BUTTON_H_
+#define ASH_SYSTEM_NETWORK_NETWORK_FEATURE_POD_BUTTON_H_
+
+#include "ash/ash_export.h"
+#include "ash/system/unified/feature_pod_button.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+
+namespace ash {
+
+// This class that provides the corresponding controller with notifications of
+// when the theme for this view changes.
+class ASH_EXPORT NetworkFeaturePodButton : public FeaturePodButton {
+ public:
+  METADATA_HEADER(NetworkFeaturePodButton);
+
+  // This class defines the interface that NetworkFeaturePodButton will use to
+  // propagate theme changes.
+  class Delegate {
+   public:
+    virtual void OnFeaturePodButtonThemeChanged() = 0;
+  };
+
+  NetworkFeaturePodButton(FeaturePodControllerBase* controller,
+                          Delegate* delegate);
+  NetworkFeaturePodButton(const NetworkFeaturePodButton&) = delete;
+  NetworkFeaturePodButton& operator=(const NetworkFeaturePodButton&) = delete;
+  ~NetworkFeaturePodButton() override;
+
+ private:
+  friend class NetworkFeaturePodButtonTest;
+
+  // views::Button:
+  void OnThemeChanged() override;
+
+  Delegate* delegate_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_NETWORK_NETWORK_FEATURE_POD_BUTTON_H_
