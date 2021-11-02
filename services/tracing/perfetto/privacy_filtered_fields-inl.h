8183470b5a96c857038e8f7220cfabc53833886e
Carlos Caballero <carlscab@google.com>
2021-11-02 18:10:10 +0000

    Update privacy filter for RendererMainThreadTaskExecution
    New field added in https://crrev.com/c/3226145
    
    Change-Id: Id0deb6efd575ad249cd51d880d26f7a942e5af53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257668
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/main@{#937460}

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -313,13 +313,18 @@ constexpr int kChromeSamplingProfilerSampleCollectedIndices[] = {1, 2, -1};
 constexpr MessageInfo kChromeSamplingProfilerSampleCollected = {
     kChromeSamplingProfilerSampleCollectedIndices, nullptr};
 
+// Proto Message: RendererMainThreadTaskExecution
+constexpr int kRendererMainThreadTaskExecutionIndices[] = {1, 2, 3, 4, -1};
+constexpr MessageInfo kRendererMainThreadTaskExecution = {
+    kRendererMainThreadTaskExecutionIndices, nullptr};
+
 // Proto Message: TrackEvent
 constexpr int kTrackEventIndices[] = {
     1,    2,    3,    5,    6,    9,    10,   11,   12,   16,   17,   22,
     23,   24,   25,   26,   27,   28,   29,   30,   31,   32,   33,   34,
     35,   36,   38,   39,   40,   41,   42,   43,   1001, 1002, 1003, 1004,
     1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016,
-    1017, 1018, 1019, 1020, 1021, 1023, 1024, 1025, -1};
+    1017, 1018, 1019, 1020, 1021, 1023, 1024, 1025, 1031, -1};
 constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     nullptr,
     nullptr,
@@ -376,7 +381,8 @@ constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     &kRenderFrameProxyHost,
     &kParkableStringCompressInBackground,
     &kParkableStringUnpark,
-    &kChromeSamplingProfilerSampleCollected};
+    &kChromeSamplingProfilerSampleCollected,
+    &kRendererMainThreadTaskExecution};
 constexpr MessageInfo kTrackEvent = {kTrackEventIndices,
                                      kTrackEventComplexMessages};
 
