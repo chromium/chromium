986513b8471cb1700cbfb7852a69d06904dbd05c
Brian Osman <brianosman@google.com>
2021-11-02 23:22:36 +0000

    Revert "Switch to Skia's newer method of paint alpha modulation of SkShader"
    This reverts commit 514151b5d5f652df1befc8a7ec35dff4801eca89.
    
    Reason for revert: Performance regression (and the incoming fix will undo the tiny change to the web_test results, so easier to revert this first).
    
    Original change's description:
    > Switch to Skia's newer method of paint alpha modulation of SkShader
    >
    > This aligns the results between CPU and GPU backend, and is easier to
    > reason about for complex shader trees. Differences on existing content
    > should be trivial.
    >
    > Bug: skia:11942
    > Change-Id: Ic44b5d601a2be26e1143db13b4ff4fa7c71c0791
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248144
    > Reviewed-by: Brian Salomon <bsalomon@google.com>
    > Commit-Queue: Brian Osman <brianosman@google.com>
    > Cr-Commit-Position: refs/heads/main@{#936883}
    
    Bug: skia:11942
    Bug: chromium:1265736
    Bug: chromium:1265678
    Change-Id: I3e75bad25f33caf67a259a02ecb15ca221749f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256586
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#937613}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -215,6 +215,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_LEGACY_INNER_JOINS
 
+#define SK_SUPPORT_LEGACY_PAINT_ALPHA_MODULATION
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
