64e17367b5f84caf12c91ab89c29b9eb608d6265
Florent Castelli <orphis@chromium.org>
2021-11-03 10:54:47 +0000

    RTCDataChannel: Send should throw when the send queue is full
    Previously, the implementation would close the data channel when the send
    queue was full.
    According to https://w3c.github.io/webrtc-pc/#datachannel-send it should
    throw a DOMException with OperationError code.
    
    Removed from fast/peerconnection/RTCPeerConnection-datachannel.html the
    parts that are already covered by WPT.
    
    Bug: 1260845
    Change-Id: I6e02bedddb02afbce52f986aa54df9e6587f5c08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226078
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937809}

--- third_party/blink/renderer/modules/peerconnection/rtc_data_channel.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_data_channel.h
@@ -164,8 +164,9 @@ class MODULES_EXPORT RTCDataChannel final
   void ScheduledEventTimerFired(TimerBase*);
 
   const scoped_refptr<webrtc::DataChannelInterface>& channel() const;
-  bool SendRawData(const char* data, size_t length);
-  bool SendDataBuffer(webrtc::DataBuffer data_buffer);
+  bool ValidateSendLength(size_t length, ExceptionState& exception_state);
+  void SendRawData(const char* data, size_t length);
+  void SendDataBuffer(webrtc::DataBuffer data_buffer);
 
   // Initializes |feature_handle_for_scheduler_|, which must not yet have been
   // initialized.
