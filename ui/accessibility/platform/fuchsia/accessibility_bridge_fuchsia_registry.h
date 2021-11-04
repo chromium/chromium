39b8d5701a5bad9165027388084212a7f6ceb064
Alexander Brusher <abrusher@google.com>
2021-11-04 09:47:36 +0000

    [fuchsia][a11y] Adds AccessibilityBridgeFuchsiaRegistry.
    This change introduces a singleton registry via which clients of the
    fuchsia accessibility bridge can access instances.
    
    AX-Relnotes: n/a
    Test: n/a
    Bug: fuchsia:82802
    Change-Id: I28af8ff73fe0c3e49bff9cbb566fb3b0bf69ca94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229220
    Commit-Queue: Alexander Brusher <abrusher@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938200}

---
+++ ui/accessibility/platform/fuchsia/accessibility_bridge_fuchsia_registry.h
@@ -0,0 +1,56 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_ACCESSIBILITY_PLATFORM_FUCHSIA_ACCESSIBILITY_BRIDGE_FUCHSIA_REGISTRY_H_
+#define UI_ACCESSIBILITY_PLATFORM_FUCHSIA_ACCESSIBILITY_BRIDGE_FUCHSIA_REGISTRY_H_
+
+#include "base/containers/flat_map.h"
+#include "ui/accessibility/ax_export.h"
+#include "ui/accessibility/platform/fuchsia/accessibility_bridge_fuchsia.h"
+
+namespace ui {
+
+// This class manages a mapping between AXTrees and their respective
+// accessibility bridge instances. The accessibility bridge instances
+// are owned by the FrameImpl for the WebContents to which the AXTrees
+// belong.
+class AX_EXPORT AccessibilityBridgeFuchsiaRegistry {
+ public:
+  // Get the global instance of this class.
+  static AccessibilityBridgeFuchsiaRegistry* GetInstance();
+
+  AccessibilityBridgeFuchsiaRegistry();
+  ~AccessibilityBridgeFuchsiaRegistry();
+
+  AccessibilityBridgeFuchsiaRegistry(
+      const AccessibilityBridgeFuchsiaRegistry&) = delete;
+  AccessibilityBridgeFuchsiaRegistry& operator=(
+      const AccessibilityBridgeFuchsiaRegistry&) = delete;
+
+  // Retrieve an |AccessibilityBridgeFuchsia| by AXTreeID.
+  //
+  // Returns nullptr if no accessibility bridge is registered for
+  // |ax_tree_id|.
+  AccessibilityBridgeFuchsia* GetAccessibilityBridge(AXTreeID ax_tree_id);
+
+  // Registers the accessibility bridge for the specified AXTreeID.
+  // There must NOT be an accessibility bridge registered for |ax_tree_id| when
+  // this method is called.
+  void RegisterAccessibilityBridge(
+      AXTreeID ax_tree_id,
+      AccessibilityBridgeFuchsia* accessibility_bridge);
+
+  // Removes the accessibility bridge for the specified AXTreeID.
+  // There must be an accessibility bridge registered for |ax_tree_id| when this
+  // method is called.
+  void UnregisterAccessibilityBridge(AXTreeID ax_tree_id);
+
+ private:
+  base::flat_map<AXTreeID, AccessibilityBridgeFuchsia*>
+      ax_tree_id_to_accessibility_bridge_map_;
+};
+
+}  // namespace ui
+
+#endif  // UI_ACCESSIBILITY_PLATFORM_FUCHSIA_ACCESSIBILITY_BRIDGE_FUCHSIA_REGISTRY_H_
