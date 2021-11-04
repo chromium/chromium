59e91c953a4fc1c552a301910bf23c6f0c9581df
Xianzhu Wang <wangxianzhu@chromium.org>
2021-11-04 20:49:37 +0000

    Fix paint location of RemoteFrameView foreign layer
    Bug: 1260250
    Change-Id: If0fa74fab0c075d7eb920bf3775d9726fa3eff9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261010
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938483}

--- third_party/blink/renderer/core/frame/remote_frame_view.cc
+++ third_party/blink/renderer/core/frame/remote_frame_view.cc
@@ -296,11 +296,10 @@ void RemoteFrameView::Paint(GraphicsContext& context,
 
   if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled() &&
       GetFrame().GetCcLayer()) {
-    auto offset = ToRoundedPoint(
-        GetLayoutEmbeddedContent()->ReplacedContentRect().offset);
     RecordForeignLayer(context, owner_layout_object,
                        DisplayItem::kForeignLayerRemoteFrame,
-                       GetFrame().GetCcLayer(), offset);
+                       GetFrame().GetCcLayer(),
+                       FrameRect().origin() + ToGfxVector2d(paint_offset));
   }
 }
 
