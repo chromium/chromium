738cc1a7f8da182711ee443cc69fb27481fa79d7
Zijie He <zijiehe@google.com>
2021-11-04 21:23:53 +0000

    [fuchsia] Enable ScopedMockClockOverrideTest.Time
    Executed 10k times, no flakiness detected.
    This is part of the effort to revise the disabled test cases on fuchsia
    and other impacted OSes.
    
    Bug: 1253618
    Fixed: 1060357
    
    Change-Id: I2a003c30fa7322e7cc7a7cd66012164890b2aa1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260271
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#938513}

--- base/test/scoped_mock_clock_override_unittest.cc
+++ base/test/scoped_mock_clock_override_unittest.cc
@@ -13,14 +13,7 @@ namespace base {
 
 namespace {
 
-#if defined(OS_FUCHSIA)
-// TODO(https://crbug.com/1060357): Enable when RTC flake is fixed.
-#define MAYBE_Time DISABLED_Time
-#else
-#define MAYBE_Time Time
-#endif
-
-TEST(ScopedMockClockOverrideTest, MAYBE_Time) {
+TEST(ScopedMockClockOverrideTest, Time) {
   // Choose a reference time that we know to be in the past but close to now.
   Time build_time = GetBuildTime();
 
