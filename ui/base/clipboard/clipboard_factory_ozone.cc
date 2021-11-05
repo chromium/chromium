6750de04719171e690e76e0fe148569d7a6b15ae
Nick Diego Yamane <nickdiego@igalia.com>
2021-11-05 02:52:35 +0000

    ozone: x11: clean up ui/base/clipboard
    This gets rid of USE_X11 and IsUsingOzonePlatform references from
    ui/base/clipboard, now that non-ozone path is no longer supported.
    
    R=dcheng@chromium.org, msisov@igalia.com
    
    Bug: 1096425
    Change-Id: Icdf0341df65cd3551f56770a499b3ff0eb5b937f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257890
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938636}

--- ui/base/clipboard/clipboard_factory_ozone.cc
+++ ui/base/clipboard/clipboard_factory_ozone.cc
@@ -9,45 +9,28 @@
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "ui/base/clipboard/clipboard_non_backed.h"
-#include "ui/base/ui_base_features.h"
-#include "ui/base/ui_base_switches.h"
-
-#if defined(USE_OZONE)
 #include "ui/base/clipboard/clipboard_ozone.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/ozone/public/ozone_platform.h"
-#endif
-
-#if defined(USE_X11)
-#include "ui/base/clipboard/clipboard_x11.h"
-#endif
 
 namespace ui {
 
-// Clipboard factory method.
-// TODO(crbug.com/1096425): Cleanup when non-Ozone path gets dropped.
 Clipboard* Clipboard::Create() {
-  // On Linux Desktop builds ozone usage depends on UseOzonePlatform feature.
-  // For all the other Ozone builds, this is set to true.
-  bool use_ozone_impl = features::IsUsingOzonePlatform();
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  // Use platform-backed implementation iff --use-system-clipbboard is passed.
-  use_ozone_impl &= base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kUseSystemClipboard);
+  // On Linux Desktop and Lacros, Ozone's Clipboard impl is always used.
+  // On Linux builds of ash-chrome, use platform-backed implementation iff
+  // use-system-clipboard command line switch is passed.
+  const bool use_ozone_impl =
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+      true;
+#else
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kUseSystemClipboard);
 #endif
 
-#if defined(USE_X11)
-  // Use X11 implementation unless UseOzonePlatform feature is enabled.
-  if (!use_ozone_impl)
-    return new ClipboardX11;
-#endif
-
-#if defined(USE_OZONE)
   if (use_ozone_impl && OzonePlatform::GetInstance()->GetPlatformClipboard())
     return new ClipboardOzone;
-#endif
 
-#if defined(USE_X11) && BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
   NOTREACHED() << "System clipboard integration should be in place.";
 #endif
   return new ClipboardNonBacked;
