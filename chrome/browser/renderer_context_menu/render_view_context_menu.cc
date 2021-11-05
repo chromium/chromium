4087acaf9a4bbc4808f5a663070ac700b2c6c789
Austin Sullivan <asully@chromium.org>
2021-11-05 22:46:29 +0000

    Revert "[unseasoned-pdf] Update context menu checks of the PDF viewer"
    This reverts commit aa10b7bd9d1a681d34a5d285ed5cc15e90c6cea7.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [unseasoned-pdf] Update context menu checks of the PDF viewer
    >
    > Context menus need to check if they are invoked in the PDF viewer to
    > provide appropriate options.
    >
    > Before, it did this by checking the URL of the invoking frame. This was
    > fine when the PDF viewer had one frame, the extension's. However, the
    > Unseasoned PDF viewer has two frames, the extension's and the plugin's.
    > The plugin's frame has an unpredictable origin, so we need to check if
    > its parent is the extension frame.
    >
    > Fixed: 1265345
    > Change-Id: I08d2d9b4fc3e96047ebca8b9eb04b270b8c56ccc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259600
    > Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#938976}
    
    Change-Id: I52d90c3bda285d1a544261906fb6bfaf810f7c87
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1267445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3265891
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Owners-Override: Austin Sullivan <asully@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938995}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -635,18 +635,6 @@ ui::MenuSourceType GetMenuSourceType(int event_flags) {
 }
 #endif
 
-bool IsFrameInPdfViewer(content::RenderFrameHost* rfh) {
-  if (!rfh)
-    return false;
-
-  if (IsPdfExtensionOrigin(rfh->GetLastCommittedOrigin()))
-    return true;
-
-  content::RenderFrameHost* parent_rfh = rfh->GetParent();
-  return parent_rfh &&
-         IsPdfExtensionOrigin(parent_rfh->GetLastCommittedOrigin());
-}
-
 }  // namespace
 
 // static
@@ -2308,10 +2296,14 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
 
     case IDC_CONTENT_CONTEXT_ROTATECW:
     case IDC_CONTENT_CONTEXT_ROTATECCW: {
+      bool is_pdf_viewer_fullscreen = false;
+#if BUILDFLAG(ENABLE_PDF)
       // Rotate commands should be disabled when in PDF Viewer's Presentation
       // mode.
-      bool is_pdf_viewer_fullscreen =
-          IsFrameInPdfViewer(GetRenderFrameHost()) && IsHTML5Fullscreen();
+      is_pdf_viewer_fullscreen =
+          IsPdfExtensionOrigin(url::Origin::Create(GetDocumentURL(params_))) &&
+          IsHTML5Fullscreen();
+#endif
       return !is_pdf_viewer_fullscreen &&
              (params_.media_flags & ContextMenuData::kMediaCanRotate) != 0;
     }
@@ -3129,7 +3121,7 @@ bool RenderViewContextMenu::IsRegionSearchEnabled() const {
       provider && !provider->image_url().empty() &&
       provider->image_url_ref().IsValid(service->search_terms_data());
   return base::FeatureList::IsEnabled(lens::features::kLensRegionSearch) &&
-         !IsFrameInPdfViewer(GetRenderFrameHost()) &&
+         !IsPdfExtensionOrigin(url::Origin::Create(GetDocumentURL(params_))) &&
          provider_supports_image_search &&
          !GetDocumentURL(params_).SchemeIs(content::kChromeUIScheme) &&
          !in_app &&
