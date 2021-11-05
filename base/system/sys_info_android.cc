aa51b26dcc63071179110d304e14f0df63c9c532
Bo Liu <boliu@chromium.org>
2021-11-05 20:57:22 +0000

    base: Bump default android version to 12.0.99
    Change-Id: Ic74255dbb9e5c0a433cf65944afac1cdc9afaaee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183786
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938922}

--- base/system/sys_info_android.cc
+++ base/system/sys_info_android.cc
@@ -64,7 +64,7 @@ namespace {
 // cannot be acquired. Use the latest Android release with a higher bug fix
 // version to avoid unnecessarily comparison errors with the latest release.
 // This should be manually kept up to date on each Android release.
-const int kDefaultAndroidMajorVersion = 10;
+const int kDefaultAndroidMajorVersion = 12;
 const int kDefaultAndroidMinorVersion = 0;
 const int kDefaultAndroidBugfixVersion = 99;
 
