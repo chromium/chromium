e5b90442f64133cca12833c81dc321f2bf0b0696
Eugene Zemtsov <eugene@chromium.org>
2021-11-08 23:20:08 +0000

    webcodecs: Avoid premature destruction of |media_encoder_|
    Fix crbug.com/1266144: Reset encoder when context is destroyed
    
    Surprisingly some callbacks can still be executed on the main thread
    after ContextDestroyed() was called. In order to avoid obsolete
    encoding-done and flush-done callbacks calling ProcessRequests()
    we need to properly reset the encoder.
    
    We could probably get away with just incrementing |reset_count_|, but
    full reset seems more logically consistent.
    
    -----
    Fix crbug.com/1267085: Delay destruction of |media_encoder_| on error
    
    Use a task runner to delete |media_encoder_| so it's never deleted from
    within encoder's error callback.
    
    
    (cherry picked from commit 995ac3d0c4a7b28065711be0ff7974b07783855f)
    
    Bug: 1266144, 1267085
    Change-Id: Ie785617d46a2ccbf9e9ca37879e7ee5aa284f0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262415
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#939030}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3269024
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4692@{#28}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- media/video/video_encoder_fallback_test.cc
+++ media/video/video_encoder_fallback_test.cc
@@ -296,9 +296,29 @@ TEST_F(VideoEncoderFallbackTest, SecondaryFailureOnInitialize) {
                               StatusCode::kEncoderUnsupportedCodec);
       }));
 
+  EXPECT_CALL(*main_video_encoder_, Encode(_, _, _))
+      .WillRepeatedly(
+          Invoke([&, this](scoped_refptr<VideoFrame> frame, bool key_frame,
+                           VideoEncoder::StatusCB done_cb) {
+            RunStatusCallbackAync(std::move(done_cb),
+                                  StatusCode::kEncoderInitializeNeverCompleted);
+          }));
+
   fallback_encoder_->Initialize(
       profile, options, std::move(output_cb),
       ValidatingStatusCB(StatusCode::kEncoderUnsupportedCodec));
+
+  for (int i = 0; i < kFrameCount; i++) {
+    auto frame = VideoFrame::CreateFrame(PIXEL_FORMAT_I420, kFrameSize,
+                                         gfx::Rect(kFrameSize), kFrameSize,
+                                         base::Seconds(i));
+    auto done_callback = base::BindLambdaForTesting([this](Status s) {
+      EXPECT_TRUE(callback_runner_->RunsTasksInCurrentSequence());
+      EXPECT_EQ(s.code(), StatusCode::kEncoderUnsupportedCodec);
+      callback_runner_->DeleteSoon(FROM_HERE, std::move(fallback_encoder_));
+    });
+    fallback_encoder_->Encode(frame, true, std::move(done_callback));
+  }
 
   RunLoop();
   EXPECT_TRUE(FallbackHappened());
