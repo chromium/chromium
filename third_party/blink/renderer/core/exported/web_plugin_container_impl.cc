38893aebad0eb99bd3f27a7201d1aac52f0a4299
Xianzhu Wang <wangxianzhu@chromium.org>
2021-11-08 21:24:41 +0000

    Fix composited plugin paint offset in multicol
    (cherry picked from commit 1f1fb48502a482888cbc6ab9c462e23e74a672b5)
    
    Bug: 1260250
    Change-Id: I675b5e1273da7a4bc388a688b6ecea4b4dafa7db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262916
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#938581}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268458
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4664@{#872}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- third_party/blink/renderer/core/exported/web_plugin_container_impl.cc
+++ third_party/blink/renderer/core/exported/web_plugin_container_impl.cc
@@ -186,12 +186,11 @@ void WebPluginContainerImpl::Paint(GraphicsContext& context,
     layer_->SetBounds(gfx::Size(Size()));
     layer_->SetIsDrawable(true);
     layer_->SetHitTestable(true);
-    auto offset = RoundedIntPoint(
-        GetLayoutEmbeddedContent()->ReplacedContentRect().offset);
     // When compositing is after paint, composited plugins should have their
     // layers inserted rather than invoking WebPlugin::paint.
     RecordForeignLayer(context, *element_->GetLayoutObject(),
-                       DisplayItem::kForeignLayerPlugin, layer_, offset);
+                       DisplayItem::kForeignLayerPlugin, layer_,
+                       FrameRect().Location() + paint_offset);
     return;
   }
 
