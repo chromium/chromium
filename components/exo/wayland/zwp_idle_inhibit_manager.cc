aa66602074fb9e69cfb050109de643a30e884625
Andrew Wolfers <aswolfers@chromium.org>
2021-11-08 15:57:16 +0000

    [exo][wayland] Implement Exo Wayland idle inhibitor protocol.
    Add implementation logic to Exo Wayland idle inhibitor by calling Ash
    PowerSaveBlocker.
    
    Bug: b:196213351
    Bug: b:193670013
    Change-Id: Ic7b1d3a4a916363fe7660cb14d8a366dbcf4371e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216114
    Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939374}

--- components/exo/wayland/zwp_idle_inhibit_manager.cc
+++ components/exo/wayland/zwp_idle_inhibit_manager.cc
@@ -5,7 +5,18 @@
 #include "components/exo/wayland/zwp_idle_inhibit_manager.h"
 
 #include <idle-inhibit-unstable-v1-server-protocol.h>
+#include <memory>
+
+#include "base/memory/scoped_refptr.h"
+#include "base/task/single_thread_task_runner.h"
+#include "base/task/thread_pool.h"
+#include "components/exo/wayland/server_util.h"
+#include "services/device/public/mojom/wake_lock.mojom.h"
+#include "services/device/wake_lock/power_save_blocker/power_save_blocker.h"
 #include "wayland-server-core.h"
+
+constexpr char kPowerSaveBlockerDescription[] =
+    "Power save blocker created by exo wayland idle inhibitor";
 
 namespace exo {
 namespace wayland {
@@ -14,6 +25,25 @@ namespace {
 
 ////////////////////////////////////////////////////////////////////////////////
 // zwp_idle_inhibitor_v1_interface:
+
+class IdleInhibitor {
+ public:
+  IdleInhibitor(
+      scoped_refptr<base::SequencedTaskRunner> ui_task_runner,
+      scoped_refptr<base::SingleThreadTaskRunner> blocking_task_runner)
+      : power_save_blocker_(std::make_unique<device::PowerSaveBlocker>(
+            /*type=*/device::mojom::WakeLockType::kPreventDisplaySleep,
+            /*reason=*/device::mojom::WakeLockReason::kOther,
+            kPowerSaveBlockerDescription,
+            ui_task_runner,
+            blocking_task_runner)) {}
+
+  IdleInhibitor(const IdleInhibitor&) = delete;
+  IdleInhibitor& operator=(const IdleInhibitor&) = delete;
+
+ private:
+  std::unique_ptr<device::PowerSaveBlocker> power_save_blocker_;
+};
 
 void zwp_idle_inhibitor_destroy(wl_client* client, wl_resource* resource) {
   wl_resource_destroy(resource);
@@ -25,6 +55,26 @@ const struct zwp_idle_inhibitor_v1_interface
 ////////////////////////////////////////////////////////////////////////////////
 // zwp_idle_inhibit_manager_v1_interface:
 
+class IdleInhibitManager {
+ public:
+  IdleInhibitManager()
+      : ui_task_runner_(base::ThreadTaskRunnerHandle::Get()),
+        blocking_task_runner_(base::ThreadPool::CreateSingleThreadTaskRunner(
+            {base::MayBlock(), base::TaskPriority::BEST_EFFORT})) {}
+
+  IdleInhibitManager(const IdleInhibitManager&) = delete;
+  IdleInhibitManager& operator=(const IdleInhibitManager&) = delete;
+
+  std::unique_ptr<IdleInhibitor> CreateInhibitor() {
+    return std::make_unique<IdleInhibitor>(ui_task_runner_,
+                                           blocking_task_runner_);
+  }
+
+ private:
+  scoped_refptr<base::SequencedTaskRunner> ui_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> blocking_task_runner_;
+};
+
 void zwp_idle_inhibit_manager_destroy(wl_client* client,
                                       wl_resource* resource) {
   wl_resource_destroy(resource);
@@ -34,12 +84,13 @@ void zwp_idle_inhibit_manager_create_inhibitor(wl_client* client,
                                                wl_resource* resource,
                                                uint32_t id,
                                                wl_resource* surface) {
-  wl_resource* inhibitor =
+  wl_resource* inhibitor_resource =
       wl_resource_create(client, &zwp_idle_inhibitor_v1_interface,
                          wl_resource_get_version(resource), id);
-  wl_resource_set_implementation(inhibitor,
-                                 &zwp_idle_inhibitor_v1_implementation,
-                                 /*data=*/nullptr, /*destroy=*/nullptr);
+  std::unique_ptr<IdleInhibitor> inhibitor =
+      GetUserDataAs<IdleInhibitManager>(resource)->CreateInhibitor();
+  SetImplementation(inhibitor_resource, &zwp_idle_inhibitor_v1_implementation,
+                    std::move(inhibitor));
 }
 
 const struct zwp_idle_inhibit_manager_v1_interface
@@ -55,9 +106,8 @@ void bind_zwp_idle_inhibit_manager(wl_client* client,
                                    uint32_t id) {
   wl_resource* resource = wl_resource_create(
       client, &zwp_idle_inhibit_manager_v1_interface, version, id);
-  wl_resource_set_implementation(resource,
-                                 &zwp_idle_inhibit_manager_v1_implementation,
-                                 data, /*destroy=*/nullptr);
+  SetImplementation(resource, &zwp_idle_inhibit_manager_v1_implementation,
+                    std::make_unique<IdleInhibitManager>());
 }
 
 }  // namespace wayland
