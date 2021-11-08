e0264c41c7b07690653e6ac972f81e30be36213a
Friedrich Horschig <fhorschig@chromium.org>
2021-11-08 14:25:13 +0000

    [Passwords] Move backend factory build guard into factory implementation
    Before this CL, Chrome on Android would not be aware of the
    android-specific store backend unless internal sources are present.
    
    This is a weak guard because public builds can be affected by the mere
    presence of the sources. Additionally, it prevents the execution from
    backend java tests early in our CQ.
    
    Therefore, move the build guard into the factory code using a build
    flag. This means the behavior at build time is unaffected but the public
    classes are always available to execute tests.
    
    Side effects:
    - Chromium public builds contain small (but effectively) unused classes
    - The factory code can live in a single file
    - simpler build files
    
    Bug: None
    Change-Id: Ic1936319148f6e6e0a25714ab785235f990353e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259048
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939346}

--- chrome/browser/password_manager/password_store_backend_factory.cc
+++ chrome/browser/password_manager/password_store_backend_factory.cc
@@ -4,20 +4,28 @@
 
 #include "components/password_manager/core/browser/password_store_backend.h"
 
+#include "build/build_config.h"
+#include "components/password_manager/core/browser/login_database.h"
+#include "components/password_manager/core/browser/password_store_built_in_backend.h"
+#include "components/password_manager/core/common/password_manager_buildflags.h"
+#include "components/prefs/pref_service.h"
+
+#if defined(OS_ANDROID)
 #include "base/feature_list.h"
 #include "chrome/browser/password_manager/android/password_store_android_backend.h"
 #include "chrome/browser/password_manager/android/password_store_android_backend_bridge.h"
-#include "components/password_manager/core/browser/login_database.h"
 #include "components/password_manager/core/browser/password_store_backend_migration_decorator.h"
-#include "components/password_manager/core/browser/password_store_built_in_backend.h"
 #include "components/password_manager/core/common/password_manager_features.h"
-#include "components/prefs/pref_service.h"
+#endif  // defined(OS_ANDROID)
 
 namespace password_manager {
 
 std::unique_ptr<PasswordStoreBackend> PasswordStoreBackend::Create(
     std::unique_ptr<LoginDatabase> login_db,
     PrefService* prefs) {
+#if !defined(OS_ANDROID) || BUILDFLAG(USE_LEGACY_PASSWORD_STORE_BACKEND)
+  return std::make_unique<PasswordStoreBuiltInBackend>(std::move(login_db));
+#else  // OS_ANDROID && !USE_LEGACY_PASSWORD_STORE_BACKEND
   if (PasswordStoreAndroidBackendBridge::CanCreateBackend()) {
     if (base::FeatureList::IsEnabled(
             password_manager::features::kUnifiedPasswordManagerAndroid)) {
@@ -31,6 +39,7 @@ std::unique_ptr<PasswordStoreBackend> PasswordStoreBackend::Create(
         prefs);
   }
   return std::make_unique<PasswordStoreBuiltInBackend>(std::move(login_db));
+#endif
 }
 
 }  // namespace password_manager
