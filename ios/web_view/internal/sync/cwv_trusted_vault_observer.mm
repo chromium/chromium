a20c4def0a2c55ecdb9be26a74c86195e0da066f
John Wu <jzw@chromium.org>
2021-11-08 21:05:00 +0000

    Add some public APIs that are required to implement trusted vault
    - Define a CWVTrustedVaultObserver that simply wraps
      syncer::TrustedVaultClient::Observer
    - Define a CWVTrustedVaultProvider that is meant to be implemented by
      the client of //ios/web_view. Essentially wrapping the API of
      syncer::TrustedVaultClient.
    
    Actual usages of these APIs will follow in another CL.
    
    Bug: 1266130
    Change-Id: I643b4152835e1de1b1aa1089a08a84d2212b6fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259235
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939515}

---
+++ ios/web_view/internal/sync/cwv_trusted_vault_observer.mm
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web_view/internal/sync/cwv_trusted_vault_observer_internal.h"
+
+#import "ios/web_view/public/cwv_trusted_vault_provider.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation CWVTrustedVaultObserver {
+  syncer::TrustedVaultClient::Observer* _observer;
+}
+
+- (instancetype)initWithTrustedVaultObserver:
+    (syncer::TrustedVaultClient::Observer*)observer {
+  self = [super init];
+  if (self) {
+    _observer = observer;
+  }
+  return self;
+}
+
+#pragma mark - Public Methods
+
+- (void)trustedVaultProviderDidChangeKeys:
+    (id<CWVTrustedVaultProvider>)provider {
+  _observer->OnTrustedVaultKeysChanged();
+}
+
+- (void)trustedVaultProviderDidChangeRecoverability:
+    (id<CWVTrustedVaultProvider>)provider {
+  _observer->OnTrustedVaultRecoverabilityChanged();
+}
+
+@end
