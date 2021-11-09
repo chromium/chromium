181c70d326baa3ff36c840951cd98c89956d9b3f
Lijin Shen <lazzzis@google.com>
2021-11-09 18:42:40 +0000

    Enable message UI feature for AutofillUpstreamTest.
    A follow-up CL of https://crrev.com/c/3246155
    
    Bug: 1263205
    Change-Id: Ib237c2a3d9f5ef63be73317d444f32af3ba9234d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262543
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#939925}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -396,6 +396,7 @@ public abstract class ChromeFeatureList {
     public static final String MESSAGES_FOR_ANDROID_READER_MODE = "MessagesForAndroidReaderMode";
     public static final String MESSAGES_FOR_ANDROID_REDUCE_LAYOUT_CHANGES =
             "MessagesForAndroidReduceLayoutChanges";
+    public static final String MESSAGES_FOR_ANDROID_SAVE_CARD = "MessagesForAndroidSaveCard";
     public static final String MESSAGES_FOR_ANDROID_SYNC_ERROR = "MessagesForAndroidSyncError";
     public static final String MOBILE_IDENTITY_CONSISTENCY_PROMOS =
             "MobileIdentityConsistencyPromos";
