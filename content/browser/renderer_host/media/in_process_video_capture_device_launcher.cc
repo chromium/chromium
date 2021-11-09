18c24da7e417117201b100826034d17d9076417b
Christopher Cameron <ccameron@chromium.org>
2021-11-09 20:44:39 +0000

    DesktopCaptureMacV2: Re-enable path
    The known bugs in this path have been fixed.
    
    Bug: 1164736
    Change-Id: I9e22c1d278fa3d4f1e9b1cd7c961014995523520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267087
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939992}

--- content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
+++ content/browser/renderer_host/media/in_process_video_capture_device_launcher.cc
@@ -82,7 +82,7 @@ const int kMaxNumberOfBuffers = media::kVideoCaptureDefaultMaxBufferPoolSize;
 
 #if defined(OS_MAC)
 const base::Feature kDesktopCaptureMacV2{"DesktopCaptureMacV2",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 #endif
 
