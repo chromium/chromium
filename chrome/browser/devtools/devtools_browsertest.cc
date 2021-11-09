d193cb16fd121267888a7c7e9c5f8541816871d5
Fabio Tirelo <ftirelo@chromium.org>
2021-11-09 17:18:31 +0000

    [Sheriff] Disable flaky test
    Bug: 1268450
    Change-Id: Ic4f324bcbb1df5020cae8c6af012f6c4cfc370e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270366
    Owners-Override: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939885}

--- chrome/browser/devtools/devtools_browsertest.cc
+++ chrome/browser/devtools/devtools_browsertest.cc
@@ -2517,7 +2517,13 @@ IN_PROC_BROWSER_TEST_F(DevToolsTest, NewWindowFromBrowserContext) {
   DevToolsWindowTesting::CloseDevToolsWindowSync(window_);
 }
 
-IN_PROC_BROWSER_TEST_F(SitePerProcessDevToolsTest, InspectElement) {
+// TODO(crbug.com/1268450): Flaky on ChromeOS.
+#if defined(OS_CHROMEOS)
+#define MAYBE_InspectElement DISABLED_InspectElement
+#else
+#define MAYBE_InspectElement InspectElement
+#endif  // defined(OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(SitePerProcessDevToolsTest, MAYBE_InspectElement) {
   GURL url(embedded_test_server()->GetURL("a.com", "/devtools/oopif.html"));
   GURL iframe_url(
       embedded_test_server()->GetURL("b.com", "/devtools/oopif_frame.html"));
