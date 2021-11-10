b2a5e96e6d78366ecb9ae47d2e91b92905ece46b
Harry Cutts <hcutts@chromium.org>
2021-11-10 18:48:37 +0000

    Diagnostics: add icons to arrow keys on keyboard diagram
    This makes them match the ones on the actual keys, and lays the
    groundwork for adding other icons (to the top row and the search key)
    once I've sourced them.
    
    Screenshot: http://shortn/_rlkNEqvC9L
    
    Change-Id: I5363b16ed247340760ff898539a388a41e59ceca
    Bug: 1207678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271673
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940400}

--- ash/webui/common/resources/keyboard_key.js
+++ ash/webui/common/resources/keyboard_key.js
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import './keyboard_icons.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 /**
@@ -22,11 +25,17 @@ export class KeyboardKeyElement extends PolymerElement {
   static get properties() {
     return {
       /**
-       * The glyph to show in the center of the key (if topGlyph is unset) or in
-       * the bottom half (otherwise).
-       * @type {string}
+       * The text to show on the key, if any.
+       * @type {?string}
        */
       mainGlyph: String,
+
+      /**
+       * The name of the icon to use, if any. The name should be of the form:
+       * `iconset_name:icon_name`.
+       * @type {?string}
+       */
+      icon: String,
     };
   }
 }
