8cd4aa6b21b024fb19f000cb8305c6b60eafb16a
Dan S <danan@chromium.org>
2021-11-10 21:11:26 +0000

    Add TODO for bug to address inconsistent dialog repositioning behavior.
    This bug results in a11y issues because the dialog is not
    repositioned when the screen is resized.
    
    Bug: 1268547
    
    Change-Id: Ia65840d07d0fcd7190411c7a25d35f0dca2a4486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272603
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940489}

--- chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h
+++ chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h
@@ -89,6 +89,9 @@ class SystemWebDialogDelegate : public ui::WebDialogDelegate {
   // If |parent| is not null, the dialog will be parented to |parent|.
   // Otherwise it will be attached to either the AlwaysOnTop container or the
   // LockSystemModal container, depending on the session state at creation.
+  // TODO(https://crbug.com/1268547): Passing a non-null |parent| here or to
+  // ShowSystemDialog() seems to prevent the dialog from properly repositioning
+  // on screen size changes (i.e. when the docked screen magnifier is enabled).
   void ShowSystemDialogForBrowserContext(content::BrowserContext* context,
                                          gfx::NativeWindow parent = nullptr);
   // Same as previous but shows a system dialog using the current active
