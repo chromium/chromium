319384a4a0fec2f23707246c310fc8c7571d27d8
Kevin Marshall <kmarshall@chromium.org>
2021-11-10 16:19:48 +0000

    [fuchsia] Disable DisablePrintOnCrashedTab test if printing unsupported.
    The test BrowserUnitTest.DisablePrintOnCrashedTab is now only
    built if the ENABLE_PRINTING buildflag is set.
    
    Bug: 1238790
    Change-Id: Ic4c3865787c0d8de849e3e5a6bc24a010559f32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272010
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940321}

--- chrome/browser/ui/browser_unittest.cc
+++ chrome/browser/ui/browser_unittest.cc
@@ -24,6 +24,7 @@
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/web_contents_tester.h"
+#include "printing/buildflags/buildflags.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 using content::SiteInstance;
@@ -113,6 +114,7 @@ TEST_F(BrowserUnitTest, MAYBE_SetBackgroundColorForNewTab) {
             *raw_contents2->GetMainFrame()->GetView()->GetBackgroundColor());
 }
 
+#if BUILDFLAG(ENABLE_PRINTING)
 // Ensure the print command gets disabled when a tab crashes.
 TEST_F(BrowserUnitTest, DisablePrintOnCrashedTab) {
   TabStripModel* tab_strip_model = browser()->tab_strip_model();
@@ -135,6 +137,7 @@ TEST_F(BrowserUnitTest, DisablePrintOnCrashedTab) {
   EXPECT_FALSE(command_updater->IsCommandEnabled(IDC_PRINT));
   EXPECT_FALSE(chrome::CanPrint(browser()));
 }
+#endif  // BUILDFLAG(ENABLE_PRINTING)
 
 // Ensure the zoom-in and zoom-out commands get disabled when a tab crashes.
 TEST_F(BrowserUnitTest, DisableZoomOnCrashedTab) {
