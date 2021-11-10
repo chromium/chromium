6d2f5ad2eba05d191746670be8e52be0096d7d4a
Daniel White <danieltwhite@google.com>
2021-11-10 19:16:46 +0000

    Capturing and logging each frames' First Contentful Paint time;
    The new feature registers a PerformanceObserver
    event listener that listens to 'paint' events, captures the time the
    event was recorded, and forwards this time back to the browser
    which logs these times in UMA.
    
    In a future CL, the browser will aggregate these times across all
    the page's frames and determine the earliest time which will also
    be logged in UMA.
    
    Bug: 1267902
    Change-Id: I2e627f144b7f7563f056a771564c2bad9a003e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262711
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940419}

--- ios/web/js_messaging/java_script_feature_util_impl.mm
+++ ios/web/js_messaging/java_script_feature_util_impl.mm
@@ -14,6 +14,7 @@
 #import "ios/web/find_in_page/find_in_page_java_script_feature.h"
 #include "ios/web/js_features/context_menu/context_menu_java_script_feature.h"
 #include "ios/web/js_features/scroll_helper/scroll_helper_java_script_feature.h"
+#include "ios/web/js_features/web_performance_metrics/web_performance_metrics_java_script_feature.h"
 #import "ios/web/js_features/window_error/window_error_java_script_feature.h"
 #import "ios/web/js_messaging/script_command_java_script_feature.h"
 #import "ios/web/js_messaging/web_frames_manager_java_script_feature.h"
@@ -114,6 +115,7 @@ std::vector<JavaScriptFeature*> GetBuiltInJavaScriptFeatures(
       GetScrollHelperJavaScriptFeature(),
       GetWindowErrorJavaScriptFeature(),
       NavigationJavaScriptFeature::GetInstance(),
+      WebPerformanceMetricsJavaScriptFeature::GetInstance(),
       ScriptCommandJavaScriptFeature::GetInstance(),
       SessionRestoreJavaScriptFeature::FromBrowserState(browser_state),
       TextFragmentsJavaScriptFeature::GetInstance(),
