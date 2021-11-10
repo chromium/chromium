4969712e009b5a20b402bd1d54c3f67be80390d5
Christoph Schwering <schwering@google.com>
2021-11-10 17:10:44 +0000

    [Autofill] Added WebView flag for AutofillDisplaceRemovedForms.
    Bug: 1215333, 1215337
    Change-Id: I199fd4043a12801ee1831ee21b4087b80614dbb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268923
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940361}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -163,6 +163,8 @@ public final class ProductionSupportedFlagList {
                             + "interact with a payment form."),
             Flag.baseFeature(AutofillFeatures.AUTOFILL_ENABLE_AUGMENTED_PHONE_COUNTRY_CODE,
                     "Enables support for phone code number fields with additional text."),
+            Flag.baseFeature(AutofillFeatures.AUTOFILL_DISPLACE_REMOVED_FORMS,
+                    "Fixes memory leaks in the renderer- and browser-form caches."),
             Flag.baseFeature(AutofillFeatures.AUTOFILL_USE_UNASSOCIATED_LISTED_ELEMENTS,
                     "Caches unowned listed elements in the document."),
             Flag.baseFeature(FeatureConstants.KEYBOARD_ACCESSORY_PAYMENT_VIRTUAL_CARD_FEATURE,
