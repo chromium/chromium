f88a597f3668ad7c1f756e0ab7c3a4ed99856731
Hailey Wang <haileywang@google.com>
2021-11-10 23:15:07 +0000

    [Snooze IPH] Add metrics for showing snooze
    (cherry picked from commit 19cf76ca490fe7aa35cf6105e614fbe86c5d790f)
    
    Bug: 1238902
    Change-Id: Ib8ba6829a38ac07d90c2547f441f16d467d14ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262542
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#939004}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272347
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#75}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- components/feature_engagement/internal/stats.cc
+++ components/feature_engagement/internal/stats.cc
@@ -123,6 +123,8 @@ void RecordShouldTriggerHelpUI(const base::Feature& feature,
     action_name.append(".");
     action_name.append(feature.name);
     base::RecordComputedAction(action_name);
+    base::UmaHistogramBoolean("InProductHelp.TextBubble.ShowSnooze",
+                              result.should_show_snooze);
   } else {
     LogTriggerHelpUIResult(name, TriggerHelpUIResult::FAILURE);
     std::string action_name =
