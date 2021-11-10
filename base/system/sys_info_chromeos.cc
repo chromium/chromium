7409c87fe3a76c571079ec3311d603ff8055cbbc
Toby Huang <tobyhuang@chromium.org>
2021-11-10 01:31:54 +0000

    Revert "Query spaced for free/total disk space"
    This reverts commit 5082d819e3110642e0aa0ccf8dfa730820db855a.
    
    Reason for revert: Suspected blocking LKGM uprev crrev.com/c/3269965.
    
    Original change's description:
    > Query spaced for free/total disk space
    >
    > On ChromeOS, spaced is the central source-of-truth for free
    > disk space information. Spaced accounts for the available
    > extents on the underlying thinpool on thinly provisioned filesystems
    > so that clients can accurately know the true amount of disk space
    > available on the device.
    >
    > BUG=b:177966496
    > TEST=test Storage Management page on ChromeOS reflects the correct
    > values.
    > TEST=unittests
    >
    > Change-Id: Iabe6f7a704813d90deb3cfd6886b25f541c49ceb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171506
    > Commit-Queue: Sarthak Kukreti <sarthakkukreti@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#939202}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b:177966496,b:205741709
    Change-Id: I825d1299b0eaf9dd0d71922c4cd73af1756048a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270705
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940111}

--- base/system/sys_info_chromeos.cc
+++ base/system/sys_info_chromeos.cc
@@ -8,8 +8,6 @@
 #include <stdint.h>
 #include <sys/utsname.h>
 
-#include "base/callback.h"
-#include "base/command_line.h"
 #include "base/cxx17_backports.h"
 #include "base/environment.h"
 #include "base/files/file.h"
@@ -17,7 +15,6 @@
 #include "base/files/file_util.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
-#include "base/process/launch.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -48,10 +45,6 @@ const char kLsbReleaseTimeKey[] = "LSB_RELEASE_TIME";  // Seconds since epoch
 const char kLsbReleaseSourceKey[] = "lsb-release";
 const char kLsbReleaseSourceEnv[] = "env";
 const char kLsbReleaseSourceFile[] = "file";
-
-const char kSpacedCliPath[] = "/usr/sbin/spaced_cli";
-const char kSpacedGetFreeDiskSpaceAction[] = "get_free_disk_space";
-const char kSpacedGetTotalDiskSpaceAction[] = "get_total_disk_space";
 
 class ChromeOSVersionInfo {
  public:
@@ -175,28 +168,6 @@ ChromeOSVersionInfo& GetChromeOSVersionInfo() {
   return *version_info;
 }
 
-SysInfo::GetAppOutputCallback* g_chromeos_get_app_output_for_test = nullptr;
-
-int64_t GetInfoFromSpaced(StringPiece action, const base::FilePath& path) {
-  CommandLine command((base::FilePath(kSpacedCliPath)));
-  command.AppendSwitchPath(action, path);
-
-  std::string output;
-  bool ret;
-  if (g_chromeos_get_app_output_for_test) {
-    ret = g_chromeos_get_app_output_for_test->Run(command, &output);
-  } else {
-    ret = GetAppOutput(command, &output);
-  }
-
-  int64_t result;
-  if (!ret || !StringToInt64(output, &result) || result < 0) {
-    return -1;
-  }
-
-  return result;
-}
-
 }  // namespace
 
 // static
@@ -278,11 +249,6 @@ void SysInfo::ResetChromeOSVersionInfoForTest() {
 }
 
 // static
-void SysInfo::SetChromeOSGetAppOutputForTest(GetAppOutputCallback* callback) {
-  g_chromeos_get_app_output_for_test = callback;
-}
-
-// static
 void SysInfo::CrashIfChromeOSNonTestImage() {
   if (!IsRunningOnChromeOS())
     return;
@@ -297,16 +263,6 @@ void SysInfo::CrashIfChromeOSNonTestImage() {
 
   // Crash if can't find test-image marker in the release track.
   CHECK_NE(track.find(kTestImageRelease), std::string::npos);
-}
-
-// static
-int64_t SysInfo::GetFreeDiskSpaceFromSpaced(const base::FilePath& path) {
-  return GetInfoFromSpaced(kSpacedGetFreeDiskSpaceAction, path);
-}
-
-// static
-int64_t SysInfo::GetTotalDiskSpaceFromSpaced(const base::FilePath& path) {
-  return GetInfoFromSpaced(kSpacedGetTotalDiskSpaceAction, path);
 }
 
 }  // namespace base
