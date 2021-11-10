cf34c819c4b7675c6f0a4da4f0708a3322e3d527
Alexander Timin <altimin@chromium.org>
2021-11-10 00:40:33 +0000

    Add TRACE_EVENT_INSTANT for incoming sync flow events
    Emit trace event when we are done handling sync flow event to ensure that the
    entire flow of handling the sync event is displayed.
    
    R=rockot@google.com
    BUG=1043616,b/204071815
    
    Change-Id: I13aad6e7a5f858e75964a7453c7dc84512062d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3269206
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940083}

--- mojo/public/cpp/bindings/lib/send_message_helper.cc
+++ mojo/public/cpp/bindings/lib/send_message_helper.cc
@@ -13,17 +13,39 @@ namespace mojo {
 namespace internal {
 
 void SendMessage(MessageReceiver& receiver, Message& message) {
+  uint64_t flow_id = message.GetTraceId();
+  bool is_sync_non_response = message.has_flag(Message::kFlagIsSync) &&
+                              !message.has_flag(Message::kFlagIsResponse);
   TRACE_EVENT_INSTANT("toplevel.flow", "Send mojo message",
-                      perfetto::Flow::Global(message.GetTraceId()));
+                      perfetto::Flow::Global(flow_id));
+
   ignore_result(receiver.Accept(&message));
+
+  // If this is a sync message which has received just received a reply, connect
+  // the point which received the sync reply (us) to the flow.
+  if (is_sync_non_response) {
+    TRACE_EVENT_INSTANT("toplevel.flow", "Receive mojo sync reply",
+                        perfetto::Flow::Global(flow_id));
+  }
 }
 
 void SendMessage(MessageReceiverWithResponder& receiver,
                  Message& message,
                  std::unique_ptr<MessageReceiver> responder) {
+  uint64_t flow_id = message.GetTraceId();
+  bool is_sync_non_response = message.has_flag(Message::kFlagIsSync) &&
+                              !message.has_flag(Message::kFlagIsResponse);
   TRACE_EVENT_INSTANT("toplevel.flow", "Send mojo message",
-                      perfetto::Flow::Global(message.GetTraceId()));
+                      perfetto::Flow::Global(flow_id));
+
   ignore_result(receiver.AcceptWithResponder(&message, std::move(responder)));
+
+  // If this is a sync message which has received just received a reply, connect
+  // the point which received the sync reply (us) to the flow.
+  if (is_sync_non_response) {
+    TRACE_EVENT_INSTANT("toplevel.flow", "Receive mojo sync reply",
+                        perfetto::Flow::Global(flow_id));
+  }
 }
 
 }  // namespace internal
