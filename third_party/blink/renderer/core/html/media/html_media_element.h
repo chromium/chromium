f362e1c968aabc68ec01185d451e5dccd5fcc438
Henrik Boström <hbos@chromium.org>
2021-11-11 18:25:53 +0000

    [Merge-97] HTMLMediaElement: Change repeating timers to WebRtcTimer.
    Part of go/metronome-task-queue-implementation.
    
    HTMLMediaElement has several timers. Some of them are only used to do
    StartOneShot() without delay (which is basically just like a PostTask),
    but progress_event_timer_ and playback_progress_timer_ are both used
    for repeating tasks.
    
    The repeated tasks happen at 250 ms and 350 ms intervals and do not
    require high timer precision, but this still potentially adds 6.9 Hz
    Idle Wake Ups. If you have 50 video tags that's ~350 Hz.
    
    By changing these repeating timers to WebRtcTimer instead this will
    allow coalescing the repeated task to get executed on the WebRTC
    metronome instead (15.6 ms precision) if the metronome is available,
    completely eliminating any Idle Wake Ups caused by the video tags.
    
    Note that the WebRTC metronome is currently behind flag. When the
    metronome isn't available, the WebRtcTimer acts like a normal timer and
    should result in no change in behavior - same Idle Wake Up and timer
    precision as before.
    
    (cherry picked from commit 0ce8d0c57b5ffbdc6cca0f6cc7c979f7b89d6bf8)
    
    Bug: chromium:1253787
    Change-Id: Ie89857b9a361ce827c368e2a221c409adb7e8212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259865
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#940230}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275692
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#98}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- third_party/blink/renderer/core/html/media/html_media_element.h
+++ third_party/blink/renderer/core/html/media/html_media_element.h
@@ -57,6 +57,8 @@
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
+#include "third_party/webrtc_overrides/metronome_provider.h"
+#include "third_party/webrtc_overrides/webrtc_timer.h"
 
 namespace cc {
 class Layer;
@@ -457,6 +459,9 @@ class CORE_EXPORT HTMLMediaElement
   void ContextLifecycleStateChanged(mojom::FrameLifecycleState) override;
   void ContextDestroyed() override;
 
+  // Gets the current context's metronome provider.
+  scoped_refptr<MetronomeProvider> GetExecutionContextMetronomeProvider() const;
+
   virtual void OnPlay() {}
   virtual void OnLoadStarted() {}
   virtual void OnLoadFinished() {}
@@ -545,8 +550,8 @@ class CORE_EXPORT HTMLMediaElement
   void SuspendForFrameClosed() override;
 
   void LoadTimerFired(TimerBase*);
-  void ProgressEventTimerFired(TimerBase*);
-  void PlaybackProgressTimerFired(TimerBase*);
+  void ProgressEventTimerFired();
+  void PlaybackProgressTimerFired();
   void ScheduleTimeupdateEvent(bool periodic_event);
   void StartPlaybackProgressTimer();
   void StartProgressEventTimer();
@@ -667,11 +672,17 @@ class CORE_EXPORT HTMLMediaElement
   mojo::PendingAssociatedReceiver<media::mojom::blink::MediaPlayerObserver>
   AddMediaPlayerObserverAndPassReceiver();
 
+  // Timers used to schedule one-shot tasks with no delay.
   HeapTaskRunnerTimer<HTMLMediaElement> load_timer_;
-  HeapTaskRunnerTimer<HTMLMediaElement> progress_event_timer_;
-  HeapTaskRunnerTimer<HTMLMediaElement> playback_progress_timer_;
   HeapTaskRunnerTimer<HTMLMediaElement> audio_tracks_timer_;
   HeapTaskRunnerTimer<HTMLMediaElement> removed_from_document_timer_;
+  // Timers used to schedule repeating tasks. For this a lower precision timer,
+  // WebRtcTimer, is used. This may reduce Idle Wake Ups when WebRTC is used.
+  // TODO(https://crbug.com/1267874): When there exists a low resolution timer
+  // in base/ that does not have a dependency on WebRTC, use such a timer
+  // instead.
+  WebRtcTimer progress_event_timer_;
+  WebRtcTimer playback_progress_timer_;
 
   Member<TimeRanges> played_time_ranges_;
   Member<EventQueue> async_event_queue_;
