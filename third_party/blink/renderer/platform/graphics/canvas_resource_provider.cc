04799fb423039246bc8da03411124940c4f1a819
Justin Novosad <junov@chromium.org>
2021-11-11 17:05:10 +0000

    Revert "Re-enable DMSAA on GPUs with slow MSAA for 2d canvas"
    This reverts commit 6587c399e9a68c1013243c09efdaaf95fce444f4.
    
    Reason for revert: Caused more regressions than it solved.
    BUG=1269156
    
    Original change's description:
    > Re-enable DMSAA on GPUs with slow MSAA for 2d canvas
    >
    > Perf regressions were recorded when we disable DMSAA on some GPUs with
    > this CL: https://bugs.chromium.org/p/chromium/issues/detail?id=1268448
    >
    > This is a partial revert
    >
    > BUG=1268600
    >
    > Change-Id: I7256d20837b89b55467f19c8d875bf7b3e4d94ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272697
    > Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    > Commit-Queue: Justin Novosad <junov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#940453}
    
    TBR=junov@chromium.org,juanmihd@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I11c7cfb62d70c4c46587fdfebf13aeb2caf5da95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1268600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276552
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940797}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1192,9 +1192,7 @@ CanvasResourceProvider::CanvasResourceProvider(
     context_provider_wrapper_->AddObserver(this);
     const auto& caps =
         context_provider_wrapper_->ContextProvider()->GetCapabilities();
-    // TODO(crbug.com/1268600): It seems that DMSAA might still be worthwhile
-    // on GPUs with slow MSAA.  Let's Monitor perf and revise if needed.
-    oopr_uses_dmsaa_ = /*!caps.msaa_is_slow &&*/ !caps.avoid_stencil_buffers;
+    oopr_uses_dmsaa_ = !caps.msaa_is_slow && !caps.avoid_stencil_buffers;
   }
   CanvasMemoryDumpProvider::Instance()->RegisterClient(this);
 }
