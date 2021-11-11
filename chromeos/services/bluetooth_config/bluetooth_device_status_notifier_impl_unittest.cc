d60f5fb270d538a123a6bc22b4f2059fae23f879
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-11-11 21:36:44 +0000

    [CrOS Bluetooth] Add device status notifier
    This class supplies properties about the current Bluetooth device status
    to observers. This class notifies observers when bluetooth devices
    status has changed. The status include, pairing a new device, connecting
    to a device, and disconnecting from a device. For this CL, we will only
    implement notifying when pairing to a new device occurs.
    
    In this CL, BluetoothDeviceStatusObserver only supplies a single
    function (OnDevicePaired), but future CLs will add more
    properties. Note that BluetoothDeviceStatusObserver is only used by
    unit tests in this CL; a future CL will instantiate it when the
    kBluetoothRevamp flag is enabled.
    
    Bug: 1010321
    Change-Id: Id417b186232dd75db87c420e351246cd41a72ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271497
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#940924}

---
+++ chromeos/services/bluetooth_config/bluetooth_device_status_notifier_impl_unittest.cc
@@ -0,0 +1,149 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/bluetooth_config/bluetooth_device_status_notifier_impl.h"
+
+#include <memory>
+#include <vector>
+
+#include "base/run_loop.h"
+#include "base/test/task_environment.h"
+#include "chromeos/services/bluetooth_config/fake_adapter_state_controller.h"
+#include "chromeos/services/bluetooth_config/fake_bluetooth_device_status_observer.h"
+#include "chromeos/services/bluetooth_config/fake_device_cache.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace bluetooth_config {
+namespace {
+
+mojom::PairedBluetoothDevicePropertiesPtr GenerateStubPairedDeviceProperties(
+    std::string id) {
+  auto device_properties = mojom::BluetoothDeviceProperties::New();
+  device_properties->id = id;
+  device_properties->public_name = u"name";
+  device_properties->device_type = mojom::DeviceType::kUnknown;
+  device_properties->audio_capability =
+      mojom::AudioOutputCapability::kNotCapableOfAudioOutput;
+  device_properties->connection_state =
+      mojom::DeviceConnectionState::kNotConnected;
+
+  mojom::PairedBluetoothDevicePropertiesPtr paired_properties =
+      mojom::PairedBluetoothDeviceProperties::New();
+  paired_properties->device_properties = std::move(device_properties);
+  return paired_properties;
+}
+
+}  // namespace
+
+class BluetoothDeviceStatusNotifierImplTest : public testing::Test {
+ protected:
+  BluetoothDeviceStatusNotifierImplTest() = default;
+  BluetoothDeviceStatusNotifierImplTest(
+      const BluetoothDeviceStatusNotifierImplTest&) = delete;
+  BluetoothDeviceStatusNotifierImplTest& operator=(
+      const BluetoothDeviceStatusNotifierImplTest&) = delete;
+  ~BluetoothDeviceStatusNotifierImplTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {
+    device_status_notifier_ =
+        std::make_unique<BluetoothDeviceStatusNotifierImpl>(
+            &fake_device_cache_);
+  }
+
+  void SetPairedDevices(
+      const std::vector<mojom::PairedBluetoothDevicePropertiesPtr>&
+          paired_devices) {
+    std::vector<mojom::PairedBluetoothDevicePropertiesPtr> copy;
+    for (const auto& paired_device : paired_devices)
+      copy.push_back(paired_device.Clone());
+
+    fake_device_cache_.SetPairedDevices(std::move(copy));
+    device_status_notifier_->FlushForTesting();
+  }
+
+  std::unique_ptr<FakeBluetoothDeviceStatusObserver> Observe() {
+    auto observer = std::make_unique<FakeBluetoothDeviceStatusObserver>();
+    device_status_notifier_->ObserveDeviceStatusChanges(
+        observer->GeneratePendingRemote());
+    device_status_notifier_->FlushForTesting();
+    return observer;
+  }
+
+ private:
+  base::test::TaskEnvironment task_environment_;
+
+  FakeAdapterStateController fake_adapter_state_controller_;
+  FakeDeviceCache fake_device_cache_{&fake_adapter_state_controller_};
+  std::unique_ptr<BluetoothDeviceStatusNotifierImpl> device_status_notifier_;
+};
+
+TEST_F(BluetoothDeviceStatusNotifierImplTest, PairedDevicesChanges) {
+  std::unique_ptr<FakeBluetoothDeviceStatusObserver> observer = Observe();
+
+  // Initially, observer would receive no updates.
+  ASSERT_EQ(0u, observer->paired_device_properties_list().size());
+  EXPECT_TRUE(observer->paired_device_properties_list().empty());
+
+  std::vector<mojom::PairedBluetoothDevicePropertiesPtr> paired_devices;
+  paired_devices.push_back(GenerateStubPairedDeviceProperties("id1"));
+
+  // Add a paired device and verify that the observer was notified.
+  SetPairedDevices(paired_devices);
+  ASSERT_EQ(1u, observer->paired_device_properties_list().size());
+  ASSERT_EQ(
+      "id1",
+      observer->paired_device_properties_list()[0]->device_properties->id);
+
+  paired_devices.push_back(GenerateStubPairedDeviceProperties("id2"));
+  paired_devices.push_back(GenerateStubPairedDeviceProperties("id3"));
+
+  // Add two paired devices and verify that the observer was notified.
+  SetPairedDevices(paired_devices);
+  ASSERT_EQ(3u, observer->paired_device_properties_list().size());
+  ASSERT_EQ(
+      "id2",
+      observer->paired_device_properties_list()[1]->device_properties->id);
+  ASSERT_EQ(
+      "id3",
+      observer->paired_device_properties_list()[2]->device_properties->id);
+
+  // Simulate device being unpaired, the observer should not be called.
+  paired_devices.pop_back();
+  SetPairedDevices(paired_devices);
+  ASSERT_EQ(3u, observer->paired_device_properties_list().size());
+
+  // Add the same device again.
+  paired_devices.push_back(GenerateStubPairedDeviceProperties("id3"));
+
+  // Verify that the observer was notified.
+  SetPairedDevices(paired_devices);
+  ASSERT_EQ(4u, observer->paired_device_properties_list().size());
+  ASSERT_EQ(
+      "id3",
+      observer->paired_device_properties_list()[3]->device_properties->id);
+}
+
+TEST_F(BluetoothDeviceStatusNotifierImplTest, DisconnectToStopObserving) {
+  std::unique_ptr<FakeBluetoothDeviceStatusObserver> observer = Observe();
+
+  // Initially, observer would receive no updates.
+  ASSERT_EQ(0u, observer->paired_device_properties_list().size());
+  EXPECT_TRUE(observer->paired_device_properties_list().empty());
+
+  // Disconnect the Mojo pipe; this should stop observing.
+  observer->DisconnectMojoPipe();
+
+  // Add a paired device; the observer should not be notified since it
+  // is no longer connected.
+  std::vector<mojom::PairedBluetoothDevicePropertiesPtr> paired_devices;
+  paired_devices.push_back(GenerateStubPairedDeviceProperties("id1"));
+  EXPECT_EQ(0u, observer->paired_device_properties_list().size());
+}
+
+}  // namespace bluetooth_config
+}  // namespace chromeos
