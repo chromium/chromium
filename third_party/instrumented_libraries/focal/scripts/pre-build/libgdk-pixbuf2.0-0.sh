aeaf99de3cdca50a54ac12f6b33fdf74beed7139
Tom Anderson <thomasanderson@chromium.org>
2021-11-12 03:25:30 +0000

    [Instrumented libraries] Fix libgdk-pixbuf2.0-0 build for Focal
    R=thestig
    
    Bug: 1260217
    Change-Id: I844ba0761192d5131077bcbafccb9fe8568d701c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277059
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941050}

--- third_party/instrumented_libraries/focal/scripts/pre-build/libgdk-pixbuf2.0-0.sh
+++ third_party/instrumented_libraries/focal/scripts/pre-build/libgdk-pixbuf2.0-0.sh
@@ -9,5 +9,13 @@
 # just-built one is used, however in MSan builds it will crash due to
 # uninstrumented dependencies.
 
-sed -i "s|\$(top_builddir)/gdk-pixbuf/gdk-pixbuf-query-loaders|/usr/bin/gdk-pixbuf-query-loaders|g" gdk-pixbuf/Makefile.am
-autoreconf
+sed -i "s|gdk_pixbuf_query_loaders,|'/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/gdk-pixbuf-query-loaders',|g" \
+	gdk-pixbuf/meson.build
+
+# gdk-pixbuf-print-mime-types requires instrumented libgio, which is unavailabe
+# during the build.  It's also not installed in /usr/bin, so we must build
+# an uninstrumented version ourselves and patch the build file to use that.
+gcc thumbnailer/gdk-pixbuf-print-mime-types.c -o gdk-pixbuf-print-mime-types \
+	$(pkg-config gdk-pixbuf-2.0 --libs --cflags)
+sed -i "s|gdk_pixbuf_print_mime_types.full_path()|'../gdk-pixbuf-print-mime-types'|g" \
+	thumbnailer/meson.build
