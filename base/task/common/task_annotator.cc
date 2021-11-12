9c2a418b17151a830b7bb84d2969fc1de77d8c07
Scott Haseley <shaseley@chromium.org>
2021-11-12 23:53:19 +0000

    [base/task] Remove dead TaskAnnotator code
    The idea of a dummy task for TaskAnnotator was introduced in
    crrev.com/c/1590247 but later removed in crrev.com/c/2345673, however a
    few bits remain. This removes IsDummyTask() and reverts
    CurrentTaskForThread() back to its previous implementation.
    
    Bug: None
    Change-Id: I0a79b1419b1022b1a9decc0ed3770f20870db204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278668
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941383}

--- base/task/common/task_annotator.cc
+++ base/task/common/task_annotator.cc
@@ -27,10 +27,6 @@ namespace {
 
 TaskAnnotator::ObserverForTesting* g_task_annotator_observer = nullptr;
 
-// Used as a sentinel to determine if a TLS-stored PendingTask is a dummy one.
-static constexpr int kSentinelSequenceNum =
-    static_cast<int>(0xF00DBAADF00DBAAD);
-
 // Returns the TLS slot that stores the PendingTask currently in progress on
 // each thread. Used to allow creating a breadcrumb of program counters on the
 // stack to help identify a task's origin in crashes.
@@ -50,28 +46,10 @@ GetTLSForCurrentScopedIpcHash() {
   return instance.get();
 }
 
-// Determines whether or not the given |task| is a dummy pending task that has
-// been injected by ScopedSetIpcHash solely for the purposes of
-// tracking IPC context.
-bool IsDummyPendingTask(const PendingTask* task) {
-  if (task->sequence_num == kSentinelSequenceNum &&
-      !task->posted_from.has_source_info() &&
-      !task->posted_from.program_counter()) {
-    return true;
-  }
-  return false;
-}
-
 }  // namespace
 
 const PendingTask* TaskAnnotator::CurrentTaskForThread() {
-  auto* current_task = GetTLSForCurrentPendingTask()->Get();
-
-  // Don't return "dummy" current tasks that are only used for storing IPC
-  // context.
-  if (!current_task || (current_task && IsDummyPendingTask(current_task)))
-    return nullptr;
-  return current_task;
+  return GetTLSForCurrentPendingTask()->Get();
 }
 
 TaskAnnotator::TaskAnnotator() = default;
