2c5cef7dd1ae58e6ad87585bff904a371eac5a2a
Dale Curtis <dalecurtis@chromium.org>
2021-11-12 03:23:05 +0000

    [WebCodecs] Fix inactivity timer.
    The current code does the opposite of what was intended, it marks
    codecs inactive if they've been used more frequently than the
    inactivity period -- this changes this and adds a test.
    
    Additionally, some RDP clients are only ticking at 1 frame/minute
    (on screen clock updating), so bump the inactivity timer to 1.5
    minutes. With pressure based collection this shouldn't be an issue.
    
    R=chcunningham
    
    Fixed: 1269397
    Change-Id: I8e8d870da25e5a14f086f66589ba2992cc4c0294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276716
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941049}

--- third_party/blink/renderer/modules/webcodecs/reclaimable_codec.h
+++ third_party/blink/renderer/modules/webcodecs/reclaimable_codec.h
@@ -9,6 +9,10 @@
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/timer.h"
 
+namespace base {
+class TickClock;
+}  // namespace base
+
 namespace blink {
 
 class DOMException;
@@ -25,6 +29,17 @@ class MODULES_EXPORT ReclaimableCodec : public GarbageCollectedMixin {
   }
 
   void SimulateCodecReclaimedForTesting();
+  void SimulateActivityTimerFiredForTesting();
+
+  void set_tick_clock_for_testing(const base::TickClock* clock) {
+    tick_clock_ = clock;
+  }
+
+  // Use 1.5 minutes since some RDP clients are only ticking at 1 FPM.
+  static constexpr base::TimeDelta kInactivityReclamationThreshold =
+      base::Seconds(90);
+  static constexpr base::TimeDelta kTimerPeriod =
+      kInactivityReclamationThreshold / 2;
 
  protected:
   // Pushes back the time at which |this| can be reclaimed due to inactivity.
@@ -47,6 +62,8 @@ class MODULES_EXPORT ReclaimableCodec : public GarbageCollectedMixin {
   // timer, before we reclaim for inactivity. This prevents immediately
   // reclaiming otherwise active codecs, right after a page suspended/resumed.
   bool last_tick_was_inactive_ = false;
+
+  const base::TickClock* tick_clock_;
 
   base::TimeTicks last_activity_;
   HeapTaskRunnerTimer<ReclaimableCodec> activity_timer_;
--- third_party/blink/renderer/modules/webcodecs/reclaimable_codec_test.cc
+++ third_party/blink/renderer/modules/webcodecs/reclaimable_codec_test.cc
@@ -4,9 +4,11 @@
 
 #include "third_party/blink/renderer/modules/webcodecs/reclaimable_codec.h"
 
+#include "base/test/simple_test_tick_clock.h"
+#include "base/time/default_tick_clock.h"
 #include "media/base/test_helpers.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
@@ -17,16 +19,24 @@ class FakeReclaimableCodec final
     : public GarbageCollected<FakeReclaimableCodec>,
       public ReclaimableCodec {
  public:
-  void SimulateActivity() { MarkCodecActive(); }
+  void SimulateActivity() {
+    MarkCodecActive();
+    reclaimed_ = false;
+  }
 
   void SimulateReset() { PauseCodecReclamation(); }
 
-  void OnCodecReclaimed(DOMException* ex) final {}
+  void OnCodecReclaimed(DOMException* ex) final { reclaimed_ = true; }
 
   // GarbageCollected override.
   void Trace(Visitor* visitor) const override {
     ReclaimableCodec::Trace(visitor);
   }
+
+  bool reclaimed() const { return reclaimed_; }
+
+ private:
+  bool reclaimed_ = false;
 };
 
 }  // namespace
@@ -61,6 +71,40 @@ TEST_F(ReclaimableCodecTest, InactivityTimerStartStops) {
   // It should be possible to restart the timer after stopping it.
   codec->SimulateActivity();
   EXPECT_TRUE(codec->IsReclamationTimerActiveForTesting());
+}
+
+TEST_F(ReclaimableCodecTest, InactivityTimerWorks) {
+  auto* codec = MakeGarbageCollected<FakeReclaimableCodec>();
+
+  // Codecs should not be reclaimable for inactivity until first activity.
+  EXPECT_FALSE(codec->IsReclamationTimerActiveForTesting());
+
+  base::SimpleTestTickClock tick_clock;
+  codec->set_tick_clock_for_testing(&tick_clock);
+
+  // Activity should start the timer.
+  codec->SimulateActivity();
+  EXPECT_TRUE(codec->IsReclamationTimerActiveForTesting());
+  EXPECT_FALSE(codec->reclaimed());
+
+  // Fire when codec is fresh to ensure first tick isn't treated as idle.
+  codec->SimulateActivityTimerFiredForTesting();
+  EXPECT_FALSE(codec->reclaimed());
+
+  // One timer period should not be enough to reclaim the codec.
+  tick_clock.Advance(ReclaimableCodec::kTimerPeriod);
+  codec->SimulateActivityTimerFiredForTesting();
+  EXPECT_FALSE(codec->reclaimed());
+
+  // Advancing an additional timer period should be enough to trigger
+  // reclamation.
+  tick_clock.Advance(ReclaimableCodec::kTimerPeriod);
+  codec->SimulateActivityTimerFiredForTesting();
+  EXPECT_TRUE(codec->reclaimed());
+
+  // Restore default tick clock since |codec| is a garbage collected object that
+  // may outlive the scope of this function.
+  codec->set_tick_clock_for_testing(base::DefaultTickClock::GetInstance());
 }
 
 }  // namespace blink
