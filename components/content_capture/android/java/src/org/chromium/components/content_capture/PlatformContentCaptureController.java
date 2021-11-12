a18c3eef3d4d5b9719146d4e778afaf0b128eeaa
Michael Bai <michaelbai@chromium.org>
2021-11-12 19:24:52 +0000

    Suppress ContentCaptureManager.getServiceComponentName() exception
    Bug: 1269814
    Change-Id: If00d7fe71461bd34acd655a262a7a986f82ce4f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279200
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941254}

--- components/content_capture/android/java/src/org/chromium/components/content_capture/PlatformContentCaptureController.java
+++ components/content_capture/android/java/src/org/chromium/components/content_capture/PlatformContentCaptureController.java
@@ -65,7 +65,12 @@ public class PlatformContentCaptureController {
             return;
         }
 
-        ComponentName componentName = mContentCaptureManager.getServiceComponentName();
+        ComponentName componentName = null;
+        try {
+            componentName = mContentCaptureManager.getServiceComponentName();
+        } catch (RuntimeException e) {
+            Log.e(TAG, "Error to get component name", e);
+        }
         if (componentName == null) {
             log("Service isn't available.");
             return;
