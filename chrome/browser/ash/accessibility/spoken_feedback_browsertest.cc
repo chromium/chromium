2043d5e72b649fb0ac7fa8b5f2705f75e303d1ee
Sergey Poromov <poromov@chromium.org>
2021-11-12 12:15:27 +0000

    [Sheriff] Disable ChromeVoxStickyModeRawKeys flaky failing on ChromiumOS ASan
    Sample run:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/40903/overview
    
    Bug: 1182542
    Change-Id: I8156d2e5994e6cb3a768b42cc642d48e6a00317d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275503
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941131}

--- chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
@@ -855,8 +855,9 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, ChromeVoxStickyMode) {
 // sending js commands above. This variant may be subject to flakes as it
 // depends on more of the UI events stack and sticky mode invocation has a
 // timing element to it.
-// Consistently failing on ChromiumOS MSan. http://crbug.com/1182542
-#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+// Consistently failing on ChromiumOS MSan and ASan. http://crbug.com/1182542
+#if defined(OS_CHROMEOS) && \
+    (defined(MEMORY_SANITIZER) || defined(ADDRESS_SANITIZER))
 #define MAYBE_ChromeVoxStickyModeRawKeys DISABLED_ChromeVoxStickyModeRawKeys
 #else
 #define MAYBE_ChromeVoxStickyModeRawKeys ChromeVoxStickyModeRawKeys
