3ad6729cce2847253bda057f741bdecf7a5e8dfb
Alexei Svitkine <asvitkine@chromium.org>
2021-11-12 20:29:50 +0000

    Update callsites to use WIN_-prefixed base::File flags.
    The flags have been renamed to make it clear that they only
    apply to Windows (see associated bug).
    
    If you're reviewing this CL, please verify your code's usage
    of the flag to ensure that the behavior is as intended. Thanks!
    
    This CL was uploaded by git cl split.
    
    R=pbos@chromium.org
    
    Bug: 1244149
    Change-Id: I04708d8bef3a21750b45fc1ca5730228b83d8b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278398
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941299}

--- ui/base/resource/data_pack.cc
+++ ui/base/resource/data_pack.cc
@@ -285,8 +285,8 @@ bool DataPack::LoadFromPath(const base::FilePath& path) {
   // Open the file for reading; allowing other consumers to also open it for
   // reading and deleting. Do not allow others to write to it.
   base::File data_file(path, base::File::FLAG_OPEN | base::File::FLAG_READ |
-                                 base::File::FLAG_EXCLUSIVE_WRITE |
-                                 base::File::FLAG_SHARE_DELETE);
+                                 base::File::FLAG_WIN_EXCLUSIVE_WRITE |
+                                 base::File::FLAG_WIN_SHARE_DELETE);
   if (!data_file.IsValid()) {
     DLOG(ERROR) << "Failed to open datapack with base::File::Error "
                 << data_file.error_details();
