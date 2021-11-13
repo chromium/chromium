76e09a6fd0770671bdf50e524b94d2ee193c4728
Katie Dektar <katie@chromium.org>
2021-11-13 00:55:24 +0000

    [Sheriff] Disable test flaking on Lacros.
    ReferrerPolicyTest.ContextMenuOrigin has had many recent flakes
    on Lacros, including linux-lacros-rel, linux-lacros-tester-rel,
    linux-lacros-rel-rts.
    
    ReferrerPolicyTest.HttpsContextMenuRedirect is also flaky.
    
    Bug: 1269942
    Change-Id: I12e58be95f2e67de5c3be699ea939ac257285303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279618
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941392}

--- chrome/browser/referrer_policy_browsertest.cc
+++ chrome/browser/referrer_policy_browsertest.cc
@@ -407,7 +407,13 @@ IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, HttpsMiddleClickTargetBlankOrigin) {
 }
 
 // Context menu, from HTTP to HTTP.
-IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, ContextMenuOrigin) {
+// TODO(crbug.com/1269942): Flaky on Lacros.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_ContextMenuOrigin DISABLED_ContextMenuOrigin
+#else
+#define MAYBE_ContextMenuOrigin ContextMenuOrigin
+#endif
+IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, MAYBE_ContextMenuOrigin) {
   ContextMenuNotificationObserver context_menu_observer(
       IDC_CONTENT_CONTEXT_OPENLINKNEWTAB);
   RunReferrerTest(
@@ -543,7 +549,13 @@ IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, MAYBE_ContextMenuRedirect) {
 }
 
 // Context menu, from HTTPS to HTTP via server redirect.
-IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, HttpsContextMenuRedirect) {
+// TODO(crbug.com/1269942): Flaky on Lacros.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_HttpsContextMenuRedirect DISABLED_HttpsContextMenuRedirect
+#else
+#define MAYBE_HttpsContextMenuRedirect HttpsContextMenuRedirect
+#endif
+IN_PROC_BROWSER_TEST_F(ReferrerPolicyTest, MAYBE_HttpsContextMenuRedirect) {
   ContextMenuNotificationObserver context_menu_observer(
       IDC_CONTENT_CONTEXT_OPENLINKNEWTAB);
   RunReferrerTest(network::mojom::ReferrerPolicy::kOrigin, START_ON_HTTPS,
