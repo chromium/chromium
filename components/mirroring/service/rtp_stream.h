3400994958c70cc10e8430e89b8bd7d2d5af618a
Jordan Bayles <jophba@chromium.org>
2021-11-15 07:51:33 +0000

    [Cast Streaming] Remove maximum refresh frame count
    Currently, the Chrome sender for cast streaming only sends refresh
    frames for a total of ten seconds. This patch removes that limit and
    sends refresh frames indefinitely.
    
    Bug: 1183485
    Change-Id: Ifc465c6afd41f07721a100c4e296a622335fdaac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279078
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941595}

--- components/mirroring/service/rtp_stream.h
+++ components/mirroring/service/rtp_stream.h
@@ -81,9 +81,6 @@ class COMPONENT_EXPORT(MIRRORING_SERVICE) VideoRtpStream
   // to a consecutive maximum.
   base::RepeatingTimer refresh_timer_;
 
-  // Counter for the number of consecutive "refresh frames" requested.
-  int consecutive_refresh_count_;
-
   // Set to true when a request for a refresh frame has been made.  This is
   // cleared once the next frame is received.
   bool expecting_a_refresh_frame_;
