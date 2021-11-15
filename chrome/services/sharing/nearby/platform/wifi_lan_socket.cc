a43d78c76674ed0ef90903699ebe8c38b77298aa
Josh Nohle <nohle@google.com>
2021-11-15 22:16:03 +0000

    [Nearby] Implement Nearby Connections's WLAN connected socket
    We implement the abstract Nearby Connections's class api::WifiLanSocket.
    The implementation is fairly trivial, essentially just controlling the
    lifetime of a network::mojom::TCPConnectedSocket and the corresponding
    input/output streams.
    
    This CL is part of the effot to add WLAN as a Nearby Connections
    transfer medium on Chrome OS. See http://go/cros-nearby-wlan for more
    details.
    
    Bug: 1261238
    Change-Id: If35f0847fece934e97faf3ae99751badfe0a5db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279600
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941843}

---
+++ chrome/services/sharing/nearby/platform/wifi_lan_socket.cc
@@ -0,0 +1,102 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/platform/wifi_lan_socket.h"
+
+#include "base/logging.h"
+#include "base/task/thread_pool.h"
+#include "chrome/services/sharing/nearby/platform/bidirectional_stream.h"
+
+namespace location {
+namespace nearby {
+namespace chrome {
+
+WifiLanSocket::ConnectedSocketParameters::ConnectedSocketParameters(
+    const net::IPEndPoint& remote_end_point,
+    mojo::PendingRemote<network::mojom::TCPConnectedSocket>
+        tcp_connected_socket,
+    mojo::ScopedDataPipeConsumerHandle receive_stream,
+    mojo::ScopedDataPipeProducerHandle send_stream)
+    : remote_end_point(remote_end_point),
+      tcp_connected_socket(std::move(tcp_connected_socket)),
+      receive_stream(std::move(receive_stream)),
+      send_stream(std::move(send_stream)) {}
+
+WifiLanSocket::ConnectedSocketParameters::~ConnectedSocketParameters() =
+    default;
+
+WifiLanSocket::ConnectedSocketParameters::ConnectedSocketParameters(
+    ConnectedSocketParameters&&) = default;
+
+WifiLanSocket::ConnectedSocketParameters&
+WifiLanSocket::ConnectedSocketParameters::operator=(
+    ConnectedSocketParameters&&) = default;
+
+WifiLanSocket::WifiLanSocket(
+    const net::IPEndPoint& remote_end_point,
+    mojo::PendingRemote<network::mojom::TCPConnectedSocket>
+        tcp_connected_socket,
+    mojo::ScopedDataPipeConsumerHandle receive_stream,
+    mojo::ScopedDataPipeProducerHandle send_stream)
+    : task_runner_(
+          base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()})),
+      tcp_connected_socket_(std::move(tcp_connected_socket), task_runner_),
+      bidirectional_stream_(connections::mojom::Medium::kWifiLan,
+                            task_runner_,
+                            std::move(receive_stream),
+                            std::move(send_stream)) {
+  tcp_connected_socket_.set_disconnect_handler(
+      base::BindOnce(&WifiLanSocket::OnTcpConnectedSocketDisconnected,
+                     base::Unretained(this)),
+      task_runner_);
+}
+
+WifiLanSocket::WifiLanSocket(
+    ConnectedSocketParameters connected_socket_parameters)
+    : WifiLanSocket(connected_socket_parameters.remote_end_point,
+                    std::move(connected_socket_parameters.tcp_connected_socket),
+                    std::move(connected_socket_parameters.receive_stream),
+                    std::move(connected_socket_parameters.send_stream)) {}
+
+WifiLanSocket::~WifiLanSocket() {
+  Close();
+}
+
+InputStream& WifiLanSocket::GetInputStream() {
+  DCHECK(bidirectional_stream_.GetInputStream());
+  return *bidirectional_stream_.GetInputStream();
+}
+
+OutputStream& WifiLanSocket::GetOutputStream() {
+  DCHECK(bidirectional_stream_.GetOutputStream());
+  return *bidirectional_stream_.GetOutputStream();
+}
+
+// Note: This is thread safe because we can call reset() on
+// |tcp_connected_socket_| as many times as we want, and
+// BidirectionalStream::Close() is thread safe.
+Exception WifiLanSocket::Close() {
+  if (tcp_connected_socket_) {
+    VLOG(1) << "WifiLanSocket::" << __func__
+            << ": Closing TCP connected socket.";
+    tcp_connected_socket_.reset();
+  }
+
+  return bidirectional_stream_.Close();
+}
+
+bool WifiLanSocket::IsClosed() const {
+  return !tcp_connected_socket_.is_bound();
+}
+
+void WifiLanSocket::OnTcpConnectedSocketDisconnected() {
+  LOG(WARNING) << "WifiLanSocket::" << __func__
+               << ": TCP connected socket unexpectedly disconnected. Closing "
+               << "WifiLanSocket.";
+  Close();
+}
+
+}  // namespace chrome
+}  // namespace nearby
+}  // namespace location
