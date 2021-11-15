4dcd19293d49cbaa0966ed4093492f68ee29188c
Lan Wei <lanwei@chromium.org>
2021-11-15 22:31:39 +0000

    Revert "[DevTools] Clarify console.dir test expectation"
    This reverts commit d3508b6c229d2917ab1c5d6865b3bdf4ad438a64.
    
    Reason for revert:
    
    http/tests/devtools/console/console-dir.js
    failed on Linux Tests (dbg)(1) [3 since first detection].
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29
    
    I cannot revert https://chromium-review.googlesource.com/c/chromium/src/+/3282147, there is some rebase issue.
    
    Original change's description:
    > [DevTools] Clarify console.dir test expectation
    >
    > The recent work on improved display of properties removed the need
    > for a restriction that prevented us from showing element properties
    > on large TypedArrays before. The new behavior is strictly better,
    > and the test content has been rewritten to clarify that this is now
    > the expected and desired behavior.
    >
    > https://developer.chrome.com/blog/new-in-devtools-95/#properties
    >
    > Bug: chromium:1076820, chromium:1119900, chromium:1203169
    > Change-Id: If04c9a36ea5b74b2223651a8b4342b2cd91848dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282291
    > Auto-Submit: Mathias Bynens <mathias@chromium.org>
    > Commit-Queue: Mathias Bynens <mathias@chromium.org>
    > Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#941731}
    
    TBR=bmeurer@chromium.org,mathias@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Iaa445eb777754dd3ed23fb574455d5baf6d0898a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1076820, chromium:1119900, chromium:1203169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282496
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Owners-Override: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941871}

--- third_party/blink/web_tests/http/tests/devtools/console/console-dir.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-dir.js
@@ -38,7 +38,7 @@
 
     // Test big typed array: should be no crash or timeout.
     var bigTypedArray = new Uint8Array(new ArrayBuffer(400 * 1000 * 1000));
-    bigTypedArray["PASS"] = "Non-element properties should show up as well.";
+    bigTypedArray["FAIL"] = "FAIL: Object.getOwnPropertyNames() should not have been run";
     console.dir(bigTypedArray);
 
     // document.createEvent("Event") has a special property "isTrusted" flagged "LegacyUnforgeable".
