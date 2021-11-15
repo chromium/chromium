97f543f6504c854a26f9af9536c2cd58775eadd8
Peter Kasting <pkasting@chromium.org>
2021-11-15 11:46:24 +0000

    Fix -Wshadow in third_party/android_crazy_linker/.
    Bug: 794619
    Change-Id: I4a7d6828445cc7a1ec04e19033f63babe4da3e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278474
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941638}

--- third_party/android_crazy_linker/src/tests/test_util.h
+++ third_party/android_crazy_linker/src/tests/test_util.h
@@ -381,11 +381,12 @@ struct RelroLibrary {
   crazy_library_t* library;
   RelroInfo relro;
 
-  void Init(const char* name, crazy_context_t* context) {
-    printf("Loading %s\n", name);
-    this->name = name;
-    if (!crazy_library_open(&this->library, name, context)) {
-      Panic("Could not open %s: %s\n", name, crazy_context_get_error(context));
+  void Init(const char* name_str, crazy_context_t* context) {
+    printf("Loading %s\n", name_str);
+    name = name_str;
+    if (!crazy_library_open(&this->library, name_str, context)) {
+      Panic("Could not open %s: %s\n", name_str,
+            crazy_context_get_error(context));
     }
   }
 
