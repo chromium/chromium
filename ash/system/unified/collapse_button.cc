5ff77005c8fe67c5fe63d3e26a7097295f1f6859
Toni Barzic <tbarzic@chromium.org>
2021-11-17 23:49:45 +0000

    Revert "dark_mode: Migrate CollapseButton to IconButton"
    This reverts commit b21034e2752b4ef8be5fdb94b6a8685eb85ec90e.
    
    Reason for revert: requires https://chromium-review.googlesource.com/c/chromium/src/+/3274136
    
    Bug:1271310
    
    Original change's description:
    > dark_mode: Migrate CollapseButton to IconButton
    >
    > Bug: 1268959
    > Change-Id: I4c9ea97cec3b7e237008a1a51dc709820c8ecece
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277433
    > Commit-Queue: Min Chen <minch@chromium.org>
    > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#942809}
    
    Bug: 1268959
    Change-Id: I885a2ca49887b9bfab5bf0bb140c3e96c334a081
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290492
    Owners-Override: Toni Barzic <tbarzic@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942872}

--- ash/system/unified/collapse_button.cc
+++ ash/system/unified/collapse_button.cc
@@ -6,18 +6,25 @@
 
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
-#include "ash/style/icon_button.h"
+#include "ash/style/ash_color_provider.h"
+#include "ash/style/element_style.h"
+#include "ash/system/tray/tray_constants.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/scoped_canvas.h"
+#include "ui/views/animation/flood_fill_ink_drop_ripple.h"
+#include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/controls/focus_ring.h"
+#include "ui/views/controls/highlight_path_generator.h"
 
 namespace ash {
 
 CollapseButton::CollapseButton(PressedCallback callback)
-    : IconButton(std::move(callback),
-                 IconButton::Type::kSmallFloating,
-                 kUnifiedMenuExpandIcon,
-                 IDS_ASH_STATUS_TRAY_COLLAPSE) {}
+    : ImageButton(std::move(callback)) {
+  views::InstallCircleHighlightPathGenerator(this);
+  TrayPopupUtils::ConfigureTrayPopupButton(this);
+}
 
 CollapseButton::~CollapseButton() = default;
 
@@ -31,6 +38,10 @@ void CollapseButton::SetExpandedAmount(double expanded_amount) {
   SchedulePaint();
 }
 
+gfx::Size CollapseButton::CalculatePreferredSize() const {
+  return gfx::Size(kTrayItemSize, kTrayItemSize);
+}
+
 void CollapseButton::PaintButtonContents(gfx::Canvas* canvas) {
   gfx::ScopedCanvas scoped(canvas);
   canvas->Translate(gfx::Vector2d(size().width() / 2, size().height() / 2));
@@ -39,7 +50,16 @@ void CollapseButton::PaintButtonContents(gfx::Canvas* canvas) {
   canvas->DrawImageInt(image, -image.width() / 2, -image.height() / 2);
 }
 
-BEGIN_METADATA(CollapseButton, IconButton)
-END_METADATA
+const char* CollapseButton::GetClassName() const {
+  return "CollapseButton";
+}
+
+void CollapseButton::OnThemeChanged() {
+  views::ImageButton::OnThemeChanged();
+  element_style::DecorateFloatingIconButton(this, kUnifiedMenuExpandIcon);
+  views::FocusRing::Get(this)->SetColor(
+      AshColorProvider::Get()->GetControlsLayerColor(
+          AshColorProvider::ControlsLayerType::kFocusRingColor));
+}
 
 }  // namespace ash
--- ash/system/unified/collapse_button.h
+++ ash/system/unified/collapse_button.h
@@ -5,18 +5,15 @@
 #ifndef ASH_SYSTEM_UNIFIED_COLLAPSE_BUTTON_H_
 #define ASH_SYSTEM_UNIFIED_COLLAPSE_BUTTON_H_
 
-#include "ash/style/icon_button.h"
-#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/controls/button/image_button.h"
 
 namespace ash {
 
 // The button with `kUnifiedMenuExpandIcon`. This button can be set as expanded
 // or collapsed through SetExpandedAmount and the icon will be rotated on the
 // `expanded_amount_`. Expanded is the default state.
-class CollapseButton : public IconButton {
+class CollapseButton : public views::ImageButton {
  public:
-  METADATA_HEADER(CollapseButton);
-
   explicit CollapseButton(PressedCallback callback);
 
   CollapseButton(const CollapseButton&) = delete;
@@ -27,8 +24,11 @@ class CollapseButton : public IconButton {
   // Change the expanded state. The icon will change.
   void SetExpandedAmount(double expanded_amount);
 
-  // IconButton:
+  // views::ImageButton:
+  gfx::Size CalculatePreferredSize() const override;
   void PaintButtonContents(gfx::Canvas* canvas) override;
+  const char* GetClassName() const override;
+  void OnThemeChanged() override;
 
  private:
   double expanded_amount_ = 1.0;
