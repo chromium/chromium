9ac41f0f554a504f55152ac6ab7bbfb23e0b7570
Etienne Pierre-doray <etiennep@chromium.org>
2021-11-18 18:57:34 +0000

    [battery]: Ensure battery tick for power estimate.
    Wait for a first battery tick before considering a battery sample
    for power estimate. The first sample that 'ticks' (is different from the
    initial) is the first sample that's used for power estimate.
    
    Bug: 1254332
    Change-Id: I83bf666ef8e16a236f9249e62e1deaf7a418c7b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270734
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943180}

--- tools/mac/power/power_sampler/battery_sampler.h
+++ tools/mac/power/power_sampler/battery_sampler.h
@@ -77,7 +77,8 @@ class BatterySampler : public Sampler {
       base::mac::ScopedIOObject<io_service_t> power_source);
 
   BatterySampler(MaybeGetBatteryDataFn maybe_get_battery_data_fn,
-                 base::mac::ScopedIOObject<io_service_t> power_source);
+                 base::mac::ScopedIOObject<io_service_t> power_source,
+                 BatteryData initial_battery_data);
 
  private:
   void StoreBatteryData(base::TimeTicks sample_time,
@@ -106,6 +107,9 @@ class BatterySampler : public Sampler {
   // consumption.
   base::TimeTicks prev_battery_sample_time_ = base::TimeTicks::Min();
   absl::optional<BatteryData> prev_battery_data_;
+  // Consumed capacity sampled at the time of creation, used to determine
+  // eligibility of early samples for power estimates.
+  const int64_t initial_consumed_mah_;
 };
 
 }  // namespace power_sampler
