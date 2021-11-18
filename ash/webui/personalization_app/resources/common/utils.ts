d9d64d3968b7129b447e65423f5d7516eb94439b
Xiaohui Chen <xiaohuic@google.com>
2021-11-18 19:07:12 +0000

    personalization: migrate common/utils.js to ts
    Bug: b:205290219
    Test: manually run chrome and test with wallpaper app
    Change-Id: I23b043900327c9972c63f682828216874d4dd317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283716
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943185}

--- ash/webui/personalization_app/resources/common/utils.ts
+++ ash/webui/personalization_app/resources/common/utils.ts
@@ -9,44 +9,46 @@
 
 /**
  * Checks if argument is an array with non-zero length.
- * @param {?Object} maybeArray
- * @return {boolean}
  */
-export function isNonEmptyArray(maybeArray) {
+export function isNonEmptyArray(maybeArray: unknown): maybeArray is unknown[] {
   return Array.isArray(maybeArray) && maybeArray.length > 0;
 }
 
 /**
  * Checks if argument is null or is an array.
- * @param {?Array} maybeNullOrArray
- * @return {boolean}
  */
-export function isNullOrArray(maybeNullOrArray) {
+export function isNullOrArray(maybeNullOrArray: unknown):
+    maybeNullOrArray is unknown[]|null {
   return maybeNullOrArray === null || Array.isArray(maybeNullOrArray);
 }
 
 /**
  * Checks if argument is null or is a number.
- * @param {?number} maybeNullOrNumber
- * @return {boolean}
  */
-export function isNullOrNumber(maybeNullOrNumber) {
+export function isNullOrNumber(maybeNullOrNumber: unknown):
+    maybeNullOrNumber is number|null {
   return maybeNullOrNumber === null || typeof maybeNullOrNumber === 'number';
 }
 
 /**
  * Attach a listener to a child element onload function. Returns a promise
  * that resolves when that child element is loaded.
- * @param {*} element A polymer element
- * @param {!string} id Id of the child element.
- * @param {function(*, function(...*): void, !Array=): void}
- *     afterNextRender callback for first render of element.
- * @return {!Promise<!HTMLElement>}
+ * `id` Id of the child element.
+ * `afterNextRender` callback for first render of element.
  */
-export function promisifyOnload(element, id, afterNextRender) {
-  const promise = new Promise((resolve) => {
+export function promisifyOnload(
+    element: HTMLElement, id: string,
+    afterNextRender: (element: HTMLElement, callback: (...args: any) => void) =>
+        void): Promise<HTMLElement> {
+  const promise = new Promise<HTMLElement>((resolve) => {
     function readyCallback() {
+      if (!element.shadowRoot) {
+        return;
+      }
       const child = element.shadowRoot.getElementById(id);
+      if (!child) {
+        return;
+      }
       child.onload = () => resolve(child);
     }
     afterNextRender(element, readyCallback);
@@ -56,39 +58,31 @@ export function promisifyOnload(element, id, afterNextRender) {
 
 /**
  * Returns true if this event is a user action to select an item.
- * @param {!Event} event
- * @return {boolean}
  */
-export function isSelectionEvent(event) {
+export function isSelectionEvent(event: Event): boolean {
   return (event instanceof MouseEvent && event.type === 'click') ||
       (event instanceof KeyboardEvent && event.key === 'Enter');
 }
 
 /**
  * Sets a css variable to control the animation delay.
- * @param {number} index
- * @return {string}
  */
-export function getLoadingPlaceholderAnimationDelay(index) {
+export function getLoadingPlaceholderAnimationDelay(index: number): string {
   return `--animation-delay: ${index * 83}ms;`;
 }
 
 /**
  * Returns the number of grid items to render per row given the current inner
  * width of the |window|.
- * @return {number}
  */
-export function getNumberOfGridItemsPerRow() {
+export function getNumberOfGridItemsPerRow(): number {
   return window.innerWidth > 688 ? 4 : 3;
 }
 
 /**
  * Normalizes the given |key| for RTL.
- * @param {string} key
- * @param {boolean} isRTL
- * @return {string}
  */
-export function normalizeKeyForRTL(key, isRTL) {
+export function normalizeKeyForRTL(key: string, isRTL: boolean): string {
   if (isRTL) {
     if (key === 'ArrowLeft') {
       return 'ArrowRight';
