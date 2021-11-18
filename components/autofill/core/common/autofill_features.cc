dbad8ffb5085636dcf09dbd431a5634315fb853c
Victor Vianna <victorvianna@google.com>
2021-11-18 12:45:10 +0000

    Enable account wallet storage flags on iOS
    Approved launch bug: crbug.com/1212154
    
    Fixed: 1206190
    Change-Id: I033c406d552e6b5a63f95dca0f25b829a3599494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097885
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943044}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -117,9 +117,8 @@ const base::Feature kAutofillEnableSupportForApartmentNumbers{
 // account-based storage when sync the transport is enabled.
 const base::Feature kAutofillEnableAccountWalletStorage {
   "AutofillEnableAccountWalletStorage",
-#if BUILDFLAG(IS_CHROMEOS_ASH) || defined(OS_IOS)
-      // Wallet transport is only currently available on Win/Mac/Linux/Android.
-      // (Somehow, swapping this check makes iOS unhappy?)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+      // Wallet transport is currently unavailable on ChromeOS.
       base::FEATURE_DISABLED_BY_DEFAULT
 #else
       base::FEATURE_ENABLED_BY_DEFAULT
--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -90,13 +90,8 @@ const base::Feature kAutofillEnableOffersInDownstream{
 // When enabled and user is signed in, a footer indicating user's e-mail address
 // and profile picture will appear at the bottom of SaveCardInfoBar.
 const base::Feature kAutofillEnableSaveCardInfoBarAccountIndicationFooter{
-  "AutofillEnableSaveCardInfoBarAccountIndicationFooter",
-#if defined(OS_IOS)
-      base::FEATURE_DISABLED_BY_DEFAULT
-#else
-      base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-};
+    "AutofillEnableSaveCardInfoBarAccountIndicationFooter",
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // When enabled, if the user interacts with the manual fallback bottom sheet
 // on Android, it'll remain sticky until the user dismisses it.
