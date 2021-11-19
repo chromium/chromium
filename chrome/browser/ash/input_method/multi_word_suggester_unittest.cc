22897dc0e7363b1f6a39ab41c2d9eb100fac6c7d
Trent Apted <tapted@chromium.org>
2021-11-19 04:59:28 +0000

    Revert "multiword: refactor suggestion tracking logic"
    This reverts commit bd697705c94e574904d27f11a5ffc1cf29d34377.
    
    Reason for revert: Suspected for failures on linux-chromeos-dbg
    
    Since https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/26464
    
    Errors like
    
    [ RUN      ] MultiWordSuggesterTest.AcceptsSuggestionOnTabPress
    ../../chrome/browser/ash/input_method/multi_word_suggester_unittest.cc:96: Failure
    Value of: suggestion_handler.GetAcceptedSuggestion()
      Actual: false
    Expected: true
    Stack trace:
    #0 0x563ae7e8abeb ash::input_method::MultiWordSuggesterTest_AcceptsSuggestionOnTabPress_Test::TestBody()
    #1 0x563ae8d1456b testing::internal::HandleSehExceptionsInMethodIfSupported<>()
    #2 0x563ae8d079e7 testing::internal::HandleExceptionsInMethodIfSupported<>()
    
    Original change's description:
    > multiword: refactor suggestion tracking logic
    >
    > This CL refactors the tracking logic inside the MultiWordSuggester.
    > There were issues arising due to the current expectations of method
    > execution order. Specifically, the OnExternalSuggestionsUpdated
    > method was expected to be called before the OnSurroundingTextChanged
    > method which is not guaranteed to be the case. In that case the
    > "confirmed length" of a suggestion would not be calculated correctly.
    >
    > This CL then attempts to fix the issue by ensuring we "track" the
    > suggestion when either, OnExternalSuggestionsUpdated or
    > OnSurroundingTextChanged is called.
    >
    > The CL also completes a bit of a refactor for the tracking logic,
    > which hopefully is easier to follow and less error prone.
    >
    > Bug: 1146266
    > Change-Id: I82b756373b0ca9b29e2230a9d38460e6aa21c936
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288658
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#943336}
    
    Bug: 1146266
    Change-Id: I68f256cab7132058ada5b3322fbb69b7eff23563
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291079
    Auto-Submit: Trent Apted <tapted@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Owners-Override: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943417}

--- chrome/browser/ash/input_method/multi_word_suggester_unittest.cc
+++ chrome/browser/ash/input_method/multi_word_suggester_unittest.cc
@@ -155,26 +155,6 @@ TEST(MultiWordSuggesterTest, CalculatesConfirmedLengthForManyWords) {
   EXPECT_EQ(suggestion_handler.GetConfirmedLength(), 3);  // whe
 }
 
-TEST(MultiWordSuggesterTest, CalculatesConfirmedLengthGreedily) {
-  FakeSuggestionHandler suggestion_handler;
-  MultiWordSuggester suggester(&suggestion_handler);
-
-  std::vector<TextSuggestion> suggestions = {
-      TextSuggestion{.mode = TextSuggestionMode::kCompletion,
-                     .type = TextSuggestionType::kMultiWord,
-                     .text = "hohohohoho"},
-  };
-
-  suggester.OnFocus(kFocusedContextId);
-  suggester.OnSurroundingTextChanged(u"merry christmas hohoho",
-                                     /*cursor_pos=*/22, /*anchor_pos=*/22);
-  suggester.OnExternalSuggestionsUpdated(suggestions);
-
-  EXPECT_TRUE(suggestion_handler.GetShowingSuggestion());
-  EXPECT_EQ(suggestion_handler.GetSuggestionText(), u"hohohohoho");
-  EXPECT_EQ(suggestion_handler.GetConfirmedLength(), 6);  // hohoho
-}
-
 TEST(MultiWordSuggesterTest, TracksLastSuggestionOnSurroundingTextChange) {
   FakeSuggestionHandler suggestion_handler;
   MultiWordSuggester suggester(&suggestion_handler);
@@ -289,6 +269,7 @@ TEST(MultiWordSuggesterTest,
   suggester.OnFocus(kFocusedContextId);
   suggester.OnSurroundingTextChanged(u"this is some text", 17, 17);
   suggester.OnExternalSuggestionsUpdated(suggestions);
+
   suggester.OnSurroundingTextChanged(u"this is some text ", 18, 18);
   suggester.Suggest(u"this is some text ", 18, 18);
   suggester.OnSurroundingTextChanged(u"this is some text f", 19, 19);
@@ -301,7 +282,8 @@ TEST(MultiWordSuggesterTest,
   suggester.Suggest(u"this is some text ", 18, 18);
   suggester.OnSurroundingTextChanged(u"this is some text", 17, 17);
 
-  EXPECT_FALSE(suggester.Suggest(u"this is some text", 17, 17));
+  EXPECT_TRUE(suggester.Suggest(u"this is some text", 17, 17));
+  EXPECT_FALSE(suggester.Suggest(u"this is some tex", 16, 16));
 }
 
 TEST(MultiWordSuggesterTest, ReturnsGenericActionIfNoSuggestionHasBeenShown) {
@@ -439,6 +421,92 @@ TEST(MultiWordSuggesterTest, RecordsTimeToDismissMetric) {
 
   histogram_tester.ExpectTotalCount(
       "InputMethod.Assistive.TimeToDismiss.MultiWord", 1);
+}
+
+TEST(MultiWordSuggesterTest, RecordsDismissedAccuracyMetric) {
+  FakeSuggestionHandler suggestion_handler;
+  MultiWordSuggester suggester(&suggestion_handler);
+
+  std::vector<TextSuggestion> suggestions = {
+      TextSuggestion{.mode = TextSuggestionMode::kPrediction,
+                     .type = TextSuggestionType::kMultiWord,
+                     .text = "are you"},
+  };
+
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 43, 0);
+
+  suggester.OnFocus(kFocusedContextId);
+  suggester.OnSurroundingTextChanged(u"how ", 4, 4);
+  suggester.OnExternalSuggestionsUpdated(suggestions);
+  suggester.Suggest(u"how a", 5, 5);
+  suggester.Suggest(u"how ar", 6, 6);
+  suggester.Suggest(u"how are", 7, 7);
+  suggester.Suggest(u"how aren", 8, 8);
+  suggester.DismissSuggestion();
+
+  // Correctly predicted "are" which is three of the seven chars in "are you",
+  // thus accuracy is 3/7 ~= 0.428 which comes to a rounded percentage int val
+  // of 43.
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 43, 1);
+}
+
+TEST(MultiWordSuggesterTest, RecordsZeroValuedDismissedAccuracy) {
+  FakeSuggestionHandler suggestion_handler;
+  MultiWordSuggester suggester(&suggestion_handler);
+
+  std::vector<TextSuggestion> suggestions = {
+      TextSuggestion{.mode = TextSuggestionMode::kPrediction,
+                     .type = TextSuggestionType::kMultiWord,
+                     .text = "are you"},
+  };
+
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 0, 0);
+
+  suggester.OnFocus(kFocusedContextId);
+  suggester.OnSurroundingTextChanged(u"how ", 4, 4);
+  suggester.OnExternalSuggestionsUpdated(suggestions);
+  suggester.Suggest(u"how d", 5, 5);
+  suggester.DismissSuggestion();
+
+  // Zero predicted chars
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 0, 1);
+}
+
+TEST(MultiWordSuggesterTest, RecordsCompletionCandidateDismissedAccuracy) {
+  FakeSuggestionHandler suggestion_handler;
+  MultiWordSuggester suggester(&suggestion_handler);
+
+  std::vector<TextSuggestion> suggestions = {
+      TextSuggestion{.mode = TextSuggestionMode::kCompletion,
+                     .type = TextSuggestionType::kMultiWord,
+                     .text = "aren\'t you"},
+  };
+
+  base::HistogramTester histogram_tester;
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 63, 0);
+
+  suggester.OnFocus(kFocusedContextId);
+  suggester.OnSurroundingTextChanged(u"why ar", 6, 6);
+  suggester.OnExternalSuggestionsUpdated(suggestions);
+  suggester.Suggest(u"why are", 7, 7);
+  suggester.Suggest(u"why aren", 8, 8);
+  suggester.Suggest(u"why aren\'", 9, 9);
+  suggester.Suggest(u"why aren\'t", 10, 10);
+  suggester.Suggest(u"why aren\'t ", 11, 11);
+  suggester.Suggest(u"why aren\'t w", 12, 12);
+  suggester.DismissSuggestion();
+
+  // Predicted the cars "en\'t " which is 5 of the 8 chars in "en\'t you",
+  // thus accuracy is 5/8 or approx 62 percent
+  histogram_tester.ExpectUniqueSample(
+      "InputMethod.Assistive.DismissedAccuracy.MultiWord", 63, 1);
 }
 
 TEST(MultiWordSuggesterTest, SurroundingTextChangesDoNotTriggerAnnouncements) {
