717d410d03102378221e149bb3ed3838b68259bc
Dale Curtis <dalecurtis@chromium.org>
2021-11-19 19:48:09 +0000

    Disable flaky ShortcutTest.UpdateShortcutVerifyProperties on Win32.
    Bug: 1271993, 1272015
    Change-Id: I81f350d9b81502411bdd060ee09312d7bdca8827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293689
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#943614}

--- base/win/shortcut_unittest.cc
+++ base/win/shortcut_unittest.cc
@@ -188,7 +188,14 @@ TEST_F(ShortcutTest, CreateShortcutVerifyProperties) {
   ValidateShortcut(link_file_, link_properties_);
 }
 
-TEST_F(ShortcutTest, UpdateShortcutVerifyProperties) {
+// TODO(crbug.com/1271993): Flaky on Win7 x86.
+#if defined(OS_WIN) && defined(ARCH_CPU_X86)
+#define MAYBE_UpdateShortcutVerifyProperties \
+  DISABLED_UpdateShortcutVerifyProperties
+#else
+#define MAYBE_UpdateShortcutVerifyProperties UpdateShortcutVerifyProperties
+#endif
+TEST_F(ShortcutTest, MAYBE_UpdateShortcutVerifyProperties) {
   ASSERT_TRUE(CreateOrUpdateShortcutLink(link_file_, link_properties_,
                                          SHORTCUT_CREATE_ALWAYS));
 
