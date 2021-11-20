ab6a75c530a79ebc5a5426553bee5bba4ab69d71
Dale Curtis <dalecurtis@chromium.org>
2021-11-20 00:01:21 +0000

    Disable flaky DoubleTapToZoomBrowserTest.TapDelayEnabled
    R=thiabaud
    
    Bug: 1271210
    Change-Id: I8060647466fd89ca31f03dbb9895cb291d5956c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293989
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#943747}

--- content/browser/renderer_host/input/double_tap_to_zoom_browsertest.cc
+++ content/browser/renderer_host/input/double_tap_to_zoom_browsertest.cc
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+#include "build/build_config.h"
 
 #include "base/command_line.h"
 #include "base/test/scoped_feature_list.h"
@@ -107,7 +108,13 @@ IN_PROC_BROWSER_TEST_P(DoubleTapToZoomBrowserTest, MobileOptimizedStatus) {
       << std::get<2>(GetParam());
 }
 
-IN_PROC_BROWSER_TEST_P(DoubleTapToZoomBrowserTest, TapDelayEnabled) {
+// TODO(crbug.com/1271210): Flaky on mac and linux.
+#if defined(OS_MAC) || defined(OS_LINUX)
+#define MAYBE_TapDelayEnabled DISABLED_TapDelayEnabled
+#else
+#define MAYBE_TapDelayEnabled TapDelayEnabled
+#endif
+IN_PROC_BROWSER_TEST_P(DoubleTapToZoomBrowserTest, MAYBE_TapDelayEnabled) {
   bool expected_is_viewport_mobile_optimized = std::get<1>(GetParam());
   LoadURL();
   WebContents* main_contents = shell()->web_contents();
