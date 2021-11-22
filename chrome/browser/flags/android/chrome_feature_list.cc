09662d0b3de79a67fcc3886d19d0f980a7d1b4e0
Shakti Sahu <shaktisahu@chromium.org>
2021-11-22 19:12:59 +0000

    [Download Progress Message UI] Enable by default
    Also removed the field trial testing config. The code cleanup will
    follow in subsequent CLs that will remove the feature flag, and
    old infobar code and tests.
    
    Bug: None
    Change-Id: If18aaa48c0baaa795ccaf2b7fcb735be187ec3f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293581
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944177}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -582,7 +582,7 @@ const base::Feature kDownloadProgressInfoBar{"DownloadProgressInfoBar",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadProgressMessage{"DownloadProgressMessage",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadFileProvider{"DownloadFileProvider",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
