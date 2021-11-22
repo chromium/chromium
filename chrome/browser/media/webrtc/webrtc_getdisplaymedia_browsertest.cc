821522ef874cfc20b81349f729b9cc4c0696b5f6
Katie Dektar <katie@chromium.org>
2021-11-22 20:31:10 +0000

    [Sheriff] Revert "Wait until capture has started in ChangeSource browser test."
    This reverts commit d4d9e3dc317a67e84a821290b0364a9652ea606c.
    
    Reason for revert: Failing on Mac 11:
    Failed most recent builds since landing, first one: https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/5034/overview
    
    Original change's description:
    > Wait until capture has started in ChangeSource browser test.
    >
    > Bug: 1272023
    > Change-Id: I89e9275ccbd94ad523605f944f0abb33b6af178b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293325
    > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > Commit-Queue: Tove Petersson <tovep@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#944103}
    
    Bug: 1272023
    Change-Id: Ie3e383aa02918c1d65c73abff3b85d6f98462bcb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296665
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Owners-Override: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944220}

--- chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
@@ -844,7 +844,9 @@ class GetDisplayMediaChangeSourceBrowserTest : public WebRtcTestBase {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(GetDisplayMediaChangeSourceBrowserTest, ChangeSource) {
+// TODO(crbug.com/1272023): Flaky on Linux Wayland, Win7.
+IN_PROC_BROWSER_TEST_F(GetDisplayMediaChangeSourceBrowserTest,
+                       DISABLED_ChangeSource) {
   ASSERT_TRUE(embedded_test_server()->Start());
   content::WebContents* captured_tab = OpenTestPageInNewTab(kCapturedPageMain);
   content::WebContents* other_tab = OpenTestPageInNewTab(kMainHtmlPage);
@@ -874,10 +876,7 @@ IN_PROC_BROWSER_TEST_F(GetDisplayMediaChangeSourceBrowserTest, ChangeSource) {
   // Click the secondary button, i.e., the "Share this tab instead" button
   GetDelegate(other_tab)->Cancel();
 
-  // Wait until the capture of the other tab has started.
-  while (!other_tab->IsBeingCaptured()) {
-    base::RunLoop().RunUntilIdle();
-  }
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_FALSE(captured_tab->IsBeingCaptured());
   EXPECT_TRUE(other_tab->IsBeingCaptured());
