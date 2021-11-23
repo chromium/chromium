e172cede0107c295f054e6e444b8cf7d4abdb833
Andrew Grieve <agrieve@chromium.org>
2021-11-23 22:33:25 +0000

    pak_util.py: Make decompress files when extracting
    Useful when trying to inspect pak files now that many entries are
    compressed.
    
    Bug: 1271380
    Change-Id: I83bb25f3d87b02686fa72deca01cb2c9ec92429c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295870
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944705}

--- testing/scripts/run_isolated_script_test.py
+++ testing/scripts/run_isolated_script_test.py
@@ -47,11 +47,13 @@ KNOWN_TYP_TEST_RUNNERS = {
     'run_blinkpy_tests.py',
     'run_mac_signing_tests.py',
     'run_mini_installer_tests.py',
+    'test_suite_all.py',  # //tools/grit:grit_python_unittests
 }
 
 KNOWN_TYP_VPYTHON3_TEST_RUNNERS = {
     'monochrome_python_tests.py',
     'run_polymer_tools_tests.py',
+    'test_suite_all.py',  # //tools/grit:grit_python_unittests
 }
 
 class IsolatedScriptTestAdapter(common.BaseIsolatedScriptArgsAdapter):
