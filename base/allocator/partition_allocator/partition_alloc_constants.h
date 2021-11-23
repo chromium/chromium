8940bf4fbccc9a415419cf6cff4856d9b9a65369
Benoit Lize <lizeb@chromium.org>
2021-11-23 16:15:31 +0000

    [PartitionAlloc] Increaase the size of the empty slot span ring on macOS
    This is enabled on Linux, where it halves the number of syscalls for
    Speedometer, and improves some Jetstream scores. Enable it on macOS as
    well.
    
    Bug: 998048
    Change-Id: I6a7e6f506ad68a860b32cb71564c86a574dbb1bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295411
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944524}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -349,10 +349,10 @@ constexpr size_t kBitsPerSizeT = sizeof(void*) * CHAR_BIT;
 // PartitionPurgeDecommitEmptySlotSpans flag will eagerly decommit all entries
 // in the ring buffer, so with periodic purge enabled, this typically happens
 // every few seconds.
-#if defined(OS_LINUX)
-// Set to a higher value on Linux, to assess impact on performance bots. This
-// roughly halves the number of syscalls done during a speedometer 2.0 run on
-// this platform.
+#if defined(OS_LINUX) || defined(OS_APPLE)
+// Set to a higher value on Linux and macOS, to assess impact on performance
+// bots. This roughly halves the number of syscalls done during a speedometer
+// 2.0 run on these platforms.
 constexpr size_t kMaxFreeableSpans = std::numeric_limits<int8_t>::max();
 #else
 constexpr size_t kMaxFreeableSpans = 16;
