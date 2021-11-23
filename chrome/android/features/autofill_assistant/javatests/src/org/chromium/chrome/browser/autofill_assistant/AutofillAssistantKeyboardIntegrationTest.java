06a9ba73761e66ec2c120d537fb20a7aa73cc4c8
Owen Min <zmin@chromium.org>
2021-11-23 20:21:38 +0000

    [Sheriff] Disable keyboardDoesNotShowOnKeyStrokes on Android
    Disable the following test on M due to flaky.
    
    org.chromium.chrome.browser.autofill_assistant
      .AutofillAssistantKeyboardIntegrationTest#keyboardDoesNotShowOnKeyStrokes
    
    Bug: 1272863
    Change-Id: I49315aeb51c33164e4ce99877827003570fcf2e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298950
    Owners-Override: Owen Min <zmin@chromium.org>
    Auto-Submit: Owen Min <zmin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944644}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantKeyboardIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantKeyboardIntegrationTest.java
@@ -25,6 +25,8 @@ import static org.chromium.chrome.browser.autofill_assistant.MiniActionTestUtil.
 import static org.chromium.chrome.browser.autofill_assistant.ProtoTestUtil.toCssSelector;
 import static org.chromium.chrome.browser.autofill_assistant.ProtoTestUtil.toIFrameCssSelector;
 
+import android.os.Build.VERSION_CODES;
+
 import androidx.test.filters.MediumTest;
 
 import org.junit.Rule;
@@ -34,6 +36,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipProto;
@@ -131,7 +134,10 @@ public class AutofillAssistantKeyboardIntegrationTest {
 
     @Test
     @MediumTest
-    public void keyboardDoesNotShowOnKeyStrokes() throws Exception {
+    @DisableIf.Build(message = "Fails on Marshmallow, https://crbug.com/1272863",
+            sdk_is_greater_than = VERSION_CODES.LOLLIPOP_MR1, sdk_is_less_than = VERSION_CODES.N)
+    public void
+    keyboardDoesNotShowOnKeyStrokes() throws Exception {
         ArrayList<ActionProto> list = new ArrayList<>();
 
         SelectorProto nameSelector = toCssSelector("#profile_name");
