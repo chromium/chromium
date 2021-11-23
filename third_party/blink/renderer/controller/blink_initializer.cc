f97e904fbce401da83a9f1c4c73f1ffe83c83b1b
Michael Lippautz <mlippautz@chromium.org>
2021-11-23 10:37:36 +0000

    heap: Replace platform/heap/heap.h with garbage_collected.h
    `heap.h` was only referring to `garbage_collected.h`. Blink code should use
    `garbage_collected.h` to declare a GCed class. This allows the header to
    be reused in future for actual heap internals.
    
    Bug: chromium:1269227
    Change-Id: I5a2046f5a0aa6e744c4df3199aba4a6b7e9a0409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295548
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944442}

--- third_party/blink/renderer/controller/blink_initializer.cc
+++ third_party/blink/renderer/controller/blink_initializer.cc
@@ -56,7 +56,7 @@
 #include "third_party/blink/renderer/platform/bindings/microtask.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/disk_data_allocator.h"
-#include "third_party/blink/renderer/platform/heap/heap.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/wtf.h"
