d8e726c2f0f1cb2d62722852316b6f8996d3c827
Adrian Taylor <adetaylor@chromium.org>
2021-11-24 21:13:14 +0000

    [rust] Pass --extern proc_macro to macro crates
    Matches cargo behavior. 95% of this CL is a test.
    
    Bug: 1069271
    Change-Id: Ib4aff95759ba9add1cb3ea104ab97a02888af9f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299645
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945110}

--- build/rust/tests/test_rust_exe/main.rs
+++ build/rust/tests/test_rust_exe/main.rs
@@ -3,12 +3,14 @@
 // found in the LICENSE file.
 
 use test_mixed_source_set_rs::say_hello_from_a_cpp_callback_from_rust;
+use test_proc_macro_crate::say_hello_from_proc_macro;
 use test_rlib_crate::say_hello_from_crate;
 use test_rust_source_set::say_hello;
 use test_rust_source_set_non_standard_arrangement::say_foo;
 
 fn main() {
     say_hello();
+    say_hello_from_proc_macro!();
     say_foo();
     say_hello_from_a_cpp_callback_from_rust();
     say_hello_from_crate();
