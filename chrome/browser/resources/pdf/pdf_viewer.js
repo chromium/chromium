effbb1253834db9d23a7880a0baf4dfb49477e97
Roman Sorokin [CET] <rsorokin@chromium.org>
2021-11-25 16:05:31 +0000

    Reland "Fix a data race when saving annotated PDFs."
    This reverts commit faea7c7f84258921c28db8d645c90de08153aa37.
    
    Reason for revert: revert did not fix the issue, test is still failing: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/18087/overview
    
    Original change's description:
    > Revert "Fix a data race when saving annotated PDFs."
    >
    > This reverts commit 42cc0d614ed359aeb92cb69620920945a630bc93.
    >
    > Reason for revert: All/PDFExtensionClipboardTest.IndividualShiftRightArrowPresses/1 started failing and became flaky on https://ci.chromium.org/p/chromium/builders/ci/linux-lacros-tester-rel
    >
    > see
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/18076/test-results?q=All%2FPDFExtensionClipboardTest.IndividualShiftRightArrowPresses%2F1+
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/18077/test-results?q=All%2FPDFExtensionClipboardTest.IndividualShiftRightArrowPresses%2F1
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/18078/test-results?q=All%2FPDFExtensionClipboardTest.IndividualShiftRightArrowPresses%2F1
    >
    > It was not flaky on the previous builds
    >
    > Original change's description:
    > > Fix a data race when saving annotated PDFs.
    > >
    > > When saving, a race condition may cause result.dataToSave to be altered
    > > to become an ArrayBuffer with length 0.  Bypass the race condition by
    > > preparing a blob before the async callback rather than inside it.
    > >
    > > Change-Id: If1e3c64d0eea474f8670399fbe017c115f0bedf0
    > > Bug: 1273461
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301599
    > > Reviewed-by: dstockwell <dstockwell@google.com>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > > Auto-Submit: John Palmer <jopalmer@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#945274}
    >
    > Bug: 1273461
    > Change-Id: I2061c564896fee873860b8c0d805660f2ca26687
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301701
    > Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945305}
    
    Bug: 1273461
    Change-Id: I34fbe6cdc5e9a87790383e905d61c872ffa4945a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301472
    Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945404}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -1120,7 +1120,8 @@ export class PDFViewerElement extends PDFViewerBaseElement {
     if (!fileName.toLowerCase().endsWith('.pdf')) {
       fileName = fileName + '.pdf';
     }
-
+    // Create blob before callback to avoid race condition.
+    const blob = new Blob([result.dataToSave], {type: 'application/pdf'});
     chrome.fileSystem.chooseEntry(
         {
           type: 'saveFile',
@@ -1137,8 +1138,7 @@ export class PDFViewerElement extends PDFViewerBaseElement {
             return;
           }
           entry.createWriter(writer => {
-            writer.write(
-                new Blob([result.dataToSave], {type: 'application/pdf'}));
+            writer.write(blob);
             // Unblock closing the window now that the user has saved
             // successfully.
             chrome.mimeHandlerPrivate.setShowBeforeUnloadDialog(false);
