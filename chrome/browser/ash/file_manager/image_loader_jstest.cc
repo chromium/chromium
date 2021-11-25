af418f369876569cf2a8597492b5773b2f4b7c23
Luciano Pacheco <lucmult@chromium.org>
2021-11-25 06:05:14 +0000

    Image Loader: Force Files SWA disabled for JS unittests
    For the Image Loader unittest doesn't matter if the Files legacy or SWA
    is running.  Since some of the JS files import refering to the legacy
    URL we force the legacy for the test.
    
    Once the SWA is fully launched we change the import to use the SWA path
    and remove this flag from the browser test.
    
    Bug: b:207576589
    Change-Id: Ib36d4a2f0c39700d88c875c7204d213464631d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301927
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945276}

--- chrome/browser/ash/file_manager/image_loader_jstest.cc
+++ chrome/browser/ash/file_manager/image_loader_jstest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/constants/ash_features.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/file_manager/file_manager_jstest_base.h"
 #include "content/public/test/browser_test.h"
 
@@ -9,6 +11,14 @@ class ImageLoaderJsTest : public FileManagerJsTestBase {
  protected:
   ImageLoaderJsTest() : FileManagerJsTestBase(
       base::FilePath(FILE_PATH_LITERAL("ui/file_manager/image_loader"))) {}
+
+  void SetUpCommandLine(base::CommandLine* command_lin) override {
+    // Until Files SWA is fully launched Image Loader imports using
+    // chrome-extension://hh.../ so we force SWA disabled here.
+    feature_list_.InitAndDisableFeature(chromeos::features::kFilesSWA);
+  }
+
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(ImageLoaderJsTest, ImageLoaderClientTest) {
