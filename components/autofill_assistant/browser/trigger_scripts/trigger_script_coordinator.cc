ac67a38ade44439bc97234c5957f9e748d8567d2
Clemens Arbesser <arbesser@google.com>
2021-11-26 17:31:25 +0000

    [Autofill Assistant] Fix possible crash in TriggerScriptCoordinator.
    There is a brief period of time between trigger script stop and the
    deletion of the coordinator. While the coordinator correctly detaches
    from its ui delegate, it will continue to receive notifications from
    the WebContents, which it observes. This allows for a race condition
    where certain code is run after the trigger script has already stopped.
    
    This CL puts some safeguards to prevent this from happening. A better
    long-term solution might be the immediate destruction of the
    coordinator, though that may be problematic for other reasons.
    
    Bug: b/207822942
    Change-Id: I7b5bc6faa9a2894e472888e4de2062e9ad5163f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3302115
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#945672}

--- components/autofill_assistant/browser/trigger_scripts/trigger_script_coordinator.cc
+++ components/autofill_assistant/browser/trigger_scripts/trigger_script_coordinator.cc
@@ -404,6 +404,15 @@ void TriggerScriptCoordinator::OnEffectiveVisibilityChanged() {
           Metrics::TriggerScriptFinishedState::DISABLED_PROACTIVE_HELP_SETTING);
       return;
     }
+    // Should never happen, this is just a failsafe and to prevent regression.
+    if (!trigger_context_) {
+      NOTREACHED() << "No trigger context";
+      if (callback_) {
+        std::move(callback_).Run(Metrics::TriggerScriptFinishedState::CANCELED,
+                                 nullptr, absl::nullopt);
+      }
+      return;
+    }
     VLOG(2) << "Restarting after tab became visible again";
     Start(deeplink_url_, std::move(trigger_context_), std::move(callback_));
   } else {
@@ -425,6 +434,7 @@ void TriggerScriptCoordinator::WebContentsDestroyed() {
                   WEB_CONTENTS_DESTROYED_WHILE_VISIBLE);
     finished_state_recorded_ = true;
   }
+  ui_delegate_->Detach();
 }
 
 void TriggerScriptCoordinator::StartCheckingTriggerConditions() {
@@ -595,6 +605,12 @@ void TriggerScriptCoordinator::RunCallback(
                                          trigger_ui_type, state);
   }
   trigger_context_->SetTriggerUIType(trigger_ui_type);
+
+  // Prevent notifications after the callback was run, i.e., after
+  // trigger_context_ was moved out of this object.
+  Observe(nullptr);
+  ui_delegate_->Detach();
+
   std::move(callback_).Run(state, std::move(trigger_context_), trigger_script);
 }
 
