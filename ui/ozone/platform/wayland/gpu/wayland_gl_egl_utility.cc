77cc85a124b70d8b10705e3221cce607e7963756
Kramer Ge <fangzhoug@chromium.org>
2021-11-29 21:17:31 +0000

    Revert "ozone/wayland: enable ANGLE."
    This reverts commit 9ea635ffde08d4858e1853c8924319cac48c58fe.
    
    Reason for revert: overlays are vertically inverted in LaCrOS past this patch.
    
    Original change's description:
    > ozone/wayland: enable ANGLE.
    >
    > This CL enables ANGLE after it started to support surfaceless path
    > (thanks to Geoff Lang).
    >
    > Cq-Include-Trybots: luci.chromium.try:gpu-fyi-try-lacros-intel-rel,gpu-fyi-try-lacros-amd-rel
    > Bug: 1231934
    > Change-Id: Id50109ad3af8ce6a78809f993486d747046cc477
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253584
    > Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    > Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    > Commit-Queue: Maksim Sisov <msisov@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#944891}
    
    Bug: 1231934
    Change-Id: Ibfdb928e5aa32318df1c90354aae594720b27571
    Cq-Include-Trybots: luci.chromium.try:gpu-fyi-try-lacros-intel-rel,gpu-fyi-try-lacros-amd-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305033
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946139}

--- ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
+++ ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
@@ -16,7 +16,6 @@
 #ifndef EGL_ANGLE_platform_angle
 #define EGL_ANGLE_platform_angle 1
 #define EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE 0x348F
-#define EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE 0x3209
 #endif /* EGL_ANGLE_platform_angle */
 
 #ifndef EGL_ANGLE_platform_angle_vulkan
@@ -24,21 +23,9 @@
 #define EGL_PLATFORM_VULKAN_DISPLAY_MODE_HEADLESS_ANGLE 0x34A5
 #endif /* EGL_ANGLE_platform_angle_vulkan */
 
-#ifndef EGL_ANGLE_platform_angle_device_type_egl_angle
-#define EGL_ANGLE_platform_angle_device_type_egl_angle
-#define EGL_PLATFORM_ANGLE_DEVICE_TYPE_EGL_ANGLE 0x348E
-#endif /* EGL_ANGLE_platform_angle_device_type_egl_angle */
-
-#ifndef EGL_ANGLE_platform_angle_opengl
-#define EGL_ANGLE_platform_angle_opengl 1
-#define EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE 0x320D
-#define EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE 0x320E
-#endif /* EGL_ANGLE_platform_angle_opengl */
-
 namespace ui {
 
 WaylandGLEGLUtility::WaylandGLEGLUtility() = default;
-
 WaylandGLEGLUtility::~WaylandGLEGLUtility() = default;
 
 void WaylandGLEGLUtility::GetAdditionalEGLAttributes(
@@ -51,17 +38,6 @@ void WaylandGLEGLUtility::GetAdditionalEGLAttributes(
         EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE);
     display_attributes->push_back(
         EGL_PLATFORM_VULKAN_DISPLAY_MODE_HEADLESS_ANGLE);
-    return;
-  }
-
-  if (std::find(display_attributes->begin(), display_attributes->end(),
-                EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE) !=
-          display_attributes->end() ||
-      std::find(display_attributes->begin(), display_attributes->end(),
-                EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE) !=
-          display_attributes->end()) {
-    display_attributes->push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE);
-    display_attributes->push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_EGL_ANGLE);
     return;
   }
 }
