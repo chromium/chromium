52cfa3e8d53ef9efab0bc5a8a87838c0df40aad4
Akihiro Ota <akihiroota@chromium.org>
2021-11-29 18:12:24 +0000

    Dictation: Convert ActionInfo typedef into SimpleMacroFactory class
    This change addresses a todo to refactor a typedef object into a class.
    The new approach is simpler and promotes encapsulation by moving
    initialization and matching logic into the SimpleMacroFactory class.
    This change also does some clean-up around naming and terminology.
    Specifically, we changed 'ActionInfo' to 'SimpleMacroFactory' since it
    more accurately conveys the purpose of the class. We also changed
    instances of 'action' and 'command' to 'macro' where appropriate.
    
    This is a pure refactor and should not result in any behavior change.
    
    AX-Relnotes: N/A
    Bug: 1216111
    Change-Id: I600c8b1f9ff98118bb26d7850af4e4e51d7b1ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296422
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946032}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/speech_parser.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/speech_parser.js
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @fileoverview Handles speech parsing for dictation.
+ */
+
 import {InputController} from './input_controller.js';
 import {InputTextViewMacro, NewLineMacro} from './macros/input_text_view_macro.js';
 import {ListCommandsMacro} from './macros/list_commands_macro.js';
@@ -13,17 +17,140 @@ import * as RepeatableKeyPressMacro from './macros/repeatable_key_press_macro.js
 // or exclude_pumpkin.js in the BUILD rule.
 import {PumpkinAvailability} from './pumpkin/pumpkin_availability.js';
 
-
-// TODO(crbug.com/1264544): Consider refactoring into a class.
 /**
  * @typedef {{
- *   commandString: string,
- *   createMacro: function():Macro,
- *   matchesMacro: function(string):boolean,
- *   matchesInputTextViewMacro: function(string):boolean,
+ *   messageId: string,
+ *   build: function(): Macro,
  * }}
  */
-let ActionInfo;
+let MacroData;
+
+/**
+ * SimpleMacroFactory helps SpeechParser convert spoken strings into Macros
+ * using string matching. Used as a fall-back when Pumpkin is not available.
+ */
+class SimpleMacroFactory {
+  /**
+   * @param {!MacroName} macroName
+   * @param {!InputController} inputController
+   * @param {boolean} isRTLLocale
+   */
+  constructor(macroName, inputController, isRTLLocale) {
+    /** @private {!MacroName} */
+    this.macroName_ = macroName;
+    /** @private {!InputController} */
+    this.inputController_ = inputController;
+    /** @private {boolean} */
+    this.isRTLLocale_ = isRTLLocale;
+
+    /** @private {Object<MacroName, MacroData>} */
+    this.data_ = {
+      [MacroName.DELETE_PREV_CHAR]: {
+        messageId: 'dictation_command_delete_prev_char',
+        build: () => new RepeatableKeyPressMacro.DeletePreviousCharacterMacro()
+      },
+      [MacroName.NAV_PREV_CHAR]: {
+        messageId: 'dictation_command_nav_prev_char',
+        build: () =>
+            new RepeatableKeyPressMacro.NavPreviousCharMacro(this.isRTLLocale_)
+      },
+      [MacroName.NAV_NEXT_CHAR]: {
+        messageId: 'dictation_command_nav_next_char',
+        build: () =>
+            new RepeatableKeyPressMacro.NavNextCharMacro(this.isRTLLocale_)
+      },
+      [MacroName.NAV_PREV_LINE]: {
+        messageId: 'dictation_command_nav_prev_line',
+        build: () => new RepeatableKeyPressMacro.NavPreviousLineMacro()
+      },
+      [MacroName.NAV_NEXT_LINE]: {
+        messageId: 'dictation_command_nav_next_line',
+        build: () => new RepeatableKeyPressMacro.NavNextLineMacro()
+      },
+      [MacroName.COPY_SELECTED_TEXT]: {
+        messageId: 'dictation_command_copy_selected_text',
+        build: () => new RepeatableKeyPressMacro.CopySelectedTextMacro()
+      },
+      [MacroName.PASTE_TEXT]: {
+        messageId: 'dictation_command_paste_text',
+        build: () => new RepeatableKeyPressMacro.PasteTextMacro()
+      },
+      [MacroName.CUT_SELECTED_TEXT]: {
+        messageId: 'dictation_command_cut_selected_text',
+        build: () => new RepeatableKeyPressMacro.CutSelectedTextMacro()
+      },
+      [MacroName.UNDO_TEXT_EDIT]: {
+        messageId: 'dictation_command_undo_text_edit',
+        build: () => new RepeatableKeyPressMacro.UndoTextEditMacro()
+      },
+      [MacroName.REDO_ACTION]: {
+        messageId: 'dictation_command_redo_action',
+        build: () => new RepeatableKeyPressMacro.RedoActionMacro()
+      },
+      [MacroName.SELECT_ALL_TEXT]: {
+        messageId: 'dictation_command_select_all_text',
+        build: () => new RepeatableKeyPressMacro.SelectAllTextMacro()
+      },
+      [MacroName.UNSELECT_TEXT]: {
+        messageId: 'dictation_command_unselect_text',
+        build: () =>
+            new RepeatableKeyPressMacro.UnselectTextMacro(this.isRTLLocale_)
+      },
+      [MacroName.LIST_COMMANDS]: {
+        messageId: 'dictation_command_list_commands',
+        build: () => new ListCommandsMacro()
+      },
+      [MacroName.NEW_LINE]: {
+        messageId: 'dictation_command_new_line',
+        build: () => new NewLineMacro(this.inputController_)
+      },
+    };
+
+    if (!this.data_[this.macroName_]) {
+      throw new Error(
+          'Macro is not supported by SimpleMacroFactory: ' + this.macroName_);
+    }
+
+    /** @private {string} */
+    this.commandString_ =
+        chrome.i18n.getMessage(this.data_[this.macroName_].messageId);
+  }
+
+  /** @return {Macro} */
+  createMacro() {
+    return this.data_[this.macroName_].build();
+  }
+
+  /**
+   * Checks whether a string matches `commandString_`, ignoring case and
+   * whitespace.
+   * @param {string} text
+   * @return {boolean}
+   */
+  matchesMacro(text) {
+    return text.trim().toLowerCase() === this.commandString_;
+  }
+
+  // TODO(crbug.com/1216111): Create a factory for InputTextViewMacro and remove
+  // this method.
+  /**
+   * Checks whether a string matches a request to type a command, i.e. for the
+   * command 'delete', it would match 'type delete', ignoring case and
+   * whitespace.
+   * @param {string} text
+   * @return {boolean}
+   */
+  matchesInputTextViewMacro(text) {
+    const expected = chrome.i18n.getMessage(
+        'dictation_command_input_text_view', this.commandString_);
+    return text.trim().toLowerCase() === expected;
+  }
+
+  /** @return {string} */
+  getCommandString() {
+    return this.commandString_;
+  }
+}
 
 /**
  * PumpkinTagger Hypothesis argument names. These should match the variable
@@ -42,18 +169,17 @@ const HypothesisArgumentName = {
  */
 export class SpeechParser {
   /**
-   * @param {InputController} inputController to interact with the IME.
+   * @param {!InputController} inputController to interact with the IME.
    */
   constructor(inputController) {
     /** @private {boolean} */
     this.commandsFeatureEnabled_ = false;
 
     /**
-     * Map of command action IDs to information about that command.
-     * This object uses localized strings.
-     * @private {!Map<MacroName, ActionInfo>}
+     * Map of macro names to a factory for that macro.
+     * @private {!Map<!MacroName, !SimpleMacroFactory>}
      */
-    this.commandMap_ = new Map();
+    this.macroFactoryMap_ = new Map();
 
     /** @private {boolean} */
     this.isRTLLocale_ = false;
@@ -61,7 +187,7 @@ export class SpeechParser {
     /** @private {speech.pumpkin.api.js.PumpkinTagger.PumpkinTagger} */
     this.pumpkinTagger_ = null;
 
-    /** @private {InputController} */
+    /** @private {!InputController} */
     this.inputController_ = inputController;
 
     /** @private {?Promise} */
@@ -120,12 +246,12 @@ export class SpeechParser {
     }
 
     // If Pumpkin is not available we can fall back to a hard-coded list
-    // of possible commands. First, see if the text matches a command action.
-    for (const [action, info] of this.commandMap_) {
-      if (info.matchesMacro(text)) {
-        return info.createMacro();
-      } else if (info.matchesInputTextViewMacro(text)) {
-        text = info.commandString;
+    // of possible macros.
+    for (const [name, factory] of this.macroFactoryMap_) {
+      if (factory.matchesMacro(text)) {
+        return factory.createMacro();
+      } else if (factory.matchesInputTextViewMacro(text)) {
+        text = factory.getCommandString();
         break;
       }
     }
@@ -152,118 +278,19 @@ export class SpeechParser {
       this.pumpkinTagger_ = null;
     }
 
-    // Does pre-work to parse commands: gets translated command strings and
-    // generates a map of commands to regular expressions that would match them.
-    // These are used as a fall-back when Pumpkin is not available.
-    // TODO(crbug.com/1264544): Don't generate commands for all names when
-    // Pumpkin is available. Just generate "new line" (the only non-pumpkin
-    // command) or nothing at all.
     for (const key in MacroName) {
-      const actionId = MacroName[key];
-      let messageId;
-      let createMacro;
-      switch (actionId) {
-        case MacroName.DELETE_PREV_CHAR:
-          messageId = 'dictation_command_delete_prev_char';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.DeletePreviousCharacterMacro();
-          };
-          break;
-        case MacroName.NAV_PREV_CHAR:
-          messageId = 'dictation_command_nav_prev_char';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.NavPreviousCharMacro(
-                this.isRTLLocale_);
-          };
-          break;
-        case MacroName.NAV_NEXT_CHAR:
-          messageId = 'dictation_command_nav_next_char';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.NavNextCharMacro(
-                this.isRTLLocale_);
-          };
-          break;
-        case MacroName.NAV_PREV_LINE:
-          messageId = 'dictation_command_nav_prev_line';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.NavPreviousLineMacro();
-          };
-          break;
-        case MacroName.NAV_NEXT_LINE:
-          messageId = 'dictation_command_nav_next_line';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.NavNextLineMacro();
-          };
-          break;
-        case MacroName.COPY_SELECTED_TEXT:
-          messageId = 'dictation_command_copy_selected_text';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.CopySelectedTextMacro();
-          };
-          break;
-        case MacroName.PASTE_TEXT:
-          messageId = 'dictation_command_paste_text';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.PasteTextMacro();
-          };
-          break;
-        case MacroName.CUT_SELECTED_TEXT:
-          messageId = 'dictation_command_cut_selected_text';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.CutSelectedTextMacro();
-          };
-          break;
-        case MacroName.UNDO_TEXT_EDIT:
-          messageId = 'dictation_command_undo_text_edit';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.UndoTextEditMacro();
-          };
-          break;
-        case MacroName.REDO_ACTION:
-          messageId = 'dictation_command_redo_action';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.RedoActionMacro();
-          };
-          break;
-        case MacroName.SELECT_ALL_TEXT:
-          messageId = 'dictation_command_select_all_text';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.SelectAllTextMacro();
-          };
-          break;
-        case MacroName.UNSELECT_TEXT:
-          messageId = 'dictation_command_unselect_text';
-          createMacro = () => {
-            return new RepeatableKeyPressMacro.UnselectTextMacro(
-                this.isRTLLocale_);
-          };
-          break;
-        case MacroName.LIST_COMMANDS:
-          messageId = 'dictation_command_list_commands';
-          createMacro = () => {
-            return new ListCommandsMacro();
-          };
-          break;
-        case MacroName.NEW_LINE:
-          messageId = 'dictation_command_new_line';
-          createMacro = () => {
-            return new NewLineMacro(this.inputController_);
-          };
-          break;
-        default:
-          // Other macros are not supported by regular expressions and
-          // should go through Pumpkin. Strongly encouraged not to add any
-          // new actions manually unless they are needed by users who do not
-          // use languages with Pumpkin translations.
-          continue;
+      const name = MacroName[key];
+      // TODO(crbug.com/1264544): Don't generate parsers for all macros when
+      // Pumpkin is available. Just generate "new line" (the only non-pumpkin
+      // command) or nothing at all.
+      if (name === MacroName.INPUT_TEXT_VIEW) {
+        continue;
       }
-      const commandString = chrome.i18n.getMessage(messageId);
-      this.commandMap_.set(actionId, {
-        commandString,
-        createMacro,
-        matchesMacro: this.commandMatcher_(commandString),
-        matchesInputTextViewMacro: this.inputTextViewMatcher_(commandString)
-      });
+
+      this.macroFactoryMap_.set(
+          name,
+          new SimpleMacroFactory(
+              name, this.inputController_, this.isRTLLocale_));
     }
   }
 
@@ -419,37 +446,6 @@ export class SpeechParser {
         console.log('Unsupported Pumpkin action: ', tag);
         return null;
     }
-  }
-
-  /**
-   * Gets a function that checks whether a string matches a
-   * command string, ignoring case and whitespace.
-   * @param {string} commandString
-   * @return {function(string):boolean}
-   * @private
-   */
-  commandMatcher_(commandString) {
-    const re = new RegExp('^[\\s]*' + commandString + '[\\s]*$', 'im');
-    return text => {
-      return re.exec(text);
-    };
-  }
-
-  /**
-   * Gets a function that checks whether a string matches
-   * a request to type a command, i.e. for the command 'delete', it would
-   * match 'type delete', ignoring case and whitespace.
-   * @param {string} commandString
-   * @return {function(string):boolean}
-   * @private
-   */
-  inputTextViewMatcher_(commandString) {
-    const query = chrome.i18n.getMessage(
-        'dictation_command_input_text_view', commandString);
-    const re = new RegExp('^[\\s]*' + query + '[\\s]*$', 'im');
-    return text => {
-      return re.exec(text);
-    };
   }
 }
 
