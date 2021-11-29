ab520eddf304bfe98dbe67f108acc765530d1c1c
Matt Mueller <mattm@chromium.org>
2021-11-29 16:31:55 +0000

    cert_verify_tool: Support PKCS #7 cert bundles as input
    Change-Id: Icaa50563a56673f0e3c4067cfc868ebcde27df9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300681
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945989}

--- net/tools/cert_verify_tool/cert_verify_tool_util.cc
+++ net/tools/cert_verify_tool/cert_verify_tool_util.cc
@@ -35,7 +35,6 @@ const char kCertificateHeader[] = "CERTIFICATE";
 void ExtractCertificatesFromData(const std::string& data_string,
                                  const base::FilePath& file_path,
                                  std::vector<CertInput>* certs) {
-  // TODO(mattm): support PKCS #7 (.p7b) files.
   net::PEMTokenizer pem_tokenizer(data_string, {kCertificateHeader});
   int block = 0;
   while (pem_tokenizer.GetNext()) {
@@ -51,6 +50,22 @@ void ExtractCertificatesFromData(const std::string& data_string,
   // If it was a PEM file, return the extracted results.
   if (block)
     return;
+
+  std::vector<bssl::UniquePtr<CRYPTO_BUFFER>> pkcs7_cert_buffers;
+  if (net::x509_util::CreateCertBuffersFromPKCS7Bytes(
+          base::as_bytes(base::make_span(data_string)), &pkcs7_cert_buffers)) {
+    int n = 0;
+    for (const auto& cert_buffer : pkcs7_cert_buffers) {
+      CertInput cert;
+      cert.der_cert = std::string(
+          net::x509_util::CryptoBufferAsStringPiece(cert_buffer.get()));
+      cert.source_file_path = file_path;
+      cert.source_details = base::StringPrintf("PKCS #7 cert %i", n);
+      certs->push_back(cert);
+      ++n;
+    }
+    return;
+  }
 
   // Otherwise, assume it is a single DER cert.
   CertInput cert;
