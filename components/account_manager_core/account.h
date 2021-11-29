8da0926de2e8a260fdebd1c3d48f043aba0bbf33
Kushagra Sinha <sinhak@chromium.org>
2021-11-29 16:49:15 +0000

    Implement comparison operators for `Account`
    Implement <, ==, and != operators for `account_manager::Account`.
    
    Additionally, clean up the implementation of
    `account_manager::AccountKey`'s equality comparison operator to use
    `std::tie` and make it consistent with the rest of the file.
    
    Change-Id: I29e5c637a1fe788a50ea733a559450e27d710d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306398
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945996}

--- components/account_manager_core/account.h
+++ components/account_manager_core/account.h
@@ -39,7 +39,7 @@ class COMPONENT_EXPORT(ACCOUNT_MANAGER_CORE) AccountKey {
   bool operator!=(const AccountKey& other) const;
 
  private:
-  // Fields are not const to allow assignmenent operator.
+  // Fields are not const to allow assignment operator.
   std::string id_;
   AccountType account_type_;
 };
@@ -51,6 +51,10 @@ struct COMPONENT_EXPORT(ACCOUNT_MANAGER_CORE) Account {
 
   // The raw, un-canonicalized email id for this account.
   std::string raw_email;
+
+  bool operator<(const Account& other) const;
+  bool operator==(const Account& other) const;
+  bool operator!=(const Account& other) const;
 };
 
 // For logging.
