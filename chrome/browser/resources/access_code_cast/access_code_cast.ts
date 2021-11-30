90f14f4d6b7d0302e743b66d761371177e12e9f8
Benjamin Zielinski <bzielinski@google.com>
2021-11-30 18:37:28 +0000

    [Cast2Class] Init unit tests; page state changes
    Set up unit tests for the Cast2Class WebUI. Start
    implementation for the WebUI, by handling basic state
    changes with UI buttons. Test for switching between
    the two types of inputs. All strings are temporary
    placeholders.
    
    Also, add fix for access_code_cast/BUILD.gn missing
    dependency.
    
    Bug: b/202529643
    Change-Id: I0e6f599a295e7055d89687bcc18263e2f18d626f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307732
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#946555}

--- chrome/browser/resources/access_code_cast/access_code_cast.ts
+++ chrome/browser/resources/access_code_cast/access_code_cast.ts
@@ -2,12 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {PageCallbackRouter} from './access_code_cast.mojom-webui.js';
 import {BrowserProxy} from './browser_proxy.js';
 
 declare const chrome: any;
+
+enum PageState {
+  CODEINPUT,
+  QRINPUT,
+}
+
+interface AccessCodeCastElement {
+  $: {
+    backButton: CrButtonElement;
+    castButton: CrButtonElement;
+    codeInputView: HTMLDivElement;
+    qrInputView: HTMLDivElement;
+  }
+}
 
 class AccessCodeCastElement extends PolymerElement {
   static get is() {
@@ -27,6 +47,11 @@ class AccessCodeCastElement extends PolymerElement {
     this.router = BrowserProxy.getInstance().callbackRouter;
   }
 
+  ready() {
+    super.ready();
+    this.setState(PageState.CODEINPUT);
+  }
+
   connectedCallback() {
     super.connectedCallback();
   }
@@ -36,8 +61,35 @@ class AccessCodeCastElement extends PolymerElement {
     this.listenerIds.forEach(id => this.router.removeListener(id));
   }
 
+  /**
+   * -------------------------
+   * Event handlers
+   * -------------------------
+   */
+
   close() {
     chrome.send('dialogClose');
+  }
+
+  switchToCodeInput() {
+    this.setState(PageState.CODEINPUT);
+  }
+
+  switchToQrInput() {
+    this.setState(PageState.QRINPUT);
+  }
+
+  /**
+   * -------------------------
+   * Helper functions
+   * -------------------------
+   */
+
+  private setState(state: PageState) {
+    this.$.codeInputView.hidden = state !== PageState.CODEINPUT;
+    this.$.castButton.hidden = state !== PageState.CODEINPUT;
+    this.$.qrInputView.hidden = state !== PageState.QRINPUT;
+    this.$.backButton.hidden = state !== PageState.QRINPUT;
   }
 }
 
