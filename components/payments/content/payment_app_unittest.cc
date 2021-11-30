0e0f8018241c6eb82206af4d43e5ca0e067f8d2e
David Bienvenu <davidbienvenu@chromium.org>
2021-11-30 20:50:23 +0000

    components_unittest: fix tsan data race in PaymentApp tests
    ScopedFeatureList needs to be destroyed after task_environment_
    is torn down.
    
    Change-Id: Ic2e1b9cb7b581a01e4887ebebb3743113aced8a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308500
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946643}

--- components/payments/content/payment_app_unittest.cc
+++ components/payments/content/payment_app_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "components/payments/content/payment_app.h"
 
+#include <memory>
+#include <utility>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
@@ -143,6 +145,10 @@ class PaymentAppTest : public testing::TestWithParam<RequiredPaymentOptions>,
     icon->eraseColor(SK_ColorRED);
   }
 
+  base::test::ScopedFeatureList& scoped_feature_list() {
+    return scoped_feature_list_;
+  }
+
   autofill::CreditCard& local_credit_card() { return local_card_; }
   std::vector<autofill::AutofillProfile*>& billing_profiles() {
     return billing_profiles_;
@@ -183,6 +189,10 @@ class PaymentAppTest : public testing::TestWithParam<RequiredPaymentOptions>,
         std::move(method_data), weak_ptr_factory_.GetWeakPtr(), "en-US");
   }
 
+  // ScopedFeatureList has to be declared before BrowserTaskEnvironment so that
+  // it is destroyed after BrowserTaskEnvironment, to prevent data race errors,
+  // caused by tasks on other threads accessing the ScopedFeatureList.
+  base::test::ScopedFeatureList scoped_feature_list_;
   content::BrowserTaskEnvironment task_environment_;
   content::TestBrowserContext browser_context_;
   content::TestWebContentsFactory test_web_contents_factory_;
@@ -380,11 +390,9 @@ TEST_P(PaymentAppTest, SortAppsBasedOnSupportedDelegations) {
 class DownRankJustInTimePaymentAppTest : public PaymentAppTest {
  public:
   DownRankJustInTimePaymentAppTest() {
-    scoped_feature_list_.InitAndEnableFeature(
+    scoped_feature_list().InitAndEnableFeature(
         features::kDownRankJustInTimePaymentApp);
   }
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
