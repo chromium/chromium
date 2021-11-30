9d288b768f59c2213b953b5035e49d9c25d29140
Eugene Zemtsov <eugene@chromium.org>
2021-11-30 00:21:50 +0000

    Reactivate zero-copy path in CreateImageFromVideoFrame()
    Currently CreateImageFromVideoFrame() is never called with empty
    |dest_rect|, thus zero copy path is never exercised. This CL checks if
    |dest_rect| has boundaries compatible with zero copy, it brings back
    zero copy path where it's possible.
    
    Bug: 1272288
    Change-Id: Id37522d46f45c4c6c7e59542f84c59745e95eef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307887
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946232}

--- third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
@@ -136,7 +136,9 @@ scoped_refptr<StaticBitmapImage> CreateImageFromVideoFrame(
   DCHECK(frame);
   const auto transform =
       frame->metadata().transformation.value_or(media::kNoTransformation);
-  if (allow_zero_copy_images && dest_rect.IsEmpty() &&
+  const bool full_frame_rect =
+      dest_rect.IsEmpty() || dest_rect == gfx::Rect(frame->coded_size());
+  if (allow_zero_copy_images && full_frame_rect &&
       transform == media::kNoTransformation && CanUseZeroCopyImages(*frame)) {
     // TODO(sandersd): Do we need to be able to handle limited-range RGB? It
     // may never happen, and SkColorSpace doesn't know about it.
