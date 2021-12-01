721f04b0f38c5c89dd89ed9d13d65642f0d6b5d2
Benoit Lize <lizeb@chromium.org>
2021-12-01 18:50:27 +0000

    [PartitionAlloc] Don't use thread_local on Apple OSes.
    thread_local is already effectively disabled for most macOS builds
    (since PartitionAlloc is the default allocator now), unify the paths.
    
    Bug: 1121427
    Change-Id: I13dc08e525f0d649535d09f669cb40f32397aecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307451
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947117}

--- base/allocator/partition_allocator/partition_alloc_config.h
+++ base/allocator/partition_allocator/partition_alloc_config.h
@@ -137,23 +137,14 @@ static_assert(sizeof(void*) != 8, "");
 // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-errors-1/compiler-error-c2492?view=msvc-160.
 // Don't use it there.
 //
-// On macOS and iOS with PartitionAlloc-Everywhere enabled, thread_local
-// allocates memory and it causes an infinite loop of ThreadCache::Get() ->
-// malloc_zone_malloc -> ShimMalloc -> ThreadCache::Get() -> ...
-// Exact stack trace is:
-//   libsystem_malloc.dylib`_malloc_zone_malloc
-//   libdyld.dylib`tlv_allocate_and_initialize_for_key
-//   libdyld.dylib`tlv_get_addr
-//   libbase.dylib`thread-local wrapper routine for
-//       base::internal::g_thread_cache
-//   libbase.dylib`base::internal::ThreadCache::Get()
-// where tlv_allocate_and_initialize_for_key performs memory allocation.
+// On macOS and iOS:
+// - With PartitionAlloc-Everywhere, thread_local allocates, reentering the
+//   allocator.
+// - Component builds triggered a clang bug: crbug.com/1243375
 //
-// Finally, we have crashes with component builds on macOS,
-// see crbug.com/1243375.
-#if !(defined(OS_WIN) && defined(COMPONENT_BUILD)) && \
-    !(defined(OS_APPLE) &&                            \
-      (BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) || defined(COMPONENT_BUILD)))
+// Regardless, the "normal" TLS access is fast on x86_64 (see partition_tls.h),
+// so don't bother with thread_local anywhere.
+#if !(defined(OS_WIN) && defined(COMPONENT_BUILD)) && !defined(OS_APPLE)
 #define PA_THREAD_LOCAL_TLS
 #endif
 
