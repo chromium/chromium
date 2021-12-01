047dbade528a8f078cf077c7cd37b52824345a28
Nico Weber <thakis@chromium.org>
2021-12-01 15:26:10 +0000

    Reland "Build with C++17 on iOS"
    This is a reland of 02b49cd3b2f51bdbc264553419a2590e4a6b91ef
    In the reland, call either std::not_fn (C++17 only) or
    std::not1 (deprecated in C++17), so that the CFI bots (which still
    use C++14 for now) don't break.
    
    Original change's description:
    > Build with C++17 on iOS
    >
    > This is to find unknown things that break with -std=c++17.
    >
    > Do NOT use C++17 features anywhere yet. We'll likely have to revert
    > this again in a bit.
    >
    > With this, C++17 is enabled everywhere except in CFI builds.
    >
    > To make crashpad build, define
    > _LIBCPP_ENABLE_CXX17_REMOVED_UNEXPECTED_FUNCTIONS in the one file that
    > uses the now-removed std::unexpected_handler.
    >
    > Bug: 752720,1274565
    > Change-Id: I5ea9853bb82c04795fd03283bc96317cfb31573e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307901
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#946842}
    
    Bug: 752720,1274565
    Change-Id: If067ee01cde000f83f6d5b43e5d21d2e6b615bfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309603
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947008}

--- base/strings/string_util.cc
+++ base/strings/string_util.cc
@@ -36,8 +36,7 @@ namespace base {
 bool IsWprintfFormatPortable(const wchar_t* format) {
   // This snippet of code checks that we can build C++17 code.
   // TODO(thakis): Enable this on all platforms, and then remove it again.
-#if !defined(OS_IOS) && !BUILDFLAG(CFI_ICALL_CHECK) && \
-    !BUILDFLAG(CFI_CAST_CHECK)
+#if !BUILDFLAG(CFI_ICALL_CHECK) && !BUILDFLAG(CFI_CAST_CHECK)
   if constexpr (constexpr int i = 0; i > 0) {
   }
 #endif
