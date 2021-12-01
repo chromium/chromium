84c6d81a36eef0c577301780a4d227a5277ed0ee
Zhiyuan Cai <zhiyuancai@chromium.org>
2021-12-01 19:15:03 +0000

    [Price Tracking] Add a flag to disallow opt-out of annotations
    This CL contains the following:
    * Record annotation opt-in metrics every time user enters grid tab
    switcher. This metrics is recorded at most once within one day to
    mitigate the user bias.
    * Add a parameter to disallow user to opt out of the annotations. We may
    also hide the price tracking menu entry if no feature needs it.
    
    Bug: 1271306
    Change-Id: I3ca8e2f7d9b68b64d3d328cc7d7a67fb041158c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290734
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947132}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceTrackingUtilities.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceTrackingUtilities.java
@@ -19,6 +19,8 @@ import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.sync.ModelType;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * A class to handle price tracking-related features.
  */
@@ -27,6 +29,9 @@ public class PriceTrackingUtilities {
     public static final String PRICE_TRACKING_PARAM = "enable_price_tracking";
     @VisibleForTesting
     public static final String PRICE_NOTIFICATION_PARAM = "enable_price_notification";
+    @VisibleForTesting
+    public static final String ALLOW_DISABLE_PRICE_ANNOTATIONS_PARAM =
+            "allow_disable_price_annotations";
     @VisibleForTesting
     public static final String TRACK_PRICES_ON_TABS =
             ChromePreferenceKeys.PRICE_TRACKING_TRACK_PRICES_ON_TABS;
@@ -42,6 +47,8 @@ public class PriceTrackingUtilities {
     @VisibleForTesting
     public static final String PRICE_ALERTS_MESSAGE_CARD_SHOW_COUNT =
             ChromePreferenceKeys.PRICE_TRACKING_PRICE_ALERTS_MESSAGE_CARD_SHOW_COUNT;
+    private static final String PRICE_ANNOTATIONS_ENABLED_METRICS_WINDOW_DURATION_PARAM =
+            "price_annotations_enabled_metrics_window_duration_ms";
 
     @VisibleForTesting
     public static final SharedPreferencesManager SHARED_PREFERENCES_MANAGER =
@@ -226,5 +233,41 @@ public class PriceTrackingUtilities {
     public static boolean isTabModelPriceTrackingEligible(TabModel tabModel) {
         // Incognito Tabs are not eligible for price tracking.
         return !tabModel.getProfile().isOffTheRecord();
+    }
+
+    /**
+     * @return how frequent we want to record metrics on whether user enables the price tracking
+     *         annotations.
+     */
+    public static int getAnnotationsEnabledMetricsWindowDurationMilliSeconds() {
+        int defaultDuration = (int) TimeUnit.DAYS.toMillis(1);
+        if (FeatureList.isInitialized()) {
+            return ChromeFeatureList.getFieldTrialParamByFeatureAsInt(
+                    ChromeFeatureList.COMMERCE_PRICE_TRACKING,
+                    PRICE_ANNOTATIONS_ENABLED_METRICS_WINDOW_DURATION_PARAM, defaultDuration);
+        }
+        return defaultDuration;
+    }
+
+    /**
+     * @return whether we allow users to disable the price annotations feature.
+     */
+    public static boolean allowUsersToDisablePriceAnnotations() {
+        if (FeatureList.isInitialized()) {
+            return isPriceTrackingEnabled()
+                    && ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
+                            ChromeFeatureList.COMMERCE_PRICE_TRACKING,
+                            ALLOW_DISABLE_PRICE_ANNOTATIONS_PARAM, true);
+        }
+        return isPriceTrackingEnabled();
+    }
+
+    /**
+     * @return whether we should show the PriceTrackingSettings menu item in grid tab switcher.
+     */
+    public static boolean shouldShowPriceTrackingMenu() {
+        return isPriceTrackingEligible()
+                && (allowUsersToDisablePriceAnnotations()
+                        || getPriceTrackingNotificationsEnabled());
     }
 }
