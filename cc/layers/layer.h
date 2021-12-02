e53fe696788516a7244ba2f87397cb0d59db602b
Bartek Nowierski <bartekn@chromium.org>
2021-12-02 05:06:07 +0000

    Revert "Speculative fix for a lacros_all_tast_tests_eve failure"
    This reverts commit f3fab6bd644f0c4181a68308a8d5bf242809c023.
    
    Reason for revert: crrev.com/c/3304485 and crrev.com/c/3305643 fixed
    the underlying issue. This workaround should no longer be necessary.
    
    Original change's description:
    > Speculative fix for a lacros_all_tast_tests_eve failure
    >
    > Bug: 1272324
    > Change-Id: I3ebf7823508cc792d5f67f8a6a3f23c7cec3f5a8
    > No-Tree-Checks: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3303977
    > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > Owners-Override: Bartek Nowierski <bartekn@chromium.org>
    > Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945738}
    
    Bug: 1272324
    Change-Id: Ie7ec577e96685f79adbeaf491342808b26821b85
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306143
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947367}

--- cc/layers/layer.h
+++ cc/layers/layer.h
@@ -15,6 +15,7 @@
 
 #include "base/auto_reset.h"
 #include "base/callback.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "cc/base/region.h"
 #include "cc/benchmarks/micro_benchmark.h"
@@ -229,10 +230,12 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
   // for each matching pixel.
   void SetMaskLayer(scoped_refptr<PictureLayer> mask_layer);
   const PictureLayer* mask_layer() const {
-    return layer_tree_inputs() ? layer_tree_inputs()->mask_layer : nullptr;
+    return layer_tree_inputs() ? layer_tree_inputs()->mask_layer.get()
+                               : nullptr;
   }
   PictureLayer* mask_layer() {
-    return layer_tree_inputs() ? layer_tree_inputs()->mask_layer : nullptr;
+    return layer_tree_inputs() ? layer_tree_inputs()->mask_layer.get()
+                               : nullptr;
   }
 
   // Marks the |dirty_rect| as being changed, which will cause a commit and
@@ -901,7 +904,7 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
 
     // If not null, points to one of child layers which is set as mask layer
     // by SetMaskLayer().
-    PictureLayer* mask_layer = nullptr;
+    raw_ptr<PictureLayer> mask_layer = nullptr;
 
     float opacity = 1.0f;
     SkBlendMode blend_mode = SkBlendMode::kSrcOver;
@@ -956,12 +959,12 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
     std::vector<std::unique_ptr<viz::CopyOutputRequest>> copy_requests;
   };
 
-  Layer* parent_;
+  raw_ptr<Layer> parent_;
 
   // Layer instances have a weak pointer to their LayerTreeHost.
   // This pointer value is nil when a Layer is not in a tree and is
   // updated via SetLayerTreeHost() if a layer moves between trees.
-  LayerTreeHost* layer_tree_host_;
+  raw_ptr<LayerTreeHost> layer_tree_host_;
 
   Inputs inputs_;
   std::unique_ptr<LayerTreeInputs> layer_tree_inputs_;
