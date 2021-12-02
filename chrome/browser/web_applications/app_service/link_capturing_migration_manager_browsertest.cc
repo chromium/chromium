1cc99d91bf3d0ab53742c224a927ae3330949762
Alan Cutter <alancutter@chromium.org>
2021-12-02 00:51:01 +0000

    Web apps: Add basic test for LinkCapturingMigrationManager
    This CL adds a test to check that LinkCapturingMigrationManager is
    setting the link capturing preference to enabled for web apps that use
    the capture_links manifest field.
    
    Bug: 1262906
    Change-Id: I8587dc1021e171728cc7da8272311677d69ebbee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309980
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947290}

---
+++ chrome/browser/web_applications/app_service/link_capturing_migration_manager_browsertest.cc
@@ -0,0 +1,54 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/web_applications/os_integration_manager.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/browser/web_applications/web_application_info.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/test/browser_test.h"
+#include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
+
+namespace web_app {
+
+class LinkCapturingMigrationManagerBrowserTest : public InProcessBrowserTest {
+ public:
+  LinkCapturingMigrationManagerBrowserTest() {
+    os_hooks_supress_ = OsIntegrationManager::ScopedSuppressOsHooksForTesting();
+  }
+  ~LinkCapturingMigrationManagerBrowserTest() override = default;
+
+  // InProcessBrowserTest:
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+    web_app::test::WaitUntilReady(
+        web_app::WebAppProvider::GetForTest(browser()->profile()));
+  }
+
+ protected:
+  ScopedOsHooksSuppress os_hooks_supress_;
+};
+
+IN_PROC_BROWSER_TEST_F(LinkCapturingMigrationManagerBrowserTest,
+                       MigrateCaptureLinks) {
+  Profile* profile = browser()->profile();
+  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile);
+
+  auto web_app_info = std::make_unique<WebApplicationInfo>();
+  web_app_info->title = u"Test app";
+  web_app_info->start_url = GURL("https://example.org");
+  web_app_info->capture_links =
+      blink::mojom::CaptureLinks::kExistingClientNavigate;
+  AppId app_id = test::InstallWebApp(profile, std::move(web_app_info));
+  proxy->FlushMojoCallsForTesting();
+
+  EXPECT_EQ(proxy->PreferredApps().FindPreferredAppForUrl(
+                GURL("https://example.org/some/path")),
+            app_id);
+}
+
+}  // namespace web_app
