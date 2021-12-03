8c49996aab9832898a52c867b79d63afbe326ece
Alexis Hetu <sugoi@google.com>
2021-12-03 23:17:43 +0000

    Disallow using gfx::BufferFormat::BGRX_8888 on MacOS with SwANGLE
    gfx::BufferFormat::BGRX_8888 is causing issues on MacOS with
    SwANGLE when a WebGL-rendered canvas with alpha:false is using
    an IOSurface as its backing storage. This CL disables the use of
    gfx::BufferFormat::BGRX_8888 only on MacOS with SwANGLE, so that
    we can use the emulation path for now and fix the final WebGL
    Conformance Test failures on MacOS with SwANGLE.
    
    Bug: chromium:1099979
    Bug: chromium:1276529
    Change-Id: I22556f18e44a68ed214c593ad17254a33b453647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312887
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948219}

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -993,7 +993,19 @@ void FeatureInfo::InitializeFeatures() {
     validators_.texture_sized_texture_filterable_internal_format.AddValue(
         GL_BGRA8_EXT);
     feature_flags_.gpu_memory_buffer_formats.Add(gfx::BufferFormat::BGRA_8888);
-    feature_flags_.gpu_memory_buffer_formats.Add(gfx::BufferFormat::BGRX_8888);
+#if defined(OS_MAC)
+    // TODO(sugoi): Remove this once crbug.com/1276529 is fixed.
+    // On Mac OS, DrawingBuffer is using an IOSurface as its backing storage,
+    // this allows WebGL-rendered canvases to be composited by the OS rather
+    // than Chrome. Currently, this causes an issue on MacOS with SwANGLE when
+    // alpha is false, so disable that case for now so that we go through
+    // emulation.
+    if (!gl_version_info_->is_angle_swiftshader)
+#endif
+    {
+      feature_flags_.gpu_memory_buffer_formats.Add(
+          gfx::BufferFormat::BGRX_8888);
+    }
   }
 
   // On desktop, all devices support BGRA render buffers (note that on desktop
