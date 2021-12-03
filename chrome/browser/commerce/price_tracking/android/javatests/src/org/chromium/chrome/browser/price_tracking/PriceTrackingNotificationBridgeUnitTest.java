2eba93590c223d3bcdc10cdc833499f9c5b1b35c
Xing Liu <xingliu@chromium.org>
2021-12-03 20:06:59 +0000

    Price tracking: Don't create the channel on start up.
    Currently the price drop notification channel is created when
    PriceTrackingNotificationBridge is created. The requirement is to
    create the channel based on commerce UI interaction.
    
    (cherry picked from commit bae227fea192c22591a4a839e0365a98049e4468)
    
    Bug: 1270560
    Change-Id: If135d9432f1831d6a9f41b6f1464f861fe2b7470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283708
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#943308}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307359
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#686}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceTrackingNotificationBridgeUnitTest.java
+++ chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceTrackingNotificationBridgeUnitTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.price_tracking;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -65,6 +66,8 @@ public class PriceTrackingNotificationBridgeUnitTest {
 
     @Mock
     PriceDropNotifier mNotifier;
+    @Mock
+    PriceDropNotificationManager mPriceDropNotificationManager;
 
     @Captor
     ArgumentCaptor<PriceDropNotifier.NotificationData> mNotificationDataCaptor;
@@ -79,7 +82,9 @@ public class PriceTrackingNotificationBridgeUnitTest {
                 .when(currencyFormatterJniMock)
                 .format(Mockito.anyLong(), Mockito.any(CurrencyFormatter.class),
                         Mockito.anyString());
-        mPriceTrackingNotificationBridge = new PriceTrackingNotificationBridge(0, mNotifier);
+        mPriceTrackingNotificationBridge =
+                new PriceTrackingNotificationBridge(0, mNotifier, mPriceDropNotificationManager);
+        when(mPriceDropNotificationManager.canPostNotification()).thenReturn(true);
     }
 
     // Creates a ChromeNotification.Builder that sets a valid ChromeNotification proto.
@@ -151,6 +156,14 @@ public class PriceTrackingNotificationBridgeUnitTest {
         Assert.assertEquals(ACTION_ID_1, data.actions.get(1).actionId);
         Assert.assertEquals(ACTION_TEXT_1, data.actions.get(1).text);
         Assert.assertEquals(String.valueOf(OFFER_ID), data.offerId);
+    }
+
+    @Test
+    public void testShowNotification_ChannelNotCreated() {
+        when(mPriceDropNotificationManager.canPostNotification()).thenReturn(false);
+        mPriceTrackingNotificationBridge.showNotification(
+                createValidChromeNotification().build().toByteArray());
+        verify(mNotifier, times(0)).showNotification(any());
     }
 
     @Test
