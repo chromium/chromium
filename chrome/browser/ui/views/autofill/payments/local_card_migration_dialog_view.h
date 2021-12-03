28265d547872a64b3b6ef00249848fbbcff659f0
Siyu An <siyua@chromium.org>
2021-12-03 08:24:15 +0000

    [M97 Merge] Fix web_contents_ in LocalCardMigrationDialogView
    Make it a weak ptr instead of raw ptr and abort showing if web_contents_
    does not exist.
    
    (cherry picked from commit f151ba1355794fc39e22cb3afec7e8c04acfc4ea)
    
    Bug: 1273674
    Change-Id: I0905a81fafe294df7065e4fae6ffcaedfca83916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308220
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#946658}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312916
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#666}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.h
+++ chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_PAYMENTS_LOCAL_CARD_MIGRATION_DIALOG_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_PAYMENTS_LOCAL_CARD_MIGRATION_DIALOG_VIEW_H_
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/autofill/payments/local_card_migration_dialog.h"
 #include "chrome/browser/ui/views/autofill/payments/dialog_view_ids.h"
 #include "components/autofill/core/browser/ui/payments/local_card_migration_dialog_controller.h"
@@ -57,7 +58,7 @@ class LocalCardMigrationDialogView : public LocalCardMigrationDialog,
 
   LocalCardMigrationDialogController* controller_;
 
-  content::WebContents* web_contents_;
+  base::WeakPtr<content::WebContents> web_contents_;
 
   // Pointer points to the LocalCardMigrationOfferView. Can be null when the
   // dialog is not in the 'offer' state.
