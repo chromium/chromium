98a26e92daab389aa3f9d2f495b17098d6551edd
Joel Hockey <joelhockey@chromium.org>
2021-12-06 02:43:44 +0000

    Revert "personalization: turn on wallpaper SWA feature"
    This reverts commit 60b3c74b7f2ca17a28907fb0b40d9dabeaa48326.
    
    Reason for revert: b/209325799
    DCHECKS in wallpaper_controller_impl.cc(1136) for linux-chromeos
    
    Original change's description:
    > personalization: turn on wallpaper SWA feature
    >
    > BUG=b:197963464
    >
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Change-Id: I4bfd581a5d3942e2da1dcaee4e967ad155258b64
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311341
    > Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#947793}
    
    Bug: b:197963464, b:209325799
    Change-Id: I34c6a1cf6fa86883b745d37c0cbc9a00c09f3d88
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312775
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948431}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1267,9 +1267,9 @@ const base::Feature kVirtualKeyboardMultipasteSuggestion{
 const base::Feature kWakeOnWifiAllowed{"WakeOnWifiAllowed",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
-// Enable new wallpaper experience SWA.
+// Enable new wallpaper experience in WebUI inside system settings.
 const base::Feature kWallpaperWebUI{"WallpaperWebUI",
-                                    base::FEATURE_ENABLED_BY_DEFAULT};
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enable full screen wallpaper preview in new wallpaper experience. Requires
 // |kWallpaperWebUI| to also be enabled.
