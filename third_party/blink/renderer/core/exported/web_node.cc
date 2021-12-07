2f1869faf2d9c8a78d752196c3291412d255ed73
Kevin McNee <mcnee@chromium.org>
2021-12-07 22:30:01 +0000

    DCHECK that blink::WebNode handles aren't created on a worker thread
    This can only be safely used from the main thread.
    
    Bug: 1276570
    Change-Id: I2aaa8a363d5e5f49271daaf001f373fb12162d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321083
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949209}

--- third_party/blink/renderer/core/exported/web_node.cc
+++ third_party/blink/renderer/core/exported/web_node.cc
@@ -53,6 +53,7 @@
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/to_v8.h"
+#include "third_party/blink/renderer/platform/wtf/wtf.h"
 
 namespace blink {
 
@@ -230,7 +231,9 @@ WebPluginContainer* WebNode::PluginContainer() const {
   return private_->GetWebPluginContainer();
 }
 
-WebNode::WebNode(Node* node) : private_(node) {}
+WebNode::WebNode(Node* node) : private_(node) {
+  DCHECK(IsMainThread());
+}
 
 WebNode& WebNode::operator=(Node* node) {
   private_ = node;
