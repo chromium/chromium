010b3ea336e25819f3d689710c802d07df7646d7
Ewann <ewannpv@google.com>
2021-12-07 18:49:22 +0000

    [iOS][MobileConfig] Check scheme
    SFSafariViewController can only open http and https URLs.
    This CL checks that the given URL is correct. If the URL isn't correct,
    the given URL is downloaded using the "open in.." toolbar.
    
    (cherry picked from commit e9227607883843839c206c350eeedb67b769ba3d)
    
    Bug: 1269478
    Change-Id: I71fc6e2eea070d1ef66f4c6f313159196674b9ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3317545
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#948601}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318458
    Cr-Commit-Position: refs/branch-heads/4692@{#780}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- ios/chrome/browser/download/browser_download_service.mm
+++ ios/chrome/browser/download/browser_download_service.mm
@@ -126,7 +126,9 @@ void BrowserDownloadService::OnDownloadCreated(
     if (tab_helper) {
       tab_helper->Download(std::move(task));
     }
-  } else if (task->GetMimeType() == kMobileConfigurationType) {
+    // SFSafariViewController can only open http and https URLs.
+  } else if (task->GetMimeType() == kMobileConfigurationType &&
+             task->GetOriginalUrl().SchemeIsHTTPOrHTTPS()) {
     MobileConfigTabHelper* tab_helper =
         MobileConfigTabHelper::FromWebState(web_state);
     if (tab_helper) {
