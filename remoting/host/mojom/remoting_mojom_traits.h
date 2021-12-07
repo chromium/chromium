48a96d9cee5becf3654b8fbebe93088acf5638ad
Joe Downing <joedow@google.com>
2021-12-07 00:06:03 +0000

    Adding a shared ArrayTraits impl for repeated protobuf fields
    Change-Id: Icd1517016e220c5c6f7318b0c54493177f3473e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315755
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948747}

--- remoting/host/mojom/remoting_mojom_traits.h
+++ remoting/host/mojom/remoting_mojom_traits.h
@@ -8,9 +8,8 @@
 #include <stddef.h>
 #include <string>
 
-#include "base/numerics/safe_conversions.h"
 #include "mojo/public/cpp/base/byte_string_mojom_traits.h"
-#include "mojo/public/cpp/bindings/array_traits.h"
+#include "mojo/public/cpp/bindings/array_traits_protobuf.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "remoting/host/mojom/desktop_session.mojom-shared.h"
@@ -292,61 +291,6 @@ class mojo::StructTraits<remoting::mojom::TouchEventPointDataView,
 
   static bool Read(remoting::mojom::TouchEventPointDataView data_view,
                    ::remoting::protocol::TouchEventPoint* out_event);
-};
-
-// TODO(joedow): Move this to //mojo/public/cpp/bindings.
-template <typename T>
-struct ArrayTraits<::google::protobuf::RepeatedPtrField<T>> {
-  using Element = T;
-
-  static bool IsNull(const ::google::protobuf::RepeatedPtrField<T>& input) {
-    // Always convert RepeatedPtrField to a non-null mojom array.
-    return false;
-  }
-
-  static T* GetData(::google::protobuf::RepeatedPtrField<T>& input) {
-    return input.data();
-  }
-
-  static const T* GetData(
-      const ::google::protobuf::RepeatedPtrField<T>& input) {
-    return input.data();
-  }
-
-  static T& GetAt(::google::protobuf::RepeatedPtrField<T>& input,
-                  size_t index) {
-    return input.at(index);
-  }
-
-  static const T& GetAt(const ::google::protobuf::RepeatedPtrField<T>& input,
-                        size_t index) {
-    return input.at(index);
-  }
-
-  static size_t GetSize(const ::google::protobuf::RepeatedPtrField<T>& input) {
-    return input.size();
-  }
-
-  static bool Resize(::google::protobuf::RepeatedPtrField<T>& input,
-                     size_t new_size) {
-    if (!base::IsValueInRangeForNumericType<int>(new_size)) {
-      return false;
-    }
-
-    // We call Reserve() to set the capacity and then add elements to increase
-    // the container size to the requested value. We can't rely on Reserve()
-    // alone as that will resize the container but size() will still report the
-    // previous number of elements which will cause deserialization failures.
-    // Unfortunately there isn't an AddRange() or similar function available so
-    // we need to add elements one at a time in a loop.
-    int requested_size = base::checked_cast<int>(new_size);
-    input.Reserve(requested_size);
-    while (input.size() < requested_size) {
-      input.Add();
-    }
-
-    return true;
-  }
 };
 
 template <>
