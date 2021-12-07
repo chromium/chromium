f858ac2450650ad337f65a14a5fd257c820997e2
Erik Chen <erikchen@chromium.org>
2021-12-07 04:20:48 +0000

    Lacros: Specify $HOME for ppapi plugins.
    This CL simply causes lacros to match the behavior of ash.
    
    Bug: 1269553
    Change-Id: Ic6a49d134a5a9f31ed08c8441646ab98de6b4bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315170
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948835}

--- content/ppapi_plugin/ppapi_plugin_main.cc
+++ content/ppapi_plugin/ppapi_plugin_main.cc
@@ -15,7 +15,6 @@
 #include "base/threading/platform_thread.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "content/child/child_process.h"
 #include "content/common/content_constants_internal.h"
 #include "content/common/content_switches_internal.h"
@@ -41,7 +40,7 @@
 #include "ui/gfx/win/direct_write.h"
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(OS_CHROMEOS)
 #include "base/files/file_util.h"
 #endif
 
@@ -118,7 +117,7 @@ int PpapiPluginMain(MainFunctionParams parameters) {
         icu::TimeZone::createTimeZone(icu::UnicodeString(time_zone.c_str())));
   }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(OS_CHROMEOS)
   // Specifies $HOME explicitly because some plugins rely on $HOME but
   // no other part of Chrome OS uses that.  See crbug.com/335290.
   base::FilePath homedir;
