e63a2e1879fbb1074473e9904d0f040ea05163e9
Peter Kasting <pkasting@chromium.org>
2021-12-08 18:05:21 +0000

    Factor out AccentColorObserver.
    This will allow the color pipeline to refer to the Windows native accent
    color.
    
    Bug: 1245688
    Change-Id: I8f733c3ecbdb6d8607d23368801835858789c6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322135
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949606}

--- chrome/browser/themes/theme_helper_win.cc
+++ chrome/browser/themes/theme_helper_win.cc
@@ -14,116 +14,14 @@
 #include "chrome/grit/theme_resources.h"
 #include "skia/ext/skia_utils_win.h"
 #include "ui/base/win/shell.h"
+#include "ui/color/win/accent_color_observer.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/views_features.h"
 
 namespace {
 
-SkColor GetDefaultInactiveFrameColor() {
-  return base::win::GetVersion() < base::win::Version::WIN10
-             ? SkColorSetRGB(0xEB, 0xEB, 0xEB)
-             : SK_ColorWHITE;
-}
-
-}  // namespace
-
-ThemeHelperWin::ThemeHelperWin() {
-  // This just checks for Windows 8+ instead of calling DwmColorsAllowed()
-  // because we want to monitor the frame color even when a custom frame is in
-  // use, so that it will be correct if at any time the user switches to the
-  // native frame.
-  if (base::win::GetVersion() >= base::win::Version::WIN8) {
-    dwm_key_ = std::make_unique<base::win::RegKey>(
-        HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\DWM", KEY_READ);
-    if (dwm_key_->Valid())
-      OnDwmKeyUpdated();
-    else
-      dwm_key_.reset();
-  }
-}
-
-ThemeHelperWin::~ThemeHelperWin() = default;
-
-bool ThemeHelperWin::ShouldUseNativeFrame(
-    const CustomThemeSupplier* theme_supplier) const {
-  const bool use_native_frame_if_enabled =
-      ShouldCustomDrawSystemTitlebar() ||
-      !HasCustomImage(IDR_THEME_FRAME, theme_supplier);
-  return use_native_frame_if_enabled && ui::win::IsAeroGlassEnabled();
-}
-
-bool ThemeHelperWin::ShouldUseIncreasedContrastThemeSupplier(
-    ui::NativeTheme* native_theme) const {
-  // On Windows the platform provides the high contrast colors, so don't use the
-  // IncreasedContrastThemeSupplier.
-  return false;
-}
-
-SkColor ThemeHelperWin::GetDefaultColor(
-    int id,
-    bool incognito,
-    const CustomThemeSupplier* theme_supplier) const {
-  // In high contrast mode on Windows the platform provides the color. Try to
-  // get that color first.
-  SkColor color;
-  if (ui::NativeTheme::GetInstanceForNativeUi()->InForcedColorsMode() &&
-      GetPlatformHighContrastColor(id, &color)) {
-    return color;
-  }
-
-  if (DwmColorsAllowed(theme_supplier)) {
-    // In Windows 10, native inactive borders are #555555 with 50% alpha.
-    // Prior to version 1809, native active borders use the accent color.
-    // In version 1809 and following, the active border is #262626 with 66%
-    // alpha unless the accent color is also used for the frame.
-    if (id == ThemeProperties::COLOR_ACCENT_BORDER_ACTIVE) {
-      return (base::win::GetVersion() >= base::win::Version::WIN10_RS5 &&
-              !dwm_frame_color_)
-                 ? SkColorSetARGB(0xa8, 0x26, 0x26, 0x26)
-                 : dwm_accent_border_color_;
-    }
-    if (id == ThemeProperties::COLOR_ACCENT_BORDER_INACTIVE)
-      return SkColorSetARGB(0x80, 0x55, 0x55, 0x55);
-
-    // When we're custom-drawing the titlebar we want to use either the colors
-    // we calculated in OnDwmKeyUpdated() or the default colors. When we're not
-    // custom-drawing the titlebar we want to match the color Windows actually
-    // uses because some things (like the incognito icon) use this color to
-    // decide whether they should draw in light or dark mode. Incognito colors
-    // should be the same as non-incognito in all cases here.
-    if (id == ThemeProperties::COLOR_FRAME_ACTIVE) {
-      if (dwm_frame_color_)
-        return dwm_frame_color_.value();
-      if (!ShouldCustomDrawSystemTitlebar())
-        return SK_ColorWHITE;
-      // Fall through and use default.
-    }
-    if (id == ThemeProperties::COLOR_FRAME_INACTIVE) {
-      if (!ShouldCustomDrawSystemTitlebar()) {
-        return inactive_frame_color_from_registry_
-                   ? dwm_inactive_frame_color_.value()
-                   : GetDefaultInactiveFrameColor();
-      }
-      if (dwm_frame_color_ && !inactive_frame_color_from_registry_) {
-        // Tint to create inactive color. Always use the non-incognito version
-        // of the tint, since the frame should look the same in both modes.
-        return color_utils::HSLShift(
-            dwm_frame_color_.value(),
-            GetTint(ThemeProperties::TINT_FRAME_INACTIVE, false,
-                    theme_supplier));
-      }
-      if (dwm_inactive_frame_color_)
-        return dwm_inactive_frame_color_.value();
-      // Fall through and use default.
-    }
-  }
-
-  return ThemeHelper::GetDefaultColor(id, incognito, theme_supplier);
-}
-
-bool ThemeHelperWin::GetPlatformHighContrastColor(int id,
-                                                  SkColor* color) const {
+bool GetPlatformHighContrastColor(int id, SkColor* color) {
   ui::NativeTheme::SystemThemeColor system_theme_color =
       ui::NativeTheme::SystemThemeColor::kNotSupported;
 
@@ -229,71 +127,117 @@ bool ThemeHelperWin::GetPlatformHighContrastColor(int id,
   return true;
 }
 
+SkColor GetDefaultInactiveFrameColor() {
+  return base::win::GetVersion() < base::win::Version::WIN10
+             ? SkColorSetRGB(0xEB, 0xEB, 0xEB)
+             : SK_ColorWHITE;
+}
+
+}  // namespace
+
+ThemeHelperWin::ThemeHelperWin() {
+  subscription_ = ui::AccentColorObserver::Get()->Subscribe(base::BindRepeating(
+      &ThemeHelperWin::OnAccentColorUpdated, base::Unretained(this)));
+  FetchAccentColors();
+}
+
+ThemeHelperWin::~ThemeHelperWin() = default;
+
+bool ThemeHelperWin::ShouldUseNativeFrame(
+    const CustomThemeSupplier* theme_supplier) const {
+  const bool use_native_frame_if_enabled =
+      ShouldCustomDrawSystemTitlebar() ||
+      !HasCustomImage(IDR_THEME_FRAME, theme_supplier);
+  return use_native_frame_if_enabled && ui::win::IsAeroGlassEnabled();
+}
+
+bool ThemeHelperWin::ShouldUseIncreasedContrastThemeSupplier(
+    ui::NativeTheme* native_theme) const {
+  // On Windows the platform provides the high contrast colors, so don't use the
+  // IncreasedContrastThemeSupplier.
+  return false;
+}
+
+SkColor ThemeHelperWin::GetDefaultColor(
+    int id,
+    bool incognito,
+    const CustomThemeSupplier* theme_supplier) const {
+  // In high contrast mode on Windows the platform provides the color. Try to
+  // get that color first.
+  SkColor color;
+  if (ui::NativeTheme::GetInstanceForNativeUi()->InForcedColorsMode() &&
+      GetPlatformHighContrastColor(id, &color)) {
+    return color;
+  }
+
+  if (DwmColorsAllowed(theme_supplier)) {
+    // In Windows 10, native inactive borders are #555555 with 50% alpha.
+    // Prior to version 1809, native active borders use the accent color.
+    // In version 1809 and following, the active border is #262626 with 66%
+    // alpha unless the accent color is also used for the frame.
+    if (id == ThemeProperties::COLOR_ACCENT_BORDER_ACTIVE) {
+      return (base::win::GetVersion() >= base::win::Version::WIN10_RS5 &&
+              !dwm_frame_color_)
+                 ? SkColorSetARGB(0xa8, 0x26, 0x26, 0x26)
+                 : dwm_accent_border_color_;
+    }
+    if (id == ThemeProperties::COLOR_ACCENT_BORDER_INACTIVE)
+      return SkColorSetARGB(0x80, 0x55, 0x55, 0x55);
+
+    // When we're custom-drawing the titlebar we want to use either the colors
+    // we calculated in OnDwmKeyUpdated() or the default colors. When we're not
+    // custom-drawing the titlebar we want to match the color Windows actually
+    // uses because some things (like the incognito icon) use this color to
+    // decide whether they should draw in light or dark mode. Incognito colors
+    // should be the same as non-incognito in all cases here.
+    if (id == ThemeProperties::COLOR_FRAME_ACTIVE) {
+      if (dwm_frame_color_)
+        return dwm_frame_color_.value();
+      if (!ShouldCustomDrawSystemTitlebar())
+        return SK_ColorWHITE;
+      // Fall through and use default.
+    }
+    if (id == ThemeProperties::COLOR_FRAME_INACTIVE) {
+      if (dwm_inactive_frame_color_)
+        return dwm_inactive_frame_color_.value();
+      if (!ShouldCustomDrawSystemTitlebar())
+        return GetDefaultInactiveFrameColor();
+      if (dwm_frame_color_) {
+        // Tint to create inactive color. Always use the non-incognito version
+        // of the tint, since the frame should look the same in both modes.
+        return color_utils::HSLShift(
+            dwm_frame_color_.value(),
+            GetTint(ThemeProperties::TINT_FRAME_INACTIVE, false,
+                    theme_supplier));
+      }
+      // Fall through and use default.
+    }
+  }
+
+  return ThemeHelper::GetDefaultColor(id, incognito, theme_supplier);
+}
+
 bool ThemeHelperWin::DwmColorsAllowed(
     const CustomThemeSupplier* theme_supplier) const {
   return ShouldUseNativeFrame(theme_supplier) &&
          (base::win::GetVersion() >= base::win::Version::WIN8);
 }
 
-void ThemeHelperWin::OnDwmKeyUpdated() {
-  dwm_accent_border_color_ = GetDefaultInactiveFrameColor();
-  DWORD colorization_color, colorization_color_balance;
-  if ((dwm_key_->ReadValueDW(L"ColorizationColor", &colorization_color) ==
-       ERROR_SUCCESS) &&
-      (dwm_key_->ReadValueDW(L"ColorizationColorBalance",
-                             &colorization_color_balance) == ERROR_SUCCESS)) {
-    // The accent border color is a linear blend between the colorization
-    // color and the neutral #d9d9d9. colorization_color_balance is the
-    // percentage of the colorization color in that blend.
-    //
-    // On Windows version 1611 colorization_color_balance can be 0xfffffff3 if
-    // the accent color is taken from the background and either the background
-    // is a solid color or was just changed to a slideshow. It's unclear what
-    // that value's supposed to mean, so change it to 80 to match Edge's
-    // behavior.
-    if (colorization_color_balance > 100)
-      colorization_color_balance = 80;
+void ThemeHelperWin::OnAccentColorUpdated() {
+  FetchAccentColors();
+  ui::NativeTheme::GetInstanceForNativeUi()->NotifyOnNativeThemeUpdated();
+}
 
-    // colorization_color's high byte is not an alpha value, so replace it
-    // with 0xff to make an opaque ARGB color.
-    SkColor input_color = SkColorSetA(colorization_color, 0xff);
+void ThemeHelperWin::FetchAccentColors() {
+  const auto* accent_color_observer = ui::AccentColorObserver::Get();
+  dwm_accent_border_color_ =
+      accent_color_observer->accent_border_color().value_or(
+          GetDefaultInactiveFrameColor());
 
-    dwm_accent_border_color_ =
-        color_utils::AlphaBlend(input_color, SkColorSetRGB(0xd9, 0xd9, 0xd9),
-                                colorization_color_balance / 100.0f);
-  }
-
-  inactive_frame_color_from_registry_ = false;
   if (base::win::GetVersion() < base::win::Version::WIN10) {
     dwm_frame_color_ = dwm_accent_border_color_;
   } else {
-    DWORD accent_color, color_prevalence;
-    bool use_dwm_frame_color =
-        dwm_key_->ReadValueDW(L"AccentColor", &accent_color) == ERROR_SUCCESS &&
-        dwm_key_->ReadValueDW(L"ColorPrevalence", &color_prevalence) ==
-            ERROR_SUCCESS &&
-        color_prevalence == 1;
-    if (use_dwm_frame_color) {
-      dwm_frame_color_ = skia::COLORREFToSkColor(accent_color);
-      DWORD accent_color_inactive;
-      if (dwm_key_->ReadValueDW(L"AccentColorInactive",
-                                &accent_color_inactive) == ERROR_SUCCESS) {
-        dwm_inactive_frame_color_ =
-            skia::COLORREFToSkColor(accent_color_inactive);
-        inactive_frame_color_from_registry_ = true;
-      }
-    } else {
-      dwm_frame_color_.reset();
-      dwm_inactive_frame_color_.reset();
-    }
-  }
-
-  // Notify native theme observers that the native theme has changed.
-  ui::NativeTheme::GetInstanceForNativeUi()->NotifyOnNativeThemeUpdated();
-
-  // Watch for future changes.
-  if (!dwm_key_->StartWatching(base::BindOnce(&ThemeHelperWin::OnDwmKeyUpdated,
-                                              base::Unretained(this)))) {
-    dwm_key_.reset();
+    dwm_frame_color_ = accent_color_observer->accent_color();
+    dwm_inactive_frame_color_ = accent_color_observer->accent_color_inactive();
   }
 }
