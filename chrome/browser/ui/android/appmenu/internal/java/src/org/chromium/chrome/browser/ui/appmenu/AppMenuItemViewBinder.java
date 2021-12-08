4182ef0e62c7ff3e2ace911ffb555c461f464a55
Sinan Sahin <sinansahin@google.com>
2021-12-08 18:53:24 +0000

    [GMNext] Make CheckBox and RadioButton dynamic
    This CL updates the CheckBox and RadioButton styles to Material3.
    However, it removes the min-width and min-height from the styles as the
    surfaces that use these controls already add their own padding/margins
    and uses a custom buttonTint since the specced color for the unchecked
    state is different than the M3 provided one.
    
    Bug: 1228454
    Change-Id: Ide58f4f57534e42a4cc661bc45d87552d1f132fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321901
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#949638}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
@@ -128,7 +128,7 @@ class AppMenuItemViewBinder {
                 checkbox.setChecked(checked);
                 ApiCompatibilityUtils.setImageTintList(checkbox,
                         AppCompatResources.getColorStateList(
-                                checkbox.getContext(), R.color.checkbox_tint));
+                                checkbox.getContext(), R.color.selection_control_button_tint));
                 setupMenuButton(checkbox, buttonModel, appMenuClickHandler);
             } else if (subIcon != null) {
                 // Display an icon alongside the MenuItem.
