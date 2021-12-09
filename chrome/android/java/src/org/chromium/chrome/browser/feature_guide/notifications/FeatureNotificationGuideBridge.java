e3548d0a5dcc3f12b30a3dad0e55a8e90f8b598b
Shakti Sahu <shaktisahu@chromium.org>
2021-12-09 18:44:32 +0000

    [feature_notification_guide] Added strings
    Bug: 1277400
    Change-Id: I0e69b3dc21ba432dcef8e4a95df936824201e60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320079
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950185}

--- chrome/android/java/src/org/chromium/chrome/browser/feature_guide/notifications/FeatureNotificationGuideBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feature_guide/notifications/FeatureNotificationGuideBridge.java
@@ -4,8 +4,12 @@
 
 package org.chromium.chrome.browser.feature_guide.notifications;
 
+import androidx.annotation.StringRes;
+
+import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.chrome.R;
 
 /**
  * Contains JNI methods needed by the feature notification guide.
@@ -30,12 +34,36 @@ public final class FeatureNotificationGuideBridge implements FeatureNotification
 
     @CalledByNative
     private String getNotificationTitle(@FeatureType int featureType) {
-        return null;
+        return ContextUtils.getApplicationContext().getResources().getString(
+                R.string.feature_notification_guide_notification_title);
     }
 
     @CalledByNative
     private String getNotificationMessage(@FeatureType int featureType) {
-        return null;
+        @StringRes
+        int stringId = 0;
+        switch (featureType) {
+            case FeatureType.DEFAULT_BROWSER:
+                stringId = R.string.feature_notification_guide_notification_message_default_browser;
+                break;
+            case FeatureType.SIGN_IN:
+                stringId = R.string.feature_notification_guide_notification_message_sign_in;
+                break;
+            case FeatureType.INCOGNITO_TAB:
+                stringId = R.string.feature_notification_guide_notification_message_incognito_tab;
+                break;
+            case FeatureType.NTP_SUGGESTION_CARD:
+                stringId =
+                        R.string.feature_notification_guide_notification_message_ntp_suggestion_card;
+                break;
+            case FeatureType.VOICE_SEARCH:
+                stringId = R.string.feature_notification_guide_notification_message_voice_search;
+                break;
+            default:
+                assert false : "Found unknown feature type " + featureType;
+                break;
+        }
+        return ContextUtils.getApplicationContext().getResources().getString(stringId);
     }
 
     @CalledByNative
