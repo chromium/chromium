09206eda7616b621c29e87c1b0ee020dbf8f6491
Simon Zünd <szuend@chromium.org>
2021-12-09 06:25:07 +0000

    [devtools] Migrate RemotePlayback to use AsyncTaskContext
    Doc: https://goo.gle/devtools-async-tasks
    
    Bug: 1275875
    Change-Id: If87fdef9653df3dce1e64b9b19b1f43c79a70cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320393
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949949}

--- third_party/blink/renderer/modules/remoteplayback/remote_playback.cc
+++ third_party/blink/renderer/modules/remoteplayback/remote_playback.cc
@@ -18,7 +18,7 @@
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/core/html/media/remote_playback_observer.h"
 #include "third_party/blink/renderer/core/html_names.h"
-#include "third_party/blink/renderer/core/probe/async_task_id.h"
+#include "third_party/blink/renderer/core/probe/async_task_context.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
 #include "third_party/blink/renderer/modules/presentation/presentation_availability_state.h"
@@ -54,10 +54,11 @@ const AtomicString& RemotePlaybackStateToString(
   }
 }
 
-void RunRemotePlaybackTask(ExecutionContext* context,
-                           base::OnceClosure task,
-                           std::unique_ptr<probe::AsyncTaskId> task_id) {
-  probe::AsyncTask async_task(context, task_id.get());
+void RunRemotePlaybackTask(
+    ExecutionContext* context,
+    base::OnceClosure task,
+    std::unique_ptr<probe::AsyncTaskContext> task_context) {
+  probe::AsyncTask async_task(context, task_context.get());
   std::move(task).Run();
 }
 
@@ -276,15 +277,15 @@ void RemotePlayback::PromptInternal() {
     base::OnceClosure task =
         WTF::Bind(&RemotePlayback::PromptCancelled, WrapPersistent(this));
 
-    std::unique_ptr<probe::AsyncTaskId> task_id =
-        std::make_unique<probe::AsyncTaskId>();
-    probe::AsyncTaskScheduled(GetExecutionContext(), "promptCancelled",
-                              task_id.get());
+    std::unique_ptr<probe::AsyncTaskContext> task_context =
+        std::make_unique<probe::AsyncTaskContext>();
+    task_context->Schedule(GetExecutionContext(), "promptCancelled");
     GetExecutionContext()
         ->GetTaskRunner(TaskType::kMediaElementEvent)
-        ->PostTask(FROM_HERE, WTF::Bind(RunRemotePlaybackTask,
-                                        WrapPersistent(GetExecutionContext()),
-                                        std::move(task), std::move(task_id)));
+        ->PostTask(FROM_HERE,
+                   WTF::Bind(RunRemotePlaybackTask,
+                             WrapPersistent(GetExecutionContext()),
+                             std::move(task), std::move(task_context)));
   }
 }
 
@@ -309,15 +310,15 @@ int RemotePlayback::WatchAvailabilityInternal(
   // We can remove the wrapper if InspectorInstrumentation returns a task id.
   base::OnceClosure task = WTF::Bind(&RemotePlayback::NotifyInitialAvailability,
                                      WrapPersistent(this), id);
-  std::unique_ptr<probe::AsyncTaskId> task_id =
-      std::make_unique<probe::AsyncTaskId>();
-  probe::AsyncTaskScheduled(GetExecutionContext(), "watchAvailabilityCallback",
-                            task_id.get());
+  std::unique_ptr<probe::AsyncTaskContext> task_context =
+      std::make_unique<probe::AsyncTaskContext>();
+  task_context->Schedule(GetExecutionContext(), "watchAvailabilityCallback");
   GetExecutionContext()
       ->GetTaskRunner(TaskType::kMediaElementEvent)
-      ->PostTask(FROM_HERE, WTF::Bind(RunRemotePlaybackTask,
-                                      WrapPersistent(GetExecutionContext()),
-                                      std::move(task), std::move(task_id)));
+      ->PostTask(FROM_HERE,
+                 WTF::Bind(RunRemotePlaybackTask,
+                           WrapPersistent(GetExecutionContext()),
+                           std::move(task), std::move(task_context)));
 
   MaybeStartListeningForAvailability();
   return id;
