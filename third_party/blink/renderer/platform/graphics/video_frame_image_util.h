e7f56464e17481577eef01cc7190a43c3bad3c41
Jose Dapena Paz <jdapena@igalia.com>
2021-12-09 18:32:21 +0000

    IWYU: unique_ptr usage in video_frame_image_util.h.
    Fix build:
        ../../third_party/blink/renderer/platform/graphics/video_frame_image_util.h:113:22: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
          113 | PLATFORM_EXPORT std::unique_ptr<CanvasResourceProvider>
              |                      ^~~~~~~~~~
    
    Bug: 957519
    Change-Id: I83474da9a374cb8ba36ba014d5cb25e4a8b082d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3326824
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950181}

--- third_party/blink/renderer/platform/graphics/video_frame_image_util.h
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util.h
@@ -5,6 +5,8 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_VIDEO_FRAME_IMAGE_UTIL_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_VIDEO_FRAME_IMAGE_UTIL_H_
 
+#include <memory>
+
 #include "base/memory/scoped_refptr.h"
 #include "media/base/video_transformation.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
