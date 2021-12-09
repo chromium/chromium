f14d7482a4e56839a0740c2a93e0109f5d01a20c
Ewann <ewannpv@google.com>
2021-12-09 10:36:28 +0000

    [iOS][Enterprise] Cleanup EnterpriseSignoutCoordinator
    This CL cleans dead code from a previous implementation.
    
    Bug: 1277878
    Change-Id: I2f7ac5f24b1685b5d8efe5561231f45b1d6e118a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320330
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950014}

--- ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_coordinator.mm
+++ ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_coordinator.mm
@@ -5,15 +5,8 @@
 #import "ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_coordinator.h"
 
 #include "base/mac/foundation_util.h"
-#include "base/notreached.h"
-#include "components/signin/public/base/signin_metrics.h"
-#import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
 #include "ios/chrome/browser/main/browser.h"
-#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_view_controller.h"
-#import "ios/chrome/browser/ui/commands/application_commands.h"
-#import "ios/chrome/browser/ui/commands/command_dispatcher.h"
-#import "ios/chrome/browser/ui/commands/show_signin_command.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -28,16 +21,10 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 
 @interface EnterpriseSignoutCoordinator () <
     ConfirmationAlertActionHandler,
-    IdentityManagerObserverBridgeDelegate,
-    UIAdaptivePresentationControllerDelegate> {
-  std::unique_ptr<signin::IdentityManagerObserverBridge>
-      _identityManagerObserver;
-}
+    UIAdaptivePresentationControllerDelegate>
+
 // ViewController that contains enterprise signout information.
 @property(nonatomic, strong) EnterpriseSignoutViewController* viewController;
-
-// Dispatcher to present the sign in screen.
-@property(nonatomic, strong) id<ApplicationCommands> handler;
 
 // YES if the sign-in is in progress.
 @property(nonatomic, assign) BOOL isSigninInProgress;
@@ -45,22 +32,6 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 @end
 
 @implementation EnterpriseSignoutCoordinator
-
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser {
-  self = [super initWithBaseViewController:viewController browser:browser];
-
-  if (self) {
-    _handler = HandlerForProtocol(self.browser->GetCommandDispatcher(),
-                                  ApplicationCommands);
-    _identityManagerObserver =
-        std::make_unique<signin::IdentityManagerObserverBridge>(
-            IdentityManagerFactory::GetForBrowserState(
-                self.browser->GetBrowserState()),
-            self);
-  }
-  return self;
-}
 
 - (void)start {
   [super start];
@@ -94,7 +65,6 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
 
 - (void)stop {
   [self dismissSignOutViewController];
-  _identityManagerObserver.reset();
   [super stop];
 }
 
@@ -120,54 +90,6 @@ constexpr CGFloat kHalfSheetCornerRadius = 20;
         dismissViewControllerAnimated:YES
                            completion:nil];
     self.viewController = nil;
-  }
-}
-
-// Presents the sign-in dialog to the user.
-- (void)showSignIn {
-  DCHECK(self.handler);
-  DCHECK(!self.isSigninInProgress);
-  self.isSigninInProgress = YES;
-
-  signin_metrics::RecordSigninUserActionForAccessPoint(
-      signin_metrics::AccessPoint::ACCESS_POINT_ENTERPRISE_SIGNOUT_COORDINATOR,
-      signin_metrics::PromoAction::PROMO_ACTION_NO_SIGNIN_PROMO);
-
-  __weak __typeof(self) weakSelf = self;
-  ShowSigninCommand* command = [[ShowSigninCommand alloc]
-      initWithOperation:AUTHENTICATION_OPERATION_SIGNIN
-               identity:nil
-            accessPoint:signin_metrics::AccessPoint::
-                            ACCESS_POINT_ENTERPRISE_SIGNOUT_COORDINATOR
-            promoAction:signin_metrics::PromoAction::
-                            PROMO_ACTION_NO_SIGNIN_PROMO
-               callback:^(BOOL success) {
-                 [weakSelf didFinishSignin];
-               }];
-  [self.handler showSignin:command baseViewController:self.baseViewController];
-}
-
-// Updates isSigninInProgress when sign-in process is done.
-- (void)didFinishSignin {
-  DCHECK(self.isSigninInProgress);
-  self.isSigninInProgress = NO;
-  [self.delegate enterpriseSignoutCoordinatorDidDismiss];
-}
-
-#pragma mark - IdentityManagerObserverBridgeDelegate
-
-// Called when a user changes the syncing state.
-- (void)onPrimaryAccountChanged:
-    (const signin::PrimaryAccountChangeEvent&)event {
-  switch (event.GetEventTypeFor(signin::ConsentLevel::kSync)) {
-    case signin::PrimaryAccountChangeEvent::Type::kSet:
-      if (!self.isSigninInProgress)
-        [self.baseViewController dismissViewControllerAnimated:true
-                                                    completion:nil];
-      break;
-    case signin::PrimaryAccountChangeEvent::Type::kCleared:
-    case signin::PrimaryAccountChangeEvent::Type::kNone:
-      break;
   }
 }
 
