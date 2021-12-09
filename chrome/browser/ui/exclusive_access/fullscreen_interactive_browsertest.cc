4dd0deed82c43e0d4c6a87289778f194c9d5cb2f
sebsg <sebsg@chromium.org>
2021-12-09 17:34:08 +0000

    [Sheriff] Disable flaky NotifyFullscreenAcquired test on lacros
    Bug: 1278361
    Change-Id: Ib76994402f9bc4545425f9a47d05fa10b4b3724a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327963
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Owners-Override: Sébastien Séguin-Gagnon <sebsg@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#950132}

--- chrome/browser/ui/exclusive_access/fullscreen_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_interactive_browsertest.cc
@@ -75,7 +75,8 @@ class FullscreenInteractiveBrowserTest : public InProcessBrowserTest {
 };
 
 // https://crbug.com/1087875: Flaky on Linux and Mac.
-#if defined(OS_MAC) || defined(OS_LINUX)
+// TODO(crbug.com/1278361): Flaky on lacros.
+#if defined(OS_MAC) || defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
 #define MAYBE_NotifyFullscreenAcquired DISABLED_NotifyFullscreenAcquired
 #else
 #define MAYBE_NotifyFullscreenAcquired NotifyFullscreenAcquired
