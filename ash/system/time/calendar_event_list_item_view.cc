5b53689eaf6ec84087b343e03b31007a45e8779c
Kevin Radtke <kradtke@chromium.org>
2021-12-10 00:51:13 +0000

    Add Calendar event list item color dot
    Adds a dot to the left of the event name in a calendar event list item.
    The color value is set based on the Google Calendar API event colorId
    mapping. If there is no event color set it uses the default calendar
    color.
    
    Bug: 1238927
    Change-Id: I1bae64878eee8c48efcec7e3b1a59233fb54018b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319422
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950360}

--- ash/system/time/calendar_event_list_item_view.cc
+++ ash/system/time/calendar_event_list_item_view.cc
@@ -13,6 +13,7 @@
 #include "google_apis/calendar/calendar_api_response_types.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/events/event.h"
+#include "ui/gfx/canvas.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/image_view.h"
@@ -30,6 +31,21 @@ constexpr int kTruncatedTitleLength = 20;
 // Paddings in this view.
 constexpr int kEntryHorizontalPadding = 20;
 
+// Radius of the event color dot.
+constexpr int kColorDotRadius = 4;
+
+// Dimension of the event color dot view.
+constexpr int kColorDotViewSize = 8;
+
+// Default Calendar API color ID to use when no event color is specifified.
+constexpr char kDefaultColorId[] = "7";
+
+// Map of Calendar API color ids and their respective hex color code.
+std::map<std::string, std::string> event_hex_color_codes = {
+    {"1", "a4bdfc"}, {"2", "7ae7bf"},  {"3", "dbadff"}, {"4", "ff887c"},
+    {"5", "fbd75b"}, {"6", "ffb878"},  {"7", "46d6db"}, {"8", "e1e1e1"},
+    {"9", "5484ed"}, {"10", "51b749"}, {"11", "dc2127"}};
+
 // Sets up the event label.
 void SetUpLabel(views::Label* label) {
   label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_CENTER);
@@ -38,6 +54,40 @@ void SetUpLabel(views::Label* label) {
   label->SetSubpixelRenderingEnabled(false);
   label->SetTextContext(CONTEXT_CALENDAR_DATE);
 }
+
+// Renders an Event color dot.
+class CalendarEventListItemDot : public views::View {
+ public:
+  CalendarEventListItemDot(std::string color_id) {
+    DCHECK(color_id.empty() || event_hex_color_codes.count(color_id));
+    std::string hex_code =
+        event_hex_color_codes[color_id.empty() ? kDefaultColorId : color_id];
+    base::HexStringToInt(hex_code, &color_);
+  }
+  ~CalendarEventListItemDot() override = default;
+  CalendarEventListItemDot(const CalendarEventListItemDot& other) = delete;
+  CalendarEventListItemDot& operator=(const CalendarEventListItemDot& other) =
+      delete;
+
+  // views::View:
+  gfx::Size CalculatePreferredSize() const override {
+    return gfx::Size(kColorDotViewSize, kColorDotViewSize);
+  }
+
+  // Draws the circle for the event color dot.
+  void OnPaint(gfx::Canvas* canvas) override {
+    cc::PaintFlags color_dot;
+    color_dot.setColor(SkColorSetA(color_, SK_AlphaOPAQUE));
+    color_dot.setStyle(cc::PaintFlags::kFill_Style);
+    color_dot.setAntiAlias(true);
+    canvas->DrawCircle(GetContentsBounds().CenterPoint(), kColorDotRadius,
+                       color_dot);
+  }
+
+ private:
+  // The color value of the dot.
+  int color_;
+};
 
 }  // namespace
 
@@ -48,12 +98,6 @@ CalendarEventListItemView::CalendarEventListItemView(
       time_range_(new views::Label()) {
   SetLayoutManager(std::make_unique<views::FillLayout>());
   GetViewAccessibility().OverrideName(GetClassName());
-
-  // TODO(https://crbug.com/1238927) Implement the event color dot. Here an
-  // info icon is used as a place holder.
-  auto* color_dot = new views::ImageView();
-  color_dot->SetImage(gfx::CreateVectorIcon(
-      views::kInfoIcon, calendar_utils::GetPrimaryTextColor()));
 
   summary_->SetText(base::UTF8ToUTF16(event.summary()));
   SetUpLabel(summary_);
@@ -72,7 +116,9 @@ CalendarEventListItemView::CalendarEventListItemView(
   // Creates a `TriView` which carries the `color_dot` and `summary_` at the
   // entry start and the `time_range_` at the entry end.
   TriView* tri_view = TrayPopupUtils::CreateDefaultRowView();
-  tri_view->AddView(TriView::Container::START, color_dot);
+  tri_view->AddView(TriView::Container::START,
+                    AddChildView(std::make_unique<CalendarEventListItemDot>(
+                        event.color_id())));
   tri_view->AddView(TriView::Container::START, summary_);
   tri_view->AddView(TriView::Container::END, time_range_);
 
