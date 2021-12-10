eeb32926eaeb0e04c53f98c7dad4c69566ce62af
Nate Fischer <ntfschr@google.com>
2021-12-10 01:07:17 +0000

    Sheriff: add @RequiresRestart to flaky test
    No change to logic. This temporarily adds @RequiresRestart to
    TabPersistentStoreUnitTest.testNtpSaveBehavior, which seems to address
    the flakiness. This seems preferable to disabling, so I've reenabled the
    test (with this annotation) for all API levels.
    
    This is only intended as a temporary workaround until a proper fix can
    be found.
    
    Bug: 1278039
    Test: run_chrome_public_test_apk -A Batch=UnitTests | grep TabPersistentStoreUnitTest
    Change-Id: I5b12202b206d233ee83167013edc198d51569078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327470
    Owners-Override: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950367}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreUnitTest.java
@@ -16,7 +16,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
-import android.os.Build;
 import android.text.TextUtils;
 
 import androidx.test.filters.SmallTest;
@@ -35,8 +34,8 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.tab.Tab;
@@ -123,9 +122,9 @@ public class TabPersistentStoreUnitTest {
 
     @Test
     @SmallTest
-    @DisableIf.
-    Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "https://crbug.com/1278039")
     @Feature("TabPersistentStore")
+    // TODO(https://crbug.com/1278039): remove @RequiresRestart workaround when culprit is found
+    @RequiresRestart("https://crbug.com/1278039")
     public void testNtpSaveBehavior() {
         when(mNormalTabModel.index()).thenReturn(TabList.INVALID_TAB_INDEX);
         when(mIncognitoTabModel.index()).thenReturn(TabList.INVALID_TAB_INDEX);
