ddc6b7a5c9165de6b9e06d8a23c4e38e997e747d
Benoit Lize <lizeb@chromium.org>
2021-12-10 09:19:43 +0000

    [base/allocator] Don't export operator new/delete on macOS.
    On macOS, the default operators for new and delete are exported by
    default. This is unneeded and undesirable, and can be removed by setting
    a compiler flag, which we were already setting when compiling libc++.
    
    This also allows us to remove the NO_CODE_FOLDING() hack, which was
    required with Identical Code Folding.
    
    This can be checked with:
    $ nm -gU out/Default/Chromium.app/Contents/Frameworks/Chromium\ Framework.framework/Versions/Current/Chromium\ Framework | c++filt
    
    which shows that the symbols are no longer exported.
    
    Bug: 1121427
    Change-Id: Ifcf65c227490208497cd07d549241b00645796fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327026
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950474}

--- base/allocator/allocator_shim_override_cpp_symbols.h
+++ base/allocator/allocator_shim_override_cpp_symbols.h
@@ -15,7 +15,6 @@
 
 #include "base/allocator/allocator_shim_internals.h"
 #include "base/compiler_specific.h"
-#include "base/debug/alias.h"
 #include "build/build_config.h"
 
 // std::align_val_t isn't available until C++17, but we want to override aligned
@@ -76,23 +75,10 @@ SHIM_CPP_SYMBOLS_EXPORT void operator delete(void* p) __THROW {
 }
 
 SHIM_CPP_SYMBOLS_EXPORT void* operator new[](size_t size) {
-#if defined(OS_APPLE)
-  // On macOS builds with Identical Code Folding (ICF), this gets merged with
-  // operator new() above, as the functions are identical. This then bumps into
-  // the framework.order check. Make sure this function is unique.
-  //
-  // TODO(lizeb): This function should not be exported at all, investigate why
-  // this is the case, and remove this hack.
-  NO_CODE_FOLDING();
-#endif  // defined(OS_APPLE)
   return ShimCppNew(size);
 }
 
 SHIM_CPP_SYMBOLS_EXPORT void operator delete[](void* p) __THROW {
-#if defined(OS_APPLE)
-  // See comment above in operator new[]().
-  NO_CODE_FOLDING();
-#endif  // defined(OS_APPLE)
   ShimCppDelete(p);
 }
 
