81d2ccc4019f7520c75c10f2ea3374c73614a8f8
John Bates <jbates@chromium.org>
2021-12-10 21:03:33 +0000

    gpu_sandbox_hook_linux: Add AMD GPU sandbox rules for libglvnd
    This is required for AMD devices to run with gpu_sandbox_start_early
    and libglvnd.
    
    Bug: b:200187231,b:202979691
    Change-Id: I966a978d8ef98cce6d21979370247f67ce4229a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328901
    Auto-Submit: John Bates <jbates@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950658}

--- content/gpu/gpu_sandbox_hook_linux.cc
+++ content/gpu/gpu_sandbox_hook_linux.cc
@@ -212,9 +212,21 @@ void AddDrmGpuPermissions(std::vector<BrokerFilePermission>* permissions) {
 }
 
 void AddAmdGpuPermissions(std::vector<BrokerFilePermission>* permissions) {
-  static const char* const kReadOnlyList[] = {"/etc/ld.so.cache",
-                                              "/usr/lib64/libEGL.so.1",
-                                              "/usr/lib64/libGLESv2.so.2"};
+  static const char* const kReadOnlyList[] = {
+      "/etc/ld.so.cache",
+      // To support threads in mesa we use --gpu-sandbox-start-early and
+      // that requires the following libs and files to be accessible.
+      "/usr/lib64/libEGL.so.1",
+      "/usr/lib64/libGLESv2.so.2",
+      "/usr/lib64/libglapi.so.0",
+      "/usr/lib64/dri/r300_dri.so",
+      "/usr/lib64/dri/r600_dri.so",
+      "/usr/lib64/dri/radeonsi_dri.so",
+      // Allow libglvnd files and libs.
+      "/usr/share/glvnd/egl_vendor.d",
+      "/usr/share/glvnd/egl_vendor.d/50_mesa.json",
+      "/usr/lib64/libEGL_mesa.so.0",
+      "/usr/lib64/libGLdispatch.so.0"};
   for (const char* item : kReadOnlyList)
     permissions->push_back(BrokerFilePermission::ReadOnly(item));
 
