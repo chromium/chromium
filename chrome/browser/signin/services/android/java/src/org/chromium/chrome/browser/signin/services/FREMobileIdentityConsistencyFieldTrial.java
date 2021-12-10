13c029645c2dff17e2a12f0d300da872d300f016
Tanmoy Mollik <triploblastic@chromium.org>
2021-12-10 14:48:32 +0000

    [FRE] Enable FREMobileIdentityConsistency experiment for 1% stable
    This experiment is launched for 10% experiment beta in M97. This cl
    enables the experiment for 1% stable for M98 to collect data as we hope
    to launch this experiment to M97 stable as well after getting the
    required approvals in the bug.
    
    Bug: 1195373
    Change-Id: Ia66b26df998151b757cf79b28047691d906d7f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324541
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950534}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/FREMobileIdentityConsistencyFieldTrial.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/FREMobileIdentityConsistencyFieldTrial.java
@@ -108,6 +108,10 @@ public class FREMobileIdentityConsistencyFieldTrial {
                 oldFreWithUmaDialogPercent = 10;
                 break;
             case Channel.STABLE:
+                enabledPercent = 1;
+                disabledPercent = 1;
+                oldFreWithUmaDialogPercent = 1;
+                break;
         }
         assert enabledPercent + disabledPercent + oldFreWithUmaDialogPercent <= 100;
 
