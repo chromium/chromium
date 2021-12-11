f56e9256b7f53f66b45fe8d4504f21852a82a1a1
Xinghui Lu <xinghuilu@chromium.org>
2021-12-11 03:56:24 +0000

    Enable SafeBrowsingOmitNonUserGesturesFromReferrerChain by default
    Bug: 1191796
    Change-Id: I04ef99c8308689635971e7d5b0bda3130acb321e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330218
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950712}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -90,7 +90,7 @@ const base::Feature kTailoredSecurityIntegration{
 
 const base::Feature kOmitNonUserGesturesFromReferrerChain{
     "SafeBrowsingOmitNonUserGesturesFromReferrerChain",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kPasswordProtectionForSignedInUsers {
   "SafeBrowsingPasswordProtectionForSignedInUsers",
