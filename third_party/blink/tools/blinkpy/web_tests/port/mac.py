0da54d900a95835c418ec1762c2a22e983efd1e0
Nico Weber <thakis@chromium.org>
2021-12-11 23:48:37 +0000

    mac: Use system httpd again
    Our hermetic httpd doesn't have an arm binary, and getting one is
    blocked on a bunch of things. Move back to system httpd for now.
    See bug for details.
    
    Morally reverts
    https://chromium-review.googlesource.com/c/chromium/src/+/1446836
    
    This adds third_party/blink/tools/apache_config/apache2-httpd-2.4-php7-prefork.conf
    which is the same as third_party/blink/tools/apache_config/apache2-httpd-2.4-php7.conf
    except with a mpm_prefork_module line added. This is needed
    on 10.15 and 11.
    
    (cherry picked from commit 1156f94e35e73182126f17cf52c45f70a8d4c45b)
    
    Bug: 1190885
    Change-Id: Icd7e14b3d94a596edbda76b44c51c49302617a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315499
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#948549}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330956
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4692@{#908}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- third_party/blink/tools/blinkpy/web_tests/port/mac.py
+++ third_party/blink/tools/blinkpy/web_tests/port/mac.py
@@ -108,14 +108,15 @@ class MacPort(base.Port):
     #
 
     def path_to_apache(self):
-        return self._path_from_chromium_base('third_party', 'apache-mac',
-                                             'bin', 'httpd')
+        return '/usr/sbin/httpd'
 
     def path_to_apache_config_file(self):
-        config_file_basename = 'apache2-httpd-%s-php7.conf' % (
-            self._apache_version(), )
-        return self._filesystem.join(self.apache_config_directory(),
-                                     config_file_basename)
+        config_file_basename = 'apache2-httpd-' + self._apache_version()
+        if self.host.platform.os_version not in ['mac10.12']:
+            config_file_basename += '-php7'
+            if self.host.platform.os_version not in ['mac10.13', 'mac10.14']:
+                config_file_basename += '-prefork'
+        return self._filesystem.join(self.apache_config_directory(), config_file_basename + '.conf')
 
     def _path_to_driver(self, target=None):
         return self._build_path_with_target(target,
