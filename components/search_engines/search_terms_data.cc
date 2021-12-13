b6f385ec3db1423580cabd0f5a2e4281c341cd44
Juan Mojica <juanmojica@google.com>
2021-12-13 18:30:34 +0000

    Launch Lens image search on desktop at ToT.
    Also disables the image search URL change if on a non-desktop platform.
    
    Bug: 1168677, 1222313
    Change-Id: I855e3838e344ba260b8829a2f8cc355f2c31e2da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328902
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#951102}

--- components/search_engines/search_terms_data.cc
+++ components/search_engines/search_terms_data.cc
@@ -5,6 +5,7 @@
 #include "components/search_engines/search_terms_data.h"
 
 #include "base/check.h"
+#include "build/build_config.h"
 #include "components/google/core/common/google_util.h"
 #include "components/lens/lens_features.h"
 #include "url/gurl.h"
@@ -20,6 +21,7 @@ std::string SearchTermsData::GoogleBaseURLValue() const {
 std::string SearchTermsData::GoogleBaseSearchByImageURLValue() const {
   const std::string kGoogleHomepageURLPath = std::string("searchbyimage/");
 
+#if !defined(OS_IOS) && !defined(OS_ANDROID)
   // If both LensStandalone and LensRegionSearch features are enabled,
   // LensStandalone parameters will take precedence even if the values differ.
   if (base::FeatureList::IsEnabled(lens::features::kLensStandalone)) {
@@ -27,6 +29,8 @@ std::string SearchTermsData::GoogleBaseSearchByImageURLValue() const {
   } else if (base::FeatureList::IsEnabled(lens::features::kLensRegionSearch)) {
     return lens::features::GetHomepageURLForRegionSearch();
   }
+#endif  // !defined(OS_IOS) && !defined(OS_ANDROID)
+
   return google_util::kGoogleHomepageURL + kGoogleHomepageURLPath;
 }
 
