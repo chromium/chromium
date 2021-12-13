e2628c1ba9645d7e3323e6cabb02b357a6b66691
W. James MacLean <wjmaclean@chromium.org>
2021-12-13 20:19:34 +0000

    Call NotifyObserversAboutShutdown() in RWHVA::Destroy().
    In the associated bug, we are hitting a crash due to access a virtual
    function after the derived class' destructor has been called, meaning
    virtual function table is in an invalid state. This CL fixes that by
    moving the code that initiates this call to a point just before the
    derived class' destructor.
    
    (cherry picked from commit 3fcc9061b26bc4ba1498f134ccdeaacaff8b3de5)
    
    Bug: 1155297
    Change-Id: I7b5ecbed8925474593428f8d76605ba6164aea90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318816
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#949157}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330579
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#942}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1158,6 +1158,9 @@ void RenderWidgetHostViewAndroid::Destroy() {
   for (auto& observer : destruction_observers_)
     observer.RenderWidgetHostViewDestroyed(this);
   destruction_observers_.Clear();
+  // Call this before the derived class is destroyed so that virtual function
+  // calls back into `this` still work.
+  NotifyObserversAboutShutdown();
   RenderWidgetHostViewBase::Destroy();
   delete this;
 }
