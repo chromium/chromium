1142cbff6e3d6867f658cfd93ca78e230b721fac
Sky Malice <skym@chromium.org>
2021-12-13 16:35:57 +0000

    Update material design to 1.6.0-alpha01.
    Bug: 1279417
    Change-Id: Ia5e624bf2c8dd2f39b4f2a67468e4ba2f15bc586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3334941
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951049}

--- third_party/android_deps/libs/com_google_android_material_material/3pp/fetch.py
+++ third_party/android_deps/libs/com_google_android_material_material/3pp/fetch.py
@@ -12,9 +12,9 @@ import argparse
 import json
 import os
 
-_FILE_URL = 'https://dl.google.com/dl/android/maven2/com/google/android/material/material/1.5.0-alpha05/material-1.5.0-alpha05.aar'
-_FILE_NAME = 'material-1.5.0-alpha05.aar'
-_FILE_VERSION = '1.5.0-alpha05'
+_FILE_URL = 'https://dl.google.com/dl/android/maven2/com/google/android/material/material/1.6.0-alpha01/material-1.6.0-alpha01.aar'
+_FILE_NAME = 'material-1.6.0-alpha01.aar'
+_FILE_VERSION = '1.6.0-alpha01'
 
 
 def do_latest():
