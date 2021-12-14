d9b61168fc67834e631462eb3689be1588a46d6b
Mihai Sardarescu <msarda@chromium.org>
2021-12-14 19:54:23 +0000

    Revert "Adding flag to enable/disable 'X' to dismiss the signin promo so it can be"
    This reverts commit 6a5b32cf1827af60caa52675df57e310789c2c0c.
    
    Reason for revert:
    This experiment is no longer needed (we already collected all necessary
    metrics).
    
    Original change's description:
    > Adding flag to enable/disable 'X' to dismiss the signin promo so it can be
    > rolled out via Finch.
    >
    > Note: the flag is default enabled.
    >
    > Bug: 1248630
    > Change-Id: I311ae52f03fa55f8e9f7d0dab61834e173155f8a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190922
    > Commit-Queue: Jonathan Freed <freedjm@chromium.org>
    > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926454}
    
    Bug: 1248630
    Change-Id: I623b2befc8a662bc44b642782d72c8e286802b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329617
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jonathan Freed <freedjm@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951609}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -139,7 +139,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &feed::kFeedImageMemoryCacheSizePercentage,
     &feed::kFeedInteractiveRefresh,
     &feed::kFeedLoadingPlaceholder,
-    &feed::kFeedSignInPromoDismiss,
     &feed::kInterestFeedContentSuggestions,
     &feed::kInterestFeedSpinnerAlwaysAnimate,
     &feed::kInterestFeedV1ClicksAndViewsConditionalUpload,
--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -336,7 +336,6 @@ public abstract class ChromeFeatureList {
     public static final String FEED_INTERACTIVE_REFRESH = "FeedInteractiveRefresh";
     public static final String FEED_LOADING_PLACEHOLDER = "FeedLoadingPlaceholder";
     public static final String FEED_RELIABILITY_LOGGING = "FeedReliabilityLogging";
-    public static final String FEED_SIGNIN_PROMO_DISMISS = "FeedSignInPromoDismiss";
     public static final String FILLING_PASSWORDS_FROM_ANY_ORIGIN = "FillingPasswordsFromAnyOrigin";
     public static final String FIXED_UMA_SESSION_RESUME_ORDER = "FixedUmaSessionResumeOrder";
     public static final String FOCUS_OMNIBOX_IN_INCOGNITO_TAB_INTENTS =
