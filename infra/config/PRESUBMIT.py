ac48e8c95e3b5af4579e0de2664c8cc286e368a2
Garrett Beaty <gbeaty@chromium.org>
2021-12-14 01:28:48 +0000

    Add a presubmit to prevent prod changes during freeze.
    Bug: 1279609
    Change-Id: I9e1bb0df2e36dcd6eebd633ae9d8b7b76f4ece8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336096
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@google.com>
    Cr-Commit-Position: refs/heads/main@{#951302}

--- infra/config/PRESUBMIT.py
+++ infra/config/PRESUBMIT.py
@@ -11,6 +11,44 @@ for more details on the presubmit API built into depot_tools.
 PRESUBMIT_VERSION = '2.0.0'
 USE_PYTHON3 = True
 
+_IGNORE_FREEZE_FOOTER = 'Ignore-Freeze'
+
+# The time module's handling of timezones is abysmal, so the boundaries are
+# precomputed in UNIX time
+_FREEZE_START = 1639641600  # 2021/12/16 00:00 -0800
+_FREEZE_END = 1641196800  # 2022/01/03 00:00 -0800
+
+
+def CheckFreeze(input_api, output_api):
+  if _FREEZE_START <= input_api.time.time() < _FREEZE_END:
+    footers = input_api.change.GitFootersFromDescription()
+    if _IGNORE_FREEZE_FOOTER not in footers:
+
+      def convert(t):
+        ts = input_api.time.localtime(t)
+        return input_api.time.strftime('%Y/%m/%d %H:%M %z', ts)
+
+      return [
+          output_api.PresubmitError(
+              'There is a prod freeze in effect from {} until {},'
+              ' files in //infra/config cannot be modified'.format(
+                  convert(_FREEZE_START), convert(_FREEZE_END)))
+      ]
+
+  return []
+
+
+def CheckTests(input_api, output_api):
+  glob = input_api.os_path.join(input_api.PresubmitLocalPath(), '*_test.py')
+  tests = input_api.canned_checks.GetUnitTests(input_api,
+                                               output_api,
+                                               input_api.glob(glob),
+                                               run_on_python2=False,
+                                               run_on_python3=True,
+                                               skip_shebang_check=True)
+  return input_api.RunTests(tests)
+
+
 def CheckLintLuciMilo(input_api, output_api):
   if ('infra/config/generated/luci/luci-milo.cfg' in input_api.LocalPaths()
       or 'infra/config/lint-luci-milo.py' in input_api.LocalPaths()):
--- testing/buildbot/PRESUBMIT.py
+++ testing/buildbot/PRESUBMIT.py
@@ -11,6 +11,32 @@ for more details on the presubmit API built into depot_tools.
 PRESUBMIT_VERSION = '2.0.0'
 USE_PYTHON3 = True
 
+_IGNORE_FREEZE_FOOTER = 'Ignore-Freeze'
+
+# The time module's handling of timezones is abysmal, so the boundaries are
+# precomputed in UNIX time
+_FREEZE_START = 1639641600  # 2021/12/16 00:00 -0800
+_FREEZE_END = 1641196800  # 2022/01/03 00:00 -0800
+
+
+def CheckFreeze(input_api, output_api):
+  if _FREEZE_START <= input_api.time.time() < _FREEZE_END:
+    footers = input_api.change.GitFootersFromDescription()
+    if _IGNORE_FREEZE_FOOTER not in footers:
+
+      def convert(t):
+        ts = input_api.time.localtime(t)
+        return input_api.time.strftime('%Y/%m/%d %H:%M %z', ts)
+
+      return [
+          output_api.PresubmitError(
+              'There is a prod freeze in effect from {} until {},'
+              ' files in //testing/buildbot cannot be modified'.format(
+                  convert(_FREEZE_START), convert(_FREEZE_END)))
+      ]
+
+  return []
+
 
 def CheckSourceSideSpecs(input_api, output_api):
   return input_api.RunTests([
