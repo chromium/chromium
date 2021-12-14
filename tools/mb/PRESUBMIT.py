961bfc307fa8029ec7a295e672c613c9557d75ea
Garrett Beaty <gbeaty@chromium.org>
2021-12-14 23:46:25 +0000

    [M96] Add a presubmit to prevent prod changes during freeze.
    (cherry picked from commit ac48e8c95e3b5af4579e0de2664c8cc286e368a2)
    
    Bug: 1279609
    Change-Id: I9e1bb0df2e36dcd6eebd633ae9d8b7b76f4ece8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336096
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#951302}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3339352
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Cr-Commit-Position: refs/branch-heads/4664@{#1308}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- tools/mb/PRESUBMIT.py
+++ tools/mb/PRESUBMIT.py
@@ -5,11 +5,55 @@
 USE_PYTHON3 = True
 
 
+_IGNORE_FREEZE_FOOTER = 'Ignore-Freeze'
+
+# The time module's handling of timezones is abysmal, so the boundaries are
+# precomputed in UNIX time
+_FREEZE_START = 1639641600  # 2021/12/16 00:00 -0800
+_FREEZE_END = 1641196800  # 2022/01/03 00:00 -0800
+
+
+def CheckFreeze(input_api, output_api):
+  if _FREEZE_START <= input_api.time.time() < _FREEZE_END:
+    footers = input_api.change.GitFootersFromDescription()
+    if _IGNORE_FREEZE_FOOTER not in footers:
+
+      def convert(t):
+        ts = input_api.time.localtime(t)
+        return input_api.time.strftime('%Y/%m/%d %H:%M %z', ts)
+
+      return [
+          output_api.PresubmitError(
+              'There is a prod freeze in effect from {} until {},'
+              ' files in //tools/mb cannot be modified'.format(
+                  convert(_FREEZE_START), convert(_FREEZE_END)))
+      ]
+
+  return []
+
+
+def CheckTests(input_api, output_api):
+  glob = input_api.os_path.join(input_api.PresubmitLocalPath(), '*_test.py')
+  tests = input_api.canned_checks.GetUnitTests(input_api,
+                                               output_api,
+                                               input_api.glob(glob),
+                                               run_on_python2=False,
+                                               run_on_python3=True,
+                                               skip_shebang_check=True)
+  return input_api.RunTests(tests)
+
+
 def _CommonChecks(input_api, output_api):
   results = []
 
   # Run Pylint over the files in the directory.
-  pylint_checks = input_api.canned_checks.GetPylint(input_api, output_api)
+  pylint_checks = input_api.canned_checks.GetPylint(
+      input_api,
+      output_api,
+      # pylint complains about Checkfreeze not being defined, its probably
+      # finding a different PRESUBMIT.py
+      files_to_skip=['PRESUBMIT_test.py'],
+  )
   results.extend(input_api.RunTests(pylint_checks))
 
   # Run the MB unittests.
@@ -23,6 +67,9 @@ def _CommonChecks(input_api, output_api):
       input_api.Command(name='mb_validate',
                         cmd=cmd, kwargs=kwargs,
                         message=output_api.PresubmitError)]))
+
+  results.extend(CheckFreeze(input_api, output_api))
+  results.extend(CheckTests(input_api, output_api))
 
   return results
 
