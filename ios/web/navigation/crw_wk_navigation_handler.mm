dca4ed1e9edee406c51bd35cdf24eca370bafbba
Joemer Ramos <joemerramos@chromium.org>
2021-12-14 00:56:08 +0000

    [iOS] Download API iOS 15+ changes for CrwWkNavigationHandler
    Added functionality so that WkWebView objects can properly use
    WKDownload and DownloadNativeTaskBridge if on iOS 15+.
    
    Bug: 1252380
    Change-Id: I9824097a0eb2919009636163417aad3b0d3583ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327863
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951295}

--- ios/web/navigation/crw_wk_navigation_handler.mm
+++ ios/web/navigation/crw_wk_navigation_handler.mm
@@ -15,6 +15,7 @@
 #import "ios/net/url_scheme_util.h"
 #include "ios/web/common/features.h"
 #import "ios/web/common/url_scheme_util.h"
+#import "ios/web/download/download_native_task_bridge.h"
 #import "ios/web/js_messaging/web_frames_manager_java_script_feature.h"
 #import "ios/web/navigation/crw_error_page_helper.h"
 #import "ios/web/navigation/crw_navigation_item_holder.h"
@@ -63,7 +64,7 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
 
 }  // namespace
 
-@interface CRWWKNavigationHandler () {
+@interface CRWWKNavigationHandler () <DownloadNativeTaskBridgeReadyDelegate> {
   // Referrer for the current page; does not include the fragment.
   NSString* _currentReferrerString;
 
@@ -73,6 +74,14 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
   // cert status from |didReceiveAuthenticationChallenge:| to
   // |didFailProvisionalNavigation:| delegate method.
   std::unique_ptr<web::CertVerificationErrorsCacheType> _certVerificationErrors;
+
+  // Used to keep track of newly created and current download
+  // task objects created from
+  // |webView:navigationAction:didBecomeDownload| and
+  // |webView:navigationResponse:didBecomeDownload|. Respectively,
+  // DownloadNativeTaskBridge objects will help provide a valid
+  // |navigationAction| or |navigationResponse|.
+  NSMutableSet<DownloadNativeTaskBridge*>* _nativeTaskBridges;
 }
 
 @property(nonatomic, weak) id<CRWWKNavigationHandlerDelegate> delegate;
@@ -88,7 +97,7 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
 // Returns the CRWCertVerificationController from self.delegate.
 @property(nonatomic, readonly, weak)
     CRWCertVerificationController* certVerificationController;
-// Returns the docuemnt URL from self.delegate.
+// Returns the document URL from self.delegate.
 @property(nonatomic, readonly, assign) GURL documentURL;
 
 @end
@@ -106,9 +115,18 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
         std::make_unique<web::CertVerificationErrorsCacheType>(
             kMaxCertErrorsCount);
 
+    _nativeTaskBridges = [NSMutableSet new];
+
     _delegate = delegate;
   }
   return self;
+}
+
+- (void)dealloc {
+  for (DownloadNativeTaskBridge* bridge in _nativeTaskBridges) {
+    [bridge cancel];
+  }
+  _nativeTaskBridges = nil;
 }
 
 #pragma mark - WKNavigationDelegate
@@ -315,12 +333,19 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
     }
   }
 
-  BOOL webControllerCanShow =
+  const BOOL webControllerCanShow =
       web::UrlHasWebScheme(requestURL) ||
       web::GetWebClient()->IsAppSpecificURL(requestURL) ||
       requestURL.SchemeIs(url::kFileScheme) ||
       requestURL.SchemeIs(url::kAboutScheme) ||
       requestURL.SchemeIs(url::kBlobScheme);
+
+  BOOL shouldPerformDownload = NO;
+  if (web::features::IsNewDownloadAPIEnabled()) {
+    if (@available(iOS 15, *)) {
+      shouldPerformDownload = action.shouldPerformDownload;
+    }
+  }
 
   __weak CRWWKNavigationHandler* weakSelf = self;
   auto callback = base::BindOnce(
@@ -340,7 +365,8 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
                       forNavigationAction:action
                        withPolicyDecision:policyDecision
                                   webView:webView
-                 forceBlockUniversalLinks:forceBlockUniversalLinks];
+                 forceBlockUniversalLinks:forceBlockUniversalLinks
+                    shouldPerformDownload:shouldPerformDownload];
       });
 
   if (!policyDecision.ShouldAllowNavigation()) {
@@ -428,6 +454,13 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
     self.webStateImpl->ShouldAllowResponse(WKResponse.response, response_info,
                                            std::move(callback));
     return;
+  }
+
+  if (web::features::IsNewDownloadAPIEnabled()) {
+    if (@available(iOS 15, *)) {
+      handler(WKNavigationResponsePolicyDownload);
+      return;
+    }
   }
 
   if (web::UrlHasWebScheme(responseURL)) {
@@ -1007,6 +1040,57 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
   [self.delegate navigationHandlerWebProcessDidCrash:self];
 }
 
+- (void)webView:(WKWebView*)webView
+     navigationAction:(WKNavigationAction*)navigationAction
+    didBecomeDownload:(WKDownload*)WKDownload API_AVAILABLE(ios(15)) {
+  // Discard the pending item to ensure that the current URL is not different
+  // from what is displayed on the view.
+  self.navigationManagerImpl->DiscardNonCommittedItems();
+
+  [_nativeTaskBridges
+      addObject:[[DownloadNativeTaskBridge alloc] initWithDownload:WKDownload
+                                             downloadReadyDelegate:self]];
+}
+
+- (void)webView:(WKWebView*)webView
+    navigationResponse:(WKNavigationResponse*)navigationResponse
+     didBecomeDownload:(WKDownload*)WKDownload API_AVAILABLE(ios(15)) {
+  // Discard the pending item to ensure that the current URL is not different
+  // from what is displayed on the view.
+  self.navigationManagerImpl->DiscardNonCommittedItems();
+
+  [_nativeTaskBridges
+      addObject:[[DownloadNativeTaskBridge alloc] initWithDownload:WKDownload
+                                             downloadReadyDelegate:self]];
+}
+
+- (void)onDownloadNativeTaskBridgeReadyForDownload:
+    (DownloadNativeTaskBridge*)bridge API_AVAILABLE(ios(15)) {
+  const GURL responseURL = net::GURLWithNSURL(bridge.response.URL);
+  const int64_t contentLength = bridge.response.expectedContentLength;
+  const std::string MIMEType =
+      base::SysNSStringToUTF8(bridge.response.MIMEType);
+
+  std::string contentDisposition;
+  scoped_refptr<net::HttpResponseHeaders> headers;
+  if ([bridge.response isKindOfClass:[NSHTTPURLResponse class]]) {
+    headers = net::CreateHeadersFromNSHTTPURLResponse(
+        static_cast<NSHTTPURLResponse*>(bridge.response));
+    if (headers) {
+      headers->GetNormalizedHeader("content-disposition", &contentDisposition);
+    }
+  }
+
+  NSString* HTTPMethod = bridge.download.originalRequest.HTTPMethod;
+  web::DownloadController::FromBrowserState(
+      self.webStateImpl->GetBrowserState())
+      ->CreateNativeDownloadTask(self.webStateImpl, [NSUUID UUID].UUIDString,
+                                 responseURL, HTTPMethod, contentDisposition,
+                                 contentLength, MIMEType, bridge);
+
+  [_nativeTaskBridges removeObject:bridge];
+}
+
 #pragma mark - Private methods
 
 // Returns the UserAgent that needs to be used for the |navigationAction| from
@@ -1365,7 +1449,8 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
            withPolicyDecision:
                (web::WebStatePolicyDecider::PolicyDecision)policyDecision
                       webView:(WKWebView*)webView
-     forceBlockUniversalLinks:(BOOL)forceBlockUniversalLinks {
+     forceBlockUniversalLinks:(BOOL)forceBlockUniversalLinks
+        shouldPerformDownload:(BOOL)shouldPerformDownload {
   if (policyDecision.ShouldAllowNavigation()) {
     if ([[action.request HTTPMethod] isEqualToString:@"POST"]) {
       // Display the confirmation dialog if a form repost is detected.
@@ -1443,6 +1528,15 @@ const web::CertVerificationErrorsCacheType::size_type kMaxCertErrorsCount = 100;
     decisionHandler(WKNavigationActionPolicyCancel);
     return;
   }
+
+  if (shouldPerformDownload) {
+    DCHECK(web::features::IsNewDownloadAPIEnabled());
+    if (@available(iOS 15, *)) {
+      decisionHandler(WKNavigationActionPolicyDownload);
+      return;
+    }
+  }
+
   BOOL isOffTheRecord = self.webStateImpl->GetBrowserState()->IsOffTheRecord();
   decisionHandler(web::GetAllowNavigationActionPolicy(
       isOffTheRecord || forceBlockUniversalLinks));
