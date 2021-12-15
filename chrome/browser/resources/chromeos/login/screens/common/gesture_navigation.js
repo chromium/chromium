2eb939eeb5b3b67bbcd20e2af5312832cd8d95b9
Roman Aleksandrov <raleksandrov@google.com>
2021-12-15 16:08:39 +0000

    oobe: Make gesture animation stoppable
    Fixed: 1229893
    Change-Id: I2896c60f5531e538553fa9307440c7d082043bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3338422
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#951958}

--- chrome/browser/resources/chromeos/login/screens/common/gesture_navigation.js
+++ chrome/browser/resources/chromeos/login/screens/common/gesture_navigation.js
@@ -121,7 +121,7 @@ class GestureNavigation extends GestureScreenElementBase {
   setPlayCurrentScreenAnimation(enabled) {
     var animation = this.$[this.uiStep].querySelector('.gesture-animation');
     if (animation) {
-      animation.setPlay(enabled);
+      animation.playing = enabled;
     }
   }
 }
