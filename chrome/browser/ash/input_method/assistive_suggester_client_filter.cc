af903f47455b452389aa64543ea9f21099830cf7
Curtis McMullan <curtismcmullan@chromium.org>
2021-12-15 23:18:02 +0000

    multiword: remove skype from allowlist
    Currently there are some compatibility issues with the ime shared
    library and the skype web client.
    
    While fixing those issues we should disable the multiword feature
    in the skype web client to avoid any issues.
    
    Bug: b:3339115
    Change-Id: I501ecdaa3b16887461f41ed01b229ddac684ba9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340447
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952139}

--- chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
+++ chrome/browser/ash/input_method/assistive_suggester_client_filter.cc
@@ -36,11 +36,12 @@ const char* kAllowedDomainsForEmojiSuggester[] = {
     "voice.google.com",
 };
 
+// TODO(b/3339115): Add web.skype.com back to the list after compatibility
+//    issues are solved.
 const char* kAllowedDomainsForMultiWordSuggester[] = {
-    "discord.com",      "messenger.com",       "web.whatsapp.com",
-    "web.skype.com",    "duo.google.com",      "hangouts.google.com",
-    "chat.google.com",  "messages.google.com", "web.telegram.org",
-    "voice.google.com",
+    "discord.com",         "messenger.com",       "web.whatsapp.com",
+    "duo.google.com",      "hangouts.google.com", "chat.google.com",
+    "messages.google.com", "web.telegram.org",    "voice.google.com",
 };
 
 const char* kTestUrls[] = {
