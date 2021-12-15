2d70046f51abe4a5b75e7859527882d4ecef7e68
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-15 11:01:51 +0000

    CCA: Migrate snackbar.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I3a16cb498892b5e36ce35c6cc954af54c6a77ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299753
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951886}

--- ash/webui/camera_app_ui/resources/js/snackbar.ts
+++ ash/webui/camera_app_ui/resources/js/snackbar.ts
@@ -4,16 +4,15 @@
 
 import * as animate from './animation.js';
 import * as dom from './dom.js';
-// eslint-disable-next-line no-unused-vars
 import {I18nString} from './i18n_string.js';
 import * as loadTimeData from './models/load_time_data.js';
 
 /**
  * Shows a snackbar message.
- * @param {!I18nString} label The label of the message to show.
- * @param {...string} substitutions The substitutions for the label.
+ * @param label The label of the message to show.
+ * @param substitutions The substitutions for the label.
  */
-export function show(label, ...substitutions) {
+export function show(label: I18nString, ...substitutions: string[]): void {
   const message = loadTimeData.getI18nMessage(label, ...substitutions);
   const el = dom.get('.snackbar', HTMLElement);
   el.textContent = '';  // Force reiterate the same message for a11y.
