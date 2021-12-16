ebc893a88186ff0a527273ffe0f866be369085c5
Jérôme Lebel <jlebel@chromium.org>
2021-12-16 10:46:43 +0000

    [iOS] Disable NTPHomeTestCase/testNewSearchFromNewTabMenu on device
    Bug: 1280323
    Change-Id: I3ba63ac7a82848fa7b42f132caca7767465e0f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341538
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952299}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -843,7 +843,13 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 #pragma mark - New Tab menu tests
 
 // Tests the "new search" menu item from the new tab menu.
-- (void)testNewSearchFromNewTabMenu {
+// TODO(crbug.com/1280323): Fails on iOS device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testNewSearchFromNewTabMenu testNewSearchFromNewTabMenu
+#else
+#define MAYBE_testNewSearchFromNewTabMenu DISABLED_testNewSearchFromNewTabMenu
+#endif
+- (void)MAYBE_testNewSearchFromNewTabMenu {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"New Search is only available in phone layout.");
   }
