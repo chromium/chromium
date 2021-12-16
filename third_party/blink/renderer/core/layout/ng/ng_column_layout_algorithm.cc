57711f313686b843b941b1ca95ea174c52d25069
Morten Stenshorne <mstensho@chromium.org>
2021-12-16 20:48:59 +0000

    Don't break before a nested multicol twice (or indefinitely).
    If we have already broken before it, don't do it again, as we'd get
    stuck forever.
    
    Bug: 1280624
    Change-Id: I39d04e9ee7d9a6237b516cb6610552dc1044e2d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344659
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952500}

--- third_party/blink/renderer/core/layout/ng/ng_column_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_column_layout_algorithm.cc
@@ -646,10 +646,35 @@ scoped_refptr<const NGLayoutResult> NGColumnLayoutAlgorithm::LayoutRow(
   // block-start border/padding), or in the outer fragmentation context), it may
   // be better to push some of the content to the next outer fragmentainer and
   // retry there.
-  bool may_have_more_space_in_next_outer_fragmentainer =
-      may_resume_in_next_outer_fragmentainer &&
-      (intrinsic_block_size_ ||
-       ConstraintSpace().FragmentainerOffsetAtBfc() > LayoutUnit());
+  bool may_have_more_space_in_next_outer_fragmentainer = false;
+  if (may_resume_in_next_outer_fragmentainer) {
+    if (intrinsic_block_size_) {
+      may_have_more_space_in_next_outer_fragmentainer = true;
+    } else {
+      if (ConstraintSpace().FragmentainerOffsetAtBfc() > LayoutUnit()) {
+        if (!BreakToken()) {
+          may_have_more_space_in_next_outer_fragmentainer = true;
+        } else {
+          // We have already added a break for this multicol container, but we
+          // think that we're not at the start of the fragmentainer. This may be
+          // the case if we have a nested floated multicol container with a
+          // non-zero block-start margin. Float margins are unbreakable and are
+          // never truncated. So we may have broken before it, and kept the
+          // margin for the next fragmentainer (as we should), but since a
+          // fragmentainer offset is a border-box offset, we'll now think that
+          // we're past the fragmentainer start (the margin edge is at the
+          // start, but the border edge isn't). This is not ideal, but for now
+          // just avoid breaking before *again*, which would cause an infinite
+          // loop with no content progress. DCHECK that this is the situation.
+          // TODO(mstensho): Find a solution to this. We may have added an
+          // unnecessary break now. Pushing the float to the next fragmentainer
+          // may not have solved anything.
+          DCHECK(Node().IsFloating());
+          DCHECK(BreakToken()->IsBreakBefore());
+        }
+      }
+    }
+  }
 
   scoped_refptr<const NGLayoutResult> result;
   absl::optional<NGBreakAppeal> min_break_appeal;
