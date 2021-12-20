9817ab616338df9cf1dbbbec6e589c4abfcacffb
Dominic Battre <battre@chromium.org>
2021-12-20 16:41:52 +0000

    Pin scrollTop to 0 during autofill preview
    This CL forces scrollTop of a ListBox to be 0 during autofill preview
    state. After autofill preview ends, it attempts to scroll the ListBox
    back so that a previously selected element becomes visible.
    
    (cherry picked from commit 55b07dc54220200313366ec821d2303cd847187a)
    
    Fixed: 1261689
    Change-Id: I8593544577cf054cca40e7a487d3248acdcfdaa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279960
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#941822}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347569
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4692@{#1097}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -123,6 +123,7 @@
 #include "third_party/blink/renderer/core/html/custom/custom_element_registry.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_controls_collection.h"
 #include "third_party/blink/renderer/core/html/forms/html_options_collection.h"
+#include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_menu_element.h"
 #include "third_party/blink/renderer/core/html/html_body_element.h"
 #include "third_party/blink/renderer/core/html/html_collection.h"
@@ -1559,6 +1560,13 @@ double Element::scrollTop() {
   if (GetDocument().ScrollingElementNoLayout() == this) {
     if (GetDocument().domWindow())
       return GetDocument().domWindow()->scrollY();
+    return 0;
+  }
+
+  // Don't disclose scroll position in preview state. See crbug.com/1261689.
+  auto* select_element = DynamicTo<HTMLSelectElement>(this);
+  if (select_element && !select_element->UsesMenuList() &&
+      !select_element->SuggestedValue().IsEmpty()) {
     return 0;
   }
 
--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -980,8 +980,16 @@ void ListBoxSelectType::DidBlur() {
 }
 
 void ListBoxSelectType::DidSetSuggestedOption(HTMLOptionElement* option) {
-  if (select_->GetLayoutObject())
-    ScrollToOption(option);
+  if (!select_->GetLayoutObject())
+    return;
+  // When ending preview state, don't leave the scroll position at the
+  // previewed element but return to the active selection end if it is
+  // defined or to the first selectable option. See crbug.com/1261689.
+  if (!option)
+    option = ActiveSelectionEnd();
+  if (!option)
+    option = FirstSelectableOption();
+  ScrollToOption(option);
 }
 
 void ListBoxSelectType::SaveLastSelection() {
