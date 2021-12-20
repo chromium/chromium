22363c47bfefe4d5acad6bf897919046a46e5d3a
Liam Brady <lbrady@google.com>
2021-12-20 20:27:49 +0000

    Write resize lock WPTs
    - Add test to ensure resizing fenced frames doesn't alter
    the innerFrame value inside a fenced frame
    - This works for ShadowDOM but not for MPArch. Add an expected
    failure for the MPArch version of the test.
    
    Bug: 1270648
    Change-Id: I82fa53b52bd7ffca2c77b739c0ca0a1e27b33b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342150
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#953006}

--- third_party/blink/web_tests/wpt_internal/fenced_frame/resources/utils.js
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/utils.js
@@ -103,6 +103,10 @@ const KEYS = {
 
   "history_navigation_performed"                : "00000000-0000-0000-0000-000000000036",
   "outer_page_ready"                            : "00000000-0000-0000-0000-000000000037",
+
+  "resize_lock_inner_page_is_ready"             : "00000000-0000-0000-0000-000000000038",
+  "resize_lock_resize_is_done"                  : "00000000-0000-0000-0000-000000000039",
+  "resize_lock_report_inner_dimensions"         : "00000000-0000-0000-0000-00000000004A",
   // Add keys above this list, incrementing the key UUID in hexadecimal
 }
 
