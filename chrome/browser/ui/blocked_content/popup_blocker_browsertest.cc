d9babe8a3ae0d7b72fa8de7bd1f00d8e4171f73e
Matthias Körber <koerber@google.com>
2021-12-20 10:26:13 +0000

    Disable PopupBlockerBrowserTest.TapGestureWithCtrlKey on Win
    Bug: 1276035
    Change-Id: I268a49bba8a1771455cc891565b688d07c2fd8ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347577
    Owners-Override: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#952872}

--- chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
@@ -743,8 +743,13 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, CtrlEnterKey) {
 }
 
 // Tests that the tapping gesture with cntl/cmd key on a link open the
-// backgournd tab.
-IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, TapGestureWithCtrlKey) {
+// background tab.
+#if defined(OS_WIN)
+#define MAYBE_TapGestureWithCtrlKey DISABLED_TapGestureWithCtrlKey
+#else
+#define MAYBE_TapGestureWithCtrlKey TapGestureWithCtrlKey
+#endif
+IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, MAYBE_TapGestureWithCtrlKey) {
   WebContents* tab = browser()->tab_strip_model()->GetActiveWebContents();
 
   GURL url(embedded_test_server()->GetURL(
