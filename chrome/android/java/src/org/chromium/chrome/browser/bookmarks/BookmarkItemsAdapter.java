269fef45c1c0923b3d8612f2ab4805f2ef16795f
Brandon Wylie <wylieb@chromium.org>
2021-12-21 19:36:05 +0000

    [Power Bookmarks] Exclude products where tracking is disabled
    (cherry picked from commit 881acd38a19663dbbe7b9162d0b06d7a60ac2614)
    
    Bug: 1279601
    Change-Id: I42e44c1c75284c3d2d8fc5a035a1f9731a37c977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336499
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#952131}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350881
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4758@{#166}
    Cr-Branched-From: 4a2cf4baf90326df19c3ee70ff987960d59a386e-refs/heads/main@{#950365}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
@@ -26,6 +26,8 @@ import org.chromium.chrome.browser.bookmarks.BookmarkListEntry.ViewType;
 import org.chromium.chrome.browser.bookmarks.BookmarkRow.Location;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.power_bookmarks.PowerBookmarkMeta;
+import org.chromium.chrome.browser.power_bookmarks.PowerBookmarkType;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.subscriptions.CommerceSubscriptionsServiceFactory;
 import org.chromium.chrome.browser.sync.SyncService;
@@ -147,6 +149,17 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
         return position;
     }
 
+    private void filterForPriceTrackingCategory(List<BookmarkId> bookmarks) {
+        for (int i = bookmarks.size() - 1; i >= 0; i--) {
+            PowerBookmarkMeta meta = mDelegate.getModel().getPowerBookmarkMeta(bookmarks.get(i));
+            if (meta == null || meta.getType() != PowerBookmarkType.SHOPPING
+                    || !meta.getShoppingSpecifics().getIsPriceTracked()) {
+                bookmarks.remove(i);
+                continue;
+            }
+        }
+    }
+
     private void setBookmarks(List<BookmarkId> bookmarks) {
         clearHighlight();
         mElements.clear();
@@ -157,6 +170,10 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
         }
 
         updateHeader(false);
+        if (BookmarkId.SHOPPING_FOLDER.equals(mCurrentFolder)) {
+            filterForPriceTrackingCategory(bookmarks);
+        }
+
         for (BookmarkId bookmarkId : bookmarks) {
             BookmarkItem item = mDelegate.getModel().getBookmarkById(bookmarkId);
 
