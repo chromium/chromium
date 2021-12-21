6547ab33c8f75362f3e7fc108334182a55969d4b
Toni Barzic <tbarzic@chromium.org>
2021-12-21 00:42:59 +0000

    Allow chromevox selection in Chrome OS launcher search
    Chrome OS launcher search results UI uses selection to verbalize the
    selected launcher search result (similar to omnibox).
    
    BUG=1276247
    
    AX-Relnotes: Fix verbalization for results in Chrome OS launcher search
    
    Change-Id: I4264e5940a0a3469ffd23df83441420eb0ba8fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3346381
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953073}

--- chrome/browser/ui/app_list/app_list_client_impl.h
+++ chrome/browser/ui/app_list/app_list_client_impl.h
@@ -151,6 +151,9 @@ class AppListClientImpl
 
   app_list::SearchController* search_controller();
 
+  void SetSearchControllerForTest(
+      std::unique_ptr<app_list::SearchController> test_controller);
+
   AppListModelUpdater* GetModelUpdaterForTest();
 
   // Initializes as if a new user logged in for testing.
