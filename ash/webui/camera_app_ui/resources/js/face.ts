0726b8c4ff3c6c17590a22d93bbbede2710a9cbb
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-22 08:11:51 +0000

    CCA: Migrate face.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I180c545608e73457f0caa523d45173693526b2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299886
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953459}

--- ash/webui/camera_app_ui/resources/js/face.ts
+++ ash/webui/camera_app_ui/resources/js/face.ts
@@ -4,8 +4,6 @@
 
 import {assertInstanceof, assertNotReached} from './assert.js';
 import * as dom from './dom.js';
-
-// eslint-disable-next-line no-unused-vars
 import {Resolution} from './type.js';
 
 // G-yellow-600 with alpha = 0.8
@@ -14,12 +12,9 @@ const RECT_COLOR = 'rgba(249, 171, 0, 0.8)';
 /**
  * Rotates the given coordinates in [0, 1] square space by the given
  * orientation.
- * @param {number} x
- * @param {number} y
- * @param {number} orientation
- * @return {!Array<number>} The rotated [x, y].
+ * @return The rotated [x, y].
  */
-function rotate(x, y, orientation) {
+function rotate(x: number, y: number, orientation: number): number[] {
   switch (orientation) {
     case 0:
       return [x, y];
@@ -37,62 +32,45 @@ function rotate(x, y, orientation) {
  * An overlay to show face rectangles over preview.
  */
 export class FaceOverlay {
+  private readonly canvas = dom.get('#preview-face-overlay', HTMLCanvasElement);
+  private readonly ctx: CanvasRenderingContext2D;
+
   /**
-   * @param {!Resolution} activeArraySize
-   * @param {number} orientation Counter-clockwise angles to apply rotation to
+   * @param orientation Counter-clockwise angles to apply rotation to
    *     the face rectangles.
    */
-  constructor(activeArraySize, orientation) {
-    /**
-     * @const {!Resolution}
-     * @private
-     */
-    this.activeArraySize_ = activeArraySize;
-
-    /**
-     * @const {number}
-     * @private
-     */
-    this.orientation_ = orientation;
-
-    /**
-     * @const {!HTMLCanvasElement}
-     * @private
-     */
-    this.canvas_ = dom.get('#preview-face-overlay', HTMLCanvasElement);
-
-    /**
-     * @const {!CanvasRenderingContext2D}
-     * @private
-     */
-    this.ctx_ = assertInstanceof(
-        this.canvas_.getContext('2d'), CanvasRenderingContext2D);
+  constructor(
+      private readonly activeArraySize: Resolution,
+      private readonly orientation: number) {
+    this.ctx = assertInstanceof(
+        this.canvas.getContext('2d'), CanvasRenderingContext2D);
   }
 
   /**
    * Shows the given rectangles on overlay. The old rectangles would be
    * cleared, if any.
-   * @param {!Array<number>} rects An array of [x1, y1, x2, y2] to represent
-   *     rectangles in the coordinate system of active array in sensor.
+   * @param rects An array of [x1, y1, x2, y2] to represent rectangles in the
+   *     coordinate system of active array in sensor.
    */
-  show(rects) {
-    this.ctx_.clearRect(0, 0, this.canvas_.width, this.canvas_.height);
+  show(rects: number[]): void {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     // TODO(b/178344897): Handle zoomed preview.
     // TODO(b/178344897): Handle screen orientation dynamically.
 
-    this.ctx_.strokeStyle = RECT_COLOR;
+    this.ctx.strokeStyle = RECT_COLOR;
     for (let i = 0; i < rects.length; i += 4) {
       let [x1, y1, x2, y2] = rects.slice(i, i + 4);
-      x1 /= this.activeArraySize_.width;
-      y1 /= this.activeArraySize_.height;
-      x2 /= this.activeArraySize_.width;
-      y2 /= this.activeArraySize_.height;
-      [x1, y1] = rotate(x1, y1, this.orientation_);
-      [x2, y2] = rotate(x2, y2, this.orientation_);
-      const canvasAspectRatio = this.canvas_.width / this.canvas_.height;
+      x1 /= this.activeArraySize.width;
+      y1 /= this.activeArraySize.height;
+      x2 /= this.activeArraySize.width;
+      y2 /= this.activeArraySize.height;
+      [x1, y1] = rotate(x1, y1, this.orientation);
+      [x2, y2] = rotate(x2, y2, this.orientation);
+
+      const canvasAspectRatio = this.canvas.width / this.canvas.height;
       const sensorAspectRatio =
-          this.activeArraySize_.width / this.activeArraySize_.height;
+          this.activeArraySize.width / this.activeArraySize.height;
       if (canvasAspectRatio > sensorAspectRatio) {
         // Canvas has wider aspect than the sensor, e.g. when we're showing a
         // 16:9 stream captured from a 4:3 sensor. Based on our hardware
@@ -100,12 +78,12 @@ export class FaceOverlay {
         // active array.
         const normalizedCanvasHeight = sensorAspectRatio / canvasAspectRatio;
         const clipped = (1 - normalizedCanvasHeight) / 2;
-        x1 *= this.canvas_.width;
+        x1 *= this.canvas.width;
         y1 = (Math.max(y1 - clipped, 0) / normalizedCanvasHeight) *
-            this.canvas_.height;
-        x2 *= this.canvas_.width;
+            this.canvas.height;
+        x2 *= this.canvas.width;
         y2 = (Math.max(y2 - clipped, 0) / normalizedCanvasHeight) *
-            this.canvas_.height;
+            this.canvas.height;
       } else if (canvasAspectRatio < sensorAspectRatio) {
         // Canvas has taller aspect than the sensor, e.g. when we're showing a
         // 4:3 stream captured from a 16:9 sensor. Based on our hardware
@@ -114,20 +92,20 @@ export class FaceOverlay {
         const normalizedCanvasWidth = canvasAspectRatio / sensorAspectRatio;
         const clipped = (1 - normalizedCanvasWidth) / 2;
         x1 = (Math.max(x1 - clipped, 0) * normalizedCanvasWidth) *
-            this.canvas_.width;
-        y1 *= this.canvas_.height;
+            this.canvas.width;
+        y1 *= this.canvas.height;
         x2 = (Math.max(x2 - clipped, 0) * normalizedCanvasWidth) *
-            this.canvas_.width;
-        y2 *= this.canvas_.height;
+            this.canvas.width;
+        y2 *= this.canvas.height;
       }
-      this.ctx_.strokeRect(x1, y1, x2 - x1, y2 - y1);
+      this.ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
     }
   }
 
   /**
    * Clears all rectangles.
    */
-  clear() {
-    this.ctx_.clearRect(0, 0, this.canvas_.width, this.canvas_.height);
+  clear(): void {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
