035d27732454e2479f9357afab3270cef1f708bd
Austin Sullivan <asully@chromium.org>
2021-12-22 23:29:19 +0000

    [Code Health] base::Value cleanup after PrefService refactor
    https://crrev.com/c/3343526 recently landed as a LSC, which
    refactored the PrefService GetList() and GetDictionary() methods.
    The blast radius of this interface was quite large, so bare minimum
    changes were made at call sites.
    
    This CL is part of an effort to clean up these call sites.
    
    The unsplit version of this CL can be found at
    https://crrev.com/c/3351044
    
    base::Value refactor guidelines:
    http://doc/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=robertogden@chromium.org
    
    Bug: 1187001, 1187061, 1187062
    Change-Id: I86678a087cc4f5839f7bd47e5097b63f219bac8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353295
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953684}

--- chrome/browser/offline_pages/prefetch/prefetch_background_task_handler_impl.cc
+++ chrome/browser/offline_pages/prefetch/prefetch_background_task_handler_impl.cc
@@ -56,8 +56,7 @@ int PrefetchBackgroundTaskHandlerImpl::GetAdditionalBackoffSeconds() const {
 
 std::unique_ptr<net::BackoffEntry>
 PrefetchBackgroundTaskHandlerImpl::GetCurrentBackoff() const {
-  const base::ListValue* value =
-      &base::Value::AsListValue(*prefs_->GetList(prefetch_prefs::kBackoff));
+  const base::Value* value = prefs_->GetList(prefetch_prefs::kBackoff);
   std::unique_ptr<net::BackoffEntry> result;
   if (value) {
     result = net::BackoffEntrySerializer::DeserializeFromValue(
