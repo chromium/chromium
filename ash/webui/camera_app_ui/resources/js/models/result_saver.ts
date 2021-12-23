d0ac709aa2bc989fd790ca5db2ce204a10149e98
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-23 03:26:50 +0000

    CCA: Migrate models/result_saver.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I7573bf1773e0f3910cff0dc6b20feb0660a51a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300150
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953742}

---
+++ ash/webui/camera_app_ui/resources/js/models/result_saver.ts
@@ -0,0 +1,39 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {VideoSaver} from './video_saver.js';
+
+/**
+ * Handles captured result photos and video.
+ * @interface
+ */
+export interface ResultSaver {
+  /**
+   * Saves photo capture result.
+   * @param blob Data of the photo to be added.
+   * @param name Name of the photo to be saved.
+   */
+  savePhoto(blob: Blob, name: string): Promise<void>;
+
+  /**
+   * Saves gif capture result.
+   * @param blob Data of the gif to be added.
+   * @param name Name of the gif to be saved.
+   */
+  saveGif(blob: Blob, name: string): Promise<void>;
+
+  /**
+   * Returns a video saver to save captured result video.
+   * @param videoRotation Clock-wise rotation in degrees to set in the
+   *     video metadata so that the saved video can be displayed in upright
+   *     orientation.
+   */
+  startSaveVideo(videoRotation: number): Promise<VideoSaver>;
+
+  /**
+   * Saves captured video result.
+   * @param video Contains the video result to be saved.
+   */
+  finishSaveVideo(video: VideoSaver): Promise<void>;
+}
