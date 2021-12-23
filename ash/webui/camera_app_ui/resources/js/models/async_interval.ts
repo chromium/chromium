e2ec832c477647a4d598ceca786840fa26c0a24f
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-23 07:09:21 +0000

    CCA: Migrate models/async_interval.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I1d6ef2230cd69b168e345111d692e35fd4ac53c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300151
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953766}

--- ash/webui/camera_app_ui/resources/js/models/async_interval.ts
+++ ash/webui/camera_app_ui/resources/js/models/async_interval.ts
@@ -6,10 +6,9 @@ import {WaitableEvent} from '../waitable_event.js';
 
 /**
  * Sleeps |delay| millisecond.
- * @param {number} delay
- * @return {!Promise} Resolved after |delay| is passed.
+ * @return Resolved after |delay| is passed.
  */
-function sleep(delay) {
+function sleep(delay: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
@@ -19,58 +18,33 @@ function sleep(delay) {
  * A helper class for setAsyncInterval().
  */
 class AsyncIntervalRunner {
-  /**
-   * @param {function(): !Promise} handler
-   * @param {number} delay
-   */
-  constructor(handler, delay) {
-    /**
-     * @type {function(): !Promise}
-     * @private
-     */
-    this.handler_ = handler;
-
-    /**
-     * @type {number}
-     * @private
-     */
-    this.delay_ = delay;
-
-    /**
-     * @type {!WaitableEvent}
-     * @private
-     */
-    this.stopped_ = new WaitableEvent();
-
-    /**
-     * @type {!Promise}
-     * @private
-     */
-    this.runningPromise_ = this.loop_();
+  private readonly stopped = new WaitableEvent();
+  private readonly runningPromise: Promise<void>;
+  constructor(
+      private readonly handler: () => Promise<void>,
+      private readonly delay: number) {
+    this.runningPromise = this.loop();
   }
-
 
   /**
    * Stops the loop and wait for the |handler| if it's running.
-   * @return {!Promise}
    */
-  async stop() {
-    this.stopped_.signal();
-    await this.runningPromise_;
+  async stop(): Promise<void> {
+    this.stopped.signal();
+    await this.runningPromise;
   }
 
   /**
    * The main loop for running handler repeatedly.
-   * @return {!Promise}
    */
-  async loop_() {
-    while (!this.stopped_.isSignaled()) {
+  private async loop(): Promise<void> {
+    while (!this.stopped.isSignaled()) {
       // Wait until |delay| passed or the runner is stopped.
-      await Promise.race([sleep(this.delay_), this.stopped_.wait()]);
-      if (this.stopped_.isSignaled()) {
+      await Promise.race([sleep(this.delay), this.stopped.wait()]);
+      if (this.stopped.isSignaled()) {
         break;
       }
-      await this.handler_();
+      await this.handler();
     }
   }
 }
@@ -78,24 +52,21 @@ class AsyncIntervalRunner {
 
 /**
  * A counter of runner, which is used as the identifier in setAsyncInterval().
- * @type {number}
  */
 let runnerCount = 0;
 
 /**
  * A map from the async interval id to the corresponding runner.
- * @type {!Map<number, !AsyncIntervalRunner>}
  */
-const runnerMap = new Map();
+const runnerMap = new Map<number, AsyncIntervalRunner>();
 
 /**
  * Repeatedly calls the async function |handler| and waits until it's resolved,
  * with a fixed delay between the next call and the previous completion time.
- * @param {function(): !Promise} handler
- * @param {number} delay
- * @return {number} A numeric, non-zero value which identifies the timer.
+ * @return A numeric, non-zero value which identifies the timer.
  */
-export function setAsyncInterval(handler, delay) {
+export function setAsyncInterval(
+    handler: () => Promise<void>, delay: number): number {
   const runner = new AsyncIntervalRunner(handler, delay);
   const id = ++runnerCount;
   runnerMap.set(id, runner);
@@ -105,10 +76,9 @@ export function setAsyncInterval(handler, delay) {
 /**
  * Cancels a timed, repeating async action by |id|, which was returned by the
  * corresponding call of setAsyncInterval().
- * @param {number} id
- * @return {!Promise} Resolved when the last action is finished.
+ * @return Resolved when the last action is finished.
  */
-export async function clearAsyncInterval(id) {
+export async function clearAsyncInterval(id: number): Promise<void> {
   const runner = runnerMap.get(id);
   if (runner === undefined) {
     return;
