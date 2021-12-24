92546cb739620e2328d62b5fcc6145703a727ab5
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-24 12:32:31 +0000

    CCA: Migrate models/file_namer.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: If92b25f4693f6eeebcdf2c2d4a104e1e894f5cb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301629
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954037}

--- ash/webui/camera_app_ui/resources/js/models/file_namer.ts
+++ ash/webui/camera_app_ui/resources/js/models/file_namer.ts
@@ -5,45 +5,40 @@
 import {assertNotReached} from '../assert.js';
 import {
   MimeType,
-  VideoType,  // eslint-disable-line no-unused-vars
+  VideoType,
 } from '../type.js';
 
 /**
  * The prefix of image files.
- * @type {string}
  */
 export const IMAGE_PREFIX = 'IMG_';
 
 /**
  * The prefix of video files.
- * @type {string}
  */
 export const VIDEO_PREFIX = 'VID_';
 
 /**
  * The prefix of scanned document files.
- * @type {string}
  */
 export const DOCUMENT_PREFIX = 'SCN_';
 
 /**
  * The suffix of burst image files.
- * @type {string}
  */
 const BURST_SUFFIX = '_BURST';
 
 /**
  * The suffix of cover image for a series of burst image files.
- * @type {string}
  */
 const BURST_COVER_SUFFIX = '_COVER';
 
 /**
  * Transforms from capture timestamp to datetime name.
- * @param {number} timestamp Timestamp to be transformed.
- * @return {string} Transformed datetime name.
+ * @param timestamp Timestamp to be transformed.
+ * @return Transformed datetime name.
  */
-function timestampToDatetimeName(timestamp) {
+function timestampToDatetimeName(timestamp: number): string {
   const pad = (n) => (n < 10 ? '0' : '') + n;
   const date = new Date(timestamp);
   return date.getFullYear() + pad(date.getMonth() + 1) + pad(date.getDate()) +
@@ -56,82 +51,77 @@ function timestampToDatetimeName(timestamp) {
  */
 export class Filenamer {
   /**
-   * @param {number=} timestamp Timestamp of camera session.
+   * Timestamp of camera session.
    */
-  constructor(timestamp) {
-    /**
-     * Timestamp of camera session.
-     * @type {number}
-     * @private
-     */
-    this.timestamp_ = timestamp === undefined ? Date.now() : timestamp;
+  private readonly timestamp: number;
+  /**
+   * Number of already saved burst images.
+   */
+  private burstCount = 0;
 
-    /**
-     * Number of already saved burst images.
-     * @type {number}
-     * @private
-     */
-    this.burstCount_ = 0;
+  /**
+   * @param timestamp Timestamp of camera session.
+   */
+  constructor(timestamp?: number) {
+    this.timestamp = timestamp ?? Date.now();
   }
 
   /**
    * Creates new filename for burst image.
-   * @param {boolean} isCover If the image is set as cover of the burst.
-   * @return {string} New filename.
+   * @param isCover If the image is set as cover of the burst.
+   * @return New filename.
    */
-  newBurstName(isCover) {
+  newBurstName(isCover: boolean): string {
     const prependZeros = (n, width) => {
       n = n + '';
       return new Array(Math.max(0, width - n.length) + 1).join('0') + n;
     };
-    return IMAGE_PREFIX + timestampToDatetimeName(this.timestamp_) +
-        BURST_SUFFIX + prependZeros(++this.burstCount_, 5) +
+    return IMAGE_PREFIX + timestampToDatetimeName(this.timestamp) +
+        BURST_SUFFIX + prependZeros(++this.burstCount, 5) +
         (isCover ? BURST_COVER_SUFFIX : '') + '.jpg';
   }
 
   /**
    * Creates new filename for video.
-   * @param {VideoType} videoType
-   * @return {string} New filename.
+   * @return New filename.
    */
-  newVideoName(videoType) {
-    return VIDEO_PREFIX + timestampToDatetimeName(this.timestamp_) + '.' +
+  newVideoName(videoType: VideoType): string {
+    return VIDEO_PREFIX + timestampToDatetimeName(this.timestamp) + '.' +
         videoType;
   }
 
   /**
    * Creates new filename for image.
-   * @return {string} New filename.
+   * @return New filename.
    */
-  newImageName() {
-    return IMAGE_PREFIX + timestampToDatetimeName(this.timestamp_) + '.jpg';
+  newImageName(): string {
+    return IMAGE_PREFIX + timestampToDatetimeName(this.timestamp) + '.jpg';
   }
 
   /**
    * Creates new filename for pdf.
-   * @param {!MimeType} type
-   * @return {string} New filename.
+   * @return New filename.
    */
-  newDocumentName(type) {
+  newDocumentName(mimeType: MimeType): string {
     const ext = (() => {
-      switch (type) {
+      switch (mimeType) {
         case MimeType.JPEG:
           return 'jpg';
         case MimeType.PDF:
           return 'pdf';
       }
-      assertNotReached(`Unknown type ${type}`);
+      assertNotReached(`Unknown type ${mimeType}`);
     })();
-    return DOCUMENT_PREFIX + timestampToDatetimeName(this.timestamp_) + '.' +
+    return DOCUMENT_PREFIX + timestampToDatetimeName(this.timestamp) + '.' +
         ext;
   }
 
   /**
    * Get the metadata name from image name.
-   * @param {string} imageName Name of image to derive the metadata name.
-   * @return {string} Metadata name of the image.
+   * @param imageName Name of image to derive the metadata name.
+   * @return Metadata name of the image.
    */
-  static getMetadataName(imageName) {
+  static getMetadataName(imageName: string): string {
     return imageName.replace(/\.[^/.]+$/, '.json');
   }
 }
