9944bbb0e797a9e9fe6fd07cfae1e949a124a9c6
Akihiro Ota <akihiroota@chromium.org>
2021-12-24 00:21:57 +0000

    Accessibility: Add crash keys for crash in A11yAnimationOneShot ctor
    This change adds crash keys to help us further diagnose the root issue
    of the associated crash (see bug for more details).
    
    AX-Relnotes: N/A
    Bug: 1254275
    Change-Id: Ia01f32be73715f49bcf91a6a37746dd01228c0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354592
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953971}

--- ash/accessibility/ui/accessibility_animation_one_shot.cc
+++ ash/accessibility/ui/accessibility_animation_one_shot.cc
@@ -5,6 +5,7 @@
 #include "ash/accessibility/ui/accessibility_animation_one_shot.h"
 
 #include "ash/shell.h"
+#include "base/debug/crash_logging.h"
 #include "ui/compositor/layer.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -17,7 +18,14 @@ AccessibilityAnimationOneShot::AccessibilityAnimationOneShot(
     : callback_(callback) {
   display::Display display =
       display::Screen::GetScreen()->GetDisplayMatching(bounds_in_dip);
+  // Crash keys for https://crbug.com/1254275
+  SCOPED_CRASH_KEY_STRING32("Accessibility", "BoundsInDip",
+                            bounds_in_dip.ToString());
+  SCOPED_CRASH_KEY_STRING32("Accessibility", "Display", display.ToString());
   aura::Window* root_window = Shell::GetRootWindowForDisplayId(display.id());
+  SCOPED_CRASH_KEY_STRING32(
+      "Accessibility", "RootWindow",
+      !root_window ? "Root window is null" : "Root window is valid");
   ui::Compositor* compositor = root_window->layer()->GetCompositor();
   animation_observation_.Observe(compositor);
 }
