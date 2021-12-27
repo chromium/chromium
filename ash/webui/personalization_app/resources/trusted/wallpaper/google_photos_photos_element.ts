a7aa61ce705294cf1a59ca915e7b3ae35a935796
David Black <dmblack@google.com>
2021-12-27 01:22:06 +0000

    personalization: Migrate GooglePhotosPhotos to typescript.
    Migrates the existing .js file to .ts.
    
    Bug: b:205773295
    Change-Id: Ide9a89f464918f6b0f4c1c642e147be2bcd5bff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352608
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#954131}

--- ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_element.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_element.ts
@@ -9,16 +9,22 @@
 import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import './styles.js';
-import '/common/styles.js';
+import '../../common/styles.js';
 
-import {getNumberOfGridItemsPerRow, isNonEmptyArray, isSelectionEvent, normalizeKeyForRTL} from '/common/utils.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
 import {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
+import {IronListElement} from 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 import {afterNextRender, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {getNumberOfGridItemsPerRow, isNonEmptyArray, isSelectionEvent, normalizeKeyForRTL} from '../../common/utils.js';
 import {WithPersonalizationStore} from '../personalization_store.js';
 
-/** @polymer */
+type PhotosRow = Array<Url|undefined>;
+
+export interface GooglePhotosPhotos {
+  $: {grid: IronListElement;};
+}
+
 export class GooglePhotosPhotos extends WithPersonalizationStore {
   static get is() {
     return 'google-photos-photos';
@@ -30,10 +36,6 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
 
   static get properties() {
     return {
-      /**
-       * Whether or not this element is currently hidden.
-       * @type {boolean}
-       */
       hidden: {
         type: Boolean,
         value: true,
@@ -41,50 +43,20 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
         observer: 'onHiddenChanged_',
       },
 
-      /**
-       * The index of the currently focused column.
-       * @type {number}
-       * @private
-       */
       focusedColIndex_: {
         type: Number,
         value: 0,
       },
 
-      /**
-       * The list of photos.
-       * @type {?Array<Url>}
-       * @private
-       */
-      photos_: {
-        type: Array,
-      },
+      photos_: Array,
 
-      /**
-       * The list of |photos_| split into the appropriate number of
-       * |photosPerRow_| so as to be rendered in a grid.
-       * @type {?Array<Array<Url>>}
-       * @private
-       */
       photosByRow_: {
         type: Array,
         computed: 'computePhotosByRow_(photos_, photosLoading_, photosPerRow_)',
       },
 
-      /**
-       * Whether the list of photos is currently loading.
-       * @type {boolean}
-       * @private
-       */
-      photosLoading_: {
-        type: Boolean,
-      },
+      photosLoading_: Boolean,
 
-      /**
-       * The number of photos to render per row in a grid.
-       * @type {number}
-       * @private
-       */
       photosPerRow_: {
         type: Number,
         value: function() {
@@ -94,24 +66,42 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
     };
   }
 
-  /** @override */
+  /** Whether or not this element is currently hidden. */
+  hidden: boolean;
+
+  /** The index of the currently focused column. */
+  private focusedColIndex_: number;
+
+  /** The list of photos. */
+  private photos_: Url[]|null|undefined;
+
+  /**
+   * The list of |photos_| split into the appropriate number of |photosPerRow_|
+   * so as to be rendered in a grid.
+   */
+  private photosByRow_: PhotosRow[]|null;
+
+  /** Whether the list of photos is currently loading. */
+  private photosLoading_: boolean;
+
+  /** The number of photos to render per row in a grid. */
+  private photosPerRow_: number;
+
   connectedCallback() {
     super.connectedCallback();
 
     this.addEventListener('iron-resize', this.onResized_.bind(this));
 
-    this.watch('photos_', state => state.wallpaper.googlePhotos.photos);
-    this.watch(
+    this.watch<GooglePhotosPhotos['photos_']>(
+        'photos_', state => state.wallpaper.googlePhotos.photos);
+    this.watch<GooglePhotosPhotos['photosLoading_']>(
         'photosLoading_', state => state.wallpaper.loading.googlePhotos.photos);
 
     this.updateFromStore();
   }
 
-  /**
-   * Invoked on changes to this element's |hidden| state.
-   * @private
-   */
-  onHiddenChanged_() {
+  /** Invoked on changes to this element's |hidden| state. */
+  private onHiddenChanged_() {
     if (this.hidden) {
       return;
     }
@@ -122,26 +112,18 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
     afterNextRender(this, () => this.$.grid.fire('iron-resize'));
   }
 
-  /**
-   * Invoked on focus of a grid row.
-   * @param {!Event } e
-   * @private
-   */
-  onGridRowFocused_(e) {
+  /** Invoked on focus of a grid row. */
+  private onGridRowFocused_(e: Event) {
     // When a grid row is focused, forward the focus event on to the grid item
     // at the focused column index.
+    const currentTarget = e.currentTarget as HTMLElement;
     const selector = `.photo[colindex="${this.focusedColIndex_}"]`;
-    e.currentTarget.querySelector(selector)?.focus();
+    (currentTarget.querySelector(selector) as HTMLElement | null)?.focus();
   }
 
-  /**
-   * Invoked on key down of a grid row.
-   * @param {!Event} e
-   * @private
-   */
-  onGridRowKeyDown_(e) {
-    const row = (/** @type {{row: Array<undefined>}} */ (e.model)).row;
-
+  /** Invoked on key down of a grid row. */
+  private onGridRowKeyDown_(e: KeyboardEvent&
+                            {model: {index: number, row: Url[]}}) {
     switch (normalizeKeyForRTL(e.key, this.i18n('textdirection') === 'rtl')) {
       case 'ArrowLeft':
         if (this.focusedColIndex_ > 0) {
@@ -151,16 +133,16 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
         } else if (e.model.index > 0) {
           // Left arrow moves focus to the preceding grid item, wrapping to the
           // preceding grid row.
-          this.focusedColIndex_ = row.length - 1;
+          this.focusedColIndex_ = e.model.row.length - 1;
           this.$.grid.focusItem(e.model.index - 1);
         }
         return;
       case 'ArrowRight':
-        if (this.focusedColIndex_ < row.length - 1) {
+        if (this.focusedColIndex_ < e.model.row.length - 1) {
           // Right arrow moves focus to the succeeding grid item.
           this.focusedColIndex_ += 1;
           this.$.grid.focusItem(e.model.index);
-        } else if (e.model.index < this.photosByRow_.length - 1) {
+        } else if (e.model.index < this.photosByRow_!.length - 1) {
           // Right arrow moves focus to the succeeding grid item, wrapping to
           // the succeeding grid row.
           this.focusedColIndex_ = 0;
@@ -172,27 +154,20 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
         // due to the synthetic redirect of focus events to grid items. To
         // escape the trap, make the |focusable| row unfocusable until has
         // advanced to the next candidate.
-        const focusable = this.$.grid.querySelector('[tabindex="0"]');
-        focusable.setAttribute('tabindex', -1);
-        afterNextRender(focusable, () => focusable.setAttribute('tabindex', 0));
+        const focusable = this.$.grid.querySelector('[tabindex="0"]')!;
+        focusable.setAttribute('tabindex', '-1');
+        afterNextRender(this, () => focusable.setAttribute('tabindex', '0'));
         return;
     }
   }
 
-  /**
-   * Invoked on resize of this element.
-   * @private
-   */
-  onResized_() {
+  /** Invoked on resize of this element. */
+  private onResized_() {
     this.photosPerRow_ = getNumberOfGridItemsPerRow();
   }
 
-  /**
-   * Invoked to compute |photosByRow_|.
-   * @return {?Array<Array<Url>>}
-   * @private
-   */
-  computePhotosByRow_() {
+  /** Invoked to compute |photosByRow_|. */
+  private computePhotosByRow_(): PhotosRow[]|null {
     if (this.photosLoading_ || !this.photosPerRow_) {
       return null;
     }
@@ -203,7 +178,7 @@ export class GooglePhotosPhotos extends WithPersonalizationStore {
         {length: Math.ceil(this.photos_.length / this.photosPerRow_)},
         (_, i) => {
           i *= this.photosPerRow_;
-          const row = this.photos_.slice(i, i + this.photosPerRow_);
+          const row: PhotosRow = this.photos_!.slice(i, i + this.photosPerRow_);
           while (row.length < this.photosPerRow_) {
             row.push(undefined);
           }
