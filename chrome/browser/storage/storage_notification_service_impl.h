c5571653d9323402e7ab316f97e675b8e7f2c6e1
Jarryd <jarrydg@chromium.org>
2021-12-29 19:49:22 +0000

    Quota: Use Threadsafe Pressure Callback.
    Fixes UAF by removing use of raw ptr to StorageNotificationService.
    Instead, the service's interface exposes a method to create a
    thread-safe callback to pass to the quota manager instead.
    
    This change also changes the parameter type for the call chain from
    url::Origin to blink::StorageKey to match the type Quota is keyed on.
    
    Bug:1275020
    
    (cherry picked from commit e304c0373f9cc4a65d39d7094e4897627e83390e)
    
    Change-Id: Icc696d22fa41324e7a6c056599db635bb5de6291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347939
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#953375}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360203
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4664@{#1352}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- chrome/browser/storage/storage_notification_service_impl.h
+++ chrome/browser/storage/storage_notification_service_impl.h
@@ -5,10 +5,12 @@
 #ifndef CHROME_BROWSER_STORAGE_STORAGE_NOTIFICATION_SERVICE_IMPL_H_
 #define CHROME_BROWSER_STORAGE_STORAGE_NOTIFICATION_SERVICE_IMPL_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/storage_notification_service.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 
 class StorageNotificationServiceImpl
     : public content::StorageNotificationService,
@@ -16,13 +18,20 @@ class StorageNotificationServiceImpl
  public:
   StorageNotificationServiceImpl();
   ~StorageNotificationServiceImpl() override;
-  void MaybeShowStoragePressureNotification(const url::Origin) override;
+  // Called from the UI thread, this method returns a callback that can passed
+  // to any thread, and proxies calls to
+  // `MaybeShowStoragePressureNotification()` back to the UI thread. It wraps a
+  // weak pointer to `this`.
+  StoragePressureNotificationCallback
+  CreateThreadSafePressureNotificationCallback() override;
+  void MaybeShowStoragePressureNotification(const blink::StorageKey) override;
   base::TimeTicks GetLastSentAtForTesting() {
     return disk_pressure_notification_last_sent_at_;
   }
 
  private:
   base::TimeTicks disk_pressure_notification_last_sent_at_;
+  base::WeakPtrFactory<StorageNotificationServiceImpl> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_STORAGE_STORAGE_NOTIFICATION_SERVICE_IMPL_H_
