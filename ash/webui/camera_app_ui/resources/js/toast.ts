7fafd8b2a74806d881b1f8faee5c1eb61fdc0e7c
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-29 10:48:28 +0000

    CCA: Migrate toast.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I1fc5743cc2f3184e729ad6fc679b389b192a587e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305006
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954428}

--- ash/webui/camera_app_ui/resources/js/toast.ts
+++ ash/webui/camera_app_ui/resources/js/toast.ts
@@ -4,16 +4,15 @@
 
 import * as animate from './animation.js';
 import * as dom from './dom.js';
-// eslint-disable-next-line no-unused-vars
 import {I18nString} from './i18n_string.js';
 import * as loadTimeData from './models/load_time_data.js';
 
 /**
  * Updates the toast message.
- * @param {string} message Message to be updated.
- * @param {boolean} spoken Whether the toast is spoken only.
+ * @param message Message to be updated.
+ * @param spoken Whether the toast is spoken only.
  */
-function update(message, spoken) {
+function update(message: string, spoken: boolean) {
   // TTS speaks changes of on-screen aria-live elements. Force content changes
   // and clear content once inactive to avoid stale content being read out.
   const element = dom.get('#toast', HTMLElement);
@@ -31,28 +30,26 @@ function update(message, spoken) {
 
 /**
  * Shows a toast with given message which doesn't need i18n.
- * @param {string} message Message to be shown.
+ * @param message Message to be shown.
  */
-export function showDebugMessage(message) {
+export function showDebugMessage(message: string): void {
   update(message, false);
 }
 
 /**
  * Shows a toast message.
- * @param {!I18nString} label The label of the message to show.
- * @param {...string} substitutions The substitutions needed for the given
- *     label.
+ * @param label The label of the message to show.
+ * @param substitutions The substitutions needed for the given label.
  */
-export function show(label, ...substitutions) {
+export function show(label: I18nString, ...substitutions: string[]): void {
   update(loadTimeData.getI18nMessage(label, ...substitutions), false);
 }
 
 /**
  * Speaks a toast message.
- * @param {!I18nString} label The label of the message to show.
- * @param {...string} substitutions The substitutions needed for the given
- *     label.
+ * @param label The label of the message to show.
+ * @param substitutions The substitutions needed for the given label.
  */
-export function speak(label, ...substitutions) {
+export function speak(label: I18nString, ...substitutions: string[]): void {
   update(loadTimeData.getI18nMessage(label, ...substitutions), true);
 }
