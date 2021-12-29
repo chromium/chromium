c98b0040bb4986bff36a4058df18e94254a415d1
Thomas Tellier <tellier@google.com>
2021-12-29 16:27:21 +0000

    [M98][CrOS] Fix ChromeVox on MGS language and keyboard selection
    Replace the customized language and keyboard bubbles with standard
    comboboxes.
    
    (cherry picked from commit 09f45329024b32ecd84bdfbc2582d8b3b96d8dae)
    
    Bug: 1229904, 1275928
    Change-Id: I8db9004a93bd75e38f839fabe226d678ef4ba61b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320319
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#953523}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347988
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4758@{#264}
    Cr-Branched-From: 4a2cf4baf90326df19c3ee70ff987960d59a386e-refs/heads/main@{#950365}

--- ash/login/ui/public_account_menu_view.cc
+++ ash/login/ui/public_account_menu_view.cc
@@ -4,227 +4,79 @@
 
 #include "ash/login/ui/public_account_menu_view.h"
 
-#include <algorithm>
-#include <iterator>
-#include <memory>
-#include <utility>
-
 #include "ash/login/ui/hover_notifier.h"
 #include "ash/login/ui/non_accessible_view.h"
 #include "ash/style/ash_color_provider.h"
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
-#include "ui/views/controls/button/button.h"
-#include "ui/views/controls/label.h"
-#include "ui/views/controls/scroll_view.h"
-#include "ui/views/controls/scrollbar/overlay_scroll_bar.h"
-#include "ui/views/layout/box_layout.h"
+#include "ui/base/models/combobox_model.h"
 
 namespace ash {
 
 constexpr int kMenuItemWidthDp = 192;
-constexpr int kMenuItemHeightDp = 28;
-constexpr int kRegularMenuItemLeftPaddingDp = 2;
-constexpr int kGroupMenuItemLeftPaddingDp = 10;
-constexpr int kNonEmptyHeight = 1;
 
 namespace {
 
-class MenuItemView : public views::Button {
+class PublicAccountComboboxModel : public ui::ComboboxModel {
  public:
-  MenuItemView(const PublicAccountMenuView::Item& item,
-               const PublicAccountMenuView::OnHighlight& on_highlight)
-      : views::Button(base::BindRepeating(&MenuItemView::ButtonPressed,
-                                          base::Unretained(this))),
-        item_(item),
-        on_highlight_(on_highlight) {
-    SetFocusBehavior(FocusBehavior::ALWAYS);
-    set_suppress_default_focus_handling();
-    SetLayoutManager(std::make_unique<views::BoxLayout>(
-        views::BoxLayout::Orientation::kHorizontal));
-    SetPreferredSize(gfx::Size(kMenuItemWidthDp, kMenuItemHeightDp));
+  PublicAccountComboboxModel(
+      const std::vector<PublicAccountMenuView::Item>& items,
+      const size_t default_index)
+      : items_(items), default_index_(default_index) {}
 
-    auto spacing = std::make_unique<NonAccessibleView>();
-    spacing->SetPreferredSize(gfx::Size(item.is_group
-                                            ? kRegularMenuItemLeftPaddingDp
-                                            : kGroupMenuItemLeftPaddingDp,
-                                        kNonEmptyHeight));
-    AddChildView(std::move(spacing));
+  PublicAccountComboboxModel(const PublicAccountComboboxModel&) = delete;
+  PublicAccountComboboxModel& operator=(const PublicAccountComboboxModel&) =
+      delete;
 
-    auto label = std::make_unique<views::Label>(base::UTF8ToUTF16(item.title));
-    label->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
-        AshColorProvider::ContentLayerType::kTextColorPrimary));
-    label->SetSubpixelRenderingEnabled(false);
-    label->SetAutoColorReadabilityEnabled(false);
-    label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-    AddChildView(std::move(label));
+  ~PublicAccountComboboxModel() override = default;
 
-    if (item.selected)
-      SetBackground(views::CreateSolidBackground(SK_ColorGRAY));
+  // ui::ComboboxModel:
+  int GetItemCount() const override { return items_.size(); }
 
-    hover_notifier_ = std::make_unique<HoverNotifier>(
-        this,
-        base::BindRepeating(&MenuItemView::OnHover, base::Unretained(this)));
-  }
-  MenuItemView(const MenuItemView&) = delete;
-  MenuItemView& operator=(const MenuItemView&) = delete;
-  ~MenuItemView() override = default;
-
-  // views::View:
-  int GetHeightForWidth(int w) const override {
-    // Make row height fixed avoiding layout manager adjustments.
-    return GetPreferredSize().height();
+  // ui::ComboboxModel:
+  std::u16string GetItemAt(int index) const override {
+    return base::UTF8ToUTF16(items_[index].title);
   }
 
-  void OnHover(bool has_hover) {
-    if (has_hover && !item_.is_group)
-      RequestFocus();
+  // ui::ComboboxModel:
+  // Alternatively, we could override `IsItemSeparatorAt`, especially since
+  // group items are considered as some sort of separators. We choose to
+  // represent them as disabled items because they were presented in a similar
+  // fashion before (i.e. the group name was visible but unclickable).
+  bool IsItemEnabledAt(int index) const override {
+    return !items_[index].is_group;
   }
 
-  void OnFocus() override {
-    ScrollViewToVisible();
-    on_highlight_.Run(false /*by_selection*/);
-  }
-
-  const PublicAccountMenuView::Item& item() const { return item_; }
+  // ui::ComboboxModel:
+  int GetDefaultIndex() const override { return default_index_; }
 
  private:
-  void ButtonPressed() {
-    if (!item_.is_group)
-      on_highlight_.Run(true /*by_selection*/);
-  }
-
-  const PublicAccountMenuView::Item item_;
-  const PublicAccountMenuView::OnHighlight on_highlight_;
-  std::unique_ptr<HoverNotifier> hover_notifier_;
-};
-
-class LoginScrollBar : public views::OverlayScrollBar {
- public:
-  LoginScrollBar() : OverlayScrollBar(false) {}
-  LoginScrollBar(const LoginScrollBar&) = delete;
-  LoginScrollBar& operator=(const LoginScrollBar&) = delete;
-  ~LoginScrollBar() override = default;
-
-  // OverlayScrollBar:
-  bool OnKeyPressed(const ui::KeyEvent& event) override {
-    // Let LoginMenuView to handle up/down keypress.
-    return false;
-  }
+  const std::vector<PublicAccountMenuView::Item>& items_;
+  const int default_index_;
 };
 
 }  // namespace
 
-PublicAccountMenuView::TestApi::TestApi(PublicAccountMenuView* view)
-    : view_(view) {}
-
-PublicAccountMenuView::TestApi::~TestApi() = default;
-
-views::View* PublicAccountMenuView::TestApi::contents() const {
-  return view_->contents_;
-}
-
 PublicAccountMenuView::Item::Item() = default;
 
 PublicAccountMenuView::PublicAccountMenuView(const std::vector<Item>& items,
-                                             views::View* anchor_view,
-                                             LoginButton* opener,
+                                             const size_t selected_index,
                                              const OnSelect& on_select)
-    : LoginBaseBubbleView(anchor_view), opener_(opener), on_select_(on_select) {
-  SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
-  set_suppress_default_focus_handling();
+    : views::Combobox(new PublicAccountComboboxModel(items, selected_index)),
+      items_(items),
+      on_select_(on_select) {
+  SetPreferredSize(
+      gfx::Size(kMenuItemWidthDp, GetHeightForWidth(kMenuItemWidthDp)));
 
-  scroller_ = new views::ScrollView();
-  scroller_->SetBackgroundColor(absl::nullopt);
-  scroller_->SetDrawOverflowIndicator(false);
-  scroller_->ClipHeightTo(kMenuItemHeightDp, kMenuItemHeightDp * 5);
-  AddChildView(scroller_);
-
-  views::BoxLayout* box_layout =
-      SetLayoutManager(std::make_unique<views::BoxLayout>(
-          views::BoxLayout::Orientation::kVertical));
-  box_layout->SetFlexForView(scroller_, 1);
-
-  auto contents = std::make_unique<NonAccessibleView>();
-  views::BoxLayout* layout =
-      contents->SetLayoutManager(std::make_unique<views::BoxLayout>(
-          views::BoxLayout::Orientation::kVertical));
-  layout->SetDefaultFlex(1);
-  layout->set_minimum_cross_axis_size(kMenuItemWidthDp);
-  layout->set_main_axis_alignment(views::BoxLayout::MainAxisAlignment::kCenter);
-
-  for (size_t i = 0; i < items.size(); i++) {
-    const Item& item = items[i];
-    contents->AddChildView(new MenuItemView(
-        item, base::BindRepeating(&PublicAccountMenuView::OnHighlightChange,
-                                  base::Unretained(this), i)));
-
-    if (item.selected)
-      selected_index_ = i;
-  }
-  contents_ = scroller_->SetContents(std::move(contents));
-  scroller_->SetVerticalScrollBar(std::make_unique<LoginScrollBar>());
+  property_changed_subscription_ = AddSelectedIndexChangedCallback(
+      base::BindRepeating(&PublicAccountMenuView::OnSelectedIndexChanged,
+                          weak_factory_.GetWeakPtr()));
 }
 
 PublicAccountMenuView::~PublicAccountMenuView() = default;
 
-void PublicAccountMenuView::OnHighlightChange(size_t item_index,
-                                              bool by_selection) {
-  selected_index_ = item_index;
-  views::View* highlight_item = contents_->children()[item_index];
-  for (views::View* child : contents_->GetChildrenInZOrder()) {
-    child->SetBackground(views::CreateSolidBackground(
-        child == highlight_item ? SK_ColorGRAY : SK_ColorTRANSPARENT));
-  }
-
-  if (by_selection) {
-    SetVisible(false);
-    MenuItemView* menu_view = static_cast<MenuItemView*>(highlight_item);
-    on_select_.Run(menu_view->item());
-  }
-  contents_->SchedulePaint();
-}
-
-LoginButton* PublicAccountMenuView::GetBubbleOpener() const {
-  return opener_;
-}
-
-void PublicAccountMenuView::OnFocus() {
-  // Forward the focus to the selected child view.
-  contents_->children()[selected_index_]->RequestFocus();
-}
-
-bool PublicAccountMenuView::OnKeyPressed(const ui::KeyEvent& event) {
-  const ui::KeyboardCode key = event.key_code();
-  if (key == ui::VKEY_UP || key == ui::VKEY_DOWN) {
-    FindNextItem(key == ui::VKEY_UP)->RequestFocus();
-    return true;
-  }
-
-  return false;
-}
-
-void PublicAccountMenuView::VisibilityChanged(View* starting_from,
-                                              bool is_visible) {
-  if (is_visible)
-    contents_->children()[selected_index_]->RequestFocus();
-}
-
-views::View* PublicAccountMenuView::FindNextItem(bool reverse) {
-  const auto& children = contents_->children();
-  const auto is_item = [](views::View* v) {
-    return !static_cast<MenuItemView*>(v)->item().is_group;
-  };
-  const auto begin = std::next(children.begin(), selected_index_);
-  if (reverse) {
-    // Subtle: make_reverse_iterator() will result in an iterator that refers to
-    // the element before its argument, which is what we want.
-    const auto i = std::find_if(std::make_reverse_iterator(begin),
-                                children.rend(), is_item);
-    return (i == children.rend()) ? *begin : *i;
-  }
-  const auto i = std::find_if(std::next(begin), children.end(), is_item);
-  return (i == children.end()) ? *begin : *i;
+void PublicAccountMenuView::OnSelectedIndexChanged() {
+  on_select_.Run(items_[GetSelectedIndex()].value);
 }
 
 }  // namespace ash
