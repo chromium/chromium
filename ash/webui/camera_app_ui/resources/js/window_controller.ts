f7834372986bc2b17b3e8e351814d9b20fbdeca3
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-03 08:15:42 +0000

    CCA: Migrate window_controller.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I85e4c0575692736da5565471119375c37dfabd66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313086
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954891}

---
+++ ash/webui/camera_app_ui/resources/js/window_controller.ts
@@ -0,0 +1,119 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertInstanceof} from './assert.js';
+import {
+  WindowStateControllerRemote,
+  WindowStateMonitorCallbackRouter,
+  WindowStateType,
+} from './mojo/type.js';
+import {wrapEndpoint} from './mojo/util.js';
+
+type WindowStateChangedEventListener = (states: WindowStateType[]) => void;
+
+/**
+ * Controller to get/set/listener for window state.
+ */
+export class WindowController {
+  /**
+   * The remote controller from Mojo interface.
+   */
+  private windowStateController: WindowStateControllerRemote|null = null;
+
+  /**
+   * Current window states.
+   */
+  private windowStates: WindowStateType[] = [];
+
+  /**
+   * Set of the listeners for window state changed events.
+   */
+  private readonly listeners = new Set<WindowStateChangedEventListener>();
+
+  /**
+   * Binds the controller remote from Mojo interface.
+   */
+  async bind(remoteController: WindowStateControllerRemote): Promise<void> {
+    this.windowStateController = remoteController;
+
+    const windowMonitorCallbackRouter =
+        wrapEndpoint(new WindowStateMonitorCallbackRouter());
+    windowMonitorCallbackRouter.onWindowStateChanged.addListener((states) => {
+      this.windowStates = states;
+      this.listeners.forEach((listener) => listener(states));
+    });
+    const {states} = await this.windowStateController.addMonitor(
+        windowMonitorCallbackRouter.$.bindNewPipeAndPassRemote());
+    this.windowStates = states;
+  }
+
+  /**
+   * Minimizes the window.
+   */
+  minimize(): Promise<void> {
+    return assertInstanceof(
+               this.windowStateController, WindowStateControllerRemote)
+        .minimize();
+  }
+
+  /**
+   * Maximizes the window.
+   */
+  maximize(): Promise<void> {
+    return assertInstanceof(
+               this.windowStateController, WindowStateControllerRemote)
+        .maximize();
+  }
+
+  /**
+   * Restores the window and leaves maximized/minimized/fullscreen state.
+   */
+  restore(): Promise<void> {
+    return assertInstanceof(
+               this.windowStateController, WindowStateControllerRemote)
+        .restore();
+  }
+
+  /**
+   * Makes the window fullscreen.
+   */
+  fullscreen(): Promise<void> {
+    return assertInstanceof(
+               this.windowStateController, WindowStateControllerRemote)
+        .fullscreen();
+  }
+
+  /**
+   * Focuses the window.
+   */
+  focus(): Promise<void> {
+    return assertInstanceof(
+               this.windowStateController, WindowStateControllerRemote)
+        .focus();
+  }
+
+  /**
+   * Returns true if the window is currently minimized.
+   */
+  isMinimized(): boolean {
+    return this.windowStates.includes(WindowStateType.MINIMIZED);
+  }
+
+  /**
+   * Returns true if the window is currently fullscreen or maximized.
+   */
+  isFullscreenOrMaximized(): boolean {
+    return this.windowStates.includes(WindowStateType.FULLSCREEN) ||
+        this.windowStates.includes(WindowStateType.MAXIMIZED);
+  }
+
+  /**
+   * Adds listener for the window state (including window size) changed events.
+   */
+  addListener(listener: WindowStateChangedEventListener): void {
+    this.listeners.add(listener);
+  }
+}
+
+export const windowController = new WindowController();
