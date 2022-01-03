5ba779b5dc36e19771a081a66e2d71e7e2010266
Titouan Rigoudy <titouan@chromium.org>
2022-01-03 13:46:20 +0000

    [Private Network Access] Test service worker registration fetches.
    Tests are currently failing because the initiator's ClientSecurityState
    is not correctly passed to the URLLoaderFactory used to perform the
    fetches from the browser process.
    
    Bug: chromium:1239551
    Change-Id: I83f57ee4f1fd81f837deb51522bda71db0aab579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3356197
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954928}

---
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/service-worker.https.window.js
@@ -0,0 +1,68 @@
+// META: script=/common/utils.js
+// META: script=resources/support.sub.js
+//
+// Spec: https://wicg.github.io/private-network-access/#integration-fetch
+//
+// These tests check that initial `ServiceWorker` script fetches are subject to
+// Private Network Access checks, just like a regular `fetch()`.
+// See also: worker.https.window.js
+
+const TestResult = {
+  SUCCESS: {
+    loaded: true,
+    unregistered: true,
+  },
+  FAILURE: {
+    loaded: false,
+    error: "TypeError",
+  },
+};
+
+async function serviceWorkerScriptTest(t, { source, target, expected }) {
+  const sourceUrl = resolveUrl("resources/service-worker-fetcher.html",
+                               sourceResolveOptions(source));
+
+  const targetUrl =
+      resolveUrl("resources/preflight.py", targetResolveOptions(target));
+  targetUrl.searchParams.append("body", "undefined");
+  targetUrl.searchParams.append("mime-type", "application/javascript");
+
+  const iframe = await appendIframe(t, document, sourceUrl);
+  const reply = futureMessage();
+
+  const message = {
+    url: targetUrl.href,
+    uuid: token(),
+  };
+  iframe.contentWindow.postMessage(message, "*");
+
+  const { error, loaded, unregistered } = await reply;
+
+  assert_equals(error, expected.error, "error");
+  assert_equals(loaded, expected.loaded, "response loaded");
+  assert_equals(unregistered, expected.unregistered, "worker unregistered");
+}
+
+promise_test(t => serviceWorkerScriptTest(t, {
+  source: {
+    server: Server.HTTPS_LOCAL,
+    treatAsPublic: true,
+  },
+  target: { server: Server.HTTPS_LOCAL },
+  expected: TestResult.FAILURE,
+}), "treat-as-public to local: failure.");
+
+promise_test(t => serviceWorkerScriptTest(t, {
+  source: {
+    server: Server.HTTPS_PRIVATE,
+    treatAsPublic: true,
+  },
+  target: { server: Server.HTTPS_PRIVATE },
+  expected: TestResult.FAILURE,
+}), "treat-as-public to private: failure.");
+
+promise_test(t => serviceWorkerScriptTest(t, {
+  source: { server: Server.HTTPS_PUBLIC },
+  target: { server: Server.HTTPS_PUBLIC },
+  expected: TestResult.SUCCESS,
+}), "public to public: success.");
