543a2721a3c2c91a73c7b8743f161fe5d8af6b01
Roberto Carrillo <robertocn@chromium.org>
2022-01-04 23:49:28 +0000

    Revert "Remove GLSurfaceEGLReadbackX11"
    This reverts commit 208a1ee1e92ea219be5003addccb5da46474c216.
    
    Reason for revert: Speculative revert, suspected of causing crash in linux-chromeos-dbg. See https://bugs.chromium.org/p/chromium/issues/detail?id=1284454
    
    Original change's description:
    > Remove GLSurfaceEGLReadbackX11
    >
    > GLSurfaceEGLReadbackX11 is used with SwANGLE & SwiftShader which had
    > a bug related to native view egl surface, and it has been fixed. So
    > GLSurfaceEGLReadbackX11 can be removed and use the default native view
    > egl surface which can avoid one extra pixels copy per frame.
    >
    > See crbug.com/swiftshader/166 & b/206138984
    >
    > Bug: None
    > Change-Id: If1c6becc838f6b9938c69bc2b758acbdc4788c76
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3349417
    > Auto-Submit: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#955267}
    
    Bug: None
    Change-Id: I9cd7b7ace0b3eb64e53bf0fb6afef1e923b0bf9f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367161
    Auto-Submit: Roberto Carrillo <robertocn@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Roberto Carrillo <robertocn@chromium.org>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955441}

---
+++ ui/ozone/platform/x11/gl_surface_egl_readback_x11.h
@@ -0,0 +1,39 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_X11_GL_SURFACE_EGL_READBACK_X11_H_
+#define UI_OZONE_PLATFORM_X11_GL_SURFACE_EGL_READBACK_X11_H_
+
+#include "ui/gfx/x/xproto.h"
+#include "ui/ozone/common/gl_surface_egl_readback.h"
+
+namespace ui {
+
+// GLSurface implementation that copies pixels from readback to an XWindow.
+class GLSurfaceEglReadbackX11 : public GLSurfaceEglReadback {
+ public:
+  explicit GLSurfaceEglReadbackX11(gfx::AcceleratedWidget window);
+
+  GLSurfaceEglReadbackX11(const GLSurfaceEglReadbackX11&) = delete;
+  GLSurfaceEglReadbackX11& operator=(const GLSurfaceEglReadbackX11&) = delete;
+
+  // gl::GLSurface:
+  bool Initialize(gl::GLSurfaceFormat format) override;
+  void Destroy() override;
+
+ private:
+  ~GLSurfaceEglReadbackX11() override;
+
+  // gl::GLSurfaceEglReadback:
+  bool HandlePixels(uint8_t* pixels) override;
+
+  const x11::Window window_;
+  x11::Connection* const connection_;
+  x11::GraphicsContext window_graphics_context_{};
+  x11::VisualId visual_{};
+};
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_X11_GL_SURFACE_EGL_READBACK_X11_H_
