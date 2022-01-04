6172c23c4896f0f7c1fea02d6ec62d045868950d
Yao Li <yaohuali@google.com>
2022-01-04 20:02:19 +0000

    ash: Create new test target ash_components_unittests and CI
    ash_unittests already has too many tests inside.
    This CL:
    - Makes //ash/components:unit_test a runnable test target
    - Moves unit tests under //ash/components/arc to the new target
    - Adds CI to make the new test target run
    
    Bug: b:206686476
    Test: New test target runs/passes locally and on CI
    autoninja -C out/Default/ ash/components:ash_components_unittests &&
    testing/xvfb.py ./out/Default/ash_components_unittests
    
    Change-Id: I5cffe1b16aeef37af6b36d10303fc9fb155982e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330797
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Amy Huang <akhuang@google.com>
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Yao Li <yaohuali@google.com>
    Cr-Commit-Position: refs/heads/main@{#955306}

---
+++ ash/components/test/run_all_unittests.cc
@@ -0,0 +1,27 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/components/test/ash_components_test_suite.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "base/threading/thread.h"
+#include "mojo/core/embedder/embedder.h"
+#include "mojo/core/embedder/scoped_ipc_support.h"
+
+int main(int argc, char** argv) {
+  ash::AshComponentsTestSuite test_suite(argc, argv);
+
+  mojo::core::Init();
+  // The IPC thread is necessary for the window service.
+  base::Thread ipc_thread("IPC thread");
+  ipc_thread.StartWithOptions(
+      base::Thread::Options(base::MessagePumpType::IO, 0));
+  mojo::core::ScopedIPCSupport ipc_support(
+      ipc_thread.task_runner(),
+      mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
+
+  return base::LaunchUnitTests(argc, argv,
+                               base::BindOnce(&ash::AshComponentsTestSuite::Run,
+                                              base::Unretained(&test_suite)));
+}
