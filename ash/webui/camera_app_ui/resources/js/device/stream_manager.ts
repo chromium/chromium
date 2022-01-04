79e18fb4b856396d85766680eb452dc6cfd11ecd
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-04 10:00:05 +0000

    CCA: Migrate device/stream_manager.js to TypeScript
    Bug: b:172340451
    Test: tsc compile
    Change-Id: Ifa1cd0f955723172f9862f44dc86aa5fd20edaad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314415
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955087}

--- ash/webui/camera_app_ui/resources/js/device/stream_manager.ts
+++ ash/webui/camera_app_ui/resources/js/device/stream_manager.ts
@@ -8,41 +8,36 @@ import {I18nString} from '../i18n_string.js';
 import * as loadTimeData from '../models/load_time_data.js';
 import {DeviceOperator} from '../mojo/device_operator.js';
 import * as toast from '../toast.js';
-// eslint-disable-next-line no-unused-vars
 import {ErrorLevel, ErrorType, Facing, VideoConfig} from '../type.js';
 import {WaitableEvent} from '../waitable_event.js';
 
 import {Camera3DeviceInfo} from './camera3_device_info.js';
 import {
-  StreamConstraints,  // eslint-disable-line no-unused-vars
+  StreamConstraints,
   toMediaStreamConstraints,
 } from './stream_constraints.js';
 
 /**
  * The singleton instance of StreamManager. Initialized by the first
  * invocation of getInstance().
- * @type {?StreamManager}
  */
-let instance = null;
+let instance: StreamManager|null = null;
 
 /**
  * Device information includs MediaDeviceInfo and Camera3DeviceInfo.
- * @typedef {{
- *   v1Info: !MediaDeviceInfo,
- *   v3Info: ?Camera3DeviceInfo
- * }}
  */
-export let DeviceInfo;
+export interface DeviceInfo {
+  v1Info: MediaDeviceInfo;
+  v3Info: Camera3DeviceInfo|null;
+}
 
 /**
  * Real and virtual device mapping.
- * @typedef {{
- *   realId: string,
- *   virtualId: string
- * }}
  */
-// eslint-disable-next-line no-unused-vars
-let VirtualMap;
+interface VirtualMap {
+  realId: string;
+  virtualId: string;
+}
 
 /**
  * Monitors device change and provides different listener callbacks for
@@ -50,67 +45,51 @@ let VirtualMap;
  */
 export class StreamManager {
   /**
-   * @private
+   * MediaDeviceInfo of all available video devices.
    */
-  constructor() {
-    /**
-     * MediaDeviceInfo of all available video devices.
-     * @type {?Promise<!Array<!MediaDeviceInfo>>}
-     * @private
-     */
-    this.devicesInfo_ = null;
+  private devicesInfo: Promise<MediaDeviceInfo[]>|null = null;
 
-    /**
-     * Camera3DeviceInfo of all available video devices. Is null on HALv1 device
-     * without mojo api support.
-     * @type {?Promise<?Array<!DeviceInfo>>}
-     * @private
-     */
-    this.camera3DevicesInfo_ = null;
+  /**
+   * Camera3DeviceInfo of all available video devices. Is null on HALv1 device
+   * without mojo api support.
+   */
+  private camera3DevicesInfo: Promise<DeviceInfo[]|null>|null = null;
 
-    /**
-     * Listeners for real device change event.
-     * @type {!Array<function(!Array<!DeviceInfo>): !Promise>}
-     * @private
-     */
-    this.realListeners_ = [];
+  /**
+   * Listeners for real device change event.
+   */
+  private readonly realListeners:
+      Array<(devices: DeviceInfo[]) => Promise<void>> = [];
 
-    /**
-     * Latest result of Camera3DeviceInfo of all real video devices.
-     * @type {!Array<!DeviceInfo>}
-     * @private
-     */
-    this.realDevices_ = [];
+  /**
+   * Latest result of Camera3DeviceInfo of all real video devices.
+   */
+  private realDevices: DeviceInfo[] = [];
 
-    /**
-     * real device id and corresponding virtual devices id mapping and it is
-     * only available on HALv3.
-     * @type {?VirtualMap}
-     * @private
-     */
-    this.virtualMap_ = null;
+  /**
+   * real device id and corresponding virtual devices id mapping and it is
+   * only available on HALv3.
+   */
+  private virtualMap: VirtualMap|null = null;
 
-    /**
-     * Signal it to indicate that the virtual device is ready.
-     * @type {?WaitableEvent<string>}
-     * @private
-     */
-    this.waitVirtual_ = null;
+  /**
+   * Signal it to indicate that the virtual device is ready.
+   */
+  private waitVirtual: WaitableEvent<string>|null = null;
 
-    /**
-     * Signal to indicate that the virtual device is successfully removed.
-     * @type {?WaitableEvent}
-     * @private
-     */
-    this.waitVirtualRemoved_ = null;
+  /**
+   * Signal to indicate that the virtual device is successfully removed.
+   */
+  private waitVirtualRemoved: WaitableEvent|null = null;
 
-    /**
-     * Filter out lagging 720p on grunt. See https://crbug.com/1122852.
-     * @const {!Promise<function(!VideoConfig): boolean>}
-     * @private
-     */
-    this.videoConfigFilter_ = (async () => {
-      const board = await loadTimeData.getBoard();
+  /**
+   * Filter out lagging 720p on grunt. See https://crbug.com/1122852.
+   */
+  private videoConfigFilter: (config: VideoConfig) => boolean;
+
+  private constructor() {
+    this.videoConfigFilter = (() => {
+      const board = loadTimeData.getBoard();
       return board === 'grunt' ? ({height}) => height < 720 : () => true;
     })();
 
@@ -121,9 +100,9 @@ export class StreamManager {
   /**
    * Creates a new instance of StreamManager if it is not set. Returns the
    *     exist instance.
-   * @return {!StreamManager} The singleton instance.
+   * @return The singleton instance.
    */
-  static getInstance() {
+  static getInstance(): StreamManager {
     if (instance === null) {
       instance = new StreamManager();
     }
@@ -133,23 +112,22 @@ export class StreamManager {
   /**
    * Registers listener to be called when state of available real devices
    * changes.
-   * @param {function(!Array<!DeviceInfo>): !Promise} listener
    */
-  addRealDeviceChangeListener(listener) {
-    this.realListeners_.push(listener);
+  addRealDeviceChangeListener(
+      listener: (devices: DeviceInfo[]) => Promise<void>): void {
+    this.realListeners.push(listener);
   }
 
   /**
    * Creates extra stream according to the constraints.
-   * @param {!StreamConstraints} constraints
-   * @return {!Promise<!MediaStream>}
    */
-  async openCaptureStream(constraints) {
+  async openCaptureStream(constraints: StreamConstraints):
+      Promise<MediaStream> {
     const realDeviceId = constraints.deviceId;
     if (await DeviceOperator.isSupported()) {
       try {
         await this.setMultipleStreamsEnabled(realDeviceId, true);
-        constraints.deviceId = this.virtualMap_.virtualId;
+        constraints.deviceId = this.virtualMap.virtualId;
       } catch (e) {
         reportError(ErrorType.MULTIPLE_STREAMS_FAILURE, ErrorLevel.ERROR, e);
       }
@@ -162,18 +140,16 @@ export class StreamManager {
 
   /**
    * Closes the given capture stream.
-   * @param {!MediaStream} captureStream
-   * @return {!Promise}
    */
-  async closeCaptureStream(captureStream) {
+  async closeCaptureStream(captureStream: MediaStream): Promise<void> {
     captureStream.getVideoTracks()[0].stop();
     const deviceOperator = await DeviceOperator.getInstance();
     if (deviceOperator !== null) {
       // We need to cache |virtualId| first since it will be wiped out after
       // disabling multi-stream.
-      const virtualId = this.virtualMap_.virtualId;
+      const virtualId = this.virtualMap.virtualId;
       try {
-        await this.setMultipleStreamsEnabled(this.virtualMap_.realId, false);
+        await this.setMultipleStreamsEnabled(this.virtualMap.realId, false);
       } catch (e) {
         reportError(ErrorType.MULTIPLE_STREAMS_FAILURE, ErrorLevel.ERROR, e);
       }
@@ -184,24 +160,22 @@ export class StreamManager {
   /**
    * Handling function for device changing.
    */
-  async deviceUpdate() {
-    const devices = await this.doDeviceInfoUpdate_();
+  async deviceUpdate(): Promise<void> {
+    const devices = await this.doDeviceInfoUpdate();
     if (devices === null) {
       return;
     }
-    await this.doDeviceNotify_(devices);
+    await this.doDeviceNotify(devices);
   }
 
   /**
    * Gets devices information via mojo IPC.
-   * @return {?Promise<?Array<!DeviceInfo>>}
-   * @private
    */
-  async doDeviceInfoUpdate_() {
-    this.devicesInfo_ = this.enumerateDevices_();
-    this.camera3DevicesInfo_ = this.queryMojoDevicesInfo_();
+  private async doDeviceInfoUpdate(): Promise<DeviceInfo[]|null>|null {
+    this.devicesInfo = this.enumerateDevices();
+    this.camera3DevicesInfo = this.queryMojoDevicesInfo();
     try {
-      return await this.camera3DevicesInfo_;
+      return await this.camera3DevicesInfo;
     } catch (e) {
       reportError(ErrorType.DEVICE_INFO_UPDATE_FAILURE, ErrorLevel.ERROR, e);
     }
@@ -211,10 +185,8 @@ export class StreamManager {
   /**
    * Notifies device changes to listeners and create a mapping for real and
    * virtual device.
-   * @param {!Array<!DeviceInfo>} devices
-   * @private
    */
-  async doDeviceNotify_(devices) {
+  private async doDeviceNotify(devices: DeviceInfo[]) {
     const isVirtual = (d) => d.v3Info !== null &&
         (d.v3Info.facing === Facing.VIRTUAL_USER ||
          d.v3Info.facing === Facing.VIRTUAL_ENV ||
@@ -224,49 +196,44 @@ export class StreamManager {
     // We currently only support one virtual device.
     assert(virtualDevices.length <= 1);
 
-    if (virtualDevices.length === 1 && this.waitVirtual_ !== null) {
-      this.waitVirtual_.signal(virtualDevices[0].v1Info.deviceId);
-      this.waitVirtual_ = null;
+    if (virtualDevices.length === 1 && this.waitVirtual !== null) {
+      this.waitVirtual.signal(virtualDevices[0].v1Info.deviceId);
+      this.waitVirtual = null;
     }
 
-    if (virtualDevices.length === 0 && this.waitVirtualRemoved_ !== null) {
-      this.waitVirtualRemoved_.signal();
-      this.waitVirtualRemoved_ = null;
+    if (virtualDevices.length === 0 && this.waitVirtualRemoved !== null) {
+      this.waitVirtualRemoved.signal();
+      this.waitVirtualRemoved = null;
     }
 
     let isRealDeviceChange = false;
-    for (const added of this.getDifference_(realDevices, this.realDevices_)) {
+    for (const added of this.getDifference(realDevices, this.realDevices)) {
       toast.speak(I18nString.STATUS_MSG_CAMERA_PLUGGED, added.v1Info.label);
       isRealDeviceChange = true;
     }
-    for (const removed of this.getDifference_(this.realDevices_, realDevices)) {
+    for (const removed of this.getDifference(this.realDevices, realDevices)) {
       toast.speak(I18nString.STATUS_MSG_CAMERA_UNPLUGGED, removed.v1Info.label);
       isRealDeviceChange = true;
     }
     if (isRealDeviceChange) {
-      this.realListeners_.map((l) => l(realDevices));
+      this.realListeners.map((l) => l(realDevices));
     }
-    this.realDevices_ = realDevices;
+    this.realDevices = realDevices;
   }
 
   /**
    * Computes |devices| - |devices2|.
-   * @param {!Array<!DeviceInfo>} devices
-   * @param {!Array<!DeviceInfo>} devices2
-   * @return {!Array<!DeviceInfo>}
    */
-  getDifference_(devices, devices2) {
+  private getDifference(devices: DeviceInfo[], devices2: DeviceInfo[]):
+      DeviceInfo[] {
     const ids = new Set(devices2.map((d) => d.v1Info.deviceId));
     return devices.filter((d) => !ids.has(d.v1Info.deviceId));
   }
 
   /**
    * Enumerates all available devices and gets their MediaDeviceInfo.
-   * @return {!Promise<!Array<!MediaDeviceInfo>>}
-   * @throws {!Error}
-   * @private
    */
-  async enumerateDevices_() {
+  private async enumerateDevices(): Promise<MediaDeviceInfo[]> {
     const devices = (await navigator.mediaDevices.enumerateDevices())
                         .filter((device) => device.kind === 'videoinput');
 
@@ -281,22 +248,19 @@ export class StreamManager {
 
   /**
    * Queries Camera3DeviceInfo of available devices through private mojo API.
-   * @return {!Promise<?Array<!DeviceInfo>>} Camera3DeviceInfo of available
-   *     devices. Maybe null on HALv1 devices without supporting private mojo
-   *     api.
-   * @throws {!Error} Thrown when camera unplugging happens between enumerating
-   *     devices and querying mojo APIs with current device info results.
-   * @private
+   * @return Camera3DeviceInfo of available devices. Maybe null on HALv1
+   *     devices without supporting private mojo api.
+   * @throws Thrown when camera unplugging happens between enumerating devices
+   *     and querying mojo APIs with current device info results.
    */
-  async queryMojoDevicesInfo_() {
-    const deviceInfos = await this.devicesInfo_;
-    const videoConfigFilter = await this.videoConfigFilter_;
+  private async queryMojoDevicesInfo(): Promise<DeviceInfo[]|null> {
+    const deviceInfos = await this.devicesInfo;
     const isV3Supported = await DeviceOperator.isSupported();
     return Promise.all(deviceInfos.map(
         async (d) => ({
           v1Info: d,
           v3Info: isV3Supported ?
-              (await Camera3DeviceInfo.create(d, videoConfigFilter)) :
+              (await Camera3DeviceInfo.create(d, this.videoConfigFilter)) :
               null,
         })));
   }
@@ -305,32 +269,32 @@ export class StreamManager {
    * Enables/Disables multiple streams on target camera device. The extra
    * stream will be reported as virtual video device from
    * navigator.mediaDevices.enumerateDevices().
-   * @param {string} deviceId The id of target camera device.
-   * @param {boolean} enabled True for eanbling multiple streams.
+   * @param deviceId The id of target camera device.
+   * @param enabled True for eanbling multiple streams.
    */
-  async setMultipleStreamsEnabled(deviceId, enabled) {
+  async setMultipleStreamsEnabled(deviceId: string, enabled: boolean):
+      Promise<void> {
     assert(await DeviceOperator.isSupported());
     const deviceOperator = await DeviceOperator.getInstance();
 
     if (enabled) {
-      /** @type {WaitableEvent<string>} */
-      const waitEvent = new WaitableEvent();
-      this.waitVirtual_ = waitEvent;
+      const waitEvent = new WaitableEvent<string>();
+      this.waitVirtual = waitEvent;
 
       await deviceOperator.setMultipleStreamsEnabled(deviceId, enabled);
       await this.deviceUpdate();
 
       const virtualId = await waitEvent.timedWait(3000);
-      this.virtualMap_ = {realId: deviceId, virtualId};
+      this.virtualMap = {realId: deviceId, virtualId};
     } else {
       const waitEvent = new WaitableEvent();
-      this.waitVirtualRemoved_ = waitEvent;
+      this.waitVirtualRemoved = waitEvent;
 
       await deviceOperator.setMultipleStreamsEnabled(deviceId, enabled);
       await this.deviceUpdate();
 
       await waitEvent.timedWait(3000);
-      this.virtualMap_ = null;
+      this.virtualMap = null;
     }
   }
 }
