2a424d6b787a0884955c18d231efb9a1f03748e9
Lijin Shen <lazzzis@google.com>
2022-01-05 00:29:23 +0000

    [Messages] Enable Sync Error messages by default
    Bug: 1246073
    Change-Id: Iaa153a1eed2b8e31fd8d1014bfff7354331b3ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366881
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#955471}

--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -55,7 +55,7 @@ constexpr base::FeatureParam<bool> kMessagesForAndroidSaveCard_UseGPayIcon{
     &kMessagesForAndroidSaveCard, "save_card_message_use_gpay_icon", true};
 
 const base::Feature kMessagesForAndroidSyncError{
-    "MessagesForAndroidSyncError", base::FEATURE_DISABLED_BY_DEFAULT};
+    "MessagesForAndroidSyncError", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMessagesForAndroidUpdatePassword{
     "MessagesForAndroidUpdatePassword", base::FEATURE_DISABLED_BY_DEFAULT};
