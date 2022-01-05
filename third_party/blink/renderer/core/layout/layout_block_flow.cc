65bafff519ff71eeb1d2cae9be3bdc293ac55911
Morten Stenshorne <mstensho@chromium.org>
2022-01-05 07:13:18 +0000

    Always tag anonymous multicol inline wrappers.
    If we don't set this bit, it will fail to act as a containing block for
    out-of-flow positioned descendants (those that are contained by some
    non-atomic inline, CSS-wise), and the OOFs won't be laid out at all,
    because the NG code will fail to find the appropriate ancestor to do so.
    
    Bug: 1282196
    Change-Id: I9b4a767d5b7d749fb35819e59ab0f316460b7754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3359742
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955580}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -4490,6 +4490,21 @@ void LayoutBlockFlow::CreateOrDestroyMultiColumnFlowThreadIfNeeded(
   DCHECK_EQ(flow_thread->Parent(), this);
 
   flow_thread->Populate();
+
+  if (auto* child = DynamicTo<LayoutNGBlockFlow>(flow_thread->FirstChild())) {
+    // Attempt to identify the anonymous inline wrapper that may have been
+    // created, if all multicol children are inline. The child insertion
+    // machinery (invoked above, when adding |flow_thread|) may already have
+    // inserted an anonymous block for other reasons (when the flow thread
+    // temporarily becomes a sibling of the actual DOM children), in which case
+    // we haven't tagged this anonymous wrapper properly. Do it now. This is
+    // important for OOF descendants, as this anonymous wrapper may act as their
+    // containing block, but that will only happen if it is tagged correctly;
+    // see LayoutObject::FindNonAnonymousContainingBlock().
+    if (child->IsAnonymousBlock() && !child->NextSibling())
+      child->SetIsAnonymousNGMulticolInlineWrapper();
+  }
+
   LayoutBlockFlowRareData& rare_data = EnsureRareData();
   DCHECK(!rare_data.multi_column_flow_thread_);
   rare_data.multi_column_flow_thread_ = flow_thread;
