4d244a28d33d386bb589b8cd606299495660a270
Sebastien Lalancette <seblalancette@chromium.org>
2022-01-05 14:10:04 +0000

    [ZT] Define RotateAttestationCredentialJob
    This new job definition will handle attestation credential rotation
    remote commands. The creation of this job is gated behind the Device
    Trust connector Finch flag.
    
    Bug: b:202431075
    Change-Id: Iad18fea4081154ca9061c2277d92cd80848260ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344072
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955692}

---
+++ chrome/browser/enterprise/remote_commands/rotate_attestation_credential_job_unittest.cc
@@ -0,0 +1,152 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/remote_commands/rotate_attestation_credential_job.h"
+
+#include "base/callback.h"
+#include "base/json/json_writer.h"
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "base/test/task_environment.h"
+#include "base/values.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/mock_device_trust_key_manager.h"
+#include "components/enterprise/browser/device_trust/device_trust_key_manager.h"
+#include "components/policy/proto/device_management_backend.pb.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using KeyRotationResult =
+    enterprise_connectors::DeviceTrustKeyManager::KeyRotationResult;
+
+using testing::_;
+using testing::Invoke;
+
+namespace enterprise_commands {
+
+namespace {
+
+constexpr policy::RemoteCommandJob::UniqueIDType kUniqueID = 123456789;
+
+const char kNonceField[] = "nonce";
+const char kNonceValue[] = "some nonce value";
+
+enterprise_management::RemoteCommand CreateCommand() {
+  enterprise_management::RemoteCommand command_proto;
+  command_proto.set_type(
+      enterprise_management::
+          RemoteCommand_Type_BROWSER_ROTATE_ATTESTATION_CREDENTIAL);
+  command_proto.set_command_id(kUniqueID);
+  return command_proto;
+}
+
+std::string GetPayloadWithNonce() {
+  base::Value root(base::Value::Type::DICTIONARY);
+  root.SetStringKey(kNonceField, kNonceValue);
+
+  std::string payload;
+  base::JSONWriter::Write(root, &payload);
+  return payload;
+}
+
+std::string GetEmptyPayload() {
+  return "{}";
+}
+
+}  // namespace
+
+class RotateAttestationCredentialJobTest : public testing::Test {
+ protected:
+  void MockKeyRotationWith(KeyRotationResult result) {
+    EXPECT_CALL(mock_key_manager_, RotateKey(kNonceValue, _))
+        .WillOnce(Invoke(
+            [result](const std::string& nonce,
+                     base::OnceCallback<void(KeyRotationResult)> callback) {
+              std::move(callback).Run(result);
+            }));
+  }
+
+  std::unique_ptr<RotateAttestationCredentialJob> CreateJob(
+      enterprise_management::RemoteCommand command_proto) {
+    auto job =
+        std::make_unique<RotateAttestationCredentialJob>(&mock_key_manager_);
+    EXPECT_TRUE(job->Init(base::TimeTicks::Now(), command_proto,
+                          enterprise_management::SignedData{}));
+    EXPECT_EQ(kUniqueID, job->unique_id());
+    EXPECT_EQ(policy::RemoteCommandJob::NOT_STARTED, job->status());
+
+    return job;
+  }
+
+  base::test::SingleThreadTaskEnvironment task_environment_;
+  testing::StrictMock<enterprise_connectors::test::MockDeviceTrustKeyManager>
+      mock_key_manager_;
+};
+
+// Tests that a job conveys a successful key rotation properly.
+TEST_F(RotateAttestationCredentialJobTest, SuccessRun) {
+  MockKeyRotationWith(KeyRotationResult::SUCCESS);
+
+  auto command_proto = CreateCommand();
+  auto json_payload = GetPayloadWithNonce();
+  command_proto.set_payload(json_payload);
+
+  base::RunLoop run_loop;
+  auto job = CreateJob(std::move(command_proto));
+
+  EXPECT_TRUE(job->Run(base::Time::Now(), base::TimeTicks::Now(),
+                       run_loop.QuitClosure()));
+  run_loop.Run();
+
+  EXPECT_EQ(job->status(), policy::RemoteCommandJob::SUCCEEDED);
+}
+
+// Tests that a job conveys a failed key rotation properly.
+TEST_F(RotateAttestationCredentialJobTest, FailedRun) {
+  MockKeyRotationWith(KeyRotationResult::FAILURE);
+
+  auto command_proto = CreateCommand();
+  auto json_payload = GetPayloadWithNonce();
+  command_proto.set_payload(json_payload);
+
+  base::RunLoop run_loop;
+  auto job = CreateJob(std::move(command_proto));
+
+  EXPECT_TRUE(job->Run(base::Time::Now(), base::TimeTicks::Now(),
+                       run_loop.QuitClosure()));
+  run_loop.Run();
+
+  EXPECT_EQ(job->status(), policy::RemoteCommandJob::FAILED);
+}
+
+// Tests that a job conveys a cancelled key rotation properly.
+TEST_F(RotateAttestationCredentialJobTest, CancelledRun) {
+  MockKeyRotationWith(KeyRotationResult::CANCELLATION);
+
+  auto command_proto = CreateCommand();
+  auto json_payload = GetPayloadWithNonce();
+  command_proto.set_payload(json_payload);
+
+  base::RunLoop run_loop;
+  auto job = CreateJob(std::move(command_proto));
+
+  EXPECT_TRUE(job->Run(base::Time::Now(), base::TimeTicks::Now(),
+                       run_loop.QuitClosure()));
+  run_loop.Run();
+
+  EXPECT_EQ(job->status(), policy::RemoteCommandJob::FAILED);
+}
+
+// Tests that a job handles a bad payload properly.
+TEST_F(RotateAttestationCredentialJobTest, BadPayload) {
+  auto command_proto = CreateCommand();
+  auto json_payload = GetEmptyPayload();
+  command_proto.set_payload(json_payload);
+
+  auto job =
+      std::make_unique<RotateAttestationCredentialJob>(&mock_key_manager_);
+  EXPECT_FALSE(job->Init(base::TimeTicks::Now(), command_proto,
+                         enterprise_management::SignedData{}));
+}
+
+}  // namespace enterprise_commands
