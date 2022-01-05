fb0bebcb4e8401c173044c531fe009ae823d1fe7
Roland Bock <rbock@google.com>
2022-01-05 19:22:19 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=jorgelo@chromium.org
    
    Bug: 1283907
    Change-Id: Iae4632cad0c41c834fcb26a622d13869fa5ee818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366346
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955822}

--- sandbox/linux/integration_tests/bpf_dsl_seccomp_unittest.cc
+++ sandbox/linux/integration_tests/bpf_dsl_seccomp_unittest.cc
@@ -801,7 +801,7 @@ ResultExpr SimpleCondTestPolicy::EvaluateSyscall(int sysno) const {
 #if defined(__NR_open)
     case __NR_open:
       flags_argument_position = 1;
-      FALLTHROUGH;
+      [[fallthrough]];
 #endif
     case __NR_openat: {  // open can be a wrapper for openat(2).
       if (sysno == __NR_openat)
