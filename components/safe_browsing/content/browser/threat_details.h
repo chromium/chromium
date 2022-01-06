2481bf37971a89c20e033379e08d252345dd491c
Xinghui Lu <xinghuilu@chromium.org>
2022-01-06 08:36:45 +0000

    Use RFH global id to ensure the RFH is valid.
    Observing via RenderFrameDeleted and RenderFrameHostChanged is not
    sufficient for validating the RFH is still valid, because the frames
    can belong to inner WebContents. As suggested in
    https://crrev.com/c/2449389, storing a GlobalFrameRoutingId is the
    preferred method of keeping a reference to a RFH.
    
    Bug: 1284367
    Change-Id: I3afb40e394d6e2e7fd19b2704e0dd68fa23c7bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367466
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956061}

--- components/safe_browsing/content/browser/threat_details.h
+++ components/safe_browsing/content/browser/threat_details.h
@@ -24,6 +24,7 @@
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "components/security_interstitials/core/unsafe_resource.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
@@ -172,7 +173,7 @@ class ThreatDetails : public content::WebContentsObserver {
 
   void OnReceivedThreatDOMDetails(
       mojo::Remote<mojom::ThreatReporter> threat_reporter,
-      content::RenderFrameHost* sender,
+      content::GlobalRenderFrameHostId sender_id,
       std::vector<mojom::ThreatDOMDetailsNodePtr> params);
 
   void AddRedirectUrlList(const std::vector<GURL>& urls);
