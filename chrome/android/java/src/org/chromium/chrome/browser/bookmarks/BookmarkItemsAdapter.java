5a372df3b482044d284380d3417880609621d5c9
Brandon Wylie <wylieb@chromium.org>
2022-01-06 21:45:13 +0000

    [Read Later] Fix crash when reordering reading list items
    Bug: 1282143
    Change-Id: I9d4d168b6308c49ae9a633e4046a12618aca75b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368921
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956249}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
@@ -451,7 +451,7 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
      */
     void moveUpOne(BookmarkId bookmarkId) {
         int pos = getPositionForBookmark(bookmarkId);
-        assert isOrderable(getItemByPosition(pos));
+        assert isReorderable(getItemByPosition(pos));
         mElements.remove(pos);
         mElements.add(pos - 1,
                 BookmarkListEntry.createBookmarkEntry(
@@ -465,7 +465,7 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
      */
     void moveDownOne(BookmarkId bookmarkId) {
         int pos = getPositionForBookmark(bookmarkId);
-        assert isOrderable(getItemByPosition(pos));
+        assert isReorderable(getItemByPosition(pos));
         mElements.remove(pos);
         mElements.add(pos + 1,
                 BookmarkListEntry.createBookmarkEntry(
@@ -593,9 +593,9 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
         return endIndex;
     }
 
-    private boolean isOrderable(BookmarkListEntry entry) {
+    private boolean isReorderable(BookmarkListEntry entry) {
         return entry != null && entry.getBookmarkItem() != null
-                && entry.getBookmarkItem().isMovable();
+                && entry.getBookmarkItem().isReorderable();
     }
 
     @Override
@@ -608,7 +608,7 @@ class BookmarkItemsAdapter extends DragReorderableListAdapter<BookmarkListEntry>
     @Override
     @VisibleForTesting
     public boolean isPassivelyDraggable(ViewHolder viewHolder) {
-        return isOrderable(getItemByHolder(viewHolder));
+        return isReorderable(getItemByHolder(viewHolder));
     }
 
     @VisibleForTesting
