4248d4be19f12383548d35f962944f060570f87f
Sushanth Rajasankar <sushraja@microsoft.com>
2022-01-06 23:44:24 +0000

    Prevent frame_rate_decider from picking rates < minimum requested
    This change fixes an issue where the framerate decider can decide to
    play 60Hz videos on 100Hz monitors at 50Hz. Granted this issue only
    repros when there are multiple videos on a page such that
    FrameRateDecider::ShouldToggleFrameInterval returns true, the issue
    exists nevertheless.
    
    The problem stems from the fact that on a 100hz monitor both 50Hz and
    100Hz are supported intervals. Since 60hz is closer to 50, the lower
    framerate gets chosen. The fix is to ensure that only a framerate
    that is greater than or equal to the require frame rate is chosen
    by the framerate decider.
    
    Change-Id: I4fcf78153efe2728d7752791b11363f620c04b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368627
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Sushanth Rajasankar <Sushraja@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#956292}

--- components/viz/service/display/frame_rate_decider.cc
+++ components/viz/service/display/frame_rate_decider.cc
@@ -212,11 +212,15 @@ void FrameRateDecider::UpdatePreferredFrameIntervalIfNeeded() {
   // ideal refresh rate.
   base::TimeDelta new_preferred_interval = UnspecifiedFrameInterval();
   if (*min_frame_sink_interval != BeginFrameArgs::MinInterval()) {
+    base::TimeDelta min_delta = base::TimeDelta::Max();
     for (auto supported_interval : supported_intervals_) {
-      // Pick the display interval which is closest to the preferred interval.
-      if ((*min_frame_sink_interval - supported_interval).magnitude() <
-          (*min_frame_sink_interval - new_preferred_interval).magnitude()) {
+      // Pick the display interval which is closest to the preferred interval
+      // and less than or equal to the min_frame_sink_interval.
+      base::TimeDelta delta = (*min_frame_sink_interval - supported_interval);
+      if (AreAlmostEqual(*min_frame_sink_interval, supported_interval) ||
+          (delta.is_positive() && delta < min_delta)) {
         new_preferred_interval = supported_interval;
+        min_delta = delta.magnitude();
       }
     }
   }
