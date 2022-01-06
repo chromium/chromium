ddb32ed4a84c642d7554a192578905e9d14f2f76
Jérôme Lebel <jlebel@chromium.org>
2022-01-06 20:19:00 +0000

    [iOS] Remove dataType in Google service settings
    Data type is not used anymore.
    
    This patch needs to be landed after crrev.com/c/3361386 to avoid
    conflict.
    
    Change-Id: I0a0e249d5b6d618fc14943f7768950fd3668c1d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3369365
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956220}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
@@ -237,13 +237,12 @@ bool GetStatusForSigninPolicy() {
                         textStringID:
                             IDS_IOS_GOOGLE_SERVICES_SETTINGS_ALLOW_SIGNIN_TEXT
                       detailStringID:
-                          IDS_IOS_GOOGLE_SERVICES_SETTINGS_ALLOW_SIGNIN_DETAIL
-                            dataType:0];
+                          IDS_IOS_GOOGLE_SERVICES_SETTINGS_ALLOW_SIGNIN_DETAIL];
   }
   // Disables "Allow Chrome Sign-in" switch with a disclosure that the
   // setting has been disabled by the organization.
   return [self
-      TableViewInfoButtonItemType:AllowChromeSigninItemType
+      tableViewInfoButtonItemType:AllowChromeSigninItemType
                      textStringID:
                          IDS_IOS_GOOGLE_SERVICES_SETTINGS_ALLOW_SIGNIN_TEXT
                    detailStringID:
@@ -358,7 +357,7 @@ bool GetStatusForSigninPolicy() {
     if (self.userPrefService->IsManagedPreference(
             prefs::kSearchSuggestEnabled)) {
       TableViewInfoButtonItem* autocompleteItem = [self
-          TableViewInfoButtonItemType:AutocompleteSearchesAndURLsManagedItemType
+          tableViewInfoButtonItemType:AutocompleteSearchesAndURLsManagedItemType
                          textStringID:
                              IDS_IOS_GOOGLE_SERVICES_SETTINGS_AUTOCOMPLETE_SEARCHES_AND_URLS_TEXT
                        detailStringID:
@@ -372,14 +371,13 @@ bool GetStatusForSigninPolicy() {
                     textStringID:
                         IDS_IOS_GOOGLE_SERVICES_SETTINGS_AUTOCOMPLETE_SEARCHES_AND_URLS_TEXT
                   detailStringID:
-                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_AUTOCOMPLETE_SEARCHES_AND_URLS_DETAIL
-                        dataType:0];
+                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_AUTOCOMPLETE_SEARCHES_AND_URLS_DETAIL];
       [items addObject:autocompleteItem];
     }
     if (self.userPrefService->IsManagedPreference(
             prefs::kSafeBrowsingEnabled)) {
       TableViewInfoButtonItem* safeBrowsingManagedItem = [self
-          TableViewInfoButtonItemType:AutocompleteSearchesAndURLsManagedItemType
+          tableViewInfoButtonItemType:AutocompleteSearchesAndURLsManagedItemType
                          textStringID:
                              IDS_IOS_GOOGLE_SERVICES_SETTINGS_SAFE_BROWSING_TEXT
                        detailStringID:
@@ -393,8 +391,7 @@ bool GetStatusForSigninPolicy() {
                     textStringID:
                         IDS_IOS_GOOGLE_SERVICES_SETTINGS_SAFE_BROWSING_TEXT
                   detailStringID:
-                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_SAFE_BROWSING_DETAIL
-                        dataType:0];
+                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_SAFE_BROWSING_DETAIL];
       safeBrowsingItem.accessibilityIdentifier =
           kSafeBrowsingItemAccessibilityIdentifier;
       [items addObject:safeBrowsingItem];
@@ -403,7 +400,7 @@ bool GetStatusForSigninPolicy() {
     if (self.localPrefService->IsManagedPreference(
             metrics::prefs::kMetricsReportingEnabled)) {
       TableViewInfoButtonItem* improveChromeItem = [self
-          TableViewInfoButtonItemType:ImproveChromeManagedItemType
+          tableViewInfoButtonItemType:ImproveChromeManagedItemType
                          textStringID:
                              IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_TEXT
                        detailStringID:
@@ -417,14 +414,13 @@ bool GetStatusForSigninPolicy() {
                     textStringID:
                         IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_TEXT
                   detailStringID:
-                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_DETAIL
-                        dataType:0];
+                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_IMPROVE_CHROME_DETAIL];
       [items addObject:improveChromeItem];
     }
     if (self.userPrefService->IsManagedPreference(
             unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled)) {
       TableViewInfoButtonItem* betterSearchAndBrowsingItem = [self
-          TableViewInfoButtonItemType:BetterSearchAndBrowsingManagedItemType
+          tableViewInfoButtonItemType:BetterSearchAndBrowsingManagedItemType
                          textStringID:
                              IDS_IOS_GOOGLE_SERVICES_SETTINGS_BETTER_SEARCH_AND_BROWSING_TEXT
                        detailStringID:
@@ -440,8 +436,7 @@ bool GetStatusForSigninPolicy() {
                     textStringID:
                         IDS_IOS_GOOGLE_SERVICES_SETTINGS_BETTER_SEARCH_AND_BROWSING_TEXT
                   detailStringID:
-                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_BETTER_SEARCH_AND_BROWSING_DETAIL
-                        dataType:0];
+                      IDS_IOS_GOOGLE_SERVICES_SETTINGS_BETTER_SEARCH_AND_BROWSING_DETAIL];
       betterSearchAndBrowsingItem.accessibilityIdentifier =
           kBetterSearchAndBrowsingItemAccessibilityID;
       [items addObject:betterSearchAndBrowsingItem];
@@ -450,7 +445,7 @@ bool GetStatusForSigninPolicy() {
       if (self.userPrefService->IsManagedPreference(
               prefs::kTrackPricesOnTabsEnabled)) {
         TableViewInfoButtonItem* trackPricesOnTabsItem = [self
-            TableViewInfoButtonItemType:TrackPricesOnTabsItemType
+            tableViewInfoButtonItemType:TrackPricesOnTabsItemType
                            textStringID:IDS_IOS_TRACK_PRICES_ON_TABS
                          detailStringID:IDS_IOS_TRACK_PRICES_ON_TABS_DESCRIPTION
                                  status:self.trackPricesOnTabsPreference
@@ -462,8 +457,7 @@ bool GetStatusForSigninPolicy() {
         SyncSwitchItem* trackPricesOnTabsItem = [self
             switchItemWithItemType:TrackPricesOnTabsItemType
                       textStringID:IDS_IOS_TRACK_PRICES_ON_TABS
-                    detailStringID:IDS_IOS_TRACK_PRICES_ON_TABS_DESCRIPTION
-                          dataType:0];
+                    detailStringID:IDS_IOS_TRACK_PRICES_ON_TABS_DESCRIPTION];
         trackPricesOnTabsItem.accessibilityIdentifier =
             kTrackPricesOnTabsItemAccessibilityID;
         [items addObject:trackPricesOnTabsItem];
@@ -492,21 +486,20 @@ bool GetStatusForSigninPolicy() {
 
 #pragma mark - Private
 
-// Creates a SyncSwitchItem instance.
+// Creates an item with a switch toggle.
 - (SyncSwitchItem*)switchItemWithItemType:(NSInteger)itemType
                              textStringID:(int)textStringID
-                           detailStringID:(int)detailStringID
-                                 dataType:(NSInteger)dataType {
+                           detailStringID:(int)detailStringID {
   SyncSwitchItem* switchItem = [[SyncSwitchItem alloc] initWithType:itemType];
   switchItem.text = GetNSString(textStringID);
   if (detailStringID)
     switchItem.detailText = GetNSString(detailStringID);
-  switchItem.dataType = dataType;
   return switchItem;
 }
 
-// Create a TableViewInfoButtonItem instance.
-- (TableViewInfoButtonItem*)TableViewInfoButtonItemType:(NSInteger)itemType
+// Create a TableViewInfoButtonItem instance used for items that the user is
+// not allowed to switch on or off (enterprise reason for example).
+- (TableViewInfoButtonItem*)tableViewInfoButtonItemType:(NSInteger)itemType
                                            textStringID:(int)textStringID
                                          detailStringID:(int)detailStringID
                                                  status:(BOOL)status
