e8431a165091c4c10eb80123e6870e794a7d7170
Titouan Rigoudy <titouan@chromium.org>
2022-01-07 10:01:04 +0000

    [Private Network Access] Test service worker update fetches.
    These do not currently apply PNA checks, hence the failures observed:
    no preflights are sent for the update fetch.
    
    Bug: chromium:1239551
    Change-Id: I01bef8129978bc5327286726e9746ae3c82048a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367628
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956446}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/service-worker.https.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/service-worker.https.window.js
@@ -5,21 +5,23 @@
 //
 // These tests check that initial `ServiceWorker` script fetches are subject to
 // Private Network Access checks, just like a regular `fetch()`.
+//
 // See also: worker.https.window.js
 
+// Results that may be expected in tests.
 const TestResult = {
   SUCCESS: {
-    loaded: true,
-    unregistered: true,
+    register: { loaded: true },
+    unregister: { unregistered: true },
   },
   FAILURE: {
-    loaded: false,
-    error: "TypeError",
+    register: { loaded: false, error: "TypeError" },
+    unregister: { unregistered: false, error: "no registration" },
   },
 };
 
-async function serviceWorkerScriptTest(t, { source, target, expected }) {
-  const sourceUrl = resolveUrl("resources/service-worker-fetcher.html",
+async function makeTest(t, { source, target, expected }) {
+  const sourceUrl = resolveUrl("resources/service-worker-bridge.html",
                                sourceResolveOptions(source));
 
   const targetUrl =
@@ -27,23 +29,37 @@ async function serviceWorkerScriptTest(t, { source, target, expected }) {
   targetUrl.searchParams.append("body", "undefined");
   targetUrl.searchParams.append("mime-type", "application/javascript");
 
+  const scope = resolveUrl(`resources/${token()}`, {...target.server}).href;
+
   const iframe = await appendIframe(t, document, sourceUrl);
-  const reply = futureMessage();
 
-  const message = {
-    url: targetUrl.href,
-    uuid: token(),
-  };
-  iframe.contentWindow.postMessage(message, "*");
+  {
+    const reply = futureMessage();
+    const message = {
+      action: "register",
+      url: targetUrl.href,
+      options: { scope },
+    };
+    iframe.contentWindow.postMessage(message, "*");
 
-  const { error, loaded, unregistered } = await reply;
+    const { error, loaded } = await reply;
 
-  assert_equals(error, expected.error, "error");
-  assert_equals(loaded, expected.loaded, "response loaded");
-  assert_equals(unregistered, expected.unregistered, "worker unregistered");
+    assert_equals(error, expected.register.error, "register error");
+    assert_equals(loaded, expected.register.loaded, "response loaded");
+  }
+
+  {
+    const reply = futureMessage();
+    iframe.contentWindow.postMessage({ action: "unregister", scope }, "*");
+
+    const { error, unregistered } = await reply;
+    assert_equals(error, expected.unregister.error, "unregister error");
+    assert_equals(
+        unregistered, expected.unregister.unregistered, "worker unregistered");
+  }
 }
 
-promise_test(t => serviceWorkerScriptTest(t, {
+promise_test(t => makeTest(t, {
   source: {
     server: Server.HTTPS_LOCAL,
     treatAsPublic: true,
@@ -52,7 +68,7 @@ promise_test(t => serviceWorkerScriptTest(t, {
   expected: TestResult.FAILURE,
 }), "treat-as-public to local: failed preflight.");
 
-promise_test(t => serviceWorkerScriptTest(t, {
+promise_test(t => makeTest(t, {
   source: {
     server: Server.HTTPS_LOCAL,
     treatAsPublic: true,
@@ -64,7 +80,7 @@ promise_test(t => serviceWorkerScriptTest(t, {
   expected: TestResult.SUCCESS,
 }), "treat-as-public to local: success.");
 
-promise_test(t => serviceWorkerScriptTest(t, {
+promise_test(t => makeTest(t, {
   source: {
     server: Server.HTTPS_PRIVATE,
     treatAsPublic: true,
@@ -73,7 +89,7 @@ promise_test(t => serviceWorkerScriptTest(t, {
   expected: TestResult.FAILURE,
 }), "treat-as-public to private: failed preflight.");
 
-promise_test(t => serviceWorkerScriptTest(t, {
+promise_test(t => makeTest(t, {
   source: {
     server: Server.HTTPS_PRIVATE,
     treatAsPublic: true,
@@ -85,7 +101,7 @@ promise_test(t => serviceWorkerScriptTest(t, {
   expected: TestResult.SUCCESS,
 }), "treat-as-public to private: success.");
 
-promise_test(t => serviceWorkerScriptTest(t, {
+promise_test(t => makeTest(t, {
   source: { server: Server.HTTPS_PUBLIC },
   target: { server: Server.HTTPS_PUBLIC },
   expected: TestResult.SUCCESS,
