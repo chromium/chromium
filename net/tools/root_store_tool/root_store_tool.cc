c93e561dfed48cc1b783da31d895c999ea645c30
Hubert Chao <hchao@chromium.org>
2022-01-10 18:54:34 +0000

    Merge ev_store_tool and root_store_tool to use the same code gen tool, with different output options.
    Bug: 1271177
    Change-Id: Iec158ddf36078cdd5df791efe365d712a28b596a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373407
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957123}

--- net/tools/root_store_tool/root_store_tool.cc
+++ net/tools/root_store_tool/root_store_tool.cc
@@ -13,12 +13,14 @@
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "crypto/openssl_util.h"
+#include "crypto/sha2.h"
 #include "net/tools/root_store_tool/root_store.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/boringssl/src/include/openssl/bio.h"
@@ -128,6 +130,124 @@ void AppendMakefilePath(std::string* out, const base::FilePath& path) {
   out->append(path_str);
 }
 
+// Returns true if file was correctly written, false otherwise.
+bool WriteRootCppFile(const RootStore& root_store,
+                      const base::FilePath cpp_path) {
+  // Root store should have at least one trust anchors.
+  CHECK_GT(root_store.trust_anchors_size(), 0);
+
+  std::string string_to_write =
+      "// This file is auto-generated, DO NOT EDIT.\n\n"
+      "const ChromeRootCertInfo kChromeRootCertList[] = {\n";
+
+  for (auto& anchor : root_store.trust_anchors()) {
+    // Every trust anchor at this point should have a DER.
+    CHECK(!anchor.der().empty());
+    std::string der = anchor.der();
+
+    // Begin struct. Assumed type of ChromeRootCertInfo:
+    //
+    // struct {
+    //   base::span<const uint8_t> der;
+    // };
+    string_to_write += "    {{{";
+
+    // Convert each character to hex representation, escaped.
+    for (auto c : der) {
+      base::StringAppendF(&string_to_write, "0x%02xu,",
+                          static_cast<uint8_t>(c));
+    }
+
+    // End struct
+    string_to_write += "}}},\n";
+  }
+  string_to_write += "};";
+  if (!base::WriteFile(cpp_path, string_to_write)) {
+    return false;
+  }
+  return true;
+}
+
+// Returns true if file was correctly written, false otherwise.
+bool WriteEvCppFile(const RootStore& root_store,
+                    const base::FilePath cpp_path) {
+  // There should be at least one EV root.
+  CHECK_GT(root_store.trust_anchors_size(), 0);
+
+  std::string string_to_write =
+      "// This file is auto-generated, DO NOT EDIT.\n\n"
+      "static const EVMetadata kEvRootCaMetadata[] = {\n";
+
+  for (auto& anchor : root_store.trust_anchors()) {
+    // Every trust anchor at this point should have a DER.
+    CHECK(!anchor.der().empty());
+
+    std::string sha256_hash = crypto::SHA256HashString(anchor.der());
+
+    // Begin struct. Assumed type of EVMetadata:
+    //
+    // struct EVMetadata {
+    //  static const size_t kMaxOIDsPerCA = 2;
+    //  SHA256HashValue fingerprint;
+    //  const base::StringPiece policy_oids[kMaxOIDsPerCA];
+    // };
+    string_to_write += "    {\n";
+    string_to_write += "        {{";
+
+    int wrap_count = 0;
+    for (auto c : sha256_hash) {
+      if (wrap_count != 0) {
+        if (wrap_count % 11 == 0) {
+          string_to_write += ",\n          ";
+        } else {
+          string_to_write += ", ";
+        }
+      }
+      base::StringAppendF(&string_to_write, "0x%02x", static_cast<uint8_t>(c));
+      wrap_count++;
+    }
+
+    string_to_write += "}},\n";
+    string_to_write += "        {\n";
+
+    // struct expects exactly two policy oids, and we can only support 1 or 2
+    // policy OIDs. These checks will need to change if we ever merge the EV and
+    // Chrome Root Store textprotos.
+    const int kMaxPolicyOids = 2;
+    int oids_size = anchor.ev_policy_oids_size();
+    std::string hexencode_hash =
+        base::HexEncode(sha256_hash.data(), sha256_hash.size());
+    if (oids_size > kMaxPolicyOids) {
+      PLOG(ERROR) << hexencode_hash << " has too many OIDs!";
+      return false;
+    } else if (oids_size < 1) {
+      PLOG(ERROR) << hexencode_hash << " has no OIDs!";
+      return false;
+    }
+    for (int i = 0; i < kMaxPolicyOids; i++) {
+      std::string oid;
+      if (i < oids_size) {
+        oid = anchor.ev_policy_oids(i);
+      }
+      string_to_write += "            \"" + oid + "\",\n";
+    }
+
+    // End struct
+    string_to_write += "        },\n";
+    string_to_write += "    },\n";
+  }
+  string_to_write += "};\n";
+  if (!base::WriteFile(cpp_path, string_to_write)) {
+    PLOG(ERROR) << "Error writing cpp include file";
+    return false;
+  }
+  return true;
+}
+
+bool ValidCppOutputFormatValue(base::StringPiece value) {
+  return (value == "root" || value == "ev");
+}
+
 }  // namespace
 
 int main(int argc, char** argv) {
@@ -145,6 +265,8 @@ int main(int argc, char** argv) {
   base::FilePath deps_path = command_line.GetSwitchValuePath("write-deps");
   base::FilePath proto_path = command_line.GetSwitchValuePath("write-proto");
   base::FilePath cpp_path = command_line.GetSwitchValuePath("write-cpp");
+  std::string cpp_output_format =
+      command_line.GetSwitchValueASCII("cpp-output-format");
   // Get root store directory. Assumptions:
   //  - $(ROOT_STORE_DIR)/root_store.textproto contains the textproto definition
   //    of the root store
@@ -156,12 +278,15 @@ int main(int argc, char** argv) {
       command_line.GetSwitchValuePath("root-store-dir");
 
   if ((proto_path.empty() && cpp_path.empty()) || root_store_dir.empty() ||
-      command_line.HasSwitch("help")) {
+      command_line.HasSwitch("help") ||
+      (!cpp_path.empty() && !ValidCppOutputFormatValue(cpp_output_format))) {
+    std::cerr << cpp_output_format << " ";
     std::cerr << "Usage: root_store_tool "
               << "--root-store-dir=<path> "
               << "[--write-deps=DEP_FILE] "
               << "[--write-proto=PROTO_FILE] "
-              << "[--write-cpp=CPP_FILE]" << std::endl;
+              << "[--write-cpp=CPP_FILE --cpp-output-format=[ev|root]] "
+              << std::endl;
     return 1;
   }
 
@@ -190,36 +315,16 @@ int main(int argc, char** argv) {
   }
 
   if (!cpp_path.empty()) {
-    // Root store should have at least one trust anchors.
-    CHECK_GT(root_store->trust_anchors_size(), 0);
-
-    std::string string_to_write =
-        "// This file is auto-generated, DO NOT EDIT.\n\n"
-        "const ChromeRootCertInfo kChromeRootCertList[] = {\n";
-
-    for (auto& anchor : root_store->trust_anchors()) {
-      // Every trust anchor at this point should have a DER.
-      CHECK(!anchor.der().empty());
-      std::string der = anchor.der();
-
-      // Begin struct. Assumed type of ChromeRootCertInfo:
-      //
-      // struct {
-      //   base::span<const uint8_t> der;
-      // };
-      string_to_write += "    {{{";
-
-      // Convert each character to hex representation, escaped.
-      for (auto c : der) {
-        base::StringAppendF(&string_to_write, "0x%02xu,",
-                            static_cast<uint8_t>(c));
-      }
-
-      // End struct
-      string_to_write += "}}},\n";
+    bool success;
+    if (cpp_output_format == "root") {
+      success = WriteRootCppFile(*root_store, cpp_path);
+    } else if (cpp_output_format == "ev") {
+      success = WriteEvCppFile(*root_store, cpp_path);
+    } else {
+      // Unknown format.
+      success = false;
     }
-    string_to_write += "};";
-    if (!base::WriteFile(cpp_path, string_to_write)) {
+    if (!success) {
       PLOG(ERROR) << "Error writing cpp include file";
       return 1;
     }
