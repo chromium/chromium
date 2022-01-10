55339e4207939a6fc7ffc9fb30e448fcfc462670
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-01-10 18:30:27 +0000

    Merge to M97: In PushMessagingManager check whether renderer can access data for origin.
    (cherry picked from commit c941c1be502c0f84a12731007bc99952d5cc5063)
    
    Bug: 1273017
    Change-Id: I57a80030de830f4dbf1d5ec86560f45edadf6c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344789
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charles Reis <creis@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#953214}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373104
    Cr-Commit-Position: refs/branch-heads/4692@{#1394}
    Cr-Branched-From: 038cd96142d384c0d2238973f1cb277725a62eba-refs/heads/main@{#938553}

--- content/browser/bad_message.h
+++ content/browser/bad_message.h
@@ -282,6 +282,9 @@ enum BadMessageReason {
   RFH_CREATE_CHILD_FRAME_SANDBOX_FLAGS = 254,
   RFPH_FOCUSED_FENCED_FRAME = 255,
   RFH_FENCED_FRAME_MOJO_WHEN_DISABLED = 258,
+  PMM_SUBSCRIBE_INVALID_ORIGIN = 259,
+  PMM_UNSUBSCRIBE_INVALID_ORIGIN = 260,
+  PMM_GET_SUBSCRIPTION_INVALID_ORIGIN = 261,
 
   // Please add new elements here. The naming convention is abbreviated class
   // name (e.g. RenderFrameHost becomes RFH) plus a unique description of the
