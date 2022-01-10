71b8f29aca2ae10f382b11cdf3bf6d70aa95ea8d
Yue Li <updowndota@google.com>
2022-01-10 17:48:57 +0000

    QA Cleanup: Remove handling for consent status migration
    Now it has been a few milestones since V2 launch, the temporary handling
    to migrate consent status from V1 can be safely removed.
    
    Bug: None
    Test: Run existing tests
    Change-Id: I915fc98c77541360011474bb1e6765ba81a6956b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374381
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957101}

--- chrome/browser/ui/quick_answers/quick_answers_state_controller.cc
+++ chrome/browser/ui/quick_answers/quick_answers_state_controller.cc
@@ -4,48 +4,9 @@
 
 #include "chrome/browser/ui/quick_answers/quick_answers_state_controller.h"
 
-#include "ash/public/cpp/assistant/assistant_state.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
-#include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "components/prefs/pref_service.h"
-
-namespace {
-
-using chromeos::assistant::prefs::kAssistantContextEnabled;
-using chromeos::assistant::prefs::kAssistantEnabled;
-using quick_answers::prefs::ConsentStatus;
-using quick_answers::prefs::kQuickAnswersConsentStatus;
-using quick_answers::prefs::kQuickAnswersEnabled;
-using quick_answers::prefs::kQuickAnswersNoticeImpressionCount;
-
-void MigrateQuickAnswersConsentStatus(PrefService* prefs) {
-  // If the consented status has not been set, migrate the current context
-  // enabled value.
-  if (prefs->FindPreference(kQuickAnswersConsentStatus)->IsDefaultValue()) {
-    if (!prefs->FindPreference(kAssistantContextEnabled)->IsDefaultValue()) {
-      // Set the consent status based on current feature eligibility.
-      bool consented =
-          ash::AssistantState::Get()->allowed_state() ==
-              chromeos::assistant::AssistantAllowedState::ALLOWED &&
-          prefs->GetBoolean(kAssistantEnabled) &&
-          prefs->GetBoolean(kAssistantContextEnabled);
-      prefs->SetInteger(
-          kQuickAnswersConsentStatus,
-          consented ? ConsentStatus::kAccepted : ConsentStatus::kRejected);
-      // Enable Quick Answers settings for existing users.
-      if (consented)
-        prefs->SetBoolean(kQuickAnswersEnabled, true);
-    } else {
-      // Set the consent status to unknown for new users.
-      prefs->SetInteger(kQuickAnswersConsentStatus, ConsentStatus::kUnknown);
-      // Reset the impression count for new users.
-      prefs->SetInteger(kQuickAnswersNoticeImpressionCount, 0);
-    }
-  }
-}
-
-}  // namespace
 
 QuickAnswersStateController::QuickAnswersStateController()
     : session_observer_(this) {}
@@ -56,5 +17,4 @@ void QuickAnswersStateController::OnFirstSessionStarted() {
   PrefService* prefs =
       ash::Shell::Get()->session_controller()->GetPrimaryUserPrefService();
   state_.RegisterPrefChanges(prefs);
-  MigrateQuickAnswersConsentStatus(prefs);
 }
