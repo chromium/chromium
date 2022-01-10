95827708c53451f742c9b2cd91f65b434504431b
Stefano Duo <stefanoduo@google.com>
2022-01-10 21:05:13 +0000

    [Code Health Rotation] Migrate content_shell_crash_test.py to python3
    Bug: 1275074
    Change-Id: Ica861374a742c267d9c809534ffc512e1eb14b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372964
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#957230}

--- content/shell/tools/breakpad_integration_test.py
+++ content/shell/tools/breakpad_integration_test.py
@@ -102,7 +102,7 @@ def get_android_dump(options, crash_dir, symbols_dir):
       cmd = [minidump_stackwalk, minidump, symbols_dir]
       proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE)
-      stack = proc.communicate()[0]
+      stack = proc.communicate()[0].decode('utf-8')
       print(stack)
 
       device.RunShellCommand(['rm', os.path.join(pending, dump)],
@@ -192,7 +192,7 @@ def run_test(options, crash_dir, symbols_dir, platform,
     failure = 'Failed to run cdb.exe.'
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
-    stack = proc.communicate()[0]
+    stack = proc.communicate()[0].decode('utf-8')
   else:
     minidump_stackwalk = os.path.join(options.build_dir, 'minidump_stackwalk')
     cmd = [minidump_stackwalk, minidump, symbols_dir]
@@ -201,7 +201,7 @@ def run_test(options, crash_dir, symbols_dir, platform,
     failure = 'Failed to run minidump_stackwalk.'
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
-    stack = proc.communicate()[0]
+    stack = proc.communicate()[0].decode('utf-8')
 
   # Check whether the stack contains a CrashIntentionally symbol.
   found_symbol = 'CrashIntentionally' in stack
