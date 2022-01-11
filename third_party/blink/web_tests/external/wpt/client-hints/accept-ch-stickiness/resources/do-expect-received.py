0b1f047b2a7acbb4650ff7e7fc85260169da4ab1
Ari Chivukula <arichiv@chromium.org>
2022-01-11 13:37:39 +0000

    [Third-Party Client Hint Delegation] (9) Add webtests
    This CL adds a bunch of wpt to verify cross-browser functionality of the
    third party client hint delegation feature.
    
    This CL is a part of a series:
    (1) Add Feature
    (2) Add 'name' identical to 'http-equiv' version
    (3) Lock 'name' version to pre-execution text state
    (4) Add third-party delegation parser
    (5) Add ability to modify PermissionsPolicy
    (6) Add 'name' support for third-party subresources
    (7) Add ability to extract raw AllowLists
    (8) Add 'name' support for third-party iframes
    (9) Add webtests
    (10) Detect/block document.write
    (11) Enable by default
    
    Bug: 1219359
    Change-Id: I6489b7f1e0f949f3d1dcbeef3e9d5ca872fdf161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3348063
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957534}

--- third_party/blink/web_tests/external/wpt/client-hints/accept-ch-stickiness/resources/do-expect-received.py
+++ third_party/blink/web_tests/external/wpt/client-hints/accept-ch-stickiness/resources/do-expect-received.py
@@ -1,14 +1,14 @@
 def main(request, response):
     """
-    Check that headers sent to navigate here *do not* contain the device-memory client
+    Check that headers sent to navigate here *do* contain the device-memory client
     hint, and report success/failure in a way compatible with
     verify_{subresource|iframe}_state() in accept-ch-test.js
     """
 
-    if b"device-memory" in request.headers or b"sec-ch-device-memory" in request.headers:
-      result = u"FAIL"
-    else:
+    if b"device-memory" in request.headers and b"sec-ch-device-memory" in request.headers:
       result = u"PASS"
+    else:
+      result = u"FAIL"
 
     content = u'''
 <script>
