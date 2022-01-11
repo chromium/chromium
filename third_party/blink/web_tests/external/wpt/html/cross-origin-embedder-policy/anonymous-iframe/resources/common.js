6abf457e47e5d00ddf1d8a759ea09b6b90c592cb
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-01-11 18:28:37 +0000

    AnonymousIframe: WPT CacheStorage
    Try to access the same key for CacheStorage from two iframe, one
    anonymous, one not. If the CacheStorage is correctly partitionned, a
    value defined from one side, shouldn't influence the other side.
    
    Bug: 1285275
    Change-Id: Ic808e4ef1ee9e06013e29111f3a0977fb10cc0c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3376969
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957633}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/anonymous-iframe/resources/common.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/anonymous-iframe/resources/common.js
@@ -8,3 +8,14 @@ const newAnonymousIframe = (child_origin) => {
   document.body.appendChild(iframe);
   return sub_document_token;
 };
+
+// Create a normal iframe. The new document will execute any scripts sent
+// toward the token it returns.
+const newIframe = (child_origin) => {
+  const sub_document_token = token();
+  let iframe = document.createElement('iframe');
+  iframe.src = child_origin + executor_path + `&uuid=${sub_document_token}`;
+  iframe.anonymous = false
+  document.body.appendChild(iframe);
+  return sub_document_token;
+};
