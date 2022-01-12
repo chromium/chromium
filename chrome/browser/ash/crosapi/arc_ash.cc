b62fcf9290134326b6cb03e6e2f37a66907aa7ac
Maksim Ivanov <emaxx@chromium.org>
2022-01-12 12:09:20 +0000

    Fix use-after-move in ArcAsh
    Fix a potential issue with the unsequenced usage and moving of a
    variable in //c/b/ash/crosapi/arc_ash.cc.
    
    This issue was found by clang-tidy "bugprone-use-after-move" warning:
    
      arc_ash.cc:277:
      'action' used after it was moved
      arc_ash.cc:278:
      move occurred here
      the use and move are unsequenced, i.e. there is no guarantee about the
      order in which they are evaluated
    
    Bug: 1122844
    Change-Id: Ie5acadda7f1fe9f91888a7ed7d6b3ce705541984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380984
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958025}

--- chrome/browser/ash/crosapi/arc_ash.cc
+++ chrome/browser/ash/crosapi/arc_ash.cc
@@ -273,8 +273,9 @@ void ArcAsh::ConvertTextSelectionActions(
     }
 
     // Generate ImageSkia icon.
+    auto icon_png_data = std::move(action->icon->icon_png_data);
     apps::ArcRawIconPngDataToImageSkia(
-        std::move(action->icon->icon_png_data), kSmallIconSizeInDip,
+        std::move(icon_png_data), kSmallIconSizeInDip,
         base::BindOnce(&ArcAsh::ConvertTextSelectionAction,
                        weak_ptr_factory_.GetWeakPtr(), converted_action,
                        std::move(action), barrier_closure));
