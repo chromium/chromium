d3e9a23027f4f236d2747ac559d74a3881d2f8a0
Fabio Tirelo <ftirelo@chromium.org>
2022-01-12 20:09:51 +0000

    Revert "Recommit (revert the revert of) "Add test cases for ksadmin command line parser""
    This reverts commit 1f2741757660f4dcdb6e579ba26be2f9cde6d5f5.
    
    Reason for revert: Causing build failures https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-archive-dbg/6041/overview
    
    Original change's description:
    > Recommit (revert the revert of) "Add test cases for ksadmin command line parser"
    >
    > This reverts commit 350ec4036b0eb104d532cbec1a92b226451ec5ed.
    >
    > Bug: 1280479
    > Change-Id: Iba953e98344570de39e52918bb3708f03192b3af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3382035
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#958220}
    
    Bug: 1280479
    Change-Id: I0bb14d05d49ea40cb54030f3bc13a9839b6bd65c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3384539
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958241}

--- chrome/updater/mac/keystone/ksadmin.h
+++ chrome/updater/mac/keystone/ksadmin.h
@@ -5,21 +5,9 @@
 #ifndef CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
 #define CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
 
-#include <string>
-
-#include "base/containers/flat_map.h"
-
 namespace updater {
 
-namespace ksadmin_internal {
-
-// Exports the function for testing purpose.
-base::flat_map<std::string, std::string> ParseCommandLine(int argc,
-                                                          const char* argv[]);
-
-}  // namespace ksadmin_internal
-
-int KSAdminAppMain(int argc, const char* argv[]);
+int KSAdminAppMain(int argc, char* argv[]);
 
 }  // namespace updater
 
