9a6077ca3915a389ee8060ff12f4400974a54de6
Lei Zhang <thestig@chromium.org>
2022-01-12 20:14:45 +0000

    Improve PrinterInfo/DriverInfo code.
    - Always check the underlying Win32 call for success. In case it they
      write garbage values into the out-params on failure.
    - Remove redundant memset() calls. Now that the buffers are allocated
      with std::make_unique(), they are already value-initialized.
    
    Change-Id: Ia5e374abd35acd86ac30da3212e180f687bf4d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380866
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958247}

--- printing/backend/printing_info_win.cc
+++ printing/backend/printing_info_win.cc
@@ -14,12 +14,12 @@ namespace internal {
 
 std::unique_ptr<uint8_t[]> GetDriverInfo(HANDLE printer, int level) {
   DWORD size = 0;
-  ::GetPrinterDriver(printer, nullptr, level, nullptr, 0, &size);
-  if (size == 0)
+  if (!::GetPrinterDriver(printer, nullptr, level, nullptr, 0, &size) ||
+      size == 0) {
     return nullptr;
+  }
 
   auto buffer = std::make_unique<uint8_t[]>(size);
-  memset(buffer.get(), 0, size);
   if (!::GetPrinterDriver(printer, nullptr, level, buffer.get(), size, &size))
     return nullptr;
 
@@ -28,15 +28,13 @@ std::unique_ptr<uint8_t[]> GetDriverInfo(HANDLE printer, int level) {
 
 std::unique_ptr<uint8_t[]> GetPrinterInfo(HANDLE printer, int level) {
   DWORD size = 0;
-  ::GetPrinter(printer, level, nullptr, 0, &size);
-  if (size == 0) {
+  if (!::GetPrinter(printer, level, nullptr, 0, &size) || size == 0) {
     LOG(WARNING) << "Failed to get size of PRINTER_INFO_" << level
                  << ", error = " << GetLastError();
     return nullptr;
   }
 
   auto buffer = std::make_unique<uint8_t[]>(size);
-  memset(buffer.get(), 0, size);
   if (!::GetPrinter(printer, level, buffer.get(), size, &size)) {
     LOG(WARNING) << "Failed to get PRINTER_INFO_" << level
                  << ", error = " << GetLastError();
