1f2741757660f4dcdb6e579ba26be2f9cde6d5f5
Xiaoling Bao <xiaolingbao@chromium.org>
2022-01-12 19:39:19 +0000

    Recommit (revert the revert of) "Add test cases for ksadmin command line parser"
    This reverts commit 350ec4036b0eb104d532cbec1a92b226451ec5ed.
    
    Bug: 1280479
    Change-Id: Iba953e98344570de39e52918bb3708f03192b3af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3382035
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958220}

--- chrome/updater/mac/keystone/ksadmin.h
+++ chrome/updater/mac/keystone/ksadmin.h
@@ -5,9 +5,21 @@
 #ifndef CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
 #define CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
 
+#include <string>
+
+#include "base/containers/flat_map.h"
+
 namespace updater {
 
-int KSAdminAppMain(int argc, char* argv[]);
+namespace ksadmin_internal {
+
+// Exports the function for testing purpose.
+base::flat_map<std::string, std::string> ParseCommandLine(int argc,
+                                                          const char* argv[]);
+
+}  // namespace ksadmin_internal
+
+int KSAdminAppMain(int argc, const char* argv[]);
 
 }  // namespace updater
 
