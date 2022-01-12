17a83db2f01ed3d658c769b1e1bab588360fea3a
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-12 04:59:57 +0000

    CCA: Migrate views/warning.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I581b2eb5fd04e4f556c7138b1231730042fe4481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3325367
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957931}

--- ash/webui/camera_app_ui/resources/js/views/warning.ts
+++ ash/webui/camera_app_ui/resources/js/views/warning.ts
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert, assertString} from '../assert.js';
+import {assertString} from '../assert.js';
 import * as dom from '../dom.js';
 import {I18nString} from '../i18n_string.js';
 import * as loadTimeData from '../models/load_time_data.js';
 import {ViewName} from '../type.js';
+import {assertEnumVariant} from '../util.js';
 
-import {View} from './view.js';
+import {EnterOptions, View} from './view.js';
 
 /**
  * The type of warning.
- * @enum {string}
  */
 export const WarningType = {
   CAMERA_PAUSED: I18nString.ERROR_MSG_CAMERA_PAUSED,
@@ -20,76 +20,55 @@ export const WarningType = {
   NO_CAMERA: I18nString.ERROR_MSG_NO_CAMERA,
 };
 
-/**
- * @param {*} value The value to check.
- * @return {!I18nString}
- */
-function assertI18nString(value) {
-  assertString(value);
-  assert(
-      Object.values(I18nString).includes(value),
-      `${value} is not a valid I18nString`);
-  return /** @type {I18nString} */ (value);
+function assertI18nString(value: unknown): I18nString {
+  const stringValue = assertString(value);
+  return assertEnumVariant(I18nString, stringValue);
 }
 
 /**
  * Creates the warning-view controller.
  */
 export class Warning extends View {
-  /**
-   * @public
-   */
+  private readonly errorNames: I18nString[] = [];
+
   constructor() {
     super(ViewName.WARNING);
-
-    /**
-     * @type {!Array<!I18nString>}
-     * @private
-     */
-    this.errorNames_ = [];
   }
 
   /**
    * Updates the error message for the latest error-name in the stack.
-   * @private
    */
-  updateMessage_() {
-    const message = this.errorNames_[this.errorNames_.length - 1];
+  private updateMessage() {
+    const message = this.errorNames[this.errorNames.length - 1];
     dom.get('#error-msg', HTMLElement).textContent =
         loadTimeData.getI18nMessage(message);
   }
 
-  /**
-   * @override
-   */
-  entering(name) {
-    name = assertI18nString(name);
+  entering(nameOption?: EnterOptions): void {
+    const name = assertI18nString(nameOption);
 
     // Remove the error-name from the stack to avoid duplication. Then make the
     // error-name the latest one to show its message.
-    const index = this.errorNames_.indexOf(name);
+    const index = this.errorNames.indexOf(name);
     if (index !== -1) {
-      this.errorNames_.splice(index, 1);
+      this.errorNames.splice(index, 1);
     }
-    this.errorNames_.push(name);
-    this.updateMessage_();
+    this.errorNames.push(name);
+    this.updateMessage();
   }
 
-  /**
-   * @override
-   */
-  leaving(...args) {
+  leaving(condition?: unknown): boolean {
     // Recovered error-name for leaving the view.
-    const name = assertI18nString(args[0]);
+    const name = assertI18nString(condition);
 
     // Remove the recovered error from the stack but don't leave the view until
     // there is no error left in the stack.
-    const index = this.errorNames_.indexOf(name);
+    const index = this.errorNames.indexOf(name);
     if (index !== -1) {
-      this.errorNames_.splice(index, 1);
+      this.errorNames.splice(index, 1);
     }
-    if (this.errorNames_.length) {
-      this.updateMessage_();
+    if (this.errorNames.length) {
+      this.updateMessage();
       return false;
     }
     dom.get('#error-msg', HTMLElement).textContent = '';
