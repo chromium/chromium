553052b3408eed1b93504c4d2e93514de1cf5e39
Philip Jägenstedt <philip@foolip.org>
2022-01-13 16:15:07 +0100

    Run BiDi tests from wpt in CI (#87)

--- third_party/chromium-bidi/runBiDiServer.sh
+++ third_party/chromium-bidi/runBiDiServer.sh
@@ -3,6 +3,9 @@
 # Go to the project root folder.
 cd "$(dirname $0)/"
 
+# Crate a folder to store logs.
+mkdir -p logs
+
 NODE_OPTIONS="--unhandled-rejections=strict" \
 DEBUG=* \
-npm run server -- "$@" 2>&1 | tee log.txt
+npm run server-no-build -- "$@" 2>&1 | tee -a logs/$$.log
---
+++ third_party/chromium-bidi/wpt-metadata/README.md
@@ -0,0 +1,1 @@
+This directory contains expectation metadata for web-platform-tests.
