d154923185f41791f5f0c770df15cb82b3b8fd5d
Corentin Wallez <cwallez@chromium.org>
2022-01-14 15:00:35 +0000

    Reland "Record Metal ReadWrite texture UMA histogram."
    This reverts commit cde9ebd97cd77c3924da223bd3b2dd3f353e9f62.
    
    Reason for revert: The reverted CL wasn't the cause of the crashes
    we were seeing, ThinLTO was. Reland the original CL.
    
    Original change's description:
    > Revert "Record Metal ReadWrite texture UMA histogram."
    >
    > This partially reverts commit 50788d87cc48faa2d9b9e549ee9ca3dec2df3920.
    >
    > The histogram definitions are kept as is best practice.
    >
    > Reason for revert: Might be causing OpenGL driver crashes.
    >
    > Original change's description:
    > > Record Metal ReadWrite texture UMA histogram.
    > >
    > > The distribution of tiers of support across Chromium users will allow
    > > the WebGPU W3C group to decide whether to require support for it.
    > >
    > > Fixed: dawn:924
    > > Change-Id: Idfa787a9d8c88f23533048f159ab90eb78e4f1ee
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330356
    > > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#951465}
    >
    > Change-Id: I6c8065312a8b2ddf0329a87d17314942c7f8698a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362527
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#956166}
    
    Change-Id: I0197c3bda707746eaa348dba5b939cb1337866b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383345
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959173}

--- gpu/config/gpu_info_collector_mac.mm
+++ gpu/config/gpu_info_collector_mac.mm
@@ -4,11 +4,45 @@
 
 #include "gpu/config/gpu_info_collector.h"
 
+#include "base/mac/scoped_nsobject.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "third_party/angle/src/gpu_info_util/SystemInfo.h"
 
+#import <Metal/Metal.h>
+
 namespace gpu {
+
+namespace {
+
+// The enums is used for an UMA histogram so we should never reorder entries or
+// remove unused values.
+enum class MetalReadWriteTextureSupportTier {
+  kUnknown = 0,
+  kTier0_NoSupport = 1,
+  kTier1_R32Formats = 2,
+  kTier2_AdditionalFormats = 3,
+  kMaxValue = kTier2_AdditionalFormats,
+};
+
+void RecordReadWriteMetalTexturesSupportedHistogram() {
+  // Metal tiers go 0, 1, 2, but we reserve 0 for when macOS is less then 10.13
+  // and we can't query.
+  NSUInteger best_tier = 0;
+
+  if (@available(macOS 10.13, *)) {
+    base::scoped_nsobject<NSArray<id<MTLDevice>>> devices(MTLCopyAllDevices());
+    for (id<MTLDevice> device in devices.get()) {
+      best_tier = std::max(best_tier, [device readWriteTextureSupport] + 1);
+    }
+  }
+
+  UMA_HISTOGRAM_ENUMERATION(
+      "Gpu.Metal.ReadWriteTextureSupport",
+      static_cast<MetalReadWriteTextureSupportTier>(best_tier));
+}
+}
 
 bool CollectContextGraphicsInfo(GPUInfo* gpu_info) {
   DCHECK(gpu_info);
@@ -17,6 +51,8 @@ bool CollectContextGraphicsInfo(GPUInfo* gpu_info) {
 
   gpu_info->macos_specific_texture_target =
       gpu::GetPlatformSpecificTextureTarget();
+
+  RecordReadWriteMetalTexturesSupportedHistogram();
 
   return CollectGraphicsInfoGL(gpu_info);
 }
