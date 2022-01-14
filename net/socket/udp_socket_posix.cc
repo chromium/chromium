1abc7a7a8da48a0e1078982ec3c687d99d3c5a38
Adam Rice <ricea@chromium.org>
2022-01-14 02:04:20 +0000

    Mac: Ignore ENOTCONN from guarded_close_np()
    The Mac OS kernel has a bug where guarded_close_np() may return an ENOTCONN error. This is impossible to handle correctly because we don't know if the socket is still open or not. Previously we crashed when this happened, but the crash frequency became too high. Potentially leak the file descriptor instead.
    
    BUG=1151048
    
    (cherry picked from commit fd67ba36cff5037bf12f2aee22c37499df8ed828)
    
    Change-Id: I7f098005be01a93811077c31e5f3d59e43341360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370122
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#957350}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388089
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4758@{#602}
    Cr-Branched-From: 4a2cf4baf90326df19c3ee70ff987960d59a386e-refs/heads/main@{#950365}

--- net/socket/udp_socket_posix.cc
+++ net/socket/udp_socket_posix.cc
@@ -291,12 +291,19 @@ void UDPSocketPosix::Close() {
 #if defined(OS_MAC)
   // Attempt to clear errors on the socket so that they are not returned by
   // close(). See https://crbug.com/1151048.
-  // TODO(ricea): Remove this if it doesn't work, or when the OS bug is fixed.
   int value = 0;
   socklen_t value_len = sizeof(value);
   HANDLE_EINTR(getsockopt(socket_, SOL_SOCKET, SO_ERROR, &value, &value_len));
 
-  PCHECK(IGNORE_EINTR(guarded_close_np(socket_, &kSocketFdGuard)) == 0);
+  if (IGNORE_EINTR(guarded_close_np(socket_, &kSocketFdGuard)) != 0) {
+    // There is a bug in the Mac OS kernel that it can return an
+    // ENOTCONN error. In this case we don't know whether the file
+    // descriptor is still allocated or not. We cannot safely close the
+    // file descriptor because it may have been reused by another
+    // thread in the meantime. We may leak file handles here and cause
+    // a crash indirectly later. See https://crbug.com/1151048.
+    PCHECK(errno == ENOTCONN);
+  }
 #else
   PCHECK(IGNORE_EINTR(close(socket_)) == 0);
 #endif  // defined(OS_MAC)
