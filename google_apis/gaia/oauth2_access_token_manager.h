9d8a4fc98d2b8d1589ac941ebf40ee071826daa6
Daniel Cheng <dcheng@chromium.org>
2022-01-14 09:47:30 +0000

    Convert WARN_UNUSED_RESULT to [[nodiscard]] in //google_apis.
    This is an automated conversion produced by the script and command in
    the attached bug.
    
    Bug: 1287045
    Change-Id: I8d30bad7bc5f3ebd3202b0b4d3586cee54a17807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387501
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959087}

--- google_apis/gaia/oauth2_access_token_manager.h
+++ google_apis/gaia/oauth2_access_token_manager.h
@@ -36,10 +36,11 @@ class OAuth2AccessTokenManager {
     virtual ~Delegate();
 
     // Creates and returns an OAuth2AccessTokenFetcher.
-    virtual std::unique_ptr<OAuth2AccessTokenFetcher> CreateAccessTokenFetcher(
+    [[nodiscard]] virtual std::unique_ptr<OAuth2AccessTokenFetcher>
+    CreateAccessTokenFetcher(
         const CoreAccountId& account_id,
         scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-        OAuth2AccessTokenConsumer* consumer) WARN_UNUSED_RESULT = 0;
+        OAuth2AccessTokenConsumer* consumer) = 0;
 
     // Returns |true| if a refresh token is available for |account_id|, and
     // |false| otherwise.
