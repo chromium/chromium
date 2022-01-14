a49b5bf31ae0cc8aca0647c03008de5aad892b62
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-01-14 15:44:47 +0000

    Revert "Take the playout AudioDevice from a MediaStreamTrack's creation frame"
    This reverts commit 6a17272bc28c96460f3b055e10d6990e5bd3f836.
    
    Reason for revert: crbug.com/1287472
    
    Original change's description:
    > Take the playout AudioDevice from a MediaStreamTrack's creation frame
    >
    > When playing a Remote Audio MediaStreamTrack (one received over a PeerConnection), use the WebRtcAudioDeviceImpl instance associated with the frame where the track was created.
    >
    > This fixes an issue where audio is not played if the track is attached to a media element inside a (same origin) iframe.
    >
    > Bug: 1239207
    > Change-Id: Ib97e43913fce75982b5e3f3b2c54a9d9a9a815a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386796
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Commit-Queue: Tony Herre <toprice@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#959107}
    
    Bug: 1239207, 1287472
    Change-Id: Ib51c032067eb0b1a8f714c28b97a0fab0f3544b6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387970
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959202}

--- third_party/blink/renderer/modules/mediastream/remote_media_stream_track_adapter.cc
+++ third_party/blink/renderer/modules/mediastream/remote_media_stream_track_adapter.cc
@@ -20,11 +20,8 @@ namespace blink {
 RemoteVideoTrackAdapter::RemoteVideoTrackAdapter(
     const scoped_refptr<base::SingleThreadTaskRunner>& main_thread,
     webrtc::VideoTrackInterface* webrtc_track,
-    scoped_refptr<MetronomeProvider> metronome_provider,
-    ExecutionContext* track_execution_context)
-    : RemoteMediaStreamTrackAdapter(main_thread,
-                                    webrtc_track,
-                                    track_execution_context),
+    scoped_refptr<MetronomeProvider> metronome_provider)
+    : RemoteMediaStreamTrackAdapter(main_thread, webrtc_track),
       metronome_provider_(std::move(metronome_provider)) {
   std::unique_ptr<TrackObserver> observer(
       new TrackObserver(main_thread, observed_track().get()));
@@ -68,11 +65,8 @@ void RemoteVideoTrackAdapter::InitializeWebVideoTrack(
 
 RemoteAudioTrackAdapter::RemoteAudioTrackAdapter(
     const scoped_refptr<base::SingleThreadTaskRunner>& main_thread,
-    webrtc::AudioTrackInterface* webrtc_track,
-    ExecutionContext* track_execution_context)
-    : RemoteMediaStreamTrackAdapter(main_thread,
-                                    webrtc_track,
-                                    track_execution_context),
+    webrtc::AudioTrackInterface* webrtc_track)
+    : RemoteMediaStreamTrackAdapter(main_thread, webrtc_track),
 #if DCHECK_IS_ON()
       unregistered_(false),
 #endif
