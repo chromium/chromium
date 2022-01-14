6ee58f36631b4cb4c6a9304f7f940cfc25d27fb8
Alan Screen <awscreen@chromium.org>
2022-01-14 23:52:18 +0000

    Fix Windows printer info error handling
    When querying Windows printing info a call is first made to determine
    the size of the structure that will hold the info.  This call will
    always fail, since an insufficient buffer has been provided, so the
    code should not return early if the error is simply
    ERROR_INSUFFICIENT_BUFFER.
    
    Fixed: 1287595
    Change-Id: Ie33c73355119b38de9b63bf212e3b98788acc779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389820
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Auto-Submit: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959385}

--- printing/backend/printing_info_win.cc
+++ printing/backend/printing_info_win.cc
@@ -14,10 +14,11 @@ namespace internal {
 
 std::unique_ptr<uint8_t[]> GetDriverInfo(HANDLE printer, int level) {
   DWORD size = 0;
-  if (!::GetPrinterDriver(printer, nullptr, level, nullptr, 0, &size) ||
-      size == 0) {
+  // ::GetPrinterDriver() will always fail on this check for the required size
+  // because the provided buffer is intentionally insufficient.
+  ::GetPrinterDriver(printer, nullptr, level, nullptr, 0, &size);
+  if (GetLastError() != ERROR_INSUFFICIENT_BUFFER || size == 0)
     return nullptr;
-  }
 
   auto buffer = std::make_unique<uint8_t[]>(size);
   if (!::GetPrinterDriver(printer, nullptr, level, buffer.get(), size, &size))
@@ -28,9 +29,13 @@ std::unique_ptr<uint8_t[]> GetDriverInfo(HANDLE printer, int level) {
 
 std::unique_ptr<uint8_t[]> GetPrinterInfo(HANDLE printer, int level) {
   DWORD size = 0;
-  if (!::GetPrinter(printer, level, nullptr, 0, &size) || size == 0) {
+  // ::GetPrinter() will always fail on this check for the required size
+  // because the provided buffer is intentionally insufficient.
+  ::GetPrinter(printer, level, nullptr, 0, &size);
+  DWORD last_err = GetLastError();
+  if (last_err != ERROR_INSUFFICIENT_BUFFER || size == 0) {
     LOG(WARNING) << "Failed to get size of PRINTER_INFO_" << level
-                 << ", error = " << GetLastError();
+                 << ", error = " << last_err;
     return nullptr;
   }
 
