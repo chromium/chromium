040ad91f1757fcd307097354a34660144bbbf344
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 22:29:03 +0000

    chrome/browser/crash_upload_list: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Id273b3c282179261262531a294dfe5f0442e9ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392705
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959693}

--- chrome/browser/crash_upload_list/crash_upload_list.cc
+++ chrome/browser/crash_upload_list/crash_upload_list.cc
@@ -7,9 +7,9 @@
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
-#if defined(OS_MAC) || defined(OS_WIN)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 #include "components/crash/core/browser/crash_upload_list_crashpad.h"
-#elif defined(OS_FUCHSIA)
+#elif BUILDFLAG(IS_FUCHSIA)
 #include "chrome/browser/crash_upload_list/crash_upload_list_fuchsia.h"
 #else
 #include "base/files/file_path.h"
@@ -20,7 +20,7 @@
 #include "components/upload_list/text_log_upload_list.h"
 #endif
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 #include "base/android/path_utils.h"
 #include "chrome/browser/crash_upload_list/crash_upload_list_android.h"
 #endif
@@ -29,21 +29,21 @@
 #include "components/crash/core/browser/crash_upload_list_crashpad.h"
 #endif
 
-#if defined(OS_LINUX)
+#if BUILDFLAG(IS_LINUX)
 #include "components/upload_list/combining_upload_list.h"
 #endif
 
 scoped_refptr<UploadList> CreateCrashUploadList() {
-#if defined(OS_MAC) || defined(OS_WIN)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
   return new CrashUploadListCrashpad();
-#elif defined(OS_ANDROID)
+#elif BUILDFLAG(IS_ANDROID)
   base::FilePath cache_dir;
   base::android::GetCacheDirectory(&cache_dir);
   base::FilePath upload_log_path =
       cache_dir.Append("Crash Reports")
           .AppendASCII(CrashUploadList::kReporterLogFilename);
   return new CrashUploadListAndroid(upload_log_path);
-#elif defined(OS_FUCHSIA)
+#elif BUILDFLAG(IS_FUCHSIA)
   return new CrashUploadListFuchsia();
 #else
 
@@ -52,7 +52,7 @@ scoped_refptr<UploadList> CreateCrashUploadList() {
 // to log uploads in CrashUploadList::kReporterLogFilename.
 // Linux is handled below.
 #if !(BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS) || \
-      defined(OS_LINUX))
+      BUILDFLAG(IS_LINUX))
   if (crash_reporter::IsCrashpadEnabled()) {
     return new CrashUploadListCrashpad();
   }
@@ -65,7 +65,7 @@ scoped_refptr<UploadList> CreateCrashUploadList() {
   scoped_refptr<UploadList> result =
       base::MakeRefCounted<TextLogUploadList>(upload_log_path);
 
-#if defined(OS_LINUX)
+#if BUILDFLAG(IS_LINUX)
   if (crash_reporter::IsCrashpadEnabled()) {
     // Crashpad keeps the records of C++ crashes (segfaults, etc) in its
     // internal database. The JavaScript error reporter writes JS error upload
@@ -77,5 +77,5 @@ scoped_refptr<UploadList> CreateCrashUploadList() {
   }
 #endif
   return result;
-#endif  // defined(OS_MAC) || defined(OS_WIN)
+#endif  // BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 }
