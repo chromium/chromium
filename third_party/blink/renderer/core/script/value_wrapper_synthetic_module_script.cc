ae19ac1c2a4881f85c5fe8ddeffe86f28bd6c7dd
Vadym Kargin <vadym.kargin@accenture.com>
2022-01-17 22:47:47 +0000

    Replace the old ToV8 with new ToV8Traits interface in the script module.
    This is a reland for CL 3253108. ThryCatch is correctly catching an exception here.
    I use exception_state to rethrow an exception, because it works in isolate.
    But the return needs only exception. Is it correct in this case to take an exception from TryCatch and omit all calls to exception_state?
    
    This update is part of work on replacing the old ToV8 interface with new ToV8Traits<IDLType>::ToV8 interface.
    
    Issue: Replace ToV8 with ToV8Traits<IDLType>::ToV8
    Bug: 1199627
    Change-Id: I9d2c4c1f6eb0196f46b5fbdd92e19878c73efe3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381911
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#960202}

--- third_party/blink/renderer/core/script/value_wrapper_synthetic_module_script.cc
+++ third_party/blink/renderer/core/script/value_wrapper_synthetic_module_script.cc
@@ -7,6 +7,7 @@
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
 #include "third_party/blink/public/platform/web_vector.h"
+#include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_css_style_sheet_init.h"
 #include "third_party/blink/renderer/core/css/css_style_sheet.h"
@@ -64,7 +65,21 @@ ValueWrapperSyntheticModuleScript::CreateCSSWrapperSyntheticModuleScript(
         v8::Local<v8::Value>(), settings_object, params.SourceURL(), KURL(),
         ScriptFetchOptions(), error);
   }
-  v8::Local<v8::Value> v8_value_stylesheet = ToV8(style_sheet, script_state);
+
+  v8::Local<v8::Value> v8_value_stylesheet;
+  {
+    // Limit the scope of v8_try_catch so that it doesn't contain
+    // the following ValueWrapperSyntheticModuleScript::CreateWithDefaultExport.
+    v8::TryCatch v8_try_catch(isolate);
+    if (!ToV8Traits<CSSStyleSheet>::ToV8(script_state, style_sheet)
+             .ToLocal(&v8_value_stylesheet)) {
+      DCHECK(v8_try_catch.HasCaught());
+      return ValueWrapperSyntheticModuleScript::CreateWithError(
+          v8::Local<v8::Value>(), settings_object, params.SourceURL(), KURL(),
+          ScriptFetchOptions(), v8_try_catch.Exception());
+    };
+  }
+
   return ValueWrapperSyntheticModuleScript::CreateWithDefaultExport(
       v8_value_stylesheet, settings_object, params.SourceURL(), KURL(),
       ScriptFetchOptions());
