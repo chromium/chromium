20a18008d523a44ca1e93c76ea65a767132d0cdb
Andrew Grieve <agrieve@chromium.org>
2022-01-17 21:22:15 +0000

    Android: Resolve relative paths in constants.SetOutputDirectory()
    Otherwise, relative paths are resolved in GetOutDirectory() as being
    relative to DIR_SOURCE_ROOT rather than CWD.
    
    Change-Id: Ie3b804f8acf4bff5a7f4a7b8d6e4e6999edbf0f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396673
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960189}

--- build/android/pylib/constants/__init__.py
+++ build/android/pylib/constants/__init__.py
@@ -203,7 +203,7 @@ def SetOutputDirectory(output_directory):
   CheckOutputDirectory(). Typically by providing an --output-dir or
   --chromium-output-dir option.
   """
-  os.environ['CHROMIUM_OUTPUT_DIR'] = output_directory
+  os.environ['CHROMIUM_OUTPUT_DIR'] = os.path.abspath(output_directory)
 
 
 # The message that is printed when the Chromium output directory cannot
