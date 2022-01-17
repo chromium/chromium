01357ff697f3c802c641dbd36b0e51df7fffe33e
Justin Cohen <justincohen@google.com>
2022-01-17 09:03:02 +0000

    ios: Don't save interactionState for downloads.
    Downloads update the WKWebView URL but not the backForwardList
    currentItem.  The URL is quickly reverted to currentItem, but there's
    no simple way to pipe that change down to WebSessionStateTabHelper.
    
    Since there's no value in saving interactionState for downloads, just
    drop them here.
    
    (cherry picked from commit a45c627e7ac209a4d19ce920e9db57e4361d690c)
    
    Fixed: 1285856
    Change-Id: I27c3e5918d60c69046ae6b971183f6bd1e58bd24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387428
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#958813}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390650
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4758@{#690}
    Cr-Branched-From: 4a2cf4baf90326df19c3ee70ff987960d59a386e-refs/heads/main@{#950365}

--- ios/chrome/browser/web/session_state/web_session_state_tab_helper.mm
+++ ios/chrome/browser/web/session_state/web_session_state_tab_helper.mm
@@ -26,6 +26,7 @@
 #import "ios/chrome/browser/web/tab_id_tab_helper.h"
 #include "ios/web/common/features.h"
 #import "ios/web/public/js_messaging/web_frame.h"
+#include "ios/web/public/navigation/navigation_context.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "ios/web/public/session/serializable_user_data_manager.h"
 #include "ios/web/public/web_client.h"
@@ -144,6 +145,11 @@ void WebSessionStateTabHelper::WebStateDestroyed(web::WebState* web_state) {
 void WebSessionStateTabHelper::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
+  // Don't record navigations that result in downloads, since these will be
+  // discarded and there's no simple callback when discarded.
+  if (navigation_context->IsDownload())
+    return;
+
   MarkStale();
 }
 
