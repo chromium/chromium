0d289140a2da329c4f5304f15244c09988653fbf
Dave Tapuska <dtapuska@chromium.org>
2022-01-17 16:43:54 +0000

    Replace ForEachFrame with ForEachRenderFrameHost.
    The tab id is set when a RenderFrameHost is created. When the initial
    TabHelper is created it needs to iterate all RenderFrameHosts owned by
    the WebContents to set its id, so using ForEachRenderFrameHost here is
    correct.
    
    BUG=1208438
    
    Change-Id: Ib56c103aec95bab45912282cd7e4d65b13d97592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386579
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960107}

--- chrome/browser/extensions/tab_helper.cc
+++ chrome/browser/extensions/tab_helper.cc
@@ -187,8 +187,8 @@ TabHelper::TabHelper(content::WebContents* web_contents)
   // The ActiveTabPermissionManager requires a session ID; ensure this
   // WebContents has one.
   CreateSessionServiceTabHelper(web_contents);
-  // The Unretained() is safe because ForEachFrame() is synchronous.
-  web_contents->ForEachFrame(
+  // The Unretained() is safe because ForEachRenderFrameHost() is synchronous.
+  web_contents->ForEachRenderFrameHost(
       base::BindRepeating(&TabHelper::SetTabId, base::Unretained(this)));
   active_tab_permission_granter_ = std::make_unique<ActiveTabPermissionGranter>(
       web_contents, sessions::SessionTabHelper::IdForTab(web_contents).id(),
