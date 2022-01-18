395139cf89f585a260d7ace6ce8dfa6101e48d8c
Alex Turner <alexmt@chromium.org>
2022-01-18 20:57:12 +0000

    Modernize {Dictionary,List}PrefUpdate in /components/language
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /components/language
    
    This CL was uploaded by git cl split.
    
    R=sclittle@chromium.org
    
    Bug: 1285745
    Change-Id: Id2532b405490e0983057b6bd31d04eba6aa7a2b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386089
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960593}

--- components/language/content/browser/ulp_language_code_locator/ulp_language_code_locator.cc
+++ components/language/content/browser/ulp_language_code_locator/ulp_language_code_locator.cc
@@ -54,8 +54,8 @@ std::vector<std::string> UlpLanguageCodeLocator::GetLanguageCodes(
   S2CellId cell(S2LatLng::FromDegrees(latitude, longitude));
   std::vector<std::string> languages;
 
-  ListPrefUpdateDeprecated update(prefs_, kCachedGeoLanguagesPref);
-  base::ListValue* celllangs_cached = update.Get();
+  ListPrefUpdate update(prefs_, kCachedGeoLanguagesPref);
+  base::Value* celllangs_cached = update.Get();
   for (size_t index = 0; index < serialized_langtrees_.size(); index++) {
     std::string language;
 
