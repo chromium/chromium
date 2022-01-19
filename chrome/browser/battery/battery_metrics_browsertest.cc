5ac7a871bf666f9d8b9282ef753c249bd4392d1f
Xiaohan Wang <xhwang@chromium.org>
2022-01-19 20:11:46 +0000

    Remove disabling macro for BatteryMetricsBrowserTest.BatteryDropUMA
    This was found as part of the migration using BUILDFLAG(IS_XXX)
    instead of defined(OS_XXX).
    
    The previous disabling macro missed a "_" so the test was never
    really disabled.
    
    This CL removed the disabling macro which shouldn't change anything.
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ic51b0be19a0398983912e588e1de6b05f82f2a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392671
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961099}

--- chrome/browser/battery/battery_metrics_browsertest.cc
+++ chrome/browser/battery/battery_metrics_browsertest.cc
@@ -129,14 +129,7 @@ class BatteryMetricsBrowserTest : public InProcessBrowserTest {
   std::unique_ptr<MockBatteryMonitor> mock_battery_monitor_;
 };
 
-#if defined(OS_WIN)
-#define DISABLED_ON_WIN(name) DISABLED##name
-#else
-#define DISABLED_ON_WIN(name) name
-#endif
-
-IN_PROC_BROWSER_TEST_F(BatteryMetricsBrowserTest,
-                       DISABLED_ON_WIN(BatteryDropUMA)) {
+IN_PROC_BROWSER_TEST_F(BatteryMetricsBrowserTest, BatteryDropUMA) {
   // Verify that drops in battery level are recorded, and drops by less than 1%
   // are aggregated together until there is a full percentage drop.
   device::mojom::BatteryStatus status;
