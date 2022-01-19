885c297869a6154aa5989af35e53d70a6e624aa3
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-19 07:09:44 +0000

    CCA: Fix all errors in intent.ts with strict mode
    Bug: b:172340451
    Test: tsc compiles for the file with strict: true compiler options set
    Change-Id: I1c9f9a99ade0c8a8bc12a780518e32f01073539e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347209
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960814}

--- ash/webui/camera_app_ui/resources/js/intent.ts
+++ ash/webui/camera_app_ui/resources/js/intent.ts
@@ -104,7 +104,7 @@ export class Intent {
    */
   static create(url: URL, mode: Mode): Intent {
     const params = url.searchParams;
-    const getBool = (key) => params.get(key) === '1';
+    const getBool = (key: string) => params.get(key) === '1';
     const param = params.get('intentId');
     if (param === null) {
       throw new ParseError(url);
