baf14a5b9f40c82da586f8d9e632ff23beb01662
Andres Calderon Jaramillo <andrescj@chromium.org>
2022-01-19 01:07:22 +0000

    arc: Gate HW video decode on cmd line and blocklist
    This CL ensures that we ignore a request to bind an ARC
    VideoDecodeAccelerator when either of the following is true:
    
    1) --disable-accelerated-video-decode is supplied.
    2) Video decode acceleration is blocked by the GPU blocklist
    (software_rendering_list.json).
    
    Bug: b:195769334
    Test: arc.VideoDecodeAccel.h264_oopvd_vm on volteer with --disable-accelerated-video-decode
    Test: arc.VideoDecodeAccel.h264_oopvd_vm on volteer with entry in software_rendering_list.json
    Change-Id: Iff375b4f81f4b9d87c8cc81a3a57f6ee74d810dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396824
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960729}

--- chrome/browser/ash/arc/video/gpu_arc_video_service_host.cc
+++ chrome/browser/ash/arc/video/gpu_arc_video_service_host.cc
@@ -22,11 +22,14 @@
 #include "base/no_destructor.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/task/bind_post_task.h"
 #include "base/threading/thread_checker.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/gpu_feature_checker.h"
 #include "content/public/browser/gpu_service_registry.h"
 #include "content/public/browser/service_process_host.h"
+#include "content/public/common/content_switches.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
@@ -60,20 +63,93 @@ class GpuArcVideoKeyedServiceFactory
 
 class VideoAcceleratorFactoryService : public mojom::VideoAcceleratorFactory {
  public:
-  VideoAcceleratorFactoryService() = default;
+  VideoAcceleratorFactoryService() {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  }
 
   VideoAcceleratorFactoryService(const VideoAcceleratorFactoryService&) =
       delete;
   VideoAcceleratorFactoryService& operator=(
       const VideoAcceleratorFactoryService&) = delete;
 
-  ~VideoAcceleratorFactoryService() override = default;
+  ~VideoAcceleratorFactoryService() override {
+    // A VideoAcceleratorFactoryService is owned by a GpuArcVideoServiceHost
+    // which is a singleton that never destroys its
+    // VideoAcceleratorFactoryService. If this behavior ever changes,
+    // VideoAcceleratorFactoryService will need to be adapted because methods
+    // such as CreateDecodeAccelerator() assume that the
+    // VideoAcceleratorFactoryService never dies. Thus the CHECK(false) here:
+    // violating this assumption without appropriate changes would be a security
+    // problem.
+    CHECK(false);
+  }
 
   void CreateDecodeAccelerator(
       mojo::PendingReceiver<mojom::VideoDecodeAccelerator> receiver,
       mojo::PendingRemote<
           mojom::ProtectedBufferManager> /*protected_buffer_manager*/)
       override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+    // Don't create a video decode accelerator if this functionality has been
+    // disabled by the command-line switch.
+    CHECK(base::CommandLine::InitializedForCurrentProcess());
+    const base::CommandLine* command_line =
+        base::CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(switches::kDisableAcceleratedVideoDecode)) {
+      LOG(WARNING) << "Video decode acceleration has been disabled through the "
+                      "command line";
+      return;
+    }
+
+    // Now check if video decode acceleration has been disabled by the GPU
+    // blocklist. Note: base::Unretained(this) is safe because *|this| should
+    // never die. See the CHECK() in the destructor.
+    const scoped_refptr<base::SingleThreadTaskRunner>& task_runner =
+        base::ThreadTaskRunnerHandle::Get();
+    CHECK(task_runner);
+    auto gpu_feature_checker = content::GpuFeatureChecker::Create(
+        gpu::GpuFeatureType::GPU_FEATURE_TYPE_ACCELERATED_VIDEO_DECODE,
+        base::BindPostTask(
+            task_runner,
+            base::BindOnce(&VideoAcceleratorFactoryService::
+                               ContinueCreatingDecodeAccelerator,
+                           base::Unretained(this), std::move(receiver))));
+    CHECK(gpu_feature_checker);
+    gpu_feature_checker->CheckGpuFeatureAvailability();
+  }
+
+  void CreateVideoDecoder(
+      mojo::PendingReceiver<mojom::VideoDecoder> receiver) override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    content::BindInterfaceInGpuProcess(std::move(receiver));
+  }
+
+  void CreateEncodeAccelerator(
+      mojo::PendingReceiver<mojom::VideoEncodeAccelerator> receiver) override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    content::BindInterfaceInGpuProcess(std::move(receiver));
+  }
+
+  void CreateProtectedBufferAllocator(
+      mojo::PendingReceiver<mojom::VideoProtectedBufferAllocator> receiver)
+      override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    content::BindInterfaceInGpuProcess(std::move(receiver));
+  }
+
+ private:
+  void ContinueCreatingDecodeAccelerator(
+      mojo::PendingReceiver<mojom::VideoDecodeAccelerator> receiver,
+      bool accelerated_video_decode_enabled) {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+    if (!accelerated_video_decode_enabled) {
+      LOG(WARNING) << "Video decode acceleration has been disabled due to the "
+                      "GPU blocklist";
+      return;
+    }
+
     if (base::FeatureList::IsEnabled(arc::kOutOfProcessVideoDecoding)) {
       // TODO(b/195769334): we should check if accelerated video decode is
       // disabled by means of a flag/switch or by GPU bug workarounds.
@@ -106,23 +182,6 @@ class VideoAcceleratorFactoryService : public mojom::VideoAcceleratorFactory {
     content::BindInterfaceInGpuProcess(std::move(receiver));
   }
 
-  void CreateVideoDecoder(
-      mojo::PendingReceiver<mojom::VideoDecoder> receiver) override {
-    content::BindInterfaceInGpuProcess(std::move(receiver));
-  }
-
-  void CreateEncodeAccelerator(
-      mojo::PendingReceiver<mojom::VideoEncodeAccelerator> receiver) override {
-    content::BindInterfaceInGpuProcess(std::move(receiver));
-  }
-
-  void CreateProtectedBufferAllocator(
-      mojo::PendingReceiver<mojom::VideoProtectedBufferAllocator> receiver)
-      override {
-    content::BindInterfaceInGpuProcess(std::move(receiver));
-  }
-
- private:
   mojo::RemoteSet<mojom::VideoAcceleratorFactory> oop_video_factories_;
 };
 
