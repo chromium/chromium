987622237efb7f05257b67515cd18bf808593bc1
John Lee <johntlee@chromium.org>
2022-01-19 21:11:03 +0000

    Disable SidePanelBookmarksListTest on Mac OS
    That test is flakey and a lot of times results in timeout.
    See:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/49236/test-results
    
    This CL also re-enables SidePanelBookmarkFolderTest as it was
    mistakenly disabled.
    
    Bug: 1288747
    Change-Id: I9f2a7a9dc7403ca791f5c216e9dee6e07e0db10c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402678
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961131}

--- chrome/test/data/webui/read_later/side_panel/side_panel_browsertest.js
+++ chrome/test/data/webui/read_later/side_panel/side_panel_browsertest.js
@@ -46,9 +46,16 @@ var SidePanelBookmarksListTest = class extends SidePanelBrowserTest {
   }
 };
 
-TEST_F('SidePanelBookmarksListTest', 'All', function() {
+GEN('#if defined(OS_MAC)');
+GEN('// Flaky, https://crbug.com/1288747');
+GEN('#define MAYBE_All DISABLED_All');
+GEN('#else');
+GEN('#define MAYBE_All All');
+GEN('#endif');
+TEST_F('SidePanelBookmarksListTest', 'MAYBE_All', function() {
   mocha.run();
 });
+GEN('#undef MAYBE_All');
 
 
 var SidePanelBookmarkFolderTest = class extends SidePanelBrowserTest {
@@ -58,14 +65,7 @@ var SidePanelBookmarkFolderTest = class extends SidePanelBrowserTest {
   }
 };
 
-
-GEN('#if defined(OS_MAC)');
-GEN('// Flaky, https://crbug.com/1288747');
-GEN('#define MAYBE_All DISABLED_All');
-GEN('#else');
-GEN('#define MAYBE_All All');
-GEN('#endif');
-TEST_F('SidePanelBookmarkFolderTest', 'MAYBE_All', function() {
+TEST_F('SidePanelBookmarkFolderTest', 'All', function() {
   mocha.run();
 });
 
