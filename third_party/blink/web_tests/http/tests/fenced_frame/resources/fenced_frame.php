f84a9ab61a0af1222989afd73bd7cf95dcaeffff
Nan Lin <linnan@chromium.org>
2022-01-19 21:26:35 +0000

    Move fenced frame ref tests to http/tests and use PHP to serve the file with the desired headers
    The document loaded in a fenced frame should have
    'Supports-Loading-Mode' header.
    
    Change-Id: I7cc80deefa9ee3a724f8183b5079272b5a153db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400985
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961140}

--- third_party/blink/web_tests/http/tests/fenced_frame/resources/fenced_frame.php
+++ third_party/blink/web_tests/http/tests/fenced_frame/resources/fenced_frame.php
@@ -1,3 +1,7 @@
+<?php
+header('Content-Type: text/html');
+header('Supports-Loading-Mode: fenced-frame');
+?>
 <body style="margin: 0px">
   <div style="background: red; width: 20px; height: 20px"></div>
   <script>
