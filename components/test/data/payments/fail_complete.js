27bf56d8edbf5de39fe99c420b09b21b80d768ea
Stephen McGruer <smcgruer@chromium.org>
2022-01-19 15:22:56 +0000

    [basic-card] Migrate PaymentRequestErrorMessageTest.CompleteFail
    This CL migrates the desktop PaymentRequestErrorMessageTest.CompleteFail
    test to not depend on basic-card. The migrated test uses a URL based
    payment method instead of basic-card, and is added as a new test so that
    coverage for the old paths will be preserved.
    
    Bug: 1221559
    Change-Id: Icaf27041a0f076b5a985d19e582f8dd818b95ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399725
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960959}

--- components/test/data/payments/fail_complete.js
+++ components/test/data/payments/fail_complete.js
@@ -8,11 +8,24 @@
 
 /**
  * Launches the PaymentRequest UI and always fails to complete the transaction.
+ *
+ * Legacy entry function until basic-card is removed.
  */
 function buy() { // eslint-disable-line no-unused-vars
+  buyWithMethods(
+      [{supportedMethods: 'basic-card', data: {supportedNetworks: ['visa']}}]);
+}
+
+/**
+ * Launches the PaymentRequest UI and always fails to complete the transaction.
+ *
+ * @param {sequence<PaymentMethodData>} methodData - An array of payment method
+ *        objects.
+ */
+function buyWithMethods(methodData) {
   try {
     new PaymentRequest(
-        [{supportedMethods: 'basic-card', data: {supportedNetworks: ['visa']}}],
+        methodData,
         {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}})
         .show()
         .then(function(resp) {
