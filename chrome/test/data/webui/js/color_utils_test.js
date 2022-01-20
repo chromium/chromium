58a5a4ff6fafb8464a0e08e134dec8959a045491
dpapad <dpapad@chromium.org>
2022-01-20 12:11:13 +0000

    WebUI: Stop using deprecated Mojo "lite" bindings in color_utils_test.js
    While the prod code was already migrated off of pre-JS modules Mojo JS
    bindings, the test code was still relying on such bindings.
    
    Bug: 1179821
    Change-Id: I9b4176983a8f0120a02453a267a54137ee284005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399310
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961403}

--- chrome/test/data/webui/js/color_utils_test.js
+++ chrome/test/data/webui/js/color_utils_test.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// So that mojo is defined.
-import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
-
 import {hexColorToSkColor, skColorToRgba} from 'chrome://resources/js/color_utils.js';
 
 import {assertDeepEquals, assertEquals} from '../chai_assert.js';
