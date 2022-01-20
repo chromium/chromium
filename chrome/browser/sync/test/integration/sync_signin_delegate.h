ac7f8f8c0b3cb1d92710ace95058c82aabc6eeca
Jose Dapena Paz <jdapena@igalia.com>
2022-01-20 17:31:53 +0000

    IWYU: missing include for unique_ptr usage in SyncSigninDelegate.
    Fix build error with libstdc++:
        ../../chrome/browser/sync/test/integration/sync_signin_delegate.h:31:6: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
           31 | std::unique_ptr<SyncSigninDelegate> CreateSyncSigninDelegate();
              |      ^~~~~~~~~~
    
    Bug: 957519
    Change-Id: Ifa4fafb4ce781fc88c54942a3a29630113cda3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403368
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#961509}

--- chrome/browser/sync/test/integration/sync_signin_delegate.h
+++ chrome/browser/sync/test/integration/sync_signin_delegate.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_SYNC_TEST_INTEGRATION_SYNC_SIGNIN_DELEGATE_H_
 #define CHROME_BROWSER_SYNC_TEST_INTEGRATION_SYNC_SIGNIN_DELEGATE_H_
 
+#include <memory>
 #include <string>
 
 class Profile;
