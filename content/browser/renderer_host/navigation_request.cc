9016c39d507de914c93843b623f499fdca84e580
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2022-01-21 02:36:46 +0000

    Make an MPArch fenced frame not recognized as a top frame in EnforceCOEP
    In the previous implementation, it uses GetParent to get a top frame.
    Note that no COEP check or CORP check will be executed for the top
    frame.  Since an MPArch fenced frame root is a main frame (not a top
    frame), GetParent returns nullptr and no COEP check has been executed.
    Even if it is configured to be same-origin explicitly, the rule is
    just ignored for the fenced frame and cross-site page has been loaded.
    
    Note that if COEP:require-corp is configured and embeddees do not have
    CORP headers or wrong CORP headers, they are treated as
    CORP: same-origin  upon the spec.
    https://wicg.github.io/cross-origin-embedder-policy/#:~:text=If%20policy%20is%20null%20and%20embedder%20policy%20value%20is%20%22require%2Dcorp%22%2C%20then%20set%20policy%20to%20%22same%2Dorigin%22
    
    Bug: 1277430
    Change-Id: If21a330def612847d8d4854e84db9e95a4d1ab47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383853
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961759}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -6674,7 +6674,7 @@ NavigationRequest::ComputeCrossOriginEmbedderPolicy() {
   // Note: we only check the outer document for fenced frames, because it's
   // unclear if other embedded cases like Portals should inherit COEP from
   // the embedder as well.
-  // TODO(https://crbug.com/1278207) add other embedded cases if needed.
+  // TODO(https://crbug.com/1278207): add other embedded cases if needed.
   RenderFrameHostImpl* const parent =
       GetNavigatingFrameType() == FrameType::kFencedFrameRoot
           ? GetParentFrameOrOuterDocument()
@@ -6740,7 +6740,7 @@ bool NavigationRequest::CheckResponseAdherenceToCoep(const GURL& url) {
   // Note: we only check the outer document for fenced frames, because it's
   // unclear if other embedded cases like Portals should inherit COEP from
   // the embedder as well.
-  // TODO(https://crbug.com/1278207) add other embedded cases if needed.
+  // TODO(https://crbug.com/1278207): add other embedded cases if needed.
   RenderFrameHostImpl* const parent =
       GetNavigatingFrameType() == FrameType::kFencedFrameRoot
           ? GetParentFrameOrOuterDocument()
@@ -6787,7 +6787,15 @@ bool NavigationRequest::CheckResponseAdherenceToCoep(const GURL& url) {
 absl::optional<network::mojom::BlockedByResponseReason>
 NavigationRequest::EnforceCOEP() {
   // https://html.spec.whatwg.org/C/#check-a-navigation-response's-adherence-to-its-embedder-policy
-  auto* parent_frame = GetParentFrame();
+
+  // Fenced frame should not be recognized as a top frame. COEP must be applied.
+  // Note: we only check the outer document for fenced frames, because it's
+  // unclear if other embedded cases like Portals should behave the same.
+  // TODO(https://crbug.com/1278207): add other embedded cases if needed.
+  RenderFrameHostImpl* const parent_frame =
+      GetNavigatingFrameType() == FrameType::kFencedFrameRoot
+          ? GetParentFrameOrOuterDocument()
+          : GetParentFrame();
   if (!parent_frame) {
     return absl::nullopt;
   }
