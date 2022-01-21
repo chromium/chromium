4a79e07b7fe14920958fda1a76a43b572229b393
Kartik Hegde <khegde@chromium.org>
2022-01-21 23:56:00 +0000

    network_diagnostics: Clean up NetworkDiagnosticsTest
    BUG=b/210752799
    TEST=./out/Default/unit_tests --gtest_filter=NetworkDiagnosticsTest.*
    
    Change-Id: I6ce47e87d23b281557fdbf47d600cf42e5259df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340298
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962157}

--- chrome/browser/ash/net/network_diagnostics/network_diagnostics_unittest.cc
+++ chrome/browser/ash/net/network_diagnostics/network_diagnostics_unittest.cc
@@ -2,32 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/net/network_diagnostics/network_diagnostics.h"
-
 #include <memory>
 #include <utility>
 
 #include "base/memory/weak_ptr.h"
 #include "base/test/bind.h"
+#include "chrome/browser/ash/net/network_diagnostics/network_diagnostics.h"
+#include "chrome/browser/ash/net/network_diagnostics/network_diagnostics_test_helper.h"
 #include "chromeos/dbus/debug_daemon/fake_debug_daemon_client.h"
-#include "chromeos/login/login_state/login_state.h"
-#include "chromeos/network/managed_network_configuration_handler.h"
-#include "chromeos/network/network_cert_loader.h"
-#include "chromeos/network/network_configuration_handler.h"
-#include "chromeos/network/network_handler.h"
-#include "chromeos/network/network_profile_handler.h"
-#include "chromeos/network/onc/network_onc_utils.h"
-#include "chromeos/network/proxy/ui_proxy_config_service.h"
-#include "chromeos/network/system_token_cert_db_storage.h"
-#include "chromeos/services/network_config/public/cpp/cros_network_config_test_helper.h"
 #include "chromeos/services/network_health/public/mojom/network_diagnostics.mojom.h"
-#include "components/onc/onc_constants.h"
-#include "components/onc/onc_pref_names.h"
-#include "components/prefs/testing_pref_service.h"
-#include "components/proxy_config/pref_proxy_config_tracker_impl.h"
-#include "components/proxy_config/proxy_config_pref_names.h"
-#include "components/sync_preferences/testing_pref_service_syncable.h"
-#include "content/public/test/browser_task_environment.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
@@ -87,7 +70,7 @@ const char kFakeValidICMPOutput[] = R"(
 
 }  // namespace
 
-class NetworkDiagnosticsTest : public ::testing::Test {
+class NetworkDiagnosticsTest : public NetworkDiagnosticsTestHelper {
  public:
   NetworkDiagnosticsTest() {
     // Set TestDebugDaemonClient
@@ -97,94 +80,22 @@ class NetworkDiagnosticsTest : public ::testing::Test {
     network_diagnostics_->BindReceiver(
         network_diagnostics_remote_.BindNewPipeAndPassReceiver());
 
-    // Initialize the ManagedNetworkConfigurationHandler and any associated
-    // properties.
-    LoginState::Initialize();
-    SystemTokenCertDbStorage::Initialize();
-    NetworkCertLoader::Initialize();
-    InitializeManagedNetworkConfigurationHandler();
-    // Note that |cros_network_config_test_helper_| must be initialized before
-    // any routine is initialized (routine initialization is done in
-    // NetworkDiagnostics). This is because |g_network_config_override| in
-    // OverrideInProcessInstanceForTesting() must be set up before the routines
-    // invoke BindToInProcessInstance(). See
-    // chromeos/services/network_config/in_process_instance.cc for further
-    // details.
-    cros_network_config_test_helper().Initialize(
-        managed_network_configuration_handler_.get());
-    // Wait until |cros_network_config_test_helper_| has initialized.
-    base::RunLoop().RunUntilIdle();
-
     // Set up properties for the WiFi service.
-    SetUpWiFi();
-  }
-
-  ~NetworkDiagnosticsTest() override {
-    NetworkCertLoader::Shutdown();
-    SystemTokenCertDbStorage::Shutdown();
-    LoginState::Shutdown();
-    managed_network_configuration_handler_.reset();
-    ui_proxy_config_service_.reset();
-    network_configuration_handler_.reset();
-    network_profile_handler_.reset();
-  }
-
-  void InitializeManagedNetworkConfigurationHandler() {
-    network_profile_handler_ = NetworkProfileHandler::InitializeForTesting();
-    network_configuration_handler_ =
-        NetworkConfigurationHandler::InitializeForTest(
-            network_state_helper().network_state_handler(),
-            cros_network_config_test_helper().network_device_handler());
-
-    PrefProxyConfigTrackerImpl::RegisterProfilePrefs(user_prefs_.registry());
-    PrefProxyConfigTrackerImpl::RegisterPrefs(local_state_.registry());
-    ::onc::RegisterProfilePrefs(user_prefs_.registry());
-    ::onc::RegisterPrefs(local_state_.registry());
-
-    ui_proxy_config_service_ = std::make_unique<chromeos::UIProxyConfigService>(
-        &user_prefs_, &local_state_,
-        network_state_helper().network_state_handler(),
-        network_profile_handler_.get());
-
-    managed_network_configuration_handler_ =
-        ManagedNetworkConfigurationHandler::InitializeForTesting(
-            network_state_helper().network_state_handler(),
-            network_profile_handler_.get(),
-            cros_network_config_test_helper().network_device_handler(),
-            network_configuration_handler_.get(),
-            ui_proxy_config_service_.get());
-
-    managed_network_configuration_handler_->SetPolicy(
-        ::onc::ONC_SOURCE_DEVICE_POLICY,
-        /*userhash=*/std::string(),
-        /*network_configs_onc=*/base::ListValue(),
-        /*global_network_config=*/base::DictionaryValue());
-
-    // Wait until the |managed_network_configuration_handler_| is initialized
-    // and set up.
-    base::RunLoop().RunUntilIdle();
-  }
-
-  void SetUpWiFi() {
-    DCHECK(wifi_path_.empty());
-    // By default, NetworkStateTestHelper already adds a WiFi device, so, we
-    // do not need to add one here. All that remains to be done is configuring
-    // the WiFi service.
-    wifi_path_ = ConfigureService(
-        R"({"GUID": "wifi_guid", "Type": "wifi", "State": "online"})");
-    SetServiceProperty(wifi_path_, shill::kSignalStrengthProperty,
-                       base::Value(100));
-    SetServiceProperty(wifi_path_, shill::kSecurityClassProperty,
+    SetUpWiFi(shill::kStateOnline);
+    SetServiceProperty(wifi_path(), shill::kSecurityClassProperty,
                        base::Value(shill::kSecurityPsk));
+
     base::RunLoop().RunUntilIdle();
   }
+
+  ~NetworkDiagnosticsTest() override = default;
 
   // Set up the name servers and change the IPConfigs for the WiFi device and
   // service by overwriting the initial IPConfigs that are set up in
   // FakeShillManagerClient::SetupDefaultEnvironment(). Attach name
   // servers to the IP config.
   void SetUpNameServers(const std::vector<std::string>& name_servers) {
-    DCHECK(!wifi_path_.empty());
+    DCHECK(!wifi_path().empty());
     // Set up the name servers
     base::ListValue dns_servers;
     for (const std::string& name_server : name_servers) {
@@ -195,43 +106,18 @@ class NetworkDiagnosticsTest : public ::testing::Test {
     base::DictionaryValue ip_config_v4_properties;
     ip_config_v4_properties.SetKey(shill::kNameServersProperty,
                                    base::Value(dns_servers.Clone()));
-    network_state_helper().ip_config_test()->AddIPConfig(
-        kIPv4ConfigPath, ip_config_v4_properties);
+    helper()->ip_config_test()->AddIPConfig(kIPv4ConfigPath,
+                                            ip_config_v4_properties);
     std::string wifi_device_path =
-        network_state_helper().device_test()->GetDevicePathForType(
-            shill::kTypeWifi);
-    network_state_helper().device_test()->SetDeviceProperty(
+        helper()->device_test()->GetDevicePathForType(shill::kTypeWifi);
+    helper()->device_test()->SetDeviceProperty(
         wifi_device_path, shill::kIPConfigsProperty, ip_config_v4_properties,
         /*notify_changed=*/true);
-    SetServiceProperty(wifi_path_, shill::kIPConfigProperty,
+    SetServiceProperty(wifi_path(), shill::kIPConfigProperty,
                        base::Value(kIPv4ConfigPath));
-
-    // Wait until the changed name servers have been notified (notification
-    // triggered by call to SetDeviceProperty() above) and that the |wifi_path_|
-    // has been set up.
-    base::RunLoop().RunUntilIdle();
   }
 
  protected:
-  std::string ConfigureService(const std::string& shill_json_string) {
-    return network_state_helper().ConfigureService(shill_json_string);
-  }
-
-  void SetServiceProperty(const std::string& service_path,
-                          const std::string& key,
-                          const base::Value& value) {
-    network_state_helper().SetServiceProperty(service_path, key, value);
-  }
-
-  network_config::CrosNetworkConfigTestHelper&
-  cros_network_config_test_helper() {
-    return cros_network_config_test_helper_;
-  }
-
-  chromeos::NetworkStateTestHelper& network_state_helper() {
-    return cros_network_config_test_helper_.network_state_helper();
-  }
-
   base::WeakPtr<NetworkDiagnosticsTest> weak_ptr() {
     return weak_factory_.GetWeakPtr();
   }
@@ -245,22 +131,7 @@ class NetworkDiagnosticsTest : public ::testing::Test {
   }
 
  private:
-  content::BrowserTaskEnvironment task_environment_;
-  std::string wifi_path_;
   std::unique_ptr<TestDebugDaemonClient> test_debug_daemon_client_;
-  std::unique_ptr<NetworkProfileHandler> network_profile_handler_;
-  std::unique_ptr<NetworkConfigurationHandler> network_configuration_handler_;
-  std::unique_ptr<ManagedNetworkConfigurationHandler>
-      managed_network_configuration_handler_;
-  std::unique_ptr<UIProxyConfigService> ui_proxy_config_service_;
-  sync_preferences::TestingPrefServiceSyncable user_prefs_;
-  TestingPrefServiceSimple local_state_;
-
-  // |cros_network_config_test_helper_| must be initialized with the
-  // ManagedConfigurationHandler. This is done in
-  // InitializeManagedNetworkConfigurationHandler().
-  network_config::CrosNetworkConfigTestHelper cros_network_config_test_helper_{
-      false};
   mojo::Remote<mojom::NetworkDiagnosticsRoutines> network_diagnostics_remote_;
   std::unique_ptr<NetworkDiagnostics> network_diagnostics_;
   base::WeakPtrFactory<NetworkDiagnosticsTest> weak_factory_{this};
