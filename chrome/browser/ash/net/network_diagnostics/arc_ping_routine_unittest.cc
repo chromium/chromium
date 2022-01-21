c898e1765eea3ff20a8b6ff3260b9e6b31a93dbc
Kartik Hegde <khegde@chromium.org>
2022-01-21 21:26:43 +0000

    network_diagnostics: Clean up ArcPingRoutineTest
    BUG=b/209082470
    TEST=./out/Default/unit_tests --gtest_filter=ArcPingRoutineTest.*
    
    Change-Id: I1375693565797726a89b2a8268fcec49e6818f16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315788
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962093}

--- chrome/browser/ash/net/network_diagnostics/arc_ping_routine_unittest.cc
+++ chrome/browser/ash/net/network_diagnostics/arc_ping_routine_unittest.cc
@@ -3,23 +3,10 @@
 // found in the LICENSE file.
 
 #include <memory>
-#include <string>
 
 #include "ash/components/arc/test/fake_net_instance.h"
-#include "base/test/task_environment.h"
 #include "chrome/browser/ash/net/network_diagnostics/arc_ping_routine.h"
-#include "chromeos/login/login_state/login_state.h"
-#include "chromeos/network/network_cert_loader.h"
-#include "chromeos/network/network_configuration_handler.h"
-#include "chromeos/network/network_profile_handler.h"
-#include "chromeos/network/onc/network_onc_utils.h"
-#include "chromeos/network/proxy/ui_proxy_config_service.h"
-#include "chromeos/network/system_token_cert_db_storage.h"
-#include "chromeos/services/network_config/public/cpp/cros_network_config_test_helper.h"
-#include "components/onc/onc_pref_names.h"
-#include "components/proxy_config/pref_proxy_config_tracker_impl.h"
-#include "components/sync_preferences/testing_pref_service_syncable.h"
-#include "content/public/test/browser_task_environment.h"
+#include "chrome/browser/ash/net/network_diagnostics/network_diagnostics_test_helper.h"
 #include "net/dns/public/dns_protocol.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
@@ -37,60 +24,31 @@ const int kHighLatencyDelayMs = 1600;
 
 }  // namespace
 
-class ArcPingRoutineTest : public ::testing::Test {
+class ArcPingRoutineTest : public NetworkDiagnosticsTestHelper {
  public:
-  ArcPingRoutineTest() {
-    LoginState::Initialize();
-    SystemTokenCertDbStorage::Initialize();
-    NetworkCertLoader::Initialize();
-    InitializeManagedNetworkConfigurationHandler();
-    // Note that |cros_network_config_test_helper_| must be initialized before
-    // |arc_ping_routine_| is initialized in SetUpRoutine(). This
-    // is because |g_network_config_override| in
-    // OverrideInProcessInstanceForTesting() must be called before
-    // BindToInProcessInstance() is called. See
-    // chromeos/services/network_config/in_process_instance.cc for further
-    // details.
-    cros_network_config_test_helper().Initialize(
-        managed_network_configuration_handler_.get());
-
-    base::RunLoop().RunUntilIdle();
-  }
-
+  ArcPingRoutineTest() = default;
   ArcPingRoutineTest(const ArcPingRoutineTest&) = delete;
   ArcPingRoutineTest& operator=(const ArcPingRoutineTest&) = delete;
-
-  ~ArcPingRoutineTest() override {
-    NetworkCertLoader::Shutdown();
-    SystemTokenCertDbStorage::Shutdown();
-    LoginState::Shutdown();
-  }
+  ~ArcPingRoutineTest() override = default;
 
   void CompareResult(
       mojom::RoutineVerdict expected_verdict,
       const std::vector<mojom::ArcPingProblem>& expected_problems,
+      base::OnceClosure quit_closure,
       mojom::RoutineResultPtr result) {
     EXPECT_EQ(expected_verdict, result->verdict);
     EXPECT_EQ(expected_problems, result->problems->get_arc_ping_problems());
-    run_loop_.Quit();
-  }
-
-  network_config::CrosNetworkConfigTestHelper&
-  cros_network_config_test_helper() {
-    return cros_network_config_test_helper_;
-  }
-
-  chromeos::NetworkStateTestHelper& network_state_helper() {
-    return cros_network_config_test_helper_.network_state_helper();
+    std::move(quit_closure).Run();
   }
 
  protected:
   void RunRoutine(mojom::RoutineVerdict expected_routine_verdict,
                   const std::vector<mojom::ArcPingProblem>& expected_problems) {
-    arc_ping_routine_->RunRoutine(
-        base::BindOnce(&ArcPingRoutineTest::CompareResult, weak_ptr(),
-                       expected_routine_verdict, expected_problems));
-    run_loop_.Run();
+    base::RunLoop run_loop;
+    arc_ping_routine_->RunRoutine(base::BindOnce(
+        &ArcPingRoutineTest::CompareResult, weak_ptr(),
+        expected_routine_verdict, expected_problems, run_loop.QuitClosure()));
+    run_loop.Run();
   }
 
   void SetUpRoutine(arc::mojom::ArcPingTestResult result) {
@@ -103,81 +61,13 @@ class ArcPingRoutineTest : public ::testing::Test {
     arc_ping_routine_->set_net_instance_for_testing(fake_net_instance_.get());
   }
 
-  void SetUpWiFi(const char* state) {
-    DCHECK(wifi_path_.empty());
-    // By default, NetworkStateTestHelper already adds a WiFi device, so, we
-    // do not need to add one here. All that remains to be done is configuring
-    // the WiFi service.
-    wifi_path_ = ConfigureService(
-        R"({"GUID": "wifi_guid", "Type": "wifi", "State": "idle"})");
-    SetServiceProperty(wifi_path_, shill::kStateProperty, base::Value(state));
-    base::RunLoop().RunUntilIdle();
-  }
-
-  void InitializeManagedNetworkConfigurationHandler() {
-    network_profile_handler_ = NetworkProfileHandler::InitializeForTesting();
-    network_configuration_handler_ =
-        NetworkConfigurationHandler::InitializeForTest(
-            network_state_helper().network_state_handler(),
-            cros_network_config_test_helper().network_device_handler());
-
-    PrefProxyConfigTrackerImpl::RegisterProfilePrefs(user_prefs_.registry());
-    PrefProxyConfigTrackerImpl::RegisterPrefs(local_state_.registry());
-    ::onc::RegisterProfilePrefs(user_prefs_.registry());
-    ::onc::RegisterPrefs(local_state_.registry());
-
-    ui_proxy_config_service_ = std::make_unique<chromeos::UIProxyConfigService>(
-        &user_prefs_, &local_state_,
-        network_state_helper().network_state_handler(),
-        network_profile_handler_.get());
-
-    managed_network_configuration_handler_ =
-        ManagedNetworkConfigurationHandler::InitializeForTesting(
-            network_state_helper().network_state_handler(),
-            network_profile_handler_.get(),
-            cros_network_config_test_helper().network_device_handler(),
-            network_configuration_handler_.get(),
-            ui_proxy_config_service_.get());
-
-    managed_network_configuration_handler_->SetPolicy(
-        ::onc::ONC_SOURCE_DEVICE_POLICY,
-        /*userhash=*/std::string(),
-        /*network_configs_onc=*/base::ListValue(),
-        /*global_network_config=*/base::DictionaryValue());
-
-    base::RunLoop().RunUntilIdle();
-  }
-
   base::WeakPtr<ArcPingRoutineTest> weak_ptr() {
     return weak_factory_.GetWeakPtr();
   }
 
  private:
-  std::string ConfigureService(const std::string& shill_json_string) {
-    return network_state_helper().ConfigureService(shill_json_string);
-  }
-  void SetServiceProperty(const std::string& service_path,
-                          const std::string& key,
-                          const base::Value& value) {
-    network_state_helper().SetServiceProperty(service_path, key, value);
-  }
-
-  // Member order declaration done in a way so that members outlive those that
-  // are dependent on them.
-  content::BrowserTaskEnvironment task_environment_;
-  base::RunLoop run_loop_;
   std::unique_ptr<ArcPingRoutine> arc_ping_routine_;
   std::unique_ptr<arc::FakeNetInstance> fake_net_instance_;
-  network_config::CrosNetworkConfigTestHelper cros_network_config_test_helper_{
-      false};
-  std::unique_ptr<NetworkProfileHandler> network_profile_handler_;
-  std::unique_ptr<NetworkConfigurationHandler> network_configuration_handler_;
-  sync_preferences::TestingPrefServiceSyncable user_prefs_;
-  TestingPrefServiceSimple local_state_;
-  std::unique_ptr<UIProxyConfigService> ui_proxy_config_service_;
-  std::string wifi_path_;
-  std::unique_ptr<ManagedNetworkConfigurationHandler>
-      managed_network_configuration_handler_;
   base::WeakPtrFactory<ArcPingRoutineTest> weak_factory_{this};
 };
 
