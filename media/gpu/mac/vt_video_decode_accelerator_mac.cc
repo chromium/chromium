4f58485b7a1e52c42db2e3a533afb01840885e16
Eugene Zemtsov <eugene@chromium.org>
2022-01-22 00:51:02 +0000

    Pass all PPS NALUs as data to the platform decoder
    It helps in cases when there are more than one PPS in the buffer
    Video Toolbox is smart enough to find and recognize them there.
    
    Test: mac-h264.glitch.me
    We'll have more testing once we switch Mac VEA to new API,
    it generates multiple SPS and PPS.
    
    Bug: 1282056
    Change-Id: Icdc98d10a4e90e2d20b38780973b05a2bea9de37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407723
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962178}

--- media/gpu/mac/vt_video_decode_accelerator_mac.cc
+++ media/gpu/mac/vt_video_decode_accelerator_mac.cc
@@ -866,6 +866,11 @@ void VTVideoDecodeAccelerator::DecodeTask(scoped_refptr<DecoderBuffer> buffer,
           return;
         }
         seen_pps_[pps_id].assign(nalu.data, nalu.data + nalu.size);
+        // Pass PPS as data to the platform decoder, it helps in cases
+        // when there are more than one PPS, Video Toolbox is smart enough
+        // to find and recognize them there.
+        nalus.push_back(nalu);
+        data_size += kNALUHeaderLength + nalu.size;
         break;
       }
 
