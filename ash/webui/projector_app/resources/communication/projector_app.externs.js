2452aa21e9ec26dbec3957fe87215e8cd653dcae
Xiqi Ruan <xiqiruan@chromium.org>
2022-01-24 22:46:31 +0000

    projector: Update projector_app.externs.js
    Copy this file from google3 to update the
    `createdTime` field of pending screencasts.
    
    Bug: b/215258794
    Change-Id: Ieef1953e15dc82dd8655a4983204811d0da5ce76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408375
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962725}

--- ash/webui/projector_app/resources/communication/projector_app.externs.js
+++ ash/webui/projector_app/resources/communication/projector_app.externs.js
@@ -129,6 +129,7 @@ projectorApp.OAuthToken = function() {};
 /**
  * The email of user associated with the oauth token request.
  * @type {string}
+ *
  */
 projectorApp.OAuthToken.prototype.email;
 
@@ -163,7 +164,17 @@ projectorApp.PendingScreencast.prototype.name;
  */
 projectorApp.PendingScreencast.prototype.uploadProgress;
 
-// TODO(b/197015567): Add other screencast fields(duration, createdDate etc.).
+/**
+ * The created time of the screencast video, as the number of milliseconds since
+ * the epoch.
+ * TODO(b/215258794): After adding |createdTime| on Chromium side, remove the
+ * 'undefined' from type definition.
+ * @type {number|undefined}
+ */
+projectorApp.PendingScreencast.prototype.createdTime;
+
+// TODO(b/205334821): We may want to provide duration field to
+// PendingScreencast.
 
 /**
  * Structure for XHR response.
