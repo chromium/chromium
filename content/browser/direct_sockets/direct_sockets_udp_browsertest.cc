604e1a4ea4213a2293997186b9c929b28b2a3b87
Andrew Rayskiy <greengrape@google.com>
2022-01-24 13:47:29 +0000

    Direct Sockets: Add WritableStream for UDPSocket.
    Add UDPWritableStreamWrapper which
    provides writable stream implementation for the UDP Socket.
    
    Streams API reference: https://streams.spec.whatwg.org/#writablestream
    UDP writable stream proposal (old): see §9.2.13
    https://www.w3.org/TR/tcp-udp-sockets/#widl-UDPSocket-writeable
    
    Bug: 1284560
    Change-Id: I004180d7f3fac7644aebebeb264e83148f3b259a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386797
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#962464}

--- content/browser/direct_sockets/direct_sockets_udp_browsertest.cc
+++ content/browser/direct_sockets/direct_sockets_udp_browsertest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/notreached.h"
+#include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "content/browser/direct_sockets/direct_sockets_service_impl.h"
@@ -21,6 +23,8 @@
 #include "net/base/ip_endpoint.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/network/public/mojom/network_context.mojom.h"
+#include "services/network/public/mojom/udp_socket.mojom.h"
+#include "services/network/test/udp_socket_test_util.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "url/gurl.h"
 
@@ -72,12 +76,34 @@ class DirectSocketsUdpBrowserTest : public ContentBrowserTest {
                                     origin_list);
   }
 
+  uint16_t CreateUDPSocket(
+      mojo::PendingRemote<network::mojom::UDPSocketListener>
+          listener_receiver_remote) {
+    GetNetworkContext()->CreateUDPSocket(
+        server_socket_.BindNewPipeAndPassReceiver(),
+        std::move(listener_receiver_remote));
+
+    server_socket_.set_disconnect_handler(
+        base::BindLambdaForTesting([]() { NOTREACHED(); }));
+
+    net::IPEndPoint server_addr(net::IPAddress::IPv4AllZeros(), 0);
+    network::test::UDPSocketTestHelper helper(&server_socket_);
+    DCHECK_EQ(net::OK, helper.BindSync(server_addr, nullptr, &server_addr));
+
+    return server_addr.port();
+  }
+
+  void ReceiveMore(uint32_t num_additional_datagrams) {
+    server_socket_->ReceiveMore(num_additional_datagrams);
+  }
+
  private:
   BrowserContext* browser_context() {
     return shell()->web_contents()->GetBrowserContext();
   }
 
   base::test::ScopedFeatureList feature_list_;
+  mojo::Remote<network::mojom::UDPSocket> server_socket_;
 };
 
 IN_PROC_BROWSER_TEST_F(DirectSocketsUdpBrowserTest, CloseUdp) {
@@ -90,6 +116,68 @@ IN_PROC_BROWSER_TEST_F(DirectSocketsUdpBrowserTest, CloseUdp) {
       "closeUdp({remoteAddress: '::1', remotePort: 993})";
 
   EXPECT_EQ("closeUdp succeeded", EvalJs(shell(), script));
+}
+
+IN_PROC_BROWSER_TEST_F(DirectSocketsUdpBrowserTest, SendUdp) {
+  // We send datagrams with one byte, two bytes, three bytes, ...
+  const uint32_t kRequiredDatagrams = 35;
+  const uint32_t kRequiredBytes =
+      kRequiredDatagrams * (kRequiredDatagrams + 1) / 2;
+  EXPECT_TRUE(NavigateToURL(shell(), GetTestPageURL()));
+
+  DirectSocketsServiceImpl::SetPermissionCallbackForTesting(
+      base::BindRepeating(&UnconditionallyPermitConnection));
+
+  // Any attempt to make this a class member results into
+  // "This caller requires a single-threaded context".
+  network::test::UDPSocketListenerImpl listener;
+  mojo::Receiver<network::mojom::UDPSocketListener> listener_receiver{
+      &listener};
+
+  const uint16_t port =
+      CreateUDPSocket(listener_receiver.BindNewPipeAndPassRemote());
+
+  ReceiveMore(kRequiredDatagrams);
+
+  const std::string script = base::StringPrintf(
+      "sendUdp({remoteAddress: '127.0.0.1', remotePort: %d}, %u)", port,
+      kRequiredBytes);
+
+  EXPECT_EQ("send succeeded", EvalJs(shell(), script));
+
+  listener.WaitForReceivedResults(kRequiredDatagrams);
+  EXPECT_EQ(listener.results().size(), kRequiredDatagrams);
+
+  uint32_t bytes_received = 0, expected_data_size = 0;
+  for (const network::test::UDPSocketListenerImpl::ReceivedResult& result :
+       listener.results()) {
+    expected_data_size++;
+    EXPECT_EQ(result.net_error, net::OK);
+    EXPECT_TRUE(result.src_addr.has_value());
+    EXPECT_TRUE(result.data.has_value());
+    EXPECT_EQ(result.data->size(), expected_data_size);
+    for (uint8_t current : *result.data) {
+      EXPECT_EQ(current, static_cast<uint8_t>('a'));
+      ++bytes_received;
+    }
+  }
+  EXPECT_EQ(bytes_received, kRequiredBytes);
+}
+
+IN_PROC_BROWSER_TEST_F(DirectSocketsUdpBrowserTest, SendUdpAfterClose) {
+  const uint32_t kRequiredBytes = 1;
+  EXPECT_TRUE(NavigateToURL(shell(), GetTestPageURL()));
+
+  DirectSocketsServiceImpl::SetPermissionCallbackForTesting(
+      base::BindRepeating(&UnconditionallyPermitConnection));
+
+  const std::string script = base::StringPrintf(
+      "sendUdpAfterClose({remoteAddress: '127.0.0.1', remotePort: 993}, %u)",
+      kRequiredBytes);
+  EXPECT_EQ(
+      "send failed: InvalidStateError: Failed to execute 'write' on "
+      "'UnderlyingSinkBase': Socket is disconnected.",
+      EvalJs(shell(), script));
 }
 
 }  // namespace content
--- third_party/blink/renderer/modules/direct_sockets/tcp_writable_stream_wrapper.h
+++ third_party/blink/renderer/modules/direct_sockets/tcp_writable_stream_wrapper.h
@@ -50,8 +50,6 @@ class MODULES_EXPORT TCPWritableStreamWrapper final
     return writable_;
   }
 
-  ScriptState* GetScriptState() { return script_state_; }
-
   void Reset();
 
   State GetState() const { return state_; }
