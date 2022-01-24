196f083d5c994ebfbaedc7b0608f6207ea29196b
ginnnnnnny <ginnyhuang@chromium.org>
2022-01-24 20:37:48 +0000

    [iOS][media-permissions] Add tests for permissions on page info
    This CL adds
    1. unit tests for the permissions mediator for the site info panel.
    2. EG tests for the permissions section.
    
    Since EarlGrey does not yet support interacting with system dialogs,
    instead of opening an actual page that requests permissions, the test
    swizzles mediator methods to check the UI when permissions are
    accessible, in the hope that the mediator code would be fully covered
    by unit tests.
    
    Bug: 1280626
    Change-Id: I8a592cb42fdad44d53bad951e734e75d836e48bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3404770
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962665}

--- ios/chrome/browser/ui/page_info/page_info_egtest.mm
+++ ios/chrome/browser/ui/page_info/page_info_egtest.mm
@@ -8,11 +8,15 @@
 #include "components/strings/grit/components_chromium_strings.h"
 #import "ios/chrome/browser/ui/page_info/page_info_constants.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
+#include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/chrome/test/earl_grey/earl_grey_scoped_block_swizzler.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#include "ios/web/common/features.h"
+#import "ios/web/public/permissions/permissions.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -23,6 +27,14 @@
 @end
 
 @implementation PageInfoTestCase
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  if (@available(iOS 15.0, *)) {
+    config.features_enabled.push_back(web::features::kMediaPermissionsControl);
+  }
+  return config;
+}
 
 // Tests that rotating the device will don't dismiss the page info view.
 - (void)testShowPageInfoRotation {
@@ -71,6 +83,51 @@
   [[EarlGrey selectElementWithMatcher:grey_text(l10n_util::GetNSString(
                                           IDS_PAGE_INFO_INTERNAL_PAGE))]
       assertWithMatcher:grey_sufficientlyVisible()];
+}
+
+// Tests that the Permissions section is not displayed, as there isn't any
+// accessible permissions.
+- (void)testShowPageInfoWithNoAccessiblePermission {
+  if (@available(iOS 15.0, *)) {
+    [ChromeEarlGrey loadURL:GURL("https://chromium.org/")];
+    [ChromeEarlGreyUI openPageInfo];
+    // Checks that permission header is visible.
+    [[EarlGrey
+        selectElementWithMatcher:grey_text(l10n_util::GetNSString(
+                                     IDS_IOS_PAGE_INFO_PERMISSIONS_HEADER))]
+        assertWithMatcher:grey_notVisible()];
+  }
+}
+
+// Tests that accessible permission is shown in Permissions section with toggle.
+- (void)testShowAccessiblePermissionInPageInfo {
+  if (@available(iOS 15.0, *)) {
+    // Mock the scenario that microphone permission is on while camera
+    // permission is not accessible.
+    EarlGreyScopedBlockSwizzler microphonePermissionAllowed(
+        @"PageInfoPermissionsMediator", @"accessiblePermissionStates", ^{
+          return @{@(web::PermissionMicrophone) : @YES};
+        });
+
+    [ChromeEarlGrey loadURL:GURL("https://chromium.org/")];
+    [ChromeEarlGreyUI openPageInfo];
+
+    // Check that permission header is visible.
+    [[EarlGrey
+        selectElementWithMatcher:grey_text(l10n_util::GetNSString(
+                                     IDS_IOS_PAGE_INFO_PERMISSIONS_HEADER))]
+        assertWithMatcher:grey_sufficientlyVisible()];
+    // Check that camera permission item is hidden.
+    [[EarlGrey selectElementWithMatcher:grey_text(l10n_util::GetNSString(
+                                            IDS_IOS_PERMISSIONS_CAMERA))]
+        assertWithMatcher:grey_notVisible()];
+    // Check that microphone permission item is visible and enabled.
+    [[EarlGrey selectElementWithMatcher:grey_text(l10n_util::GetNSString(
+                                            IDS_IOS_PERMISSIONS_MICROPHONE))]
+        assertWithMatcher:grey_sufficientlyVisible()];
+    [[EarlGrey selectElementWithMatcher:grey_switchWithOnState(YES)]
+        assertWithMatcher:grey_sufficientlyVisible()];
+  }
 }
 
 @end
--- ios/chrome/browser/ui/page_info/page_info_view_controller.mm
+++ ios/chrome/browser/ui/page_info/page_info_view_controller.mm
@@ -149,14 +149,14 @@ float kTitleLabelMinimumScaleFactor = 0.7f;
   [self.tableViewModel setHeader:permissionsHeaderItem
         forSectionWithIdentifier:SectionIdentifierPermissions];
 
-  [self addSwitchIfExistForPermission:web::PermissionCamera
-                            withLabel:l10n_util::GetNSString(
-                                          IDS_IOS_PERMISSIONS_CAMERA)
-                               toItem:ItemTypePermissionsCamera];
-  [self addSwitchIfExistForPermission:web::PermissionMicrophone
-                            withLabel:l10n_util::GetNSString(
-                                          IDS_IOS_PERMISSIONS_MICROPHONE)
-                               toItem:ItemTypePermissionsMicrophone];
+  [self addSwitchIfAccessibleForPermission:web::PermissionCamera
+                                 withLabel:l10n_util::GetNSString(
+                                               IDS_IOS_PERMISSIONS_CAMERA)
+                                    toItem:ItemTypePermissionsCamera];
+  [self addSwitchIfAccessibleForPermission:web::PermissionMicrophone
+                                 withLabel:l10n_util::GetNSString(
+                                               IDS_IOS_PERMISSIONS_MICROPHONE)
+                                    toItem:ItemTypePermissionsMicrophone];
 
   TableViewLinkHeaderFooterItem* permissionsDescription =
       [[TableViewLinkHeaderFooterItem alloc]
@@ -270,11 +270,12 @@ float kTitleLabelMinimumScaleFactor = 0.7f;
   [self.permissionsDelegate toggleStateForPermission:permission];
 }
 
-// If |permission| is accessible, add a switch for it to the Permissions section
-// of the table.
-- (void)addSwitchIfExistForPermission:(web::Permission)permission
-                            withLabel:(NSString*)label
-                               toItem:(ItemType)item API_AVAILABLE(ios(15.0)) {
+// If |permission| is accessible, adds a switch for it to the Permissions
+// section of the table.
+- (void)addSwitchIfAccessibleForPermission:(web::Permission)permission
+                                 withLabel:(NSString*)label
+                                    toItem:(ItemType)item
+    API_AVAILABLE(ios(15.0)) {
   if ([self.permissionsDelegate isPermissionAccessible:permission]) {
     TableViewSwitchItem* switchItem =
         [[TableViewSwitchItem alloc] initWithType:item];
