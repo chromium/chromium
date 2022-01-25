3c0c0ec5718ada2e66aa4ada76c2ab3639210e34
Trevor Perrier <perrier@chromium.org>
2022-01-25 08:25:28 +0000

    [Merge][ULP] Check ULP for exact and base language matches
    Previously when checking if a language was in the ULP list we were
    looking for exact matches.  This CL adds a check for the base language
    match as well (e.g. pt-MZ == pt-BR).
    
    This CL also adds ULPLanguageStatus::kLanguageEmpty which is used if
    no translate target language has been set.
    
    (cherry picked from commit 380b7c5e67de99ae05f58c40021285077baad824)
    
    Bug: 1288551
    Change-Id: Iceb602aed75536404b1ca3b884f91ec772bf33e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402684
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#961776}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413800
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4844@{#28}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- components/language/core/browser/ulp_metrics_logger_unittest.cc
+++ components/language/core/browser/ulp_metrics_logger_unittest.cc
@@ -23,10 +23,11 @@ TEST(ULPMetricsLoggerTest, TestUILanguageStatus) {
   ULPMetricsLogger logger;
   base::HistogramTester histogram;
 
-  logger.RecordInitiationUILanguageInULP(ULPLanguageStatus::kTopULPLanguage);
+  logger.RecordInitiationUILanguageInULP(
+      ULPLanguageStatus::kTopULPLanguageExactMatch);
 
   histogram.ExpectUniqueSample(kInitiationUILanguageInULPHistogram,
-                               ULPLanguageStatus::kTopULPLanguage, 1);
+                               ULPLanguageStatus::kTopULPLanguageExactMatch, 1);
 }
 
 TEST(ULPMetricsLoggerTest, TestTranslateTargetStatus) {
@@ -34,10 +35,11 @@ TEST(ULPMetricsLoggerTest, TestTranslateTargetStatus) {
   base::HistogramTester histogram;
 
   logger.RecordInitiationTranslateTargetInULP(
-      ULPLanguageStatus::kNonTopULPLanguage);
+      ULPLanguageStatus::kNonTopULPLanguageExactMatch);
 
   histogram.ExpectUniqueSample(kInitiationTranslateTargetInULPHistogram,
-                               ULPLanguageStatus::kNonTopULPLanguage, 1);
+                               ULPLanguageStatus::kNonTopULPLanguageExactMatch,
+                               1);
 }
 
 TEST(ULPMetricsLoggerTest, TestTopAcceptLanguageStatus) {
@@ -59,6 +61,58 @@ TEST(ULPMetricsLoggerTest, TestAcceptLanguagesULPOverlap) {
 
   histogram.ExpectUniqueSample(kInitiationAcceptLanguagesULPOverlapHistogram,
                                21, 1);
+}
+
+TEST(ULPMetricsLoggerTest, TestDetermineLanguageStatus) {
+  ULPMetricsLogger logger;
+  std::vector<std::string> ulp_languages = {"en-US", "es-419", "pt-BR", "de",
+                                            "fr-CA"};
+
+  EXPECT_EQ(ULPLanguageStatus::kTopULPLanguageExactMatch,
+            logger.DetermineLanguageStatus("en-US", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kNonTopULPLanguageExactMatch,
+            logger.DetermineLanguageStatus("de", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kTopULPLanguageBaseMatch,
+            logger.DetermineLanguageStatus("en-GB", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kNonTopULPLanguageBaseMatch,
+            logger.DetermineLanguageStatus("pt", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kLanguageNotInULP,
+            logger.DetermineLanguageStatus("zu", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kLanguageEmpty,
+            logger.DetermineLanguageStatus("", ulp_languages));
+
+  EXPECT_EQ(ULPLanguageStatus::kLanguageEmpty,
+            logger.DetermineLanguageStatus("und", ulp_languages));
+}
+
+TEST(ULPMetricsLoggerTest, TestULPLanguagesInAcceptLanguagesRatio) {
+  ULPMetricsLogger logger;
+  std::vector<std::string> ulp_languages = {"en-US", "es", "pt-BR", "de",
+                                            "fr-CA"};
+
+  EXPECT_EQ(0, logger.ULPLanguagesInAcceptLanguagesRatio({"en-GB", "af", "zu"},
+                                                         ulp_languages));
+
+  EXPECT_EQ(20, logger.ULPLanguagesInAcceptLanguagesRatio({"en-US", "af", "zu"},
+                                                          ulp_languages));
+
+  EXPECT_EQ(40, logger.ULPLanguagesInAcceptLanguagesRatio(
+                    {"en-US", "af", "zu", "es"}, ulp_languages));
+
+  EXPECT_EQ(60, logger.ULPLanguagesInAcceptLanguagesRatio(
+                    {"en-US", "af", "pt-BR", "es"}, ulp_languages));
+
+  EXPECT_EQ(80, logger.ULPLanguagesInAcceptLanguagesRatio(
+                    {"en-US", "af", "pt-BR", "es", "de"}, ulp_languages));
+
+  EXPECT_EQ(100,
+            logger.ULPLanguagesInAcceptLanguagesRatio(
+                {"en-US", "af", "pt-BR", "es", "de", "fr-CA"}, ulp_languages));
 }
 
 }  // namespace language
