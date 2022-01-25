720504241bf2d08e1ad9f78420cec9dbae5c3238
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-25 04:32:01 +0000

    CCA: Fix all errors in mojo/chrome_helper.ts with strict mode
    Bug: b:172340451
    Test: tsc compiles for the file with strict: true compiler options set
    Change-Id: I7e543ad6d0c23b59308e2d5be252474df7a4bf2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355666
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962852}

--- ash/webui/camera_app_ui/resources/js/mojo/chrome_helper.ts
+++ ash/webui/camera_app_ui/resources/js/mojo/chrome_helper.ts
@@ -139,7 +139,7 @@ export class ChromeHelper {
         wrapEndpoint(new CameraUsageOwnershipMonitorCallbackRouter());
 
     usageCallbackRouter.onCameraUsageOwnershipChanged.addListener(
-        async (hasUsage) => {
+        async (hasUsage: boolean) => {
           if (hasUsage) {
             await exploitUsage();
           } else {
