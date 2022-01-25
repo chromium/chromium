8c95096ea10c9395ce142615cd5cab83621b12b4
Johann Koenig <johannkoenig@google.com>
2022-01-25 23:22:25 +0000

    Revert "Reland "Direct Sockets: Add WritableStream for UDPSocket.""
    This reverts commit 4a3be276e10779d930b12bbb975f7a1d9909019b.
    
    Reason for revert: SendUdp is failing on Mac and linux as well
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-xenial-rel/18717/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/73111/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests/111329/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.13%20Tests/47261/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.15%20Tests/20534/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/21101/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-trusty-rel/32889/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ubsan-vptr/10288/overview
    
    Original change's description:
    > Reland "Direct Sockets: Add WritableStream for UDPSocket."
    >
    > This is a reland of 604e1a4ea4213a2293997186b9c929b28b2a3b87
    >
    > DirectSocketsUdpBrowserTest.SendUdp is failing on the Win10 Tests x64 builder,
    > so the reland guards it with a !BUILDFLAG(IS_WIN) ifdef.
    >
    > Original change's description:
    > > Direct Sockets: Add WritableStream for UDPSocket.
    > >
    > > Add UDPWritableStreamWrapper which
    > > provides writable stream implementation for the UDP Socket.
    > >
    > > Streams API reference: https://streams.spec.whatwg.org/#writablestream
    > > UDP writable stream proposal (old): see §9.2.13
    > > https://www.w3.org/TR/tcp-udp-sockets/#widl-UDPSocket-writeable
    > >
    > > Bug: 1284560
    > > Change-Id: I004180d7f3fac7644aebebeb264e83148f3b259a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386797
    > > Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    > > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > > Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#962464}
    >
    > Bug: 1284560
    > Change-Id: I56295fd85d0916f3ba1371020dc6df14ef4b4366
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412435
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    > Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#963165}
    
    Bug: 1284560
    Change-Id: Ic1ae75d41d41772e56297cb4cdeb31150e6eec9f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412614
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Owners-Override: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#963262}

--- third_party/blink/renderer/modules/direct_sockets/tcp_socket_unittest.cc
+++ third_party/blink/renderer/modules/direct_sockets/tcp_socket_unittest.cc
@@ -33,7 +33,7 @@ class TCPSocketCreator {
     return tcp_socket;
   }
 
-  ScriptPromise GetScriptPromise() { return create_promise_; }
+  ScriptPromise GetSciptPromise() { return create_promise_; }
 
  private:
   ScriptPromise create_promise_;
@@ -43,13 +43,13 @@ TEST(TCPSocketTest, Create) {
   V8TestingScope scope;
   TCPSocketCreator tcp_socket_creator;
 
-  auto create_promise = tcp_socket_creator.GetScriptPromise();
+  auto create_promise = tcp_socket_creator.GetSciptPromise();
   EXPECT_TRUE(create_promise.IsEmpty());
 
   tcp_socket_creator.Create(scope);
 
   auto* script_state = scope.GetScriptState();
-  create_promise = tcp_socket_creator.GetScriptPromise();
+  create_promise = tcp_socket_creator.GetSciptPromise();
   ScriptPromiseTester create_tester(script_state, create_promise);
   EXPECT_TRUE(create_promise.IsAssociatedWith(script_state));
 
@@ -62,7 +62,7 @@ TEST(TCPSocketTest, CloseBeforeInit) {
 
   auto* tcp_socket = tcp_socket_creator.Create(scope);
   auto* script_state = scope.GetScriptState();
-  auto create_promise = tcp_socket_creator.GetScriptPromise();
+  auto create_promise = tcp_socket_creator.GetSciptPromise();
   ScriptPromiseTester create_tester(script_state, create_promise);
   ASSERT_FALSE(create_tester.IsRejected());
 
@@ -89,7 +89,7 @@ TEST(TCPSocketTest, CloseAfterInitWithoutResultOK) {
 
   auto* tcp_socket = tcp_socket_creator.Create(scope);
   auto* script_state = scope.GetScriptState();
-  auto create_promise = tcp_socket_creator.GetScriptPromise();
+  auto create_promise = tcp_socket_creator.GetSciptPromise();
   ScriptPromiseTester create_tester(script_state, create_promise);
   ASSERT_FALSE(create_tester.IsRejected());
 
@@ -121,7 +121,7 @@ TEST(TCPSocketTest, CloseAfterInitWithResultOK) {
 
   auto* tcp_socket = tcp_socket_creator.Create(scope);
   auto* script_state = scope.GetScriptState();
-  auto create_promise = tcp_socket_creator.GetScriptPromise();
+  auto create_promise = tcp_socket_creator.GetSciptPromise();
   ScriptPromiseTester create_tester(script_state, create_promise);
   ASSERT_FALSE(create_tester.IsFulfilled());
 
@@ -148,7 +148,7 @@ TEST(TCPSocketTest, OnSocketObserverConnectionError) {
 
   auto* tcp_socket = tcp_socket_creator.Create(scope);
   auto* script_state = scope.GetScriptState();
-  auto create_promise = tcp_socket_creator.GetScriptPromise();
+  auto create_promise = tcp_socket_creator.GetSciptPromise();
   ScriptPromiseTester create_tester(script_state, create_promise);
   ASSERT_FALSE(create_tester.IsRejected());
 
