1316978b3cdfad5459feb8b23d21190dd549452d
Titouan Rigoudy <titouan@chromium.org>
2022-01-26 20:18:42 +0000

    [Private Network Access] Test nested worker script fetches.
    These are currently failing due to lack of support in
    worker_script_fetcher.cc. Indeed, a `ClientSecurityState` is supplied to
    the `URLLoaderFactory` used to fetch the script only if the worker is
    being instantiated by a document / `RenderFrameHostImpl`. This should be
    fixed now that `DedicatedWorkerHost` stores a client security state of
    its own.
    
    Bug: chromium:1282637
    Change-Id: Ibdad59d6effa7de3be72a56a78a09e4c3690ff83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412097
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963670}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/support.sub.js
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/support.sub.js
@@ -343,19 +343,47 @@ const WorkerScriptTestResult = {
   FAILURE: { error: "unknown error" },
 };
 
+function workerScriptUrl(target) {
+  const url =
+    resolveUrl("resources/preflight.py", targetResolveOptions(target));
+
+  url.searchParams.append("body", "postMessage({ loaded: true })")
+  url.searchParams.append("mime-type", "application/javascript")
+
+  return url;
+}
+
 async function workerScriptTest(t, { source, target, expected }) {
   const sourceUrl =
       resolveUrl("resources/worker-fetcher.html", sourceResolveOptions(source));
 
-  const targetUrl =
-      resolveUrl("resources/preflight.py", targetResolveOptions(target));
-  targetUrl.searchParams.append("body", "postMessage({ loaded: true })")
-  targetUrl.searchParams.append("mime-type", "application/javascript")
+  const targetUrl = workerScriptUrl(target);
 
   const iframe = await appendIframe(t, document, sourceUrl);
   const reply = futureMessage();
 
   iframe.contentWindow.postMessage({ url: targetUrl.href }, "*");
+
+  const { error, loaded } = await reply;
+
+  assert_equals(error, expected.error, "worker error");
+  assert_equals(loaded, expected.loaded, "response loaded");
+}
+
+async function nestedWorkerScriptTest(t, { source, target, expected }) {
+  const targetUrl = workerScriptUrl(target);
+
+  const sourceUrl = resolveUrl(
+      "resources/worker-fetcher.js", sourceResolveOptions(source));
+  sourceUrl.searchParams.append("url", targetUrl);
+
+  // Iframe must be same-origin with the parent worker.
+  const iframeUrl = new URL("worker-fetcher.html", sourceUrl);
+
+  const iframe = await appendIframe(t, document, iframeUrl);
+  const reply = futureMessage();
+
+  iframe.contentWindow.postMessage({ url: sourceUrl.href }, "*");
 
   const { error, loaded } = await reply;
 
