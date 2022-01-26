ba38c343667b8382e1647cd3c78245ca89ea5bf7
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-01-26 17:46:33 +0000

    AnonymousIframe: Check FencedFrame Interaction (3/3)
    Add a WPT:
    It shows you can bypass fencedframe using the anonymous one.
    ```
    
                             A (this document)
       ┌────────────────────────┴──┐
     ┌─┼──────────────────────┐    D  (anonymous-iframe)
     │ B (fenced-frame)       │    ^
     │ │                      │    |
     │ C (anonymous-iframe) -->----` (BroadcastChannel)
     └────────────────────────┘
    ```
    
    Note that the mparch implementation is working correctly, because it
    uses a different content::Page.
    
    If the shadow-dom implementation ship, this would require finding a way to use a different nonce inside the fencedframe.
    
    Bug: 1287458
    Change-Id: I8282a8acf8a0c7bf938f54ca9c0abf04215ffc5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3395556
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963576}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/anonymous-iframe/resources/common.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/anonymous-iframe/resources/common.js
@@ -1,9 +1,11 @@
 // Create an anonymous iframe. The new document will execute any scripts sent
 // toward the token it returns.
-const newAnonymousIframe = (child_origin) => {
+const newAnonymousIframe = (child_origin, opt_headers) => {
+  opt_headers ||= "";
   const sub_document_token = token();
   let iframe = document.createElement('iframe');
-  iframe.src = child_origin + executor_path + `&uuid=${sub_document_token}`;
+  iframe.src = child_origin + executor_path + opt_headers +
+    `&uuid=${sub_document_token}`;
   iframe.anonymous = true;
   document.body.appendChild(iframe);
   return sub_document_token;
@@ -22,25 +24,22 @@ const newIframe = (child_origin) => {
 
 // Create a popup. The new document will execute any scripts sent toward the
 // token it returns.
-const newPopup = (origin, opt_headers) => {
-  opt_headers ||= "";
+const newPopup = (test, origin) => {
   const popup_token = token();
-  const popup = window.open(origin + executor_path + opt_headers +
-    `&uuid=${popup_token}`);
-  add_completion_callback(() => popup.close());
+  const popup = window.open(origin + executor_path + `&uuid=${popup_token}`);
+  test.add_cleanup(() => popup.close());
   return popup_token;
 }
 
 // Create a fenced frame. The new document will execute any scripts sent
 // toward the token it returns.
-const newFencedFrame = (child_origin, opt_headers) => {
-  opt_headers ||= "";
+const newFencedFrame = (child_origin) => {
   const support_loading_mode_fenced_frame =
     "|header(Supports-Loading-Mode,fenced-frame)";
   const sub_document_token = token();
   const fencedframe = document.createElement('fencedframe');
   fencedframe.src = child_origin + executor_path +
-    support_loading_mode_fenced_frame + opt_headers +
+    support_loading_mode_fenced_frame +
     `&uuid=${sub_document_token}`;
   document.body.appendChild(fencedframe);
   return sub_document_token;
