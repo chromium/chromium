6568d1dcf1e5ab992f37b93228d7e6335451ed44
David Bienvenu <davidbienvenu@chromium.org>
2022-01-26 19:19:51 +0000

    Move wstring.mojom to base, windows-only
    Moving Windows shortcut creation out-of-process needs wstring.mojom
    to be in base.
    
    Bug: 1276348
    Change-Id: Ic320e8099f10eeea58d73088942c08d0b300afd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415073
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963636}

--- mojo/public/cpp/base/wstring_mojom_traits.cc
+++ mojo/public/cpp/base/wstring_mojom_traits.cc
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/chrome_cleaner/mojom/typemaps/wstring_mojom_traits.h"
+#include "mojo/public/cpp/base/wstring_mojom_traits.h"
 
 #include "mojo/public/cpp/bindings/array_data_view.h"
 
 namespace mojo {
 
 // static
-bool StructTraits<chrome_cleaner::mojom::WStringDataView, std::wstring>::Read(
-    chrome_cleaner::mojom::WStringDataView data,
+bool StructTraits<mojo_base::mojom::WStringDataView, std::wstring>::Read(
+    mojo_base::mojom::WStringDataView data,
     std::wstring* out) {
   ArrayDataView<uint16_t> view;
   data.GetDataDataView(&view);
