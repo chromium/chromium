abade7432a3cf36a8486c21d586ffc6152a77b37
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-26 04:09:13 +0000

    CCA: Fix all errors in untrusted_ga_helper.ts with strict mode
    Bug: b:172340451
    Test: tsc compiles for the file with strict: true compiler options set
    Change-Id: Ic618bc9ecfe68fe1f9582101ccdad2b132d3ecab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3357007
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963383}

--- ash/webui/camera_app_ui/resources/js/untrusted_ga_helper.ts
+++ ash/webui/camera_app_ui/resources/js/untrusted_ga_helper.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assert} from './assert.js';
+
 /**
  * The GA library URL in trusted type.
  */
@@ -10,6 +12,14 @@ const gaLibraryURL = (() => {
       'ga-js-static', {createScriptURL: () => '../js/lib/analytics.js'});
   return staticUrlPolicy.createScriptURL('');
 })();
+
+declare global {
+  interface Window {
+    // Used for GA to support renaming, see
+    // https://developers.google.com/analytics/devguides/collection/analyticsjs/tracking-snippet-reference#tracking-unminified
+    GoogleAnalyticsObject: 'ga';
+  }
+}
 
 /**
  * Initializes GA for sending metrics.
@@ -20,26 +30,32 @@ const gaLibraryURL = (() => {
 function initGA(
     id: string, clientId: string,
     setClientIdCallback: (clientId: string) => void): void {
-  // GA initialization function which is mostly copied from
+  // GA initialization function which is copied and inlined from
   // https://developers.google.com/analytics/devguides/collection/analyticsjs.
-  (function(i, s, o, g, r) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function(...args) {
-      (i[r].q = i[r].q || []).push(args);
-    }, i[r].l = Date.now();
-    const a = s.createElement(o);
-    const m = s.getElementsByTagName(o)[0];
-    a['async'] = 1;
-    a['src'] = g;
-    m.parentNode.insertBefore(a, m);
-  })(window, document, 'script', gaLibraryURL, 'ga');
+  window.GoogleAnalyticsObject = 'ga';
+  // Creates an initial ga() function.
+  // The queued commands will be executed once analytics.js loads.
+  window.ga = window.ga || function(...args: unknown[]) {
+    (window.ga.q = window.ga.q || []).push(args);
+  } as UniversalAnalytics.ga;
+  window.ga.l = Date.now();
+  const a = document.createElement('script');
+  const m = document.getElementsByTagName('script')[0];
+  a.async = true;
+  // TypeScript doesn't support setting .src to TrustedScriptURL yet.
+  a.src = gaLibraryURL as unknown as string;
+  assert(m.parentNode !== null);
+  m.parentNode.insertBefore(a, m);
 
   window.ga('create', id, {
     'storage': 'none',
     'clientId': clientId,
   });
 
-  window.ga((tracker) => setClientIdCallback(tracker.get('clientId')));
+  window.ga((tracker?: UniversalAnalytics.Tracker) => {
+    assert(tracker !== undefined);
+    setClientIdCallback(tracker.get('clientId'));
+  });
 
   // By default GA uses a fake image and sets its source to the target URL to
   // record metrics. Since requesting remote image violates the policy of
@@ -65,7 +81,13 @@ function sendGAEvent(event: UniversalAnalytics.FieldsObject): void {
  * @param enabled True if the metrics is enabled.
  */
 function setMetricsEnabled(id: string, enabled: boolean): void {
-  window[`ga-disable-${id}`] = !enabled;
+  // GA use global `ga-disable-GA_MEASUREMENT_ID` to disable a particular
+  // measurement. See
+  // https://developers.google.com/analytics/devguides/collection/gtagjs/user-opt-out
+  // TODO(pihsun): Use `ga-disable-${string}` as index and move into the
+  // declare global block when we have newer TypeScript compiler to support
+  // that.
+  (window as unknown as Record<string, boolean>)[`ga-disable-${id}`] = !enabled;
 }
 
 export interface GAHelperInterface {
