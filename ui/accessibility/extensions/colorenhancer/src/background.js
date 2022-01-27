0deb729f2e26ae8fe0676c45039bf56a589810bf
Anastasia Helfinstein <anastasi@google.com>
2022-01-27 18:58:27 +0000

    Convert color enhancer background code to an ES6 style class
    This is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: 1289423
    Test: Manually inspected extension behavior (unchanged).
    Change-Id: I4c55b0c5bf3671c4f01a52e8cd46f34dfa121414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415714
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#964167}

--- ui/accessibility/extensions/colorenhancer/src/background.js
+++ ui/accessibility/extensions/colorenhancer/src/background.js
@@ -4,92 +4,104 @@
 
 importScripts('./common.js', './storage.js');
 
-const storage = new Storage();
+class Background {
+  constructor() {
+    /** @private {Storage} */
+    this.storage_ = new Storage();
 
-function forEachTab(tabCallback) {
-  chrome.windows.getAll({'populate': true}, windows => {
-    for (const w of windows) {
-      for (const tab of w.tabs) {
-        if (isDisallowedUrl(tab.url)) {
-          continue;
+    this.init_();
+  }
+
+  /**
+   * @param {function(chrome.tabs.Tab)} tabCallback A function that performs
+   *     an action on each tab
+   * @private
+   */
+  forEachTab_(tabCallback) {
+    chrome.windows.getAll({'populate': true}, windows => {
+      for (const w of windows) {
+        for (const tab of w.tabs) {
+          if (isDisallowedUrl(tab.url)) {
+            continue;
+          }
+          tabCallback(tab);
         }
-        tabCallback(tab);
       }
+    });
+  }
+
+  /**
+   * Adds filter script and css to all existing tabs.
+   *
+   * TODO(wnwen): Verify content scripts are not being injected multiple times.
+   * @private
+   */
+  injectContentScripts_() {
+    this.forEachTab_(tab => chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['src/common.js', 'src/cvd.js'],
+      }));
+  }
+
+  /**
+   * Updates all existing tabs with config values.
+   * @private
+   */
+  updateTabs_() {
+    this.forEachTab_(async (tab) => {
+      const msg = {
+        'delta': await this.storage_.getSiteDelta(siteFromUrl(tab.url)),
+        'severity': await this.storage_.getDefaultSeverity(),
+        'type': await this.storage_.getDefaultType(),
+        'simulate': await this.storage_.getDefaultSimulate(),
+        'enable': await this.storage_.getDefaultEnable()
+      };
+      debugPrint('updateTabs: sending ' + JSON.stringify(msg) + ' to ' +
+          siteFromUrl(tab.url));
+      chrome.tabs.sendMessage(tab.id, msg);
+    });
+  }
+
+  /** @private */
+  async onInitReceived_(sender) {
+    let delta;
+    if (sender.tab) {
+      delta = await this.storage_.getSiteDelta(siteFromUrl(sender.tab.url));
+    } else {
+      delta = await this.storage_.getDefaultDelta();
     }
-  });
-}
 
-/**
- * Adds filter script and css to all existing tabs.
- *
- * TODO(wnwen): Verify content scripts are not being injected multiple times.
- */
-function injectContentScripts() {
-  forEachTab(tab => chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['src/common.js', 'src/cvd.js'],
-    }));
-}
-
-/**
- * Updates all existing tabs with config values.
- */
-function updateTabs() {
-  forEachTab(async function(tab) {
-    const msg = {
-      'delta': await storage.getSiteDelta(siteFromUrl(tab.url)),
-      'severity': await storage.getDefaultSeverity(),
-      'type': await storage.getDefaultType(),
-      'simulate': await storage.getDefaultSimulate(),
-      'enable': await storage.getDefaultEnable()
+    return {
+      'delta': delta,
+      'severity': await this.storage_.getDefaultSeverity(),
+      'type': await this.storage_.getDefaultType(),
+      'simulate': await this.storage_.getDefaultSimulate(),
+      'enable': await this.storage_.getDefaultEnable()
     };
-    debugPrint('updateTabs: sending ' + JSON.stringify(msg) + ' to ' +
-        siteFromUrl(tab.url));
-    chrome.tabs.sendMessage(tab.id, msg);
-  });
-}
-
-async function onInitReceived(sender) {
-  let delta;
-  if (sender.tab) {
-    delta = await storage.getSiteDelta(siteFromUrl(sender.tab.url));
-  } else {
-    delta = await storage.getDefaultDelta();
   }
 
-  return {
-    'delta': delta,
-    'severity': await storage.getDefaultSeverity(),
-    'type': await storage.getDefaultType(),
-    'simulate': await storage.getDefaultSimulate(),
-    'enable': await storage.getDefaultEnable()
-  };
+  /**
+   * Initial extension loading.
+   * @private
+   */
+  init_() {
+    this.injectContentScripts_();
+    this.updateTabs_();
+
+    chrome.runtime.onMessage.addListener(
+        (message, sender, sendResponse) => {
+          if (message === 'init') {
+            this.onInitReceived_(sender).then(sendResponse);
+            return true;  // Keep message context open for async response.
+          } else if (message === 'updateTabs') {
+            this.updateTabs_();
+          }
+        });
+    chrome.storage.onChanged.addListener(this.updateTabs_.bind(this));
+
+    //TODO(mustaq): Handle uninstall
+
+  }
 }
 
-/**
- * Initial extension loading.
- */
-(function initialize() {
-  injectContentScripts();
-  updateTabs();
-
-  chrome.runtime.onMessage.addListener(
-      function(message, sender, sendResponse) {
-        if (message === 'init') {
-          onInitReceived(sender).then(sendResponse);
-          return true;  // Keep message context open for async response.
-        }
-      });
-
-  //TODO(mustaq): Handle uninstall
-
-  chrome.storage.onChanged.addListener(function() {
-    updateTabs();
-  });
-})();
-
-chrome.runtime.onMessage.addListener(message => {
-  if (message === 'updateTabs') {
-    updateTabs();
-  }
-});
+const background = new Background();
