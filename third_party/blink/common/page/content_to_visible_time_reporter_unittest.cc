e39470e1b276fc9c78aca274b60f5e44a7bb4cf6
Joe Mason <joenotcharles@google.com>
2022-01-27 18:12:17 +0000

    Add ContentToVisibleTimeReporter unit tests with TabSwitchMetrics2 feature
    Also changes ContentToVisibleTimeReporter to cache the feature value on
    first lookup instead of in the constructor, because in the test it is
    cumbersome to create the ContentToVisibleTimeReporter after initializing
    ScopedFeatureList.
    
    R=jonross
    
    Bug: 1164477
    Change-Id: I611778e093f66193211e49202fdcebce5d0af330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407687
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964140}

--- third_party/blink/common/page/content_to_visible_time_reporter_unittest.cc
+++ third_party/blink/common/page/content_to_visible_time_reporter_unittest.cc
@@ -2,15 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
 #include <strstream>
 #include <utility>
+#include <vector>
 
 #include "base/containers/contains.h"
+#include "base/containers/extend.h"
 #include "base/rand_util.h"
+#include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/page/content_to_visible_time_reporter.h"
 #include "ui/gfx/presentation_feedback.h"
 
@@ -28,93 +34,167 @@ constexpr base::TimeDelta kOtherDuration = base::Milliseconds(4242);
 struct TabStateParams {
   bool has_saved_frames;
   bool destination_is_loaded;
-  const char* duration_histogram;
-  const char* incomplete_duration_histogram;
-  const char* result_histogram;
+  bool tab_switch_metrics2_enabled;
+  const char* histogram_suffix;
 };
 
 constexpr TabStateParams kTabStatesToTest[] = {
     // WithSavedFrames
-    {.has_saved_frames = true,
-     .destination_is_loaded = true,
-     .duration_histogram = "Browser.Tabs.TotalSwitchDuration.WithSavedFrames",
-     .incomplete_duration_histogram =
-         "Browser.Tabs.TotalIncompleteSwitchDuration.WithSavedFrames",
-     .result_histogram = "Browser.Tabs.TabSwitchResult.WithSavedFrames"},
+    {
+        .has_saved_frames = true,
+        .destination_is_loaded = true,
+        .tab_switch_metrics2_enabled = false,
+        .histogram_suffix = "WithSavedFrames",
+    },
+    {
+        .has_saved_frames = true,
+        .destination_is_loaded = true,
+        .tab_switch_metrics2_enabled = true,
+        .histogram_suffix = "WithSavedFrames",
+    },
+
     // NoSavedFrames_Loaded
-    {.has_saved_frames = false,
-     .destination_is_loaded = true,
-     .duration_histogram =
-         "Browser.Tabs.TotalSwitchDuration.NoSavedFrames_Loaded",
-     .incomplete_duration_histogram =
-         "Browser.Tabs.TotalIncompleteSwitchDuration.NoSavedFrames_Loaded",
-     .result_histogram = "Browser.Tabs.TabSwitchResult.NoSavedFrames_Loaded"},
+    {
+        .has_saved_frames = false,
+        .destination_is_loaded = true,
+        .tab_switch_metrics2_enabled = false,
+        .histogram_suffix = "NoSavedFrames_Loaded",
+    },
+    {
+        .has_saved_frames = false,
+        .destination_is_loaded = true,
+        .tab_switch_metrics2_enabled = true,
+        .histogram_suffix = "NoSavedFrames_Loaded",
+    },
+
     // NoSavedFrames_NotLoaded
-    {.has_saved_frames = false,
-     .destination_is_loaded = false,
-     .duration_histogram =
-         "Browser.Tabs.TotalSwitchDuration.NoSavedFrames_NotLoaded",
-     .incomplete_duration_histogram =
-         "Browser.Tabs.TotalIncompleteSwitchDuration.NoSavedFrames_"
-         "NotLoaded",
-     .result_histogram =
-         "Browser.Tabs.TabSwitchResult.NoSavedFrames_NotLoaded"},
+    {
+        .has_saved_frames = false,
+        .destination_is_loaded = false,
+        .tab_switch_metrics2_enabled = false,
+        .histogram_suffix = "NoSavedFrames_NotLoaded",
+    },
+    {
+        .has_saved_frames = false,
+        .destination_is_loaded = false,
+        .tab_switch_metrics2_enabled = true,
+        .histogram_suffix = "NoSavedFrames_NotLoaded",
+    },
 };
 
 class ContentToVisibleTimeReporterTest
     : public ::testing::TestWithParam<TabStateParams> {
  protected:
   ContentToVisibleTimeReporterTest() : tab_state_(GetParam()) {
+    scoped_feature_list_.InitWithFeatureState(
+        blink::features::kTabSwitchMetrics2,
+        tab_state_.tab_switch_metrics2_enabled);
+
+    duration_histograms_.push_back(base::StrCat(
+        {"Browser.Tabs.TotalSwitchDuration.", tab_state_.histogram_suffix}));
+    incomplete_duration_histograms_.push_back(
+        base::StrCat({"Browser.Tabs.TotalIncompleteSwitchDuration.",
+                      tab_state_.histogram_suffix}));
+    result_histograms_.push_back(base::StrCat(
+        {"Browser.Tabs.TabSwitchResult.", tab_state_.histogram_suffix}));
+
+    if (tab_state_.tab_switch_metrics2_enabled) {
+      // Additional metrics logged when the TabSwitchMetrics2 feature is
+      // enabled.
+      // TODO(crbug.com/1164477): When the feature is enabled both the old and
+      // new histograms are logged, so that the old histograms with and without
+      // the feature can be easily compared in an A/B test. When the feature
+      // ships by default remove the old histograms.
+      duration_histograms_.push_back(base::StrCat(
+          {"Browser.Tabs.TotalSwitchDuration2.", tab_state_.histogram_suffix}));
+      incomplete_duration_histograms_.push_back(
+          base::StrCat({"Browser.Tabs.TotalIncompleteSwitchDuration2.",
+                        tab_state_.histogram_suffix}));
+      result_histograms_.push_back(base::StrCat(
+          {"Browser.Tabs.TabSwitchResult2.", tab_state_.histogram_suffix}));
+    }
+
     // Expect all histograms to be empty.
     ExpectHistogramsEmptyExcept({});
   }
 
   void ExpectHistogramsEmptyExcept(
-      std::vector<const char*> histograms_with_values) {
+      const std::vector<std::string>& histograms_with_values) {
     constexpr const char* kAllHistograms[] = {
+        // Pre-TabSwitchMetrics2 feature.
         "Browser.Tabs.TotalSwitchDuration.WithSavedFrames",
         "Browser.Tabs.TotalSwitchDuration.NoSavedFrames_Loaded",
         "Browser.Tabs.TotalSwitchDuration.NoSavedFrames_NotLoaded",
         "Browser.Tabs.TotalIncompleteSwitchDuration.WithSavedFrames",
         "Browser.Tabs.TotalIncompleteSwitchDuration.NoSavedFrames_Loaded",
-        "Browser.Tabs.TotalIncompleteSwitchDuration.NoSavedFrames_NotLoaded",
+        "Browser.Tabs.TotalIncompleteSwitchDuration.NoSavedFrames_"
+        "NotLoaded",
         "Browser.Tabs.TabSwitchResult.WithSavedFrames",
         "Browser.Tabs.TabSwitchResult.NoSavedFrames_Loaded",
         "Browser.Tabs.TabSwitchResult.NoSavedFrames_NotLoaded",
-        kWebContentsUnOccludedHistogram,
-        kBfcacheRestoreHistogram};
+        // With TabSwitchMetrics2 feature.
+        "Browser.Tabs.TotalSwitchDuration2.WithSavedFrames",
+        "Browser.Tabs.TotalSwitchDuration2.NoSavedFrames_Loaded",
+        "Browser.Tabs.TotalSwitchDuration2.NoSavedFrames_NotLoaded",
+        "Browser.Tabs.TotalIncompleteSwitchDuration2.WithSavedFrames",
+        "Browser.Tabs.TotalIncompleteSwitchDuration2.NoSavedFrames_"
+        "Loaded",
+        "Browser.Tabs.TotalIncompleteSwitchDuration2.NoSavedFrames_"
+        "NotLoaded",
+        "Browser.Tabs.TabSwitchResult2.WithSavedFrames",
+        "Browser.Tabs.TabSwitchResult2.NoSavedFrames_Loaded",
+        "Browser.Tabs.TabSwitchResult2.NoSavedFrames_NotLoaded",
+        // Non-tab switch.
+        kWebContentsUnOccludedHistogram, kBfcacheRestoreHistogram};
+    std::vector<std::string> unexpected_histograms;
     for (const char* histogram : kAllHistograms) {
       if (!base::Contains(histograms_with_values, histogram))
-        ExpectTotalSamples(histogram, 0);
+        unexpected_histograms.push_back(histogram);
+    }
+    ExpectTotalSamples(unexpected_histograms, 0);
+  }
+
+  void ExpectTotalSamples(const std::vector<std::string>& histogram_names,
+                          int expected_count) {
+    for (const std::string& histogram_name : histogram_names) {
+      SCOPED_TRACE(base::StringPrintf("Expect %d samples in %s.",
+                                      expected_count, histogram_name.c_str()));
+      EXPECT_EQ(static_cast<int>(
+                    histogram_tester_.GetAllSamples(histogram_name).size()),
+                expected_count);
     }
   }
 
-  void ExpectTotalSamples(const char* histogram_name, int expected_count) {
-    SCOPED_TRACE(base::StringPrintf("Expect %d samples in %s.", expected_count,
-                                    histogram_name));
-    EXPECT_EQ(static_cast<int>(
-                  histogram_tester_.GetAllSamples(histogram_name).size()),
-              expected_count);
+  void ExpectTimeBucketCounts(const std::vector<std::string>& histogram_names,
+                              base::TimeDelta value,
+                              int count) {
+    for (const std::string& histogram_name : histogram_names) {
+      histogram_tester_.ExpectTimeBucketCount(histogram_name, value, count);
+    }
   }
 
-  void ExpectTimeBucketCount(const char* histogram_name,
-                             base::TimeDelta value,
-                             int count) {
-    histogram_tester_.ExpectTimeBucketCount(histogram_name, value, count);
-  }
-
-  void ExpectResultBucketCount(
-      const char* histogram_name,
+  void ExpectResultBucketCounts(
+      const std::vector<std::string>& histogram_names,
       ContentToVisibleTimeReporter::TabSwitchResult value,
       int count) {
-    histogram_tester_.ExpectBucketCount(histogram_name, value, count);
+    for (const std::string& histogram_name : histogram_names) {
+      histogram_tester_.ExpectBucketCount(histogram_name, value, count);
+    }
   }
 
+  // Create `feature_list_` before `task_environment_` and destroy it after to
+  // avoid a race in destruction.
+  base::test::ScopedFeatureList scoped_feature_list_;
   base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   ContentToVisibleTimeReporter tab_switch_time_recorder_;
   base::HistogramTester histogram_tester_;
   TabStateParams tab_state_;
+
+  // Expected histogram names to be logged for the given TabStateParams.
+  std::vector<std::string> duration_histograms_;
+  std::vector<std::string> incomplete_duration_histograms_;
+  std::vector<std::string> result_histograms_;
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
@@ -137,17 +217,19 @@ TEST_P(ContentToVisibleTimeReporterTest, TimeIsRecorded) {
       end, end - start, gfx::PresentationFeedback::Flags::kHWCompletion);
   std::move(callback).Run(presentation_feedback);
 
-  ExpectHistogramsEmptyExcept(
-      {tab_state_.duration_histogram, tab_state_.result_histogram});
+  std::vector<std::string> expected_histograms;
+  base::Extend(expected_histograms, duration_histograms_);
+  base::Extend(expected_histograms, result_histograms_);
+  ExpectHistogramsEmptyExcept(expected_histograms);
 
   // Duration.
-  ExpectTotalSamples(tab_state_.duration_histogram, 1);
-  ExpectTimeBucketCount(tab_state_.duration_histogram, kDuration, 1);
+  ExpectTotalSamples(duration_histograms_, 1);
+  ExpectTimeBucketCounts(duration_histograms_, kDuration, 1);
 
   // Result.
-  ExpectTotalSamples(tab_state_.result_histogram, 1);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectTotalSamples(result_histograms_, 1);
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kSuccess, 1);
 }
 
@@ -164,12 +246,12 @@ TEST_P(ContentToVisibleTimeReporterTest, PresentationFailure) {
           /* show_reason_bfcache_restore */ false));
   std::move(callback).Run(gfx::PresentationFeedback::Failure());
 
-  ExpectHistogramsEmptyExcept({tab_state_.result_histogram});
+  ExpectHistogramsEmptyExcept(result_histograms_);
 
   // Result (no duration is recorded on presentation failure).
-  ExpectTotalSamples(tab_state_.result_histogram, 1);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectTotalSamples(result_histograms_, 1);
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kPresentationFailure, 1);
 }
 
@@ -188,17 +270,19 @@ TEST_P(ContentToVisibleTimeReporterTest, HideBeforePresentFrame) {
   task_environment_.FastForwardBy(kDuration);
   tab_switch_time_recorder_.TabWasHidden();
 
-  ExpectHistogramsEmptyExcept(
-      {tab_state_.result_histogram, tab_state_.incomplete_duration_histogram});
+  std::vector<std::string> expected_histograms;
+  base::Extend(expected_histograms, result_histograms_);
+  base::Extend(expected_histograms, incomplete_duration_histograms_);
+  ExpectHistogramsEmptyExcept(expected_histograms);
 
   // Duration.
-  ExpectTotalSamples(tab_state_.incomplete_duration_histogram, 1);
-  ExpectTimeBucketCount(tab_state_.incomplete_duration_histogram, kDuration, 1);
+  ExpectTotalSamples(incomplete_duration_histograms_, 1);
+  ExpectTimeBucketCounts(incomplete_duration_histograms_, kDuration, 1);
 
   // Result.
-  ExpectTotalSamples(tab_state_.result_histogram, 1);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectTotalSamples(result_histograms_, 1);
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kIncomplete, 1);
 
   const auto start2 = base::TimeTicks::Now();
@@ -214,23 +298,23 @@ TEST_P(ContentToVisibleTimeReporterTest, HideBeforePresentFrame) {
       end2, end2 - start2, gfx::PresentationFeedback::Flags::kHWCompletion);
   std::move(callback2).Run(presentation_feedback);
 
-  ExpectHistogramsEmptyExcept({tab_state_.duration_histogram,
-                               tab_state_.result_histogram,
-                               tab_state_.incomplete_duration_histogram});
+  // Now the tab switch completes, and adds a duration histogram.
+  base::Extend(expected_histograms, duration_histograms_);
+  ExpectHistogramsEmptyExcept(expected_histograms);
 
   // Duration.
-  ExpectTotalSamples(tab_state_.incomplete_duration_histogram, 1);
-  ExpectTimeBucketCount(tab_state_.incomplete_duration_histogram, kDuration, 1);
-  ExpectTotalSamples(tab_state_.duration_histogram, 1);
-  ExpectTimeBucketCount(tab_state_.duration_histogram, kOtherDuration, 1);
+  ExpectTotalSamples(incomplete_duration_histograms_, 1);
+  ExpectTimeBucketCounts(incomplete_duration_histograms_, kDuration, 1);
+  ExpectTotalSamples(duration_histograms_, 1);
+  ExpectTimeBucketCounts(duration_histograms_, kOtherDuration, 1);
 
   // Result.
-  ExpectTotalSamples(tab_state_.result_histogram, 2);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectTotalSamples(result_histograms_, 2);
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kIncomplete, 1);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kSuccess, 1);
 }
 
@@ -252,8 +336,8 @@ TEST_P(ContentToVisibleTimeReporterTest, UnoccludedTimeIsRecorded) {
   ExpectHistogramsEmptyExcept({kWebContentsUnOccludedHistogram});
 
   // UnOccluded.
-  ExpectTotalSamples(kWebContentsUnOccludedHistogram, 1);
-  ExpectTimeBucketCount(kWebContentsUnOccludedHistogram, kDuration, 1);
+  ExpectTotalSamples({kWebContentsUnOccludedHistogram}, 1);
+  ExpectTimeBucketCounts({kWebContentsUnOccludedHistogram}, kDuration, 1);
 }
 
 // Time is properly recorded to histogram when we have unoccluded event
@@ -273,23 +357,24 @@ TEST_P(ContentToVisibleTimeReporterTest,
       end, end - start, gfx::PresentationFeedback::Flags::kHWCompletion);
   std::move(callback).Run(presentation_feedback);
 
-  ExpectHistogramsEmptyExcept({tab_state_.duration_histogram,
-                               tab_state_.result_histogram,
-                               kWebContentsUnOccludedHistogram});
+  std::vector<std::string> expected_histograms{kWebContentsUnOccludedHistogram};
+  base::Extend(expected_histograms, duration_histograms_);
+  base::Extend(expected_histograms, result_histograms_);
+  ExpectHistogramsEmptyExcept(expected_histograms);
 
   // Duration.
-  ExpectTotalSamples(tab_state_.duration_histogram, 1);
-  ExpectTimeBucketCount(tab_state_.duration_histogram, kDuration, 1);
+  ExpectTotalSamples(duration_histograms_, 1);
+  ExpectTimeBucketCounts(duration_histograms_, kDuration, 1);
 
   // Result.
-  ExpectTotalSamples(tab_state_.result_histogram, 1);
-  ExpectResultBucketCount(
-      tab_state_.result_histogram,
+  ExpectTotalSamples(result_histograms_, 1);
+  ExpectResultBucketCounts(
+      result_histograms_,
       ContentToVisibleTimeReporter::TabSwitchResult::kSuccess, 1);
 
   // UnOccluded.
-  ExpectTotalSamples(kWebContentsUnOccludedHistogram, 1);
-  ExpectTimeBucketCount(kWebContentsUnOccludedHistogram, kDuration, 1);
+  ExpectTotalSamples({kWebContentsUnOccludedHistogram}, 1);
+  ExpectTimeBucketCounts({kWebContentsUnOccludedHistogram}, kDuration, 1);
 }
 
 // Time is properly recorded to histogram when we have bfcache restore event.
@@ -310,8 +395,8 @@ TEST_P(ContentToVisibleTimeReporterTest, BfcacheRestoreTimeIsRecorded) {
   ExpectHistogramsEmptyExcept({kBfcacheRestoreHistogram});
 
   // Bfcache restore.
-  ExpectTotalSamples(kBfcacheRestoreHistogram, 1);
-  ExpectTimeBucketCount(kBfcacheRestoreHistogram, kDuration, 1);
+  ExpectTotalSamples({kBfcacheRestoreHistogram}, 1);
+  ExpectTimeBucketCounts({kBfcacheRestoreHistogram}, kDuration, 1);
 }
 
 }  // namespace blink
