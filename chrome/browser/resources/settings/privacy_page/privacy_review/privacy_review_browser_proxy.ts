14f9406f1327a6d7f2944649d4914ccf99665595
Ehimare Okoyomon <eokoyomon@chromium.org>
2022-01-27 14:12:13 +0000

    Privacy Review: Hide promo if more than x impressions
    Use local storage to store the number of impressions and hide the
    promo if more than the max.
    
    Bug: 1215630, 1287253
    Change-Id: I50845bee1e0b3bd4d9dc0e66c4e927d03f4169dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379161
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964016}

--- chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_browser_proxy.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_browser_proxy.ts
@@ -6,15 +6,42 @@
 import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
 // clang-format on
 
+// The number of times the prviacy review promo has been shown.
+export const MAX_PRIVACY_REVIEW_PROMO_IMPRESSION: number = 10;
+
+// Key to be used with the localStorage for the privacy review promo.
+const PRIVACY_REVIEW_PROMO_IMPRESSION_COUNT_KEY: string =
+    'privacy-review-promo-count';
+
 export interface PrivacyReviewBrowserProxy {
   /** Get the current privacy review status. */
   isPrivacyReviewAvailable(): Promise<boolean>;
+
+  /** @return The number of times the privacy review promo was shown. */
+  getPromoImpressionCount(): number;
+
+  /** Increment The number of times the privacy review promo was shown. */
+  incrementPromoImpressionCount(): void;
 }
 
 export class PrivacyReviewBrowserProxyImpl implements
     PrivacyReviewBrowserProxy {
   isPrivacyReviewAvailable() {
     return sendWithPromise('isPrivacyReviewAvailable');
+  }
+
+  getPromoImpressionCount() {
+    return parseInt(
+               window.localStorage.getItem(
+                   PRIVACY_REVIEW_PROMO_IMPRESSION_COUNT_KEY)!,
+               10) ||
+        0;
+  }
+
+  incrementPromoImpressionCount() {
+    window.localStorage.setItem(
+        PRIVACY_REVIEW_PROMO_IMPRESSION_COUNT_KEY,
+        (this.getPromoImpressionCount() + 1).toString());
   }
 
   static getInstance(): PrivacyReviewBrowserProxy {
