1528ca8b3d99450ce0274778f0682e2793541328
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-01-27 20:15:21 +0000

    M99 merge: [Extensions] Fix a crash in ActionOpenPopupFunction
    The object deleted itself in OnBrowserContextShutdown(), which caused
    a use-after-free when control was handed back to Shutdown(), which
    assumed the object was still valid.
    
    Hold a scoped_refptr<> in the caller, so destruction is delayed until
    Shutdown() completes.
    
    (cherry picked from commit 9c07c7f7e9872b22cc6215c4cb0af7398ef421cc)
    
    Bug: 1289715
    Change-Id: I097bfa25987d68cd2f1c04a6dccda8b908bc05a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415550
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#963506}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420951
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4844@{#84}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- extensions/browser/extension_function.cc
+++ extensions/browser/extension_function.cc
@@ -593,6 +593,10 @@ void ExtensionFunction::SetDispatcher(
 }
 
 void ExtensionFunction::Shutdown() {
+  // Wait until the end of this function to delete |this|, in case
+  // OnBrowserContextShutdown() decrements the refcount.
+  scoped_refptr<ExtensionFunction> keep_alive{this};
+
   // Allow the extension function to perform any cleanup before nulling out
   // `browser_context_`.
   OnBrowserContextShutdown();
