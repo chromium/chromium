c43300370279a727f2ac1a9015d77f8df91e51e5
Egor Pasko <pasko@chromium.org>
2022-01-28 15:46:03 +0000

    android: LinkerTest: reset ShadowRecordHistogram in setUp
    Call ShadowRecordHistogram.reset() in setUp. Also convert calls like
    assertEquals(true, _) to assertTrue(_).
    
    Bug: None
    Change-Id: I5a5553451f1b2d150404ea1d4f1025c87165d8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420494
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964592}

--- base/android/junit/src/org/chromium/base/library_loader/LinkerTest.java
+++ base/android/junit/src/org/chromium/base/library_loader/LinkerTest.java
@@ -45,6 +45,7 @@ public class LinkerTest {
 
     @Before
     public void setUp() {
+        ShadowRecordHistogram.reset();
         Linker.setNativesForTesting(mNativeMock);
         ModernLinker.setModernLinkerNativesForTesting(mModernLinkerNativeMock);
     }
@@ -72,7 +73,7 @@ public class LinkerTest {
                 /* asRelroProducer= */ false, PreferAddress.RESERVE_HINT, someAddress);
 
         // Verify.
-        Assert.assertEquals(false, linker.mRelroProducer);
+        Assert.assertFalse(linker.mRelroProducer);
         Mockito.verify(linker).keepMemoryReservationUntilLoad();
         Mockito.verify(mNativeMock).reserveMemoryForLibrary(anyLibInfo());
         Assert.assertNotEquals(null, linker.mLocalLibInfo);
@@ -92,7 +93,7 @@ public class LinkerTest {
                 /* asRelroProducer= */ false, PreferAddress.RESERVE_HINT, someAddress);
 
         // Verify.
-        Assert.assertEquals(false, linker.mRelroProducer);
+        Assert.assertFalse(linker.mRelroProducer);
         Mockito.verify(linker).keepMemoryReservationUntilLoad();
         Mockito.verify(mNativeMock, Mockito.never()).reserveMemoryForLibrary(anyLibInfo());
     }
@@ -108,7 +109,7 @@ public class LinkerTest {
         linker.ensureInitialized(/* asRelroProducer= */ true, PreferAddress.RESERVE_RANDOM, 0);
 
         // Verify.
-        Assert.assertEquals(true, linker.mRelroProducer);
+        Assert.assertTrue(linker.mRelroProducer);
         Mockito.verify(linker).keepMemoryReservationUntilLoad();
         Mockito.verify(mNativeMock)
                 .findMemoryRegionAtRandomAddress(anyLibInfo(), ArgumentMatchers.eq(true));
@@ -281,7 +282,7 @@ public class LinkerTest {
         // Verify.
         Assert.assertNotNull(linker.mWebviewReservationSearchResult);
         Assert.assertEquals(1,
-                RecordHistogram.getHistogramTotalCountForTesting(
+                ShadowRecordHistogram.getHistogramTotalCountForTesting(
                         "ChromiumAndroidLinker.TimeToFindWebViewReservation.Found.Zygote"));
     }
 
