02aaa2684219b9dcbfff87b49722382e8f0ee9d9
xiangdong kong <xiangdongkong@google.com>
2022-01-28 22:24:29 +0000

    feedback: Implement the on-input event handler on the search page
    As a user enters more text or delete some text, the handler will search
    for help contents and update the suggested help content section.
    
    To avoid unnecessary round trips, update will be attempted only when the
    change of the count of characters is 3 or more. We can fine tune this
    threshold as needed in future.
    
    This Cl uses fake data and displays the help content in the same
    context.
    
    For security reason, followup CLs will move the help content inside a
    iframe which renders under an untrusted context.
    
    No styling is applied yet.
    
    Screenshots:
    - Before typing: https://screenshot.googleplex.com/ABcBbM6gLSbNRwC.png
    - After typing: https://screenshot.googleplex.com/xkMYLhdQ4QzkiJ7.png
    
    Bug: b:185624798
    Test: browser_tests --gtest_filter="OSFeedbackBrowserTest.*"
    Change-Id: Idad878de90d379e9597be8ef7be4eae4701a4a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416272
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#964808}

--- ash/webui/os_feedback_ui/resources/search_page.js
+++ ash/webui/os_feedback_ui/resources/search_page.js
@@ -9,6 +9,22 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {HelpContentList, HelpContentProviderInterface} from './feedback_types.js';
+import {getHelpContentProvider} from './mojo_interface_provider.js';
+
+/**
+ * The minimum number of characters added or deleted to start a new search for
+ * help content.
+ * @type {number}
+ */
+const MIN_CHARS_COUNT = 3;
+
+/**
+ * The maximum number of help contents wanted per search.
+ *  @type {number}
+ */
+const MAX_RESULTS = 5;
+
 /**
  * @fileoverview
  * 'search-page' is the first step of the feedback tool. It displays live help
@@ -21,6 +37,48 @@ export class SearchPageElement extends PolymerElement {
 
   static get template() {
     return html`{__html_template__}`;
+  }
+
+  constructor() {
+    super();
+
+    /**
+     * Record the most recent number of characters in the input for which a
+     * search has been attempted.
+     * @private {number}
+     */
+    this.lastCharCount_ = 0;
+
+    /** @private {!HelpContentProviderInterface} */
+    this.helpContentProvider_ = getHelpContentProvider();
+
+    /** @private {!HelpContentList} */
+    this.helpContentList_ = [];
+  }
+
+  /**
+   * @private
+   */
+  handleInputChanged_(e) {
+    const newInput = e.target.value;
+    // Get the number of characters in the input.
+    const newCharCount = [...newInput].length;
+
+    if (Math.abs(newCharCount - this.lastCharCount_) >= MIN_CHARS_COUNT) {
+      this.lastCharCount_ = newCharCount;
+      this.fetchHelpContent_(newInput);
+    }
+  }
+
+  /**
+   * @param {string} query
+   * @private
+   */
+  fetchHelpContent_(query) {
+    this.helpContentProvider_.getHelpContents(query, MAX_RESULTS)
+        .then((/** !HelpContentList */ newContentList) => {
+          this.helpContentList_ = newContentList;
+        });
   }
 }
 
--- chrome/test/data/webui/chromeos/os_feedback_ui/search_page_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/search_page_test.js
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {fakeHelpContentList} from 'chrome://os-feedback/fake_data.js';
+import {FakeHelpContentProvider} from 'chrome://os-feedback/fake_help_content_provider.js';
+import {HelpContentElement} from 'chrome://os-feedback/help_content.js';
+import {setHelpContentProviderForTesting} from 'chrome://os-feedback/mojo_interface_provider.js';
 import {SearchPageElement} from 'chrome://os-feedback/search_page.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
@@ -11,13 +15,23 @@ export function searchPageTestSuite() {
   /** @type {?SearchPageElement} */
   let page = null;
 
+  /** @type {?FakeHelpContentProvider} */
+  let provider = null;
+
   setup(() => {
     document.body.innerHTML = '';
+    // Create provider.
+    provider = new FakeHelpContentProvider();
+    // Setup help contents.
+    provider.setFakeHelpContents(fakeHelpContentList);
+    // Set the fake provider.
+    setHelpContentProviderForTesting(provider);
   });
 
   teardown(() => {
     page.remove();
     page = null;
+    provider = null;
   });
 
   function initializePage() {
@@ -30,6 +44,17 @@ export function searchPageTestSuite() {
     return flushTasks();
   }
 
+  /**
+   * Find all help content items.
+   * @return {!NodeList}
+   */
+  function findAllHelpItems() {
+    const helpContentElement =
+        page.shadowRoot.querySelector('#helpContentFrame');
+    return helpContentElement.shadowRoot.querySelectorAll('.help-item a');
+  }
+
+  /** Test that expected html elements are in the page after loaded. */
   test('SearchPageLoaded', () => {
     return initializePage().then(() => {
       // Verify the title is in the page.
@@ -46,5 +71,85 @@ export function searchPageTestSuite() {
       const btnContinue = page.shadowRoot.querySelector('#btnContinue');
       assertTrue(!!btnContinue);
     });
+  });
+
+  /**
+   * Test that the text area accepts input and may fire search query to retrieve
+   * help contents.
+   * - Case 1: When number of characters entered is less than 3, search is not
+   *   triggered.
+   */
+  test('HelpContentNotPopulatedWithTwoOrLessChars', () => {
+    return initializePage()
+        .then(() => {
+          const textAreaElement =
+              page.shadowRoot.querySelector('#descriptionText');
+          assertTrue(!!textAreaElement);
+          // Verify the textarea is empty.
+          assertEquals('', textAreaElement.value);
+
+          // Verify the help content is empty.
+          assertEquals(0, findAllHelpItems().length);
+
+          // Enter two chars.
+          textAreaElement.value = 'ab';
+          // Setting the value of the textarea in code does not trigger the
+          // input event. So we trigger it here.
+          textAreaElement.dispatchEvent(new Event('input'));
+
+          return flushTasks();
+        })
+        .then(() => {
+          // Verify the help content is still empty.
+          assertEquals(0, findAllHelpItems().length);
+        });
+  });
+
+  /**
+   * Test that the text area accepts input and may fire search query to retrieve
+   * help contents.
+   * - Case 2: When number of characters newly entered is 3 or more, search is
+   *   triggered and help contents are populated.
+   */
+  test('HelpContentPopulated', () => {
+    /** {?Element} */
+    let textAreaElement = null;
+
+    return initializePage()
+        .then(() => {
+          textAreaElement = page.shadowRoot.querySelector('#descriptionText');
+          assertTrue(!!textAreaElement);
+          // Verify the textarea is empty.
+          assertEquals('', textAreaElement.value);
+
+          // Verify the help content is empty.
+          assertEquals(0, findAllHelpItems().length);
+
+          // Enter three chars.
+          textAreaElement.value = 'abc';
+          // Setting the value of the textarea in code does not trigger the
+          // input event. So we trigger it here.
+          textAreaElement.dispatchEvent(new Event('input'));
+
+          return flushTasks();
+        })
+        .then(() => {
+          // Verify the help content is populated with 5 links.
+          assertEquals(5, findAllHelpItems().length);
+
+          // Setup fake provider so to return 2 items instead of 5.
+          const newContentList = fakeHelpContentList.slice(1, 3);
+          provider.setFakeHelpContents(newContentList);
+
+          // Enter three more characters. This should trigger another search.
+          textAreaElement.value = 'abc123';
+          textAreaElement.dispatchEvent(new Event('input'));
+
+          return flushTasks();
+        })
+        .then(() => {
+          // Verify the help content is populated with 2 links.
+          assertEquals(2, findAllHelpItems().length);
+        });
   });
 }
