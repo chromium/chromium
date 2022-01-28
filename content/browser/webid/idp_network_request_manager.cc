f15ab1f70c6f9e9c9b7901a1616d73cb4b401897
Christian Biesinger <cbiesinger@chromium.org>
2022-01-28 18:59:37 +0000

    [FedCM] Actually omit credentials for uncredentialed requests
    I have verified that this does not break testing with our partner.
    
    R=kenrb@chromium.org, yigu@chromium.org
    
    Fixed: 1159177
    Change-Id: I4ca56d6eb7c9747edba337f2bd0e8862546fe8ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3424978
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964697}

--- content/browser/webid/idp_network_request_manager.cc
+++ content/browser/webid/idp_network_request_manager.cc
@@ -837,13 +837,7 @@ IdpNetworkRequestManager::CreateUncredentialedUrlLoader(
   const url::Origin& idp_origin = url::Origin::Create(provider_);
 
   resource_request->url = target_url;
-  // TODO(kenrb): credentials_mode should be kOmit, but for prototyping
-  // purposes it is useful to be able to run test IdPs on services that always
-  // require cookies. This needs to be changed back when a better solution is
-  // found or those test IdPs are no longer required.
-  // See https://crbug.com/1159177.
-  resource_request->credentials_mode =
-      network::mojom::CredentialsMode::kInclude;
+  resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->headers.SetHeader(net::HttpRequestHeaders::kAccept,
                                       kRequestBodyContentType);
   AddCsrfHeader(resource_request.get());
