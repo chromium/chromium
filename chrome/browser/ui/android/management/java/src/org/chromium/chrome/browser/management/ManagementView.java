229fdef02ef02ca70cc97fd630d686a1f2584f32
Og Astorga <ogastorga@chromium.org>
2022-01-28 01:01:14 +0000

    [Clank CBCM] Better chrome://management dynamic padding
    The chrome://management page padding is dynamic depending on the device. This CL sets the correct padding by defining two XML @dimen sizes instead of programmatically changing the size.
    
    Bug: 1290667
    Change-Id: If57db834efabe5721dc3287abd5a0c0ccf57c384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422107
    Commit-Queue: Og Astorga <ogastorga@chromium.org>
    Auto-Submit: Og Astorga <ogastorga@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964357}

--- chrome/browser/ui/android/management/java/src/org/chromium/chrome/browser/management/ManagementView.java
+++ chrome/browser/ui/android/management/java/src/org/chromium/chrome/browser/management/ManagementView.java
@@ -14,9 +14,6 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import org.chromium.base.ContextUtils;
-import org.chromium.ui.base.DeviceFormFactor;
-
 /**
  * The View that renders the ManagementPage (chrome://management).
  * Consists of an medium size image icon over title and descriptive text.
@@ -41,14 +38,6 @@ public class ManagementView extends ConstraintLayout {
         mTitle = (TextView) findViewById(R.id.title_text);
         mDescription = (TextView) findViewById(R.id.description_text);
         mLearnMore = (TextView) findViewById(R.id.learn_more);
-
-        // Tablet layout has a 48dp padding.
-        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(
-                    ContextUtils.getApplicationContext())) {
-            float pixelsPerDp = getResources().getDisplayMetrics().density;
-            int paddingPixels = Math.round(48 * pixelsPerDp);
-            this.setPadding(paddingPixels, paddingPixels, paddingPixels, paddingPixels);
-        }
 
         // Set default management status
         mIsManaged = false;
