4edf9410f7e116f0f5cc246ede8558a2a31fab45
Patrick Monette <pmonette@chromium.org>
2022-01-28 20:32:06 +0000

    Add another histogram tracking the sampling interval of IOPMPowerSource
    The previous histogram was useful to determine the precision of the
    sampling interval time delta of IOPMPowerSource but a bigger range is
    needed to figure out why the sample is in the underflow bucket 20% of
    the time.
    
    Bug: 1248057
    Change-Id: I52d1f1efd6eabaea46b7d8d1e9a8c7606164c490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414291
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964751}

--- chrome/browser/metrics/power/power_metrics_reporter.cc
+++ chrome/browser/metrics/power/power_metrics_reporter.cc
@@ -421,11 +421,19 @@ void PowerMetricsReporter::OnIOPMPowerSourceSamplingEvent() {
   // The delta is expected to be almost always 60 seconds. Split the buckets for
   // 0.2s granularity (10s interval with 50 buckets + 1 underflow bucket + 1
   // overflow bucket) around that value.
+  base::TimeDelta sampling_event_delta = now_ticks - *last_event_time_ticks_;
   base::HistogramBase* histogram = base::LinearHistogram::FactoryTimeGet(
       "Power.IOPMPowerSource.SamplingEventDelta",
       /*min=*/base::Seconds(55), /*max=*/base::Seconds(65), /*buckets=*/52,
       base::HistogramBase::kUmaTargetedHistogramFlag);
-  histogram->AddTime(now_ticks - *last_event_time_ticks_);
+  histogram->AddTime(sampling_event_delta);
   *last_event_time_ticks_ = now_ticks;
+
+  // Same as the above but using a range that starts from zero and significantly
+  // goes beyond the expected mean time of |sampling_event_delta| (which is 60
+  // seconds.).
+  base::UmaHistogramMediumTimes(
+      "Power.IOPMPowerSource.SamplingEventDelta.MediumTimes",
+      sampling_event_delta);
 }
 #endif  // BUILDFLAG(IS_MAC)
