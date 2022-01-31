3e53cdb2dc89cff01337684a110b79e29c1fecb2
Andrew Grieve <agrieve@chromium.org>
2022-01-31 15:21:29 +0000

    SuperSize: Move logic for apk other symbols archive.py -> apk.py
    No functional change. Just moving methods.
    
    Bug: None
    Change-Id: I3257b1226b4fd8ec0624f680d59b355143e3d2e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423571
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965184}

--- tools/binary_size/libsupersize/archive_util.py
+++ tools/binary_size/libsupersize/archive_util.py
@@ -1,7 +1,6 @@
 # Copyright 2022 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Logic needed by multiple archive-related modules."""
 
 import logging
