b9677d76a22cc5e67903ccc908d05354ab1de4b2
Darren Shen <shend@chromium.org>
2022-01-31 05:25:26 +0000

    sheriff: Disable flaky DevToolsTest.TestPauseWhenScriptIsRunning.
    Already disabled for Linux/ChromeOS on ASAN. Disable it even without ASAN.
    
    Bug: 1289529
    Change-Id: Ifb877928ae4f72168423e051d8d75550d339b3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426857
    Owners-Override: Darren Shen <shend@google.com>
    Reviewed-by: Patti <patricialor@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965080}

--- chrome/browser/devtools/devtools_browsertest.cc
+++ chrome/browser/devtools/devtools_browsertest.cc
@@ -1659,9 +1659,9 @@ IN_PROC_BROWSER_TEST_F(DevToolsTest, MAYBE_TestPauseWhenLoadingDevTools) {
     defined(ARCH_CPU_ARM_FAMILY)
 // Timing out on linux ARM bot: https://crbug/238453
 #define MAYBE_TestPauseWhenScriptIsRunning DISABLED_TestPauseWhenScriptIsRunning
-#elif ((BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)) && \
-       defined(MEMORY_SANITIZER))
+#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 // Timing out on Linux and ChromeOS MSan: https://crbug.com/1181692
+// Flaky failures: https://crbug/1289529
 #define MAYBE_TestPauseWhenScriptIsRunning DISABLED_TestPauseWhenScriptIsRunning
 #else
 #define MAYBE_TestPauseWhenScriptIsRunning TestPauseWhenScriptIsRunning
