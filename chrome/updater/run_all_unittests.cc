a786339c1a16c2365f8c89754f4543f81c06d030
Sorin Jianu <sorin@chromium.org>
2022-01-31 14:16:03 +0000

    Updater: increase test action and test launcher timeouts.
    The monolithic (non-component) debug build for Windows has flaky tests
    when run with the default test timeouts. The default timeouts were
    close to being marginal anyway.
    
    This CL increases the values for the test action (how long it waits
    for something to happen in the test) and test launcher action (how long
    it waits for a test to complete) when the command line does not include
    the overrides for the timeouts.
    
    Fixed: 1292041
    Change-Id: Ic35d749a6cd7a84a6d209e3c63052ad41f30b86c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426314
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965163}

--- chrome/updater/run_all_unittests.cc
+++ chrome/updater/run_all_unittests.cc
@@ -5,9 +5,12 @@
 #include <iostream>
 
 #include "base/bind.h"
+#include "base/callback_helpers.h"
+#include "base/command_line.h"
 #include "base/process/process.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "base/test/test_switches.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_paths.h"
 
@@ -43,11 +46,25 @@ void FixExecutionPriorities() {
   ::SetThreadPriority(::GetCurrentThread(), THREAD_PRIORITY_NORMAL);
 }
 
+void MaybeIncreaseTestTimeouts(int argc, char** argv) {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (!command_line->HasSwitch(switches::kTestLauncherTimeout)) {
+    command_line->AppendSwitchASCII(switches::kTestLauncherTimeout, "60000");
+  }
+  if (!command_line->HasSwitch(switches::kUiTestActionTimeout)) {
+    command_line->AppendSwitchASCII(switches::kUiTestActionTimeout, "30000");
+  }
+}
+
 }  // namespace
 
 #endif  // BUILDFLAG(IS_WIN)
 
 int main(int argc, char** argv) {
+  base::CommandLine::Init(argc, argv);
+  base::ScopedClosureRunner reset_command_line(
+      base::BindOnce(&base::CommandLine::Reset));
+
 #if BUILDFLAG(IS_WIN)
   std::cerr << "Process priority: " << base::Process::Current().GetPriority()
             << std::endl;
@@ -59,10 +76,15 @@ int main(int argc, char** argv) {
   // swarming task runs with a priority below normal.
   FixExecutionPriorities();
 
+  // Change the test timeout defaults if the command line arguments to override
+  // them are not present.
+  MaybeIncreaseTestTimeouts(argc, argv);
+
   auto scoped_com_initializer =
       std::make_unique<base::win::ScopedCOMInitializer>(
           base::win::ScopedCOMInitializer::kMTA);
 #endif
+
   base::TestSuite test_suite(argc, argv);
   chrome::RegisterPathProvider();
   return base::LaunchUnitTestsSerially(
