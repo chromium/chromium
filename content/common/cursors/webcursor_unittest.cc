38a8343085e53889eba48fcff78a6c2295927333
Mike Wasserman <msw@chromium.org>
2022-02-01 01:47:47 +0000

    Revert "Make web cursor size limits match on browser and renderer"
    This reverts commit 868b44dd8b4a1a3b9698f561ca17f75e4ec78dd2.
    
    Reason for revert: https://crbug.com/1292426
    
    Original change's description:
    > Make web cursor size limits match on browser and renderer
    >
    > Use NSCursor arrowCursor on Mac for ui::mojom::CursorType::kNull.
    > (i.e. when WebCursor is constructed with an overly large custom cursor)
    >
    > Bug: 1246188
    > Test: Automated unit tests and WPTs
    > Change-Id: I89627fa13cba96b755b8f80adbc91cfc865b6b1b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413912
    > Reviewed-by: Henrique Ferreiro <hferreiro@igalia.com>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Commit-Queue: Mike Wasserman <msw@chromium.org>
    > Auto-Submit: Mike Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#964378}
    
    Bug: 1246188
    Change-Id: Id7b3b88e65c012993537ce96c2b5064b7b76646e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428347
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965475}

--- content/common/cursors/webcursor_unittest.cc
+++ content/common/cursors/webcursor_unittest.cc
@@ -122,11 +122,11 @@ TEST(WebCursorTest, SetCursor) {
 
   // SetCursor should return false when the image width is too large.
   cursor.set_image_scale_factor(1.f);
-  cursor.set_custom_bitmap(CreateTestBitmap(129, 3));
+  cursor.set_custom_bitmap(CreateTestBitmap(1025, 3));
   EXPECT_FALSE(webcursor.SetCursor(cursor));
 
   // SetCursor should return false when the image height is too large.
-  cursor.set_custom_bitmap(CreateTestBitmap(3, 129));
+  cursor.set_custom_bitmap(CreateTestBitmap(3, 1025));
   EXPECT_FALSE(webcursor.SetCursor(cursor));
 
   // SetCursor should return false when the scaled image width is too large.
@@ -136,7 +136,7 @@ TEST(WebCursorTest, SetCursor) {
 
   // SetCursor should return false when the scaled image height is too large.
   cursor.set_image_scale_factor(0.1f);
-  cursor.set_custom_bitmap(CreateTestBitmap(5, 20));
+  cursor.set_custom_bitmap(CreateTestBitmap(5, 200));
   EXPECT_FALSE(webcursor.SetCursor(cursor));
 }
 
