218a9d9a473175ea11b8fbb81c089e04fda8f809
Anastasia Helfinstein <anastasi@google.com>
2022-02-01 23:26:13 +0000

    Convert high contrast popup code to an ES6 style class
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: 1292237
    Test: Manually inspect extension behavior.
    Change-Id: I5fce9e2da49ec4c45a61a4181fb264922b08073b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428824
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#965934}

--- ui/accessibility/extensions/highcontrast/popup.js
+++ ui/accessibility/extensions/highcontrast/popup.js
@@ -2,132 +2,146 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var site;
-var key1;
-var key2;
-Storage.initialize();
+class Popup {
+  constructor() {
+    /** @type {string} */
+    this.site;
 
-function setRadio(name, value) {
-  var radios = document.querySelectorAll('input[name="' + name + '"]');
-  for (var i = 0; i < radios.length; i++) {
-    radios[i].checked = (radios[i].value == value);
-    radios[i].disabled = !Storage.enabled;
-  }
-}
+    /** @type {string} */
+    this.key1;
 
-function update() {
-  document.body.className = Storage.enabled ? '' : 'disabled';
+    /** @type {string} */
+    this.key2;
 
-  if (Storage.enabled) {
-    $('title').innerText = chrome.i18n.getMessage('highcontrast_enabled');
-    $('toggle').innerHTML =
-        '<b>' + chrome.i18n.getMessage('highcontrast_disable') + '</b><br>' +
-        '<span class="kb">(' + key1 + ')</span>';
-    $('subcontrols').style.display = 'block';
-  } else {
-    $('title').innerText = chrome.i18n.getMessage('highcontrast_disabled');
-    $('toggle').innerHTML =
-        '<b>' + chrome.i18n.getMessage('highcontrast_enable') + '</b><br>' +
-        '<span class="kb">(' + key1 + ')</span>';
-    $('subcontrols').style.display = 'none';
+    this.init();
   }
 
-  if (site) {
-    const scheme = Storage.getSiteScheme(site);
-    setRadio('scheme', scheme);
-    $('make_default').disabled = (scheme == Storage.scheme);
-  } else {
-    setRadio('scheme', Storage.scheme);
-  }
-  if (Storage.enabled) {
-    document.documentElement.setAttribute(
-        'hc',
-        site ? 'a' + Storage.getSiteScheme(site) : 'a' + Storage.scheme);
-  } else {
-    document.documentElement.setAttribute('hc', 'a0');
-  }
-  chrome.extension.getBackgroundPage().updateTabs();
-}
-
-function onToggle() {
-  Storage.enabled = !Storage.enabled;
-  update();
-}
-
-function onForget() {
-  Storage.resetSiteSchemes();
-  update();
-}
-
-function onSchemeChange(value) {
-  if (site) {
-    Storage.setSiteScheme(site, value);
-  } else {
-    Storage.scheme = value;
-  }
-  update();
-}
-
-function onMakeDefault() {
-  Storage.scheme = Storage.getSiteScheme(site);
-  update();
-}
-
-function addRadioListeners() {
-  var radios = document.querySelectorAll('input[name="scheme"]');
-  for (var i = 0; i < radios.length; i++) {
-    radios[i].addEventListener('change', function(evt) {
-      onSchemeChange(evt.target.value);
-    }, false);
-    radios[i].addEventListener('click', function(evt) {
-      onSchemeChange(evt.target.value);
-    }, false);
-  }
-}
-
-function init() {
-  var i18nElements = document.querySelectorAll('*[i18n-content]');
-  for (var i = 0; i < i18nElements.length; i++) {
-    var elem = i18nElements[i];
-    var msg = elem.getAttribute('i18n-content');
-    elem.innerHTML = chrome.i18n.getMessage(msg);
-  }
-
-  addRadioListeners();
-  $('toggle').addEventListener('click', onToggle, false);
-  $('make_default').addEventListener('click', onMakeDefault, false);
-  $('forget').addEventListener('click', onForget, false);
-  if (navigator.appVersion.indexOf('Mac') != -1) {
-    key1 = '&#x2318;+Shift+F11';
-    key2 = '&#x2318;+Shift+F12';
-  } else {
-    key1 = 'Shift+F11';
-    key2 = 'Shift+F12';
-  }
-
-  chrome.windows.getLastFocused({'populate': true}, function(window) {
-    for (var i = 0; i < window.tabs.length; i++) {
-      var tab = window.tabs[i];
-      if (tab.active) {
-        if (isDisallowedUrl(tab.url)) {
-          $('scheme_title').innerText =
-              chrome.i18n.getMessage('highcontrast_default');
-          $('make_default').style.display = 'none';
-        } else {
-          site = siteFromUrl(tab.url);
-          $('scheme_title').innerHTML =
-              chrome.i18n.getMessage('highcontrast_',
-                  '<b>' + site + '</b>:<br>' +
-                  '<span class="kb">(' + key2 + ')</span>');
-          $('make_default').style.display = 'block';
-        }
-        update();
-        return;
-      }
+  /** @param {number} value */
+  setSchemeRadio(value) {
+    var radios = document.querySelectorAll('input[name="scheme"]');
+    for (var i = 0; i < radios.length; i++) {
+      radios[i].checked = (radios[i].value == value);
+      radios[i].disabled = !Storage.enabled;
     }
-    site = 'unknown site';
-    update();
-  });
+  }
+
+  update() {
+    document.body.className = Storage.enabled ? '' : 'disabled';
+
+    if (Storage.enabled) {
+      $('title').innerText = chrome.i18n.getMessage('highcontrast_enabled');
+      $('toggle').innerHTML = '<b>' +
+          chrome.i18n.getMessage('highcontrast_disable') + '</b><br>' +
+          '<span class="kb">(' + this.key1 + ')</span>';
+      $('subcontrols').style.display = 'block';
+    } else {
+      $('title').innerText = chrome.i18n.getMessage('highcontrast_disabled');
+      $('toggle').innerHTML = '<b>' +
+          chrome.i18n.getMessage('highcontrast_enable') + '</b><br>' +
+          '<span class="kb">(' + this.key1 + ')</span>';
+      $('subcontrols').style.display = 'none';
+    }
+
+    if (this.site) {
+      const scheme = Storage.getSiteScheme(this.site);
+      this.setSchemeRadio(scheme);
+      $('make_default').disabled = (scheme == Storage.scheme);
+    } else {
+      this.setSchemeRadio(Storage.scheme);
+    }
+    if (Storage.enabled) {
+      document.documentElement.setAttribute(
+          'hc',
+          this.site ? 'a' + Storage.getSiteScheme(this.site) :
+                      'a' + Storage.scheme);
+    } else {
+      document.documentElement.setAttribute('hc', 'a0');
+    }
+    chrome.extension.getBackgroundPage().updateTabs();
+  }
+
+  /** @param {number} value */
+  onSchemeChange(value) {
+    if (this.site) {
+      Storage.setSiteScheme(this.site, value);
+    } else {
+      Storage.scheme = value;
+    }
+  }
+
+  onToggle() {
+    Storage.enabled = !Storage.enabled;
+    this.update();
+  }
+
+  onForget() {
+    Storage.resetSiteSchemes();
+    this.update();
+  }
+
+  onMakeDefault() {
+    Storage.scheme = Storage.getSiteScheme(this.site);
+    this.update();
+  }
+
+  addRadioListeners() {
+    var radios = document.querySelectorAll('input[name="scheme"]');
+    for (var i = 0; i < radios.length; i++) {
+      radios[i].addEventListener('change', (event) => {
+        this.onSchemeChange(event.target.value);
+      }, false);
+      radios[i].addEventListener('click', (event) => {
+        this.onSchemeChange(event.target.value);
+      }, false);
+    }
+  }
+
+  init() {
+    var i18nElements = document.querySelectorAll('*[i18n-content]');
+    for (var i = 0; i < i18nElements.length; i++) {
+      var elem = i18nElements[i];
+      var msg = elem.getAttribute('i18n-content');
+      elem.innerHTML = chrome.i18n.getMessage(msg);
+    }
+
+    this.addRadioListeners();
+    $('toggle').addEventListener('click', this.onToggle.bind(this), false);
+    $('make_default')
+        .addEventListener('click', this.onMakeDefault.bind(this), false);
+    $('forget').addEventListener('click', this.onForget.bind(this), false);
+    if (navigator.appVersion.indexOf('Mac') != -1) {
+      this.key1 = '&#x2318;+Shift+F11';
+      this.key2 = '&#x2318;+Shift+F12';
+    } else {
+      this.key1 = 'Shift+F11';
+      this.key2 = 'Shift+F12';
+    }
+
+    chrome.windows.getLastFocused({'populate': true}, (window) => {
+      for (var i = 0; i < window.tabs.length; i++) {
+        var tab = window.tabs[i];
+        if (tab.active) {
+          if (isDisallowedUrl(tab.url)) {
+            $('scheme_title').innerText =
+                chrome.i18n.getMessage('highcontrast_default');
+            $('make_default').style.display = 'none';
+          } else {
+            this.site = siteFromUrl(tab.url);
+            $('scheme_title').innerHTML = chrome.i18n.getMessage(
+                'highcontrast_',
+                '<b>' + this.site + '</b>:<br>' +
+                    '<span class="kb">(' + this.key2 + ')</span>');
+            $('make_default').style.display = 'block';
+          }
+          this.update();
+          return;
+        }
+      }
+      this.site = 'unknown site';
+      this.update();
+    });
+  }
 }
 
-window.addEventListener('load', init, false);
+window.addEventListener('load', () => new Popup(), false);
+Storage.initialize();
