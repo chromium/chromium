e08e964bf29d265e0c8256ac6efe4976de0072af
Stephen Chenney <schenney@chromium.org>
2022-02-01 21:57:47 +0000

    [Paint Holding] Enable PaintHoldingCrossOrigin
    Enabling for mobile platforms.
    
    Bug: 1120345
    Change-Id: Icb0385262afe26b4a9412f44e096edcd83f55240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426650
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965886}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -60,16 +60,8 @@ const base::Feature kPaintHolding{"PaintHolding",
                                   base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enable defer commits to avoid flash of unstyled content, for all navigation.
-// Disabled on Mobile to allow for a delayed Finch roll-out. Enabled on
-// Desktop platforms.
-const base::Feature kPaintHoldingCrossOrigin {
-  "PaintHoldingCrossOrigin",
-#if BUILDFLAG(IS_ANDROID)
-      base::FEATURE_DISABLED_BY_DEFAULT
-#else
-      base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kPaintHoldingCrossOrigin{"PaintHoldingCrossOrigin",
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enable eagerly setting up a CacheStorage interface pointer and
 // passing it to service workers on startup as an optimization.
