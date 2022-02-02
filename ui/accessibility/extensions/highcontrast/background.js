0d79794bf874220fc1689bf876eada114b7d7489
Anastasia Helfinstein <anastasi@google.com>
2022-02-02 00:44:51 +0000

    Abstract out logic to perform a function for all tabs
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: 1292237
    Test: Manually inspect extension behavior.
    Change-Id: I6f1c9f6e3e046e9c8aad094a922bbd3c12da6853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428830
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#965979}

--- ui/accessibility/extensions/highcontrast/background.js
+++ ui/accessibility/extensions/highcontrast/background.js
@@ -4,42 +4,32 @@
 
 Storage.initialize();
 
-function injectContentScripts() {
+function forAllTabs(tabCallback) {
   chrome.windows.getAll({'populate': true}, function(windows) {
-    for (var i = 0; i < windows.length; i++) {
-      var tabs = windows[i].tabs;
-      for (var j = 0; j < tabs.length; j++) {
-        var url = tabs[j].url;
-        if (url.startsWith('chrome') || url.startsWith('about'))
+    for (const window of windows) {
+      for (const tab of window.tabs) {
+        if (isDisallowedUrl(tab.url)) {
           continue;
-
-        chrome.tabs.executeScript(
-            tabs[j].id,
-            {file: 'highcontrast.js', allFrames: true});
+        }
+        tabCallback(tab);
       }
     }
   });
 }
 
+function injectContentScripts() {
+  forAllTabs(tab => chrome.tabs.executeScript(
+      tab.id,
+      {file: 'highcontrast.js', allFrames: true}));
+}
+
 function updateTabs() {
-  var msg = {
-    'enabled': Storage.enabled
-  };
-  chrome.windows.getAll({'populate': true}, function(windows) {
-    for (var i = 0; i < windows.length; i++) {
-      var tabs = windows[i].tabs;
-      for (var j = 0; j < tabs.length; j++) {
-        var url = tabs[j].url;
-        if (isDisallowedUrl(url)) {
-          continue;
-        }
-        var msg = {
-          'enabled': Storage.enabled,
-          'scheme': Storage.getSiteScheme(siteFromUrl(url))
-        };
-        chrome.tabs.sendRequest(tabs[j].id, msg);
-      }
-    }
+  forAllTabs(tab => {
+    const msg = {
+      'enabled': Storage.enabled,
+      'scheme': Storage.getSiteScheme(siteFromUrl(tab.url))
+    };
+    chrome.tabs.sendRequest(tab.id, msg);
   });
 }
 
