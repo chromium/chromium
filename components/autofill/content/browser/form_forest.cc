125feb4a02d00c2285031d6794aaafb43fed73cb
Christoph Schwering <schwering@google.com>
2022-02-02 21:10:31 +0000

    [Autofill] Require shared-autofill in main frames for cross-origin fill.
    AutofillAcrossIframes supports two directions of cross-origin filling:
    - When triggered on a non-main-origin field, main-origin fields may be
      filled if they are non-sensitive.
    - When triggered on a main-origin field, non-main-origin fields may be
      filled if their frame has the "shared-autofill" feature enabled.
    
    This CL strengthens the first condition in the following way:
    - When triggered on a non-main-origin field, main-origin fields may be
      filled if
      - their frame has the "shared-autofill" feature and
      - they are are non-sensitive.
    
    With this change, "shared-autofill" is a necessary condition for
    cross-origin filling.
    
    By default, "shared-autofill" is enabled in the main frame, its
    same-origin children, their same-origin children, etc.
    
    Bug: 1293379, 1187842
    Change-Id: Ib8def16330c7a169cb7c89a9e96f3e69151b2472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3432125
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#966395}

--- components/autofill/content/browser/form_forest.cc
+++ components/autofill/content/browser/form_forest.cc
@@ -36,8 +36,7 @@
 #define AFCRASHDUMP() base::debug::DumpWithoutCrashing()
 #endif
 
-namespace autofill {
-namespace internal {
+namespace autofill::internal {
 
 namespace {
 
@@ -612,8 +611,8 @@ std::vector<FormData> FormForest::GetRendererFormsOfBrowserForm(
       };
       // Fields in frames whose permissions policy allows shared-autofill may
       // be filled if the |triggered_origin| is the main origin.
-      auto has_shared_autofill_permission = [&mutable_this](
-                                                LocalFrameToken frame_token) {
+      auto HasSharedAutofillPermission = [&mutable_this](
+                                             LocalFrameToken frame_token) {
         FrameData* frame = mutable_this.GetFrameData(frame_token);
         return frame && frame->driver && frame->driver->render_frame_host() &&
                frame->driver->render_frame_host()->IsFeatureEnabled(
@@ -626,9 +625,10 @@ std::vector<FormData> FormForest::GetRendererFormsOfBrowserForm(
           it != field_type_map.end() ? it->second : UNKNOWN_TYPE;
       return field.origin == triggered_origin ||
              (field.origin == main_origin &&
+              HasSharedAutofillPermission(renderer_form->host_frame) &&
               !IsSensitiveFieldType(field_type)) ||
              (triggered_origin == main_origin &&
-              has_shared_autofill_permission(renderer_form->host_frame));
+              HasSharedAutofillPermission(renderer_form->host_frame));
     };
 
     renderer_form->fields.push_back(browser_field);
@@ -639,5 +639,4 @@ std::vector<FormData> FormForest::GetRendererFormsOfBrowserForm(
   return renderer_forms;
 }
 
-}  // namespace internal
-}  // namespace autofill
+}  // namespace autofill::internal
