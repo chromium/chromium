839db646e7fb0a20fda6f74585c65b0fd06f7b29
Elly Fong-Jones <ellyjones@chromium.org>
2022-02-03 23:53:26 +0000

    Revert "sharing: default-enable ShareUsageRanking"
    This reverts commit 782979ca7bae823d004435bbe1920bda4ade6299.
    
    Reason for revert: did not intend to land this yet
    
    Original change's description:
    > sharing: default-enable ShareUsageRanking
    >
    > It's approved for 100% stable launch :)
    >
    > Bug: 1293916
    > Change-Id: I78c77606c738116b5d9b4bfdca1a1bbd8f09d58b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3437909
    > Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#966887}
    
    Bug: 1293916
    Change-Id: I9057ee436f6864406113b4991eb705f6733acde8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438412
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967002}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -942,7 +942,7 @@ const base::Feature kSharesheetCopyToClipboard{
 
 #if BUILDFLAG(IS_ANDROID)
 const base::Feature kShareUsageRanking{"ShareUsageRanking",
-                                       base::FEATURE_ENABLED_BY_DEFAULT};
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kShareUsageRankingFixedMore{
     "ShareUsageRankingFixedMore", base::FEATURE_ENABLED_BY_DEFAULT};
 #endif
