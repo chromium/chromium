4c53dcfdf90e61995fee56157665ae34933a8636
Hidehiko Abe <hidehiko@chromium.org>
2022-02-03 06:38:44 +0000

    Disable thread priority resetting on exec.
    There was a deadlock found so temporarily disable this.
    Note that there was another CL crrev.com/c/3412559 to do this,
    but it turned out that there are two very similar classes,
    one is used, and the other is not. The old CL disabled the unused
    one, and this CL disable the correct one.
    
    BUG=1289736
    TEST=Build.
    
    (cherry picked from commit 154c3dd49c6e5e124da07ae8512ed783e71ef53a)
    
    Change-Id: I9b44b75afe0c6dff4e6f928fccc404d95e2b9c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429339
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#965769}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435527
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#213}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -303,10 +303,14 @@ class LacrosThreadPriorityDelegate
     : public base::LaunchOptions::PreExecDelegate {
  public:
   void RunAsyncSafe() override {
+    // TODO(crbug.com/1289736): Currently, this is causing some deadlock issue.
+    // It looks like inside the function, we seem to call async unsafe API.
+    // For the mitigation, disabling this temporarily.
+    // We should revisit here, and see the impact of performance.
     // SetCurrentThreadPriority() needs file I/O on /proc and /sys.
-    base::ScopedAllowBlocking allow_blocking;
-    base::PlatformThread::SetCurrentThreadPriority(
-        base::ThreadPriority::NORMAL);
+    // base::ScopedAllowBlocking allow_blocking;
+    // base::PlatformThread::SetCurrentThreadPriority(
+    //     base::ThreadPriority::NORMAL);
   }
 };
 
