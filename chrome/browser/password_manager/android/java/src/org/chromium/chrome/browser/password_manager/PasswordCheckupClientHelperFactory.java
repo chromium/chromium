0b39bf305da75517e0de255fd72567a335034e8c
Viktor Semeniuk <vsemeniuk@google.com>
2022-02-04 13:55:37 +0000

    [UPM] Interface for PasswordCheckupClient
    Downstream implementation will implement PasswordCheckupClientHelper
    to present Password Check inside Credential Manager, start Password
    Checkup and get number of breached credentials.
    
    Bug: 1291979
    Change-Id: I2aac7d7989d0cc1b6f52e2a7aa7036ba4cd25868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420289
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#967222}

---
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordCheckupClientHelperFactory.java
@@ -0,0 +1,20 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+/**
+ * This factory returns an implementation for the helper. The factory itself is also implemented
+ * downstream.
+ */
+public abstract class PasswordCheckupClientHelperFactory {
+    /**
+     * Returns the downstream implementation provided by subclasses.
+     *
+     * @return An implementation of the {@link PasswordCheckupClientHelper} if one exists.
+     */
+    public PasswordCheckupClientHelper createHelper() {
+        return null;
+    }
+}
