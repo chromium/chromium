5d34edddbeab3d8b70b2dfee5d983d72af20aa8d
David Black <dmblack@google.com>
2022-02-04 14:45:12 +0000

    Update `smart_privacy.svg` for dark/light mode.
    Updated light and forked dark mode assets with those provided by UX.
    
    Before (light): http://shortn/_dXXlIl2DYK
    After (light):  http://shortn/_RDYjXniQX3
    Before (dark):  http://shortn/_U8sPZb6Wnm
    After (dark):   http://shortn/_8cfSV4BxIC
    
    Bug: 1293607
    Change-Id: I429201b19bc104fe47e347db6b69b8b0fbcfc4e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434100
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#967239}

--- chrome/browser/resources/settings/chromeos/os_privacy_page/smart_privacy_page.js
+++ chrome/browser/resources/settings/chromeos/os_privacy_page/smart_privacy_page.js
@@ -9,6 +9,7 @@
 
 import '//resources/cr_elements/shared_style_css.m.js';
 import '//resources/cr_elements/shared_vars_css.m.js';
+import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '../../controls/extension_controlled_indicator.js';
 import '../../settings_page/settings_animated_pages.js';
 import '../../settings_page/settings_subpage.js';
@@ -55,6 +56,14 @@ class SettingsSmartPrivacyPage extends SettingsSmartPrivacyPageBase {
         notify: true,
       },
 
+      /**
+       * Whether the smart privacy page is being rendered in dark mode.
+       * @private {boolean}
+       */
+      isDarkModeActive_: {
+        type: Boolean,
+        value: false,
+      },
 
       /**
        * Whether or not quick dim is enabled.
@@ -103,6 +112,18 @@ class SettingsSmartPrivacyPage extends SettingsSmartPrivacyPageBase {
     }
 
     this.attemptDeepLink();
+  }
+
+  /**
+   * Returns the image source based on whether the smart privacy page is being
+   * rendered in dark mode.
+   * @returns {string}
+   * @private
+   */
+  getImageSource_() {
+    return this.isDarkModeActive_ ?
+        'chrome://os-settings/images/smart_privacy_dark.svg' :
+        'chrome://os-settings/images/smart_privacy.svg';
   }
 }
 
