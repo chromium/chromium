917f042f6707fac7c578254584689a62feaa695b
Matt Jones <mdjones@chromium.org>
2022-02-07 18:13:19 +0000

    [M96-LTS] Fix possible UAF in DistillerJavascriptServiceImpl
    M96 merge issues:
      distiller_javascript_service_impl.h:
        - distiller_ui_handle_, distilled_page_prefs_ declared as
        simple raw pointers instead of raw_ptr<>
        - conflicting includes
      dom_distiller_service.h:
        - conflicting includes
    
    This patch moves away from using raw pointers in the distiller
    javascript service, instead using a weak pointer to the main distiller
    service. Methods that call into the distiller service now check the
    weak pointer to make sure it is still alive, removing the potential
    for a use-after-free bug.
    
    (cherry picked from commit ee3c9dfc50f9330c8205cd7eff97dd18774573c3)
    
    Bug: 1284916
    Change-Id: I6d2348ec4c12b4c068f5ae2efc1bd97a49e6fe3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389816
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#960510}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417138
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Jana Grill <janagrill@google.com>
    Owners-Override: Jana Grill <janagrill@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/4664@{#1460}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- components/dom_distiller/core/dom_distiller_service.h
+++ components/dom_distiller/core/dom_distiller_service.h
@@ -10,6 +10,7 @@
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "components/dom_distiller/core/article_entry.h"
 #include "components/dom_distiller/core/distilled_page_prefs.h"
 #include "components/dom_distiller/core/distiller_page.h"
@@ -85,6 +86,7 @@ class DomDistillerService : public DomDistillerServiceInterface {
       std::unique_ptr<SourcePageHandle> handle) override;
   DistilledPagePrefs* GetDistilledPagePrefs() override;
   DistillerUIHandle* GetDistillerUIHandle() override;
+  base::WeakPtr<DomDistillerService> GetWeakPtr();
 
  private:
   void CancelTask(TaskTracker* task);
@@ -111,6 +113,8 @@ class DomDistillerService : public DomDistillerServiceInterface {
 
   typedef std::vector<std::unique_ptr<TaskTracker>> TaskList;
   TaskList tasks_;
+
+  base::WeakPtrFactory<DomDistillerService> weak_ptr_factory_;
 };
 
 }  // namespace dom_distiller
