c5cc4cbdd16a70e24230396cad77b8ea0bb5e0ba
Henrik Boström <hbos@chromium.org>
2022-02-07 07:55:20 +0000

    Refactor Metronome TQ tests to make use of ::testing::MockFunction.
    What the CL description said.
    
    Bug: chromium:1288392
    Change-Id: I9f1806a8fc075763713e2c6c8c93515d095488c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435706
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967699}

--- third_party/webrtc_overrides/test/metronome_like_task_queue_test.cc
+++ third_party/webrtc_overrides/test/metronome_like_task_queue_test.cc
@@ -10,55 +10,81 @@
 #include "base/time/time.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/webrtc/api/task_queue/queued_task.h"
 #include "third_party/webrtc/api/task_queue/task_queue_factory.h"
+#include "third_party/webrtc/rtc_base/task_utils/to_queued_task.h"
 
 namespace blink {
 
 using ::testing::ElementsAre;
+using ::testing::MockFunction;
+
+namespace {
+
+std::unique_ptr<webrtc::QueuedTask> MockFunctionAsQueuedTask(
+    MockFunction<void()>& mock_function) {
+  return webrtc::ToQueuedTask(mock_function.AsStdFunction());
+}
+
+class MockCallback {
+ public:
+  MockCallback() {
+    EXPECT_CALL(callback_, Call()).WillRepeatedly([&]() { ++callback_count_; });
+  }
+
+  size_t callback_count() const { return callback_count_; }
+  bool was_called() const { return callback_count_ > 0; }
+
+  std::unique_ptr<webrtc::QueuedTask> ToQueuedTask() {
+    return MockFunctionAsQueuedTask(callback_);
+  }
+
+ private:
+  MockFunction<void()> callback_;
+  size_t callback_count_ = 0;
+};
+
+}  // namespace
 
 TEST_P(MetronomeLikeTaskQueueTest, PostTaskRunsPriorToTick) {
   auto task_queue = task_queue_factory_->CreateTaskQueue(
       "MetronomeTestQueue", webrtc::TaskQueueFactory::Priority::NORMAL);
 
-  bool did_run = false;
-  task_queue->PostTask(webrtc::ToQueuedTask([&did_run]() { did_run = true; }));
+  MockCallback callback;
+  task_queue->PostTask(callback.ToQueuedTask());
 
-  EXPECT_FALSE(did_run);
+  EXPECT_FALSE(callback.was_called());
   task_environment_.FastForwardBy(base::Nanoseconds(1));
-  EXPECT_TRUE(did_run);
+  EXPECT_TRUE(callback.was_called());
 }
 
 TEST_P(MetronomeLikeTaskQueueTest, NormalPriorityDelayedTasksRunOnTicks) {
   auto task_queue = task_queue_factory_->CreateTaskQueue(
       "MetronomeTestQueue", webrtc::TaskQueueFactory::Priority::NORMAL);
 
-  int task_run_counter = 0;
-
   // Delay task until next tick.
-  task_queue->PostDelayedTask(
-      webrtc::ToQueuedTask([&task_run_counter]() { ++task_run_counter; }),
-      metronome_tick_.InMilliseconds());
-  EXPECT_EQ(task_run_counter, 0);
+  MockCallback callback;
+  task_queue->PostDelayedTask(callback.ToQueuedTask(),
+                              metronome_tick_.InMilliseconds());
+  EXPECT_EQ(callback.callback_count(), 0u);
   task_environment_.FastForwardBy(metronome_tick_);
-  EXPECT_EQ(task_run_counter, 1);
+  EXPECT_EQ(callback.callback_count(), 1u);
 
   // Delay half a tick. A full tick must pass before it runs.
-  task_queue->PostDelayedTask(
-      webrtc::ToQueuedTask([&task_run_counter]() { ++task_run_counter; }),
-      (metronome_tick_ / 2).InMilliseconds());
+  task_queue->PostDelayedTask(callback.ToQueuedTask(),
+                              (metronome_tick_ / 2).InMilliseconds());
   task_environment_.FastForwardBy(metronome_tick_ - base::Milliseconds(1));
-  EXPECT_EQ(task_run_counter, 1);
+  EXPECT_EQ(callback.callback_count(), 1u);
   task_environment_.FastForwardBy(base::Milliseconds(1));
-  EXPECT_EQ(task_run_counter, 2);
+  EXPECT_EQ(callback.callback_count(), 2u);
 
   // Delay several ticks.
-  task_queue->PostDelayedTask(
-      webrtc::ToQueuedTask([&task_run_counter]() { ++task_run_counter; }),
-      (metronome_tick_ * 3).InMilliseconds());
+  task_queue->PostDelayedTask(callback.ToQueuedTask(),
+                              (metronome_tick_ * 3).InMilliseconds());
   task_environment_.FastForwardBy(metronome_tick_ * 2);
-  EXPECT_EQ(task_run_counter, 2);
+  EXPECT_EQ(callback.callback_count(), 2u);
   task_environment_.FastForwardBy(metronome_tick_);
-  EXPECT_EQ(task_run_counter, 3);
+  EXPECT_EQ(callback.callback_count(), 3u);
 }
 
 TEST_P(MetronomeLikeTaskQueueTest,
@@ -66,50 +92,38 @@ TEST_P(MetronomeLikeTaskQueueTest,
   auto task_queue = task_queue_factory_->CreateTaskQueue(
       "MetronomeTestQueue", webrtc::TaskQueueFactory::Priority::NORMAL);
 
-  bool did_run = false;
-  task_queue->PostDelayedHighPrecisionTask(
-      webrtc::ToQueuedTask([&did_run]() { did_run = true; }),
-      /*milliseconds=*/1);
+  MockCallback callback;
+  task_queue->PostDelayedHighPrecisionTask(callback.ToQueuedTask(),
+                                           /*milliseconds=*/1);
 
-  EXPECT_FALSE(did_run);
+  EXPECT_FALSE(callback.was_called());
   task_environment_.FastForwardBy(base::Milliseconds(1));
-  EXPECT_TRUE(did_run);
+  EXPECT_TRUE(callback.was_called());
 }
 
 TEST_P(MetronomeLikeTaskQueueTest, CanDeleteTaskQueueWhileTasksAreInFlight) {
   auto task_queue = task_queue_factory_->CreateTaskQueue(
       "MetronomeTestQueue", webrtc::TaskQueueFactory::Priority::NORMAL);
 
-  bool did_run_post_task = false;
-  task_queue->PostTask(webrtc::ToQueuedTask(
-      [&did_run_post_task]() { did_run_post_task = true; }));
-  bool did_run_low_precision_delay_task = false;
-  task_queue->PostDelayedTask(
-      webrtc::ToQueuedTask([&did_run_low_precision_delay_task]() {
-        did_run_low_precision_delay_task = true;
-      }),
-      /*milliseconds=*/1);
-  bool did_run_high_precision_delay_task = false;
+  MockCallback callback;
+  task_queue->PostTask(callback.ToQueuedTask());
+  MockFunction<void()> low_precision_callback;
+  EXPECT_CALL(low_precision_callback, Call()).Times(0);
+  task_queue->PostDelayedTask(MockFunctionAsQueuedTask(low_precision_callback),
+                              /*milliseconds=*/1);
+  MockFunction<void()> high_precision_callback;
+  EXPECT_CALL(high_precision_callback, Call()).Times(0);
   task_queue->PostDelayedHighPrecisionTask(
-      webrtc::ToQueuedTask([&did_run_high_precision_delay_task]() {
-        did_run_high_precision_delay_task = true;
-      }),
+      MockFunctionAsQueuedTask(high_precision_callback),
       /*milliseconds=*/1);
 
-  EXPECT_FALSE(did_run_post_task);
-  EXPECT_FALSE(did_run_low_precision_delay_task);
-  EXPECT_FALSE(did_run_high_precision_delay_task);
+  EXPECT_FALSE(callback.was_called());
   task_queue.reset();
-
   // The PostTask is executed, but not the delayed tasks.
-  EXPECT_TRUE(did_run_post_task);
-  EXPECT_FALSE(did_run_low_precision_delay_task);
-  EXPECT_FALSE(did_run_high_precision_delay_task);
-
-  // The delayed tasks never run because they have been cancelled.
+  EXPECT_TRUE(callback.was_called());
+  // Advance a tick. If the delayed tasks had not been cancelled, this would
+  // trigger them to run. Our EXPECT_CALLs ensure us that they do not run.
   task_environment_.FastForwardBy(metronome_tick_);
-  EXPECT_FALSE(did_run_low_precision_delay_task);
-  EXPECT_FALSE(did_run_high_precision_delay_task);
 }
 
 TEST_P(MetronomeLikeTaskQueueTest, DelayedTasksRunInOrder) {
