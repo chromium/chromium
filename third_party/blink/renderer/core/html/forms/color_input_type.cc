763af8e1bbc32e4833e2b64211c65d8aca804076
Jihwan Kim <bluewhale.marc@gmail.com>
2022-02-07 22:59:09 +0000

    Show console warning when input[type=color] is clicked without user gesture
    Bug: 1292541
    Change-Id: I547f184a6957b1c2fb2a7fd9ddc89669dd84ad36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439768
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968049}

--- third_party/blink/renderer/core/html/forms/color_input_type.cc
+++ third_party/blink/renderer/core/html/forms/color_input_type.cc
@@ -45,6 +45,7 @@
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
 #include "third_party/blink/renderer/core/html/html_div_element.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
@@ -148,8 +149,13 @@ void ColorInputType::HandleDOMActivateEvent(Event& event) {
     return;
 
   Document& document = GetElement().GetDocument();
-  if (!LocalFrame::HasTransientUserActivation(document.GetFrame()))
+  if (!LocalFrame::HasTransientUserActivation(document.GetFrame())) {
+    document.AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
+        mojom::blink::ConsoleMessageSource::kJavaScript,
+        mojom::blink::ConsoleMessageLevel::kWarning,
+        "A user gesture is required to show the color picker."));
     return;
+  }
 
   ChromeClient* chrome_client = GetChromeClient();
   if (chrome_client && !HasOpenedPopup()) {
