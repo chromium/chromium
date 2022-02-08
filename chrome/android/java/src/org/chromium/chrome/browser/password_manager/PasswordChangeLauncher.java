d229baea8bde6af5520f963ae9278b03feab7ac6
Marc Treib <treib@chromium.org>
2022-02-08 10:22:08 +0000

    PasswordChangeLauncher: URL-encode string params
    When reading these params, they are URL-*de*coded (see
    TriggerContext.getStringParameter()), so they  must be *en*coded on the
    writing side to make things work.
    Without this, e.g. usernames containing "+" didn't work.
    
    This CL is the equivalent of crrev.com/c/2846501, which did the same
    thing for the other automated-password-change trigger point
    (PasswordCheckChangePasswordHelper).
    
    Bug: 1264320
    Change-Id: I41a6743343a043096ac6ecac4a1f7335496098ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440409
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968258}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordChangeLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordChangeLauncher.java
@@ -10,6 +10,9 @@ import org.chromium.chrome.browser.autofill_assistant.TriggerContext;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.url.GURL;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /** Class for starting a password change flow in Autofill Assistant. */
 public class PasswordChangeLauncher {
     /**
@@ -26,6 +29,8 @@ public class PasswordChangeLauncher {
     private static final String DEBUG_SOCKET_ID = "DEBUG_SOCKET_ID";
     private static final int IN_CHROME_CALLER = 7;
 
+    private static final String ENCODING = "UTF-8";
+
     @CalledByNative
     public static void start(
             WindowAndroid windowAndroid, GURL origin, String username, boolean skipLogin) {
@@ -34,17 +39,26 @@ public class PasswordChangeLauncher {
 
     public static void start(WindowAndroid windowAndroid, GURL origin, String username,
             boolean skipLogin, String debugBundleId, String debutSocketId) {
-        AutofillAssistantFacade.start(windowAndroid.getActivity().get(),
-                TriggerContext.newBuilder()
-                        .addParameter(DEBUG_BUNDLE_ID, debugBundleId)
-                        .addParameter(DEBUG_SOCKET_ID, debutSocketId)
-                        .addParameter(PASSWORD_CHANGE_USERNAME_PARAMETER, username)
-                        .addParameter(PASSWORD_CHANGE_SKIP_LOGIN_PARAMETER, skipLogin)
-                        .addParameter(INTENT_PARAMETER, INTENT)
-                        .addParameter(TriggerContext.PARAMETER_START_IMMEDIATELY, true)
-                        .addParameter(TriggerContext.PARAMETER_ENABLED, true)
-                        .addParameter(TriggerContext.PARAMETER_ORIGINAL_DEEPLINK, origin.getSpec())
-                        .addParameter(TriggerContext.PARAMETER_CALLER, IN_CHROME_CALLER)
-                        .build());
+        // Note: All string-typed parameters must be URL-encoded, because the
+        // corresponding extraction logic will URL-*de*code them before use,
+        // see TriggerContext.java.
+        try {
+            AutofillAssistantFacade.start(windowAndroid.getActivity().get(),
+                    TriggerContext.newBuilder()
+                            .addParameter(DEBUG_BUNDLE_ID, debugBundleId)
+                            .addParameter(DEBUG_SOCKET_ID, debutSocketId)
+                            .addParameter(
+                                    PASSWORD_CHANGE_USERNAME_PARAMETER, URLEncoder.encode(username))
+                            .addParameter(PASSWORD_CHANGE_SKIP_LOGIN_PARAMETER, skipLogin)
+                            .addParameter(INTENT_PARAMETER, INTENT)
+                            .addParameter(TriggerContext.PARAMETER_START_IMMEDIATELY, true)
+                            .addParameter(TriggerContext.PARAMETER_ENABLED, true)
+                            .addParameter(TriggerContext.PARAMETER_ORIGINAL_DEEPLINK,
+                                    URLEncoder.encode(origin.getSpec(), ENCODING))
+                            .addParameter(TriggerContext.PARAMETER_CALLER, IN_CHROME_CALLER)
+                            .build());
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("Encoding not available.", e);
+        }
     }
 }
