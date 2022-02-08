87fd0feeebfd8a2ef3ea9d840af9b5f5679fed99
Ioana Pandele <ioanap@chromium.org>
2022-02-08 13:52:54 +0000

    [PwdSettings] Instantiate the settings accessor factory
    ... and correct a comment in CredentialManagerLauncherFactoryImpl.
    
    Bug: 1289700
    Change-Id: Id4ca6ee7667828085549e2c0eff04f0b64bb50a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420487
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968297}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordSettingsAccessorFactory.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordSettingsAccessorFactory.java
@@ -4,27 +4,11 @@
 
 package org.chromium.chrome.browser.password_manager;
 
-import static org.chromium.base.ThreadUtils.assertOnUiThread;
-
 /**
  * This factory returns an implementation for the password settings accessor. The factory itself is
  * also implemented downstream.
  */
 public abstract class PasswordSettingsAccessorFactory {
-    private static PasswordSettingsAccessorFactory sInstance;
-
-    /**
-     * Returns a settings accessor factory to be invoked whenever {@link #createAccessor()} is
-     * called. If no factory was used yet, it is created.
-     *
-     * @return The shared {@link PasswordSettingsAccessorFactory} instance.
-     */
-    public static PasswordSettingsAccessorFactory getInstance() {
-        assertOnUiThread();
-        // TODO(crbug.com/1289700): Create an instance if it doesn't exist and return it.
-        return sInstance;
-    }
-
     /**
      * Returns the downstream implementation provided by subclasses.
      *
