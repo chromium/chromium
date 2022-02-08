6b430ce0bde800078a972a906937f08e8a9f989a
Salvador Guerrero <salg@google.com>
2022-02-08 20:04:03 +0000

    [Sheriff] Disable FirstRunIntegrationTest#testAbortFirstRun
    Bug: 1295396
    Change-Id: I81f9b1aada2a92b156afc30ff64e7ffcb845d257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448611
    Quick-Run: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Owners-Override: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#968507}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -378,6 +378,7 @@ public class FirstRunIntegrationTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1295396")
     public void testAbortFirstRun() throws Exception {
         launchViewIntent(TEST_URL);
         Activity chromeLauncherActivity = waitForActivity(ChromeLauncherActivity.class);
