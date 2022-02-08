61e6cc5290b655d099933883eb9a532241f5404a
Vadym Kargin <vadym.kargin@accenture.com>
2022-02-08 00:27:24 +0000

    File System Access: Replace DOMException with V8ThrowDOMException.
    This CL is a part of chain of updates for replacing DOMException with V8ThrowDOMException.
    
    #file system access
    
    Issue: Replace DOMException with ExceptionState/V8ThrowDOMException
    Bug: 991544
    Change-Id: I63d47418902d8d75cb215311348651859c4de4ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413691
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#968104}

--- third_party/blink/renderer/modules/file_system_access/file_system_directory_handle.cc
+++ third_party/blink/renderer/modules/file_system_access/file_system_directory_handle.cc
@@ -12,6 +12,7 @@
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_manager.mojom-blink.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_file_system_directory_handle.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_file_system_get_directory_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_file_system_get_file_options.h"
@@ -92,8 +93,10 @@ ScriptPromise FileSystemDirectoryHandle::getFileHandle(
   ScriptPromise result = resolver->Promise();
 
   if (!mojo_ptr_.is_bound()) {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kInvalidStateError));
+    // TODO(crbug.com/1293949): Add an error message.
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver->GetScriptState()->GetIsolate(),
+        DOMExceptionCode::kInvalidStateError, ""));
     return result;
   }
 
@@ -129,8 +132,10 @@ ScriptPromise FileSystemDirectoryHandle::getDirectoryHandle(
   ScriptPromise result = resolver->Promise();
 
   if (!mojo_ptr_.is_bound()) {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kInvalidStateError));
+    // TODO(crbug.com/1293949): Add an error message.
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver->GetScriptState()->GetIsolate(),
+        DOMExceptionCode::kInvalidStateError, ""));
     return result;
   }
 
@@ -166,8 +171,10 @@ ScriptPromise FileSystemDirectoryHandle::removeEntry(
   ScriptPromise result = resolver->Promise();
 
   if (!mojo_ptr_.is_bound()) {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kInvalidStateError));
+    // TODO(crbug.com/1293949): Add an error message.
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver->GetScriptState()->GetIsolate(),
+        DOMExceptionCode::kInvalidStateError, ""));
     return result;
   }
 
@@ -192,8 +199,10 @@ ScriptPromise FileSystemDirectoryHandle::resolve(
   ScriptPromise result = resolver->Promise();
 
   if (!mojo_ptr_.is_bound()) {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kInvalidStateError));
+    // TODO(crbug.com/1293949): Add an error message.
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver->GetScriptState()->GetIsolate(),
+        DOMExceptionCode::kInvalidStateError, ""));
     return result;
   }
 
