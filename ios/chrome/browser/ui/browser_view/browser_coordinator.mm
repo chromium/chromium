f04ae5c467fe7ca771daa9edc666146efae81bcc
Robbie Gibson <rkgibson@google.com>
2022-02-08 22:40:42 +0000

    [iOS] Stop NonModal Promo Coordinator with other browser coordinators
    The nonModalPromoCoordinator should be stopped when the promo is
    dismissed, but sometimes this link doesn't work. To prevent crashes,
    stop the nonModalPromoCoordinator when the BrowserCoordinator is
    shutting down just in case.
    
    (cherry picked from commit fe1e0a3329b706c682a6dc4997a47da3089277ca)
    
    Bug: 1241310
    Change-Id: Ica4de8a9fde5cf2f590d5c0ff084aa81743a34c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433538
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#966838}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448738
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#366}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -570,6 +570,9 @@
 
   [self.textFragmentsCoordinator stop];
   self.textFragmentsCoordinator = nil;
+
+  [self.nonModalPromoCoordinator stop];
+  self.nonModalPromoCoordinator = nil;
 }
 
 // Starts mediators owned by this coordinator.
