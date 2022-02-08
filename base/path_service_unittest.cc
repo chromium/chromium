d8762fe66fd8de5f5c018d0c4a7a0d20ed809098
David Dorwin <ddorwin@chromium.org>
2022-02-08 00:19:48 +0000

    [fuchsia][base] Update test array size to match values
    The array size was not updated when values were removed in
    https://crrev.com/c/3188614/40/base/path_service_unittest.cc.
    
    Switch all instances to C++17 syntax to avoid this entirely.
    
    Bug: 1263691, 1184980, 1262430
    Change-Id: I4c831032a73c01b7a07c6c28e58383f0aef0d180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371470
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968099}

--- base/path_service_unittest.cc
+++ base/path_service_unittest.cc
@@ -114,7 +114,7 @@ TEST_F(PathServiceTest, Get) {
   // crbug.com/1257402). Current implementation is described before each key.
   // TODO(crbug.com/1257402): Remove the definition of these keys on Android
   // or at least fix the behavior of DIR_HOME.
-  constexpr std::array<int, 2> kUnsupportedKeys = {
+  constexpr std::array kUnsupportedKeys = {
       // Though DIR_HOME is not intended to be supported, PathProviderPosix
       // handles it and returns true. Thus, it is NOT included in the array.
       /* DIR_HOME, */
@@ -123,16 +123,16 @@ TEST_F(PathServiceTest, Get) {
       // PathProviderPosix handles it but fails at some point.
       DIR_USER_DESKTOP};
 #elif BUILDFLAG(IS_IOS)
-  constexpr std::array<int, 1> kUnsupportedKeys = {
+  constexpr std::array kUnsupportedKeys = {
       // DIR_USER_DESKTOP is not implemented on iOS. See crbug.com/1257402.
       DIR_USER_DESKTOP};
 
 #elif BUILDFLAG(IS_FUCHSIA)
-  constexpr std::array<int, 3> kUnsupportedKeys = {
+  constexpr std::array kUnsupportedKeys = {
       // TODO(crbug.com/1231928): Implement DIR_USER_DESKTOP.
       DIR_USER_DESKTOP};
 #else
-  constexpr std::array<int, 0> kUnsupportedKeys = {};
+  constexpr std::array<BasePathKey, 0> kUnsupportedKeys = {};
 #endif  // BUILDFLAG(IS_ANDROID)
   for (int key = PATH_START + 1; key < PATH_END; ++key) {
     if (std::find(kUnsupportedKeys.begin(), kUnsupportedKeys.end(), key) ==
