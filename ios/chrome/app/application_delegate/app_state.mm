7f6bd2209cad96efad1539898623770c3602f3bd
Sylvain Defresne <sdefresne@chromium.org>
2022-02-08 20:54:42 +0000

    [ios] Stop access ApplicationContext before creation
    The ApplicationContext is created when the application enter the
    InitStageBrowserObjectsForBackgroundHandlers (in MainController).
    Before that point many Chrome objects are not available and cannot
    be used.
    
    When the OS decides to garbage collect "scenes" while the app is
    not running, it sends -application:didDiscardSceneSessions: the
    next time the application is started. This message is sent
    before -sceneWillConnect: which means that the app may still not
    be in InitStageBrowserObjectsForBackgroundHandlers (or above).
    
    The ChromeIdentityService may not yet be accessible by that point
    as it depends on the ApplicationContext being created. Since the
    purpose of -application:didDiscardSceneSessions: is to allow the
    application to clean up its state tied to those sessions, it is
    fine if the deletion is delayed until the application has finished
    correctly initialized.
    
    Since observing the AppState's InitStage changing requires to be
    an AppStateObserver, put the observation in MainApplicationDelegate
    (observing AppState in AppState is an anti-pattern according to
    a comment in AppState).
    
    (cherry picked from commit 18154991c809c7e482d37ba5df0923a00941ee3a)
    
    Fixed: 1292641
    Change-Id: I75a378be3782d2da6c171dcfc926be3578382620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431645
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#966660}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448072
    Reviewed-by: Harry Souders <harrysouders@google.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Auto-Submit: Harry Souders <harrysouders@google.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#359}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -30,10 +30,9 @@
 #import "ios/chrome/app/main_application_delegate.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/browsing_data/sessions_storage_util.h"
+#include "ios/chrome/browser/browsing_data/sessions_storage_util.h"
 #include "ios/chrome/browser/chrome_constants.h"
 #include "ios/chrome/browser/crash_report/crash_helper.h"
-#import "ios/chrome/browser/crash_report/crash_keys_helper.h"
 #include "ios/chrome/browser/crash_report/crash_keys_helper.h"
 #include "ios/chrome/browser/crash_report/crash_loop_detection_util.h"
 #include "ios/chrome/browser/crash_report/features.h"
@@ -58,6 +57,7 @@
 #include "ios/public/provider/chrome/browser/app_distribution/app_distribution_api.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.h"
+#include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #import "ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.h"
 #include "ios/web/public/thread/web_task_traits.h"
 #include "net/url_request/url_request_context.h"
@@ -415,12 +415,12 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
 
 - (void)application:(UIApplication*)application
     didDiscardSceneSessions:(NSSet<UISceneSession*>*)sceneSessions {
-  NSMutableArray<NSString*>* sessionIDs =
-      [NSMutableArray arrayWithCapacity:sceneSessions.count];
-  // This method is invoked by iOS to inform the application that the sessions
-  // for "closed windows" is garbage collected and that any data associated with
-  // them by the application needs to be deleted.
-  //
+  DCHECK_GE(self.initStage, InitStageBrowserObjectsForBackgroundHandlers);
+
+  ios::GetChromeBrowserProvider()
+      .GetChromeIdentityService()
+      ->ApplicationDidDiscardSceneSessions(sceneSessions);
+
   // Usually Chrome uses -[SceneState sceneSessionID] as identifier to properly
   // support devices that do not support multi-window (and which use a constant
   // identifier). For devices that do not support multi-window the session is
@@ -430,6 +430,8 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
   // session is garbage collected.
   //
   // Thus it is always correct to use -persistentIdentifier here.
+  NSMutableArray<NSString*>* sessionIDs =
+      [NSMutableArray arrayWithCapacity:sceneSessions.count];
   for (UISceneSession* session in sceneSessions) {
     [sessionIDs addObject:session.persistentIdentifier];
   }
@@ -633,7 +635,6 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
   // Start recording info about this session.
   [[PreviousSessionInfo sharedInstance] beginRecordingCurrentSession];
 }
-
 
 #pragma mark - UIBlockerManager
 
