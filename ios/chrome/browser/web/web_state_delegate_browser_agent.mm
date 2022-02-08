63712b39e4034de932d3c64eb153079d8ed5c97c
Gauthier Ambard <gambard@chromium.org>
2022-02-08 08:28:33 +0000

    [iOS] Add DEPS disallowing browser/ui includes from browser/
    The model code is not supposed to be depend on UI code. Adding a DEPS
    to prevent additional dependencies in the future (current ones are added
    as exceptions).
    
    Bug: 1294160
    Change-Id: Icf25dc51733b306ddf2d3b4a8af788142c005be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440025
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968231}

--- ios/chrome/browser/web/web_state_delegate_browser_agent.mm
+++ ios/chrome/browser/web/web_state_delegate_browser_agent.mm
@@ -11,10 +11,9 @@
 #import "ios/chrome/browser/overlays/public/overlay_request_queue.h"
 #import "ios/chrome/browser/overlays/public/overlay_response.h"
 #import "ios/chrome/browser/overlays/public/web_content_area/http_auth_overlay.h"
-#import "ios/chrome/browser/ui/dialogs/nsurl_protection_space_util.h"
-
 #import "ios/chrome/browser/snapshots/snapshot_tab_helper.h"
 #import "ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.h"
+#import "ios/chrome/browser/ui/dialogs/nsurl_protection_space_util.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
 #import "ios/chrome/browser/url_loading/url_loading_params.h"
 #import "ios/chrome/browser/web/blocked_popup_tab_helper.h"
