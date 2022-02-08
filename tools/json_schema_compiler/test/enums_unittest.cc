9f322e9abb88d8089b249b561b5d0b6be9e03c0d
Ghazale Hosseinabadi <ghazale@google.com>
2022-02-08 22:04:10 +0000

    [Code health] Remove uses of Value::Equal
    This CL removes uses of Value::Equal in
    tools/json_schema_compiler/test/enums_unittest.cc.
    
    Bug: 1187018
    Change-Id: I729429b6b624a9945ae07b3ad641c3b5f1fc732a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426283
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968578}

--- tools/json_schema_compiler/test/enums_unittest.cc
+++ tools/json_schema_compiler/test/enums_unittest.cc
@@ -20,7 +20,7 @@ TEST(JsonSchemaCompilerEnumsTest, EnumTypePopulate) {
     value.SetString("type", "one");
     EXPECT_TRUE(enums::EnumType::Populate(value, &enum_type));
     EXPECT_EQ(enums::ENUMERATION_ONE, enum_type.type);
-    EXPECT_TRUE(value.Equals(enum_type.ToValue().get()));
+    EXPECT_EQ(value, *enum_type.ToValue());
   }
   {
     enums::EnumType enum_type;
@@ -55,11 +55,11 @@ TEST(JsonSchemaCompilerEnumsTest, EnumsAsTypes) {
 
     value.SetString("enumeration", "one");
     ASSERT_TRUE(enums::HasEnumeration::Populate(value, &enumeration));
-    EXPECT_TRUE(value.Equals(enumeration.ToValue().get()));
+    EXPECT_EQ(value, *enumeration.ToValue());
 
     value.SetString("optional_enumeration", "two");
     ASSERT_TRUE(enums::HasEnumeration::Populate(value, &enumeration));
-    EXPECT_TRUE(value.Equals(enumeration.ToValue().get()));
+    EXPECT_EQ(value, *enumeration.ToValue());
   }
   {
     enums::ReferenceEnum enumeration;
@@ -68,7 +68,7 @@ TEST(JsonSchemaCompilerEnumsTest, EnumsAsTypes) {
 
     value.SetString("reference_enum", "one");
     ASSERT_TRUE(enums::ReferenceEnum::Populate(value, &enumeration));
-    EXPECT_TRUE(value.Equals(enumeration.ToValue().get()));
+    EXPECT_EQ(value, *enumeration.ToValue());
   }
 }
 
@@ -100,7 +100,7 @@ TEST(JsonSchemaCompilerEnumsTest, ReturnsEnumCreate) {
     enums::Enumeration state = enums::ENUMERATION_ONE;
     auto result = std::make_unique<base::Value>(ToString(state));
     auto expected = std::make_unique<base::Value>("one");
-    EXPECT_TRUE(result->Equals(expected.get()));
+    EXPECT_EQ(*result, *expected);
   }
   {
     enums::Enumeration state = enums::ENUMERATION_ONE;
@@ -129,14 +129,14 @@ TEST(JsonSchemaCompilerEnumsTest, OptionalEnumTypePopulate) {
     value.SetString("type", "two");
     EXPECT_TRUE(enums::OptionalEnumType::Populate(value, &enum_type));
     EXPECT_EQ(enums::ENUMERATION_TWO, enum_type.type);
-    EXPECT_TRUE(value.Equals(enum_type.ToValue().get()));
+    EXPECT_EQ(value, *enum_type.ToValue());
   }
   {
     enums::OptionalEnumType enum_type;
     base::DictionaryValue value;
     EXPECT_TRUE(enums::OptionalEnumType::Populate(value, &enum_type));
     EXPECT_EQ(enums::ENUMERATION_NONE, enum_type.type);
-    EXPECT_TRUE(value.Equals(enum_type.ToValue().get()));
+    EXPECT_EQ(value, *enum_type.ToValue());
   }
   {
     enums::OptionalEnumType enum_type;
@@ -255,7 +255,7 @@ TEST(JsonSchemaCompilerEnumsTest, OnEnumFiredCreate) {
     enums::Enumeration some_enum = enums::ENUMERATION_ONE;
     auto result = std::make_unique<base::Value>(ToString(some_enum));
     auto expected = std::make_unique<base::Value>("one");
-    EXPECT_TRUE(result->Equals(expected.get()));
+    EXPECT_EQ(*result, *expected);
   }
   {
     enums::Enumeration some_enum = enums::ENUMERATION_ONE;
