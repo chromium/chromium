1b341ffa8795f1f2a8a9f491830e20c0499aa953
Leonid Baraz <lbaraz@chromium.org>
2022-02-08 20:40:12 +0000

    Make missive client disconnectable.
    DisconnectableClient is a wrapper on top of an abstract client which may
    become available or unavailable at any moment. Calls made through the
    client are rejected with error if the client is not available, and
    cancelled immediately returning an error, if it becomes unavailable
    while the call is in progress.
    The wrapper is used for dBus connection to missive client in Chrome.
    
    (cherry picked from commit 04b917579545768bc5b1f357f6b00ac0251c29bf)
    (cherry picked from commit 235b8680ec74d8b28bec92ddff4608fdc160989a)
    (cherry picked from commit 2a3c30b27a9a50de9cbe2bb82f3218818be77ffe)
    
    Bug: b:205814822
    Change-Id: Ia3883573ea7dea284b26c3c861bdd1f44ae37953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3444549
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#356}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- chromeos/dbus/missive/missive_client.cc
+++ chromeos/dbus/missive/missive_client.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/callback.h"
@@ -18,6 +19,7 @@
 #include "components/reporting/proto/interface.pb.h"
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/proto/synced/record_constants.pb.h"
+#include "components/reporting/util/disconnectable_client.h"
 #include "components/reporting/util/status.h"
 #include "components/reporting/util/statusor.h"
 #include "dbus/bus.h"
@@ -39,12 +41,15 @@ MissiveClient* g_instance = nullptr;
 
 class MissiveClientImpl : public MissiveClient {
  public:
-  MissiveClientImpl() = default;
+  MissiveClientImpl() : client_(origin_task_runner()) {}
   MissiveClientImpl(const MissiveClientImpl& other) = delete;
   MissiveClientImpl& operator=(const MissiveClientImpl& other) = delete;
-  ~MissiveClientImpl() override = default;
+  ~MissiveClientImpl() override {
+    client_.SetAvailability(/*is_available=*/false);
+  }
 
   void Init(dbus::Bus* const bus) {
+    DCHECK(bus);
     origin_task_runner_ = bus->GetOriginTaskRunner();
 
     DCHECK(!missive_service_proxy_);
@@ -62,65 +67,34 @@ class MissiveClientImpl : public MissiveClient {
                      base::OnceCallback<void(reporting::Status)>
                          completion_callback) override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    reporting::EnqueueRecordRequest request;
-    *request.mutable_record() = std::move(record);
-    request.set_priority(priority);
-
-    dbus::MethodCall method_call(missive::kMissiveServiceInterface,
-                                 missive::kEnqueueRecord);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendProtoAsArrayOfBytes(request);
-
-    MaybeMakeCall(
-        &method_call,
-        base::BindOnce(&MissiveClientImpl::HandleEnqueueRecordResponse,
-                       weak_ptr_factory_.GetWeakPtr(),
-                       std::move(completion_callback)));
+    auto delegate = std::make_unique<EnqueueRecordDelegate>(
+        priority, std::move(record), this, std::move(completion_callback));
+    client_.MaybeMakeCall(std::move(delegate));
   }
 
   void Flush(const reporting::Priority priority,
              base::OnceCallback<void(reporting::Status)> completion_callback)
       override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    reporting::FlushPriorityRequest request;
-    request.set_priority(priority);
-    dbus::MethodCall method_call(missive::kMissiveServiceInterface,
-                                 missive::kFlushPriority);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendProtoAsArrayOfBytes(request);
-
-    MaybeMakeCall(&method_call,
-                  base::BindOnce(&MissiveClientImpl::HandleFlushResponse,
-                                 weak_ptr_factory_.GetWeakPtr(),
-                                 std::move(completion_callback)));
+    auto delegate = std::make_unique<FlushDelegate>(
+        priority, this, std::move(completion_callback));
+    client_.MaybeMakeCall(std::move(delegate));
   }
 
   void UpdateEncryptionKey(
       const reporting::SignedEncryptionInfo& encryption_info) override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    reporting::UpdateEncryptionKeyRequest request;
-    *request.mutable_signed_encryption_info() = std::move(encryption_info);
-    dbus::MethodCall method_call(missive::kMissiveServiceInterface,
-                                 missive::kUpdateEncryptionKey);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendProtoAsArrayOfBytes(request);
-
-    MaybeMakeCall(&method_call, base::DoNothing());
-    return;
+    auto delegate =
+        std::make_unique<UpdateEncryptionKeyDelegate>(encryption_info, this);
+    client_.MaybeMakeCall(std::move(delegate));
   }
 
   void ReportSuccess(const reporting::SequenceInformation& sequence_information,
                      bool force_confirm) override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    reporting::ConfirmRecordUploadRequest request;
-    *request.mutable_sequence_information() = std::move(sequence_information);
-    request.set_force_confirm(force_confirm);
-    dbus::MethodCall method_call(missive::kMissiveServiceInterface,
-                                 missive::kConfirmRecordUpload);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendProtoAsArrayOfBytes(request);
-
-    MaybeMakeCall(&method_call, base::DoNothing());
+    auto delegate = std::make_unique<ReportSuccessDelegate>(
+        sequence_information, force_confirm, this);
+    client_.MaybeMakeCall(std::move(delegate));
   }
 
   MissiveClient::TestInterface* GetTestInterface() override { return nullptr; }
@@ -130,96 +104,208 @@ class MissiveClientImpl : public MissiveClient {
   }
 
  private:
-  void HandleEnqueueRecordResponse(
-      base::OnceCallback<void(reporting::Status)> completion_callback,
-      reporting::StatusOr<dbus::Response*> response_or_status) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
+  // Class implements DisconnectableClient::Delegate specifically for dBus
+  // calls. Logic that handles dBus connect/disconnect cases remains with the
+  // base class.
+  class DBusDelegate : public reporting::DisconnectableClient::Delegate {
+   public:
+    DBusDelegate(const DBusDelegate& other) = delete;
+    DBusDelegate& operator=(const DBusDelegate& other) = delete;
+    ~DBusDelegate() override = default;
 
-    if (!response_or_status.ok()) {
-      std::move(completion_callback).Run(response_or_status.status());
-      return;
+    // Writes request into dBus message writer.
+    virtual bool WriteRequest(dbus::MessageWriter* writer) = 0;
+
+    // Parses response, retrieves status information from it and returns it.
+    // Optional - returns OK if absent.
+    virtual reporting::Status ParseResponse(dbus::MessageReader* reader) {
+      return reporting::Status::StatusOK();
     }
 
-    dbus::MessageReader reader(response_or_status.ValueOrDie());
-    reporting::EnqueueRecordResponse response_body;
-    reader.PopArrayOfBytesAsProto(&response_body);
+   protected:
+    DBusDelegate(
+        const char* dbus_method,
+        MissiveClientImpl* owner,
+        base::OnceCallback<void(reporting::Status)> completion_callback)
+        : dbus_method_(dbus_method),
+          owner_(owner),
+          completion_callback_(std::move(completion_callback)) {}
 
-    reporting::Status status;
-    status.RestoreFrom(response_body.status());
-    std::move(completion_callback).Run(status);
-  }
+   private:
+    // Implementation of DisconnectableClient::Delegate
+    void DoCall(base::OnceClosure cb) final {
+      DCHECK_CALLED_ON_VALID_SEQUENCE(owner_->origin_checker_);
+      dbus::MethodCall method_call(missive::kMissiveServiceInterface,
+                                   dbus_method_);
+      dbus::MessageWriter writer(&method_call);
+      if (!WriteRequest(&writer)) {
+        reporting::Status status(
+            reporting::error::UNKNOWN,
+            "MessageWriter was unable to append the request.");
+        LOG(ERROR) << status;
+        std::move(completion_callback_).Run(status);
+        return;
+      }
 
-  void HandleFlushResponse(
-      base::OnceCallback<void(reporting::Status)> completion_callback,
-      reporting::StatusOr<dbus::Response*> response_or_status) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-
-    if (!response_or_status.ok()) {
-      std::move(completion_callback).Run(response_or_status.status());
-      return;
+      // Make a dBus call.
+      owner_->missive_service_proxy_->CallMethod(
+          &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+          base::BindOnce(
+              [](base::OnceClosure cb, base::WeakPtr<DBusDelegate> self,
+                 dbus::Response* response) {
+                if (!self) {
+                  return;  // Delegate already deleted.
+                }
+                DCHECK_CALLED_ON_VALID_SEQUENCE(self->owner_->origin_checker_);
+                if (!response) {
+                  self->Respond(Status(reporting::error::UNAVAILABLE,
+                                       "Returned no response"));
+                  return;
+                }
+                self->response_ = response;
+                std::move(cb).Run();
+              },
+              std::move(cb), weak_ptr_factory_.GetWeakPtr()));
     }
 
-    dbus::MessageReader reader(response_or_status.ValueOrDie());
-    reporting::FlushPriorityResponse response_body;
-    reader.PopArrayOfBytesAsProto(&response_body);
-
-    reporting::Status status;
-    status.RestoreFrom(response_body.status());
-    std::move(completion_callback).Run(status);
-  }
-
-  void MaybeMakeCall(
-      dbus::MethodCall* call,
-      base::OnceCallback<void(reporting::StatusOr<dbus::Response*>)>
-          completion_callback) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    // Bail out, if missive daemon is not available over dBus.
-    if (!is_available_) {
-      std::move(completion_callback)
-          .Run(Status(reporting::error::UNAVAILABLE,
-                      "Missive is not available"));
-      return;
+    // Process dBus response, if status is OK, or error otherwise.
+    void Respond(reporting::Status status) final {
+      DCHECK_CALLED_ON_VALID_SEQUENCE(owner_->origin_checker_);
+      if (status.ok()) {
+        dbus::MessageReader reader(response_);
+        status = ParseResponse(&reader);
+      }
+      DCHECK(completion_callback_);
+      std::move(completion_callback_).Run(status);
     }
-    // Make a dBus call.
-    missive_service_proxy_->CallMethod(
-        call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-        base::BindOnce(&MissiveClientImpl::CallResponded,
-                       weak_ptr_factory_.GetWeakPtr(),
-                       std::move(completion_callback)));
-  }
 
-  void CallResponded(
-      base::OnceCallback<void(reporting::StatusOr<dbus::Response*>)>
-          completion_callback,
-      dbus::Response* response) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    if (!response) {
-      std::move(completion_callback)
-          .Run(Status(reporting::error::UNAVAILABLE,
-                      "Missive has not responded"));
-      return;
+    const char* const dbus_method_;
+    dbus::Response* response_;
+    MissiveClientImpl* const owner_;
+    base::OnceCallback<void(reporting::Status)> completion_callback_;
+
+    // Weak pointer factory - must be last member of the class.
+    base::WeakPtrFactory<DBusDelegate> weak_ptr_factory_{this};
+  };
+
+  class EnqueueRecordDelegate : public DBusDelegate {
+   public:
+    EnqueueRecordDelegate(
+        reporting::Priority priority,
+        reporting::Record record,
+        MissiveClientImpl* owner,
+        base::OnceCallback<void(reporting::Status)> completion_callback)
+        : DBusDelegate(missive::kEnqueueRecord,
+                       owner,
+                       std::move(completion_callback)) {
+      *request_.mutable_record() = std::move(record);
+      request_.set_priority(priority);
     }
-    std::move(completion_callback).Run(response);
-  }
+
+    bool WriteRequest(dbus::MessageWriter* writer) override {
+      return writer->AppendProtoAsArrayOfBytes(request_);
+    }
+
+    reporting::Status ParseResponse(dbus::MessageReader* reader) override {
+      reporting::EnqueueRecordResponse response_body;
+      if (!reader->PopArrayOfBytesAsProto(&response_body)) {
+        return reporting::Status(reporting::error::INTERNAL,
+                                 "Response was not parsable.");
+      }
+      reporting::Status status;
+      status.RestoreFrom(response_body.status());
+      return status;
+    }
+
+   private:
+    reporting::EnqueueRecordRequest request_;
+  };
+
+  class FlushDelegate : public DBusDelegate {
+   public:
+    FlushDelegate(
+        reporting::Priority priority,
+        MissiveClientImpl* owner,
+        base::OnceCallback<void(reporting::Status)> completion_callback)
+        : DBusDelegate(missive::kFlushPriority,
+                       owner,
+                       std::move(completion_callback)) {
+      request_.set_priority(priority);
+    }
+
+    bool WriteRequest(dbus::MessageWriter* writer) override {
+      return writer->AppendProtoAsArrayOfBytes(request_);
+    }
+
+    reporting::Status ParseResponse(dbus::MessageReader* reader) override {
+      reporting::FlushPriorityResponse response_body;
+      if (!reader->PopArrayOfBytesAsProto(&response_body)) {
+        return reporting::Status(reporting::error::INTERNAL,
+                                 "Response was not parsable.");
+      }
+      reporting::Status status;
+      status.RestoreFrom(response_body.status());
+      return status;
+    }
+
+   private:
+    reporting::FlushPriorityRequest request_;
+  };
+
+  class UpdateEncryptionKeyDelegate : public DBusDelegate {
+   public:
+    UpdateEncryptionKeyDelegate(
+        const reporting::SignedEncryptionInfo& encryption_info,
+        MissiveClientImpl* owner)
+        : DBusDelegate(missive::kUpdateEncryptionKey,
+                       owner,
+                       base::DoNothing()) {
+      *request_.mutable_signed_encryption_info() = encryption_info;
+    }
+
+    bool WriteRequest(dbus::MessageWriter* writer) override {
+      return writer->AppendProtoAsArrayOfBytes(request_);
+    }
+
+   private:
+    reporting::UpdateEncryptionKeyRequest request_;
+  };
+
+  class ReportSuccessDelegate : public DBusDelegate {
+   public:
+    ReportSuccessDelegate(
+        const reporting::SequenceInformation& sequence_information,
+        bool force_confirm,
+        MissiveClientImpl* owner)
+        : DBusDelegate(missive::kConfirmRecordUpload,
+                       owner,
+                       base::DoNothing()) {
+      *request_.mutable_sequence_information() = sequence_information;
+      request_.set_force_confirm(force_confirm);
+    }
+
+    bool WriteRequest(dbus::MessageWriter* writer) override {
+      return writer->AppendProtoAsArrayOfBytes(request_);
+    }
+
+   private:
+    reporting::ConfirmRecordUploadRequest request_;
+  };
 
   void OwnerChanged(const std::string& old_owner,
                     const std::string& new_owner) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    is_available_ = !new_owner.empty();
-    LOG(WARNING) << missive::kMissiveServiceInterface
-                 << " changed owner, is_available=" << is_available_;
+    client_.SetAvailability(/*is_available=*/!new_owner.empty());
   }
 
   void ServerAvailable(bool service_is_available) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(origin_checker_);
-    is_available_ = service_is_available;
-    LOG(WARNING) << missive::kMissiveServiceInterface
-                 << " became available, is_available=" << is_available_;
+    client_.SetAvailability(/*is_available=*/service_is_available);
   }
 
   scoped_refptr<dbus::ObjectProxy> missive_service_proxy_;
 
-  bool is_available_{false};
+  reporting::DisconnectableClient client_;
 
   // Weak pointer factory - must be last member of the class.
   base::WeakPtrFactory<MissiveClientImpl> weak_ptr_factory_{this};
---
+++ components/reporting/util/disconnectable_client.cc
@@ -0,0 +1,77 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/reporting/util/disconnectable_client.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/callback.h"
+#include "base/containers/fixed_flat_map.h"
+#include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/rand_util.h"
+#include "base/sequence_checker.h"
+#include "base/task/sequenced_task_runner.h"
+#include "components/reporting/util/status.h"
+
+namespace reporting {
+
+DisconnectableClient::DisconnectableClient(
+    scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : task_runner_(task_runner) {}
+
+DisconnectableClient::~DisconnectableClient() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  SetAvailability(/*is_available=*/false);
+}
+
+void DisconnectableClient::MaybeMakeCall(std::unique_ptr<Delegate> delegate) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // Bail out, if missive daemon is not available over dBus.
+  if (!is_available_) {
+    delegate->Respond(
+        Status(reporting::error::UNAVAILABLE, "Service is unavailable"));
+    return;
+  }
+  // Add the delegate to the map.
+  const auto id = base::RandUint64();
+  auto res = outstanding_delegates_.emplace(id, std::move(delegate));
+  DCHECK(res.second) << "Duplicate call id " << id;
+  // Make a call, resume on CallResponded, when response is received.
+  res.first->second->DoCall(base::BindOnce(&DisconnectableClient::CallResponded,
+                                           weak_ptr_factory_.GetWeakPtr(), id));
+}
+
+void DisconnectableClient::CallResponded(uint64_t id) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  auto it = outstanding_delegates_.find(id);
+  if (it == outstanding_delegates_.end()) {
+    // Callback has already been removed, no action needed.
+    return;
+  }
+  // Respond through the |delegate|.
+  auto delegate = std::move(it->second);
+  outstanding_delegates_.erase(it);
+  // Respond.
+  delegate->Respond(Status::StatusOK());
+}
+
+void DisconnectableClient::SetAvailability(bool is_available) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  is_available_ = is_available;
+  LOG(WARNING) << "Service became " << (is_available_ ? "" : "un")
+               << "available";
+  if (!is_available_) {
+    // Cancel all pending calls.
+    for (auto& p : outstanding_delegates_) {
+      std::move(p.second)->Respond(
+          Status(reporting::error::UNAVAILABLE, "Service is unavailable"));
+    }
+    outstanding_delegates_.clear();
+  }
+}
+
+}  // namespace reporting
