510b7a5bd1f883f7e4f6f4b90f7bd8d2daa51735
Lindsay Pasricha <lindsayw@google.com>
2022-02-09 15:39:26 +0000

    Disabling testDownloadUsdz and testVisibilitychangeEventFired
    on device since it fails on iOS15 device.
    
    Bug: 1060374
    Change-Id: Ib4361e256bda7d43e416be6973e77ca49d567209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447908
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968930}

--- ios/chrome/browser/ui/download/ar_quick_look_egtest.mm
+++ ios/chrome/browser/ui/download/ar_quick_look_egtest.mm
@@ -90,7 +90,13 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 }
 
 // Tests that QLPreviewController is shown for sucessfully downloaded USDZ file.
-- (void)testDownloadUsdz {
+// TODO(crbug.com/1060374): Test fails on iOS15 device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testDownloadUsdz DISABLED_testDownloadUsdz
+#else
+#define MAYBE_testDownloadUsdz testDownloadUsdz
+#endif
+- (void)MAYBE_testDownloadUsdz {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Good"];
   [ChromeEarlGrey tapWebStateElementWithID:@"good"];
@@ -170,7 +176,14 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 
 // Tests that the visibilitychange event is fired when quicklook is
 // shown/hidden.
-- (void)testVisibilitychangeEventFired {
+// TODO(crbug.com/1060374): Test fails on iOS15 device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testVisibilitychangeEventFired \
+  DISABLED_testVisibilitychangeEventFired
+#else
+#define MAYBE_testVisibilitychangeEventFired testVisibilitychangeEventFired
+#endif
+- (void)MAYBE_testVisibilitychangeEventFired {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Good"];
   [ChromeEarlGrey tapWebStateElementWithID:@"good"];
