ebc7a36d55e6dea0be2ca76db1bff6f616c6afd5
Stefan Mitic <stefan.mitic@nutanix.com>
2022-02-09 20:00:53 +0000

    Reland "Enable support for WebRTC H.264 YUV444 decoding."
    This is a reland of 5ae9244412a6501f19b6b8a914284d139355b48e
    
    Original change's description:
    > Enable support for WebRTC H.264 YUV444 decoding.
    >
    > WebRTC now supports H.264 YUV444 decoding. This change adds code to
    > Blink rtc_vide_decoder_factory.cc to enable that feature for use in
    > Chromium.
    >
    > Bug: 1251096
    > Change-Id: Ib801d1acf3b3679692016899a648dccf103e5856
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3398199
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#960755}
    
    Bug: 1251096
    Change-Id: Idedf2cd06484a761275a83d7f038345233809ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449222
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969067}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_factory.cc
@@ -44,7 +44,7 @@ struct CodecConfig {
   media::VideoCodecProfile profile;
 };
 
-constexpr std::array<CodecConfig, 8> kCodecConfigs = {{
+constexpr std::array<CodecConfig, 9> kCodecConfigs = {{
     {media::VideoCodec::kVP8, media::VP8PROFILE_ANY},
     {media::VideoCodec::kVP9, media::VP9PROFILE_PROFILE0},
     {media::VideoCodec::kVP9, media::VP9PROFILE_PROFILE1},
@@ -52,6 +52,7 @@ constexpr std::array<CodecConfig, 8> kCodecConfigs = {{
     {media::VideoCodec::kH264, media::H264PROFILE_BASELINE},
     {media::VideoCodec::kH264, media::H264PROFILE_MAIN},
     {media::VideoCodec::kH264, media::H264PROFILE_HIGH},
+    {media::VideoCodec::kH264, media::H264PROFILE_HIGH444PREDICTIVEPROFILE},
     {media::VideoCodec::kAV1, media::AV1PROFILE_PROFILE_MAIN},
 }};
 
@@ -95,6 +96,9 @@ absl::optional<webrtc::SdpVideoFormat> VdcToWebRtcFormat(
           break;
         case media::H264PROFILE_HIGH:
           h264_profile = webrtc::H264Profile::kProfileHigh;
+          break;
+        case media::H264PROFILE_HIGH444PREDICTIVEPROFILE:
+          h264_profile = webrtc::H264Profile::kProfilePredictiveHigh444;
           break;
         default:
           // Unsupported H264 profile in WebRTC.
