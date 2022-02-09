f7913c12a4cd2e0873ed85f1d7a6565016b4a756
Irem Uguz <iremuguz@google.com>
2022-02-09 10:39:49 +0000

    support-tool: Migrate Support Tool UI to Typescript from Javascript
    As a follow-up to comment in cl/3172793 and the announcement about
    requiring Typescript in new WebUI (see the attached bug for more
    details), migrate Support Tool UI components to Typescript.
    
    "--enable-features=SupportTool" to the /etc/chrome_dev.conf file and go
    "out/Default/browser_tests --gtest_filter=*SupportTool*" to run browser
    tests.
    
    Bug: b:210633043
    Test: Turn on SupportTool feature flag by adding
    chrome: //support-tool ui for manual testing. Run
    Change-Id: I207a1f0bcfd8692decf17d926bac87c007b3fa43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440028
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#968840}

--- chrome/browser/resources/support_tool/support_tool.ts
+++ chrome/browser/resources/support_tool/support_tool.ts
@@ -4,40 +4,31 @@
  */
 
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
-import '//resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import {isChromeOS} from 'chrome://resources/js/cr.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-/** @polymer */
 export class SupportToolElement extends PolymerElement {
   static get is() {
     return 'support-tool';
   }
 
+  // TODO(crbug.com/1292025): Use html_to_wrapper() for getting the template.
   static get template() {
     return html`{__html_template__}`;
   }
 
   static get properties() {
     return {
-      collectBrowserLogs_: {
-        type: Boolean,
-        value: true,
-      },
-      collectChromeOSLogs_: {
-        type: Boolean,
-        value() {
-          return isChromeOS;
-        },
-      },
-      hideChromeOS_: {
-        type: Boolean,
-        value() {
-          return !isChromeOS;
-        },
-      }
+      collectBrowserLogs_: Boolean,
+      collectChromeOSLogs_: Boolean,
+      hideChromeOS_: Boolean,
     };
   }
+
+  private collectBrowserLogs_: boolean = true;
+  private collectChromeOSLogs_: boolean = isChromeOS;
+  private hideChromeOS_: boolean = !isChromeOS;
 }
 
 customElements.define(SupportToolElement.is, SupportToolElement);
