fcc2fafa0a179355a3d99ab88a8e97ecc665a416
Sylvain Defresne <sdefresne@chromium.org>
2022-02-09 19:22:55 +0000

    [swift] Fix depfile generation
    The depfile generated by swiftc uses absolute paths (like most
    tools shipping with Xcode). The ninja rules generated by gn on
    the other hand uses paths relative to the build directory.
    
    The use of both absolute and relative paths causes ninja to not
    realise that the input of a .swift module from the depfile are
    the output of another target (ninja appears to use compare the
    string verbatim, and to not convert them to normalize them as
    paths).
    
    Change the swiftc.py wrapper script to convert the paths from
    absolute to relative (for all path below //, path pointing out
    of the checkout are left as absolute since they cannot be the
    output of a build rule).
    
    The ninja documentation recommends using relative paths:
    > If the include directory directives are using absolute paths,
    > your depfile may result in a mixture of relative and absolute
    > paths. Paths used by other build rules need to match exactly.
    > Therefore, it is recommended to use relative paths in these
    > cases.
    
    Fixed: 1287114
    Change-Id: I84be701cc322e801086fc4b0e2f4df57d2a4c6a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450658
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969045}

--- build/toolchain/ios/swiftc.py
+++ build/toolchain/ios/swiftc.py
@@ -3,17 +3,11 @@
 # found in the LICENSE file.
 
 import argparse
-import collections
 import json
 import os
 import subprocess
 import sys
 import tempfile
-
-
-class OrderedSet(collections.OrderedDict):
-  def add(self, value):
-    self[value] = True
 
 
 def compile_module(module, sources, settings, extras, tmpdir):
@@ -136,7 +130,20 @@ def compile_module(module, sources, settings, extras, tmpdir):
   if process.returncode:
     sys.exit(process.returncode)
 
-  depfile_content = collections.OrderedDict()
+  # The swiftc compiler generates depfile that uses absolute paths, but
+  # ninja requires paths in depfiles to be identical to paths used in
+  # the build.ninja files.
+  #
+  # Since gn generates paths relative to the build directory for all paths
+  # below the repository checkout, we need to convert those to relative
+  # paths.
+  #
+  # See https://crbug.com/1287114 for build failure that happen when the
+  # paths in the depfile are kept absolute.
+  out_dir = os.getcwd() + os.path.sep
+  src_dir = os.path.abspath(settings.root_dir) + os.path.sep
+
+  depfile_content = dict()
   for key in output_file_map:
 
     # When whole module optimisation is disabled, there will be an entry
@@ -155,15 +162,20 @@ def compile_module(module, sources, settings, extras, tmpdir):
       else:
         key = os.path.splitext(settings.module_path)[0] + ext
       if key not in depfile_content:
-        depfile_content[key] = OrderedSet()
+        depfile_content[key] = set()
       for path in inputs.split():
+        if path.startswith(src_dir) or path.startswith(out_dir):
+          path = os.path.relpath(path, out_dir)
         depfile_content[key].add(path)
 
+  if not settings.depfile_filter:
+    keys = depfile_content.keys()
+  else:
+    keys = (key for key in settings.depfile_filter if key in depfile_content)
+
   with open(settings.depfile, 'w') as depfile:
-    for key in depfile_content:
-      if not settings.depfile_filter or key in settings.depfile_filter:
-        inputs = depfile_content[key]
-        depfile.write('%s : %s\n' % (key, ' '.join(inputs)))
+    for key in sorted(keys):
+      depfile.write('%s : %s\n' % (key, ' '.join(sorted(depfile_content[key]))))
 
 
 def main(args):
@@ -208,6 +220,11 @@ def main(args):
                       dest='system_framework_dirs',
                       action='append',
                       help='add dir to system framework search path')
+  parser.add_argument('-root-dir',
+                      dest='root_dir',
+                      action='store',
+                      required=True,
+                      help='path to the root of the repository')
 
   parsed, extras = parser.parse_known_args(args)
   with tempfile.TemporaryDirectory() as tmpdir:
