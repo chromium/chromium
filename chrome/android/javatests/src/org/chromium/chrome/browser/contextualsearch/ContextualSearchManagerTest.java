ed782dfb84f457fbb6fd87903216a70fd6293991
Peter Kotwicz <pkotwicz@chromium.org>
2022-02-09 22:02:45 +0000

    Revert "[TTS] Reenable testQuickActionIntent"
    This reverts commit be1478d44c2291b15e405ac085c7fdf561d783aa.
    
    Reason for revert: Test is still flaky on
    android-arm64-tests bot
    
    Sample failing test run:
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests/5267/overview
    
    The failure is due to tap selecting "consumes" instead of the intended
    selection.
    
    BUG=1294351
    
    Original change's description:
    > [TTS] Reenable testQuickActionIntent
    >
    > Speculative reenable of the above flaky test.
    >
    > BUG=
    >
    > Change-Id: Ibed2ebea60a7e913172ff4a1ba582ce31dfb76bc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422817
    > Reviewed-by: Gang Wu <gangwu@chromium.org>
    > Commit-Queue: Donn Denman <donnd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#964946}
    
    Bug: 
    Change-Id: I900fec15d04eae3b2d157ee594ec0ab5a025102a
    Owners-Override: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450173
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Owners-Override: Peter Kotwicz <pkotwicz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969132}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -3339,7 +3339,7 @@ public class ContextualSearchManagerTest {
     @SmallTest
     @Feature({"ContextualSearch"})
     @ParameterAnnotations.UseMethodParameter(FeatureParamProvider.class)
-    // Previously flaky and disabled 4/2021.  https://crbug.com/1192285
+    @FlakyTest(message = "Disabled 4/2021.  https://crbug.com/1192285")
     public void testQuickActionIntent(@EnabledFeature int enabledFeature) throws Exception {
         // Add a new filter to the activity monitor that matches the intent that should be fired.
         IntentFilter quickActionFilter = new IntentFilter(Intent.ACTION_VIEW);
