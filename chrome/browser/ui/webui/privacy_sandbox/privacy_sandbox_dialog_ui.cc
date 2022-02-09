693e92579f5104800dc9bfd797447aa8eddb7e03
Olesia Marukhno <olesiamarukhno@google.com>
2022-02-09 14:00:43 +0000

    [privacy sandbox strings] Add strings placeholders
    Add strings placeholders for the privacy sandbox dialog. The strings are
    not final yet and are marked as translatable="false". They will be
    updated in a separate CL with actual strings.
    
    Bug: 1286276
    Change-Id: I2bc751fde202dd322a40b94402027bbbb10f51ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450111
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#968890}

--- chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_ui.cc
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_ui.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/privacy_sandbox_resources.h"
 #include "chrome/grit/privacy_sandbox_resources_map.h"
 #include "content/public/browser/web_ui.h"
@@ -24,6 +25,42 @@ PrivacySandboxDialogUI::PrivacySandboxDialogUI(content::WebUI* web_ui)
       source,
       base::make_span(kPrivacySandboxResources, kPrivacySandboxResourcesSize),
       IDR_PRIVACY_SANDBOX_PRIVACY_SANDBOX_DIALOG_HTML);
+
+  static constexpr webui::LocalizedString kStrings[] = {
+      {"title", IDS_PRIVACY_SANDBOX_DIALOG_TITLE},
+      {"subtitle", IDS_PRIVACY_SANDBOX_DIALOG_SUBTITLE},
+      {"bodyHeader1", IDS_PRIVACY_SANDBOX_DIALOG_BODY_HEADER_1},
+      {"bodyDescription1", IDS_PRIVACY_SANDBOX_DIALOG_BODY_DESCRIPTION_1},
+      {"bodyHeader2", IDS_PRIVACY_SANDBOX_DIALOG_BODY_HEADER_2},
+      {"bodyDescription2", IDS_PRIVACY_SANDBOX_DIALOG_BODY_DESCRIPTION_2},
+      {"learnMoreLabel", IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_LABEL},
+      {"bottomSummary", IDS_PRIVACY_SANDBOX_DIALOG_BOTTOM_SUMMARY},
+      {"learnMoreSection1Header",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_1_HEADER},
+      {"learnMoreSection1BulletPoint1",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_1_BULLET_POINT_1},
+      {"learnMoreSection1BulletPoint2",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_1_BULLET_POINT_2},
+      {"learnMoreSection1BulletPoint3",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_1_BULLET_POINT_3},
+      {"learnMoreSection2Header",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_2_HEADER},
+      {"learnMoreSection2BulletPoint1",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_2_BULLET_POINT_1},
+      {"learnMoreSection2BulletPoint2",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_2_BULLET_POINT_2},
+      {"learnMoreSection2BulletPoint3",
+       IDS_PRIVACY_SANDBOX_DIALOG_LEARN_MORE_SECTION_2_BULLET_POINT_3},
+      {"consentAcceptButton", IDS_PRIVACY_SANDBOX_DIALOG_CONSENT_ACCEPT_BUTTON},
+      {"consentDeclineButton",
+       IDS_PRIVACY_SANDBOX_DIALOG_CONSENT_DECLINE_BUTTON},
+      {"noticeAcknowledgeButton",
+       IDS_PRIVACY_SANDBOX_DIALOG_NOTICE_ACKNOWLEDGE_BUTTON},
+      {"noticeOpenSettingsButton",
+       IDS_PRIVACY_SANDBOX_DIALOG_NOTICE_OPEN_SETTINGS_BUTTON},
+  };
+
+  source->AddLocalizedStrings(kStrings);
 
   content::WebUIDataSource::Add(Profile::FromWebUI(web_ui), source);
 }
