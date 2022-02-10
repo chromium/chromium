b9f91eefe8267b9bfb7628a4d4080dd5814bb03a
Colin Blundell <blundell@chromium.org>
2022-02-10 09:10:19 +0000

    Make FullscreenManagerTest robust against startup race condition
    FullscreenManagerTest#testHidingBrowserControlsPreservesScrollOffset
    does the following:
    
    - Loads a page that listens for an onResize event and verifies that an
      expected scroll has occurred
    - Initiates the scroll
    - Waits for the page to receive an onResize event and asserts that the
      page has verified that the scroll state is as expected
    
    However, this flow is subject to a subtle race:
    
    - At startup, browser-side initialization of compositor state sends the
      renderer messages to update visual properties, which result in resize
      events.
    - Navigation commit sends a navigation commit event to the renderer,
      which then commits the new document.
    
    The initial navigation commit event is not ordered with respect to the
    browser-side initialization of compositor state:
    
    - The former event is driven by the flow of the navigation itself (e.g.,
      for a navigation that goes to the network, the initial navigation
      commit occurs on network response).
    - The latter event is triggered by a task that is posted as part of
      Clank’s Java startup.
    
    If the initial navigation commit event is received first by the
    renderer, the resize events from browser-side initialization of
    compositor state will be associated with the document that was loaded
    as part of the commit. If that happens in the context of this test, the
    test will fail.
    
    Any resize events thus triggered are triggered before browser startup
    is complete. To make the test robust against this race, this CL changes
    it to instruct the test webpage to start listening for onResize only
    after browser startup has completed (this is straightforward to do
    because launchShellWithUrl() blocks until browser startup is complete).
    As the renderer will process this message after processing the IPC
    that causes onResize events during browser startup, we guarantee that
    the test webpage will be listening for the onResize that should occur
    as part of the scroll that is subsequently initiated by the test.
    
    This race condition happens to not occur in the production codebase,
    but is triggered by an ongoing experiment.  Specifically, the test
    fails at ToT if "--enable-features=TreatBootstrapAsDefault" is passed,
    and it succeeds with that parameter if this patch is applied (passed
    100 iterations in a local run, whereas it previously reliably failed).
    
    Bug: 1280192, 1258621
    Change-Id: Iace0aaec8dbb96e291199f80a5ecd83122a3bc8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387328
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969352}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -145,12 +145,16 @@ public class FullscreenManagerTest {
             + "  <div style='width: 150vw'>wide</div>"
             + "  <script>"
             + "    load_promise = new Promise(r => {onload = r});"
-            + "    resize_promise = new Promise(r => {onresize = r});"
+            + "    resize_promise = null;"
             + "    reached_bottom = () => {"
             + "      return Math.abs("
             + "        (se => se.scrollHeight - (se.scrollTop + visualViewport.offsetTop +"
             + "          visualViewport.height))(document.scrollingElement)"
             + "      ) < 1;"
+            + "    };"
+            + "    start_listening_for_on_resize = () => {"
+            + "      resize_promise = new Promise(r => {onresize = r});"
+            + "      return true;"
             + "    };"
             + "  </script>"
             + "</body>"
@@ -433,15 +437,26 @@ public class FullscreenManagerTest {
         mActivityTestRule.startMainActivityWithURL(SCROLL_OFFSET_TEST_PAGE);
 
         ChromeActivity activity = mActivityTestRule.getActivity();
-        BrowserControlsManager browserControlsManager = activity.getBrowserControlsManager();
+        WebContents webContents = activity.getActivityTab().getWebContents();
 
+        // Browser startup generates resize events as part of compositor initialization. Depending
+        // on the relative timing of that initialization and the initial navigation, the test page
+        // may receive these onResize events. To ensure that the test page's onResize handler
+        // triggers only for the fling we initiate below, we tell the test page to start listening
+        // for onResize only now that browser startup has fully completed (note that
+        // startMainActivityWithURL() waits for full browser initialization before returning, and
+        // hence the renderer's processing of the earlier resize events will be ordered before its
+        // reception of the message sent below).
+        JavaScriptUtils.runJavascriptWithAsyncResult(
+                webContents, "domAutomationController.send(start_listening_for_on_resize());");
+
+        BrowserControlsManager browserControlsManager = activity.getBrowserControlsManager();
         CriteriaHelper.pollUiThread(
                 () -> { return browserControlsManager.getTopControlOffset() == 0f; });
 
         Point displaySize = new Point();
         activity.getWindowManager().getDefaultDisplay().getSize(displaySize);
 
-        WebContents webContents = activity.getActivityTab().getWebContents();
         FullscreenManagerTestUtils.waitForPageToBeScrollable(activity.getActivityTab());
 
         JavaScriptUtils.runJavascriptWithAsyncResult(
