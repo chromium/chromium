f4c76e8f1b15782a59e5c3a3fa56ad630bb99115
Ewann <ewannpv@google.com>
2022-02-11 10:50:15 +0000

    [iOS][MediaPermissions][5/N] Adds infobar UI
    This CL opens the permissions infobar modal when the 'Edit' button from
    the banner is tapped.
    This CL adds:
    - PermissionsInfobarInteractionHandler
    - PermissionsInfobarBannerInteractionHandler
    
    Bug: 1289645
    Change-Id: I16526de88d10783a11f29a9a28bf649ec50c6d34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452150
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969883}

--- ios/chrome/browser/overlays/public/infobar_modal/permissions/permissions_modal_overlay_request_config.mm
+++ ios/chrome/browser/overlays/public/infobar_modal/permissions/permissions_modal_overlay_request_config.mm
@@ -5,6 +5,7 @@
 #import "ios/chrome/browser/overlays/public/infobar_modal/permissions/permissions_modal_overlay_request_config.h"
 
 #include "ios/chrome/browser/infobars/infobar_ios.h"
+#import "ios/chrome/browser/infobars/overlays/permissions_overlay_infobar_delegate.h"
 #import "ios/chrome/browser/overlays/public/common/infobars/infobar_overlay_request_config.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -17,8 +18,9 @@ PermissionsInfobarModalOverlayRequestConfig::
     PermissionsInfobarModalOverlayRequestConfig(InfoBarIOS* infobar)
     : infobar_(infobar) {
   DCHECK(infobar_);
-  // TODO(crbug.com/1289645): Retrieve the current webstate from the permissions
-  // delegate once implemented.
+  PermissionsOverlayInfobarDelegate* delegate =
+      static_cast<PermissionsOverlayInfobarDelegate*>(infobar_->delegate());
+  web_state_ = delegate->GetWebState();
 }
 
 PermissionsInfobarModalOverlayRequestConfig::
