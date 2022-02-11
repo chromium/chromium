9092f423848ab3f120a06a88b8fb9b2ff0cdb6c5
Mehryar Keshavarz <mehryar@google.com>
2022-02-11 16:19:15 +0000

    change browser switcher internal page to render with polymer
    Bug: 1258133
    Change-Id: I4dbd1993206fb10b8b62ba40344d5b76dd3f2d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448941
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Mehryar Keshavarz <mehryar@google.com>
    Cr-Commit-Position: refs/heads/main@{#969964}

--- chrome/browser/resources/browser_switch/internals/app.ts
+++ chrome/browser/resources/browser_switch/internals/app.ts
@@ -7,9 +7,9 @@ import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 
 import {addWebUIListener} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {dom, html, PolymerElement,} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {BrowserSwitchInternalsProxy, BrowserSwitchInternalsProxyImpl, Decision, RuleSet, RuleSetList, RulesetSources, TimestampPair} from './browser_switch_internals_proxy.js';
+import {BrowserSwitchInternalsProxy, BrowserSwitchInternalsProxyImpl, Decision, RuleSet, RuleSetList, RulesetSources, TimestampPair,} from './browser_switch_internals_proxy.js';
 
 type ListName = 'sitelist'|'greylist';
 
@@ -34,12 +34,57 @@ class BrowserSwitchInternalsAppElement extends
       showSearch_: {
         type: Boolean,
         value: false,
-      }
+      },
+      lastFetch_: {
+        type: String,
+        value: '',
+      },
+      nextFetch_: {
+        type: String,
+        value: '',
+      },
+      urlCheckerInput_: {
+        type: String,
+        observer: 'checkUrl_',
+      },
+      urlCheckerOutput_: {
+        type: Array,
+        value: [],
+      },
+      browserName_: {
+        type: String,
+        value: getBrowserName,
+      },
+      altBrowserName_: {
+        type: String,
+        value: getAltBrowserName,
+      },
+      greyListRules_: {
+        type: Array,
+        value: [],
+      },
+      siteListRules_: {
+        type: Array,
+        value: [],
+      },
+      xmlSiteLists_: {
+        type: Array,
+        value: [],
+      },
     };
   }
 
   private isBrowserSwitcherEnabled_: boolean;
   private showSearch_: boolean;
+  private browserName_: string;
+  private altBrowserName_: string;
+  private greyListRules_: object[];
+  private siteListRules_: object[];
+  private xmlSiteLists_: object[];
+  private urlCheckerInput_: string;
+  private urlCheckerOutput_: string[];
+  private lastFetch_: string;
+  private nextFetch_: string;
 
   /** @override */
   ready() {
@@ -48,81 +93,40 @@ class BrowserSwitchInternalsAppElement extends
     this.updateEverything();
 
     document.addEventListener('DOMContentLoaded', () => {
-      this.generateStaticContent();
       addWebUIListener('data-changed', () => this.updateEverything());
     });
   }
 
-  /**
-   * Clears the table, and inserts a header row.
-   * @param headerTemplate Template to use to re-create the header row.
-   */
-  clearTable(table: HTMLTableElement, headerTemplate: HTMLTemplateElement) {
-    table.innerHTML = '';
-    const headerRow = document.importNode(headerTemplate.content, true);
-    table.appendChild(headerRow);
+  getRuleBrowserName(rule: string) {
+    return rule.startsWith('!') ? getBrowserName() : getAltBrowserName();
   }
 
-  /**
-   * Creates and returns a <tr> element for the given rule.
-   */
-  createRowForRule(rule: string, rulesetName: string, listType: ListName):
-      HTMLTableRowElement {
-    const templateName = `rule-row-template-${listType}`;
-    const row = document.importNode(
-                    (this.$[templateName] as HTMLTemplateElement).content,
-                    true) as unknown as HTMLTableRowElement;
-    const cells = row.querySelectorAll('td');
-    cells[0].innerText = rule;
-    cells[0].className = 'url';
-    if (listType === 'sitelist') {
-      cells[1].innerText =
-          rule.startsWith('!') ? getBrowserName() : getAltBrowserName();
-      cells[2].appendChild(this.createRulesetColumnWithTooltip(rulesetName));
-    } else if (listType === 'greylist') {
-      cells[1].appendChild(this.createRulesetColumnWithTooltip(rulesetName));
-    }
-
-    return row;
-  }
-
-  createRulesetColumnWithTooltip(rulesetName: string): HTMLSpanElement {
-    const textSpan = document.createElement('span');
-    textSpan.className = 'tooltip';
-    textSpan.innerText = rulesetName;
-    const tooltipSpan = document.createElement('span');
-    tooltipSpan.className = 'right';
+  getPolicyFromRuleset(ruleSetName: string) {
     const rulesetToPolicy: Record<string, string> = {
-      'gpo': 'BrowserSwitcherUrlList',
-      'ieem': 'BrowserSwitcherUseIeSitelist',
-      'external_sitelist': 'BrowserSwitcherExternalSitelistUrl',
-      'external_greylist': 'BrowserSwitcherExternalGreylistUrl',
+      gpo: 'BrowserSwitcherUrlList',
+      ieem: 'BrowserSwitcherUseIeSitelist',
+      external_sitelist: 'BrowserSwitcherExternalSitelistUrl',
+      external_greylist: 'BrowserSwitcherExternalGreylistUrl',
     };
-    tooltipSpan.innerText = rulesetToPolicy[rulesetName];
-    textSpan.appendChild(tooltipSpan);
-    return textSpan;
+    return rulesetToPolicy[ruleSetName];
   }
-
   /**
    * Updates the content of all tables after receiving data from the backend.
    */
   updateTables(rulesets: RuleSetList) {
-    const siteListHeaderTemplate =
-        this.$['header-row-template-sitelist'] as HTMLTemplateElement;
-    const greyListHeaderTemplate =
-        this.$['header-row-template-greylist'] as HTMLTemplateElement;
-    this.clearTable(
-        this.$['sitelist'] as HTMLTableElement, siteListHeaderTemplate);
-    this.clearTable(
-        this.$['greylist'] as HTMLTableElement, greyListHeaderTemplate);
+    const listNameToProperty: Record<string, string> = {
+      sitelist: 'siteListRules_',
+      greylist: 'greyListRules_',
+    };
+    this.siteListRules_ = [];
+    this.greyListRules_ = [];
 
     for (const [rulesetName, ruleset] of Object.entries(rulesets)) {
       for (const [listName, rules] of Object.entries(ruleset as RuleSet)) {
-        const table = this.$[listName];
-        for (const rule of rules) {
-          table.appendChild(
-              this.createRowForRule(rule, rulesetName, listName as ListName));
-        }
+        this.push(listNameToProperty[listName], ...rules.map((rule) => ({
+                                                               rulesetName,
+                                                               rule,
+                                                             })));
       }
     }
   }
@@ -178,28 +182,28 @@ class BrowserSwitchInternalsAppElement extends
     return [opensIn, reason];
   }
 
-  checkUrl() {
-    let url = (this.$['url-checker-input'] as HTMLInputElement).value;
+  checkUrl_(url: string) {
     if (!url) {
-      (this.$['output'] as HTMLUListElement).innerText = '';
+      this.urlCheckerOutput_ = [];
       return;
     }
+
     if (!url.includes('://')) {
       url = 'http://' + url;
     }
+
     getProxy()
         .getDecision(url)
         .then((decision) => {
           // URL is valid.
-          const [output, reason] = this.urlOutputText(decision);
-          (this.$['output'] as HTMLUListElement).innerText = output;
-          (this.$['reason'] as HTMLUListElement).innerText = reason;
+          this.urlCheckerOutput_ = this.urlOutputText(decision);
         })
         .catch((errorMessage) => {
           // URL is invalid.
           console.warn(errorMessage);
-          (this.$['output'] as HTMLUListElement).innerText =
-              'Invalid URL. Make sure it is formatted properly.';
+          this.urlCheckerOutput_ = [
+            'Invalid URL. Make sure it is formatted properly.',
+          ];
         });
   }
 
@@ -214,69 +218,26 @@ class BrowserSwitchInternalsAppElement extends
     if (!timestamps) {
       return;
     }
+    const {last_fetch, next_fetch} = timestamps;
 
-    const lastFetch = new Date(timestamps.last_fetch);
-    const nextFetch = new Date(timestamps.next_fetch);
-
-    if (lastFetch.valueOf() == 0) {
-      // Not fetched yet.
-      (this.$['xml-not-fetched-yet'] as HTMLParagraphElement).style.display =
-          'block';
-      (this.$['xml-last-fetch'] as HTMLParagraphElement).style.display = 'none';
-    } else {
-      // Already fetched.
-      (this.$['xml-not-fetched-yet'] as HTMLParagraphElement).style.display =
-          'none';
-      (this.$['xml-last-fetch'] as HTMLParagraphElement).style.display =
-          'block';
-    }
-
-    (this.$['xml-next-fetch'] as HTMLParagraphElement).style.display =
-        nextFetch.valueOf() ? 'block' : 'none';
-
-    (this.$['last-fetch-placeholder'] as HTMLSpanElement).innerText =
-        formatTime(lastFetch);
-    (this.$['next-fetch-placeholder'] as HTMLSpanElement).innerText =
-        formatTime(nextFetch);
+    this.lastFetch_ = last_fetch !== 0 ? formatTime(last_fetch) : '';
+    this.nextFetch_ = next_fetch !== 0 ? formatTime(next_fetch) : '';
   }
 
   /**
    * Update the table under the "XML sitelists" section.
    */
-  updateXmlTable({browser_switcher: sources}: RulesetSources) {
-    const headerTemplate =
-        this.$['xml-header-row-template'] as HTMLTemplateElement;
-    this.clearTable(
-        this.$['xml-sitelists'] as HTMLTableElement, headerTemplate);
-
-    for (const [prefName, url] of Object.entries(sources)) {
-      // Hacky: guess the policy name from the pref name by converting 'foo_bar'
-      // to 'BrowserSwitcherFooBar'. This relies on prefs having the same name
-      // as the associated policy.
-      const policyName =
-          'BrowserSwitcher' + snakeCaseToUpperCamelCase(prefName);
-      const row = document.importNode(
-          (this.$['xml-row-template'] as HTMLTemplateElement).content, true);
-      const cells = row.querySelectorAll('td');
-      cells[0].innerText = policyName;
-      cells[1].innerText = url || '(not configured)';
-      cells[1].className = 'url';
-      this.$['xml-sitelists'].appendChild(row);
-    }
-
-    // Hide/show the description paragraphs depending on whether any XML
-    // sitelist is configured.
-    const enabled = Object.values(sources).some(x => !!x);
-    (this.$['xml-description-wrapper'] as HTMLDivElement).style.display =
-        enabled ? 'block' : 'none';
-  }
-
-  // TODO(crbug.com/1258133): Make `browserName' and `altBrowserName' Polymer
-  // properties, and replace this method with HTML.
-  generateStaticContent() {
-    (this.$['greylist-description'] as HTMLParagraphElement).innerText =
-        `URLs matching these rules won't trigger a browser switch and can be open in either ${
-            getBrowserName()} or ${getAltBrowserName()}.`;
+  updateXmlTable(rulesetSources: RulesetSources) {
+    this.xmlSiteLists_ = [];
+    this.push(
+        'xmlSiteLists_',
+        ...Object.entries(rulesetSources)
+            .map(([prefName, url]) => ({
+                   // Hacky name guessing
+                   policyName: 'BrowserSwitcher' +
+                       snakeCaseToUpperCamelCase(prefName.split('.')[1]),
+                   url: url || '(not configured)',
+                 })));
   }
 
   /**
@@ -292,7 +253,6 @@ class BrowserSwitchInternalsAppElement extends
           (timestamps) => this.updateTimestamps(timestamps));
       getProxy().getRulesetSources().then(
           (sources) => this.updateXmlTable(sources));
-      this.checkUrl();
     }
   }
 }
@@ -315,7 +275,8 @@ function snakeCaseToUpperCamelCase(symbol: string): string {
 /**
  * Formats |date| as "HH:MM:SS".
  */
-function formatTime(date: Date): string {
+function formatTime(dateNumber: number): string {
+  const date = new Date(dateNumber);
   const hh = date.getHours().toString().padStart(2, '0');
   const mm = date.getMinutes().toString().padStart(2, '0');
   const ss = date.getSeconds().toString().padStart(2, '0');
