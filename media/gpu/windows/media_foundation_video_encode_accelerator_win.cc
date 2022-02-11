faedf8e25396c5bff06cfad7a8b1c2dc32017408
Rahul Singh <rahsin@microsoft.com>
2022-02-11 18:43:15 +0000

    [M98] Revert "Enable constrained baseline profile for H.264 HW encode."
    This reverts commit 40c17c7be606275ccde514b2b074859246752f7a.
    
    Reason for revert: With CBP enabled for HWMFT, we are seeing abnormally
    high key frame generation rate (50 to 300 frames per minute). In
    addition, resolution doesn't scale up past 360p. The expectation is
    720p. Reverting this commit which enabled CBP for H.264 HW Encode to
    mitigate the issue.
    
    Bug: 1295815
    
    Original change's description:
    > Enable constrained baseline profile for H.264 HW encode.
    >
    > This patch enables hardware H.264 CBP encoding on Windows,
    > and it may have problems on other platforms, like Mac in
    > https://crbug.com/1088650. Also, Nvidia RTX 2060 series are
    > temporarily disabled until they provide fix to it.
    >
    > Bug: 982799, 1088650
    > Change-Id: I0463edf30f3d7152a3dc0f8d0b41aede872efa85
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268587
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Chunbo Hua <chunbo.hua@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#944358}
    
    (cherry picked from commit e686a30ccd2c31fc3ff32d480e955aa6bf7d57f0)
    
    Bug: 982799, 1088650
    Change-Id: I9d94d6290e6a186c0a445f8d277dcdb4c42bbd6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450338
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/main@{#969265}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453867
    Reviewed-by: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4758@{#1148}
    Cr-Branched-From: 4a2cf4baf90326df19c3ee70ff987960d59a386e-refs/heads/main@{#950365}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -354,8 +354,7 @@ bool MediaFoundationVideoEncodeAccelerator::Initialize(const Config& config,
     return false;
   }
 
-  if (!ActivateAsyncEncoder(pp_activate, encoder_count,
-                            config.is_constrained_h264)) {
+  if (!ActivateAsyncEncoder(pp_activate, encoder_count)) {
     DLOG(ERROR) << "Failed activating an async hardware encoder MFT.";
 
     if (pp_activate) {
@@ -618,8 +617,7 @@ uint32_t MediaFoundationVideoEncodeAccelerator::EnumerateHardwareEncoders(
 
 bool MediaFoundationVideoEncodeAccelerator::ActivateAsyncEncoder(
     IMFActivate** pp_activate,
-    uint32_t encoder_count,
-    bool is_constrained_h264) {
+    uint32_t encoder_count) {
   DVLOG(3) << __func__;
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
@@ -633,19 +631,6 @@ bool MediaFoundationVideoEncodeAccelerator::ActivateAsyncEncoder(
       if (encoder_.Get() != nullptr) {
         DCHECK(SUCCEEDED(hr));
         auto vendor = GetDriverVendor(pp_activate[i]);
-
-        // Skip NVIDIA GPU due to https://crbug.com/1088650 for constrained
-        // baseline profile H.264 encoding, and go to the next instance
-        // according to merit value.
-        if (vendor == DriverVendor::kNvidia && codec_ == VideoCodec::kH264 &&
-            is_constrained_h264) {
-          DLOG(WARNING)
-              << "Skipped NVIDIA GPU due to https://crbug.com/1088650";
-          pp_activate[i]->ShutdownObject();
-          encoder_.Reset();
-          hr = E_FAIL;
-          continue;
-        }
 
         activate_ = pp_activate[i];
         vendor_ = vendor;
