5ecff2ec8b2d47de5891d2a8cde43564de851df0
Nancy Wang <nancylingwang@chromium.org>
2022-02-11 00:09:28 +0000

    Add the kAppServiceOnAppTypeInitializedWithoutMojom flag.
    kAppServiceOnAppTypeInitializedWithoutMojom will be used to control
    whether uses the non mojom App data for the OnAppTypeInitialized
    interface in CL:3451875.
    
    Add features files in components/services/app_service directory,
    because the flag is used in components/services/app_service, which
    can't access chrome/ directory.
    
    BUG=1253250
    
    Change-Id: Ia27ad5a69afe155f0703fbef0fb4915e806004e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447872
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969705}

---
+++ components/services/app_service/public/cpp/features.cc
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/services/app_service/public/cpp/features.h"
+
+namespace apps {
+
+const base::Feature kAppServiceOnAppTypeInitializedWithoutMojom{
+    "AppServiceOnAppTypeInitializedWithoutMojom",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace apps
