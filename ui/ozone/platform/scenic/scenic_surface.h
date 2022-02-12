02544b90f5a9fb9f1b1f2c779c76f68ac5544680
Sergey Ulanov <sergeyu@chromium.org>
2022-02-12 00:23:57 +0000

    [Fuchsia] Hide ImagePipe when the view is detached from the scene
    Previously ScenicSurface was pushing an empty image to the ImagePipe to
    ensure that the outdated content is not displayed when the view is
    re-added to the scene. That approach didn't work reliably because
    compositor may push new frames after the empty frame.
    This change implements a different solution: ImagePipe is detached from
    the view until the surface is ready to present a new frame.
    
    Bug: 1280802
    Change-Id: I616c6250682f3aeff43287f7b2cdbf2278574c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456844
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970240}

--- ui/ozone/platform/scenic/scenic_surface.h
+++ ui/ozone/platform/scenic/scenic_surface.h
@@ -141,8 +141,6 @@ class ScenicSurface : public PlatformWindowSurface {
   void OnPresentComplete(fuchsia::images::PresentationInfo presentation_info);
   void UpdateViewHolderScene();
 
-  void PresentEmptyImage();
-
   scenic::Session scenic_session_;
   std::unique_ptr<scenic::View> parent_;
 
@@ -176,6 +174,7 @@ class ScenicSurface : public PlatformWindowSurface {
   std::vector<zx::event> release_fences_from_last_present_;
 
   // Scenic resources used for the primary plane, that is not an overlay.
+  scenic::EntityNode root_node_;
   scenic::ShapeNode main_shape_;
   scenic::Material main_material_;
   gfx::SizeF main_shape_size_;
@@ -183,6 +182,8 @@ class ScenicSurface : public PlatformWindowSurface {
   ScenicSurfaceFactory* const scenic_surface_factory_;
   SysmemBufferManager* const sysmem_buffer_manager_;
   const gfx::AcceleratedWidget window_;
+
+  bool attach_root_on_present_ = false;
 
   struct OverlayViewInfo {
     OverlayViewInfo(scenic::ViewHolder holder, scenic::EntityNode node);
