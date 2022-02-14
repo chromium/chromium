ecc5a01949532d297119bd6d32712841200a0c74
Peng Huang <penghuang@chromium.org>
2022-02-14 19:10:16 +0000

    Remove glGetError() from GLImageEGL::BindTexImage() for release build
    Bug: 1296585
    Change-Id: I2eecf116b92498b447a773acd4df19d9b47f404f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462328
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970746}

--- ui/gl/gl_image_egl.cc
+++ ui/gl/gl_image_egl.cc
@@ -4,10 +4,6 @@
 
 #include "ui/gl/gl_image_egl.h"
 
-#include "base/metrics/histogram_macros.h"
-#include "base/time/time.h"
-#include "base/timer/elapsed_timer.h"
-#include "base/timer/timer.h"
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_enums.h"
@@ -56,19 +52,8 @@ bool GLImageEGL::BindTexImage(unsigned target) {
   DCHECK_EQ(BIND, ShouldBindOrCopy());
 
   glEGLImageTargetTexture2DOES(target, egl_image_);
-  base::ElapsedTimer thread_blocked_timer;
-  const GLenum error = glGetError();
-  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
-      "Gpu.GL.GetErrorDuration.GLImageEGL.BindTexImage",
-      thread_blocked_timer.Elapsed(), base::Microseconds(10),
-      base::Milliseconds(16), 100);
-
-  DLOG_IF(ERROR, error != GL_NO_ERROR)
-      << "Error binding EGLImage: " << GLEnums::GetStringError(error);
-
-  UMA_HISTOGRAM_BOOLEAN("Gpu.GL.GetErrorResult.GLImageEGL.BindTexImage",
-                        error != GL_NO_ERROR);
-  return error == GL_NO_ERROR;
+  DCHECK_EQ(static_cast<GLenum>(GL_NO_ERROR), glGetError());
+  return true;
 }
 
 }  // namespace gl
