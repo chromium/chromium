a4cbe3149ec7258c9b3104511c3157d2a05ecb58
Scott Violet <sky@chromium.org>
2022-02-14 21:39:48 +0000

    cc: ensure only decrease tile memory priority
    If gpu-rasterization is enabled LayerTreeHostImpl may
    change the memory priority. This changes it so that the
    priority is only decreased, not increased. The interesting
    case is if the policy is CUTOFF_ALLOW_REQUIRED_ONLY, then
    the policy should not be increased to CUTOFF_ALLOW_NICE_TO_HAVE.
    
    BUG=1289362
    
    Change-Id: If427c35b4ca124c0f730dd17647fb849bd2b4454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457504
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970843}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -3521,10 +3521,13 @@ void LayerTreeHostImpl::SetNeedsRedraw() {
 
 ManagedMemoryPolicy LayerTreeHostImpl::ActualManagedMemoryPolicy() const {
   ManagedMemoryPolicy actual = cached_managed_memory_policy_;
+  // The following may lower the cutoff, but should never raise it.
   if (debug_state_.rasterize_only_visible_content) {
     actual.priority_cutoff_when_visible =
         gpu::MemoryAllocation::CUTOFF_ALLOW_REQUIRED_ONLY;
-  } else if (use_gpu_rasterization()) {
+  } else if (use_gpu_rasterization() &&
+             actual.priority_cutoff_when_visible ==
+                 gpu::MemoryAllocation::CUTOFF_ALLOW_EVERYTHING) {
     actual.priority_cutoff_when_visible =
         gpu::MemoryAllocation::CUTOFF_ALLOW_NICE_TO_HAVE;
   }
