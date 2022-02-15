e1d22b35f2079e90d4c583f6ac6fd033d256cc48
Chuong Ho <hdchuong@google.com>
2022-02-15 05:51:16 +0000

    emoji-picker: add trie structure for search
    New search extension needs trie to store/retrieve keywords quickly.
    There is some difficulty importing `trie` from `third_party` libraries.
    
    This CL builds simplified version of trie to suit emoji-picker's needs.
    
    Bug: b/218934057
    Demo: None. Please follow unit tests to see what output is.
    Change-Id: I07171adc1bf989685b83dc0a176e952dc57394b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3446869
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#971063}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_browsertest.js
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_browsertest.js
@@ -70,3 +70,21 @@ var EmojiPickerExtensionBrowserTest = class extends PolymerTest {
 TEST_F('EmojiPickerExtensionBrowserTest', 'All', function() {
   mocha.run();
 });
+
+var EmojiPickerTrieTest = class extends PolymerTest {
+  /** @override */
+  get featureList() {
+    return {
+      enabled: ['chromeos::features::kImeSystemEmojiPickerSearchExtension']
+    };
+  }
+  /** @override */
+  get browsePreload() {
+    return 'chrome://emoji-picker/test_loader.html?module=' +
+        'chromeos/emoji_picker/emoji_picker_trie_test.js';
+  }
+};
+
+TEST_F('EmojiPickerTrieTest', 'All', function() {
+  mocha.run();
+});
