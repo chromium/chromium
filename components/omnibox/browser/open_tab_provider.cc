2ca01ba5ebf507b8c61728c37dac511b19774441
Rachel Wong <wrong@chromium.org>
2022-02-15 05:25:26 +0000

    [open tabs] Add a basic filter for open tab matches.
    This CL performs a basic filter via exact substring matching on the
    input and open tab titles. All matches are kept and there is no scoring
    yet.
    
    For the MVP launcher open tab search, these results will be further
    refined and scored in the launcher using the chromeos
    TokenizedStringMatch library. Further iterations will perform proper
    matching and scoring into the OpenTabProvider here.
    
    Bug: 1293702
    Change-Id: I2846f898c61edf0593e254fbea3533a48abc4dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3454324
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971054}

--- components/omnibox/browser/open_tab_provider.cc
+++ components/omnibox/browser/open_tab_provider.cc
@@ -4,12 +4,15 @@
 
 #include "components/omnibox/browser/open_tab_provider.h"
 
-#include "components/omnibox/browser/autocomplete_match.h"
+#include "base/i18n/case_conversion.h"
+#include "base/strings/string_split.h"
+#include "base/strings/utf_string_conversions.h"
+#include "components/omnibox/browser/autocomplete_match_classification.h"
 #include "components/omnibox/browser/tab_matcher.h"
 
-#if !BUILDFLAG(IS_IOS)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 #include "content/public/browser/web_contents.h"
-#endif  // !BUILDFLAG(IS_IOS)
+#endif
 
 OpenTabProvider::OpenTabProvider(AutocompleteProviderClient* client)
     : AutocompleteProvider(AutocompleteProvider::TYPE_OPEN_TAB),
@@ -25,13 +28,65 @@ void OpenTabProvider::Start(const AutocompleteInput& input,
   }
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+  constexpr size_t kMinQueryLength = 2;
+  if (input.text().length() < kMinQueryLength) {
+    // Exit early if the query is too short. This is to mitigate a short query
+    // matching a large volume of results with low confidence.
+    return;
+  }
+
+  std::vector<std::u16string> query_terms;
+  for (const auto& term :
+       base::SplitString(base::i18n::ToLower(input.text()), u" ",
+                         base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY)) {
+    query_terms.push_back(term);
+  }
+
+  // Perform basic substring matching on the query terms.
+  // TODO(crbug.com/1293702): This currently filters via exact substring matches
+  // and does not perform scoring yet.
   for (auto* web_contents : client_->GetTabMatcher().GetOpenTabs()) {
-    // TODO(crbug.com/1293702): This is a placeholder implementation for the WIP
-    // matching and scoring algorithm.
-    if (web_contents) {
-      AutocompleteMatch empty_match;
-      matches_.push_back(empty_match);
+    const GURL& url = web_contents->GetLastCommittedURL();
+    if (!url.is_valid()) {
+      continue;
+    }
+
+    const std::u16string title = base::i18n::ToLower(web_contents->GetTitle());
+    for (const auto& query_term : query_terms) {
+      if (title.find(query_term) != std::string::npos) {
+        matches_.push_back(
+            CreateOpenTabMatch(input.text(), web_contents->GetTitle(), url));
+        break;
+      }
     }
   }
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+}
+
+AutocompleteMatch OpenTabProvider::CreateOpenTabMatch(
+    const std::u16string& input_text,
+    const std::u16string& title,
+    const GURL& url) {
+  DCHECK(url.is_valid());
+
+  // TODO(crbug.com/1293702): This uses a placeholder relevance. The scoring
+  // will need to be revisited as results may be truncated later.
+  AutocompleteMatch match(this, /*relevance=*/1000,
+                          /*deletable=*/false, AutocompleteMatchType::OPEN_TAB);
+  match.destination_url = url;
+
+  match.contents = base::UTF8ToUTF16(url.spec());
+  auto contents_terms = FindTermMatches(input_text, match.contents);
+  match.contents_class = ClassifyTermMatches(
+      contents_terms, match.contents.size(),
+      ACMatchClassification::MATCH | ACMatchClassification::URL,
+      ACMatchClassification::URL);
+
+  match.description = title;
+  auto description_terms = FindTermMatches(input_text, match.description);
+  match.description_class = ClassifyTermMatches(
+      description_terms, match.description.size(), ACMatchClassification::MATCH,
+      ACMatchClassification::NONE);
+
+  return match;
 }
--- components/omnibox/browser/open_tab_provider.h
+++ components/omnibox/browser/open_tab_provider.h
@@ -7,12 +7,12 @@
 
 #include "base/memory/raw_ptr.h"
 #include "components/omnibox/browser/autocomplete_input.h"
+#include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
 
 // This provider matches user input against open tabs. It is *not* included as a
 // default provider.
-// TODO(crbug.com/1293702): This is WIP and still a skeleton.
 class OpenTabProvider : public AutocompleteProvider {
  public:
   explicit OpenTabProvider(AutocompleteProviderClient* client);
@@ -24,6 +24,9 @@ class OpenTabProvider : public AutocompleteProvider {
 
  private:
   ~OpenTabProvider() override;
+  AutocompleteMatch CreateOpenTabMatch(const std::u16string& input_text,
+                                       const std::u16string& title,
+                                       const GURL& url);
 
   raw_ptr<AutocompleteProviderClient> client_;
 };
