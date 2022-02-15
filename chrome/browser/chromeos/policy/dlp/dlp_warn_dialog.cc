6df50e4542b8ad6d0f265a036d7e22066374755b
Jan Kopański <jkopanski@google.com>
2022-02-15 11:41:46 +0000

    Fix dlp warn dialog colors
    Warn dialog colors were set incorrectly when #dark-light-mode flag was
    disabled.
    Scoped the dialog creation with light mode.
    
    Fixed: 1296217
    Change-Id: Idb67075ee3acd02c5ccd82e72399746c865e9a5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460675
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Jan Kopański <jkopanski@google.com>
    Cr-Commit-Position: refs/heads/main@{#971162}

--- chrome/browser/chromeos/policy/dlp/dlp_warn_dialog.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_warn_dialog.cc
@@ -26,6 +26,7 @@
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/public/cpp/style/color_provider.h"
+#include "ash/public/cpp/style/scoped_light_mode_as_default.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 namespace policy {
@@ -156,6 +157,10 @@ void AddGeneralInformation(views::View* upper_panel,
                            DlpWarnDialog::DlpWarnDialogOptions options) {
 // TODO(crbug.com/1261496) Enable dynamic UI color & theme in lacros
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  // When #dark-light-mode flag is disabled (default setting), the color mode is
+  // by default set to dark mode. The warn dialog has white background for the
+  // default setting, so it should use light mode color palette.
+  ash::ScopedLightModeAsDefault scoped_light_mode_as_default;
   ash::ColorProvider* color_provider = ash::ColorProvider::Get();
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
@@ -224,6 +229,10 @@ void AddConfidentialContentRow(
     const DlpConfidentialContent& confidential_content) {
 // TODO(crbug.com/1261496) Enable dynamic UI color & theme in lacros
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  // When #dark-light-mode flag is disabled (default setting), the color mode is
+  // by default set to dark mode. The warn dialog has white background for the
+  // default setting, so it should use light mode color palette.
+  ash::ScopedLightModeAsDefault scoped_light_mode_as_default;
   ash::ColorProvider* color_provider = ash::ColorProvider::Get();
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
