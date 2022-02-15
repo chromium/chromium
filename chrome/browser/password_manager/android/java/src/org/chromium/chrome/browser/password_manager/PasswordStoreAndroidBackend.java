47d91c426080b4370b97c787037f5c93ce54353a
Viktor Semeniuk <vsemeniuk@google.com>
2022-02-15 14:19:12 +0000

    [UPM] Functions which allows password migration on sign-in/sign-out
    This change adds getAllLogins, getAutofillableLogins,
    getLoginsForSignonRealm, addLogin, updateLogin, removeLogin with a
    syncing account parameter. This allows to obtain/update logins
    disregarding the current sync status. After downstream implementation is
    ready default and deprecated functions can be removed.
    
    Bug: 1279444
    Change-Id: I798a54de14cec3ba888cf26fbe97da59729ad5e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455792
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#971199}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreAndroidBackend.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordStoreAndroidBackend.java
@@ -4,7 +4,11 @@
 
 package org.chromium.chrome.browser.password_manager;
 
+import android.accounts.Account;
+
 import org.chromium.base.Callback;
+
+import java.util.Optional;
 
 /**
  * Interface to send backend requests to a downstream implementation to fulfill password store
@@ -48,9 +52,24 @@ public interface PasswordStoreAndroidBackend {
      * @param loginsReply Callback that is called on success with serialized {@link
      *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #getAllLogins(Optional<Account>, Callback<byte[]>,
+     *         Callback<Exception>)}.
      */
-    void getAllLogins(@PasswordStoreOperationTarget int target, Callback<byte[]> loginsReply,
-            Callback<Exception> failureCallback);
+    default void getAllLogins(@PasswordStoreOperationTarget int target,
+            Callback<byte[]> loginsReply, Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async list call to retrieve all logins.
+     *
+     * @param syncingAccount Account used to sync passwords. If the syncingAccount is empty local
+     *         account will be used.
+     * @param loginsReply Callback that is called on success with serialized {@link
+     *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * TODO(crbug.com/1278767): Remove default keyword after downstream implementation is ready.
+     */
+    default void getAllLogins(Optional<Account> syncingAccount, Callback<byte[]> loginsReply,
+            Callback<Exception> failureCallback){};
 
     /**
      * Triggers an async list call to retrieve autofillable logins.
@@ -58,8 +77,23 @@ public interface PasswordStoreAndroidBackend {
      * @param loginsReply Callback that is called on success with serialized {@link
      *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #getAutofillableLogins(Optional<Account>, Callback<byte[]>,
+     *         Callback<Exception>)}.
      */
-    void getAutofillableLogins(Callback<byte[]> loginsReply, Callback<Exception> failureCallback);
+    default void getAutofillableLogins(
+            Callback<byte[]> loginsReply, Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async list call to retrieve autofillable logins.
+     *
+     * @param syncingAccount Account used to sync passwords. If the syncingAccount is empty local
+     *         account will be used.
+     * @param loginsReply Callback that is called on success with serialized {@link
+     *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     */
+    default void getAutofillableLogins(Optional<Account> syncingAccount,
+            Callback<byte[]> loginsReply, Callback<Exception> failureCallback){};
 
     /**
      * Triggers an async list call to retrieve logins with matching signon realm.
@@ -69,29 +103,78 @@ public interface PasswordStoreAndroidBackend {
      * @param loginsReply Callback that is called on success with serialized {@link
      *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #getLoginsForSignonRealm(String, Optional<Account>, Callback<byte[]>,
+     *         Callback<Exception>)}.
      */
-    void getLoginsForSignonRealm(
-            String signonRealm, Callback<byte[]> loginsReply, Callback<Exception> failureCallback);
+    default void getLoginsForSignonRealm(String signonRealm, Callback<byte[]> loginsReply,
+            Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async list call to retrieve logins with matching signon realm.
+     *
+     * @param signonRealm Signon realm string matched by a substring match. The returned results
+     * must be validated (e.g matching "sample.com" also returns logins for "not-sample.com").
+     * @param syncingAccount Account used to sync passwords. If the syncingAccount is empty local
+     *         account will be used.
+     * @param loginsReply Callback that is called on success with serialized {@link
+     *         org.chromium.components.sync.protocol.ListPasswordsResult} data.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * TODO(crbug.com/1278767): Remove default keyword after downstream implementation is ready.
+     */
+    default void getLoginsForSignonRealm(String signonRealm, Optional<Account> syncingAccount,
+            Callback<byte[]> loginsReply, Callback<Exception> failureCallback){};
 
     /**
      * Triggers an async call to add a login to the store.
      *
-     * @param pwdWithLocalData Serialized PasswordWithLocalData identifying the login to be added.
+     * @param pwdWithLocalData Serialized PasswordWithLocalData to be added.
      * @param successCallback Callback that is called on success.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #addLogin(byte[], Optional<Account>, String, Runnable,
+     *         Callback<Exception>)}.
      */
-    void addLogin(
-            byte[] pwdWithLocalData, Runnable successCallback, Callback<Exception> failureCallback);
+    default void addLogin(byte[] pwdWithLocalData, Runnable successCallback,
+            Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async call to add a login to the store.
+     *
+     * @param pwdWithLocalData Serialized PasswordWithLocalData to be added.
+     * @param syncingAccount Account used to sync passwords. If Nullopt was provided local account
+     *         will be used.
+     * @param successCallback Callback that is called on success.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * TODO(crbug.com/1278767): Remove default keyword after downstream implementation is ready.
+     */
+    default void addLogin(byte[] pwdWithLocalData, Optional<Account> syncingAccount,
+            Runnable successCallback, Callback<Exception> failureCallback){};
 
     /**
      * Triggers an async call to update a login in the store.
      *
-     * @param pwdWithLocalData Serialized PasswordWithLocalData identifying the login to be updated.
+     * @param pwdWithLocalData Serialized PasswordWithLocalData replacing a login with the same
+     *         unique key.
      * @param successCallback Callback that is called on success.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #updateLogin(byte[], Optional<Account>, Runnable,
+     *         Callback<Exception>)}.
      */
-    void updateLogin(
-            byte[] pwdWithLocalData, Runnable successCallback, Callback<Exception> failureCallback);
+    default void updateLogin(byte[] pwdWithLocalData, Runnable successCallback,
+            Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async call to update a login in the store.
+     *
+     * @param pwdWithLocalData Serialized PasswordWithLocalData replacing a login with the same
+     *         unique key.
+     * @param syncingAccount Account used to sync passwords. If Nullopt was provided local account
+     *         will be used.
+     * @param successCallback Callback that is called on success.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * TODO(crbug.com/1278767): Remove default keyword after downstream implementation is ready.
+     */
+    default void updateLogin(byte[] pwdWithLocalData, Optional<Account> syncingAccount,
+            Runnable successCallback, Callback<Exception> failureCallback){};
 
     /**
      * Triggers an async call to remove a login from store.
@@ -100,7 +183,22 @@ public interface PasswordStoreAndroidBackend {
      * @param target enum {@link StoreOperationTarget} to decide which storage to use.
      * @param successCallback Callback that is called on success.
      * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * @deprecated use {@link #removeLogin(byte[], Optional<Account>, Runnable,
+     *         Callback<Exception>)}.
      */
-    void removeLogin(byte[] pwdSpecificsData, @PasswordStoreOperationTarget int target,
-            Runnable successCallback, Callback<Exception> failureCallback);
+    default void removeLogin(byte[] pwdSpecificsData, @PasswordStoreOperationTarget int target,
+            Runnable successCallback, Callback<Exception> failureCallback){};
+
+    /**
+     * Triggers an async call to remove a login from store.
+     *
+     * @param pwdSpecificsData Serialized PasswordSpecificsData identifying the login to be deleted.
+     * @param syncingAccount Account used to sync passwords. If Nullopt was provided local account
+     *         will be used.
+     * @param successCallback Callback that is called on success.
+     * @param failureCallback A callback that is called on failure for any reason. May return sync.
+     * TODO(crbug.com/1278767): Remove default keyword after downstream implementation is ready.
+     */
+    default void removeLogin(byte[] pwdSpecificsData, Optional<Account> syncingAccount,
+            Runnable successCallback, Callback<Exception> failureCallback){};
 }
