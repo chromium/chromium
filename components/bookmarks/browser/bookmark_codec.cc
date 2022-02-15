0d2a54bbb5cdb42fe09d545b4d5a1d717142c5dd
Rushan Suleymanov <rushans@google.com>
2022-02-15 08:59:57 +0000

    [Sync] Reorder encoding bookmarks to reduce memory usage
    This CL changes the order of bookmarks encoding but the behaviour
    should remain the same.
    
    Sync metadata is now encoded in the very beginning to be able to clean
    up the metadata.
    
    Bug: 1277230
    Change-Id: I6bf407bb2526368f933a1ac2e7544b18831ad69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460724
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#971120}

--- components/bookmarks/browser/bookmark_codec.cc
+++ components/bookmarks/browser/bookmark_codec.cc
@@ -51,6 +51,18 @@ const char BookmarkCodec::kSyncMetadata[] = "sync_metadata";
 // Current version of the file.
 static const int kCurrentVersion = 1;
 
+namespace {
+
+// Encodes Sync metadata and cleans up the input string to decrease peak memory
+// usage during encoding.
+base::Value EncodeSyncMetadata(std::string sync_metadata_str) {
+  std::string sync_metadata_str_base64;
+  base::Base64Encode(sync_metadata_str, &sync_metadata_str_base64);
+  return base::Value(std::move(sync_metadata_str_base64));
+}
+
+}  // namespace
+
 BookmarkCodec::BookmarkCodec()
     : ids_reassigned_(false),
       guids_reassigned_(false),
@@ -60,10 +72,10 @@ BookmarkCodec::BookmarkCodec()
 BookmarkCodec::~BookmarkCodec() = default;
 
 base::Value BookmarkCodec::Encode(BookmarkModel* model,
-                                  const std::string& sync_metadata_str) {
+                                  std::string sync_metadata_str) {
   return Encode(model->bookmark_bar_node(), model->other_node(),
                 model->mobile_node(), model->root_node()->GetMetaInfoMap(),
-                sync_metadata_str);
+                std::move(sync_metadata_str));
 }
 
 base::Value BookmarkCodec::Encode(
@@ -71,9 +83,21 @@ base::Value BookmarkCodec::Encode(
     const BookmarkNode* other_folder_node,
     const BookmarkNode* mobile_folder_node,
     const BookmarkNode::MetaInfoMap* model_meta_info_map,
-    const std::string& sync_metadata_str) {
+    std::string sync_metadata_str) {
   ids_reassigned_ = false;
   guids_reassigned_ = false;
+
+  base::Value main(base::Value::Type::DICTIONARY);
+  main.SetIntKey(kVersionKey, kCurrentVersion);
+
+  // Encode Sync metadata before encoding other fields to reduce peak memory
+  // usage.
+  if (!sync_metadata_str.empty()) {
+    main.SetKey(kSyncMetadata,
+                EncodeSyncMetadata(std::move(sync_metadata_str)));
+    sync_metadata_str.clear();
+  }
+
   InitializeChecksum();
   base::Value roots(base::Value::Type::DICTIONARY);
   roots.SetKey(kBookmarkBarFolderNameKey, EncodeNode(bookmark_bar_node));
@@ -81,20 +105,13 @@ base::Value BookmarkCodec::Encode(
   roots.SetKey(kMobileBookmarkFolderNameKey, EncodeNode(mobile_folder_node));
   if (model_meta_info_map)
     roots.SetKey(kMetaInfo, EncodeMetaInfo(*model_meta_info_map));
-  base::Value main(base::Value::Type::DICTIONARY);
-  main.SetIntKey(kVersionKey, kCurrentVersion);
   FinalizeChecksum();
   // We are going to store the computed checksum. So set stored checksum to be
   // the same as computed checksum.
   stored_checksum_ = computed_checksum_;
   main.SetStringKey(kChecksumKey, computed_checksum_);
+
   main.SetKey(kRootsKey, std::move(roots));
-  if (!sync_metadata_str.empty()) {
-    std::string sync_metadata_str_base64;
-    base::Base64Encode(sync_metadata_str, &sync_metadata_str_base64);
-    main.SetKey(kSyncMetadata,
-                base::Value(std::move(sync_metadata_str_base64)));
-  }
   return main;
 }
 
