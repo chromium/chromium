4c404c5c694deb98c689b246ff8054810e5bbb52
Kevin McNee <mcnee@chromium.org>
2022-02-15 18:04:28 +0000

    Specify python3 for run_with_dummy_home.py's shebang
    This would no longer run when invoked directly on machines whose
    unversioned python package has been removed.
    
    We now specify the version, and since this doesn't appear to have
    any issues running under python3, we use python3.
    
    Bug: 1275016
    Change-Id: Iba3dffe2d27ee1cd0d786d6ae2e37fcc38d9dd15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465374
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971311}

--- testing/run_with_dummy_home.py
+++ testing/run_with_dummy_home.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -9,8 +9,6 @@ Helper for running a test with a dummy $HOME, populated with just enough for
 tests to run and pass. Useful for isolating tests from the real $HOME, which
 can contain config files that negatively affect test performance.
 """
-
-from __future__ import print_function
 
 import os
 import shutil
