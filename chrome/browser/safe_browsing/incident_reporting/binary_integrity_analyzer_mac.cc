13cec53b57158706ea3e057ba2d5a8aac2acc3c1
Mark Mentovai <mark@chromium.org>
2022-02-15 17:52:57 +0000

    mac: Update Safe Browsing to identify Chrome across a certificate change
    The Developer ID certificate used to sign Chrome is changing. Rather
    than pin Safe Browsing’s requirement string by hash, do the same thing
    we are now doing for Chrome’s designated requirement: “Apple issued a
    Developer ID Application certificate to Google (indicated by team ID)”.
    The new requirement string will accept Chrome when signed by either the
    old or new certificate.
    
    (cherry picked from commit 7b1441f65d5bef3c0fe531809cccdeb40f9465c6)
    
    Bug: 1263152, 1296612
    Change-Id: I4dcbf3358ce0a93ff70eb54ba0fba0ffee5e1ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456802
    Auto-Submit: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#970032}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3463896
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#550}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer_mac.cc
+++ chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer_mac.cc
@@ -45,17 +45,72 @@ void VerifyBinaryIntegrityHelper(IncidentReceiver* incident_receiver,
 }  // namespace
 
 std::vector<PathAndRequirement> GetCriticalPathsAndRequirements() {
-  // Get the path to the main executable.
   std::vector<PathAndRequirement> critical_binaries;
-  // This requirement describes a developer ID signed application,
-  // with Google's team identifier, and the com.Google.Chrome[.canary]
-  // identifier.
+  // This requirement describes a developer ID signed application, with Google's
+  // team identifier, and the com.Google.Chrome[.channel] identifier.
+  //
+  // This is the default requirement that codesign normally uses, from 12.0.1
+  // Security-60157.40.30.0.1/OSX/libsecurity_codesigning/lib/drmaker.cpp
+  // Security::CodeSigning::DRMaker::appleAnchor, in the isDeveloperIDSignature
+  // case. The precise requirement here is tailored to Google's Developer ID
+  // Application signing certificate by including the correct team ID,
+  // DEVELOPER_ID_LEAF_TEAM_ID (EQHXZ8M8AV). It forms the trust chain from Apple
+  // to Google's Developer ID Application signing certificate.
+  //
+  // 1.2.840.113635.100.6 = {iso(1) member-body(2) us(840) apple(113635)
+  // appleDataSecurity(100) appleCertificateExtensions(6)}. The .1 that may
+  // follow is appleCertificateExtensionCodeSigning(1).
+  //
+  // DEVELOPER_ID_LEAF_APPLICATION_OID (1.2.840.113635.100.6.1.13) must be
+  // present on the leaf certificate (the one issued to the developer). This
+  // attribute is defined in the Apple Developer ID Certification Practice
+  // Statement document,
+  // https://www.apple.com/certificateauthority/Apple_Developer_ID_CPS. It
+  // identifies Developer ID Application signing certificates. drmaker.cpp
+  // refers to this OID as caspianLeafMarker and devIdLeafMarkerOID, with the
+  // comment "Caspian leaf certificate marker". Based on context and other
+  // appearances in the Security project's source code, Caspian is a code name
+  // for Developer ID.
+  //
+  // DEVELOPER_ID_INTERMEDIATE_OID (1.2.840.113635.100.6.2.6) must be present on
+  // the intermediate certificate. There's no published definition of this
+  // attribute, but it is present on the Developer ID Certification Authority
+  // certificate. Among Apple-published intermediate certificates at
+  // https://www.apple.com/certificateauthority/, the attribute is unique to
+  // Developer ID. drmaker.cpp refers to this OID as caspianSdkMarker and
+  // devIdSdkMarkerOID, with the comment "Caspian intermediate marker". Even
+  // absent a published definition, this is reasonable assurance that the
+  // attribute is being used correctly (and, in fact, following codesign's lead
+  // is strong evidence).
+  //
+  // The difference between "anchor apple" and "anchor apple generic" is that
+  // "anchor apple" indicates that something was signed by Apple as Apple's own
+  // product, where "anchor apple generic" indicates that something was signed
+  // by Apple but is not necessarily Apple's own product. 12.0.1
+  // Security-60157.40.30.0.1/OSX/libsecurity_codesigning/lib/reqinterp.cpp
+  // Security::CodeSigning::Requirement::Interpreter::eval, cases opAppleAnchor
+  // and opAppleGenericAnchor.
+  //
+  // Some code ends up signed with a requirement that "certificate
+  // leaf[field.1.2.840.113635.100.6.1.9]" be present. Per the Apple Worldwide
+  // Developer Relations Certification Practice Statement document,
+  // https://www.apple.com/certificateauthority/WWDR_CPS, this indicates Mac App
+  // Store Application signing. As this is out of scope for Chrome, it does not
+  // appear in the requirement string here.
+#define DEVELOPER_ID_INTERMEDIATE_OID "field.1.2.840.113635.100.6.2.6"
+#define DEVELOPER_ID_LEAF_APPLICATION_OID "field.1.2.840.113635.100.6.1.13"
+#define DEVELOPER_ID_LEAF_TEAM_ID "EQHXZ8M8AV"
+  // clang-format off
   std::string requirement =
       "(identifier \"com.google.Chrome\" or "
       "identifier \"com.google.Chrome.beta\" or "
       "identifier \"com.google.Chrome.dev\" or "
-      "identifier \"com.google.Chrome.canary\") "
-      "and certificate leaf = H\"c9a99324ca3fcb23dbcc36bd5fd4f9753305130a\"";
+      "identifier \"com.google.Chrome.canary\") and "
+      "anchor apple generic and "
+      "certificate 1[" DEVELOPER_ID_INTERMEDIATE_OID "] and "
+      "certificate leaf[" DEVELOPER_ID_LEAF_APPLICATION_OID "] and "
+      "certificate leaf[subject.OU] = " DEVELOPER_ID_LEAF_TEAM_ID;
+  // clang-format on
 
   critical_binaries.push_back(
       PathAndRequirement(base::mac::OuterBundlePath(), requirement));
