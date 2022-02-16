8a31acdc619455e5b5b628fc2d0609b38b49b3fd
Takuto Ikuta <tikuta@chromium.org>
2022-02-16 13:45:15 +0000

    testing: migrate browser tests to python3
    Fixed: 1293351
    Cq-Include-Trybots: luci.chromium.try:mac11-arm64-rel
    Change-Id: Ife9bce74168cd9f051f186283681a95995603f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465962
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971783}

--- chrome/test/data/native_messaging/native_hosts/empty_app.bat
+++ chrome/test/data/native_messaging/native_hosts/empty_app.bat
@@ -3,4 +3,4 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-python "%~dp0/empty_app.py" %*
+python3 "%~dp0/empty_app.py" %*
--- chrome/test/data/native_messaging/native_hosts/empty_app.py
+++ chrome/test/data/native_messaging/native_hosts/empty_app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -11,13 +11,13 @@ import struct
 
 while 1:
   # Read the message type (first 4 bytes).
-  typeBytes = sys.stdin.read(4)
+  typeBytes = sys.stdin.buffer.read(4)
 
   if len(typeBytes) == 0:
     break
 
   # Read the message length (4 bytes).
-  textLength = struct.unpack('i', sys.stdin.read(4))[0]
+  textLength = struct.unpack('i', sys.stdin.buffer.read(4))[0]
 
   # Read the text (JSON object) of the message.
-  text = sys.stdin.read(textLength)
+  text = sys.stdin.buffer.read(textLength)
