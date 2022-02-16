2b6c73d98ef2587834cd64dcda269f3ab225b833
Andrew Paseltiner <apaseltiner@chromium.org>
2022-02-16 17:20:14 +0000

    [base] Add hex string conversion function for absl::uint128
    For use with crrev.com/c/3444322, which cannot use absl::SimpleHexAtoi
    because it is banned.
    
    Bug: 1294272
    Change-Id: I68ab9f2faee6241cc8fa181acff074d0db689678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464194
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971905}

---
+++ base/strings/abseil_string_number_conversions.cc
@@ -0,0 +1,17 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/abseil_string_number_conversions.h"
+
+#include "base/strings/string_number_conversions_internal.h"
+#include "base/strings/string_piece.h"
+#include "third_party/abseil-cpp/absl/numeric/int128.h"
+
+namespace base {
+
+bool HexStringToUInt128(StringPiece input, absl::uint128* output) {
+  return internal::HexStringToIntImpl(input, *output);
+}
+
+}  // namespace base
