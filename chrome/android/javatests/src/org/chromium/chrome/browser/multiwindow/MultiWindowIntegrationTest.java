dfff74895ca34f7624be4c6b87b6710257738b59
Matthew Jones <mdjones@chromium.org>
2022-02-16 16:40:52 +0000

    Check if transitioning to tab switcher before finishing activity
    The slight layout event timing changes made by crrev.com/c/3451700
    require updates to features watching the layout system. The tab
    switcher is not necessarily showing at the precise moment it is
    being checked. In this case, when a tab is closed, the main
    activity checks for the tab switcher to determine whether to
    close. Prior to the above patch, the tab switcher was already
    showing. After, the tab switcher is in the process of showing but
    not completely shown. The condition causing the window to close
    now checks if the next layout is the tab switcher as well before
    finishing the activity.
    
    A test has also been added for this behavior that fails without
    the change to ChromeTabbedActivity.
    
    Bug: 1297006
    Change-Id: I52d1f4dc2898a9161636dbb2581d483051b9a187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466355
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971885}

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowIntegrationTest.java
@@ -34,6 +34,8 @@ import org.chromium.chrome.browser.ChromeTabbedActivity2;
 import org.chromium.chrome.browser.app.tabmodel.TabWindowManagerSingleton;
 import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.layouts.LayoutTestUtils;
+import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
@@ -136,5 +138,34 @@ public class MultiWindowIntegrationTest {
         // At this point cta2 should have zero tabs, and cta should have 2 tabs (NTP, 'google').
         waitForTabs("CTA2", cta2, 0, Tab.INVALID_TAB_ID);
         waitForTabs("CTA", cta, 2, googleTabId);
+    }
+
+    @Test
+    @MediumTest
+    @Feature("MultiWindow")
+    @RequiresApi(Build.VERSION_CODES.N)
+    @CommandLineFlags.Add({ChromeSwitches.DISABLE_TAB_MERGING_FOR_TESTING,
+            ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+    public void
+    testMovingLastTabKeepsActivityAlive() {
+        final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
+        int blankTab = cta.getActivityTabProvider().get().getId();
+
+        // Move the blank tab to cta2.
+        MenuUtils.invokeCustomMenuActionSync(InstrumentationRegistry.getInstrumentation(), cta,
+                R.id.move_to_other_window_menu_id);
+
+        final ChromeTabbedActivity2 cta2 = waitForSecondChromeTabbedActivity();
+
+        // At this point cta2 should have zero tabs, and cta should have 1 tab.
+        waitForTabs("CTA", cta, 0, Tab.INVALID_TAB_ID);
+        waitForTabs("CTA2", cta2, 1, blankTab);
+
+        // Once all the tabs from one activity have been removed, the tab switcher should be shown.
+        LayoutTestUtils.waitForLayout(cta.getLayoutManager(), LayoutType.TAB_SWITCHER);
+
+        // The activity should also remain alive.
+        Assert.assertFalse("The original activity should not be finishing!", cta.isFinishing());
+        Assert.assertFalse("The original activity should still be alive!", cta.isDestroyed());
     }
 }
