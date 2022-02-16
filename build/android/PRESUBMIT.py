05d220cb0954b3e3bc6ee3f1f7882fd58b5615e1
Andrew Grieve <agrieve@chromium.org>
2022-02-16 16:25:23 +0000

    Py2->Py3 for build/android/PRESUBMIT.py
    Bug: 1262299
    Change-Id: I9ee3a26b117352fb65f4a4420ac07ab3e0ac810c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466818
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Norman Rosas <normando@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971874}

--- build/android/PRESUBMIT.py
+++ build/android/PRESUBMIT.py
@@ -8,6 +8,8 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts for
 details on the presubmit API built into depot_tools.
 """
 
+USE_PYTHON3 = True
+
 
 def CommonChecks(input_api, output_api):
   build_android_dir = input_api.PresubmitLocalPath()
