c25b7d786406b0d54040817c670609730aba663e
Stephen McGruer <smcgruer@chromium.org>
2022-02-16 22:44:41 +0000

    [PaymentRequest] Delay removal of show-without-UA until M102
    I2D&R update: https://groups.google.com/a/chromium.org/g/blink-dev/c/xCW746n6XJI/m/dN7zT8dzAgAJ
    
    Bug: 825270
    Change-Id: I2a34452ef119a3f1f2ae55a00f2faa429118ed68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465214
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972115}

--- third_party/blink/renderer/core/frame/deprecation.cc
+++ third_party/blink/renderer/core/frame/deprecation.cc
@@ -694,7 +694,7 @@ const DeprecationInfo GetDeprecationInfo(const WebFeature feature) {
 
     case WebFeature::kPaymentRequestShowWithoutGesture:
       return DeprecationInfo::WithFeatureAndChromeStatusID(
-          "PaymentRequestShowWithoutGesture", kM100,
+          "PaymentRequestShowWithoutGesture", kM102,
           "Calling PaymentRequest.show() without user activation",
           "5948593429020672");
 
