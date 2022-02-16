1daa76512114e47dcb4efeb270fe60f7464c792c
Eric Willigers <ericwilligers@chromium.org>
2022-02-16 16:32:43 +0000

    Lacros: Rename ImmersiveRevealedLockAsh
    We rename ImmersiveRevealedLockAsh to ImmersiveRevealedLockChromeos
    as the class is used in both Ash and Lacros.
    
    Change-Id: Ie29eea7ec4c85ad7803d874ad0d11e00c48c070b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466883
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971879}

--- chrome/browser/ui/views/frame/immersive_mode_controller_chromeos.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_chromeos.cc
@@ -51,13 +51,14 @@ ToImmersiveFullscreenControllerAnimateReveal(
   return chromeos::ImmersiveFullscreenController::ANIMATE_REVEAL_NO;
 }
 
-class ImmersiveRevealedLockAsh : public ImmersiveRevealedLock {
+class ImmersiveRevealedLockChromeos : public ImmersiveRevealedLock {
  public:
-  explicit ImmersiveRevealedLockAsh(chromeos::ImmersiveRevealedLock* lock)
+  explicit ImmersiveRevealedLockChromeos(chromeos::ImmersiveRevealedLock* lock)
       : lock_(lock) {}
 
-  ImmersiveRevealedLockAsh(const ImmersiveRevealedLockAsh&) = delete;
-  ImmersiveRevealedLockAsh& operator=(const ImmersiveRevealedLockAsh&) = delete;
+  ImmersiveRevealedLockChromeos(const ImmersiveRevealedLockChromeos&) = delete;
+  ImmersiveRevealedLockChromeos& operator=(
+      const ImmersiveRevealedLockChromeos&) = delete;
 
  private:
   std::unique_ptr<chromeos::ImmersiveRevealedLock> lock_;
@@ -114,7 +115,7 @@ int ImmersiveModeControllerChromeos::GetTopContainerVerticalOffset(
 
 ImmersiveRevealedLock* ImmersiveModeControllerChromeos::GetRevealedLock(
     AnimateReveal animate_reveal) {
-  return new ImmersiveRevealedLockAsh(controller_.GetRevealedLock(
+  return new ImmersiveRevealedLockChromeos(controller_.GetRevealedLock(
       ToImmersiveFullscreenControllerAnimateReveal(animate_reveal)));
 }
 
