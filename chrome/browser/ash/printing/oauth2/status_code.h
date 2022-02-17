2d595ac828776223c61cd0eeab9bebbcde8c7a7c
Piotr Pawliczek <pawliczek@chromium.org>
2022-02-17 15:37:35 +0000

    OAuth2 for IPP: Helper class to do HTTP exchanges
    HttpExchange is a helper class that is used by other components to
    communicate with authorization servers.
    
    BUG=b:201442267
    TEST=unit_tests --gtest_filter=PrintingOAuth2HttpExchange*
    
    Change-Id: Ic2400f148a2cda12035a797e15fd4b0b15ad07d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311837
    Reviewed-by: Pranav Batra <batrapranav@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972450}

---
+++ chrome/browser/ash/printing/oauth2/status_code.h
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_PRINTING_OAUTH2_STATUS_CODE_H_
+#define CHROME_BROWSER_ASH_PRINTING_OAUTH2_STATUS_CODE_H_
+
+#include <string>
+
+#include "base/callback.h"
+
+namespace ash {
+namespace printing {
+namespace oauth2 {
+
+enum class StatusCode {
+  // Success - no errors occurred.
+  kOK = 0,
+  // Required session already exists, so the authorization attempt was ignored.
+  kAlreadyAuthorized,
+  // The client is registered to the server but there is no active OAuth2
+  // sessions. Run the method InitAuthorization(...) and then
+  // FinishAuthorization(...) to start OAuth2 session,
+  kAuthorizationNeeded,
+  // The client is not registered to the server and the server does not support
+  // dynamic registration (as described in rfc7591).
+  kClientNotRegistered,
+  // The server is unknown (not trusted).
+  kUnknownAuthorizationServer,
+  // The server denied the request.
+  kAccessDenied,
+  // Access token is invalid or expired (used internally only).
+  kInvalidAccessToken,
+  // The server sent 503 Service Unavailable HTTP status code.
+  kServerTemporarilyUnavailable,
+  // The server sent 500 Internal Server Error HTTP status code.
+  kServerError,
+  // The server sent invalid or unexpected response.
+  kInvalidResponse,
+  // Cannot open HTTPS connection to the server.
+  kConnectionError,
+  // Maximum number of pending authorizations was reached.
+  kTooManyPendingAuthorizations,
+  // Unexpected or unknown error occurred.
+  kUnexpectedError
+};
+
+// This type of callback is returned to the caller. When `status` equals
+// StatusCode::kOK the access token or authorization URL is returned in
+// `data`. In other case `data` contains an error message or an empty string.
+using AuthServerSessionCallback =
+    base::OnceCallback<void(StatusCode status, const std::string& data)>;
+
+}  // namespace oauth2
+}  // namespace printing
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_PRINTING_OAUTH2_STATUS_CODE_H_
