49550b3db82dc7fd3af81551bd2fb6e8c42fc8c6
thefrog <thefrog@chromium.org>
2022-02-17 15:29:24 +0000

    [WL] Avoid trying to fetch the access token for client side detection
    For weblayer, using the SafeBrowsingTokenFetcherImpl token fetcher will
    not return a token until there is a production mechanism for enabling
    Gaia-keyed CSD. For safety, this CL avoids making the call to try to
    fetch the token.
    
    Bug: 1190615, 1171215
    Change-Id: I39aadabf990aed0e33cdeca178e52e85a04b8a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464796
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972446}

--- weblayer/browser/safe_browsing/weblayer_safe_browsing_tab_observer_delegate.cc
+++ weblayer/browser/safe_browsing/weblayer_safe_browsing_tab_observer_delegate.cc
@@ -32,8 +32,11 @@ CreateClientSideDetectionHost(content::WebContents* web_contents,
           &ProfileImpl::access_token_fetch_delegate,
           base::Unretained(ProfileImpl::FromBrowserContext(browser_context)))),
       static_cast<BrowserContextImpl*>(browser_context)->IsOffTheRecord(),
+      // TODO(crbug.com/1171215): Change this to production mechanism for
+      // enabling Gaia-keyed CSD once that mechanism is determined. See also
+      // crbug.com/1190615.
       /* account_signed_in_callback= */ base::BindRepeating([]() {
-        return true;
+        return false;
       }));
 }
 
