25bc39bdeced613e4ae5dd76d5308b53c78437e4
spdonghao <spdonghao@chromium.org>
2022-02-18 22:33:51 +0000

    Reland "[Start] Make logo smaller and remove top margin of fake search box."
    This is a reland of 9f923503509af3c08a3e3b0020392fcbee54d51d
    
    The test is failing because mView#getHeight is not ready yet when
    StartSurfaceToolbarCoordinator#shouldShowRealSearchBox is called.
    Updated it to dimension |toolbar_height_no_shadow|.
    
    Original change's description:
    > [Start] Make logo smaller and remove top margin of fake search box.
    >
    > Before: https://screenshot.googleplex.com/4yeynmCEnFy6cJy
    > After: https://screenshot.googleplex.com/B2B8o3DxJHNTX5U
    >
    > Bug: 1298084
    > Change-Id: I34ff1ef8bc04af1a79c379522320f83aa221847c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468940
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Hao Dong <spdonghao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#971981}
    
    (cherry picked from commit e632b1995deb72e0b7c7fa7d8c2dbc35bf402379)
    
    Bug: 1298084
    Change-Id: Ife9b2d77d904d73ab49a18159afe2ec2c65fdac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472408
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Auto-Submit: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#972681}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472745
    Cr-Commit-Position: refs/branch-heads/4844@{#683}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTabSwitcherTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTabSwitcherTest.java
@@ -536,6 +536,7 @@ public class InstantStartTabSwitcherTest {
         mActivityTestRule.resumeMainActivityFromLauncher();
 
         // Create a non search result tab and check the shared preferences.
+        StartSurfaceTestUtils.waitForOverviewVisible(cta);
         StartSurfaceTestUtils.waitForTabModel(cta);
         StartSurfaceTestUtils.launchFirstMVTile(cta, 1);
 
