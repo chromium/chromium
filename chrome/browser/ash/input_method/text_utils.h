ec20113ed43a2d813076a11fdf4a25a3692a43be
amraboelkher <amraboelkher@chromium.org>
2022-02-18 05:50:41 +0000

    PSM libs: Fix text_utils inconsistent variable types
    The original caller in InputMethodEngine::SetSurroundingText() starts
    with uint32_t values. Then, it casts them into integers.
    
    However for all integers + size_t, there will always be
    comparison warning between signed and unsigned types.
    
    shell-encryption and private_membership leaked some configs
    which disable that default warning which should have been
    treated as an error.
    
    In order to re-enable all these warnings for chrome/browser/*
    in this CL crrev.com/c/3303510, all warnings need to be fixed.
    
    This CL is fixing the comparison between two variables to have
    the same type as unsigned for text_utils. Since it is safe
    and guaranteed to have all |pos| parameter values as unsigned
    before casting them out.
    
    Bug: chromium:1212837, b:201763017
    Change-Id: I0810e44dbdd801a4e204bd7ed20fd3363cce7f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472766
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972818}

--- chrome/browser/ash/input_method/text_utils.h
+++ chrome/browser/ash/input_method/text_utils.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_ASH_INPUT_METHOD_TEXT_UTILS_H_
 #define CHROME_BROWSER_ASH_INPUT_METHOD_TEXT_UTILS_H_
 
+#include <limits>
 #include <string>
 
 #include "ui/gfx/range/range.h"
@@ -13,7 +14,7 @@
 namespace ash {
 namespace input_method {
 
-const int kUndefined = -1;
+constexpr uint32_t kUndefined = std::numeric_limits<uint32_t>::max();
 
 struct Sentence {
   Sentence();
@@ -31,17 +32,17 @@ struct Sentence {
 
 // Find the index of the last sentence end before |pos|, returns |kUndefined| if
 // not found.
-int FindLastSentenceEnd(const std::u16string& text, int pos);
+uint32_t FindLastSentenceEnd(const std::u16string& text, uint32_t pos);
 
 // Find the index of the first sentence end equal or after |pos|, returns
 // |kUndefined| if not found.
-int FindNextSentenceEnd(const std::u16string& text, int pos);
+uint32_t FindNextSentenceEnd(const std::u16string& text, uint32_t pos);
 
 // Find the last sentence before cursor position |pos|.
-Sentence FindLastSentence(const std::u16string& text, int pos);
+Sentence FindLastSentence(const std::u16string& text, uint32_t pos);
 
 // Find the sentence containing the cursor position |pos|.
-Sentence FindCurrentSentence(const std::u16string& text, int pos);
+Sentence FindCurrentSentence(const std::u16string& text, uint32_t pos);
 
 }  // namespace input_method
 }  // namespace ash
--- chrome/browser/ash/input_method/text_utils_unittest.cc
+++ chrome/browser/ash/input_method/text_utils_unittest.cc
@@ -12,97 +12,97 @@ namespace {
 
 TEST(TextUtilsTest, FindsLastSentenceEnd) {
   // Simple test cases.
-  EXPECT_EQ(FindLastSentenceEnd(u"This is a test.", 15), 14);
-  EXPECT_EQ(FindLastSentenceEnd(u"This is a test.", 14), kUndefined);
+  EXPECT_EQ(FindLastSentenceEnd(u"This is a test.", 15u), 14u);
+  EXPECT_EQ(FindLastSentenceEnd(u"This is a test.", 14u), kUndefined);
 
   // Ignores non-terminal dot.
-  EXPECT_EQ(FindLastSentenceEnd(u"Hi! There are 1.5 million people.", 20), 2);
-  EXPECT_EQ(FindLastSentenceEnd(u"What? Mr. Smith isn't here!", 20), 4);
-  EXPECT_EQ(FindLastSentenceEnd(u"What? (Mr. Smith isn't here!)", 20), 4);
+  EXPECT_EQ(FindLastSentenceEnd(u"Hi! There are 1.5 million people.", 20u), 2u);
+  EXPECT_EQ(FindLastSentenceEnd(u"What? Mr. Smith isn't here!", 20u), 4u);
+  EXPECT_EQ(FindLastSentenceEnd(u"What? (Mr. Smith isn't here!)", 20u), 4u);
 
   // Ends with section end.
-  EXPECT_EQ(FindLastSentenceEnd(u"He said: \"You are superb!\"", 26), 25);
-  EXPECT_EQ(FindLastSentenceEnd(u"(This is a test.) Hi!", 19), 16);
+  EXPECT_EQ(FindLastSentenceEnd(u"He said: \"You are superb!\"", 26u), 25u);
+  EXPECT_EQ(FindLastSentenceEnd(u"(This is a test.) Hi!", 19u), 16u);
 
   // Ends with emoticon.
-  EXPECT_EQ(FindLastSentenceEnd(u"Thank you :) Have a nice day!", 20), 11);
-  EXPECT_EQ(FindLastSentenceEnd(u"I am so sorry :-(", 17), 16);
+  EXPECT_EQ(FindLastSentenceEnd(u"Thank you :) Have a nice day!", 20u), 11u);
+  EXPECT_EQ(FindLastSentenceEnd(u"I am so sorry :-(", 17u), 16u);
 
   // Multiple lines cases.
-  EXPECT_EQ(FindLastSentenceEnd(u"This is a dog\n\nThat is a cat", 20), 13);
-  EXPECT_EQ(FindLastSentenceEnd(u"This is a dog\n", 13), 12);
+  EXPECT_EQ(FindLastSentenceEnd(u"This is a dog\n\nThat is a cat", 20u), 13u);
+  EXPECT_EQ(FindLastSentenceEnd(u"This is a dog\n", 13u), 12u);
 }
 
 TEST(TextUtilsTest, FindsNextSentenceEnd) {
   // Simple test cases.
-  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 15), kUndefined);
-  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 14), 14);
-  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 0), 14);
+  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 15u), kUndefined);
+  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 14u), 14u);
+  EXPECT_EQ(FindNextSentenceEnd(u"This is a test.", 0u), 14u);
 
   // Ignores non-terminal dot.
-  EXPECT_EQ(FindNextSentenceEnd(u"Hi! There are 1.5 million people.", 8), 32);
-  EXPECT_EQ(FindNextSentenceEnd(u"What? Mr. Smith isn't here!", 6), 26);
-  EXPECT_EQ(FindNextSentenceEnd(u"What? (Mr. Smith isn't here!)", 6), 28);
+  EXPECT_EQ(FindNextSentenceEnd(u"Hi! There are 1.5 million people.", 8u), 32u);
+  EXPECT_EQ(FindNextSentenceEnd(u"What? Mr. Smith isn't here!", 6u), 26u);
+  EXPECT_EQ(FindNextSentenceEnd(u"What? (Mr. Smith isn't here!)", 6u), 28u);
 
   // Ends with section end.
-  EXPECT_EQ(FindNextSentenceEnd(u"He said: \"You are superb!\"", 0), 25);
-  EXPECT_EQ(FindNextSentenceEnd(u"(This is a test.) Hi!", 10), 16);
+  EXPECT_EQ(FindNextSentenceEnd(u"He said: \"You are superb!\"", 0u), 25u);
+  EXPECT_EQ(FindNextSentenceEnd(u"(This is a test.) Hi!", 10u), 16u);
 
   // Ends with emoticon.
-  EXPECT_EQ(FindNextSentenceEnd(u"Thank you :) Have a nice day!", 0), 11);
-  EXPECT_EQ(FindNextSentenceEnd(u"I am so sorry :-(", 16), 16);
+  EXPECT_EQ(FindNextSentenceEnd(u"Thank you :) Have a nice day!", 0u), 11u);
+  EXPECT_EQ(FindNextSentenceEnd(u"I am so sorry :-(", 16u), 16u);
 
   // Multiple lines cases.
-  EXPECT_EQ(FindNextSentenceEnd(u"This is a dog\n\nThat is a cat", 0), 12);
+  EXPECT_EQ(FindNextSentenceEnd(u"This is a dog\n\nThat is a cat", 0u), 12u);
 }
 
 TEST(TextUtilsTest, FindLastSentence) {
-  EXPECT_EQ(FindLastSentence(u"This is a test.", 14), Sentence());
-  EXPECT_EQ(FindLastSentence(u"This is a test.", 15), Sentence());
-  EXPECT_EQ(FindLastSentence(u"This is a test. ", 15),
+  EXPECT_EQ(FindLastSentence(u"This is a test.", 14u), Sentence());
+  EXPECT_EQ(FindLastSentence(u"This is a test.", 15u), Sentence());
+  EXPECT_EQ(FindLastSentence(u"This is a test. ", 15u),
             Sentence(gfx::Range(0, 15), u"This is a test."));
-  EXPECT_EQ(FindLastSentence(u"Hi! This is a test.  ", 19),
+  EXPECT_EQ(FindLastSentence(u"Hi! This is a test.  ", 19u),
             Sentence(gfx::Range(4, 19), u"This is a test."));
 
-  EXPECT_EQ(FindLastSentence(u"Hi! There are 1.5 million people.", 20),
+  EXPECT_EQ(FindLastSentence(u"Hi! There are 1.5 million people.", 20u),
             Sentence(gfx::Range(0, 3), u"Hi!"));
-  EXPECT_EQ(FindLastSentence(u"What? Mr. Smith isn't here!", 20),
+  EXPECT_EQ(FindLastSentence(u"What? Mr. Smith isn't here!", 20u),
             Sentence(gfx::Range(0, 5), u"What?"));
-  EXPECT_EQ(FindLastSentence(u"What? (Mr. Smith isn't here!)", 20),
+  EXPECT_EQ(FindLastSentence(u"What? (Mr. Smith isn't here!)", 20u),
             Sentence(gfx::Range(0, 5), u"What?"));
 
-  EXPECT_EQ(FindLastSentence(u"Thank you :) Have a nice day!", 20),
+  EXPECT_EQ(FindLastSentence(u"Thank you :) Have a nice day!", 20u),
             Sentence(gfx::Range(0, 12), u"Thank you :)"));
 
-  EXPECT_EQ(FindLastSentence(u"This is a dog\nThat is a cat", 20),
+  EXPECT_EQ(FindLastSentence(u"This is a dog\nThat is a cat", 20u),
             Sentence(gfx::Range(0, 13), u"This is a dog"));
-  EXPECT_EQ(FindLastSentence(u"This is a dog\n\nThat is a cat", 20),
+  EXPECT_EQ(FindLastSentence(u"This is a dog\n\nThat is a cat", 20u),
             Sentence());
-  EXPECT_EQ(FindLastSentence(u"This is a dog\n", 13), Sentence());
+  EXPECT_EQ(FindLastSentence(u"This is a dog\n", 13u), Sentence());
 }
 
 TEST(TextUtilsTest, FindCurrentSentence) {
-  EXPECT_EQ(FindCurrentSentence(u"This is a test.", 14),
+  EXPECT_EQ(FindCurrentSentence(u"This is a test.", 14u),
             Sentence(gfx::Range(0, 15), u"This is a test."));
-  EXPECT_EQ(FindCurrentSentence(u"This is a test.", 15),
+  EXPECT_EQ(FindCurrentSentence(u"This is a test.", 15u),
             Sentence(gfx::Range(0, 15), u"This is a test."));
-  EXPECT_EQ(FindCurrentSentence(u"This is a test. ", 15), Sentence());
-  EXPECT_EQ(FindCurrentSentence(u"Hi! This is a test.  ", 18),
+  EXPECT_EQ(FindCurrentSentence(u"This is a test. ", 15u), Sentence());
+  EXPECT_EQ(FindCurrentSentence(u"Hi! This is a test.  ", 18u),
             Sentence(gfx::Range(4, 19), u"This is a test."));
 
-  EXPECT_EQ(FindCurrentSentence(u"Hi! There are 1.5 million people.", 20),
+  EXPECT_EQ(FindCurrentSentence(u"Hi! There are 1.5 million people.", 20u),
             Sentence(gfx::Range(4, 33), u"There are 1.5 million people."));
-  EXPECT_EQ(FindCurrentSentence(u"What? Mr. Smith isn't here!", 20),
+  EXPECT_EQ(FindCurrentSentence(u"What? Mr. Smith isn't here!", 20u),
             Sentence(gfx::Range(6, 27), u"Mr. Smith isn't here!"));
-  EXPECT_EQ(FindCurrentSentence(u"What? (Mr. Smith isn't here!)", 20),
+  EXPECT_EQ(FindCurrentSentence(u"What? (Mr. Smith isn't here!)", 20u),
             Sentence(gfx::Range(6, 29), u"(Mr. Smith isn't here!)"));
 
-  EXPECT_EQ(FindCurrentSentence(u"Thank you :) Have a nice day!", 20),
+  EXPECT_EQ(FindCurrentSentence(u"Thank you :) Have a nice day!", 20u),
             Sentence(gfx::Range(13, 29), u"Have a nice day!"));
 
-  EXPECT_EQ(FindCurrentSentence(u"This is a dog\nThat is a cat", 20),
+  EXPECT_EQ(FindCurrentSentence(u"This is a dog\nThat is a cat", 20u),
             Sentence(gfx::Range(14, 27), u"That is a cat"));
-  EXPECT_EQ(FindCurrentSentence(u"This is a dog\n", 13),
+  EXPECT_EQ(FindCurrentSentence(u"This is a dog\n", 13u),
             Sentence(gfx::Range(0, 13), u"This is a dog"));
 }
 
