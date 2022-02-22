157ee86e841e595ef7d1347d159fa348ac571ec0
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-02-22 14:47:35 +0000

    DanglingPtr: Use PA_DCHECK insted of DCHECK.
    This removes a dependency of partition_alloc from base.
    
    Bug: 1294702
    Change-Id: If8c86dcb3c9016d6243e22cf464f76669f765748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3478544
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973718}

--- base/allocator/partition_allocator/dangling_raw_ptr_checks.cc
+++ base/allocator/partition_allocator/dangling_raw_ptr_checks.cc
@@ -4,9 +4,8 @@
 
 #include "base/allocator/partition_allocator/dangling_raw_ptr_checks.h"
 
+#include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/base_export.h"
-#include "base/check.h"
-#include "base/logging.h"
 
 namespace partition_alloc {
 
@@ -16,22 +15,22 @@ DanglingRawPtrReleasedFn* g_dangling_raw_ptr_released_fn = [](uintptr_t) {};
 }  // namespace
 
 DanglingRawPtrDetectedFn* GetDanglingRawPtrDetectedFn() {
-  DCHECK(g_dangling_raw_ptr_detected_fn);
+  PA_DCHECK(g_dangling_raw_ptr_detected_fn);
   return g_dangling_raw_ptr_detected_fn;
 }
 
 DanglingRawPtrDetectedFn* GetDanglingRawPtrReleasedFn() {
-  DCHECK(g_dangling_raw_ptr_released_fn);
+  PA_DCHECK(g_dangling_raw_ptr_released_fn);
   return g_dangling_raw_ptr_released_fn;
 }
 
 void SetDanglingRawPtrDetectedFn(DanglingRawPtrDetectedFn fn) {
-  DCHECK(fn);
+  PA_DCHECK(fn);
   g_dangling_raw_ptr_detected_fn = fn;
 }
 
 void SetDanglingRawPtrReleasedFn(DanglingRawPtrReleasedFn fn) {
-  DCHECK(fn);
+  PA_DCHECK(fn);
   g_dangling_raw_ptr_released_fn = fn;
 }
 
