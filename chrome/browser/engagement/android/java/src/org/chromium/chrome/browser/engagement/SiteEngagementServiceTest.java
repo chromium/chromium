2c4b397cc65bafb925b2a0738db26851afa22e0a
Jinsuk Kim <jinsukkim@chromium.org>
2022-02-23 16:38:57 +0000

    Android: Remove deps on chrome_java in SiteEngagementService module
    The tests in Site engagement needs only a Profile object with which
    to initialize the service. Uses ChromeBrowserTestRule to provide
    what's necessary for them. The dependency on chrome_java can be
    removed.
    
    Bug: 1273313
    Change-Id: I99e8d8ae2f7ad4e56a9f35dd735e5655300f237a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475292
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974208}

--- chrome/browser/engagement/android/java/src/org/chromium/chrome/browser/engagement/SiteEngagementServiceTest.java
+++ chrome/browser/engagement/android/java/src/org/chromium/chrome/browser/engagement/SiteEngagementServiceTest.java
@@ -8,7 +8,6 @@ import androidx.test.filters.SmallTest;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,10 +17,11 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
-import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
 import org.chromium.components.site_engagement.SiteEngagementService;
+import org.chromium.content_public.browser.BrowserContextHandle;
+import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
 /**
  * Test for the Site Engagement Service Java binding.
@@ -32,13 +32,8 @@ import org.chromium.components.site_engagement.SiteEngagementService;
 public class SiteEngagementServiceTest {
     private static final String URL = "https://www.example.com";
 
-    @ClassRule
-    public static ChromeTabbedActivityTestRule sActivityTestRule =
-            new ChromeTabbedActivityTestRule();
-
     @Rule
-    public BlankCTATabInitialStateRule mBlankCTATabInitialStateRule =
-            new BlankCTATabInitialStateRule(sActivityTestRule, false);
+    public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
 
     /**
      * Verify that setting the engagement score for a URL and reading it back it works.
@@ -47,20 +42,16 @@ public class SiteEngagementServiceTest {
     @SmallTest
     @Feature({"Engagement"})
     public void testSettingAndRetrievingScore() throws Throwable {
-        sActivityTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                SiteEngagementService service =
-                        SiteEngagementService.getForBrowserContext(Profile.fromWebContents(
-                                sActivityTestRule.getActivity().getActivityTab().getWebContents()));
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            BrowserContextHandle handle = Profile.getLastUsedRegularProfile();
+            SiteEngagementService service = SiteEngagementService.getForBrowserContext(handle);
 
-                Assert.assertEquals(0.0, service.getScore(URL), 0);
-                service.resetBaseScoreForUrl(URL, 5.0);
-                Assert.assertEquals(5.0, service.getScore(URL), 0);
+            Assert.assertEquals(0.0, service.getScore(URL), 0);
+            service.resetBaseScoreForUrl(URL, 5.0);
+            Assert.assertEquals(5.0, service.getScore(URL), 0);
 
-                service.resetBaseScoreForUrl(URL, 2.0);
-                Assert.assertEquals(2.0, service.getScore(URL), 0);
-            }
+            service.resetBaseScoreForUrl(URL, 2.0);
+            Assert.assertEquals(2.0, service.getScore(URL), 0);
         });
     }
 
@@ -71,35 +62,27 @@ public class SiteEngagementServiceTest {
     @SmallTest
     @Feature({"Engagement"})
     public void testRepeatedlyGettingService() throws Throwable {
-        sActivityTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Profile profile = Profile.fromWebContents(
-                        sActivityTestRule.getActivity().getActivityTab().getWebContents());
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            Profile profile = Profile.getLastUsedRegularProfile();
 
-                Assert.assertEquals(
-                        0.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
-                SiteEngagementService.getForBrowserContext(profile).resetBaseScoreForUrl(URL, 5.0);
-                Assert.assertEquals(
-                        5.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
+            Assert.assertEquals(
+                    0.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
+            SiteEngagementService.getForBrowserContext(profile).resetBaseScoreForUrl(URL, 5.0);
+            Assert.assertEquals(
+                    5.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
 
-                SiteEngagementService.getForBrowserContext(profile).resetBaseScoreForUrl(URL, 2.0);
-                Assert.assertEquals(
-                        2.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
-            }
+            SiteEngagementService.getForBrowserContext(profile).resetBaseScoreForUrl(URL, 2.0);
+            Assert.assertEquals(
+                    2.0, SiteEngagementService.getForBrowserContext(profile).getScore(URL), 0);
         });
     }
 
     @After
     public void tearDown() {
-        sActivityTestRule.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                SiteEngagementService service =
-                        SiteEngagementService.getForBrowserContext(Profile.fromWebContents(
-                                sActivityTestRule.getActivity().getActivityTab().getWebContents()));
-                service.resetBaseScoreForUrl(URL, 0.0);
-            }
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            BrowserContextHandle handle = Profile.getLastUsedRegularProfile();
+            SiteEngagementService service = SiteEngagementService.getForBrowserContext(handle);
+            service.resetBaseScoreForUrl(URL, 0.0);
         });
     }
 }
