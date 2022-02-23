e8bd0b73bd4850996be5e209694be9f31a0da6ee
Rune Lillesveen <futhark@chromium.org>
2022-02-23 15:54:47 +0000

    Remove incorrect DCHECK for InStyleRecalc()
    Also mark ancestors child-dirty if this SetStyle happens outside style
    recalc to make sure we reach the element during the next recalc.
    
    Bug: 1298917
    Change-Id: Iaf1341bd2aa957ba3454144725157a3c6b211439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484283
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974184}

--- third_party/blink/renderer/core/layout/layout_list_item.cc
+++ third_party/blink/renderer/core/layout/layout_list_item.cc
@@ -64,15 +64,15 @@ void LayoutListItem::StyleDidChange(StyleDifference diff,
   if (old_style && (StyleRef().ListStyleType() ||
                     (current_image && !current_image->ErrorOccurred()))) {
     // The old_style check makes sure we don't enter here when attaching the
-    // LayoutObject. Check that this happens during style recalc.
-    DCHECK(GetDocument().InStyleRecalc());
+    // LayoutObject.
     DCHECK(!GetDocument().GetStyleEngine().InRebuildLayoutTree());
     // We may enter here when propagating writing-mode and direction from body
     // to the root element after layout tree rebuild. Skip NotifyOfSubtreeChange
     // for that case.
     if (GetDocument().documentElement() != GetNode() ||
         GetDocument().GetStyleEngine().NeedsStyleRecalc()) {
-      NotifyOfSubtreeChange();
+      if (NotifyOfSubtreeChange() && !GetDocument().InStyleRecalc())
+        GetNode()->MarkAncestorsWithChildNeedsStyleRecalc();
     }
   }
 
