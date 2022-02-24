ca4d4b2d60e16c7f01fe3fad9e5e74a1ba716499
Daniel Cheng <dcheng@chromium.org>
2022-02-24 13:07:01 +0000

    Replace url::Replacements<T> with GURL::Replacements in /chrome/browser.
    When clearing URL components, the two interfaces are exactly identical.
    When replacing a URL component, GURL::Replacements has a simpler
    interface.
    
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 1271865
    Change-Id: Ie75e6cf7feb7aadc2cd4569ee2102945f6ec8577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487267
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974608}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -755,9 +755,9 @@ std::string& GetIOThreadApplicationLocale() {
 GURL ReplaceURLHostAndPath(const GURL& url,
                            const std::string& host,
                            const std::string& path) {
-  url::Replacements<char> replacements;
-  replacements.SetHost(host.c_str(), url::Component(0, host.length()));
-  replacements.SetPath(path.c_str(), url::Component(0, path.length()));
+  GURL::Replacements replacements;
+  replacements.SetHostStr(host);
+  replacements.SetPathStr(path);
   return url.ReplaceComponents(replacements);
 }
 
