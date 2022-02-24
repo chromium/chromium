3ed45937964b424cb44755bd797b395247e9bfec
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-02-24 20:01:43 +0000

    Add test coverage for syncer::StringOrdinal
    This is a preparation to immediately merge Ordinal with StringOrdinal
    in crrev.com/c/3488388. The goal is to migrate the unit tests while
    also making sure that behavior gets preserved by the other CL. Since
    Ordinal's behavior is weird (see crbug.com/1300525#c1), we do the best
    we can, i.e. replace N in the general case with the N-th letter in the
    0-indexed alphabet. Some of the tests for LongOrdinal and LargeOrdinal
    are also removed since they are no longer interesting.
    
    Bug: 1300525
    Change-Id: I3f1d06b0a0ce42804827a771aa4c78af97972fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488271
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#974778}

--- components/sync/base/ordinal_unittest.cc
+++ components/sync/base/ordinal_unittest.cc
@@ -2,125 +2,63 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(crbug.com/1300525): Move this file to model/ and remove this include
+// (it's kept now to avoid a presubmit error). None of that is done now to allow
+// for a friendlier diff.
 #include "components/sync/base/ordinal.h"
 
 #include <cctype>
 #include <vector>
 
 #include "base/rand_util.h"
+#include "components/sync/model/string_ordinal.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace syncer {
 
 namespace {
 
-struct TestOrdinalTraits {
-  static const uint8_t kZeroDigit = '0';
-  static const uint8_t kMaxDigit = '3';
-  static const size_t kMinLength = 1;
-};
-
-struct LongOrdinalTraits {
-  static const uint8_t kZeroDigit = '0';
-  static const uint8_t kMaxDigit = '9';
-  static const size_t kMinLength = 5;
-};
-
-struct LargeOrdinalTraits {
-  static const uint8_t kZeroDigit = 0;
-  static const uint8_t kMaxDigit = UINT8_MAX;
-  static const size_t kMinLength = 1;
-};
-
-using TestOrdinal = Ordinal<TestOrdinalTraits>;
-using LongOrdinal = Ordinal<LongOrdinalTraits>;
-using LargeOrdinal = Ordinal<LargeOrdinalTraits>;
-
-static_assert(TestOrdinal::kZeroDigit == '0',
-              "incorrect TestOrdinal zero digit");
-static_assert(TestOrdinal::kOneDigit == '1', "incorrect TestOrdinal one digit");
-static_assert(TestOrdinal::kMidDigit == '2', "incorrect TestOrdinal min digit");
-static_assert(TestOrdinal::kMaxDigit == '3', "incorrect TestOrdinal max digit");
-static_assert(TestOrdinal::kMidDigitValue == 2,
-              "incorrect TestOrdinal mid digit value");
-static_assert(TestOrdinal::kMaxDigitValue == 3,
-              "incorrect TestOrdinal max digit value");
-static_assert(TestOrdinal::kRadix == 4, "incorrect TestOrdinal radix");
-
-static_assert(LongOrdinal::kZeroDigit == '0',
-              "incorrect LongOrdinal zero digit");
-static_assert(LongOrdinal::kOneDigit == '1', "incorrect LongOrdinal one digit");
-static_assert(LongOrdinal::kMidDigit == '5', "incorrect LongOrdinal mid digit");
-static_assert(LongOrdinal::kMaxDigit == '9', "incorrect LongOrdinal max digit");
-static_assert(LongOrdinal::kMidDigitValue == 5,
-              "incorrect LongOrdinal mid digit value");
-static_assert(LongOrdinal::kMaxDigitValue == 9,
-              "incorrect LongOrdinal max digit value");
-static_assert(LongOrdinal::kRadix == 10, "incorrect LongOrdinal radix");
-
-static_assert(static_cast<char>(LargeOrdinal::kZeroDigit) == '\x00',
-              "incorrect LargeOrdinal zero digit");
-static_assert(static_cast<char>(LargeOrdinal::kOneDigit) == '\x01',
-              "incorrect LargeOrdinal one digit");
-static_assert(static_cast<char>(LargeOrdinal::kMidDigit) == '\x80',
-              "incorrect LargeOrdinal mid digit");
-static_assert(static_cast<char>(LargeOrdinal::kMaxDigit) == '\xff',
-              "incorrect LargeOrdinal max digit");
-static_assert(LargeOrdinal::kMidDigitValue == 128,
-              "incorrect LargeOrdinal mid digit value");
-static_assert(LargeOrdinal::kMaxDigitValue == 255,
-              "incorrect LargeOrdinal max digit value");
-static_assert(LargeOrdinal::kRadix == 256, "incorrect LargeOrdinal radix");
-
-// Create Ordinals that satisfy all but one criterion for validity.
+// Create StringOrdinals that satisfy all but one criterion for validity.
 // IsValid() should return false for all of them.
-TEST(Ordinal, Invalid) {
+TEST(StringOrdinalTest, Invalid) {
   // Length criterion.
-  EXPECT_FALSE(TestOrdinal(std::string()).IsValid());
-  EXPECT_FALSE(LongOrdinal("0001").IsValid());
+  EXPECT_FALSE(StringOrdinal(std::string()).IsValid());
 
-  const char kBeforeZero[] = {'0' - 1, '\0'};
-  const char kAfterNine[] = {'9' + 1, '\0'};
+  const char kBeforeA[] = {'a' - 1, '\0'};
+  const char kAfterZ[] = {'z' + 1, '\0'};
 
   // Character criterion.
-  EXPECT_FALSE(TestOrdinal(kBeforeZero).IsValid());
-  EXPECT_FALSE(TestOrdinal("4").IsValid());
-  EXPECT_FALSE(LongOrdinal(std::string("0000") + kBeforeZero).IsValid());
-  EXPECT_FALSE(LongOrdinal(std::string("0000") + kAfterNine).IsValid());
+  EXPECT_FALSE(StringOrdinal(kBeforeA).IsValid());
+  EXPECT_FALSE(StringOrdinal(kAfterZ).IsValid());
 
   // Zero criterion.
-  EXPECT_FALSE(TestOrdinal("0").IsValid());
-  EXPECT_FALSE(TestOrdinal("00000").IsValid());
+  EXPECT_FALSE(StringOrdinal("a").IsValid());
 
   // Trailing zero criterion.
-  EXPECT_FALSE(TestOrdinal("10").IsValid());
-  EXPECT_FALSE(TestOrdinal("111110").IsValid());
+  EXPECT_FALSE(StringOrdinal("ba").IsValid());
 }
 
-// Create Ordinals that satisfy all criteria for validity.
+// Create StringOrdinals that satisfy all criteria for validity.
 // IsValid() should return true for all of them.
-TEST(Ordinal, Valid) {
+TEST(StringOrdinalTest, Valid) {
   // Length criterion.
-  EXPECT_TRUE(TestOrdinal("1").IsValid());
-  EXPECT_TRUE(LongOrdinal("10000").IsValid());
+  EXPECT_TRUE(StringOrdinal("b").IsValid());
 }
 
-// Create Ordinals from CreateInitialOrdinal.  They should be valid
+// Create StringOrdinals from CreateInitialOrdinal.  They should be valid
 // and close to the middle of the range.
-TEST(Ordinal, CreateInitialOrdinal) {
-  const TestOrdinal& ordinal1 = TestOrdinal::CreateInitialOrdinal();
-  const LongOrdinal& ordinal2 = LongOrdinal::CreateInitialOrdinal();
-  ASSERT_TRUE(ordinal1.IsValid());
-  ASSERT_TRUE(ordinal2.IsValid());
-  EXPECT_TRUE(ordinal1.Equals(TestOrdinal("2")));
-  EXPECT_TRUE(ordinal2.Equals(LongOrdinal("50000")));
+TEST(StringOrdinalTest, CreateInitialOrdinal) {
+  const StringOrdinal& ordinal = StringOrdinal::CreateInitialOrdinal();
+  ASSERT_TRUE(ordinal.IsValid());
+  // "n" is the midpoint letter of the alphabet.
+  EXPECT_TRUE(ordinal.Equals(StringOrdinal("n")));
 }
 
-// Create an invalid and a valid Ordinal.  EqualsOrBothInvalid should
+// Create an invalid and a valid StringOrdinal.  EqualsOrBothInvalid should
 // return true if called reflexively and false otherwise.
-TEST(Ordinal, EqualsOrBothInvalid) {
-  const TestOrdinal& valid_ordinal = TestOrdinal::CreateInitialOrdinal();
-  const TestOrdinal invalid_ordinal;
+TEST(StringOrdinalTest, EqualsOrBothInvalid) {
+  const StringOrdinal& valid_ordinal = StringOrdinal::CreateInitialOrdinal();
+  const StringOrdinal invalid_ordinal;
 
   EXPECT_TRUE(valid_ordinal.EqualsOrBothInvalid(valid_ordinal));
   EXPECT_TRUE(invalid_ordinal.EqualsOrBothInvalid(invalid_ordinal));
@@ -128,12 +66,12 @@ TEST(Ordinal, EqualsOrBothInvalid) {
   EXPECT_FALSE(valid_ordinal.EqualsOrBothInvalid(invalid_ordinal));
 }
 
-// Create three Ordinals in order.  LessThan should return values
+// Create three StringOrdinals in order.  LessThan should return values
 // consistent with that order.
-TEST(Ordinal, LessThan) {
-  const TestOrdinal small_ordinal("1");
-  const TestOrdinal middle_ordinal("2");
-  const TestOrdinal big_ordinal("3");
+TEST(StringOrdinalTest, LessThan) {
+  const StringOrdinal small_ordinal("b");
+  const StringOrdinal middle_ordinal("c");
+  const StringOrdinal big_ordinal("d");
 
   EXPECT_FALSE(small_ordinal.LessThan(small_ordinal));
   EXPECT_TRUE(small_ordinal.LessThan(middle_ordinal));
@@ -148,22 +86,12 @@ TEST(Ordinal, LessThan) {
   EXPECT_FALSE(big_ordinal.LessThan(big_ordinal));
 }
 
-// Create two single-digit ordinals with byte values 0 and 255.  The
-// former should compare as less than the latter, even though the
-// native char type may be signed.
-TEST(Ordinal, LessThanLarge) {
-  const LargeOrdinal small_ordinal("\x01");
-  const LargeOrdinal big_ordinal("\xff");
-
-  EXPECT_TRUE(small_ordinal.LessThan(big_ordinal));
-}
-
-// Create three Ordinals in order.  GreaterThan should return values
+// Create three StringOrdinals in order.  GreaterThan should return values
 // consistent with that order.
-TEST(Ordinal, GreaterThan) {
-  const LongOrdinal small_ordinal("10000");
-  const LongOrdinal middle_ordinal("55555");
-  const LongOrdinal big_ordinal("99999");
+TEST(StringOrdinalTest, GreaterThan) {
+  const StringOrdinal small_ordinal("b");
+  const StringOrdinal middle_ordinal("c");
+  const StringOrdinal big_ordinal("d");
 
   EXPECT_FALSE(small_ordinal.GreaterThan(small_ordinal));
   EXPECT_FALSE(small_ordinal.GreaterThan(middle_ordinal));
@@ -178,11 +106,11 @@ TEST(Ordinal, GreaterThan) {
   EXPECT_FALSE(big_ordinal.GreaterThan(big_ordinal));
 }
 
-// Create two valid Ordinals.  Equals should return true only when
+// Create two valid StringOrdinals.  Equals should return true only when
 // called reflexively.
-TEST(Ordinal, Equals) {
-  const TestOrdinal ordinal1("1");
-  const TestOrdinal ordinal2("2");
+TEST(StringOrdinalTest, Equals) {
+  const StringOrdinal ordinal1("b");
+  const StringOrdinal ordinal2("c");
 
   EXPECT_TRUE(ordinal1.Equals(ordinal1));
   EXPECT_FALSE(ordinal1.Equals(ordinal2));
@@ -193,10 +121,9 @@ TEST(Ordinal, Equals) {
 
 // Create some valid ordinals from some byte strings.
 // ToInternalValue() should return the original byte string.
-TEST(OrdinalTest, ToInternalValue) {
-  EXPECT_EQ("2", TestOrdinal("2").ToInternalValue());
-  EXPECT_EQ("12345", LongOrdinal("12345").ToInternalValue());
-  EXPECT_EQ("\1\2\3\4\5", LargeOrdinal("\1\2\3\4\5").ToInternalValue());
+TEST(StringOrdinalTest, ToInternalValue) {
+  EXPECT_EQ("c", StringOrdinal("c").ToInternalValue());
+  EXPECT_EQ("bcdef", StringOrdinal("bcdef").ToInternalValue());
 }
 
 bool IsNonEmptyPrintableString(const std::string& str) {
@@ -211,24 +138,23 @@ bool IsNonEmptyPrintableString(const std::string& str) {
 
 // Create some invalid/valid ordinals.  ToDebugString() should always
 // return a non-empty printable string.
-TEST(OrdinalTest, ToDebugString) {
-  EXPECT_TRUE(IsNonEmptyPrintableString(TestOrdinal().ToDebugString()));
+TEST(StringOrdinalTest, ToDebugString) {
+  EXPECT_TRUE(IsNonEmptyPrintableString(StringOrdinal().ToDebugString()));
+  EXPECT_TRUE(IsNonEmptyPrintableString(
+      StringOrdinal("invalid string").ToDebugString()));
+  EXPECT_TRUE(IsNonEmptyPrintableString(StringOrdinal("c").ToDebugString()));
   EXPECT_TRUE(
-      IsNonEmptyPrintableString(TestOrdinal("invalid string").ToDebugString()));
-  EXPECT_TRUE(IsNonEmptyPrintableString(TestOrdinal("2").ToDebugString()));
-  EXPECT_TRUE(IsNonEmptyPrintableString(LongOrdinal("12345").ToDebugString()));
-  EXPECT_TRUE(
-      IsNonEmptyPrintableString(LargeOrdinal("\1\2\3\4\5").ToDebugString()));
+      IsNonEmptyPrintableString(StringOrdinal("bcdef").ToDebugString()));
 }
 
-// Create three Ordinals in order.  LessThanFn should return values
+// Create three StringOrdinals in order.  LessThanFn should return values
 // consistent with that order.
-TEST(Ordinal, LessThanFn) {
-  const TestOrdinal small_ordinal("1");
-  const TestOrdinal middle_ordinal("2");
-  const TestOrdinal big_ordinal("3");
+TEST(StringOrdinalTest, LessThanFn) {
+  const StringOrdinal small_ordinal("b");
+  const StringOrdinal middle_ordinal("c");
+  const StringOrdinal big_ordinal("d");
 
-  const TestOrdinal::LessThanFn less_than;
+  const StringOrdinal::LessThanFn less_than;
 
   EXPECT_FALSE(less_than(small_ordinal, small_ordinal));
   EXPECT_TRUE(less_than(small_ordinal, middle_ordinal));
@@ -243,91 +169,78 @@ TEST(Ordinal, LessThanFn) {
   EXPECT_FALSE(less_than(big_ordinal, big_ordinal));
 }
 
-template <typename Traits>
 std::string GetBetween(const std::string& ordinal_string1,
                        const std::string& ordinal_string2) {
-  const Ordinal<Traits> ordinal1(ordinal_string1);
-  const Ordinal<Traits> ordinal2(ordinal_string2);
-  const Ordinal<Traits> between1 = ordinal1.CreateBetween(ordinal2);
-  const Ordinal<Traits> between2 = ordinal2.CreateBetween(ordinal1);
+  const StringOrdinal ordinal1(ordinal_string1);
+  const StringOrdinal ordinal2(ordinal_string2);
+  const StringOrdinal between1 = ordinal1.CreateBetween(ordinal2);
+  const StringOrdinal between2 = ordinal2.CreateBetween(ordinal1);
   EXPECT_TRUE(between1.Equals(between2));
   return between1.ToInternalValue();
 }
 
-// Create some Ordinals from single-digit strings.  Given two strings
-// from this set, CreateBetween should return an Ordinal roughly between
+// Create some StringOrdinals from single-digit strings.  Given two strings
+// from this set, CreateBetween should return a StringOrdinal roughly between
 // them that are also single-digit when possible.
-TEST(Ordinal, CreateBetweenSingleDigit) {
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("1", "3"));
-  EXPECT_EQ("12", GetBetween<TestOrdinal>("1", "2"));
-  EXPECT_EQ("22", GetBetween<TestOrdinal>("2", "3"));
+TEST(StringOrdinalTest, CreateBetweenSingleDigit) {
+  EXPECT_EQ("c", GetBetween("b", "d"));
+  EXPECT_EQ("bn", GetBetween("b", "c"));
+  EXPECT_EQ("cn", GetBetween("c", "d"));
 }
 
-// Create some Ordinals from strings of various lengths.  Given two
-// strings from this set, CreateBetween should return an Ordinal roughly
+// Create some StringOrdinals from strings of various lengths.  Given two
+// strings from this set, CreateBetween should return an StringOrdinal roughly
 // between them that have as few digits as possible.
-TEST(Ordinal, CreateBetweenDifferentLengths) {
-  EXPECT_EQ("102", GetBetween<TestOrdinal>("1", "11"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("1", "31"));
-  EXPECT_EQ("132", GetBetween<TestOrdinal>("13", "2"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("10001", "3"));
-  EXPECT_EQ("20000", GetBetween<LongOrdinal>("10001", "30000"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("10002", "3"));
-  EXPECT_EQ("20001", GetBetween<LongOrdinal>("10002", "30000"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("1", "30002"));
-  EXPECT_EQ("20001", GetBetween<LongOrdinal>("10000", "30002"));
+TEST(StringOrdinalTest, CreateBetweenDifferentLengths) {
+  EXPECT_EQ("ban", GetBetween("b", "bb"));
+  EXPECT_EQ("c", GetBetween("b", "db"));
+  EXPECT_EQ("bo", GetBetween("bd", "c"));
+  EXPECT_EQ("c", GetBetween("baaab", "d"));
+  EXPECT_EQ("c", GetBetween("baaac", "d"));
+  EXPECT_EQ("c", GetBetween("b", "daaac"));
 }
 
-// Create some Ordinals specifically designed to trigger overflow
+// Create some StringOrdinals specifically designed to trigger overflow
 // cases.  Given two strings from this set, CreateBetween should
-// return an Ordinal roughly between them that have as few digits as
+// return a StringOrdinal roughly between them that have as few digits as
 // possible.
-TEST(Ordinal, CreateBetweenOverflow) {
-  EXPECT_EQ("03", GetBetween<TestOrdinal>("01", "11"));
-  EXPECT_EQ("13", GetBetween<TestOrdinal>("11", "21"));
-  EXPECT_EQ("113", GetBetween<TestOrdinal>("111", "121"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("001", "333"));
-  EXPECT_EQ("31", GetBetween<TestOrdinal>("222", "333"));
-  EXPECT_EQ("3", GetBetween<TestOrdinal>("201", "333"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("003", "333"));
-  EXPECT_EQ("2", GetBetween<TestOrdinal>("2223", "1113"));
+TEST(StringOrdinalTest, CreateBetweenOverflow) {
+  EXPECT_EQ("ao", GetBetween("ab", "bb"));
+  EXPECT_EQ("bo", GetBetween("bb", "cb"));
+  EXPECT_EQ("bbo", GetBetween("bbb", "bcb"));
+  EXPECT_EQ("bo", GetBetween("aab", "ddd"));
+  EXPECT_EQ("cpp", GetBetween("ccc", "ddd"));
+  EXPECT_EQ("co", GetBetween("cab", "ddd"));
+  EXPECT_EQ("bo", GetBetween("aad", "ddd"));
+  EXPECT_EQ("boo", GetBetween("cccd", "bbbd"));
 }
 
-// Create some Ordinals specifically designed to trigger digit
-// overflow cases.  Given two strings from this set, CreateBetween
-// should return an Ordinal roughly between them that have as few digits
-// as possible.
-TEST(Ordinal, CreateBetweenOverflowLarge) {
-  EXPECT_EQ("\x80", GetBetween<LargeOrdinal>("\x01\xff", "\xff\xff"));
-  EXPECT_EQ("\xff\xfe\x80", GetBetween<LargeOrdinal>("\xff\xfe", "\xff\xff"));
+// Create some StringOrdinals.  CreateBefore should return a StringOrdinal
+// roughly halfway towards "a".
+TEST(StringOrdinalTest, CreateBefore) {
+  EXPECT_EQ("an", StringOrdinal("b").CreateBefore().ToInternalValue());
+  EXPECT_EQ("ao", StringOrdinal("bb").CreateBefore().ToInternalValue());
+  EXPECT_EQ("ao", StringOrdinal("bc").CreateBefore().ToInternalValue());
+  EXPECT_EQ("ap", StringOrdinal("bd").CreateBefore().ToInternalValue());
 }
 
-// Create some Ordinals.  CreateBefore should return an Ordinal
-// roughly halfway towards 0.
-TEST(Ordinal, CreateBefore) {
-  EXPECT_EQ("02", TestOrdinal("1").CreateBefore().ToInternalValue());
-  EXPECT_EQ("03", TestOrdinal("11").CreateBefore().ToInternalValue());
-  EXPECT_EQ("03", TestOrdinal("12").CreateBefore().ToInternalValue());
-  EXPECT_EQ("1", TestOrdinal("13").CreateBefore().ToInternalValue());
+// Create some StringOrdinals.  CreateAfter should return a StringOrdinal
+// roughly halfway towards "a".
+TEST(StringOrdinalTest, CreateAfter) {
+  EXPECT_EQ("o", StringOrdinal("d").CreateAfter().ToInternalValue());
+  EXPECT_EQ("on", StringOrdinal("dc").CreateAfter().ToInternalValue());
+  EXPECT_EQ("ooon", StringOrdinal("dddc").CreateAfter().ToInternalValue());
+  EXPECT_EQ("o", StringOrdinal("cc").CreateAfter().ToInternalValue());
+  EXPECT_EQ("o", StringOrdinal("cd").CreateAfter().ToInternalValue());
 }
 
-// Create some Ordinals.  CreateAfter should return an Ordinal
-// roughly halfway towards 0.
-TEST(Ordinal, CreateAfter) {
-  EXPECT_EQ("31", TestOrdinal("3").CreateAfter().ToInternalValue());
-  EXPECT_EQ("322", TestOrdinal("32").CreateAfter().ToInternalValue());
-  EXPECT_EQ("33322", TestOrdinal("3332").CreateAfter().ToInternalValue());
-  EXPECT_EQ("3", TestOrdinal("22").CreateAfter().ToInternalValue());
-  EXPECT_EQ("3", TestOrdinal("23").CreateAfter().ToInternalValue());
-}
-
-// Create two valid Ordinals.  EqualsFn should return true only when
+// Create two valid StringOrdinals.  EqualsFn should return true only when
 // called reflexively.
-TEST(Ordinal, EqualsFn) {
-  const TestOrdinal ordinal1("1");
-  const TestOrdinal ordinal2("2");
+TEST(StringOrdinalTest, EqualsFn) {
+  const StringOrdinal ordinal1("b");
+  const StringOrdinal ordinal2("c");
 
-  const TestOrdinal::EqualsFn equals;
+  const StringOrdinal::EqualsFn equals;
 
   EXPECT_TRUE(equals(ordinal1, ordinal1));
   EXPECT_FALSE(equals(ordinal1, ordinal2));
@@ -336,25 +249,25 @@ TEST(Ordinal, EqualsFn) {
   EXPECT_TRUE(equals(ordinal2, ordinal2));
 }
 
-// Create some Ordinals and shuffle them.  Sorting them using
+// Create some StringOrdinals and shuffle them.  Sorting them using
 // LessThanFn should produce the correct order.
-TEST(Ordinal, Sort) {
-  const LongOrdinal ordinal1("12345");
-  const LongOrdinal ordinal2("54321");
-  const LongOrdinal ordinal3("87654");
-  const LongOrdinal ordinal4("98765");
+TEST(StringOrdinalTest, Sort) {
+  const StringOrdinal ordinal1("bcefg");
+  const StringOrdinal ordinal2("gfecb");
+  const StringOrdinal ordinal3("ihgfe");
+  const StringOrdinal ordinal4("jihgf");
 
-  std::vector<LongOrdinal> sorted_ordinals;
+  std::vector<StringOrdinal> sorted_ordinals;
   sorted_ordinals.push_back(ordinal1);
   sorted_ordinals.push_back(ordinal2);
   sorted_ordinals.push_back(ordinal3);
   sorted_ordinals.push_back(ordinal4);
 
-  std::vector<LongOrdinal> ordinals = sorted_ordinals;
+  std::vector<StringOrdinal> ordinals = sorted_ordinals;
   base::RandomShuffle(ordinals.begin(), ordinals.end());
-  std::sort(ordinals.begin(), ordinals.end(), LongOrdinal::LessThanFn());
+  std::sort(ordinals.begin(), ordinals.end(), StringOrdinal::LessThanFn());
   EXPECT_TRUE(std::equal(ordinals.begin(), ordinals.end(),
-                         sorted_ordinals.begin(), LongOrdinal::EqualsFn()));
+                         sorted_ordinals.begin(), StringOrdinal::EqualsFn()));
 }
 
 }  // namespace
