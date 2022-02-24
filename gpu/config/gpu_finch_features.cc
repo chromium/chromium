2433624818dd5fce9cbe1b4e35fa562d9ad911f3
Vikas Soni <vikassoni@chromium.org>
2022-02-24 22:09:03 +0000

    [Dr-Dc] [Merge-M100] Disable DrDc on vulkan.
    Disable DrDc on android for vulkan until the known issues are fixed.
    
    (cherry picked from commit b382de6283a166947b777a8b765ae25d792a2326)
    
    Bug: 1291298
    Change-Id: I5de467519ffd3f4d2a739d64f55e672a5ae5f257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485418
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#974277}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488098
    Cr-Commit-Position: refs/branch-heads/4896@{#81}
    Cr-Branched-From: 1f63ff4bc27570761b35ffbc7f938f6586f7bee8-refs/heads/main@{#972766}

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -323,6 +323,10 @@ bool IsDrDcEnabled() {
     return false;
   }
 
+  // Currently not supported for vulkan until crbug.com/1291298 is fixed.
+  if (IsUsingVulkan())
+    return false;
+
   // DrDc is supported on android MediaPlayer and MCVD path only when
   // AImageReader is enabled.
   if (!IsAImageReaderEnabled())
