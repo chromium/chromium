70ac0436a922e3a4a72a5a387a7e0dc4064a41b3
Addison Luh <aluh@chromium.org>
2022-02-24 20:02:17 +0000

    Revert "Temporary fix for rendering corruption on 4K or larger displays."
    This reverts commit 5db15f6a732246e52744041ef1e6c438108456bb.
    
    Reason for revert: Caused race condition (crbug/1299331)
    
    Original change's description:
    > Temporary fix for rendering corruption on 4K or larger displays.
    >
    > This change temporarily increases the compositor memory limit for
    > displays that are 4K or larger. The rendering was using up more memory
    > than expected, hitting the limit and causing parts of the UI to not be
    > rendered.
    >
    > Tested with emulator with a large display size and confirmed the memory
    > limit was increased.
    >
    > (cherry picked from commit 3fcfa32692cd6506ecb8ed3a5a5f492ed750ab8c)
    >
    > Bug: 1261776
    > Change-Id: I502323d6098fda5d96cb28c76aad4debecd3d3ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413653
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Addison Luh <aluh@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/main@{#963721}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426433
    > Auto-Submit: Addison Luh <aluh@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/4844@{#198}
    > Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}
    
    Bug: 1261776,1299331
    Change-Id: I1ebb450d5c6b44475494a779a467feef5fda690f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489128
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#827}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- ash/display/window_tree_host_manager.cc
+++ ash/display/window_tree_host_manager.cc
@@ -27,7 +27,6 @@
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/wm/window_util.h"
 #include "base/bind.h"
-#include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/histogram_functions.h"
@@ -68,13 +67,8 @@ namespace {
 // This is initialized in the constructor, and then in CreatePrimaryHost().
 int64_t primary_display_id = -1;
 
-// The default compositor memory limit.
-constexpr char kUICompositorDefaultMemoryLimitMB[] = "512";
-// The compositor memory limit when display size is larger than a threshold.
-constexpr char kUICompositorLargeMemoryLimitMB[] = "1024";
-// The display size threshold, above which the larger memory limit is used.
-// Pixel size was chosen to trigger for 4K+ displays. See: crbug.com/1261776
-constexpr int kUICompositorMemoryLimitDisplaySizeThreshold = 3500;
+// The default memory limit: 512mb.
+const char kUICompositorDefaultMemoryLimitMB[] = "512";
 
 // An UMA signal for the current effective resolution is sent at this rate. This
 // keeps track of the effective resolution most used on internal display by the
@@ -304,6 +298,14 @@ void WindowTreeHostManager::Shutdown() {
 
 void WindowTreeHostManager::CreatePrimaryHost(
     const AshWindowTreeHostInitParams& init_params) {
+  auto* command_line = base::CommandLine::ForCurrentProcess();
+  if (!command_line->HasSwitch(
+          switches::kUiCompositorMemoryLimitWhenVisibleMB)) {
+    command_line->AppendSwitchASCII(
+        switches::kUiCompositorMemoryLimitWhenVisibleMB,
+        kUICompositorDefaultMemoryLimitMB);
+  }
+
   const display::Display& primary_candidate =
       GetDisplayManager()->GetPrimaryDisplayCandidate();
   primary_display_id = primary_candidate.id();
@@ -863,29 +865,6 @@ AshWindowTreeHost* WindowTreeHostManager::AddWindowTreeHostForDisplay(
   }
   params_with_bounds.display_id = display.id();
   params_with_bounds.device_scale_factor = display.device_scale_factor();
-
-  auto* command_line = base::CommandLine::ForCurrentProcess();
-  base::ScopedClosureRunner switch_remover;
-  if (!command_line->HasSwitch(
-          switches::kUiCompositorMemoryLimitWhenVisibleMB)) {
-    // TODO(crbug/1261776): Temporarily increase compositor memory limit for
-    // 4K+ displays to avoid rendering corruption.
-    // Check both width and height in case of rotated display.
-    const char* memory_limit =
-        std::max(display.GetSizeInPixel().width(),
-                 display.GetSizeInPixel().height()) >
-                kUICompositorMemoryLimitDisplaySizeThreshold
-            ? kUICompositorLargeMemoryLimitMB
-            : kUICompositorDefaultMemoryLimitMB;
-    command_line->AppendSwitchASCII(
-        switches::kUiCompositorMemoryLimitWhenVisibleMB, memory_limit);
-    switch_remover.ReplaceClosure(base::BindOnce(
-        [](base::CommandLine* cmd) {
-          cmd->RemoveSwitch(switches::kUiCompositorMemoryLimitWhenVisibleMB);
-        },
-        command_line));
-  }
-
   // The AshWindowTreeHost ends up owned by the RootWindowControllers created
   // by this class.
   AshWindowTreeHost* ash_host =
