cb94d249e82312d945a28d4cd707b5b844184c2a
Mason Freed <masonf@chromium.org>
2022-02-24 20:23:27 +0000

    Support dark mode for text files.
    Add a meta tag that indicates that dark mode is supported when
    creating the document tree for the plaintext document.
    
    Fixed: 1294905
    Change-Id: Id67f27e294a44860e35f345b2f830af97926d78c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3474692
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974802}

--- third_party/blink/renderer/core/html/parser/text_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/text_document_parser.cc
@@ -49,10 +49,21 @@ void TextDocumentParser::AppendBytes(const char* data, size_t length) {
 void TextDocumentParser::InsertFakePreElement() {
   // In principle, we should create a specialized tree builder for
   // TextDocuments, but instead we re-use the existing HTMLTreeBuilder. We
-  // create a fake token and give it to the tree builder rather than sending
-  // fake bytes through the front-end of the parser to avoid distrubing the
-  // line/column number calculations.
+  // create two fake tokens and pass them to the tree builder rather than
+  // sending fake bytes through the front-end of the parser to avoid disturbing
+  // the line/column number calculations.
   Vector<Attribute> attributes;
+
+  // Allow the browser to display the text file in dark mode if it is set as
+  // the preferred color scheme.
+  attributes.push_back(Attribute(html_names::kNameAttr, "color-scheme"));
+  attributes.push_back(Attribute(html_names::kContentAttr, "light dark"));
+  AtomicHTMLToken fake_meta(HTMLToken::kStartTag,
+                            html_names::kMetaTag.LocalName(), attributes);
+  TreeBuilder()->ConstructTree(&fake_meta);
+  attributes.clear();
+
+  // Wrap the actual contents of the text file in <pre>.
   attributes.push_back(Attribute(
       html_names::kStyleAttr, "word-wrap: break-word; white-space: pre-wrap;"));
   AtomicHTMLToken fake_pre(HTMLToken::kStartTag,
