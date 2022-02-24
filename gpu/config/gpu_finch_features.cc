7e82a762fd0c602b7a9efc89c7fd03a879b46e83
Vikas Soni <vikassoni@chromium.org>
2022-02-24 23:47:41 +0000

    [Dr-Dc] Disable DrDc on devices with 1 max image.
    Disable DrDc on devices which only support 1 max image in AImageReader
    since both gpu thread needs at least 1 image each to work.
    
    Bug: 1295386
    Change-Id: I2e169b5ccf84a64497329acf8b3b057a60407e63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489372
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974906}

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -324,8 +324,10 @@ bool IsDrDcEnabled() {
     return false;
 
   // DrDc is supported on android MediaPlayer and MCVD path only when
-  // AImageReader is enabled.
-  if (!IsAImageReaderEnabled())
+  // AImageReader is enabled. Also DrDc requires AImageReader max size to be
+  // at least 2 for each gpu thread. Hence DrDc is disabled on devices which has
+  // only 1 image.
+  if (!IsAImageReaderEnabled() || LimitAImageReaderMaxSizeToOne())
     return false;
 
   // Check block list against build info.
