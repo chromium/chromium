b0aae52b1f939cf248dad4b8c80ca416ef9f7c15
Kevin Ellis <kevers@google.com>
2022-02-24 20:36:21 +0000

    Speculative fix for "timeout waiting for X event" failures
    The timeout for scrollend events was increased here:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/2832125
    
    Applying the same extended deadline for other wait on events. If this
    significantly drops the flake scores, we should look at removing the
    timeout entirely.  Note that doing so would potentially change flakes
    from FAIL to TIMEOUT and thus leaving to a followup patch.
    
    Bug: 1281810
    Change-Id: I92ffbe80b6d2a7d950375cf5c50d5be2608ba843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488692
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974807}

--- third_party/blink/web_tests/resources/gesture-util.js
+++ third_party/blink/web_tests/resources/gesture-util.js
@@ -150,7 +150,7 @@ function waitForAnimationEndTimeBased(getValue) {
   })
 }
 
-function waitForEvent(eventTarget, eventName, timeoutMs = 1000) {
+function waitForEvent(eventTarget, eventName, timeoutMs = 2000) {
   return new Promise((resolve, reject) => {
     const eventListener = (evt) => {
       clearTimeout(timeout);
@@ -165,7 +165,7 @@ function waitForEvent(eventTarget, eventName, timeoutMs = 1000) {
   });
 }
 
-function waitForScrollEvent(eventTarget, timeoutMs = 1000) {
+function waitForScrollEvent(eventTarget, timeoutMs = 2000) {
   return waitForEvent(eventTarget, 'scroll', timeoutMs);
 }
 
