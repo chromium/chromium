ca7de71f0902a6c0de8a7f61cc40aaa902ff768b
Stephen McGruer <smcgruer@chromium.org>
2022-02-24 16:41:40 +0000

    [Sheriff] Disable TabScrubberBrowserTest.Smoke
    Flaking very badly on linux-lacros-rel
    
    Bug: 1298835
    Change-Id: Ib6642d1e59bf896c603284493cd79c84d4fe1258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488688
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Owners-Override: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974681}

--- chrome/browser/lacros/tab_scrubber_lacros_browsertest.cc
+++ chrome/browser/lacros/tab_scrubber_lacros_browsertest.cc
@@ -43,7 +43,8 @@ TabStrip* GetTabStrip(Browser* browser) {
 // It verifies that triggering a tab scrubbing request when a lacros window
 // is upfront and active, does not activates the logic in Ash, and forwards
 // the call to Lacros.
-IN_PROC_BROWSER_TEST_F(TabScrubberBrowserTest, Smoke) {
+// TODO(crbug.com/1298835): Flaking very badly
+IN_PROC_BROWSER_TEST_F(TabScrubberBrowserTest, DISABLED_Smoke) {
   auto* lacros_service = chromeos::LacrosService::Get();
   ASSERT_TRUE(lacros_service->IsAvailable<crosapi::mojom::TestController>());
   // This test requires the tab scrubbing API.
