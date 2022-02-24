8ddc3ded960de7bdadd583861a299ec1ab9139a9
Siyu An <siyua@chromium.org>
2022-02-24 23:44:12 +0000

    [Merge 100][VCN] Change the style of contents in virtual card unenroll dialog
    Before: https://screenshot.googleplex.com/BXXMTosEQHrGrWd
    After: https://screenshot.googleplex.com/7LYPdkVGupyKEy3
    
    (cherry picked from commit bba6f8c6d848e3179076d629919112fde5cd1abf)
    
    Bug: 1298761
    Change-Id: I257abd3304bf9b070027eaafefa956bdecb14678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469769
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#972796}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481542
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4896@{#85}
    Cr-Branched-From: 1f63ff4bc27570761b35ffbc7f938f6586f7bee8-refs/heads/main@{#972766}

--- chrome/browser/resources/settings/autofill_page/virtual_card_unenroll_dialog.ts
+++ chrome/browser/resources/settings/autofill_page/virtual_card_unenroll_dialog.ts
@@ -11,6 +11,8 @@
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import '../settings_shared_css.js';
 
 import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
