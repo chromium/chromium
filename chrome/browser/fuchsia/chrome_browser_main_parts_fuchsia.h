fccc58f0f92c06ece90a0afba5f8d553012feeb0
Benjamin Lerman <qsr@chromium.org>
2022-02-24 19:43:37 +0000

    [fuchsia] Remove Chrome CFv1 manifest and code
    Fuchsia now consumes the Chrome CFv2 manifest in all product
    configurations, so the old CMX and associated code paths can be removed.
    
    Bug: 1298481, 1211758
    Change-Id: Id51ff716c856ce96057b467fe516ad897191c0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472081
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974747}

--- chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.h
+++ chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.h
@@ -32,24 +32,23 @@ class ChromeBrowserMainPartsFuchsia : public ChromeBrowserMainParts {
   // content::BrowserMainParts overrides.
   int PreEarlyInitialization() override;
   int PreMainMessageLoopRun() override;
-  void PostMainMessageLoopRun() override;
 
  private:
-  class UseGraphicalPresenter;
-  class ViewProviderRouter;
+  class ViewPresenter;
 
   std::unique_ptr<base::ProcessLifecycle> lifecycle_;
 
-  // Implementations used when running under CFv2. Under CFv2 Chrome runs in the
-  // background, only opening windows when requested to via the
-  // fuchsia.element.Manager service. The browser process must remain live until
-  // explicitly torn-down by the ELF runner.
+  // Used to allow the shell to (re)open Chrome windows
   std::unique_ptr<ElementManagerImpl> element_manager_;
-  std::unique_ptr<ScopedKeepAlive> keep_alive_;
-  std::unique_ptr<UseGraphicalPresenter> use_graphical_presenter_;
 
-  // TODO(crbug.com/1284806): Remove this once ViewProvider is deprecated.
-  std::unique_ptr<ViewProviderRouter> view_provider_;
+  // Under CFv2 Chrome runs in the background, only opening windows when
+  // requested to via the fuchsia.element.Manager service. The browser process
+  // must remain live until explicitly torn-down by the ELF runner.
+  std::unique_ptr<ScopedKeepAlive> keep_alive_;
+
+  // Helper class that configures Ozone to use GraphicalPresenter to display a
+  // new View for each new top-level window.
+  std::unique_ptr<ViewPresenter> view_presenter_;
 };
 
 #endif  // CHROME_BROWSER_FUCHSIA_CHROME_BROWSER_MAIN_PARTS_FUCHSIA_H_
