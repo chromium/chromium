0cca9914f77de6cad1728855f5185260d85e3ef3
Lei Zhang <thestig@chromium.org>
2022-02-24 18:01:25 +0000

    Speculatively re-enable PdfFindRequestManagerTestWithPdfPartialLoading.
    This test passes locally across test runs. Speculatively re-enable it
    and see if the flakiness still occurs.
    
    Bug: 1247167
    Change-Id: I3da7cdcc8179aaa045b1bc82aabc195f8662ab67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486107
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974719}

--- chrome/browser/pdf/pdf_find_request_manager_browsertest.cc
+++ chrome/browser/pdf/pdf_find_request_manager_browsertest.cc
@@ -180,10 +180,8 @@ void SendRangeResponse(net::test_server::ControllableHttpResponse* response,
 
 // Tests searching in a PDF received in chunks via range-requests.  See also
 // https://crbug.com/1027173.
-
-// TODO(crbug.com/1247167): Flaky.
 IN_PROC_BROWSER_TEST_P(PdfFindRequestManagerTestWithPdfPartialLoading,
-                       DISABLED_FindInChunkedPDF) {
+                       FindInChunkedPDF) {
   constexpr uint32_t kStalledResponseSize =
       chrome_pdf::DocumentLoaderImpl::kDefaultRequestSize + 123;
 
