351f20f7f9b54d8ff1bb352c07288fcf45b10495
Xing Liu <xingliu@chromium.org>
2022-02-24 00:46:31 +0000

    Segmentation platform: Introduce an interface for feature processing.
    Introduce an abstract interface that generates data for ML model.
    
    Bug: 1300003
    Change-Id: I5714a3bc282c96c0b68a1fadab3235f6462dd15b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482602
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#974423}

---
+++ components/segmentation_platform/internal/execution/query_processor.h
@@ -0,0 +1,43 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SEGMENTATION_PLATFORM_INTERNAL_EXECUTION_QUERY_PROCESSOR_H_
+#define COMPONENTS_SEGMENTATION_PLATFORM_INTERNAL_EXECUTION_QUERY_PROCESSOR_H_
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "base/callback.h"
+
+namespace segmentation_platform {
+class FeatureProcessorState;
+
+// Interface that converts aribitrary data to a list of tensor in asynchronous
+// callback, which can be fed into machine learning model as training data or
+// inference data.
+class QueryProcessor {
+ public:
+  using FeatureIndex = int;
+  using Tensor = std::vector<float>;
+  using IndexedTensors = std::map<FeatureIndex, Tensor>;
+
+  // Processes the data and return the tensor values in |callback|.
+  virtual void Process(
+      std::unique_ptr<FeatureProcessorState> feature_processor_state,
+      base::OnceCallback<IndexedTensors> callback) = 0;
+
+  // Disallow copy/assign.
+  QueryProcessor(const QueryProcessor&) = delete;
+  QueryProcessor& operator=(const QueryProcessor&) = delete;
+
+  virtual ~QueryProcessor() = default;
+
+ protected:
+  QueryProcessor() = default;
+};
+
+}  // namespace segmentation_platform
+
+#endif  // COMPONENTS_SEGMENTATION_PLATFORM_INTERNAL_EXECUTION_QUERY_PROCESSOR_H_
