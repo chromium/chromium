9552d7f50c85dfb5176e905dfd9b9cc244f26546
Renato Silva <rrsilva@google.com>
2022-02-25 13:59:52 +0000

    [Sheriff] Re-enable Security Page tests on Mac.
    These tests should not be disabled on release versions on Mac.
    Otherwise test coverage is lost on that platform. This test was
    previously disabled in https://crrev.com/c/3479916 due to failures
    on the Mac10.12 builder. However, this builder did not have a successful
    build in more than a month.
    
    Bug: 1222886
    Change-Id: Ib159f16d9f16d69db44ab887709fc06824f105a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490243
    Owners-Override: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975101}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -642,8 +642,7 @@ TEST_F('CrSettingsSiteDataDetailsSubpageTest', 'All', function() {
 ].forEach(test => registerTest(...test));
 
 // Timeout on Linux dbg bots: https://crbug.com/1133412
-// Fails on Mac bots: https://crbug.com/1222886
-GEN('#if !((BUILDFLAG(IS_LINUX) && !defined(NDEBUG)) || BUILDFLAG(IS_MAC))');
+GEN('#if !(BUILDFLAG(IS_LINUX) && !defined(NDEBUG))');
 [['SecurityPage', 'security_page_test.js'],
 ].forEach(test => registerTest(...test));
 GEN('#endif');
