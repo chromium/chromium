c6c31a113479c289c299f5121dc2d62cb9802d46
Mike Wasserman <msw@chromium.org>
2022-02-25 21:38:56 +0000

    Window Placement: Fix crash from recursion amid change dispatch
    UpdateScreenInfos() crashed in recursive calls from DispatchEvent().
    (i.e. on screen events during a handler's nested window.print() loop)
    
    Enqueue async event dispatch to avoid UpdateScreenInfos() recursion.
    Also, skip change event dispatch steps during class initialization.
    Add a regression test to prevent similar crashes in the future.
    
    This change was spun out from https://crrev.com/c/3310042
    
    Bug: 1273841
    Test: Automated; No crash on screen events amid handler print() loops.
    Change-Id: I80bd177f43893a5d638736fe8a4b5b861b50357f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489926
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975279}

---
+++ chrome/browser/window_placement/window_placement_printing_interactive_uitest.cc
@@ -0,0 +1,135 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "build/chromeos_buildflags.h"
+#include "chrome/browser/printing/print_preview_dialog_controller.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/permissions/permission_request_manager.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "third_party/blink/public/common/features.h"
+#include "ui/display/screen_base.h"
+#include "ui/display/test/scoped_screen_override.h"
+#include "ui/display/test/test_screen.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/shell.h"
+#include "ui/display/test/display_manager_test_api.h"  // nogncheck
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+class WindowPlacementTest : public InProcessBrowserTest {
+ public:
+  void SetUpOnMainThread() override {
+    // Window placement features are only available on secure contexts.
+    https_test_server_ = std::make_unique<net::EmbeddedTestServer>(
+        net::EmbeddedTestServer::TYPE_HTTPS);
+    https_test_server_->ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+    ASSERT_TRUE(https_test_server_->Start());
+  }
+
+ protected:
+  std::unique_ptr<net::EmbeddedTestServer> https_test_server_;
+  base::test::ScopedFeatureList scoped_feature_list_{
+      blink::features::kWindowPlacement};
+};
+
+// TODO(crbug.com/1042990): Windows crashes static casting to ScreenWin.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_NoCrashOnEventsDuringHandlerPrint \
+  DISABLED_NoCrashOnEventsDuringHandlerPrint
+#else
+#define MAYBE_NoCrashOnEventsDuringHandlerPrint \
+  NoCrashOnEventsDuringHandlerPrint
+#endif
+// Test that screen change events occurring while an event handler is running
+// a nested event loop (i.e. via window.print()) do not cause a crash.
+// Regression test for crbug.com/1273841
+IN_PROC_BROWSER_TEST_F(WindowPlacementTest,
+                       MAYBE_NoCrashOnEventsDuringHandlerPrint) {
+  // Update the display configuration to mock display changes.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  display::test::DisplayManagerTestApi(ash::Shell::Get()->display_manager())
+      .UpdateDisplay("0+0-803x600");
+#else
+  display::ScreenBase screen;
+  screen.display_list().AddDisplay({1, gfx::Rect(0, 0, 803, 600)},
+                                   display::DisplayList::Type::PRIMARY);
+  display::test::ScopedScreenOverride screen_override(&screen);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+  ASSERT_EQ(1, display::Screen::GetScreen()->GetNumDisplays());
+
+  // Navigate in a new tab to observe the test screen instance as needed.
+  const GURL url(https_test_server_->GetURL("/simple.html"));
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), url, WindowOpenDisposition::NEW_FOREGROUND_TAB,
+      ui_test_utils::BROWSER_TEST_WAIT_FOR_TAB |
+          ui_test_utils::BROWSER_TEST_WAIT_FOR_LOAD_STOP);
+
+  // Auto-accept the Window Placement permission request.
+  auto* tab = browser()->tab_strip_model()->GetActiveWebContents();
+  permissions::PermissionRequestManager* permission_request_manager_tab =
+      permissions::PermissionRequestManager::FromWebContents(tab);
+  permission_request_manager_tab->set_auto_response_for_test(
+      permissions::PermissionRequestManager::ACCEPT_ALL);
+
+  // Add a currentscreenchange event handler that will freeze JS via print().
+  auto* script = R"(
+      function freezeJS(e) {
+        self.print();
+        screenDetails.removeEventListener('currentscreenchange', freezeJS);
+      }
+
+      let screenDetails;
+      self.getScreenDetails().then(s => {
+        screenDetails = s;
+        screenDetails.addEventListener('currentscreenchange', freezeJS);
+      });
+  )";
+  content::WebContentsAddedObserver web_contents_added_observer;
+  ASSERT_TRUE(EvalJs(tab, script).error.empty());
+
+  // Alter the display to trigger the currentscreenchange event and print().
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  display::test::DisplayManagerTestApi(ash::Shell::Get()->display_manager())
+      .UpdateDisplay("0+0-807x600");
+#else
+  screen.display_list().UpdateDisplay({1, gfx::Rect(0, 0, 807, 600)},
+                                      display::DisplayList::Type::PRIMARY);
+  EXPECT_EQ(screen.display_list().displays().size(), 1u);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+  ASSERT_EQ(1, display::Screen::GetScreen()->GetNumDisplays());
+
+  content::WebContents* print_preview =
+      web_contents_added_observer.GetWebContents();
+  EXPECT_EQ(print_preview, printing::PrintPreviewDialogController::GetInstance()
+                               ->GetPrintPreviewForContents(tab));
+  content::AwaitDocumentOnLoadCompleted(print_preview);
+
+  // Add a second display while the print preview dialog is showing.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  display::test::DisplayManagerTestApi(ash::Shell::Get()->display_manager())
+      .UpdateDisplay("0+0-807x600,1000+0-804x600");
+#else
+  screen.display_list().AddDisplay({2, gfx::Rect(1000, 0, 804, 600)},
+                                   display::DisplayList::Type::NOT_PRIMARY);
+  EXPECT_EQ(screen.display_list().displays().size(), 2u);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+  ASSERT_EQ(2, display::Screen::GetScreen()->GetNumDisplays());
+
+  // Cancel print to unfreeze the tab's JS and check that no crash occurred.
+  content::WebContentsDestroyedWatcher destroyed_watcher(print_preview);
+  // Note: This could be a browser_test if cancelling the print preview dialog
+  // was more broadly possible without simulating a key press.
+  // PrintViewManager::PrintPreviewDone() worked on Linux, but not Chrome OS.
+  EXPECT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_ESCAPE, false,
+                                              false, false, false));
+  destroyed_watcher.Wait();
+  ASSERT_FALSE(tab->IsCrashed());
+  EXPECT_EQ(true, EvalJs(tab, "true"));
+}
