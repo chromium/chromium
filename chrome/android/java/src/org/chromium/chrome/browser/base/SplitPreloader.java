68a655ba41bb23c86211b5a010173e25a6e97449
Trevor Perrier <perrier@chromium.org>
2022-02-25 19:33:40 +0000

    [SplitAppCompat] Override locale in preload context
    Fix for creating an isolated split context in SplitPreloader that has
    the global locale override applied.
    
    Bug: 1297941
    Change-Id: If69a0bff0e5401a3dbc2bc3bf1684e6d7c44ff37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490049
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975213}

--- chrome/android/java/src/org/chromium/chrome/browser/base/SplitPreloader.java
+++ chrome/android/java/src/org/chromium/chrome/browser/base/SplitPreloader.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.base;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.SystemClock;
 
 import androidx.collection.SimpleArrayMap;
@@ -14,6 +15,7 @@ import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.TaskTraits;
+import org.chromium.chrome.browser.language.GlobalAppLocaleController;
 
 /**
  * Handles preloading split Contexts on a background thread. Loading a new isolated split
@@ -87,7 +89,13 @@ public class SplitPreloader {
 
         private Context createSplitContext() {
             if (BundleUtils.isIsolatedSplitInstalled(mContext, mName)) {
-                return BundleUtils.createIsolatedSplitContext(mContext, mName);
+                Context context = BundleUtils.createIsolatedSplitContext(mContext, mName);
+                if (GlobalAppLocaleController.getInstance().isOverridden()) {
+                    Configuration config =
+                            GlobalAppLocaleController.getInstance().getOverrideConfig(context);
+                    context = context.createConfigurationContext(config);
+                }
+                return context;
             }
             return mContext;
         }
