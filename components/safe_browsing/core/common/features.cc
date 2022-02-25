fc5b758be9b31793802cedc1d1f7283d0cc5e6b5
Daniel Rubery <drubery@chromium.org>
2022-02-25 00:11:46 +0000

    Enable Tailored Security integration at ToT
    We're close to launching to 100% on Desktop, so enable the feature flag
    in code.
    
    Bug: 1256880
    Change-Id: I4476cefcf6df31d0258900d058fe58d436e04eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489073
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974920}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -98,7 +98,13 @@ const base::Feature kSimplifiedUrlDisplay{"SimplifiedUrlDisplay",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTailoredSecurityIntegration{
-    "TailoredSecurityIntegration", base::FEATURE_DISABLED_BY_DEFAULT};
+  "TailoredSecurityIntegration",
+#if BUILDFLAG(IS_ANDROID)
+      base::FEATURE_DISABLED_BY_DEFAULT
+#else
+      base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+};
 
 const base::Feature kOmitNonUserGesturesFromReferrerChain{
     "SafeBrowsingOmitNonUserGesturesFromReferrerChain",
