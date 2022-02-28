13bef0ffec93ce83fa1bf4f6a67d054de6fb9887
Alexander Hendrich <hendrich@chromium.org>
2022-02-28 10:43:33 +0000

    Re-enable PolicyPrefsTest.PolicyToPrefsMapping
    Bug did not contain any logs in plain text and the linked logs are not up-to-date anymore. Re-enabling this test in order to regenerate new flaky failures.
    
    Bug: 1294825
    Change-Id: Ifc14acf2e447d783797fea317502b51c18dc5531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488326
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975710}

--- chrome/browser/policy/policy_prefs_browsertest.cc
+++ chrome/browser/policy/policy_prefs_browsertest.cc
@@ -114,13 +114,11 @@ class PolicyPrefsTest : public PlatformBrowserTest {
 
 // Verifies that policies make their corresponding preferences become managed,
 // and that the user can't override that setting.
-// TODO(crbug.com/1294825) Flaky on ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_PolicyToPrefsMapping DISABLED_PolicyToPrefsMapping
-#else
-#define MAYBE_PolicyToPrefsMapping PolicyToPrefsMapping
-#endif
-IN_PROC_BROWSER_TEST_F(PolicyPrefsTest, MAYBE_PolicyToPrefsMapping) {
+// README SHERIFFs: This test encapsulates a whole suite of individual browser
+// tests for performance reasons and therefore has an increased chance of
+// failure/flakiness. Please add hendrich@chromium.org on any related bugs when
+// disabling this test.
+IN_PROC_BROWSER_TEST_F(PolicyPrefsTest, PolicyToPrefsMapping) {
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   policy::FakeBrowserDMTokenStorage storage;
   policy::BrowserDMTokenStorage::SetForTesting(&storage);
