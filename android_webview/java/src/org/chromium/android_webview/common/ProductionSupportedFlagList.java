3901f4caf891dabd6bcb1df2d099c2bfbaea6659
Steven Holte <holte@chromium.org>
2022-02-28 20:00:41 +0000

    Add a flag for VariationsFakeCrashAfterStartup.
    Change-Id: Ia2349ef32c8c8c0d7d6abe6e1981705494f44829
    Bug: 1238246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488816
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975903}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -12,6 +12,7 @@ import org.chromium.components.feature_engagement.FeatureConstants;
 import org.chromium.components.metrics.MetricsSwitches;
 import org.chromium.components.network_session_configurator.NetworkSessionSwitches;
 import org.chromium.components.power_scheduler.PowerSchedulerFeatures;
+import org.chromium.components.variations.VariationsFeatures;
 import org.chromium.components.viz.common.VizFeatures;
 import org.chromium.content_public.common.ContentFeatures;
 import org.chromium.content_public.common.ContentSwitches;
@@ -272,5 +273,7 @@ public final class ProductionSupportedFlagList {
             Flag.baseFeature(AwFeatures.WEBVIEW_X_REQUESTED_WITH_HEADER,
                     "Enables automatic insertion of XRequestedWith header "
                             + "on all outgoing requests."),
+            Flag.baseFeature(VariationsFeatures.VARIATIONS_FAKE_CRASH_AFTER_STARTUP,
+                    "Generates a crash dump without crashing after startup."),
     };
 }
