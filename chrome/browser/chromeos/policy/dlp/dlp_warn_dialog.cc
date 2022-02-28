feba7b79d52005f75ffd6c6967c12ffdb7c46362
Henrique Ferreiro <hferreiro@igalia.com>
2022-02-28 21:56:45 +0000

    Fix LayoutProvider include in chrome/browser/chromeos/
    Use the correct include for views::LayoutProvider and update DEPS file.
    
    Bug: 798569
    Change-Id: I7e6e05b9c8d56945174fe58cc4cdbd0a04422359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488329
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#975957}

--- chrome/browser/chromeos/policy/dlp/dlp_warn_dialog.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_warn_dialog.cc
@@ -10,7 +10,6 @@
 
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_confidential_contents.h"
-#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -22,6 +21,7 @@
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/layout_provider.h"
 #include "ui/views/widget/widget.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
