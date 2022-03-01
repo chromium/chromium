7af9133e92158c9e95f420b5d7ba815a9fddf4b5
Gavin Williams <gavinwill@chromium.org>
2022-03-01 22:24:46 +0000

    Input: Update a11y values for haptics slider
    This change maps the click sensitivity pref values fom [1,3,5] to
    [1,2,3] only for ChromeVox.
    
    Fixed: b/221105728
    Change-Id: If6d4d491b03d2a44da7cf3f98e1b02881b569580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491244
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976435}

--- chrome/browser/resources/settings/chromeos/device_page/pointers.js
+++ chrome/browser/resources/settings/chromeos/device_page/pointers.js
@@ -14,6 +14,7 @@ import '../../controls/settings_slider.js';
 import '../../controls/settings_toggle_button.js';
 import '../../settings_shared_css.js';
 
+import {SliderTick} from '//resources/cr_elements/cr_slider/cr_slider.js';
 import {loadTimeData} from '//resources/js/load_time_data.m.js';
 import {afterNextRender, flush, html, Polymer, TemplateInstanceBase, Templatizer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -90,9 +91,21 @@ Polymer({
       readOnly: true,
     },
 
+    /**
+     * The click sensitivity values from prefs are [1,3,5] but ChromeVox needs
+     * to announce them as [1,2,3].
+     * @type {!Array<SliderTick>}
+     * @private
+     */
     hapticClickSensitivityValues_: {
       type: Array,
-      value: [1, 3, 5],
+      value() {
+        return [
+          {value: 1, ariaValue: 1},
+          {value: 3, ariaValue: 2},
+          {value: 5, ariaValue: 3},
+        ];
+      },
       readOnly: true,
     },
 
