e58aefd89bcda87f47c15df097ae9fd2ad16c095
Mei Liang <meiliang@chromium.org>
2022-03-01 22:55:15 +0000

    [ConsentV2] Add consent dialog element
    This dialog will be used for the Chrome Cart module in the NTP. The spec
    has not been finalized yet, but the dialog should look very similar to
    this: https://screenshot.googleplex.com/6oQTEdJuy2JBnT9
    
    (cherry picked from commit 16c9e4532014afe820d8e4dae897faa06e578250)
    
    Change-Id: Ie8718c3c76a422a4cb42ad2d13be8a4be03e38fa
    Bug: 1298116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464702
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#974319}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498901
    Cr-Commit-Position: refs/branch-heads/4896@{#194}
    Cr-Branched-From: 1f63ff4bc27570761b35ffbc7f938f6586f7bee8-refs/heads/main@{#972766}

---
+++ chrome/test/data/webui/new_tab_page/modules/cart/discount_consent_dialog_test.ts
@@ -0,0 +1,76 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://new-tab-page/new_tab_page.js';
+import 'chrome://webui-test/mojo_webui_test_support.js';
+import {DiscountConsentDialog} from 'chrome://new-tab-page/new_tab_page.js';
+import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import {pressAndReleaseKeyOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+suite('NewTabPageDiscountConsentDialogTest', () => {
+  enum ActionSignal {
+    ACCEPTED,
+    DISMISSED,
+    REJECTED,
+  }
+
+  let discountConsentDialog: DiscountConsentDialog;
+  let actionSignalCaptured: ActionSignal;
+
+  setup(() => {
+    document.body.innerHTML = '';
+    discountConsentDialog = document.createElement('discount-consent-dialog');
+    document.body.appendChild(discountConsentDialog);
+
+    discountConsentDialog.addEventListener(
+        'discount-consent-accepted',
+        () => actionSignalCaptured = ActionSignal.ACCEPTED);
+    discountConsentDialog.addEventListener(
+        'discount-consent-rejected',
+        () => actionSignalCaptured = ActionSignal.REJECTED);
+    discountConsentDialog.addEventListener(
+        'discount-consent-dismissed',
+        () => actionSignalCaptured = ActionSignal.DISMISSED);
+  });
+
+  test('opens crDialog on attach', () => {
+    assertTrue(discountConsentDialog.$.dialog.open);
+  });
+
+  test(
+      'emits discount-consent-rejected event when reject button is clicked',
+      () => {
+        discountConsentDialog.$.cancelButton.click();
+        assertEquals(ActionSignal.REJECTED, actionSignalCaptured);
+      });
+
+  test(
+      'emits discount-consent-accepted event when accept button is clicked',
+      () => {
+        discountConsentDialog.$.confirmButton.click();
+        assertEquals(ActionSignal.ACCEPTED, actionSignalCaptured);
+      });
+
+  test(
+      'emits discount-consent-dismissed event when close button is clicked',
+      () => {
+        discountConsentDialog.shadowRoot!
+            .querySelector<CrDialogElement>('#dialog')!.cancel();
+        assertEquals(ActionSignal.DISMISSED, actionSignalCaptured);
+      });
+
+  test('emits discount-consent-dismissed event when esc key is pressed', () => {
+    pressAndReleaseKeyOn(discountConsentDialog, 27, '', 'Escape');
+    assertEquals(ActionSignal.DISMISSED, actionSignalCaptured);
+  });
+
+  test('verify dialogTitle property', () => {
+    discountConsentDialog.dialogTitle = 'Title';
+    const title =
+        discountConsentDialog.shadowRoot!.querySelector('div[slot=title]');
+    assertTrue(title != null);
+    assertEquals('Title', title.textContent);
+  });
+});
