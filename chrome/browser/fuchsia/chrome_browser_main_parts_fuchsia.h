29d955e928dbcec9df4e82713998ec2a55475242
Rohan Pavone <rohpavone@chromium.org>
2022-03-01 21:57:15 +0000

    Revert "[fuchsia] Remove Chrome CFv1 manifest and code"
    This reverts commit fccc58f0f92c06ece90a0afba5f8d553012feeb0.
    
    Reason for revert: CFv1 migration did not take into account E2E test needs.
    Also breaks browser_tests on FYI arm64 and x64 bots.
    
    Bug: 1301997,1301999, 1301800
    
    Original change's description:
    > [fuchsia] Remove Chrome CFv1 manifest and code
    >
    > Fuchsia now consumes the Chrome CFv2 manifest in all product
    > configurations, so the old CMX and associated code paths can be removed.
    >
    > Bug: 1298481, 1211758
    > Change-Id: Id51ff716c856ce96057b467fe516ad897191c0ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472081
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#974747}
    
    Bug: 1298481, 1211758
    Change-Id: I2454aeeacf29abb97ca0ef2cfc1fac37f1bd683a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3496048
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976421}

--- chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.h
+++ chrome/browser/fuchsia/chrome_browser_main_parts_fuchsia.h
@@ -32,23 +32,24 @@ class ChromeBrowserMainPartsFuchsia : public ChromeBrowserMainParts {
   // content::BrowserMainParts overrides.
   int PreEarlyInitialization() override;
   int PreMainMessageLoopRun() override;
+  void PostMainMessageLoopRun() override;
 
  private:
-  class ViewPresenter;
+  class UseGraphicalPresenter;
+  class ViewProviderRouter;
 
   std::unique_ptr<base::ProcessLifecycle> lifecycle_;
 
-  // Used to allow the shell to (re)open Chrome windows
+  // Implementations used when running under CFv2. Under CFv2 Chrome runs in the
+  // background, only opening windows when requested to via the
+  // fuchsia.element.Manager service. The browser process must remain live until
+  // explicitly torn-down by the ELF runner.
   std::unique_ptr<ElementManagerImpl> element_manager_;
-
-  // Under CFv2 Chrome runs in the background, only opening windows when
-  // requested to via the fuchsia.element.Manager service. The browser process
-  // must remain live until explicitly torn-down by the ELF runner.
   std::unique_ptr<ScopedKeepAlive> keep_alive_;
+  std::unique_ptr<UseGraphicalPresenter> use_graphical_presenter_;
 
-  // Helper class that configures Ozone to use GraphicalPresenter to display a
-  // new View for each new top-level window.
-  std::unique_ptr<ViewPresenter> view_presenter_;
+  // TODO(crbug.com/1284806): Remove this once ViewProvider is deprecated.
+  std::unique_ptr<ViewProviderRouter> view_provider_;
 };
 
 #endif  // CHROME_BROWSER_FUCHSIA_CHROME_BROWSER_MAIN_PARTS_FUCHSIA_H_
