0f1c4a6fe4fdaf12ca93973eb95032a7370468f7
Mark Pearson <mpearson@chromium.org>
2022-03-02 01:10:21 +0000

    Disable PaymentRequestPaymentAppTest.NotSupportedError on Linux
    It's flaky.
    
    BUG=1296289
    
    Change-Id: I018a3bbdd6fa7daa3edcae5dc9f93a9df83b605f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498865
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Owners-Override: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976494}

--- chrome/browser/ui/views/payments/payment_request_payment_app_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_request_payment_app_browsertest.cc
@@ -4,6 +4,7 @@
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -170,7 +171,13 @@ class PaymentRequestPaymentAppTest : public PaymentRequestBrowserTestBase {
 };
 
 // Test payment request methods are not supported by the payment app.
-IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, NotSupportedError) {
+// Flaky on Linux: http://crbug.com/1296289
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_NotSupportedError DISABLED_NotSupportedError
+#else
+#define MAYBE_NotSupportedError NotSupportedError
+#endif
+IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, MAYBE_NotSupportedError) {
   InstallAlicePayForMethod("https://frankpay.com");
 
   {
