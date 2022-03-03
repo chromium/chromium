a369e46581ca927f08a498415e2ed54c9119e0d3
Daniel White <danieltwhite@google.com>
2022-03-03 14:58:14 +0000

    Convert VoiceSearchNavigationTabHelperTest
    In preparation to remove ChromeWebTest, convert
    VoiceSearchNavigationTabHelperTest to PlatformTest.
    
    Bug: 1290862
    Change-Id: Id0e8971f631e054c24941bdccdd2095761671757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498522
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#977084}

--- ios/chrome/browser/voice/voice_search_navigations_tab_helper_unittest.mm
+++ ios/chrome/browser/voice/voice_search_navigations_tab_helper_unittest.mm
@@ -4,23 +4,38 @@
 
 #import "ios/chrome/browser/voice/voice_search_navigations_tab_helper.h"
 
-#import "ios/chrome/browser/web/chrome_web_test.h"
+#include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#include "ios/web/public/test/web_state_test_util.h"
+#include "ios/web/public/test/web_task_environment.h"
+#include "testing/platform_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 // Test fixture for VoiceSearchNavigations.
-class VoiceSearchNavigationsTest : public ChromeWebTest {
+class VoiceSearchNavigationsTest : public PlatformTest {
  public:
   void SetUp() override {
-    ChromeWebTest::SetUp();
+    PlatformTest::SetUp();
+    browser_state_ = TestChromeBrowserState::Builder().Build();
+
+    web::WebState::CreateParams params(browser_state_.get());
+    web_state_ = web::WebState::Create(params);
+
     VoiceSearchNavigationTabHelper::CreateForWebState(web_state());
   }
 
   VoiceSearchNavigationTabHelper* navigations() {
     return VoiceSearchNavigationTabHelper::FromWebState(web_state());
   }
+
+ protected:
+  web::WebState* web_state() { return web_state_.get(); }
+
+  web::WebTaskEnvironment task_environment_;
+  std::unique_ptr<TestChromeBrowserState> browser_state_;
+  std::unique_ptr<web::WebState> web_state_;
 };
 
 // Tests that a navigation commit reset the value of
@@ -28,6 +43,6 @@ class VoiceSearchNavigationsTest : public ChromeWebTest {
 TEST_F(VoiceSearchNavigationsTest, CommitResetVoiceSearchExpectation) {
   navigations()->WillLoadVoiceSearchResult();
   EXPECT_TRUE(navigations()->IsExpectingVoiceSearch());
-  LoadHtml(@"<html></html>");
+  web::test::LoadHtml(@"<html></html>", web_state());
   EXPECT_FALSE(navigations()->IsExpectingVoiceSearch());
 }
