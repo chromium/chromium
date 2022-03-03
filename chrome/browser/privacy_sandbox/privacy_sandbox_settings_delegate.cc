5db1bf0a6fabd882819240efd2ad03f1078a42c0
Theodore Olsauskas-Warren <sauski@google.com>
2022-03-03 14:25:53 +0000

    Privacy Sandbox Settings: Include account capability in restricted check
    CL extends the logic which determines whether the Privacy Sandbox is
    restricted to include checking the new account capability.
    
    Bug: 1286276
    Change-Id: I64050ef8fe6cdce0f45b48025fd4b9b933853743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495282
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#977072}

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.cc
@@ -5,9 +5,33 @@
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.h"
 
 #include "base/feature_list.h"
-#include "base/logging.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/signin/public/identity_manager/tribool.h"
+
+namespace {
+
+bool PrivacySandboxRestrictedByAcccountCapability(Profile* profile) {
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+
+  if (!identity_manager)
+    return false;
+
+  const auto core_account_info =
+      identity_manager->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin);
+  const AccountInfo account_info =
+      identity_manager->FindExtendedAccountInfo(core_account_info);
+  auto capability =
+      account_info.capabilities.can_run_chrome_privacy_sandbox_trials();
+
+  // The Privacy Sandbox is not considered restricted unless the capability
+  // has a definitive false signal.
+  return capability == signin::Tribool::kFalse;
+}
+
+}  // namespace
 
 PrivacySandboxSettingsDelegate::PrivacySandboxSettingsDelegate(Profile* profile)
     : profile_(profile) {}
@@ -18,8 +42,8 @@ bool PrivacySandboxSettingsDelegate::IsPrivacySandboxRestricted() {
   // When the Privacy Sandbox 3 feature is enabled, the Sandbox is restricted
   // for Child users.
   if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings3)) {
-    // TODO(crbug.com/1286276): Integrate other identity provided signals.
-    return profile_->IsChild();
+    return profile_->IsChild() ||
+           PrivacySandboxRestrictedByAcccountCapability(profile_);
   }
   // No restrictions apply otherwise.
   return false;
---
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate_unittest.cc
@@ -0,0 +1,108 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.h"
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
+#include "chrome/browser/supervised_user/supervised_user_test_util.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/privacy_sandbox/privacy_sandbox_features.h"
+#include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
+#include "components/signin/public/identity_manager/identity_test_environment.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+constexpr char kTestEmail[] = "test@test.com";
+
+}
+
+class PrivacySandboxSettingsDelegateTest : public testing::Test {
+ public:
+  PrivacySandboxSettingsDelegateTest() {
+    profile_ = IdentityTestEnvironmentProfileAdaptor::
+        CreateProfileForIdentityTestEnvironment();
+    adapter_ =
+        std::make_unique<IdentityTestEnvironmentProfileAdaptor>(profile_.get());
+    identity_test_env()->MakePrimaryAccountAvailable(
+        kTestEmail, signin::ConsentLevel::kSignin);
+    delegate_ =
+        std::make_unique<PrivacySandboxSettingsDelegate>(profile_.get());
+  }
+
+ protected:
+  void SetPrivacySandboxAccountCapability(const std::string& account,
+                                          bool enabled) {
+    auto account_info = identity_test_env()
+                            ->identity_manager()
+                            ->FindExtendedAccountInfoByEmailAddress(kTestEmail);
+    AccountCapabilitiesTestMutator mutator(&account_info.capabilities);
+    // The Privacy Sandbox capability is currently connected to the sync promo.
+    // TODO(crbug.com/1298865): Replace with the actual capability when it is
+    // available.
+    mutator.set_can_offer_extended_chrome_sync_promos(enabled);
+    signin::UpdateAccountInfoForAccount(identity_test_env()->identity_manager(),
+                                        account_info);
+  }
+
+  PrivacySandboxSettingsDelegate* delegate() { return delegate_.get(); }
+  base::test::ScopedFeatureList* feature_list() { return &feature_list_; }
+  signin::IdentityTestEnvironment* identity_test_env() {
+    return adapter_->identity_test_env();
+  }
+  TestingProfile* profile() { return profile_.get(); }
+
+ private:
+  content::BrowserTaskEnvironment browser_task_environment_;
+  base::test::ScopedFeatureList feature_list_;
+  std::unique_ptr<IdentityTestEnvironmentProfileAdaptor> adapter_;
+  std::unique_ptr<TestingProfile> profile_;
+  std::unique_ptr<PrivacySandboxSettingsDelegate> delegate_;
+};
+
+TEST_F(PrivacySandboxSettingsDelegateTest, CapabilityRestriction) {
+  feature_list()->InitAndEnableFeature(
+      privacy_sandbox::kPrivacySandboxSettings3);
+  // Initially the account capability will be in an unknown state, which
+  // should be interpreted as no restriction.
+  EXPECT_FALSE(delegate()->IsPrivacySandboxRestricted());
+
+  // Altering the capability to either enabled or disabled should be reflected
+  // as a restriction on the sandbox.
+  SetPrivacySandboxAccountCapability(kTestEmail, false);
+  EXPECT_TRUE(delegate()->IsPrivacySandboxRestricted());
+  SetPrivacySandboxAccountCapability(kTestEmail, true);
+  EXPECT_FALSE(delegate()->IsPrivacySandboxRestricted());
+
+  // If the Privacy Sandbox Settings 3 feature is disabled the capability
+  // restriction should not apply.
+  feature_list()->Reset();
+  feature_list()->InitAndDisableFeature(
+      privacy_sandbox::kPrivacySandboxSettings3);
+  SetPrivacySandboxAccountCapability(kTestEmail, false);
+  EXPECT_FALSE(delegate()->IsPrivacySandboxRestricted());
+}
+
+#if BUILDFLAG(ENABLE_SUPERVISED_USERS)
+TEST_F(PrivacySandboxSettingsDelegateTest, SupervisedUser) {
+  // The sandbox should always be restricted for supervised users.
+  profile()->SetIsSupervisedProfile();
+  feature_list()->InitAndEnableFeature(
+      privacy_sandbox::kPrivacySandboxSettings3);
+  EXPECT_TRUE(delegate()->IsPrivacySandboxRestricted());
+
+  // The capability should not override profile supervision.
+  SetPrivacySandboxAccountCapability(kTestEmail, true);
+  EXPECT_TRUE(delegate()->IsPrivacySandboxRestricted());
+
+  // If the Privacy Sandbox Settings 3 feature is disabled, the supervised
+  // user restriction should not apply.
+  feature_list()->Reset();
+  feature_list()->InitAndDisableFeature(
+      privacy_sandbox::kPrivacySandboxSettings3);
+  EXPECT_FALSE(delegate()->IsPrivacySandboxRestricted());
+}
+#endif
