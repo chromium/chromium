fced18a29d11cfdcc9b74cb0490533c0cca8232d
Yoichi Osato <yoichio@chromium.org>
2022-03-03 05:43:13 +0000

    Introduce sandboxed NetworkQualityTracker test
    This CL adds a test that confirms OnEffectiveConnectionTypeChanged
    listening from NetworkQualityEstimator is working in a sandboxed
    process.
    
    Bug: 1297437
    Change-Id: I329b3d5566e33e7503b667bc3225c70270dbbf9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3478995
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976982}

--- content/browser/net/sandboxed_nqe_browsertest.cc
+++ content/browser/net/sandboxed_nqe_browsertest.cc
@@ -11,9 +11,47 @@
 #include "content/public/test/content_browser_test.h"
 #include "sandbox/features.h"
 #include "sandbox/policy/features.h"
+#include "services/network/public/cpp/network_quality_tracker.h"
+#include "services/network/public/mojom/network_service_test.mojom.h"
 
 namespace content {
 namespace {
+
+class TestNetworkQualityObserver
+    : public network::NetworkQualityTracker::EffectiveConnectionTypeObserver {
+ public:
+  TestNetworkQualityObserver() = default;
+
+  // NetworkQualityTracker::EffectiveConnectionTypeObserver implementation:
+  void OnEffectiveConnectionTypeChanged(
+      net::EffectiveConnectionType type) override {
+    effective_connection_type_ = type;
+    if (effective_connection_type_ != run_loop_wait_effective_connection_type_)
+      return;
+    run_loop_->Quit();
+  }
+
+  void WaitForNotification(
+      net::EffectiveConnectionType run_loop_wait_effective_connection_type) {
+    if (effective_connection_type_ == run_loop_wait_effective_connection_type)
+      return;
+    run_loop_wait_effective_connection_type_ =
+        run_loop_wait_effective_connection_type;
+    run_loop_ = std::make_unique<base::RunLoop>();
+    run_loop_->Run();
+  }
+
+  net::EffectiveConnectionType EffectiveConnectionType() const {
+    return effective_connection_type_;
+  }
+
+ private:
+  std::unique_ptr<base::RunLoop> run_loop_;
+  net::EffectiveConnectionType run_loop_wait_effective_connection_type_ =
+      net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN;
+  net::EffectiveConnectionType effective_connection_type_ =
+      net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN;
+};
 
 class SandboxedNQEBrowserTest : public ContentBrowserTest {
  public:
@@ -47,15 +85,61 @@ class SandboxedNQEBrowserTest : public ContentBrowserTest {
     ContentBrowserTest::SetUp();
   }
 
+  // Simulates a network quality change.
+  void SimulateNetworkQualityChange(net::EffectiveConnectionType type) {
+    mojo::ScopedAllowSyncCallForTesting allow_sync_call;
+    DCHECK(content::GetNetworkService());
+
+    mojo::Remote<network::mojom::NetworkServiceTest> network_service_test;
+    content::GetNetworkService()->BindTestInterface(
+        network_service_test.BindNewPipeAndPassReceiver());
+    base::RunLoop run_loop;
+    network_service_test->SimulateNetworkQualityChange(type,
+                                                       run_loop.QuitClosure());
+    run_loop.Run();
+  }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
+// NetworkQualityEstimater used to call android syscall to gather device info
+// in the constructor, which has been removed. This test confirms that.
 IN_PROC_BROWSER_TEST_F(SandboxedNQEBrowserTest, GetNetworkService) {
   EXPECT_TRUE(GetNetworkService());
 }
 
-// TODO(yoichio): Add more tests.
+// Simulate EffectiveConnectionType change in NetworkQualityEstimator and
+// reports it to mojo client.
+IN_PROC_BROWSER_TEST_F(SandboxedNQEBrowserTest, NetworkQualityTracker) {
+  // Change the network quality to UNKNOWN to prevent any spurious
+  // notifications.
+  SimulateNetworkQualityChange(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN);
+
+  std::unique_ptr<network::NetworkQualityTracker> tracker =
+      std::make_unique<network::NetworkQualityTracker>(
+          base::BindRepeating(&GetNetworkService));
+  TestNetworkQualityObserver network_quality_observer;
+  tracker->AddEffectiveConnectionTypeObserver(&network_quality_observer);
+
+  SimulateNetworkQualityChange(net::EFFECTIVE_CONNECTION_TYPE_4G);
+  network_quality_observer.WaitForNotification(
+      net::EFFECTIVE_CONNECTION_TYPE_4G);
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_4G,
+            network_quality_observer.EffectiveConnectionType());
+
+  SimulateNetworkQualityChange(net::EFFECTIVE_CONNECTION_TYPE_3G);
+  network_quality_observer.WaitForNotification(
+      net::EFFECTIVE_CONNECTION_TYPE_3G);
+  EXPECT_EQ(net::EFFECTIVE_CONNECTION_TYPE_3G,
+            network_quality_observer.EffectiveConnectionType());
+
+  // Typical RTT and downlink values when effective connection type is 3G. Taken
+  // from net::NetworkQualityEstimatorParams.
+  EXPECT_EQ(base::Milliseconds(450), tracker->GetHttpRTT());
+  EXPECT_EQ(base::Milliseconds(400), tracker->GetTransportRTT());
+  EXPECT_EQ(400, tracker->GetDownstreamThroughputKbps());
+}
 
 }  // namespace
 }  // namespace content
