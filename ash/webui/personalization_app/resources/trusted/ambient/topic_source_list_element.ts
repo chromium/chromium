b6461dbac765640864f9a00ca476da02d14e6d08
Jason Thai <jasontt@chromium.org>
2022-03-03 21:47:36 +0000

    personalization: fix topic sources not rendering correctly
    Topic sources element failed to render when it becomes visible
    again after navigating back from album pages on reload. Force
    rendering when the element becomes available.
    
    BUG=b/222092417
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I073b68a83ce426173a07e0746150c13f5fcc0db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498957
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977341}

--- ash/webui/personalization_app/resources/trusted/ambient/topic_source_list_element.ts
+++ ash/webui/personalization_app/resources/trusted/ambient/topic_source_list_element.ts
@@ -10,7 +10,7 @@
 import 'chrome://personalization/trusted/ambient/topic_source_item_element.js';
 import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 
-import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {afterNextRender, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {TopicSource} from '../personalization_app.mojom-webui.js';
 import {WithPersonalizationStore} from '../personalization_store.js';
@@ -26,6 +26,12 @@ export class TopicSourceList extends WithPersonalizationStore {
 
   static get properties() {
     return {
+      hidden: {
+        type: Boolean,
+        reflectToAttribute: true,
+        observer: 'onHiddenChanged_',
+      },
+
       topicSources: {
         type: Array,
         value: [TopicSource.kGooglePhotos, TopicSource.kArtGallery],
@@ -37,9 +43,23 @@ export class TopicSourceList extends WithPersonalizationStore {
     };
   }
 
+  hidden: boolean;
   topicSources: Array<TopicSource>;
   selectedTopicSource: TopicSource;
   hasGooglePhotosAlbums: boolean;
+
+  private onHiddenChanged_(hidden: boolean) {
+    if (hidden) {
+      return;
+    }
+
+    // When iron-list items change while their parent element is hidden, the
+    // iron-list will render incorrectly. Force relayout by invalidating the
+    // iron-list when this element becomes visible.
+    afterNextRender(this, () => {
+      this.shadowRoot!.querySelector('iron-list')!.fire('iron-resize');
+    });
+  }
 
   private isSelected_(
       topicSource: TopicSource, selectedTopicSource: TopicSource) {
