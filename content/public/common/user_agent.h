39bebbe2fffc1956168e417ed44bffc34c144726
Mike Taylor <miketaylr@chromium.org>
2022-03-03 22:16:44 +0000

    Freeze CrOS platform version to 14541.0.0 when UA Reduction flag is enabled.
    Bug: 1302637
    Change-Id: I5026b15b282961c33dc3515d5dd571f764088105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498489
    Reviewed-by: Ali Beyad <abeyad@chromium.org>
    Auto-Submit: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977358}

--- content/public/common/user_agent.h
+++ content/public/common/user_agent.h
@@ -23,7 +23,7 @@ const char kAndroid[] =
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s.0.0.0 %s"
     "Safari/537.36";
 const char kUnifiedPlatformAndroid[] = "Linux; Android 10; K";
-const char kUnifiedPlatformCrOS[] = "X11; CrOS x86_64";
+const char kUnifiedPlatformCrOS[] = "X11; CrOS x86_64 14541.0.0";
 const char kUnifiedPlatformLinux[] = "X11; Linux x86_64";
 const char kUnifiedPlatformMacOS[] = "Macintosh; Intel Mac OS X 10_15_7";
 const char kUnifiedPlatformWindows[] = "Windows NT 10.0; Win64; x64";
