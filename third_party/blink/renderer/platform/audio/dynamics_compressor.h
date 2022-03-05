ded220f1a779f200f6fecb23883e2caaa4c001f9
Michael Wilson <mjwilson@chromium.org>
2022-03-05 02:30:15 +0000

    Move dynamics_compressor_kernel into dynamics_compressor
    This is a first step into further optimizations of dynamics_compressor.
    This CL should not affect functionality.
    
    This is a sub-change of https://crrev.com/c/3461396
    
    Change-Id: I9526ba498ac46a38c577b03c536c474e9e3bd457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499756
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977928}

--- third_party/blink/renderer/platform/audio/dynamics_compressor.h
+++ third_party/blink/renderer/platform/audio/dynamics_compressor.h
@@ -31,11 +31,111 @@
 
 #include <memory>
 #include "third_party/blink/renderer/platform/audio/audio_array.h"
-#include "third_party/blink/renderer/platform/audio/dynamics_compressor_kernel.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
+
+class DynamicsCompressorKernel {
+  DISALLOW_NEW();
+
+ public:
+  DynamicsCompressorKernel(float sample_rate, unsigned number_of_channels);
+  DynamicsCompressorKernel(const DynamicsCompressorKernel&) = delete;
+  DynamicsCompressorKernel& operator=(const DynamicsCompressorKernel&) = delete;
+
+  void SetNumberOfChannels(unsigned);
+
+  // Performs stereo-linked compression.
+  void Process(const float* source_channels[],
+               float* destination_channels[],
+               unsigned number_of_channels,
+               unsigned frames_to_process,
+
+               float db_threshold,
+               float db_knee,
+               float ratio,
+               float attack_time,
+               float release_time,
+               float pre_delay_time,
+               float db_post_gain,
+               float effect_blend,
+
+               float release_zone1,
+               float release_zone2,
+               float release_zone3,
+               float release_zone4);
+
+  void Reset();
+
+  unsigned LatencyFrames() const { return last_pre_delay_frames_; }
+
+  float SampleRate() const { return sample_rate_; }
+
+  float MeteringGain() const { return metering_gain_; }
+
+  double TailTime() const;
+
+ protected:
+  float sample_rate_;
+
+  float detector_average_;
+  float compressor_gain_;
+
+  // Metering
+  float metering_release_k_;
+  float metering_gain_;
+
+  // Lookahead section.
+  enum { kMaxPreDelayFrames = 1024 };
+  enum { kMaxPreDelayFramesMask = kMaxPreDelayFrames - 1 };
+  enum {
+    kDefaultPreDelayFrames = 256
+  };  // setPreDelayTime() will override this initial value
+  unsigned last_pre_delay_frames_;
+  void SetPreDelayTime(float);
+
+  Vector<std::unique_ptr<AudioFloatArray>> pre_delay_buffers_;
+  int pre_delay_read_index_;
+  int pre_delay_write_index_;
+
+  float max_attack_compression_diff_db_;
+
+  // Static compression curve.
+  float KneeCurve(float x, float k) const;
+  float Saturate(float x, float k) const;
+  float SlopeAt(float x, float k) const;
+  float KAtSlope(float desired_slope) const;
+
+  float UpdateStaticCurveParameters(float db_threshold,
+                                    float db_knee,
+                                    float ratio);
+
+  // Amount of input change in dB required for 1 dB of output change.
+  // This applies to the portion of the curve above m_kneeThresholdDb (see
+  // below).
+  float ratio_;
+  float slope_;  // Inverse ratio.
+
+  // The input to output change below the threshold is linear 1:1.
+  float linear_threshold_;
+  float db_threshold_;
+
+  // m_dbKnee is the number of dB above the threshold before we enter the
+  // "ratio" portion of the curve.
+  // m_kneeThresholdDb = m_dbThreshold + m_dbKnee
+  // The portion between m_dbThreshold and m_kneeThresholdDb is the "soft knee"
+  // portion of the curve which transitions smoothly from the linear portion to
+  // the ratio portion.
+  float db_knee_;
+  float knee_threshold_;
+  float knee_threshold_db_;
+  float yknee_threshold_db_;
+
+  // Internal parameter for the knee portion of the curve.
+  float knee_;
+};
 
 class AudioBus;
 
