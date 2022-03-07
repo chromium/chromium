d48fd383bdcd9952c690f9b729c38313895d19b0
Hans Wennborg <hans@chromium.org>
2022-03-07 13:20:59 +0000

    Roll clang llvmorg-15-init-1995-g5bec1ea7-4 : llvmorg-15-init-3540-g85c53c70-1
    Ran `tools/clang/scripts/upload_revision.py 85c53c7092176e5f611fe620867eb2b82a30c80d`.
    
    Bug: 1298960
    Binary-Size: Android +60 KB. We'll investigate but not block on this.
    Change-Id: I0a2ec2d10328f9db504786251fa78b0901556f8b
    Cq-Include-Trybots: chromium/try:android-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:win-asan,win7-rel
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3503256
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978234}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,8 +35,8 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-15-init-1995-g5bec1ea7'
-CLANG_SUB_REVISION = 4
+CLANG_REVISION = 'llvmorg-15-init-3540-g85c53c70'
+CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
 RELEASE_VERSION = '15.0.0'
