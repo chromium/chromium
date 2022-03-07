e3e7862718bce4eb785c59d96359a35ca64c6ac5
liberato@chromium.org <liberato@chromium.org>
2022-03-07 20:17:13 +0000

    [M99] Don't use a deleted RenderFrameHost.
    Since we do not check for frame liveness, a RenderFrameHost might be
    deleted (in the use-after-free sense) without another call to
    RenderFrameDeleted.  So, WeakPtr it to avoid these cases.
    
    (cherry picked from commit 4d71d571302a75192f26d9e1122150ae5c600abb)
    
    Bug: 1299422
    Change-Id: Ie4fe85f88ef80f4e4c3d0452397c0e5050ed881c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482540
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#976357}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508293
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4844@{#997}
    Cr-Branched-From: 007241ce2e6c8e5a7b306cc36c730cd07cd38825-refs/heads/main@{#961656}

--- content/browser/display_cutout/display_cutout_host_impl.h
+++ content/browser/display_cutout/display_cutout_host_impl.h
@@ -6,6 +6,7 @@
 #define CONTENT_BROWSER_DISPLAY_CUTOUT_DISPLAY_CUTOUT_HOST_IMPL_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/render_frame_host_receiver_set.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "third_party/blink/public/mojom/page/display_cutout.mojom.h"
@@ -13,6 +14,7 @@
 
 namespace content {
 
+class RenderFrameHostImpl;
 class WebContentsImpl;
 
 class DisplayCutoutHostImpl : public blink::mojom::DisplayCutoutHost {
@@ -67,7 +69,7 @@ class DisplayCutoutHostImpl : public blink::mojom::DisplayCutoutHost {
   // `WebContentsImpl::current_fullscreen_frame_` because it also considers
   // browser side driven fullscreen mode, not just renderer side requested
   // frames.
-  raw_ptr<RenderFrameHost> current_rfh_ = nullptr;
+  base::WeakPtr<RenderFrameHostImpl> current_rfh_;
 
   // Stores a map of RenderFrameHosts and their current viewport fit values.
   std::map<RenderFrameHost*, blink::mojom::ViewportFit> values_;
