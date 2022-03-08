c65464f44dd20ecc9f161c0b9309787562e989c7
Clark DuVall <cduvall@chromium.org>
2022-03-08 22:10:56 +0000

    Add crash key for debugging Resource::AppendData crash
    Bug: 1302204
    Change-Id: Ide97e43cd38d5bc7a73ff560fe7c6bdbe2297346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508402
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978904}

--- third_party/blink/renderer/platform/loader/fetch/resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource.cc
@@ -30,6 +30,8 @@
 #include <cassert>
 #include <memory>
 
+#include "base/debug/crash_logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/default_clock.h"
 #include "build/build_config.h"
@@ -253,10 +255,14 @@ void Resource::AppendData(const char* data, size_t length) {
   DCHECK(!is_revalidating_);
   DCHECK(!ErrorOccurred());
   if (options_.data_buffering_policy == kBufferData) {
-    if (data_)
+    if (data_) {
       data_->Append(data, length);
-    else
+    } else {
+      // TODO(crbug.com/1302204): Remove this once the crash is fixed.
+      SCOPED_CRASH_KEY_STRING32("Resource", "append_data_length",
+                                base::NumberToString(length));
       data_ = SharedBuffer::Create(data, length);
+    }
     SetEncodedSize(data_->size());
   }
   NotifyDataReceived(data, length);
