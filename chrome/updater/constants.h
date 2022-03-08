d1b41d76ba7cfe8b62278cce972a470a6b9d9b94
Joshua Pawlicki <waffles@chromium.org>
2022-03-08 14:40:18 +0000

    Revert "Updater: Create a mac metainstaller."
    This reverts commit 9e98f675ab502435e1ace0a4541b2681aace8323.
    
    Reason for revert: crbug.com/1304108 - it turns out there is a monopoly on the "installer" target name.
    
    Original change's description:
    > Updater: Create a mac metainstaller.
    >
    > This metainstaller wraps the distribution of the updater into a single
    > executable, and will be used as part of the recovery component payload.
    >
    > It does not support tagging (at this time).
    >
    > Bug: 1281971
    > Change-Id: I48ff31db1b8ba2840975453ababe0a926c4caa99
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489228
    > Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#978357}
    
    Bug: 1281971
    Change-Id: I432dd49d5e6aa0222847482ee196cfb75d5199ce
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507893
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#978710}

--- chrome/updater/constants.h
+++ chrome/updater/constants.h
@@ -300,10 +300,6 @@ constexpr int kErrorFailedAwaitingLaunchdUpdateServiceInternalJob = 32;
 
 constexpr int kErrorTagParsing = 50;
 
-// Metainstaller errors.
-constexpr int kErrorCreatingTempDir = 60;
-constexpr int kErrorUnpackingResource = 61;
-
 // Policy Management constants.
 // The maximum value allowed for policy AutoUpdateCheckPeriodMinutes.
 constexpr int kMaxAutoUpdateCheckPeriodMinutes = 43200;
