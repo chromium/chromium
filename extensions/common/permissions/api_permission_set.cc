f1a99bb03c757d102e7921fb556286a235e65ab3
Xiaohan Wang <xhwang@chromium.org>
2022-03-08 00:13:03 +0000

    permissions: Replace LOG(WARNING) with VLOG(1)
    In a debug build api_permission_set.cc is generating a log of warning
    messages that normal developers aren't really interested in.
    
    Replace it with DVLOG(1) as suggested in Chromium coding style:
    https://chromium.googlesource.com/chromium/src/+/HEAD/styleguide/c++/c++.md#logging
    
    Change-Id: Icff89586ec93c2312da8752dd1a7daae56c3b813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508218
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978531}

--- extensions/common/permissions/api_permission_set.cc
+++ extensions/common/permissions/api_permission_set.cc
@@ -68,7 +68,7 @@ bool CreateAPIPermission(const std::string& permission_str,
         }
         return false;
       }
-      LOG(WARNING) << "Parse permission failed.";
+      VLOG(1) << "Parse permission failed.";
     } else {
       api_permissions->insert(std::move(permission));
     }
@@ -78,7 +78,7 @@ bool CreateAPIPermission(const std::string& permission_str,
   if (unhandled_permissions)
     unhandled_permissions->push_back(permission_str);
   else
-    LOG(WARNING) << "Unknown permission[" << permission_str << "].";
+    VLOG(1) << "Unknown permission[" << permission_str << "].";
 
   return true;
 }
@@ -96,7 +96,7 @@ bool ParseChildPermissions(const std::string& base_name,
             errors::kInvalidPermission, base_name);
         return false;
       }
-      LOG(WARNING) << "Permission value is not a list.";
+      VLOG(1) << "Permission value is not a list.";
       // Failed to parse, but since error is NULL, failures are not fatal so
       // return true here anyway.
       return true;
@@ -114,7 +114,7 @@ bool ParseChildPermissions(const std::string& base_name,
               base_name + '.' + base::NumberToString(i));
           return false;
         }
-        LOG(WARNING) << "Permission is not a string.";
+        VLOG(1) << "Permission is not a string.";
         continue;
       }
 
@@ -155,7 +155,7 @@ bool APIPermissionSet::ParseFromJSON(
                                                    "<root>");
       return false;
     }
-    LOG(WARNING) << "Root Permissions value is not a list.";
+    VLOG(1) << "Root Permissions value is not a list.";
     // Failed to parse, but since error is NULL, failures are not fatal so
     // return true here anyway.
     return true;
@@ -177,7 +177,7 @@ bool APIPermissionSet::ParseFromJSON(
                                                      base::NumberToString(i));
         return false;
       }
-      LOG(WARNING) << "Permission is not a string or single key dict.";
+      VLOG(1) << "Permission is not a string or single key dict.";
       continue;
     }
 
