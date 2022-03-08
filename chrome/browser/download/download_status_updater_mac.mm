3c24c7476376fde368b1a575f6b1d1eae06adfbb
Avi Drissman <avi@chromium.org>
2022-03-08 20:48:22 +0000

    Update NSProgress code
    NSProgress was SPI when it was adopted in Chromium. Given that it
    became API in macOS 10.9, and Chromium requires macOS 10.11, update
    the code to compile against the public SDK.
    
    In addition, unpublish the progress objects earlier, as soon as all
    the data is downloaded, and before the file is renamed to its final
    name. This avoids a race condition in macOS that sometimes leaves
    progress indicators in list view in the Finder.
    
    Fixed: 1304233
    Change-Id: I249514b4fd69aab5bc67f710f67e47164c9fedcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510208
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978841}

--- chrome/browser/download/download_status_updater_mac.mm
+++ chrome/browser/download/download_status_updater_mac.mm
@@ -4,93 +4,20 @@
 
 #include "chrome/browser/download/download_status_updater.h"
 
+#import <Foundation/Foundation.h>
+
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_nsobject.h"
-#include "base/strings/sys_string_conversions.h"
+#include "base/memory/scoped_policy.h"
 #include "base/supports_user_data.h"
 #import "chrome/browser/ui/cocoa/dock_icon.h"
 #include "components/download/public/common/download_item.h"
-#include "url/gurl.h"
+#import "net/base/mac/url_conversions.h"
+
+// An NSProgress note. When updating so that macOS 10.13 is the minimum OS, all
+// the -setUserInfoObject:forKey: calls can be turned into property accesses.
 
 namespace {
-
-// These are not the keys themselves; they are the names for dynamic lookup via
-// the ProgressString() function.
-
-// Public keys, SPI in 10.8, API in 10.9:
-NSString* const kNSProgressEstimatedTimeRemainingKeyName =
-    @"NSProgressEstimatedTimeRemainingKey";
-NSString* const kNSProgressFileOperationKindDownloadingName =
-    @"NSProgressFileOperationKindDownloading";
-NSString* const kNSProgressFileOperationKindKeyName =
-    @"NSProgressFileOperationKindKey";
-NSString* const kNSProgressFileURLKeyName =
-    @"NSProgressFileURLKey";
-NSString* const kNSProgressKindFileName =
-    @"NSProgressKindFile";
-NSString* const kNSProgressThroughputKeyName =
-    @"NSProgressThroughputKey";
-
-// Private keys, SPI in 10.8 and 10.9:
-// TODO(avi): Are any of these actually needed for the NSProgress integration?
-NSString* const kNSProgressFileDownloadingSourceURLKeyName =
-    @"NSProgressFileDownloadingSourceURLKey";
-NSString* const kNSProgressFileLocationCanChangeKeyName =
-    @"NSProgressFileLocationCanChangeKey";
-
-// Given an NSProgress string name (kNSProgress[...]Name above), looks up the
-// real symbol of that name from Foundation and returns it.
-NSString* ProgressString(NSString* string) {
-  static NSMutableDictionary* cache;
-  static CFBundleRef foundation;
-  if (!cache) {
-    cache = [[NSMutableDictionary alloc] init];
-    foundation = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.Foundation"));
-  }
-
-  NSString* result = cache[string];
-  if (!result) {
-    NSString** ref = static_cast<NSString**>(
-        CFBundleGetDataPointerForName(foundation,
-                                      base::mac::NSToCFCast(string)));
-    if (ref) {
-      result = *ref;
-      cache[string] = result;
-    }
-  }
-
-  if (!result && string == kNSProgressEstimatedTimeRemainingKeyName) {
-    // Perhaps this is 10.8; try the old name of this key.
-    NSString** ref = static_cast<NSString**>(
-        CFBundleGetDataPointerForName(foundation,
-                                      CFSTR("NSProgressEstimatedTimeKey")));
-    if (ref) {
-      result = *ref;
-      cache[string] = result;
-    }
-  }
-
-  if (!result) {
-    // Huh. At least return a local copy of the expected string.
-    result = string;
-    NSString* const kKeySuffix = @"Key";
-    if ([result hasSuffix:kKeySuffix])
-      result = [result substringToIndex:[result length] - [kKeySuffix length]];
-  }
-
-  return result;
-}
-
-bool NSProgressSupported() {
-  static bool supported;
-  static bool valid;
-  if (!valid) {
-    supported = NSClassFromString(@"NSProgress");
-    valid = true;
-  }
-
-  return supported;
-}
 
 const char kCrNSProgressUserDataKey[] = "CrNSProgressUserData";
 
@@ -98,7 +25,7 @@ class CrNSProgressUserData : public base::SupportsUserData::Data {
  public:
   CrNSProgressUserData(NSProgress* progress, const base::FilePath& target)
       : target_(target) {
-    progress_.reset(progress);
+    progress_.reset(progress, base::scoped_policy::RETAIN);
   }
   ~CrNSProgressUserData() override { [progress_.get() unpublish]; }
 
@@ -111,9 +38,9 @@ class CrNSProgressUserData : public base::SupportsUserData::Data {
   base::FilePath target_;
 };
 
-void UpdateAppIcon(int download_count,
-                   bool progress_known,
-                   float progress) {
+void UpdateAppDockIcon(int download_count,
+                       bool progress_known,
+                       float progress) {
   DockIcon* dock_icon = [DockIcon sharedDockIcon];
   [dock_icon setDownloads:download_count];
   [dock_icon setIndeterminate:!progress_known];
@@ -121,77 +48,71 @@ void UpdateAppIcon(int download_count,
   [dock_icon updateIcon];
 }
 
-void CreateNSProgress(download::DownloadItem* download) {
-  NSURL* source_url = [NSURL URLWithString:
-      base::SysUTF8ToNSString(download->GetURL().possibly_invalid_spec())];
+CrNSProgressUserData* CreateOrGetNSProgress(download::DownloadItem* download) {
+  CrNSProgressUserData* progress_data = static_cast<CrNSProgressUserData*>(
+      download->GetUserData(&kCrNSProgressUserDataKey));
+  if (progress_data)
+    return progress_data;
+
   base::FilePath destination_path = download->GetFullPath();
-  NSURL* destination_url = [NSURL fileURLWithPath:
-      base::mac::FilePathToNSString(destination_path)];
+  NSURL* destination_url = base::mac::FilePathToNSURL(destination_path);
 
-  NSDictionary* user_info = @{
-    ProgressString(kNSProgressFileLocationCanChangeKeyName) : @true,
-    ProgressString(kNSProgressFileOperationKindKeyName) :
-        ProgressString(kNSProgressFileOperationKindDownloadingName),
-    ProgressString(kNSProgressFileURLKeyName) : destination_url
-  };
+  NSProgress* progress = [NSProgress progressWithTotalUnitCount:-1];
+  progress.kind = NSProgressKindFile;
+  [progress setUserInfoObject:NSProgressFileOperationKindDownloading
+                       forKey:NSProgressFileOperationKindKey];
+  [progress setUserInfoObject:destination_url forKey:NSProgressFileURLKey];
 
-  Class progress_class = NSClassFromString(@"NSProgress");
-  NSProgress* progress = [progress_class performSelector:@selector(alloc)];
-  progress = [progress performSelector:@selector(initWithParent:userInfo:)
-                            withObject:nil
-                            withObject:user_info];
-  progress.kind = ProgressString(kNSProgressKindFileName);
-
-  if (source_url) {
-    [progress setUserInfoObject:source_url forKey:
-        ProgressString(kNSProgressFileDownloadingSourceURLKeyName)];
-  }
-
+  // Don't publish a pause/resume handler. The only users of `NSProgress` are
+  // outside of Chromium, and none currently implement pausing published
+  // progresses. Because there is no way to test pausing, do not implement or
+  // ship it.
   progress.pausable = NO;
-  progress.cancellable = YES;
-  [progress setCancellationHandler:^{
-      dispatch_async(dispatch_get_main_queue(), ^{
-          download->Cancel(true);
-      });
-  }];
 
-  progress.totalUnitCount = download->GetTotalBytes();
-  progress.completedUnitCount = download->GetReceivedBytes();
+  // Do publish a cancellation handler. In icon view, the Finder provides a
+  // little (X) button on the icon, and using it will cause this callback.
+  progress.cancellable = YES;
+  progress.cancellationHandler = ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      download->Cancel(/*user_cancel=*/true);
+    });
+  };
 
   [progress publish];
 
   download->SetUserData(
       &kCrNSProgressUserDataKey,
       std::make_unique<CrNSProgressUserData>(progress, destination_path));
+
+  return static_cast<CrNSProgressUserData*>(
+      download->GetUserData(&kCrNSProgressUserDataKey));
 }
 
-void UpdateNSProgress(download::DownloadItem* download,
-                      CrNSProgressUserData* progress_data) {
+void UpdateNSProgress(download::DownloadItem* download) {
+  CrNSProgressUserData* progress_data = CreateOrGetNSProgress(download);
+
   NSProgress* progress = progress_data->progress();
   progress.totalUnitCount = download->GetTotalBytes();
   progress.completedUnitCount = download->GetReceivedBytes();
   [progress setUserInfoObject:@(download->CurrentSpeed())
-                       forKey:ProgressString(kNSProgressThroughputKeyName)];
+                       forKey:NSProgressThroughputKey];
 
   base::TimeDelta time_remaining;
-  NSNumber* time_remaining_ns = nil;
+  NSNumber* ns_time_remaining = nil;
   if (download->TimeRemaining(&time_remaining))
-    time_remaining_ns = @(time_remaining.InSeconds());
-  [progress setUserInfoObject:time_remaining_ns
-               forKey:ProgressString(kNSProgressEstimatedTimeRemainingKeyName)];
+    ns_time_remaining = @(time_remaining.InSeconds());
+  [progress setUserInfoObject:ns_time_remaining
+                       forKey:NSProgressEstimatedTimeRemainingKey];
 
   base::FilePath download_path = download->GetFullPath();
   if (progress_data->target() != download_path) {
     progress_data->setTarget(download_path);
-    NSURL* download_url = [NSURL fileURLWithPath:
-        base::mac::FilePathToNSString(download_path)];
-    [progress setUserInfoObject:download_url
-                         forKey:ProgressString(kNSProgressFileURLKeyName)];
+    NSURL* download_url = base::mac::FilePathToNSURL(download_path);
+    [progress setUserInfoObject:download_url forKey:NSProgressFileURLKey];
   }
 }
 
-void DestroyNSProgress(download::DownloadItem* download,
-                       CrNSProgressUserData* progress_data) {
+void DestroyNSProgress(download::DownloadItem* download) {
   download->RemoveUserData(&kCrNSProgressUserDataKey);
 }
 
@@ -199,33 +120,31 @@ void DestroyNSProgress(download::DownloadItem* download,
 
 void DownloadStatusUpdater::UpdateAppIconDownloadProgress(
     download::DownloadItem* download) {
-  // Always update overall progress.
+  // Always update overall progress in the Dock icon.
 
   float progress = 0;
   int download_count = 0;
   bool progress_known = GetProgress(&progress, &download_count);
-  UpdateAppIcon(download_count, progress_known, progress);
+  UpdateAppDockIcon(download_count, progress_known, progress);
 
-  // Update NSProgress-based indicators.
-
-  if (NSProgressSupported()) {
-    CrNSProgressUserData* progress_data = static_cast<CrNSProgressUserData*>(
-        download->GetUserData(&kCrNSProgressUserDataKey));
-
-    // Only show progress if the download is IN_PROGRESS and it hasn't been
-    // renamed to its final name. Setting the progress after the final rename
-    // results in the file being stuck in an in-progress state on the dock. See
-    // http://crbug.com/166683.
-    if (download->GetState() == download::DownloadItem::IN_PROGRESS &&
-        !download->GetFullPath().empty() &&
-        download->GetFullPath() != download->GetTargetFilePath()) {
-      if (!progress_data)
-        CreateNSProgress(download);
-      else
-        UpdateNSProgress(download, progress_data);
-    } else {
-      DestroyNSProgress(download, progress_data);
-    }
+  // Update `NSProgress`-based indicators. Only show progress:
+  //   - if the download is IN_PROGRESS, and
+  //   - it has not yet saved all the data, and
+  //   - it hasn't been renamed to its final name.
+  //
+  // There's a race condition in macOS code where unpublishing an `NSProgress`
+  // object for a file that was renamed will sometimes leave a progress
+  // indicator visible in the Finder (https://crbug.com/1304233). Therefore, as
+  // soon as `DownloadItem::AllDataSaved()` returns true, do the unpublish.
+  // As an additional bug to avoid (http://crbug.com/166683), never update the
+  // data of an `NSProgress` after the file name has changed, as that can result
+  // in the file being stuck in an in-progress state in the Dock.
+  if (download->GetState() == download::DownloadItem::IN_PROGRESS &&
+      !download->AllDataSaved() && !download->GetFullPath().empty() &&
+      download->GetFullPath() != download->GetTargetFilePath()) {
+    UpdateNSProgress(download);
+  } else {
+    DestroyNSProgress(download);
   }
 
   // Handle downloads that ended.
