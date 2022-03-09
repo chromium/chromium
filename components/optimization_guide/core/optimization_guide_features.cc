e8b5ba87dcd3147c32fce3c582ca5e27f2655531
Xing Liu <xingliu@chromium.org>
2022-03-09 02:09:23 +0000

    Revert "Enable high priority download by default"
    This reverts commit 5352b27d3642c2d22e1039069cc01fa78d9aff62.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1303354
    
    
    Original change's description:
    > Enable high priority download by default
    >
    > This is experimented with language detection. So enabling it by default.
    >
    > Bug: 1303017
    > Change-Id: I48333dccfd228099973c7eb5bbd053197ee911ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499696
    > Commit-Queue: Raj T <rajendrant@chromium.org>
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#977797}
    
    Bug: 1303017
    Change-Id: Ifee7b3ddb6653ddda35480385d43b74518d1ceb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510984
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Owners-Override: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979026}

--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -424,7 +424,7 @@ bool IsModelDownloadingEnabled() {
 bool IsUnrestrictedModelDownloadingEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kOptimizationGuideModelDownloading, "unrestricted_model_downloading",
-      true);
+      false);
 }
 
 bool IsPageContentAnnotationEnabled() {
