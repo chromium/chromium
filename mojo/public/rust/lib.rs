b2e16b38797a3bfea606dab682c619ebcb445eb0
danakj <danakj@chromium.org>
2022-03-09 19:17:19 +0000

    Remove extern crate linking workaround in mojo.
    Mojo was declaring `extern crate base` in order to ensure base's Rust
    crate was linked into mojo_rust_extra_tests. We have improved the deps
    and public_deps in the mixed_target GN templates however, and this is
    no longer needed to link.
    
    R=collinbaker@chromium.org
    
    Bug: 1289742, 1296156
    Change-Id: I442343d45a7da8bdfc41894cc7d07816e0d7afe2
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,android-rust-arm-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513976
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979356}

--- mojo/public/rust/lib.rs
+++ mojo/public/rust/lib.rs
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Workaround for linkage bug for rust binary -> C++ component -> rust component
-// dependency. TOOD(https://crbug.com/1289742): remove this after fix.
-extern crate base;
-
 #[macro_use]
 mod macros {
     /// This macro must be used at the top-level in any
