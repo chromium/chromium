acd42be5a5f3d9d6bb70991620e1e2df886128b6
Adrian Taylor <adetaylor@chromium.org>
2022-03-09 23:30:03 +0000

    [rust] Helpful error if wrong gclient args given
    Nowadays some parts of our Rust build depend upon clang libraries
    being present. Give a helpful error message if they're not.
    
    Bug: 1296236
    Change-Id: If4eb596dc3450c288fab18cc96b4d21702a1e0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513520
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979487}

--- build/rust/clanglibs/find_clanglibs.py
+++ build/rust/clanglibs/find_clanglibs.py
@@ -27,6 +27,15 @@ def main():
                       help="Where to find clang and LLVM libs",
                       required=True)
   args = parser.parse_args()
+  if not os.path.exists(os.path.join(
+      args.clang_libs_dir, "libclang.a")) and not os.path.exists(
+          os.path.join(args.clang_libs_dir, "libclang.lib")):
+    print("Couldn't find libclang.a|lib. Please ensure you set a custom var in "
+          "your .gclient file like so:\n"
+          "  \"custom_vars\": { \"checkout_clang_libs\": True, }\n"
+          "Then run gclient sync; rm "
+          "<out dir>/obj/build/rust/clanglibs/clang_libs.rsp.")
+    return -1
   with open(args.depfile, 'w') as depfile:
     depfile.write("%s:" % args.output)
     with open(args.output, 'w') as output:
