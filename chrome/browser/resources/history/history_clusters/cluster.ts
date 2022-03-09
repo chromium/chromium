c0e1f5e9a8162943150e834f8edeb473d993bf9e
Tommy C. Li <tommycli@chromium.org>
2022-03-09 22:30:01 +0000

    [journeys] Combine the <top-visit> and <cluster> elements.
    Folds the <top-visit> element into <cluster> because:
     - <cluster> was the only user of <top-visit>
     - <cluster> was nothing more than a very thin wrapper over <top-visit>
    
    This CL should have no changes in behavior. It's pretty much a copy
    paste merging of two elements into one.
    
    Bug: 1294348
    Change-Id: Ia012dcc807400baf887c9e0e45adf5124d0189f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3511830
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979458}

--- chrome/browser/resources/history/history_clusters/cluster.ts
+++ chrome/browser/resources/history/history_clusters/cluster.ts
@@ -2,11 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './page_favicon.js';
+import './search_query.js';
+import './shared_style.js';
 import './shared_vars.js';
-import './top_visit.js';
+import './url_visit.js';
+import 'chrome://resources/cr_elements/cr_icons_css.m.js';
+import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
 
 import {assert} from 'chrome://resources/js/assert_ts.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxyImpl} from './browser_proxy.js';
@@ -47,6 +51,40 @@ class HistoryClusterElement extends PolymerElement {
        * The cluster displayed by this element.
        */
       cluster: Object,
+
+      /**
+       * Whether the default-hidden related visits are visible.
+       */
+      expanded_: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false,
+      },
+
+      /**
+       * Whether there are default-hidden related visits.
+       */
+      hasHiddenRelatedVisits_: {
+        type: Boolean,
+        computed: `computeHasHiddenRelatedVisits_(hiddenRelatedVisits_)`,
+        reflectToAttribute: true,
+      },
+
+      /**
+       * The default-hidden related visits.
+       */
+      hiddenRelatedVisits_: {
+        type: Object,
+        computed: `computeHiddenRelatedVisits_(cluster.visit.relatedVisits.*)`,
+      },
+
+      /**
+       * The always-visible related visits.
+       */
+      visibleRelatedVisits_: {
+        type: Object,
+        computed: `computeVisibleRelatedVisits_(cluster.visit.relatedVisits.*)`,
+      },
     };
   }
 
@@ -58,6 +96,8 @@ class HistoryClusterElement extends PolymerElement {
   index: number;
   private callbackRouter_: PageCallbackRouter;
   private onVisitsRemovedListenerId_: number|null = null;
+  private expanded_: boolean;
+  private hiddenRelatedVisits_: Array<URLVisit>;
 
   //============================================================================
   // Overridden methods
@@ -96,14 +136,35 @@ class HistoryClusterElement extends PolymerElement {
         ClusterAction.RELATED_SEARCH_CLICKED, this.index);
   }
 
-  private onRelatedVisitsVisibilityToggled_() {
-    MetricsProxyImpl.getInstance().recordClusterAction(
-        ClusterAction.RELATED_VISITS_VISIBILITY_TOGGLED, this.index);
-  }
-
   private onVisitClicked_() {
     MetricsProxyImpl.getInstance().recordClusterAction(
         ClusterAction.VISIT_CLICKED, this.index);
+  }
+
+  private onToggleButtonKeyDown_(e: KeyboardEvent) {
+    if (e.key !== 'Enter' && e.key !== ' ') {
+      return;
+    }
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    this.onToggleButtonClick_();
+  }
+
+  private onToggleButtonClick_() {
+    this.expanded_ = !this.expanded_;
+
+    MetricsProxyImpl.getInstance().recordClusterAction(
+        ClusterAction.RELATED_VISITS_VISIBILITY_TOGGLED, this.index);
+
+    // Dispatch an event to notify the parent elements of a resize. Note that
+    // this simple solution only works because the child iron-collapse has
+    // animations disabled. Otherwise, it gets an incorrect mid-animation size.
+    this.dispatchEvent(new CustomEvent('iron-resize', {
+      bubbles: true,
+      composed: true,
+    }));
   }
 
   //============================================================================
@@ -154,6 +215,39 @@ class HistoryClusterElement extends PolymerElement {
       bubbles: true,
       composed: true,
     }));
+  }
+
+  private computeHasHiddenRelatedVisits_(): boolean {
+    return this.hiddenRelatedVisits_.length > 0;
+  }
+
+  private computeHiddenRelatedVisits_(): Array<URLVisit> {
+    return this.cluster.visit.relatedVisits.filter((visit: URLVisit) => {
+      return visit.belowTheFold;
+    });
+  }
+
+  private computeVisibleRelatedVisits_(): Array<URLVisit> {
+    return this.cluster.visit.relatedVisits.filter((visit: URLVisit) => {
+      return !visit.belowTheFold;
+    });
+  }
+
+  /**
+   * Returns the label of the toggle button based on whether the default-hidden
+   * related visits are visible.
+   */
+  private getToggleButtonLabel_(_expanded: boolean): string {
+    return loadTimeData.getString(
+        this.expanded_ ? 'toggleButtonLabelLess' : 'toggleButtonLabelMore');
+  }
+
+  /**
+   * Returns the index of `relatedVisit` among the visits in the cluster.
+   */
+  private getVisitIndex_(relatedVisit: URLVisit): number {
+    // Index 0 represents the top visit.
+    return this.cluster.visit.relatedVisits.indexOf(relatedVisit) + 1;
   }
 }
 
