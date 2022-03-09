170fd09f901105a488df055c001e7afce0e4a9a6
Peter Kasting <pkasting@chromium.org>
2022-03-09 22:20:17 +0000

    Fix segfault.
    (cherry picked from commit a82f8a711d6f93222aabbd2242ebd9dc28503718)
    
    Bug: 1304747
    Change-Id: Ic39276b68d72ae4ed8b5ed7e3407c6363005e5f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514352
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#979315}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513524
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4896@{#421}
    Cr-Branched-From: 1f63ff4bc27570761b35ffbc7f938f6586f7bee8-refs/heads/main@{#972766}

--- chrome/browser/component_updater/pki_metadata_component_installer.cc
+++ chrome/browser/component_updater/pki_metadata_component_installer.cc
@@ -244,7 +244,7 @@ void PKIMetadataComponentInstallerPolicy::UpdateNetworkServiceOnUI(
   popular_scts.reserve(proto->popular_scts().size());
   std::transform(
       proto->popular_scts().begin(), proto->popular_scts().end(),
-      popular_scts.begin(), [](std::string sct) {
+      std::back_inserter(popular_scts), [](std::string sct) {
         const uint8_t* raw_data = reinterpret_cast<const uint8_t*>(sct.data());
         return std::vector<uint8_t>(raw_data, raw_data + sct.length());
       });
