5e489de274b6f1676d81669aac86c5145c561826
Ross Wollman <ross.wollman@gmail.com>
2022-03-10 20:40:42 +0000

    DevTools: Fix Service Worker networkId missing
    For the initial Service Worker script, `Network.requestWillBeBeSent` is
    observed, but the corresponding `Fetch.requestPaused` event when
    performing interception was missing a `networkId` param. This prevented a
    CDP consumer from definitively associating the two events.
    
    Requests from within the Service Worker appear to have proper
    `networkId` attribution, so it appears it was just for the script
    itself.
    
    Discovered while working on Playwright: https://github.com/microsoft/playwright/issues/1090
    
    R=caseq
    
    Bug: 1304381
    Change-Id: I55446db8b305ae81767c100efa27aced22152da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510917
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979924}

--- content/browser/service_worker/service_worker_new_script_fetcher.cc
+++ content/browser/service_worker/service_worker_new_script_fetcher.cc
@@ -106,6 +106,7 @@ void ServiceWorkerNewScriptFetcher::StartScriptLoadingWithNewResourceID(
 
   // Notify to DevTools that the request for fetching the service worker script
   // is about to start. It fires `Network.onRequestWillBeSent` event.
+  request.devtools_request_id = version_->reporting_source().ToString();
   devtools_instrumentation::OnServiceWorkerMainScriptRequestWillBeSent(
       requesting_frame_id_, version_->reporting_source(), request);
 
