0383fcfd6e60f98b07fa678f708f01af49ee1bf4
Andrew Paseltiner <apaseltiner@chromium.org>
2022-03-11 00:12:30 +0000

    Add basic fuzzer for attribution event trigger data header parsing
    This fuzzer's input isn't constrained to our JSON schema or even valid
    JSON at all, so it is has a relatively low probability of finding an
    issue, but we can refine that in followup CLs.
    
    Bug: 1300278
    Change-Id: I46630937c3322d173373fa08ca2991cefda481da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517471
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980046}

---
+++ third_party/blink/renderer/core/frame/attribution_event_trigger_data_fuzzer.cc
@@ -0,0 +1,30 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "third_party/blink/public/mojom/conversions/attribution_data_host.mojom-blink.h"
+#include "third_party/blink/renderer/core/frame/attribution_response_parsing.h"
+#include "third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+
+namespace blink {
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static BlinkFuzzerTestSupport test_support = BlinkFuzzerTestSupport();
+
+  const AtomicString input(data, static_cast<unsigned>(size));
+  WTF::Vector<mojom::blink::EventTriggerDataPtr> output;
+  attribution_response_parsing::ParseEventTriggerData(input, output);
+
+  return 0;
+}
+
+}  // namespace blink
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  return blink::LLVMFuzzerTestOneInput(data, size);
+}
