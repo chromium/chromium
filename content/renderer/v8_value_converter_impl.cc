792dfc5076a78a61ff0150c1915655efaacbc1cf
David Bertoni <dbertoni@chromium.org>
2022-03-11 02:50:45 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /content/renderer.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=haraken@chromium.org
    
    Bug: 1187097
    Change-Id: Ieb011b86239d43bd2dd4e9977f17d7aa0a1a7145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518286
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980105}

--- content/renderer/v8_value_converter_impl.cc
+++ content/renderer/v8_value_converter_impl.cc
@@ -477,18 +477,19 @@ std::unique_ptr<base::Value> V8ValueConverterImpl::FromV8Array(
 
     if (!val->HasRealIndexedProperty(isolate->GetCurrentContext(), i)
              .FromMaybe(false)) {
-      result->Append(std::make_unique<base::Value>());
+      result->Append(base::Value());
       continue;
     }
 
     std::unique_ptr<base::Value> child =
         FromV8ValueImpl(state, child_v8, isolate);
-    if (child)
-      result->Append(std::move(child));
-    else
+    if (child) {
+      result->Append(base::Value::FromUniquePtrValue(std::move(child)));
+    } else {
       // JSON.stringify puts null in places where values don't serialize, for
       // example undefined and functions. Emulate that behavior.
-      result->Append(std::make_unique<base::Value>());
+      result->Append(base::Value());
+    }
   }
   return std::move(result);
 }
