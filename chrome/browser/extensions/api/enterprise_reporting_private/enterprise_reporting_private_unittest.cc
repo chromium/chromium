8916f057a150b0b024532b798a58fbc1f4f741ea
David Bertoni <dbertoni@chromium.org>
2022-03-14 13:01:37 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /chrome/browser/extensions/api/enterprise_reporting_private.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=domfc@chromium.org
    
    Bug: 1187097
    Change-Id: Id69deb27b00e5b927da2ea95a4f7933fd8bb175e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517990
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980524}

--- chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_unittest.cc
+++ chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_unittest.cc
@@ -118,8 +118,7 @@ TEST_F(EnterpriseReportingPrivateDeviceDataFunctionsTest, StoreDeviceData) {
       base::MakeRefCounted<EnterpriseReportingPrivateSetDeviceDataFunction>();
   std::unique_ptr<base::ListValue> values = std::make_unique<base::ListValue>();
   values->Append("a");
-  values->Append(
-      std::make_unique<base::Value>(base::Value::BlobStorage({1, 2, 3})));
+  values->Append(base::Value(base::Value::BlobStorage({1, 2, 3})));
   extension_function_test_utils::RunFunction(function.get(), std::move(values),
                                              browser(),
                                              extensions::api_test_utils::NONE);
@@ -152,8 +151,7 @@ TEST_F(EnterpriseReportingPrivateDeviceDataFunctionsTest, DeviceBadId) {
   std::unique_ptr<base::ListValue> set_values =
       std::make_unique<base::ListValue>();
   set_values->Append("a/b");
-  set_values->Append(
-      std::make_unique<base::Value>(base::Value::BlobStorage({1, 2, 3})));
+  set_values->Append(base::Value(base::Value::BlobStorage({1, 2, 3})));
   extension_function_test_utils::RunFunction(set_function.get(),
                                              std::move(set_values), browser(),
                                              extensions::api_test_utils::NONE);
@@ -178,8 +176,7 @@ TEST_F(EnterpriseReportingPrivateDeviceDataFunctionsTest, RetrieveDeviceData) {
   std::unique_ptr<base::ListValue> set_values =
       std::make_unique<base::ListValue>();
   set_values->Append("c");
-  set_values->Append(
-      std::make_unique<base::Value>(base::Value::BlobStorage({1, 2, 3})));
+  set_values->Append(base::Value(base::Value::BlobStorage({1, 2, 3})));
   extension_function_test_utils::RunFunction(set_function.get(),
                                              std::move(set_values), browser(),
                                              extensions::api_test_utils::NONE);
