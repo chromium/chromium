769c3add64e2a7439dc6194de25d7d04a24fd93d
Elmehdi Rahmaoui <erahmaoui@google.com>
2022-03-14 14:42:16 +0000

    [iOS] Implementation of gradient masking feature (no RTL yet)
    This CL replaces plain text views with fade clipped truncated views in
    the new omnibox popup using SwiftUI.
    
    Bug: 1294264
    Change-Id: I64c365b7ec9923af82e9f37b90617dd75d1608db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512872
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#980554}

--- ios/chrome/browser/ui/omnibox/popup/shared/popup_match_row_view.swift
+++ ios/chrome/browser/ui/omnibox/popup/shared/popup_match_row_view.swift
@@ -5,6 +5,39 @@
 import SwiftUI
 import ios_chrome_common_ui_colors_swift
 
+/// GradientOverlayRight applies a fading effect as an overlay from left to right.
+struct GradientOverlayRight: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .overlay(
+        LinearGradient(
+          gradient: Gradient(stops: [
+            Gradient.Stop(color: .white.opacity((0)), location: 0.9),
+            Gradient.Stop(color: .white, location: 1),
+          ]),
+          startPoint: .leading, endPoint: .trailing)
+      )
+  }
+}
+
+/// GradientOverlayRight applies a fading effect as an overlay from right to left.
+struct GradientOverlayLeft: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .overlay(
+        LinearGradient(
+          gradient: Gradient(stops: [
+            Gradient.Stop(color: .white.opacity((0)), location: 0.1),
+            Gradient.Stop(color: .white, location: 0),
+          ]),
+          startPoint: .leading, endPoint: .trailing)
+      )
+  }
+}
+
+/// PreferenceKey to listen to changes of a view's size.
 struct SizePreferenceKey: PreferenceKey {
   static var defaultValue = CGSize.zero
   static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
@@ -12,7 +45,59 @@ struct SizePreferenceKey: PreferenceKey {
   }
 }
 
+/// A view modifier that applies a transparent gradient effect when the content
+/// is too wide to fit in its container. The content is clipped on the trailing
+/// edge.
+struct TruncatedWithGradient: ViewModifier {
+
+  // Child view size (width,height)
+  @State var childSize = CGSize.zero
+
+  // Widths comparison tracker
+  @State var truncated = false
+
+  func body(content: Content) -> some View {
+    let rawContent = GeometryReader { outerGeo in
+      content
+        .fixedSize(horizontal: true, vertical: true)
+        .overlay(
+          GeometryReader { innerGeo in
+            Rectangle()
+              .hidden()
+              .preference(key: SizePreferenceKey.self, value: innerGeo.size)
+              .onPreferenceChange(SizePreferenceKey.self) {
+                newSize in
+                childSize = newSize
+                truncated = childSize.width > outerGeo.size.width
+              }
+          }
+        )
+    }
+    .frame(height: childSize.height)
+    .clipped()
+
+    if truncated {
+      rawContent.gradientOverlayRight()
+    } else {
+      rawContent
+    }
+  }
+}
+
+extension View {
+  func truncatedWithGradient() -> some View {
+    self.modifier(TruncatedWithGradient())
+  }
+  func gradientOverlayLeft() -> some View {
+    self.modifier(GradientOverlayLeft())
+  }
+  func gradientOverlayRight() -> some View {
+    self.modifier(GradientOverlayRight())
+  }
+}
+
 struct PopupMatchRowView: View {
+
   enum Dimensions {
     static let actionButtonOffset = CGSize(width: -5, height: 0)
     static let actionButtonOuterPadding = EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0)
@@ -52,12 +137,14 @@ struct PopupMatchRowView: View {
           VStack(alignment: .leading, spacing: 0) {
             Text(match.text)
               .lineLimit(1)
+              .truncatedWithGradient()
 
             if let subtitle = match.detailText, !subtitle.isEmpty {
               Text(subtitle)
                 .font(.footnote)
                 .foregroundColor(Color.gray)
                 .lineLimit(1)
+                .truncatedWithGradient()
             }
           }
           .frame(height: Dimensions.textHeight)
