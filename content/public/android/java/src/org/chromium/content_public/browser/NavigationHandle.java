4ffed54a74df5f9167c7c4db9f668389b6540c5f
Michael Thiessen <mthiesse@chromium.org>
2022-03-14 16:59:32 +0000

    Update Redirect state in Java NavigationHandle
    Plumbing of redirect state to the Java NavigationHandle forgot to
    update the state when a redirect occurs.
    
    Adds a regression test.
    
    Bug: 1303928
    Change-Id: I2a2c130528e3d2441f980d616c95c190e52ce2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513091
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980618}

--- content/public/android/java/src/org/chromium/content_public/browser/NavigationHandle.java
+++ content/public/android/java/src/org/chromium/content_public/browser/NavigationHandle.java
@@ -41,7 +41,7 @@ public class NavigationHandle {
     private final Impression mImpression;
     private final boolean mIsPost;
     private final boolean mHasUserGesture;
-    private final boolean mIsRedirect;
+    private boolean mIsRedirect;
     private final boolean mIsExternalProtocol;
     private final long mNavigationId;
     private final boolean mIsPageActivation;
@@ -78,6 +78,7 @@ public class NavigationHandle {
     @CalledByNative
     private void didRedirect(GURL url) {
         mUrl = url;
+        mIsRedirect = true;
     }
 
     /**
