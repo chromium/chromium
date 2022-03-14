e2037b300fc5c2c824a2ff857875f4e16f04ea9c
David Bertoni <dbertoni@chromium.org>
2022-03-14 14:30:06 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /content/browser/android/java.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=torne@chromium.org
    
    Bug: 1187097
    Change-Id: Ie3019afe5fa80d7df80fed577e66cc702c71f0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518308
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980548}

--- content/browser/android/java/gin_java_method_invocation_helper_unittest.cc
+++ content/browser/android/java/gin_java_method_invocation_helper_unittest.cc
@@ -130,27 +130,30 @@ TEST_F(GinJavaMethodInvocationHelperTest, RetrievalOfObjectsNoObjects) {
 TEST_F(GinJavaMethodInvocationHelperTest, RetrievalOfObjectsHaveObjects) {
   base::ListValue objects;
   objects.Append(100);
-  objects.Append(GinJavaBridgeValue::CreateObjectIDValue(1));
-  auto sub_list = std::make_unique<base::ListValue>();
-  sub_list->Append(200);
-  sub_list->Append(GinJavaBridgeValue::CreateObjectIDValue(2));
-  objects.Append(std::move(sub_list));
-  auto sub_dict = std::make_unique<base::DictionaryValue>();
-  sub_dict->SetInteger("1", 300);
-  sub_dict->SetKey("2", base::Value::FromUniquePtrValue(
-                            GinJavaBridgeValue::CreateObjectIDValue(3)));
-  objects.Append(std::move(sub_dict));
-  auto sub_list_with_dict = std::make_unique<base::ListValue>();
-  auto sub_sub_dict = std::make_unique<base::DictionaryValue>();
-  sub_sub_dict->SetKey("1", base::Value::FromUniquePtrValue(
-                                GinJavaBridgeValue::CreateObjectIDValue(4)));
-  sub_list_with_dict->Append(std::move(sub_sub_dict));
-  objects.Append(std::move(sub_list_with_dict));
-  auto sub_dict_with_list = std::make_unique<base::DictionaryValue>();
-  base::ListValue sub_sub_list;
-  sub_sub_list.Append(GinJavaBridgeValue::CreateObjectIDValue(5));
-  sub_dict_with_list->SetKey("1", std::move(sub_sub_list));
-  objects.Append(std::move(sub_dict_with_list));
+  objects.Append(base::Value::FromUniquePtrValue(
+      GinJavaBridgeValue::CreateObjectIDValue(1)));
+  base::Value::List sub_list;
+  sub_list.Append(200);
+  sub_list.Append(base::Value::FromUniquePtrValue(
+      GinJavaBridgeValue::CreateObjectIDValue(2)));
+  objects.Append(base::Value(std::move(sub_list)));
+  base::Value::Dict sub_dict;
+  sub_dict.Set("1", 300);
+  sub_dict.Set("2", base::Value::FromUniquePtrValue(
+                        GinJavaBridgeValue::CreateObjectIDValue(3)));
+  objects.Append(base::Value(std::move(sub_dict)));
+  base::Value::List sub_list_with_dict;
+  base::Value::Dict sub_sub_dict;
+  sub_sub_dict.Set("1", base::Value::FromUniquePtrValue(
+                            GinJavaBridgeValue::CreateObjectIDValue(4)));
+  sub_list_with_dict.Append(std::move(sub_sub_dict));
+  objects.Append(base::Value(std::move(sub_list_with_dict)));
+  base::Value::Dict sub_dict_with_list;
+  base::Value::List sub_sub_list;
+  sub_sub_list.Append(base::Value::FromUniquePtrValue(
+      GinJavaBridgeValue::CreateObjectIDValue(5)));
+  sub_dict_with_list.Set("1", std::move(sub_sub_list));
+  objects.Append(base::Value(std::move(sub_dict_with_list)));
 
   scoped_refptr<GinJavaMethodInvocationHelper> helper =
       new GinJavaMethodInvocationHelper(
