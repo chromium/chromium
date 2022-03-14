ffa866a5ae9eaaaaca088fbfbbc44aaf3a5d78c6
Brian Dunn <brian@theophil.us>
2022-03-14 06:53:14 +0000

    Add a lock to avoid abortion
    Bug: chromedriver:4045
    Change-Id: I34f44f07d7320d7198b21f57f220e7ac8a29ee3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499059
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980446}

--- chrome/test/chromedriver/keycode_text_conversion_mac.mm
+++ chrome/test/chromedriver/keycode_text_conversion_mac.mm
@@ -10,8 +10,22 @@
 
 #include "base/mac/scoped_cftyperef.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/synchronization/lock.h"
 #include "chrome/test/chromedriver/chrome/ui_events.h"
 #include "ui/events/keycodes/keyboard_code_conversion_mac.h"
+
+base::Lock tis_lock_;
+
+UniChar GetCharacter(UInt16 mac_key_code, UInt32 modifier_key_state) {
+  UInt32 dead_key_state = 0;
+
+  base::AutoLock lock(tis_lock_);
+  base::ScopedCFTypeRef<TISInputSourceRef> input_source(
+      TISCopyCurrentKeyboardLayoutInputSource());
+  return ui::TranslatedUnicodeCharFromKeyCode(
+      input_source.get(), mac_key_code, kUCKeyActionDown, modifier_key_state,
+      LMGetKbdLast(), &dead_key_state);
+}
 
 bool ConvertKeyCodeToText(
     ui::KeyboardCode key_code, int modifiers, std::string* text,
@@ -37,12 +51,8 @@ bool ConvertKeyCodeToText(
   // on UCKeyTranslate for more info.
   UInt32 modifier_key_state = (mac_modifiers >> 8) & 0xFF;
 
-  UInt32 dead_key_state = 0;
-  base::ScopedCFTypeRef<TISInputSourceRef> input_source(
-      TISCopyCurrentKeyboardLayoutInputSource());
-  UniChar character = ui::TranslatedUnicodeCharFromKeyCode(
-      input_source.get(), static_cast<UInt16>(mac_key_code), kUCKeyActionDown,
-      modifier_key_state, LMGetKbdLast(), &dead_key_state);
+  UniChar character =
+      GetCharacter(static_cast<UInt16>(mac_key_code), modifier_key_state);
 
   if (character && !std::iscntrl(character)) {
     std::u16string text16;
