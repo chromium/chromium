7a5c51931ae4e35f66778644ed1fd5824faec436
David Bertoni <dbertoni@chromium.org>
2022-03-14 17:08:47 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /chrome/browser/ui/webui/bookmarks.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=johntlee@chromium.org
    
    Bug: 1187097
    Change-Id: I2a533d0ad409456bb6a9cb2f6799d38410b0628b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518112
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980627}

--- chrome/browser/ui/webui/bookmarks/bookmarks_browsertest.cc
+++ chrome/browser/ui/webui/bookmarks/bookmarks_browsertest.cc
@@ -33,16 +33,17 @@ void BookmarksBrowserTest::SetupExtensionAPITest() {
       ManagedBookmarkServiceFactory::GetForProfile(profile);
   bookmarks::test::WaitForBookmarkModelToLoad(model);
 
-  base::ListValue list;
-  std::unique_ptr<base::DictionaryValue> node(new base::DictionaryValue());
-  node->SetStringKey("name", "Managed Bookmark");
-  node->SetStringKey("url", "http://www.chromium.org");
+  base::Value::List list;
+  base::Value::Dict node;
+  node.Set("name", "Managed Bookmark");
+  node.Set("url", "http://www.chromium.org");
+  list.Append(node.Clone());
+  node.clear();
+  node.Set("name", "Managed Folder");
+  node.Set("children", base::Value::List());
   list.Append(std::move(node));
-  node = std::make_unique<base::DictionaryValue>();
-  node->SetStringKey("name", "Managed Folder");
-  node->Set("children", std::make_unique<base::ListValue>());
-  list.Append(std::move(node));
-  profile->GetPrefs()->Set(bookmarks::prefs::kManagedBookmarks, list);
+  profile->GetPrefs()->Set(bookmarks::prefs::kManagedBookmarks,
+                           base::Value(std::move(list)));
   ASSERT_EQ(2u, managed->managed_node()->children().size());
 }
 
