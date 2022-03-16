67480299ccc788ef3184453f0f49ee832a005587
danakj <danakj@chromium.org>
2022-03-16 16:12:36 +0000

    Add BASE_EXPORT to all glue functions in base::rs_glue.
    These functions are used across components (from the Rust library)
    so they need to be exported for component builds.
    
    R=adetaylor@chromium.org
    
    Bug: 1296156
    Change-Id: I154ec89ba6f9aa7cbe4053f6b7460fdd836bd90b
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,android-rust-arm-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530066
    Reviewed-by: Adrian Taylor <adetaylor@google.com>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981672}

--- base/rs_glue/values_glue.h
+++ base/rs_glue/values_glue.h
@@ -6,6 +6,8 @@
 #define BASE_RS_GLUE_VALUES_GLUE_H_
 
 #include <stddef.h>
+
+#include "base/base_export.h"
 #include "base/strings/string_piece_rust.h"
 #include "base/values.h"
 #include "third_party/rust/cxx/v1/crate/include/cxx.h"
@@ -23,27 +25,29 @@ namespace rs_glue {
 using ValueSlot = absl::optional<base::Value>;
 
 // Function purposes explained in mod.rs in the same directory.
-void ValueSetNoneKey(base::Value& v, rust::Str key);
-void ValueSetBoolKey(base::Value& v, rust::Str key, bool value);
-void ValueSetIntegerKey(base::Value& v, rust::Str key, int value);
-void ValueSetDoubleKey(base::Value& v, rust::Str key, double value);
-void ValueSetStringKey(base::Value& v, rust::Str key, rust::Str value);
-base::Value& ValueSetDictKey(base::Value& v, rust::Str key);
-base::Value& ValueSetListKey(base::Value& v, rust::Str key);
-void ValueAppendNone(base::Value& v);
-void ValueAppendString(base::Value& v, rust::Str value);
-base::Value& ValueAppendDict(base::Value& v);
-base::Value& ValueAppendList(base::Value& v);
-void ValueReserveSize(base::Value& v, size_t len);
-std::unique_ptr<ValueSlot> NewValueSlotForTesting();
-rust::String DumpValueSlot(const ValueSlot& v);
-void ConstructNoneValue(ValueSlot& v);
-void ConstructBoolValue(ValueSlot& v, bool value);
-void ConstructIntegerValue(ValueSlot& v, int value);
-void ConstructDoubleValue(ValueSlot& v, double value);
-void ConstructStringValue(ValueSlot& v, rust::Str value);
-base::Value& ConstructDictValue(ValueSlot& v);
-base::Value& ConstructListValue(ValueSlot& v);
+BASE_EXPORT void ValueSetNoneKey(base::Value& v, rust::Str key);
+BASE_EXPORT void ValueSetBoolKey(base::Value& v, rust::Str key, bool value);
+BASE_EXPORT void ValueSetIntegerKey(base::Value& v, rust::Str key, int value);
+BASE_EXPORT void ValueSetDoubleKey(base::Value& v, rust::Str key, double value);
+BASE_EXPORT void ValueSetStringKey(base::Value& v,
+                                   rust::Str key,
+                                   rust::Str value);
+BASE_EXPORT base::Value& ValueSetDictKey(base::Value& v, rust::Str key);
+BASE_EXPORT base::Value& ValueSetListKey(base::Value& v, rust::Str key);
+BASE_EXPORT void ValueAppendNone(base::Value& v);
+BASE_EXPORT void ValueAppendString(base::Value& v, rust::Str value);
+BASE_EXPORT base::Value& ValueAppendDict(base::Value& v);
+BASE_EXPORT base::Value& ValueAppendList(base::Value& v);
+BASE_EXPORT void ValueReserveSize(base::Value& v, size_t len);
+BASE_EXPORT std::unique_ptr<ValueSlot> NewValueSlotForTesting();
+BASE_EXPORT rust::String DumpValueSlot(const ValueSlot& v);
+BASE_EXPORT void ConstructNoneValue(ValueSlot& v);
+BASE_EXPORT void ConstructBoolValue(ValueSlot& v, bool value);
+BASE_EXPORT void ConstructIntegerValue(ValueSlot& v, int value);
+BASE_EXPORT void ConstructDoubleValue(ValueSlot& v, double value);
+BASE_EXPORT void ConstructStringValue(ValueSlot& v, rust::Str value);
+BASE_EXPORT base::Value& ConstructDictValue(ValueSlot& v);
+BASE_EXPORT base::Value& ConstructListValue(ValueSlot& v);
 
 }  // namespace rs_glue
 }  // namespace base
