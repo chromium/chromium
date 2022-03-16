d1f7ff9dfc8742e2b828cfd30dd83c2c1c9f4e33
Joemer Ramos <joemerramos@chromium.org>
2022-03-16 15:33:38 +0000

    [iOS] Remove iOS 14 guards in JavaScriptContentWorld
    Clean available and compile guards since build will be on iOS 14 or
    higher
    
    Bug: 1129484
    Change-Id: I04c3b613858b6cce7f137dc2cadfbe9a330e78ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523466
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981652}

--- ios/web/js_messaging/java_script_content_world.mm
+++ ios/web/js_messaging/java_script_content_world.mm
@@ -84,26 +84,22 @@ void JavaScriptContentWorld::AddFeature(const JavaScriptFeature* feature) {
     return;
   }
 
-#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
-  if (@available(iOS 14, *)) {
-    // Ensure |feature| supports this content world.
-    if (content_world_) {
-      JavaScriptFeature::ContentWorld incompatible_world_value;
-      if (content_world_ == WKContentWorld.pageWorld) {
-        // A feature specifying kIsolatedWorldOnly must not be added to the page
-        // content world.
-        incompatible_world_value =
-            JavaScriptFeature::ContentWorld::kIsolatedWorldOnly;
-      } else {
-        // A feature specifying kPageContentWorld must not be added to an
-        // isolated world.
-        incompatible_world_value =
-            JavaScriptFeature::ContentWorld::kPageContentWorld;
-      }
-      DCHECK_NE(feature->GetSupportedContentWorld(), incompatible_world_value);
+  // Ensure |feature| supports this content world.
+  if (content_world_) {
+    JavaScriptFeature::ContentWorld incompatible_world_value;
+    if (content_world_ == WKContentWorld.pageWorld) {
+      // A feature specifying kIsolatedWorldOnly must not be added to the page
+      // content world.
+      incompatible_world_value =
+          JavaScriptFeature::ContentWorld::kIsolatedWorldOnly;
+    } else {
+      // A feature specifying kPageContentWorld must not be added to an
+      // isolated world.
+      incompatible_world_value =
+          JavaScriptFeature::ContentWorld::kPageContentWorld;
     }
+    DCHECK_NE(feature->GetSupportedContentWorld(), incompatible_world_value);
   }
-#endif  // defined(__IPHONE14_0)
 
   features_.insert(feature);
 
@@ -123,27 +119,20 @@ void JavaScriptContentWorld::AddFeature(const JavaScriptFeature* feature) {
         feature_script.GetTargetFrames() !=
         JavaScriptFeature::FeatureScript::TargetFrames::kAllFrames;
 
-    WKUserScript* user_script = nil;
-#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
-    if (@available(iOS 14, *)) {
-      if (content_world_) {
-        user_script = [[WKUserScript alloc]
-              initWithSource:feature_script.GetScriptString()
-               injectionTime:injection_time
-            forMainFrameOnly:main_frame_only
-              inContentWorld:content_world_];
-      }
-    }
-#endif  // defined(__IPHONE14_0)
-
-    if (!user_script) {
-      user_script =
+    if (content_world_) {
+      WKUserScript* user_script =
+          [[WKUserScript alloc] initWithSource:feature_script.GetScriptString()
+                                 injectionTime:injection_time
+                              forMainFrameOnly:main_frame_only
+                                inContentWorld:content_world_];
+      [user_content_controller_ addUserScript:user_script];
+    } else {
+      WKUserScript* user_script =
           [[WKUserScript alloc] initWithSource:feature_script.GetScriptString()
                                  injectionTime:injection_time
                               forMainFrameOnly:main_frame_only];
+      [user_content_controller_ addUserScript:user_script];
     }
-
-    [user_content_controller_ addUserScript:user_script];
   }
 
   // Setup Javascript message callback.
@@ -156,20 +145,13 @@ void JavaScriptContentWorld::AddFeature(const JavaScriptFeature* feature) {
         base::SysUTF8ToNSString(optional_handler_name.value());
 
     std::unique_ptr<ScopedWKScriptMessageHandler> script_message_handler;
-
-#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
-    if (@available(iOS 14, *)) {
-      if (content_world_) {
-        script_message_handler = std::make_unique<ScopedWKScriptMessageHandler>(
-            user_content_controller_, handler_name, content_world_,
-            base::BindRepeating(&JavaScriptContentWorld::ScriptMessageReceived,
-                                weak_factory_.GetWeakPtr(), handler.value(),
-                                browser_state_));
-      }
-    }
-#endif  // defined(__IPHONE14_0)
-
-    if (!script_message_handler.get()) {
+    if (content_world_) {
+      script_message_handler = std::make_unique<ScopedWKScriptMessageHandler>(
+          user_content_controller_, handler_name, content_world_,
+          base::BindRepeating(&JavaScriptContentWorld::ScriptMessageReceived,
+                              weak_factory_.GetWeakPtr(), handler.value(),
+                              browser_state_));
+    } else {
       script_message_handler = std::make_unique<ScopedWKScriptMessageHandler>(
           user_content_controller_, handler_name,
           base::BindRepeating(&JavaScriptContentWorld::ScriptMessageReceived,
