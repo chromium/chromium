e3400b3ca0004f5286e0ecca82e6e669159cf2ee
Brandon Wylie <wylieb@chromium.org>
2022-03-16 23:43:20 +0000

    Remove partnercustomizations from AppHooks
    AppHooks has been replaced with the delegate implementation pattern.
    The AppHooks code isn't used anymore and can be removed.
    
    Bug: 1291657
    Change-Id: I184ff23710dedec26303c8bd064ef2f51ef75dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517825
    Reviewed-by: bttk - <bttk@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981913}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -30,7 +30,6 @@ import org.chromium.chrome.browser.notifications.chime.ChimeDelegate;
 import org.chromium.chrome.browser.omaha.RequestGenerator;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmark;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksProviderIterator;
-import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.password_manager.GooglePasswordManagerUIProvider;
 import org.chromium.chrome.browser.policy.PolicyAuditor;
 import org.chromium.chrome.browser.rlz.RevenueStats;
@@ -246,14 +245,6 @@ public abstract class AppHooks {
     @Nullable
     public PartnerBookmark.BookmarkIterator getPartnerBookmarkIterator() {
         return PartnerBookmarksProviderIterator.createIfAvailable();
-    }
-
-    /**
-     * @return An instance of PartnerBrowserCustomizations.Provider that provides customizations
-     * specified by partners.
-     */
-    public PartnerBrowserCustomizations.Provider getCustomizationProvider() {
-        return new PartnerBrowserCustomizations.ProviderPackage();
     }
 
     /**
