eb1bb6f9b89a5a787a9f77b758698829e79b3d02
Jeffrey Cohen <jeffreycohen@chromium.org>
2022-03-16 20:36:05 +0000

    Revert "[CodeHealthRotation] remove EventFilteringInfo Class and complete"
    This reverts commit 47a2981f49edfab5910b24c0811e1d93f0cb8e63.
    
    Reason for revert: Crashing in worker thread crbug.com/1306871
    
    Original change's description:
    > [CodeHealthRotation] remove EventFilteringInfo Class and complete
    > conversion to using mojo struct
    >
    > Bug: 1222550
    > Change-Id: I04f24d1c03bcb5087fd614d0feda7145515985cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378654
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#980882}
    
    Bug: 1222550
    Change-Id: I7dc90b76ba44d2e57dfa11eb0a39c2deb7d9d216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529418
    Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981844}

--- extensions/browser/event_router.h
+++ extensions/browser/event_router.h
@@ -449,8 +449,8 @@ class EventRouter : public KeyedService,
                                int64_t service_worker_version_id);
 
   void RouteDispatchEvent(content::RenderProcessHost* rph,
-                          mojom::DispatchEventParamsPtr params,
-                          base::ListValue& event_args);
+                          const mojom::DispatchEventParamsPtr params,
+                          const base::ListValue& event_args);
 
   // static
   static void DoDispatchEventToSenderBookkeeping(
