bafec2415e83c4712e299c0ce1dadb202b25bbf9
David Maunder <davidjm@chromium.org>
2022-03-16 18:31:22 +0000

    Retrieve CriticalPersistedTabData attributes in mapper
    Currently we deserialize the FlatBuffer in the mapper (which
    is used on the same I/O thread as the file read) and then pull
    the attributes off the FlatBuffer in the TabImpl constructor.
    I am observing a small number of crashes on
    flatBuffer.webContentsStateBytesAsByteBuffer() by which point it is
    too late to fall back to TabState or re-create the Tab. This CL
    moves field acquisition for the FlatBuffer to the mapper which means
    if something goes wrong we can return a null
    SerializedCriticalPersistedTabData which will in turn force the code
    to fallback to TabState (and failing that re-create the Tab).
    
    Bug: 1302098
    Change-Id: I183760f80dfe993dc3a1b67aeaa151eed869db57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527607
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981752}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
@@ -502,7 +502,7 @@ public class TabPersistentStoreTest {
                     tabId, isIncognito, (res) -> { callbackHelper.notifyCalled(res); });
         });
         callbackHelper.waitForCallback(chCount);
-        Assert.assertEquals(isNull, callbackHelper.getRes().isEmpty());
+        Assert.assertEquals(isNull, callbackHelper.getRes() == null);
     }
 
     private static class CPTDCallbackHelper extends CallbackHelper {
