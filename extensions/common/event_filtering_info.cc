6219eb14e99c3e658340b5fa33a3a2d67520622d
Jeffrey Cohen <jeffreycohen@chromium.org>
2022-03-16 17:52:23 +0000

    Revert "[CodeHealthRotation] remove EventFilteringInfo Class and complete"
    This reverts commit 47a2981f49edfab5910b24c0811e1d93f0cb8e63.
    
    Reason for revert: Crashing in worker thread crbug.com/1306871
    
    Original change's description:
    > [CodeHealthRotation] remove EventFilteringInfo Class and complete
    > conversion to using mojo struct
    >
    > Bug: 1222550
    > Change-Id: I04f24d1c03bcb5087fd614d0feda7145515985cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378654
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#980882}
    
    Bug: 1222550
    Change-Id: I7dc90b76ba44d2e57dfa11eb0a39c2deb7d9d216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530490
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4947@{#3}
    Cr-Branched-From: 1a43dd6a9b356bc6ae06fb27447822b644e1d679-refs/heads/main@{#981442}

---
+++ extensions/common/event_filtering_info.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/common/event_filtering_info.h"
+
+namespace extensions {
+
+EventFilteringInfo::EventFilteringInfo() {}
+EventFilteringInfo::EventFilteringInfo(const EventFilteringInfo& other) =
+    default;
+EventFilteringInfo::~EventFilteringInfo() {}
+
+}  // namespace extensions
