9d5f976baf7adde2ea9e6b2168360e152c904318
Mason Freed <masonf@chromium.org>
2022-03-16 23:24:12 +0000

    Renumber a bug and change description
    It seems that the rollback of this buffer size increase *did*
    fix the crash rate increase. I filed a fresh bug, and this updates
    the comment text. No functional changes here.
    
    Bug: 1260751,1306998
    Change-Id: I24feae19c26a9f2174699afdc198384e780a07a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531192
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981900}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -214,9 +214,8 @@ static_assert(kDefaultDataPipeAllocationSize >= net::kMaxBytesToSniff,
 // static
 uint32_t GetDataPipeDefaultAllocationSize(DataPipeAllocationSize option) {
 #if BUILDFLAG(IS_CHROMEOS)
-  // TODO(crbug.com/1260751): It is unclear if the increased data pipe size
-  // is responsible for an increased CrOS crash rate, so the size is being
-  // reverted to the default while we investigate.
+  // TODO(crbug.com/1306998): ChromeOS experiences a much higher OOM crash
+  // rate if the larger data pipe size is used.
   return kDefaultDataPipeAllocationSize;
 #else
   // For low-memory devices, always use the (smaller) default buffer size.
