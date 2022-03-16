ba6bc5aab4499a41028f2409988bb43e8e5b6428
Nohemi Fernandez <fernandex@chromium.org>
2022-03-16 15:01:20 +0000

    Refactor AccountCapabilitiesTest to use parametized methods.
    Makes it simpler to add capabilities to the test class by parametizing
    several test cases applicable to all capabilities.
    
    Bug: 1304162
    Change-Id: Ifa7f00ba54ed3d1efd58292cc2ddfd02d8ae7f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525123
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981632}

--- components/signin/public/android/junit/src/org/chromium/components/signin/base/AccountCapabilitiesTest.java
+++ components/signin/public/android/junit/src/org/chromium/components/signin/base/AccountCapabilitiesTest.java
@@ -6,28 +6,91 @@ package org.chromium.components.signin.base;
 
 import static org.mockito.Mockito.spy;
 
+import com.google.common.collect.Lists;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
-import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.params.BlockJUnit4RunnerDelegate;
+import org.chromium.base.test.params.ParameterAnnotations;
+import org.chromium.base.test.params.ParameterProvider;
+import org.chromium.base.test.params.ParameterSet;
+import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.components.signin.AccountCapabilitiesConstants;
 import org.chromium.components.signin.AccountManagerDelegate;
 import org.chromium.components.signin.Tribool;
 import org.chromium.components.signin.test.util.FakeAccountManagerDelegate;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Test class for {@link AccountCapabilities}.
- * TODO(crbug.com/1304162): Refactor to use parameterized test cases.
  */
-@RunWith(BaseRobolectricTestRunner.class)
+@RunWith(ParameterizedRunner.class)
+@ParameterAnnotations.UseRunnerDelegate(BlockJUnit4RunnerDelegate.class)
 @Config(manifest = Config.NONE)
 public final class AccountCapabilitiesTest {
     private FakeAccountManagerDelegate mDelegate;
+
+    /**
+     * Returns the capability value for the specified capability name
+     * from the appropriate getter in AccountCapabilities.
+     */
+    public static @Tribool int getCapability(
+            String capabilityName, AccountCapabilities capabilities) {
+        switch (capabilityName) {
+            case AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME:
+                return capabilities.canOfferExtendedSyncPromos();
+            case AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME:
+                return capabilities.canRunChromePrivacySandboxTrials();
+            case AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME:
+                return capabilities.isSubjectToParentalControls();
+        }
+        assert false : "Capability name is not known.";
+        return -1;
+    }
+
+    /**
+     * List of parameters to run in capability fetching tests.
+     */
+    public static class CapabilitiesTestParams implements ParameterProvider {
+        private static List<ParameterSet> sCapabilties = Arrays.asList(
+                new ParameterSet()
+                        .name("CanRunChromePrivacySandboxTrials")
+                        .value(AccountCapabilitiesConstants
+                                        .CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME),
+                new ParameterSet()
+                        .name("IsSubjectToParentalControls")
+                        .value(AccountCapabilitiesConstants
+                                        .IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME),
+                new ParameterSet()
+                        .name("CanOfferExtendedChromeSyncPromos")
+                        .value(AccountCapabilitiesConstants
+                                        .CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME));
+
+        // Returns String value added from Capabilities ParameterSet.
+        static String getCapabilityName(ParameterSet parameterSet) {
+            String parameterSetString = parameterSet.toString();
+            // Removes the brackets added by Arrays.toString for parameter value.
+            return parameterSetString.replace("[", "").replace("]", "");
+        }
+
+        static {
+            // Asserts that the list of parameters contains all supported capability names.
+            assert AccountCapabilities.SUPPORTED_ACCOUNT_CAPABILITY_NAMES.containsAll(
+                    Lists.transform(sCapabilties, (paramSet) -> getCapabilityName(paramSet)));
+        }
+
+        @Override
+        public Iterable<ParameterSet> getParameters() {
+            return sCapabilties;
+        }
+    }
 
     @Before
     public void setUp() {
@@ -35,126 +98,43 @@ public final class AccountCapabilitiesTest {
     }
 
     @Test
-    public void testCanRunChromePrivacySandboxTrialsException() {
+    @ParameterAnnotations.UseMethodParameter(CapabilitiesTestParams.class)
+    public void testCapabilityResponseException(String capabilityName) {
         AccountCapabilities capabilities = new AccountCapabilities();
         capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.canRunChromePrivacySandboxTrials(), Tribool.UNKNOWN);
+                capabilityName, AccountManagerDelegate.CapabilityResponse.EXCEPTION);
+        Assert.assertEquals(getCapability(capabilityName, capabilities), Tribool.UNKNOWN);
     }
 
     @Test
-    public void testCanRunChromePrivacySandboxTrialsTrue() {
+    @ParameterAnnotations.UseMethodParameter(CapabilitiesTestParams.class)
+    public void testCapabilityResponseYes(String capabilityName) {
         AccountCapabilities capabilities = new AccountCapabilities();
         capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.YES);
-        Assert.assertEquals(capabilities.canRunChromePrivacySandboxTrials(), Tribool.TRUE);
+                capabilityName, AccountManagerDelegate.CapabilityResponse.YES);
+        Assert.assertEquals(getCapability(capabilityName, capabilities), Tribool.TRUE);
     }
 
     @Test
-    public void testCanRunChromePrivacySandboxTrialsFalse() {
+    @ParameterAnnotations.UseMethodParameter(CapabilitiesTestParams.class)
+    public void testCapabilityResponseNo(String capabilityName) {
         AccountCapabilities capabilities = new AccountCapabilities();
         capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.canRunChromePrivacySandboxTrials(), Tribool.FALSE);
+                capabilityName, AccountManagerDelegate.CapabilityResponse.NO);
+        Assert.assertEquals(getCapability(capabilityName, capabilities), Tribool.FALSE);
     }
 
     @Test
-    public void testCanRunChromePrivacySandboxTrialsFalseAfterException() {
+    @ParameterAnnotations.UseMethodParameter(CapabilitiesTestParams.class)
+    public void testCapabilityResponseFalseAfterException(String capabilityName) {
         AccountCapabilities capabilities = new AccountCapabilities();
         capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.canRunChromePrivacySandboxTrials(), Tribool.FALSE);
+                capabilityName, AccountManagerDelegate.CapabilityResponse.NO);
+        Assert.assertEquals(getCapability(capabilityName, capabilities), Tribool.FALSE);
 
         capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_RUN_CHROME_PRIVACY_SANDBOX_TRIALS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.canRunChromePrivacySandboxTrials(), Tribool.FALSE);
-    }
-
-    @Test
-    public void testCanOfferExtendedSyncPromosException() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.canOfferExtendedSyncPromos(), Tribool.UNKNOWN);
-    }
-
-    @Test
-    public void testCanOfferExtendedSyncPromosTrue() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.YES);
-        Assert.assertEquals(capabilities.canOfferExtendedSyncPromos(), Tribool.TRUE);
-    }
-
-    @Test
-    public void testCanOfferExtendedSyncPromosFalse() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.canOfferExtendedSyncPromos(), Tribool.FALSE);
-    }
-
-    @Test
-    public void testCanOfferExtendedSyncPromosFalseAfterException() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.canOfferExtendedSyncPromos(), Tribool.FALSE);
-
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.CAN_OFFER_EXTENDED_CHROME_SYNC_PROMOS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.canOfferExtendedSyncPromos(), Tribool.FALSE);
-    }
-
-    @Test
-    public void testIsSubjectToParentalControlsException() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.isSubjectToParentalControls(), Tribool.UNKNOWN);
-    }
-
-    @Test
-    public void testIsSubjectToParentalControlsTrue() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.YES);
-        Assert.assertEquals(capabilities.isSubjectToParentalControls(), Tribool.TRUE);
-    }
-
-    @Test
-    public void testIsSubjectToParentalControlsFalse() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.isSubjectToParentalControls(), Tribool.FALSE);
-    }
-
-    @Test
-    public void testIsSubjectToParentalControlsFalseAfterException() {
-        AccountCapabilities capabilities = new AccountCapabilities();
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.NO);
-        Assert.assertEquals(capabilities.isSubjectToParentalControls(), Tribool.FALSE);
-
-        capabilities.setAccountCapability(
-                AccountCapabilitiesConstants.IS_SUBJECT_TO_PARENTAL_CONTROLS_CAPABILITY_NAME,
-                AccountManagerDelegate.CapabilityResponse.EXCEPTION);
-        Assert.assertEquals(capabilities.isSubjectToParentalControls(), Tribool.FALSE);
+                capabilityName, AccountManagerDelegate.CapabilityResponse.EXCEPTION);
+        Assert.assertEquals(getCapability(capabilityName, capabilities), Tribool.FALSE);
     }
 
     @Test
