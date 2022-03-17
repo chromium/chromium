32eaf929988291a030b419ee20370357210e674d
Anastasiia N <anastasiian@chromium.org>
2022-03-17 08:06:16 +0000

    Revert "[CameraRoll] Improve unitest coverage"
    This reverts commit e820ce531184f8ffd81ef67e1870b544704a0e75.
    
    Reason for revert: CameraRollThumbnailTest.ViewLayout is failing on
    Linux Chromium OS ASan LSan Tests
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/42131/overview
    
    Original change's description:
    > [CameraRoll] Improve unitest coverage
    >
    > * The goal is to have >=90% unit test coverage on camera roll related
    >   classes.
    > * Add unit test for camera_roll_thumbnail
    > * camera_roll_menu_model was tested as part of CameraRollThumbnail
    >   button press event and menu item click event.
    >
    > Change-Id: Iee05bf52bdb7ee1839feb2eb31fa122630e17c96
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514099
    > Auto-Submit: Jianbing Wu <jianbing@google.com>
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Commit-Queue: Jon Mann <jonmann@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#981776}
    
    Change-Id: I0af85472d5e7055f53b862f373ac7d3c7443f005
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531908
    Owners-Override: Anastasiia N <anastasiian@chromium.org>
    Auto-Submit: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#982080}

--- ash/system/phonehub/camera_roll_thumbnail.h
+++ ash/system/phonehub/camera_roll_thumbnail.h
@@ -38,9 +38,6 @@ class ASH_EXPORT CameraRollThumbnail : public views::MenuButton {
  private:
   FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, ImageThumbnail);
   FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, VideoThumbnail);
-  FRIEND_TEST_ALL_PREFIXES(CameraRollThumbnailTest, ImageThumbnail);
-  FRIEND_TEST_ALL_PREFIXES(CameraRollThumbnailTest, VideoThumbnail);
-  FRIEND_TEST_ALL_PREFIXES(CameraRollThumbnailTest, PressButtonAndMenuItem);
 
   void ButtonPressed();
   ui::SimpleMenuModel* GetMenuModel();
