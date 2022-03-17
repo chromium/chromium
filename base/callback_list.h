f736f5bb1be88fc8e3c5f7262ee6abaab4092afa
Lei Zhang <thestig@chromium.org>
2022-03-17 03:35:18 +0000

    Remove an obsolete comment about base::CallbackList.
    Since base::Callback is gone, the comment is no longer necessary.
    
    Change-Id: Ieeb259a6932f72be213e579b4cd19143bf9f82d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531548
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982024}

--- base/callback_list.h
+++ base/callback_list.h
@@ -340,9 +340,7 @@ class RepeatingCallbackList
   }
 };
 
-// Syntactic sugar to parallel that used for Callbacks.
-// ClosureList explicitly not provided since it is not used, and CallbackList
-// is deprecated. {Once,Repeating}ClosureList should instead be used.
+// Syntactic sugar to parallel that used for {Once,Repeating}Callbacks.
 using OnceClosureList = OnceCallbackList<void()>;
 using RepeatingClosureList = RepeatingCallbackList<void()>;
 
