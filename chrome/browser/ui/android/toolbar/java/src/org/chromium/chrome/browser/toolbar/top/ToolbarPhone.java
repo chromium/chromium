3b87c44cee4377ea6d77742f9871b2dfc3719051
spdonghao <spdonghao@chromium.org>
2022-03-17 18:49:30 +0000

    Add a null check for LocationBarPhone in posted runnable in #onDefaultSearchEngineChanged.
    Bug: 1306554
    Change-Id: If37d0475ff92494fd6964f9b8066133f5a8c8ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527424
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982297}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -45,6 +45,7 @@ import androidx.appcompat.graphics.drawable.DrawableWrapper;
 import androidx.core.widget.ImageViewCompat;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.CallbackController;
 import org.chromium.base.MathUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.chrome.browser.browser_controls.BrowserStateBrowserControlsVisibilityDelegate;
@@ -286,6 +287,8 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
      */
     private float mLocBarWidthChangeFraction;
 
+    private CallbackController mCallbackController = new CallbackController();
+
     /**
      * A global layout listener used to capture a new texture when the experimental toolbar button
      * is added or removed.
@@ -387,6 +390,10 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
 
     @Override
     void destroy() {
+        if (mCallbackController != null) {
+            mCallbackController.destroy();
+            mCallbackController = null;
+        }
         cancelAnimations();
         super.destroy();
     }
@@ -2344,12 +2351,12 @@ public class ToolbarPhone extends ToolbarLayout implements OnClickListener, TabC
         // TODO(tedchoc): Move away from updating based on the search engine change and instead
         //                add the toolbar as a listener to the NewTabPage and udpate only when
         //                it notifies the listeners that it has changed its state.
-        post(new Runnable() {
-            @Override
-            public void run() {
+        post(() -> {
+            if (mCallbackController == null) return;
+            mCallbackController.makeCancelable(() -> {
                 updateVisualsForLocationBarState();
                 updateNtpAnimationState();
-            }
+            });
         });
     }
 
