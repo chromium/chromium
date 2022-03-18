840d7b733ecd833e6510c888af8a3accb0e71924
Simon Zünd <szuend@chromium.org>
2022-03-18 13:12:20 +0000

    [devtools] Make attribution-invalid-data.js more deterministic
    In rare cases (locally only happened once in a few hundred runs) issues
    arrive in reverse order on the protocol level. This CL accounts for
    that by sorting issues.
    
    Drive-by: This CL also marks more inspector-protocol tests as slow,
    particularly we moved inspector-protocol tests to `http/tests/` a
    while ago but never updated the SlowExpectation. Investigating this
    flake also showed that conversion tests seem to take a while to run
    on windows bots so we mark the conversion tests as slow on Win as
    well.
    
    R=alexrudenko@chromium.org
    
    Fixed: 1306848
    Change-Id: Ic57d25140cb71d68560addfb463cd071566194a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3535962
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982682}

--- third_party/blink/web_tests/http/tests/inspector-protocol/conversion/attribution-invalid-data.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/conversion/attribution-invalid-data.js
@@ -14,9 +14,17 @@
     <!DOCTYPE html>
     <img src="https://devtools.test:8443/inspector-protocol/conversion/resources/conversion-redirect.php"></img>
     <img src="https://devtools.test:8443/inspector-protocol/conversion/resources/conversion-redirect.php?trigger-data=badinteger"></img>`);
-  const issues = await Promise.all(issuePromises);
-  testRunner.log(issues[0].params.issue, "Issue reported: ", ['requestId']);
-  testRunner.log(issues[1].params.issue, "Issue reported: ", ['requestId']);
+
+  const issues = (await Promise.all(issuePromises)).map(issue => issue.params.issue);
+
+  // In rare cases the issues arrive in reverse order. Lets sort them to be sure.
+  issues.sort((i1, i2) => {
+    return (i1.invalidParamter < i2.invalidParamter) ? -1 :
+           (i2.invalidParamter < i1.invalidParamter) ?  1 : 0;
+  });
+
+  testRunner.log(issues[0], "Issue reported: ", ['requestId']);
+  testRunner.log(issues[1], "Issue reported: ", ['requestId']);
 
   testRunner.completeTest();
 })
