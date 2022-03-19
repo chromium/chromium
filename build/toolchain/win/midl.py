1ff57b45ef5b77961650441461e38596e86b6b8a
Will Harris <wfh@chromium.org>
2022-03-19 17:13:01 +0000

    Always print midl output even if failure return code.
    BUG=1307907
    
    Change-Id: I07f69acb7b48dfcc59ea62633caef8986138fe41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537690
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983067}

--- build/toolchain/win/midl.py
+++ build/toolchain/win/midl.py
@@ -264,8 +264,6 @@ def run_midl(args, env_dict):
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
     out, _ = popen.communicate()
-    if popen.returncode != 0:
-      return popen.returncode, midl_output_dir
 
     # Filter junk out of stdout, and write filtered versions. Output we want
     # to filter is pairs of lines that look like this:
@@ -278,6 +276,9 @@ def run_midl(args, env_dict):
     for line in lines:
       if not line.startswith(prefixes) and line not in processing:
         print(line)
+
+    if popen.returncode != 0:
+      return popen.returncode, midl_output_dir
 
     for f in os.listdir(midl_output_dir):
       ZapTimestamp(os.path.join(midl_output_dir, f))
