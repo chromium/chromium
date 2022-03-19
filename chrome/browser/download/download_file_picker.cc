da1cf358986f72a6c0b233808edd07b307082572
Erik Chen <erikchen@chromium.org>
2022-03-19 00:08:51 +0000

    lacros: Fix crash in download picker for extensions.
    There were two problems:
    (1) Extensions that invoke the download picker go through a code path on
    Linux that wasn't compiled into Lacros.
    (2) The implementation of FindBrowserWithActiveWindow picks the most
    recently used browser, and returns nullptr if the browser's window is
    not active. This CL switches to always using the most recent browser.
    
    Bug: 1303992
    Change-Id: I3bd96c6d7bf35bcddc09a2551cc14cdb2386a20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537674
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982967}

--- chrome/browser/download/download_file_picker.cc
+++ chrome/browser/download/download_file_picker.cc
@@ -14,8 +14,8 @@
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
 
-#if BUILDFLAG(IS_LINUX)
-#include "chrome/browser/ui/browser_finder.h"
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "ui/aura/window.h"
 #endif
@@ -74,10 +74,13 @@ DownloadFilePicker::DownloadFilePicker(DownloadItem* item,
   // dialog in Linux. See SelectFileImpl() in select_file_dialog_linux_gtk.cc.
   // Here we make owning_window host to browser current active window
   // if it is null. https://crbug.com/1301898
-#if BUILDFLAG(IS_LINUX)
-  if (!owning_window || !owning_window->GetHost())
-    owning_window =
-        chrome::FindBrowserWithActiveWindow()->window()->GetNativeWindow();
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+  if (!owning_window || !owning_window->GetHost()) {
+    owning_window = BrowserList::GetInstance()
+                        ->GetLastActive()
+                        ->window()
+                        ->GetNativeWindow();
+  }
 #endif
 
   select_file_dialog_->SelectFile(
