d7a1a518fc03b7bb160fa4ce6b507d481c671ef7
Tom Anderson <thomasanderson@chromium.org>
2022-03-19 00:50:05 +0000

    Reland "Dispatch one event at a time in X11EventWatcherFdWatch"
    This is a reland of commit 185ef49e12df76385fe5258d76047a21ab5f7e61
    
    Original change's description:
    > Dispatch one event at a time in X11EventWatcherFdWatch
    >
    > This CL is similar to [1] which switched X11EventWatcherGlib to dispatch
    > one event at a time. Note: X11EventWatcherGlib is used on Linux and
    > X11EventWatcherFdWatch is used on ChromeOS.
    >
    > Dispatching only one event is necessary for proper behavior of
    > base::RunLoop::Quit(). If multiple events are dispatched, then the
    > behavior is more similar to base::RunLoop::QuitWhenIdle().
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/2907732
    >
    > R=sky
    >
    > Change-Id: If3b47888511ed10142968ce682ab357bf176d24f
    > Fixed: 1257412
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448192
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#971353}
    
    Fixed: 1257412
    
    Change-Id: If6304102d7d933a2393b858377d7022e2499165f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537095
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982980}

--- ui/events/platform/x11/x11_event_watcher_fdwatch.cc
+++ ui/events/platform/x11/x11_event_watcher_fdwatch.cc
@@ -4,12 +4,17 @@
 
 #include "ui/events/platform/x11/x11_event_watcher_fdwatch.h"
 
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "base/task/current_thread.h"
 
 namespace ui {
 
 X11EventWatcherFdWatch::X11EventWatcherFdWatch(X11EventSource* source)
-    : event_source_(source), watcher_controller_(FROM_HERE) {}
+    : event_source_(source),
+      connection_watcher_(FROM_HERE),
+      pipe_watcher_(FROM_HERE) {}
 
 X11EventWatcherFdWatch::~X11EventWatcherFdWatch() {
   StopWatching();
@@ -23,7 +28,11 @@ void X11EventWatcherFdWatch::StartWatching() {
 
   int fd = event_source_->connection()->GetFd();
   base::CurrentUIThread::Get()->WatchFileDescriptor(
-      fd, true, base::MessagePumpForUI::WATCH_READ, &watcher_controller_, this);
+      fd, true, base::MessagePumpForUI::WATCH_READ, &connection_watcher_, this);
+
+  PCHECK(pipe2(pipe_, O_CLOEXEC | O_NONBLOCK) != -1);
+  base::CurrentUIThread::Get()->WatchFileDescriptor(
+      pipe_[0], true, base::MessagePumpForUI::WATCH_READ, &pipe_watcher_, this);
   started_ = true;
 }
 
@@ -31,14 +40,37 @@ void X11EventWatcherFdWatch::StopWatching() {
   if (!started_)
     return;
 
-  watcher_controller_.StopWatchingFileDescriptor();
+  connection_watcher_.StopWatchingFileDescriptor();
+  pipe_watcher_.StopWatchingFileDescriptor();
+  for (int& pfd : pipe_) {
+    PCHECK(close(pfd) != -1);
+    pfd = -1;
+  }
   started_ = false;
 }
 
 void X11EventWatcherFdWatch::OnFileCanReadWithoutBlocking(int fd) {
-  // We must dispatch all events, otherwise we may deadlock polling for new
-  // events when we already have events queued.
-  event_source_->connection()->DispatchAll();
+  // Drain the pipe up to 128 bytes at a time (this should be enough to
+  // empty it under normal conditions in a single syscall).
+  char buf[128];
+  while (read(pipe_[0], &buf, sizeof(buf)) != -1) {
+  }
+  PCHECK(errno == EAGAIN);
+
+  // Dispatch a single event.
+  auto* connection = event_source_->connection();
+  if (!connection->HasPendingResponses())
+    connection->ReadResponses();
+  connection->Dispatch();
+  connection->Flush();
+
+  // We may deadlock if there are more events to dispatch but the socket is not
+  // readable. To prevent this, write a byte to the pipe so that the message
+  // loop will wake up and dispatch the event. This will cause the event to be
+  // dispatched with the same priority instead of being delayed if PostTask was
+  // used.
+  if (connection->HasPendingResponses())
+    PCHECK(write(pipe_[1], "a", 1) != -1 || errno == EAGAIN);
 }
 
 void X11EventWatcherFdWatch::OnFileCanWriteWithoutBlocking(int fd) {
