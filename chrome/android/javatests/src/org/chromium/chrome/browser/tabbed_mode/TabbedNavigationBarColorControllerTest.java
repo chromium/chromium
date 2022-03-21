01a89c684fb2dd13bc6bad02a1809fc4f546da26
Salvador Guerrero <salg@google.com>
2022-03-21 21:03:08 +0000

    Disabled failing tests in TabbedNavigationBarColorControllerTest
    Bug: 1308777
    Change-Id: I864d067f3522c1bf2374b1b68f289fac765f1daa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538825
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#983484}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -68,6 +69,7 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleOverview() {
         assertEquals("Navigation bar should be white before entering overview mode.",
                 mLightNavigationColor, mWindow.getNavigationBarColor());
@@ -87,6 +89,7 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleIncognito() {
         assertEquals("Navigation bar should be white on normal tabs.", mLightNavigationColor,
                 mWindow.getNavigationBarColor());
@@ -106,6 +109,7 @@ public class TabbedNavigationBarColorControllerTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1308777")
     public void testToggleFullscreen() throws TimeoutException {
         assertEquals("Navigation bar should be white before entering fullscreen mode.",
                 mLightNavigationColor, mWindow.getNavigationBarColor());
