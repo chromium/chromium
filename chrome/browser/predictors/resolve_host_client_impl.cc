85157ec59ba2c99beb87dec460bd7c647ac97d0d
Colin Blundell <blundell@chromium.org>
2022-03-21 17:31:45 +0000

    [Preconnect] Add metric for queueing time of preconnect callbacks
    This metric will allow us to determine how much of the latency of
    host resolution/proxy lookup is due to the completion callbacks being
    queued (which can be impacted by prioritization when scheduling these
    tasks).
    
    Bug: 1257582
    Change-Id: I0a039b47b02f75928a8c8e5f2e493f84aac91e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528675
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983374}

--- chrome/browser/predictors/resolve_host_client_impl.cc
+++ chrome/browser/predictors/resolve_host_client_impl.cc
@@ -8,6 +8,7 @@
 
 #include "base/bind.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/task/common/task_annotator.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/host_port_pair.h"
@@ -52,6 +53,21 @@ void ResolveHostClientImpl::OnComplete(
     const absl::optional<net::AddressList>& resolved_addresses) {
   UMA_HISTOGRAM_TIMES("Navigation.Preconnect.ResolveHostLatency",
                       base::TimeTicks::Now() - resolve_host_start_time_);
+
+  auto* task = base::TaskAnnotator::CurrentTaskForThread();
+  // As this method is executed as a callback from a Mojo call, it should be
+  // executed via RunTask() and thus have a non-delayed PendingTask associated
+  // with it.
+  DCHECK(task);
+  DCHECK(task->delayed_run_time.is_null());
+
+  // The task will have a null |queue_time| if run synchronously (this happens
+  // in unit tests, for example).
+  base::TimeTicks queue_time =
+      !task->queue_time.is_null() ? task->queue_time : base::TimeTicks::Now();
+  UMA_HISTOGRAM_TIMES("Navigation.Preconnect.ResolveHostCallbackQueueingTime",
+                      base::TimeTicks::Now() - queue_time);
+
   std::move(callback_).Run(result == net::OK);
 }
 
