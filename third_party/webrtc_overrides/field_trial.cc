3bb524433063d82371bad919f13962ef4d8fa256
Ali Tofigh <alito@chromium.org>
2022-03-21 16:23:07 +0000

    Provide absl::string_view version of FindFullName in webrtc_overrides/field_trial.cc
    BUG: 1308387
    Change-Id: Ib9bcfa12961c2447f6af8baa6c531e1523b2db93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540183
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Ali Tofigh <alito@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983342}

--- third_party/webrtc_overrides/field_trial.cc
+++ third_party/webrtc_overrides/field_trial.cc
@@ -3,13 +3,22 @@
 // found in the LICENSE file.
 
 #include "base/metrics/field_trial.h"
+#include "base/strings/string_piece.h"
+#include "third_party/abseil-cpp/absl/strings/string_view.h"
 
 // Define webrtc::field_trial::FindFullName to provide webrtc with a field trial
 // implementation.
 namespace webrtc {
 namespace field_trial {
+
+std::string FindFullName(absl::string_view trial_name) {
+  return base::FieldTrialList::FindFullName(
+      base::StringPiece(trial_name.data(), trial_name.length()));
+}
+
 std::string FindFullName(const std::string& trial_name) {
   return base::FieldTrialList::FindFullName(trial_name);
 }
+
 }  // namespace field_trial
 }  // namespace webrtc
