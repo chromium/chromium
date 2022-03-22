c13d494784f3ff1fc1a6aa15f6a188a03e281540
Kevin Lubick <kjlubick@google.com>
2022-03-22 11:56:33 +0000

    Replace unused Skia include with precise includes and forward declares
    According to go/chrome-includes, this will save about 115 MB in
    build size (0.05% of the current total).
    
    Removing the include did not break anything when building locally.
    Just to be sure, I made sure the this file properly followed
    IWYU for Skia types.
    
    Bug: 242216
    Change-Id: Ic20fc4b7828656705631bd4152778cbe5ee80a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3541160
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983764}

--- third_party/blink/renderer/platform/graphics/canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource.h
@@ -17,11 +17,16 @@
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_wrapper.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
-#include "third_party/skia/include/gpu/GrBackendSurface.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
+#include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
 
 #include <dawn/webgpu_cpp.h>
+
+class GrDirectContext;
+class GrBackendTexture;
+class SkImage;
 
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_CANVAS_RESOURCE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_CANVAS_RESOURCE_H_
