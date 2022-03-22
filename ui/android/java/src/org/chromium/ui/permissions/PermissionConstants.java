ee29ac626e0f1fc76fda3f1a53c36cb583119a6c
Finnur Thorarinsson <finnur@chromium.org>
2022-03-22 17:39:41 +0000

    [Android/MediaPicker]: Catch up with late changes to the SDK.
    The permission for images has changed recently (they added
    an S in IMAGES) and READ_EXTERNAL_DATA is no longer needed
    for this to function. Special thanks to Peter Birk Pakkenberg
    for helping to test this on SDK version 220317.
    
    BUG=1285818
    
    (cherry picked from commit 7951a45fe1762316fd1a7fad60102aa0d9fe0469)
    
    Change-Id: Ib8977653ce5790160d88022307f119a87b784dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3532801
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#982814}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542545
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4951@{#48}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- ui/android/java/src/org/chromium/ui/permissions/PermissionConstants.java
+++ ui/android/java/src/org/chromium/ui/permissions/PermissionConstants.java
@@ -13,6 +13,6 @@ public final class PermissionConstants {
     public static final String NOTIFICATION_PERMISSION = "android.permission.POST_NOTIFICATIONS";
 
     // TODO(finnur): Replace this with permission constant from {@link Manifest.permission}.
-    public static final String READ_MEDIA_IMAGE = "android.permission.READ_MEDIA_IMAGE";
+    public static final String READ_MEDIA_IMAGES = "android.permission.READ_MEDIA_IMAGES";
     public static final String READ_MEDIA_VIDEO = "android.permission.READ_MEDIA_VIDEO";
 }
