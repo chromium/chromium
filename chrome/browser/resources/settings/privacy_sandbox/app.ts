8b4c1f19eba59460b4264ded497f0234574dca59
Theodore Olsauskas-Warren <sauski@google.com>
2022-03-22 13:41:40 +0000

    Privacy Sandbox Settings: Inject tooltip style manually
    The style the paper-tooltip, CSS property mix-ins were used. These do
    not appear to work in optimized WebUI builds. This CL adjusts the way
    paper-tooltips are styled such that they are injected into the
    shadow dom stylesheets for the paper-tooltip.
    
    (cherry picked from commit 39957823fff6172b751faec7865ff1a72bf4503b)
    
    Bug: 1308258
    Change-Id: Icbfb0d6a05d7cb600b99b1f5ad2e77561dafc23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3535439
    Auto-Submit: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#983249}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538569
    Cr-Commit-Position: refs/branch-heads/4951@{#42}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- chrome/browser/resources/settings/privacy_sandbox/app.ts
+++ chrome/browser/resources/settings/privacy_sandbox/app.ts
@@ -359,6 +359,33 @@ export class PrivacySandboxAppElement extends PrivacySandboxAppElementBase {
         target.id === 'topicsTooltipIcon' ? '#topicsTooltip' :
                                             '#fledgeTooltip')!;
 
+    // Directly inject the required style into the stylesheets of the paper
+    // tooltip element. This is a workaround for CSS mixin properties seemingly
+    // being removed in optimized WebUI builds, and the paper-tooltip not
+    // supporting other styling methods.
+    // TODO(crbug.com/1308262): Expose required style hooks on paper-tooltip
+    const sheet = new CSSStyleSheet();
+    // @ts-ignore
+    sheet.replaceSync(`
+      #tooltip {
+            border-radius: 4px;
+            box-shadow: var(--cr-elevation-2);
+            font-family: Roboto, Arial, sans-serif;
+            font-size: inherit;
+            font-weight: 400;
+            line-height: 154%;  /* 20px. */
+            margin: 0 4px;
+      }`);
+    // @ts-ignore
+    const elemStyleSheets = tooltip.shadowRoot.adoptedStyleSheets;
+
+    if (elemStyleSheets.length === 0 ||
+        JSON.stringify(elemStyleSheets.slice(-1)[0]) !==
+            JSON.stringify(sheet)) {
+      // @ts-ignore
+      tooltip.shadowRoot.adoptedStyleSheets = [...elemStyleSheets, sheet];
+    }
+
     const hide = () => {
       tooltip.hide();
       target.removeEventListener('mouseleave', hide);
