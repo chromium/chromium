8c3e202bf99b4d39871d72f7c5ceffdd42e280eb
Florian Gauger <fga@google.com>
2022-03-23 10:45:41 +0000

    [AA CEX] Add JS_FORCED_ROUNDTRIP status
    This status will be used by js flows when they want to force an
    additional roundtrip. Can be used to cancel a speculative execution.
    
    (cherry picked from commit 693d3604eb2b60421e7d01d04925653a0f1de28f)
    
    Bug: 1308337
    Change-Id: I9b586a2b77702e411ea6bf583f827b90ca24f73b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536654
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#983246}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3545147
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4951@{#77}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- components/autofill_assistant/browser/client_status.cc
+++ components/autofill_assistant/browser/client_status.cc
@@ -170,6 +170,9 @@ std::ostream& operator<<(std::ostream& out,
     case ProcessedActionStatusProto::USER_DATA_REQUEST_FAILED:
       out << "USER_DATA_REQUEST_FAILED";
       break;
+    case ProcessedActionStatusProto::JS_FORCED_ROUNDTRIP:
+      out << "JS_FORCED_ROUNDTRIP";
+      break;
 
       // Intentionally no default case to make compilation fail if a new value
       // was added to the enum but not to this list.
