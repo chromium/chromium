eb456b634e4b917e475e99858da9cb5669f111f3
Kramer Ge <fangzhoug@chromium.org>
2022-03-23 20:45:25 +0000

    [LaCrOS]Temporary workaround to disable RED_8 overlay candidates creations through SharedImageInterface
    LaCrOS on ARM devices can't create RED_8 gmb's via Gbm device, this
    capability is not communicated to clients. Causing certain shared image
    creations to fail when delegated compositing is on. Disable this.
    
    (cherry picked from commit 3171a47d5f899ffe8bf11b6f2c65f542f49ec956)
    
    Change-Id: Ia03c9a00450f45d68f1194bb5614cc1cae3506c1
    Bug: 1301891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537451
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#982914}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546838
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Auto-Submit: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4951@{#97}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- components/viz/common/resources/resource_format_utils.cc
+++ components/viz/common/resources/resource_format_utils.cc
@@ -12,6 +12,7 @@
 
 #include "base/check_op.h"
 #include "base/notreached.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/gfx/buffer_types.h"
 
 namespace viz {
@@ -403,7 +404,13 @@ unsigned int TextureStorageFormat(ResourceFormat format,
 bool IsGpuMemoryBufferFormatSupported(ResourceFormat format) {
   switch (format) {
     case BGRA_8888:
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
+    // TODO(crbug.com/1307837): On ARM devices LaCrOS can't create RED_8
+    // GpuMemoryBuffer Objects with GBM device. This capability should be
+    // plumbed and known by clients requesting shared images as overlay
+    // candidate.
     case RED_8:
+#endif
     case R16_EXT:
     case RGBA_4444:
     case RGBA_8888:
@@ -416,6 +423,9 @@ bool IsGpuMemoryBufferFormatSupported(ResourceFormat format) {
     case ETC1:
     case ALPHA_8:
     case LUMINANCE_8:
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+    case RED_8:
+#endif
     case RGB_565:
     case LUMINANCE_F16:
     case BGR_565:
