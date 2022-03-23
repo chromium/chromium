92042bdb76a4b2d1f3d7e8a79fd4e5f944bff7f0
Clement Yan <clamclamyan@google.com>
2022-03-23 01:40:45 +0000

    [System Extensions] Setup testing framework for testing window management API.
    Due to limitations in the current platform, we test system extension APIs via enabling them using a flag.
    Then, tests can then be written as js code and passed to the helper to
    run inside a service worker accessing the API methods on a sample page.
    
    Bug: b/221124035
    Change-Id: If1c5157d029a096806be0c43ab39adaaecd8f7dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3521113
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#984138}

---
+++ chrome/browser/ash/system_extensions/api/window_management/cros_window_browsertest.cc
@@ -0,0 +1,126 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/constants/ash_features.h"
+#include "ash/constants/ash_switches.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_piece_forward.h"
+#include "base/test/bind.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/test/browser_test.h"
+
+namespace {
+
+static constexpr char kEventListenerCode[] = R"(
+  self.addEventListener('message', async (event) => {
+    try {
+      await test();
+      event.source.postMessage("PASS");
+    } catch (e) {
+      console.log(e.message);
+      event.source.postMessage("FAIL - Check console LOGS");
+    }
+  });
+)";
+
+static constexpr char kPostTestStart[] = R"(
+  (async () => {
+    const saw_message = new Promise(resolve => {
+      navigator.serviceWorker.onmessage = event => {
+        resolve(event.data);
+      };
+    });
+    const registration = await navigator.serviceWorker.ready;
+    registration.active.postMessage('test');
+    return await saw_message;
+  })();
+)";
+
+class CrosWindowBrowserTest : public InProcessBrowserTest {
+ public:
+  CrosWindowBrowserTest() {
+    feature_list_.InitAndEnableFeature(ash::features::kSystemExtensions);
+  }
+  ~CrosWindowBrowserTest() override = default;
+
+  // TODO(b/210737979):
+  // Remove switch toggles when service workers are supported on
+  // chrome-untrusted://
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(ash::switches::kSystemExtensionsDebug);
+    command_line->AppendSwitchASCII(
+        switches::kEnableBlinkFeatures,
+        "BlinkExtensionChromeOS,BlinkExtensionChromeOSWindowManagement");
+  }
+
+  void RunTest(base::StringPiece test_code) {
+    // Initialize embedded test server.
+    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
+
+    // Register test code with listener as .js file.
+    const std::string js_code = base::StrCat({test_code, kEventListenerCode});
+    embedded_test_server()->RegisterRequestHandler(base::BindLambdaForTesting(
+        [js_code](const net::test_server::HttpRequest& request)
+            -> std::unique_ptr<net::test_server::HttpResponse> {
+          if (request.relative_url != "/test_service_worker.js") {
+            return nullptr;
+          }
+          std::unique_ptr<net::test_server::BasicHttpResponse> http_response(
+              std::make_unique<net::test_server::BasicHttpResponse>());
+          http_response->set_code(net::HTTP_OK);
+          http_response->set_content(js_code);
+          http_response->set_content_type("text/javascript");
+          return std::move(http_response);
+        }));
+
+    // Register test code js as service worker
+    embedded_test_server()->StartAcceptingConnections();
+    EXPECT_TRUE(ui_test_utils::NavigateToURL(
+        browser(), embedded_test_server()->GetURL(
+                       "/service_worker/create_service_worker.html")));
+    EXPECT_EQ("DONE",
+              EvalJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                     "register('/test_service_worker.js');"));
+
+    // Post message to service worker listener to trigger test code and
+    // evaluate.
+    EXPECT_EQ("PASS",
+              EvalJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                     kPostTestStart));
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+}  // namespace
+
+IN_PROC_BROWSER_TEST_F(CrosWindowBrowserTest, CrosWindowSetFullscreen) {
+  const char test_code[] = R"(
+async function test() {
+  let windows = await chromeos.windowManagement.windows();
+  if (windows[0].isFullscreen) {
+    throw new Error("Window started in fullscreen.");
+  }
+
+  windows[0].setFullscreen(true);
+  windows = await chromeos.windowManagement.windows();
+  if (!windows[0].isFullscreen) {
+    throw new Error("cros_window.setFullscreen(true) failed");
+  }
+
+  windows[0].setFullscreen(false);
+  windows = await chromeos.windowManagement.windows();
+  if (windows[0].isFullscreen) {
+    throw new Error("cros_window.setFullscreen(false) failed");
+  }
+}
+  )";
+
+  RunTest(test_code);
+}
