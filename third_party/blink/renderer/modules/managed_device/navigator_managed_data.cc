6083135252280d9b43e26169eb35154a9ac861ea
UwU UwU <uwu7586@gmail.com>
2022-03-23 08:06:42 +0000

    NavigatorManagedData: Prevent iterator invalidation during Promise resolution
    Bug: 1307223
    Change-Id: Iead6cf8c6236a95dbdfe7877c912f6ba86b370ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3532439
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984230}

--- third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
+++ third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
@@ -108,8 +108,14 @@ void NavigatorManagedData::OnServiceConnectionError() {
       !managed_configuration_service_.is_connected()) {
     managed_configuration_service_.reset();
   }
+
+  // Move the set to a local variable to prevent script execution in Reject()
+  // from invalidating the iterator used by the loop.
+  HeapHashSet<Member<ScriptPromiseResolver>> pending_promises;
+  pending_promises_.swap(pending_promises);
+
   // Resolve all pending promises with a failure.
-  for (ScriptPromiseResolver* resolver : pending_promises_) {
+  for (ScriptPromiseResolver* resolver : pending_promises) {
     resolver->Reject(
         MakeGarbageCollected<DOMException>(DOMExceptionCode::kNotAllowedError,
                                            kNotHighTrustedAppExceptionMessage));
