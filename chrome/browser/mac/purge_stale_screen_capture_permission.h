a564e933eaef7d12d9df1df1f870158d9560b4f7
Tom Burgin <bur@chromium.org>
2022-03-23 19:09:52 +0000

    purge stale screen capture permission: make use of ui::IsScreenCaptureAllowed()
    This CL is a follow up from https://crrev.com/c/3535659. That CL was
    authored during a tight timeline and a few oversights were made. Namely
    the TCC.db is indeed SIP protected and in normal circumstances Chrome is
    not able to read it. We missed that `ui::IsScreenCaptureAllowed()`
    exists. Move to use that now.
    
    As a time saving measure we didn't include a comment for
    `PurgeStaleScreenCapturePermission()`. Added a comment.
    
    These changes are not an emergency and do not need to instigate a
    respin. However if there are any planed respins, these changes would be
    an improvement for anyone who has yet to update to M99.0.4484.83.
    Getting this merged into the relevant branches may be useful.
    
    Bug: 1307502
    
    Change-Id: Ic7eb705cf3761be18173f5a2033678401b967517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538881
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Tom Burgin <bur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984463}

--- chrome/browser/mac/purge_stale_screen_capture_permission.h
+++ chrome/browser/mac/purge_stale_screen_capture_permission.h
@@ -7,6 +7,25 @@
 
 namespace chrome {
 
+// Purge stale TCC screen capture permission for the bundle id of the running
+// process, if applicable.
+//
+// A stale permission can exist when a previous version of an app's code signing
+// designated requirement fails to validate the currently running app. The
+// designated requirement active at the time of permission approval is stored in
+// a macOS controlled database on disk. If this cached designated requirement is
+// unable to validate the current running version of the app then screen capture
+// permission will be denied. See https://crbug.com/1307502 for more details.
+//
+// This function only affects branded builds running on macOS 10.15+; macOS
+// began supervising screen capture in macOS 10.15. If it is determined that the
+// running app does not have screen capture permission a command will be run to
+// purge any active screen capture permission. This will clear out a permission
+// with a stale designated requirement while leaving systems with a valid
+// permission untouched. A successful purge will only happen once. Subsequent
+// calls to this function will be a NOP. If there is a failure while attempting
+// to purge, subsequent calls to this function will attempt the purge up to a
+// maximum of 3 attempts.
 void PurgeStaleScreenCapturePermission();
 
 }  // namespace chrome
--- chrome/browser/mac/purge_stale_screen_capture_permission.mm
+++ chrome/browser/mac/purge_stale_screen_capture_permission.mm
@@ -4,86 +4,23 @@
 
 #include "chrome/browser/mac/purge_stale_screen_capture_permission.h"
 
-#include <CoreFoundation/CoreFoundation.h>
 #import <Foundation/Foundation.h>
-#include <Security/Security.h>
 
 #include <string>
+#include <vector>
 
-#include "base/files/file_path.h"
-#include "base/logging.h"
 #include "base/mac/bundle_locations.h"
-#include "base/mac/foundation_util.h"
-#include "base/mac/mac_logging.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
-#include "base/strings/string_util.h"
-#include "base/strings/sys_string_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/branding_buildflags.h"
-#include "sql/database.h"
-#include "sql/statement.h"
+#include "ui/base/cocoa/permissions_utils.h"
 
 namespace chrome {
 
 namespace {
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-
-bool RequirementForBundleIDNeedsReset(NSString* bundle_id) {
-  base::FilePath local_application_support_path;
-  if (!base::mac::GetLocalDirectory(NSApplicationSupportDirectory,
-                                    &local_application_support_path)) {
-    return true;
-  }
-  base::FilePath local_tcc_db_path(
-      local_application_support_path.Append("com.apple.TCC").Append("TCC.db"));
-  sql::DatabaseOptions options;
-  options.exclusive_locking = false;
-  sql::Database tcc_db = sql::Database(options);
-
-  if (!tcc_db.Open(local_tcc_db_path)) {
-    // On macOS 10.15 and macOS 11 this open is expected to fail due to SIP.
-    return true;
-  }
-  sql::Statement s(tcc_db.GetUniqueStatement(
-      "SELECT csreq FROM access WHERE client_type=0 AND client=? AND "
-      "service='kTCCServiceScreenCapture'"));
-  s.BindString(0, bundle_id.UTF8String);
-
-  while (s.Step()) {
-    base::span<const uint8_t> csreq_blob = s.ColumnBlob(0);
-    if (csreq_blob.empty()) {
-      return true;
-    }
-
-    base::ScopedCFTypeRef<CFDataRef> csreq_data(
-        CFDataCreate(nullptr, csreq_blob.data(), csreq_blob.size()));
-    base::ScopedCFTypeRef<SecRequirementRef> requirement;
-    OSStatus status = SecRequirementCreateWithData(
-        csreq_data, kSecCSDefaultFlags, requirement.InitializeInto());
-    if (status != errSecSuccess) {
-      OSSTATUS_LOG(ERROR, status) << "SecRequirementCreateWithData";
-      return true;
-    }
-    base::ScopedCFTypeRef<CFStringRef> requirement_string;
-    status = SecRequirementCopyString(requirement, kSecCSDefaultFlags,
-                                      requirement_string.InitializeInto());
-    if (status != errSecSuccess) {
-      OSSTATUS_LOG(ERROR, status) << "SecRequirementCopyString";
-      return true;
-    }
-
-    static constexpr char kCurrentRequirementTail[] =
-        " and certificate leaf[subject.OU] = EQHXZ8M8AV";
-    if (!base::EndsWith(base::SysCFStringRefToUTF8(requirement_string),
-                        kCurrentRequirementTail)) {
-      return true;
-    }
-  }
-
-  return !s.Succeeded();
-}
 
 bool ResetTCCScreenCaptureForBundleID(NSString* bundle_id) {
   if (!bundle_id.length) {
@@ -99,21 +36,26 @@ bool ResetTCCScreenCaptureForBundleID(NSString* bundle_id) {
 }
 
 bool AttemptPurgeStaleScreenCapturePermission() {
-  NSString* bundle_identifier = base::mac::MainBundle().bundleIdentifier;
-  if (RequirementForBundleIDNeedsReset(bundle_identifier)) {
-    // Paranoia about the exec failing for some reason. Retry once for good
-    // measure.
-    for (int i = 0; i < 2; ++i) {
-      if (ResetTCCScreenCaptureForBundleID(bundle_identifier)) {
-        // The stale record has been purged.
-        return true;
-      }
-    }
-    return false;
+  if (ui::IsScreenCaptureAllowed()) {
+    // We have access, no stale records exist.
+    return true;
   }
 
-  // The requirement is valid or doesn't exist.
-  return true;
+  // We don't have access. This could be because a stale record exists or there
+  // are no records. Reset the permission. This will clear out any potentially
+  // stale records. If there are no records a reset is harmless. The loop is
+  // paranoia about the exec failing for some reason. Retry once for good
+  // measure.
+  NSString* bundle_identifier = base::mac::MainBundle().bundleIdentifier;
+  for (int i = 0; i < 2; ++i) {
+    if (ResetTCCScreenCaptureForBundleID(bundle_identifier)) {
+      // The stale record has been purged.
+      return true;
+    }
+  }
+
+  // A stale record could still exist. Let the caller know.
+  return false;
 }
 
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
