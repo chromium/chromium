944defc49233838d14ce6e0b66e629aa0133f06d
Pu Shi <pushi@google.com>
2022-03-23 21:12:19 +0000

    [Nearby] Enable Nearby Share visibility reminder.
    Set the feature flag of Nearby Share visibility reminder to enabled
    by default.
    
    TEST = manually tested with 15 seconds timer delay and worked
    normally.
    
    Bug: 1265562
    Change-Id: I22a8f1560275fdc2fe282cc0a11ff6b11ea7e178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547197
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#984525}

--- chrome/browser/nearby_sharing/common/nearby_share_features.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_features.cc
@@ -43,7 +43,7 @@ const base::Feature kNearbySharingSelfShare{"NearbySharingSelfShare",
 // Enables notification to reminde users of their visibility selections.
 const base::Feature kNearbySharingVisibilityReminder(
     "NearbyShareVisibilityReminder",
-    base::FEATURE_DISABLED_BY_DEFAULT);
+    base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables use of WebRTC in Nearby Share.
 const base::Feature kNearbySharingWebRtc{"NearbySharingWebRtc",
