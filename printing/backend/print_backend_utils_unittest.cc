aa39dc77cc00f6094cb89235ae3789b8dbc6a59b
Daniel Hosseinian <dhoss@chromium.org>
2022-03-24 16:57:52 +0000

    [printing] Add test cases for ParsePaper()
    Test some additional media sizes defined by CUPS to increase confidence
    in code paths and to avoid future regressions. These new media sizes
    introduce variety in the dimension values and the media names.
    
    Change-Id: I5e0461374ad1f16752ceda64805762353e027823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547097
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984891}

--- printing/backend/print_backend_utils_unittest.cc
+++ printing/backend/print_backend_utils_unittest.cc
@@ -10,18 +10,55 @@
 
 namespace printing {
 
-TEST(PrintBackendUtilsTest, ParsePaper) {
-  PrinterSemanticCapsAndDefaults::Paper paper_mm =
-      ParsePaper("iso_a4_210x297mm");
-  EXPECT_EQ(gfx::Size(210000, 297000), paper_mm.size_um);
-  EXPECT_EQ("iso_a4_210x297mm", paper_mm.vendor_id);
-  EXPECT_EQ("iso a4", paper_mm.display_name);
+TEST(PrintBackendUtilsTest, ParsePaperA4) {
+  PrinterSemanticCapsAndDefaults::Paper paper = ParsePaper("iso_a4_210x297mm");
+  EXPECT_EQ(gfx::Size(210000, 297000), paper.size_um);
+  EXPECT_EQ("iso_a4_210x297mm", paper.vendor_id);
+  EXPECT_EQ("iso a4", paper.display_name);
+}
 
-  PrinterSemanticCapsAndDefaults::Paper paper_in =
+TEST(PrintBackendUtilsTest, ParsePaperNaLetter) {
+  PrinterSemanticCapsAndDefaults::Paper paper =
       ParsePaper("na_letter_8.5x11in");
-  EXPECT_EQ(gfx::Size(215900, 279400), paper_in.size_um);
-  EXPECT_EQ("na_letter_8.5x11in", paper_in.vendor_id);
-  EXPECT_EQ("na letter", paper_in.display_name);
+  EXPECT_EQ(gfx::Size(215900, 279400), paper.size_um);
+  EXPECT_EQ("na_letter_8.5x11in", paper.vendor_id);
+  EXPECT_EQ("na letter", paper.display_name);
+}
+
+TEST(PrintBackendUtilsTest, ParsePaperNaIndex4x6) {
+  // Note that "na_index-4x6_4x6in" has a dimension within the media name. Test
+  // that parsing is not affected.
+  PrinterSemanticCapsAndDefaults::Paper paper =
+      ParsePaper("na_index-4x6_4x6in");
+  EXPECT_EQ(gfx::Size(101600, 152400), paper.size_um);
+  EXPECT_EQ("na_index-4x6_4x6in", paper.vendor_id);
+  EXPECT_EQ("na index-4x6", paper.display_name);
+}
+
+TEST(PrintBackendUtilsTest, ParsePaperNaNumber10) {
+  // Test that a paper size with a fractional dimension is not affected by
+  // rounding errors.
+  PrinterSemanticCapsAndDefaults::Paper paper =
+      ParsePaper("na_number-10_4.125x9.5in");
+  EXPECT_EQ(gfx::Size(104775, 241300), paper.size_um);
+  EXPECT_EQ("na_number-10_4.125x9.5in", paper.vendor_id);
+  EXPECT_EQ("na number-10", paper.display_name);
+}
+
+TEST(PrintBackendUtilsTest, ParsePaperBadUnit) {
+  PrinterSemanticCapsAndDefaults::Paper paper_bad =
+      ParsePaper("bad_unit_666x666bad");
+  EXPECT_TRUE(paper_bad.size_um.IsEmpty());
+  EXPECT_EQ("bad_unit_666x666bad", paper_bad.vendor_id);
+  EXPECT_EQ("bad unit", paper_bad.display_name);
+}
+
+TEST(PrintBackendUtilsTest, ParsePaperBadOneDimension) {
+  PrinterSemanticCapsAndDefaults::Paper paper_bad =
+      ParsePaper("bad_one_dimension_666mm");
+  EXPECT_TRUE(paper_bad.size_um.IsEmpty());
+  EXPECT_EQ("bad_one_dimension_666mm", paper_bad.vendor_id);
+  EXPECT_EQ("bad one dimension", paper_bad.display_name);
 }
 
 }  // namespace printing
