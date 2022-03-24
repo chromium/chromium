426f247d06421d426efc692b89fa69c982bc9437
Chris Lu <thegreenfrog@chromium.org>
2022-03-24 15:10:00 +0000

    [ios] Turn on SingleCellContentSuggestions by defalt
    Change-Id: I27f8aa333b7631b78b64df2d3d99a3ffeb0b7aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546462
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984830}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
@@ -17,7 +17,7 @@ const base::Feature kSingleNtp{"SingleNTP", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Feature disabled by default.
 const base::Feature kSingleCellContentSuggestions{
-    "SingleCellContentSuggestions", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SingleCellContentSuggestions", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Feature disabled by default.
 const base::Feature kContentSuggestionsHeaderMigration{
