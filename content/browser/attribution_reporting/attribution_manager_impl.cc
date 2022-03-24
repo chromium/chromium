b1ad9e3211c3992df820f3bc62a4d05f8009c48e
Nan Lin <linnan@chromium.org>
2022-03-24 22:12:26 +0000

    [attribution_reporting] Add metrics for create aggregatable report status
    Add metrics to measure the status of aggregatable report creation in
    storage layer. Also updated new enum labels for
    "ConversionStorageCreateReportStatus".
    
    (cherry picked from commit 854af5bd98039bd83441adce87b2aceee27be88c)
    
    Bug: 1308832
    Change-Id: I7429caa24ef2d02655d69ecda93435a6766dab36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533159
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#984509}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550817
    Auto-Submit: Nan Lin <linnan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4951@{#125}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- content/browser/attribution_reporting/attribution_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_manager_impl.cc
@@ -102,8 +102,12 @@ bool IsOriginSessionOnly(
   return false;
 }
 
-void RecordCreateReportStatus(AttributionTrigger::EventLevelResult status) {
-  base::UmaHistogramEnumeration("Conversions.CreateReportStatus", status);
+void RecordCreateReportStatus(CreateReportResult result) {
+  base::UmaHistogramEnumeration("Conversions.CreateReportStatus",
+                                result.event_level_status());
+  base::UmaHistogramEnumeration(
+      "Conversions.AggregatableReport.CreateReportStatus",
+      result.aggregatable_status());
 }
 
 ConversionReportSendOutcome ConvertToConversionReportSendOutcome(
@@ -486,9 +490,7 @@ void AttributionManagerImpl::ProcessNextEvent(bool is_debug_cookie_set) {
 }
 
 void AttributionManagerImpl::OnReportStored(CreateReportResult result) {
-  RecordCreateReportStatus(result.event_level_status());
-
-  // TODO(crbug.com/1285319): Log metrics for aggregatable reports.
+  RecordCreateReportStatus(result);
 
   if (std::vector<AttributionReport>& new_reports = result.new_reports();
       !new_reports.empty()) {
