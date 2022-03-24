61176b77895fc4b2cda2d0e77e25f1090c22e7e6
Takashi Toyoshima <toyoshim@chromium.org>
2022-03-24 00:48:28 +0000

    PageLoadMetrics: Add PageLoadTrackerTest
    Bug: 1301880
    Change-Id: I65fbe30fe3d5266fa4146b0946043d7568256ec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3546196
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#984629}

--- components/page_load_metrics/browser/observers/early_hints_page_load_metrics_observer_unittest.cc
+++ components/page_load_metrics/browser/observers/early_hints_page_load_metrics_observer_unittest.cc
@@ -55,7 +55,6 @@ class EarlyHintsPageLoadMetricsObserverTest
   }
 };
 
-// TODO(https://crbug.com/1301880): Add tests for other PageTypes.
 TEST_F(EarlyHintsPageLoadMetricsObserverTest, PageType) {
   NavigateAndCommit(GURL(kTestUrl));
   PopulateTimingForHistograms();
