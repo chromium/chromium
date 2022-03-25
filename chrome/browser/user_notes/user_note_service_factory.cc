2bc64aaa5cdf4637adca610655dcfd4c3f1b4801
Guillaume Jenkins <gujen@google.com>
2022-03-25 20:29:54 +0000

    [Notes] Add UserNoteServiceFactory
    Creates the //chrome/browser/user_notes directory for the chrome layer
    logic of the User Notes feature.
    
    Also creates the BrowserContextKeyedFactory for the UserNoteService.
    
    Bug: 1303886
    Change-Id: Idafff2d9cd208d35426c75a210333dbf2fdf9f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3547338
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#985465}

---
+++ chrome/browser/user_notes/user_note_service_factory.cc
@@ -0,0 +1,53 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/user_notes/user_note_service_factory.h"
+
+#include <utility>
+
+#include "base/memory/singleton.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/user_notes/browser/user_note_service.h"
+#include "components/user_notes/user_notes_features.h"
+#include "content/public/browser/browser_context.h"
+
+namespace user_notes {
+
+// static
+UserNoteService* UserNoteServiceFactory::GetForContext(
+    content::BrowserContext* context) {
+  return static_cast<UserNoteService*>(
+      GetInstance()->GetServiceForBrowserContext(context,
+                                                 /*create=*/true));
+}
+
+// static
+UserNoteServiceFactory* UserNoteServiceFactory::GetInstance() {
+  return base::Singleton<UserNoteServiceFactory>::get();
+}
+
+UserNoteServiceFactory::UserNoteServiceFactory()
+    : BrowserContextKeyedServiceFactory(
+          "UserNoteService",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+UserNoteServiceFactory::~UserNoteServiceFactory() = default;
+
+KeyedService* UserNoteServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  DCHECK(IsUserNotesEnabled());
+  return new UserNoteService();
+}
+
+content::BrowserContext* UserNoteServiceFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  // For now, the feature is not supported in Incognito mode.
+  if (context->IsOffTheRecord()) {
+    return nullptr;
+  }
+
+  return context;
+}
+
+}  // namespace user_notes
