25178e4009558060ece7a14f2c83ab4c7d94ef34
Cheick Cisse <cheickcisse@google.com>
2022-03-25 14:01:18 +0000

    [Notes] Add the user note UI coordinator
    This CL implements UserNotesUI to communicate with UserNoteService and implements TabStripModelObserver to keep track of the active tab.
    UserNoteUICoordinator will be responsible for displaying user notes in the side panel upon user request.
    
    Bug: 1308743
    Change-Id: Ie4a625ab652538b5f79e4b0c825660d301bd8cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3542071
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#985278}

---
+++ chrome/browser/ui/views/side_panel/user_note_ui_coordinator.cc
@@ -0,0 +1,46 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/side_panel/user_note_ui_coordinator.h"
+
+UserNoteUICoordinator::UserNoteUICoordinator(Browser* browser)
+    : browser_(browser) {
+  browser_->tab_strip_model()->AddObserver(this);
+}
+
+UserNoteUICoordinator::~UserNoteUICoordinator() {
+  browser_->tab_strip_model()->RemoveObserver(this);
+}
+
+void UserNoteUICoordinator::FocusNote(const std::string& guid) {
+  // TODO(cheickcisse): Implement FocusNote, which will be called by
+  // UserNoteService to inform, inform the user note side panel to scroll the
+  // corresponding note into view in the side panel.
+}
+
+void UserNoteUICoordinator::StartNoteCreation(const std::string& guid,
+                                              gfx::Rect bounds) {
+  // TODO(cheickcisse): Implement StartNoteCreation, which will be called by
+  // UserNoteService to add a new note in the side panel. The new note entry row
+  // will be position at y relative to existing notes in the side panel.
+}
+
+void UserNoteUICoordinator::Invalidate() {
+  // TODO(cheickcisse): Implement Invalidate, which will be called by
+  // UserNoteService or by OnTabStripModelChanged to fetch the latest list of
+  // notes to display from UserNotePageData associated with the active tab.
+}
+
+void UserNoteUICoordinator::Show() {
+  // TODO(cheickcisse): Implement Show, which will be called by UserNoteService
+  // to open notes in the side panel.
+}
+
+void UserNoteUICoordinator::OnTabStripModelChanged(
+    TabStripModel* tab_strip_model,
+    const TabStripModelChange& change,
+    const TabStripSelectionChange& selection) {
+  // TODO(cheickcisse): Implement OnTabStripModelChanged, which should call
+  // Invalidate() to poll the latest list of notes to display.
+}
---
+++ chrome/browser/ui/views/side_panel/user_note_ui_coordinator.h
@@ -0,0 +1,37 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_USER_NOTE_UI_COORDINATOR_H_
+#define CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_USER_NOTE_UI_COORDINATOR_H_
+
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "components/user_notes/interfaces/user_notes_ui.h"
+
+class UserNoteUICoordinator : public user_notes::UserNotesUI,
+                              public TabStripModelObserver {
+ public:
+  explicit UserNoteUICoordinator(Browser* browser);
+
+  UserNoteUICoordinator(const UserNoteUICoordinator&) = delete;
+  UserNoteUICoordinator& operator=(const UserNoteUICoordinator&) = delete;
+  ~UserNoteUICoordinator() override;
+
+  // UserNoteUI overrides
+  void FocusNote(const std::string& guid) override;
+  void StartNoteCreation(const std::string& guid, gfx::Rect bounds) override;
+  void Invalidate() override;
+  void Show() override;
+
+  // TabStripModelObserver overrides
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
+
+ private:
+  Browser* browser_;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_USER_NOTE_UI_COORDINATOR_H_
