51e3254ec0e651670493c861acd9219f63f86d29
Kyle Williams <kdgwill@google.com>
2022-03-25 17:57:36 +0000

    [M96 CfM Ambassador]Expose is_cfm build flag via chromeInfoPrivate API.
    ./out/Default/browser_tests \
    --gtest_filter=ChromeOSInfoPrivateTest.TestGetIsMeetDevice'
    
    (cherry picked from commit c567c2da80d223b0cbdc91639f368fe76c4621a1)
    
    Bug: 1304020
    Test: 'autoninja -C out/Default/ chrome/test:browser_tests && xvfb-run \
    Change-Id: I4df73fc2dc5d6839f7efbe795b5f82b528278311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507775
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Auto-Submit: Kyle Williams <kdgwill@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#984930}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3550459
    Cr-Commit-Position: refs/branch-heads/4664_110@{#5}
    Cr-Branched-From: d5ef0e8214bc14c9b5bbf69a1515e431394c62a6-refs/branch-heads/4664@{#1283}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- third_party/closure_compiler/externs/chromeos_info_private.js
+++ third_party/closure_compiler/externs/chromeos_info_private.js
@@ -99,6 +99,7 @@ chrome.chromeosInfoPrivate.AssistantStatus = {
  *   customizationId: (string|undefined),
  *   homeProvider: (string|undefined),
  *   hwid: (string|undefined),
+ *   isMeetDevice: (boolean|undefined),
  *   initialLocale: (string|undefined),
  *   isOwner: (boolean|undefined),
  *   sessionType: (!chrome.chromeosInfoPrivate.SessionType|undefined),
