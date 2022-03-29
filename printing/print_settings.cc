e8df231b5539f6a068f83a0e6f7cabe3aada7414
Lei Zhang <thestig@chromium.org>
2022-03-29 00:03:09 +0000

    Simplify the copy ctor for PrintSettings.
    Instead of copying field by field, just use operator=, which is
    essentially the same thing.
    
    Change-Id: I873c2f87941a10dc712ba5bdc558466bd3282434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3552322
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986229}

--- printing/print_settings.cc
+++ printing/print_settings.cc
@@ -240,40 +240,7 @@ PrintSettings::PrintSettings() {
 }
 
 PrintSettings::PrintSettings(const PrintSettings& settings) {
-  ranges_ = settings.ranges_;
-  selection_only_ = settings.selection_only_;
-  margin_type_ = settings.margin_type_;
-  title_ = settings.title_;
-  url_ = settings.url_;
-  display_header_footer_ = settings.display_header_footer_;
-  should_print_backgrounds_ = settings.should_print_backgrounds_;
-  collate_ = settings.collate_;
-  color_ = settings.color_;
-  copies_ = settings.copies_;
-  duplex_mode_ = settings.duplex_mode_;
-  device_name_ = settings.device_name_;
-  requested_media_ = settings.requested_media_;
-  page_setup_device_units_ = settings.page_setup_device_units_;
-  dpi_ = settings.dpi_;
-  scale_factor_ = settings.scale_factor_;
-  rasterize_pdf_ = settings.rasterize_pdf_;
-  rasterize_pdf_dpi_ = settings.rasterize_pdf_dpi_;
-  landscape_ = settings.landscape_;
-  supports_alpha_blend_ = settings.supports_alpha_blend_;
-#if BUILDFLAG(IS_WIN)
-  printer_language_type_ = settings.printer_language_type_;
-#endif
-  is_modifiable_ = settings.is_modifiable_;
-  pages_per_sheet_ = settings.pages_per_sheet_;
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-  for (const auto& item : settings.advanced_settings_)
-    advanced_settings_.emplace(item.first, item.second.Clone());
-#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
-#if BUILDFLAG(IS_CHROMEOS)
-  send_user_info_ = settings.send_user_info_;
-  username_ = settings.username_;
-  pin_value_ = settings.pin_value_;
-#endif  // BUILDFLAG(IS_CHROMEOS)
+  *this = settings;
 }
 
 PrintSettings& PrintSettings::operator=(const PrintSettings& settings) {
