34d84421d4ab91586e2a155b0fca09ce1ea73bd4
Dan McArdle <dmcardle@chromium.org>
2022-03-29 15:05:26 +0000

    Add simple validation tests for DohProviderEntry::GetList()
    Change-Id: Ib4509fc1c0f4831eaec46b712689ead171e551e6
    Fixed: 1305297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516235
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986495}

--- net/dns/public/doh_provider_entry_unittest.cc
+++ net/dns/public/doh_provider_entry_unittest.cc
@@ -4,6 +4,9 @@
 
 #include "net/dns/public/doh_provider_entry.h"
 
+#include <set>
+#include <string>
+
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest-death-test.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -14,6 +17,25 @@ namespace {
 TEST(DohProviderListTest, GetDohProviderList) {
   const DohProviderEntry::List& list = DohProviderEntry::GetList();
   EXPECT_FALSE(list.empty());
+}
+
+TEST(DohProviderListTest, ProviderNamesAreUnique) {
+  std::set<std::string> names;
+  for (const DohProviderEntry* entry : DohProviderEntry::GetList()) {
+    EXPECT_FALSE(entry->provider.empty());
+    auto [_, did_insert] = names.insert(entry->provider);
+    EXPECT_TRUE(did_insert);
+  }
+}
+
+TEST(DohProviderListTest, UiNamesAreUniqueOrEmpty) {
+  std::set<std::string> ui_names;
+  for (const DohProviderEntry* entry : DohProviderEntry::GetList()) {
+    if (entry->ui_name.empty())
+      continue;
+    auto [_, did_insert] = ui_names.insert(entry->ui_name);
+    EXPECT_TRUE(did_insert) << "UI name was not unique: " << entry->ui_name;
+  }
 }
 
 TEST(DohProviderListTest, NonEmptyDnsOverTlsHostnames) {
