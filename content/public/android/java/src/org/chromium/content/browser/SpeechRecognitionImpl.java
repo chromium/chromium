07c347e2a5faf1bdb0e4d48353d06f86da0e51e0
Evan Liu <evliu@google.com>
2022-03-29 23:26:43 +0000

    Switch the Android WebSpeech API to use Speech Services by Google app
    This CL switches the Android WebSpeech API over to using Speech Services
    by Google app for Android S onwards.
    
    Change-Id: Ia6b01279ec4983afcbe1ea038f3d97e4052cd79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3555398
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#986764}

--- content/public/android/java/src/org/chromium/content/browser/SpeechRecognitionImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/SpeechRecognitionImpl.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognitionService;
@@ -39,8 +40,9 @@ public class SpeechRecognitionImpl {
     private static final String TAG = "SpeechRecog";
 
     // Constants describing the speech recognition provider we depend on.
-    private static final String PROVIDER_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
-    private static final int PROVIDER_MIN_VERSION = 300207030;
+    private static final String AGSA_PACKAGE_NAME = "com.google.android.googlequicksearchbox";
+    private static final int AGSA_MIN_VERSION = 300207030;
+    private static final String SSBG_PACKAGE_NAME = "com.google.android.tts";
 
     // We track the recognition state to remember what events we need to send when recognition is
     // being aborted. Once Android's recognizer is cancelled, its listener won't yield any more
@@ -188,11 +190,27 @@ public class SpeechRecognitionImpl {
      * continuous recognition.
      */
     // TODO(crbug.com/635567): Fix this properly.
-    @SuppressLint("WrongConstant")
     public static boolean initialize() {
         Context context = ContextUtils.getApplicationContext();
         if (!SpeechRecognizer.isRecognitionAvailable(context)) return false;
 
+        sRecognitionProvider = createRecognitionProvider();
+        if (sRecognitionProvider == null) return false;
+        return true;
+    }
+
+    /** Returns null if there is no Google LLC provided RecognitionService available on device. */
+    private static ComponentName createRecognitionProvider() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return getComponent(SSBG_PACKAGE_NAME, -1);
+        } else {
+            return getComponent(AGSA_PACKAGE_NAME, AGSA_MIN_VERSION);
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private static ComponentName getComponent(String packageName, int packageMinVersion) {
+        Context context = ContextUtils.getApplicationContext();
         PackageManager pm = context.getPackageManager();
         Intent intent = new Intent(RecognitionService.SERVICE_INTERFACE);
         final List<ResolveInfo> list = pm.queryIntentServices(intent, PackageManager.GET_SERVICES);
@@ -200,20 +218,20 @@ public class SpeechRecognitionImpl {
         for (ResolveInfo resolve : list) {
             ServiceInfo service = resolve.serviceInfo;
 
-            if (!service.packageName.equals(PROVIDER_PACKAGE_NAME)) continue;
+            if (!service.packageName.equals(packageName)) continue;
 
-            if (PackageUtils.getPackageVersion(context, service.packageName)
-                    < PROVIDER_MIN_VERSION) {
-                continue;
+            // No need to verify package version if packageMinVersion != -1.
+            if (packageMinVersion != -1) {
+                if (PackageUtils.getPackageVersion(context, service.packageName)
+                        < packageMinVersion) {
+                    continue;
+                }
             }
 
-            sRecognitionProvider = new ComponentName(service.packageName, service.name);
-
-            return true;
+            return new ComponentName(service.packageName, service.name);
         }
 
-        // If we reach this point, we failed to find a suitable recognition provider.
-        return false;
+        return null;
     }
 
     private SpeechRecognitionImpl(long nativeSpeechRecognizerImplAndroid) {
