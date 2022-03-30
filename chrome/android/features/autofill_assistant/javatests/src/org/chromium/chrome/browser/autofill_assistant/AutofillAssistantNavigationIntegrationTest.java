402821aa6a5ce66a0e666f5fb854fb2e5fdb23f2
Brandon Wylie <wylieb@chromium.org>
2022-03-30 22:15:03 +0000

    [Sheriff] Disable failing #navigatingWithLocationBarShowsError
    Bug: 1311815
    Change-Id: Ia230dcc2004c3d31a0accc233e6312c833f97197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561197
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#987214}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantNavigationIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantNavigationIntegrationTest.java
@@ -32,6 +32,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.NavigateProto;
@@ -80,6 +81,7 @@ public class AutofillAssistantNavigationIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1311815")
     public void navigatingWithLocationBarShowsError() {
         ArrayList<ActionProto> list = new ArrayList<>();
         list.add(ActionProto.newBuilder()
