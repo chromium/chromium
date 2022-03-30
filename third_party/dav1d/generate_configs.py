a614d39033593e84c5e94fc4b350fab75c66dae2
Dale Curtis <dalecurtis@chromium.org>
2022-03-30 00:28:33 +0000

    Fix generate_configs.py to use /winsysroot instead of LIB env var.
    Also switches to python3 and reformats the Python files using
    git cl format --python (PS#1 is just the reformatted files,
    PS#3 is python3 change and subsequent reformat, PS#4+ is actual
    changes).
    
    Fixed: 1308867
    Change-Id: Ibdb4c6ced2d834e89d70fe9b2be788776f1cdaf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3558030
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#986788}

--- third_party/dav1d/generate_configs.py
+++ third_party/dav1d/generate_configs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -32,197 +32,197 @@ WINDOWS_BUILD_ARGS = ['-Dc_winlibs=']
 
 
 def PrintAndCheckCall(argv, *args, **kwargs):
-  print('\n-------------------------------------------------\nRunning %s' %
-        ' '.join(argv))
-  c = subprocess.check_call(argv, *args, **kwargs)
+    print('\n-------------------------------------------------\nRunning %s' %
+          ' '.join(argv))
+    c = subprocess.check_call(argv, *args, **kwargs)
 
 
 def RewriteFile(path, search_replace):
-  with open(path) as f:
-    contents = f.read()
-  with open(path, 'w') as f:
-    for search, replace in search_replace:
-      contents = re.sub(search, replace, contents)
+    with open(path) as f:
+        contents = f.read()
+    with open(path, 'w') as f:
+        for search, replace in search_replace:
+            contents = re.sub(search, replace, contents)
 
-    # Cleanup trailing newlines.
-    f.write(contents.strip() + '\n')
+        # Cleanup trailing newlines.
+        f.write(contents.strip() + '\n')
 
 
 def SetupWindowsCrossCompileToolchain(target_arch):
-  # First retrieve various MSVC and Windows SDK paths.
-  output = subprocess.check_output([
-      os.path.join(CHROMIUM_ROOT_DIR, 'build', 'vs_toolchain.py'),
-      'get_toolchain_dir'
-  ], universal_newlines=True)
+    # First retrieve various MSVC and Windows SDK paths.
+    output = subprocess.check_output([
+        'python3',
+        os.path.join(CHROMIUM_ROOT_DIR, 'build', 'vs_toolchain.py'),
+        'get_toolchain_dir'
+    ],
+                                     universal_newlines=True)
 
-  # Turn this into a dictionary.
-  win_dirs = gn_helpers.FromGNArgs(output)
+    # Turn this into a dictionary.
+    win_dirs = gn_helpers.FromGNArgs(output)
 
-  # Use those paths with a second script which will tell us the proper include
-  # and lib paths to specify for cflags and ldflags respectively.
-  output = subprocess.check_output([
-      'python',
-      os.path.join(CHROMIUM_ROOT_DIR, 'build', 'toolchain', 'win',
-                   'setup_toolchain.py'), win_dirs['vs_path'],
-      win_dirs['sdk_path'], win_dirs['runtime_dirs'], 'win', target_arch, 'none'
-  ], universal_newlines=True)
+    # Use those paths with a second script which will tell us the proper include
+    # and lib paths to specify for cflags and ldflags respectively.
+    output = subprocess.check_output([
+        'python3',
+        os.path.join(CHROMIUM_ROOT_DIR, 'build', 'toolchain', 'win',
+                     'setup_toolchain.py'), win_dirs['vs_path'],
+        win_dirs['sdk_path'], win_dirs['runtime_dirs'], 'win', target_arch,
+        'none'
+    ],
+                                     universal_newlines=True)
 
-  flags = gn_helpers.FromGNArgs(output)
-  cwd = os.getcwd()
+    flags = gn_helpers.FromGNArgs(output)
+    cwd = os.getcwd()
 
-  target_env = os.environ
+    target_env = os.environ
 
-  # Each path is of the form:
-  # "/I../depot_tools/win_toolchain/vs_files/20d5f2553f/Windows Kits/10/Include/10.0.19041.0/winrt"
-  #
-  # Since there's a space in the include path, inputs are quoted in |flags|, we
-  # can helpfully use shlex to split on spaces while preserving quoted strings.
-  include_paths = []
-  for include_path in shlex.split(flags['include_flags_I']):
-    # Apparently setup_toolchain prefers relative include paths, which
-    # may work for chrome, but it does not work for dav1d, so let's make
-    # them asbolute again.
-    include_path = os.path.abspath(os.path.join(cwd, include_path[2:]))
-    include_paths.append(include_path)
+    # Each path is of the form:
+    # "/I../depot_tools/win_toolchain/vs_files/20d5f2553f/Windows Kits/10/Include/10.0.19041.0/winrt"
+    #
+    # Since there's a space in the include path, inputs are quoted in |flags|, we
+    # can helpfully use shlex to split on spaces while preserving quoted strings.
+    include_paths = []
+    for include_path in shlex.split(flags['include_flags_I']):
+        # Apparently setup_toolchain prefers relative include paths, which
+        # may work for chrome, but it does not work for dav1d, so let's make
+        # them asbolute again.
+        include_path = os.path.abspath(os.path.join(cwd, include_path[2:]))
+        include_paths.append(include_path)
 
-  # TODO(dalecurtis): Why isn't the ucrt path printed?
-  flags['vc_lib_ucrt_path'] = flags['vc_lib_um_path'].replace('/um/', '/ucrt/')
+    SYSROOT_PREFIX = '/winsysroot:'
+    for k in flags:
+        if SYSROOT_PREFIX in flags[k]:
+            target_env['WINSYSROOT'] = os.path.abspath(
+                os.path.join(cwd, flags[k][len(SYSROOT_PREFIX):]))
+            break
 
-  # Unlike the cflags, the lib include paths are each in a separate variable.
-  lib_paths = []
-  for k in flags:
-    # libpath_flags is like cflags. Since it is also redundant, skip it.
-    if 'lib' in k and k != 'libpath_flags':
-      lib_paths.append(flags[k])
-
-  target_env = os.environ
-  target_env['INCLUDE'] = ';'.join(include_paths)
-  target_env['LIB'] = ';'.join(lib_paths)
-  return target_env
+    target_env = os.environ
+    target_env['INCLUDE'] = ';'.join(include_paths)
+    return target_env
 
 
 def CopyConfigs(src_dir, dest_dir):
-  if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-  shutil.copy(os.path.join(src_dir, 'config.h'), dest_dir)
+    shutil.copy(os.path.join(src_dir, 'config.h'), dest_dir)
 
-  # The .asm file will not be present for all configurations.
-  asm_file = os.path.join(src_dir, 'config.asm')
-  if os.path.exists(asm_file):
-    shutil.copy(asm_file, dest_dir)
+    # The .asm file will not be present for all configurations.
+    asm_file = os.path.join(src_dir, 'config.asm')
+    if os.path.exists(asm_file):
+        shutil.copy(asm_file, dest_dir)
 
 
 def GenerateConfig(config_dir, env, special_args=[]):
-  temp_dir = tempfile.mkdtemp()
-  PrintAndCheckCall(
-      MESON + DEFAULT_BUILD_ARGS + special_args + [temp_dir],
-      cwd='libdav1d',
-      env=env)
+    temp_dir = tempfile.mkdtemp()
+    PrintAndCheckCall(MESON + DEFAULT_BUILD_ARGS + special_args + [temp_dir],
+                      cwd='libdav1d',
+                      env=env)
 
-  RewriteFile(
-      os.path.join(temp_dir, 'config.h'),
-      [
-          # We don't want non-visible log strings polluting the official binary.
-          (r'(#define CONFIG_LOG .*)',
-           r'// \1 -- Logging is controlled by Chromium'),
-
-          # The Chromium build system already defines this.
-          (r'(#define _WIN32_WINNT .*)',
-           r'// \1 -- Windows version is controlled by Chromium'),
-
-          # Clang LTO doesn't respect stack alignment, so we must use the
-          # platform's default stack alignment; https://crbug.com/928743.
-          (r'(#define STACK_ALIGNMENT \d{1,2})',
-           r'// \1 -- Stack alignment is controlled by Chromium'),
-
-          # Android doesn't have pthread_getaffinity_np.
-          (r'(#define HAVE_PTHREAD_GETAFFINITY_NP \d{1,2})',
-           r'// \1 -- Controlled by Chomium'),
-      ])
-
-  config_asm_path = os.path.join(temp_dir, 'config.asm')
-  if (os.path.exists(config_asm_path)):
     RewriteFile(
-        config_asm_path,
-        [(r'(%define STACK_ALIGNMENT \d{1,2})',
-          r'; \1 -- Stack alignment is controlled by Chromium')])
+        os.path.join(temp_dir, 'config.h'),
+        [
+            # We don't want non-visible log strings polluting the official binary.
+            (r'(#define CONFIG_LOG .*)',
+             r'// \1 -- Logging is controlled by Chromium'),
 
-  CopyConfigs(temp_dir, config_dir)
+            # The Chromium build system already defines this.
+            (r'(#define _WIN32_WINNT .*)',
+             r'// \1 -- Windows version is controlled by Chromium'),
 
-  shutil.rmtree(temp_dir)
+            # Clang LTO doesn't respect stack alignment, so we must use the
+            # platform's default stack alignment; https://crbug.com/928743.
+            (r'(#define STACK_ALIGNMENT \d{1,2})',
+             r'// \1 -- Stack alignment is controlled by Chromium'),
+
+            # Android doesn't have pthread_getaffinity_np.
+            (r'(#define HAVE_PTHREAD_GETAFFINITY_NP \d{1,2})',
+             r'// \1 -- Controlled by Chomium'),
+        ])
+
+    config_asm_path = os.path.join(temp_dir, 'config.asm')
+    if (os.path.exists(config_asm_path)):
+        RewriteFile(config_asm_path,
+                    [(r'(%define STACK_ALIGNMENT \d{1,2})',
+                      r'; \1 -- Stack alignment is controlled by Chromium')])
+
+    CopyConfigs(temp_dir, config_dir)
+
+    shutil.rmtree(temp_dir)
 
 
 def GenerateWindowsArm64Config(src_dir):
-  win_arm64_dir = 'config/win/arm64'
-  if not os.path.exists(win_arm64_dir):
-    os.makedirs(win_arm64_dir)
+    win_arm64_dir = 'config/win/arm64'
+    if not os.path.exists(win_arm64_dir):
+        os.makedirs(win_arm64_dir)
 
-  shutil.copy(os.path.join(src_dir, 'config.h'), win_arm64_dir)
+    shutil.copy(os.path.join(src_dir, 'config.h'), win_arm64_dir)
 
-  # Flip flags such that it looks like an arm64 configuration.
-  RewriteFile(
-      os.path.join(win_arm64_dir, 'config.h'),
-      [(r'#define ARCH_X86 1', r'#define ARCH_X86 0'),
-       (r'#define ARCH_X86_64 1', r'#define ARCH_X86_64 0'),
-       (r'#define ARCH_AARCH64 0', r'#define ARCH_AARCH64 1')])
+    # Flip flags such that it looks like an arm64 configuration.
+    RewriteFile(os.path.join(win_arm64_dir, 'config.h'),
+                [(r'#define ARCH_X86 1', r'#define ARCH_X86 0'),
+                 (r'#define ARCH_X86_64 1', r'#define ARCH_X86_64 0'),
+                 (r'#define ARCH_AARCH64 0', r'#define ARCH_AARCH64 1')])
 
 
 def CopyVersions(src_dir, dest_dir):
-  if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-  shutil.copy(os.path.join(src_dir, 'include', 'dav1d', 'version.h'), dest_dir)
-  shutil.copy(os.path.join(src_dir, 'include', 'vcs_version.h'), dest_dir)
+    shutil.copy(os.path.join(src_dir, 'include', 'dav1d', 'version.h'),
+                dest_dir)
+    shutil.copy(os.path.join(src_dir, 'include', 'vcs_version.h'), dest_dir)
 
 
 def GenerateVersion(version_dir, env):
-  temp_dir = tempfile.mkdtemp()
-  PrintAndCheckCall(
-      MESON + DEFAULT_BUILD_ARGS + [temp_dir],
-      cwd='libdav1d',
-      env=env)
-  PrintAndCheckCall(
-      ['ninja', '-C', temp_dir, 'include/vcs_version.h'],
-      cwd='libdav1d',
-      env=env)
+    temp_dir = tempfile.mkdtemp()
+    PrintAndCheckCall(MESON + DEFAULT_BUILD_ARGS + [temp_dir],
+                      cwd='libdav1d',
+                      env=env)
+    PrintAndCheckCall(['ninja', '-C', temp_dir, 'include/vcs_version.h'],
+                      cwd='libdav1d',
+                      env=env)
 
-  CopyVersions(temp_dir, version_dir)
+    CopyVersions(temp_dir, version_dir)
 
-  shutil.rmtree(temp_dir)
+    shutil.rmtree(temp_dir)
 
 
 def main():
-  linux_env = os.environ
-  linux_env['CC'] = 'clang'
+    linux_env = os.environ
+    linux_env['CC'] = 'clang'
 
-  GenerateConfig('config/linux/x64', linux_env)
-  GenerateConfig('config/linux-noasm/x64', linux_env, ['-Denable_asm=false'])
+    GenerateConfig('config/linux/x64', linux_env)
+    GenerateConfig('config/linux-noasm/x64', linux_env, ['-Denable_asm=false'])
 
-  GenerateConfig('config/linux/x86', linux_env,
-                 ['--cross-file', '../crossfiles/linux32.crossfile'])
-  GenerateConfig('config/linux/arm', linux_env,
-                 ['--cross-file', '../crossfiles/arm.crossfile'])
-  GenerateConfig('config/linux/arm64', linux_env,
-                 ['--cross-file', '../crossfiles/arm64.crossfile'])
+    GenerateConfig('config/linux/x86', linux_env,
+                   ['--cross-file', '../crossfiles/linux32.crossfile'])
+    GenerateConfig('config/linux/arm', linux_env,
+                   ['--cross-file', '../crossfiles/arm.crossfile'])
+    GenerateConfig('config/linux/arm64', linux_env,
+                   ['--cross-file', '../crossfiles/arm64.crossfile'])
 
-  win_x86_env = SetupWindowsCrossCompileToolchain('x86')
-  GenerateConfig('config/win/x86', win_x86_env,
-                 ['--cross-file', '../crossfiles/win32.crossfile'] +
-                 WINDOWS_BUILD_ARGS)
+    win_x86_env = SetupWindowsCrossCompileToolchain('x86')
+    GenerateConfig('config/win/x86', win_x86_env,
+                   ['--cross-file', '../crossfiles/win32.crossfile'] + [
+                       '-Dc_args=-m32 -fuse-ld=lld /winsysroot ' +
+                       win_x86_env['WINSYSROOT']
+                   ] + WINDOWS_BUILD_ARGS)
 
-  win_x64_dir = 'config/win/x64'
-  win_x64_env = SetupWindowsCrossCompileToolchain('x64')
-  GenerateConfig(win_x64_dir, win_x64_env,
-                 ['--cross-file', '../crossfiles/win64.crossfile'] +
-                 WINDOWS_BUILD_ARGS)
+    win_x64_dir = 'config/win/x64'
+    win_x64_env = SetupWindowsCrossCompileToolchain('x64')
+    GenerateConfig(
+        win_x64_dir, win_x64_env,
+        ['--cross-file', '../crossfiles/win64.crossfile'] +
+        ['-Dc_args=-fuse-ld=lld /winsysroot ' + win_x64_env['WINSYSROOT']] +
+        WINDOWS_BUILD_ARGS)
 
-  # Sadly meson doesn't support arm64 + clang-cl, so we need to create the
-  # Windows arm64 config from the Windows x64 config.
-  GenerateWindowsArm64Config(win_x64_dir)
+    # Sadly meson doesn't support arm64 + clang-cl, so we need to create the
+    # Windows arm64 config from the Windows x64 config.
+    GenerateWindowsArm64Config(win_x64_dir)
 
-  GenerateVersion('version', linux_env)
+    GenerateVersion('version', linux_env)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- third_party/dav1d/generate_source.py
+++ third_party/dav1d/generate_source.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -29,56 +29,58 @@ _DAV1D_ENTRY_POINT_SOURCES = [
 
 
 def _Glob(pattern):
-  # Replace path separator. Needed when running on Windows.
-  return  [i.replace(os.sep, '/') for i in glob.glob(pattern)]
+    # Replace path separator. Needed when running on Windows.
+    return [i.replace(os.sep, '/') for i in glob.glob(pattern)]
 
 
 def _WriteArray(fd, var_name, array, filter_list=[], last_entry=False):
-  if len(array) == 0:
-    fd.write("var_name = []\n")
-    return
+    if len(array) == 0:
+        fd.write("var_name = []\n")
+        return
 
-  fd.write("%s = [\n" % var_name)
-  for item in sorted(array):
-    if item not in filter_list:
-      fd.write("  \"%s\",\n" % item)
-  fd.write("]\n")
-  if not last_entry:
-    fd.write("\n")
+    fd.write("%s = [\n" % var_name)
+    for item in sorted(array):
+        if item not in filter_list:
+            fd.write("  \"%s\",\n" % item)
+    fd.write("]\n")
+    if not last_entry:
+        fd.write("\n")
 
 
 def _WriteGn(fd):
-  fd.write(_COPYRIGHT)
-  _WriteArray(fd, "x86_asm_sources", _Glob("libdav1d/src/x86/*.asm"),
-              ["libdav1d/src/x86/filmgrain_common.asm"])
-  _WriteArray(fd, "x86_template_sources", _Glob("libdav1d/src/x86/*_tmpl.c"))
+    fd.write(_COPYRIGHT)
+    _WriteArray(fd, "x86_asm_sources", _Glob("libdav1d/src/x86/*.asm"),
+                ["libdav1d/src/x86/filmgrain_common.asm"])
+    _WriteArray(fd, "x86_template_sources", _Glob("libdav1d/src/x86/*_tmpl.c"))
 
-  _WriteArray(fd, "arm32_asm_sources", _Glob("libdav1d/src/arm/32/*.S"),
-              _Glob("libdav1d/src/arm/32/*_tmpl.S") +
-              ["libdav1d/src/arm/32/util.S"])
-  _WriteArray(fd, "arm64_asm_sources", _Glob("libdav1d/src/arm/64/*.S"),
-              _Glob("libdav1d/src/arm/64/*_tmpl.S") +
-              ["libdav1d/src/arm/64/util.S"])
-  _WriteArray(fd, "arm_template_sources", _Glob("libdav1d/src/arm/*_tmpl.c"))
+    _WriteArray(
+        fd, "arm32_asm_sources", _Glob("libdav1d/src/arm/32/*.S"),
+        _Glob("libdav1d/src/arm/32/*_tmpl.S") + ["libdav1d/src/arm/32/util.S"])
+    _WriteArray(
+        fd, "arm64_asm_sources", _Glob("libdav1d/src/arm/64/*.S"),
+        _Glob("libdav1d/src/arm/64/*_tmpl.S") + ["libdav1d/src/arm/64/util.S"])
+    _WriteArray(fd, "arm_template_sources", _Glob("libdav1d/src/arm/*_tmpl.c"))
 
-  template_sources = _Glob("libdav1d/src/*_tmpl.c")
-  _WriteArray(fd, "template_sources", template_sources)
+    template_sources = _Glob("libdav1d/src/*_tmpl.c")
+    _WriteArray(fd, "template_sources", template_sources)
 
-  # Generate list of sources which need to be compiled multiple times with the
-  # correct -DBIT_DEPTH=8|10 option specified each time.
-  _WriteArray(fd, "c_headers", _Glob("libdav1d/src/*.h"),
-             _DAV1D_ENTRY_POINT_SOURCES + template_sources)
-  _WriteArray(fd, "c_sources", _Glob("libdav1d/src/*.c"),
-             _DAV1D_ENTRY_POINT_SOURCES + template_sources)
+    # Generate list of sources which need to be compiled multiple times with the
+    # correct -DBIT_DEPTH=8|10 option specified each time.
+    _WriteArray(fd, "c_headers", _Glob("libdav1d/src/*.h"),
+                _DAV1D_ENTRY_POINT_SOURCES + template_sources)
+    _WriteArray(fd, "c_sources", _Glob("libdav1d/src/*.c"),
+                _DAV1D_ENTRY_POINT_SOURCES + template_sources)
 
-  _WriteArray(
-      fd, "entry_point_sources", _DAV1D_ENTRY_POINT_SOURCES, last_entry=True)
+    _WriteArray(fd,
+                "entry_point_sources",
+                _DAV1D_ENTRY_POINT_SOURCES,
+                last_entry=True)
 
 
 def main():
-  with open("dav1d_generated.gni", "w") as fd:
-    _WriteGn(fd)
+    with open("dav1d_generated.gni", "w") as fd:
+        _WriteGn(fd)
 
 
 if __name__ == "__main__":
-  main()
+    main()
