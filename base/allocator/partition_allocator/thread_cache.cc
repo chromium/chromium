792ab2401d587ece5ce08af57817a86eb103d05c
Benoit Lize <lizeb@chromium.org>
2022-03-30 11:29:45 +0000

    [PartitionAlloc] Clamp periodic purge interval
    Thread cache periodic purge frequency is adjusted at each purge to stay
    between a low and high value. However, if there is a background thread
    which holds onto a large amount of cached memory *and* is sleeping, its
    cached memory will not be reclaimed in-between two purges, leading to
    the interval shrinking way below the minimum.
    
    This happens for the OOBE on CrOS, as seen in the linked bug. To fix it,
    clamp the purge interval.
    
    Bisected by rsorokin@.
    
    Bug: 1311292
    Change-Id: I2b2620d584ddec464eddaba13888494fec6645a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3560230
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#986933}

--- base/allocator/partition_allocator/thread_cache.cc
+++ base/allocator/partition_allocator/thread_cache.cc
@@ -280,6 +280,17 @@ void ThreadCacheRegistry::RunPeriodicPurge() {
         std::min(kMaxPurgeInterval, periodic_purge_next_interval_ * 2);
   }
 
+  // Make sure that the next interval is in the right bounds. Even though the
+  // logic above should eventually converge to a reasonable interval, if a
+  // sleeping background thread holds onto a large amount of cached memory, then
+  // |PurgeAll()| will not free any memory from it, and the first branch above
+  // can be taken repeatedly until the interval gets very small, as the amount
+  // of cached memory cannot change between calls (since we do not purge
+  // background threads, but only ask them to purge their own cache at the next
+  // allocation).
+  periodic_purge_next_interval_ = std::clamp(
+      periodic_purge_next_interval_, kMinPurgeInterval, kMaxPurgeInterval);
+
   PurgeAll();
 }
 
