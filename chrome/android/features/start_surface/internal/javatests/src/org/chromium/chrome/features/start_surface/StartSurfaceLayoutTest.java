5f9fbaabf1ed205ef67fd05e8c479dd07a366588
Theresa Sullivan <twellington@chromium.org>
2022-03-30 22:59:13 +0000

    Revert "[Sheriff] Disable #testTabSuggestionMessageCardDismissAfterTabClosing"
    This reverts commit f35500cbc270fd9260cb551e87f8d9a3f969f17f.
    
    Reason for revert: Conflicting with another CL we need to land and potentially merge to M100 / M101... will reland this asap!
    
    Original change's description:
    > [Sheriff] Disable #testTabSuggestionMessageCardDismissAfterTabClosing
    >
    > Bug: 1311825
    > Change-Id: I2871c653bb0f1f9c3830a19b57e9c4a118ebb244
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561510
    > Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Owners-Override: Brandon Wylie <wylieb@chromium.org>
    > Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#987221}
    
    Bug: 1311825
    Change-Id: I9411a8fa269a86b99b076d92c05733692b5a727f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561515
    Auto-Submit: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#987236}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
@@ -964,7 +964,6 @@ public class StartSurfaceLayoutTest {
             ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study"})
     @CommandLineFlags.Add({BASE_PARAMS + "/baseline_tab_suggestions/true" +
             "/baseline_close_tab_suggestions/true/min_time_between_prefetches/0"})
-    @DisabledTest(message = "https://crbug.com/1311825")
     public void testTabSuggestionMessageCardDismissAfterTabClosing() throws InterruptedException {
         // clang-format on
         prepareTabs(3, 0, mUrl);
