cdbc2246863fff8eccd20cc6a452f5c5d8ee4370
Avi Drissman <avi@chromium.org>
2022-03-30 19:07:43 +0000

    Fix dock crash
    Reverse iterations are hard, especially with unsigned ints. Use the
    built-in utility methods on NSArray instead, to fix a crash with
    dock manipulation code.
    
    Fixed: 1311414
    Change-Id: Id23e2c882e3531b30e96b19055f79cc57f46118a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561462
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987105}

--- chrome/browser/mac/dock.mm
+++ chrome/browser/mac/dock.mm
@@ -127,15 +127,15 @@ NSMutableArray* PersistentAppPaths(NSArray* persistent_apps) {
   return app_paths;
 }
 
-bool IsAppAtPathAWebBrowser(NSString* app_path) {
+BOOL IsAppAtPathAWebBrowser(NSString* app_path) {
   NSBundle* app_bundle = [NSBundle bundleWithPath:app_path];
   if (!app_bundle)
-    return false;
+    return NO;
 
   NSArray* activities = base::mac::ObjCCast<NSArray>(
       [app_bundle objectForInfoDictionaryKey:@"NSUserActivityTypes"]);
   if (!activities)
-    return false;
+    return NO;
 
   return [activities containsObject:NSUserActivityTypeBrowsingWeb];
 }
@@ -304,13 +304,14 @@ AddIconStatus AddIcon(NSString* installed_path, NSString* dmg_app_path) {
       if (app_index == NSNotFound) {
         // Put the new application after the last browser application already
         // present in the Dock.
-        for (NSUInteger index = [persistent_apps count] - 1; index >= 0;
-             --index) {
-          if (IsAppAtPathAWebBrowser(persistent_app_paths[index])) {
-            app_index = index + 1;
-            break;
-          }
-        }
+        NSUInteger last_browser = [persistent_app_paths
+            indexOfObjectWithOptions:NSEnumerationReverse
+                         passingTest:^(NSString* app_path, NSUInteger idx,
+                                       BOOL* stop) {
+                           return IsAppAtPathAWebBrowser(app_path);
+                         }];
+        if (last_browser != NSNotFound)
+          app_index = last_browser + 1;
       }
 
       if (app_index == NSNotFound) {
