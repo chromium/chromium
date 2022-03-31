c55446aa85e2be99cf01673229f2798cb8cf64fd
Aya ElAttar <ayaelattar@chromium.org>
2022-03-31 14:14:20 +0000

    [sheriff] Disable TranslateFrameBinderFencedFrameBrowserTest.NotBindingInFencedFrame on Lacros
    Bug: 1312008
    Change-Id: Iefe4596f3b6f4b2d6bb44c22cbe70c7bc8cb15d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3562919
    Owners-Override: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Commit-Queue: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987439}

--- chrome/browser/translate/translate_frame_binder_browsertest.cc
+++ chrome/browser/translate/translate_frame_binder_browsertest.cc
@@ -173,8 +173,14 @@ class TranslateFrameBinderFencedFrameBrowserTest
   content::test::FencedFrameTestHelper fenced_frame_helper_;
 };
 
+// TODO(crbug.com/1312008): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_NotBindingInFencedFrame DISABLED_NotBindingInFencedFrame
+#else
+#define MAYBE_NotBindingInFencedFrame NotBindingInFencedFrame
+#endif
 IN_PROC_BROWSER_TEST_F(TranslateFrameBinderFencedFrameBrowserTest,
-                       NotBindingInFencedFrame) {
+                       MAYBE_NotBindingInFencedFrame) {
   TestTranslateDriverBindingContentBrowserClient test_browser_client;
   auto* old_browser_client = SetBrowserClientForTesting(&test_browser_client);
 
