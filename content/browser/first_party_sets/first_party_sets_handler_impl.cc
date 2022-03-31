9e1c09aa84fedfe7ba6823a6818182e23d0755e4
Shuran Huang <shuuran@chromium.org>
2022-03-31 13:58:57 +0000

    Write serialized FPSs data using ImportantFileWriter.
    Improve the writing resilience by using the method
    ImportantFileWriter::WriteFileAtomically(), which writes data to a
    temporary file first then rename it to become the real one.
    
    Bug: 1219656, 1311652
    Change-Id: I48b9809bcacc90ac8980554ddc13d219a76d3a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3557391
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987433}

--- content/browser/first_party_sets/first_party_sets_handler_impl.cc
+++ content/browser/first_party_sets/first_party_sets_handler_impl.cc
@@ -14,6 +14,7 @@
 #include "base/containers/flat_set.h"
 #include "base/files/file.h"
 #include "base/files/file_util.h"
+#include "base/files/important_file_writer.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -46,13 +47,9 @@ std::string LoadSetsFromDisk(const base::FilePath& path) {
 }
 
 // Writes the sets as raw JSON to the storage file.
-//
-// TODO(crbug.com/1219656): To handle the cases of file corrupting due to
-// incomplete writes, write to a temp file then rename over the old file.
-void MaybeWriteSetsToDisk(const base::FilePath& path, const std::string& sets) {
+void MaybeWriteSetsToDisk(const base::FilePath& path, base::StringPiece sets) {
   DCHECK(!path.empty());
-
-  if (!base::WriteFile(path, sets)) {
+  if (!base::ImportantFileWriter::WriteFileAtomically(path, sets)) {
     VLOG(1) << "Failed writing serialized First-Party Sets to file "
             << path.MaybeAsASCII();
   }
