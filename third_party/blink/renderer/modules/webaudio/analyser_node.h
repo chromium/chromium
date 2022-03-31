40db8f829a48560da3f3c54122c1c85cd4b0ab43
Michael Wilson <mjwilson@chromium.org>
2022-03-31 14:24:08 +0000

    [WebAudio] Split analyser node and handler into separate files
    Currently these classes are both are located in the same file,
    but they mainly operate on different threads.
    
    As a step in making the threading in our WebAudio implementation
    easier to understand, put these classes in separate files.
    
    This change should not affect functionality.
    
    Change-Id: I53a060531eafcc6cfd330a9b1145da85c485da3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561361
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987441}

--- third_party/blink/renderer/modules/webaudio/analyser_node.h
+++ third_party/blink/renderer/modules/webaudio/analyser_node.h
@@ -29,73 +29,13 @@
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_basic_inspector_node.h"
-#include "third_party/blink/renderer/modules/webaudio/realtime_analyser.h"
 
 namespace blink {
 
-class BaseAudioContext;
+class AnalyserHandler;
 class AnalyserOptions;
+class BaseAudioContext;
 class ExceptionState;
-
-class AnalyserHandler final : public AudioBasicInspectorHandler {
- public:
-  static scoped_refptr<AnalyserHandler> Create(AudioNode&, float sample_rate);
-  ~AnalyserHandler() override;
-
-  // AudioHandler
-  void Process(uint32_t frames_to_process) override;
-
-  unsigned FftSize() const { return analyser_.FftSize(); }
-  void SetFftSize(unsigned size, ExceptionState&);
-
-  unsigned FrequencyBinCount() const { return analyser_.FrequencyBinCount(); }
-
-  void SetMinDecibels(double k, ExceptionState&);
-  double MinDecibels() const { return analyser_.MinDecibels(); }
-
-  void SetMaxDecibels(double k, ExceptionState&);
-  double MaxDecibels() const { return analyser_.MaxDecibels(); }
-
-  void SetMinMaxDecibels(double min, double max, ExceptionState&);
-
-  void SetSmoothingTimeConstant(double k, ExceptionState&);
-  double SmoothingTimeConstant() const {
-    return analyser_.SmoothingTimeConstant();
-  }
-
-  void GetFloatFrequencyData(DOMFloat32Array* array, double current_time) {
-    analyser_.GetFloatFrequencyData(array, current_time);
-  }
-  void GetByteFrequencyData(DOMUint8Array* array, double current_time) {
-    analyser_.GetByteFrequencyData(array, current_time);
-  }
-  void GetFloatTimeDomainData(DOMFloat32Array* array) {
-    analyser_.GetFloatTimeDomainData(array);
-  }
-  void GetByteTimeDomainData(DOMUint8Array* array) {
-    analyser_.GetByteTimeDomainData(array);
-  }
-
-  // AnalyserNode needs special handling when updating the pull status
-  // because the node must get pulled even if there are no inputs or
-  // outputs so that the internal state is properly updated with the
-  // correct time data.
-  void UpdatePullStatusIfNeeded() override;
-
-  bool RequiresTailProcessing() const final;
-  double TailTime() const final;
-
- private:
-  AnalyserHandler(AudioNode&, float sample_rate);
-  bool PropagatesSilence() const override {
-    // An AnalyserNode does actually propogate silence, but to get the
-    // time and FFT data updated correctly, process() needs to be
-    // called even if all the inputs are silent.
-    return false;
-  }
-
-  RealtimeAnalyser analyser_;
-};
 
 class AnalyserNode final : public AudioBasicInspectorNode {
   DEFINE_WRAPPERTYPEINFO();
--- third_party/blink/renderer/modules/webaudio/base_audio_context.cc
+++ third_party/blink/renderer/modules/webaudio/base_audio_context.cc
@@ -25,6 +25,8 @@
 
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 
+#include <algorithm>
+
 #include "base/metrics/histogram_functions.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
@@ -71,6 +73,7 @@
 #include "third_party/blink/renderer/modules/webaudio/script_processor_node.h"
 #include "third_party/blink/renderer/modules/webaudio/stereo_panner_node.h"
 #include "third_party/blink/renderer/modules/webaudio/wave_shaper_node.h"
+#include "third_party/blink/renderer/platform/audio/fft_frame.h"
 #include "third_party/blink/renderer/platform/audio/iir_filter.h"
 #include "third_party/blink/renderer/platform/audio/vector_math.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -79,8 +82,6 @@
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/uuid.h"
-
-#include <algorithm>
 
 namespace blink {
 
