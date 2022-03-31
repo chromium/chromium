f0e9b945d0c9d5bb8d31b044326fd646aa7f53d8
Adenilson Cavalcanti <cavalcantii@chromium.org>
2022-03-31 01:56:46 +0000

    [zlib] Re-sync with zlib 1.2.12, patch 2 of N
    Ported:
    - Fix bug when window full in deflate_stored()
    - Fix CLEAR_HASH macro to be usable as a single statement
    - Avoid a conversion error in gzseek when off_t type too small
    
    Bug: 1032721
    Change-Id: I0f023f57affbd7ad4d163823f43cfbe51f770129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561506
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987297}

--- third_party/zlib/deflate.c
+++ third_party/zlib/deflate.c
@@ -176,10 +176,15 @@ local const config configuration_table[10] = {
 /* ===========================================================================
  * Initialize the hash table (avoiding 64K overflow for 16 bit systems).
  * prev[] will be initialized on the fly.
+ * TODO(cavalcantii): optimization opportunity, check comments on:
+ * https://chromium-review.googlesource.com/c/chromium/src/+/3561506/
  */
 #define CLEAR_HASH(s) \
-    s->head[s->hash_size-1] = NIL; \
-    zmemzero((Bytef *)s->head, (unsigned)(s->hash_size-1)*sizeof(*s->head));
+    do { \
+        s->head[s->hash_size-1] = NIL; \
+        zmemzero((Bytef *)s->head, \
+                 (unsigned)(s->hash_size-1)*sizeof(*s->head)); \
+    } while (0)
 
 /* ===========================================================================
  * Slide the hash table when sliding the window down (could be avoided with 32
@@ -1851,7 +1856,7 @@ local block_state deflate_stored(s, flush)
         return block_done;
 
     /* Fill the window with any remaining input. */
-    have = s->window_size - s->strstart - 1;
+    have = s->window_size - s->strstart;
     if (s->strm->avail_in > have && s->block_start >= (long)s->w_size) {
         /* Slide the window down. */
         s->block_start -= s->w_size;
