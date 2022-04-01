1ed730ea45632ada1555d0eb7eb710e74de393a0
Adithya Srinivasan <adithyas@chromium.org>
2022-04-01 18:53:03 +0000

    Sheriff: Disable TestServerProcess.test_basic
    Bug: 1309323
    Change-Id: I360960d2d529b753dc0038fa17185b439ac05aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566127
    Auto-Submit: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988041}

--- third_party/blink/tools/blinkpy/web_tests/port/server_process_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/port/server_process_unittest.py
@@ -90,6 +90,7 @@ class FakeServerProcess(server_process.ServerProcess):
 
 
 class TestServerProcess(unittest.TestCase):
+    @unittest.skip("https://crbug.com/1309323")
     def test_basic(self):
         cmd = [
             sys.executable, '-c',
