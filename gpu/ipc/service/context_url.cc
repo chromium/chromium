2b4cd53d482218b7d37929672df30f5671c9cbe3
Peng Huang <penghuang@chromium.org>
2022-04-01 13:21:56 +0000

    Always keep the last active context URL in crash key
    Bug: none
    Change-Id: I11ffbb64edeb8e1c9779a2c38770f466dbc975ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565240
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987877}

--- gpu/ipc/service/context_url.cc
+++ gpu/ipc/service/context_url.cc
@@ -13,6 +13,17 @@ namespace gpu {
 
 // static
 void ContextUrl::SetActiveUrl(const gpu::ContextUrl& active_url) {
+  bool is_skia_renderer = active_url.url() == "chrome://gpu/SkiaRenderer";
+
+  {
+    static crash_reporter::CrashKeyString<8> crash_key(
+        "gpu-context-is-skia-renderer");
+    crash_key.Set(is_skia_renderer ? "true" : "false");
+  }
+
+  if (is_skia_renderer)
+    return;
+
   // Skip setting crash key when URL hash hasn't changed.
   static size_t last_url_hash = 0;
   if (active_url.hash() == last_url_hash)
@@ -23,8 +34,10 @@ void ContextUrl::SetActiveUrl(const gpu::ContextUrl& active_url) {
   // Note that the url is intentionally excluded from WebView and WebLayer
   // crash dumps using an allowlist for privacy reasons. See
   // kWebViewCrashKeyAllowList and kWebLayerCrashKeyAllowList.
-  static crash_reporter::CrashKeyString<1024> crash_key("gpu-url-chunk");
-  crash_key.Set(active_url.url().possibly_invalid_spec());
+  {
+    static crash_reporter::CrashKeyString<1024> crash_key("gpu-url-chunk");
+    crash_key.Set(active_url.url().possibly_invalid_spec());
+  }
 }
 
 ContextUrl::ContextUrl(GURL url)
