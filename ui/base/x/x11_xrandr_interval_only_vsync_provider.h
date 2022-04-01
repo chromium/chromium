8761dbebf59b78fd0f1d93c8fd1153fcc2824604
Peng Huang <penghuang@chromium.org>
2022-04-01 15:11:24 +0000

    XrandrIntervalOnlyVSyncProvider: listen x11::RandR::NotifyEvent
    Currently XrandrIntervalOnlyVSyncProvider get refresh interval by
    calling GetPrimaryDisplayRefreshIntervalFromXrandr() for every
    100 frames. GetPrimaryDisplayRefreshIntervalFromXrandr() uses
    4 sync x11 IPC. It blocks GPU thread. This CL improves it by
    listening xrandr events, and only call
    GetPrimaryDisplayRefreshIntervalFromXrandr(), when display mode is
    changed.
    
    Bug: 1308981
    Change-Id: I778adf1f2253a08cd51c9346ff5b0d978b1b53b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563263
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#987918}

--- ui/base/x/x11_xrandr_interval_only_vsync_provider.h
+++ ui/base/x/x11_xrandr_interval_only_vsync_provider.h
@@ -6,7 +6,6 @@
 #define UI_BASE_X_X11_XRANDR_INTERVAL_ONLY_VSYNC_PROVIDER_H_
 
 #include "base/component_export.h"
-#include "base/time/time.h"
 #include "ui/gfx/vsync_provider.h"
 
 namespace ui {
@@ -15,6 +14,7 @@ class COMPONENT_EXPORT(UI_BASE_X) XrandrIntervalOnlyVSyncProvider
     : public gfx::VSyncProvider {
  public:
   explicit XrandrIntervalOnlyVSyncProvider();
+  ~XrandrIntervalOnlyVSyncProvider() override;
 
   // gfx::VSyncProvider:
   void GetVSyncParameters(UpdateVSyncCallback callback) override;
@@ -22,13 +22,6 @@ class COMPONENT_EXPORT(UI_BASE_X) XrandrIntervalOnlyVSyncProvider
                                      base::TimeDelta* interval) override;
   bool SupportGetVSyncParametersIfAvailable() const override;
   bool IsHWClock() const override;
-
- private:
-  void UpdateInterval();
-
-  base::TimeDelta interval_;
-  static const int kCallsBetweenUpdates = 100;
-  int calls_since_last_update_ = kCallsBetweenUpdates;
 };
 
 }  // namespace ui
