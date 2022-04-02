cf4af5c6ccc2a3556109ffc547fda7febca1bb9e
Bruce Dawson <brucedawson@chromium.org>
2022-04-02 05:36:15 +0000

    Fix Windows path limits in eslint.py
    When running "git cl presubmit --all" the eslint.py script may be called
    with ~1,100 or more arguments. This leads to a command line that is too
    long for Windows to handle. This change invokes eslint multiple times to
    avoid hitting the limit.
    
    This change was initially blocked by a buggy presubmit which was fixed
    in crrev.com/c/3553927.
    
    This change can be (and has been) tested with this command (using the
    recently added --files option):
    
        git cl presubmit --files=*.js --force
    
    For much faster results return early from PanProjectChecks in
    depot_tools/presubmit_canned_checks.py.
    
    See crrev.com/c/3554633 for another example of fixing this issue.
    
    Bug: 1309977
    Change-Id: If97d9a7e5258f2783d6cd9a01a6a8b3095a1ae54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3554791
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988257}

--- tools/web_dev_style/eslint.py
+++ tools/web_dev_style/eslint.py
@@ -10,6 +10,7 @@ def Run(os_path=None, args=None):
     _SRC_PATH = os_path.normpath(os_path.join(_HERE_PATH, '..', '..'))
     _NODE_PATH = os_path.join(_SRC_PATH, 'third_party', 'node')
 
+    import os
     import sys
     old_sys_path = sys.path[:]
     sys.path.append(_NODE_PATH)
@@ -18,11 +19,29 @@ def Run(os_path=None, args=None):
   finally:
     sys.path = old_sys_path
 
-  return node.RunNode([
-      node_modules.PathToEsLint(),
-      '--resolve-plugins-relative-to',
-      os_path.join(_NODE_PATH, 'node_modules'),
-  ] + args)
+  # When running git cl presubmit --all this presubmit may be asked to check
+  # ~1,100 files, leading to a command line that is about 92,000 characters.
+  # This goes past the Windows 8191 character cmd.exe limit and causes cryptic
+  # failures. To avoid these we break the command up into smaller pieces. The
+  # non-Windows limit is chosen so that the code that splits up commands will
+  # get some exercise on other platforms.
+  # Depending on how long the command is on Windows the error may be:
+  #     The command line is too long.
+  # Or it may be:
+  #     OSError: Execution failed with error: [WinError 206] The filename or
+  #     extension is too long.
+  # I suspect that the latter error comes from CreateProcess hitting its 32768
+  # character limit.
+  files_per_command = 50 if os.name == 'nt' else 1000
+  results = []
+  for i in range(0, len(args), files_per_command):
+    results.append(
+        node.RunNode([
+            node_modules.PathToEsLint(),
+            '--resolve-plugins-relative-to',
+            os_path.join(_NODE_PATH, 'node_modules'),
+        ] + args[i:i + files_per_command]))
+  return results
 
 
 if __name__ == '__main__':
