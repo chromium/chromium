934820bc1a50f575054711aef3ad6ff9b08ded2c
John Wu <jzw@chromium.org>
2022-04-04 18:23:51 +0000

    Move safe_browsing_error to ios/components
    This is to allow sharing of safe browsing code with ios/web_view.
    
    Bug: 1304908
    Change-Id: I6342d8bd2db2ed3c76b4707c941829df0f853613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561324
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988586}

--- ios/components/security_interstitials/safe_browsing/safe_browsing_error.h
+++ ios/components/security_interstitials/safe_browsing/safe_browsing_error.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
-#define IOS_CHROME_BROWSER_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
+#ifndef IOS_COMPONENTS_SECURITY_INTERSTITIALS_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
+#define IOS_COMPONENTS_SECURITY_INTERSTITIALS_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
 
 #import <Foundation/Foundation.h>
 
@@ -13,4 +13,4 @@ extern const NSErrorDomain kSafeBrowsingErrorDomain;
 // Error code for navigations to unsafe resources
 extern const NSInteger kUnsafeResourceErrorCode;
 
-#endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
+#endif  // IOS_COMPONENTS_SECURITY_INTERSTITIALS_SAFE_BROWSING_SAFE_BROWSING_ERROR_H_
--- ios/components/security_interstitials/safe_browsing/safe_browsing_error.mm
+++ ios/components/security_interstitials/safe_browsing/safe_browsing_error.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/safe_browsing/safe_browsing_error.h"
+#import "ios/components/security_interstitials/safe_browsing/safe_browsing_error.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
