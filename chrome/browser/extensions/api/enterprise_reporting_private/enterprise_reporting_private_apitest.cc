85418e5835cfad1d18efec2de88803860583e58c
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-04-04 17:06:23 +0000

    Revert "Re-enable GetPersistentSecret test on ARM Mac"
    This reverts commit 491f5eff56a9faf982e3ad12850d06a69d26382a.
    
    Reason for revert: crbug.com/1313016
    
    Original change's description:
    > Re-enable GetPersistentSecret test on ARM Mac
    >
    > Bug: 1222670
    > Change-Id: I397b74cfe57c4fcad273fa58599e72758d4f5d91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563583
    > Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#988442}
    
    Bug: 1222670
    Change-Id: I1a81887256431cf4549d0cc8b3ccdf3097db7fd1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570021
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#988542}

--- chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_apitest.cc
+++ chrome/browser/extensions/api/enterprise_reporting_private/enterprise_reporting_private_apitest.cc
@@ -128,7 +128,14 @@ IN_PROC_BROWSER_TEST_F(EnterpriseReportingPrivateApiTest, GetDeviceId) {
   RunTest(base::StringPrintf(kTest, kAssertions));
 }
 
-IN_PROC_BROWSER_TEST_F(EnterpriseReportingPrivateApiTest, GetPersistentSecret) {
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
+// https://crbug.com/1222670
+#define MAYBE_GetPersistentSecret DISABLED_GetPersistentSecret
+#else
+#define MAYBE_GetPersistentSecret GetPersistentSecret
+#endif
+IN_PROC_BROWSER_TEST_F(EnterpriseReportingPrivateApiTest,
+                       MAYBE_GetPersistentSecret) {
   constexpr char kAssertions[] =
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
       "chrome.test.assertNoLastError();"
