b1d6b19d5a4e2b807cbd99c975e767988cceada5
Jason Rhee <jasonrhee@google.com>
2022-04-05 01:30:25 +0000

    [Smartlock] Add metadata for LoginAuthFactorsView for testing.
    This is required for us to track an animation that we need to wait for
    completion of during a smartlock.Unlock e2e test.
    
    Change-Id: If4b7a106229bd2a863474b48a36801ce213f540e
    Bug: b/226204290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570060
    Reviewed-by: Curt Clemens <cclem@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Jason Rhee <jasonrhee@google.com>
    Cr-Commit-Position: refs/heads/main@{#988795}

--- ash/login/ui/login_auth_factors_view.h
+++ ash/login/ui/login_auth_factors_view.h
@@ -11,6 +11,7 @@
 #include "ash/login/ui/auth_factor_model.h"
 #include "base/callback.h"
 #include "base/timer/timer.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -28,6 +29,7 @@ class ArrowButtonView;
 // login screens.
 class ASH_EXPORT LoginAuthFactorsView : public views::View {
  public:
+  METADATA_HEADER(LoginAuthFactorsView);
   using AuthFactorState = AuthFactorModel::AuthFactorState;
 
   // TestApi is used for tests to get internal implementation details.
