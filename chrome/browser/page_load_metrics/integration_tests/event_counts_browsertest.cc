7443458166fb00788c407d9bb58140cd4bc4b139
Wez <wez@chromium.org>
2022-04-05 08:23:45 +0000

    Revert "Waiting to receive all the events in JS to finish event count test"
    This reverts commit a4ed3aeeed28597350e1e5325726bd890352790d.
    
    Reason for revert: Re-enables MetricIntegrationTest.EventCountsForTapOnScreen which is flaky.
    
    Original change's description:
    > Waiting to receive all the events in JS to finish event count test
    >
    > This test is to verify if we receive the correct events in performance
    > EventTiming API, but we do not know when the events are received. If we
    > add event handlers in JS, and wait until we receive all the events, we
    > can guarantee that the events generated by the Testdriver are received
    > in the renderer.
    >
    > In this test, we simulate a touch down, a touch up, a tap, the touch
    > event will generate touch and pointer events, the tap event will
    > generate mouse events and click events. We were just listening to the
    > mousedown events, but we still got errors of missing touch events in the
    > eventCount, it seems that the touch events come after the mouse event
    > and pointer event.
    > We will check the event count in the performance observer until the
    > event handlers to receive the click event, touch event and pointer
    > events. We may get timeout, but we want to give it a try to see if it
    > works.
    >
    > I split the test into two parts to simply the events in each test,
    > https://bugs.chromium.org/p/chromium/issues/detail?id=1313167. I will
    > add another one once this one is no longer flaky.
    >
    >
    > Bug: 1311383
    > Change-Id: Id146cfed8e85ce9267bf31161dd294fe3ae00911
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3565490
    > Reviewed-by: Nicolás Peña <npm@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#988659}
    
    Bug: 1311383, 1313252
    Change-Id: I7953261faa989a7d1e2290a05525610b3098fced
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3568375
    Auto-Submit: Wez <wez@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#988875}

--- chrome/browser/page_load_metrics/integration_tests/event_counts_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/event_counts_browsertest.cc
@@ -9,7 +9,13 @@
 #include "content/public/test/browser_test_utils.h"
 
 #if defined(USE_AURA)
-IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, EventCountsForTapOnScreen) {
+// TODO(crbug.com/1311383) Fix flakiness and reenable the test.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_EventCounts DISABLED_EventCounts
+#else
+#define MAYBE_EventCounts EventCounts
+#endif
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, MAYBE_EventCounts) {
   LoadHTML(R"HTML(
     <p>Sample website</p>
     <script type="text/javascript">
@@ -32,12 +38,15 @@ IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, EventCountsForTapOnScreen) {
   content::SimulateTouchEventAt(web_contents(), ui::ET_TOUCH_RELEASED,
                                 gfx::Point(30, 60));
 
-  // Wait until we receive the simulated events in the event handlers to check
-  // the PerformanceObserver.
-  while (
-      EvalJs(web_contents(), "window.eventCounts.click").ExtractInt() < 1 ||
-      EvalJs(web_contents(), "window.eventCounts.pointerup").ExtractInt() < 1 ||
-      EvalJs(web_contents(), "window.eventCounts.touchend").ExtractInt() < 1) {
+  // Simulate clicks.
+  content::SimulateMouseClick(web_contents(), 0,
+                              blink::WebMouseEvent::Button::kLeft);
+  content::SimulateMouseClick(web_contents(), 0,
+                              blink::WebMouseEvent::Button::kLeft);
+
+  while (EvalJs(web_contents(), "window.eventCounts.click").ExtractInt() < 3 &&
+         EvalJs(web_contents(), "window.eventCounts.pointerup").ExtractInt() <
+             3) {
     base::RunLoop run_loop;
     base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
         FROM_HERE, run_loop.QuitClosure(), base::Milliseconds(100));
@@ -68,11 +77,11 @@ IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, EventCountsForTapOnScreen) {
       EvalJs(web_contents(), "window.performance.eventCounts.get('touchend')")
           .ExtractInt();
 
-  EXPECT_EQ(expected_pointerdown, 1);
-  EXPECT_EQ(expected_pointerup, 1);
-  EXPECT_EQ(expected_mousedown, 1);
-  EXPECT_EQ(expected_mouseup, 1);
-  EXPECT_EQ(expected_click, 1);
+  EXPECT_EQ(expected_pointerdown, 3);
+  EXPECT_EQ(expected_pointerup, 3);
+  EXPECT_EQ(expected_mousedown, 3);
+  EXPECT_EQ(expected_mouseup, 3);
+  EXPECT_EQ(expected_click, 3);
   EXPECT_EQ(expected_touchstart, 1);
   EXPECT_EQ(expected_touchend, 1);
 }
