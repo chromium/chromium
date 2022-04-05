60e60486743eaea07bc4d905c8257e05475cd96a
Hirthanan Subenderan <hirthanan@google.com>
2022-04-05 20:28:17 +0000

    Finch enabled query requests should happen before imports.
    In the future, we want to enable query requests for the check membership
    requests. In the case where both finch flags for monthly check in and
    check membership are enabled. We want to prioritize the enabled check
    membership request.
    
    (cherry picked from commit 28c7286a167959c1eec6fb599ec8379730f42dc6)
    
    Change-Id: I5765de0f6c8c59d588d2d4ff3aa676e40d5c4933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3553648
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#985648}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571492
    Auto-Submit: Hirthanan Subenderan <hirthanan@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4951@{#481}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- ash/components/device_activity/device_activity_client.cc
+++ ash/components/device_activity/device_activity_client.cc
@@ -388,16 +388,16 @@ void DeviceActivityClient::TransitionOutOfIdle(
         }
       case psm_rlwe::RlweUseCase::CROS_FRESNEL_MONTHLY:
         if (base::FeatureList::IsEnabled(
-                features::kDeviceActiveClientMonthlyCheckIn)) {
-          // During rollout, we perform CheckIn without CheckMembership for
-          // powerwash, recovery, or RMA devices.
-          TransitionToCheckIn(current_use_case);
+                features::kDeviceActiveClientMonthlyCheckMembership)) {
+          TransitionToCheckMembershipOprf(current_use_case);
           return;
         }
 
         if (base::FeatureList::IsEnabled(
-                features::kDeviceActiveClientMonthlyCheckMembership)) {
-          TransitionToCheckMembershipOprf(current_use_case);
+                features::kDeviceActiveClientMonthlyCheckIn)) {
+          // During rollout, we perform CheckIn without CheckMembership for
+          // powerwash, recovery, or RMA devices.
+          TransitionToCheckIn(current_use_case);
           return;
         }
 
