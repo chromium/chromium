c1afe4c0ee12ec5864f1de1046a11081def05689
Yue Zhang <yuezhanggg@chromium.org>
2022-04-05 19:17:29 +0000

    Reland "[CHU] Populate CommerceHeuristicsData on renderer side"
    This reverts commit 2ba2d427d9900fd3051924b28cf4cafba71ce9c8.
    
    Reason for revert: Fix the failing test.
    
    Original change's description:
    > Revert "[CHU] Populate CommerceHeuristicsData on renderer side"
    >
    > This reverts commit f085c67a8ecda16b014c601f5871bf38cc312f43.
    >
    > Reason for revert:
    > CommerceHintAgentTest.VisitCart_PerDomain_FromComponent
    > CommerceHintAgentTest.VisitCart_GeneralPattern_FromComponent
    > Tests started to consistently fail after this CL. 
    > See https://ci.chromium.org/p/chromium/builders/ci/linux-bfcache-rel , builds 30517-30519
    >
    > Original change's description:
    > > [CHU] Populate CommerceHeuristicsData on renderer side
    > >
    > > This CL leverages the existing OnNavigation mojo API to pass heuristics
    > > data to renderer side to populate the renderer side
    > > CommerceHeuristicsData. One optimization we applied here is that due to
    > > site isolation, render frames of different domains will live in their
    > > own process respectively, so we only need to populate the renderer side
    > > CommerceHeuristicsData with heuristics of current domain.
    > >
    > > Bug: 1300332
    > > Change-Id: I179570d743cd208a4f56257ac97731f25f21e7f0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3539517
    > > Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    > > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#986871}
    >
    > Bug: 1300332
    > Change-Id: I2ee77f7e80998136967b01e0620c8c0dec694b3f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3559990
    > Auto-Submit: Michael Ershov <miersh@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Michael Ershov <miersh@google.com>
    > Commit-Queue: Michael Ershov <miersh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#986903}
    
    (cherry picked from commit 272958945c42cf24828cd3c119c3162bcd192c52)
    
    Bug: 1300332
    Change-Id: I58fa1ae21e6fb0f7fb3c88c5b3b137190687955a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561884
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#987635}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572185
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4951@{#473}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- chrome/renderer/cart/commerce_hint_agent_browsertest.cc
+++ chrome/renderer/cart/commerce_hint_agent_browsertest.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/cart/cart_db_content.pb.h"
 #include "chrome/browser/cart/cart_service.h"
+#include "chrome/browser/cart/commerce_hint_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/persisted_state_db/profile_proto_db.h"
@@ -42,6 +43,7 @@
 #include "chrome/test/base/android/android_browser_test.h"
 #else
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/commerce/core/commerce_heuristics_data.h"
 #endif
 
 namespace {
@@ -191,6 +193,8 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
     PlatformBrowserTest::SetUpOnMainThread();
     Profile* profile =
         Profile::FromBrowserContext(web_contents()->GetBrowserContext());
+    commerce_hint_service_ =
+        cart::CommerceHintService::FromWebContents(web_contents());
     service_ = CartServiceFactory::GetForProfile(profile);
     auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
     ASSERT_TRUE(identity_manager);
@@ -414,6 +418,7 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
 
   base::test::ScopedFeatureList scoped_feature_list_;
   CartService* service_;
+  cart::CommerceHintService* commerce_hint_service_;
   net::EmbeddedTestServer https_server_{net::EmbeddedTestServer::TYPE_HTTPS};
   std::unique_ptr<ukm::TestAutoSetUkmRecorder> ukm_recorder_;
   bool satisfied_;
@@ -477,6 +482,65 @@ IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, VisitCart) {
   WaitForCartCount(kExpectedExample);
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS)
+
+// Flaky on Windows: https://crbug.com/1300332.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_VisitCart_GeneralPattern_FromComponent \
+  DISABLED_VisitCart_GeneralPattern_FromComponent
+#else
+#define MAYBE_VisitCart_GeneralPattern_FromComponent \
+  VisitCart_GeneralPattern_FromComponent
+#endif
+IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest,
+                       MAYBE_VisitCart_GeneralPattern_FromComponent) {
+  bool is_populated =
+      commerce_hint_service_->InitializeCommerceHeuristicsForTesting(
+          base::Version("0.0.0.1"), "{}", R"###(
+          {
+            "cart_page_url_regex": "(special|lol)"
+          }
+      )###",
+          "", "");
+  DCHECK(is_populated);
+
+  NavigateToURL("https://www.guitarcenter.com/special.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 1);
+
+  NavigateToURL("https://www.guitarcenter.com/cart.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 1);
+
+  NavigateToURL("https://www.guitarcenter.com/lol.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 2);
+}
+
+IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest,
+                       VisitCart_PerDomain_FromComponent) {
+  bool is_populated =
+      commerce_hint_service_->InitializeCommerceHeuristicsForTesting(
+          base::Version("0.0.0.1"), R"###(
+          {
+            "guitarcenter.com": {
+              "cart_url_regex" : "unique|laugh"
+            }
+          }
+      )###",
+          R"###(
+          {
+            "cart_page_url_regex": "(special|lol)"
+          }
+      )###",
+          "", "");
+  DCHECK(is_populated);
+
+  NavigateToURL("https://www.guitarcenter.com/unique.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 1);
+
+  NavigateToURL("https://www.guitarcenter.com/special.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 1);
+
+  NavigateToURL("https://www.guitarcenter.com/laugh.html");
+  WaitForUmaCount("Commerce.Carts.VisitCart", 2);
+}
 
 IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, ExtractCart) {
   // This page has three products.
@@ -668,7 +732,9 @@ IN_PROC_BROWSER_TEST_F(CommerceHintCacaoTest, Passed) {
 // nothing is specified in AddHintForTesting(), and the real hints are not
 // downloaded, all the URLs are considered non-shopping.
 IN_PROC_BROWSER_TEST_F(CommerceHintCacaoTest, Rejected) {
-  NavigateToURL("https://www.guitarcenter.com/");
+  NavigateToURL("https://www.guitarcenter.com/cart");
+  WaitForCartCount(kEmptyExpected);
+
   SendXHR("/add-to-cart", "product: 123");
   base::PlatformThread::Sleep(TestTimeouts::tiny_timeout() * 30);
   WaitForCartCount(kEmptyExpected);
@@ -1200,8 +1266,7 @@ class CommerceHintOptimizeRendererTest : public CommerceHintAgentTest {
   void SetUpInProcessBrowserTestFixture() override {
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {{ntp_features::kNtpChromeCartModule,
-          {{"optimize-renderer-signal", "true"},
-           {"cart-extraction-gap-time", "0s"}}},
+          {{"cart-extraction-gap-time", "0s"}}},
          {optimization_guide::features::kOptimizationHints, {{}}}},
         {});
   }
--- components/commerce/core/commerce_heuristics_data_unittest.cc
+++ components/commerce/core/commerce_heuristics_data_unittest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "components/commerce/core/commerce_heuristics_data.h"
+#include "base/strings/string_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace commerce_heuristics {
@@ -56,14 +57,14 @@ TEST_F(CommerceHeuristicsDataTest, TestVersion) {
   base::Version version1("2022.1.1.1200");
   commerce_heuristics::CommerceHeuristicsData::GetInstance().UpdateVersion(
       version1);
-  ASSERT_EQ(
+  EXPECT_EQ(
       commerce_heuristics::CommerceHeuristicsData::GetInstance().GetVersion(),
       "2022.1.1.1200");
 
   base::Version version2("2022.2.1.1300");
   commerce_heuristics::CommerceHeuristicsData::GetInstance().UpdateVersion(
       version2);
-  ASSERT_EQ(
+  EXPECT_EQ(
       commerce_heuristics::CommerceHeuristicsData::GetInstance().GetVersion(),
       "2022.2.1.1300");
 }
@@ -74,49 +75,49 @@ TEST_F(CommerceHeuristicsDataTest, TestPopulateHintHeuristics_Success) {
                                              kGlobalHeuristicsJSONData, "",
                                              ""));
   auto* hint_heuristics = GetHintHeuristics();
-  ASSERT_EQ(hint_heuristics->size(), 3u);
-  ASSERT_TRUE(hint_heuristics->contains("foo.com"));
-  ASSERT_TRUE(hint_heuristics->contains("bar.com"));
-  ASSERT_TRUE(hint_heuristics->contains("baz.com"));
-  ASSERT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("merchant_name"),
+  EXPECT_EQ(hint_heuristics->size(), 3u);
+  EXPECT_TRUE(hint_heuristics->contains("foo.com"));
+  EXPECT_TRUE(hint_heuristics->contains("bar.com"));
+  EXPECT_TRUE(hint_heuristics->contains("baz.com"));
+  EXPECT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("merchant_name"),
             "Foo");
-  ASSERT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("cart_url"),
+  EXPECT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("cart_url"),
             "foo.com/cart");
-  ASSERT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("cart_url_regex"),
+  EXPECT_EQ(*hint_heuristics->FindDict("foo.com")->FindString("cart_url_regex"),
             "foo.com/([^/]+/)?cart");
-  ASSERT_EQ(*hint_heuristics->FindDict("bar.com")->FindString("merchant_name"),
+  EXPECT_EQ(*hint_heuristics->FindDict("bar.com")->FindString("merchant_name"),
             "Bar");
-  ASSERT_EQ(
+  EXPECT_EQ(
       *hint_heuristics->FindDict("bar.com")->FindString("checkout_url_regex"),
       "bar.com/([^/]+/)?checkout");
-  ASSERT_EQ(
+  EXPECT_EQ(
       *hint_heuristics->FindDict("baz.com")->FindString("purchase_url_regex"),
       "baz.com/([^/]+/)?purchase");
   auto* global_heuristics = GetGlobalHeuristics();
-  ASSERT_EQ(global_heuristics->size(), 7u);
-  ASSERT_TRUE(global_heuristics->contains("sensitive_product_regex"));
-  ASSERT_EQ(*global_heuristics->FindString("sensitive_product_regex"),
+  EXPECT_EQ(global_heuristics->size(), 7u);
+  EXPECT_TRUE(global_heuristics->contains("sensitive_product_regex"));
+  EXPECT_EQ(*global_heuristics->FindString("sensitive_product_regex"),
             "\\b\\B");
-  ASSERT_TRUE(
+  EXPECT_TRUE(
       global_heuristics->contains("rule_discount_partner_merchant_regex"));
-  ASSERT_EQ(
+  EXPECT_EQ(
       *global_heuristics->FindString("rule_discount_partner_merchant_regex"),
       "foo");
-  ASSERT_TRUE(
+  EXPECT_TRUE(
       global_heuristics->contains("coupon_discount_partner_merchant_regex"));
-  ASSERT_EQ(
+  EXPECT_EQ(
       *global_heuristics->FindString("coupon_discount_partner_merchant_regex"),
       "bar");
-  ASSERT_TRUE(global_heuristics->contains("cart_page_url_regex"));
-  ASSERT_EQ(*global_heuristics->FindString("cart_page_url_regex"), "cart");
-  ASSERT_TRUE(global_heuristics->contains("checkout_page_url_regex"));
-  ASSERT_EQ(*global_heuristics->FindString("checkout_page_url_regex"),
+  EXPECT_TRUE(global_heuristics->contains("cart_page_url_regex"));
+  EXPECT_EQ(*global_heuristics->FindString("cart_page_url_regex"), "cart");
+  EXPECT_TRUE(global_heuristics->contains("checkout_page_url_regex"));
+  EXPECT_EQ(*global_heuristics->FindString("checkout_page_url_regex"),
             "checkout");
-  ASSERT_TRUE(global_heuristics->contains("purchase_button_text_regex"));
-  ASSERT_EQ(*global_heuristics->FindString("purchase_button_text_regex"),
+  EXPECT_TRUE(global_heuristics->contains("purchase_button_text_regex"));
+  EXPECT_EQ(*global_heuristics->FindString("purchase_button_text_regex"),
             "purchase");
-  ASSERT_TRUE(global_heuristics->contains("add_to_cart_request_regex"));
-  ASSERT_EQ(*global_heuristics->FindString("add_to_cart_request_regex"),
+  EXPECT_TRUE(global_heuristics->contains("add_to_cart_request_regex"));
+  EXPECT_EQ(*global_heuristics->FindString("add_to_cart_request_regex"),
             "add_to_cart");
 }
 
@@ -130,11 +131,11 @@ TEST_F(CommerceHeuristicsDataTest, TestPopulateHeuristics_Failure) {
               "merchant_name": "Bar"
   )###";
 
-  ASSERT_FALSE(commerce_heuristics::CommerceHeuristicsData::GetInstance()
+  EXPECT_FALSE(commerce_heuristics::CommerceHeuristicsData::GetInstance()
                    .PopulateDataFromComponent(broken_hint_json_string,
                                               kGlobalHeuristicsJSONData, "",
                                               ""));
-  ASSERT_FALSE(
+  EXPECT_FALSE(
       commerce_heuristics::CommerceHeuristicsData::GetInstance()
           .PopulateDataFromComponent(kHintHeuristicsJSONData, "{", "", ""));
 }
@@ -145,10 +146,10 @@ TEST_F(CommerceHeuristicsDataTest, TestGetMerchantName) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(*data.GetMerchantName("foo.com"), "Foo");
-  ASSERT_EQ(*data.GetMerchantName("bar.com"), "Bar");
-  ASSERT_FALSE(data.GetMerchantName("baz.com").has_value());
-  ASSERT_FALSE(data.GetMerchantName("xyz.com").has_value());
+  EXPECT_EQ(*data.GetMerchantName("foo.com"), "Foo");
+  EXPECT_EQ(*data.GetMerchantName("bar.com"), "Bar");
+  EXPECT_FALSE(data.GetMerchantName("baz.com").has_value());
+  EXPECT_FALSE(data.GetMerchantName("xyz.com").has_value());
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetMerchantCartURL) {
@@ -157,8 +158,8 @@ TEST_F(CommerceHeuristicsDataTest, TestGetMerchantCartURL) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(*data.GetMerchantCartURL("foo.com"), "foo.com/cart");
-  ASSERT_FALSE(data.GetMerchantCartURL("baz.com").has_value());
+  EXPECT_EQ(*data.GetMerchantCartURL("foo.com"), "foo.com/cart");
+  EXPECT_FALSE(data.GetMerchantCartURL("baz.com").has_value());
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetProductSkipPattern) {
@@ -167,7 +168,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetProductSkipPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetProductSkipPattern()->pattern(), "\\b\\B");
+  EXPECT_EQ(data.GetProductSkipPattern()->pattern(), "\\b\\B");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetRuleDiscountPartnerMerchantPattern) {
@@ -176,7 +177,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetRuleDiscountPartnerMerchantPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetRuleDiscountPartnerMerchantPattern()->pattern(), "foo");
+  EXPECT_EQ(data.GetRuleDiscountPartnerMerchantPattern()->pattern(), "foo");
 }
 
 TEST_F(CommerceHeuristicsDataTest,
@@ -186,7 +187,7 @@ TEST_F(CommerceHeuristicsDataTest,
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCouponDiscountPartnerMerchantPattern()->pattern(), "bar");
+  EXPECT_EQ(data.GetCouponDiscountPartnerMerchantPattern()->pattern(), "bar");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetCartPageURLPattern) {
@@ -195,7 +196,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetCartPageURLPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCartPageURLPattern()->pattern(), "cart");
+  EXPECT_EQ(data.GetCartPageURLPattern()->pattern(), "cart");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetCheckoutPageURLPattern) {
@@ -204,7 +205,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetCheckoutPageURLPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCheckoutPageURLPattern()->pattern(), "checkout");
+  EXPECT_EQ(data.GetCheckoutPageURLPattern()->pattern(), "checkout");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetPurchaseButtonTextPattern) {
@@ -213,7 +214,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetPurchaseButtonTextPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetPurchaseButtonTextPattern()->pattern(), "purchase");
+  EXPECT_EQ(data.GetPurchaseButtonTextPattern()->pattern(), "purchase");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetAddToCartRequestPattern) {
@@ -222,7 +223,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetAddToCartRequestPattern) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetAddToCartRequestPattern()->pattern(), "add_to_cart");
+  EXPECT_EQ(data.GetAddToCartRequestPattern()->pattern(), "add_to_cart");
 }
 
 TEST_F(CommerceHeuristicsDataTest, TestGetCartPageURLPatternForDomain) {
@@ -231,7 +232,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetCartPageURLPatternForDomain) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCartPageURLPatternForDomain("foo.com")->pattern(),
+  EXPECT_EQ(data.GetCartPageURLPatternForDomain("foo.com")->pattern(),
             "foo.com/([^/]+/)?cart");
 }
 
@@ -241,7 +242,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetCheckoutPageURLPatternForDomain) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCheckoutPageURLPatternForDomain("bar.com")->pattern(),
+  EXPECT_EQ(data.GetCheckoutPageURLPatternForDomain("bar.com")->pattern(),
             "bar.com/([^/]+/)?checkout");
 }
 
@@ -251,7 +252,7 @@ TEST_F(CommerceHeuristicsDataTest, TestGetPurchasePageURLPatternForDomain) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetPurchasePageURLPatternForDomain("baz.com")->pattern(),
+  EXPECT_EQ(data.GetPurchasePageURLPatternForDomain("baz.com")->pattern(),
             "baz.com/([^/]+/)?purchase");
 }
 
@@ -261,11 +262,11 @@ TEST_F(CommerceHeuristicsDataTest, TestRepopulateHintData) {
   ASSERT_TRUE(data.PopulateDataFromComponent(
       kHintHeuristicsJSONData, kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_EQ(data.GetCartPageURLPatternForDomain("foo.com")->pattern(),
+  EXPECT_EQ(data.GetCartPageURLPatternForDomain("foo.com")->pattern(),
             "foo.com/([^/]+/)?cart");
-  ASSERT_EQ(data.GetCheckoutPageURLPatternForDomain("bar.com")->pattern(),
+  EXPECT_EQ(data.GetCheckoutPageURLPatternForDomain("bar.com")->pattern(),
             "bar.com/([^/]+/)?checkout");
-  ASSERT_EQ(data.GetPurchasePageURLPatternForDomain("baz.com")->pattern(),
+  EXPECT_EQ(data.GetPurchasePageURLPatternForDomain("baz.com")->pattern(),
             "baz.com/([^/]+/)?purchase");
 
   ASSERT_TRUE(data.PopulateDataFromComponent(
@@ -278,10 +279,57 @@ TEST_F(CommerceHeuristicsDataTest, TestRepopulateHintData) {
   )###",
       kGlobalHeuristicsJSONData, "", ""));
 
-  ASSERT_FALSE(data.GetCartPageURLPatternForDomain("foo.com"));
-  ASSERT_FALSE(data.GetCheckoutPageURLPatternForDomain("bar.com"));
-  ASSERT_FALSE(data.GetPurchasePageURLPatternForDomain("baz.com"));
-  ASSERT_EQ(data.GetPurchasePageURLPatternForDomain("qux.com")->pattern(),
+  EXPECT_FALSE(data.GetCartPageURLPatternForDomain("foo.com"));
+  EXPECT_FALSE(data.GetCheckoutPageURLPatternForDomain("bar.com"));
+  EXPECT_FALSE(data.GetPurchasePageURLPatternForDomain("baz.com"));
+  EXPECT_EQ(data.GetPurchasePageURLPatternForDomain("qux.com")->pattern(),
             "qux.com/([^/]+/)?purchase");
+}
+
+TEST_F(CommerceHeuristicsDataTest, TestGetHintHeuristicsJSONForDomain) {
+  auto& data = commerce_heuristics::CommerceHeuristicsData::GetInstance();
+  std::string hint_heuristics_string = R"###(
+      {
+          "foo.com":{
+              "merchant_name":"Foo"
+          },
+          "bar.com":{}
+      }
+  )###";
+  std::string foo_expected = R"###(
+      {
+          "foo.com":{
+              "merchant_name":"Foo"
+          }
+      }
+  )###";
+  foo_expected = base::CollapseWhitespaceASCII(foo_expected, true);
+
+  ASSERT_TRUE(
+      data.PopulateDataFromComponent(hint_heuristics_string, "{}", "", ""));
+
+  auto foo_heuristics =
+      commerce_heuristics::CommerceHeuristicsData::GetInstance()
+          .GetHintHeuristicsJSONForDomain("foo.com");
+  EXPECT_TRUE(foo_heuristics.has_value());
+  EXPECT_EQ(*foo_heuristics, foo_expected);
+  auto bar_heuristics =
+      commerce_heuristics::CommerceHeuristicsData::GetInstance()
+          .GetHintHeuristicsJSONForDomain("bar.com");
+  EXPECT_FALSE(bar_heuristics.has_value());
+}
+
+TEST_F(CommerceHeuristicsDataTest, TestGetGlobalHeuristicsJSON) {
+  auto& data = commerce_heuristics::CommerceHeuristicsData::GetInstance();
+  std::string global_heuristics_string = "{\"cart_pattern\" : \"foo\"}";
+
+  ASSERT_TRUE(
+      data.PopulateDataFromComponent("{}", global_heuristics_string, "", ""));
+
+  auto global_heuristics_optional =
+      commerce_heuristics::CommerceHeuristicsData::GetInstance()
+          .GetGlobalHeuristicsJSON();
+  EXPECT_TRUE(global_heuristics_optional.has_value());
+  EXPECT_EQ(*global_heuristics_optional, global_heuristics_string);
 }
 }  // namespace commerce_heuristics
