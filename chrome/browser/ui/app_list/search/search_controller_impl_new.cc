74451847f8dabfa28eefd1b3d81a339b17e3ad78
Amanda Deacon <amandadeacon@chromium.org>
2022-04-06 04:22:40 +0000

    [Launcher health] Move sorting lambdas into separate file.
    Part of a series of code health changes to the SearchControllerImplNew.
    See go/launcher-health for details.
    
    Test: none new; refactor only
    Bug: 1258415
    Change-Id: Ie8e10d4d441462398afdebe7cc909abdf620cd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3567850
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989272}

--- chrome/browser/ui/app_list/search/search_controller_impl_new.cc
+++ chrome/browser/ui/app_list/search/search_controller_impl_new.cc
@@ -29,6 +29,7 @@
 #include "chrome/browser/ui/app_list/search/common/string_util.h"
 #include "chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder.h"
 #include "chrome/browser/ui/app_list/search/ranking/ranker_delegate.h"
+#include "chrome/browser/ui/app_list/search/ranking/scoring.h"
 #include "chrome/browser/ui/app_list/search/ranking/util.h"
 #include "chrome/browser/ui/app_list/search/search_features.h"
 #include "chrome/browser/ui/app_list/search/search_metrics_observer.h"
@@ -347,27 +348,8 @@ void SearchControllerImplNew::Rank(ProviderType provider_type) {
 }
 
 void SearchControllerImplNew::Publish() {
-  // Sort categories first by burn-in iteration number, then by score.
-  std::sort(categories_.begin(), categories_.end(),
-            [](const auto& a, const auto& b) {
-              const int a_burnin = a.burnin_iteration;
-              const int b_burnin = b.burnin_iteration;
-              if (a.category == Category::kSearchAndAssistant ||
-                  b.category == Category::kSearchAndAssistant) {
-                // Special-case the search and assistant category, which should
-                // always be sorted last.
-                return b.category == Category::kSearchAndAssistant;
-              } else if (a_burnin != b_burnin) {
-                // Sort order: 0, 1, 2, 3, ... then -1.
-                // The effect of this is to sort by arrival order, with unseen
-                // categories ranked last.
-                // N.B. (a ^ b) < 0 checks for opposite sign.
-                return (a_burnin ^ b_burnin) < 0 ? a_burnin > b_burnin
-                                                 : a_burnin < b_burnin;
-              } else {
-                return a.score > b.score;
-              }
-            });
+  SortCategories(categories_);
+
   // Create a vector of category enums in display order.
   std::vector<Category> category_enums;
   for (const auto& category : categories_)
@@ -400,54 +382,7 @@ void SearchControllerImplNew::Publish() {
     }
   }
 
-  // TODO(crbug.com/1258415): Refactor this lambda to be a method on the Scoring
-  // struct.
-  std::sort(
-      all_results.begin(), all_results.end(),
-      [&](const ChromeSearchResult* a, const ChromeSearchResult* b) {
-        const int a_best_match_rank = a->scoring().best_match_rank;
-        const int b_best_match_rank = b->scoring().best_match_rank;
-        if (a_best_match_rank != b_best_match_rank) {
-          // First, sort by best match. All best matches are brought to
-          // the front of the list and ordered by best_match_rank.
-          //
-          // The following gives sort order:
-          //    0, 1, 2, ... (best matches) then -1 (non-best matches).
-          // N.B. (a ^ b) < 0 checks for opposite sign.
-          return (a_best_match_rank ^ b_best_match_rank) < 0
-                     ? a_best_match_rank > b_best_match_rank
-                     : a_best_match_rank < b_best_match_rank;
-        } else if (a_best_match_rank == -1 && a->category() != b->category()) {
-          // Next, sort by categories, except for within best match.
-          // |categories_| has been sorted above so the first category in
-          // |categories_| should be ranked more highly.
-          for (const auto& category : categories_) {
-            if (category.category == a->category()) {
-              return true;
-            } else if (category.category == b->category()) {
-              return false;
-            }
-          }
-          // Any category associated with a result should also be present
-          // in |categories_|.
-          NOTREACHED();
-          return false;
-        } else if (a->scoring().burnin_iteration !=
-                   b->scoring().burnin_iteration) {
-          // Next, sort by burn-in iteration number. This has no effect on
-          // results which arrive pre-burn-in. For post-burn-in results
-          // for a given category, later-arriving results are placed below
-          // earlier-arriving results.
-          // This happens before sorting on display_score, as a trade-off
-          // between ranking accuracy and UX pop-in mitigation.
-          return a->scoring().burnin_iteration < b->scoring().burnin_iteration;
-        } else if (a->scoring().continue_rank != b->scoring().continue_rank) {
-          return a->scoring().continue_rank > b->scoring().continue_rank;
-        } else {
-          // Lastly, sort by display score.
-          return a->display_score() > b->display_score();
-        }
-      });
+  SortResults(all_results, categories_);
 
   if (!observer_list_.empty()) {
     std::vector<const ChromeSearchResult*> observer_results;
