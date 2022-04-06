3ecb1b6dfc3e7dda83e3e9fdee781d3f1fd1112f
Chong Gu <chonggu@google.com>
2022-04-06 19:30:52 +0000

    [cast3p] Add cast_core_screenshot_test
    Remove info collection for cast core and add screenshot test instead.
    Add pexpect library to Chromium's .vpython3 file.
    Add an html page that facilitates drawing. This page will be served
    locally during the test run.
    
    Change-Id: I33388706b4fe10d4b63a2f0f5aaaf7af384f6fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3563532
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#989574}

--- chromecast/cast_core/test/cast_project_config.py
+++ chromecast/cast_core/test/cast_project_config.py
@@ -6,11 +6,12 @@ import os
 import sys
 
 CHROMIUM_SRC_DIR = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), '..', '..'))
+    os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 
 sys.path.append(os.path.join(CHROMIUM_SRC_DIR, 'tools', 'perf'))
 from chrome_telemetry_build import chromium_config
 
-CAST_TEST_DIR = os.path.join(CHROMIUM_SRC_DIR, 'chromecast', 'test')
+CAST_TEST_DIR = os.path.join(
+    CHROMIUM_SRC_DIR, 'chromecast', 'cast_core', 'test')
 CONFIG = chromium_config.ChromiumConfig(top_level_dir=CAST_TEST_DIR,
                                         benchmark_dirs=[CAST_TEST_DIR])
