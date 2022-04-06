f2b7cd54d04a05a30dc067b33d4e621ae7c2e074
Michael Wilson <mjwilson@chromium.org>
2022-04-06 05:44:39 +0000

    [WebAudio] Break ChannelMerger node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: Ic2b35dd441c9b2e382f8e44095d2bc788802e38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3572466
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989290}

--- third_party/blink/renderer/modules/webaudio/channel_merger_node.h
+++ third_party/blink/renderer/modules/webaudio/channel_merger_node.h
@@ -29,33 +29,14 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_CHANNEL_MERGER_NODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_CHANNEL_MERGER_NODE_H_
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
 
 class BaseAudioContext;
 class ChannelMergerOptions;
-
-class ChannelMergerHandler final : public AudioHandler {
- public:
-  static scoped_refptr<ChannelMergerHandler> Create(AudioNode&,
-                                                    float sample_rate,
-                                                    unsigned number_of_inputs);
-
-  void Process(uint32_t frames_to_process) override;
-  void SetChannelCount(unsigned, ExceptionState&) final;
-  void SetChannelCountMode(const String&, ExceptionState&) final;
-
-  double TailTime() const override { return 0; }
-  double LatencyTime() const override { return 0; }
-  bool RequiresTailProcessing() const final { return false; }
-
- private:
-  ChannelMergerHandler(AudioNode&,
-                       float sample_rate,
-                       unsigned number_of_inputs);
-};
+class ExceptionState;
 
 class ChannelMergerNode final : public AudioNode {
   DEFINE_WRAPPERTYPEINFO();
