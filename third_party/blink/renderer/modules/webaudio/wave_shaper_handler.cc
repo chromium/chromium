d972d76a9ef92004ab3ce90bd5532ae745d0ab77
Michael Wilson <mjwilson@chromium.org>
2022-04-06 04:33:46 +0000

    [WebAudio] Break WaveShaper node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: I2d3136bcd45dbe15169c6f460cc4e4b7607ce164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3571103
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#989274}

---
+++ third_party/blink/renderer/modules/webaudio/wave_shaper_handler.cc
@@ -0,0 +1,32 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webaudio/wave_shaper_handler.h"
+
+#include <memory>
+
+#include "third_party/blink/renderer/modules/webaudio/audio_node.h"
+#include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
+#include "third_party/blink/renderer/modules/webaudio/wave_shaper_processor.h"
+
+namespace blink {
+
+WaveShaperHandler::WaveShaperHandler(AudioNode& node, float sample_rate)
+    : AudioBasicProcessorHandler(
+          kNodeTypeWaveShaper,
+          node,
+          sample_rate,
+          std::make_unique<WaveShaperProcessor>(
+              sample_rate,
+              1,
+              node.context()->GetDeferredTaskHandler().RenderQuantumFrames())) {
+  Initialize();
+}
+
+scoped_refptr<WaveShaperHandler> WaveShaperHandler::Create(AudioNode& node,
+                                                           float sample_rate) {
+  return base::AdoptRef(new WaveShaperHandler(node, sample_rate));
+}
+
+}  // namespace blink
--- third_party/blink/renderer/modules/webaudio/wave_shaper_node.cc
+++ third_party/blink/renderer/modules/webaudio/wave_shaper_node.cc
@@ -25,32 +25,15 @@
 
 #include "third_party/blink/renderer/modules/webaudio/wave_shaper_node.h"
 
-#include <memory>
-
 #include "third_party/blink/renderer/bindings/modules/v8/v8_wave_shaper_options.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_graph_tracer.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
+#include "third_party/blink/renderer/modules/webaudio/wave_shaper_handler.h"
+#include "third_party/blink/renderer/modules/webaudio/wave_shaper_processor.h"
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
 namespace blink {
-
-WaveShaperHandler::WaveShaperHandler(AudioNode& node, float sample_rate)
-    : AudioBasicProcessorHandler(
-          kNodeTypeWaveShaper,
-          node,
-          sample_rate,
-          std::make_unique<WaveShaperProcessor>(
-              sample_rate,
-              1,
-              node.context()->GetDeferredTaskHandler().RenderQuantumFrames())) {
-  Initialize();
-}
-
-scoped_refptr<WaveShaperHandler> WaveShaperHandler::Create(AudioNode& node,
-                                                           float sample_rate) {
-  return base::AdoptRef(new WaveShaperHandler(node, sample_rate));
-}
 
 WaveShaperNode::WaveShaperNode(BaseAudioContext& context) : AudioNode(context) {
   SetHandler(WaveShaperHandler::Create(*this, context.sampleRate()));
