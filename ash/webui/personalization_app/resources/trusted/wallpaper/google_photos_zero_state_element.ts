abff36b77578a3d5c8f89407635f1f79ef281f48
Angus L. M. McLean IV <angusmclean@chromium.org>
2022-04-07 17:59:21 +0000

    personalization: Fix no images icon resource for dark/light mode
    Replaces the svg with an updated one (thanks elizabethchiu@) for
    dark/light mode, so that visibility is better in both cases. This asset
    is also designed with the size of the image in the spec in mind, instead
    of borrowing one that was designed for a smaller size.
    
    Before dark: https://screenshot.googleplex.com/6h7UtqYtZoWm2P7
    Before light: https://screenshot.googleplex.com/8vYzRTwBuHq4iMX
    After dark: https://screenshot.googleplex.com/8MuZBLc7K7kgDo5
    After light: https://screenshot.googleplex.com/6GvsxnQ7Sekhc5C
    
    
    Bug: b:206812418
    Change-Id: Ib824b2a6bebeb1675198a8a4d759ec63e7320dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540256
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Angus McLean <angusmclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990013}

--- ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_zero_state_element.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_zero_state_element.ts
@@ -22,6 +22,18 @@ export class GooglePhotosZeroState extends WithPersonalizationStore {
     return getTemplate();
   }
 
+  static get properties() {
+    return {
+      isDarkModeActive_: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+
+  /** Whether the page is being rendered in dark mode. */
+  private isDarkModeActive_: boolean;
+
   /** Returns the message to be displayed. */
   private getMessage_(): string {
     return this.i18nAdvanced('googlePhotosZeroStateMessage', {
@@ -29,6 +41,12 @@ export class GooglePhotosZeroState extends WithPersonalizationStore {
         '<a target="_blank" href="https://photos.google.com">photos.google.com</a>'
       ]
     });
+  }
+
+  private getImageSource_(): string {
+    return this.isDarkModeActive_ ?
+        'chrome://personalization/common/no_google_photos_images_dark.svg' :
+        'chrome://personalization/common/no_google_photos_images.svg';
   }
 }
 
