5255bb4799e890f0f2ea18be29b33d7aedc3a0a1
Kristi Park <kristipark@google.com>
2022-04-07 17:18:16 +0000

    Revert "capture-selfie-cam: Show resize button conditionally"
    This reverts commit 187b3b7bd42defaeafd03e759bef66765c72ce51.
    
    Reason for revert: Suspected test failures in Linux ChromiumOS MSan Tests (https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/27254/test-results)
    
    Original change's description:
    > capture-selfie-cam: Show resize button conditionally
    >
    > Per UX specs mentioned in crbug.com/1308919, the resize button should be
    > hidden by default and only show on mouse hover or tap. When the mouse
    > exits the preview widget or after tapping on the preview widget, the
    > resize button will stay visible for a predefined duration i.e. 4.5s and
    > gets hidden. Animation is added in the appear and disappear process to
    > show the fading effects.
    > Demo video:
    > https://bugs.chromium.org/p/chromium/issues/detail?id=1308919#c5
    >
    > Follow-up CL:
    > The resize button should always show when switch access is enabled.
    > (Need to get PM and UX's feedback)
    >
    > Bug: 1308919
    > Test: Added tests + manually test
    >
    > Change-Id: Ib38e114696a1b20aeaf9d82afd636cbc5ec5b55b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3561708
    > Reviewed-by: Min Chen <minch@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Michele Fan <michelefan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#989619}
    
    Bug: 1308919
    Change-Id: Ibabcde5c8754dd04c575753a09ff597a1fea2956
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576536
    Owners-Override: Kristi Park <kristipark@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kristi Park <kristipark@google.com>
    Cr-Commit-Position: refs/heads/main@{#989989}

--- ash/capture_mode/capture_mode_constants.h
+++ ash/capture_mode/capture_mode_constants.h
@@ -5,7 +5,6 @@
 #ifndef ASH_CAPTURE_MODE_CAPTURE_MODE_CONSTANTS_H_
 #define ASH_CAPTURE_MODE_CAPTURE_MODE_CONSTANTS_H_
 
-#include "base/time/time.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/size.h"
@@ -47,10 +46,6 @@ constexpr int kSpaceBetweenCameraPreviewAndEdges = 16;
 // The space between the bottom of camera preview resize button and the bottom
 // of the camera preview.
 constexpr int kSpaceBetweenResizeButtonAndCameraPreview = 12;
-
-// The duration to continue showing resize button since the mouse exiting the
-// preview bounds or the last tap on the preview widget.
-constexpr base::TimeDelta kResizeButtonShowDuration = base::Milliseconds(4500);
 
 }  // namespace capture_mode
 
