5bb22ded0de9f7bee58e48f657b6c29b4c104cc9
Yuwei Huang <yuweih@chromium.org>
2022-04-07 23:52:23 +0000

    Reland "[remoting host] Implement RemoteWebAuthnExtensionNotifier"
    This is a reland of commit 8fecb333d0e2fac06e64922a73eafff7b28d41b6
    
    Fixed broken unit tests that call CreateUniqueTempDir() inside
    DCHECK(), which gets stripped off in non-debug builds, by simply
    replacing DCHECK()s with EXPECT_TRUE()s.
    
    Original change's description:
    > [remoting host] Implement RemoteWebAuthnExtensionNotifier
    >
    > This CL provides implementation for the RemoteWebAuthnExtensionNotifier
    > class, which is based on the webAuthenticationProxy
    > onRemoteSessionStateChange API introduced in crrev.com/c/3514116. The
    > gist is that Chrome will watch for changes of some files in a specific
    > directory, and whenever these files are changed, the
    > onRemoteSessionStateChange event will be triggered on the Chrome
    > extension.
    >
    > Bug: 1225870
    > Change-Id: I7e8ea5c58c18a4525e7e2be4f60adb5ec7558b5a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3566521
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#990074}
    
    Bug: 1225870
    Change-Id: If43ecdb489b3e3d95a8ee487c8b70ae14d5b99ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576893
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990190}

--- remoting/host/webauthn/remote_webauthn_extension_notifier.cc
+++ remoting/host/webauthn/remote_webauthn_extension_notifier.cc
@@ -4,19 +4,202 @@
 
 #include "remoting/host/webauthn/remote_webauthn_extension_notifier.h"
 
+#include <vector>
+
+#include "base/base_paths.h"
+#include "base/bind.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/location.h"
+#include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
+#include "base/path_service.h"
+#include "base/sequence_checker.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "build/build_config.h"
 
 namespace remoting {
+namespace {
 
-RemoteWebAuthnExtensionNotifier::RemoteWebAuthnExtensionNotifier() = default;
+// Returns a list of directories that different Chrome channels might use to
+// watch for file changes for firing the onRemoteSessionStateChange event on the
+// extension.
+//
+// The remote state change directory is located at:
+//
+//   REMOTE_STATE_CHANGE_DIRECTORY =
+//     $DEFAULT_UDD/WebAuthenticationProxyRemoteSessionStateChange
+//
+// And the file for firing the event on the extension is located at:
+//
+//   $REMOTE_STATE_CHANGE_DIRECTORY/$EXTENSION_ID
+//
+// DEFAULT_UDD (default user data directory) is documented here:
+//
+//   https://chromium.googlesource.com/chromium/src/+/master/docs/user_data_dir.md#default-location
+//
+// Note that the default UDD is always used, and UDD overrides from browser
+// launch args or env vars are ignored so the path is more discoverable by the
+// native process.
+//
+// Caller should check if the directory exists before writing files to it. A
+// directory only exists if the corresponding Chrome version is installed.
+std::vector<base::FilePath> GetRemoteStateChangeDirPaths() {
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+  constexpr base::FilePath::CharType kStateChangeDirName[] =
+      FILE_PATH_LITERAL("WebAuthenticationProxyRemoteSessionStateChange");
+#endif
 
-RemoteWebAuthnExtensionNotifier::~RemoteWebAuthnExtensionNotifier() = default;
+  std::vector<base::FilePath> dirs;
+
+#if BUILDFLAG(IS_LINUX)
+  base::FilePath base_path;
+  if (!base::PathService::Get(base::DIR_HOME, &base_path)) {
+    LOG(ERROR) << "Failed to get local app data dir";
+    return dirs;
+  }
+  base_path = base_path.Append(".config");
+  dirs.push_back(base_path.Append("google-chrome").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path.Append("google-chrome-beta").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path.Append("google-chrome-unstable").Append(kStateChangeDirName));
+  dirs.push_back(base_path.Append("chromium").Append(kStateChangeDirName));
+#elif BUILDFLAG(IS_WIN)
+  base::FilePath base_path;
+  if (!base::PathService::Get(base::DIR_LOCAL_APP_DATA, &base_path)) {
+    LOG(ERROR) << "Failed to get local app data dir";
+    return dirs;
+  }
+  base::FilePath base_path_google = base_path.Append(L"Google");
+  dirs.push_back(
+      base_path_google.Append(L"Chrome").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path_google.Append(L"Chrome Beta").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path_google.Append(L"Chrome Dev").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path_google.Append(L"Chrome SxS").Append(kStateChangeDirName));
+  dirs.push_back(base_path.Append(L"Chromium").Append(kStateChangeDirName));
+#elif BUILDFLAG(IS_MAC)
+  base::FilePath base_path;
+  if (!base::PathService::Get(base::DIR_APP_DATA, &base_path)) {
+    LOG(ERROR) << "Failed to get app data dir";
+    return dirs;
+  }
+  base::FilePath base_path_google = base_path.Append("Google");
+  dirs.push_back(base_path_google.Append("Chrome").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path_google.Append("Chrome Beta").Append(kStateChangeDirName));
+  dirs.push_back(
+      base_path_google.Append("Chrome Canary").Append(kStateChangeDirName));
+  dirs.push_back(base_path.Append("Chromium").Append(kStateChangeDirName));
+#else
+  NOTIMPLEMENTED();
+#endif
+  return dirs;
+}
+
+}  // namespace
+
+#if defined(NDEBUG)
+// Prod extension ID
+const base::FilePath::CharType
+    RemoteWebAuthnExtensionNotifier::kRemoteWebAuthnExtensionId[] =
+        FILE_PATH_LITERAL("inomeogfingihgjfjlpeplalcfajhgai");
+#else
+// Dev extension ID
+const base::FilePath::CharType
+    RemoteWebAuthnExtensionNotifier::kRemoteWebAuthnExtensionId[] =
+        FILE_PATH_LITERAL("hpodccmdligbeohchckkeajbfohibipg");
+#endif
+
+// Core class for writing wakeup files on the IO sequence. Must be used and
+// deleted on the same sequence.
+class RemoteWebAuthnExtensionNotifier::Core final {
+ public:
+  explicit Core(std::vector<base::FilePath> remote_state_change_dirs);
+  ~Core();
+
+  void WakeUpExtension();
+
+ private:
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  std::vector<base::FilePath> remote_state_change_dirs_;
+  base::WeakPtrFactory<Core> weak_factory_{this};
+};
+
+RemoteWebAuthnExtensionNotifier::Core::Core(
+    std::vector<base::FilePath> remote_state_change_dirs)
+    : remote_state_change_dirs_(std::move(remote_state_change_dirs)) {}
+
+RemoteWebAuthnExtensionNotifier::Core::~Core() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+void RemoteWebAuthnExtensionNotifier::Core::WakeUpExtension() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  for (const base::FilePath& dir : remote_state_change_dirs_) {
+    if (!base::DirectoryExists(dir)) {
+      VLOG(1) << "Ignored non-directory path: " << dir;
+      continue;
+    }
+    auto file_path = dir.Append(kRemoteWebAuthnExtensionId);
+    VLOG(1) << "Writing extension wakeup file: " << file_path;
+    // Content of file doesn't matter so we just write an empty string.
+    base::WriteFile(file_path, "");
+  }
+}
+
+RemoteWebAuthnExtensionNotifier::RemoteWebAuthnExtensionNotifier()
+    : RemoteWebAuthnExtensionNotifier(
+          GetRemoteStateChangeDirPaths(),
+          base::ThreadPool::CreateSequencedTaskRunner(
+              {base::MayBlock(), base::WithBaseSyncPrimitives()})) {}
+
+RemoteWebAuthnExtensionNotifier::RemoteWebAuthnExtensionNotifier(
+    std::vector<base::FilePath> remote_state_change_dirs,
+    scoped_refptr<base::SequencedTaskRunner> io_task_runner) {
+  core_ = base::SequenceBound<Core>(io_task_runner,
+                                    std::move(remote_state_change_dirs));
+}
+
+RemoteWebAuthnExtensionNotifier::~RemoteWebAuthnExtensionNotifier() {
+  if (is_wake_up_scheduled_) {
+    // The scheduled RemoteWebAuthnExtensionNotifier::WakeUpExtension will not
+    // be executed since |weak_factory_| will soon be invalidated, so we just
+    // async-call Core::WakeUpExtension here. It is scheduled before the
+    // deletion of |core_| on the IO sequence, so it is guaranteed to be
+    // executed.
+    core_.AsyncCall(&Core::WakeUpExtension);
+  }
+}
 
 void RemoteWebAuthnExtensionNotifier::NotifyStateChange() {
-  // TODO(yuweih): Replace with real implementation. Also NotifyStateChange()
-  // might get called multiple times in the same turn of the message loop, so
-  // the calls should be deduplicated by posting a task to the task runner.
-  NOTIMPLEMENTED();
+  // NotifyStateChange might be called multiple times in the same turn of the
+  // message loop, so we deduplicate by posting a task to the current task
+  // runner, which posts another to execute Core::WakeUpExtension on the IO
+  // sequence.
+  if (is_wake_up_scheduled_) {
+    return;
+  }
+  is_wake_up_scheduled_ = true;
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&RemoteWebAuthnExtensionNotifier::WakeUpExtension,
+                     weak_factory_.GetWeakPtr()));
+}
+
+void RemoteWebAuthnExtensionNotifier::WakeUpExtension() {
+  is_wake_up_scheduled_ = false;
+
+  core_.AsyncCall(&Core::WakeUpExtension);
 }
 
 }  // namespace remoting
