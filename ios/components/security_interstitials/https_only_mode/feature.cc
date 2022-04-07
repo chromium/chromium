d6c0327790c0058d5e03449bd2dd02562180db8b
Mustafa Emre Acer <meacer@chromium.org>
2022-04-07 20:26:13 +0000

    HTTPS-Only Mode: Add feature flag for iOS
    This CL adds the feature flag to for HTTPS-Only mode for iOS. The
    feature will roll out via waterfall and not through Finch. As a result,
    we don't want to share the feature flag with desktop as the desktop flag
    is already enabled.
    
    Bug: 1302509
    Change-Id: I0d647066e4a39670c05e6ee3de7b082c5a1b44df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3576003
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#990092}

---
+++ ios/components/security_interstitials/https_only_mode/feature.cc
@@ -0,0 +1,14 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/components/security_interstitials/https_only_mode/feature.h"
+
+namespace security_interstitials {
+namespace features {
+
+const base::Feature kHttpsOnlyMode{"HttpsOnlyMode",
+                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
+}  // namespace security_interstitials
