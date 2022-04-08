890225e692243e5c112ad3c08676d411a0b46750
Ken Rockot <rockot@google.com>
2022-04-08 19:28:43 +0000

    ipcz: Build adaptation for Chromium vs standalone
    Split out from the ipcz uber-CL: https://crrev.com/c/3570271
    
    Defines the ipcz_source_set() GN template, which for a target "foo"
    emits two nearly identical source_sets: "foo_chromium" and
    "foo_standalone". The foo_chromium target depends on //base and
    can only be generated in Chromium builds. The foo_standalone
    target has no Chromium dependencies, but instead defines
    IPCZ_STANDALONE and depends on some polyfills for base
    functionality defined in ipcz/src/standalone.
    
    This also adds a basic ipcz_source_set("util") for util/log.h
    and util/stack_trace.h, for portable equivalents of base/logging.h
    and base/debug/stack_trace.h. The util_chromium target uses the
    existing //base implementations, while the util_standalone target
    uses ipcz polyfill.
    
    A simple compile/smoke test exercises both logging and stack trace
    features.
    
    Finally, the Chromium variant of the test suite is linked into the
    existing //ipc:ipc_tests target for immediate CI coverage. The
    standalone variant is also still compiled after this CL, by virtue
    of an existing dependency from //mojo:tests onto ipcz_tests. This
    will be left in place only for compile coverage.
    
    Bug: 1299283
    Change-Id: I2cf550599cdef44d3e8b280d9fc8066bd163619e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573141
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#990528}

--- third_party/ipcz/src/test/run_all_tests.cc
+++ third_party/ipcz/src/test/run_all_tests.cc
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "standalone/base/logging.h"
+#include "standalone/base/stack_trace.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 int main(int argc, char** argv) {
+  // TODO(rockot): Implement basic command line parsing for standalone builds.
+  ipcz::standalone::StackTrace::EnableStackTraceSymbolization(argv[0]);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
