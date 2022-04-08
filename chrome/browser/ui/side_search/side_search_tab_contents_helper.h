e7960c9f91a4dc2f3c1c8ba4f2f70f2c732b1c0f
Thomas Lukaszewicz <tluk@chromium.org>
2022-04-08 22:13:19 +0000

    Revert "[side search] Defer the service check until after first paint"
    This reverts commit 7dda913fdf3f59ec011afbd96f6e3cfc813351a4.
    
    Reason for revert:
    Patch does not address first contentful paint regression
    
    Original change's description:
    > [side search] Defer the service check until after first paint
    >
    > This CL moves the side search service check request to happen
    > after the tab contents has performed its first non-empty paint
    > rather than immediately after it has committed the navigation.
    >
    > Typically the page will commit before painting begins and the
    > overhead imposed by the service check results in a regression
    > in page paint times.
    >
    > By deferring the service check to after the first non-empty
    > paint we ensure that the service check does not interfere with
    > initial paint timings. Follow up CLs will look at moving the
    > service check to an appropriate time when the browser is
    > idle to avoid resource contention where possible.
    >
    > Bug: 1314612, 1307155
    > Change-Id: I5b51bfcd4a3ec3e26664f760efe121e8bb3541a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578279
    > Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#990537}
    
    Bug: 1314612, 1307155
    Change-Id: I66fa865ba9f3e957d699ac83c9b91e59ca99e16b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579728
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#990616}

--- chrome/browser/ui/side_search/side_search_tab_contents_helper.h
+++ chrome/browser/ui/side_search/side_search_tab_contents_helper.h
@@ -77,7 +77,6 @@ class SideSearchTabContentsHelper
       content::NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
-  void DidFirstVisuallyNonEmptyPaint() override;
 
   // SideSearchConfig::Observer:
   void OnSideSearchConfigChanged() override;
