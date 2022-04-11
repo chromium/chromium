16d8476b44473d6c2b85a821d709bebc102306eb
Michael Wilson <mjwilson@chromium.org>
2022-04-11 22:50:10 +0000

    [WebAudio] Break Gain node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Because this change was smaller and more independent, I spent the time
    to fix the #include statements as well.
    
    Change-Id: I20791cd71948289ba24ae94580036addb8d0b0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573208
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991247}

--- third_party/blink/renderer/modules/webaudio/gain_node.h
+++ third_party/blink/renderer/modules/webaudio/gain_node.h
@@ -26,47 +26,19 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_GAIN_NODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBAUDIO_GAIN_NODE_H_
 
-#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_param.h"
-#include "third_party/blink/renderer/platform/wtf/threading.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
 
 class BaseAudioContext;
 class GainOptions;
+class ExceptionState;
 
 // GainNode is an AudioNode with one input and one output which applies a gain
 // (volume) change to the audio signal.  De-zippering (smoothing) is applied
 // when the gain value is changed dynamically.
-
-class GainHandler final : public AudioHandler {
- public:
-  static scoped_refptr<GainHandler> Create(AudioNode&,
-                                           float sample_rate,
-                                           AudioParamHandler& gain);
-
-  // AudioHandler
-  void Process(uint32_t frames_to_process) override;
-  void ProcessOnlyAudioParams(uint32_t frames_to_process) override;
-
-  // Called in the main thread when the number of channels for the input may
-  // have changed.
-  void CheckNumberOfChannelsForInput(AudioNodeInput*) override;
-
-  // AudioNode
-  double TailTime() const override { return 0; }
-  double LatencyTime() const override { return 0; }
-  bool RequiresTailProcessing() const final { return false; }
-
- private:
-  GainHandler(AudioNode&, float sample_rate, AudioParamHandler& gain);
-
-  scoped_refptr<AudioParamHandler> gain_;
-
-  AudioFloatArray sample_accurate_gain_values_;
-};
-
 class GainNode final : public AudioNode {
   DEFINE_WRAPPERTYPEINFO();
 
