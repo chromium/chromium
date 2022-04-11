754b58d33af976667b09d754e6e8c46c0c1dcda0
Brian Salomon <bsalomon@google.com>
2022-04-11 16:59:31 +0000

    Suppress Skia backdrop filter fix for rebaselining.
    Revert and rebase after this change rolls into Chrome:
    https://skia-review.googlesource.com/c/skia/+/528652
    
    Bug: 1313579
    Change-Id: Iab37c62a2c338ead258fb2f9960b89987c92c118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577185
    Auto-Submit: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/main@{#991067}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -231,6 +231,9 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_LEGACY_INNER_JOINS
 
+// crbug.com/1313579
+#define SK_DISABLE_SKIF_TOLERANCE_ROUND
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
