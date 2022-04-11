daaa983037ac0cb4a770318de1a4490fccae011b
Mike Dougherty <michaeldo@chromium.org>
2022-04-11 22:20:45 +0000

    Allow relative paths in third_party JavaScript
    Update the rollup plugin path resolver to allow relative paths in third
    party scripts. This is necessary as the content of these scripts may not
    be within our control.
    
    Bug: 1313654
    Change-Id: Id31ddb157501d831ec3f8dfc47ab589792909e88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581538
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991229}

--- ios/web/public/js_messaging/rollup_plugin_src_path_resolver.js
+++ ios/web/public/js_messaging/rollup_plugin_src_path_resolver.js
@@ -18,14 +18,23 @@ export default function src_path_resolver(rootPath) {
         return null;
       }
 
-      if (!source.startsWith('//')) {
-        this.error(
-            `Invalid path, imports must be absolute from src root and start ` +
-            `with '//'. Failed path: '${source}'`);
+      if (source.startsWith('//')) {
+        // Strip leading double slashes and combine path with root.
+        return path.join(rootPath, source.substr(2));
       }
 
-      // Strip leading double slashes and combine path with root.
-      return path.join(rootPath, source.substr(2));
+      // Relative paths are acceptable from within third-party scripts.
+      if (!source.startsWith('/')) {
+        let fullPath = path.join(path.dirname(originScript), source);
+        let relativeToRoot = path.relative(rootPath, fullPath);
+        if (relativeToRoot.startsWith('third_party')) {
+          return fullPath;
+        }
+      }
+
+      this.error(
+          `Invalid path, imports must be absolute from src root and start ` +
+          `with '//'. Failed path: '${source}'`);
     },
   };
 }
