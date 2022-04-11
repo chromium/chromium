6580be161eb07d2773847c1ceebd8b376e9729e0
Yeunjoo Choi <ychoi@igalia.com>
2022-04-11 01:17:44 +0000

    [mparch] Remove TODO in NewTabPageUI::DidStartNavigation
    This CL removes TODO for mparch in NewTabPageUI::DidStartNavigation
    which logs the time for primary page navigation. Plus, there seems to
    be no use of mparch features in the new tab page for now.
    
    Bug: 1218946
    Change-Id: I1ed97bbca37a1158f39cb8b0937aa663952e6042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3570847
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#990850}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -752,9 +752,6 @@ void NewTabPageUI::OnNtpCustomBackgroundServiceShuttingDown() {
 
 void NewTabPageUI::DidStartNavigation(
     content::NavigationHandle* navigation_handle) {
-  // TODO(https://crbug.com/1218946): With MPArch there may be multiple main
-  // frames. This caller was converted automatically to the primary main frame
-  // to preserve its semantics. Follow up to confirm correctness.
   if (navigation_handle->IsInPrimaryMainFrame() &&
       navigation_handle->GetURL() == GURL(chrome::kChromeUINewTabPageURL)) {
     navigation_start_time_ = base::Time::Now();
