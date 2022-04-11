15abbd13a91d0de1f53c739080c2ca40051e3869
Tom Lukaszewicz <tluk@chromium.org>
2022-04-11 19:42:22 +0000

    [M101 Merge][side search] Extend the duration the page action icon label is shown
    This CL extends the duration that the side search icon view's label
    is shown after it animates in to 5s (up from 1.8s).
    
    This extension gives users sufficient time to be able to notice and
    read the presented text. Current timings pose an accessibility issue
    as most users will not be able to notice and read the label text
    before the label disappears.
    
    See demo below:
    https://screencast.googleplex.com/cast/NjM0MjQ4MTIwMjM4MDgwMHxkMmJlMzUzNy1mZA
    
    (cherry picked from commit bdcde31d35d75de945cf2eedb0494b5eb2e09498)
    
    Bug: 1311454
    Change-Id: I05b7e2e36c1e288c9bd106ad3a78329dc98e3632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3573700
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#989804}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582359
    Cr-Commit-Position: refs/branch-heads/4951@{#652}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- chrome/browser/ui/views/side_search/side_search_icon_view.h
+++ chrome/browser/ui/views/side_search/side_search_icon_view.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_UI_VIEWS_SIDE_SEARCH_SIDE_SEARCH_ICON_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "chrome/browser/ui/views/side_search/default_search_icon_source.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -34,12 +35,22 @@ class SideSearchIconView : public PageActionIconView {
   const gfx::VectorIcon& GetVectorIcon() const override;
   ui::ImageModel GetSizedIconImage(int size) const override;
   std::u16string GetTextForTooltipAndAccessibleName() const override;
+  void AnimationProgressed(const gfx::Animation* animation) override;
 
  private:
   raw_ptr<Browser> browser_ = nullptr;
 
   // Source for the default search icon image used by this page action.
   DefaultSearchIconSource default_search_icon_source_;
+
+  // Animates out the side search icon label after a fixed period of time. This
+  // keeps the label visible for long enough to give users an opportunity to
+  // read the label text.
+  base::OneShotTimer animate_out_timer_;
+
+  // Boolean that tracks whether we should extend the duration for which the
+  // label is shown when it animates in.
+  bool should_extend_label_shown_duration_ = false;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_SIDE_SEARCH_SIDE_SEARCH_ICON_VIEW_H_
