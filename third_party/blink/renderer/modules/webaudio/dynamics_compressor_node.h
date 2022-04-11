4b9b99dca3d0da09ef018e4ce3f6155b6abaab8d
Michael Wilson <mjwilson@chromium.org>
2022-04-11 22:54:05 +0000

    [WebAudio] Break DynamicsCompressor node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Change-Id: I924d3af4bc1ef1009386692a715d7a81c3af529b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578252
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991250}

--- third_party/blink/renderer/modules/webaudio/dynamics_compressor_node.h
+++ third_party/blink/renderer/modules/webaudio/dynamics_compressor_node.h
@@ -28,64 +28,18 @@
 
 #include <atomic>
 #include <memory>
+
 #include "base/gtest_prod_util.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_param.h"
+#include "third_party/blink/renderer/modules/webaudio/dynamics_compressor_handler.h"
 
 namespace blink {
 
 class BaseAudioContext;
 class DynamicsCompressor;
 class DynamicsCompressorOptions;
-
-class MODULES_EXPORT DynamicsCompressorHandler final : public AudioHandler {
- public:
-  static scoped_refptr<DynamicsCompressorHandler> Create(
-      AudioNode&,
-      float sample_rate,
-      AudioParamHandler& threshold,
-      AudioParamHandler& knee,
-      AudioParamHandler& ratio,
-      AudioParamHandler& attack,
-      AudioParamHandler& release);
-
-  ~DynamicsCompressorHandler() override;
-
-  // AudioHandler
-  void Process(uint32_t frames_to_process) override;
-  void ProcessOnlyAudioParams(uint32_t frames_to_process) override;
-  void Initialize() override;
-
-  float ReductionValue() const {
-    return reduction_.load(std::memory_order_relaxed);
-  }
-
-  void SetChannelCount(unsigned, ExceptionState&) final;
-  void SetChannelCountMode(const String&, ExceptionState&) final;
-
- private:
-  DynamicsCompressorHandler(AudioNode&,
-                            float sample_rate,
-                            AudioParamHandler& threshold,
-                            AudioParamHandler& knee,
-                            AudioParamHandler& ratio,
-                            AudioParamHandler& attack,
-                            AudioParamHandler& release);
-  bool RequiresTailProcessing() const final;
-  double TailTime() const override;
-  double LatencyTime() const override;
-
-  std::unique_ptr<DynamicsCompressor> dynamics_compressor_;
-  scoped_refptr<AudioParamHandler> threshold_;
-  scoped_refptr<AudioParamHandler> knee_;
-  scoped_refptr<AudioParamHandler> ratio_;
-  std::atomic<float> reduction_;
-  scoped_refptr<AudioParamHandler> attack_;
-  scoped_refptr<AudioParamHandler> release_;
-
-  FRIEND_TEST_ALL_PREFIXES(DynamicsCompressorNodeTest, ProcessorLifetime);
-};
 
 class MODULES_EXPORT DynamicsCompressorNode final : public AudioNode {
   DEFINE_WRAPPERTYPEINFO();
