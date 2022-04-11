80ff8e0d8877f493086ee6b07d95e504be508fec
Tommy Martino <tmartino@google.com>
2022-04-11 21:49:53 +0000

    [SH iOS AMP] Disable SH AMP by default on iOS
    Bug: 1313162
    Change-Id: I362cfd7423aa967d069a3074442497359feadab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582817
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#991211}

--- components/shared_highlighting/core/common/shared_highlighting_features.cc
+++ components/shared_highlighting/core/common/shared_highlighting_features.cc
@@ -5,6 +5,7 @@
 #include "components/shared_highlighting/core/common/shared_highlighting_features.h"
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace shared_highlighting {
 
@@ -16,8 +17,14 @@ constexpr base::FeatureParam<int> kPreemptiveLinkGenTimeoutLengthMs{
 const base::Feature kSharedHighlightingV2{"SharedHighlightingV2",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kSharedHighlightingAmp{"SharedHighlightingAmp",
-                                           base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kSharedHighlightingAmp {
+  "SharedHighlightingAmp",
+#if BUILDFLAG(IS_IOS)
+      base::FEATURE_DISABLED_BY_DEFAULT
+#else
+      base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+};
 
 const base::Feature kIOSSharedHighlightingV2{"IOSSharedHighlightingV2",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
