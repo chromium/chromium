48cca3285af6d1c68d68e72fe16f4037cf01596c
Mohamed Heikal <mheikal@chromium.org>
2022-04-12 18:51:31 +0000

    Add presubmit that prevents deps on //base or //third_party from //build
    Other embedders of //build like v8, webrtc and angle do not checkout
    base and manually add new //third_party deps to their repo. Adding a new
    dep from //build to either of those prefixes can break them.
    
    Change-Id: I071af22ee717516aa02e4765a56986c323c9b8fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583895
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991643}

---
+++ build/PRESUBMIT.py
@@ -0,0 +1,45 @@
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+PRESUBMIT_VERSION = '2.0.0'
+
+# This line is 'magic' in that git-cl looks for it to decide whether to
+# use Python3 instead of Python2 when running the code in this file.
+USE_PYTHON3 = True
+
+import textwrap
+
+
+def CheckNoBadDeps(input_api, output_api):
+  """Prevent additions of bad dependencies from the //build prefix."""
+  build_file_patterns = [
+      r'(.+/)?BUILD\.gn',
+      r'.+\.gni',
+  ]
+  bad_pattern = input_api.re.compile(r'^[^#]*//(base|third_party|components)')
+
+  warning_message = textwrap.dedent("""
+      The //build directory is meant to be as hermetic as possible so that
+      other projects (webrtc, v8, angle) can make use of it. If you are adding
+      a new dep from //build onto another directory, you should consider:
+      1) Can that dep live within //build?
+      2) Can the dep be guarded by "build_with_chromium"?
+      3) Have you made this new dep easy to pull in for other projects (ideally
+      a matter of adding a DEPS entry).:""")
+
+  def FilterFile(affected_file):
+    return input_api.FilterSourceFile(affected_file,
+                                      files_to_check=build_file_patterns)
+
+  problems = []
+  for f in input_api.AffectedSourceFiles(FilterFile):
+    local_path = f.LocalPath()
+    for line_number, line in f.ChangedContents():
+      if (bad_pattern.search(line)):
+        problems.append('%s:%d\n    %s' %
+                        (local_path, line_number, line.strip()))
+  if problems:
+    return [output_api.PresubmitPromptOrNotify(warning_message, problems)]
+  else:
+    return []
