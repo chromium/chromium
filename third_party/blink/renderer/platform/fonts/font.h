006fdc7b2e2841e39b8fe34f7cecdc6c7bfe83a4
Yoshifumi Inoue <yosin@chromium.org>
2022-04-12 08:34:35 +0000

    [parallel-text-shaping] Introduce CrossThreadCopier<Font>
    This patch introduces `CrossThreadCopier<Font>` to pass `Font` as
    arguments to `PostCrossThread` for prepration of parallel text
    shaping[1].
    
    [1] http://crrev.com/c/3211416 Parallel Text Shaping
    
    Bug: 1264280
    Change-Id: I37087a60c705756433c86c4243727de57dcc5d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578206
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991412}

--- third_party/blink/renderer/platform/fonts/font.h
+++ third_party/blink/renderer/platform/fonts/font.h
@@ -36,6 +36,7 @@
 #include "third_party/blink/renderer/platform/text/tab_size.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_copier.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
@@ -276,5 +277,15 @@ inline float Font::TabWidth(const SimpleFontData* font_data,
 }
 
 }  // namespace blink
+
+namespace WTF {
+
+template <>
+struct CrossThreadCopier<blink::Font>
+    : public CrossThreadCopierPassThrough<blink::Font> {
+  STATIC_ONLY(CrossThreadCopier);
+};
+
+}  // namespace WTF
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_FONT_H_
