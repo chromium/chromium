e7b47decd68a10ff603cb403bc18a3475ff34bc3
Junbo Ke <juke@chromium.org>
2022-04-12 23:21:57 +0000

    [Chromecast] Specify the default external mojo broker path on Android.
    This CL is meant for unblocking the test of cast_service_bundle on
    device.
    
    Bug: b:228229867
    Test: On device
    Change-Id: Ic15da3b433506a88067ddd707faf394268a9a32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3582467
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991788}

--- chromecast/external_mojo/public/cpp/common.cc
+++ chromecast/external_mojo/public/cpp/common.cc
@@ -4,16 +4,23 @@
 
 #include "chromecast/external_mojo/public/cpp/common.h"
 
+#include "base/check.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/path_service.h"
 #include "base/strings/string_piece.h"
+#include "build/build_config.h"
 
 namespace chromecast {
 namespace external_mojo {
 
 namespace {
+
+#if !BUILDFLAG(IS_ANDROID)
 // Default path for Unix domain socket used by external Mojo services to connect
 // to Mojo services within cast_shell.
 constexpr base::StringPiece kDefaultBrokerPath("/tmp/cast_mojo_broker");
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 // Command-line arg to change the Unix domain socket path to connect to the
 // Cast Mojo broker.
@@ -26,7 +33,16 @@ std::string GetBrokerPath() {
   if (command_line->HasSwitch(kCastMojoBrokerPathSwitch)) {
     broker_path = command_line->GetSwitchValueASCII(kCastMojoBrokerPathSwitch);
   } else {
+#if BUILDFLAG(IS_ANDROID)
+    // Android apps don't have access to `/tmp` folder. Use app's data folder
+    // instead to store the socket file.
+    base::FilePath socket_path;
+    CHECK(base::PathService::Get(base::DIR_ANDROID_APP_DATA, &socket_path));
+    socket_path = socket_path.AppendASCII(FILE_PATH_LITERAL("cast_mojo_broker"));
+    broker_path = socket_path.MaybeAsASCII();
+#else
     broker_path = std::string(kDefaultBrokerPath);
+#endif  // BUILDFLAG(IS_ANDROID)
   }
   return broker_path;
 }
