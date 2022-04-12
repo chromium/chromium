0f4e2d79e5255b05da02927e05ee628712911ac7
Nancy Wang <nancylingwang@chromium.org>
2022-04-12 22:18:03 +0000

    Enable the AppServiceCrosApiOnAppsWithoutMojom flag.
    Enable the AppServiceCrosApiOnAppsWithoutMojom flag to send the apps
    info to the lacros side from the non mojom apps struct.
    
    BUG=1253250
    
    Change-Id: I34fd884f0ee375f365cddbe8024eb9a3ad6d2513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3580406
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#991757}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -14,6 +14,6 @@ const base::Feature kAppServiceOnAppUpdateWithoutMojom{
     "AppServiceOnAppUpdateWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature AppServiceCrosApiOnAppsWithoutMojom{
-    "AppServiceCrosApiOnAppsWithoutMojom", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AppServiceCrosApiOnAppsWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace apps
