297960b8a607bc1c6c9571fc1dec3d74b85ec8be
Frédéric Wang <fwang@igalia.com>
2022-04-13 05:19:50 +0000

    Fix mistakes in setFontFamilies tests and make them more robust
    Tests `set-font-families.js` and `set-font-families-for-script.js` both
    use value "fixed" as a font-family. This is however not a valid generic
    family name, the one corresponding to the "fixed" font preferences is
    "monospace" [1] [2]. As long as there is no fonts called "fixed" on the
    system, this will just use the standard font as a fallback.
    
    Additionally, `set-font-families-for-scripts.js` does not set any custom
    font-family for the div elements in the Russian language. So the initial
    font is used, which is again set to the standard font [1].
    
    These mistakes are hidden by the fact that these tests use FontFamilies
    objects whose "standard" font is actually set to the same value as the
    "sansSerif" and "fixed" fonts.
    
    This CL fixes the above mistakes and adjusts FontFamilies accordingly so
    that they can become visible. Additionally, they are tweaked so that:
    - The inherited fonts on the div elements are verified to be a custom
      Ahem font, rather than the standard font.
    - The div with "font-family: fixed" is tested to ensure it uses the
      standard font as a fallback, rather than the fixed font.
    - The FontFamilies objects are changed to map the invalid "monospace"
      key to a completely different system font, to verify it indeed has no
      effect.
    
    This CL does not fix the limitation that multiple fonts of FontFamilies
    objects are mapped to a same value (making impossible to distinguish
    these mappings) or that only a few generic font are tested (e.g. serif,
    cursive and fantasy are supported but untested). This could be done in
    follow-up CLs.
    
    [1] https://docs.google.com/document/d/1bvY_Npe2zLW_705KXdmecsH6P9I9wBMpHRsZ9CxWNOI/edit#
    [2] https://drafts.csswg.org/css-fonts/#generic-font-families
    
    Bug: 1240117
    
    Change-Id: I5d71582fe76827ea3836c5167ac4a59496beb8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3578956
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#991893}

--- third_party/blink/web_tests/inspector-protocol/page/set-font-families.js
+++ third_party/blink/web_tests/inspector-protocol/page/set-font-families.js
@@ -1,24 +1,33 @@
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startHTML(
+  var {dp} = await testRunner.startHTML(
     `<html>
       <style>
         @font-face {
             font-family: myahem;
             src: url(../../resources/Ahem.woff) format("woff");
         }
+        body {
+          font-family: myahem;
+        }
         div.standard_font {
+          font-family: initial;
         }
         div.sans_serif_font {
           font-family: sans-serif;
         }
         div.fixed_font {
+          font-family: monospace;
+        }
+        div.notgeneric_font {
           font-family: fixed;
         }
       </style>
       <body>
+        <div class=inherited_font>Inherited</div>
         <div class=standard_font>Standard</div>
         <div class=sans_serif_font>SansSerif</div>
         <div class=fixed_font>Fixed</div>
+        <div class=notgeneric_font>NotGeneric</div>
       </body>
     </html>`,
   'Tests Page.setFontFamilies.');
@@ -31,22 +40,31 @@
     const root = (await dp.DOM.getDocument()).result.root;
     const nodeId = (await dp.DOM.querySelector(
       {nodeId: root.nodeId, selector: selector})).result.nodeId;
-    const fonts = (await dp.CSS.getPlatformFontsForNode(
-      {nodeId: nodeId})).result.fonts;
+    const fonts = (await dp.CSS.getPlatformFontsForNode({nodeId})).result.fonts;
     testRunner.log(fonts);
   }
 
   // Override generic fonts
+  // This uses a different family name for the standard font, so that one can
+  // determine whether a font-family value is treated as a generic name.
   await dp.Page.setFontFamilies({fontFamilies: {
-    standard: "Ahem",
+    standard: "Courier",
     sansSerif: "Ahem",
-    fixed: "Ahem"
+    fixed: "Ahem", // Corresponds to the "monospace" font-family.
+    monospace: "Times", // Ignored, not a valid property of FontFamilies.
   }});
 
-  // Log overriden generic fonts
+  // Log the custom Ahem font inherited from the body.
+  await logPlatformFonts('.inherited_font');
+
+  // Log overridden generic fonts.
   await logPlatformFonts('.standard_font');
   await logPlatformFonts('.sans_serif_font');
   await logPlatformFonts('.fixed_font');
+
+  // Log the overridden standard font as a fallback (assuming no font with name
+  // "fixed" is available on the system).
+  await logPlatformFonts('.notgeneric_font');
 
   testRunner.completeTest();
 })
