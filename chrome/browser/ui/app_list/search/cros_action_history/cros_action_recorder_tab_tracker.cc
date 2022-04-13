e368abd3dc8c5e731981f693f004e108e6a39be9
Miyoung Shin <myid.shin@igalia.com>
2022-04-13 07:26:43 +0000

    MPArch: Remove TODO in CrOSActionRecorderTabTracker
    This CL removes the auto injected comments for IsInMainFrame to
    IsInPrimaryMainFrame in CrOSActionRecorderTabTracker and adds a unit
    test to ensure not to record the action in prerendering.
    
    Bug: 1218946
    Change-Id: I8cb5c24920f9e52fa276173e419e4d44b7bb7411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583584
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#991918}

--- chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder_tab_tracker.cc
+++ chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder_tab_tracker.cc
@@ -28,9 +28,6 @@ bool CrOSActionRecorderTabTracker::ShouldSkip() {
 //   (2) Preloading is counted as a navigation of a new tab.
 void CrOSActionRecorderTabTracker::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
-  // TODO(https://crbug.com/1218946): With MPArch there may be multiple main
-  // frames. This caller was converted automatically to the primary main frame
-  // to preserve its semantics. Follow up to confirm correctness.
   if (!navigation_handle->HasCommitted() ||
       !navigation_handle->IsInPrimaryMainFrame() ||
       navigation_handle->IsSameDocument() || ShouldSkip()) {
--- chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder_tab_tracker_unittest.cc
+++ chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder_tab_tracker_unittest.cc
@@ -7,8 +7,11 @@
 #include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/search/cros_action_history/cros_action_recorder.h"
+#include "content/public/test/navigation_simulator.h"
+#include "content/public/test/web_contents_tester.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 
 #include "base/strings/strcat.h"
 #include "chrome/browser/ui/browser.h"
@@ -66,10 +69,10 @@ class CrOSActionRecorderTabTrackerTest
 
   void ExpectCrOSAction(const CrOSActionProto& action,
                         const std::string& action_name,
-                        const int url_index) {
+                        const GURL& url) {
     // Expected action_name should be action_name-url;
     const std::string expected_action_name =
-        base::StrCat({action_name, "-", TestUrls()[url_index].spec()});
+        base::StrCat({action_name, "-", url.spec()});
     EXPECT_EQ(action.action_name(), expected_action_name);
   }
 
@@ -82,20 +85,76 @@ class CrOSActionRecorderTabTrackerTest
 TEST_F(CrOSActionRecorderTabTrackerTest, NavigateAndForegroundTest) {
   // Add and activate the first tab will only log a TabNavigated event.
   AddNewTab(0);
-  ExpectCrOSAction(GetCrOSActionHistory().actions(0), "TabNavigated", 0);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(0), "TabNavigated",
+                   TestUrls()[0]);
 
   // Add and activate the second tab will log a TabNavigated event, and then a
   // TabReactivated event.
   AddNewTab(1);
-  ExpectCrOSAction(GetCrOSActionHistory().actions(1), "TabNavigated", 1);
-  ExpectCrOSAction(GetCrOSActionHistory().actions(2), "TabReactivated", 1);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(1), "TabNavigated",
+                   TestUrls()[1]);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(2), "TabReactivated",
+                   TestUrls()[1]);
 
   // Switch to tab@0 will log a TabReactivated event.
   tab_activity_simulator_.SwitchToTabAt(tab_strip_model_, 0);
-  ExpectCrOSAction(GetCrOSActionHistory().actions(3), "TabReactivated", 0);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(3), "TabReactivated",
+                   TestUrls()[0]);
 
   // Check no extra events are logged.
   EXPECT_EQ(GetCrOSActionHistory().actions_size(), 4);
+}
+
+class CrOSActionRecorderTabTrackerPrerenderTest
+    : public CrOSActionRecorderTabTrackerTest {
+ public:
+  CrOSActionRecorderTabTrackerPrerenderTest() {
+    scoped_feature_list_.InitWithFeatures(
+        {blink::features::kPrerender2},
+        // Disable the memory requirement of Prerender2 so the test can run on
+        // any bot.
+        {blink::features::kPrerender2MemoryControls});
+  }
+  ~CrOSActionRecorderTabTrackerPrerenderTest() override = default;
+
+  content::WebContents* GetActiveWebContents() const {
+    return tab_strip_model_->GetActiveWebContents();
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+TEST_F(CrOSActionRecorderTabTrackerPrerenderTest,
+       EnsureDoNotRecordActionInPrerendering) {
+  // Add and activate the first tab will only log a TabNavigated event.
+  AddNewTab(0);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(0), "TabNavigated",
+                   TestUrls()[0]);
+
+  // Add a prerender page.
+  const GURL prerendering_url("https://test0.example.com/?prerendering");
+  content::RenderFrameHost* prerender_frame =
+      content::WebContentsTester::For(GetActiveWebContents())
+          ->AddPrerenderAndCommitNavigation(prerendering_url);
+  ASSERT_NE(prerender_frame, nullptr);
+  EXPECT_EQ(prerender_frame->GetLifecycleState(),
+            content::RenderFrameHost::LifecycleState::kPrerendering);
+
+  // Ensure not to record the TabNavigated event in prerendering.
+  EXPECT_EQ(GetCrOSActionHistory().actions_size(), 1);
+
+  // Activate the prerendered page.
+  content::NavigationSimulator::NavigateAndCommitFromDocument(
+      prerendering_url, GetActiveWebContents()->GetMainFrame());
+  EXPECT_EQ(prerender_frame->GetLifecycleState(),
+            content::RenderFrameHost::LifecycleState::kActive);
+
+  // Ensure to record the TabNavigated event after the prerendered page is
+  // activated.
+  EXPECT_EQ(GetCrOSActionHistory().actions_size(), 2);
+  ExpectCrOSAction(GetCrOSActionHistory().actions(1), "TabNavigated",
+                   prerendering_url);
 }
 
 }  // namespace app_list
