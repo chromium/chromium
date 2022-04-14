f2c064f77c386cf663d261171684a897a416ebcf
Egor Pasko <pasko@chromium.org>
2022-04-14 13:15:36 +0000

    android: make adb_logcat_monitor.py run in python3
    Make adb_logcat_monitor.py run in python3 as advertised in shebang.
    
    Currently the step |logcat_dump| on |orderfile-clankium| fails with:
    
        File "[...]/src/build/android/adb_logcat_monitor.py", line 85, [...]
          return re.findall('^(\\S+)\tdevice$', out, re.MULTILINE)
        File "[...]/re.py", line 241, in findall
          return _compile(pattern, flags).findall(string)
      TypeError: cannot use a string pattern on a bytes-like object
    
    Decode the string we have got from Popen as 'latin-1'. Fix an
    unimportant bug in commandline parsing. Enable some more logging.
    
    Bug: None
    Change-Id: I4b72e00c9c1c92e88629933880a7d90063b6226a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585450
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#992502}

--- build/android/adb_logcat_monitor.py
+++ build/android/adb_logcat_monitor.py
@@ -82,7 +82,7 @@ def GetAttachedDevices(adb_cmd):
                                 stderr=subprocess.PIPE).communicate()
     if err:
       logging.warning('adb device error %s', err.strip())
-    return re.findall('^(\\S+)\tdevice$', out, re.MULTILINE)
+    return re.findall('^(\\S+)\tdevice$', out.decode('latin1'), re.MULTILINE)
   except TimeoutException:
     logging.warning('"adb devices" command timed out')
     return []
@@ -148,10 +148,11 @@ def main(base_dir, adb_cmd='adb'):
 
 
 if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
   if 2 <= len(sys.argv) <= 3:
     print('adb_logcat_monitor: Initializing')
     if len(sys.argv) == 2:
       sys.exit(main(sys.argv[1]))
-  sys.exit(main(sys.argv[1], sys.argv[2]))
+    sys.exit(main(sys.argv[1], sys.argv[2]))
 
   print('Usage: %s <base_dir> [<adb_binary_path>]' % sys.argv[0])
