af22e45038ac2678e94f06b197df51cfdc3514dd
David Sanders <dsanders11@ucsbalum.com>
2022-04-15 16:24:18 +0000

    Do IWYU for //mojo/public/cpp/system/data_pipe.h
    Bug: 242216
    Change-Id: I553a0c2544a750e4c8219159967bdfcce4e42279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587464
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#992954}

--- mojo/public/cpp/system/data_pipe.h
+++ mojo/public/cpp/system/data_pipe.h
@@ -14,9 +14,8 @@
 
 #include <stdint.h>
 
-#include "base/check.h"
-#include "base/compiler_specific.h"
 #include "mojo/public/c/system/data_pipe.h"
+#include "mojo/public/c/system/types.h"
 #include "mojo/public/cpp/system/handle.h"
 
 namespace mojo {
