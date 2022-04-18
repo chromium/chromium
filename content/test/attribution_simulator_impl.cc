ad206fff9b71d91a379c20c0e38a2e9d8131dbf5
Andrew Paseltiner <apaseltiner@chromium.org>
2022-04-18 14:36:19 +0000

    Remove outdated TODO from attribution simulator
    The simulator already supports aggregatable reports.
    
    Change-Id: I42fe67e830446ef0a28fd98ff3d1779bfec4a6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587379
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993316}

--- content/test/attribution_simulator_impl.cc
+++ content/test/attribution_simulator_impl.cc
@@ -267,8 +267,6 @@ class AttributionEventHandler : public AttributionObserver {
     base::Value input_value = std::move(input_values_.front());
     input_values_.pop_front();
 
-    // TODO(linnan): Support aggregatable reports in the simulator.
-
     std::stringstream event_level_reason;
     switch (result.event_level_status()) {
       case AttributionTrigger::EventLevelResult::kSuccess:
