aedd367d0d25359609af74d6026b686982a80a03
Victor Tan <victortan@chromium.org>
2022-04-18 16:36:34 +0000

    Migrate iframe-based Client Hint WPTs to import client hints lists
    Introduce two new files into "client-hints/resources" to centralize the
    dependencies of client hint token lists across javascript and python
    tests:
    
      - export.js
      - clienthintslist.py
    
    Eventually all Client Hints tests will be migrated to using these lists
    to pare down the number of tests touched when creating new hints; this
    CL pertains to the tests that depend on
    "client-hints/resources/expect-client-hints-headers-iframe.py"
    
    One of change is the query strings fed to
    "expect-client-hints-headers-iframe.py" is now generated in
    "client-hints/resources/feature-policy-navigation.js".
    
    Another change is fixing tests related to `sec-ch-ua-platform` since we
    added platform to client hints returned by default in
    getHighEntropyValues in https://crrev.com/c/3389295.
    
    Bug: 1304740
    Change-Id: Id41ddbe1ee29c10f7b7c96a283f7eca2abec438b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3549556
    Reviewed-by: Ali Beyad <abeyad@chromium.org>
    Commit-Queue: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993338}

--- third_party/blink/web_tests/external/wpt/client-hints/sandbox/resources/util.js
+++ third_party/blink/web_tests/external/wpt/client-hints/sandbox/resources/util.js
@@ -1,26 +1,3 @@
-const iframe_src =
-  "/client-hints/resources/expect-client-hints-headers-iframe.py";
-
-const expect_iframe_no_hints = iframe_src +
-  "?sec-ch-device-memory=false" +
-  "&device-memory=false" +
-  "&sec-ch-dpr=false" +
-  "&dpr=false" +
-  "&sec-ch-viewport-width=false" +
-  "&viewport-width=false" +
-  "&sec-ch-ua=true" +
-  "&sec-ch-ua-mobile=true";
-
-const expect_iframe_hints = iframe_src +
-  "?sec-ch-device-memory=true" +
-  "&device-memory=true" +
-  "&sec-ch-dpr=true" +
-  "&dpr=true" +
-  "&sec-ch-viewport-width=true" +
-  "&viewport-width=true" +
-  "&sec-ch-ua=true" +
-  "&sec-ch-ua-mobile=true";
-
 function sandbox_iframe_test(sandbox, src, title) {
   return promise_test(async t => {
     const iframe = document.createElement("iframe");
