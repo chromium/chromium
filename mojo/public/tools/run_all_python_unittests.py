d52db528d5c0c89fa623ff87257addc35af6a9c4
Alex Gough <ajgo@chromium.org>
2022-04-18 22:14:34 +0000

    Run all mojo python unittests on the bots
    mojom_bindings_generator_unittest was not run as it was
    not swept up by mojo\public\tools\run_all_python_unittests.py.
    Now it is. Modules used by the test are added as data deps for
    the isolate. Tests in mojo/public/tools/mojom were not added
    to the isolate so did not run.
    
    Each directory with mojo python unittests is now in a build.gn
    file and dep'd into the main mojo_python_unittests target.
    
    Before this change 99 tests ran on the bots.
    Now:
    Start running tests: 2022-04-18 00:16:02.918460
    178 tests passed, 0 skipped, 0 failures.
    
    Bug: 931315
    Change-Id: Iaffda14af96b160ed76c5791ed90267bc0b7a088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585895
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993461}

--- mojo/public/tools/run_all_python_unittests.py
+++ mojo/public/tools/run_all_python_unittests.py
@@ -8,11 +8,13 @@ import sys
 
 _TOOLS_DIR = os.path.dirname(__file__)
 _MOJOM_DIR = os.path.join(_TOOLS_DIR, 'mojom')
+_BINDINGS_DIR = os.path.join(_TOOLS_DIR, 'bindings')
 _SRC_DIR = os.path.join(_TOOLS_DIR, os.path.pardir, os.path.pardir,
                         os.path.pardir)
 
 # Ensure that the mojom library is discoverable.
 sys.path.append(_MOJOM_DIR)
+sys.path.append(_BINDINGS_DIR)
 
 # Help Python find typ in //third_party/catapult/third_party/typ/
 sys.path.append(
@@ -21,7 +23,7 @@ import typ
 
 
 def Main():
-  return typ.main(top_level_dir=_MOJOM_DIR)
+  return typ.main(top_level_dirs=[_MOJOM_DIR, _BINDINGS_DIR])
 
 
 if __name__ == '__main__':
