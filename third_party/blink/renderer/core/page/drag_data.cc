e2b8856012e068e16a9a343525961972bc45b480
Xiaocheng Hu <xiaochengh@chromium.org>
2022-04-18 01:14:45 +0000

    [M101] Sanitize DragData markup before inserting it into document
    (cherry picked from commit 5164a0fe3391283663e1196cf4576ec233985e89)
    
    Fixed: 1315040
    Change-Id: I8a0ddfb983d12c185f7e943d3d5277788199b011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579670
    Quick-Run: Xiaocheng Hu <xiaochengh@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#991324}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3588887
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/4951@{#831}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- third_party/blink/renderer/core/page/drag_data.cc
+++ third_party/blink/renderer/core/page/drag_data.cc
@@ -131,8 +131,8 @@ DocumentFragment* DragData::AsFragment(LocalFrame* frame) const {
     platform_drag_data_->HtmlAndBaseURL(html, base_url);
     DCHECK(frame->GetDocument());
     if (DocumentFragment* fragment =
-            CreateFragmentFromMarkup(*frame->GetDocument(), html, base_url,
-                                     kDisallowScriptingAndPluginContent))
+            CreateSanitizedFragmentFromMarkupWithContext(
+                *frame->GetDocument(), html, 0, html.length(), base_url))
       return fragment;
   }
 
