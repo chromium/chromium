a7afd4020d49d43544ee325bcab44cc7905bf3cb
Dylan Cutler <dylancutler@google.com>
2022-04-19 20:38:56 +0000

    Cherry-pick of https://crrev.com/c/3076060 into M102
    Enable PartitionedCookies when --enable-experimental-cookie-features is
    set
    
    (cherry picked from commit 3b1be5b0bc5f4f128006c315d5c6fad3275286dd)
    
    Bug: 1225444
    Change-Id: I59d684d3319e9a171c0c97bdfe064b8d3dbf1c2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076060
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#993681}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594202
    Cr-Commit-Position: refs/branch-heads/5005@{#36}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -332,6 +332,7 @@ const char kEnableCaretBrowsing[] = "enable-caret-browsing";
 //   net::features::kSameSiteDefaultChecksMethodRigorously
 //   net::features::kSchemefulSameSite
 //   net::features::kCookieSameSiteConsidersRedirectChain
+//   net::features::kPartitionedCookies
 const char kEnableExperimentalCookieFeatures[] =
     "enable-experimental-cookie-features";
 
