44199ce4b974b71afbcc6b91ce7c9821f60b9f98
Michael Wilson <mjwilson@chromium.org>
2022-04-19 04:01:02 +0000

    [WebAudio] Break RealtimeAudioDestination node and handler into separate files
    Sub-change of https://crrev.com/c/3563603
    
    This should not cause any functional changes.
    
    Change-Id: I58ddb77dd77391b6386fad18ac7aee30845c280e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3583106
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993559}

---
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_destination_handler.cc
@@ -0,0 +1,330 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webaudio/realtime_audio_destination_handler.h"
+
+#include "base/feature_list.h"
+#include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/platform/web_audio_latency_hint.h"
+#include "third_party/blink/public/web/web_local_frame.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_context.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_node_input.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_node_output.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_worklet.h"
+#include "third_party/blink/renderer/modules/webaudio/audio_worklet_messaging_proxy.h"
+#include "third_party/blink/renderer/platform/audio/audio_utilities.h"
+#include "third_party/blink/renderer/platform/audio/denormal_disabler.h"
+#include "third_party/blink/renderer/platform/bindings/exception_messages.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_copier_base.h"
+
+namespace blink {
+
+scoped_refptr<RealtimeAudioDestinationHandler>
+RealtimeAudioDestinationHandler::Create(AudioNode& node,
+                                        const WebAudioLatencyHint& latency_hint,
+                                        absl::optional<float> sample_rate) {
+  return base::AdoptRef(
+      new RealtimeAudioDestinationHandler(node, latency_hint, sample_rate));
+}
+
+RealtimeAudioDestinationHandler::RealtimeAudioDestinationHandler(
+    AudioNode& node,
+    const WebAudioLatencyHint& latency_hint,
+    absl::optional<float> sample_rate)
+    : AudioDestinationHandler(node),
+      latency_hint_(latency_hint),
+      sample_rate_(sample_rate),
+      allow_pulling_audio_graph_(false),
+      task_runner_(Context()->GetExecutionContext()->GetTaskRunner(
+          TaskType::kInternalMediaRealTime)) {
+  // Node-specific default channel count and mixing rules.
+  channel_count_ = 2;
+  SetInternalChannelCountMode(kExplicit);
+  SetInternalChannelInterpretation(AudioBus::kSpeakers);
+}
+
+RealtimeAudioDestinationHandler::~RealtimeAudioDestinationHandler() {
+  DCHECK(!IsInitialized());
+}
+
+void RealtimeAudioDestinationHandler::Dispose() {
+  Uninitialize();
+  AudioDestinationHandler::Dispose();
+}
+
+void RealtimeAudioDestinationHandler::Initialize() {
+  DCHECK(IsMainThread());
+
+  CreatePlatformDestination();
+  AudioHandler::Initialize();
+}
+
+void RealtimeAudioDestinationHandler::Uninitialize() {
+  DCHECK(IsMainThread());
+
+  // It is possible that the handler is already uninitialized.
+  if (!IsInitialized()) {
+    return;
+  }
+
+  StopPlatformDestination();
+  AudioHandler::Uninitialize();
+}
+
+void RealtimeAudioDestinationHandler::SetChannelCount(
+    unsigned channel_count,
+    ExceptionState& exception_state) {
+  DCHECK(IsMainThread());
+
+  // TODO(crbug.com/1307461): Currently creating a platform destination requires
+  // a valid frame/document. This assumption is incorrect.
+  if (!blink::WebLocalFrame::FrameForCurrentContext()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidStateError,
+        "Cannot change channel count on a detached document.");
+    return;
+  }
+
+  // The channelCount for the input to this node controls the actual number of
+  // channels we send to the audio hardware. It can only be set if the number
+  // is less than the number of hardware channels.
+  if (channel_count > MaxChannelCount()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kIndexSizeError,
+        ExceptionMessages::IndexOutsideRange<unsigned>(
+            "channel count", channel_count, 1,
+            ExceptionMessages::kInclusiveBound, MaxChannelCount(),
+            ExceptionMessages::kInclusiveBound));
+    return;
+  }
+
+  uint32_t old_channel_count = ChannelCount();
+  AudioHandler::SetChannelCount(channel_count, exception_state);
+
+  // Stop, re-create and start the destination to apply the new channel count.
+  if (ChannelCount() != old_channel_count && !exception_state.HadException()) {
+    StopPlatformDestination();
+    CreatePlatformDestination();
+    StartPlatformDestination();
+  }
+}
+
+void RealtimeAudioDestinationHandler::StartRendering() {
+  DCHECK(IsMainThread());
+
+  StartPlatformDestination();
+}
+
+void RealtimeAudioDestinationHandler::StopRendering() {
+  DCHECK(IsMainThread());
+
+  StopPlatformDestination();
+}
+
+void RealtimeAudioDestinationHandler::Pause() {
+  DCHECK(IsMainThread());
+  if (platform_destination_) {
+    platform_destination_->Pause();
+  }
+}
+
+void RealtimeAudioDestinationHandler::Resume() {
+  DCHECK(IsMainThread());
+  if (platform_destination_) {
+    platform_destination_->Resume();
+  }
+}
+
+void RealtimeAudioDestinationHandler::RestartRendering() {
+  DCHECK(IsMainThread());
+
+  StopRendering();
+  StartRendering();
+}
+
+uint32_t RealtimeAudioDestinationHandler::MaxChannelCount() const {
+  return AudioDestination::MaxChannelCount();
+}
+
+double RealtimeAudioDestinationHandler::SampleRate() const {
+  // This can be accessed from both threads (main and audio), so it is
+  // possible that |platform_destination_| is not fully functional when it
+  // is accssed by the audio thread.
+  return platform_destination_ ? platform_destination_->SampleRate() : 0;
+}
+
+void RealtimeAudioDestinationHandler::Render(
+    AudioBus* destination_bus,
+    uint32_t number_of_frames,
+    const AudioIOPosition& output_position,
+    const AudioCallbackMetric& metric) {
+  TRACE_EVENT0("webaudio", "RealtimeAudioDestinationHandler::Render");
+
+  // Denormals can seriously hurt performance of audio processing. This will
+  // take care of all AudioNode processes within this scope.
+  DenormalDisabler denormal_disabler;
+
+  AudioContext* context = static_cast<AudioContext*>(Context());
+
+  // A sanity check for the associated context, but this does not guarantee the
+  // safe execution of the subsequence operations because the hanlder holds
+  // the context as |UntracedMember| and it can go away anytime.
+  DCHECK(context);
+  if (!context) {
+    return;
+  }
+
+  context->GetDeferredTaskHandler().SetAudioThreadToCurrentThread();
+
+  // If this node is not initialized yet, pass silence to the platform audio
+  // destination. It is for the case where this node is in the middle of
+  // tear-down process.
+  if (!IsInitialized()) {
+    destination_bus->Zero();
+    return;
+  }
+
+  context->HandlePreRenderTasks(&output_position, &metric);
+
+  // Only pull on the audio graph if we have not stopped the destination.  It
+  // takes time for the destination to stop, but we want to stop pulling before
+  // the destination has actually stopped.
+  if (IsPullingAudioGraphAllowed()) {
+    // Renders the graph by pulling all the inputs to this node. This will in
+    // turn pull on their inputs, all the way backwards through the graph.
+    scoped_refptr<AudioBus> rendered_bus =
+        Input(0).Pull(destination_bus, number_of_frames);
+
+    DCHECK(rendered_bus);
+    if (!rendered_bus) {
+      // AudioNodeInput might be in the middle of destruction. Then the internal
+      // summing bus will return as nullptr. Then zero out the output.
+      destination_bus->Zero();
+    } else if (rendered_bus != destination_bus) {
+      // In-place processing was not possible. Copy the rendered result to the
+      // given |destination_bus| buffer.
+      destination_bus->CopyFrom(*rendered_bus);
+    }
+  } else {
+    destination_bus->Zero();
+  }
+
+  // Processes "automatic" nodes that are not connected to anything. This can
+  // be done after copying because it does not affect the rendered result.
+  context->GetDeferredTaskHandler().ProcessAutomaticPullNodes(number_of_frames);
+
+  context->HandlePostRenderTasks();
+
+  context->HandleAudibility(destination_bus);
+
+  // Advances the current sample-frame.
+  AdvanceCurrentSampleFrame(number_of_frames);
+
+  context->UpdateWorkletGlobalScopeOnRenderingThread();
+
+  SetDetectSilenceIfNecessary(
+      context->GetDeferredTaskHandler().HasAutomaticPullNodes());
+}
+
+// A flag for using FakeAudioWorker when an AudioContext with "playback"
+// latency outputs silence.
+const base::Feature kUseFakeAudioWorkerForPlaybackLatency{
+    "UseFakeAudioWorkerForPlaybackLatency", base::FEATURE_ENABLED_BY_DEFAULT};
+
+void RealtimeAudioDestinationHandler::SetDetectSilenceIfNecessary(
+    bool has_automatic_pull_nodes) {
+  // Use a FakeAudioWorker for a silent AudioContext with playback latency only
+  // when it is allowed by a command line flag.
+  if (base::FeatureList::IsEnabled(kUseFakeAudioWorkerForPlaybackLatency)) {
+    // For playback latency, relax the callback timing restriction so the
+    // SilentSinkSuspender can fall back a FakeAudioWorker if necessary.
+    if (latency_hint_.Category() == WebAudioLatencyHint::kCategoryPlayback) {
+      DCHECK(is_detecting_silence_);
+      return;
+    }
+  }
+
+  // For other latency profiles (interactive, balanced, exact), use the
+  // following heristics for the FakeAudioWorker activation after detecting
+  // silence:
+  // a) When there is no automatic pull nodes (APN) in the graph, or
+  // b) When this destination node has one or more input connection.
+  bool needs_silence_detection =
+      !has_automatic_pull_nodes || Input(0).IsConnected();
+
+  // Post a cross-thread task only when the detecting condition has changed.
+  if (is_detecting_silence_ != needs_silence_detection) {
+    PostCrossThreadTask(
+        *task_runner_, FROM_HERE,
+        CrossThreadBindOnce(&RealtimeAudioDestinationHandler::SetDetectSilence,
+                            AsWeakPtr(), needs_silence_detection));
+    is_detecting_silence_ = needs_silence_detection;
+  }
+}
+
+void RealtimeAudioDestinationHandler::SetDetectSilence(bool detect_silence) {
+  DCHECK(IsMainThread());
+
+  platform_destination_->SetDetectSilence(detect_silence);
+}
+
+uint32_t RealtimeAudioDestinationHandler::GetCallbackBufferSize() const {
+  DCHECK(IsMainThread());
+  DCHECK(IsInitialized());
+
+  return platform_destination_->CallbackBufferSize();
+}
+
+int RealtimeAudioDestinationHandler::GetFramesPerBuffer() const {
+  DCHECK(IsMainThread());
+  DCHECK(IsInitialized());
+
+  return platform_destination_ ? platform_destination_->FramesPerBuffer() : 0;
+}
+
+void RealtimeAudioDestinationHandler::CreatePlatformDestination() {
+  platform_destination_ = AudioDestination::Create(
+      *this, ChannelCount(), latency_hint_, sample_rate_,
+      Context()->GetDeferredTaskHandler().RenderQuantumFrames());
+}
+
+void RealtimeAudioDestinationHandler::StartPlatformDestination() {
+  DCHECK(IsMainThread());
+
+  if (platform_destination_->IsPlaying()) {
+    return;
+  }
+
+  AudioWorklet* audio_worklet = Context()->audioWorklet();
+  if (audio_worklet && audio_worklet->IsReady()) {
+    // This task runner is only used to fire the audio render callback, so it
+    // MUST not be throttled to avoid potential audio glitch.
+    platform_destination_->StartWithWorkletTaskRunner(
+        audio_worklet->GetMessagingProxy()
+            ->GetBackingWorkerThread()
+            ->GetTaskRunner(TaskType::kInternalMediaRealTime));
+  } else {
+    platform_destination_->Start();
+  }
+
+  // Allow the graph to be pulled once the destination actually starts
+  // requesting data.
+  EnablePullingAudioGraph();
+}
+
+void RealtimeAudioDestinationHandler::StopPlatformDestination() {
+  DCHECK(IsMainThread());
+
+  // Stop pulling on the graph, even if the destination is still requesting data
+  // for a while. (It may take a bit of time for the destination to stop.)
+  DisablePullingAudioGraph();
+
+  if (platform_destination_->IsPlaying()) {
+    platform_destination_->Stop();
+  }
+}
+
+}  // namespace blink
--- third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.h
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.h
@@ -28,9 +28,11 @@
 
 #include <atomic>
 #include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "third_party/blink/public/platform/web_audio_latency_hint.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_destination_node.h"
+#include "third_party/blink/renderer/modules/webaudio/realtime_audio_destination_handler.h"
 #include "third_party/blink/renderer/platform/audio/audio_callback_metric_reporter.h"
 #include "third_party/blink/renderer/platform/audio/audio_destination.h"
 #include "third_party/blink/renderer/platform/audio/audio_io_callback.h"
@@ -40,106 +42,6 @@ namespace blink {
 class AudioContext;
 class ExceptionState;
 class WebAudioLatencyHint;
-
-class RealtimeAudioDestinationHandler final
-    : public AudioDestinationHandler,
-      public AudioIOCallback,
-      public base::SupportsWeakPtr<RealtimeAudioDestinationHandler> {
- public:
-  static scoped_refptr<RealtimeAudioDestinationHandler> Create(
-      AudioNode&,
-      const WebAudioLatencyHint&,
-      absl::optional<float> sample_rate);
-  ~RealtimeAudioDestinationHandler() override;
-
-  // For AudioHandler.
-  void Dispose() override;
-  void Initialize() override;
-  void Uninitialize() override;
-  void SetChannelCount(unsigned, ExceptionState&) override;
-  double LatencyTime() const override { return 0; }
-  double TailTime() const override { return 0; }
-  bool RequiresTailProcessing() const final { return false; }
-
-  // For AudioDestinationHandler.
-  void StartRendering() override;
-  void StopRendering() override;
-  void Pause() override;
-  void Resume() override;
-  void RestartRendering() override;
-  uint32_t MaxChannelCount() const override;
-  double SampleRate() const override;
-
-  // For AudioIOCallback. This is invoked by the platform audio destination to
-  // get the next render quantum into |destination_bus| and update
-  // |output_position|.
-  void Render(AudioBus* destination_bus,
-              uint32_t number_of_frames,
-              const AudioIOPosition& output_position,
-              const AudioCallbackMetric& metric) final;
-
-  // Returns a hadrware callback buffer size from audio infra.
-  uint32_t GetCallbackBufferSize() const;
-
-  // Returns a given frames-per-buffer size from audio infra.
-  int GetFramesPerBuffer() const;
-
-  bool IsPullingAudioGraphAllowed() const {
-    return allow_pulling_audio_graph_.load(std::memory_order_acquire);
-  }
-
-  // Sets the detect silence flag for the platform destination.
-  void SetDetectSilence(bool detect_silence);
-
- private:
-  explicit RealtimeAudioDestinationHandler(AudioNode&,
-                                           const WebAudioLatencyHint&,
-                                           absl::optional<float> sample_rate);
-
-  void CreatePlatformDestination();
-  void StartPlatformDestination();
-  void StopPlatformDestination();
-
-  // Checks the current silent detection condition (e.g. the number of
-  // automatic pull nodes) and flips the switch if necessary. Called within the
-  // Render() method.
-  void SetDetectSilenceIfNecessary(bool has_automatic_pull_nodes);
-
-  // Should only be called from StartPlatformDestination.
-  void EnablePullingAudioGraph() {
-    allow_pulling_audio_graph_.store(true, std::memory_order_release);
-  }
-
-  // Should only be called from StopPlatformDestination.
-  void DisablePullingAudioGraph() {
-    allow_pulling_audio_graph_.store(false, std::memory_order_release);
-  }
-
-  const WebAudioLatencyHint latency_hint_;
-
-  // Holds the audio device thread that runs the real time audio context.
-  scoped_refptr<AudioDestination> platform_destination_;
-
-  absl::optional<float> sample_rate_;
-
-  // If true, the audio graph will be pulled to get new data.  Otherwise, the
-  // graph is not pulled, even if the audio thread is still running and
-  // requesting data.
-  //
-  // Must be modified only in StartPlatformDestination (via
-  // EnablePullingAudioGraph) or StopPlatformDestination (via
-  // DisablePullingAudioGraph) .  This is modified only by the main threda and
-  // the audio thread only reads this.
-  std::atomic_bool allow_pulling_audio_graph_;
-
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-
-  // Represents the current condition of silence detection. By default, the
-  // silence detection is active.
-  bool is_detecting_silence_ = true;
-};
-
-// -----------------------------------------------------------------------------
 
 class RealtimeAudioDestinationNode final : public AudioDestinationNode {
  public:
