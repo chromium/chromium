13d7c41552453595a1bc500d3b1da4927f3ece26
Bruce Dawson <brucedawson@chromium.org>
2022-04-19 17:13:36 +0000

    Disable binary_size tests on Windows
    bcanalyzer and other binary_size tools don't work on Windows so these
    tests just give lots of noisy and cryptic error messages. This change
    disables these tests on Windows so that "git cl presubmit --all" output
    will have a better signal to noise ratio.
    
    Typical errors (hugely simplified) were:
    
        ERROR: testAnalyzer (__main__.BcAnalyzerTest)
        ...
        OSError: [WinError 193] %1 is not a valid Win32 application
    
    This change was tested with:
    
        git cl presubmit --files=tools/binary_size/*
    
    Bug: 1309977
    Change-Id: I68117cfeeb083594b11622c5def9aedc3267c91f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3591277
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993759}

--- tools/binary_size/PRESUBMIT.py
+++ tools/binary_size/PRESUBMIT.py
@@ -13,19 +13,23 @@ USE_PYTHON3 = True
 
 def CommonChecks(input_api, output_api):
   output = []
-  output.extend(
-      input_api.canned_checks.RunPylint(input_api, output_api, version='2.6'))
-  py_tests = input_api.canned_checks.GetUnitTestsRecursively(
-      input_api,
-      output_api,
-      input_api.PresubmitLocalPath(),
-      files_to_check=[r'.+_test\.py$'],
-      files_to_skip=[],
-      run_on_python2=False,
-      run_on_python3=True,
-      skip_shebang_check=True)
+  # These tools don't run on Windows so these tests don't work and give many
+  # verbose and cryptic failure messages. Linting the code is also skipped on
+  # Windows because it will fail due to OS differences.
+  if input_api.sys.platform != 'win32':
+    output.extend(
+        input_api.canned_checks.RunPylint(input_api, output_api, version='2.6'))
+    py_tests = input_api.canned_checks.GetUnitTestsRecursively(
+        input_api,
+        output_api,
+        input_api.PresubmitLocalPath(),
+        files_to_check=[r'.+_test\.py$'],
+        files_to_skip=[],
+        run_on_python2=False,
+        run_on_python3=True,
+        skip_shebang_check=True)
+    output.extend(input_api.RunTests(py_tests, False))
 
-  output.extend(input_api.RunTests(py_tests, False))
   output.extend(
       input_api.canned_checks.CheckPatchFormatted(input_api,
                                                   output_api,
