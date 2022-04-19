b8998b6944aeae10e674ad2bda09c90f065c718b
Dale Curtis <dalecurtis@chromium.org>
2022-04-19 23:35:37 +0000

    Attempt to improve reliability of GPU video playback tests.
    This makes a couple changes which should improve reliability:
    - Uses requestVideoFrameCallback() for frame detection. This may be
      sufficient by itself, but I've kept the waitForSwapsComplete() since
      we've reintroduced it in other flaky tests previously.
    - Mutes the playback, which will prevent a real audio device from being
      spun up -- these tests only care about pixel data.
    - Adds an error listener which will fail the test instead of silently
      timing out (though now that we use rVFC it shouldn't be a problem).
    
    R=bsheedy, tguilbert
    
    Bug: 1316538
    Change-Id: Id32b7f21d1e59392196c6953e261e0bae97b3358
    Cq-Include-Trybots: luci.chromium.try:win_optional_gpu_tests_rel,mac_optional_gpu_tests_rel,linux_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594060
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993952}

--- content/test/data/gpu/pixel_video_test.js
+++ content/test/data/gpu/pixel_video_test.js
@@ -6,29 +6,40 @@ var video;
 
 // Some videos are less than 60 fps, so actual video frame presentations
 // could be much less than 30.
-var g_swaps_before_success = 30
+var g_swaps_before_success = 30;
+
+var abort = false;
 
 function main() {
-  video = document.getElementById("video");
+  video = document.getElementById('video');
   video.loop = true;
-  video.addEventListener('timeupdate', waitForVideoToPlay);
-  video.play();
-}
+  video.muted = true;  // No need to exercise audio paths.
 
-function waitForVideoToPlay() {
-  if (video.currentTime > 0) {
-    video.removeEventListener('timeupdate', waitForVideoToPlay);
+  video.onerror = _ => {
+    abort = true;
+    domAutomationController.send('FAIL');
+  };
+
+  video.play();
+
+  // These tests expect playback, so we intentionally don't request the frame
+  // callback before starting playback. Since these videos loop there should
+  // always be frames being generated.
+  video.requestVideoFrameCallback(_ => {
     chrome.gpuBenchmarking.addSwapCompletionEventListener(
         waitForSwapsToComplete);
-  }
+  });
 }
 
 function waitForSwapsToComplete() {
+  if (abort)
+    return;
+
   g_swaps_before_success--;
   if (g_swaps_before_success > 0) {
     chrome.gpuBenchmarking.addSwapCompletionEventListener(
         waitForSwapsToComplete);
   } else {
-    domAutomationController.send("SUCCESS");
+    domAutomationController.send('SUCCESS');
   }
 }
