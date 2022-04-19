7b917dfb264e38581700ec6430e544846b13fabe
Eriko Kurimoto <elkurin@chromium.org>
2022-04-19 00:35:30 +0000

    [Resource file sharing]: Add shared resource pak path
    This CL add FILE_SHARED_RESOURCE_PACK path to chrome paths.
    
    This change is due to the plan to merge the info in
    FILE_FALLBACK_RESOURCES_PACK and FILE_RESOURCES_MAP into
    FILE_SHARED_RESOURCE_PACK.
    
    This CL doesn't affect the behavior.
    
    Bug: 1253280
    Change-Id: I8404acd2c9462c228d737d55a0956a54497070ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581505
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993506}

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -367,20 +367,15 @@ bool PathProvider(int key, base::FilePath* result) {
       break;
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-    case chrome::FILE_FALLBACK_RESOURCES_PACK:
+    case chrome::FILE_RESOURCES_FOR_SHARING_PACK:
       if (!GetDefaultUserDataDirectory(&cur))
         return false;
-      cur = cur.Append(FILE_PATH_LITERAL("resources_fallback.pak"));
+      cur = cur.Append(FILE_PATH_LITERAL("resources_for_sharing.rspak"));
       break;
     case chrome::FILE_ASH_RESOURCES_PACK:
       if (!chrome::GetAshResourcesPath(&cur))
         return false;
       cur = cur.Append("resources.pak");
-      break;
-    case chrome::FILE_RESOURCES_MAP:
-      if (!GetDefaultUserDataDirectory(&cur))
-        return false;
-      cur = cur.Append(FILE_PATH_LITERAL("resources.map"));
       break;
 #endif
 
