cff1716f858dab0927825cbf25b7c29172da546c
Melissa Zhang <melzhang@google.com>
2022-04-19 07:55:40 +0000

    [Provisioning] : Filter data based on locale.
    Bug:1310686
    
    Change-Id: Ieb94807f30bea02ceac006ec1a4ee3a239c1da81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581670
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#993598}

--- chrome/browser/apps/app_discovery_service/game_fetcher.cc
+++ chrome/browser/apps/app_discovery_service/game_fetcher.cc
@@ -7,10 +7,68 @@
 #include <memory>
 #include <utility>
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_discovery_service/game_extras.h"
 #include "chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/country_codes/country_codes.h"
+#include "components/language/core/browser/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace {
+extern const char kDefaultLocale[] = "en-US";
+
+bool AvailableInCurrentLocale(
+    const apps::proto::LocaleAvailability& app_with_locale) {
+  int current_country_id = country_codes::GetCurrentCountryID();
+  // TODO(melzhang) : This should not be returning -1. Return true for now so
+  // that we have data during development.
+  if (current_country_id == -1) {
+    return true;
+  }
+  for (const auto& country_code : app_with_locale.available_country_codes()) {
+    int country_id = country_codes::CountryStringToCountryID(country_code);
+    if (country_id == current_country_id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+std::u16string GetLocalisedName(
+    const apps::proto::LocaleAvailability& app_with_locale,
+    Profile* profile) {
+  PrefService* prefs = profile->GetPrefs();
+  DCHECK(prefs);
+  std::string locale = prefs->GetString(language::prefs::kApplicationLocale);
+  std::string resolved_locale;
+  l10n_util::CheckAndResolveLocale(locale, &resolved_locale,
+                                   /*perform_io=*/false);
+  std::string localised_name;
+  std::string fallback_name;
+  for (const auto& available_localised_name :
+       app_with_locale.available_localised_names()) {
+    // When the current locale does not match any of the supported
+    // |language_codes|, we default to en-US. Store the en-US name as a
+    // fallback.
+    if (available_localised_name.language_code() == kDefaultLocale) {
+      fallback_name = available_localised_name.name_in_language();
+    }
+    if (available_localised_name.language_code() != resolved_locale) {
+      continue;
+    }
+    localised_name = available_localised_name.name_in_language();
+  }
+  if (localised_name.empty()) {
+    DCHECK(!fallback_name.empty());
+    localised_name = fallback_name;
+  }
+  return base::UTF8ToUTF16(localised_name);
+}
+
+}  // namespace
 
 namespace apps {
 
@@ -41,13 +99,15 @@ std::vector<Result> GameFetcher::GetAppsForCurrentLocale(
     const proto::AppWithLocaleList& app_data) {
   std::vector<Result> results;
   for (const auto& app_with_locale : app_data.app_with_locale()) {
-    // TODO(melzhang) : Filter based on locale.
+    if (!AvailableInCurrentLocale(app_with_locale.locale_availability())) {
+      continue;
+    }
     auto extras = std::make_unique<GameExtras>(
         absl::nullopt, GameExtras::Source::kTestSource, GURL());
-    // TODO(melzhang) : Pass in locale specific title.
-    results.push_back(Result(AppSource::kGames,
-                             app_with_locale.app().app_id_for_platform(),
-                             u"TempAppTitle", std::move(extras)));
+    results.push_back(Result(
+        AppSource::kGames, app_with_locale.app().app_id_for_platform(),
+        GetLocalisedName(app_with_locale.locale_availability(), profile_),
+        std::move(extras)));
   }
   return results;
 }
