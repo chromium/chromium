2396a9e318b8a31ac90d412e95af0d2a488ae343
David Roger <droger@chromium.org>
2022-04-19 08:15:56 +0000

    Revert "[reland][privacy sandbox settings] Handle notice dismiss in web UI"
    This reverts commit 1ada708bedb355742054f7c848ba0c1359a61fb3.
    
    Still failing on Wayland:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(Wayland)/12327/overview
    
    Original change's description:
    > [reland][privacy sandbox settings] Handle notice dismiss in web UI
    >
    > Dismissing the notice was handled in the dialog view. The action
    > didn't go through the handler pipeline and didn't trigger HaTS. It also
    > recorded kNoticeClosedNoAction as well as kNoticeDismiss.
    >
    > Handle pressing ESC in the web UI instead to go through the same path as
    > other user actions.
    >
    > CL was reverted because new interactive tests are flaky on ChromeOS and
    > Wayland:
    > https://chromium-review.googlesource.com/c/chromium/src/+/3584774
    > Disable them on ChromeOS and Wayland for now.
    >
    > Bug: 1312042
    > Change-Id: Ib9f59ca83ea2efcd3d0ec2891378690e396a1935
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3581996
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Cr-Commit-Position: refs/heads/main@{#992472}
    
    (cherry picked from commit 7cde19e59e51095d121e0d481cb443fa031e1099)
    
    Bug: 1312042
    Change-Id: Ia0eccea499e81a99823ab5104f0cd1ba60c6ab63
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3587187
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#992890}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3589012
    Owners-Override: Robert Liao <robliao@google.com>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/branch-heads/5005@{#27}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view_browsertest.cc
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 #include "build/build_config.h"
-#include "chrome/browser/privacy_sandbox/mock_privacy_sandbox_service.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -12,6 +12,7 @@
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h"
+#include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -22,6 +23,19 @@ namespace {
 
 constexpr int kAverageBrowserWidth = 800;
 constexpr int kAverageBrowserHeight = 700;
+
+class MockPrivacySandboxService : public PrivacySandboxService {
+ public:
+  MOCK_METHOD(void,
+              DialogActionOccurred,
+              (PrivacySandboxService::DialogAction),
+              (override));
+};
+
+std::unique_ptr<KeyedService> BuildMockPrivacySandboxService(
+    content::BrowserContext* context) {
+  return std::make_unique<::testing::NiceMock<MockPrivacySandboxService>>();
+}
 
 }  // namespace
 
@@ -64,6 +78,63 @@ class PrivacySandboxDialogViewBrowserTest : public DialogBrowserTest {
  private:
   raw_ptr<MockPrivacySandboxService> mock_service_;
 };
+
+IN_PROC_BROWSER_TEST_F(PrivacySandboxDialogViewBrowserTest,
+                       EscapeClosesNotice) {
+  // Check that when the escape key is pressed, the notice is closed.
+  EXPECT_CALL(
+      *mock_service(),
+      DialogActionOccurred(PrivacySandboxService::DialogAction::kNoticeShown));
+  EXPECT_CALL(*mock_service(),
+              DialogActionOccurred(
+                  PrivacySandboxService::DialogAction::kNoticeDismiss));
+  EXPECT_CALL(
+      *mock_service(),
+      DialogActionOccurred(
+          PrivacySandboxService::DialogAction::kNoticeClosedNoInteraction));
+  views::NamedWidgetShownWaiter waiter(
+      views::test::AnyWidgetTestPasskey{},
+      PrivacySandboxDialogView::kViewClassName);
+  ShowPrivacySandboxDialog(browser(),
+                           PrivacySandboxService::DialogType::kNotice);
+  auto* dialog = waiter.WaitIfNeededAndGet();
+  dialog->CloseWithReason(views::Widget::ClosedReason::kEscKeyPressed);
+  EXPECT_TRUE(dialog->IsClosed());
+
+  // While IsClosed updated immediately, the widget will only actually close,
+  // and thus inform the service asynchronously so must be waited for.
+  base::RunLoop().RunUntilIdle();
+
+  // Shutting down the browser test will naturally shut the dialog, verify
+  // expectations before that happens.
+  testing::Mock::VerifyAndClearExpectations(mock_service());
+}
+
+IN_PROC_BROWSER_TEST_F(PrivacySandboxDialogViewBrowserTest,
+                       EscapeDoesntCloseConsent) {
+  // Check that when the escape key is pressed, the consent is not closed.
+  EXPECT_CALL(
+      *mock_service(),
+      DialogActionOccurred(PrivacySandboxService::DialogAction::kConsentShown));
+  EXPECT_CALL(
+      *mock_service(),
+      DialogActionOccurred(
+          PrivacySandboxService::DialogAction::kConsentClosedNoDecision))
+      .Times(0);
+  views::NamedWidgetShownWaiter waiter(
+      views::test::AnyWidgetTestPasskey{},
+      PrivacySandboxDialogView::kViewClassName);
+  ShowPrivacySandboxDialog(browser(),
+                           PrivacySandboxService::DialogType::kConsent);
+  auto* dialog = waiter.WaitIfNeededAndGet();
+  dialog->CloseWithReason(views::Widget::ClosedReason::kEscKeyPressed);
+  base::RunLoop().RunUntilIdle();
+  EXPECT_FALSE(dialog->IsClosed());
+
+  // Shutting down the browser test will naturally shut the dialog, verify
+  // expectations before that happens.
+  testing::Mock::VerifyAndClearExpectations(mock_service());
+}
 
 #if BUILDFLAG(IS_WIN)
 #define MAYBE_InvokeUi_Consent DISABLED_InvokeUi_Consent
