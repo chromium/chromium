115f6c7f74bdfb6bb5ab8c8ee98d15af578bfbfd
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-04-21 11:15:04 +0000

    Disable flaky testFocusChange.
    Bug: 1318382
    Change-Id: I1d5bac041e9ce102857ba392ecf10deb3c9b05a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3598851
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Owners-Override: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#994610}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithEditTextTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithEditTextTest.java
@@ -35,6 +35,7 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.components.browser_ui.widget.test.R;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.KeyboardVisibilityDelegate;
@@ -236,6 +237,7 @@ public class RadioButtonWithEditTextTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1318382")
     public void testFocusChange() {
         Assert.assertFalse(mRadioButtonWithEditText.hasFocus());
         TestThreadUtils.runOnUiThreadBlocking(() -> { mRadioButtonWithEditText.setChecked(true); });
