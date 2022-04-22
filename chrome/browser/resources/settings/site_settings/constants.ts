ad0f0b3c8e04d0b5baa7b0dbd2542dce284dfb83
Xiaohan Wang <xhwang@chromium.org>
2022-04-22 08:42:38 +0000

    chrome: Fix broken links in site_settings/constants.ts
    Some source/header files referenced have been moved. Update the path in
    the comments to reflect this.
    
    Change-Id: I34bad078f289f54bf8178a8b0fd2b9cbbce923a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3601553
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#995094}

--- chrome/browser/resources/settings/site_settings/constants.ts
+++ chrome/browser/resources/settings/site_settings/constants.ts
@@ -7,7 +7,7 @@
  * the UI. Both top-level categories and content settings that represent
  * individual permissions under Site Details should appear here.
  * This should be kept in sync with the |kContentSettingsTypeGroupNames| array
- * in chrome/browser/ui/webui/site_settings_helper.cc
+ * in chrome/browser/ui/webui/settings/site_settings_helper.cc
  */
 export enum ContentSettingsTypes {
   ADS = 'ads',
@@ -64,7 +64,7 @@ export enum ContentSetting {
 /**
  * All possible ChooserTypes that we currently support configuring in the UI.
  * This should be kept in sync with the |kChooserTypeGroupNames| array in
- * chrome/browser/ui/webui/site_settings_helper.cc
+ * chrome/browser/ui/webui/settings/site_settings_helper.cc
  */
 export enum ChooserType {
   NONE = '',
@@ -88,7 +88,7 @@ export enum CookieControlsMode {
 /**
  * Contains the possible sources of a ContentSetting.
  * This should be kept in sync with the |SiteSettingSource| enum in
- * chrome/browser/ui/webui/site_settings_helper.h
+ * chrome/browser/ui/webui/settings/site_settings_helper.h
  */
 export enum SiteSettingSource {
   ADS_FILTER_BLACKLIST = 'ads-filter-blacklist',
