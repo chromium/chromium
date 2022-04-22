2de7d171011f010323407e8dd3631bfec86311eb
Louis Romero <lpromero@google.com>
2022-04-22 11:56:13 +0000

    Add support for KVO on UIView window
    -[UIView window] property doesn't support KVO by default. To add support
    for it, this CL swizzles -willMoveToWindow: and -didMoveToWindow to add
    the KVO notifications.
    
    When adding the new swift_source_set, I discovered a bug with swiftc
    when not compiling with modules. I made a workaround so that Swift
    source can be imported into Objective-C(++) files (like the unit test
    file in this CL for example).
    
    (cherry picked from commit 9470a3f6a80b3f673f5c9230a7eb9660309d1674)
    
    Bug: 1315559
    Change-Id: I0bca03ada2dd189b1445c453da67fde54c108f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3579318
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#993705}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599184
    Auto-Submit: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5005@{#102}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

---
+++ ios/chrome/browser/ui/util/UIView+WindowObserving.swift
@@ -0,0 +1,61 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import ObjectiveC
+import UIKit
+
+/// Extends UIView to notify via KVO when it moved to a new window.
+extension UIView {
+  /// Whether the original and alternative implementations have been swapped.
+  private static var swizzled = false
+
+  /// Whether all instances of UIView will notify key-value observers for their `window` property.
+  ///
+  /// Default is `false`.
+  @objc static var cr_supportsWindowObserving: Bool {
+    get {
+      return swizzled
+    }
+    set {
+      if swizzled != newValue {
+        // Swap implementations.
+        guard
+          let willMove1 = class_getInstanceMethod(UIView.self, #selector(willMove(toWindow:))),
+          let willMove2 = class_getInstanceMethod(UIView.self, #selector(cr_willMove(toWindow:))),
+          let didMove1 = class_getInstanceMethod(UIView.self, #selector(didMoveToWindow)),
+          let didMove2 = class_getInstanceMethod(UIView.self, #selector(cr_didMoveToWindow))
+        else {
+          // If it failed here, don't change the `swizzled` state.
+          return
+        }
+        method_exchangeImplementations(willMove1, willMove2)
+        method_exchangeImplementations(didMove1, didMove2)
+        // Change the `swizzled` state.
+        swizzled = newValue
+      }
+    }
+  }
+
+  /// Signals that the `window` key is supported via Manual Change Notification.
+  /// https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/KeyValueObserving/Articles/KVOCompliance.html#//apple_ref/doc/uid/20002178-SW3
+  @objc class func automaticallyNotifiesObserversOfWindow() -> Bool {
+    return false
+  }
+
+  @objc private func cr_willMove(toWindow newWindow: UIWindow?) {
+    cr_willMove(toWindow: newWindow)
+    willChangeValue(forKey: "window")
+  }
+
+  @objc private func cr_didMoveToWindow() {
+    cr_didMoveToWindow()
+    didChangeValue(forKey: "window")
+  }
+}
+
+/// TODO(crbug.com/1316061): This extension is just a pretext to force swiftc to import the bridging
+/// header `util_swift_bridge.h`, and transitively, UIKit/UIKit.h.
+extension CrBug1316061View {
+  @objc func doNothing() {}
+}
---
+++ ios/chrome/browser/ui/util/util_swift_bridge.h
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_UTIL_UTIL_SWIFT_BRIDGE_H_
+#define IOS_CHROME_BROWSER_UI_UTIL_UTIL_SWIFT_BRIDGE_H_
+
+// TODO(crbug.com/1316061): This import is just a pretext to force swiftc to
+// import this bridging header, and transitively, UIKit/UIKit.h.
+#import "ios/chrome/browser/ui/util/crbug_1316061_view.h"
+
+#endif  // IOS_CHROME_BROWSER_UI_UTIL_UTIL_SWIFT_BRIDGE_H_
