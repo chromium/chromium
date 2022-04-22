2b8380df7e0c82b42fe6bbf2b5f0a893ae18d8d2
Hong Xu <xuhong@chromium.org>
2022-04-22 23:47:42 +0000

    Add EventUploadSizeController
    Add `EventUploadSizeController` to control the size of the event to be
    uploaded. Currently factors related to local storage limits are not
    considered. The logic to actually use `EventUploadSizeController` is
    also temporarily disabled, until a policy to enable/disable this feature
    is added in a later CL.
    
    Bug: b:214039157
    Change-Id: I8d18b317a3e40eeb8cdbc39c6107a526807f3c68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596745
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#995426}

--- chrome/browser/ash/dbus/encrypted_reporting_service_provider_unittest.cc
+++ chrome/browser/ash/dbus/encrypted_reporting_service_provider_unittest.cc
@@ -11,7 +11,6 @@
 #include "base/memory/ref_counted.h"
 #include "base/task/thread_pool.h"
 #include "base/test/scoped_feature_list.h"
-#include "base/test/task_environment.h"
 #include "chrome/browser/policy/messaging_layer/upload/fake_upload_client.h"
 #include "chrome/browser/policy/messaging_layer/upload/upload_client.h"
 #include "chrome/browser/policy/messaging_layer/util/test_request_payload.h"
@@ -21,6 +20,7 @@
 #include "components/policy/core/common/cloud/dm_token.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 #include "components/reporting/proto/interface.pb.h"
+#include "content/public/test/browser_task_environment.h"
 #include "dbus/exported_object.h"
 #include "dbus/message.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -161,7 +161,7 @@ class EncryptedReportingServiceProviderTest : public ::testing::Test {
   }
 
   // Must be initialized before any other class member.
-  base::test::TaskEnvironment task_environment_;
+  content::BrowserTaskEnvironment task_environment_;
 
   policy::MockCloudPolicyClient cloud_policy_client_;
   reporting::EncryptedRecord record_;
