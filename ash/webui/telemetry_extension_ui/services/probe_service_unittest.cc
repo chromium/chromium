abc19e199abcdc3837df121b45a3ae3c41a7d545
Chung-Sheng Wu <chungsheng@google.com>
2022-04-22 07:24:56 +0000

    cros_healthd: Migrate to FakeCrosHealthd in telemetry_extension_ui/
    Bug: b/210565604
    Test: CQ
    Change-Id: Ia699ba798ef8db7d4fa3d9ad7638f42a42a73836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599391
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Chung-Sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#995066}

--- ash/webui/telemetry_extension_ui/services/probe_service_unittest.cc
+++ ash/webui/telemetry_extension_ui/services/probe_service_unittest.cc
@@ -10,11 +10,9 @@
 #include "base/bind.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
-#include "chromeos/ash/components/dbus/cros_healthd/cros_healthd_client.h"
-#include "chromeos/ash/components/dbus/cros_healthd/fake_cros_healthd_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/debug_daemon/fake_debug_daemon_client.h"
-#include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
+#include "chromeos/services/cros_healthd/public/cpp/fake_cros_healthd.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -30,15 +28,11 @@ class ProbeServiceTest : public testing::Test {
     chromeos::DBusThreadManager::GetSetterForTesting()->SetDebugDaemonClient(
         std::move(fake_debugd_client));
 
-    cros_healthd::CrosHealthdClient::InitializeFake();
+    cros_healthd::FakeCrosHealthd::Initialize();
   }
 
   void TearDown() override {
-    cros_healthd::CrosHealthdClient::Shutdown();
-
-    // Wait for ServiceConnection to observe the destruction of the client.
-    cros_healthd::ServiceConnection::GetInstance()->FlushForTesting();
-
+    cros_healthd::FakeCrosHealthd::Shutdown();
     chromeos::DBusThreadManager::Shutdown();
   }
 
@@ -73,7 +67,7 @@ TEST_F(ProbeServiceTest, ProbeTelemetryInfoSuccess) {
     info->battery_result = cros_healthd::mojom::BatteryResult::NewBatteryInfo(
         std::move(battery_info));
 
-    cros_healthd::FakeCrosHealthdClient::Get()
+    cros_healthd::FakeCrosHealthd::Get()
         ->SetProbeTelemetryInfoResponseForTesting(info);
   }
 
