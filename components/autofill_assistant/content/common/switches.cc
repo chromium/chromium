e46e2a46077814dfbf96da1b00e64f89113275a0
sandromaggi <sandromaggi@google.com>
2022-04-22 10:49:08 +0000

    Reland "Add switch for logging to AnnotateDom"
    This is a reland of commit a25642ba9262aa2da419bae7e18cb0c042a718ac
    
    Change:
    https://chromium-review.googlesource.com/c/chromium/src/+/3599022/1..2
    
    Original change's description:
    > Add switch for logging to AnnotateDom
    >
    > If the switch is present, Autofill Assistant's
    > AnnotateDom feature will log its extracted signals
    > to the console.
    >
    > Bug: b:228953382
    > Change-Id: I29005f1f6137af292c16536f22fdba80245f1c86
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3584164
    > Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    > Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#994655}
    
    Bug: b:228953382
    Change-Id: Id8fba445689c0d4b914ea0e5931d03ab5a387a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599022
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Quick-Run: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#995128}

---
+++ components/autofill_assistant/content/common/switches.cc
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/content/common/switches.h"
+
+namespace autofill_assistant::switches {
+
+// Enables annotating DOM debugging when set.
+const char kAutofillAssistantDebugAnnotateDom[] =
+    "autofill-assistant-debug-annotate-dom";
+
+}  // namespace autofill_assistant::switches
