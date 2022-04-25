0c810a6d81182f8f1d4cb69ba6f061df420c1a63
Daniel Cheng <dcheng@chromium.org>
2022-04-25 20:49:40 +0000

    Convert SHOUTY_CASE Mojo union tags to kCamelCase in //components/metrics/structured/mojom.
    This CL was uploaded by git cl split.
    
    R=kinuko@chromium.org
    
    Bug: 1148486
    Change-Id: Ia068cd098e5925902b3e8843b1dfd4e91f46ca44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599223
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995739}

--- components/metrics/structured/mojom/event_mojom_traits.cc
+++ components/metrics/structured/mojom/event_mojom_traits.cc
@@ -22,18 +22,17 @@ UnionTraits<metrics::structured::mojom::MetricValueDataView,
     GetTag(const metrics::structured::Event::MetricValue& metric_value) {
   switch (metric_value.type) {
     case metrics::structured::Event::MetricType::kHmac:
-      return metrics::structured::mojom::MetricValueDataView::Tag::HMAC_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kHmacValue;
     case metrics::structured::Event::MetricType::kLong:
-      return metrics::structured::mojom::MetricValueDataView::Tag::LONG_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kLongValue;
     case metrics::structured::Event::MetricType::kInt:
-      return metrics::structured::mojom::MetricValueDataView::Tag::INT_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kIntValue;
     case metrics::structured::Event::MetricType::kDouble:
-      return metrics::structured::mojom::MetricValueDataView::Tag::DOUBLE_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kDoubleValue;
     case metrics::structured::Event::MetricType::kRawString:
-      return metrics::structured::mojom::MetricValueDataView::Tag::
-          RAW_STR_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kRawStrValue;
     case metrics::structured::Event::MetricType::kBoolean:
-      return metrics::structured::mojom::MetricValueDataView::Tag::BOOL_VALUE;
+      return metrics::structured::mojom::MetricValueDataView::Tag::kBoolValue;
   }
 }
 
@@ -43,7 +42,7 @@ bool UnionTraits<metrics::structured::mojom::MetricValueDataView,
     Read(metrics::structured::mojom::MetricValueDataView metric,
          metrics::structured::Event::MetricValue* out) {
   switch (metric.tag()) {
-    case metrics::structured::mojom::MetricValueDataView::Tag::HMAC_VALUE: {
+    case metrics::structured::mojom::MetricValueDataView::Tag::kHmacValue: {
       std::string hmac_value;
       if (!metric.ReadHmacValue(&hmac_value))
         return false;
@@ -51,19 +50,19 @@ bool UnionTraits<metrics::structured::mojom::MetricValueDataView,
       out->value = base::Value(std::move(hmac_value));
       break;
     }
-    case metrics::structured::mojom::MetricValueDataView::Tag::LONG_VALUE:
+    case metrics::structured::mojom::MetricValueDataView::Tag::kLongValue:
       out->type = metrics::structured::Event::MetricType::kLong;
       out->value = base::Value(base::NumberToString(metric.long_value()));
       break;
-    case metrics::structured::mojom::MetricValueDataView::Tag::INT_VALUE:
+    case metrics::structured::mojom::MetricValueDataView::Tag::kIntValue:
       out->type = metrics::structured::Event::MetricType::kInt;
       out->value = base::Value(metric.int_value());
       break;
-    case metrics::structured::mojom::MetricValueDataView::Tag::DOUBLE_VALUE:
+    case metrics::structured::mojom::MetricValueDataView::Tag::kDoubleValue:
       out->type = metrics::structured::Event::MetricType::kDouble;
       out->value = base::Value(metric.double_value());
       break;
-    case metrics::structured::mojom::MetricValueDataView::Tag::RAW_STR_VALUE: {
+    case metrics::structured::mojom::MetricValueDataView::Tag::kRawStrValue: {
       std::string raw_str_value;
       if (!metric.ReadRawStrValue(&raw_str_value))
         return false;
@@ -71,7 +70,7 @@ bool UnionTraits<metrics::structured::mojom::MetricValueDataView,
       out->value = base::Value(std::move(raw_str_value));
       break;
     }
-    case metrics::structured::mojom::MetricValueDataView::Tag::BOOL_VALUE:
+    case metrics::structured::mojom::MetricValueDataView::Tag::kBoolValue:
       out->type = metrics::structured::Event::MetricType::kBoolean;
       out->value = base::Value(metric.bool_value());
       break;
