321694912e19fb00a823b35d27e8f50c5b090f8a
Sebastien Lalancette <seblalancette@chromium.org>
2022-04-25 21:21:56 +0000

    [NewEV] Create Signals Names Constants
    Added a new file under the Enterprise component where signals names
    can be defined, and then used as keys in JSON dictionaries to
    store/retrieve the signal values.
    
    Bug: b:229737662
    Change-Id: I77998b7a6e43903556aca65949b46a883fe5e6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3594759
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995763}

---
+++ components/enterprise/signals/common/signals_constants.cc
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/signals/common/signals_constants.h"
+
+namespace enterprise_signals {
+namespace names {
+
+// Name of the parameterized signal for getting information from resources
+// stored on the file system. This includes the presence/absence of
+// files/folders, and also additional signals' extraction from executables.
+const char kFileSystemInfo[] = "fileSystemInfo";
+
+// Name of the parameterized signal for getting information from settings
+// storage (e.g. Registry, Plist) on the device.
+const char kSettings[] = "settings";
+
+// Name of the signal for getting information about AV software installed on
+// the device.
+const char kAntiVirusInfo[] = "antiVirusInfo";
+
+// Name of the signal for getting information about installed hotfixes on the
+// device.
+const char kInstalledHotfixes[] = "hotfixes";
+
+}  // namespace names
+}  // namespace enterprise_signals
---
+++ components/enterprise/signals/common/signals_constants.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
+#define COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
+
+namespace enterprise_signals {
+
+// Signal names can be used as keys to store/retrieve signal values from
+// dictionaries.
+namespace names {
+
+extern const char kFileSystemInfo[];
+extern const char kSettings[];
+extern const char kAntiVirusInfo[];
+extern const char kInstalledHotfixes[];
+
+}  // namespace names
+
+}  // namespace enterprise_signals
+
+#endif  // COMPONENTS_ENTERPRISE_SIGNALS_COMMON_SIGNALS_CONSTANTS_H_
