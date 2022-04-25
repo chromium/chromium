00422e71754140d6bcdba4d8716837e515a6292c
Guillaume Jenkins <gujen@google.com>
2022-04-25 20:35:43 +0000

    [Notes] Add note diff calculation logic
    Adds user_note_utils namespace and a function to compute the difference
    between notes that are currently showing in frames of open tabs, and the
    metadata returned from the database for the URLs of these open tabs.
    
    For each open frame, the function looks at the note metadata for the
    corresponding URL, and from that, sorts note IDs into 3 categories:
      1) Notes that are in the metadata snapshot but not in the page are
         brand new notes that didn't exist when we first loaded notes into
         the frame.
      2) Notes that are in both the metadata snapshot and the frame, but
         whose modification time is greater in the metadata snapshot are
         notes that have been modified since we loaded them in the frame.
      3) Finally, notes that are in the frame but not in the metadata
         snapshot are notes that have been deleted since we loaded them in
         the frame.
    
    The function returns the computed diffs as an array of
    FrameUserNoteChanges, one for each frame where notes have changed. Frames
    whose notes have not changed will not produce a FrameUserNoteChanges
    object.
    
    Comprehensive unit tests will come in the next CL.
    
    Fixed: 1313146
    Change-Id: Icadec489def1f969fe14bed6f91d517864af11d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3596423
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#995732}

---
+++ components/user_notes/browser/user_note_utils.cc
@@ -0,0 +1,87 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/user_notes/browser/user_note_utils.h"
+
+#include "base/unguessable_token.h"
+#include "components/user_notes/browser/frame_user_note_changes.h"
+#include "components/user_notes/browser/user_notes_manager.h"
+#include "components/user_notes/interfaces/user_note_metadata_snapshot.h"
+#include "components/user_notes/model/user_note_metadata.h"
+#include "content/public/browser/render_frame_host.h"
+#include "url/gurl.h"
+
+namespace user_notes {
+
+std::vector<FrameUserNoteChanges> CalculateNoteChanges(
+    const std::vector<content::RenderFrameHost*>& rfhs,
+    const UserNoteMetadataSnapshot& metadata_snapshot) {
+  std::vector<FrameUserNoteChanges> result;
+
+  // Create an empty metadata map to simplify the algorithm below for cases
+  // where there's no entry in the metadata snapshot for a frame's URL.
+  UserNoteMetadataSnapshot::IdToMetadataMap empty_map;
+
+  for (content::RenderFrameHost* rfh : rfhs) {
+    // Notes should only be processed for the primary page.
+    DCHECK(rfh->GetMainFrame()->IsInPrimaryMainFrame());
+
+    UserNotesManager* notes_manager =
+        UserNotesManager::GetForPage(rfh->GetPage());
+
+    if (!notes_manager) {
+      // Frame is part of an uninteresting page (for User Notes purposes), such
+      // as the error document.
+      continue;
+    }
+
+    const std::vector<UserNoteInstance*>& instances =
+        notes_manager->GetAllNoteInstances();
+    GURL url = rfh->GetLastCommittedURL();
+    const UserNoteMetadataSnapshot::IdToMetadataMap* map_for_url =
+        metadata_snapshot.GetMapForUrl(url);
+    const UserNoteMetadataSnapshot::IdToMetadataMap* metadata_map =
+        map_for_url ? map_for_url : &empty_map;
+
+    std::vector<base::UnguessableToken> added;
+    std::vector<base::UnguessableToken> modified;
+    std::vector<base::UnguessableToken> removed;
+
+    // First, iterate through the existing notes in the frame to detect notes
+    // that have been removed and notes that have been modified.
+    for (UserNoteInstance* instance : instances) {
+      const UserNote& model = instance->model();
+      const auto& metadata_it = metadata_map->find(model.id());
+      if (metadata_it == metadata_map->end()) {
+        if (url == model.target().target_page()) {
+          // Note has been removed.
+          removed.emplace_back(base::UnguessableToken(model.id()));
+        }
+      } else if (metadata_it->second->modification_date() >
+                 model.metadata().modification_date()) {
+        // Note has been modified.
+        modified.emplace_back(base::UnguessableToken(model.id()));
+      }
+    }
+
+    // Finally, iterate through the notes in the metadata to detect notes that
+    // have been added.
+    for (const auto& metadata_it : *metadata_map) {
+      const base::UnguessableToken& id = metadata_it.first;
+      if (!notes_manager->GetNoteInstance(id)) {
+        // This is a new note.
+        added.emplace_back(base::UnguessableToken(id));
+      }
+    }
+
+    if (!added.empty() || !removed.empty() || !modified.empty()) {
+      result.emplace_back(FrameUserNoteChanges(
+          rfh, std::move(added), std::move(modified), std::move(removed)));
+    }
+  }
+
+  return result;
+}
+
+}  // namespace user_notes
