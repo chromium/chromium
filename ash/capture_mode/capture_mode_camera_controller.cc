52f16fce026a51a01c29e1cdc1c6ff3a83e54859
Michele Fan <michelefan@chromium.org>
2022-04-26 03:12:47 +0000

    capture-mode-selfie: Fix Preview Initial Size
    CalculatePreviewInitialSize() in camera controller uses the full
    workarea shorter side and doesn't divide it by the divider. Fix the wrong initial size by dividing by the divider.
    
    Change-Id: I7272bc914450d417de9ce8a63931a389c5952551
    Bug: 1319665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3606156
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#995952}

--- ash/capture_mode/capture_mode_camera_controller.cc
+++ ash/capture_mode/capture_mode_camera_controller.cc
@@ -274,7 +274,9 @@ gfx::Size CalculatePreviewInitialSize() {
     const int shorter_side = std::min(work_area.width(), work_area.height());
     max_shorter_side = std::max(max_shorter_side, shorter_side);
   }
-  return gfx::Size(max_shorter_side, max_shorter_side);
+  const int preview_diameter =
+      max_shorter_side / capture_mode::kCaptureSurfaceShortSideDivider;
+  return gfx::Size(preview_diameter, preview_diameter);
 }
 
 // Returns the appropriate `message_id` for ChromeVox alert on setting camera
