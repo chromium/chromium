0706e69a53d7c373e069d0e0c752bf600c92e79d
Renato Silva <rrsilva@google.com>
2022-04-26 11:56:53 +0000

    SAML - Re-enable tests on release builds.
    These tests were disabled on all builds, but we should strive to have
    them running at least on release builds until the issue is fixed.
    
    Bug: 1315447
    Change-Id: I4810225d9e5d73f3ed19614b841e430ffe66bf69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608148
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996108}

--- chrome/browser/ash/login/saml/saml_browsertest.cc
+++ chrome/browser/ash/login/saml/saml_browsertest.cc
@@ -722,8 +722,13 @@ IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, ScrapedMultiple) {
 }
 
 // Tests the no password scraped flow.
-// TODO(crbug.com/1315447): Disabled due to flakiness.
-IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, DISABLED_ScrapedNone) {
+// TODO(crbug.com/1315447): Disabled for debug builds.
+#if !defined(NDEBUG)
+#define MAYBE_ScrapedNone DISABLED_ScrapedNone
+#else
+#define MAYBE_ScrapedNone ScrapedNone
+#endif
+IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, MAYBE_ScrapedNone) {
   base::HistogramTester histogram_tester;
   fake_saml_idp()->SetLoginHTMLTemplate("saml_login_no_passwords.html");
 
@@ -813,8 +818,13 @@ IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures,
 
 // Tests the password confirm flow when more than one password is scraped: show
 // error on the first failure and fatal error on the second failure.
-// TODO(crbug.com/1315447): Disabled due to flakiness.
-IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, DISABLED_PasswordConfirmFlow) {
+// TODO(crbug.com/1315447): Disabled for debug builds.
+#if !defined(NDEBUG)
+#define MAYBE_PasswordConfirmFlow DISABLED_PasswordConfirmFlow
+#else
+#define MAYBE_PasswordConfirmFlow PasswordConfirmFlow
+#endif
+IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, MAYBE_PasswordConfirmFlow) {
   fake_saml_idp()->SetLoginHTMLTemplate("saml_login_two_passwords.html");
   StartSamlAndWaitForIdpPageLoad(
       saml_test_users::kFirstUserCorpExampleComEmail);
