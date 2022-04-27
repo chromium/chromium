ed0ef74eb1a8a485d5e3672c9509264ef4bdee45
Jérôme Lebel <jlebel@chromium.org>
2022-04-27 14:01:34 +0000

    [iOS] Disable some FeedManagementTestCase test for devices
    Disable tests on iphone-device and ipad-device:
    FeedManagementTestCase/testOpenFeedManagementSurface
    FeedManagementTestCase/testOpenFeedManagementSurface
    
    Bug: 1320264
    Change-Id: I4520df51ec407d1736db2e4ba2d50fdfbab3fb72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610133
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996675}

--- ios/chrome/browser/ui/ntp/feed_management/feed_management_egtest.mm
+++ ios/chrome/browser/ui/ntp/feed_management/feed_management_egtest.mm
@@ -82,7 +82,14 @@ void DismissSignOut() {
   DismissSignOut();
 }
 
-- (void)testSignedInOpenAndCloseFeedMenu {
+// TODO(crbug.com/1277545): Test fails on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testSignedInOpenAndCloseFeedMenu testSignedInOpenAndCloseFeedMenu
+#else
+#define MAYBE_testSignedInOpenAndCloseFeedMenu \
+  DISABLED_testSignedInOpenAndCloseFeedMenu
+#endif
+- (void)MAYBE_testSignedInOpenAndCloseFeedMenu {
   // Sign into a fake identity.
   FakeChromeIdentity* fakeIdentity1 = [FakeChromeIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity1];
@@ -101,7 +108,14 @@ void DismissSignOut() {
   DismissSignOut();
 }
 
-- (void)testOpenFeedManagementSurface {
+// TODO(crbug.com/1277545): Test fails on device.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testOpenFeedManagementSurface testOpenFeedManagementSurface
+#else
+#define MAYBE_testOpenFeedManagementSurface \
+  DISABLED_testOpenFeedManagementSurface
+#endif
+- (void)MAYBE_testOpenFeedManagementSurface {
   // Sign into a fake identity.
   FakeChromeIdentity* fakeIdentity1 = [FakeChromeIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity1];
