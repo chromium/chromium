d72fd4a4881a5284e30edb661ddc47ee4d535594
Daniel Cheng <dcheng@chromium.org>
2022-04-27 01:58:42 +0000

    Remove use of "generic" MojoUnion::New() helper in //chromeos/services/federated
    The New() method for unions has a generic-sounding name but actually
    creates a union containing the first declared subtype. In general, this
    behavior is non-intuitive; it can even give the impression that it
    creates an empty union that doesn't contain any of its subtypes.  It is
    also inefficient: many uses of the New() method immediately populate a
    different subtype, so the initial allocation to hold the first declared
    subtype is simply allocator churn.
    
    For these reasons, usage of MojoUnion::New() is being phased out and
    removed.
    
    This CL was uploaded by git cl split.
    
    R=martis@chromium.org
    
    Bug: 1261169
    Change-Id: I0efc035b1b42fd730144a67fdd7a973f0f2d385b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3602231
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996483}

--- chromeos/services/federated/public/cpp/federated_example_util.cc
+++ chromeos/services/federated/public/cpp/federated_example_util.cc
@@ -8,24 +8,15 @@ namespace chromeos {
 namespace federated {
 
 mojom::ValueListPtr CreateInt64List(const std::vector<int64_t>& values) {
-  mojom::ValueListPtr value_list = mojom::ValueList::New();
-  value_list->set_int64_list(mojom::Int64List::New());
-  value_list->get_int64_list()->value = values;
-  return value_list;
+  return mojom::ValueList::NewInt64List(mojom::Int64List::New(values));
 }
 
 mojom::ValueListPtr CreateFloatList(const std::vector<double>& values) {
-  mojom::ValueListPtr value_list = mojom::ValueList::New();
-  value_list->set_float_list(mojom::FloatList::New());
-  value_list->get_float_list()->value = values;
-  return value_list;
+  return mojom::ValueList::NewFloatList(mojom::FloatList::New(values));
 }
 
 mojom::ValueListPtr CreateStringList(const std::vector<std::string>& values) {
-  mojom::ValueListPtr value_list = mojom::ValueList::New();
-  value_list->set_string_list(mojom::StringList::New());
-  value_list->get_string_list()->value = values;
-  return value_list;
+  return mojom::ValueList::NewStringList(mojom::StringList::New(values));
 }
 
 }  // namespace federated
