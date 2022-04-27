a79690768271d9ef24f068c27d12c55a6decee52
Frank Liberato <liberato@chromium.org>
2022-04-27 20:19:10 +0000

    Reland "Improve full screen toast for picture in picture."
    This is a reland of commit 4467745fff4dab3f622baf75e40aca552b9ab6dc
    
    This change removes the assert, and replaces it with a no-op.
    
    TL;DR: ordering guarantees aren't guaranteed, and doing
    nothing is okay since it'll be consistent shortly.
    
    Original change's description:
    > Improve full screen toast for picture in picture.
    >
    > Previously, chrome would display a toast to tell the user how to
    > exit full screen.  When switching to picture in picture, the toast
    > would fail to fade out since the fade-out timer was stopped while
    > the toast was still on-screen.  This resulted in the toast being
    > visible in the picture in picture window indefinitely.
    >
    > This CL generally cleans up the toast logic.  In the process, it
    > fixes that bug.  In particular, there is no longer any time at which
    > the toast is attached to the view hierarchy that there is not also a
    > timer running to remove it.
    >
    > Once we enter a state where we believe that the user must see the
    > toast, we'll stay in that state until either (a) we successfully
    > show the toast long enough for the user to see it, or (b) we exit
    > fullscreen.  If we find a point where we shouldn't show the toast
    > right now, like entering picture in picture or losing the focus,
    > then we'll immediately hide the toast.  When we get back into a
    > state where the toast is relevant, we'll re-show it.
    >
    > Previously, the toast timer could be stopped and re-started when
    > the toast was off screen.  This CL also does away with that; the
    > toast must be on-screen for a sufficiently long, continuous amount
    > of time so the user can read it.  The user probably does not read
    > half until it disappears due to entering picture in picture, then
    > read the other half; they probably start over.  At least, that's
    > not what I seem to do.
    >
    > Toast animations are also cleaned up.  We now only fade out the
    > toast when the timer expires.  If we exit full screen, or enter
    > picture in picture, then we abruptly remove the toast as part of
    > the transition.  When we re-show the toast, we'll fade it back in.
    > This generally looks nicer; the transitions into and out of the
    > cases where we'd like to show a toast are abrupt anyway, so
    > removing the toast is invisible.
    >
    > Change-Id: I97737cb557695933ae114eb37b1463aca2ed1b24
    > Bug: 1316051, 1311683
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3585548
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#995847}
    
    Bug: 1316051, 1311683
    Change-Id: I0915b728a54777f0f0436d3bfec5ce0da4e1bcf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608715
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996865}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenHtmlApiHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenHtmlApiHandler.java
@@ -22,6 +22,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.core.util.ObjectsCompat;
 
 import org.chromium.base.ActivityState;
@@ -76,6 +77,8 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
     private static final long CLEAR_LAYOUT_FULLSCREEN_DELAY_MS = 20;
     // Fade in/out animation duration for fullscreen notification toast.
     private static final int TOAST_FADE_MS = 500;
+    // Time that the notification toast remains on-screen before starting to fade out.
+    private static final int TOAST_SHOW_DURATION_MS = 5000;
 
     private final Activity mActivity;
     private final Handler mHandler;
@@ -98,11 +101,32 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
 
     // Toast at the top of the screen that is shown when user enters fullscreen for the
     // first time.
+    //
+    // This is whether we believe that we need to show the user a notification toast.  It's false if
+    // we're not in full screen, or if we are in full screen but have already shown the toast for
+    // enough time for the user to read it.  The toast might or might not actually be on-screen
+    // right now; we remove it in some cases like when we lose window focus.  However, as long as
+    // we'll be in full screen, we still keep the toast pending until we successfully show it.
+    private boolean mIsNotificationToastPending;
+
+    // Sometimes, the toast must be removed temporarily, such as when we lose focus or if we
+    // transition to picture-in-picture.  In those cases, the toast is removed from the view
+    // hierarchy, and these fields are cleared.  The toast will be re-created from scratch when it's
+    // appropriate to show it again.  `mIsNotificationToastPending` won't be reset in those cases,
+    // though, since we'll still want to show the toast when it's possible to do so.
+    //
+    // If `mNotificationToast` exists, then it's attached to the view hierarchy, though it might be
+    // animating to or from alpha=0.  Any time the toast exists, we also have an animation for it,
+    // to allow us to fade it in, and eventually back out.  The animation is not cleared when it
+    // completes; it's only cleared when we also detach the toast and clear `mNotificationToast`.
+    //
+    // Importantly, it's possible that `mNotificationToast` is not null while no toast is pending.
+    // This can happen when the toast has been on-screen long enough, and is fading out.
     private View mNotificationToast;
-    private long mNotificationStartTimestamp;
-    private long mNotificationRemainingTimeMs;
-    private final Runnable mHideNotificationToastRunnable;
     private ViewPropertyAnimator mToastFadeAnimation;
+
+    // Runnable that will complete the current toast and fade it out.
+    private final Runnable mFadeOutNotificationToastRunnable;
 
     private OnLayoutChangeListener mFullscreenOnLayoutChangeListener;
 
@@ -224,7 +248,7 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
         mPersistentModeSupplier = new ObservableSupplierImpl<>();
         mPersistentModeSupplier.set(false);
         mExitFullscreenOnStop = exitFullscreenOnStop;
-        mHideNotificationToastRunnable = this::hideNotificationToast;
+        mFadeOutNotificationToastRunnable = this::fadeOutNotificationToast;
     }
 
     /**
@@ -338,7 +362,8 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
         }
     }
 
-    private void destroySelectActionMode(Tab tab) {
+    @VisibleForTesting
+    /* package */ void destroySelectActionMode(Tab tab) {
         WebContents webContents = tab.getWebContents();
         if (webContents != null) {
             SelectionPopupController.fromWebContents(webContents).destroySelectActionMode();
@@ -398,6 +423,7 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
     @Override
     public void exitPersistentFullscreenMode() {
         if (getPersistentFullscreenMode()) {
+            cancelNotificationToast();
             mPersistentModeSupplier.set(false);
 
             if (mWebContentsInFullscreen != null && mTabInFullscreen != null) {
@@ -430,7 +456,7 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
     }
 
     private void exitFullscreen(WebContents webContents, View contentView, Tab tab) {
-        hideNotificationToast();
+        cancelNotificationToast();
         mHandler.removeMessages(MSG_ID_SET_FULLSCREEN_SYSTEM_UI_FLAGS);
         mHandler.removeMessages(MSG_ID_CLEAR_LAYOUT_FULLSCREEN_FLAG);
 
@@ -540,6 +566,7 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
         if (mFullscreenOnLayoutChangeListener != null) {
             contentView.removeOnLayoutChangeListener(mFullscreenOnLayoutChangeListener);
         }
+
         mFullscreenOnLayoutChangeListener = new OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom,
@@ -554,10 +581,7 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
 
                 if ((bottom - top) <= (oldBottom - oldTop)) return;
 
-                // The toast tells user how to leave fullscreen by touching the screen. Currently
-                // we do not show the toast when we're browsing in VR, since VR doesn't have
-                // touchscreen and the toast doesn't have any useful information.
-                if (shouldShowToast()) showNotificationToast();
+                beginNotificationToast();
                 contentView.removeOnLayoutChangeListener(this);
             }
         };
@@ -580,48 +604,153 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
      * Whether we show a toast message when entering fullscreen.
      */
     private boolean shouldShowToast() {
-        return !(VrModuleProvider.getDelegate().isInVr()
-                || VrModuleProvider.getDelegate().bootsToVr());
+        // If there's no notification toast pending, such as when we're not in full screen or after
+        // we've already displayed it for longe enough, then we don't need to show the toast now.
+        if (!mIsNotificationToastPending) return false;
+
+        if (mTabInFullscreen == null) return false;
+
+        if (mTab == null) return false;
+
+        // The toast tells user how to leave fullscreen by touching the screen. Currently
+        // we do not show the toast when we're browsing in VR, since VR doesn't have
+        // touchscreen and the toast doesn't have any useful information.
+        if (VrModuleProvider.getDelegate().isInVr() || VrModuleProvider.getDelegate().bootsToVr()) {
+            return false;
+        }
+
+        final ViewGroup parent = mTab.getContentView();
+        if (parent == null) return false;
+
+        // The window must have the focus, so that it is not obscured while the notification is
+        // showing.  This also covers the case of picture in picture video, but any case of an
+        // unfocused window should prevent the toast.
+        if (!parent.hasWindowFocus()) return false;
+
+        return true;
     }
 
     /**
-     * Create and show the fullscreen notification toast.
+     * Create and show the fullscreen notification toast, if it's not already visible and if it
+     * should be visible.  It's okay to call this when it should not be; we'll do nothing.  This
+     * will fade the toast in if needed.  It will also schedule a timer to fade it back out, if it's
+     * not hidden or cancelled before then.
      */
-    private void showNotificationToast() {
+    private void createAndShowNotificationToast() {
+        // If it's already visible, then that's fine.  That includes if it's currently fading out;
+        // that's part of it.
+        if (mNotificationToast != null) return;
+
+        // If the toast should not be visible, then do nothing.
+        if (!shouldShowToast()) return;
+
         assert mTab != null && mTab.getContentView() != null;
-        ViewGroup parent = mTab.getContentView();
-        if (mNotificationToast != null) {
-            assert mToastFadeAnimation != null;
-            mToastFadeAnimation.cancel();
-            parent.removeView(mNotificationToast);
+
+        // Create a new toast and fade it in, or re-use one we've created before.
+        mNotificationToast = mActivity.getWindow().findViewById(R.id.fullscreen_notification);
+        boolean addView = false;
+        if (mNotificationToast == null) {
+            mNotificationToast =
+                    LayoutInflater.from(mActivity).inflate(R.layout.fullscreen_notification, null);
+            addView = true;
         }
-        mNotificationToast =
-                LayoutInflater.from(mActivity).inflate(R.layout.fullscreen_notification, null);
         mNotificationToast.setAlpha(0);
-        parent.addView(mNotificationToast);
         mToastFadeAnimation = mNotificationToast.animate();
-        mToastFadeAnimation.alpha(1).setDuration(TOAST_FADE_MS).start();
-        mNotificationRemainingTimeMs = 5000;
-        if (parent.hasWindowFocus()) {
-            mNotificationStartTimestamp = System.currentTimeMillis();
-            mHandler.postDelayed(mHideNotificationToastRunnable, mNotificationRemainingTimeMs);
+        if (addView) {
+            mActivity.addContentView(mNotificationToast,
+                    new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+        } else {
+            mNotificationToast.setVisibility(View.VISIBLE);
         }
+
+        mToastFadeAnimation.alpha(1).setDuration(TOAST_FADE_MS).start();
+        mHandler.postDelayed(mFadeOutNotificationToastRunnable, TOAST_SHOW_DURATION_MS);
     }
 
     /**
-     * Hides the notification toast.
+     * Pause the notification toast, which hides it and stops all the timers.  It's okay if there is
+     * not currently a toast; we don't change any state in that case.  This will abruptly hide the
+     * toast, rather than fade it out.  This does not change `mIsNotificationToastPending`; the
+     * toast hasn't been shown long enough.
      */
-    private void hideNotificationToast() {
+    private void hideImmediatelyNotificationToast() {
         if (mNotificationToast == null) return;
+
+        // Stop the fade-out timer.
+        mHandler.removeCallbacks(mFadeOutNotificationToastRunnable);
+
+        // Remove it immediately, without fading out.
         assert mToastFadeAnimation != null;
-        mToastFadeAnimation.alpha(0).setDuration(TOAST_FADE_MS).withEndAction(() -> {
-            // The Tab might have been destroyed while the toast is on.
-            if (mTab != null && mTab.getContentView() != null) {
-                mTab.getContentView().removeView(mNotificationToast);
-            }
-            mNotificationToast = null;
-            mToastFadeAnimation = null;
-        });
+        mToastFadeAnimation.cancel();
+        mToastFadeAnimation = null;
+        // We can't actually remove it, so this will do.
+        mNotificationToast.setVisibility(View.GONE);
+        mNotificationToast = null;
+    }
+
+    /**
+     * Begin a new instance of the notification toast.  If the toast should not be shown right now,
+     * we'll start showing it when we can.
+     */
+    private void beginNotificationToast() {
+        // It would be nice if we could determine that we're not starting a new toast while a
+        // previous one is fading out.  We can't ask the animation for its current target value.  We
+        // could almost check that there's not a notification pending and also that there's no
+        // current toast.  When a notification is pending, the previous toast hasn't completed yet,
+        // so nobody should be starting a new one.  When `mNotificationToast` is not null, but
+        // pending is false, then the fade-out animation has started but not completed.  Only when
+        // they're both false is it in the steady-state of "no notification" that would let us start
+        // a new one.
+        //
+        // The problem with that is that there are cases when we double-enter fullscreen.  In
+        // particular, changing the visibility of the navigation bar and/or status bar can cause us
+        // to think that we're entering fullscreen without an intervening exit.  In this case, the
+        // right thing to do is to continue with the toast from the previous full screen, if it's
+        // still on-screen.  If it's fading out now, just let it continue to fade out.  The user has
+        // already seen it for the full duration, and we've not actually exited fullscreen.
+        if (mNotificationToast != null) {
+            // Don't reset the pending flag here -- either it's on the screen or fading out, and
+            // either way is correct.  We have not actually exited fullscreen, so we shouldn't
+            // re-display the notification.
+            return;
+        }
+
+        mIsNotificationToastPending = true;
+        createAndShowNotificationToast();
+    }
+
+    /**
+     * Cancel a toast immediately, without fading out.  For example, if we leave fullscreen, then
+     * the toast isn't needed anymore.
+     */
+    private void cancelNotificationToast() {
+        hideImmediatelyNotificationToast();
+        // Don't restart it either.
+        mIsNotificationToastPending = false;
+    }
+
+    /**
+     * Called when the notification toast should not be shown any more, because it's been on-screen
+     * long enough for the user to read it.  To re-show it, one must call `beginNotificationToast()`
+     * again.  Show / hide of the toast will no-op until then.
+     */
+    private void fadeOutNotificationToast() {
+        if (mNotificationToast == null) return;
+
+        // Clear this first, so that we know that the toast timer has expired already.
+        mIsNotificationToastPending = false;
+
+        // Cancel any timer that will start the fade-out animation, in case it's running.  It might
+        // not be, especially if we're called by it.
+        mHandler.removeCallbacks(mFadeOutNotificationToastRunnable);
+
+        // Start the fade-out animation.
+        assert mToastFadeAnimation != null;
+        mToastFadeAnimation.cancel();
+        mToastFadeAnimation.alpha(0)
+                .setDuration(TOAST_FADE_MS)
+                .withEndAction(this::hideImmediatelyNotificationToast);
     }
 
     // ActivityStateListener
@@ -653,15 +782,20 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
     @Override
     public void onWindowFocusChanged(Activity activity, boolean hasWindowFocus) {
         if (mActivity != activity) return;
-        if (mNotificationToast != null) {
-            if (hasWindowFocus) {
-                mNotificationStartTimestamp = System.currentTimeMillis();
-                mHandler.postDelayed(mHideNotificationToastRunnable, mNotificationRemainingTimeMs);
-            } else {
-                mHandler.removeCallbacks(mHideNotificationToastRunnable);
-                mNotificationRemainingTimeMs -=
-                        System.currentTimeMillis() - mNotificationStartTimestamp;
-            }
+
+        // Try to show / hide the toast, if we need to.  Note that these won't do anything if the
+        // toast should not be visible, such as if we re-gain the window focus after having
+        // completed the most recently started notification toast.
+        //
+        // Also note that this handles picture-in-picture.  We definitely do not want the toast to
+        // be visible then; it's not relevant and also takes up almost all of the window.  We could
+        // also do this on ActivityStateChanged => PAUSED if Activity.isInPictureInPictureMode(),
+        // but it doesn't seem to be needed.
+        if (hasWindowFocus) {
+            createAndShowNotificationToast();
+        } else {
+            // While we don't have the focus, hide any ongoing notification.
+            hideImmediatelyNotificationToast();
         }
 
         mHandler.removeMessages(MSG_ID_SET_FULLSCREEN_SYSTEM_UI_FLAGS);
@@ -751,5 +885,9 @@ public class FullscreenHtmlApiHandler implements ActivityStateListener, WindowFo
 
     void setTabForTesting(Tab tab) {
         mTab = tab;
+    }
+
+    boolean isToastVisibleForTesting() {
+        return mNotificationToast != null;
     }
 }
