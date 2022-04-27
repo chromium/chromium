243053e18d455d8331909d4f84afd5c6c0157053
Ravjit Uppal <ravjit@chromium.org>
2022-04-27 21:04:58 +0000

    Update decision threshold for notification prediction from 0.83 to 0.81
    Due to change in the ML models generated on the backend, we need to update the threshold values on client to be in sync.
    
    (cherry picked from commit 5c27e45f97bf3466ea8720a700d3bfad89470326)
    
    Bug: 1319801
    Change-Id: I856a7ad57400f0492bf1edaa6533d548c4b48231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3608128
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#996105}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612002
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Auto-Submit: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5005@{#210}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- components/permissions/prediction_service/prediction_common.h
+++ components/permissions/prediction_service/prediction_common.h
@@ -22,7 +22,7 @@ constexpr float kRoundToMultiplesOf = 0.1f;
 constexpr int kCountBuckets[] = {20, 15, 12, 10, 9, 8, 7, 6, 5, 4};
 
 // Thresholds of the likelihood that triggers the CPSS prompts.
-constexpr float kNotificationPredictionsThreshold = 0.83;
+constexpr float kNotificationPredictionsThreshold = 0.81;
 constexpr float kGeolocationPredictionsThreshold = 0.87;
 
 // Returns the ratio rounded to the nearest 10%. It returns a value between 0
