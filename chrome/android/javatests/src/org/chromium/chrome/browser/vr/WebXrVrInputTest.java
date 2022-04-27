fd68f0ea40db76d6845b5e8b0df3c19f0fa85c27
Brian Sheedy <bsheedy@chromium.org>
2022-04-27 20:05:05 +0000

    Deflake WebXR screen tap test
    (Hopefully) deflakes the WebXR VR screen tap input tests by making the
    test wait for each screen tap to be acked before sending another. In a
    perfect world, every tap would be registered regardless of how many we
    send or how quickly, but we've previously run into issues with taps
    being eaten by garbage collection on older devices. That issue looks
    like it might still be persisting, so only sending one tap at a time
    should hopefully work around the issue.
    
    Bug: 1312033
    Change-Id: Ic60817ce29a0bc893b9e8d3e082de280795e2bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611525
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996855}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
@@ -263,11 +263,15 @@ public class WebXrVrInputTest {
         // Android doesn't seem to like sending touch events too quickly, so have a short delay
         // between events.
         for (int i = 0; i < iterations; i++) {
-            long downTime = sendScreenTouchDown(view, x, y);
-            SystemClock.sleep(100);
-            sendScreenTouchUp(view, x, y, downTime);
-            SystemClock.sleep(100);
+            sendScreenTap(view, x, y);
         }
+    }
+
+    private void sendScreenTap(final View view, final int x, final int y) {
+        long downTime = sendScreenTouchDown(view, x, y);
+        SystemClock.sleep(100);
+        sendScreenTouchUp(view, x, y, downTime);
+        SystemClock.sleep(100);
     }
 
     /**
@@ -313,20 +317,20 @@ public class WebXrVrInputTest {
     // Note that the page should load the appropriate test page and enter any relevant session
     // before calling this.
     private void testScreenTapsRegisteredOnCardboardImpl(View presentationView, int numIterations) {
-        // Make it so that the webpage doesn't try to finish the JavaScript step after each input
-        // since we don't need to ack each one like with the Daydream controller.
-        mWebXrVrTestFramework.runJavaScriptOrFail(
-                "finishAfterEachInput = false", POLL_TIMEOUT_SHORT_MS);
         mWebXrVrTestFramework.runJavaScriptOrFail(
                 "stepSetupListeners(" + String.valueOf(numIterations) + ")", POLL_TIMEOUT_SHORT_MS);
 
         int x = presentationView.getWidth() / 2;
         int y = presentationView.getHeight() / 2;
 
-        // Tap the screen a bunch of times and make sure that they're all registered.
-        spamScreenTaps(presentationView, x, y, numIterations);
+        // Tap the screen a bunch of times and make sure that they're all registered. Ideally, we
+        // shouldn't have to ack each one, but it's possible for inputs to get eaten by garbage
+        // collection if there are multiple in flight, so only send one at a time.
+        for (int i = 0; i < numIterations; i++) {
+            sendScreenTap(presentationView, x, y);
+            mWebXrVrTestFramework.waitOnJavaScriptStep();
+        }
 
-        mWebXrVrTestFramework.waitOnJavaScriptStep();
         mWebXrVrTestFramework.endTest();
     }
 
