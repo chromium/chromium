d5d441dcdc2d776b9f8ab65978d1f37860c967c4
hmare <hmare@google.com>
2022-04-28 20:49:38 +0000

    Add signal names to signals constants file
    This just change adds the signal names from the attestation protos
    to the signals_constants file. This is need so the new signals
    dictionary can use the constants names as a key in the follow up cl.
    
    Bug: b/230644308
    Change-Id: I802fb58340ce792cc94b8015a66e32459a691954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611235
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997397}

--- components/enterprise/common/signals/signals_constants.cc
+++ components/enterprise/common/signals/signals_constants.cc
@@ -7,22 +7,133 @@
 namespace enterprise_signals {
 namespace names {
 
-// Name of the parameterized signal for getting information from resources
-// stored on the file system. This includes the presence/absence of
-// files/folders, and also additional signals' extraction from executables.
-const char kFileSystemInfo[] = "fileSystemInfo";
-
-// Name of the parameterized signal for getting information from settings
-// storage (e.g. Registry, Plist) on the device.
-const char kSettings[] = "settings";
+// Name of the signal for getting information of the AllowScreenLock
+// policy https://chromeenterprise.google/policies/?policy=AllowScreenLock.
+const char kAllowScreenLock[] = "allowSreenLock";
 
 // Name of the signal for getting information about AV software installed on
 // the device.
 const char kAntiVirusInfo[] = "antiVirusInfo";
 
-// Name of the signal for getting information about installed hotfixes on the
-// device.
+// Name of the signal for getting information about the browser version.
+const char kBrowserVersion[] = "browserVersion";
+
+// Name of the signal for getting information about whether a built in
+// dns is enabled on the device.
+const char kBuiltInDnsClientEnabled[] = "builtInDnsClientEnabled";
+
+// Name of the signal for getting information about whether chrome cleanup
+// is enabled on the device.
+const char kChromeCleanupEnabled[] = "chromeCleanupEnabled";
+
+// Name of the signal for getting information about the device id.
+const char kDeviceId[] = "deviceId";
+
+// Name of the signal for getting information about the device
+// manufacturer (e.g. Dell).
+const char kDeviceManufacturer[] = "deviceManufacturer";
+
+// Name of the signal for getting information about the device model
+// (e.g. iPhone 12 Max).
+const char kDeviceModel[] = "deviceModel";
+
+// Name of the signal for getting information about the human readable
+// name for the device.
+const char kDisplayName[] = "displayName";
+
+// Name of the signal for getting information about the dns address of
+// the device.
+const char kDnsAddress[] = "dnsAddress";
+
+// Name of the signal for getting information about the CBCM enrollment
+// domain of the browser.
+const char kEnrollmentDomain[] = "enrollmentDomain";
+
+// Name of the parameterized signal for getting information from resources
+// stored on the file system. This includes the presence/absence of
+// files/folders, and also additional signals' extraction from executables.
+const char kFileSystemInfo[] = "fileSystemInfo";
+
+// Name of the signal for getting information about whether firewall is
+// enabled on the device.
+const char kFirewallOn[] = "firewallOn";
+
+// Name of the signal for getting information about the IMEI.
+const char kImei[] = "imei";
+
+// Name of the signal for getting information about installed hotfixes on
+// the device.
 const char kInstalledHotfixes[] = "hotfixes";
+
+// Name of the signal for getting information about whether the disk
+// on the device is encrypted.
+const char kIsDiskEncrypted[] = "isDiskEncrypted";
+
+// Name of the signal for getting information about whether the device is
+// jailbroken or modified.
+const char kIsJailbroken[] = "isJailBroken";
+
+// Name of the signal for getting information about whether access to
+// the OS user is protected by a password.
+const char kIsPasswordProtected[] = "isProtectedByPassword";
+
+// Name of the signal for getting information about the MEID.
+const char kMeid[] = "meid";
+
+// Name of the signal for getting information about the obfuscated CBCM
+// enrolled customer Id.
+const char kObfuscatedCustomerId[] = "obfuscatedCustomerId";
+
+// Name of the signal for getting information about the OS running
+// on the device (e.g. Chrome OS).
+const char kOs[] = "os";
+
+// Name of the signal for getting information about the OS version
+// of the device (e.g. macOS 10.15.7).
+const char kOsVersion[] = "osVersion";
+
+// Name of the signal for getting information about whether the device
+// has a password reuse protection warning trigger.
+const char kPasswordProtectionWarningTrigger[] =
+    "passwordPotectionWarningTrigger";
+
+// Name of the signal for getting information about whether users can
+// access other computers from Chrome using Chrome Remote Desktop.
+const char kRemoteDesktopAvailable[] = "remoteDesktopAvailable";
+
+// Name of the signal for getting information of the value of the
+// SafeBrowsingProtectionLevel policy.
+// https://chromeenterprise.google/policies/#SafeBrowsingProtectionLevel
+const char kSafeBrowsingProtectionLevel[] = "safeBrowsingProtectionLevel";
+
+// Name of the signal for getting information about the device serial
+// number.
+const char kSerialNumber[] = "serialNumber";
+
+// Name of the parameterized signal for getting information from settings
+// storage (e.g. Registry, Plist) on the device.
+const char kSettings[] = "settings";
+
+// Name of the signal for getting information about the signed in profile
+// name.
+const char kSignedInProfileName[] = "signedInProfileName";
+
+// Name of the signal for getting information of the value of the
+// SitePerProcess policy.
+// https://chromeenterprise.google/policies/#SitePerProcess
+const char kSiteIsolationEnabled[] = "siteIsolationEnabled";
+
+// Name of the signal for getting information about whether third party
+// blocking is enabled on the device.
+const char kThirdPartyBlockingEnabled[] = "thirdPartyBlockingEnabled";
+
+// Name of the signal for getting information about the hash
+// of the EKPub certificate of the TPM on the device, if available.
+const char kTpmHash[] = "tpmHash";
+
+// Name of the signal for getting information about the windows domain
+// the device has joined.
+const char kWindowsDomain[] = "windowsDomain";
 
 }  // namespace names
 }  // namespace enterprise_signals
--- components/enterprise/common/signals/signals_constants.h
+++ components/enterprise/common/signals/signals_constants.h
@@ -11,10 +11,38 @@ namespace enterprise_signals {
 // dictionaries.
 namespace names {
 
-extern const char kFileSystemInfo[];
-extern const char kSettings[];
+extern const char kAllowScreenLock[];
 extern const char kAntiVirusInfo[];
+extern const char kBrowserVersion[];
+extern const char kBuiltInDnsClientEnabled[];
+extern const char kChromeCleanupEnabled[];
+extern const char kDeviceId[];
+extern const char kDeviceManufacturer[];
+extern const char kDeviceModel[];
+extern const char kDisplayName[];
+extern const char kDnsAddress[];
+extern const char kEnrollmentDomain[];
+extern const char kFileSystemInfo[];
+extern const char kFirewallOn[];
+extern const char kImei[];
 extern const char kInstalledHotfixes[];
+extern const char kIsDiskEncrypted[];
+extern const char kIsJailbroken[];
+extern const char kIsPasswordProtected[];
+extern const char kMeid[];
+extern const char kObfuscatedCustomerId[];
+extern const char kOs[];
+extern const char kOsVersion[];
+extern const char kPasswordProtectionWarningTrigger[];
+extern const char kRemoteDesktopAvailable[];
+extern const char kSafeBrowsingProtectionLevel[];
+extern const char kSerialNumber[];
+extern const char kSettings[];
+extern const char kSignedInProfileName[];
+extern const char kSiteIsolationEnabled[];
+extern const char kThirdPartyBlockingEnabled[];
+extern const char kTpmHash[];
+extern const char kWindowsDomain[];
 
 }  // namespace names
 
