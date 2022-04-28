caa9d5f68737c9f9a5cc4ad1c985f67aa6fe608a
Peng Huang <penghuang@chromium.org>
2022-04-28 00:07:45 +0000

    Vulkan: fix DCHECK() crash in VulkanSurface
    The problem is because chrome and Vulkan WSI use two xcb
    connections, so it may cause out of sync problem during resizing
    chrome window.
    
    Bug: 1242767
    Change-Id: I8f765440b1e1b701a4f32bc7f15e6a562c5cd691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612225
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#996956}

--- ui/ozone/platform/x11/vulkan_surface_x11.cc
+++ ui/ozone/platform/x11/vulkan_surface_x11.cc
@@ -41,6 +41,7 @@ std::unique_ptr<VulkanSurfaceX11> VulkanSurfaceX11::Create(
     return nullptr;
   }
 
+  // TODO(penghuang): using the same xcb connection for VulkanSurface.
   VkSurfaceKHR vk_surface;
   const VkXcbSurfaceCreateInfoKHR surface_create_info = {
       .sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR,
@@ -104,10 +105,13 @@ bool VulkanSurfaceX11::Reshape(const gfx::Size& size,
                                gfx::OverlayTransform pre_transform) {
   DCHECK_EQ(pre_transform, gfx::OVERLAY_TRANSFORM_NONE);
 
-  auto* connection = x11::Connection::Get();
-  connection->ConfigureWindow(x11::ConfigureWindowRequest{
-      .window = window_, .width = size.width(), .height = size.height()});
-  connection->Flush();
+  // Vulkan WSI uses a separate xcb connection, so we need to synchronize
+  // ConfigureWindow call.
+  x11::Connection::Get()
+      ->ConfigureWindow(x11::ConfigureWindowRequest{
+          .window = window_, .width = size.width(), .height = size.height()})
+      .Sync();
+
   return VulkanSurface::Reshape(size, pre_transform);
 }
 
