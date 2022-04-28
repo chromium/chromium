c0690d3beb39cfc0a08c68598ebe4d216b02702d
Nico Weber <thakis@chromium.org>
2022-04-28 17:49:19 +0000

    Roll clang llvmorg-15-init-8609-g3254f468-1 : llvmorg-15-init-8945-g3d7da810-1
    Ran `./tools/clang/scripts/upload_revision.py 3d7da810e4a6e1ade2c0d649f17f8aa820248765`.
    
    Bug: 1319248
    Change-Id: I7a2516b5f0e42d7e3b0c6e2077a78efeb616747c
    Cq-Include-Trybots: chromium/try:android-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:win-asan,win7-rel
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612904
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997282}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,7 +35,7 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-15-init-8609-g3254f468'
+CLANG_REVISION = 'llvmorg-15-init-8945-g3d7da810'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
