def3925d4dfdc74d36e5339e7624a6e59c2a19fc
Mike Pinkerton <pinkerton@google.com>
2022-04-28 16:12:34 +0000

    [Swift][C++ interop] Add tests for passing C++ objects as parameters.
    Test passing C++ objects as pointers and references, to both other C++
    objects as well as Swift functions.
    
    Bug: none
    
    Change-Id: I179ba23db56d1c62342d6625a8079855f75651de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3611043
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997226}

---
+++ ios/chrome/test/swift_interop/pointer/object_passing_xctest.swift
@@ -0,0 +1,72 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Pointer
+import XCTest
+
+class ObjectPassingTest: XCTestCase {
+
+  func testReferenceParameters() throws {
+    var a = Object(10)
+    var b = Object(20)
+    #if swift(>=5.6)
+      let passer = ObjectPassing()
+    #else
+      var passer = ObjectPassing()
+    #endif
+    XCTAssertEqual(a.GetValue(), 10)
+    XCTAssertEqual(passer.AddReferences(&a, &b), 30)
+  }
+
+  func testPointerParameters() throws {
+    var a = Object(10)
+    var b = Object(20)
+    #if swift(>=5.6)
+      let passer = ObjectPassing()
+    #else
+      var passer = ObjectPassing()
+    #endif
+
+    XCTAssertEqual(a.GetValue(), 10)
+    XCTAssertEqual(passer.AddPointers(&a, &b), 30)
+  }
+
+  func testMismatchedParameters() throws {
+    // var obj = Object(10)
+    // var str = "Hello"
+    // let passer = ObjectPassing()
+
+    // Correctly doesn't compile because the types don't match. Error is
+    // "cannot convert value of type 'UnsafeMutablePointer<String>' to expected
+    // argument type 'UnsafeMutablePointer<Object>'.
+    // XCTAssertEqual(passer.AddPointers(&obj, &obj, &str), 60, "")
+  }
+
+  // Note: if this method returns `Int` instead of `Int32` the compiler
+  // will crash (filed as https://github.com/apple/swift/issues/58458).
+  // Note: prior to Swift 5.6, calling GetValue() (which is const) on these objects
+  // results in a compiler error about calling immutable methods on a `let` object.
+  // Omit this test on earlier Swift versions.
+  #if swift(>=5.6)
+    func addObjects(one: Object, two: Object) -> Int32 {
+      return one.GetValue() + two.GetValue()
+    }
+  #endif
+
+  func addPointerObjects(one: UnsafeMutablePointer<Object>, two: UnsafeMutablePointer<Object>)
+    -> Int32
+  {
+    return one.pointee.GetValue() + two.pointee.GetValue()
+  }
+
+  func testPassToSwift() throws {
+    var a = Object(10)
+    var b = Object(20)
+
+    #if swift(>=5.6)
+      XCTAssertEqual(addObjects(one: a, two: b), 30)
+    #endif
+    XCTAssertEqual(addPointerObjects(one: &a, two: &b), 30)
+  }
+}
