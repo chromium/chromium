a0dd2f7557ab2e33e43f8dd76eb715bcfdefe552
Veronique Nguyen <veronguyen@google.com>
2022-04-29 15:59:43 +0000

    [iOS] Add test case coverage for URL display changes
    - Add unit test for Reading List Table View Item.
    - Add unit test for Credential with mobile prefix in their URL.
    
    Bug: 1317115
    Change-Id: I039611e41a7bd11c8a648875256b18eed6880536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605747
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997728}

--- ios/chrome/browser/credential_provider/archivable_credential+password_form_unittest.mm
+++ ios/chrome/browser/credential_provider/archivable_credential+password_form_unittest.mm
@@ -136,4 +136,37 @@ TEST_F(ArchivableCredentialPasswordFormTest, PasswordFormFromCredential) {
   EXPECT_EQ(passwordForm.signon_realm, "http://www.alpha.example.com/");
 }
 
+// Tests the creation of a credential from a password form (that has a mobile
+// prefix).
+TEST_F(ArchivableCredentialPasswordFormTest, CreationWithMobileURL) {
+  NSString* username = @"username_value";
+  NSString* favicon = @"favicon_value";
+  NSString* keychainIdentifier = @"keychain_identifier_value";
+  NSString* validationIdentifier = @"validation_identifier_value";
+  NSString* url = @"http://m.alpha.example.com/path/and?args=8";
+
+  PasswordForm passwordForm;
+  passwordForm.times_used = 10;
+  passwordForm.username_element = u"username_element";
+  passwordForm.password_element = u"password_element";
+  passwordForm.username_value = base::SysNSStringToUTF16(username);
+  passwordForm.encrypted_password = base::SysNSStringToUTF8(keychainIdentifier);
+  passwordForm.url = GURL(base::SysNSStringToUTF16(url));
+  ArchivableCredential* credential =
+      [[ArchivableCredential alloc] initWithPasswordForm:passwordForm
+                                                 favicon:favicon
+                                    validationIdentifier:validationIdentifier];
+
+  EXPECT_TRUE(credential);
+  EXPECT_EQ(passwordForm.times_used, credential.rank);
+  EXPECT_NSEQ(username, credential.user);
+  EXPECT_NSEQ(favicon, credential.favicon);
+  EXPECT_NSEQ(validationIdentifier, credential.validationIdentifier);
+  EXPECT_NSEQ(keychainIdentifier, credential.keychainIdentifier);
+  EXPECT_NSEQ(@"alpha.example.com", credential.serviceName);
+  EXPECT_NSEQ(@"http://m.alpha.example.com/path/and?args=8|"
+              @"username_element|username_value|password_element|",
+              credential.recordIdentifier);
+}
+
 }  // namespace
