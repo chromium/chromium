32d397cc31b363a5874f3ca26ad4de2ecfe94a65
Bruce Dawson <brucedawson@chromium.org>
2022-04-29 14:54:50 +0000

    Check for SDK existence
    Chrome currently requires the 10.0.20348 SDK to be installed or it will
    not build. We request that specific version, but if it isn't installed
    we end up with LIB and INCLUDE environment variables that point to
    non-existent locations.
    
    This checks for non-existent locations and fails earlier with a clearer
    error message. The (simplified) message looks like this:
    
    Traceback (most recent call last):
      File ".../build/toolchain/win/setup_toolchain.py", line 316...
    ...
      File ".../build/toolchain/win/setup_toolchain.py", line 67...
        raise Exception(
    Exception: Path "C:\Program Files (x86)\Windows Kits\10\\include\10.0.20348.0\\shared" from environment variable "include" does not exist. Make sure the necessary SDK is installed.
    
    Bug: 1320520
    Change-Id: I8baf87f1bcdfe9931adf9270a04afa138928739e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615330
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997708}

--- build/toolchain/win/setup_toolchain.py
+++ build/toolchain/win/setup_toolchain.py
@@ -58,6 +58,15 @@ def _ExtractImportantEnvironment(output_of_set):
           # path. Add the path to this python here so that if it's not in the
           # path when ninja is run later, python will still be found.
           setting = os.path.dirname(sys.executable) + os.pathsep + setting
+        if envvar in ['include', 'lib']:
+          # Make sure that the include and lib paths point to directories that
+          # exist. This ensures a (relatively) clear error message if the
+          # required SDK is not installed.
+          for part in setting.split(';'):
+            if not os.path.exists(part):
+              raise Exception(
+                  'Path "%s" from environment variable "%s" does not exist. '
+                  'Make sure the necessary SDK is installed.' % (part, envvar))
         env[var.upper()] = setting
         break
   if sys.platform in ('win32', 'cygwin'):
