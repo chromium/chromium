0e21d9ea5271060f232ec09ddd23b6f4a9ccff5c
Kenichi Ishibashi <bashi@chromium.org>
2022-04-29 23:35:34 +0000

    Enable Early Hints preload/preconnect during navigation by default
    Intent-to-ship link:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/8iIvkmZNUhU/m/sTL81ZNaAwAJ
    
    Bug: 1197989
    Change-Id: I69ba6fda404d7b0b673b09cd75c297cef1c936af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3612442
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997992}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -281,7 +281,7 @@ const base::Feature kEarlyEstablishGpuChannel{
 
 // Enable Early Hints subresource preloads for navigation.
 const base::Feature kEarlyHintsPreloadForNavigation{
-    "EarlyHintsPreloadForNavigation", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EarlyHintsPreloadForNavigation", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Requires documents embedded via <iframe>, etc, to explicitly opt-into the
 // embedding: https://github.com/mikewest/embedding-requires-opt-in.
