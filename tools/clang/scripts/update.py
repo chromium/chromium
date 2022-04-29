f2a968f6513167caf5335dfbe6c29123675168af
Arthur Eubanks <aeubanks@google.com>
2022-04-29 22:04:56 +0000

    Revert "[clang] Build clang with pinned clang on all platforms"
    This reverts commit e4598f8d5c0b63528f3b1450aaeafcefe2ddd8ad.
    
    Reason for revert: breaks ToT win bots, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/ToTWin/1391/overview
    
    Original change's description:
    > [clang] Build clang with pinned clang on all platforms
    >
    > Followup to https://crrev.com/c/3572241.
    >
    > We can't use NamedTemporaryFile on Windows (can't read from it while having a handle to it): https://docs.python.org/3.9/library/tempfile.html. Use a consistent path for the pinned update.py instead.
    >
    > Bug: 1010466, 1013560
    > Change-Id: Ibaa0e7d2e9789ac24a852f3c7083a59cb242e793
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3590911
    > Reviewed-by: Hans Wennborg <hans@chromium.org>
    > Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    > Cr-Commit-Position: refs/heads/main@{#997580}
    
    Bug: 1010466, 1013560
    Change-Id: Ia71a175de39d73aaa217fdf4e53df5d17b1cef84
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3616878
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#997944}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -36,7 +36,7 @@ import zlib
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
 CLANG_REVISION = 'llvmorg-15-init-8945-g3d7da810'
-CLANG_SUB_REVISION = 2
+CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
 RELEASE_VERSION = '15.0.0'
