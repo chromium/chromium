348263486d653ea45e498d228b2b42e362e7d78c
Sophey <sophey@chromium.org>
2022-04-29 23:07:07 +0000

    [Sheriff] Actually disable UpdateDailyRefreshWallpaperCalledOnLogin test
    Bug: 1320991
    Change-Id: Ife9cbdfa07b89aeb8956d9fbcfe4718a002d80a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617150
    Auto-Submit: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Owners-Override: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997972}

--- ash/wallpaper/wallpaper_controller_unittest.cc
+++ ash/wallpaper/wallpaper_controller_unittest.cc
@@ -3790,9 +3790,9 @@ TEST_F(WallpaperControllerWallpaperWebUiTest, UpdateDailyRefreshWallpaper) {
   EXPECT_EQ(expected, client_.get_fetch_daily_refresh_wallpaper_param());
 }
 
-// Disabled due to flakes; see https://crrev.com/1320991.
+// Disabled due to flakes; see crbug/1320991.
 TEST_F(WallpaperControllerWallpaperWebUiTest,
-       DSAIBLED_UpdateDailyRefreshWallpaperCalledOnLogin) {
+       DISABLED_UpdateDailyRefreshWallpaperCalledOnLogin) {
   SimulateUserLogin(account_id_1);
 
   WallpaperInfo info = WallpaperInfo(OnlineWallpaperParams(
