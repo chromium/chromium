27d5810995455fc1356344f2aed02a171315ee52
Sophie Chang <sophiechang@chromium.org>
2022-04-29 22:10:11 +0000

    Add a histogram to the magnitude of the closest visit if a specific visit we attempted to store content annotations for could not be found
    Bug: 1321222
    Change-Id: Idfb9bb5dfc872f03c489e449a116919fed160a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617203
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#997947}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -417,10 +417,17 @@ void PageContentAnnotationsService::OnURLQueried(
     return;
   }
 
+  base::TimeDelta min_magnitude_between_visits = base::TimeDelta::Max();
   bool did_store_content_annotations = false;
   for (const auto& visit_for_url : url_result.visits) {
-    if (visit.nav_entry_timestamp != visit_for_url.visit_time)
+    if (visit.nav_entry_timestamp != visit_for_url.visit_time) {
+      base::TimeDelta magnitude_between_visits =
+          (visit.nav_entry_timestamp - visit_for_url.visit_time).magnitude();
+      if (magnitude_between_visits < min_magnitude_between_visits) {
+        min_magnitude_between_visits = magnitude_between_visits;
+      }
       continue;
+    }
 
     std::move(callback).Run(visit_for_url.visit_id);
 
@@ -429,6 +436,13 @@ void PageContentAnnotationsService::OnURLQueried(
   }
   LogPageContentAnnotationsStorageStatus(
       did_store_content_annotations ? kSuccess : kSpecificVisitForUrlNotFound);
+  if (!did_store_content_annotations) {
+    DCHECK_NE(min_magnitude_between_visits, base::TimeDelta::Max());
+    base::UmaHistogramTimes(
+        "OptimizationGuide.PageContentAnnotationsService."
+        "ContentAnnotationsStorageMinMagnitudeForVisitNotFound",
+        min_magnitude_between_visits);
+  }
 }
 
 void PageContentAnnotationsService::GetMetadataForEntityId(
