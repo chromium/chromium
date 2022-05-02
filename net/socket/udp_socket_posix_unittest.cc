0bbe25345d5d06eba5bbb8b0bf7b64f323c007f1
Gabriel Charette <gab@chromium.org>
2022-05-02 21:19:01 +0000

    Revert "Disable UDPSocketPosixTest.WriteAsyncBasicDelay on iOS"
    This reverts commit b4f012c6c3eddcb46d1ad503ab46ae7963ca6731.
    
    Reason for revert: crbug.com/934778#c2 mentions a surprising 
    task_environment.cc bug. Many things have changed in this space
    since. Let's try to re-enable.
    
    Same as https://chromium-review.googlesource.com/c/chromium/src/+/3621710
    but applied on "main" instead of "master" as original commit is old.
    
    Original change's description:
    > Disable UDPSocketPosixTest.WriteAsyncBasicDelay on iOS
    > because it is flaky
    >
    > TBR=mmenke@chromium.org
    >
    > Bug: 934778
    > Change-Id: I06f222489a3bcc61d6b5b6f2ce2b7f3559ad65aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/1483051
    > Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    > Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634626}
    
    Change-Id: Iec9f26f29130e20de9bc44f03fc5f39564f022d6
    Fixed: 934778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3621781
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998569}

--- net/socket/udp_socket_posix_unittest.cc
+++ net/socket/udp_socket_posix_unittest.cc
@@ -650,13 +650,7 @@ TEST_F(UDPSocketPosixTest, WriteAsyncNoBatchingError) {
   EXPECT_EQ(ERR_INVALID_HANDLE, rv);
 }
 
-#if BUILDFLAG(IS_IOS)
-#define MAYBE_WriteAsyncBasicDelay DISABLED_WriteAsyncBasicDelay
-#else
-#define MAYBE_WriteAsyncBasicDelay WriteAsyncBasicDelay
-#endif
-// TODO(crbug.com/934778) The test is flaky on iOS.
-TEST_F(UDPSocketPosixTest, MAYBE_WriteAsyncBasicDelay) {
+TEST_F(UDPSocketPosixTest, WriteAsyncBasicDelay) {
   socket_.SetWriteBatchingActive(true);
   socket_.SetWriteMultiCoreEnabled(true);
   DatagramBuffers buffers;
