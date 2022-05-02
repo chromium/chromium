cbba1d4d16d3dfeb51421749cafa34c2666f8b26
Arthur Eubanks <aeubanks@google.com>
2022-05-02 22:13:14 +0000

    [build] Remove global -Wno-deprecated-non-prototype
    Fix an instance in libevent.
    
    Sink -Wno-deprecated-non-prototype into zlib, it's the only remaining library where this warning fires. (upstream bug https://github.com/madler/zlib/issues/633)
    
    Bug: 1314867
    Change-Id: I2547ba1b358ab90ec6dece4f0879b2ebe6f59820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3615937
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#998593}

--- base/third_party/libevent/strlcpy.c
+++ base/third_party/libevent/strlcpy.c
@@ -46,10 +46,7 @@ static char *rcsid = "$OpenBSD: strlcpy.c,v 1.5 2001/05/13 15:40:16 deraadt Exp 
  * Returns strlen(src); if retval >= siz, truncation occurred.
  */
 size_t
-_event_strlcpy(dst, src, siz)
-	char *dst;
-	const char *src;
-	size_t siz;
+_event_strlcpy(char *dst, const char *src, size_t siz)
 {
 	register char *d = dst;
 	register const char *s = src;
