420973215f8f98f0a2bec0996758da39f3aa0a20
Benoit Lize <lizeb@chromium.org>
2022-05-02 08:48:00 +0000

    [tools/memory] Make pa_heap_dump record heap compressibility on macOS
    Bug: 1217582
    Change-Id: I9c905ce518050ffe6e87ed9fd0ac72d81724c7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613651
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998283}

--- tools/memory/partition_allocator/pa_dump_heap.cc
+++ tools/memory/partition_allocator/pa_dump_heap.cc
@@ -22,6 +22,7 @@
 #include "base/files/file.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
+#include "base/memory/page_size.h"
 #include "base/strings/stringprintf.h"
 #include "base/thread_annotations.h"
 #include "base/values.h"
@@ -227,44 +228,51 @@ class HeapDumper {
 
       auto page_sizes = base::Value(base::Value::Type::LIST);
       // Looking at how well the heap would compress.
-      constexpr size_t kPageSize = 1 << 12;
+      const size_t page_size = base::GetPageSize();
       for (uintptr_t page_address = address;
            page_address < address + partition_alloc::internal::kSuperPageSize;
-           page_address += kPageSize) {
+           page_address += page_size) {
         auto maybe_pagemap_entry = EntryAtAddress(pagemap_fd_, page_address);
         size_t uncompressed_size = 0, compressed_size = 0;
 
         bool all_zeros = true;
-        for (size_t i = 0; i < kPageSize; i++) {
+        for (size_t i = 0; i < page_size; i++) {
           if (reinterpret_cast<unsigned char*>(page_address)[i]) {
             all_zeros = false;
             break;
           }
         }
 
-        if (maybe_pagemap_entry && !all_zeros) {
+        bool should_report;
+        if (!maybe_pagemap_entry) {
+          // We cannot tell whether a page has been decommitted, but all-zero
+          // likely indicates that. Only report data for pages that the other
+          // pages.
+          should_report = !all_zeros;
+        } else {
           // If it's not in memory and not in swap, only the PTE exists.
-          bool populated =
+          should_report =
               maybe_pagemap_entry->present || maybe_pagemap_entry->swapped;
-          if (populated) {
-            std::string compressed;
-            uncompressed_size = kPageSize;
-            // Use snappy to approximate what a fast compression algorithm
-            // operating with a page granularity would do. This is not the
-            // algorithm used in either Linux or macOS, but should give some
-            // indication.
-            compressed_size =
-                snappy::Compress(reinterpret_cast<const char*>(page_address),
-                                 kPageSize, &compressed);
-          }
         }
 
-        auto page_size = base::Value(base::Value::Type::DICTIONARY);
-        page_size.SetKey("uncompressed",
-                         base::Value{static_cast<int>(uncompressed_size)});
-        page_size.SetKey("compressed",
-                         base::Value{static_cast<int>(compressed_size)});
-        page_sizes.Append(std::move(page_size));
+        if (should_report) {
+          std::string compressed;
+          uncompressed_size = page_size;
+          // Use snappy to approximate what a fast compression algorithm
+          // operating with a page granularity would do. This is not the
+          // algorithm used in either Linux or macOS, but should give some
+          // indication.
+          compressed_size =
+              snappy::Compress(reinterpret_cast<const char*>(page_address),
+                               page_size, &compressed);
+        }
+
+        auto page_size_dict = base::Value(base::Value::Type::DICTIONARY);
+        page_size_dict.SetKey("uncompressed",
+                              base::Value{static_cast<int>(uncompressed_size)});
+        page_size_dict.SetKey("compressed",
+                              base::Value{static_cast<int>(compressed_size)});
+        page_sizes.Append(std::move(page_size_dict));
       }
       ret.SetKey("page_sizes", std::move(page_sizes));
 
