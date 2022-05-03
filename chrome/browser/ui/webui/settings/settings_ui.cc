aec91c29e7f75a078d614a3e0d59cefc5b68c173
Rainhard Findling <rainhard@chromium.org>
2022-05-03 08:59:51 +0000

    Privacy Guide: only show when PS is available in Settings
    Gate the |privacyGuideEnabled| boolean on the privacy sandbox being
    available in Settings. This is sufficient as all UIs that would include
    PG UIs check this boolean, and as the booloan is set once and doesn't
    change afterwards.
    
    Fixed: 1320222
    Bug: 1215630
    Change-Id: Ia98aedce4baa4c5a6d18fa7578abaa5bbe32c5e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613656
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998771}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -312,16 +312,17 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
   html_source->AddBoolean("userCannotManuallyEnterPassword", false);
 #endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
-  html_source->AddBoolean(
-      "privacyGuideEnabled",
-      !chrome::ShouldDisplayManagedUi(profile) &&
-          base::FeatureList::IsEnabled(features::kPrivacyGuide));
+  bool privacy_guide_enabled =
+      !chrome::ShouldDisplayManagedUi(profile) && !profile->IsChild() &&
+      !PrivacySandboxServiceFactory::GetForProfile(profile)
+           ->IsPrivacySandboxRestricted() &&
+      base::FeatureList::IsEnabled(features::kPrivacyGuide);
+  html_source->AddBoolean("privacyGuideEnabled", privacy_guide_enabled);
 
   html_source->AddBoolean(
       "privacyGuide2Enabled",
-      !chrome::ShouldDisplayManagedUi(profile) &&
-          // #privacy-guide-2 only has effect if #privacy-guide is enabled too.
-          base::FeatureList::IsEnabled(features::kPrivacyGuide) &&
+      // #privacy-guide-2 only has effect if #privacy-guide is enabled too.
+      privacy_guide_enabled &&
           base::FeatureList::IsEnabled(features::kPrivacyGuide2));
 
   AddSettingsPageUIHandler(std::make_unique<AboutHandler>(profile));
