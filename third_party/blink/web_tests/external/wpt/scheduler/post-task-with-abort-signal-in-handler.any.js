0a00b72ea5aa1317e76e953a77ae1fcd4201b16c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-05-03 20:26:09 +0000

    Import wpt@146851fc385ee6e4c1a7c2434c41a9c170489438
    Using wpt-import in Chromium 1b70b91967908ece4bad948096c7782ebcff9d2b.
    With Chromium commits locally applied on WPT:
    5e10fc62d2 "[layout] Switch radio/checkbox to have an intrinsic inline/block size."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I5857f1b96b12ac2aca4f6e58a9f829d1f723ba9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3624126
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#999054}

--- third_party/blink/web_tests/external/wpt/scheduler/post-task-with-abort-signal-in-handler.any.js
+++ third_party/blink/web_tests/external/wpt/scheduler/post-task-with-abort-signal-in-handler.any.js
@@ -13,8 +13,8 @@ promise_test(t => {
 promise_test(t => {
   const controller = new TaskController();
   const signal = controller.signal;
-  return promise_rejects_dom(t, 'AbortError', scheduler.postTask(async () => {
+  return scheduler.postTask(async () => {
     await new Promise(resolve => t.step_timeout(resolve, 0));
     controller.abort();
-  }, { signal }));
+  }, { signal });
 }, 'Posting a task with a signal and abort the signal when running the async callback');
