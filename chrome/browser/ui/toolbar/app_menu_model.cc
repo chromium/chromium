chromium/chromium

commit 1b8061d5236fb8896f3c05286d8b12e41cd3e974

Consolidate behavior of menu items for IncognitoModeAvailability policy
When the policy IncognitoModeAvailability is set to 1, the overflow menu
item "New incognito window" should be disabled instead of hidden. This
is the current behavior for the context menu item "Open link in
incognito window".

Bug: 1100791
Change-Id: I21f622482cfc4671e782410e5c71a51543b395e2
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605771
Reviewed-by: Dana Fried <dfried@chromium.org>
Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
Cr-Commit-Position: refs/heads/main@{#996581}

chrome/browser/ui/toolbar/app_menu_model.cc
@@ -757,7 +757,12 @@ bool AppMenuModel::IsCommandIdEnabled(int command_id) const {
   if (error)
     return true;
 
-  return chrome::IsCommandEnabled(browser_, command_id);
+  switch (command_id) {
+    case IDC_NEW_INCOGNITO_WINDOW:
+      return IncognitoModePrefs::IsIncognitoAllowed(browser_->profile());
+    default:
+      return chrome::IsCommandEnabled(browser_, command_id);
+  }
 }
 
 bool AppMenuModel::IsCommandIdVisible(int command_id) const {
@@ -845,8 +850,9 @@ void AppMenuModel::Build() {
                                        ? IDS_NEW_INCOGNITO_TAB
                                        : IDS_NEW_TAB);
   AddItemWithStringId(IDC_NEW_WINDOW, IDS_NEW_WINDOW);
-  if (ShouldShowNewIncognitoWindowMenuItem())
-    AddItemWithStringId(IDC_NEW_INCOGNITO_WINDOW, IDS_NEW_INCOGNITO_WINDOW);
+
+  AddItemWithStringId(IDC_NEW_INCOGNITO_WINDOW, IDS_NEW_INCOGNITO_WINDOW);
+
   AddSeparator(ui::NORMAL_SEPARATOR);
 
   if (!browser_->profile()->IsOffTheRecord()) {
@@ -1003,10 +1009,6 @@ void AppMenuModel::UpdateZoomControls() {
         zoom::ZoomController::FromWebContents(web_contents())->GetZoomPercent();
   }
   zoom_label_ = base::FormatPercent(zoom_percent);
-}
-
-bool AppMenuModel::ShouldShowNewIncognitoWindowMenuItem() {
-  return IncognitoModePrefs::IsIncognitoAllowed(browser_->profile());
 }
 
 bool AppMenuModel::AddGlobalErrorMenuItems() {

chrome/browser/ui/toolbar/app_menu_model.h
@@ -202,8 +202,6 @@ class AppMenuModel : public ui::SimpleMenuModel,
  private:
   friend class ::MockAppMenuModel;
 
-  bool ShouldShowNewIncognitoWindowMenuItem();
-
   // Adds actionable global error menu items to the menu.
   // Examples: Extension permissions and sign in errors.
   // Returns a boolean indicating whether any menu items were added.

commit 2703aae3f4813cd9a65e96e90f1afdbfa8943178

Hide New Incognito Window App Menu item for Guest Mode only
This fixes a regression introduced by 1b8061d.
This App menu item will be shown as disabled if Incognito mode is
disabled by a policy. If the session is in Guest Mode, the menu item
will be hidden.

Bug: 1321554
Change-Id: Id7fc67b8562bcd76cd47c8c7e6fcba06c68b024a
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3620378
Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
Reviewed-by: Mike Wasserman <msw@chromium.org>
Cr-Commit-Position: refs/heads/main@{#998786}

chrome/browser/ui/toolbar/app_menu_model.cc
@@ -851,7 +851,10 @@ void AppMenuModel::Build() {
                                        : IDS_NEW_TAB);
   AddItemWithStringId(IDC_NEW_WINDOW, IDS_NEW_WINDOW);
 
-  AddItemWithStringId(IDC_NEW_INCOGNITO_WINDOW, IDS_NEW_INCOGNITO_WINDOW);
+  // This menu item is not visible in Guest Mode. If incognito mode is not
+  // available, it will be shown in disabled state. (crbug.com/1100791)
+  if (!browser_->profile()->IsGuestSession())
+    AddItemWithStringId(IDC_NEW_INCOGNITO_WINDOW, IDS_NEW_INCOGNITO_WINDOW);
 
   AddSeparator(ui::NORMAL_SEPARATOR);
 