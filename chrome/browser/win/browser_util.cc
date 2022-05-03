4a782f468874e3d36ddc704f83f784d9d8283540
Will Harris <wfh@chromium.org>
2022-05-03 08:07:00 +0000

    Change IsBrowserAlreadyRunning to be more resilient.
    This changes IsBrowserAlreadyRunning to set the event name
    based on the NT path to the executable chrome is running from
    rather than using the Win32 path.
    
    This means that if browser was running under a previously
    hardlinked directory these are now treated the same.
    
    e.g. C:\Documents and Settings\user\appdata\local\google\Chrome SxS\Application\chrome.exe
    is now treated the same as C:\Users\user\appdata\local\google\Chrome SxS\Application\chrome.exe
    as they are the same file, but different Win32 paths.
    
    Previously, these two paths would not have been considered the same
    path. This could cause issues during in-use upgrades.
    
    TEST=Run chrome, verify the correct Event is created in WinObj.
    BUG=1314491
    
    Change-Id: I5c5fb23754af47db4b7b78ca6e240e4d3eb47361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3617153
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#998756}

--- chrome/browser/win/browser_util.cc
+++ chrome/browser/win/browser_util.cc
@@ -9,24 +9,63 @@
 #include <algorithm>
 #include <string>
 
-#include "base/base_paths.h"
-#include "base/files/file_path.h"
-#include "base/path_service.h"
+#include "base/logging.h"
 
 namespace browser_util {
 
+namespace {
+
+// Determine the NT path name for the current process. Returns an empty path if
+// a failure occurs.
+std::wstring GetCurrentProcessExecutablePath() {
+  std::wstring image_path;
+  image_path.resize(MAX_PATH);
+  DWORD path_length = image_path.size();
+  BOOL success =
+      ::QueryFullProcessImageNameW(::GetCurrentProcess(), PROCESS_NAME_NATIVE,
+                                   image_path.data(), &path_length);
+  if (!success && ::GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
+    // Process name is potentially greater than MAX_PATH, try larger max size.
+    // https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation
+    image_path.resize(UNICODE_STRING_MAX_CHARS);
+    path_length = image_path.size();
+    success =
+        ::QueryFullProcessImageNameW(::GetCurrentProcess(), PROCESS_NAME_NATIVE,
+                                     image_path.data(), &path_length);
+  }
+  if (!success) {
+    PLOG_IF(ERROR, ::GetLastError() != ERROR_GEN_FAILURE)
+        << "Failed to get process image path";
+    return std::wstring();
+  }
+  image_path.resize(path_length);
+  return image_path;
+}
+
+}  // namespace
+
 bool IsBrowserAlreadyRunning() {
   static HANDLE handle = NULL;
-  base::FilePath exe_path;
-  base::PathService::Get(base::FILE_EXE, &exe_path);
-  std::wstring exe = exe_path.value();
-  std::replace(exe.begin(), exe.end(), '\\', '!');
-  std::transform(exe.begin(), exe.end(), exe.begin(), tolower);
-  exe = L"Global\\" + exe;
+
+  std::wstring nt_path_name = GetCurrentProcessExecutablePath();
+  if (nt_path_name.empty()) {
+    // If this fails, there isn't much that can be done. However, assuming that
+    // browser is *not* already running is the safer action here, as it means
+    // that any pending upgrade actions will occur and hopefully the issue that
+    // caused this failure will be resolved by the newer version. This might
+    // cause the currently running browser to be temporarily broken, but it's
+    // probably broken already if QueryFullProcessImageNameW is failing.
+    return false;
+  }
+
+  std::replace(nt_path_name.begin(), nt_path_name.end(), '\\', '!');
+  std::transform(nt_path_name.begin(), nt_path_name.end(), nt_path_name.begin(),
+                 tolower);
+  nt_path_name = L"Global\\" + nt_path_name;
   if (handle != NULL)
-    CloseHandle(handle);
-  handle = CreateEvent(NULL, TRUE, TRUE, exe.c_str());
-  int error = GetLastError();
+    ::CloseHandle(handle);
+  handle = ::CreateEventW(NULL, TRUE, TRUE, nt_path_name.c_str());
+  int error = ::GetLastError();
   return (error == ERROR_ALREADY_EXISTS || error == ERROR_ACCESS_DENIED);
 }
 
