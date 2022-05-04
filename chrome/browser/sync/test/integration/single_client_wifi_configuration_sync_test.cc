4f5f08cf799dc2a69f508b26b0b43ad31371f8ec
Maksim Moskvitin <mmoskvitin@google.com>
2022-05-04 12:06:41 +0000

    [Sync] Add simple integration test for wifi_sync
    This CL adds ShouldDownloadSingleWifiConfiguration, which verifies
    that wifi network is added by sync during the initial sync. More
    importantly, this is the first sync_integration_test for wifi_sync and
    some helpers + fixture introduced by this CL as well.
    
    Bug: 1077152, b/207087646
    Change-Id: I7d967b4e6269b5fc6e2d2da3c104aa3a8b854c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3610407
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999341}

---
+++ chrome/browser/sync/test/integration/single_client_wifi_configuration_sync_test.cc
@@ -0,0 +1,185 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "ash/public/cpp/network_config_service.h"
+#include "chrome/browser/sync/test/integration/status_change_checker.h"
+#include "chrome/browser/sync/test/integration/sync_test.h"
+#include "chromeos/components/sync_wifi/network_identifier.h"
+#include "chromeos/components/sync_wifi/test_data_generator.h"
+#include "chromeos/dbus/shill/shill_profile_client.h"
+#include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
+#include "components/sync/engine/loopback_server/persistent_unique_client_entity.h"
+#include "components/sync/nigori/cryptographer_impl.h"
+#include "components/sync/nigori/nigori_test_utils.h"
+#include "components/sync/protocol/wifi_configuration_specifics.pb.h"
+#include "components/sync/test/fake_server/fake_server.h"
+#include "content/public/test/browser_test.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace {
+
+std::string GetClientTag(const sync_pb::WifiConfigurationSpecifics& specifics) {
+  return chromeos::sync_wifi::NetworkIdentifier::FromProto(specifics)
+      .SerializeToString();
+}
+
+void InjectKeystoreEncryptedServerWifiConfiguration(
+    fake_server::FakeServer* fake_server,
+    const sync_pb::WifiConfigurationSpecifics& unencrypted_specifics) {
+  sync_pb::EntitySpecifics wrapped_unencrypted_specifics;
+  *wrapped_unencrypted_specifics.mutable_wifi_configuration() =
+      unencrypted_specifics;
+
+  sync_pb::EntitySpecifics encrypted_specifics;
+  const syncer::KeyParamsForTesting keystore_key_params =
+      syncer::KeystoreKeyParamsForTesting(
+          fake_server->GetKeystoreKeys().back());
+  auto cryptographer = syncer::CryptographerImpl::FromSingleKeyForTesting(
+      keystore_key_params.password, keystore_key_params.derivation_params);
+  bool encrypt_result = cryptographer->Encrypt(
+      wrapped_unencrypted_specifics, encrypted_specifics.mutable_encrypted());
+  *encrypted_specifics.mutable_wifi_configuration() =
+      sync_pb::WifiConfigurationSpecifics();
+  DCHECK(encrypt_result);
+
+  fake_server->InjectEntity(
+      syncer::PersistentUniqueClientEntity::CreateFromSpecificsForTesting(
+          /*non_unique_name=*/"encrypted", GetClientTag(unencrypted_specifics),
+          encrypted_specifics,
+          /*creation_time=*/0, /*last_modified_time=*/0));
+}
+
+// Observes Shill networks state via IPC. Used to wait until wifi configuration
+// becomes locally available.
+class LocalWifiConfigurationChecker
+    : public StatusChangeChecker,
+      public chromeos::network_config::mojom::CrosNetworkConfigObserver {
+ public:
+  LocalWifiConfigurationChecker(
+      mojo::Remote<chromeos::network_config::mojom::CrosNetworkConfig>*
+          remote_cros_network_config,
+      const std::string& expected_ssid)
+      : expected_ssid_(expected_ssid),
+        remote_cros_network_config_(remote_cros_network_config) {
+    DCHECK(remote_cros_network_config_);
+    (*remote_cros_network_config_)
+        ->AddObserver(receiver_.BindNewPipeAndPassRemote());
+    OnNetworkStateListChanged();
+  }
+
+  LocalWifiConfigurationChecker(const LocalWifiConfigurationChecker& other) =
+      delete;
+  LocalWifiConfigurationChecker& operator=(
+      const LocalWifiConfigurationChecker& other) = delete;
+  ~LocalWifiConfigurationChecker() override = default;
+
+  // CrosNetworkConfigObserver implementation.
+  void OnNetworkStateListChanged() override {
+    (*remote_cros_network_config_)
+        ->GetNetworkStateList(
+            chromeos::network_config::mojom::NetworkFilter::New(
+                chromeos::network_config::mojom::FilterType::kConfigured,
+                chromeos::network_config::mojom::NetworkType::kWiFi,
+                /*limit=*/0),
+            base::BindOnce(&LocalWifiConfigurationChecker::OnGetNetworkList,
+                           weak_ptr_factory_.GetWeakPtr()));
+  }
+
+  void OnActiveNetworksChanged(
+      std::vector<chromeos::network_config::mojom::NetworkStatePropertiesPtr>
+          networks) override {}
+  void OnNetworkStateChanged(
+      chromeos::network_config::mojom::NetworkStatePropertiesPtr network)
+      override {}
+  void OnDeviceStateListChanged() override {}
+  void OnVpnProvidersChanged() override {}
+  void OnNetworkCertificatesChanged() override {}
+  void OnPoliciesApplied(const std::string& userhash) override {}
+
+  // StatusChangeChecker implementation.
+  bool IsExitConditionSatisfied(std::ostream* os) override {
+    *os << "Waiting for network with ssid: " << expected_ssid_;
+    for (const auto& network : networks_) {
+      if (network->type_state->get_wifi()->ssid == expected_ssid_) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void OnGetNetworkList(
+      std::vector<chromeos::network_config::mojom::NetworkStatePropertiesPtr>
+          networks) {
+    networks_ = std::move(networks);
+    CheckExitCondition();
+  }
+
+ private:
+  const std::string expected_ssid_;
+  std::vector<chromeos::network_config::mojom::NetworkStatePropertiesPtr>
+      networks_;
+
+  mojo::Remote<chromeos::network_config::mojom::CrosNetworkConfig>*
+      remote_cros_network_config_;
+  mojo::Receiver<chromeos::network_config::mojom::CrosNetworkConfigObserver>
+      receiver_{this};
+  base::WeakPtrFactory<LocalWifiConfigurationChecker> weak_ptr_factory_{this};
+};
+
+// TODO(crbug.com/1077152): add more tests.
+class SingleClientWifiConfigurationSyncTest : public SyncTest {
+ public:
+  SingleClientWifiConfigurationSyncTest() : SyncTest(SINGLE_CLIENT) {}
+  SingleClientWifiConfigurationSyncTest(
+      const SingleClientWifiConfigurationSyncTest&) = delete;
+  SingleClientWifiConfigurationSyncTest& operator=(
+      const SingleClientWifiConfigurationSyncTest&) = delete;
+  ~SingleClientWifiConfigurationSyncTest() override = default;
+
+  void SetUpOnMainThread() override {
+    ash::GetNetworkConfigService(
+        remote_cros_network_config_.BindNewPipeAndPassReceiver());
+
+    SyncTest::SetUpOnMainThread();
+  }
+
+  void SetupShill() {
+    // TODO(crbug.com/1077152): figure out where |userhash| is hardcoded and use
+    // some shared constant here.
+    chromeos::ShillProfileClient::Get()->GetTestInterface()->AddProfile(
+        GetProfile(0)->GetPath().value(), /*userhash=*/"test-user");
+  }
+
+  mojo::Remote<chromeos::network_config::mojom::CrosNetworkConfig>*
+  remote_cros_network_config() {
+    return &remote_cros_network_config_;
+  }
+
+ private:
+  mojo::Remote<chromeos::network_config::mojom::CrosNetworkConfig>
+      remote_cros_network_config_;
+};
+
+IN_PROC_BROWSER_TEST_F(SingleClientWifiConfigurationSyncTest,
+                       ShouldDownloadSingleWifiConfiguration) {
+  const std::string kTestSsid = "test_wifi";
+  InjectKeystoreEncryptedServerWifiConfiguration(
+      GetFakeServer(),
+      /*unencrypted_specifics=*/chromeos::sync_wifi::GenerateTestWifiSpecifics(
+          chromeos::sync_wifi::GeneratePskNetworkId(kTestSsid)));
+
+  ASSERT_TRUE(SetupSync());
+  SetupShill();
+
+  EXPECT_TRUE(
+      LocalWifiConfigurationChecker(remote_cros_network_config(), kTestSsid)
+          .Wait());
+}
+
+}  // namespace
