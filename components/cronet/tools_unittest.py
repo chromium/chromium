4876b8b4e5c471446a1963fd9ca997d17333e087
Bruce Dawson <brucedawson@chromium.org>
2022-05-04 16:01:46 +0000

    Disable cronet unittests on Windows
    When running "git cl presubmit --all" (or git cl presubmit --files=*.idl
    for faster results) on Windows the cronet PRESUBMIT calls
    tools_unittest.py which calls api_static_checks_unittest.py which tries
    to invoke java, jar, etc. These typically aren't installed on Windows so
    this leads to about 20 KB of cryptic failure messages, which hides more
    pressing issues.
    
    This disables the cronet unittests on Windows.
    
    Bug: 1309977
    Change-Id: If4c8f6fe38bb74869532e28c3154bb8f5056881a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3589014
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#999434}

--- components/cronet/tools_unittest.py
+++ components/cronet/tools_unittest.py
@@ -9,5 +9,9 @@ import sys
 import unittest
 
 if __name__ == '__main__':
+  # jar/java/javac aren't typically installed on Windows so these tests don't
+  # work and give many verbose and cryptic failure messages.
+  if sys.platform == 'win32':
+    sys.exit(0)
   suite = unittest.TestLoader().discover('tools', pattern = "*_unittest.py")
   sys.exit(0 if unittest.TextTestRunner().run(suite).wasSuccessful() else 1)
