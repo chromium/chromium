df585fb0bed966ec319dcef9f0340e73496b205b
Mohamed Amir Yosef <mamir@chromium.org>
2022-05-04 11:41:01 +0000

    [Passwords] Update password generation offer title for GPM
    Screenshot: http://screen/9FGN6FEuaMketyT
    
    Bug: 1309480
    Change-Id: Iede5ec81c7dd0b8db7dd4ed29e89bcb12bfc7224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627551
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999334}

--- chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
+++ chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
@@ -31,6 +31,7 @@
 #include "components/password_manager/core/browser/password_manager.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_manager_driver.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/navigation_handle.h"
@@ -387,9 +388,15 @@ const std::u16string& PasswordGenerationPopupControllerImpl::password() const {
 }
 
 std::u16string PasswordGenerationPopupControllerImpl::SuggestedText() {
-  return l10n_util::GetStringUTF16(
-      state_ == kOfferGeneration ? IDS_PASSWORD_GENERATION_SUGGESTION
-                                 : IDS_PASSWORD_GENERATION_EDITING_SUGGESTION);
+  if (state_ == kOfferGeneration) {
+    return l10n_util::GetStringUTF16(
+        base::FeatureList::IsEnabled(
+            password_manager::features::kUnifiedPasswordManagerDesktop)
+            ? IDS_PASSWORD_GENERATION_SUGGESTION_GPM
+            : IDS_PASSWORD_GENERATION_SUGGESTION);
+  }
+
+  return l10n_util::GetStringUTF16(IDS_PASSWORD_GENERATION_EDITING_SUGGESTION);
 }
 
 const std::u16string& PasswordGenerationPopupControllerImpl::HelpText() {
