d282c7a3531fa18f724443f6aef7bdf9571cff62
Jayson Adams <shrike@google.com>
2022-05-04 18:56:03 +0000

    [macOS TTS] Cache TTS voice info for better performance
    During my power investigations I discovered that
    TtsPlatformImplMac::GetVoices() can take nearly 2ms to complete, which
    is not a long time but it is a very large number of CPU cycles. The
    problem comes from rebuilding the voice info datastructure, which
    involves expensive macOS framework calls (in particular, lookup of
    locale info). From looking at traces it appears GetVoices() can be
    called repeatedly. This cl caches the voice data for better performance
    (wall time 7.7ms -> 0.4ms; CPU duration 1.7ms -> 0.3ms). We flush the
    cache when the user leaves and returns to Chrome, on the chance they
    made locale or voice changes while they were gone.
    
    Bug: 1318932
    
    Test: out/Default/content_unittests --gtest_filter=TtsMacTest*
    
        out/Default/content_unittests --gtest_filter=TtsControllerTest*
    
    Change-Id: I30a605506bda8abaea9b380cb3476221d3b6ba26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3599523
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#999520}

--- content/browser/speech/tts_mac.mm
+++ content/browser/speech/tts_mac.mm
@@ -2,111 +2,104 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "content/browser/speech/tts_mac.h"
+
 #include <string>
 
 #include "base/bind.h"
-#include "base/callback.h"
-#include "base/mac/scoped_nsobject.h"
-#include "base/no_destructor.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
-#include "content/browser/speech/tts_platform_impl.h"
 #include "content/public/browser/tts_controller.h"
 
-#import <Cocoa/Cocoa.h>
+namespace {
 
-class TtsPlatformImplMac;
+std::vector<content::VoiceData>& VoicesRef() {
+  static base::NoDestructor<std::vector<content::VoiceData>> voices([]() {
+    [[NSNotificationCenter defaultCenter]
+        addObserverForName:NSApplicationWillBecomeActiveNotification
+                    object:nil
+                     queue:nil
+                usingBlock:^(NSNotification* notification) {
+                  // The user might have switched to Settings or some other app
+                  // to change voices or locale settings. Avoid a stale cache by
+                  // forcing a rebuild of the voices vector after the app
+                  // becomes active.
+                  VoicesRef().clear();
+                }];
+    return std::vector<content::VoiceData>();
+  }());
 
-@interface ChromeTtsDelegate : NSObject <NSSpeechSynthesizerDelegate> {
- @private
-  TtsPlatformImplMac* _ttsImplMac;  // weak.
+  return *voices;
 }
 
-- (id)initWithPlatformImplMac:(TtsPlatformImplMac*)ttsImplMac;
+std::vector<content::VoiceData>& Voices() {
+  std::vector<content::VoiceData>& voices = VoicesRef();
 
-@end
+  if (!voices.empty())
+    return voices;
 
-// Subclass of NSSpeechSynthesizer that takes an utterance
-// string on initialization, retains it and only allows it
-// to be spoken once.
-//
-// We construct a new NSSpeechSynthesizer for each utterance, for
-// two reasons:
-// 1. To associate delegate callbacks with a particular utterance,
-//    without assuming anything undocumented about the protocol.
-// 2. To work around http://openradar.appspot.com/radar?id=2854403,
-//    where Nuance voices don't retain the utterance string and
-//    crash when trying to call willSpeakWord.
-@interface SingleUseSpeechSynthesizer : NSSpeechSynthesizer {
- @private
-  base::scoped_nsobject<NSString> _utterance;
-  bool _didSpeak;
+  base::scoped_nsobject<NSMutableArray> voiceIdentifiers(
+      [NSSpeechSynthesizer.availableVoices mutableCopy]);
+
+  NSString* defaultVoice = NSSpeechSynthesizer.defaultVoice;
+  if (defaultVoice) {
+    [voiceIdentifiers removeObject:defaultVoice];
+    [voiceIdentifiers insertObject:defaultVoice atIndex:0];
+  }
+
+  voices.reserve([voiceIdentifiers count]);
+
+  for (NSString* voiceIdentifier in voiceIdentifiers.get()) {
+    voices.push_back(content::VoiceData());
+    content::VoiceData& data = voices.back();
+
+    NSDictionary* attributes =
+        [NSSpeechSynthesizer attributesForVoice:voiceIdentifier];
+    NSString* name = attributes[NSVoiceName];
+    NSString* localeIdentifier = attributes[NSVoiceLocaleIdentifier];
+
+    data.native = true;
+    data.native_voice_identifier = base::SysNSStringToUTF8(voiceIdentifier);
+    data.name = base::SysNSStringToUTF8(name);
+
+    NSDictionary* localeComponents =
+        [NSLocale componentsFromLocaleIdentifier:localeIdentifier];
+    NSString* language = localeComponents[NSLocaleLanguageCode];
+    NSString* country = localeComponents[NSLocaleCountryCode];
+    if (language && country) {
+      data.lang = base::SysNSStringToUTF8(
+          [NSString stringWithFormat:@"%@-%@", language, country]);
+    } else {
+      data.lang = base::SysNSStringToUTF8(language);
+    }
+    data.events.insert(content::TTS_EVENT_START);
+    data.events.insert(content::TTS_EVENT_END);
+    data.events.insert(content::TTS_EVENT_WORD);
+    data.events.insert(content::TTS_EVENT_ERROR);
+    data.events.insert(content::TTS_EVENT_CANCELLED);
+    data.events.insert(content::TTS_EVENT_INTERRUPTED);
+    data.events.insert(content::TTS_EVENT_PAUSE);
+    data.events.insert(content::TTS_EVENT_RESUME);
+  }
+
+  return voices;
 }
 
-- (id)initWithUtterance:(NSString*)utterance;
-- (bool)startSpeakingRetainedUtterance;
-- (bool)startSpeakingString:(NSString*)utterance;
-
-@end
-
-class TtsPlatformImplMac : public content::TtsPlatformImpl {
- public:
-  TtsPlatformImplMac(const TtsPlatformImplMac&) = delete;
-  TtsPlatformImplMac& operator=(const TtsPlatformImplMac&) = delete;
-
-  bool PlatformImplSupported() override { return true; }
-  bool PlatformImplInitialized() override { return true; }
-
-  void Speak(int utterance_id,
-             const std::string& utterance,
-             const std::string& lang,
-             const content::VoiceData& voice,
-             const content::UtteranceContinuousParameters& params,
-             base::OnceCallback<void(bool)> on_speak_finished) override;
-
-  bool StopSpeaking() override;
-
-  void Pause() override;
-
-  void Resume() override;
-
-  bool IsSpeaking() override;
-
-  void GetVoices(std::vector<content::VoiceData>* out_voices) override;
-
-  // Called by ChromeTtsDelegate when we get a callback from the
-  // native speech engine.
-  void OnSpeechEvent(NSSpeechSynthesizer* sender,
-                     content::TtsEventType event_type,
-                     int char_index,
-                     int char_length,
-                     const std::string& error_message);
-
-  // Get the single instance of this class.
-  static TtsPlatformImplMac* GetInstance();
-
- private:
-  friend base::NoDestructor<TtsPlatformImplMac>;
-  TtsPlatformImplMac();
-
-  void ProcessSpeech(int utterance_id,
-                     const std::string& lang,
-                     const content::VoiceData& voice,
-                     const content::UtteranceContinuousParameters& params,
-                     base::OnceCallback<void(bool)> on_speak_finished,
-                     const std::string& parsed_utterance);
-
-  base::scoped_nsobject<SingleUseSpeechSynthesizer> speech_synthesizer_;
-  base::scoped_nsobject<ChromeTtsDelegate> delegate_;
-  int utterance_id_ = -1;
-  std::string utterance_;
-  int last_char_index_ = 0;
-  bool paused_ = false;
-};
+}  // namespace
 
 // static
 content::TtsPlatformImpl* content::TtsPlatformImpl::GetInstance() {
   return TtsPlatformImplMac::GetInstance();
+}
+
+TtsPlatformImplMac::~TtsPlatformImplMac() = default;
+
+bool TtsPlatformImplMac::PlatformImplSupported() {
+  return true;
+}
+
+bool TtsPlatformImplMac::PlatformImplInitialized() {
+  return true;
 }
 
 void TtsPlatformImplMac::Speak(
@@ -228,54 +221,7 @@ bool TtsPlatformImplMac::IsSpeaking() {
 }
 
 void TtsPlatformImplMac::GetVoices(std::vector<content::VoiceData>* outVoices) {
-  NSArray* voices = [NSSpeechSynthesizer availableVoices];
-
-  // Create a new temporary array of the available voices with
-  // the default voice first.
-  NSMutableArray* orderedVoices =
-      [NSMutableArray arrayWithCapacity:[voices count]];
-  NSString* defaultVoice = [NSSpeechSynthesizer defaultVoice];
-  if (defaultVoice) {
-    [orderedVoices addObject:defaultVoice];
-  }
-  for (NSString* voiceIdentifier in voices) {
-    if (![voiceIdentifier isEqualToString:defaultVoice])
-      [orderedVoices addObject:voiceIdentifier];
-  }
-
-  for (NSString* voiceIdentifier in orderedVoices) {
-    outVoices->push_back(content::VoiceData());
-    content::VoiceData& data = outVoices->back();
-
-    NSDictionary* attributes =
-        [NSSpeechSynthesizer attributesForVoice:voiceIdentifier];
-    NSString* name = [attributes objectForKey:NSVoiceName];
-    NSString* localeIdentifier =
-        [attributes objectForKey:NSVoiceLocaleIdentifier];
-
-    data.native = true;
-    data.native_voice_identifier = base::SysNSStringToUTF8(voiceIdentifier);
-    data.name = base::SysNSStringToUTF8(name);
-
-    NSDictionary* localeComponents =
-        [NSLocale componentsFromLocaleIdentifier:localeIdentifier];
-    NSString* language = [localeComponents objectForKey:NSLocaleLanguageCode];
-    NSString* country = [localeComponents objectForKey:NSLocaleCountryCode];
-    if (language && country) {
-      data.lang = base::SysNSStringToUTF8(
-          [NSString stringWithFormat:@"%@-%@", language, country]);
-    } else {
-      data.lang = base::SysNSStringToUTF8(language);
-    }
-    data.events.insert(content::TTS_EVENT_START);
-    data.events.insert(content::TTS_EVENT_END);
-    data.events.insert(content::TTS_EVENT_WORD);
-    data.events.insert(content::TTS_EVENT_ERROR);
-    data.events.insert(content::TTS_EVENT_CANCELLED);
-    data.events.insert(content::TTS_EVENT_INTERRUPTED);
-    data.events.insert(content::TTS_EVENT_PAUSE);
-    data.events.insert(content::TTS_EVENT_RESUME);
-  }
+  *outVoices = Voices();
 }
 
 void TtsPlatformImplMac::OnSpeechEvent(NSSpeechSynthesizer* sender,
@@ -307,7 +253,15 @@ TtsPlatformImplMac* TtsPlatformImplMac::GetInstance() {
   return tts_platform.get();
 }
 
-@implementation ChromeTtsDelegate
+// static
+std::vector<content::VoiceData>& TtsPlatformImplMac::VoicesRefForTesting() {
+  return VoicesRef();
+}
+
+@implementation ChromeTtsDelegate {
+ @private
+  TtsPlatformImplMac* _ttsImplMac;  // weak.
+}
 
 - (id)initWithPlatformImplMac:(TtsPlatformImplMac*)ttsImplMac {
   if ((self = [super init])) {
@@ -349,7 +303,11 @@ TtsPlatformImplMac* TtsPlatformImplMac::GetInstance() {
 
 @end
 
-@implementation SingleUseSpeechSynthesizer
+@implementation SingleUseSpeechSynthesizer {
+ @private
+  base::scoped_nsobject<NSString> _utterance;
+  bool _didSpeak;
+}
 
 - (id)initWithUtterance:(NSString*)utterance {
   self = [super init];
