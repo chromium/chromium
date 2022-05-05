b741282884623ce6adcc047d30ae2595939557ba
Dave Tapuska <dtapuska@chromium.org>
2022-05-05 18:43:18 +0000

    Convert vlogs in LayoutShiftTracker to log fenced frames as subframes.
    These vlogs can be changed to IsOutermostMainFrame to report the
    relevant information.
    
    BUG=1314858
    
    Change-Id: I6a999c6213adb9e975c012a55d51d7640002fb05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630339
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000006}

--- third_party/blink/renderer/core/layout/layout_shift_tracker.cc
+++ third_party/blink/renderer/core/layout/layout_shift_tracker.cc
@@ -350,7 +350,7 @@ void LayoutShiftTracker::ObjectShifted(
 
   LocalFrame& frame = frame_view_->GetFrame();
   if (ShouldLog(frame)) {
-    VLOG(1) << "in " << (frame.IsMainFrame() ? "" : "subframe ")
+    VLOG(1) << "in " << (frame.IsOutermostMainFrame() ? "" : "subframe ")
             << frame.GetDocument()->Url() << ", " << object << " moved from "
             << old_rect_in_root.ToString() << " to "
             << new_rect_in_root.ToString() << " (visible from "
@@ -532,7 +532,7 @@ void LayoutShiftTracker::NotifyPrePaintFinishedInternal() {
 
   LocalFrame& frame = frame_view_->GetFrame();
   if (ShouldLog(frame)) {
-    VLOG(1) << "in " << (frame.IsMainFrame() ? "" : "subframe ")
+    VLOG(1) << "in " << (frame.IsOutermostMainFrame() ? "" : "subframe ")
             << frame.GetDocument()->Url() << ", viewport was "
             << (impact_fraction * 100) << "% impacted with distance fraction "
             << move_distance_factor << " and subframe weighting factor "
@@ -611,7 +611,7 @@ void LayoutShiftTracker::ReportShift(double score_delta,
       "frame", ToTraceValue(&frame));
 
   if (ShouldLog(frame)) {
-    VLOG(1) << "in " << (frame.IsMainFrame() ? "" : "subframe ")
+    VLOG(1) << "in " << (frame.IsOutermostMainFrame() ? "" : "subframe ")
             << frame.GetDocument()->Url().GetString() << ", layout shift of "
             << score_delta
             << (had_recent_input ? " excluded by recent input" : " reported")
