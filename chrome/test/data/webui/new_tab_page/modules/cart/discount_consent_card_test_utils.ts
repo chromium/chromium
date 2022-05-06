5e8db709c9aeff6bf868240e6a54dc508e89e330
dpapad <dpapad@chromium.org>
2022-05-06 20:34:42 +0000

    Fix newly found license header violations in some TypeScript files.
    These checks were enabled at https://crrev.com/c/3626289.
    
    Bug: 1309977
    Change-Id: Iafd1d8f1b6e1676f47c03dbe66d476638b8ba10b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629915
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000543}

--- chrome/test/data/webui/new_tab_page/modules/cart/discount_consent_card_test_utils.ts
+++ chrome/test/data/webui/new_tab_page/modules/cart/discount_consent_card_test_utils.ts
@@ -1,6 +1,6 @@
 // Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// found in the LICENSE file.
 
 import {DiscountConsentCard} from 'chrome://new-tab-page/lazy_load.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
