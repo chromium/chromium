8d9434804747eca0b10a35dff5a5032efb41688a
Lan Wei <lanwei@chromium.org>
2022-05-06 22:46:55 +0000

    Deflake total input delay metric browser test
    This test is to verify the total time of input delay of three mouse
    click events. The test was flaky is because we did not wait for main
    frame's hit-test data before sending the mouse-event and and we should
    also ensure the compositor thread is aware of the event listener and we
    should also listen to all the mouse events that are generated from the
    three mouse clicks.
    
    Bug: 1163677
    Change-Id: I6826b18f13795e493ee403282549ec709ff5e615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3605760
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000587}

--- chrome/browser/page_load_metrics/integration_tests/total_input_delay_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/total_input_delay_browsertest.cc
@@ -8,6 +8,7 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/hit_test_region_observer.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
 using ukm::TestUkmRecorder;
@@ -61,14 +62,52 @@ IN_PROC_BROWSER_TEST_F(TotalInputDelayIntegrationTest, NoInputEvent) {
       PageLoad::kInteractiveTiming_TotalAdjustedInputDelayName, int64_t(0), 0);
 }
 
-// Flaky: crbug.com/1163677
-IN_PROC_BROWSER_TEST_F(TotalInputDelayIntegrationTest,
-                       DISABLED_MultipleInputEvents) {
+IN_PROC_BROWSER_TEST_F(TotalInputDelayIntegrationTest, MultipleInputEvents) {
   LoadHTML(R"HTML(
-    <p>Sample website</p>
+    <script type="text/javascript">
+    let eventCounts = {mouseup: 0, pointerup: 0, click: 0};
+    const loadPromise = new Promise(resolve => {
+      window.addEventListener("load", () => {
+        resolve(true);
+      });
+    });
+
+    let eventPromise;
+    checkLoad = async () => {
+      await loadPromise;
+      eventPromise = new Promise(resolve => {
+        for (let evt in eventCounts) {
+          window.addEventListener(evt, function(e) {
+            eventCounts[e.type]++;
+            if (eventCounts.click == 3 &&
+                eventCounts.pointerup == 3 &&
+                eventCounts.mouseup == 3) {
+              resolve(true);
+            }
+          });
+        }
+      });
+      return true;
+    };
+
+    runtest = async () => {
+      // return await eventPromise;
+      return true;
+    };
+    </script>
   )HTML");
 
   StartTracing({"loading"});
+
+  // Make sure the page is fully loaded.
+  ASSERT_TRUE(EvalJs(web_contents(), "checkLoad()").ExtractBool());
+
+  // We should wait for the main frame's hit-test data to be ready before
+  // sending the click event below to avoid flakiness.
+  content::WaitForHitTestData(web_contents()->GetMainFrame());
+  // Ensure the compositor thread is aware of the mouse events.
+  content::MainThreadFrameObserver frame_observer(GetRenderWidgetHost());
+  frame_observer.Wait();
 
   // Simulate user's input.
   content::SimulateMouseClick(web_contents(), 0,
@@ -77,6 +116,8 @@ IN_PROC_BROWSER_TEST_F(TotalInputDelayIntegrationTest,
                               blink::WebMouseEvent::Button::kLeft);
   content::SimulateMouseClick(web_contents(), 0,
                               blink::WebMouseEvent::Button::kLeft);
+
+  ASSERT_TRUE(EvalJs(web_contents(), "runtest()").ExtractBool());
 
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL("about:blank")));
 
