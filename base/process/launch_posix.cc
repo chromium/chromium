53be7a06e6d5006faa74bd6e93420e2e2522d45f
马爱国 <imaiguo@gmail.com>
2022-05-06 07:55:56 +0000

    feat: Add loongarch support in LaunchProcess
    support cpu like: 3A5000 3C5000
    
    BUG=1307193
    
    Change-Id: I41c77e750df38985fe13c51b4b3ebb86f691b945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529910
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000279}

--- base/process/launch_posix.cc
+++ base/process/launch_posix.cc
@@ -706,7 +706,7 @@ NOINLINE pid_t CloneAndLongjmpInChild(unsigned long flags,
   alignas(16) char stack_buf[PTHREAD_STACK_MIN];
 #if defined(ARCH_CPU_X86_FAMILY) || defined(ARCH_CPU_ARM_FAMILY) ||   \
     defined(ARCH_CPU_MIPS_FAMILY) || defined(ARCH_CPU_S390_FAMILY) || \
-    defined(ARCH_CPU_PPC64_FAMILY)
+    defined(ARCH_CPU_PPC64_FAMILY) || defined(ARCH_CPU_LOONG_FAMILY)
   // The stack grows downward.
   void* stack = stack_buf + sizeof(stack_buf);
 #else
