542a48948361fc4a086c7d25ed88b055e378c63d
Alex Gough <ajgo@chromium.org>
2022-05-06 23:21:46 +0000

    Mark KeyEventType, KeyEventResult and CommitTextCursorBehavior as [Extensible]
    These three enums are marked as [Stable] but not [Extensible]. It
    should be safe to mark them as [Extensible] without adding any values.
    
    CommitTextCursorBehavior only seems to be supplied here[0] where
    kMoveCursorAfterText is the chosen value. The only use of
    kMoveCursorBeforeText is here[1] which is just used to map to the
    underlying type. Existing use-sites should therefore be safe,
    and After makes for a sensible [Default] for the enum.
    
    KeyEventType and KeyEventResult are effectively used as bools[2].
    Any existing callers will be expecting or sending one of the existing
    values, so it should be fine to set a [Default] and not add any fields.
    
    [0] https://source.chromium.org/chromium/chromium/src/+/main:ash/services/ime/rule_based_engine.cc;drc=287fa204b78ccaa8da5c1a155c5fbfacafef31c8;l=50
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/input_method/native_input_method_engine.cc;drc=dc9cf084eee60bba076ee7fdca546ae68b94c907;l=1107
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/input_method/native_input_method_engine.cc;drc=287fa204b78ccaa8da5c1a155c5fbfacafef31c8;l=407
    
    Bug: 1193875
    Change-Id: Ia3814932ec027b9f69c21011e96529d6813c6e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3628399
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000603}

--- mojo/public/tools/bindings/checks/mojom_attributes_check.py
+++ mojo/public/tools/bindings/checks/mojom_attributes_check.py
@@ -80,9 +80,6 @@ _UNION_FIELD_ATTRIBUTES = _COMMON_FIELD_ATTRIBUTES | {
 
 # TODO(https://crbug.com/1193875) empty this set and remove the allowlist.
 _STABLE_ONLY_ALLOWLISTED_ENUMS = {
-    'ash.ime.mojom.CommitTextCursorBehavior',
-    'ash.ime.mojom.KeyEventResult',
-    'ash.ime.mojom.KeyEventType',
     'crosapi.mojom.OptionalBool',
     'crosapi.mojom.RequestActivityIconsStatus',
     'crosapi.mojom.RequestTextSelectionActionsStatus',
