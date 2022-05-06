0e2c16e80566ca62b820f9d09ee5041cf4d5d3bc
Traian Captan <tcaptan@chromium.org>
2022-05-06 18:18:36 +0000

    Fixed typo in PaintLayer comments
    R=pdr
    
    Bug: None
    Change-Id: I72a36cfc7c465786e51107cf03352319bbd34920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3629965
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000465}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -124,7 +124,7 @@ struct CORE_EXPORT PaintLayerRareData final
 // PaintLayer is an old object that handles lots of unrelated operations.
 //
 // We want it to die at some point and be replaced by more focused objects,
-// which would remove (or at least compartimentalize) a lot of complexity.
+// which would remove (or at least compartmentalize) a lot of complexity.
 // See the STATUS OF PAINTLAYER section below.
 //
 // The class is central to painting and hit-testing. That's because it handles
