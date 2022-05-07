3b82aab9d610bf729a986ef644d77811547ec8d0
Bruce Dawson <brucedawson@chromium.org>
2022-05-07 00:58:42 +0000

    Handle thread name exception correctly
    Microsoft's sample code for SetThreadName uses
    __except(EXCEPTION_EXECUTE_HANDLER) to handle the exception, whereas
    Chrome uses __except(EXCEPTION_CONTINUE_EXECUTION). This causes problems
    on Windows 11 when Chromium is embedded and a monitoring process or
    debugger that can't handle this is attached.
    
    The code has used EXCEPTION_CONTINUE_EXECUTION since at least 2010, and
    probably the sample code used to recommend that, but it seems reasonable
    to update the code to the new recommendation.
    
    The SetThreadDescription API has mostly replaced the need for the old
    exception-based method of setting the thread name but we might as well
    leave it around in case any legacy tools still use it.
    
    The code reviewer noticed that we were dividing by sizeof(DWORD)
    instead of by sizeof(ULONG_PTR) so we were passing the wrong argument
    count in 64-bit versions of Chrome so this change fixes that as well.
    
    Bug: 1321977
    Change-Id: I8dd143b5a573656ac236762329d8cc718907b5de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3630632
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1000641}

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -81,9 +81,9 @@ void SetNameInternal(PlatformThreadId thread_id, const char* name) {
   info.dwFlags = 0;
 
   __try {
-    RaiseException(kVCThreadNameException, 0, sizeof(info)/sizeof(DWORD),
-                   reinterpret_cast<DWORD_PTR*>(&info));
-  } __except(EXCEPTION_CONTINUE_EXECUTION) {
+    RaiseException(kVCThreadNameException, 0, sizeof(info) / sizeof(ULONG_PTR),
+                   reinterpret_cast<ULONG_PTR*>(&info));
+  } __except (EXCEPTION_EXECUTE_HANDLER) {
   }
 }
 
