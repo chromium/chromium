5e5b56711a386f9e84fb55bae49ea18dc8b1ba4d
Richard C Yeh <rcy@google.com>
2022-05-10 13:55:54 +0000

    Uprev min env version for rtanalytics-full for M100.
    Also delete rtanalytics-light, because we are no longer
    producing that (and have not produced it in years).
    
    Bug: b/215754013
    Change-Id: I1c35b1780232ed5fcb311571f5480624f8f636d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3635363
    Commit-Queue: Richard Yeh <rcy@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001474}

--- chrome/browser/component_updater/cros_component_installer_chromeos.cc
+++ chrome/browser/component_updater/cros_component_installer_chromeos.cc
@@ -39,9 +39,7 @@ namespace {
 const ComponentConfig kConfigs[] = {
     {"cros-termina", ComponentConfig::PolicyType::kEnvVersion, "980.1",
      "e9d960f84f628e1f42d05de4046bb5b3154b6f1f65c08412c6af57a29aecaffb"},
-    {"rtanalytics-light", ComponentConfig::PolicyType::kEnvVersion, "96.0",
-     "69f09d33c439c2ab55bbbe24b47ab55cb3f6c0bd1f1ef46eefea3216ec925038"},
-    {"rtanalytics-full", ComponentConfig::PolicyType::kEnvVersion, "96.0",
+    {"rtanalytics-full", ComponentConfig::PolicyType::kEnvVersion, "100.0",
      "c93c3e1013c52100a20038b405ac854d69fa889f6dc4fa6f188267051e05e444"},
     {"demo-mode-resources", ComponentConfig::PolicyType::kEnvVersion, "1.0",
      "93c093ebac788581389015e9c59c5af111d2fa5174d206eb795042e6376cbd10"},
