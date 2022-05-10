bea9806e16b7f19743781a26bf7fd6bcff835d7a
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-05-10 17:55:37 +0000

    [cleanup] Remove unnecessary logic within ComputeMinMaxInlineSizes.
    As above. This line is unnessecery - as we only reach this line if
    we don't have a definite block-size.
    
    There should be no behaviour change.
    
    Change-Id: Ifc85ee0897f8350ca8656670f3fb4eee44470434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639264
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001598}

--- third_party/blink/renderer/core/layout/ng/ng_length_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_length_utils.h
@@ -329,9 +329,7 @@ MinMaxSizes ComputeMinMaxInlineSizes(const NGConstraintSpace& space,
 
   // This implements the transferred min/max sizes per:
   // https://drafts.csswg.org/css-sizing-4/#aspect-ratio-size-transfers
-  if (!style.AspectRatio().IsAuto() &&
-      BlockLengthUnresolvable(space, style.LogicalHeight()) &&
-      style.LogicalWidth().IsAuto() &&
+  if (!style.AspectRatio().IsAuto() && style.LogicalWidth().IsAuto() &&
       space.InlineAutoBehavior() != NGAutoBehavior::kStretchExplicit) {
     MinMaxSizes transferred_sizes =
         ComputeMinMaxInlineSizesFromAspectRatio(space, style, border_padding);
