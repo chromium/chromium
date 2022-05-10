58bbeccfc251955cab8716c6fec5b8e05e7c1ec6
Maggie Chen <magchen@chromium.org>
2022-05-10 01:23:49 +0000

    Allow overlay trace tests to take onscreen size and swap delay as input.
    The purpose of this CL is to prepare for the fix on the flaky tests by adjusting overlay onscreen size and swap delay.
    
    No actual change in size or timing in this CL.
    
    Bug: 1278681
    Change-Id: I9cca846cdb8b7f03aeb6e6c08c3cd1f2937ab48e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3636103
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1001275}

--- content/test/data/gpu/pixel_video_test.js
+++ content/test/data/gpu/pixel_video_test.js
@@ -5,6 +5,12 @@
 var video;
 var abort = false;
 
+// The time waiting for collecting overlay presentation statistics.
+// 500 ms is supposed to be 15 frames at 30 Hz. However, only ~7 frames or less
+// are recorded in OverlayModeTraceTest_DirectComposition tests.
+// Reducing this delay might cause flakes in some tests.
+var delayMs = 500;
+
 function logOutput(s) {
   if (window.domAutomationController)
     window.domAutomationController.log(s);
@@ -12,10 +18,46 @@ function logOutput(s) {
     console.log(s);
 }
 
+const parsedString = (function (names) {
+  const pairs = {};
+  for (let i = 0; i < names.length; ++i) {
+    var keyValue = names[i].split('=', 2);
+    if (keyValue.length == 1)
+      pairs[keyValue[0]] = '';
+    else
+      pairs[keyValue[0]] = decodeURIComponent(keyValue[1].replace(/\+/g, ' '));
+  }
+  return pairs;
+})(window.location.search.substr(1).split('&'));
+
+function getTimeDelay() {
+  let delayString = parsedString['delay'];
+  if (delayString != undefined)
+    delayMs = delayString;
+
+  return delayMs;
+}
+
+function setVideoSize() {
+  let width = '240';
+  let height = '135';
+
+  let widthString = parsedString['width'];
+  let heightString = parsedString['height'];
+  if (widthString != undefined)
+    width = widthString;
+  if (heightString != undefined)
+    height = heightString;
+
+  video.width = width;
+  video.height = height;
+}
+
 function main() {
   video = document.getElementById('video');
   video.loop = true;
   video.muted = true;  // No need to exercise audio paths.
+  setVideoSize(video);
 
   video.onerror = e => {
     logOutput(`Test failed: ${e.message}`);
@@ -33,12 +75,13 @@ function main() {
     logOutput(`First frame: ${f.width}x${f.height}, ts: ${f.mediaTime}`);
 
     // Trace tests on Windows need some time to collect statistics from the
-    // overlay system, so allow for a 500ms delay (~30 swaps at 60Hz).
+    // overlay system, so allow delay before verifying the stats. A 1000ms is
+    // about ~30 swaps at 30Hz.
     setTimeout(_ => {
       if (abort)
         return;
       logOutput('Test complete.');
       domAutomationController.send('SUCCESS');
-    }, 500);
+    }, getTimeDelay());
   });
 }
