76b158c30544c48d8be37d72d59f496b58d43691
David Bertoni <dbertoni@chromium.org>
2022-05-11 20:08:23 +0000

    [CodeHealth] Remove uses of ListValue::Append in /chrome/browser/supervised_user.
    This CL removes uses of the overload that accepts a
    std::unique_ptr<base::Value>. It attempts to modernize code where
    feasible and uses base::Value::FromUniquePtrValue where it's not.
    
    This CL was uploaded by git cl split.
    
    R=agawronska@chromium.org
    
    Bug: 1187097
    Change-Id: I489a82eb95752708f3197435cb133fc955252af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640295
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002269}

--- chrome/browser/supervised_user/child_accounts/family_info_fetcher_unittest.cc
+++ chrome/browser/supervised_user/child_accounts/family_info_fetcher_unittest.cc
@@ -77,36 +77,32 @@ std::string BuildEmptyGetFamilyProfileResponse() {
 
 std::string BuildGetFamilyMembersResponse(
     const std::vector<FamilyInfoFetcher::FamilyMember>& members) {
-  base::DictionaryValue dict;
-  auto list = std::make_unique<base::ListValue>();
+  base::Value::Dict dict;
+  base::Value::List list;
   for (size_t i = 0; i < members.size(); i++) {
     const FamilyInfoFetcher::FamilyMember& member = members[i];
-    std::unique_ptr<base::DictionaryValue> member_dict(
-        new base::DictionaryValue);
-    member_dict->SetKey("userId", base::Value(member.obfuscated_gaia_id));
-    member_dict->SetKey(
-        "role", base::Value(FamilyInfoFetcher::RoleToString(member.role)));
+    base::Value::Dict member_dict;
+    member_dict.Set("userId", member.obfuscated_gaia_id);
+    member_dict.Set("role", FamilyInfoFetcher::RoleToString(member.role));
     if (!member.display_name.empty() ||
         !member.email.empty() ||
         !member.profile_url.empty() ||
         !member.profile_image_url.empty()) {
-      auto profile_dict = std::make_unique<base::DictionaryValue>();
+      base::Value::Dict profile_dict;
       if (!member.display_name.empty())
-        profile_dict->SetKey("displayName", base::Value(member.display_name));
+        profile_dict.Set("displayName", member.display_name);
       if (!member.email.empty())
-        profile_dict->SetKey("email", base::Value(member.email));
+        profile_dict.Set("email", member.email);
       if (!member.profile_url.empty())
-        profile_dict->SetKey("profileUrl", base::Value(member.profile_url));
+        profile_dict.Set("profileUrl", member.profile_url);
       if (!member.profile_image_url.empty())
-        profile_dict->SetKey("profileImageUrl",
-                             base::Value(member.profile_image_url));
+        profile_dict.Set("profileImageUrl", member.profile_image_url);
 
-      member_dict->SetKey(
-          "profile", base::Value::FromUniquePtrValue(std::move(profile_dict)));
+      member_dict.Set("profile", std::move(profile_dict));
     }
-    list->Append(std::move(member_dict));
+    list.Append(std::move(member_dict));
   }
-  dict.SetKey("members", base::Value::FromUniquePtrValue(std::move(list)));
+  dict.Set("members", std::move(list));
   std::string result;
   base::JSONWriter::Write(dict, &result);
   return result;
