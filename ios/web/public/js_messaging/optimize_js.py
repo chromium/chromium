eacaac5095b75a408bee5f9d0fabec0d409f0a65
Mike Dougherty <michaeldo@chromium.org>
2022-05-11 16:17:20 +0000

    Update optimize_js.gni to support dependencies
    Imported scripts will be resolved against the files listed in the
    manifest files produced by the targets in `deps`. If the import is
    not listed in any of the provided manifest files, it is assumed to exist
    at `root_dir`.
    
    Additionally, add support for an `output_name` which is passed to
    rollup to specify the name of JS exports.
    
    Bug: 1316376
    Change-Id: I9aad9591d37e0e2541dde0a67f6bdedaf2c86b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627128
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002127}

--- ios/web/public/js_messaging/optimize_js.py
+++ ios/web/public/js_messaging/optimize_js.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 import argparse
+import json
 import os
 import shutil
 import sys
@@ -16,25 +17,48 @@ sys.path.append(os.path.join(_SRC_PATH, 'third_party', 'node'))
 import node
 import node_modules
 
-def create_rollup_config(config_out_file, rollup_root_dir):
+def create_rollup_config(config_out_file, rollup_root_dir, path_mappings):
   """Generates a rollup config file to configure and use the path resolver
   plugin which enforces import path format."""
   plugin_path = os.path.join(os.path.abspath(_HERE_PATH),
       'rollup_plugin_src_path_resolver.js')
 
+  path_mappings_json = json.dumps(path_mappings, separators=(',', ':'))
+
   config_content = f'''
       import src_path_resolver from '{plugin_path}';
       export default ({{
-        plugins: [ src_path_resolver('{rollup_root_dir}') ]
+        plugins: [
+          src_path_resolver('{rollup_root_dir}', '{path_mappings_json}')
+        ]
       }});
       '''
   with open(config_out_file, 'w') as f:
     f.write(config_content)
 
-def optimize_js(primary_script, js_out_file, skip_minification):
+def optimize_js(primary_script,
+    js_out_file,
+    path_mapping_manifests,
+    skip_minification,
+    output_name):
   """Creates a single output JavaScript file from |primary_script| (and any
-  imported scripts) and writes it out to |js_out_file|. The output script will
-  be minimized unless skip_minification is false."""
+  imported scripts) and writes it out to |js_out_file|. Script imports will be
+  resolved by searching for the script path in the maifest files passed to
+  |path_mapping_manifests| and using the directory location listed in the
+  manifest file to locate the source file.
+  The output script will be minimized unless |skip_minification| is false."""
+
+  path_mappings = {}
+  if path_mapping_manifests != None:
+    for manifest_path in path_mapping_manifests:
+      with open(manifest_path, 'r', encoding='utf-8') as manifest:
+        manifest_contents = json.load(manifest)
+        base_dir = manifest_contents['base_dir']
+        files = manifest_contents['files']
+
+        for f in files:
+          path_mappings[f] = os.path.abspath(os.path.join(base_dir, f))
+
   output_script_dir = os.path.dirname(js_out_file)
 
   os.makedirs(output_script_dir, exist_ok=True)
@@ -42,12 +66,14 @@ def optimize_js(primary_script, js_out_file, skip_minification):
 
     rollup_config_file = \
         os.path.join(tmp_out_dir, 'rollup.config.js')
-    create_rollup_config(rollup_config_file, os.path.abspath(_SRC_PATH))
-
+    create_rollup_config(
+        rollup_config_file,
+        os.path.abspath(_SRC_PATH),
+        path_mappings)
     rollup_processed_file = \
         os.path.join(tmp_out_dir, os.path.basename(js_out_file))
 
-    node.RunNode([node_modules.PathToRollup(),
+    rollup_command = [node_modules.PathToRollup(),
         primary_script,
         '--format',
         'iife',
@@ -58,7 +84,12 @@ def optimize_js(primary_script, js_out_file, skip_minification):
         '--file',
         rollup_processed_file,
         '--silent',
-    ])
+    ]
+
+    if output_name != None:
+      rollup_command += ['--name', output_name]
+
+    node.RunNode(rollup_command)
 
     with open(rollup_processed_file, 'r', encoding='utf-8') as f:
       output = f.read()
@@ -88,15 +119,27 @@ def main(argv):
       required=True,
       help="Path to the output file for the compiled JavaScript to be written")
   parser.add_argument(
+      '--path-mapping-manifests',
+      required=False,
+      nargs='*',
+      default = None,
+      help="Path mapping manifest files to locate script imports.")
+  parser.add_argument(
       '--skip-minification',
       default=False,
       action='store_true')
+  parser.add_argument(
+      '--output-name',
+      required=False,
+      default = None,
+      help="The name for the exported symbols")
   args = parser.parse_args(argv)
 
   primary_script = os.path.abspath(args.primary_script)
   js_out_file = os.path.abspath(args.js_out_file)
 
-  optimize_js(primary_script, js_out_file, args.skip_minification)
+  optimize_js(primary_script, js_out_file,
+      args.path_mapping_manifests, args.skip_minification, args.output_name)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
--- ios/web/public/js_messaging/rollup_plugin_src_path_resolver.js
+++ ios/web/public/js_messaging/rollup_plugin_src_path_resolver.js
@@ -8,19 +8,28 @@
 
 import path from 'path'
 
-export default function src_path_resolver(rootPath) {
+export default function src_path_resolver(rootPath, pathMappingsJson) {
   return {
     name: 'rollup-plugin-src-path-resolver',
 
     resolveId(source, originScript) {
-      // Allow root files to be processed by rollup.
+      // Allow root files to be processed directly by rollup.
       if (!originScript) {
         return null;
       }
 
       if (source.startsWith('//')) {
-        // Strip leading double slashes and combine path with root.
-        return path.join(rootPath, source.substr(2));
+        // Strip leading double slashes
+        const sourcePath = source.substr(2);
+
+        // Check if an import exists within a dependency directory.
+        const pathMappings = JSON.parse(pathMappingsJson);
+        if (sourcePath in pathMappings) {
+          return pathMappings[sourcePath]
+        }
+
+        // Assume file exists at root.
+        return path.join(rootPath, sourcePath);
       }
 
       // Relative paths are acceptable from within third-party scripts.
