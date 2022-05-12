41722b7ddbf2aeca7bacb3ac235283e47526ec46
Matthew Mourgos <mmourgos@chromium.org>
2022-05-12 22:22:16 +0000

    Inset home button focus ring to avoid clipping the focus ring
    before: https://screenshot.googleplex.com/47jwsYrqpM7eZRK.png
    after:  https://screenshot.googleplex.com/7SrFaSfQcijTsXC.png
    
    Bug: 1148720
    Change-Id: I24ea1c84cb44874e7fa8d19d0c330acb617f7851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645150
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002884}

--- ash/shelf/shelf_button.cc
+++ ash/shelf/shelf_button.cc
@@ -11,6 +11,7 @@
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/views/animation/ink_drop.h"
+#include "ui/views/controls/highlight_path_generator.h"
 
 namespace ash {
 
@@ -24,6 +25,10 @@ ShelfButton::ShelfButton(Shelf* shelf,
   SetFocusBehavior(FocusBehavior::ALWAYS);
   views::InkDrop::Get(this)->SetMode(
       views::InkDropHost::InkDropMode::ON_NO_GESTURE_HANDLER);
+  // Inset focus ring path to avoid clipping the edges of the ring.
+  views::FocusRing::Get(this)->SetPathGenerator(
+      std::make_unique<views::CircleHighlightPathGenerator>(
+          gfx::Insets(views::FocusRing::kDefaultHaloInset)));
   SetFocusPainter(nullptr);
   views::InkDrop::UseInkDropForSquareRipple(views::InkDrop::Get(this),
                                             /*highlight_on_hover=*/false);
