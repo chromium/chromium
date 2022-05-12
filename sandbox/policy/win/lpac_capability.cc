665fe35103b5fbd57a825098b646c57d9411f60c
Xiaohan Wang <xhwang@chromium.org>
2022-05-12 02:00:00 +0000

    media: Revert kMediaFoundationCdmData name change
    In crrev.com/c/3615261, we renamed kMediaFoundationCdmData which caused
    permission error. This CL reverts that change so the issue doesn't propagate to beta or stable channels.
    
    Bug: 1323885
    Change-Id: I50ba1e7b14cd0cba2b93e7e806c0c7b50fd6dbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642905
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1002417}

--- sandbox/policy/win/lpac_capability.cc
+++ sandbox/policy/win/lpac_capability.cc
@@ -7,9 +7,11 @@
 namespace sandbox {
 namespace policy {
 
+// WARNING: Renaming a capability could cause backward compatible issues!
+
 // Capability used by the Media Foundation CDM to grant read and write access to
 // a folder under the Chrome user's profile.
-const wchar_t kMediaFoundationCdmData[] = L"mediaFoundationCdmData";
+const wchar_t kMediaFoundationCdmData[] = L"lpacMediaFoundationCdmData";
 
 // Capability for Media Foundation CDM files that needs read and execute access.
 const wchar_t kMediaFoundationCdmFiles[] = L"mediaFoundationCdmFiles";
