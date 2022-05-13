6f5ee6270f41a305ef938eaefdf71725bb0e05f4
Chang Lyu <changlyu@google.com>
2022-05-13 16:26:58 +0000

    Add translation to browser switch URL checker reason
    Adding translation of i18n to browser switch internal, section URL checker, display reason part.
    
    Bug: 1258133
    Change-Id: I8b0dc9540a9d61924d66e36b43052cdf8cd0ce39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3641151
    Commit-Queue: Chang Lyu <changlyu@google.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003153}

--- chrome/browser/resources/browser_switch/internals/app.ts
+++ chrome/browser/resources/browser_switch/internals/app.ts
@@ -149,31 +149,30 @@ class BrowserSwitchInternalsAppElement extends
     let reason = '';
     if (decision.matching_rule) {
       if (decision.matching_rule.startsWith('!')) {
-        reason += `Reason: The inverted rule ${
-            JSON.stringify(decision.matching_rule)} was found in `;
+        reason += this.i18n(
+                      'openBrowserInvertRuleReason',
+                      JSON.stringify(decision.matching_rule)) +
+            '\n';
       } else {
-        reason +=
-            `Reason: ${JSON.stringify(decision.matching_rule)} was found in `;
+        const list = decision.reason === 'sitelist' ?
+            this.i18n('forceOpenTitle') :
+            this.i18n('ignoreTitle');
+        reason += this.i18n(
+                      'openBrowserRuleReason',
+                      JSON.stringify(decision.matching_rule), list) +
+            '\n';
       }
     }
     // if undefined - add nothing to the output
 
     switch (decision.reason) {
       case 'globally_disabled':
-        reason += 'Reason: The BrowserSwitcherEnabled policy is false.\n';
-        break;
+        throw new Error('BrowserSwitcherEnabled policy is set as false!');
       case 'protocol':
-        reason +=
-            'Reason: LBS only supports http://, https://, and file:// URLs.\n';
-        break;
-      case 'sitelist':
-        reason += 'the "Force open in" list.\n';
-        break;
-      case 'greylist':
-        reason += 'the "Ignore" list.\n';
+        reason += this.i18n('openBrowserProtocolReason') + '\n';
         break;
       case 'default':
-        reason += `Reason: LBS stays in ${browserName} by default.\n`;
+        reason += this.i18n('openBrowserDefaultReason', browserName) + '\n';
         break;
     }
 
