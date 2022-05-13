a282bca7f307276bf594efbf1bb812befc5bb580
Katherine Lai <laikatherine@google.com>
2022-05-13 18:45:59 +0000

    [Floss] Reenable device filtering
    Floss disabled device filtering since there were missing APIs
    for getting device info (COD, transport type). Those have since
    been added and we can reenable device filtering.
    
    Bug: b:202335393
    Change-Id: Ib8a36448c5ac8faaaaa5b6f48bb12d446266c82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646631
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003238}

--- device/bluetooth/chromeos/bluetooth_utils.cc
+++ device/bluetooth/chromeos/bluetooth_utils.cc
@@ -17,7 +17,6 @@
 #include "build/chromeos_buildflags.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "device/base/features.h"
-#include "device/bluetooth/floss/floss_features.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -181,12 +180,6 @@ device::BluetoothAdapter::DeviceList FilterBluetoothDeviceList(
 }
 
 bool IsUnsupportedDevice(const device::BluetoothDevice* device) {
-  // With Floss, device list filtering is still unstable. We disable filtering
-  // first so that Floss testing of other features can be unblocked.
-  // TODO(b/202335393): Enable device filtering once it's stable with Floss.
-  if (base::FeatureList::IsEnabled(floss::features::kFlossEnabled))
-    return false;
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (ash::switches::IsUnfilteredBluetoothDevicesEnabled())
     return false;
