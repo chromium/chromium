df0eb6bc00e90d55beeb400ce9a414c4a909accf
Simon Pelchat <spelchat@chromium.org>
2022-05-13 19:21:00 +0000

    Do not use event listeners for anchor element interaction preloading.
    Using event listeners causes two issues:
    
    1) It causes FirstInputDelay calculations to be wrong because all link
       clicks are not included in FirstInputDelay calculations, as if the
       page inserted the event listener.
    2) Many tests assume that there are no event listeners installed on
       an empty document.
    
    Bug: 1323865
    Change-Id: I2d5afe3dd151fe8b5969db7fcc64d5a7b391cb07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640543
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Max Curran <curranmax@google.com>
    Cr-Commit-Position: refs/heads/main@{#1003258}

--- third_party/blink/renderer/core/loader/anchor_element_interaction_tracker.h
+++ third_party/blink/renderer/core/loader/anchor_element_interaction_tracker.h
@@ -6,22 +6,25 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_ANCHOR_ELEMENT_INTERACTION_TRACKER_H_
 
 #include "third_party/blink/public/mojom/loader/anchor_element_interaction_host.mojom-blink.h"
+#include "third_party/blink/renderer/core/html/html_anchor_element.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 
 namespace blink {
 
-class AnchorElementListener;
 class Document;
+class EventTarget;
+class HTMLAnchorElement;
 class KURL;
+class Node;
+class PointerEvent;
 
-// Creates an event listener for mousedown events anywhere on a document.
-// If there is one, the listener will retrieve the valid href from the anchor
-// element from the event. The tracker will report the href value to the
-// browser process via Mojo. The browser process can use this information
-// to preload (e.g. preconnect the origin) the URL in order to improve
-// performance.
+// Tracks pointerdown events anywhere on a document.  On receiving a pointerdown
+// event, the tracker will retrieve the valid href from the anchor element from
+// the event and will report the href value to the browser process via Mojo. The
+// browser process can use this information to preload (e.g. preconnect to the
+// origin) the URL in order to improve performance.
 class AnchorElementInteractionTracker
     : public GarbageCollected<AnchorElementInteractionTracker> {
  public:
@@ -29,12 +32,17 @@ class AnchorElementInteractionTracker
 
   static bool IsFeatureEnabled();
 
-  void OnPointerDown(const KURL& url);
+  void OnPointerDown(EventTarget& target, const PointerEvent& pointer_event);
 
   void Trace(Visitor* visitor) const;
 
  private:
-  Member<AnchorElementListener> anchor_element_listener_;
+  HTMLAnchorElement* FirstAnchorElementIncludingSelf(Node* node);
+
+  // Gets the `anchor's` href attribute if it is part
+  // of the HTTP family
+  KURL GetHrefEligibleForPreloading(const HTMLAnchorElement& anchor);
+
   HeapMojoRemote<mojom::blink::AnchorElementInteractionHost> interaction_host_;
 };
 
