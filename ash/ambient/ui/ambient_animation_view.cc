4958920f346fb3a5acdf41dedcf319678b253f59
Eric Sum <esum@google.com>
2022-05-13 02:14:35 +0000

    Move media string to top-right corner of screen.
    This was motivated by portrait mode. UX decided to move it to
    the top-right with 28 pixel padding for both landscape and
    portrait mode so that it doesn't possibly overlap with the
    text attribution at the bottom of the screen.
    
    This CL also adds a text shadow correction for weather/time
    that was missing before. It barely makes a noticeable difference
    since it's just a couple of pixels, but it's technically more
    correct and consistent with how the slideshow screensaver
    positions it.
    
    Bug: b:217271296
    
    Change-Id: I83db86e63aaf5f05c92eaa2a4fb72265200fbd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3640545
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1002939}

--- ash/ambient/ui/ambient_animation_view.cc
+++ ash/ambient/ui/ambient_animation_view.cc
@@ -38,6 +38,7 @@
 #include "ui/compositor/compositor.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/geometry/outsets.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/shadow_value.h"
 #include "ui/views/background.h"
@@ -67,10 +68,9 @@ constexpr base::TimeDelta kThroughputTrackerRestartPeriod = base::Seconds(30);
 // AmbientAnimationView to the top-left of the weather/time content views.
 constexpr int kWeatherTimeBorderPaddingDip = 28;
 
-// Amount of padding from the left and bottom of the AmbientAnimationView's
-// bounds to the bottom-left of the media string content views.
-constexpr int kMediaStringPaddingFromLeftDip = 28;
-constexpr int kMediaStringPaddingFromBottomDip = 24;
+// Amount of padding from the top-right of the AmbientAnimationView's
+// bounds to the top-right of the media string content views.
+constexpr int kMediaStringPaddingDip = 28;
 
 constexpr int kMediaStringTextElevation = 1;
 
@@ -108,12 +108,38 @@ int GetPaddingForAnimationJitter() {
                    abs(kAnimationJitterConfig.y_max_translation)});
 }
 
+// When text with shadows requires X pixels of padding from the edges of its
+// bounding view, it is not always sufficient to simply create a border within
+// the view that is X pixels wide. In the event that the text's shadow extends
+// past the text in a given direction, the text's shadow ends up with X pixels
+// of padding from the edge rather than the text itself.
+//
+// This returns the amount to *subtract* from each side of a text view's border
+// such that the text ultimately has X pixels of padding from the view's edge,
+// and the shadow may extend into the padding.
+gfx::Outsets GetTextShadowCorrection(const gfx::ShadowValues& text_shadows) {
+  // A positive shadow outset means the shadow extends past the text in that
+  // direction. A negative shadow outset means the shadow is "behind" the text
+  // in that direction. In this case, subtracting the negative outset value
+  // will result in padding that is too large (X + <shadow offset>). Hence,
+  // impose a "floor" of 0 pixels here.
+  static constexpr gfx::Outsets kZeroOutsetsFloor;
+  gfx::Outsets shadow_outsets =
+      gfx::ShadowValue::GetMargin(text_shadows).ToOutsets();
+  shadow_outsets.SetToMax(kZeroOutsetsFloor);
+  return shadow_outsets;
+}
+
 // The border serves as padding between the GlanceableInfoView and its
 // parent view's bounds.
 std::unique_ptr<views::Border> CreateGlanceableInfoBorder(
     const gfx::Vector2d& jitter = gfx::Vector2d()) {
-  int top_padding = kWeatherTimeBorderPaddingDip + jitter.y();
-  int left_padding = kWeatherTimeBorderPaddingDip + jitter.x();
+  gfx::Outsets shadow_text_correction =
+      GetTextShadowCorrection(ambient::util::GetTextShadowValues(nullptr));
+  int top_padding =
+      kWeatherTimeBorderPaddingDip - shadow_text_correction.top() + jitter.y();
+  int left_padding =
+      kWeatherTimeBorderPaddingDip - shadow_text_correction.left() + jitter.x();
   DCHECK_GE(top_padding, 0);
   DCHECK_GE(left_padding, 0);
   return views::CreateEmptyBorder(
@@ -124,16 +150,16 @@ std::unique_ptr<views::Border> CreateGlanceableInfoBorder(
 // parent view's bounds.
 std::unique_ptr<views::Border> CreateMediaStringBorder(
     const gfx::Vector2d& jitter = gfx::Vector2d()) {
-  gfx::Insets shadow_insets = gfx::ShadowValue::GetMargin(
+  gfx::Outsets shadow_text_correction = GetTextShadowCorrection(
       ambient::util::GetTextShadowValues(nullptr, kMediaStringTextElevation));
-  int bottom_padding =
-      kMediaStringPaddingFromBottomDip + shadow_insets.bottom() + jitter.y();
-  int left_padding =
-      kMediaStringPaddingFromLeftDip + shadow_insets.left() + jitter.x();
-  DCHECK_GE(bottom_padding, 0);
-  DCHECK_GE(left_padding, 0);
+  int top_padding =
+      kMediaStringPaddingDip - shadow_text_correction.top() + jitter.y();
+  int right_padding =
+      kMediaStringPaddingDip - shadow_text_correction.right() + jitter.x();
+  DCHECK_GE(top_padding, 0);
+  DCHECK_GE(right_padding, 0);
   return views::CreateEmptyBorder(
-      gfx::Insets::TLBR(0, left_padding, bottom_padding, 0));
+      gfx::Insets::TLBR(top_padding, 0, 0, right_padding));
 }
 
 }  // namespace
@@ -244,16 +270,16 @@ void AmbientAnimationView::Init(AmbientViewDelegate* view_delegate) {
       view_delegate, kTimeFontSizeDip,
       /*time_temperature_font_color=*/gfx::kGoogleGrey900));
 
-  // Media string should appear in the bottom-left corner of the
+  // Media string should appear in the top-right corner of the
   // AmbientAnimationView's bounds.
   media_string_container_ =
       AddChildView(std::make_unique<views::BoxLayoutView>());
   media_string_container_->SetOrientation(
       views::BoxLayout::Orientation::kVertical);
   media_string_container_->SetMainAxisAlignment(
-      views::BoxLayout::MainAxisAlignment::kEnd);
+      views::BoxLayout::MainAxisAlignment::kStart);
   media_string_container_->SetCrossAxisAlignment(
-      views::BoxLayout::CrossAxisAlignment::kStart);
+      views::BoxLayout::CrossAxisAlignment::kEnd);
   media_string_container_->SetBorder(CreateMediaStringBorder());
   MediaStringView* media_string_view = media_string_container_->AddChildView(
       std::make_unique<MediaStringView>(MediaStringView::Settings(
