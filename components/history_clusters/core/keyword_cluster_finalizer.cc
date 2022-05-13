0b6a73f652eaa631d78ad21b1de7349ab45e2a09
Sophie Chang <sophiechang@chromium.org>
2022-05-13 19:27:07 +0000

    Plumb aliases to the keywords for journeys omnibox action
    This also removes the batching code in OnDeviceClusteringBackend for
    simplicity of passing the entity metadata map around and moves the
    params for anything that controls omnibox action keywords to the
    omniboxaction feature for easier experimentation
    
    Bug: 1324728
    Change-Id: Ia582bd6c82bf87609f3c1620c26b46711b80b7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643460
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003266}

--- components/history_clusters/core/keyword_cluster_finalizer.cc
+++ components/history_clusters/core/keyword_cluster_finalizer.cc
@@ -10,17 +10,20 @@
 #include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "components/history_clusters/core/on_device_clustering_util.h"
+#include "components/optimization_guide/core/entity_metadata.h"
 
 namespace history_clusters {
 
-KeywordClusterFinalizer::KeywordClusterFinalizer() = default;
+KeywordClusterFinalizer::KeywordClusterFinalizer(
+    const base::flat_map<std::string, optimization_guide::EntityMetadata>&
+        entity_metadata_map)
+    : entity_metadata_map_(entity_metadata_map) {}
 KeywordClusterFinalizer::~KeywordClusterFinalizer() = default;
 
 void KeywordClusterFinalizer::FinalizeCluster(history::Cluster& cluster) {
   base::flat_set<std::u16string> keywords_set;
   for (const auto& visit : cluster.visits) {
-    if (GetConfig().should_exclude_keywords_from_noisy_visits &&
-        IsNoisyVisit(visit)) {
+    if (!GetConfig().keyword_filter_on_noisy_visits && IsNoisyVisit(visit)) {
       // Do not put keywords if user visits the page a lot and it's not a
       // search-like visit.
       continue;
@@ -29,8 +32,20 @@ void KeywordClusterFinalizer::FinalizeCluster(history::Cluster& cluster) {
     for (const auto& entity :
          visit.annotated_visit.content_annotations.model_annotations.entities) {
       keywords_set.insert(base::UTF8ToUTF16(entity.id));
+
+      if (GetConfig().keyword_filter_on_entity_aliases) {
+        auto it = entity_metadata_map_.find(entity.id);
+        if (it != entity_metadata_map_.end()) {
+          for (size_t i = 0; i < it->second.human_readable_aliases.size() &&
+                             i < GetConfig().max_entity_aliases_in_keywords;
+               i++) {
+            keywords_set.insert(
+                base::UTF8ToUTF16(it->second.human_readable_aliases[i]));
+          }
+        }
+      }
     }
-    if (GetConfig().should_include_categories_in_keywords) {
+    if (GetConfig().keyword_filter_on_categories) {
       for (const auto& category : visit.annotated_visit.content_annotations
                                       .model_annotations.categories) {
         keywords_set.insert(base::UTF8ToUTF16(category.id));
--- components/history_clusters/core/keyword_cluster_finalizer.h
+++ components/history_clusters/core/keyword_cluster_finalizer.h
@@ -5,18 +5,33 @@
 #ifndef COMPONENTS_HISTORY_CLUSTERS_CORE_KEYWORD_CLUSTER_FINALIZER_H_
 #define COMPONENTS_HISTORY_CLUSTERS_CORE_KEYWORD_CLUSTER_FINALIZER_H_
 
+#include <string>
+
+#include "base/containers/flat_set.h"
 #include "components/history_clusters/core/cluster_finalizer.h"
+
+namespace optimization_guide {
+struct EntityMetadata;
+}  // namespace optimization_guide
 
 namespace history_clusters {
 
 // A cluster finalizer that determines the set of keywords for a given cluster.
 class KeywordClusterFinalizer : public ClusterFinalizer {
  public:
-  KeywordClusterFinalizer();
+  explicit KeywordClusterFinalizer(
+      const base::flat_map<std::string, optimization_guide::EntityMetadata>&
+          entity_metadata_map);
   ~KeywordClusterFinalizer() override;
 
   // ClusterFinalizer:
   void FinalizeCluster(history::Cluster& cluster) override;
+
+ private:
+  // A map from human readable entity name to the metadata associated with that
+  // entity name.
+  const base::flat_map<std::string, optimization_guide::EntityMetadata>
+      entity_metadata_map_;
 };
 
 }  // namespace history_clusters
