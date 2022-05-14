ad7e4d8ad3bf835412ed4120fe6ac580105d40a1
dpapad <dpapad@chromium.org>
2022-05-14 00:31:00 +0000

    Management: Import Polymer templates at runtime.
    Also use html_to_wrapper() instead of html_to_js() for Polymer icons
    file.
    
    Bug: 1292025,1322682
    Change-Id: I342315f06791987bc5bac9e9cb2192e1b2921424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3639736
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003419}

--- chrome/browser/resources/management/management_ui.ts
+++ chrome/browser/resources/management/management_ui.ts
@@ -9,14 +9,15 @@ import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
-import './icons.js';
+import './icons.html.js';
 import './strings.m.js';
 
 import {I18nMixin} from 'chrome://resources/js/i18n_mixin.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {sanitizeInnerHtml} from 'chrome://resources/js/parse_html_subset.m.js';
 import {WebUIListenerMixin} from 'chrome://resources/js/web_ui_listener_mixin.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './management_ui.html.js';
 
 import {BrowserReportingResponse, Extension, ManagementBrowserProxy, ManagementBrowserProxyImpl, ReportingType, ThreatProtectionInfo} from './management_browser_proxy.js';
 // <if expr="chromeos_ash">
@@ -33,6 +34,10 @@ const ManagementUiElementBase = WebUIListenerMixin(I18nMixin(PolymerElement));
 class ManagementUiElement extends ManagementUiElementBase {
   static get is() {
     return 'management-ui';
+  }
+
+  static get template() {
+    return getTemplate();
   }
 
   static get properties() {
@@ -376,10 +381,6 @@ class ManagementUiElement extends ManagementUiElementBase {
       this.managementNoticeHtml_ = data.browserManagementNotice;
       // </if>
     });
-  }
-
-  static get template() {
-    return html`{__html_template__}`;
   }
 }
 
