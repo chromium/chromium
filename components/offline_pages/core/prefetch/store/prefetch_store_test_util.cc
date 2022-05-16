e3f7ae1e7d1d560cf8f63d529d450c0055ad1b1c
Solomon Kinard <solomonkinard@chromium.org>
2022-05-16 18:24:58 +0000

    OfflinePages: Remove extra slashes from copyright header
    Bug: None
    Change-Id: I6478fca38ad9c2e21a6562a83d87cc59ddb0e89d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3645685
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1003844}

--- components/offline_pages/core/prefetch/store/prefetch_store_test_util.cc
+++ components/offline_pages/core/prefetch/store/prefetch_store_test_util.cc
@@ -1,5 +1,5 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "components/offline_pages/core/prefetch/store/prefetch_store_test_util.h"
