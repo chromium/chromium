8c7c26bb0d3f884162aee343468b5afa352d5922
Solomon Kinard <solomonkinard@chromium.org>
2022-05-17 17:47:39 +0000

    Extensions: Favicon: Remove extra slashes
    Bug: 104102
    Change-Id: I941fb492767aa45e48bb35d1478efe64182ccf1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651189
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004348}

--- chrome/browser/extensions/api/favicon/favicon_apitest.cc
+++ chrome/browser/extensions/api/favicon/favicon_apitest.cc
@@ -1,6 +1,6 @@
 // Copyright 2022 The Chromium Authors. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
