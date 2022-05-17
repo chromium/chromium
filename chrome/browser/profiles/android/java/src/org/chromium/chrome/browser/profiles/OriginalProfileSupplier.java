6498465aa60ee6dd4eb424051f57f0cf51dc0679
Andrew Heard <andrewheard@chromium.org>
2022-05-17 20:49:55 +0000

    Refactor OriginalProfileSupplier as one-shot.
    Updates OriginalProfileSupplier to extend OneshotSupplierImpl instead of
    ObservableSupplierImpl because it already behaves like a one-shot
    supplier. This change signals to users of the class that they are not
    responsible for removing observers after the first (original) profile is
    obtained.
    
    Change-Id: I959ca160f64ac5b09d86b3eccd28e2d87c07c864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646507
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Andrew Heard <andrewheard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004440}

--- chrome/browser/profiles/android/java/src/org/chromium/chrome/browser/profiles/OriginalProfileSupplier.java
+++ chrome/browser/profiles/android/java/src/org/chromium/chrome/browser/profiles/OriginalProfileSupplier.java
@@ -4,21 +4,23 @@
 
 package org.chromium.chrome.browser.profiles;
 
-import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.base.lifetime.Destroyable;
+import org.chromium.base.supplier.OneshotSupplierImpl;
 
 /**
- * {@link org.chromium.base.supplier.ObservableSupplier} for {@link Profile} that notifies observers
- * when the original {@link Profile} object is first created. Creation happens once per process on
- * Android; all other profiles are incognito derivatives of the original profile.
+ * A {@link OneshotSupplierImpl} for the first, non-incognito, {@link Profile} created.
  *
- * <p>
- * If you call addObserver after the original profile has been created, a task
- * will be immediately posted to notify your observer, and the profile will be synchronously
- * returned. Like {@link org.chromium.base.supplier.ObservableSupplier}, this class must only be
- * accessed from a single thread.
+ * <p>This class notifies observers when the original {@link Profile} object is first created.
+ * Creation happens once per process on Android; all other profiles are incognito derivatives of the
+ * original profile.
+ *
+ * <p>If you call {@link #onAvailable(org.chromium.base.Callback)} after the original profile has
+ * been created, a task will be immediately posted to notify your observer, and the profile will be
+ * synchronously returned. Like {@link org.chromium.base.supplier.OneshotSupplier}, this class must
+ * only be accessed from a single thread.
  */
 public class OriginalProfileSupplier
-        extends ObservableSupplierImpl<Profile> implements ProfileManager.Observer {
+        extends OneshotSupplierImpl<Profile> implements Destroyable, ProfileManager.Observer {
     public OriginalProfileSupplier() {
         ProfileManager.addObserver(this);
     }
@@ -32,4 +34,9 @@ public class OriginalProfileSupplier
 
     @Override
     public void onProfileDestroyed(Profile profile) {}
+
+    @Override
+    public void destroy() {
+        if (!hasValue()) ProfileManager.removeObserver(this);
+    }
 }
