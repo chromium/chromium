ea3846bba2937424070466d98762e59b21114e4f
Etienne Pierre-Doray <etiennep@chromium.org>
2022-05-17 18:36:13 +0000

    Revert "[ad density] Record average-viewport-ad-density UKM"
    This reverts commit 5ebe5df65ff7840fe867ace32989f8c2a72af038.
    
    Reason for revert: AdsPageLoadMetricsObserverBrowserTest.AverageViewportAdDensity is flaky
    
    Original change's description:
    > [ad density] Record average-viewport-ad-density UKM
    >
    > When a subframe or the viewport is updated, (re-)calculate the viewport
    > ad density. At the end of the page load, report the time-averaged
    > viewport-ad-density.
    >
    > Another behavior change/fix: before this patch, the subframe rect could
    > go beyond the main frame rect, and the density calculated could in
    > theory go beyond 100. This patch updates the behavior to only calculate
    > the ad density within a bounding rectangle (for either page-ad-density,
    > or viewport-ad-density)
    >
    > UKM collection (privacy approved):
    > https://docs.google.com/document/d/1_VnxtTCXzT0UvzLIrr0WOS7nu9BFoEWvbcZMEq5YBCs/
    >
    > Bug: 1316362
    > Change-Id: I8672ca00e611fb476f8a1202ef1cbb3069d21549
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481259
    > Reviewed-by: John Delaney <johnidel@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1003954}
    
    Bug: 1316362, 1326576
    Change-Id: I5a690ed9635b80cb79fbb2f4d0b94330aee300e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647739
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Owners-Override: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004372}

--- components/page_load_metrics/browser/observers/ad_metrics/ads_page_load_metrics_observer.h
+++ components/page_load_metrics/browser/observers/ad_metrics/ads_page_load_metrics_observer.h
@@ -135,8 +135,6 @@ class AdsPageLoadMetricsObserver
   void OnMainFrameIntersectionRectChanged(
       content::RenderFrameHost* render_frame_host,
       const gfx::Rect& main_frame_intersection_rect) override;
-  void OnMainFrameViewportRectChanged(
-      const gfx::Rect& main_frame_viewport_rect) override;
   void OnSubFrameDeleted(int frame_tree_node_id) override;
 
   void SetHeavyAdThresholdNoiseProviderForTesting(
