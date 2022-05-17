2254445512d7389d5c4377b67b5005e3f01ce8a8
Pu Shi <pushi@google.com>
2022-05-17 16:03:44 +0000

    [Nearby] Fix use-after-free bug caused by use of base::Unretained(this)
    The base::Unretained(this) is being used in the ScheduledExecutor to
    bind callbacks to a SequencedTaskRunner, which causes use-after-free
    bug. By adding a separate WeakPtrFactory, it ensures the pointer to
    the object gets invalidated before invoking callback function in the
    task runner.
    
    (cherry picked from commit 85d5e104eaa7f75f4bd905629381a5898f2f1f7f)
    
    Fixed: 1316846
    TEST: manully tested with DCHECKs enabled and passed unit tests.
    Change-Id: Id11f404779336a22a718f4c5b6a3cb88f80bdfb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3642637
    Commit-Queue: Pu Shi <pushi@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1002936}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3651398
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4951@{#1259}
    Cr-Branched-From: 27de6227ca357da0d57ae2c7b18da170c4651438-refs/heads/main@{#982481}

--- chrome/services/sharing/nearby/platform/scheduled_executor.cc
+++ chrome/services/sharing/nearby/platform/scheduled_executor.cc
@@ -107,11 +107,11 @@ std::shared_ptr<api::Cancelable> ScheduledExecutor::Schedule(
   timer_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&ScheduledExecutor::StartTimerWithId,
-                     base::Unretained(this), id,
+                     timer_task_runner_weak_factory_.GetWeakPtr(), id,
                      base::Microseconds(absl::ToInt64Microseconds(duration))));
 
-  return std::make_shared<CancelableTask>(
-      base::BindOnce(&TryCancelTask, weak_factory_.GetWeakPtr(), id));
+  return std::make_shared<CancelableTask>(base::BindOnce(
+      &TryCancelTask, cancelable_task_weak_factory_.GetWeakPtr(), id));
 }
 
 void ScheduledExecutor::StartTimerWithId(const base::UnguessableToken& id,
@@ -125,9 +125,10 @@ void ScheduledExecutor::StartTimerWithId(const base::UnguessableToken& id,
     return;
 
   it->second->timer.SetTaskRunner(timer_task_runner_);
-  it->second->timer.Start(FROM_HERE, delay,
-                          base::BindOnce(&ScheduledExecutor::RunTaskWithId,
-                                         base::Unretained(this), id));
+  it->second->timer.Start(
+      FROM_HERE, delay,
+      base::BindOnce(&ScheduledExecutor::RunTaskWithId,
+                     timer_task_runner_weak_factory_.GetWeakPtr(), id));
 }
 
 void ScheduledExecutor::StopTimerWithIdAndDeleteTaskEntry(
@@ -177,7 +178,7 @@ bool ScheduledExecutor::OnTaskCancelled(const base::UnguessableToken& id) {
   timer_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&ScheduledExecutor::StopTimerWithIdAndDeleteTaskEntry,
-                     base::Unretained(this), id));
+                     timer_task_runner_weak_factory_.GetWeakPtr(), id));
   return true;
 }
 
