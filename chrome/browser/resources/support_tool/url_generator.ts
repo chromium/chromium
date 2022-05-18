783fd2e2f906e4021f9c8e7e94bd5db452f3bb3e
Irem Uguz <iremuguz@google.com>
2022-05-18 09:41:13 +0000

    support-tool: Update URL generator UI
    - Decrease the opacity of URL text field when URL is not generated to
    make it look uneditable.
    - Display an information text before copy button to let Google support
    know why they need to copy the generated URL.
    Screenshots after change: https://screenshot.googleplex.com/jvnde3ysUKD9oxZ.png
    
    Bug: b:232393225
    Change-Id: I3cab6367d9ad4660788f78b66fa0ffe6add71d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3652046
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1004682}

--- chrome/browser/resources/support_tool/url_generator.ts
+++ chrome/browser/resources/support_tool/url_generator.ts
@@ -10,6 +10,7 @@ import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 
 import {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
