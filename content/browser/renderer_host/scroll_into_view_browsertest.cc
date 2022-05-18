5f9791b21c36fc8038c9cfbde251f5d72e544e5f
Scott Little <sclittle@chromium.org>
2022-05-18 20:20:40 +0000

    Disable flaky ScrollIntoView test on Windows.
    The tests
    InsetScrollIntoViewBrowserTest.InsetsCauseScrollToFocusedEditable/* have
    been flaking on Windows builders lately, so this CL disables them.
    
    Bug: 1323876
    Change-Id: I82b5f56f85cd376c7add7257ffb2fe58c92ee44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654478
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Owners-Override: Scott Little <sclittle@chromium.org>
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1004937}

--- content/browser/renderer_host/scroll_into_view_browsertest.cc
+++ content/browser/renderer_host/scroll_into_view_browsertest.cc
@@ -674,8 +674,17 @@ class InsetScrollIntoViewBrowserTest
 
 // Ensure that insetting the viewport causes the visual viewport to be resized
 // and focused editable scrolled into view. (https://crbug.com/927483)
+//
+// TODO(https://crbug.com/1323876): Failing flakily on Windows.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_InsetsCauseScrollToFocusedEditable \
+  DISABLED_InsetsCauseScrollToFocusedEditable
+#else
+#define MAYBE_InsetsCauseScrollToFocusedEditable \
+  InsetsCauseScrollToFocusedEditable
+#endif
 IN_PROC_BROWSER_TEST_P(InsetScrollIntoViewBrowserTest,
-                       InsetsCauseScrollToFocusedEditable) {
+                       MAYBE_InsetsCauseScrollToFocusedEditable) {
   ASSERT_TRUE(SetupTest("siteA(siteB(siteC))"));
 
   int visual_viewport_height_before = GetVisualViewport().height;
