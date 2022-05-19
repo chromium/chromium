a5c799b281d156806b1ddddf4d22c9f408e258dc
Yifan Luo <lyf@chromium.org>
2022-05-19 12:00:31 +0000

    [Anonymous Iframe] Fix anonymous window test
    Change-Id: I500401411d17a244d6076b86fb90952ebb79250f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3643918
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005202}

--- third_party/blink/web_tests/external/wpt/html/anonymous-iframe/anonymous-window.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/html/anonymous-iframe/anonymous-window.tentative.https.window.js
@@ -2,47 +2,55 @@
 // META: script=/common/dispatcher/dispatcher.js
 // META: script=/html/cross-origin-embedder-policy/credentialless/resources/common.js
 
-const ORIGIN = get_host_info();
+const {ORIGIN} = get_host_info();
 
 promise_test_parallel(async t => {
   const iframe = document.createElement("iframe");
+  iframe.src = ORIGIN + "/common/blank.html?pipe=status(204)";
+  iframe.anonymous = false;
   document.body.appendChild(iframe);
-  iframe.contentWindow.modified = true;
   iframe.anonymous = true;
-  iframe.src = ORIGIN + "/blank.html";
-  // Wait for navigation finished.
+  iframe.contentWindow.modified = true;
+  iframe.src = ORIGIN + "/common/blank.html";
+  // Wait for navigation to complete.
   await t.step_wait(() =>
     iframe.contentWindow.location.href === iframe.src,
     "Wait for the navigation to complete");
   assert_true(iframe.anonymous);
+  assert_true(iframe.contentWindow.isAnonymouslyFramed);
   assert_equals(undefined, iframe.contentWindow.modified);
 }, "Anonymous (false => true) => window not reused.");
 
 promise_test_parallel(async t => {
   const iframe = document.createElement("iframe");
+  iframe.src = ORIGIN + "/common/blank.html?pipe=status(204)";
   iframe.anonymous = true;
   document.body.appendChild(iframe);
-  iframe.contentWindow.modified = true;
   iframe.anonymous = false;
-  iframe.src = ORIGIN + "/blank.html";
-  // Wait for navigation finished.
+  iframe.contentWindow.modified = true;
+  iframe.src = ORIGIN + "/common/blank.html";
+  // Wait for navigation to complete.
   await t.step_wait(() =>
     iframe.contentWindow.location.href === iframe.src,
     "Wait for the navigation to complete");
   assert_false(iframe.anonymous);
+  assert_false(iframe.contentWindow.isAnonymouslyFramed);
   assert_equals(undefined, iframe.contentWindow.modified);
 }, "Anonymous (true => false) => window not reused.");
 
 promise_test_parallel(async t => {
   const iframe = document.createElement("iframe");
   iframe.anonymous = true;
+  iframe.src = ORIGIN + "/common/blank.html?pipe=status(204)";
   document.body.appendChild(iframe);
+  iframe.anonymous = true;
   iframe.contentWindow.modified = true;
-  iframe.src = ORIGIN + "/blank.html";
-  // Wait for navigation finished.
+  iframe.src = ORIGIN + "/common/blank.html";
+  // Wait for navigation to complete.
   await t.step_wait(() =>
     iframe.contentWindow.location.href === iframe.src,
     "Wait for the navigation to complete");
   assert_true(iframe.anonymous);
+  assert_true(iframe.contentWindow.isAnonymouslyFramed);
   assert_true(iframe.contentWindow.modified);
 }, "Anonymous (true => true) => window reused.");
