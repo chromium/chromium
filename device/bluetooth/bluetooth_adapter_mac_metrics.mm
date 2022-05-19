f6c3a5b15b8dacfb51a7e7400cd5e5d82daf902a
Avi Drissman <avi@chromium.org>
2022-05-19 20:02:18 +0000

    Remove OS X 10.11 and macOS 10.12 code from device/bluetooth/
    Bug: 1322548
    Change-Id: I8b60800b42556443465aae900ef2c39417a027ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654558
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005363}

--- device/bluetooth/bluetooth_adapter_mac_metrics.mm
+++ device/bluetooth/bluetooth_adapter_mac_metrics.mm
@@ -42,15 +42,7 @@ MacOSBluetoothOperationsResult GetMacOSOperationResultFromNSError(
       case CBErrorAlreadyAdvertising:
         return MacOSBluetoothOperationsResult::CBERROR_ALREADY_ADVERTISING;
       case CBErrorConnectionFailed:
-        if (base::mac::IsAtLeastOS10_13()) {
-          return MacOSBluetoothOperationsResult::CBERROR_CONNECTION_FAILED;
-        } else {
-          // For macOS 10.12 or before, the value CBErrorMaxConnection has the
-          // same value than CBErrorConnectionFailed and the same description
-          // than CBErrorConnectionLimitReached.
-          return MacOSBluetoothOperationsResult::
-              CBERROR_CONNECTION_LIMIT_REACHED;
-        }
+        return MacOSBluetoothOperationsResult::CBERROR_CONNECTION_FAILED;
       case CBErrorConnectionLimitReached:
         return MacOSBluetoothOperationsResult::CBERROR_CONNECTION_LIMIT_REACHED;
       case CBErrorUnknownDevice:
--- device/bluetooth/bluetooth_low_energy_device_mac.mm
+++ device/bluetooth/bluetooth_low_energy_device_mac.mm
@@ -22,18 +22,6 @@
 #include "device/bluetooth/bluetooth_remote_gatt_service_mac.h"
 #include "device/bluetooth/public/cpp/bluetooth_address.h"
 
-// Remove when Chrome no longer supports 10.12.
-#if defined(MAC_OS_X_VERSION_10_13)
-
-// In the 10.13 SDK, CBPeripheral became a subclass of CBPeer, which defines
-// -[CBPeer identifier] as partially available. Pretend it still exists on
-// CBPeripheral. At runtime the implementation on CBPeer will be invoked.
-@interface CBPeripheral (HighSierraSDK)
-@property(readonly, nonatomic) NSUUID* identifier;
-@end
-
-#endif  // MAC_OS_X_VERSION_10_13
-
 namespace device {
 
 BluetoothLowEnergyDeviceMac::BluetoothLowEnergyDeviceMac(
