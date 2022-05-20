f0c03af4a59e5225e8446600dd7fae04414e3ee2
Miriam Zimmerman <mutexlox@google.com>
2022-05-20 21:35:45 +0000

    metrics: Remove MoreFrequentUmaUploads logic.
    This experiment caused unacceptably high logs size increases, and it was
    disabled. So, remove associated code.
    
    This is logically a rollback of https://crrev.com/c/3046879 and
    https://crrev.com/c/3188666, but was manually generated.
    
    BUG=1211493
    TEST=CQ
    
    Change-Id: I1a7de695c63bee88d77e16ec0a1071cacea2c0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655697
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005967}

--- components/metrics/net/cellular_logic_helper.cc
+++ components/metrics/net/cellular_logic_helper.cc
@@ -4,7 +4,6 @@
 
 #include "components/metrics/net/cellular_logic_helper.h"
 
-#include "base/feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -22,14 +21,6 @@ const int kStandardUploadIntervalCellularSeconds = 15 * 60;  // Fifteen minutes.
 const int kStandardUploadIntervalSeconds = 30 * 60;  // Thirty minutes.
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-// A feature to control whether we upload UMA logs more frequently.
-const base::Feature kMoreFrequentUmaUploads{"MoreFrequentUmaUploads",
-                                            base::FEATURE_DISABLED_BY_DEFAULT};
-// The interval between these more-frequent uploads.
-constexpr base::TimeDelta kMoreFrequentUploadInterval = base::Minutes(5);
-#endif  // IS_CHROMEOS_ASH
-
 #if BUILDFLAG(IS_ANDROID)
 const bool kDefaultCellularLogicEnabled = true;
 #else
@@ -42,10 +33,6 @@ base::TimeDelta GetUploadInterval(bool use_cellular_upload_interval) {
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
   if (use_cellular_upload_interval)
     return base::Seconds(kStandardUploadIntervalCellularSeconds);
-#elif BUILDFLAG(IS_CHROMEOS_ASH)
-  if (base::FeatureList::IsEnabled(kMoreFrequentUmaUploads)) {
-    return kMoreFrequentUploadInterval;
-  }
 #endif
   return base::Seconds(kStandardUploadIntervalSeconds);
 }
