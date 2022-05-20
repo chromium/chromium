f5aa7fa08465b6f9222053782fff5a2e3d55931c
Avi Drissman <avi@chromium.org>
2022-05-20 16:56:59 +0000

    Remove 10.11 and 10.12 OS functions
    OS X 10.11 and macOS 10.12 are not supported at runtime. Remove
    functions that query for those OS versions.
    
    Bug: 1322548
    Change-Id: I9a6ab60f93594a4470de5cb1f64cbf2462c6a51b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654793
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1005850}

--- base/mac/mac_util_unittest.mm
+++ base/mac/mac_util_unittest.mm
@@ -135,36 +135,10 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
   EXPECT_FALSE(IsAtLeastOS##V());
 
   if (major == 10) {
-    if (minor == 11) {
-      EXPECT_TRUE(IsOS10_11());
-      EXPECT_TRUE(IsAtMostOS10_11());
+    if (minor == 13) {
+      EXPECT_TRUE(IsOS10_13());
+      EXPECT_TRUE(IsAtMostOS10_13());
 
-      TEST_FOR_FUTURE_10_OS(12);
-      TEST_FOR_FUTURE_10_OS(13);
-      TEST_FOR_FUTURE_10_OS(14);
-      TEST_FOR_FUTURE_10_OS(15);
-      TEST_FOR_FUTURE_OS(11);
-      TEST_FOR_FUTURE_OS(12);
-
-      EXPECT_FALSE(IsOSLaterThan12_DontCallThis());
-    } else if (minor == 12) {
-      EXPECT_FALSE(IsOS10_11());
-      EXPECT_FALSE(IsAtMostOS10_11());
-
-      TEST_FOR_SAME_10_OS(12);
-      TEST_FOR_FUTURE_10_OS(13);
-      TEST_FOR_FUTURE_10_OS(14);
-      TEST_FOR_FUTURE_10_OS(15);
-      TEST_FOR_FUTURE_OS(11);
-      TEST_FOR_FUTURE_OS(12);
-
-      EXPECT_FALSE(IsOSLaterThan12_DontCallThis());
-    } else if (minor == 13) {
-      EXPECT_FALSE(IsOS10_11());
-      EXPECT_FALSE(IsAtMostOS10_11());
-
-      TEST_FOR_PAST_10_OS(12);
-      TEST_FOR_SAME_10_OS(13);
       TEST_FOR_FUTURE_10_OS(14);
       TEST_FOR_FUTURE_10_OS(15);
       TEST_FOR_FUTURE_OS(11);
@@ -172,11 +146,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan12_DontCallThis());
     } else if (minor == 14) {
-      EXPECT_FALSE(IsOS10_11());
-      EXPECT_FALSE(IsAtMostOS10_11());
+      EXPECT_FALSE(IsOS10_13());
+      EXPECT_FALSE(IsAtMostOS10_13());
 
-      TEST_FOR_PAST_10_OS(12);
-      TEST_FOR_PAST_10_OS(13);
       TEST_FOR_SAME_10_OS(14);
       TEST_FOR_FUTURE_10_OS(15);
       TEST_FOR_FUTURE_OS(11);
@@ -184,11 +156,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan12_DontCallThis());
     } else if (minor == 15) {
-      EXPECT_FALSE(IsOS10_11());
-      EXPECT_FALSE(IsAtMostOS10_11());
+      EXPECT_FALSE(IsOS10_13());
+      EXPECT_FALSE(IsAtMostOS10_13());
 
-      TEST_FOR_PAST_10_OS(12);
-      TEST_FOR_PAST_10_OS(13);
       TEST_FOR_PAST_10_OS(14);
       TEST_FOR_SAME_10_OS(15);
       TEST_FOR_FUTURE_OS(11);
@@ -200,11 +170,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
       FAIL() << "Unexpected 10.x macOS.";
     }
   } else if (major == 11) {
-    EXPECT_FALSE(IsOS10_11());
-    EXPECT_FALSE(IsAtMostOS10_11());
+    EXPECT_FALSE(IsOS10_13());
+    EXPECT_FALSE(IsAtMostOS10_13());
 
-    TEST_FOR_PAST_10_OS(12);
-    TEST_FOR_PAST_10_OS(13);
     TEST_FOR_PAST_10_OS(14);
     TEST_FOR_PAST_10_OS(15);
     TEST_FOR_SAME_OS(11);
@@ -212,11 +180,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
     EXPECT_FALSE(IsOSLaterThan12_DontCallThis());
   } else if (major == 12) {
-    EXPECT_FALSE(IsOS10_11());
-    EXPECT_FALSE(IsAtMostOS10_11());
+    EXPECT_FALSE(IsOS10_13());
+    EXPECT_FALSE(IsAtMostOS10_13());
 
-    TEST_FOR_PAST_10_OS(12);
-    TEST_FOR_PAST_10_OS(13);
     TEST_FOR_PAST_10_OS(14);
     TEST_FOR_PAST_10_OS(15);
     TEST_FOR_PAST_OS(11);
