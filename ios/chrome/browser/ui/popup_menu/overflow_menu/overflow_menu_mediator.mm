b65b2f7bdb5fa97df333378e8de8e323a41b8667
Tina Wang <tinazwang@google.com>
2022-05-20 22:49:27 +0000

    [WebChannels] Correct icons in overflow menu and feed management menu
    Currently the +" button after "follow XYZ.com" is very dark. It's
    because it uses the wrong source from popup_menu/.
    Current: https://screenshot.googleplex.com/86zcZffRs6HunTs
    After Fix: https://screenshot.googleplex.com/5ZaxTEtpfcKgM6D
    
    The "open in new page" icon on Interests, Hidden and Activity are too
    small. The correct size of SF symbols is 22pt.
    Current: https://screenshot.googleplex.com/B7TrdcwvVJUXjRg
    After Fix: https://screenshot.googleplex.com/5ggHfRhZzzS2VpC
    
    (cherry picked from commit 1b6c66bf2457dcf4d971e4d92d2e52513d268901)
    
    Bug: 1327066
    Change-Id: I35ce195a423f4dfc0c35fbd3c4f7c8ef5bc2f195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3654773
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1005328}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656240
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5060@{#148}
    Cr-Branched-From: b83393d0f4038aeaf67f970a024d8101df7348d1-refs/heads/main@{#1002911}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -1169,8 +1169,9 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
                                        base::SysNSStringToUTF16(domainName))
              : l10n_util::GetNSStringF(IDS_IOS_TOOLS_MENU_FOLLOW,
                                        base::SysNSStringToUTF16(domainName));
-  [self.followAction setStoredImageName:status ? @"popup_menu_unfollow"
-                                               : @"popup_menu_follow"];
+  [self.followAction setStoredImageName:status
+                                            ? @"overflow_menu_action_unfollow"
+                                            : @"overflow_menu_action_follow"];
   __weak __typeof(self) weakSelf = self;
   self.followAction.handler = ^{
     RecordAction(status ? UserMetricsAction("MobileMenuUnfollow")
