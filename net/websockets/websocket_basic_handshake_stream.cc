80e9dc8fb942cf3d61ad783452e182a5bd3ee343
Dan McArdle <dmcardle@chromium.org>
2022-05-23 01:43:12 +0000

    Eliminate base::LowerCaseEqualsASCII in /net/websockets
    This CL is a behavior-preserving refactor. It replaces calls to
    base::LowerCaseEqualsASCII with base::EqualsCaseInsensitiveASCII.
    
    We are deprecating base::LowerCaseEqualsASCII because its interface is a
    mild footgun, in that it requires the second parameter to be lowercase.
    
    See the design doc for more details.
    https://docs.google.com/document/d/18zWV0zN4oAqMZpond1Ut0QviHUeHCSwCo08mqThwHkQ/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=yoichio@chromium.org
    
    Bug: 1326529
    Change-Id: I85661ac151ca1f5a4b5c3885534a8a5623503656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3658128
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006271}

--- net/websockets/websocket_basic_handshake_stream.cc
+++ net/websockets/websocket_basic_handshake_stream.cc
@@ -117,7 +117,8 @@ bool ValidateUpgrade(const HttpResponseHeaders* headers,
     return false;
   }
 
-  if (!base::LowerCaseEqualsASCII(value, websockets::kWebSocketLowercase)) {
+  if (!base::EqualsCaseInsensitiveASCII(value,
+                                        websockets::kWebSocketLowercase)) {
     *failure_message =
         "'Upgrade' header value is not 'WebSocket': " + value;
     return false;
