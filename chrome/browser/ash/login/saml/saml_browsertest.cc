74e3301e5149316aca8a844f2595e3d423cfb672
Renato Silva <rrsilva@google.com>
2022-05-23 10:39:31 +0000

    OOBE - Re-enable SAML tests on debug builds.
    This test was disabled because of short timeouts in dbg builds.
    The timeout has been increased and less failures are occurring.
    
    Link to CL: https://crrev.com/c/3647713
    
    Bug: 1315447,  1327650
    Change-Id: I1301f93c5a7cd82f44f540fb15a4b11c586c72d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650928
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006378}

--- chrome/browser/ash/login/saml/saml_browsertest.cc
+++ chrome/browser/ash/login/saml/saml_browsertest.cc
@@ -735,14 +735,7 @@ IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, ScrapedMultiple) {
                                      1);
 }
 
-// Tests the no password scraped flow.
-// TODO(crbug.com/1315447): Disabled for debug builds.
-#if !defined(NDEBUG)
-#define MAYBE_ScrapedNone DISABLED_ScrapedNone
-#else
-#define MAYBE_ScrapedNone ScrapedNone
-#endif
-IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, MAYBE_ScrapedNone) {
+IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, ScrapedNone) {
   base::HistogramTester histogram_tester;
   fake_saml_idp()->SetLoginHTMLTemplate("saml_login_no_passwords.html");
 
@@ -833,15 +826,7 @@ IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures,
   WaitForSigninScreen();
 }
 
-// Tests the password confirm flow when more than one password is scraped: show
-// error on the first failure and fatal error on the second failure.
-// TODO(crbug.com/1315447): Disabled for debug builds.
-#if !defined(NDEBUG)
-#define MAYBE_PasswordConfirmFlow DISABLED_PasswordConfirmFlow
-#else
-#define MAYBE_PasswordConfirmFlow PasswordConfirmFlow
-#endif
-IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, MAYBE_PasswordConfirmFlow) {
+IN_PROC_BROWSER_TEST_P(SamlTestWithFeatures, PasswordConfirmFlow) {
   base::HistogramTester histogram_tester;
   fake_saml_idp()->SetLoginHTMLTemplate("saml_login_two_passwords.html");
   StartSamlAndWaitForIdpPageLoad(
