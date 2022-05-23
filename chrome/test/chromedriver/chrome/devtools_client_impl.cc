f1795ca9f858cf76d98d01a4f4314b5ecb778be1
Brandon Walderman <brwalder@microsoft.com>
2022-05-23 17:41:19 +0000

    Filter command responses from detached CDP sessions
    Sometimes, ChromeDriver can receive a command response for a command
    sent to a session that has been detached. The repro steps are something
    like this:
    
    1. DevToolsClientImpl for the root session 'A' calls SendCommand and
       polls for a response.
    2. An event is received for a nested DevToolsClientImpl session 'B'.
    3. DevToolsClientImpl 'B' sends a nested command as part of the event
       handler and polls for a response.
    4. Session 'B' detaches while DevToolsClientImpl 'B' is waiting for a
       response, so this DevToolsClientImpl stops polling and detaches.
    5. Later, the browser processes the command from session 'B'. The
       CDP session is already gone, so the browser responds with an error
       message containing no "sessionId" property.
    6. DevToolsClientImpl 'A' is still polling since step 1 and receives
       the error message from step 5.
    7. Since the error message has no sessionId, it is handled by the
       root DevToolsClientImpl 'A'.
    8. The message has an "id" that is not in DevToolsClientImpl 'A's map
       so DevToolsClientImpl::ProcessCommandResponse returns an error.
       The polling stops and the SendCommand call for step 1 fails.
    
    The fix is to detect this condition in ProcessCommandReponse. If the
    CDP reponse is a "session not found" error and the "id" is
    unrecognized, then simply ignore this message and continue pumping
    other messages.
    
    The fix has been verified with the repro in the attached bug.
    
    Bug: chromedriver:4121
    Change-Id: I6238de74dacb7fd2bc5e115ca5b6625232ce9ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3658212
    Commit-Queue: Brandon Walderman <brwalder@microsoft.com>
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1006507}

--- chrome/test/chromedriver/chrome/devtools_client_impl.cc
+++ chrome/test/chromedriver/chrome/devtools_client_impl.cc
@@ -26,7 +26,6 @@
 
 namespace {
 
-const int kCdpMethodNotFoundCode = -32601;
 const char kInspectorDefaultContextError[] =
     "Cannot find default execution context";
 const char kInspectorContextError[] = "Cannot find context with specified id";
@@ -39,6 +38,9 @@ const char kInspectorPushPermissionError[] =
     "Push Permission without userVisibleOnly:true isn't supported";
 const char kInspectorNoSuchFrameError[] =
     "Frame with the given id was not found.";
+
+static constexpr int kSessionNotFoundInspectorCode = -32001;
+static constexpr int kCdpMethodNotFoundCode = -32601;
 static constexpr int kInvalidParamsInspectorCode = -32602;
 
 class ScopedIncrementer {
@@ -564,8 +566,23 @@ Status DevToolsClientImpl::ProcessCommandResponse(
             << " (id=" << response.id << ") " << id_ << " " << result;
   }
 
-  if (iter == response_info_map_.end())
+  if (iter == response_info_map_.end()) {
+    // A CDP session may become detached while a command sent to that session
+    // is still pending. When the browser eventually tries to process this
+    // command, it sends a response with an error and no session ID. Since
+    // there is no session ID, this message will be routed here to the root
+    // DevToolsClientImpl. If we receive such a response, just ignore it
+    // since the session it belongs to is already detached.
+    if (parent_ == nullptr) {
+      if (!response.result) {
+        const Status status = internal::ParseInspectorError(response.error);
+        if (status.code() == StatusCode::kNoSuchFrame) {
+          return Status(kOk);
+        }
+      }
+    }
     return Status(kUnknownError, "unexpected command response");
+  }
 
   scoped_refptr<ResponseInfo> response_info = response_info_map_[response.id];
   response_info_map_.erase(response.id);
@@ -698,6 +715,9 @@ Status ParseInspectorError(const std::string& error_json) {
     if (maybe_code.value() == kCdpMethodNotFoundCode) {
       return Status(kUnknownCommand,
                     maybe_message ? *maybe_message : "UnknownCommand");
+    } else if (maybe_code.value() == kSessionNotFoundInspectorCode) {
+      return Status(kNoSuchFrame,
+                    maybe_message ? *maybe_message : "inspector detached");
     }
   }
 
