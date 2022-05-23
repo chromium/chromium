1c76405c5943e5488b0b62175cabbad84868fcf3
Josiah K <josiahk@google.com>
2022-05-23 19:19:40 +0000

    [A11y Settings Revamp] Enable surfacing A11y OS Settings above Advanced section
    AX-Relnotes: Make the Accessibility Settings sub-section visible and more discoverable in Chrome OS settings by moving it above the Advanced section.
    Fixed: 1327529
    Change-Id: Ib2a72443a2af683c74bdd13149ea5e852061c332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656807
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006547}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -153,7 +153,7 @@ bool IsAccessibilityOSSettingsReorganizationEnabled() {
       ::features::kAccessibilityOSSettingsReorganization);
 }
 const base::Feature kAccessibilityOSSettingsVisibility{
-    "AccessibilityOSSettingsVisibility", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AccessibilityOSSettingsVisibility", base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool IsAccessibilityOSSettingsVisibilityEnabled() {
   return base::FeatureList::IsEnabled(
