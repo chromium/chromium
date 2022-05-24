65603ccad05a859c03a60a37eaff098ad7b251be
Wes Okuhara <wesokuhara@google.com>
2022-05-24 19:09:58 +0000

    CrOS Settings: Convert os-toolbar to class-based syntax
    Converts Polymer elements to the class-based syntax to prepare for the
    upcoming TypeScript migration.
    
    Bug: chromium:1206112, chromium:1315757
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*SettingsUi2*'
    Test: out/Default/browser_tests --gtest_filter='*OSSettings*SearchBox*'
    Test: Manual UI testing
    Change-Id: I65719e4900fed6fd266578490ca8885088ed42a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3656463
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1006991}

--- chrome/browser/resources/settings/chromeos/os_toolbar/os_toolbar.js
+++ chrome/browser/resources/settings/chromeos/os_toolbar/os_toolbar.js
@@ -2,68 +2,78 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
-import '//resources/cr_elements/cr_icons_css.m.js';
-import '//resources/cr_elements/hidden_style_css.m.js';
-import '//resources/cr_elements/icons.m.js';
-import '//resources/cr_elements/shared_vars_css.m.js';
-import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import 'chrome://resources/cr_elements/cr_icons_css.m.js';
+import 'chrome://resources/cr_elements/hidden_style_css.m.js';
+import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '../os_settings_search_box/os_settings_search_box.js';
-import '//resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 
-import {assert, assertNotReached} from '//resources/js/assert.m.js';
-import {loadTimeData} from '//resources/js/load_time_data.m.js';
-import {afterNextRender, flush, html, Polymer, TemplateInstanceBase, Templatizer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-Polymer({
-  _template: html`{__html_template__}`,
-  // Not "toolbar" because element names must contain a hyphen.
-  is: 'os-toolbar',
+/** @polymer */
+class OsToolbarElement extends PolymerElement {
+  static get is() {
+    return 'os-toolbar';
+  }
 
-  properties: {
-    // Value is proxied through to cr-toolbar-search-field. When true,
-    // the search field will show a processing spinner.
-    spinnerActive: Boolean,
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
-    // Controls whether the menu button is shown at the start of the menu.
-    showMenu: {type: Boolean, value: false},
+  static get properties() {
+    return {
+      // Value is proxied through to cr-toolbar-search-field. When true,
+      // the search field will show a processing spinner.
+      spinnerActive: Boolean,
 
-    // Controls whether the search field is shown.
-    showSearch: {type: Boolean, value: true},
+      // Controls whether the menu button is shown at the start of the menu.
+      showMenu: {type: Boolean, value: false},
 
-    // True when the toolbar is displaying in narrow mode.
-    narrow: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
+      // Controls whether the search field is shown.
+      showSearch: {type: Boolean, value: true},
 
-    /**
-     * True when the toolbar is displaying in an extremely narrow mode that the
-     * viewport may cutoff an OsSettingsSearchBox with a specific px width.
-     * @private
-     */
-    isSearchBoxCutoff_: {
-      type: Boolean,
-      reflectToAttribute: true,
-    },
+      // True when the toolbar is displaying in narrow mode.
+      narrow: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
 
-    /** @private */
-    showingSearch_: {
-      type: Boolean,
-      reflectToAttribute: true,
-    },
-  },
+      /**
+       * True when the toolbar is displaying in an extremely narrow mode that
+       * the viewport may cutoff an OsSettingsSearchBox with a specific px
+       * width.
+       * @private
+       */
+      isSearchBoxCutoff_: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
+
+      /** @private */
+      showingSearch_: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
+    };
+  }
 
   /** @return {?CrToolbarSearchFieldElement} */
   getSearchField() {
     return /** @type {?CrToolbarSearchFieldElement} */ (
         this.shadowRoot.querySelector('os-settings-search-box')
-            .$$('cr-toolbar-search-field'));
-  },
+            .shadowRoot.querySelector('cr-toolbar-search-field'));
+  }
 
   /** @private */
   onMenuTap_() {
-    this.fire('os-toolbar-menu-tap');
-  },
-});
+    const event =
+        new CustomEvent('os-toolbar-menu-tap', {bubbles: true, composed: true});
+    this.dispatchEvent(event);
+  }
+}
+
+customElements.define(OsToolbarElement.is, OsToolbarElement);
