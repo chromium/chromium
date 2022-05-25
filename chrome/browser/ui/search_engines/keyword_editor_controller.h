ab06185e49911aaf1970b5686b0934bf927142cb
Angela Yoeurng <yoangela@chromium.org>
2022-05-25 18:25:25 +0000

    [omnibox][search_engines] Disable delete for starter pack engines.
    This CL disables the ability to delete any starter pack search engine
    such as @bookmark and @history from the chrome://settings/searchEngines
    page.
    
    Bug: 1287313
    Change-Id: I2f2c71637442943f78175585aedb6000bdd894bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665737
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007487}

--- chrome/browser/ui/search_engines/keyword_editor_controller.h
+++ chrome/browser/ui/search_engines/keyword_editor_controller.h
@@ -46,7 +46,7 @@ class KeywordEditorController {
 
   // Return true if the given `url` can be removed. A `url` can be removed if it
   // is a normal entry (non-extension) and is not the current default search
-  // engine.
+  // engine or a starter pack engine.
   bool CanRemove(const TemplateURL* url) const;
 
   // Return true if the given `url` can be activated. A `url` can be activated
