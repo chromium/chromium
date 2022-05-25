a962a558685e8887f871706ae7136ab7d6643dff
Luciano Pacheco <lucmult@chromium.org>
2022-05-25 04:48:06 +0000

    Files app: Add initial build config for TS
    Add a ts_library() in //ui/file_manager/ for the entire Files app,
    including background, foreground and the shared worker. This TS
    configuration outputs all required JS in a new generated folder "tsc".
    The "preprocessed" folder is still used as the input for ts_library().
    
    The 3 builds for Files app (background, foreground and the worker) are
    all generated from the new "tsc" folder.
    
    Refactors:
    1. Changed Audio Player and Image Loader to use their own
    preprocess_if_expr because they're always built from the "preprocessed"
    folder instead of the "tsc".
    2. Move the list of static and generated JS files to its own files.gni
    file to make the BUILD.gn more readable and to be able to reuse the list
    between the preprocess_if_expr() and the new ts_library().
    
    The follow up CL shows how to use the TS in the Files app:
    https://chromium-review.googlesource.com/c/chromium/src/+/3650690/3
    
    Bug: b:228112761
    Change-Id: If1141ec85716356c0a80c9991125fdff8989cb89
    Cq-Include-Trybots: luci.chrome.try:chromeos-eve-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3577936
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007232}

---
+++ ui/file_manager/file_manager/hello.ts
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Placeholder function to test the build of TS in Files app.
+ *
+ * @param name any name or text to be greeted.
+ * @returns a greeting string
+ */
+export function hello(name: string): string {
+  return `Hello ${name}`;
+}
