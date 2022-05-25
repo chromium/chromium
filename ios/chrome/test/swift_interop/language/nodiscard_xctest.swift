47d9c8235d58c54eb8601fcdcc6457828a486685
Mike Pinkerton <pinkerton@google.com>
2022-05-25 14:56:40 +0000

    Reland [Swift][Cxx interop] Add [[nodiscard]] tests.
    Add tests for C++17's [[nodiscard]] attribute, with accompanying
    Github bug.
    
    Bug: none
    Change-Id: Ic5048d2cb611bab95a8c8ef928122558e36c7177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3655937
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1005911}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665202
    Cr-Commit-Position: refs/heads/main@{#1007379}

---
+++ ios/chrome/test/swift_interop/language/nodiscard_xctest.swift
@@ -0,0 +1,42 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Language
+import XCTest
+
+class NoDiscardTest: XCTestCase {
+
+  func testNoDiscardFunction() throws {
+    let value = NoDiscardAdd(5, 10)
+    XCTAssertEqual(value, 15)
+
+    // NoDiscardAdd() is nodiscard, corectly gives a warning.
+    // NoDiscardAdd(10,10)
+  }
+
+  func testNoDiscardMethod() throws {
+    var multiplier = NoDiscardMultiply()
+    let value = multiplier.Multiply(10, 10)
+    XCTAssertEqual(value, 100)
+
+    // NoDiscardMultiply::Multiply is nodiscard, correctly gives a warning.
+    // multiplier.Multiply(50, 50)
+
+    let result = multiplier.Divide(100, 10)
+    XCTAssertEqual(result, 10)
+
+    // NoDiscardMultiply::Divide has no annotation, correctly doesn't give a warning
+    multiplier.Divide(100, 10)
+  }
+
+  func testNoDiscardStruct() throws {
+    let error: NoDiscardError = NoDiscardReturnError(10, 10)
+    XCTAssertEqual(error.value_, 20)
+
+    // NoDiscardError is declared nodiscard, so ignoring the return value of
+    // NoDiscardReturnError() should be a warning, but isn't.
+    // Filed as: https://github.com/apple/swift/issues/59002
+    NoDiscardReturnError(50, 50)
+  }
+}
