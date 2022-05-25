f8be5f66bd6a9d0439e6255c50d226f56d5b74dd
Sophie Chang <sophiechang@chromium.org>
2022-05-25 20:28:01 +0000

    Write model downloads to somewhere in the profile dir to avoid model downloads dir growing out of bounds
    This ensures that the models downloaded for the profile are deleted alongside the profile.
    
    This also deletes the old model paths (both the metadata and the downloads) so that we can get users out of a bad state
    
    (cherry picked from commit be967bbfb15345232fa8aacf5dc1f5e448a457bc)
    
    Bug: 1327975
    Change-Id: Ib707a79a0b73a5d5322005505802afaa9d56b994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3665649
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1007214}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666278
    Cr-Commit-Position: refs/branch-heads/5005@{#1021}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
@@ -60,8 +60,15 @@ namespace {
 
 const char kOldOptimizationGuideHintStore[] = "previews_hint_cache_store";
 
+const char kOldOptimizationGuidePredictionModelAndFeaturesStore[] =
+    "optimization_guide_model_and_features_store";
+
 // Deletes old store paths that were written in incorrect locations.
 void DeleteOldStorePaths(const base::FilePath& profile_path) {
+  // Added 04/2021.
+
+  // Delete profile_path.<other path> as that was not actually in the profile
+  // dir.
   base::ThreadPool::PostTask(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(
@@ -71,9 +78,29 @@ void DeleteOldStorePaths(const base::FilePath& profile_path) {
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(
           base::GetDeletePathRecursivelyCallback(),
-          profile_path.AddExtension(
-              optimization_guide::
-                  kOptimizationGuidePredictionModelAndFeaturesStore)));
+          profile_path.AddExtensionASCII(
+              kOldOptimizationGuidePredictionModelAndFeaturesStore)));
+
+  // Added 05/2022.
+
+  // Delete profile_path/optimization_guide_model_and_features_store/...
+  // as it contains pointers to the bad model download location.
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+      base::BindOnce(
+          base::GetDeletePathRecursivelyCallback(),
+          profile_path.AppendASCII(
+              kOldOptimizationGuidePredictionModelAndFeaturesStore)));
+
+  // Delete the Chrome-wide model download location that wasn't previously
+  // getting cleaned up when profiles were getting deleted.
+  base::FilePath models_dir;
+  base::PathService::Get(chrome::DIR_OPTIMIZATION_GUIDE_PREDICTION_MODELS,
+                         &models_dir);
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+      base::BindOnce(
+          base::GetDeletePathRecursivelyCallback(), models_dir));
 }
 
 // Returns the profile to use for when setting up the keyed service when the
@@ -231,15 +258,14 @@ void OptimizationGuideKeyedService::Initialize() {
             : nullptr;
     hint_store = hint_store_ ? hint_store_->AsWeakPtr() : nullptr;
 
-    prediction_model_and_features_store_ =
-        std::make_unique<optimization_guide::OptimizationGuideStore>(
-            proto_db_provider,
-            profile_path.Append(
-                optimization_guide::
-                    kOptimizationGuidePredictionModelAndFeaturesStore),
-            base::ThreadPool::CreateSequencedTaskRunner(
-                {base::MayBlock(), base::TaskPriority::BEST_EFFORT}),
-            profile->GetPrefs());
+    prediction_model_and_features_store_ = std::make_unique<
+        optimization_guide::OptimizationGuideStore>(
+        proto_db_provider,
+        profile_path.Append(
+            optimization_guide::kOptimizationGuidePredictionModelMetadataStore),
+        base::ThreadPool::CreateSequencedTaskRunner(
+            {base::MayBlock(), base::TaskPriority::BEST_EFFORT}),
+        profile->GetPrefs());
     prediction_model_and_features_store =
         prediction_model_and_features_store_->AsWeakPtr();
   }
@@ -250,14 +276,21 @@ void OptimizationGuideKeyedService::Initialize() {
       tab_url_provider_.get(), url_loader_factory,
       MaybeCreatePushNotificationManager(profile),
       optimization_guide_logger_.get());
-  base::FilePath models_dir;
-  base::PathService::Get(chrome::DIR_OPTIMIZATION_GUIDE_PREDICTION_MODELS,
-                         &models_dir);
+  base::FilePath model_downloads_dir;
+  if (!profile->IsOffTheRecord()) {
+    // Do not explicitly hand off the model downloads directory to
+    // off-the-record profiles. Underneath the hood, this variable is only used
+    // in non off-the-record profiles to know where to download the model files
+    // to. Off-the-record profiles read the model locations from the original
+    // profiles they are associated with.
+    model_downloads_dir = profile_path.Append(
+        optimization_guide::kOptimizationGuidePredictionModelDownloads);
+  }
 
   prediction_manager_ = std::make_unique<optimization_guide::PredictionManager>(
       prediction_model_and_features_store, url_loader_factory,
       profile->GetPrefs(), profile->IsOffTheRecord(),
-      g_browser_process->GetApplicationLocale(), models_dir,
+      g_browser_process->GetApplicationLocale(), model_downloads_dir,
       optimization_guide_logger_.get(),
       base::BindOnce(
           &OptimizationGuideKeyedService::BackgroundDownloadServiceProvider,
@@ -265,9 +298,12 @@ void OptimizationGuideKeyedService::Initialize() {
           // |this| owns |prediction_manager_|.
           base::Unretained(this)));
 
-  // The previous store paths were written in incorrect locations. Delete the
-  // old paths. Remove this code in 04/2022 since it should be assumed that all
-  // clients that had the previous path have had their previous stores deleted.
+  // Some previous paths were written in incorrect locations. Delete the
+  // old paths.
+  //
+  // TODO(crbug.com/1328981): Remove this code in 05/2023 since it should be
+  // assumed that all clients that had the previous path have had their previous
+  // stores deleted.
   DeleteOldStorePaths(profile_path);
 
   OPTIMIZATION_GUIDE_LOG(optimization_guide_logger_,
