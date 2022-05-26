d74c8e17578eee4f5a255fe4ddc2f10ce876a938
Yoshifumi Inoue <yosin@chromium.org>
2022-05-26 05:12:32 +0000

    [parallel-text-shaping] Utilize FontTestBase
    This patch changes
     * `BlockListBitmapGlyphsTest`
     * `FontCacheTest`
    to utilize `FontTestBase`, will rest font threads, for preparation
    of parallel-text-shaping.
    
    Bug: 1264280
    Change-Id: Ia6993813e64702927cc7d5ec800685dcf6322995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668986
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1007727}

--- third_party/blink/renderer/platform/fonts/bitmap_glyphs_block_list_test.cc
+++ third_party/blink/renderer/platform/fonts/bitmap_glyphs_block_list_test.cc
@@ -6,11 +6,13 @@
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
 
 #include "build/build_config.h"
-#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/testing/font_test_base.h"
 
 namespace blink {
 
 #if BUILDFLAG(IS_WIN)
+
+class BlockListBitmapGlyphsTest : public FontTestBase {};
 
 static void TestBitmapGlyphsBlockListed(AtomicString windows_family_name,
                                         bool block_listed_expected) {
@@ -30,15 +32,15 @@ static void TestBitmapGlyphsBlockListed(AtomicString windows_family_name,
                 *font_platform_data.Typeface()));
 }
 
-TEST(BlockListBitmapGlyphsTest, Simsun) {
+TEST_F(BlockListBitmapGlyphsTest, Simsun) {
   TestBitmapGlyphsBlockListed("Simsun", false);
 }
 
-TEST(BlockListBitmapGlyphsTest, Arial) {
+TEST_F(BlockListBitmapGlyphsTest, Arial) {
   TestBitmapGlyphsBlockListed("Arial", false);
 }
 
-TEST(BlockListBitmapGlyphsTest, Calibri) {
+TEST_F(BlockListBitmapGlyphsTest, Calibri) {
   TestBitmapGlyphsBlockListed("Calibri", true);
 }
 
