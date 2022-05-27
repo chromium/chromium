1fab3ffa923cbf4031783f3c887712563cee5000
Hidehiko Abe <hidehiko@chromium.org>
2022-05-27 04:30:39 +0000

    Remove GetProfileByUserUnsafe() part 3.
    ProfileHelper is going to be moved out from chrome/browser. Removing
    unneeded dependencies.
    
    The usage of GetProfileByUserUnsafe() here intended to return the
    Profile for the given specific user.
    
    BUG=361528, 1325210
    TEST=Tryjob.
    
    Change-Id: I4f3b6d6a19c79a6d8150de0a5da091a75f52e4c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3670702
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1008136}

--- chrome/browser/extensions/extension_garbage_collector_chromeos.cc
+++ chrome/browser/extensions/extension_garbage_collector_chromeos.cc
@@ -65,7 +65,7 @@ bool ExtensionGarbageCollectorChromeOS::CanGarbageCollectSharedExtensions() {
     if (!active_users[i]->is_profile_created())
       return false;
     Profile* profile =
-        ash::ProfileHelper::Get()->GetProfileByUserUnsafe(active_users[i]);
+        ash::ProfileHelper::Get()->GetProfileByUser(active_users[i]);
     ExtensionGarbageCollectorChromeOS* gc =
         ExtensionGarbageCollectorChromeOS::Get(profile);
     if (gc && gc->crx_installs_in_progress_ > 0)
