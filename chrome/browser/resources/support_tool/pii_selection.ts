ae14d85da569a48cf56e45c47869cae78caaeddf
Irem Uguz <iremuguz@google.com>
2022-05-31 13:23:27 +0000

    support-tool: Add buttons to select all/none PII types to keep in UI
    Add radio-button group with three options keep all, keep none and keep
    some PII types in the collected data in PII selection page of Support
    Tool UI.
    Make the details of PII items non-expanded by default.
    
    Change is made according to mocks and comments in:
    go/log-aggregation-debug-page-mocks
    
    Screenshot after change: https://screenshot.googleplex.com/9cvQ4ximVEpQFxA.png
    
    review page.
    
    Bug: b:232392401
    Test: Open Support Tool (chrome://support-tool) and navigate to PII
    Change-Id: Iadde7ab50575523ec624c3c3c9325820070c0801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3660144
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009070}

--- chrome/browser/resources/support_tool/pii_selection.ts
+++ chrome/browser/resources/support_tool/pii_selection.ts
@@ -5,12 +5,23 @@
 import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.m.js';
+import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.m.js';
+import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import './support_tool_shared_css.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 import {BrowserProxy, BrowserProxyImpl, PIIDataItem} from './browser_proxy.js';
 import {getTemplate} from './pii_selection.html.js';
+
+// Names of the radio buttons which allow the user to choose to keep or remove
+// their PII data.
+enum PiiRadioButtons {
+  INCLUDE_ALL = 'include-all',
+  INCLUDE_NONE = 'include-none',
+  INCLUDE_SOME = 'include-some',
+}
 
 export class PIISelectionElement extends PolymerElement {
   static get is() {
@@ -26,22 +37,36 @@ export class PIISelectionElement extends PolymerElement {
       selectAll_: {
         type: Boolean,
         value: false,
-        observer: 'onSelectAllChanged_',
       },
       detectedPIIItems_: {
         type: Array,
         value: () => [],
+      },
+      piiRadioButtonsEnum_: {
+        readonly: true,
+        type: Object,
+        value: PiiRadioButtons,
+      },
+      selectedRadioButton_: {
+        type: String,
+        value: PiiRadioButtons.INCLUDE_NONE,
+      },
+      showPIISelection_: {
+        type: Boolean,
+        value: false,
       }
     };
   }
 
   private selectAll_: boolean;
+  private selectedRadioButton_: string;
+  private showPIISelection_: boolean;
   private detectedPIIItems_: PIIDataItem[];
   private browserProxy_: BrowserProxy = BrowserProxyImpl.getInstance();
 
   updateDetectedPIIItems(items: PIIDataItem[]) {
     items.forEach((item) => {
-      item.expandDetails = true;
+      item.expandDetails = false;
     });
     this.detectedPIIItems_ = items;
   }
@@ -54,12 +79,28 @@ export class PIISelectionElement extends PolymerElement {
     this.browserProxy_.startDataExport(this.detectedPIIItems_);
   }
 
-  private onSelectAllChanged_() {
+  // Sets this.selectAll_ and updates this.detectedPIIItems_ contents
+  // accordingly.
+  private setSelectAll_(selectAll: boolean) {
+    this.selectAll_ = selectAll;
+    // We won't be showing PII selection checkboxes when this.selectAll_ is set.
+    this.showPIISelection_ = false;
     // Update this.detectedPIIItems_ to reflect the selection choice.
     for (let index = 0; index < this.detectedPIIItems_.length; index++) {
       // Mutate the array observably. See:
       // https://polymer-library.polymer-project.org/3.0/docs/devguide/data-system#make-observable-changes
       this.set(`detectedPIIItems_.${index}.keep`, this.selectAll_);
+    }
+  }
+
+  private onSelectedRadioButtonChanged_(event: CustomEvent<{value: string}>) {
+    this.selectedRadioButton_ = event.detail.value;
+    if (this.selectedRadioButton_ === PiiRadioButtons.INCLUDE_ALL) {
+      this.setSelectAll_(true);
+    } else if (this.selectedRadioButton_ === PiiRadioButtons.INCLUDE_NONE) {
+      this.setSelectAll_(false);
+    } else {
+      this.showPIISelection_ = true;
     }
   }
 }
