fd8a95410228276ddcbf58b25d23652ea000c80a
Mei Liang <meiliang@chromium.org>
2022-05-31 21:53:55 +0000

    [ConsentV2] Add the consent dialog view
    This CL implements the complete consent dialog view.
    
    Mock: https://screenshot.googleplex.com/BWL9oYXggrXJfci
    Screenshot: https://screenshot.googleplex.com/7GAshxrJbNA8dfN
    
    (cherry picked from commit ed06c8f6e464dcdd85deef0116bae4f00600b4d5)
    
    Bug: 1318685
    Change-Id: Iaa0c9368a1f5010b524e87b4690025d75456c2c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3603024
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1005546}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3680288
    Reviewed-by: Prudhvikumar Bommana <pbommana@google.com>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/5060@{#430}
    Cr-Branched-From: b83393d0f4038aeaf67f970a024d8101df7348d1-refs/heads/main@{#1002911}

---
+++ chrome/browser/ui/views/commerce/ntp_discount_consent_dialog_view_browsertest.cc
@@ -0,0 +1,33 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/commerce/ntp_discount_consent_dialog_view.h"
+
+#include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "content/public/test/browser_test.h"
+
+// Test harness for integration tests using NtpDiscountConsentView.
+class NtpDiscountConsentDialogViewBrowserTest : public DialogBrowserTest {
+ public:
+  NtpDiscountConsentDialogViewBrowserTest() = default;
+
+  NtpDiscountConsentDialogViewBrowserTest(
+      const NtpDiscountConsentDialogViewBrowserTest&) = delete;
+  NtpDiscountConsentDialogViewBrowserTest& operator=(
+      const NtpDiscountConsentDialogViewBrowserTest&) = delete;
+
+  // DialogBrowserTest:
+  void ShowUi(const std::string& name) override {
+    NtpDiscountConsentDialogView::Show(
+        browser(),
+        base::BindOnce([](chrome_cart::mojom::ConsentStatus status) {}));
+  }
+};
+
+// Shows the dialog for bookmarking all tabs. This shows a BookmarkEditorView
+// dialog, with a tree view, where a user can rename and select a parent folder.
+IN_PROC_BROWSER_TEST_F(NtpDiscountConsentDialogViewBrowserTest,
+                       InvokeUi_default) {
+  ShowAndVerifyUi();
+}
