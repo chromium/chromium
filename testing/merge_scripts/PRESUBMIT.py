b327336956a3b6a230f96235ea1d49f93e2e10fa
Bruce Dawson <brucedawson@chromium.org>
2022-05-31 16:48:40 +0000

    Stop running testing unittests under Python 2
    The *_unittest.py and unexpected_passes_common/*_unittest.py scripts
    have been running under Python 3 for a few weeks (some longer) without
    problems so it seems reasonable to switch them to Python 2 only, to
    drive down the Python 2 invocations during the presubmits.
    
    Bug: 1313804
    Change-Id: Iafb24ed77c98e2821e59ac788d9fa15cea51eb29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674656
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009132}

--- testing/merge_scripts/PRESUBMIT.py
+++ testing/merge_scripts/PRESUBMIT.py
@@ -13,6 +13,7 @@ USE_PYTHON3 = True
 def CommonChecks(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
       input_api, output_api, '.', [ r'^.+_test\.py$'],
+      run_on_python2=False,
       skip_shebang_check = True)
 
 def CheckChangeOnUpload(input_api, output_api):
