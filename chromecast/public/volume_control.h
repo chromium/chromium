e783145401b38d270e133e4566bee03f0fee9896
Ken MacKay <kmackay@chromium.org>
2022-05-31 16:40:21 +0000

    [Chromecast] Add a value to disable audio volume feedback
    Bug: internal b/159172975
    Change-Id: I596071ea60dad400a4771b399d0976a0a5f37058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3669739
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009128}

--- chromecast/public/volume_control.h
+++ chromecast/public/volume_control.h
@@ -48,6 +48,8 @@ enum class VolumeChangeSource {
   kAutomatic,         // Automatic volume change, no user involvement.
   kAutoWithFeedback,  // Automatic volume change, but we still want to have
                       // volume feedback UX.
+  kUserWithNoAudioFeedback,  // User-initiated change, but audible feedback is
+                             // disabled.
 };
 
 inline std::ostream& operator<<(std::ostream& os,
@@ -59,6 +61,8 @@ inline std::ostream& operator<<(std::ostream& os,
       return os << "AUTOMATIC";
     case VolumeChangeSource::kAutoWithFeedback:
       return os << "AUTO_WITH_FEEDBACK";
+    case VolumeChangeSource::kUserWithNoAudioFeedback:
+      return os << "USER_NO_AUDIO_FEEDBACK";
   }
 }
 
