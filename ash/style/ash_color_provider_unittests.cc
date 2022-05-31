8d5e085023fa8845d649e26124b110a2822c722c
minch <minch@chromium.org>
2022-05-31 16:42:32 +0000

    dark_mode: Fix tests failure after enabling the feature - part10
    This cl fixes AshColorProviderTest* failure after enabling this
    feature and when it is in dark mode.
    
    Bug: 1326629
    Change-Id: I713d701f6a2db74d437210ceab09da4d62549d3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3674517
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009129}

--- ash/style/ash_color_provider_unittests.cc
+++ ash/style/ash_color_provider_unittests.cc
@@ -4,38 +4,46 @@
 
 #include "ash/style/ash_color_provider.h"
 
+#include "ash/login/login_screen_controller.h"
 #include "ash/login/ui/login_test_base.h"
 #include "ash/public/cpp/login_types.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/session/test_session_controller_client.h"
+#include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/test/scoped_feature_list.h"
 #include "chromeos/constants/chromeos_features.h"
 
 namespace ash {
 
-using AshColorProviderTest = LoginTestBase;
+using AshColorProviderTest = NoSessionAshTestBase;
 
 // Tests the color mode in non-active user sessions.
 TEST_F(AshColorProviderTest, ColorModeInNonActiveUserSessions) {
   auto* client = GetSessionControllerClient();
   auto* color_provider = AshColorProvider::Get();
 
-  // When dark/light mode is enabled. Color mode in non-active user sessions
-  // (e.g, login page) should be DARK, but LIGHT while in OOBE.
   base::test::ScopedFeatureList enable_dark_light;
   enable_dark_light.InitAndEnableFeature(chromeos::features::kDarkLightMode);
   client->SetSessionState(session_manager::SessionState::UNKNOWN);
+  // When dark/light mode is enabled. Color mode in non-active user sessions
+  // (e.g, login page) should be DARK.
+  auto* active_user_pref_service =
+      Shell::Get()->session_controller()->GetPrimaryUserPrefService();
+  ASSERT_FALSE(active_user_pref_service);
   EXPECT_TRUE(color_provider->IsDarkModeEnabled());
 
+  // But color mode should be LIGHT in OOBE.
+  auto* dispatcher = Shell::Get()->login_screen_controller()->data_dispatcher();
   client->SetSessionState(session_manager::SessionState::OOBE);
-  DataDispatcher()->NotifyOobeDialogState(OobeDialogState::USER_CREATION);
+  dispatcher->NotifyOobeDialogState(OobeDialogState::USER_CREATION);
   EXPECT_FALSE(color_provider->IsDarkModeEnabled());
 
   client->SetSessionState(session_manager::SessionState::LOGIN_PRIMARY);
-  DataDispatcher()->NotifyOobeDialogState(OobeDialogState::HIDDEN);
+  dispatcher->NotifyOobeDialogState(OobeDialogState::HIDDEN);
   EXPECT_TRUE(color_provider->IsDarkModeEnabled());
 
-  DataDispatcher()->NotifyOobeDialogState(OobeDialogState::GAIA_SIGNIN);
+  dispatcher->NotifyOobeDialogState(OobeDialogState::GAIA_SIGNIN);
   EXPECT_FALSE(color_provider->IsDarkModeEnabled());
 
   // When dark/light mode is disabled. Color mode in non-active user sessions
@@ -45,7 +53,7 @@ TEST_F(AshColorProviderTest, ColorModeInNonActiveUserSessions) {
   client->SetSessionState(session_manager::SessionState::UNKNOWN);
   EXPECT_TRUE(color_provider->IsDarkModeEnabled());
   client->SetSessionState(session_manager::SessionState::OOBE);
-  DataDispatcher()->NotifyOobeDialogState(OobeDialogState::USER_CREATION);
+  dispatcher->NotifyOobeDialogState(OobeDialogState::USER_CREATION);
   EXPECT_TRUE(color_provider->IsDarkModeEnabled());
 }
 
