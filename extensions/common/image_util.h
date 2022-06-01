763f712d12a9ace2ff4fe9d310ed79a9859dead0
David Bertoni <dbertoni@chromium.org>
2022-06-01 20:43:33 +0000

    [Extensions] Limit the bitmap size for icon visibility analysis.
    There are crash reports as a result of allocation a bitmap for icon
    visibility analysis. This seems strange unless the webstore doesn't
    limit the size of an icon or perhaps extensions that aren't from the
    webstore are causing the crashes.
    
    The fix is to both limit the size of the icon to be analyzed and use
    an allocation function that fails gracefully.
    
    Bug: 1155746
    Change-Id: Iaa227dd499cc9b5a0f60be2f5336cf1c6d1f80b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3661161
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009795}

--- extensions/common/image_util.h
+++ extensions/common/image_util.h
@@ -40,11 +40,16 @@ bool IsRenderedIconSufficientlyVisible(const SkBitmap& bitmap,
 bool IsRenderedIconAtPathSufficientlyVisible(const base::FilePath& path,
                                              SkColor background_color);
 
+// Icons should be a reasonable size for analysis. There have been crash
+// reports due to memory allocation issues with calls to
+// SkBitmap::allocN32Pixels. See crbug.com/1155746.
+constexpr int kMaxAllowedPixels = 2048 * 2048;
+
 // Renders the icon bitmap onto another bitmap, combining it with the specified
 // background color. The output bitmap must be empty.
-void RenderIconForVisibilityAnalysis(const SkBitmap& icon,
-                                     SkColor background_color,
-                                     SkBitmap* rendered_icon);
+[[nodiscard]] bool RenderIconForVisibilityAnalysis(const SkBitmap& icon,
+                                                   SkColor background_color,
+                                                   SkBitmap* rendered_icon);
 
 // Load a PNG image from a file into the destination bitmap.
 bool LoadPngFromFile(const base::FilePath& path, SkBitmap* dst);
