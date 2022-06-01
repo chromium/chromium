1b07c450a7526137304ee8e8ea16e49f0aee9f3f
Xiang Ji <jxiang@google.com>
2022-06-01 20:54:53 +0000

    Fix CBCM enrollment system test
    The legend is hidden from the shadow DOM recently. Accommodating this change.
    
    Validation: tryjob and local pass.
    
    Bug: 1329483
    Change-Id: I1e66e16300f6459d7345ab91238107a960127638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3671619
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1009802}

--- chrome/test/enterprise/e2e/policy/cloud_management_enrollment_token/cloud_enrollment_webdriver.py
+++ chrome/test/enterprise/e2e/policy/cloud_management_enrollment_token/cloud_enrollment_webdriver.py
@@ -9,6 +9,15 @@ from selenium import webdriver
 import test_util
 
 
+def getElementFromShadowRoot(driver, element, selector):
+  if element is None:
+    return None
+  else:
+    return driver.execute_script(
+        "return arguments[0].shadowRoot.querySelector(arguments[1])", element,
+        selector)
+
+
 def main(argv):
   options = webdriver.ChromeOptions()
   os.environ["CHROME_LOG_FILE"] = r"c:\temp\chrome_log.txt"
@@ -22,10 +31,15 @@ def main(argv):
     policy_url = "chrome://policy"
     driver.get(policy_url)
     driver.find_element_by_id('reload-policies').click
-    print(driver.find_element_by_class_name('legend').text)
-    print(driver.find_element_by_class_name('machine-enrollment-name').text)
-    print(driver.find_element_by_class_name('machine-enrollment-token').text)
-    print(driver.find_element_by_class_name('status').text)
+    # Give the page 2 seconds to render the legend
+    time.sleep(2)
+    status_box = driver.find_element_by_css_selector("status-box")
+    el = getElementFromShadowRoot(driver, status_box, "fieldset")
+
+    print(el.find_element_by_class_name('legend').text)
+    print(el.find_element_by_class_name('machine-enrollment-name').text)
+    print(el.find_element_by_class_name('machine-enrollment-token').text)
+    print(el.find_element_by_class_name('status').text)
   except Exception as error:
     print(error)
   finally:
