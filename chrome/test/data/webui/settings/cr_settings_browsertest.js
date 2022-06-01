157a72ae1e6f1eed74f5bf60132989a53c4960d2
David Roger <droger@chromium.org>
2022-06-01 11:56:12 +0000

    [Lacros] Add sublabel to "Clear Cookie On Exit" setting
    The string is:
    
      "Signs you out of most sites. You won't be signed out of your
       Google Account."
    
    Screenshot:
    https://storage.cloud.google.com/chromium-translation-screenshots/14d766028b9f4509cb83ea8dc050c0afff0f0189
    
    Bug: 1104667
    Change-Id: I32eaf3313c4376a2a1008d334bf0a69364d4b6bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666719
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009543}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -631,6 +631,12 @@ var CrSettingsRouteTest = class extends CrSettingsBrowserTest {
   }
 };
 
+GEN('#if BUILDFLAG(IS_CHROMEOS_LACROS)');
+TEST_F('CrSettingsCookiesPageTest', 'LacrosSecondaryProfile', function() {
+  runMochaSuite('CrSettingsCookiesPageTest_lacrosSecondaryProfile');
+});
+GEN('#endif');
+
 TEST_F('CrSettingsRouteTest', 'Basic', function() {
   runMochaSuite('route');
 });
