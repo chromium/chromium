6c9f8554db44f3079e4bc3ed6b3c86affca668c2
Ian Clelland <iclelland@chromium.org>
2022-06-01 20:01:22 +0000

    Revert "[Telemetry] Enforce secure conn in ApiGuard"
    This reverts commit 4bcbd6c7b69fd5e46bed40a7c7d791b0f03bd1b3.
    
    Reason for revert:
    Failing bots:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/21998
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/43618
    
    Two bots are failing because of this change; looks like a couple of tests need to be updated.
    
    
    Original change's description:
    > [Telemetry] Enforce secure conn in ApiGuard
    >
    > Enforce that the PWA connection is secure (e.g. no cert errors) before
    > accessing the telemetry APIs.
    >
    > Bug: b:229774016
    > Test: browser_tests --gtest_fitler="*TelemetryExtension*"
    > Test: unit_tests --gtest_filter="*ApiGuardDelegate*"
    > Test: tast run <IP> telemetryextension.APIAvailableRoutines.*
    >
    > Change-Id: I333e50387467a875f4364a060dd27579dfb5a516
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3666977
    > Reviewed-by: Oleh Lamzin <lamzin@google.com>
    > Commit-Queue: Mahmoud Aly <mgawad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1009588}
    
    Bug: b:229774016
    Change-Id: I21c0afa789aa9d17711d5560f495340a8914d40b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3681964
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009772}

--- chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_api_guard_function_browsertest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_api_guard_function_browsertest.cc
@@ -17,11 +17,7 @@
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/content_mock_cert_verifier.h"
-#include "net/base/net_errors.h"
-#include "net/cert/x509_certificate.h"
 #include "net/dns/mock_host_resolver.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -298,9 +294,7 @@ class TelemetryExtensionApiGuardRealDelegateBrowserTest
     : public BaseTelemetryExtensionBrowserTest {
  public:
   TelemetryExtensionApiGuardRealDelegateBrowserTest()
-      : fake_hardware_info_delegate_factory_("HP"),
-        https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
-    https_server_.ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+      : fake_hardware_info_delegate_factory_("HP") {
     // Make sure device manufacturer is allowlisted.
     HardwareInfoDelegate::Factory::SetForTesting(
         &fake_hardware_info_delegate_factory_);
@@ -313,12 +307,6 @@ class TelemetryExtensionApiGuardRealDelegateBrowserTest
       const TelemetryExtensionApiGuardRealDelegateBrowserTest&) = delete;
 
   // BaseTelemetryExtensionBrowserTest:
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    BaseTelemetryExtensionBrowserTest::SetUpCommandLine(command_line);
-
-    mock_cert_verifier_.SetUpCommandLine(command_line);
-  }
-
   void SetUpOnMainThread() override {
     // Skip BaseTelemetryExtensionBrowserTest::SetUpOnMainThread() as it sets up
     // a FakeApiGuardDelegate instance.
@@ -327,8 +315,6 @@ class TelemetryExtensionApiGuardRealDelegateBrowserTest
     // Must be initialized before dealing with UserManager.
     user_manager_enabler_ = std::make_unique<user_manager::ScopedUserManager>(
         std::make_unique<ash::FakeChromeUserManager>());
-
-    ASSERT_TRUE(https_server_.Start());
 
     // This is needed when navigating to a network URL (e.g.
     // ui_test_utils::NavigateToURL). Rules can only be added before
@@ -348,48 +334,14 @@ class TelemetryExtensionApiGuardRealDelegateBrowserTest
     BaseTelemetryExtensionBrowserTest::TearDownOnMainThread();
   }
 
-  void SetUpInProcessBrowserTestFixture() override {
-    BaseTelemetryExtensionBrowserTest::SetUpInProcessBrowserTestFixture();
-
-    mock_cert_verifier_.SetUpInProcessBrowserTestFixture();
-  }
-
-  void TearDownInProcessBrowserTestFixture() override {
-    mock_cert_verifier_.TearDownInProcessBrowserTestFixture();
-
-    BaseTelemetryExtensionBrowserTest::TearDownInProcessBrowserTestFixture();
-  }
-
-  content::ContentMockCertVerifier::CertVerifier* mock_cert_verifier() {
-    return mock_cert_verifier_.mock_cert_verifier();
-  }
-
-  void SetUpMockCertVerifierForHttpsServer() {
-    scoped_refptr<net::X509Certificate> cert(https_server_.GetCertificate());
-    net::CertVerifyResult verify_result;
-    verify_result.is_issued_by_known_root = true;
-    verify_result.verified_cert = cert;
-    verify_result.cert_status = net::OK;
-
-    mock_cert_verifier()->AddResultForCert(cert, verify_result, net::OK);
-  }
-
  protected:
   ash::FakeChromeUserManager* GetFakeUserManager() const {
     return static_cast<ash::FakeChromeUserManager*>(
         user_manager::UserManager::Get());
   }
 
-  GURL GetPwaGURL() const { return https_server_.GetURL("/ssl/google.html"); }
-
-  // BaseTelemetryExtensionBrowserTest:
-  std::string pwa_page_url() const override { return GetPwaGURL().spec(); }
-  std::string matches_origin() const override { return GetPwaGURL().spec(); }
-
   FakeHardwareInfoDelegate::Factory fake_hardware_info_delegate_factory_;
   std::unique_ptr<user_manager::ScopedUserManager> user_manager_enabler_;
-  net::EmbeddedTestServer https_server_;
-  content::ContentMockCertVerifier mock_cert_verifier_;
 };
 
 // Smoke test to verify that real ApiGuardDelegate works in prod.
@@ -403,13 +355,7 @@ IN_PROC_BROWSER_TEST_F(TelemetryExtensionApiGuardRealDelegateBrowserTest,
   user_manager->SwitchActiveUser(account_id);
   user_manager->SetOwnerId(account_id);
 
-  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      chromeos::switches::kTelemetryExtensionPwaOriginOverrideForTesting,
-      pwa_page_url());
-
-  SetUpMockCertVerifierForHttpsServer();
-
-  // Make sure PWA UI is open and secure.
+  // Make sure PWA UI is open.
   auto* pwa_page_rfh =
       ui_test_utils::NavigateToURL(browser(), GURL(pwa_page_url()));
   ASSERT_TRUE(pwa_page_rfh);
