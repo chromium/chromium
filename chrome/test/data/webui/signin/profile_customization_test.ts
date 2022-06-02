b5bc0c5b6e2ddecf123ffbc2900f8469193d40b9
Gabriel Oliveira <gabolvr@google.com>
2022-06-02 12:39:44 +0000

    Parameterize ProfileCustomization tests based on inDialogDesign and test for skip button
    Bug: 1282157
    Change-Id: I79d24819852dae6542ae3b69e142acc1e5515a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682469
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010045}

--- chrome/test/data/webui/signin/profile_customization_test.ts
+++ chrome/test/data/webui/signin/profile_customization_test.ts
@@ -14,105 +14,116 @@ import {isChildVisible} from 'chrome://webui-test/test_util.js';
 
 import {TestProfileCustomizationBrowserProxy} from './test_profile_customization_browser_proxy.js';
 
-suite('ProfileCustomizationTest', function() {
-  let app: ProfileCustomizationAppElement;
-  let browserProxy: TestProfileCustomizationBrowserProxy;
+[true, false].forEach(inDialogDesign => {
+  const inDialogDesignSuffix = inDialogDesign ? 'InDialogDesign' : '';
 
-  const AVATAR_URL_1 = 'chrome://theme/IDR_PROFILE_AVATAR_1';
-  const AVATAR_URL_2 = 'chrome://theme/IDR_PROFILE_AVATAR_2';
-  const WELCOME_TEXT_1 = 'Welcome, Bob';
-  const WELCOME_TEXT_2 = 'Hi, Elisa';
+  suite(`ProfileCustomizationTest${inDialogDesignSuffix}`, function() {
+    let app: ProfileCustomizationAppElement;
+    let browserProxy: TestProfileCustomizationBrowserProxy;
 
-  setup(function() {
-    loadTimeData.overrideValues({
-      profileName: 'TestName',
+    const AVATAR_URL_1 = 'chrome://theme/IDR_PROFILE_AVATAR_1';
+    const AVATAR_URL_2 = 'chrome://theme/IDR_PROFILE_AVATAR_2';
+    const WELCOME_TEXT_1 = 'Welcome, Bob';
+    const WELCOME_TEXT_2 = 'Hi, Elisa';
+
+    setup(function() {
+      loadTimeData.overrideValues({
+        profileName: 'TestName',
+        profileCustomizationInDialogDesign: inDialogDesign,
+      });
+      browserProxy = new TestProfileCustomizationBrowserProxy();
+      browserProxy.setProfileInfo({
+        backgroundColor: 'rgb(0, 255, 0)',
+        pictureUrl: AVATAR_URL_1,
+        isManaged: false,
+        welcomeTitle: WELCOME_TEXT_1,
+      });
+      ProfileCustomizationBrowserProxyImpl.setInstance(browserProxy);
+      document.body.innerHTML = '';
+      app = document.createElement('profile-customization-app');
+      document.body.append(app);
+      return browserProxy.whenCalled('initialized');
     });
-    browserProxy = new TestProfileCustomizationBrowserProxy();
-    browserProxy.setProfileInfo({
-      backgroundColor: 'rgb(0, 255, 0)',
-      pictureUrl: AVATAR_URL_1,
-      isManaged: false,
-      welcomeTitle: WELCOME_TEXT_1,
+
+    function checkImageUrl(elementId: string, expectedUrl: string) {
+      assertTrue(isChildVisible(app, elementId));
+      const img = app.shadowRoot!.querySelector<HTMLImageElement>(elementId)!;
+      assertEquals(expectedUrl, img.src);
+    }
+
+    // Checks that clicking Done without interacting with the input does not
+    // change the name.
+    test('ClickDone', function() {
+      assertTrue(isChildVisible(app, '#doneButton'));
+      const doneButton = app.$.doneButton;
+      assertFalse(doneButton.disabled);
+      doneButton.click();
+      return browserProxy.whenCalled('done').then(
+          profileName => assertEquals('TestName', profileName));
     });
-    ProfileCustomizationBrowserProxyImpl.setInstance(browserProxy);
-    document.body.innerHTML = '';
-    app = document.createElement('profile-customization-app');
-    document.body.append(app);
-    return browserProxy.whenCalled('initialized');
-  });
 
-  function checkImageUrl(elementId: string, expectedUrl: string) {
-    assertTrue(isChildVisible(app, elementId));
-    const img = app.shadowRoot!.querySelector<HTMLImageElement>(elementId)!;
-    assertEquals(expectedUrl, img.src);
-  }
+    // Checks that the name can be changed.
+    test('ChangeName', function() {
+      const nameInput = app.$.nameInput;
+      // Check the default value for the input.
+      assertEquals('TestName', nameInput.value);
+      assertFalse(nameInput.invalid);
 
-  // Checks that clicking Done without interacting with the input does not
-  // change the name.
-  test('ClickDone', function() {
-    assertTrue(isChildVisible(app, '#doneButton'));
-    const doneButton = app.$.doneButton;
-    assertFalse(doneButton.disabled);
-    doneButton.click();
-    return browserProxy.whenCalled('done').then(
-        profileName => assertEquals('TestName', profileName));
-  });
+      // Invalid name (white space).
+      nameInput.value = '   ';
+      assertTrue(nameInput.invalid);
 
-  // Checks that the name can be changed.
-  test('ChangeName', function() {
-    const nameInput = app.$.nameInput;
-    // Check the default value for the input.
-    assertEquals('TestName', nameInput.value);
-    assertFalse(nameInput.invalid);
+      // The button is disabled.
+      assertTrue(isChildVisible(app, '#doneButton'));
+      const doneButton = app.$.doneButton;
+      assertTrue(doneButton.disabled);
 
-    // Invalid name (white space).
-    nameInput.value = '   ';
-    assertTrue(nameInput.invalid);
+      // Empty name.
+      nameInput.value = '';
+      assertTrue(nameInput.invalid);
+      assertTrue(doneButton.disabled);
 
-    // The button is disabled.
-    assertTrue(isChildVisible(app, '#doneButton'));
-    const doneButton = app.$.doneButton;
-    assertTrue(doneButton.disabled);
+      // Valid name.
+      nameInput.value = 'Bob';
+      assertFalse(nameInput.invalid);
 
-    // Empty name.
-    nameInput.value = '';
-    assertTrue(nameInput.invalid);
-    assertTrue(doneButton.disabled);
-
-    // Valid name.
-    nameInput.value = 'Bob';
-    assertFalse(nameInput.invalid);
-
-    // Click done, and check that the new name is sent.
-    assertTrue(isChildVisible(app, '#doneButton'));
-    assertFalse(doneButton.disabled);
-    doneButton.click();
-    return browserProxy.whenCalled('done').then(
-        profileName => assertEquals('Bob', profileName));
-  });
-
-  test('ProfileInfo', function() {
-    const header = app.shadowRoot!.querySelector<HTMLElement>('#header')!;
-    // Check initial info.
-    assertEquals(app.$.title.innerText, WELCOME_TEXT_1);
-    assertEquals('rgb(0, 255, 0)', getComputedStyle(header).backgroundColor);
-    checkImageUrl('#avatar', AVATAR_URL_1);
-    assertFalse(isChildVisible(app, '#badge'));
-    // Update the info.
-    const color2 = 'rgb(4, 5, 6)';
-    webUIListenerCallback('on-profile-info-changed', {
-      backgroundColor: color2,
-      pictureUrl: AVATAR_URL_2,
-      isManaged: true,
-      welcomeTitle: WELCOME_TEXT_2,
+      // Click done, and check that the new name is sent.
+      assertTrue(isChildVisible(app, '#doneButton'));
+      assertFalse(doneButton.disabled);
+      doneButton.click();
+      return browserProxy.whenCalled('done').then(
+          profileName => assertEquals('Bob', profileName));
     });
-    assertEquals(app.$.title.innerText, WELCOME_TEXT_2);
-    assertEquals(color2, getComputedStyle(header).backgroundColor);
-    checkImageUrl('#avatar', AVATAR_URL_2);
-    assertTrue(isChildVisible(app, '#badge'));
-  });
 
-  test('ThemeSelector', function() {
-    assertTrue(!!app.shadowRoot!.querySelector('#themeSelector'));
+    test('ProfileInfo', function() {
+      const header = app.shadowRoot!.querySelector<HTMLElement>('#header')!;
+      // Check initial info.
+      assertEquals(app.$.title.innerText, WELCOME_TEXT_1);
+      assertEquals('rgb(0, 255, 0)', getComputedStyle(header).backgroundColor);
+      checkImageUrl('#avatar', AVATAR_URL_1);
+      assertFalse(isChildVisible(app, '#badge'));
+      // Update the info.
+      const color2 = 'rgb(4, 5, 6)';
+      webUIListenerCallback('on-profile-info-changed', {
+        backgroundColor: color2,
+        pictureUrl: AVATAR_URL_2,
+        isManaged: true,
+        welcomeTitle: WELCOME_TEXT_2,
+      });
+      assertEquals(app.$.title.innerText, WELCOME_TEXT_2);
+      assertEquals(color2, getComputedStyle(header).backgroundColor);
+      checkImageUrl('#avatar', AVATAR_URL_2);
+      assertTrue(isChildVisible(app, '#badge'));
+    });
+
+    test('ThemeSelector', function() {
+      assertTrue(!!app.shadowRoot!.querySelector('#themeSelector'));
+    });
+
+    // Checks that the Skip button is present when the page is displayed in a
+    // dialog in Sync Promo
+    test('HasSkipButton', function() {
+      assertEquals(inDialogDesign, isChildVisible(app, '#skipButton'));
+    });
   });
 });
