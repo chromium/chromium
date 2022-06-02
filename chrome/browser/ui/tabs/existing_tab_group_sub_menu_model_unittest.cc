4f3c6315df79f4b0b5cba2173675d17819725102
dljames <dljames@google.com>
2022-06-02 00:10:34 +0000

    [ Bug Fix ] Add tab to group.
    Previously the 'Add tab to group' context menu for tabs would only show the tab groups for the current window.
    
    This fix addresses the issue by showing an exhaustive list of all tab groups that appear in any window for the user.
    
    Change-Id: I6fcc4f840088074efc543cd2f78fcbf4b8599226
    Bug: 1190408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3662064
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1009869}

--- chrome/browser/ui/tabs/existing_tab_group_sub_menu_model_unittest.cc
+++ chrome/browser/ui/tabs/existing_tab_group_sub_menu_model_unittest.cc
@@ -3,17 +3,20 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/tabs/existing_tab_group_sub_menu_model.h"
+#include <memory>
 
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_tab_menu_model_delegate.h"
+#include "chrome/browser/ui/tabs/tab_group.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
+#include "components/tab_groups/tab_group_id.h"
 #include "url/gurl.h"
-
-namespace {
 
 class ExistingTabGroupSubMenuModelTest : public BrowserWithTestWindowTest {
  public:
-  ExistingTabGroupSubMenuModelTest() : BrowserWithTestWindowTest() {}
+  ExistingTabGroupSubMenuModelTest() = default;
 };
 
 // Ensure that add to group submenu only appears when there is another group to
@@ -49,14 +52,138 @@ TEST_F(ExistingTabGroupSubMenuModelTest, BuildSubmenuItems) {
   ASSERT_FALSE(model->GetTabGroupForTab(2).has_value());
   ASSERT_EQ(model->count(), 3);
 
-  ExistingTabGroupSubMenuModel menu1(nullptr, model, 0);
+  ExistingTabGroupSubMenuModel menu1(nullptr, nullptr, model, 0);
   EXPECT_EQ(3, menu1.GetItemCount());
 
-  ExistingTabGroupSubMenuModel menu2(nullptr, model, 1);
+  ExistingTabGroupSubMenuModel menu2(nullptr, nullptr, model, 1);
   EXPECT_EQ(3, menu2.GetItemCount());
 
-  ExistingTabGroupSubMenuModel menu3(nullptr, model, 2);
+  ExistingTabGroupSubMenuModel menu3(nullptr, nullptr, model, 2);
   EXPECT_EQ(4, menu3.GetItemCount());
 }
 
-}  // namespace
+// Verify tabs can be added tab groups in the same window.
+TEST_F(ExistingTabGroupSubMenuModelTest, AddTabsToGroupSameWindow) {
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+
+  TabStripModel* model = browser()->tab_strip_model();
+  model->AddToNewGroup({0});
+  model->AddToNewGroup({1});
+  ExistingTabGroupSubMenuModel menu(nullptr, nullptr, model, 2);
+  EXPECT_EQ(4, menu.GetItemCount());
+
+  // Move the tab at index 2 into the group with the tab at index 0.
+  menu.ExecuteExistingCommandForTesting(0);
+
+  ASSERT_EQ(size_t(2), model->group_model()->ListTabGroups().size());
+  EXPECT_EQ(2, model->group_model()
+                   ->GetTabGroup(model->group_model()->ListTabGroups()[0])
+                   ->tab_count());
+
+  ExistingTabGroupSubMenuModel menu2(nullptr, nullptr, model, 2);
+  EXPECT_EQ(3, menu2.GetItemCount());
+}
+
+// Verify non-selected tabs can be added tab groups in the same window.
+TEST_F(ExistingTabGroupSubMenuModelTest, AddNonSelectedTabsToTabGroup) {
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+
+  TabStripModel* model = browser()->tab_strip_model();
+  model->AddToNewGroup({0});
+  model->AddToNewGroup({1});
+
+  // Select the tab at index 2.
+  model->ToggleSelectionAt(2);
+
+  // Create the menu on the tab at index 3.
+  ExistingTabGroupSubMenuModel menu(nullptr, nullptr, model, 3);
+  EXPECT_EQ(4, menu.GetItemCount());
+
+  // Move the tab at index 2 into the group with the tab at index 0.
+  menu.ExecuteExistingCommandForTesting(0);
+
+  ASSERT_EQ(size_t(2), model->group_model()->ListTabGroups().size());
+  EXPECT_EQ(2, model->group_model()
+                   ->GetTabGroup(model->group_model()->ListTabGroups()[0])
+                   ->tab_count());
+
+  ExistingTabGroupSubMenuModel menu2(nullptr, nullptr, model, 2);
+  ExistingTabGroupSubMenuModel menu3(nullptr, nullptr, model, 3);
+
+  EXPECT_EQ(3, menu2.GetItemCount());
+  EXPECT_EQ(4, menu3.GetItemCount());
+}
+
+// Verify tabs can be added to tab groups in other browser windows.
+TEST_F(ExistingTabGroupSubMenuModelTest, AddAllSelectedTabsToAnotherWindow) {
+  std::unique_ptr<BrowserWindow> window_1 = CreateBrowserWindow();
+  std::unique_ptr<Browser> new_browser = CreateBrowser(
+      browser()->profile(), browser()->type(), false, window_1.get());
+
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+  AddTab(browser(), GURL("chrome://newtab"));
+
+  AddTab(new_browser.get(), GURL("chrome://newtab"));
+  AddTab(new_browser.get(), GURL("chrome://newtab"));
+  AddTab(new_browser.get(), GURL("chrome://newtab"));
+  AddTab(new_browser.get(), GURL("chrome://newtab"));
+
+  TabStripModel* model_1 = new_browser->tab_strip_model();
+  TabStripModel* model_2 = browser()->tab_strip_model();
+
+  EXPECT_EQ(model_1->count(), 4);
+  EXPECT_EQ(model_1->count(), 4);
+
+  std::unique_ptr<TabMenuModelDelegate> delegate_1 =
+      std::make_unique<chrome::BrowserTabMenuModelDelegate>(new_browser.get());
+
+  // First tabs of each model consists of a tab group.
+  model_1->AddToNewGroup({0});
+  model_2->AddToNewGroup({0});
+
+  ExistingTabGroupSubMenuModel menu_1(nullptr, delegate_1.get(), model_1, 1);
+
+  // In order to move the 3 un-grouped tabs in `model_1` we must select those
+  // tabs in addition to unselecting the grouped tab. We do this in reverse
+  // order since at this point the only tab that is selected is the grouped tab.
+  // We are unable to deselect this tab first. Doing so creates a state where no
+  // tabs are selected which is not allowed.
+  for (int i = model_1->count() - 1; i >= 0; --i)
+    model_1->ToggleSelectionAt(i);
+
+  const ui::ListSelectionModel::SelectedIndices selection_indices =
+      model_1->selection_model().selected_indices();
+  std::vector<int> selected_indices =
+      std::vector<int>(selection_indices.begin(), selection_indices.end());
+  EXPECT_EQ(selected_indices.size(), size_t(3));
+  EXPECT_EQ(4, menu_1.GetItemCount());
+
+  // Move the 3 selected indices in model_1 to model_2.
+  menu_1.ExecuteExistingCommandForTesting(1);
+  EXPECT_EQ(model_2->count(), 7);
+
+  // Verify the tab group in model_2 now has 4 tabs in it.
+  TabGroup* group = model_2->group_model()->GetTabGroup(
+      model_2->GetTabGroupForTab(0).value());
+  EXPECT_EQ(group->tab_count(), 4);
+
+  int num_selected = 0;
+
+  for (int i = 0; i < model_2->count(); ++i) {
+    if (model_2->IsTabSelected(i))
+      ++num_selected;
+  }
+
+  // Expect the number of tabs we moved from model_1 into model_2 is still 3.
+  EXPECT_EQ(num_selected, 3);
+
+  new_browser.get()->tab_strip_model()->CloseAllTabs();
+  new_browser.reset();
+}
