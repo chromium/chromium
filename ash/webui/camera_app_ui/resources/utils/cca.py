182fed81551306bedd75d7619332ef6ed06bcb43
Pi-Hsun Shih <pihsun@chromium.org>
2022-06-02 09:36:41 +0000

    CCA: use a+rX for permission when deploying
    From man page, `If none of these are given, the effect is as if (a) were
    given, but bits that are set in the umask are not affected.`. Adding `a`
    to explicit request it to be set for user/group/other bits.
    
    Also use +X which only sets executable bit on folders.
    
    Bug: None
    Test: manually `umask 077; chmod -R go-rx resources` on host and check that cca.py deploy works
    Change-Id: Ibe4a7b6887bf5c9e98941772c9c2deb991a06202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3685935
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010004}

--- ash/webui/camera_app_ui/resources/utils/cca.py
+++ ash/webui/camera_app_ui/resources/utils/cca.py
@@ -155,11 +155,12 @@ def deploy(args):
         # system umask while transferring new files, and since workstation
         # defaults to have file not readable by others, this makes deployed
         # file not readable by Chrome.
-        # Set --chmod=+r,D+x to rsync to fix this, and set --perms so existing
-        # files that might have the wrong permission will have their permission
-        # fixed.
+        # Set --chmod=a+rX to rsync to fix this ('a' so it won't be affected by
+        # local umask, +r for read and +X for executable bit on folder), and
+        # set --perms so existing files that might have the wrong permission
+        # will have their permission fixed.
         '--perms',
-        '--chmod=+r,D+x',
+        '--chmod=a+rX',
         f'{tsc_dir}/',
         f'{args.device}:{CCA_OVERRIDE_PATH}/js/',
     ]
@@ -173,7 +174,7 @@ def deploy(args):
             '--delete',
             '--mkpath',
             '--perms',
-            '--chmod=+r,D+x',
+            '--chmod=a+rX',
             f'{os.path.join(cca_root, dir)}/',
             f'{args.device}:{CCA_OVERRIDE_PATH}/{dir}/',
         ]
