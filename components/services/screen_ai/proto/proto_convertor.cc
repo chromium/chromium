dd96fee2388f1eb4698d1c6aa4b98fd6188ec93c
Nektarios Paisios <nektar@chromium.org>
2022-06-02 16:42:26 +0000

    Serializes an OCR proto buff into an AXTreeUpdate
    R=dtseng@chromium.org, rhalavani@chromium.org
    
    AX-Relnotes: n/a.
    Change-Id: I21441e6079abde87db815a3c092de8f93ea9a6e2
    Bug: 1248380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668973
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010136}

--- components/services/screen_ai/proto/proto_convertor.cc
+++ components/services/screen_ai/proto/proto_convertor.cc
@@ -4,9 +4,19 @@
 
 #include "components/services/screen_ai/proto/proto_convertor.h"
 
-#include <memory>
+#include <stdint.h>
 
-#include "base/containers/contains.h"
+#include <algorithm>
+#include <array>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "base/check_op.h"
+#include "base/notreached.h"
 #include "components/services/screen_ai/proto/chrome_screen_ai.pb.h"
 #include "components/services/screen_ai/proto/dimension.pb.h"
 #include "components/services/screen_ai/proto/view_hierarchy.pb.h"
@@ -15,6 +25,7 @@
 #include "ui/accessibility/ax_enum_util.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
+#include "ui/accessibility/ax_role_properties.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/transform.h"
 
@@ -24,38 +35,50 @@ namespace {
 // accepted.
 // TODO(https://crbug.com/1278249): Add experiment or heuristics to better
 // adjust this threshold.
-const float kScreenAIMinConfidenceThreshold = 0.1;
+constexpr float kScreenAIMinConfidenceThreshold = 0.1f;
 
 // Returns the next valid ID that can be used for identifying `AXNode`s in the
 // accessibility tree.
 ui::AXNodeID GetNextNodeID() {
-  static int next_node_id = 1;
+  static ui::AXNodeID next_node_id{1};
   return next_node_id++;
 }
 
-void SerializePredictedType(
-    const chrome_screen_ai::UIComponent_PredictedType& predicted_type,
+// Returns whether the provided `predicted_type` is:
+// A) set, and
+// B) has a confidence that is above our acceptance threshold.
+bool SerializePredictedType(
+    const chrome_screen_ai::UIComponent::PredictedType& predicted_type,
     ui::AXNodeData& out_data) {
+  DCHECK_EQ(out_data.role, ax::mojom::Role::kUnknown);
+  if (predicted_type.confidence() < 0.0f ||
+      predicted_type.confidence() > 1.0f) {
+    NOTREACHED()
+        << "Unrecognized chrome_screen_ai::PredictedType::confidence value: "
+        << predicted_type.confidence();
+    return false;  // Confidence is out of bounds.
+  }
+  if (predicted_type.confidence() < kScreenAIMinConfidenceThreshold)
+    return false;
   switch (predicted_type.type_of_case()) {
-    case chrome_screen_ai::UIComponent_PredictedType::kEnumType:
-      // TODO(https://crbug.com/1278249): Add tests to ensure these two types
-      // match. Add a PRESUBMIT test that compares the proto and enum.
-      // TODO(accessibility): Why do we even need an enum. Couldn't all
-      // predicted types be strings? We could easily map from a string to an
-      // ax::mojom::Role. Then, we won't need to keep the enums synced.
+    case chrome_screen_ai::UIComponent::PredictedType::kEnumType:
+      // TODO(https://crbug.com/1278249): We do not actually need an enum. All
+      // predicted types could be strings. We could easily map from a string to
+      // an `ax::mojom::Role`. Then, we won't need to keep the enums synced.
       out_data.role = static_cast<ax::mojom::Role>(predicted_type.enum_type());
       break;
-    case chrome_screen_ai::UIComponent_PredictedType::kStringType:
+    case chrome_screen_ai::UIComponent::PredictedType::kStringType:
       out_data.role = ax::mojom::Role::kGenericContainer;
       out_data.AddStringAttribute(ax::mojom::StringAttribute::kRoleDescription,
                                   predicted_type.string_type());
       break;
-    case chrome_screen_ai::UIComponent_PredictedType::TYPE_OF_NOT_SET:
-      // TODO(accessibility): Why is this a possibility if the member in the
-      // proto is not marked optional?
-      NOTREACHED();
-      break;
+    case chrome_screen_ai::UIComponent::PredictedType::TYPE_OF_NOT_SET:
+      NOTREACHED() << "Malformed proto message: Required field "
+                      "`chrome_screen_ai::UIComponent::PredictedType` not set.";
+      return false;
   }
+
+  return true;
 }
 
 void SerializeBoundingBox(const chrome_screen_ai::Rect& bounding_box,
@@ -64,6 +87,9 @@ void SerializeBoundingBox(const chrome_screen_ai::Rect& bounding_box,
   out_data.relative_bounds.bounds =
       gfx::RectF(bounding_box.x(), bounding_box.y(), bounding_box.width(),
                  bounding_box.height());
+  // A negative width or height will result in an empty rect.
+  if (out_data.relative_bounds.bounds.IsEmpty())
+    return;
   if (container_id != ui::kInvalidAXNodeID)
     out_data.relative_bounds.offset_container_id = container_id;
   if (bounding_box.angle()) {
@@ -72,21 +98,199 @@ void SerializeBoundingBox(const chrome_screen_ai::Rect& bounding_box,
   }
 }
 
-absl::optional<ui::AXNodeData> SerializeUIComponent(
-    const chrome_screen_ai::UIComponent& ui_component) {
-  // The score is only used to prune very low confidence detections and we don't
-  // use it in the accessibility tree.
-  if (ui_component.predicted_type().confidence() <
-      kScreenAIMinConfidenceThreshold) {
-    return absl::nullopt;
+void SerializeDirection(const chrome_screen_ai::Orientation& direction,
+                        ui::AXNodeData& out_data) {
+  if (!chrome_screen_ai::Orientation_IsValid(direction)) {
+    NOTREACHED() << "Unrecognized chrome_screen_ai::Direction value: "
+                 << direction;
+    return;
   }
+  // TODO(accessibility): Why is writing direction represented using the
+  // orientation enum whose values are in part non-sensical for use as a writing
+  // direction? E.g., what does `ORIENTATION_ROTATED_HORIZONTAL` mean?
+  switch (direction) {
+    case chrome_screen_ai::Orientation::ORIENTATION_DEFAULT:
+    case chrome_screen_ai::Orientation::ORIENTATION_HORIZONTAL:
+      out_data.AddIntAttribute(
+          ax::mojom::IntAttribute::kTextDirection,
+          static_cast<int32_t>(ax::mojom::WritingDirection::kLtr));
+      break;
+    case chrome_screen_ai::Orientation::ORIENTATION_VERTICAL:
+      out_data.AddIntAttribute(
+          ax::mojom::IntAttribute::kTextDirection,
+          static_cast<int32_t>(ax::mojom::WritingDirection::kTtb));
+      break;
+    case chrome_screen_ai::Orientation::ORIENTATION_ROTATED_HORIZONTAL:
+      out_data.AddIntAttribute(
+          ax::mojom::IntAttribute::kTextDirection,
+          static_cast<int32_t>(ax::mojom::WritingDirection::kRtl));
+      break;
+    case chrome_screen_ai::Orientation::ORIENTATION_ROTATED_VERTICAL:
+      out_data.AddIntAttribute(
+          ax::mojom::IntAttribute::kTextDirection,
+          static_cast<int32_t>(ax::mojom::WritingDirection::kBtt));
+      break;
+    case google::protobuf::kint32min:
+    case google::protobuf::kint32max:
+      // Ordinarily, a default case should have been added to permit future
+      // additions to `chrome_screen_ai::Orientation`. However, in this
+      // case, both the screen_ai library and this code should always be in
+      // sync.
+      NOTREACHED() << "Unrecognized chrome_screen_ai::Direction value: "
+                   << direction;
+      break;
+  }
+}
 
-  ui::AXNodeData node_data;
-  node_data.id = GetNextNodeID();
-  SerializePredictedType(ui_component.predicted_type(), node_data);
-  SerializeBoundingBox(ui_component.bounding_box(),
-                       /* container_id */ ui::kInvalidAXNodeID, node_data);
-  return node_data;
+void SerializeContentType(const chrome_screen_ai::ContentType& content_type,
+                          ui::AXNodeData& out_data) {
+  if (!chrome_screen_ai::ContentType_IsValid(content_type)) {
+    NOTREACHED() << "Unrecognized chrome_screen_ai::ContentType value: "
+                 << content_type;
+    return;
+  }
+  switch (content_type) {
+    case chrome_screen_ai::CONTENT_TYPE_PRINTED_TEXT:
+    case chrome_screen_ai::CONTENT_TYPE_HANDWRITTEN_TEXT:
+      out_data.role = ax::mojom::Role::kStaticText;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_IMAGE:
+      out_data.role = ax::mojom::Role::kImage;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_LINE_DRAWING:
+      out_data.role = ax::mojom::Role::kGraphicsObject;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_SEPARATOR:
+      out_data.role = ax::mojom::Role::kSplitter;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_UNREADABLE_TEXT:
+      out_data.role = ax::mojom::Role::kGraphicsObject;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_FORMULA:
+    case chrome_screen_ai::CONTENT_TYPE_HANDWRITTEN_FORMULA:
+      // Note that `Role::kMath` indicates that the formula is not represented
+      // as a subtree of MathML elements in the accessibility tree, but as a raw
+      // string which may optionally be written in MathML, but could also be
+      // written in plain text.
+      out_data.role = ax::mojom::Role::kMath;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_SIGNATURE:
+      // Signatures may be readable, but even when they are not we could still
+      // try our best.
+      // TODO(accessibility): Explore adding a description attribute informing
+      // the user that this is a signature, e.g. via ARIA Annotations.
+      out_data.role = ax::mojom::Role::kStaticText;
+      break;
+    case chrome_screen_ai::CONTENT_TYPE_UNKNOWN:
+      // This should be "Role::kPresentational" but it has been erroniously
+      // removed from the codebase.
+      // TODO(nektar): Add presentational role back to avoid confusion with the
+      // meaning of kNone vs. kUnknown.
+      out_data.role = ax::mojom::Role::kNone;  // Presentational.
+      break;
+    case google::protobuf::kint32min:
+    case google::protobuf::kint32max:
+      // Ordinarily, a default case should have been added to permit future
+      // additions to `chrome_screen_ai::ContentType`. However, in this
+      // case, both the screen_ai library and this code should always be in
+      // sync.
+      NOTREACHED() << "Unrecognized chrome_screen_ai::ContentType value: "
+                   << content_type;
+      break;
+  }
+}
+
+void SerializeWordBox(const chrome_screen_ai::WordBox& word_box,
+                      const size_t index,
+                      ui::AXNodeData& parent_node,
+                      std::vector<ui::AXNodeData>& node_data) {
+  DCHECK_LT(index, node_data.size());
+  DCHECK_NE(parent_node.id, ui::kInvalidAXNodeID);
+  ui::AXNodeData& word_box_node = node_data[index];
+  DCHECK_EQ(word_box_node.role, ax::mojom::Role::kUnknown);
+  if (word_box.confidence() < 0.0f || word_box.confidence() > 1.0f) {
+    NOTREACHED() << "Unrecognized chrome_screen_ai::WordBox::confidence value: "
+                 << word_box.confidence();
+    return;  // Confidence is out of bounds.
+  }
+  if (word_box.confidence() < kScreenAIMinConfidenceThreshold)
+    return;
+  word_box_node.role = ax::mojom::Role::kInlineTextBox;
+  word_box_node.id = GetNextNodeID();
+  SerializeBoundingBox(word_box.bounding_box(), parent_node.id, word_box_node);
+  // Since the role is `kInlineTextBox`, NameFrom would automatically and
+  // correctly be set to `ax::mojom::NameFrom::kContents`.
+  if (word_box.has_space_after()) {
+    word_box_node.SetName(word_box.utf8_string() + " ");
+  } else {
+    word_box_node.SetName(word_box.utf8_string());
+  }
+  // TODO(nektar): DCHECK that line box's text is equal to the concatenation of
+  // the text found in all contained word boxes.
+  // TODO(nektar): Set character bounding box information.
+  if (word_box.estimate_color_success()) {
+    word_box_node.AddIntAttribute(ax::mojom::IntAttribute::kBackgroundColor,
+                                  word_box.background_rgb_value());
+    word_box_node.AddIntAttribute(ax::mojom::IntAttribute::kColor,
+                                  word_box.foreground_rgb_value());
+  }
+  SerializeDirection(
+      static_cast<chrome_screen_ai::Orientation>(word_box.direction()),
+      word_box_node);
+  parent_node.child_ids.push_back(word_box_node.id);
+}
+
+void SerializeUIComponent(const chrome_screen_ai::UIComponent& ui_component,
+                          const size_t index,
+                          ui::AXNodeData& parent_node,
+                          std::vector<ui::AXNodeData>& node_data) {
+  DCHECK_LT(index, node_data.size());
+  DCHECK_NE(parent_node.id, ui::kInvalidAXNodeID);
+  ui::AXNodeData& current_node = node_data[index];
+  if (SerializePredictedType(ui_component.predicted_type(), current_node))
+    return;
+  current_node.id = GetNextNodeID();
+  SerializeBoundingBox(ui_component.bounding_box(), parent_node.id,
+                       current_node);
+  parent_node.child_ids.push_back(current_node.id);
+}
+
+void SerializeLineBox(const chrome_screen_ai::LineBox& line_box,
+                      const size_t index,
+                      ui::AXNodeData& parent_node,
+                      std::vector<ui::AXNodeData>& node_data) {
+  DCHECK_LT(index, node_data.size());
+  DCHECK_NE(parent_node.id, ui::kInvalidAXNodeID);
+  ui::AXNodeData& line_box_node = node_data[index];
+  DCHECK_EQ(line_box_node.role, ax::mojom::Role::kUnknown);
+  if (line_box.confidence() < 0.0f || line_box.confidence() > 1.0f) {
+    NOTREACHED() << "Unrecognized chrome_screen_ai::LineBox::confidence value: "
+                 << line_box.confidence();
+    return;  // Confidence is out of bounds.
+  }
+  if (line_box.confidence() < kScreenAIMinConfidenceThreshold)
+    return;
+  SerializeContentType(line_box.content_type(), line_box_node);
+  line_box_node.id = GetNextNodeID();
+  if (ui::IsText(line_box_node.role)) {
+    size_t word_node_index = index + 1u;
+    for (const auto& word : line_box.words())
+      SerializeWordBox(word, word_node_index++, line_box_node, node_data);
+  }
+  SerializeBoundingBox(line_box.bounding_box(), parent_node.id, line_box_node);
+  // Since the role is `kStaticText`, NameFrom would automatically and correctly
+  // be set to `ax::mojom::NameFrom::kContents`.
+  line_box_node.SetName(line_box.utf8_string());
+  if (!line_box.language().empty()) {
+    // TODO(nektar): Only set language if different from parent node to
+    // minimize memory usage.
+    line_box_node.AddStringAttribute(ax::mojom::StringAttribute::kLanguage,
+                                     line_box.language());
+  }
+  SerializeDirection(
+      static_cast<chrome_screen_ai::Orientation>(line_box.direction()),
+      line_box_node);
+  parent_node.child_ids.push_back(line_box_node.id);
 }
 
 // Adds the subtree of |nodes[node_index_to_add]| to |nodes_order| with
@@ -113,19 +317,91 @@ ui::AXTreeUpdate ScreenAIVisualAnnotationToAXTreeUpdate(
 
   chrome_screen_ai::VisualAnnotation visual_annotation;
   if (!visual_annotation.ParseFromString(serialized_proto)) {
-    VLOG(1) << "Could not parse Screen AI library output.";
+    NOTREACHED() << "Could not parse Screen AI library output.";
     return update;
   }
 
   // TODO(https://crbug.com/1278249): Create an AXTreeSource and create the
   // update using AXTreeSerializer.
 
-  for (const auto& ui_component : visual_annotation.ui_component()) {
-    absl::optional<ui::AXNodeData> node_data =
-        SerializeUIComponent(ui_component);
-    if (node_data)
-      update.nodes.push_back(*node_data);
+  // Each `UIComponent`, `LineBox`, and `WordBox` will take up one node in the
+  // accessibility tree, resulting in hundreds of nodes, making it inefficient
+  // to push_back one node at a time. We pre-allocate the needed nodes making
+  // node creation an O(n) operation.
+  const size_t word_count = std::accumulate(
+      std::begin(visual_annotation.lines()),
+      std::end(visual_annotation.lines()), 0u,
+      [](const size_t& count, const chrome_screen_ai::LineBox& line_box) {
+        return count + line_box.words().size();
+      });
+
+  // Each unique `chrome_screen_ai::LineBox::block_id` creates a new
+  // paragraph, each paragraph is placed in its correct reading order,
+  // and each paragraph has a sorted set of line boxes. Line boxes are sorted
+  // using their `chrome_screen_ai::LineBox::order_within_block` member and they
+  // are identified by their index in the container of line boxes. Use std::map
+  // to sort both paragraphs and lines, both operations having an O(n * log(n))
+  // complexity.
+  // TODO(accessibility): Determine reading order based on visual positioning of
+  // paragraphs, not on their block IDs.
+  std::map<int32_t, std::map<int32_t, int>> blocks_to_lines_map;
+  for (int i = 0; i < visual_annotation.lines_size(); ++i) {
+    const chrome_screen_ai::LineBox& line = visual_annotation.lines(i);
+    blocks_to_lines_map[line.block_id()].emplace(
+        std::make_pair(line.order_within_block(), i));
   }
+
+  size_t rootnodes_count = 0u;
+  if (!visual_annotation.ui_component().empty())
+    ++rootnodes_count;
+  if (!visual_annotation.lines().empty())
+    ++rootnodes_count;
+
+  std::vector<ui::AXNodeData> nodes(
+      rootnodes_count + visual_annotation.ui_component().size() +
+      blocks_to_lines_map.size() + visual_annotation.lines().size() +
+      word_count);
+  size_t index = 0u;
+
+  if (!visual_annotation.ui_component().empty()) {
+    ui::AXNodeData& rootnode = nodes[index++];
+    rootnode.role = ax::mojom::Role::kDialog;
+    rootnode.id = GetNextNodeID();
+    // TODO(nektar): Set the bounding box of `rootnode` to the bounding box of
+    // the screen snapshot.
+    for (const auto& ui_component : visual_annotation.ui_component())
+      SerializeUIComponent(ui_component, index++, rootnode, nodes);
+  }
+
+  if (!visual_annotation.lines().empty()) {
+    // We assume that OCR is performed on a page-by-page basis.
+    ui::AXNodeData& page_node = nodes[index++];
+    page_node.role = ax::mojom::Role::kRegion;
+    page_node.id = GetNextNodeID();
+    page_node.AddBoolAttribute(ax::mojom::BoolAttribute::kIsPageBreakingObject,
+                               true);
+    // TODO(nektar): Set the bounding box of `page_node` to the bounding box of
+    // the captured image.
+    for (const auto& block_to_lines_pair : blocks_to_lines_map) {
+      for (const auto& line_sequence_number_to_index_pair :
+           block_to_lines_pair.second) {
+        const chrome_screen_ai::LineBox& line_box =
+            visual_annotation.lines(line_sequence_number_to_index_pair.second);
+        SerializeLineBox(line_box, index++, page_node, nodes);
+        index += line_box.words().size();
+      }
+    }
+  }
+
+  // Filter out invalid / unrecognized / unused nodes from the update.
+  update.nodes.resize(nodes.size());
+  auto end_node_iter =
+      std::copy_if(std::begin(nodes), std::end(nodes), std::begin(update.nodes),
+                   [](const ui::AXNodeData& node_data) {
+                     return node_data.role != ax::mojom::Role::kUnknown &&
+                            node_data.id != ui::kInvalidAXNodeID;
+                   });
+  update.nodes.resize(std::distance(std::begin(update.nodes), end_node_iter));
 
   // TODO(https://crbug.com/1278249): Add UMA metrics to record the number of
   // annotations, item types, confidence levels, etc.
@@ -179,8 +455,7 @@ std::string Screen2xSnapshotToViewHierarchy(const ui::AXTreeUpdate& snapshot) {
 
   for (int node_index : nodes_order) {
     const ui::AXNodeData& node = snapshot.nodes[node_index];
-    int ax_node_id = static_cast<int>(node.id);
-
+    const ui::AXNodeID& ax_node_id = node.id;
     screenai::UiElement* uie = view_hierarchy.add_ui_elements();
     screenai::UiElementAttribute* attrib = nullptr;
 
--- components/services/screen_ai/proto/proto_convertor_unittest.cc
+++ components/services/screen_ai/proto/proto_convertor_unittest.cc
@@ -3,6 +3,10 @@
 // found in the LICENSE file.
 
 #include "components/services/screen_ai/proto/proto_convertor.h"
+
+#include <string>
+
+#include "components/services/screen_ai/proto/chrome_screen_ai.pb.h"
 #include "components/services/screen_ai/proto/view_hierarchy.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -10,14 +14,14 @@
 
 namespace {
 
-const int kMaxChildInTemplate = 3;
+constexpr int kMaxChildInTemplate = 3;
 
 // A dummy tree node definition.
-typedef struct {
-  int node_id;
+struct NodeTemplate {
+  ui::AXNodeID node_id;
   int child_count;
-  int child_ids[kMaxChildInTemplate];
-} NodeTemplate;
+  ui::AXNodeID child_ids[kMaxChildInTemplate];
+};
 
 ui::AXTreeUpdate CreateAXTreeUpdateFromTemplate(int root_id,
                                                 NodeTemplate* nodes_template,
@@ -49,6 +53,140 @@ namespace screen_ai {
 
 using ProtoConvertorTest = testing::Test;
 
+TEST_F(ProtoConvertorTest, ScreenAIVisualAnnotationToAXTreeUpdate) {
+  chrome_screen_ai::VisualAnnotation annotation;
+
+  {
+    chrome_screen_ai::UIComponent* component_0 = annotation.add_ui_component();
+    chrome_screen_ai::UIComponent::PredictedType* type_0 =
+        component_0->mutable_predicted_type();
+    type_0->set_enum_type(chrome_screen_ai::UIComponent::BUTTON);
+    type_0->set_confidence(0.8f);
+    chrome_screen_ai::Rect* box_0 = component_0->mutable_bounding_box();
+    box_0->set_x(0);
+    box_0->set_y(1);
+    box_0->set_width(2);
+    box_0->set_height(3);
+    box_0->set_angle(90.0f);
+
+    chrome_screen_ai::UIComponent* component_1 = annotation.add_ui_component();
+    chrome_screen_ai::UIComponent::PredictedType* type_1 =
+        component_1->mutable_predicted_type();
+    type_1->set_string_type("Presentational");
+    // If the confidence is low, this component together with all its fields
+    // should be ignored.
+    type_1->set_confidence(0.05f);
+    chrome_screen_ai::Rect* box_1 = component_1->mutable_bounding_box();
+    box_1->set_x(0);
+    box_1->set_y(0);
+    box_1->set_width(5);
+    box_1->set_height(5);
+
+    chrome_screen_ai::UIComponent* component_2 = annotation.add_ui_component();
+    chrome_screen_ai::UIComponent::PredictedType* type_2 =
+        component_2->mutable_predicted_type();
+    type_2->set_string_type("Signature");
+    type_2->set_confidence(0.6f);
+    chrome_screen_ai::Rect* box_2 = component_2->mutable_bounding_box();
+    // `x`, `y`, and `angle` should be defaulted to 0 since they are singular
+    // proto3 fields, not proto2.
+    box_2->set_width(5);
+    box_2->set_height(5);
+  }
+
+  {
+    std::string serialized_annotation;
+    ASSERT_TRUE(annotation.SerializeToString(&serialized_annotation));
+    const ui::AXTreeUpdate update =
+        ScreenAIVisualAnnotationToAXTreeUpdate(serialized_annotation);
+
+    const std::string expected_update(
+        "id=1 dialog (0, 0)-(0, 0) child_ids=2\n");
+    EXPECT_EQ(expected_update, update.ToString());
+  }
+
+  {
+    chrome_screen_ai::LineBox* line_0 = annotation.add_lines();
+
+    chrome_screen_ai::WordBox* word_0_0 = line_0->add_words();
+    chrome_screen_ai::Rect* box_0_0 = word_0_0->mutable_bounding_box();
+    box_0_0->set_x(100);
+    box_0_0->set_y(100);
+    box_0_0->set_width(250);
+    box_0_0->set_height(20);
+    word_0_0->set_utf8_string("Hello");
+    word_0_0->set_has_space_after(true);
+    word_0_0->set_confidence(0.9f);
+    word_0_0->set_estimate_color_success(true);
+    word_0_0->set_background_rgb_value(50000);
+    word_0_0->set_foreground_rgb_value(25000);
+
+    chrome_screen_ai::WordBox* word_0_1 = line_0->add_words();
+    chrome_screen_ai::Rect* box_0_1 = word_0_1->mutable_bounding_box();
+    box_0_1->set_x(350);
+    box_0_1->set_y(100);
+    box_0_1->set_width(250);
+    box_0_1->set_height(20);
+    word_0_1->set_utf8_string("world");
+    // `word_0_1.has_space_after()` should be defaulted to false.
+    word_0_1->set_confidence(0.9f);
+    word_0_1->set_estimate_color_success(true);
+    word_0_1->set_background_rgb_value(50000);
+    word_0_1->set_foreground_rgb_value(25000);
+
+    chrome_screen_ai::Rect* box_0 = line_0->mutable_bounding_box();
+    box_0->set_x(100);
+    box_0->set_y(100);
+    box_0->set_width(500);
+    box_0->set_height(20);
+    line_0->set_utf8_string("Hello world");
+    line_0->set_confidence(0.9f);
+    line_0->set_language("en");
+    line_0->set_block_id(2);
+    line_0->set_order_within_block(1);
+
+    chrome_screen_ai::LineBox* line_1 = annotation.add_lines();
+    line_1->set_confidence(0.0f);
+    // Language, and the line as a whole,  should be ignored since the
+    // confidence is zero.
+    line_1->set_language("en");
+    line_1->set_block_id(1);
+    line_1->set_order_within_block(0);
+
+    chrome_screen_ai::LineBox* line_2 = annotation.add_lines();
+    line_2->set_confidence(0.7f);
+    chrome_screen_ai::Rect* box_2 = line_2->mutable_bounding_box();
+    // No bounding box should be created in the AXTree because the height is -5.
+    box_2->set_width(5);
+    box_2->set_height(-5);
+    line_2->set_block_id(2);
+    line_2->set_order_within_block(0);
+  }
+
+  {
+    std::string serialized_annotation;
+    ASSERT_TRUE(annotation.SerializeToString(&serialized_annotation));
+    const ui::AXTreeUpdate update =
+        ScreenAIVisualAnnotationToAXTreeUpdate(serialized_annotation);
+
+    const std::string expected_update(
+        "id=3 dialog (0, 0)-(0, 0) child_ids=4\n"
+        "id=5 region (0, 0)-(0, 0) is_page_breaking_object=true child_ids=6,7\n"
+        "  id=6 staticText (0, 0)-(5, 0) name_from=contents text_direction=ltr "
+        "name=\n"
+        "  id=7 staticText offset_container_id=5 (100, 100)-(500, 20) "
+        "name_from=contents text_direction=ltr name=Hello world language=en "
+        "child_ids=8,9\n"
+        "    id=8 inlineTextBox offset_container_id=7 (100, 100)-(250, 20) "
+        "name_from=contents background_color=&C350 color=&61A8 "
+        "text_direction=ltr name=Hello \n"
+        "    id=9 inlineTextBox offset_container_id=7 (350, 100)-(250, 20) "
+        "name_from=contents background_color=&C350 color=&61A8 "
+        "text_direction=ltr name=world\n");
+    EXPECT_EQ(expected_update, update.ToString());
+  }
+}
+
 // Tests if the given tree is properly traversed and new ids are assigned.
 TEST_F(ProtoConvertorTest, PreOrderTreeGeneration) {
   // Input Tree:
@@ -78,7 +216,7 @@ TEST_F(ProtoConvertorTest, PreOrderTreeGeneration) {
       CreateAXTreeUpdateFromTemplate(1, input_tree, nodes_count);
   std::string serialized_proto = Screen2xSnapshotToViewHierarchy(tree_update);
   screenai::ViewHierarchy view_hierarchy;
-  EXPECT_TRUE(view_hierarchy.ParseFromString(serialized_proto));
+  ASSERT_TRUE(view_hierarchy.ParseFromString(serialized_proto));
 
   // Verify.
   EXPECT_EQ(view_hierarchy.ui_elements().size(), nodes_count);
