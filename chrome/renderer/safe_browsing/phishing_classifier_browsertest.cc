e83e8ecea6f5ee13bc2a8614bb089ba1d810408b
Daniel Rubery <drubery@chromium.org>
2022-06-02 21:46:32 +0000

    Add browser_test for CSPP TfLite classification
    This CL checks in two real CSPP models so that we can get full test
    coverage on how these models perform. It then runs basic classifications
    with each of them.
    
    Change-Id: Ia9b3fb2f5cacff182c4e03e492836a18cfb1036b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3658910
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010300}

--- chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
@@ -9,9 +9,11 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/test_discardable_memory_allocator.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/renderer/chrome_content_renderer_client.h"
 #include "chrome/test/base/chrome_render_view_test.h"
 #include "components/safe_browsing/buildflags.h"
@@ -48,8 +50,9 @@ class TestChromeContentRendererClient : public ChromeContentRendererClient {
   }
 };
 
-class PhishingClassifierTest : public ChromeRenderViewTest,
-                               public ::testing::WithParamInterface<bool> {
+class PhishingClassifierTest
+    : public ChromeRenderViewTest,
+      public ::testing::WithParamInterface<std::tuple<bool, bool>> {
  protected:
   PhishingClassifierTest()
       : url_tld_token_net_(features::kUrlTldToken + std::string("net")),
@@ -61,17 +64,19 @@ class PhishingClassifierTest : public ChromeRenderViewTest,
 
   void SetUp() override {
     ChromeRenderViewTest::SetUp();
-    if (GetParam()) {
-      PrepareModel();
+    bool is_protobuf, use_tflite;
+    std::tie(is_protobuf, use_tflite) = GetParam();
+    if (is_protobuf) {
+      PrepareModel(use_tflite);
     } else {
-      PrepareFlatModel();
+      PrepareFlatModel(use_tflite);
     }
     SetUpClassifier();
 
     base::DiscardableMemoryAllocator::SetInstance(&test_allocator_);
   }
 
-  void PrepareFlatModel() {
+  void PrepareFlatModel(bool use_tflite) {
     flatbuffers::FlatBufferBuilder builder(1024);
     std::vector<flatbuffers::Offset<flat::Hash>> hashes;
     // Make sure this is sorted.
@@ -130,10 +135,43 @@ class PhishingClassifierTest : public ChromeRenderViewTest,
         builder.CreateVector(page_words_vector);
 
     std::vector<flatbuffers::Offset<flat::TfLiteModelMetadata_::Threshold>>
-        thresholds_vector = {};
+        thresholds_vector;
+    if (use_tflite) {
+      thresholds_vector = {
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "502fd246eb6fad3eae0387c54e4ebe74", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "7c4065b088444b37d273872b771e6940", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "712036bd72bf185a2a4f88de9141d02d", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "9e9c15bfa7cb3f8699e2271116a4175c", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "6c2cb3f559e7a03f37dd873fc007dc65", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "1cbeb74661a5e7e05c993f2524781611", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "989790016b6adca9d46b9c8ec6b8fe3a", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "501067590331ca2d243c669e6084c47e", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "40aed7e33c100058e54c73af3ed49524", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "62f53ea23c7ad2590db711235a45fd38", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "ee6fb9baa44f192bc3c53d8d3c6f7a3d", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "ea54b0830d871286e2b4023bbb431710", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "25645a55b844f970337218ea8f1f26b7", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "c9a8640be09f97f170f1a2708058c48f", 2.0),
+          flat::TfLiteModelMetadata_::CreateThresholdDirect(
+              builder, "953255ea26aa8578d06593ff33e99298", 2.0)};
+    }
     flatbuffers::Offset<flat::TfLiteModelMetadata> tflite_metadata_flat =
-        flat::CreateTfLiteModelMetadataDirect(builder, 0, &thresholds_vector, 0,
-                                              0);
+        flat::CreateTfLiteModelMetadataDirect(builder, 0, &thresholds_vector,
+                                              48, 48);
 
     flat::ClientSideModelBuilder csd_model_builder(builder);
     csd_model_builder.add_hashes(hashes_flat);
@@ -155,11 +193,18 @@ class PhishingClassifierTest : public ChromeRenderViewTest,
     ASSERT_TRUE(mapped_region_.IsValid());
     memcpy(mapped_region_.mapping.memory(), model_str.data(),
            model_str.length());
+    base::File tflite_model;
+    if (use_tflite) {
+      base::FilePath tflite_path;
+      GetTfliteModelPath(&tflite_path),
+          tflite_model = base::File(
+              tflite_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
+    }
     ScorerStorage::GetInstance()->SetScorer(FlatBufferModelScorer::Create(
-        mapped_region_.region.Duplicate(), base::File()));
+        mapped_region_.region.Duplicate(), std::move(tflite_model)));
   }
 
-  void PrepareModel() {
+  void PrepareModel(bool use_tflite) {
     // Construct a model to test with.  We include one feature from each of
     // the feature extractors, which allows us to verify that they all ran.
     ClientSideModel model;
@@ -196,8 +241,52 @@ class PhishingClassifierTest : public ChromeRenderViewTest,
     model.set_max_shingles_per_page(100);
     model.set_shingle_size(3);
 
-    ScorerStorage::GetInstance()->SetScorer(
-        ProtobufModelScorer::Create(model.SerializeAsString(), base::File()));
+    base::File tflite_model;
+    if (use_tflite) {
+      base::FilePath tflite_path;
+      GetTfliteModelPath(&tflite_path),
+          tflite_model = base::File(
+              tflite_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
+
+      model.mutable_tflite_metadata()->set_input_width(48);
+      model.mutable_tflite_metadata()->set_input_height(48);
+
+      std::vector<std::pair<std::string, double>> thresholds{
+          {"502fd246eb6fad3eae0387c54e4ebe74", 2.0},
+          {"7c4065b088444b37d273872b771e6940", 2.0},
+          {"712036bd72bf185a2a4f88de9141d02d", 2.0},
+          {"9e9c15bfa7cb3f8699e2271116a4175c", 2.0},
+          {"6c2cb3f559e7a03f37dd873fc007dc65", 2.0},
+          {"1cbeb74661a5e7e05c993f2524781611", 2.0},
+          {"989790016b6adca9d46b9c8ec6b8fe3a", 2.0},
+          {"501067590331ca2d243c669e6084c47e", 2.0},
+          {"40aed7e33c100058e54c73af3ed49524", 2.0},
+          {"62f53ea23c7ad2590db711235a45fd38", 2.0},
+          {"ee6fb9baa44f192bc3c53d8d3c6f7a3d", 2.0},
+          {"ea54b0830d871286e2b4023bbb431710", 2.0},
+          {"25645a55b844f970337218ea8f1f26b7", 2.0},
+          {"c9a8640be09f97f170f1a2708058c48f", 2.0},
+          {"953255ea26aa8578d06593ff33e99298", 2.0}};
+      for (const auto& label_and_threshold : thresholds) {
+        TfLiteModelMetadata::Threshold* threshold =
+            model.mutable_tflite_metadata()->add_thresholds();
+        threshold->set_label(label_and_threshold.first);
+        threshold->set_threshold(label_and_threshold.second);
+      }
+    }
+    ScorerStorage::GetInstance()->SetScorer(ProtobufModelScorer::Create(
+        model.SerializeAsString(), std::move(tflite_model)));
+  }
+
+  void GetTfliteModelPath(base::FilePath* path) {
+    ASSERT_TRUE(base::PathService::Get(chrome::DIR_TEST_DATA, path));
+#if BUILDFLAG(IS_ANDROID)
+    *path = path->AppendASCII("safe_browsing")
+                .AppendASCII("visual_model_android.tflite");
+#else
+    *path = path->AppendASCII("safe_browsing")
+                .AppendASCII("visual_model_desktop.tflite");
+#endif
   }
 
   void SetUpClassifier() {
@@ -359,6 +448,7 @@ TEST_P(PhishingClassifierTest, TestPhishingPagesAreDomMatches) {
       "<html><body><a href=\"http://phishing.com/\">login</a></body></html>");
   RunPhishingClassifier(&page_text_);
 
+  EXPECT_NE(PhishingClassifier::kInvalidScore, phishy_score_);
   EXPECT_TRUE(is_phishing_);
   EXPECT_TRUE(is_dom_match_);
 }
@@ -368,13 +458,15 @@ TEST_P(PhishingClassifierTest, TestSafePagesAreNotDomMatches) {
            "<html><body><a href=\"http://safe.com/\">login</a></body></html>");
   RunPhishingClassifier(&page_text_);
 
+  EXPECT_NE(PhishingClassifier::kInvalidScore, phishy_score_);
   EXPECT_FALSE(is_phishing_);
   EXPECT_FALSE(is_dom_match_);
 }
 
 INSTANTIATE_TEST_SUITE_P(CSDModelTypes,
                          PhishingClassifierTest,
-                         ::testing::Bool());
+                         ::testing::Combine(::testing::Bool(),
+                                            ::testing::Bool()));
 
 // TODO(jialiul): Add test to verify that classification only starts on GET
 // method. It seems there is no easy way to simulate a HTTP POST in
