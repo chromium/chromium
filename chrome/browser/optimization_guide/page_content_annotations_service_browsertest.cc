bb58e23722539eb31d1a4616ec5b73c5517e84c0
Robert Ogden <robertogden@chromium.org>
2022-06-02 21:48:14 +0000

    Enable PageTopics browsertest on disabled platforms
    This should have been done in https://crrev.com/c/3537744, oops.
    
    Fixed: 1307251
    Bug: 1307251
    Cq-Include-Trybots: luci.chromium.try:mac11-arm64-rel
    Cq-Include-Trybots: luci.chromium.try:win7-rel
    Change-Id: I9b8b3f03be647c59f07cd00486b28e5a28fbad24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687837
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010301}

--- chrome/browser/optimization_guide/page_content_annotations_service_browsertest.cc
+++ chrome/browser/optimization_guide/page_content_annotations_service_browsertest.cc
@@ -274,14 +274,8 @@ class PageContentAnnotationsServicePageTopicsBrowserTest
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-// TODO(crbug.com/1307251): Failing on Win 7 and also on Mac 11.
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
-#define MAYBE_E2EWithGoldenTestData DISABLED_E2EWithGoldenTestData
-#else
-#define MAYBE_E2EWithGoldenTestData E2EWithGoldenTestData
-#endif
 IN_PROC_BROWSER_TEST_F(PageContentAnnotationsServicePageTopicsBrowserTest,
-                       MAYBE_E2EWithGoldenTestData) {
+                       E2EWithGoldenTestData) {
   PageContentAnnotationsService* service =
       PageContentAnnotationsServiceFactory::GetForProfile(browser()->profile());
   ASSERT_TRUE(service);
