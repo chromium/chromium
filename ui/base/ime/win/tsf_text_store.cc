78b86cae08358efb5f314f6234f0c9f7af15bb6f
Siye Liu <siliu@microsoft.com>
2022-06-02 20:06:32 +0000

    Set selection range after committed composition only for non-IME input
    Some voice input tools (Windows voice typing, Windows voice access) may
    set selection range after committed composition. However, IMEs may rely
    on selection state to work. The original CL set selection regardless of
    input method which caused regression in Korean IME. The fix for this
    issue is to only scope the original CL to non-IME input.
    
    Ideally, we should use |GUID_TFCAT_TIP_SPEECH| to determine if current input method is voice typing/voice access, however, |ITfInputProcessorProfileMgr::GetActiveProfile| only supports |GUID_TFCAT_TIP_KEYBOARD| according to https://docs.microsoft.com/en-us/windows/win32/api/msctf/nf-msctf-itfinputprocessorprofilemgr-getactiveprofile. For now, we use the API to determine if current input methos is IME.
    
    Original CL: crrev.com/971181
    
    Bug: 1329857
    Change-Id: Idc7c43d5b807c3ccd65381b19f8d6f18617adca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3682653
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010245}

--- ui/base/ime/win/tsf_text_store.cc
+++ ui/base/ime/win/tsf_text_store.cc
@@ -68,6 +68,14 @@ HRESULT TSFTextStore::Initialize() {
     return hr;
   }
 
+  hr = ::CoCreateInstance(CLSID_TF_InputProcessorProfiles, nullptr,
+                          CLSCTX_INPROC_SERVER,
+                          IID_PPV_ARGS(&input_processor_profile_mgr_));
+  if (FAILED(hr)) {
+    DVLOG(1) << "Failed to initialize InputProcessorProfileMgr.";
+    return hr;
+  }
+
   return S_OK;
 }
 
@@ -636,8 +644,10 @@ HRESULT TSFTextStore::RequestLock(DWORD lock_flags, HRESULT* result) {
         is_tic_write_in_progress_ = false;
       } else {
         composition_start_ = selection_.start();
-        if (!selection_.EqualsIgnoringDirection(selection_from_client_))
+        if (!selection_.EqualsIgnoringDirection(selection_from_client_) &&
+            !IsInputIME()) {
           text_input_client_->SetEditableSelectionRange(selection_);
+        }
         CalculateTextandSelectionDiffAndNotifyIfNeeded();
       }
       ResetCacheAfterEditSession();
@@ -1480,7 +1490,7 @@ void TSFTextStore::CommitTextAndEndCompositionIfAny(size_t old_size,
     text_input_client_->ClearCompositionText();
   }
 
-  if (!selection_.is_empty() && !is_selection_interim_char_ &&
+  if (!selection_.is_empty() && !IsInputIME() &&
       selection_.GetMax() <= string_buffer_document_.size()) {
     text_input_client_->SetEditableSelectionRange(selection_);
   }
@@ -1582,10 +1592,6 @@ void TSFTextStore::GetStyle(const TF_DISPLAYATTRIBUTE& attribute,
 void TSFTextStore::ResetCacheAfterEditSession() {
   // reset the flag since we've already inserted/replaced the text.
   new_text_inserted_ = false;
-  if (is_selection_interim_char_) {
-    // Need to reset |selection_| for interim char selection.
-    selection_ = gfx::Range(selection_.GetMax());
-  }
   is_selection_interim_char_ = false;
   // reset |on_start_composition_called_| for next edit session.
   on_start_composition_called_ = false;
@@ -1593,6 +1599,16 @@ void TSFTextStore::ResetCacheAfterEditSession() {
   // reset string_buffer_ if composition is no longer active.
   if (text_input_client_ && !text_input_client_->HasCompositionText())
     string_pending_insertion_.clear();
+}
+
+bool TSFTextStore::IsInputIME() const {
+  TF_INPUTPROCESSORPROFILE profile;
+  if (SUCCEEDED(input_processor_profile_mgr_->GetActiveProfile(
+          GUID_TFCAT_TIP_KEYBOARD, &profile))) {
+    return profile.hkl == NULL &&
+           profile.dwProfileType == TF_PROFILETYPE_INPUTPROCESSOR;
+  }
+  return false;
 }
 
 }  // namespace ui
