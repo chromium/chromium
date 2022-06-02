2f89efb28ce5bcb1bae1c1238df0d6c2216a616f
Victor Vasiliev <vasilvv@chromium.org>
2022-06-02 20:58:31 +0000

    Remove QuicHttpProxyBackend
    I have no evidence this is currently used (I never received any reply to
    <https://bugs.chromium.org/p/chromium/issues/detail?id=1316459#c4>), and
    the tests are flaky.
    
    Anyone still relying on this working can use
    <https://www.envoyproxy.io/> instead, which uses same QUIC library and
    is actively supported.
    
    Bug: 1316459
    Change-Id: Ifa5e5c0f03e313dab4f82be04c44f6107861aa55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686481
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010271}

--- net/tools/quic/quic_simple_server_backend_factory.cc
+++ net/tools/quic/quic_simple_server_backend_factory.cc
@@ -5,43 +5,13 @@
 #include "net/tools/quic/quic_simple_server_backend_factory.h"
 #include "net/third_party/quiche/src/quiche/common/platform/api/quiche_command_line_flags.h"
 #include "net/third_party/quiche/src/quiche/quic/platform/api/quic_flags.h"
-#include "net/tools/quic/quic_http_proxy_backend_stream.h"
-
-DEFINE_QUICHE_COMMAND_LINE_FLAG(
-    std::string,
-    quic_mode,
-    "cache",
-    "Specifies the mode for the server to operate in. Either "
-    "'cache' or 'proxy'");
-
-DEFINE_QUICHE_COMMAND_LINE_FLAG(
-    std::string,
-    quic_proxy_backend_url,
-    "",
-    "URL with http/https, IP address or host name and the port number of the "
-    "backend server.");
 
 namespace net {
 
 std::unique_ptr<quic::QuicSimpleServerBackend>
 QuicSimpleServerBackendFactory::CreateBackend() {
-  if (GetQuicFlag(FLAGS_quic_mode) == "cache") {
-    quic::QuicToyServer::MemoryCacheBackendFactory backend_factory;
-    return backend_factory.CreateBackend();
-  }
-
-  if (GetQuicFlag(FLAGS_quic_mode) != "proxy") {
-    LOG(ERROR) << "unknown --mode. cache or proxy are valid mode of operation";
-    return nullptr;
-  }
-
-  auto backend = std::make_unique<net::QuicHttpProxyBackend>();
-  std::string url = GetQuicFlag(FLAGS_quic_proxy_backend_url);
-  if (!url.empty() && !backend->InitializeBackend(url)) {
-    LOG(ERROR) << "--quic_proxy_backend_url " << url << " is not valid !";
-    return nullptr;
-  }
-  return backend;
+  quic::QuicToyServer::MemoryCacheBackendFactory backend_factory;
+  return backend_factory.CreateBackend();
 }
 
 }  // namespace net
