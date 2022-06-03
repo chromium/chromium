5c0cbcad2fef152161e68297df6d51698535dcea
Saifuddin Hitawala <hitawala@chromium.org>
2022-06-03 04:17:43 +0000

    WrappedSkImage - Unify support check between GL/Vulkan
    WrappedSkImage earlier checked for usage support based on whether
    SkiaRenderer was used with GL or Vulkan. Now, we don't have a special
    path for GL anymore and that has been removed as part of CopySubTexture
    for GL removal in RasterDecoder. As a result, GL and Vulkan work the
    same and so we unify it here in the IsSupported check.
    
    Also, fix the RasterDecoder unit tests which earlier tested with
    SharedImageGLRepresentation instead of SharedImageSkiaRepresentation.
    
    Change-Id: Ib08d1a776877209b151a0f234e3535d57d6a1794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3647740
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010436}

--- gpu/command_buffer/service/raster_decoder_unittest.cc
+++ gpu/command_buffer/service/raster_decoder_unittest.cc
@@ -239,6 +239,11 @@ class RasterDecoderOOPTest : public testing::Test, DecoderClient {
     client_texture_mailbox_ =
         CreateMailbox(viz::ResourceFormat::RGBA_8888, /*width=*/2,
                       /*height=*/2, /*cleared=*/false);
+
+    // When creating the mailbox, we create a WrappedSkImage shared image which
+    // sets this flag to true. Some tests expect this flag to be false when
+    // testing so we reset it back here to false.
+    context_state_->set_need_context_state_reset(/*reset=*/false);
   }
   void TearDown() override {
     context_state_->MakeCurrent(nullptr);
@@ -301,7 +306,8 @@ class RasterDecoderOOPTest : public testing::Test, DecoderClient {
     if (cleared) {
       SharedImageRepresentationFactory repr_factory(shared_image_manager(),
                                                     nullptr);
-      auto representation = repr_factory.ProduceGLTexture(mailbox);
+      auto representation =
+          repr_factory.ProduceSkia(mailbox, context_state_.get());
       representation->SetCleared();
     }
 
@@ -382,8 +388,8 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DSizeMismatch) {
 
   SharedImageRepresentationFactory repr_factory(shared_image_manager(),
                                                 nullptr);
-  auto representation = repr_factory.ProduceGLTexture(client_texture_mailbox_);
-  gles2::Texture* dest_texture = representation->GetTexture();
+  auto representation =
+      repr_factory.ProduceSkia(client_texture_mailbox_, context_state_.get());
 
   {
     // This will initialize the bottom right corner of destination.
@@ -391,8 +397,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DSizeMismatch) {
     cmd.Init(1, 1, 0, 0, 1, 1, false, mailboxes);
     EXPECT_EQ(error::kNoError, ExecuteImmediateCmd(cmd, sizeof(mailboxes)));
     EXPECT_EQ(GL_NO_ERROR, GetGLError());
-    EXPECT_EQ(dest_texture->GetLevelClearedRect(GL_TEXTURE_2D, 0),
-              gfx::Rect(1, 1, 1, 1));
+    EXPECT_EQ(representation->ClearedRect(), gfx::Rect(1, 1, 1, 1));
   }
 
   {
@@ -401,8 +406,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DSizeMismatch) {
     cmd.Init(2, 2, 0, 0, 1, 1, false, mailboxes);
     EXPECT_EQ(error::kNoError, ExecuteImmediateCmd(cmd, sizeof(mailboxes)));
     EXPECT_EQ(GL_INVALID_VALUE, GetGLError());
-    EXPECT_EQ(dest_texture->GetLevelClearedRect(GL_TEXTURE_2D, 0),
-              gfx::Rect(1, 1, 1, 1));
+    EXPECT_EQ(representation->ClearedRect(), gfx::Rect(1, 1, 1, 1));
   }
 
   {
@@ -411,8 +415,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DSizeMismatch) {
     cmd.Init(0, 0, 0, 0, 2, 2, false, mailboxes);
     EXPECT_EQ(error::kNoError, ExecuteImmediateCmd(cmd, sizeof(mailboxes)));
     EXPECT_EQ(GL_INVALID_VALUE, GetGLError());
-    EXPECT_EQ(dest_texture->GetLevelClearedRect(GL_TEXTURE_2D, 0),
-              gfx::Rect(1, 1, 1, 1));
+    EXPECT_EQ(representation->ClearedRect(), gfx::Rect(1, 1, 1, 1));
   }
 }
 
@@ -428,7 +431,8 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DTwiceClearsUnclearedTexture) {
 
   SharedImageRepresentationFactory repr_factory(shared_image_manager(),
                                                 nullptr);
-  auto representation = repr_factory.ProduceGLTexture(client_texture_mailbox_);
+  auto representation =
+      repr_factory.ProduceSkia(client_texture_mailbox_, context_state_.get());
   EXPECT_FALSE(representation->IsCleared());
 
   // This will initialize the top half of destination.
@@ -464,7 +468,8 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DPartialFailsWithUnalignedRect) {
 
   SharedImageRepresentationFactory repr_factory(shared_image_manager(),
                                                 nullptr);
-  auto representation = repr_factory.ProduceGLTexture(client_texture_mailbox_);
+  auto representation =
+      repr_factory.ProduceSkia(client_texture_mailbox_, context_state_.get());
   EXPECT_FALSE(representation->IsCleared());
 
   // This will initialize the top half of destination.
