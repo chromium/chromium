4c2fc063ac159d3020b78e9d14fd2285f304c8b0
Matt Menke <mmenke@chromium.org>
2022-06-03 19:24:45 +0000

    ui/base/test/skia_gold_pixel_diff.cc: Remove use of Value::DictStorage
    Value::DictStorage is deprecated. Value::Dict should be used instead.
    
    Bug: 1187001
    Change-Id: Ia8b4f94330002cb8c41ffc04b70ab03f66aeddf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686587
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1010697}

--- ui/base/test/skia_gold_pixel_diff.cc
+++ ui/base/test/skia_gold_pixel_diff.cc
@@ -83,7 +83,7 @@ void AppendArgsJustAfterProgram(base::CommandLine& cmd,
   argv.insert(argv.begin() + 1, args.begin(), args.end());
 }
 
-void FillInSystemEnvironment(base::Value::DictStorage& ds) {
+void FillInSystemEnvironment(base::Value::Dict& ds) {
   std::string processor = "unknown";
 #if defined(ARCH_CPU_X86)
   processor = "x86";
@@ -93,8 +93,8 @@ void FillInSystemEnvironment(base::Value::DictStorage& ds) {
   LOG(WARNING) << "Unknown Processor.";
 #endif
 
-  ds["system"] = base::Value(SkiaGoldPixelDiff::GetPlatform());
-  ds["processor"] = base::Value(processor);
+  ds.Set("system", SkiaGoldPixelDiff::GetPlatform());
+  ds.Set("processor", processor);
 }
 
 // Fill in test environment to the keys_file. The format is json.
@@ -103,7 +103,7 @@ void FillInSystemEnvironment(base::Value::DictStorage& ds) {
 // should be filled in. Eg: operating system, graphics card, processor
 // architecture, screen resolution, etc.
 bool FillInTestEnvironment(const base::FilePath& keys_file) {
-  base::Value::DictStorage ds;
+  base::Value::Dict ds;
   FillInSystemEnvironment(ds);
   base::Value root(std::move(ds));
   std::string content;
