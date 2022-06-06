2331829f6a49594ebb05ab38a6d84e574e1ca856
Mustaq Ahmed <mustaq@google.com>
2022-06-06 22:02:47 +0000

    Partially revert "Fix touch-action update timing for touch pointers"
    This partially reverts commit bd37a1d9e6256a8248e89d8fc07fdd18f97c0120
    keeping the new WPT intact.
    
    Reason for revert: the exposed behavior have made PointerEvent's
    touch-action behavior very similar to canceling TouchEvent's
    touchstart, and it seems to have been exposed for years!  We need a
    PSA and impact assessment.
    
    Fixed: 1328973
    Bug: 1314739
    
    Original change's description:
    > Fix touch-action update timing for touch pointers.
    >
    > Historically the update was done right before touchstart event firing.
    > When we shipped pointer events, we missed to move this update to
    > pointerdown event firing.  As a result, changing the touch-action
    > after pointerdown but before touchstart unexpectedly exposed the
    > change.
    >
    > This CL fixes this historical mistake.
    >
    > Fixed: 1314739
    > Change-Id: I59f4d0bd6d89bf577603644714e975fe56d05239
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3627854
    > Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#999634}
    
    (cherry picked from commit cac214d7bddb892023d198ff863027f7a6d22d31)
    
    Change-Id: I4f04a7bbdb0749de6da491d5e84f7cd5e748bd26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3668933
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1007969}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3688820
    Cr-Commit-Position: refs/branch-heads/5060@{#616}
    Cr-Branched-From: b83393d0f4038aeaf67f970a024d8101df7348d1-refs/heads/main@{#1002911}

--- third_party/blink/renderer/core/input/pointer_event_manager.cc
+++ third_party/blink/renderer/core/input/pointer_event_manager.cc
@@ -658,11 +658,6 @@ WebInputEventResult PointerEventManager::HandlePointerEvent(
         mojom::blink::UserActivationNotificationType::kInteraction);
   }
 
-  if (event.GetType() == WebInputEvent::Type::kPointerDown) {
-    touch_event_manager_->UpdateTouchAttributeMapsForPointerDown(
-        event, pointer_event_target);
-  }
-
   WebInputEventResult result = DispatchTouchPointerEvent(
       event, coalesced_events, predicted_events, pointer_event_target);
 
