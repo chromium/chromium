3df1cd66e4670491908e3ada3dfbaf410703e862
Mei Liang <meiliang@chromium.org>
2022-06-07 06:26:05 +0000

    [ConsentV2] Polish the dialog view
    This CL polishes the dialog view with the spacing value specify in the
    approved mock.
    
    Mock: https://screenshot.googleplex.com/A4CnxoqaTj4dTnc
    Implementation: https://screenshot.googleplex.com/8WLZpe6SB9QGNos
    Demo: https://screencast.googleplex.com/cast/NDYzMTkxMjAxOTY1NjcwNHxmNGYzYzlkOC1iZg
    
    Bug: 1318685
    Change-Id: I10ef93322c467ca828d7528bb47f8186822271e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3673790
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011307}

--- chrome/browser/ui/views/commerce/ntp_discount_consent_dialog_view.cc
+++ chrome/browser/ui/views/commerce/ntp_discount_consent_dialog_view.cc
@@ -22,10 +22,17 @@
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/fill_layout.h"
+#include "ui/views/view_class_properties.h"
 
 namespace {
+// TODO(crbug.com/1328709): These are approved one-off dialog style, we will
+// migrate when this bug is resolved.
 // Spacing between child of the Discount Consent Dialog View
 constexpr int kChildSpacing = 24;
+constexpr int kBannerImageTopMargin = 44;
+constexpr int kBodyContainerHorizontalMargin = 22;
+constexpr int kBodyContainerBottomMargin = 54;
 }  // namespace
 
 namespace commerce {
@@ -65,30 +72,54 @@ NtpDiscountConsentDialogView::NtpDiscountConsentDialogView(
                                   base::Unretained(this)));
 
   // Set up dialog content view.
+  auto* layout_provider = ChromeLayoutProvider::Get();
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical,
-      ChromeLayoutProvider::Get()->GetInsetsMetric(views::INSETS_DIALOG),
-      kChildSpacing));
+      layout_provider->GetInsetsMetric(views::INSETS_DIALOG), kChildSpacing));
 
   ui::ResourceBundle& bundle = ui::ResourceBundle::GetSharedInstance();
 
-  AddChildView(std::make_unique<ThemeTrackingNonAccessibleImageView>(
-      *bundle.GetImageSkiaNamed(IDR_NTP_CART_DISCOUNT_CONSENT_LIGHT),
-      *bundle.GetImageSkiaNamed(IDR_NTP_CART_DISCOUNT_CONSENT_DARK),
-      base::BindRepeating(&NtpDiscountConsentDialogView::GetBackgroundColor,
-                          base::Unretained(this))));
+  auto banner_image_container = std::make_unique<views::View>();
+  banner_image_container->SetLayoutManager(
+      std::make_unique<views::FillLayout>());
+  banner_image_container->SetProperty(
+      views::kMarginsKey, gfx::Insets::TLBR(kBannerImageTopMargin, 0, 0, 0));
+  // It is safe to use base::Unretained() here because the GetBackgroundColor
+  // will not be called after this class is destroyed.
+  banner_image_container->AddChildView(
+      std::make_unique<ThemeTrackingNonAccessibleImageView>(
+          *bundle.GetImageSkiaNamed(IDR_NTP_CART_DISCOUNT_CONSENT_LIGHT),
+          *bundle.GetImageSkiaNamed(IDR_NTP_CART_DISCOUNT_CONSENT_DARK),
+          base::BindRepeating(&NtpDiscountConsentDialogView::GetBackgroundColor,
+                              base::Unretained(this))));
+  AddChildView(std::move(banner_image_container));
 
-  auto title_label = std::make_unique<views::Label>(
-      l10n_util::GetStringUTF16(IDS_NATIVE_NTP_CART_DISCOUNT_CONSENT_TITLE),
-      CONTEXT_HEADLINE, views::style::STYLE_PRIMARY);
-  auto* title = AddChildView(std::move(title_label));
-  title->SetMultiLine(true);
+  // TODO(crbug.com/1331844): Remove the view wrappers when the bug is resolved.
+  auto title_container = std::make_unique<views::View>();
+  title_container->SetLayoutManager(std::make_unique<views::FillLayout>());
+  auto* title_label =
+      title_container->AddChildView(std::make_unique<views::Label>(
+          l10n_util::GetStringUTF16(IDS_NATIVE_NTP_CART_DISCOUNT_CONSENT_TITLE),
+          CONTEXT_HEADLINE, views::style::STYLE_PRIMARY));
+  title_label->SetMultiLine(true);
+  AddChildView(std::move(title_container));
 
-  auto consent_label = std::make_unique<views::Label>(
-      l10n_util::GetStringUTF16(IDS_NATIVE_NTP_CART_DISCOUNT_CONSENT_BODY),
-      views::style::CONTEXT_DIALOG_BODY_TEXT, views::style::STYLE_SECONDARY);
-  auto* consent = AddChildView(std::move(consent_label));
-  consent->SetMultiLine(true);
+  // TODO(crbug.com/1331844): Remove the view wrappers when the bug is resolved.
+  auto body_container = std::make_unique<views::View>();
+  body_container->SetLayoutManager(std::make_unique<views::FillLayout>());
+  body_container->SetProperty(
+      views::kMarginsKey, gfx::Insets::TLBR(0, kBodyContainerHorizontalMargin,
+                                            kBodyContainerBottomMargin,
+                                            kBodyContainerHorizontalMargin));
+  auto* consent_label =
+      body_container->AddChildView(std::make_unique<views::Label>(
+          l10n_util::GetStringUTF16(IDS_NATIVE_NTP_CART_DISCOUNT_CONSENT_BODY),
+          views::style::CONTEXT_DIALOG_BODY_TEXT,
+          views::style::STYLE_SECONDARY));
+  consent_label->SetHorizontalAlignment(gfx::ALIGN_CENTER);
+  consent_label->SetAllowCharacterBreak(true);
+  consent_label->SetMultiLine(true);
+  AddChildView(std::move(body_container));
 }
 
 NtpDiscountConsentDialogView::~NtpDiscountConsentDialogView() = default;
