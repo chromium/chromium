2813392442e5ff5cc8702a743bdd52a1bab82be4
Matt Wolenetz <wolenetz@chromium.org>
2022-06-08 20:08:04 +0000

    MSE-in-Workers: srcObject part 1: MediaSourceHandle
    Adds core MediaSourceHandle interface and modules implementation.
    Later changes will enable apps to get a handle from a dedicated worker
    MediaSource, post it to the main thread and attach it to an
    HTMLMediaElement via the srcObject attribute; they will also include
    test updates.
    
    References:
    Full prototype CL:
        https://chromium-review.googlesource.com/c/chromium/src/+/3515334
    MSE spec issue:
        https://github.com/w3c/media-source/issues/175
    MSE spec feature updates switching from worker MSE attachment via
      object URL to attachment via srcObject MediaSourceHandle:
      * https://github.com/w3c/media-source/pull/305
      * further clarifications in discussion at
        https://github.com/w3c/media-source/pull/306#issuecomment-1144180822
    
    BUG=878133
    
    Change-Id: I9a3b47ea02d2755a9860999e245eafc26f864977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687146
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012115}

---
+++ third_party/blink/renderer/modules/mediasource/media_source_handle_impl.cc
@@ -0,0 +1,41 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/mediasource/media_source_handle_impl.h"
+
+#include "base/logging.h"
+#include "third_party/blink/renderer/core/html/media/media_source_attachment.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+
+MediaSourceHandleImpl::MediaSourceHandleImpl(
+    scoped_refptr<MediaSourceAttachment> attachment,
+    String internal_blob_url)
+    : attachment_(attachment), internal_blob_url_(internal_blob_url) {
+  DCHECK(attachment_);
+  DCHECK(!internal_blob_url.IsEmpty());
+
+  DVLOG(1) << __func__ << " this=" << this << ", attachment_=" << attachment_
+           << ", internal_blob_url_=" << internal_blob_url_;
+}
+
+MediaSourceHandleImpl::~MediaSourceHandleImpl() {
+  DVLOG(1) << __func__ << " this=" << this;
+}
+
+scoped_refptr<MediaSourceAttachment> MediaSourceHandleImpl::GetAttachment() {
+  return attachment_;
+}
+
+String MediaSourceHandleImpl::GetInternalBlobURL() {
+  return internal_blob_url_;
+}
+
+void MediaSourceHandleImpl::Trace(Visitor* visitor) const {
+  ScriptWrappable::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/modules/mediasource/media_source_handle_impl.h
@@ -0,0 +1,38 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_MEDIA_SOURCE_HANDLE_IMPL_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_MEDIA_SOURCE_HANDLE_IMPL_H_
+
+#include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/core/html/media/media_source_attachment.h"
+#include "third_party/blink/renderer/core/html/media/media_source_handle.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+
+class MediaSourceHandleImpl final : public ScriptWrappable,
+                                    public MediaSourceHandle {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit MediaSourceHandleImpl(
+      scoped_refptr<MediaSourceAttachment> attachment,
+      String internal_blob_url);
+  ~MediaSourceHandleImpl() override;
+
+  scoped_refptr<MediaSourceAttachment> GetAttachment() override;
+  String GetInternalBlobURL() override;
+
+  void Trace(Visitor*) const override;
+
+ private:
+  scoped_refptr<MediaSourceAttachment> attachment_;
+  String internal_blob_url_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASOURCE_MEDIA_SOURCE_HANDLE_IMPL_H_
