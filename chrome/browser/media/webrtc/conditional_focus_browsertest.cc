2fa69dc2fe6ddc20dd5e817010275fef554bad1c
Justin DeWitt <dewittj@chromium.org>
2022-06-08 17:37:22 +0000

    Revert "[Region Capture] Ship Region Capture by default"
    This reverts commit ea2757c88a876523232b921199dbc1f450ca4a98.
    
    Reason for revert: org.chromium.webview_shell.test.WebViewLayoutTest#testWebViewIncludedStableInterfaces fails
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-x86-rel/9195/test-results?q=ExactID%3Aninja%3A%2F%2Fandroid_webview%2Ftools%2Fsystem_webview_shell%3Asystem_webview_shell_layout_test_apk%2Forg.chromium.webview_shell.test.WebViewLayoutTest%23testWebViewIncludedStableInterfaces+VHash%3Ab0b1d13e037a60b3
    
    java.lang.AssertionError: 
    WebView does not expose the Blink interfaces below. Add them to
    //android_webview/tools/system_webview_shell/test/data/webexposed/not-webview-exposed.txt
    to suppress this error.
    	- interface CropTarget
    	- interface BrowserCaptureMediaStreamTrack : MediaStreamTrack
    
    Original change's description:
    > [Region Capture] Ship Region Capture by default
    >
    > Intent-to-ship thread:
    > https://groups.google.com/a/chromium.org/g/blink-dev/c/eZE9LTJMUlk
    >
    > To ensure FocusableMediaStreamTrack is not Web-exposed too,
    > this CL changes the parent of BrowserCaptureMediaStreamTrack
    > from FocusableMediaStreamTrack to MediaStreamTrack *in IDL*.
    > In C++, FocusableMediaStreamTrack is still the direct parent.
    > This allows focus() to still be exposed on both.
    > However, note that focus() is still gated by origin trial
    > on both classes.
    >
    > Bug: 1247761
    > Change-Id: Ia7ba5ce71c2c4d31784b7050d682e3077f83a0ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3687799
    > Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1011839}
    
    Bug: 1247761
    Change-Id: Iac04f2d2be6520f85be49763ee01306dd0a68ef7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696796
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Justin DeWitt <dewittj@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012047}

--- chrome/browser/media/webrtc/conditional_focus_browsertest.cc
+++ chrome/browser/media/webrtc/conditional_focus_browsertest.cc
@@ -276,7 +276,7 @@ IN_PROC_BROWSER_TEST_F(ConditionalFocusBrowserTest,
   // Test.
   CallFocusAndExpectError(
       "InvalidStateError: Failed to execute 'focus' on "
-      "'BrowserCaptureMediaStreamTrack': Method may only be called once.");
+      "'FocusableMediaStreamTrack': Method may only be called once.");
 }
 
 IN_PROC_BROWSER_TEST_F(ConditionalFocusBrowserTest,
@@ -291,7 +291,7 @@ IN_PROC_BROWSER_TEST_F(ConditionalFocusBrowserTest,
   EXPECT_EQ(
       script_result,
       "InvalidStateError: Failed to execute 'focus' on "
-      "'BrowserCaptureMediaStreamTrack': Method may not be invoked on clones.");
+      "'FocusableMediaStreamTrack': Method may not be invoked on clones.");
 }
 
 IN_PROC_BROWSER_TEST_F(ConditionalFocusBrowserTest,
@@ -302,8 +302,8 @@ IN_PROC_BROWSER_TEST_F(ConditionalFocusBrowserTest,
           /*on_correct_microtask=*/false,
           /*expected_result=*/
           "InvalidStateError: Failed to execute 'focus' on "
-          "'BrowserCaptureMediaStreamTrack': The microtask on which the "
-          "Promise was settled has terminated.");
+          "'FocusableMediaStreamTrack': The microtask on which the Promise was "
+          "settled has terminated.");
 }
 
 #endif  //  !BUILDFLAG(IS_CHROMEOS_LACROS)
