c3fe354aeee8432539fe42ddb4b3ac3e71736cd4
Ian Helmke <helmke@google.com>
2022-06-08 00:31:46 +0000

    lacros - fix extension controlled pref observers
    Currently creating an observer for an extension controlled pref causes
    ash to crash. This is a prerequisite to the full implementation of
    extension-controlled pref observers in lacros.
    
    Bug: 1218145
    Change-Id: Iad18872f3614a4125c5623226a69c9848554512b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690828
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ian Helmke <helmke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1011757}

--- chrome/browser/ash/crosapi/prefs_ash.cc
+++ chrome/browser/ash/crosapi/prefs_ash.cc
@@ -311,7 +311,8 @@ absl::optional<PrefsAsh::State> PrefsAsh::GetState(mojom::PrefPath path) {
         return absl::nullopt;
       }
       std::string pref_name = GetExtensionPrefNameForPref(path);
-      return State{profile_prefs_registrar_->prefs(), nullptr, true, pref_name};
+      return State{profile_prefs_registrar_->prefs(),
+                   profile_prefs_registrar_.get(), true, pref_name};
     }
   }
 }
