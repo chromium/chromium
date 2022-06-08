6f9cbc4dde0c89337d6edf2cfccaee79976ba4c0
François Degros <fdegros@chromium.org>
2022-06-08 08:29:38 +0000

    [Files App] Add output operator for chromeos::MountErrorType
    BUG=chromium:1313346
    TEST=CQ
    
    Change-Id: Idf870aea11358208f3dd543c172dd3e088996706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3689102
    Reviewed-by: Marcello Salomao <msalomao@google.com>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1011844}

--- chromeos/dbus/cros_disks/cros_disks_client.h
+++ chromeos/dbus/cros_disks/cros_disks_client.h
@@ -5,7 +5,8 @@
 #ifndef CHROMEOS_DBUS_CROS_DISKS_CROS_DISKS_CLIENT_H_
 #define CHROMEOS_DBUS_CROS_DISKS_CROS_DISKS_CLIENT_H_
 
-#include <stdint.h>
+#include <cstdint>
+#include <ostream>
 
 #include <memory>
 #include <string>
@@ -50,7 +51,7 @@ enum DeviceType {
 };
 
 // Mount error code used by cros-disks.
-// These values are not the same as cros_disks::MountErrorType.
+// These values are NOT the same as cros_disks::MountErrorType.
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
 enum MountError {
@@ -76,6 +77,10 @@ enum MountError {
   MOUNT_ERROR_CANCELLED = 19,
   MOUNT_ERROR_COUNT,
 };
+
+// Output operator for logging.
+COMPONENT_EXPORT(CHROMEOS_DBUS_CROS_DISKS)
+std::ostream& operator<<(std::ostream& out, MountError error);
 
 // Rename error reported by cros-disks.
 enum RenameError {
