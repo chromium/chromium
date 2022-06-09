376c1198e2dd7b981a648929977d84e55c0c5e3f
Mark Brand <markbrand@google.com>
2022-06-09 08:56:54 +0000

    MojoLPM: Update FileSystemManager fuzzer.
    Refactoring to remove duplicated code and slow RunLoops. (See change 2566768)
    
    Change-Id: I3e444a0fc699d80fd01faac829d6d420aaf2bc13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3551605
    Commit-Queue: Mark Brand <markbrand@google.com>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012392}

--- content/test/fuzzer/file_system_manager_mojolpm_fuzzer.cc
+++ content/test/fuzzer/file_system_manager_mojolpm_fuzzer.cc
@@ -5,14 +5,8 @@
 #include <stdint.h>
 #include <utility>
 
-#include "base/at_exit.h"
-#include "base/base_switches.h"
-#include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/i18n/icu_util.h"
 #include "base/no_destructor.h"
-#include "base/test/test_switches.h"
-#include "base/test/test_timeouts.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"  // nogncheck
@@ -25,7 +19,7 @@
 #include "content/public/test/test_content_client_initializer.h"
 #include "content/test/fuzzer/file_system_manager_mojolpm_fuzzer.pb.h"
 #include "content/test/fuzzer/mojolpm_fuzzer_support.h"
-#include "mojo/core/embedder/embedder.h"
+#include "mojo/public/tools/fuzzers/mojolpm.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/browser/quota/special_storage_policy.h"
 #include "storage/browser/test/mock_special_storage_policy.h"
@@ -69,35 +63,28 @@ scoped_refptr<base::SequencedTaskRunner> GetFuzzerTaskRunner() {
 // Since the Browser process will host one FileSystemManagerImpl per
 // RenderProcessHost, we emulate this by allowing the fuzzer to create (and
 // destroy) multiple FileSystemManagerImpl instances.
-class FileSystemManagerTestcase {
+class FileSystemManagerTestcase
+    : public ::mojolpm::Testcase<
+          content::fuzzing::file_system_manager::proto::Testcase,
+          content::fuzzing::file_system_manager::proto::Action> {
  public:
+  using ProtoTestcase = content::fuzzing::file_system_manager::proto::Testcase;
+  using ProtoAction = content::fuzzing::file_system_manager::proto::Action;
+
   explicit FileSystemManagerTestcase(
       const content::fuzzing::file_system_manager::proto::Testcase& testcase);
 
-  // Returns true once either all of the actions in the testcase have been
-  // performed, or the per-testcase action limit has been exceeded.
-  //
-  // This should only be called from the fuzzer sequence.
-  bool IsFinished();
+  void SetUp(base::OnceClosure done_closure) override;
+  void TearDown(base::OnceClosure done_closure) override;
 
-  // If there are still actions remaining in the testcase, this will perform the
-  // next sequence of actions before returning.
-  //
-  // If IsFinished() would return true, then calling this function is a no-op.
-  //
-  // This should only be called from the fuzzer sequence.
-  void NextAction();
-
-  void SetUp();
-  void TearDown();
+  void RunAction(const ProtoAction& action,
+                 base::OnceClosure done_closure) override;
 
  private:
-  using Action = content::fuzzing::file_system_manager::proto::Action;
-
-  void SetUpOnIOThread();
-  void SetUpOnUIThread();
-  void TearDownOnIOThread();
-  void TearDownOnUIThread();
+  void SetUpOnIOThread(base::OnceClosure done_closure);
+  void SetUpOnUIThread(base::OnceClosure done_closure);
+  void TearDownOnIOThread(base::OnceClosure done_closure);
+  void TearDownOnUIThread(base::OnceClosure done_closure);
 
   // Used by AddFileSystemManager to create and bind FileSystemManagerImpl on the
   // UI thread.
@@ -115,19 +102,8 @@ class FileSystemManagerTestcase {
       uint32_t id,
       content::fuzzing::file_system_manager::proto::NewFileSystemManagerAction::
           RenderProcessId render_process_id,
-      const storage_key_proto::StorageKey& storage_key);
-
-  // The proto message describing the test actions to perform.
-  const content::fuzzing::file_system_manager::proto::Testcase& testcase_;
-
-  // Apply a reasonable upper-bound on testcase complexity to avoid timeouts.
-  const int max_action_count_ = 512;
-
-  // Count of total actions performed in this testcase.
-  int action_count_ = 0;
-
-  // The index of the next sequence of actions to execute.
-  int next_sequence_idx_ = 0;
+      const storage_key_proto::StorageKey& storage_key,
+      base::OnceClosure done_closure);
 
   // Prerequisite state
   TestBrowserContext browser_context_;
@@ -139,40 +115,28 @@ class FileSystemManagerTestcase {
   // Access only from UI thread.
   std::unique_ptr<FileSystemManagerImpl>
       file_system_manager_impls_[kNumRenderers];
-
-  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 FileSystemManagerTestcase::FileSystemManagerTestcase(
     const content::fuzzing::file_system_manager::proto::Testcase& testcase)
-    : testcase_(testcase), browser_context_() {
+    : Testcase<ProtoTestcase, ProtoAction>(testcase), browser_context_() {
   // FileSystemManagerTestcase is created on the main thread, but the actions
   // that we want to validate the sequencing of take place on the fuzzer
   // sequence.
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
-void FileSystemManagerTestcase::SetUp() {
+void FileSystemManagerTestcase::SetUp(base::OnceClosure done_closure) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  base::RunLoop io_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  GetIOThreadTaskRunner({})->PostTaskAndReply(
+  GetIOThreadTaskRunner({})->PostTask(
       FROM_HERE,
       base::BindOnce(&FileSystemManagerTestcase::SetUpOnIOThread,
-                     base::Unretained(this)),
-      io_run_loop.QuitClosure());
-  io_run_loop.Run();
-
-  base::RunLoop ui_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  GetUIThreadTaskRunner({})->PostTaskAndReply(
-      FROM_HERE,
-      base::BindOnce(&FileSystemManagerTestcase::SetUpOnUIThread,
-                     base::Unretained(this)),
-      ui_run_loop.QuitClosure());
-  ui_run_loop.Run();
+                     base::Unretained(this), std::move(done_closure)));
 }
 
-void FileSystemManagerTestcase::SetUpOnIOThread() {
+void FileSystemManagerTestcase::SetUpOnIOThread(
+    base::OnceClosure done_closure) {
   CHECK(temp_dir_.CreateUniqueTempDir());
   file_system_context_ =
       storage::CreateFileSystemContextForTesting(nullptr, temp_dir_.GetPath());
@@ -180,9 +144,15 @@ void FileSystemManagerTestcase::SetUpOnIOThread() {
   blob_storage_context_ = base::MakeRefCounted<ChromeBlobStorageContext>();
   blob_storage_context_->InitializeOnIOThread(
       temp_dir_.GetPath(), temp_dir_.GetPath(), GetIOThreadTaskRunner({}));
+
+  GetUIThreadTaskRunner({})->PostTask(
+      FROM_HERE,
+      base::BindOnce(&FileSystemManagerTestcase::SetUpOnUIThread,
+                     base::Unretained(this), std::move(done_closure)));
 }
 
-void FileSystemManagerTestcase::SetUpOnUIThread() {
+void FileSystemManagerTestcase::SetUpOnUIThread(
+    base::OnceClosure done_closure) {
   ChildProcessSecurityPolicyImpl* p =
       ChildProcessSecurityPolicyImpl::GetInstance();
   p->RegisterFileSystemPermissionPolicy(storage::kFileSystemTypeTest,
@@ -198,96 +168,83 @@ void FileSystemManagerTestcase::SetUpOnUIThread() {
         i, file_system_context_, blob_storage_context_);
     p->Add(i, &browser_context_);
   }
+
+  GetFuzzerTaskRunner()->PostTask(FROM_HERE, std::move(done_closure));
 }
 
-void FileSystemManagerTestcase::TearDown() {
+void FileSystemManagerTestcase::TearDown(base::OnceClosure done_closure) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  base::RunLoop ui_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  content::GetUIThreadTaskRunner({})->PostTaskAndReply(
+  GetUIThreadTaskRunner({})->PostTask(
       FROM_HERE,
       base::BindOnce(&FileSystemManagerTestcase::TearDownOnUIThread,
-                     base::Unretained(this)),
-      ui_run_loop.QuitClosure());
-  ui_run_loop.Run();
-
-  base::RunLoop io_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  content::GetIOThreadTaskRunner({})->PostTaskAndReply(
-      FROM_HERE,
-      base::BindOnce(&FileSystemManagerTestcase::TearDownOnIOThread,
-                     base::Unretained(this)),
-      io_run_loop.QuitClosure());
-  io_run_loop.Run();
+                     base::Unretained(this), std::move(done_closure)));
 }
 
-void FileSystemManagerTestcase::TearDownOnIOThread() {
+void FileSystemManagerTestcase::TearDownOnIOThread(
+    base::OnceClosure done_closure) {
   for (size_t i = 0; i < kNumRenderers; i++) {
     file_system_manager_impls_[i].reset();
   }
+
+  GetFuzzerTaskRunner()->PostTask(FROM_HERE, std::move(done_closure));
 }
 
-void FileSystemManagerTestcase::TearDownOnUIThread() {
+void FileSystemManagerTestcase::TearDownOnUIThread(
+    base::OnceClosure done_closure) {
   ChildProcessSecurityPolicyImpl* p =
       ChildProcessSecurityPolicyImpl::GetInstance();
   for (size_t i = 0; i < kNumRenderers; i++) {
     p->Remove(i);
   }
+
+  GetIOThreadTaskRunner({})->PostTask(
+      FROM_HERE,
+      base::BindOnce(&FileSystemManagerTestcase::TearDownOnIOThread,
+                     base::Unretained(this), std::move(done_closure)));
 }
 
-bool FileSystemManagerTestcase::IsFinished() {
+void FileSystemManagerTestcase::RunAction(const ProtoAction& action,
+                                          base::OnceClosure run_closure) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return next_sequence_idx_ >= testcase_.sequence_indexes_size();
-}
 
-void FileSystemManagerTestcase::NextAction() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (next_sequence_idx_ < testcase_.sequence_indexes_size()) {
-    auto sequence_idx = testcase_.sequence_indexes(next_sequence_idx_++);
-    const auto& sequence =
-        testcase_.sequences(sequence_idx % testcase_.sequences_size());
-    for (auto action_idx : sequence.action_indexes()) {
-      if (!testcase_.actions_size() || ++action_count_ > max_action_count_) {
-        return;
+  const auto ThreadId_UI =
+      content::fuzzing::file_system_manager::proto::RunThreadAction_ThreadId_UI;
+  const auto ThreadId_IO =
+      content::fuzzing::file_system_manager::proto::RunThreadAction_ThreadId_IO;
+
+  switch (action.action_case()) {
+    case ProtoAction::kNewFileSystemManager:
+      AddFileSystemManager(action.new_file_system_manager().id(),
+                           action.new_file_system_manager().render_process_id(),
+                           action.new_file_system_manager().storage_key(),
+                           std::move(run_closure));
+      return;
+
+    case ProtoAction::kRunThread:
+      if (action.run_thread().id() == ThreadId_UI) {
+        content::GetUIThreadTaskRunner({})->PostTaskAndReply(
+            FROM_HERE, base::DoNothing(), std::move(run_closure));
+      } else if (action.run_thread().id() == ThreadId_IO) {
+        content::GetIOThreadTaskRunner({})->PostTaskAndReply(
+            FROM_HERE, base::DoNothing(), std::move(run_closure));
       }
-      const auto& action =
-          testcase_.actions(action_idx % testcase_.actions_size());
-      switch (action.action_case()) {
-        case Action::kNewFileSystemManager:
-          AddFileSystemManager(
-              action.new_file_system_manager().id(),
-              action.new_file_system_manager().render_process_id(),
-              action.new_file_system_manager().storage_key());
-          break;
+      return;
 
-        case Action::kRunThread:
-          if (action.run_thread().id()) {
-            base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-            GetUIThreadTaskRunner({})->PostTask(FROM_HERE,
-                                                run_loop.QuitClosure());
-            run_loop.Run();
-          } else {
-            base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-            GetIOThreadTaskRunner({})->PostTask(FROM_HERE,
-                                                run_loop.QuitClosure());
-            run_loop.Run();
-          }
-          break;
+    case ProtoAction::kFileSystemManagerRemoteAction:
+      ::mojolpm::HandleRemoteAction(action.file_system_manager_remote_action());
+      break;
 
-        case Action::kFileSystemManagerRemoteAction:
-          ::mojolpm::HandleRemoteAction(
-              action.file_system_manager_remote_action());
-          break;
+    case ProtoAction::kFileSystemCancellableOperationRemoteAction:
+      ::mojolpm::HandleRemoteAction(
+          action.file_system_cancellable_operation_remote_action());
+      break;
 
-        case Action::kFileSystemCancellableOperationRemoteAction:
-          ::mojolpm::HandleRemoteAction(
-              action.file_system_cancellable_operation_remote_action());
-          break;
-
-        case Action::ACTION_NOT_SET:
-          break;
-      }
-    }
+    case ProtoAction::ACTION_NOT_SET:
+      break;
   }
+
+  GetFuzzerTaskRunner()->PostTask(FROM_HERE, std::move(run_closure));
 }
 
 void FileSystemManagerTestcase::AddFileSystemManagerImpl(
@@ -305,64 +262,34 @@ void FileSystemManagerTestcase::AddFileSystemManagerImpl(
       storage_key_proto::Convert(storage_key), std::move(receiver));
 }
 
+static void AddFileSystemManagerInstance(
+    uint32_t id,
+    mojo::Remote<::blink::mojom::FileSystemManager> remote,
+    base::OnceClosure run_closure) {
+  ::mojolpm::GetContext()->AddInstance(id, std::move(remote));
+
+  std::move(run_closure).Run();
+}
+
 void FileSystemManagerTestcase::AddFileSystemManager(
     uint32_t id,
     content::fuzzing::file_system_manager::proto::NewFileSystemManagerAction::
         RenderProcessId render_process_id,
-    const storage_key_proto::StorageKey& storage_key) {
+    const storage_key_proto::StorageKey& storage_key,
+    base::OnceClosure run_closure) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   mojo::Remote<::blink::mojom::FileSystemManager> remote;
   auto receiver = remote.BindNewPipeAndPassReceiver();
 
-  base::RunLoop run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  content::GetIOThreadTaskRunner({})->PostTaskAndReply(
+  GetIOThreadTaskRunner({})->PostTaskAndReply(
       FROM_HERE,
       base::BindOnce(&FileSystemManagerTestcase::AddFileSystemManagerImpl,
                      base::Unretained(this), id, render_process_id, storage_key,
                      std::move(receiver)),
-      run_loop.QuitClosure());
-  run_loop.Run();
-
-  ::mojolpm::GetContext()->AddInstance(id, std::move(remote));
+      base::BindOnce(&AddFileSystemManagerInstance, id, std::move(remote),
+                     std::move(run_closure)));
 }
 }  // namespace content
-
-// Helper function to keep scheduling fuzzer actions on the current runloop
-// until the testcase has completed, and then quit the runloop.
-void NextAction(content::FileSystemManagerTestcase* testcase,
-                base::RepeatingClosure quit_closure) {
-  if (!testcase->IsFinished()) {
-    testcase->NextAction();
-    content::GetFuzzerTaskRunner()->PostTask(
-        FROM_HERE, base::BindOnce(NextAction, base::Unretained(testcase),
-                                  std::move(quit_closure)));
-  } else {
-    content::GetFuzzerTaskRunner()->PostTask(FROM_HERE,
-                                             std::move(quit_closure));
-  }
-}
-
-// Helper function to setup and run the testcase, since we need to do that from
-// the fuzzer sequence rather than the main thread.
-void RunTestcase(content::FileSystemManagerTestcase* testcase) {
-  mojo::Message message;
-  auto dispatch_context =
-      std::make_unique<mojo::internal::MessageDispatchContext>(&message);
-
-  testcase->SetUp();
-
-  ::mojolpm::GetContext()->StartTestcase();
-
-  base::RunLoop fuzzer_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
-  content::GetFuzzerTaskRunner()->PostTask(
-      FROM_HERE, base::BindOnce(NextAction, base::Unretained(testcase),
-                                fuzzer_run_loop.QuitClosure()));
-  fuzzer_run_loop.Run();
-
-  ::mojolpm::GetContext()->EndTestcase();
-
-  testcase->TearDown();
-}
 
 DEFINE_BINARY_PROTO_FUZZER(
     const content::fuzzing::file_system_manager::proto::Testcase&
@@ -377,13 +304,16 @@ DEFINE_BINARY_PROTO_FUZZER(
 
   content::FileSystemManagerTestcase testcase(proto_testcase);
 
-  base::RunLoop ui_run_loop(base::RunLoop::Type::kNestableTasksAllowed);
+  base::RunLoop main_run_loop;
 
-  // Unretained is safe here, because ui_run_loop has to finish before testcase
-  // goes out of scope.
-  content::GetFuzzerTaskRunner()->PostTaskAndReply(
-      FROM_HERE, base::BindOnce(RunTestcase, base::Unretained(&testcase)),
-      ui_run_loop.QuitClosure());
+  // Unretained is safe here, because `main_run_loop` has to finish before
+  // testcase goes out of scope.
+  content::GetFuzzerTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&mojolpm::RunTestcase<content::FileSystemManagerTestcase>,
+                     base::Unretained(&testcase),
+                     content::GetFuzzerTaskRunner(),
+                     main_run_loop.QuitClosure()));
 
-  ui_run_loop.Run();
+  main_run_loop.Run();
 }
