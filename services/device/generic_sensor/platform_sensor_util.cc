dad3417fa0ae9c77e52f87f9899fdbad96f7b56c
Juha Vainio <juha.j.vainio@intel.com>
2022-06-09 11:47:03 +0000

    [Sensors] Add static_assert check for kAlsRoundingMultiple
    All other k*RoundingMultiple had static_assert check, only
    kAlsRoundingMultiple missed this check.
    
    Fixed: 1330925
    Change-Id: If4f7ffdd1a5afc70f1cdfd500c40913ee4db4369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698076
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Auto-Submit: Juha J Vainio <juha.j.vainio@intel.com>
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1012432}

--- services/device/generic_sensor/platform_sensor_util.cc
+++ services/device/generic_sensor/platform_sensor_util.cc
@@ -17,6 +17,9 @@ namespace {
 static_assert(kAccelerometerRoundingMultiple > 0.0,
               "Rounding multiple must be positive.");
 
+static_assert(kAlsRoundingMultiple > 0,
+              "Rounding multiple must be positive.");
+
 static_assert(kGyroscopeRoundingMultiple > 0.0,
               "Rounding multiple must be positive.");
 
