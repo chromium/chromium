cd5c92e0533b15d59d269f927401c8279f6ae4d1
dpapad <dpapad@chromium.org>
2022-06-10 20:41:20 +0000

    Migrate chrome://components to TypeScript.
    Bug: 1316438
    Change-Id: I93362cc232f85a162d986f1cbebe4e8cbaf22ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699214
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013112}

--- chrome/browser/resources/components/components.ts
+++ chrome/browser/resources/components/components.ts
@@ -4,28 +4,52 @@
 
 import './strings.m.js';
 
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {addWebUIListener, isChromeOS, sendWithPromise} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {$} from 'chrome://resources/js/util.m.js';
+
+declare global {
+  class JsEvalContext {
+    constructor(data: any);
+  }
+  function jstProcess(context: JsEvalContext, template: HTMLElement): void;
+
+  const trustedTypes: {emptyHTML: string};
+}
+
+type Component = {
+  id: string,
+  name: string,
+  status: string,
+  version: string,
+};
+
+type ComponentsData = {
+  components: Component[],
+  showOsLink: boolean,
+};
 
 /**
  * An array of the latest component data including ID, name, status and
  * version. This is populated in returnComponentsData() for the convenience of
  * tests.
  */
-let currentComponentsData = null;
+let currentComponentsData: Component[]|null = null;
 
 /**
  * Takes the |componentsData| input argument which represents data about the
  * currently installed components and populates the html jstemplate with
  * that data. It expects an object structure like the above.
- * @param {Object} componentsData Detailed info about installed components.
+ * @param componentsData Detailed info about installed components.
  *      Same expected format as returnComponentsData().
  */
-function renderTemplate(componentsData) {
+function renderTemplate(componentsData: ComponentsData) {
   // This is the javascript code that processes the template:
   const input = new JsEvalContext(componentsData);
-  const output = $('component-template').cloneNode(true);
+  const output =
+      document.body.querySelector<HTMLElement>(
+                       '#component-template')!.cloneNode(true) as HTMLElement;
   $('component-placeholder').innerHTML = trustedTypes.emptyHTML;
   $('component-placeholder').appendChild(output);
   jstProcess(input, output);
@@ -36,7 +60,7 @@ function renderTemplate(componentsData) {
   if (crosUrlRedirectButton) {
     crosUrlRedirectButton.onclick = crosUrlComponentRedirect;
   }
-// </if>
+  // </if>
 }
 
 // <if expr="chromeos_ash or chromeos_lacros">
@@ -60,26 +84,9 @@ function requestComponentsData() {
  * Called by the WebUI to re-populate the page with data representing the
  * current state of installed components. The componentsData will also be
  * stored in currentComponentsData to be available to JS for testing purposes.
- * @param {{
- *   components: !Array<!{name: string, version: string}>,
- *   showOsLink: Boolean
- * }} componentsData Detailed info about installed components. The template
- * expects each component's format to match the following structure to correctly
- *     populate the page:
- *   {
- *     components: [
- *       {
- *          name: 'Component1',
- *          version: '1.2.3',
- *       },
- *       {
- *          name: 'Component2',
- *          version: '4.5.6',
- *       },
- *     ]
- *   }
+ * @param componentsData Detailed info about installed components.
  */
-function returnComponentsData(componentsData) {
+function returnComponentsData(componentsData: ComponentsData) {
   const bodyContainer = $('body-container');
   const body = document.body;
 
@@ -93,19 +100,21 @@ function returnComponentsData(componentsData) {
   renderTemplate(componentsData);
 
   // Add handlers to dynamically created HTML elements.
-  const links = document.getElementsByClassName('button-check-update');
-  for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function(e) {
-      handleCheckUpdate(this);
+  const links =
+      document.body.querySelectorAll<HTMLButtonElement>('.button-check-update');
+  for (const link of links) {
+    link.onclick = function(e) {
+      handleCheckUpdate(link);
       e.preventDefault();
     };
   }
 
   // Disable some controls for Guest mode in ChromeOS.
   if (isChromeOS && loadTimeData.getBoolean('isGuest')) {
-    document.querySelectorAll('[guest-disabled]').forEach(function(element) {
-      element.disabled = true;
-    });
+    document.body.querySelectorAll<HTMLButtonElement>('[guest-disabled]')
+        .forEach(function(element) {
+          element.disabled = true;
+        });
   }
 
   const systemFlagsLinkDiv = $('os-link-container');
@@ -117,18 +126,25 @@ function returnComponentsData(componentsData) {
   body.className = 'show-tmi-mode-initial';
 }
 
+type ComponentEvent = {
+  event: string,
+  id?: string,
+  version?: string,
+};
+
 /**
  * Listener called when state of component updater service changes.
- * @param {Object} eventArgs Contains event and component ID. Component ID is
- * optional.
+ * @param event Contains event and component ID. Component ID is
+ *     optional.
  */
-function onComponentEvent(eventArgs) {
-  if (!eventArgs['id']) {
+function onComponentEvent(event: ComponentEvent) {
+  if (!event.id) {
     return;
   }
 
-  const id = eventArgs['id'];
+  const id = event.id;
 
+  assert(currentComponentsData);
   const filteredComponents = currentComponentsData.filter(function(entry) {
     return entry.id === id;
   });
@@ -140,24 +156,25 @@ function onComponentEvent(eventArgs) {
   }
 
   const component = filteredComponents[0];
+  assert(component);
 
-  const status = eventArgs['event'];
+  const status = event.event;
   $('status-' + id).textContent = status;
-  component['status'] = status;
+  component.status = status;
 
-  if (eventArgs['version']) {
-    const version = eventArgs['version'];
+  if (event.version) {
+    const version = event.version;
     $('version-' + id).textContent = version;
-    component['version'] = version;
+    component.version = version;
   }
 }
 
 /**
  * Handles an 'enable' or 'disable' button getting clicked.
- * @param {HTMLElement} node The HTML element representing the component
- *     being checked for update.
+ * @param node The HTML element representing the component being checked for
+ *     update.
  */
-function handleCheckUpdate(node) {
+function handleCheckUpdate(node: HTMLElement) {
   $('status-' + String(node.id)).textContent =
       loadTimeData.getString('checkingLabel');
 
