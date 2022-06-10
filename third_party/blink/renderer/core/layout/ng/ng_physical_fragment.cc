66a3db021b79e19717a36280a56e2c29e19a2b54
jongdeok.kim <jongdeok.kim@navercorp.com>
2022-06-10 01:09:22 +0000

    Add outline rects for descendant with additional offset
    When descendant has a layer, AddOutlineRects() and LocalToAncestorRects() has
    different offset compared to legacy layout.
    
    This CL use same offset to have same result.
    
    Bug: 1330568
    Change-Id: I535d97179c8b422d54cd8a7b18b3e26b8173411b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686934
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: jongdeok kim <jongdeok.kim@navercorp.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1012803}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
@@ -963,14 +963,12 @@ void NGPhysicalFragment::AddOutlineRectsForDescendant(
     if (descendant_box->HasLayer()) {
       DCHECK(descendant_layout_object);
       Vector<PhysicalRect> layer_outline_rects;
-      descendant_box->AddOutlineRects(additional_offset, outline_type,
+      descendant_box->AddOutlineRects(PhysicalOffset(), outline_type,
                                       &layer_outline_rects);
 
-      // Don't pass additional_offset because LocalToAncestorRects will itself
-      // apply it.
       descendant_layout_object->LocalToAncestorRects(
           layer_outline_rects, containing_block, PhysicalOffset(),
-          PhysicalOffset());
+          additional_offset);
       outline_rects->AppendVector(layer_outline_rects);
       return;
     }
