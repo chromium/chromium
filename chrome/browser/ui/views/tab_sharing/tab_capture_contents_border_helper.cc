b217bf4b19d4d1e9425698bbb878273aae2f1259
Keren Zhu <kerenzhu@chromium.org>
2022-06-13 21:52:44 +0000

    [M104] Remove dangling tab capture contents border widget when browser closes
    When sharing a tab, an overlaying widget is created to display the blue
    contents border as a child of browser widget. However, this widget is
    not closed along with the browser widget. When native theme updates,
    this dangling widget will access its parent and cause a use-after-free.
    
    This CL fixes it by using the default NATIVE_WIDGET_OWNS_WIDGET for the
    border widget. Since it is a child of the browser widget, the native
    widget should be closed alongside its parent.
    
    Bug: 1318555
    Change-Id: I098889c23c9a74663860f3a1dfec6ab74b2d32e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696593
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5112@{#38}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- chrome/browser/ui/views/tab_sharing/tab_capture_contents_border_helper.cc
+++ chrome/browser/ui/views/tab_sharing/tab_capture_contents_border_helper.cc
@@ -55,7 +55,6 @@ void InitContentsBorderWidget(content::WebContents* web_contents) {
 
   views::Widget* widget = new views::Widget;
   views::Widget::InitParams params(views::Widget::InitParams::TYPE_POPUP);
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
   views::Widget* frame = browser_view->contents_web_view()->GetWidget();
   params.parent = frame->GetNativeView();
