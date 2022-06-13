f47cd97f0baeb26682472c11a45134df84b1d969
Matt Menke <mmenke@chromium.org>
2022-06-13 16:45:00 +0000

    Remove unused URLFetcher include in components/invalidation/
    Bug: 1010491
    Change-Id: I6af0382735b00f8ab1d7f9d663cc07cc0a40ac74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699332
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013508}

--- components/invalidation/impl/per_user_topic_subscription_request_unittest.cc
+++ components/invalidation/impl/per_user_topic_subscription_request_unittest.cc
@@ -16,12 +16,12 @@
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
-#include "net/url_request/test_url_fetcher_factory.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace invalidation {
 
