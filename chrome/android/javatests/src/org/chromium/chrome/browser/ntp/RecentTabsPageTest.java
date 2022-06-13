a3c761d03fb5cab985f8b1b6feb231878bcfcb06
ckitagawa <ckitagawa@chromium.org>
2022-06-13 21:17:30 +0000

    [Android Tab Restore] Update icon size
    Update the icon and font size.
    
    Original design was for 48dp bubbles, but this update never happened.
    We should have designed for 40dp bubbles. (Smaller icon and font).
    
    Bug: 1334723
    Change-Id: I005f627d8c8878869fb64b830b725ff3687a4a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3696976
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1013657}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
@@ -71,7 +71,7 @@ public class RecentTabsPageTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(5)
+                    .setRevision(6)
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_MOBILE_RECENT_TABS)
                     .build();
 
