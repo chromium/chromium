c335567ab296e2e357c710ca13687e7387fd404f
Brittany Hartmire <bhartmire@google.com>
2022-06-14 22:30:43 +0000

    Reland "[QuickStart] Create FastPairAdvertiser class"
    This is a reland of commit e0e796024712349b827546eb3d472aac326a0ee0.
    
    There are two changes to resolve failures on the Linux Chromium OS ASan
    LSan Tests builder.
    
    1. Store base::UnguessableToken() in its own variable before converting
    it to bytes inside FastPairAdvertiser::GenerateManufacturerMetadata().
    2. Update FakeBluetoothAdvertisement:: Unregister() inside unittests to
    change local fields (only used for testing) before the advertisement
    object gets destroyed in callback methods. A comment explaining this
    was added.
    
    Original change's description:
    > [QuickStart] Create FastPairAdvertiser class
    >
    > FastPairAdvertiser emits a BLE advertisement that should trigger the
    > Fast Pair half sheet notification on the remote device. This class is
    > similar to FastInitiationAdvertiser but uses different service data
    > with the addition of manufacturer data. Metadata specifications can be
    > found in b/233682041.
    >
    > TEST=Verified metadata produces halfsheet and added unittest suite,
    > FastPairAdvertiserTest.
    >
    > Bug: b/234655072
    > Change-Id: Ia9298623438c5651eaaebf223d6eb255a82c117b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3690571
    > Reviewed-by: Michael Hansen <hansenmichael@google.com>
    > Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1012755}
    
    Change-Id: Iecc2bcf8eaf25e30084ca6c87edfeeca184d5356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3700689
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1014177}

---
+++ chromeos/ash/components/oobe_quick_start/connectivity/fast_pair_advertiser.cc
@@ -0,0 +1,179 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/oobe_quick_start/connectivity/fast_pair_advertiser.h"
+
+#include <string>
+
+#include "base/callback_helpers.h"
+#include "base/logging.h"
+#include "base/unguessable_token.h"
+
+namespace {
+
+constexpr const char kFastPairServiceUuid[] =
+    "0000fe2c-0000-1000-8000-00805f9b34fb";
+constexpr uint8_t kFastPairModelId[] = {0x41, 0xc0, 0xd9};
+constexpr uint16_t kCompanyId = 0x00e0;
+
+// TODO(b/207087915): This value comes from Android, but we may need to
+// find a more appropriate power setting for Chrome OS devices.
+const int8_t kAdjustedTxPower = -66;
+
+}  // namespace
+
+// static
+FastPairAdvertiser::Factory* FastPairAdvertiser::Factory::factory_instance_ =
+    nullptr;
+
+// static
+std::unique_ptr<FastPairAdvertiser> FastPairAdvertiser::Factory::Create(
+    scoped_refptr<device::BluetoothAdapter> adapter) {
+  if (factory_instance_)
+    return factory_instance_->CreateInstance(adapter);
+
+  return std::make_unique<FastPairAdvertiser>(adapter);
+}
+
+// static
+void FastPairAdvertiser::Factory::SetFactoryForTesting(
+    FastPairAdvertiser::Factory* factory) {
+  factory_instance_ = factory;
+}
+
+FastPairAdvertiser::FastPairAdvertiser(
+    scoped_refptr<device::BluetoothAdapter> adapter) {
+  DCHECK(adapter && adapter->IsPresent() && adapter->IsPowered());
+  adapter_ = adapter;
+}
+
+FastPairAdvertiser::~FastPairAdvertiser() {
+  StopAdvertising(base::DoNothing());
+}
+
+void FastPairAdvertiser::AdvertisementReleased(
+    device::BluetoothAdvertisement* advertisement) {
+  StopAdvertising(base::DoNothing());
+}
+
+void FastPairAdvertiser::StartAdvertising(
+    base::OnceCallback<void()> callback,
+    base::OnceCallback<void()> error_callback) {
+  DCHECK(adapter_->IsPresent() && adapter_->IsPowered());
+  DCHECK(!advertisement_);
+  RegisterAdvertisement(std::move(callback), std::move(error_callback));
+}
+
+void FastPairAdvertiser::StopAdvertising(base::OnceCallback<void()> callback) {
+  if (!advertisement_) {
+    std::move(callback).Run();
+    // |this| might be destroyed here, do not access local fields.
+    return;
+  }
+
+  UnregisterAdvertisement(std::move(callback));
+}
+
+void FastPairAdvertiser::RegisterAdvertisement(
+    base::OnceClosure callback,
+    base::OnceClosure error_callback) {
+  auto advertisement_data =
+      std::make_unique<device::BluetoothAdvertisement::Data>(
+          device::BluetoothAdvertisement::ADVERTISEMENT_TYPE_BROADCAST);
+
+  auto list = std::make_unique<device::BluetoothAdvertisement::UUIDList>();
+  list->push_back(kFastPairServiceUuid);
+  advertisement_data->set_service_uuids(std::move(list));
+
+  auto service_data =
+      std::make_unique<device::BluetoothAdvertisement::ServiceData>();
+  auto payload = std::vector<uint8_t>(std::begin(kFastPairModelId),
+                                      std::end(kFastPairModelId));
+  auto service_metadata = GenerateServiceMetadata();
+  payload.insert(std::end(payload), std::begin(service_metadata),
+                 std::end(service_metadata));
+  service_data->insert(std::pair<std::string, std::vector<uint8_t>>(
+      kFastPairServiceUuid, payload));
+  advertisement_data->set_service_data(std::move(service_data));
+
+  auto manufacturer_data =
+      std::make_unique<device::BluetoothAdvertisement::ManufacturerData>();
+  auto manufacturer_metadata = GenerateManufacturerMetadata();
+  manufacturer_data->insert(std::pair<uint16_t, std::vector<uint8_t>>(
+      kCompanyId, manufacturer_metadata));
+  advertisement_data->set_manufacturer_data(std::move(manufacturer_data));
+
+  advertisement_data->set_include_tx_power(true);
+
+  adapter_->RegisterAdvertisement(
+      std::move(advertisement_data),
+      base::BindOnce(&FastPairAdvertiser::OnRegisterAdvertisement,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)),
+      base::BindOnce(&FastPairAdvertiser::OnRegisterAdvertisementError,
+                     weak_ptr_factory_.GetWeakPtr(),
+                     std::move(error_callback)));
+}
+
+void FastPairAdvertiser::OnRegisterAdvertisement(
+    base::OnceClosure callback,
+    scoped_refptr<device::BluetoothAdvertisement> advertisement) {
+  advertisement_ = advertisement;
+  advertisement_->AddObserver(this);
+  std::move(callback).Run();
+}
+
+void FastPairAdvertiser::OnRegisterAdvertisementError(
+    base::OnceClosure error_callback,
+    device::BluetoothAdvertisement::ErrorCode error_code) {
+  LOG(ERROR) << __func__ << " failed with error code = " << error_code;
+  std::move(error_callback).Run();
+  // |this| might be destroyed here, do not access local fields.
+}
+
+void FastPairAdvertiser::UnregisterAdvertisement(base::OnceClosure callback) {
+  stop_callback_ = std::move(callback);
+  advertisement_->RemoveObserver(this);
+  advertisement_->Unregister(
+      base::BindOnce(&FastPairAdvertiser::OnUnregisterAdvertisement,
+                     weak_ptr_factory_.GetWeakPtr()),
+      base::BindOnce(&FastPairAdvertiser::OnUnregisterAdvertisementError,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void FastPairAdvertiser::OnUnregisterAdvertisement() {
+  advertisement_.reset();
+  std::move(stop_callback_).Run();
+  // |this| might be destroyed here, do not access local fields.
+}
+
+void FastPairAdvertiser::OnUnregisterAdvertisementError(
+    device::BluetoothAdvertisement::ErrorCode error_code) {
+  LOG(WARNING) << __func__ << " failed with error code = " << error_code;
+  advertisement_.reset();
+  std::move(stop_callback_).Run();
+  // |this| might be destroyed here, do not access local fields.
+}
+
+std::vector<uint8_t> FastPairAdvertiser::GenerateServiceMetadata() {
+  std::vector<uint8_t> metadata;
+
+  // Note: We convert this to a positive value before transport to align with
+  // Android's behavior.
+  int8_t powerConverted = -kAdjustedTxPower;
+  metadata.push_back(powerConverted);
+  return metadata;
+}
+
+std::vector<uint8_t> FastPairAdvertiser::GenerateManufacturerMetadata() {
+  // TODO(b/235403498): This code may need to be updated later to be derived
+  // from the device BT address. It is not required in order for the
+  // advertisement to trigger the Fast Pair halfsheet.
+  auto token = base::UnguessableToken::Create();
+  base::span<const uint8_t> fast_pair_code = token.AsBytes().first(2);
+
+  std::vector<uint8_t> metadata(std::begin(fast_pair_code),
+                                std::end(fast_pair_code));
+
+  return metadata;
+}
---
+++ chromeos/ash/components/oobe_quick_start/connectivity/fast_pair_advertiser_unittest.cc
@@ -0,0 +1,270 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/oobe_quick_start/connectivity/fast_pair_advertiser.h"
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "base/callback_helpers.h"
+#include "base/memory/ptr_util.h"
+#include "base/test/bind.h"
+#include "base/time/time.h"
+#include "device/bluetooth/test/mock_bluetooth_adapter.h"
+#include "device/bluetooth/test/mock_bluetooth_advertisement.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::_;
+using testing::NiceMock;
+using testing::Return;
+
+namespace {
+constexpr const char kFastPairServiceUuid[] =
+    "0000fe2c-0000-1000-8000-00805f9b34fb";
+const uint8_t kFastPairModelId[] = {0x41, 0xc0, 0xd9};
+const int8_t kAdjustedTxPower = -66;
+
+}  // namespace
+
+struct RegisterAdvertisementArgs {
+  RegisterAdvertisementArgs(
+      const device::BluetoothAdvertisement::UUIDList& service_uuids,
+      const device::BluetoothAdvertisement::ServiceData& service_data,
+      device::BluetoothAdapter::CreateAdvertisementCallback callback,
+      device::BluetoothAdapter::AdvertisementErrorCallback error_callback)
+      : service_uuids(service_uuids),
+        service_data(service_data),
+        callback(std::move(callback)),
+        error_callback(std::move(error_callback)) {}
+
+  device::BluetoothAdvertisement::UUIDList service_uuids;
+  device::BluetoothAdvertisement::ServiceData service_data;
+  device::BluetoothAdapter::CreateAdvertisementCallback callback;
+  device::BluetoothAdapter::AdvertisementErrorCallback error_callback;
+};
+
+class MockBluetoothAdapterWithAdvertisements
+    : public device::MockBluetoothAdapter {
+ public:
+  MOCK_METHOD1(RegisterAdvertisementWithArgsStruct,
+               void(RegisterAdvertisementArgs*));
+
+  void RegisterAdvertisement(
+      std::unique_ptr<device::BluetoothAdvertisement::Data> advertisement_data,
+      device::BluetoothAdapter::CreateAdvertisementCallback callback,
+      device::BluetoothAdapter::AdvertisementErrorCallback error_callback)
+      override {
+    RegisterAdvertisementWithArgsStruct(new RegisterAdvertisementArgs(
+        *advertisement_data->service_uuids(),
+        *advertisement_data->service_data(), std::move(callback),
+        std::move(error_callback)));
+  }
+
+ protected:
+  ~MockBluetoothAdapterWithAdvertisements() override = default;
+};
+
+class FakeBluetoothAdvertisement : public device::BluetoothAdvertisement {
+ public:
+  // device::BluetoothAdvertisement:
+  void Unregister(SuccessCallback success_callback,
+                  ErrorCallback error_callback) override {
+    // Both of these callbacks destroy the BluetoothAdvertisement object, so
+    // update the |called_unregister_success_callback_| and
+    // |called_unregister_error_callback_| bools immediately before actually
+    // invoking their respective callbacks in order to avoid the use of heap
+    // allocated memory after it has been freed.
+    if (should_unregister_succeed_) {
+      called_unregister_success_callback_ = true;
+      std::move(success_callback).Run();
+      return;
+    }
+
+    called_unregister_error_callback_ = true;
+    std::move(error_callback)
+        .Run(device::BluetoothAdvertisement::ErrorCode::
+                 INVALID_ADVERTISEMENT_ERROR_CODE);
+  }
+
+  bool HasObserver(Observer* observer) {
+    return observers_.HasObserver(observer);
+  }
+
+  void ReleaseAdvertisement() {
+    for (auto& observer : observers_)
+      observer.AdvertisementReleased(this);
+  }
+
+  bool called_unregister_success_callback() {
+    return called_unregister_success_callback_;
+  }
+
+  bool called_unregister_error_callback() {
+    return called_unregister_error_callback_;
+  }
+
+  void set_should_unregister_succeed(bool should_unregister_succeed) {
+    should_unregister_succeed_ = should_unregister_succeed;
+  }
+
+ protected:
+  ~FakeBluetoothAdvertisement() override = default;
+
+  bool should_unregister_succeed_ = true;
+  bool called_unregister_success_callback_ = false;
+  bool called_unregister_error_callback_ = false;
+};
+
+class FastPairAdvertiserTest : public testing::Test {
+ public:
+  FastPairAdvertiserTest(const FastPairAdvertiserTest&) = delete;
+  FastPairAdvertiserTest& operator=(const FastPairAdvertiserTest&) = delete;
+
+ protected:
+  FastPairAdvertiserTest() = default;
+
+  void SetUp() override {
+    mock_adapter_ = base::MakeRefCounted<
+        NiceMock<MockBluetoothAdapterWithAdvertisements>>();
+    ON_CALL(*mock_adapter_, IsPresent()).WillByDefault(Return(true));
+    ON_CALL(*mock_adapter_, IsPowered()).WillByDefault(Return(true));
+    ON_CALL(*mock_adapter_, RegisterAdvertisementWithArgsStruct(_))
+        .WillByDefault(Invoke(
+            this, &FastPairAdvertiserTest::OnAdapterRegisterAdvertisement));
+
+    fast_pair_advertiser_ = std::make_unique<FastPairAdvertiser>(mock_adapter_);
+  }
+
+  void OnAdapterRegisterAdvertisement(RegisterAdvertisementArgs* args) {
+    register_args_ = base::WrapUnique(args);
+  }
+
+  void StartAdvertising() {
+    fast_pair_advertiser_->StartAdvertising(
+        base::BindOnce(&FastPairAdvertiserTest::OnStartAdvertising,
+                       base::Unretained(this)),
+        base::BindOnce(&FastPairAdvertiserTest::OnStartAdvertisingError,
+                       base::Unretained(this)));
+    auto service_uuid_list =
+        std::make_unique<device::BluetoothAdvertisement::UUIDList>();
+    service_uuid_list->push_back(kFastPairServiceUuid);
+    EXPECT_EQ(*service_uuid_list, register_args_->service_uuids);
+
+    auto expected_payload = std::vector<uint8_t>(std::begin(kFastPairModelId),
+                                                 std::end(kFastPairModelId));
+    std::vector<uint8_t> metadata;
+    int8_t power_converted = -kAdjustedTxPower;
+    metadata.push_back(power_converted);
+    expected_payload.insert(std::end(expected_payload), std::begin(metadata),
+                            std::end(metadata));
+
+    EXPECT_EQ(expected_payload,
+              register_args_->service_data[kFastPairServiceUuid]);
+  }
+
+  void StopAdvertising() {
+    fast_pair_advertiser_->StopAdvertising(base::BindOnce(
+        &FastPairAdvertiserTest::OnStopAdvertising, base::Unretained(this)));
+  }
+
+  void OnStartAdvertising() { called_on_start_advertising_ = true; }
+
+  void OnStartAdvertisingError() { called_on_start_advertising_error_ = true; }
+
+  void OnStopAdvertising() { called_on_stop_advertising_ = true; }
+
+  bool called_on_start_advertising() { return called_on_start_advertising_; }
+  bool called_on_start_advertising_error() {
+    return called_on_start_advertising_error_;
+  }
+  bool called_on_stop_advertising() { return called_on_stop_advertising_; }
+
+  scoped_refptr<NiceMock<MockBluetoothAdapterWithAdvertisements>> mock_adapter_;
+  std::unique_ptr<FastPairAdvertiser> fast_pair_advertiser_;
+  std::unique_ptr<RegisterAdvertisementArgs> register_args_;
+  bool called_on_start_advertising_ = false;
+  bool called_on_start_advertising_error_ = false;
+  bool called_on_stop_advertising_ = false;
+};
+
+TEST_F(FastPairAdvertiserTest, TestStartAdvertising_Success) {
+  StartAdvertising();
+  auto fake_advertisement = base::MakeRefCounted<FakeBluetoothAdvertisement>();
+  std::move(register_args_->callback).Run(fake_advertisement);
+
+  EXPECT_TRUE(called_on_start_advertising());
+  EXPECT_FALSE(called_on_start_advertising_error());
+  EXPECT_FALSE(called_on_stop_advertising());
+  EXPECT_TRUE(fake_advertisement->HasObserver(fast_pair_advertiser_.get()));
+}
+
+TEST_F(FastPairAdvertiserTest, TestStartAdvertising_Error) {
+  StartAdvertising();
+  std::move(register_args_->error_callback)
+      .Run(device::BluetoothAdvertisement::ErrorCode::
+               INVALID_ADVERTISEMENT_ERROR_CODE);
+
+  EXPECT_FALSE(called_on_start_advertising());
+  EXPECT_TRUE(called_on_start_advertising_error());
+  EXPECT_FALSE(called_on_stop_advertising());
+}
+
+// Regression test for crbug.com/1109581.
+TEST_F(FastPairAdvertiserTest, TestStartAdvertising_DeleteInErrorCallback) {
+  fast_pair_advertiser_->StartAdvertising(
+      base::DoNothing(),
+      base::BindLambdaForTesting([&]() { fast_pair_advertiser_.reset(); }));
+
+  std::move(register_args_->error_callback)
+      .Run(device::BluetoothAdvertisement::ErrorCode::
+               INVALID_ADVERTISEMENT_ERROR_CODE);
+
+  EXPECT_FALSE(fast_pair_advertiser_);
+}
+
+TEST_F(FastPairAdvertiserTest, TestStopAdvertising_Success) {
+  StartAdvertising();
+  auto fake_advertisement = base::MakeRefCounted<FakeBluetoothAdvertisement>();
+  std::move(register_args_->callback).Run(fake_advertisement);
+
+  fake_advertisement->set_should_unregister_succeed(true);
+  StopAdvertising();
+
+  EXPECT_TRUE(fake_advertisement->called_unregister_success_callback());
+  EXPECT_FALSE(fake_advertisement->called_unregister_error_callback());
+  EXPECT_TRUE(called_on_start_advertising());
+  EXPECT_FALSE(called_on_start_advertising_error());
+  EXPECT_TRUE(called_on_stop_advertising());
+}
+
+TEST_F(FastPairAdvertiserTest, TestStopAdvertising_Error) {
+  StartAdvertising();
+  auto fake_advertisement = base::MakeRefCounted<FakeBluetoothAdvertisement>();
+  std::move(register_args_->callback).Run(fake_advertisement);
+
+  fake_advertisement->set_should_unregister_succeed(false);
+  StopAdvertising();
+
+  EXPECT_FALSE(fake_advertisement->called_unregister_success_callback());
+  EXPECT_TRUE(fake_advertisement->called_unregister_error_callback());
+  EXPECT_TRUE(called_on_start_advertising());
+  EXPECT_FALSE(called_on_start_advertising_error());
+  EXPECT_TRUE(called_on_stop_advertising());
+}
+
+TEST_F(FastPairAdvertiserTest, TestAdvertisementReleased) {
+  StartAdvertising();
+  auto fake_advertisement = base::MakeRefCounted<FakeBluetoothAdvertisement>();
+  std::move(register_args_->callback).Run(fake_advertisement);
+
+  EXPECT_TRUE(fake_advertisement->HasObserver(fast_pair_advertiser_.get()));
+
+  fake_advertisement->ReleaseAdvertisement();
+
+  EXPECT_TRUE(called_on_start_advertising());
+  EXPECT_FALSE(called_on_start_advertising_error());
+  EXPECT_FALSE(called_on_stop_advertising());
+  EXPECT_FALSE(fake_advertisement->HasObserver(fast_pair_advertiser_.get()));
+}
