dc0275c8445e78b7ee782efe897924f9aa7c302c
Lijin Shen <lazzzis@google.com>
2022-06-14 20:25:28 +0000

    Fix back press not closing tab in groups
    The launch type of a tab opened by clicking on context menu is
    FROM_LONGPRESS_BACKGROUND_IN_GROUP[1]. But tab of this type does not
    show toast "open in background" and is not closed when back gesture
    is performed.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java;l=618
    
    Bug: 1333390
    Change-Id: I86674e1db9d36ace8499ac0070dd6f08296f4399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704065
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014134}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -642,6 +642,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                 public void didAddTab(
                         Tab tab, @TabLaunchType int type, @TabCreationState int creationState) {
                     if (type == TabLaunchType.FROM_LONGPRESS_BACKGROUND
+                            || type == TabLaunchType.FROM_LONGPRESS_BACKGROUND_IN_GROUP
                             || type == TabLaunchType.FROM_RECENT_TABS
                                     && !DeviceClassManager.enableAnimations()) {
                         Toast.makeText(ChromeTabbedActivity.this, R.string.open_in_new_tab_toast,
@@ -2335,6 +2336,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                 || type == TabLaunchType.FROM_LONGPRESS_FOREGROUND
                 || type == TabLaunchType.FROM_LONGPRESS_INCOGNITO
                 || type == TabLaunchType.FROM_LONGPRESS_BACKGROUND
+                || type == TabLaunchType.FROM_LONGPRESS_BACKGROUND_IN_GROUP
                 || type == TabLaunchType.FROM_RECENT_TABS
                 || (type == TabLaunchType.FROM_RESTORE
                         && CriticalPersistedTabData.from(tab).getParentId() != Tab.INVALID_TAB_ID);
