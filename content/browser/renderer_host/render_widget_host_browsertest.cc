60d445904cd0f83ee1ea099ebc45cc86cad86919
Sahir Vellani <sahir.vellani@microsoft.com>
2022-06-14 19:25:31 +0000

    Filter DelegatedInk metadatas with same timestamp
    In order to prevent unnecessary work in the GPU process and extra calls
    to the windows StartNewTrail method, filter out delegated ink metadatas
    in DelegatedInkTrailPresenter::updateInkTrailStartPoint if the
    timestamp of the event is the same as its predecessor.
    
    Duplicate timestamps can be the result of the web app calling updateInkTrailStartPoint repeatedly with the same event. This results
    in the same metadata being sent to the DelegatedInkPointRenderer, which
    causes a drop in points being rendered until a new metadata is sent.
    
    Change-Id: I14de7b3c0aee5c9e71f3665763dc697475437947
    Bug: 1335238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698883
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014102}

--- content/browser/renderer_host/render_widget_host_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_browsertest.cc
@@ -978,4 +978,46 @@ IN_PROC_BROWSER_TEST_F(RenderWidgetHostDelegatedInkMetadataTest,
       host()->render_frame_metadata_provider()->LastRenderFrameMetadata());
 }
 
+// If the DelegatedInkTrailPresenter creates a metadata that has the same
+// timestamp as the previous one, it does not set the metadata.
+IN_PROC_BROWSER_TEST_F(RenderWidgetHostDelegatedInkMetadataTest,
+                       DuplicateMetadata) {
+  ASSERT_TRUE(ExecJs(shell()->web_contents(), R"(
+      let presenter = null;
+      navigator.ink.requestPresenter().then(e => { presenter = e; });
+      let style = { color: 'green', diameter: 21 };
+      let first_move_event = null;
+
+      window.addEventListener('pointermove' , evt => {
+        if (first_move_event == null) {
+          first_move_event = evt;
+        }
+        presenter.updateInkTrailStartPoint(first_move_event, style);
+      });
+      )"));
+  SimulateRoutedMouseEvent(blink::WebInputEvent::Type::kMouseMove, 10, 10, 0,
+                           false);
+  RunUntilInputProcessed(host());
+
+  {
+    const cc::RenderFrameMetadata& last_metadata =
+        host()->render_frame_metadata_provider()->LastRenderFrameMetadata();
+    EXPECT_TRUE(last_metadata.delegated_ink_metadata.has_value());
+    EXPECT_TRUE(
+        last_metadata.delegated_ink_metadata.value().delegated_ink_is_hovering);
+  }
+
+  // Confirm metadata has no value when updateInkTrailStartPoint is called
+  // with the same event.
+  SimulateRoutedMouseEvent(blink::WebInputEvent::Type::kMouseMove, 20, 20,
+                           blink::WebInputEvent::kLeftButtonDown, false);
+  RunUntilInputProcessed(host());
+
+  {
+    const cc::RenderFrameMetadata& last_metadata =
+        host()->render_frame_metadata_provider()->LastRenderFrameMetadata();
+    EXPECT_FALSE(last_metadata.delegated_ink_metadata.has_value());
+  }
+}
+
 }  // namespace content
