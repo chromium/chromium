afd86ccb917cfa16a584999a73ac8fe28b632424
ckitagawa <ckitagawa@chromium.org>
2022-06-15 18:14:21 +0000

    [Fixit] Batch unit tests
    Batch some unit tests I moved yesterday.
    
    Change-Id: If96a11d296679edba0ebc91bb5aa29227d2a154d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708053
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014538}

--- components/embedder_support/android/javatests/src/org/chromium/components/embedder_support/delegate/ColorPickerDialogRenderTest.java
+++ components/embedder_support/android/javatests/src/org/chromium/components/embedder_support/delegate/ColorPickerDialogRenderTest.java
@@ -22,6 +22,7 @@ import org.chromium.base.test.params.ParameterAnnotations;
 import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.R;
@@ -37,6 +38,7 @@ import java.util.List;
  */
 @RunWith(ParameterizedRunner.class)
 @UseRunnerDelegate(BaseJUnit4RunnerDelegate.class)
+@Batch(Batch.UNIT_TESTS)
 public class ColorPickerDialogRenderTest extends BlankUiTestActivityTestCase {
     @ParameterAnnotations.ClassParameter
     private static List<ParameterSet> sClassParams =
