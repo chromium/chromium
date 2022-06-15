446efe46fd53758f4ea9cf4ae145c3090cf35fda
Jason Thai <jasontt@chromium.org>
2022-06-15 02:55:45 +0000

    personalization: add entry point metric for backlight icon
    Add an additional enum value for the backlight icon entry point to the
    personalization hub.
    
    BUG=b/229289414
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I0c2ae6a00cf3223b4fe432cb36bd8bd3d3cd5afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3704842
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014271}

--- ash/system/keyboard_brightness/unified_keyboard_brightness_slider_controller.cc
+++ ash/system/keyboard_brightness/unified_keyboard_brightness_slider_controller.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "ash/constants/ash_features.h"
+#include "ash/constants/personalization_entry_point.h"
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -20,6 +21,7 @@
 #include "ash/webui/personalization_app/mojom/personalization_app.mojom-forward.h"
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -99,6 +101,10 @@ class UnifiedKeyboardBrightnessView : public UnifiedSliderView,
   }
 
   void OnKeyboardBacklightColorIconPressed() {
+    // Record entry point metric to Personalization Hub.
+    base::UmaHistogramEnumeration(
+        kPersonalizationEntryPointHistogramName,
+        PersonalizationEntryPoint::kKeyboardBrightnessSlider);
     NewWindowDelegate* primary_delegate = NewWindowDelegate::GetPrimary();
     primary_delegate->OpenPersonalizationHub();
     return;
