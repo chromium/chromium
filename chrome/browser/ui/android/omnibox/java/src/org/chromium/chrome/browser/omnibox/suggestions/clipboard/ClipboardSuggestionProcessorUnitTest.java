d770a571547d2c3de91fe534924c4d12936ab73c
Gang Wu <gangwu@chromium.org>
2022-06-15 02:03:35 +0000

    Relocate ClipboardSuggestionProcessorUnitTest to JUnitTests.
    Fixed:1331897
    
    Change-Id: Ib917629fe2347851546a2f0010955f16c4e40860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705997
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014247}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/clipboard/ClipboardSuggestionProcessorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/clipboard/ClipboardSuggestionProcessorUnitTest.java
@@ -11,7 +11,6 @@ import static org.mockito.Mockito.verify;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.drawable.BitmapDrawable;
 import android.view.ContextThemeWrapper;
 import android.view.ViewGroup;
@@ -22,16 +21,17 @@ import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.UiThreadTest;
-import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties;
@@ -45,27 +45,26 @@ import org.chromium.components.favicon.LargeIconBridge;
 import org.chromium.components.favicon.LargeIconBridge.LargeIconCallback;
 import org.chromium.components.omnibox.AutocompleteMatch;
 import org.chromium.components.omnibox.AutocompleteMatchBuilder;
-import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.url.GURL;
+import org.chromium.url.JUnitTestGURLs;
+import org.chromium.url.ShadowGURL;
 
 import java.io.ByteArrayOutputStream;
 
 /**
  * Tests for {@link ClipboardSuggestionProcessor}.
  */
-@RunWith(BaseJUnit4ClassRunner.class)
-@Batch(Batch.UNIT_TESTS)
-public class ClipboardSuggestionProcessorTest {
-    private static final GURL TEST_URL = new GURL("http://url");
-    private static final GURL ARABIC_URL = new GURL("http://site.com/مرحبا/123");
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE, shadows = {ShadowGURL.class})
+public class ClipboardSuggestionProcessorUnitTest {
+    private static final GURL TEST_URL = JUnitTestGURLs.getGURL(JUnitTestGURLs.EXAMPLE_URL);
 
-    @Mock
-    SuggestionHost mSuggestionHost;
-    @Mock
-    LargeIconBridge mIconBridge;
-    @Mock
-    Resources mResources;
+    public @Rule MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    private @Mock SuggestionHost mSuggestionHost;
+    private @Mock LargeIconBridge mIconBridge;
+    private @Mock Resources mResources;
 
     private Context mContext;
     private ClipboardSuggestionProcessor mProcessor;
@@ -79,11 +78,9 @@ public class ClipboardSuggestionProcessorTest {
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        NativeLibraryTestUtils.loadNativeLibraryNoBrowserProcess();
         mContext = new ContextThemeWrapper(
                 ContextUtils.getApplicationContext(), R.style.Theme_BrowserUI_DayNight);
-        mBitmap = Bitmap.createBitmap(10, 5, Config.ARGB_8888);
+        mBitmap = Bitmap.createBitmap(10, 5, Bitmap.Config.ARGB_8888);
         mProcessor = new ClipboardSuggestionProcessor(mContext, mSuggestionHost, () -> mIconBridge);
         mRootView = new LinearLayout(mContext);
         mTitleTextView = new TextView(mContext);
@@ -140,7 +137,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_identifyUrlSuggestion() {
         createClipboardSuggestion(OmniboxSuggestionType.CLIPBOARD_URL, GURL.emptyGURL());
         Assert.assertFalse(mModel.get(SuggestionViewProperties.IS_SEARCH_SUGGESTION));
@@ -152,7 +148,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_showsFaviconWhenAvailable() {
         final ArgumentCaptor<LargeIconCallback> callback =
                 ArgumentCaptor.forClass(LargeIconCallback.class);
@@ -171,7 +166,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_showsFallbackIconWhenNoFaviconIsAvailable() {
         final ArgumentCaptor<LargeIconCallback> callback =
                 ArgumentCaptor.forClass(LargeIconCallback.class);
@@ -189,14 +183,13 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipobardSuggestion_urlAndTextDirection() {
         final ArgumentCaptor<LargeIconCallback> callback =
                 ArgumentCaptor.forClass(LargeIconCallback.class);
         // URL
-        createClipboardSuggestionAndClickReveal(OmniboxSuggestionType.CLIPBOARD_URL, ARABIC_URL);
+        createClipboardSuggestionAndClickReveal(OmniboxSuggestionType.CLIPBOARD_URL, TEST_URL);
         Assert.assertFalse(mModel.get(SuggestionViewProperties.IS_SEARCH_SUGGESTION));
-        verify(mIconBridge).getLargeIconForUrl(eq(ARABIC_URL), anyInt(), callback.capture());
+        verify(mIconBridge).getLargeIconForUrl(eq(TEST_URL), anyInt(), callback.capture());
         callback.getValue().onLargeIconAvailable(null, 0, false, 0);
         Assert.assertEquals(TextView.TEXT_DIRECTION_LTR, mLastSetTextDirection);
 
@@ -208,7 +201,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_showsThumbnailWhenAvailable() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         Assert.assertTrue(mBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos));
@@ -228,12 +220,11 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_thumbnailShouldResizeIfTooLarge() {
         int size = mContext.getResources().getDimensionPixelSize(
                 R.dimen.omnibox_suggestion_decoration_image_size);
 
-        Bitmap largeBitmap = Bitmap.createBitmap(size * 2, size * 2, Config.ARGB_8888);
+        Bitmap largeBitmap = Bitmap.createBitmap(size * 2, size * 2, Bitmap.Config.ARGB_8888);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         Assert.assertTrue(largeBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos));
         byte[] bitmapData = baos.toByteArray();
@@ -248,7 +239,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_revealButton() {
         createClipboardSuggestion(OmniboxSuggestionType.CLIPBOARD_URL, GURL.emptyGURL());
         Assert.assertNotNull(mModel.get(BaseSuggestionViewProperties.ACTIONS));
@@ -268,7 +258,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_noContentByDefault() {
         createClipboardSuggestion(OmniboxSuggestionType.CLIPBOARD_URL, GURL.emptyGURL());
         SuggestionSpannable textLine2 = mModel.get(SuggestionViewProperties.TEXT_LINE_2_TEXT);
@@ -285,7 +274,6 @@ public class ClipboardSuggestionProcessorTest {
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void clipboardSuggestion_revealAndConcealButton() {
         createClipboardSuggestion(OmniboxSuggestionType.CLIPBOARD_URL, GURL.emptyGURL());
         SuggestionSpannable textLine2 = mModel.get(SuggestionViewProperties.TEXT_LINE_2_TEXT);
