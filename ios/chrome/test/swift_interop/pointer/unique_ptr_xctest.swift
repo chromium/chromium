3c9f11608775f9c97bad6591dd9b4603a6e461ab
Rohit Rao <rohitrao@chromium.org>
2022-06-16 12:13:17 +0000

    [ios] Updates unique_ptr_xctest.swift for Swift 5.7.
    One assertion in this test no longer compiles in Official builds with
    Swift 5.7.
    
    BUG=1331345
    
    Change-Id: I07bd1b54a0f945e26c63e740fa533bcd362671a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706797
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014880}

--- ios/chrome/test/swift_interop/pointer/unique_ptr_xctest.swift
+++ ios/chrome/test/swift_interop/pointer/unique_ptr_xctest.swift
@@ -12,7 +12,12 @@ class UniquePointerTest: XCTestCase {
     var returner = ValueReturner()
     let object = returner.ObjectPointer()!
     XCTAssertEqual(object.pointee.IsValid(), true, "")
-    XCTAssertEqual(object.pointee.GetValue(), 17, "")
+
+    // DOESN'T COMPILE: value of type 'Value' has no member 'GetValue'
+    // in 5.7 official builds. http://crbug.com/1336937
+    #if swift(<5.7)
+      XCTAssertEqual(object.pointee.GetValue(), 17, "")
+    #endif
   }
 
 }
