e481244c0ce4a40f62a6e640d5c2af454a1661e2
Matt Menke <mmenke@chromium.org>
2022-06-16 18:38:16 +0000

    Remove url_fetcher.h includes from gcm/
    The class has been deprecated for quite some time, and we're working on
    removing it.
    
    Also remove some other unused net/ includes.
    
    Bug: 1010491
    Change-Id: I337f60d6f4170c8fcc803af63c40043c04d42532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698673
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015013}

--- google_apis/gcm/engine/gcm_unregistration_request_handler.cc
+++ google_apis/gcm/engine/gcm_unregistration_request_handler.cc
@@ -4,9 +4,9 @@
 
 #include "google_apis/gcm/engine/gcm_unregistration_request_handler.h"
 
+#include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "google_apis/gcm/base/gcm_util.h"
-#include "net/url_request/url_fetcher.h"
 
 namespace gcm {
 
