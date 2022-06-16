366872efdb1dbb9ae5fc0098be3518c6df02ada6
Ender <ender@google.com>
2022-06-16 19:18:36 +0000

    Relocate EditUrlSuggestionUnitTest to JUnitTests.
    Change-Id: I70ce03fcb0a86d1fe394d3a5ebcb9269d0b46ca5
    Fixed: 1331898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3705875
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015019}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionUnitTest.java
@@ -24,13 +24,13 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.UserDataHost;
-import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.UiThreadTest;
-import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.omnibox.suggestions.FaviconFetcher;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionHost;
@@ -46,21 +46,21 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.omnibox.AutocompleteMatch;
 import org.chromium.components.search_engines.TemplateUrlService;
-import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.base.Clipboard;
-import org.chromium.ui.base.ClipboardAndroidTestSupport;
 import org.chromium.ui.base.ClipboardImpl;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.url.GURL;
+import org.chromium.url.JUnitTestGURLs;
+import org.chromium.url.ShadowGURL;
 
 import java.util.List;
 
 /**
  * Unit tests for the "edit url" omnibox suggestion.
  */
-@RunWith(BaseJUnit4ClassRunner.class)
-@Batch(Batch.UNIT_TESTS)
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE, shadows = {ShadowGURL.class})
 public final class EditUrlSuggestionUnitTest {
     private static final String TEST_TITLE = "Test Page";
     private static final String FOOBAR_SEARCH_TERMS = "foobar";
@@ -70,65 +70,35 @@ public final class EditUrlSuggestionUnitTest {
     private static final int ACTION_COPY = 1;
     private static final int ACTION_EDIT = 2;
 
-    private final GURL mTestUrl = new GURL("http://www.example.com");
-    private final GURL mFoobarSearchUrl =
-            new GURL("http://www.example.com?q=" + FOOBAR_SEARCH_TERMS);
-    private final GURL mBarbazSearchUrl =
-            new GURL("http://www.example.com?q=" + BARBAZ_SEARCH_TERMS);
-    private EditUrlSuggestionProcessor mProcessor;
-    private PropertyModel mModel;
+    private static final GURL WEB_URL = JUnitTestGURLs.getGURL(JUnitTestGURLs.URL_1);
+    private static final GURL SEARCH_URL_1 = JUnitTestGURLs.getGURL(JUnitTestGURLs.SEARCH_URL);
+    private static final GURL SEARCH_URL_2 = JUnitTestGURLs.getGURL(JUnitTestGURLs.SEARCH_2_URL);
 
-    @Rule
-    public TestRule mFeaturesProcessor = new Features.JUnitProcessor();
+    public @Rule TestRule mFeaturesProcessor = new Features.JUnitProcessor();
+    public @Rule MockitoRule mMockitoRule = MockitoJUnit.rule();
 
-    @Mock
-    private ShareDelegate mShareDelegate;
-
-    @Mock
-    private Tab mTab;
-
-    @Mock
-    private SadTab mSadTab;
-
-    @Mock
-    private AutocompleteMatch mWhatYouTypedSuggestion;
-
-    @Mock
-    private AutocompleteMatch mOtherSuggestion;
-
-    @Mock
-    private AutocompleteMatch mSearchSuggestion;
-
-    @Mock
-    private UrlBarDelegate mUrlBarDelegate;
-
-    @Mock
-    private View mEditButton;
-
-    @Mock
-    private View mSuggestionView;
-
-    @Mock
-    private FaviconFetcher mIconFetcher;
-
-    @Mock
-    private TemplateUrlService mTemplateUrlService;
-
-    @Mock
-    private SuggestionHost mSuggestionHost;
-
-    @Mock
-    private ClipboardManager mClipboardManager;
+    private @Mock ShareDelegate mShareDelegate;
+    private @Mock Tab mTab;
+    private @Mock SadTab mSadTab;
+    private @Mock AutocompleteMatch mWhatYouTypedSuggestion;
+    private @Mock AutocompleteMatch mOtherSuggestion;
+    private @Mock AutocompleteMatch mSearchSuggestion;
+    private @Mock UrlBarDelegate mUrlBarDelegate;
+    private @Mock View mEditButton;
+    private @Mock View mSuggestionView;
+    private @Mock FaviconFetcher mIconFetcher;
+    private @Mock TemplateUrlService mTemplateUrlService;
+    private @Mock SuggestionHost mSuggestionHost;
+    private @Mock ClipboardManager mClipboardManager;
 
     // The original (real) ClipboardManager to be restored after a test run.
     private ClipboardManager mOldClipboardManager;
     private UserDataHost mUserDataHost;
+    private EditUrlSuggestionProcessor mProcessor;
+    private PropertyModel mModel;
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        NativeLibraryTestUtils.loadNativeLibraryNoBrowserProcess();
-
         TemplateUrlServiceFactory.setInstanceForTesting(mTemplateUrlService);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
@@ -145,7 +115,7 @@ public final class EditUrlSuggestionUnitTest {
                     () -> mTab, () -> mShareDelegate);
         });
 
-        doReturn(mTestUrl).when(mTab).getUrl();
+        doReturn(WEB_URL).when(mTab).getUrl();
         doReturn(TEST_TITLE).when(mTab).getTitle();
         doReturn(false).when(mTab).isNativePage();
         doReturn(true).when(mTab).isInitialized();
@@ -154,11 +124,11 @@ public final class EditUrlSuggestionUnitTest {
         doReturn(mUserDataHost).when(mTab).getUserDataHost();
         doReturn(false).when(mSadTab).isShowing();
         doReturn(OmniboxSuggestionType.URL_WHAT_YOU_TYPED).when(mWhatYouTypedSuggestion).getType();
-        doReturn(mTestUrl.getSpec()).when(mWhatYouTypedSuggestion).getDisplayText();
-        doReturn(mTestUrl).when(mWhatYouTypedSuggestion).getUrl();
+        doReturn(WEB_URL.getSpec()).when(mWhatYouTypedSuggestion).getDisplayText();
+        doReturn(WEB_URL).when(mWhatYouTypedSuggestion).getUrl();
 
         doReturn(OmniboxSuggestionType.SEARCH_WHAT_YOU_TYPED).when(mSearchSuggestion).getType();
-        doReturn(mFoobarSearchUrl).when(mSearchSuggestion).getUrl();
+        doReturn(SEARCH_URL_1).when(mSearchSuggestion).getUrl();
         doReturn(FOOBAR_SEARCH_TERMS).when(mSearchSuggestion).getFillIntoEdit();
 
         doReturn(OmniboxSuggestionType.SEARCH_HISTORY).when(mOtherSuggestion).getType();
@@ -170,13 +140,11 @@ public final class EditUrlSuggestionUnitTest {
             ((ClipboardImpl) Clipboard.getInstance())
                     .overrideClipboardManagerForTesting(mOldClipboardManager);
         });
-        ClipboardAndroidTestSupport.cleanup();
     }
 
     /** Test that the suggestion is triggered. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testUrlSuggestionTriggered() {
         verifyUrlSuggestionTriggered(/* isIncognito */ false);
     }
@@ -184,7 +152,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test that the suggestion is triggered in Incognito. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testSuggestionTriggered_Incognito() {
         verifyUrlSuggestionTriggered(/* isIncognito */ true);
     }
@@ -192,7 +159,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test that the suggestion is not triggered if its url doesn't match the current page's. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testWhatYouTypedWrongUrl() {
         verifyWhatYouTypedWrongUrl(/* isIncognito */ false);
     }
@@ -203,7 +169,6 @@ public final class EditUrlSuggestionUnitTest {
      */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testWhatYouTypedWrongUrl_Incognito() {
         verifyWhatYouTypedWrongUrl(/* isIncognito */ true);
     }
@@ -211,7 +176,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test the edit button is pressed, the correct method in the URL bar delegate is triggered. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testEditButtonPress() {
         verifyEditButtonPress(/* isIncognito */ false);
     }
@@ -222,7 +186,6 @@ public final class EditUrlSuggestionUnitTest {
      */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testEditButtonPress_Incognito() {
         verifyEditButtonPress(/* isIncognito */ true);
     }
@@ -230,7 +193,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test the share button is pressed, we trigger the share menu. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testShareButtonPress() {
         verifyShareButtonPress(/* isIncognito */ false);
     }
@@ -238,7 +200,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test the share button is pressed, we trigger the share menu in Incognito. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testShareButtonPress_Incognito() {
         verifyShareButtonPress(/* isIncognito */ true);
     }
@@ -246,7 +207,6 @@ public final class EditUrlSuggestionUnitTest {
     /** Test the copy button is pressed, we update clipboard. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testCopyButtonPress() {
         verifyCopyButtonPress(/* isIncognito */ false);
     }
@@ -254,21 +214,18 @@ public final class EditUrlSuggestionUnitTest {
     /** Test the copy button is pressed, we update clipboard in Incognito. */
     @Test
     @SmallTest
-    @UiThreadTest
     public void testCopyButtonPress_Incognito() {
         verifyCopyButtonPress(/* isIncognito */ true);
     }
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void testSearchSuggestionTriggered() {
         verifySearchSuggestionTriggered(/* isIncognito */ false);
     }
 
     @Test
     @SmallTest
-    @UiThreadTest
     public void testSearchSuggestionTriggered_Incognito() {
         verifySearchSuggestionTriggered(/* isIncognito */ true);
     }
@@ -285,8 +242,7 @@ public final class EditUrlSuggestionUnitTest {
         Assert.assertEquals("The model should have the title set.", TEST_TITLE,
                 mModel.get(SuggestionViewProperties.TEXT_LINE_1_TEXT).toString());
 
-        Assert.assertEquals("The model should have the URL set to the tab's URL",
-                mTestUrl.getSpec(),
+        Assert.assertEquals("The model should have the URL set to the tab's URL", WEB_URL.getSpec(),
                 mModel.get(SuggestionViewProperties.TEXT_LINE_2_TEXT).toString());
     }
 
@@ -294,7 +250,7 @@ public final class EditUrlSuggestionUnitTest {
         setIncognito(isIncognito);
         mProcessor.onUrlFocusChange(true);
 
-        when(mWhatYouTypedSuggestion.getUrl()).thenReturn(mFoobarSearchUrl);
+        when(mWhatYouTypedSuggestion.getUrl()).thenReturn(SEARCH_URL_1);
         Assert.assertFalse("The processor should not handle the suggestion.",
                 mProcessor.doesProcessSuggestion(mWhatYouTypedSuggestion, 0));
     }
@@ -308,7 +264,7 @@ public final class EditUrlSuggestionUnitTest {
         List<Action> actions = mModel.get(BaseSuggestionViewProperties.ACTIONS);
         Assert.assertEquals("EditUrl suggestion should have 3 action buttons.", 3, actions.size());
         actions.get(ACTION_EDIT).callback.run();
-        verify(mUrlBarDelegate).setOmniboxEditingText(mTestUrl.getSpec());
+        verify(mUrlBarDelegate).setOmniboxEditingText(WEB_URL.getSpec());
     }
 
     private void verifyShareButtonPress(boolean isIncognito) {
@@ -338,7 +294,7 @@ public final class EditUrlSuggestionUnitTest {
         verify(mClipboardManager, times(1)).setPrimaryClip(argument.capture());
 
         ClipData clip = new ClipData(
-                "url", new String[] {"text/x-moz-url"}, new ClipData.Item(mTestUrl.getSpec()));
+                "url", new String[] {"text/x-moz-url"}, new ClipData.Item(WEB_URL.getSpec()));
 
         // ClipData doesn't implement equals, but their string representations matching should be
         // good enough.
@@ -347,17 +303,17 @@ public final class EditUrlSuggestionUnitTest {
 
     private void verifySearchSuggestionTriggered(boolean isIncognito) {
         setIncognito(isIncognito);
-        when(mTab.getUrl()).thenReturn(mFoobarSearchUrl);
+        when(mTab.getUrl()).thenReturn(SEARCH_URL_1);
         mProcessor.onUrlFocusChange(true);
-        when(mTemplateUrlService.getSearchQueryForUrl(mFoobarSearchUrl))
+        when(mTemplateUrlService.getSearchQueryForUrl(SEARCH_URL_1))
                 .thenReturn(FOOBAR_SEARCH_TERMS);
-        when(mTemplateUrlService.getSearchQueryForUrl(mBarbazSearchUrl))
+        when(mTemplateUrlService.getSearchQueryForUrl(SEARCH_URL_2))
                 .thenReturn(BARBAZ_SEARCH_TERMS);
 
         Assert.assertFalse(mProcessor.doesProcessSuggestion(mSearchSuggestion, 0));
         Assert.assertFalse(mProcessor.doesProcessSuggestion(mSearchSuggestion, 1));
 
-        when(mSearchSuggestion.getUrl()).thenReturn(mBarbazSearchUrl);
+        when(mSearchSuggestion.getUrl()).thenReturn(SEARCH_URL_2);
         when(mSearchSuggestion.getFillIntoEdit()).thenReturn(BARBAZ_SEARCH_TERMS);
 
         Assert.assertFalse(mProcessor.doesProcessSuggestion(mSearchSuggestion, 0));
