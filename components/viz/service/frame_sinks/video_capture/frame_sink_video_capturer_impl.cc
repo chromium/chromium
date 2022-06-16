e3d965184b88eb1d7138f10ea3f08a6bbf292c62
Jordan Bayles <jophba@chromium.org>
2022-06-16 15:38:56 +0000

    Reland "[Region Capture] fix blue border in iframes"
    This is a reland of commit 684d14c900b56ed8f21e3c59dfce9fafcafcef2c
    
    Original change's description:
    > [Region Capture] fix blue border in iframes
    >
    > This patch changes the FSVCI to no longer output a region_capture_rect
    > whenever the region under capture is embedded in a separate renderer.
    > This effectively resolves a bug where the blue border is drawn in the
    > wrong location in this case.
    >
    > Bug: 1327560
    > Change-Id: I99a2154f9f455e86df2aaa7151f84178e5202c1e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3699030
    > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1014101}
    
    (cherry picked from commit fb777fb09ef681879e46c0ea25e2d34f03ac9f87)
    
    Bug: 1327560
    Change-Id: I4360c1768c50d1c2494769a03038951592d93bb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706576
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1014582}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706910
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Reviewed-by: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/5112@{#95}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
+++ components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
@@ -971,7 +971,17 @@ void FrameSinkVideoCapturerImpl::MaybeCaptureFrame(
     return;
   }
 
-  if (absl::holds_alternative<RegionCaptureCropId>(target_->sub_target)) {
+  // If the target is in a different renderer than the root renderer (indicated
+  // by having a different frame sink ID), we currently cannot provide
+  // reasonable metadata about the region capture rect. For more context, see
+  // https://crbug.com/1327560.
+  //
+  // TODO(https://crbug.com/1335175): Provide accurate bounds for elements
+  // embedded in different renderers.
+  const bool is_same_frame_sink_as_requested =
+      resolved_target_->GetFrameSinkId() == target_->frame_sink_id;
+  if (absl::holds_alternative<RegionCaptureCropId>(target_->sub_target) &&
+      is_same_frame_sink_as_requested) {
     const float scale_factor = frame_metadata.device_scale_factor;
     metadata.region_capture_rect =
         scale_factor ? ScaleToEnclosingRect(capture_region, 1.0f / scale_factor)
