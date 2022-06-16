dd2b2e3c3b044e4b336c273ef674b042a1883bff
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-06-16 18:01:51 +0000

    Add GPO support for Local Content Analysis policies
    This adds logic to bypass the cloud-only requirement of content analysis
    policies when the policy is set to a service provider that supports LCA
    and the LocalContentAnalysisEnabled feature is enabled.
    
    Bug: b:233772217
    Change-Id: Iec25352efa092f18371fafc83c6088a2775ac223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708515
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1014997}

--- chrome/browser/enterprise/connectors/enterprise_connectors_policy_handler_unittest.cc
+++ chrome/browser/enterprise/connectors/enterprise_connectors_policy_handler_unittest.cc
@@ -8,7 +8,10 @@
 #include <tuple>
 
 #include "base/json/json_reader.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_features.h"
+#include "chrome/browser/enterprise/connectors/connectors_prefs.h"
 #include "components/policy/core/browser/policy_error_map.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
@@ -71,15 +74,49 @@ constexpr char kInvalidPolicy[] = R"(
       },
     ])";
 
+constexpr char kValidLocalContentAnalysisPolicy[] = R"(
+    [
+      {
+        "service_provider": "local_test",
+        "enable": "yes",
+      },
+    ])";
+
+constexpr char kInvalidProviderLocalContentAnalysisPolicy[] = R"(
+    [
+      {
+        "service_provider": "google",
+        "enable": "yes",
+      },
+    ])";
+
+constexpr char kFakeProviderLocalContentAnalysisPolicy[] = R"(
+    [
+      {
+        "service_provider": "google",
+        "enable": "yes",
+      },
+    ])";
+
 }  // namespace
 
-class EnterpriseConnectorsPolicyHandlerTest
-    : public testing::TestWithParam<
+class EnterpriseConnectorsPolicyHandlerTestBase {
+ public:
+  virtual const char* policy() const = 0;
+
+  absl::optional<base::Value> policy_value() const {
+    return base::JSONReader::Read(policy(), base::JSON_ALLOW_TRAILING_COMMAS);
+  }
+};
+
+class EnterpriseConnectorsPolicyHandlerCloudOnlyTest
+    : public EnterpriseConnectorsPolicyHandlerTestBase,
+      public testing::TestWithParam<
           std::tuple<const char*, const char*, policy::PolicySource>> {
  public:
   const char* policy_scope() const { return std::get<0>(GetParam()); }
 
-  const char* policy() const { return std::get<1>(GetParam()); }
+  const char* policy() const override { return std::get<1>(GetParam()); }
 
   policy::PolicySource source() const { return std::get<2>(GetParam()); }
 
@@ -91,13 +128,9 @@ class EnterpriseConnectorsPolicyHandlerTest
     return source() == policy::PolicySource::POLICY_SOURCE_CLOUD ||
            source() == policy::PolicySource::POLICY_SOURCE_CLOUD_FROM_ASH;
   }
-
-  absl::optional<base::Value> policy_value() const {
-    return base::JSONReader::Read(policy(), base::JSON_ALLOW_TRAILING_COMMAS);
-  }
 };
 
-TEST_P(EnterpriseConnectorsPolicyHandlerTest, Test) {
+TEST_P(EnterpriseConnectorsPolicyHandlerCloudOnlyTest, Test) {
   std::string error;
   policy::Schema validation_schema = policy::Schema::Parse(kSchema, &error);
   ASSERT_TRUE(error.empty());
@@ -143,8 +176,8 @@ TEST_P(EnterpriseConnectorsPolicyHandlerTest, Test) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    EnterpriseConnectorsPolicyHandlerTest,
-    EnterpriseConnectorsPolicyHandlerTest,
+    EnterpriseConnectorsPolicyHandlerCloudOnlyTest,
+    EnterpriseConnectorsPolicyHandlerCloudOnlyTest,
     testing::Combine(
         testing::Values(kTestScopePref, nullptr),
         testing::Values(kValidPolicy, kInvalidPolicy, kEmptyPolicy),
@@ -152,5 +185,76 @@ INSTANTIATE_TEST_SUITE_P(
                         policy::PolicySource::POLICY_SOURCE_CLOUD_FROM_ASH,
                         policy::PolicySource::POLICY_SOURCE_ACTIVE_DIRECTORY,
                         policy::PolicySource::POLICY_SOURCE_PLATFORM)));
+
+class EnterpriseConnectorsPolicyHandlerLocalTest
+    : public EnterpriseConnectorsPolicyHandlerTestBase,
+      public testing::TestWithParam<
+          std::tuple<const char*, const char*, bool>> {
+ public:
+  EnterpriseConnectorsPolicyHandlerLocalTest() {
+    if (enable_feature())
+      scoped_feature_list_.InitAndEnableFeature(kLocalContentAnalysisEnabled);
+  }
+
+  const char* policy() const override { return std::get<0>(GetParam()); }
+  const char* policy_pref() const { return std::get<1>(GetParam()); }
+  bool enable_feature() const { return std::get<2>(GetParam()); }
+
+  bool policy_is_valid() const {
+    if (policy() == kEmptyPolicy)
+      return true;
+
+    if (!enable_feature())
+      return false;
+
+    if (policy_pref() != kOnFileAttachedPref ||
+        policy_pref() != kOnFileDownloadedPref ||
+        policy_pref() != kOnBulkDataEntryPref ||
+        policy_pref() != kOnPrintPref) {
+      return false;
+    }
+
+    return policy() == kValidLocalContentAnalysisPolicy;
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+TEST_P(EnterpriseConnectorsPolicyHandlerLocalTest, Test) {
+  std::string error;
+  policy::Schema validation_schema = policy::Schema::Parse(kSchema, &error);
+  ASSERT_TRUE(error.empty());
+
+  policy::PolicyMap policy_map;
+  if (policy() != kEmptyPolicy) {
+    policy_map.Set(kPolicyName, policy::PolicyLevel::POLICY_LEVEL_MANDATORY,
+                   policy::PolicyScope::POLICY_SCOPE_MACHINE,
+                   policy::PolicySource::POLICY_SOURCE_PLATFORM, policy_value(),
+                   nullptr);
+  }
+
+  auto handler = std::make_unique<EnterpriseConnectorsPolicyHandler>(
+      kPolicyName, policy_pref(), kTestScopePref, validation_schema);
+  policy::PolicyErrorMap errors;
+  ASSERT_EQ(policy_is_valid(),
+            handler->CheckPolicySettings(policy_map, &errors));
+  ASSERT_EQ(policy_is_valid(), errors.empty());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    EnterpriseConnectorsPolicyHandlerLocalTest,
+    EnterpriseConnectorsPolicyHandlerLocalTest,
+    testing::Combine(testing::Values(kValidLocalContentAnalysisPolicy,
+                                     kInvalidProviderLocalContentAnalysisPolicy,
+                                     kFakeProviderLocalContentAnalysisPolicy,
+                                     kEmptyPolicy),
+                     testing::Values(kOnFileAttachedPref,
+                                     kOnFileDownloadedPref,
+                                     kOnBulkDataEntryPref,
+                                     kOnPrintPref,
+                                     kOnSecurityEventPref,
+                                     kSendDownloadToCloudPref),
+                     testing::Bool()));
 
 }  // namespace enterprise_connectors
