dd727cce6c6e0fb9e6d3e13c211dc736fe4f3e19
Anna Tsvirchkova <atsvirchkova@google.com>
2022-06-17 10:24:54 +0000

    [PwdMessageDialog] Remove deprecated and unused methods from PasswordCheckupClientHelper
    Removes methods from PasswordCheckupClientHelper, which are no longer used.
    This can be submitted only after this CL
    https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/4798813
    
    Bug: 1315916
    Change-Id: I44379eab85ec5eb80b58ef188afd655008d7f835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686474
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015300}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordCheckupClientHelper.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordCheckupClientHelper.java
@@ -35,53 +35,6 @@ public interface PasswordCheckupClientHelper {
      *         account will be used.
      * @param successCallback callback called with the intent if the retrieving was successful
      * @param failureCallback callback called if the retrieving failed with the encountered error.
-     *      The error should be a value from {@link CredentialManagerError}.
-     */
-    @Deprecated
-    default void getPasswordCheckupPendingIntent(@PasswordCheckReferrer int referrer,
-            Optional<String> accountName, Callback<PendingIntent> successCallback,
-            Callback<Integer> failureCallback) {}
-
-    /**
-     * Asynchronously runs Password Checkup and stores the result in PasswordSpecifics then saves it
-     * to the ChromeSync module.
-     *
-     * @param referrer the place that requested to start a check.
-     * @param accountName the account name that is syncing passwords. If no value was provided local
-     *         account will be used.
-     * @param successCallback callback called with Password Check started successful
-     * @param failureCallback callback called if encountered an error.
-     *      The error should be a value from {@link CredentialManagerError}.
-     */
-    @Deprecated
-    default void runPasswordCheckup(@PasswordCheckReferrer int referrer,
-            Optional<String> accountName, Callback<Void> successCallback,
-            Callback<Integer> failureCallback) {}
-
-    /**
-     * Asynchronously returns the number of breached credentials for the provided account.
-     *
-     * @param referrer the place that requested number of breached credentials.
-     * @param accountName the account name that is syncing passwords. If no value was provided local
-     *         account will be used.
-     * @param successCallback callback called with the number of breached passwords.
-     * @param failureCallback callback called if encountered an error.
-     *      The error should be a value from {@link CredentialManagerError}.
-     */
-    @Deprecated
-    default void getNumberOfBreachedCredentials(@PasswordCheckReferrer int referrer,
-            Optional<String> accountName, Callback<Integer> successCallback,
-            Callback<Integer> failureCallback) {}
-
-    /**
-     * Retrieves a pending intent that can be used to launch the Password Checkup UI in the
-     * credential manager. The intent is to either be used immediately or discarded.
-     *
-     * @param referrer the place that will launch the password checkup UI
-     * @param accountName the account name that is syncing passwords. If no value was provided local
-     *         account will be used.
-     * @param successCallback callback called with the intent if the retrieving was successful
-     * @param failureCallback callback called if the retrieving failed with the encountered error.
      */
     default void getPasswordCheckupIntent(@PasswordCheckReferrer int referrer,
             Optional<String> accountName, Callback<PendingIntent> successCallback,
