df0e7186834bf8972c0461f667299968859ef0bd
John Abd-El-Malek <jam@chromium.org>
2022-06-17 22:18:08 +0000

    Parametize OptimizeNetworkBuffers so we can experiment with different number of bytes to read at a time.
    This will allow us to see the effect of the increased mojo data buffer size on WebView's InputStream separately from the increase in bytes read. It'll also allow us to experiment on all platforms with different number of bytes to read, since now it went from 64KB to effectively 2MB. Also rename the feature to make it easy to analyze the new data.
    
    (cherry picked from commit 6e10f5629a5e006910ca4e722a83bfa5f3bc4e92)
    
    Bug: 1318925
    Change-Id: If1c6e6e6e23090be348f9b94f11db4f19f0960a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708493
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1014969}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711615
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5112@{#139}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- services/network/public/cpp/features.h
+++ services/network/public/cpp/features.h
@@ -113,6 +113,9 @@ COMPONENT_EXPORT(NETWORK_CPP)
 extern const base::Feature kOptimizeNetworkBuffers;
 
 COMPONENT_EXPORT(NETWORK_CPP)
+extern const base::FeatureParam<int> kOptimizeNetworkBuffersBytesReadLimit;
+
+COMPONENT_EXPORT(NETWORK_CPP)
 extern const base::Feature kCacheTransparency;
 
 COMPONENT_EXPORT(NETWORK_CPP)
