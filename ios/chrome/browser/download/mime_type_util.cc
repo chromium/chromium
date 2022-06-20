4f6b814bc08139157250b5eda18ca9405c66968a
Ewann <ewannpv@google.com>
2022-06-20 09:01:45 +0000

    [iOS][Downloads][1/N] Extends MIME type support
    This CL adds new MIME types to 'mime_type_util.h'.
    These new MIME types will be added in the 'DownloadMimeTypeResult' enum
    in the next CL.
    
    Bug: 1328718
    Change-Id: I89547fc02490728bfcd50385924db0bf18b27e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3663022
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015837}

--- ios/chrome/browser/download/mime_type_util.cc
+++ ios/chrome/browser/download/mime_type_util.cc
@@ -33,6 +33,79 @@ char kAmazonKindleBookMimeType[] = "application/vnd.amazon.ebook";
 char kBinaryDataMimeType[] = "application/octet-stream";
 char kBitTorrentMimeType[] = "application/x-bittorrent";
 char kJavaArchiveMimeType[] = "application/java-archive";
+char kAACAudioMimetype[] = "audio/aac";
+char kAbiWordDocumentMimetype[] = "application/x-abiword";
+char kArchiveDocumentMimetype[] = "application/x-freearc";
+char kAVIFImageMimetype[] = "image/avif";
+char kAVIVideoMimetype[] = "video/x-msvideo";
+char kGenericBitmapMimetype[] = "image/bmp";
+char kMicrosoftBitmapMimetype[] = "image/x-ms-bmp";
+char kBZipArchiveMimetype[] = "application/x-bzip";
+char kBZip2ArchiveMimetype[] = "application/x-bzip2";
+char kCDAudioMimetype[] = "application/x-cdf";
+char kCShellScriptMimetype[] = "application/x-csh";
+char kCascadingStyleSheetMimetype[] = "text/css";
+char kCommaSeparatedValuesMimetype[] = "text/csv";
+char kMicrosoftWordMimetype[] = "application/msword";
+char kMicrosoftWordXMLMimetype[] =
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+char kMSEmbeddedOpenTypefontMimetype[] = "application/vnd.ms-fontobject";
+char kElectronicPublicationMimetype[] = "application/epub+zip";
+char kGZipCompressedArchiveMimetype[] = "application/gzip";
+char kGraphicsInterchangeFormatMimetype[] = "image/gif";
+char kHyperTextMarkupLanguageMimetype[] = "text/html";
+char kIconFormatMimetype[] = "image/vnd.microsoft.icon";
+char kJPEGImageMimetype[] = "image/jpeg";
+char kJavaScriptMimetype[] = "text/javascript";
+char kJSONFormatMimetype[] = "application/json";
+char kJSONLDFormatMimetype[] = "application/ld+json";
+char kMusicalInstrumentDigitalInterfaceMimetype[] = "audio/midi";
+char kXMusicalInstrumentDigitalInterfaceMimetype[] = "audio/x-midi";
+char kMP3AudioMimetype[] = "audio/mpeg";
+char kMP4VideoMimetype[] = "video/mp4";
+char kMPEGVideoMimetype[] = "video/mpeg";
+char kOpenDocumentPresentationDocumentMimetype[] =
+    "application/vnd.oasis.opendocument.presentation";
+char kOpenDocumentSpreadsheetDocumentMimetype[] =
+    "application/vnd.oasis.opendocument.spreadsheet";
+char kOpenDocumentTextDocumentMimetype[] =
+    "application/vnd.oasis.opendocument.text";
+char kOGGAudioMimetype[] = "audio/ogg";
+char kOGGVideoMimetype[] = "video/ogg";
+char kOGGMimetype[] = "application/ogg";
+char kOpusAudioMimetype[] = "audio/opus";
+char kOpenTypeFontMimetype[] = "font/otf";
+char kPortableNetworkGraphicMimetype[] = "image/png";
+char kAdobePortableDocumentFormatMimetype[] = "application/pdf";
+char kHypertextPreprocessorMimetype[] = "application/x-httpd-php";
+char kMicrosoftPowerPointMimetype[] = "application/vnd.ms-powerpoint";
+char kMicrosoftPowerPointOpenXMLMimetype[] =
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+char kRARArchiveMimetype[] = "application/vnd.rar";
+char kRichTextFormatMimetype[] = "application/rtf";
+char kBourneShellScriptMimetype[] = "application/x-sh";
+char kScalableVectorGraphicMimetype[] = "image/svg+xml";
+char kTaggedImageFileFormatMimetype[] = "image/tiff";
+char kMPEGTransportStreamMimetype[] = "video/mp2t";
+char kTrueTypeFontMimetype[] = "font/ttf";
+char kTextMimetype[] = "text/plain";
+char kMicrosoftVisioMimetype[] = "application/vnd.visio";
+char kWaveformAudioFormatMimetype[] = "audio/wav";
+char kWEBMAudioMimetype[] = "audio/webm";
+char kWEBMVideoMimetype[] = "video/webm";
+char kWEBPImageMimetype[] = "image/webp";
+char kWebOpenFontMimetype[] = "font/woff";
+char kWebOpenFont2Mimetype[] = "font/woff2";
+char kXHTMLMimetype[] = "application/xhtml+xml";
+char kMicrosoftExcelMimetype[] = "application/vnd.ms-excel";
+char kMicrosoftExcelOpenXMLMimetype[] =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+char kXMLMimetype[] = "application/xml";
+char kXULMimetype[] = "application/vnd.mozilla.xul+xml";
+char k3GPPVideoMimetype[] = "video/3gpp";
+char k3GPPAudioMimetype[] = "audio/3gpp";
+char k3GPP2VideoMimetype[] = "video/3gpp2";
+char k3GPP2AudioMimetype[] = "audio/3gpp2";
 
 bool IsUsdzFileFormat(const std::string& mime_type,
                       const base::FilePath& suggested_path) {
--- ios/chrome/browser/download/mime_type_util.h
+++ ios/chrome/browser/download/mime_type_util.h
@@ -50,7 +50,7 @@ extern char kRARArchiveMimeType[];
 // MIME Type for TAR archive.
 extern char kTarArchiveMimeType[];
 
-// MIME Type for Adobe Flash .
+// MIME Type for Adobe Flash.
 extern char kAdobeFlashMimeType[];
 
 // MIME Type for Amazon kindle book.
@@ -68,6 +68,74 @@ extern char kJavaArchiveMimeType[];
 // Legacy USDZ content types.
 extern char kLegacyUsdzMimeType[];
 extern char kLegacyPixarUsdzMimeType[];
+
+// MIME Types that don't have specific treatment.
+extern char kAACAudioMimetype[];
+extern char kAbiWordDocumentMimetype[];
+extern char kArchiveDocumentMimetype[];
+extern char kAVIFImageMimetype[];
+extern char kAVIVideoMimetype[];
+extern char kGenericBitmapMimetype[];
+extern char kMicrosoftBitmapMimetype[];
+extern char kBZip2ArchiveMimetype[];
+extern char kCDAudioMimetype[];
+extern char kCShellScriptMimetype[];
+extern char kCascadingStyleSheetMimetype[];
+extern char kCommaSeparatedValuesMimetype[];
+extern char kMicrosoftWordMimetype[];
+extern char kMicrosoftWordXMLMimetype[];
+extern char kMSEmbeddedOpenTypefontMimetype[];
+extern char kElectronicPublicationMimetype[];
+extern char kGZipCompressedArchiveMimetype[];
+extern char kGraphicsInterchangeFormatMimetype[];
+extern char kHyperTextMarkupLanguageMimetype[];
+extern char kIconFormatMimetype[];
+extern char kJPEGImageMimetype[];
+extern char kJavaScriptMimetype[];
+extern char kJSONFormatMimetype[];
+extern char kJSONLDFormatMimetype[];
+extern char kMusicalInstrumentDigitalInterfaceMimetype[];
+extern char kXMusicalInstrumentDigitalInterfaceMimetype[];
+extern char kMP3AudioMimetype[];
+extern char kMP4VideoMimetype[];
+extern char kMPEGVideoMimetype[];
+extern char kOpenDocumentPresentationDocumentMimetype[];
+extern char kOpenDocumentSpreadsheetDocumentMimetype[];
+extern char kOpenDocumentTextDocumentMimetype[];
+extern char kOGGAudioMimetype[];
+extern char kOGGVideoMimetype[];
+extern char kOGGMimetype[];
+extern char kOpusAudioMimetype[];
+extern char kOpenTypeFontMimetype[];
+extern char kPortableNetworkGraphicMimetype[];
+extern char kAdobePortableDocumentFormatMimetype[];
+extern char kHypertextPreprocessorMimetype[];
+extern char kMicrosoftPowerPointMimetype[];
+extern char kMicrosoftPowerPointOpenXMLMimetype[];
+extern char kRARArchiveMimetype[];
+extern char kRichTextFormatMimetype[];
+extern char kBourneShellScriptMimetype[];
+extern char kScalableVectorGraphicMimetype[];
+extern char kTaggedImageFileFormatMimetype[];
+extern char kMPEGTransportStreamMimetype[];
+extern char kTrueTypeFontMimetype[];
+extern char kTextMimetype[];
+extern char kMicrosoftVisioMimetype[];
+extern char kWaveformAudioFormatMimetype[];
+extern char kWEBMAudioMimetype[];
+extern char kWEBMVideoMimetype[];
+extern char kWEBPImageMimetype[];
+extern char kWebOpenFontMimetype[];
+extern char kWebOpenFont2Mimetype[];
+extern char kXHTMLMimetype[];
+extern char kMicrosoftExcelMimetype[];
+extern char kMicrosoftExcelOpenXMLMimetype[];
+extern char kXMLMimetype[];
+extern char kXULMimetype[];
+extern char k3GPPVideoMimetype[];
+extern char k3GPPAudioMimetype[];
+extern char k3GPP2VideoMimetype[];
+extern char k3GPP2AudioMimetype[];
 
 // Returns whether the content-type or the file extension match those of a USDZ
 // 3D model. The file extension is checked in addition to the content-type since
