d596943d3da8a954f4fa7639905aa1397203bc1b
Egor Pasko <pasko@chromium.org>
2022-06-20 12:06:25 +0000

    SafeBrowsingApiBridge.java: remove deprecated method and use sLock
    Usage of SafeBrowsingApiHandlerBridge.setSafeBrowsingHandlerType() was
    replaced by .setHandler() recently.
    
    Remove the old method and the associated (more complicated) path.
    
    Also switch to lock-based synchronization instead of having a mix of
    volatile, LazyHolder and thread restrictions. Protecting everything by a
    single lock allows for simpler reasoning and more compile-time checks
    via the @GuardedBy annotations.
    
    The lock is only longlived during the handler initialization. During
    that time having more than one URL check requests is unlikely. We are
    theoretically increasing latency if compared to the LazyHolder path. The
    potential slowdown is added at the beginning of each SB check. Since
    checking a URL by SB commonly involves acquiring several locks, running
    tasks, queuing, the potential extra overhead would appear ridiculously
    small in comparison, if any.
    
    Bug: 1296097
    Change-Id: I56df1da131e28b00774225180fadadebd07062ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708771
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1015868}

--- components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafeBrowsingApiBridge.java
+++ components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafeBrowsingApiBridge.java
@@ -4,13 +4,13 @@
 
 package org.chromium.components.safe_browsing;
 
+import androidx.annotation.GuardedBy;
+
 import org.chromium.base.Log;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
-
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Helper for calling GMSCore Safe Browsing API from native code.
@@ -28,28 +28,19 @@ public final class SafeBrowsingApiBridge {
     private static final String TAG = "SBApiBridge";
     private static final boolean DEBUG = false;
 
-    // Volatile to allow it being set from any thread.
-    private static volatile SafeBrowsingApiHandler sHandler;
-    private static UrlCheckTimeObserver sUrlCheckTimeObserver;
+    private static final Object sLock = new Object();
 
-    // Deprecated. See setHandler().
-    private static Class<? extends SafeBrowsingApiHandler> sHandlerClass;
+    @GuardedBy("sLock")
+    private static boolean sHandlerInitCalled;
+
+    @GuardedBy("sLock")
+    private static SafeBrowsingApiHandler sHandler;
+
+    @GuardedBy("sLock")
+    private static UrlCheckTimeObserver sUrlCheckTimeObserver;
 
     private SafeBrowsingApiBridge() {
         // Util class, do not instantiate.
-    }
-
-    /**
-     * Set the class-file for the implementation of SafeBrowsingApiHandler to use when the safe
-     * browsing api is invoked.
-     *
-     * TODO(crbug.com/1296097): Remove this method when all clients are switched to
-     * {@link #setHandler(SafeBrowsingApiHandler)}.
-     */
-    @Deprecated
-    public static void setSafeBrowsingHandlerType(
-            Class<? extends SafeBrowsingApiHandler> handlerClass) {
-        sHandlerClass = handlerClass;
     }
 
     /**
@@ -58,8 +49,10 @@ public final class SafeBrowsingApiBridge {
      * @param handler An instance that has not been initialized.
      */
     public static void setHandler(SafeBrowsingApiHandler handler) {
-        assert sHandler == null;
-        sHandler = handler;
+        synchronized (sLock) {
+            assert sHandler == null;
+            sHandler = handler;
+        }
     }
 
     /**
@@ -72,11 +65,9 @@ public final class SafeBrowsingApiBridge {
      */
     @CalledByNative
     public static boolean ensureInitialized() {
-        return getHandler() != null;
-    }
-
-    private static SafeBrowsingApiHandler getHandler() {
-        return LazyHolder.INSTANCE;
+        synchronized (sLock) {
+            return getHandler() != null;
+        }
     }
 
     /**
@@ -94,17 +85,21 @@ public final class SafeBrowsingApiBridge {
      * Set the observer to notify about the time it took to respond for SafeBrowsing. Notified for
      * the first URL check, and only once.
      *
-     * The notification happens on another thread. The caller *must* guarantee that setting the
-     * observer happens-before (in JMM sense) the first SafeBrowsing request is made.
-     *
      * @param observer the observer to notify.
      */
     public static void setOneTimeUrlCheckObserver(UrlCheckTimeObserver observer) {
-        sUrlCheckTimeObserver = observer;
+        synchronized (sLock) {
+            sUrlCheckTimeObserver = observer;
+        }
     }
 
-    private static class LazyHolder {
-        static final SafeBrowsingApiHandler INSTANCE = initHandler();
+    @GuardedBy("sLock")
+    private static SafeBrowsingApiHandler getHandler() {
+        if (!sHandlerInitCalled) {
+            sHandler = initHandler();
+            sHandlerInitCalled = true;
+        }
+        return sHandler;
     }
 
     /**
@@ -114,68 +109,72 @@ public final class SafeBrowsingApiBridge {
      *
      * @return the handler if it is usable, or null if the API is not supported.
      */
+    @GuardedBy("sLock")
     private static SafeBrowsingApiHandler initHandler() {
         try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.initHandler")) {
-            return initHandlerInTraceEvent();
+            if (DEBUG) {
+                Log.i(TAG, "initHandler");
+            }
+            if (sHandler == null) return null;
+            return sHandler.init(new LookupDoneObserver()) ? sHandler : null;
         }
     }
 
-    private static SafeBrowsingApiHandler initHandlerInTraceEvent() {
-        if (DEBUG) {
-            Log.i(TAG, "initHandler");
-        }
-        SafeBrowsingApiHandler handler;
-        if (sHandler != null) {
-            handler = sHandler;
-        } else {
-            try {
-                handler = sHandlerClass.getDeclaredConstructor().newInstance();
-            } catch (NullPointerException | InstantiationException | IllegalAccessException
-                    | NoSuchMethodException | InvocationTargetException e) {
-                Log.e(TAG, "Failed to init handler: " + e.getMessage());
-                return null;
+    private static class LookupDoneObserver implements SafeBrowsingApiHandler.Observer {
+        @Override
+        public void onUrlCheckDone(
+                long callbackId, int resultStatus, String metadata, long checkDelta) {
+            synchronized (sLock) {
+                if (sUrlCheckTimeObserver != null) {
+                    sUrlCheckTimeObserver.onUrlCheckTime(checkDelta);
+                    TraceEvent.instant("FirstSafeBrowsingResponse", String.valueOf(checkDelta));
+                    sUrlCheckTimeObserver = null;
+                }
+                SafeBrowsingApiBridgeJni.get().onUrlCheckDone(
+                        callbackId, resultStatus, metadata, checkDelta);
             }
         }
-        boolean initSuccessful = handler.init((callbackId, resultStatus, metadata, checkDelta) -> {
-            if (sUrlCheckTimeObserver != null) {
-                sUrlCheckTimeObserver.onUrlCheckTime(checkDelta);
-                TraceEvent.instant("FirstSafeBrowsingResponse", String.valueOf(checkDelta));
-                sUrlCheckTimeObserver = null;
-            }
-            SafeBrowsingApiBridgeJni.get().onUrlCheckDone(
-                    callbackId, resultStatus, metadata, checkDelta);
-        });
-        return initSuccessful ? handler : null;
     }
 
     /**
      * Starts a Safe Browsing check.
+     *
+     * Must only be called if {@link #ensureInitialized()} returns true.
      */
     @CalledByNative
     private static void startUriLookup(long callbackId, String uri, int[] threatsOfInterest) {
-        try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.startUriLookup")) {
-            assert getHandler() != null;
-            if (DEBUG) {
-                Log.i(TAG, "Starting request: %s", uri);
-            }
-            getHandler().startUriLookup(callbackId, uri, threatsOfInterest);
-            if (DEBUG) {
-                Log.i(TAG, "Done starting request: %s", uri);
+        synchronized (sLock) {
+            assert sHandlerInitCalled;
+            assert sHandler != null;
+            try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.startUriLookup")) {
+                if (DEBUG) {
+                    Log.i(TAG, "Starting request: %s", uri);
+                }
+                getHandler().startUriLookup(callbackId, uri, threatsOfInterest);
+                if (DEBUG) {
+                    Log.i(TAG, "Done starting request: %s", uri);
+                }
             }
         }
     }
 
     /**
-     * TODO(crbug.com/995926): Make this call async
      * Starts a Safe Browsing Allowlist check.
      *
+     * Must only be called if {@link #ensureInitialized()} returns true.
+     *
      * @return true iff the uri is in the allowlist.
+     *
+     * TODO(crbug.com/995926): Make this call async.
      */
     @CalledByNative
     private static boolean startAllowlistLookup(String uri, int threatType) {
-        try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.startAllowlistLookup")) {
-            assert getHandler() != null;
-            return getHandler().startAllowlistLookup(uri, threatType);
+        synchronized (sLock) {
+            assert sHandlerInitCalled;
+            assert sHandler != null;
+            try (TraceEvent t = TraceEvent.scoped("SafeBrowsingApiBridge.startAllowlistLookup")) {
+                return getHandler().startAllowlistLookup(uri, threatType);
+            }
         }
     }
 
