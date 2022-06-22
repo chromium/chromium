b0a8822df23ee4d99aab34af4c92d56470b94d43
Hong Xu <xuhong@chromium.org>
2022-06-22 23:50:38 +0000

    Add analytics for some ERP upload parameters
    Add analytics for ERP upload size adjustment. We would like to see
    some of the parameters in production before we enable the feature from
    the server side.
    
    Bug: b:214039157
    Change-Id: I9cd173d02fc9ff282ee7f8556617d8eaa98cd694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3711050
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1016928}

--- chrome/browser/policy/messaging_layer/upload/event_upload_size_controller.cc
+++ chrome/browser/policy/messaging_layer/upload/event_upload_size_controller.cc
@@ -8,6 +8,7 @@
 #include <limits>
 #include <vector>
 
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/policy/messaging_layer/upload/network_condition_service.h"
 #include "components/reporting/proto/synced/record.pb.h"
 
@@ -19,7 +20,18 @@ EventUploadSizeController::EventUploadSizeController(
     uint64_t remaining_storage_capacity)
     : new_events_rate_(new_events_rate > 0 ? new_events_rate : 1),
       remaining_storage_capacity_(remaining_storage_capacity),
-      max_upload_size_(ComputeMaxUploadSize(network_condition_service)) {}
+      max_upload_size_(ComputeMaxUploadSize(network_condition_service)) {
+  base::UmaHistogramCounts10000(
+      "Browser.ERP.EventUploadSizeAdjustment.NewEventsRate", new_events_rate_);
+  base::UmaHistogramCustomCounts(
+      "Browser.ERP.EventUploadSizeAdjustment.RemainingStorageCapacity",
+      /*sample=*/remaining_storage_capacity_,
+      /*min=*/1,
+      /*exclusive_max=*/80'000'000,
+      /*buckets=*/50);
+  base::UmaHistogramCounts10M(
+      "Browser.ERP.EventUploadSizeAdjustment.MaxUploadSize", max_upload_size_);
+}
 
 bool EventUploadSizeController::IsMaximumUploadSizeReached() const {
   return Enabler::Get() && uploaded_size_ >= max_upload_size_;
