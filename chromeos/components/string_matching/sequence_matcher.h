ace9106398d7e771e390eca69f3672a7e11a7166
Amanda Deacon <amandadeacon@chromium.org>
2022-06-22 03:06:44 +0000

    [String matching] Expand SequenceMatcher comments and add TODOs.
    Aid readability of the DP edit distance algorithm, and the
    recursive block matching algorithm. Expand comments in general.
    
    Test: unit tests
    Bug: 1336160
    Change-Id: I936dd47416871e76cf1062d13ad958d1f9edce13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3713463
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016521}

--- chromeos/components/string_matching/sequence_matcher.h
+++ chromeos/components/string_matching/sequence_matcher.h
@@ -17,24 +17,24 @@ namespace string_matching {
 // (https://docs.python.org/2/library/difflib.html#difflib.SequenceMatcher)
 class SequenceMatcher {
  public:
-  // Representing a common substring between |first_string_| and
-  // |second_string_|.
+  // Representing a common substring between `first_string_` and
+  // `second_string_`.
   struct Match {
     Match();
     Match(int pos_first, int pos_second, int len);
-    // Starting position of the common substring in |first_string_|.
+    // Starting position of the common substring in `first_string_`.
     int pos_first_string;
-    // Starting position of the common substring in |second_string_|.
+    // Starting position of the common substring in `second_string_`.
     int pos_second_string;
     // Length of the common substring.
     int length;
   };
 
-  // |num_matching_blocks_penalty| is used to penalize too many small matching
+  // `num_matching_blocks_penalty` is used to penalize too many small matching
   // blocks. For the same number of matching characters, we prefer fewer
   // matching blocks. Value equal to 0 means no penalty. Values greater than 0
   // means heavier penalty will be applied to larger number of blocks. This is
-  // only appled if |use_edit_distance| is false.
+  // only appled if `use_edit_distance` is false.
   SequenceMatcher(const std::u16string& first_string,
                   const std::u16string& second_string,
                   bool use_edit_distance,
@@ -45,24 +45,37 @@ class SequenceMatcher {
 
   ~SequenceMatcher() = default;
 
-  // Calculates similarity ratio of |first_string_| and |second_string_|.
+  // Calculates similarity ratio of `first_string_` and `second_string_`.
   double Ratio();
-  // Calculates the Damerau–Levenshtein distance between |first_string_| and
-  // |second_string_|.
-  // See https://en.wikipedia.org/wiki/Damerau–Levenshtein_distance for more
+  // Calculates the Damerau–Levenshtein restricted edit distance between
+  // `first_string_` and `second_string_`. Also known as the "optimal string
+  // alignment distance".
+  //
+  // The algorithm considers the following edit operations: insertion, deletion,
+  // substitution, and two-character transposition. It does not consider
+  // multiple adjacent transpositions. See
+  // https://en.wikipedia.org/wiki/Damerau–Levenshtein_distance for more
   // details.
   int EditDistance();
   // Finds the longest common substring between
-  // |first_string_[first_start:first_end]| and
-  // |second_string_[second_start:second_end]|.
+  // `first_string_[first_start:first_end]` and
+  // `second_string_[second_start:second_end]`. Used by
+  // GetMatchingBlocks().
   Match FindLongestMatch(int first_start,
                          int first_end,
                          int second_start,
                          int second_end);
-  // Get all matching blocks of |first_string_| and |second_string_|.
-  // All blocks will be sorted by starting position of them in the
-  // |first_string_|. The last matching block will always be
-  // Match(first_string_.size(), second_string_.size(), 0).
+  // Get all matching blocks of `first_string_` and `second_string_`.
+  // All blocks will be sorted by their starting position within
+  // `first_string_`.
+  //
+  // The last matching block will always be:
+  //
+  //   Match(first_string_.size(), second_string_.size(), 0).
+  //
+  // This is to cover the case where two strings have no matching blocks,
+  // so that we have something to store in `matching_blocks_` to indicate
+  // that matching has taken place.
   std::vector<Match> GetMatchingBlocks();
 
  private:
@@ -76,9 +89,9 @@ class SequenceMatcher {
   // Controls whether to use edit distance to calculate ratio.
   bool use_edit_distance_;
   int edit_distance_ = -1;
-  // For each character |c| in |second_string_|, this variable
-  // |char_to_positions_| stores all positions where |c| occurs in
-  // |second_string_|.
+  // For each character `c` in `second_string_`, this variable
+  // `char_to_positions_` stores all positions where `c` occurs in
+  // `second_string_`.
   std::unordered_map<char, std::vector<int>> char_to_positions_;
   // Memory for dynamic programming algorithm used in FindLongestMatch().
   std::vector<int> dp_common_string_;
