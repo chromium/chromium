c300e510a75fabbe54cc6febe58f3475a0920b29
Mustafa Emre Acer <meacer@chromium.org>
2022-06-22 23:45:42 +0000

    HTTPS-Only Mode: Disable tab helpers when the UI is disabled
    HTTPS-Only Mode is enabled through a pref. The pref is set through the
    UI which is only displayed when kHttpsOnlyMode feature is enabled.
    This means that we don't need the tab helpers when kHttpsOnlyMode is
    disabled, because there is no way to enable the feature from the UI.
    
    This CL disables the tab helpers if the feature is disabled. This is
    mostly a no-op, but it improves stability in case there is a bug in
    tab helpers. It also acts as a kill switch if the feature launch goes
    bad.
    
    Bug: 1337082
    Change-Id: I8916ca98e373fbff1b6a78ba3a4e3dddc27f66df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3708204
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016923}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -87,6 +87,7 @@
 #import "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
 #import "ios/chrome/browser/web/web_performance_metrics/web_performance_metrics_tab_helper.h"
 #import "ios/chrome/browser/webui/net_export_tab_helper.h"
+#include "ios/components/security_interstitials/https_only_mode/feature.h"
 #import "ios/components/security_interstitials/https_only_mode/https_only_mode_container.h"
 #import "ios/components/security_interstitials/ios_blocking_page_tab_helper.h"
 #import "ios/components/security_interstitials/lookalikes/lookalike_url_container.h"
@@ -237,9 +238,12 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
   RepostFormTabHelper::CreateForWebState(web_state);
   NetExportTabHelper::CreateForWebState(web_state);
 
-  HttpsOnlyModeUpgradeTabHelper::CreateForWebState(web_state,
-                                                   browser_state->GetPrefs());
-  HttpsOnlyModeContainer::CreateForWebState(web_state);
+  if (base::FeatureList::IsEnabled(
+          security_interstitials::features::kHttpsOnlyMode)) {
+    HttpsOnlyModeUpgradeTabHelper::CreateForWebState(web_state,
+                                                     browser_state->GetPrefs());
+    HttpsOnlyModeContainer::CreateForWebState(web_state);
+  }
 
   if (IsWebChannelsEnabled()) {
     FollowTabHelper::CreateForWebState(web_state);
