3b633d885d76c5129cffd43c67844822ea48e2ae
Tushar Agarwal <agarwaltushar@google.com>
2022-06-22 16:48:28 +0000

    Revert "Enable profiling for stack sampled metrics for all supported threads in the gpu process on Android."
    This reverts commit 24f589bf4eb02b2df6b65872a23e629231db5f91.
    
    Reason for revert: Potential cause of crashes: https://crbug.com/1337074#c5.
    
    Original change's description:
    > Enable profiling for stack sampled metrics for all supported threads in
    > the gpu process on Android.
    >
    > This should (hopefully) provide more/better signals for future
    > optimization work.
    >
    > Please refer to the attached bug for testing details.
    >
    > Change-Id: I5f3ba66bdcb04f47af6c314c24fd653dbcd02699
    > Bug: 1326430
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3644664
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1014482}
    
    Bug: 1337074,1326430
    Change-Id: Ib99c4728352baba436cc95fa6e8da34a6ffc5399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3706591
    Auto-Submit: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016752}

--- chrome/common/profiler/thread_profiler_platform_configuration_unittest.cc
+++ chrome/common/profiler/thread_profiler_platform_configuration_unittest.cc
@@ -151,10 +151,10 @@ MAYBE_PLATFORM_CONFIG_TEST_F(ThreadProfilerPlatformConfigurationTest,
 MAYBE_PLATFORM_CONFIG_TEST_F(ThreadProfilerPlatformConfigurationTest,
                              GetChildProcessEnableFraction) {
 #if BUILDFLAG(IS_ANDROID)
+  EXPECT_EQ(0.0, config()->GetChildProcessEnableFraction(
+                     metrics::CallStackProfileParams::Process::kGpu));
   EXPECT_EQ(0.75, config()->GetChildProcessEnableFraction(
                       metrics::CallStackProfileParams::Process::kRenderer));
-  EXPECT_EQ(1.0, config()->GetChildProcessEnableFraction(
-                     metrics::CallStackProfileParams::Process::kGpu));
   EXPECT_EQ(0.0,
             config()->GetChildProcessEnableFraction(
                 metrics::CallStackProfileParams::Process::kNetworkService));
@@ -187,13 +187,13 @@ MAYBE_PLATFORM_CONFIG_TEST_F(ThreadProfilerPlatformConfigurationTest,
       metrics::CallStackProfileParams::Process::kBrowser,
       metrics::CallStackProfileParams::Thread::kIo));
 
-  EXPECT_TRUE(config()->IsEnabledForThread(
+  EXPECT_FALSE(config()->IsEnabledForThread(
       metrics::CallStackProfileParams::Process::kGpu,
       metrics::CallStackProfileParams::Thread::kMain));
-  EXPECT_TRUE(config()->IsEnabledForThread(
+  EXPECT_FALSE(config()->IsEnabledForThread(
       metrics::CallStackProfileParams::Process::kGpu,
       metrics::CallStackProfileParams::Thread::kIo));
-  EXPECT_TRUE(config()->IsEnabledForThread(
+  EXPECT_FALSE(config()->IsEnabledForThread(
       metrics::CallStackProfileParams::Process::kGpu,
       metrics::CallStackProfileParams::Thread::kCompositor));
 
