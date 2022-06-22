d46ae9e709a0ba2ff148a7d9db11676bf1293315
Joey Arhar <jarhar@chromium.org>
2022-06-22 23:21:32 +0000

    Replace Document::forms() CHECK with To<>
    Fixed: 1293602
    Change-Id: I49edb8efa9aa6a185f2f26fc68b2ee2f255c71b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715525
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1016908}

--- third_party/blink/renderer/core/exported/web_document.cc
+++ third_party/blink/renderer/core/exported/web_document.cc
@@ -205,12 +205,7 @@ WebVector<WebFormElement> WebDocument::Forms() const {
   Vector<WebFormElement> form_elements;
   form_elements.ReserveCapacity(forms->length());
   for (Element* element : *forms) {
-    auto* html_form_element = blink::DynamicTo<HTMLFormElement>(element);
-    // TODO(https://crbug.com/1293602): Make this a To<> instead of a CHECK.
-    CHECK(html_form_element)
-        << "Document::forms() returned a non-form element! " << element;
-    if (html_form_element)
-      form_elements.emplace_back(html_form_element);
+    form_elements.emplace_back(blink::To<HTMLFormElement>(element));
   }
   return form_elements;
 }
