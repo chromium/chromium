7d6e636b69f0827bb6afb1a63f147ceeb4b3a4de
Daniel Libby <dlibby@microsoft.com>
2022-06-22 06:20:25 +0000

    Convert role=alert events to live region changed for UIA
    Firing system alerts from web content is bad form - Narrator will prefix
    the announcement with "Alert", which we've gotten feedback is
    undesirable for users.
    
    This change uses live region changed events instead when encountering
    role=alert. Note that we are not changing direct usage of
    ax::mojom::Event::kAlert, which will still translate to
    UIA_SystemAlertEventId.
    
    
    Change-Id: Iedc75053769122e8add4b67e05e0d009ac8630c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3715513
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1016567}

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -186,7 +186,10 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
       break;
     case ui::AXEventGenerator::Event::ALERT:
       FireWinAccessibilityEvent(EVENT_SYSTEM_ALERT, node);
-      FireUiaAccessibilityEvent(UIA_SystemAlertEventId, node);
+      // Generated 'ALERT' events come from role=alert nodes in the tree.
+      // These should just be treated as normal live region changed events,
+      // since we don't want web pages to be performing system-wide alerts.
+      FireUiaAccessibilityEvent(UIA_LiveRegionChangedEventId, node);
       break;
     case ui::AXEventGenerator::Event::ATOMIC_CHANGED:
       HandleAriaPropertiesChangedEvent(*node);
