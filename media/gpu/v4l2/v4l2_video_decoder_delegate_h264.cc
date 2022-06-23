0df6d1638bfa3f4a61c29c8301fc3416568d0d21
Brian Norris <briannorris@chromium.org>
2022-06-23 18:50:47 +0000

    media/gpu/v4l2: Send H264 scaling list in raster order
    The Chromium parser gives the original (zigzag) order, but the V4L2 API
    requests raster order. Do the conversion as we fill out the API structs.
    
    BUG=b:215673865, b:230377834
    TEST=video.ChromeStackDecoderVerification.h264_files_from_bugs on kevin
    
    Change-Id: I96c0af6d17073029ab03b5aeb2bc255e637eb7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3718933
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017279}

--- media/gpu/v4l2/v4l2_video_decoder_delegate_h264.cc
+++ media/gpu/v4l2/v4l2_video_decoder_delegate_h264.cc
@@ -21,6 +21,20 @@
 
 namespace media {
 
+namespace {
+
+constexpr uint8_t zigzag_4x4[] = {
+    0, 1, 4, 8, 5, 2, 3, 6, 9, 12, 13, 10, 7, 11, 14, 15,
+};
+
+constexpr uint8_t zigzag_8x8[] = {
+    0,  1,  8,  16, 9,  2,  3,  10, 17, 24, 32, 25, 18, 11, 4,  5,
+    12, 19, 26, 33, 40, 48, 41, 34, 27, 20, 13, 6,  7,  14, 21, 28,
+    35, 42, 49, 56, 57, 50, 43, 36, 29, 22, 15, 23, 30, 37, 44, 51,
+    58, 59, 52, 45, 38, 31, 39, 46, 53, 60, 61, 54, 47, 55, 62, 63};
+
+}  // namespace
+
 // This struct contains the kernel-specific parts of the H264 acceleration,
 // that we don't want to expose in the .h file since they may differ from
 // upstream.
@@ -251,13 +265,23 @@ V4L2VideoDecoderDelegateH264::SubmitFrameMetadata(
   for (size_t i = 0; i < std::size(v4l2_scaling_matrix.scaling_list_4x4); ++i) {
     for (size_t j = 0; j < std::size(v4l2_scaling_matrix.scaling_list_4x4[i]);
          ++j) {
-      v4l2_scaling_matrix.scaling_list_4x4[i][j] = scaling_list4x4[i][j];
+      // Parser uses source (zigzag) order, while V4L2 API requires raster
+      // order.
+      static_assert(
+          std::extent<decltype(v4l2_scaling_matrix.scaling_list_4x4), 1>() ==
+          std::extent<decltype(zigzag_4x4)>());
+      v4l2_scaling_matrix.scaling_list_4x4[i][zigzag_4x4[j]] =
+          scaling_list4x4[i][j];
     }
   }
   for (size_t i = 0; i < std::size(v4l2_scaling_matrix.scaling_list_8x8); ++i) {
     for (size_t j = 0; j < std::size(v4l2_scaling_matrix.scaling_list_8x8[i]);
          ++j) {
-      v4l2_scaling_matrix.scaling_list_8x8[i][j] = scaling_list8x8[i][j];
+      static_assert(
+          std::extent<decltype(v4l2_scaling_matrix.scaling_list_8x8), 1>() ==
+          std::extent<decltype(zigzag_8x8)>());
+      v4l2_scaling_matrix.scaling_list_8x8[i][zigzag_8x8[j]] =
+          scaling_list8x8[i][j];
     }
   }
 
