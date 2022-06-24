715d00de6d5c40330f7424a3463a0fdbdf3df61a
Ahmed Mehfooz <amehfooz@chromium.org>
2022-06-24 19:34:40 +0000

    Fix crash caused by notification removal during animation
    If a grouped child notification is removed before the SingleToGrouped
    animation is complete, the notification object is unexpectedly deleted
    which was resulting in a crash.
    
    Bug: 1337650
    Change-Id: Ie8c432bb25d0ab1599e0d41aaa1528f645930f16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722763
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1017779}

--- ash/system/message_center/notification_grouping_controller_unittest.cc
+++ ash/system/message_center/notification_grouping_controller_unittest.cc
@@ -8,7 +8,9 @@
 #include "ash/system/message_center/ash_message_popup_collection.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/test/ash_test_base.h"
+#include "ash/test/layer_animation_stopped_waiter.h"
 #include "base/test/scoped_feature_list.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
 #include "ui/message_center/views/message_popup_view.h"
@@ -242,6 +244,27 @@ TEST_F(NotificationGroupingControllerTest,
 
   std::string id_parent = id0 + kIdSuffixForGroupContainerNotification;
   EXPECT_TRUE(message_center->FindNotificationById(id_parent)->group_parent());
+}
+
+TEST_F(NotificationGroupingControllerTest,
+       GroupedNotificationRemovedDuringAnimation) {
+  auto* message_center = MessageCenter::Get();
+  std::string id0, id1;
+  const GURL url(u"http://test-url.com/");
+
+  // Enable animations.
+  ui::ScopedAnimationDurationScaleMode duration(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+
+  id0 = AddNotificationWithOriginUrl(url);
+  id1 = AddNotificationWithOriginUrl(url);
+
+  // Remove notification with `id` before the animation completes.
+  message_center->RemoveNotification(id1, true);
+
+  // Wait for the animation to end to ensure there is no crash
+  LayerAnimationStoppedWaiter waiter;
+  waiter.Wait(GetPopupView(id0)->message_view()->layer());
 }
 
 }  // namespace ash
