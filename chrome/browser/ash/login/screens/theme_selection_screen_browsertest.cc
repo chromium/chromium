dc0a71a030d756d4f9751f20aa3490c27fdb0cca
Jakub Flis <jaflis@google.com>
2022-06-24 11:15:32 +0000

    [OOBE] Update tablet strings in Dark/Light Mode OOBE screen
    Currently all texts on the OOBE theme selection screen are displayed
    regardless of device type (tablet, clamshell). Update strings for
    tablets to provide a better UX.
    
    Bug: 1334662
    Change-Id: Ib264186304d6bb4a7f5c4c4f9ed9bf6e23ce1ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3714597
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Jakub Flis <jaflis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1017572}

--- chrome/browser/ash/login/screens/theme_selection_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/theme_selection_screen_browsertest.cc
@@ -7,6 +7,8 @@
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/login_screen_test_api.h"
 #include "ash/public/cpp/style/color_provider.h"
+#include "ash/shell.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "chrome/browser/ash/login/screens/guest_tos_screen.h"
 #include "chrome/browser/ash/login/screens/welcome_screen.h"
 #include "chrome/browser/ash/login/test/fake_eula_mixin.h"
@@ -35,6 +37,10 @@ const test::UIPath kLightThemeButtonPath = {kThemeSelectionId,
                                             kLightThemeButton};
 const test::UIPath kAutoThemeButtonPath = {kThemeSelectionId, kAutoThemeButton};
 const test::UIPath kNextButtonPath = {kThemeSelectionId, "nextButton"};
+const test::UIPath kScreenSubtitleClamshellPath = {
+    kThemeSelectionId, "theme-selection-subtitle-clamshell"};
+const test::UIPath kScreenSubtitleTabletPath = {
+    kThemeSelectionId, "theme-selection-subtitle-tablet"};
 }  // namespace
 
 class ThemeSelectionScreenTest
@@ -70,6 +76,12 @@ class ThemeSelectionScreenTest
     base::RunLoop run_loop;
     quit_closure_ = base::BindOnce(run_loop.QuitClosure());
     run_loop.Run();
+  }
+
+  void setTabletMode(bool enabled) {
+    ash::TabletMode::Waiter waiter(enabled);
+    ash::Shell::Get()->tablet_mode_controller()->SetEnabledForTest(enabled);
+    waiter.Wait();
   }
 
   absl::optional<ThemeSelectionScreen::Result> result_;
@@ -118,6 +130,20 @@ IN_PROC_BROWSER_TEST_P(ThemeSelectionScreenTest, SelectTheme) {
   } else if (selectedOption == kAutoThemeButton) {
     EXPECT_EQ(profile->GetPrefs()->GetInteger(prefs::kDarkModeScheduleType), 1);
   }
+}
+
+IN_PROC_BROWSER_TEST_F(ThemeSelectionScreenTest, ToggleTabletMode) {
+  ShowThemeSelectionScreen();
+  // by default clamshell mode is used
+  test::OobeJS().ExpectVisiblePath(kScreenSubtitleClamshellPath);
+
+  // switch to tablet mode
+  setTabletMode(true);
+  test::OobeJS().ExpectVisiblePath(kScreenSubtitleTabletPath);
+
+  // and back to clamshell
+  setTabletMode(false);
+  test::OobeJS().ExpectVisiblePath(kScreenSubtitleClamshellPath);
 }
 
 INSTANTIATE_TEST_SUITE_P(All,
