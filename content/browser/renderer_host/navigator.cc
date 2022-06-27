5ecf0da3bec55d5abc07ffc3313b31cfa3865be9
Miyoung Shin <myid.shin@igalia.com>
2022-06-27 05:00:30 +0000

    [MPArch] Remove TODO(crbug.com/1314749) in Navigator
    It makes sense to use IsMainFrame in
    Navigator::GetNavigationEntryForRendererInitiatedNavigation
    since each main frame has its own NavigationController, even
    when not the primary outermost main frame (e.g., prerendering,
    fenced frames).
    
    So, this CL removes TODO comment that had been added when switching
    IsOutermostMainFrame.
    
    Bug: 1314749
    Change-Id: I2a77078aad03266c91ebc7a7bafcdb1fa58f0cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721155
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018103}

--- content/browser/renderer_host/navigator.cc
+++ content/browser/renderer_host/navigator.cc
@@ -1255,12 +1255,9 @@ NavigationEntryImpl*
 Navigator::GetNavigationEntryForRendererInitiatedNavigation(
     const blink::mojom::CommonNavigationParams& common_params,
     FrameTreeNode* frame_tree_node) {
-  // TODO(crbug.com/1314749): With MPArch there may be multiple main frames and
-  // so IsMainFrame should not be used to identify embedded frames. In this
-  // case, we use IsMainFrame since our only instance of embedded main frames
-  // (fenced frames) have their own NavigationController, and we will enforce
-  // main frame behavior at that level. Will need to follow up to confirm
-  // correctness.
+  // With MPArch, there may be multiple main frames, but each one has its own
+  // NavigationController. Thus, it's correct to check for NavigationEntries for
+  // each main frame, even if one is embedded (e.g., a fenced frame).
   if (!frame_tree_node->IsMainFrame())
     return nullptr;
 
