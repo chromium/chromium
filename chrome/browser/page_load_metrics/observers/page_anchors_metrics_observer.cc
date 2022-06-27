df50765be65b00bb0a30281f5c04920b18ddab53
Takashi Toyoshima <toyoshim@chromium.org>
2022-06-27 06:21:51 +0000

    PageLoadMetrics: PageAnchorsMetricsObserver for Prerender2
    If a page starts as a pre-rendered page,
    PageAnchorsMetricsObserver needs to be silent til the page
    is activated, and records metrics only if the event happens
    after the activation.
    
    Bug: 1317494
    Change-Id: Ifac9bd2e58cf898316ed10de29388a1df0139488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3695883
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018115}

--- chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.cc
@@ -63,6 +63,14 @@ void PageAnchorsMetricsObserver::RecordUkm() {
 }
 
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
+PageAnchorsMetricsObserver::OnPrerenderStart(
+    content::NavigationHandle* navigation_handle,
+    const GURL& currently_committed_url) {
+  is_in_prerendered_page_ = true;
+  return CONTINUE_OBSERVING;
+}
+
+page_load_metrics::PageLoadMetricsObserver::ObservePolicy
 PageAnchorsMetricsObserver::OnFencedFramesStart(
     content::NavigationHandle* navigation_handle,
     const GURL& currently_committed_url) {
@@ -73,11 +81,25 @@ PageAnchorsMetricsObserver::OnFencedFramesStart(
 
 void PageAnchorsMetricsObserver::OnComplete(
     const page_load_metrics::mojom::PageLoadTiming&) {
+  // Do not report Ukm while prerendering.
+  if (is_in_prerendered_page_)
+    return;
+
   RecordUkm();
 }
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
 PageAnchorsMetricsObserver::FlushMetricsOnAppEnterBackground(
     const page_load_metrics::mojom::PageLoadTiming&) {
+  // Do not report Ukm while prerendering.
+  if (is_in_prerendered_page_)
+    return CONTINUE_OBSERVING;
+
   RecordUkm();
   return STOP_OBSERVING;
+}
+
+void PageAnchorsMetricsObserver::DidActivatePrerenderedPage(
+    content::NavigationHandle* navigation_handle) {
+  DCHECK(is_in_prerendered_page_);
+  is_in_prerendered_page_ = false;
 }
