cd41706932620d25e14ca6ecf7d3adef61537fbb
Vignesh Shenvi <vshenvi@google.com>
2022-06-27 23:14:27 +0000

    Add browser tests for the ContactCenterInsightsExtensionManager
    This change adds browser tests for the
    ContactCenterInsightsExtensionManager for completeness so we also verify
    the Contact Center Insights extension is actually installed when the
    policy is set.
    
    Bug: b:235562441
    Change-Id: Id29b22a354d3d5ef053784363606e659f46d4dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698939
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1018454}

---
+++ chrome/browser/chromeos/extensions/contact_center_insights/contact_center_insights_extension_manager_lacros_browsertest.cc
@@ -0,0 +1,85 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/common/extensions/extension_constants.h"
+#include "chrome/common/pref_names.h"
+#include "chromeos/startup/browser_init_params.h"
+#include "components/policy/core/common/policy_loader_lacros.h"
+#include "content/public/test/browser_test.h"
+#include "extensions/browser/extension_registry.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace {
+
+constexpr char kFakeDMToken[] = "fake-dm-token";
+constexpr char kFakeProfileClientId[] = "fake-profile-client-id";
+constexpr char kAffiliationId[] = "affiliation-id";
+constexpr char kDomain[] = "domain.com";
+
+void SetupUserDeviceAffiliation() {
+  ::enterprise_management::PolicyData profile_policy_data;
+  profile_policy_data.add_user_affiliation_ids(kAffiliationId);
+  profile_policy_data.set_managed_by(kDomain);
+  profile_policy_data.set_device_id(kFakeProfileClientId);
+  profile_policy_data.set_request_token(kFakeDMToken);
+  ::policy::PolicyLoaderLacros::set_main_user_policy_data_for_testing(
+      std::move(profile_policy_data));
+
+  ::crosapi::mojom::BrowserInitParamsPtr init_params =
+      ::crosapi::mojom::BrowserInitParams::New();
+  init_params->device_properties = crosapi::mojom::DeviceProperties::New();
+  init_params->device_properties->device_dm_token = kFakeDMToken;
+  init_params->device_properties->device_affiliation_ids = {kAffiliationId};
+  BrowserInitParams::SetInitParamsForTests(std::move(init_params));
+}
+
+// Browser test that validates extension installation status when the
+// `InsightsExtensionEnabled` policy is set/unset on Lacros.
+class ContactCenterInsightsExtensionManagerBrowserTest
+    : public ::extensions::ExtensionBrowserTest {
+ protected:
+  void SetUpInProcessBrowserTestFixture() override {
+    ::extensions::ExtensionBrowserTest::SetUpInProcessBrowserTestFixture();
+    SetupUserDeviceAffiliation();
+  }
+
+  void SetPrefValue(bool value) {
+    profile()->GetPrefs()->SetBoolean(::prefs::kInsightsExtensionEnabled,
+                                      value);
+    content::RunAllTasksUntilIdle();
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(ContactCenterInsightsExtensionManagerBrowserTest,
+                       ExtensionUnloadedByDefault) {
+  EXPECT_FALSE(extension_registry()->GetInstalledExtension(
+      ::extension_misc::kContactCenterInsightsExtensionId));
+}
+
+IN_PROC_BROWSER_TEST_F(ContactCenterInsightsExtensionManagerBrowserTest,
+                       InstallExtensionWhenPrefSet) {
+  SetPrefValue(true);
+  EXPECT_TRUE(extension_registry()->enabled_extensions().GetByID(
+      ::extension_misc::kContactCenterInsightsExtensionId));
+}
+
+IN_PROC_BROWSER_TEST_F(ContactCenterInsightsExtensionManagerBrowserTest,
+                       ExtensionUninstalledWhenPrefUnset) {
+  // Set pref to enable extension.
+  SetPrefValue(true);
+  EXPECT_TRUE(extension_registry()->enabled_extensions().GetByID(
+      ::extension_misc::kContactCenterInsightsExtensionId));
+
+  // Unset pref and verify extension is unloaded
+  SetPrefValue(false);
+  EXPECT_FALSE(extension_registry()->GetInstalledExtension(
+      ::extension_misc::kContactCenterInsightsExtensionId));
+}
+
+}  // namespace
+}  // namespace chromeos
