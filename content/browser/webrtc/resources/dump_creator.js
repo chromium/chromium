008f93fef5cccb08321d2e14a37311a0beac9efb
Philipp Hancke <philipp.hancke@googlemail.com>
2022-06-27 11:18:31 +0000

    webrtc-internals: add option to download as gzip
    using the CompressionStream API
    
    BUG=chromium:1339784
    
    Change-Id: I4ca82087760390220bd352cca48a0d6866fbef24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3702311
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018173}

--- content/browser/webrtc/resources/dump_creator.js
+++ content/browser/webrtc/resources/dump_creator.js
@@ -40,37 +40,37 @@ export class DumpCreator {
     content.appendChild($('dump-template').content.cloneNode(true));
     content.getElementsByTagName('a')[0].addEventListener(
         'click', this.onDownloadData_.bind(this));
-    content.getElementsByTagName('input')[0].addEventListener(
-        'click', this.onAudioDebugRecordingsChanged_.bind(this));
     content.getElementsByTagName('input')[1].addEventListener(
+        'click', this.onAudioDebugRecordingsChanged_.bind(this));
+    content.getElementsByTagName('input')[2].addEventListener(
         'click', this.onEventLogRecordingsChanged_.bind(this));
   }
 
   // Mark the diagnostic audio recording checkbox checked.
   setAudioDebugRecordingsCheckbox() {
-    this.root_.getElementsByTagName('input')[0].checked = true;
+    this.root_.getElementsByTagName('input')[1].checked = true;
   }
 
   // Mark the diagnostic audio recording checkbox unchecked.
   clearAudioDebugRecordingsCheckbox() {
-    this.root_.getElementsByTagName('input')[0].checked = false;
+    this.root_.getElementsByTagName('input')[1].checked = false;
   }
 
   // Mark the event log recording checkbox checked.
   setEventLogRecordingsCheckbox() {
-    this.root_.getElementsByTagName('input')[1].checked = true;
+    this.root_.getElementsByTagName('input')[2].checked = true;
   }
 
   // Mark the event log recording checkbox unchecked.
   clearEventLogRecordingsCheckbox() {
-    this.root_.getElementsByTagName('input')[1].checked = false;
+    this.root_.getElementsByTagName('input')[2].checked = false;
   }
 
   // Mark the event log recording checkbox as mutable/immutable.
   setEventLogRecordingsCheckboxMutability(mutable) {
     // TODO(eladalon): Remove reliance on number and order of elements.
     // https://crbug.com/817391
-    this.root_.getElementsByTagName('input')[1].disabled = !mutable;
+    this.root_.getElementsByTagName('input')[2].disabled = !mutable;
     if (!mutable) {
       const label = this.root_.getElementsByTagName('label')[2];
       label.style = 'color:red;';
@@ -84,20 +84,34 @@ export class DumpCreator {
    *
    * @private
    */
-  onDownloadData_() {
+  async onDownloadData_(event) {
+    const useCompression = this.root_.getElementsByTagName('input')[0].checked;
     const dumpObject = {
       'getUserMedia': userMediaRequests,
       'PeerConnections': peerConnectionDataStore,
       'UserAgent': navigator.userAgent,
     };
     const textBlob =
-        new Blob([JSON.stringify(dumpObject, null, 1)], {type: 'octet/stream'});
-    const URL = window.URL.createObjectURL(textBlob);
-
+      new Blob([JSON.stringify(dumpObject, null, 1)], {type: 'octet/stream'});
+    let url;
+    if (useCompression) {
+      const compressionStream = new CompressionStream('gzip');
+      const binaryStream = textBlob.stream().pipeThrough(compressionStream);
+      const binaryBlob = await new Response(binaryStream).blob();
+      url = URL.createObjectURL(binaryBlob);
+      // Since this is async we can't use the default event and need to click
+      // again (while avoiding an infinite loop).
+      const anchor = document.createElement('a');
+      anchor.download = 'webrtc_internals_dump.gz'
+      anchor.href = url;
+      anchor.click();
+      return;
+    }
+    url = URL.createObjectURL(textBlob);
     const anchor = this.root_.getElementsByTagName('a')[0];
-    anchor.href = URL;
-    anchor.download = 'webrtc_internals_dump.txt';
-    // The default action of the anchor will download the URL.
+    anchor.download = 'webrtc_internals_dump.txt'
+    anchor.href = url;
+    // The default action of the anchor will download the url.
   }
 
   /**
@@ -106,7 +120,7 @@ export class DumpCreator {
    * @private
    */
   onAudioDebugRecordingsChanged_() {
-    const enabled = this.root_.getElementsByTagName('input')[0].checked;
+    const enabled = this.root_.getElementsByTagName('input')[1].checked;
     if (enabled) {
       chrome.send('enableAudioDebugRecordings');
     } else {
@@ -120,7 +134,7 @@ export class DumpCreator {
    * @private
    */
   onEventLogRecordingsChanged_() {
-    const enabled = this.root_.getElementsByTagName('input')[1].checked;
+    const enabled = this.root_.getElementsByTagName('input')[2].checked;
     if (enabled) {
       chrome.send('enableEventLogRecordings');
     } else {
