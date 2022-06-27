53cc20b5470e70c17465ff6c2cbb9298c39b708d
Takashi Toyoshima <toyoshim@chromium.org>
2022-06-27 07:57:25 +0000

    Prerender2: verify chrome.declarativeContent behaviors
    chrome.declarativeContent API is used to manage per-tab UI states,
    and its declarative rules should not match navigation for
    pre-rendered pages to keep the tab state consistent with the
    primary page. Instead, it should match for pre-rendered pages'
    activation as it's going to be the primary page.
    
    Currently, we run the rule matching evaluation in
    extensions::ChromeContentRulesRegistry::DidFinishNavigation,
    and this method is called only for the primary main frame's
    navigation completion via TabHelper::DidFinishNavigation.
    So, it works as we expect.
    
    This patch adds SetIconAPITest.Prerendering test case to
    guarantee this expected behavior. To minimize duplicated test
    code, this patch also factor out common setup code into
    LoadTestExtension(), and split the Overview test into two
    separated cases, SetIconAPITest.Overview for the original
    basic test suites, and SetIconAPITest.Parameter that was lately
    added as a regression test.
    
    SetIconAPITest.Prerendering requires to setup the embedded
    test server as Prerender needs a successful HTTP response to
    be alive.
    
    Relevant Explainer in the design doc:
    https://docs.google.com/document/d/16Sy2Xq-AcnhwnpC0q232OMSmxTpQYDANRAO-zJ8DKiA/edit#bookmark=id.ir95wsz9iv3n
    
    Bug: 1278141
    Change-Id: Ib812c0870e186e54b2318bd5ae66ac64c07230de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3713495
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018130}

--- chrome/browser/extensions/api/declarative_content/set_icon_apitest.cc
+++ chrome/browser/extensions/api/declarative_content/set_icon_apitest.cc
@@ -8,32 +8,28 @@
 #include "components/version_info/version_info.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/prerender_test_util.h"
 #include "extensions/browser/api/declarative/rules_registry.h"
 #include "extensions/browser/api/declarative/rules_registry_service.h"
 #include "extensions/browser/extension_action_manager.h"
 #include "extensions/common/features/feature_channel.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "ui/gfx/image/image.h"
 
 namespace extensions {
 namespace {
 
-const char kDeclarativeContentManifest[] =
-    "{\n"
-    "  \"name\": \"Declarative Content apitest\",\n"
-    "  \"version\": \"0.1\",\n"
-    "  \"manifest_version\": 2,\n"
-    "  \"description\": \n"
-    "      \"end-to-end browser test for the declarative Content API\",\n"
-    "  \"background\": {\n"
-    "    \"scripts\": [\"background.js\"]\n"
-    "  },\n"
-    "  \"page_action\": {},\n"
-    "  \"permissions\": [\n"
-    "    \"declarativeContent\"\n"
-    "  ]\n"
-    "}\n";
+const char kDeclarativeContentManifest[] = R"({
+  "name": "Declarative Content apitest",
+  "version": "0.1",
+  "manifest_version": 2,
+  "description": "end-to-end browser test for the declarative Content API",
+  "background": {"scripts": ["background.js"]},
+  "page_action": {},
+  "permissions": ["declarativeContent"]
+})";
 
 constexpr char kOneByOneImageData[] =
     "GAAAAAAAAAAQAAAAAAAAADAAAAAAAAAAKAAAAAAAAAACAAAAAQAAAAEAAAAAAAAAAAAAAAAAAA"
@@ -44,88 +40,159 @@ class SetIconAPITest : public ExtensionApiTest {
   SetIconAPITest()
       // Set the channel to "trunk" since declarativeContent is restricted
       // to trunk.
-      : current_channel_(version_info::Channel::UNKNOWN) {
-  }
+      : current_channel_(version_info::Channel::UNKNOWN) {}
   ~SetIconAPITest() override {}
 
+ protected:
+  const Extension* LoadTestExtension() {
+    ext_dir_.WriteManifest(kDeclarativeContentManifest);
+    ext_dir_.WriteFile(FILE_PATH_LITERAL("background.js"), R"(
+  var declarative = chrome.declarative;
+
+  var PageStateMatcher = chrome.declarativeContent.PageStateMatcher;
+  var SetIcon = chrome.declarativeContent.SetIcon;
+
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var imageData = ctx.createImageData(1, 1);
+
+  var rule0 = {
+    conditions: [new PageStateMatcher({pageUrl: {queryContains: "show"}})],
+    actions: [new SetIcon({"imageData": imageData})]
+  };
+  var testEvent = chrome.declarativeContent.onPageChanged;
+
+  testEvent.removeRules(undefined, function() {
+    testEvent.addRules([rule0], function() {
+      chrome.test.sendMessage("ready", function(reply) {});
+    });
+  });
+)");
+    ExtensionTestMessageListener ready("ready");
+    const Extension* extension = LoadExtension(ext_dir_.UnpackedPath());
+    if (!extension)
+      return nullptr;
+
+    // Wait for declarative rules to be set up.
+    profile()->GetDefaultStoragePartition()->FlushNetworkInterfaceForTesting();
+    EXPECT_TRUE(GetExtensionAction(*extension));
+
+    EXPECT_TRUE(ready.WaitUntilSatisfied());
+
+    return extension;
+  }
+
+  const ExtensionAction* GetExtensionAction(const Extension& extension) {
+    return ExtensionActionManager::Get(profile())->GetExtensionAction(
+        extension);
+  }
+
+  content::WebContents* GetActiveWebContents() {
+    return browser()->tab_strip_model()->GetWebContentsAt(0);
+  }
+
+ private:
   extensions::ScopedCurrentChannel current_channel_;
   TestExtensionDir ext_dir_;
 };
 
 IN_PROC_BROWSER_TEST_F(SetIconAPITest, Overview) {
-  ext_dir_.WriteManifest(kDeclarativeContentManifest);
-  ext_dir_.WriteFile(
-      FILE_PATH_LITERAL("background.js"),
-      "var declarative = chrome.declarative;\n"
-      "\n"
-      "var PageStateMatcher = chrome.declarativeContent.PageStateMatcher;\n"
-      "var SetIcon = chrome.declarativeContent.SetIcon;\n"
-      "\n"
-      "var canvas = document.createElement(\'canvas\');\n"
-      "var ctx = canvas.getContext(\"2d\");"
-      "var imageData = ctx.createImageData(1, 1);\n"
-      "\n"
-      "var rule0 = {\n"
-      "  conditions: [new PageStateMatcher({\n"
-      "                   pageUrl: {hostPrefix: \"test1\"}})],\n"
-      "  actions: [new SetIcon({\"imageData\": imageData})]\n"
-      "}\n"
-      "\n"
-      "var testEvent = chrome.declarativeContent.onPageChanged;\n"
-      "\n"
-      "testEvent.removeRules(undefined, function() {\n"
-      "  testEvent.addRules([rule0], function() {\n"
-      "    chrome.test.sendMessage(\"ready\", function(reply) {\n"
-      "    })\n"
-      "  });\n"
-      "});\n");
-  ExtensionTestMessageListener ready("ready");
-  const Extension* extension = LoadExtension(ext_dir_.UnpackedPath());
+  const Extension* extension = LoadTestExtension();
   ASSERT_TRUE(extension);
-  // Wait for declarative rules to be set up.
-  profile()->GetDefaultStoragePartition()->FlushNetworkInterfaceForTesting();
-  const ExtensionAction* action =
-      ExtensionActionManager::Get(browser()->profile())
-          ->GetExtensionAction(*extension);
-  ASSERT_TRUE(action);
 
-  ASSERT_TRUE(ready.WaitUntilSatisfied());
-
-  // Regression test for crbug.com/1231027.
-  {
-    scoped_refptr<RulesRegistry> rules_registry =
-        extensions::RulesRegistryService::Get(browser()->profile())
-            ->GetRulesRegistry(RulesRegistryService::kDefaultRulesRegistryID,
-                               "declarativeContent.onPageChanged");
-    ASSERT_TRUE(rules_registry);
-
-    std::vector<const api::events::Rule*> rules;
-    rules_registry->GetAllRules(extension->id(), &rules);
-    ASSERT_EQ(1u, rules.size());
-    ASSERT_EQ(rules[0]->actions.size(), 1u);
-
-    base::Value& action_value = *rules[0]->actions[0];
-    base::Value* action_instance_type = action_value.FindPath("instanceType");
-    ASSERT_TRUE(action_instance_type);
-    EXPECT_EQ("declarativeContent.SetIcon", action_instance_type->GetString());
-
-    base::Value* image_data_value = action_value.FindPath({"imageData", "1"});
-    ASSERT_TRUE(image_data_value);
-    EXPECT_EQ(kOneByOneImageData, image_data_value->GetString());
-  }
-
-  content::WebContents* const tab =
-      browser()->tab_strip_model()->GetWebContentsAt(0);
+  content::WebContents* const tab = GetActiveWebContents();
   const int tab_id = ExtensionTabUtil::GetTabId(tab);
 
   // There should be no declarative icon until we navigate to a matched page.
+  const ExtensionAction* action = GetExtensionAction(*extension);
+  ASSERT_TRUE(action);
+
   EXPECT_TRUE(action->GetDeclarativeIcon(tab_id).IsEmpty());
-  EXPECT_FALSE(NavigateInRenderer(tab, GURL("http://test1/")));
+  EXPECT_FALSE(NavigateInRenderer(tab, GURL("http://example.com/?show")));
   EXPECT_FALSE(action->GetDeclarativeIcon(tab_id).IsEmpty());
 
   // Navigating to an unmatched page should reset the icon.
-  EXPECT_FALSE(NavigateInRenderer(tab, GURL("http://test2/")));
+  EXPECT_FALSE(NavigateInRenderer(tab, GURL("http://example.com/?hide")));
   EXPECT_TRUE(action->GetDeclarativeIcon(tab_id).IsEmpty());
 }
+
+// Regression test for crbug.com/1231027.
+IN_PROC_BROWSER_TEST_F(SetIconAPITest, Parameter) {
+  const Extension* extension = LoadTestExtension();
+  ASSERT_TRUE(extension);
+
+  scoped_refptr<RulesRegistry> rules_registry =
+      extensions::RulesRegistryService::Get(browser()->profile())
+          ->GetRulesRegistry(RulesRegistryService::kDefaultRulesRegistryID,
+                             "declarativeContent.onPageChanged");
+  ASSERT_TRUE(rules_registry);
+
+  std::vector<const api::events::Rule*> rules;
+  rules_registry->GetAllRules(extension->id(), &rules);
+  ASSERT_EQ(1u, rules.size());
+  ASSERT_EQ(rules[0]->actions.size(), 1u);
+
+  base::Value& action_value = *rules[0]->actions[0];
+  base::Value* action_instance_type = action_value.FindPath("instanceType");
+  ASSERT_TRUE(action_instance_type);
+  EXPECT_EQ("declarativeContent.SetIcon", action_instance_type->GetString());
+
+  base::Value* image_data_value = action_value.FindPath({"imageData", "1"});
+  ASSERT_TRUE(image_data_value);
+  EXPECT_EQ(kOneByOneImageData, image_data_value->GetString());
+}
+
+class SetIconAPIPrerenderingTest : public SetIconAPITest {
+ public:
+  SetIconAPIPrerenderingTest()
+      : prerender_helper_(base::BindRepeating(
+            &SetIconAPIPrerenderingTest::GetActiveWebContents,
+            base::Unretained(this))) {}
+  ~SetIconAPIPrerenderingTest() override = default;
+
+ protected:
+  int Prerender(const GURL& url) { return prerender_helper_.AddPrerender(url); }
+  void Activate(const GURL& url) { prerender_helper_.NavigatePrimaryPage(url); }
+
+ private:
+  void SetUp() override {
+    prerender_helper_.SetUp(embedded_test_server());
+    ExtensionApiTest::SetUp();
+  }
+
+  content::test::PrerenderTestHelper prerender_helper_;
+};
+
+IN_PROC_BROWSER_TEST_F(SetIconAPIPrerenderingTest, Overview) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  const Extension* extension = LoadTestExtension();
+  ASSERT_TRUE(extension);
+
+  content::WebContents* const tab = GetActiveWebContents();
+  const int tab_id = ExtensionTabUtil::GetTabId(tab);
+
+  // There should be no declarative icon until we navigate to a matched page.
+  const ExtensionAction* action = GetExtensionAction(*extension);
+  ASSERT_TRUE(action);
+
+  EXPECT_TRUE(action->GetDeclarativeIcon(tab_id).IsEmpty());
+
+  const GURL kInitialUrl = embedded_test_server()->GetURL("/empty.html?show");
+  EXPECT_TRUE(NavigateInRenderer(tab, kInitialUrl));
+  EXPECT_FALSE(action->GetDeclarativeIcon(tab_id).IsEmpty());
+
+  // Prerendering an unmatched page should not reset the icon.
+  const GURL kPrerenderingUrl =
+      embedded_test_server()->GetURL("/empty.html?hide");
+  int host_id = Prerender(kPrerenderingUrl);
+  ASSERT_NE(content::RenderFrameHost::kNoFrameTreeNodeId, host_id);
+  EXPECT_FALSE(action->GetDeclarativeIcon(tab_id).IsEmpty());
+
+  // Activating the unmatched page should reset the icon.
+  Activate(kPrerenderingUrl);
+  EXPECT_TRUE(action->GetDeclarativeIcon(tab_id).IsEmpty());
+  EXPECT_EQ(tab->GetLastCommittedURL(), kPrerenderingUrl);
+}
+
 }  // namespace
 }  // namespace extensions
