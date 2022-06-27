ed26d8e9ab79308488eb8a1e08917010f98a411d
Brandon Wylie <wylieb@chromium.org>
2022-06-27 21:52:11 +0000

    [Sheriff] Disable failing BookmarkTest#testReadingListOpenInRegularTab
    Bug: 1339976
    Change-Id: I32abf40cdd0ef2c6d1c34048990f770d5961cce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727450
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018424}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
@@ -1283,6 +1283,7 @@ public class BookmarkTest {
     @SmallTest
     @Features.EnableFeatures({ChromeFeatureList.READ_LATER + ":use_cct/false"})
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
+    @DisabledTest(message = "crbug.com/1339976")
     public void testReadingListOpenInRegularTab() throws Exception {
         addReadingListBookmark(TEST_PAGE_TITLE_GOOGLE, mTestUrlA);
         BookmarkPromoHeader.forcePromoStateForTests(SyncPromoState.NO_PROMO);
