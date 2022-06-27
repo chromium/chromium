0e8dd840d7e559577dde040034d9f4cc79736238
Joshua Pawlicki <waffles@chromium.org>
2022-06-27 17:46:29 +0000

    Updater: Fix chromium mac 3pp install scripts.
    -f is the wrong test to use since .app is a directory.
    
    But, we don't need the if statement anymore in the first place, so just
    simplify the whole thing.
    
    Bug: 1292189
    Change-Id: I4d9108026bc26567ab1efab00072976096b2ed34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726235
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018295}

--- third_party/updater/chromium_mac_arm64/3pp/install.sh
+++ third_party/updater/chromium_mac_arm64/3pp/install.sh
@@ -11,8 +11,4 @@ set -o pipefail
 # The commands below should output the built product to this directory.
 PREFIX="$1"
 
-if [ -f "updater/ChromiumUpdater_test.app" ]; then
-  mv updater/ChromiumUpdater_test.app "$PREFIX"
-elif [ -f "updater/ChromiumUpdater.app" ]; then
-  mv updater/ChromiumUpdater.app "$PREFIX"
-fi
+mv updater/ChromiumUpdater_test.app "$PREFIX"
