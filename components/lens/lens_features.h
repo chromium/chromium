77a4358fbbfbbba7133d944883a53ccb208a9b4f
Duncan Mercer <mercerd@google.com>
2022-06-28 18:46:29 +0000

    Update Lens feature flags to prepare for upcoming Lens experiments.
    After talks with the Chrome metrics team, it was clear we needed to update our feature flags to be able to run many experiments in parallel. This change moves the transparent images fix that was guarded by a feature param feature to its own feature flag. This change also moves the context menu string feature params to a separate feature. Finally, the kFullscreenSearch is no longer a flag and instead a param tied to the same feature as the context menu strings since the AVS experiments aretied to the new context menu strings.
    
    (cherry picked from commit 057fbc4bc0d755aa91af428224d645b4979fcc9b)
    
    Bug: 1339018, b:236970765
    Change-Id: Ib41408100e5dd62b5d4fbea2427ab79ccd4e2e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3722538
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1018397}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732749
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/branch-heads/5112@{#404}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- components/lens/lens_features.h
+++ components/lens/lens_features.h
@@ -16,8 +16,15 @@ namespace features {
 // Enables context menu search by image sending to the Lens homepage.
 extern const base::Feature kLensStandalone;
 
-// Enables Lens fullscreen search on Desktop platforms.
-extern const base::Feature kLensFullscreenSearch;
+// Feature that controls the compression of images before they are sent to Lens.
+extern const base::Feature kLensImageCompression;
+
+// Enables a variety of changes aimed to improve user's engagement with current
+// Lens features.
+extern const base::Feature kLensSearchOptimizations;
+
+// Enables a fix to properly handle transparent images in Lens Image Search
+extern const base::Feature kLensTransparentImagesFix;
 
 // Enables a fix for cursor pointer/crosshair state over overlay on Mac.
 // TODO(crbug/1266514): make default and remove feature once launched.
@@ -42,8 +49,8 @@ extern const base::FeatureParam<bool> kEnableUKMLoggingForImageSearch;
 // Enables the side panel for Lens features on Chrome where supported.
 extern const base::FeatureParam<bool> kEnableSidePanelForLens;
 
-// Sends images as PNG to Lens Standalone to fix issues with transparency.
-extern const base::FeatureParam<bool> kSendImagesAsPng;
+// Enables Lens fullscreen search on Desktop platforms.
+extern const base::FeatureParam<bool> kEnableFullscreenSearch;
 
 // Returns whether to enable UKM logging for Lens Region Search feature.
 extern bool GetEnableUKMLoggingForRegionSearch();
