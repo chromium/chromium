607d006745f5a88b4708567a91489d484370544f
David Maunder <davidjm@google.com>
2022-06-28 23:29:02 +0000

    [Sheriff] Enable TabGridDialogTest#testUndoClosureInDialog_WithStartSurface for M
    It has been demonstrated that the test is not flaky on M
    
    Bug: 1335185
    Change-Id: I23e13b307b4c365f5b0f3af318794b907f3fe9bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733169
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018857}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -929,8 +929,10 @@ public class TabGridDialogTest {
     @MediumTest
     @Features.EnableFeatures({ChromeFeatureList.START_SURFACE_ANDROID + "<Study"})
     @CommandLineFlags.Add({"force-fieldtrials=Study/Group", START_SURFACE_BASE_PARAMS + "/single"})
-    @DisabledTest(message = "crbug.com/1119899, crbug.com/1131545")
-    public void testUndoClosureInDialog_WithStartSurface() throws Exception {
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.M,
+            message = "crbug.com/1119899, crbug.com/1131545")
+    public void
+    testUndoClosureInDialog_WithStartSurface() throws Exception {
         // Create a tab group with 2 tabs.
         finishActivity(mActivityTestRule.getActivity());
         createThumbnailBitmapAndWriteToFile(0);
