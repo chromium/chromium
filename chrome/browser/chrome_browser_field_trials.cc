51c49130e061e74427071ab501ec70379d5946fc
Peter Boström <pbos@chromium.org>
2022-06-28 15:40:27 +0000

    [stability] Remove DCheckIsFatal client-side cointoss
    DCHECK_IS_CONFIGURABLE configures DCHECKs to be non-FATAL by default,
    and non-FATAL DCHECKs will dump-without-crashing if they fire.
    
    Remove the client-side field-trial for configurable DCHECKs leaving the
    DCheckIsFatal Feature available for developers to opt-in to run with
    FATAL rather than dump-without-crashing.
    
    Bug: 596231
    Change-Id: Id04af25c20c53cae2c5fc6951f051d4f73bd7c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3727614
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1018700}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -60,31 +60,6 @@ void CreateFallbackUkmSamplingTrialIfNeeded(base::FeatureList* feature_list) {
       chrome::GetChannel() == version_info::Channel::STABLE, feature_list);
 }
 
-void MaybeCreateDCheckIsFatalFieldTrial(base::FeatureList* feature_list) {
-#if defined(DCHECK_IS_CONFIGURABLE)
-  // If DCHECK_IS_CONFIGURABLE then configure the DCheckIsFatal dynamic trial
-  // (see crbug.com/596231). This must be instantiated before the FeatureList
-  // is set, since FeatureList::SetInstance() will configure LOGGING_DCHECK
-  // based on the Feature's state, in DCHECK_IS_CONFIGURABLE builds.
-  // Always enable the trial at 50/50 per-session.
-  base::FieldTrial* const trial = base::FieldTrialList::FactoryGetFieldTrial(
-      "DCheckIsFatal", 100, "Default",
-      base::FieldTrial::RandomizationType::SESSION_RANDOMIZED,
-      /* default_group_number */ nullptr);
-  const int enabled_group = trial->AppendGroup("Enabled_20220517", 50);
-  trial->AppendGroup("Disabled_20220517", 50);
-
-  LOG(WARNING) << "DCheckIsFatal group: " << trial->group_name();
-
-  feature_list->RegisterFieldTrialOverride(
-      base::kDCheckIsFatalFeature.name,
-      ((trial->group() == enabled_group)
-           ? base::FeatureList::OVERRIDE_ENABLE_FEATURE
-           : base::FeatureList::OVERRIDE_DISABLE_FEATURE),
-      trial);
-#endif  // defined(DCHECK_IS_CONFIGURABLE)
-}
-
 }  // namespace
 
 ChromeBrowserFieldTrials::ChromeBrowserFieldTrials(PrefService* local_state)
@@ -124,8 +99,6 @@ void ChromeBrowserFieldTrials::SetUpFeatureControllingFieldTrials(
     ash::multidevice_setup::CreateFirstRunFieldTrial(feature_list);
 #endif
   }
-
-  MaybeCreateDCheckIsFatalFieldTrial(feature_list);
 }
 
 void ChromeBrowserFieldTrials::RegisterSyntheticTrials() {
