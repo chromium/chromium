16a5c233c3304f32f2aa0b6ebf0150458a386bf3
Steinar H. Gunderson <sesse@chromium.org>
2022-06-29 22:54:33 +0000

    Add a perftest for parsing HTML.
    This allows for easier isolation and benchmark comparison of the HTML
    parsing part only from Speedometer.
    
    Change-Id: I8395090a5632dbcb44e85cdb912d7c893f22c3f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734848
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019375}

--- third_party/blink/renderer/core/css/style_perftest.cc
+++ third_party/blink/renderer/core/css/style_perftest.cc
@@ -27,89 +27,13 @@
 #include "third_party/blink/renderer/core/html/html_body_element.h"
 #include "third_party/blink/renderer/core/loader/empty_clients.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/core/testing/no_network_web_url_loader.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/platform/heap/process_heap.h"
-#include "third_party/blink/renderer/platform/scheduler/test/fake_task_runner.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
 
 namespace blink {
-
-// A WebURLLoader simulating that requests time out forever due to no network.
-// (We don't really want to benchmark URL loading.)
-class NoNetworkWebURLLoader : public WebURLLoader {
- public:
-  NoNetworkWebURLLoader() = default;
-  NoNetworkWebURLLoader(const NoNetworkWebURLLoader&) = delete;
-  NoNetworkWebURLLoader& operator=(const NoNetworkWebURLLoader&) = delete;
-
-  // WebURLLoader member functions:
-  void LoadSynchronously(
-      std::unique_ptr<network::ResourceRequest> request,
-      scoped_refptr<WebURLRequestExtraData> url_request_extra_data,
-      bool pass_response_pipe_to_client,
-      bool no_mime_sniffing,
-      base::TimeDelta timeout_interval,
-      WebURLLoaderClient* client,
-      WebURLResponse& response,
-      absl::optional<WebURLError>&,
-      WebData&,
-      int64_t& encoded_data_length,
-      int64_t& encoded_body_length,
-      blink::WebBlobInfo& downloaded_blob,
-      std::unique_ptr<blink::ResourceLoadInfoNotifierWrapper>
-          resource_load_info_notifier_wrapper) override {
-    // Nothing should call this in our test.
-    NOTREACHED();
-  }
-  void LoadAsynchronously(
-      std::unique_ptr<network::ResourceRequest> request,
-      scoped_refptr<WebURLRequestExtraData> url_request_extra_data,
-      bool no_mime_sniffing,
-      std::unique_ptr<blink::ResourceLoadInfoNotifierWrapper>
-          resource_load_info_notifier_wrapper,
-      WebURLLoaderClient* client) override {
-    // We simply never call back, simulating load times that are larger
-    // than the test runtime.
-  }
-  void Freeze(WebLoaderFreezeMode mode) override {
-    // Ignore.
-  }
-  void DidChangePriority(WebURLRequest::Priority new_priority,
-                         int intra_priority_value) override {
-    // Ignore.
-  }
-  scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunnerForBodyLoader()
-      override {
-    return base::MakeRefCounted<scheduler::FakeTaskRunner>();
-  }
-};
-
-class NoNetworkWebURLLoaderFactory : public WebURLLoaderFactory {
- public:
-  NoNetworkWebURLLoaderFactory() = default;
-
-  std::unique_ptr<WebURLLoader> CreateURLLoader(
-      const WebURLRequest&,
-      std::unique_ptr<blink::scheduler::WebResourceLoadingTaskRunnerHandle>,
-      std::unique_ptr<blink::scheduler::WebResourceLoadingTaskRunnerHandle>,
-      CrossVariantMojoRemote<blink::mojom::KeepAliveHandleInterfaceBase>,
-      WebBackForwardCacheLoaderHelper) override {
-    return std::make_unique<NoNetworkWebURLLoader>();
-  }
-};
-
-// A LocalFrameClient that uses NoNetworkWebURLLoader, so that nothing external
-// is ever loaded.
-class NoNetworkLocalFrameClient : public EmptyLocalFrameClient {
- public:
-  NoNetworkLocalFrameClient() = default;
-
- private:
-  std::unique_ptr<WebURLLoaderFactory> CreateURLLoaderFactory() override {
-    return std::make_unique<NoNetworkWebURLLoaderFactory>();
-  }
-};
 
 static std::unique_ptr<DummyPageHolder> LoadDumpedPage(
     const base::Value::Dict& dict,
