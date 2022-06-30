db747a4be87547fef44680bf58e9c6e7efe9c7bc
Michael Thiessen <mthiesse@chromium.org>
2022-06-30 15:37:27 +0000

    Fix indentation in python script in install-build-deps.sh
    This was failing when run with the error:
    
    Building apt package list.
      File "<string>", line 2
        import re,sys; \
    
    IndentationError: unexpected indent
    Change-Id: I40f5cc3fb3bd222592480e6191ce328ce9d50120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737127
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019653}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -34,13 +34,13 @@ build_apt_package_list() {
   echo "Building apt package list." >&2
   apt-cache dumpavail | \
     python3 -c '\
-      import re,sys; \
-      o = sys.stdin.read(); \
-      p = {"i386": ":i386"}; \
-      f = re.M | re.S; \
-      r = re.compile(r"^Package: (.+?)$.+?^Architecture: (.+?)$", f); \
-      m = ["%s%s" % (x, p.get(y, "")) for x, y in re.findall(r, o)]; \
-      print("\n".join(m))'
+import re,sys; \
+o = sys.stdin.read(); \
+p = {"i386": ":i386"}; \
+f = re.M | re.S; \
+r = re.compile(r"^Package: (.+?)$.+?^Architecture: (.+?)$", f); \
+m = ["%s%s" % (x, p.get(y, "")) for x, y in re.findall(r, o)]; \
+print("\n".join(m))'
 }
 
 # Checks whether a particular package is available in the repos.
