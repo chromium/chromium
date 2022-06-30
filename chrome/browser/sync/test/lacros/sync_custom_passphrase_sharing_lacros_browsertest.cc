9bfeb8d96d55e83874bda549714a273dd3b85631
Maksim Moskvitin <mmoskvitin@google.com>
2022-06-30 15:22:06 +0000

    [Sync] Add Lacros browsertest for custom passphrase sharing
    This CL adds the first test for custom passphrase sharing between Ash
    and Lacros. The test verifies Lacros behavior when custom passphrase is
    set by other client and Ash provides decryption key to Lacros.
    
    Instead of using real Crosapi, test fixture replaces them with mocks.
    To achieve proper initialization of the browser (e.g. initial setup of
    Crosapi), the test is added as lacros_chrome_browsertest. Despite of
    this, it's still similar to sync_integration_test, e.g. derived from
    SyncTest.
    
    Bug: 1327602
    Change-Id: I610cb235d2cf3c1cbadb515238c7431f0ab513c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726196
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019646}

---
+++ chrome/browser/sync/test/lacros/sync_custom_passphrase_sharing_lacros_browsertest.cc
@@ -0,0 +1,212 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <utility>
+
+#include "chrome/browser/sync/test/integration/encryption_helper.h"
+#include "chrome/browser/sync/test/integration/passwords_helper.h"
+#include "chrome/browser/sync/test/integration/sync_test.h"
+#include "chrome/common/chrome_constants.h"
+#include "chromeos/crosapi/mojom/account_manager.mojom.h"
+#include "chromeos/crosapi/mojom/sync.mojom.h"
+#include "chromeos/lacros/lacros_service.h"
+#include "components/sync/chromeos/explicit_passphrase_mojo_utils.h"
+#include "components/sync/nigori/nigori_test_utils.h"
+#include "components/sync/test/fake_server/fake_server_nigori_helper.h"
+#include "content/public/test/browser_test.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+using testing::_;
+
+MATCHER_P(AccountKeyEq, expected_account_key, "") {
+  const crosapi::mojom::AccountKeyPtr& given_account_key = arg;
+  return given_account_key->id == expected_account_key.id &&
+         given_account_key->account_type == expected_account_key.account_type;
+}
+
+class MockSyncExplicitPassphraseClientAsh
+    : public crosapi::mojom::SyncExplicitPassphraseClient {
+ public:
+  MockSyncExplicitPassphraseClientAsh() = default;
+  ~MockSyncExplicitPassphraseClientAsh() override = default;
+
+  MOCK_METHOD(void,
+              AddObserver,
+              (mojo::PendingRemote<
+                  crosapi::mojom::SyncExplicitPassphraseClientObserver>),
+              (override));
+  MOCK_METHOD(void,
+              GetDecryptionNigoriKey,
+              (crosapi::mojom::AccountKeyPtr, GetDecryptionNigoriKeyCallback),
+              (override));
+  MOCK_METHOD(void,
+              SetDecryptionNigoriKey,
+              (crosapi::mojom::AccountKeyPtr, crosapi::mojom::NigoriKeyPtr),
+              (override));
+};
+
+class MockSyncMojoService : public crosapi::mojom::SyncService {
+ public:
+  MockSyncMojoService() = default;
+  ~MockSyncMojoService() override = default;
+
+  MOCK_METHOD(
+      void,
+      BindExplicitPassphraseClient,
+      (mojo::PendingReceiver<crosapi::mojom::SyncExplicitPassphraseClient>),
+      (override));
+  MOCK_METHOD(void,
+              BindUserSettingsClient,
+              (mojo::PendingReceiver<crosapi::mojom::SyncUserSettingsClient>),
+              (override));
+};
+
+class SyncCustomPassphraseSharingLacrosBrowserTest : public SyncTest {
+ public:
+  SyncCustomPassphraseSharingLacrosBrowserTest() : SyncTest(SINGLE_CLIENT) {}
+  SyncCustomPassphraseSharingLacrosBrowserTest(
+      const SyncCustomPassphraseSharingLacrosBrowserTest&) = delete;
+  SyncCustomPassphraseSharingLacrosBrowserTest& operator=(
+      const SyncCustomPassphraseSharingLacrosBrowserTest&) = delete;
+  ~SyncCustomPassphraseSharingLacrosBrowserTest() override = default;
+
+  base::FilePath GetProfileBaseName(int index) override {
+    // Custom passphrase sharing is enabled only for the main profile, so
+    // SyncTest should setup sync using it.
+    DCHECK_EQ(index, 0);
+    return base::FilePath(chrome::kInitialProfile);
+  }
+
+  // This test replaces production SyncService Crosapi interface with a mock.
+  // It needs to be done before connection between Ash and Lacros explicit
+  // passphrase clients is established (during creation of browser extra parts),
+  // but after LacrosService is initialized. Thus CreatedBrowserMainParts() is
+  // the only available option.
+  void CreatedBrowserMainParts(
+      content::BrowserMainParts* browser_main_parts) override {
+    SyncTest::CreatedBrowserMainParts(browser_main_parts);
+
+    // If SyncService Crosapi interface is not available on this version of
+    // ash-chrome, this test suite will no-op.
+    if (!IsServiceAvailable()) {
+      return;
+    }
+
+    // Replace the production SyncService Crosapi interface with a mock for
+    // testing.
+    mojo::Remote<crosapi::mojom::SyncService>& remote =
+        chromeos::LacrosService::Get()
+            ->GetRemote<crosapi::mojom::SyncService>();
+    remote.reset();
+    sync_mojo_service_receiver_.Bind(remote.BindNewPipeAndPassReceiver());
+
+    // Lacros client is not expected to call these methods more than once.
+    ON_CALL(sync_mojo_service_, BindExplicitPassphraseClient)
+        .WillByDefault(testing::Invoke(
+            this, &SyncCustomPassphraseSharingLacrosBrowserTest::
+                      BindExplicitPassphraseClient));
+
+    ON_CALL(client_ash_, AddObserver)
+        .WillByDefault(testing::Invoke(
+            this,
+            &SyncCustomPassphraseSharingLacrosBrowserTest::AddClientObserver));
+  }
+
+  bool IsServiceAvailable() const {
+    const chromeos::LacrosService* lacros_service =
+        chromeos::LacrosService::Get();
+    return lacros_service &&
+           lacros_service->IsAvailable<crosapi::mojom::SyncService>();
+  }
+
+  crosapi::mojom::AccountKey GetSyncingUserAccountKey() {
+    crosapi::mojom::AccountKey account_key;
+    account_key.id = GetSyncService(0)->GetAccountInfo().gaia;
+    account_key.account_type = crosapi::mojom::AccountType::kGaia;
+    return account_key;
+  }
+
+  MockSyncExplicitPassphraseClientAsh* client_ash() { return &client_ash_; }
+
+  crosapi::mojom::SyncExplicitPassphraseClientObserver* client_observer() {
+    return client_observer_remote_.get();
+  }
+
+ private:
+  void BindExplicitPassphraseClient(
+      mojo::PendingReceiver<crosapi::mojom::SyncExplicitPassphraseClient>
+          pending_receiver) {
+    client_ash_receiver_.Bind(std::move(pending_receiver));
+  }
+
+  void AddClientObserver(
+      mojo::PendingRemote<crosapi::mojom::SyncExplicitPassphraseClientObserver>
+          pending_remote) {
+    client_observer_remote_.Bind(std::move(pending_remote));
+  }
+
+  testing::NiceMock<MockSyncMojoService> sync_mojo_service_;
+  testing::NiceMock<MockSyncExplicitPassphraseClientAsh> client_ash_;
+
+  // Mojo fields order is important to allow safe use of `this` when passing
+  // callbacks.
+  mojo::Remote<crosapi::mojom::SyncExplicitPassphraseClientObserver>
+      client_observer_remote_;
+  mojo::Receiver<crosapi::mojom::SyncExplicitPassphraseClient>
+      client_ash_receiver_{&client_ash_};
+  mojo::Receiver<crosapi::mojom::SyncService> sync_mojo_service_receiver_{
+      &sync_mojo_service_};
+};
+
+IN_PROC_BROWSER_TEST_F(SyncCustomPassphraseSharingLacrosBrowserTest,
+                       ShouldGetDecryptionKeyFromAsh) {
+  if (!IsServiceAvailable()) {
+    GTEST_SKIP() << "Unsupported Ash version.";
+  }
+
+  ASSERT_TRUE(SetupSync());
+
+  // Mimic custom passphrase being set by other client.
+  const syncer::KeyParamsForTesting kKeyParams =
+      syncer::ScryptPassphraseKeyParamsForTesting("hunter2");
+  fake_server::SetNigoriInFakeServer(
+      syncer::BuildCustomPassphraseNigoriSpecifics(kKeyParams),
+      GetFakeServer());
+
+  // Inject server password encrypted with a custom passphrase.
+  const password_manager::PasswordForm password_form =
+      passwords_helper::CreateTestPasswordForm(0);
+  passwords_helper::InjectEncryptedServerPassword(
+      password_form, kKeyParams.password, kKeyParams.derivation_params,
+      GetFakeServer());
+
+  // Data isn't decryptable yet, client should enter passphrase required state.
+  ASSERT_TRUE(
+      PassphraseRequiredStateChecker(GetSyncService(0), /*desired_state=*/true)
+          .Wait());
+
+  // Mimic passphrase being provided by Ash, verify that passphrase is no longer
+  // required and the data is decryptable.
+  EXPECT_CALL(*client_ash(), GetDecryptionNigoriKey(
+                                 AccountKeyEq(GetSyncingUserAccountKey()), _))
+      .WillOnce([&kKeyParams](auto account_key, auto callback) {
+        std::move(callback).Run(
+            syncer::NigoriToMojo(*syncer::Nigori::CreateByDerivation(
+                kKeyParams.derivation_params, kKeyParams.password)));
+      });
+  client_observer()->OnPassphraseAvailable();
+  EXPECT_TRUE(
+      PassphraseRequiredStateChecker(GetSyncService(0), /*desired_state=*/false)
+          .Wait());
+  EXPECT_TRUE(PasswordFormsChecker(0, {password_form}).Wait());
+}
+
+}  // namespace
