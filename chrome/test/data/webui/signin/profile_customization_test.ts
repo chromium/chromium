dfad62e54a24985585757938c939f10f1945d71d
Gabriel Oliveira <gabolvr@google.com>
2022-06-30 14:42:13 +0000

    [M104][Signin] Use regex pattern in static title in profile customization test
    (cherry picked from commit ea8be594b52ae9ab37b8ada76c5ad08ee2a6f2f6)
    
    Fixed: 1336935
    Change-Id: I9bd13aade15879f2e173609de5a56c422d8abc5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3721016
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1017191}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734879
    Cr-Commit-Position: refs/branch-heads/5112@{#473}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- chrome/test/data/webui/signin/profile_customization_test.ts
+++ chrome/test/data/webui/signin/profile_customization_test.ts
@@ -25,7 +25,7 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
     const AVATAR_URL_2 = 'chrome://theme/IDR_PROFILE_AVATAR_2';
     const WELCOME_TEXT_1 = 'Welcome, Bob';
     const WELCOME_TEXT_2 = 'Hi, Elisa';
-    const STATIC_TITLE = 'Customize your Chromium profile';
+    const STATIC_TITLE_PATTERN = /^Customize your (Chromium|Chrome) profile$/g;
 
     setup(function() {
       loadTimeData.overrideValues({
@@ -99,9 +99,11 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
     test('ProfileInfo', function() {
       const header = app.shadowRoot!.querySelector<HTMLElement>('#header')!;
       // Check initial info.
-      assertEquals(
-          app.$.title.innerText,
-          inDialogDesign ? STATIC_TITLE : WELCOME_TEXT_1);
+      if (inDialogDesign) {
+        assertTrue(app.$.title.innerText.match(STATIC_TITLE_PATTERN) != null);
+      } else {
+        assertEquals(app.$.title.innerText, WELCOME_TEXT_1);
+      }
       assertEquals('rgb(0, 255, 0)', getComputedStyle(header).backgroundColor);
       checkImageUrl('#avatar', AVATAR_URL_1);
       assertFalse(isChildVisible(app, '#badge'));
@@ -113,9 +115,11 @@ import {TestProfileCustomizationBrowserProxy} from './test_profile_customization
         isManaged: true,
         welcomeTitle: WELCOME_TEXT_2,
       });
-      assertEquals(
-          app.$.title.innerText,
-          inDialogDesign ? STATIC_TITLE : WELCOME_TEXT_2);
+      if (inDialogDesign) {
+        assertTrue(app.$.title.innerText.match(STATIC_TITLE_PATTERN) != null);
+      } else {
+        assertEquals(app.$.title.innerText, WELCOME_TEXT_2);
+      }
       assertEquals(color2, getComputedStyle(header).backgroundColor);
       checkImageUrl('#avatar', AVATAR_URL_2);
       assertTrue(isChildVisible(app, '#badge'));
