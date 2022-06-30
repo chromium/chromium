8dd43b7cbf919ddb14c08cc4ca307ac7a373a7b2
Mohsen Izadi <mohsen@chromium.org>
2022-06-30 00:02:54 +0000

    Remove cluster telemetry repaint benchmark
    The benchmark is not used anymore.
    
    Bug: 1327374
    Change-Id: Ib56652f6130ec868f26615cc5080cbd6eb191744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732472
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Ravi Mistry <rmistry@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019400}

--- tools/perf/contrib/cluster_telemetry/ct_benchmarks_unittest.py
+++ tools/perf/contrib/cluster_telemetry/ct_benchmarks_unittest.py
@@ -10,7 +10,6 @@ import six
 from telemetry.page import shared_page_state
 
 from contrib.cluster_telemetry import rasterize_and_record_micro_ct
-from contrib.cluster_telemetry import repaint
 from contrib.cluster_telemetry import skpicture_printer
 
 
@@ -28,7 +27,6 @@ class CTBenchmarks(unittest.TestCase):
   def setUp(self):
     self.ct_benchmarks = [
         rasterize_and_record_micro_ct.RasterizeAndRecordMicroCT(),
-        repaint.RepaintCT(),
         skpicture_printer.SkpicturePrinterCT(),
     ]
     self.shared_page_state_class = shared_page_state.SharedMobilePageState
