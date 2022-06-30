a58f28b1451fb984dff487703e1943baaabe52df
Andrew Rayskiy <greengrape@google.com>
2022-06-30 13:25:59 +0000

    chrome.printingMetrics API: Fix failing unit tests.
    Bug: b/190146887
    Change-Id: I78fd4f2704436fc1fa53acd7ae66d15cf54ca347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3736905
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1019598}

--- chrome/browser/chromeos/extensions/printing_metrics/printing_metrics_api_unittest.cc
+++ chrome/browser/chromeos/extensions/printing_metrics/printing_metrics_api_unittest.cc
@@ -7,9 +7,11 @@
 #include "chrome/browser/ash/printing/history/mock_print_job_history_service.h"
 #include "chrome/browser/ash/printing/history/print_job_history_service_factory.h"
 #include "chrome/browser/ash/printing/history/print_job_info.pb.h"
+#include "chrome/browser/chromeos/extensions/printing_metrics/printing_metrics_service.h"
 #include "chrome/browser/extensions/api/printing/printing_api.h"
 #include "chrome/browser/extensions/extension_api_unittest.h"
 #include "chrome/common/extensions/api/printing_metrics.h"
+#include "content/public/browser/browser_context.h"
 #include "extensions/browser/api_test_utils.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
@@ -26,10 +28,19 @@ constexpr char kTitle2[] = "title2";
 
 constexpr int kPagesNumber = 3;
 
-// Creates a new MockPrintJobHistoryService for the given |context|.
+std::unique_ptr<KeyedService> BuildEventRouter(
+    content::BrowserContext* context) {
+  return std::make_unique<extensions::EventRouter>(context, nullptr);
+}
+
 std::unique_ptr<KeyedService> BuildPrintJobHistoryService(
     content::BrowserContext* context) {
   return std::make_unique<ash::MockPrintJobHistoryService>();
+}
+
+std::unique_ptr<KeyedService> BuildPrintingMetricsService(
+    content::BrowserContext* context) {
+  return std::make_unique<PrintingMetricsService>(context);
 }
 
 void ReturnNoPrintJobs(ash::PrintJobDatabase::GetPrintJobsCallback callback) {
@@ -79,9 +90,12 @@ class PrintingMetricsApiUnittest : public ExtensionApiUnittest {
             .Build();
     set_extension(extension);
 
+    EventRouterFactory::GetInstance()->SetTestingFactory(
+        profile(), base::BindRepeating(&BuildEventRouter));
     ash::PrintJobHistoryServiceFactory::GetInstance()->SetTestingFactory(
-        browser()->profile(),
-        base::BindRepeating(&BuildPrintJobHistoryService));
+        profile(), base::BindRepeating(&BuildPrintJobHistoryService));
+    PrintingMetricsService::GetFactoryInstance()->SetTestingFactory(
+        profile(), base::BindRepeating(&BuildPrintingMetricsService));
   }
 
  protected:
@@ -125,11 +139,19 @@ TEST_F(PrintingMetricsApiUnittest, GetPrintJobs_OnePrintJob) {
   ASSERT_EQ(1u, result->GetList().size());
   std::unique_ptr<api::printing_metrics::PrintJobInfo> print_job_info =
       api::printing_metrics::PrintJobInfo::FromValue(result->GetList()[0]);
-  EXPECT_TRUE(print_job_info);
-  EXPECT_EQ(kTitle1, print_job_info->title);
-  EXPECT_EQ(api::printing_metrics::PRINT_JOB_STATUS_FAILED,
-            print_job_info->status);
-  EXPECT_EQ(kPagesNumber, print_job_info->number_of_pages);
+
+  EXPECT_THAT(
+      print_job_info,
+      testing::AllOf(
+          testing::NotNull(),
+          testing::Pointee(testing::AllOf(
+              testing::Field(&api::printing_metrics::PrintJobInfo::title,
+                             kTitle1),
+              testing::Field(&api::printing_metrics::PrintJobInfo::status,
+                             api::printing_metrics::PRINT_JOB_STATUS_FAILED),
+              testing::Field(
+                  &api::printing_metrics::PrintJobInfo::number_of_pages,
+                  kPagesNumber)))));
 }
 
 // Test that calling |printingMetrics.getPrintJobs()| returns both mock print
