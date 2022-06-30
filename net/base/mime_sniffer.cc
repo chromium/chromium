7bf77b106e70a4c390bb37bc45e95c876beedc29
Tsuyoshi Horo <horo@chromium.org>
2022-06-30 17:26:16 +0000

    [/net] modernize-loop-convert mime_sniffer.cc
    This should cause no functional changes.
    
    Bug: 1334056
    Change-Id: Iae50f22b60c59efb11235c410a01482dce673f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3737489
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019713}

--- net/base/mime_sniffer.cc
+++ net/base/mime_sniffer.cc
@@ -828,8 +828,8 @@ bool LooksLikeBinary(base::StringPiece content) {
   // represents byte 0x1F.
   const uint32_t kBinaryBits =
       ~(1u << '\t' | 1u << '\n' | 1u << '\r' | 1u << '\f' | 1u << '\x1b');
-  for (size_t i = 0; i < content.length(); ++i) {
-    uint8_t byte = static_cast<uint8_t>(content[i]);
+  for (char c : content) {
+    uint8_t byte = static_cast<uint8_t>(c);
     if (byte < 0x20 && (kBinaryBits & (1u << byte)))
       return true;
   }
