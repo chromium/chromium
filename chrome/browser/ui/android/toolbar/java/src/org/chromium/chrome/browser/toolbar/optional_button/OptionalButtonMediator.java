b127271b16d0d749987192ae30b7c123b8827123
Salvador Guerrero <salg@google.com>
2022-07-01 22:28:24 +0000

    Create MVC classes for Optional Toolbar Button
    ToolbarPhone has an optional button that shows up between the omnibox
    and the tab switcher button. This button is mainly used to surface
    features on a user-by-user basis (a.k.a. Magic Toolbar), it contains a
    show/hide animation.
    
    The contextual page action (CPA) project aims to surface features based
    on the current website. With the limited real estate on the toolbar we
    decided to replace the Magic Toolbar action with a contextual action
    when loading compatible websites. We are aiming to experiment with two
    main variations, one that swaps the icons in place and another one that
    exposes the new action on an action chip which then colapses back into
    a regular icon.
    
    This CL adds a coordinator/mediator/viewbinder to control
    OptionalButtonView. This view was implemented on a previous CL
    (https://chromium-review.googlesource.com/c/chromium/src/+/3700608) and
    it will be integrated into ToolbarPhone in a follow-up CL (https://chromium-review.googlesource.com/c/chromium/src/+/3697050/)
    
    Figma spec:
    https://www.figma.com/proto/xDx5P5tjLf8CXD70BY6wqP/Contextual-Actions?page-id=334%3A6697&node-id=334%3A8441&viewport=307%2C48%2C0.15&scaling=min-zoom&starting-point-node-id=334%3A8308&show-proto-sidebar=1
    
    PRD:
    https://docs.google.com/document/d/10ApoEZwkUS5LsqDw1YjGtDOWzfXrukpNeuocJQI6xkI/edit?usp=sharing
    
    Animation technical details:
    https://docs.google.com/document/d/16IiK39BdCw3iCbQijT2BWE99rZ0r1ueE3TDtNLC4uRc/edit?usp=sharing
    
    Original CL in experimental branch:
    https://chromium-review.googlesource.com/c/experimental/chromium/src/+/3578413
    
    
    Bug: 1301633
    Change-Id: I04f3accb831098fb4273256a6d74584932b0c773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3698772
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1020264}

---
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/optional_button/OptionalButtonMediator.java
@@ -0,0 +1,67 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar.optional_button;
+
+import android.content.res.ColorStateList;
+import android.view.ViewGroup;
+
+import org.chromium.base.Callback;
+import org.chromium.base.supplier.BooleanSupplier;
+import org.chromium.chrome.browser.toolbar.ButtonData;
+import org.chromium.ui.modelutil.PropertyModel;
+
+class OptionalButtonMediator {
+    private final PropertyModel mModel;
+
+    public OptionalButtonMediator(PropertyModel model) {
+        mModel = model;
+    }
+
+    void updateButton(ButtonData buttonData) {
+        mModel.set(OptionalButtonProperties.BUTTON_DATA, buttonData);
+        if (buttonData != null) {
+            mModel.set(OptionalButtonProperties.IS_ENABLED, buttonData.isEnabled());
+        }
+    }
+
+    void setTransitionRoot(ViewGroup transitionRoot) {
+        mModel.set(OptionalButtonProperties.TRANSITION_ROOT, transitionRoot);
+    }
+
+    void setTransitionStartedCallback(Callback<Integer> transitionStartedCallback) {
+        mModel.set(OptionalButtonProperties.TRANSITION_STARTED_CALLBACK, transitionStartedCallback);
+    }
+
+    void setTransitionFinishedCallback(Callback<Integer> transitionFinishedCallback) {
+        mModel.set(
+                OptionalButtonProperties.TRANSITION_FINISHED_CALLBACK, transitionFinishedCallback);
+    }
+
+    void setIconForegroundColor(ColorStateList colorStateList) {
+        mModel.set(OptionalButtonProperties.ICON_TINT_LIST, colorStateList);
+    }
+
+    void setBackgroundColorFilter(int backgroundColor) {
+        mModel.set(OptionalButtonProperties.ICON_BACKGROUND_COLOR, backgroundColor);
+    }
+
+    public void setOnBeforeHideTransitionCallback(Runnable onBeforeHideTransitionCallback) {
+        mModel.set(OptionalButtonProperties.ON_BEFORE_HIDE_TRANSITION_CALLBACK,
+                onBeforeHideTransitionCallback);
+    }
+
+    public void setPaddingStart(int paddingStart) {
+        mModel.set(OptionalButtonProperties.PADDING_START, paddingStart);
+    }
+
+    public void cancelTransition() {
+        mModel.set(OptionalButtonProperties.TRANSITION_CANCELLATION_REQUESTED, true);
+    }
+
+    public void setIsAnimationAllowedPredicate(BooleanSupplier isAnimationAllowedPredicate) {
+        mModel.set(OptionalButtonProperties.IS_ANIMATION_ALLOWED_PREDICATE,
+                isAnimationAllowedPredicate);
+    }
+}
