65d70b06d88507a9eb92313bb9b61d4c901bfcb7
Zoraiz Naeem <zoraiznaeem@chromium.org>
2022-07-01 06:45:39 +0000

    Adds widget information into windows and views debug hierarchy logs.
    The additional widget information(name, visible, active, fullscreen bounds, layer id, etc.) and their position with respect to windows should help with identification of widgets creating a views.
    
    Bug: 1313613
    Change-Id: I9084afd68e45ef37f5675afa527ec27bcff223cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3725539
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019971}

--- ash/debug.cc
+++ ash/debug.cc
@@ -46,6 +46,9 @@ void PrintViewHierarchy(std::ostringstream* out) {
   views::Widget* widget = views::Widget::GetWidgetForNativeView(active_window);
   if (!widget)
     return;
+
+  *out << "Host widget:\n";
+  views::PrintWidgetInformation(*widget, /*detailed*/ true, out);
   views::PrintViewHierarchy(widget->GetRootView(), out);
 }
 
@@ -64,7 +67,8 @@ void PrintWindowHierarchy(const aura::Window* active_window,
   const gfx::Vector2dF& subpixel_position_offset =
       window->layer()->GetSubpixelOffset();
   *out << indent_str;
-  *out << name << " (" << window << ")"
+  *out << " [window]";
+  *out << " " << name << " (" << window << ")"
        << " type=" << window->GetType();
   int window_id = window->GetId();
   if (window_id != aura::Window::kInitialId)
@@ -102,6 +106,13 @@ void PrintWindowHierarchy(const aura::Window* active_window,
   if (pkg_name)
     *out << " pkg_name=" << *pkg_name;
   *out << '\n';
+
+  views::Widget* widget = views::Widget::GetWidgetForNativeView(window);
+  if (widget) {
+    *out << std::string(indent + 3, ' ');
+    *out << " [widget]";
+    views::PrintWidgetInformation(*widget, /*detailed*/ false, out);
+  }
 
   for (aura::Window* child : window->children()) {
     PrintWindowHierarchy(active_window, focused_window, capture_window, child,
