d2cdaf5f2fd6b23de6978dc671671025790b229b
Vincent Boisselle <vincb@google.com>
2022-07-04 14:08:29 +0000

    [ios]🏢 Make vincb owner of policy UI code
    Bug: 1331300
    Change-Id: I8a8409458affe5d560c7fb46214f8cf1d611bc7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3686558
    Reviewed-by: edchin <edchin@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020567}

--- ios/chrome/browser/ui/policy/signin_policy_scene_agent.h
+++ ios/chrome/browser/ui/policy/signin_policy_scene_agent.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_MAIN_SIGNIN_POLICY_SCENE_AGENT_H_
-#define IOS_CHROME_BROWSER_UI_MAIN_SIGNIN_POLICY_SCENE_AGENT_H_
+#ifndef IOS_CHROME_BROWSER_UI_POLICY_SIGNIN_POLICY_SCENE_AGENT_H_
+#define IOS_CHROME_BROWSER_UI_POLICY_SIGNIN_POLICY_SCENE_AGENT_H_
 
 #import "ios/chrome/browser/ui/main/observing_scene_state_agent.h"
 
@@ -38,4 +38,4 @@
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_MAIN_SIGNIN_POLICY_SCENE_AGENT_H_
+#endif  // IOS_CHROME_BROWSER_UI_POLICY_SIGNIN_POLICY_SCENE_AGENT_H_
