8cca6057a99dd578a5f9790b02604726c9b34ad5
Peter Conn <peconn@chromium.org>
2022-07-05 10:55:53 +0000

    ⚕️ Revive RunningInChromeTest#showsNewRunningInChrome
    According to the linked bug, Test Reviver thinks this test is no longer
    flaky and can be re-enabled.
    
    Bug: 1333213
    Change-Id: I190a2401a3b71ce28fc93a1b03407c5444358e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3745483
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020803}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/RunningInChromeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/RunningInChromeTest.java
@@ -34,7 +34,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeApplicationImpl;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
@@ -131,7 +130,6 @@ public class RunningInChromeTest {
 
     @Test
     @MediumTest
-    @FlakyTest(message = "https://crbug.com/1164424")
     public void showsNewRunningInChrome() throws TimeoutException {
         launch(createTrustedWebActivityIntent(mTestPage));
 
