244ebc3e857b50b9d9f449c7573443b6acd40e24
Koji Ishii <kojii@chromium.org>
2022-07-05 08:08:37 +0000

    Reland "[anchor-position] Uncull inline boxes if `anchor-name`"
    This is a reland of commit e2a82a9e5582a2f976275470eff328b173f9a5f0
    
    This was reverted for a performance regression crbug.com/1341658,
    but the numbers recovered without the revert.
    https://chromeperf.appspot.com/group_report?group_id=e341621e-4327-40ea-8715-efae26dc7c8b
    
    Also a pinpoint run shows this CL is not related.
    https://pinpoint-dot-chromeperf.appspot.com/job/1199521aaa0000
    
    Original change's description:
    > [anchor-position] Uncull inline boxes if `anchor-name`
    >
    > This patch stops culling inline boxes if they have the
    > `anchor-name` property.
    >
    > This patch also adds inline formatting context test cases for
    > |NGAnchorQuery|.
    >
    > This patch has no behavior changes because changes are under a
    > runtime flag.
    >
    > Bug: 1309178
    > Change-Id: I1d49f1f8e4c92625bcd0265cde3ee06c8530327c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3734709
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1020482}
    
    Bug: 1309178, 1341658
    Change-Id: I1ab2a45d5488b21da1fd4f21047dbe1ff10a96f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3744321
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020744}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -410,6 +410,9 @@ bool LayoutInline::ComputeInitialShouldCreateBoxFragment(
       style.MayHaveMargin())
     return true;
 
+  if (!style.AnchorName().IsNull())
+    return true;
+
   return ComputeIsAbsoluteContainer(&style) ||
          NGOutlineUtils::HasPaintedOutline(style, GetNode()) ||
          CanBeHitTestTargetPseudoNodeStyle(style);
