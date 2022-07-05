21272820bea927f577746b8b18853f3d541b7d48
Michael Martis <martis@chromium.org>
2022-07-05 08:14:00 +0000

    [launcher-lacros] Exposed utils to ash-chrome.
    In order to transmit Omnibox results from lacros-chrome, our result
    objects will eventually be thin wrappers around the
    crosapi::SearchResult type.
    
    However that means (until the lacros project is complete) we will need
    Ash to perform the conversion:
    AutocompleteMatch -> crosapi::SearchResult -> result object.
    
    This CL exposes to Ash the utils necessary for that conversion.
    
    Details at go/launcher-lacros-refactor.
    
    Bug: 1228587
    Change-Id: Ieac65ae20c06bb7792e73bd2c2d02b5858425b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3713204
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1020754}

--- chrome/browser/chromeos/launcher_search/search_util.h
+++ chrome/browser/chromeos/launcher_search/search_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
-#define CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
+#ifndef CHROME_BROWSER_CHROMEOS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
+#define CHROME_BROWSER_CHROMEOS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
 
 #include "chromeos/crosapi/mojom/launcher_search.mojom.h"
 #include "components/omnibox/browser/autocomplete_input.h"
@@ -19,9 +19,6 @@ class BookmarkModel;
 }  // namespace bookmarks
 
 namespace crosapi {
-
-// TODO(crbug.com/1228587): This code will be shared with ash. Move this file
-// into an appropriate location.
 
 // Returns a bitmask of the AutocompleteProvider types to be used by Launcher
 // search.
@@ -42,4 +39,4 @@ mojom::SearchResultPtr CreateResult(AutocompleteMatch& match,
 
 }  // namespace crosapi
 
-#endif  // CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
+#endif  // CHROME_BROWSER_CHROMEOS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
