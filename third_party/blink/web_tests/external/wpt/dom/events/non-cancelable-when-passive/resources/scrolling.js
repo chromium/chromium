2d7234a4e7d0aee705f78359bc7d95211550db6a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-07-05 17:54:17 +0000

    Import wpt@95d4bd97e123a54e8c7171b639d3e4fab89e68be
    Using wpt-import in Chromium 93cb5b36cf91d26f4334db229959e3137f67bba3.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I7b642cd142c10d58e9741dfaf5ff8b05305bd821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746877
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1020937}

---
+++ third_party/blink/web_tests/external/wpt/dom/events/non-cancelable-when-passive/resources/scrolling.js
@@ -0,0 +1,22 @@
+function runTest({target, eventName, passive, expectCancelable}) {
+  let cancelable = null;
+  let arrived = false;
+  target.addEventListener(eventName, function (event) {
+    cancelable = event.cancelable;
+    arrived = true;
+  }, {passive});
+
+  promise_test(async (t) => {
+    t.add_cleanup(() => {
+      document.querySelector('.remove-on-cleanup')?.remove();
+    });
+    const pos_x = Math.floor(window.innerWidth / 2);
+    const pos_y = Math.floor(window.innerHeight / 2);
+    const delta_x = 0;
+    const delta_y = 100;
+    await new test_driver.Actions()
+      .scroll(pos_x, pos_y, delta_x, delta_y).send();
+    await waitFor(() => arrived);
+    assert_equals(cancelable, expectCancelable);
+  });
+}
