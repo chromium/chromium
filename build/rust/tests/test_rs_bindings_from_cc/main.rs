e4760c76e16d620a885c4650e5c761f20cc10e30
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-06 16:46:05 +0000

    [rust] rs_bindings_from_cc.gni: Support for *multiple* `public_headers`.
    After this CL the `rs_bindings_from_cc` target template accepts a *list*
    of `public_headers` (rather than just a single *string*).
    
    Bug: 1329611
    Change-Id: I1d927a405690d53ce4eea95c63d79bba452df3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3741423
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021224}

--- build/rust/tests/test_rs_bindings_from_cc/main.rs
+++ build/rust/tests/test_rs_bindings_from_cc/main.rs
@@ -4,13 +4,15 @@
 
 fn main() {
     println!("Hello world!");
-    println!("MultiplyViaCc(100,42) = {}", ::self_contained_header_rs_api::MultiplyViaCc(100, 42));
+    println!("AddViaCc(100,42) = {}", ::self_contained_target_rs_api::AddViaCc(100, 42));
+    println!("MultiplyViaCc(100,42) = {}", ::self_contained_target_rs_api::MultiplyViaCc(100, 42));
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_call_via_crubit() {
-        assert_eq!(100 * 42, ::self_contained_header_rs_api::MultiplyViaCc(100, 42));
+    fn test_self_contained_target() {
+        assert_eq!(100 + 42, ::self_contained_target_rs_api::AddViaCc(100, 42));
+        assert_eq!(100 * 42, ::self_contained_target_rs_api::MultiplyViaCc(100, 42));
     }
 }
