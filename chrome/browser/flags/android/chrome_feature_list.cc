9236013e6d3d4469eebd2be8188bde0a0a4d8de3
Wenyu Fu <wenyufu@chromium.org>
2022-07-06 18:28:19 +0000

    Revert "[RCTS] Default enable RCTS for metrics collection"
    This reverts commit 42a0bbdba3b70622dce4c11a18d7d4f0d13ee1b9.
    
    Reason for revert: crashing CCT on closure - crbug.com/1340878
    
    Original change's description:
    > [RCTS] Default enable RCTS for metrics collection
    >
    > Bug: 1322240
    > Change-Id: Ia44e97bc847173b9be3318687a7bc6571efd6f6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3733411
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1018878}
    
    Bug: 1322240, 1340878
    Change-Id: I8a511fbce633ee5991488daa8de9fd75225250b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749316
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021271}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -533,7 +533,7 @@ const base::Feature kCCTResourcePrefetch{"CCTResourcePrefetch",
                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kCCTRetainingState{"CCTRetainingState",
-                                       base::FEATURE_ENABLED_BY_DEFAULT};
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCCTToolbarCustomizations{"CCTToolbarCustomizations",
                                               base::FEATURE_ENABLED_BY_DEFAULT};
