0ba6ae3d447de7bc599a191f6792a4e6676f10a3
Shrek Shao <shrekshao@google.com>
2022-07-07 23:12:48 +0000

    Fix dawn write handle data update OOB check
    Bug: chromium:1340654
    Change-Id: I9d87cb868eccc380f707ab6c3c6bdc26c386fbfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3738662
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021911}

--- gpu/command_buffer/service/dawn_service_memory_transfer_service.cc
+++ gpu/command_buffer/service/dawn_service_memory_transfer_service.cc
@@ -30,7 +30,8 @@ class ReadHandleImpl
                            size_t offset,
                            size_t size,
                            void* serializePointer) override {
-    DCHECK_LE(size + offset, size_);
+    DCHECK_LE(offset, size_);
+    DCHECK_LE(size, size_ - offset);
     // Copy the data into the shared memory allocation.
     // In the case of buffer mapping, this is the mapped GPU memory which we
     // copy into client-visible shared memory.
@@ -57,9 +58,15 @@ class WriteHandleImpl
                              size_t size) override {
     // Nothing is serialized because we're using shared memory.
     DCHECK_EQ(deserialize_size, 0u);
-    DCHECK_LE(size + offset, size_);
     DCHECK(mTargetData);
     DCHECK(ptr_);
+
+    if (offset > mDataLength || size > mDataLength - offset) {
+      return false;
+    }
+    if (offset > size_ || size > size_ - offset) {
+      return false;
+    }
 
     // Copy from shared memory into the target buffer.
     // mTargetData will always be the starting address
