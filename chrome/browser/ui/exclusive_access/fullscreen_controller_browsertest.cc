ce703cd19d1958cdb20d76a8fcf6ff0b2455f28f
Mike Wasserman <msw@chromium.org>
2022-07-07 20:19:47 +0000

    Sheriff: Disable flaky KeyboardLock test on Lacros
    Bug: 1340114
    Change-Id: Ic825081649a0fb0eb9dc526eaf15835e25f79214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3749642
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Owners-Override: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1021829}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_browsertest.cc
@@ -147,8 +147,16 @@ IN_PROC_BROWSER_TEST_F(FullscreenControllerTest,
                   ->IsKeyboardLockActive());
 }
 
+// Disabled for flaky SEGFAULTs on Lacros: crbug.com/1340114
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_KeyboardLockNotLockedInExtensionFullscreenMode \
+  DISABLED_KeyboardLockNotLockedInExtensionFullscreenMode
+#else
+#define MAYBE_KeyboardLockNotLockedInExtensionFullscreenMode \
+  KeyboardLockNotLockedInExtensionFullscreenMode
+#endif  // IS_CHROMEOS_LACROS
 IN_PROC_BROWSER_TEST_F(FullscreenControllerTest,
-                       KeyboardLockNotLockedInExtensionFullscreenMode) {
+                       MAYBE_KeyboardLockNotLockedInExtensionFullscreenMode) {
   EnterExtensionInitiatedFullscreen();
   ASSERT_TRUE(RequestKeyboardLock(/*esc_key_locked=*/true));
   ASSERT_FALSE(GetExclusiveAccessManager()
