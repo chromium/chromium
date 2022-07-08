866590fb3aed715eb57a0826d6d2974dd67346cc
Mitsuru Oshima <oshima@chromium.org>
2022-07-08 16:47:38 +0000

    Do not call StartDrag twice.
    This should be called after the window is shown.
    The window is shown when the buffer is attached, this can be
    called twice, when created and when shown.
    
    I also added DCHECK so that we can catch if there is another path
    that leads to the duplicated call.
    
    Bug: 1336706
    Change-Id: I418ec0c56b7f04da3ac6eacc15a40202b114a88f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3748832
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022190}

--- components/exo/extended_drag_source.cc
+++ components/exo/extended_drag_source.cc
@@ -223,9 +223,10 @@ void ExtendedDragSource::OnToplevelWindowDragStarted(
   if (drag_source_window_)
     drag_source_window_->AddObserver(this);
   MaybeLockCursor();
-
-  if (dragged_window_holder_ && dragged_window_holder_->toplevel_window())
+  if (dragged_window_holder_ && dragged_window_holder_->toplevel_window() &&
+      dragged_window_holder_->toplevel_window()->IsVisible()) {
     StartDrag(dragged_window_holder_->toplevel_window());
+  }
 }
 
 DragOperation ExtendedDragSource::OnToplevelWindowDragDropped() {
@@ -292,6 +293,7 @@ void ExtendedDragSource::UnlockCursor() {
 }
 
 void ExtendedDragSource::StartDrag(aura::Window* toplevel) {
+  DCHECK(!event_blocker_);
   // Ensure |toplevel| window does skip events while it's being dragged.
   event_blocker_ =
       std::make_unique<aura::ScopedWindowEventTargetingBlocker>(toplevel);
