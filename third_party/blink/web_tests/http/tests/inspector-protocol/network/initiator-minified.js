cd8f95575870a945273fb03faecbc01d961c9615
Danil Somsikov <dsv@chromium.org>
2022-07-08 12:41:37 +0000

    Try fixing initiator-minified.js test flakiness by disabling cache.
    Bug: 1327867
    Change-Id: Ibf2b7e2ba5e7c047a0e21dd28f1c54ff7763e1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751384
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022111}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/initiator-minified.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/initiator-minified.js
@@ -1,7 +1,7 @@
 (async function(testRunner) {
-  const {page, session, dp} = await testRunner.startBlank(
-    `Tests that the initiator position is correct even when that initiator is minified.`);
-  testRunner.startDumpingProtocolMessages();
+  const {page, session, dp} = await testRunner.startURL(
+      'resources/minified.html',
+      `Tests that the initiator position is correct even when that initiator is minified.`);
   window.onerror = (msg) => testRunner.log('onerror: ' + msg);
   window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
   let errorForLog = new Error();
@@ -9,7 +9,7 @@
 
   dp.Network.enable();
   dp.Page.enable();
-  dp.Page.navigate({url: testRunner.url('resources/minified.html')});
+  dp.Page.reload({ignoreCache: true});
 
   let requests = [];
   dp.Network.onLoadingFailed(e => testRunnler.log(JSON.stringify(e)));
