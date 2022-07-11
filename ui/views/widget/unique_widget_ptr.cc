6ebe1e131e66de3c90f1ad0e42d8826d9902aec5
Keren Zhu <kerenzhu@chromium.org>
2022-07-11 17:59:28 +0000

    Allow constructing UniqueWidgetPtr from a subclass instance of Widget
    Bug: 1218885
    Change-Id: Ib25060822914d3b0edbba9bed5705f86921f94b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750238
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022770}

--- ui/views/widget/unique_widget_ptr.cc
+++ ui/views/widget/unique_widget_ptr.cc
@@ -6,67 +6,13 @@
 
 #include <utility>
 
-#include "base/scoped_observation.h"
-#include "ui/views/view.h"
-#include "ui/views/widget/widget.h"
-#include "ui/views/widget/widget_observer.h"
-
 namespace views {
-
-namespace {
-
-struct WidgetAutoCloser {
-  void operator()(Widget* widget) {
-    widget->CloseWithReason(Widget::ClosedReason::kUnspecified);
-  }
-};
-
-using WidgetAutoClosePtr = std::unique_ptr<Widget, WidgetAutoCloser>;
-
-}  // namespace
-
-class UniqueWidgetPtr::UniqueWidgetPtrImpl : public WidgetObserver {
- public:
-  UniqueWidgetPtrImpl() = default;
-  // This class acts like unique_ptr<Widget>, so this constructor is
-  // deliberately implicit.
-  UniqueWidgetPtrImpl(std::unique_ptr<Widget> widget)  // NOLINT
-      : widget_closer_(widget.release()) {
-    widget_observation_.Observe(widget_closer_.get());
-  }
-
-  UniqueWidgetPtrImpl(const UniqueWidgetPtrImpl&) = delete;
-
-  UniqueWidgetPtrImpl& operator=(const UniqueWidgetPtrImpl&) = delete;
-
-  ~UniqueWidgetPtrImpl() override = default;
-
-  Widget* Get() const { return widget_closer_.get(); }
-
-  void Reset() {
-    if (!widget_closer_)
-      return;
-    widget_observation_.Reset();
-    widget_closer_.reset();
-  }
-
-  // WidgetObserver overrides.
-  void OnWidgetDestroying(Widget* widget) override {
-    DCHECK_EQ(widget, widget_closer_.get());
-    widget_observation_.Reset();
-    widget_closer_.release();
-  }
-
- private:
-  base::ScopedObservation<Widget, WidgetObserver> widget_observation_{this};
-  WidgetAutoClosePtr widget_closer_;
-};
 
 UniqueWidgetPtr::UniqueWidgetPtr() = default;
 
-UniqueWidgetPtr::UniqueWidgetPtr(std::unique_ptr<Widget> widget)
-    : unique_widget_ptr_impl_(
-          std::make_unique<UniqueWidgetPtrImpl>(std::move(widget))) {}
+UniqueWidgetPtr::UniqueWidgetPtr(std::unique_ptr<Widget> widget) {
+  Init(std::move(widget));
+}
 
 UniqueWidgetPtr::UniqueWidgetPtr(UniqueWidgetPtr&& other) = default;
 
@@ -92,6 +38,40 @@ void UniqueWidgetPtr::reset() {
 
 Widget* UniqueWidgetPtr::get() const {
   return unique_widget_ptr_impl_ ? unique_widget_ptr_impl_->Get() : nullptr;
+}
+
+void UniqueWidgetPtr::Init(std::unique_ptr<Widget> widget) {
+  unique_widget_ptr_impl_ = std::make_unique<Impl>(std::move(widget));
+}
+
+void UniqueWidgetPtr::Impl::WidgetAutoCloser::operator()(Widget* widget) {
+  widget->CloseWithReason(Widget::ClosedReason::kUnspecified);
+}
+
+UniqueWidgetPtr::Impl::Impl() = default;
+
+UniqueWidgetPtr::Impl::Impl(std::unique_ptr<Widget> widget)
+    : widget_closer_(widget.release()) {
+  widget_observation_.Observe(widget_closer_.get());
+}
+
+UniqueWidgetPtr::Impl::~Impl() = default;
+
+Widget* UniqueWidgetPtr::Impl::Get() const {
+  return widget_closer_.get();
+}
+
+void UniqueWidgetPtr::Impl::Reset() {
+  if (!widget_closer_)
+    return;
+  widget_observation_.Reset();
+  widget_closer_.reset();
+}
+
+void UniqueWidgetPtr::Impl::OnWidgetDestroying(Widget* widget) {
+  DCHECK_EQ(widget, widget_closer_.get());
+  widget_observation_.Reset();
+  widget_closer_.release();
 }
 
 }  // namespace views
--- ui/views/widget/unique_widget_ptr.h
+++ ui/views/widget/unique_widget_ptr.h
@@ -7,21 +7,35 @@
 
 #include <memory>
 
+#include "base/scoped_observation.h"
 #include "ui/views/views_export.h"
+#include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace views {
 
-class Widget;
-
-// Ensures the Widget is properly closed when this special
-// auto pointer goes out of scope.
-
+// A weak owning pointer to a Widget.
+// This smart pointer acts like a unique_ptr: it ensures the Widget is
+// properly closed when it goes out of scope.
+// It also acts like a WeakPtr: the Widget may be deleted by its native
+// widget and `get()` becomes nullptr when this happens.
+// Caller may check the validity of this pointer before dereferencing it if
+// the widget lifetime is in doubt.
 class VIEWS_EXPORT UniqueWidgetPtr {
  public:
   UniqueWidgetPtr();
   // This class acts like a std::unique_ptr<Widget>, so this constructor is
   // deliberately implicit.
   UniqueWidgetPtr(std::unique_ptr<Widget> widget);  // NOLINT
+
+  // Construct from a subclass instance of Widget. Note that ~Widget() is
+  // virtual, so the downcasting is safe. This constructor is deliberately
+  // implicit.
+  template <class U>
+  UniqueWidgetPtr(std::unique_ptr<U> widget) {  // NOLINT
+    Init(std::unique_ptr<Widget>(widget.release()));
+  }
+
   UniqueWidgetPtr(UniqueWidgetPtr&&);
   UniqueWidgetPtr& operator=(UniqueWidgetPtr&&);
   ~UniqueWidgetPtr();
@@ -33,9 +47,31 @@ class VIEWS_EXPORT UniqueWidgetPtr {
   Widget* get() const;
 
  private:
-  class UniqueWidgetPtrImpl;
+  class Impl : public WidgetObserver {
+   public:
+    Impl();
+    explicit Impl(std::unique_ptr<Widget> widget);
+    Impl(const Impl&) = delete;
+    Impl& operator=(const Impl&) = delete;
+    ~Impl() override;
 
-  std::unique_ptr<UniqueWidgetPtrImpl> unique_widget_ptr_impl_;
+    Widget* Get() const;
+    void Reset();
+
+    // WidgetObserver overrides.
+    void OnWidgetDestroying(Widget* widget) override;
+
+   private:
+    struct WidgetAutoCloser {
+      void operator()(Widget* widget);
+    };
+
+    base::ScopedObservation<Widget, WidgetObserver> widget_observation_{this};
+    std::unique_ptr<Widget, WidgetAutoCloser> widget_closer_;
+  };
+  void Init(std::unique_ptr<Widget> widget);
+
+  std::unique_ptr<Impl> unique_widget_ptr_impl_;
 };
 
 }  // namespace views
