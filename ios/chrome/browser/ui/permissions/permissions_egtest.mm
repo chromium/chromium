90560dd36344523cc0dde4e5b791eb0bc3ca11ef
Rohit Rao <rohitrao@chromium.org>
2022-07-11 17:04:18 +0000

    [ios] Disables testPermissionsAfterTabSwitch on iOS 16.
    This test consistently fails on iOS 16. Disabling while we investigate
    whether this is an OS bug.
    
    BUG=1343380
    
    Change-Id: Ia245af658c805e37e623cc891319520f7f1000ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755447
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022738}

--- ios/chrome/browser/ui/permissions/permissions_egtest.mm
+++ ios/chrome/browser/ui/permissions/permissions_egtest.mm
@@ -16,6 +16,7 @@
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/disabled_test_macros.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ios/web/common/features.h"
 #include "ios/web/public/permissions/permissions.h"
@@ -407,6 +408,10 @@ void TapDoneButtonOnInfobarModal() {
 // Tests that permissions stay the same after user switches to another tab then
 // comes back.
 - (void)testPermissionsAfterTabSwitch {
+  if (@available(iOS 16.0, *)) {
+    EARL_GREY_TEST_DISABLED(@"Fails on iOS 16");
+  }
+
   if (@available(iOS 15.0, *)) {
     GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
     // Opens a page that requests both camera and microphone permissions, and
