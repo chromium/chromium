e3da253d6a7b3c0cfcbd14cdf21e6bc647cab581
Hao Dong <spdonghao@chromium.org>
2022-07-11 21:25:09 +0000

    Revert "tabmodel: disable UndoTabModelUnitTest"
    This reverts commit 2925fd22b7a2f428d435ce47c8c79e0cb93459b0.
    
    Reason for revert: fix CL http://crrev/c/3751630 has landed
    
    Original change's description:
    > tabmodel: disable UndoTabModelUnitTest
    >
    > Disable UndoTabModelUnitTest as it is failing on multiple builders.
    > Reverting the offending change is possible, but it seems we prefer to
    > fix the test instead.
    >
    > Bug: 1343385
    > Change-Id: I4bdc1a2235082c973a5fecbd44e83ea780a43caa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755091
    > Owners-Override: Egor Pasko <pasko@chromium.org>
    > Reviewed-by: Hao Dong <spdonghao@chromium.org>
    > Commit-Queue: Hao Dong <spdonghao@chromium.org>
    > Commit-Queue: Egor Pasko <pasko@chromium.org>
    > Auto-Submit: Egor Pasko <pasko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1022728}
    
    Bug: 1343385
    Change-Id: I470a9145d1f9c5aa65fedbb2db7062d46f89db74
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755646
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1022898}

--- chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/UndoTabModelUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/UndoTabModelUnitTest.java
@@ -22,7 +22,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.flags.ActivityType;
@@ -45,7 +44,6 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
-@DisabledTest(message = "crbug.com/1343385")
 public class UndoTabModelUnitTest {
     private static final long FAKE_NATIVE_ADDRESS = 123L;
     private static final Tab[] sEmptyList = new Tab[] {};
