4fb23d43b178748d2224a7fa8748e4fce5e2653b
Felipe Andrade <fsandrade@google.com>
2022-07-12 11:14:52 +0000

    Tentatively re-enable ActiveDirectoryJoinTest.* tests
    Tentatively re-enable the following tests:
    ActiveDirectoryJoinTest.TestActiveDirectoryEnrollment_UIErrors
    ActiveDirectoryJoinTest.TestActiveDirectoryEnrollment_Streamline
    
    Looking at past build runs, it seems like some ActiveDirectoryJoinTest*
    and ActiveDirectoryLoginTest* tests were failing with timeouts around
    the time the associated bugs were reported. However, they're no longer
    flaky, according to the latest builder runs:
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg
    
    Bug: 1316567, 1318903
    Change-Id: I01983dcdb34712d6d4e7787063c986de4c5e3f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756872
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023155}

--- chrome/browser/ash/login/enterprise_enrollment_browsertest.cc
+++ chrome/browser/ash/login/enterprise_enrollment_browsertest.cc
@@ -573,16 +573,8 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryJoinTest,
 // Directory domain join screen. Verifies the domain join screen is displayed.
 // Submits Active Directory different incorrect credentials. Verifies that the
 // correct error is displayed.
-// TODO(crbug.com/1318903): Re-enable this test on linux-chromeos-dbg.
-#if !defined(NDEBUG)
-#define MAYBE_TestActiveDirectoryEnrollment_UIErrors \
-  DISABLED_TestActiveDirectoryEnrollment_UIErrors
-#else
-#define MAYBE_TestActiveDirectoryEnrollment_UIErrors \
-  TestActiveDirectoryEnrollment_UIErrors
-#endif
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryJoinTest,
-                       MAYBE_TestActiveDirectoryEnrollment_UIErrors) {
+                       TestActiveDirectoryEnrollment_UIErrors) {
   ShowEnrollmentScreen();
   enrollment_helper_.SetupActiveDirectoryJoin(
       enrollment_screen(), kAdUserDomain, std::string(), kDMToken);
@@ -648,10 +640,8 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryJoinTest,
 
 // Check that configuration for the streamline Active Directory domain join
 // propagates correctly to the Domain Join UI.
-//
-// TODO(crbug.com/1316567): Re-enable this test.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryJoinTest,
-                       DISABLED_TestActiveDirectoryEnrollment_Streamline) {
+                       TestActiveDirectoryEnrollment_Streamline) {
   ShowEnrollmentScreen();
   std::string binary_config;
   EXPECT_TRUE(base::Base64Decode(kAdDomainJoinEncryptedConfig, &binary_config));
