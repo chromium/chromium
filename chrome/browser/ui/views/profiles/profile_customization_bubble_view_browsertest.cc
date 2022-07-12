905fbfddc1a63c39db4b5d5af4ab907286d14d51
Stephen McGruer <smcgruer@chromium.org>
2022-07-12 14:56:27 +0000

    [Sheriff] Disable flaky ProfileCustomizationDialogBrowserTest.IPH on Linux
    Bug: 1343668
    Change-Id: Ie59ffffdfa5979e8dd32ef72d0080b48350d90e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758243
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Owners-Override: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023207}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_view_browsertest.cc
@@ -101,7 +101,13 @@ IN_PROC_BROWSER_TEST_F(ProfileCustomizationBubbleBrowserTest,
   ShowAndVerifyUi();
 }
 
-IN_PROC_BROWSER_TEST_F(ProfileCustomizationBubbleBrowserTest, IPH) {
+// TODO(crbug.com/1343668): Flaky on Linux
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_IPH DISABLED_IPH
+#else
+#define MAYBE_IPH IPH
+#endif
+IN_PROC_BROWSER_TEST_F(ProfileCustomizationBubbleBrowserTest, MAYBE_IPH) {
   AvatarToolbarButton::SetIPHMinDelayAfterCreationForTesting(base::Seconds(0));
   auto lock = BrowserFeaturePromoController::BlockActiveWindowCheckForTesting();
   // Create the customization bubble, owned by the view hierarchy.
