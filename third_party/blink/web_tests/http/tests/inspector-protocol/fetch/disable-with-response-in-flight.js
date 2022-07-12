67211538364e7359d46733fa464f81449f546c11
Andrey Kosyakov <caseq@chromium.org>
2022-07-12 17:58:01 +0000

    Re-implement and deflake request-response-interception-disable-between.js as fetch test
    Change-Id: I3fc38a21473f1ef8e6b96b880e77a0bb800f4a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3754720
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023300}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/disable-with-response-in-flight.js
@@ -0,0 +1,27 @@
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      `Tests that requests are completed if interception is disabled between request and response.`);
+
+  const bp = testRunner.browserP();
+  // Intercept at browser level too, so we can delay response to
+  // after renderer fetch is disabled.
+  await bp.Fetch.enable({patterns: [{requestStage: 'Response'}]});
+  await dp.Fetch.enable({patterns: [{requestStage: 'Request'}, {requestStage: 'Response'}]});
+  const contentPromise = session.evaluateAsync(`
+      fetch('/devtools/network/resources/resource.php?size=10')
+          .then(response => response.text())
+  `);
+  const requestEvent = (await dp.Fetch.onceRequestPaused()).params;
+  testRunner.log('Request intercepted: ' + requestEvent.request.url.split('/').pop());
+  testRunner.log('Continuing request');
+  dp.Fetch.continueRequest({requestId: requestEvent.requestId});
+  const responseEvent = (await bp.Fetch.onceRequestPaused()).params;
+  // Now the original interception still waits for response here, disable it.
+  testRunner.log('Disabling request interception')
+  await dp.Fetch.disable();
+  bp.Fetch.continueRequest({requestId: responseEvent.requestId});
+
+  testRunner.log('Body:\n' + await contentPromise);
+
+  testRunner.completeTest();
+})
