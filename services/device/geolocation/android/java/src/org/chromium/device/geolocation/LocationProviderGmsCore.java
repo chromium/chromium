ff318c8d8f3a26bf4370d8ec653814cf2d9bc427
Benoit Lize <lizeb@chromium.org>
2022-07-12 13:31:24 +0000

    Revert "geolocation: Migrate to FusedLocationProviderClient"
    This reverts commit 7c307338388330e918bf46685ff15724c519a1bb.
    
    Reason for revert: Suspected CL for crbug.com/1343751
    
    Original change's description:
    > geolocation: Migrate to FusedLocationProviderClient
    >
    > The FusedLocationProviderApi interface is deprecated and in particular
    > the getLastLocation() method should not be called from the UI thread
    > because it performs a synchronous IPC. The FusedLocationProviderClient
    > version of this method is properly asynchronous.
    >
    > Binary-Size: Switching to undeprecated GMSCore API (unavoidable).
    > Bug: 1341172
    > Change-Id: I36ca3b30e20a50f70f27178ebbc79bbb9e4e9860
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3750204
    > Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1022226}
    
    Bug: 1341172, 1343751
    Change-Id: I0b77b6f1231fd2ea75439cf86e19fe0a446a7b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757703
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023183}

--- services/device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderGmsCore.java
+++ services/device/geolocation/android/java/src/org/chromium/device/geolocation/LocationProviderGmsCore.java
@@ -7,15 +7,21 @@ package org.chromium.device.geolocation;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
+import org.chromium.components.location.LocationUtils;
 import org.chromium.gms.ChromiumPlayServicesAvailability;
 
 /**
@@ -23,17 +29,19 @@ import org.chromium.gms.ChromiumPlayServicesAvailability;
  *
  * https://developers.google.com/android/reference/com/google/android/gms/location/package-summary
  */
-public class LocationProviderGmsCore implements LocationProvider {
+public class LocationProviderGmsCore implements ConnectionCallbacks, OnConnectionFailedListener,
+                                                LocationListener, LocationProvider {
     private static final String TAG = "LocationProvider";
 
     // Values for the LocationRequest's setInterval for normal and high accuracy, respectively.
     private static final long UPDATE_INTERVAL_MS = 1000;
     private static final long UPDATE_INTERVAL_FAST_MS = 500;
 
-    private final Context mContext;
-    private final FusedLocationProviderClient mClient;
+    private final GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderApi mLocationProviderApi = LocationServices.FusedLocationApi;
 
-    private LocationCallback mLocationCallback;
+    private boolean mEnablehighAccuracy;
+    private LocationRequest mLocationRequest;
 
     public static boolean isGooglePlayServicesAvailable(Context context) {
         return ChromiumPlayServicesAvailability.isGooglePlayServicesAvailable(context);
@@ -41,93 +49,117 @@ public class LocationProviderGmsCore implements LocationProvider {
 
     LocationProviderGmsCore(Context context) {
         Log.i(TAG, "Google Play Services");
-        mContext = context;
-        mClient = LocationServices.getFusedLocationProviderClient(context);
-        assert mClient != null;
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                                   .addApi(LocationServices.API)
+                                   .addConnectionCallbacks(this)
+                                   .addOnConnectionFailedListener(this)
+                                   .build();
+        assert mGoogleApiClient != null;
     }
 
-    LocationProviderGmsCore(Context context, FusedLocationProviderClient client) {
-        mContext = context;
-        mClient = client;
+    LocationProviderGmsCore(GoogleApiClient client, FusedLocationProviderApi locationApi) {
+        mGoogleApiClient = client;
+        mLocationProviderApi = locationApi;
     }
 
-    // LocationProvider implementation
+    // ConnectionCallbacks implementation
     @Override
-    public void start(boolean enableHighAccuracy) {
+    public void onConnected(Bundle connectionHint) {
         ThreadUtils.assertOnUiThread();
 
-        LocationRequest locationRequest = LocationRequest.create();
-        if (mContext.checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        mLocationRequest = LocationRequest.create();
+        if (mGoogleApiClient.getContext().checkCallingOrSelfPermission(
+                    Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             // Workaround for a bug in Google Play Services where, if an app only has
             // ACCESS_COARSE_LOCATION, trying to request PRIORITY_HIGH_ACCURACY will throw a
             // SecurityException even on Android S. See: b/184924939.
-            enableHighAccuracy = false;
-        }
-
-        if (enableHighAccuracy) {
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        } else if (mEnablehighAccuracy) {
             // With enableHighAccuracy, request a faster update interval and configure the provider
             // for high accuracy mode.
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                     .setInterval(UPDATE_INTERVAL_FAST_MS);
         } else {
             // Use balanced mode by default. In this mode, the API will prefer the network provider
             // but may use sensor data (for instance, GPS) if high accuracy is requested by another
             // app.
-            locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
-                    .setInterval(UPDATE_INTERVAL_MS);
-        }
-
-        if (mLocationCallback != null) {
-            mClient.removeLocationUpdates(mLocationCallback);
-        }
-
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                LocationProviderAdapter.onNewLocationAvailable(locationResult.getLastLocation());
+            //
+            // If location is configured for sensors-only then elevate the priority to ensure GPS
+            // and other sensors are used.
+            if (LocationUtils.getInstance().isSystemLocationSettingSensorsOnly()) {
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            } else {
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
             }
-        };
+            mLocationRequest.setInterval(UPDATE_INTERVAL_MS);
+        }
+
+        final Location location = mLocationProviderApi.getLastLocation(mGoogleApiClient);
+        if (location != null) {
+            LocationProviderAdapter.onNewLocationAvailable(location);
+        }
 
         try {
             // Request updates on UI Thread replicating LocationProviderAndroid's behaviour.
-            mClient.requestLocationUpdates(
-                           locationRequest, mLocationCallback, ThreadUtils.getUiThreadLooper())
-                    .addOnFailureListener((e) -> {
-                        Log.e(TAG, "mClient.requestLocationUpdates() " + e);
-                        LocationProviderAdapter.newErrorAvailable(
-                                "Failed to request location updates: " + e.toString());
-                    });
+            mLocationProviderApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this, ThreadUtils.getUiThreadLooper());
         } catch (IllegalStateException e) {
             // IllegalStateException is thrown "If this method is executed in a thread that has not
             // called Looper.prepare()".
-            Log.e(TAG, "mClient.requestLocationUpdates() " + e);
+            Log.e(TAG, "mLocationProviderApi.requestLocationUpdates() " + e);
             LocationProviderAdapter.newErrorAvailable(
                     "Failed to request location updates: " + e.toString());
             assert false;
         } catch (SecurityException e) {
             // SecurityException is thrown when the app is missing location permissions. See
             // crbug.com/731271.
-            Log.e(TAG, "mClient.requestLocationUpdates() missing permissions " + e);
+            Log.e(TAG, "mLocationProviderApi.requestLocationUpdates() missing permissions " + e);
             LocationProviderAdapter.newErrorAvailable(
                     "Failed to request location updates due to permissions: " + e.toString());
         }
     }
 
     @Override
+    public void onConnectionSuspended(int cause) {}
+
+    // OnConnectionFailedListener implementation
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        LocationProviderAdapter.newErrorAvailable(
+                "Failed to connect to Google Play Services: " + result.toString());
+    }
+
+    // LocationProvider implementation
+    @Override
+    public void start(boolean enableHighAccuracy) {
+        ThreadUtils.assertOnUiThread();
+        if (mGoogleApiClient.isConnected()) mGoogleApiClient.disconnect();
+
+        mEnablehighAccuracy = enableHighAccuracy;
+        mGoogleApiClient.connect(); // Should return via onConnected().
+    }
+
+    @Override
     public void stop() {
         ThreadUtils.assertOnUiThread();
+        if (!mGoogleApiClient.isConnected()) return;
 
-        mClient.removeLocationUpdates(mLocationCallback);
-        mLocationCallback = null;
+        mLocationProviderApi.removeLocationUpdates(mGoogleApiClient, this);
+
+        mGoogleApiClient.disconnect();
     }
 
     @Override
     public boolean isRunning() {
         assert ThreadUtils.runningOnUiThread();
-        return mLocationCallback != null;
+        if (mGoogleApiClient == null) return false;
+        return mGoogleApiClient.isConnecting() || mGoogleApiClient.isConnected();
+    }
+
+    // LocationListener implementation
+    @Override
+    public void onLocationChanged(Location location) {
+        LocationProviderAdapter.onNewLocationAvailable(location);
     }
 }
--- services/device/geolocation/android/junit/src/org/chromium/device/geolocation/LocationProviderTest.java
+++ services/device/geolocation/android/junit/src/org/chromium/device/geolocation/LocationProviderTest.java
@@ -7,25 +7,29 @@ package org.chromium.device.geolocation;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doAnswer;
 
-import android.content.Context;
 import android.location.LocationManager;
 import android.os.Build;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.ParameterizedRobolectricTestRunner;
 import org.robolectric.ParameterizedRobolectricTestRunner.Parameters;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLocationManager;
-import org.robolectric.shadows.ShadowLog;
+import org.robolectric.shadows.ShadowLog; // remove me ?
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.DisabledTest;
@@ -54,6 +58,11 @@ public class LocationProviderTest {
         return Arrays.asList(new Object[][] {{LocationProviderType.MOCK},
                 {LocationProviderType.ANDROID}, {LocationProviderType.GMS_CORE}});
     }
+
+    // Member variables for LocationProviderType.GMS_CORE case.
+    @Mock
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mGoogleApiClientIsConnected;
 
     private LocationManager mLocationManager;
     private ShadowLocationManager mShadowLocationManager;
@@ -136,12 +145,44 @@ public class LocationProviderTest {
     }
 
     private void setLocationProviderGmsCore() {
-        Context context = Mockito.mock(Context.class);
-        FusedLocationProviderClient fusedLocationProviderClient =
-                Mockito.mock(FusedLocationProviderClient.class);
+        // Robolectric has a ShadowGoogleApiClientBuilder class that mocks the behaviour of the real
+        // class very closely, but it's not available in our build
+        mGoogleApiClient = Mockito.mock(GoogleApiClient.class);
+        mGoogleApiClientIsConnected = false;
+        doAnswer(new Answer<Boolean>() {
+            @Override
+            public Boolean answer(InvocationOnMock invocation) {
+                return mGoogleApiClientIsConnected;
+            }
+        })
+                .when(mGoogleApiClient)
+                .isConnected();
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                mGoogleApiClientIsConnected = true;
+                return null;
+            }
+        })
+                .when(mGoogleApiClient)
+                .connect();
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                mGoogleApiClientIsConnected = false;
+                return null;
+            }
+        })
+                .when(mGoogleApiClient)
+                .disconnect();
+
+        FusedLocationProviderApi fusedLocationProviderApi =
+                Mockito.mock(FusedLocationProviderApi.class);
 
         LocationProviderGmsCore locationProviderGmsCore =
-                new LocationProviderGmsCore(context, fusedLocationProviderClient);
+                new LocationProviderGmsCore(mGoogleApiClient, fusedLocationProviderApi);
 
         LocationProviderFactory.setLocationProviderImpl(locationProviderGmsCore);
     }
