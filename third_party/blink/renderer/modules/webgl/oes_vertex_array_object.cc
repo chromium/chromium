e575f82241daf87ac54ccb3ef674a51874bec415
Kenneth Russell <kbr@chromium.org>
2022-07-12 02:02:32 +0000

    Improve validation in OES_vertex_array_object extension.
    deleteVertexArrayOES and isVertexArrayOES were missing needed
    validation checks per specification.
    
    Fixes strengthened conformance test checks being introduced in
    https://github.com/KhronosGroup/WebGL/pull/3458 .
    
    Fixed: 1343626
    Change-Id: I293520e0eccd2119ca2b6c0440e2b05024cb5f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3756856
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023019}

--- third_party/blink/renderer/modules/webgl/oes_vertex_array_object.cc
+++ third_party/blink/renderer/modules/webgl/oes_vertex_array_object.cc
@@ -55,7 +55,20 @@ WebGLVertexArrayObjectOES* OESVertexArrayObject::createVertexArrayOES() {
 void OESVertexArrayObject::deleteVertexArrayOES(
     WebGLVertexArrayObjectOES* array_object) {
   WebGLExtensionScopedContext scoped(this);
-  if (!array_object || scoped.IsLost())
+  if (scoped.IsLost() || !array_object)
+    return;
+
+  // ValidateWebGLObject generates an error if the object has already been
+  // deleted, so we must replicate most of its checks here.
+  if (!array_object->Validate(scoped.Context()->ContextGroup(),
+                              scoped.Context())) {
+    scoped.Context()->SynthesizeGLError(
+        GL_INVALID_OPERATION, "deleteVertexArrayOES",
+        "object does not belong to this context");
+    return;
+  }
+
+  if (array_object->MarkedForDeletion())
     return;
 
   if (!array_object->IsDefaultObject() &&
@@ -68,7 +81,9 @@ void OESVertexArrayObject::deleteVertexArrayOES(
 GLboolean OESVertexArrayObject::isVertexArrayOES(
     WebGLVertexArrayObjectOES* array_object) {
   WebGLExtensionScopedContext scoped(this);
-  if (!array_object || scoped.IsLost())
+  if (scoped.IsLost() || !array_object ||
+      !array_object->Validate(scoped.Context()->ContextGroup(),
+                              scoped.Context()))
     return 0;
 
   if (!array_object->HasEverBeenBound())
