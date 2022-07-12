f582a2e6a0c6e37748dbfe44390923b38c7f8e64
Clemens Backes <clemensb@chromium.org>
2022-07-12 16:11:48 +0000

    [content][test] Set V8 flags before initialization
    V8 flags will be disallowed to be changed after V8 initialization
    (https://crrev.com/c/3707284). So the RenderViewTest should set flags
    before initializing blink.
    
    R=sky@chromium.org
    CC=cbruni@chromium.org
    
    Bug: v8:12887
    Change-Id: I7417ac613290a9e76578a5c9099b413706baa8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757709
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023253}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -415,6 +415,12 @@ void RenderViewTest::SetUp() {
 
   render_thread_->SetIOTaskRunner(test_io_thread_->task_runner());
 
+  // Setting flags and really doing anything with WebKit is fairly fragile and
+  // hacky, but this is the world we live in...
+  // Note that we need to set flags *before* initializing V8 (as part of blink),
+  // as it's not allowed to modify them later.
+  v8::V8::SetFlagsFromString("--expose-gc");
+
   // ContentClient must be initialized before Blink, because Blink now eagerly
   // loads the default stylesheets, which are fetched from the resource bundle
   // using ContentClient.
@@ -452,11 +458,6 @@ void RenderViewTest::SetUp() {
   params_ = std::make_unique<MainFunctionParams>(command_line_.get());
   platform_ = std::make_unique<RendererMainPlatformDelegate>(*params_);
   platform_->PlatformInitialize();
-
-  // Setting flags and really doing anything with WebKit is fairly fragile and
-  // hacky, but this is the world we live in...
-  std::string flags("--expose-gc");
-  v8::V8::SetFlagsFromString(flags.c_str(), flags.size());
 
   // Ensure that we register any necessary schemes when initializing WebKit,
   // since we are using a MockRenderThread.
