95b86108dc664787d3b898cd061268af3bab78c8
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-13 21:08:18 +0000

    [rust] rs_bindings_from_cc.gni: Use GN metadata for targets_and_headers.
    This CL refactors how Chromium calculates the --targets_and_headers
    cmdline argument for Crubit's `rs_bindings_from_cc` tool.  After this CL
    the calculation is based on GN's `metadata` which should in the future
    work for non-self-contained targets (i.e. should gather information
    about targets and headers that are transitively depended upon).
    
    Still, for now this CL has only been tested when generating Rust
    bindings for a single, self-contained C++ target.  More work is needed
    to support correctly processing transitive dependencies - this will most
    likely require supporting explicit, separate `bindings_deps` and
    `bindings_target` inputs to the `rs_bindings_from_cc` template.
    
    Bug: 1329611
    Change-Id: I8893707bad8856468d74bc17ad698f8f1b7276e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3753303
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023929}

--- build/rust/run_rs_bindings_from_cc.py
+++ build/rust/run_rs_bindings_from_cc.py
@@ -19,6 +19,8 @@ from util import build_utils
 
 RUST_TOOLCHAIN_DIR = os.path.join(CHROMIUM_SRC_DIR, "third_party",
                                   "rust-toolchain")
+RUSTFMT_EXE_PATH = os.path.join(RUST_TOOLCHAIN_DIR, "bin", "rustfmt")
+RUSTFMT_CONFIG_PATH = os.path.join(CHROMIUM_SRC_DIR, ".rustfmt.toml")
 RS_BINDINGS_FROM_CC_EXE_PATH = os.path.join(RUST_TOOLCHAIN_DIR, "bin",
                                             "rs_bindings_from_cc")
 
@@ -48,9 +50,51 @@ RS_BINDINGS_FROM_CC_EXE_PATH = os.path.join(RUST_TOOLCHAIN_DIR, "bin",
 
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--targets_and_headers_from_gn",
+                      metavar="FILE",
+                      help="File parsed into --targets_and_headers Crubit arg",
+                      required=True),
+  parser.add_argument("--public_headers",
+                      metavar="FILE",
+                      help="Passed through to Crubit",
+                      required=True),
+  parser.add_argument("--rs_out",
+                      metavar="FILE",
+                      help="Passed through to Crubit",
+                      required=True),
+  parser.add_argument("--cc_out",
+                      metavar="FILE",
+                      help="Passed through to Crubit",
+                      required=True),
+  parser.add_argument("clang_args",
+                      metavar="CLANGARGS",
+                      help="Arguments to forward to clang libraries",
+                      nargs=argparse.REMAINDER)
+  args = parser.parse_args()
+
+  # Fixed config.
+  genargs = []
+  genargs.extend([
+      "--crubit_support_path",
+      "third_party/crubit/src/rs_bindings_from_cc/support"
+  ])
+  genargs.extend(["--rustfmt_exe_path", RUSTFMT_EXE_PATH])
+  genargs.extend(["--rustfmt_config_path", RUSTFMT_CONFIG_PATH])
+
+  # Morph contents of the GN-generated JSON file into a single-line string.
+  with open(args.targets_and_headers_from_gn, "r") as f:
+    targets_and_headers = f.read().replace('\n', ' ').replace('\r', ' ')
+  genargs.extend(["--targets_and_headers", targets_and_headers])
+
+  # Other inputs and outputs.
+  genargs.extend(["--public_headers", args.public_headers])
+  genargs.extend(["--rs_out", args.rs_out])
+  genargs.extend(["--cc_out", args.cc_out])
+
   # The call to `filter_clang_args` is needed to avoid the following error:
   # error: unable to find plugin 'find-bad-constructs'
-  args = filter_clang_args(sys.argv)
+  genargs.extend(filter_clang_args(args.clang_args))
 
   # TODO(crbug.com/1297592): This warning needs to be suppressed, because
   # otherwise Crubit/Clang complains as follows:
@@ -58,12 +102,12 @@ def main():
   #     'linker' input unused [-Werror,-Wunused-command-line-argument]
   # Maybe `build/rust/rs_bindings_from_cc.gni` gives too much in `args`?  But
   # then `{{cflags}}` seems perfectly reasonable...
-  args += ["-Wno-unused-command-line-argument"]
+  genargs += ["-Wno-unused-command-line-argument"]
 
   # TODO(crbug.com/1297592): run_bindgen.py removes the outputs when the tool
   # fails.  Maybe we need to do something similar here?  OTOH in most failure
   # modes Crubit will fail *before* generating its outputs...
-  return subprocess.run([RS_BINDINGS_FROM_CC_EXE_PATH, *args]).returncode
+  return subprocess.run([RS_BINDINGS_FROM_CC_EXE_PATH, *genargs]).returncode
 
 
 if __name__ == '__main__':
