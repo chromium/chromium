f851dc7dc1027258692398aff28da7fb0a44ec81
Benoit Lize <lizeb@chromium.org>
2022-07-13 12:22:42 +0000

    [base/allocator] Remove pre-C++17 hacks for aligned operators
    After https://chromium-review.googlesource.com/c/chromium/src/+/3749737,
    we do not need special casing anything, and can just define the aligned
    operators.
    
    Bug: 1274388
    Change-Id: I74602454684bbeb9ef51fd9eb29b996a7e330a8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757746
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023682}

--- base/allocator/allocator_shim_override_cpp_symbols.h
+++ base/allocator/allocator_shim_override_cpp_symbols.h
@@ -17,43 +17,6 @@
 #include "base/compiler_specific.h"
 #include "build/build_config.h"
 
-// std::align_val_t isn't available until C++17, but we want to override aligned
-// new/delete anyway to prevent a possible situation where a library gets loaded
-// in that uses the aligned operators.  We want to avoid a situation where
-// separate heaps are used.
-// TODO(thomasanderson): Remove this once building with C++17 or later.
-#if defined(__cpp_aligned_new) && __cpp_aligned_new >= 201606
-#define ALIGN_VAL_T std::align_val_t
-#define ALIGN_LINKAGE
-#define ALIGN_NEW operator new
-#define ALIGN_NEW_NOTHROW operator new
-#define ALIGN_DEL operator delete
-#define ALIGN_DEL_SIZED operator delete
-#define ALIGN_DEL_NOTHROW operator delete
-#define ALIGN_NEW_ARR operator new[]
-#define ALIGN_NEW_ARR_NOTHROW operator new[]
-#define ALIGN_DEL_ARR operator delete[]
-#define ALIGN_DEL_ARR_SIZED operator delete[]
-#define ALIGN_DEL_ARR_NOTHROW operator delete[]
-#else
-#define ALIGN_VAL_T size_t
-#define ALIGN_LINKAGE extern "C"
-#if BUILDFLAG(IS_WIN)
-#error "Mangling is different on these platforms."
-#else
-#define ALIGN_NEW _ZnwmSt11align_val_t
-#define ALIGN_NEW_NOTHROW _ZnwmSt11align_val_tRKSt9nothrow_t
-#define ALIGN_DEL _ZdlPvSt11align_val_t
-#define ALIGN_DEL_SIZED _ZdlPvmSt11align_val_t
-#define ALIGN_DEL_NOTHROW _ZdlPvSt11align_val_tRKSt9nothrow_t
-#define ALIGN_NEW_ARR _ZnamSt11align_val_t
-#define ALIGN_NEW_ARR_NOTHROW _ZnamSt11align_val_tRKSt9nothrow_t
-#define ALIGN_DEL_ARR _ZdaPvSt11align_val_t
-#define ALIGN_DEL_ARR_SIZED _ZdaPvmSt11align_val_t
-#define ALIGN_DEL_ARR_NOTHROW _ZdaPvSt11align_val_tRKSt9nothrow_t
-#endif
-#endif
-
 #if !BUILDFLAG(IS_APPLE)
 #define SHIM_CPP_SYMBOLS_EXPORT SHIM_ALWAYS_EXPORT
 #else
@@ -110,57 +73,58 @@ SHIM_CPP_SYMBOLS_EXPORT void operator delete[](void* p, size_t) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void* ALIGN_NEW(std::size_t size,
-                                                      ALIGN_VAL_T alignment) {
+SHIM_CPP_SYMBOLS_EXPORT void* operator new(std::size_t size,
+                                           std::align_val_t alignment) {
   return ShimCppAlignedNew(size, static_cast<size_t>(alignment));
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void* ALIGN_NEW_NOTHROW(
-    std::size_t size,
-    ALIGN_VAL_T alignment,
-    const std::nothrow_t&) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void* operator new(std::size_t size,
+                                           std::align_val_t alignment,
+                                           const std::nothrow_t&) __THROW {
   return ShimCppAlignedNew(size, static_cast<size_t>(alignment));
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void ALIGN_DEL(void* p,
-                                                     ALIGN_VAL_T) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete(void* p,
+                                             std::align_val_t) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void
-ALIGN_DEL_SIZED(void* p, std::size_t size, ALIGN_VAL_T) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete(void* p,
+                                             std::size_t size,
+                                             std::align_val_t) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void
-ALIGN_DEL_NOTHROW(void* p, ALIGN_VAL_T, const std::nothrow_t&) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete(void* p,
+                                             std::align_val_t,
+                                             const std::nothrow_t&) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void* ALIGN_NEW_ARR(
-    std::size_t size,
-    ALIGN_VAL_T alignment) {
+SHIM_CPP_SYMBOLS_EXPORT void* operator new[](std::size_t size,
+                                             std::align_val_t alignment) {
   return ShimCppAlignedNew(size, static_cast<size_t>(alignment));
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void* ALIGN_NEW_ARR_NOTHROW(
-    std::size_t size,
-    ALIGN_VAL_T alignment,
-    const std::nothrow_t&) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void* operator new[](std::size_t size,
+                                             std::align_val_t alignment,
+                                             const std::nothrow_t&) __THROW {
   return ShimCppAlignedNew(size, static_cast<size_t>(alignment));
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void ALIGN_DEL_ARR(void* p,
-                                                         ALIGN_VAL_T) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete[](void* p,
+                                               std::align_val_t) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void
-ALIGN_DEL_ARR_SIZED(void* p, std::size_t size, ALIGN_VAL_T) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete[](void* p,
+                                               std::size_t size,
+                                               std::align_val_t) __THROW {
   ShimCppDelete(p);
 }
 
-ALIGN_LINKAGE SHIM_CPP_SYMBOLS_EXPORT void
-ALIGN_DEL_ARR_NOTHROW(void* p, ALIGN_VAL_T, const std::nothrow_t&) __THROW {
+SHIM_CPP_SYMBOLS_EXPORT void operator delete[](void* p,
+                                               std::align_val_t,
+                                               const std::nothrow_t&) __THROW {
   ShimCppDelete(p);
 }
