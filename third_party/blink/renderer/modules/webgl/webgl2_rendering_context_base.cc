ec087f091aed85d5dd1e2d1c8c31f8e66128fffc
Kenneth Russell <kbr@chromium.org>
2022-07-13 00:11:32 +0000

    Bypass active transform feedback checks upon lost context.
    Once the context is lost, this check in WebGL 2.0's useProgram is no
    longer meaningful. Fixes strengthened test in
    https://github.com/KhronosGroup/WebGL/pull/3462 .
    
    Fixed: 1343722
    Change-Id: I13a9cefed53244d4928e6d8c63bb818ab25aecb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3759237
    Reviewed-by: Gregg Tavares <gman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1023496}

--- third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc
@@ -6251,7 +6251,7 @@ void WebGL2RenderingContextBase::RestoreCurrentFramebuffer() {
 }
 
 void WebGL2RenderingContextBase::useProgram(WebGLProgram* program) {
-  if (transform_feedback_binding_->active() &&
+  if (!isContextLost() && transform_feedback_binding_->active() &&
       !transform_feedback_binding_->paused()) {
     SynthesizeGLError(GL_INVALID_OPERATION, "useProgram",
                       "transform feedback is active and not paused");
