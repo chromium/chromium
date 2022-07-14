0a59e518c7ec356a634099393cd3c1feb9c55f2c
Theodore Olsauskas-Warren <sauski@google.com>
2022-07-14 08:37:41 +0000

    Revert "Reland "Add AutofillEnableExtendedAddressFormats to field trial config.""
    This reverts commit 71447f86ba4ee9790e5ea52bbaf5fac4f67b47e9.
    
    Reason for revert: Expected culprit of crbug.com/1344429
    
    Original change's description:
    > Reland "Add AutofillEnableExtendedAddressFormats to field trial config."
    >
    > This is a reland of commit c01bae6451cc587a85c26fb71f7e91cdbd62e98e
    >
    > The original CL renamed "showHonorific" to "EnableHonorificPrefix" for
    > consistency, but missed several spots, causing the failure. This CL simply leaves "showHonorifics" as is.
    >
    > Original change's description:
    > > Add AutofillEnableExtendedAddressFormats to field trial config.
    > >
    > > This breaks a browser test that verifies how the setting pages look.
    > > The logic checks the US and GB formats, the latter of which is changed
    > > by this feature (County field, see
    > > https://screenshot.googleplex.com/3w34HAVUpJXMfTa). This is resolved by
    > > adapting the expectations accordingly, similar to what is done for
    > > honorifix prefixes already.
    > >
    > > Bug: 1300548
    > > Change-Id: Iae55e02024f3eb3244b96d17ca7be4246a2c7487
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751159
    > > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > > Reviewed-by: Christoph Schwering <schwering@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1022062}
    >
    > Bug: 1300548, 1344429
    > Change-Id: I52b97afa773e301d728a0b592ee89b4d3509a32c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3751041
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1023645}
    
    Bug: 1300548
    Change-Id: Id362b5fbdfab7f02e872d4a71d23e04661866fe5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3757814
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1024165}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -237,7 +237,6 @@ TEST_F('CrSettingsAutofillSectionCompanyEnabledTest', 'All', function() {
   const loadTimeDataOverride = {};
   loadTimeDataOverride['EnableCompanyName'] = true;
   loadTimeDataOverride['showHonorific'] = true;
-  loadTimeDataOverride['EnableExtendedAddressFormat'] = true;
   loadTimeData.overrideValues(loadTimeDataOverride);
   mocha.run();
 });
