a1be8d6449b051e4bfd194f14e49b381e64b17d9
Stephen McGruer <smcgruer@chromium.org>
2022-07-15 13:26:30 +0000

    [basic-card] Remove HasEnrolledInstrumentBasicCardTest tests
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. The first step is to clean up its
    usage in tests.
    
    Bug: 1209835
    Change-Id: I43f5d5cfc994e81a7ae1ff47efd6c43975634e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764181
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024712}

--- chrome/browser/payments/has_enrolled_instrument_browsertest.cc
+++ chrome/browser/payments/has_enrolled_instrument_browsertest.cc
@@ -2,24 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/test/scoped_feature_list.h"
 #include "chrome/test/payments/payment_request_platform_browsertest_base.h"
 #include "chrome/test/payments/personal_data_manager_test_util.h"
-#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace payments {
 namespace {
 
-class HasEnrolledInstrumentBaseTest
+class HasEnrolledInstrumentPaymentHandlerTest
     : public PaymentRequestPlatformBrowserTestBase {
  public:
-  HasEnrolledInstrumentBaseTest(const HasEnrolledInstrumentBaseTest&) = delete;
-  HasEnrolledInstrumentBaseTest& operator=(
-      const HasEnrolledInstrumentBaseTest&) = delete;
+  HasEnrolledInstrumentPaymentHandlerTest(
+      const HasEnrolledInstrumentPaymentHandlerTest&) = delete;
+  HasEnrolledInstrumentPaymentHandlerTest& operator=(
+      const HasEnrolledInstrumentPaymentHandlerTest&) = delete;
 
-  ~HasEnrolledInstrumentBaseTest() override = default;
+  ~HasEnrolledInstrumentPaymentHandlerTest() override = default;
 
   void SetUpOnMainThread() override {
     PaymentRequestPlatformBrowserTestBase::SetUpOnMainThread();
@@ -28,9 +27,8 @@ class HasEnrolledInstrumentBaseTest
 
   // Helper function to test that all variations of hasEnrolledInstrument()
   // returns |expected|.
-  void ExpectHasEnrolledInstrumentIs(
-      bool expected,
-      const std::string& payment_method = "basic-card") {
+  void ExpectHasEnrolledInstrumentIs(bool expected,
+                                     const std::string& payment_method) {
     EXPECT_EQ(expected, content::EvalJs(
                             GetActiveWebContents(),
                             content::JsReplace("hasEnrolledInstrument({}, $1)",
@@ -50,140 +48,12 @@ class HasEnrolledInstrumentBaseTest
   }
 
  protected:
-  HasEnrolledInstrumentBaseTest() = default;
-  base::test::ScopedFeatureList feature_list_;
-};
-
-class HasEnrolledInstrumentBasicCardTest
-    : public HasEnrolledInstrumentBaseTest {
- public:
-  HasEnrolledInstrumentBasicCardTest() {
-    feature_list_.InitAndEnableFeature(features::kPaymentRequestBasicCard);
-  }
-
-  HasEnrolledInstrumentBasicCardTest(
-      const HasEnrolledInstrumentBasicCardTest&) = delete;
-  HasEnrolledInstrumentBasicCardTest& operator=(
-      const HasEnrolledInstrumentBasicCardTest&) = delete;
-
-  ~HasEnrolledInstrumentBasicCardTest() override = default;
-};
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest, NoCard) {
-  ExpectHasEnrolledInstrumentIs(false);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest, NoBillingAddress) {
-  AddCreditCard(autofill::test::GetCreditCard());
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest,
-                       HaveShippingNoBillingAddress) {
-  CreateAndAddAutofillProfile();
-  AddCreditCard(autofill::test::GetCreditCard());
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest,
-                       HaveShippingAndBillingAddress) {
-  CreateAndAddCreditCardForProfile(CreateAndAddAutofillProfile());
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest, InvalidCardNumber) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  AddAutofillProfile(address);
-  autofill::CreditCard card = CreatCreditCardForProfile(address);
-  card.SetRawInfo(autofill::ServerFieldType::CREDIT_CARD_NUMBER,
-                  u"1111111111111111");
-  AddCreditCard(card);
-
-  ExpectHasEnrolledInstrumentIs(false);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest, ExpiredCard) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  AddAutofillProfile(address);
-  autofill::CreditCard card = CreatCreditCardForProfile(address);
-  card.SetExpirationYear(2000);
-  AddCreditCard(card);
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-// TODO(https://crbug.com/994799): Unify autofill data validation and returned
-// data across platforms.
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest,
-                       HaveNoNameShippingAndBillingAddress) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  address.SetRawInfo(autofill::ServerFieldType::NAME_FIRST, std::u16string());
-  address.SetRawInfo(autofill::ServerFieldType::NAME_MIDDLE, std::u16string());
-  address.SetRawInfo(autofill::ServerFieldType::NAME_LAST, std::u16string());
-  // For structured names, it is necessary to explicitly reset the full name
-  // and the full name with the prefix.
-  address.SetInfo(autofill::ServerFieldType::NAME_FULL, std::u16string(),
-                  "en-US");
-  address.SetInfo(autofill::ServerFieldType::NAME_FULL_WITH_HONORIFIC_PREFIX,
-                  std::u16string(), "en-US");
-  AddAutofillProfile(address);
-  CreateAndAddCreditCardForProfile(address);
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest,
-                       HaveNoStreetShippingAndBillingAddress) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  address.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_STREET_ADDRESS,
-                     std::u16string());
-  AddAutofillProfile(address);
-  CreateAndAddCreditCardForProfile(address);
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest, NoEmailAddress) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  address.SetRawInfo(autofill::ServerFieldType::EMAIL_ADDRESS,
-                     std::u16string());
-  AddAutofillProfile(address);
-  CreateAndAddCreditCardForProfile(address);
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentBasicCardTest,
-                       InvalidEmailAddress) {
-  autofill::AutofillProfile address = autofill::test::GetFullProfile();
-  address.SetRawInfo(autofill::ServerFieldType::EMAIL_ADDRESS,
-                     u"this-is-not-a-valid-email-address");
-  AddAutofillProfile(address);
-  CreateAndAddCreditCardForProfile(address);
-
-  ExpectHasEnrolledInstrumentIs(true);
-}
-
-class HasEnrolledInstrumentPaymentHandlerTest
-    : public HasEnrolledInstrumentBaseTest {
- public:
-  HasEnrolledInstrumentPaymentHandlerTest() {
-    feature_list_.InitAndDisableFeature(features::kPaymentRequestBasicCard);
-  }
-
-  HasEnrolledInstrumentPaymentHandlerTest(
-      const HasEnrolledInstrumentPaymentHandlerTest&) = delete;
-  HasEnrolledInstrumentPaymentHandlerTest& operator=(
-      const HasEnrolledInstrumentPaymentHandlerTest&) = delete;
-
-  ~HasEnrolledInstrumentPaymentHandlerTest() override = default;
+  HasEnrolledInstrumentPaymentHandlerTest() = default;
 };
 
 IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentPaymentHandlerTest,
                        FalseWithoutPaymentHandler) {
-  ExpectHasEnrolledInstrumentIs(false);
+  ExpectHasEnrolledInstrumentIs(false, "non-existent-payment-method");
 }
 
 IN_PROC_BROWSER_TEST_F(HasEnrolledInstrumentPaymentHandlerTest,
