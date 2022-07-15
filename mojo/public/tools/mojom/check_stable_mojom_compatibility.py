ee4bbac2acab4f0f80bd54d55d2775be2a000693
Daniel Cheng <dcheng@chromium.org>
2022-07-15 16:40:54 +0000

    Reland "Require [Default] when [Extensible] is used."
    This is a reland of commit 9b60c441f1ba9805540d019265c6effd812a9b09
    
    Chromebox For Meetings is apparently a tree closer ¯\_(ツ)_/¯
    
    Original change's description:
    > Require [Default] when [Extensible] is used.
    >
    > A large allowlist remains for now, to allow incremental fixes over time.
    >
    > Bug: 1316161
    > Cq-Include-Trybots: luci.chromium.try:android-official
    > Cq-Include-Trybots: luci.chromium.try:fuchsia-official
    > Cq-Include-Trybots: luci.chromium.try:linux-official
    > Cq-Include-Trybots: luci.chromium.try:mac-official
    > Cq-Include-Trybots: luci.chromium.try:win-official
    > Cq-Include-Trybots: luci.chrome.try:chromeos-eve-chrome
    > Cq-Include-Trybots: luci.chrome.try:linux-chrome
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Cq-Include-Trybots: luci.chrome.try:mac-chrome
    > Cq-Include-Trybots: luci.chrome.try:win64-chrome
    > Change-Id: I81aa7f129677396b96f8a686ce38eec626777f54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3761268
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024580}
    
    Bug: 1316161
    Change-Id: I1d9924928467aed4af82f7af3cd9173028862afa
    Cq-Include-Trybots: luci.chromium.try:android-official
    Cq-Include-Trybots: luci.chromium.try:fuchsia-official
    Cq-Include-Trybots: luci.chromium.try:linux-cfm-rel
    Cq-Include-Trybots: luci.chromium.try:linux-official
    Cq-Include-Trybots: luci.chromium.try:mac-official
    Cq-Include-Trybots: luci.chromium.try:win-official
    Cq-Include-Trybots: luci.chrome.try:chromeos-eve-chrome
    Cq-Include-Trybots: luci.chrome.try:linux-chrome
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Cq-Include-Trybots: luci.chrome.try:win64-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764653
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024783}

--- mojo/public/tools/mojom/check_stable_mojom_compatibility.py
+++ mojo/public/tools/mojom/check_stable_mojom_compatibility.py
@@ -40,6 +40,8 @@ def _ValidateDelta(root, delta):
   transitive closure of a mojom's input dependencies all at once.
   """
 
+  translate.is_running_backwards_compatibility_check_hack = True
+
   # First build a map of all files covered by the delta
   affected_files = set()
   old_files = {}
