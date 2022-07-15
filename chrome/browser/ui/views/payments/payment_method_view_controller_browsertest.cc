d5c538b720d84b5436a4409e45b0b57f53e494d0
Stephen McGruer <smcgruer@chromium.org>
2022-07-15 16:46:54 +0000

    [basic-card] Remove PaymentMethodViewControllerBasicCardTest tests
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. The first step is to clean up its
    usage in tests.
    
    Bug: 1209835
    Change-Id: I19533a1997e74e4570fe6ad71daca141eb4f38fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764860
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024787}

--- chrome/browser/ui/views/payments/payment_method_view_controller_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_method_view_controller_browsertest.cc
@@ -33,9 +33,7 @@ class PaymentMethodViewControllerTest : public PaymentRequestBrowserTestBase {
  protected:
   PaymentMethodViewControllerTest()
       : gpay_server_(net::EmbeddedTestServer::TYPE_HTTPS),
-        kylepay_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
-    feature_list_.InitAndEnableFeature(::features::kPaymentRequestBasicCard);
-  }
+        kylepay_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -71,133 +69,6 @@ class PaymentMethodViewControllerTest : public PaymentRequestBrowserTestBase {
   base::test::ScopedFeatureList feature_list_;
 };
 
-class PaymentMethodViewControllerBasicCardTest
-    : public PaymentMethodViewControllerTest {
- public:
-  PaymentMethodViewControllerBasicCardTest(
-      const PaymentMethodViewControllerBasicCardTest&) = delete;
-  PaymentMethodViewControllerBasicCardTest& operator=(
-      const PaymentMethodViewControllerBasicCardTest&) = delete;
-
- protected:
-  PaymentMethodViewControllerBasicCardTest() {
-    feature_list_.InitAndEnableFeature(features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerBasicCardTest,
-                       OneCardSelected) {
-  NavigateTo("/payment_request_no_shipping_test.html");
-  autofill::AutofillProfile billing_profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(billing_profile);
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_profile.guid());
-  AddCreditCard(card);
-
-  InvokePaymentRequestUI();
-  OpenPaymentMethodScreen();
-
-  PaymentRequest* request = GetPaymentRequests().front();
-  EXPECT_EQ(1U, request->state()->available_apps().size());
-
-  views::View* list_view = dialog_view()->GetViewByID(
-      static_cast<int>(DialogViewID::PAYMENT_METHOD_SHEET_LIST_VIEW));
-  EXPECT_TRUE(list_view);
-  EXPECT_EQ(1u, list_view->children().size());
-
-  EXPECT_EQ(request->state()->available_apps().front().get(),
-            request->state()->selected_app());
-  views::View* checkmark_view = list_view->children().front()->GetViewByID(
-      static_cast<int>(DialogViewID::CHECKMARK_VIEW));
-  EXPECT_TRUE(checkmark_view->GetVisible());
-}
-
-IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerBasicCardTest,
-                       OneCardSelectedOutOfMany) {
-  NavigateTo("/payment_request_no_shipping_test.html");
-  autofill::AutofillProfile billing_profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(billing_profile);
-
-  autofill::CreditCard card1 = autofill::test::GetCreditCard();
-  card1.set_billing_address_id(billing_profile.guid());
-
-  // Ensure that this card is the first suggestion.
-  card1.set_use_count(5U);
-  AddCreditCard(card1);
-
-  // Slightly different visa.
-  autofill::CreditCard card2 = autofill::test::GetCreditCard();
-  card2.SetNumber(u"4111111111111112");
-  card2.set_billing_address_id(billing_profile.guid());
-  card2.set_use_count(1U);
-  AddCreditCard(card2);
-
-  InvokePaymentRequestUI();
-  OpenPaymentMethodScreen();
-
-  PaymentRequest* request = GetPaymentRequests().front();
-  EXPECT_EQ(2U, request->state()->available_apps().size());
-  EXPECT_EQ(request->state()->available_apps().front().get(),
-            request->state()->selected_app());
-
-  views::View* list_view = dialog_view()->GetViewByID(
-      static_cast<int>(DialogViewID::PAYMENT_METHOD_SHEET_LIST_VIEW));
-  EXPECT_TRUE(list_view);
-  EXPECT_EQ(2u, list_view->children().size());
-
-  EXPECT_EQ(request->state()->available_apps().front().get(),
-            request->state()->selected_app());
-  views::View* checkmark_view = list_view->children()[0]->GetViewByID(
-      static_cast<int>(DialogViewID::CHECKMARK_VIEW));
-  EXPECT_TRUE(checkmark_view->GetVisible());
-
-  views::View* checkmark_view2 = list_view->children()[1]->GetViewByID(
-      static_cast<int>(DialogViewID::CHECKMARK_VIEW));
-  EXPECT_FALSE(checkmark_view2->GetVisible());
-
-  ResetEventWaiter(DialogEvent::BACK_NAVIGATION);
-  // Simulate selecting the second card.
-  ClickOnDialogViewAndWait(list_view->children()[1]);
-
-  EXPECT_EQ(request->state()->available_apps().back().get(),
-            request->state()->selected_app());
-
-  OpenPaymentMethodScreen();
-  list_view = dialog_view()->GetViewByID(
-      static_cast<int>(DialogViewID::PAYMENT_METHOD_SHEET_LIST_VIEW));
-
-  ResetEventWaiter(DialogEvent::BACK_NAVIGATION);
-  // Clicking on the second card again should not modify any state, and should
-  // return to the main payment sheet.
-  ClickOnDialogViewAndWait(list_view->children()[1]);
-
-  EXPECT_EQ(request->state()->available_apps().back().get(),
-            request->state()->selected_app());
-}
-
-IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerBasicCardTest,
-                       EditButtonOpensEditor) {
-  NavigateTo("/payment_request_no_shipping_test.html");
-  AddCreditCard(autofill::test::GetCreditCard());
-
-  InvokePaymentRequestUI();
-  OpenPaymentMethodScreen();
-
-  views::View* list_view = dialog_view()->GetViewByID(
-      static_cast<int>(DialogViewID::PAYMENT_METHOD_SHEET_LIST_VIEW));
-  EXPECT_TRUE(list_view);
-  EXPECT_EQ(1u, list_view->children().size());
-
-  views::View* edit_button = list_view->children().front()->GetViewByID(
-      static_cast<int>(DialogViewID::EDIT_ITEM_BUTTON));
-
-  ResetEventWaiter(DialogEvent::CREDIT_CARD_EDITOR_OPENED);
-  ClickOnDialogViewAndWait(edit_button);
-}
-
 IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerTest,
                        DoNotShowAddCardWhenBasicCardIsNotSupported) {
   NavigateTo("/payment_request_bobpay_and_cards_test.html");
@@ -220,24 +91,7 @@ IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerTest,
   EXPECT_EQ(nullptr, add_card_button);
 }
 
-class PaymentMethodViewControllerPaymentHandlerTest
-    : public PaymentMethodViewControllerTest {
- public:
-  PaymentMethodViewControllerPaymentHandlerTest(
-      const PaymentMethodViewControllerPaymentHandlerTest&) = delete;
-  PaymentMethodViewControllerPaymentHandlerTest& operator=(
-      const PaymentMethodViewControllerPaymentHandlerTest&) = delete;
-
- protected:
-  PaymentMethodViewControllerPaymentHandlerTest() {
-    feature_list_.InitAndDisableFeature(features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerPaymentHandlerTest,
+IN_PROC_BROWSER_TEST_F(PaymentMethodViewControllerTest,
                        OneAppSelectedOutOfMany) {
   std::string payment_method_a;
   InstallPaymentApp("a.com", "/nickpay.com/app.js", &payment_method_a);
