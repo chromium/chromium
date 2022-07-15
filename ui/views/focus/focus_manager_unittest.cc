3088264be4fddb2c9d40e9217760632a6bd1d0f6
Robert Liao <robliao@chromium.org>
2022-07-15 17:22:14 +0000

    Remove WIDGET_OWNS_NATIVE_WIDGET from Views Focus Manager Unit Tests
    Bug: 1218885
    Change-Id: I82b65dc7d4a363e6612d5e5ee965ee38462cdbc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764171
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1024807}

--- ui/views/focus/focus_manager_unittest.cc
+++ ui/views/focus/focus_manager_unittest.cc
@@ -30,6 +30,7 @@
 #include "ui/views/test/test_platform_native_widget.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/view_class_properties.h"
+#include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
 
 #if defined(USE_AURA)
@@ -157,17 +158,18 @@ TEST_F(FocusManagerTest, WidgetFocusChangeListener) {
   TestWidgetFocusChangeListener widget_listener;
   AddWidgetFocusChangeListener(&widget_listener);
 
-  Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_WINDOW);
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
-  params.bounds = gfx::Rect(10, 10, 100, 100);
-  params.parent = GetWidget()->GetNativeView();
-
-  std::unique_ptr<Widget> widget1(new Widget);
-  widget1->Init(std::move(params));
+  Widget::InitParams params1 = CreateParams(Widget::InitParams::TYPE_WINDOW);
+  params1.bounds = gfx::Rect(10, 10, 100, 100);
+  params1.parent = GetWidget()->GetNativeView();
+  UniqueWidgetPtr widget1 = std::make_unique<Widget>();
+  widget1->Init(std::move(params1));
   widget1->Show();
 
-  std::unique_ptr<Widget> widget2(new Widget);
-  widget2->Init(std::move(params));
+  Widget::InitParams params2 = CreateParams(Widget::InitParams::TYPE_WINDOW);
+  params2.bounds = gfx::Rect(10, 10, 100, 100);
+  params2.parent = GetWidget()->GetNativeView();
+  UniqueWidgetPtr widget2 = std::make_unique<Widget>();
+  widget2->Init(std::move(params2));
   widget2->Show();
 
   widget_listener.ClearFocusChanges();
@@ -914,25 +916,27 @@ TEST_F(FocusManagerTest, AdvanceFocusStaysInWidget) {
   GetContentsView()->AddChildView(widget_view);
 
   // Create a widget with two views, focus the second.
-  std::unique_ptr<AdvanceFocusWidgetDelegate> delegate;
   Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_WINDOW);
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   params.child = true;
   params.bounds = gfx::Rect(10, 10, 100, 100);
   params.parent = GetWidget()->GetNativeView();
-  Widget child_widget;
-  delegate = std::make_unique<AdvanceFocusWidgetDelegate>(&child_widget);
-  params.delegate = delegate.get();
-  child_widget.Init(std::move(params));
+  UniqueWidgetPtr child_widget = std::make_unique<Widget>();
+  std::unique_ptr<AdvanceFocusWidgetDelegate> delegate_owned =
+      std::make_unique<AdvanceFocusWidgetDelegate>(child_widget.get());
+  AdvanceFocusWidgetDelegate* delegate = delegate_owned.get();
+  params.delegate = delegate_owned.release();
+  delegate->SetOwnedByWidget(true);
+  child_widget->Init(std::move(params));
+
   View* view1 = new View;
   view1->SetFocusBehavior(View::FocusBehavior::ALWAYS);
   view1->SetBounds(0, 0, 20, 20);
   View* view2 = new View;
   view2->SetFocusBehavior(View::FocusBehavior::ALWAYS);
   view2->SetBounds(20, 0, 20, 20);
-  child_widget.client_view()->AddChildView(view1);
-  child_widget.client_view()->AddChildView(view2);
-  child_widget.Show();
+  child_widget->client_view()->AddChildView(view1);
+  child_widget->client_view()->AddChildView(view2);
+  child_widget->Show();
   view2->RequestFocus();
   EXPECT_EQ(view2, GetFocusManager()->GetFocusedView());
 
@@ -947,12 +951,6 @@ TEST_F(FocusManagerTest, AdvanceFocusStaysInWidget) {
   // And forward again, wrapping back to |view1|.
   GetFocusManager()->AdvanceFocus(true);
   EXPECT_EQ(view1, GetFocusManager()->GetFocusedView());
-
-  // Allow focus to go to the parent, and focus backwards which should now move
-  // up |widget_view| (in the parent).
-  delegate->SetFocusTraversesOut(true);
-  GetFocusManager()->AdvanceFocus(true);
-  EXPECT_EQ(widget_view, GetFocusManager()->GetFocusedView());
 }
 
 TEST_F(FocusManagerTest, NavigateIntoAnchoredDialog) {
@@ -1123,13 +1121,12 @@ class DesktopWidgetFocusManagerTest : public FocusManagerTest {
 };
 
 TEST_F(DesktopWidgetFocusManagerTest, AnchoredDialogInDesktopNativeWidgetAura) {
-  Widget widget;
+  UniqueWidgetPtr widget = std::make_unique<Widget>();
   Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_WINDOW);
-  params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   params.bounds = gfx::Rect(0, 0, 1024, 768);
-  widget.Init(std::move(params));
-  widget.Show();
-  widget.Activate();
+  widget->Init(std::move(params));
+  widget->Show();
+  widget->Activate();
 
   View* parent1 = new View();
   View* parent2 = new View();
@@ -1137,8 +1134,8 @@ TEST_F(DesktopWidgetFocusManagerTest, AnchoredDialogInDesktopNativeWidgetAura) {
   parent1->SetFocusBehavior(View::FocusBehavior::ALWAYS);
   parent2->SetFocusBehavior(View::FocusBehavior::ALWAYS);
 
-  widget.GetRootView()->AddChildView(parent1);
-  widget.GetRootView()->AddChildView(parent2);
+  widget->GetRootView()->AddChildView(parent1);
+  widget->GetRootView()->AddChildView(parent2);
 
   TestBubbleDialogDelegateView* bubble_delegate =
       TestBubbleDialogDelegateView::CreateAndShowBubble(parent2);
@@ -1156,24 +1153,24 @@ TEST_F(DesktopWidgetFocusManagerTest, AnchoredDialogInDesktopNativeWidgetAura) {
   parent2->SetProperty(views::kSkipAccessibilityPaintChecks, true);
   child->SetProperty(views::kSkipAccessibilityPaintChecks, true);
 
-  widget.Activate();
+  widget->Activate();
   parent1->RequestFocus();
   base::RunLoop().RunUntilIdle();
 
   // Initially the outer widget's window is focused.
   aura::client::FocusClient* focus_client =
-      aura::client::GetFocusClient(widget.GetNativeView());
-  ASSERT_EQ(widget.GetNativeView(), focus_client->GetFocusedWindow());
+      aura::client::GetFocusClient(widget->GetNativeView());
+  ASSERT_EQ(widget->GetNativeView(), focus_client->GetFocusedWindow());
 
   // Navigate forwards
-  widget.GetFocusManager()->AdvanceFocus(false);
+  widget->GetFocusManager()->AdvanceFocus(false);
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(parent2->HasFocus());
-  widget.GetFocusManager()->AdvanceFocus(false);
+  widget->GetFocusManager()->AdvanceFocus(false);
   EXPECT_TRUE(child->HasFocus());
 
   // Now the bubble widget's window is focused.
-  ASSERT_NE(widget.GetNativeView(), focus_client->GetFocusedWindow());
+  ASSERT_NE(widget->GetNativeView(), focus_client->GetFocusedWindow());
   ASSERT_EQ(bubble_widget->GetNativeView(), focus_client->GetFocusedWindow());
 
   // Navigate backwards
@@ -1181,7 +1178,7 @@ TEST_F(DesktopWidgetFocusManagerTest, AnchoredDialogInDesktopNativeWidgetAura) {
   EXPECT_TRUE(parent2->HasFocus());
 
   // Finally, the outer widget's window should be focused again.
-  ASSERT_EQ(widget.GetNativeView(), focus_client->GetFocusedWindow());
+  ASSERT_EQ(widget->GetNativeView(), focus_client->GetFocusedWindow());
 }
 #endif
 
