4e82324ce49cda456c4af4e5f42570bc93e90aca
Stephen McGruer <smcgruer@chromium.org>
2022-07-18 15:02:52 +0000

    [basic-card] Remove PaymentRequestNoUpdateWithTest basic card tests
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. The first step is to clean up its
    usage in tests.
    
    Bug: 1209835
    Change-Id: I0fd12b2ac66ffa7f92a70664e250c1e7ffa59a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770087
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025255}

--- chrome/browser/ui/views/payments/payment_request_no_update_with_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_request_no_update_with_browsertest.cc
@@ -3,179 +3,21 @@
 // found in the LICENSE file.
 
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
-#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace payments {
 
-class PaymentRequestNoUpdateWithTest : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestNoUpdateWithTest(const PaymentRequestNoUpdateWithTest&) =
-      delete;
-  PaymentRequestNoUpdateWithTest& operator=(
-      const PaymentRequestNoUpdateWithTest&) = delete;
-
- protected:
-  PaymentRequestNoUpdateWithTest() {
-    feature_list_.InitAndEnableFeature(::features::kPaymentRequestBasicCard);
-  }
-
-  void RunJavaScriptFunctionToOpenPaymentRequestUI(
-      const std::string& function_name) {
-    ResetEventWaiterForDialogOpened();
-
-    content::WebContents* web_contents = GetActiveWebContents();
-    ASSERT_TRUE(content::ExecuteScript(web_contents, function_name + "();"));
-
-    WaitForObservedEvent();
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+using PaymentRequestNoUpdateWithTest = PaymentRequestBrowserTestBase;
 
 // A merchant that does not listen to shipping address update events will not
 // cause timeouts in UI.
 IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithTest, BuyWithoutListeners) {
-  NavigateTo("/payment_request_no_update_with_test.html");
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  AddAutofillProfile(autofill::test::GetFullProfile2());
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);
-
-  RunJavaScriptFunctionToOpenPaymentRequestUI("buyWithoutListeners");
-
-  OpenShippingAddressSectionScreen();
-  ResetEventWaiterForSequence({DialogEvent::PROCESSING_SPINNER_SHOWN,
-                               DialogEvent::PROCESSING_SPINNER_HIDDEN,
-                               DialogEvent::SPEC_DONE_UPDATING,
-                               DialogEvent::BACK_NAVIGATION});
-  ClickOnChildInListViewAndWait(
-      /* child_index=*/1, /*total_num_children=*/2,
-      DialogViewID::SHIPPING_ADDRESS_SHEET_LIST_VIEW,
-      /*wait_for_animation=*/false);
-  // Wait for the animation here explicitly, otherwise
-  // ClickOnChildInListViewAndWait tries to install an AnimationDelegate before
-  // the animation is kicked off (since that's triggered off of the spec being
-  // updated) and this hits a DCHECK.
-  WaitForAnimation();
-
-  PayWithCreditCardAndWait(u"123");
-
-  ExpectBodyContains({"freeShipping"});
-}
-
-// A merchant that listens to shipping address update events, but does not call
-// updateWith() on the event, will not cause timeouts in UI.
-IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithTest,
-                       BuyWithoutCallingUpdateWith) {
-  NavigateTo("/payment_request_no_update_with_test.html");
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  AddAutofillProfile(autofill::test::GetFullProfile2());
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);
-
-  RunJavaScriptFunctionToOpenPaymentRequestUI("buyWithoutCallingUpdateWith");
-
-  OpenShippingAddressSectionScreen();
-  ResetEventWaiterForSequence({DialogEvent::PROCESSING_SPINNER_SHOWN,
-                               DialogEvent::PROCESSING_SPINNER_HIDDEN,
-                               DialogEvent::SPEC_DONE_UPDATING,
-                               DialogEvent::BACK_NAVIGATION});
-  ClickOnChildInListViewAndWait(
-      /* child_index=*/1, /*total_num_children=*/2,
-      DialogViewID::SHIPPING_ADDRESS_SHEET_LIST_VIEW,
-      /*wait_for_animation=*/false);
-  // Wait for the animation here explicitly, otherwise
-  // ClickOnChildInListViewAndWait tries to install an AnimationDelegate before
-  // the animation is kicked off (since that's triggered off of the spec being
-  // updated) and this hits a DCHECK.
-  WaitForAnimation();
-
-  PayWithCreditCardAndWait(u"123");
-
-  ExpectBodyContains({"freeShipping"});
-}
-
-// A merchant that invokes updateWith() directly without using a promise will
-// not cause timeouts in UI.
-IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithTest, BuyWithoutPromises) {
-  NavigateTo("/payment_request_no_update_with_test.html");
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  AddAutofillProfile(autofill::test::GetFullProfile2());
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);
-
-  RunJavaScriptFunctionToOpenPaymentRequestUI("buyWithoutPromises");
-
-  OpenOrderSummaryScreen();
-  EXPECT_EQ(u"$5.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  ClickOnBackArrow();
-
-  OpenShippingAddressSectionScreen();
-  ResetEventWaiterForSequence({DialogEvent::PROCESSING_SPINNER_SHOWN,
-                               DialogEvent::PROCESSING_SPINNER_HIDDEN,
-                               DialogEvent::SPEC_DONE_UPDATING,
-                               DialogEvent::BACK_NAVIGATION});
-  ClickOnChildInListViewAndWait(
-      /* child_index=*/1, /*total_num_children=*/2,
-      DialogViewID::SHIPPING_ADDRESS_SHEET_LIST_VIEW,
-      /*wait_for_animation=*/false);
-  // Wait for the animation here explicitly, otherwise
-  // ClickOnChildInListViewAndWait tries to install an AnimationDelegate before
-  // the animation is kicked off (since that's triggered off of the spec being
-  // updated) and this hits a DCHECK.
-  WaitForAnimation();
-
-  OpenOrderSummaryScreen();
-  EXPECT_EQ(u"$10.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  ClickOnBackArrow();
-
-  PayWithCreditCardAndWait(u"123");
-
-  ExpectBodyContains({"updatedShipping"});
-}
-
-// The tests in this class correspond to the tests of the same name in
-// PaymentRequestShippingOptionViewControllerTest, with basic-card disabled.
-// Parameterized tests are not used because the test setup for both tests are
-// too different.
-class PaymentRequestNoUpdateWithBasicCardDisabledTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestNoUpdateWithBasicCardDisabledTest(
-      const PaymentRequestNoUpdateWithBasicCardDisabledTest&) = delete;
-  PaymentRequestNoUpdateWithBasicCardDisabledTest& operator=(
-      const PaymentRequestNoUpdateWithBasicCardDisabledTest&) = delete;
-
- protected:
-  PaymentRequestNoUpdateWithBasicCardDisabledTest() {
-    feature_list_.InitAndDisableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-// A merchant that does not listen to shipping address update events will not
-// cause timeouts in UI.
-IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithBasicCardDisabledTest,
-                       BuyWithoutListeners) {
   // Installs two apps so that the Payment Request UI will be shown.
   std::string a_method_name;
   InstallPaymentApp("a.com", "payment_request_success_responder.js",
@@ -223,7 +65,7 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithBasicCardDisabledTest,
 
 // A merchant that listens to shipping address update events, but does not call
 // updateWith() on the event, will not cause timeouts in UI.
-IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithBasicCardDisabledTest,
+IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithTest,
                        BuyWithoutCallingUpdateWith) {
   // Installs two apps so that the Payment Request UI will be shown.
   std::string a_method_name;
@@ -272,8 +114,7 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithBasicCardDisabledTest,
 
 // A merchant that invokes updateWith() directly without using a promise will
 // not cause timeouts in UI.
-IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithBasicCardDisabledTest,
-                       BuyWithoutPromises) {
+IN_PROC_BROWSER_TEST_F(PaymentRequestNoUpdateWithTest, BuyWithoutPromises) {
   // Installs two apps so that the Payment Request UI will be shown.
   std::string a_method_name;
   InstallPaymentApp("a.com", "payment_request_success_responder.js",
