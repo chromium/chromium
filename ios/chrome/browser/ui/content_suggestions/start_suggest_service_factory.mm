f12ff1fa6c49defeddcadff40b3fc071c1c67176
Chris Lu <thegreenfrog@chromium.org>
2022-07-18 21:35:39 +0000

    [ios] Create StartSuggestServiceFactory
    This creates and manages the iOS instance of StartSuggestService.
    StartSuggestServiceResponseBridge will bridge the completion callbacks
    to an Obj-C requester.
    
    Bug: 1340123
    Change-Id: I1e4fb3804a67ae995f464b8efbc01e66ee2aa45a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766058
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025416}

---
+++ ios/chrome/browser/ui/content_suggestions/start_suggest_service_factory.mm
@@ -0,0 +1,52 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/content_suggestions/start_suggest_service_factory.h"
+
+#import "components/keyed_service/ios/browser_state_dependency_manager.h"
+#import "ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.h"
+#import "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/chrome_url_constants.h"
+#import "ios/chrome/browser/search_engines/template_url_service_factory.h"
+#import "services/network/public/cpp/shared_url_loader_factory.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// static
+StartSuggestService* StartSuggestServiceFactory::GetForBrowserState(
+    ChromeBrowserState* browser_state,
+    bool create_if_necessary) {
+  return static_cast<StartSuggestService*>(
+      GetInstance()->GetServiceForBrowserState(browser_state,
+                                               create_if_necessary));
+}
+
+// static
+StartSuggestServiceFactory* StartSuggestServiceFactory::GetInstance() {
+  static base::NoDestructor<StartSuggestServiceFactory> instance;
+  return instance.get();
+}
+
+std::unique_ptr<KeyedService>
+StartSuggestServiceFactory::BuildServiceInstanceFor(
+    web::BrowserState* context) const {
+  ChromeBrowserState* browser_state =
+      ChromeBrowserState::FromBrowserState(context);
+  TemplateURLService* template_url_service =
+      ios::TemplateURLServiceFactory::GetForBrowserState(browser_state);
+  auto url_loader_factory = browser_state->GetSharedURLLoaderFactory();
+  return std::make_unique<StartSuggestService>(
+      template_url_service, url_loader_factory,
+      std::make_unique<AutocompleteSchemeClassifierImpl>(),
+      GURL(kChromeUINewTabURL));
+}
+
+StartSuggestServiceFactory::StartSuggestServiceFactory()
+    : BrowserStateKeyedServiceFactory(
+          "StartSuggestService",
+          BrowserStateDependencyManager::GetInstance()) {}
+
+StartSuggestServiceFactory::~StartSuggestServiceFactory() {}
