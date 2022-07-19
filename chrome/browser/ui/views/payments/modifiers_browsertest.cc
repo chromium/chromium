c7036903118cbf0ea11f9d67a2601b18fc83aec4
Stephen McGruer <smcgruer@chromium.org>
2022-07-19 16:20:22 +0000

    [basic-card] Remove PaymentRequestModifiersTest basic card tests
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. The first step is to clean up its
    usage in tests.
    
    Bug: 1209835
    Change-Id: Id51dcfc38b359ee145d93ee56d5725273c197b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770780
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025768}

--- chrome/browser/ui/views/payments/modifiers_browsertest.cc
+++ chrome/browser/ui/views/payments/modifiers_browsertest.cc
@@ -5,13 +5,9 @@
 #include <vector>
 
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
 #include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
-#include "components/autofill/core/browser/autofill_test_utils.h"
-#include "components/payments/core/features.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
-#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -25,20 +21,12 @@ class PaymentRequestModifiersTest : public PaymentRequestBrowserTestBase {
       delete;
 
  protected:
-  PaymentRequestModifiersTest() {}
+  PaymentRequestModifiersTest() = default;
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     PaymentRequestBrowserTestBase::SetUpCommandLine(command_line);
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
-  }
-
-  void SetUpInProcessBrowserTestFixture() override {
-    PaymentRequestBrowserTestBase::SetUpInProcessBrowserTestFixture();
-
-    feature_list_.InitWithFeatures(
-        {::features::kPaymentRequestBasicCard, features::kWebPaymentsModifiers},
-        {});
   }
 
   size_t GetLineCount() {
@@ -47,172 +35,9 @@ class PaymentRequestModifiersTest : public PaymentRequestBrowserTestBase {
         top->GetViewByID(static_cast<int>(DialogViewID::CONTENT_VIEW));
     return content->children().size();
   }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersTest,
-                       NoModifierAppliedIfNoSelectedInstrument) {
-  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
-  InvokePaymentRequestUI();
-  OpenOrderSummaryScreen();
-
-  EXPECT_EQ(u"$5.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  // There's only the total line.
-  EXPECT_EQ(1u, GetLineCount());
-}
-
-IN_PROC_BROWSER_TEST_F(
-    PaymentRequestModifiersTest,
-    ModifierNotAppliedIfSelectedInstrumentWithoutMatchingNetwork) {
-  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
-  autofill::AutofillProfile profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(profile);
-  autofill::CreditCard card(
-      autofill::test::GetMaskedServerCard());  // Mastercard card.
-  card.set_billing_address_id(profile.guid());
-  AddCreditCard(card);
-
-  ResetEventWaiterForDialogOpened();
-  content::WebContents* web_contents = GetActiveWebContents();
-  const std::string click_buy_button_js =
-      "(function() { "
-      "document.getElementById('visa_supported_network')."
-      "click(); })();";
-  ASSERT_TRUE(content::ExecuteScript(web_contents, click_buy_button_js));
-  WaitForObservedEvent();
-  // The web-modal dialog should be open.
-  web_modal::WebContentsModalDialogManager* web_contents_modal_dialog_manager =
-      web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
-  EXPECT_TRUE(web_contents_modal_dialog_manager->IsDialogActive());
-
-  OpenOrderSummaryScreen();
-
-  EXPECT_EQ(u"$5.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  // There's only the total line.
-  EXPECT_EQ(1u, GetLineCount());
-}
-
-IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersTest,
-                       ModifierAppliedToBasicCardWithoutTypeOrNetwork) {
-  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
-  autofill::AutofillProfile profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(profile);
-  autofill::CreditCard card(autofill::test::GetCreditCard());  // Visa card.
-  card.set_billing_address_id(profile.guid());
-  AddCreditCard(card);
-
-  InvokePaymentRequestUI();
-  OpenOrderSummaryScreen();
-
-  EXPECT_EQ(u"$4.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  // There's the total line and the discount line.
-  EXPECT_EQ(2u, GetLineCount());
-}
-
-IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersTest,
-                       ModifierAppliedToUnknownTypeWithMatchingNetwork) {
-  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
-  autofill::AutofillProfile profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(profile);
-  autofill::CreditCard card(autofill::test::GetCreditCard());  // Visa card.
-  // Change to Mastercard to match the test case.
-  card.SetRawInfo(autofill::CREDIT_CARD_NUMBER, u"5555555555554444");
-  card.set_billing_address_id(profile.guid());
-  AddCreditCard(card);
-
-  ResetEventWaiterForDialogOpened();
-  content::WebContents* web_contents = GetActiveWebContents();
-  const std::string click_buy_button_js =
-      "(function() { "
-      "document.getElementById('mastercard_any_supported_type')."
-      "click(); })();";
-  ASSERT_TRUE(content::ExecuteScript(web_contents, click_buy_button_js));
-  WaitForObservedEvent();
-  // The web-modal dialog should be open.
-  web_modal::WebContentsModalDialogManager* web_contents_modal_dialog_manager =
-      web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
-  EXPECT_TRUE(web_contents_modal_dialog_manager->IsDialogActive());
-
-  OpenOrderSummaryScreen();
-
-  EXPECT_EQ(u"$4.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  // There's the total line and the discount line.
-  EXPECT_EQ(2u, GetLineCount());
-}
-
-IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersTest,
-                       NoTotalInModifierDoesNotCrash) {
-  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
-  autofill::AutofillProfile profile(autofill::test::GetFullProfile());
-  AddAutofillProfile(profile);
-  autofill::CreditCard card(autofill::test::GetCreditCard());  // Visa card.
-  // Change to Mastercard to match the test case.
-  card.SetRawInfo(autofill::CREDIT_CARD_NUMBER, u"5555555555554444");
-  card.set_billing_address_id(profile.guid());
-  AddCreditCard(card);
-
-  ResetEventWaiterForDialogOpened();
-  content::WebContents* web_contents = GetActiveWebContents();
-  const std::string click_buy_button_js =
-      "(function() { document.getElementById('no_total').click(); })();";
-  ASSERT_TRUE(content::ExecuteScript(web_contents, click_buy_button_js));
-  WaitForObservedEvent();
-  // The web-modal dialog should be open.
-  web_modal::WebContentsModalDialogManager* web_contents_modal_dialog_manager =
-      web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
-  EXPECT_TRUE(web_contents_modal_dialog_manager->IsDialogActive());
-
-  OpenOrderSummaryScreen();
-
-  // The price is the global total, because the modifier does not have total.
-  EXPECT_EQ(u"$5.00",
-            GetLabelText(DialogViewID::ORDER_SUMMARY_TOTAL_AMOUNT_LABEL));
-  // Only global total is available.
-  EXPECT_EQ(1u, GetLineCount());
-}
-
-class PaymentRequestModifiersBasicCardDisabledTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestModifiersBasicCardDisabledTest(
-      const PaymentRequestModifiersBasicCardDisabledTest&) = delete;
-  PaymentRequestModifiersBasicCardDisabledTest& operator=(
-      const PaymentRequestModifiersBasicCardDisabledTest&) = delete;
-
- protected:
-  PaymentRequestModifiersBasicCardDisabledTest() = default;
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    PaymentRequestBrowserTestBase::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(
-        switches::kEnableExperimentalWebPlatformFeatures);
-  }
-
-  void SetUpInProcessBrowserTestFixture() override {
-    PaymentRequestBrowserTestBase::SetUpInProcessBrowserTestFixture();
-
-    feature_list_.InitWithFeatures({features::kWebPaymentsModifiers},
-                                   {::features::kPaymentRequestBasicCard});
-  }
-
-  size_t GetLineCount() {
-    auto* top = dialog_view()->view_stack_for_testing()->top();
-    const auto* content =
-        top->GetViewByID(static_cast<int>(DialogViewID::CONTENT_VIEW));
-    return content->children().size();
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersBasicCardDisabledTest,
                        NoModifierAppliedIfNoSelectedInstrument) {
   // We need to control the default-selected payment app in the UI, to know that
   // the modifiers should not apply. To achieve this, we install one of the apps
@@ -240,7 +65,7 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersBasicCardDisabledTest,
   EXPECT_EQ(1u, GetLineCount());
 }
 
-IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersBasicCardDisabledTest,
+IN_PROC_BROWSER_TEST_F(PaymentRequestModifiersTest,
                        NoTotalInModifierDoesNotCrash) {
   // The modifier without the total applies to the first method; to make sure
   // that it is always selected we install the second method without an icon.
