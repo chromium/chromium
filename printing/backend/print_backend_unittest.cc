6bda68163514fa1702157f145cba06b35ee3c0a2
Andy Phan <andyphan@chromium.org>
2022-07-19 02:56:34 +0000

    Refactor print_backend_unittest.cc
    Move local strings to global consts for reusability and readability.
    Combine ParseValueForXpsPrinterCapabilities and
    ParseCorrectPageOutputQualityForXpsPrinterCapabilities unit tests
    into one, because they are essentially the same.
    
    Change-Id: Ia288f303bad5b9cf41c40a9a0200f61a95d7ebfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3770044
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025542}

--- printing/backend/print_backend_unittest.cc
+++ printing/backend/print_backend_unittest.cc
@@ -12,76 +12,12 @@
 
 namespace printing {
 
-// PrintBackendTest makes use of a real print backend instance, and thus will
-// interact with printer drivers installed on a system.  This can be useful on
-// machines which a developer has control over the driver installations, but is
-// less useful on bots which are managed by the infra team.
-// These tests are intended to be run manually by developers using the
-// --run-manual flag.
-class PrintBackendTest : public testing::Test {
- public:
-  void SetUp() override {
-    print_backend_ = PrintBackend::CreateInstance(/*locale=*/"");
-  }
-
-  PrintBackend* GetPrintBackend() { return print_backend_.get(); }
-
- private:
-  scoped_refptr<PrintBackend> print_backend_;
-};
-
-// Check behavior of `EnumeratePrinters()`.  At least one of the tests
-// {EnumeratePrintersSomeInstalled, EnumeratePrintersNoneInstalled} should
-// fail, since a single machine can't have both some and no printers installed.
-// A developer running these manually can verify that the appropriate test is
-// passing for the given state of installed printer drivers on the system being
-// checked.
-TEST_F(PrintBackendTest, MANUAL_EnumeratePrintersSomeInstalled) {
-  PrinterList printer_list;
-
-  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
-            mojom::ResultCode::kSuccess);
-  EXPECT_FALSE(printer_list.empty());
-
-  DLOG(WARNING) << "Number of printers found: " << printer_list.size();
-  for (const auto& printer : printer_list) {
-    DLOG(WARNING) << "Found printer: `" << printer.printer_name << "`";
-  }
-}
-
-TEST_F(PrintBackendTest, MANUAL_EnumeratePrintersNoneInstalled) {
-  PrinterList printer_list;
-
-  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
-            mojom::ResultCode::kSuccess);
-  EXPECT_TRUE(printer_list.empty());
-}
+namespace {
 
 #if BUILDFLAG(IS_WIN)
-
-// This test is for the XPS API that read the XML capabilities of a
-// specific printer.
-TEST_F(PrintBackendTest, MANUAL_GetXmlPrinterCapabilitiesForXpsDriver) {
-  PrinterList printer_list;
-  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
-            mojom::ResultCode::kSuccess);
-  for (const auto& printer : printer_list) {
-    std::string capabilities;
-    EXPECT_EQ(GetPrintBackend()->GetXmlPrinterCapabilitiesForXpsDriver(
-                  printer.printer_name, capabilities),
-              mojom::ResultCode::kSuccess);
-  }
-}
-
-TEST_F(PrintBackendTest, ParseValueForXpsPrinterCapabilities) {
-  // Convert a JSON string into a base::Value.
-  // Since parsing XML data to base::Value can not be done here,
-  // use JSONReader to create a base::Value. The JSON string
-  // in this test is based on the XML data returned by
-  // `GetXmlPrinterCapabilitiesForXpsDriver` API and processed by data_decoder
-  // service.
-  // This is the correct format of XPS "PageOutputQuality" capability.
-  base::Value correct_capabilities = base::test::ParseJson(R"({
+// The correct format of XPS "PageOutputQuality" and "PageOutputColor"
+// capabilities.
+constexpr char kCorrectCapabilities[] = R"({
   "type": "element",
   "tag": "psf:PrintCapabilities",
   "children": [
@@ -225,188 +161,11 @@ TEST_F(PrintBackendTest, ParseValueForXpsPrinterCapabilities) {
     }
   ]
 }
-)");
-  PrinterSemanticCapsAndDefaults printer_info;
+)";
 
-  // Expect that parsing XPS Printer Capabilities is successful.
-  // After parsing, `printer_info` will have 2 capabilities: "PageOutputQuality"
-  // and "PageOutputColor".
-  EXPECT_EQ(GetPrintBackend()->ParseValueForXpsPrinterCapabilities(
-                correct_capabilities, &printer_info),
-            mojom::ResultCode::kSuccess);
-}
-
-TEST_F(PrintBackendTest,
-       ParseCorrectPageOutputQualityForXpsPrinterCapabilities) {
-  // This is the correct format of XPS "PageOutputQuality" capability.
-  base::Value correct_capabilities = base::test::ParseJson(R"({
-  "type": "element",
-  "tag": "psf:PrintCapabilities",
-  "children": [
-    {
-      "type": "element",
-      "tag": "psf:Feature",
-      "attributes": {
-        "name": "psk:PageOutputQuality"
-      },
-      "children": [
-        {
-          "type": "element",
-          "tag": "psf:Feature",
-          "attributes": {
-            "name": "psk:PageOutputQuality"
-          }
-        },
-        {
-          "type": "element",
-          "tag": "psf:Property",
-          "attributes": {
-            "name": "psf:SelectionType"
-          },
-          "children": [
-            {
-              "type": "element",
-              "tag": "psf:Value",
-              "attributes": {
-                "xsi:type": "xsd:QName"
-              },
-              "children": [
-                {
-                  "type": "text",
-                  "text": "psk:PickOne"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "type": "element",
-          "tag": "psf:Property",
-          "attributes": {
-            "name": "psf:DisplayName"
-          },
-          "children": [
-            {
-              "type": "element",
-              "tag": "psf:Value",
-              "attributes": {
-                "xsi:type": "xsd:string"
-              },
-              "children": [
-                {
-                  "type": "text",
-                  "text": "Quality"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "type": "element",
-          "tag": "psf:Option",
-          "attributes": {
-            "name": "ns0000:Draft",
-            "constrain": "psk:None"
-          },
-          "children": [
-            {
-              "type": "element",
-              "tag": "psf:Property",
-              "attributes": {
-                "name": "psf:DisplayName"
-              },
-              "children": [
-                {
-                  "type": "element",
-                  "tag": "psf:Value",
-                  "attributes": {
-                    "xsi:type": "xsd:string"
-                  },
-                  "children": [
-                    {
-                      "type": "text",
-                      "text": "Draft"
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "type": "element",
-          "tag": "psf:Option",
-          "attributes": {
-            "name": "ns0000:Advanced",
-            "constrain": "psk:None"
-          },
-          "children": [
-            {
-              "type": "element",
-              "tag": "psf:Property",
-              "attributes": {
-                "name": "psf:DisplayName"
-              },
-              "children": [
-                {
-                  "type": "element",
-                  "tag": "psf:Value",
-                  "attributes": {
-                    "xsi:type": "xsd:string"
-                  },
-                  "children": [
-                    {
-                      "type": "text",
-                      "text": "Advanced"
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "type": "element",
-          "tag": "psf:Option",
-          "attributes": {
-            "name": "psk:Normal"
-          }
-        }
-      ]
-    },
-    {
-      "type": "element",
-      "tag": "psf:Feature",
-      "attributes": {
-        "name": "psk:PageOutputColor"
-      }
-    }
-  ]
-}
-)");
-  PrinterSemanticCapsAndDefaults printer_info;
-
-  // Expect that parsing XPS Printer Capabilities is successful.
-  // After parsing, `printer_info` will have 2 capabilities: "PageOutputQuality"
-  // and "PageOutputColor".
-  EXPECT_EQ(GetPrintBackend()->ParseValueForXpsPrinterCapabilities(
-                correct_capabilities, &printer_info),
-            mojom::ResultCode::kSuccess);
-
-  // After parsing, `PageOutputQuality` of `printer_info` is expected to have 3
-  // options listed in `kPageOutputQualities`.
-  const PageOutputQualityAttributes kPageOutputQualities = {
-      PageOutputQualityAttribute("Draft", "ns0000:Draft"),
-      PageOutputQualityAttribute("Advanced", "ns0000:Advanced"),
-      PageOutputQualityAttribute("", "psk:Normal")};
-  EXPECT_EQ(printer_info.page_output_quality->qualities, kPageOutputQualities);
-}
-
-TEST_F(PrintBackendTest,
-       ParseIncorrectPageOutputQualityForXpsPrinterCapabilities) {
-  // This string below is incorrect format of XPS `PageOutputQuality` capability
-  // when the property inside option ns0000:Draft does not have any value.
-  base::Value incorrect_capabilities = base::test::ParseJson(R"({
+// The incorrect format of XPS `PageOutputQuality` capability.
+// The property inside option ns0000:Draft does not have any value.
+constexpr char kIncorrectCapabilities[] = R"({
   "type": "element",
   "tag": "psf:PrintCapabilities",
   "children": [
@@ -489,10 +248,95 @@ TEST_F(PrintBackendTest,
     }
   ]
 }
-)");
+)";
+
+const PageOutputQualityAttributes kPageOutputQualities = {
+    PageOutputQualityAttribute("Draft", "ns0000:Draft"),
+    PageOutputQualityAttribute("Advanced", "ns0000:Advanced"),
+    PageOutputQualityAttribute("", "psk:Normal")};
+#endif  // BUILDFLAG(IS_WIN)
+
+}  // namespace
+
+// PrintBackendTest makes use of a real print backend instance, and thus will
+// interact with printer drivers installed on a system.  This can be useful on
+// machines which a developer has control over the driver installations, but is
+// less useful on bots which are managed by the infra team.
+// These tests are intended to be run manually by developers using the
+// --run-manual flag.
+class PrintBackendTest : public testing::Test {
+ public:
+  void SetUp() override {
+    print_backend_ = PrintBackend::CreateInstance(/*locale=*/"");
+  }
+
+  PrintBackend* GetPrintBackend() { return print_backend_.get(); }
+
+ private:
+  scoped_refptr<PrintBackend> print_backend_;
+};
+
+// Check behavior of `EnumeratePrinters()`.  At least one of the tests
+// {EnumeratePrintersSomeInstalled, EnumeratePrintersNoneInstalled} should
+// fail, since a single machine can't have both some and no printers installed.
+// A developer running these manually can verify that the appropriate test is
+// passing for the given state of installed printer drivers on the system being
+// checked.
+TEST_F(PrintBackendTest, MANUAL_EnumeratePrintersSomeInstalled) {
+  PrinterList printer_list;
+
+  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
+            mojom::ResultCode::kSuccess);
+  EXPECT_FALSE(printer_list.empty());
+
+  DLOG(WARNING) << "Number of printers found: " << printer_list.size();
+  for (const auto& printer : printer_list) {
+    DLOG(WARNING) << "Found printer: `" << printer.printer_name << "`";
+  }
+}
+
+TEST_F(PrintBackendTest, MANUAL_EnumeratePrintersNoneInstalled) {
+  PrinterList printer_list;
+
+  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
+            mojom::ResultCode::kSuccess);
+  EXPECT_TRUE(printer_list.empty());
+}
+
+#if BUILDFLAG(IS_WIN)
+
+// This test is for the XPS API that read the XML capabilities of a
+// specific printer.
+TEST_F(PrintBackendTest, MANUAL_GetXmlPrinterCapabilitiesForXpsDriver) {
+  PrinterList printer_list;
+  EXPECT_EQ(GetPrintBackend()->EnumeratePrinters(printer_list),
+            mojom::ResultCode::kSuccess);
+  for (const auto& printer : printer_list) {
+    std::string capabilities;
+    EXPECT_EQ(GetPrintBackend()->GetXmlPrinterCapabilitiesForXpsDriver(
+                  printer.printer_name, capabilities),
+              mojom::ResultCode::kSuccess);
+  }
+}
+
+TEST_F(PrintBackendTest,
+       ParseCorrectPageOutputQualityForXpsPrinterCapabilities) {
+  PrinterSemanticCapsAndDefaults printer_info;
+
+  // Expect that parsing XPS Printer Capabilities is successful.
+  // After parsing, `printer_info` will have 2 capabilities: "PageOutputQuality"
+  // and "PageOutputColor".
+  EXPECT_EQ(GetPrintBackend()->ParseValueForXpsPrinterCapabilities(
+                base::test::ParseJson(kCorrectCapabilities), &printer_info),
+            mojom::ResultCode::kSuccess);
+  ASSERT_EQ(printer_info.page_output_quality->qualities, kPageOutputQualities);
+}
+
+TEST_F(PrintBackendTest,
+       ParseIncorrectPageOutputQualityForXpsPrinterCapabilities) {
   PrinterSemanticCapsAndDefaults printer_info;
   EXPECT_EQ(GetPrintBackend()->ParseValueForXpsPrinterCapabilities(
-                incorrect_capabilities, &printer_info),
+                base::test::ParseJson(kIncorrectCapabilities), &printer_info),
             mojom::ResultCode::kFailed);
 }
 
