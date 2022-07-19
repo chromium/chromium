168a27e54197910c8b4a0292935ab290559eb1f2
Stephen McGruer <smcgruer@chromium.org>
2022-07-19 16:09:12 +0000

    [basic-card] Remove payment_request_use_stats_browsertest.cc basic card tests
    Basic card is now fully deprecated and disabled, and we are starting to
    remove its code from the codebase. The first step is to clean up its
    usage in tests.
    
    Bug: 1209835, 938763, 1327722, 1328016
    Change-Id: I9eab352c0b76270ca8a3ecd619f3287b600af5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3767830
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1025758}

--- chrome/browser/ui/views/payments/payment_request_use_stats_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_request_use_stats_browsertest.cc
@@ -5,7 +5,6 @@
 #include <vector>
 
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -14,11 +13,9 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
-#include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/test_autofill_clock.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
-#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 
@@ -31,142 +28,11 @@ const base::Time kSomeLaterDate = base::Time::FromDoubleT(1497552271);
 
 }  // namespace
 
-class PaymentRequestAutofillInstrumentUseStatsTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestAutofillInstrumentUseStatsTest(
-      const PaymentRequestAutofillInstrumentUseStatsTest&) = delete;
-  PaymentRequestAutofillInstrumentUseStatsTest& operator=(
-      const PaymentRequestAutofillInstrumentUseStatsTest&) = delete;
-
- protected:
-  PaymentRequestAutofillInstrumentUseStatsTest() {}
-};
-
-// Tests that use stats for the autofill payment instrument used in a Payment
-// Request are properly updated upon completion.
-// TODO(crbug.com/938763): Flaky on Linux and Win10, at least
-IN_PROC_BROWSER_TEST_F(PaymentRequestAutofillInstrumentUseStatsTest,
-                       DISABLED_RecordUse) {
-  NavigateTo("/payment_request_no_shipping_test.html");
-  autofill::TestAutofillClock test_clock;
-  test_clock.SetNow(kSomeDate);
-
-  // Setup a credit card with an associated billing address.
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);  // Visa.
-
-  // Check that the initial use stats were set correctly.
-  autofill::CreditCard* initial_card =
-      GetDataManager()->GetCreditCardByGUID(card.guid());
-  EXPECT_EQ(1U, initial_card->use_count());
-  EXPECT_EQ(kSomeDate, initial_card->use_date());
-
-  // Complete the Payment Request.
-  test_clock.SetNow(kSomeLaterDate);
-  InvokePaymentRequestUI();
-  ResetEventWaiter(DialogEvent::DIALOG_CLOSED);
-  PayWithCreditCardAndWait(u"123");
-  WaitForOnPersonalDataChanged();
-
-  // Check that the usage of the card was recorded.
-  autofill::CreditCard* updated_card =
-      GetDataManager()->GetCreditCardByGUID(card.guid());
-  EXPECT_EQ(2U, updated_card->use_count());
-  EXPECT_EQ(kSomeLaterDate, updated_card->use_date());
-}
-
-class PaymentRequestShippingAddressUseStatsTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestShippingAddressUseStatsTest(
-      const PaymentRequestShippingAddressUseStatsTest&) = delete;
-  PaymentRequestShippingAddressUseStatsTest& operator=(
-      const PaymentRequestShippingAddressUseStatsTest&) = delete;
-
- protected:
-  PaymentRequestShippingAddressUseStatsTest() {
-    feature_list_.InitAndEnableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+using PaymentRequestShippingAddressUseStatsTest = PaymentRequestBrowserTestBase;
 
 // Tests that use stats for the shipping address used in a Payment Request are
 // properly updated upon completion.
-// TODO(crbug.com/1327722): Test is flaky.
-IN_PROC_BROWSER_TEST_F(PaymentRequestShippingAddressUseStatsTest,
-                       DISABLED_RecordUse) {
-  NavigateTo("/payment_request_free_shipping_test.html");
-  autofill::TestAutofillClock test_clock;
-  test_clock.SetNow(kSomeDate);
-
-  // Create a billing address and a card that uses it.
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);  // Visa.
-
-  // Create a shipping address with a higher frecency score, so that it is
-  // selected as the default shipping address.
-  autofill::AutofillProfile shipping_address =
-      autofill::test::GetFullProfile2();
-  shipping_address.set_use_count(3);
-  AddAutofillProfile(shipping_address);
-
-  // Check that the initial use stats were set correctly.
-  autofill::AutofillProfile* initial_shipping =
-      GetDataManager()->GetProfileByGUID(shipping_address.guid());
-  EXPECT_EQ(3U, initial_shipping->use_count());
-  EXPECT_EQ(kSomeDate, initial_shipping->use_date());
-
-  // Complete the Payment Request.
-  test_clock.SetNow(kSomeLaterDate);
-  InvokePaymentRequestUI();
-  ResetEventWaiter(DialogEvent::DIALOG_CLOSED);
-  PayWithCreditCardAndWait(u"123");
-  WaitForOnPersonalDataChanged();
-
-  // Check that the usage of the profile was recorded.
-  autofill::AutofillProfile* updated_shipping =
-      GetDataManager()->GetProfileByGUID(shipping_address.guid());
-  EXPECT_EQ(4U, updated_shipping->use_count());
-  EXPECT_EQ(kSomeLaterDate, updated_shipping->use_date());
-}
-
-// The tests in this class correspond to the tests of the same name in
-// PaymentRequestShippingAddressUseStatsTest, but with basic-card disabled.
-// Parameterized tests are not used because the test setup for both tests are
-// too different.
-class PaymentRequestShippingAddressUseStatsBasicCardDisabledTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestShippingAddressUseStatsBasicCardDisabledTest(
-      const PaymentRequestShippingAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-  PaymentRequestShippingAddressUseStatsBasicCardDisabledTest& operator=(
-      const PaymentRequestShippingAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-
- protected:
-  PaymentRequestShippingAddressUseStatsBasicCardDisabledTest() {
-    feature_list_.InitAndDisableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-// Tests that use stats for the shipping address used in a Payment Request are
-// properly updated upon completion.
-IN_PROC_BROWSER_TEST_F(
-    PaymentRequestShippingAddressUseStatsBasicCardDisabledTest,
-    RecordUse) {
+IN_PROC_BROWSER_TEST_F(PaymentRequestShippingAddressUseStatsTest, RecordUse) {
   std::string payment_method_name;
   InstallPaymentApp("a.com", "payment_request_success_responder.js",
                     &payment_method_name);
@@ -207,98 +73,11 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_EQ(kSomeLaterDate, updated_shipping->use_date());
 }
 
-class PaymentRequestContactAddressUseStatsTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestContactAddressUseStatsTest(
-      const PaymentRequestContactAddressUseStatsTest&) = delete;
-  PaymentRequestContactAddressUseStatsTest& operator=(
-      const PaymentRequestContactAddressUseStatsTest&) = delete;
-
- protected:
-  PaymentRequestContactAddressUseStatsTest() {
-    feature_list_.InitAndEnableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+using PaymentRequestContactAddressUseStatsTest = PaymentRequestBrowserTestBase;
 
 // Tests that use stats for the contact address used in a Payment Request are
 // properly updated upon completion.
-// TODO(crbug.com/1328016): Flaky on Linux, investigate and re-enable.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_RecordUse DISABLED_RecordUse
-#else
-#define MAYBE_RecordUse RecordUse
-#endif
-IN_PROC_BROWSER_TEST_F(PaymentRequestContactAddressUseStatsTest,
-                       MAYBE_RecordUse) {
-  NavigateTo("/payment_request_name_test.html");
-  autofill::TestAutofillClock test_clock;
-  test_clock.SetNow(kSomeDate);
-
-  // Setup a credit card with an associated billing address.
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);  // Visa.
-
-  // Create a contact address with a higher frecency score, so that it is
-  // selected as the default contact address.
-  autofill::AutofillProfile contact_address = autofill::test::GetFullProfile2();
-  contact_address.set_use_count(3);
-  AddAutofillProfile(contact_address);
-
-  // Check that the initial use stats were set correctly.
-  autofill::AutofillProfile* initial_contact =
-      GetDataManager()->GetProfileByGUID(contact_address.guid());
-  EXPECT_EQ(3U, initial_contact->use_count());
-  EXPECT_EQ(kSomeDate, initial_contact->use_date());
-
-  // Complete the Payment Request.
-  test_clock.SetNow(kSomeLaterDate);
-  InvokePaymentRequestUI();
-  ResetEventWaiter(DialogEvent::DIALOG_CLOSED);
-  PayWithCreditCardAndWait(u"123");
-  WaitForOnPersonalDataChanged();
-
-  // Check that the usage of the profile was recorded.
-  autofill::AutofillProfile* updated_contact =
-      GetDataManager()->GetProfileByGUID(contact_address.guid());
-  EXPECT_EQ(4U, updated_contact->use_count());
-  EXPECT_EQ(kSomeLaterDate, updated_contact->use_date());
-}
-
-// The tests in this class correspond to the tests of the same name in
-// PaymentRequestContactAddressUseStatsTest, but with basic-card disabled.
-// Parameterized tests are not used because the test setup for both tests are
-// too different.
-class PaymentRequestContactAddressUseStatsBasicCardDisabledTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestContactAddressUseStatsBasicCardDisabledTest(
-      const PaymentRequestContactAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-  PaymentRequestContactAddressUseStatsBasicCardDisabledTest& operator=(
-      const PaymentRequestContactAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-
- protected:
-  PaymentRequestContactAddressUseStatsBasicCardDisabledTest() {
-    feature_list_.InitAndDisableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-// Tests that use stats for the contact address used in a Payment Request are
-// properly updated upon completion.
-IN_PROC_BROWSER_TEST_F(
-    PaymentRequestContactAddressUseStatsBasicCardDisabledTest,
-    RecordUse) {
+IN_PROC_BROWSER_TEST_F(PaymentRequestContactAddressUseStatsTest, RecordUse) {
   std::string payment_method_name;
   InstallPaymentApp("a.com", "payment_request_success_responder.js",
                     &payment_method_name);
@@ -338,96 +117,13 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_EQ(kSomeLaterDate, updated_contact->use_date());
 }
 
-class PaymentRequestSameShippingAndContactAddressUseStatsTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestSameShippingAndContactAddressUseStatsTest(
-      const PaymentRequestSameShippingAndContactAddressUseStatsTest&) = delete;
-  PaymentRequestSameShippingAndContactAddressUseStatsTest& operator=(
-      const PaymentRequestSameShippingAndContactAddressUseStatsTest&) = delete;
-
- protected:
-  PaymentRequestSameShippingAndContactAddressUseStatsTest() {
-    feature_list_.InitAndEnableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+using PaymentRequestSameShippingAndContactAddressUseStatsTest =
+    PaymentRequestBrowserTestBase;
 
 // Tests that use stats for an address that was used both as a shipping and
 // contact address in a Payment Request are properly updated upon completion.
-// TODO(crbug.com/1328016): Flaky on Linux, investigate and re-enable.
-// MAYBE_RecordUse defined above (PaymentRequestContactAddressUseStatsTest
-// fixture).
 IN_PROC_BROWSER_TEST_F(PaymentRequestSameShippingAndContactAddressUseStatsTest,
-                       MAYBE_RecordUse) {
-  NavigateTo("/payment_request_contact_details_and_free_shipping_test.html");
-  autofill::TestAutofillClock test_clock;
-  test_clock.SetNow(kSomeDate);
-
-  // Setup a credit card with an associated billing address.
-  autofill::AutofillProfile billing_address = autofill::test::GetFullProfile();
-  AddAutofillProfile(billing_address);
-  autofill::CreditCard card = autofill::test::GetCreditCard();
-  card.set_billing_address_id(billing_address.guid());
-  AddCreditCard(card);  // Visa.
-
-  // Create an address with a higher frecency score, so that it is selected as
-  // the default shipping and contact address.
-  autofill::AutofillProfile multi_address = autofill::test::GetFullProfile2();
-  multi_address.set_use_count(3);
-  AddAutofillProfile(multi_address);
-
-  // Check that the initial use stats were set correctly.
-  autofill::AutofillProfile* initial_multi =
-      GetDataManager()->GetProfileByGUID(multi_address.guid());
-  EXPECT_EQ(3U, initial_multi->use_count());
-  EXPECT_EQ(kSomeDate, initial_multi->use_date());
-
-  // Complete the Payment Request.
-  test_clock.SetNow(kSomeLaterDate);
-  InvokePaymentRequestUI();
-  ResetEventWaiter(DialogEvent::DIALOG_CLOSED);
-  PayWithCreditCardAndWait(u"123");
-  WaitForOnPersonalDataChanged();
-
-  // Check that the usage of the profile was only recorded once.
-  autofill::AutofillProfile* updated_multi =
-      GetDataManager()->GetProfileByGUID(multi_address.guid());
-  EXPECT_EQ(4U, updated_multi->use_count());
-  EXPECT_EQ(kSomeLaterDate, updated_multi->use_date());
-}
-
-// The tests in this class correspond to the tests of the same name in
-// PaymentRequestSameShippingAndContactAddressUseStatsTest, but with basic-card
-// disabled. Parameterized tests are not used because the test setup for both
-// tests are too different.
-class PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest
-    : public PaymentRequestBrowserTestBase {
- public:
-  PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest(
-      const PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-  PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest&
-  operator=(
-      const PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest&) =
-      delete;
-
- protected:
-  PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest() {
-    feature_list_.InitAndDisableFeature(::features::kPaymentRequestBasicCard);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-// Tests that use stats for an address that was used both as a shipping and
-// contact address in a Payment Request are properly updated upon completion.
-IN_PROC_BROWSER_TEST_F(
-    PaymentRequestSameShippingAndContactAddressUseStatsBasicCardDisabledTest,
-    RecordUse) {
+                       RecordUse) {
   std::string payment_method_name;
   InstallPaymentApp("a.com", "payment_request_success_responder.js",
                     &payment_method_name);
