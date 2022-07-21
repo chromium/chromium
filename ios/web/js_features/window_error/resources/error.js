a29a04c850e787cb63c01f3a0a72f60c1b05dd17
Yi Gu <yigu@chromium.org>
2022-07-21 19:53:15 +0000

    Revert "Convert error.js to TypeScript"
    This reverts commit 63720ec1bb2e02b83812325c16b53d8076f59daf.
    
    Reason for revert: Tree closure: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/251262/overview
    
    Original change's description:
    > Convert error.js to TypeScript
    >
    > To improve compile time error reporting, reduce unforeseen breakages,
    > and modernize the codebase's scripts, error.js has been converted
    > to TypeScript.
    >
    > Bug: 1325547
    > Change-Id: Ia05e5b33480fc7a10061a8ce14a24554da3950fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781042
    > Commit-Queue: Daniel White <danieltwhite@google.com>
    > Reviewed-by: Ali Juma <ajuma@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1026911}
    
    Bug: 1325547
    Change-Id: I9927664251fd88e54daf867d725cb392880bc897
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781427
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026921}

--- ios/web/js_features/window_error/resources/error.js
+++ ios/web/js_features/window_error/resources/error.js
@@ -6,19 +6,16 @@
  * @fileoverview Error listener to report error details to the native app.
  */
 
-import {sendWebKitMessage} from '//ios/web/public/js_messaging/resources/utils.js'
+// Requires functions from common.js
 
 /**
  * JavaScript errors are logged on the main application side. The handler is
  * added ASAP to catch any errors in startup.
  */
-function errorEventHandler(event: ErrorEvent): void {
-  sendWebKitMessage('WindowErrorResultHandler',
+window.addEventListener('error', function(event) {
+  __gCrWeb.common.sendWebKitMessage('WindowErrorResultHandler',
       {'filename' : event.filename,
        'line_number' : event.lineno,
        'message': event.message.toString()
       });
-}
-
-
-window.addEventListener('error', errorEventHandler);
+});
