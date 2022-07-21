55c5f78cf718f026dd8f1e77ab91fc4fb6b1956a
Peter Kasting <pkasting@chromium.org>
2022-07-21 20:45:39 +0000

    Fix narrowing warnings on Cast Linux ARM64.
    Bug: 1292951
    Change-Id: I3adfd18b5316c7df61661161e35029e3206f54be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781169
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026946}

--- base/allocator/partition_allocator/page_allocator_constants.h
+++ base/allocator/partition_allocator/page_allocator_constants.h
@@ -42,8 +42,8 @@ namespace partition_alloc::internal {
 // Use PageAllocationGranularity(), PageAllocationGranularityShift()
 // to initialize and retrieve these values safely.
 struct PageCharacteristics {
-  std::atomic<int> size;
-  std::atomic<int> shift;
+  std::atomic<size_t> size;
+  std::atomic<size_t> shift;
 };
 extern PageCharacteristics page_characteristics;
 
@@ -82,9 +82,10 @@ PageAllocationGranularityShift() {
 #elif BUILDFLAG(IS_LINUX) && defined(ARCH_CPU_ARM64)
   // arm64 supports 4kb (shift = 12), 16kb (shift = 14), and 64kb (shift = 16)
   // page sizes. Retrieve from or initialize cache.
-  int shift = page_characteristics.shift.load(std::memory_order_relaxed);
+  size_t shift = page_characteristics.shift.load(std::memory_order_relaxed);
   if (PA_UNLIKELY(shift == 0)) {
-    shift = __builtin_ctz((int)PageAllocationGranularity());
+    shift = static_cast<size_t>(
+        __builtin_ctz((unsigned int)PageAllocationGranularity()));
     page_characteristics.shift.store(shift, std::memory_order_relaxed);
   }
   return shift;
@@ -102,9 +103,9 @@ PageAllocationGranularity() {
 #elif BUILDFLAG(IS_LINUX) && defined(ARCH_CPU_ARM64)
   // arm64 supports 4kb, 16kb, and 64kb page sizes. Retrieve from or
   // initialize cache.
-  int size = page_characteristics.size.load(std::memory_order_relaxed);
+  size_t size = page_characteristics.size.load(std::memory_order_relaxed);
   if (PA_UNLIKELY(size == 0)) {
-    size = getpagesize();
+    size = static_cast<size_t>(getpagesize());
     page_characteristics.size.store(size, std::memory_order_relaxed);
   }
   return size;
