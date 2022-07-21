c64226e698d97f5d52e3c01555f1775051dbba24
Lukasz Anforowicz <lukasza@chromium.org>
2022-07-21 19:51:14 +0000

    [rust] rs_bindings_from_cc.gni: Shortening cmdlines via `--flagfile`.
    Crubit parses its cmdline flags using Abseil which supports writing
    cmdline flags into a file (one flag per line) and then invoking the
    executable with `--flagfile=<filepath>`.  This helps reduce the concerns
    about running into platform-specific cmdline length limitations.
    
    Bug: 1329611
    Change-Id: I97bed563ca280a3acceeecf90f5b66961f1dea43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781283
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026918}

--- build/rust/run_rs_bindings_from_cc.py
+++ build/rust/run_rs_bindings_from_cc.py
@@ -12,7 +12,7 @@ import sys
 
 # Set up path to be able to import build_utils.
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-CHROMIUM_SRC_DIR = os.path.join(THIS_DIR, os.pardir, os.pardir)
+CHROMIUM_SRC_DIR = os.path.relpath(os.path.join(THIS_DIR, os.pardir, os.pardir))
 sys.path.append(THIS_DIR)
 sys.path.append(os.path.join(CHROMIUM_SRC_DIR, 'build', 'android', 'gyp'))
 from run_bindgen import filter_clang_args
@@ -59,20 +59,17 @@ def main():
                       nargs=argparse.REMAINDER)
   args = parser.parse_args()
 
-  # Start building the cmdline args to pass to the `rs_bindings_from_cc` tool.
-  genargs = [RS_BINDINGS_FROM_CC_EXE_PATH]
-
   # Output paths
-  genargs.extend(["--rs_out", args.rs_out])
-  genargs.extend(["--cc_out", args.cc_out])
+  generator_args = []
+  generator_args.append("--rs_out={0}".format(os.path.relpath(args.rs_out)))
+  generator_args.append("--cc_out={0}".format(os.path.relpath(args.cc_out)))
   if "CRUBIT_DEBUG" in os.environ:
-    genargs.extend(["--ir_out", args.rs_out.replace(".rs", ".ir")])
+    generator_args.append("--ir_out={0}".format(
+        os.path.relpath(args.rs_out).replace(".rs", ".ir")))
 
   # Public headers.
-  genargs.extend([
-      "--public_headers",
-      ",".join([os.path.relpath(hdr) for hdr in args.public_headers.split(",")])
-  ])
+  generator_args.append("--public_headers={0}".format(",".join(
+      [os.path.relpath(hdr) for hdr in args.public_headers.split(",")])))
 
   # Targets to headers map.
   with open(args.targets_and_headers_from_gn, "r") as f:
@@ -81,38 +78,52 @@ def main():
     hdrs = entry["h"]
     for i in range(len(hdrs)):
       hdrs[i] = os.path.relpath(hdrs[i])
-  genargs.extend(["--targets_and_headers", json.dumps(targets_and_headers)])
+  generator_args.append("--targets_and_headers={0}".format(
+      json.dumps(targets_and_headers)))
 
   # All Crubit invocations in Chromium share the following cmdline args.
-  genargs.extend(["--rustfmt_exe_path", RUSTFMT_EXE_PATH])
-  genargs.extend(["--rustfmt_config_path", RUSTFMT_CONFIG_PATH])
-  genargs.extend([
-      "--crubit_support_path",
-      "third_party/crubit/src/rs_bindings_from_cc/support"
-  ])
+  generator_args.append(f"--rustfmt_exe_path={RUSTFMT_EXE_PATH}")
+  generator_args.append(f"--rustfmt_config_path={RUSTFMT_CONFIG_PATH}")
+  generator_args.append(
+      "--crubit_support_path=third_party/crubit/src/rs_bindings_from_cc/support"
+  )
+
+  # Long cmdlines may not work - work around that by using Abseil's `--flagfile`
+  # https://abseil.io/docs/python/guides/flags#a-note-about---flagfile
+  #
+  # Note that `clang_args` are not written to the flag file, because Abseil's
+  # flag parsing code is only aware of `ABSL_FLAG`-declared flags and doesn't
+  # know about Clang args (e.g. `-W...` or `-I...`).
+  params_file_path = os.path.relpath(args.rs_out).replace(".rs", ".params")
+  with open(params_file_path, "w") as f:
+    for line in generator_args:
+      print(line, file=f)
 
   # Clang arguments.
   #
   # The call to `filter_clang_args` is needed to avoid the following error:
   # error: unable to find plugin 'find-bad-constructs'
-  genargs.extend(filter_clang_args(args.clang_args))
+  clang_args = []
+  clang_args.extend(filter_clang_args(args.clang_args))
   # TODO(crbug.com/1329611): This warning needs to be suppressed, because
   # otherwise Crubit/Clang complains as follows:
   #     error: .../third_party/rust-toolchain/bin/rs_bindings_from_cc:
   #     'linker' input unused [-Werror,-Wunused-command-line-argument]
   # Maybe `build/rust/rs_bindings_from_cc.gni` gives too much in `args`?  But
   # then `{{cflags}}` seems perfectly reasonable...
-  genargs += ["-Wno-unused-command-line-argument"]
+  clang_args += ["-Wno-unused-command-line-argument"]
 
   # Print a copy&pastable final cmdline when asked for debugging help.
+  cmdline = [RS_BINDINGS_FROM_CC_EXE_PATH, f"--flagfile={params_file_path}"]
+  cmdline.extend(clang_args)
   if "CRUBIT_DEBUG" in os.environ:
-    pretty_cmdline = format_cmdline(genargs)
+    pretty_cmdline = format_cmdline(cmdline)
     print(f"CRUBIT_DEBUG: CMDLINE: {pretty_cmdline}", file=sys.stderr)
 
   # TODO(crbug.com/1329611): run_bindgen.py removes the outputs when the tool
   # fails.  Maybe we need to do something similar here?  OTOH in most failure
   # modes Crubit will fail *before* generating its outputs...
-  return subprocess.run(genargs).returncode
+  return subprocess.run(cmdline).returncode
 
 
 if __name__ == '__main__':
