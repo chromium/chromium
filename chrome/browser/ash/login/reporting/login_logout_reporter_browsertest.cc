385d0d4516d5ee3a716292743dc807df00e33735
Ahmed Nasr <anasr@google.com>
2022-07-21 16:21:23 +0000

    Add browser test for Managed Guest (public session) login reporting.
    Bug: b:237811826
    Change-Id: Ia5625add6d465d0fad777ba3d0f4d42f65f23628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777671
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1026807}

--- chrome/browser/ash/login/reporting/login_logout_reporter_browsertest.cc
+++ chrome/browser/ash/login/reporting/login_logout_reporter_browsertest.cc
@@ -19,16 +19,24 @@
 #include "chrome/browser/ash/login/test/fake_gaia_mixin.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
+#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
+#include "chrome/browser/ash/policy/core/device_local_account.h"
+#include "chrome/browser/ash/policy/core/device_local_account_policy_service.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
 #include "chrome/browser/policy/messaging_layer/proto/synced/login_logout_event.pb.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/ash/components/dbus/session_manager/fake_session_manager_client.h"
 #include "chromeos/dbus/missive/missive_client.h"
 #include "chromeos/dbus/missive/missive_client_test_observer.h"
 #include "components/account_id/account_id.h"
+#include "components/policy/core/common/cloud/cloud_policy_store.h"
+#include "components/policy/proto/chrome_device_policy.pb.h"
+#include "components/policy/proto/device_management_backend.pb.h"
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/proto/synced/record_constants.pb.h"
+#include "components/user_manager/user_manager.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -36,27 +44,89 @@
 
 using chromeos::MissiveClient;
 using chromeos::MissiveClientTestObserver;
+using enterprise_management::ChromeDeviceSettingsProto;
+using enterprise_management::DeviceLocalAccountInfoProto;
 using reporting::Destination;
 using reporting::Priority;
 using reporting::Record;
 using testing::Eq;
+using testing::InvokeWithoutArgs;
 using testing::SizeIs;
 
 namespace ash::reporting {
+namespace {
+
+constexpr char kPublicSessionUserEmail[] = "public_session_user@localhost";
+
+Record GetNextLoginLogoutRecord(MissiveClientTestObserver* observer) {
+  std::tuple<Priority, Record> enqueued_record =
+      observer->GetNextEnqueuedRecord();
+  Priority priority = std::get<0>(enqueued_record);
+  Record record = std::get<1>(enqueued_record);
+
+  EXPECT_THAT(priority, Eq(Priority::SECURITY));
+  return record;
+}
+
+absl::optional<Record> MaybeGetEnqueudLoginLogoutRecord() {
+  const std::vector<Record>& records =
+      MissiveClient::Get()->GetTestInterface()->GetEnqueuedRecords(
+          Priority::SECURITY);
+  for (const Record& record : records) {
+    if (record.destination() == Destination::LOGIN_LOGOUT_EVENTS) {
+      return record;
+    }
+  }
+  return absl::nullopt;
+}
+
+class PublicSessionUserCreationWaiter
+    : public user_manager::UserManager::Observer {
+ public:
+  PublicSessionUserCreationWaiter() = default;
+
+  PublicSessionUserCreationWaiter(const PublicSessionUserCreationWaiter&) =
+      delete;
+  PublicSessionUserCreationWaiter& operator=(
+      const PublicSessionUserCreationWaiter&) = delete;
+
+  ~PublicSessionUserCreationWaiter() override = default;
+
+  void Wait(const AccountId& public_session_account_id) {
+    if (user_manager::UserManager::Get()->IsKnownUser(
+            public_session_account_id)) {
+      return;
+    }
+
+    local_state_changed_run_loop_ = std::make_unique<base::RunLoop>();
+    user_manager::UserManager::Get()->AddObserver(this);
+    local_state_changed_run_loop_->Run();
+    user_manager::UserManager::Get()->RemoveObserver(this);
+  }
+
+  // user_manager::UserManager::Observer:
+  void LocalStateChanged(user_manager::UserManager* user_manager) override {
+    local_state_changed_run_loop_->Quit();
+  }
+
+ private:
+  std::unique_ptr<base::RunLoop> local_state_changed_run_loop_;
+};
 
 class LoginLogoutReporterBrowserTest
     : public policy::DevicePolicyCrosBrowserTest {
  public:
+  LoginLogoutReporterBrowserTest(const LoginLogoutReporterBrowserTest&) =
+      delete;
+  LoginLogoutReporterBrowserTest& operator=(
+      const LoginLogoutReporterBrowserTest&) = delete;
+
+ protected:
   LoginLogoutReporterBrowserTest() {
     login_manager_.set_session_restore_enabled();
     scoped_testing_cros_settings_.device_settings()->SetBoolean(
         kReportDeviceLoginLogout, true);
   }
-
-  LoginLogoutReporterBrowserTest(const LoginLogoutReporterBrowserTest&) =
-      delete;
-  LoginLogoutReporterBrowserTest& operator=(
-      const LoginLogoutReporterBrowserTest&) = delete;
 
   ~LoginLogoutReporterBrowserTest() override = default;
 
@@ -83,29 +153,6 @@ class LoginLogoutReporterBrowserTest
     const std::string& password = user_context.GetKey()->GetSecret();
     LoginScreenTestApi::SubmitPassword(test_user_.account_id, password,
                                        /*check_if_submittable=*/true);
-  }
-
- protected:
-  Record GetNextLoginLogoutRecord(MissiveClientTestObserver* observer) {
-    std::tuple<Priority, Record> enqueued_record =
-        observer->GetNextEnqueuedRecord();
-    Priority priority = std::get<0>(enqueued_record);
-    Record record = std::get<1>(enqueued_record);
-
-    EXPECT_THAT(priority, Eq(Priority::SECURITY));
-    return record;
-  }
-
-  absl::optional<Record> MaybeGetEnqueudLoginLogoutRecord() {
-    const std::vector<Record>& records =
-        MissiveClient::Get()->GetTestInterface()->GetEnqueuedRecords(
-            Priority::SECURITY);
-    for (const Record& record : records) {
-      if (record.destination() == Destination::LOGIN_LOGOUT_EVENTS) {
-        return record;
-      }
-    }
-    return absl::nullopt;
   }
 
   const LoginManagerMixin::TestUserInfo test_user_{
@@ -200,4 +247,106 @@ IN_PROC_BROWSER_TEST_F(LoginLogoutReporterBrowserTest, GuestLogin) {
   EXPECT_FALSE(login_record_data.login_event().has_failure());
 }
 
+class LoginLogoutReporterPublicSessionBrowserTest
+    : public policy::DevicePolicyCrosBrowserTest {
+ public:
+  LoginLogoutReporterPublicSessionBrowserTest(
+      const LoginLogoutReporterPublicSessionBrowserTest&) = delete;
+  LoginLogoutReporterPublicSessionBrowserTest& operator=(
+      const LoginLogoutReporterPublicSessionBrowserTest&) = delete;
+
+ protected:
+  LoginLogoutReporterPublicSessionBrowserTest() = default;
+  ~LoginLogoutReporterPublicSessionBrowserTest() override = default;
+
+  void SetUpOnMainThread() override {
+    policy::DevicePolicyCrosBrowserTest::SetUpOnMainThread();
+
+    // Wait for the public session user to be created.
+    PublicSessionUserCreationWaiter public_session_waiter;
+    public_session_waiter.Wait(public_session_account_id_);
+    EXPECT_TRUE(user_manager::UserManager::Get()->IsKnownUser(
+        public_session_account_id_));
+
+    // Wait for the device local account policy to be installed.
+    policy::CloudPolicyStore* const store =
+        TestingBrowserProcess::GetGlobal()
+            ->platform_part()
+            ->browser_policy_connector_ash()
+            ->GetDeviceLocalAccountPolicyService()
+            ->GetBrokerForUser(public_session_account_id_.GetUserEmail())
+            ->core()
+            ->store();
+    if (!store->has_policy()) {
+      policy::MockCloudPolicyStoreObserver observer;
+
+      base::RunLoop loop;
+      store->AddObserver(&observer);
+      EXPECT_CALL(observer, OnStoreLoaded(store))
+          .Times(1)
+          .WillOnce(InvokeWithoutArgs(&loop, &base::RunLoop::Quit));
+      loop.Run();
+      store->RemoveObserver(&observer);
+    }
+  }
+
+  void SetUpInProcessBrowserTestFixture() override {
+    policy::DevicePolicyCrosBrowserTest::SetUpInProcessBrowserTestFixture();
+
+    // Setup the device policy.
+    ChromeDeviceSettingsProto& proto(device_policy()->payload());
+    DeviceLocalAccountInfoProto* account =
+        proto.mutable_device_local_accounts()->add_account();
+    account->set_account_id(kPublicSessionUserEmail);
+    account->set_type(DeviceLocalAccountInfoProto::ACCOUNT_TYPE_PUBLIC_SESSION);
+    // Enable login/logout reporting.
+    proto.mutable_device_reporting()->set_report_login_logout(true);
+    RefreshDevicePolicy();
+
+    // Setup the device local account policy.
+    policy::UserPolicyBuilder device_local_account_policy;
+    device_local_account_policy.policy_data().set_username(
+        kPublicSessionUserEmail);
+    device_local_account_policy.policy_data().set_policy_type(
+        policy::dm_protocol::kChromePublicAccountPolicyType);
+    device_local_account_policy.policy_data().set_settings_entity_id(
+        kPublicSessionUserEmail);
+    device_local_account_policy.Build();
+    session_manager_client()->set_device_local_account_policy(
+        kPublicSessionUserEmail, device_local_account_policy.GetBlob());
+  }
+
+  const AccountId public_session_account_id_ =
+      AccountId::FromUserEmail(policy::GenerateDeviceLocalAccountUserId(
+          kPublicSessionUserEmail,
+          policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION));
+
+  LoginManagerMixin login_manager_{&mixin_host_, {}};
+};
+
+IN_PROC_BROWSER_TEST_F(LoginLogoutReporterPublicSessionBrowserTest,
+                       LoginSuccessful) {
+  MissiveClientTestObserver observer(Destination::LOGIN_LOGOUT_EVENTS);
+
+  ASSERT_TRUE(
+      LoginScreenTestApi::ExpandPublicSessionPod(public_session_account_id_));
+  LoginScreenTestApi::ClickPublicExpandedSubmitButton();
+  test::WaitForPrimaryUserSessionStart();
+  base::RunLoop().RunUntilIdle();
+
+  user_manager::UserManager* user_manager = user_manager::UserManager::Get();
+  ASSERT_TRUE(user_manager->IsLoggedInAsPublicAccount());
+
+  Record login_record = GetNextLoginLogoutRecord(&observer);
+
+  LoginLogoutRecord login_record_data;
+  ASSERT_TRUE(login_record_data.ParseFromString(login_record.data()));
+  EXPECT_THAT(login_record_data.session_type(),
+              Eq(LoginLogoutSessionType::PUBLIC_ACCOUNT_SESSION));
+  EXPECT_FALSE(login_record_data.has_affiliated_user());
+  ASSERT_TRUE(login_record_data.has_login_event());
+  EXPECT_FALSE(login_record_data.login_event().has_failure());
+}
+
+}  // namespace
 }  // namespace ash::reporting
