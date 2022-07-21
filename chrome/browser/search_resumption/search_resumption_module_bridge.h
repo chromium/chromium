b973ad11d0836336d90922adaa26b47c0725abfe
Xi Han <hanxi@google.com>
2022-07-21 16:23:01 +0000

    [SearchResumption] Add Factory and bridge for StartSuggestService.
    In this CL, we add the factory for StartSuggestService. A bridge class
    is also added to manage calls between C++ and Java.
    
    Bug: 1332596
    Change-Id: I5d32289227c226186082ca0c31bf16b5b629ff1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778245
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026808}

---
+++ chrome/browser/search_resumption/search_resumption_module_bridge.h
@@ -0,0 +1,47 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SEARCH_RESUMPTION_SEARCH_RESUMPTION_MODULE_BRIDGE_H_
+#define CHROME_BROWSER_SEARCH_RESUMPTION_SEARCH_RESUMPTION_MODULE_BRIDGE_H_
+
+#include <jni.h>
+#include <vector>
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/memory/weak_ptr.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+#include "components/search/start_suggest_service.h"
+
+class Profile;
+
+namespace search_resumption_module {
+// Bridge between the C++ and the Java for fetching suggestions in the search
+// resumption module on NTP.
+class SearchResumptionModuleBridge {
+ public:
+  SearchResumptionModuleBridge(JNIEnv* env, jobject obj, Profile* profile);
+  SearchResumptionModuleBridge(const SearchResumptionModuleBridge&) = delete;
+  SearchResumptionModuleBridge& operator=(const SearchResumptionModuleBridge&) =
+      delete;
+
+  void Destroy(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj);
+
+  void FetchSuggestions(JNIEnv* env,
+                        const base::android::JavaParamRef<jobject>& obj,
+                        const base::android::JavaParamRef<jstring>& j_page_url);
+
+ private:
+  ~SearchResumptionModuleBridge();
+  void OnSuggestionsReceived(std::vector<QuerySuggestion> suggestions);
+
+  StartSuggestService* start_suggest_service_;
+  base::android::ScopedJavaGlobalRef<jobject> java_object_;
+
+  const base::WeakPtrFactory<SearchResumptionModuleBridge> weak_ptr_factory_{
+      this};
+};
+
+}  // namespace search_resumption_module
+#endif  // CHROME_BROWSER_SEARCH_RESUMPTION_SEARCH_RESUMPTION_MODULE_BRIDGE_H_
