b824e59bdf4a62d6c3c030dd9ed53567b83406f0
Fumitoshi Ukai <ukai@google.com>
2022-07-21 05:30:57 +0000

    fix fetch_reclient_cfgs.py for windows
    on windows dev machine, cipd is depot_tools/cipd.bat,
    but it fails with
     FileNotFoundError: [WinError 2] The system cannot find the file specified.
    
    on windows builders, it works as cipd.exe can be found in %PATH%
    
    Use shell=True to work on both use case.
    
    Bug: 1346179
    Change-Id: Iabf092ebcc4781d1a3c1801caf465b76cc00fd14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3778907
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026625}

--- buildtools/reclient_cfgs/fetch_reclient_cfgs.py
+++ buildtools/reclient_cfgs/fetch_reclient_cfgs.py
@@ -37,6 +37,7 @@ def CipdEnsure(pkg_name, ref, directory):
     output = subprocess.check_output(
         ['cipd', 'ensure', '-root', directory,
          '-ensure-file', '-'],
+        shell=True,
         input=('%s %s' % (pkg_name, ref)).encode('utf-8'))
     print(output)
 
