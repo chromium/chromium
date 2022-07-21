e43a4d78b0b26d60fc141aadbe1ef15225f504e9
Justin Cohen <justincohen@google.com>
2022-07-21 01:16:30 +0000

    ios: Disable OpenInManagerTestCase/testOpenInMOV on iOS16.
    Bug: 1346136
    Change-Id: Ifaf1caddcdd9a65cc4bfb8e6d71134db9a8b2a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777739
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1026571}

--- ios/chrome/browser/ui/open_in/open_in_controller_egtest.mm
+++ ios/chrome/browser/ui/open_in/open_in_controller_egtest.mm
@@ -134,6 +134,10 @@ using base::test::ios::WaitUntilConditionOrTimeout;
 
 // Tests that open in button do not appears when opening a MOV file.
 - (void)testOpenInMOV {
+  // TODO(crbug.com/1346136): Opening this mov is crashing WebContent on iOS16.
+  if (@available(iOS 16, *)) {
+    EARL_GREY_TEST_DISABLED(@"testOpenInMOV crashing WebContent on iOS16");
+  }
   [ChromeEarlGrey loadURL:self.testServer->GetURL(kMOVPath)];
   [[EarlGrey selectElementWithMatcher:chrome_test_util::OpenInButton()]
       assertWithMatcher:grey_nil()];
