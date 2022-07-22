68149d8bc5fc60d420dcb530e16394dbd3ea56fd
Greg Thompson <grt@chromium.org>
2022-07-22 07:18:28 +0000

    [Windows] Attempt to fix TaskbarIconFinder.Simple flakes.
    UI automation can be very, very slow on machines under load. Extend the
    RunLoop timeout for this test in an attempt to reduce flakes.
    
    Bug: 1344131
    Change-Id: I67381b074c5cfc51532ceb962b3f1a53a6a93e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3766746
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027142}

--- chrome/browser/win/taskbar_icon_finder_unittest.cc
+++ chrome/browser/win/taskbar_icon_finder_unittest.cc
@@ -6,8 +6,11 @@
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/location.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_run_loop_timeout.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_timeouts.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // The most simple test possible to ensure that the finder doesn't leak or
@@ -16,6 +19,14 @@ TEST(TaskbarIconFinder, Simple) {
   base::test::TaskEnvironment task_environment;
   base::RunLoop run_loop;
 
+  // Extend the timeout since the use of UI automation can take some time on a
+  // machine under heavy load (e.g., one that is running many tests in
+  // parallel).
+  base::test::ScopedRunLoopTimeout scoped_run_loop_timeout(
+      FROM_HERE, TestTimeouts::action_max_timeout(), base::BindRepeating([]() {
+        return std::string(
+            "FindTaskbarIcon did not complete in a timely fashion.");
+      }));
   FindTaskbarIcon(
       base::BindOnce([](base::OnceClosure quit,
                         const gfx::Rect& rect) { std::move(quit).Run(); },
