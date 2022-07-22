38a10d36bdf2bca10bdda950ba0f9b58dbe9f44c
Mingjing Zhang <mjzhang@chromium.org>
2022-07-22 20:40:36 +0000

    Revert "WebGL DrawingBuffer: Use SharedImage instead of GpuMemoryBuffer"
    This reverts commit 668e0ca34a6c4488d7a1cfceb10341a106a50e27.
    
    Reason for revert: crbug.com/1346737 Consistent Pixel_CanvasLowLatencyWebGL* failures on ChromeOS
    
    Original change's description:
    > WebGL DrawingBuffer: Use SharedImage instead of GpuMemoryBuffer
    >
    > Historically, the only way for the DrawingBuffer to be IOSurface
    > backed was to allocate it as a GpuMemoryBuffer and bind a GLImage to
    > it.
    >
    > We now have the SharedImage interface (and before that we had the
    > anonymous GLImage backing), and so the use of GpuMemoryBuffer here
    > is only cost (namely, a synchronous IPC).
    >
    > Continue using GpuMemoryBuffers for the RGB emulation path for now.
    > It can be removed when the validating command decoder is removed.
    >
    > Bug: 924198
    > Change-Id: If89de88cc7505775162f53a0589f32a7e67aa1a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762756
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Commit-Queue: ccameron chromium <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1027029}
    
    Bug: 924198
    Change-Id: Ie36554252c21bb3cf08a491198dc15142f183b06
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3781214
    Owners-Override: Mingjing Zhang <mjzhang@google.com>
    Commit-Queue: Mingjing Zhang <mjzhang@google.com>
    Auto-Submit: Mingjing Zhang <mjzhang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1027409}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer_test.cc
@@ -510,6 +510,71 @@ TEST_F(DrawingBufferImageChromiumTest, VerifyResizingReallocatesImages) {
   EXPECT_EQ(0u, sii->shared_image_count());
 }
 
+TEST_F(DrawingBufferImageChromiumTest, AllocationFailure) {
+  GLES2InterfaceForTests* gl_ = drawing_buffer_->ContextGLForTests();
+  viz::TestGpuMemoryBufferManager* gmb_manager =
+      static_cast<viz::TestGpuMemoryBufferManager*>(
+          Platform::Current()->GetGpuMemoryBufferManager());
+  viz::TestSharedImageInterface* sii =
+      drawing_buffer_->SharedImageInterfaceForTests();
+
+  viz::TransferableResource resource1;
+  viz::ReleaseCallback release_callback1;
+  viz::TransferableResource resource2;
+  viz::ReleaseCallback release_callback2;
+  viz::TransferableResource resource3;
+  viz::ReleaseCallback release_callback3;
+
+  // Request a resource. A SharedImage should already be created. Everything
+  // works as expected.
+  EXPECT_CALL(*gl_, CreateAndTexStorage2DSharedImageCHROMIUMMock(_)).Times(1);
+  EXPECT_FALSE(drawing_buffer_->MarkContentsChanged());
+  EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(nullptr, &resource1,
+                                                           &release_callback1));
+  EXPECT_TRUE(resource1.is_overlay_candidate);
+  gpu::Mailbox mailbox1;
+  mailbox1.SetName(gl_->last_imported_shared_image()->name);
+  EXPECT_TRUE(sii->CheckSharedImageExists(mailbox1));
+  testing::Mock::VerifyAndClearExpectations(gl_);
+  VerifyStateWasRestored();
+
+  // Force GpuMemoryBuffer creation failure. Request another resource. It should
+  // still be provided, but this time with allowOverlay = false.
+  EXPECT_CALL(*gl_, CreateAndTexStorage2DSharedImageCHROMIUMMock(_)).Times(1);
+  gmb_manager->SetFailOnCreate(true);
+  EXPECT_TRUE(drawing_buffer_->MarkContentsChanged());
+  EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(nullptr, &resource2,
+                                                           &release_callback2));
+  EXPECT_FALSE(resource2.is_overlay_candidate);
+  gpu::Mailbox mailbox2;
+  mailbox2.SetName(gl_->last_imported_shared_image()->name);
+  EXPECT_TRUE(sii->CheckSharedImageExists(mailbox2));
+  VerifyStateWasRestored();
+
+  // Check that if GpuMemoryBuffer allocation starts working again, resources
+  // are correctly created with allowOverlay = true.
+  EXPECT_CALL(*gl_, CreateAndTexStorage2DSharedImageCHROMIUMMock(_)).Times(1);
+  gmb_manager->SetFailOnCreate(false);
+  EXPECT_TRUE(drawing_buffer_->MarkContentsChanged());
+  EXPECT_TRUE(drawing_buffer_->PrepareTransferableResource(nullptr, &resource3,
+                                                           &release_callback3));
+  EXPECT_TRUE(resource3.is_overlay_candidate);
+  gpu::Mailbox mailbox3;
+  mailbox3.SetName(gl_->last_imported_shared_image()->name);
+  EXPECT_TRUE(sii->CheckSharedImageExists(mailbox3));
+  testing::Mock::VerifyAndClearExpectations(gl_);
+  VerifyStateWasRestored();
+
+  std::move(release_callback1).Run(gpu::SyncToken(), false /* lostResource */);
+  std::move(release_callback2).Run(gpu::SyncToken(), false /* lostResource */);
+  std::move(release_callback3).Run(gpu::SyncToken(), false /* lostResource */);
+
+  drawing_buffer_->BeginDestruction();
+  EXPECT_FALSE(sii->CheckSharedImageExists(mailbox1));
+  EXPECT_FALSE(sii->CheckSharedImageExists(mailbox2));
+  EXPECT_FALSE(sii->CheckSharedImageExists(mailbox3));
+}
+
 class DepthStencilTrackingGLES2Interface
     : public gpu::gles2::GLES2InterfaceStub {
  public:
