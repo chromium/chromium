462d9b6cb19e69fec812e740758a23ab5d541f80
Md Shahadat Hossain Shahin <shahinmd@google.com>
2022-07-25 17:57:35 +0000

    [Privacy Hub] Mute/unmute microphone when the toggle is pressed
    This CL adds functionality to mute/unmute the microphones globally when
    the privacy hub microphone toggle is pressed.
    
    The microphone can also be muted/unmuted from the system tray button.
    The buttons will be synced in an upcoming CL.
    
    Bug: b/237259506
    Change-Id: Ie0cd5d1233ddd97b7f3e8ee01df194043747978a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3735288
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027842}

---
+++ ash/system/privacy_hub/microphone_privacy_switch_controller_unittest.cc
@@ -0,0 +1,57 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/privacy_hub/microphone_privacy_switch_controller.h"
+
+#include "ash/components/audio/cras_audio_handler.h"
+#include "ash/constants/ash_pref_names.h"
+#include "ash/public/cpp/microphone_mute_notification_delegate.h"
+#include "ash/session/session_controller_impl.h"
+#include "ash/shell.h"
+#include "ash/test/ash_test_base.h"
+
+namespace ash {
+
+class FakeMicrophoneMuteNotificationDelegate
+    : public MicrophoneMuteNotificationDelegate {
+ public:
+  absl::optional<std::u16string> GetAppAccessingMicrophone() override {
+    return absl::nullopt;
+  }
+};
+
+class MicrophonePrivacySwitchControllerTest : public AshTestBase {
+ public:
+  MicrophonePrivacySwitchControllerTest() = default;
+  ~MicrophonePrivacySwitchControllerTest() override = default;
+
+  // AshTestBase:
+  void SetUp() override {
+    AshTestBase::SetUp();
+
+    // This makes sure a globale instance of MicrophoneMuteNotificationDelegate
+    // is created before running tests.
+    delegate_ = std::make_unique<FakeMicrophoneMuteNotificationDelegate>();
+  }
+
+ protected:
+  void SetUserPref(bool allowed) {
+    Shell::Get()->session_controller()->GetActivePrefService()->SetBoolean(
+        prefs::kUserMicrophoneAllowed, allowed);
+  }
+
+ private:
+  std::unique_ptr<FakeMicrophoneMuteNotificationDelegate> delegate_;
+};
+
+TEST_F(MicrophonePrivacySwitchControllerTest, OnPreferenceChanged) {
+  static constexpr bool user_preferences[] = {false, true, false};
+
+  for (bool microphone_allowed : user_preferences) {
+    SetUserPref(microphone_allowed);
+    EXPECT_EQ(CrasAudioHandler::Get()->IsInputMuted(), !microphone_allowed);
+  }
+}
+
+}  // namespace ash
