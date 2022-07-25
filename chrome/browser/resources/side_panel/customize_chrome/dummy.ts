631dceea383f209234ef9fa57ff05beb8b34c0a1
Nihar Majmudar <niharm@google.com>
2022-07-25 19:13:06 +0000

    Customize Chrome side panel: Use build_webui() GN rule.
    This CL moves the Customize Chrome WebUI to use build_webui() in its
    resources BUILD.gn file.
    
    This follow up CL gets rid of the dummy.ts file https://chromium-review.googlesource.com/c/chromium/src/+/3769344.
    
    Bug: 1341007
    Change-Id: I3fa7b7c58e9808844e1e2c983335579d8aa93c1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783327
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1027886}

---
+++ chrome/browser/resources/side_panel/customize_chrome/dummy.ts
@@ -0,0 +1,5 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Dummy ts file to set up build_webui(). Replaced in follow up CL.
