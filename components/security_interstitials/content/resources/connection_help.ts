cf8868751d8db4edc4c6e61f24fd04a8e0c1fad2
dpapad <dpapad@chromium.org>
2022-07-25 16:58:47 +0000

    Migrate chrome://connection-help to TypeScript.
    Bug: 1316438
    Change-Id: I93e33e7119635b7f9dc97d72b3b15a819e3a1e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780149
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1027805}

--- components/security_interstitials/content/resources/connection_help.ts
+++ components/security_interstitials/content/resources/connection_help.ts
@@ -7,23 +7,25 @@ import './strings.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {$} from 'chrome://resources/js/util.m.js';
 
-const HIDDEN_CLASS = 'hidden';
+const HIDDEN_CLASS: string = 'hidden';
 
 function setupEvents() {
-  $('details-certerror-button').addEventListener('click', function(event) {
+  $('details-certerror-button').addEventListener('click', function(_event) {
     toggleHidden('details-certerror', 'details-certerror-button');
   });
-  $('details-connectnetwork-button').addEventListener('click', function(event) {
-    toggleHidden('details-connectnetwork', 'details-connectnetwork-button');
-  });
-  $('details-clock-button').addEventListener('click', function(event) {
+  $('details-connectnetwork-button')
+      .addEventListener('click', function(_event) {
+        toggleHidden('details-connectnetwork', 'details-connectnetwork-button');
+      });
+  $('details-clock-button').addEventListener('click', function(_event) {
     toggleHidden('details-clock', 'details-clock-button');
   });
   if (loadTimeData.getBoolean('isWindows')) {
     $('windows-only').classList.remove(HIDDEN_CLASS);
-    $('details-mitmsoftware-button').addEventListener('click', function(event) {
-      toggleHidden('details-mitmsoftware', 'details-mitmsoftware-button');
-    });
+    $('details-mitmsoftware-button')
+        .addEventListener('click', function(_event) {
+          toggleHidden('details-mitmsoftware', 'details-mitmsoftware-button');
+        });
   }
   switch (window.location.hash) {
     case '#' + loadTimeData.getInteger('certCommonNameInvalid'):
@@ -38,11 +40,10 @@ function setupEvents() {
   }
 }
 
-function toggleHidden(className, buttonName) {
+function toggleHidden(className: string, buttonName: string) {
   const hiddenDetails = $(className).classList.toggle(HIDDEN_CLASS);
-  $(buttonName).innerText = hiddenDetails ?
-      loadTimeData.getString('connectionHelpShowMore') :
-      loadTimeData.getString('connectionHelpShowLess');
+  $(buttonName).innerText = loadTimeData.getString(
+      hiddenDetails ? 'connectionHelpShowMore' : 'connectionHelpShowLess');
 }
 
 document.addEventListener('DOMContentLoaded', setupEvents);
