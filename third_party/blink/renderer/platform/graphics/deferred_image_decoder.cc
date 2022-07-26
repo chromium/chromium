3b38b5a124de838e89c273b8f71c8c5f10291cdd
Dave Tapuska <dtapuska@chromium.org>
2022-07-26 16:25:49 +0000

    Remove expired histograms from blink.
    Blink.ImageDecoders.InitiallyFullyDecodedByteSize and
    Blink.ImageDecoders.IncrementallyDecodedByteSize were expired so remove
    the code that logs them
    
    Blink.ResourceLoadScheduler.TrafficBytes.KBPerFrameStatus was removed
    long ago, so remove it from the histogram list.
    
    BUG=1255768
    
    Change-Id: I2119033c8e89819c17b417fbab7e4fa3e8644311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3783409
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028303}

--- third_party/blink/renderer/platform/graphics/deferred_image_decoder.cc
+++ third_party/blink/renderer/platform/graphics/deferred_image_decoder.cc
@@ -76,47 +76,6 @@ void ReportIncrementalDecodeNeeded(bool all_data_received,
   }
 }
 
-void RecordByteSizeAndWhetherIncrementalDecode(const String& image_type,
-                                               bool incrementally_decoded,
-                                               size_t bytes) {
-  DCHECK(IsMainThread());
-  // A base::HistogramBase::Sample may not fit the number of bytes of the image.
-  base::HistogramBase::Sample sample_bytes =
-      base::saturated_cast<base::HistogramBase::Sample>(bytes);
-  if (image_type == "jpg") {
-    if (incrementally_decoded) {
-      DEFINE_STATIC_LOCAL(
-          CustomCountHistogram, jpeg_byte_size_incrementally_decoded_histogram,
-          ("Blink.ImageDecoders.IncrementallyDecodedByteSize.Jpeg",
-           125 /* min */, 15000000 /* 15 MB */, 100 /* bucket count */));
-      jpeg_byte_size_incrementally_decoded_histogram.Count(sample_bytes);
-    } else {
-      DEFINE_STATIC_LOCAL(
-          CustomCountHistogram,
-          jpeg_byte_size_initially_fully_decoded_histogram,
-          ("Blink.ImageDecoders.InitiallyFullyDecodedByteSize.Jpeg",
-           125 /* min */, 15000000 /* 15 MB */, 100 /* bucket count */));
-      jpeg_byte_size_initially_fully_decoded_histogram.Count(sample_bytes);
-    }
-  } else {
-    DCHECK_EQ(image_type, "webp");
-    if (incrementally_decoded) {
-      DEFINE_STATIC_LOCAL(
-          CustomCountHistogram, webp_byte_size_incrementally_decoded_histogram,
-          ("Blink.ImageDecoders.IncrementallyDecodedByteSize.WebP",
-           125 /* min */, 15000000 /* 15 MB */, 100 /* bucket count */));
-      webp_byte_size_incrementally_decoded_histogram.Count(sample_bytes);
-    } else {
-      DEFINE_STATIC_LOCAL(
-          CustomCountHistogram,
-          webp_byte_size_initially_fully_decoded_histogram,
-          ("Blink.ImageDecoders.InitiallyFullyDecodedByteSize.WebP",
-           125 /* min */, 15000000 /* 15 MB */, 100 /* bucket count */));
-      webp_byte_size_initially_fully_decoded_histogram.Count(sample_bytes);
-    }
-  }
-}
-
 }  // namespace
 
 struct DeferredFrameData {
@@ -238,14 +197,6 @@ sk_sp<PaintImageGenerator> DeferredImageDecoder::CreateGenerator() {
       complete_frame_content_id_, all_data_received_, can_yuv_decode_,
       *image_metadata_);
   first_decoding_generator_created_ = true;
-
-  size_t image_byte_size = ByteSize();
-  if (all_data_received_ && (image_type == "jpg" || image_type == "webp")) {
-    DCHECK(incremental_decode_needed_.has_value());
-    DCHECK(image_byte_size);
-    RecordByteSizeAndWhetherIncrementalDecode(
-        image_type, incremental_decode_needed_.value(), image_byte_size);
-  }
 
   return generator;
 }
