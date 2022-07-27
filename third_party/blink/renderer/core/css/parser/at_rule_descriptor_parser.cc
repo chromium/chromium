545aac9815510ec074d78e2a63887a0d59e1b9fa
Rune Lillesveen <futhark@chromium.org>
2022-07-27 20:36:05 +0000

    CSS-wide keywords are not allowed unquoted in local()
    Instead of dropping the descriptor, we stored a local() descriptor with
    an empty font name which caused a crash. If the parsed font name is the
    empty string, drop the descriptor.
    
    Bug: 1342244
    Change-Id: I5eb2c450b2019df475ed0caafba647fd9dc7fb61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789343
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028890}

--- third_party/blink/renderer/core/css/parser/at_rule_descriptor_parser.cc
+++ third_party/blink/renderer/core/css/parser/at_rule_descriptor_parser.cc
@@ -114,6 +114,8 @@ CSSValue* ConsumeFontFaceSrcLocal(CSSParserTokenRange& range,
     String family_name = css_parsing_utils::ConcatenateFamilyName(args);
     if (!args.AtEnd())
       return nullptr;
+    if (family_name.IsEmpty())
+      return nullptr;
     return CSSFontFaceSrcValue::CreateLocal(
         family_name, context.JavascriptWorld(),
         context.IsOriginClean() ? OriginClean::kTrue : OriginClean::kFalse,
