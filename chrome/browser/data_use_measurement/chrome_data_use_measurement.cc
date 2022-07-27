d39f6f496142fe1e9752b6904ca44a8ee37d48aa
Max Curran <curranmax@chromium.org>
2022-07-27 18:36:38 +0000

    Remove Chrome::MaybeRecordNetworkBytesOS and update related histograms.
    This CL removes Chrome::MaybeRecordNetworkBytesOS because it can be very
    slow due to calls to Android APIs, and the data from the Network Service
    should be sufficient. Additionally the CL cleans up the
    DataUse.BytesReceived3.* and DataUseBytesSent3.* histogrmas by:
    1) extending their expiration by one year, and 2) inlining the
    MeasuringLayer variant now that there was only the "Delegate" variant.
    
    Bug: 1339449
    Change-Id: Ib16cb7ad6e38cd5a9c271a4175ba96df4848453b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788261
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1028840}

--- chrome/browser/data_use_measurement/chrome_data_use_measurement.cc
+++ chrome/browser/data_use_measurement/chrome_data_use_measurement.cc
@@ -10,10 +10,6 @@
 #include "base/trace_event/trace_event.h"
 #include "content/public/browser/browser_thread.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "net/android/traffic_stats.h"
-#endif
-
 using content::BrowserThread;
 
 // static
@@ -26,20 +22,7 @@ ChromeDataUseMeasurement& ChromeDataUseMeasurement::GetInstance() {
   return *s_chrome_data_use_measurement;
 }
 
-ChromeDataUseMeasurement::ChromeDataUseMeasurement() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  TRACE_EVENT0("browser", "ChromeDataUseMeasurement");
-
-#if BUILDFLAG(IS_ANDROID)
-  int64_t bytes = 0;
-  // Query Android traffic stats.
-  if (net::android::traffic_stats::GetCurrentUidRxBytes(&bytes))
-    rx_bytes_os_ = bytes;
-
-  if (net::android::traffic_stats::GetCurrentUidTxBytes(&bytes))
-    tx_bytes_os_ = bytes;
-#endif
-}
+ChromeDataUseMeasurement::ChromeDataUseMeasurement() = default;
 
 ChromeDataUseMeasurement::~ChromeDataUseMeasurement() = default;
 
@@ -74,54 +57,4 @@ void ChromeDataUseMeasurement::ReportDataUsage(TrafficDirection dir,
 
   if (dir == TrafficDirection::kUpstream)
     UMA_HISTOGRAM_COUNTS_1M("DataUse.BytesSent3.Delegate", message_size_bytes);
-
-#if BUILDFLAG(IS_ANDROID)
-  bytes_transferred_since_last_traffic_stats_query_ += message_size_bytes;
-  MaybeRecordNetworkBytesOS();
-#endif
 }
-
-#if BUILDFLAG(IS_ANDROID)
-void ChromeDataUseMeasurement::MaybeRecordNetworkBytesOS() {
-  // Minimum number of bytes that should be reported by the network delegate
-  // before Android's TrafficStats API is queried (if Chrome is not in
-  // background). This reduces the overhead of repeatedly calling the API.
-  static const int64_t kMinDelegateBytes = 25000;
-
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (bytes_transferred_since_last_traffic_stats_query_ < kMinDelegateBytes) {
-    return;
-  }
-  bytes_transferred_since_last_traffic_stats_query_ = 0;
-  int64_t bytes = 0;
-  // Query Android traffic stats directly instead of registering with the
-  // DataUseAggregator since the latter does not provide notifications for
-  // the incognito traffic.
-  if (net::android::traffic_stats::GetCurrentUidRxBytes(&bytes)) {
-    if (rx_bytes_os_ != 0) {
-      DCHECK_GE(bytes, rx_bytes_os_);
-      if (bytes > rx_bytes_os_) {
-        int64_t incremental_bytes = bytes - rx_bytes_os_;
-        // Do not record samples with value 0.
-        base::UmaHistogramCustomCounts("DataUse.BytesReceived3.OS",
-                                       incremental_bytes, 50, 10 * 1000 * 1000,
-                                       50);
-      }
-    }
-    rx_bytes_os_ = bytes;
-  }
-
-  if (net::android::traffic_stats::GetCurrentUidTxBytes(&bytes)) {
-    if (tx_bytes_os_ != 0) {
-      DCHECK_GE(bytes, tx_bytes_os_);
-      if (bytes > tx_bytes_os_) {
-        int64_t incremental_bytes = bytes - tx_bytes_os_;
-        // Do not record samples with value 0.
-        UMA_HISTOGRAM_COUNTS_1M("DataUse.BytesSent3.OS", incremental_bytes);
-      }
-    }
-    tx_bytes_os_ = bytes;
-  }
-}
-
-#endif
--- chrome/browser/data_use_measurement/chrome_data_use_measurement.h
+++ chrome/browser/data_use_measurement/chrome_data_use_measurement.h
@@ -6,7 +6,6 @@
 #define CHROME_BROWSER_DATA_USE_MEASUREMENT_CHROME_DATA_USE_MEASUREMENT_H_
 
 #include "base/sequence_checker.h"
-#include "build/build_config.h"
 
 class ChromeDataUseMeasurement {
  public:
@@ -33,23 +32,6 @@ class ChromeDataUseMeasurement {
   // histogram DataUse.Services.{Dimensions} with, services as the buckets.
   // |app_state| indicates the app state which can be foreground, or background.
   void ReportDataUsage(TrafficDirection dir, int64_t message_size_bytes);
-
-#if BUILDFLAG(IS_ANDROID)
-  // Records the count of bytes received and sent by Chrome on the network as
-  // reported by the operating system.
-  void MaybeRecordNetworkBytesOS();
-
-  // Number of bytes received and sent by Chromium as reported by the operating
-  // system when it was last queried for traffic statistics. Set to 0 if the
-  // operating system was never queried.
-  int64_t rx_bytes_os_ = 0;
-  int64_t tx_bytes_os_ = 0;
-
-  // Number of bytes received and sent by Chromium as reported by the network
-  // delegate since the operating system was last queried for traffic
-  // statistics.
-  int64_t bytes_transferred_since_last_traffic_stats_query_ = 0;
-#endif
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
