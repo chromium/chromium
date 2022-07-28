ae5904cd515b05cceb233355adae014e912281a0
Ramin Halavati <rhalavati@chromium.org>
2022-07-28 16:17:32 +0000

    Add basic browser test for Screen AI service.
    Adds a basic browser test the ensures requesting ScreenAI visual
    annotation will trigger the request to the service.
    This CL will be followed by ensuring ScreenAI service is available and
    annotations are acquired.
    
    Bug: 1278249
    Change-Id: I02c59ae04817def2517b57c00eda816a50a0e443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776540
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029298}

---
+++ chrome/browser/accessibility/screen_ai_service_browsertest.cc
@@ -0,0 +1,54 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/accessibility/ax_screen_ai_annotator.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/test/browser_test.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/image/image.h"
+
+namespace screen_ai {
+
+namespace {
+
+class MockAXScreenAIAnnotator : public AXScreenAIAnnotator {
+ public:
+  explicit MockAXScreenAIAnnotator(Browser* browser)
+      : AXScreenAIAnnotator(browser) {}
+  MOCK_METHOD(void,
+              OnScreenshotReceived,
+              (const ui::AXTreeID& ax_tree_id, gfx::Image snapshot),
+              (override));
+};
+
+}  // namespace
+
+using ScreenAIServiceTest = InProcessBrowserTest;
+
+IN_PROC_BROWSER_TEST_F(ScreenAIServiceTest, ScreenShotTest) {
+  MockAXScreenAIAnnotator* annotator = new MockAXScreenAIAnnotator(browser());
+  browser()->SetScreenAIAnnotatorForTesting(
+      std::unique_ptr<AXScreenAIAnnotator>(annotator));
+
+  base::RunLoop run_loop;
+
+  EXPECT_CALL(*annotator, OnScreenshotReceived)
+      .WillOnce(
+          [&run_loop](const ui::AXTreeID& ax_tree_id, gfx::Image snapshot) {
+            EXPECT_FALSE(snapshot.IsEmpty());
+            EXPECT_GT(snapshot.Size().width(), 0);
+            EXPECT_GT(snapshot.Size().height(), 0);
+            run_loop.Quit();
+          });
+
+  browser()->RunScreenAIAnnotator();
+  run_loop.Run();
+
+  // TODO(https://crbug.com/1278249): Expect OnAnnotationReceived once library
+  // binary is available for test.
+}
+
+}  // namespace screen_ai
