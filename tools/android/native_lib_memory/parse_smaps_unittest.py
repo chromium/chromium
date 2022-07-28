0dce8f4158b50a4267804d4e3e980c018e5c95da
Takuto Ikuta <tikuta@chromium.org>
2022-07-28 09:15:50 +0000

    use vpython3 for scripts in /tools/android/native_lib_memory
    This is made by `git cl split` from https://crrev.com/c/3759062.
    
    This assumes that these scripts are already migrated to Python3.
    
    If not, do one of the following to remove dependency to .vpython file.
    * fix python3 compatibility by yourselfs before or after the CL submission
    * file a blocking issue of crbug.com/1347705 with how to run the script so that
      people who are interested in Python3 fix can take the issue.
    
    This CL was uploaded by git cl split.
    
    R=lizeb@chromium.org
    
    Change-Id: I673f67f254bdef566616149ffebad11b1002040e
    Bug: 1347705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789555
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029143}

--- tools/android/native_lib_memory/parse_smaps_unittest.py
+++ tools/android/native_lib_memory/parse_smaps_unittest.py
@@ -30,34 +30,20 @@ MMUPageSize:           4 kB
 Locked:                0 kB
 VmFlags: rd ex mr mw me de sd""".split('\n')
     mappings = parse_smaps._ParseProcSmapsLines(CONTENT)
-    self.assertEquals(1, len(mappings))
-    self.assertEquals(0x7fff4fbfc000, mappings[0].start)
-    self.assertEquals(0x7fff4fbfe000, mappings[0].end)
-    self.assertEquals('r-xp', mappings[0].permissions)
-    self.assertEquals(0, mappings[0].offset)
-    self.assertEquals('[vdso]', mappings[0].pathname)
-    self.assertEquals(
-        ['Size',
-         'Rss',
-         'Pss',
-         'Shared_Clean',
-         'Shared_Dirty',
-         'Private_Clean',
-         'Private_Dirty',
-         'Referenced',
-         'Anonymous',
-         'AnonHugePages',
-         'ShmemPmdMapped',
-         'Shared_Hugetlb',
-         'Private_Hugetlb',
-         'Swap',
-         'SwapPss',
-         'KernelPageSize',
-         'MMUPageSize',
-         'Locked',
-         'VmFlags'], mappings[0].fields.keys())
-    self.assertEquals(8, mappings[0].fields['Size'])
-    self.assertEquals('rd ex mr mw me de sd', mappings[0].fields['VmFlags'])
+    self.assertEqual(1, len(mappings))
+    self.assertEqual(0x7fff4fbfc000, mappings[0].start)
+    self.assertEqual(0x7fff4fbfe000, mappings[0].end)
+    self.assertEqual('r-xp', mappings[0].permissions)
+    self.assertEqual(0, mappings[0].offset)
+    self.assertEqual('[vdso]', mappings[0].pathname)
+    self.assertEqual([
+        'Size', 'Rss', 'Pss', 'Shared_Clean', 'Shared_Dirty', 'Private_Clean',
+        'Private_Dirty', 'Referenced', 'Anonymous', 'AnonHugePages',
+        'ShmemPmdMapped', 'Shared_Hugetlb', 'Private_Hugetlb', 'Swap',
+        'SwapPss', 'KernelPageSize', 'MMUPageSize', 'Locked', 'VmFlags'
+    ], list(mappings[0].fields.keys()))
+    self.assertEqual(8, mappings[0].fields['Size'])
+    self.assertEqual('rd ex mr mw me de sd', mappings[0].fields['VmFlags'])
 
   def testSeveralEntries(self):
     CONTENT = """7fff4fbfc000-7fff4fbfe000 r-xp 00000000 00:00 0      [vdso]
@@ -67,32 +53,33 @@ ffffffffff600000-ffffffffff601000 r-xp 00000010 00:00 0      /tmp/foo
 Size:                  192 kB
 VmFlags: rd ex""".split('\n')
     mappings = parse_smaps._ParseProcSmapsLines(CONTENT)
-    self.assertEquals(2, len(mappings))
-    self.assertEquals(0x7fff4fbfc000, mappings[0].start)
-    self.assertEquals(0x7fff4fbfe000, mappings[0].end)
-    self.assertEquals('r-xp', mappings[0].permissions)
-    self.assertEquals(0, mappings[0].offset)
-    self.assertEquals('[vdso]', mappings[0].pathname)
-    self.assertEquals(['Size', 'VmFlags'], mappings[0].fields.keys())
-    self.assertEquals(8, mappings[0].fields['Size'])
-    self.assertEquals('rd ex mr mw me de sd', mappings[0].fields['VmFlags'])
+    self.assertEqual(2, len(mappings))
+    self.assertEqual(0x7fff4fbfc000, mappings[0].start)
+    self.assertEqual(0x7fff4fbfe000, mappings[0].end)
+    self.assertEqual('r-xp', mappings[0].permissions)
+    self.assertEqual(0, mappings[0].offset)
+    self.assertEqual('[vdso]', mappings[0].pathname)
+    self.assertEqual(['Size', 'VmFlags'], list(mappings[0].fields.keys()))
+    self.assertEqual(8, mappings[0].fields['Size'])
+    self.assertEqual('rd ex mr mw me de sd', mappings[0].fields['VmFlags'])
 
-    self.assertEquals(0xffffffffff600000, mappings[1].start)
-    self.assertEquals(0xffffffffff601000, mappings[1].end)
-    self.assertEquals('r-xp', mappings[1].permissions)
-    self.assertEquals(0x10, mappings[1].offset)
-    self.assertEquals('/tmp/foo', mappings[1].pathname)
-    self.assertEquals(['Size', 'VmFlags'], mappings[1].fields.keys())
-    self.assertEquals(192, mappings[1].fields['Size'])
-    self.assertEquals('rd ex', mappings[1].fields['VmFlags'])
+    self.assertEqual(0xffffffffff600000, mappings[1].start)
+    self.assertEqual(0xffffffffff601000, mappings[1].end)
+    self.assertEqual('r-xp', mappings[1].permissions)
+    self.assertEqual(0x10, mappings[1].offset)
+    self.assertEqual('/tmp/foo', mappings[1].pathname)
+    self.assertEqual(['Size', 'VmFlags'], list(mappings[1].fields.keys()))
+    self.assertEqual(192, mappings[1].fields['Size'])
+    self.assertEqual('rd ex', mappings[1].fields['VmFlags'])
 
   def testToString(self):
     CONTENT = """7fff4fbfc000-7fff4fbfe000 r-xp 00000010 00:00 0      [vdso]
 Size:                  8 kB
 VmFlags: rd ex mr mw me de sd""".split('\n')
     mappings = parse_smaps._ParseProcSmapsLines(CONTENT)
-    self.assertEquals(1, len(mappings))
-    self.assertEquals("""7fff4fbfc000-7fff4fbfe000 r-xp 10 [vdso]
+    self.assertEqual(1, len(mappings))
+    self.assertEqual(
+        """7fff4fbfc000-7fff4fbfe000 r-xp 10 [vdso]
 Size: 8 kB
 VmFlags: rd ex mr mw me de sd""", mappings[0].ToString())
 
