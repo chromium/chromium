d2748467b3ad11fd773f9baaa0f47fd95eb1da25
Ahmed Nasr <anasr@google.com>
2022-07-28 00:41:12 +0000

    Add kiosk login/logout reporting browser test.
    Bug: b:237811826
    Change-Id: I7610a00d1ffa1c17aaf7c43a7ce8237a532c8ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789834
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029020}

--- chrome/browser/ash/login/reporting/login_logout_reporter_browsertest.cc
+++ chrome/browser/ash/login/reporting/login_logout_reporter_browsertest.cc
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <vector>
 
+#include "apps/test/app_window_waiter.h"
 #include "ash/components/login/auth/public/auth_failure.h"
 #include "ash/components/login/auth/public/key.h"
 #include "ash/components/login/auth/public/user_context.h"
@@ -14,9 +15,14 @@
 #include "ash/public/cpp/login_screen_test_api.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
+#include "base/auto_reset.h"
 #include "base/run_loop.h"
+#include "chrome/browser/ash/app_mode/fake_cws.h"
+#include "chrome/browser/ash/login/app_mode/kiosk_launch_controller.h"
 #include "chrome/browser/ash/login/session/user_session_manager_test_api.h"
+#include "chrome/browser/ash/login/test/embedded_test_server_setup_mixin.h"
 #include "chrome/browser/ash/login/test/fake_gaia_mixin.h"
+#include "chrome/browser/ash/login/test/kiosk_apps_mixin.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
@@ -25,7 +31,10 @@
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
+#include "chrome/browser/extensions/browsertest_util.h"
 #include "chrome/browser/policy/messaging_layer/proto/synced/login_logout_event.pb.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/ash/components/dbus/session_manager/fake_session_manager_client.h"
 #include "chromeos/dbus/missive/missive_client.h"
@@ -39,6 +48,10 @@
 #include "components/reporting/proto/synced/record_constants.pb.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/browser/app_window/app_window.h"
+#include "extensions/browser/app_window/app_window_registry.h"
+#include "extensions/browser/app_window/native_app_window.h"
+#include "net/dns/mock_host_resolver.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -347,6 +360,109 @@ IN_PROC_BROWSER_TEST_F(LoginLogoutReporterPublicSessionBrowserTest,
   EXPECT_FALSE(login_record_data.has_affiliated_user());
   ASSERT_TRUE(login_record_data.has_login_event());
   EXPECT_FALSE(login_record_data.login_event().has_failure());
+}
+
+class LoginLogoutReporterKioskBrowserTest
+    : public MixinBasedInProcessBrowserTest {
+ public:
+  LoginLogoutReporterKioskBrowserTest(
+      const LoginLogoutReporterKioskBrowserTest&) = delete;
+  LoginLogoutReporterKioskBrowserTest& operator=(
+      const LoginLogoutReporterKioskBrowserTest&) = delete;
+
+ protected:
+  LoginLogoutReporterKioskBrowserTest() = default;
+
+  ~LoginLogoutReporterKioskBrowserTest() override = default;
+
+  void SetUp() override {
+    skip_splash_wait_override_ =
+        KioskLaunchController::SkipSplashScreenWaitForTesting();
+    login_manager_.set_session_restore_enabled();
+
+    MixinBasedInProcessBrowserTest::SetUp();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    MixinBasedInProcessBrowserTest::SetUpCommandLine(command_line);
+
+    fake_cws_.Init(embedded_test_server());
+    fake_cws_.SetUpdateCrx(GetTestAppId(), GetTestAppId() + ".crx", "1.0.0");
+  }
+
+  void SetUpInProcessBrowserTestFixture() override {
+    MixinBasedInProcessBrowserTest::SetUpInProcessBrowserTestFixture();
+
+    host_resolver()->AddRule("*", "127.0.0.1");
+    SessionManagerClient::InitializeFakeInMemory();
+    FakeSessionManagerClient::Get()->set_supports_browser_restart(true);
+
+    ChromeDeviceSettingsProto& proto(policy_helper_.device_policy()->payload());
+    KioskAppsMixin::AppendAutoLaunchKioskAccount(&proto);
+    proto.mutable_device_reporting()->set_report_login_logout(true);
+    policy_helper_.RefreshDevicePolicy();
+  }
+
+  void SetUpOnMainThread() override {
+    MixinBasedInProcessBrowserTest::SetUpOnMainThread();
+    extensions::browsertest_util::CreateAndInitializeLocalCache();
+  }
+
+  std::string GetTestAppId() const { return KioskAppsMixin::kKioskAppId; }
+
+ private:
+  FakeCWS fake_cws_;
+  policy::DevicePolicyCrosTestHelper policy_helper_;
+  std::unique_ptr<base::AutoReset<bool>> skip_splash_wait_override_;
+  EmbeddedTestServerSetupMixin embedded_test_server_{&mixin_host_,
+                                                     embedded_test_server()};
+
+  DeviceStateMixin device_state_{
+      &mixin_host_, DeviceStateMixin::State::OOBE_COMPLETED_CLOUD_ENROLLED};
+  LoginManagerMixin login_manager_{&mixin_host_, {}};
+};
+
+IN_PROC_BROWSER_TEST_F(LoginLogoutReporterKioskBrowserTest,
+                       LoginSuccessfulThenLogout) {
+  MissiveClientTestObserver observer(Destination::LOGIN_LOGOUT_EVENTS);
+  test::WaitForPrimaryUserSessionStart();
+
+  user_manager::UserManager* user_manager = user_manager::UserManager::Get();
+  ASSERT_TRUE(user_manager->IsLoggedInAsKioskApp());
+
+  absl::optional<Record> login_record = MaybeGetEnqueudLoginLogoutRecord();
+
+  if (!login_record.has_value()) {
+    // Record is not enqueued yet, so wait for it.
+    login_record = GetNextLoginLogoutRecord(&observer);
+  }
+
+  LoginLogoutRecord login_record_data;
+  ASSERT_TRUE(login_record_data.ParseFromString(login_record->data()));
+  EXPECT_THAT(login_record_data.session_type(),
+              Eq(LoginLogoutSessionType::KIOSK_SESSION));
+  EXPECT_FALSE(login_record_data.has_affiliated_user());
+  ASSERT_TRUE(login_record_data.has_login_event());
+  EXPECT_FALSE(login_record_data.login_event().has_failure());
+
+  // Wait for the window to appear.
+  extensions::AppWindow* const window =
+      apps::AppWindowWaiter(extensions::AppWindowRegistry::Get(
+                                ProfileManager::GetPrimaryUserProfile()),
+                            GetTestAppId())
+          .Wait();
+  ASSERT_TRUE(window);
+
+  // Terminate the app.
+  window->GetBaseWindow()->Close();
+
+  Record logout_record = GetNextLoginLogoutRecord(&observer);
+  LoginLogoutRecord logout_record_data;
+  ASSERT_TRUE(logout_record_data.ParseFromString(logout_record.data()));
+  EXPECT_THAT(logout_record_data.session_type(),
+              Eq(LoginLogoutSessionType::KIOSK_SESSION));
+  EXPECT_FALSE(logout_record_data.has_affiliated_user());
+  EXPECT_TRUE(logout_record_data.has_logout_event());
 }
 
 }  // namespace
