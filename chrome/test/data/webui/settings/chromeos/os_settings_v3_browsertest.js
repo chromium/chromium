2dd2509596a5cd203d46ff9652a1480f825e67fe
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-07-28 05:11:28 +0000

    sheriff: Disable OSSettingsOsBluetoothSavedDevicesListV3Test.AllJsTests
    Builder: linux-chromeos-dbg https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg
    
    First failing build: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/b8807537157289235377
    
    Change-Id: Iab6590c5ef842e7d8b1faf6a23678fd1fa9325a4
    Bug: b/240502461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787828
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029091}

--- chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
@@ -207,9 +207,11 @@ var OSSettingsOsBluetoothSavedDevicesListV3Test =
   }
 };
 
-TEST_F('OSSettingsOsBluetoothSavedDevicesListV3Test', 'AllJsTests', () => {
-  mocha.run();
-});
+TEST_F(
+    'OSSettingsOsBluetoothSavedDevicesListV3Test', 'DISABLED_AllJsTests',
+    () => {
+      mocha.run();
+    });
 
 var OSSettingsSearchEngineV3Test = class extends OSSettingsV3BrowserTest {
   /** @override */
