f6bdb57fc8a8224b265a824adf547cdbac8f6e31
Ben Reich <benreich@chromium.org>
2022-07-29 00:56:42 +0000

    [MirrorSync] Add basic UI structure for folder-selector
    The <folder-selector> component will serve as a hierarchy picker to
    choose which folders to start syncing. This implements the
    <manage-mirrorsync> component as the conduit for mojo interaction and
    the <folder-selector> as the place to take UI event.
    
    The UX for this has not been finalised, so this is a best attempt at
    the current mocks [1]. Avoiding implementing the button on submit for
    now as we have no endpoint to send it to, this and tests will come in
    a follow up CL.
    
    Demo: http://shortn/_uTHQWj46Ox
    
    [1] http://shortn/_CGH3a3M6oL
    
    Bug: b:237066325
    Change-Id: Ifa39c12c3a5dd576f0495ec0877d3e5bbb9e02e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3787665
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029556}

--- chrome/browser/resources/chromeos/manage_mirrorsync/components/manage_mirrorsync.ts
+++ chrome/browser/resources/chromeos/manage_mirrorsync/components/manage_mirrorsync.ts
@@ -4,6 +4,9 @@
 
 import './folder_selector.js';
 
+import {BrowserProxy} from '../browser_proxy.js';
+
+import {FOLDER_EXPANDED, FolderExpandedEvent, FolderSelector} from './folder_selector.js';
 import {getTemplate} from './manage_mirrorsync.html.js';
 
 /**
@@ -11,25 +14,42 @@ import {getTemplate} from './manage_mirrorsync.html.js';
  * state for the chrome://manage-mirrorsync dialog.
  */
 class ManageMirrorSync extends HTMLElement {
+  /**
+   * The <folder-selector> component on the page. Gets set when the
+   * <manage-mirrorsync> components connects to the DOM.
+   */
+  private folderSelector?: FolderSelector;
+
+  /**
+   * Bind the onSpecifyFolderSelection event listener to enable removal of it
+   * once the first click has been registered.
+   */
+  private onSpecifyFolderSelectionBound: (event: Event) => void;
+
   constructor() {
     super();
     this.attachShadow({mode: 'open'})
         .appendChild(getTemplate().content.cloneNode(true));
+
+    this.onSpecifyFolderSelectionBound =
+        this.onSpecifyFolderSelection.bind(this);
   }
 
   /**
    * Invoked when the <manage-mirrorsync> web component is attached to the DOM.
    */
   connectedCallback() {
-    this.shadowRoot?.querySelector('#selected')
-        ?.addEventListener('click', this.onSpecifyFolderSelection.bind(this));
+    this.shadowRoot!.getElementById('selected')!.addEventListener(
+        'click', this.onSpecifyFolderSelectionBound);
+    this.folderSelector =
+        this.shadowRoot!.querySelector('folder-selector') as FolderSelector;
   }
 
   /**
    * Shows the folder-selector web component if the specific folder selection
    * option was chosen. Doesn't hide after initially shown.
    */
-  private onSpecifyFolderSelection(event: Event) {
+  private async onSpecifyFolderSelection(event: Event) {
     if (!event.currentTarget) {
       return;
     }
@@ -37,8 +57,27 @@ class ManageMirrorSync extends HTMLElement {
     if (!isChecked) {
       return;
     }
-    this.shadowRoot?.querySelector('folder-selector')
-        ?.toggleAttribute('hidden', false);
+    this.folderSelector?.addEventListener(
+        FOLDER_EXPANDED,
+        (event: FolderExpandedEvent) => this.onFolderExpanded(event.detail));
+    this.folderSelector?.toggleAttribute('hidden', false);
+    this.shadowRoot!.getElementById('selected')!.removeEventListener(
+        'click', this.onSpecifyFolderSelectionBound);
+
+    // Kick off retrieving the child folders for the root path.
+    this.onFolderExpanded('/');
+  }
+
+  /**
+   * When a folder gets expanded, kick off the retrieval of the child folders.
+   * This will call back into the <folder-selector> element with the data on
+   * retrieval.
+   */
+  private async onFolderExpanded(path: string) {
+    const proxy = BrowserProxy.getInstance().handler;
+    const folderPaths =
+        (await proxy.getChildFolders({path})).paths.map(({path}) => path);
+    this.folderSelector?.addChildFolders(folderPaths);
   }
 }
 
