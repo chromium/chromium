1e88d18d823509470b34885a46592cf8d980178c
Ernesto Izquierdo Clua <eic@google.com>
2022-07-29 17:39:08 +0000

    [CVC] Added header for new cvc component
    This patch adds the new CVC's header view and its model. Configuration
    of the header based on the cvc state(cvc only input, cvc + expiry date,
    error states) will be done in future patches after the rest of ui
    components are created.
    
    Fixed: 1345652
    Change-Id: I9f7a11490cedcdd3a46c04b755d8961abde12d1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3780595
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1029855}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.mm
@@ -4,13 +4,30 @@
 
 #import "ios/chrome/browser/ui/autofill/card_unmask_prompt_view_controller.h"
 
+#import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/browser/ui/payments/card_unmask_prompt_controller.h"
 #import "ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h"
+#import "ios/chrome/browser/ui/autofill/cells/cvc_header_item.h"
 #import "ios/chrome/browser/ui/table_view/table_view_utils.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+NSString* const kCardUnmaskPromptTableViewAccessibilityID =
+    @"CardUnmaskPromptTableViewAccessibilityID";
+
+namespace {
+
+typedef NS_ENUM(NSInteger, SectionIdentifier) {
+  SectionIdentifierMain = kSectionIdentifierEnumZero,
+};
+
+typedef NS_ENUM(NSInteger, ItemType) {
+  ItemTypeHeader = kItemTypeEnumZero,
+};
+
+}  // namespace
 
 @interface CardUnmaskPromptViewController () {
   // Owns `self`.
@@ -31,12 +48,46 @@
   return self;
 }
 
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.tableView.accessibilityIdentifier =
+      kCardUnmaskPromptTableViewAccessibilityID;
+
+  [self loadModel];
+}
+
+- (void)loadModel {
+  [super loadModel];
+
+  TableViewModel* model = self.tableViewModel;
+  [model addSectionWithIdentifier:SectionIdentifierMain];
+
+  [model setHeader:[self createHeaderItem]
+      forSectionWithIdentifier:SectionIdentifierMain];
+}
+
 #pragma mark - UIAdaptivePresentationControllerDelegate
+
 - (void)presentationControllerDidDismiss:
     (UIPresentationController*)presentationController {
   // Notify bridge that UI was dismissed.
   _bridge->NavigationControllerDismissed();
   _bridge = nullptr;
+}
+
+#pragma mark - Private
+
+// Returns a newly created item for the footer of the section.
+- (CVCHeaderItem*)createHeaderItem {
+  autofill::CardUnmaskPromptController* controller = _bridge->GetController();
+  NSString* instructions =
+      base::SysUTF16ToNSString(controller->GetInstructionsMessage());
+
+  CVCHeaderItem* header = [[CVCHeaderItem alloc] initWithType:ItemTypeHeader];
+  header.instructionsText = instructions;
+
+  return header;
 }
 
 @end
