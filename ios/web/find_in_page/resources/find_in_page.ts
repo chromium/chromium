243810787f80e9f65dd0d1ac2008e3c8690eb890
Mike Dougherty <michaeldo@chromium.org>
2022-07-29 22:47:22 +0000

    [iOS][Find in Page] Convert Match class to TypeScript
    Move Match class from find_in_page_native_api.js to find_in_page.ts. The
    only change in behavior is a fix for a TypeScript error where opacity
    was being compared to the number 0, which will always fail. In order to
    properly compare the element opacity, the item is checked for a
    non-empty value, then converted to a number.
    
    Bug: 1348327
    Change-Id: I0051d333705aa15fdc876de7fdc2d1f98757700c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3793178
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1029982}

--- ios/web/find_in_page/resources/find_in_page.ts
+++ ios/web/find_in_page/resources/find_in_page.ts
@@ -3,6 +3,179 @@
 // found in the LICENSE file.
 
 /**
+ * Class name of CSS element that selects a highlighted match with orange.
+ * @type {string}
+ */
+const CSS_CLASS_NAME_SELECT = 'find_selected';
+
+/**
+ * Returns the width of the document.body.  Sometimes though the body lies to
+ * try to make the page not break rails, so attempt to find those as well.
+ * An example: wikipedia pages for the ipad.
+ * @return {number} Width of the document body.
+ */
+function getBodyWidth_(): number {
+  let body = document.body;
+  let documentElement = document.documentElement;
+  return Math.max(
+      body.scrollWidth, documentElement.scrollWidth, body.offsetWidth,
+      documentElement.offsetWidth, body.clientWidth,
+      documentElement.clientWidth);
+};
+
+/**
+ * Returns the height of the document.body.  Sometimes though the body lies to
+ * try to make the page not break rails, so attempt to find those as well.
+ * An example: wikipedia pages for the ipad.
+ * @return {number} Height of the document body.
+ */
+function getBodyHeight_(): number {
+  let body = document.body;
+  let documentElement = document.documentElement;
+  return Math.max(
+      body.scrollHeight, documentElement.scrollHeight, body.offsetHeight,
+      documentElement.offsetHeight, body.clientHeight,
+      documentElement.clientHeight);
+};
+
+/**
+ * Helper function that determines if an element is visible.
+ * @param {Element} elem Element to check.
+ * @return {boolean} Whether elem is visible or not.
+ */
+function isElementVisible_(elem: HTMLElement): boolean {
+  if (!elem) {
+    return false;
+  }
+  let top = 0;
+  let left = 0;
+  let bottom = Infinity;
+  let right = Infinity;
+
+  let originalElement = elem;
+  let nextOffsetParent = originalElement.offsetParent;
+
+  // We are currently handling all scrolling through the app, which means we can
+  // only scroll the window, not any scrollable containers in the DOM itself. So
+  // for now this function returns false if the element is scrolled outside the
+  // viewable area of its ancestors.
+  // TODO(crbug.com/915357): handle scrolling within the DOM.
+  let bodyHeight = getBodyHeight_();
+  let bodyWidth = getBodyWidth_();
+
+  while (elem && elem.nodeName.toUpperCase() !== 'BODY') {
+    if (elem.style.display === 'none' || elem.style.visibility === 'hidden') {
+      return false;
+    }
+
+    // Check that there is a value set before converting to a Number, otherwise
+    // and empty string will convert to opacity zero and a visible item will be
+    // assumed hidden.
+    if (elem.style.opacity.length) {
+      const opacity = Number(elem.style.opacity);
+      if (!isNaN(opacity) && opacity === 0) {
+        return false;
+      }
+    }
+
+    if (elem.ownerDocument && elem.ownerDocument.defaultView) {
+      const computedStyle =
+          elem.ownerDocument.defaultView.getComputedStyle(elem, null);
+      if (computedStyle.display === 'none' ||
+          computedStyle.visibility === 'hidden') {
+        return false;
+      }
+
+      // Check that there is a value set before converting to a Number,
+      // otherwise and empty string will convert to opacity zero and a visible
+      // item will be assumed hidden.
+      if (computedStyle.opacity.length) {
+        const opacity = Number(computedStyle.opacity);
+        if (!isNaN(opacity) && opacity === 0) {
+          return false;
+        }
+      }
+    }
+
+    // For the original element and all ancestor offsetParents, trim down the
+    // visible area of the original element.
+    if (elem.isSameNode(originalElement) || elem.isSameNode(nextOffsetParent)) {
+      let visible = elem.getBoundingClientRect();
+      if (elem.style.overflow === 'hidden' &&
+          (visible.width === 0 || visible.height === 0))
+        return false;
+
+      top = Math.max(top, visible.top + window.pageYOffset);
+      bottom = Math.min(bottom, visible.bottom + window.pageYOffset);
+      left = Math.max(left, visible.left + window.pageXOffset);
+      right = Math.min(right, visible.right + window.pageXOffset);
+
+      // The element is not within the original viewport.
+      let notWithinViewport = top < 0 || left < 0;
+
+      // The element is flowing off the boundary of the page. Note this is
+      // not comparing to the size of the window, but the calculated offset
+      // size of the document body. This can happen if the element is within
+      // a scrollable container in the page.
+      let offPage = right > bodyWidth || bottom > bodyHeight;
+      if (notWithinViewport || offPage) {
+        return false;
+      }
+      nextOffsetParent = elem.offsetParent;
+    }
+
+    if (!(elem.parentNode instanceof HTMLElement)) {
+      break;
+    }
+
+    elem = elem.parentNode;
+  }
+  return true;
+};
+
+
+/**
+ * A Match represents a match result in the document. |this.nodes| stores all
+ * the <chrome_find> Nodes created for highlighting the matched text. If it
+ * contains only one Node, it means the match is found within one HTML TEXT
+ * Node, otherwise the match involves multiple HTML TEXT Nodes.
+ */
+class Match {
+  nodes: HTMLElement[] = [];
+
+  /**
+   * Returns if all <chrome_find> Nodes of this match are visible.
+   * @return {Boolean} If the Match is visible.
+   */
+  visible(): boolean {
+    for (const node of this.nodes) {
+      if (!isElementVisible_(node))
+        return false;
+    }
+    return true;
+  }
+
+  /**
+   * Adds orange color highlight for "selected match result", over the yellow
+   * color highlight for "normal match result".
+   */
+  addSelectHighlight(): void {
+    for (const node of this.nodes) {
+      node.classList.add(CSS_CLASS_NAME_SELECT);
+    }
+  }
+
+  /**
+   * Clears the orange color highlight.
+   */
+  removeSelectHighlight(): void {
+    for (const node of this.nodes) {
+      node.classList.remove(CSS_CLASS_NAME_SELECT);
+    }
+  }
+}
+
+/**
  * A Section contains the info of one TEXT node in the |allText_|. The node's
  * textContent is [begin, end) of |allText_|.
  */
@@ -23,4 +196,4 @@ class Section {
   }
 }
 
-export {Section}
+export {CSS_CLASS_NAME_SELECT, Match, Section}
--- ios/web/find_in_page/resources/find_in_page_native_api.js
+++ ios/web/find_in_page/resources/find_in_page_native_api.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {Section} from '//ios/web/find_in_page/resources/find_in_page.js';
+import {CSS_CLASS_NAME_SELECT, Match, Section} from '//ios/web/find_in_page/resources/find_in_page.js';
 
 /**
  * Based on code from the Google iOS app.
@@ -60,51 +60,6 @@ function findFirstSectionEndsAfter_(index) {
     }
   }
   return left;
-}
-
-/**
- * A Match represents a match result in the document. |this.nodes| stores all
- * the <chrome_find> Nodes created for highlighting the matched text. If it
- * contains only one Node, it means the match is found within one HTML TEXT
- * Node, otherwise the match involves multiple HTML TEXT Nodes.
- */
-class Match {
-  constructor() {
-    this.nodes = [];
-  }
-
-  /**
-   * Returns if all <chrome_find> Nodes of this match are visible.
-   * @return {Boolean} If the Match is visible.
-   */
-  visible() {
-    for (let i = 0; i < this.nodes.length; ++i) {
-      if (!isElementVisible_(this.nodes[i]))
-        return false;
-    }
-    return true;
-  }
-
-  /**
-   * Adds orange color highlight for "selected match result", over the yellow
-   * color highlight for "normal match result".
-   * @return {undefined}
-   */
-  addSelectHighlight() {
-    for (let i = 0; i < this.nodes.length; ++i) {
-      this.nodes[i].classList.add(CSS_CLASS_NAME_SELECT);
-    }
-  }
-
-  /**
-   * Clears the orange color highlight.
-   * @return {undefined}
-   */
-  removeSelectHighlight() {
-    for (let i = 0; i < this.nodes.length; ++i) {
-      this.nodes[i].classList.remove(CSS_CLASS_NAME_SELECT);
-    }
-  }
 }
 
 /**
@@ -324,12 +279,6 @@ const IGNORE_NODE_NAMES = new Set([
  * @type {string}
  */
 const CSS_CLASS_NAME = 'find_in_page';
-
-/**
- * Class name of CSS element that selects a highlighted match with orange.
- * @type {string}
- */
-const CSS_CLASS_NAME_SELECT = 'find_selected';
 
 /**
  * ID of CSS style.
@@ -767,103 +716,6 @@ __gCrWeb.findInPage.stop = function() {
     cleanUp_();
   }
   __gCrWeb.findInPage.hasInitialized = false;
-};
-
-/**
- * Returns the width of the document.body.  Sometimes though the body lies to
- * try to make the page not break rails, so attempt to find those as well.
- * An example: wikipedia pages for the ipad.
- * @return {number} Width of the document body.
- */
-function getBodyWidth_() {
-  let body = document.body;
-  let documentElement = document.documentElement;
-  return Math.max(
-      body.scrollWidth, documentElement.scrollWidth, body.offsetWidth,
-      documentElement.offsetWidth, body.clientWidth,
-      documentElement.clientWidth);
-};
-
-/**
- * Returns the height of the document.body.  Sometimes though the body lies to
- * try to make the page not break rails, so attempt to find those as well.
- * An example: wikipedia pages for the ipad.
- * @return {number} Height of the document body.
- */
-function getBodyHeight_() {
-  let body = document.body;
-  let documentElement = document.documentElement;
-  return Math.max(
-      body.scrollHeight, documentElement.scrollHeight, body.offsetHeight,
-      documentElement.offsetHeight, body.clientHeight,
-      documentElement.clientHeight);
-};
-
-/**
- * Helper function that determines if an element is visible.
- * @param {Element} elem Element to check.
- * @return {boolean} Whether elem is visible or not.
- */
-function isElementVisible_(elem) {
-  if (!elem) {
-    return false;
-  }
-  let top = 0;
-  let left = 0;
-  let bottom = Infinity;
-  let right = Infinity;
-
-  let originalElement = elem;
-  let nextOffsetParent = originalElement.offsetParent;
-
-  // We are currently handling all scrolling through the app, which means we can
-  // only scroll the window, not any scrollable containers in the DOM itself. So
-  // for now this function returns false if the element is scrolled outside the
-  // viewable area of its ancestors.
-  // TODO(crbug.com/915357): handle scrolling within the DOM.
-  let bodyHeight = getBodyHeight_();
-  let bodyWidth = getBodyWidth_();
-
-  while (elem && elem.nodeName.toUpperCase() != 'BODY') {
-    let computedStyle =
-        elem.ownerDocument.defaultView.getComputedStyle(elem, null);
-
-    if (elem.style.display === 'none' || elem.style.visibility === 'hidden' ||
-        elem.style.opacity === 0 || computedStyle.display === 'none' ||
-        computedStyle.visibility === 'hidden' || computedStyle.opacity === 0) {
-      return false;
-    }
-
-    // For the original element and all ancestor offsetParents, trim down the
-    // visible area of the original element.
-    if (elem.isSameNode(originalElement) || elem.isSameNode(nextOffsetParent)) {
-      let visible = elem.getBoundingClientRect();
-      if (elem.style.overflow === 'hidden' &&
-          (visible.width === 0 || visible.height === 0))
-        return false;
-
-      top = Math.max(top, visible.top + window.pageYOffset);
-      bottom = Math.min(bottom, visible.bottom + window.pageYOffset);
-      left = Math.max(left, visible.left + window.pageXOffset);
-      right = Math.min(right, visible.right + window.pageXOffset);
-
-      // The element is not within the original viewport.
-      let notWithinViewport = top < 0 || left < 0;
-
-      // The element is flowing off the boundary of the page. Note this is
-      // not comparing to the size of the window, but the calculated offset
-      // size of the document body. This can happen if the element is within
-      // a scrollable container in the page.
-      let offPage = right > bodyWidth || bottom > bodyHeight;
-      if (notWithinViewport || offPage) {
-        return false;
-      }
-      nextOffsetParent = elem.offsetParent;
-    }
-
-    elem = elem.parentNode;
-  }
-  return true;
 };
 
 /**
