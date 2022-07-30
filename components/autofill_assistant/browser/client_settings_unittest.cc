105b32c0c05a7f85620de6bba9ba38b1d134fbfc
Matteo Ruello <mruel@google.com>
2022-07-30 10:43:56 +0000

    Increase UT coverage for client_settings
    This adds tests for all previously untested fields in client_settings.cc
    
    Bug: b/240654322
    Change-Id: I0fd6416acab40f666fc35b05c304a4850d4f67e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3792009
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Auto-Submit: Matteo Ruello <mruel@google.com>
    Commit-Queue: Matteo Ruello <mruel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1030043}

--- components/autofill_assistant/browser/client_settings_unittest.cc
+++ components/autofill_assistant/browser/client_settings_unittest.cc
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 
 #include "components/autofill_assistant/browser/client_settings.h"
+#include <string>
+#include "base/time/time.h"
 #include "components/autofill_assistant/browser/mock_client.h"
 
 namespace autofill_assistant {
@@ -41,6 +43,29 @@ TEST_F(ClientSettingsTest, CheckLegacyOverlayImage) {
   ASSERT_TRUE(settings.overlay_image.has_value());
   EXPECT_EQ(settings.overlay_image->image_drawable().bitmap().url(),
             "https://www.example.com/favicon.ico");
+}
+
+TEST_F(ClientSettingsTest, NoImageSizeInvalidOverlay) {
+  ClientSettingsProto proto;
+  proto.mutable_overlay_image()->set_image_url(
+      "https://www.example.com/favicon.ico");
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_FALSE(settings.overlay_image.has_value());
+}
+
+TEST_F(ClientSettingsTest, TextWithoutColorOrSizeInvalidOverlay) {
+  ClientSettingsProto proto;
+  proto.mutable_overlay_image()->set_image_url(
+      "https://www.example.com/favicon.ico");
+  proto.mutable_overlay_image()->set_text("Test text");
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_FALSE(settings.overlay_image.has_value());
 }
 
 TEST_F(ClientSettingsTest, NoDisplayStringsNoLocale) {
@@ -186,6 +211,127 @@ TEST_F(ClientSettingsTest, EmptyUpdateDoesNotResetDisplayStrings) {
                               Pair(ClientSettingsProto::GIVE_UP, "give_up"),
                               Pair(ClientSettingsProto::MAYBE_GIVE_UP,
                                    "maybe_give_up")))));
+}
+
+TEST_F(ClientSettingsTest, PeriodicIntervalValues) {
+  ClientSettingsProto proto;
+  proto.set_periodic_script_check_interval_ms(987);
+  proto.set_periodic_element_check_interval_ms(876);
+  proto.set_element_position_update_interval_ms(765);
+  proto.set_short_wait_for_element_deadline_ms(654);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.periodic_script_check_interval, base::Milliseconds(987));
+  EXPECT_EQ(settings.periodic_element_check_interval, base::Milliseconds(876));
+  EXPECT_EQ(settings.element_position_update_interval, base::Milliseconds(765));
+  EXPECT_EQ(settings.short_wait_for_element_deadline, base::Milliseconds(654));
+}
+
+TEST_F(ClientSettingsTest, BoxModel) {
+  ClientSettingsProto proto;
+  proto.set_box_model_check_interval_ms(543);
+  proto.set_box_model_check_count(6);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.box_model_check_interval, base::Milliseconds(543));
+  EXPECT_EQ(settings.box_model_check_count, 6);
+}
+
+TEST_F(ClientSettingsTest, DocumentReadyCheckTimeoutValue) {
+  ClientSettingsProto proto;
+  proto.set_document_ready_check_timeout_ms(432);
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.document_ready_check_timeout, base::Milliseconds(432));
+}
+
+TEST_F(ClientSettingsTest, CancelDelayValue) {
+  ClientSettingsProto proto;
+  proto.set_cancel_delay_ms(321);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.cancel_delay, base::Milliseconds(321));
+}
+
+TEST_F(ClientSettingsTest, Tap) {
+  ClientSettingsProto proto;
+  proto.set_tap_count(8);
+  proto.set_tap_tracking_duration_ms(210);
+  proto.set_tap_shutdown_delay_ms(109);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.tap_count, 8);
+  EXPECT_EQ(settings.tap_tracking_duration, base::Milliseconds(210));
+  EXPECT_EQ(settings.tap_shutdown_delay, base::Milliseconds(109));
+}
+
+TEST_F(ClientSettingsTest, TalkBackSheetSizeFractionValue) {
+  ClientSettingsProto proto;
+  proto.set_talkback_sheet_size_fraction(.6f);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.talkback_sheet_size_fraction, .6f);
+}
+
+TEST_F(ClientSettingsTest, SelectorObserver) {
+  ClientSettingsProto proto;
+  proto.set_selector_observer_debounce_interval_ms(987);
+  proto.set_selector_observer_extra_timeout_ms(876);
+
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_EQ(settings.selector_observer_extra_timeout, base::Milliseconds(876));
+  EXPECT_EQ(settings.selector_observer_debounce_interval,
+            base::Milliseconds(987));
+}
+
+TEST_F(ClientSettingsTest, SlowWarningSettings) {
+  ClientSettingsProto proto;
+  ClientSettingsProto::SlowWarningSettings slow_settings;
+
+  slow_settings.set_enable_slow_connection_warnings(true);
+  slow_settings.set_enable_slow_website_warnings(true);
+  slow_settings.set_only_show_warning_once(true);
+  slow_settings.set_only_show_connection_warning_once(true);
+  slow_settings.set_only_show_website_warning_once(true);
+  slow_settings.set_warning_delay_ms(987);
+  slow_settings.set_slow_roundtrip_threshold_ms(876);
+  slow_settings.set_max_consecutive_slow_roundtrips(765);
+  slow_settings.set_minimum_warning_message_duration_ms(654);
+  slow_settings.set_message_mode(
+      ClientSettingsProto::SlowWarningSettings::CONCATENATE);
+  slow_settings.set_slow_connection_message("Slow Connection");
+  slow_settings.set_slow_website_message("Slow Website");
+
+  *proto.mutable_slow_warning_settings() = slow_settings;
+  ClientSettings settings;
+  settings.UpdateFromProto(proto);
+
+  EXPECT_TRUE(settings.enable_slow_connection_warnings);
+  EXPECT_TRUE(settings.enable_slow_website_warnings);
+  EXPECT_TRUE(settings.only_show_warning_once);
+  EXPECT_TRUE(settings.only_show_connection_warning_once);
+  EXPECT_TRUE(settings.only_show_website_warning_once);
+  EXPECT_EQ(settings.warning_delay, base::Milliseconds(987));
+  EXPECT_EQ(settings.slow_roundtrip_threshold, base::Milliseconds(876));
+  EXPECT_EQ(settings.max_consecutive_slow_roundtrips, 765);
+  EXPECT_EQ(settings.minimum_warning_duration, base::Milliseconds(654));
+  EXPECT_EQ(settings.message_mode,
+            ClientSettingsProto::SlowWarningSettings::CONCATENATE);
+  EXPECT_EQ(settings.slow_connection_message, "Slow Connection");
+  EXPECT_EQ(settings.slow_website_message, "Slow Website");
 }
 }  // namespace
 }  // namespace autofill_assistant
