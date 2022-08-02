0fb09b3e2e50c57d2c1c14337f2f4a66a8240dbf
L. David Baron <dbaron@chromium.org>
2022-08-02 22:36:28 +0000

    Fix stack-use-after-scope by not returning new Vector.
    This fixes this getter method to return a const reference to the
    existing vector, which avoids a stack-use-after-scope in
    Element::ChangeToggle.
    
    Bug: 1250716
    Change-Id: I166d099fa37745522352df64deddcb7be51a9e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806601
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030786}

--- third_party/blink/renderer/core/style/toggle_root.h
+++ third_party/blink/renderer/core/style/toggle_root.h
@@ -125,7 +125,7 @@ class ToggleRoot {
   bool operator!=(const ToggleRoot& other) const { return !(*this == other); }
 
   const AtomicString& Name() const { return name_; }
-  States StateSet() const { return states_; }
+  const States& StateSet() const { return states_; }
   State InitialState() const { return value_; }
   ToggleOverflow Overflow() const { return overflow_; }
   bool IsGroup() const { return is_group_; }
