e18f5d8d2d81b4b81b88b169267f2b13c128ab21
Maggie Cai <mxcai@google.com>
2022-08-02 04:36:47 +0000

    [Deduplication] Read deduplication data and store in service.
    This CL make the app deduplication service observe the app provisioning
    data manager to read the deduplication data and store it in the service.
    
    BUG=b/238394602
    
    Change-Id: If9197217e75e7d58f0fbff52207fa70dc4dd0227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784498
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030397}

--- chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
+++ chrome/browser/apps/app_deduplication_service/app_deduplication_service.h
@@ -5,20 +5,45 @@
 #ifndef CHROME_BROWSER_APPS_APP_DEDUPLICATION_SERVICE_APP_DEDUPLICATION_SERVICE_H_
 #define CHROME_BROWSER_APPS_APP_DEDUPLICATION_SERVICE_APP_DEDUPLICATION_SERVICE_H_
 
+#include <map>
+#include <string>
+
+#include "base/scoped_observation.h"
+#include "chrome/browser/apps/app_deduplication_service/duplicate_group.h"
+#include "chrome/browser/apps/app_deduplication_service/entry_types.h"
+#include "chrome/browser/apps/app_provisioning_service/app_provisioning_data_manager.h"
+#include "chrome/browser/apps/app_provisioning_service/proto/app_data.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class Profile;
 
-namespace apps {
+namespace apps::deduplication {
 
-class AppDeduplicationService : public KeyedService {
+class AppDeduplicationService : public KeyedService,
+                                public AppProvisioningDataManager::Observer {
  public:
   explicit AppDeduplicationService(Profile* profile);
   ~AppDeduplicationService() override;
   AppDeduplicationService(const AppDeduplicationService&) = delete;
   AppDeduplicationService& operator=(const AppDeduplicationService&) = delete;
+
+ private:
+  friend class AppDeduplicationServiceTest;
+  FRIEND_TEST_ALL_PREFIXES(AppDeduplicationServiceTest,
+                           OnDuplicatedAppsMapUpdated);
+
+  // AppProvisioningDataManager::Observer:
+  void OnDuplicatedAppsMapUpdated(
+      const proto::DuplicatedAppsMap& duplicated_apps_map) override;
+
+  std::map<std::string, DuplicateGroup> duplication_map_;
+  std::map<EntryId, std::string> entry_to_group_map_;
+
+  base::ScopedObservation<AppProvisioningDataManager,
+                          AppProvisioningDataManager::Observer>
+      app_provisioning_data_observeration_{this};
 };
 
-}  // namespace apps
+}  // namespace apps::deduplication
 
 #endif  // CHROME_BROWSER_APPS_APP_DEDUPLICATION_SERVICE_APP_DEDUPLICATION_SERVICE_H_
