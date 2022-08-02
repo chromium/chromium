4148da565100fec41d8557d6f856d6317786dcae
Nancy Wang <nancylingwang@chromium.org>
2022-08-02 06:56:21 +0000

    Enable the kAppServiceLaunchWithoutMojom flag.
    BUG=1253250
    
    Change-Id: I7bc88618c3c1d789d765f59d88b422d2dad2b85c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3791930
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030424}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -10,6 +10,6 @@ const base::Feature kAppServicePreferredAppsWithoutMojom{
     "AppServicePreferredAppsWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAppServiceLaunchWithoutMojom{
-    "AppServiceLaunchWithoutMojom", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AppServiceLaunchWithoutMojom", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace apps
