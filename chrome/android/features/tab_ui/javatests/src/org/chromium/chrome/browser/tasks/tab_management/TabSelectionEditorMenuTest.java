1e2dabedd487b448cc7b1bb4c9da60df996a5613
ckitagawa <ckitagawa@chromium.org>
2022-08-02 21:48:56 +0000

    [Android Bulk Tab Edit] Disable click test flakes
    MenuItem click fails with perform(click()) not possible as < 90%
    visible.
    
    ActionView click fails as the CallbackHelper never fired.
    
    Issue seems to be primarily on Android 12 perhaps an issue with
    animations not disabling? See https://crbug.com/1225707. That said
    I did see one occurrence on P so doing a blanket disable to be safe.
    
    I'm having trouble reproducing locally so disabling until I can fix.
    
    Bug: 1348666, 1348710
    Change-Id: Ie328eb6e6acae640d53857d94b9423dabbfcc611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806103
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030760}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorMenuTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorMenuTest.java
@@ -33,6 +33,7 @@ import org.mockito.MockitoAnnotations;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -238,6 +239,7 @@ public class TabSelectionEditorMenuTest extends BlankUiTestActivityTestCase {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1348710")
     public void testSingleActionView_Click() throws Exception {
         List<FakeTabSelectionEditorAction> actions = new ArrayList<>();
         TestThreadUtils.runOnUiThreadBlocking(() -> {
@@ -321,6 +323,7 @@ public class TabSelectionEditorMenuTest extends BlankUiTestActivityTestCase {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1348666")
     public void testSingleMenuItem_Click() throws Exception {
         List<FakeTabSelectionEditorAction> actions = new ArrayList<>();
         TestThreadUtils.runOnUiThreadBlocking(() -> {
