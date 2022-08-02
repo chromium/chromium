c45e5039b68118f2e97efc8ee87d49ddcced90f9
Nasser Al-shawwa <alshawwa@chromium.org>
2022-08-02 18:11:47 +0000

    Mask username for reported password breach events
    Bug: 1238577
    Change-Id: I8bd26fa35f0416c6f1b0faacf6b8a3583f5b512a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3803645
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030659}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
@@ -873,8 +873,8 @@ TEST_F(SafeBrowsingPrivateEventRouterTest, TestOnPasswordBreach) {
   validator.ExpectPasswordBreachEvent(
       "SAFETY_CHECK",
       {
-          {"https://first.example.com/", u"first_user_name"},
-          {"https://second.example.com/", u"second_user_name"},
+          {"https://first.example.com/", u"*****"},
+          {"https://second.example.com/", u"*****@gmail.com"},
       },
       profile_->GetProfileUserName());
 
@@ -882,7 +882,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest, TestOnPasswordBreach) {
       "SAFETY_CHECK",
       {
           {GURL("https://first.example.com"), u"first_user_name"},
-          {GURL("https://second.example.com"), u"second_user_name"},
+          {GURL("https://second.example.com"), u"second_user_name@gmail.com"},
       });
 }
 
@@ -934,7 +934,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest,
   validator.ExpectPasswordBreachEvent(
       "SAFETY_CHECK",
       {
-          {"https://secondexample.com/", u"second_user_name"},
+          {"https://secondexample.com/", u"*****"},
       },
       profile_->GetProfileUserName());
 
