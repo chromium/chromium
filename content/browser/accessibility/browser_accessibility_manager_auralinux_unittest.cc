ee81e881e101e865e17fc87565a71eecd39759f9
Morten Stenshorne <mstensho@chromium.org>
2022-08-02 13:33:43 +0000

    Establish a BrowserTaskEnvironment in BrowserAccessibilityManagerAuraLinuxTest.
    Don't rely on someone else doing it for us. This test used to pass when
    run together with other unit tests, but not when run separately.
    Recently it had also started failing on the bots.
    
    Bug: 1349124
    Change-Id: I8f64a196205747f4472b130945bf66c5ac56ac85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804681
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Owners-Override: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1030512}

--- content/browser/accessibility/browser_accessibility_manager_auralinux_unittest.cc
+++ content/browser/accessibility/browser_accessibility_manager_auralinux_unittest.cc
@@ -9,6 +9,7 @@
 #include "content/browser/accessibility/browser_accessibility.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/accessibility/test_browser_accessibility_delegate.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/platform/ax_platform_node.h"
@@ -35,6 +36,9 @@ class BrowserAccessibilityManagerAuraLinuxTest : public testing::Test {
  private:
   void SetUp() override;
   ui::testing::ScopedAxModeSetter ax_mode_setter_;
+
+  // See crbug.com/1349124
+  content::BrowserTaskEnvironment task_environment_;
 };
 
 void BrowserAccessibilityManagerAuraLinuxTest::SetUp() {
