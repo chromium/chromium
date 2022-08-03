f10326dcc8a5f40b376f996d611c5f29eb25ce6d
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2022-08-03 20:24:56 +0000

    Remove String::IsolatedCopy from renderer/modules/webcodecs
    With the introduction of a shared AtomicStringTable and atomic
    ref_count, we have made WTF::StringImpl thread safe. Hence, making
    isolated copies of WTF::Strings is no longer required for thread safety.
    
    In this CL we remove the calls to String::IsolatedCopy() from
    renderer/modules/webcodecs/ files.
    
    Bug: 1083392
    Change-Id: Iaa112bcf21bf199874aaed5280ddfb371cf5a1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806128
    Commit-Queue: Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031176}

--- third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
@@ -184,7 +184,7 @@ ImageDecoderExternal::ImageDecoderExternal(ScriptState* script_state,
     }
 
     decoder_ = std::make_unique<WTF::SequenceBound<ImageDecoderCore>>(
-        decode_task_runner_, mime_type_.IsolatedCopy(), /*data=*/nullptr,
+        decode_task_runner_, mime_type_, /*data=*/nullptr,
         /*data_complete=*/false, alpha_option, color_behavior, desired_size,
         animation_option_);
 
@@ -249,7 +249,7 @@ ImageDecoderExternal::ImageDecoderExternal(ScriptState* script_state,
   data_complete_ = true;
   completed_property_->ResolveWithUndefined();
   decoder_ = std::make_unique<WTF::SequenceBound<ImageDecoderCore>>(
-      decode_task_runner_, mime_type_.IsolatedCopy(), std::move(segment_reader),
+      decode_task_runner_, mime_type_, std::move(segment_reader),
       data_complete_, alpha_option, color_behavior, desired_size,
       animation_option_);
 
