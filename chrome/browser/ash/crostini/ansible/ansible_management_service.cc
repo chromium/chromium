chromium/chromium

commit c58956d881b3e58bde8ca7af863a6571f3544729

Adds reporting current ansible status message to the configuration dialog
+ [ansible_management_service] Adds functionality to report IN_PROGRESS
  signals
+ [crostini_ansible_software_config_view] Adds visual element which
  updates on PlaybookSignalProgress sent from
  ansible_management_service.
+ [crostini_ansible_software_config_view] Adds test to validate progress
  signal is correctly sent.

BUG: b:237345049
TEST: Browsertests/unittests run.
Change-Id: I1119c3d9326389c11cb7503614297107fddf1843
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746057
Commit-Queue: Justin Huang <justinhuang@google.com>
Reviewed-by: Nicholas Verne <nverne@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1026054}

chrome/browser/ash/crostini/ansible/ansible_management_service.cc
@@ -225,7 +225,12 @@ void AnsibleManagementService::OnApplyAnsiblePlaybookProgress(
       OnConfigurationFinished(container_id, false);
       break;
     case vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::IN_PROGRESS:
-      // TODO(okalitova): Report Ansible playbook application progress.
+      for (auto& observer : observers_) {
+        observer.OnAnsibleSoftwareConfigurationProgress(
+            container_id,
+            std::vector<std::string>(signal.status_string().begin(),
+                                     signal.status_string().end()));
+      }
       break;
     default:
       NOTREACHED();

chrome/browser/ash/crostini/ansible/ansible_management_service.h
@@ -43,6 +43,9 @@ class AnsibleManagementService : public KeyedService,
     ~Observer() override = default;
     virtual void OnAnsibleSoftwareConfigurationStarted(
         const guest_os::GuestId& container_id) = 0;
+    virtual void OnAnsibleSoftwareConfigurationProgress(
+        const guest_os::GuestId& container_id,
+        const std::vector<std::string>& status_lines) {}
     virtual void OnAnsibleSoftwareConfigurationFinished(
         const guest_os::GuestId& container_id,
         bool success) = 0;

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.cc
@@ -6,6 +6,7 @@
 
 #include "base/callback_helpers.h"
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -82,6 +83,19 @@ std::u16string CrostiniAnsibleSoftwareConfigView::GetSubtextLabel() const {
 void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationStarted(
     const guest_os::GuestId& container_id) {}
 
+void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationProgress(
+    const guest_os::GuestId& container_id,
+    const std::vector<std::string>& status_lines) {
+  std::u16string status_string;
+  if (!base::UTF8ToUTF16(status_lines.back().c_str(),
+                         status_lines.back().size(), &status_string)) {
+    LOG(ERROR) << "Failed to convert status line into a UTF16 string: "
+               << status_lines.back();
+  } else {
+    progress_label_->SetText(status_string);
+  }
+}
+
 void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationFinished(
     const guest_os::GuestId& container_id,
     bool success) {
@@ -104,6 +118,11 @@ void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationFinished(
 std::u16string
 CrostiniAnsibleSoftwareConfigView::GetSubtextLabelStringForTesting() {
   return subtext_label_->GetText();
+}
+
+std::u16string
+CrostiniAnsibleSoftwareConfigView::GetProgressLabelStringForTesting() {
+  return progress_label_->GetText();
 }
 
 // static
@@ -135,12 +154,18 @@ CrostiniAnsibleSoftwareConfigView::CrostiniAnsibleSoftwareConfigView(
   subtext_label_ = AddChildView(std::move(subtext_label));
 
   // Add infinite progress bar.
-  // TODO(crbug.com/1000173): add progress reporting and display text above
-  // progress bar indicating current process.
   auto progress_bar = std::make_unique<views::ProgressBar>();
   // Values outside the range [0,1] display an infinite loading animation.
   progress_bar->SetValue(-1);
   progress_bar_ = AddChildView(std::move(progress_bar));
+
+  // Adds text below the infinite progress bar stating the last action.
+  // Currently can't really display the full progress since Ansible doesn't
+  // expose this by default w/o specific playbook hacks.
+  auto progress_label = std::make_unique<views::Label>();
+  progress_label->SetMultiLine(true);
+  progress_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  progress_label_ = AddChildView(std::move(progress_label));
 
   default_container_ansible_filepath_ = profile->GetPrefs()->GetFilePath(
       crostini::prefs::kCrostiniAnsiblePlaybookFilePath);

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.h
@@ -35,11 +35,15 @@ class CrostiniAnsibleSoftwareConfigView
   // crostini::AnsibleManagementService::Observer:
   void OnAnsibleSoftwareConfigurationStarted(
       const guest_os::GuestId& container_id) override;
+  void OnAnsibleSoftwareConfigurationProgress(
+      const guest_os::GuestId& container_id,
+      const std::vector<std::string>& status_lines) override;
   void OnAnsibleSoftwareConfigurationFinished(
       const guest_os::GuestId& container_id,
       bool success) override;
 
   std::u16string GetSubtextLabelStringForTesting();
+  std::u16string GetProgressLabelStringForTesting();
 
   static CrostiniAnsibleSoftwareConfigView* GetActiveViewForTesting();
 
@@ -61,6 +65,7 @@ class CrostiniAnsibleSoftwareConfigView
   crostini::AnsibleManagementService* ansible_management_service_ = nullptr;
 
   views::Label* subtext_label_ = nullptr;
+  views::Label* progress_label_ = nullptr;
   views::ProgressBar* progress_bar_ = nullptr;
   base::FilePath default_container_ansible_filepath_;
 

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view_browsertest.cc
@@ -4,7 +4,10 @@
 
 #include "chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.h"
 
+#include <string>
+
 #include "base/callback_helpers.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "chrome/browser/ash/crostini/ansible/ansible_management_service.h"
 #include "chrome/browser/ash/crostini/ansible/ansible_management_test_helper.h"
@@ -22,6 +25,8 @@
 #include "services/network/test/test_network_connection_tracker.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/devicetype_utils.h"
+
+constexpr char kProgressString[] = "Yesh milord. More work?";
 
 class CrostiniAnsibleSoftwareConfigViewBrowserTest
     : public CrostiniDialogBrowserTest,
@@ -48,11 +53,27 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
   // crostini::AnsibleManagementService::Observer
   void OnAnsibleSoftwareConfigurationStarted(
       const guest_os::GuestId& container_id) override {}
+
+  void OnAnsibleSoftwareConfigurationProgress(
+      const guest_os::GuestId& container_id,
+      const std::vector<std::string>& status_lines) override {}
+
   void OnAnsibleSoftwareConfigurationFinished(
       const guest_os::GuestId& container_id,
       bool success) override {}
 
   void OnApplyAnsiblePlaybook(const guest_os::GuestId& container_id) override {
+    if (send_ansible_progress_) {
+      EXPECT_NE(nullptr, ActiveView());
+      vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal signal;
+      signal.set_status(
+          vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::IN_PROGRESS);
+      signal.set_vm_name(crostini::DefaultContainerId().vm_name);
+      signal.set_container_name(crostini::DefaultContainerId().container_name);
+      signal.add_status_string(kProgressString);
+      ansible_management_service()->OnApplyAnsiblePlaybookProgress(signal);
+      status_string_ = ActiveView()->GetProgressLabelStringForTesting();
+    }
     if (is_apply_ansible_success_) {
       EXPECT_NE(nullptr, ActiveView());
       vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal signal;
@@ -154,7 +175,12 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
     is_install_ansible_success_ = success;
   }
 
+  void SetSendAnsibleProgress(bool show_progress) {
+    send_ansible_progress_ = show_progress;
+  }
+
   guest_os::GuestId container_id_;
+  std::u16string status_string_;
 
  private:
   bool HasAcceptButton() { return ActiveView()->GetOkButton() != nullptr; }
@@ -190,6 +216,7 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
 
   bool is_install_ansible_success_;
   bool is_apply_ansible_success_;
+  bool send_ansible_progress_;
   std::unique_ptr<network::TestNetworkConnectionTracker>
       network_connection_tracker_;
   std::unique_ptr<crostini::AnsibleManagementTestHelper> test_helper_;
@@ -292,6 +319,25 @@ IN_PROC_BROWSER_TEST_F(CrostiniAnsibleSoftwareConfigViewBrowserTest,
       base::BindLambdaForTesting([&](bool success) { run_loop()->Quit(); }));
 
   run_loop()->Run();
+
+  EXPECT_TRUE(HasNoView());
+}
+
+IN_PROC_BROWSER_TEST_F(CrostiniAnsibleSoftwareConfigViewBrowserTest,
+                       AnsibleConfigFlowWithProgress_Successful) {
+  SetSendAnsibleProgress(true);
+  ansible_management_service()->ConfigureContainer(
+      crostini::DefaultContainerId(),
+      browser()->profile()->GetPrefs()->GetFilePath(
+          crostini::prefs::kCrostiniAnsiblePlaybookFilePath),
+      base::BindLambdaForTesting([&](bool success) { run_loop()->Quit(); }));
+
+  run_loop()->Run();
+  std::u16string expected;
+  // -1 to anti the null at the end of the string.
+  ASSERT_TRUE(base::UTF8ToUTF16(kProgressString, sizeof(kProgressString) - 1,
+                                &expected));
+  EXPECT_EQ(status_string_, expected);
 
   EXPECT_TRUE(HasNoView());
 }

commit 1e1a83f2de281782acffb6dc9965f4942d30b46e

Reland "Adds reporting current ansible status message to the configuration dialog"
This is a reland of commit c58956d881b3e58bde8ca7af863a6571f3544729

Caused CQ errors because send_ansible_progress_ wasn't always
initialized.

Original change's description:
> Adds reporting current ansible status message to the configuration dialog
>
> + [ansible_management_service] Adds functionality to report IN_PROGRESS
>   signals
> + [crostini_ansible_software_config_view] Adds visual element which
>   updates on PlaybookSignalProgress sent from
>   ansible_management_service.
> + [crostini_ansible_software_config_view] Adds test to validate progress
>   signal is correctly sent.
>
> BUG: b:237345049
> TEST: Browsertests/unittests run.
> Change-Id: I1119c3d9326389c11cb7503614297107fddf1843
> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3746057
> Commit-Queue: Justin Huang <justinhuang@google.com>
> Reviewed-by: Nicholas Verne <nverne@chromium.org>
> Cr-Commit-Position: refs/heads/main@{#1026054}

Change-Id: I7341300cb2df647839c3067d49f0aaa0a97d673d
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3777017
Reviewed-by: Nicholas Verne <nverne@chromium.org>
Commit-Queue: Justin Huang <justinhuang@google.com>
Cr-Commit-Position: refs/heads/main@{#1030889}

chrome/browser/ash/crostini/ansible/ansible_management_service.cc
@@ -225,7 +225,12 @@ void AnsibleManagementService::OnApplyAnsiblePlaybookProgress(
       OnConfigurationFinished(container_id, false);
       break;
     case vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::IN_PROGRESS:
-      // TODO(okalitova): Report Ansible playbook application progress.
+      for (auto& observer : observers_) {
+        observer.OnAnsibleSoftwareConfigurationProgress(
+            container_id,
+            std::vector<std::string>(signal.status_string().begin(),
+                                     signal.status_string().end()));
+      }
       break;
     default:
       NOTREACHED();

chrome/browser/ash/crostini/ansible/ansible_management_service.h
@@ -43,6 +43,9 @@ class AnsibleManagementService : public KeyedService,
     ~Observer() override = default;
     virtual void OnAnsibleSoftwareConfigurationStarted(
         const guest_os::GuestId& container_id) = 0;
+    virtual void OnAnsibleSoftwareConfigurationProgress(
+        const guest_os::GuestId& container_id,
+        const std::vector<std::string>& status_lines) {}
     virtual void OnAnsibleSoftwareConfigurationFinished(
         const guest_os::GuestId& container_id,
         bool success) = 0;

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.cc
@@ -6,6 +6,7 @@
 
 #include "base/callback_helpers.h"
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -82,6 +83,19 @@ std::u16string CrostiniAnsibleSoftwareConfigView::GetSubtextLabel() const {
 void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationStarted(
     const guest_os::GuestId& container_id) {}
 
+void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationProgress(
+    const guest_os::GuestId& container_id,
+    const std::vector<std::string>& status_lines) {
+  std::u16string status_string;
+  if (!base::UTF8ToUTF16(status_lines.back().c_str(),
+                         status_lines.back().size(), &status_string)) {
+    LOG(ERROR) << "Failed to convert status line into a UTF16 string: "
+               << status_lines.back();
+  } else {
+    progress_label_->SetText(status_string);
+  }
+}
+
 void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationFinished(
     const guest_os::GuestId& container_id,
     bool success) {
@@ -104,6 +118,11 @@ void CrostiniAnsibleSoftwareConfigView::OnAnsibleSoftwareConfigurationFinished(
 std::u16string
 CrostiniAnsibleSoftwareConfigView::GetSubtextLabelStringForTesting() {
   return subtext_label_->GetText();
+}
+
+std::u16string
+CrostiniAnsibleSoftwareConfigView::GetProgressLabelStringForTesting() {
+  return progress_label_->GetText();
 }
 
 // static
@@ -135,12 +154,18 @@ CrostiniAnsibleSoftwareConfigView::CrostiniAnsibleSoftwareConfigView(
   subtext_label_ = AddChildView(std::move(subtext_label));
 
   // Add infinite progress bar.
-  // TODO(crbug.com/1000173): add progress reporting and display text above
-  // progress bar indicating current process.
   auto progress_bar = std::make_unique<views::ProgressBar>();
   // Values outside the range [0,1] display an infinite loading animation.
   progress_bar->SetValue(-1);
   progress_bar_ = AddChildView(std::move(progress_bar));
+
+  // Adds text below the infinite progress bar stating the last action.
+  // Currently can't really display the full progress since Ansible doesn't
+  // expose this by default w/o specific playbook hacks.
+  auto progress_label = std::make_unique<views::Label>();
+  progress_label->SetMultiLine(true);
+  progress_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  progress_label_ = AddChildView(std::move(progress_label));
 
   default_container_ansible_filepath_ = profile->GetPrefs()->GetFilePath(
       crostini::prefs::kCrostiniAnsiblePlaybookFilePath);

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.h
@@ -35,11 +35,15 @@ class CrostiniAnsibleSoftwareConfigView
   // crostini::AnsibleManagementService::Observer:
   void OnAnsibleSoftwareConfigurationStarted(
       const guest_os::GuestId& container_id) override;
+  void OnAnsibleSoftwareConfigurationProgress(
+      const guest_os::GuestId& container_id,
+      const std::vector<std::string>& status_lines) override;
   void OnAnsibleSoftwareConfigurationFinished(
       const guest_os::GuestId& container_id,
       bool success) override;
 
   std::u16string GetSubtextLabelStringForTesting();
+  std::u16string GetProgressLabelStringForTesting();
 
   static CrostiniAnsibleSoftwareConfigView* GetActiveViewForTesting();
 
@@ -61,6 +65,7 @@ class CrostiniAnsibleSoftwareConfigView
   crostini::AnsibleManagementService* ansible_management_service_ = nullptr;
 
   views::Label* subtext_label_ = nullptr;
+  views::Label* progress_label_ = nullptr;
   views::ProgressBar* progress_bar_ = nullptr;
   base::FilePath default_container_ansible_filepath_;
 

chrome/browser/ui/views/crostini/crostini_ansible_software_config_view_browsertest.cc
@@ -4,7 +4,10 @@
 
 #include "chrome/browser/ui/views/crostini/crostini_ansible_software_config_view.h"
 
+#include <string>
+
 #include "base/callback_helpers.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "chrome/browser/ash/crostini/ansible/ansible_management_service.h"
 #include "chrome/browser/ash/crostini/ansible/ansible_management_test_helper.h"
@@ -22,6 +25,8 @@
 #include "services/network/test/test_network_connection_tracker.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/devicetype_utils.h"
+
+constexpr char kProgressString[] = "Yesh milord. More work?";
 
 class CrostiniAnsibleSoftwareConfigViewBrowserTest
     : public CrostiniDialogBrowserTest,
@@ -48,11 +53,27 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
   // crostini::AnsibleManagementService::Observer
   void OnAnsibleSoftwareConfigurationStarted(
       const guest_os::GuestId& container_id) override {}
+
+  void OnAnsibleSoftwareConfigurationProgress(
+      const guest_os::GuestId& container_id,
+      const std::vector<std::string>& status_lines) override {}
+
   void OnAnsibleSoftwareConfigurationFinished(
       const guest_os::GuestId& container_id,
       bool success) override {}
 
   void OnApplyAnsiblePlaybook(const guest_os::GuestId& container_id) override {
+    if (send_ansible_progress_) {
+      EXPECT_NE(nullptr, ActiveView());
+      vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal signal;
+      signal.set_status(
+          vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::IN_PROGRESS);
+      signal.set_vm_name(crostini::DefaultContainerId().vm_name);
+      signal.set_container_name(crostini::DefaultContainerId().container_name);
+      signal.add_status_string(kProgressString);
+      ansible_management_service()->OnApplyAnsiblePlaybookProgress(signal);
+      status_string_ = ActiveView()->GetProgressLabelStringForTesting();
+    }
     if (is_apply_ansible_success_) {
       EXPECT_NE(nullptr, ActiveView());
       vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal signal;
@@ -108,6 +129,7 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
     // Set sensible defaults.
     is_install_ansible_success_ = true;
     is_apply_ansible_success_ = true;
+    send_ansible_progress_ = false;
   }
 
   void TearDownOnMainThread() override {
@@ -154,7 +176,12 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
     is_install_ansible_success_ = success;
   }
 
+  void SetSendAnsibleProgress(bool show_progress) {
+    send_ansible_progress_ = show_progress;
+  }
+
   guest_os::GuestId container_id_;
+  std::u16string status_string_;
 
  private:
   bool HasAcceptButton() { return ActiveView()->GetOkButton() != nullptr; }
@@ -190,6 +217,7 @@ class CrostiniAnsibleSoftwareConfigViewBrowserTest
 
   bool is_install_ansible_success_;
   bool is_apply_ansible_success_;
+  bool send_ansible_progress_;
   std::unique_ptr<network::TestNetworkConnectionTracker>
       network_connection_tracker_;
   std::unique_ptr<crostini::AnsibleManagementTestHelper> test_helper_;
@@ -292,6 +320,25 @@ IN_PROC_BROWSER_TEST_F(CrostiniAnsibleSoftwareConfigViewBrowserTest,
       base::BindLambdaForTesting([&](bool success) { run_loop()->Quit(); }));
 
   run_loop()->Run();
+
+  EXPECT_TRUE(HasNoView());
+}
+
+IN_PROC_BROWSER_TEST_F(CrostiniAnsibleSoftwareConfigViewBrowserTest,
+                       AnsibleConfigFlowWithProgress_Successful) {
+  SetSendAnsibleProgress(true);
+  ansible_management_service()->ConfigureContainer(
+      crostini::DefaultContainerId(),
+      browser()->profile()->GetPrefs()->GetFilePath(
+          crostini::prefs::kCrostiniAnsiblePlaybookFilePath),
+      base::BindLambdaForTesting([&](bool success) { run_loop()->Quit(); }));
+
+  run_loop()->Run();
+  std::u16string expected;
+  // -1 to anti the null at the end of the string.
+  ASSERT_TRUE(base::UTF8ToUTF16(kProgressString, sizeof(kProgressString) - 1,
+                                &expected));
+  EXPECT_EQ(status_string_, expected);
 
   EXPECT_TRUE(HasNoView());
 }