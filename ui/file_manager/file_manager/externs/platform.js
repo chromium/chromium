eb877e20377c287d2db3483fe67e8e4f4b740ef3
Luciano Pacheco <lucmult@chromium.org>
2022-08-03 12:36:08 +0000

    Files app: Remove unused files/code
    Since Audio Player has been removed we can remove some files.
    
    Change-Id: I47e63d95bfed28a220e4faa553ed1c263bf0bf4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3789552
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031016}

--- ui/file_manager/file_manager/externs/platform.js
+++ ui/file_manager/file_manager/externs/platform.js
@@ -11,35 +11,3 @@
  */
 Window.prototype.webkitResolveLocalFileSystemURL = function(
     url, successCallback, opt_errorCallback) {};
-
-/**
- * Media error: MEDIA_ERR_ABORTED.
- * @type {number}
- * @const
- * @see http://dev.w3.org/html5/spec-author-view/video.html#mediaerror
- */
-MediaError.MEDIA_ERR_ABORTED = 1;
-
-/**
- * Media error: MEDIA_ERR_NETWORK.
- * @type {number}
- * @const
- * @see http://dev.w3.org/html5/spec-author-view/video.html#mediaerror
- */
-MediaError.MEDIA_ERR_NETWORK = 2;
-
-/**
- * Media error: MEDIA_ERR_DECODE.
- * @type {number}
- * @const
- * @see http://dev.w3.org/html5/spec-author-view/video.html#mediaerror
- */
-MediaError.MEDIA_ERR_DECODE = 3;
-
-/**
- * Media error: MEDIA_ERR_SRC_NOT_SUPPORTED.
- * @type {number}
- * @const
- * @see http://dev.w3.org/html5/spec-author-view/video.html#mediaerror
- */
-MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED = 4;
