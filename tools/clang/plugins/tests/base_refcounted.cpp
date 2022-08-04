962a22c529a48f96cd754e6bc1ef739a43d5096b
Hans Wennborg <hans@chromium.org>
2022-08-04 15:31:37 +0000

    [clang] Update plugin test expectations after Clang diagnostics change
    Bug: 1348124
    Change-Id: Ic6eeb184e73c82a44dd7627de8c8d01b3d2daabb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3804865
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031505}

--- tools/clang/plugins/tests/base_refcounted.cpp
+++ tools/clang/plugins/tests/base_refcounted.cpp
@@ -43,7 +43,7 @@ class Foo {
     virtual ~BarInterface() {}
   };
 
-  typedef base::RefCounted<Foo::BarInterface> RefCountedBar; // TODO(crbug.com/1348124): Drop the Foo:: qualifier.
+  typedef base::RefCounted<BarInterface> RefCountedBar;
   typedef RefCountedBar AnotherTypedef;
 };
 
