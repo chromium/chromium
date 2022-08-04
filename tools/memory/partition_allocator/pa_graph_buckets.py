1073ca69e8d3a8527869eb8668fef1ce99023833
Thiabaud Engelbrecht <thiabaud@google.com>
2022-08-04 15:38:21 +0000

    [partition_alloc] Update script for bucketing scheme to display size
    Currently, the graphs from the script show only internal fragmentation.
    This CL updates them to also show the number of MiB wasted.
    
    Change-Id: Iea5aa7b5cac46bcbb818b8c8b8d92742c35b664a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3809572
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1031516}

--- tools/memory/partition_allocator/pa_graph_buckets.py
+++ tools/memory/partition_allocator/pa_graph_buckets.py
@@ -23,29 +23,36 @@ def main(argv: list[str]) -> None:
   DUMPNAME: final = "dump.dat"
 
   fig = plt.figure(figsize=(16, 8))
-  ax = fig.add_subplot(1, 1, 1)
+  ax1 = fig.add_subplot(1, 1, 1)
+  ax2 = ax1.twinx()
 
   def animate(i):
     bucket_sizes = []
     x = []
-    y = []
+    y1 = []
+    y2 = []
     with open(DUMPNAME, 'r') as f:
       for line in f.readlines():
         index, bucket_size, num_allocs, total_size, fragmentation = line.strip(
         ).split(',')
         print(index, bucket_size, num_allocs, total_size, fragmentation)
         x.append(int(index))
-        bucket_sizes.append(int(bucket_size))
-        y.append(int(fragmentation))
+        # format buckets sizes with commas, e.g. 50000 -> 50,000
+        bucket_sizes.append('{:,}'.format(int(bucket_size)))
+        y1.append(int(fragmentation))
+        y2.append(int(total_size) * int(fragmentation) / 100)
 
-    ax.clear()
-    plt.xticks(x, bucket_sizes, rotation='vertical')
-    plt.ylim((0, 100))
+    ax1.clear()
+    ax2.clear()
+    ax1.set_xticks(x, bucket_sizes, rotation='vertical')
+    ax2.set_xticks(x, bucket_sizes, rotation='vertical')
     plt.xlim(left=-.5, right=len(bucket_sizes))
     plt.xlabel('Bucket Size')
-    plt.ylabel('Internal Fragmentation (%)')
+    ax1.set_ylabel('Internal Fragmentation (%)', color='g')
+    ax2.set_ylabel('Wasted (MiB)', color='plum')
     plt.title('Internal Fragmentation vs Bucket Size')
-    ax.bar(x, y, color='g')
+    ax1.bar(x, y1, alpha=0.5, color='g')
+    ax2.bar(x, y2, alpha=0.5, color='plum')
 
   ani = animation.FuncAnimation(fig, animate, interval=1000)
   plt.show()
