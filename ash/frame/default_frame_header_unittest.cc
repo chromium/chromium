87aabf0c68873b80c33a519239406df19f6f2de4
Andrew Xu <andrewxu@chromium.org>
2022-08-04 21:50:25 +0000

    [Deflakiness] Fix `DefaultFrameHeaderTest`
    My recent CL (https://crrev.com/c/3806105) ensures that the shelf
    background animation does not run if the zero animation duration
    scope flag is enabled. However, it breaks some test cases of
    `DefaultFrameHeaderTest` that actually rely on the shelf background
    animation to update the test window's frame. Because the background
    animation is skipped now, the loop to wait for the window frame
    never ends.
    
    This CL fixes this issue by using `WaitForNextFrameToBePresented()`
    to wait for the root window's incoming frame.
    
    Bug: 1349633,1349573
    Change-Id: I627800a38991e3c0956432ad3db9b6ae1ab04903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812098
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031609}

--- ash/frame/default_frame_header_unittest.cc
+++ ash/frame/default_frame_header_unittest.cc
@@ -17,9 +17,11 @@
 #include "chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.h"
 #include "chromeos/ui/frame/frame_header.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_tree_host.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
+#include "ui/compositor/test/test_utils.h"
 #include "ui/gfx/animation/animation_test_api.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/views/test/test_views.h"
@@ -175,8 +177,7 @@ class FramePaintWaiter : public ui::CompositorObserver {
   FrameHeader* frame_header_ = nullptr;
 };
 
-// TODO(crbug.com/1349573): Revive this test.
-TEST_F(DefaultFrameHeaderTest, DISABLED_DeleteDuringAnimation) {
+TEST_F(DefaultFrameHeaderTest, DeleteDuringAnimation) {
   const auto bounds = gfx::Rect(100, 100);
   auto win0 = CreateAppWindow(bounds, AppType::BROWSER);
   auto win1 = CreateAppWindow(bounds, AppType::BROWSER);
@@ -186,9 +187,8 @@ TEST_F(DefaultFrameHeaderTest, DISABLED_DeleteDuringAnimation) {
 
   EXPECT_TRUE(wm::IsActiveWindow(win1.get()));
 
-  // A frame will not animate until it is painted first.
-  FramePaintWaiter(win0.get()).Wait();
-  FramePaintWaiter(win1.get()).Wait();
+  // Waits until `FrameHeader` gets painted.
+  EXPECT_TRUE(ui::WaitForNextFrameToBePresented(win0->GetHost()->compositor()));
 
   ui::ScopedAnimationDurationScaleMode non_zero_duration_mode(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
@@ -214,18 +214,17 @@ TEST_F(DefaultFrameHeaderTest, DISABLED_DeleteDuringAnimation) {
   EXPECT_TRUE(checker.destroyed());
 }
 
-// TODO(crbug.com/1349633): Revive this test.
 // Make sure that the animation is canceled when resized.
-TEST_F(DefaultFrameHeaderTest, DISABLED_ResizeAndReorderDuringAnimation) {
+TEST_F(DefaultFrameHeaderTest, ResizeAndReorderDuringAnimation) {
   const auto bounds = gfx::Rect(100, 100);
   auto win_0 = CreateAppWindow(bounds, AppType::BROWSER);
   auto win_1 = CreateAppWindow(bounds, AppType::BROWSER);
 
   EXPECT_TRUE(wm::IsActiveWindow(win_1.get()));
 
-  // A frame will not animate until it is painted first.
-  FramePaintWaiter(win_0.get()).Wait();
-  FramePaintWaiter(win_1.get()).Wait();
+  // Waits until `FrameHeader` gets painted.
+  EXPECT_TRUE(
+      ui::WaitForNextFrameToBePresented(win_0->GetHost()->compositor()));
 
   ui::ScopedAnimationDurationScaleMode non_zero_duration_mode(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
