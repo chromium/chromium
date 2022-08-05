6a8b838df61a87ea40384be74af2e86baac128a5
Mike Dougherty <michaeldo@chromium.org>
2022-08-05 03:55:53 +0000

    [M104] Add missing DependsOn statements for TabsSearchServiceFactory
    (cherry picked from commit 0b24dc3c17afc95139896d5402c2438f67647588)
    
    Fixed: 1346607
    Change-Id: I41fe99d8b9b33a61b92c5495993675ffb56c4fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807996
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1031193}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810634
    Reviewed-by: Harry Souders <harrysouders@google.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5112@{#1392}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- ios/chrome/browser/tabs_search/tabs_search_service_factory.mm
+++ ios/chrome/browser/tabs_search/tabs_search_service_factory.mm
@@ -7,6 +7,13 @@
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/history/history_service_factory.h"
+#import "ios/chrome/browser/history/web_history_service_factory.h"
+#import "ios/chrome/browser/main/browser_list_factory.h"
+#import "ios/chrome/browser/sessions/ios_chrome_tab_restore_service_factory.h"
+#import "ios/chrome/browser/signin/identity_manager_factory.h"
+#import "ios/chrome/browser/sync/session_sync_service_factory.h"
+#import "ios/chrome/browser/sync/sync_service_factory.h"
 #import "ios/chrome/browser/tabs_search/tabs_search_service.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -27,7 +34,14 @@ TabsSearchServiceFactory* TabsSearchServiceFactory::GetInstance() {
 TabsSearchServiceFactory::TabsSearchServiceFactory()
     : BrowserStateKeyedServiceFactory(
           "TabsSearchService",
-          BrowserStateDependencyManager::GetInstance()) {}
+          BrowserStateDependencyManager::GetInstance()) {
+  DependsOn(BrowserListFactory::GetInstance());
+  DependsOn(IOSChromeTabRestoreServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(ios::HistoryServiceFactory::GetInstance());
+  DependsOn(SessionSyncServiceFactory::GetInstance());
+  DependsOn(SyncServiceFactory::GetInstance());
+}
 
 TabsSearchServiceFactory::~TabsSearchServiceFactory() = default;
 
