16b5c170ca0f3683e7643e941094a174471a026b
Yuzu Saijo <yuzus@chromium.org>
2022-08-05 06:04:25 +0000

    [sheriff] Disable flaky browsertest on linux-chromeos-rel
    This CL disables a flaky browsertest
    DemoSetupArcSupportedTest.OnlineSetupFlowSuccessWithCountryCustomization
    
    Bug: 1350365
    Change-Id: I508bf499657282422f34f06b53ce344e84c39689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810760
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031790}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -537,8 +537,16 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
 
+// Disabled due to test failure. http://crbug.com/1350365
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#define MAYBE_OnlineSetupFlowSuccessWithCountryCustomization \
+  DISABLED_OnlineSetupFlowSuccessWithCountryCustomization
+#else
+#define MAYBE_OnlineSetupFlowSuccessWithCountryCustomization \
+  OnlineSetupFlowSuccessWithCountryCustomization
+#endif
 IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
-                       OnlineSetupFlowSuccessWithCountryCustomization) {
+                       MAYBE_OnlineSetupFlowSuccessWithCountryCustomization) {
   // Simulate successful online setup.
   enrollment_helper_.ExpectEnrollmentMode(
       policy::EnrollmentConfig::MODE_ATTESTATION);
