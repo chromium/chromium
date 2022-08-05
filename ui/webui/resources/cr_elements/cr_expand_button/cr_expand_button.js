6f10a01c25695013ac5b9d0245f45b31c7bdb79e
Rebekah Potter <rbpotter@chromium.org>
2022-08-05 00:08:16 +0000

    WebUI: Migrate CrExpandButton to class-based syntax
    This is in preparation for migrating to TypeScript
    
    Bug: 1206112
    Change-Id: I1b947b897cbe71000c114d6f4b206d95593eb6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3808030
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031682}

--- ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
+++ ui/webui/resources/cr_elements/cr_expand_button/cr_expand_button.js
@@ -7,73 +7,91 @@
  * 'cr-expand-button' is a chrome-specific wrapper around a button that toggles
  * between an opened (expanded) and closed state.
  */
-import {Polymer, html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-
-import {focusWithoutInk} from '../../js/cr/ui/focus_without_ink.m.js';
 import '../cr_actionable_row_style.m.js';
 import '../cr_icon_button/cr_icon_button.m.js';
 import '../icons.m.js';
 import '../shared_vars_css.m.js';
 
-Polymer({
-  is: 'cr-expand-button',
+import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-  _template: html`{__html_template__}`,
+import {focusWithoutInk} from '../../js/cr/ui/focus_without_ink.m.js';
 
-  properties: {
-    /**
-     * If true, the button is in the expanded state and will show the icon
-     * specified in the `collapseIcon` property. If false, the button shows the
-     * icon specified in the `expandIcon` property.
-     */
-    expanded: {
-      type: Boolean,
-      value: false,
-      notify: true,
-      observer: 'onExpandedChange_',
-    },
+/** @polymer */
+export class CrExpandButtonElement extends PolymerElement {
+  static get is() {
+    return 'cr-expand-button';
+  }
 
-    /**
-     * If true, the button will be disabled and grayed out.
-     */
-    disabled: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
-    /** A11y text descriptor for this control. */
-    ariaLabel: {
-      type: String,
-      observer: 'onAriaLabelChange_',
-    },
+  static get properties() {
+    return {
+      /**
+       * If true, the button is in the expanded state and will show the icon
+       * specified in the `collapseIcon` property. If false, the button shows
+       * the icon specified in the `expandIcon` property.
+       */
+      expanded: {
+        type: Boolean,
+        value: false,
+        notify: true,
+        observer: 'onExpandedChange_',
+      },
 
-    tabIndex: {
-      type: Number,
-      value: 0,
-    },
+      /**
+       * If true, the button will be disabled and grayed out.
+       */
+      disabled: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
 
-    expandIcon: {
-      type: String,
-      value: 'cr:expand-more',
-      observer: 'onIconChange_',
-    },
+      /** A11y text descriptor for this control. */
+      ariaLabel: {
+        type: String,
+        observer: 'onAriaLabelChange_',
+      },
 
-    collapseIcon: {
-      type: String,
-      value: 'cr:expand-less',
-      observer: 'onIconChange_',
-    },
+      tabIndex: {
+        type: Number,
+        value: 0,
+      },
 
-    expandTitle: String,
-    collapseTitle: String,
+      expandIcon: {
+        type: String,
+        value: 'cr:expand-more',
+        observer: 'onIconChange_',
+      },
 
-    tooltipText_: {
-      type: String,
-      computed: 'computeTooltipText_(expandTitle, collapseTitle, expanded)',
-      observer: 'onTooltipTextChange_',
-    },
-  },
+      collapseIcon: {
+        type: String,
+        value: 'cr:expand-less',
+        observer: 'onIconChange_',
+      },
+
+      expandTitle: String,
+      collapseTitle: String,
+
+      tooltipText_: {
+        type: String,
+        computed: 'computeTooltipText_(expandTitle, collapseTitle, expanded)',
+        observer: 'onTooltipTextChange_',
+      },
+    };
+  }
+
+  static get observers() {
+    return ['updateAriaExpanded_(disabled, expanded)'];
+  }
+
+  /** @override */
+  ready() {
+    super.ready();
+    this.addEventListener('click', this.toggleExpand_);
+  }
 
   /**
    * @return {string}
@@ -81,34 +99,26 @@ Polymer({
    */
   computeTooltipText_() {
     return this.expanded ? this.collapseTitle : this.expandTitle;
-  },
+  }
 
   /** @private */
   onTooltipTextChange_() {
     this.title = this.tooltipText_;
-  },
-
-  observers: [
-    'updateAriaExpanded_(disabled, expanded)',
-  ],
-
-  listeners: {
-    click: 'toggleExpand_',
-  },
+  }
 
   /** @type {boolean} */
   get noink() {
     return this.$.icon.noink;
-  },
+  }
 
   /** @type {boolean} */
   set noink(value) {
     this.$.icon.noink = value;
-  },
+  }
 
   focus() {
     this.$.icon.focus();
-  },
+  }
 
   /** @private */
   onAriaLabelChange_() {
@@ -119,22 +129,22 @@ Polymer({
       this.$.icon.removeAttribute('aria-label');
       this.$.icon.setAttribute('aria-labelledby', 'label');
     }
-  },
+  }
 
   /** @private */
   onExpandedChange_() {
     this.updateIcon_();
-  },
+  }
 
   /** @private */
   onIconChange_() {
     this.updateIcon_();
-  },
+  }
 
   /** @private */
   updateIcon_() {
     this.$.icon.ironIcon = this.expanded ? this.collapseIcon : this.expandIcon;
-  },
+  }
 
   /**
    * @param {!Event} event
@@ -149,7 +159,7 @@ Polymer({
     this.scrollIntoViewIfNeeded();
     this.expanded = !this.expanded;
     focusWithoutInk(this.$.icon);
-  },
+  }
 
   /** @private */
   updateAriaExpanded_() {
@@ -158,5 +168,7 @@ Polymer({
     } else {
       this.$.icon.setAttribute('aria-expanded', this.expanded);
     }
-  },
-});
+  }
+}
+
+customElements.define(CrExpandButtonElement.is, CrExpandButtonElement);
