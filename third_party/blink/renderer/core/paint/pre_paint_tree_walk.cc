7c119977cdfd00f041f33534df8c803ad6d4df06
Morten Stenshorne <mstensho@chromium.org>
2022-08-05 09:11:44 +0000

    Correct fix for bug 1343167.
    Revert the partial revert in CL:3752215 and fix the root cause.
    
    The real problem here was that we set up NGPrePaintInfo::
    fragmentainer_is_oof_containing_block incorrectly when an out-of-flow
    child escapes the fragmentation context in which the parent
    participates.
    
    Bug: 1343167, 1343746
    Change-Id: I06d4e93a027f65e93eb3cf3ee641eb58a50bc29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811156
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1031827}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
@@ -449,9 +449,8 @@ void PrePaintTreeWalk::UpdateContextForOOFContainer(
   // context. If we're not participating in block fragmentation, the containing
   // fragment of an OOF fragment is always simply the parent.
   const LayoutBox* box = DynamicTo<LayoutBox>(&object);
-  // TODO(crbug.com/1343746): Review the revert of the following lines.
-  if (context.current_container.fragment && box &&
-      box->GetNGPaginationBreakability() == LayoutBox::kForbidBreaks) {
+  if (!context.current_container.IsInFragmentationContext() ||
+      (box && box->GetNGPaginationBreakability() == LayoutBox::kForbidBreaks)) {
     context.current_container.fragment = fragment;
   }
 
@@ -922,20 +921,27 @@ void PrePaintTreeWalk::WalkLayoutObjectChildren(
     }
 
     if (box_fragment) {
-      NGPrePaintInfo pre_paint_info(
-          *box_fragment, paint_offset, fragmentainer_idx, is_first_for_node,
-          is_last_for_node, is_inside_fragment_child,
-          context.current_container.IsInFragmentationContext());
+      const ContainingFragment* container_for_child =
+          &context.current_container;
+      bool is_in_different_fragmentation_context = false;
       if (oof_containing_fragment_info &&
           context.current_container.fragmentation_nesting_level !=
               oof_containing_fragment_info->fragmentation_nesting_level) {
         // We're walking an out-of-flow positioned descendant that isn't in the
-        // same fragmentation context as parent_object. Update the context, so
-        // that we create FragmentData objects correctly both for the descendant
-        // and all its descendants.
+        // same fragmentation context as parent_object. We need to update the
+        // context, so that we create FragmentData objects correctly both for
+        // the descendant and all its descendants.
+        container_for_child = oof_containing_fragment_info;
+        is_in_different_fragmentation_context = true;
+      }
+      NGPrePaintInfo pre_paint_info(
+          *box_fragment, paint_offset, fragmentainer_idx, is_first_for_node,
+          is_last_for_node, is_inside_fragment_child,
+          container_for_child->IsInFragmentationContext());
+      if (is_in_different_fragmentation_context) {
         PrePaintTreeWalkContext oof_context(
             context, NeedsTreeBuilderContextUpdate(*child, context));
-        oof_context.current_container = *oof_containing_fragment_info;
+        oof_context.current_container = *container_for_child;
         Walk(*child, oof_context, &pre_paint_info);
       } else {
         Walk(*child, context, &pre_paint_info);
