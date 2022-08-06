3dd57e7aadb8e0b5f0e1c3af85133b37bede165c
Rohit Rao <rohitrao@chromium.org>
2022-08-06 16:56:54 +0000

    [ios] Fixes persistent flake in CookiesTestCase.testClearIncognitoFromMain.
    This test uses EG helpers to close all incognito tabs via model calls,
    but as a result the tab grid is in an unexpected state (the main UI is
    displaying normal tabs, while the tab grid is on the incognito
    panel). A thumbstrip-specific codepath ends up incorrectly telling the
    BVCContainerViewController to display nothing, resulting in an empty
    black screen.
    
    We can temporarily work around this failure by opening and closing the
    tab grid -- this resets the current panel to show normal tabs, which
    avoids the buggy codepaths.
    
    Bug: 1350742
    Change-Id: I248b8057440f2e98f0a55578ae25130f04f7df00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813707
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032300}

--- ios/chrome/browser/net/cookies_egtest.mm
+++ ios/chrome/browser/net/cookies_egtest.mm
@@ -8,14 +8,15 @@
 
 #import <XCTest/XCTest.h>
 
-#include "base/strings/stringprintf.h"
-#include "base/strings/sys_string_conversions.h"
+#import "base/strings/stringprintf.h"
+#import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
-#include "net/test/embedded_test_server/default_handlers.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
-#include "url/gurl.h"
+#import "net/test/embedded_test_server/default_handlers.h"
+#import "net/test/embedded_test_server/embedded_test_server.h"
+#import "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -105,6 +106,13 @@ std::string IncognitoCookiePath() {
                          @"Normal cookie should still exist in normal mode.");
   GREYAssertEqual(1U, cookies.count,
                   @"Only one cookie should be found in normal mode.");
+
+  // Work around a TabGrid bug by opening and closing the grid before
+  // proceeding.
+  // TODO(crbug.com/1350742): Fix the underlying bug and remove this workaround.
+  [ChromeEarlGrey showTabSwitcher];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::TabGridDoneButton()]
+      performAction:grey_tap()];
 
   // Finally, closes all incognito tabs while still in normal tab.
   // Checks that incognito cookie is gone.
