fccbaa2eb8eeb241d52d5474d3cacc0e6dc44cdd
Ari Chivukula <arichiv@chromium.org>
2022-08-06 01:13:16 +0000

    [CodeHealth] Use `is_chromeos` in Grit files (/chrome/browser/resources/sandbox_internals)
    This CL was uploaded by git cl split.
    
    R=tsepez@chromium.org
    
    Bug: 1316150
    Change-Id: I8f37f467db0c9b57b92a4c97ce67cb5ae97b31bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812145
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032207}

--- chrome/browser/resources/sandbox_internals/sandbox_internals.ts
+++ chrome/browser/resources/sandbox_internals/sandbox_internals.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// <if expr="is_linux or chromeos_ash or chromeos_lacros">
+// <if expr="is_linux or is_chromeos">
 import './strings.m.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
@@ -121,7 +121,7 @@ function androidHandler() {
 }
 // </if>
 
-// <if expr="is_linux or chromeos_ash or chromeos_lacros">
+// <if expr="is_linux or is_chromeos">
 
 /**
  * Adds a status row that reports either Yes or No.
@@ -182,7 +182,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // <if expr="is_android">
   androidHandler();
   // </if>
-  // <if expr="is_linux or chromeos_ash or chromeos_lacros">
+  // <if expr="is_linux or is_chromeos">
   linuxHandler();
   // </if>
 });
