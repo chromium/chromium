0a7ba943890c1d24efe7136eee8afb192a28891d
Rohit Rao <rohitrao@chromium.org>
2022-08-07 16:20:55 +0000

    [ios] Fixes occasional flake in NTPHomeTestCase.
    Some of the tests in NTPHomeTestCase assume that various modules
    (Shortcuts, Discover) will be present, but the kTrendingQueriesModule
    experiment can sometimes hide these modules. Work around these
    failures by disabling the experiment for tests that require specific
    modules to be present.
    
    Bug: 1350826
    Change-Id: Id85b8cda28ef6c9340151957abaadd9500440b87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3814116
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032372}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -184,6 +184,17 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 #define MAYBE_testCollectionShortcuts testCollectionShortcuts
 #endif
 - (void)MAYBE_testCollectionShortcuts {
+  // Relaunch the app with trending queries disabled, to ensure that the
+  // shortcuts module is always present.
+  // TODO(crbug.com/1350826): Trending queries is configured as a
+  // first-run trial, and one of the arms removes the Shortcuts
+  // module. Fix these tests to force an appropriate configuration or
+  // otherwise support the various possible experiment arms.
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  config.features_disabled.push_back(kTrendingQueriesModule);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
   // Check the Bookmarks.
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabelId(
@@ -726,6 +737,17 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 #define MAYBE_testFavicons testFavicons
 #endif
 - (void)MAYBE_testFavicons {
+  // Relaunch the app with trending queries disabled, to ensure that the
+  // shortcuts module is always present.
+  // TODO(crbug.com/1350826): Trending queries is configured as a
+  // first-run trial, and one of the arms removes the Shortcuts
+  // module. Fix these tests to force an appropriate configuration or
+  // otherwise support the various possible experiment arms.
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  config.features_disabled.push_back(kTrendingQueriesModule);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
   for (NSInteger index = 0; index < 4; index++) {
     [[EarlGrey
         selectElementWithMatcher:
@@ -1109,6 +1131,17 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 #define MAYBE_testFeedAblationHidesFeed testFeedAblationHidesFeed
 #endif
 - (void)MAYBE_testFeedAblationHidesFeed {
+  // Relaunch the app with trending queries disabled, to ensure that the
+  // discover feed is always present.
+  // TODO(crbug.com/1350826): Trending queries is configured as a
+  // first-run trial, and one of the arms removes the discover
+  // feed. Fix these tests to force an appropriate configuration or
+  // otherwise support the various possible experiment arms.
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  config.features_disabled.push_back(kTrendingQueriesModule);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
   // Ensures that feed header is visible before enabling ablation.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::DiscoverHeaderLabel()]
       assertWithMatcher:grey_sufficientlyVisible()];
@@ -1124,8 +1157,6 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
       assertWithMatcher:grey_notNil()];
 
   // Relaunch the app with ablation enabled.
-  AppLaunchConfiguration config = [self appConfigurationForTestCase];
-  config.relaunch_policy = ForceRelaunchByCleanShutdown;
   config.features_enabled.push_back(kEnableFeedAblation);
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
