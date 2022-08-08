f62d32d0481bda7922480e3f0cdca284ba0f53b3
Daniel Murphy <dmurph@google.com>
2022-08-08 19:27:00 +0000

    [dPWA] Launching status bar removal by default on WML
    (cherry picked from commit 687f71df55e8611a0d5d11da5557304cce54efaf)
    
    Bug: 1336681, 1350268
    Change-Id: I2be3374de1bc4fbdb4ec25b62b5f97510fd7298f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788511
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1028911}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816928
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5195@{#336}
    Cr-Branched-From: 7aa3f074a7907975b001346cc0288d0214af8451-refs/heads/main@{#1027018}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -790,14 +790,8 @@ const base::Feature kAbusiveNotificationPermissionRevocation{
     "AbusiveOriginNotificationPermissionRevocation",
     base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kRemoveStatusBarInWebApps {
-  "RemoveStatusBarInWebApps",
-#if BUILDFLAG(IS_CHROMEOS)
-      base::FEATURE_ENABLED_BY_DEFAULT
-#else
-      base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kRemoveStatusBarInWebApps{"RemoveStatusBarInWebApps",
+                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 // Enables permanent removal of Legacy Supervised Users on startup.
