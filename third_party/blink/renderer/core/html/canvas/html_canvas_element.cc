e9baae777203826a359c0ee9a10bf7451415649c
Quyen Le <lehoangquyen@chromium.org>
2022-08-08 16:24:55 +0000

    Revert "[m105] CanvasCapture: Make Pixel_WebGPUCanvasOneCopyCapture consistent."
    This reverts commit 2bbab2367056f8383a94c075a2552f7b5b8d77a9.
    
    Reason for revert: This CL makes the Pixel_WebGPUCanvasDisableOneCopyCapture fail 100% of the time on the Mac Retina M105 builder:
    
    https://ci.chromium.org/p/chromium-m105/builders/ci/Mac%20Retina%20Release%20%28AMD%29
    https://ci.chromium.org/ui/p/chromium-m105/builders/ci/Mac%20Retina%20Release%20(AMD)/301/overview
    
    Original change's description:
    > [m105] CanvasCapture: Make Pixel_WebGPUCanvasOneCopyCapture consistent.
    >
    > Pixel_WebGPUCanvasOneCopyCapture was flaky due to WebRTC's inconsistent
    > compression quality.
    > This CL changes Pixel_WebGPUCanvasOneCopyCapture to use
    > canvas.captureStream() as source of the video element directly.
    >
    > This also requires a change in html_canvas_element to activate
    > OneCopyCanvasCapture feature path when the canvas rendering context's
    > color is opaque.
    > Previously this feature was only utilised when the video sink can
    > discard alpha (such as WebRTC).
    >
    > (cherry picked from commit c0bb919bd7661ca10096f9019505b84e0a31060e)
    >
    > Bug: 1349480
    > Bug: 1349676
    > Change-Id: I414389a4dec4fc1b7d8e90053c7fd1c08817f0a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805198
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/main@{#1031446}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3811417
    > Commit-Queue: Kenneth Russell <kbr@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/branch-heads/5195@{#309}
    > Cr-Branched-From: 7aa3f074a7907975b001346cc0288d0214af8451-refs/heads/main@{#1027018}
    
    Bug: 1349480
    Bug: 1349676
    Change-Id: I1736fd43d1ec5db9ea19d7b329f03aa811cd30c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817662
    Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mason Freed <masonfreed@google.com>
    Cr-Commit-Position: refs/branch-heads/5195@{#326}
    Cr-Branched-From: 7aa3f074a7907975b001346cc0288d0214af8451-refs/heads/main@{#1027018}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -725,11 +725,6 @@ void HTMLCanvasElement::NotifyListenersCanvasChanged() {
     copier_ = std::make_unique<StaticBitmapImageToVideoFrameCopier>(
         base::FeatureList::IsEnabled(kTwoCopyCanvasCapture));
   }
-
-  const bool context_color_is_opaque =
-      context_ ? context_->CanvasRenderingContextSkColorInfo().isOpaque()
-               : false;
-
   for (CanvasDrawListener* listener : listeners_) {
     if (!listener->NeedsNewFrame())
       continue;
@@ -743,7 +738,7 @@ void HTMLCanvasElement::NotifyListenersCanvasChanged() {
     // First attempt to copy directly from the rendering context to a video
     // frame. Not all rendering contexts need to support this (for contexts
     // where GetSourceImageForCanvasInternal is zero-copy, this is superfluous).
-    if (context_ && (context_color_is_opaque || can_discard_alpha) &&
+    if (context_ && can_discard_alpha &&
         base::FeatureList::IsEnabled(kOneCopyCanvasCapture)) {
       if (context_->CopyRenderingResultsToVideoFrame(
               copier_->GetAcceleratedVideoFramePool(
