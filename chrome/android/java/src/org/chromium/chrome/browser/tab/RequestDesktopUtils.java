011686755bd58092f2c720d2dbe0b223c15c9261
Shu Yang <shuyng@google.com>
2022-08-08 21:08:48 +0000

    [RDSD] Repurpose app menu from site level to domain level RDS setting
    - Get DomainAndRegistry from url to set domain level exception;
    - Use host directly if fails to extract DomainAndRegistry from url;
    - Clear subdomain level exception if any.
    
    Bug: 1348832
    Change-Id: Id0fb99ef7a71f69a069d32d1e2111f9eb1ad9244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3796551
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032724}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/RequestDesktopUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/RequestDesktopUtils.java
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 package org.chromium.chrome.browser.tab;
 
+import android.text.TextUtils;
+
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 
@@ -12,6 +14,7 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
 import org.chromium.components.content_settings.ContentSettingValues;
 import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.ukm.UkmRecorder;
 import org.chromium.content_public.browser.BrowserContextHandle;
 import org.chromium.url.GURL;
@@ -27,6 +30,8 @@ public class RequestDesktopUtils {
             "default_on_display_size_threshold_inches";
     private static final double DEFAULT_GLOBAL_SETTING_DEFAULT_ON_DISPLAY_SIZE_THRESHOLD_INCHES =
             12.0;
+    private static final String ANY_SUBDOMAIN_PATTERN = "[*.]";
+    private static final String SITE_WILDCARD = "*";
 
     // Note: these values must match the UserAgentRequestType enum in enums.xml.
     @IntDef({UserAgentRequestType.REQUEST_DESKTOP, UserAgentRequestType.REQUEST_MOBILE})
@@ -81,7 +86,8 @@ public class RequestDesktopUtils {
     }
 
     /**
-     * Set or remove a site exception with URL for {@link ContentSettingsType.REQUEST_DESKTOP_SITE}.
+     * Set or remove a domain level exception with URL for {@link
+     * ContentSettingsType.REQUEST_DESKTOP_SITE}. Clear the subdomain level exception if any.
      * @param browserContextHandle Target browser context whose content settings needs to be
      *         updated.
      * @param url  {@link GURL} for the site that changes in desktop user agent.
@@ -89,6 +95,20 @@ public class RequestDesktopUtils {
      */
     public static void setRequestDesktopSiteContentSettingsForUrl(
             BrowserContextHandle browserContextHandle, GURL url, boolean useDesktopUserAgent) {
+        String domainAndRegistry =
+                UrlUtilities.getDomainAndRegistry(url.getSpec(), /*includePrivateRegistries*/ true);
+        // Use host only (no scheme/port/path) for ContentSettings to ensure consistency.
+        String hostPattern;
+        if (TextUtils.isEmpty(domainAndRegistry)) {
+            // Use host directly if fails to extract domain from url (e.g. ip address).
+            hostPattern = url.getHost();
+        } else {
+            hostPattern = ANY_SUBDOMAIN_PATTERN + domainAndRegistry;
+            // Clear subdomain level exception if any.
+            WebsitePreferenceBridge.setContentSettingCustomScope(browserContextHandle,
+                    ContentSettingsType.REQUEST_DESKTOP_SITE, url.getHost(),
+                    /*secondaryPattern*/ SITE_WILDCARD, ContentSettingValues.DEFAULT);
+        }
         @ContentSettingValues
         int defaultValue = WebsitePreferenceBridge.getDefaultContentSetting(
                 browserContextHandle, ContentSettingsType.REQUEST_DESKTOP_SITE);
@@ -106,8 +126,11 @@ public class RequestDesktopUtils {
             contentSettingValue = blockDesktopGlobally ? ContentSettingValues.DEFAULT
                                                        : ContentSettingValues.BLOCK;
         }
-        WebsitePreferenceBridge.setContentSettingDefaultScope(browserContextHandle,
-                ContentSettingsType.REQUEST_DESKTOP_SITE, url, url, contentSettingValue);
+
+        // Set or remove a domain level exception.
+        WebsitePreferenceBridge.setContentSettingCustomScope(browserContextHandle,
+                ContentSettingsType.REQUEST_DESKTOP_SITE, hostPattern,
+                /*secondaryPattern*/ SITE_WILDCARD, contentSettingValue);
     }
 
     /**
--- chrome/android/junit/src/org/chromium/chrome/browser/tab/RequestDesktopUtilsUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tab/RequestDesktopUtilsUnitTest.java
@@ -5,7 +5,9 @@
 package org.chromium.chrome.browser.tab;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 
@@ -24,77 +26,144 @@ import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
 import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridgeJni;
 import org.chromium.components.content_settings.ContentSettingValues;
 import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.embedder_support.util.UrlUtilities;
+import org.chromium.components.embedder_support.util.UrlUtilitiesJni;
 import org.chromium.content_public.browser.BrowserContextHandle;
 import org.chromium.url.GURL;
+import org.chromium.url.JUnitTestGURLs;
+import org.chromium.url.ShadowGURL;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit tests for {@link RequestDesktopUtils}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
-@SuppressWarnings("DoNotMock") // Mocking GURL
+@Config(manifest = Config.NONE, shadows = {ShadowGURL.class})
 public class RequestDesktopUtilsUnitTest {
     @Rule
     public JniMocker mJniMocker = new JniMocker();
 
     @Mock
-    WebsitePreferenceBridge.Natives mWebsitePreferenceBridgeJniMock;
+    private WebsitePreferenceBridge.Natives mWebsitePreferenceBridgeJniMock;
     @Mock
-    BrowserContextHandle mBrowserContextHandleMock;
+    private UrlUtilities.Natives mUrlUtilitiesJniMock;
     @Mock
-    GURL mGurlMock;
+    private BrowserContextHandle mBrowserContextHandleMock;
 
     private @ContentSettingValues int mRdsDefaultValue;
-    private @ContentSettingValues int mLastRdsContentSettingExceptionValue;
+    private final Map<String, Integer> mContentSettingMap = new HashMap<>();
+    private final GURL mGoogleUrl = new GURL(JUnitTestGURLs.GOOGLE_URL);
+    private final GURL mMapsUrl = new GURL(JUnitTestGURLs.MAPS_URL);
+    private static final String GOOGLE_COM = "[*.]google.com/";
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
 
         mJniMocker.mock(WebsitePreferenceBridgeJni.TEST_HOOKS, mWebsitePreferenceBridgeJniMock);
+        mJniMocker.mock(UrlUtilitiesJni.TEST_HOOKS, mUrlUtilitiesJniMock);
 
         doAnswer(invocation -> mRdsDefaultValue)
                 .when(mWebsitePreferenceBridgeJniMock)
                 .getDefaultContentSetting(any(), eq(ContentSettingsType.REQUEST_DESKTOP_SITE));
-
         doAnswer(invocation -> {
-            mLastRdsContentSettingExceptionValue = invocation.getArgument(4);
+            mContentSettingMap.put(invocation.getArgument(2), invocation.getArgument(4));
             return null;
         })
                 .when(mWebsitePreferenceBridgeJniMock)
-                .setContentSettingDefaultScope(any(), eq(ContentSettingsType.REQUEST_DESKTOP_SITE),
-                        any(), any(), anyInt());
-        doAnswer(invocation -> mLastRdsContentSettingExceptionValue)
-                .when(mWebsitePreferenceBridgeJniMock)
-                .getContentSetting(
-                        any(), eq(ContentSettingsType.REQUEST_DESKTOP_SITE), any(), any());
+                .setContentSettingCustomScope(any(), eq(ContentSettingsType.REQUEST_DESKTOP_SITE),
+                        anyString(), anyString(), anyInt());
+        doAnswer(invocation -> getDomainAndRegistry(invocation.getArgument(0)))
+                .when(mUrlUtilitiesJniMock)
+                .getDomainAndRegistry(anyString(), anyBoolean());
     }
 
     @Test
-    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultBlock() {
+    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultBlock_SiteBlock() {
         mRdsDefaultValue = ContentSettingValues.BLOCK;
+        // Pre-existing subdomain setting.
+        mContentSettingMap.put(mGoogleUrl.getHost(), ContentSettingValues.BLOCK);
         RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
-                mBrowserContextHandleMock, mGurlMock, true);
-        Assert.assertEquals("Request Desktop Site content settings Exception is not set correctly.",
-                ContentSettingValues.ALLOW, mLastRdsContentSettingExceptionValue);
+                mBrowserContextHandleMock, mGoogleUrl, true);
+        Assert.assertEquals("Request Desktop Site domain level setting is not set correctly.",
+                ContentSettingValues.ALLOW, mContentSettingMap.get(GOOGLE_COM).intValue());
+        Assert.assertEquals("Request Desktop Site subdomain level setting should be removed.",
+                ContentSettingValues.DEFAULT,
+                mContentSettingMap.get(mGoogleUrl.getHost()).intValue());
 
         RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
-                mBrowserContextHandleMock, mGurlMock, false);
-        Assert.assertEquals("Request Desktop Site content settings should be removed.",
-                ContentSettingValues.DEFAULT, mLastRdsContentSettingExceptionValue);
+                mBrowserContextHandleMock, mMapsUrl, false);
+        Assert.assertEquals("Request Desktop Site domain level setting should be removed.",
+                ContentSettingValues.DEFAULT, mContentSettingMap.get(GOOGLE_COM).intValue());
     }
 
     @Test
-    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultAllow() {
-        mRdsDefaultValue = ContentSettingValues.ALLOW;
+    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultBlock_SiteAllow() {
+        mRdsDefaultValue = ContentSettingValues.BLOCK;
+        // Pre-existing subdomain setting.
+        mContentSettingMap.put(mGoogleUrl.getHost(), ContentSettingValues.ALLOW);
         RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
-                mBrowserContextHandleMock, mGurlMock, false);
-        Assert.assertEquals("Request Desktop Site content settings Exception is not set correctly.",
-                ContentSettingValues.BLOCK, mLastRdsContentSettingExceptionValue);
+                mBrowserContextHandleMock, mGoogleUrl, false);
+        Assert.assertEquals("Request Desktop Site domain level setting should be removed.",
+                ContentSettingValues.DEFAULT, mContentSettingMap.get(GOOGLE_COM).intValue());
+        Assert.assertEquals("Request Desktop Site subdomain level setting should be removed.",
+                ContentSettingValues.DEFAULT,
+                mContentSettingMap.get(mGoogleUrl.getHost()).intValue());
 
         RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
-                mBrowserContextHandleMock, mGurlMock, true);
-        Assert.assertEquals("Request Desktop Site content settings should be removed.",
-                ContentSettingValues.DEFAULT, mLastRdsContentSettingExceptionValue);
+                mBrowserContextHandleMock, mMapsUrl, true);
+        Assert.assertEquals("Request Desktop Site domain level setting is not set correctly.",
+                ContentSettingValues.ALLOW, mContentSettingMap.get(GOOGLE_COM).intValue());
+    }
+
+    @Test
+    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultAllow_SiteAllow() {
+        mRdsDefaultValue = ContentSettingValues.ALLOW;
+        // Pre-existing subdomain setting.
+        mContentSettingMap.put(mGoogleUrl.getHost(), ContentSettingValues.ALLOW);
+        RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
+                mBrowserContextHandleMock, mGoogleUrl, false);
+        Assert.assertEquals("Request Desktop Site domain level setting is not set correctly.",
+                ContentSettingValues.BLOCK, mContentSettingMap.get(GOOGLE_COM).intValue());
+        Assert.assertEquals("Request Desktop Site subdomain level setting should be removed.",
+                ContentSettingValues.DEFAULT,
+                mContentSettingMap.get(mGoogleUrl.getHost()).intValue());
+
+        RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
+                mBrowserContextHandleMock, mMapsUrl, true);
+        Assert.assertEquals("Request Desktop Site domain level setting should be removed.",
+                ContentSettingValues.DEFAULT, mContentSettingMap.get(GOOGLE_COM).intValue());
+    }
+
+    @Test
+    public void testSetRequestDesktopSiteContentSettingsForUrl_DefaultAllow_SiteBlock() {
+        mRdsDefaultValue = ContentSettingValues.ALLOW;
+        // Pre-existing subdomain setting.
+        mContentSettingMap.put(mGoogleUrl.getHost(), ContentSettingValues.BLOCK);
+        RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
+                mBrowserContextHandleMock, mGoogleUrl, true);
+        Assert.assertEquals("Request Desktop Site domain level setting should be removed.",
+                ContentSettingValues.DEFAULT, mContentSettingMap.get(GOOGLE_COM).intValue());
+        Assert.assertEquals("Request Desktop Site subdomain level setting should be removed.",
+                ContentSettingValues.DEFAULT,
+                mContentSettingMap.get(mGoogleUrl.getHost()).intValue());
+
+        RequestDesktopUtils.setRequestDesktopSiteContentSettingsForUrl(
+                mBrowserContextHandleMock, mMapsUrl, false);
+        Assert.assertEquals("Request Desktop Site domain level setting is not set correctly.",
+                ContentSettingValues.BLOCK, mContentSettingMap.get(GOOGLE_COM).intValue());
+    }
+
+    /**
+     * Helper to get organization-identifying host from URLs. The real implementation calls
+     * {@link UrlUtilities}. It's not useful to actually reimplement it, so just return a string in
+     * a trivial way.
+     * @param origin A URL.
+     * @return The organization-identifying host from the given URL.
+     */
+    private String getDomainAndRegistry(String origin) {
+        return origin.replaceAll(".*\\.(.+\\.[^.]+$)", "$1");
     }
 }
