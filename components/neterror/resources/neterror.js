045126ad72d63781541cfedf8b117d392f0deef2
Ari Chivukula <arichiv@chromium.org>
2022-08-08 21:31:56 +0000

    [CodeHealth] Use `is_chromeos` in Grit files (/components/neterror/resources)
    This CL was uploaded by git cl split.
    
    R=johntlee@chromium.org
    
    Bug: 1316150
    Change-Id: Idcb504769ce9e0b1da4efab044b507bfe31d42f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812147
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032738}

--- components/neterror/resources/neterror.js
+++ components/neterror/resources/neterror.js
@@ -130,7 +130,7 @@ function detailsButtonClick() {
 
 let primaryControlOnLeft = true;
 // clang-format off
-// <if expr="is_macosx or is_ios or is_linux or chromeos_ash or chromeos_lacros or is_android">
+// <if expr="is_macosx or is_ios or is_linux or is_chromeos or is_android">
 // clang-format on
 primaryControlOnLeft = false;
 // </if>
