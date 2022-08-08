ac58f87a3bdde9892637c2e31c95e9aa786d29f3
Kevin Haslett <khaslett@chromium.org>
2022-08-08 20:17:36 +0000

    Use ToRoundedRect instead of ToNearestRect in OverlayCacheKey
    This prevents a DCHECK when OverlayCandidates are non-pixel-aligned.
    
    Bug: 1350576
    Change-Id: I39ef10b41e4d842feadcf93e6e9cb63ce5663eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3813680
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1032694}

--- components/viz/service/display/overlay_combination_cache.cc
+++ components/viz/service/display/overlay_combination_cache.cc
@@ -22,9 +22,9 @@ namespace viz {
 
 OverlayCacheKey::OverlayCacheKey(
     const OverlayProposedCandidate& proposed_candidate) {
-  // Rounding the display_rect here matches the behaviour of Ozone when testing
+  // Rounding the display_rect here matches the behaviour of DRM when testing
   // OverlayCandidates.
-  display_rect = gfx::ToNearestRect(proposed_candidate.candidate.display_rect);
+  display_rect = gfx::ToRoundedRect(proposed_candidate.candidate.display_rect);
   strategy = proposed_candidate.strategy->GetUMAEnum();
 }
 
