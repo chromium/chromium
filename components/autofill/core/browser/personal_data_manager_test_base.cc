a01c73c0268806e3d3817ea68b5993e2c486922d
Vidhan <vidhanj@google.com>
2022-08-09 17:46:28 +0000

    Add separate files for PersonalDataManagerTestBase
    PersonalDataManagerTestBase would also be used in the unit tests
    of PersonalDataManagerCleaner.
    
    Change-Id: I9cade6402cad5ca8c001602b708ac9fef368c861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747882
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033075}

---
+++ components/autofill/core/browser/personal_data_manager_test_base.cc
@@ -0,0 +1,191 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/personal_data_manager_test_base.h"
+#include "components/autofill/core/browser/autofill_test_utils.h"
+#include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/common/autofill_clock.h"
+#include "components/autofill/core/common/autofill_features.h"
+
+namespace autofill {
+
+namespace {
+
+const char kPrimaryAccountEmail[] = "syncuser@example.com";
+const char kSyncTransportAccountEmail[] = "transport@example.com";
+
+ACTION_P(QuitMessageLoop, loop) {
+  loop->Quit();
+}
+
+}  // anonymous namespace
+
+PersonalDataLoadedObserverMock::PersonalDataLoadedObserverMock() = default;
+PersonalDataLoadedObserverMock::~PersonalDataLoadedObserverMock() = default;
+
+// static
+std::vector<base::Feature>
+PersonalDataManagerTestBase::GetDefaultEnabledFeatures() {
+  // Enable account storage by default, some tests will override this to be
+  // false.
+  return {features::kAutofillEnableAccountWalletStorage};
+}
+
+PersonalDataManagerTestBase::PersonalDataManagerTestBase(
+    const std::vector<base::Feature>& additional_enabled_features)
+    : identity_test_env_(&test_url_loader_factory_) {
+  std::vector<base::Feature> all_enabled_features(
+      PersonalDataManagerTestBase::GetDefaultEnabledFeatures());
+  base::ranges::copy(additional_enabled_features,
+                     std::back_inserter(all_enabled_features));
+  scoped_features_.InitWithFeatures(all_enabled_features,
+                                    /*disabled_features=*/{});
+}
+
+PersonalDataManagerTestBase::~PersonalDataManagerTestBase() = default;
+
+void PersonalDataManagerTestBase::SetUpTest() {
+  OSCryptMocker::SetUp();
+  prefs_ = test::PrefServiceForTesting();
+  base::FilePath path(WebDatabase::kInMemoryPath);
+  profile_web_database_ =
+      new WebDatabaseService(path, base::ThreadTaskRunnerHandle::Get(),
+                             base::ThreadTaskRunnerHandle::Get());
+
+  // Hacky: hold onto a pointer but pass ownership.
+  profile_autofill_table_ = new AutofillTable;
+  profile_web_database_->AddTable(
+      std::unique_ptr<WebDatabaseTable>(profile_autofill_table_));
+  profile_web_database_->LoadDatabase();
+  profile_database_service_ = new AutofillWebDataService(
+      profile_web_database_, base::ThreadTaskRunnerHandle::Get(),
+      base::ThreadTaskRunnerHandle::Get());
+  profile_database_service_->Init(base::NullCallback());
+
+  account_web_database_ = new WebDatabaseService(
+      base::FilePath(WebDatabase::kInMemoryPath),
+      base::ThreadTaskRunnerHandle::Get(), base::ThreadTaskRunnerHandle::Get());
+  account_autofill_table_ = new AutofillTable;
+  account_web_database_->AddTable(
+      std::unique_ptr<WebDatabaseTable>(account_autofill_table_));
+  account_web_database_->LoadDatabase();
+  account_database_service_ = new AutofillWebDataService(
+      account_web_database_, base::ThreadTaskRunnerHandle::Get(),
+      base::ThreadTaskRunnerHandle::Get());
+  account_database_service_->Init(base::NullCallback());
+
+  strike_database_ = std::make_unique<TestInMemoryStrikeDatabase>();
+
+  test::DisableSystemServices(prefs_.get());
+}
+
+void PersonalDataManagerTestBase::TearDownTest() {
+  // Order of destruction is important as BrowserAutofillManager relies on
+  // PersonalDataManager to be around when it gets destroyed.
+  test::ReenableSystemServices();
+  OSCryptMocker::TearDown();
+}
+
+void PersonalDataManagerTestBase::ResetPersonalDataManager(
+    bool is_incognito,
+    bool use_sync_transport_mode,
+    PersonalDataManager* personal_data) {
+  personal_data->Init(
+      scoped_refptr<AutofillWebDataService>(profile_database_service_),
+      base::FeatureList::IsEnabled(
+          features::kAutofillEnableAccountWalletStorage)
+          ? scoped_refptr<AutofillWebDataService>(account_database_service_)
+          : nullptr,
+      prefs_.get(), prefs_.get(), identity_test_env_.identity_manager(),
+      /*history_service=*/nullptr, strike_database_.get(),
+      /*image_fetcher=*/nullptr, is_incognito);
+
+  personal_data->AddObserver(&personal_data_observer_);
+  std::string email = use_sync_transport_mode ? kSyncTransportAccountEmail
+                                              : kPrimaryAccountEmail;
+  // Set the account in both IdentityManager and SyncService.
+  CoreAccountInfo account_info;
+  signin::ConsentLevel consent_level = use_sync_transport_mode
+                                           ? signin::ConsentLevel::kSignin
+                                           : signin::ConsentLevel::kSync;
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+  identity_test_env_.ClearPrimaryAccount();
+  account_info = identity_test_env_.SetPrimaryAccount(email, consent_level);
+#else
+  // In ChromeOS-Ash, clearing/resetting the primary account is not supported.
+  // So if an account already exists, reuse it (and make sure it matches).
+  if (identity_test_env_.identity_manager()->HasPrimaryAccount(consent_level)) {
+    account_info = identity_test_env_.identity_manager()->GetPrimaryAccountInfo(
+        consent_level);
+    ASSERT_EQ(account_info.email, email);
+  } else {
+    account_info = identity_test_env_.SetPrimaryAccount(email, consent_level);
+  }
+#endif
+  sync_service_.SetAccountInfo(account_info);
+  sync_service_.SetHasSyncConsent(!use_sync_transport_mode);
+  personal_data->OnSyncServiceInitialized(&sync_service_);
+  personal_data->OnStateChanged(&sync_service_);
+
+  WaitForOnPersonalDataChangedRepeatedly();
+}
+
+[[nodiscard]] bool PersonalDataManagerTestBase::TurnOnSyncFeature(
+    PersonalDataManager* personal_data) {
+  sync_service_.SetHasSyncConsent(true);
+  if (!sync_service_.IsSyncFeatureEnabled())
+    return false;
+  personal_data->OnStateChanged(&sync_service_);
+
+  return personal_data->IsSyncFeatureEnabled();
+}
+
+void PersonalDataManagerTestBase::RemoveByGUIDFromPersonalDataManager(
+    const std::string& guid,
+    PersonalDataManager* personal_data) {
+  base::RunLoop run_loop;
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
+      .WillOnce(QuitMessageLoop(&run_loop));
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
+      .Times(testing::AnyNumber());
+
+  personal_data->RemoveByGUID(guid);
+  run_loop.Run();
+}
+
+void PersonalDataManagerTestBase::SetServerCards(
+    std::vector<CreditCard> server_cards) {
+  test::SetServerCreditCards(account_autofill_table_, server_cards);
+}
+
+// Verify that the web database has been updated and the notification sent.
+void PersonalDataManagerTestBase::WaitOnceForOnPersonalDataChanged() {
+  base::RunLoop run_loop;
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
+      .WillOnce(QuitMessageLoop(&run_loop));
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged()).Times(1);
+  run_loop.Run();
+}
+
+// Verifies that the web database has been updated and the notification sent.
+void PersonalDataManagerTestBase::WaitForOnPersonalDataChanged() {
+  base::RunLoop run_loop;
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
+      .WillOnce(QuitMessageLoop(&run_loop));
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
+      .Times(testing::AnyNumber());
+  run_loop.Run();
+}
+
+// Verifies that the web database has been updated and the notification sent.
+void PersonalDataManagerTestBase::WaitForOnPersonalDataChangedRepeatedly() {
+  base::RunLoop run_loop;
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
+      .WillRepeatedly(QuitMessageLoop(&run_loop));
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
+      .Times(testing::AnyNumber());
+  run_loop.Run();
+}
+
+}  // namespace autofill
--- components/autofill/core/browser/personal_data_manager_unittest.cc
+++ components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -37,30 +37,23 @@
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
-#include "components/autofill/core/browser/personal_data_manager_observer.h"
+#include "components/autofill/core/browser/personal_data_manager_test_base.h"
 #include "components/autofill/core/browser/sync_utils.h"
 #include "components/autofill/core/browser/test_autofill_clock.h"
-#include "components/autofill/core/browser/test_inmemory_strike_database.h"
 #include "components/autofill/core/browser/ui/label_formatter_utils.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
 #include "components/autofill/core/browser/ui/suggestion_selection.h"
-#include "components/autofill/core/browser/webdata/autofill_table.h"
-#include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autofill_clock.h"
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/autofill_prefs.h"
-#include "components/os_crypt/os_crypt_mocker.h"
-#include "components/prefs/pref_service.h"
+#include "components/autofill/core/common/autofill_switches.h"
+#include "components/autofill/core/common/form_data.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/sync/driver/test_sync_service.h"
 #include "components/version_info/version_info.h"
-#include "components/webdata/common/web_data_service_base.h"
-#include "components/webdata/common/web_database_service.h"
-#include "google_apis/gaia/google_service_auth_error.h"
-#include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/image/image_unittest_util.h"
@@ -74,7 +67,6 @@ namespace {
 
 const char kPrimaryAccountEmail[] = "syncuser@example.com";
 const char16_t kPrimaryAccountEmail16[] = u"syncuser@example.com";
-const char kSyncTransportAccountEmail[] = "transport@example.com";
 
 enum UserMode { USER_MODE_NORMAL, USER_MODE_INCOGNITO };
 
@@ -85,15 +77,6 @@ const base::Time kMuchLaterTime = base::Time::FromDoubleT(5000);
 ACTION_P(QuitMessageLoop, loop) {
   loop->Quit();
 }
-
-class PersonalDataLoadedObserverMock : public PersonalDataManagerObserver {
- public:
-  PersonalDataLoadedObserverMock() = default;
-  ~PersonalDataLoadedObserverMock() override = default;
-
-  MOCK_METHOD(void, OnPersonalDataChanged, (), (override));
-  MOCK_METHOD(void, OnPersonalDataFinishedProfileTasks, (), (override));
-};
 
 class PersonalDataManagerMock : public PersonalDataManager {
  public:
@@ -139,206 +122,7 @@ void ExpectSameElements(const std::vector<T*>& expectations,
             results_copy.end());
 }
 
-class ScopedFeatureListWrapper {
- public:
-  explicit ScopedFeatureListWrapper(
-      const std::vector<base::Feature>& default_enabled_features,
-      const std::vector<base::Feature>& additional_enabled_features) {
-    std::vector<base::Feature> all_enabled_features(default_enabled_features);
-    std::copy(additional_enabled_features.begin(),
-              additional_enabled_features.end(),
-              std::back_inserter(all_enabled_features));
-    scoped_features_.InitWithFeatures(all_enabled_features,
-                                      /*disabled_features=*/{});
-  }
-  ~ScopedFeatureListWrapper() = default;
-
- private:
-  base::test::ScopedFeatureList scoped_features_;
-};
-
 }  // anonymous namespace
-
-class PersonalDataManagerTestBase {
- protected:
-  static std::vector<base::Feature> GetDefaultEnabledFeatures() {
-    // Enable account storage by default, some tests will override this to be
-    // false.
-    return {features::kAutofillEnableAccountWalletStorage};
-  }
-
-  PersonalDataManagerTestBase()
-      : scoped_features_(
-            PersonalDataManagerTestBase::GetDefaultEnabledFeatures(),
-            /*additional_enabled_features=*/{}),
-        identity_test_env_(&test_url_loader_factory_) {}
-
-  explicit PersonalDataManagerTestBase(
-      const std::vector<base::Feature>& additioanal_enabled_features)
-      : scoped_features_(
-            PersonalDataManagerTestBase::GetDefaultEnabledFeatures(),
-            additioanal_enabled_features),
-        identity_test_env_(&test_url_loader_factory_) {}
-
-  void SetUpTest() {
-    OSCryptMocker::SetUp();
-    prefs_ = test::PrefServiceForTesting();
-    base::FilePath path(WebDatabase::kInMemoryPath);
-    profile_web_database_ =
-        new WebDatabaseService(path, base::ThreadTaskRunnerHandle::Get(),
-                               base::ThreadTaskRunnerHandle::Get());
-
-    // Hacky: hold onto a pointer but pass ownership.
-    profile_autofill_table_ = new AutofillTable;
-    profile_web_database_->AddTable(
-        std::unique_ptr<WebDatabaseTable>(profile_autofill_table_));
-    profile_web_database_->LoadDatabase();
-    profile_database_service_ = new AutofillWebDataService(
-        profile_web_database_, base::ThreadTaskRunnerHandle::Get(),
-        base::ThreadTaskRunnerHandle::Get());
-    profile_database_service_->Init(base::NullCallback());
-
-    account_web_database_ =
-        new WebDatabaseService(base::FilePath(WebDatabase::kInMemoryPath),
-                               base::ThreadTaskRunnerHandle::Get(),
-                               base::ThreadTaskRunnerHandle::Get());
-    account_autofill_table_ = new AutofillTable;
-    account_web_database_->AddTable(
-        std::unique_ptr<WebDatabaseTable>(account_autofill_table_));
-    account_web_database_->LoadDatabase();
-    account_database_service_ = new AutofillWebDataService(
-        account_web_database_, base::ThreadTaskRunnerHandle::Get(),
-        base::ThreadTaskRunnerHandle::Get());
-    account_database_service_->Init(base::NullCallback());
-
-    strike_database_ = std::make_unique<TestInMemoryStrikeDatabase>();
-
-    test::DisableSystemServices(prefs_.get());
-  }
-
-  void TearDownTest() {
-    // Order of destruction is important as BrowserAutofillManager relies on
-    // PersonalDataManager to be around when it gets destroyed.
-    test::ReenableSystemServices();
-    OSCryptMocker::TearDown();
-  }
-
-  void ResetPersonalDataManager(UserMode user_mode,
-                                bool use_sync_transport_mode,
-                                PersonalDataManager* personal_data) {
-    bool is_incognito = (user_mode == USER_MODE_INCOGNITO);
-
-    personal_data->Init(
-        scoped_refptr<AutofillWebDataService>(profile_database_service_),
-        base::FeatureList::IsEnabled(
-            features::kAutofillEnableAccountWalletStorage)
-            ? scoped_refptr<AutofillWebDataService>(account_database_service_)
-            : nullptr,
-        prefs_.get(), prefs_.get(), identity_test_env_.identity_manager(),
-        /*history_service=*/nullptr, strike_database_.get(),
-        /*image_fetcher=*/nullptr, is_incognito);
-
-    personal_data->AddObserver(&personal_data_observer_);
-    std::string email = use_sync_transport_mode ? kSyncTransportAccountEmail
-                                                : kPrimaryAccountEmail;
-    // Set the account in both IdentityManager and SyncService.
-    CoreAccountInfo account_info;
-    signin::ConsentLevel consent_level = use_sync_transport_mode
-                                             ? signin::ConsentLevel::kSignin
-                                             : signin::ConsentLevel::kSync;
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-    identity_test_env_.ClearPrimaryAccount();
-    account_info = identity_test_env_.SetPrimaryAccount(email, consent_level);
-#else
-    // In ChromeOS-Ash, clearing/resetting the primary account is not supported.
-    // So if an account already exists, reuse it (and make sure it matches).
-    if (identity_test_env_.identity_manager()->HasPrimaryAccount(
-            consent_level)) {
-      account_info =
-          identity_test_env_.identity_manager()->GetPrimaryAccountInfo(
-              consent_level);
-      ASSERT_EQ(account_info.email, email);
-    } else {
-      account_info = identity_test_env_.SetPrimaryAccount(email, consent_level);
-    }
-#endif
-    sync_service_.SetAccountInfo(account_info);
-    sync_service_.SetHasSyncConsent(!use_sync_transport_mode);
-    personal_data->OnSyncServiceInitialized(&sync_service_);
-    personal_data->OnStateChanged(&sync_service_);
-
-    WaitForOnPersonalDataChangedRepeatedly();
-  }
-
-  [[nodiscard]] bool TurnOnSyncFeature(PersonalDataManager* personal_data) {
-    sync_service_.SetHasSyncConsent(true);
-    if (!sync_service_.IsSyncFeatureEnabled())
-      return false;
-    personal_data->OnStateChanged(&sync_service_);
-
-    return personal_data->IsSyncFeatureEnabled();
-  }
-
-  void RemoveByGUIDFromPersonalDataManager(const std::string& guid,
-                                           PersonalDataManager* personal_data) {
-    base::RunLoop run_loop;
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
-        .WillOnce(QuitMessageLoop(&run_loop));
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-        .Times(testing::AnyNumber());
-
-    personal_data->RemoveByGUID(guid);
-    run_loop.Run();
-  }
-
-  void SetServerCards(std::vector<CreditCard> server_cards) {
-    test::SetServerCreditCards(account_autofill_table_, server_cards);
-  }
-
-  // Verify that the web database has been updated and the notification sent.
-  void WaitOnceForOnPersonalDataChanged() {
-    base::RunLoop run_loop;
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
-        .WillOnce(QuitMessageLoop(&run_loop));
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged()).Times(1);
-    run_loop.Run();
-  }
-
-  // Verifies that the web database has been updated and the notification sent.
-  void WaitForOnPersonalDataChanged() {
-    base::RunLoop run_loop;
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
-        .WillOnce(QuitMessageLoop(&run_loop));
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-        .Times(testing::AnyNumber());
-    run_loop.Run();
-  }
-
-  // Verifies that the web database has been updated and the notification sent.
-  void WaitForOnPersonalDataChangedRepeatedly() {
-    base::RunLoop run_loop;
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataFinishedProfileTasks())
-        .WillRepeatedly(QuitMessageLoop(&run_loop));
-    EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
-        .Times(testing::AnyNumber());
-    run_loop.Run();
-  }
-
-  base::test::TaskEnvironment task_environment_;
-  std::unique_ptr<PrefService> prefs_;
-  ScopedFeatureListWrapper scoped_features_;
-  network::TestURLLoaderFactory test_url_loader_factory_;
-  signin::IdentityTestEnvironment identity_test_env_;
-  syncer::TestSyncService sync_service_;
-  scoped_refptr<AutofillWebDataService> profile_database_service_;
-  scoped_refptr<AutofillWebDataService> account_database_service_;
-  scoped_refptr<WebDatabaseService> profile_web_database_;
-  scoped_refptr<WebDatabaseService> account_web_database_;
-  raw_ptr<AutofillTable> profile_autofill_table_;  // weak ref
-  raw_ptr<AutofillTable> account_autofill_table_;  // weak ref
-  std::unique_ptr<StrikeDatabaseBase> strike_database_;
-  PersonalDataLoadedObserverMock personal_data_observer_;
-};
 
 class PersonalDataManagerHelper : public PersonalDataManagerTestBase {
  protected:
@@ -360,7 +144,8 @@ class PersonalDataManagerHelper : public PersonalDataManagerTestBase {
       personal_data_->Shutdown();
     personal_data_ = std::make_unique<PersonalDataManager>("EN", "US");
     PersonalDataManagerTestBase::ResetPersonalDataManager(
-        user_mode, use_sync_transport_mode, personal_data_.get());
+        user_mode == USER_MODE_INCOGNITO, use_sync_transport_mode,
+        personal_data_.get());
   }
 
   void ResetProfiles() {
@@ -626,7 +411,8 @@ class PersonalDataManagerMockTest : public PersonalDataManagerTestBase,
     personal_data_ =
         std::make_unique<PersonalDataManagerMock>("en", std::string());
     PersonalDataManagerTestBase::ResetPersonalDataManager(
-        user_mode, /*use_sync_transport_mode=*/true, personal_data_.get());
+        user_mode == USER_MODE_INCOGNITO, /*use_sync_transport_mode=*/true,
+        personal_data_.get());
   }
 
   bool TurnOnSyncFeature() {
