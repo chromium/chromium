0f354c8e01c06807f3a0c5e3fca8ddd92499b639
Rakina Zata Amni <rakina@chromium.org>
2022-08-09 21:56:50 +0000

    Enable SynthesizePageLoadOnlyOnInitialMainDocumentAccess by default
    This has been on 50% of Stable for > 2 weeks (since Jul 12, see cl/460312986) and no bugs or significant increase in crashes have
    been detected. Finch UMA overview also looks good:
    https://uma.googleplex.com/p/chrome/variations?sid=63481284f190ffb8fab695058f048acd
    
    After this CL, we will be able to remove the special handling added in crrev.com/c/3427984 (will be done in a follow-up)
    
    Change-Id: Ic1f98731ed4e47e23bdfb592f78874bf97bfce5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819203
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033216}

--- android_webview/common/aw_features.cc
+++ android_webview/common/aw_features.cc
@@ -110,7 +110,7 @@ const base::FeatureParam<int> kWebViewXRequestedWithHeaderMode{
 // invalidates the URL after).
 const base::Feature kWebViewSynthesizePageLoadOnlyOnInitialMainDocumentAccess{
     "WebViewSynthesizePageLoadOnlyOnInitialMainDocumentAccess",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace android_webview
