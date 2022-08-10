23dc5e23c49fae950ab97874123dc7dfe30037ec
Joshua Pawlicki <waffles@chromium.org>
2022-08-10 20:31:46 +0000

    Updater: propagate timeouts to the helper process.
    Bug: 1345887
    Change-Id: I6b54d8cadeb4989d9c86491ccc617cae715ccdd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823476
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033698}

--- chrome/updater/test/integration_test_commands_system.cc
+++ chrome/updater/test/integration_test_commands_system.cc
@@ -16,6 +16,7 @@
 #include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/test_switches.h"
 #include "base/values.h"
 #include "base/version.h"
 #include "build/build_config.h"
@@ -336,6 +337,14 @@ class IntegrationTestCommandsSystem : public IntegrationTestCommands {
     helper_command.AppendSwitchASCII("gtest_filter",
                                      "TestHelperCommandRunner.Run");
     helper_command.AppendSwitchASCII("gtest_brief", "1");
+    for (const std::string& s :
+         {switches::kUiTestActionTimeout, switches::kUiTestActionMaxTimeout,
+          switches::kTestTinyTimeout, switches::kTestLauncherTimeout}) {
+      if (command_line.HasSwitch(s)) {
+        helper_command.AppendSwitchNative(s,
+                                          command_line.GetSwitchValueNative(s));
+      }
+    }
 
     int exit_code = -1;
     ASSERT_TRUE(Run(updater_scope_, helper_command, &exit_code));
