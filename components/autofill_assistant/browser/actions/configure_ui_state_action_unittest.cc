3eb8969259ccd1002f05746e93af3d72acfe9953
Wladyslaw Kwasnicki <wladek@google.com>
2022-08-10 12:47:00 +0000

    [Autofill Assistant] Add unittests for the ConfigureUIState action.
    Bug: b:203183831
    Change-Id: Idee3ad448c88fa3db93d89ca537076d80566713b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822663
    Commit-Queue: Wladyslaw Kwasnicki <wladek@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#1033474}

---
+++ components/autofill_assistant/browser/actions/configure_ui_state_action_unittest.cc
@@ -0,0 +1,73 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/actions/configure_ui_state_action.h"
+
+#include "action_delegate.h"
+#include "base/test/gmock_callback_support.h"
+#include "base/test/mock_callback.h"
+#include "components/autofill_assistant/browser/actions/mock_action_delegate.h"
+#include "components/autofill_assistant/browser/service.pb.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace autofill_assistant {
+namespace {
+
+using ::testing::Property;
+
+class ConfigureUiStateActionTest : public testing::Test {
+ public:
+  ConfigureUiStateActionTest() = default;
+
+ protected:
+  MockActionDelegate mock_action_delegate_;
+  base::MockCallback<Action::ProcessActionCallback> callback_;
+};
+
+TEST_F(ConfigureUiStateActionTest, SetDefault) {
+  ActionProto action_proto;
+  action_proto.mutable_configure_ui_state()->set_overlay_behavior(
+      ConfigureUiStateProto_OverlayBehavior_DEFAULT);
+  ConfigureUiStateAction action(&mock_action_delegate_, action_proto);
+
+  EXPECT_CALL(mock_action_delegate_,
+              SetOverlayBehavior(ConfigureUiStateProto_OverlayBehavior_DEFAULT))
+      .Times(1);
+  EXPECT_CALL(
+      callback_,
+      Run(Pointee(Property(&ProcessedActionProto::status, ACTION_APPLIED))));
+
+  action.ProcessAction(callback_.Get());
+}
+
+TEST_F(ConfigureUiStateActionTest, SetHidden) {
+  ActionProto action_proto;
+  action_proto.mutable_configure_ui_state()->set_overlay_behavior(
+      ConfigureUiStateProto_OverlayBehavior_HIDDEN);
+  ConfigureUiStateAction action(&mock_action_delegate_, action_proto);
+
+  EXPECT_CALL(mock_action_delegate_,
+              SetOverlayBehavior(ConfigureUiStateProto_OverlayBehavior_HIDDEN))
+      .Times(1);
+  EXPECT_CALL(
+      callback_,
+      Run(Pointee(Property(&ProcessedActionProto::status, ACTION_APPLIED))));
+
+  action.ProcessAction(callback_.Get());
+}
+
+TEST_F(ConfigureUiStateActionTest, OverlayBehaviorNotSet) {
+  ActionProto action_proto;
+  ConfigureUiStateAction action(&mock_action_delegate_, action_proto);
+
+  EXPECT_CALL(mock_action_delegate_, SetOverlayBehavior).Times(0);
+  EXPECT_CALL(
+      callback_,
+      Run(Pointee(Property(&ProcessedActionProto::status, ACTION_APPLIED))));
+
+  action.ProcessAction(callback_.Get());
+}
+
+}  // namespace
+}  // namespace autofill_assistant
