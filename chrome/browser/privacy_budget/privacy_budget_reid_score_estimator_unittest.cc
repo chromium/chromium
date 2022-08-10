4c5a60b25022923fe60273c6c38ec32289f80fc7
Saeid Eid <saeideid@chromium.org>
2022-08-10 09:52:34 +0000

    [Identifiability Study] Refactoring Reid Estimator Unit Testing
    Multiple unit tests for the Reid estimator were testing almost the same results but with different scenarios. We refactored those tests in this CL by replacing them with a parameterised one to remove the code duplication.
    
    Bug: 1330244
    Change-Id: Id7710a9420e47f1069f41202f46918cbde9dc8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3817527
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Saeid Eid <saeideid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033431}

--- chrome/browser/privacy_budget/privacy_budget_reid_score_estimator_unittest.cc
+++ chrome/browser/privacy_budget/privacy_budget_reid_score_estimator_unittest.cc
@@ -20,7 +20,8 @@
 #include "third_party/blink/public/common/privacy_budget/identifiable_token.h"
 #include "third_party/blink/public/common/privacy_budget/scoped_identifiability_test_sample_collector.h"
 
-class PrivacyBudgetReidScoreEstimatorStandaloneTest : public ::testing::Test {
+class PrivacyBudgetReidScoreEstimatorStandaloneTest
+    : public ::testing::TestWithParam<std::tuple<uint32_t, int, int, bool>> {
  public:
   PrivacyBudgetReidScoreEstimatorStandaloneTest() {
     prefs::RegisterPrivacyBudgetPrefs(pref_service_.registry());
@@ -29,6 +30,38 @@ class PrivacyBudgetReidScoreEstimatorStandaloneTest : public ::testing::Test {
   TestingPrefServiceSimple* pref_service() { return &pref_service_; }
 
   void RunUntilIdle() { task_environment_.RunUntilIdle(); }
+
+  void ProcessReidRecords(IdentifiabilityStudyGroupSettings* settings,
+                          bool fixedToken) {
+    for (int i = 0; i < kNumIterations; ++i) {
+      PrivacyBudgetReidScoreEstimator reid_storage(settings, pref_service());
+      reid_storage.ResetPersistedState();
+      reid_storage.Init();
+      int64_t token1 =
+          fixedToken ? 1 : static_cast<int64_t>(base::RandUint64());
+      int64_t token2 =
+          fixedToken ? 2 : static_cast<int64_t>(base::RandUint64());
+      // Process values for 2 surfaces.
+      reid_storage.ProcessForReidScore(kSurface_1,
+                                       blink::IdentifiableToken(token1));
+      reid_storage.ProcessForReidScore(kSurface_2,
+                                       blink::IdentifiableToken(token2));
+    }
+  }
+
+  // Example surfaces for testing.
+  const blink::IdentifiableSurface kSurface_1 =
+      blink::IdentifiableSurface::FromMetricHash(2077075229u);
+  const blink::IdentifiableSurface kSurface_2 =
+      blink::IdentifiableSurface::FromMetricHash(1122849309u);
+
+  // Expected Reid surface key to be reported based on the example surfaces
+  // defined (kSurface_1, kSurface_2) using the function
+  // IdentifiableSurface::FromTypeAndToken() with type
+  // IdentifiableSurface::Type::kReidScoreEstimator.
+  const uint64_t kExpectedSurface = 11332616172707669541u;
+
+  const int kNumIterations = 50;
 
  private:
   TestingPrefServiceSimple pref_service_;
@@ -56,8 +89,9 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
   reid_storage.Init();
 }
 
-TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
-       ReportReidFixedTokenRandomSalt) {
+TEST_P(PrivacyBudgetReidScoreEstimatorStandaloneTest,
+       ReportReidwithParameters) {
+  const auto [salt_ranges, bits, noise, fixed_token] = GetParam();
   auto settings = IdentifiabilityStudyGroupSettings::InitFrom(
       /*enabled=*/true,
       /*expected_surface_count=*/0,
@@ -66,177 +100,22 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       /*blocks_weights=*/"",
       /*allowed_random_types=*/"",
       /*reid_blocks=*/"2077075229;1122849309",
-      /*reid_blocks_salts_ranges=*/"1000000",
-      /*reid_blocks_bits=*/"1",
-      /*reid_blocks_noise_probabilities=*/"0");
+      /*reid_blocks_salts_ranges=*/base::NumberToString(salt_ranges),
+      /*reid_blocks_bits=*/base::NumberToString(bits),
+      /*reid_blocks_noise_probabilities=*/base::NumberToString(noise));
 
-  constexpr auto surface_1 =
-      blink::IdentifiableSurface::FromMetricHash(2077075229u);
-  constexpr auto surface_2 =
-      blink::IdentifiableSurface::FromMetricHash(1122849309u);
-
-  int64_t token1 = 1234;
-  int64_t token2 = 12345;
-  constexpr int num_iterations = 50;
   ukm::TestAutoSetUkmRecorder test_recorder;
   base::RunLoop run_loop;
   test_recorder.SetOnAddEntryCallback(
       ukm::builders::Identifiability::kEntryName,
-      base::BarrierClosure(num_iterations, run_loop.QuitClosure()));
+      base::BarrierClosure(kNumIterations, run_loop.QuitClosure()));
   blink::test::ScopedIdentifiabilityTestSampleCollector collector;
-  for (int i = 0; i < num_iterations; ++i) {
-    PrivacyBudgetReidScoreEstimator reid_storage(&settings, pref_service());
-    reid_storage.ResetPersistedState();
-    reid_storage.Init();
-    // Process values for 2 surfaces.
-    reid_storage.ProcessForReidScore(surface_1,
-                                     blink::IdentifiableToken(token1));
-    reid_storage.ProcessForReidScore(surface_2,
-                                     blink::IdentifiableToken(token2));
-  }
+  ProcessReidRecords(&settings, fixed_token);
   // This should let the async tasks run.
   run_loop.Run();
   const auto& entries = collector.entries();
   bool has_value_0 = false;
   bool has_value_1 = false;
-  int count = 0;
-  for (auto& entry : entries) {
-    for (auto& metric : entry.metrics) {
-      auto surface = metric.surface;
-      if (surface.GetType() ==
-          blink::IdentifiableSurface::Type::kReidScoreEstimator) {
-        EXPECT_EQ(metric.surface.ToUkmMetricHash(), 11332616172707669541u);
-        ++count;
-        uint64_t hash = static_cast<uint64_t>(metric.value.ToUkmMetricValue());
-        uint32_t reid_bits = hash & 0xFFFFFFFF;
-        EXPECT_TRUE(reid_bits == 0 || reid_bits == 1);
-        if (reid_bits == 0)
-          has_value_0 = true;
-        else if (reid_bits == 1)
-          has_value_1 = true;
-        uint32_t salt = (hash >> 32);
-        EXPECT_LT(salt, 1000000u);
-      }
-    }
-  }
-  EXPECT_EQ(count, num_iterations);
-  // Since the 1 bit should be random, the probability of it being always 0 or
-  // always 1 is 2/(2^num_iterations), hence it should be negligible.
-  EXPECT_TRUE(has_value_0);
-  EXPECT_TRUE(has_value_1);
-}
-
-TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
-       ReportReidRandomTokenFixedSalt) {
-  auto settings = IdentifiabilityStudyGroupSettings::InitFrom(
-      /*enabled=*/true,
-      /*expected_surface_count=*/0,
-      /*surface_budget=*/0,
-      /*blocks=*/"",
-      /*blocks_weights=*/"",
-      /*allowed_random_types=*/"",
-      /*reid_blocks=*/"2077075229;1122849309",
-      /*reid_blocks_salts_ranges=*/"1",
-      /*reid_blocks_bits=*/"1",
-      /*reid_blocks_noise_probabilities=*/"0");
-
-  constexpr auto surface_1 =
-      blink::IdentifiableSurface::FromMetricHash(2077075229u);
-  constexpr auto surface_2 =
-      blink::IdentifiableSurface::FromMetricHash(1122849309u);
-  constexpr int num_iterations = 50;
-  ukm::TestAutoSetUkmRecorder test_recorder;
-  base::RunLoop run_loop;
-  test_recorder.SetOnAddEntryCallback(
-      ukm::builders::Identifiability::kEntryName,
-      base::BarrierClosure(num_iterations, run_loop.QuitClosure()));
-  blink::test::ScopedIdentifiabilityTestSampleCollector collector;
-  for (int i = 0; i < num_iterations; ++i) {
-    PrivacyBudgetReidScoreEstimator reid_storage(&settings, pref_service());
-    reid_storage.ResetPersistedState();
-    reid_storage.Init();
-    // Create random tokens.
-    int64_t token1 = static_cast<int64_t>(base::RandUint64());
-    int64_t token2 = static_cast<int64_t>(base::RandUint64());
-    // Process values for 2 surfaces.
-    reid_storage.ProcessForReidScore(surface_1,
-                                     blink::IdentifiableToken(token1));
-    reid_storage.ProcessForReidScore(surface_2,
-                                     blink::IdentifiableToken(token2));
-  }
-  // This should let the async tasks run.
-  run_loop.Run();
-  const auto& entries = collector.entries();
-  bool has_value_0 = false;
-  bool has_value_1 = false;
-  int count = 0;
-  for (auto& entry : entries) {
-    for (auto& metric : entry.metrics) {
-      auto surface = metric.surface;
-      if (surface.GetType() ==
-          blink::IdentifiableSurface::Type::kReidScoreEstimator) {
-        EXPECT_EQ(metric.surface.ToUkmMetricHash(), 11332616172707669541u);
-        ++count;
-        uint64_t hash = static_cast<uint64_t>(metric.value.ToUkmMetricValue());
-        uint32_t reid_bits = hash & 0xFFFFFFFF;
-        EXPECT_TRUE(reid_bits == 0 || reid_bits == 1);
-        if (reid_bits == 0)
-          has_value_0 = true;
-        else if (reid_bits == 1)
-          has_value_1 = true;
-        uint32_t salt = (hash >> 32);
-        EXPECT_EQ(salt, 0u);
-      }
-    }
-  }
-  EXPECT_EQ(count, num_iterations);
-  // Since the 1 bit should be random, the probability of it being always 0 or
-  // always 1 is 2/(2^num_iterations), hence it should be negligible.
-  EXPECT_TRUE(has_value_0);
-  EXPECT_TRUE(has_value_1);
-}
-
-TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
-       ReportReidFixedTokenFixedSaltAllNoise) {
-  auto settings = IdentifiabilityStudyGroupSettings::InitFrom(
-      /*enabled=*/true,
-      /*expected_surface_count=*/0,
-      /*surface_budget=*/0,
-      /*blocks=*/"",
-      /*blocks_weights=*/"",
-      /*allowed_random_types=*/"",
-      /*reid_blocks=*/"2077075229;1122849309",
-      /*reid_blocks_salts_ranges=*/"1",
-      /*reid_blocks_bits=*/"32",
-      /*reid_blocks_noise_probabilities=*/"1");
-
-  constexpr auto surface_1 =
-      blink::IdentifiableSurface::FromMetricHash(2077075229u);
-  constexpr auto surface_2 =
-      blink::IdentifiableSurface::FromMetricHash(1122849309u);
-
-  int64_t token1 = 1234;
-  int64_t token2 = 12345;
-  constexpr int num_iterations = 50;
-  ukm::TestAutoSetUkmRecorder test_recorder;
-  base::RunLoop run_loop;
-  test_recorder.SetOnAddEntryCallback(
-      ukm::builders::Identifiability::kEntryName,
-      base::BarrierClosure(num_iterations, run_loop.QuitClosure()));
-  blink::test::ScopedIdentifiabilityTestSampleCollector collector;
-  for (int i = 0; i < num_iterations; ++i) {
-    PrivacyBudgetReidScoreEstimator reid_storage(&settings, pref_service());
-    reid_storage.ResetPersistedState();
-    reid_storage.Init();
-    // Process values for 2 surfaces.
-    reid_storage.ProcessForReidScore(surface_1,
-                                     blink::IdentifiableToken(token1));
-    reid_storage.ProcessForReidScore(surface_2,
-                                     blink::IdentifiableToken(token2));
-  }
-  // This should let the async tasks run.
-  run_loop.Run();
-  const auto& entries = collector.entries();
   base::flat_set<uint32_t> reid_results;
   int count = 0;
   for (auto& entry : entries) {
@@ -244,20 +123,40 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       auto surface = metric.surface;
       if (surface.GetType() ==
           blink::IdentifiableSurface::Type::kReidScoreEstimator) {
-        EXPECT_EQ(metric.surface.ToUkmMetricHash(), 11332616172707669541u);
+        EXPECT_EQ(metric.surface.ToUkmMetricHash(), kExpectedSurface);
         ++count;
         uint64_t hash = static_cast<uint64_t>(metric.value.ToUkmMetricValue());
         uint32_t reid_bits = hash & 0xFFFFFFFF;
-        // Result should be noise i.e. didn't appeared before.
-        EXPECT_FALSE(reid_results.contains(reid_bits));
-        reid_results.insert(reid_bits);
+        if (noise == 1) {
+          // Result should be noise i.e. didn't appear before.
+          EXPECT_FALSE(reid_results.contains(reid_bits));
+          reid_results.insert(reid_bits);
+        } else {
+          EXPECT_TRUE(reid_bits == 0 || reid_bits == 1);
+          if (reid_bits == 0)
+            has_value_0 = true;
+          else if (reid_bits == 1)
+            has_value_1 = true;
+        }
         uint32_t salt = (hash >> 32);
-        EXPECT_EQ(salt, 0u);
+        EXPECT_TRUE((salt >= 0) && (salt < salt_ranges));
       }
     }
   }
-  EXPECT_EQ(count, num_iterations);
+  EXPECT_EQ(count, kNumIterations);
+  // Since the 1 bit should be random, the probability of it being always 0 or
+  // always 1 is 2/(2^kNumIterations), hence it should be negligible.
+  if (noise != 1) {
+    EXPECT_TRUE(has_value_0);
+    EXPECT_TRUE(has_value_1);
+  }
 }
+
+INSTANTIATE_TEST_SUITE_P(PrivacyBudgetReidScoreEstimatorParameterizedTest,
+                         PrivacyBudgetReidScoreEstimatorStandaloneTest,
+                         ::testing::Values(std::make_tuple(1000000, 1, 0, true),
+                                           std::make_tuple(1, 1, 0, false),
+                                           std::make_tuple(1, 32, 1, true)));
 
 TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
        ReidHashIsReportedOnlyOnce) {
@@ -273,11 +172,6 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       /*reid_blocks_bits=*/"1",
       /*reid_blocks_noise_probabilities=*/"0");
 
-  constexpr auto surface_1 =
-      blink::IdentifiableSurface::FromMetricHash(2077075229u);
-  constexpr auto surface_2 =
-      blink::IdentifiableSurface::FromMetricHash(1122849309u);
-
   ukm::TestAutoSetUkmRecorder test_recorder;
 
   {
@@ -291,8 +185,8 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       blink::test::ScopedIdentifiabilityTestSampleCollector collector;
 
       // Process values for 2 surfaces.
-      reid_storage.ProcessForReidScore(surface_1, blink::IdentifiableToken(1));
-      reid_storage.ProcessForReidScore(surface_2, blink::IdentifiableToken(2));
+      reid_storage.ProcessForReidScore(kSurface_1, blink::IdentifiableToken(1));
+      reid_storage.ProcessForReidScore(kSurface_2, blink::IdentifiableToken(2));
 
       // This should let the async tasks run.
       run_loop.Run();
@@ -301,7 +195,7 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       EXPECT_EQ(entries.size(), 1u);
       EXPECT_EQ(entries[0].metrics.size(), 1u);
       EXPECT_EQ(entries[0].metrics[0].surface.ToUkmMetricHash(),
-                11332616172707669541u);
+                kExpectedSurface);
     }
 
     // Now check that the reid hash is not reported again if we see again the
@@ -309,8 +203,8 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
     {
       blink::test::ScopedIdentifiabilityTestSampleCollector collector;
 
-      reid_storage.ProcessForReidScore(surface_1, blink::IdentifiableToken(1));
-      reid_storage.ProcessForReidScore(surface_2, blink::IdentifiableToken(2));
+      reid_storage.ProcessForReidScore(kSurface_1, blink::IdentifiableToken(1));
+      reid_storage.ProcessForReidScore(kSurface_2, blink::IdentifiableToken(2));
 
       RunUntilIdle();
       const auto& entries = collector.entries();
@@ -327,8 +221,8 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
     {
       blink::test::ScopedIdentifiabilityTestSampleCollector collector;
 
-      reid_storage.ProcessForReidScore(surface_1, blink::IdentifiableToken(1));
-      reid_storage.ProcessForReidScore(surface_2, blink::IdentifiableToken(2));
+      reid_storage.ProcessForReidScore(kSurface_1, blink::IdentifiableToken(1));
+      reid_storage.ProcessForReidScore(kSurface_2, blink::IdentifiableToken(2));
 
       RunUntilIdle();
 
@@ -347,8 +241,8 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
           ukm::builders::Identifiability::kEntryName, run_loop.QuitClosure());
 
       blink::test::ScopedIdentifiabilityTestSampleCollector collector;
-      reid_storage.ProcessForReidScore(surface_1, blink::IdentifiableToken(1));
-      reid_storage.ProcessForReidScore(surface_2, blink::IdentifiableToken(2));
+      reid_storage.ProcessForReidScore(kSurface_1, blink::IdentifiableToken(1));
+      reid_storage.ProcessForReidScore(kSurface_2, blink::IdentifiableToken(2));
 
       run_loop.Run();
 
@@ -356,7 +250,7 @@ TEST_F(PrivacyBudgetReidScoreEstimatorStandaloneTest,
       EXPECT_EQ(entries.size(), 1u);
       EXPECT_EQ(entries[0].metrics.size(), 1u);
       EXPECT_EQ(entries[0].metrics[0].surface.ToUkmMetricHash(),
-                11332616172707669541u);
+                kExpectedSurface);
     }
   }
 }
