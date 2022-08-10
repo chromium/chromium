ca6d0d540406e607f48208f017dc5de0bba8fce3
Chris Lu <thegreenfrog@chromium.org>
2022-08-10 21:05:57 +0000

    [ios] Remove extra margin when calculating ContentSuggestionsHeight
    The StackView doesn't add spacing under the last element, so
    there is no need to factor spacing in below the Shortcuts.
    
    Bug: 1351198
    Change-Id: Ia7eb9a9df386ab3fcfb0d383bd07db4f129b0b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3823725
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1033710}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -618,8 +618,7 @@ CGFloat GetModuleWidthForHorizontalTraitCollection(
       height +=
           MostVisitedCellSize(
               UIApplication.sharedApplication.preferredContentSizeCategory)
-              .height +
-          kMostVisitedBottomMargin;
+              .height;
     }
   }
   if (self.returnToRecentTabTile) {
