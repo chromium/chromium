b3c058f5ae4f24439f06324b669c3f07fc4b9a2a
Manish Mandlik <mmandlik@google.com>
2022-08-11 23:41:27 +0000

    Add a feature flag for bluetooth devcoredump
    Bluetooth controllers emit a devcoredump whenever controller exception
    occurs. Add a flag to enable bluetooth devcoredump collection feature.
    
    BUG=b:203034370
    TEST=autoninja chrome
    
    Change-Id: I33da39826cc95d1b6e760e1ceae719b7b4cfeb81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812278
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034239}

---
+++ device/bluetooth/chromeos_platform_features.cc
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/chromeos_platform_features.h"
+
+namespace chromeos::bluetooth::features {
+
+const base::Feature kBluetoothCoredump{"BluetoothCoredump",
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace chromeos::bluetooth::features
