1c1d8da948f206db207540562098c4f1ab9eccbe
Daniel White <danieltwhite@google.com>
2022-08-11 19:32:49 +0000

    Convert link_to_text.js to TypeScript
    To improve compile time error reporting, reduce unforeseen breakages,
    and modernize the codebase's scripts, link_to_text.js has been converted
    to TypeScript.
    
    Bug: 1325547
    Change-Id: Icbb93233424b1ece1a479a56ac0e781158b57e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3755651
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034129}

--- ios/chrome/browser/link_to_text/resources/link_to_text.ts
+++ ios/chrome/browser/link_to_text/resources/link_to_text.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 import * as utils from '//third_party/text-fragments-polyfill/src/src/fragment-generation-utils.js';
 
 /**
@@ -9,44 +10,37 @@ import * as utils from '//third_party/text-fragments-polyfill/src/src/fragment-g
  * the utility functions from the text-fragments-polyfill library.
  */
 
-__gCrWeb['linkToText'] = {};
-
 /**
  * Attempts to generate a link with text fragments for the current
  * page's selection.
  */
-__gCrWeb.linkToText.getLinkToText =
-    function() {
+function getLinkToText() {
   const selection = window.getSelection();
-  const selectedText = `"${selection.toString()}"`;
-  const selectionRect = {x: 0, y: 0, width: 0, height: 0};
+  let selectionRect = {x: 0, y: 0, width: 0, height: 0};
 
-  if (selection.rangeCount) {
+  if (selection && selection.rangeCount) {
     // Get the selection range's first client rect.
     const domRect = selection.getRangeAt(0).getClientRects()[0];
-    selectionRect.x = domRect.x;
-    selectionRect.y = domRect.y;
-    selectionRect.width = domRect.width;
-    selectionRect.height = domRect.height;
+    if (domRect) {
+      selectionRect.x = domRect.x;
+      selectionRect.y = domRect.y;
+      selectionRect.width = domRect.width;
+      selectionRect.height = domRect.height;
+    }
   }
 
+  const selectedText = `"${selection?.toString()}"`;
   const canonicalLinkNode = document.querySelector('link[rel=\'canonical\']');
 
-  const response = utils.generateFragment(selection);
+  const response = utils.generateFragment(selection as Selection);
   return {
     status: response.status,
     fragment: response.fragment,
     selectedText: selectedText,
     selectionRect: selectionRect,
-    canonicalUrl: canonicalLinkNode && canonicalLinkNode.getAttribute('href')
+    canonicalUrl: canonicalLinkNode
+        && canonicalLinkNode.getAttribute('href')
   };
 }
 
-/**
- * Checks if the range is suitable to attempt link generation; the feature
- * should be disabled if this does not return true.
- */
-__gCrWeb.linkToText.checkPreconditions = function() {
-  return utils.isValidRangeForFragmentGeneration(
-      window.getSelection().getRangeAt(0));
-}
+gCrWeb.linkToText =  { getLinkToText };
