951479617a85dcbfe50464629ead920c3579178a
Samuel Huang <huangs@chromium.org>
2022-08-12 21:04:23 +0000

    [fileSystem API] Fix requestFileSystem() notification icon loading.
    ShowNotificationForAutoGrantedRequestFileSystem() is used by
    chrome.fileSystem.requestFileSystem() to show a system notification
    when an auto-launched Kiosk app gains access to a volume.
    
    The function uses helper class AppNotificationLauncher. The class
    previously self-deletes in OnAppImageUpdated(). This was triggered in
    ChromeAppIconLoader::FetchImage(), and was problematic because:
    1. Synchronous update can take place on initial icon creation,
       resulting in UAF in FetchImage() on re-entry
    2. Asynchronous update can take place when the actual icon loads.
       Therefore deferred deletion can lead to races that randomly cause
       UAF and double-free.
    3. Even without the memory issues, deleting the class on update
       prevents the desired icon load!
    
    This CL fixes the above issues by extending the lifetime of
    AppNotificationLauncher (and ChromeAppIconLoader instance) to match that of the notification. To this end, we also force every
    ShowNotificationForAutoGrantedRequestFileSystem() notification to show, instead of being suppressed by an existing notification opened for the
    same volume. Details:
    * AppNotificationLauncher:
      * Remove self-delete in OnAppImageUpdated().
      * Implement message_center::NotificationDelegate, and in Close(),
        indirectly self-delete by deleting the provided (in InitAndShow())
        message_center::Notification instance, which holds a reference to
        |this|.
    * ShowNotificationForAutoGrantedRequestFileSystem():
      * Add increasing sequence number to |notification_id|, to force
        notification to appear always.
      * Instantiate AppNotificationLauncher, and pass it to
        message_center::Notification so it can listen to dialog Close.
    
    (cherry picked from commit 151c462940349b7dc5b64e3e2a6bd1bf1422cbcd)
    
    Bug: 1343894, 1263204
    Change-Id: I7c808f814cb8fe517cfa49d8c3e5114e5aa16b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3776315
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1027773}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828494
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5112@{#1458}
    Cr-Branched-From: b13d3fe7b3c47a56354ef54b221008afa754412e-refs/heads/main@{#1012729}

--- chrome/browser/extensions/api/file_system/request_file_system_notification.cc
+++ chrome/browser/extensions/api/file_system/request_file_system_notification.cc
@@ -9,7 +9,9 @@
 
 #include "ash/constants/notifier_catalogs.h"
 #include "base/callback.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
 #include "chrome/browser/browser_process.h"
@@ -35,9 +37,10 @@ namespace {
 const int kIconSize = 48;
 
 // Loads an app's icon and uses it to display a notification.
-class AppNotificationLauncher : public AppIconLoaderDelegate {
+class AppNotificationLauncher : public AppIconLoaderDelegate,
+                                public message_center::NotificationDelegate {
  public:
-  // This class owns and deletes itself after showing the notification.
+  // This class owns and deletes itself after the shown notification closes.
   AppNotificationLauncher() = default;
 
   AppNotificationLauncher(const AppNotificationLauncher&) = delete;
@@ -52,24 +55,33 @@ class AppNotificationLauncher : public AppIconLoaderDelegate {
     icon_loader_ =
         std::make_unique<ChromeAppIconLoader>(profile, kIconSize, this);
     icon_loader_->FetchImage(extension.id());
-
-    // |this| may be destroyed!
   }
 
   // AppIconLoaderDelegate overrides:
+  // This is triggered from FetchImage() in InitAndShow(), and can be called
+  // multiple times, synchronously or asynchronously.
   void OnAppImageUpdated(const std::string& id,
                          const gfx::ImageSkia& image) override {
     pending_notification_->set_icon(ui::ImageModel::FromImageSkia(image));
-    NotificationDisplayService::GetForProfile(profile_)->Display(
-        NotificationHandler::Type::TRANSIENT, *pending_notification_,
-        /*metadata=*/nullptr);
-    delete this;
+    auto* notification_display_service =
+        NotificationDisplayService::GetForProfile(profile_);
+
+    notification_display_service->Display(NotificationHandler::Type::TRANSIENT,
+                                          *pending_notification_,
+                                          /*metadata=*/nullptr);
+  }
+
+  // message_center::NotificationDelegate override.
+  void Close(bool by_user) override {
+    // On notification close, free |pending_notification| (which holds a
+    // reference to this) to trigger self-deletion.
+    pending_notification_.reset();
   }
 
  private:
   ~AppNotificationLauncher() override = default;
 
-  Profile* profile_;
+  base::raw_ptr<Profile> profile_;
   std::unique_ptr<AppIconLoader> icon_loader_;
   std::unique_ptr<message_center::Notification> pending_notification_;
 };
@@ -87,8 +99,15 @@ void ShowNotificationForAutoGrantedRequestFileSystem(
   if (!volume.get())
     return;
 
+  static int sequence = 0;
+  // Create globally unique |notification_id| so that notifications are not
+  // suppressed, thus allowing each AppNotificationLauncher instance to
+  // correspond to an actual notification, and properly deallocated on close.
   const std::string notification_id =
-      extension.id() + "-" + volume->volume_id();
+      base::StringPrintf("%s-%s-%d", extension.id().c_str(),
+                         volume->volume_id().c_str(), sequence);
+  ++sequence;
+
   message_center::RichNotificationData data;
 
   // TODO(mtomasz): Share this code with RequestFileSystemDialogView.
@@ -101,6 +120,10 @@ void ShowNotificationForAutoGrantedRequestFileSystem(
           : IDS_FILE_SYSTEM_REQUEST_FILE_SYSTEM_NOTIFICATION_MESSAGE,
       display_name);
 
+  // Helper that self-deletes when the displayed notification closes.
+  scoped_refptr<AppNotificationLauncher> app_notification_launcher =
+      base::MakeRefCounted<AppNotificationLauncher>();
+
   std::unique_ptr<message_center::Notification> notification(new Notification(
       message_center::NOTIFICATION_TYPE_SIMPLE, notification_id,
       base::UTF8ToUTF16(extension.name()), message,
@@ -110,11 +133,10 @@ void ShowNotificationForAutoGrantedRequestFileSystem(
       message_center::NotifierId(
           message_center::NotifierType::SYSTEM_COMPONENT, notification_id,
           ash::NotificationCatalogName::kRequestFileSystem),
-      data, base::MakeRefCounted<message_center::NotificationDelegate>()));
+      data, app_notification_launcher));
 
-  // AppNotificationLauncher will delete itself.
-  (new AppNotificationLauncher())
-      ->InitAndShow(profile, extension, std::move(notification));
+  app_notification_launcher->InitAndShow(profile, extension,
+                                         std::move(notification));
 }
 
 }  // namespace extensions
