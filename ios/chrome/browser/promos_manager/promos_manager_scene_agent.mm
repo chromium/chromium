392ca50c738a947c71ffd51dfdbf873659edb8b3
Benjamin Williams <bwwilliams@google.com>
2022-08-12 00:15:28 +0000

    Implement SceneStateObserver for PromosManagerSceneAgent.
    Bug: 1349501
    Change-Id: I8b0c8598399268a451f7a9b7e8ac861f14a5826d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3806284
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034254}

--- ios/chrome/browser/promos_manager/promos_manager_scene_agent.mm
+++ ios/chrome/browser/promos_manager/promos_manager_scene_agent.mm
@@ -4,13 +4,23 @@
 
 #import "ios/chrome/browser/promos_manager/promos_manager_scene_agent.h"
 
+#import "base/time/time.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/app_state_observer.h"
+#import "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+// If the previous app foregrounding occurred less than
+// `kMinutesBetweenForegrounding` minutes ago, a promo won't be displayed.
+constexpr int kMinutesBetweenForegrounding = 240;  // (4 hours) (inclusive)
+
+}  // namespace
 
 @interface PromosManagerSceneAgent () <AppStateObserver>
 @end
@@ -40,6 +50,21 @@
   [self handlePromoDisplayIfUIAvailable];
 }
 
+#pragma mark - SceneStateObserver
+
+- (void)sceneStateDidDisableUI:(SceneState*)sceneState {
+  [self.sceneState.appState removeObserver:self];
+  [self.sceneState removeObserver:self];
+}
+
+- (void)sceneState:(SceneState*)sceneState
+    transitionedToActivationLevel:(SceneActivationLevel)level {
+  // Monitor the scene activation level to consider showing a promo
+  // when the scene becomes active and in the foreground. In which case the
+  // scene is visible and interactable.
+  [self handlePromoDisplayIfUIAvailable];
+}
+
 #pragma mark - Private
 
 // Handle the display of a promo if the scene UI is available to display one.
@@ -62,29 +87,40 @@
   if (self.sceneState.activationLevel < SceneActivationLevelForegroundActive)
     return NO;
 
-  // (3) There is no UI blocker.
+  // (3) At least `kMinutesBetweenForegrounding` have elapsed since the app was
+  // last foregrounded, if ever.
+  if (!self.sceneState.appState.lastTimeInForeground.is_null()) {
+    base::TimeDelta elapsedTimeSinceLastForeground =
+        base::TimeTicks::Now() - self.sceneState.appState.lastTimeInForeground;
+
+    if (elapsedTimeSinceLastForeground.InMinutes() <
+        kMinutesBetweenForegrounding)
+      return NO;
+  }
+
+  // (4) There is no UI blocker.
   if (self.sceneState.appState.currentUIBlocker)
     return NO;
 
-  // (4) The app isn't shutting down.
+  // (5) The app isn't shutting down.
   if (self.sceneState.appState.appIsTerminating)
     return NO;
 
-  // (5) There are no launch intents (external intents).
+  // (6) There are no launch intents (external intents).
   if (self.sceneState.startupHadExternalIntent)
     return NO;
 
-  // (6) The app isn't launching after a crash.
+  // (7) The app isn't launching after a crash.
   if (self.sceneState.appState.postCrashLaunch)
     return NO;
 
   // Additional, sensible checks to add to minimize user annoyance:
 
-  // (7) The user isn't currently signing in.
+  // (8) The user isn't currently signing in.
   if (self.sceneState.signinInProgress)
     return NO;
 
-  // (8) The user isn't currently looking at a modal overlay.
+  // (9) The user isn't currently looking at a modal overlay.
   if (self.sceneState.presentingModalOverlay)
     return NO;
 
