1fef1d2e74d75f2653497344a5723c3f53ef5e93
Mehrab N <mehrab@chromium.org>
2022-08-12 02:20:06 +0000

    autocorrect: add test fixture
    There are common codes for setting up the test environment. They are now
    moved to a test fixture allowing to reduce related code duplications and
    simplify addition of new tests and reading the existing ones. Also, a unittest related to accepting autocorrect with key presses is improved.
    
    Bug: b:161490813
    Change-Id: I99f0e67a98529bdce0c0b68c01527e531f7dcd81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812946
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: Mehrab N <mehrab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034300}

--- chrome/browser/ash/input_method/autocorrect_manager_unittest.cc
+++ chrome/browser/ash/input_method/autocorrect_manager_unittest.cc
@@ -40,9 +40,9 @@ AssistiveWindowProperties CreateVisibleUndoWindowProperties(
   AssistiveWindowProperties window_properties;
   window_properties.type = ui::ime::AssistiveWindowType::kUndoWindow;
   window_properties.visible = true;
-  window_properties.announce_string = l10n_util::GetStringFUTF16(
-      IDS_SUGGESTION_AUTOCORRECT_UNDO_WINDOW_SHOWN, original_text,
-      autocorrected_text);
+  window_properties.announce_string =
+      l10n_util::GetStringFUTF16(IDS_SUGGESTION_AUTOCORRECT_UNDO_WINDOW_SHOWN,
+                                 original_text, autocorrected_text);
   return window_properties;
 }
 
@@ -99,150 +99,131 @@ class MockSuggestionHandler : public SuggestionHandlerInterface {
   MOCK_METHOD(void, Announce, (const std::u16string& text), (override));
 };
 
-TEST(AutocorrectManagerTest, HandleAutocorrectSetsAutocorrectRange) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  MockSuggestionHandler mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
+class AutocorrectManagerTest : public testing::Test {
+ protected:
+  AutocorrectManagerTest() : manager_(&mock_suggestion_handler_) {
+    ui::IMEBridge::Get()->SetInputContextHandler(
+        &mock_ime_input_context_handler_);
+    keyboard_client_ = ChromeKeyboardControllerClient::CreateForTest();
+    keyboard_client_->set_keyboard_visible_for_test(false);
+  }
 
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+  ui::MockIMEInputContextHandler mock_ime_input_context_handler_;
+  ::testing::StrictMock<MockSuggestionHandler> mock_suggestion_handler_;
+  AutocorrectManager manager_;
+  std::unique_ptr<ChromeKeyboardControllerClient> keyboard_client_;
+  base::HistogramTester histogram_tester_;
+};
 
-  EXPECT_EQ(mock_ime_input_context_handler.GetAutocorrectRange(),
+TEST_F(AutocorrectManagerTest, HandleAutocorrectSetsAutocorrectRange) {
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+  EXPECT_EQ(mock_ime_input_context_handler_.GetAutocorrectRange(),
             gfx::Range(0, 3));
 }
 
-TEST(AutocorrectManagerTest, OnKeyEventHidesUnderlineAfterEnoughKeyPresses) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  MockSuggestionHandler mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+TEST_F(AutocorrectManagerTest, OnKeyEventHidesUnderlineAfterEnoughKeyPresses) {
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
 
-  const auto key_event =
+  const ui::KeyEvent key_event =
       CreateKeyEvent(ui::DomKey::FromCharacter('a'), ui::DomCode::US_A);
-  EXPECT_FALSE(manager.OnKeyEvent(key_event));
-  EXPECT_FALSE(manager.OnKeyEvent(key_event));
-  EXPECT_FALSE(manager.OnKeyEvent(key_event));
-  EXPECT_FALSE(manager.OnKeyEvent(key_event));
-  EXPECT_FALSE(manager.OnKeyEvent(key_event));
+  EXPECT_FALSE(manager_.OnKeyEvent(key_event));
+  EXPECT_FALSE(manager_.OnKeyEvent(key_event));
+  EXPECT_FALSE(manager_.OnKeyEvent(key_event));
+  EXPECT_FALSE(manager_.OnKeyEvent(key_event));
 
-  EXPECT_TRUE(mock_ime_input_context_handler.GetAutocorrectRange().is_empty());
+  EXPECT_TRUE(mock_ime_input_context_handler_.GetAutocorrectRange().is_empty());
 }
 
-TEST(AutocorrectManagerTest, MovingCursorInsideRangeShowsAssistiveWindow) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  ::testing::StrictMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
-                                   /*anchor_pos=*/4);
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+TEST_F(AutocorrectManagerTest, MovingCursorInsideRangeShowsAssistiveWindow) {
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
+                                    /*anchor_pos=*/4);
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
 
   AssistiveWindowProperties properties =
       CreateVisibleUndoWindowProperties(u"teh", u"the");
-  EXPECT_CALL(mock_suggestion_handler,
+  EXPECT_CALL(mock_suggestion_handler_,
               SetAssistiveWindowProperties(_, properties, _));
 
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/1,
-                                   /*anchor_pos=*/1);
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/1,
+                                    /*anchor_pos=*/1);
 }
 
-TEST(AutocorrectManagerTest, MovingCursorOutsideRangeHidesAssistiveWindow) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  ::testing::StrictMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
-                                   /*anchor_pos=*/4);
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+TEST_F(AutocorrectManagerTest, MovingCursorOutsideRangeHidesAssistiveWindow) {
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
+                                    /*anchor_pos=*/4);
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
 
   {
     ::testing::InSequence seq;
 
     AssistiveWindowProperties shown_properties =
         CreateVisibleUndoWindowProperties(u"teh", u"the");
-    EXPECT_CALL(mock_suggestion_handler,
+    EXPECT_CALL(mock_suggestion_handler_,
                 SetAssistiveWindowProperties(_, shown_properties, _));
 
     AssistiveWindowProperties hidden_properties =
         CreateHiddenUndoWindowProperties();
-    EXPECT_CALL(mock_suggestion_handler,
+    EXPECT_CALL(mock_suggestion_handler_,
                 SetAssistiveWindowProperties(_, hidden_properties, _));
   }
 
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/1,
-                                   /*anchor_pos=*/1);
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
-                                   /*anchor_pos=*/4);
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/1,
+                                    /*anchor_pos=*/1);
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
+                                    /*anchor_pos=*/4);
 }
 
-TEST(AutocorrectManagerTest, UndoAutocorrectSingleWordInComposition) {
+TEST_F(AutocorrectManagerTest, UndoAutocorrectSingleWordInComposition) {
   ui::FakeTextInputClient fake_text_input_client(ui::TEXT_INPUT_TYPE_TEXT);
   ui::InputMethodAsh ime(nullptr);
   ui::IMEBridge::Get()->SetInputContextHandler(&ime);
   ime.SetFocusedTextInputClient(&fake_text_input_client);
 
-  ::testing::NiceMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  manager.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
-                                   /*anchor_pos=*/4);
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+  manager_.OnSurroundingTextChanged(u"the ", /*cursor_pos=*/4,
+                                    /*anchor_pos=*/4);
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
 
   // Move cursor to the middle of 'the' and bring the text into composition.
   fake_text_input_client.SetTextAndSelection(u"the ", gfx::Range(2));
   ime.SetComposingRange(0, 3, {});
 
-  manager.UndoAutocorrect();
+  manager_.UndoAutocorrect();
 
   EXPECT_EQ(fake_text_input_client.text(), u"teh ");
 }
 
-TEST(AutocorrectManagerTest, UndoAutocorrectMultipleWordInComposition) {
+TEST_F(AutocorrectManagerTest, UndoAutocorrectMultipleWordInComposition) {
   ui::FakeTextInputClient fake_text_input_client(ui::TEXT_INPUT_TYPE_TEXT);
   ui::InputMethodAsh ime(nullptr);
   ui::IMEBridge::Get()->SetInputContextHandler(&ime);
   ime.SetFocusedTextInputClient(&fake_text_input_client);
 
-  ::testing::NiceMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  manager.OnSurroundingTextChanged(u"hello world ", /*cursor_pos=*/12,
-                                   /*anchor_pos=*/12);
-  manager.HandleAutocorrect(gfx::Range(0, 11), u"helloworld", u"hello world");
+  manager_.OnSurroundingTextChanged(u"hello world ", /*cursor_pos=*/12,
+                                    /*anchor_pos=*/12);
+  manager_.HandleAutocorrect(gfx::Range(0, 11), u"helloworld", u"hello world");
 
   // Move cursor to the middle of 'hello' and bring the word into composition.
   fake_text_input_client.SetTextAndSelection(u"hello world ", gfx::Range(2));
   ime.SetComposingRange(0, 5, {});
 
-  manager.UndoAutocorrect();
+  manager_.UndoAutocorrect();
 
   EXPECT_EQ(fake_text_input_client.text(), u"helloworld ");
 }
 
-TEST(AutocorrectManagerTest, RecordVirtualKeyboardMetricsWhenVisible) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  ::testing::StrictMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  auto keyboard_client = ChromeKeyboardControllerClient::CreateForTest();
-  keyboard_client->set_keyboard_visible_for_test(true);
-  base::HistogramTester histogram_tester;
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
-  histogram_tester.ExpectUniqueSample(
+TEST_F(AutocorrectManagerTest, RecordVirtualKeyboardMetricsWhenVisible) {
+  keyboard_client_->set_keyboard_visible_for_test(true);
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+  histogram_tester_.ExpectUniqueSample(
       "InputMethod.Assistive.Autocorrect.Actions.VK",
       1 /*AutocorrectActions::kUnderlined*/, 1);
 }
 
-TEST(AutocorrectManagerTest,
-     DoesNotRecordVirtualKeyboardMetricsWhenNotVisible) {
-  ui::MockIMEInputContextHandler mock_ime_input_context_handler;
-  ui::IMEBridge::Get()->SetInputContextHandler(&mock_ime_input_context_handler);
-  ::testing::StrictMock<MockSuggestionHandler> mock_suggestion_handler;
-  AutocorrectManager manager(&mock_suggestion_handler);
-  auto keyboard_client = ChromeKeyboardControllerClient::CreateForTest();
-  keyboard_client->set_keyboard_visible_for_test(false);
-  base::HistogramTester histogram_tester;
-  manager.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
-  histogram_tester.ExpectUniqueSample(
+TEST_F(AutocorrectManagerTest,
+       DoesNotRecordVirtualKeyboardMetricsWhenNotVisible) {
+  keyboard_client_->set_keyboard_visible_for_test(false);
+  manager_.HandleAutocorrect(gfx::Range(0, 3), u"teh", u"the");
+  histogram_tester_.ExpectUniqueSample(
       "InputMethod.Assistive.Autocorrect.Actions.VK",
       1 /*AutocorrectActions::kUnderlined*/, 0);
 }
