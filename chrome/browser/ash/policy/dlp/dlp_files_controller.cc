08ab021cabdc1607409ef8fd2a89099e0b5c2dec
Aida Zolic <aidazolic@chromium.org>
2022-08-12 08:33:30 +0000

    Reland "DLP: Return values in GetDlpRestrictionDetails()"
    This is a reland of commit 48e7cc3527fc2e2191d542140b6d0507d3442b70
    
    Original change's description:
    > DLP: Return values in GetDlpRestrictionDetails()
    >
    > Implements the method in DlpFilesController and modifies the private
    > fileManager API to call the method and retrieve the restriction details.
    >
    > R=ayaelattar@chromium.org, lucmult@chromium.org
    >
    > Bug: 1346254
    > Change-Id: I986c21be8166bcfe0bc9bff1d17f0d3cc3605577
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810422
    > Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    > Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1033456}
    
    Bug: 1346254
    Change-Id: I29aa33b7af8a49d41aa8f7f481b18962a76de340
    Fixed: 1351737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3822910
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034388}

--- chrome/browser/ash/policy/dlp/dlp_files_controller.cc
+++ chrome/browser/ash/policy/dlp/dlp_files_controller.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ash/policy/dlp/dlp_files_controller.h"
 
 #include <sys/types.h>
+#include <iterator>
 #include <string>
 
 #include "base/bind.h"
@@ -14,9 +15,7 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/notreached.h"
-#include "base/task/task_traits.h"
-#include "base/task/thread_pool.h"
+#include "base/ranges/algorithm.h"
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
@@ -84,6 +83,18 @@ DlpFilesController::DlpFileMetadata::DlpFileMetadata(
     const std::string& source_url,
     bool is_dlp_restricted)
     : source_url(source_url), is_dlp_restricted(is_dlp_restricted) {}
+
+DlpFilesController::DlpFileRestrictionDetails::DlpFileRestrictionDetails() =
+    default;
+
+DlpFilesController::DlpFileRestrictionDetails::DlpFileRestrictionDetails(
+    DlpFileRestrictionDetails&&) = default;
+DlpFilesController::DlpFileRestrictionDetails&
+DlpFilesController::DlpFileRestrictionDetails::operator=(
+    DlpFilesController::DlpFileRestrictionDetails&&) = default;
+
+DlpFilesController::DlpFileRestrictionDetails::~DlpFileRestrictionDetails() =
+    default;
 
 DlpFilesController::DlpFilesController() = default;
 
@@ -213,16 +224,53 @@ void DlpFilesController::IsFilesTransferRestricted(
   std::move(result_callback).Run(std::move(restricted_files_sources));
 }
 
-std::map<std::string, std::set<std::string>>
+std::vector<DlpFilesController::DlpFileRestrictionDetails>
 DlpFilesController::GetDlpRestrictionDetails(const std::string& sourceUrl) {
   policy::DlpRulesManager* dlp_rules_manager =
       policy::DlpRulesManagerFactory::GetForPrimaryProfile();
   if (!dlp_rules_manager) {
     return {};
   }
-  // TODO(crbug.com/1346254): Call DlpRulesManager to get the restricted
-  // destinations and components; aggregate and convert to string format.
-  return {};
+
+  const GURL source(sourceUrl);
+  const DlpRulesManager::AggregatedDestinations aggregated_destinations =
+      dlp_rules_manager->GetAggregatedDestinations(
+          source, DlpRulesManager::Restriction::kFiles);
+  const DlpRulesManager::AggregatedComponents aggregated_components =
+      dlp_rules_manager->GetAggregatedComponents(
+          source, DlpRulesManager::Restriction::kFiles);
+
+  std::vector<DlpFilesController::DlpFileRestrictionDetails> result;
+  // Add levels for which urls are set.
+  for (const auto& [level, urls] : aggregated_destinations) {
+    DlpFileRestrictionDetails details;
+    details.level = level;
+    base::ranges::move(urls.begin(), urls.end(),
+                       std::back_inserter(details.urls));
+    // Add the components for this level, if any.
+    const auto it = aggregated_components.find(level);
+    if (it != aggregated_components.end()) {
+      base::ranges::move(it->second.begin(), it->second.end(),
+                         std::back_inserter(details.components));
+    }
+    result.emplace_back(std::move(details));
+  }
+
+  // There might be levels for which only components are set, so we need to add
+  // those separately.
+  for (const auto& [level, components] : aggregated_components) {
+    if (aggregated_destinations.find(level) != aggregated_destinations.end()) {
+      // Already added in the previous loop.
+      continue;
+    }
+    DlpFileRestrictionDetails details;
+    details.level = level;
+    base::ranges::move(components.begin(), components.end(),
+                       std::back_inserter(details.components));
+    result.emplace_back(std::move(details));
+  }
+
+  return result;
 }
 
 bool DlpFilesController::IsDlpPolicyMatched(const std::string& source_url) {
--- chrome/test/data/extensions/api_test/file_browser/dlp_metadata/test.js
+++ chrome/test/data/extensions/api_test/file_browser/dlp_metadata/test.js
@@ -118,6 +118,30 @@ chrome.test.getConfig(config => {
         }
       ]);
       break;
+    case 'restriction_details':
+      chrome.test.runTests([async function getDlpRestrictionDetails() {
+        chrome.fileManagerPrivate.getDlpRestrictionDetails(
+            'https://example1.com',
+            chrome.test.callbackPass(dlpRestrictionDetails => {
+              chrome.test.assertEq(
+                  [
+                    {
+                      'components': [
+                        'android_files', 'crostini', 'guest_os', 'removable'
+                      ],
+                      'level': 'block',
+                      'urls': ['https://external.com']
+                    },
+                    {
+                      'components': ['drive'],
+                      'level': 'allow',
+                      'urls': ['https://internal.com']
+                    }
+                  ],
+                  dlpRestrictionDetails);
+            }));
+      }]);
+      break;
     case 'default':
       chrome.test.runTests([
         async function getDlpMetadata() {
@@ -141,13 +165,6 @@ chrome.test.getConfig(config => {
                     ],
                     dlpMetadata);
               }))
-        },
-        async function getDlpRestrictionDetails() {
-          chrome.fileManagerPrivate.getDlpRestrictionDetails(
-              'https://example1.com',
-              chrome.test.callbackPass(dlpRestrictionDetails => {
-                chrome.test.assertEq([], dlpRestrictionDetails);
-              }));
         }
       ]);
       break;
