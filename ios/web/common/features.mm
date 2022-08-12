a9c1a6108155d6225aa6c6bcb7beaa0a4f3f0e88
ginnnnnnny <ginnyhuang@chromium.org>
2022-08-12 20:33:26 +0000

    Revert "[iOS][media-permissions] Enable media permissions by default"
    This reverts commit 20513c6143dcd864c9dfff18b069cff5ededac16.
    
    Reason for revert: Need to be disabled by default again to fix bug.
    
    Original change's description:
    > [iOS][media-permissions] Enable media permissions by default
    >
    > CL to clean up the respective base::Feature would come after.
    >
    > Bug: 1284228
    > Change-Id: Id5a35f3705bf569d6790fcb3eef4d292aaa955c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3764826
    > Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    > Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1024885}
    
    Bug: 1284228,1242633
    Change-Id: Ic84c785551f765929964f7ebaa2d28c65a5c357e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829787
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034677}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -50,7 +50,7 @@ const base::Feature kEnableFullscreenAPI{"EnableFullscreenAPI",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kMediaPermissionsControl{"MediaPermissionsControl",
-                                             base::FEATURE_ENABLED_BY_DEFAULT};
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kUseLoadSimulatedRequestForOfflinePage{
     "UseLoadSimulatedRequestForErrorPageNavigation",
