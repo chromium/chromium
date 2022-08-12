27beedd93ad5e34e8b4114ea0e674bfd34f2ab6b
Christian Dullweber <dullweber@chromium.org>
2022-08-12 13:59:02 +0000

    [M102-LTS] CookieControls: Avoid potential UAF by adding WeakPtr
    CookieControlsBubbleView might access an invalid controller_ pointer
    if the icon is destroyed before the bubble. Use a WeakPtr to ensure
    that the pointer is still valid before accessing it.
    
    (cherry picked from commit 09f521bc01d0e6d73de6a06abb9c5310cfb84754)
    
    Bug: 1343699
    Change-Id: I3cc7f6bf128b982df61077e19ecb481491f9d673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3762608
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1024222}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3816810
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Owners-Override: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/5005@{#1296}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- components/content_settings/browser/ui/cookie_controls_controller.h
+++ components/content_settings/browser/ui/cookie_controls_controller.h
@@ -7,6 +7,7 @@
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
 #include "components/content_settings/browser/page_specific_content_settings.h"
@@ -25,7 +26,9 @@ class CookieSettings;
 class CookieControlsView;
 
 // Handles the tab specific state for cookie controls.
-class CookieControlsController : content_settings::CookieSettings::Observer {
+class CookieControlsController
+    : content_settings::CookieSettings::Observer,
+      public base::SupportsWeakPtr<CookieControlsController> {
  public:
   CookieControlsController(
       scoped_refptr<content_settings::CookieSettings> cookie_settings,
