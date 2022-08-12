e8ba7f9f5d5166625e7c36b459308ef9b2d3da86
Joemer Ramos <joemerramos@chromium.org>
2022-08-12 14:56:58 +0000

    [iOS] Moving Tailored Security Outcome Enum To Components Directory
    The tailored security outcome enum should be used across platforms so that APIs across platforms communicate with each other through the same
    enums. This is pre-work before implementing Tailored Security Service
    on iOS.
    
    Bug: 1351160
    Change-Id: Id9d051e27bde1b2b4a133ad4e4c4a1a0470acbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3820026
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034466}

--- chrome/browser/ui/views/safe_browsing/tailored_security_desktop_dialog.cc
+++ chrome/browser/ui/views/safe_browsing/tailored_security_desktop_dialog.cc
@@ -9,12 +9,12 @@
 
 #include "base/callback.h"
 #include "base/metrics/histogram_functions.h"
-#include "chrome/browser/safe_browsing/tailored_security/tailored_security_outcome.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
+#include "components/safe_browsing/core/browser/tailored_security_service/tailored_security_outcome.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/l10n/l10n_util.h"
