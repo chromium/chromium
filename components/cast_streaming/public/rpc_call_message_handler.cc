92f5abb8ea47eed924f60d62fa17ecb27fa76907
Ryan Keane <rwkeane@google.com>
2022-08-13 00:36:17 +0000

    [Cast Streaming] Refactor Dispatching Methods
    This CL refactors the RpcMessageCallHandler method implementations to
    call into anonymous namespace methods instead of using large, difficult-
    to-read code blocks.
    
    Bug: b/209844961
    Change-Id: I945fe733fcce1be0cd7ed6b953d9a45a39631357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3805901
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1034759}

--- components/cast_streaming/public/rpc_call_message_handler.cc
+++ components/cast_streaming/public/rpc_call_message_handler.cc
@@ -49,6 +49,142 @@ absl::optional<media::VideoDecoderConfig> ExtractVideoConfig(
   return config;
 }
 
+bool DispatchInitializationAcquireRendererRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcInitializationCallMessageHandler* client) {
+  if (!message->has_integer_value()) {
+    LOG(ERROR)
+        << "RPC_ACQUIRE_RENDERER with no integer_value() property received";
+    return false;
+  }
+  client->OnRpcAcquireRenderer(message->integer_value());
+  return true;
+}
+
+bool DispatchInitializationAcquireDemuxerRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcInitializationCallMessageHandler* client) {
+  if (!message->has_acquire_demuxer_rpc()) {
+    LOG(ERROR) << "RPC_ACQUIRE_DEMUXER with no acquire_demuxer_rpc() "
+                  "property received";
+    return false;
+  }
+
+  const auto rpc = message->acquire_demuxer_rpc();
+  client->OnRpcAcquireDemuxer(rpc.audio_demuxer_handle(),
+                              rpc.video_demuxer_handle());
+  return true;
+}
+
+bool DispatchRendererFlushUntilRpcCall(openscreen::cast::RpcMessage* message,
+                                       RpcRendererCallMessageHandler* client) {
+  if (!message->has_renderer_flushuntil_rpc()) {
+    LOG(ERROR) << "RPC_R_FLUSHUNTIL with no renderer_flushuntil_rpc() "
+                  "property received";
+    return false;
+  }
+  const openscreen::cast::RendererFlushUntil flush_message =
+      message->renderer_flushuntil_rpc();
+  client->OnRpcFlush(flush_message.audio_count(), flush_message.video_count());
+  return true;
+}
+
+bool DispatchRendererStartPlayingFromRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcRendererCallMessageHandler* client) {
+  if (!message->has_integer64_value()) {
+    LOG(ERROR)
+        << "RPC_R_STARTPLAYINGFROM with no integer64_value() property received";
+    return false;
+  }
+  const base::TimeDelta time = base::Microseconds(message->integer64_value());
+  client->OnRpcStartPlayingFrom(time);
+  return true;
+}
+
+bool DispatchRendererSetPlaybackRateRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcRendererCallMessageHandler* client) {
+  if (!message->has_double_value()) {
+    LOG(ERROR)
+        << "RPC_R_SETPLAYBACKRATE with no double_value() property received";
+    return false;
+  }
+  client->OnRpcSetPlaybackRate(message->double_value());
+  return true;
+}
+
+bool DispatchRendererSetVolumeRpcCall(openscreen::cast::RpcMessage* message,
+                                      RpcRendererCallMessageHandler* client) {
+  if (!message->has_double_value()) {
+    LOG(ERROR) << "RPC_R_SETVOLUME with no double_value() property received";
+    return false;
+  }
+  client->OnRpcSetVolume(message->double_value());
+  return true;
+}
+
+bool DispatchDemuxerStreamInitializeCBRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcDemuxerStreamCBMessageHandler* client) {
+  if (!message->has_demuxerstream_initializecb_rpc()) {
+    LOG(ERROR) << "RPC_DS_INITIALIZE_CALLBACK with no "
+                  "demuxerstream_initializecb_rpc() property received";
+    return false;
+  }
+  const auto& callback_message = message->demuxerstream_initializecb_rpc();
+  client->OnRpcInitializeCallback(message->handle(),
+                                  ExtractAudioConfig(callback_message),
+                                  ExtractVideoConfig(callback_message));
+  return true;
+}
+
+bool DispatchDemuxerStreamReadUntilCBRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcDemuxerStreamCBMessageHandler* client) {
+  if (!message->has_demuxerstream_readuntilcb_rpc()) {
+    LOG(ERROR) << "RPC_DS_READUNTIL with no "
+                  "demuxerstream_readuntilcb_rpc() property received";
+    return false;
+  }
+  const auto& rpc_message = message->demuxerstream_readuntilcb_rpc();
+  auto audio_config = ExtractAudioConfig(rpc_message);
+  auto video_config = ExtractVideoConfig(rpc_message);
+
+  const auto status = ToDemuxerStreamStatus(rpc_message.status());
+  if ((audio_config || video_config) &&
+      status != media::DemuxerStream::kConfigChanged) {
+    LOG(ERROR) << "RPC_DS_READUNTIL with status != kConfigChanged contains "
+                  "a new config";
+    return false;
+  }
+
+  if (!audio_config && !video_config &&
+      status == media::DemuxerStream::kConfigChanged) {
+    LOG(ERROR) << "RPC_DS_READUNTIL with status = kConfigChanged contains "
+                  "no config";
+    return false;
+  }
+  const uint32_t total_frames_received = rpc_message.count();
+  client->OnRpcReadUntilCallback(message->handle(), std::move(audio_config),
+                                 std::move(video_config),
+                                 total_frames_received);
+  return true;
+}
+
+bool DispatchDemuxerStreamEnableBitstreamConverterCBRpcCall(
+    openscreen::cast::RpcMessage* message,
+    RpcDemuxerStreamCBMessageHandler* client) {
+  if (!message->has_boolean_value()) {
+    LOG(ERROR) << "RPC_DS_ENABLEBITSTREAMCONVERTER_CALLBACK with no "
+                  "boolean_value() property received";
+    return false;
+  }
+  client->OnRpcEnableBitstreamConverterCallback(message->handle(),
+                                                message->boolean_value());
+  return true;
+}
+
 }  // namespace
 
 RpcInitializationCallMessageHandler::~RpcInitializationCallMessageHandler() =
@@ -65,26 +201,10 @@ bool DispatchInitializationRpcCall(
   DCHECK(client);
 
   switch (message->proc()) {
-    case openscreen::cast::RpcMessage::RPC_ACQUIRE_RENDERER: {
-      if (!message->has_integer_value()) {
-        return false;
-      }
-      client->OnRpcAcquireRenderer(message->integer_value());
-      return true;
-    }
-    case openscreen::cast::RpcMessage::RPC_ACQUIRE_DEMUXER: {
-      if (!message->has_acquire_demuxer_rpc()) {
-        LOG(ERROR) << "RPC_ACQUIRE_DEMUXER with no acquire_demuxer_rpc() "
-                      "property received";
-        return false;
-      }
-      const int audio_stream_handle =
-          message->acquire_demuxer_rpc().audio_demuxer_handle();
-      const int video_stream_handle =
-          message->acquire_demuxer_rpc().video_demuxer_handle();
-      client->OnRpcAcquireDemuxer(audio_stream_handle, video_stream_handle);
-      return true;
-    }
+    case openscreen::cast::RpcMessage::RPC_ACQUIRE_RENDERER:
+      return DispatchInitializationAcquireRendererRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_ACQUIRE_DEMUXER:
+      return DispatchInitializationAcquireDemuxerRpcCall(message, client);
     default:
       return false;
   }
@@ -99,41 +219,14 @@ bool DispatchRendererRpcCall(openscreen::cast::RpcMessage* message,
     case openscreen::cast::RpcMessage::RPC_R_INITIALIZE:
       client->OnRpcInitialize();
       return true;
-    case openscreen::cast::RpcMessage::RPC_R_FLUSHUNTIL: {
-      if (!message->has_renderer_flushuntil_rpc()) {
-        LOG(ERROR) << "RPC_R_FLUSHUNTIL with no renderer_flushuntil_rpc() "
-                      "property received";
-        return false;
-      }
-      const openscreen::cast::RendererFlushUntil flush_message =
-          message->renderer_flushuntil_rpc();
-      client->OnRpcFlush(flush_message.audio_count(),
-                         flush_message.video_count());
-      return true;
-    }
-    case openscreen::cast::RpcMessage::RPC_R_STARTPLAYINGFROM: {
-      if (!message->has_integer64_value()) {
-        return false;
-      }
-      const base::TimeDelta time =
-          base::Microseconds(message->integer64_value());
-      client->OnRpcStartPlayingFrom(time);
-      return true;
-    }
-    case openscreen::cast::RpcMessage::RPC_R_SETPLAYBACKRATE: {
-      if (!message->has_double_value()) {
-        return false;
-      }
-      client->OnRpcSetPlaybackRate(message->double_value());
-      return true;
-    }
-    case openscreen::cast::RpcMessage::RPC_R_SETVOLUME: {
-      if (!message->has_double_value()) {
-        return false;
-      }
-      client->OnRpcSetVolume(message->double_value());
-      return true;
-    }
+    case openscreen::cast::RpcMessage::RPC_R_FLUSHUNTIL:
+      return DispatchRendererFlushUntilRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_R_STARTPLAYINGFROM:
+      return DispatchRendererStartPlayingFromRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_R_SETPLAYBACKRATE:
+      return DispatchRendererSetPlaybackRateRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_R_SETVOLUME:
+      return DispatchRendererSetVolumeRpcCall(message, client);
     default:
       return false;
   }
@@ -145,55 +238,13 @@ bool DispatchDemuxerStreamCBRpcCall(openscreen::cast::RpcMessage* message,
   DCHECK(client);
 
   switch (message->proc()) {
-    case openscreen::cast::RpcMessage::RPC_DS_INITIALIZE_CALLBACK: {
-      if (!message->has_demuxerstream_initializecb_rpc()) {
-        LOG(ERROR) << "RPC_DS_INITIALIZE_CALLBACK with no "
-                      "demuxerstream_initializecb_rpc() property received";
-        return false;
-      }
-      const auto& callback_message = message->demuxerstream_initializecb_rpc();
-      client->OnRpcInitializeCallback(message->handle(),
-                                      ExtractAudioConfig(callback_message),
-                                      ExtractVideoConfig(callback_message));
-      return true;
-    }
-    case openscreen::cast::RpcMessage::RPC_DS_READUNTIL_CALLBACK: {
-      if (!message->has_demuxerstream_readuntilcb_rpc()) {
-        LOG(ERROR) << "RPC_DS_READUNTIL with no "
-                      "demuxerstream_readuntilcb_rpc() property received";
-        return false;
-      }
-      const auto& rpc_message = message->demuxerstream_readuntilcb_rpc();
-      auto audio_config = ExtractAudioConfig(rpc_message);
-      auto video_config = ExtractVideoConfig(rpc_message);
-
-      const auto status = ToDemuxerStreamStatus(rpc_message.status());
-      if ((audio_config || video_config) &&
-          status != media::DemuxerStream::kConfigChanged) {
-        LOG(ERROR) << "RPC_DS_READUNTIL with status != kConfigChanged contains "
-                      "a new config";
-        return false;
-      } else if (!audio_config && !video_config &&
-                 status == media::DemuxerStream::kConfigChanged) {
-        LOG(ERROR) << "RPC_DS_READUNTIL with status = kConfigChanged contains "
-                      "no config";
-        return false;
-      }
-      const uint32_t total_frames_received = rpc_message.count();
-      client->OnRpcReadUntilCallback(message->handle(), std::move(audio_config),
-                                     std::move(video_config),
-                                     total_frames_received);
-      return true;
-    }
-    case openscreen::cast::RpcMessage::
-        RPC_DS_ENABLEBITSTREAMCONVERTER_CALLBACK: {
-      if (!message->has_boolean_value()) {
-        return false;
-      }
-      client->OnRpcEnableBitstreamConverterCallback(message->handle(),
-                                                    message->boolean_value());
-      return true;
-    }
+    case openscreen::cast::RpcMessage::RPC_DS_INITIALIZE_CALLBACK:
+      return DispatchDemuxerStreamInitializeCBRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_DS_READUNTIL_CALLBACK:
+      return DispatchDemuxerStreamReadUntilCBRpcCall(message, client);
+    case openscreen::cast::RpcMessage::RPC_DS_ENABLEBITSTREAMCONVERTER_CALLBACK:
+      return DispatchDemuxerStreamEnableBitstreamConverterCBRpcCall(message,
+                                                                    client);
     default:
       return false;
   }
