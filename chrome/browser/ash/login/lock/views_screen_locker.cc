a98abc08dac0d2ef90199d76e5ea9838f717f101
Elie Maamari <emaamari@google.com>
2022-08-13 10:30:14 +0000

    [M96-LTS] Sign out instead of crash if screen lock timeout reached
    M96 merge issues:
      ash/wm/lock_state_controller.cc:
        conflicting includes
    
      chrome/browser/ui/ash/session_controller_client_impl.cc:
        conflicting namespace on RequestLockScreen() call
    
    Additionally, add logs to better understand why the screen lock
    timeout was reached.
    
    Bug: b/228873153
    Change-Id: I1eede69e81797cae158571c45a6998c669f0b926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3758065
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1024207}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3782809
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Owners-Override: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/branch-heads/4664@{#1683}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}

--- chrome/browser/ash/login/lock/views_screen_locker.cc
+++ chrome/browser/ash/login/lock/views_screen_locker.cc
@@ -39,6 +39,10 @@
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "ui/base/ime/ash/ime_keyboard.h"
 
+// TODO(b/228873153): Remove after figuring out the root cause of the bug
+#undef ENABLED_VLOG_LEVEL
+#define ENABLED_VLOG_LEVEL 1
+
 namespace ash {
 
 ViewsScreenLocker::ViewsScreenLocker(ScreenLocker* screen_locker)
@@ -57,6 +61,7 @@ ViewsScreenLocker::~ViewsScreenLocker() {
 }
 
 void ViewsScreenLocker::Init() {
+  VLOG(1) << "b/228873153 : ViewsScreenLocker::Init()";
   lock_time_ = base::TimeTicks::Now();
   user_selection_screen_->Init(screen_locker_->GetUsersToShow());
 
