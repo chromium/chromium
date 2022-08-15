2e45a3a28095b331e14fa8d6b22526ff69bb9496
Fredrik Hernqvist <fhernqvist@google.com>
2022-08-15 08:12:12 +0000

    Change type of kIntervalBetweenVolumeUpdatesMs from int to TimeDelta
    Change-Id: Id1485386e91a5936074b1caf17b18a789eb07fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825929
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1034995}

--- media/audio/agc_audio_stream.h
+++ media/audio/agc_audio_stream.h
@@ -68,7 +68,8 @@ template <typename AudioInterface>
 class MEDIA_EXPORT AgcAudioStream : public AudioInterface {
  public:
   // Time between two successive timer events.
-  static const int kIntervalBetweenVolumeUpdatesMs = 1000;
+  static constexpr base::TimeDelta kIntervalBetweenVolumeUpdates =
+      base::Milliseconds(1000);
 
   AgcAudioStream()
       : agc_is_enabled_(false), max_volume_(0.0), normalized_volume_(0.0) {
@@ -103,8 +104,8 @@ class MEDIA_EXPORT AgcAudioStream : public AudioInterface {
     // volume from 0.
     QueryAndStoreNewMicrophoneVolume();
 
-    timer_.Start(FROM_HERE, base::Milliseconds(kIntervalBetweenVolumeUpdatesMs),
-                 this, &AgcAudioStream::QueryAndStoreNewMicrophoneVolume);
+    timer_.Start(FROM_HERE, kIntervalBetweenVolumeUpdates, this,
+                 &AgcAudioStream::QueryAndStoreNewMicrophoneVolume);
   }
 
   // Stops the periodic timer which periodically checks and updates the
