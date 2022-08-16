9e87dd323be4f012dbfcfb7c31ea0f278ba05e37
Jeevan Shikaram <jshikaram@chromium.org>
2022-08-16 04:53:31 +0000

    Revert "Add TRACE_EVENT Encode() function of MojoVEA and its clients"
    This reverts commit 4609f724a3677c3ead89de51a5c50501fdc1053e.
    
    Reason for revert: Causing compile failures in linux-perfetto-rel builder.
    
    Example failure: https://ci.chromium.org/ui/p/chromium/builders/try/linux-perfetto-rel/8378
    
    Original change's description:
    > Add TRACE_EVENT Encode() function of MojoVEA and its clients
    >
    > This CL adds TRACE_EVENT to MojoVEA::Encode() and its caller functions
    > so as to measure the pre-processing time like scaling and buffer
    > allocation for Encode().
    >
    > Bug: b:241362231
    > Test: chrome://tracing in Google Meet call
    > Change-Id: Ic3cfa624230b20466bd7b96f96907283ee1959e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3812821
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Reviewed-by: Ryan Keane <rwkeane@google.com>
    > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1035034}
    
    Bug: b:241362231
    Change-Id: If2e47912d0f36909b30092419ebb6b6a28212dc7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3833514
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Owners-Override: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1035366}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.cc
@@ -23,7 +23,6 @@
 #include "base/thread_annotations.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
-#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "media/base/bind_to_current_loop.h"
@@ -1315,7 +1314,6 @@ void RTCVideoEncoder::Impl::LogAndNotifyError(
 }
 
 void RTCVideoEncoder::Impl::EncodeOneFrame() {
-  TRACE_EVENT0("media,webrtc", "RTCVideoEncoder::Impl::EncodeOneFrame");
   DVLOG(3) << "Impl::EncodeOneFrame()";
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(input_next_frame_);
@@ -1359,8 +1357,6 @@ void RTCVideoEncoder::Impl::EncodeOneFrame() {
   }
 
   if (requires_copy_or_scale) {
-    TRACE_EVENT0("media,webrtc",
-                 "RTCVideoEncoder::Impl::EncodeOneFrame::CopyOrScale");
     const base::TimeDelta timestamp =
         frame ? frame->timestamp()
               : base::Milliseconds(next_frame->ntp_time_ms());
@@ -1466,8 +1462,6 @@ void RTCVideoEncoder::Impl::EncodeOneFrame() {
 }
 
 void RTCVideoEncoder::Impl::EncodeOneFrameWithNativeInput() {
-  TRACE_EVENT0("media,webrtc",
-               "RTCVideoEncoder::Impl::EncodeOneFrameWithNativeInput");
   DVLOG(3) << "Impl::EncodeOneFrameWithNativeInput()";
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(input_buffers_.IsEmpty() && input_buffers_free_.IsEmpty());
