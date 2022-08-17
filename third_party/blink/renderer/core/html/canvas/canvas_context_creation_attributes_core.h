e6af55af5bd8a79c6f6f174f295d30b7bba1c0fb
Aaron Krajeski <aaronhk@chromium.org>
2022-08-17 19:41:22 +0000

    Remove reference to non-existent flag
    Change-Id: I4293e69511505cbdac93e0beb59a8c2a7aff1075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831812
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036233}

--- third_party/blink/renderer/core/html/canvas/canvas_context_creation_attributes_core.h
+++ third_party/blink/renderer/core/html/canvas/canvas_context_creation_attributes_core.h
@@ -33,8 +33,7 @@ class CORE_EXPORT CanvasContextCreationAttributesCore {
   bool preserve_drawing_buffer = false;
   String power_preference = "default";
   bool stencil = false;
-  // Help to determine whether to use GPU or CPU for the canvas. It can only
-  // be set to true when the new-canvas-2d-api flag is enabled.
+  // Help to determine whether to use GPU or CPU for the canvas.
   bool will_read_frequently = false;
   bool xr_compatible = false;
 };
