f72dc7efbf9870b708bd577e5f765cdfe7b2b6cb
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-08-17 21:36:14 +0000

    [stts/android] Hide "Your devices" link if there's no target device
    Screenshot: crbug.com/1219434#c33
    This is the equivalent of crrev.com/c/3826260 for Android.
    Aaaand adjust the padding in
    send_tab_to_self_feature_unavailable_prompt.xml once again.
    
    Bug: 1219434
    Change-Id: Idd7f88577f614997939313763851fb6ef3abb72a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3827219
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036288}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/ManageAccountDevicesLinkView.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/ManageAccountDevicesLinkView.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.share.send_tab_to_self;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.Browser;
@@ -35,8 +36,18 @@ import org.chromium.ui.text.SpanApplier;
 public class ManageAccountDevicesLinkView extends LinearLayout {
     private static final int ACCOUNT_AVATAR_SIZE_DP = 24;
 
+    private final boolean mShowLink;
+
     public ManageAccountDevicesLinkView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(
+                attrs, R.styleable.ManageAccountDevicesLinkView, 0, 0);
+        try {
+            mShowLink =
+                    attributes.getBoolean(R.styleable.ManageAccountDevicesLinkView_showLink, false);
+        } finally {
+            attributes.recycle();
+        }
         inflateIfVisible();
     }
 
@@ -76,20 +87,24 @@ public class ManageAccountDevicesLinkView extends LinearLayout {
                     accountAvatarSizePx / 2, accountAvatarSizePx / 2);
         }
 
-        // The link is opened in a new tab to avoid exiting the current page, which the user
-        // possibly wants to share (maybe they just clicked "Manage devices" by mistake).
-        SpannableString linkText = SpanApplier.applySpans(
-                getResources().getString(
-                        R.string.send_tab_to_self_manage_devices_link, account.getEmail()),
-                new SpanApplier.SpanInfo("<link>", "</link>",
-                        new NoUnderlineClickableSpan(
-                                getContext(), this::openManageDevicesPageInNewTab)));
         TextView linkView = findViewById(R.id.manage_devices_link);
-        linkView.setText(linkText);
-        linkView.setMovementMethod(LinkMovementMethod.getInstance());
+        if (mShowLink) {
+            SpannableString linkText = SpanApplier.applySpans(
+                    getResources().getString(
+                            R.string.send_tab_to_self_manage_devices_link, account.getEmail()),
+                    new SpanApplier.SpanInfo("<link>", "</link>",
+                            new NoUnderlineClickableSpan(
+                                    getContext(), this::openManageDevicesPageInNewTab)));
+            linkView.setText(linkText);
+            linkView.setMovementMethod(LinkMovementMethod.getInstance());
+        } else {
+            linkView.setText(account.getEmail());
+        }
     }
 
     private void openManageDevicesPageInNewTab(View unused) {
+        // The link is opened in a new tab to avoid exiting the current page, which the user
+        // possibly wants to share (maybe they just clicked "Manage devices" by mistake).
         Intent intent =
                 new Intent()
                         .setAction(Intent.ACTION_VIEW)
