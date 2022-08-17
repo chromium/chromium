a3f3ec9df7b35f5e83b2061c3ec450ee08a12906
Elly Fong-Jones <ellyjones@chromium.org>
2022-08-17 22:44:43 +0000

    sheriff: disable slow iframe unload test under asan/tsan
    It's regularly timing out.
    
    Bug: 1096612
    Change-Id: I931e6b4d25a602603f001931360ed56ca7a85d7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3832533
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Owners-Override: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036329}

--- content/browser/site_per_process_unload_browsertest.cc
+++ content/browser/site_per_process_unload_browsertest.cc
@@ -888,10 +888,19 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest,
   shutdown_C.Wait();
 }
 
+#if defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER)
+// Too slow under sanitizers, even with increased timeout:
+// https://crbug.com/1096612
+#define MAYBE_DetachedIframeUnloadHandlerABCB \
+  DISABLED_DetachedIframeUnloadHandlerABCB
+#else
+#define MAYBE_DetachedIframeUnloadHandlerABCB DetachedIframeUnloadHandlerABCB
+#endif
+
 // When an iframe is detached, check that unload handlers execute in all of its
 // child frames. Start from A(B1(C(B2))) and delete B1 from A.
 IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest,
-                       DetachedIframeUnloadHandlerABCB) {
+                       MAYBE_DetachedIframeUnloadHandlerABCB) {
   // This test takes longer to run, because multiple processes are waiting on
   // each other's documents to execute unload handler before destroying their
   // documents. https://crbug.com/1311985
