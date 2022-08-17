8e096f65bd14d8ecdd5f4c18752e8d1906f7f891
Zengfeng Ye <zenye@microsoft.com>
2022-08-17 13:42:01 +0000

    [ios] Fix crash when segmentation platform feature disabled.
    `segmentation_otr_web_state_observer_` in ApplicationContextImpl can
    only be initialized when `kSegmentationPlatformFeature` is enabled.
    Although `kSegmentationPlatformFeature` is enabled by default currently,
    but we'd better add non-nullptr check for
    `segmentation_otr_web_state_observer_` in case this feature can appear
    in the disabled feature list.
    
    Bug: 1353619
    Change-Id: I9e6d4b314ea24d5be68d68282dd40f9479729724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831629
    Commit-Queue: Éric Noyau <noyau@chromium.org>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036024}

--- ios/chrome/browser/application_context_impl.mm
+++ ios/chrome/browser/application_context_impl.mm
@@ -171,8 +171,11 @@ void ApplicationContextImpl::StartTearDown() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
   // Destroy the segmentation OTR observer before
-  // `chrome_browser_state_manager_`.
-  segmentation_otr_web_state_observer_->TearDown();
+  // `chrome_browser_state_manager_`. `segmentation_otr_web_state_observer_` may
+  // not be initialized when segmentation platform feature is disabled
+  if (segmentation_otr_web_state_observer_) {
+    segmentation_otr_web_state_observer_->TearDown();
+  }
 
   // We need to destroy the MetricsServicesManager and NetworkTimeTracker before
   // the IO thread gets destroyed, since the destructor can call the URLFetcher
