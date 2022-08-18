8decf26b45d9134fb1ff049b6be6fe99345f80e8
Wez <wez@chromium.org>
2022-08-18 18:08:27 +0000

    Revert "[mojo] Disable flaking SharedBufferTest"
    This reverts commit b92a1d6a6af973c2cb271995cc7d80a3f19b3aa8.
    
    Reason for revert: AFAICT this has not actually been flaking recently, prior to being disabled.
    
    Original change's description:
    > [mojo] Disable flaking SharedBufferTest
    >
    > Bug: 1287637, 1342222
    > Bug: fuchsia:96241
    > Change-Id: I659fded6ee7aaffddfcd94958adc0d894923343e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3747786
    > Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    > Owners-Override: Adithya Srinivasan <adithyas@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Auto-Submit: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1021648}
    
    Bug: 1287637, 1342222
    Bug: fuchsia:96241
    Change-Id: I395d20485bf815c6ea2616b79386e24b199886a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3829561
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1036695}

--- mojo/core/shared_buffer_unittest.cc
+++ mojo/core/shared_buffer_unittest.cc
@@ -198,9 +198,7 @@ DEFINE_TEST_CLIENT_TEST_WITH_PIPE(ReceiveAndEditBufferParent,
 // Android multi-process tests are not executing the new process. This is flaky.
 #define MAYBE_PassHandleBetweenCousins DISABLED_PassHandleBetweenCousins
 #else
-// TODO(crbug.com/1287637): This test flakes frequently on various platforms,
-// with failure return-codes from RunTestClientAndGetExitCode().
-#define MAYBE_PassHandleBetweenCousins DISABLED_PassHandleBetweenCousins
+#define MAYBE_PassHandleBetweenCousins PassHandleBetweenCousins
 #endif
 TEST_F(SharedBufferTest, MAYBE_PassHandleBetweenCousins) {
   const std::string message = "hello";
