4d2ae5cd8aa0369264bd4a8ed9283a715b31036c
Gauthier Ambard <gambard@chromium.org>
2022-08-18 12:17:27 +0000

    [iOS] Speculative fix for TabGrid indicator
    It seems that the ActivityIndicator of the tab grid is shown and started
    but is only hidden.
    Try to also stop it to see if it reduces the flakes linked to the
    indicator still executed.
    
    Bug: 1352970
    Change-Id: I62d36e527135397fcc186eb310ed446da62d7408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3831543
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036557}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_cell.mm
@@ -289,6 +289,7 @@ void PositionView(UIView* view, CGPoint point) {
 }
 
 - (void)hideActivityIndicator {
+  [self.activityIndicator stopAnimating];
   [self.activityIndicator setHidden:YES];
   [self.iconView setHidden:NO];
 }
