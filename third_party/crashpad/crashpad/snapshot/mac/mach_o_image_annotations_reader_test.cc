bccbb72265bc366c6480ad3a0eb84716f35d8192
Leonard Grey <lgrey@chromium.org>
2022-08-18 22:41:44 +0000

    Mac: re-enable MachOImageAnnotationsReader tests
    These were fixed in the Crashpad roll this CL depends on.
    
    Bug: 844396, 1334418
    Change-Id: I930b441167c34ed2803a9e0c92aaa187bca72e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838842
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036827}

--- third_party/crashpad/crashpad/snapshot/mac/mach_o_image_annotations_reader_test.cc
+++ third_party/crashpad/crashpad/snapshot/mac/mach_o_image_annotations_reader_test.cc
@@ -470,16 +470,13 @@ TEST(MachOImageAnnotationsReader, CrashAbort) {
   test_mach_o_image_annotations_reader.Run();
 }
 
-// Flaky on ASAN https://crbug.com/844396
-// Flaky in general https://crbug.com/1334418
-TEST(MachOImageAnnotationsReader, DISABLED_CrashModuleInitialization) {
+TEST(MachOImageAnnotationsReader, CrashModuleInitialization) {
   TestMachOImageAnnotationsReader test_mach_o_image_annotations_reader(
       TestMachOImageAnnotationsReader::kCrashModuleInitialization);
   test_mach_o_image_annotations_reader.Run();
 }
 
-// Flaky in general https://crbug.com/1334418
-TEST(MachOImageAnnotationsReader, DISABLED_CrashDyld) {
+TEST(MachOImageAnnotationsReader, CrashDyld) {
   TestMachOImageAnnotationsReader test_mach_o_image_annotations_reader(
       TestMachOImageAnnotationsReader::kCrashDyld);
   test_mach_o_image_annotations_reader.Run();
