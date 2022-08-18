04469dca392b229c94fe2b26aa7d0a7742920c2b
Takashi Sakamoto <tasak@google.com>
2022-08-18 03:54:25 +0000

    Fix ChildProcessFieldTrialSyncerTest.FieldTrialState failure on backuprefptr fyi bots
    ChildProcessFieldTrialSyncerTest.FieldTrialState doesn't consider the
     case: --enable-features=TestFeature:TestParam/TestValue. If the
    parameter is given, StudyTestFeature.GroupTestFeature will be created
     and the test will always fail. So we need to use
    ScopedFeatureList::InitWithEmptyFeatureAndFieldTrialLists() to prepare
    empty feature and field trial lists.
    
    Change-Id: I9574e3076358462a342637ec4a60497b20daca5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3830742
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1036435}

--- components/variations/child_process_field_trial_syncer_unittest.cc
+++ components/variations/child_process_field_trial_syncer_unittest.cc
@@ -11,6 +11,7 @@
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -19,6 +20,12 @@ namespace variations {
 
 TEST(ChildProcessFieldTrialSyncerTest, FieldTrialState) {
   base::test::TaskEnvironment task_environment;
+  base::test::ScopedFeatureList scoped_feature_list;
+
+  // We expect there are no field trials, because we creates 3 field trials
+  // A, B and C, and checks EXPECT_EQ("*A/G1/B/G2/C/G3/", states_string).
+  // So we need to create a new scope with empty feature and field trial lists.
+  scoped_feature_list.InitWithEmptyFeatureAndFieldTrialLists();
 
   // We don't use the descriptor here anyways so it's ok to pass -1.
   base::FieldTrialList::CreateTrialsFromCommandLine(
