c4a306d0a88dc12f4a5e15e66004ac2c3a4cab79
Vikas Soni <vikassoni@chromium.org>
2022-08-19 18:33:23 +0000

    [Dr-Dc] Disable DrDc on some gpus .
    Disable DrDc on some gpus via gpudriverbugworkaround.
    
    (cherry picked from commit 1e7201436c8cd050c51d5a5fdf4c8e74e793a27f)
    
    Bug: 1354201
    Change-Id: Ia54ea2ae67bdd93d5085e81eca32b33433073eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3840470
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1036894}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842868
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5249@{#5}
    Cr-Branched-From: 4f7bea5de862aaa52e6bde5920755a9ef9db120b-refs/heads/main@{#1036826}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -1094,7 +1094,8 @@ RasterDecoderImpl::RasterDecoderImpl(
           features::kDisableFlushWorkaroundForMacCrash)),
 #endif
       is_raw_draw_enabled_(features::IsUsingRawDraw()),
-      is_drdc_enabled_(features::IsDrDcEnabled()) {
+      is_drdc_enabled_(features::IsDrDcEnabled() &&
+                       !feature_info()->workarounds().disable_drdc) {
   DCHECK(shared_context_state_);
   shared_context_state_->AddContextLostObserver(this);
 }
