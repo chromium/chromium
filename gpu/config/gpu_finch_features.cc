50a85fa470de5de5d9fca09cba6f1bedf84cf52c
Vikas Soni <vikassoni@chromium.org>
2022-08-19 17:40:00 +0000

    [Dr-Dc] Add more devices to be blocklisted.
    Add more devices to be blocklisted here in chrome code since adding it
    via finch is very slow.
    
    Bug: 1340054
    Change-Id: I3343d67ede012765fbfe36d6feb34a505d1a389e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842363
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037178}

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -279,7 +279,9 @@ const base::FeatureParam<std::string> kVulkanBlockListByAndroidBuildFP{
 // Blocklists meant for DrDc.
 // crbug.com/1294648
 const base::FeatureParam<std::string> kDrDcBlockListByDevice{
-    &kEnableDrDc, "BlockListByDevice", "LF9810_2GB"};
+    &kEnableDrDc, "BlockListByDevice",
+    "LF9810_2GB|amber|chopin|secret|a03|SO-51B|on7xelte|j7xelte|F41B|doha|"
+    "rk322x_box|a20s"};
 
 // crbug.com/1340059, crbug.com/1340064
 const base::FeatureParam<std::string> kDrDcBlockListByModel{
