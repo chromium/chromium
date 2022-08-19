91d5d6b2dd8725ef1a779e41af309f36b6f865bb
Hiroki Kumazaki <kumagi@google.com>
2022-08-19 10:36:16 +0000

    Do not create MF checker when Viewport or ViewportMeta disabled
    Evaluated MF results with Viewport disabled environments are purely noise and waste of resource.
    Let's disable it.
    
    Bug: 1350607
    Change-Id: I82c4e46887d276cf5d37d7cedbabf13c1201141b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841362
    Auto-Submit: Hiroki Kumazaki <kumagi@google.com>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Hiroki Kumazaki <kumagi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1037012}

--- third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.cc
+++ third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.cc
@@ -15,6 +15,7 @@
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/page_scale_constraints_set.h"
 #include "third_party/blink/renderer/core/frame/root_frame_viewport.h"
+#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/viewport_data.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
@@ -435,7 +436,9 @@ MobileFriendlinessChecker* MobileFriendlinessChecker::Create(
   // it's not useful to generate mobile friendliness metrics for
   // devtools, svg, etc.
   if (!frame_view.GetFrame().Client()->IsLocalFrameClientImpl() ||
-      !frame_view.GetFrame().IsOutermostMainFrame()) {
+      !frame_view.GetFrame().IsOutermostMainFrame() ||
+      !frame_view.GetPage()->GetSettings().GetViewportEnabled() ||
+      !frame_view.GetPage()->GetSettings().GetViewportMetaEnabled()) {
     return nullptr;
   }
   return MakeGarbageCollected<MobileFriendlinessChecker>(frame_view);
