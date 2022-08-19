ba02c41d2620bda68b12a42728225cacd0cd9b73
Avi Drissman <avi@chromium.org>
2022-08-19 20:19:32 +0000

    Warn when launching Chromium with Rosetta
    r1037181 added LSRequiresNativeExecution to the Info.plist so that
    Chromium can't be run under Rosetta for double-click launches.
    In instances where Chromium is run under Rosetta in different
    circumstances, it's likely accidental, so warn the user in a
    noticeable way so that they are likely to fix it.
    
    Fixed: 1305353
    Change-Id: Iedd81203f1a3524e734e7ddd7b0945678e4f558a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3841204
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037247}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -251,6 +251,11 @@
 #if BUILDFLAG(IS_MAC)
 #include <Security/Security.h>
 
+#if defined(ARCH_CPU_X86_64)
+#include "base/mac/mac_util.h"
+#include "base/threading/platform_thread.h"
+#endif  // defined(ARCH_CPU_X86_64)
+
 #include "chrome/browser/app_controller_mac.h"
 #include "chrome/browser/mac/keystone_glue.h"
 #include "chrome/browser/ui/ui_features.h"
@@ -1043,6 +1048,20 @@ int ChromeBrowserMainParts::PreCreateThreadsImpl() {
         // BUILDFLAG(IS_OPENBSD)
 
 #if BUILDFLAG(IS_MAC)
+#if defined(ARCH_CPU_X86_64)
+  // The use of Rosetta to run the x64 version of Chromium on Arm is neither
+  // tested nor maintained, and there are reports of it crashing in weird ways
+  // (e.g. https://crbug.com/1305353). Warn the user if this is the case, as
+  // it's almost certainly accidental on their part.
+  if (base::mac::GetCPUType() == base::mac::CPUType::kTranslatedIntel) {
+    LOG(ERROR) << "The use of Rosetta to run the x64 version of Chromium on "
+                  "Arm is neither tested nor maintained, and unexpected "
+                  "behavior will likely result. Please check that all tools "
+                  "that spawn Chromium are Arm-native.";
+    base::PlatformThread::Sleep(base::Seconds(3));
+  }
+#endif  // defined(ARCH_CPU_X86_64)
+
   // Get the Keychain API to register for distributed notifications on the main
   // thread, which has a proper CFRunloop, instead of later on the I/O thread,
   // which doesn't. This ensures those notifications will get delivered
