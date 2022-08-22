72a1299d0af842886908ae721ac266008cd92574
Avi Drissman <avi@chromium.org>
2022-08-22 19:47:44 +0000

    Loosen requirements for FaceDetectionImplMacTest.ScanOneFace
    This test is testing an OS service, so be more flexible and accept
    answers that are at least as good as we expect.
    
    Fixed: 1107962
    Change-Id: I702d7c71876866141ad55c9b5bcb054efbbc632a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842910
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037894}

--- services/shape_detection/face_detection_impl_mac_unittest.mm
+++ services/shape_detection/face_detection_impl_mac_unittest.mm
@@ -54,43 +54,26 @@ struct TestParams {
   int image_height;
   const char* image_path;
   size_t num_faces;
-  size_t num_landmarks;
-  size_t num_mouth_points;
+  size_t min_num_landmarks;
+  size_t min_num_mouth_points;
   FaceDetectorFactory factory;
 };
 
 std::vector<TestParams> GetTestParams() {
-  if (@available(macOS 10.14, *)) {
-    return {
-        {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 4, 10,
-         base::BindRepeating(&CreateFaceDetectorImplMacVision)},
-        {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {true, 240, 240, "services/test/data/the_beatles.jpg", 4, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 4, 10,
-         base::BindRepeating(&CreateFaceDetectorImplMacVision)},
-    };
-  } else {
-    return {
-        {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 4, 10,
-         base::BindRepeating(&CreateFaceDetectorImplMacVision)},
-        {false, 240, 240, "services/test/data/the_beatles.jpg", 3, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {true, 240, 240, "services/test/data/the_beatles.jpg", 3, 3, 1,
-         base::BindRepeating(&CreateFaceDetectorImplMac)},
-        {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 4, 10,
-         base::BindRepeating(&CreateFaceDetectorImplMacVision)},
-    };
-  }
+  return {
+      {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
+       base::BindRepeating(&CreateFaceDetectorImplMac)},
+      {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
+       base::BindRepeating(&CreateFaceDetectorImplMac)},
+      {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 4, 10,
+       base::BindRepeating(&CreateFaceDetectorImplMacVision)},
+      {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 3, 1,
+       base::BindRepeating(&CreateFaceDetectorImplMac)},
+      {true, 240, 240, "services/test/data/the_beatles.jpg", 4, 3, 1,
+       base::BindRepeating(&CreateFaceDetectorImplMac)},
+      {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 4, 10,
+       base::BindRepeating(&CreateFaceDetectorImplMacVision)},
+  };
 }
 
 }  // anonymous namespace
@@ -100,16 +83,19 @@ class FaceDetectionImplMacTest : public TestWithParam<struct TestParams> {
   ~FaceDetectionImplMacTest() override = default;
 
   void DetectCallback(size_t num_faces,
-                      size_t num_landmarks,
-                      size_t num_mouth_points,
+                      size_t min_num_landmarks,
+                      size_t min_num_mouth_points,
                       std::vector<mojom::FaceDetectionResultPtr> results) {
     EXPECT_EQ(num_faces, results.size());
     for (const auto& face : results) {
-      EXPECT_EQ(num_landmarks, face->landmarks.size());
+      // Relying on the OS for detection means evolving results as the OS
+      // improves. Do an "at least as many" check against landmarks and mouth
+      // points to adapt.
+      EXPECT_LE(min_num_landmarks, face->landmarks.size());
       EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[0]->type);
       EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[1]->type);
       EXPECT_EQ(mojom::LandmarkType::MOUTH, face->landmarks[2]->type);
-      EXPECT_EQ(num_mouth_points, face->landmarks[2]->locations.size());
+      EXPECT_LE(min_num_mouth_points, face->landmarks[2]->locations.size());
     }
     Detection();
   }
@@ -123,8 +109,7 @@ TEST_P(FaceDetectionImplMacTest, CreateAndDestroy) {
   impl_ = GetParam().factory.Run(mojom::FaceDetectorOptions::New());
 }
 
-// Flakily fails on multiple configurations. https://crbug.com/1107962
-TEST_P(FaceDetectionImplMacTest, DISABLED_ScanOneFace) {
+TEST_P(FaceDetectionImplMacTest, ScanOneFace) {
   // Face detection test needs a GPU.
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kUseGpuInTests)) {
@@ -158,11 +143,11 @@ TEST_P(FaceDetectionImplMacTest, DISABLED_ScanOneFace) {
   // Send the image to Detect() and expect the response in callback.
   EXPECT_CALL(*this, Detection())
       .WillOnce(RunOnceClosure(run_loop.QuitClosure()));
-  impl_->Detect(
-      *image,
-      base::BindOnce(&FaceDetectionImplMacTest::DetectCallback,
-                     base::Unretained(this), GetParam().num_faces,
-                     GetParam().num_landmarks, GetParam().num_mouth_points));
+  impl_->Detect(*image,
+                base::BindOnce(&FaceDetectionImplMacTest::DetectCallback,
+                               base::Unretained(this), GetParam().num_faces,
+                               GetParam().min_num_landmarks,
+                               GetParam().min_num_mouth_points));
 
   run_loop.Run();
 }
