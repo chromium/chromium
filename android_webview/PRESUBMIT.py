e13d3a743085febcad3a8a6247e14dbb281d181d
avvall <avvall@google.com>
2022-08-22 22:15:37 +0000

    WebView: Presubmit adhering to Chromium Python style guide
    At https://github.com/google/styleguide/blob/gh-pages/pyguide.md, developers for Chromium should adhere to this guide for Python development.
    On top of that, WebView should ignore all additions of
    Context#bindService in //android_webview/support_library/boundary_interfaces.
    
    Test: Please see associated bug for testing details.
    Bug: 1355500
    Change-Id: I0e27687cdb1ded7461431b704cfc2a424f0c6f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846041
    Commit-Queue: Adam Walls <avvall@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037963}

--- android_webview/PRESUBMIT.py
+++ android_webview/PRESUBMIT.py
@@ -11,36 +11,36 @@ USE_PYTHON3 = True
 
 def CheckChangeOnCommit(input_api, output_api):
   results = []
-  results.extend(_CheckNo_Context_bindService_Added(input_api, output_api))
+  results.extend(_CheckNoContextBindServiceAdded(input_api, output_api))
   return results
 
 def CheckChangeOnUpload(input_api, output_api):
   results = []
-  results.extend(_CheckNo_Context_bindService_Added(input_api, output_api))
+  results.extend(_CheckNoContextBindServiceAdded(input_api, output_api))
   return results
 
-
-def _CheckNo_Context_bindService_Added(input_api, output_api):
+def _CheckNoContextBindServiceAdded(input_api, output_api):
   """Checks that new no files under //android_webview directly use the
   Context#bindService. This is because Android platform disallows calling
   Context#bindService() from within a BroadcastReceiver context.
   """
   errors = []
-  run_with_pattern_part_api = input_api.re.compile(
+  bind_service_pattern = input_api.re.compile(
       r'.*\.bindService\(.*')
 
   def _FilterFile(affected_file):
-    skipFiles = (input_api.DEFAULT_FILES_TO_SKIP +
+    skip_files = (input_api.DEFAULT_FILES_TO_SKIP +
       (r'.*android_webview[/\\]common[/\\]services[/\\]ServiceHelper\.java',
+        r'.*android_webview[/\\]support_library[/\\]boundary_interfaces[/\\].*',
         r'.*android_webview[/\\]js_sandbox[/\\].*',))
     return input_api.FilterSourceFile(
         affected_file,
-        files_to_skip=skipFiles,
+        files_to_skip=skip_files,
         files_to_check=[r'.+\.java$'])
 
   for f in input_api.AffectedSourceFiles(_FilterFile):
     for line_num, line in f.ChangedContents():
-      match = run_with_pattern_part_api.search(line)
+      match = bind_service_pattern.search(line)
       if match:
         if "ServiceHelper.bindService" not in line:
           errors.append("%s:%d:%s" % (f.LocalPath(), line_num, line))
