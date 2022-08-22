dd552276abe6e146b6f891acfbcdbae559efc8fb
Peter Boström <pbos@chromium.org>
2022-08-22 01:24:46 +0000

    Update check_unittest.cc to use CHECK_WILL_STREAM
    This keeps check_unittest.cc's use of streams in sync with CHECK itself.
    
    Bug: 1354494
    Change-Id: I816deb87956266fdf03800ac5b2e6fe1385b1ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3843358
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037523}

--- base/check_unittest.cc
+++ base/check_unittest.cc
@@ -57,7 +57,7 @@ class ScopedCheckExpectation {
 // Macro which expects a CHECK to fire with a certain message. If msg starts
 // with "=~", it's interpreted as a regular expression.
 // Example: EXPECT_CHECK("Check failed: false.", CHECK(false));
-#if defined(OFFICIAL_BUILD) && defined(NDEBUG)
+#if !CHECK_WILL_STREAM()
 #define EXPECT_CHECK(msg, check_expr) \
   do {                                \
     EXPECT_CHECK_DEATH(check_expr);   \
@@ -68,7 +68,7 @@ class ScopedCheckExpectation {
     ScopedCheckExpectation check_exp(__FILE__, __LINE__, msg); \
     check_expr;                                                \
   } while (0)
-#endif
+#endif  // !CHECK_WILL_STREAM()
 
 // Macro which expects a DCHECK to fire if DCHECKs are enabled.
 #define EXPECT_DCHECK(msg, check_expr)                                         \
@@ -353,11 +353,11 @@ TEST_F(CheckTest, ConfigurableDCheckFeature) {
 struct StructWithOstream {
   bool operator==(const StructWithOstream& o) const { return &o == this; }
 };
-#if !(defined(OFFICIAL_BUILD) && defined(NDEBUG))
+#if CHECK_WILL_STREAM()
 std::ostream& operator<<(std::ostream& out, const StructWithOstream&) {
   return out << "ostream";
 }
-#endif
+#endif  // CHECK_WILL_STREAM()
 
 struct StructWithToString {
   bool operator==(const StructWithToString& o) const { return &o == this; }
@@ -370,12 +370,12 @@ struct StructWithToStringAndOstream {
   }
   std::string ToString() const { return "ToString"; }
 };
-#if !(defined(OFFICIAL_BUILD) && defined(NDEBUG))
+#if CHECK_WILL_STREAM()
 std::ostream& operator<<(std::ostream& out,
                          const StructWithToStringAndOstream&) {
   return out << "ostream";
 }
-#endif
+#endif  // CHECK_WILL_STREAM()
 
 struct StructWithToStringNotStdString {
   struct PseudoString {};
@@ -385,12 +385,12 @@ struct StructWithToStringNotStdString {
   }
   PseudoString ToString() const { return PseudoString(); }
 };
-#if !(defined(OFFICIAL_BUILD) && defined(NDEBUG))
+#if CHECK_WILL_STREAM()
 std::ostream& operator<<(std::ostream& out,
                          const StructWithToStringNotStdString::PseudoString&) {
   return out << "ToString+ostream";
 }
-#endif
+#endif  // CHECK_WILL_STREAM()
 
 TEST_F(CheckTest, OstreamVsToString) {
   StructWithOstream a, b;
