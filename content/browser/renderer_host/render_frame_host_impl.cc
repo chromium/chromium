013f414600b345905bc5b2b7850d86ebd914f113
Miyoung Shin <myid.shin@igalia.com>
2022-08-22 03:48:04 +0000

    [MPArch] Add ReportBadMessage in RFH::UpdateTargetURL
    This CL adds ReportBadMessage in RenderFrameHostImpl::UpdateTargetURL
    to kill the offending renderer if this message is called
    during prerendering from renderer since the renderer only calls
    it when the mouse over the URL or the keyboard focuses the URL.
    
    Bug: 1190019
    Change-Id: Id91b657cc667dc0cf940617702b1b23de4b91566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3807142
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1037550}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -5350,6 +5350,12 @@ void RenderFrameHostImpl::TakeFocus(bool reverse) {
 void RenderFrameHostImpl::UpdateTargetURL(
     const GURL& url,
     blink::mojom::LocalMainFrameHost::UpdateTargetURLCallback callback) {
+  // Prerendering pages should not reach this code since the renderer only calls
+  // this when the mouse over the URL or keyboard focuses the URL.
+  if (lifecycle_state_ == LifecycleStateImpl::kPrerendering) {
+    mojo::ReportBadMessage("Unexpected UpdateTargetURL from renderer");
+    return;
+  }
   delegate_->UpdateTargetURL(this, url);
   std::move(callback).Run();
 }
