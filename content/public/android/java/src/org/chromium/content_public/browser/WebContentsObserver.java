941dd3482b00a5de2c0b693b51cac946314562ab
Peilin Wang <peilinwang@google.com>
2022-08-22 23:50:19 +0000

    [M105 Merge][fieldtrial] Add config for NotifyJavaSpuriouslyToMeasurePerf
    (cherry picked from commit c08cb51af9c07af552f24654ce7349c9e0814172)
    
    Bug: 1351884, 1353380
    Change-Id: Id9d1e907dbabe1abf403f94c126fdbf539b3c0dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3828307
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1035066}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3844880
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/branch-heads/5195@{#820}
    Cr-Branched-From: 7aa3f074a7907975b001346cc0288d0214af8451-refs/heads/main@{#1027018}

--- content/public/android/java/src/org/chromium/content_public/browser/WebContentsObserver.java
+++ content/public/android/java/src/org/chromium/content_public/browser/WebContentsObserver.java
@@ -56,7 +56,7 @@ public abstract class WebContentsObserver {
     public void didStartNavigationInPrimaryMainFrame(NavigationHandle navigationHandle) {}
 
     /**
-     * TODO(crbug.com/1337446) Remove when NotifyJavaSupriouslyToMeasurePerf experiment is finished.
+     * TODO(crbug.com/1337446) Remove when NotifyJavaSpuriouslyToMeasurePerf experiment is finished.
      * No-op, for measuring performance of calling didStartNavigation in only the primary main
      * frame vs calling it in all frames.
      */
