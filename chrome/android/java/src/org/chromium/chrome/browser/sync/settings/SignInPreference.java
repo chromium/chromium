0955f0f218fbd7a02043afe4a2151a831835cc3a
Tanmoy Mollik <triploblastic@chromium.org>
2022-08-22 16:49:34 +0000

    Reland "Reland "Add Tangible sync flow for signin row in main settings""
    This reverts commit ff9407ba3cae86cfc2e59388a9e2d6c4282a7182.
    
    Reason for revert: The sign-in row is hidden when promos are shown if
    none of SYNC_ANDROID_PROMOS_WITH_ILLUSTRATION, SYNC_ANDROID_PROMOS_WITH_SINGLE_BUTTON or SYNC_ANDROID_PROMOS_WITH_TITLE is enabled.
    This cl uses one of these flags - SYNC_ANDROID_PROMOS_WITH_SINGLE_BUTTON - to fix the tests.
    
    Original change's description:
    > Revert "Reland "Add Tangible sync flow for signin row in main settings""
    >
    > This reverts commit df177914f44a95fafbd73b7ec8d5bca732160aa1.
    >
    > Reason for revert: Failing bots for another reason
    > https://bugs.chromium.org/p/chromium/issues/detail?id=1353505
    >
    > Original change's description:
    > > Reland "Add Tangible sync flow for signin row in main settings"
    > >
    > > This reverts commit 7935edc2e4d5a7d963df448334f565695f39a2c1.
    > >
    > > Reason for revert:
    > > Fixes the tests. Sync promo uses the same string as the signin
    > > preference when there are no accounts.
    > > In order to fix the tests an account is added to the device so that
    > > the promo has a different text.
    > >
    > > Original change's description:
    > > > Revert "Add Tangible sync flow for signin row in main settings"
    > > >
    > > > This reverts commit 4cee9379113bfc4c729d491143793be5192b49df.
    > > >
    > > > Reason for revert: newly added tests fail on bots (see
    > > > https://crbug.com/1353505).
    > > >
    > > > Original change's description:
    > > > > Add Tangible sync flow for signin row in main settings
    > > > >
    > > > > This cl adds the tangible sync flow launch capability to the signin
    > > > > preference in main settings page. This flow is guarded by a flag.
    > > > >
    > > > > Bug: 1332527
    > > > > Change-Id: I9222fa0512e1fcd55a012a9fa37853a9b66b0226
    > > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819763
    > > > > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > > > > Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    > > > > Cr-Commit-Position: refs/heads/main@{#1035435}
    > > >
    > > > Bug: 1332527, 1353505
    > > > Change-Id: I6e82abcd635da0999b0c7752c77d0ef08986f193
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834451
    > > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > > Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    > > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1035761}
    > >
    > > Bug: 1332527, 1353505
    > > Change-Id: I387ba7a5c31e28a8f82726af53ac68c76ced6487
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3834251
    > > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > > Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1036573}
    >
    > Bug: 1332527, 1353505
    > Change-Id: Icd2b88b2d67bf945edc006e09629633a75680541
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3838475
    > Commit-Queue: Lijin Shen <lazzzis@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Lijin Shen <lazzzis@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1036813}
    
    Bug: 1332527, 1353505
    Change-Id: I0ba69c3047a91f01ac508d277a1c30797cd9a97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845610
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1037803}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SignInPreference.java
@@ -13,6 +13,7 @@ import androidx.preference.PreferenceViewHolder;
 
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.SyncConsentActivityLauncherImpl;
@@ -22,6 +23,7 @@ import org.chromium.chrome.browser.signin.services.ProfileDataCache;
 import org.chromium.chrome.browser.signin.services.SigninManager.SignInStateObserver;
 import org.chromium.chrome.browser.sync.SyncService;
 import org.chromium.chrome.browser.sync.SyncService.SyncStateChangedListener;
+import org.chromium.chrome.browser.ui.signin.TangibleSyncCoordinator;
 import org.chromium.components.browser_ui.settings.ManagedPreferencesUtils;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.signin.AccountManagerFacade;
@@ -32,6 +34,7 @@ import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.base.ViewUtils;
+import org.chromium.ui.modaldialog.ModalDialogManagerHolder;
 
 /**
  * A preference that displays "Sign in to Chrome" when the user is not sign in, and displays
@@ -156,9 +159,18 @@ public class SignInPreference
         setFragment(null);
         setIcon(AppCompatResources.getDrawable(getContext(), R.drawable.logo_avatar_anonymous));
         setViewEnabled(true);
-        setOnPreferenceClickListener(pref
-                -> SyncConsentActivityLauncherImpl.get().launchActivityIfAllowed(
-                        getContext(), SigninAccessPoint.SETTINGS_SYNC_OFF_ROW));
+        setOnPreferenceClickListener(pref -> {
+            if (ChromeFeatureList.isEnabled(ChromeFeatureList.TANGIBLE_SYNC)) {
+                TangibleSyncCoordinator.start(getContext(),
+                        ((ModalDialogManagerHolder) getContext()).getModalDialogManager(),
+                        SyncConsentActivityLauncherImpl.get(),
+                        SigninAccessPoint.SETTINGS_SYNC_OFF_ROW);
+                return true;
+            } else {
+                return SyncConsentActivityLauncherImpl.get().launchActivityIfAllowed(
+                        getContext(), SigninAccessPoint.SETTINGS_SYNC_OFF_ROW);
+            }
+        });
 
         if (!mWasGenericSigninPromoDisplayed) {
             RecordUserAction.record("Signin_Impression_FromSettings");
