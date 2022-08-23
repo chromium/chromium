0490f4bec8b8bb147242ac343bf3b8a37a024977
Ahmed Fakhry <afakhry@chromium.org>
2022-08-23 23:48:14 +0000

    capture_mode: Fix a potential UAF
    This cannot be repro'ed locally, but it was possible
    to get the capture mode bar in a state in which it
    can be picked up as the selected window to be
    captured.
    
    This CL adds code to guard against this.
    
    Fixed: 1355752
    Change-Id: I291ea8747463d4d59fc932f6d9f8962f571835c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852825
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038476}

--- ash/capture_mode/capture_mode_util.cc
+++ ash/capture_mode/capture_mode_util.cc
@@ -419,6 +419,14 @@ aura::Window* GetTopMostCapturableWindowAtPoint(
 
   if (controller->IsActive()) {
     auto* capture_session = controller->capture_mode_session();
+    DCHECK(capture_session->capture_mode_bar_widget());
+    ignore_windows.insert(
+        capture_session->capture_mode_bar_widget()->GetNativeWindow());
+
+    if (auto* capture_settings_widget =
+            capture_session->capture_mode_settings_widget()) {
+      ignore_windows.insert(capture_settings_widget->GetNativeWindow());
+    }
 
     if (auto* capture_label_widget = capture_session->capture_label_widget())
       ignore_windows.insert(capture_label_widget->GetNativeWindow());
