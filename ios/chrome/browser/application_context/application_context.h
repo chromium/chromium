79e27be7ae840e073a6ba1abf746d1f941d5dd27
Gauthier Ambard <gambard@chromium.org>
2022-08-23 14:24:17 +0000

    [iOS] Prepare ApplicationContext to be moved
    This is a first CL for downstream code compatibility. The file will be
    moved later.
    
    Bug: 1354614
    Change-Id: I810ad7b5c7b209de2a15d5a76aade2474ef5f40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3842689
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038231}

---
+++ ios/chrome/browser/application_context/application_context.h
@@ -0,0 +1,13 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
+#define IOS_CHROME_BROWSER_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
+
+#import "ios/chrome/browser/application_context.h"
+
+// This file is a temporary fix for compilation. As of now it is just a
+// forwarded include.
+
+#endif  // IOS_CHROME_BROWSER_APPLICATION_CONTEXT_APPLICATION_CONTEXT_H_
