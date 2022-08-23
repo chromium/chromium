cfc5d7fb3b8a7bb8711d143d9231a942737ea166
Gauthier Ambard <gambard@chromium.org>
2022-08-23 16:41:12 +0000

    [iOS] Prepare the pref_names to be moved.
    The file will be moved later, after the downstream compatibility is
    done.
    
    Bug: 1354614
    Change-Id: Iaeb1888a476abf2423a9b168c5b7f95d08805dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3849015
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038281}

---
+++ ios/chrome/browser/prefs/pref_names.h
@@ -0,0 +1,11 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PREFS_PREF_NAMES_H_
+#define IOS_CHROME_BROWSER_PREFS_PREF_NAMES_H_
+
+// This is a temporary fix for downstream compatibility.
+#include "ios/chrome/browser/pref_names.h"
+
+#endif  // IOS_CHROME_BROWSER_PREFS_PREF_NAMES_H_
