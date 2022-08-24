85db8758bcda52d3d3823a6d4aa4891fb6c7f2d0
Justin Cohen <justincohen@google.com>
2022-08-24 00:10:03 +0000

    ios: Cleanup crash helper test fixture.
    Change-Id: Id1183e399ec24c82ab260ab6bd10f7e4506e05b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3819658
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1038490}

--- ios/chrome/browser/crash_report/crash_helper_unittest.mm
+++ ios/chrome/browser/crash_report/crash_helper_unittest.mm
@@ -4,20 +4,21 @@
 
 #import "ios/chrome/browser/crash_report/crash_helper.h"
 
-#include "base/strings/sys_string_conversions.h"
-#include "components/breadcrumbs/core/crash_reporter_breadcrumb_constants.h"
-#include "components/breadcrumbs/core/crash_reporter_breadcrumb_observer.h"
+#import "base/strings/sys_string_conversions.h"
+#import "base/test/task_environment.h"
+#import "components/breadcrumbs/core/crash_reporter_breadcrumb_constants.h"
+#import "components/breadcrumbs/core/crash_reporter_breadcrumb_observer.h"
 #import "ios/chrome/browser/crash_report/crash_keys_helper.h"
-#include "ios/chrome/browser/crash_report/crash_report_helper.h"
-#include "ios/chrome/browser/crash_report/main_thread_freeze_detector.h"
-#include "ios/chrome/common/crash_report/crash_helper.h"
+#import "ios/chrome/browser/crash_report/crash_report_helper.h"
+#import "ios/chrome/browser/crash_report/main_thread_freeze_detector.h"
+#import "ios/chrome/common/crash_report/crash_helper.h"
 #import "ios/chrome/test/ocmock/OCMockObject+BreakpadControllerTesting.h"
 #import "ios/testing/scoped_block_swizzler.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "testing/platform_test.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/platform_test.h"
 #import "third_party/breakpad/breakpad/src/client/ios/BreakpadController.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
-#include "third_party/ocmock/gtest_support.h"
+#import "third_party/ocmock/gtest_support.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -41,6 +42,7 @@ class BreakpadHelperTest : public PlatformTest {
     id implementation_block = ^BreakpadController*(id self) {
       return mock_breakpad_controller_;
     };
+    crash_helper::SyncCrashpadEnabledOnNextRun();
     breakpad_controller_shared_instance_swizzler_.reset(new ScopedBlockSwizzler(
         [BreakpadController class], @selector(sharedInstance),
         implementation_block));
@@ -55,6 +57,7 @@ class BreakpadHelperTest : public PlatformTest {
 
  protected:
   id mock_breakpad_controller_;
+  base::test::TaskEnvironment task_environment;
   std::unique_ptr<ScopedBlockSwizzler>
       breakpad_controller_shared_instance_swizzler_;
 };
