a9428ced551e5e4a2e28bc1af6bbeea31754bf92
mlcui <mlcui@google.com>
2022-08-24 07:38:42 +0000

    keyboard_shortcut_banner: Load HTML templates at runtime
    Use html_to_wrapper() instead of html_to_js() for web component files to
    aid in loading HTML templates at runtime. This is done so that the line
    numbers of reported errors match the checked-in code.
    
    Bug: 1292025
    Test: Manual UI testing
    Change-Id: I7715932d34fbae05e5a1e1a7fb462192c24a62a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3848954
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Auto-Submit: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1038627}

--- chrome/browser/resources/settings/chromeos/keyboard_shortcut_banner/keyboard_shortcut_banner.js
+++ chrome/browser/resources/settings/chromeos/keyboard_shortcut_banner/keyboard_shortcut_banner.js
@@ -16,7 +16,9 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
 import {getInstance as getAnnouncerInstance} from 'chrome://resources/cr_elements/cr_a11y_announcer/cr_a11y_announcer.js';
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
-import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './keyboard_shortcut_banner.html.js';
 
 /**
  * @constructor
@@ -33,7 +35,7 @@ class KeyboardShortcutBanner extends KeyboardShortcutBannerBase {
   }
 
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
