e5359afebc7595ca9811f78ee37e25b669e65187
Zachary Tan <tanzachary@chromium.org>
2022-08-25 21:44:35 +0000

    [FedCM] Fix heap-buffer-overflow in RequestToken
    Bug: 1356187
    Change-Id: I92592ffb9bf2d183db91583d1445c8777e2409c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856716
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039450}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -297,6 +297,18 @@ void FederatedAuthRequestImpl::RequestToken(
     RequestTokenCallback callback) {
   // TODO(crbug.com/1348262): Temporarily support only the first IDP, extend to
   // support multiple IDPs.
+  if (identity_provider_ptrs.empty()) {
+    std::move(callback).Run(RequestTokenStatus::kError, "");
+    return;
+  }
+
+  for (const auto& identity_provider : identity_provider_ptrs) {
+    if (!identity_provider) {
+      std::move(callback).Run(RequestTokenStatus::kError, "");
+      return;
+    }
+  }
+
   blink::mojom::IdentityProviderPtr identity_provider_ptr =
       std::move(identity_provider_ptrs[0]);
 
