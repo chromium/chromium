6485033cbf2c30c09efeab03c2cecfa8d185efc9
Anastasiia N <anastasiian@chromium.org>
2022-08-25 14:00:48 +0000

    Disable LensRegionSearchWithValidRegionUnifiedSidePanel
    Bug: 1356504
    Change-Id: Id9ce51e9e981bf41cf97a74f42864faab8d227e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856746
    Owners-Override: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039226}

--- chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
@@ -2045,8 +2045,16 @@ class SearchByRegionWithUnifiedSidePanelBrowserTest
   base::RepeatingClosure quit_closure_;
 };
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+// TODO(crbug.com/1356584): Failing on Chrome OS.
+#define MAYBE_LensRegionSearchWithValidRegionUnifiedSidePanel \
+  DISABLED_LensRegionSearchWithValidRegionUnifiedSidePanel
+#else
+#define MAYBE_LensRegionSearchWithValidRegionUnifiedSidePanel \
+  LensRegionSearchWithValidRegionUnifiedSidePanel
+#endif
 IN_PROC_BROWSER_TEST_F(SearchByRegionWithUnifiedSidePanelBrowserTest,
-                       LensRegionSearchWithValidRegionUnifiedSidePanel) {
+                       MAYBE_LensRegionSearchWithValidRegionUnifiedSidePanel) {
   lens::CreateLensUnifiedSidePanelEntryForTesting(browser());
   SetupAndLoadPage("/empty.html");
   // We need a base::RunLoop to ensure that our test does not finish until the
