ffb3bb94e2d87e2bcc77c0db6dc3f11bc448d74e
Sky Malice <skym@chromium.org>
2022-08-25 03:57:46 +0000

    Mitigate NPE on constraint change.
    The theory is that we are post-destroy, and the coordinator is
    actually null. It doesn't really matter if a capture happens, so be
    defensive in the suppression code. Hopefully the ToolbarManager
    destroy will be enough to stop this, but since we do not have a repro,
    it's difficult to be certain.
    
    (cherry picked from commit cc7df154739e97470c6543e3542006c4618a750f)
    
    Bug: 1356153
    Change-Id: Ie7a1aea020fe624191bc2f5c73732a620760552a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855120
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1039067}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3855521
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5260@{#2}
    Cr-Branched-From: 36307074a546485e40ad2295e546e3a681596689-refs/heads/main@{#1039004}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -371,6 +371,13 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
             }
         }
 
+        public void destroy() {
+            if (mCurrentConstraintDelegate != null) {
+                mCurrentConstraintDelegate.removeObserver(this);
+                mCurrentConstraintDelegate = null;
+            }
+        }
+
         @Override
         public void onResult(Integer result) {
             set(result);
@@ -1620,6 +1627,8 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
         mActivity.unregisterComponentCallbacks(mComponentCallbacks);
         mComponentCallbacks = null;
         ChromeAccessibilityUtil.get().removeObserver(this);
+
+        mConstraintsProxy.destroy();
     }
 
     /**
