43f640710abf43e2282bcbd099811ecfe0d4d503
L. David Baron <dbaron@chromium.org>
2022-08-26 23:02:41 +0000

    Implement the CSSToggleEvent interface.
    This updates the WPT copy (at interfaces/css-toggle.tentative.idl) of
    the toggle spec's IDL to the latest version, since it's relevant to this
    change.
    
    (Support for toggles is behind the CSSToggles flag, which is currently
    off.)
    
    Bug: 1250716
    Change-Id: I1978528d7608e12ba6afdf3031bb20f8aa3d4535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857190
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040014}

---
+++ third_party/blink/renderer/core/dom/css_toggle_event.cc
@@ -0,0 +1,27 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/dom/css_toggle_event.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/v8_css_toggle_event_init.h"
+#include "third_party/blink/renderer/core/dom/css_toggle.h"
+
+namespace blink {
+
+CSSToggleEvent::CSSToggleEvent(const AtomicString& type,
+                               const CSSToggleEventInit* init)
+    : Event(type, init) {
+  if (init) {
+    toggle_name_ = init->toggleName();
+    toggle_ = init->toggle();
+  }
+}
+
+void CSSToggleEvent::Trace(Visitor* visitor) const {
+  visitor->Trace(toggle_);
+
+  Event::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/core/dom/css_toggle_event.h
@@ -0,0 +1,39 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_CSS_TOGGLE_EVENT_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_CSS_TOGGLE_EVENT_H_
+
+#include "third_party/blink/renderer/core/dom/events/event.h"
+
+namespace blink {
+
+class CSSToggle;
+class CSSToggleEventInit;
+
+class CORE_EXPORT CSSToggleEvent : public Event {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static CSSToggleEvent* Create(
+      const AtomicString& type,
+      const CSSToggleEventInit* initializer = nullptr) {
+    return MakeGarbageCollected<CSSToggleEvent>(type, initializer);
+  }
+
+  CSSToggleEvent(const AtomicString&, const CSSToggleEventInit*);
+
+  String toggleName() const { return toggle_name_; }
+  CSSToggle* toggle() const { return toggle_; }
+
+  void Trace(Visitor*) const override;
+
+ private:
+  String toggle_name_;
+  Member<CSSToggle> toggle_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_DOM_CSS_TOGGLE_EVENT_H_
