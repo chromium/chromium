22608b7876f6a8d69a796d9260999ad01e126512
Joemer Ramos <joemerramos@chromium.org>
2022-08-26 21:36:45 +0000

    [iOS] Creating TailoredSecurityTabHelper
    Created a TailoredSecurityTabHelper so that we could attach this to a
    WebState and listen to events for message prompts. This is the
    equivalent of TailoredSecurityUrlObserver and should follow similar
    functionality.
    
    Bug: 1357020
    Change-Id: I843e9b3d38f03fafa4939fc2a31afb734477177e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3846296
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039982}

---
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper.mm
@@ -0,0 +1,140 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper.h"
+
+#import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.h"
+#import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service_observer_util.h"
+#import "components/signin/public/identity_manager/identity_manager.h"
+#import "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/signin/identity_manager_factory.h"
+#import "ios/web/public/navigation/navigation_context.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+#pragma mark - TailoredSecurityTabHelper
+
+// static
+void TailoredSecurityTabHelper::CreateForWebState(
+    web::WebState* web_state,
+    safe_browsing::TailoredSecurityService* service) {
+  if (FromWebState(web_state))
+    return;
+
+  web_state->SetUserData(
+      UserDataKey(),
+      std::make_unique<TailoredSecurityTabHelper>(web_state, service));
+}
+
+TailoredSecurityTabHelper::TailoredSecurityTabHelper(
+    web::WebState* web_state,
+    safe_browsing::TailoredSecurityService* service)
+    : service_(service), web_state_(web_state) {
+  bool focused = false;
+
+  if (service_) {
+    service_->AddObserver(this);
+  }
+
+  if (web_state) {
+    web_state->AddObserver(this);
+    focused = web_state->IsVisible();
+  }
+  UpdateFocusAndURL(focused, web_state->GetLastCommittedURL());
+}
+
+TailoredSecurityTabHelper::~TailoredSecurityTabHelper() {
+  if (service_) {
+    service_->RemoveObserver(this);
+    if (has_query_request_) {
+      service_->RemoveQueryRequest();
+      has_query_request_ = false;
+    }
+  }
+}
+
+WEB_STATE_USER_DATA_KEY_IMPL(TailoredSecurityTabHelper)
+
+#pragma mark - TailoredSecurityServiceObserver
+
+void TailoredSecurityTabHelper::OnTailoredSecurityBitChanged(
+    bool enabled,
+    base::Time previous_update) {
+  ChromeBrowserState* browser_state =
+      ChromeBrowserState::FromBrowserState(web_state()->GetBrowserState());
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForBrowserState(browser_state);
+  if (!enabled || !safe_browsing::CanShowUnconsentedTailoredSecurityDialog(
+                      identity_manager, browser_state->GetPrefs()))
+    return;
+
+  if (base::Time::NowFromSystemTime() - previous_update <=
+      base::Minutes(safe_browsing::kThresholdForInFlowNotificationMinutes)) {
+    // TODO(crbug.com/1353363): Send signal to show InfoBar.
+  }
+}
+
+void TailoredSecurityTabHelper::OnTailoredSecurityServiceDestroyed() {
+  service_->RemoveObserver(this);
+  service_ = nullptr;
+}
+
+#pragma mark - web::WebStateObserver
+void TailoredSecurityTabHelper::DidFinishNavigation(
+    web::WebState* web_state,
+    web::NavigationContext* navigation_context) {
+  bool sameDocumentNavigation = navigation_context->IsSameDocument();
+  if (!sameDocumentNavigation) {
+    UpdateFocusAndURL(true, navigation_context->GetUrl());
+  }
+};
+
+void TailoredSecurityTabHelper::WasShown(web::WebState* web_state) {
+  UpdateFocusAndURL(true, last_url_);
+};
+
+void TailoredSecurityTabHelper::WasHidden(web::WebState* web_state) {
+  UpdateFocusAndURL(false, last_url_);
+};
+
+void TailoredSecurityTabHelper::WebStateDestroyed(web::WebState* web_state) {
+  web_state->RemoveObserver(this);
+  web_state_ = nullptr;
+}
+
+#pragma mark - Private methods
+
+void TailoredSecurityTabHelper::UpdateFocusAndURL(bool focused,
+                                                  const GURL& url) {
+  DCHECK(web_state());
+  ChromeBrowserState* browser_state =
+      ChromeBrowserState::FromBrowserState(web_state()->GetBrowserState());
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForBrowserState(browser_state);
+
+  if (!safe_browsing::CanShowUnconsentedTailoredSecurityDialog(
+          identity_manager, browser_state->GetPrefs())) {
+    return;
+  }
+
+  if (service_) {
+    bool should_query =
+        focused && safe_browsing::CanQueryTailoredSecurityForUrl(url);
+    bool old_should_query =
+        focused_ && safe_browsing::CanQueryTailoredSecurityForUrl(last_url_);
+    if (should_query && !old_should_query) {
+      service_->AddQueryRequest();
+      has_query_request_ = true;
+    }
+    if (!should_query && old_should_query) {
+      service_->RemoveQueryRequest();
+      has_query_request_ = false;
+    }
+  }
+
+  focused_ = focused;
+  last_url_ = url;
+}
---
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper_unittest.mm
@@ -0,0 +1,102 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safe_browsing/tailored_security/tailored_security_tab_helper.h"
+
+#import "base/test/task_environment.h"
+#import "components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.h"
+#import "components/sync_preferences/pref_service_mock_factory.h"
+#import "components/sync_preferences/pref_service_syncable.h"
+#import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/prefs/browser_prefs.h"
+#import "ios/web/public/test/fakes/fake_navigation_context.h"
+#import "ios/web/public/test/fakes/fake_web_state.h"
+#import "services/network/public/cpp/shared_url_loader_factory.h"
+#import "testing/gmock/include/gmock/gmock.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace safe_browsing {
+
+namespace {
+
+// Mock class for TailoredSecurityService.
+class MockTailoredSecurityService : public TailoredSecurityService {
+ public:
+  MockTailoredSecurityService() : TailoredSecurityService(nullptr, nullptr) {}
+  MOCK_METHOD0(AddQueryRequest, void());
+  MOCK_METHOD0(RemoveQueryRequest, void());
+  MOCK_METHOD2(MaybeNotifySyncUser, void(bool, base::Time));
+  MOCK_METHOD0(GetURLLoaderFactory,
+               scoped_refptr<network::SharedURLLoaderFactory>());
+  MOCK_METHOD1(ShowSyncNotification, void(bool));
+};
+
+// Starts and finishes a mock navigation successfully.
+void PerformFakeNavigation(std::string url, web::FakeWebState* web_state) {
+  web::FakeNavigationContext context;
+  context.SetUrl(GURL(url));
+  context.SetHasCommitted(true);
+  context.SetIsSameDocument(false);
+  web_state->OnNavigationStarted(&context);
+  web_state->OnNavigationFinished(&context);
+}
+
+}  // namespace
+
+class TailoredSecurityTabHelperTest : public PlatformTest {
+ protected:
+  TailoredSecurityTabHelperTest() {
+    scoped_refptr<user_prefs::PrefRegistrySyncable> registry =
+        base::MakeRefCounted<user_prefs::PrefRegistrySyncable>();
+    RegisterBrowserStatePrefs(registry.get());
+    sync_preferences::PrefServiceMockFactory factory;
+
+    TestChromeBrowserState::Builder test_cbs_builder;
+    test_cbs_builder.SetPrefService(factory.CreateSyncable(registry.get()));
+    chrome_browser_state_ = test_cbs_builder.Build();
+    web_state_.SetBrowserState(chrome_browser_state_.get());
+  }
+
+  base::test::TaskEnvironment task_environment_;
+  std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
+  web::FakeWebState web_state_;
+};
+
+// Tests if query request is added when a WebState is shown and removing the
+// request when the WebState is hidden.
+TEST_F(TailoredSecurityTabHelperTest, QueryRequestOnFocus) {
+  MockTailoredSecurityService mock_service;
+  TailoredSecurityTabHelper::CreateForWebState(&web_state_, &mock_service);
+  TailoredSecurityTabHelper* tab_helper =
+      TailoredSecurityTabHelper::FromWebState(&web_state_);
+
+  EXPECT_CALL(mock_service, AddQueryRequest());
+  PerformFakeNavigation("https://google.com", &web_state_);
+
+  EXPECT_CALL(mock_service, RemoveQueryRequest());
+  tab_helper->WasHidden(nullptr);
+}
+
+// Tests how the tab helper responds to a mock navigation.
+TEST_F(TailoredSecurityTabHelperTest, QueryRequestOnNavigation) {
+  MockTailoredSecurityService mock_service;
+  TailoredSecurityTabHelper::CreateForWebState(&web_state_, &mock_service);
+  TailoredSecurityTabHelper* tab_helper =
+      TailoredSecurityTabHelper::FromWebState(&web_state_);
+
+  tab_helper->WasShown(nullptr);
+
+  EXPECT_CALL(mock_service, AddQueryRequest());
+  PerformFakeNavigation("https://google.com", &web_state_);
+
+  EXPECT_CALL(mock_service, RemoveQueryRequest());
+  PerformFakeNavigation("https://example.com", &web_state_);
+}
+
+}  // namespace safe_browsing
