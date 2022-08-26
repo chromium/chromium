466516bc1a8a708e08c19ac1bb3ca5fbc2a08f34
James Ye <jamesye@google.com>
2022-08-26 03:18:58 +0000

    exo: Use Crostini icon for notifications
    Replace the default Chrome icon with a Crostini/Linux icon to
    distinguish notifications from Crostini apps.
    
    Bug: b:242660232
    Test: Run notify-send <message> in Crostini
    Change-Id: I85c0612cb8664eb03eff5e11197a15fd001b013e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3853701
    Commit-Queue: James Ye <jamesye@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039567}

--- components/exo/notification.cc
+++ components/exo/notification.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "base/strings/utf_string_conversions.h"
@@ -75,6 +76,7 @@ Notification::Notification(
                             ->user_info.account_id.GetUserEmail();
 
   message_center::RichNotificationData data;
+  data.vector_small_image = &ash::kNotificationLinuxIcon;
   for (const auto& button : buttons)
     data.buttons.emplace_back(base::UTF8ToUTF16(button));
 
