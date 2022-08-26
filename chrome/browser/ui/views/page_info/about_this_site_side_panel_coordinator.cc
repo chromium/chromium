e445ca450ca50600a54a58e0a167358a1fdddb63
Christian Dullweber <dullweber@chromium.org>
2022-08-26 15:04:59 +0000

    AboutThisSite: Use info icon on Desktop
    Screenshot: https://crbug.com/1318000#c40
    Bug: 1318000
    Change-Id: I84c89b4ff8d153a770bfeb6fef6a581f1ce3d86a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3856743
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1039786}

--- chrome/browser/ui/views/page_info/about_this_site_side_panel_coordinator.cc
+++ chrome/browser/ui/views/page_info/about_this_site_side_panel_coordinator.cc
@@ -17,11 +17,11 @@
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
 #include "components/page_info/core/about_this_site_service.h"
 #include "components/strings/grit/components_strings.h"
-#include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/views/vector_icons.h"
 
 void ShowAboutThisSiteSidePanel(content::WebContents* web_contents,
                                 const content::OpenURLParams& params) {
@@ -65,8 +65,8 @@ void AboutThisSideSidePanelCoordinator::RegisterEntry(
     auto entry = std::make_unique<SidePanelEntry>(
         SidePanelEntry::Id::kAboutThisSite,
         l10n_util::GetStringUTF16(IDS_PAGE_INFO_ABOUT_THIS_PAGE_TITLE),
-        ui::ImageModel::FromVectorIcon(vector_icons::kGoogleColorIcon,
-                                       ui::kColorIcon, icon_size),
+        ui::ImageModel::FromVectorIcon(views::kInfoIcon, ui::kColorIcon,
+                                       icon_size),
         base::BindRepeating(
             &AboutThisSideSidePanelCoordinator::CreateAboutThisSiteWebView,
             base::Unretained(this)));
