ba13d1b3344a2f17b0176963595835b396492a54
Shakti Sahu <shaktisahu@chromium.org>
2022-08-27 03:02:58 +0000

    Added model for resume heavy users
    Bug: 1356739
    Change-Id: Ie625ea5cc216eaa6fdce0850e8dc564c0a750dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3857178
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040063}

---
+++ components/segmentation_platform/embedder/default_model/resume_heavy_user_model.cc
@@ -0,0 +1,93 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/segmentation_platform/embedder/default_model/resume_heavy_user_model.h"
+
+#include <array>
+
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "components/segmentation_platform/internal/metadata/metadata_writer.h"
+#include "components/segmentation_platform/public/model_provider.h"
+#include "components/segmentation_platform/public/proto/model_metadata.pb.h"
+
+namespace segmentation_platform {
+
+namespace {
+using proto::SegmentId;
+
+// Default parameters for the model.
+constexpr SegmentId kSegmentId = SegmentId::RESUME_HEAVY_USER_SEGMENT;
+
+// Discrete mapping parameters.
+constexpr char kDiscreteMappingKey[] = "resume_heavy_user";
+constexpr std::pair<float, int> kDiscreteMappings[] = {{1, 1}};
+
+// InputFeatures.
+constexpr std::array<MetadataWriter::UMAFeature, 5> kUMAFeatures = {
+    MetadataWriter::UMAFeature::FromUserAction("MobileBookmarkManagerOpen", 7),
+    MetadataWriter::UMAFeature::FromUserAction("NewTabPage.MostVisited.Clicked",
+                                               7),
+    MetadataWriter::UMAFeature::FromUserAction("TabGroup.Created.OpenInNewTab",
+                                               7),
+    MetadataWriter::UMAFeature::FromUserAction("Android.HistoryPage.OpenItem",
+                                               7),
+    MetadataWriter::UMAFeature::FromUserAction("MobileMenuRecentTabs", 7),
+};
+
+}  // namespace
+
+ResumeHeavyUserModel::ResumeHeavyUserModel() : ModelProvider(kSegmentId) {}
+
+void ResumeHeavyUserModel::InitAndFetchModel(
+    const ModelUpdatedCallback& model_updated_callback) {
+  proto::SegmentationModelMetadata metadata;
+  MetadataWriter writer(&metadata);
+  writer.SetSegmentationMetadataConfig(
+      proto::TimeUnit::DAY, /*bucket_duration=*/1, /*signal_storage_length=*/14,
+      /*min_signal_collection_length=*/7, /*result_time_to_live=*/3);
+
+  // Set discrete mapping.
+  writer.AddDiscreteMappingEntries(kDiscreteMappingKey, kDiscreteMappings, 1);
+
+  // Set features.
+  writer.AddUmaFeatures(kUMAFeatures.data(), kUMAFeatures.size());
+
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindRepeating(model_updated_callback, kSegmentId,
+                                     std::move(metadata), /*model_version=*/1));
+}
+
+void ResumeHeavyUserModel::ExecuteModelWithInput(
+    const std::vector<float>& inputs,
+    ExecutionCallback callback) {
+  // Invalid inputs.
+  if (inputs.size() != kUMAFeatures.size()) {
+    base::SequencedTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(std::move(callback), absl::nullopt));
+    return;
+  }
+
+  const int bookmarks_opened = inputs[0];
+  const int mv_tiles_clicked = inputs[1];
+  const int opened_ntp_from_tab_groups = inputs[2];
+  const int opened_item_from_history = inputs[3];
+  const int opened_recent_tabs = inputs[4];
+  float result = 0;
+
+  // Determine if the user has used chrome features to resume workflow.
+  if (bookmarks_opened >= 2 || mv_tiles_clicked >= 2 ||
+      opened_ntp_from_tab_groups >= 2 || opened_item_from_history >= 2 ||
+      opened_recent_tabs >= 2) {
+    result = 1;
+  }
+
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), result));
+}
+
+bool ResumeHeavyUserModel::ModelAvailable() {
+  return true;
+}
+
+}  // namespace segmentation_platform
---
+++ components/segmentation_platform/embedder/default_model/resume_heavy_user_model.h
@@ -0,0 +1,34 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SEGMENTATION_PLATFORM_EMBEDDER_DEFAULT_MODEL_RESUME_HEAVY_USER_MODEL_H_
+#define COMPONENTS_SEGMENTATION_PLATFORM_EMBEDDER_DEFAULT_MODEL_RESUME_HEAVY_USER_MODEL_H_
+
+#include "components/segmentation_platform/public/model_provider.h"
+
+namespace segmentation_platform {
+
+// Segmentation resume heavy user model provider. Provides a default model and
+// metadata for the resume heavy user segment.
+// TODO(shaktisahu): Find signals for desktop users.
+class ResumeHeavyUserModel : public ModelProvider {
+ public:
+  ResumeHeavyUserModel();
+  ~ResumeHeavyUserModel() override = default;
+
+  // Disallow copy/assign.
+  ResumeHeavyUserModel(ResumeHeavyUserModel&) = delete;
+  ResumeHeavyUserModel& operator=(ResumeHeavyUserModel&) = delete;
+
+  // ModelProvider implementation.
+  void InitAndFetchModel(
+      const ModelUpdatedCallback& model_updated_callback) override;
+  void ExecuteModelWithInput(const std::vector<float>& inputs,
+                             ExecutionCallback callback) override;
+  bool ModelAvailable() override;
+};
+
+}  // namespace segmentation_platform
+
+#endif  // COMPONENTS_SEGMENTATION_PLATFORM_EMBEDDER_DEFAULT_MODEL_RESUME_HEAVY_USER_MODEL_H_
