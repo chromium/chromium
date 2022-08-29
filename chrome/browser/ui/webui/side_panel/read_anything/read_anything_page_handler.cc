7788595bd51cc6e67c2c53bc2e35cd160d72a7ba
Abigail Klein <abigailbklein@google.com>
2022-08-29 20:26:59 +0000

    [Read Anything] Return early in destructor if coordinator is null.
    This fixes a crash.
    
    Bug: 1266555
    Change-Id: I78062a75d99d8a0f13cfbe93f99061a045927708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3852322
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040577}

--- chrome/browser/ui/webui/side_panel/read_anything/read_anything_page_handler.cc
+++ chrome/browser/ui/webui/side_panel/read_anything/read_anything_page_handler.cc
@@ -41,18 +41,19 @@ ReadAnythingPageHandler::ReadAnythingPageHandler(
 }
 
 ReadAnythingPageHandler::~ReadAnythingPageHandler() {
-  delegate_ = static_cast<ReadAnythingPageHandler::Delegate*>(
-      coordinator_->GetController());
-  if (delegate_)
-    delegate_->OnUIDestroyed();
+  if (!coordinator_)
+    return;
 
   // If |this| is destroyed before the |ReadAnythingCoordinator|, then remove
   // |this| from the observer lists. In the cases where the coordinator is
   // destroyed first, these will have been destroyed before this call.
-  if (coordinator_) {
-    coordinator_->RemoveObserver(this);
-    coordinator_->RemoveModelObserver(this);
-  }
+  coordinator_->RemoveObserver(this);
+  coordinator_->RemoveModelObserver(this);
+
+  delegate_ = static_cast<ReadAnythingPageHandler::Delegate*>(
+      coordinator_->GetController());
+  if (delegate_)
+    delegate_->OnUIDestroyed();
 }
 
 void ReadAnythingPageHandler::OnCoordinatorDestroyed() {
