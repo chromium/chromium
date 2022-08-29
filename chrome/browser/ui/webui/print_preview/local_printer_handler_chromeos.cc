704a8bc6cef236b15319e058eb43eea82250bca1
Daniel Cheng <dcheng@chromium.org>
2022-08-29 00:21:16 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //chrome/browser/ui/webui/print_preview.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=thestig@chromium.org
    
    Bug: 1356184
    Change-Id: I0a1168cfaabd4b91eaa8a51edbb84bd1d0f6481e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860848
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040221}

--- chrome/browser/ui/webui/print_preview/local_printer_handler_chromeos.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_chromeos.cc
@@ -17,6 +17,7 @@
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
+#include "base/types/optional_util.h"
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/webui/print_preview/print_preview_utils.h"
@@ -117,7 +118,7 @@ base::Value::Dict LocalPrinterHandlerChromeos::CapabilityToValue(
                                            ? kValueTrue
                                            : kValueFalse}}),
       PrinterSemanticCapsAndDefaults::Papers(), caps->has_secure_protocol,
-      base::OptionalOrNullptr(caps->capabilities));
+      base::OptionalToPtr(caps->capabilities));
 }
 
 // static
