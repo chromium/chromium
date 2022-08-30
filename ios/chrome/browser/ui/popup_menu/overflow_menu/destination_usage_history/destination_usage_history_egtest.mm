316e21d9cd6c46d042b6c7c01f8a24381639943a
Gauthier Ambard <gambard@chromium.org>
2022-08-30 15:53:35 +0000

    [iOS] Change pipes to backticks in /ios/chrome/browser/ui/popup_menu/overflow_menu
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=rkgibson@google.com
    
    Bug: 1311955
    Change-Id: I74063abf1a6e59103b7d707fd149b95a20d1b786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865529
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041003}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/destination_usage_history/destination_usage_history_egtest.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/destination_usage_history/destination_usage_history_egtest.mm
@@ -23,9 +23,9 @@
 
 namespace {
 // Unexpectedly, the first and last destinations in the carousel overlap their
-// neighbors. This makes |RightConstraint()| an insufficient layout constraint
+// neighbors. This makes `RightConstraint()` an insufficient layout constraint
 // for comparing destinations at the carousel's ends. A constraint with
-// negative minimum separation, |RightConstraintWithOverlap()|, must be
+// negative minimum separation, `RightConstraintWithOverlap()`, must be
 // introduced to account for this.
 GREYLayoutConstraint* RightConstraintWithOverlap() {
   return [GREYLayoutConstraint
@@ -72,7 +72,7 @@ GREYLayoutConstraint* RightConstraint() {
 // 7. Site Information
 // 8. Settings
 //
-// When |isNTP| is true, this method excludes the Site Information (#7)
+// When `isNTP` is true, this method excludes the Site Information (#7)
 // destination from the layout check, because Site Information is excluded from
 // the destinations carousel on the NTP.
 + (void)verifyCarouselHasDefaultSortOrderOnNTP:(BOOL)isNTP {
@@ -148,7 +148,7 @@ GREYLayoutConstraint* RightConstraint() {
 // 5. Site Information
 // 6. Settings
 //
-// When |isNTP| is true, this method excludes the Site Information (#5)
+// When `isNTP` is true, this method excludes the Site Information (#5)
 // destination from the layout check, because Site Information is excluded from
 // the destinations carousel on the NTP.
 + (void)verifyCarouselHasDefaultSortOrderOnNTPForIncognito:(BOOL)isNTP {
