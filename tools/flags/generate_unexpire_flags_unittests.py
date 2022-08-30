9686b2e7f8db5a351fa49de9b532fc6e1448fc84
Josip Sokcevic <sokcevic@google.com>
2022-08-30 21:44:39 +0000

    Update to the new OWNERS file parser
    Move from the deprecated OWNERS file parser to the new OWNERS file
    API client and move shared list_flags functions to utils.py.
    
    In addition, deduplicate owners, as there might be duplicates across
    the group of flag owners in the json file and those specified in the
    OWNERS file.
    
    This previously landed as r1035533. The difference to this patch is
    that it uses vpython3 for list_owners which satisfies the new dependencies
    found in owners_client, as well as sorting the list so that the output is
    consistent and the doctest can always pass. Python files that are invoked via
    ninja need not to use vpython3 and therefore can't depend on depot_tools owners
    anymore.
    
    R=avi@chromium.org
    
    Bug: 1313804
    Fixed: 1351519
    Change-Id: Ic3ee5ec1e537f3d91c4aeb69eb066a098548da99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854423
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041188}

--- tools/flags/generate_unexpire_flags_unittests.py
+++ tools/flags/generate_unexpire_flags_unittests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -12,10 +12,9 @@ class TestUnexpireGenerator(unittest.TestCase):
   TEST_MSTONE = 123
 
   def read_golden_file(self, extension):
-    return file(
-        os.path.join(
-            os.path.dirname(__file__),
-            'unexpire_test.' + extension + '.expected')).read()
+    return open(
+        os.path.join(os.path.dirname(__file__),
+                     'unexpire_test.' + extension + '.expected')).read()
 
   def testCcFile(self):
     cc = generate_unexpire_flags.gen_features_impl('foobar', 123)
