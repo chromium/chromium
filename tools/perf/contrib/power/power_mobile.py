29cbad113514e68fcbe6f5f24031b019614c224e
Tushar Agarwal <agarwaltushar@google.com>
2022-08-30 15:08:18 +0000

    Reduce redundancy in `power_mobile` benchmark by reusing code from `PerfBenchmarkWithProfiling`.
    Change-Id: I3ca081af21c4bb593d5e83ed26d599a686beb00d
    Bug: chromium:1338581
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865954
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040972}

--- tools/perf/contrib/power/power_mobile.py
+++ tools/perf/contrib/power/power_mobile.py
@@ -2,50 +2,28 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from core import perf_benchmark
 from core import platforms
 from contrib.power.top_sites_story import ContribPowerMobileTopSitesStorySet
+from contrib.power.perf_benchmark_with_profiling import PerfBenchmarkWithProfiling
 from telemetry import benchmark
 from telemetry import story
-from telemetry.timeline.chrome_trace_category_filter import ChromeTraceCategoryFilter
-from telemetry.web_perf import timeline_based_measurement
 
 
 @benchmark.Info(emails=['chrometto-team@google.com'])
-class ContribPowerMobile(perf_benchmark.PerfBenchmark):
+class ContribPowerMobile(PerfBenchmarkWithProfiling):
 
   SUPPORTED_PLATFORMS = [story.expectations.ALL_ANDROID]
   SUPPORTED_PLATFORM_TAGS = [platforms.ANDROID]
 
-  def __init__(self):
-    super(ContribPowerMobile, self).__init__()
-    self._browser_package = None
-
   def CreateStorySet(self, options):
+    del options  # unused
     return ContribPowerMobileTopSitesStorySet()
 
-  def CreateCoreTimelineBasedMeasurementOptions(self):
-    options = timeline_based_measurement.Options()
-    options.config.chrome_trace_config.SetCategoryFilter(
-        ChromeTraceCategoryFilter(''))
-    options.config.enable_experimental_system_tracing = True
-    options.config.system_trace_config.EnableProfiling(
-        '%s*' % self._browser_package, 300)
-    options.config.system_trace_config.EnableChrome(
-        chrome_trace_config=options.config.chrome_trace_config)
-    # options.config.system_trace_config.EnableProcessStats()
-    # options.config.system_trace_config.EnablePower()
-    # options.config.system_trace_config.EnableFtraceCpu()
-    options.config.system_trace_config.EnableFtraceSched()
-    return options
+  def GetSamplingFrequencyHz(self):
+    return 300
 
-  def CustomizeOptions(self, finder_options, possible_browser=None):
-    # We are not supposed to overwrite this method (see PerfBenchmark) but it is
-    # the only way to get to the possible_browser in time to set the parameters
-    # for stack sampling.
-    super(ContribPowerMobile, self).CustomizeOptions(finder_options,
-                                                     possible_browser)
-    self._browser_package = possible_browser.browser_package
+  def CustomizeSystemTraceConfig(self, system_trace_config):
+    system_trace_config.EnableFtraceSched()
 
   @classmethod
   def Name(cls):
