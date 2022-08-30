12a3e4c4483c66ce15720ec1566681e2cd8bd686
Bohdan Tyshchenko <bohdanty@google.com>
2022-08-30 07:41:54 +0000

    [OOBE] - Extend OOBE test API with the theme selection logic
    Add logic to query elements on the Theme Selection screen using the test
    api.
    
    Bug: b:242980448
    Change-Id: I2b65a75a6eb3d3fd4f5e095b9a55546994f21bab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3851217
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040830}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -325,6 +325,58 @@ class MarketingOptInScreenTester extends ScreenElementApi {
 class ThemeSelectionScreenTester extends ScreenElementApi {
   constructor() {
     super('theme-selection');
+    this.themeRadioButton = new PolymerElementApi(this, '#theme');
+    this.lightThemeButton = new PolymerElementApi(this, '#lightThemeButton');
+    this.darkThemeButton = new PolymerElementApi(this, '#darkThemeButton');
+    this.autoThemeButton = new PolymerElementApi(this, '#autoThemeButton');
+    this.textHeader = new PolymerElementApi(this, '#theme-selection-title');
+  }
+
+  /**
+   * Returns if the Theme Selection Screen is ready for test interaction.
+   * @return {boolean}
+   */
+  isReadyForTesting() {
+    return this.isVisible() && this.lightThemeButton.isVisible() &&
+        this.darkThemeButton.isVisible() && this.autoThemeButton.isVisible();
+  }
+
+  /**
+   * Presses light theme button to select it.
+   */
+  selectLightTheme() {
+    this.lightThemeButton.click();
+  }
+
+  /**
+   * Presses dark theme button to select it.
+   */
+  selectDarkTheme() {
+    this.darkThemeButton.click();
+  }
+
+  /**
+   * Presses auto theme button to select it.
+   */
+  selectAutoTheme() {
+    this.autoThemeButton.click();
+  }
+
+  /**
+   * Finds which theme is selected.
+   * @returns {string}
+   */
+  getNameOfSelectedTheme() {
+    return this.themeRadioButton.element().selected;
+  }
+
+  /**
+   * Retrieves computed color of the screen header. This value will be used to
+   * determine screen's color mode.
+   * @returns {string}
+   */
+  getHeaderTextColor() {
+    return window.getComputedStyle(this.textHeader.element()).color;
   }
 }
 
