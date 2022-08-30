9e6a38e9941058f837b3b25677a9166b9e5fb4de
Xiyuan Xia <xiyuan@google.com>
2022-08-30 21:48:12 +0000

    ash: Use DragImageAndTooltipContainer for security surfaces
    Use DragImageAndTooltipContainer show security surfaces on top of
    top-level windows, menus (web pop-ups), and bubbles.
    
    Bug: 1317904
    Change-Id: Iff942e5776623aac8c3398f45c65648ae2421290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863590
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041191}

--- ash/public/cpp/shell_window_ids.h
+++ ash/public/cpp/shell_window_ids.h
@@ -143,7 +143,8 @@ enum ShellWindowId {
   // The container for menus.
   kShellWindowId_MenuContainer,
 
-  // The container for drag/drop images and tooltips.
+  // The container for drag/drop images, tooltips, and widgets that are tagged
+  // with ui::ZOrderLevel::kSecuritySurface.
   kShellWindowId_DragImageAndTooltipContainer,
 
   // The container for the fullscreen power button menu.
