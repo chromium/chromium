799969a62786cd4a199990f4a8e3b893536db0e7
CJ Huang <chenjih@google.com>
2022-08-30 03:02:43 +0000

    Reland "[String matching] edit diacritics remove function"
    This CL is not associated with the test failure which originally prompted the revert. The contents of the original CL are not yet referenced elsewhere in the codebase.
    
    This is a reland of commit 1f5f55972a3644a66d10d64318e55d6a7533024f
    
    Original change's description:
    > [String matching] edit diacritics remove function
    >
    > the diacritic map construction is changed into compile-time constant to
    > save on repeated parsing. One char to two conversions are removed for
    > this implementation.
    >
    > Bug: 1347334
    > Change-Id: Id118dfd67c3c50aa7f64c55ccfbb702656bfe0d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3861648
    > Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    > Auto-Submit: CJ Huang <chenjih@google.com>
    > Commit-Queue: CJ Huang <chenjih@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1040263}
    
    Bug: 1347334
    Change-Id: I35dccfcf18c8b0d1fc2eb128dee77a901c6f379c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864004
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1040734}

--- chromeos/ash/components/string_matching/diacritic_utils.cc
+++ chromeos/ash/components/string_matching/diacritic_utils.cc
@@ -7,42 +7,55 @@
 #include <string>
 #include <vector>
 
-#include "base/containers/flat_map.h"
+#include "base/containers/fixed_flat_map.h"
 
 namespace ash::string_matching {
 
-// Intentionally only covering Latin-script accented characters for our initial
-// implementation of diacritic-insensitive search.
-DiacriticUtils::DiacriticUtils() {
-  std::vector<std::pair<std::u16string, std::u16string>>
-      diacritics_to_stripped = {
-          {u"áàâäāå", u"a"}, {u"ÁÀÂÄĀÅ", u"A"}, {u"éèêëē", u"e"},
-          {u"ÉÈÊËĒ", u"E"},  {u"íìîïī", u"i"},  {u"óòôöōø", u"o"},
-          {u"úùûüū", u"u"},  {u"ÚÙÛÜŪ", u"U"},  {u"ýỳŷÿȳ", u"y"},
-          {u"ÝỲŶŸȲ", u"Y"},  {u"ç", u"c"},      {u"Ç", u"C"},
-          {u"ñ", u"n"},      {u"Ñ", u"N"},      {u"æ", u"ae"},
-          {u"Æ", u"AE"},     {u"œ", u"oe"},     {u"Œ", u"OE"},
-      };
+const std::u16string RemoveDiacritics(const std::u16string& str) {
+  // For the initial implementation of diacritic-insensitive search:
+  // 1) Intentionally only covering Latin-script accented characters.
+  // 2) Only deal with 1-to-1 char mapping i.e., "æ > ae; œ > oe; Æ > AE; Œ >
+  // OE" are ignored in this implementation. The implemented ones are listed
+  // as below:
 
-  for (const auto& mapping_pair : diacritics_to_stripped) {
-    for (const auto& diacritic : mapping_pair.first) {
-      conversion_map_[diacritic] = mapping_pair.second;
-    }
-  }
-}
+  // "[ á à â ä ā å ] > a; "
+  // "[ Á À Â Ä Ā Å ] > A; "
+  // "[ é è ê ë ē   ] > e; "
+  // "[ É È Ê Ë Ē   ] > E; "
+  // "[ í ì î ï ī   ] > i; "
+  // "[ Í Ì Î Ï Ī   ] > I; "
+  // "[ ó ò ô ö ō ø ] > o; "
+  // "[ Ó Ò Ô Ö Ō Ø ] > O; "
+  // "[ ú ù û ü ū   ] > u; "
+  // "[ Ú Ù Û Ü Ū   ] > U; "
+  // "[ ý ỳ ŷ ÿ ȳ   ] > y; "
+  // "[ Ý Ỳ Ŷ Ÿ Ȳ   ] > Y; "
+  // "ç > c; ñ > n; "
+  // "Ç > C; Ñ > N;"
 
-DiacriticUtils::~DiacriticUtils() = default;
+  // clang-format off
+  static constexpr auto kConversionMap =
+    base::MakeFixedFlatMap<char16_t, char16_t>({
+      {u'á', u'a'}, {u'à', u'a'}, {u'â', u'a'}, {u'ä', u'a'}, {u'ā', u'a'}, {u'å', u'a'},
+      {u'Á', u'A'}, {u'À', u'A'}, {u'Â', u'A'}, {u'Ä', u'A'}, {u'Ā', u'A'}, {u'Å', u'A'},
+      {u'é', u'e'}, {u'è', u'e'}, {u'ê', u'e'}, {u'ë', u'e'}, {u'ē', u'e'},
+      {u'É', u'E'}, {u'È', u'E'}, {u'Ê', u'E'}, {u'Ë', u'E'}, {u'Ē', u'E'},
+      {u'í', u'i'}, {u'ì', u'i'}, {u'î', u'i'}, {u'ï', u'i'}, {u'ī', u'i'},
+      {u'Í', u'I'}, {u'Ì', u'I'}, {u'Î', u'I'}, {u'Ï', u'I'}, {u'Ī', u'I'},
+      {u'ó', u'o'}, {u'ò', u'o'}, {u'ô', u'o'}, {u'ö', u'o'}, {u'ō', u'o'}, {u'ø', u'o'},
+      {u'Ó', u'O'}, {u'Ò', u'O'}, {u'Ô', u'O'}, {u'Ö', u'O'}, {u'Ō', u'O'}, {u'Ø', u'O'},
+      {u'ú', u'u'}, {u'ù', u'u'}, {u'û', u'u'}, {u'ü', u'u'}, {u'ū', u'u'},
+      {u'Ú', u'U'}, {u'Ù', u'U'}, {u'Û', u'U'}, {u'Ü', u'U'}, {u'Ū', u'U'},
+      {u'ý', u'y'}, {u'ỳ', u'y'}, {u'ŷ', u'y'}, {u'ÿ', u'y'}, {u'ȳ', u'y'},
+      {u'Ý', u'Y'}, {u'Ỳ', u'Y'}, {u'Ŷ', u'Y'}, {u'Ÿ', u'Y'}, {u'Ȳ', u'Y'},
+      {u'ç', u'c'}, {u'Ç', u'C'}, {u'ñ', u'n'}, {u'Ñ', u'N'},
+      });
+  // clang-format on
 
-const std::u16string DiacriticUtils::RemoveDiacritics(
-    const std::u16string& str) {
   std::u16string result;
   for (auto letter : str) {
-    auto it = conversion_map_.find(letter);
-    if (it == conversion_map_.end()) {
-      result.push_back(letter);
-    } else {
-      result.append(it->second);
-    }
+    auto* it = kConversionMap.find(letter);
+    result.push_back(it == kConversionMap.end() ? letter : it->second);
   }
   return result;
 }
