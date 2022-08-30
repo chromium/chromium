e4dd21469dac3526a608c949533bd874e8359e28
Brando <brandosocarras@google.com>
2022-08-30 03:09:40 +0000

    [Nearby Share] FileAttachment unit tests for overridden Attachment functions
    Unit tests include testing all possible FileType parameters and testing that, when moved to ShareTarget, FileAttachment data is the same.
    
    TEST=this
    
    Fixed: b/232970430
    Change-Id: I9946397be44e0709924c9812e5f63e8c34343145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854512
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1040746}

--- chrome/browser/nearby_sharing/file_attachment_unittest.cc
+++ chrome/browser/nearby_sharing/file_attachment_unittest.cc
@@ -3,28 +3,80 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/nearby_sharing/file_attachment.h"
+#include "chrome/browser/nearby_sharing/share_target.h"
 
 #include "base/files/file_path.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
-const int64_t kFileSize = 2048;
-const char kFileName[] = "secure_file_name.html";
-base::FilePath kFilePath = base::FilePath();
-const char kMimeType[] = "text/html";
-FileAttachment::Type kMetadataType =
-    sharing::mojom::FileMetadata::Type::kUnknown;
+base::FilePath kFilePath = base::FilePath("test.html");
+
+struct FileShareType {
+  std::string file_name;
+  nearby_share::mojom::ShareType share_type;
+} kFileTypes[] = {
+  {"test.jpg", nearby_share::mojom::ShareType::kImageFile},
+  {"test.mp4", nearby_share::mojom::ShareType::kVideoFile},
+  {"test.wav", nearby_share::mojom::ShareType::kAudioFile},
+  {"test.pdf", nearby_share::mojom::ShareType::kPdfFile},
+  {"test.other", nearby_share::mojom::ShareType::kUnknownFile}
+};
+
+struct GoogleAppsFileShareType {
+  std::string file_name;
+  std::string mime_type;
+  nearby_share::mojom::ShareType share_type;
+} kGoogleAppsFileTypes[] = {
+  {"test.gdoc", "application/vnd.google-apps.document", nearby_share::mojom::ShareType::kGoogleDocsFile},
+  {"test.gsheet", "application/vnd.google-apps.spreadsheet", nearby_share::mojom::ShareType::kGoogleSheetsFile},
+  {"test.gslides", "application/vnd.google-apps.presentation", nearby_share::mojom::ShareType::kGoogleSlidesFile}
+};
+
 }  // namespace
 
-TEST(FileAttachmentTest, FileAttachmentTest) {
-  FileAttachment attachment(/*id=*/0, kFileSize, kFileName, kMimeType,
-                            kMetadataType);
+class FileAttachmentShareTypeTest : public testing::TestWithParam<FileShareType> { };
 
-  EXPECT_EQ(attachment.size(), kFileSize);
-  EXPECT_EQ(attachment.file_name(), kFileName);
-  EXPECT_EQ(attachment.mime_type(), kMimeType);
-  EXPECT_EQ(attachment.type(), kMetadataType);
+class FileAttachmentGoogleAppsShareTypeTest : public testing::TestWithParam<GoogleAppsFileShareType> { };
 
-  attachment.set_file_path(kFilePath);
-  EXPECT_EQ(attachment.file_path(), kFilePath);
+TEST(FileAttachmentTest, CreateFileAttachment) {
+    FileAttachment attachment = FileAttachment(kFilePath);
+
+    EXPECT_EQ(attachment.size(), 0u);
+    EXPECT_EQ(attachment.file_name(), "test.html");
+    EXPECT_EQ(attachment.mime_type(), "text/html");
+    EXPECT_EQ(attachment.type(), FileAttachment::Type::kUnknown);
+    EXPECT_EQ(attachment.file_path(), kFilePath);
+    EXPECT_EQ(attachment.GetDescription(), "test.html");
 }
+
+TEST(FileAttachmentTest, MoveShareTarget) {
+  FileAttachment attachment = FileAttachment(kFilePath);
+  ShareTarget target;
+  EXPECT_EQ(target.file_attachments.size(), 0u);
+  attachment.MoveToShareTarget(target);
+  EXPECT_EQ(target.file_attachments.size(), 1u);
+}
+
+TEST_P(FileAttachmentShareTypeTest, GetShareType) {
+  FileShareType fileTypePair = GetParam();
+  FileAttachment attachment = FileAttachment(base::FilePath(fileTypePair.file_name));
+  EXPECT_EQ(attachment.GetShareType(), fileTypePair.share_type);
+}
+
+INSTANTIATE_TEST_SUITE_P(FileAttachmentTest, FileAttachmentShareTypeTest,
+                         testing::ValuesIn(kFileTypes));
+
+TEST_P(FileAttachmentGoogleAppsShareTypeTest, GetShareType) {
+  GoogleAppsFileShareType fileType = GetParam();
+
+  FileAttachment attachment = FileAttachment(/*id=*/ 0u,
+                                             /*size=*/ 0u,
+                                             fileType.file_name,
+                                             fileType.mime_type,
+                                             FileAttachment::Type::kUnknown);
+
+  EXPECT_EQ(attachment.GetShareType(), fileType.share_type);
+}
+
+INSTANTIATE_TEST_SUITE_P(FileAttachmentTest, FileAttachmentGoogleAppsShareTypeTest,
+                         testing::ValuesIn(kGoogleAppsFileTypes));
