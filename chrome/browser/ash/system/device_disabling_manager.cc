44f7027a58edee3cdd77046f606981f120abb502
Roland Bock <rbock@google.com>
2022-08-31 20:44:04 +0000

    Refactor ash/system to use PrefService::GetValueDict
    This is part of a larger migration effort based on
    go/trust-your-prefs.
    
    Bug: 1341996
    Test: Refactored unit tests
    Change-Id: Ia6469722dd1436b0179bd23216820e77a3954ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3825965
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1041719}

--- chrome/browser/ash/system/device_disabling_manager.cc
+++ chrome/browser/ash/system/device_disabling_manager.cc
@@ -128,8 +128,8 @@ void DeviceDisablingManager::CheckWhetherDeviceDisabledDuringOOBE(
   enrollment_domain_.clear();
   const std::string* maybe_enrollment_domain =
       g_browser_process->local_state()
-          ->GetDictionary(prefs::kServerBackedDeviceState)
-          ->FindStringKey(policy::kDeviceStateManagementDomain);
+          ->GetValueDict(prefs::kServerBackedDeviceState)
+          .FindString(policy::kDeviceStateManagementDomain);
   enrollment_domain_ =
       maybe_enrollment_domain ? *maybe_enrollment_domain : std::string();
 
@@ -140,8 +140,8 @@ void DeviceDisablingManager::CheckWhetherDeviceDisabledDuringOOBE(
   // Update the disabled message.
   const std::string* maybe_disabled_message =
       g_browser_process->local_state()
-          ->GetDictionary(prefs::kServerBackedDeviceState)
-          ->FindStringKey(policy::kDeviceStateDisabledMessage);
+          ->GetValueDict(prefs::kServerBackedDeviceState)
+          .FindString(policy::kDeviceStateDisabledMessage);
   std::string disabled_message =
       maybe_disabled_message ? *maybe_disabled_message : std::string();
   CacheDisabledMessageAndNotify(disabled_message);
