80873caf1d739697f3982ff21653e68deca5f3ec
Bowen Li <lbowen@google.com>
2022-08-31 22:08:34 +0000

    Create metrics wrapper scaffolding code for demo SWA migration
    Created the wrapper files to add demo-mode-app metrics to UMA.
    Tested with demo-mode-app component to trigger the function.
    
    Doc: go/demo-mode-uma-migration
    
    Bug: b:238810157
    Change-Id: Ie5dd455d0fc3ee9e9b7380a13117dd59cc05b025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854433
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Bowen Li <lbowen@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041761}

---
+++ ash/webui/demo_mode_app_ui/resources/demo_mode_metrics_service.js
@@ -0,0 +1,18 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Provides interfaces for emitting metrics from demo mode apps to UMA.
+ */
+class DemoMetricsService {
+  constructor() {}
+
+  // Record the action that the user breaks the current Attract Loop.
+  recordAttractLoopBreak() {
+    chrome.metricsPrivateIndividualApis.recordUserAction(
+        'DemoMode.AttractLoop.Break');
+  }
+}
+
+export const demoMetricsService = new DemoMetricsService();
