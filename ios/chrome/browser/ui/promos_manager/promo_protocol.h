865965d58e615d9ae43c9d5c47261b461e141767
Benjamin Williams <bwwilliams@google.com>
2022-08-31 15:08:28 +0000

    Create PromoProtocol.
    PromoProtocol defines the minimum set of data required to create a Promo
    maintained by the PromosManager. A promo object must have a unique
    mapping to a promo enum (promos_manager::Promo), and (optionally) can
    define promo-specific impression limits to control its display behavior.
    
    Change-Id: Ibc368904c92202ce3231f5728a850fab126fe8b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864425
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041532}

---
+++ ios/chrome/browser/ui/promos_manager/promo_protocol.h
@@ -0,0 +1,32 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMO_PROTOCOL_H_
+#define IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMO_PROTOCOL_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/promos_manager/constants.h"
+#import "ios/chrome/browser/promos_manager/impression_limit.h"
+
+// PromoProtocol defines the minimum set of data required to create a
+// Promo maintained by the PromosManager. A promo object must have a
+// unique mapping to a promo enum (promos_manager::Promo), and
+// (optionally) can define promo-specific impression limits to control
+// its display behavior.
+@protocol PromoProtocol <NSObject>
+
+@optional
+
+// The promo-specific impression limits.
+- (NSArray<ImpressionLimit*>*)impressionLimits;
+
+@required
+
+// Which promos_manager::Promo the object is uniquely associated with.
+- (promos_manager::Promo)identifier;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMO_PROTOCOL_H_
