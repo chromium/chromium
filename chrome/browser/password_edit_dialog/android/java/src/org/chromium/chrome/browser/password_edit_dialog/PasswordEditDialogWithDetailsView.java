ff9b823ba6bf95ab10ba19b3d294115a94c08408
Anna Tsvirchkova <atsvirchkova@google.com>
2022-09-01 10:21:31 +0000

    [PwdMessageDialog] Username combobox styling
    Changes the style of the username input field dropdown so that it would
    look like the usual spinner.
    See the screenshot in the bug link.
    
    Bug: 1315916
    Change-Id: I29777c306b71c9166075d35b8e20c003f8f33139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865511
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042007}

--- chrome/browser/password_edit_dialog/android/java/src/org/chromium/chrome/browser/password_edit_dialog/PasswordEditDialogWithDetailsView.java
+++ chrome/browser/password_edit_dialog/android/java/src/org/chromium/chrome/browser/password_edit_dialog/PasswordEditDialogWithDetailsView.java
@@ -80,8 +80,7 @@ public class PasswordEditDialogWithDetailsView extends PasswordEditDialogView {
     @Override
     public void setUsernames(List<String> usernames, String initialUsername) {
         ArrayAdapter<String> usernamesAdapter = new NoFilterArrayAdapter<>(
-                getContext(), android.R.layout.simple_spinner_item, usernames);
-        usernamesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                getContext(), R.layout.password_edit_dialog_dropdown_item, usernames);
         mUsernameView.setAdapter(usernamesAdapter);
         mUsernameView.setText(initialUsername);
     }
