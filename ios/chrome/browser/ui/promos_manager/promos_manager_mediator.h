500e5ae62254fe5b5e7466ad618631ac4ab2b40a
Benjamin Williams <bwwilliams@google.com>
2022-09-01 14:24:51 +0000

    Create PromosManagerMediator.
    PromosManagerMediator should be created to communicate with the
    PromosManager to fetch the next promo for display, if any.
    
    If there's a promo to display, the mediator should dispatch it via a
    command to be handled elsewhere in the application.
    
    Change-Id: I15e3f68d009dd102f28f98e83fd52d310d420700
    Bug: 1358576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3863645
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042090}

---
+++ ios/chrome/browser/ui/promos_manager/promos_manager_mediator.h
@@ -0,0 +1,36 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMOS_MANAGER_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMOS_MANAGER_MEDIATOR_H_
+
+#import "ios/chrome/browser/promos_manager/promos_manager.h"
+#import "ios/chrome/browser/ui/commands/promos_manager_commands.h"
+#import "ios/chrome/browser/ui/promos_manager/promos_manager_scene_availability_observer.h"
+
+// A mediator that observes when it's a good time to display a promo, and
+// communicates with PromosManager to find the next promo
+// (promos_manager::Promo), if any, to display.
+//
+// If there exists a promo to display, communicates this information to the rest
+// of the application via `handler`.
+@interface PromosManagerMediator
+    : NSObject <PromosManagerSceneAvailabilityObserver>
+
+// Designated initializer.
+- (instancetype)initWithPromosManager:(PromosManager*)promosManager
+                           andHandler:(id<PromosManagerCommands>)handler
+    NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+// The Promos Manager used for deciding which promo should be displayed, if any.
+@property(nonatomic, assign) PromosManager* promosManager;
+
+// Handler used to send a command for displaying a given promo.
+@property(nonatomic, weak) id<PromosManagerCommands> handler;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_PROMOS_MANAGER_MEDIATOR_H_
---
+++ ios/chrome/browser/ui/promos_manager/promos_manager_mediator.mm
@@ -0,0 +1,44 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/promos_manager/promos_manager_mediator.h"
+
+#import "ios/chrome/browser/promos_manager/constants.h"
+#import "third_party/abseil-cpp/absl/types/optional.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation PromosManagerMediator
+
+- (instancetype)initWithPromosManager:(PromosManager*)promosManager
+                           andHandler:(id<PromosManagerCommands>)handler {
+  if (self = [super init]) {
+    _promosManager = promosManager;
+    _handler = handler;
+  }
+
+  return self;
+}
+
+#pragma mark - PromosManagerSceneAvailabilityObserver
+
+// Queries the PromosManager for the next promo (promos_manager::Promo) to
+// display, if any.
+//
+// If there's an eligible promo to display, dispatches it via `handler` to be
+// handled by the rest of the application.
+- (void)sceneDidBecomeAvailableForPromo {
+  DCHECK_NE(_promosManager, nullptr);
+  DCHECK(_handler);
+
+  absl::optional<promos_manager::Promo> nextPromoForDisplay =
+      self.promosManager->NextPromoForDisplay();
+
+  if (nextPromoForDisplay.has_value())
+    [self.handler displayPromo:nextPromoForDisplay.value()];
+}
+
+@end
