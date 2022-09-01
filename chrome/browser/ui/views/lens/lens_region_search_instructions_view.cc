e679960d3f6b5101fdcdee36051ed142fdf41367
Juan Mojica <juanmojica@google.com>
2022-09-01 21:32:18 +0000

    Add top margin and resize selection icons.
    This also replaces the image selection icon with a more standard type.
    The top margin should only apply to selection icons that contain a cross
    cursor.
    
    General Selection Icon Screenshot: https://screenshot.googleplex.com/ByV3iF83t4pbLG9
    Image Selection Icon Screenshot: https://screenshot.googleplex.com/55fJz9ctpHMR8u6
    
    Bug: b:243549841, 1358727
    Change-Id: If97e6ba0a37e99f130ef1f4a3d10bdb01667a3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866602
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042300}

--- chrome/browser/ui/views/lens/lens_region_search_instructions_view.cc
+++ chrome/browser/ui/views/lens/lens_region_search_instructions_view.cc
@@ -33,7 +33,7 @@ constexpr int kCloseButtonExtraMargin = 4;
 constexpr int kCloseButtonSize = 17;
 constexpr int kCornerRadius = 18;
 constexpr int kLabelExtraLeftMargin = 2;
-constexpr int kSelectionIconSize = 16;
+constexpr int kSelectionIconTopMargin = 2;
 
 int GetLensInstructionChipString() {
   if (features::UseAltChipString()) {
@@ -123,7 +123,17 @@ void LensRegionSearchInstructionsView::Init() {
     auto selection_icon_view =
         std::make_unique<views::ImageView>(ui::ImageModel::FromVectorIcon(
             selection_icon, kColorFeatureLensPromoBubbleForeground,
-            kSelectionIconSize));
+            layout_provider->GetDistanceMetric(
+                DISTANCE_BUBBLE_HEADER_VECTOR_ICON_SIZE)));
+    // TODO(b/244610006): We need to set a top margin to make sure our icons
+    // feel properly centered even though they are vertically centered. Only
+    // needed for the selection icons which contain a cross cursor. Asset should
+    // be updated in the future to make this unnecessary.
+    selection_icon_view->SetProperty(
+        views::kMarginsKey,
+        gfx::Insets::TLBR(
+            features::UseSelectionIconWithImage() ? 0 : kSelectionIconTopMargin,
+            0, 0, 0));
     AddChildView(std::move(selection_icon_view));
   }
 
