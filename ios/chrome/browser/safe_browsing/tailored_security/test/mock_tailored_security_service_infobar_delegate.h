45cf3f21dd5035285c3bca293e92a1013c012408
Joemer Ramos <joemerramos@chromium.org>
2022-09-01 15:51:13 +0000

    [iOS] Create Mock Infobar Delegate for Tailored Security Service
    Create a mock infobar delegate for tailored security service. This CL
    ony has the file initializations and base functionality of a mock
    delegate. More functionality and usage will be added in follow-up
    CLs so that CLs are easier to read and manage in general.
    
    Bug: 1358531
    Change-Id: I5f121f6a380cab0c36abef0cbd069b3bb6be4e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866084
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042120}

---
+++ ios/chrome/browser/safe_browsing/tailored_security/test/mock_tailored_security_service_infobar_delegate.h
@@ -0,0 +1,35 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TEST_MOCK_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
+#define IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TEST_MOCK_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.h"
+#import "testing/gmock/include/gmock/gmock.h"
+
+namespace safe_browsing {
+
+// Mock infobar delegate for tailored security service.
+class MockTailoredSecurityServiceInfobarDelegate
+    : public TailoredSecurityServiceInfobarDelegate {
+ public:
+  MockTailoredSecurityServiceInfobarDelegate(bool consent_status);
+  ~MockTailoredSecurityServiceInfobarDelegate() override;
+
+  // Factory method that creates a mock tailored security service delegate..
+  static std::unique_ptr<MockTailoredSecurityServiceInfobarDelegate> Create(
+      bool consent_status);
+
+  MOCK_METHOD0(InfoBarDismissed, void());
+  MOCK_METHOD0(Accept, bool());
+  MOCK_METHOD0(Cancel, bool());
+  MOCK_METHOD1(InfobarPresenting, void(bool automatic));
+  MOCK_METHOD0(InfobarDismissed, void());
+};
+
+}  // namespace safe_browsing
+
+#endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TEST_MOCK_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
---
+++ ios/chrome/browser/safe_browsing/tailored_security/test/mock_tailored_security_service_infobar_delegate.mm
@@ -0,0 +1,29 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safe_browsing/tailored_security/test/mock_tailored_security_service_infobar_delegate.h"
+
+#import "base/memory/ptr_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace safe_browsing {
+
+// static
+std::unique_ptr<MockTailoredSecurityServiceInfobarDelegate>
+MockTailoredSecurityServiceInfobarDelegate::Create(bool consent_status) {
+  return base::WrapUnique(
+      new MockTailoredSecurityServiceInfobarDelegate(consent_status));
+}
+
+MockTailoredSecurityServiceInfobarDelegate::
+    MockTailoredSecurityServiceInfobarDelegate(bool consent_status)
+    : TailoredSecurityServiceInfobarDelegate(consent_status) {}
+
+MockTailoredSecurityServiceInfobarDelegate::
+    ~MockTailoredSecurityServiceInfobarDelegate() = default;
+
+}  // namespace safe_browsing
