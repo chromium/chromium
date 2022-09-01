a08532ec9c1aa022beb2e912057b9322264b9efe
Steven Holte <holte@chromium.org>
2022-09-01 19:29:33 +0000

    Set default initializations for ClientFilterableState.
    This should help guarantee there are no uninitialized memory usages, since that's not trivially checkable. This fixes some test objects that leave these uninitialized.
    
    Bug: 1358059
    Change-Id: I7f6e112a9c260ad3579ac87361bf1164c60faf80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3866653
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042246}

--- components/variations/client_filterable_state.h
+++ components/variations/client_filterable_state.h
@@ -63,16 +63,16 @@ struct COMPONENT_EXPORT(VARIATIONS) ClientFilterableState {
   base::Version os_version;
 
   // The Channel for this Chrome installation.
-  Study::Channel channel;
+  Study::Channel channel = Study::UNKNOWN;
 
   // The hardware form factor that Chrome is running on.
-  Study::FormFactor form_factor;
+  Study::FormFactor form_factor = Study::DESKTOP;
 
   // The CPU architecture on which Chrome is running.
-  Study::CpuArchitecture cpu_architecture;
+  Study::CpuArchitecture cpu_architecture = Study::X86_64;
 
   // The OS on which Chrome is running.
-  Study::Platform platform;
+  Study::Platform platform = Study::PLATFORM_WINDOWS;
 
   // The named hardware configuration that Chrome is running on -- used to
   // identify models of devices.
