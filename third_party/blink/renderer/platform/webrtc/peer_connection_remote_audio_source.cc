d97a60f92cfd8aaa459056e4f6725f74069fc368
Fredrik Hernqvist <fhernqvist@google.com>
2022-09-01 09:42:02 +0000

    Use new AudioParameters constructors in /third_party/blink/renderer/platform/webrtc
    Once all usages of the old AudioParameters constructors have been removed,
    they will be deleted.
    
    This CL was uploaded by git cl split.
    
    R=ilnik@chromium.org
    
    Bug: 1286281
    Change-Id: I165852eb10bc8bb3a3723879a60d4d53507c4f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867369
    Auto-Submit: Fredrik Hernqvist <fhernqvist@google.com>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1041998}

--- third_party/blink/renderer/platform/webrtc/peer_connection_remote_audio_source.cc
+++ third_party/blink/renderer/platform/webrtc/peer_connection_remote_audio_source.cc
@@ -159,9 +159,10 @@ void PeerConnectionRemoteAudioSource::OnData(const void* audio_data,
       params.channels() != channels_int ||
       params.sample_rate() != sample_rate ||
       params.frames_per_buffer() != frames_int) {
-    MediaStreamAudioSource::SetFormat(media::AudioParameters(
-        media::AudioParameters::AUDIO_PCM_LOW_LATENCY,
-        media::GuessChannelLayout(channels_int), sample_rate, frames_int));
+    MediaStreamAudioSource::SetFormat(
+        media::AudioParameters(media::AudioParameters::AUDIO_PCM_LOW_LATENCY,
+                               media::ChannelLayoutConfig::Guess(channels_int),
+                               sample_rate, frames_int));
   }
 
   MediaStreamAudioSource::DeliverDataToTracks(*audio_bus_, playout_time);
