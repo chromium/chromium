a528682b61aa855f70ec02b5dca8d335a164edbd
Joemer Ramos <joemerramos@chromium.org>
2022-09-01 14:13:10 +0000

    [iOS] Create new delegate and OverlayRequestConfig for Tailored Security
    Tailored Security service needs to have an infobar to make users
    aware of their Safe Browsing protection level. In order to have a
    customatized infobar banner, we need to create the infrastructure
    starting with an OverlayRequestConfig and a delegate which will hold
    information which will be sent to the config. This CL is meant to create
    files needed for the overall implementation to work.
    
    Bug: 1357605
    Change-Id: Ida1b45b4c3fade789a278a0118e8d3e9084fab8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865974
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042086}

---
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.h
@@ -0,0 +1,50 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
+#define IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
+
+#import "components/infobars/core/confirm_infobar_delegate.h"
+
+#import <string>
+
+namespace safe_browsing {
+
+class TailoredSecurityServiceInfobarDelegate : public ConfirmInfoBarDelegate {
+ public:
+  TailoredSecurityServiceInfobarDelegate(bool consent_status);
+  TailoredSecurityServiceInfobarDelegate(
+      const TailoredSecurityServiceInfobarDelegate&) = delete;
+  TailoredSecurityServiceInfobarDelegate& operator=(
+      const TailoredSecurityServiceInfobarDelegate&) = delete;
+
+  // Returns |delegate| as an TailoredSecurityServiceInfobarDelegate, or
+  // nullptr if it is of another type.
+  static TailoredSecurityServiceInfobarDelegate* FromInfobarDelegate(
+      infobars::InfoBarDelegate* delegate);
+
+  // Returns the message button text.
+  std::u16string GetMessageActionText() const;
+
+  // Returns the subtitle text to be displayed in the banner.
+  std::u16string GetDescription() const;
+
+  // Returns the consent status of the user.
+  bool consent_status() const { return consent_status_; }
+
+  // ConfirmInfoBarDelegate
+  std::u16string GetMessageText() const override;
+  infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
+  bool EqualsDelegate(infobars::InfoBarDelegate* delegate) const override;
+
+ private:
+  // Returns the consent status based on if the user has enabled Account
+  // Enhanced Safe Browsing. Default value is false since Account Enhanced Safe
+  // Browsing is disabled by default.
+  bool consent_status_ = false;
+};
+
+}  // namespace safe_browsing
+
+#endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE_H_
---
+++ ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.mm
@@ -0,0 +1,55 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safe_browsing/tailored_security/tailored_security_service_infobar_delegate.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+
+// TODO(crbug.com/1358259): Replace test strings.
+const std::u16string test_consent_string = u"test_consent_string";
+const std::u16string test_unconsent_string = u"test_unconsent_string";
+
+}  // namespace
+
+namespace safe_browsing {
+TailoredSecurityServiceInfobarDelegate::TailoredSecurityServiceInfobarDelegate(
+    bool consent_status)
+    : consent_status_(consent_status) {}
+
+TailoredSecurityServiceInfobarDelegate*
+TailoredSecurityServiceInfobarDelegate::FromInfobarDelegate(
+    infobars::InfoBarDelegate* delegate) {
+  return delegate->GetIdentifier() == TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE
+             ? static_cast<TailoredSecurityServiceInfobarDelegate*>(delegate)
+             : nullptr;
+}
+
+std::u16string TailoredSecurityServiceInfobarDelegate::GetMessageText() const {
+  return consent_status_ ? test_consent_string : test_unconsent_string;
+}
+
+std::u16string TailoredSecurityServiceInfobarDelegate::GetDescription() const {
+  return consent_status_ ? test_consent_string : nil;
+}
+
+std::u16string TailoredSecurityServiceInfobarDelegate::GetMessageActionText()
+    const {
+  return consent_status_ ? test_consent_string : test_unconsent_string;
+}
+
+bool TailoredSecurityServiceInfobarDelegate::EqualsDelegate(
+    infobars::InfoBarDelegate* delegate) const {
+  return delegate->GetIdentifier() == GetIdentifier();
+}
+
+infobars::InfoBarDelegate::InfoBarIdentifier
+TailoredSecurityServiceInfobarDelegate::GetIdentifier() const {
+  return TAILORED_SECURITY_SERVICE_INFOBAR_DELEGATE;
+}
+
+}  // namespace safe_browsing
