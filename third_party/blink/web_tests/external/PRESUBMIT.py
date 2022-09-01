b7e5e0346d6bf25de7f120092bf22d3d3d1ba0f9
Jonathan Lee <jonathanjlee@google.com>
2022-09-01 21:43:39 +0000

    [wpt] Do not `wpt lint` metadata files
    The linter produces false positives:
    * The `.ini` extension pushes some tests past the wpt-mandated filename
      length limit of 150. This is just a heuristic (the hard limit on
      Windows is 260 characters), so adding `.ini` to tests that pass the
      linter is probably fine.
    * Subtest names can mention the internal `web-platform.test` domain,
      but this is a function of the test.
    
    Bug: 1299650
    Change-Id: Iaa296ac452f9dc5b50486a59477d72e33488963a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3869178
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1042320}

--- third_party/blink/web_tests/external/PRESUBMIT.py
+++ third_party/blink/web_tests/external/PRESUBMIT.py
@@ -52,7 +52,7 @@ def _LintWPT(input_api, output_api):
         if abs_path.endswith(input_api.os_path.relpath(abs_path, wpt_path)):
             paths_in_wpt.append(abs_path)
 
-    # If there are changes in LayoutTests/external that aren't in wpt, e.g.
+    # If there are changes in web_tests/external that aren't in wpt, e.g.
     # changes to wpt_automation or this presubmit script, then we can return
     # to avoid running the linter on all files in wpt (which is slow).
     if not paths_in_wpt:
@@ -70,6 +70,7 @@ def _LintWPT(input_api, output_api):
         'lint',
         '--repo-root=%s' % wpt_path,
         '--ignore-glob=*-expected.txt',
+        '--ignore-glob=*.ini',
         '--ignore-glob=*DIR_METADATA',
         '--ignore-glob=*OWNERS',
         '--paths-file=%s' % paths_name,
