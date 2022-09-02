30a939b8748b8ba71a9806c1f6b7d0f848cf576c
Matthew DeVore <matvore@chromium.org>
2022-09-02 06:47:33 +0000

    Split GameModeControllerTest according to mode type
    The split is not perfect. There is still a reference to Borealis game
    mode in the ARC test, for testing when switching directly between the
    two modes. The ARC test needs to use the HistogramTester so I put it in
    the parent class.
    
    Bug: b:244123832
    Test: GameModeControllerForArcTest+GameModeControllerForBorealisTest
    Change-Id: I8009e3e550e24034525e7e1c5a90b91f3bb0a82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3867306
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042469}

---
+++ chrome/browser/ash/game_mode/game_mode_controller_for_arc_unittest.cc
@@ -0,0 +1,190 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/game_mode/testing/game_mode_controller_test_base.h"
+
+#include "ash/components/arc/arc_features.h"
+#include "ash/components/arc/test/fake_app_instance.h"
+#include "ash/shell.h"
+#include "ash/test/test_widget_builder.h"
+#include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ash/borealis/testing/widgets.h"
+#include "chrome/browser/ui/app_list/arc/arc_app_test.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chromeos/ash/components/dbus/resourced/fake_resourced_client.h"
+#include "components/exo/shell_surface_util.h"
+#include "ui/views/widget/widget.h"
+
+namespace game_mode {
+namespace {
+
+class GameModeControllerForArcTest : public GameModeControllerTestBase {
+ protected:
+  void SetUp() override {
+    features_.InitAndEnableFeature(arc::kGameModeFeature);
+
+    GameModeControllerTestBase::SetUp();
+
+    arc_app_test_.SetUp(profile_.get());
+
+    focus_client_ =
+        aura::client::GetFocusClient(ash::Shell::GetPrimaryRootWindow());
+    ASSERT_TRUE(focus_client_);
+  }
+
+  void TearDown() override {
+    game_mode::ClearArcPkgNamesForTesting();
+    focus_client_ = nullptr;
+    arc_app_test_.TearDown();
+
+    GameModeControllerTestBase::TearDown();
+  }
+
+  std::unique_ptr<views::Widget> CreateArcTaskWidget(int task_id) {
+    ash::TestWidgetBuilder builder;
+    builder.SetShow(false);
+    auto widget = builder.BuildOwnsNativeWidget();
+    exo::SetShellApplicationId(
+        widget->GetNativeWindow(),
+        base::StringPrintf("org.chromium.arc.%d", task_id));
+    return widget;
+  }
+
+  ArcAppTest arc_app_test_;
+  aura::client::FocusClient* focus_client_ = nullptr;
+  base::test::ScopedFeatureList features_;
+};
+
+TEST_F(GameModeControllerForArcTest, ChangingFullScreenTogglesGameMode) {
+  arc_app_test_.app_instance()->SetTaskInfo(42, "org.funstuff.client",
+                                            "activity");
+  game_mode::AddArcPkgNameForTesting("org.funstuff.client");
+
+  auto game_widget = CreateArcTaskWidget(42);
+  game_widget->Show();
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::OFF);
+
+  EXPECT_EQ(0, fake_resourced_client_->get_enter_game_mode_count());
+  game_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::ARC);
+
+  EXPECT_EQ(0, fake_resourced_client_->get_exit_game_mode_count());
+  game_widget->SetFullscreen(false);
+  EXPECT_EQ(1, fake_resourced_client_->get_exit_game_mode_count());
+}
+
+TEST_F(GameModeControllerForArcTest, SwitchToNonGameArcAppTurnsOffGameMode) {
+  arc_app_test_.app_instance()->SetTaskInfo(2424, "net.another.game",
+                                            "activity");
+  game_mode::AddArcPkgNameForTesting("net.another.game");
+
+  auto game_widget = CreateArcTaskWidget(2424);
+  game_widget->Show();
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::OFF);
+
+  game_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::ARC);
+
+  arc_app_test_.app_instance()->set_app_category_of_pkg(
+        "net.recipes.search", arc::mojom::AppCategory::kProductivity);
+  arc_app_test_.app_instance()->SetTaskInfo(9999, "net.recipes.search",
+                                            "activity");
+
+  EXPECT_EQ(0, fake_resourced_client_->get_exit_game_mode_count());
+  auto app_widget = CreateArcTaskWidget(9999);
+  app_widget->Show();
+  EXPECT_EQ(1, fake_resourced_client_->get_exit_game_mode_count());
+}
+
+TEST_F(GameModeControllerForArcTest,
+       SwitchToNonArcWindowAndBackTurnsOffGameMode) {
+  arc_app_test_.app_instance()->SetTaskInfo(42, "org.some.game", "activity");
+  game_mode::AddArcPkgNameForTesting("org.some.game");
+
+  auto game_widget = CreateArcTaskWidget(42);
+  game_widget->Show();
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::OFF);
+
+  game_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::ARC);
+
+  EXPECT_EQ(0, fake_resourced_client_->get_exit_game_mode_count());
+  auto other_window = CreateTestWindow();
+  EXPECT_EQ(1, fake_resourced_client_->get_exit_game_mode_count());
+
+  // Move focus back to window already fullscreen, and we should turn game mode
+  // back on.
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+  EXPECT_TRUE(game_widget->IsFullscreen());
+  game_widget->Show();
+  EXPECT_EQ(2, fake_resourced_client_->get_enter_game_mode_count());
+}
+
+TEST_F(GameModeControllerForArcTest, SwitchToBorealisWindowAndBack) {
+  arc_app_test_.app_instance()->SetTaskInfo(14, "jp.foo.game", "activity");
+  game_mode::AddArcPkgNameForTesting("jp.foo.game");
+
+  auto non_game_widget = ash::TestWidgetBuilder()
+      .SetShow(true)
+      .BuildOwnsNativeWidget();
+
+  auto game_widget = CreateArcTaskWidget(14);
+
+  std::unique_ptr<views::Widget> borealis_widget =
+      borealis::CreateFakeWidget("org.chromium.borealis.foo");
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::OFF);
+  EXPECT_EQ(0, fake_resourced_client_->get_enter_game_mode_count());
+
+  game_widget->Show();
+  game_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::ARC);
+  borealis_widget->Show();
+  borealis_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_exit_game_mode_count());
+  EXPECT_EQ(2, fake_resourced_client_->get_enter_game_mode_count());
+
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::BOREALIS);
+  game_widget->Show();
+  EXPECT_EQ(2, fake_resourced_client_->get_exit_game_mode_count());
+  EXPECT_EQ(3, fake_resourced_client_->get_enter_game_mode_count());
+}
+
+TEST_F(GameModeControllerForArcTest, IdentifyGameWithGetAppCategory) {
+  arc_app_test_.app_instance()->set_app_category_of_pkg(
+      "org.an_awesome.game", arc::mojom::AppCategory::kGame);
+  arc_app_test_.app_instance()->SetTaskInfo(
+      9882, "org.an_awesome.game", "activity");
+
+  auto game_widget = CreateArcTaskWidget(9882);
+  game_widget->Show();
+  fake_resourced_client_->set_set_game_mode_response(
+      ash::ResourcedClient::GameMode::OFF);
+  game_widget->SetFullscreen(true);
+  EXPECT_EQ(1, fake_resourced_client_->get_enter_game_mode_count());
+}
+
+}  // namespace
+}  // namespace game_mode
