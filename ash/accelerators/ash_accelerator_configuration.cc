89d28243d413b40015f18801fd4eae094a03b972
Jimmy <jimmyxgong@chromium.org>
2022-09-02 01:23:33 +0000

    shortcuts: Add AshAcceleratorConfig stub
    AshAcceleratorConfig is an AcceleratorConfiguration that provides an
    interface to view/modify ash accelerators. This is only a stub version.
    
    Bug: b/216049298
    Test: None
    Change-Id: I91f53a54f04d7b543d2a0d792b81e3740e6b02b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3836861
    Quick-Run: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042415}

---
+++ ash/accelerators/ash_accelerator_configuration.cc
@@ -0,0 +1,56 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/accelerators/ash_accelerator_configuration.h"
+
+#include <vector>
+
+#include "ash/public/mojom/accelerator_info.mojom.h"
+#include "ui/base/accelerators/accelerator.h"
+
+namespace ash {
+
+AshAcceleratorConfiguration::AshAcceleratorConfiguration()
+    : AcceleratorConfiguration(ash::mojom::AcceleratorSource::kAsh) {}
+AshAcceleratorConfiguration::~AshAcceleratorConfiguration() = default;
+
+// TODO(jimmyxgong): Implement all functions below as these are only stubs.
+const std::vector<AcceleratorInfo>&
+AshAcceleratorConfiguration::GetConfigForAction(AcceleratorAction actionId) {
+  return accelerator_infos_;
+}
+
+bool AshAcceleratorConfiguration::IsMutable() const {
+  return false;
+}
+
+AcceleratorConfigResult AshAcceleratorConfiguration::AddUserAccelerator(
+    AcceleratorAction action,
+    const ui::Accelerator& accelerator) {
+  return AcceleratorConfigResult::kActionLocked;
+}
+
+AcceleratorConfigResult AshAcceleratorConfiguration::RemoveAccelerator(
+    AcceleratorAction action,
+    const ui::Accelerator& accelerator) {
+  return AcceleratorConfigResult::kActionLocked;
+}
+
+AcceleratorConfigResult AshAcceleratorConfiguration::ReplaceAccelerator(
+    AcceleratorAction action,
+    const ui::Accelerator& old_acc,
+    const ui::Accelerator& new_acc) {
+  return AcceleratorConfigResult::kActionLocked;
+}
+
+AcceleratorConfigResult AshAcceleratorConfiguration::RestoreDefault(
+    AcceleratorAction action) {
+  return AcceleratorConfigResult::kActionLocked;
+}
+
+AcceleratorConfigResult AshAcceleratorConfiguration::RestoreAllDefaults() {
+  return AcceleratorConfigResult::kActionLocked;
+}
+
+}  // namespace ash
---
+++ ash/accelerators/ash_accelerator_configuration.h
@@ -0,0 +1,50 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_ACCELERATORS_ASH_ACCELERATOR_CONFIGURATION_H_
+#define ASH_ACCELERATORS_ASH_ACCELERATOR_CONFIGURATION_H_
+
+#include <vector>
+
+#include "ash/ash_export.h"
+#include "ash/public/cpp/accelerator_configuration.h"
+#include "ash/public/mojom/accelerator_info.mojom.h"
+
+namespace ash {
+
+// Implementor of AcceleratorConfiguration for Ash accelerators.
+// This class exist as a way to provide access to view and modify Ash
+// accelerators.
+class ASH_EXPORT AshAcceleratorConfiguration : public AcceleratorConfiguration {
+ public:
+  AshAcceleratorConfiguration();
+  AshAcceleratorConfiguration(const AshAcceleratorConfiguration&) = delete;
+  AshAcceleratorConfiguration& operator=(const AshAcceleratorConfiguration&) =
+      delete;
+  ~AshAcceleratorConfiguration() override;
+
+  // AcceleratorConfiguration::
+  const std::vector<AcceleratorInfo>& GetConfigForAction(
+      AcceleratorAction actionId) override;
+  bool IsMutable() const override;
+  AcceleratorConfigResult AddUserAccelerator(
+      AcceleratorAction action,
+      const ui::Accelerator& accelerator) override;
+  AcceleratorConfigResult RemoveAccelerator(
+      AcceleratorAction action,
+      const ui::Accelerator& accelerator) override;
+  AcceleratorConfigResult ReplaceAccelerator(
+      AcceleratorAction action,
+      const ui::Accelerator& old_acc,
+      const ui::Accelerator& new_acc) override;
+  AcceleratorConfigResult RestoreDefault(AcceleratorAction action) override;
+  AcceleratorConfigResult RestoreAllDefaults() override;
+
+ private:
+  std::vector<AcceleratorInfo> accelerator_infos_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_ACCELERATORS_ASH_ACCELERATOR_CONFIGURATION_H_
