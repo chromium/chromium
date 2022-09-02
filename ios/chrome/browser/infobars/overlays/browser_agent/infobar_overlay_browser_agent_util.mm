0682383c8de694ee3d7872b3afce76508ee83cdf
Joemer Ramos <joemerramos@chromium.org>
2022-09-02 20:23:14 +0000

    [iOS] Create Interaction Handler for TailoredSecurity Infobar
    The new tailored security infobar needs to have an interaction
    handler to properly work with requested functionality. Good to note
    that the modal parameter is null here since a tailored security infobar
    doesn't need a modal overlay.
    
    LOW_COVERAGE_REASON=Util file isn't unit tested
    
    Bug: 1357605
    Change-Id: Id046448d97b05501c0ba76e16b020724841abc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871095
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042756}

--- ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
@@ -5,6 +5,7 @@
 #include "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.h"
 
 #include "base/feature_list.h"
+#import "components/safe_browsing/core/common/features.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/confirm/confirm_infobar_interaction_handler.h"
@@ -12,6 +13,7 @@
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/update_password_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/permissions/permissions_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/save_card/save_card_infobar_interaction_handler.h"
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/tailored_security/tailored_security_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/translate/translate_infobar_interaction_handler.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -36,4 +38,9 @@ void AttachInfobarOverlayBrowserAgent(Browser* browser) {
       std::make_unique<SaveAddressProfileInfobarInteractionHandler>());
   browser_agent->AddInfobarInteractionHandler(
       std::make_unique<PermissionsInfobarInteractionHandler>());
+  if (base::FeatureList::IsEnabled(
+          safe_browsing::kTailoredSecurityIntegration)) {
+    browser_agent->AddInfobarInteractionHandler(
+        std::make_unique<TailoredSecurityInfobarInteractionHandler>());
+  }
 }
---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/tailored_security/tailored_security_infobar_banner_interaction_handler_unittest.mm
@@ -0,0 +1,60 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/tailored_security/tailored_security_infobar_banner_interaction_handler.h"
+
+#import "ios/chrome/browser/infobars/infobar_manager_impl.h"
+#import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_inserter.h"
+#import "ios/chrome/browser/infobars/test/fake_infobar_ios.h"
+#import "ios/chrome/browser/overlays/public/infobar_banner/tailored_security_service_infobar_banner_overlay_request_config.h"
+#import "ios/chrome/browser/safe_browsing/tailored_security/test/mock_tailored_security_service_infobar_delegate.h"
+#import "ios/web/public/test/fakes/fake_navigation_manager.h"
+#import "ios/web/public/test/fakes/fake_web_state.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Test fixture for TailoredSecurityInfobarBannerInteractionHandlerTest.
+class TailoredSecurityInfobarBannerInteractionHandlerTest
+    : public PlatformTest {
+ public:
+  TailoredSecurityInfobarBannerInteractionHandlerTest()
+      : handler_(
+            tailored_security_service_infobar_overlays::
+                TailoredSecurityServiceBannerRequestConfig::RequestSupport()) {
+    web_state_.SetNavigationManager(
+        std::make_unique<web::FakeNavigationManager>());
+    InfobarOverlayRequestInserter::CreateForWebState(&web_state_);
+    InfoBarManagerImpl::CreateForWebState(&web_state_);
+    std::unique_ptr<InfoBarIOS> infobar = std::make_unique<InfoBarIOS>(
+        InfobarType::kInfobarTypeTailoredSecurityService,
+        safe_browsing::MockTailoredSecurityServiceInfobarDelegate::Create(
+            /*consent_status*/ true));
+    infobar_ = infobar.get();
+    InfoBarManagerImpl::FromWebState(&web_state_)
+        ->AddInfoBar(std::move(infobar));
+  }
+
+  safe_browsing::MockTailoredSecurityServiceInfobarDelegate& mock_delegate() {
+    return *static_cast<
+        safe_browsing::MockTailoredSecurityServiceInfobarDelegate*>(
+        infobar_->delegate());
+  }
+
+ protected:
+  TailoredSecurityInfobarBannerInteractionHandler handler_;
+  web::FakeWebState web_state_;
+  InfoBarIOS* infobar_;
+};
+
+// Tests MainButtonTapped() calls Accept() on the mock delegate and resets
+// the infobar to be accepted.
+TEST_F(TailoredSecurityInfobarBannerInteractionHandlerTest, MainButton) {
+  ASSERT_FALSE(infobar_->accepted());
+  EXPECT_CALL(mock_delegate(), Accept()).WillOnce(testing::Return(true));
+  handler_.MainButtonTapped(infobar_);
+  EXPECT_TRUE(infobar_->accepted());
+}
