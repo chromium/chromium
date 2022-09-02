44cd84a5e027c0eb65d874d90aa8dc6bdfe7b74f
Emilia Paz <emiliapaz@chromium.org>
2022-09-02 18:28:26 +0000

    [Extensions] Re-enable ExtensionsToolbarControls unit test
    Re-enable test after crrev.com/c/3865167 fixed issue where
    withholding extensions permissions failed due to browser shutdown globals not been reset.
    
    Reload page bubble was added when changing site access since the test
    became disabled. Accept it so permissions are properly updated.
    
    Test successfully run 100 times on a row locally.
    
    Bug: 1304951
    Change-Id: I042d1ba05e51e79be831a51e15859ea5c6771dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864082
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042685}

--- chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_controls_unittest.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_controls.h"
 
 #include "base/test/metrics/user_action_tester.h"
+#include "chrome/browser/extensions/extension_action_runner.h"
 #include "chrome/browser/extensions/extension_context_menu_model.h"
 #include "chrome/browser/extensions/site_permissions_helper.h"
 #include "chrome/browser/ui/views/extensions/extensions_request_access_button.h"
@@ -169,7 +170,6 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
   // site. Uninstall extension in unit tests is flaky.
 }
 
-// TODO(crbug.com/1321562) Disabled for flakiness.
 TEST_F(ExtensionsToolbarControlsUnitTest,
        RequestAccessButtonVisibility_NavigationBetweenPages) {
   content::WebContentsTester* web_contents_tester =
@@ -196,10 +196,8 @@ TEST_F(ExtensionsToolbarControlsUnitTest,
   EXPECT_FALSE(IsRequestAccessButtonVisible());
 }
 
-// TODO(crbug.com/1321562) Disabled for flakiness.
-TEST_F(
-    ExtensionsToolbarControlsUnitTest,
-    DISABLED_RequestAccessButtonVisibility_ContextMenuChangesHostPermissions) {
+TEST_F(ExtensionsToolbarControlsUnitTest,
+       RequestAccessButtonVisibility_ContextMenuChangesHostPermissions) {
   content::WebContentsTester* web_contents_tester =
       AddWebContentsAndGetTester();
   const GURL url_a("http://www.a.com");
@@ -220,6 +218,13 @@ TEST_F(
       extension.get(), browser(), extensions::ExtensionContextMenuModel::PINNED,
       nullptr, true,
       extensions::ExtensionContextMenuModel::ContextMenuSource::kToolbarAction);
+
+  // Changing the context menu may trigger the reload page bubble. Accept it so
+  // permissions are updated.
+  extensions::ExtensionActionRunner* runner =
+      extensions::ExtensionActionRunner::GetForWebContents(
+          browser()->tab_strip_model()->GetActiveWebContents());
+  runner->accept_bubble_for_testing(true);
 
   // Change the extension to run only on click using the context
   // menu. The extension should request access to the current site.
