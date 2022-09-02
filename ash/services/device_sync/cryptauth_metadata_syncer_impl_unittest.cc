cf0507b72b8a88dcce3d544978974ef518eb765f
Daniel Cheng <dcheng@chromium.org>
2022-09-02 16:23:47 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //ash/services/device_sync.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Bug: 1356184
    Change-Id: Id1c9e1fe5a54e713e95e1397fa223b03c20aa3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3860650
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1042607}

--- ash/services/device_sync/cryptauth_metadata_syncer_impl_unittest.cc
+++ ash/services/device_sync/cryptauth_metadata_syncer_impl_unittest.cc
@@ -32,6 +32,7 @@
 #include "base/no_destructor.h"
 #include "base/stl_util.h"
 #include "base/timer/mock_timer.h"
+#include "base/types/optional_util.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -154,7 +155,7 @@ class DeviceSyncCryptAuthMetadataSyncerImplTest
     metadata_syncer_->SyncMetadata(
         GetRequestContext(),
         *GetLocalDeviceForTest().better_together_device_metadata,
-        base::OptionalOrNullptr(initial_group_key_),
+        base::OptionalToPtr(initial_group_key_),
         base::BindOnce(
             &DeviceSyncCryptAuthMetadataSyncerImplTest::OnMetadataSyncComplete,
             base::Unretained(this)));
