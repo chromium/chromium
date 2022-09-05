30a8d188667b77a00f27eabe46b4c5505be95da6
Daniel Cheng <dcheng@chromium.org>
2022-09-05 15:32:49 +0000

    Migrate base::OptionalOrNullptr() to base::OptionalToPtr() in //components/viz.
    This also updates the includes to the new location, since this helper
    has been split out of //base/stl_util.h
    
    These CLs were automatically generated using the following commands:
    
    sed -i s^OptionalOrNullptr^OptionalToPtr^g $(git gs OptionalOrNullptr --name-only | grep -v base | grep -v audit_non_blink_usage.py) and then git cl format
    git commit -a -m "Step 1"
    tools/add_header.py --remove --header '"base/stl_util.h"' $(grep -l -P "base::to_underlying|base::GetUnderlyingContainer|base::STLClearObject|base::STLCount|base::ConstCastIterator|base::STLSetDifference|base::STLSetUnion|base::STLSetIntersection|base::IsNotIn" $(git diff HEAD^..HEAD --name-only))
    git commit -a -m "Step 2"
    tools/add_header.py --header '"base/types/optional_util.h"' $(git diff HEAD^^..HEAD^ --name-only)
    git commit -a -m "Step 3"
    
    And then using git cl split on the resulting diff.
    
    This CL was uploaded by git cl split.
    
    R=penghuang@chromium.org
    
    Bug: 1356184
    Change-Id: I0f0bdd84bc5bcec13ffcbdbebb5ce1f2841dbf15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3859823
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043227}

--- components/viz/test/test_in_process_context_provider.cc
+++ components/viz/test/test_in_process_context_provider.cc
@@ -5,12 +5,14 @@
 #include "components/viz/test/test_in_process_context_provider.h"
 
 #include <stdint.h>
+
 #include <memory>
 #include <utility>
 
 #include "base/lazy_instance.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/types/optional_util.h"
 #include "components/viz/common/gpu/context_cache_controller.h"
 #include "components/viz/common/resources/platform_color.h"
 #include "components/viz/service/display/display_compositor_memory_and_task_controller.h"
@@ -173,7 +175,7 @@ ContextCacheController* TestInProcessContextProvider::CacheController() {
 }
 
 base::Lock* TestInProcessContextProvider::GetLock() {
-  return base::OptionalOrNullptr(context_lock_);
+  return base::OptionalToPtr(context_lock_);
 }
 
 const gpu::Capabilities& TestInProcessContextProvider::ContextCapabilities()
