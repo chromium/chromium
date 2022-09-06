e7f4d9e8b1067f254b83e7986d8bb4b5e3030af6
Pâris <pmeuleman@chromium.org>
2022-09-06 15:10:11 +0000

    Mark dangling pointers occurring on linux-wayland-rel CQ
    This dangling pointers cases occurred in linux-wayland-rel CQ built
    with dangling pointer detection enabled, with crash behavior in [1].
    
    The impacted tests are:
    AcceptHeaderTest.Check
    AccessibilityActionBrowserTest.AriaGridSelectedChangedEvent
    AccessibilityActionBrowserTest.FocusLostOnDeletedNode
    AccessibilityActionBrowserTest.FocusLostOnDeletedNodeInInnerWebContents
    AccessibilityActionBrowserTest.HorizontalScroll
    AccessibilityActionBrowserTest.ScrollIntoView
    AccessibilityActionBrowserTest.VerticalScroll
    AccessibilityAuraLinuxBrowserTest.SetCaretInTextWithGeneratedContent
    AccessibilityAuraLinuxBrowserTest.SetSelectionWithIgnoredObjects
    AccessibilityAuraLinuxBrowserTest.TestCaretMovedInNumberInput
    AccessibilityAuraLinuxBrowserTest.TestFocusInputTextFields
    AccessibilityAuraLinuxBrowserTest.TestTextSelectionAcrossElements
    AccessibilityCanvasActionBrowserTest.CanvasGetImage
    AccessibilityCanvasActionBrowserTest.CanvasGetImageScale
    AccessibilityModeTest.AccessibilityModeComplete
    AccessibilityModeTest.AccessibilityModeWebContentsOnly
    AccessibilityModeTest.ReEnablingAccessibilityDoesNotTimeout
    AggregationServiceInternalsWebUiBrowserTest.WebUISendReports_ReportsRemoved
    AggregationServiceInternalsWebUiBrowserTest.WebUIShownWithReports_ReportsDisplayed
    All/AccessibilityHitTestingBrowserTest.NearestLeafInIframes/ZoomFactor1
    All/AccessibilityHitTestingCrossProcessBrowserTest.HitTestingInCrossProcessIframeWithScrolling/ZoomFactor1
    All/AccessibilityHitTestingCrossProcessBrowserTest.HitTestingInCrossProcessIframeWithScrolling/ZoomFactor2
    All/AppBannerBackForwardCacheBrowserTest.TestAppBannerCaching/0
    All/AttributionSrcFencedFrameBrowserTest.DefaultMode_SourceNotRegistered/1
    All/BackForwardCacheBrowserTestWithFencedFrames.DoesNotCacheFencedFramesIndirectEmbedder/0
    All/BackForwardCacheBrowserTestWithFencedFrames.DoesNotCacheFencedFramesIndirectEmbedder/1
    All/BackForwardCacheBrowserTestWithFlagForAXEvents.EvictOnAccessibilityEventsOrNot/0
    All/BackForwardCacheBrowserTestWithFlagForAXEvents.EvictOnAccessibilityEventsOrNot/1
    All/BackForwardCacheBrowserTestWithFlagForScreenReader.ScreenReaderOn/0
    All/BackForwardCacheDedicatedWorkerFlagBrowserTest.PageWithDedicatedWorkerCachedOrNot/1
    
    CQ run with this patch applied can be checked in [2], content_browsertests pass.
    
    [1] https://ci.chromium.org/ui/p/chromium/builders/try/linux-wayland-rel/288469/steps
    [2] https://ci.chromium.org/ui/p/chromium/builders/try/linux-wayland-rel/288602/steps
    
    Change-Id: I472c07e492341363db141ee2ef7d46dfc9042a63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3875326
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043441}

--- ui/ozone/platform/wayland/host/wayland_frame_manager.h
+++ ui/ozone/platform/wayland/host/wayland_frame_manager.h
@@ -57,7 +57,7 @@ struct WaylandFrame {
   friend class WaylandFrameManager;
 
   uint32_t frame_id;
-  raw_ptr<WaylandSurface> root_surface;
+  raw_ptr<WaylandSurface, DanglingUntriaged> root_surface;
   wl::WaylandOverlayConfig root_config;
   base::circular_deque<std::pair<WaylandSubsurface*, wl::WaylandOverlayConfig>>
       subsurfaces_to_overlays;
