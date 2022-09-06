68f3f6dd4111b1b8a75bdacecdbe27abd2365faf
Hans Wennborg <hans@chromium.org>
2022-09-06 14:46:07 +0000

    Update del_ninja_deps_cache.py to also handle __tuple not being a dir
    An upcoming libc++ roll is changing __tuple from a file to a dir,
    causing the same problem we had with __string a while back; see bug.
    
    Bug: 1337238
    Change-Id: I266416eaa8d77e36ef11c6880007e04421b7fcc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3874828
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043424}

--- build/del_ninja_deps_cache.py
+++ build/del_ninja_deps_cache.py
@@ -2,9 +2,9 @@
 # Copyright (c) 2022 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Delete .ninja_deps if it references files inside libc++'s __string dir,
-which has since been reverted back to a file, and would cause Ninja fail on
-Windows. See crbug.com/1337238 ..."""
+"""Delete .ninja_deps if it references files inside a libc++ dir which has
+since been reverted back to a file, and would cause Ninja fail on Windows. See
+crbug.com/1337238"""
 
 import os
 import sys
@@ -13,27 +13,25 @@ import sys
 def main():
   os.chdir(os.path.join(os.path.dirname(__file__), '..'))
 
-  if os.path.isdir('buildtools/third_party/libc++/trunk/include/__string'):
-    # If __string is a dir, Ninja will not fail.
-    return 0
+  # Paths that have switched between being a directory and regular file.
+  bad_dirs = [
+      'buildtools/third_party/libc++/trunk/include/__string',
+      'buildtools/third_party/libc++/trunk/include/__tuple',
+  ]
 
-  for d in os.listdir('out'):
-    obj_file = os.path.join(
-        'out', d,
-        'obj/buildtools/third_party/libc++/libc++/legacy_debug_handler.obj')
-    if not os.path.exists(obj_file):
-      # It seems we have not done a build with the libc++ roll.
+  for bad_dir in bad_dirs:
+    if os.path.isdir(bad_dir):
+      # If it's a dir, .ninja_deps referencing files in it is not a problem.
       continue
 
-    try:
-      deps = os.path.join('out', d, '.ninja_deps')
-      if b'__string/char_traits.h' in open(deps, 'rb').read():
-        print('Deleting ', deps)
-        os.remove(deps)
-        print('Deleting ', obj_file)
-        os.remove(obj_file)
-    except FileNotFoundError:
-      pass
+    for out_dir in os.listdir('out'):
+      ninja_deps = os.path.join('out', out_dir, '.ninja_deps')
+      try:
+        if str.encode(bad_dir) + b'/' in open(ninja_deps, 'rb').read():
+          print('Deleting', ninja_deps)
+          os.remove(ninja_deps)
+      except FileNotFoundError:
+        pass
 
   return 0
 
