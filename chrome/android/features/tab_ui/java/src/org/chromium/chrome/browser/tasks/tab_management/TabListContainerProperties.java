2908fc25e0a1492c1bc0d40788967ba550fbbc79
Rohit Agarwal <roagarwal@chromium.org>
2022-09-06 16:17:31 +0000

    [Android][IncognitoReauth] Fix tabswitcher transition issue.
    When the re-auth is being shown inside the tabswitcher and we toggle
    to the regular mode we momentarily flash the incognito tab list which
    is not ideal as it leaks the Incognito content within the context of
    the re-auth. This was due to item animator for the underlying
    TabListRecyclerView which holds the TabList.
    
    This CL adds a new TabListContainerProperty to control the item
    animation when we add/remove the re-auth view.
    
    Demo:
    
    Before
    https://drive.google.com/file/d/1Ou_YjkhVSShEXbv5lb7_m_cdvAa46Gz6/view?usp=sharing
    
    After
    https://drive.google.com/file/d/1lGspi70scDUhD4I5sLWYlqemm0uKZ4pR/view?usp=sharing
    
    Bug: 1227656
    Change-Id: Ia38fbb4514835637efc28b13a5ae7f0ef44dc970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3854300
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043471}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerProperties.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerProperties.java
@@ -40,7 +40,19 @@ class TabListContainerProperties {
     public static final PropertyModel.WritableIntPropertyKey BOTTOM_PADDING =
             new PropertyModel.WritableIntPropertyKey();
 
-    public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {IS_VISIBLE, IS_INCOGNITO,
-            VISIBILITY_LISTENER, INITIAL_SCROLL_INDEX, ANIMATE_VISIBILITY_CHANGES, TOP_MARGIN,
-            BOTTOM_CONTROLS_HEIGHT, SHADOW_TOP_OFFSET, BOTTOM_PADDING};
+    /**
+     * A property which controls whether to use the default animator specified in the underlying
+     * {@link TabListRecyclerView} or use a null item animator.
+     *
+     * TODO(crbug.com/1227656): This property is used only by the Incognito re-auth client and once
+     * the re-auth integration with tab-switcher design is further improved then remove this if no
+     * other clients use this property.
+     */
+    public static final PropertyModel.WritableBooleanPropertyKey TAB_LIST_ITEM_ANIMATOR_ENABLED =
+            new PropertyModel.WritableBooleanPropertyKey();
+
+    public static final PropertyKey[] ALL_KEYS =
+            new PropertyKey[] {IS_VISIBLE, IS_INCOGNITO, VISIBILITY_LISTENER, INITIAL_SCROLL_INDEX,
+                    ANIMATE_VISIBILITY_CHANGES, TOP_MARGIN, BOTTOM_CONTROLS_HEIGHT,
+                    SHADOW_TOP_OFFSET, BOTTOM_PADDING, TAB_LIST_ITEM_ANIMATOR_ENABLED};
 }
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerViewBinder.java
@@ -11,6 +11,7 @@ import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerP
 import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.IS_INCOGNITO;
 import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.IS_VISIBLE;
 import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.SHADOW_TOP_OFFSET;
+import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.TAB_LIST_ITEM_ANIMATOR_ENABLED;
 import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.TOP_MARGIN;
 import static org.chromium.chrome.browser.tasks.tab_management.TabListContainerProperties.VISIBILITY_LISTENER;
 
@@ -64,6 +65,8 @@ class TabListContainerViewBinder {
             view.setShadowTopOffset(model.get(SHADOW_TOP_OFFSET));
         } else if (BOTTOM_PADDING == propertyKey) {
             view.setBottomPadding(model.get(BOTTOM_PADDING));
+        } else if (TAB_LIST_ITEM_ANIMATOR_ENABLED == propertyKey) {
+            view.toggleItemAnimation(model.get(TAB_LIST_ITEM_ANIMATOR_ENABLED));
         }
     }
 }
