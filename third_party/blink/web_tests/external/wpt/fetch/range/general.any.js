b444279f985808b4e471f9ca3bb669ac946af06f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-09-07 17:51:02 +0000

    Import wpt@3b05df4685f0ddffa783c35140846850921708c8
    Using wpt-import in Chromium becc0bf6b4c5213b6b871361f7eb4242b0dde144.
    With Chromium commits locally applied on WPT:
    7eed88d612 "Update popup-redirect-cache.https.html to use dispatcher"
    a693cb8616 "Ensure that sends to each uuid occur in order."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    futhark@chromium.org, andruud@chromium.org, ericwilligers@chromium.org, shend@chromium.org:
      external/wpt/css/css-conditional
    rego@igalia.com:
      external/wpt/css/css-contain
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I7e202dacc96a835b2d7e34292948e21b799ebf68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876655
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1044064}

--- third_party/blink/web_tests/external/wpt/fetch/range/general.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/range/general.any.js
@@ -87,7 +87,10 @@ promise_test(async () => {
     });
 
     const response = await fetch(stashTakeURL);
-    assert_equals(await response.json(), 'identity', `Expect identity accept-encoding if range header is ${JSON.stringify(rangeHeader)}`);
+
+    assert_regexp_match(await response.json(),
+                        /.*\bidentity\b.*/,
+                        `Expect identity accept-encoding if range header is ${JSON.stringify(rangeHeader)}`);
   }
 }, `Fetch with range header will be sent with Accept-Encoding: identity`);
 
