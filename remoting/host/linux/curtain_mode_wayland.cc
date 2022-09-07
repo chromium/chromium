3e2070c3ed3cfc09df07528952e91bed120600d0
Salman Malik <salmanmalik@chromium.org>
2022-09-07 14:52:46 +0000

    curtain_mode_wayland: Add a stub
    Wayland is going to be running a headless session.
    The CRD and local session are completely separate on linux
    anyway so we assume curtaining for Wayland by default.
    
    Bug: chromium:1291247
    Change-Id: Iae62cf85acd13950be482b268327962abd5e9867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865738
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Salman Malik <salmanmalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043953}

--- remoting/host/linux/curtain_mode_wayland.cc
+++ remoting/host/linux/curtain_mode_wayland.cc
@@ -4,19 +4,43 @@
 
 #include "remoting/host/curtain_mode.h"
 
+#include <memory>
+
 #include "base/callback.h"
 #include "base/notreached.h"
 #include "base/task/single_thread_task_runner.h"
 
 namespace remoting {
 
+namespace {
+class CurtainModeWayland : public CurtainMode {
+ public:
+  CurtainModeWayland();
+
+  CurtainModeWayland(const CurtainModeWayland&) = delete;
+  CurtainModeWayland& operator=(const CurtainModeWayland&) = delete;
+
+  // Overriden from CurtainMode.
+  bool Activate() override;
+};
+
+CurtainModeWayland::CurtainModeWayland() = default;
+
+bool CurtainModeWayland::Activate() {
+  // Our implementation runs headlessly on a session with a previously
+  // unused / new display socket, so we can assume that the session is
+  // curtained.
+  return true;
+}
+
+}  // namespace
+
 // static
 std::unique_ptr<CurtainMode> CurtainMode::Create(
     scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
     base::WeakPtr<ClientSessionControl> client_session_control) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return std::make_unique<CurtainModeWayland>();
 }
 
 }  // namespace remoting
