7683bc9b8397c757298a72a8456b6b28143b869c
Yuzhu Shen <yzshen@chromium.org>
2022-09-07 16:32:47 +0000

    Re-enable SitePerProcessHighDPIHitTestBrowserTest for lacros.
    Given that they could pass 200 runs on try bots, the flakiness might have been addressed.
    
    This reverts commit d89f8488e45ca3f4fbaec82d4f72998338496630.
    
    Bug: 1350913
    Change-Id: I4378457ec3a118d1a35ec1a84483d7020b0a1082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3877238
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044018}

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -852,11 +852,6 @@ class SitePerProcessHighDPIHitTestBrowserTest
 
   SitePerProcessHighDPIHitTestBrowserTest() {}
 
-// TODO(crbug.com/1350913): Re-enable when fixed.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  void SetUp() override { GTEST_SKIP(); }
-#endif
-
  protected:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     SitePerProcessHitTestBrowserTest::SetUpCommandLine(command_line);
