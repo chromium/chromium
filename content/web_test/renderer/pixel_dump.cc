2478eed6a208fd6b01b2aa58437053999c5577fd
Thorben Troebst <troebst@google.com>
2022-09-07 17:09:07 +0000

    Reland: Add support for WPT "reftest-pages" meta tag.
    This adds support for the "reftest-pages" HTML meta tag that is used
    by WPT print reftests to specify which pages should be printed. This
    has the format `<meta name=reftest-pages content="ranges">` where
    "ranges" is a list of pages and page ranges like "-3,5,8,10-".
    
    Note that we do not yet run WPT print reftests correctly so the test
    is required to call `testRunner.setPrinting()`.
    
    Reland: SplitStringPiece was called on the result of a function call
    creating undefined behavior - fixed by extracting the result into
    a named variable to extend the lifetime.
    
    Bug: 1090628
    Change-Id: Ic4ddbd6e1ed93e9a556ceefce93e55416aceaeae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878862
    Commit-Queue: Thorben Troebst <troebst@google.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044036}

--- content/web_test/renderer/pixel_dump.cc
+++ content/web_test/renderer/pixel_dump.cc
@@ -21,6 +21,8 @@
 #include "mojo/public/cpp/bindings/remote.h"
 #include "printing/metafile_skia.h"
 #include "printing/mojom/print.mojom.h"
+#include "printing/page_number.h"
+#include "printing/page_range.h"
 #include "printing/print_settings.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "skia/ext/platform_canvas.h"
@@ -29,6 +31,7 @@
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/public/web/web_frame.h"
 #include "third_party/blink/public/web/web_frame_widget.h"
 #include "third_party/blink/public/web/web_local_frame.h"
@@ -40,7 +43,8 @@
 
 namespace content {
 
-SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame) {
+SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame,
+                            const printing::PageRanges& page_ranges) {
   auto* frame_widget = web_frame->LocalRoot()->FrameWidget();
   frame_widget->UpdateAllLifecyclePhases(blink::DocumentUpdateReason::kTest);
 
@@ -48,8 +52,11 @@ SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame) {
 
   uint32_t page_count =
       web_frame->PrintBegin(page_size_in_pixels, blink::WebNode());
+
+  blink::WebVector<uint32_t> pages(
+      printing::PageNumber::GetPages(page_ranges, page_count));
   gfx::Size spool_size =
-      web_frame->SpoolSizeInPixelsForTesting(page_size_in_pixels, page_count);
+      web_frame->SpoolSizeInPixelsForTesting(page_size_in_pixels, pages);
 
   bool is_opaque = false;
 
@@ -65,7 +72,8 @@ SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame) {
                                   printing::PrintSettings::NewCookie());
   cc::SkiaPaintCanvas canvas(bitmap);
   canvas.SetPrintingMetafile(&metafile);
-  web_frame->PrintPagesForTesting(&canvas, page_size_in_pixels, spool_size);
+  web_frame->PrintPagesForTesting(&canvas, page_size_in_pixels, spool_size,
+                                  &pages);
   web_frame->PrintEnd();
   return bitmap;
 }
--- content/web_test/renderer/pixel_dump.h
+++ content/web_test/renderer/pixel_dump.h
@@ -5,6 +5,8 @@
 #ifndef CONTENT_WEB_TEST_RENDERER_PIXEL_DUMP_H_
 #define CONTENT_WEB_TEST_RENDERER_PIXEL_DUMP_H_
 
+#include "printing/page_range.h"
+
 class SkBitmap;
 
 namespace blink {
@@ -15,7 +17,8 @@ namespace content {
 
 // Goes through a test-only path to dump the frame's pixel output as if it was
 // printed.
-SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame);
+SkBitmap PrintFrameToBitmap(blink::WebLocalFrame* web_frame,
+                            const printing::PageRanges& pages);
 
 }  // namespace content
 
