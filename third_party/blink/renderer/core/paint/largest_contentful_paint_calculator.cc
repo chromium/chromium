0850c9caaced5125b7fb13b85528562dd1a48ac5
Noam Rosenthal <nrosenthal@chromium.org>
2022-09-07 11:30:08 +0000

    Null-check DOMWindow in LargestContentfulPaint code
    The missing null-check was causing a crash.
    
    Fixed: 1360361
    Bug: 1360361
    Change-Id: I04b7c7b5d26ca2b264617b2f22ab724ce5f90d65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878163
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1043911}

--- third_party/blink/renderer/core/paint/largest_contentful_paint_calculator.cc
+++ third_party/blink/renderer/core/paint/largest_contentful_paint_calculator.cc
@@ -81,11 +81,6 @@ void LargestContentfulPaintCalculator::UpdateLargestContentfulImage(
   const AtomicString& image_id =
       image_element ? image_element->GetIdAttribute() : AtomicString();
 
-  if (!largest_image->origin_clean) {
-    UseCounter::Count(window_performance_->DomWindow()->document(),
-                      WebFeature::kLCPCandidateImageFromOriginDirtyStyle);
-  }
-
   window_performance_->OnLargestContentfulPaintUpdated(
       expose_paint_time_to_api ? largest_image->paint_time : base::TimeTicks(),
       largest_image->first_size, largest_image->load_time,
@@ -95,6 +90,11 @@ void LargestContentfulPaintCalculator::UpdateLargestContentfulImage(
 
   // TODO: update trace value with animated frame data
   if (LocalDOMWindow* window = window_performance_->DomWindow()) {
+    if (!largest_image->origin_clean) {
+      UseCounter::Count(window->document(),
+                        WebFeature::kLCPCandidateImageFromOriginDirtyStyle);
+    }
+
     TRACE_EVENT_MARK_WITH_TIMESTAMP2(kTraceCategories, kLCPCandidate,
                                      largest_image->paint_time, "data",
                                      ImageCandidateTraceData(largest_image),
