b7aa3c84d3254cd31620f3f5a8e00a93365d6a7a
Benjamin Williams <bwwilliams@google.com>
2022-09-08 15:22:44 +0000

    Create StandardPromoActionHandler.
    StandardPromoActionHandler is the new protocol that feature teams will
    confirm to when creating standard display promos.
    
    It enables feature teams to provide functionality for the following:
    
    1. "Primary Action" button.
    2. "Dismiss" button
    3. "Secondary Action" button.
    4. "Learn More" button.
    5. "Tertiary Action" button.
    
    Change-Id: I1fddf9907cb255e0b9aee423d9a48feeb528ce7b
    Bug: 1361360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876927
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044559}

---
+++ ios/chrome/browser/ui/promos_manager/standard_promo_action_handler.h
@@ -0,0 +1,31 @@
+// Copyright 2022 The Chromium Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ACTION_HANDLER_H_
+#define IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ACTION_HANDLER_H_
+
+#import <Foundation/Foundation.h>
+
+@protocol StandardPromoActionHandler <NSObject>
+
+// The "Primary Action" was touched.
+- (void)standardPromoPrimaryAction;
+
+@optional
+
+// The "Dismiss" button was touched.
+- (void)standardPromoDismissAction;
+
+// The "Secondary Action" was touched.
+- (void)standardPromoSecondaryAction;
+
+// The "Learn More" button was touched.
+- (void)standardPromoLearnMoreAction;
+
+// The "Tertiary Action" was touched.
+- (void)standardPromoTertiaryAction;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_PROMOS_MANAGER_STANDARD_PROMO_ACTION_HANDLER_H_
