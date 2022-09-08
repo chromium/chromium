01528bb488fded191b4de8db1650656c285721fb
Avi Drissman <avi@chromium.org>
2022-09-08 15:21:46 +0000

    Update tools/boilerplate.py
    Make it generate a conforming copyright header.
    
    Bug: 1098010
    Change-Id: Ia2751c8c0fdbd44b0bb147b3683abf63f9137220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879908
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044558}

--- tools/boilerplate.py
+++ tools/boilerplate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2014 The Chromium Authors. All rights reserved.
+# Copyright 2014 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -17,7 +17,7 @@ import os.path
 import sys
 
 LINES = [
-    f'Copyright {date.today().year} The Chromium Authors.',
+    f'Copyright {date.today().year} The Chromium Authors',
     'Use of this source code is governed by a BSD-style license that can be',
     'found in the LICENSE file.'
 ]
