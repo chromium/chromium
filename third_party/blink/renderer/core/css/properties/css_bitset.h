2da42f7887c87c664d147b9eec36c774a106b9b6
Yuheng Huang <yuhengh@google.com>
2022-09-08 17:49:27 +0000

    Revert "Faster checking of CSS property exposure."
    This reverts commit bafff744058860ae3d7eb71372cfa48f1aca6912.
    
    Reason for revert: failing tests consistently crbug.com/1361408
    
    Original change's description:
    > Faster checking of CSS property exposure.
    >
    > For properties that are not behind runtime flags (which are nearly all,
    > in practice), we can avoid resolving and looking them up to check the
    > exposure. This saves a little time in parsing.
    >
    > Microbenchmarks (Zen 2, LTO but no PGO):
    >
    > Parse (µs)             Before     After    Perf      95% CI (BCa)
    > =================== ========= ========= ======= =================
    > ECommerce                1653      1618   +2.2%  [ +1.3%,  +2.5%]
    > Encyclopedia             8611      8377   +2.8%  [ +2.3%,  +3.3%]
    > Extension                1513      1496   +1.1%  [ +0.3%,  +2.0%]
    > News                     9826      9572   +2.7%  [ +2.3%,  +3.0%]
    > Search                   5862      5680   +3.2%  [ +2.6%,  +4.0%]
    > Social1                 19576     19320   +1.3%  [ +0.7%,  +1.9%]
    > Social2                   706       684   +3.2%  [ +2.3%,  +4.6%]
    > Sports                  66445     64772   +2.6%  [ +2.1%,  +3.1%]
    > Video                   43666     42006   +4.0%  [ +1.4%,  +6.1%]
    > Geometric mean                            +2.6%  [ +2.0%,  +3.0%]
    >
    > Change-Id: I50a7c4719645ec83f8d46a54a68df1e6e97c4dc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3878346
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1044495}
    
    Change-Id: I2015f654b9fbfc2526f27f3f08fc4375b7a34bfa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884256
    Auto-Submit: Yuheng Huang <yuhengh@google.com>
    Owners-Override: Yuheng Huang <yuhengh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044649}

--- third_party/blink/renderer/core/css/properties/css_bitset.h
+++ third_party/blink/renderer/core/css/properties/css_bitset.h
@@ -6,7 +6,6 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PROPERTIES_CSS_BITSET_H_
 
 #include <algorithm>
-#include <array>
 #include <cstring>
 #include <initializer_list>
 
@@ -42,14 +41,13 @@ class CORE_EXPORT CSSBitsetBase {
       Set(id);
   }
 
-  // This exists really only for generated code to be able to create global
-  // (const) objects without requiring global constructors.
-  explicit constexpr CSSBitsetBase(const std::array<uint64_t, kChunks>& chunks)
-      : chunks_{chunks} {}
+  void operator=(const CSSBitsetBase& o) {
+    std::memcpy(chunks_, o.chunks_, sizeof(chunks_));
+  }
 
-  CSSBitsetBase& operator=(const CSSBitsetBase& o) = default;
-
-  bool operator==(const CSSBitsetBase& o) const { return chunks_ == o.chunks_; }
+  bool operator==(const CSSBitsetBase& o) const {
+    return std::memcmp(chunks_, o.chunks_, sizeof(chunks_)) == 0;
+  }
   bool operator!=(const CSSBitsetBase& o) const { return !(*this == o); }
 
   inline void Set(CSSPropertyID id) {
@@ -78,7 +76,7 @@ class CORE_EXPORT CSSBitsetBase {
     return false;
   }
 
-  inline void Reset() { std::memset(chunks_.data(), 0, sizeof(chunks_)); }
+  inline void Reset() { std::memset(chunks_, 0, sizeof(chunks_)); }
 
   // Yields the CSSPropertyIDs which are set.
   class Iterator {
@@ -138,11 +136,11 @@ class CORE_EXPORT CSSBitsetBase {
     uint64_t chunk_ = 0;
   };
 
-  Iterator begin() const { return Iterator(chunks_.data(), 0, 0); }
-  Iterator end() const { return Iterator(chunks_.data(), kChunks, kBits); }
+  Iterator begin() const { return Iterator(chunks_, 0, 0); }
+  Iterator end() const { return Iterator(chunks_, kChunks, kBits); }
 
  private:
-  std::array<uint64_t, kChunks> chunks_;
+  uint64_t chunks_[kChunks];
 };
 
 using CSSBitset = CSSBitsetBase<kNumCSSPropertyIDs>;
