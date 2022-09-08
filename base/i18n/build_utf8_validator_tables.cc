e4622aaeccea84652488d1822c28c78b7115684f
Avi Drissman <avi@chromium.org>
2022-09-08 20:36:06 +0000

    Update copyright headers in base/
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c34.
    
    An earlier version of this CL, https://crrev.com/c/3879904,
    was reverted due to an issue that was resolved with
    https://crrev.com/c/3881211.
    
    No-Try: true
    Bug: 1098010
    Change-Id: Ibd6ffb97e66835bc299fe7b85876c3e2927b2345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883841
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044747}

--- base/i18n/build_utf8_validator_tables.cc
+++ base/i18n/build_utf8_validator_tables.cc
@@ -1,4 +1,4 @@
-// Copyright 2014 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,7 +52,7 @@ const char kHelpText[] =
     "Usage: build_utf8_validator_tables [ --help ] [ --output=<file> ]\n";
 
 const char kProlog[] =
-    "// Copyright 2013 The Chromium Authors. All rights reserved.\n"
+    "// Copyright 2013 The Chromium Authors\n"
     "// Use of this source code is governed by a BSD-style license that can "
     "be\n"
     "// found in the LICENSE file.\n"
