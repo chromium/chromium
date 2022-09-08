87200c6ecc5d36a6631eab1de0448e04711b445b
Joshua Pawlicki <waffles@chromium.org>
2022-09-08 18:22:00 +0000

    Updater: Fix O4 privileged helper crashing due to uninitialized tpool.
    Fixed: 1361382
    Change-Id: I3d4844230b46374f4ecc396abcfd235556d85e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883616
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044672}

--- chrome/updater/mac/privileged_helper/main.mm
+++ chrome/updater/mac/privileged_helper/main.mm
@@ -5,6 +5,7 @@
 #include "base/command_line.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/task/single_thread_task_executor.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/threading/platform_thread.h"
 #include "chrome/updater/mac/privileged_helper/server.h"
 
@@ -12,5 +13,9 @@ int main(int argc, const char* argv[]) {
   base::PlatformThread::SetName("UpdaterHelperMain");
   base::CommandLine::Init(argc, argv);
   base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
+  base::ThreadPoolInstance::Create("UpdaterHelperThreadPool");
+  base::ThreadPoolInstance::Get()->Start({1});
+  const base::ScopedClosureRunner shutdown_thread_pool(
+      base::BindOnce([]() { base::ThreadPoolInstance::Get()->Shutdown(); }));
   return updater::PrivilegedHelperServerInstance()->Run();
 }
