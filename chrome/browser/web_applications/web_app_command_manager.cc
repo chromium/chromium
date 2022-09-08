af238f3fdc1b65f39108e865f20c78d5be00aee1
Daniel Murphy <dmurph@chromium.org>
2022-09-08 01:48:47 +0000

    [dPWA] Fixing command log from disappearing
    Change-Id: I9629909a18778b428f67b22af9a001979ecbbf03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3880783
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044305}

--- chrome/browser/web_applications/web_app_command_manager.cc
+++ chrome/browser/web_applications/web_app_command_manager.cc
@@ -196,8 +196,8 @@ void WebAppCommandManager::NotifySyncSourceRemoved(
 
 base::Value WebAppCommandManager::ToDebugValue() {
   base::Value::List command_log;
-  for (auto& command_value : command_debug_log_) {
-    command_log.Append(std ::move(command_value));
+  for (const auto& command_value : command_debug_log_) {
+    command_log.Append(command_value.Clone());
   }
 
   base::Value::List queued;
