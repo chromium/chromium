b4a6fc6fdfced8bba2bfd1d063c0829b3d266212
Jason Zhang <jiajunz@google.com>
2022-09-08 22:00:21 +0000

    [CrOS Hotspot] Introduce CrosHotspotConfig and CrosHotspotConfigObserver
    This CL creates a new CrosHotspotConfig Mojo interface and an
    implementation of that interface. The implementation provides cached
    state for hotspot, set hotspot configuration and enable/disable hotspot.
    
    It also implements CrosHotspotConfigObserver for hotspot info updates.
    
    Change-Id: I906941c34583dcc094066e03f9a2ea85ac909e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3871789
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1044796}

---
+++ chromeos/services/hotspot_config/public/cpp/cros_hotspot_config_observer.cc
@@ -0,0 +1,14 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/hotspot_config/public/cpp/cros_hotspot_config_observer.h"
+
+namespace chromeos::hotspot_config {
+
+void CrosHotspotConfigObserver::OnHotspotInfoChanged() {}
+
+void CrosHotspotConfigObserver::OnHotspotStateFailed(
+    const std::string& error_code) {}
+
+}  // namespace chromeos::hotspot_config
