087c6f527f17ccb5222c2b8a6e8f396c6aaebb86
Gauthier Ambard <gambard@chromium.org>
2022-09-08 18:29:13 +0000

    [iOS] Change pipes to backticks in /ios/web/favicon
    Please review the CL to ensure that nothing is broken and the script
    didn't miss some pipes.
    
    This should allow us to have a better formatting in Xcode.
    
    This CL was uploaded by git cl split.
    
    R=ajuma@chromium.org
    
    Bug: 1311955
    Change-Id: Ieb9d53bfd25e6780be2ab347529d720732b8b0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883716
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044678}

--- ios/web/favicon/favicon_util.h
+++ ios/web/favicon/favicon_util.h
@@ -10,8 +10,8 @@
 
 namespace web {
 
-// Extracts the favicon url out of |favicons| and put them into
-// the |out_parameter|. The |page_origin| is used to get the default favicon.ico
+// Extracts the favicon url out of `favicons` and put them into
+// the `out_parameter`. The `page_origin` is used to get the default favicon.ico
 // at the root of the page if there is none in the message. The message is
 // structured as containing a list of favicons containing the href, rel and
 // sizes attributes of the favicons. Returns whether the extraction was
