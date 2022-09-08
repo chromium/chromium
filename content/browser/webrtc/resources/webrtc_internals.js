5f5fb37c1ad88720ca9ed7bfd85377030fb416c5
Philipp Hancke <phancke@microsoft.com>
2022-09-08 12:18:30 +0000

    webrtc-internals: remove extmap-allow-mixed deprecation notice
    which was removed in M94
    
    BUG=chromium:1199623
    
    Change-Id: Ica475e25d4a40f61db25d8a17710b4b4632bae68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879446
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1044494}

--- content/browser/webrtc/resources/webrtc_internals.js
+++ content/browser/webrtc/resources/webrtc_internals.js
@@ -337,16 +337,6 @@ function addPeerConnection(data) {
   const deprecationNotices = document.createElement('ul');
   if (data.rtcConfiguration) {
     deprecationNotices.className = 'peerconnection-deprecations';
-    if (data.rtcConfiguration.indexOf('extmapAllowMixed: false') !== -1) {
-      // Hard deprecation, setting "false" will no longer work.
-      appendChildWithText(deprecationNotices, 'li',
-        'Note: The RTCPeerConnection offerAllowExtmapMixed ' +
-        'option is a non-standard feature. This feature will be removed ' +
-        'in M93 (Canary: July 15, 2021; Stable: August 24, 2021). For ' +
-        'interoperability with legacy WebRTC versions that throw errors ' +
-        'when attempting to parse the a=extmap-allow-mixed line in the ' +
-        'SDP remove the line from the SDP during signalling.');
-    }
   }
   if (data.constraints) {
     if (data.constraints.indexOf('enableDtlsSrtp:') !== -1) {
--- third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
@@ -399,8 +399,6 @@ String SerializeConfiguration(
   if (usesInsertableStreams) {
     result.Append(", encodedInsertableStreams: true");
   }
-  result.Append(", extmapAllowMixed: ");
-  result.Append(SerializeBoolean(config.offer_extmap_allow_mixed));
   result.Append(" }");
   return result.ToString();
 }
