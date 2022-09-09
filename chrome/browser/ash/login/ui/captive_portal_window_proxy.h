2cc387c407978d6a97e047c90472e80caf012e3e
Danila Kuzmin <dkuzmin@google.com>
2022-09-09 09:44:52 +0000

    [CaptivePortalWindowProxy]: Postpone closing of the window
    CaptivePortalWindowProxy::Close might be called during
    NavigationControllerImpl::NotifyNavigationEntryCommitted which
    invalidates NavigationControllerDelegate in the middle of the function.
    
    Postpone the Close on the current thread, that should prevent the
    early pointer invalidation and fix the crash.
    
    Bug: 1267477
    Change-Id: I5e3f6368858011692a56ae4dacda0a8d0e505267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883297
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045001}

--- chrome/browser/ash/login/ui/captive_portal_window_proxy.h
+++ chrome/browser/ash/login/ui/captive_portal_window_proxy.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -102,18 +103,15 @@ class CaptivePortalWindowProxy : public views::WidgetObserver {
   // notifications from `widget_` and resets it.
   void DetachFromWidget(views::Widget* widget);
 
-  CaptivePortalView* captive_portal_view_for_testing() {
-    return captive_portal_view_for_testing_;
-  }
-
   Profile* profile_ = ProfileHelper::GetSigninProfile();
   content::WebContents* web_contents_;
   views::Widget* widget_ = nullptr;
 
   std::unique_ptr<CaptivePortalView> captive_portal_view_;
-  CaptivePortalView* captive_portal_view_for_testing_ = nullptr;
 
   base::ObserverList<Observer> observers_;
+
+  base::WeakPtrFactory<CaptivePortalWindowProxy> weak_factory_{this};
 };
 
 }  // namespace ash
