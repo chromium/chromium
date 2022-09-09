3fd3e23fba49863df52cb848993f46ee891fe16f
dpapad <dpapad@chromium.org>
2022-09-09 07:17:17 +0000

    WebUI: Update references to old cr_elements URLs in ui/file_manager/
    The old URLs have been kept as aliases to make it possible to update the
    codebase to the new URLs incrementally.
    
    Bug: 1184053
    Change-Id: I9b8c305543cb76b5b9ad33c3d6e744945be5d74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3879776
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044954}

--- ui/file_manager/file_manager/foreground/elements/files_format_dialog.js
+++ ui/file_manager/file_manager/foreground/elements/files_format_dialog.js
@@ -5,10 +5,10 @@
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
-import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/cr_elements/icons.html.js';
 import 'chrome://resources/cr_elements/md_select.css.js';
-import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
