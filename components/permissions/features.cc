9b34eeb0dfb22aafb99c2c66caefa2bf0deb3a94
Illia Klimov <elklm@google.com>
2022-09-09 15:11:54 +0000

    Disable the quiet permission chip on Android.
    The quiet permission chip is not supported on Android. The feature can be disabled.
    
    Bug: 1360560
    Change-Id: I501fbcbdef617f5ca15d9e646767073e1454fb4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3884042
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Quick-Run: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1045108}

--- components/permissions/features.cc
+++ components/permissions/features.cc
@@ -35,10 +35,16 @@ const base::Feature kOneTimeGeolocationPermission{
 const base::Feature kPermissionChip{"PermissionChip",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
+#if BUILDFLAG(IS_ANDROID)
+// Not supported on Android.
+const base::Feature kPermissionQuietChip{"PermissionQuietChip",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+#else
 // Enables a less prominent permission prompt that uses a chip in the location
 // bar. Requires chrome://flags/#quiet-notification-prompts to be enabled.
 const base::Feature kPermissionQuietChip{"PermissionQuietChip",
                                          base::FEATURE_ENABLED_BY_DEFAULT};
+#endif  // BUILDFLAG(IS_ANDROID)
 
 const base::Feature kPermissionChipAutoDismiss{
     "PermissionChipAutoDismiss", base::FEATURE_ENABLED_BY_DEFAULT};
