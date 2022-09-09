3e5b5be1df1be4257918feb7333c1f99843c3309
Avi Drissman <avi@chromium.org>
2022-09-09 02:09:09 +0000

    Fix the copyright header
    As per discussions with our lawyer, because a Yandex employee
    submitted this file to us as a direct contribution to a Chromium
    project covered by Google's Contributor License Agreement, this file
    is covered by the standard Chromium licensing terms.
    
    Bug: none
    Change-Id: I50c3fbdcb2374a7b79ccfa668dc6934268098dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883845
    Owners-Override: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1044900}

--- base/test/test_file_util_win_unittest.cc
+++ base/test/test_file_util_win_unittest.cc
@@ -1,4 +1,6 @@
-// Copyright 2022 Yandex LLC. All rights reserved.
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "base/test/test_file_util.h"
 
