79792884537b6da3c6db47df90cb53441c894b83
Andrey Zaytsev <andzaytsev@google.com>
2022-09-09 18:49:09 +0000

    Add StorageKey to StorageUsageInfo
    This is used to add partitioned storage support to things like LocalStorage::GetUsage (see crrev.com/c/3884041).
    
    Bug: 1361545
    Change-Id: I0650aed04b101cf5242f35436e1b17d74a03446b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3883898
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1045253}

--- content/public/browser/storage_usage_info.h
+++ content/public/browser/storage_usage_info.h
@@ -9,6 +9,7 @@
 
 #include "base/time/time.h"
 #include "content/common/content_export.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/origin.h"
 
 namespace content {
@@ -16,17 +17,32 @@ namespace content {
 // Used to report per-origin storage info for a storage type. The storage type
 // (Cache API, Indexed DB, Local Storage, etc) is implied by context.
 struct CONTENT_EXPORT StorageUsageInfo {
+  StorageUsageInfo(const blink::StorageKey& storage_key,
+                   int64_t total_size_bytes,
+                   base::Time last_modified)
+      : storage_key(storage_key),
+        origin(storage_key.origin()),
+        total_size_bytes(total_size_bytes),
+        last_modified(last_modified) {}
+
+  // DEPRECATED: Use the above StorageKey constructor
+  // (https://crbug.com/1361545).
   StorageUsageInfo(const url::Origin& origin,
                    int64_t total_size_bytes,
                    base::Time last_modified)
-      : origin(origin),
+      : storage_key(blink::StorageKey(origin)),
+        origin(origin),
         total_size_bytes(total_size_bytes),
         last_modified(last_modified) {}
 
   // For assignment into maps without wordy emplace(std::make_pair()) syntax.
   StorageUsageInfo() = default;
 
+  // The storage key this object is describing.
+  blink::StorageKey storage_key;
+
   // The origin this object is describing.
+  // DEPRECATED, use storage_key instead (https://crbug.com/1361545).
   url::Origin origin;
 
   // The total size, including resources, in bytes.
