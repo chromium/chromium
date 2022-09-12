3751234482eabd353c4f133c6f88fe0d9a04f3d0
Kevin Haslett <khaslett@chromium.org>
2022-09-12 23:04:13 +0000

    Enable UseMultipleOverlays feature by default on Ash
    This is in preparation of fully launching the feature.
    
    Analysis doc of Finch stable 1% results on M104: go/cros-mo-results
    
    Bug: 1309995
    Change-Id: Ie07d2161143202a5cdd49af089ffacd1538355b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3885835
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kevin Haslett <khaslett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046093}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -50,8 +50,14 @@ const base::Feature kEnableOverlayPrioritization {
 #endif
 };
 
-const base::Feature kUseMultipleOverlays{"UseMultipleOverlays",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kUseMultipleOverlays {
+  "UseMultipleOverlays",
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 const char kMaxOverlaysParam[] = "max_overlays";
 
 const base::Feature kDelegatedCompositing {
@@ -344,7 +350,7 @@ int MaxOverlaysConsidered() {
   }
 
   return base::GetFieldTrialParamByFeatureAsInt(kUseMultipleOverlays,
-                                                kMaxOverlaysParam, 2);
+                                                kMaxOverlaysParam, 8);
 }
 
 bool ShouldVideoDetectorIgnoreNonVideoFrames() {
