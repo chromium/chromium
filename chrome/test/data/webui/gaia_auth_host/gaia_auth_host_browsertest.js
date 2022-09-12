07041b7c56bae471067b6fa264d284c5bcd5a365
Renato Silva <rrsilva@google.com>
2022-09-12 23:11:44 +0000

    [SAML] Move saml password attributes test
    Move the SAML Password Attributes test into the new modular test
    structure of gaia_auth_host.
    
    Bug: 1184731, 1350059, b/245282934
    Change-Id: I69eb268d1147ef47abdbad68dae5d2b873bf4605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876273
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1046096}

--- chrome/test/data/webui/gaia_auth_host/gaia_auth_host_browsertest.js
+++ chrome/test/data/webui/gaia_auth_host/gaia_auth_host_browsertest.js
@@ -26,8 +26,9 @@ var GaiaAuthHostBrowserTest = class extends testing.Test {
 };
 
 [['PasswordChangeAuthenticator', 'password_change_authenticator_test.js'],
- ['SamlUsernameAutofill', 'saml_username_autofill_test.js'],
+ ['SamlPasswordAttributes', 'saml_password_attributes_test.js'],
  ['SamlTimestamps', 'saml_timestamps_test.js'],
+ ['SamlUsernameAutofill', 'saml_username_autofill_test.js'],
 ].forEach(test => registerTest(...test));
 
 // Common WebUI pattern for test registration.
