faf47829d94d2aa35305c1ff5fff1bcd613c54b5
Peter Birk Pakkenberg <pbirk@chromium.org>
2022-09-12 22:07:50 +0000

    Add Persistent Origin Trial storage
    This CL introduces a persistence provider based on PrefService.
    
    This CL is part of the persistent origin trials implementation.
    Full implementation plan here:
    
    https: //docs.google.com/document/d/1v4cwALXi33VgYrdqFCldDT0N0BuwiXzESt7NIJIiOTs/edit?usp=sharing
    Bug: 1257579
    Change-Id: I9038b4c21f7e12a458e0e2798b4c8aa45a3c06e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3784791
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046054}

--- components/origin_trials/browser/origin_trials.cc
+++ components/origin_trials/browser/origin_trials.cc
@@ -43,6 +43,9 @@ void OriginTrials::PersistTrialsFromTokens(
     const url::Origin& origin,
     const base::span<const base::StringPiece> header_tokens,
     const base::Time current_time) {
+  if (origin.opaque())
+    return;
+
   base::flat_set<PersistedTrialToken> enabled_persistent_trial_tokens;
 
   for (const base::StringPiece token : header_tokens) {
@@ -66,6 +69,9 @@ base::flat_set<std::string> OriginTrials::GetPersistedTrialsForOriginWithMatch(
     const url::Origin& origin,
     const base::Time current_time,
     const absl::optional<const base::StringPiece> trial_name_match) const {
+  if (origin.opaque())
+    return {};
+
   base::flat_set<PersistedTrialToken> saved_tokens =
       persistence_provider_->GetPersistentTrialTokens(origin);
 
