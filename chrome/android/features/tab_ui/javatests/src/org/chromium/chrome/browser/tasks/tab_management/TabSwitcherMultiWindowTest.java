bea99ab0a38a61a5e50d0577e3b0b7e95f73e59a
Wenyu Fu <wenyufu@chromium.org>
2022-09-13 18:19:58 +0000

    [Sheriff] Disable TabSwitcherMultiWindowTest#testMoveLastIncognitoTab
    Bug: 1363248
    Change-Id: I526c611ea77784ea92767b6a49d7a268801cac17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893726
    Owners-Override: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046465}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMultiWindowTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMultiWindowTest.java
@@ -36,6 +36,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -244,6 +245,7 @@ public class TabSwitcherMultiWindowTest {
     @Test
     @MediumTest
     @RequiresApi(Build.VERSION_CODES.N)
+    @DisabledTest(message = "https://crbug.com/1363248")
     // clang-format off
     @Features.EnableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID,
         ChromeFeatureList.TAB_REPARENTING})
