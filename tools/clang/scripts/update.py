79dc4db20eea5e33ec5eb863f6e6ec448a982d85
Nico Weber <thakis@chromium.org>
2022-09-13 17:07:45 +0000

    Roll clang llvmorg-16-init-4141-gf48931f3-2 : llvmorg-16-init-4438-gc922cac8-1
    Ran `./tools/clang/scripts/upload_revision.py c922cac868b0c6082bb35ea013969d8fc03a29e9`.
    
    Bug: 1362883
    Change-Id: I4ea7bf1054383903fe3dbb60809681664e2e8ee8
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst
    Cq-Include-Trybots: chromium/try:win7-rel
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3889980
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Alan Zhao <ayzhao@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1046430}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,8 +35,8 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-16-init-4141-gf48931f3'
-CLANG_SUB_REVISION = 2
+CLANG_REVISION = 'llvmorg-16-init-4438-gc922cac8'
+CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
 RELEASE_VERSION = '16.0.0'
