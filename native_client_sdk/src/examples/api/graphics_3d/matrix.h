5e16a5fca1416b7614a51a2f9cce0e8de808d048
Avi Drissman <avi@chromium.org>
2022-09-14 19:55:12 +0000

    Update copyright headers in native_client_sdk/
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95, in addition to hand-crafted searches
    to wrangle "Native Client".
    
    No-Try: true
    Bug: 1098010
    Change-Id: If08c3a3b8ea51b0374ddb49f9056d5edd376e2e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3894903
    Reviewed-by: Sam Clegg <sbc@chromium.org>
    Commit-Queue: Sam Clegg <sbc@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047069}

--- native_client_sdk/src/examples/api/graphics_3d/matrix.h
+++ native_client_sdk/src/examples/api/graphics_3d/matrix.h
@@ -1,7 +1,7 @@
 #ifndef EXAMPLES_API_GRAPHICS_3D_MATRIX_H_
 #define EXAMPLES_API_GRAPHICS_3D_MATRIX_H_
 
-/* Copyright (c) 2012 The Chromium Authors. All rights reserved.
+/* Copyright 2012 The Chromium Authors
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
