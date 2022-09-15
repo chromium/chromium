88849963a3adc629fc9ffc7e59f7e77f3f45d698
Steven Bennetts <stevenjb@chromium.org>
2022-09-15 23:52:59 +0000

    CaptivePortalDetector: Add CHECKs
    Some additional CHECKs and a State enum for additional CHECKs.
    This is part of an effort to track down crashes, see issue for details.
    
    Bug: 1361443
    Test: unit_tests, browser_tests, crash reporter
    Change-Id: I26d74de1de8db5dffd60bc7f232567c7a1a02c68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3901212
    Quick-Run: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047744}

--- components/captive_portal/core/captive_portal_detector.cc
+++ components/captive_portal/core/captive_portal_detector.cc
@@ -103,6 +103,7 @@ void CaptivePortalDetector::StartProbe(
   simple_loader_->SetAllowHttpErrorResults(true);
   network::SimpleURLLoader::BodyAsStringCallback callback = base::BindOnce(
       &CaptivePortalDetector::OnSimpleLoaderComplete, base::Unretained(this));
+  state_ = State::kProbe;
   simple_loader_->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
       loader_factory_, std::move(callback));
 }
@@ -114,13 +115,15 @@ void CaptivePortalDetector::Cancel() {
   // Cancel any pending calls to StartProbe().
   weak_factory_.InvalidateWeakPtrs();
 #endif
+  state_ = State::kCancelled;
 }
 
 void CaptivePortalDetector::OnSimpleLoaderComplete(
     std::unique_ptr<std::string> response_body) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(FetchingURL());
-  DCHECK(!detection_callback_.is_null());
+  CHECK_EQ(state_, State::kProbe);
+  CHECK(FetchingURL());
+  CHECK(!detection_callback_.is_null());
 
   int response_code = 0;
   net::HttpResponseHeaders* headers = nullptr;
@@ -131,6 +134,7 @@ void CaptivePortalDetector::OnSimpleLoaderComplete(
   }
   OnSimpleLoaderCompleteInternal(simple_loader_->NetError(), response_code,
                                  simple_loader_->GetFinalURL(), headers);
+  state_ = State::kCompleted;
 }
 
 void CaptivePortalDetector::OnSimpleLoaderCompleteInternal(
@@ -142,6 +146,7 @@ void CaptivePortalDetector::OnSimpleLoaderCompleteInternal(
   GetCaptivePortalResultFromResponse(net_error, response_code, url, headers,
                                      &results);
   simple_loader_.reset();
+  CHECK(detection_callback_);
   std::move(detection_callback_).Run(results);
 }
 
