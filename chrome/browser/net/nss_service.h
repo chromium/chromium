4eec94f9d775f6aa807cc97ddf177d105cc71180
Michael Ershov <miersh@google.com>
2022-09-15 10:20:09 +0000

    Introduce FakeNssService
    Introduce FakeNssService - a helper class to make it easier
    to initialize NSS for profiles in tests.
    
    Bug: b:231424338
    Test: None
    Change-Id: Id8730b8a93cd5d6b3bc757d6e2de5930b738892e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3876106
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1047391}

--- chrome/browser/net/nss_service.h
+++ chrome/browser/net/nss_service.h
@@ -50,7 +50,7 @@ class NssService : public KeyedService {
   // thread and then invoked.  While the returned getter must be invoked on
   // the IO thread, this method itself may only be invoked on the UI thread,
   // where the NssService lives.
-  NssCertDatabaseGetter CreateNSSCertDatabaseGetterForIOThread();
+  virtual NssCertDatabaseGetter CreateNSSCertDatabaseGetterForIOThread();
 
   // Unsafely returns the `NssCertDatabase` directly to the caller (on the UI
   // thread). This is unsafe, because if the `content::BrowserContext` / this
