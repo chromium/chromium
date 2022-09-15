51006fa50c56235528efe8106d05230b2ac111a7
Stephen Nusko <nuskos@chromium.org>
2022-09-15 12:59:01 +0000

    Allow AndroidToolbar message through privacy filtering
    approved in cl/474332887
    
    Change-Id: I078e3648a51729abd4316a3667bcbb1495230666
    Bug: 1324678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3898833
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047423}

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -364,6 +364,10 @@ constexpr int kChromeSqlDiagnosticsIndices[] = {1, 2, 3, 4,  5, 6,
 constexpr MessageInfo kChromeSqlDiagnostics = {kChromeSqlDiagnosticsIndices,
                                                nullptr};
 
+// Proto Message: AndroidToolbar
+constexpr int kAndroidToolbarIndices[] = {1, 2, 3, -1};
+constexpr MessageInfo kAndroidToolbar = {kAndroidToolbarIndices, nullptr};
+
 // Proto Message: TrackEvent
 constexpr int kTrackEventIndices[] = {
     1,    2,    3,    5,    6,    9,    10,   11,   12,   16,   17,   22,
@@ -371,7 +375,7 @@ constexpr int kTrackEventIndices[] = {
     35,   36,   38,   39,   40,   41,   42,   43,   47,   48,   1001, 1002,
     1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014,
     1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1031,
-    1032, 1033, 1034, 1036, 1038, 1039, -1};
+    1032, 1033, 1034, 1036, 1038, 1039, 1041, -1};
 constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     nullptr,
     nullptr,
@@ -438,7 +442,8 @@ constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     &kSiteInstanceGroup,
     nullptr,
     &kAndroidIPC,
-    &kChromeSqlDiagnostics};
+    &kChromeSqlDiagnostics,
+    &kAndroidToolbar};
 constexpr MessageInfo kTrackEvent = {kTrackEventIndices,
                                      kTrackEventComplexMessages};
 
