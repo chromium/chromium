c672704cf35de8dd4a6e39b58e84c5b78be5bf57
Ryan Hansberry <hansberry@chromium.org>
2022-09-15 18:59:58 +0000

    [Eche] Default-enable attestation cert generation.
    This will allow attestation certificates to be generated and uploaded
    in M107, so that they will be available on Day 1 of Eche launch (M108).
    
    TEST: Manually tested with custom logs to ensure code is run.
    TODO: Verify on phone that attestation certs are present and valid.
    
    Bug: 246842111
    Change-Id: I5fe339cddc0c2ea03c2244e07a646f2320866383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896891
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047599}

--- ash/services/device_sync/cryptauth_device_syncer_impl.cc
+++ ash/services/device_sync/cryptauth_device_syncer_impl.cc
@@ -234,7 +234,7 @@ void CryptAuthDeviceSyncerImpl::AttemptNextStep() {
       GetBluetoothAddress();
       return;
     case State::kWaitingForBluetoothAddress:
-      if (features::IsEcheSWAEnabled()) {
+      if (features::IsCrossDeviceAttestationCertificateGenerationEnabled()) {
         GetAttestationCertificates();
         return;
       }
@@ -758,7 +758,7 @@ void CryptAuthDeviceSyncerImpl::FinishAttempt(
   if (result_type == CryptAuthDeviceSyncResult::ResultType::kSuccess) {
     synced_bluetooth_address_tracker_->SetLastSyncedBluetoothAddress(
         local_better_together_device_metadata_.bluetooth_public_address());
-    if (features::IsEcheSWAEnabled()) {
+    if (features::IsCrossDeviceAttestationCertificateGenerationEnabled()) {
       if (are_attestation_certs_valid_) {
         attestation_certificates_syncer_->SetLastSyncTimestamp();
       }
