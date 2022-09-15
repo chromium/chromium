098756533733ea50b2dcb1c40d9a9e18d49febbe
Avi Drissman <avi@chromium.org>
2022-09-15 20:03:19 +0000

    Update copyright headers in skia/, services, sandbox/
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I316cc584728dda1a59695a33fbbb742eaf598bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899175
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047640}

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #ifndef SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_MEMORY_INSTRUMENTATION_OS_METRICS_H_
