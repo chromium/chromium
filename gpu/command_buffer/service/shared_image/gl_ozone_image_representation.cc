a51037b34d6e42752afe25a1830912a479ed0dd2
Saifuddin Hitawala <hitawala@chromium.org>
2022-09-15 17:18:08 +0000

    Add DumpWithoutCrashing if OzoneImageBacking::BeginAccess fails
    Recent change to OzoneImageBacking [1] may have triggered context
    lost issue due to an early return. This change adds a
    DumpWithoutCrashing instead to see if there are clients causing
    context losses and fix them.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3876745
    
    Bug: 1363435, b/243039477
    Change-Id: I375e233a0fe605634c1133ee9e7c3058b9d9f26e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3893345
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1047555}

--- gpu/command_buffer/service/shared_image/gl_ozone_image_representation.cc
+++ gpu/command_buffer/service/shared_image/gl_ozone_image_representation.cc
@@ -5,6 +5,7 @@
 #include "gpu/command_buffer/service/shared_image/gl_ozone_image_representation.h"
 #include <memory>
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "components/viz/common/resources/resource_format.h"
@@ -60,8 +61,13 @@ bool GLOzoneImageRepresentationShared::BeginAccess(
     bool& need_end_fence) {
   bool readonly = mode != GL_SHARED_IMAGE_ACCESS_MODE_READWRITE_CHROMIUM;
   std::vector<gfx::GpuFenceHandle> fences;
-  ozone_backing->BeginAccess(readonly, OzoneImageBacking::AccessStream::kGL,
-                             &fences, need_end_fence);
+  if (!ozone_backing->BeginAccess(readonly,
+                                  OzoneImageBacking::AccessStream::kGL, &fences,
+                                  need_end_fence)) {
+    // Ideally we should return early (false) here, but that breaks and leads to
+    // context loss so adding a temporary dump for debugging.
+    base::debug::DumpWithoutCrashing();
+  }
 
   // ChromeOS VMs don't support gpu fences, so there is no good way to
   // synchronize with GL.
