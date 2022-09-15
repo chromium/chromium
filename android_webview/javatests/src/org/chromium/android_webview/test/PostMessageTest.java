chromium/chromium

commit 57e4ed82bfbd12b6256d2fcadcb8026f38880380

[WebView] Refactor AppWebMessagePort.java to a native wrapper
This CL move the send/receive message over MessagePort logic from Java
to native code, and remove useless MessagePortDescriptor in Java.
The public API of AppWebMessagePort is not changed.

https://chromium-review.googlesource.com/c/chromium/src/+/3585178
is related, this CL will make encode/decode data in a consistent way.

Change-Id: Ie53c6b79386429ae49264927fd9e955c3d433a53
Bug: 1023334
Bug: 1324599
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3646943
Commit-Queue: Linyue He <linyhe@microsoft.com>
Reviewed-by: Richard Coles <torne@chromium.org>
Reviewed-by: Bo Liu <boliu@chromium.org>
Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1022416}

content/browser/android/app_web_message_port.cc
@@ -1,92 +1,192 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/public/browser/android/app_web_message_port.h"
+#include "content/browser/android/app_web_message_port.h"
+#include <memory>
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/memory/ptr_util.h"
+#include "content/browser/android/message_payload.h"
 #include "content/public/android/content_jni_headers/AppWebMessagePort_jni.h"
+#include "content/public/browser/android/message_port_helper.h"
+#include "content/public/browser/browser_thread.h"
+#include "mojo/public/cpp/bindings/message.h"
+#include "mojo/public/cpp/system/message_pipe.h"
+#include "third_party/blink/public/common/messaging/message_port_channel.h"
+#include "third_party/blink/public/common/messaging/message_port_descriptor.h"
 #include "third_party/blink/public/common/messaging/string_message_codec.h"
+#include "third_party/blink/public/common/messaging/transferable_message.h"
+#include "third_party/blink/public/common/messaging/transferable_message_mojom_traits.h"
+#include "third_party/blink/public/mojom/messaging/transferable_message.mojom.h"
 
-namespace content {
+namespace content::android {
 
-namespace AppWebMessagePort {
+base::android::ScopedJavaLocalRef<jobjectArray> CreateJavaMessagePort(
+    std::vector<blink::MessagePortDescriptor> descriptors) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  std::vector<base::android::ScopedJavaLocalRef<jobject>> j_descriptors;
+  j_descriptors.reserve(descriptors.size());
+  for (auto& descriptor : descriptors) {
+    j_descriptors.push_back(AppWebMessagePort::Create(std::move(descriptor)));
+  }
+
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return base::android::ToTypedJavaArrayOfObjects(
+      env, base::make_span(j_descriptors),
+      base::android::GetClass(
+          env, kClassPath_org_chromium_content_browser_AppWebMessagePort));
+}
 
 // static
-std::vector<blink::MessagePortDescriptor> UnwrapJavaArray(
+base::android::ScopedJavaLocalRef<jobject> AppWebMessagePort::Create(
+    blink::MessagePortDescriptor&& descriptor) {
+  auto ptr = base::WrapUnique(new AppWebMessagePort(std::move(descriptor)));
+  JNIEnv* env = base::android::AttachCurrentThread();
+  auto* raw_ptr = ptr.get();
+  auto j_obj = Java_AppWebMessagePort_Constructor(
+      env, reinterpret_cast<intptr_t>(ptr.release()));
+  raw_ptr->j_obj_ = JavaObjectWeakGlobalRef(env, j_obj);
+  return j_obj;
+}
+
+// static
+std::vector<blink::MessagePortDescriptor> AppWebMessagePort::Release(
     JNIEnv* env,
     const base::android::JavaRef<jobjectArray>& jports) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   std::vector<blink::MessagePortDescriptor> ports;
   if (!jports.is_null()) {
     for (auto jport : jports.ReadElements<jobject>()) {
-      jlong port_ptr =
-          Java_AppWebMessagePort_releaseNativeMessagePortDescriptor(env, jport);
+      jlong port_ptr = Java_AppWebMessagePort_getNativeObj(env, jport);
       // Ports are heap allocated native objects. Since we are taking ownership
       // of the object from the Java code we are responsible for cleaning it up.
-      std::unique_ptr<blink::MessagePortDescriptor> port = base::WrapUnique(
-          reinterpret_cast<blink::MessagePortDescriptor*>(port_ptr));
-      ports.push_back(std::move(*port));
+      std::unique_ptr<AppWebMessagePort> port =
+          base::WrapUnique(reinterpret_cast<AppWebMessagePort*>(port_ptr));
+      ports.emplace_back(port->PassPort());
     }
   }
   return ports;
 }
 
-// static
-base::android::ScopedJavaGlobalRef<jobjectArray> WrapJavaArray(
+AppWebMessagePort::AppWebMessagePort(blink::MessagePortDescriptor&& descriptor)
+    : runner_(base::ThreadTaskRunnerHandle::Get()),
+      descriptor_(std::move(descriptor)) {
+  // AppWebMessagePort can only be created on main thread.
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  connector_ = std::make_unique<mojo::Connector>(
+      descriptor_.TakeHandleToEntangleWithEmbedder(),
+      mojo::Connector::SINGLE_THREADED_SEND);
+  connector_->set_connection_error_handler(
+      base::BindOnce(&AppWebMessagePort::OnPipeError, base::Unretained(this)));
+}
+
+AppWebMessagePort::~AppWebMessagePort() {
+  DCHECK(runner_->BelongsToCurrentThread());
+  GiveDisentangledHandleIfNeeded();
+  JNIEnv* env = base::android::AttachCurrentThread();
+  Java_AppWebMessagePort_nativeDestroyed(env, GetJavaObj(env));
+}
+
+// JNI
+void AppWebMessagePort::PostMessage(
     JNIEnv* env,
-    std::vector<blink::MessagePortDescriptor> descriptors) {
-  // Convert to an array of raw blink::MessagePortDescriptor pointers. Ownership
-  // of these objects is passed to Java.
-  std::vector<int64_t> descriptor_ptrs;
-  descriptor_ptrs.reserve(descriptors.size());
-  for (size_t i = 0; i < descriptors.size(); ++i) {
-    blink::MessagePortDescriptor* descriptor =
-        new blink::MessagePortDescriptor(std::move(descriptors[i]));
-    descriptor_ptrs.push_back(reinterpret_cast<int64_t>(descriptor));
+    const base::android::JavaParamRef<jobject>& j_message_payload,
+    const base::android::JavaParamRef<jobjectArray>& j_ports) {
+  DCHECK(runner_->BelongsToCurrentThread());
+  DCHECK(descriptor_.IsValid());
+  DCHECK(connector_);
+  blink::TransferableMessage transferable_message =
+      CreateTransferableMessageFromJavaMessagePayload(
+          base::android::ScopedJavaLocalRef<jobject>(j_message_payload));
+  transferable_message.ports =
+      blink::MessagePortChannel::CreateFromHandles(Release(env, j_ports));
+
+  mojo::Message mojo_message =
+      blink::mojom::TransferableMessage::SerializeAsMessage(
+          &transferable_message);
+  bool send_result = connector_->Accept(&mojo_message);
+  DCHECK(send_result);
+}
+
+void AppWebMessagePort::SetShouldReceiveMessages(JNIEnv* env,
+                                                 bool should_receive_message) {
+  DCHECK(runner_->BelongsToCurrentThread());
+  DCHECK(connector_);
+  if (!should_receive_message) {
+    connector_->set_incoming_receiver(nullptr);
+  } else {
+    connector_->set_incoming_receiver(this);
+    if (!is_watching_) {
+      is_watching_ = true;
+      connector_->StartReceiving(runner_);
+    }
   }
-
-  // Now convert to a Java array.
-  base::android::ScopedJavaLocalRef<jlongArray> native_descriptors =
-      base::android::ToJavaLongArray(env, descriptor_ptrs.data(),
-                                     descriptor_ptrs.size());
-
-  // And finally convert this to a Java array of AppWebMessagePorts.
-  base::android::ScopedJavaLocalRef<jobjectArray> ports =
-      Java_AppWebMessagePort_createFromNativeBlinkMessagePortDescriptors(
-          env, native_descriptors);
-
-  base::android::ScopedJavaGlobalRef<jobjectArray> global_ports(ports);
-  return global_ports;
 }
 
-}  // namespace AppWebMessagePort
-
-base::android::ScopedJavaLocalRef<jstring>
-JNI_AppWebMessagePort_DecodeStringMessage(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jbyteArray>& encoded_data) {
-  std::vector<uint8_t> encoded_message;
-  base::android::JavaByteArrayToByteVector(env, encoded_data, &encoded_message);
-
-  std::u16string message;
-  if (!blink::DecodeStringMessage(encoded_message, &message))
-    return nullptr;
-
-  base::android::ScopedJavaLocalRef<jstring> jmessage =
-      base::android::ConvertUTF16ToJavaString(env, message);
-  return jmessage;
+void AppWebMessagePort::CloseAndDestroy(JNIEnv* env) {
+  DCHECK(runner_->BelongsToCurrentThread());
+  DCHECK(connector_);
+  delete this;
 }
 
-base::android::ScopedJavaLocalRef<jbyteArray>
-JNI_AppWebMessagePort_EncodeStringMessage(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jstring>& jmessage) {
-  std::vector<uint8_t> encoded_message = blink::EncodeStringMessage(
-      base::android::ConvertJavaStringToUTF16(jmessage));
-  return base::android::ToJavaByteArray(env, encoded_message);
+// mojo::MessageReceiver:
+bool AppWebMessagePort::Accept(mojo::Message* message) {
+  DCHECK(runner_->BelongsToCurrentThread());
+  blink::TransferableMessage transferable_message;
+  if (!blink::mojom::TransferableMessage::DeserializeFromMessage(
+          std::move(*message), &transferable_message)) {
+    return false;
+  }
+  auto j_ports = CreateJavaMessagePort(
+      blink::MessagePortChannel::ReleaseHandles(transferable_message.ports));
+  base::android::ScopedJavaLocalRef<jobject> j_message =
+      CreateJavaMessagePayload(transferable_message);
+  if (j_message) {
+    JNIEnv* env = base::android::AttachCurrentThread();
+    Java_AppWebMessagePort_onMessage(env, GetJavaObj(env), j_message, j_ports);
+  }
+  return true;
 }
 
-}  // namespace content
+blink::MessagePortDescriptor AppWebMessagePort::PassPort() {
+  DCHECK(runner_->BelongsToCurrentThread());
+  JNIEnv* env = base::android::AttachCurrentThread();
+  Java_AppWebMessagePort_setTransferred(env, GetJavaObj(env));
+  GiveDisentangledHandleIfNeeded();
+  return std::move(descriptor_);
+}
+
+void AppWebMessagePort::OnPipeError() {
+  DCHECK(runner_->BelongsToCurrentThread());
+  connector_errored_ = true;
+}
+
+void AppWebMessagePort::GiveDisentangledHandleIfNeeded() {
+  DCHECK(runner_->BelongsToCurrentThread());
+  if (!connector_ || !descriptor_.IsValid()) {
+    return;
+  }
+  if (connector_errored_) {
+    // Return an empty message pipe.
+    descriptor_.GiveDisentangledHandle(mojo::ScopedMessagePipeHandle());
+  } else {
+    descriptor_.GiveDisentangledHandle(connector_->PassMessagePipe());
+  }
+  connector_.reset();
+}
+
+base::android::ScopedJavaLocalRef<jobjectArray>
+JNI_AppWebMessagePort_CreatePair(JNIEnv* env) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  blink::MessagePortDescriptorPair port_pair;
+  std::vector<blink::MessagePortDescriptor> descriptors;
+  descriptors.emplace_back(port_pair.TakePort0());
+  descriptors.emplace_back(port_pair.TakePort1());
+  return CreateJavaMessagePort(std::move(descriptors));
+}
+
+}  // namespace content::android

content/browser/android/app_web_message_port.h
@@ -1,0 +1,112 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_ANDROID_APP_WEB_MESSAGE_PORT_H_
+#define CONTENT_BROWSER_ANDROID_APP_WEB_MESSAGE_PORT_H_
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_weak_ref.h"
+#include "base/android/scoped_java_ref.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/task/single_thread_task_runner.h"
+#include "content/common/content_export.h"
+#include "third_party/blink/public/common/messaging/message_port_descriptor.h"
+#include "third_party/blink/public/mojom/messaging/transferable_message.mojom.h"
+
+namespace mojo {
+class Connector;
+class Message;
+};  // namespace mojo
+
+namespace content::android {
+
+// This is the native side of the java `AppWebMessagePort`, which is present
+// while the port is still bound to the java object. While bound, the lifetime
+// of the native side is owned by the java side. Creation: The only way to
+// create this object is via `content::MessagePort::CreateJavaMessagePort`. The
+// native object will be created first, and Java object is created within the
+// constructor of native object.
+//
+// Destruction:
+// 1. Pass the ownership to `blink::MessagePortDescriptor` with
+// `Release`. This unbinds the message port from the java
+// object and moves it to `MessagePortDescriptor` to be passed in a message.
+// Native `AppWebMessagePort` object is deleted as part of this operation.
+// 2. Close the MessagePort in Java.
+// 3. The Java object is garbage collected, but `close` is not called.
+//
+// Threading: Methods should be called on UI thread only.
+class CONTENT_EXPORT AppWebMessagePort : public mojo::MessageReceiver {
+ public:
+  // Create `AppWebMessagePort` from a `MessagePortDescriptor`, this takes the
+  // ownership of `MessagePortDescriptor`. `AppWebMessagePort` is created and
+  // returned.
+  static base::android::ScopedJavaLocalRef<jobject> Create(
+      blink::MessagePortDescriptor&& descriptor);
+
+  // Given an array of `AppWebMessagePort` objects, unwraps them and returns an
+  // equivalent array of `blink::MessagePortDescriptors`. The underlying
+  // instances(Java and Native) will be destroyed.
+  static std::vector<blink::MessagePortDescriptor> Release(
+      JNIEnv* env,
+      const base::android::JavaRef<jobjectArray>&
+          jports /* org.chromium.content.browser.AppWebMessagePort */);
+
+  // When clean up native, we notify Java instance to release native handle.
+  ~AppWebMessagePort() override;
+
+  void PostMessage(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_message_payload,
+      const base::android::JavaParamRef<jobjectArray>& j_ports);
+  void SetShouldReceiveMessages(JNIEnv* env, bool should_receive_message);
+  void CloseAndDestroy(JNIEnv* env);
+
+ private:
+  explicit AppWebMessagePort(blink::MessagePortDescriptor&& descriptor);
+
+  // Convert `j_obj_` weak reference to a strong local reference.
+  // The Java object is always available until native object destroyed.
+  // This assumption is still true even when the Java object is GC-ed, since
+  // it's strong referenced again by PostTask.
+  base::android::ScopedJavaLocalRef<jobject> GetJavaObj(JNIEnv* env) {
+    auto java_ref = j_obj_.get(env);
+    DCHECK(java_ref);
+    return java_ref;
+  }
+
+  // Pass out the port descriptor. This port will become invalid after calling
+  // this. This can only be called if `PostMessage` and `SetMessageCallback` are
+  // never called.
+  blink::MessagePortDescriptor PassPort();
+
+  // The error handler for the connector. This lets the instrumentation be aware
+  // of the message pipe closing itself due to error, which happens
+  // unconditionally. Note that a subsequent call to
+  // `Connector::PassMessagePipe` should always return an invalid handle at that
+  // point.
+  // TODO(chrisha): Make this an immediate notification that the channel has
+  // been torn down rather than waiting for the owning `MessagePort` to be
+  // cleaned up.
+  void OnPipeError();
+  void GiveDisentangledHandleIfNeeded();
+
+  // mojo::MessageReceiver:
+  bool Accept(mojo::Message* message) override;
+
+  // UI thread.
+  scoped_refptr<base::SingleThreadTaskRunner> runner_;
+
+  blink::MessagePortDescriptor descriptor_;
+  JavaObjectWeakGlobalRef j_obj_;
+
+  bool connector_errored_ = false;
+  bool is_watching_ = false;
+
+  std::unique_ptr<mojo::Connector> connector_;
+};
+
+}  // namespace content::android
+
+#endif  // CONTENT_BROWSER_ANDROID_APP_WEB_MESSAGE_PORT_H_

android_webview/javatests/src/org/chromium/android_webview/test/PostMessageTest.java
@@ -24,11 +24,16 @@ import org.junit.runner.RunWith;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.CommonResources;
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.task.PostTask;
+import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.MessagePayload;
 import org.chromium.content_public.browser.MessagePort;
+import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.content_public.browser.test.util.TestCallbackHelperContainer.OnPageFinishedHelper;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.util.TestWebServer;
@@ -36,10 +41,13 @@ import org.chromium.net.test.util.TestWebServer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * The tests for content postMessage API.
  */
+@Batch(Batch.PER_CLASS)
 @RunWith(AwJUnit4ClassRunner.class)
 public class PostMessageTest {
     @Rule
@@ -968,5 +976,173 @@ public class PostMessageTest {
         MessageObject.Data data = mMessageObject.waitForMessage();
         Assert.assertEquals(WEBVIEW_MESSAGE, data.mMessage);
         Assert.assertEquals(SOURCE_ORIGIN, data.mOrigin);
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    public void testMessagePortLifecycle() throws Throwable {
+        final String baseUrl = mWebServer.getBaseUrl();
+        loadPage(TEST_PAGE);
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
+            final MessagePort[] ports = mAwContents.createMessageChannel();
+            Assert.assertFalse(ports[0].isTransferred());
+            Assert.assertFalse(ports[0].isClosed());
+            Assert.assertFalse(ports[0].isStarted());
+            Assert.assertFalse(ports[1].isTransferred());
+            Assert.assertFalse(ports[1].isClosed());
+            Assert.assertFalse(ports[1].isStarted());
+
+            // Post port1 to main frame.
+            mAwContents.postMessageToMainFrame(
+                    new MessagePayload("1"), baseUrl, new MessagePort[] {ports[1]});
+            Assert.assertTrue(ports[1].isTransferred());
+            Assert.assertFalse(ports[1].isClosed());
+            Assert.assertFalse(ports[1].isStarted());
+
+            // Close one port.
+            ports[0].close();
+            Assert.assertFalse(ports[0].isTransferred());
+            Assert.assertTrue(ports[0].isClosed());
+            Assert.assertFalse(ports[0].isStarted());
+        });
+    }
+
+    private static final String COUNT_PORT_FROM_MESSAGE = "<!DOCTYPE html><html><body>"
+            + "    <script>"
+            + "        var counter = 0;"
+            + "        var received = '';"
+            + "        onmessage = function (e) {"
+            + "            e.ports[0].onmessage = function(e) {"
+            + "                received += e.data;"
+            + "                counter += e.ports.length;"
+            + "                document.title = received + counter;"
+            + "                e.ports[0].postMessage(received + counter);"
+            + "            };"
+            + "        };"
+            + "   </script>"
+            + "</body></html>";
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    // Previously postMessage can be called on any thread, but no tests or CTS tests checked.
+    public void testTransferPortOnAnotherThread() throws Throwable {
+        loadPage(COUNT_PORT_FROM_MESSAGE);
+        final ChannelContainer container = new ChannelContainer();
+        final MessagePort[] ports =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mAwContents.postMessageToMainFrame(
+                    new MessagePayload(""), "*", new MessagePort[] {ports[1]});
+        });
+        final MessagePort[] ports2 =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+        ports2[0].setMessageCallback((messagePayload, sentPorts) -> {
+            ThreadUtils.checkUiThread();
+            container.notifyCalled(messagePayload);
+        }, null);
+        ports[0].postMessage(new MessagePayload(HELLO), new MessagePort[] {ports2[1]});
+        expectTitle(HELLO + "1");
+        Assert.assertEquals(HELLO + "1", container.waitForMessageCallback().getStringValue());
+        ports[0].close();
+        ports2[0].close();
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    public void testTransferPortImmediateAfterPostMessageOnAnotherThread() throws Throwable {
+        loadPage(COUNT_PORT_FROM_MESSAGE);
+        final MessagePort[] ports =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mAwContents.postMessageToMainFrame(
+                    new MessagePayload(""), "*", new MessagePort[] {ports[1]});
+        });
+        final CallbackHelper callbackHelper = new CallbackHelper();
+        final AtomicReference<IllegalStateException> exceptionRef = new AtomicReference<>();
+        PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> {
+            try {
+                callbackHelper.waitForCallback(0);
+                mAwContents.postMessageToMainFrame(
+                        new MessagePayload(HELLO), "*", new MessagePort[] {ports[0]});
+            } catch (TimeoutException ignored) {
+            } catch (IllegalStateException e) {
+                exceptionRef.set(e);
+                callbackHelper.notifyCalled();
+            }
+        });
+        ports[0].postMessage(new MessagePayload(HELLO), null);
+        callbackHelper.notifyCalled();
+
+        callbackHelper.waitForCallback(1);
+        Assert.assertEquals("Port is already started", exceptionRef.get().getMessage());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    public void testCloseMessagePortOnAnotherThread() throws Throwable {
+        final MessagePort[] messagePorts = new MessagePort[1];
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
+            final MessagePort[] ports = mAwContents.createMessageChannel();
+            messagePorts[0] = ports[0];
+            // Move message port into |receiving| state.
+            messagePorts[0].setMessageCallback((messagePayload, sentPorts) -> {}, null);
+        });
+        // Close message channel on another thread, simulate the case where the "finalize" is called
+        // on finalizer thread.
+        messagePorts[0].close();
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    public void testTransferPortInAnotherThreadRaceCondition() throws Throwable {
+        loadPage(COUNT_PORT_FROM_MESSAGE);
+        final MessagePort[] ports =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mAwContents.postMessageToMainFrame(
+                    new MessagePayload(""), "*", new MessagePort[] {ports[1]});
+        });
+        final MessagePort[] portsToTransfer =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+        // Transfer the port in another thread.
+        ports[0].postMessage(new MessagePayload("test"), new MessagePort[] {portsToTransfer[0]});
+        // Check port2[0] is transferred right now.
+        Assert.assertTrue(portsToTransfer[0].isTransferred());
+        // Set callback on the just transferred port right now. It should fail.
+        try {
+            portsToTransfer[0].setMessageCallback((messagePayload, sentPorts) -> {}, null);
+            Assert.fail("Port transferred, should not able to listen on");
+        } catch (IllegalStateException e) {
+            // Ignored.
+        }
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    public void testSetReceiverAfterMessageReceived() throws Throwable {
+        loadPage(COUNT_PORT_FROM_MESSAGE);
+        final ChannelContainer container = new ChannelContainer();
+        final HandlerThread thread = new HandlerThread("test-thread");
+        thread.start();
+        final Handler handler = new Handler(thread.getLooper());
+        final MessagePort[] ports =
+                TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.createMessageChannel());
+
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
+            // Post message before set callback
+            ports[0].postMessage(new MessagePayload("msg1"), null);
+        });
+        ports[1].setMessageCallback((messagePayload, sentPorts) -> {
+            container.notifyCalled(messagePayload);
+        }, handler);
+        Assert.assertEquals("msg1", container.waitForMessageCallback().getStringValue());
     }
 }

commit 0f3235315aa64d6c57d3d2eca98527c5ad1eea37

[WebView] Fix MessagePort not receiving messages after garbage collected
Adapted from
https://chromium-review.googlesource.com/c/chromium/src/+/3898652
by linyhe@microsoft.com

The bug was introduced from commit 57e4ed82bfbd12b6256d2fcadcb8026f38880380.
Before the refactor, even the Java MessagePort is GC-ed, the
listener (MessageCallback) is still available, since it was
referenced by the Mojo Connector.

This broke with refactor, because a port is closed when it's finalized,
breaking the previous contract.

Change the lifetime so that as long as the port can receive messages, it
will be kept alive by a strong reference. This necessarily lead to leaks
in situations such as setting listener to both ports in a channel and
not closing either one of them, but matches old behavior.

Bug: b:245837736
Change-Id: Ib2c0314422b900ecaf1dc5706e70a678b00de92e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899437
Commit-Queue: Bo Liu <boliu@chromium.org>
Reviewed-by: Richard Coles <torne@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1047539}

content/browser/android/app_web_message_port.cc
@@ -118,8 +118,12 @@ void AppWebMessagePort::SetShouldReceiveMessages(JNIEnv* env,
   DCHECK(connector_);
   if (!should_receive_message) {
     connector_->set_incoming_receiver(nullptr);
+    j_strong_obj_.Reset();
   } else {
     connector_->set_incoming_receiver(this);
+    if (!connector_errored_) {
+      j_strong_obj_ = j_obj_.get(env);
+    }
     if (!is_watching_) {
       is_watching_ = true;
       connector_->StartReceiving(runner_);
@@ -171,6 +175,7 @@ blink::MessagePortDescriptor AppWebMessagePort::PassPort() {
 void AppWebMessagePort::OnPipeError() {
   DCHECK(runner_->BelongsToCurrentThread());
   connector_errored_ = true;
+  j_strong_obj_.Reset();
 }
 
 void AppWebMessagePort::GiveDisentangledHandleIfNeeded() {

content/browser/android/app_web_message_port.h
@@ -101,6 +101,10 @@ class CONTENT_EXPORT AppWebMessagePort : public mojo::MessageReceiver {
   blink::MessagePortDescriptor descriptor_;
   JavaObjectWeakGlobalRef j_obj_;
 
+  // Set when this port is receiving messages. Port should be kept alive
+  // as long as it can still receive messages.
+  base::android::ScopedJavaGlobalRef<jobject> j_strong_obj_;
+
   bool connector_errored_ = false;
   bool is_watching_ = false;
 

android_webview/javatests/src/org/chromium/android_webview/test/PostMessageTest.java
@@ -1334,4 +1334,41 @@ public class PostMessageTest {
         }, handler);
         Assert.assertEquals("msg1", container.waitForMessageCallback().getStringValue());
     }
+
+    private static final String COPY_PORT_MESSAGE_FROM_WINDOW = "<!DOCTYPE html><html><body>"
+            + "    <script>"
+            + "        var port = null;"
+            + "        onmessage = function (e) {"
+            + "            if (e.ports[0]) port = e.ports[0];"
+            + "            else port.postMessage(e.data);"
+            + "        };"
+            + "   </script>"
+            + "</body></html>";
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView", "Android-PostMessage"})
+    // Regression test of https://issuetracker.google.com/245837736
+    public void testMessageListenerAvailableAfterPortGarbageCollected() throws Throwable {
+        loadPage(COPY_PORT_MESSAGE_FROM_WINDOW);
+        final ChannelContainer container = new ChannelContainer();
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
+            MessagePort[] ports = mAwContents.createMessageChannel();
+            ports[0].setMessageCallback((message, p) -> container.notifyCalled(message), null);
+            mAwContents.postMessageToMainFrame(
+                    new MessagePayload("*"), "*", new MessagePort[] {ports[1]});
+            ports = null;
+        });
+        for (int i = 0; i < 100; ++i) {
+            final String message = HELLO + i;
+            Runtime.getRuntime().gc();
+            InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> {
+                // Trigger GC to make ports[0] being garbage collected. Note that despite that what
+                // JavaDoc says about invoking "gc()", both Dalvik and ART actually run the
+                // collector.
+                mAwContents.postMessageToMainFrame(new MessagePayload(message), "*", null);
+            });
+            Assert.assertEquals(message, container.waitForMessageCallback().getStringValue());
+        }
+    }
 }