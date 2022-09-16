8c591efb0b630e3edf19789bb37d901e6c83149c
Wei4 Wang <wei4.wang@intel.com>
2022-09-16 10:41:17 +0000

    [ComputePressure] Remove NullCpuProbe
    NullCpuProbe is used for Windows and Mac when there is only
    CpuProbeLinux. Now CpuProbeWin and CpuProbeMac are already
    merged, So NullCpuProbe is useless.
    
    Removing NullCpuProbe also makes it clear for these unsupported
    platforms by returning NotSupportedError instand of using this
    NullCpuProbe.
    
    Bug: 1363967
    Change-Id: I6e31349ab16a7ff120c59cea0efdb63605d40661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3896947
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1047921}

--- services/device/compute_pressure/cpu_probe.cc
+++ services/device/compute_pressure/cpu_probe.cc
@@ -6,10 +6,7 @@
 
 #include <memory>
 
-#include "base/sequence_checker.h"
-#include "base/threading/scoped_blocking_call.h"
 #include "build/build_config.h"
-#include "services/device/compute_pressure/pressure_sample.h"
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "services/device/compute_pressure/cpu_probe_linux.h"
 #elif BUILDFLAG(IS_WIN)
@@ -20,51 +17,20 @@
 
 namespace device {
 
-constexpr PressureSample CpuProbe::kUnsupportedValue;
-
 CpuProbe::CpuProbe() = default;
 CpuProbe::~CpuProbe() = default;
 
-// Default implementation for platforms that don't have one.
-class NullCpuProbe : public CpuProbe {
- public:
-  NullCpuProbe() { DETACH_FROM_SEQUENCE(sequence_checker_); }
-  ~NullCpuProbe() override {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  }
-
-  // CpuProbe implementation.
-  void Update() override {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-    // Ensure that this method is called on a sequence that is allowed to do
-    // IO, even on OSes that don't have a CpuProbe implementation yet.
-    base::ScopedBlockingCall scoped_blocking_call(
-        FROM_HERE, base::BlockingType::MAY_BLOCK);
-  }
-
-  PressureSample LastSample() override {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-    return CpuProbe::kUnsupportedValue;
-  }
-
- private:
-  SEQUENCE_CHECKER(sequence_checker_);
-};
-
 // static
 std::unique_ptr<CpuProbe> CpuProbe::Create() {
-#if BUILDFLAG(IS_ANDROID)
-  return nullptr;
-#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   return CpuProbeLinux::Create();
 #elif BUILDFLAG(IS_WIN)
   return CpuProbeWin::Create();
 #elif BUILDFLAG(IS_MAC)
   return CpuProbeMac::Create();
 #else
-  return std::make_unique<NullCpuProbe>();
-#endif  // BUILDFLAG(IS_ANDROID)
+  return nullptr;
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 }
 
 }  // namespace device
