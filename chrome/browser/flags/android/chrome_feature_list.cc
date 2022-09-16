01f188ae59346d0bbe0e711478180eea043cda45
Xi Han <hanxi@google.com>
2022-09-16 23:10:54 +0000

    [Start] Enabled tab-switcher-on-return by default.
    This feature flag is only used to show start surface which is enabled on
    ToT. Flip this flag too.
    
    Bug: 1356180
    Change-Id: I16468f5ab33e04d42ce004bd75260f8655d8b028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902882
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048282}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -888,7 +888,7 @@ const base::Feature kDiscoverFeedMultiColumn{"DiscoverFeedMultiColumn",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTabSwitcherOnReturn{"TabSwitcherOnReturn",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kTabToGTSAnimation{"TabToGTSAnimation",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
