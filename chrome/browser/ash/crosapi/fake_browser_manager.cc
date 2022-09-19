424390ec28433812bbe5b9f8bd08eebc4277e454
Roman Sorokin <rsorokin@chromium.org>
2022-09-19 19:49:47 +0000

    [Code Health] Remove GetAsDictionary from */ash/* files
    Remove in /chrome/browser/ash/crosapi
    
    This CL was uploaded by git cl split.
    
    R=rbock@google.com
    
    Bug: 1187011
    Change-Id: Iee3212f43789b93087656e106c178500bf4c6bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904612
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1048746}

--- chrome/browser/ash/crosapi/fake_browser_manager.cc
+++ chrome/browser/ash/crosapi/fake_browser_manager.cc
@@ -73,9 +73,6 @@ void FakeBrowserManager::NewFullscreenWindow(
 }
 
 void FakeBrowserManager::GetFeedbackData(GetFeedbackDataCallback callback) {
-  const base::DictionaryValue* sysinfo_entries;
-  feedback_response_.GetAsDictionary(&sysinfo_entries);
-
   // Run |callback| with the pre-set |feedback_responses_|, unless testing
   // client requests waiting for crosapi mojo disconnected event being observed.
   if (!wait_for_mojo_disconnect_)
