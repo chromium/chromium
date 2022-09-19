b45b9dc37819065010a46a81ad40fe937907bf0a
Aida Zolic <aidazolic@chromium.org>
2022-09-19 09:35:05 +0000

    DLP: Add file picker/saver translation strings
    Adds translation strings instead of hardcoded values for two DLP
    banners in the files app.
    
    Fixed: 1360874
    Change-Id: Idf69d61705a4007ee9bed1f598e72dbd98f14407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899113
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048536}

--- ui/file_manager/file_manager/foreground/js/ui/banners/dlp_restricted_banner.js
+++ ui/file_manager/file_manager/foreground/js/ui/banners/dlp_restricted_banner.js
@@ -5,6 +5,7 @@
 import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {DialogType} from '../../../../common/js/dialog_type.js';
+import {str} from '../../../../common/js/util.js';
 import {VolumeManagerCommon} from '../../../../common/js/volume_manager_types.js';
 import {Banner} from '../../../../externs/banner.js';
 
@@ -40,7 +41,7 @@ export class DlpRestrictedBanner extends StateBanner {
    * @returns {!Array<!Banner.AllowedVolume>}
    */
   allowedVolumes() {
-    // TODO(aidazolic): confirm this list is correct.
+    // TODO(crbug.com/1364014): return the correct types here.
     return [
       {root: VolumeManagerCommon.RootType.DOWNLOADS},
       {root: VolumeManagerCommon.RootType.COMPUTERS_GRAND_ROOT},
@@ -74,16 +75,13 @@ export class DlpRestrictedBanner extends StateBanner {
       return;
     }
     const text = this.shadowRoot.querySelector('span[slot="text"]');
-    // TODO(crbug.com/1360874): update to translation strings when they're
-    // final.
     switch (context.type) {
       case DialogType.SELECT_OPEN_FILE:
       case DialogType.SELECT_OPEN_MULTI_FILE:
-        text.innerText = 'Administrator policy restricts access to some files';
+        text.innerText = str('DLP_FILE_PICKER_BANNER');
         return;
       case DialogType.SELECT_SAVEAS_FILE:
-        text.innerText =
-            'Administrator policy restricts saving to some locations';
+        text.innerText = str('DLP_FILE_SAVER_BANNER');
         return;
       default:
         console.warn(`The DLP banner should not be shown for ${context.type}.`);
