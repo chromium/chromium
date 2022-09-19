389a776876563185b527b29679e6450b20d8bfa6
Alex Kingsborough <akingsb@google.com>
2022-09-19 22:09:56 +0000

    [Fast Pair] Put forget dialog behind FP flag
    The bluetooth device forget dialog was added as part of Fast Pair and
    therefore should be behind the Fast Pair flag.
    
    original dialog cl: crrev/c/3756863
    
    (cherry picked from commit 15c2904192c0cdbaf88285d6db0a54b7d35d9e7b)
    
    Test: Tested on DUT with flag on and off
    Change-Id: Ib4b653fb40c0418be4f3c3c15bace1f240c5e8cb
    Bug: b/247033179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900805
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1048244}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902826
    Cr-Commit-Position: refs/branch-heads/5249@{#502}
    Cr-Branched-From: 4f7bea5de862aaa52e6bde5920755a9ef9db120b-refs/heads/main@{#1036826}

--- chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
@@ -19,9 +19,10 @@ import {BluetoothUiSurface, recordBluetoothUiSurfaceMetrics} from 'chrome://reso
 import {BatteryType} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_types.js';
 import {getBatteryPercentage, getDeviceName, hasAnyDetailedBatteryInfo, hasDefaultImage, hasTrueWirelessImages} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_utils.js';
 import {getBluetoothConfig} from 'chrome://resources/cr_components/chromeos/bluetooth/cros_bluetooth_config.js';
-import {assertNotReached} from 'chrome://resources/js/assert.m.js';
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
 import {WebUIListenerBehavior, WebUIListenerBehaviorInterface} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
+import {assertNotReached} from 'chrome://resources/js/assert.m.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {AudioOutputCapability, BluetoothSystemProperties, DeviceConnectionState, DeviceType, PairedBluetoothDeviceProperties} from 'chrome://resources/mojo/chromeos/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom-webui.js';
 import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -642,7 +643,11 @@ class SettingsBluetoothDeviceDetailSubpageElement extends
 
   /** @private */
   onForgetButtonClicked_() {
-    this.shouldShowForgetDeviceDialog_ = true;
+    if (loadTimeData.getBoolean('enableFastPairFlag')) {
+      this.shouldShowForgetDeviceDialog_ = true;
+    } else {
+      getBluetoothConfig().forget(this.deviceId_);
+    }
   }
 
   /** @private */
