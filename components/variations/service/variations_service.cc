e79cc6f5f94317b2fdb25466e5c73402cf8dde25
Colin Blundell <blundell@chromium.org>
2022-09-20 14:10:24 +0000

    [VariationsService] Clean up usage of IS_CHROMEOS_LACROS
    https://chromium-review.googlesource.com/c/chromium/src/+/2628711
    transformed a usage of OS_LINUX to
    (OS_LINUX | BUILDFLAG(IS_CHROMEOS_LACROS) in preparation for Lacros
    being ChromeOS instead of Linux. However, in this particular case
    IS_CHROMEOS_LACROS is used higher in the list, and so is not needed
    (and should not be) here. thestig@ noticed the error post the CL
    landing. This CL cleans it up. It is a functional no-op, due to the
    presence of IS_CHROMEOS_LACROS higher in the list.
    
    Change-Id: I262f0a84180a2aee701e17e960b82f68056e9a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905147
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049073}

--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -94,8 +94,7 @@ std::string GetPlatformString() {
   return "android";
 #elif BUILDFLAG(IS_FUCHSIA)
   return "fuchsia";
-#elif (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) || \
-    BUILDFLAG(IS_BSD) || BUILDFLAG(IS_SOLARIS)
+#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD) || BUILDFLAG(IS_SOLARIS)
   // Default BSD and SOLARIS to Linux to not break those builds, although these
   // platforms are not officially supported by Chrome.
   return "linux";
