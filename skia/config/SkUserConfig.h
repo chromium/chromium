cbb16c7e559199553f0b3a4e8c065a5b49dee223
Ben Wagner <bungeman@chromium.org>
2022-09-20 19:14:53 +0000

    Remove no-op defines
    The SK_SUPPORT_LEGACY_RUNTIME_EFFECTS and SK_LEGACY_INNER_JOINS flags no
    longer control anything and may be removed.
    
    Change-Id: I42e9c4a52d983cc52f234efb91a5da60b17383ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905057
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049267}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -214,11 +214,7 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_SUPPORT_LEGACY_DRAWLOOPER
 
-#define SK_SUPPORT_LEGACY_RUNTIME_EFFECTS
-
 #define SK_SUPPORT_LEGACY_DITHER
-
-#define SK_LEGACY_INNER_JOINS
 
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
