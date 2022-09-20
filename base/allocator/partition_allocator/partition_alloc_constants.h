df79f0b6fbe69bb0f35982f0345ccffd3fca11f4
Wang Qing <wangqing-hf@loongson.cn>
2022-09-20 16:46:47 +0000

    base::PartitionAlloc: Add loongarch64 page size support.
    Change-Id: Ief0ed761b3a301efb1f68d9356dc54ba9a37e197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899637
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049161}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -80,7 +80,7 @@ constexpr size_t kPartitionCachelineSize = 64;
 // other constant values, we pack _all_ `PartitionRoot::Alloc` sizes perfectly
 // up against the end of a system page.
 
-#if defined(_MIPS_ARCH_LOONGSON)
+#if defined(_MIPS_ARCH_LOONGSON) || defined(ARCH_CPU_LOONG64)
 PAGE_ALLOCATOR_CONSTANTS_DECLARE_CONSTEXPR PA_ALWAYS_INLINE size_t
 PartitionPageShift() {
   return 16;  // 64 KiB
