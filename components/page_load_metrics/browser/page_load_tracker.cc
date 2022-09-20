6287d79f0499b2094838d5db436083e39c1d0a58
keno <kenoss@chromium.org>
2022-09-20 07:37:43 +0000

    PageLoadMetrics: Correct visibilility check
    In PageLoadTracker, the possible visibility state transitions and events
    are:
    
    A. navigation_start (fg) -> first background (bg)
       -> first foreground (fg)
    B. navigation_start (bg) -> first foreground (fg)
       -> first background (bg)
    C. navigation_start (bg) -> activation_start (fg)
       -> first background (bg) -> first foreground (fg)
    
    where fg = foreground, bg = background. A and B are non prerendered and
    C is prerendered.
    
    Before this CL, the check in PageShown handled A and B but failed with
    C. This CL fixes the check and refactors the similar check in
    PageHidden.
    
    Bug: 1355803
    Change-Id: I5368404d273d9cc434157c12ac5ad453195f4834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3864827
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1048984}

--- components/page_load_metrics/browser/page_load_tracker.cc
+++ components/page_load_metrics/browser/page_load_tracker.cc
@@ -315,17 +315,33 @@ void PageLoadTracker::PageHidden() {
       (!back_forward_cache_restores_.empty() &&
        !back_forward_cache_restores_.back()
             .first_background_time.has_value())) {
-    // Make sure we either started in the foreground and haven't been
-    // foregrounded yet, or started in the background and have already been
-    // foregrounded.
-    base::TimeTicks background_time;
-
-    if (!first_background_time_.has_value() &&
-        (prerendering_state_ == PrerenderingState::kNoPrerendering)) {
-      DCHECK_EQ(started_in_foreground_, !first_foreground_time_.has_value());
+    // The possible visibility state transitions and events are:
+    //
+    // A. navigation_start (fg) -> first background (bg)
+    //    -> first foreground (fg)
+    // B. navigation_start (bg) -> first foreground (fg)
+    //    -> first background (bg)
+    // C. navigation_start (bg) -> activation_start (fg)
+    //    -> first background (bg) -> first foreground (fg)
+    //
+    // where fg = foreground, bg = background. A and B are non prerendered and C
+    // is prerendered.
+    //
+    // PageShown and PgaeHidden are not called for navigation_start and
+    // actiivation_start; called when the visibility is changed after
+    // navigation_start (resp. activation_start) for non prerendered (resp.
+    // prerendered) pages.
+    //
+    // Here we check that the first background follows some event in foreground.
+    if (!first_background_time_.has_value()) {
+      if (prerendering_state_ == PrerenderingState::kNoPrerendering) {
+        DCHECK_EQ(!started_in_foreground_, first_foreground_time_.has_value());
+      } else {
+        DCHECK(!first_foreground_time_.has_value());
+      }
     }
 
-    background_time = base::TimeTicks::Now();
+    base::TimeTicks background_time = base::TimeTicks::Now();
     ClampBrowserTimestampIfInterProcessTimeTickSkew(&background_time);
     DCHECK_GE(background_time, navigation_start_);
 
@@ -354,12 +370,16 @@ void PageLoadTracker::PageHidden() {
 void PageLoadTracker::PageShown() {
   // Only log the first time we foreground in a given page load.
   if (!first_foreground_time_.has_value()) {
-    // Make sure we either started in the background and haven't been
-    // backgrounded yet, or started in the foreground and have already been
-    // backgrounded.
-    base::TimeTicks foreground_time;
-    DCHECK_NE(started_in_foreground_, !first_background_time_.has_value());
-    foreground_time = base::TimeTicks::Now();
+    // See comment about visibility state transitions in PageHidden.
+    //
+    // Here we check that the first foreground follows some event in background.
+    if (prerendering_state_ == PrerenderingState::kNoPrerendering) {
+      DCHECK_EQ(started_in_foreground_, first_background_time_.has_value());
+    } else {
+      DCHECK(first_background_time_.has_value());
+    }
+
+    base::TimeTicks foreground_time = base::TimeTicks::Now();
     ClampBrowserTimestampIfInterProcessTimeTickSkew(&foreground_time);
     DCHECK_GE(foreground_time, navigation_start_);
     first_foreground_time_ = foreground_time;
