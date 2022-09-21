db1d20faf6b5fc44ad91f63d5e14c1d734c14e44
Maxim Anufriev <maxan@google.com>
2022-09-21 10:25:57 +0000

    [Android][UPM] Extract utils related to the UPM users eviction
    This CL extracts the logic of UPM user evictions before introducing
    ignorable/retriable error lists and evictions versioning.
    
    Bug: 1362464
    Change-Id: Iede1a112d708d5288678244f1eda3ed62d988a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3890926
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049560}

---
+++ chrome/browser/password_manager/android/password_manager_eviction_util.cc
@@ -0,0 +1,46 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/password_manager/android/password_manager_eviction_util.h"
+
+#include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
+#include "components/prefs/pref_service.h"
+
+namespace password_manager_upm_eviction {
+
+bool IsCurrentUserEvicted(const PrefService* prefs) {
+  return prefs->GetBoolean(
+      password_manager::prefs::kUnenrolledFromGoogleMobileServicesDueToErrors);
+}
+
+void EvictCurrentUser(int api_error_code, PrefService* prefs) {
+  if (prefs->GetBoolean(password_manager::prefs::
+                            kUnenrolledFromGoogleMobileServicesDueToErrors))
+    // User is already evicted, keep the original eviction reason.
+    return;
+
+  prefs->SetBoolean(
+      password_manager::prefs::kUnenrolledFromGoogleMobileServicesDueToErrors,
+      true);
+  prefs->SetInteger(password_manager::prefs::
+                        kUnenrolledFromGoogleMobileServicesAfterApiErrorCode,
+                    api_error_code);
+
+  // Reset migration prefs so when the user can join the experiment again,
+  // non-syncable data and settings can be migrated to GMS Core.
+  prefs->SetInteger(
+      password_manager::prefs::kCurrentMigrationVersionToGoogleMobileServices,
+      0);
+  prefs->SetDouble(password_manager::prefs::kTimeOfLastMigrationAttempt, 0.0);
+  prefs->SetBoolean(password_manager::prefs::kSettingsMigratedToUPM, false);
+
+  base::UmaHistogramBoolean("PasswordManager.UnenrolledFromUPMDueToErrors",
+                            true);
+  LOG(ERROR) << "Unenrolled from UPM due to error with code: "
+             << api_error_code;
+}
+
+}  // namespace password_manager_upm_eviction
