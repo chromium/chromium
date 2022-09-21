d4adc9600afb553cd24e770bb496d8e33bfe9710
Martin Bidlingmaier <mbid@google.com>
2022-09-21 16:54:58 +0000

    Refactor os settings browser test api implementation
    The implementation uses simple polling now when waiting for an element
    to appear or a property to hold. This is less performant, but should not
    be noticeable because test runtime is dominated by starting chrome and
    loading the webui. On the other hand, polling is less error-prone and
    probably less flaky.
    
    The commit also extracts util functions into a separate module, and
    includes a few minor new assertions: Authenticate checks whether the
    dialog disappears, and some element properties are checked to hold for a
    some small duration.
    
    BUG=b:243696986
    
    Change-Id: Ib2ff462316bf441d234b43758574bf5030ae3835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899898
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1049693}

--- chrome/test/data/webui/settings/chromeos/test_api.ts
+++ chrome/test/data/webui/settings/chromeos/test_api.ts
@@ -5,208 +5,155 @@
 import {assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {LockScreenSettingsInterface, LockScreenSettingsReceiver, LockScreenSettingsRemote, OSSettingsBrowserProcess, OSSettingsDriverInterface, OSSettingsDriverReceiver} from './test_api.test-mojom-webui.js';
-
-const kDefaultTimeoutMilliseconds = 10 * 1000;
-
-// Applies a function to an element until the function returns not null.
-// Reapplies the function if the element changes.
-async function watchElement<E extends DocumentFragment|Element, T>(
-    element: E, elementFunction: (el: E) => (T | null),
-    timeoutMilliseconds: number =
-        kDefaultTimeoutMilliseconds): Promise<T|null> {
-  const result = elementFunction(element);
-  if (result != null) {
-    return result;
-  }
-
-  const asyncResult = await new Promise((resolve: (val: T|null) => void) => {
-    interface State {
-      mutationObserver: MutationObserver|null;
-      timeoutID: number|null;
-    }
-    const state: State = {
-      mutationObserver: null,
-      timeoutID: null,
-    };
-
-    const done = (result: T|null) => {
-      if (state.mutationObserver != null) {
-        state.mutationObserver.disconnect();
-      }
-      if (state.timeoutID != null) {
-        clearTimeout(state.timeoutID);
-      }
-      resolve(result);
-    };
-
-    state.mutationObserver = new MutationObserver(() => {
-      const result = elementFunction(element);
-      if (result != null) {
-        done(result);
-      }
-    });
-    state.mutationObserver.observe(
-        element, {childList: true, subtree: true, attributes: true});
-
-    state.timeoutID = setTimeout(() => {
-      done(null);
-    }, timeoutMilliseconds);
-  });
-
-  return asyncResult;
-}
-
-// Like querySelector, but waits for the element to appear if necessary.
-async function querySelectorAsync(
-    root: DocumentFragment|Element, selector: string,
-    timeoutMilliseconds: number =
-        kDefaultTimeoutMilliseconds): Promise<Element|null> {
-  return watchElement(
-      root, (root) => root.querySelector(selector), timeoutMilliseconds);
-}
-
-// Find an element that is nested inside shadow roots using a sequence of query
-// selectors. The first query is run from |root|. Subsequent queries are run
-// within the |shadowRoot| of the previous result. Returns |null| if any of
-// the queries did not yield a result.
-async function querySelectorAsyncShadowRoots(
-    root: DocumentFragment|Element, selectors: string[],
-    timeoutMilliseconds: number =
-        kDefaultTimeoutMilliseconds): Promise<Element|null> {
-  assertTrue(selectors.length > 0);
-
-  const initSelectors = selectors.slice(0, selectors.length - 1);
-  const lastSelector = selectors[selectors.length - 1];
-  assertTrue(lastSelector != null);
-  for (const selector of initSelectors) {
-    const el = await querySelectorAsync(root, selector, timeoutMilliseconds);
-    if (el == null) {
-      return null;
-    }
-    const shadowRoot =
-        await watchElement(el, (el) => el.shadowRoot, timeoutMilliseconds);
-    if (shadowRoot == null) {
-      return null;
-    }
-    root = shadowRoot;
-  }
-
-  return querySelectorAsync(root, lastSelector, timeoutMilliseconds);
-}
+import {assertAsync, assertForDuration, hasBooleanProperty, hasProperty, Lazy, querySelectorShadow, retryUntilSome} from './utils.js';
 
 export class LockScreenSettings implements LockScreenSettingsInterface {
-  privacyPage: Element;
-  lockScreen: Element;
+  // Relevant elements are stored as lazy values because element identity might
+  // change. For example, the settings page destroys |passwordDialog| after
+  // successful authentication and creates a new element if it requires
+  // authentication again.
+  private lockScreen: Lazy<HTMLElement>;
+  private passwordDialog: Lazy<HTMLElement|null>;
 
-  constructor(privacyPage: Element) {
-    this.privacyPage = privacyPage;
-    assertTrue(privacyPage.shadowRoot != null);
-    const lockScreen =
-        privacyPage.shadowRoot.querySelector('settings-lock-screen');
-    assertTrue(lockScreen != null);
-    this.lockScreen = lockScreen;
+  constructor(params: {
+    lockScreen: Lazy<HTMLElement>,
+    passwordDialog: Lazy<HTMLElement|null>,
+  }) {
+    this.lockScreen = params.lockScreen;
+    assertTrue(this.lockScreen().shadowRoot != null);
+    this.passwordDialog = params.passwordDialog;
   }
 
-  async passwordPrompt(): Promise<Element|null> {
-    assertTrue(this.privacyPage.shadowRoot != null);
-    const passwordDialog = await querySelectorAsync(
-        this.privacyPage.shadowRoot, '#passwordDialog');
-    if (passwordDialog == null) {
-      return null;
-    }
-
-    assertTrue(passwordDialog.shadowRoot != null);
-    return querySelectorAsync(passwordDialog.shadowRoot, '#passwordPrompt');
+  private shadowRoot(): ShadowRoot {
+    const lockScreen = this.lockScreen();
+    assertTrue(lockScreen.shadowRoot != null);
+    return lockScreen.shadowRoot;
   }
 
   async authenticate(password: string): Promise<void> {
-    const passwordPrompt = await this.passwordPrompt();
+    const passwordDialog = await retryUntilSome(this.passwordDialog);
+    assertTrue(passwordDialog != null);
+    assertTrue(passwordDialog.shadowRoot != null);
+
+    const passwordPrompt =
+        passwordDialog.shadowRoot.getElementById('passwordPrompt');
     assertTrue(passwordPrompt != null);
     assertTrue(passwordPrompt.shadowRoot != null);
 
     const passwordInput =
-        await querySelectorAsync(passwordPrompt.shadowRoot, '#passwordInput');
+        passwordPrompt.shadowRoot.getElementById('passwordInput');
     assertTrue(passwordInput != null);
-    (passwordInput as HTMLElement & {value: any}).value = password;
+    assertTrue(hasProperty(passwordInput, 'value'));
+
+    passwordInput.value = password;
 
     const confirmButton =
-        await querySelectorAsync(passwordPrompt.shadowRoot, '#confirmButton');
-    assertTrue(confirmButton instanceof HTMLElement);
+        passwordPrompt.shadowRoot.getElementById('confirmButton');
+    assertTrue(confirmButton != null);
     confirmButton.click();
+
+    await assertAsync(() => this.passwordDialog() == null);
   }
 
-  async recoveryToggle(): Promise<HTMLElement|null> {
-    assertTrue(this.lockScreen.shadowRoot != null);
-    const toggle =
-        await querySelectorAsync(this.lockScreen.shadowRoot, '#recoveryToggle');
-    assertTrue(toggle == null || toggle instanceof HTMLElement);
+  private recoveryToggle(): HTMLElement&{checked: boolean}|null {
+    const toggle = this.shadowRoot().getElementById('recoveryToggle');
+    if (toggle == null) {
+      return null;
+    }
+    assertTrue(hasBooleanProperty(toggle, 'checked'));
     return toggle;
   }
 
   async assertRecoveryControlVisibility(isVisible: boolean): Promise<void> {
-    // TODO(b/239416325): We could be more thorough here -- check that the
-    // toggle does not have visibility: hidden, has non-zero size, is not
-    // covered by other elements etc.
-    const toggle = await this.recoveryToggle();
-    const toggleExists = toggle != null;
-    assertTrue(isVisible === toggleExists);
+    const property = () => {
+      const toggle = this.recoveryToggle();
+      if (toggle == null) {
+        return !isVisible;
+      }
+      return toggle.hidden === !isVisible;
+    };
+
+    await assertAsync(property);
+    await assertForDuration(property);
   }
 
   async assertRecoveryConfigured(isConfigured: boolean): Promise<void> {
-    const toggle =
-        await this.recoveryToggle() as HTMLElement & {checked: boolean};
-    assertTrue(toggle.checked === isConfigured);
+    const property = () => {
+      const toggle = this.recoveryToggle();
+      if (toggle == null) {
+        return false;
+      }
+      return toggle.checked === isConfigured;
+    };
+
+    await assertAsync(property);
+    await assertForDuration(property);
   }
 
   async toggleRecoveryConfiguration(): Promise<void> {
-    const toggle = await this.recoveryToggle() as HTMLElement &
-        {checked: boolean, disabled: boolean};
+    const toggle = await retryUntilSome(() => this.recoveryToggle());
     assertTrue(toggle != null);
+    const previousChecked = toggle.checked;
+    const toggleIsFlipped = () => toggle.checked === previousChecked;
 
-    const previouslyChecked = toggle.checked;
     toggle.click();
 
-    // If the toggle is immediately toggled, that's OK. Otherwise we wait wait
-    // until it becomes toggled.
-    if (toggle.checked === !previouslyChecked) {
+    // If the toggle flips immediately, that's OK. Otherwise we need to wait
+    // until it flips.
+    if (!toggleIsFlipped()) {
       return;
     }
 
-    assertTrue(toggle.disabled);
+    assertTrue(hasBooleanProperty(toggle, 'disabled') && toggle.disabled);
     // Click again to see whether something weird happens.
     toggle.click();
 
-    // Check that toggle is eventually checked.
-    const notNullIfToggled = await watchElement(toggle, (toggle) => {
-      if (toggle.checked === !previouslyChecked) {
-        return {};
-      }
-      return null;
-    });
-
-    assertTrue(notNullIfToggled != null);
+    await assertAsync(toggleIsFlipped);
   }
 }
 
 class OSSettingsDriver implements OSSettingsDriverInterface {
   async goToLockScreenSettings():
       Promise<{lockScreenSettings: LockScreenSettingsRemote}> {
-    const privacyPage = await querySelectorAsyncShadowRoots(document.body, [
-      'os-settings-ui',
-      'os-settings-main',
-      'os-settings-page',
-      'os-settings-privacy-page',
-    ]);
-    assertTrue(privacyPage != null);
+    const privacyPage =
+        await retryUntilSome(() => querySelectorShadow(document.body, [
+                               'os-settings-ui',
+                               'os-settings-main',
+                               'os-settings-page',
+                               'os-settings-privacy-page',
+                             ]));
+    assertTrue(privacyPage instanceof HTMLElement);
     assertTrue(privacyPage.shadowRoot != null);
 
-    const trigger = await querySelectorAsync(
-        privacyPage.shadowRoot, '#lockScreenSubpageTrigger');
-    assertTrue(trigger instanceof HTMLElement);
+
+    // Click on button to go to lock screen settings.
+    const trigger =
+        privacyPage.shadowRoot.getElementById('lockScreenSubpageTrigger');
+    assertTrue(trigger != null);
     trigger.click();
 
-    const lockScreen = new LockScreenSettings(privacyPage);
-    const receiver = new LockScreenSettingsReceiver(lockScreen);
+    const lockScreen: Lazy<HTMLElement> = () => {
+      assertTrue(privacyPage.shadowRoot != null);
+      const lockScreen =
+          privacyPage.shadowRoot.querySelector('settings-lock-screen');
+      assertTrue(lockScreen instanceof HTMLElement);
+      return lockScreen;
+    };
+
+    const passwordDialog: Lazy<HTMLElement|null> = () => {
+      assertTrue(privacyPage.shadowRoot != null);
+      const passwordDialog =
+          privacyPage.shadowRoot.getElementById('passwordDialog');
+      if (passwordDialog == null) {
+        return null;
+      }
+
+      return passwordDialog;
+    };
+
+    const lockScreenSettings =
+        new LockScreenSettings({lockScreen, passwordDialog});
+    const receiver = new LockScreenSettingsReceiver(lockScreenSettings);
     const remote = receiver.$.bindNewPipeAndPassRemote();
 
     return {lockScreenSettings: remote};
