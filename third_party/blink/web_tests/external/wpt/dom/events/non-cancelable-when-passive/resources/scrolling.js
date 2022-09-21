cb60e1a33e5695a31740f5386946fb1e0fc330a3
Mehdi Kazemi <mehdika@chromium.org>
2022-09-21 21:38:38 +0000

    Speculative deflake WPTs for non-passive wheel events
    Delay test run until after the page has rendered.
    
    Bug: 1348696,1346158,1344591,1344175,1343935,1343664,1342526
    Change-Id: Idcbfe6d69ef772242cb5fe1409949560739834f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3906105
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Mehdi Kazemi <mehdika@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049880}

--- third_party/blink/web_tests/external/wpt/dom/events/non-cancelable-when-passive/resources/scrolling.js
+++ third_party/blink/web_tests/external/wpt/dom/events/non-cancelable-when-passive/resources/scrolling.js
@@ -1,4 +1,15 @@
-function runTest({target, eventName, passive, expectCancelable}) {
+function raf() {
+  return new Promise((resolve) => {
+    // rAF twice.
+    window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(resolve);
+    });
+  });
+}
+
+async function runTest({target, eventName, passive, expectCancelable}) {
+  await raf();
+
   let cancelable = null;
   let arrived = false;
   target.addEventListener(eventName, function (event) {
@@ -14,6 +25,7 @@ function runTest({target, eventName, passive, expectCancelable}) {
     const pos_y = Math.floor(window.innerHeight / 2);
     const delta_x = 0;
     const delta_y = 100;
+
     await new test_driver.Actions()
       .scroll(pos_x, pos_y, delta_x, delta_y).send();
     await waitFor(() => arrived);
