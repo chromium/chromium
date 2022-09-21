04b9cf751c90254648fb46f7289eef872c2fc905
Brad Triebwasser <btriebw@chromium.org>
2022-09-21 21:36:37 +0000

    Add web platform tests for multi-screen fullscreen.
    Adds a manual web platform test which tests fullscreen with target displays and swapping between them. Also cleans up helpers.js and refactors some of the existing tests to re-use some common code.
    
    Bug: 1022988
    Change-Id: I239b5c52d3e7ab71702002018f3cbdc3a247716d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3899616
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049877}

--- third_party/blink/web_tests/external/wpt/window-placement/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/window-placement/resources/helpers.js
@@ -1,8 +1,12 @@
-// Logs (append) an HTML string to the document in a list format.
+
+// Logs (appends) an HTML string to a logger element in a list format.
+// An element in the document with id "logger" will be used as the log
+// container.
 function log(str) {
   const entry = document.createElement('li');
   entry.innerHTML = str;
-  logger.appendChild(entry);
+  const loggerElement = document.getElementById('logger');
+  loggerElement.appendChild(entry);
   return entry;
 }
 
@@ -35,4 +39,30 @@ async function setUpWindowPlacement(setUpTest, setUpButton) {
   }
   await setUpClick;
   setUpButton.disabled = true;
+}
+
+
+// Adds a button to the given `buttonContainer` element with the contents of
+// `name`. Attaches an event watcher to the given test and waits for a signal
+// from the test driver to click the button. If no test driver is available
+// (manual testing) then awaits an actual click from the user instead. If
+// `disableOnClick` is true, the button will also be disabled after it is
+// clicked.
+async function addTestTriggerButtonAndAwaitClick(buttonContainer, name, test) {
+  const button = document.createElement('button');
+  button.innerHTML = name;
+  const entry = document.createElement('li');
+  entry.appendChild(button);
+  buttonContainer.appendChild(entry);
+  const testWatcher = new EventWatcher(test, button, ['click']);
+  const buttonClick = testWatcher.wait_for('click');
+  // Disable the button when it is clicked.
+  button.onclick = function() {
+    button.disabled = true;
+  };
+  try {  // Support manual testing where test_driver is not running.
+    await test_driver.click(button);
+  } catch {
+  }
+  await buttonClick;
 }
