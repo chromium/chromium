c144231868cea4ec27b92583ee52f6fd87533000
Tom Anderson <thomasanderson@chromium.org>
2022-09-22 20:49:40 +0000

    Generate qt_shim_moc.cc with an ancient version of moc
    The current qt_shim_moc.cc was generated with moc from QT 5.15.2.  When
    building without the sysroot, this may cause build failures if the QT
    headers are too old.  This CL regenerates qt_shim_moc.cc with an ancient
    version of moc for maximum compatibility.
    
    This CL also adds gen_qt_shim_moc.sh to more easily regenerate
    qt_shim_moc.cc.
    
    R=thestig
    
    Fixed: 1365358
    Change-Id: I1fdfc17f2ae81eaab2897385db3dcf85f9d3c2a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911697
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050381}

--- ui/qt/qt_shim_moc.cc
+++ ui/qt/qt_shim_moc.cc
@@ -5,44 +5,35 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'qt_shim.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
-#include <memory>
 #include "ui/qt/qt_shim.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'qt_shim.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.2. It"
+#error "This file was generated using the moc from 5.3.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-QT_WARNING_PUSH
-QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_qt__QtShim_t {
   QByteArrayData data[6];
-  char stringdata0[52];
+  char stringdata[52];
 };
-#define QT_MOC_LITERAL(idx, ofs, len)                                        \
-  Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(                   \
-      len, qptrdiff(offsetof(qt_meta_stringdata_qt__QtShim_t, stringdata0) + \
+#define QT_MOC_LITERAL(idx, ofs, len)                                       \
+  Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(                  \
+      len, qptrdiff(offsetof(qt_meta_stringdata_qt__QtShim_t, stringdata) + \
                     ofs - idx * sizeof(QByteArrayData)))
 static const qt_meta_stringdata_qt__QtShim_t qt_meta_stringdata_qt__QtShim = {
-    {
-        QT_MOC_LITERAL(0, 0, 10),   // "qt::QtShim"
-        QT_MOC_LITERAL(1, 11, 11),  // "FontChanged"
-        QT_MOC_LITERAL(2, 23, 0),   // ""
-        QT_MOC_LITERAL(3, 24, 4),   // "font"
-        QT_MOC_LITERAL(4, 29, 14),  // "PaletteChanged"
-        QT_MOC_LITERAL(5, 44, 7)    // "palette"
-
-    },
+    {QT_MOC_LITERAL(0, 0, 10), QT_MOC_LITERAL(1, 11, 11),
+     QT_MOC_LITERAL(2, 23, 0), QT_MOC_LITERAL(3, 24, 4),
+     QT_MOC_LITERAL(4, 29, 14), QT_MOC_LITERAL(5, 44, 7)},
     "qt::QtShim\0FontChanged\0\0font\0"
     "PaletteChanged\0palette"};
 #undef QT_MOC_LITERAL
@@ -50,7 +41,7 @@ static const qt_meta_stringdata_qt__QtShim_t qt_meta_stringdata_qt__QtShim = {
 static const uint qt_meta_data_qt__QtShim[] = {
 
     // content:
-    8,      // revision
+    7,      // revision
     0,      // classname
     0, 0,   // classinfo
     2, 14,  // methods
@@ -75,8 +66,7 @@ void qt::QtShim::qt_static_metacall(QObject* _o,
                                     int _id,
                                     void** _a) {
   if (_c == QMetaObject::InvokeMetaMethod) {
-    auto* _t = static_cast<QtShim*>(_o);
-    Q_UNUSED(_t)
+    QtShim* _t = static_cast<QtShim*>(_o);
     switch (_id) {
       case 0:
         _t->FontChanged((*reinterpret_cast<const QFont(*)>(_a[1])));
@@ -89,10 +79,9 @@ void qt::QtShim::qt_static_metacall(QObject* _o,
   }
 }
 
-QT_INIT_METAOBJECT const QMetaObject qt::QtShim::staticMetaObject = {
-    {QMetaObject::SuperData::link<QObject::staticMetaObject>(),
-     qt_meta_stringdata_qt__QtShim.data, qt_meta_data_qt__QtShim,
-     qt_static_metacall, nullptr, nullptr}};
+const QMetaObject qt::QtShim::staticMetaObject = {
+    {&QObject::staticMetaObject, qt_meta_stringdata_qt__QtShim.data,
+     qt_meta_data_qt__QtShim, qt_static_metacall, 0, 0}};
 
 const QMetaObject* qt::QtShim::metaObject() const {
   return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject()
@@ -101,11 +90,11 @@ const QMetaObject* qt::QtShim::metaObject() const {
 
 void* qt::QtShim::qt_metacast(const char* _clname) {
   if (!_clname)
-    return nullptr;
-  if (!strcmp(_clname, qt_meta_stringdata_qt__QtShim.stringdata0))
-    return static_cast<void*>(this);
+    return 0;
+  if (!strcmp(_clname, qt_meta_stringdata_qt__QtShim.stringdata))
+    return static_cast<void*>(const_cast<QtShim*>(this));
   if (!strcmp(_clname, "QtInterface"))
-    return static_cast<QtInterface*>(this);
+    return static_cast<QtInterface*>(const_cast<QtShim*>(this));
   return QObject::qt_metacast(_clname);
 }
 
@@ -124,5 +113,4 @@ int qt::QtShim::qt_metacall(QMetaObject::Call _c, int _id, void** _a) {
   }
   return _id;
 }
-QT_WARNING_POP
 QT_END_MOC_NAMESPACE
