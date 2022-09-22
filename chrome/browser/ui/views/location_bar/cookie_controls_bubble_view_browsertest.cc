e6293246d64944b939c9ecfee7976f654eb65f80
Igor Ruvinov <igorruvinov@chromium.org>
2022-09-22 21:31:01 +0000

    [Sheriff] Disable flaky bubble view test InvokeUi_NotWorkingClicked.
    The test is flaky on all platforms.
    
    Flake portal (score of 78):
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyWQsSBUZsYWtlIk5jaHJvbWl1bUBicm93c2VyX3Rlc3RzQENvb2tpZUNvbnRyb2xzQnViYmxlVmlld1Rlc3QuSW52b2tlVWlfTm90V29ya2luZ0NsaWNrZWQM
    
    Bug: 1332525
    Change-Id: I4fcdb386ea892312551262a5ab879bf584ce35c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913448
    Owners-Override: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Olivia Yingst <huiyingst@google.com>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050398}

--- chrome/browser/ui/views/location_bar/cookie_controls_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls_bubble_view_browsertest.cc
@@ -160,14 +160,9 @@ IN_PROC_BROWSER_TEST_F(CookieControlsBubbleViewTest, DISABLED_InvokeUi_CookiesBl
 }
 
 // Test opening cookie controls bubble and clicking on "not working" link.
-// TODO(https://crbug.com/1332525): Failing on Mac Arm64.
-#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
-#define MAYBE_InvokeUi_NotWorkingClicked DISABLED_InvokeUi_NotWorkingClicked
-#else
-#define MAYBE_InvokeUi_NotWorkingClicked InvokeUi_NotWorkingClicked
-#endif
+// TODO(https://crbug.com/1332525): Flaky on all platforms.
 IN_PROC_BROWSER_TEST_F(CookieControlsBubbleViewTest,
-                       MAYBE_InvokeUi_NotWorkingClicked) {
+                       DISABLED_InvokeUi_NotWorkingClicked) {
   // Block 3p cookies.
   SetThirdPartyCookieBlocking(true);
 
