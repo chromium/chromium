a040ca1a75c066e503a6c48133db7a207d944ab0
Sky Malice <skym@chromium.org>
2022-09-22 16:35:16 +0000

    Default tint mode to SRC_IN for ChromeImageButton.
    The default for ImageButton is SRC_ATOP, which does not seem to work
    well with tints with alpha that's not fully opaque. The icon becomes
    more white, instead of letting whatever's behind it blend through.
    
    Bug: 1366631
    Change-Id: I521e7d2d1f06b2918324e8d4c3647cf83e188afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911145
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050236}

--- ui/android/java/src/org/chromium/ui/widget/ChromeImageButton.java
+++ ui/android/java/src/org/chromium/ui/widget/ChromeImageButton.java
@@ -5,6 +5,7 @@
 package org.chromium.ui.widget;
 
 import android.content.Context;
+import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
@@ -26,6 +27,17 @@ public class ChromeImageButton extends AppCompatImageButton {
 
     public ChromeImageButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        // ImageView defaults to SRC_ATOP when there's a tint. This interacts poorly with tints that
+        // contain alpha, so adjust the default to SRC_IN when this case is found. This will cause
+        // the drawable to be mutated, but the tint should already be causing that anyway.
+        if (getImageTintList() != null && getImageTintMode() == Mode.SRC_ATOP) {
+            setImageTintMode(Mode.SRC_IN);
+        }
     }
 
     @Override
