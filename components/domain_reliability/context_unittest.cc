3a55ee2db6aad41a63e5652c291f31fde9840e7f
David Bertoni <dbertoni@chromium.org>
2022-09-22 00:37:26 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /components/domain_reliability.
    This CL was uploaded by git cl split.
    
    R=ricea@chromium.org
    
    Bug: 1303949
    Change-Id: I49b38ac95114a6eb0d9ff1d8ad93a79cb956eb82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908819
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1049971}

--- components/domain_reliability/context_unittest.cc
+++ components/domain_reliability/context_unittest.cc
@@ -110,9 +110,9 @@ bool GetEntryFromReport(const Value* report,
   if (!report || !report->is_dict())
     return false;
   const Value* entries = report->FindListKey("entries");
-  if (!entries || index >= entries->GetListDeprecated().size())
+  if (!entries || index >= entries->GetList().size())
     return false;
-  const Value& entry = entries->GetListDeprecated()[index];
+  const Value& entry = entries->GetList()[index];
   if (!entry.is_dict())
     return false;
   *entry_out = &entry;
