2152a139af1b032e3a9fb00837964d7667af30de
Igor Ruvinov <igorruvinov@chromium.org>
2022-09-22 19:42:24 +0000

    Revert "[code-health] Migrate create_zip to python 3"
    This reverts commit 2fae8a5b73242ba6673f84ca63d66a874c53eba5.
    
    Reason for revert: May be causing mini_installer RC failure https://ci.chromium.org/ui/p/chromium/builders/ci/win-archive-dbg/28065/overview
    
    Original change's description:
    > [code-health] Migrate create_zip to python 3
    >
    > * Update instructions to match.
    > * Update bat file to use python 3 from PATH rather than a hardcoded
    >   location.
    >
    > Bug: 1291048
    > Change-Id: Ie84e9baa76da56900f2f2c6fc4eca3787d8dc360
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3564140
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1049735}
    
    Bug: 1291048
    Change-Id: I3d5ae334d4742e575355f902452c398e3ff733a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914904
    Owners-Override: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1050340}

--- chrome/test/mini_installer/zip_test_runner.bat
+++ chrome/test/mini_installer/zip_test_runner.bat
@@ -6,9 +6,8 @@ rem will be passed to run_mini_installer_tests.py. See
 rem chrome\test\mini_installer\run_mini_installer_tests.py for the args that
 rem can be passed.
 
-python3 -m pip install --upgrade pip
-python3 -m pip install psutil
-python3 -m pip install requests
-python3 -m pip install pypiwin32
-python3 chrome\test\mini_installer\run_mini_installer_tests.py {run_args} %*
+c:\Python27\python -m pip install --upgrade pip
+c:\Python27\Scripts\pip install psutil
+c:\Python27\Scripts\pip install pywin32
+c:\Python27\python chrome\test\mini_installer\run_mini_installer_tests.py {run_args} %*
 pause
