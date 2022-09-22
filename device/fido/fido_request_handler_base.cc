99787db5bb6387d1acfd445f584f870abccd9a44
Martin Kreichgauer <martinkr@google.com>
2022-09-22 18:23:05 +0000

    webauthn: Offer platform authenticator only with matching credential
    Change FidoRequestHandlerBase::OnHavePlatformCredentialStatus() to erase
    the internal transport from the list of available transports if the
    platform authenticator reports that it has no matching credential for
    the current GetAssertion request. This causes the "This device" option
    not to appear in mechanism selection in these cases.
    
    This change is gated behind the
    `device::kWebAuthnNewDiscoverableCredentialsUi` feature (default off).
    
    Bug: 1358719
    Change-Id: I28fc98c069ef67fc8a49578aa391d68a64acbc52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3911345
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050294}

--- device/fido/fido_request_handler_base.cc
+++ device/fido/fido_request_handler_base.cc
@@ -9,6 +9,7 @@
 #include "base/bind.h"
 #include "base/containers/contains.h"
 #include "base/containers/cxx20_erase.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/strings/string_piece.h"
@@ -19,6 +20,7 @@
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/fido/ble_adapter_manager.h"
 #include "device/fido/discoverable_credential_metadata.h"
+#include "device/fido/features.h"
 #include "device/fido/fido_authenticator.h"
 #include "device/fido/fido_discovery_factory.h"
 
@@ -401,13 +403,20 @@ void FidoRequestHandlerBase::OnHavePlatformCredentialStatus(
     bool have_credential) {
   DCHECK_EQ(transport_availability_info_.has_platform_authenticator_credential,
             RecognizedCredential::kUnknown);
-
-  transport_availability_info_.has_platform_authenticator_credential =
-      have_credential ? RecognizedCredential::kHasRecognizedCredential
-                      : RecognizedCredential::kNoRecognizedCredential;
-  transport_availability_info_.recognized_platform_authenticator_credentials =
-      std::move(creds);
-
+  if (base::FeatureList::IsEnabled(
+          device::kWebAuthnNewDiscoverableCredentialsUi) &&
+      !have_credential) {
+    transport_availability_info_.has_platform_authenticator_credential =
+        RecognizedCredential::kNoRecognizedCredential;
+    transport_availability_info_.available_transports.erase(
+        FidoTransportProtocol::kInternal);
+  } else {
+    transport_availability_info_.has_platform_authenticator_credential =
+        have_credential ? RecognizedCredential::kHasRecognizedCredential
+                        : RecognizedCredential::kNoRecognizedCredential;
+    transport_availability_info_.recognized_platform_authenticator_credentials =
+        std::move(creds);
+  }
   transport_availability_callback_readiness_
       ->platform_credential_check_pending = false;
   MaybeSignalTransportsEnumerated();
