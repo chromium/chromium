55cfd2931f92c10e3c72d6e1caed31798d5684a6
Hira Mahmood <hiramahmood@google.com>
2022-09-22 14:15:05 +0000

    Register App Store Rating in PromosManagerCoordinator.
    This change registers the AppStoreRatingDisplayHandler in
    PromosManagerCoordinator under the kAppStoreRating feature flag.
    
    Bug: 1359065
    Change-Id: I4b56b704b819ac0fa6a3d70b0395d16fb3671ff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3910962
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050173}

--- ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
+++ ios/chrome/browser/ui/promos_manager/promos_manager_coordinator.mm
@@ -13,6 +13,8 @@
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/application_context/application_context.h"
 #import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/ui/app_store_rating/app_store_rating_display_handler.h"
+#import "ios/chrome/browser/ui/app_store_rating/features.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/promos_manager_commands.h"
 #import "ios/chrome/browser/ui/post_restore_signin/features.h"
@@ -328,7 +330,10 @@
 
 - (void)registerPromos {
   // Add StandardPromoDisplayHandler promos here. For example:
-  // TODO(crbug.com/1360880): Create first StandardPromoDisplayHandler promo.
+  if (IsAppStoreRatingEnabled()) {
+    _displayHandlerPromos[promos_manager::Promo::AppStoreRating] =
+        [[AppStoreRatingDisplayHandler alloc] init];
+  }
 
   // Add StandardPromoViewProvider promos here. For example:
   // TODO(crbug.com/1360880): Create first StandardPromoViewProvider promo.
