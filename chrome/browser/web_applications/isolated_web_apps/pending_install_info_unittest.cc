6a6697f24e5d8d0c1bc34be5453104877d334135
Dmitrii Kuragin <kuragin@chromium.org>
2022-09-23 03:51:39 +0000

    Add `PendingInstallInfo` class to pass isolation data from install command to url loader factory.
    Bug: 1333966
    Change-Id: I3ddc70102f2f83d231f1705c1bd98d226c81a6c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3905059
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Auto-Submit: Dmitrii Kuragin <kuragin@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050505}

---
+++ chrome/browser/web_applications/isolated_web_apps/pending_install_info_unittest.cc
@@ -0,0 +1,121 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/isolated_web_apps/pending_install_info.h"
+
+#include <memory>
+
+#include "base/check.h"
+#include "base/files/file_path.h"
+#include "base/memory/raw_ref.h"
+#include "chrome/browser/web_applications/isolation_data.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace web_app {
+namespace {
+
+using ::testing::Eq;
+using ::testing::IsFalse;
+using ::testing::Optional;
+
+std::unique_ptr<TestingProfile> CreateTestingProfile() {
+  TestingProfile::Builder builder;
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  builder.SetIsMainProfile(true);
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
+  return builder.Build();
+}
+
+class WebContentsContainer {
+ public:
+  explicit WebContentsContainer(content::BrowserContext& context)
+      : context_(context) {}
+
+  content::WebContents& web_contents() { return *web_contents_; }
+
+ private:
+  base::raw_ref<content::BrowserContext> context_;
+
+  std::unique_ptr<content::WebContents> web_contents_ =
+      content::WebContents::Create(
+          content::WebContents::CreateParams(&context_));
+};
+
+class PendingInstallInfoTest : public ::testing::Test {
+ protected:
+  content::WebContents& web_contents() {
+    return web_contents_container_.web_contents();
+  }
+
+ private:
+  content::BrowserTaskEnvironment browser_task_environment_;
+
+  std::unique_ptr<TestingProfile> profile_ = CreateTestingProfile();
+  WebContentsContainer web_contents_container_{/*context=*/*profile_};
+};
+
+TEST_F(PendingInstallInfoTest, SameInstallInfoForTheSameWebContents) {
+  auto& one = IsolatedWebAppPendingInstallInfo::FromWebContents(web_contents());
+  auto& two = IsolatedWebAppPendingInstallInfo::FromWebContents(web_contents());
+  EXPECT_EQ(&one, &two);
+}
+
+TEST_F(PendingInstallInfoTest, DifferentInstancesForDifferentWebContents) {
+  std::unique_ptr<TestingProfile> profile = CreateTestingProfile();
+
+  WebContentsContainer web_contents_container_one{/*context=*/*profile};
+  WebContentsContainer web_contents_container_two{/*context=*/*profile};
+
+  EXPECT_NE(&IsolatedWebAppPendingInstallInfo::FromWebContents(
+                web_contents_container_one.web_contents()),
+            &IsolatedWebAppPendingInstallInfo::FromWebContents(
+                web_contents_container_two.web_contents()));
+}
+
+TEST_F(PendingInstallInfoTest, CanSetAndGetIsolationData) {
+  auto& install_info =
+      IsolatedWebAppPendingInstallInfo::FromWebContents(web_contents());
+  install_info.set_isolation_data(IsolationData{
+      IsolationData::DevModeProxy{.proxy_url = "some testing proxy url"}});
+
+  EXPECT_THAT(install_info.isolation_data(),
+              Optional(Eq(IsolationData{IsolationData::DevModeProxy{
+                  .proxy_url = "some testing proxy url"}})));
+}
+
+TEST_F(PendingInstallInfoTest, CanSetAndGetAnotherIsolationData) {
+  auto& install_info =
+      IsolatedWebAppPendingInstallInfo::FromWebContents(web_contents());
+  install_info.set_isolation_data(IsolationData{IsolationData::DevModeBundle{
+      .path = base::FilePath{FILE_PATH_LITERAL("some testing bundle path")}}});
+
+  EXPECT_THAT(install_info.isolation_data(),
+              Optional(Eq(IsolationData{IsolationData::DevModeBundle{
+                  .path = base::FilePath{
+                      FILE_PATH_LITERAL("some testing bundle path")}}})));
+}
+
+TEST_F(PendingInstallInfoTest, IsolationDataIsEmptyAfterReset) {
+  auto& install_info =
+      IsolatedWebAppPendingInstallInfo::FromWebContents(web_contents());
+
+  EXPECT_THAT(install_info.isolation_data().has_value(), IsFalse());
+
+  install_info.set_isolation_data(IsolationData{IsolationData::DevModeBundle{
+      .path = base::FilePath{FILE_PATH_LITERAL("some testing bundle path")}}});
+
+  install_info.ResetIsolationData();
+
+  EXPECT_THAT(install_info.isolation_data().has_value(), IsFalse());
+}
+
+}  // namespace
+}  // namespace web_app
