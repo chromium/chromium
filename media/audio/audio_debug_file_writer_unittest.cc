72f2a6822394401d1e0d33ec45aee57b8ac8edf7
Fredrik Hernqvist <fhernqvist@google.com>
2022-09-23 16:19:57 +0000

    Cache AudioBuses when debug recording
    In order to reduce memory allocation on realtime threads. This is done
    by introducing AudioBusPool.
    
    Fixed: 1340561
    Change-Id: I43aae520d27d641571c5b8acbe3e655c3d94e237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3904625
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050682}

--- media/audio/audio_debug_file_writer_unittest.cc
+++ media/audio/audio_debug_file_writer_unittest.cc
@@ -15,11 +15,15 @@
 #include "base/sys_byteorder.h"
 #include "base/test/task_environment.h"
 #include "base/threading/thread.h"
+#include "media/audio/audio_bus_pool.h"
 #include "media/audio/audio_debug_file_writer.h"
 #include "media/base/audio_bus.h"
+#include "media/base/audio_parameters.h"
 #include "media/base/audio_sample_types.h"
 #include "media/base/test_helpers.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using testing::_;
 
 namespace media {
 
@@ -42,6 +46,51 @@ uint32_t ReadLE4(const char* buf) {
 base::File OpenFile(const base::FilePath& file_path) {
   return base::File(file_path, base::File::FLAG_OPEN | base::File::FLAG_WRITE);
 }
+
+// Serves only to expose the protected Create method of AudioDebugFileWriter.
+class AudioDebugFileWriterUnderTest : public AudioDebugFileWriter {
+ public:
+  // Should not be instantiated
+  AudioDebugFileWriterUnderTest() = delete;
+
+  static Ptr Create(const AudioParameters& params,
+                    base::File file,
+                    std::unique_ptr<AudioBusPool> audio_bus_pool) {
+    return AudioDebugFileWriter::Create(params, std::move(file),
+                                        std::move(audio_bus_pool));
+  }
+};
+
+class MockAudioBusPool : public AudioBusPool {
+ public:
+  explicit MockAudioBusPool(const AudioParameters& params) : params_(params) {}
+
+  MockAudioBusPool(const MockAudioBusPool&) = delete;
+  MockAudioBusPool& operator=(const MockAudioBusPool&) = delete;
+  ~MockAudioBusPool() override = default;
+
+  std::unique_ptr<AudioBus> GetAudioBus() override {
+    if (audio_bus_to_return_) {
+      return std::move(audio_bus_to_return_);
+    }
+    return AudioBus::Create(params_);
+  }
+
+  MOCK_METHOD1(OnInsertAudioBus, void(AudioBus*));
+
+  void InsertAudioBus(std::unique_ptr<AudioBus> audio_bus) override {
+    OnInsertAudioBus(audio_bus.get());
+  }
+
+  void SetNextAudioBus(std::unique_ptr<AudioBus> audio_bus) {
+    audio_bus_to_return_ = std::move(audio_bus);
+  }
+
+ private:
+  const AudioParameters params_;
+
+  std::unique_ptr<AudioBus> audio_bus_to_return_;
+};
 
 }  // namespace
 
@@ -171,7 +220,10 @@ class AudioDebugFileWriterTest
     }
   }
 
-  void DoDebugRecording() {
+  void DoDebugRecording(bool expect_buses_returned_to_pool = true) {
+    EXPECT_CALL(*mock_audio_bus_pool_, OnInsertAudioBus(_))
+        .Times(expect_buses_returned_to_pool ? writes_ : 0);
+
     for (int i = 0; i < writes_; ++i) {
       std::unique_ptr<AudioBus> bus =
           AudioBus::Create(params_.channels(), params_.frames_per_buffer());
@@ -181,8 +233,15 @@ class AudioDebugFileWriterTest
               i * params_.channels() * params_.frames_per_buffer(),
           params_.frames_per_buffer());
 
-      debug_writer_->Write(std::move(bus));
+      debug_writer_->Write(*bus);
     }
+  }
+
+  void CreateDebugWriter(base::File file) {
+    auto audio_bus_pool = std::make_unique<MockAudioBusPool>(params_);
+    mock_audio_bus_pool_ = audio_bus_pool.get();
+    debug_writer_ = AudioDebugFileWriterUnderTest::Create(
+        params_, std::move(file), std::move(audio_bus_pool));
   }
 
  protected:
@@ -191,6 +250,9 @@ class AudioDebugFileWriterTest
 
   // Writer under test.
   AudioDebugFileWriter::Ptr debug_writer_;
+
+  // Pointer to the AudioBusPool of the most recently created writer.
+  MockAudioBusPool* mock_audio_bus_pool_;
 
   // AudioBus parameters.
   AudioParameters params_;
@@ -220,7 +282,7 @@ TEST_P(AudioDebugFileWriterTest, WaveRecordingTest) {
   base::File file = OpenFile(file_path);
   ASSERT_TRUE(file.IsValid());
 
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file));
+  CreateDebugWriter(std::move(file));
 
   DoDebugRecording();
 
@@ -237,6 +299,27 @@ TEST_P(AudioDebugFileWriterTest, WaveRecordingTest) {
   }
 }
 
+TEST_P(AudioDebugFileWriterTest, ShouldReuseAudioBusesWithPool) {
+  base::FilePath file_path;
+  ASSERT_TRUE(base::CreateTemporaryFile(&file_path));
+  base::File file = OpenFile(file_path);
+  ASSERT_TRUE(file.IsValid());
+
+  CreateDebugWriter(std::move(file));
+
+  // Set a specific audio bus to be returned by the pool.
+  std::unique_ptr<AudioBus> reference_audio_bus = AudioBus::Create(params_);
+  AudioBus* reference_audio_bus_ptr = reference_audio_bus.get();
+  mock_audio_bus_pool_->SetNextAudioBus(std::move(reference_audio_bus));
+
+  // Expect that same audio bus to be returned to the pool.
+  EXPECT_CALL(*mock_audio_bus_pool_, OnInsertAudioBus(reference_audio_bus_ptr));
+
+  std::unique_ptr<AudioBus> bus = AudioBus::Create(params_);
+  debug_writer_->Write(*bus);
+  task_environment_.RunUntilIdle();
+}
+
 TEST_P(AudioDebugFileWriterSingleThreadTest,
        DeletedBeforeRecordingFinishedOnFileThread) {
   base::FilePath file_path;
@@ -244,7 +327,7 @@ TEST_P(AudioDebugFileWriterSingleThreadTest,
   base::File file = OpenFile(file_path);
   ASSERT_TRUE(file.IsValid());
 
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file));
+  CreateDebugWriter(std::move(file));
 
   DoDebugRecording();
 
@@ -263,9 +346,9 @@ TEST_P(AudioDebugFileWriterSingleThreadTest,
 
 TEST_P(AudioDebugFileWriterBehavioralTest, StartWithInvalidFile) {
   base::File file;  // Invalid file, recording should not crash
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file));
+  CreateDebugWriter(std::move(file));
 
-  DoDebugRecording();
+  DoDebugRecording(/*expect_buses_returned_to_pool = */ false);
   task_environment_.RunUntilIdle();
 }
 
@@ -280,9 +363,9 @@ TEST_P(AudioDebugFileWriterBehavioralTest, StartStopStartStop) {
   base::File file2 = OpenFile(file_path2);
   ASSERT_TRUE(file2.IsValid());
 
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file1));
+  CreateDebugWriter(std::move(file1));
   DoDebugRecording();
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file2));
+  CreateDebugWriter(std::move(file2));
   DoDebugRecording();
   debug_writer_.reset();
   task_environment_.RunUntilIdle();
@@ -305,7 +388,7 @@ TEST_P(AudioDebugFileWriterBehavioralTest, DestroyStarted) {
   ASSERT_TRUE(base::CreateTemporaryFile(&file_path));
   base::File file = OpenFile(file_path);
   ASSERT_TRUE(file.IsValid());
-  debug_writer_ = AudioDebugFileWriter::Create(params_, std::move(file));
+  CreateDebugWriter(std::move(file));
 
   debug_writer_.reset();
   task_environment_.RunUntilIdle();
