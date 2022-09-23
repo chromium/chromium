e626f28b829abafd42e6a50e8297832997d0e542
Olesia Marukhno <olesiamarukhno@google.com>
2022-09-23 12:14:01 +0000

    [site data] Display partitioned site state
    The original CL was reverted here: https://chromium-review.googlesource.com/c/chromium/src/+/3905752. The CL added flaky tests. Flakiness was caused by setting cookies in JS code executed from
    browser tests. To fix that, use test pages with cookies set on load.
    
    If the site has accessed only partitioned cookies, mark it as "Only accessing partitioned storage" (strings will be updated separately).
    
    To get the effective setting for partitioned access, get the content
    setting from the HostContentSettingMap instead of CookieSettings because
    partitioned access isn't affected by 3PC blocking.
    
    If there both partitioned and unpartitioned cookies:
    * if both are allowed, the state is "Allowed"
    * if both are blocked, the state is "Blocked"
    * if partitioned cookies are allowed (or session only) and regular cookies are blocked (because of 3PC blocking), display as "Only accessing partitioned storage". Only one entry is shown per host.
    
    Bug: 1344787
    Change-Id: I65284f667be384b08a3cf20ec74a37143f439615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913058
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1050587}

--- chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
@@ -6,6 +6,7 @@
 
 #include "chrome/browser/browsing_data/cookies_tree_model.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -17,6 +18,7 @@
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/content_settings/browser/page_specific_content_settings.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/omnibox/browser/favicon_cache.h"
 #include "components/strings/grit/components_strings.h"
@@ -33,6 +35,7 @@ namespace {
 struct PageSpecificSiteDataDialogSite {
   url::Origin origin;
   ContentSetting setting;
+  bool is_fully_partitioned;
 };
 
 struct PageSpecificSiteDataDialogSection {
@@ -139,6 +142,8 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
         HistoryServiceFactory::GetForProfile(
             profile, ServiceAccessType::EXPLICIT_ACCESS));
     cookie_settings_ = CookieSettingsFactory::GetForProfile(profile);
+    host_content_settings_map_ =
+        HostContentSettingsMapFactory::GetForProfile(profile);
   }
 
   void OnDialogExplicitlyClosed() {
@@ -168,18 +173,30 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
   }
 
   std::vector<PageSpecificSiteDataDialogSite> GetAllSites() {
-    // TODO(crbug.com/1344787): Keep a map of all origins to avoid having
-    // multiple entries. This could happen when there are both partitioned and
-    // regular cookies.
-    std::vector<PageSpecificSiteDataDialogSite> sites;
+    std::map<std::string, PageSpecificSiteDataDialogSite> sites_map;
     for (const auto& node :
          allowed_cookies_tree_model_->GetRoot()->children()) {
-      sites.push_back(CreateSiteFromHostNode(node.get()));
+      sites_map.emplace(node->GetDetailedInfo().origin.host(),
+                        CreateSiteFromHostNode(node.get()));
     }
     for (const auto& node :
          blocked_cookies_tree_model_->GetRoot()->children()) {
-      sites.push_back(CreateSiteFromHostNode(node.get()));
+      auto entry = sites_map.find(node->GetDetailedInfo().origin.host());
+      if (entry == sites_map.end()) {
+        sites_map.emplace(node->GetDetailedInfo().origin.host(),
+                          CreateSiteFromHostNode(node.get()));
+      } else {
+        // There is already an entry with the same origin. If that's the case,
+        // the entry should be from the allowed tree and it should be fully
+        // partitioned. The effective setting might be blocked if the site was
+        // blocked and the page wasn't reloaded.
+        DCHECK(entry->second.is_fully_partitioned);
+      }
     }
+
+    std::vector<PageSpecificSiteDataDialogSite> sites;
+    for (auto site : sites_map)
+      sites.push_back(site.second);
 
     std::sort(sites.begin(), sites.end(), [](const auto& o1, const auto& o2) {
       return GetContentSettingRowOrder(o1.setting) <
@@ -246,9 +263,48 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
         site.origin.GetURL(), current_url, &source,
         content_settings::CookieSettings::QueryReason::kCookies);
     // TODO(crbug.com/1344787): Handle sources other than SETTING_SOURCE_USER.
-    // TODO(crbug.com/1344787): Handle partitioned nodes.
+
+    site.is_fully_partitioned = AreAllCookiesPartitioned(node);
+    if (site.is_fully_partitioned) {
+      // Because partitioned cookies are considered first party cookies, if
+      // first party is blocked from accessing storage, partitioned cookies are
+      // too.
+      ContentSetting first_party_setting =
+          host_content_settings_map_->GetContentSetting(
+              current_url, GURL(), ContentSettingsType::COOKIES);
+      if (first_party_setting == CONTENT_SETTING_BLOCK) {
+        site.setting = CONTENT_SETTING_BLOCK;
+      } else {
+        // Check the explicit content setting from HostContentSettingsMap
+        // instead of CookieSettings because partitioned cookies aren't
+        // third-party cookies and are not influenced by third-party cookie
+        // blocking.
+        site.setting = host_content_settings_map_->GetContentSetting(
+            site.origin.GetURL(), current_url, ContentSettingsType::COOKIES);
+      }
+    }
 
     return site;
+  }
+
+  bool AreAllCookiesPartitioned(CookieTreeNode* node) {
+    bool all_partitioned = true;
+    for (const auto& storage_type_node : node->children()) {
+      if (storage_type_node->GetDetailedInfo().node_type !=
+          CookieTreeNode::DetailedInfo::TYPE_COOKIES) {
+        all_partitioned = false;
+        break;
+      }
+
+      for (const auto& cookie_node : storage_type_node->children()) {
+        if (!cookie_node->GetDetailedInfo().cookie->IsPartitioned()) {
+          all_partitioned = false;
+          break;
+        }
+      }
+    }
+
+    return all_partitioned;
   }
 
   base::WeakPtr<content::WebContents> web_contents_;
@@ -259,6 +315,7 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
   std::unique_ptr<CookiesTreeModel> blocked_cookies_tree_model_;
   std::unique_ptr<FaviconCache> favicon_cache_;
   scoped_refptr<content_settings::CookieSettings> cookie_settings_;
+  raw_ptr<HostContentSettingsMap> host_content_settings_map_;
 
   // Whether user has done any changes to the site data, deleted site data for a
   // site or created a content setting exception for a site.
@@ -278,7 +335,8 @@ class PageSpecificSiteDataSectionView : public views::BoxLayoutView {
       // the row view and the delegate are owned by the dialog and will be
       // destroyed when the dialog is destroyed.
       auto* const row_view = AddChildView(std::make_unique<SiteDataRowView>(
-          site.origin, site.setting, delegate->favicon_cache(),
+          site.origin, site.setting, site.is_fully_partitioned,
+          delegate->favicon_cache(),
           base::BindRepeating(
               &PageSpecificSiteDataDialogModelDelegate::DeleteStoredObjects,
               base::Unretained(delegate)),
--- chrome/browser/ui/views/site_data/page_specific_site_data_dialog_browsertest.cc
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog_browsertest.cc
@@ -5,6 +5,7 @@
 #include <stddef.h>
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/site_data/page_specific_site_data_dialog.h"
@@ -12,16 +13,35 @@
 #include "chrome/browser/ui/views/site_data/site_data_row_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/page_info/core/features.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/events/base_event_utils.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/interaction/element_tracker_views.h"
+#include "ui/views/test/button_test_api.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/widget/any_widget_observer.h"
+
+namespace {
+
+void ClickButton(views::Button* button) {
+  views::test::ButtonTestApi test_api(button);
+  ui::MouseEvent e(ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
+                   ui::EventTimeForNow(), 0, 0);
+  test_api.NotifyClick(e);
+}
+
+}  // namespace
 
 class PageSpecificSiteDataDialogBrowserTest
     : public InProcessBrowserTest,
@@ -30,6 +50,8 @@ class PageSpecificSiteDataDialogBrowserTest
   PageSpecificSiteDataDialogBrowserTest() {
     feature_list_.InitWithFeatureState(page_info::kPageSpecificSiteDataDialog,
                                        GetParam());
+    https_server_ = std::make_unique<net::EmbeddedTestServer>(
+        net::EmbeddedTestServer::TYPE_HTTPS);
   }
 
   PageSpecificSiteDataDialogBrowserTest(
@@ -41,13 +63,19 @@ class PageSpecificSiteDataDialogBrowserTest
 
   // InProcessBrowserTest:
   void SetUpOnMainThread() override {
+    https_server()->SetSSLConfig(net::EmbeddedTestServer::CERT_TEST_NAMES);
+    https_server()->ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
+    content::SetupCrossSiteRedirector(https_server());
+    ASSERT_TRUE(https_server()->Start());
 
     // Load a page with cookies.
     ASSERT_TRUE(ui_test_utils::NavigateToURL(
-        browser(), embedded_test_server()->GetURL("a.test", "/cookie1.html")));
+        browser(), https_server()->GetURL("a.test", "/cookie1.html")));
   }
+
+  net::EmbeddedTestServer* https_server() { return https_server_.get(); }
 
   views::Widget* OpenDialog() {
     std::string widget_name =
@@ -65,7 +93,18 @@ class PageSpecificSiteDataDialogBrowserTest
     auto* element_tracker = ui::ElementTracker::GetElementTracker();
     auto* tracked_element =
         element_tracker->GetFirstMatchingElement(id, context);
+    DCHECK(tracked_element);
     return tracked_element->AsA<views::TrackedElementViews>()->view();
+  }
+
+  views::View* GetViewByIdentifierAtIndex(ui::ElementContext context,
+                                          ui::ElementIdentifier id,
+                                          size_t index) {
+    auto* element_tracker = ui::ElementTracker::GetElementTracker();
+    auto tracked_elements =
+        element_tracker->GetAllMatchingElements(id, context);
+    DCHECK(tracked_elements.size() > index);
+    return tracked_elements[index]->AsA<views::TrackedElementViews>()->view();
   }
 
   void ClickDeleteMenuItem(SiteDataRowView* row_view) {
@@ -103,6 +142,7 @@ class PageSpecificSiteDataDialogBrowserTest
 
  private:
   base::test::ScopedFeatureList feature_list_;
+  std::unique_ptr<net::EmbeddedTestServer> https_server_;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -126,7 +166,7 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
   // Navigating to the another page with the same origin won't close dialog.
   auto* dialog = OpenDialog();
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("a.test", "/cookie2.html")));
+      browser(), https_server()->GetURL("a.test", "/cookie2.html")));
   EXPECT_FALSE(dialog->IsClosed());
 }
 
@@ -138,7 +178,7 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
   // Navigation in the owning tab will close dialog.
   auto* dialog = OpenDialog();
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("b.test", "/cookie2.html")));
+      browser(), https_server()->GetURL("b.test", "/cookie2.html")));
 
   browser()->tab_strip_model()->GetActiveWebContents()->Close();
   EXPECT_TRUE(dialog->IsClosed());
@@ -201,6 +241,9 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest, DeleteMenuItem) {
       GetViewByIdentifier(context, kPageSpecificSiteDataDialogRowForTesting);
   auto* row_view = static_cast<SiteDataRowView*>(view);
   EXPECT_TRUE(row_view->GetVisible());
+  ClickButton(row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
   ClickDeleteMenuItem(row_view);
   EXPECT_FALSE(row_view->GetVisible());
   // TODO(crbug.com/1344787): Check the histograms value.
@@ -220,6 +263,9 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest, BlockMenuItem) {
   auto* row_view = static_cast<SiteDataRowView*>(view);
   // TODO(crbug.com/1344787): The label shouldn't be visible here but GetVisible
   // returns true. It's not actually visible because it has size 0.
+  ClickButton(row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
   ClickBlockMenuItem(row_view);
   EXPECT_TRUE(row_view->state_label_for_testing()->GetVisible());
   EXPECT_EQ(row_view->state_label_for_testing()->GetText(), u"Blocked");
@@ -245,6 +291,9 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest, AllowMenuItem) {
   // returns true. It's not actually visible because it has size 0.
   // TODO(crbug.com/1344787): Setup a site with blocked cookies to start with
   // blocked state here.
+  ClickButton(row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
   ClickBlockMenuItem(row_view);
   EXPECT_EQ(row_view->state_label_for_testing()->GetText(), u"Blocked");
   ClickAllowMenuItem(row_view);
@@ -271,6 +320,9 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
   auto* row_view = static_cast<SiteDataRowView*>(view);
   // TODO(crbug.com/1344787): The label shouldn't be visible here but GetVisible
   // returns true. It's not actually visible because it has size 0.
+  ClickButton(row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
   ClickClearOnExitMenuItem(row_view);
   EXPECT_TRUE(row_view->state_label_for_testing()->GetVisible());
   EXPECT_EQ(row_view->state_label_for_testing()->GetText(), u"Clear on close");
@@ -278,6 +330,113 @@ IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
 
   dialog->Close();
   EXPECT_EQ(1u, infobar_count());
+}
+
+IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
+                       PartitionedCookies) {
+  if (!GetParam()) {
+    return;
+  }
+
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), https_server()->GetURL(
+                     "a.test", "/third_party_partitioned_cookies.html")));
+
+  auto* dialog = OpenDialog();
+  ui::ElementContext context =
+      views::ElementTrackerViews::GetContextForWidget(dialog);
+
+  // Get the first row: "b.test" with only partitioned cookie set.
+  auto* partitioned_row_view =
+      static_cast<SiteDataRowView*>(GetViewByIdentifierAtIndex(
+          context, kPageSpecificSiteDataDialogRowForTesting, /*index=*/1));
+  // Only partitioned cookie was set in this third-party context.
+  EXPECT_TRUE(partitioned_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(partitioned_row_view->state_label_for_testing()->GetText(),
+            u"Only using partitioned storage");
+  ClickButton(partitioned_row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
+  ClickBlockMenuItem(partitioned_row_view);
+  EXPECT_TRUE(partitioned_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(partitioned_row_view->state_label_for_testing()->GetText(),
+            u"Blocked");
+  // TODO(crbug.com/1344787): Check the histograms value.
+
+  // Get the second row: "c.test" with both partitioned and regular cookies set.
+  auto* mixed_row_view =
+      static_cast<SiteDataRowView*>(GetViewByIdentifierAtIndex(
+          context, kPageSpecificSiteDataDialogRowForTesting, /*index=*/2));
+  // Both third-party and partitioned cookies are allowed access.
+  // TODO(crbug.com/1344787): The label shouldn't be visible here but GetVisible
+  // returns true. It's not actually visible because it has size 0.
+  EXPECT_EQ(mixed_row_view->state_label_for_testing()->GetText(), u"Allowed");
+  ClickButton(mixed_row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
+  ClickBlockMenuItem(mixed_row_view);
+  EXPECT_TRUE(mixed_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(mixed_row_view->state_label_for_testing()->GetText(), u"Blocked");
+  // TODO(crbug.com/1344787): Check the histograms value.
+}
+
+IN_PROC_BROWSER_TEST_P(PageSpecificSiteDataDialogBrowserTest,
+                       PartitionedCookiesAndBlockedThirdParty) {
+  if (!GetParam()) {
+    return;
+  }
+
+  // Block third-party cookies.
+  browser()->profile()->GetPrefs()->SetInteger(
+      prefs::kCookieControlsMode,
+      static_cast<int>(content_settings::CookieControlsMode::kBlockThirdParty));
+
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), https_server()->GetURL(
+                     "a.test", "/third_party_partitioned_cookies.html")));
+
+  auto* dialog = OpenDialog();
+  ui::ElementContext context =
+      views::ElementTrackerViews::GetContextForWidget(dialog);
+
+  // Get the first row: "b.test" with only partitioned cookie set.
+  auto* partitioned_row_view =
+      static_cast<SiteDataRowView*>(GetViewByIdentifierAtIndex(
+          context, kPageSpecificSiteDataDialogRowForTesting, /*index=*/1));
+  // Only partitioned cookie was set in this third-party context. Partitioned
+  // cookie isn't blocked as a third party cookie because it is treated as a
+  // first party. The state is "Only using partitioned storage" as other types
+  // of storage are not used or blocked from access.
+  EXPECT_TRUE(partitioned_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(partitioned_row_view->state_label_for_testing()->GetText(),
+            u"Only using partitioned storage");
+  ClickButton(partitioned_row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
+  ClickBlockMenuItem(partitioned_row_view);
+  EXPECT_TRUE(partitioned_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(partitioned_row_view->state_label_for_testing()->GetText(),
+            u"Blocked");
+  // TODO(crbug.com/1344787): Check the histograms value.
+
+  // Get the second row: "c.test" with both partitioned and regular cookies set.
+  auto* mixed_row_view =
+      static_cast<SiteDataRowView*>(GetViewByIdentifierAtIndex(
+          context, kPageSpecificSiteDataDialogRowForTesting, /*index=*/2));
+  // Regular third party cookies is blocked by third-party cookie blocking
+  // perf, partitioned cookie isn't because it is treated
+  // as a first party. The state is "Only using partitioned storage" as other
+  // types of storage are not used or blocked from access.
+  EXPECT_TRUE(mixed_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(mixed_row_view->state_label_for_testing()->GetText(),
+            u"Only using partitioned storage");
+  ClickButton(mixed_row_view->menu_button_for_testing());
+  // TODO(crbug.com/1344787): Use the actual menu to perform action. Check if
+  // correct menu item are displayed.
+  ClickBlockMenuItem(mixed_row_view);
+  EXPECT_TRUE(mixed_row_view->state_label_for_testing()->GetVisible());
+  EXPECT_EQ(mixed_row_view->state_label_for_testing()->GetText(), u"Blocked");
+  // TODO(crbug.com/1344787): Check the histograms value.
 }
 
 // Run tests with kPageSpecificSiteDataDialog flag enabled and disabled.
