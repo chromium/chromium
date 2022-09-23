2e8eec15482b59c71a1d4b9f92ca0e379ca83ef2
Mei Liang <meiliang@chromium.org>
2022-09-23 18:24:32 +0000

    [PriceTracking] Update the body content
    This CL uses the updated body string resource with the most recently
    modified bookmark folder name. The bookmark folder name in the
    TYPE_NORMAL bubble is clickable. Clicking the folder name launches the bookmarks editor.
    
    Screenshots:
      * FUE bubble: https://screenshot.googleplex.com/6Q83qeRZYSkPAPK
      * Normal bubble: https://screenshot.googleplex.com/4UFnw488PiiPfFo
    
    Demo showing the bookmarks editor: https://screencast.googleplex.com/cast/NDg4OTY0OTY5Nzc4MzgwOHxjYzBjODJkNy00Mw
    
    Bug: 1346612
    Change-Id: I6f81bf3acae779b49e225cc981aa2f9e84153728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3902827
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1050738}

--- chrome/browser/ui/views/commerce/price_tracking_bubble_dialog_view.cc
+++ chrome/browser/ui/views/commerce/price_tracking_bubble_dialog_view.cc
@@ -4,31 +4,65 @@
 
 #include "chrome/browser/ui/views/commerce/price_tracking_bubble_dialog_view.h"
 
+#include "chrome/browser/bookmarks/bookmark_model_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/bookmarks/bookmark_editor.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "components/bookmarks/browser/bookmark_model.h"
+#include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/views/controls/label.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/view_tracker.h"
 #include "ui/views/view_utils.h"
 
+namespace {
+
+const std::u16string& GetMostRecentlyModifiedUserBookmarkFolderName(
+    Profile* profile) {
+  bookmarks::BookmarkModel* const model =
+      BookmarkModelFactory::GetForBrowserContext(profile);
+  const std::vector<const bookmarks::BookmarkNode*> nodes =
+      bookmarks::GetMostRecentlyModifiedUserFolders(model, 1);
+  return nodes[0]->GetTitle();
+}
+
+std::unique_ptr<views::StyledLabel> CreateBodyLabel(std::u16string& body_text) {
+  return views::Builder<views::StyledLabel>()
+      .SetDefaultTextStyle(views::style::STYLE_SECONDARY)
+      .SetTextContext(views::style::CONTEXT_DIALOG_BODY_TEXT)
+      .SetText(body_text)
+      .SetHorizontalAlignment(gfx::ALIGN_LEFT)
+      .Build();
+}
+
+}  // namespace
+
 PriceTrackingBubbleDialogView::PriceTrackingBubbleDialogView(
     View* anchor_view,
     content::WebContents* web_contents,
+    Profile* profile,
+    const GURL& url,
     OnTrackPriceCallback on_track_price_callback,
     Type type)
     : LocationBarBubbleDelegateView(anchor_view, web_contents),
-      type_(type),
-      action_callback_(std::move(on_track_price_callback)) {
+      profile_(profile),
+      url_(url),
+      action_callback_(std::move(on_track_price_callback)),
+      type_(type) {
   SetShowCloseButton(true);
+  SetLayoutManager(std::make_unique<views::FillLayout>());
   SetButtons(ui::DIALOG_BUTTON_CANCEL | ui::DIALOG_BUTTON_OK);
+  set_fixed_width(views::LayoutProvider::Get()->GetDistanceMetric(
+      views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
+
   auto run_callback = [](PriceTrackingBubbleDialogView* bubble, bool is_track) {
     std::move(bubble->action_callback_).Run(is_track);
   };
-  // TODO(meiliang@): Update the body to use the string that includes the
-  // bookmark name.
-  auto body = l10n_util::GetStringUTF16(
-      IDS_OMNIBOX_TRACK_PRICE_DIALOG_EMAIL_DESCRIPTION);
+
+  auto folder_name = GetMostRecentlyModifiedUserBookmarkFolderName(profile_);
+
   if (type == PriceTrackingBubbleDialogView::Type::TYPE_FUE) {
     SetTitle(l10n_util::GetStringUTF16(
         IDS_OMNIBOX_TRACK_PRICE_DIALOG_TITLE_FIRST_RUN));
@@ -40,6 +74,9 @@ PriceTrackingBubbleDialogView::PriceTrackingBubbleDialogView(
                        IDS_OMNIBOX_TRACK_PRICE_DIALOG_CANCEL_BUTTON));
     SetAcceptCallback(
         base::BindOnce(run_callback, base::Unretained(this), true));
+    auto body_text = l10n_util::GetStringFUTF16(
+        IDS_OMNIBOX_TRACK_PRICE_DIALOG_DESCRIPTION_FIRST_RUN, folder_name);
+    body_label_ = AddChildView(CreateBodyLabel(body_text));
   } else if (type == PriceTrackingBubbleDialogView::Type::TYPE_NORMAL) {
     SetTitle(
         l10n_util::GetStringUTF16(IDS_OMNIBOX_TRACKING_PRICE_DIALOG_TITLE));
@@ -51,23 +88,41 @@ PriceTrackingBubbleDialogView::PriceTrackingBubbleDialogView(
                        IDS_OMNIBOX_TRACKING_PRICE_DIALOG_UNTRACK_BUTTON));
     SetCancelCallback(
         base::BindOnce(run_callback, base::Unretained(this), false));
-    // TODO(meiliang@): Update the body to use the new normal bubble string.
-  }
 
-  SetLayoutManager(std::make_unique<views::FillLayout>());
-  AddChildView(views::Builder<views::Label>()
-                   .CopyAddressTo(&body_label_)
-                   .SetTextStyle(views::style::STYLE_SECONDARY)
-                   .SetTextContext(views::style::CONTEXT_DIALOG_BODY_TEXT)
-                   .SetText(body)
-                   .SetMultiLine(true)
-                   .SetHorizontalAlignment(gfx::ALIGN_LEFT)
-                   .Build());
-  set_fixed_width(views::LayoutProvider::Get()->GetDistanceMetric(
-      views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
+    auto body_text = l10n_util::GetStringFUTF16(
+        IDS_OMNIBOX_TRACKING_PRICE_DIALOG_DESCRIPTION, folder_name);
+    body_label_ = AddChildView(CreateBodyLabel(body_text));
+    // Offset the period at the end by minus one.
+    int32_t offset = body_text.length() - folder_name.length() - 1;
+    views::StyledLabel::RangeStyleInfo style_info =
+        views::StyledLabel::RangeStyleInfo::CreateForLink(base::BindRepeating(
+            &PriceTrackingBubbleDialogView::ShowBookmarkEditor,
+            weak_factory_.GetWeakPtr()));
+    body_label_->AddStyleRange(
+        gfx::Range(offset, offset + folder_name.length()), style_info);
+  }
 }
 
 PriceTrackingBubbleDialogView::~PriceTrackingBubbleDialogView() = default;
+
+void PriceTrackingBubbleDialogView::ShowBookmarkEditor() {
+  DCHECK(anchor_widget());
+
+  gfx::NativeWindow native_parent = anchor_widget()->GetNativeWindow();
+  DCHECK(native_parent);
+
+  GetWidget()->Close();
+
+  const bookmarks::BookmarkNode* node =
+      BookmarkModelFactory::GetForBrowserContext(profile_)
+          ->GetMostRecentlyAddedUserNodeForURL(url_);
+
+  if (node && native_parent) {
+    BookmarkEditor::Show(native_parent, profile_,
+                         BookmarkEditor::EditDetails::EditNode(node),
+                         BookmarkEditor::SHOW_TREE);
+  }
+}
 
 // PriceTrackingBubbleCoordinator
 PriceTrackingBubbleCoordinator::PriceTrackingBubbleCoordinator(
@@ -78,12 +133,14 @@ PriceTrackingBubbleCoordinator::~PriceTrackingBubbleCoordinator() = default;
 
 void PriceTrackingBubbleCoordinator::Show(
     content::WebContents* web_contents,
+    Profile* profile,
+    const GURL& url,
     PriceTrackingBubbleDialogView::OnTrackPriceCallback callback,
     PriceTrackingBubbleDialogView::Type type) {
   DCHECK(!tracker_.view());
 
   auto bubble = std::make_unique<PriceTrackingBubbleDialogView>(
-      anchor_view_, web_contents, std::move(callback), type);
+      anchor_view_, web_contents, profile, url, std::move(callback), type);
   tracker_.SetView(bubble.get());
   PriceTrackingBubbleDialogView::CreateBubble(std::move(bubble))->Show();
 }
