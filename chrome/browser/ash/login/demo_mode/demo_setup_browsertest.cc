3ee358c179da17b8f7c457c36116499bd414d9f1
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2022-09-26 07:45:58 +0000

    sheriff: Disable DemoSetupProgressStepsTest.SetupProgessStepsDisplayCorrectly
    Bug: 1323032
    Change-Id: Iccff75ce2f42df0078503f1a373b57cca9464182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3917654
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051094}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -875,7 +875,8 @@ class DemoSetupProgressStepsTest : public DemoSetupArcSupportedTest {
 };
 
 IN_PROC_BROWSER_TEST_F(DemoSetupProgressStepsTest,
-                       SetupProgessStepsDisplayCorrectly) {
+                       // TODO(crbug.com/1323032): Re-enable this test
+                       DISABLED_SetupProgessStepsDisplayCorrectly) {
   SimulateNetworkConnected();
   TriggerDemoModeOnWelcomeScreen();
   UseOnlineModeOnNetworkScreen();
