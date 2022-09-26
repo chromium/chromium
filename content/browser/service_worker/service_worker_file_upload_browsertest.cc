09fb7b32a6e59c2db036ad3cd059d873f1cbcf36
Maksim Ivanov <emaxx@chromium.org>
2022-09-26 01:38:19 +0000

    Use Value::Take...() in /content/browser/service_worker
    Refactor to use the new base::Value
    TakeString/TakeDict/TakeList methods that transfer ownership
    in a clearer way and allow clang-tidy catch misuses.
    
    This CL was uploaded by git cl split.
    
    R=yyanagisawa@chromium.org
    
    Bug: 1360570
    Change-Id: I5eae419e87c88be202d1da3dfc79f54766f12609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913101
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051050}

--- content/browser/service_worker/service_worker_file_upload_browsertest.cc
+++ content/browser/service_worker/service_worker_file_upload_browsertest.cc
@@ -190,7 +190,7 @@ class ServiceWorkerFileUploadTest : public testing::WithParamInterface<bool>,
     absl::optional<base::Value> parsed_result = base::test::ParseJson(result);
     ASSERT_TRUE(parsed_result);
     ASSERT_TRUE(parsed_result->is_dict());
-    out_result = std::move(parsed_result->GetDict());
+    out_result = std::move(*parsed_result).TakeDict();
   }
 
   // Helper for tests where the service worker falls back to network.
@@ -322,7 +322,7 @@ class ServiceWorkerFileUploadTest : public testing::WithParamInterface<bool>,
     base::ReplaceFirstSubstringAfterOffset(&expectation, 0, "@SIZE@",
                                            base::NumberToString(kFileSize));
     absl::optional<base::Value> result = base::test::ParseJson(expectation);
-    return std::move(result->GetDict());
+    return std::move(*result).TakeDict();
   }
 
  private:
