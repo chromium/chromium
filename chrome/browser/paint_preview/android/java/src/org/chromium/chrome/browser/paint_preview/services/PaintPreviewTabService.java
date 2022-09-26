53482a685ad9986b6f9939a3c4850786fe67b02c
Calder Kitagawa <ckitagawa@chromium.org>
2022-09-26 15:45:47 +0000

    [Paint Preview] Default ALLOW_SRP to false.
    We are going to launch the version of Paint Preview that does not support SRP captures. Make that the default behavior on ToT.
    
    Fixed: 1320021
    Change-Id: I0805f2a8af1b7a4c1ce0ca33cba0873f35744195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3913459
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051209}

--- chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
+++ chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabService.java
@@ -38,7 +38,7 @@ import java.io.File;
 public class PaintPreviewTabService implements NativePaintPreviewServiceProvider {
     public static final BooleanCachedFieldTrialParameter ALLOW_SRP =
             new BooleanCachedFieldTrialParameter(
-                    ChromeFeatureList.PAINT_PREVIEW_SHOW_ON_STARTUP, "allow_srp", true);
+                    ChromeFeatureList.PAINT_PREVIEW_SHOW_ON_STARTUP, "allow_srp", false);
 
     private static final long AUDIT_START_DELAY_MS = 2 * 60 * 1000; // Two minutes;
     private static boolean sIsAccessibilityEnabledForTesting;
