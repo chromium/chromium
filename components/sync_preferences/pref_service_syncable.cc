6fa934d3af1d9e3d4cb393599d8111538e4766ba
Roman Sorokin <rsorokin@chromium.org>
2022-09-27 13:13:07 +0000

    [Cleanup] Remove obsolete SyncSettingsCategorization code
    Remove the obsolete code which is executed when
    IsSyncSettingsCategorizationEnabled is false.
    The feature has been enabled for a while now.
    We removed the flag recently: http://crrev.com/c/3859278
    
    This CL was uploaded by git cl split.
    
    R=mmoskvitin@google.com
    
    Bug: 1227417, 1249845
    Test: CQ
    Change-Id: I1018987aa687cb2f0e19f37a866acc3102281097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3918294
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051756}

--- components/sync_preferences/pref_service_syncable.cc
+++ components/sync_preferences/pref_service_syncable.cc
@@ -219,28 +219,16 @@ void PrefServiceSyncable::AddRegisteredSyncablePreference(
   }
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   if (flags & user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF) {
-    if (chromeos::features::IsSyncSettingsCategorizationEnabled()) {
-      // Register the pref under the new ModelType::OS_PREFERENCES.
-      os_pref_sync_associator_.RegisterPref(path);
-      // Also register under the old ModelType::PREFERENCES. This ensures that
-      // local changes to OS prefs are also synced to old clients that have the
-      // pref registered as a browser SYNCABLE_PREF.
-      pref_sync_associator_.RegisterPrefWithLegacyModelType(path);
-    } else {
-      // Behave like an old client and treat this pref like it was registered
-      // as a SYNCABLE_PREF under ModelType::PREFERENCES.
-      pref_sync_associator_.RegisterPref(path);
-    }
+    os_pref_sync_associator_.RegisterPref(path);
+    // Also register under the old ModelType::PREFERENCES. This ensures that
+    // local changes to OS prefs are also synced to old clients that have the
+    // pref registered as a browser SYNCABLE_PREF.
+    pref_sync_associator_.RegisterPrefWithLegacyModelType(path);
     return;
   }
   if (flags & user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PRIORITY_PREF) {
-    // See comments for SYNCABLE_OS_PREF above.
-    if (chromeos::features::IsSyncSettingsCategorizationEnabled()) {
-      os_priority_pref_sync_associator_.RegisterPref(path);
-      priority_pref_sync_associator_.RegisterPrefWithLegacyModelType(path);
-    } else {
-      priority_pref_sync_associator_.RegisterPref(path);
-    }
+    os_priority_pref_sync_associator_.RegisterPref(path);
+    priority_pref_sync_associator_.RegisterPrefWithLegacyModelType(path);
     return;
   }
 #endif
