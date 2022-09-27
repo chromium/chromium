2d0771bb2cfaa458e387eb5de5e81f2f3ef22c96
Cheick Cisse <cheickcisse@google.com>
2022-09-27 15:35:21 +0000

    [WhatsNew] Create WhatsNewTableViewItem
    This CL:
    - Creates a table view item for What's New
    - Includes two Labels for the title and the detail text
    - Includes a main background rounded view, an optional icon rounded view, and an icon view.
    
    UI: https://docs.google.com/document/d/1DhjKaQQL38oEqLu9Ov8MAWAu3yt9ZlPhjwGM1q26V_k
    
    Bug: 1363710
    Change-Id: I6a049d783e9f638d1030046e9225dfcdcca4b1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3895365
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051828}

---
+++ ios/chrome/browser/ui/whats_new/cells/whats_new_table_view_item_unittest.mm
@@ -0,0 +1,145 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/whats_new/cells/whats_new_table_view_item.h"
+
+#import "base/mac/foundation_util.h"
+#import "ios/chrome/browser/ui/icons/chrome_icon.h"
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
+#import "testing/gtest_mac.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+using WhatsNewTableViewItemTest = PlatformTest;
+}  // namespace
+
+// Tests that the UILabels and icon are set properly after a call to
+// configureCell.
+TEST_F(WhatsNewTableViewItemTest, ItemProperties) {
+  NSString* title = @"Title";
+  NSString* detail_text = @"Detail text";
+
+  WhatsNewTableViewItem* item = [[WhatsNewTableViewItem alloc] initWithType:0];
+  item.title = title;
+  item.detailText = detail_text;
+  item.iconImage = [UIImage imageNamed:@"ic_search"];
+  item.iconBackgroundColor = UIColor.blueColor;
+
+  id cell = [[[item cellClass] alloc] init];
+  ASSERT_TRUE([cell isMemberOfClass:[WhatsNewTableViewCell class]]);
+
+  WhatsNewTableViewCell* whats_new_cell =
+      base::mac::ObjCCastStrict<WhatsNewTableViewCell>(cell);
+
+  ChromeTableViewStyler* styler = [[ChromeTableViewStyler alloc] init];
+  [item configureCell:cell withStyler:styler];
+
+  // Check text label (title).
+  EXPECT_NSEQ(title, whats_new_cell.textLabel.text);
+  EXPECT_EQ(NO,
+            whats_new_cell.textLabel.translatesAutoresizingMaskIntoConstraints);
+  EXPECT_EQ([UIFont preferredFontForTextStyle:UIFontTextStyleBody],
+            whats_new_cell.textLabel.font);
+  EXPECT_EQ(YES, whats_new_cell.textLabel.adjustsFontForContentSizeCategory);
+  EXPECT_EQ(1, whats_new_cell.textLabel.numberOfLines);
+
+  // Check detail text label.
+  EXPECT_NSEQ(detail_text, whats_new_cell.detailTextLabel.text);
+  EXPECT_EQ(
+      NO,
+      whats_new_cell.detailTextLabel.translatesAutoresizingMaskIntoConstraints);
+  EXPECT_EQ([UIFont preferredFontForTextStyle:kTableViewSublabelFontStyle],
+            whats_new_cell.detailTextLabel.font);
+  EXPECT_EQ(YES,
+            whats_new_cell.detailTextLabel.adjustsFontForContentSizeCategory);
+  EXPECT_EQ(2, whats_new_cell.detailTextLabel.numberOfLines);
+
+  // Check that the main background is set properly.
+  UIImageView* main_background_image_view =
+      base::mac::ObjCCastStrict<UIImageView>(
+          whats_new_cell.contentView.subviews[0]);
+  EXPECT_NSEQ([[UIImage imageNamed:@"whats_new_icon_tile"]
+                  imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate],
+              main_background_image_view.image);
+
+  // Check that the background icon view is set properly.
+  UIImageView* icon_background_image_view =
+      whats_new_cell.iconBackgroundImageView;
+  EXPECT_NSEQ([[UIImage imageNamed:@"whats_new_icon_tile"]
+                  imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate],
+              icon_background_image_view.image);
+  EXPECT_EQ(UIColor.blueColor, icon_background_image_view.tintColor);
+
+  // Check that icon view is set properly .
+  UIImageView* icon_view = whats_new_cell.iconView;
+  EXPECT_NSEQ([ChromeIcon searchIcon], icon_view.image);
+}
+
+// Tests that the icon background in hidden when iconBackgroundImageView is not
+// set.
+TEST_F(WhatsNewTableViewItemTest, ItemWithoutBackgroundImageView) {
+  NSString* title = @"Title";
+  NSString* detail_text = @"Detail text";
+
+  WhatsNewTableViewItem* item = [[WhatsNewTableViewItem alloc] initWithType:0];
+  item.title = title;
+  item.detailText = detail_text;
+  item.iconImage = [UIImage imageNamed:@"ic_search"];
+
+  id cell = [[[item cellClass] alloc] init];
+  ASSERT_TRUE([cell isMemberOfClass:[WhatsNewTableViewCell class]]);
+
+  WhatsNewTableViewCell* whats_new_cell =
+      base::mac::ObjCCastStrict<WhatsNewTableViewCell>(cell);
+
+  ChromeTableViewStyler* styler = [[ChromeTableViewStyler alloc] init];
+  [item configureCell:cell withStyler:styler];
+
+  // Check text label (title).
+  EXPECT_NSEQ(title, whats_new_cell.textLabel.text);
+
+  EXPECT_EQ(NO,
+            whats_new_cell.textLabel.translatesAutoresizingMaskIntoConstraints);
+  EXPECT_EQ([UIFont preferredFontForTextStyle:UIFontTextStyleBody],
+            whats_new_cell.textLabel.font);
+  EXPECT_EQ(YES, whats_new_cell.textLabel.adjustsFontForContentSizeCategory);
+  EXPECT_EQ(1, whats_new_cell.textLabel.numberOfLines);
+
+  // Check detail text label.
+  EXPECT_NSEQ(detail_text, whats_new_cell.detailTextLabel.text);
+  EXPECT_EQ(
+      NO,
+      whats_new_cell.detailTextLabel.translatesAutoresizingMaskIntoConstraints);
+  EXPECT_EQ([UIFont preferredFontForTextStyle:kTableViewSublabelFontStyle],
+            whats_new_cell.detailTextLabel.font);
+  EXPECT_EQ(YES,
+            whats_new_cell.detailTextLabel.adjustsFontForContentSizeCategory);
+  EXPECT_EQ(2, whats_new_cell.detailTextLabel.numberOfLines);
+
+  // Check that the main background is set properly.
+  UIImageView* main_background_image_view =
+      base::mac::ObjCCastStrict<UIImageView>(
+          whats_new_cell.contentView.subviews[0]);
+  EXPECT_NSEQ([[UIImage imageNamed:@"whats_new_icon_tile"]
+                  imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate],
+              main_background_image_view.image);
+
+  // Check that the background icon view is hidden.
+  UIImageView* icon_background_image_view =
+      whats_new_cell.iconBackgroundImageView;
+  EXPECT_NSEQ([[UIImage imageNamed:@"whats_new_icon_tile"]
+                  imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate],
+              icon_background_image_view.image);
+  EXPECT_EQ(YES, icon_background_image_view.hidden);
+
+  // Check that icon view is set properly .
+  UIImageView* icon_view = whats_new_cell.iconView;
+  EXPECT_NSEQ([ChromeIcon searchIcon], icon_view.image);
+}
