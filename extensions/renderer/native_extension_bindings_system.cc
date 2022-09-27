edfb998b52776b6e839f0f2fad74515f8b3b9ae9
Maksim Ivanov <emaxx@chromium.org>
2022-09-27 17:08:12 +0000

    Use Value::Take...() in /extensions/renderer
    Refactor to use the new base::Value
    TakeString/TakeDict/TakeList methods that transfer ownership
    in a clearer way and allow clang-tidy catch misuses.
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 1360570
    Change-Id: I43dae48b8bc9ae5ce29419ce07d6e6ec7c5e4cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914213
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1051887}

--- extensions/renderer/native_extension_bindings_system.cc
+++ extensions/renderer/native_extension_bindings_system.cc
@@ -847,7 +847,7 @@ void NativeExtensionBindingsSystem::SendRequest(
 
   auto params = mojom::RequestParams::New();
   params->name = request->method_name;
-  base::Value::List args = std::move(request->arguments_list->GetList());
+  base::Value::List args = std::move(*request->arguments_list).TakeList();
   params->arguments = std::move(args);
   params->extension_id = script_context->GetExtensionID();
   params->source_url = url;
