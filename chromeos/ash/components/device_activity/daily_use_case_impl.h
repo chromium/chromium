3433516743a3a5701dcb46fc3ddf7ed7b1d009f1
Hirthanan Subenderan <hirthanan@google.com>
2022-09-27 17:20:39 +0000

    Update overridden methods comment style
    BUG=NONE
    
    Change-Id: I90dd59f0dfbb49f75e42e9fa47cf4c50a63e3323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3707424
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051901}

--- chromeos/ash/components/device_activity/daily_use_case_impl.h
+++ chromeos/ash/components/device_activity/daily_use_case_impl.h
@@ -34,20 +34,8 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DEVICE_ACTIVITY) DailyUseCaseImpl
   DailyUseCaseImpl& operator=(const DailyUseCaseImpl&) = delete;
   ~DailyUseCaseImpl() override;
 
-  // Generate the window identifier for the kCrosDaily use case.
-  // For example, the daily use case should generate a window identifier
-  // formatted: yyyyMMdd.
-  //
-  // It is generated on demand each time the state machine leaves the idle
-  // state. It is reused by several states. It is reset to nullopt. This field
-  // is used apart of PSM Import request.
+  // DeviceActiveUseCase:
   std::string GenerateUTCWindowIdentifier(base::Time ts) const override;
-
-  // Generate Fresnel PSM import request body.
-  // Sets the daily device metadata dimensions sent by PSM import.
-  //
-  // Important: Each new dimension added to metadata will need to be approved by
-  // privacy.
   ImportDataRequest GenerateImportRequestBody() override;
 };
 
