3b5f3e74374305f3e2ddd16963318178822bda46
Daniel Cheng <dcheng@chromium.org>
2022-09-27 23:15:20 +0000

    Use helper macros to define base::Features in //pdf
    This allows:
    - features to be defined with a consistent set of qualifiers, and for
      that set of qualifiers to be updated over time as appropriate.
    - better PRESUBMIT checks to ensure that base::Features are not defined
      in headers.
    - simplifies things for scripts trying to extract feature definitions
      out of C++ code.
    
    The primary CL was generated using a script that automatically rewrites
    base::Feature declarations and definitions to the macro form. Changes to
    any files with known incompatibilities with the macros (base::Features
    without static storage duration and base::Features declared as static
    class members) were then fully reverted; those changes will be manually
    handled in followups.
    
    This CL was uploaded by git cl split.
    
    R=nigi@chromium.org
    
    Bug: 1364289
    Change-Id: I34c3456e1e2e9de77a0f886382cb42bbbf5122eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922441
    Reviewed-by: Nigi <nigi@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052102}

--- pdf/pdf_features.cc
+++ pdf/pdf_features.cc
@@ -7,25 +7,30 @@
 namespace chrome_pdf {
 namespace features {
 
-const base::Feature kAccessiblePDFForm = {"AccessiblePDFForm",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kAccessiblePDFForm,
+             "AccessiblePDFForm",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // "Incremental loading" refers to loading the PDF as it arrives.
 // TODO(crbug.com/1064175): Remove this once incremental loading is fixed.
-const base::Feature kPdfIncrementalLoading = {
-    "PdfIncrementalLoading", base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kPdfIncrementalLoading,
+             "PdfIncrementalLoading",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // "Partial loading" refers to loading only specific parts of the PDF.
 // TODO(crbug.com/1064175): Remove this once partial loading is fixed.
-const base::Feature kPdfPartialLoading = {"PdfPartialLoading",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kPdfPartialLoading,
+             "PdfPartialLoading",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Feature has no effect if Chrome is built with no XFA support.
-const base::Feature kPdfXfaSupport = {"PdfXfaSupport",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
+BASE_FEATURE(kPdfXfaSupport,
+             "PdfXfaSupport",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
-const base::Feature kTabAcrossPDFAnnotations = {
-    "TabAcrossPDFAnnotations", base::FEATURE_ENABLED_BY_DEFAULT};
+BASE_FEATURE(kTabAcrossPDFAnnotations,
+             "TabAcrossPDFAnnotations",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
 }  // namespace chrome_pdf
--- pdf/pdf_features.h
+++ pdf/pdf_features.h
@@ -13,11 +13,11 @@
 namespace chrome_pdf {
 namespace features {
 
-extern const base::Feature kAccessiblePDFForm;
-extern const base::Feature kPdfIncrementalLoading;
-extern const base::Feature kPdfPartialLoading;
-extern const base::Feature kPdfXfaSupport;
-extern const base::Feature kTabAcrossPDFAnnotations;
+BASE_DECLARE_FEATURE(kAccessiblePDFForm);
+BASE_DECLARE_FEATURE(kPdfIncrementalLoading);
+BASE_DECLARE_FEATURE(kPdfPartialLoading);
+BASE_DECLARE_FEATURE(kPdfXfaSupport);
+BASE_DECLARE_FEATURE(kTabAcrossPDFAnnotations);
 
 }  // namespace features
 }  // namespace chrome_pdf
