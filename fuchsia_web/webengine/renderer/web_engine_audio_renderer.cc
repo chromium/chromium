79af80ef970d937374d928b75ba5e793112a4fde
evliu <evliu@google.com>
2022-09-27 04:34:42 +0000

    [webengine] Reduce logspam from WebEngineAudioRenderer::SetWasPlayedWithUserActivation
    This CL replaces NOTIMPLEMENTED() with NOTIMPLEMENTED_LOG_ONCE() inside
    WebEngineAudioRenderer::SetWasPlayedWithUserActivation which is
    currently not implemented on Fuchsia as requested in https://crrev.com/c/3340085/7/fuchsia/engine/renderer/web_engine_audio_renderer.cc#307.
    
    Change-Id: Ibaf144e42ee543d35c7b27c14c718e2c7ab1474d
    Bug: 1280059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3919751
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1051611}

--- fuchsia_web/webengine/renderer/web_engine_audio_renderer.cc
+++ fuchsia_web/webengine/renderer/web_engine_audio_renderer.cc
@@ -314,12 +314,15 @@ void WebEngineAudioRenderer::SetLatencyHint(
 }
 
 void WebEngineAudioRenderer::SetPreservesPitch(bool preserves_pitch) {
+  // TODO(crbug.com/1368392): Implement this.
   NOTIMPLEMENTED();
 }
 
 void WebEngineAudioRenderer::SetWasPlayedWithUserActivation(
     bool was_played_with_user_activation) {
-  NOTIMPLEMENTED();
+  // WebEngine does not use this signal. This is currently only used by the Live
+  // Caption feature.
+  NOTIMPLEMENTED_LOG_ONCE();
 }
 
 void WebEngineAudioRenderer::StartTicking() {
