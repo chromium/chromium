b2403a0e370062b6956831c8ba2cfc9d44409daa
Nina Satragno <nsatragno@chromium.org>
2022-09-27 20:28:34 +0000

    [webauthn] Integration tests for autofill UI
    Add integration tests for WebAuthn autofill UI on desktop.
    
    Bug: 1171985
    Change-Id: I771e193a8dd66593fa86e99fc7f9ede2621bfe52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914724
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052028}

--- chrome/browser/webauthn/chrome_webauthn_browsertest.cc
+++ chrome/browser/webauthn/chrome_webauthn_browsertest.cc
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
 #include <sstream>
 #include <vector>
 
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
@@ -17,37 +19,47 @@
 #include "chrome/browser/extensions/install_verifier.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/password_manager/chrome_webauthn_credentials_delegate.h"
+#include "chrome/browser/ssl/cert_verifier_browser_test.h"
+#include "chrome/browser/ui/autofill/autofill_popup_controller_impl.h"
+#include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/network_session_configurator/common/network_switches.h"
+#include "components/password_manager/core/browser/password_ui_utils.h"
 #include "content/public/browser/authenticator_environment.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "device/fido/cable/cable_discovery_data.h"
-#include "device/fido/cable/v2_test_util.h"
 #include "device/fido/features.h"
 #include "device/fido/fido_transport_protocol.h"
+#include "device/fido/public_key_credential_user_entity.h"
 #include "device/fido/virtual_ctap2_device.h"
 #include "device/fido/virtual_fido_device.h"
 #include "device/fido/virtual_fido_device_factory.h"
-#include "device/fido/virtual_u2f_device.h"
 #include "extensions/common/extension_builder.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
-#include "services/network/public/mojom/network_context.mojom-forward.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "device/fido/win/fake_webauthn_api.h"
+#endif  // BUILDFLAG(IS_WIN)
+
 namespace {
+
+static constexpr uint8_t kCredentialID[] = {1, 2, 3, 4};
 
 // This file tests WebAuthn features that depend on specific //chrome behaviour.
 // Tests that don't depend on that should go into
 // content/browser/webauth/webauth_browsertest.cc.
-
-class WebAuthnBrowserTest : public InProcessBrowserTest {
+class WebAuthnBrowserTest : public CertVerifierBrowserTest {
  public:
   WebAuthnBrowserTest() = default;
 
@@ -55,15 +67,28 @@ class WebAuthnBrowserTest : public InProcessBrowserTest {
   WebAuthnBrowserTest& operator=(const WebAuthnBrowserTest&) = delete;
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
+    CertVerifierBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
   }
 
+  void SetUp() override {
+    ASSERT_TRUE(https_server_.InitializeAndListen());
+    CertVerifierBrowserTest::SetUp();
+  }
+
   void SetUpOnMainThread() override {
-    InProcessBrowserTest::SetUpOnMainThread();
+    CertVerifierBrowserTest::SetUpOnMainThread();
 
     https_server_.ServeFilesFromSourceDirectory(GetChromeTestDataDir());
-    CHECK(https_server_.Start());
+    https_server_.StartAcceptingConnections();
     host_resolver()->AddRule("*", "127.0.0.1");
+
+    // Allowlist all certs for the HTTPS server.
+    auto cert = https_server_.GetCertificate();
+    net::CertVerifyResult verify_result;
+    verify_result.cert_status = 0;
+    verify_result.verified_cert = cert;
+    mock_cert_verifier()->AddResultForCert(cert.get(), verify_result, net::OK);
   }
 
  protected:
@@ -155,7 +180,6 @@ IN_PROC_BROWSER_TEST_F(WebAuthnBrowserTest, ChromeExtensions) {
       std::make_unique<device::test::VirtualFidoDeviceFactory>();
   const GURL url = extension->GetResourceURL(kPageFile);
   auto extension_id = url.host();
-  static const uint8_t kCredentialID[] = {1, 2, 3, 4};
   virtual_device_factory->mutable_state()->InjectRegistration(
       kCredentialID, "chrome-extension://" + extension_id);
 
@@ -248,7 +272,6 @@ class WebAuthnConditionalUITest : public WebAuthnBrowserTest {
     auto virtual_device_factory =
         std::make_unique<device::test::VirtualFidoDeviceFactory>();
     virtual_device_factory_ = virtual_device_factory.get();
-    static constexpr uint8_t kCredentialID[] = {1, 2, 3, 4};
     virtual_device_factory->mutable_state()->InjectResidentKey(
         kCredentialID, "www.example.com", std::vector<uint8_t>{5, 6, 7, 8},
         "flandre", "Flandre Scarlet");
@@ -272,22 +295,21 @@ class WebAuthnConditionalUITest : public WebAuthnBrowserTest {
   raw_ptr<device::test::VirtualFidoDeviceFactory> virtual_device_factory_;
 };
 
+static constexpr char kConditionalUIRequest[] = R"((() => {
+navigator.credentials.get({
+  mediation: 'conditional',
+  publicKey: {
+    challenge: new Uint8Array([1,2,3,4]),
+    timeout: 10000,
+    allowCredentials: [],
+  }}).then(c => window.domAutomationController.send('webauthn: OK'),
+           e => window.domAutomationController.send('error ' + e));
+})())";
+
 // Tests that the "Sign in with another device…" button dispatches requests to
 // plugged in authenticators.
 IN_PROC_BROWSER_TEST_F(WebAuthnConditionalUITest,
                        ConditionalUIOtherDeviceButton) {
-  static constexpr char kRequest[] = R"((() => {
-  let cred_id = new Uint8Array([1,2,3,4]);
-  navigator.credentials.get({
-    mediation: 'conditional',
-    publicKey: {
-      challenge: cred_id,
-      timeout: 10000,
-      allowCredentials: [],
-    }}).then(c => window.domAutomationController.send('webauthn: OK'),
-             e => window.domAutomationController.send('error ' + e));
-  })())";
-
   // Make a Conditional UI request. The authenticator should not be dispatched
   // to before the user clicks the "Sign in with another device…" button.
   virtual_device_factory_->mutable_state()->simulate_press_callback =
@@ -298,7 +320,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthnConditionalUITest,
   content::WebContents* web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   content::DOMMessageQueue message_queue(web_contents);
-  content::ExecuteScriptAsync(web_contents, kRequest);
+  content::ExecuteScriptAsync(web_contents, kConditionalUIRequest);
   observer_->WaitForUI();
 
   // Allow the virtual device to respond to requests, then simulate clicking the
@@ -316,6 +338,140 @@ IN_PROC_BROWSER_TEST_F(WebAuthnConditionalUITest,
   EXPECT_EQ(observer_->accounts_.size(), 1u);
   EXPECT_EQ(observer_->accounts_.at(0), "01020304");
 }
+
+// Autofill integration tests --------------------------------------------------
+
+// Base class for autofill integration tests, contains the actual test code but
+// no setup.
+class WebAuthnAutofillIntegrationTest : public WebAuthnBrowserTest {
+ protected:
+  void SetUpOnMainThread() override {
+    WebAuthnBrowserTest::SetUpOnMainThread();
+    ASSERT_TRUE(ui_test_utils::NavigateToURL(
+        browser(),
+        https_server_.GetURL("www.example.com",
+                             "/webauthn_conditional_mediation.html")));
+  }
+
+  void RunSelectAccountTest() {
+    // Make sure input events cannot close the autofill popup.
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    autofill::ChromeAutofillClient* autofill_client =
+        autofill::ChromeAutofillClient::FromWebContents(web_contents);
+    autofill_client->KeepPopupOpenForTesting();
+
+    // Execute the Conditional UI request.
+    content::DOMMessageQueue message_queue(web_contents);
+    content::ExecuteScriptAsync(web_contents, kConditionalUIRequest);
+
+    // Interact with the username field until the popup shows up. This has the
+    // effect of waiting for the browser to send the renderer the password
+    // information, and waiting for the UI to render.
+    base::WeakPtr<autofill::AutofillPopupController> popup_controller;
+    while (!popup_controller) {
+      content::SimulateMouseClickOrTapElementWithId(web_contents, "username");
+      popup_controller = autofill_client->popup_controller_for_testing();
+    }
+
+    // Find the webauthn credential on the suggestions list.
+    auto suggestions = popup_controller->GetSuggestions();
+    size_t suggestion_index;
+    autofill::Suggestion webauthn_entry;
+    for (suggestion_index = 0; suggestion_index < suggestions.size();
+         ++suggestion_index) {
+      if (suggestions[suggestion_index].frontend_id ==
+          autofill::PopupItemId::POPUP_ITEM_ID_WEBAUTHN_CREDENTIAL) {
+        webauthn_entry = suggestions[suggestion_index];
+        break;
+      }
+    }
+    ASSERT_LT(suggestion_index, suggestions.size())
+        << "WebAuthn entry not found";
+    EXPECT_EQ(webauthn_entry.main_text.value, u"flandre");
+    EXPECT_EQ(webauthn_entry.labels.at(0).at(0).value,
+              l10n_util::GetStringUTF16(
+                  password_manager::GetPlatformAuthenticatorLabel()));
+    EXPECT_EQ(webauthn_entry.icon, "globeIcon");
+
+    // Click the credential.
+    popup_controller->AcceptSuggestion(suggestion_index);
+    std::string result;
+    ASSERT_TRUE(message_queue.WaitForMessage(&result));
+    EXPECT_EQ(result, "\"webauthn: OK\"");
+  }
+
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kWebAuthConditionalUI};
+  raw_ptr<device::test::VirtualFidoDeviceFactory> virtual_device_factory_;
+};
+
+// Autofill integration test using the devtools virtual environment.
+class WebAuthnDevtoolsAutofillIntegrationTest
+    : public WebAuthnAutofillIntegrationTest {
+ public:
+  void SetUpOnMainThread() override {
+    WebAuthnAutofillIntegrationTest::SetUpOnMainThread();
+
+    // Set up a fake virtual device.
+    auto virtual_device_factory =
+        std::make_unique<device::test::VirtualFidoDeviceFactory>();
+    virtual_device_factory->SetTransport(
+        device::FidoTransportProtocol::kInternal);
+    virtual_device_factory_ = virtual_device_factory.get();
+    virtual_device_factory->mutable_state()->InjectResidentKey(
+        kCredentialID, "www.example.com", std::vector<uint8_t>{5, 6, 7, 8},
+        "flandre", "Flandre Scarlet");
+    virtual_device_factory->mutable_state()->fingerprints_enrolled = true;
+    device::VirtualCtap2Device::Config config;
+    config.resident_key_support = true;
+    config.internal_uv_support = true;
+    virtual_device_factory->SetCtap2Config(std::move(config));
+    content::AuthenticatorEnvironment::GetInstance()
+        ->ReplaceDefaultDiscoveryFactoryForTesting(
+            std::move(virtual_device_factory));
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(WebAuthnDevtoolsAutofillIntegrationTest, SelectAccount) {
+  RunSelectAccountTest();
+}
+
+#if BUILDFLAG(IS_WIN)
+// Autofill integration test using the Windows fake API.
+class WebAuthnWindowsAutofillIntegrationTest
+    : public WebAuthnAutofillIntegrationTest {
+ public:
+  void SetUpOnMainThread() override {
+    WebAuthnAutofillIntegrationTest::SetUpOnMainThread();
+
+    // Set up the fake Windows platform authenticator.
+    fake_webauthn_api_ = std::make_unique<device::FakeWinWebAuthnApi>();
+    fake_webauthn_api_->set_version(WEBAUTHN_API_VERSION_4);
+    fake_webauthn_api_->set_is_uvpaa(true);
+    fake_webauthn_api_->set_supports_silent_discovery(true);
+    device::PublicKeyCredentialUserEntity user({1, 2, 3, 4}, "flandre",
+                                               "Flandre Scarlet");
+    device::PublicKeyCredentialRpEntity rp("www.example.com");
+    fake_webauthn_api_->InjectDiscoverableCredential(
+        kCredentialID, std::move(rp), std::move(user));
+
+    // Inject the fake Windows platform authenticator.
+    auto device_factory =
+        std::make_unique<device::test::VirtualFidoDeviceFactory>();
+    device_factory->set_win_webauthn_api(fake_webauthn_api_.get());
+    content::AuthenticatorEnvironment::GetInstance()
+        ->ReplaceDefaultDiscoveryFactoryForTesting(std::move(device_factory));
+  }
+
+ protected:
+  std::unique_ptr<device::FakeWinWebAuthnApi> fake_webauthn_api_;
+};
+
+IN_PROC_BROWSER_TEST_F(WebAuthnWindowsAutofillIntegrationTest, SelectAccount) {
+  RunSelectAccountTest();
+}
+#endif  // BUILDFLAG(IS_WIN)
 
 // WebAuthnCableExtension exercises code paths where a server sends a caBLEv2
 // extension in a get() request.
@@ -367,7 +523,6 @@ class WebAuthnCableExtension : public WebAuthnBrowserTest {
 
     auto virtual_device_factory =
         std::make_unique<device::test::VirtualFidoDeviceFactory>();
-    static const uint8_t kCredentialID[] = {1, 2, 3, 4};
     virtual_device_factory->mutable_state()->InjectRegistration(
         kCredentialID, "www.example.com");
     content::AuthenticatorEnvironment::GetInstance()
@@ -612,7 +767,6 @@ class WebAuthnCableSecondFactor : public WebAuthnBrowserTest {
       void AddAuthenticator() {
         scoped_refptr<device::VirtualFidoDevice::State> state(
             new device::VirtualFidoDevice::State);
-        static const uint8_t kCredentialID[] = {1, 2, 3, 4};
         state->InjectRegistration(kCredentialID, "www.example.com");
         state->fingerprints_enrolled = true;
 
---
+++ chrome/browser/webauthn/chrome_webauthn_mac_browsertest.mm
@@ -0,0 +1,151 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <vector>
+
+#include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
+#include "chrome/browser/password_manager/chrome_webauthn_credentials_delegate.h"
+#include "chrome/browser/ssl/cert_verifier_browser_test.h"
+#include "chrome/browser/ui/autofill/autofill_popup_controller_impl.h"
+#include "chrome/browser/ui/autofill/chrome_autofill_client.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/autofill/core/browser/ui/popup_item_ids.h"
+#include "components/network_session_configurator/common/network_switches.h"
+#include "components/password_manager/core/browser/password_ui_utils.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/common/content_features.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "device/fido/features.h"
+#include "device/fido/mac/credential_store.h"
+#include "device/fido/mac/scoped_touch_id_test_environment.h"
+#include "device/fido/public_key_credential_user_entity.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace {
+
+static constexpr char kConditionalUIRequest[] = R"((() => {
+navigator.credentials.get({
+  mediation: 'conditional',
+  publicKey: {
+    challenge: new Uint8Array([1,2,3,4]),
+    timeout: 10000,
+    allowCredentials: [],
+  }}).then(c => window.domAutomationController.send('webauthn: OK'),
+           e => window.domAutomationController.send('error ' + e));
+})())";
+
+class WebAuthnMacAutofillIntegrationTest : public CertVerifierBrowserTest {
+ protected:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    CertVerifierBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
+  }
+
+  void SetUp() override {
+    ASSERT_TRUE(https_server_.InitializeAndListen());
+    CertVerifierBrowserTest::SetUp();
+  }
+
+  void SetUpOnMainThread() override {
+    CertVerifierBrowserTest::SetUpOnMainThread();
+
+    https_server_.ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+    https_server_.StartAcceptingConnections();
+    host_resolver()->AddRule("*", "127.0.0.1");
+
+    // Allowlist all certs for the HTTPS server.
+    auto cert = https_server_.GetCertificate();
+    net::CertVerifyResult verify_result;
+    verify_result.cert_status = 0;
+    verify_result.verified_cert = cert;
+    mock_cert_verifier()->AddResultForCert(cert.get(), verify_result, net::OK);
+    ASSERT_TRUE(ui_test_utils::NavigateToURL(
+        browser(),
+        https_server_.GetURL("www.example.com",
+                             "/webauthn_conditional_mediation.html")));
+
+    // Set up the fake keychain.
+    config_ =
+        ChromeWebAuthenticationDelegate::TouchIdAuthenticatorConfigForProfile(
+            browser()->profile());
+    touch_id_test_environment_ =
+        std::make_unique<device::fido::mac::ScopedTouchIdTestEnvironment>(
+            config_);
+    store_ =
+        std::make_unique<device::fido::mac::TouchIdCredentialStore>(config_);
+    device::PublicKeyCredentialUserEntity user({1, 2, 3, 4}, "flandre",
+                                               "Flandre Scarlet");
+    store_->CreateCredential(
+        "www.example.com", std::move(user),
+        device::fido::mac::TouchIdCredentialStore::kDiscoverable);
+    touch_id_test_environment_->SimulateTouchIdPromptSuccess();
+  }
+
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kWebAuthConditionalUI};
+  net::EmbeddedTestServer https_server_{net::EmbeddedTestServer::TYPE_HTTPS};
+  device::fido::mac::AuthenticatorConfig config_;
+  std::unique_ptr<device::fido::mac::ScopedTouchIdTestEnvironment>
+      touch_id_test_environment_;
+  std::unique_ptr<device::fido::mac::TouchIdCredentialStore> store_;
+};
+
+// Integration test between the mac keychain platform authenticator and autofill
+// UI.
+IN_PROC_BROWSER_TEST_F(WebAuthnMacAutofillIntegrationTest, SelectAccount) {
+  // Make sure input events cannot close the autofill popup.
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  autofill::ChromeAutofillClient* autofill_client =
+      autofill::ChromeAutofillClient::FromWebContents(web_contents);
+  autofill_client->KeepPopupOpenForTesting();
+
+  // Execute the Conditional UI request.
+  content::DOMMessageQueue message_queue(web_contents);
+  content::ExecuteScriptAsync(web_contents, kConditionalUIRequest);
+
+  // Interact with the username field until the popup shows up. This has the
+  // effect of waiting for the browser to send the renderer the password
+  // information, and waiting for the UI to render.
+  base::WeakPtr<autofill::AutofillPopupController> popup_controller;
+  while (!popup_controller) {
+    content::SimulateMouseClickOrTapElementWithId(web_contents, "username");
+    popup_controller = autofill_client->popup_controller_for_testing();
+  }
+
+  auto suggestions = popup_controller->GetSuggestions();
+  size_t suggestion_index;
+  autofill::Suggestion webauthn_entry;
+  for (suggestion_index = 0; suggestion_index < suggestions.size();
+       ++suggestion_index) {
+    if (suggestions[suggestion_index].frontend_id ==
+        autofill::PopupItemId::POPUP_ITEM_ID_WEBAUTHN_CREDENTIAL) {
+      webauthn_entry = suggestions[suggestion_index];
+      break;
+    }
+  }
+  ASSERT_LT(suggestion_index, suggestions.size()) << "WebAuthn entry not found";
+  EXPECT_EQ(webauthn_entry.main_text.value, u"flandre");
+  EXPECT_EQ(webauthn_entry.labels.at(0).at(0).value,
+            l10n_util::GetStringUTF16(
+                password_manager::GetPlatformAuthenticatorLabel()));
+  EXPECT_EQ(webauthn_entry.icon, "globeIcon");
+
+  // Click the credential.
+  popup_controller->AcceptSuggestion(suggestion_index);
+  std::string result;
+  ASSERT_TRUE(message_queue.WaitForMessage(&result));
+  EXPECT_EQ(result, "\"webauthn: OK\"");
+}
+
+}  // namespace
