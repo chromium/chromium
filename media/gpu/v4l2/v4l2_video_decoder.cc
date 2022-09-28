a53151a3510b91cb800fd624b5a7cfe6b626683c
Miguel Casas <mcasas@chromium.org>
2022-09-28 04:08:42 +0000

    media/gpu/v4l2: increase input queue size
    This CL increases the input queue size to be above 16, to support
    test vectors with 16 reference frames.
    
    Technically speaking the ref frames are part of the output queue,
    but because of the way the code is written now, the input queue size
    caps the output one. This should be fixed, but is a much larger
    task.
    
    This is verified by deploying and running the H.264 Main Profile
    test vectors on asurada (MT8192) and cherry (MT8195) locally.
    
    Bug: b/249325255, b/212179577
    Change-Id: Iacd210d050084b8fd6a5d48a2be18768cf81734f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923095
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052203}

--- media/gpu/v4l2/v4l2_video_decoder.cc
+++ media/gpu/v4l2/v4l2_video_decoder.cc
@@ -42,7 +42,10 @@ constexpr int k1080pArea = 1920 * 1088;
 constexpr size_t kInputBufferMaxSizeFor1080p = 1024 * 1024;
 // Input bitstream buffer size for up to 4k streams.
 constexpr size_t kInputBufferMaxSizeFor4k = 4 * kInputBufferMaxSizeFor1080p;
-constexpr size_t kNumInputBuffers = 8;
+// Some H.264 test vectors (CAPCM*1_Sand_E.h264) need 16 reference frames.
+// TODO(b/249325255): reduce this number to e.g. 8 or even less when it does not
+// artificially limit the size of the CAPTURE (decoded video frames) queue.
+constexpr size_t kNumInputBuffers = 17;
 
 // Input format V4L2 fourccs this class supports.
 constexpr uint32_t kSupportedInputFourccs[] = {
