d1d10a8066770b3943014086813aba7724cab244
Mahmoud Rashad <mmrashad@google.com>
2022-09-28 12:21:19 +0000

    [Sync] Rename signin_promo_signin_button to sync_promo_signin_button
    This CL automatically renames signin_promo_signin_button to
    sync_promo_signin_button using VSCode Find and replace, no behavioral
    changes.
    
    Bug: 1319351
    Change-Id: I37cdb8b27221f8a6626ad1803a6310063e5c0deb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925678
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1052342}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -110,7 +110,7 @@ public class BookmarkPersonalizedSigninPromoTest {
                 .launchActivityForPromoDefaultFlow(any(Context.class), anyInt(), anyString());
         showBookmarkManagerAndCheckSigninPromoIsDisplayed();
 
-        onView(allOf(withId(R.id.signin_promo_signin_button), withEffectiveVisibility(VISIBLE)))
+        onView(allOf(withId(R.id.sync_promo_signin_button), withEffectiveVisibility(VISIBLE)))
                 .perform(click());
 
         verify(mMockSyncConsentActivityLauncher)
@@ -147,7 +147,7 @@ public class BookmarkPersonalizedSigninPromoTest {
                 .when(SyncConsentActivityLauncherImpl.get())
                 .launchActivityForPromoAddAccountFlow(any(Context.class), anyInt());
         showBookmarkManagerAndCheckSigninPromoIsDisplayed();
-        onView(withId(R.id.signin_promo_signin_button)).perform(click());
+        onView(withId(R.id.sync_promo_signin_button)).perform(click());
         verify(mMockSyncConsentActivityLauncher)
                 .launchActivityForPromoAddAccountFlow(
                         any(Activity.class), eq(SigninAccessPoint.BOOKMARK_MANAGER));
