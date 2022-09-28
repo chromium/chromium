b95931cdad7e252d02e5518de34474109d960804
Gauthier Ambard <gambard@chromium.org>
2022-09-28 15:22:40 +0000

    [iOS] Fix typo in symbol name
    The symbol name was wrong.
    
    Fixed: 1368432, 1368431
    Change-Id: Ib8151f789e7ab73f660505eb096f220fca444919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925683
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052415}

--- ios/chrome/browser/ui/icons/chrome_symbol.mm
+++ ios/chrome/browser/ui/icons/chrome_symbol.mm
@@ -56,7 +56,7 @@ NSString* const kPlusCircleFillSymbol = @"plus_circle_fill";
 NSString* const kPopupBadgeMinusSymbol = @"popup_badge_minus";
 NSString* const kPhotoBadgePlusSymbol = @"photo_badge_plus";
 NSString* const kPhotoBadgeMagnifyingglassSymbol =
-    @"photo_badge_magnifyinggglass";
+    @"photo_badge_magnifyingglass";
 NSString* const kReadingListSymbol = @"square_bullet_square";
 NSString* const kRecentTabsSymbol = @"laptopcomputer_and_phone";
 NSString* const kLanguageSymbol = @"language";
