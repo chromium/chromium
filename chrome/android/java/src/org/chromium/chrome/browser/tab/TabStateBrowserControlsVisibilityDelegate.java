dea4d8f82990014610413857de846e0d5d6fb62f
Kevin Grosu <kgrosu@google.com>
2022-09-28 22:12:48 +0000

    [CCT] Toolbar disappears when scroll up, after switching theme
    This is happening because when switching themes the BrowserControlsState is incorrectly reported as BrowserControlsState.BOTH instead of BrowserControlsState.SHOWN when EXTRA_ENABLE_URLBAR_HIDING is not set or set to false. Upon investigation it seems that on the normal boot/loading of the CCT the TabStateBrowserControlsVisibilityDelegate updateVisibilityConstraints method is being called as part of different tab events that are being observed. However, in the theme change logic, these values are no longer called (mostly because the page is already loaded) for the object the is being used to determine the BrowserControlsState for the current CCT.
    
    Looking at the TabStateBrowserControlsVisibilityDelegate constructor it is not calling updateVisibilityConstraints() while all the other visibility delegates (BrowserStateBrowserControlsVisibilityDelegate, CustomTabBrowserControlsVisibilityDelegate,ComposedBrowserControlsVisibilityDelegate) are calling it (or similar logic). Adding the call to updateVisibilityConstraints() fixes the issue observed and now the BrowserControlsState is correctly calculated when EXTRA_ENABLE_URLBAR_HIDING is not set or set to false.
    
    Please see the linked bug for more information about the investigation of the issue.
    
    Bug: 1360447
    Change-Id: Id8f8e9b5dfef59cbd683bbecc4608e8f31e8805d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3923976
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Kevin Grosu <kgrosu@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052667}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabStateBrowserControlsVisibilityDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabStateBrowserControlsVisibilityDelegate.java
@@ -161,6 +161,7 @@ public class TabStateBrowserControlsVisibilityDelegate
             }
         });
         onWebContentsUpdated(mTab.getWebContents());
+        updateVisibilityConstraints();
     }
 
     private void onWebContentsUpdated(WebContents contents) {
