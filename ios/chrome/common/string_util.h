05d8a25b5b0b09734619068a7247c4c4f3db732c
Mark Cogan <marq@google.com>
2022-09-28 17:46:02 +0000

    [iOS] Delete unused string utilities.
    A few string utilities had no call sites; this CL removes them and
    their unit tests.
    
    Change-Id: I9242a6ae23babfe502086553a3457b06313183ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925587
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1052506}

--- ios/chrome/common/string_util.h
+++ ios/chrome/common/string_util.h
@@ -65,27 +65,6 @@ StringWithTags ParseStringWithTags(NSString* text,
                                    NSString* begin_tag,
                                    NSString* end_tag);
 
-// Utility method that returns an NSCharacterSet containing Unicode graphics
-// and drawing characters (but not including the Braille Patterns characters).
-NSCharacterSet* GraphicCharactersSet();
-
-// Cleans an NSString by collapsing whitespace and removing leading and trailing
-// spaces. If `removeGraphicChars` is true, unicode graphic characters will also
-// be removed from the string.
-NSString* CleanNSStringForDisplay(NSString* dirty, BOOL removeGraphicChars);
-
-// Cleans a std::string identically to CleanNSStringForDisplay()
-std::string CleanStringForDisplay(const std::string& dirty,
-                                  BOOL removeGraphicChars);
-
-// Find the longest leading substring of `string` that, when rendered with
-// `attributes`, will fit on a single line inside `targetWidth`. If `trailing`
-// is YES, then find the trailing (instead of leading) substring.
-NSString* SubstringOfWidth(NSString* string,
-                           NSDictionary* attributes,
-                           CGFloat targetWidth,
-                           BOOL trailing);
-
 // Returns the bound of an attributed string with NSRange
 // `characterRange` in the `textView`.
 CGRect TextViewLinkBound(UITextView* textView, NSRange characterRange);
