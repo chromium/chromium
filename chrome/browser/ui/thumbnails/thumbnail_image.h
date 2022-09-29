af7df76c1438275eb90425702dfbcca3a9e79cfd
Dana Fried <dfried@chromium.org>
2022-09-29 18:58:49 +0000

    [Tab Hover Cards] Ensure thumbnail image is thread-safe.
    This may be the cause of the attached bug.
    
    Bug: 1367344
    Change-Id: I02c065f9ad027eb1e22e9e966e4960b7697188ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3926315
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053100}

--- chrome/browser/ui/thumbnails/thumbnail_image.h
+++ chrome/browser/ui/thumbnails/thumbnail_image.h
@@ -26,7 +26,7 @@ class TimeTicks;
 
 // Stores compressed thumbnail data for a tab and can vend that data as an
 // uncompressed image to observers.
-class ThumbnailImage : public base::RefCounted<ThumbnailImage> {
+class ThumbnailImage : public base::RefCountedThreadSafe<ThumbnailImage> {
  public:
   // Describes the readiness of the source page for thumbnail capture.
   enum class CaptureReadiness : int {
@@ -170,7 +170,7 @@ class ThumbnailImage : public base::RefCounted<ThumbnailImage> {
  private:
   friend class Delegate;
   friend class ThumbnailImageTest;
-  friend class base::RefCounted<ThumbnailImage>;
+  friend class base::RefCountedThreadSafe<ThumbnailImage>;
 
   virtual ~ThumbnailImage();
 
