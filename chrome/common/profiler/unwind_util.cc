271ceeb832d33fa7ca92834cd41d6dfed58fd2bd
Tushar Agarwal <agarwaltushar@google.com>
2022-09-29 16:35:53 +0000

    Update `AreUnwinderAssetsAvailable` to filter for official builds only.
    This is because unwind assets are embedded for certain tests that are
    used in local/CQ builds.
    
    For example:
    * https://source.chromium.org/chromium/chromium/src/+/main:services/BUILD.gn;l=20;drc=888a1dbf5e374d3821ccc29832afb5813eab0587
    * https://source.chromium.org/chromium/chromium/src/+/main:chrome/test/BUILD.gn;l=953;drc=f6ba4fdfbf5205ec3e1db9c63ff1b5abe2535414
    * https://source.chromium.org/chromium/chromium/src/+/main:base/BUILD.gn;l=3645;drc=e51c138cb9ce85c72a408bfb7904b6adc4f9cbc8
    
    Bug: 1364268,1360376
    Change-Id: I05f75888f9a3915ab740eac838657cb0dc90b05e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921582
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053036}

--- chrome/common/profiler/unwind_util.cc
+++ chrome/common/profiler/unwind_util.cc
@@ -159,11 +159,16 @@ std::vector<std::unique_ptr<base::Unwinder>> CreateLibunwindstackUnwinders(
 // unwinders to work -- are available in the current context. Unwinder assets
 // are only embedded into certain builds of Chrome.
 bool AreUnwinderAssetsAvailable(version_info::Channel channel) {
+#if defined(OFFICIAL_BUILD) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
   // CFI is currently only embedded into dev, canary, and beta builds of Chrome:
   // https://crsrc.org/c/chrome/android/chrome_public_apk_tmpl.gni;l=30-36;drc=32cca7e9d8c49d42e393c75ffb404a0f8899704d
   return channel == version_info::Channel::CANARY ||
          channel == version_info::Channel::DEV ||
          channel == version_info::Channel::BETA;
+#else
+  // Local/CQ builds.
+  return true;
+#endif
 }
 
 // Manages installation of the module prerequisite for unwinding. Android, in
