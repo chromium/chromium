fe54a4fda68a91c3b687becb93bae98980376e2d
Steve Kobes <skobes@chromium.org>
2022-09-29 16:56:05 +0000

    Enable Scroll Unification on trunk.
    Bug: 476553
    Change-Id: Iebab413d434f6ec96f1a247e544d1b0c9300589e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3903621
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053052}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -42,7 +42,7 @@ BASE_FEATURE(kRemoveMobileViewportDoubleTap,
 
 BASE_FEATURE(kScrollUnification,
              "ScrollUnification",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSchedulerSmoothnessForAnimatedScrolls,
              "SmoothnessModeForAnimatedScrolls",
