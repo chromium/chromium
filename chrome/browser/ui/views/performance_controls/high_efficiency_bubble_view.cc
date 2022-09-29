b0d6ca9c8f3b25edf0c79fca06e079727db2c976
Eshwar Stalin <estalin@chromium.org>
2022-09-29 16:28:40 +0000

    Fixing a bad merge in High Efficiency bubble dialog
    A previous CL which refactored link implementation in DialogModel conflicted with the string changes made to the High Efficiency bubble dialog but the merge resolution wasn't done correctly.
    
    Bug: None
    Change-Id: I5db0a10de9f5f8c1c4dcc05f8333485818b1f67c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928322
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053034}

--- chrome/browser/ui/views/performance_controls/high_efficiency_bubble_view.cc
+++ chrome/browser/ui/views/performance_controls/high_efficiency_bubble_view.cc
@@ -37,14 +37,16 @@ views::BubbleDialogModelHost* HighEfficiencyBubbleView::ShowBubble(
           .SetDialogDestroyingCallback(
               base::BindOnce(&HighEfficiencyBubbleDelegate::OnDialogDestroy,
                              base::Unretained(bubble_delegate)))
-          .AddParagraph(ui::DialogModelLabel(IDS_HIGH_EFFICIENCY_DIALOG_BODY)
-                            .set_is_secondary())
-          .AddOkButton(base::OnceClosure(), l10n_util::GetStringUTF16(IDS_DONE))
-          .AddExtraLink(ui::DialogModelLabel::CreateLink(
-              IDS_SETTINGS_TITLE,
-              base::BindRepeating(
-                  &HighEfficiencyBubbleDelegate::OnSettingsClicked,
-                  base::Unretained(bubble_delegate))))
+          .AddParagraph(
+              ui::DialogModelLabel::CreateWithReplacement(
+                  IDS_HIGH_EFFICIENCY_DIALOG_BODY,
+                  ui::DialogModelLabel::CreateLink(
+                      IDS_HIGH_EFFICIENCY_DIALOG_BODY_LINK_TEXT,
+                      base::BindRepeating(
+                          &HighEfficiencyBubbleDelegate::OnSettingsClicked,
+                          base::Unretained(bubble_delegate))))
+                  .set_is_secondary())
+          .AddOkButton(base::DoNothing())
           .Build();
 
   auto bubble_unique = std::make_unique<views::BubbleDialogModelHost>(
