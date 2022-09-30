9e9e662220a8a0342901b33b10aebc33dc521fe2
Filip Gorski <fgorski@chromium.org>
2022-09-30 17:17:47 +0000

    [Sheriff] Disable HardeningTest.SuccessfulCorruption on Android
    Bug: 1370048
    Change-Id: I9977ff3d2571fe6f9578d96feb22c605cbb5ce79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930848
    Owners-Override: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053634}

--- base/allocator/partition_allocator/hardening_unittest.cc
+++ base/allocator/partition_allocator/hardening_unittest.cc
@@ -112,6 +112,9 @@ TEST(HardeningTest, MetadataPointerCrashing) {
 #endif  // !BUILDFLAG(IS_ANDROID) && defined(GTEST_HAS_DEATH_TEST) &&
         // defined(PA_HAS_FREELIST_SHADOW_ENTRY)
 
+// Below test also misbehaves on Android, crbug.com/1370048
+#if !BUILDFLAG(IS_ANDROID)
+
 TEST(HardeningTest, SuccessfulCorruption) {
   PartitionRoot<ThreadSafe> root({
       PartitionOptions::AlignedAlloc::kAllowed,
@@ -156,6 +159,7 @@ TEST(HardeningTest, SuccessfulCorruption) {
   EXPECT_EQ(new_data2, to_corrupt);
 #endif  // BUILDFLAG(USE_FREESLOT_BITMAP)
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace
 }  // namespace partition_alloc::internal
