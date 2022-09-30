b5ff89ef71c07937bcb08e1d5b380da106737dba
Tom Anderson <thomasanderson@chromium.org>
2022-09-30 08:48:26 +0000

    Add wayland-protocols to sysroots
    R=adunaev
    
    Change-Id: I38dda9bb4db7542129600eb8b086814ad155d741
    Bug: 1366678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929479
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053466}

--- build/linux/sysroot_scripts/sysroot-creator.sh
+++ build/linux/sysroot_scripts/sysroot-creator.sh
@@ -484,8 +484,9 @@ InstallIntoSysroot() {
     dpkg-deb -e ${package} ${INSTALL_ROOT}/debian/${base_package}/DEBIAN
   done
 
-  # Prune /usr/share, leaving only pkgconfig.
-  ls -d ${INSTALL_ROOT}/usr/share/* | grep -v "/pkgconfig$" | xargs rm -r
+  # Prune /usr/share, leaving only pkgconfig and wayland-protocols.
+  ls -d ${INSTALL_ROOT}/usr/share/* | \
+    grep -v "/\(pkgconfig\|wayland-protocols\)$" | xargs rm -r
 }
 
 
