6ca53c0fcea1373be7fc0f2898fc3440ca99ebb8
Alan Zhao <ayzhao@google.com>
2022-09-30 01:27:21 +0000

    Replace 7z binaries with CIPD packages
    Rather than relying on 7z executables checked into Git, we now use 7z
    binaries distributed 3pp CIPD packages, which were introduced in
    https://crrev.com/c/3915306.
    
    The binaries are installed if and only if the build targets Windows.
    Two versions of the binaries are installed. For all host platforms, we
    install a win64 version in bin/win64. This is meant for Chromium targets
    that package 7z for distribution. A host platform specific version is
    also installed in bin/host_platform, suitable for build scripts that
    call the 7z executables.
    
    The main motivation for this change is that the linux-win_cross-rel bots
    seem to be broken because the bots have an older version of the 7z
    binary that does not support the arguments passed to it by the build
    script. By using a CIPD package, we ensure that we always have a binary
    with the required options.
    
    Additionally:
    * Reenable gcp_installer for cross builds since this should fix the
      issue.
    * Update build scripts as necessary.
    * Fix a path separator bug in make_setup.py
    * Update README.chromium
    
    Unfortunately there is no linux-win_cross-rel tryjob to verify this CL,
    but I did perform a successful build locally on my gLinux machine with
    the exact same gn args, so this should work.
    
    (cherry picked from commit 8c47b8f21a910031c05d34a547d848a24166bb34)
    
    Bug: 1143303
    Change-Id: I5bb55219d545a3dfd9d9f4d8b1276834e4456ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3858186
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1052096}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928926
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Alan Zhao <ayzhao@google.com>
    Cr-Commit-Position: refs/branch-heads/5304@{#274}
    Cr-Branched-From: 5d7b1fc9cb7103d9c82eed647cf4be38cf09738b-refs/heads/main@{#1047731}

--- chrome/updater/win/installer/create_installer_archive.py
+++ chrome/updater/win/installer/create_installer_archive.py
@@ -86,15 +86,12 @@ def CopySectionFilesToStagingDir(config, section, staging_dir, src_dir,
 
 
 def GetLZMAExec(build_dir):
+    executable = '7za'
     if sys.platform == 'win32':
-        lzma_exec = os.path.join(build_dir, "..", "..", "third_party",
-                                 "lzma_sdk", "bin", "win64", "7za.exe")
-    elif sys.platform == 'darwin':
-        lzma_exec = os.path.join(build_dir, "..", "..", "third_party",
-                                 "lzma_sdk", "bin", "mac64", "7zz")
-    else:
-        lzma_exec = '7zr'  # Use system 7zr.
-    return lzma_exec
+        executable += '.exe'
+
+    return os.path.join(build_dir, "..", "..", "third_party", "lzma_sdk",
+                        "bin", "host_platform", executable)
 
 
 def MakeStagingDirectory(staging_dir):
