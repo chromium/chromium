97b3d82d94af95d5d0487a456baa7289ff82faf6
paulzhchen <paulzchen@google.com>
2022-09-30 16:06:06 +0000

    [Eche] Disable EcheSWASendStartSignaling feature.
    Change EcheSWASendStartSignaling to default OFF.
    
    Bug: b/249732379
    Change-Id: Icb378be5e43717213779bb6b6dd8e2d3b9ccf7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925090
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Mavis Hsu <mavishsu@google.com>
    Auto-Submit: Paulz Chen <paulzchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1053601}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -680,7 +680,7 @@ BASE_FEATURE(kEcheSWAMeasureLatency,
 // Enables sending start signaling to establish Eche's WebRTC connection.
 BASE_FEATURE(kEcheSWASendStartSignaling,
              "EcheSWASendStartSignaling",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Allows disabling the stun servers when establishing a WebRTC connection to
 // Eche.
