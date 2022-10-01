e1a97837425b893cf583a3442e340ce62a54a14c
Haiyang Pan <hypan@google.com>
2022-10-01 02:14:41 +0000

    Remove SERVICE_ACCOUNT_JSON from logdog wrapper.
    In LUCI env, the logdog should just work with LUCI_CONTEXT out of box.
    
    Bug: 1311796
    Change-Id: I9981bdbc0aeed911b3921748eec473fccc60ccad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3931205
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Owners-Override: Ben Pastene <bpastene@chromium.org>
    Auto-Submit: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053859}

--- build/android/test_wrapper/logdog_wrapper.py
+++ build/android/test_wrapper/logdog_wrapper.py
@@ -28,8 +28,6 @@ from py_utils import tempfile_ext
 PROJECT = 'chromium'
 OUTPUT = 'logdog'
 COORDINATOR_HOST = 'luci-logdog.appspot.com'
-SERVICE_ACCOUNT_JSON = ('/creds/service_accounts'
-                        '/service-account-luci-logdog-publisher.json')
 LOGDOG_TERMINATION_TIMEOUT = 30
 
 
@@ -110,7 +108,6 @@ def main():
           '-project', PROJECT,
           '-output', OUTPUT,
           '-prefix', prefix,
-          '--service-account-json', SERVICE_ACCOUNT_JSON,
           '-coordinator-host', COORDINATOR_HOST,
           'serve',
           '-streamserver-uri', streamserver_uri]
