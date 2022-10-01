c2725a5f5cf8fd9bff503b099087d66d40b5d234
David Bertoni <dbertoni@chromium.org>
2022-10-01 01:50:31 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ui/app_list/arc.
    This CL was uploaded by git cl split.
    
    R=lgcheng@google.com
    
    Bug: 1303949
    Change-Id: I006e3763a89543678936ae7345acd18e3bceab19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929795
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1053858}

--- chrome/browser/ui/app_list/arc/arc_usb_host_permission_manager.cc
+++ chrome/browser/ui/app_list/arc/arc_usb_host_permission_manager.cc
@@ -177,7 +177,7 @@ void ArcUsbHostPermissionManager::RestorePermissionFromChromePrefs() {
     }
 
     for (const auto& access_permision :
-         access_permision_list_value->GetListDeprecated()) {
+         access_permision_list_value->GetList()) {
       const base::Value* serial_number_value = access_permision.FindKeyOfType(
           kUsbSerialNumber, base::Value::Type::STRING);
       if (!serial_number_value) {
