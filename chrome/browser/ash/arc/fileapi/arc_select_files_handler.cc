5ee82a481f9072fea35f878bd74139f6a1e6e065
David Bertoni <dbertoni@chromium.org>
2022-10-03 23:28:58 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ash/arc/fileapi.
    This CL was uploaded by git cl split.
    
    R=djacobo@chromium.org
    
    Bug: 1303949
    Change-Id: I780cfa8b8400fbeb81e541da8b763fb3fdc5bd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933669
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054414}

--- chrome/browser/ash/arc/fileapi/arc_select_files_handler.cc
+++ chrome/browser/ash/arc/fileapi/arc_select_files_handler.cc
@@ -82,7 +82,7 @@ void ConvertToElementVector(
   if (!list_value || !list_value->is_list())
     return;
 
-  for (const base::Value& value : list_value->GetListDeprecated()) {
+  for (const base::Value& value : list_value->GetList()) {
     mojom::FileSelectorElementPtr element = mojom::FileSelectorElement::New();
     element->name = value.GetString();
     elements->push_back(std::move(element));
