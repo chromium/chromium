b312720421eeeb0dcd941087bd05ce9d8cf82e2d
Sorin Jianu <sorin@chromium.org>
2022-10-03 22:34:57 +0000

    Avoid the crash in UpdaterTest.UpdaterTestVersionResource.
    When the version resource object can't be created, dereferencing it
    leads to a crash. This does not explain why the version resource
    object can't always be created but it avoids the crash.
    
    Bug: 1370794
    Change-Id: Iaac87d30bdbdc00902463dae751a11037170294d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932029
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054374}

--- chrome/updater/updater_unittest.cc
+++ chrome/updater/updater_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
@@ -58,17 +59,20 @@ TEST(UpdaterTest, UpdaterExitCode) {
 // Tests that the updater test target version resource contains specific
 // information to disambiguate the binary. For Windows builds and during tests,
 // the "updater_test.exe" file is being installed as "updater.exe", therefore
-// it is useful to tell them apart.
+// it is useful to have a hint of information available somewhere, to determine
+// whether "updater.exe" is the test variant or the production variant.
 TEST(UpdaterTest, UpdaterTestVersionResource) {
   base::FilePath this_executable_path;
   ASSERT_TRUE(base::PathService::Get(base::FILE_EXE, &this_executable_path));
-
-  const base::FilePath executable_test(FILE_PATH_LITERAL("updater_test.exe"));
+  const base::FilePath updater_test_name(FILE_PATH_LITERAL("updater_test.exe"));
+  const base::FilePath updater_test_path(
+      this_executable_path.DirName().Append(updater_test_name));
+  ASSERT_TRUE(base::PathExists(updater_test_path)) << updater_test_path;
   const std::unique_ptr<FileVersionInfoWin> version_info =
-      FileVersionInfoWin::CreateFileVersionInfoWin(
-          this_executable_path.DirName().Append(executable_test));
-
-  EXPECT_EQ(version_info->original_filename(), executable_test.AsUTF16Unsafe());
+      FileVersionInfoWin::CreateFileVersionInfoWin(updater_test_path);
+  ASSERT_NE(version_info, nullptr);
+  EXPECT_EQ(version_info->original_filename(),
+            updater_test_name.AsUTF16Unsafe());
 }
 
 // Checks that the unit test has the SE_DEBUG_NAME privilege when the process is
