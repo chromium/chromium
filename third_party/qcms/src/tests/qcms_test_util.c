ec4408f76a4f5966b70b3d080fb54b8059c21b25
Avi Drissman <avi@chromium.org>
2022-10-04 16:48:35 +0000

    Update copyright headers in third_party/q*/ and third_party/r*/
    These files are used to import new versions of third-party code, to
    integrate that code into Chromium, or are otherwise not from upstream.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I5eeaad1d47ab1ae554571a746906432151f327df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934922
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054783}

--- third_party/qcms/src/tests/qcms_test_util.c
+++ third_party/qcms/src/tests/qcms_test_util.c
@@ -1,6 +1,6 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the Chromium LICENSE file.
+// found in the LICENSE file.
 
 #include "qcms_test_util.h"
 
