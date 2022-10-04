0923c3952a38ae246328c2c19d5097357ab2709e
Salman Malik <salmanmalik@chromium.org>
2022-10-04 21:38:43 +0000

    desktop_capturer_proxy: Wrap wayland capturer in diff capturer
    Without this, we observe consistently high CPU utilization
    by chromoting binary (perf suggests time spent in encoding / codecs).
    
    Bug: chromium:1291247
    Change-Id: I61409150982773d6741373907f58beaf018a53d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921826
    Commit-Queue: Salman Malik <salmanmalik@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054949}

--- remoting/host/desktop_capturer_proxy.cc
+++ remoting/host/desktop_capturer_proxy.cc
@@ -97,7 +97,14 @@ void DesktopCapturerProxy::Core::CreateCapturer(
   }
 #elif defined(REMOTING_USE_WAYLAND)
   if (options.allow_pipewire() && DesktopCapturer::IsRunningUnderWayland()) {
-    capturer_ = std::make_unique<WaylandDesktopCapturer>(options);
+    // Even though wayland itself has mechanism to detect surface damage in a
+    // fine grained fashion, the framebuffer handed over by the compositor over
+    // to the pipewire stream contains the entire scene (and not just the delta)
+    // from one frame to another. DesktopCapturerDiffWrapper is the closest
+    // thing that can help mimic XDamage behavior for wayland and avoid
+    // unnecessary frame encoding overhead.
+    capturer_ = std::make_unique<webrtc::DesktopCapturerDifferWrapper>(
+        std::make_unique<WaylandDesktopCapturer>(options));
   } else {
     capturer_ = webrtc::DesktopCapturer::CreateScreenCapturer(options);
   }
