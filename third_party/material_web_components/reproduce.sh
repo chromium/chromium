5964ce8eb626cc3c18f437f194a1e9234aebe8aa
Avi Drissman <avi@chromium.org>
2022-10-04 19:30:51 +0000

    Update copyright headers in third_party/m*/ and third_party/n*/
    These files are used to import new versions of third-party code, to
    integrate that code into Chromium, or are otherwise not from upstream.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    No-Presubmit: true
    Bug: 1098010
    Change-Id: I354f22ed87bfe621958020b5ad0214075734e649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933378
    Owners-Override: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054878}

--- third_party/material_web_components/reproduce.sh
+++ third_party/material_web_components/reproduce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021 The Chromium Authors. All rights reserved.
+# Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -65,7 +65,7 @@ if [[ ! -z "${new}${deleted}" ]]; then
 fi
 
 cat > BUILD.gn << EOF
-# Copyright 2022 The Chromium Authors. All rights reserved.
+# Copyright 2022 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
