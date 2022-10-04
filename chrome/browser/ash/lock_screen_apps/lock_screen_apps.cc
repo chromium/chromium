f8e5513a621246d4159cc8874418725088956eeb
David Bertoni <dbertoni@chromium.org>
2022-10-04 02:25:07 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ash/lock_screen_apps.
    This CL was uploaded by git cl split.
    
    R=glenrob@chromium.org
    
    Bug: 1303949
    Change-Id: Ia26c0a5eaff7aeef43c0d1b59074bf6b04b688e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933671
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054490}

--- chrome/browser/ash/lock_screen_apps/lock_screen_apps.cc
+++ chrome/browser/ash/lock_screen_apps/lock_screen_apps.cc
@@ -132,7 +132,7 @@ std::unique_ptr<std::set<std::string>> GetAllowedLockScreenApps(
 
   auto allowed_apps = std::make_unique<std::set<std::string>>();
   for (const base::Value& app_value :
-       allowed_lock_screen_apps_value->GetListDeprecated()) {
+       allowed_lock_screen_apps_value->GetList()) {
     if (!app_value.is_string()) {
       LOG(ERROR) << "Invalid app ID value " << app_value;
       continue;
