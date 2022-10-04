26955db6af38f71ffc7d6eaf4821c86289360861
Tomasz Jurkiewicz <tju@google.com>
2022-10-04 08:54:42 +0000

    Move Access Token fetch utilities to separate files.
    Change-Id: Ia6d9cbfd3f249bde01899419fb42741138e74505
    
    b: b/250798468
    Change-Id: Ia6d9cbfd3f249bde01899419fb42741138e74505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3921762
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1054589}

--- chrome/browser/supervised_user/kids_chrome_management/kids_access_token_fetcher_unittest.cc
+++ chrome/browser/supervised_user/kids_chrome_management/kids_access_token_fetcher_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/supervised_user/kids_chrome_management/kids_management_service.h"
+#include "chrome/browser/supervised_user/kids_chrome_management/kids_access_token_fetcher.h"
 
 #include "base/bind.h"
 #include "base/functional/callback.h"
--- chrome/browser/supervised_user/kids_chrome_management/kids_management_service.h
+++ chrome/browser/supervised_user/kids_chrome_management/kids_management_service.h
@@ -5,46 +5,10 @@
 #ifndef CHROME_BROWSER_SUPERVISED_USER_KIDS_CHROME_MANAGEMENT_KIDS_MANAGEMENT_SERVICE_H_
 #define CHROME_BROWSER_SUPERVISED_USER_KIDS_CHROME_MANAGEMENT_KIDS_MANAGEMENT_SERVICE_H_
 
-#include <memory>
-
 #include "base/memory/singleton.h"
-#include "base/types/expected.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/signin/public/identity_manager/access_token_fetcher.h"
-#include "components/signin/public/identity_manager/access_token_info.h"
-#include "components/signin/public/identity_manager/identity_manager.h"
-#include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
 #include "content/public/browser/browser_context.h"
-#include "google_apis/gaia/gaia_constants.h"
-#include "google_apis/gaia/google_service_auth_error.h"
-#include "google_apis/gaia/oauth2_access_token_manager.h"
-
-// Responsible for initialing the access token workflow. Executes the consuming
-// callback when the fetch is done, and then becomes disposable.
-class KidsAccessTokenFetcher {
- public:
-  // For convenience, the interface of signin::PrimaryAccountAccessTokenFetcher
-  // is wrapped into one value, so the decision how to handle errors is up to
-  // consumers of access token fetcher.
-  using Consumer = base::OnceCallback<void(
-      base::expected<signin::AccessTokenInfo, GoogleServiceAuthError>)>;
-  // Non copyable.
-  KidsAccessTokenFetcher() = delete;
-  explicit KidsAccessTokenFetcher(signin::IdentityManager& identity_manager,
-                                  Consumer consumer);
-  KidsAccessTokenFetcher(const KidsAccessTokenFetcher&) = delete;
-  KidsAccessTokenFetcher& operator=(const KidsAccessTokenFetcher&) = delete;
-  ~KidsAccessTokenFetcher();
-
- private:
-  void OnAccessTokenFetchComplete(GoogleServiceAuthError error,
-                                  signin::AccessTokenInfo access_token_info);
-  static const OAuth2AccessTokenManager::ScopeSet& Scopes();
-  Consumer consumer_;
-  std::unique_ptr<signin::PrimaryAccountAccessTokenFetcher>
-      primary_account_access_token_fetcher_;
-};
 
 // A keyed service aggregating services for respective RPCs in
 // KidsManagementAPI.
