0d800f1e5dca266ef7ba2a8650cfc9ef5e1969c1
Arnaud Mandy <arnaud.mandy@intel.com>
2022-10-04 12:50:12 +0000

    compute pressure: WPT: Fix timestamp issue
    The timestamp that needs to be sent to Blink in the pressure updates
    should be a JS object representing mojo_base.mojom.Time type.
    
    Change-Id: I6a1866d584b4e3152761c96e9ccf8062c3b3a8c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930750
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1054665}

--- third_party/blink/web_tests/external/wpt/resources/chromium/mock-pressure-service.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/mock-pressure-service.js
@@ -19,11 +19,6 @@ class MockPressureService {
       ['power-supply', PressureFactor.kPowerSupply]
     ]);
     this.pressureServiceReadingTimerId_ = null;
-    // Sets a timestamp by creating a DOMHighResTimeStamp from a given
-    // platform timestamp. In this mock implementation we use a starting value
-    // and an increment step value that resemble a platform timestamp
-    // reasonably enough.
-    this.timestamp_ = window.performance.timeOrigin;
   }
 
   start() {
@@ -64,11 +59,28 @@ class MockPressureService {
     if (this.pressureServiceReadingTimerId_ != null)
       stopPlatformCollector();
 
+    // The following code for calculating the timestamp was taken from
+    // https://source.chromium.org/chromium/chromium/src/+/main:third_party/
+    // blink/web_tests/http/tests/resources/
+    // geolocation-mock.js;l=131;drc=37a9b6c03b9bda9fcd62fc0e5e8016c278abd31f
+
+    // The new Date().getTime() returns the number of milliseconds since the
+    // UNIX epoch (1970-01-01 00::00:00 UTC), while |internalValue| of the
+    // device.mojom.PressureUpdate represents the value of microseconds since
+    // the Windows FILETIME epoch (1601-01-01 00:00:00 UTC). So add the delta
+    // when sets the |internalValue|. See more info in //base/time/time.h.
+    const windowsEpoch = Date.UTC(1601, 0, 1, 0, 0, 0, 0);
+    const unixEpoch = Date.UTC(1970, 0, 1, 0, 0, 0, 0);
+    // |epochDeltaInMs| equals to base::Time::kTimeTToMicrosecondsOffset.
+    const epochDeltaInMs = unixEpoch - windowsEpoch;
+
     const timeout = (1 / sampleRate) * 1000;
     this.pressureServiceReadingTimerId_ = window.setInterval(() => {
       if (this.pressureUpdate_ === null || this.observer_ === null)
         return;
-      this.pressureUpdate_.timestamp = this.timestamp_++;
+      this.pressureUpdate_.timestamp = {
+        internalValue: BigInt((new Date().getTime() + epochDeltaInMs) * 1000)
+      };
       this.observer_.onUpdate(this.pressureUpdate_);
       this.updatesDelivered_++;
     }, timeout);
@@ -102,7 +114,6 @@ class MockPressureService {
     this.pressureUpdate_ = {
       state: this.mojomStateType_.get(state),
       factors: pressureFactors,
-      timestamp: window.performance.timeOrigin
     };
   }
 }
