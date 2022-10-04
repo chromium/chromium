e7270a7dc8e555a7634e6e274adc49d79903f678
Roman Sorokin <rsorokin@chromium.org>
2022-10-04 10:19:36 +0000

    [known_user] Remove deprecated known_user::GetAccountId
    Migrate to their KnownUser variant in
    /chrome/browser/signin
    
    This CL was uploaded by git cl split.
    
    R=msalama@chromium.org
    
    Bug: 1150434
    Change-Id: Iecb6dc02d0b49eaad14e249b8b0a9d901be4ba87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925213
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1054625}

--- chrome/browser/signin/account_id_from_account_info.cc
+++ chrome/browser/signin/account_id_from_account_info.cc
@@ -4,16 +4,13 @@
 
 #include "chrome/browser/signin/account_id_from_account_info.h"
 #include "build/chromeos_buildflags.h"
+#include "components/account_id/account_id.h"
 #include "google_apis/gaia/gaia_auth_util.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "components/user_manager/known_user.h"
-#endif
 
 AccountId AccountIdFromAccountInfo(const CoreAccountInfo& account_info) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  return user_manager::known_user::GetAccountId(
-      account_info.email, account_info.gaia, AccountType::GOOGLE);
+  return AccountId::FromNonCanonicalEmail(account_info.email, account_info.gaia,
+                                          AccountType::GOOGLE);
 #else
   if (account_info.email.empty() || account_info.gaia.empty())
     return EmptyAccountId();
