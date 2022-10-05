345dcb0c34e246929fff635e0a1ca56a856e2c5b
Hans Wennborg <hans@chromium.org>
2022-10-05 11:31:02 +0000

    Roll clang llvmorg-16-init-6457-g20a269cf-1 : llvmorg-16-init-6578-g0d30e92f-1
    Ran `tools/clang/scripts/upload_revision.py 0d30e92f59589de44a185c53671b7fe2e83cd2ae`.
    
    Bug: 1370060
    Change-Id: I544954f39dc19dea37cf7541c7ec97b84d035716
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst
    Cq-Include-Trybots: chromium/try:win-asan,win7-rel
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933048
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055218}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,7 +35,7 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-16-init-6457-g20a269cf'
+CLANG_REVISION = 'llvmorg-16-init-6578-g0d30e92f'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
