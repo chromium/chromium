39f25ad8c43920833d40ca50eb9c07e9c0a935bc
David Bertoni <dbertoni@chromium.org>
2022-10-05 15:34:46 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /chrome/browser/ash/arc/accessibility.
    This CL was uploaded by git cl split.
    
    R=aleventhal@chromium.org
    
    Bug: 1303949
    Change-Id: Ib45bd67f534d7401b642dcab52169fd649adf229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3932531
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1055300}

--- chrome/browser/ash/arc/accessibility/arc_accessibility_helper_bridge_unittest.cc
+++ chrome/browser/ash/arc/accessibility/arc_accessibility_helper_bridge_unittest.cc
@@ -234,8 +234,8 @@ TEST_F(ArcAccessibilityHelperBridgeTest, AnnouncementEvent) {
 
   ASSERT_EQ(1, helper_bridge->GetEventCount(event_name));
   ASSERT_EQ(event_name, helper_bridge->last_event->event_name);
-  base::Value::ConstListView arg =
-      helper_bridge->last_event->event_args[0].GetListDeprecated();
+  const base::Value::List& arg =
+      helper_bridge->last_event->event_args[0].GetList();
   ASSERT_EQ(1U, arg.size());
   ASSERT_EQ(announce_text, arg[0].GetString());
 }
@@ -264,8 +264,8 @@ TEST_F(ArcAccessibilityHelperBridgeTest, NotificationStateChangedEvent) {
 
   ASSERT_EQ(1, helper_bridge->GetEventCount(event_name));
   ASSERT_EQ(event_name, helper_bridge->last_event->event_name);
-  base::Value::ConstListView arg =
-      helper_bridge->last_event->event_args[0].GetListDeprecated();
+  const base::Value::List& arg =
+      helper_bridge->last_event->event_args[0].GetList();
   ASSERT_EQ(1U, arg.size());
   ASSERT_EQ(toast_text, arg[0].GetString());
 
