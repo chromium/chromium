505076bcaae379da9852d769e239b0ddf19263dc
Avi Drissman <avi@chromium.org>
2022-10-06 21:15:30 +0000

    Update copyright headers in third_party/l*/
    These files are used to import new versions of third-party code, to
    integrate that code into Chromium, or are otherwise not from upstream.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Try: true
    Bug: 1098010
    Change-Id: I7c39eac60420c4c5d3968d723836fc6477eb5bfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937444
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056007}

--- third_party/leveldatabase/leveldb_unittest_main.cc
+++ third_party/leveldatabase/leveldb_unittest_main.cc
@@ -1,7 +1,6 @@
-
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
+// found in the LICENSE file.
 
 #include "base/command_line.h"
 #include "base/test/task_environment.h"
