4a91258482e24818a44a4c9268e53068ae6dc88f
Jack Yammine <jyammine@google.com>
2022-10-07 08:30:35 +0000

    [fyfre]Remove hardcoded strings
    Replace hard coded strings by ones that can be
    identified and used by the translator.
    
    
    Bug: 1347507
    Change-Id: I2ebc36248b61a1ebb4f93ec18120ef9dce40d18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3930597
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056190}

--- chrome/browser/ui/webui/intro/intro_ui.cc
+++ chrome/browser/ui/webui/intro/intro_ui.cc
@@ -11,10 +11,37 @@
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/intro_resources.h"
 #include "chrome/grit/intro_resources_map.h"
+#include "components/signin/public/base/signin_buildflags.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-namespace {}  // namespace
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+#include "chrome/grit/chromium_strings.h"
+#include "chrome/grit/generated_resources.h"
+#endif
+
+namespace {
+
+void AddStrings(content::WebUIDataSource* html_source) {
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  static constexpr webui::LocalizedString kLocalizedStrings[] = {
+      {"pageTitle", IDS_FRE_SIGN_IN_TITLE},
+      {"pageSubtitle", IDS_FRE_SIGN_IN_SUBTITLE},
+      {"devicesCardTitle", IDS_FRE_DEVICES_CARD_TITLE},
+      {"devicesCardDescription", IDS_FRE_DEVICES_CARD_DESCRIPTION},
+      {"securityCardTitle", IDS_FRE_SECURITY_CARD_TITLE},
+      {"securityCardDescription", IDS_FRE_SECURITY_CARD_DESCRIPTION},
+      {"backupCardTitle", IDS_FRE_BACKUP_CARD_TITLE},
+      {"backupCardDescription", IDS_FRE_BACKUP_CARD_DESCRIPTION},
+      {"declineSignInButtonTitle", IDS_FRE_DECLINE_SIGN_IN_BUTTON_TITLE},
+      {"acceptSignInButtonTitle", IDS_FRE_ACCEPT_SIGN_IN_BUTTON_TITLE},
+  };
+
+  html_source->AddLocalizedStrings(kLocalizedStrings);
+#endif
+}
+
+}  // namespace
 
 IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
   DCHECK(base::FeatureList::IsEnabled(kForYouFre));
@@ -27,6 +54,8 @@ IntroUI::IntroUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
   webui::SetupWebUIDataSource(
       source, base::make_span(kIntroResources, kIntroResourcesSize),
       IDR_INTRO_INTRO_HTML);
+
+  AddStrings(source);
 }
 
 IntroUI::~IntroUI() = default;
