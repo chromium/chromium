2190488addeeb3f33b43bf78324ab6e6ef90f495
Xinghui Lu <xinghuilu@chromium.org>
2022-10-07 22:31:49 +0000

    [M107] Reset the profile pointer in PreferenceValidationDelegate before the profile is destroyed.
    (cherry picked from commit 57d96518a0ab600d75b118fbeec2d7a3f526ed2a)
    
    Bug: 1364662
    Change-Id: I24678c1f5030474c9193a5552463da6ffbdec6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929491
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1054911}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939997
    Auto-Submit: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5304@{#533}
    Cr-Branched-From: 5d7b1fc9cb7103d9c82eed647cf4be38cf09738b-refs/heads/main@{#1047731}

--- chrome/browser/safe_browsing/incident_reporting/preference_validation_delegate.h
+++ chrome/browser/safe_browsing/incident_reporting/preference_validation_delegate.h
@@ -8,11 +8,12 @@
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/values.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_observer.h"
 #include "services/preferences/public/mojom/tracked_preference_validation_delegate.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-
-class Profile;
 
 namespace safe_browsing {
 
@@ -22,7 +23,8 @@ class IncidentReceiver;
 // for preference validation failures. The profile for which the delegate
 // operates must outlive the delegate itself.
 class PreferenceValidationDelegate
-    : public prefs::mojom::TrackedPreferenceValidationDelegate {
+    : public prefs::mojom::TrackedPreferenceValidationDelegate,
+      public ProfileObserver {
  public:
   PreferenceValidationDelegate(
       Profile* profile,
@@ -52,8 +54,13 @@ class PreferenceValidationDelegate
           external_validation_value_state,
       bool is_personal) override;
 
+  // ProfileManagerObserver methods:
+  void OnProfileWillBeDestroyed(Profile* profile) override;
+
   raw_ptr<Profile> profile_;
   std::unique_ptr<IncidentReceiver> incident_receiver_;
+
+  base::ScopedObservation<Profile, ProfileObserver> profile_observation_{this};
 };
 
 }  // namespace safe_browsing
