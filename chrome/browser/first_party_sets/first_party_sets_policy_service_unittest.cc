88e1d54f86ea6daaf4cda1137c39003db7c9af5e
Chris Fredrickson <cfredric@chromium.org>
2022-10-07 23:07:44 +0000

    Fix unreliable test setup.
    The tests didn't reset the state of the FirstPartySetsHandler instance,
    which may depend on the kFirstPartySets feature.
    
    Bug: 1372526
    Change-Id: Iba0a336b34907511aabf45600c722f617943c555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938319
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056561}

--- chrome/browser/first_party_sets/first_party_sets_policy_service_unittest.cc
+++ chrome/browser/first_party_sets/first_party_sets_policy_service_unittest.cc
@@ -11,6 +11,7 @@
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
+#include "content/public/browser/first_party_sets_handler.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "net/base/schemeful_site.h"
@@ -48,6 +49,7 @@ class FirstPartySetsPolicyServiceTest : public testing::Test {
   }
 
   void SetUp() override {
+    content::FirstPartySetsHandler::GetInstance()->ResetForTesting();
     mock_delegate_receiver_.Bind(
         mock_delegate_remote_.BindNewPipeAndPassReceiver());
 
