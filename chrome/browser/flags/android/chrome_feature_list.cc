984cdf73fa0b5966d0d183d81b88634a64fb6360
Alex Mitra <alexmitra@chromium.org>
2022-10-07 13:27:29 +0000

    Turns CCTNewDownloadTab on by default.
    Also removes the fieldtrial_testing_config.
    launch/4201038
    
    Change-Id: I77c4dc8bbd95abb557061fa674770229e1d2852c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936846
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056266}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -562,7 +562,7 @@ BASE_FEATURE(kCCTClientDataHeader,
 
 BASE_FEATURE(kCCTNewDownloadTab,
              "CCTNewDownloadTab",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTIncognito, "CCTIncognito", base::FEATURE_ENABLED_BY_DEFAULT);
 
