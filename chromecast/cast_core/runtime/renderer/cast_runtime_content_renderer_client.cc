ed2ae87b412008e5d52766ab1c8bbc96203f052b
Vigen Issahhanjan <vigeni@google.com>
2022-10-08 02:00:48 +0000

    Use explicit renderer:simple_client where needed.
    Bug: none
    Test: build
    Change-Id: I90b6180f24687e6469b7cd434a7f2770bb35655d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3935280
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1056610}

--- chromecast/cast_core/runtime/renderer/cast_runtime_content_renderer_client.cc
+++ chromecast/cast_core/runtime/renderer/cast_runtime_content_renderer_client.cc
@@ -4,29 +4,14 @@
 
 #include "chromecast/cast_core/runtime/renderer/cast_runtime_content_renderer_client.h"
 
-#include "base/bind.h"
-#include "chromecast/cast_core/runtime/common/cors_exempt_headers.h"
-#include "chromecast/renderer/cast_url_loader_throttle_provider.h"
 #include "components/cast_streaming/renderer/public/resource_provider.h"
 #include "components/cast_streaming/renderer/public/resource_provider_factory.h"
-#include "content/public/renderer/render_frame.h"
-#include "content/public/renderer/render_frame_media_playback_options.h"
-#include "media/base/demuxer.h"
-#include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
 
 namespace chromecast {
 
 CastRuntimeContentRendererClient::CastRuntimeContentRendererClient() = default;
 
 CastRuntimeContentRendererClient::~CastRuntimeContentRendererClient() = default;
-
-std::unique_ptr<blink::URLLoaderThrottleProvider>
-CastRuntimeContentRendererClient::CreateURLLoaderThrottleProvider(
-    blink::URLLoaderThrottleProviderType type) {
-  return std::make_unique<CastURLLoaderThrottleProvider>(
-      type, /*url_filter_manager=*/nullptr, this,
-      base::BindRepeating(&IsHeaderCorsExempt));
-}
 
 std::unique_ptr<cast_streaming::ResourceProvider>
 CastRuntimeContentRendererClient::CreateCastStreamingResourceProvider() {
--- chromecast/cast_core/runtime/renderer/cast_runtime_content_renderer_client.h
+++ chromecast/cast_core/runtime/renderer/cast_runtime_content_renderer_client.h
@@ -7,9 +7,7 @@
 
 #include <memory>
 
-#include "base/containers/flat_map.h"
 #include "chromecast/renderer/cast_content_renderer_client.h"
-#include "chromecast/renderer/url_rewrite_rules_provider.h"
 
 namespace cast_streaming {
 class ResourceProvider;
@@ -32,9 +30,6 @@ class CastRuntimeContentRendererClient
       CastRuntimeContentRendererClient&&) = delete;
 
   // content::ContentRendererClient overrides.
-  std::unique_ptr<blink::URLLoaderThrottleProvider>
-  CreateURLLoaderThrottleProvider(
-      blink::URLLoaderThrottleProviderType type) override;
   std::unique_ptr<cast_streaming::ResourceProvider>
   CreateCastStreamingResourceProvider() override;
 
