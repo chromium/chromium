b06ae618c9e26862015829df300c49f14b0b9d0c
Avi Drissman <avi@chromium.org>
2022-10-08 19:24:31 +0000

    Update copyright headers in third_party/b*/, third_party/c*/, and third_party/d*/
    These files are used to import new versions of third-party code, to
    integrate that code into Chromium, or are otherwise not from upstream.
    
    The methodology used to generate this CL is documented in
    https://crbug.com/1098010#c95.
    
    No-Presubmit: true
    Bug: 1098010
    Change-Id: I53a789c94c11b10774bb987ca8f796a879a0044a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936784
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056701}

--- third_party/crashpad/update.py
+++ third_party/crashpad/update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# Copyright 2015 The Chromium Authors. All rights reserved.
+# Copyright 2015 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
--- third_party/distributed_point_functions/glog/logging.h
+++ third_party/distributed_point_functions/glog/logging.h
@@ -1,6 +1,6 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found
-// in the LICENSE file.
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef THIRD_PARTY_DISTRIBUTED_POINT_FUNCTIONS_GLOG_LOGGING_H_
 #define THIRD_PARTY_DISTRIBUTED_POINT_FUNCTIONS_GLOG_LOGGING_H_
