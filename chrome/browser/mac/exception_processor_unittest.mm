b893c27817d854f4c50e2a75854bfe82adf7c55a
Leonard Grey <lgrey@chromium.org>
2022-10-10 13:57:04 +0000

    Mac: disable ExceptionProcessorTest.ThrowExceptionInRunLoopWithoutProcessor under LSAN
    The exception makes us leak the runloop, and leaking the runloop makes the death due to LSAN, so we get the wrong exit code.
    
    Bug: 1320449
    Change-Id: I10117edd633ee669b032af4f9638e956e08adea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939806
    Auto-Submit: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056928}

--- chrome/browser/mac/exception_processor_unittest.mm
+++ chrome/browser/mac/exception_processor_unittest.mm
@@ -210,8 +210,17 @@ void ThrowExceptionInRunLoopWithoutProcessor() {
   exit(1);
 }
 
+// Under LSAN this dies from leaking the run loop instead of how we expect it to
+// die, so the exit code is wrong.
+#if defined(LEAK_SANITIZER)
+#define MAYBE_ThrowExceptionInRunLoopWithoutProcessor \
+  DISABLED_ThrowExceptionInRunLoopWithoutProcessor
+#else
+#define MAYBE_ThrowExceptionInRunLoopWithoutProcessor \
+  ThrowExceptionInRunLoopWithoutProcessor
+#endif
 // Tests basic exception handling when the preprocessor is disabled.
-TEST(ExceptionProcessorTest, ThrowExceptionInRunLoopWithoutProcessor) {
+TEST(ExceptionProcessorTest, MAYBE_ThrowExceptionInRunLoopWithoutProcessor) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   EXPECT_EXIT(ThrowExceptionInRunLoopWithoutProcessor(),
               [](int exit_code) -> bool {
