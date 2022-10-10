223caec2c142f4f0a7bb471516048a9a461abc20
Maxim Anufriev <maxan@google.com>
2022-10-10 18:52:14 +0000

    [M107][UPM][Android] Add flag overriding the UPM error handling
    This CL overrides the default behaviour of the password manager GMS Core
    backend with the values that are expected to be rolled out to all users
    in the long term.
    
    This includes:
    - Adding resolvable and unresolvable errors to ignored errors list. With
      active `google-mobile-services-passwords-error-messages` this will
      result in not evicting users on authentication errors.
    
    - Enabling fallback to the local backend on errors for user-affecting
      read operations.
    
    (cherry picked from commit 60f17aaa25b333c48e31fdfe200560563fe4bbaf)
    
    Bug: 1369651,1362464
    Change-Id: I7705d0ed87563d0b1a2645c0a0a2991e04735458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928985
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1056389}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941640
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Maxim Anufriev <maxan@google.com>
    Auto-Submit: Maxim Anufriev <maxan@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5304@{#601}
    Cr-Branched-From: 5d7b1fc9cb7103d9c82eed647cf4be38cf09738b-refs/heads/main@{#1047731}

--- components/password_manager/core/common/password_manager_features.h
+++ components/password_manager/core/common/password_manager_features.h
@@ -102,13 +102,52 @@ constexpr base::FeatureParam<bool> kPasswordChangeLiveExperimentParam = {
 
 #if BUILDFLAG(IS_ANDROID)
 extern const base::FeatureParam<int> kMigrationVersion;
-extern const base::FeatureParam<int> kGmsApiErrorListVersion;
-extern const base::FeatureParam<std::string> kIgnoredGmsApiErrors;
-extern const base::FeatureParam<std::string> kRetriableGmsApiErrors;
-extern const base::FeatureParam<bool> kFallbackOnModifyingOperations;
-extern const base::FeatureParam<bool> kFallbackOnRemoveOperations;
-extern const base::FeatureParam<bool> kFallbackOnUserAffectingReadOperations;
-extern const base::FeatureParam<bool> kFallbackOnNonUserAffectingReadOperations;
+
+// Current version of the GMS Core API errors lists. Users save this value on
+// eviction due to error and will only be re-enrolled to the experiment if the
+// configured version is greater than the saved one.
+constexpr base::FeatureParam<int> kGmsApiErrorListVersion = {
+    &kUnifiedPasswordManagerAndroid, "api_error_list_version", 0};
+
+// Current list of the GMS Core API error codes that should be ignored and not
+// result in user eviction.
+// Codes DEVELOPER_ERROR=10, BAD_REQUEST=11008 are ignored to keep the default
+// pre-M107 behaviour.
+constexpr base::FeatureParam<std::string> kIgnoredGmsApiErrors = {
+    &kUnifiedPasswordManagerAndroid, "ignored_api_errors", "10,11008"};
+
+// Current list of the GMS Core API error codes considered retriable.
+// User could still be evicted if retries do not resolve the error.
+constexpr base::FeatureParam<std::string> kRetriableGmsApiErrors = {
+    &kUnifiedPasswordManagerAndroid, "retriable_api_errors", ""};
+
+// Enables fallback to the Chrome built-in backend if the operation executed on
+// the GMS Core backend returns with error. Errors listed in the
+// |kIgnoredGmsApiErrors| will not fallback and will be directly returned to the
+// caller to be addressed in a specific way.
+
+// Fallback on AddLogin and UpdateLogin operations. This is default behaviour
+// since M103.
+constexpr base::FeatureParam<bool> kFallbackOnModifyingOperations = {
+    &kUnifiedPasswordManagerAndroid, "fallback_on_modifying_operations", true};
+
+// Fallback on RemoveLogin* operations.
+constexpr base::FeatureParam<bool> kFallbackOnRemoveOperations = {
+    &kUnifiedPasswordManagerAndroid, "fallback_on_remove_operations", false};
+
+// Fallback on FillMatchingLogins which is needed to perform autofill and could
+// affect user experience.
+constexpr base::FeatureParam<bool> kFallbackOnUserAffectingReadOperations = {
+    &kUnifiedPasswordManagerAndroid,
+    "fallback_on_user_affecting_read_operations", false};
+
+// Fallback on GetAllLogins* and GetAutofillableLogins operations which are
+// needed for certain features (e.g. PhishGuard) but do not affect the core
+// experience.
+constexpr base::FeatureParam<bool> kFallbackOnNonUserAffectingReadOperations = {
+    &kUnifiedPasswordManagerAndroid,
+    "fallback_on_non_user_affecting_read_operations", false};
+
 constexpr base::FeatureParam<UpmExperimentVariation>::Option
     kUpmExperimentVariationOption[] = {
         {UpmExperimentVariation::kEnableForSyncingUsers, "0"},
