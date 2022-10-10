01efcb4698b6f5803db8560e0e4c536027af7008
Rune Lillesveen <futhark@chromium.org>
2022-10-10 11:53:46 +0000

    Revert "Reland "Speculatively re-enable WebViewInteractiveTest.KeyboardFocusWindowCycle""
    This reverts commit 0afa56a3deac3b53db616d7391ed1ab628fe8894.
    
    Reason for revert: still flaky on multiple platforms (CROS, Mac, Win)
    
    Original change's description:
    > Reland "Speculatively re-enable WebViewInteractiveTest.KeyboardFocusWindowCycle"
    >
    > This is a reland of commit d58e9a89c36ddfa0fe1888521218edb7f79daed0
    >
    > This CL re-enables the test for all platforms except Linux, which
    > is still flaky.
    >
    > Original change's description:
    > > Speculatively re-enable WebViewInteractiveTest.KeyboardFocusWindowCycle
    > >
    > > When tested locally with --gtest_repeat=50, this exhibited no flakes.
    > >
    > > Bug: 817067,706830,847201
    > > Change-Id: I8feb670a6fda16361a8a176fb78bb7030355e847
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925734
    > > Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    > > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1052572}
    >
    > Bug: 817067,706830,847201
    > Change-Id: Id45ee9433543eec8137eeaa8c95eaea5a0591a97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3929979
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1054849}
    
    Bug: 817067,706830,847201
    Change-Id: I926b842a29d275c93703cfca9117ddcaffc4918a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3941686
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Abhijeet Kandalkar <abhijeet@igalia.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1056895}

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -1410,8 +1410,11 @@ IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, MAYBE_KeyboardFocusSimple) {
 // and regains focus. Additionally, the webview does not process keypresses sent
 // while another window is focused.
 // http://crbug.com/660044.
+// Flaky on MacOSX, crbug.com/817067.
 // Flaky on linux, crbug.com/706830.
-#if BUILDFLAG(IS_LINUX)
+// Flaky on Windows, crbug.com/847201.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC) || \
+    BUILDFLAG(IS_WIN)
 #define MAYBE_KeyboardFocusWindowCycle DISABLED_KeyboardFocusWindowCycle
 #else
 #define MAYBE_KeyboardFocusWindowCycle KeyboardFocusWindowCycle
