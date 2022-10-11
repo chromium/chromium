a24dc7dc089193d058ed8a65fb3da59580ace7e5
Peter Boström <pbos@chromium.org>
2022-10-11 23:48:20 +0000

    Reland "Add CHECK to CRT InvalidParameters handler"
    This reverts commit b1a44ed724faefa429d4361a6f917df74357235f.
    
    Reason for revert: Attempt to reland with IWYU fixes.
    
    Original change's description:
    > Revert "Add CHECK to CRT InvalidParameters handler"
    >
    > This reverts commit b7dfa804995768ec0f8eab7b717692fad4798541.
    >
    > Reason for revert: This reverts commit b7dfa804995768ec0f8eab7b717692fad4798541.
    >
    > Reason for revert: Breaks compilation on win-swangle-x64 bot: https://ci.chromium.org/ui/p/chromium/builders/ci/win-swangle-x64/67325/overview
    > Original change's description:
    > > Add CHECK to CRT InvalidParameters handler
    > >
    > > This should let us log more info when invalid CRT calls are made.
    > >
    > > Bug: 1372309
    > > Change-Id: I5064777b12dd97b97d8f12e2d0cbd9dbfab6b25a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3939128
    > > Reviewed-by: Greg Thompson <grt@chromium.org>
    > > Reviewed-by: Robert Liao <robliao@chromium.org>
    > > Commit-Queue: Peter Boström <pbos@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1057529}
    >
    > Bug: 1372309
    > Change-Id: Ifdb8f3155da861fe8d4f6b989e68d638310f5b2e
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946781
    > Auto-Submit: Jonathan Ross <jonross@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Jonathan Ross <jonross@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1057558}
    
    Bug: 1372309
    Cq-Include-Trybots: luci.chromium.try:win-swangle-try-x64
    Change-Id: I448c43c96ccba1ed4b266e4a66c5860a57982532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945954
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057780}

--- base/win/process_startup_helper.cc
+++ base/win/process_startup_helper.cc
@@ -7,13 +7,19 @@
 #include <crtdbg.h>
 #include <new.h>
 
+#include <ostream>
+#include <string>
+
 #include "base/base_switches.h"
+#include "base/check.h"
 #include "base/command_line.h"
+#include "base/strings/utf_string_conversions.h"
 
 namespace {
 
-// Handlers for invalid parameter and pure call. They generate a breakpoint to
-// tell breakpad that it needs to dump the process.
+// Handlers for invalid parameter and pure call. They generate an unoptimized
+// CHECK() unconditionally to always generate file+line error messages even in
+// official builds.
 // These functions should be written to be unique in order to avoid confusing
 // call stacks from /OPT:ICF function folding. Printing a unique message or
 // returning a unique value will do this. Note that for best results they need
@@ -23,13 +29,20 @@ void InvalidParameter(const wchar_t* expression,
                       const wchar_t* file,
                       unsigned int line,
                       uintptr_t reserved) {
-  __debugbreak();
+  logging::CheckError::Check(base::WideToUTF8(file).c_str(),
+                             static_cast<int>(line),
+                             base::WideToUTF8(expression).c_str())
+          .stream()
+      << base::WideToUTF8(function);
   // Use a different exit code from PureCall to avoid COMDAT folding.
   _exit(1);
 }
 
 void PureCall() {
-  __debugbreak();
+  // This inlines a CHECK(false) that won't be optimized to IMMEDIATE_CRASH() in
+  // an official build and hence will set a crash key for file:line for better
+  // error reporting.
+  logging::CheckError::Check(__FILE__, __LINE__, "false");
   // Use a different exit code from InvalidParameter to avoid COMDAT folding.
   _exit(2);
 }
