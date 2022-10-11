709e680b357ff7bf9d49695630e819afafa98efd
Anna Sato <annasato@google.com>
2022-10-11 12:38:50 +0000

    Introduce template class to bind crosapi
    In each crosapi test, we need to bind the crosapi we use.
    The function |BindCrosapiInterface()| provides an easy way to bind it.
    
    It will be used only in ash crosapi test.
    
    Bug: 1357441
    Change-Id: Icc91f77ed4ccec77c3e91c395fc2d3c969dcbb68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924660
    Commit-Queue: Anna Sato <annasato@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057411}

--- chrome/browser/ash/crosapi/test/crosapi_test_base.cc
+++ chrome/browser/ash/crosapi/test/crosapi_test_base.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,4 @@ void CrosapiTestBase::SetUp() {
   ASSERT_TRUE(AshCrosapiTestEnv::GetInstance()->IsValid());
 }
 
-mojo::Remote<mojom::Crosapi>& CrosapiTestBase::GetCrosapiRemote() {
-  return AshCrosapiTestEnv::GetInstance()->GetCrosapiRemote();
-}
 }  // namespace crosapi
--- chrome/browser/ash/crosapi/test/crosapi_test_base.h
+++ chrome/browser/ash/crosapi/test/crosapi_test_base.h
@@ -1,10 +1,11 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CHROME_BROWSER_ASH_CROSAPI_TEST_CROSAPI_TEST_BASE_H_
 #define CHROME_BROWSER_ASH_CROSAPI_TEST_CROSAPI_TEST_BASE_H_
 
+#include "chrome/browser/ash/crosapi/test/ash_crosapi_tests_env.h"
 #include "chromeos/crosapi/mojom/crosapi.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -15,14 +16,30 @@ namespace crosapi {
 class CrosapiTestBase : public ::testing::Test {
  public:
   CrosapiTestBase();
-  ~CrosapiTestBase() override;
   CrosapiTestBase(const CrosapiTestBase&) = delete;
   CrosapiTestBase& operator=(const CrosapiTestBase&) = delete;
+  ~CrosapiTestBase() override;
 
   void SetUp() override;
 
  protected:
-  mojo::Remote<mojom::Crosapi>& GetCrosapiRemote();
+  // This function binds a remote for a given CrosapiInterface.
+  // For example:
+  //
+  //   auto file_manager =
+  //       BindCrosapiInterface(&mojom::Crosapi::BindFileManager);
+  //
+  //   file_manager->OpenFile(filepath, &result);
+  //   EXPECT_EQ(crosapi::mojom::OpenResult::kFailedPathNotFound, result);
+  //
+  template <typename CrosapiInterface>
+  mojo::Remote<CrosapiInterface> BindCrosapiInterface(void (
+      mojom::Crosapi::*bind_func)(mojo::PendingReceiver<CrosapiInterface>)) {
+    mojo::Remote<CrosapiInterface> remote;
+    (AshCrosapiTestEnv::GetInstance()->GetCrosapiRemote().get()->*bind_func)(
+        remote.BindNewPipeAndPassReceiver());
+    return std::move(remote);
+  }
 };
 
 }  // namespace crosapi
