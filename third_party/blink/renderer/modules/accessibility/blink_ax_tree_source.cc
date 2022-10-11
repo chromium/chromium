5f0881fb76efc919867c8dba2ddc6fc839a423c4
Chris Harrelson <chrishtr@chromium.org>
2022-10-11 20:49:24 +0000

    Add more detached & null checks in BlinkAXTreeSource
    Some of these were missing in the original commit that created this
    class.
    
    Also added a DCHECK(frozen_) in GetTreeData.
    
    (cherry picked from commit 1a741a826f5d8b8e65345c6f2ac8a06bf3c8a75a)
    
    Change-Id: I12ee2c6e947251944996c1f326412e61df33f8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3942982
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1057513}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946460
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5304@{#658}
    Cr-Branched-From: 5d7b1fc9cb7103d9c82eed647cf4be38cf09738b-refs/heads/main@{#1047731}

--- third_party/blink/renderer/modules/accessibility/blink_ax_tree_source.cc
+++ third_party/blink/renderer/modules/accessibility/blink_ax_tree_source.cc
@@ -76,10 +76,10 @@ bool BlinkAXTreeSource::ShouldLoadInlineTextBoxes(const AXObject* obj) const {
 
   AXObject* focused_object = GetFocusedObject();
   AXID focus_id = -1;
-  if (focused_object)
+  if (focused_object && !focused_object->IsDetached())
     focus_id = focused_object->AXObjectID();
   const AXObject* ancestor = obj;
-  while (ancestor) {
+  while (ancestor && !ancestor->IsDetached()) {
     AXID ancestor_id = ancestor->AXObjectID();
     if (load_inline_text_boxes_ids_.Contains(ancestor_id) ||
         (ancestor_id == focus_id && ancestor->IsEditable())) {
@@ -136,11 +136,11 @@ void BlinkAXTreeSource::Selection(
   focus_offset = -1;
   focus_affinity = ax::mojom::blink::TextAffinity::kDownstream;
 
-  if (!obj)
+  if (!obj || obj->IsDetached())
     return;
 
   AXObject* focus = GetFocusedObject();
-  if (!focus)
+  if (!focus || focus->IsDetached())
     return;
 
   const auto ax_selection =
@@ -180,6 +180,7 @@ static ui::AXTreeID GetAXTreeID(LocalFrame* local_frame) {
 }
 
 bool BlinkAXTreeSource::GetTreeData(ui::AXTreeData* tree_data) const {
+  CHECK(frozen_);
   AXObject* root = GetRoot();
   tree_data->doctype = "html";
   tree_data->loaded = root->IsLoaded();
@@ -348,6 +349,8 @@ AXObject* BlinkAXTreeSource::GetParent(AXObject* node) const {
 }
 
 bool BlinkAXTreeSource::IsIgnored(AXObject* node) const {
+  if (!node || node->IsDetached())
+    return false;
   return node->AccessibilityIsIgnored();
 }
 
@@ -364,6 +367,8 @@ AXObject* BlinkAXTreeSource::GetNull() const {
 }
 
 std::string BlinkAXTreeSource::GetDebugString(AXObject* node) const {
+  if (!node || node->IsDetached())
+    return "";
   return node->ToString(true).Utf8();
 }
 
@@ -380,14 +385,16 @@ void BlinkAXTreeSource::SerializeNode(AXObject* src,
       ax_object_cache_->GetDocument().Lifecycle());
 #endif
 
-  dst->id = src->AXObjectID();
-  dst->role = src->RoleValue();
-
   if (!src || src->IsDetached() || !src->AccessibilityIsIncludedInTree()) {
     dst->AddState(ax::mojom::blink::State::kIgnored);
+    dst->id = -1;
+    dst->role = ax::mojom::blink::Role::kUnknown;
     NOTREACHED();
     return;
   }
+
+  dst->id = src->AXObjectID();
+  dst->role = src->RoleValue();
 
   // TODO(crbug.com/1068668): AX onion soup - finish migrating the rest of
   // this function inside of AXObject::Serialize and removing
