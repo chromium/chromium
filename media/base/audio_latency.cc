1c467f518c451e781704404b2932a2c798cbef39
Sergey Ulanov <sergeyu@chromium.org>
2022-10-11 18:33:37 +0000

    Reland "[Fuchsia] Increase buffer size for audio playback"
    This is a reland of commit c11b7d9c38e0a6cf64351eafdbee078e4de1c5e8
    
    Original change's description:
    > [Fuchsia] Increase buffer size for audio playback
    >
    > Previously audio streams were always using 10ms buffer size, which is
    > suboptimal for streams that don't need low latency. Updated
    > AudioLatency::GetHighLatencyBufferSize() to use 80ms buffers on Fuchsia
    > (consistent with ChromeOS) and updated AudioManagerFuchsia to allow
    > any buffer size.
    > Also updated IsResamplingPassthroughSupported() to return true on
    > Fuchsia - that allows to avoid resampling audio streams to 48kHz.
    >
    > Bug: 1369227
    > Change-Id: I3fe563a6de29bc941b7a70632e7ce20ba2326c71
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3924048
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1056384}
    
    Bug: 1369227
    Change-Id: Ibecc84082ec0fbfacf0866f771724e9a64c51ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3938454
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057568}

--- media/base/audio_latency.cc
+++ media/base/audio_latency.cc
@@ -24,11 +24,15 @@
 #include "media/base/mac/audio_latency_mac.h"
 #endif
 
+#if BUILDFLAG(IS_FUCHSIA)
+#include "base/fuchsia/scheduler.h"
+#endif
+
 namespace media {
 
 namespace {
 
-#if !BUILDFLAG(IS_WIN)
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_FUCHSIA)
 // Taken from "Bit Twiddling Hacks"
 // http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
 uint32_t RoundUpToPowerOfTwo(uint32_t v) {
@@ -67,6 +71,8 @@ int LCM(int a, int b) {
 bool AudioLatency::IsResamplingPassthroughSupported(LatencyType type) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   return true;
+#elif BUILDFLAG(IS_FUCHSIA)
+  return true;
 #elif BUILDFLAG(IS_ANDROID)
   // Only N MR1+ has support for OpenSLES performance modes which allow for
   // power efficient playback. Per the Android audio team, we shouldn't waste
@@ -83,12 +89,20 @@ bool AudioLatency::IsResamplingPassthroughSupported(LatencyType type) {
 // static
 int AudioLatency::GetHighLatencyBufferSize(int sample_rate,
                                            int preferred_buffer_size) {
-  // Empirically, we consider 20ms of samples to be high latency.
-#if !defined(USE_CRAS)
+#if defined(USE_CRAS)
+  // Use 80ms rounded to a power of 2.
+  const double eighty_ms_size = 8.0 * sample_rate / 100;
+  const int high_latency_buffer_size = RoundUpToPowerOfTwo(eighty_ms_size);
+#elif BUILDFLAG(IS_FUCHSIA)
+  // Use 80ms buffers. Doesn't need to be aligned to power of 2, but it should
+  // be a multiple of the scheduling period used for audio threads.
+  constexpr base::TimeDelta period = base::Milliseconds(80);
+  static_assert(static_cast<int>(period / base::kAudioSchedulingPeriod) ==
+                period / base::kAudioSchedulingPeriod);
+  const int high_latency_buffer_size = period.InMilliseconds() * sample_rate /
+                                       base::Time::kMillisecondsPerSecond;
+#elif BUILDFLAG(IS_WIN)
   const double twenty_ms_size = 2.0 * sample_rate / 100;
-#endif
-
-#if BUILDFLAG(IS_WIN)
   preferred_buffer_size = std::max(preferred_buffer_size, 1);
 
   // Windows doesn't use power of two buffer sizes, so we should always round up
@@ -109,13 +123,9 @@ int AudioLatency::GetHighLatencyBufferSize(int sample_rate,
   //
   // On Linux, the minimum hardware buffer size is 512, so the lower calculated
   // values are unused.  OSX may have a value as low as 128.
-#if defined(USE_CRAS)
-  const double eighty_ms_size = 8.0 * sample_rate / 100;
-  const int high_latency_buffer_size = RoundUpToPowerOfTwo(eighty_ms_size);
-#else
+  const double twenty_ms_size = 2.0 * sample_rate / 100;
   const int high_latency_buffer_size = RoundUpToPowerOfTwo(twenty_ms_size);
-#endif  // defined(USE_CRAS)
-#endif  // BUILDFLAG(IS_WIN)
+#endif
 
   return std::max(preferred_buffer_size, high_latency_buffer_size);
 }
