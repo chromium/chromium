379729fc1fa6e077fbce78972f19ae2f2b9480be
Andrew Grieve <agrieve@chromium.org>
2022-10-11 17:34:25 +0000

    Android: Catch UndeclaredThrowableException when calling grantUriPermission
    It looks like in Android R & S, the SecurityException is an
    UndeclaredThrowableException by the time it reaches our try/catch.
    
    Bug: 1373209
    Change-Id: I7b40309c55faa733788499b0426459c9a7f05dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946098
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057539}

--- chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
@@ -313,11 +313,13 @@ public class LaunchIntentDispatcher implements IntentHandler.IntentHandlerDelega
         // achieved by simply adding the FLAG_GRANT_READ_URI_PERMISSION to the Intent, since the
         // data URI on the Intent isn't |uri|, it just has |uri| as a query parameter.
         if (uri != null && UrlConstants.CONTENT_SCHEME.equals(uri.getScheme())) {
+            String packageName = context.getPackageName();
             try {
-                context.grantUriPermission(
-                        context.getPackageName(), uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            } catch (SecurityException e) {
-                Log.w(TAG, "Unable to grant Uri permission. ", e);
+                context.grantUriPermission(packageName, uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            } catch (Exception e) {
+                // SecurityException or UndeclaredThrowableException.
+                // https://crbug.com/1373209
+                Log.w(TAG, "Unable to grant Uri permission", e);
             }
         }
 
