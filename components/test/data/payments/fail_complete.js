65cc0803aafeb361c6a658a6fdde4a0ff052155a
Nick Burris <nburris@chromium.org>
2022-10-11 21:35:43 +0000

    [ZBB] Migrate flaky PaymentRequestFailCompleteTest
    This patch migrates a flaky-disabled Java test using basic-card to a
    cross-platform browsertest using a payment app, for improved coverage
    and stability.
    
    Bug: 1331181
    Change-Id: I7f545d29337b5678c9961b2532c609ea7e46c80d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3936786
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057709}

--- components/test/data/payments/fail_complete.js
+++ components/test/data/payments/fail_complete.js
@@ -9,38 +9,33 @@
 /**
  * Launches the PaymentRequest UI and always fails to complete the transaction.
  *
- * Legacy entry function until basic-card is removed.
- */
-function buy() { // eslint-disable-line no-unused-vars
-  buyWithMethods(
-      [{supportedMethods: 'basic-card', data: {supportedNetworks: ['visa']}}]);
-}
-
-/**
- * Launches the PaymentRequest UI and always fails to complete the transaction.
- *
  * @param {sequence<PaymentMethodData>} methodData - An array of payment method
  *        objects.
  */
-function buyWithMethods(methodData) {
+function buyWithMethods(methodData) { // eslint-disable-line no-unused-vars
+  failComplete(methodData)
+      .then((result) => print(result))
+      .catch((error) => print(error.message));
+}
+
+/**
+ * Launches the PaymentRequest UI and always fails to complete the transaction,
+ * returning 'Transaction failed' if there was no error.
+ *
+ * @param {sequence<PaymentMethodData>} methodData - An array of payment method
+ *        objects.
+ * @return {string} - 'Transaction failed' if there was no error, otherwise the
+ *        error message.
+ */
+async function failComplete(methodData) {
   try {
-    new PaymentRequest(
+    const request = new PaymentRequest(
         methodData,
-        {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}})
-        .show()
-        .then(function(resp) {
-          resp.complete('fail')
-              .then(function() {
-                print('Transaction failed');
-              })
-              .catch(function(error) {
-                print(error.message);
-              });
-        })
-        .catch(function(error) {
-          print(error.message);
-        });
+        {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}});
+    const response = await request.show();
+    await response.complete('fail');
+    return 'Transaction failed';
   } catch (error) {
-    print(error.message);
+    return error.toString();
   }
 }
