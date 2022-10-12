24dd4e298aac3360f7eddd5aed3abccc2b95faba
Rohan Pavone <rohpavone@chromium.org>
2022-10-12 19:07:16 +0000

    [perf] Never delete dict while iterating.
    This throws a RuntimeError and should be avoided. The new
    implementation makes a copy only of keys that are not 'undefined'
    or False-y.
    
    Change-Id: I6fa52f9ff743af0b4c439c8b7a0bb5e9fceed470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947493
    Commit-Queue: Rohan Pavone <rohpavone@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058204}

--- tools/perf/core/upload_results_to_perf_dashboard.py
+++ tools/perf/core/upload_results_to_perf_dashboard.py
@@ -224,7 +224,5 @@ def _GetPerfDashboardRevisionsWithProperties(
   versions['git_revision'] = git_revision
   versions['point_id'] = point_id
   # There are a lot of "bad" revisions to check for, so clean them all up here.
-  for key in versions:
-    if not versions[key] or versions[key] == 'undefined':
-      del versions[key]
-  return versions
+  new_versions = {k: v for k, v in versions.items() if v and v != 'undefined'}
+  return new_versions
