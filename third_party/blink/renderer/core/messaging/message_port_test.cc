226be6616673bdddb33cbe3e848d3599dfce8e98
Jeremy Roman <jbroman@chromium.org>
2022-10-12 17:19:21 +0000

    Add a couple quick unit tests for MessagePort event dispatch.
    This improves unit test coverage, which automated tools are complaining
    that we should do.
    
    Change-Id: I0ca0f2773eb625e08ec170d7c3f78dfb17de75b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946519
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058114}

---
+++ third_party/blink/renderer/core/messaging/message_port_test.cc
@@ -0,0 +1,71 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/messaging/message_port.h"
+
+#include "base/run_loop.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/messaging/transferable_message.mojom-blink.h"
+#include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/event_type_names.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/messaging/blink_transferable_message.h"
+#include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
+#include "third_party/blink/renderer/core/testing/wait_for_event.h"
+
+namespace blink {
+namespace {
+
+BlinkTransferableMessage MakeNullMessage() {
+  BlinkTransferableMessage message;
+  message.message = SerializedScriptValue::NullValue();
+  message.sender_agent_cluster_id = base::UnguessableToken::Create();
+  return message;
+}
+
+TEST(MessagePortTest, DispatchMessageEvent) {
+  DummyPageHolder holder;
+  LocalDOMWindow* window = holder.GetFrame().DomWindow();
+
+  MessagePort* port = MakeGarbageCollected<MessagePort>(*window);
+
+  base::RunLoop run_loop;
+  auto* wait = MakeGarbageCollected<WaitForEvent>();
+  wait->AddEventListener(port, event_type_names::kMessage);
+  wait->AddEventListener(port, event_type_names::kMessageerror);
+  wait->AddCompletionClosure(run_loop.QuitClosure());
+
+  mojo::Message mojo_message =
+      mojom::blink::TransferableMessage::WrapAsMessage(MakeNullMessage());
+  ASSERT_TRUE(static_cast<mojo::MessageReceiver*>(port)->Accept(&mojo_message));
+  run_loop.Run();
+
+  EXPECT_EQ(wait->GetLastEvent()->type(), event_type_names::kMessage);
+}
+
+TEST(MessagePortTest, DispatchMessageErrorEvent_LockedAgentCluster) {
+  DummyPageHolder holder;
+  LocalDOMWindow* window = holder.GetFrame().DomWindow();
+
+  MessagePort* port = MakeGarbageCollected<MessagePort>(*window);
+
+  base::RunLoop run_loop;
+  auto* wait = MakeGarbageCollected<WaitForEvent>();
+  wait->AddEventListener(port, event_type_names::kMessage);
+  wait->AddEventListener(port, event_type_names::kMessageerror);
+  wait->AddCompletionClosure(run_loop.QuitClosure());
+
+  auto message = MakeNullMessage();
+  message.locked_to_sender_agent_cluster = true;
+  mojo::Message mojo_message =
+      mojom::blink::TransferableMessage::WrapAsMessage(std::move(message));
+  ASSERT_TRUE(static_cast<mojo::MessageReceiver*>(port)->Accept(&mojo_message));
+  run_loop.Run();
+
+  EXPECT_EQ(wait->GetLastEvent()->type(), event_type_names::kMessageerror);
+}
+
+}  // namespace
+}  // namespace blink
