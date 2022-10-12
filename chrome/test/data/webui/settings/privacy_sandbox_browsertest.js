4088c40ee33ba5cddaf54b890a855dec694d113b
Rushan Suleymanov <rushans@google.com>
2022-10-12 09:30:52 +0000

    Disable PrivacySandboxTest.All test on Mac
    Bug: 1373779
    Change-Id: I2de907b61182e79a7cd8741469f76e5112aa2379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945103
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1057945}

--- chrome/test/data/webui/settings/privacy_sandbox_browsertest.js
+++ chrome/test/data/webui/settings/privacy_sandbox_browsertest.js
@@ -7,6 +7,7 @@
 // Polymer BrowserTest fixture.
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
+GEN('#include "build/build_config.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
 var PrivacySandboxTest = class extends PolymerTest {
@@ -16,6 +17,12 @@ var PrivacySandboxTest = class extends PolymerTest {
   }
 };
 
-TEST_F('PrivacySandboxTest', 'All', function() {
+// TODO(crbug.com/1373779): Flaky on Mac.
+GEN('#if BUILDFLAG(IS_MAC)');
+GEN('#define MAYBE_All DISABLED_All');
+GEN('#else');
+GEN('#define MAYBE_All All');
+GEN('#endif');
+TEST_F('PrivacySandboxTest', 'MAYBE_All', function() {
   mocha.run();
 });
