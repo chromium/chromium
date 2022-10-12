add7879decfe863adadb76b0be0c6a776f0cb02e
Tom Anderson <thomasanderson@chromium.org>
2022-10-12 00:07:48 +0000

    Remove libwayland-client from allowed deps
    This is no longer required after:
    https://chromium-review.googlesource.com/c/chromium/src/+/3947549
    
    R=thestig
    
    Bug: 1370990
    Change-Id: I6413c9b9e30f3d356290d344eeedf180e0a7e3e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946513
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1057793}

--- chrome/installer/linux/rpm/update_package_provides.py
+++ chrome/installer/linux/rpm/update_package_provides.py
@@ -56,7 +56,6 @@ LIBRARY_FILTER = set([
     "libsmime3.so",
     "libstdc++.so",
     "libuuid.so",
-    "libwayland-client.so",
     "libxcb.so",
     "libxcb-dri3.so.0",
     "libxkbcommon.so.0",
