15f396a24f107520f11c403d5ecaa4ec9f098701
Scott Little <sclittle@chromium.org>
2022-10-12 20:29:47 +0000

    Added JUnit tests for TranslateMessage.
    This CL only adds tests, and doesn't change any user-visible behavior.
    
    Bug: 1373663
    Change-Id: I2f6a50f8c99f5f9238ffa83aff5fbe9e90f666f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945888
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058243}

--- components/translate/content/android/java/src/org/chromium/components/translate/TranslateMessage.java
+++ components/translate/content/android/java/src/org/chromium/components/translate/TranslateMessage.java
@@ -11,6 +11,7 @@ import android.text.TextUtils;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -95,7 +96,8 @@ class TranslateMessage implements TranslateMessageSecondaryMenu.Handler {
                 dismissalDurationSeconds);
     }
 
-    private TranslateMessage(@NonNull Context context, @NonNull MessageDispatcher messageDispatcher,
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    TranslateMessage(@NonNull Context context, @NonNull MessageDispatcher messageDispatcher,
             @NonNull WebContents webContents, long nativeTranslateMessage,
             int dismissalDurationSeconds) {
         mContext = context;
