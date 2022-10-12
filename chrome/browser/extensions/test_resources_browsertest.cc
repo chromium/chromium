758c8842e3d01846c6fdc3b861a2099416384873
Yanzhu Du <yzd@google.com>
2022-10-12 20:00:01 +0000

    Load component extension can update extension without version change
    Bug: 1356742, b/243584674
    Change-Id: I266d99368805ed93eb7c21f7dc333a9b346920de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945957
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058228}

--- chrome/browser/extensions/test_resources_browsertest.cc
+++ chrome/browser/extensions/test_resources_browsertest.cc
@@ -13,12 +13,21 @@
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest.h"
+#include "extensions/common/manifest_constants.h"
+#include "extensions/common/manifest_handlers/externally_connectable.h"
+#include "extensions/common/url_pattern.h"
 #include "extensions/test/test_extension_dir.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace extensions {
 
 namespace {
+
+constexpr char kComponentExtensionKey[] =
+    "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+uU63MD6T82Ldq5wjrDFn5mGmPnnnj"
+    "WZBWxYXfpG4kVf0s+p24VkXwTXsxeI12bRm8/ft9sOq0XiLfgQEh5JrVUZqvFlaZYoS+g"
+    "iZfUqzKFGMLa4uiSMDnvv+byxrqAepKz5G8XX/q5Wm5cvpdjwgiu9z9iM768xJy+Ca/G5"
+    "qQwIDAQAB";
 
 // The value set by the script
 // in chrome/test/data/extensions/test_resources_test/script.js.
@@ -97,11 +106,6 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, TestResourcesLoad) {
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
                        TestResourcesLoadInComponentExtension) {
   TestExtensionDir test_dir;
-  constexpr char kKey[] =
-      "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+uU63MD6T82Ldq5wjrDFn5mGmPnnnj"
-      "WZBWxYXfpG4kVf0s+p24VkXwTXsxeI12bRm8/ft9sOq0XiLfgQEh5JrVUZqvFlaZYoS+g"
-      "iZfUqzKFGMLa4uiSMDnvv+byxrqAepKz5G8XX/q5Wm5cvpdjwgiu9z9iM768xJy+Ca/G5"
-      "qQwIDAQAB";
   constexpr char kManifestTemplate[] =
       R"({
            "name": "Test Extension",
@@ -109,7 +113,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
            "manifest_version": 2,
            "key": "%s"
          })";
-  test_dir.WriteManifest(base::StringPrintf(kManifestTemplate, kKey));
+  test_dir.WriteManifest(
+      base::StringPrintf(kManifestTemplate, kComponentExtensionKey));
 
   constexpr char kPageHtml[] =
       R"(<html>
@@ -129,6 +134,60 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
   absl::optional<int> sentinel = RetrieveSentinelValue(browser());
   ASSERT_TRUE(sentinel);
   EXPECT_EQ(kSentinelValue, *sentinel);
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
+                       LoadComponentExtensionUpdateWithManifestChanges) {
+  TestExtensionDir test_dir;
+
+  static constexpr char test_domain1[] = "http://*.domain1.com/*";
+  static constexpr char test_domain2[] = "http://*.domain2.com/*";
+
+  static constexpr char kManifestTemplate[] =
+      R"({
+           "name": "Test Component Extension",
+           "version": "1",
+           "description": "",
+           "manifest_version": 3,
+           "key": "%s",
+           "externally_connectable": {
+             "matches": [
+                "%s"
+             ]
+           }
+         })";
+
+  test_dir.WriteManifest(base::StringPrintf(
+      kManifestTemplate, kComponentExtensionKey, test_domain1));
+
+  const Extension* extension1 =
+      LoadExtensionAsComponent(test_dir.UnpackedPath());
+  ASSERT_TRUE(extension1);
+  EXPECT_EQ(mojom::ManifestLocation::kComponent, extension1->location());
+
+  ExternallyConnectableInfo* info1 = static_cast<ExternallyConnectableInfo*>(
+      extension1->GetManifestData(manifest_keys::kExternallyConnectable));
+  ASSERT_TRUE(info1);
+  EXPECT_EQ(1ul, info1->matches.size());
+
+  EXPECT_EQ(URLPattern(URLPattern::SCHEME_ALL, test_domain1),
+            *info1->matches.begin());
+
+  // Update the manifest and load the extension again.
+  test_dir.WriteManifest(base::StringPrintf(
+      kManifestTemplate, kComponentExtensionKey, test_domain2));
+  const Extension* extension2 =
+      LoadExtensionAsComponent(test_dir.UnpackedPath());
+  ASSERT_TRUE(extension2);
+  EXPECT_EQ(mojom::ManifestLocation::kComponent, extension2->location());
+
+  ExternallyConnectableInfo* info2 = static_cast<ExternallyConnectableInfo*>(
+      extension2->GetManifestData(manifest_keys::kExternallyConnectable));
+  ASSERT_TRUE(info2);
+  EXPECT_EQ(1ul, info2->matches.size());
+
+  EXPECT_EQ(URLPattern(URLPattern::SCHEME_ALL, test_domain2),
+            *info2->matches.begin());
 }
 
 // Tests that resources from _test_resources can be loaded from different
