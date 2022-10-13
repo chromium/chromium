e57363bea8d908ccd7eb80b51bf2bf3d2d46e8a4
Sorin Jianu <sorin@chromium.org>
2022-10-13 18:38:08 +0000

    Reland "Enable former flaky test IntegrationTest.SelfUpdateFromOldReal."
    This is a reland of commit 59ad6768ace45938169ec68e08df1fa4e10d2b05
    
    Original change's description:
    > Enable former flaky test IntegrationTest.SelfUpdateFromOldReal.
    >
    > We'd like to see if the typelib error is still present after the
    > recent fixes.
    >
    > Bug: 1341471
    > Change-Id: Ia5c984f8648b20f2da225a60c986ae16f7e1db50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3947483
    > Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    > Auto-Submit: Sorin Jianu <sorin@chromium.org>
    > Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1057700}
    
    Bug: 1341471
    Change-Id: Ic43e1ae6cd802309e4d0084b5ffd055adbbef8ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953894
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058842}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -750,14 +750,7 @@ TEST_F(IntegrationTest, UnregisterUnownedApp) {
 
 #if BUILDFLAG(CHROMIUM_BRANDING) || BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #if !defined(COMPONENT_BUILD)
-// Disabled on Windows due to undiagnosed typelib errors even after
-// instrumenting the build; see https://crbug.com/1341471.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_SelfUpdateFromOldReal DISABLED_SelfUpdateFromOldReal
-#else
-#define MAYBE_SelfUpdateFromOldReal SelfUpdateFromOldReal
-#endif
-TEST_F(IntegrationTest, MAYBE_SelfUpdateFromOldReal) {
+TEST_F(IntegrationTest, SelfUpdateFromOldReal) {
   ScopedServer test_server(test_commands_);
 
   SetupRealUpdaterLowerVersion();
