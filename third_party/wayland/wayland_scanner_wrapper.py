37381776f3985930239fcf1e061394de14ceb2f9
Alexander Dunaev <adunaev@igalia.com>
2022-10-13 10:25:49 +0000

    [linux/wayland] Cleaned up the code of the wrapper for wayland-scanner.
    This is a minor follow-up to the earlier work.  After some changes made
    in parallel by other contributors [1], we no longer need to support
    different versions of libwayland for client and server, so here the
    TODOs for that work are removed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3950350
    
    Bug: 1366678
    Change-Id: I26f42509a557052965d15dd59f0784f75d9c14ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952150
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Auto-Submit: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1058609}

--- third_party/wayland/wayland_scanner_wrapper.py
+++ third_party/wayland/wayland_scanner_wrapper.py
@@ -38,14 +38,14 @@ def main(argv):
   protocols = options.protocols
   generator_type = options.generator_type
 
-  version = subprocess.check_output([cmd, '--version'],
-                                    stderr=subprocess.STDOUT).decode('utf-8')
+  version = subprocess.check_output([cmd, "--version"],
+                                    stderr=subprocess.STDOUT).decode("utf-8")
   # The version is of the form "wayland-scanner 1.18.0\n"
-  version = tuple([int(x) for x in version.strip().split(' ')[1].split('.')])
+  version = tuple([int(x) for x in version.strip().split(" ")[1].split(".")])
   # This needs to generate private-code to avoid ODR
   # violation and avoid hacks such as in https://crrev.com/c/2416941/
   # See third_party/wayland/BUILD.gn#212 there for details there.
-  private_code_type = 'private-code' if version > (1, 14, 90) else 'code'
+  private_code_type = "private-code" if version > (1, 14, 90) else "code"
 
   for protocol in protocols:
     protocol_path = os.path.join(src_root, protocol)
@@ -53,19 +53,19 @@ def main(argv):
     out_base_name = os.path.join(root_gen_dir, protocol_without_extension)
     if generator_type == "protocol-marshalling":
       generate_code(cmd, [ private_code_type ], protocol_path,
-                              out_base_name + "-protocol.c")
+                    out_base_name + "-protocol.c")
     elif generator_type == "protocol-client":
-      scanner_args = [ 'client-header' ]
+      scanner_args = [ "client-header" ]
       generate_code(cmd, scanner_args, protocol_path,
                     out_base_name + "-client-protocol.h")
-      scanner_args.append('-c')
+      scanner_args.append("-c")
       generate_code(cmd, scanner_args, protocol_path,
                     out_base_name + "-client-protocol-core.h")
     elif generator_type == "protocol-server":
-      scanner_args = [ 'server-header' ]
+      scanner_args = [ "server-header" ]
       generate_code(cmd, scanner_args, protocol_path,
                     out_base_name + "-server-protocol.h")
-      scanner_args.append('-c')
+      scanner_args.append("-c")
       generate_code(cmd, scanner_args, protocol_path,
                     out_base_name + "-server-protocol-core.h")
     else:
@@ -73,9 +73,9 @@ def main(argv):
 
       generate_code(cmd, [ private_code_type ], protocol_path,
                     out_base_name + "-protocol.c")
-      generate_code(cmd, [ 'client-header' ], protocol_path,
+      generate_code(cmd, [ "client-header" ], protocol_path,
                     out_base_name + "-client-protocol.h")
-      generate_code(cmd, [ 'server-header' ], protocol_path,
+      generate_code(cmd, [ "server-header" ], protocol_path,
                     out_base_name + "-server-protocol.h")
 
 
