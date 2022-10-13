774152d606295edfbe8795407ac842b7de9037d9
Martin Kreichgauer <martinkr@google.com>
2022-10-13 21:48:19 +0000

    Reland "webauthn: Reorder USB mechanism in request dialog"
    This reverts commit 0bc47067733da3a4b2d9775f712e82fee31c313a.
    
    Reason for revert: This change is not the culprit
    
    Original change's description:
    > Revert "webauthn: Reorder USB mechanism in request dialog"
    >
    > This reverts commit 00b81f5cda556fd362e655dfa0fa9f224b69616c.
    >
    > Reason for revert: Suspect culprit for AuthenticatorDialogViewTest.InvokeUi_manage_devices failure 
    >
    > Original change's description:
    > > webauthn: Reorder USB mechanism in request dialog
    > >
    > > Slot USB after "This device" but before "Another device".
    > >
    > > Bug: 1358719
    > > Change-Id: I6d915e075f890049ff190eca96ce29b951d5da77
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3946564
    > > Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    > > Reviewed-by: Adam Langley <agl@chromium.org>
    > > Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1058452}
    >
    > Bug: 1358719, 1374211
    > Change-Id: I9ad00edbb6260951d230314722dfda13fc4ce43d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952010
    > Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    > Owners-Override: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1058581}
    
    Bug: 1358719, 1374211
    Change-Id: I659946ad24e2ac3e9b997f767cf0114971dc8673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952983
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1058939}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -1107,14 +1107,13 @@ void AuthenticatorRequestDialogModel::PopulateMechanisms(
     priority_transport = AuthenticatorTransport::kInternal;
   }
 
-  std::vector<AuthenticatorTransport> transports_to_list_if_active = {
-      AuthenticatorTransport::kUsbHumanInterfaceDevice,
-  };
-
+  std::vector<AuthenticatorTransport> transports_to_list_if_active;
   if (!use_conditional_mediation_) {
     // Conditional requests offer platform credentials through the autofill UI.
     transports_to_list_if_active.push_back(AuthenticatorTransport::kInternal);
   }
+  transports_to_list_if_active.push_back(
+      AuthenticatorTransport::kUsbHumanInterfaceDevice);
 
   const auto kCable = AuthenticatorTransport::kHybrid;
   bool include_add_phone_option = false;
