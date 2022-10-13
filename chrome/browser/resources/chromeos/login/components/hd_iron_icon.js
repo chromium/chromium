01b1a1164c6cf1911a6897ead32fb16ba2bca029
Renato Silva <rrsilva@google.com>
2022-10-13 19:47:18 +0000

    OOBE - Polymer3 - Check-in hd-iron-icon
    Check-in the modularised version of hd-iron-icon.
    
    Bug: 1184731, b/253399994
    Change-Id: I70f5fe780ae951213ff1c565028ce81464fe859d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952292
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058886}

--- chrome/browser/resources/chromeos/login/components/hd_iron_icon.js
+++ chrome/browser/resources/chromeos/login/components/hd_iron_icon.js
@@ -2,17 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/* #js_imports_placeholder */
+/**
+ * @fileoverview <iron-icon> that automatically displays one of the two icons
+ * depending on display resolution,
+ *
+ * Example:
+ *    <hd-iron-icon icon1x="icons:wifi-1x"
+ * icon2x="icons:wifi-2x"></hd-iron-icon>
+ *
+ *  Attributes:
+ *    1x and 2x icons:
+ *      'icon1x' - a name of icon from material design set to show on button.
+ *      'icon2x' - a name of icon from material design set to show on button.
+ *   1x and 2x sources:
+ *     'src1x' - A direct source to a file. For example, a SVG file.
+ *     'src2x' - A direct source to a file. For example, a SVG file.
+ */
+
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import './common_styles/common_styles.m.js';
+
+import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 /**
  * @polymer
  */
-class HdIronIcon extends Polymer.Element {
+class HdIronIcon extends PolymerElement {
   static get is() {
     return 'hd-iron-icon';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
--- chrome/browser/resources/chromeos/login/screens/oobe/hid_detection.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/hid_detection.js
@@ -9,7 +9,7 @@
 
 import '//resources/polymer/v3_0/paper-styles/color.js';
 import '//resources/ash/common/bluetooth/bluetooth_pairing_enter_code_page.js';
-import '../../components/hd_iron_icon.m.js';
+import '../../components/hd_iron_icon.js';
 import '../../components/oobe_icons.m.js';
 import '../../components/common_styles/common_styles.m.js';
 import '../../components/common_styles/oobe_dialog_host_styles.m.js';
