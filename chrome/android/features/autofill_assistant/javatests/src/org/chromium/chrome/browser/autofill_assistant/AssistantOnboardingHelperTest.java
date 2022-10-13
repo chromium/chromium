4683e99b43826ec066c901da116ac5dfd51bb8e2
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-10-13 13:22:44 +0000

    Disable flaky testDialogOnboarding.
    Bug: 1374238
    Change-Id: Ie6e0ad492304830d8af7283be5c5495be176e8a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952291
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1058657}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AssistantOnboardingHelperTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AssistantOnboardingHelperTest.java
@@ -34,6 +34,7 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
@@ -96,6 +97,7 @@ public class AssistantOnboardingHelperTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1374238")
     public void testDialogOnboarding() {
         TestThreadUtils.runOnUiThreadBlocking(
                 ()
