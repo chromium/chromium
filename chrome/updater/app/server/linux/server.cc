a44680e12168b8620e61f72638aaa44b76570235
Noah Rose Ledesma <noahrose@google.com>
2022-10-13 16:43:21 +0000

    Add Linux AppServer stub
    A server stub is being added to reduce friction with RPC security
    reviews. Future CLs will implement these RPC calls and attach the client
    and server via Mojo Invitations.
    
    Bug: 1371142
    Change-Id: Ie030fc09babfc52cbdca39bfcca3dcdac61d1068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3940065
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1058771}

--- chrome/updater/app/server/linux/server.cc
+++ chrome/updater/app/server/linux/server.cc
@@ -7,13 +7,42 @@
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
 #include "chrome/updater/app/app.h"
+#include "chrome/updater/update_service.h"
+#include "chrome/updater/update_service_internal.h"
 
 namespace updater {
 
+AppServerLinux::AppServerLinux() = default;
+AppServerLinux::~AppServerLinux() = default;
+
 // TODO(crbug.com/1276117) - implement.
-scoped_refptr<App> MakeAppServer() {
+void AppServerLinux::ActiveDuty(scoped_refptr<UpdateService> update_service) {
   NOTIMPLEMENTED();
-  return nullptr;
+}
+
+void AppServerLinux::ActiveDutyInternal(
+    scoped_refptr<UpdateServiceInternal> update_service_internal) {
+  NOTIMPLEMENTED();
+}
+
+bool AppServerLinux::SwapInNewVersion() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool AppServerLinux::MigrateLegacyUpdaters(
+    base::RepeatingCallback<void(const RegistrationRequest&)>
+        register_callback) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void AppServerLinux::UninstallSelf() {
+  NOTIMPLEMENTED();
+}
+
+scoped_refptr<App> MakeAppServer() {
+  return base::MakeRefCounted<AppServerLinux>();
 }
 
 }  // namespace updater
--- chrome/updater/app/server/linux/server.h
+++ chrome/updater/app/server/linux/server.h
@@ -6,10 +6,31 @@
 #define CHROME_UPDATER_APP_SERVER_LINUX_SERVER_H_
 
 #include "base/memory/scoped_refptr.h"
+#include "chrome/updater/app/app_server.h"
 
 namespace updater {
 
 class App;
+class UpdateService;
+class UpdateServiceInternal;
+
+class AppServerLinux : public AppServer {
+ public:
+  AppServerLinux();
+
+ private:
+  ~AppServerLinux() override;
+
+  // Overrides for AppServer.
+  void ActiveDuty(scoped_refptr<UpdateService> update_service) override;
+  void ActiveDutyInternal(
+      scoped_refptr<UpdateServiceInternal> update_service_internal) override;
+  bool SwapInNewVersion() override;
+  bool MigrateLegacyUpdaters(
+      base::RepeatingCallback<void(const RegistrationRequest&)>
+          register_callback) override;
+  void UninstallSelf() override;
+};
 
 scoped_refptr<App> MakeAppServer();
 
