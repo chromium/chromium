113016ff157a9b565b2b1e622eebf8fab6676dbc
David Bertoni <dbertoni@chromium.org>
2022-10-14 00:51:23 +0000

    [Code Health] Convert Value::GetListDeprecated() to Value::GetList() in /content/renderer/java.
    This CL was uploaded by git cl split.
    
    R=michaelbai@chromium.org
    
    Bug: 1303949
    Change-Id: Iecd9f7d0939cf7721c9f370bb6e263b57565a2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3945949
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059038}

--- content/renderer/java/gin_java_bridge_value_converter_unittest.cc
+++ content/renderer/java/gin_java_bridge_value_converter_unittest.cc
@@ -140,9 +140,9 @@ TEST_F(GinJavaBridgeValueConverterTest, TypedArrays) {
         converter->FromV8Value(v8_typed_array, context));
     ASSERT_TRUE(list_value.get()) << typed_array_type;
     ASSERT_TRUE(list_value->is_list()) << typed_array_type;
-    EXPECT_EQ(1u, list_value->GetListDeprecated().size()) << typed_array_type;
+    EXPECT_EQ(1u, list_value->GetList().size()) << typed_array_type;
 
-    const auto value = list_value->GetListDeprecated().cbegin();
+    const auto value = list_value->GetList().cbegin();
     if (value->type() == base::Value::Type::BINARY) {
       std::unique_ptr<const GinJavaBridgeValue> gin_value(
           GinJavaBridgeValue::FromValue(&*value));
