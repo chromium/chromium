523771772ebe6805275b973b478cb56e0b0bedcd
Brian Ho <hob@chromium.org>
2022-10-14 18:39:10 +0000

    blink: Report video dropped frames on ChromeOS
    Currently, we never report frames as dropped on ChromeOS and Linux in
    the VideoFrameSubmitter due to inconsistent presentation feedback
    flags. While this may be true on Linux, I don't think this is the
    case on CrOS (at least, anymore) and it preventing CrOS from having
    meaningful video smoothness metrics.
    
    Let's try removing this restriction.
    
    Change-Id: I68e06e40b8574e3e78c4280b2754130ef1d98c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3949909
    Commit-Queue: Brian Ho <hob@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1059381}

--- third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
@@ -347,7 +347,7 @@ void VideoFrameSubmitter::OnBeginFrame(
       continue;
     auto& feedback =
         timing_details.find(frame_token)->value.presentation_feedback;
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX)
     // TODO: On Linux failure flag is unreliable, and perfectly rendered frames
     // are reported as failures all the time.
     bool presentation_failure = false;
