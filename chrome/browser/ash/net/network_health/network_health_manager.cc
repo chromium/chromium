8c90691ae3500e783246f7b4c39b059df5075de8
Steven Bennetts <stevenjb@chromium.org>
2022-10-17 16:53:11 +0000

    Move NetworkHealthService instance ownership to in_process_instance.cc (Take 2)
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/3933496 with a fix.
    
    The fix is to call network_health::GetInProcessInstance() to construct
    and register the service.
    
    This is part 3/4:
        1. chrome/browser/ash/net/network_health/NetworkHealthService
           -> NetworkHealthAsh
        2. chrome/browser/ash/net/network_health/NetworkHealth
           -> chromeos/services/network_health/NetworkHealthService
        3. chromeos/services/network_health/in_process_instance.cc owns
           the browser global NetworkHealthService instance, available to
           Ash code outside of src/chrome.
        4. chromeos/services/network_health/public/cpp/network_health_helper
           provides a helper class for observing either an in-process instance
           (on Ash) or a remote instance (on Lacros) and tracking the active
           networks.
    
    BUG=b:250725789
    TEST=Code compiles, unittests, chromeos_unittests
    
    Change-Id: Ief0b4076feed7782a268dbd5537f5cb79a9e1734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953074
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060000}

--- chrome/browser/ash/net/network_health/network_health_manager.cc
+++ chrome/browser/ash/net/network_health/network_health_manager.cc
@@ -7,14 +7,15 @@
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/net/network_diagnostics/network_diagnostics.h"
 #include "chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.h"
+#include "chromeos/services/network_health/in_process_instance.h"
 #include "chromeos/services/network_health/network_health_service.h"
 
 namespace ash {
 namespace network_health {
 
 NetworkHealthManager::NetworkHealthManager() {
-  network_health_service_ =
-      std::make_unique<chromeos::network_health::NetworkHealthService>();
+  // Ensure that the NetworkHealthService instance is running.
+  chromeos::network_health::GetInProcessInstance();
   network_diagnostics_ =
       std::make_unique<network_diagnostics::NetworkDiagnostics>(
           DebugDaemonClient::Get());
@@ -41,7 +42,8 @@ NetworkHealthManager::GetDiagnosticsRemoteAndBindReceiver() {
 void NetworkHealthManager::BindHealthReceiver(
     mojo::PendingReceiver<chromeos::network_health::mojom::NetworkHealthService>
         receiver) {
-  network_health_service_->BindReceiver(std::move(receiver));
+  chromeos::network_health::GetInProcessInstance()->BindReceiver(
+      std::move(receiver));
 }
 
 void NetworkHealthManager::BindDiagnosticsReceiver(
@@ -54,7 +56,8 @@ void NetworkHealthManager::BindDiagnosticsReceiver(
 void NetworkHealthManager::AddObserver(
     mojo::PendingRemote<chromeos::network_health::mojom::NetworkEventsObserver>
         observer) {
-  network_health_service_->AddObserver(std::move(observer));
+  chromeos::network_health::GetInProcessInstance()->AddObserver(
+      std::move(observer));
 }
 
 NetworkHealthManager* NetworkHealthManager::GetInstance() {
