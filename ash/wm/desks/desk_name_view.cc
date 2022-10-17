007a0c8c27a0bc7b2e291bbdc778d68968ce1cbc
Andrew Pantera <andp@chromium.org>
2022-10-17 18:03:02 +0000

    Virtual Desks Chromevox announcement on name view highlight.
    When a user tabs into a desk name view trigger an announcement to tell
    them how to edit the text.
    
    Bug: b/247921161
    Change-Id: I3924059278a7d087c84948265397d5016bfe2721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3953640
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060049}

--- ash/wm/desks/desk_name_view.cc
+++ ash/wm/desks/desk_name_view.cc
@@ -6,11 +6,14 @@
 
 #include <memory>
 
+#include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/wm/desks/desk_mini_view.h"
 #include "ash/wm/desks/desks_bar_view.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/views/focus/focus_manager.h"
@@ -63,6 +66,16 @@ void DeskNameView::CommitChanges(views::Widget* widget) {
 }
 
 void DeskNameView::OnViewHighlighted() {
+  if (!HasFocus()) {
+    // When the highlight is the result of tabbing, as opposed to clicking or
+    // chromevoxing, the name view will not have focus, so the user should be
+    // told how to focus and edit the field.
+    Shell::Get()
+        ->accessibility_controller()
+        ->TriggerAccessibilityAlertWithMessage(l10n_util::GetStringUTF8(
+            IDS_ASH_DESKS_NAME_HIGHLIGHT_NOTIFICATION));
+  }
+
   DesksTextfield::OnViewHighlighted();
   mini_view_->owner_bar()->ScrollToShowMiniViewIfNecessary(mini_view_);
 }
