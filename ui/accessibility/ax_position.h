1e56db572438afd3d7be1badccd550e7b87600ab
Alexander Surkov <asurkov@igalia.com>
2022-10-18 16:38:19 +0000

    ax_ui: AXPosition::CreatePositionAtStart/EndOfAnchor crash
    AXPosition::CreatePositionAtStart/EndOfAnchor crashes if a position
    has no valid anchor. In particular, a no valid anchor case may happen
    when there's no associated ax tree manager, for example, in case of
    ax assistant structure.
    
    Add a null check of an anchor and make the methods to return a null
    position. It may be felt somewhat hacky but later when AXPosition will
    be using AXNode instance (instead of tree_id/node_id pair) it will be
    no issue anymore.
    
    Regressed by: crrev.com/c/3810575
    
    Change-Id: Ib8db030316eafeeaa89f5b62c116fb50a2948813
    Bug: 1370437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3960790
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1060518}

--- ui/accessibility/ax_position.h
+++ ui/accessibility/ax_position.h
@@ -2286,27 +2286,33 @@ class AXPosition {
   }
 
   AXPositionInstance CreatePositionAtStartOfAnchor() const {
+    const AXNode* anchor = GetAnchor();
+    if (!anchor)
+      return CreateNullPosition();
+
     switch (kind_) {
       case AXPositionKind::NULL_POSITION:
         return CreateNullPosition();
       case AXPositionKind::TREE_POSITION:
-        return CreateTreePositionAtStartOfAnchor(*GetAnchor());
+        return CreateTreePositionAtStartOfAnchor(*anchor);
       case AXPositionKind::TEXT_POSITION:
-        return CreateTextPosition(*GetAnchor(), 0 /* text_offset */,
+        return CreateTextPosition(*anchor, 0 /* text_offset */,
                                   ax::mojom::TextAffinity::kDownstream);
     }
   }
 
   AXPositionInstance CreatePositionAtEndOfAnchor() const {
+    const AXNode* anchor = GetAnchor();
+    if (!anchor)
+      return CreateNullPosition();
+
     switch (kind_) {
       case AXPositionKind::NULL_POSITION:
         return CreateNullPosition();
-      case AXPositionKind::TREE_POSITION: {
-        DCHECK(GetAnchor());
-        return CreateTreePositionAtEndOfAnchor(*GetAnchor());
-      }
+      case AXPositionKind::TREE_POSITION:
+        return CreateTreePositionAtEndOfAnchor(*anchor);
       case AXPositionKind::TEXT_POSITION:
-        return CreateTextPosition(*GetAnchor(), MaxTextOffset(),
+        return CreateTextPosition(*anchor, MaxTextOffset(),
                                   ax::mojom::TextAffinity::kDownstream);
     }
   }
