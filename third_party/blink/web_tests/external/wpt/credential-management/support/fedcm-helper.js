d274f42214afcc8ee9585bd0553ef37ceb581b69
Peter Kotwicz <pkotwicz@chromium.org>
2022-10-18 19:34:22 +0000

    [FedCM] Improve CSP Test
    This CL improves the FedCM CSP test so that the test fails if the
    FedCM API does not respect CSP.
    
    Change-Id: Ibfebea806b34aa0c1b5e0f4fbf503e395f0a1d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961791
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060625}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.js
+++
@@ -1,9 +0,0 @@
-// Set the identity provider cookie.
-export function set_fedcm_cookie() {
-  return new Promise(resolve => {
-    const img = document.createElement('img');
-    img.src = 'support/set_cookie';
-    img.addEventListener('error', resolve);
-    document.body.appendChild(img);
-  });
-}
---
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -0,0 +1,24 @@
+// Set the identity provider cookie.
+export function set_fedcm_cookie() {
+  return new Promise(resolve => {
+    const img = document.createElement('img');
+    img.src = 'support/set_cookie';
+    img.addEventListener('error', resolve);
+    document.body.appendChild(img);
+  });
+}
+
+// Returns FedCM CredentialRequestOptions for which navigator.credentials.get()
+// succeeds.
+export function default_request_options() {
+  const manifest_origin = 'https://{{host}}:{{ports[https][0]}}';
+  return {
+    identity: {
+      providers: [{
+        configURL: manifest_origin + '/credential-management/support/fedcm.json',
+        clientId: '1',
+        nonce: '2',
+      }]
+    }
+  };
+}
