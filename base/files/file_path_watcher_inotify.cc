1ff8e6d71bc2577d15f71e76bae87e60f480826c
Alan Cutter <alancutter@chromium.org>
2022-10-18 13:14:56 +0000

    Replace GetWeakPtr() with GetWatcherEntry() in FilePathWatcherImpl
    This CL refactors FilePathWatcherImpl to not expose its WeakPtr
    directly and instead expose a more high level GetWatcherEntry().
    This also enables FilePathWatcherImpl to not expose
    GetTaskRunner().
    
    R=wez@chromium.org
    
    Bug: 1366168
    Change-Id: I671af5e72965a62a2c09a17d3a0bc394528241c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3934006
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060425}

--- base/files/file_path_watcher_inotify.cc
+++ base/files/file_path_watcher_inotify.cc
@@ -119,6 +119,13 @@ class InotifyReader {
 #else
   using Watch = int;
 #endif
+
+  // Record of watchers tracked for watch descriptors.
+  struct WatcherEntry {
+    scoped_refptr<SequencedTaskRunner> task_runner;
+    WeakPtr<FilePathWatcherImpl> watcher;
+  };
+
   static constexpr Watch kInvalidWatch = static_cast<Watch>(-1);
   static constexpr Watch kWatchLimitExceeded = static_cast<Watch>(-2);
 
@@ -140,12 +147,6 @@ class InotifyReader {
 
  private:
   friend struct LazyInstanceTraitsBase<InotifyReader>;
-
-  // Record of watchers tracked for watch descriptors.
-  struct WatcherEntry {
-    scoped_refptr<SequencedTaskRunner> task_runner;
-    WeakPtr<FilePathWatcherImpl> watcher;
-  };
 
   InotifyReader();
   // There is no destructor because |g_inotify_reader| is a
@@ -197,11 +198,8 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate {
   // would exceed the limit if adding one more.
   bool WouldExceedWatchLimit() const;
 
-  // Returns the task runner to be used with this.
-  scoped_refptr<SequencedTaskRunner> GetTaskRunner() const;
-
-  // Returns the WeakPtr of this, must be called on the original sequence.
-  WeakPtr<FilePathWatcherImpl> GetWeakPtr() const;
+  // Returns a WatcherEntry for this, must be called on the original sequence.
+  InotifyReader::WatcherEntry GetWatcherEntry();
 
  private:
   // Start watching |path| for changes and notify |delegate| on each change.
@@ -365,8 +363,7 @@ InotifyReader::Watch InotifyReader::AddWatch(const FilePath& path,
     return kInvalidWatch;
   const Watch watch = static_cast<Watch>(watch_int);
 
-  watchers_[watch].emplace(std::make_pair(
-      watcher, WatcherEntry{watcher->GetTaskRunner(), watcher->GetWeakPtr()}));
+  watchers_[watch].emplace(std::make_pair(watcher, watcher->GetWatcherEntry()));
 
   return watch;
 }
@@ -557,14 +554,9 @@ bool FilePathWatcherImpl::WouldExceedWatchLimit() const {
   return number_of_inotify_watches >= GetMaxNumberOfInotifyWatches();
 }
 
-scoped_refptr<SequencedTaskRunner> FilePathWatcherImpl::GetTaskRunner() const {
+InotifyReader::WatcherEntry FilePathWatcherImpl::GetWatcherEntry() {
   DCHECK(task_runner()->RunsTasksInCurrentSequence());
-  return task_runner();
-}
-
-WeakPtr<FilePathWatcherImpl> FilePathWatcherImpl::GetWeakPtr() const {
-  DCHECK(task_runner()->RunsTasksInCurrentSequence());
-  return weak_factory_.GetWeakPtr();
+  return {task_runner(), weak_factory_.GetWeakPtr()};
 }
 
 bool FilePathWatcherImpl::Watch(const FilePath& path,
