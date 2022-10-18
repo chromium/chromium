7c7e064f2f4537338d7c14025f35f948cd5bb19e
Xi Han <hanxi@google.com>
2022-10-18 21:48:12 +0000

    [Sheriff] Disable NavigationHandlerTest#testCloseChromeAtHistoryStackHead.
    Bug: 1376200
    Change-Id: Id42b91fb68f908bdbb649ee3543c43d7099302bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963633
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Owners-Override: Xi Han <hanxi@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060681}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.layouts.LayoutManager;
@@ -114,6 +115,7 @@ public class NavigationHandlerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1376200")
     public void testCloseChromeAtHistoryStackHead() {
         loadNewTabPage();
         mNavUtils.swipeFromLeftEdge();
