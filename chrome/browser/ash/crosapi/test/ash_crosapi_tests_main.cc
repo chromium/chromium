da67673df9fb80cdcfbeb4bb941a159ea8895885
Eriko Kurimoto <elkurin@chromium.org>
2022-10-18 14:14:02 +0000

    AshCrosapiTest: Introduce CrosapiTestSuite and use LaunchUnitTests
    This CL introduces CrosapiTestSuite and initializes mojo and
    AshCrosapiTestEnv in it so that we can use LaunchUnitTests().
    
    Bug: 1357441
    Change-Id: I793976c0f7b36eb1d0542a89adea15f203faaa56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963568
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1060447}

--- chrome/browser/ash/crosapi/test/ash_crosapi_tests_main.cc
+++ chrome/browser/ash/crosapi/test/ash_crosapi_tests_main.cc
@@ -4,31 +4,73 @@
 
 #include <memory>
 
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/task/single_thread_task_executor.h"
-#include "base/test/launcher/test_launcher.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
 #include "base/threading/thread.h"
 #include "chrome/browser/ash/crosapi/test/ash_crosapi_tests_env.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
 
+namespace {
+
+// Test Suite used for Crosapi Test.
+// It initializes Mojo and AshCrosapiTestEnv.
+class CrosapiTestSuite : public base::TestSuite {
+ public:
+  CrosapiTestSuite(int argc, char** argv) : base::TestSuite(argc, argv) {}
+
+  CrosapiTestSuite(const CrosapiTestSuite&) = delete;
+  CrosapiTestSuite& operator=(const CrosapiTestSuite&) = delete;
+
+  ~CrosapiTestSuite() override = default;
+
+ protected:
+  void Initialize() override {
+    base::TestSuite::Initialize();
+
+    // Starts a new IO thread to run IPC tasks.
+    io_thread_.StartWithOptions(
+        base::Thread::Options(base::MessagePumpType::IO, 0));
+    mojo::core::Init();
+    ipc_support_ = std::make_unique<mojo::core::ScopedIPCSupport>(
+        io_thread_.task_runner(),
+        mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
+
+    executor_ = std::make_unique<base::SingleThreadTaskExecutor>(
+        base::MessagePumpType::IO);
+
+    // Construct AshCrosapiTestEnv.
+    env_ = std::make_unique<crosapi::AshCrosapiTestEnv>();
+  }
+
+  void Shutdown() override {
+    env_.reset();
+    executor_.reset();
+    ipc_support_.reset();
+    io_thread_.Stop();
+    base::TestSuite::Shutdown();
+  }
+
+ private:
+  base::Thread io_thread_{"MojoThread"};
+  std::unique_ptr<mojo::core::ScopedIPCSupport> ipc_support_;
+
+  std::unique_ptr<base::SingleThreadTaskExecutor> executor_;
+  std::unique_ptr<crosapi::AshCrosapiTestEnv> env_;
+};
+
+}  // namespace
+
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
 
-  // Starts a new IO thread to run IPC tasks.
-  base::Thread io_thread("MojoThread");
-  io_thread.StartWithOptions(
-      base::Thread::Options(base::MessagePumpType::IO, 0));
-  mojo::core::Init();
-  mojo::core::ScopedIPCSupport ipc_support(
-      io_thread.task_runner(),
-      mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
-
-  base::SingleThreadTaskExecutor executor(base::MessagePumpType::IO);
-
-  // Sets up crosapi test environment.
-  auto env = std::make_unique<crosapi::AshCrosapiTestEnv>();
-
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  CrosapiTestSuite test_suite(argc, argv);
+  // Run test serially.
+  // TODO(elkurin): Support parallel testing for better performance.
+  return base::LaunchUnitTestsSerially(
+      argc, argv,
+      BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
 }
