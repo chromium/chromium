7e81e5ae240240b9dcebd6743f1ac60a66d05106
Ben Pastene <bpastene@chromium.org>
2022-10-19 17:25:25 +0000

    Exit early in blink's merge_results.py when no JSON is available
    Right now, the exit codes of merge scripts are ignored. This means that
    any assert failures encountered in this script are silently ignored
    and have no effect in the builds.
    
    We'd like to start exposing these exit codes and make them fail the
    build (crbug.com/1346781). However, this particular assert in
    web_tests' merge script will fail the script when no swarming task
    produced any JSON. This can happen somewhat frequently if shards are
    expiring. eg:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-wpt-identity-fyi-rel/45681/overview
    
    Were the merge script's exit code exposed, that assert failure is
    bubbled-up and causes the build to fail early, eg:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-wpt-identity-fyi-rel/45682/overview
    
    But expired tasks aren't super uncommon, and we don't want the whole
    build quitting early if web_tests' shards expire. So this makes that
    scenario non-fatal for web_tests' merge script. eg: See the first run
    of web_tests in:
    https://luci-milo.appspot.com/swarming/task/5e05dc41fb105d10
    
    The suite is still considered a failure, but web_tests' merge_script
    doesn't fail the whole build. (The expirations later in the build are
    unrelated.)
    
    Bug: 1376204, 1346781
    Change-Id: I9a31b30f31c7e4d04647b2a27aeed0c9fab696da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964769
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061108}

--- third_party/blink/tools/blinkpy/web_tests/merge_results.py
+++ third_party/blink/tools/blinkpy/web_tests/merge_results.py
@@ -928,9 +928,11 @@ directory. The script will be given the arguments plus
 
     # Map the isolate arguments back to our output / input arguments.
     if args.output_json:
-        logging.info('Running with isolated arguments')
-        assert args.positional
+        if not args.positional:
+            logging.warning('No input JSON arguments passed. Quitting early.')
+            return
 
+        logging.info('Running with isolated arguments')
         # TODO(tansell): Once removed everywhere, these lines can be removed.
         # For now we just check nobody is supply arguments we didn't expect.
         if args.results_json_override_with_build_property:
