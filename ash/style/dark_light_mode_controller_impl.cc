dc52cd34189faa69d307f5a8a2208b4a6867b9a7
Maksim Ivanov <emaxx@chromium.org>
2022-10-19 21:06:25 +0000

    Revert "Add debug logs for dark/light theme switches"
    This reverts commit c9c6d0f9dc15ae727190437ffd3888a4d371a2fe.
    
    Reason for revert: The temporary verbose logging is not needed
    anymore. The crashes were fixed.
    
    Original change's description:
    > Add debug logs for dark/light theme switches
    >
    > Add temporary verbose ("WARNING") logs for the theme code. This will
    > help continue the investigation of login-screen crashes in the PIN
    > scenarios, in case the previous workarounds were insufficient.
    >
    > The logs should be removed after the crashes are proven to be fixed in
    > the wild.
    >
    > Bug: 1339004
    > Change-Id: Ife1bdfb56d1ac4db31264369d6eed590c362e1fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3845282
    > Commit-Queue: Sean Kau <skau@chromium.org>
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1037929}
    
    Bug: 1339004
    Change-Id: I648998f313ce4f09abcf0de0eb6582b6d2281980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965651
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061233}

--- ash/style/dark_light_mode_controller_impl.cc
+++ ash/style/dark_light_mode_controller_impl.cc
@@ -14,7 +14,6 @@
 #include "ash/style/color_util.h"
 #include "ash/style/dark_light_mode_nudge_controller.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
-#include "base/logging.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -69,10 +68,6 @@ DarkLightModeControllerImpl::DarkLightModeControllerImpl()
       nudge_controller_(std::make_unique<DarkLightModeNudgeController>()) {
   DCHECK(!g_instance);
   g_instance = this;
-
-  // TODO(crbug/1339004): Remove after fixing the crashes.
-  LOG(WARNING) << "Initial theme is "
-               << (IsDarkModeEnabled() ? "dark" : "light");
 
   // May be null in unit tests.
   if (Shell::HasInstance()) {
@@ -286,10 +281,6 @@ const char* DarkLightModeControllerImpl::GetFeatureName() const {
 
 void DarkLightModeControllerImpl::NotifyColorModeChanges() {
   const bool is_enabled = IsDarkModeEnabled();
-
-  // TODO(crbug/1339004): Remove after fixing the crashes.
-  LOG(WARNING) << "Theme is " << (is_enabled ? "dark" : "light");
-
   cros_styles::SetDarkModeEnabled(is_enabled);
   for (auto& observer : observers_)
     observer.OnColorModeChanged(is_enabled);
