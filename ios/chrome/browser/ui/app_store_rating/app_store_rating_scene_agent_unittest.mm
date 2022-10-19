5523dc4dd5844ac8bbdcca81ccf2e5372b3bf683
Hira Mahmood <hiramahmood@google.com>
2022-10-19 21:50:46 +0000

    Implement unit tests for AppStoreRatingSceneAgent.
    This change implements unit tests for the AppStoreRatingSceneAgent
    class.
    
    Bug: 1359065
    Change-Id: Id7da02bd2e53b314f5c5ca2f8bf475efd495afee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3952411
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061256}

--- ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent_unittest.mm
+++ ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent_unittest.mm
@@ -3,18 +3,225 @@
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/app_store_rating/app_store_rating_scene_agent.h"
+
+#import "base/values.h"
+#import "components/password_manager/core/browser/password_manager_util.h"
+#import "components/password_manager/core/common/password_manager_pref_names.h"
+#import "components/sync_preferences/testing_pref_service_syncable.h"
+#import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/prefs/browser_prefs.h"
+#import "ios/chrome/browser/prefs/pref_names.h"
+#import "ios/chrome/browser/promos_manager/constants.h"
+#import "ios/chrome/browser/promos_manager/mock_promos_manager.h"
+#import "ios/chrome/browser/promos_manager/promos_manager.h"
+#import "ios/chrome/browser/ui/app_store_rating/constants.h"
+#import "ios/chrome/browser/ui/default_promo/default_browser_utils.h"
+#import "ios/chrome/browser/ui/main/browser_interface_provider.h"
+#import "ios/chrome/browser/ui/main/test/fake_scene_state.h"
+#import "ios/web/public/test/web_task_environment.h"
+#import "testing/gmock/include/gmock/gmock.h"
 #import "testing/platform_test.h"
+#import "third_party/ocmock/OCMock/OCMock.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+using ::testing::_;
+using ::testing::AnyNumber;
+
 // Test fixture for testing AppStoreRatingSceneAgent class.
 class AppStoreRatingSceneAgentTest : public PlatformTest {
  protected:
-  AppStoreRatingSceneAgentTest() {}
+  AppStoreRatingSceneAgentTest() {
+    CreateMockPromosManager();
+    CreateFakeSceneState();
+    CreateAppStoreRatingSceneAgent();
+  }
 
-  ~AppStoreRatingSceneAgentTest() override {}
+  ~AppStoreRatingSceneAgentTest() override { ClearUserDefaults(); }
 
+ protected:
+  std::unique_ptr<TestChromeBrowserState> browser_state_;
+  web::WebTaskEnvironment task_environment_;
   AppStoreRatingSceneAgent* test_scene_agent_;
+  std::unique_ptr<MockPromosManager> promos_manager_;
+  FakeSceneState* fake_scene_state_;
+
+  // Create a MockPromosManager.
+  void CreateMockPromosManager() {
+    promos_manager_ = std::make_unique<MockPromosManager>();
+  }
+
+  // Create a FakeSceneState.
+  void CreateFakeSceneState() {
+    id mockAppState = OCMClassMock([AppState class]);
+    TestChromeBrowserState::Builder builder;
+    browser_state_ = builder.Build();
+    fake_scene_state_ =
+        [[FakeSceneState alloc] initWithAppState:mockAppState
+                                    browserState:browser_state_.get()];
+  }
+
+  // Create an AppStoreRatingSceneAgent to test.
+  void CreateAppStoreRatingSceneAgent() {
+    test_scene_agent_ = [[AppStoreRatingSceneAgent alloc]
+        initWithPromosManager:promos_manager_.get()];
+    test_scene_agent_.sceneState = fake_scene_state_;
+  }
+
+  // Set kAppStoreRatingTotalDaysOnChromeKey in NSUserDefaults.
+  void SetTotalDaysOnChrome(int days) {
+    [[NSUserDefaults standardUserDefaults]
+        setInteger:days
+            forKey:kAppStoreRatingTotalDaysOnChromeKey];
+  }
+
+  // Set kAppStoreRatingActiveDaysInPastWeekKey in NSUserDefaults to an array of
+  // NSDate objects.
+  void SetActiveDaysInPastWeek(int activeDays) {
+    NSMutableArray* array = [[NSMutableArray alloc] init];
+    for (int a = activeDays - 1; a >= 0; a--) {
+      [array addObject:CreateDateFromToday(-a)];
+    }
+    [[NSUserDefaults standardUserDefaults]
+        setObject:array
+           forKey:kAppStoreRatingActiveDaysInPastWeekKey];
+  }
+
+  // Remove the keys added to NSUserDefaults.
+  void ClearUserDefaults() {
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    [defaults removeObjectForKey:kAppStoreRatingActiveDaysInPastWeekKey];
+    [defaults removeObjectForKey:kAppStoreRatingTotalDaysOnChromeKey];
+    [defaults removeObjectForKey:kLastHTTPURLOpenTime];
+  }
+
+  // Set kLastHTTPURLOpenTime in NSUserDefaults so
+  // ChromeIsLikelyDefaultBrowser() returns true.
+  void SetTrueChromeLikelyDefaultBrowser() {
+    [[NSUserDefaults standardUserDefaults] setObject:[NSDate date]
+                                              forKey:kLastHTTPURLOpenTime];
+  }
+
+  // Remove kLastHTTPURLOpenTime from NSUserDefaults so
+  // ChromeIsLikelyDefaultBrowser() returns false.
+  void SetFalseChromeLikelyDefaultBrowser() {
+    [[NSUserDefaults standardUserDefaults]
+        removeObjectForKey:kLastHTTPURLOpenTime];
+  }
+
+  // Enable Credentials Provider.
+  void EnableCPE() {
+    browser_state_->GetPrefs()->SetBoolean(
+        password_manager::prefs::kCredentialProviderEnabledOnStartup, true);
+  }
+
+  // Disable Credentials Provider.
+  void DisableCPE() {
+    browser_state_->GetPrefs()->SetBoolean(
+        password_manager::prefs::kCredentialProviderEnabledOnStartup, false);
+  }
+
+  // Helper method that creates an NSDate object.
+  NSDate* CreateDateFromToday(int daysToAdd) {
+    NSDate* now = [NSDate date];
+    NSDateComponents* components = [[NSDateComponents alloc] init];
+    [components setDay:daysToAdd];
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* newDay = [calendar dateByAddingComponents:components
+                                               toDate:now
+                                              options:0];
+    return newDay;
+  }
 };
+
+#pragma mark - Tests
+
+// Tests that promo display is not requested when the user has
+// used Chrome for less than 3 days in the past week, but meets
+// the other requirements to be considered engaged.
+TEST_F(AppStoreRatingSceneAgentTest, TestChromeNotUsed3DaysInPastWeek) {
+  EXPECT_CALL(*promos_manager_.get(), RegisterPromoForSingleDisplay(_))
+      .Times(0);
+
+  SetActiveDaysInPastWeek(1);
+  SetTotalDaysOnChrome(16);
+  EnableCPE();
+  SetTrueChromeLikelyDefaultBrowser();
+
+  // Simulating the user launching or resuming the app.
+  [test_scene_agent_ sceneState:fake_scene_state_
+      transitionedToActivationLevel:SceneActivationLevelForegroundActive];
+}
+
+// Tests that promo display is not requested when the user has
+// used Chrome for less than 15 days overall, but meets
+// the other requirements to be considered engaged.
+TEST_F(AppStoreRatingSceneAgentTest, TestChromeNotUsed15Days) {
+  EXPECT_CALL(*promos_manager_.get(), RegisterPromoForSingleDisplay(_))
+      .Times(0);
+
+  SetActiveDaysInPastWeek(3);
+  SetTotalDaysOnChrome(10);
+  EnableCPE();
+  SetTrueChromeLikelyDefaultBrowser();
+
+  // Simulating the user launching or resuming the app.
+  [test_scene_agent_ sceneState:fake_scene_state_
+      transitionedToActivationLevel:SceneActivationLevelForegroundActive];
+}
+
+// Tests that promo display is not requested when the user has not
+// enabled CPE, but meets the other requirements to be considered engaged.
+TEST_F(AppStoreRatingSceneAgentTest, TestCPENotEnabled) {
+  EXPECT_CALL(*promos_manager_.get(), RegisterPromoForSingleDisplay(_))
+      .Times(0);
+
+  SetActiveDaysInPastWeek(4);
+  SetTotalDaysOnChrome(17);
+  DisableCPE();
+  SetTrueChromeLikelyDefaultBrowser();
+
+  // Simulating the user launching or resuming the app.
+  [test_scene_agent_ sceneState:fake_scene_state_
+      transitionedToActivationLevel:SceneActivationLevelForegroundActive];
+}
+
+// Tests that promo display is not requested when the user has not set
+// Chrome as their default browser, but meets the other requirements to be
+// considered engaged.
+TEST_F(AppStoreRatingSceneAgentTest, TestChromeNotDefaultBrowser) {
+  EXPECT_CALL(*promos_manager_.get(), RegisterPromoForSingleDisplay(_))
+      .Times(0);
+
+  SetActiveDaysInPastWeek(7);
+  SetTotalDaysOnChrome(15);
+  EnableCPE();
+  SetFalseChromeLikelyDefaultBrowser();
+
+  // Simulating the user launching or resuming the app.
+  [test_scene_agent_ sceneState:fake_scene_state_
+      transitionedToActivationLevel:SceneActivationLevelForegroundActive];
+}
+
+// Tests that promo display is requested when the user meets all
+// requirements to be considered engaged.
+TEST_F(AppStoreRatingSceneAgentTest, TestPromoCorrectlyRequested) {
+  EXPECT_CALL(
+      *promos_manager_.get(),
+      RegisterPromoForSingleDisplay(promos_manager::Promo::AppStoreRating))
+      .Times(1);
+
+  // Creating the requirements to be considered engaged.
+  SetActiveDaysInPastWeek(3);
+  SetTotalDaysOnChrome(15);
+  EnableCPE();
+  SetTrueChromeLikelyDefaultBrowser();
+
+  // Simulating the user launching or resuming the app.
+  [test_scene_agent_ sceneState:fake_scene_state_
+      transitionedToActivationLevel:SceneActivationLevelForegroundActive];
+}
