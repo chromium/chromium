792b3280990bd51a883c2be2c25ca8d9c9d552fd
David Bienvenu <davidbienvenu@chromium.org>
2022-10-20 18:43:09 +0000

    Update instructions to current version of Visual Studio
    Change-Id: I71b95b46a33cdad86aa158eb8a378223327e0e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968346
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061722}

--- tools/win/pe_summarize.py
+++ tools/win/pe_summarize.py
@@ -118,8 +118,8 @@ def main():
             sections = None
     except WindowsError as error:
       if error.winerror == 2:
-        print (r'Cannot find dumpbin. Run "C:\Program Files (x86)\Microsoft '
-               r'Visual Studio\2017\Professional\VC\Auxiliary\Build'
+        print (r'Cannot find dumpbin. Run "C:\Program Files\Microsoft '
+               r'Visual Studio\2022\Professional\VC\Auxiliary\Build'
                r'\vcvarsall.bat amd64" or similar to add dumpbin to the path.')
       else:
         print(error)
