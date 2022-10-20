cfc1cfd96ef7d07f27f9fd205f52ac47754ae787
Sorin Jianu <sorin@chromium.org>
2022-10-20 04:13:26 +0000

    Updater: add a URL moniker fetcher test.
    This provides a baseline for how fast a download from the test server
    on the test bot is.
    
    Bug: 1376671
    Change-Id: I38a87682a0e7c1814f03e123043c05be876717c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967612
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061400}

--- chrome/updater/win/net/network_unittest.cc
+++ chrome/updater/win/net/network_unittest.cc
@@ -4,15 +4,25 @@
 
 #include "chrome/updater/win/net/network.h"
 
-#include <memory>
+#include <Urlmon.h>
 
-#include "base/memory/ref_counted.h"
-#include "base/run_loop.h"
+#include <memory>
+#include <string>
+
+#include "base/base_paths.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/path_service.h"
+#include "base/strings/strcat.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/task_environment.h"
 #include "chrome/updater/policy/service.h"
 #include "chrome/updater/unittest_util.h"
 #include "chrome/updater/win/net/network_fetcher.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace updater {
 
@@ -24,6 +34,28 @@ TEST(UpdaterTestNetwork, NetworkFetcherWinHTTPFactory) {
           PolicyServiceProxyConfiguration::Get(test::CreateTestPolicyService()))
           ->Create();
   EXPECT_NE(fetcher, nullptr);
+}
+
+// Tests that a direct download through URL moniker from a local HTTP server is
+// reasonably fast. This provides a baseline to compare the network throughput
+// of various fetchers.
+TEST(UpdaterTestNetwork, URLMonFetcher) {
+  base::test::SingleThreadTaskEnvironment task_environment(
+      base::test::SingleThreadTaskEnvironment::MainThreadType::UI);
+  net::test_server::EmbeddedTestServer server;
+  server.ServeFilesFromSourceDirectory("chrome/updater/test/data");
+  net::test_server::EmbeddedTestServerHandle server_handle =
+      server.StartAndReturnHandle();
+  base::ScopedTempDir scoped_dir;
+  ASSERT_TRUE(scoped_dir.CreateUniqueTempDir());
+  const base::FilePath dest =
+      scoped_dir.GetPath().AppendASCII("updater-signed.exe");
+  EXPECT_HRESULT_SUCCEEDED(::URLDownloadToFile(
+      nullptr,
+      base::ASCIIToWide(base::StrCat({server.base_url().spec(), "signed.exe"}))
+          .c_str(),
+      dest.value().c_str(), 0, nullptr));
+  EXPECT_TRUE(base::PathExists(dest));
 }
 
 }  // namespace updater
