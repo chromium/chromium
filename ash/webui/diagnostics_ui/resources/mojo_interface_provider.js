5af017cd8e2e66690fa4fdf4360a572e895995f6
Michael Checo <michaelcheco@google.com>
2022-10-20 23:12:59 +0000

    Keyboard Diagnostics: Setup FakeInputDataProvider
    - This change allows us to deploy the diagnostics app with fake input
      data.
    
    Bug: b/207089601
    Change-Id: I5ba6440d78dc1a7f5e352ae22a37d242cf05dc80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3965761
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1061876}

--- ash/webui/diagnostics_ui/resources/mojo_interface_provider.js
+++ ash/webui/diagnostics_ui/resources/mojo_interface_provider.js
@@ -4,7 +4,8 @@
 
 import {assert} from 'chrome://resources/js/assert.js';
 
-import {fakeAllNetworksAvailable, fakeBatteryChargeStatus, fakeBatteryHealth, fakeBatteryInfo, fakeCellularNetwork, fakeCpuUsage, fakeEthernetNetwork, fakeMemoryUsage, fakePowerRoutineResults, fakeRoutineResults, fakeSystemInfo, fakeWifiNetwork} from './fake_data.js';
+import {fakeAllNetworksAvailable, fakeBatteryChargeStatus, fakeBatteryHealth, fakeBatteryInfo, fakeCellularNetwork, fakeCpuUsage, fakeEthernetNetwork, fakeKeyboards, fakeMemoryUsage, fakePowerRoutineResults, fakeRoutineResults, fakeSystemInfo, fakeTouchDevices, fakeWifiNetwork} from './fake_data.js';
+import {FakeInputDataProvider} from './fake_input_data_provider.js';
 import {FakeNetworkHealthProvider} from './fake_network_health_provider.js';
 import {FakeSystemDataProvider} from './fake_system_data_provider.js';
 import {FakeSystemRoutineController} from './fake_system_routine_controller.js';
@@ -153,6 +154,13 @@ export function getNetworkHealthProvider() {
   return networkHealthProvider;
 }
 
+// Creates a FakeInputDataProvider with fake devices setup.
+function setupFakeInputDataProvider() {
+  const provider = new FakeInputDataProvider();
+  provider.setFakeConnectedDevices(fakeKeyboards, fakeTouchDevices);
+  setInputDataProviderForTesting(provider);
+}
+
 /**
  * @param {!InputDataProviderInterface} testProvider
  */
@@ -165,7 +173,11 @@ export function setInputDataProviderForTesting(testProvider) {
  */
 export function getInputDataProvider() {
   if (!inputDataProvider) {
-    inputDataProvider = InputDataProvider.getRemote();
+    if (useFakeProviders) {
+      setupFakeInputDataProvider();
+    } else {
+      inputDataProvider = InputDataProvider.getRemote();
+    }
   }
 
   assert(!!inputDataProvider);
