2dca60eb7213fd5e214dc30aa51996fbf420cb24
Dana Fried <dfried@chromium.org>
2022-10-20 19:33:20 +0000

    [Test Automation] Correctly handle keyboard click of HoverButton
    Previously, InteractionTestUtilViews sent both key-down and key-up
    events to buttons blindly when asked to press a button.
    
    This works for the vast majority of buttons, but does not work for
    HoverButton in all cases due to the fact that HoverButton performs its
    click action on key down rather than key up.
    
    If the click action dismisses the surface that the HoverButton is on,
    then, the button may no longer exist when the key-up event is sent.
    
    This CL does a check in-between sending the events to verify that the
    target button has not disappeared before trying to send the key-up
    event. It also adds a test that explicitly users HoverButton.
    
    Bug: 1374319
    Change-Id: I275ef0d6f6aa3301d7d1ac9c9e6a193005d99c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3966717
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061744}

--- ui/views/interaction/interaction_test_util_views.cc
+++ ui/views/interaction/interaction_test_util_views.cc
@@ -4,6 +4,7 @@
 
 #include "ui/views/interaction/interaction_test_util_views.h"
 
+#include "base/scoped_observation.h"
 #include "build/build_config.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
@@ -21,6 +22,7 @@
 #include "ui/views/controls/menu/submenu_view.h"
 #include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/view.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/view_utils.h"
 
 namespace views::test {
@@ -32,12 +34,26 @@ InteractionTestUtilSimulatorViews::~InteractionTestUtilSimulatorViews() =
 
 namespace {
 
+// Checks to see if a View has been destroyed. This is necessary because some
+// menu-like buttons can confirm and dismiss on key down rather than on key up,
+// which can in turn result in the view being destroyed between events.
+class ViewDestroyedObserver : public ViewObserver {
+ public:
+  explicit ViewDestroyedObserver(View* view) { observation_.Observe(view); }
+
+  bool is_destroyed() const { return !observation_.IsObserving(); }
+
+ private:
+  void OnViewIsDeleting(View* observed_view) override { observation_.Reset(); }
+
+  base::ScopedObservation<View, ViewObserver> observation_{this};
+};
+
 gfx::Point GetCenter(views::View* view) {
   return view->GetLocalBounds().CenterPoint();
 }
 
-template <class T>
-void SendDefaultAction(T* target) {
+void SendDefaultAction(View* target) {
   ui::AXActionData action;
   action.action = ax::mojom::Action::kDoDefault;
   CHECK(target->HandleAccessibleAction(action));
@@ -79,11 +95,16 @@ void SendTapGesture(T* target, const gfx::Point& point) {
 
 // Sends a key press to the specified `target`.
 void SendKeyPress(View* view, ui::KeyboardCode code) {
+  // Verify that the button is not destroyed after the key-down before trying
+  // to send the key-up.
+  ViewDestroyedObserver observer(view);
   view->OnKeyPressed(ui::KeyEvent(ui::ET_KEY_PRESSED, code, ui::EF_NONE,
                                   ui::EventTimeForNow()));
 
-  view->OnKeyReleased(ui::KeyEvent(ui::ET_KEY_RELEASED, code, ui::EF_NONE,
-                                   ui::EventTimeForNow()));
+  if (!observer.is_destroyed()) {
+    view->OnKeyReleased(ui::KeyEvent(ui::ET_KEY_RELEASED, code, ui::EF_NONE,
+                                     ui::EventTimeForNow()));
+  }
 }
 
 }  // namespace
