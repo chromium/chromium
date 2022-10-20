1869b022ab05473f39ec5d8c6e0eb1f31aa03d86
Daniel Murphy <dmurph@google.com>
2022-10-20 22:20:17 +0000

    [dPWA] Refactoring launches to always be async.
    Context:
    Reading information from the WebAppProvider system is often inherently
    racy (given all of our async data sources). Putting operations behind
    commands guarantees the the data read is not going to change across
    async calls (and that you aren't reading it in the middle of an
    operation like 'install', 'uninstall', etc).
    
    This project protects 'launch' to make sure it doesn't happen during
    and 'uncommitted read' state.
    
    This change was created by using 'git cl split', and is the first part
    in making dPWA launches async. The app service is designed to have
    launches be async (and in lacros they will ALWAYS be async), but
    currently they are "mostly" synchronous. This means that there are a
    variety of tests (and even one instance of production code - the
    intent launcher) which assumes this. This patch fixes all of those
    cases.
    
    Specifically patches split here:
    * Add browser or navigation waiters to wait until launches complete in
      tests.
    * Modifies the method signatures required for making launches async for
      making launches async (but just calls the callback synchronously for
      now.
    * Fixes a future bug for Lacros where the intent navigation closes the
      window before launching the app, causing the browser/profile to start
      shutting down. Keep-alives were added to ensure this does not happen.
    
    There is no functional change in this patch.
    If you want to see a patch with all test changes & functionality
    changes, see:
    https://chromium-review.googlesource.com/c/chromium/src/+/3929017
    
    $description
    
    This CL was uploaded by git cl split.
    
    R=oshima@chromium.org
    
    Bug: b/242059352, 1377102
    Change-Id: I0a6c27b861fb07fd1fb271eb656abd36fdcf835f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969021
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061845}

--- chrome/browser/chromeos/tablet_mode/chrome_content_browser_client_tablet_mode_part_browsertest.cc
+++ chrome/browser/chromeos/tablet_mode/chrome_content_browser_client_tablet_mode_part_browsertest.cc
@@ -10,6 +10,7 @@
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/test/browser_test.h"
@@ -37,7 +38,10 @@ IN_PROC_BROWSER_TEST_F(ChromeContentBrowserClientTabletModePartTest,
 
   // Open the OS settings window.
   auto* settings = chrome::SettingsWindowManager::GetInstance();
+  ui_test_utils::BrowserChangeObserver browser_opened(
+      nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
   settings->ShowOSSettings(profile);
+  browser_opened.Wait();
 
   // The OS settings window still uses the default font sizes.
   Browser* browser = settings->FindBrowserForProfile(profile);
