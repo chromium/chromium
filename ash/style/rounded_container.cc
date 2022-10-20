0fab813506606258df1903667ffa8d16ab1bb5f4
Jiaming Cheng <jiamingc@chromium.org>
2022-10-20 22:32:43 +0000

    Add a revamped container
    A container with the new Jelly color. It supports 4 different styles:
    Top rounded,
    Bottom rounded,
    All rounded,
    Not rounded.
    
    This will be later used in the the revamped list views. For example,
    for the tray detailed view, the same group of entries will be put in
    one container.
    
    One example screenshot:
    https://screenshot.googleplex.com/rEbu3yMxvy9NTon
    
    Bug: b:253091169
    Change-Id: Ie4628cc8ef491a83b68e3f391e2bdc0981d59f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967627
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061854}

---
+++ ash/style/rounded_container.cc
@@ -0,0 +1,64 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/style/rounded_container.h"
+
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/color/color_provider.h"
+#include "ui/compositor/layer.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/background.h"
+#include "ui/views/layout/box_layout.h"
+#include "ui/views/view.h"
+
+namespace ash {
+namespace {
+
+// Rounded radius.
+constexpr int kNonRoundedSideRadius = 4;
+constexpr int kRoundedSideRadius = 16;
+
+}  // namespace
+
+RoundedContainer::RoundedContainer(Behavior corner_behavior)
+    : corner_behavior_(corner_behavior) {
+  SetLayoutManager(std::make_unique<views::BoxLayout>(
+      views::BoxLayout::Orientation::kVertical));
+
+  SetPaintToLayer();
+  layer()->SetRoundedCornerRadius(GetRoundedCorners());
+}
+
+RoundedContainer::~RoundedContainer() = default;
+
+void RoundedContainer::OnThemeChanged() {
+  views::View::OnThemeChanged();
+
+  SkColor background_color =
+      GetColorProvider()->GetColor(cros_tokens::kCrosSysSysOnBase);
+  SetBackground(views::CreateSolidBackground(background_color));
+}
+
+gfx::RoundedCornersF RoundedContainer::GetRoundedCorners() {
+  switch (corner_behavior_) {
+    case Behavior::kNotRounded:
+      return {kNonRoundedSideRadius, kNonRoundedSideRadius,
+              kNonRoundedSideRadius, kNonRoundedSideRadius};
+    case Behavior::kAllRounded:
+      return {kRoundedSideRadius, kRoundedSideRadius, kRoundedSideRadius,
+              kRoundedSideRadius};
+    case Behavior::kTopRounded:
+      return {kRoundedSideRadius, kRoundedSideRadius, kNonRoundedSideRadius,
+              kNonRoundedSideRadius};
+    case Behavior::kBottomRounded:
+      return {kNonRoundedSideRadius, kNonRoundedSideRadius, kRoundedSideRadius,
+              kRoundedSideRadius};
+  }
+}
+
+BEGIN_METADATA(RoundedContainer, views::View);
+END_METADATA
+
+}  // namespace ash
---
+++ ash/style/rounded_container.h
@@ -0,0 +1,43 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_STYLE_ROUNDED_CONTAINER_H_
+#define ASH_STYLE_ROUNDED_CONTAINER_H_
+
+#include "ash/ash_export.h"
+#include "ui/views/view.h"
+
+namespace gfx {
+class RoundedCornersF;
+}  // namespace gfx
+
+namespace ash {
+
+// A rounded countainer which can be used in any list views to carry the items.
+// It provides 4 `Behavior` styles.
+class ASH_EXPORT RoundedContainer : public views::View {
+ public:
+  METADATA_HEADER(RoundedContainer);
+
+  enum class Behavior { kNotRounded, kTopRounded, kBottomRounded, kAllRounded };
+
+  explicit RoundedContainer(Behavior corner_behavior = Behavior::kAllRounded);
+  RoundedContainer(const RoundedContainer& other) = delete;
+  RoundedContainer& operator=(const RoundedContainer& other) = delete;
+  ~RoundedContainer() override;
+
+ private:
+  // views::View:
+  void OnThemeChanged() override;
+
+  // Returns the corners based on the `corner_behavior_`;
+  gfx::RoundedCornersF GetRoundedCorners();
+
+  // The shape of this container. Defaults to `kAllRounded`.
+  Behavior corner_behavior_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_STYLE_ROUNDED_CONTAINER_H_
