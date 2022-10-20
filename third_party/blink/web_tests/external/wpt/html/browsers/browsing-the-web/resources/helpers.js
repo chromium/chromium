2685a5c9a7a46956c34ea77199fd7cbcf65188ba
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-10-20 07:20:40 +0000

    Import wpt@8a68e3423cb7b3ebd833b295e70d3d4231cc67b1
    Using wpt-import in Chromium 34acf9452b9871eace653047a5af68fedac80b33.
    With Chromium commits locally applied on WPT:
    c386df4690 "[Private Network Access] Test targetAddressSpace more thoroughly."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I0150515e8786f07833b60848f13f009b76065780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967850
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1061454}

--- third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/resources/helpers.js
@@ -1,3 +1,12 @@
+// This file contains general helpers for navigation/history tests. The goal is
+// to make tests more imperative and ordered, instead of requiring lots of
+// nested callbacks and jumping back and forth. However,
+// html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
+// might be even better at that, so prefer that when you can.
+//
+// TODO(domenic): consider unifying with
+// overlapping-navigations-and-traversals/resources/helpers.mjs.
+
 window.openWindow = (url, t) => {
   const w = window.open(url);
   t?.add_cleanup(() => w.close());
@@ -17,8 +26,24 @@ window.addIframe = (url = "/common/blank.html", doc = document) => {
   });
 };
 
+window.addSrcdocIframe = async () => {
+  const iframe = document.createElement("iframe");
+  iframe.srcdoc = `<script>window.parent.postMessage("srcdoc ready", "*")</scr` + `ipt>`;
+  document.body.append(iframe);
+
+  assert_equals(await waitForMessage(iframe.contentWindow), "srcdoc ready");
+
+  return iframe;
+};
+
 window.waitToAvoidReplace = t => {
   return new Promise(resolve => t.step_timeout(resolve, 0));
+};
+
+window.waitForIframeLoad = iframe => {
+  return new Promise(resolve => {
+    iframe.addEventListener("load", () => resolve(), { once: true });
+  });
 };
 
 window.waitForMessage = expectedSource => {
@@ -46,4 +71,14 @@ window.srcdocThatPostsParentOpener = text => {
       };
     <\/script>
   `;
+};
+
+window.failOnMessage = expectedSource => {
+  return new Promise((_, reject) => {
+    window.addEventListener("message", ({ source, data }) => {
+      if (source === expectedSource) {
+        reject(new Error(`Received message "${data}" but expected to receive no message`));
+      }
+    });
+  });
 };
