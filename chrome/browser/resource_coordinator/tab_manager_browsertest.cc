e3b671b1623a519f31f3a4a59adf7ffd50eb0149
Kevin McNee <mcnee@chromium.org>
2022-10-21 21:25:22 +0000

    Migrate TabManagerTest off of NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME
    We switch to using other test helpers: TestNavigationObserver for
    navigating existing WebContents and WaitForLoadStop for waiting for
    the load of a newly created WebContents.
    
    Technically, these wait for the entire WebContents to load instead of
    just the primary main frame, but the distinction does not seem to
    matter for these tests.
    
    Bug: 1174761
    Change-Id: I13003500426223f0d48517030f2f851e973ecf4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964611
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062358}

--- chrome/browser/resource_coordinator/tab_manager_browsertest.cc
+++ chrome/browser/resource_coordinator/tab_manager_browsertest.cc
@@ -161,23 +161,18 @@ class TabManagerTest : public InProcessBrowserTest {
 
   void OpenTwoTabs(const GURL& first_url, const GURL& second_url) {
     // Open two tabs. Wait for both of them to load.
-    content::WindowedNotificationObserver load1(
-        content::NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME,
-        content::NotificationService::AllSources());
+    content::TestNavigationObserver load1(tsm()->GetActiveWebContents(), 1);
     OpenURLParams open1(first_url, content::Referrer(),
                         WindowOpenDisposition::CURRENT_TAB,
                         ui::PAGE_TRANSITION_TYPED, false);
     browser()->OpenURL(open1);
     load1.Wait();
 
-    content::WindowedNotificationObserver load2(
-        content::NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME,
-        content::NotificationService::AllSources());
     OpenURLParams open2(second_url, content::Referrer(),
                         WindowOpenDisposition::NEW_BACKGROUND_TAB,
                         ui::PAGE_TRANSITION_TYPED, false);
-    browser()->OpenURL(open2);
-    load2.Wait();
+    auto* tab2 = browser()->OpenURL(open2);
+    content::WaitForLoadStop(tab2);
 
     ASSERT_EQ(2, tsm()->count());
   }
