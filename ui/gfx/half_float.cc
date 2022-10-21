802fc7d2e7446a40532825fe46181df939743c50
Bruno Pitrus <brunopitrus@hotmail.com>
2022-10-21 03:55:00 +0000

    Correct invalid type pun
    `uint32_t` is not allowed to alias a `float` in C++.
    
    Change-Id: I8ab65ef66f163d8d23a9cc20dba44a3c213f392b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3968579
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1061990}

--- ui/gfx/half_float.cc
+++ ui/gfx/half_float.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstring>
+
 #include "ui/gfx/half_float.h"
 
 namespace gfx {
@@ -9,7 +11,9 @@ namespace gfx {
 void FloatToHalfFloat(const float* input, HalfFloat* output, size_t num) {
   for (size_t i = 0; i < num; i++) {
     float tmp = input[i] * 1.9259299444e-34f;
-    uint32_t tmp2 = *reinterpret_cast<uint32_t*>(&tmp) + (1 << 12);
+    uint32_t tmp2;
+    std::memcpy(&tmp2, &tmp, 4);
+    tmp2 += (1 << 12);
     output[i] = (tmp2 & 0x80000000UL) >> 16 | (tmp2 >> 13);
   }
 }
