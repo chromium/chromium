b4d406b98ec5b0e9d61e45f230b592a0bb708bf4
Sorin Jianu <sorin@chromium.org>
2022-10-21 13:32:03 +0000

    Implement a download test using the updater network fetcher.
    This provides a hint about the download throughput when running the
    tests on bots.
    
    In addition, the test checks that the network fetcher is not blocking
    its sequence.
    
    Bug: 1377176,1376671
    Change-Id: I5ce81c720f1ecc9e8fcdffa8d15109848b35ff18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969569
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062102}

--- chrome/updater/win/net/network_unittest.cc
+++ chrome/updater/win/net/network_unittest.cc
@@ -13,10 +13,13 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/functional/bind.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/path_service.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/task_environment.h"
+#include "base/threading/thread_restrictions.h"
 #include "chrome/updater/policy/service.h"
 #include "chrome/updater/unittest_util.h"
 #include "chrome/updater/win/net/network_fetcher.h"
@@ -25,37 +28,68 @@
 #include "url/gurl.h"
 
 namespace updater {
+namespace {
 
-TEST(UpdaterTestNetwork, NetworkFetcherWinHTTPFactory) {
-  base::test::SingleThreadTaskEnvironment task_environment(
-      base::test::SingleThreadTaskEnvironment::MainThreadType::UI);
-  std::unique_ptr<update_client::NetworkFetcher> fetcher =
-      base::MakeRefCounted<NetworkFetcherFactory>(
-          PolicyServiceProxyConfiguration::Get(test::CreateTestPolicyService()))
-          ->Create();
-  EXPECT_NE(fetcher, nullptr);
-}
+// Sets up a download for "chrome/updater/test/data/signed.exe" using the
+// embedded test server running on localhost.
+class UpdaterDownloadTest : public ::testing::Test {
+ protected:
+  ~UpdaterDownloadTest() override = default;
+
+  base::test::TaskEnvironment task_environment_;
+  net::test_server::EmbeddedTestServer server_;
+  net::test_server::EmbeddedTestServerHandle server_handle_;
+  base::ScopedTempDir scoped_dir_;
+  base::FilePath dest_;
+  GURL gurl_;
+
+ private:
+  void SetUp() override {
+    server_.ServeFilesFromSourceDirectory("chrome/updater/test/data");
+    server_handle_ = server_.StartAndReturnHandle();
+    ASSERT_TRUE(scoped_dir_.CreateUniqueTempDir());
+    dest_ = scoped_dir_.GetPath().AppendASCII("updater-signed.exe");
+    gurl_ = GURL(base::StrCat({server_.base_url().spec(), "signed.exe"}));
+  }
+};
+
+}  // namespace
 
 // Tests that a direct download through URL moniker from a local HTTP server is
 // reasonably fast. This provides a baseline to compare the network throughput
 // of various fetchers.
-TEST(UpdaterTestNetwork, URLMonFetcher) {
-  base::test::SingleThreadTaskEnvironment task_environment(
-      base::test::SingleThreadTaskEnvironment::MainThreadType::UI);
-  net::test_server::EmbeddedTestServer server;
-  server.ServeFilesFromSourceDirectory("chrome/updater/test/data");
-  net::test_server::EmbeddedTestServerHandle server_handle =
-      server.StartAndReturnHandle();
-  base::ScopedTempDir scoped_dir;
-  ASSERT_TRUE(scoped_dir.CreateUniqueTempDir());
-  const base::FilePath dest =
-      scoped_dir.GetPath().AppendASCII("updater-signed.exe");
-  EXPECT_HRESULT_SUCCEEDED(::URLDownloadToFile(
-      nullptr,
-      base::ASCIIToWide(base::StrCat({server.base_url().spec(), "signed.exe"}))
-          .c_str(),
-      dest.value().c_str(), 0, nullptr));
-  EXPECT_TRUE(base::PathExists(dest));
+TEST_F(UpdaterDownloadTest, URLMonFetcher) {
+  EXPECT_FALSE(base::PathExists(dest_));
+  EXPECT_HRESULT_SUCCEEDED(
+      ::URLDownloadToFile(nullptr, base::ASCIIToWide(gurl_.spec()).c_str(),
+                          dest_.value().c_str(), 0, nullptr));
+  EXPECT_TRUE(base::PathExists(dest_));
+}
+
+TEST_F(UpdaterDownloadTest, NetworkFetcher) {
+  EXPECT_FALSE(base::PathExists(dest_));
+  {
+    // The fetcher must not block the sequence when downloading.
+    base::ScopedDisallowBlocking no_blocking_allowed_on_sequence;
+
+    base::RunLoop run_loop;
+    auto factory = base::MakeRefCounted<NetworkFetcherFactory>(
+        PolicyServiceProxyConfiguration::Get(test::CreateTestPolicyService()));
+    ASSERT_NE(factory, nullptr);
+    std::unique_ptr<update_client::NetworkFetcher> fetcher = factory->Create();
+    ASSERT_NE(fetcher, nullptr);
+    fetcher->DownloadToFile(
+        gurl_, dest_,
+        base::BindOnce([](int response_code, int64_t /*content_length*/) {
+          EXPECT_EQ(response_code, 200);
+        }),
+        base::BindRepeating([](int64_t /*current*/) {}),
+        base::BindOnce([](int net_error, int64_t content_size) {
+          EXPECT_EQ(net_error, 0);
+        }).Then(run_loop.QuitClosure()));
+    run_loop.Run();
+  }
+  EXPECT_TRUE(base::PathExists(dest_));
 }
 
 }  // namespace updater
