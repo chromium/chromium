eea0292e6b3afbccc69d8ae3b7c8f4f86ff50e4a
Jungshik Shin <jshin@chromium.org>
2022-10-21 11:33:24 +0000

    Fix PRESUBMIT in chrome/test/data/policy
    PRESUBMIT.py in chrome/test/data/policy calls PRESUBMIT
    in components/policy/resources/policy_templates.json which
    relies on module, policy_templates in the directory. So the
    directory has to be added to sys.path.
    
    This issue was introduced in https://chromium-review.googlesource.com/c/chromium/src/+/3909773 .
    
    Bug: None
    Test: Manual; add a change in c/t/data/policy and run 'git cl presubmit'
    Change-Id: Id053342d403e06e79f1a85f487d1aec9739c0ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3969351
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062045}

--- chrome/test/data/policy/PRESUBMIT.py
+++ chrome/test/data/policy/PRESUBMIT.py
@@ -5,6 +5,8 @@
 USE_PYTHON3 = True
 
 import os
+import sys
+
 
 def RunOtherPresubmit(function_name, input_api, output_api):
   # Apply the PRESUBMIT for components/policy/resources to run the syntax check
@@ -15,8 +17,15 @@ def RunOtherPresubmit(function_name, input_api, output_api):
          for changed_file in input_api.change.AffectedFiles()):
     return []
 
-  presubmit_path = os.path.join(input_api.change.RepositoryRoot(),
-                                component_resources_path, 'PRESUBMIT.py')
+  # need to add components/policy/resources because PRESUBMIT.py there
+  # imports policy_templates module in the directory.
+  component_resources_path_absolute = os.path.join(
+      input_api.change.RepositoryRoot(), component_resources_path)
+  if (component_resources_path_absolute not in sys.path):
+    sys.path.append(component_resources_path_absolute)
+
+  presubmit_path = os.path.join(component_resources_path_absolute,
+                                'PRESUBMIT.py')
 
   presubmit_content = input_api.ReadFile(presubmit_path)
   global_vars = {}
