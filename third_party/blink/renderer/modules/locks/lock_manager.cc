a837b63a1861e5a52525f7751d872ba3f2f5f401
Scott Haseley <shaseley@chromium.org>
2022-10-21 17:57:02 +0000

    AbortSignal: explicit abort algorithm lifetime in LockManager
    This is a follow-up to crrev.com/c/3931192 which added
    AbortSignal::AlgorithmHandle for explicit abort algorithm lifetime
    management.
    
    This CL ties the lifetime of a lock request's abort algorithm to the
    lifetime of the request, explicitly clearing the handle when the abort
    will no longer have effect (on lock granted or failed).
    
    Bug: 1296280, 1323391
    Change-Id: Ic9e3ea1bba8ab21d7801a3f4ee54879b5ebedcb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3964957
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062229}

--- third_party/blink/renderer/modules/locks/lock_manager.cc
+++ third_party/blink/renderer/modules/locks/lock_manager.cc
@@ -98,6 +98,7 @@ class LockManager::LockRequestImpl final
     visitor->Trace(manager_);
     visitor->Trace(callback_);
     visitor->Trace(receiver_);
+    visitor->Trace(abort_handle_);
   }
 
   const char* NameInHeapSnapshot() const override {
@@ -108,6 +109,11 @@ class LockManager::LockRequestImpl final
   // unblocking further requests, without waiting for GC finalize the object.
   void Cancel() { receiver_.reset(); }
 
+  void InitializeAbortAlgorithm(AbortSignal::AlgorithmHandle& handle) {
+    DCHECK(!abort_handle_);
+    abort_handle_ = &handle;
+  }
+
   void Abort(const String& reason) override {
     // Abort signal after acquisition should be ignored.
     if (!manager_->IsPendingRequest(this))
@@ -115,6 +121,7 @@ class LockManager::LockRequestImpl final
 
     manager_->RemovePendingRequest(this);
     receiver_.reset();
+    abort_handle_.Clear();
 
     DCHECK(resolver_);
 
@@ -136,6 +143,7 @@ class LockManager::LockRequestImpl final
 
     manager_->RemovePendingRequest(this);
     receiver_.reset();
+    abort_handle_.Clear();
 
     ScriptState* script_state = resolver_->GetScriptState();
     if (!script_state->ContextIsValid())
@@ -161,6 +169,7 @@ class LockManager::LockRequestImpl final
 
     manager_->RemovePendingRequest(this);
     receiver_.reset();
+    abort_handle_.Clear();
 
     ScriptState* script_state = resolver_->GetScriptState();
     if (!script_state->ContextIsValid()) {
@@ -216,6 +225,10 @@ class LockManager::LockRequestImpl final
   // registered. If the context is destroyed then |manager_| will dispose of
   // |this| which terminates the request on the service side.
   Member<LockManager> manager_;
+
+  // Handle that keeps the associated abort algorithm alive for the duration of
+  // the request.
+  Member<AbortSignal::AlgorithmHandle> abort_handle_;
 };
 
 const char LockManager::kSupplementName[] = "LockManager";
@@ -406,9 +419,10 @@ void LockManager::RequestImpl(ScriptPromiseResolver* resolver,
     // 11.2.1. Enqueue the steps to abort the request request to the lock task
     // queue.
     // 11.2.2. Reject promise with an "AbortError" DOMException.
-    options->signal()->AddAlgorithm(
+    AbortSignal::AlgorithmHandle* handle = options->signal()->AddAlgorithm(
         WTF::BindOnce(&LockRequestImpl::Abort, WrapWeakPersistent(request),
                       String(kRequestAbortedMessage)));
+    request->InitializeAbortAlgorithm(*handle);
   }
   service_->RequestLock(name, mode, wait, std::move(request_remote));
 }
