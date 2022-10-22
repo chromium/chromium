11def35f1d924627e3e53cabf79c4afcf76f2b7f
Caleb Raitto <caraitto@chromium.org>
2022-10-22 00:28:57 +0000

    Add a fuzzer for base/strings/escape.h
    See discussion in [0].
    
    [0]
    https://groups.google.com/a/chromium.org/g/security-dev/c/6WdWuexfBto
    
    Bug: 1320908
    Change-Id: I3aa0588598e35e51d5f66899129825d4ded41309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3925791
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1062430}

---
+++ base/strings/escape_fuzzer.cc
@@ -0,0 +1,26 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/escape.h"
+
+// Prevent the optimizer from optimizing away a function call by "using" the
+// result.
+//
+// TODO(crbug.com/1377534): Replace this with a more general solution.
+void UseResult(const std::string& input) {
+  volatile char c;
+  if (input.length() > 0)
+    c = input[0];
+  (void)c;
+}
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::StringPiece data_string(reinterpret_cast<const char*>(data), size);
+
+  UseResult(base::EscapeQueryParamValue(data_string, /*use_plus=*/false));
+  UseResult(base::EscapeQueryParamValue(data_string, /*use_plus=*/true));
+
+  return 0;
+}
