3ebbe37c66bcb5f6712a41199563e9b311b92a47
Bastian Kersting <bkersting@google.com>
2022-10-24 13:58:43 +0000

    system-extensions: Add initial Telemetry interface
    This CL adds a new interface to the Blink mojom API surface:
    the CrosTelemetry interface.
    
    This CL is part of a bigger effort that enables APIs for the
    Managed Device Health Services SysX type, for more information
    on this please see (internal only):
    go/dd-managed-devices-health-services-apis.
    
    Bug: b/253175827, b/250779561
    Test: browser_tests --gtest_filter="ManagedDeviceHealthServicesBrowserTest*"
    Change-Id: I5f3c0232eec525e8d521ef829e354fe072f8d441
    Low-Coverage-Reason: This is just skeleton code and part of a bigger effort. This code will get tested in a later CL.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3955835
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1062789}

--- third_party/blink/renderer/extensions/chromeos/system_extensions/managed_device_health_services/telemetry/cros_telemetry.cc
+++ third_party/blink/renderer/extensions/chromeos/system_extensions/managed_device_health_services/telemetry/cros_telemetry.cc
@@ -1,8 +1,10 @@
-// Copyright 2022 The ChromiumOS Authors
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/extensions/chromeos/system_extensions/managed_device_health_services/telemetry/cros_telemetry.h"
+
+#include <utility>
 
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
@@ -24,12 +26,30 @@ CrosTelemetry& CrosTelemetry::From(ExecutionContext& execution_context) {
 
 CrosTelemetry::CrosTelemetry(ExecutionContext& execution_context)
     : Supplement(execution_context),
-      ExecutionContextClient(&execution_context) {}
+      ExecutionContextClient(&execution_context),
+      remote_telemetry_service_(&execution_context) {}
 
 void CrosTelemetry::Trace(Visitor* visitor) const {
   Supplement<ExecutionContext>::Trace(visitor);
   ExecutionContextClient::Trace(visitor);
   ScriptWrappable::Trace(visitor);
+  visitor->Trace(remote_telemetry_service_);
+}
+
+mojom::blink::CrosTelemetry* CrosTelemetry::GetCrosTelemetryServiceOrNull() {
+  auto* execution_context = GetExecutionContext();
+  if (!execution_context) {
+    return nullptr;
+  }
+
+  if (!remote_telemetry_service_.is_bound()) {
+    auto receiver = remote_telemetry_service_.BindNewPipeAndPassReceiver(
+        execution_context->GetTaskRunner(TaskType::kMiscPlatformAPI));
+    execution_context->GetBrowserInterfaceBroker().GetInterface(
+        std::move(receiver));
+  }
+
+  return remote_telemetry_service_.get();
 }
 
 }  // namespace blink
