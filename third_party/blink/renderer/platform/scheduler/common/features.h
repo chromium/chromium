355b9ece07a405ea3290bb23725f3c8820dc9d66
Dave Tapuska <dtapuska@chromium.org>
2022-10-25 21:24:47 +0000

    Enable Microtask queue per Animation Worklet.
    Now that all microtasks queued are associated with the context and not
    the default queue. Enable animation worklet having its own microtask
    queue.
    
    BUG=1074769
    
    Change-Id: Ib97460d9da1a3c7b7e629b4bfcdc47f5d0f569bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3979893
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063500}

--- third_party/blink/renderer/platform/scheduler/common/features.h
+++ third_party/blink/renderer/platform/scheduler/common/features.h
@@ -181,7 +181,7 @@ BASE_FEATURE(kMicrotaskQueuePerPaintWorklet,
 
 BASE_FEATURE(kMicrotaskQueuePerAnimationWorklet,
              "BlinkSchedulerMicroTaskQueuePerAnimationWorklet",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMicrotaskQueuePerAudioWorklet,
              "BlinkSchedulerMicroTaskQueuePerAudioWorklet",
