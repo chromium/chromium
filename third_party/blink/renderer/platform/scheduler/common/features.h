ba1af8ade4303a565cc03471f0cc376bc9a50e6d
Dave Tapuska <dtapuska@chromium.org>
2022-10-25 18:53:26 +0000

    Enable Microtask queue per Paint Worklet.
    Now that all microtasks queued are associated with the context and not
    the default queue. Enable paint worklet having its own microtask queue.
    
    BUG=1074769
    
    Change-Id: Ib10aad07b411468445f4c6335874e6ba28b99710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975449
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063422}

--- third_party/blink/renderer/platform/scheduler/common/features.h
+++ third_party/blink/renderer/platform/scheduler/common/features.h
@@ -177,7 +177,7 @@ BASE_FEATURE(kMicrotaskQueuePerWindowAgent,
 
 BASE_FEATURE(kMicrotaskQueuePerPaintWorklet,
              "BlinkSchedulerMicroTaskQueuePerPaintWorklet",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMicrotaskQueuePerAnimationWorklet,
              "BlinkSchedulerMicroTaskQueuePerAnimationWorklet",
