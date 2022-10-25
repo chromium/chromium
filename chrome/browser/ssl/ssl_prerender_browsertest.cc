e81b3a46c055cf705cfb1f4a622f8742fc1d7a9c
Henrique Ferreiro <hferreiro@igalia.com>
2022-10-25 11:14:14 +0000

    prerendering: cancel for insecure forms submissions
    Even though form submissions don't include user data (a prerender
    cannot provide any input), the prerendered form submission could still
    leak data over the network (e.g. the path).
    
    There's an exception to this: Reloading a GET form will proceed since
    those submissions are not marked as form submissions on reloads, and
    the only way to detect them is to check the InsecureFormTabStorage, to
    check if the navigation is coming from another mixed form
    interstitial. But this object is stored on the WebContents, so a
    prerender navigation should not use it.
    
    Bug: 1362520
    Change-Id: Iae18def029be3a2830376ced92bac432c51f8904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3810375
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1063239}

--- chrome/browser/ssl/ssl_prerender_browsertest.cc
+++ chrome/browser/ssl/ssl_prerender_browsertest.cc
@@ -6,19 +6,27 @@
 
 #include "base/bind.h"
 #include "base/files/file_path.h"
+#include "base/strings/string_number_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/interstitials/security_interstitial_page_test_utils.h"
+#include "chrome/browser/preloading/prerender/prerender_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/security_interstitials/content/security_interstitial_page.h"
 #include "components/security_interstitials/content/security_interstitial_tab_helper.h"
+#include "components/security_interstitials/core/controller_client.h"
+#include "content/public/browser/prerender_handle.h"
+#include "content/public/browser/prerender_trigger_type.h"
+#include "content/public/browser/reload_type.h"
 #include "content/public/browser/ssl_host_state_delegate.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/prerender_test_util.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/url_loader_interceptor.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/cert_test_util.h"
@@ -292,6 +300,95 @@ IN_PROC_BROWSER_TEST_F(SSLPrerenderTest,
             tab);
     EXPECT_FALSE(helper);
     histograms.ExpectTotalCount(kHistogramName, 0);
+  }
+}
+
+// Prerenders a page that tries to submit an insecure form and checks that this
+// cancels the prerender even if the primary page is proceeding on an insecure
+// form.
+IN_PROC_BROWSER_TEST_F(SSLPrerenderTest,
+                       InsecureFormSubmissionCancelsPrerenderEvenIfProceeding) {
+  base::HistogramTester histograms;
+  const std::string kHistogramName =
+      "Security.MixedForm.InterstitialTriggerState";
+
+  // Histogram should start off empty.
+  histograms.ExpectTotalCount(kHistogramName, 0);
+
+  auto https_server = CreateHTTPSServer(GetChromeTestDataDir());
+  ASSERT_TRUE(https_server->Start());
+
+  // Add a "replace_text=" query param that the test server will use to replace
+  // the string "REPLACE_WITH_HOST_AND_PORT" in the destination page.
+  net::HostPortPair host_port_pair =
+      net::HostPortPair::FromURL(https_server->GetURL("a.test", "/"));
+  std::string replacement_path = GetFilePathWithHostAndPortReplacement(
+      "/ssl/page_displays_insecure_form.html",
+      embedded_test_server()->host_port_pair());
+
+  // Use "a.test" since the default host is 127.0.0.1 and that's considered a
+  // "potentially trustworthy origin" by the throttle so navigation won't
+  // trigger the throttle.
+  const GURL kUrl = https_server->GetURL("a.test", replacement_path);
+
+  // Test steps
+  {
+    ASSERT_TRUE(NavigateToURL(browser(), kUrl));
+
+    // Submit a form targeting an insecure URL.
+    content::TestNavigationObserver nav_observer(web_contents(), 1);
+    ASSERT_TRUE(ExecJs(web_contents(), "submitForm();"));
+    nav_observer.Wait();
+    security_interstitials::SecurityInterstitialTabHelper* helper =
+        security_interstitials::SecurityInterstitialTabHelper::FromWebContents(
+            web_contents());
+    ASSERT_TRUE(helper);
+    EXPECT_TRUE(helper->IsDisplayingInterstitial());
+    histograms.ExpectTotalCount(kHistogramName, 1);
+
+    // Prerender the same insecure form.
+    std::unique_ptr<content::PrerenderHandle> prerender_handle =
+        web_contents()->StartPrerendering(
+            kUrl, content::PrerenderTriggerType::kEmbedder,
+            prerender_utils::kDirectUrlInputMetricSuffix,
+            ui::PageTransitionFromInt(ui::PAGE_TRANSITION_TYPED |
+                                      ui::PAGE_TRANSITION_FROM_ADDRESS_BAR),
+            nullptr);
+    ASSERT_TRUE(prerender_handle);
+    const int kPrerenderHostId = prerender_helper_.GetHostForUrl(kUrl);
+    ASSERT_NE(kPrerenderHostId, content::RenderFrameHost::kNoFrameTreeNodeId);
+    prerender_helper_.WaitForPrerenderLoadCompletion(kPrerenderHostId);
+
+    // Proceed with the interstitial page in the primary page.
+    content::TestNavigationObserver nav_observer2(web_contents(), 1);
+    helper =
+        security_interstitials::SecurityInterstitialTabHelper::FromWebContents(
+            web_contents());
+    helper->GetBlockingPageForCurrentlyCommittedNavigationForTesting()
+        ->CommandReceived(
+            base::NumberToString(security_interstitials::CMD_PROCEED));
+    nav_observer2.Wait();
+    ASSERT_TRUE(helper);
+    EXPECT_FALSE(helper->IsDisplayingInterstitial());
+
+    // Submit the prerendered form. The prerender should be destroyed.
+    PrerenderHostObserver host_observer(*web_contents(), kPrerenderHostId);
+    ASSERT_TRUE(
+        ExecJs(prerender_helper_.GetPrerenderedMainFrameHost(kPrerenderHostId),
+               "submitForm();"));
+    host_observer.WaitForDestroyed();
+
+    // The prerender navigation should be canceled as part of the response.
+    // Ensure the prerender host is destroyed, no interstitial is showing, and
+    // we didn't affect the relevant metric.
+    EXPECT_EQ(prerender_helper_.GetHostForUrl(kUrl),
+              RenderFrameHost::kNoFrameTreeNodeId);
+    helper =
+        security_interstitials::SecurityInterstitialTabHelper::FromWebContents(
+            web_contents());
+    ASSERT_TRUE(helper);
+    EXPECT_FALSE(helper->IsDisplayingInterstitial());
+    histograms.ExpectTotalCount(kHistogramName, 1);
   }
 }
 
--- components/security_interstitials/content/insecure_form_navigation_throttle.cc
+++ components/security_interstitials/content/insecure_form_navigation_throttle.cc
@@ -61,11 +61,11 @@ InsecureFormNavigationThrottle::WillRedirectRequest() {
 
 content::NavigationThrottle::ThrottleCheckResult
 InsecureFormNavigationThrottle::WillProcessResponse() {
-  // If there is an InsecureFormTabStorage associated to |web_contents_|, clear
-  // the IsProceeding flag.
+  // If there is an InsecureFormTabStorage associated to `web_contents_`, clear
+  // the IsProceeding flag, except when prerendering.
   InsecureFormTabStorage* tab_storage = InsecureFormTabStorage::FromWebContents(
       navigation_handle()->GetWebContents());
-  if (tab_storage) {
+  if (tab_storage && !navigation_handle()->IsInPrerenderedMainFrame()) {
     tab_storage->SetIsProceeding(false);
     tab_storage->SetInterstitialShown(false);
   }
@@ -100,9 +100,20 @@ InsecureFormNavigationThrottle::GetThrottleResultForMixedForm(
   // submissions are not marked as form submissions on reloads, so we check if
   // this navigation is coming from another mixed form interstitial.
   if (!handle->IsFormSubmission() &&
-      (!tab_storage || !tab_storage->InterstitialShown())) {
+      (handle->IsInPrerenderedMainFrame() ||
+       (!tab_storage || !tab_storage->InterstitialShown()))) {
     return content::NavigationThrottle::PROCEED;
   }
+
+  // If the form is in a prerendered page, cancel it. Even though the form
+  // submission wouldn't include user data (a prerender cannot provide any
+  // input), the prerendered form submission could still leak data over the
+  // network (e.g. the path).
+  // There's an exception to this: Reloading a GET form will proceed since a
+  // prerender shouldn't check the InsecureFormTabStorage, which is a per-tab
+  // object. This is done in the check above.
+  if (handle->IsInPrerenderedMainFrame())
+    return content::NavigationThrottle::CANCEL;
 
   // If user has just chosen to proceed on an interstitial, we don't show
   // another one.
@@ -144,11 +155,6 @@ InsecureFormNavigationThrottle::GetThrottleResultForMixedForm(
   if (should_proceed) {
     LogMixedFormInterstitialMetrics(log_state);
     return content::NavigationThrottle::PROCEED;
-  } else if (handle->IsInPrerenderedMainFrame()) {
-    // If we're prerendered, avoid logging any metrics or showing an
-    // interstitial if the prerender will be canceled. This will cancel the
-    // form navigation as well as the prerender.
-    return content::NavigationThrottle::CANCEL;
   }
 
   LogMixedFormInterstitialMetrics(log_state);
