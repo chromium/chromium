a883a9493cd728e60c5b9f9398f0e9b9cac89ba9
Kevin McNee <mcnee@chromium.org>
2022-10-26 08:29:39 +0000

    Migrate PermissionRequestFromExtension off of NotificationService
    We convert the use of NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME to the
    test helper CreateAndLoadWebContentsObserver. Technically, this waits
    for the entire WebContents to load instead of just the primary main
    frame, but the distinction does not seem to matter for these tests.
    
    Bug: 1174761
    Change-Id: I86bbff11154f0f16499757a631f9587c7a593f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978909
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063694}

--- chrome/browser/permissions/permissions_security_model_interactive_uitest.cc
+++ chrome/browser/permissions/permissions_security_model_interactive_uitest.cc
@@ -24,7 +24,6 @@
 #include "components/permissions/test/mock_permission_prompt_factory.h"
 #include "components/permissions/test/permission_request_observer.h"
 #include "content/public/browser/disallow_activation_reason.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/permission_controller.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
@@ -1784,16 +1783,11 @@ class PermissionRequestFromExtension : public extensions::ExtensionApiTest {
   // with `id`.
   content::WebContents* OpenPopupViaToolbar(const std::string& id) {
     EXPECT_FALSE(id.empty());
-    content::WindowedNotificationObserver popup_observer(
-        content::NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME,
-        content::NotificationService::AllSources());
+    content::CreateAndLoadWebContentsObserver popup_observer;
     ExtensionActionTestHelper::Create(browser())->Press(id);
-    popup_observer.Wait();
+    content::WebContents* popup = popup_observer.Wait();
     EnsurePopupActive();
-    const auto& source =
-        static_cast<const content::Source<content::WebContents>&>(
-            popup_observer.source());
-    return source.ptr();
+    return popup;
   }
 
   void VerifyExtensionsPopupPage(std::string extension_path) {
