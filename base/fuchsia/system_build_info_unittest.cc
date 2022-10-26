4032e2b43bc98104075fa62bad84dde5019b81f3
Fabrice de Gans <fdegans@chromium.org>
2022-10-26 23:27:16 +0000

    Revert "[Fuchsia] Enable tests in system_build_info_unittest"
    This reverts commit efc3bd0579a533985cc9dd079bf6bd9266c9a80d.
    
    Reason for revert: Will break Fuchsia bots
    
    Original change's description:
    > [Fuchsia] Enable tests in system_build_info_unittest
    >
    > They are consistently passing on fuchsia-fyi-x64-asan and fuchsia-fyi-arm64-dbg.
    >
    > This is part of the effort to re-enable disabled tests on fuchsia
    > builders in Chromium. You may get the list of passing tests in the
    > snapshots in http://dr/corp/drive/folders/1YLEHxPZ9CAiXFggxLCuTOz6WcDT4XAoN.
    >
    > Bug: 1326674
    > Fixed: 1326674
    > Change-Id: I4e4d4f22487f7a07ab92aabd55c244a0a12f309e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3979431
    > Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    > Commit-Queue: Zijie He <zijiehe@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1064061}
    
    Bug: 1326674
    Change-Id: I622897d4eea8928098afc67e184b3450b8d148c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984221
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064074}

--- base/fuchsia/system_build_info_unittest.cc
+++ base/fuchsia/system_build_info_unittest.cc
@@ -11,8 +11,11 @@ namespace base {
 
 // Ensures that when FetchAndCacheSystemInfo() has not been called in the
 // process that a  DCHECK fires to alert the developer.
+//
+// TODO(crbug.com/1326674) Ensure the test passes on Fuchsia bots and
+// re-enable.
 TEST(BuildInfoDeathTest,
-     GetCachedBuildInfo_DcheckIfNotAlreadyFetched) {
+     DISABLED_GetCachedBuildInfo_DcheckIfNotAlreadyFetched) {
   // Clear the cached build info to force an error condition.
   ClearCachedSystemInfoForTesting();
 
@@ -26,7 +29,9 @@ TEST(BuildInfoDeathTest,
   FetchAndCacheSystemInfo();
 }
 
-TEST(BuildInfoTest, GetCachedBuildInfo_CheckExpectedValues) {
+// TODO(crbug.com/1326674) Ensure the test passes on Fuchsia bots and
+// re-enable.
+TEST(BuildInfoTest, DISABLED_GetCachedBuildInfo_CheckExpectedValues) {
   // Ensure the cached BuildInfo is in a known state.
   ClearCachedSystemInfoForTesting();
   FetchAndCacheSystemInfo();
