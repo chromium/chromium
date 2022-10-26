380b4eb0ed98c06ba2410323dcf69c5262f09412
Harald Alvestrand <hta@chromium.org>
2022-10-26 15:15:38 +0000

    Use webrtc mocks for RtpSender and RtpReceiver
    Bug: webrtc:14594
    Change-Id: I90dfe5ed1065a2975d6f71616c43fdec4a3e1cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3981835
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063805}

--- third_party/blink/renderer/modules/peerconnection/testing/mock_rtp_sender.h
+++ third_party/blink/renderer/modules/peerconnection/testing/mock_rtp_sender.h
@@ -5,65 +5,17 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_TESTING_MOCK_RTP_SENDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_TESTING_MOCK_RTP_SENDER_H_
 
-#include <string>
-#include <vector>
-
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
 #include "third_party/webrtc/api/rtp_parameters.h"
 #include "third_party/webrtc/api/rtp_sender_interface.h"
 #include "third_party/webrtc/api/scoped_refptr.h"
+#include "third_party/webrtc/api/test/mock_rtpsender.h"
 #include "third_party/webrtc/rtc_base/ref_count.h"
 
 namespace blink {
 
-class MockRtpSender : public rtc::RefCountedObject<webrtc::RtpSenderInterface> {
- public:
-  MOCK_METHOD(bool, SetTrack, (webrtc::MediaStreamTrackInterface*), (override));
-  MOCK_METHOD(rtc::scoped_refptr<webrtc::MediaStreamTrackInterface>,
-              track,
-              (),
-              (const override));
-  MOCK_METHOD(rtc::scoped_refptr<webrtc::DtlsTransportInterface>,
-              dtls_transport,
-              (),
-              (const override));
-  MOCK_METHOD(uint32_t, ssrc, (), (const override));
-  MOCK_METHOD(cricket::MediaType, media_type, (), (const override));
-  MOCK_METHOD(std::string, id, (), (const override));
-  MOCK_METHOD(std::vector<std::string>, stream_ids, (), (const override));
-  MOCK_METHOD(void, SetStreams, (const std::vector<std::string>&), (override));
-  MOCK_METHOD(std::vector<webrtc::RtpEncodingParameters>,
-              init_send_encodings,
-              (),
-              (const override));
-  MOCK_METHOD(webrtc::RtpParameters, GetParameters, (), (const override));
-  MOCK_METHOD(webrtc::RTCError,
-              SetParameters,
-              (const webrtc::RtpParameters&),
-              (override));
-  MOCK_METHOD(rtc::scoped_refptr<webrtc::DtmfSenderInterface>,
-              GetDtmfSender,
-              (),
-              (const override));
-  MOCK_METHOD(void,
-              SetFrameEncryptor,
-              (rtc::scoped_refptr<webrtc::FrameEncryptorInterface>),
-              (override));
-  MOCK_METHOD(rtc::scoped_refptr<webrtc::FrameEncryptorInterface>,
-              GetFrameEncryptor,
-              (),
-              (const, override));
-  MOCK_METHOD(void,
-              SetEncoderToPacketizerFrameTransformer,
-              (rtc::scoped_refptr<webrtc::FrameTransformerInterface>),
-              (override));
-  MOCK_METHOD(
-      void,
-      SetEncoderSelector,
-      (std::unique_ptr<webrtc::VideoEncoderFactory::EncoderSelectorInterface>),
-      (override));
-};
+class MockRtpSender : public rtc::RefCountedObject<webrtc::MockRtpSender> {};
 
 }  // namespace blink
 
