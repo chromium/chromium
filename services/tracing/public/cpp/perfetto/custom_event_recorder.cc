e270ac8c22d8d1aebcc811b0ea4f49af294784b8
Igor Kraskevich <kraskevich@google.com>
2022-10-26 11:00:02 +0000

    [tracing] Emit active processes into core TrackEvent proto
    The data is the same, but it goes into a new field in TrackEvent instead of an extension.
    
    See aosp/2267930 for why we need this change.
    
    Bug: b/255295843
    Change-Id: I33ee6e75432816ea496ea1b9acf565df421d4d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3613266
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1063730}

--- services/tracing/public/cpp/perfetto/custom_event_recorder.cc
+++ services/tracing/public/cpp/perfetto/custom_event_recorder.cc
@@ -22,6 +22,7 @@
 #include "services/tracing/public/cpp/perfetto/trace_string_lookup.h"
 #include "third_party/perfetto/include/perfetto/tracing/internal/track_event_internal.h"
 #include "third_party/perfetto/include/perfetto/tracing/track_event_interned_data_index.h"
+#include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_active_processes.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_histogram_sample.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_process_descriptor.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/chrome_user_event.pbzero.h"
@@ -87,16 +88,16 @@ void CustomEventRecorder::EmitRecurringUpdates() {
   auto* instance = CustomEventRecorder::GetInstance();
   if (instance && instance->active_processes_callback_) {
     const auto pids = instance->active_processes_callback_.Run();
-    TRACE_EVENT_INSTANT(
-        "__metadata", "ActiveProcesses", perfetto::Track::Global(0),
-        [&pids](perfetto::EventContext ctx) {
-          auto* active_processes =
-              ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>()
-                  ->set_active_processes();
-          for (const auto& pid : pids) {
-            active_processes->add_pid(pid);
-          }
-        });
+    TRACE_EVENT_INSTANT("__metadata", "ActiveProcesses",
+                        perfetto::Track::Global(0),
+                        [&pids](perfetto::EventContext ctx) {
+                          auto* active_processes =
+                              ctx.event<perfetto::protos::pbzero::TrackEvent>()
+                                  ->set_chrome_active_processes();
+                          for (const auto& pid : pids) {
+                            active_processes->add_pid(pid);
+                          }
+                        });
   }
 #if BUILDFLAG(IS_ANDROID)
   static const ChromeProcessDescriptor::ProcessType process_type =
