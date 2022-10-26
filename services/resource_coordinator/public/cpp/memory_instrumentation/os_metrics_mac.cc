9286d2ac57085cfab2a17dfa097ec0e4d57cdc36
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2022-10-26 18:08:01 +0000

    Add resident set size and peak resident set to os metrics on mac
    Bug: 1372551
    Change-Id: I7a017f1bf99337510eb2dedeacb9bffd4177e9c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3982974
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1063910}

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_mac.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_mac.cc
@@ -251,6 +251,8 @@ bool OSMetrics::FillOSMemoryDump(base::ProcessId pid,
 
   dump->platform_private_footprint->internal_bytes = info.internal;
   dump->platform_private_footprint->compressed_bytes = info.compressed;
+  dump->resident_set_kb = info.resident_size / 1024;
+  dump->peak_resident_set_kb = info.resident_size_peak / 1024;
 
   // The |phys_footprint| field was introduced in 10.11.
   if (count == ChromeTaskVMInfoCount) {
