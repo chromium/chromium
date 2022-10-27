6b6949a0b7ab5cf64d50d2cbc100108cb4d7e50c
Markus Heintz <markusheintz@chromium.org>
2022-10-27 11:59:47 +0000

    Revert "[fyfre]Add dark mode pixel test"
    This reverts commit a5825e80e34262dad97281c552a36152497a02e5.
    
    Reason for revert: 
    
    Added tests are consistently failing on 
    https://ci.chromium.org/p/chromium/builders/ci/Win11%20Tests%20x64
    
    Since:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win11%20Tests%20x64/6932/test-results
    
    Original change's description:
    > [fyfre]Add dark mode pixel test
    >
    > Add a pixel test for the dark mode of the intro page of the Desktop
    > FRE.
    >
    > Change the pixel test to be able to take parameters.
    >
    > Bug: 1347507
    > Change-Id: Id3154a3c2680293a230f8fbfde4d7383f1b4b0a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3975608
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Commit-Queue: Jack Yammine <jyammine@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1063798}
    
    Bug: 1347507
    Change-Id: I03cab47fc5824375e09943356a9509dc4ed62bd5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986030
    Auto-Submit: Markus Heintz <markusheintz@chromium.org>
    Commit-Queue: Markus Heintz <markusheintz@chromium.org>
    Owners-Override: Markus Heintz <markusheintz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1064270}

--- chrome/browser/ui/views/profiles/first_run_intro_browsertest.cc
+++ chrome/browser/ui/views/profiles/first_run_intro_browsertest.cc
@@ -2,20 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/functional/bind.h"
 #include "base/strings/strcat.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_ui.h"
 #include "chrome/browser/ui/views/profiles/first_run_flow_controller_dice.h"
+#include "chrome/browser/ui/views/profiles/profile_management_flow_controller.h"
 #include "chrome/browser/ui/views/profiles/profile_management_step_controller.h"
 #include "chrome/browser/ui/views/profiles/profile_picker_view_test_utils.h"
-#include "chrome/common/chrome_features.h"
+#include "chrome/browser/ui/views/profiles/profile_picker_web_contents_host.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/base/ui_base_switches.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
+#include "ui/views/widget/widget.h"
 
 #if !BUILDFLAG(ENABLE_DICE_SUPPORT)
 #error Platform not supported
@@ -23,45 +25,9 @@
 
 // Tests for the chrome://intro WebUI page. They live here and not in the webui
 // directory because they manipulate views.
-namespace {
 
-struct TestParam {
-  std::string test_suffix = "";
-  bool use_dark_theme = false;
-};
-
-// To be passed as 4th argument to `INSTANTIATE_TEST_SUITE_P()`, allows the test
-// to be named like `<TestClassName>.InvokeUi_default/<TestSuffix>` instead
-// of using the index of the param in `kTestParam` as suffix.
-std::string ParamToTestSuffix(const ::testing::TestParamInfo<TestParam>& info) {
-  return info.param.test_suffix;
-}
-
-// Permutations of supported parameters.
-const TestParam kTestParams[] = {
-    {.test_suffix = "DarkTheme", .use_dark_theme = true},
-    {.test_suffix = "LightTheme", .use_dark_theme = false},
-};
-
-}  // namespace
-
-class FirstRunIntroPixelTest : public UiBrowserTest,
-                               public testing::WithParamInterface<TestParam> {
+class FirstRunIntroPixelTest : public UiBrowserTest {
  public:
-  FirstRunIntroPixelTest() {
-    std::vector<base::test::FeatureRef> enabled_features = {kForYouFre};
-    if (GetParam().use_dark_theme) {
-      enabled_features.push_back(features::kWebUIDarkMode);
-    }
-    scoped_feature_list_.InitWithFeatures(enabled_features, {});
-  }
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    if (GetParam().use_dark_theme) {
-      command_line->AppendSwitch(switches::kForceDarkMode);
-    }
-  }
-
   void ShowUi(const std::string& name) override {
     ui::ScopedAnimationDurationScaleMode disable_animation(
         ui::ScopedAnimationDurationScaleMode::ZERO_DURATION);
@@ -93,16 +59,11 @@ class FirstRunIntroPixelTest : public UiBrowserTest,
     return profile_picker_view_->GetWidget();
   }
 
-  base::test::ScopedFeatureList scoped_feature_list_;
+  base::test::ScopedFeatureList scoped_feature_list_{kForYouFre};
   raw_ptr<ProfileManagementStepTestView, DanglingUntriaged>
       profile_picker_view_;
 };
 
-IN_PROC_BROWSER_TEST_P(FirstRunIntroPixelTest, InvokeUi_default) {
+IN_PROC_BROWSER_TEST_F(FirstRunIntroPixelTest, InvokeUi_default) {
   ShowAndVerifyUi();
 }
-
-INSTANTIATE_TEST_SUITE_P(,
-                         FirstRunIntroPixelTest,
-                         testing::ValuesIn(kTestParams),
-                         &ParamToTestSuffix);
