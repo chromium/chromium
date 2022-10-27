16185c2300bee4e2c602b6054214db408d9fa2f0
Brian Osman <brianosman@google.com>
2022-10-27 13:16:06 +0000

    Guard upcoming change to Skia's mipmap builder
    Change-Id: I4c720e16ca9f3b6139065a4afe8c0d12cafb9478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983374
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1064292}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -216,6 +216,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_SUPPORT_LEGACY_DITHER
 
+#define SK_USE_LEGACY_MIPMAP_BUILDER
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
