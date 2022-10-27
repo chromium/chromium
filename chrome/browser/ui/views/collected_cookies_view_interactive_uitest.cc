362fa8d1d721f1cc936ea05f4aeb5970d82749ab
Dana Fried <dfried@chromium.org>
2022-10-27 00:38:00 +0000

    [Test Automation] Migrate collected cookies tests to Kombucha 1.5
    This cuts down the boilerplate on existing tests, so that they
    demonstrate the current state-of-the-art for writing interactive tests.
    
    Change-Id: I00c48438941ab1d871b9b72c01580fbe5d243d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3928417
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064112}

--- chrome/browser/ui/views/collected_cookies_view_interactive_uitest.cc
+++ chrome/browser/ui/views/collected_cookies_view_interactive_uitest.cc
@@ -9,24 +9,14 @@
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/collected_cookies_views.h"
 #include "chrome/browser/ui/views/page_info/page_info_main_view.h"
-#include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/interaction/interaction_test_util_browser.h"
-#include "chrome/test/interaction/webcontents_interaction_test_util.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/page_info/core/features.h"
 #include "content/public/test/browser_test.h"
 #include "net/dns/mock_host_resolver.h"
-#include "ui/base/interaction/expect_call_in_scope.h"
-#include "ui/base/interaction/interaction_sequence.h"
-#include "ui/views/controls/tabbed_pane/tabbed_pane.h"
 #include "ui/views/controls/tree/tree_view.h"
-#include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/view_utils.h"
 
-namespace {
-DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kWebContentsInteractionTestUtilTestId);
-}  // namespace
-
-class CollectedCookiesViewInteractiveUiTest : public InProcessBrowserTest {
+class CollectedCookiesViewInteractiveUiTest : public InteractiveBrowserTest {
  public:
   CollectedCookiesViewInteractiveUiTest() {
     // TODO(crbug.com/1344787): Clean up when PageSpecificSiteDataDialog is
@@ -37,18 +27,15 @@ class CollectedCookiesViewInteractiveUiTest : public InProcessBrowserTest {
   }
 
   ~CollectedCookiesViewInteractiveUiTest() override = default;
-  CollectedCookiesViewInteractiveUiTest(
-      const CollectedCookiesViewInteractiveUiTest&) = delete;
-  void operator=(const CollectedCookiesViewInteractiveUiTest&) = delete;
 
   void SetUp() override {
     set_open_about_blank_on_browser_launch(true);
     ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
-    InProcessBrowserTest::SetUp();
+    InteractiveBrowserTest::SetUp();
   }
 
   void SetUpOnMainThread() override {
-    InProcessBrowserTest::SetUpOnMainThread();
+    InteractiveBrowserTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
     content::SetupCrossSiteRedirector(embedded_test_server());
     embedded_test_server()->StartAcceptingConnections();
@@ -56,89 +43,42 @@ class CollectedCookiesViewInteractiveUiTest : public InProcessBrowserTest {
 
   void TearDownOnMainThread() override {
     EXPECT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
-    InProcessBrowserTest::TearDownOnMainThread();
+    InteractiveBrowserTest::TearDownOnMainThread();
   }
 
-  // Helper function that wait for element to show and click on it.
-  // This function will be implemented by a test util framework later.
-  auto Click(ui::ElementIdentifier element_id) {
-    return ui::InteractionSequence::StepBuilder()
-        .SetElementID(element_id)
-        .SetMustRemainVisible(false)
-        .SetStartCallback(base::BindLambdaForTesting(
-            [&](ui::InteractionSequence*, ui::TrackedElement* element) {
-              test_util_.PressButton(element);
-            }))
-        .Build();
-  }
-
-  InteractionTestUtilBrowser test_util_;
+ private:
   base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(CollectedCookiesViewInteractiveUiTest,
                        CheckCookiesAreBlockedInIncognitoWindow) {
-  UNCALLED_MOCK_CALLBACK(ui::InteractionSequence::CompletedCallback, completed);
-  UNCALLED_MOCK_CALLBACK(ui::InteractionSequence::AbortedCallback, aborted);
-
   const GURL third_party_cookie_page_url =
       embedded_test_server()->GetURL("a.com", "/third_party_cookie.html");
 
-  Browser* browser = CreateIncognitoBrowser();
+  Browser* incognito_browser = CreateIncognitoBrowser();
 
-  auto util = WebContentsInteractionTestUtil::ForExistingTabInBrowser(
-      browser, kWebContentsInteractionTestUtilTestId);
-  util->LoadPage(third_party_cookie_page_url);
+  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kWebPageElementId);
 
-  auto sequence =
-      ui::InteractionSequence::Builder()
-          .SetCompletedCallback(completed.Get())
-          .SetAbortedCallback(aborted.Get())
-          .SetContext(browser->window()->GetElementContext())
-          // Wait for page loaded.
-          .AddStep(ui::InteractionSequence::StepBuilder()
-                       .SetElementID(kWebContentsInteractionTestUtilTestId)
-                       .Build())
-          .AddStep(Click(kLocationIconElementId))
-          .AddStep(Click(PageInfoMainView::kCookieButtonElementId))
-          // Switch to Cookies in use - Blocked tab.
-          .AddStep(
-              ui::InteractionSequence::StepBuilder()
-                  .SetElementID(CollectedCookiesViews::kTabbedPaneElementId)
-                  .SetFindElementInAnyContext(true)
-                  .SetStartCallback(base::BindLambdaForTesting(
-                      [&](ui::InteractionSequence*,
-                          ui::TrackedElement* element) {
-                        // Tab on Blocked tab.
-                        views::AsViewClass<views::TabbedPane>(
-                            element->AsA<views::TrackedElementViews>()->view())
-                            ->SelectTabAt(1);
-                      }))
-                  .Build())
-          // Verify the blocked cookies.
-          .AddStep(ui::InteractionSequence::StepBuilder()
-                       .SetElementID(
-                           CollectedCookiesViews::kBlockedCookiesTreeElementId)
-                       .SetFindElementInAnyContext(true)
-                       .SetStartCallback(base::BindLambdaForTesting(
-                           [&](ui::InteractionSequence*,
-                               ui::TrackedElement* element) {
-                             auto* blocked_cookies_tree =
-                                 views::AsViewClass<views::TreeView>(
-                                     element->AsA<views::TrackedElementViews>()
-                                         ->view());
-                             ASSERT_EQ(1u, blocked_cookies_tree->GetRowCount());
+  InstrumentTab(incognito_browser, kWebPageElementId);
 
-                             auto* model = blocked_cookies_tree->model();
-                             auto nodes = model->GetChildren(model->GetRoot());
-                             ASSERT_EQ(1u, nodes.size());
-                             ASSERT_EQ("http://b.com",
-                                       static_cast<CookieTreeNode*>(nodes[0])
-                                           ->GetDetailedInfo()
-                                           .origin.Serialize());
-                           }))
-                       .Build())
-          .Build();
+  RunTestSequenceInContext(
+      incognito_browser->window()->GetElementContext(),
+      NavigateWebContents(kWebPageElementId, third_party_cookie_page_url),
+      PressButton(kLocationIconElementId),
+      PressButton(PageInfoMainView::kCookieButtonElementId),
+      // Select "blocked" tab.
+      InAnyContext(SelectTab(CollectedCookiesViews::kTabbedPaneElementId, 1)),
+      InAnyContext(AfterShow(
+          CollectedCookiesViews::kBlockedCookiesTreeElementId,
+          base::BindLambdaForTesting([&](ui::TrackedElement* element) {
+            auto* blocked_cookies_tree = AsView<views::TreeView>(element);
+            ASSERT_EQ(1u, blocked_cookies_tree->GetRowCount());
 
-  EXPECT_CALL_IN_SCOPE(completed, Run, sequence->RunSynchronouslyForTesting());
+            auto* model = blocked_cookies_tree->model();
+            auto nodes = model->GetChildren(model->GetRoot());
+            ASSERT_EQ(1u, nodes.size());
+            ASSERT_EQ("http://b.com", static_cast<CookieTreeNode*>(nodes[0])
+                                          ->GetDetailedInfo()
+                                          .origin.Serialize());
+          }))));
 }
