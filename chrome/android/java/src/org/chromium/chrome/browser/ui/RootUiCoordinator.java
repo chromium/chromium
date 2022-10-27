aa7de0a2c59b8d91036c78ea44b246eb369fd609
Calder Kitagawa <ckitagawa@chromium.org>
2022-10-27 20:00:42 +0000

    [Scroll Capture] Enable on Tablets
    Enable Scroll Capture on Tablets. There is a known issue where this may
    time out on the first attempt due to an issue on the Android framework
    side. I'd like to enable it anyway to allow the framework team to
    debug it does work about 50% of the time first try and 100% on any
    retry. This feature's usage on phones is very rare so we might as well
    try.
    
    If required, we could put this behind a flag, but it is working for
    most purposes other than the flaky first attempt...
    
    Bug: 1377217
    Change-Id: I2da520671fa907c109f3b7e1da834848911912fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988044
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1064499}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -899,10 +899,7 @@ public class RootUiCoordinator
     }
 
     private void initScrollCapture() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S
-                || DeviceFormFactor.isWindowOnTablet(mWindowAndroid)) {
-            return;
-        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return;
 
         mScrollCaptureManager = new ScrollCaptureManager(mActivityTabProvider);
     }
