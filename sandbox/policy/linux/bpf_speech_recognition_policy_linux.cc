236b087bf292aa68c36846393145458cecac0402
evliu <evliu@google.com>
2022-10-29 01:27:01 +0000

    Allow mmap in the speech recognition sandbox
    This CL updates the speech recognition sandbox policy to allow the mmap
    syscall. While mmap calls always existed within the Speech stack, recent
    code churn now triggers this call on the initialization of SODA.
    
    Change-Id: I99754d5ebc60975a88e8b2843e092f36e7244a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3978731
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065128}

--- sandbox/policy/linux/bpf_speech_recognition_policy_linux.cc
+++ sandbox/policy/linux/bpf_speech_recognition_policy_linux.cc
@@ -4,6 +4,8 @@
 
 #include "sandbox/policy/linux/bpf_speech_recognition_policy_linux.h"
 
+#include <sys/mman.h>
+
 #include "sandbox/linux/bpf_dsl/bpf_dsl.h"
 #include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
 #include "sandbox/linux/syscall_broker/broker_process.h"
@@ -24,6 +26,17 @@ SpeechRecognitionProcessPolicy::~SpeechRecognitionProcessPolicy() = default;
 ResultExpr SpeechRecognitionProcessPolicy::EvaluateSyscall(
     int system_call_number) const {
   switch (system_call_number) {
+#if defined(__NR_mmap)
+    case __NR_mmap: {
+      // The speech recognition sandbox requires the MAP_POPULATE flag in
+      // addition to the default flags.
+      const uint64_t kAllowedMask = MAP_POPULATE;
+      const bpf_dsl::Arg<int> flags(3);
+      return (flags & ~kAllowedMask) == 0
+                 ? Allow()
+                 : BPFBasePolicy::EvaluateSyscall(system_call_number);
+    }
+#endif
     // Required by the Speech On-Device API (SODA) binary to find the
     // appropriate configuration file to use within a language pack directory.
 #if defined(__NR_getdents64)
