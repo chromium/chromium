18308e29df31ab18caef6febbc84ccec2fa36156
Maksim Ivanov <emaxx@chromium.org>
2022-10-29 00:02:42 +0000

    Fuzz autofill::LegalMessageLine::Parse
    Add autofill_legal_message_line_fuzzer for covering low-level parsing
    code in //components/autofill/core/browser/payments/legal_message_line.cc.
    
    Bug: 1379110
    Change-Id: Iec3507226fdd4129ce6c2d06cb10e22332374008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989205
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065107}

---
+++ components/autofill/core/browser/payments/legal_message_line_fuzzer.cc
@@ -0,0 +1,29 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "base/json/json_reader.h"
+#include "base/values.h"
+#include "components/autofill/core/browser/payments/legal_message_line.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider provider(data, size);
+
+  // Prepare fuzzed parameters.
+  absl::optional<base::Value> legal_message =
+      base::JSONReader::Read(provider.ConsumeRandomLengthString());
+  if (!legal_message || !legal_message->is_dict())
+    return 0;
+  bool escape_apostrophes = provider.ConsumeBool();
+
+  // Run tested code.
+  autofill::LegalMessageLines lines;
+  autofill::LegalMessageLine::Parse(*legal_message, &lines, escape_apostrophes);
+
+  return 0;
+}
