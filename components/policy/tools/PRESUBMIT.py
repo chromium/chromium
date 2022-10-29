a48508d9456bc332de8c9e9f46c7cb32a828a51d
Yann Dago <ydago@chromium.org>
2022-10-29 04:11:31 +0000

    Treat Miscellaneous policies items as single policies in documentation
    Fix components/policy/tools/PRESUBMIT.py by adding the resources
    presubmit directory in sys.path
    
    (cherry picked from commit dfb0259e93f8d754c12716af7e5564d46083206b)
    
    Bug: 1379279
    Change-Id: Ide7d5850b5bc393ad04c90927ea78fe49878521f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991608
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Quick-Run: Yann Dago <ydago@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1065023}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3989431
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/5384@{#3}
    Cr-Branched-From: d7302dd81f72e8f80b80f97eae346cb98b3ef065-refs/heads/main@{#1064036}

--- components/policy/tools/PRESUBMIT.py
+++ components/policy/tools/PRESUBMIT.py
@@ -3,7 +3,7 @@
 # found in the LICENSE file.
 
 import os
-
+import sys
 
 USE_PYTHON3 = True
 
@@ -12,13 +12,20 @@ def _RunOtherPresubmit(function_name, input_api, output_api):
   # Apply the PRESUBMIT for components/policy/resources to run the syntax check
   component_resources_path = os.path.join('components', 'policy', 'resources')
 
+  # need to add components/policy/resources because PRESUBMIT.py there
+  # imports policy_templates module in the directory.
+  component_resources_path_absolute = os.path.join(
+      input_api.change.RepositoryRoot(), component_resources_path)
+  if (component_resources_path_absolute not in sys.path):
+    sys.path.append(component_resources_path_absolute)
+
   # Skip the presubmit if //components/policy/resources is changed as well.
   if any(component_resources_path in os.path.dirname(changed_file.LocalPath())
          for changed_file in input_api.change.AffectedFiles()):
     return []
 
-  presubmit_path = os.path.join(input_api.change.RepositoryRoot(),
-                                component_resources_path, 'PRESUBMIT.py')
+  presubmit_path = os.path.join(component_resources_path_absolute,
+                                'PRESUBMIT.py')
   presubmit_content = input_api.ReadFile(presubmit_path)
   global_vars = {}
   exec(presubmit_content, global_vars)
