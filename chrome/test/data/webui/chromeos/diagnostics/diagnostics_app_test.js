2056045d2e58d20ac57ef07beca935c49135294e
wenyu zhang <zhangwenyu@google.com>
2022-10-31 18:33:22 +0000

    diagnostics: Separate two test suites into different files
    This is a small refactor to prepare a larger refactor to run each
    component browsertest separately. Running each component separately
    aims to fix the timeout issue.
    
    gtest_filter=DiagnosticsAppWithInput.MANUAL_AppForInputHiding,
    MANUAL_App
    
    Bug: crbug.com/1372958
    Test: --run-manual --
    Change-Id: Ia97d5a3617e483e4c2a9de644d027deda8f9e784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3983766
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1065581}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_app_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_app_test.js
@@ -7,15 +7,12 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
 import {DiagnosticsAppElement} from 'chrome://diagnostics/diagnostics_app.js';
 import {DiagnosticsBrowserProxyImpl} from 'chrome://diagnostics/diagnostics_browser_proxy.js';
-import {fakeBatteryChargeStatus, fakeBatteryHealth, fakeBatteryInfo, fakeCellularNetwork, fakeCpuUsage, fakeEthernetNetwork, fakeKeyboards, fakeMemoryUsage, fakeNetworkGuidInfoList, fakeSystemInfo, fakeTouchDevices, fakeWifiNetwork} from 'chrome://diagnostics/fake_data.js';
-import {FakeInputDataProvider} from 'chrome://diagnostics/fake_input_data_provider.js';
+import {fakeBatteryChargeStatus, fakeBatteryHealth, fakeBatteryInfo, fakeCellularNetwork, fakeCpuUsage, fakeEthernetNetwork, fakeMemoryUsage, fakeNetworkGuidInfoList, fakeSystemInfo, fakeWifiNetwork} from 'chrome://diagnostics/fake_data.js';
 import {FakeNetworkHealthProvider} from 'chrome://diagnostics/fake_network_health_provider.js';
 import {FakeSystemDataProvider} from 'chrome://diagnostics/fake_system_data_provider.js';
 import {FakeSystemRoutineController} from 'chrome://diagnostics/fake_system_routine_controller.js';
-import {KeyboardInfo} from 'chrome://diagnostics/input_data_provider.mojom-webui.js';
-import {setInputDataProviderForTesting, setNetworkHealthProviderForTesting, setSystemDataProviderForTesting, setSystemRoutineControllerForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
+import {setNetworkHealthProviderForTesting, setSystemDataProviderForTesting, setSystemRoutineControllerForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 import {BatteryChargeStatus, BatteryHealth, BatteryInfo, CpuUsage, MemoryUsage, SystemInfo} from 'chrome://diagnostics/system_data_provider.mojom-webui.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 
 import {assertFalse, assertTrue} from '../../chai_assert.js';
@@ -235,94 +232,4 @@ export function appTestSuite() {
           });
     });
   }
-}
-
-export function appTestSuiteForInputHiding() {
-  /** @type {?DiagnosticsAppElement} */
-  let page = null;
-
-  /** @type {?FakeSystemDataProvider} */
-  let systemDataProvider = null;
-
-  /** @type {?FakeInputDataProvider} */
-  let inputDataProvider = null;
-
-  /** @type {?TestDiagnosticsBrowserProxy} */
-  let DiagnosticsBrowserProxy = null;
-
-  suiteSetup(() => {
-    systemDataProvider = new FakeSystemDataProvider();
-    systemDataProvider.setFakeSystemInfo(fakeSystemInfo);
-    systemDataProvider.setFakeBatteryChargeStatus(fakeBatteryChargeStatus);
-    systemDataProvider.setFakeBatteryHealth(fakeBatteryHealth);
-    systemDataProvider.setFakeBatteryInfo(fakeBatteryInfo);
-    systemDataProvider.setFakeCpuUsage(fakeCpuUsage);
-    systemDataProvider.setFakeMemoryUsage(fakeMemoryUsage);
-    setSystemDataProviderForTesting(systemDataProvider);
-
-    inputDataProvider = new FakeInputDataProvider();
-    setInputDataProviderForTesting(inputDataProvider);
-
-    DiagnosticsBrowserProxy = new TestDiagnosticsBrowserProxy();
-    DiagnosticsBrowserProxyImpl.setInstance(DiagnosticsBrowserProxy);
-  });
-
-  setup(() => {
-    document.body.innerHTML = '';
-
-    loadTimeData.overrideValues(
-        {isTouchpadEnabled: false, isTouchscreenEnabled: false});
-  });
-
-  teardown(() => {
-    loadTimeData.overrideValues(
-        {isTouchpadEnabled: true, isTouchscreenEnabled: true});
-
-    page.remove();
-    page = null;
-    inputDataProvider.reset();
-  });
-
-  /** @param {!Array<!KeyboardInfo>} keyboards */
-  function initializeDiagnosticsApp(keyboards) {
-    assertFalse(!!page);
-
-    inputDataProvider.setFakeConnectedDevices(keyboards, fakeTouchDevices);
-
-    page = /** @type {!DiagnosticsAppElement} */ (
-        document.createElement('diagnostics-app'));
-    assertTrue(!!page);
-    document.body.appendChild(page);
-    return flushTasks();
-  }
-
-  /** @param {!string} id */
-  function navigationSelectorHasId(id) {
-    const items = page.shadowRoot.querySelector('navigation-view-panel')
-                      .shadowRoot.querySelector('navigation-selector')
-                      .selectorItems;
-    return !!items.find((item) => item.id === id);
-  }
-
-  test('InputPageHiddenWhenNoKeyboardsConnected', async () => {
-    await initializeDiagnosticsApp([]);
-    assertFalse(navigationSelectorHasId('input'));
-
-    inputDataProvider.addFakeConnectedKeyboard(fakeKeyboards[0]);
-    await flushTasks();
-    assertTrue(navigationSelectorHasId('input'));
-
-    inputDataProvider.removeFakeConnectedKeyboardById(fakeKeyboards[0].id);
-    await flushTasks();
-    assertFalse(navigationSelectorHasId('input'));
-  });
-
-  test('InputPageShownWhenKeyboardConnectedAtLaunch', async () => {
-    await initializeDiagnosticsApp([fakeKeyboards[0]]);
-    assertTrue(navigationSelectorHasId('input'));
-
-    inputDataProvider.removeFakeConnectedKeyboardById(fakeKeyboards[0].id);
-    await flushTasks();
-    assertFalse(navigationSelectorHasId('input'));
-  });
 }
