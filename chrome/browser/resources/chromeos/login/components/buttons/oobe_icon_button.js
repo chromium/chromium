d149449e830f395fbc4eff96afb6e19c36625470
Renato Silva <rrsilva@google.com>
2022-10-31 17:05:45 +0000

    OOBE - Polymer3 - Check-in oobe-icon-button
    Check-in the modularised version of oobe-icon-button.
    
    Bug: 1184731, b/250891214
    Change-Id: I995935706c21dd8e77fb2c4097c3a1820af92760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3993404
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065527}

--- chrome/browser/resources/chromeos/login/components/buttons/oobe_icon_button.js
+++ chrome/browser/resources/chromeos/login/components/buttons/oobe_icon_button.js
@@ -3,18 +3,50 @@
 // found in the LICENSE file.
 
 
-/* #js_imports_placeholder */
+import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/cr_icons.css.js';
+import '//resources/cr_elements/cr_shared_style.css.js';
+import '../common_styles/common_styles.m.js';
+import '../hd_iron_icon.js';
+import '../oobe_vars/oobe_custom_vars_css.m.js';
+
+import {html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {OobeBaseButton} from './oobe_base_button.js';
 
 /**
- * @polymer
+ *   Material design button that shows an icon and displays text.
+ *
+ *   Example:
+ *     <oobe-icon-button icon="close" text-key="offlineLoginCloseBtn">
+ *     </oobe-icon-button>
+ *    or
+ *     <oobe-icon-button icon="close"
+ *         label-for-aria="[[i18nDynamic(locale, 'offlineLoginCloseBtn')]]">
+ *       <div slot="text">[[i18nDynamic(locale, 'offlineLoginCloseBtn')]]</div>
+ *     </oobe-icon-button>
+ *
+ *   Attributes:
+ *     'text-key' - ID of localized string to be used as button text.
+ *     1x and 2x icons:
+ *       'icon1x' - a name of icon from material design set to show on button.
+ *       'icon2x' - a name of icon from material design set to show on button.
+ *     'label-for-aria' - accessibility label, override usual behavior
+ *                        (string specified by text-key is used as aria-label).
+ *                        Elements that use slot="text" must provide
+ *                        label-for-aria value.
+ *
  */
-/* #export */ class OobeIconButton extends OobeBaseButton {
 
+/** @polymer */
+export class OobeIconButton extends OobeBaseButton {
   static get is() {
     return 'oobe-icon-button';
   }
 
-  /* #html_template_placeholder */
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
   static get properties() {
     return {
@@ -26,7 +58,6 @@
   updateIconVisibility_() {
     this.$.icon.hidden = (this.icon1x === undefined || this.icon1x.length == 0);
   }
-
 }
 
 customElements.define(OobeIconButton.is, OobeIconButton);
--- chrome/browser/resources/chromeos/login/screens/common/marketing_opt_in.js
+++ chrome/browser/resources/chromeos/login/screens/common/marketing_opt_in.js
@@ -22,7 +22,7 @@ import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../component
 import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.m.js';
 import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.m.js';
 import {OobeBackButton} from '../../components/buttons/oobe_back_button.m.js';
-import {OobeIconButton} from '../../components/buttons/oobe_icon_button.m.js';
+import {OobeIconButton} from '../../components/buttons/oobe_icon_button.js';
 import {OobeTextButton} from '../../components/buttons/oobe_text_button.m.js';
 import {OOBE_UI_STATE, SCREEN_GAIA_SIGNIN} from '../../components/display_manager_types.m.js';
 import {OobeA11yOption} from '../../components/oobe_a11y_option.js';
