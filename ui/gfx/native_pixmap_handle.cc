3fd451785b224f3a82d9c531b781bfc529c7608c
Andres Calderon Jaramillo <andrescj@chromium.org>
2022-10-31 16:56:43 +0000

    Reland "ClientNativePixmapFactory: Improve validation."
    This is a reland of commit 64e4766a0f6e6dd079588b9d65fea6132371522a
    
    Reland changes (see diff since PS1):
    
    The original CL caused some test breakages [1]. While I figure out the
    details, I made the validation in
    ClientNativePixmapFactoryDmabuf::ImportFromHandle() apply only when
    creating a ClientNativePixmapDmaBuf and not a ClientNativePixmapOpaque.
    Note that this is not a validation regression with respect of
    tip-of-tree.
    
    [1] https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-eve-chrome/24724
    
    Original change's description:
    > ClientNativePixmapFactory: Improve validation.
    >
    > This CL improves the validation performed by ClientNativePixmapFactory
    > implementations on NativePixmapHandles by adding extra validation and
    > making wider use of checked math and safe conversions.
    >
    > This CL also takes this opportunity to move duplicate validation code in
    > client_native_pixmap_dmabuf.cc, client_native_pixmap_factory_dmabuf.cc,
    > client_native_pixmap_factory_flatland.cc, and
    > client_native_pixmap_factory_scenic.cc to a helper function,
    > gfx::CanFitImageForSizeAndFormat() so that there's less of a chance of
    > validation diverging in the future for these implementations.
    >
    > Bug: 1372019
    > Test: None
    > Change-Id: I8ba714f256c315eb760c24d0b3de0886046614c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3937773
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    > Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1065129}
    
    Test: arc.AudioValidity.playback_vm on eve
    Test: arc.AudioValidity.record_vm on eve
    Test: arc.ChromeCrash.vm_logged_in on eve
    Test: arc.WindowState.clamshell_vm on eve
    Test: crostini.AppAndroidStudio.clamshell_stable on eve
    Test: crostini.AppEmacs.clamshell_stable on eve
    Test: crostini.CopyPaste.wayland_to_wayland_bullseye_stable on eve
    Test: crostini.CopyPaste.wayland_to_x11_bullseye_stable on eve
    Test: crostini.CopyPaste.x11_to_wayland_bullseye_stable on eve
    Test: crostini.SecureCopyPaste.copy_wayland_bullseye_stable on eve
    Test: crostini.SecureCopyPaste.copy_x11_bullseye_stable on eve
    Test: crostini.SecureCopyPaste.paste_wayland_bullseye_stable on eve
    Test: crostini.Toolkit.gtk3_wayland_bullseye_stable on eve
    Test: crostini.Toolkit.gtk3_x11_bullseye_stable on eve
    Test: crostini.Toolkit.qt5_bullseye_stable on eve
    Test: inputs.PhysicalKeyboardGrammarCheck.lacros on eve
    Test: mlservice.WebHandwritingRecognition.lacros on eve
    Bug: 1372019
    Change-Id: I7a78d45f55b9522b623cb98ab51ba5cfa0627696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3991908
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065522}

--- ui/gfx/native_pixmap_handle.cc
+++ ui/gfx/native_pixmap_handle.cc
@@ -8,6 +8,8 @@
 
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "ui/gfx/buffer_format_util.h"
+#include "ui/gfx/geometry/size.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include <drm_fourcc.h>
@@ -123,6 +125,75 @@ NativePixmapHandle CloneHandleForIPC(const NativePixmapHandle& handle) {
 #endif
 
   return clone;
+}
+
+bool CanFitImageForSizeAndFormat(const gfx::NativePixmapHandle& handle,
+                                 const gfx::Size& size,
+                                 gfx::BufferFormat format,
+                                 bool assume_single_memory_object) {
+  size_t expected_planes = gfx::NumberOfPlanesForLinearBufferFormat(format);
+  if (expected_planes == 0 || handle.planes.size() != expected_planes)
+    return false;
+
+  size_t total_size = 0u;
+  if (assume_single_memory_object) {
+    if (!base::IsValueInRangeForNumericType<size_t>(
+            handle.planes.back().offset) ||
+        !base::IsValueInRangeForNumericType<size_t>(
+            handle.planes.back().size)) {
+      return false;
+    }
+    const base::CheckedNumeric<size_t> total_size_checked =
+        base::CheckAdd(base::checked_cast<size_t>(handle.planes.back().offset),
+                       base::checked_cast<size_t>(handle.planes.back().size));
+    if (!total_size_checked.IsValid())
+      return false;
+    total_size = total_size_checked.ValueOrDie();
+  }
+
+  for (size_t i = 0; i < handle.planes.size(); ++i) {
+    const size_t plane_stride =
+        base::strict_cast<size_t>(handle.planes[i].stride);
+    size_t min_stride = 0;
+    if (!gfx::RowSizeForBufferFormatChecked(
+            base::checked_cast<size_t>(size.width()), format, i, &min_stride) ||
+        plane_stride < min_stride) {
+      return false;
+    }
+
+    const size_t subsample_factor = SubsamplingFactorForBufferFormat(format, i);
+    const base::CheckedNumeric<size_t> plane_height =
+        base::CheckDiv(base::CheckAdd(base::checked_cast<size_t>(size.height()),
+                                      base::CheckSub(subsample_factor, 1)),
+                       subsample_factor);
+    const base::CheckedNumeric<size_t> min_size = plane_height * plane_stride;
+    if (!min_size.IsValid<uint64_t>() ||
+        handle.planes[i].size < min_size.ValueOrDie<uint64_t>()) {
+      return false;
+    }
+
+    // The stride must be a valid integer in order to be consistent with the
+    // GpuMemoryBuffer::stride()/gfx::ClientNativePixmap::GetStride() APIs.
+    // Also, refer to http://crbug.com/1093644#c1 for some comments on this
+    // check and others in this method.
+    if (!base::IsValueInRangeForNumericType<int>(plane_stride))
+      return false;
+
+    if (assume_single_memory_object) {
+      if (!base::IsValueInRangeForNumericType<size_t>(
+              handle.planes[i].offset) ||
+          !base::IsValueInRangeForNumericType<size_t>(handle.planes[i].size)) {
+        return false;
+      }
+      base::CheckedNumeric<size_t> end_pos =
+          base::CheckAdd(base::checked_cast<size_t>(handle.planes[i].offset),
+                         base::checked_cast<size_t>(handle.planes[i].size));
+      if (!end_pos.IsValid() || end_pos.ValueOrDie() > total_size)
+        return false;
+    }
+  }
+
+  return true;
 }
 
 }  // namespace gfx
