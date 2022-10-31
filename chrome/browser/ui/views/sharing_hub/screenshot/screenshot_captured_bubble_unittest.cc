48be518914af674a8844759433bc1816f5d469e8
Elly Fong-Jones <ellyjones@chromium.org>
2022-10-31 23:58:31 +0000

    desktop-screenshots: add unit test for edit behavior
    There was an existing unit test that claimed to cover
    the behavior of loading the editor when the "Edit" button
    was clicked, but that test was actually a no-op - it
    constructed a ScreenshotCapturedBubble but never actually
    clicked the Edit button. This change implements that test
    for real.
    
    Change-Id: I330e13d7149743cdf9a40ac19a01e308e0354b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3971890
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065714}

--- chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble_unittest.cc
+++ chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble_unittest.cc
@@ -8,6 +8,8 @@
 
 #include "base/callback.h"
 #include "base/test/bind.h"
+#include "chrome/browser/image_editor/image_editor_component_info.h"
+#include "chrome/browser/share/share_features.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/common/webui_url_constants.h"
@@ -16,28 +18,116 @@
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/web_contents_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/events/base_event_utils.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/controls/button/label_button.h"
+#include "ui/views/test/widget_test.h"
 
 namespace sharing_hub {
 
-class ScreenshotCapturedBubbleTest : public ChromeViewsTestBase {};
+namespace {
 
-TEST_F(ScreenshotCapturedBubbleTest, EditNavigatesToImageEditorWebUI) {
+gfx::Image CreateTestImage() {
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(16, 14);
+  bitmap.eraseColor(SK_ColorYELLOW);
+  gfx::ImageSkia img(gfx::ImageSkia::CreateFrom1xBitmap(bitmap));
+  return gfx::Image(img);
+}
+
+bool IsButtonWithLabel(const std::u16string& label, const views::View* v) {
+  auto* button = views::Button::AsButton(v);
+  if (!button || strcmp(button->GetClassName(), "MdTextButton"))
+    return false;
+  return static_cast<const views::LabelButton*>(button)->GetText() == label;
+}
+
+void ClickButton(views::Button* button) {
+  const gfx::Point point(10, 10);
+  const ui::MouseEvent event(ui::ET_MOUSE_PRESSED, point, point,
+                             ui::EventTimeForNow(), ui::EF_LEFT_MOUSE_BUTTON,
+                             ui::EF_LEFT_MOUSE_BUTTON);
+  button->OnMousePressed(event);
+  button->OnMouseReleased(event);
+}
+
+}  // namespace
+
+class ScreenshotCapturedBubbleTest : public ChromeViewsTestBase {
+ public:
+  void SetUp() override {
+    ChromeViewsTestBase::SetUp();
+    // This simulates the editor being installed for tests, so that the Edit
+    // button will show up.
+    image_editor::ImageEditorComponentInfo::GetInstance()->SetInstalledPath(
+        base::FilePath(base::FilePath::kCurrentDirectory));
+    anchor_widget_ = CreateTestWidget();
+  }
+
+  void TearDown() override {
+    if (bubble_widget_)
+      bubble_widget_->CloseNow();
+    anchor_widget_->CloseNow();
+    image_editor::ImageEditorComponentInfo::GetInstance()->SetInstalledPath({});
+    ChromeViewsTestBase::TearDown();
+  }
+
+  void ShowBubbleWithEditCallback(
+      base::OnceCallback<void(NavigateParams*)> edit) {
+    auto bubble = std::make_unique<ScreenshotCapturedBubble>(
+        anchor_widget_->GetRootView(), test_web_contents_.get(),
+        CreateTestImage(), &profile_, std::move(edit));
+    bubble_ = bubble.get();
+    bubble_widget_ =
+        views::BubbleDialogDelegateView::CreateBubble(std::move(bubble));
+    bubble_->ShowForReason(LocationBarBubbleDelegateView::USER_GESTURE);
+  }
+
+ protected:
+  ScreenshotCapturedBubble* bubble() { return bubble_; }
+  views::Widget* bubble_widget() { return bubble_widget_; }
+
+ private:
+  base::test::ScopedFeatureList features_{
+      share::kSharingDesktopScreenshotsEdit};
   TestingProfile profile_;
   // This enables uses of TestWebContents.
   content::RenderViewHostTestEnabler test_render_host_factories_;
   std::unique_ptr<content::WebContents> test_web_contents_ =
       content::WebContentsTester::CreateTestWebContents(&profile_, nullptr);
 
-  const gfx::Image image;
-  ScreenshotCapturedBubble bubble(
-      nullptr, test_web_contents_.get(), image, nullptr,
+  std::unique_ptr<views::Widget> anchor_widget_;
+  ScreenshotCapturedBubble* bubble_;
+  views::Widget* bubble_widget_;
+};
+
+TEST_F(ScreenshotCapturedBubbleTest, EditNavigatesToImageEditorWebUI) {
+  base::RunLoop run_loop;
+
+  GURL navigated_url;
+  WindowOpenDisposition navigated_disposition;
+  NavigateParams::WindowAction navigated_action;
+
+  ShowBubbleWithEditCallback(
       base::BindLambdaForTesting([&](NavigateParams* params) {
-        EXPECT_EQ(chrome::kChromeUIImageEditorURL, params->url.spec());
-        EXPECT_EQ(WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                  params->disposition);
-        EXPECT_EQ(NavigateParams::SHOW_WINDOW, params->window_action);
+        navigated_url = params->url;
+        navigated_disposition = params->disposition;
+        navigated_action = params->window_action;
+        run_loop.Quit();
       }));
+
+  auto* button =
+      static_cast<views::Button*>(views::test::AnyViewMatchingPredicate(
+          bubble_widget(), base::BindRepeating(&IsButtonWithLabel, u"Edit")));
+  ASSERT_TRUE(button);
+  ClickButton(button);
+
+  run_loop.Run();
+
+  EXPECT_EQ(chrome::kChromeUIImageEditorURL, navigated_url.spec());
+  EXPECT_EQ(WindowOpenDisposition::NEW_FOREGROUND_TAB, navigated_disposition);
+  EXPECT_EQ(NavigateParams::SHOW_WINDOW, navigated_action);
 }
 
 }  // namespace sharing_hub
