23dece9cec12423fc2371c3019ea23a6337452ae
Ben Wagner <benjaminwagner@google.com>
2022-10-31 17:47:06 +0000

    Remove excessive logging from TabCaptureApiTest.MultipleExtensions
    Followup to https://chromium-review.googlesource.com/c/chromium/src/+/3968763
    
    Bug: 1379782
    Change-Id: I8579e1a30f45ceec5007c4b273e9648c5e9e2341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990793
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Auto-Submit: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065547}

--- chrome/test/data/extensions/api_test/tab_capture/multiple_extensions/common.js
+++ chrome/test/data/extensions/api_test/tab_capture/multiple_extensions/common.js
@@ -5,7 +5,6 @@
 // Triggers tab capture asynchronously. If successful, sends a "success"
 // message.
 function startTabCapture() {
-  console.log('Starting tab capture...');
   chrome.tabCapture.capture({audio: true, video: false}, captureStream => {
     if (chrome.runtime.lastError) {
       console.error(JSON.stringify(chrome.runtime.lastError));
