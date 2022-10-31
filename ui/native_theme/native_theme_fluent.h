aad76592d065d06594a28ca23865c80db613cd2c
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2022-10-31 19:01:29 +0000

    Reland "[Fluent Scrollbars] Paint Segoe Fluent Icons for scrollbar arrows if the font is available on the device."
    This is a reland of commit c0009aa2ffeaddae5e3c70dcdf51f9a931c17183
    
    Original change's description:
    > [Fluent Scrollbars] Paint Segoe Fluent Icons for scrollbar arrows if
    > the font is available on the device.
    >
    > The CL implements the painting of Fluent arrow icons by the visual
    > specification if the font is available on the device. Added more unit
    > tests to cover default and pressed states for arrow rects.
    >
    > Also, made native_theme_fluent.* files Windows-specific. We are
    > planning to support the Linux platform eventually but it will be
    > done later. Please see the task for tracking:
    > https://crbug.com/1379075
    >
    > Bug: 1353576, 1351598
    > Change-Id: I707140f9855599e664987a3005774e105e5098ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984249
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    > Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1064923}
    
    Bug: 1353576, 1351598
    Change-Id: Ibe6174dc95eba49c6c42ff07bb8bd681751b4e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3988955
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065599}

--- ui/native_theme/native_theme_fluent.h
+++ ui/native_theme/native_theme_fluent.h
@@ -9,7 +9,12 @@
 
 namespace gfx {
 class Rect;
+class RectF;
 }  // namespace gfx
+
+template <typename T>
+class sk_sp;
+class SkTypeface;
 
 namespace ui {
 
@@ -55,8 +60,7 @@ class NATIVE_THEME_EXPORT NativeThemeFluent : public NativeThemeBase {
                         const ExtraParams& extra) const override;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(NativeThemeFluentTest, VerticalArrowRectDefault);
-  FRIEND_TEST_ALL_PREFIXES(NativeThemeFluentTest, HorizontalArrowRectDefault);
+  friend class NativeThemeFluentTest;
 
   void PaintButton(cc::PaintCanvas* canvas,
                    const ColorProvider* color_provider,
@@ -73,13 +77,26 @@ class NATIVE_THEME_EXPORT NativeThemeFluent : public NativeThemeBase {
   // within the scrollbar button rect. The goal is to keep the arrow in the
   // center of the button with the applied kFluentScrollbarArrowOffset. See
   // OffsetArrowRect method for more details.
-  gfx::Rect GetArrowRect(const gfx::Rect& rect, Part part, State state) const;
+  gfx::RectF GetArrowRect(const gfx::Rect& rect, Part part, State state) const;
+
+  // An arrow rect is a square. Returns the side length based on the state and
+  // the font availability.
+  int GetArrowSideLength(State state) const;
 
   // By Fluent design, arrow rect is offset from the center to the side opposite
   // from the track rect border by kFluentScrollbarArrowOffset px.
-  void OffsetArrowRect(gfx::Rect& arrow_rect,
+  void OffsetArrowRect(gfx::RectF& arrow_rect,
                        Part part,
                        int max_arrow_rect_side) const;
+
+  // Returns true if the font with arrow icons is present on the device.
+  bool ArrowIconsAvailable() const { return typeface_.get(); }
+
+  const char* GetArrowCodePointForScrollbarPart(Part part) const;
+
+  // The value stores a shared pointer to SkTypeface with the font family, which
+  // contains arrow icons.
+  sk_sp<SkTypeface> typeface_;
 };
 
 }  // namespace ui
