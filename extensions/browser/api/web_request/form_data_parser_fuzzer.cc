7bced20de693323907330abcd3b885540cba0658
Maksim Ivanov <emaxx@chromium.org>
2022-11-01 23:53:48 +0000

    Fuzz extensions FormDataParser
    Implement fuzzer for checking data parsing logic in
    //extensions/browser/api/web_request/form_data_parser.cc.
    
    Bug: 1380531
    Change-Id: If8cbb41607d7bfe53566ae1a221691793062882d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990569
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066237}

---
+++ extensions/browser/api/web_request/form_data_parser_fuzzer.cc
@@ -0,0 +1,96 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "base/logging.h"
+#include "base/strings/string_piece.h"
+#include "extensions/browser/api/web_request/form_data_parser.h"
+#include "net/http/http_request_headers.h"
+#include "net/http/http_util.h"
+
+using extensions::FormDataParser;
+
+namespace {
+
+// Does initialization and holds state that's shared across all runs.
+class Environment {
+ public:
+  Environment() {
+    // Disable noisy logging.
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+  }
+};
+
+net::HttpRequestHeaders GenerateHttpRequestHeaders(
+    FuzzedDataProvider& provider) {
+  net::HttpRequestHeaders headers;
+  for (;;) {
+    const std::string key = provider.ConsumeRandomLengthString();
+    const std::string value = provider.ConsumeRandomLengthString();
+    if (key.empty() || !net::HttpUtil::IsValidHeaderName(key) ||
+        !net::HttpUtil::IsValidHeaderValue(value)) {
+      break;
+    }
+    headers.SetHeader(key, value);
+  }
+  return headers;
+}
+
+}  // namespace
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+  FuzzedDataProvider provider(data, size);
+
+  // Create parser sources. Per API contract, they must outlive the parser.
+  std::vector<std::string> sources;
+  for (;;) {
+    std::string source = provider.ConsumeRandomLengthString();
+    if (source.empty())
+      break;
+    sources.push_back(std::move(source));
+  }
+
+  // Create a parser with random initialization parameters.
+  std::unique_ptr<FormDataParser> parser;
+  switch (provider.ConsumeIntegralInRange<int>(0, 2)) {
+    case 0: {
+      parser = FormDataParser::Create(GenerateHttpRequestHeaders(provider));
+      break;
+    }
+    case 1: {
+      parser = FormDataParser::CreateFromContentTypeHeader(
+          /*content_type_header=*/nullptr);
+      break;
+    }
+    case 2: {
+      std::string content_type_header = provider.ConsumeRandomLengthString();
+      parser =
+          FormDataParser::CreateFromContentTypeHeader(&content_type_header);
+      break;
+    }
+  }
+  if (!parser)
+    return 0;
+
+  // Run the parser.
+  for (const auto& source : sources) {
+    parser->SetSource(source);
+
+    FormDataParser::Result result;
+    while (parser->GetNextNameValue(&result)) {
+      // Discard the result - we can't verify anything in it here.
+    }
+  }
+
+  return 0;
+}
