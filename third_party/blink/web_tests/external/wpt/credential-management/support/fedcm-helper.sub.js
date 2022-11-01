f91d3524981595a16578efe0b652a80f7b22868f
Peter Kotwicz <pkotwicz@chromium.org>
2022-11-01 01:15:38 +0000

    [FedCM][Test] Add tests for redirect and caching behaviour
    This CL:
    - Adds WPT tests that accounts endpoint and token endpoint do not
    redirect
    - Adds WPT test that the client metadata endpoint request is not
    cached
    
    BUG=1368434
    
    Change-Id: Ice6b68705e0d8504ba9d96ec697ce9158c1746c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3961610
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1065768}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -34,3 +34,27 @@ export function fedcm_test(test_func) {
     await test_func(t);
   });
 }
+
+function select_manifest_impl(manifest_url) {
+  const url_query = (manifest_url === undefined)
+      ? '' : '?manifest_url=${manifest_url}';
+
+  return new Promise(resolve => {
+    const img = document.createElement('img');
+    img.src = 'support/fedcm/select_manifest_in_root_manifest.py?${url_query}';
+    img.addEventListener('error', resolve);
+    document.body.appendChild(img);
+  });
+}
+
+// Sets the manifest returned by the next fetch of /.well-known/web_identity
+// select_manifest() only affects the next fetch and not any subsequent fetches
+// (ex second next fetch).
+export function select_manifest(test, test_options) {
+  // Add cleanup in case that /.well-known/web_identity is not fetched at all.
+  test.add_cleanup(async () => {
+    await select_manifest_impl();
+  });
+  const manifest_url = test_options.identity.providers[0].configURL;
+  return select_manifest_impl(manifest_url);
+}
