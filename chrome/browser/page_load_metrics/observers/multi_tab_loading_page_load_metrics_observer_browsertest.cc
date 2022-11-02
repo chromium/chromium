9b99aa6efce3f07f4ed119fbae673696a9905ce6
Nidhi Jaju <nidhijaju@chromium.org>
2022-11-02 03:47:47 +0000

    [Sheriff] Disable MultiTabLoadingPageLoadMetricsBrowserTest.MultiTabForeground on Mac
    MultiTabLoadingPageLoadMetricsBrowserTest.MultiTabForeground was already
    disabled on Linux and ChromeOS, but it is also flaky on Mac, so
    disabling for Mac as well.
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/mac12-arm64-rel-tests/3539/overview
    
    Bug: 1310328
    Change-Id: I79499d17a0405fa04a2c8e7e543bd20e01d7c643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995895
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066326}

--- chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer_browsertest.cc
@@ -57,8 +57,8 @@ IN_PROC_BROWSER_TEST_F(MultiTabLoadingPageLoadMetricsBrowserTest, SingleTab) {
       0);
 }
 
-// TODO(crbug.com/1310328): Test is flaky on Linux, lacros, Chrome OS.
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+// TODO(crbug.com/1310328): Test is flaky on Linux, lacros, Chrome OS, Mac.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
 #define MAYBE_MultiTabForeground DISABLED_MultiTabForeground
 #else
 #define MAYBE_MultiTabForeground MultiTabForeground
