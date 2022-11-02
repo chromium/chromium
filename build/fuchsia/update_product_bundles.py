a193d6c2f3ae62da8bda2090014062df24c8708b
Chong Gu <chonggu@google.com>
2022-11-02 20:00:33 +0000

    [Fuchsia][CFv2 Script] Allow SDK under test to also download product bundles
    These SDKs require a change to the pbms.metadata variable in order for
    them to be able to find the correct product bundles.
    
    Bug: 1380807
    Change-Id: I6b9f303a3c1b241d456bfb68359dfe146951035c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999866
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Auto-Submit: Chong Gu <chonggu@google.com>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066638}

--- build/fuchsia/update_product_bundles.py
+++ build/fuchsia/update_product_bundles.py
@@ -11,8 +11,8 @@ import json
 import logging
 import os
 import sys
-import tempfile
 
+from contextlib import ExitStack
 import common
 import ffx_session
 import log_manager
@@ -83,73 +83,89 @@ def main():
   # Check whether there's Fuchsia support for this platform.
   common.GetHostOsFromPlatform()
 
-  signature_filename = os.path.join(common.PRODUCT_BUNDLE_SIGNATURE_FILE)
-  curr_signature = {}
-  if os.path.exists(signature_filename):
-    with open(signature_filename, 'r') as f:
-      curr_signature = json.load(f)
-
-  new_sdk_hash = get_hash_from_sdk()
-  new_signature = {'sdk_hash': new_sdk_hash}
-
   new_product_bundles = convert_to_product_bundle(
       args.product_bundles.split(','))
   for pb in new_product_bundles:
     if pb not in _PRODUCT_BUNDLES:
-      raise ValueError('%s is not part of the Fuchsia product bundle.' % pb)
+      raise ValueError(f'{pb} is not part of the Fuchsia product bundle.')
 
-  with tempfile.TemporaryDirectory() as tmpdirname:
+  if '*' in args.product_bundles:
+    raise ValueError('Wildcards are no longer supported, all product bundles '
+                     'need to be explicitly listed. The full list can be '
+                     'found in the DEPS file.')
+
+  with ExitStack() as stack:
     ffx_runner = ffx_session.FfxRunner(log_manager.LogManager(None))
-    with ffx_runner.scoped_config('pbms.storage.path', common.IMAGES_ROOT):
+    stack.enter_context(
+        ffx_runner.scoped_config('pbms.storage.path', common.IMAGES_ROOT))
 
-      # If SDK versions match, remove the product bundles that are no longer
-      # needed and download missing ones.
-      if curr_signature.get('sdk_hash') == new_sdk_hash:
-        curr_signature.get('sdk_hash')
-        new_signature['path'] = curr_signature.get('path')
-        new_product_bundle_hash = []
+    # TODO(crbug/1380807): Remove when product bundles can be downloaded
+    # for custom SDKs without editing metadata
+    override_file = os.path.join(os.path.dirname(__file__), 'sdk_override.txt')
+    if os.path.isfile(override_file):
+      with open(override_file) as f:
+        pb_metadata = f.read().split('\n')
+        pb_metadata.append('{sdk.root}/*.json')
+      stack.enter_context(
+          ffx_runner.scoped_config('pbms.metadata', json.dumps((pb_metadata))))
 
-        for image in curr_signature.get('images', []):
-          if image in new_product_bundles:
-            new_product_bundle_hash.append(image)
-          else:
-            logging.info('Removing no longer needed Fuchsia image %s' % image)
-            ffx_runner.run_ffx(('product-bundle', 'remove', '-f', image))
+    signature_filename = common.PRODUCT_BUNDLE_SIGNATURE_FILE
+    curr_signature = {}
+    if os.path.exists(signature_filename):
+      with open(signature_filename, 'r') as f:
+        curr_signature = json.load(f)
 
-        bundles_to_download = set(new_product_bundles) - \
-                              set(curr_signature.get('images', []))
-        for bundle in bundles_to_download:
-          download_product_bundle(bundle, ffx_runner)
-        new_product_bundle_hash.extend(bundles_to_download)
-        new_signature['images'] = new_product_bundle_hash
+    new_sdk_hash = get_hash_from_sdk()
+    new_signature = {'sdk_hash': new_sdk_hash}
 
-        with open(signature_filename, 'w') as f:
-          f.write(json.dumps(new_signature))
-        return 0
+    # If SDK versions match, remove the product bundles that are no longer
+    # needed and download missing ones.
+    if curr_signature.get('sdk_hash') == new_sdk_hash:
+      curr_signature.get('sdk_hash')
+      new_signature['path'] = curr_signature.get('path')
+      new_product_bundle_hash = []
 
-      # If SDK versions do not match, remove all existing product bundles
-      # and download the ones required.
-      for pb in curr_signature.get('images', []):
-        ffx_runner.run_ffx(('product-bundle', 'remove', '-f', pb))
+      for image in curr_signature.get('images', []):
+        if image in new_product_bundles:
+          new_product_bundle_hash.append(image)
+        else:
+          logging.info('Removing no longer needed Fuchsia image %s' % image)
+          ffx_runner.run_ffx(('product-bundle', 'remove', '-f', image))
 
-      curr_subdir = []
-      if os.path.exists(common.IMAGES_ROOT):
-        curr_subdir = os.listdir(common.IMAGES_ROOT)
-      common.MakeCleanDirectory(common.IMAGES_ROOT)
-
-      for pb in new_product_bundles:
-        download_product_bundle(pb, ffx_runner)
-      new_signature['images'] = new_product_bundles
-
-      new_subdir = os.listdir(common.IMAGES_ROOT)
-      subdir_diff = set(new_subdir) - set(curr_subdir)
-      if len(subdir_diff) != 1:
-        raise RuntimeError(f'Expected one new created subdirectory but got '
-                           '{subdir_diff} instead.')
-      new_signature['path'] = list(subdir_diff)[0]
+      bundles_to_download = set(new_product_bundles) - \
+                            set(curr_signature.get('images', []))
+      for bundle in bundles_to_download:
+        download_product_bundle(bundle, ffx_runner)
+      new_product_bundle_hash.extend(bundles_to_download)
+      new_signature['images'] = new_product_bundle_hash
 
       with open(signature_filename, 'w') as f:
         f.write(json.dumps(new_signature))
+      return 0
+
+    # If SDK versions do not match, remove all existing product bundles
+    # and download the ones required.
+    for pb in curr_signature.get('images', []):
+      ffx_runner.run_ffx(('product-bundle', 'remove', '-f', pb))
+
+    curr_subdir = []
+    if os.path.exists(common.IMAGES_ROOT):
+      curr_subdir = os.listdir(common.IMAGES_ROOT)
+    common.MakeCleanDirectory(common.IMAGES_ROOT)
+
+    for pb in new_product_bundles:
+      download_product_bundle(pb, ffx_runner)
+    new_signature['images'] = new_product_bundles
+
+    new_subdir = os.listdir(common.IMAGES_ROOT)
+    subdir_diff = set(new_subdir) - set(curr_subdir)
+    if len(subdir_diff) != 1:
+      raise RuntimeError(f'Expected one new created subdirectory but got '
+                         '{subdir_diff} instead.')
+    new_signature['path'] = list(subdir_diff)[0]
+
+    with open(signature_filename, 'w') as f:
+      f.write(json.dumps(new_signature))
   return 0
 
 
