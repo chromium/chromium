1118355101217a48908bdc7158376344ff7b3617
Nicolás Peña Moreno <npm@chromium.org>
2022-11-02 21:52:49 +0000

    [M108][FedCM] Fix crash when email is empty
    (cherry picked from commit 659b08cbd8db0a7efc8615b0d7115ca2cdf666b2)
    
    Bug: 1377464
    Change-Id: If21513ba2691a419d6fa822ee939210d289d5907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3972001
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1065698}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999252
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5359@{#534}
    Cr-Branched-From: 27d3765d341b09369006d030f83f582a29eb57ae-refs/heads/main@{#1058933}

--- chrome/browser/ui/views/hover_button.cc
+++ chrome/browser/ui/views/hover_button.cc
@@ -247,6 +247,9 @@ void HoverButton::SetTitleTextStyle(views::style::TextStyle text_style,
 
 void HoverButton::SetSubtitleTextStyle(int text_context,
                                        views::style::TextStyle text_style) {
+  if (!subtitle())
+    return;
+
   subtitle()->SetTextContext(text_context);
   subtitle()->SetTextStyle(text_style);
   subtitle()->SetAutoColorReadabilityEnabled(true);
