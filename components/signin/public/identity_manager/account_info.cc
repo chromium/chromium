cb7a5eb770cb32ac2241ef1707e74e97eeb8750e
Boris Sazonov <bsazonov@chromium.org>
2022-11-02 17:12:40 +0000

    [Android] Add DCHECKs to methods for converting account info
    ConvertToJavaCoreAccountInfo, ConvertToJavaAccountInfo,
    ConvertFromJavaCoreAccountInfo and ConvertFromJavaCoreAccountId don't
    work on null or empty object, add explicit DCHECKs to reflect that.
    
    Bug: 1378317
    Change-Id: I6dd89f5d3aab28c529d93e24474aae1c7778d7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3990590
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066549}

--- components/signin/public/identity_manager/account_info.cc
+++ components/signin/public/identity_manager/account_info.cc
@@ -156,6 +156,7 @@ std::ostream& operator<<(std::ostream& os, const CoreAccountInfo& account) {
 base::android::ScopedJavaLocalRef<jobject> ConvertToJavaCoreAccountInfo(
     JNIEnv* env,
     const CoreAccountInfo& account_info) {
+  DCHECK(!account_info.IsEmpty());
   return signin::Java_CoreAccountInfo_Constructor(
       env, ConvertToJavaCoreAccountId(env, account_info.account_id),
       base::android::ConvertUTF8ToJavaString(env, account_info.email),
@@ -165,6 +166,7 @@ base::android::ScopedJavaLocalRef<jobject> ConvertToJavaCoreAccountInfo(
 base::android::ScopedJavaLocalRef<jobject> ConvertToJavaAccountInfo(
     JNIEnv* env,
     const AccountInfo& account_info) {
+  DCHECK(!account_info.IsEmpty());
   gfx::Image avatar_image = account_info.account_image;
   return signin::Java_AccountInfo_Constructor(
       env, ConvertToJavaCoreAccountId(env, account_info.account_id),
@@ -189,6 +191,7 @@ base::android::ScopedJavaLocalRef<jobject> ConvertToJavaCoreAccountId(
 CoreAccountInfo ConvertFromJavaCoreAccountInfo(
     JNIEnv* env,
     const base::android::JavaRef<jobject>& j_core_account_info) {
+  DCHECK(j_core_account_info);
   CoreAccountInfo account;
   account.account_id = ConvertFromJavaCoreAccountId(
       env, signin::Java_CoreAccountInfo_getId(env, j_core_account_info));
@@ -202,6 +205,7 @@ CoreAccountInfo ConvertFromJavaCoreAccountInfo(
 CoreAccountId ConvertFromJavaCoreAccountId(
     JNIEnv* env,
     const base::android::JavaRef<jobject>& j_core_account_id) {
+  DCHECK(j_core_account_id);
   CoreAccountId id =
       CoreAccountId::FromString(base::android::ConvertJavaStringToUTF8(
           signin::Java_CoreAccountId_getId(env, j_core_account_id)));
