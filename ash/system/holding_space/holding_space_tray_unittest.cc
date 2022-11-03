49873151add4df671d42cc223dff5ac06cb3833c
angel <angelsan@google.com>
2022-11-03 16:29:02 +0000

    holdingspace: Enable EnterAndExitAnimations unittest.
    EnterAndExitAnimations unittest was failing due to it's animations not happening. This tests parent class `AshTestBase` is setting its ScopedAnimationDurationScaleMode::duration_multiplier_ to 0. This means animation durations for these tests are being scaled down to 0 seconds. This CL changes this specific tests duration_multiplier_ to ui::ScopedAnimationDurationScaleMode::FAST_DURATION in order for the animations it is testing to happen.
    
    Bug: 1208501
    Change-Id: I58e539af0b30e180007f482acbe0f86808ec4400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3995336
    Reviewed-by: David Black <dmblack@google.com>
    Auto-Submit: Angel Sanchez <angelsan@google.com>
    Commit-Queue: Angel Sanchez <angelsan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067055}

--- ash/system/holding_space/holding_space_tray_unittest.cc
+++ ash/system/holding_space/holding_space_tray_unittest.cc
@@ -52,6 +52,7 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/canvas_painter.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/compositor/test/layer_animation_stopped_waiter.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/gfx/color_utils.h"
@@ -2069,9 +2070,12 @@ TEST_F(HoldingSpaceTrayTest, CloseTrayBubbleAfterDoubleClick) {
   EXPECT_FALSE(test_api()->IsShowing());
 }
 
-// TODO(crbug.com/1208501): Fix flakes and re-enable.
 // Verifies that the holding space tray animates in and out as expected.
-TEST_F(HoldingSpaceTrayTest, DISABLED_EnterAndExitAnimations) {
+TEST_F(HoldingSpaceTrayTest, EnterAndExitAnimations) {
+  // Ensure animations are run.
+  ui::ScopedAnimationDurationScaleMode scoped_animation_duration_scale_mode(
+      ui::ScopedAnimationDurationScaleMode::FAST_DURATION);
+
   // Prior to session start, the tray should not be showing.
   EXPECT_FALSE(test_api()->IsShowingInShelf());
 
