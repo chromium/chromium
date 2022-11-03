fc80eb6d9268a038fc4e72f0b6b625e9811d8cb7
Gabriel Oliveira <gabolvr@google.com>
2022-11-03 08:17:55 +0000

    [Signin] Refactor string resources registration in Sync Confirmation
    Bug: 1374702
    Change-Id: I408edbc9d100cf73cdf8a33688ce908c2545b225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3984526
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1066905}

--- chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
+++ chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
@@ -94,13 +94,15 @@ void SyncConfirmationUI::InitializeForSyncConfirmation(
   int title_id = IDS_SYNC_CONFIRMATION_TITLE;
   int info_title_id = IDS_SYNC_CONFIRMATION_SYNC_INFO_TITLE;
   int confirm_label_id = IDS_SYNC_CONFIRMATION_CONFIRM_BUTTON_LABEL;
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  title_id = IDS_SYNC_CONFIRMATION_TITLE_LACROS_NON_FORCED;
-#endif
-  AddStringResource(source, "syncConfirmationSyncInfoDesc",
-                    IDS_SYNC_CONFIRMATION_SYNC_INFO_DESC);
-  AddStringResource(source, "syncConfirmationSettingsInfo",
-                    IDS_SYNC_CONFIRMATION_SETTINGS_INFO);
+  int undo_label_id = IDS_CANCEL;
+  int settings_label_id = IDS_SYNC_CONFIRMATION_SETTINGS_BUTTON_LABEL;
+  int illustration_id =
+      IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_ILLUSTRATION_SVG;
+  int illustration_dark_id =
+      IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_ILLUSTRATION_DARK_SVG;
+  std::string illustration_path = "images/sync_confirmation_illustration.svg";
+  std::string illustration_dark_path =
+      "images/sync_confirmation_illustration_dark.svg";
 
   source->AddResourcePath(
       "sync_confirmation_app.js",
@@ -119,68 +121,65 @@ void SyncConfirmationUI::InitializeForSyncConfirmation(
 
   source->AddString("accountPictureUrl",
                     profiles::GetPlaceholderAvatarIconUrl());
-  switch (style) {
-    case SyncConfirmationStyle::kSigninInterceptModal: {
-      DCHECK(base::FeatureList::IsEnabled(kSyncPromoAfterSigninIntercept));
-      ProfileAttributesEntry* entry =
-          g_browser_process->profile_manager()
-              ->GetProfileAttributesStorage()
-              .GetProfileAttributesWithPath(profile_->GetPath());
-      DCHECK(entry);
-      std::u16string gaia_name = entry->GetGAIANameToDisplay();
-      if (gaia_name.empty())
-        gaia_name = entry->GetLocalProfileName();
-      AddStringResourceWithPlaceholder(
-          source, "syncConfirmationTitle",
-          IDS_SYNC_CONFIRMATION_WELCOME_TITLE_SIGNIN_INTERCEPT, gaia_name);
-      AddStringResource(source, "syncConfirmationSyncInfoTitle",
-                        IDS_SYNC_CONFIRMATION_SYNC_INFO_SIGNIN_INTERCEPT);
-      AddStringResource(source, "syncConfirmationConfirmLabel",
-                        IDS_SYNC_CONFIRMATION_TURN_ON_SYNC_BUTTON_LABEL);
-      AddStringResource(source, "syncConfirmationUndoLabel", IDS_NO_THANKS);
-      AddStringResource(source, "syncConfirmationSettingsLabel",
-                        IDS_SYNC_CONFIRMATION_SETTINGS_BUTTON_LABEL);
 
-      source->AddResourcePath(
-          "images/sync_confirmation_signin_intercept_illustration.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_SIGNIN_INTERCEPT_ILLUSTRATION_SVG);
-      source->AddResourcePath(
-          "images/sync_confirmation_signin_intercept_illustration_dark.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_SIGNIN_INTERCEPT_ILLUSTRATION_DARK_SVG);
-      break;
-    }
-    case SyncConfirmationStyle::kDefaultModal:
-      AddStringResource(source, "syncConfirmationTitle", title_id);
-      AddStringResource(source, "syncConfirmationSyncInfoTitle", info_title_id);
-      AddStringResource(source, "syncConfirmationConfirmLabel",
-                        confirm_label_id);
-      AddStringResource(source, "syncConfirmationUndoLabel", IDS_CANCEL);
-      AddStringResource(source, "syncConfirmationSettingsLabel",
-                        IDS_SYNC_CONFIRMATION_SETTINGS_BUTTON_LABEL);
-
-      source->AddResourcePath(
-          "images/sync_confirmation_illustration.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_ILLUSTRATION_SVG);
-      source->AddResourcePath(
-          "images/sync_confirmation_illustration_dark.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_ILLUSTRATION_DARK_SVG);
-      break;
-    case SyncConfirmationStyle::kWindow:
-      AddStringResource(source, "syncConfirmationTitle", title_id);
-      AddStringResource(source, "syncConfirmationSyncInfoTitle", info_title_id);
-      AddStringResource(source, "syncConfirmationConfirmLabel",
-                        confirm_label_id);
-      AddStringResource(source, "syncConfirmationUndoLabel", IDS_NO_THANKS);
-      AddStringResource(source, "syncConfirmationSettingsLabel",
-                        IDS_SYNC_CONFIRMATION_REFRESHED_SETTINGS_BUTTON_LABEL);
-      source->AddResourcePath(
-          "images/sync_confirmation_refreshed_illustration.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_REFRESHED_ILLUSTRATION_SVG);
-      source->AddResourcePath(
-          "images/sync_confirmation_refreshed_illustration_dark.svg",
-          IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_REFRESHED_ILLUSTRATION_DARK_SVG);
-      break;
+  // Default overrides without placeholders
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  title_id = IDS_SYNC_CONFIRMATION_TITLE_LACROS_NON_FORCED;
+#endif
+  if (style == SyncConfirmationStyle::kSigninInterceptModal) {
+    DCHECK(base::FeatureList::IsEnabled(kSyncPromoAfterSigninIntercept));
+    info_title_id = IDS_SYNC_CONFIRMATION_SYNC_INFO_SIGNIN_INTERCEPT;
+    confirm_label_id = IDS_SYNC_CONFIRMATION_TURN_ON_SYNC_BUTTON_LABEL;
+    undo_label_id = IDS_NO_THANKS;
+    illustration_path =
+        "images/sync_confirmation_signin_intercept_illustration.svg";
+    illustration_id =
+        IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_SIGNIN_INTERCEPT_ILLUSTRATION_SVG;
+    illustration_dark_path =
+        "images/sync_confirmation_signin_intercept_illustration_dark.svg";
+    illustration_dark_id =
+        IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_SIGNIN_INTERCEPT_ILLUSTRATION_DARK_SVG;
+  } else if (style == SyncConfirmationStyle::kWindow) {
+    undo_label_id = IDS_NO_THANKS;
+    settings_label_id = IDS_SYNC_CONFIRMATION_REFRESHED_SETTINGS_BUTTON_LABEL;
+    illustration_path = "images/sync_confirmation_refreshed_illustration.svg";
+    illustration_id =
+        IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_REFRESHED_ILLUSTRATION_SVG;
+    illustration_dark_path =
+        "images/sync_confirmation_refreshed_illustration_dark.svg";
+    illustration_dark_id =
+        IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_SYNC_CONFIRMATION_REFRESHED_ILLUSTRATION_DARK_SVG;
   }
+
+  // Registering and resolving the strings with placeholders
+  if (style == SyncConfirmationStyle::kSigninInterceptModal) {
+    ProfileAttributesEntry* entry =
+        g_browser_process->profile_manager()
+            ->GetProfileAttributesStorage()
+            .GetProfileAttributesWithPath(profile_->GetPath());
+    DCHECK(entry);
+    std::u16string gaia_name = entry->GetGAIANameToDisplay();
+    if (gaia_name.empty())
+      gaia_name = entry->GetLocalProfileName();
+    AddStringResourceWithPlaceholder(
+        source, "syncConfirmationTitle",
+        IDS_SYNC_CONFIRMATION_WELCOME_TITLE_SIGNIN_INTERCEPT, gaia_name);
+  } else {
+    AddStringResource(source, "syncConfirmationTitle", title_id);
+  }
+
+  // Registering and resolving the strings without placeholders
+  AddStringResource(source, "syncConfirmationSyncInfoTitle", info_title_id);
+  AddStringResource(source, "syncConfirmationConfirmLabel", confirm_label_id);
+  AddStringResource(source, "syncConfirmationUndoLabel", undo_label_id);
+  AddStringResource(source, "syncConfirmationSettingsLabel", settings_label_id);
+  AddStringResource(source, "syncConfirmationSyncInfoDesc",
+                    IDS_SYNC_CONFIRMATION_SYNC_INFO_DESC);
+  AddStringResource(source, "syncConfirmationSettingsInfo",
+                    IDS_SYNC_CONFIRMATION_SETTINGS_INFO);
+
+  source->AddResourcePath(illustration_path, illustration_id);
+  source->AddResourcePath(illustration_dark_path, illustration_dark_id);
 }
 
 void SyncConfirmationUI::InitializeForSyncDisabled(
