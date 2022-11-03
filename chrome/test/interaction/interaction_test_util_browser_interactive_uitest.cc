aea7c753971bbd221b6e0c26b88ad03d05d2616e
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-11-03 21:47:28 +0000

    Revert "[Test Automation] Re-enable disabled test and check for flakiness."
    This reverts commit 01a2e44d00719e0ce6ea6facf031b9ca781bf45f.
    
    Reason for revert: [sheriff] Looks like the test is still flaky.
    
    Original change's description:
    > [Test Automation] Re-enable disabled test and check for flakiness.
    >
    > Change-Id: Ibcac53dda772a4a400abbaebc74c22d0c9bf3871
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3933227
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1067130}
    
    Change-Id: I98bf8a6df177a246e1689765c10b94c9d24692aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004071
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Owners-Override: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067238}

--- chrome/test/interaction/interaction_test_util_browser_interactive_uitest.cc
+++ chrome/test/interaction/interaction_test_util_browser_interactive_uitest.cc
@@ -46,11 +46,12 @@ IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserUiTest,
 }
 
 // This test checks that we can attach to a WebUI that is not embedded in a tab.
-// TODO(crbug.com/1354017): May be flaky. Reopen bug if necessary.
+// TODO(crbug.com/1354017): Re-enable this test
 IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserUiTest,
-                       CompareScreenshot_SecondaryWebUI) {
+                       DISABLED_CompareScreenshot_SecondaryWebUI) {
   // This will capture the tab search page when it is displayed.
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kTabSearchPageElementId);
+  std::unique_ptr<WebContentsInteractionTestUtil> tab_search_page;
 
   // Need to wait for the tab items to actually show up in the tab list (this
   // can be asynchronous).
@@ -69,23 +70,31 @@ IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserUiTest,
       PressButton(kTabSearchButtonElementId),
       // Wait for the tab search bubble to appear and instrument its WebUI.
       AfterShow(kTabSearchBubbleElementId,
-                base::BindLambdaForTesting([&](ui::TrackedElement* element) {
+                base::BindLambdaForTesting([&](ui::InteractionSequence*,
+                                               ui::TrackedElement* element) {
                   auto* const bubble_view =
-                      AsView<WebUIBubbleDialogView>(element);
-                  InstrumentNonTabWebView(bubble_view->web_view(),
-                                          kTabSearchPageElementId);
+                      views::AsViewClass<WebUIBubbleDialogView>(
+                          element->AsA<views::TrackedElementViews>()->view());
+                  tab_search_page =
+                      WebContentsInteractionTestUtil::ForNonTabWebView(
+                          bubble_view->web_view(), kTabSearchPageElementId);
                 })),
       // Wait for the tab search page to appear, and then ensure it is
       // rendered at an appropriate size.
       AfterShow(kTabSearchPageElementId,
-                base::BindLambdaForTesting([&](ui::TrackedElement* el) {
-                  AsInstrumentedWebContents(el)->SendEventOnWebViewMinimumSize(
-                      kMinimumBubbleSize, kTabDataDisplayedEvent,
-                      kTabSearchItemQuery, kMinimumEntrySize);
-                })),
+                base::BindLambdaForTesting(
+                    [&](ui::InteractionSequence*, ui::TrackedElement* element) {
+                      tab_search_page->SendEventOnWebViewMinimumSize(
+                          kMinimumBubbleSize, kTabDataDisplayedEvent,
+                          kTabSearchItemQuery, kMinimumEntrySize);
+                    })),
       // With both the bubble and data at nonzero size, it should be safe to
       // take a screenshot.
-      AfterEvent(kTabSearchPageElementId, kTabDataDisplayedEvent,
-                 base::DoNothing()),
-      Screenshot(kTabSearchPageElementId, std::string(), "3664291"));
+      AfterEvent(
+          kTabSearchPageElementId, kTabDataDisplayedEvent,
+          base::BindLambdaForTesting([&](ui::InteractionSequence* sequence,
+                                         ui::TrackedElement* element) {
+            EXPECT_TRUE(InteractionTestUtilBrowser::CompareScreenshot(
+                element, std::string(), "3664291"));
+          })));
 }
