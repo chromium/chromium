0f3367e388eb4b3460db17bdb962b6d8ec7227e5
Collin Baker <collinbaker@chromium.org>
2022-11-03 21:08:02 +0000

    Handle multiple versions of same stdlib dependency name
    libstd is allowed to depend on multiple versions of the same crate,
    even though it is unusual. A recent upstream change added such a
    dependency.
    
    find_std_rlibs.py was incompatible with this: it expected to simply
    erase the metadata from a distribution's stdlib dep filenames, but
    this leads to conflicting libs if there's more than one with the same
    crate name.
    
    This CL enables multiple versions of the same lib while keeping
    behavior identical if there is only one version.
    
    A subsequent CL will actually use this new functionality.
    
    Bug: 1377429
    Change-Id: Iee02f5eee7c5c54a98aa9a1725682e223d4b3a5d
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg,linux-rust-x64-dbg,linux-rust-x64-rel,android-rust-arm-rel
    Change-Id: Iee02f5eee7c5c54a98aa9a1725682e223d4b3a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4000230
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067224}

--- build/rust/std/find_std_rlibs.py
+++ build/rust/std/find_std_rlibs.py
@@ -14,11 +14,10 @@ import shutil
 import subprocess
 import re
 
-REMOVE_METADATA_SUFFIX_RE = re.compile(b"-[0-9a-f]*", re.I)
+from collections import defaultdict
 
-
-def expand_name(concise_name):
-  return "lib%s.rlib" % concise_name
+EXPECTED_STDLIB_INPUT_REGEX = re.compile(r"([0-9a-z_]+)(?:-([0-9]+))?$")
+RLIB_NAME_REGEX = re.compile(r"lib([0-9a-z_]+)-([0-9a-f]+)\.rlib$")
 
 
 def main():
@@ -42,12 +41,24 @@ def main():
   parser.add_argument("--expected-rustc-version",
                       help="The string we expect to be reported by 'rustc -V'")
   args = parser.parse_args()
-  # Ensure we handle each rlib in the expected list exactly once.
+
+  # Expected rlibs by concise name (the crate name, plus a disambiguating suffix
+  # e.g. "-2" when necessary).
   if args.stdlibs:
-    rlibs_expected = [expand_name(x) for x in args.stdlibs.split(',')]
+    rlibs_expected = set()
+    for lib in args.stdlibs.split(','):
+      # The version is only included if there's more than one of `name`, and
+      # even then is only included for the 2nd onward.
+      (name, version) = EXPECTED_STDLIB_INPUT_REGEX.match(lib).group(1, 2)
+      if version is None:
+        rlibs_expected.add(name)
+      else:
+        rlibs_expected.add(f"{name}-{version}")
   else:
     rlibs_expected = None
-  rlibs_to_skip = [expand_name(x) for x in args.skip_stdlibs.split(',')]
+
+  rlibs_to_skip = set(args.skip_stdlibs.split(','))
+
   # First, ask rustc to confirm it's the version expected.
   rustc = os.path.join(args.rust_bin_dir, "rustc")
   if args.expected_rustc_version:
@@ -59,11 +70,13 @@ def main():
                       "but it was actually %s. Please adjust your "
                       "gn arguments to match." %
                       (args.expected_rustc_version, rustc_version))
+
   # Ask rustc where to find the stdlib for this target.
   rustc_args = [rustc, "--print", "target-libdir"]
   if args.target:
     rustc_args.extend(["--target", args.target])
-  rustlib_dir = subprocess.check_output(rustc_args).rstrip()
+  rustlib_dir = subprocess.check_output(rustc_args).rstrip().decode()
+
   # Copy the rlibs to a predictable location. Whilst we're doing so,
   # also write a .d file so that ninja knows it doesn't need to do this
   # again unless the source rlibs change.
@@ -74,35 +87,68 @@ def main():
     # single output depends on all the inputs. We choose any one of the
     # output rlibs for that purpose. If any of the input rlibs change, ninja
     # will run this script again and we'll copy them all afresh.
-    depfile.write("%s:" %
-                  (os.path.join(args.output, expand_name(args.depfile_target))))
-    for f in os.listdir(rustlib_dir):
-      if f.endswith(b'.rlib'):
-        # As standard Rust includes a hash on the end of each filename
-        # representing certain metadata, to ensure that clients will link
-        # against the correct version. As gn will be manually passing
-        # the correct file path to our linker invocations, we don't need
-        # that, and it would prevent us having the predictable filenames
-        # which we need for statically computable gn dependency rules.
-        (concise_name, count) = REMOVE_METADATA_SUFFIX_RE.subn(b"", f)
-        if count == 0:
-          raise Exception("Unable to remove suffix from %s" % f)
-        if concise_name.decode() in rlibs_to_skip:
-          continue
-        if rlibs_expected is not None:
-          if concise_name.decode() not in rlibs_expected:
-            raise Exception("Found stdlib rlib that wasn't expected: %s" %
-                            concise_name)
-          rlibs_expected.remove(concise_name.decode())
-        infile = os.path.join(rustlib_dir, f)
-        outfile = os.path.join(str.encode(args.output), concise_name)
-        depfile.write(" %s" % (infile.decode()))
-        if (not os.path.exists(outfile)
-            or os.stat(infile).st_mtime != os.stat(outfile).st_mtime):
-          if os.path.exists(outfile):
-            st = os.stat(outfile)
-            os.chmod(outfile, st.st_mode | stat.S_IWUSR)
-          shutil.copy(infile, outfile)
+    depfile.write(
+        "%s:" % (os.path.join(args.output, "lib%s.rlib" % args.depfile_target)))
+
+    # Each rlib is named "lib<crate_name>-<metadata>.rlib". The metadata
+    # disambiguates multiple crates of the same name. We want to throw away the
+    # metadata and use stable names. To do so, we replace the metadata bit with
+    # a simple number 1, 2, etc. It doesn't matter how we assign these numbers
+    # as long as it's consistent for a particular set of rlibs.
+
+    # The rlib names present in the Rust distribution, including metadata. We
+    # sort this list so crates of the same name are ordered by metadata. Also
+    # filter out names that aren't rlibs.
+    rlibs_present = [
+        name for name in os.listdir(rustlib_dir) if name.endswith('.rlib')
+    ]
+    rlibs_present.sort()
+
+    # Keep a count of the instances a crate name, so we can disambiguate the
+    # rlibs with an incrementing number at the end.
+    rlibs_seen = defaultdict(lambda: 0)
+
+    for f in rlibs_present:
+      # As standard Rust includes a hash on the end of each filename
+      # representing certain metadata, to ensure that clients will link
+      # against the correct version. As gn will be manually passing
+      # the correct file path to our linker invocations, we don't need
+      # that, and it would prevent us having the predictable filenames
+      # which we need for statically computable gn dependency rules.
+      (crate_name, metadata) = RLIB_NAME_REGEX.match(f).group(1, 2)
+      if crate_name in rlibs_to_skip:
+        continue
+
+      # Use the number of times we've seen this name to disambiguate the output
+      # filenames. Since we sort the input filenames including the metadata,
+      # this will be the same every time.
+      #
+      # Only append the times seen if it is greater than 1. This allows the
+      # BUILD.gn file to avoid adding '-1' to every name if there's only one
+      # version of a particular one.
+      rlibs_seen[crate_name] += 1
+      if rlibs_seen[crate_name] == 1:
+        concise_name = crate_name
+      else:
+        concise_name = "%s-%d" % (crate_name, rlibs_seen[crate_name])
+
+      output_filename = f"lib{concise_name}.rlib"
+
+      if rlibs_expected is not None:
+        if concise_name not in rlibs_expected:
+          raise Exception("Found stdlib rlib that wasn't expected: %s" % f)
+        rlibs_expected.remove(concise_name)
+
+      infile = os.path.join(rustlib_dir, f)
+      outfile = os.path.join(args.output, output_filename)
+      depfile.write(" %s" % infile)
+      if (not os.path.exists(outfile)
+          or os.stat(infile).st_mtime != os.stat(outfile).st_mtime):
+        if os.path.exists(outfile):
+          st = os.stat(outfile)
+          os.chmod(outfile, st.st_mode | stat.S_IWUSR)
+        shutil.copy(infile, outfile)
+
     depfile.write("\n")
     if rlibs_expected:
       raise Exception("We failed to find all expected stdlib rlibs: %s" %
