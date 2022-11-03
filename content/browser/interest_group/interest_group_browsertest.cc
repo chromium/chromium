f98e95a06a7228dc5279081c0ba4eb6c5f9b1125
Caleb Raitto <caraitto@chromium.org>
2022-11-03 00:02:46 +0000

    Test DirectFromSellerSignals renderer validation
    Bug: 1186444,1320908
    Change-Id: I7c452ce3f493b82559ba1cddb5b8229bc242ad9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998697
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066799}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -2977,6 +2977,74 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
+                       RunAdAuctionInvalidDirectFromSellerSignalsInvalidURL) {
+  ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
+
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://invalid^&' for AuctionAdConfig with "
+      "seller 'https://test.com' cannot be resolved to a valid URL.",
+      RunAuctionAndWait(R"({
+      seller: 'https://test.com',
+      decisionLogicUrl: 'https://test.com',
+      directFromSellerSignals: 'https://invalid^&'
+  })"));
+  WaitForAccessObserved({});
+}
+
+IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
+                       RunAdAuctionInvalidDirectFromSellerSignalsNotHttps) {
+  ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
+
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'http://test.com/signals' for AuctionAdConfig "
+      "with seller 'https://test.com' must match seller origin; only https "
+      "scheme is supported.",
+      RunAuctionAndWait(R"({
+      seller: 'https://test.com',
+      decisionLogicUrl: 'https://test.com',
+      directFromSellerSignals: 'http://test.com/signals'
+  })"));
+  WaitForAccessObserved({});
+}
+
+IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
+                       RunAdAuctionInvalidDirectFromSellerSignalsWrongOrigin) {
+  ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
+
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://test2.com/signals' for AuctionAdConfig "
+      "with seller 'https://test.com' must match seller origin; only https "
+      "scheme is supported.",
+      RunAuctionAndWait(R"({
+      seller: 'https://test.com',
+      decisionLogicUrl: 'https://test.com',
+      directFromSellerSignals: 'https://test2.com/signals'
+  })"));
+  WaitForAccessObserved({});
+}
+
+IN_PROC_BROWSER_TEST_F(
+    InterestGroupBrowserTest,
+    RunAdAuctionInvalidDirectFromSellerSignalsHasQueryString) {
+  ASSERT_TRUE(NavigateToURL(shell(), https_server_->GetURL("a.test", "/echo")));
+
+  EXPECT_EQ(
+      "TypeError: Failed to execute 'runAdAuction' on 'Navigator': "
+      "directFromSellerSignals 'https://test.com/signals?shouldntBeHere' for "
+      "AuctionAdConfig with seller 'https://test.com' URL prefix must not have "
+      "a query string.",
+      RunAuctionAndWait(R"({
+      seller: 'https://test.com',
+      decisionLogicUrl: 'https://test.com',
+      directFromSellerSignals: 'https://test.com/signals?shouldntBeHere'
+  })"));
+  WaitForAccessObserved({});
+}
+
+IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
                        RunAdAuctionBuyersNoInterestGroup) {
   GURL test_url = https_server_->GetURL("a.test", "/echo");
   ASSERT_TRUE(NavigateToURL(shell(), test_url));
