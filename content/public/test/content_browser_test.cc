215880e6fa2157c18c891c8b9a2d982b00b37491
danakj <danakj@chromium.org>
2022-11-04 21:19:05 +0000

    Remove Rust browsertest support code
    We won't be writing browser tests in Rust any time soon.
    
    R=creis@chromium.org
    
    Bug: 1381552
    Change-Id: Iae21a04bfbe42210afe815f2976902792c606936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006855
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1067689}

--- content/public/test/content_browser_test.cc
+++ content/public/test/content_browser_test.cc
@@ -179,8 +179,4 @@ base::FilePath ContentBrowserTest::GetTestDataFilePath() {
   return base::FilePath(FILE_PATH_LITERAL("content/test/data"));
 }
 
-#if defined(RUST_ENABLED)
-RUST_BROWSERTEST_TEST_SUITE_FACTORY(ContentBrowserTest);
-#endif
-
 }  // namespace content
