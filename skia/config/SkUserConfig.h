a5ff9d0e23067728c33542ee0f9440448d342132
Brian Osman <brianosman@google.com>
2022-11-04 14:19:10 +0000

    Guard upcoming change to Skia's raster pipeline clamping logic
    Bug: skia:13715
    Change-Id: Ie15ff2a24185b8ce26e6389c87ff941ccabcf24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004604
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1067522}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -218,6 +218,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_USE_LEGACY_MIPMAP_BUILDER
 
+#define SK_USE_LEGACY_GAMUT_CLAMP
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
