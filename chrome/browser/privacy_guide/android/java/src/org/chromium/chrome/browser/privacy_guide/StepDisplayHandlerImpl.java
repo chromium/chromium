e63e2913940794c046f9ba040500c4b0a630d88c
Rubin Deliallisi <rubindl@chromium.org>
2022-11-07 10:26:29 +0000

    [Privacy Guide]: Cookie fragment visibility
    Implementation of the conditions for which the Cookies page should or should not be displayed.
    
    Bug: 1238896
    Change-Id: Iab4e9bca3ae8e71cf1b9b40991992dce2ddb593c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4005955
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068028}

--- chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImpl.java
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImpl.java
@@ -4,9 +4,15 @@
 
 package org.chromium.chrome.browser.privacy_guide;
 
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingBridge;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingState;
 import org.chromium.chrome.browser.sync.SyncService;
+import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
+import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.content_settings.CookieControlsMode;
+import org.chromium.components.content_settings.PrefNames;
+import org.chromium.components.user_prefs.UserPrefs;
 
 /**
  * Computes for each privacy guide step whether it should be displayed or not.
@@ -25,7 +31,11 @@ class StepDisplayHandlerImpl implements StepDisplayHandler {
 
     @Override
     public boolean shouldDisplayCookies() {
-        // TODO: Check the settings for the CookiesFragment.
-        return true;
+        boolean allowCookies = WebsitePreferenceBridge.isCategoryEnabled(
+                Profile.getLastUsedRegularProfile(), ContentSettingsType.COOKIES);
+        @CookieControlsMode
+        int cookieControlsMode = UserPrefs.get(Profile.getLastUsedRegularProfile())
+                                         .getInteger(PrefNames.COOKIE_CONTROLS_MODE);
+        return allowCookies && cookieControlsMode != CookieControlsMode.OFF;
     }
 }
--- chrome/browser/privacy_guide/android/junit/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImplTest.java
+++ chrome/browser/privacy_guide/android/junit/src/org/chromium/chrome/browser/privacy_guide/StepDisplayHandlerImplTest.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.privacy_guide;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,16 +14,24 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingBridge;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingBridgeJni;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingState;
 import org.chromium.chrome.browser.sync.SyncService;
+import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
+import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridgeJni;
+import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.content_settings.CookieControlsMode;
+import org.chromium.components.content_settings.PrefNames;
+import org.chromium.components.prefs.PrefService;
+import org.chromium.components.user_prefs.UserPrefs;
+import org.chromium.components.user_prefs.UserPrefsJni;
 
 /**
  * JUnit tests of the class {@link StepDisplayHandlerImpl}.
@@ -31,7 +40,7 @@ import org.chromium.chrome.browser.sync.SyncService;
 @RunWith(BaseRobolectricTestRunner.class)
 public class StepDisplayHandlerImplTest {
     @Rule
-    public JniMocker mocker = new JniMocker();
+    public JniMocker mMocker = new JniMocker();
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
@@ -39,13 +48,27 @@ public class StepDisplayHandlerImplTest {
     private SafeBrowsingBridge.Natives mSBNativesMock;
     @Mock
     private SyncService mSyncService;
+    @Mock
+    private Profile mProfile;
+    @Mock
+    private PrefService mPrefServiceMock;
+    @Mock
+    private UserPrefs.Natives mUserPrefsNativesMock;
+    @Mock
+    private WebsitePreferenceBridge.Natives mWebsitePreferenceNativesMock;
 
     private StepDisplayHandler mStepDisplayHandler;
 
     @Before
     public void setUp() {
+        Profile.setLastUsedProfileForTesting(mProfile);
+        mMocker.mock(UserPrefsJni.TEST_HOOKS, mUserPrefsNativesMock);
+        when(mUserPrefsNativesMock.get(mProfile)).thenReturn(mPrefServiceMock);
+
+        mMocker.mock(WebsitePreferenceBridgeJni.TEST_HOOKS, mWebsitePreferenceNativesMock);
+
         SyncService.overrideForTests(mSyncService);
-        mocker.mock(SafeBrowsingBridgeJni.TEST_HOOKS, mSBNativesMock);
+        mMocker.mock(SafeBrowsingBridgeJni.TEST_HOOKS, mSBNativesMock);
         mStepDisplayHandler = new StepDisplayHandlerImpl();
     }
 
@@ -55,11 +78,19 @@ public class StepDisplayHandlerImplTest {
     }
 
     private void setSBState(@SafeBrowsingState int sbState) {
-        Mockito.when(mSBNativesMock.getSafeBrowsingState()).thenReturn(sbState);
+        when(mSBNativesMock.getSafeBrowsingState()).thenReturn(sbState);
     }
 
     private void setSyncState(boolean enabled) {
-        Mockito.when(mSyncService.isSyncFeatureEnabled()).thenReturn(enabled);
+        when(mSyncService.isSyncFeatureEnabled()).thenReturn(enabled);
+    }
+
+    private void setCookieState(@CookieControlsMode int cookieControlsMode, boolean allowCookies) {
+        when(mPrefServiceMock.getInteger(PrefNames.COOKIE_CONTROLS_MODE))
+                .thenReturn(cookieControlsMode);
+        when(mWebsitePreferenceNativesMock.isContentSettingEnabled(
+                     mProfile, ContentSettingsType.COOKIES))
+                .thenReturn(allowCookies);
     }
 
     @Test
@@ -90,5 +121,29 @@ public class StepDisplayHandlerImplTest {
     public void testDontDisplayHistorySyncWhenSyncOff() {
         setSyncState(false);
         assertFalse(mStepDisplayHandler.shouldDisplaySync());
+    }
+
+    @Test
+    public void testDontDisplayCookiesWhenCookiesAllAllowed() {
+        setCookieState(CookieControlsMode.OFF, true);
+        assertFalse(mStepDisplayHandler.shouldDisplayCookies());
+    }
+
+    @Test
+    public void testDisplayCookiesWhenCookiesThirdPartyBlocked() {
+        setCookieState(CookieControlsMode.BLOCK_THIRD_PARTY, true);
+        assertTrue(mStepDisplayHandler.shouldDisplayCookies());
+    }
+
+    @Test
+    public void testDisplayCookiesWhenCookiesIncognitoBlocked() {
+        setCookieState(CookieControlsMode.INCOGNITO_ONLY, true);
+        assertTrue(mStepDisplayHandler.shouldDisplayCookies());
+    }
+
+    @Test
+    public void testDontDisplayCookiesWhenCookiesAllBlocked() {
+        setCookieState(CookieControlsMode.BLOCK_THIRD_PARTY, false);
+        assertFalse(mStepDisplayHandler.shouldDisplayCookies());
     }
 }
