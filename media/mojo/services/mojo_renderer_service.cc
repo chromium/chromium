6ae30623c07afa67496812a1f824fc7edae422fd
Xiaohan Wang <xhwang@chromium.org>
2022-11-07 22:48:44 +0000

    media: Minor cleanup in MojoRendererService
    Use constexpr for timedelta which is cleaner. Something I happen to see
    and fix.
    
    Bug: None
    Test: None
    Change-Id: I977e9765a5719db60707d8e66313da070a1243bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4006200
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068369}

--- media/mojo/services/mojo_renderer_service.cc
+++ media/mojo/services/mojo_renderer_service.cc
@@ -9,6 +9,7 @@
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/time/time.h"
 #include "media/base/cdm_context.h"
 #include "media/base/media_url_demuxer.h"
 #include "media/base/renderer.h"
@@ -20,7 +21,7 @@
 namespace media {
 
 // Time interval to update media time.
-const int kTimeUpdateIntervalMs = 50;
+constexpr auto kTimeUpdateInterval = base::Milliseconds(50);
 
 // static
 mojo::SelfOwnedReceiverRef<mojom::Renderer> MojoRendererService::Create(
@@ -245,7 +246,7 @@ void MojoRendererService::UpdateMediaTime(bool force) {
   base::TimeDelta max_time = media_time;
   // Allow some slop to account for delays in scheduling time update tasks.
   if (time_update_timer_.IsRunning() && (playback_rate_ > 0))
-    max_time += base::Milliseconds(2 * kTimeUpdateIntervalMs);
+    max_time += 2 * kTimeUpdateInterval;
 
   client_->OnTimeUpdate(media_time, max_time, base::TimeTicks::Now());
   last_media_time_ = media_time;
@@ -263,7 +264,7 @@ void MojoRendererService::SchedulePeriodicMediaTimeUpdates() {
 
   UpdateMediaTime(true);
   time_update_timer_.Start(
-      FROM_HERE, base::Milliseconds(kTimeUpdateIntervalMs),
+      FROM_HERE, kTimeUpdateInterval,
       base::BindRepeating(&MojoRendererService::UpdateMediaTime, weak_this_,
                           false));
 }
