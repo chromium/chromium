10767ac0a15bf970a1f3588b5ba774e2b023e5d4
Nico Weber <thakis@chromium.org>
2022-11-09 19:00:13 +0000

    win: Get rid of courgette.exe
    There's an old TODO about moving from courgette.exe to courgette64.exe.
    See if we can actually do it!
    
    Cq-Include-Trybots: luci.chromium.try:win-official,win32-official
    Cq-Include-Trybots: luci.chrome.try:win-chrome
    Bug: 629243,1382256
    Change-Id: I26dfa10e68e115359a908cecc8f08db26b1f459c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4013527
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069263}

--- chrome/tools/build/win/create_installer_archive.py
+++ chrome/tools/build/win/create_installer_archive.py
@@ -34,7 +34,7 @@ CHROME_PATCH_FILE_SUFFIX = "_patch"  # prefixed by options.output_name
 COMPRESSED_ARCHIVE_SUFFIX = ".packed.7z"
 
 COMPRESSED_FILE_EXT = ".packed.7z"     # extension of patch archive file
-COURGETTE_EXEC = "courgette.exe"
+COURGETTE_EXEC = "courgette64.exe"
 MINI_INSTALLER_INPUT_FILE = "packed_files.txt"
 PATCH_FILE_EXT = '.diff'
 SETUP_EXEC = "setup.exe"
@@ -599,7 +599,7 @@ def _ParseOptions():
   parser.add_option('-l', '--last_chrome_installer',
       help='Generate differential installer. The value of this parameter '
            'specifies the directory that contains base versions of '
-           'setup.exe, courgette.exe (if --diff_algorithm is COURGETTE) '
+           'setup.exe, courgette64.exe (if --diff_algorithm is COURGETTE) '
            '& chrome.7z.')
   parser.add_option('-f', '--setup_exe_format', default='COMPRESSED',
       help='How setup.exe should be included {COMPRESSED|DIFF|FULL}.')
