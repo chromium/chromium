b14d1dd130b250afd7d509fe245cedeb5570e77c
Tom Burgin <bur@chromium.org>
2022-11-09 02:53:08 +0000

    immersive fullscreen mac: fix web content view hit test
    In immersive fullscreen right clicks in certain sections of the web
    content view are being routed to top chrome. This results in the
    incorrect context menu being displayed. This is expected behavior
    in non-immersive fullscreen since top chrome does indeed live in the
    area defined in the hit test.
    
    This CL bypasses the top chrome hit test calculations when in immersive
    fullscreen.
    
    Bug: 1369641
    Change-Id: I2ac66699b46a7029e90458b7df124e8a7f169c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4008689
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Tom Burgin <bur@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1068944}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -3789,6 +3789,15 @@ views::CloseRequestResult BrowserView::OnWindowCloseRequested() {
 }
 
 int BrowserView::NonClientHitTest(const gfx::Point& point) {
+#if BUILDFLAG(IS_MAC)
+  // The top container while in immersive fullscreen on macOS lives in another
+  // Widget (OverlayWidget). This means that BrowserView does not need to
+  // consult BrowserViewLayout::NonClientHitTest() to calculate the hit test.
+  if (IsImmersiveModeEnabled()) {
+    return views::ClientView::NonClientHitTest(point);
+  }
+#endif  // BUILDFLAG(IS_MAC)
+
   return GetBrowserViewLayout()->NonClientHitTest(point);
 }
 
