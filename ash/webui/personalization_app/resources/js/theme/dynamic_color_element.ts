138bf6913fcd91f9fb3248f2bf71f69e0cc70eea
Erica Lee <ericamlee@google.com>
2022-11-09 23:09:31 +0000

    [personalization] dynamic color key handling
    Add button highlighting similar to the theme buttons -- a single button
    is tabbable, and you can use arrows to move left and right among the
    buttons.
    
    The screencast uses a modified button highlighting color, because
    `cros-focus-ring-color-rgb` doesn't exist in Dynamic Color yet.
    
    BUG=b:254480759
    TEST=https://screencast.googleplex.com/cast/NDUzNjg5MDg4MDQyNTk4NHxjOTVlMjA0YS00MQ
    
    Change-Id: I080c6b7fc208acc3d5b3b168ddf67ad8f7084280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4004130
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069419}

--- ash/webui/personalization_app/resources/js/theme/dynamic_color_element.ts
+++ ash/webui/personalization_app/resources/js/theme/dynamic_color_element.ts
@@ -6,8 +6,15 @@
  * @fileoverview This component displays the dynamic color options.
  */
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import '../../css/common.css.js';
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
+import 'chrome://resources/polymer/v3_0/iron-a11y-keys/iron-a11y-keys.js';
+import 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
+
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import {IronA11yKeysElement} from 'chrome://resources/polymer/v3_0/iron-a11y-keys/iron-a11y-keys.js';
+import {IronSelectorElement} from 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
 
 import {WithPersonalizationStore} from '../personalization_store.js';
 
@@ -18,6 +25,14 @@ export interface DynamicColorScheme {
   primaryColor: string;
   secondaryColor: string;
   tertiaryColor: string;
+}
+
+export interface DynamicColorElement {
+  $: {
+    keys: IronA11yKeysElement,
+    colorSchemeSelector: IronSelectorElement,
+    staticColorSelector: IronSelectorElement,
+  };
 }
 
 export class DynamicColorElement extends WithPersonalizationStore {
@@ -83,12 +98,70 @@ export class DynamicColorElement extends WithPersonalizationStore {
           ];
         },
       },
+      // The color scheme button currently highlighted by keyboard navigation.
+      colorSchemeSelectedButton_: {
+        type: Object,
+        notify: true,
+      },
+      // The static color button currently highlighted by keyboard navigation.
+      staticColorSelectedButton_: {
+        type: Object,
+        notify: true,
+      },
     };
   }
 
   automaticSeedColorEnabled: boolean;
   private staticColors_: string[];
   private schemes_: DynamicColorScheme[];
+  private colorSchemeSelectedButton_: CrButtonElement;
+  private staticColorSelectedButton_: CrButtonElement;
+
+  override ready() {
+    super.ready();
+    this.$.keys.target = this.automaticSeedColorEnabled ?
+        this.$.colorSchemeSelector :
+        this.$.staticColorSelector;
+  }
+
+  /** Handle keyboard navigation. */
+  private onKeysPress_(
+      e: CustomEvent<{key: string, keyboardEvent: KeyboardEvent}>) {
+    const selector = this.automaticSeedColorEnabled ?
+        this.$.colorSchemeSelector :
+        this.$.staticColorSelector;
+    const prevButton = this.automaticSeedColorEnabled ?
+        this.colorSchemeSelectedButton_ :
+        this.staticColorSelectedButton_;
+    switch (e.detail.key) {
+      case 'left':
+        selector.selectPrevious();
+        break;
+      case 'right':
+        selector.selectNext();
+        break;
+      default:
+        return;
+    }
+    // Remove focus state of previous button.
+    if (prevButton) {
+      prevButton.removeAttribute('tabindex');
+    }
+    // Add focus state for new button.
+    if (this.automaticSeedColorEnabled && this.colorSchemeSelectedButton_) {
+      this.colorSchemeSelectedButton_.setAttribute('tabindex', '0');
+      this.colorSchemeSelectedButton_.focus();
+    }
+    if (!this.automaticSeedColorEnabled && this.staticColorSelectedButton_) {
+      this.staticColorSelectedButton_.setAttribute('tabindex', '0');
+      this.staticColorSelectedButton_.focus();
+    }
+    e.detail.keyboardEvent.preventDefault();
+  }
+
+  private getTabIndex_(id: string): string {
+    return id === 'tonal' || id === 'var(--google-blue-500)' ? '0' : '-1';
+  }
 }
 
 customElements.define(DynamicColorElement.is, DynamicColorElement);
