c2241f06c9c26e87041d5ccec660b3d5c3028e89
Nate Chapin <japhet@chromium.org>
2022-11-09 19:03:32 +0000

    Revert "Split up SAA tests, add test for basic permission states."
    This reverts commit 83372fa176ed0ecafe1e9b41ffa79317e61a1500.
    
    Reason for revert: Failures on mac bots (e.g., https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/12813/overview)
    
    Original change's description:
    > Split up SAA tests, add test for basic permission states.
    >
    > This removes the recurive monolithical single SAA test file in favor of
    > smaller ones. This helps test grant cases where we need to have both
    > user interaction and permission grants, which were extremely unreliable
    > with the larger test file.
    >
    > Bug: 1378969, 1374277
    > Change-Id: Iebc9bd48185ff398181bf1a16c35995a2467cb54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3986031
    > Commit-Queue: Johann Hofmann <johannhof@chromium.org>
    > Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1069134}
    
    Bug: 1378969, 1374277
    Change-Id: I8bd132001df0a4b0b42a21fcafa39493cd464a13
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018473
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069267}

--- third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess.sub.https.window.js
@@ -7,7 +7,10 @@
 //
 // testPrefix: Prefix each test case with an indicator so we know what context
 // they are run in if they are used in multiple iframes.
-const {testPrefix} = processQueryParams();
+//
+// topLevelDocument: Keep track of if we run these tests in a nested context, we
+// don't want to recurse forever.
+const {testPrefix, topLevelDocument} = processQueryParams();
 
 // Common tests to run in all frames.
 test(() => {
@@ -19,53 +22,60 @@ promise_test(t => {
     "document.requestStorageAccess() call without user gesture");
 }, "[" + testPrefix + "] document.requestStorageAccess() should be rejected with a NotAllowedError by default with no user gesture");
 
-promise_test(
-    async () => {
-      await test_driver.set_permission(
-          {name: 'storage-access'}, 'granted');
+// Logic to load test cases within combinations of iFrames.
+if (topLevelDocument) {
+  // This specific test will run only as a top level test (not as a worker).
+  // Specific requestStorageAccess() scenarios will be tested within the context
+  // of various iFrames
+  promise_test(t => {
+    const description = "document.requestStorageAccess() call in a detached frame";
+    // Can't use `promise_rejects_dom` here, since the error comes from the wrong global.
+    return RunRequestStorageAccessInDetachedFrame()
+      .then(t.unreached_func("Should have rejected: " + description), (e) => {
+        assert_equals(e.name, 'InvalidStateError', description);
+      });
+  }, "[non-fully-active] document.requestStorageAccess() should not resolve when run in a detached frame");
 
-      const {promise} = await RunCallbackWithGesture('b1', () => document.requestStorageAccess());
-      await promise;
+  promise_test(t => {
+    return promise_rejects_dom(t, 'InvalidStateError', RunRequestStorageAccessViaDomParser(),
+     "document.requestStorageAccess() in a detached DOMParser result");
+  }, "[non-fully-active] document.requestStorageAccess() should not resolve when run in a detached DOMParser document");
 
-      // Cleanup
-      await test_driver.set_permission(
-          {name: 'storage-access'}, 'prompt');
-    },
-    '[' + testPrefix +
-        '] document.requestStorageAccess() should be resolved when called properly with a user gesture');
+  // Create a test with a single-child same-origin iframe.
+  const sameOriginFramePromise = RunTestsInIFrame(
+      'resources/requestStorageAccess-iframe.html?testCase=same-origin-frame&rootdocument=false');
 
-if (testPrefix == 'cross-origin-frame' || testPrefix == 'nested-cross-origin-frame') {
-  promise_test(
-      async t => {
-        await test_driver.set_permission(
-            {name: 'storage-access'}, 'prompt');
+  // Create a test with a single-child cross-origin iframe.
+  const crossOriginFramePromise = RunTestsInIFrame(
+      'https://{{domains[www]}}:{{ports[https][0]}}/storage-access-api/resources/requestStorageAccess-iframe.https.html?testCase=cross-origin-frame&rootdocument=false');
 
-        const {promise} = RunCallbackWithGesture('b2', () => {
-          return promise_rejects_dom(t, "NotAllowedError", document.requestStorageAccess(),
-            "document.requestStorageAccess() call without permission");
-        });
+  // Validate the nested-iframe scenario where the same-origin frame
+  // containing the tests is not the first child.
+  const nestedSameOriginFramePromise = RunTestsInNestedIFrame(
+      'resources/requestStorageAccess-iframe.html?testCase=nested-same-origin-frame&rootdocument=false');
 
-        await promise;
-      },
-      '[' + testPrefix +
-          '] document.requestStorageAccess() should be rejected with a NotAllowedError without permission grant');
+  // Validate the nested-iframe scenario where the cross-origin frame
+  // containing the tests is not the first child.
+  const nestedCrossOriginFramePromise = RunTestsInNestedIFrame(
+      'https://{{domains[www]}}:{{ports[https][0]}}/storage-access-api/resources/requestStorageAccess-iframe.https.html?testCase=nested-cross-origin-frame&rootdocument=false');
 
-  promise_test(
-      async t => {
-        await test_driver.set_permission(
-            {name: 'storage-access'}, 'denied');
-
-        const {promise} = RunCallbackWithGesture('b3', () => {
-          return promise_rejects_dom(t, "NotAllowedError", document.requestStorageAccess(),
-            "document.requestStorageAccess() call without permission");
-        });
-
-        await promise;
-
-        // Cleanup
-        await test_driver.set_permission(
-            {name: 'storage-access'}, 'prompt');
-      },
-      '[' + testPrefix +
-          '] document.requestStorageAccess() should be rejected with a NotAllowedError with denied permission');
+  // Because the iframe tests expect no user activation, and because they
+  // load asynchronously, we want to first run those tests before simulating
+  // clicks on the page.
+  Promise
+      .all([
+        sameOriginFramePromise,
+        crossOriginFramePromise,
+        nestedSameOriginFramePromise,
+        nestedCrossOriginFramePromise,
+      ])
+      .then(() => {
+        promise_test(
+            async () => {
+              const {promise} = await RunCallbackWithGesture('b1', () => document.requestStorageAccess());
+              return promise;
+            },
+            '[' + testPrefix +
+                '] document.requestStorageAccess() should be resolved when called properly with a user gesture');
+      });
 }
