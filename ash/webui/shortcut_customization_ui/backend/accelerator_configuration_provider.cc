aeda72d604b7d77bb6ab51406672ea7ea293b391
Longbo Wei <longbowei@google.com>
2022-11-09 21:28:27 +0000

    Revert "Add initial alias acceleratorInfos for top-row keys"
    This reverts commit 8c0419000cef33b00d44115c47017d96a79b16c0.
    
    Reason for revert: AcceleratorConfigurationProviderTest.ConnectedKeyboardsUpdated
    failing on several bots, it could be related to this CL.
    
    Original change's description:
    > Add initial alias acceleratorInfos for top-row keys
    >
    > * If a shortcut has a top-row key([modifier] + topRowKey), when
    >   prefs::kSendFunctionKeys is true, it will change to [search]
    >   [modifier] + [FKeys]
    > * This change is based off of kFkeysToSystemKeys2
    > * Will implement per-keyboard alias for top-row accelerators in the
    >   future.
    >
    > Bug: b:216049298
    > Test: ash_webui_unittest
    > Change-Id: I230c3f787488a8c42a237ff3a757b6ebdea8d2d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967885
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: Longbo Wei <longbowei@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1069206}
    
    Bug: b:216049298
    Change-Id: I22c829154ec5b8a52eecf66c0d85c3158b27c612
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4018158
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069274}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -8,14 +8,10 @@
 
 #include "ash/accelerators/accelerator_layout_table.h"
 #include "ash/accelerators/ash_accelerator_configuration.h"
-#include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/accelerators_util.h"
-#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
-#include "base/containers/fixed_flat_map.h"
 #include "base/containers/flat_map.h"
-#include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/clone_traits.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -28,24 +24,6 @@
 namespace ash {
 
 namespace {
-
-// A map between Top row keys to Function keys.
-// TODO(longbowei): This mapping is temporary, create a helper function in
-// `ui/chromeos/events/keyboard_layout_util.h` to handle fetching the layout
-// keys.
-constexpr auto kLayout2TopRowKeyToFKeyMap =
-    base::MakeFixedFlatMap<ui::KeyboardCode, ui::KeyboardCode>({
-        {ui::KeyboardCode::VKEY_BROWSER_BACK, ui::KeyboardCode::VKEY_F1},
-        {ui::KeyboardCode::VKEY_BROWSER_FORWARD, ui::KeyboardCode::VKEY_F2},
-        {ui::KeyboardCode::VKEY_BROWSER_REFRESH, ui::KeyboardCode::VKEY_F3},
-        {ui::KeyboardCode::VKEY_ZOOM, ui::KeyboardCode::VKEY_F4},
-        {ui::KeyboardCode::VKEY_MEDIA_LAUNCH_APP1, ui::KeyboardCode::VKEY_F5},
-        {ui::KeyboardCode::VKEY_BRIGHTNESS_DOWN, ui::KeyboardCode::VKEY_F6},
-        {ui::KeyboardCode::VKEY_BRIGHTNESS_UP, ui::KeyboardCode::VKEY_F7},
-        {ui::KeyboardCode::VKEY_VOLUME_MUTE, ui::KeyboardCode::VKEY_F8},
-        {ui::KeyboardCode::VKEY_VOLUME_DOWN, ui::KeyboardCode::VKEY_F9},
-        {ui::KeyboardCode::VKEY_VOLUME_UP, ui::KeyboardCode::VKEY_F10},
-    });
 
 mojom::AcceleratorInfoPtr CreateAcceleratorInfo(
     const ui::Accelerator& accelerator,
@@ -78,57 +56,6 @@ mojom::AcceleratorLayoutInfoPtr LayoutInfoToMojom(
   layout_info->action = static_cast<uint32_t>(action_id);
 
   return layout_info;
-}
-
-bool TopRowKeysAreFunctionKeys() {
-  const PrefService* pref_service =
-      Shell::Get()->session_controller()->GetLastActiveUserPrefService();
-  if (!pref_service)
-    return false;
-  return pref_service->GetBoolean(prefs::kSendFunctionKeys);
-}
-
-bool IsTopRowKey(const ui::KeyboardCode& accelerator_keycode) {
-  // A set that includes all top row keys from different keyboards.
-  // TODO(longbowei): Now only include top row keys from layout2, add more top
-  // row keys from other keyboards in the future.
-  static const base::NoDestructor<base::flat_set<ui::KeyboardCode>>
-      top_row_action_keys({
-          ui::VKEY_BROWSER_BACK,
-          ui::VKEY_BROWSER_REFRESH,
-          ui::VKEY_ZOOM,
-          ui::VKEY_MEDIA_LAUNCH_APP1,
-          ui::VKEY_BRIGHTNESS_DOWN,
-          ui::VKEY_BRIGHTNESS_UP,
-          ui::VKEY_MEDIA_PLAY_PAUSE,
-          ui::VKEY_VOLUME_MUTE,
-          ui::VKEY_VOLUME_DOWN,
-          ui::VKEY_VOLUME_UP,
-      });
-  return base::Contains(*top_row_action_keys, accelerator_keycode);
-}
-
-ui::Accelerator CreateTopRowRemappedAccelerator(ui::Accelerator accelerator) {
-  if (TopRowKeysAreFunctionKeys() &&
-      kLayout2TopRowKeyToFKeyMap.contains(accelerator.key_code())) {
-    // If top row keys are function keys, top row shortcut will become
-    // [Fkey] + [search] + [modifiers]
-    ui::Accelerator new_accelerator(
-        kLayout2TopRowKeyToFKeyMap.at(accelerator.key_code()),
-        accelerator.modifiers() | ui::EF_COMMAND_DOWN, accelerator.key_state());
-    return new_accelerator;
-  }
-
-  return accelerator;
-}
-
-ui::Accelerator CreateRemappedAccelerator(ui::Accelerator accelerator) {
-  // TODO(longbowei): Add six-pack remapping in the next CL.
-  if (IsTopRowKey(accelerator.key_code())) {
-    return CreateTopRowRemappedAccelerator(accelerator);
-  }
-
-  return accelerator;
 }
 
 }  // namespace
@@ -247,17 +174,12 @@ AcceleratorConfigurationProvider::CreateConfigurationMap() {
     for (const auto& [action_id, accelerators] : id_to_accelerators) {
       std::vector<mojom::AcceleratorInfoPtr> infos_mojom;
       for (const auto& accelerator : accelerators) {
-        // Update the accelerator by doing F-keys remapping
-        // TODO(longbowei): Add six-pack remapping in next CL.
-        ui::Accelerator updated_accelerator =
-            CreateRemappedAccelerator(accelerator);
-
         // |locked| and and |has_key_event| are both default to true now.
         // For |locked|, ash accelerators should not be locked when
         // customization is allowed. For |has_key_event|, we need to determine
         // its state based off of a keyboard device id.
         mojom::AcceleratorInfoPtr info = CreateAcceleratorInfo(
-            updated_accelerator, /*locked=*/true,
+            accelerator, /*locked=*/true,
             /*has_key_event=*/true, GetAcceleratorType(accelerator),
             mojom::AcceleratorState::kEnabled);
         infos_mojom.push_back(std::move(info));
