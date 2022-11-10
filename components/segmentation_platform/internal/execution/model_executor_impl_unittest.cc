c1f84f8ce5b522dbd999b3e77a5eaabcab522cc9
Stephan Hartmann <stha09@googlemail.com>
2022-11-10 09:02:25 +0000

    GCC: use double-brace init in model_executor_impl_unittest.cc
    Otherwise GCC wants to use an explicit constructor for
    absl::optional which isn't suitable.
    
    Bug: 819294
    Change-Id: I89bee2c8fc1331a02c1d58d80bc0489bcb8b3c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016875
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1069646}

--- components/segmentation_platform/internal/execution/model_executor_impl_unittest.cc
+++ components/segmentation_platform/internal/execution/model_executor_impl_unittest.cc
@@ -205,7 +205,7 @@ TEST_F(ModelExecutorTest, ExecuteModelWithMultipleFeatures) {
   EXPECT_CALL(mock_model_, ModelAvailable()).WillRepeatedly(Return(true));
   EXPECT_CALL(mock_model_, ExecuteModelWithInput(inputs, _))
       .WillOnce(
-          RunOnceCallback<1>(absl::optional<ModelProvider::Response>({0.8})));
+          RunOnceCallback<1>(absl::optional<ModelProvider::Response>{{0.8}}));
 
   ExecuteModel(
       *metadata_writer.FindOrCreateSegment(kSegmentId), &mock_model_,
