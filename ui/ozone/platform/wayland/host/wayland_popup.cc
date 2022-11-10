220039692f0a4fcb76bd2b558aea2ef69f725c92
Eriko Kurimoto <elkurin@chromium.org>
2022-11-10 02:29:07 +0000

    Exo: Set aura_surface_ on WaylandPopup on shell popup creation
    This CL resets aura_surface_ in WaylandPopup::CreateShellPopup().
    This refactor is required for servser side tooltip for Lacros.
    ref: crrev.com/c/3878453
    
    This CL doesn't change the behavior.
    
    Change-Id: If94b4fc9a0c9a3d2824132f83313b4931995a35f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4015586
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1069539}

--- ui/ozone/platform/wayland/host/wayland_popup.cc
+++ ui/ozone/platform/wayland/host/wayland_popup.cc
@@ -76,6 +76,11 @@ bool WaylandPopup::CreateShellPopup() {
     return false;
   }
 
+  if (connection()->zaura_shell() && !aura_surface_) {
+    aura_surface_.reset(zaura_shell_get_aura_surface(
+        connection()->zaura_shell()->wl_object(), root_surface()->surface()));
+  }
+
   parent_window()->set_child_window(this);
   UpdateDecoration();
   return true;
@@ -98,11 +103,6 @@ void WaylandPopup::UpdateDecoration() {
   // Decorate the frame using the older protocol. Can be removed once Lacros >=
   // M107. Reshown popups will not be decorated if |aura_surface_| isn't reset
   // when server implements the older protocol.
-  if (!aura_surface_) {
-    aura_surface_.reset(zaura_shell_get_aura_surface(
-        connection()->zaura_shell()->wl_object(), root_surface()->surface()));
-  }
-
   if (shadow_type_ == PlatformWindowShadowType::kDrop) {
     zaura_surface_set_frame(aura_surface_.get(),
                             ZAURA_SURFACE_FRAME_TYPE_SHADOW);
