8b7d0b11c78849108a700f2f76248cea6a44e02c
Olesia Marukhno <olesiamarukhno@google.com>
2022-11-10 16:11:25 +0000

    [privacy sandbox] Use view manager to switch between dialog steps
    Use view manager component to switch between steps in the combined dialog. Add saving step UI: a spinner and a label.
    
    Bug: 1378703
    Change-Id: I47e2cb5b15a134fc1431985854bcf7a76dca4137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4020334
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069794}

--- chrome/browser/resources/privacy_sandbox/privacy_sandbox_combined_dialog_app.ts
+++ chrome/browser/resources/privacy_sandbox/privacy_sandbox_combined_dialog_app.ts
@@ -2,6 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
+import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import './strings.m.js';
+
+import {CrViewManagerElement} from 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './privacy_sandbox_combined_dialog_app.html.js';
@@ -10,6 +18,12 @@ export enum PrivacySandboxCombinedDialogStep {
   CONSENT = 'consent',
   SAVING = 'saving',
   NOTICE = 'notice',
+}
+
+export interface PrivacySandboxCombinedDialogAppElement {
+  $: {
+    viewManager: CrViewManagerElement,
+  };
 }
 
 export class PrivacySandboxCombinedDialogAppElement extends PolymerElement {
@@ -31,7 +45,7 @@ export class PrivacySandboxCombinedDialogAppElement extends PolymerElement {
     };
   }
 
-  private step_: string;
+  private step_: PrivacySandboxCombinedDialogStep;
 
   override ready() {
     super.ready();
@@ -39,14 +53,32 @@ export class PrivacySandboxCombinedDialogAppElement extends PolymerElement {
     // Support starting with notice step instead of starting with consent step.
     const step = new URLSearchParams(window.location.search).get('step');
     if (step === PrivacySandboxCombinedDialogStep.NOTICE) {
-      this.step_ = step;
+      this.navigateToStep_(PrivacySandboxCombinedDialogStep.NOTICE);
     } else {
-      this.step_ = PrivacySandboxCombinedDialogStep.CONSENT;
+      this.navigateToStep_(PrivacySandboxCombinedDialogStep.CONSENT);
     }
   }
 
-  private isCurrentStep_(step: PrivacySandboxCombinedDialogStep) {
-    return this.step_ === step;
+  private onConsentStepResolved_() {
+    this.navigateToStep_(PrivacySandboxCombinedDialogStep.SAVING);
+    const savingDurationMs = 1000;
+    setTimeout(() => {
+      this.navigateToStep_(PrivacySandboxCombinedDialogStep.NOTICE);
+    }, savingDurationMs);
+  }
+
+  private navigateToStep_(step: PrivacySandboxCombinedDialogStep) {
+    assert(step !== this.step_);
+    this.step_ = step;
+
+    // TODO(crbug.com/1378703): Check if animations are disabled by global
+    // control.
+    const animateFromLeftToRight =
+        loadTimeData.getString('textdirection') === 'ltr';
+    this.$.viewManager.switchView(
+        this.step_,
+        animateFromLeftToRight ? 'slide-in-fade-in-ltr' :
+                                 'slide-in-fade-in-rtl');
   }
 }
 
