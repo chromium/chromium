347db1f2e54285a564fdd068a58cc2580ebac6a0
Clemens Arbesser <arbesser@google.com>
2022-11-10 20:56:43 +0000

    Added mock for script executor ui delegate
    Change-Id: I7017b96f41810a24ca53ebd2ad828caf0128852c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967783
    Reviewed-by: Florian Gauger <fga@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1069980}

---
+++ components/autofill_assistant/browser/mock_script_executor_ui_delegate.cc
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/mock_script_executor_ui_delegate.h"
+
+namespace autofill_assistant {
+
+MockScriptExecutorUiDelegate::MockScriptExecutorUiDelegate() = default;
+MockScriptExecutorUiDelegate::~MockScriptExecutorUiDelegate() = default;
+
+}  // namespace autofill_assistant
