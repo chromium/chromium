b721e79ab92d3ff5fa9a985aa944850c85135618
Kirubel Aklilu <kaklilu@chromium.org>
2022-11-11 19:44:04 +0000

    [DIPS] Add base::Feature and feature params for DIPS
    Bug: 1375302
    Change-Id: Icc43ff6f5a067bdd6f9339d1ef01256e6b9462fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016774
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070436}

---
+++ chrome/browser/dips/dips_features.h
@@ -0,0 +1,21 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DIPS_DIPS_FEATURES_H_
+#define CHROME_BROWSER_DIPS_DIPS_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+#include "base/time/time.h"
+
+namespace dips {
+
+BASE_DECLARE_FEATURE(kFeature);
+extern const base::FeatureParam<bool> kPersistedDatabaseEnabled;
+extern const base::FeatureParam<base::TimeDelta> kGracePeriod;
+extern const base::FeatureParam<base::TimeDelta> kTimerDelay;
+
+}  // namespace dips
+
+#endif  // CHROME_BROWSER_DIPS_DIPS_FEATURES_H_
