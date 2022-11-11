4b98a3d3ccbd1609118871a94b83c2cf830a3397
Robert Flack <flackr@chromium.org>
2022-11-11 16:06:25 +0000

    Use compositor priority for native paint worklets
    Activating a pending tree with native paint worklets blocks on
    getting paint ops for those worklets[1]. With the default thread
    priority this work may be starved behind other non rendering
    critical work delaying activation of the pending tree slowing
    down the pipeline.
    
    This feature sets the thread type to compositing to reflect its
    criticality to the composited rendering of new frames. Since the
    native paint worklet workload is minimal and only running when
    producing new frames this should not starve compositing critical
    work.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/scheduler/scheduler_state_machine.cc?q=file:cc%2Fscheduler%2Fscheduler_state_machine.cc%20if%5C%20%5C(processing_paint_worklets_for_pending_tree_%5C)&ss=chromium%2Fchromium%2Fsrc
    
    Bug: b:249348436
    Change-Id: I28f651a328cf2036ea2947b4078a7be14da15530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999983
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070304}

--- third_party/blink/renderer/modules/csspaint/nativepaint/native_paint_definition.cc
+++ third_party/blink/renderer/modules/csspaint/nativepaint/native_paint_definition.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/modules/csspaint/nativepaint/native_paint_definition.h"
 
+#include "base/feature_list.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/workers/worker_backing_thread_startup_data.h"
@@ -12,6 +13,16 @@
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 
 namespace blink {
+
+namespace {
+
+// If enabled, uses the compositor priority to avoid potential starvation
+// when the compositor waits for worklets before activating pending trees.
+BASE_FEATURE(kNativePaintUsesCompositorPriority,
+             "NativePaintUsesCompositorPriority",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace
 
 NativePaintDefinition::NativePaintDefinition(
     LocalFrame* local_root,
@@ -22,9 +33,19 @@ NativePaintDefinition::NativePaintDefinition(
   ExecutionContext* context = local_root->DomWindow();
   FrameOrWorkerScheduler* scheduler =
       context ? context->GetScheduler() : nullptr;
-  // TODO(crbug.com/1143407): We don't need this thread if we can make the
-  // compositor thread support GC.
+  // TODO(crbug.com/1143407): We shouldn't need a thread for native paint
+  // definitions as they're running trusted code so it is okay to be run
+  // directly on the compositor thread. The main issue is that many blink
+  // classes require garbage collection to construct, so we need to add
+  // GC support to the compositor to avoid this.
   ThreadCreationParams params(ThreadType::kAnimationAndPaintWorkletThread);
+  if (base::FeatureList::IsEnabled(kNativePaintUsesCompositorPriority)) {
+    // The base thread type is used to establish the priority of the thread. As
+    // the compositor waits for this thread to produce paint ops before it can
+    // activate the pending tree, we use compositing thread type to reflect that
+    // the work is critical to compositing.
+    params.base_thread_type = base::ThreadType::kCompositing;
+  }
   worker_backing_thread_ = std::make_unique<WorkerBackingThread>(
       params.SetFrameOrWorkerScheduler(scheduler));
   auto startup_data = WorkerBackingThreadStartupData::CreateDefault();
