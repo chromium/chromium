7495637e73a1c5878d1ff8f2df7939cf4eb98d82
hmare <hmare@google.com>
2022-11-11 19:01:46 +0000

    [ProfileID] Add metrics for profile identifier creation
    Created metrics that are recorded if the call to get profile id fails. These metrics are important for making debugging easier.
    
    Bug: b/254637702
    Change-Id: I79158919e1f895a4ad948e774a896d86bba3c4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3977349
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070397}

--- components/enterprise/browser/identifiers/profile_id_service.cc
+++ components/enterprise/browser/identifiers/profile_id_service.cc
@@ -9,12 +9,27 @@
 #include "base/base64url.h"
 #include "base/check.h"
 #include "base/hash/sha1.h"
+#include "base/metrics/histogram_functions.h"
 #include "components/enterprise/browser/identifiers/identifiers_prefs.h"
 #include "components/enterprise/browser/identifiers/profile_id_delegate.h"
 #include "components/prefs/pref_service.h"
 
 namespace enterprise {
 
+namespace {
+
+static constexpr char kErrorHistogramName[] =
+    "Enterprise.ProfileIdentifier.Error";
+static constexpr char kStatusHistogramName[] =
+    "Enterprise.ProfileIdentifier.Status";
+
+absl::nullopt_t RecordError(ProfileIdService::Error error) {
+  base::UmaHistogramBoolean(kStatusHistogramName, false);
+  base::UmaHistogramEnumeration(kErrorHistogramName, error);
+  return absl::nullopt;
+}
+
+}  // namespace
 ProfileIdService::ProfileIdService(std::unique_ptr<ProfileIdDelegate> delegate,
                                    PrefService* profile_prefs)
     : delegate_(std::move(delegate)), profile_prefs_(profile_prefs) {
@@ -27,16 +42,21 @@ ProfileIdService::~ProfileIdService() = default;
 absl::optional<std::string> ProfileIdService::GetProfileId() {
   std::string profile_guid = profile_prefs_->GetString(kProfileGUIDPref);
   if (profile_guid.empty())
-    return absl::nullopt;
+    return RecordError(Error::kGetProfileGUIDFailure);
 
   auto device_id = delegate_->GetDeviceId();
   if (device_id.empty())
-    return absl::nullopt;
+    return RecordError(Error::kGetDeviceIdFailure);
 
   std::string encoded_string;
   base::Base64UrlEncode(base::SHA1HashString(profile_guid + device_id),
                         base::Base64UrlEncodePolicy::OMIT_PADDING,
                         &encoded_string);
+
+  if (encoded_string.empty())
+    return RecordError(Error::kProfileIdURLEncodeFailure);
+
+  base::UmaHistogramBoolean(kStatusHistogramName, true);
   return encoded_string;
 }
 
