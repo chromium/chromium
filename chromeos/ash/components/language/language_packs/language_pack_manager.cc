0fe69df6af31929f954a42a651f915b93966304f
Claudio M <claudiomagni@google.com>
2022-11-13 23:19:58 +0000

    language_packs: Support the core set of languages for TTS
    We are adding support for ES, FR, HI, NL, PT and SV, which are currently
    on rootfs. This is the first set of languages that we are enabling.
    This does not include EN and JA, which are on rootfs: we want to keep
    the legacy path for them for now.
    
    The DLCs have been created here: https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/4011610
    
    Bug: b:237350367
    Change-Id: I3c7cd8a1ef920e9c4aa87f9a71ab06a349335f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4012359
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1070814}

--- chromeos/ash/components/language/language_packs/language_pack_manager.cc
+++ chromeos/ash/components/language/language_packs/language_pack_manager.cc
@@ -138,7 +138,13 @@ const base::flat_map<PackSpecPair, std::string>& GetAllLanguagePackDlcIds() {
           {{kHandwritingFeatureId, "zh"}, "handwriting-zh"},
 
           // Text-To-Speech.
+          {{kTtsFeatureId, "es-es"}, "tts-es-es"},
           {{kTtsFeatureId, "es-us"}, "tts-es-us"},
+          {{kTtsFeatureId, "fr-fr"}, "tts-fr-fr"},
+          {{kTtsFeatureId, "hi-in"}, "tts-hi-in"},
+          {{kTtsFeatureId, "nl-nl"}, "tts-nl-nl"},
+          {{kTtsFeatureId, "pt-br"}, "tts-pt-br"},
+          {{kTtsFeatureId, "sv-se"}, "tts-sv-se"},
       });
 
   return *all_dlc_ids;
