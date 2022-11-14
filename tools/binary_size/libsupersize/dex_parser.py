f56a4c2a15a2240212e792c347819bc5f2266fed
Samuel Huang <huangs@chromium.org>
2022-11-14 21:32:32 +0000

    [SuperSize] Extract stream_reader.py from dex_parser.py.
    This CL extracts low-level bytearray data extraction code from
    dex_parser.py to separate library stream_reader.py, to declutter the
    file and allow code reuse. Details:
    * Add stream_reader.py:
      * Add class StreamReader to extract general parsing functions from
        _DexReader.
        * Change "Read*()" functions to "Next*()".
          * Semantic change for ReadString() => NextString(): Now caller
            needs explicit seek.
        * Simplify integer parsing by hard-coding sizes.
        * Add {NextBytes(), NextSLeb128(), NextStruct()}.
      * Extract {_ParseULeb128(), _ParseSLeb128()} as separate helpers.
    * dex_parser.py:
      * _DexMapList: Replace __index__() and __contains__() with get(). This
        also allows caller to be simplified.
      * Move DEX constants {_TYPE_TYPE_LIST, _CLASS_ACCESS_FLAGS} to be
        file global.
      * class _DexReader: Now extends stream_reader.StreamReader. Keep
        NextHeader() (was ReadHeader()) since it's DEX specific.
        * Change to use NextStruct().
      * Reorder classes, fields, evaluations, options to follow the order
        in https://source.android.com/docs/core/runtime/dex-format.
    * Change "dex" to "DEX" in comments and output strings.
    
    Bug: 881319, 1270438, 1343117
    Change-Id: Ifbc907a9875c3d388f6d0226bcaebb25775661b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022574
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071229}

--- tools/binary_size/libsupersize/dex_parser.py
+++ tools/binary_size/libsupersize/dex_parser.py
@@ -2,13 +2,14 @@
 # Copyright 2019 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Utilities for optimistically parsing dex files.
+"""Utilities for optimistically parsing DEX files.
 
-This file is not meant to provide a generic tool for analyzing dex files.
-A DexFile class that exposes access to several memory items in the dex format
+This file is not meant to provide a generic tool for analyzing DEX files.
+A DexFile class that exposes access to several memory items in the DEX format
 is provided, but it does not include error handling or validation.
 """
 
+import abc
 import argparse
 import collections
 import errno
@@ -17,6 +18,8 @@ import re
 import struct
 import sys
 import zipfile
+
+import stream_reader
 
 # https://source.android.com/devices/tech/dalvik/dex-format#header-item
 _DEX_HEADER_FMT = (
@@ -45,21 +48,44 @@ _DEX_HEADER_FMT = (
     ('data_off', 'I'),
 )
 
+# Full list of type codes:
+# https://source.android.com/devices/tech/dalvik/dex-format#type-codes
+_TYPE_TYPE_LIST = 0x1001
+
+_CLASS_ACCESS_FLAGS = {
+    0x1: 'public',
+    0x2: 'private',
+    0x4: 'protected',
+    0x8: 'static',
+    0x10: 'final',
+    0x200: 'interface',
+    0x400: 'abstract',
+    0x1000: 'synthetic',
+    0x2000: 'annotation',
+    0x4000: 'enum',
+}
+
 DexHeader = collections.namedtuple('DexHeader',
                                    ','.join(t[0] for t in _DEX_HEADER_FMT))
 
 # Simple memory items.
+_StringDataItem = collections.namedtuple('StringItem', 'utf16_size,data')
 _TypeIdItem = collections.namedtuple('TypeIdItem', 'descriptor_idx')
 _ProtoIdItem = collections.namedtuple(
     'ProtoIdItem', 'shorty_idx,return_type_idx,parameters_off')
 _MethodIdItem = collections.namedtuple('MethodIdItem',
                                        'type_idx,proto_idx,name_idx')
 _TypeItem = collections.namedtuple('TypeItem', 'type_idx')
-_StringDataItem = collections.namedtuple('StringItem', 'utf16_size,data')
 _ClassDefItem = collections.namedtuple(
     'ClassDefItem',
     'class_idx,access_flags,superclass_idx,interfaces_off,source_file_idx,'
     'annotations_off,class_data_off,static_values_off')
+
+
+class _DexReader(stream_reader.StreamReader):
+  def NextDexHeader(self):
+    header_fmt = '<' + ''.join(t[1] for t in _DEX_HEADER_FMT)
+    return DexHeader._make(self.NextStruct(header_fmt))
 
 
 class _MemoryItemList:
@@ -88,8 +114,7 @@ class _MemoryItemList:
     self.offset = offset
     self.size = size
     reader.Seek(first_item_offset or offset)
-    self._items = [factory(reader) for _ in range(size)]
-
+    self._items = reader.NextList(size, factory)
     if alignment:
       reader.AlignUpTo(alignment)
 
@@ -112,43 +137,43 @@ class _MemoryItemList:
         type(self).__name__, self.offset, self.size, item_type_part)
 
 
+class _StringItemList(_MemoryItemList):
+  def __init__(self, reader, offset, size):
+    reader.Seek(offset)
+    string_item_offsets = iter([reader.NextUInt() for _ in range(size)])
+
+    def factory(x):
+      x.Seek(next(string_item_offsets))
+      string = x.NextString()
+      return _StringDataItem(len(string), string)
+
+    super().__init__(reader, offset, size, factory)
+
+
 class _TypeIdItemList(_MemoryItemList):
   def __init__(self, reader, offset, size):
-    factory = lambda x: _TypeIdItem(x.ReadUInt())
+    factory = lambda x: _TypeIdItem(x.NextUInt())
     super().__init__(reader, offset, size, factory)
 
 
 class _ProtoIdItemList(_MemoryItemList):
   def __init__(self, reader, offset, size):
-    factory = lambda x: _ProtoIdItem(x.ReadUInt(), x.ReadUInt(), x.ReadUInt())
+    factory = lambda x: _ProtoIdItem(x.NextUInt(), x.NextUInt(), x.NextUInt())
     super().__init__(reader, offset, size, factory)
 
 
 class _MethodIdItemList(_MemoryItemList):
   def __init__(self, reader, offset, size):
     factory = (
-        lambda x: _MethodIdItem(x.ReadUShort(), x.ReadUShort(), x.ReadUInt()))
-    super().__init__(reader, offset, size, factory)
-
-
-class _StringItemList(_MemoryItemList):
-  def __init__(self, reader, offset, size):
-    reader.Seek(offset)
-    string_item_offsets = iter([reader.ReadUInt() for _ in range(size)])
-
-    def factory(x):
-      data_offset = next(string_item_offsets)
-      string = x.ReadString(data_offset)
-      return _StringDataItem(len(string), string)
-
+        lambda x: _MethodIdItem(x.NextUShort(), x.NextUShort(), x.NextUInt()))
     super().__init__(reader, offset, size, factory)
 
 
 class _TypeListItem(_MemoryItemList):
   def __init__(self, reader):
     offset = reader.Tell()
-    size = reader.ReadUInt()
-    factory = lambda x: _TypeItem(x.ReadUShort())
+    size = reader.NextUInt()
+    factory = lambda x: _TypeItem(x.NextUShort())
     # This is necessary because we need to extract the size of the type list
     # (in other cases the list size is provided in the header).
     first_item_offset = reader.Tell()
@@ -170,7 +195,7 @@ class _ClassDefItemList(_MemoryItemList):
     reader.Seek(offset)
 
     def factory(x):
-      return _ClassDefItem(*(x.ReadUInt()
+      return _ClassDefItem(*(x.NextUInt()
                              for _ in range(len(_ClassDefItem._fields))))
 
     super().__init__(reader, offset, size, factory)
@@ -178,10 +203,10 @@ class _ClassDefItemList(_MemoryItemList):
 
 class _DexMapItem:
   def __init__(self, reader):
-    self.type = reader.ReadUShort()
-    reader.ReadUShort()
-    self.size = reader.ReadUInt()
-    self.offset = reader.ReadUInt()
+    self.type = reader.NextUShort()
+    reader.NextUShort()  # Unused.
+    self.size = reader.NextUInt()
+    self.offset = reader.NextUInt()
 
   def __repr__(self):
     return '_DexMapItem(type={}, size={}, offset={:#x})'.format(
@@ -189,180 +214,53 @@ class _DexMapItem:
 
 
 class _DexMapList:
-  # Full list of type codes:
-  # https://source.android.com/devices/tech/dalvik/dex-format#type-codes
-  TYPE_TYPE_LIST = 0x1001
-
   def __init__(self, reader, offset):
     self._map = {}
     reader.Seek(offset)
-    self._size = reader.ReadUInt()
+    self._size = reader.NextUInt()
     for _ in range(self._size):
       item = _DexMapItem(reader)
       self._map[item.type] = item
 
-  def __getitem__(self, key):
-    return self._map[key]
-
-  def __contains__(self, key):
-    return key in self._map
+  def get(self, key):
+    return self._map.get(key)
 
   def __repr__(self):
     return '_DexMapList(size={}, items={})'.format(self._size, self._map)
 
 
-class _DexReader:
-  def __init__(self, data):
-    self._data = data
-    self._pos = 0
-
-  def Seek(self, offset):
-    self._pos = offset
-
-  def Tell(self):
-    return self._pos
-
-  def ReadUByte(self):
-    return self._ReadData('<B')
-
-  def ReadUShort(self):
-    return self._ReadData('<H')
-
-  def ReadUInt(self):
-    return self._ReadData('<I')
-
-  def ReadString(self, data_offset):
-    string_length, string_offset = self._ReadULeb128(data_offset)
-    string_data_offset = string_offset + data_offset
-    return self._DecodeMUtf8(string_length, string_data_offset)
-
-  def AlignUpTo(self, align_unit):
-    off_by = self._pos % align_unit
-    if off_by:
-      self.Seek(self._pos + align_unit - off_by)
-
-  def ReadHeader(self):
-    header_fmt = '<' + ''.join(t[1] for t in _DEX_HEADER_FMT)
-    return DexHeader._make(struct.unpack_from(header_fmt, self._data))
-
-  def _ReadData(self, fmt):
-    ret = struct.unpack_from(fmt, self._data, self._pos)[0]
-    self._pos += struct.calcsize(fmt)
-    return ret
-
-  def _ReadULeb128(self, data_offset):
-    """Returns a tuple of (uleb128 value, number of bytes occupied).
-
-    From DWARF3 spec: http://dwarfstd.org/doc/Dwarf3.pdf
-
-    Args:
-      data_offset: Location of the unsigned LEB128.
-    """
-    value = 0
-    shift = 0
-    cur_offset = data_offset
-    while True:
-      byte = self._data[cur_offset]
-      cur_offset += 1
-      value |= (byte & 0b01111111) << shift
-      if (byte & 0b10000000) == 0:
-        break
-      shift += 7
-
-    return value, cur_offset - data_offset
-
-  def _DecodeMUtf8(self, string_length, offset):
-    """Returns the string located at the specified offset.
-
-    See https://source.android.com/devices/tech/dalvik/dex-format#mutf-8
-
-    Ported from the Android Java implementation:
-    https://android.googlesource.com/platform/dalvik/+/fe107fb6e3f308ac5174ebdc5a794ee880c741d9/dx/src/com/android/dex/Mutf8.java#34
-
-    Args:
-      string_length: The length of the decoded string.
-      offset: Offset to the beginning of the string.
-    """
-    self.Seek(offset)
-    ret = ''
-
-    for _ in range(string_length):
-      a = self.ReadUByte()
-      if a == 0:
-        raise _MUTf8DecodeError('Early string termination encountered',
-                                string_length, offset)
-      if (a & 0x80) == 0x00:
-        code = a
-      elif (a & 0xe0) == 0xc0:
-        b = self.ReadUByte()
-        if (b & 0xc0) != 0x80:
-          raise _MUTf8DecodeError('Error in byte 2', string_length, offset)
-        code = ((a & 0x1f) << 6) | (b & 0x3f)
-      elif (a & 0xf0) == 0xe0:
-        b = self.ReadUByte()
-        c = self.ReadUByte()
-        if (b & 0xc0) != 0x80 or (c & 0xc0) != 0x80:
-          raise _MUTf8DecodeError('Error in byte 3 or 4', string_length, offset)
-        code = ((a & 0x0f) << 12) | ((b & 0x3f) << 6) | (c & 0x3f)
-      else:
-        raise _MUTf8DecodeError('Bad byte', string_length, offset)
-      ret += chr(code)
-
-    if self.ReadUByte() != 0x00:
-      raise _MUTf8DecodeError('Expected string termination', string_length,
-                              offset)
-
-    return ret
-
-
-class _MUTf8DecodeError(Exception):
-  def __init__(self, message, length, offset):
-    message += ' (decoded string length: {}, string data offset: {:#x})'.format(
-        length, offset)
-    super().__init__(message)
-
-
 class DexFile:
-  """Represents a single dex file.
+  """Represents a single DEX file.
 
-  Parses and exposes access to dex file structure and contents, as described
+  Parses and exposes access to DEX file structure and contents, as described
   at https://source.android.com/devices/tech/dalvik/dex-format
 
   Fields:
-    reader: _DexReader object used to decode dex file contents.
-    header: DexHeader for this dex file.
-    map_list: _DexMapList object containing list of dex file contents.
-    type_item_list: _TypeIdItemList containing type_id_items.
-    proto_item_list: _ProtoIdItemList containing proto_id_items.
-    method_item_list: _MethodIdItemList containing method_id_items.
-    string_item_list: _StringItemList containing string_data_items that are
+    reader: _DexReader object used to decode DEX file contents.
+    header: DexHeader for this DEX file.
+    map_list: _DexMapList object containing list of DEX file contents.
+    string_item_list: _StringItemList containing _StringDataItems that are
       referenced by index in other sections.
+    type_item_list: _TypeIdItemList containing _TypeIdItems.
+    proto_item_list: _ProtoIdItemList containing _ProtoIdItems.
+    method_item_list: _MethodIdItemList containing _MethodIdItems.
     type_list_item_list: _TypeListItemList containing _TypeListItems.
-      _TypeListItems are referenced by their offsets from other dex items.
+      _TypeListItems are referenced by their offsets from other DEX items.
     class_def_item_list: _ClassDefItemList containing _ClassDefItems.
   """
-  _CLASS_ACCESS_FLAGS = {
-      0x1: 'public',
-      0x2: 'private',
-      0x4: 'protected',
-      0x8: 'static',
-      0x10: 'final',
-      0x200: 'interface',
-      0x400: 'abstract',
-      0x1000: 'synthetic',
-      0x2000: 'annotation',
-      0x4000: 'enum',
-  }
 
   def __init__(self, data):
     """Decodes dex file memory sections.
 
     Args:
-      data: bytearray containing the contents of a dex file.
+      data: bytearray containing the contents of a DEX file.
     """
     self.reader = _DexReader(data)
-    self.header = self.reader.ReadHeader()
+    self.header = self.reader.NextDexHeader()
     self.map_list = _DexMapList(self.reader, self.header.map_off)
+    self.string_item_list = _StringItemList(self.reader,
+                                            self.header.string_ids_off,
+                                            self.header.string_ids_size)
     self.type_item_list = _TypeIdItemList(self.reader, self.header.type_ids_off,
                                           self.header.type_ids_size)
     self.proto_item_list = _ProtoIdItemList(self.reader,
@@ -371,19 +269,15 @@ class DexFile:
     self.method_item_list = _MethodIdItemList(self.reader,
                                               self.header.method_ids_off,
                                               self.header.method_ids_size)
-    self.string_item_list = _StringItemList(self.reader,
-                                            self.header.string_ids_off,
-                                            self.header.string_ids_size)
     self.class_def_item_list = _ClassDefItemList(self.reader,
                                                  self.header.class_defs_off,
                                                  self.header.class_defs_size)
 
-    type_list_key = _DexMapList.TYPE_TYPE_LIST
-    if type_list_key in self.map_list:
-      map_list_item = self.map_list[type_list_key]
+    type_list_item = self.map_list.get(_TYPE_TYPE_LIST)
+    if type_list_item:
       self.type_list_item_list = _TypeListItemList(self.reader,
-                                                   map_list_item.offset,
-                                                   map_list_item.size)
+                                                   type_list_item.offset,
+                                                   type_list_item.size)
     else:
       self.type_list_item_list = _TypeListItemList(self.reader, 0, 0)
     self._type_lists_by_offset = {
@@ -391,12 +285,12 @@ class DexFile:
         for type_list in self.type_list_item_list
     }
 
-  def GetString(self, string_item_idx):
-    string_item = self.string_item_list[string_item_idx]
+  def GetString(self, string_idx):
+    string_item = self.string_item_list[string_idx]
     return string_item.data
 
-  def GetTypeString(self, type_item_idx):
-    type_item = self.type_item_list[type_item_idx]
+  def GetTypeString(self, type_idx):
+    type_item = self.type_item_list[type_idx]
     return self.GetString(type_item.descriptor_idx)
 
   def GetTypeListStringsByOffset(self, offset):
@@ -408,11 +302,11 @@ class DexFile:
   @staticmethod
   def ResolveClassAccessFlags(access_flags):
     return tuple(flag_string
-                 for flag, flag_string in DexFile._CLASS_ACCESS_FLAGS.items()
+                 for flag, flag_string in _CLASS_ACCESS_FLAGS.items()
                  if flag & access_flags)
 
   def IterMethodSignatureParts(self):
-    """Yields the string components of dex methods in a dex file.
+    """Yields the string components of DEX methods in a DEX file.
 
     Yields:
       Tuples that look like:
@@ -446,8 +340,21 @@ class _DumpCommand:
   def __init__(self, dexfile):
     self._dexfile = dexfile
 
+  @abc.abstractmethod
   def Run(self):
-    raise NotImplementedError()
+    pass
+
+
+class _DumpSummary(_DumpCommand):
+  def Run(self):
+    print(self._dexfile)
+
+
+class _DumpStrings(_DumpCommand):
+  def Run(self):
+    for string_item in self._dexfile.string_item_list:
+      # Some strings are likely to be non-ascii (vs. methods/classes).
+      print(string_item.data.encode('utf-8'))
 
 
 class _DumpMethods(_DumpCommand):
@@ -456,13 +363,6 @@ class _DumpMethods(_DumpCommand):
       class_type, return_type, method_name, parameter_types = parts
       print('{} {} (return type={}, parameters={})'.format(
           class_type, method_name, return_type, parameter_types))
-
-
-class _DumpStrings(_DumpCommand):
-  def Run(self):
-    for string_item in self._dexfile.string_item_list:
-      # Some strings are likely to be non-ascii (vs. methods/classes).
-      print(string_item.data.encode('utf-8'))
 
 
 class _DumpClasses(_DumpCommand):
@@ -477,18 +377,13 @@ class _DumpClasses(_DumpCommand):
           class_string, superclass_string, interfaces, access_flags))
 
 
-class _DumpSummary(_DumpCommand):
-  def Run(self):
-    print(self._dexfile)
-
-
 def _DumpDexItems(dexfile_data, name, item):
   dexfile = DexFile(bytearray(dexfile_data))
   print('dex_parser: Dumping {} for {}'.format(item, name))
   cmds = {
       'summary': _DumpSummary,
-      'methods': _DumpMethods,
       'strings': _DumpStrings,
+      'methods': _DumpMethods,
       'classes': _DumpClasses,
   }
   try:
@@ -500,11 +395,11 @@ def _DumpDexItems(dexfile_data, name, item):
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Dump dex contents to stdout.')
+  parser = argparse.ArgumentParser(description='Dump DEX contents to stdout.')
   parser.add_argument('input',
                       help='Input (.dex, .jar, .zip, .aab, .apk) file path.')
   parser.add_argument('item',
-                      choices=('methods', 'strings', 'classes', 'summary'),
+                      choices=('summary', 'strings', 'methods', 'classes'),
                       help='Item to dump',
                       nargs='?',
                       default='summary')
