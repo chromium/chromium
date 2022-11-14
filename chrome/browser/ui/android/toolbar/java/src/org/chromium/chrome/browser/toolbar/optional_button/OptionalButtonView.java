fab4bf85dc9b22d6ad7d34e89a24533b2293f1f0
Salvador Guerrero <salg@google.com>
2022-11-14 21:17:00 +0000

    [Contextual Page Actions] Fixed crash on action chip transition
    This CL fixes a null pointer exception when trying to post a task on an
    OptionalButtonView instance that has been detached. This happens when
    we finish the action chip expansion animation and we try to schedule
    the collapse animation.
    
    (cherry picked from commit a0233c6d3c87aed1b05301ff88ddd51eac469f43)
    
    Bug: 1383559
    Change-Id: I6fded66cab4f36368606f0469ccbc89586c0ea53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4022030
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1070604}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024872
    Auto-Submit: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5359@{#828}
    Cr-Branched-From: 27d3765d341b09369006d030f83f582a29eb57ae-refs/heads/main@{#1058933}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/optional_button/OptionalButtonView.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/optional_button/OptionalButtonView.java
@@ -34,6 +34,7 @@ import com.google.android.material.color.MaterialColors;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.supplier.BooleanSupplier;
 import org.chromium.chrome.browser.toolbar.ButtonData;
 import org.chromium.chrome.browser.toolbar.ButtonData.ButtonSpec;
@@ -69,6 +70,7 @@ class OptionalButtonView extends FrameLayout implements TransitionListener {
     private int mBackgroundColorFilter;
     private Runnable mOnBeforeHideTransitionCallback;
     private Callback<Transition> mFakeBeginTransitionForTesting;
+    private Handler mHandler;
     private Handler mHandlerForTesting;
 
     private @State int mState;
@@ -283,7 +285,11 @@ class OptionalButtonView extends FrameLayout implements TransitionListener {
             return mHandlerForTesting;
         }
 
-        return super.getHandler();
+        if (mHandler == null) {
+            mHandler = new Handler(ThreadUtils.getUiThreadLooper());
+        }
+
+        return mHandler;
     }
 
     @Override
