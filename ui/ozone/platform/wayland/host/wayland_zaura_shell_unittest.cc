1a2d493f70d93d96ea6883315a141d92584d5445
Alexander Dunaev <adunaev@igalia.com>
2022-11-15 15:28:46 +0000

    [linux/wayland] Fixed threading in wayland ZAura shell unit tests.
    This patch converts the wayland ZAura shell unit tests into async ones.
    
    Bug: 1365887
    Change-Id: I0652e402b6f65281b7fe545a11c637eb66017d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028637
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1071607}

--- ui/ozone/platform/wayland/host/wayland_zaura_shell_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_zaura_shell_unittest.cc
@@ -2,46 +2,42 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <aura-shell-server-protocol.h>
-
-#include "base/run_loop.h"
-#include "base/test/scoped_feature_list.h"
-#include "base/test/task_environment.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "ui/ozone/common/features.h"
-#include "ui/ozone/platform/wayland/host/wayland_connection.h"
-#include "ui/ozone/platform/wayland/host/wayland_event_source.h"
 #include "ui/ozone/platform/wayland/host/wayland_zaura_shell.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
 #include "ui/ozone/platform/wayland/test/mock_zaura_shell.h"
 #include "ui/ozone/platform/wayland/test/test_wayland_server_thread.h"
+#include "ui/ozone/platform/wayland/test/wayland_test.h"
+
+using testing::Values;
 
 namespace ui {
 
-TEST(WaylandZAuraShellTest, BugFix) {
-  base::test::SingleThreadTaskEnvironment task_environment(
-      base::test::SingleThreadTaskEnvironment::MainThreadType::UI);
-  wl::TestWaylandServerThread server;
-  ASSERT_TRUE(server.Start({}));
-  wl::MockZAuraShell* zaura_shell = server.zaura_shell();
+// TODO(crbug.com/1365887): change this to
+// `using WaylandZAuraShellTest = WaylandTest`
+// once the default mode becomes asynchronous.
+class WaylandZAuraShellTest : public WaylandTest {
+ public:
+  WaylandZAuraShellTest() : WaylandTest(TestServerMode::kAsync) {}
+  WaylandZAuraShellTest(const WaylandZAuraShellTest&) = delete;
+  WaylandZAuraShellTest& operator=(const WaylandZAuraShellTest&) = delete;
+  ~WaylandZAuraShellTest() override = default;
+};
 
-  WaylandConnection connection;
-  ASSERT_TRUE(connection.Initialize());
-  connection.event_source()->StartProcessingEvents();
+TEST_P(WaylandZAuraShellTest, BugFix) {
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    auto* const zaura_shell = server->zaura_shell()->resource();
+    zaura_shell_send_bug_fix(zaura_shell, 1);
+    zaura_shell_send_bug_fix(zaura_shell, 3);
+  });
 
-  base::RunLoop().RunUntilIdle();
-  server.Pause();
-
-  ASSERT_TRUE(server.zaura_shell()->resource());
-  zaura_shell_send_bug_fix(zaura_shell->resource(), 1);
-  zaura_shell_send_bug_fix(zaura_shell->resource(), 3);
-
-  server.Resume();
-  base::RunLoop().RunUntilIdle();
-  server.Pause();
-
-  ASSERT_TRUE(connection.zaura_shell()->HasBugFix(1));
-  ASSERT_TRUE(connection.zaura_shell()->HasBugFix(3));
-  ASSERT_FALSE(connection.zaura_shell()->HasBugFix(2));
+  ASSERT_TRUE(connection_->zaura_shell()->HasBugFix(1));
+  ASSERT_TRUE(connection_->zaura_shell()->HasBugFix(3));
+  ASSERT_FALSE(connection_->zaura_shell()->HasBugFix(2));
 }
+
+INSTANTIATE_TEST_SUITE_P(XdgVersionStableTest,
+                         WaylandZAuraShellTest,
+                         Values(wl::ServerConfig{}));
 
 }  // namespace ui
