12579792aaeeadce5d00eb77abc125ebb20b9e45
Iman Saboori <isaboori@google.com>
2022-11-15 03:35:34 +0000

    Adding preconnector for preloading link selection heuristics
    Speculation rules provide a way for the developers to specify which preloading actions (i.e preload, prerender) are safe to perform on which URLs, however the user agent is not required and in most cases should not perform all of them. The browser link selection heuristics should help the UA to find the best actions among the candidates to execute.
    
    In this CL preconnector class is added.
    
    Design doc: https://docs.google.com/document/d/1sobXevsA3zND30QZLIMZ2sogy201F_P9eBkNQA6j220
    
    Bug: 1374023
    Change-Id: I12dab55d255c70f6c086f1f7fd2c63fb84bba93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4019793
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1071425}

---
+++ content/browser/preloading/preconnector.h
@@ -0,0 +1,33 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_PRELOADING_PRECONNECTOR_H_
+#define CONTENT_BROWSER_PRELOADING_PRECONNECTOR_H_
+
+#include "content/common/content_export.h"
+#include "content/public/browser/anchor_element_preconnect_delegate.h"
+#include "url/gurl.h"
+
+namespace content {
+
+class RenderFrameHost;
+
+// Handles heuristics based preconnects.
+// TODO(isaboori): Implement the preconnect logic directly in content/browser
+// and remove the delegate.
+class CONTENT_EXPORT Preconnector {
+ public:
+  Preconnector() = delete;
+  explicit Preconnector(content::RenderFrameHost& render_frame_host);
+  ~Preconnector();
+
+  void MaybePreconnect(const GURL& url);
+
+ private:
+  std::unique_ptr<AnchorElementPreconnectDelegate> preconnect_delegate_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_PRELOADING_PRECONNECTOR_H_
