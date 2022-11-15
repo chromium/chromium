680ec62b5c57c4d0c7a6f4380cd6005eafc11da8
Daniel Rubery <drubery@chromium.org>
2022-11-15 17:49:06 +0000

    Enable VisualFeaturesInReusePings by default
    This is ready to launch, so enable it by default at HEAD.
    
    Bug: 1362864
    Change-Id: I1a5cefb6216cd04f98872628ba8332125e1a603b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027067
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071689}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -215,7 +215,7 @@ BASE_FEATURE(kThreatDomDetailsTagAndAttributeFeature,
 
 BASE_FEATURE(kVisualFeaturesForReusePings,
              "VisualFeaturesInReusePings",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kVisualFeaturesSizes,
              "VisualFeaturesSizes",
