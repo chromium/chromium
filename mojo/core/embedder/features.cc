cd084518d2810d8d211b33aadd4611ebfbbf627b
Egor Pasko <pasko@chromium.org>
2022-11-15 16:06:37 +0000

    mojo: Enable feature MojoAvoidRandomPipeId by default
    The results from Stable are rather positive, and the effect is small.
    See crbug.com/1351813#c6 for detailed results.
    
    Keeping the base::Feature(MojoAvoidRandomPipeId) for a holdback - our
    collection of other small optimizations like this. Will probably keep
    the flag for a fair chunk of 2023.
    
    Bug: 1351813
    Change-Id: I4068dd5354b14aa9844fa9ab9461aff502fb2a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4027568
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071631}

--- mojo/core/embedder/features.cc
+++ mojo/core/embedder/features.cc
@@ -33,7 +33,7 @@ BASE_FEATURE(kMojoInlineMessagePayloads,
 
 BASE_FEATURE(kMojoAvoidRandomPipeId,
              "MojoAvoidRandomPipeId",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMojoIpcz, "MojoIpcz", base::FEATURE_DISABLED_BY_DEFAULT);
 
