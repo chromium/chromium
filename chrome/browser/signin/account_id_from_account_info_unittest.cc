4d383e03abb05e3ad7ad33c6d80a0856538479d2
Roman Sorokin <rsorokin@chromium.org>
2022-11-16 16:24:34 +0000

    [M109] Signin: Use GetAccountId from the known_user
    The function could find a missing id: either email or gaia id
    
    This CL should fix a possible regression introduced in
    http://crrev.com/c/3925213 - when the email or gaia is empty in
    account_info
    
    (cherry picked from commit ffe442e83b94521382681d280e43e9616fa6074c)
    
    Bug: 1370701, 1150434
    Change-Id: Ifdf8e8ac6edcdcb4c2fe0d5cd5ee0aabcd507401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024447
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1071544}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030473
    Cr-Commit-Position: refs/branch-heads/5414@{#78}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- chrome/browser/signin/account_id_from_account_info_unittest.cc
+++ chrome/browser/signin/account_id_from_account_info_unittest.cc
@@ -3,14 +3,15 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/signin/account_id_from_account_info.h"
+#include "chrome/test/base/scoped_testing_local_state.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-class AccountIdFromAccountInfoTest : public testing::Test {};
 
 // Tests that AccountIdFromAccountInfo() passes along a canonicalized email to
 // AccountId.
 TEST(AccountIdFromAccountInfoTest,
      AccountIdFromAccountInfo_CanonicalizesRawEmail) {
+  ScopedTestingLocalState local_state(TestingBrowserProcess::GetGlobal());
   AccountInfo info;
   info.email = "test.email@gmail.com";
   info.gaia = "test_id";
