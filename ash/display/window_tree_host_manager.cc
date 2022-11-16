46298e97855b1e23d6f1a896ba1efaed6a7e690b
James Cook <jamescook@chromium.org>
2022-11-16 01:14:18 +0000

    Stop recording expired histogram Ash.Display.PrimaryDisplayZoomAtStartup
    This histogram expired in 2020-09-06. Delete the recording code. Also
    delete the histograms.xml entry.
    
    Fixed: 1385032
    Change-Id: Icd158e3f0c3faf9104406e50f320f57f1a4a1c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4028843
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1071961}

--- ash/display/window_tree_host_manager.cc
+++ ash/display/window_tree_host_manager.cc
@@ -336,19 +336,6 @@ void WindowTreeHostManager::InitHosts() {
     }
   }
 
-  // Record display zoom for the primary display for https://crbug.com/955071.
-  // This can be removed after M79.
-  const display::ManagedDisplayInfo& display_info =
-      display_manager->GetDisplayInfo(primary_display_id);
-  int zoom_percent = std::round(display_info.zoom_factor() * 100);
-  constexpr int kMaxValue = 300;
-  constexpr int kBucketSize = 5;
-  constexpr int kBucketCount = kMaxValue / kBucketSize + 1;
-  base::LinearHistogram::FactoryGet(
-      "Ash.Display.PrimaryDisplayZoomAtStartup", kBucketSize, kMaxValue,
-      kBucketCount, base::HistogramBase::kUmaTargetedHistogramFlag)
-      ->Add(zoom_percent);
-
   for (auto& observer : observers_)
     observer.OnDisplaysInitialized();
 }
