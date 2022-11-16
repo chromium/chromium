95e63e9bd6631d26350cec9f40c1b93dc99e2435
Ella Ge <eirage@chromium.org>
2022-11-16 18:30:29 +0000

    Reland "Enable WebApkUniqueId by default on M109"
    This reverts commit 9ec481d957330faecf906b9284a7c924b0e15eca.
    
    Reason for revert: Test is fixed, re-enable now
    
    Original change's description:
    > Revert "Enable WebApkUniqueId by default on M109"
    >
    > This reverts commit 8b763a9e98f6d83c36e5ce9be8177c77eebe68bc.
    >
    > Reason for revert: Suspected cause for WebApkUpdateDataFetcherTest#testLaunchWithDifferentManifestUrl flakiness.
    > See https://crbug.com/1383350 for details.
    >
    > Original change's description:
    > > Enable WebApkUniqueId by default on M109
    > >
    > > Bug: 1342070
    > > Change-Id: Ia375c3f687156ea6b1fa0022995a5157947ab0d9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4016864
    > > Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    > > Auto-Submit: Ella Ge <eirage@chromium.org>
    > > Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1069787}
    >
    > Bug: 1342070, 1383350
    > Change-Id: I2cc95fcc29eb6f3107d4e8bf35396821952bb942
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4024333
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    > Owners-Override: Tommy Nyquist <nyquist@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1070602}
    
    Bug: 1342070, 1383350
    Change-Id: Ibcf2f505bcd45ab7834dd18da49cbef2ae6b6dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032108
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072342}

--- components/webapps/browser/features.cc
+++ components/webapps/browser/features.cc
@@ -34,7 +34,7 @@ extern const base::FeatureParam<int>
 // Enables PWA Unique IDs for WebAPKs.
 BASE_FEATURE(kWebApkUniqueId,
              "WebApkUniqueId",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 // When the user clicks "Create Shortcut" in the dot menu, the current page is
