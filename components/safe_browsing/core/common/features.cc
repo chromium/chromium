65446bd997cc875c65f080d26405890ad70a429e
thefrog <thefrog@chromium.org>
2022-11-16 22:02:29 +0000

    Enable SafeBrowsingCsbrrWithToken by default
    Bug: 1232149
    Change-Id: I6733b95a248cc7a20f1f929d22b3431c4d4671cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030662
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072466}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -170,7 +170,7 @@ BASE_FEATURE(kSafeBrowsingCsbrrNewDownloadTrigger,
 
 BASE_FEATURE(kSafeBrowsingCsbrrWithToken,
              "SafeBrowsingCsbrrWithToken",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSafeBrowsingDisableConsumerCsdForEnterprise,
              "SafeBrowsingDisableConsumerCsdForEnterprise",
