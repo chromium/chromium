68159cb8bf40c81fd3a40753627469ef0dd3b2e7
Xianzhu Wang <wangxianzhu@chromium.org>
2022-11-16 19:10:35 +0000

    Remove no-op blink clang blugin option fix-bugs-of-is-considered-abstract
    Change-Id: I86ecb13006d6641c96a93ce182a5442bf4ec8cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032106
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072372}

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -45,8 +45,6 @@ class BlinkGCPluginAction : public PluginASTAction {
         options_.enable_members_on_stack_check = true;
       } else if (arg == "enable-extra-padding-check") {
         options_.enable_extra_padding_check = true;
-      } else if (arg == "fix-bugs-of-is-considered-abstract") {
-        // This flag is now always enabled. TODO(wangxianzhu): Remove this flag.
       } else {
         llvm::errs() << "Unknown blink-gc-plugin argument: " << arg << "\n";
         return false;
