b2e60f07f8f0b85c9fd803243aefb52fc5dad93d
Jesse McKenna <jessemckenna@google.com>
2022-11-17 20:49:46 +0000

    Remove Win 8 fallback handling in DWriteFontProxy
    This change removes an assertion that IDWriteFontFallback is available
    only on Windows 10+. Windows 7/8 are no longer supported on M110, so
    there is no need to explicitly handle their behavior.
    
    Because the fallback is always available on Windows 10+, this change
    simply passes true to `SetUseSkiaFontFallback()`, i.e., the Skia font
    fallback is always okay to use.
    
    This fixes a DCHECK failure on Windows 10 when run in Windows-8
    compatibility mode [1]. When run in this mode, the fallback is
    available (because the real OS version is Windows 10) but the OS
    version reported by GetVersion() is Windows 8.
    
    [1] done by:
      1. right-clicking a taskbar icon
      2. navigating to Properties > Compatibility
      3. checking the box "Run this program in compatibility mode for"
      4. choosing the desired Windows version in the dropdown menu
      5. clicking OK
      6. launching Chrome via the taskbar icon
    
    Bug: 1384364, 1385856
    Change-Id: I4def45695fd92c781f1e3426232eb1acb2b449b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4032294
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073021}

--- content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.cc
+++ content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.cc
@@ -79,17 +79,8 @@ void InitializeDWriteFontProxy() {
 
   skia::OverrideDefaultSkFontMgr(std::move(skia_font_manager));
 
-  // When IDWriteFontFallback is not available (prior to Win8.1) Skia will
-  // still attempt to use DirectWrite to determine fallback fonts (in
-  // SkFontMgr_DirectWrite::onMatchFamilyStyleCharacter), which will likely
-  // result in trying to load the system font collection. To avoid that and
-  // instead fall back on WebKit's fallback logic, we don't use Skia's font
-  // fallback if IDWriteFontFallback is not available.
-  // This flag can be removed when Win8.0 and earlier are no longer supported.
-  bool fallback_available = g_font_fallback != nullptr;
-  DCHECK_EQ(fallback_available,
-            base::win::GetVersion() > base::win::Version::WIN8);
-  blink::WebFontRendering::SetUseSkiaFontFallback(fallback_available);
+  DCHECK(g_font_fallback);
+  blink::WebFontRendering::SetUseSkiaFontFallback(true);
 }
 
 void UninitializeDWriteFontProxy() {
