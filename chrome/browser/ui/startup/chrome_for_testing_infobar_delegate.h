00614908b44790e48c26c20adbe231e9295b2b7c
Thiago Perrotta <tperrotta@chromium.org>
2022-11-17 13:07:57 +0000

    Add a persistent infobar warning when launching Chrome for Testing
    The CfT infobar is meant to warn developers/users that CfT is an unsupported product that does not auto-update.
    
    Design document: https://goo.gle/chrome-for-testing#bookmark=id.8oia0mpldm1d
    
    Preview: https://bugs.chromium.org/p/chromium/issues/detail?id=1336611#c56
    
    Bug: 1336611
    Change-Id: Ia4a017c2828a5fcaae97de57dfe8c6ae27136c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3922097
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Thiago Perrotta <tperrotta@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072750}

---
+++ chrome/browser/ui/startup/chrome_for_testing_infobar_delegate.h
@@ -0,0 +1,42 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_STARTUP_CHROME_FOR_TESTING_INFOBAR_DELEGATE_H_
+#define CHROME_BROWSER_UI_STARTUP_CHROME_FOR_TESTING_INFOBAR_DELEGATE_H_
+
+#include <string>
+
+#include "components/infobars/core/confirm_infobar_delegate.h"
+#include "url/gurl.h"
+
+namespace infobars {
+class ContentInfoBarManager;
+}
+
+// An infobar for Chrome for Testing, which displays a message saying that this
+// flavor of chrome is unsupported and does not auto-update.
+class ChromeForTestingInfoBarDelegate : public ConfirmInfoBarDelegate {
+ public:
+  static void Create();
+  static void Create(infobars::ContentInfoBarManager* infobar_manager);
+
+  ChromeForTestingInfoBarDelegate(const ChromeForTestingInfoBarDelegate&) =
+      delete;
+  ChromeForTestingInfoBarDelegate& operator=(
+      const ChromeForTestingInfoBarDelegate&) = delete;
+  ChromeForTestingInfoBarDelegate() = default;
+  ~ChromeForTestingInfoBarDelegate() override = default;
+
+ private:
+  infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
+  std::u16string GetMessageText() const override;
+  std::u16string GetLinkText() const override;
+  GURL GetLinkURL() const override;
+  bool ShouldExpire(const NavigationDetails& details) const override;
+  bool ShouldAnimate() const override;
+  int GetButtons() const override;
+  bool IsCloseable() const override;
+};
+
+#endif  // CHROME_BROWSER_UI_STARTUP_CHROME_FOR_TESTING_INFOBAR_DELEGATE_H_
--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -8,6 +8,7 @@
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chrome_for_testing/buildflags.h"
 #include "chrome/browser/obsolete_system/obsolete_system.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -25,6 +26,10 @@
 #include "components/prefs/pref_service.h"
 #include "content/public/common/content_switches.h"
 #include "google_apis/google_api_keys.h"
+
+#if BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+#include "chrome/browser/ui/startup/chrome_for_testing_infobar_delegate.h"
+#endif
 
 namespace {
 bool ShouldShowBadFlagsSecurityWarnings() {
@@ -70,8 +75,22 @@ void AddInfoBarsIfNecessary(Browser* browser,
 
   // Show the Automation info bar unless it has been disabled by policy.
   bool show_bad_flags_security_warnings = ShouldShowBadFlagsSecurityWarnings();
-  if (IsAutomationEnabled() && show_bad_flags_security_warnings) {
-    AutomationInfoBarDelegate::Create();
+
+  content::WebContents* web_contents =
+      browser->tab_strip_model()->GetActiveWebContents();
+  DCHECK(web_contents);
+
+  infobars::ContentInfoBarManager* infobar_manager =
+      infobars::ContentInfoBarManager::FromWebContents(web_contents);
+
+  if (show_bad_flags_security_warnings) {
+#if BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+    // TODO(crbug.com/1336611): Switch to a global infobar.
+    ChromeForTestingInfoBarDelegate::Create(infobar_manager);
+#endif
+
+    if (IsAutomationEnabled())
+      AutomationInfoBarDelegate::Create();
   }
 
   // Do not show any other info bars in Kiosk mode, because it's unlikely that
@@ -103,15 +122,8 @@ void AddInfoBarsIfNecessary(Browser* browser,
       return;
     infobars_shown = true;
 
-    content::WebContents* web_contents =
-        browser->tab_strip_model()->GetActiveWebContents();
-    DCHECK(web_contents);
-
     if (show_bad_flags_security_warnings)
       chrome::ShowBadFlagsPrompt(web_contents);
-
-    infobars::ContentInfoBarManager* infobar_manager =
-        infobars::ContentInfoBarManager::FromWebContents(web_contents);
 
     if (!google_apis::HasAPIKeyConfigured())
       GoogleApiKeysInfoBarDelegate::Create(infobar_manager);
