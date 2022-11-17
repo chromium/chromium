c18aa235234e8a7ed39fa66a51194686c062656a
Austin Sullivan <asully@chromium.org>
2022-11-17 20:55:56 +0000

    sql: Do not compile Chromium with SQLITE_OMIT_COMPLETE
    Chromium ships the SQLite CLI (the sqlite_shell and sqlite_shell_dev
    build targets) for developmental purposes.
    
    As of SQLite version 3.40.0, building the CLI is incompatible with
    passing the SQLITE_OMIT_COMPLETE build flag.
    
    This adds a trivial hit to the SQLite binary size (820 bytes, or ~0.13%)
    
    Bug: 1385498
    Change-Id: I413d0dfac8654d191b6e4d20816266427dd5d8c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031359
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073026}

--- third_party/sqlite/sqlite3.h
+++ third_party/sqlite/sqlite3.h
@@ -14,30 +14,6 @@
 // dynamic library loader.
 #include "third_party/sqlite/src/amalgamation/rename_exports.h"
 
-#if defined(SQLITE_OMIT_COMPLETE)
-// When SQLITE_OMIT_COMPLETE is defined, sqlite3.h does not emit a declaration
-// for sqlite3_complete(). SQLite's shell.c stubs out the function by #defining
-// a macro.
-//
-// In order to avoid a macro redefinition warning, we must undo the #define in
-// rename_exports.h.
-//
-// Historical note: SQLite's shell.c initially did not support building against
-// a libary with SQLITE_OMIT_COMPLETE at all. The first attempt at adding
-// support was https://www.sqlite.org/src/info/c3e816cca4ddf096 which defined
-// sqlite_complete() as a stub function in shell.c. This worked on UNIX systems,
-// but caused a compilation error on Windows, where sqlite3.h declares
-// sqlite3_complete() as a __declspec(dllimport). The Windows build error was
-// fixed in https://www.sqlite.org/src/info/d584a0cb51281594 at our request.
-// While the current approach of using a macro requires the workaround here, it
-// is preferable to the previous version, which did not build at all on Windows.
-#if defined(sqlite3_complete)
-#undef sqlite3_complete
-#else
-#error "This workaround is no longer needed."
-#endif  // !defined(sqlite3_complete)
-#endif  // defined(SQLITE_OMIT_COMPLETE)
-
 #if defined(SQLITE_OMIT_COMPILEOPTION_DIAGS)
 // When SQLITE_OMIT_COMPILEOPTION_DIAGS is defined, sqlite3.h emits macros
 // instead of declarations for sqlite3_compileoption_{get,used}().
