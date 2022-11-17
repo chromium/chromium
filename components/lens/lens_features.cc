63487996229e8bb417cea87bec40d1fca4d042a7
Fergal Daly <fergal@chromium.org>
2022-11-17 00:50:35 +0000

    Revert "Flip flag for region search on PDF viewer."
    This reverts commit 02c53b85754d76eb73c5de969c2fdd67a21aedc2.
    
    Reason for revert: Speculatively blaming for failure of PDFExtensionRegionSearchTest started failing after this change.
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/27363/test-results
    
    Original change's description:
    > Flip flag for region search on PDF viewer.
    >
    > Bug: b/250676823
    > Change-Id: Ie35f2425ebe44a4482f620d2670f2ba389c4f7c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4029070
    > Reviewed-by: Juan Mojica <juanmojica@google.com>
    > Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1072376}
    
    Bug: b/250676823, 1385542
    Change-Id: Ia59de59a73ce42084f49eb28d8ecf206906588ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4033266
    Owners-Override: Fergal Daly <fergal@google.com>
    Owners-Override: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1072537}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -32,7 +32,7 @@ BASE_FEATURE(kEnableLatencyLogging,
 
 BASE_FEATURE(kEnableRegionSearchOnPdfViewer,
              "LensEnableRegionSearchOnPdfViewer",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensInstructionChipImprovements,
              "LensInstructionChipImprovements",
