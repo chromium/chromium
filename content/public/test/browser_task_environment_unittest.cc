367a4b42c10c25aa8e535cc6a19364aec129839c
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-11-18 12:42:21 +0000

    Cleanup: Use base/test/gtest_util.h more often [7/N]
    Depending on the platform and compile flag, GTest's death test are
    more or less reliable. This forced users to wrap their code with
    macros. This header provide a better alternative. This patch makes
    use of it to simplify code and increase the proportion of developer
    using them (by the virtue of copy-pasting existing usages).
    
    This patch uses base/test/gtest_util.h to simplify this file.
    
    Bug: 1376132
    Change-Id: I1b6212a296fcd35e817fb19fbf540baed24fdb8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3967198
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073320}

--- content/public/test/browser_task_environment_unittest.cc
+++ content/public/test/browser_task_environment_unittest.cc
@@ -14,6 +14,7 @@
 #include "base/task/current_thread.h"
 #include "base/task/thread_pool.h"
 #include "base/test/bind.h"
+#include "base/test/gtest_util.h"
 #include "build/build_config.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -32,13 +33,7 @@ namespace {
 constexpr int kNumHops = 13;
 constexpr int kNumTasks = 8;
 
-#if DCHECK_IS_ON() && !BUILDFLAG(IS_ANDROID)
-// Expect that in builds with working DCHECK messages the failure message
-// includes a hint towards using the BrowserTaskEnvironment class.
 const char kDeathMatcher[] = "Check failed:.*\n*.*BrowserTaskEnvironment";
-#else
-const char kDeathMatcher[] = "";
-#endif
 
 void PostTaskToUIThread(int iteration, base::subtle::Atomic32* tasks_run);
 
@@ -121,12 +116,10 @@ TEST(BrowserTaskEnvironmentTest, RunIOThreadUntilIdle) {
 }
 
 TEST(BrowserTaskEnvironmentTest, MessageLoopTypeMismatch) {
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
-
   base::test::TaskEnvironment task_environment(
       base::test::TaskEnvironment::MainThreadType::UI);
 
-  EXPECT_DEATH_IF_SUPPORTED(
+  BASE_EXPECT_DEATH(
       {
         BrowserTaskEnvironment second_task_environment(
             BrowserTaskEnvironment::IO_MAINLOOP);
@@ -135,9 +128,7 @@ TEST(BrowserTaskEnvironmentTest, MessageLoopTypeMismatch) {
 }
 
 TEST(BrowserTaskEnvironmentTest, MultipleBrowserTaskEnvironment) {
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
-
-  EXPECT_DEATH_IF_SUPPORTED(
+  BASE_EXPECT_DEATH(
       {
         BrowserTaskEnvironment task_environment;
         BrowserTaskEnvironment other_task_environment;
@@ -192,30 +183,28 @@ TEST(BrowserTaskEnvironmentTest, TraitsConstructorOverrideMainThreadType) {
 // Verify that posting tasks to the UI thread without having the
 // BrowserTaskEnvironment instance cause a crash.
 TEST(BrowserTaskEnvironmentTest, NotInitializedUIThread) {
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
   base::test::TaskEnvironment task_environment(
       base::test::TaskEnvironment::MainThreadType::UI);
 
-  EXPECT_DEATH_IF_SUPPORTED(
-      { GetUIThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()); },
+  EXPECT_DCHECK_DEATH_WITH(
+      GetUIThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()),
       kDeathMatcher);
-  EXPECT_DEATH_IF_SUPPORTED(
-      { GetUIThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()); },
+  EXPECT_DCHECK_DEATH_WITH(
+      GetUIThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()),
       kDeathMatcher);
 }
 
 // Verify that posting tasks to the IO thread without having the
 // BrowserTaskEnvironment instance cause a crash.
 TEST(BrowserTaskEnvironmentTest, NotInitializedIOThread) {
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
   base::test::TaskEnvironment task_environment(
       base::test::TaskEnvironment::MainThreadType::IO);
 
-  EXPECT_DEATH_IF_SUPPORTED(
-      { GetIOThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()); },
+  EXPECT_DCHECK_DEATH_WITH(
+      GetIOThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()),
       kDeathMatcher);
-  EXPECT_DEATH_IF_SUPPORTED(
-      { GetIOThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()); },
+  EXPECT_DCHECK_DEATH_WITH(
+      GetIOThreadTaskRunner({})->PostTask(FROM_HERE, base::DoNothing()),
       kDeathMatcher);
 }
 
