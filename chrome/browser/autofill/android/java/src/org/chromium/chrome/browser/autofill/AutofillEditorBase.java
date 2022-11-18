3b9f78f540ed4dc6127c53a4b926a286ed74427c
Ivana Žužić <izuzic@google.com>
2022-11-18 14:33:11 +0000

    [TTFCC] Add a new autofill target
    The target will be used for modularizing the autofill code that is now
    part of chrome_java.
    
    Bug: 1247698
    Change-Id: Iafe6bd4258ced348ac48b4e4c1dfaaf9a0e1b1df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037766
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073351}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/AutofillEditorBase.java
@@ -0,0 +1,10 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill;
+
+/** Base class for Autofill editors (e.g. credit cards and profiles). */
+public class AutofillEditorBase {
+    // TODO(1247698): Move the AutofillEditorBase code here.
+}
