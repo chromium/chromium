9335654be849dcf4e5f4bc232a11fcd19c0ad7be
Alexander Dunaev <adunaev@igalia.com>
2022-11-18 07:59:44 +0000

    [linux/wayland] Fixed threading in wayland text input wrapper unit tests.
    This patch converts the wayland text input wrapper unit tests into async ones.
    
    Bug: 1365887
    Change-Id: Iddaa3fe9f254cb8c7d53444e67a48659fd8c6276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4034446
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1073253}

--- ui/ozone/platform/wayland/host/zwp_text_input_wrapper_v1_unittest.cc
+++ ui/ozone/platform/wayland/host/zwp_text_input_wrapper_v1_unittest.cc
@@ -17,6 +17,7 @@
 #include "ui/ozone/platform/wayland/test/wayland_test.h"
 
 using ::testing::InSequence;
+using ::testing::Mock;
 using ::testing::Values;
 
 namespace ui {
@@ -24,7 +25,7 @@ namespace {
 
 class ZWPTextInputWrapperV1Test : public WaylandTest {
  public:
-  ZWPTextInputWrapperV1Test() = default;
+  ZWPTextInputWrapperV1Test() : WaylandTest(TestServerMode::kAsync) {}
   ZWPTextInputWrapperV1Test(const ZWPTextInputWrapperV1Test&) = delete;
   ZWPTextInputWrapperV1Test& operator=(const ZWPTextInputWrapperV1Test&) =
       delete;
@@ -36,75 +37,77 @@ class ZWPTextInputWrapperV1Test : public WaylandTest {
     wrapper_ = std::make_unique<ZWPTextInputWrapperV1>(
         connection_.get(), nullptr, connection_->text_input_manager_v1(),
         connection_->text_input_extension_v1());
-
-    connection_->Flush();
-    Sync();
-
-    mock_text_input_ = server_.text_input_manager_v1()->text_input();
-    ASSERT_TRUE(mock_text_input_);
-    mock_ext_text_input_ =
-        server_.text_input_extension_v1()->extended_text_input();
-    ASSERT_TRUE(mock_ext_text_input_);
   }
 
  protected:
   std::unique_ptr<ZWPTextInputWrapperV1> wrapper_;
-  wl::MockZwpTextInput* mock_text_input_ = nullptr;
-  wl::MockZcrExtendedTextInput* mock_ext_text_input_ = nullptr;
 };
 
 TEST_P(ZWPTextInputWrapperV1Test,
        FinalizeVirtualKeyboardChangesShowInputPanel) {
-  InSequence s;
-  EXPECT_CALL(*mock_text_input_, ShowInputPanel());
-  EXPECT_CALL(*mock_ext_text_input_, FinalizeVirtualKeyboardChanges());
-  wrapper_->ShowInputPanel();
-  connection_->Flush();
-  Sync();
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    InSequence s;
+    EXPECT_CALL(*server->text_input_manager_v1()->text_input(),
+                ShowInputPanel());
+    EXPECT_CALL(*server->text_input_extension_v1()->extended_text_input(),
+                FinalizeVirtualKeyboardChanges());
+  });
 
-  // Flush again after sync, so the scheduled finalize request is processed.
-  connection_->Flush();
-  Sync();
+  wrapper_->ShowInputPanel();
+
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    Mock::VerifyAndClearExpectations(
+        server->text_input_manager_v1()->text_input());
+    Mock::VerifyAndClearExpectations(server->text_input_extension_v1());
+  });
 }
 
 TEST_P(ZWPTextInputWrapperV1Test,
        FinalizeVirtualKeyboardChangesHideInputPanel) {
-  InSequence s;
-  EXPECT_CALL(*mock_text_input_, HideInputPanel());
-  EXPECT_CALL(*mock_ext_text_input_, FinalizeVirtualKeyboardChanges());
-  wrapper_->HideInputPanel();
-  connection_->Flush();
-  Sync();
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    InSequence s;
+    EXPECT_CALL(*server->text_input_manager_v1()->text_input(),
+                HideInputPanel());
+    EXPECT_CALL(*server->text_input_extension_v1()->extended_text_input(),
+                FinalizeVirtualKeyboardChanges());
+  });
 
-  // Flush again after sync, so the scheduled finalize request is processed.
-  connection_->Flush();
-  Sync();
+  wrapper_->HideInputPanel();
+
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    Mock::VerifyAndClearExpectations(
+        server->text_input_manager_v1()->text_input());
+    Mock::VerifyAndClearExpectations(
+        server->text_input_extension_v1()->extended_text_input());
+  });
 }
 
 TEST_P(ZWPTextInputWrapperV1Test,
        FinalizeVirtualKeyboardChangesMultipleInputPanelChanges) {
-  InSequence s;
-  EXPECT_CALL(*mock_text_input_, ShowInputPanel());
-  EXPECT_CALL(*mock_text_input_, HideInputPanel());
-  EXPECT_CALL(*mock_text_input_, ShowInputPanel());
-  EXPECT_CALL(*mock_text_input_, HideInputPanel());
-  EXPECT_CALL(*mock_text_input_, ShowInputPanel());
-  EXPECT_CALL(*mock_ext_text_input_, FinalizeVirtualKeyboardChanges());
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    auto* const mock_text_input = server->text_input_manager_v1()->text_input();
+    InSequence s;
+    EXPECT_CALL(*mock_text_input, ShowInputPanel());
+    EXPECT_CALL(*mock_text_input, HideInputPanel());
+    EXPECT_CALL(*mock_text_input, ShowInputPanel());
+    EXPECT_CALL(*mock_text_input, HideInputPanel());
+    EXPECT_CALL(*mock_text_input, ShowInputPanel());
+    EXPECT_CALL(*server->text_input_extension_v1()->extended_text_input(),
+                FinalizeVirtualKeyboardChanges());
+  });
+
   wrapper_->ShowInputPanel();
   wrapper_->HideInputPanel();
   wrapper_->ShowInputPanel();
   wrapper_->HideInputPanel();
   wrapper_->ShowInputPanel();
-  connection_->Flush();
-  Sync();
 
-  // Flush again after sync, so the scheduled finalize request is processed.
-  connection_->Flush();
-  Sync();
-
-  // Flush and sync again to make sure no extra finalize request.
-  connection_->Flush();
-  Sync();
+  PostToServerAndWait([](wl::TestWaylandServerThread* server) {
+    Mock::VerifyAndClearExpectations(
+        server->text_input_manager_v1()->text_input());
+    Mock::VerifyAndClearExpectations(
+        server->text_input_extension_v1()->extended_text_input());
+  });
 }
 
 INSTANTIATE_TEST_SUITE_P(XdgVersionStableTest,
