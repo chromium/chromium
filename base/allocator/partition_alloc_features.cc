6689bf0c09ed480c55464330dcb87eb79b722a22
Keishi Hattori <keishi@chromium.org>
2022-11-18 04:31:57 +0000

    [BRP] Test enable_backup_ref_ptr_slow_checks temporarily on mac
    Test enable_backup_ref_ptr_slow_checks build flag and PartitionAllocBackupRefPtr experiment for one Canary release.
    The size increase for PartitionRefCount might be a workaround for the bug.
    
    Bug: 1378822
    Change-Id: I7249ceb5fa8c586747833ffad8ae9587e7b9a4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4019078
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073206}

--- base/allocator/partition_alloc_features.cc
+++ base/allocator/partition_alloc_features.cc
@@ -87,7 +87,9 @@ BASE_FEATURE(kPartitionAllocLargeEmptySlotSpanRing,
 
 BASE_FEATURE(kPartitionAllocBackupRefPtr,
              "PartitionAllocBackupRefPtr",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) || \
+// TODO(keishi): Enabled on mac temporarily for debugging. Will be removed after
+// one Canary release.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || \
     (BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && BUILDFLAG(IS_LINUX))
              FEATURE_ENABLED_BY_DEFAULT
 #else
