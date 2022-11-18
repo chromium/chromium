6a18c237ea62faece9a01ce5841c29f33555888e
danakj <danakj@chromium.org>
2022-11-18 21:46:16 +0000

    Remove crates.py, as gnrt now has all the same abilities and is better
    Update the docs to use gnrt for downloading crates.
    
    R=lukasza@chromium.org
    
    Bug: 1381552
    Change-Id: I49736da7ebb7f756fb55aac430ad43ae7bd71a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4036171
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1073562}

--- tools/crates/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Copyright 2021 The Chromium Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-python3 -m unittest discover -s lib -p "*_test.py" $*
