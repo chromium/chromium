024fb1f2b9a7bd75d6ec56710ba566646c005e4c
Jason Rhee <jasonrhee@google.com>
2022-11-19 00:05:52 +0000

    [QuickStart] Add barebones TargetDeviceClient with SendPayload
    This CL adds the base client class with public interfaces coming later that integrates with AuthenticatedConnection to prepare requests and parse responses of round trips with Android. go/cros-quick-start-base-client-class
    
    TEST=Written unit test initializing and testing SendPayload using a fake_nearby_connection.
    
    Bug: b/258680767
    Change-Id: Ib44a81574ea1820f40aa16032200d0a3d59237c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031533
    Reviewed-by: Curt Clemens <cclem@google.com>
    Commit-Queue: Jason Rhee <jasonrhee@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1073645}

---
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/target_device_client_base.h
@@ -0,0 +1,46 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_TARGET_DEVICE_CLIENT_BASE_H_
+#define CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_TARGET_DEVICE_CLIENT_BASE_H_
+
+#include "base/values.h"
+#include "chrome/browser/ash/login/oobe_quick_start/connectivity/quick_start_decoder.h"
+#include "chrome/browser/nearby_sharing/public/cpp/nearby_connection.h"
+
+namespace ash::quick_start {
+
+class TargetDeviceClientBaseTest;
+
+// TargetDeviceClientBase is the parent class for all clients that will prepare
+// requests and parse responses for all round trips between the Chromebook
+// and Android phone during Quick Start. There will always be only one client
+// running at any time. Multiple clients will interfere with each other.
+class TargetDeviceClientBase {
+ public:
+  TargetDeviceClientBase(const TargetDeviceClientBase&) = delete;
+  TargetDeviceClientBase& operator=(const TargetDeviceClientBase&) = delete;
+  ~TargetDeviceClientBase();
+
+ protected:
+  TargetDeviceClientBase(NearbyConnection* nearby_connection,
+                         QuickStartDecoder* quick_start_decoder);
+  void SendPayload(const base::Value::Dict& message_payload);
+
+  // passed as a callback to nearby_connection_->Read();
+  void OnDataRead(absl::optional<std::vector<uint8_t>> bytes);
+
+  NearbyConnection* nearby_connection_;
+  QuickStartDecoder* quick_start_decoder_;
+
+ private:
+  // This will allow TargetDeviceClientBaseTest to call the protected methods
+  // for unit tests.
+  friend class TargetDeviceClientBaseTest;
+  base::WeakPtrFactory<TargetDeviceClientBase> weak_ptr_factory_{this};
+};
+
+}  // namespace ash::quick_start
+
+#endif  // CHROME_BROWSER_ASH_LOGIN_OOBE_QUICK_START_CONNECTIVITY_TARGET_DEVICE_CLIENT_BASE_H_
