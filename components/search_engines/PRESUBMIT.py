2b63f20b418930681b8ebad95de80299c44283a7
Orin Jaworski <orinj@google.com>
2022-11-21 20:29:54 +0000

    Add PRESUBMIT.py for components/search_engines and check data version
    This CL creates a presubmit check for components/search_engines and
    warns if prepopulated_engines.json has changed without also changing
    kCurrentDataVersion. See bug for more details.
    
    Bug: 1380077
    Change-Id: Ic633209199fa8e80a4eb09776599518e1421851b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031353
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074235}

---
+++ components/search_engines/PRESUBMIT.py
@@ -0,0 +1,36 @@
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Presubmit script for changes affecting components/search_engines/
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+USE_PYTHON3 = True
+
+import os
+
+def _CheckPrepopulatedEnginesVersion(input_api, output_api):
+  """Check that no changes were made to prepopulated_engines.json without
+  also updating kCurrentDataVersion"""
+  results = []
+  file = next((f for f in input_api.AffectedFiles()
+                 if os.path.basename(f.LocalPath()) ==
+                 "prepopulated_engines.json"), None)
+
+  if file != None and not any(line for line in file.ChangedContents()
+                              if "kCurrentDataVersion" in line[1]):
+      results.append(output_api.PresubmitPromptWarning(
+          ("prepopulated_engines.json changed but kCurrentDataVersion "
+           "did not. Please ensure the version is rolled up when making "
+           "meaningful changes to prepopulated_engines.json")))
+
+  return results
+
+def CheckChangeOnUpload(input_api, output_api):
+  results = []
+  results.extend(_CheckPrepopulatedEnginesVersion(input_api, output_api))
+  return results
+
