d001faa7ef4f71af81d286e53cab19d4ec719327
Mikhail Khokhlov <khokhlov@google.com>
2022-11-21 15:38:15 +0000

    Use TraceAnalyzer in GpuChannelManagerTest
    Chrome's tracing backend is being migrated to Perfetto SDK, which
    doesn't support FILTERING_MODE. Tests should instead collect a trace
    and test it against test expectations.
    
    Bug: b/257422128
    Change-Id: Idfc077f21a223cfc4c8098551043fcdbff9058c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030516
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074091}

--- gpu/ipc/service/gpu_channel_manager_unittest.cc
+++ gpu/ipc/service/gpu_channel_manager_unittest.cc
@@ -8,12 +8,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "base/trace_event/category_registry.h"
-#include "base/trace_event/trace_arguments.h"
-#include "base/trace_event/trace_category.h"
-#include "base/trace_event/trace_event_filter.h"
-#include "base/trace_event/trace_event_impl.h"
-#include "base/trace_event/trace_log.h"
+#include "base/test/trace_event_analyzer.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/capabilities.h"
@@ -23,59 +18,6 @@
 #include "gpu/ipc/common/gpu_channel.mojom.h"
 #include "gpu/ipc/service/gpu_channel.h"
 #include "gpu/ipc/service/gpu_channel_test_common.h"
-
-namespace {
-
-// static
-// Cache of the last TraceEvent seen by TestTraceEventFilter, as it cannot be
-// stored directly in the class, due to the filtering methods being const.
-std::unique_ptr<base::trace_event::TraceEvent> g_trace_event;
-
-// Testing filter to observe "gpu" trace events. The latest one seen is copied
-// into |g_trace_event|.
-class TestTraceEventFilter : public base::trace_event::TraceEventFilter {
- public:
-  TestTraceEventFilter() { g_trace_event.reset(); }
-  ~TestTraceEventFilter() override { g_trace_event.reset(); }
-
-  static std::unique_ptr<base::trace_event::TraceEventFilter> Factory(
-      const std::string& predicate_name) {
-    std::unique_ptr<TestTraceEventFilter> res =
-        std::make_unique<TestTraceEventFilter>();
-    return res;
-  }
-
-  // base::trace_event::TraceEventFilter:
-  bool FilterTraceEvent(
-      const base::trace_event::TraceEvent& trace_event) const override {
-    const auto* category =
-        base::trace_event::CategoryRegistry::GetCategoryByStatePtr(
-            trace_event.category_group_enabled());
-
-    if (!strcmp(category->name(), "gpu")) {
-      CHECK_EQ(2u, trace_event.arg_size()) << trace_event.name();
-      // The first arg is always recorded as a uint64_t, whereas the second is
-      // a TracedValue. Here we force the first to be recorded as_uint, as on
-      // KitKat the union is failing to transpose correctly when using
-      // as_convertable.
-      std::unique_ptr<base::trace_event::TraceArguments> args =
-          std::make_unique<base::trace_event::TraceArguments>(
-              trace_event.arg_name(0), trace_event.arg_value(0).as_uint,
-              trace_event.arg_name(1),
-              static_cast<void*>(trace_event.arg_value(1).as_convertable));
-
-      g_trace_event = std::make_unique<base::trace_event::TraceEvent>(
-          trace_event.thread_id(), trace_event.timestamp(),
-          trace_event.thread_timestamp(), trace_event.phase(),
-          trace_event.category_group_enabled(), trace_event.name(),
-          trace_event.scope(), trace_event.id(), trace_event.bind_id(),
-          args.get(), trace_event.flags());
-    }
-    return true;
-  }
-};
-
-}  // namespace
 
 namespace gpu {
 
@@ -200,21 +142,7 @@ TEST_F(GpuChannelManagerTest, OnBackgroundedWithWebGL) {
 // Tests that peak memory usage is only reported for valid sequence numbers,
 // and that polling shuts down the monitoring.
 TEST_F(GpuChannelManagerTest, GpuPeakMemoryOnlyReportedForValidSequence) {
-  // Setup filtering to observe traces emitted.
-  base::trace_event::TraceLog* trace_log =
-      base::trace_event::TraceLog::GetInstance();
-  trace_log->SetFilterFactoryForTesting(TestTraceEventFilter::Factory);
-  const char config_json[] = R"(
-      {
-        "event_filters": [
-           {
-             "filter_predicate": "gpu",
-             "included_categories": ["*"]
-           }
-        ]
-      } )";
-  trace_log->SetEnabled(base::trace_event::TraceConfig(config_json),
-                        base::trace_event::TraceLog::FILTERING_MODE);
+  trace_analyzer::Start("gpu");
 
   GpuChannelManager* manager = channel_manager();
   const CommandBufferId buffer_id =
@@ -226,21 +154,10 @@ TEST_F(GpuChannelManagerTest, GpuPeakMemoryOnlyReportedForValidSequence) {
   manager->StartPeakMemoryMonitor(sequence_num);
   EXPECT_EQ(current_memory, GetMonitorsPeakMemoryUsage(sequence_num));
 
-  // A trace should have been emitted.
-  EXPECT_NE(nullptr, g_trace_event);
-  EXPECT_STREQ("PeakMemoryTracking", g_trace_event->name());
-  EXPECT_STREQ("start", g_trace_event->arg_name(0));
-  EXPECT_EQ(current_memory, g_trace_event->arg_value(0).as_uint);
-  EXPECT_STREQ("start_sources", g_trace_event->arg_name(1));
-  EXPECT_NE(nullptr, g_trace_event->arg_value(1).as_pointer);
-  g_trace_event.reset();
-
   // With no request to listen to memory it should report 0.
   const uint32_t invalid_sequence_num = 1337;
   EXPECT_EQ(0u, GetMonitorsPeakMemoryUsage(invalid_sequence_num));
   EXPECT_EQ(0u, GetManagersPeakMemoryUsage(invalid_sequence_num));
-  // There should be no trace emitted for invalid sequence.
-  EXPECT_EQ(nullptr, g_trace_event);
 
   // The valid sequence should receive a report.
   EXPECT_EQ(current_memory, GetManagersPeakMemoryUsage(sequence_num));
@@ -248,19 +165,34 @@ TEST_F(GpuChannelManagerTest, GpuPeakMemoryOnlyReportedForValidSequence) {
   EXPECT_EQ(0u, GetMonitorsPeakMemoryUsage(sequence_num));
   EXPECT_EQ(0u, GetManagersPeakMemoryUsage(sequence_num));
 
-  // A trace should have been emitted as well.
-  EXPECT_NE(nullptr, g_trace_event);
-  EXPECT_STREQ("PeakMemoryTracking", g_trace_event->name());
-  EXPECT_STREQ("peak", g_trace_event->arg_name(0));
-  EXPECT_EQ(current_memory, g_trace_event->arg_value(0).as_uint);
-  EXPECT_STREQ("end_sources", g_trace_event->arg_name(1));
-  EXPECT_NE(nullptr, g_trace_event->arg_value(1).as_pointer);
-  g_trace_event.reset();
+  auto analyzer = trace_analyzer::Stop();
+  trace_analyzer::TraceEventVector events;
+  analyzer->FindEvents(trace_analyzer::Query::EventNameIs("PeakMemoryTracking"),
+                       &events);
 
-  // Tracing's globals are not reset between tests. Clear out our filter and
-  // disable tracing.
-  trace_log->SetFilterFactoryForTesting(nullptr);
-  trace_log->SetDisabled(base::trace_event::TraceLog::FILTERING_MODE);
+  EXPECT_EQ(2u, events.size());
+
+  ASSERT_TRUE(events[0]->HasNumberArg("start"));
+  EXPECT_EQ(current_memory,
+            static_cast<uint64_t>(events[0]->GetKnownArgAsDouble("start")));
+  ASSERT_TRUE(events[0]->HasDictArg("start_sources"));
+  EXPECT_FALSE(events[0]->GetKnownArgAsDict("start_sources").empty());
+
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+  // In Perfetto, arguments of begin and end events are merged and emitted
+  // with the begin event.
+  const int kEndArgumentsSliceIndex = 0;
+#else
+  const int kEndArgumentsSliceIndex = 1;
+#endif
+  ASSERT_TRUE(events[kEndArgumentsSliceIndex]->HasNumberArg("peak"));
+  EXPECT_EQ(current_memory,
+            static_cast<uint64_t>(
+                events[kEndArgumentsSliceIndex]->GetKnownArgAsDouble("peak")));
+  ASSERT_TRUE(events[kEndArgumentsSliceIndex]->HasDictArg("end_sources"));
+  EXPECT_FALSE(events[kEndArgumentsSliceIndex]
+                   ->GetKnownArgAsDict("end_sources")
+                   .empty());
 }
 
 // Tests that while a channel may exist for longer than a request to monitor,
