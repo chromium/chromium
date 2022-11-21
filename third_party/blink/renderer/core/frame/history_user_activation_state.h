f8e05d3705f200d883e55884d078fce5b9c1ddfc
shivanigithub <shivanisha@chromium.org>
2022-11-21 23:45:05 +0000

    Rephrase history intervention desc in HistoryUserActivationState
    This CL slightly changes the description of the history intervention
    in the comment to make it clear that `skippable` is not set when an
    entry is created but rather when it is navigated away from.
    
    Also added some minor edits.
    
    Change-Id: Ifedc5fe42f320257e406eff2db873aa13e35b460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4042392
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074328}

--- third_party/blink/renderer/core/frame/history_user_activation_state.h
+++ third_party/blink/renderer/core/frame/history_user_activation_state.h
@@ -8,21 +8,21 @@
 namespace blink {
 
 // Used to decide whether to allow web pages to prevent history traversal, and
-// to ensure they do not doing so twice in a row without an intervening user
+// to ensure they are not doing so twice in a row without an intervening user
 // activation.
 // This is used for a similar purpose to the history manipulation intervention
 // (src/docs/history_manipulation_intervention.md), but at a different
 // point in time. The intervention is used to make history entries skippable
-// when they are created, while HistoryUserActivationState is used to determine
-// whether the web page is allowed to block when traversing to an already
-// created history entry.
+// when they are navigated away from e.g. by creating a new entry, while
+// HistoryUserActivationState is used to determine whether the web page is
+// allowed to block when traversing to an already created history entry.
 // HistoryUserActivationState records activation at the same time as
 // UserActivationState, but consume behaves differently.
-// ConsumeHistoryUserActivation() only affects the target window, not
-// descendants, and HistoryUserActivationState consume is only triggered by
-// specific APIs that block history traversals. Therefore there will be cases
-// where HasHistoryUserActivation() is true but UserActivationState::IsActive()
-// is false, and vice versa.
+// `HistoryUserActivationState::Consume()` is only called on the target window,
+// not descendants, and is only triggered by specific APIs that block history
+// traversals. Therefore there will be cases where
+// `HistoryUserActivationState::IsActive()` is true but
+// `UserActivationState::IsActive()` is false, and vice versa.
 class HistoryUserActivationState {
  public:
   HistoryUserActivationState() = default;
