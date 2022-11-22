e0bfa586b5b49387f01e5b81f2182d5dd8b65bff
Santiago Castano Moreno <scmoreno@google.com>
2022-11-22 21:01:46 +0000

    Reporting: Added croshealthd bus sampler handler.
    (3/9) Continuation of Refactoring cros_healthd_metric_sampler. Added the
    bus category result handler.
    
    Bug: b:255631907
    
    Change-Id: I3b8bbbad05aca2d526d671e31a78846b69803ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4044061
    Reviewed-by: Alberto Juarez <albertojuarez@google.com>
    Commit-Queue: Santiago Castano <scmoreno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074846}

--- chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.cc
@@ -3,9 +3,10 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.h"
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_audio_sampler_handler.h"
 
 #include "base/logging.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_audio_sampler_handler.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_bus_sampler_handler.h"
 #include "chromeos/ash/services/cros_healthd/public/cpp/service_connection.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -14,26 +15,6 @@ namespace reporting {
 namespace {
 
 namespace cros_healthd = ::ash::cros_healthd::mojom;
-
-ThunderboltSecurityLevel TranslateThunderboltSecurityLevel(
-    cros_healthd::ThunderboltSecurityLevel security_level) {
-  switch (security_level) {
-    case cros_healthd::ThunderboltSecurityLevel::kNone:
-      return THUNDERBOLT_SECURITY_NONE_LEVEL;
-    case cros_healthd::ThunderboltSecurityLevel::kUserLevel:
-      return THUNDERBOLT_SECURITY_USER_LEVEL;
-    case cros_healthd::ThunderboltSecurityLevel::kSecureLevel:
-      return THUNDERBOLT_SECURITY_SECURE_LEVEL;
-    case cros_healthd::ThunderboltSecurityLevel::kDpOnlyLevel:
-      return THUNDERBOLT_SECURITY_DP_ONLY_LEVEL;
-    case cros_healthd::ThunderboltSecurityLevel::kUsbOnlyLevel:
-      return THUNDERBOLT_SECURITY_USB_ONLY_LEVEL;
-    case cros_healthd::ThunderboltSecurityLevel::kNoPcieLevel:
-      return THUNDERBOLT_SECURITY_NO_PCIE_LEVEL;
-  }
-
-  NOTREACHED();
-}
 
 MemoryEncryptionState TranslateMemoryEncryptionState(
     cros_healthd::EncryptionState encryption_state) {
@@ -63,72 +44,6 @@ MemoryEncryptionAlgorithm TranslateMemoryEncryptionAlgorithm(
   }
 
   NOTREACHED();
-}
-
-void HandleBusResult(OptionalMetricCallback callback,
-                     CrosHealthdMetricSampler::MetricType metric_type,
-                     cros_healthd::TelemetryInfoPtr result) {
-  absl::optional<MetricData> metric_data;
-  const auto& bus_result = result->bus_result;
-
-  if (!bus_result.is_null()) {
-    switch (bus_result->which()) {
-      case cros_healthd::BusResult::Tag::kError: {
-        DVLOG(1) << "cros_healthd: Error getting bus info: "
-                 << bus_result->get_error()->msg;
-        break;
-      }
-
-      case cros_healthd::BusResult::Tag::kBusDevices: {
-        for (const auto& bus_device : bus_result->get_bus_devices()) {
-          const auto& bus_info = bus_device->bus_info;
-          if (metric_type == CrosHealthdMetricSampler::MetricType::kInfo) {
-            if (bus_info->is_thunderbolt_bus_info()) {
-              if (!metric_data.has_value()) {
-                metric_data = absl::make_optional<MetricData>();
-              }
-              auto* const thunderbolt_info_out =
-                  metric_data->mutable_info_data()
-                      ->mutable_bus_device_info()
-                      ->add_thunderbolt_info();
-              thunderbolt_info_out->set_security_level(
-                  TranslateThunderboltSecurityLevel(
-                      bus_info->get_thunderbolt_bus_info()->security_level));
-            }
-          } else if (metric_type ==
-                     CrosHealthdMetricSampler::MetricType::kTelemetry) {
-            if (bus_info->is_usb_bus_info()) {
-              if (!metric_data.has_value()) {
-                metric_data = absl::make_optional<MetricData>();
-              }
-              auto* const usb_telemetry_out =
-                  metric_data->mutable_telemetry_data()
-                      ->mutable_peripherals_telemetry()
-                      ->add_usb_telemetry();
-              usb_telemetry_out->set_vid(
-                  bus_info->get_usb_bus_info()->vendor_id);
-              usb_telemetry_out->set_pid(
-                  bus_info->get_usb_bus_info()->product_id);
-              usb_telemetry_out->set_class_id(
-                  bus_info->get_usb_bus_info()->class_id);
-              usb_telemetry_out->set_subclass_id(
-                  bus_info->get_usb_bus_info()->subclass_id);
-              usb_telemetry_out->set_vendor(bus_device->vendor_name);
-              usb_telemetry_out->set_name(bus_device->product_name);
-              if (bus_info->get_usb_bus_info()->fwupd_firmware_version_info) {
-                usb_telemetry_out->set_firmware_version(
-                    bus_info->get_usb_bus_info()
-                        ->fwupd_firmware_version_info->version);
-              }
-            }
-          }
-        }
-        break;
-      }
-    }
-  }
-
-  std::move(callback).Run(std::move(metric_data));
 }
 
 void HandleCpuResult(OptionalMetricCallback callback,
@@ -497,7 +412,8 @@ void OnHealthdInfoReceived(OptionalMetricCallback callback,
       break;
     }
     case cros_healthd::ProbeCategoryEnum::kBus: {
-      HandleBusResult(std::move(callback), metric_type, std::move(result));
+      CrosHealthdBusSamplerHandler handler = CrosHealthdBusSamplerHandler(metric_type);
+      handler.HandleResult(std::move(result), std::move(callback));
       break;
     }
     case cros_healthd::ProbeCategoryEnum::kCpu: {
