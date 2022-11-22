f475ed226218f76c71549a0a23ea9ebeafca4f73
Thiago Perrotta <tperrotta@chromium.org>
2022-11-22 16:35:47 +0000

    tools/is_flaky.py: modernize from python2 to python3
    Bug: 1239351, 941669
    Change-Id: Ie86bebd2c2555741a7a9bafedc2dc3ca794e4ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040223
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074686}

--- tools/flakiness/is_flaky.py
+++ tools/flakiness/is_flaky.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2014 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 """Runs a test repeatedly to measure its flakiness. The return code is non-zero
 if the failure rate is higher than the specified threshold, but is not 100%."""
-
-from __future__ import print_function
 
 import argparse
 import multiprocessing.dummy
