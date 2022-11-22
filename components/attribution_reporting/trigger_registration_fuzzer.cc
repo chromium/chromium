17f597909e5dbdaeeee0d8919df9dd45a4be442b
Nan Lin <linnan@chromium.org>
2022-11-22 02:21:44 +0000

    Add fuzzer for trigger registration
    Bug: 1382538
    Change-Id: I2b39fbf6e822bf674a588c8683695b09a78dab02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4030663
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1074415}

---
+++ components/attribution_reporting/trigger_registration_fuzzer.cc
@@ -0,0 +1,58 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <utility>
+
+#include "base/command_line.h"
+#include "base/i18n/icu_util.h"
+#include "base/json/json_reader.h"
+#include "base/logging.h"
+#include "base/values.h"
+#include "components/attribution_reporting/suitable_origin.h"
+#include "components/attribution_reporting/trigger_registration.h"
+#include "testing/libfuzzer/proto/json.pb.h"
+#include "testing/libfuzzer/proto/json_proto_converter.h"
+#include "testing/libfuzzer/proto/lpm_interface.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace attribution_reporting {
+
+namespace {
+
+struct Environment {
+  Environment() {
+    base::CommandLine::Init(0, nullptr);
+    base::i18n::InitializeICU();
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+  }
+};
+
+}  // namespace
+
+DEFINE_PROTO_FUZZER(const json_proto::JsonValue& json_value) {
+  static Environment env;
+
+  json_proto::JsonProtoConverter converter;
+  std::string native_input = converter.Convert(json_value);
+
+  if (getenv("LPM_DUMP_NATIVE_INPUT"))
+    std::cout << native_input << std::endl;
+
+  absl::optional<base::Value> input = base::JSONReader::Read(
+      native_input, base::JSONParserOptions::JSON_PARSE_RFC);
+  if (!input || !input->is_dict())
+    return;
+
+  std::ignore = TriggerRegistration::Parse(
+      std::move(*input).TakeDict(),
+      /*reporting_origin=*/
+      *SuitableOrigin::Deserialize("https://r.test/"));
+}
+
+}  // namespace attribution_reporting
