d1f13790eeade39bc82b75d3fd4307094806dd2a
Gabriel Oliveira <gabolvr@google.com>
2022-11-22 16:57:34 +0000

    [Privacy Sandbox] Create the layout for ad privacy settings page
    Preview: https://crbug.com/1378703#c55
    
    Bug: 1378703, b/254413936
    Change-Id: Ie773cf34cb431afbdbb2f039afd8098e1b80688d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047861
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074699}

--- chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_page.ts
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import '../../settings_shared.css.js';
+
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './privacy_sandbox_page.html.js';
@@ -14,6 +17,30 @@ export class SettingsPrivacySandboxPageElement extends PolymerElement {
   static get template() {
     return getTemplate();
   }
+
+  private computePrivacySandboxTopicsSublabel_(): string {
+    // TODO(b/254412639): Change sublabel based on the respective toggle being
+    // enabled.
+    return 'Enabled Nulla eros tortor, placerat blandit dictum a, interdum id metus';
+  }
+
+  private computePrivacySandboxFledgeSublabel_(): string {
+    // TODO(b/254410792): Change sublabel based on the respective toggle being
+    // enabled.
+    return 'Enabled Duis scelerisque a mi eget ultricies';
+  }
+
+  private computePrivacySandboxMeasurementSublabel_(): string {
+    // TODO(b/254412652): Change sublabel based on the respective toggle being
+    // enabled.
+    return 'Enabled Vivamus id lacus et lacus porttitor vulputate. Sed semper egestas orci vel maximus.';
+  }
+
+  private onPrivacySandboxTopicsClick_() {}
+
+  private onPrivacySandboxFledgeClick_() {}
+
+  private onPrivacySandboxMeasurementClick_() {}
 }
 
 declare global {
