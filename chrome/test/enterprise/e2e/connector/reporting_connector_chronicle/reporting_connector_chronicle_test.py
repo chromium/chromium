386e8a0c8cd6a997c784f7967aac54ffa1c2e1a4
Meyron Levanon <meyron@google.com>
2022-11-22 00:16:42 +0000

    Added Chronicle reporting connector 2E2 test
    Added an E2E test that verifies events are being sent to a Chronicle instance
    via the reporting connector feature
    
    b: 241008617
    Change-Id: I12c58e73e47793606681df847d7ff940c634b340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3998013
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Commit-Queue: Meyron Levanon <meyron@google.com>
    Cr-Commit-Position: refs/heads/main@{#1074359}

---
+++ chrome/test/enterprise/e2e/connector/reporting_connector_chronicle/reporting_connector_chronicle_test.py
@@ -0,0 +1,72 @@
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import re
+import time
+from datetime import datetime
+
+from chrome_ent_test.infra.core import before_all
+from chrome_ent_test.infra.core import category
+from chrome_ent_test.infra.core import environment
+from chrome_ent_test.infra.core import test
+from infra import ChromeEnterpriseTestCase
+from .chronicle_api_service import ChronicleApiService
+
+
+@category("chrome_only")
+@environment(file="../connector_test.asset.textpb")
+class ReportingConnectorwithChronicleTest(ChromeEnterpriseTestCase):
+  """Test the Realtime Reporting pipeline events"""
+
+  def getCredentials(self):
+    path = "gs://%s/secrets/chronicleCredentials.json" % self.gsbucket
+    cmd = r'gsutil cat ' + path
+    return self.RunCommand(self.win_config['dc'], cmd).rstrip().decode()
+
+  @before_all
+  def setup(self):
+    self.InstallChrome(self.win_config['client'])
+    self.EnableUITest(self.win_config['client'])
+
+  @test
+  def test_browser_enrolled_prod(self):
+    eventFound = False
+    path = "gs://%s/secrets/CELabOrg-enrollToken" % self.gsbucket
+    cmd = r'gsutil cat ' + path
+    token = self.RunCommand(self.win_config['dc'], cmd).rstrip().decode()
+    self.SetPolicy(self.win_config['dc'], r'CloudManagementEnrollmentToken',
+                   token, 'String')
+
+    self.SetPolicy(self.win_config['dc'], r'SafeBrowsingEnabled', 1, 'DWORD')
+    self.RunCommand(self.win_config['client'], 'gpupdate /force')
+    testStartTime = datetime.utcnow()
+    # trigger malware event & get device id from browser
+    localDir = os.path.dirname(os.path.abspath(__file__))
+    commonDir = os.path.dirname(localDir)
+    clientId = self.RunUITest(
+        self.win_config['client'],
+        os.path.join(commonDir, 'common', 'realtime_reporting_ui_test.py'),
+        timeout=600)
+    clientId = re.search(r'DeviceId:.*$',
+                         clientId.strip()).group(0).replace('DeviceId:',
+                                                            '').rstrip("\\rn'")
+
+    # wait until events are logged in the connector
+    max_wait_time_secs = 360
+    delta_secs = 10
+    total_wait_time_secs = 0
+    crendentials = self.getCredentials()
+
+    while total_wait_time_secs < max_wait_time_secs:
+      chronicleService = ChronicleApiService(crendentials)
+      chronicleService.loadEvents(
+          testStartTime=testStartTime, deviceId=clientId)
+      if chronicleService.doesEventExist(
+          eventName='badNavigationEvent', deviceId=clientId):
+        return
+      time.sleep(delta_secs)
+      total_wait_time_secs += delta_secs
+
+    raise Exception('Timed out ' 'badNavigationEvent event not found')
