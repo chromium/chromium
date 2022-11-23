b7dff48457da5e6efb141e9dfa3343b869d77744
Danil Somsikov <dsv@chromium.org>
2022-11-23 13:38:45 +0000

    Update layout tests in devtools/service-work to work with tab target.
    Bug: 1370050
    Change-Id: Ief32a559b451a049a19eba1a9df15558bd9e3167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051160
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075126}

--- third_party/blink/web_tests/http/tests/devtools/service-workers/service-worker-agents.js
+++ third_party/blink/web_tests/http/tests/devtools/service-workers/service-worker-agents.js
@@ -15,7 +15,7 @@
 
   TestRunner.addSniffer(SDK.MainConnection.prototype, 'sendRawMessage', function(messageString) {
     var message = JSON.parse(messageString);
-    if (!message.sessionId)
+    if (!message.sessionId || message.sessionId === SDK.targetManager.mainFrameTarget().sessionId)
       return;
     if (messageString.includes('DOM.'))
       TestRunner.addResult('DOM-related command should NOT be issued: ' + messageString);
