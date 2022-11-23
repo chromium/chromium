095075e0963b26483f91e6eda3d526354498c10d
Andrés Olivares <andoli@chromium.org>
2022-11-23 14:47:04 +0000

    Add tests for interactions trace events
    Events are dispatched with the name "EventTiming" and enable the
    Interactions track in DevTools Perf panel.
    
    The test triggers a keyboard interaction and waits for the
    PerformanceEventTiming API to report the corresponding entries in order
    to collect the matching trace events that will be tested.
    
    Bug: 1383130
    Change-Id: Ia431009316663e4e61a840abd193254a8cb1751f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4047407
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075149}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/interactions.js
@@ -0,0 +1,38 @@
+(async function(testRunner) {
+  const {session, dp} =
+      await testRunner.startBlank('Tests the data of EventTiming trace events');
+
+  const TracingHelper =
+      await testRunner.loadScript('../resources/tracing-test.js');
+  const tracingHelper = new TracingHelper(testRunner, session);
+
+  await tracingHelper.startTracing('devtools.timeline');
+
+  await dp.Page.navigate({
+    url: 'http://127.0.0.1:8000/inspector-protocol/resources/interactions.html'
+  });
+
+  await session.evaluate('injectInputEventListener()');
+
+  // Dispatch a keyboard interaction.
+  await dp.Input.dispatchKeyEvent({type: 'keyDown', key: 'A'});
+  await dp.Input.dispatchKeyEvent({type: 'keyUp', key: 'A'});
+
+  // Wait for trace events.
+  await session.evaluateAsync(`window.__interactionPromise`);
+
+  const devtoolsEvents = await tracingHelper.stopTracing(/devtools\.timeline/);
+  const eventTimingTraces =
+      devtoolsEvents.filter(event => event.name === 'EventTiming');
+  const keyUpBeginEvent =
+      eventTimingTraces.find(event => event.args?.data?.type === 'keydown');
+  const keyUpEndEvent = eventTimingTraces.find(
+      event => event.id === keyUpBeginEvent.id && !event.args.data);
+  testRunner.log(`Got EventTiming begin event for keydown event with phase ${
+      keyUpBeginEvent.ph}:`);
+  tracingHelper.logEventShape(keyUpBeginEvent);
+  testRunner.log(`Got EventTiming end event for keydown event with phase ${
+      keyUpEndEvent.ph}:`);
+  tracingHelper.logEventShape(keyUpEndEvent);
+  testRunner.completeTest();
+})
