272c037d9e40f966c4bcf4f92c3c02b2414c2d56
Palak Agarwal <agpalak@chromium.org>
2022-11-23 15:44:43 +0000

    Define class ScriptPromiseResultTracker for metric tracking
    As part of the effort to improve metric and latency tracking of Chrome
    RTC APIs, we have introduced the class ScriptPromiseResultTracker which wraps around ScriptPromiseResolver.
    
    In the follow-up CLs, we would replace the usage of ScriptPromiseResolver with ScriptPromiseResultTracker for our APIs of interest.
    
    Bug: 1373398
    Change-Id: Ifa91ff641da1c703ec0f701a743c867af0203591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031635
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075160}

---
+++ third_party/blink/renderer/bindings/core/v8/script_promise_result_tracker.h
@@ -0,0 +1,115 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCRIPT_PROMISE_RESULT_TRACKER_H_
+#define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCRIPT_PROMISE_RESULT_TRACKER_H_
+
+#include "base/metrics/histogram_functions.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+
+namespace blink {
+
+// ScriptPromiseResultTracker is a wrapper around ScriptPromiseResolver which
+// simplifies recording UMA metric and latency for APIs.
+
+// Callers should ensure that the ResultEnumType has kOk and kTimedOut as
+// values.
+template <typename ResultEnumType>
+class CORE_EXPORT ScriptPromiseResultTracker
+    : public GarbageCollected<ScriptPromiseResultTracker<ResultEnumType>> {
+ public:
+  // If the targeted histograms are "WebRTC.EnumerateDevices.Result" and
+  // "WebRTC.EnumerateDevices.Latency", the input to |metric_name_prefix| should
+  // be "WebRTC.EnumerateDevices".
+  //
+  // |timeout_interval| is the timeout limit after which a
+  // ResultEnumType::kTimedOut response is recorded in the Result histogram.
+  //
+  // This creates/accesses the Latency histogram which has |n_buckets_| buckets
+  // and the range of the buckets are from (min_latency_bucket,
+  // max_latency_bucket).
+  ScriptPromiseResultTracker(
+      ScriptState* script_state,
+      std::string metric_name_prefix,
+      base::TimeDelta timeout_interval,
+      base::TimeDelta min_latency_bucket = base::Milliseconds(1),
+      base::TimeDelta max_latency_bucket = base::Seconds(10),
+      size_t n_buckets = 50)
+      : metric_name_prefix_(std::move(metric_name_prefix)),
+        start_time_(base::TimeTicks::Now()),
+        timeout_interval_(timeout_interval),
+        min_latency_bucket_(min_latency_bucket),
+        max_latency_bucket_(max_latency_bucket),
+        n_buckets_(n_buckets) {
+    CHECK(!metric_name_prefix_.empty());
+    resolver_ = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+    if (timeout_interval.is_positive()) {
+      ExecutionContext::From(script_state)
+          ->GetTaskRunner(TaskType::kInternalDefault)
+          ->PostDelayedTask(
+              FROM_HERE,
+              WTF::BindOnce(&ScriptPromiseResultTracker::RecordResult,
+                            WrapPersistent(this), ResultEnumType::kTimedOut),
+              timeout_interval);
+    }
+  }
+  ScriptPromiseResultTracker(const ScriptPromiseResultTracker&) = delete;
+  ScriptPromiseResultTracker& operator=(const ScriptPromiseResultTracker&) =
+      delete;
+  ~ScriptPromiseResultTracker() = default;
+
+  template <typename T>
+  void Resolve(T value, ResultEnumType result = ResultEnumType::kOk) {
+    RecordResult(result);
+    RecordLatency();
+    resolver_->Resolve(value);
+  }
+
+  template <typename T>
+  void Reject(T value, ResultEnumType result) {
+    RecordResult(result);
+    RecordLatency();
+    resolver_->Reject(value);
+  }
+
+  void RecordResult(ResultEnumType result) {
+    if (is_result_recorded_)
+      return;
+
+    is_result_recorded_ = true;
+    base::UmaHistogramEnumeration(metric_name_prefix_ + ".Result", result);
+  }
+
+  void RecordLatency() {
+    if (is_latency_recorded_)
+      return;
+
+    is_latency_recorded_ = true;
+    const base::TimeDelta elapsed = base::TimeTicks::Now() - start_time_;
+    base::UmaHistogramCustomTimes(metric_name_prefix_ + ".Latency", elapsed,
+                                  min_latency_bucket_, max_latency_bucket_,
+                                  n_buckets_);
+  }
+
+  ScriptState* GetScriptState() const { return resolver_->GetScriptState(); }
+
+  ScriptPromise Promise() { return resolver_->Promise(); }
+
+  void Trace(Visitor* visitor) const { visitor->Trace(resolver_); }
+
+ private:
+  Member<ScriptPromiseResolver> resolver_;
+  std::string metric_name_prefix_;
+  base::TimeTicks start_time_;
+  base::TimeDelta timeout_interval_;
+  base::TimeDelta min_latency_bucket_;
+  base::TimeDelta max_latency_bucket_;
+  size_t n_buckets_;
+  bool is_latency_recorded_ = false;
+  bool is_result_recorded_ = false;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCRIPT_PROMISE_RESULT_TRACKER_H_
