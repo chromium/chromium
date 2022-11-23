129f6d9a12b63914f3f1172f4ee75872d2813c7c
Yoshifuim Inoue <yosin@chromium.org>
2022-11-23 04:53:49 +0000

    [editing] (4/4) Make ComparePositions() faster
    This patch is the fourth and last part of a series of patches to make
    `ComparePositions()` faster.
    
    This patch makes `Comparator` class to handle both absolute position and
    relative position for making "SelectAll" command faster.
    
    Note: Absolute positions are positions with container node and integer
    offset, e.g. container@k. Relative positions are positions with container
    and `Node*` before positions. For example:
      `<div>... N children ...</div>`
     * `<div>@0` == `<div>+nullptr`
     * `<div>@1` == `<div>+FirstChild(<div>)`
     * `<div>@2` == `<div>+ChildAT(<div>, 2)`
     * ...
     * `<div>@N` == `<div>@afterChildren` == `<div>+LastChild(<div>)`
    
    Note: When using node before position, we can not get parent node in
    `CompareNodesInSameParent()` by `Traversal::Parent(*child_b)`.
    Note: `RangeBoundaryPoint` has `child_before_boundary_` member.
    
    `SelectAll` command makes selection to
        `{container@0, container@afterChildren}`,
    before this patch `ComparePositions()` counts the number of children in
    `container`. After this patch, we don't need it.
    
    The perf_test[1] results on local machine are:
      Before: 1244.305ms (before `FastPositionIterator`)
      Before: 796.040ms (after `FastPositionIterator`)
      After:  625.475ms
    It is 1.273  times faster than before.
    
    This is done by introducing:
    `CompareNodesInSameParent(int offset_a, int offset_b)`
    `CompareNodesInSameParent(int offset_a, const Node* child_b)`
    Change to take a node before position instead of node after position
    `CompareNodesInSameParent(const Node* child_a, int offset_b)`
    and a dispatcher function with template help classes `IntOffset` and
    `NodeOffset`.
    
    [1] third_party/blink/perf_tests/editing/many_spans_with_move_select_all.html
    
    The series is
      1. https://crrev.com/c/4039247 Move to "compare_positions.cc"
      2. https://crrev.com/c/4040525 Move logic to functions
      3. https://crrev.com/c/4040521 CompareNodesInSameParent faster
      4. https://crrev.com/c/4040842 Relative positions faster
    
    Bug: 1323777
    Change-Id: I2758d527919906b98f5cbe60ff2011b768f19fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040842
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075038}

--- third_party/blink/renderer/core/editing/compare_positions.cc
+++ third_party/blink/renderer/core/editing/compare_positions.cc
@@ -32,6 +32,8 @@ namespace blink {
 
 namespace {
 
+constexpr int kInvalidOffset = -1;
+
 template <typename Traversal>
 int16_t SlowComparePositions(const Node* container_a,
                              int offset_a,
@@ -136,6 +138,8 @@ class Comparator {
   STATIC_ONLY(Comparator);
 
  public:
+  // Integer offset version of `ComparePositions()`.
+  //
   // Returns
   //  -1 if `node_a` is before `node_b`
   //   0 if `node_a == node_b`
@@ -150,6 +154,98 @@ class Comparator {
                                   const Node* container_b,
                                   int offset_b,
                                   bool* disconnected) {
+    return ComparePositionsInternal(container_a, IntAsOffset(offset_a),
+                                    container_b, IntAsOffset(offset_b),
+                                    disconnected);
+  }
+
+  // Integer/Node offset version of `ComparePositions()`.
+  //
+  // Returns
+  //  -1 if `node_a` is before `node_b`
+  //   0 if `node_a == node_b`
+  //   1 if `node_a` is after `node_b`
+  //    where
+  //      * `node_a == Traversal::ChildAt(*container_a, offset_a)`
+  //      * `node_b == Traversal::ChildAt(*container_a, offset_b)`
+  // and set `disconnected` to true if `node_a` and `node_b` are in different
+  // tree scopes.
+  static int16_t ComparePositions(const Node* container_a,
+                                  int offset_a,
+                                  const Node* child_a,
+                                  const Node* container_b,
+                                  int offset_b,
+                                  const Node* child_b,
+                                  bool* disconnected = nullptr) {
+    if (offset_a == kInvalidOffset && offset_b == kInvalidOffset) {
+      return ComparePositionsInternal(container_a, NodeAsOffset(child_a),
+                                      container_b, NodeAsOffset(child_b),
+                                      disconnected);
+    }
+
+    if (offset_a == kInvalidOffset) {
+      return ComparePositionsInternal(container_a, NodeAsOffset(child_a),
+                                      container_b, IntAsOffset(offset_b),
+                                      disconnected);
+    }
+
+    if (offset_b == kInvalidOffset) {
+      return ComparePositionsInternal(container_a, IntAsOffset(offset_a),
+                                      container_b, NodeAsOffset(child_b),
+                                      disconnected);
+    }
+
+    return ComparePositionsInternal(container_a, IntAsOffset(offset_a),
+                                    container_b, IntAsOffset(offset_b),
+                                    disconnected);
+  }
+
+ private:
+  enum Result : int16_t {
+    kAIsBeforeB = -1,
+    kAIsEqualToB = 0,
+    kAIsAfterB = 1,
+  };
+
+  // The wrapper class of `int` offset.
+  class IntAsOffset {
+    STACK_ALLOCATED();
+
+   public:
+    explicit IntAsOffset(int value) : value_(value) {}
+    int Get() const { return value_; }
+
+   private:
+    int value_;
+  };
+
+  // The wrapper class of offset in `Node*` before position.
+  class NodeAsOffset {
+    STACK_ALLOCATED();
+
+   public:
+    explicit NodeAsOffset(const Node* value) : value_(value) {}
+    const Node* Get() const { return value_; }
+
+   private:
+    const Node* value_;
+  };
+
+  // Returns
+  //  -1 if `child_a` is before `child_b`
+  //   0 if `child_a == child_b`
+  //   1 if `child_a` is after `child_b`
+  //    where
+  //      * `child_a == Traversal::ChildAt(*container_a, offset_a)`
+  //      * `child_b == Traversal::ChildAt(*container_a, offset_b)`
+  // and set `disconnected` to true if `child_a` and `child_b` are in different
+  // tree scopes.
+  template <typename OffsetA, typename OffsetB>
+  static int16_t ComparePositionsInternal(const Node* container_a,
+                                          OffsetA offset_a,
+                                          const Node* container_b,
+                                          OffsetB offset_b,
+                                          bool* disconnected) {
     DCHECK(container_a);
     DCHECK(container_b);
 
@@ -164,13 +260,8 @@ class Comparator {
     // see DOM2 traversal & range section 2.5
 
     // Case 1: both points have the same container
-    if (container_a == container_b) {
-      if (offset_a == offset_b)
-        return kAIsEqualToB;
-      if (offset_a < offset_b)
-        return kAIsBeforeB;
-      return kAIsAfterB;
-    }
+    if (container_a == container_b)
+      return CompareNodesInSameParent(offset_a.Get(), offset_b.Get());
 
     // Case 2: node C (container B or an ancestor) is a child node of A, e.g.
     //  * A < B
@@ -180,8 +271,11 @@ class Comparator {
     //  * A == C2
     //             A
     //      `<a>...<c2>...<b>...B...</b>...</c2>...</a>`
-    if (const Node* node_c2 = FindChildnInAncestors(*container_b, *container_a))
-      return CompareNodesInSameParent(offset_a, node_c2, kAIsBeforeB);
+    if (const Node* node_c2 =
+            FindChildnInAncestors(*container_b, *container_a)) {
+      return CompareNodesInSameParent(
+          offset_a.Get(), Traversal::PreviousSibling(*node_c2), kAIsBeforeB);
+    }
 
     // Case 3: node C (container A or an ancestor) is a child node of B, e.g.
     //  * B < A
@@ -191,8 +285,11 @@ class Comparator {
     //  * B == C3
     //             B
     //      `<b>...<c3>...<a>...A...</a>...</b>`
-    if (const Node* node_c3 = FindChildnInAncestors(*container_a, *container_b))
-      return -CompareNodesInSameParent(offset_b, node_c3, kAIsBeforeB);
+    if (const Node* node_c3 =
+            FindChildnInAncestors(*container_a, *container_b)) {
+      return -CompareNodesInSameParent(
+          offset_b.Get(), Traversal::PreviousSibling(*node_c3), kAIsBeforeB);
+    }
 
     // case 4: containers A & B are siblings, or children of siblings
     // ### we need to do a traversal here instead
@@ -215,36 +312,50 @@ class Comparator {
     return CompareNodesInSameParent(adjusted_child_a, adjusted_child_b);
   }
 
- private:
-  enum Result : int16_t {
-    kAIsBeforeB = -1,
-    kAIsEqualToB = 0,
-    kAIsAfterB = 1,
-  };
-
-  // Returns where `offset_b =  Traversal::Index(*child_b)`:
+  // Returns
   //  -1 if `offset_a < offset_b`
   //   0 if `offset_a == offset_b`
   //   1 if `offset_a > offset_b`
+  //     where ```
+  //        offset_b =  child_before_position_b
+  //            ? Traversal::Index(*child_before_position_b) + 1
+  //            : 0 ```
   // The number of iteration is `std::min(offset_a, offset_b)`.
   static Result CompareNodesInSameParent(
       int offset_a,
-      const Node* child_b,
+      const Node* child_before_position_b,
       Result result_of_a_is_equal_to_b = kAIsEqualToB) {
-    DCHECK(child_b);
-    int offset_b = 0;
-    for (const Node& child_a :
-         Traversal::ChildrenOf(*Traversal::Parent(*child_b))) {
-      if (child_a == child_b || offset_a == offset_b) {
-        const int diff = offset_a - offset_b;
-        return !diff      ? result_of_a_is_equal_to_b
-               : diff > 0 ? kAIsAfterB
-                          : kAIsBeforeB;
-      }
-      ++offset_b;
+    if (!child_before_position_b)
+      return !offset_a ? result_of_a_is_equal_to_b : kAIsAfterB;
+    if (!offset_a)
+      return kAIsBeforeB;
+    // Starts from offset 1 and after `child_before_position_b`.
+    const Node& child_b = *child_before_position_b;
+    int offset = 1;
+    for (const Node& child :
+         Traversal::ChildrenOf(*Traversal::Parent(child_b))) {
+      if (offset_a == offset)
+        return child == child_b ? result_of_a_is_equal_to_b : kAIsBeforeB;
+      if (child == child_b)
+        return kAIsAfterB;
+      ++offset;
     }
     NOTREACHED();
     return result_of_a_is_equal_to_b;
+  }
+
+  static int16_t CompareNodesInSameParent(
+      int offset_a,
+      int offset_b,
+      Result result_of_a_is_equal_to_b = kAIsEqualToB) {
+    if (offset_a == offset_b)
+      return result_of_a_is_equal_to_b;
+    return offset_a < offset_b ? kAIsBeforeB : kAIsAfterB;
+  }
+
+  static int16_t CompareNodesInSameParent(const Node* child_before_position_a,
+                                          int offset_b) {
+    return -CompareNodesInSameParent(offset_b, child_before_position_a);
   }
 
   // Returns
@@ -276,14 +387,19 @@ class Comparator {
   //    `offset_b` == `Traversal::Index(*child_b)`
   //
   //  ```
+  // Note: We call this function both "node before position" and "node after
+  // position" cases. For "node after position" case, `child_a` and `child_b`
+  // should not be `nullptr`.
   static int16_t CompareNodesInSameParent(
       const Node* child_a,
       const Node* child_b,
       Result result_of_a_is_equal_to_b = kAIsEqualToB) {
-    DCHECK(child_a);
-    DCHECK(child_b);
     if (child_a == child_b)
       return result_of_a_is_equal_to_b;
+    if (!child_a)
+      return kAIsBeforeB;
+    if (!child_b)
+      return kAIsAfterB;
     DCHECK_EQ(Traversal::Parent(*child_a), Traversal::Parent(*child_b));
     const Node* backward_a = child_a;
     const Node* forward_a = child_a;
@@ -351,9 +467,10 @@ int16_t ComparePositionsInFlatTree(const Node* container_a,
       container_a, offset_a, container_b, offset_b, disconnected);
 }
 
+// Note: We should not this function outside this file.
 // Compare two positions, taking into account the possibility that one or both
 // could be inside a shadow tree. Only works for non-null values.
-int16_t ComparePositions(const Position& a, const Position& b) {
+int16_t SlowComparePositions(const Position& a, const Position& b) {
   DCHECK(a.IsNotNull());
   DCHECK(b.IsNotNull());
   const TreeScope* common_scope = Position::CommonAncestorTreeScope(a, b);
@@ -385,6 +502,57 @@ int16_t ComparePositions(const Position& a, const Position& b) {
   return result ? result : bias;
 }
 
+int16_t ComparePositions(const Position& position_a,
+                         const Position& position_b) {
+  if (!RuntimeEnabledFeatures::FastComparePositionsEnabled())
+    return SlowComparePositions(position_a, position_b);
+
+  DCHECK(position_a.IsNotNull());
+  DCHECK(position_b.IsNotNull());
+
+  const TreeScope* common_scope =
+      Position::CommonAncestorTreeScope(position_a, position_b);
+
+  DCHECK(common_scope);
+  if (!common_scope)
+    return 0;
+
+  Node* const container_a = position_a.ComputeContainerNode();
+  Node* const node_a = common_scope->AncestorInThisScope(container_a);
+  DCHECK(node_a);
+  const bool has_descendant_a = node_a != container_a;
+
+  Node* const container_b = position_b.ComputeContainerNode();
+  Node* const node_b = common_scope->AncestorInThisScope(container_b);
+  DCHECK(node_b);
+  const bool has_descendant_b = node_b != container_b;
+
+  const int offset_a = position_a.IsOffsetInAnchor() && !has_descendant_a
+                           ? position_a.OffsetInContainerNode()
+                           : kInvalidOffset;
+
+  const int offset_b = position_b.IsOffsetInAnchor() && !has_descendant_b
+                           ? position_b.OffsetInContainerNode()
+                           : kInvalidOffset;
+
+  Node* const child_a = position_a.IsOffsetInAnchor() || has_descendant_a
+                            ? nullptr
+                            : position_a.ComputeNodeBeforePosition();
+
+  Node* const child_b = position_b.IsOffsetInAnchor() || has_descendant_b
+                            ? nullptr
+                            : position_b.ComputeNodeBeforePosition();
+
+  const int16_t bias = node_a != node_b   ? 0
+                       : has_descendant_a ? 1
+                       : has_descendant_b ? -1
+                                          : 0;
+
+  const int16_t result = Comparator<NodeTraversal>::ComparePositions(
+      node_a, offset_a, child_a, node_b, offset_b, child_b);
+  return result ? result : bias;
+}
+
 int16_t ComparePositions(const PositionWithAffinity& a,
                          const PositionWithAffinity& b) {
   return ComparePositions(a.GetPosition(), b.GetPosition());
@@ -394,8 +562,9 @@ int16_t ComparePositions(const VisiblePosition& a, const VisiblePosition& b) {
   return ComparePositions(a.DeepEquivalent(), b.DeepEquivalent());
 }
 
-int16_t ComparePositions(const PositionInFlatTree& position_a,
-                         const PositionInFlatTree& position_b) {
+// Note: We should not this function outside this file.
+int16_t SlowComparePositions(const PositionInFlatTree& position_a,
+                             const PositionInFlatTree& position_b) {
   DCHECK(position_a.IsNotNull());
   DCHECK(position_b.IsNotNull());
 
@@ -405,6 +574,37 @@ int16_t ComparePositions(const PositionInFlatTree& position_a,
   int offset_b = position_b.ComputeOffsetInContainerNode();
   return ComparePositionsInFlatTree(container_a, offset_a, container_b,
                                     offset_b);
+}
+
+int16_t ComparePositions(const PositionInFlatTree& position_a,
+                         const PositionInFlatTree& position_b) {
+  DCHECK(position_a.IsNotNull());
+  DCHECK(position_b.IsNotNull());
+
+  if (!RuntimeEnabledFeatures::FastComparePositionsEnabled())
+    return SlowComparePositions(position_a, position_b);
+
+  Node* const container_a = position_a.ComputeContainerNode();
+  Node* const container_b = position_b.ComputeContainerNode();
+
+  const int offset_a = position_a.IsOffsetInAnchor()
+                           ? position_a.OffsetInContainerNode()
+                           : kInvalidOffset;
+
+  const int offset_b = position_b.IsOffsetInAnchor()
+                           ? position_b.OffsetInContainerNode()
+                           : kInvalidOffset;
+
+  Node* const child_a = position_a.IsOffsetInAnchor()
+                            ? nullptr
+                            : position_a.ComputeNodeBeforePosition();
+
+  Node* const child_b = position_b.IsOffsetInAnchor()
+                            ? nullptr
+                            : position_b.ComputeNodeBeforePosition();
+
+  return Comparator<FlatTreeTraversal>::ComparePositions(
+      container_a, offset_a, child_a, container_b, offset_b, child_b);
 }
 
 }  // namespace blink
