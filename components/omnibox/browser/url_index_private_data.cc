26c5b6d93edf0870a158bef971dc5cf83882abb3
manukh <manukh@chromium.org>
2022-11-24 01:41:35 +0000

    [omnibox][domain] Optimize counting unique hosts for HQP specificity.
    HQP demotes suggestions when many suggestions match the input. When the
    `kHistoryQuickProviderSpecificityScoreCountUniqueHosts` feature is
    enabled, it considers the # of unique hosts instead of # of suggestions.
    
    This had some latency regressions in stable and beta experiments
    docs.google.com/document/d/17dsyd-i7dTSfIsL8mD925FHSd9868Zf8iKyJiTH80RI
    
    To address the regression, this CL makes calculating the # of unique
    hosts more performant. Previously, HQP mapped the history IDs to their
    URL hosts, constructed a `base::flat_set` in 1 go, and looked at the
    set's size. The number of suggestions typically ranges 1-200.
    
    This CL:
    a) Uses `std::set` instead of a `flat_set`
    b) Constructs the set by insertion instead of in 1 go. This allows early
    exiting once we've found 4 unique hosts, since HQP gives the same
    demotion for 4 and beyond unique hosts anyways.
    
    Local timing indicated the latency went from 45 microseconds to 1.5
    microseconds. This is trivial regardless, but in case the regression in
    experiments was real, it's worth optimizing.
    
    Bug: 1356261
    Change-Id: I6c66e24bb186dde7dd021fcb410da8df0cafc026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051992
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075428}

--- components/omnibox/browser/url_index_private_data.cc
+++ components/omnibox/browser/url_index_private_data.cc
@@ -669,16 +669,26 @@ void URLIndexPrivateData::HistoryIdsToScoredMatches(
   // unique hosts in the matches instead.
   static bool count_unique_hosts = base::FeatureList::IsEnabled(
       omnibox::kHistoryQuickProviderSpecificityScoreCountUniqueHosts);
-  const size_t num_unique_hosts =
-      count_unique_hosts ? base::MakeFlatSet<std::string>(
-                               history_ids, {},
-                               [&](const auto history_id) {
-                                 return history_info_map_.find(history_id)
-                                     ->second.url_row.url()
-                                     .host();
-                               })
-                               .size()
-                         : history_ids.size();
+  size_t num_unique_hosts;
+  if (count_unique_hosts) {
+    std::set<std::string> unique_hosts = {};
+    for (const auto& history_id : history_ids) {
+      DCHECK(history_info_map_.count(history_id));
+      unique_hosts.insert(
+          history_info_map_.find(history_id)->second.url_row.url().host());
+      // `ScoredHistoryMatch` assigns the same specificity to suggestions for
+      // counts 4 or larger.
+      // TODO(manukh) Should share `kMaxUniqueHosts` with `ScoredHistoryMatch`,
+      //  but doing so is complicated as it's derived from parsing the default
+      //  string value for the finch param `kHQPNumMatchesScoresRule`.
+      constexpr size_t kMaxUniqueHosts = 4;
+      if (unique_hosts.size() >= kMaxUniqueHosts)
+        break;
+    }
+    num_unique_hosts = unique_hosts.size();
+  } else {
+    num_unique_hosts = history_ids.size();
+  }
 
   for (HistoryID history_id : history_ids) {
     auto hist_pos = history_info_map_.find(history_id);
