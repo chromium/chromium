c4b7f8be7091dee044f825d3280624795085f168
Ce Chen <cch@chromium.org>
2022-11-24 05:32:35 +0000

    [omnibox] add tokenizer for on device tail suggest.
    Bug: 1372112
    Change-Id: I8f7624d51ffb4eb0c0d2eec2d420fdca4f5db0d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4031017
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: C Ch <cch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1075477}

---
+++ components/omnibox/browser/on_device_tail_tokenizer.cc
@@ -0,0 +1,242 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/on_device_tail_tokenizer.h"
+
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
+#include "base/files/file_util.h"
+#include "base/logging.h"
+#include "base/strings/string_util.h"
+
+namespace {
+// Maximum vocabulary file size that will be loaded in bytes.
+static constexpr size_t kVocabFileSizeLimit = 64 * 1024;
+
+// The max num of single char tokens.
+static constexpr size_t kNumSingleChar = 256;
+
+// Special control tokens.
+static constexpr char kBeginQueryToken[] = "<Q>";
+static constexpr char kEndQueryToken[] = "</Q>";
+static constexpr char kEmptyPreviousQueryToken[] = "<NPQ>";
+static constexpr char kUnknownToken[] = "<UNK>";
+
+std::ostream& operator<<(std::ostream& os,
+                         const base::flat_set<std::string>& tokens) {
+  if (tokens.empty()) {
+    return os;
+  }
+
+  auto iter = tokens.begin();
+  os << *iter;
+  ++iter;
+
+  for (; iter != tokens.end(); iter++) {
+    os << ", " << *iter;
+  }
+  return os;
+}
+
+}  // namespace
+
+OnDeviceTailTokenization::OnDeviceTailTokenization() = default;
+
+OnDeviceTailTokenization::~OnDeviceTailTokenization() = default;
+
+OnDeviceTailTokenizer::OnDeviceTailTokenizer() = default;
+
+OnDeviceTailTokenizer::~OnDeviceTailTokenizer() = default;
+
+bool OnDeviceTailTokenizer::Init(const base::FilePath& vocabulary_filepath) {
+  std::string vocabulary_content;
+  if (!base::ReadFileToStringWithMaxSize(
+          vocabulary_filepath, &vocabulary_content, kVocabFileSizeLimit)) {
+    DVLOG(1) << "Failed to read the vocabulary file "
+             << vocabulary_filepath.LossyDisplayName();
+    return false;
+  }
+
+  base::flat_set<std::string> control_tokens = {
+      kBeginQueryToken, kEndQueryToken, kEmptyPreviousQueryToken,
+      kUnknownToken};
+
+  std::string token;
+  max_token_length_ = 0;
+
+  // The first 256 tokens are ASCII characters.
+  for (size_t i = 0; i < kNumSingleChar; i++) {
+    token = static_cast<char>(i);
+    InsertTokenToMaps(token);
+  }
+
+  std::stringstream vocabulary(vocabulary_content);
+  while (std::getline(vocabulary, token)) {
+    if (token.empty()) {
+      break;
+    }
+
+    // Duplicate tokens are not allowed.
+    if (token_to_id_.find(token) != token_to_id_.end()) {
+      Reset();
+      DVLOG(1) << "Duplicate token found: " << token;
+      return false;
+    }
+
+    InsertTokenToMaps(token);
+    if (control_tokens.find(token) != control_tokens.end()) {
+      control_tokens.erase(token);
+    } else {
+      max_token_length_ = std::max<size_t>(max_token_length_, token.size());
+    }
+  }
+
+  // A valid vocabulary should include all control tokens.
+  if (!control_tokens.empty()) {
+    Reset();
+    DVLOG(1) << "Missing following control tokens: " << control_tokens;
+    return false;
+  }
+
+  InitAmbiguousMap();
+
+  return IsReady();
+}
+
+bool OnDeviceTailTokenizer::IsReady() const {
+  return !token_to_id_.empty();
+}
+
+void OnDeviceTailTokenizer::Reset() {
+  token_to_id_.clear();
+  id_to_token_.clear();
+  ambiguous_tokens_.clear();
+}
+
+std::string OnDeviceTailTokenizer::IDToToken(const size_t token_id) const {
+  if (token_id < 0 || token_id >= id_to_token_.size()) {
+    return kUnknownToken;
+  }
+  return id_to_token_[token_id];
+}
+
+size_t OnDeviceTailTokenizer::TokenToID(const std::string& token) const {
+  auto match = token_to_id_.find(token);
+  if (match == token_to_id_.end()) {
+    // The ID for unknown token.
+    return token_to_id_.find(kUnknownToken)->second;
+  }
+  return match->second;
+}
+
+void OnDeviceTailTokenizer::InitAmbiguousMap() {
+  base::flat_map<std::string, size_t> prefix_count;
+  for (const std::string& token : id_to_token_) {
+    // Skip special tokens.
+    if (token[0] == '<') {
+      continue;
+    }
+
+    for (size_t len = 1; len <= token.size(); len++) {
+      prefix_count[token.substr(0, len)] += 1;
+    }
+  }
+
+  // Marks tokens as ambiguous if corresponding prefixes occur multiple times.
+  for (const auto& iter : prefix_count) {
+    if (iter.second > 1) {
+      ambiguous_tokens_.insert(iter.first);
+    }
+  }
+}
+
+bool OnDeviceTailTokenizer::IsAmbiguousToken(const std::string& token) const {
+  return ambiguous_tokens_.find(token) != ambiguous_tokens_.end();
+}
+
+void OnDeviceTailTokenizer::EncodeRawString(
+    const std::string& raw_string,
+    std::vector<std::pair<std::string, size_t>>* token_and_ids) const {
+  size_t i = 0;
+  while (i < raw_string.size()) {
+    // Tries longest possible matches first and reduces the length gradually
+    // until a match is found.
+    size_t len = std::min<size_t>(max_token_length_, raw_string.size() - i);
+    while (len >= 1) {
+      auto iter = token_to_id_.find(raw_string.substr(i, len));
+      if (iter != token_to_id_.end()) {
+        token_and_ids->push_back({iter->first, iter->second});
+        i += len;
+        break;
+      }
+      len--;
+    }
+
+    // Unknown token is found.
+    if (len == 0) {
+      DVLOG(1) << "Invalid token found for raw string: " << raw_string;
+      token_and_ids->clear();
+      return;
+    }
+  }
+}
+
+void OnDeviceTailTokenizer::TokenizePrevQuery(
+    const std::string& prev_query,
+    std::vector<size_t>* prev_query_token_ids) const {
+  prev_query_token_ids->clear();
+
+  if (prev_query.empty()) {
+    // Uses the special control token <NPQ> to mark empty previous query.
+    prev_query_token_ids->push_back(TokenToID(kEmptyPreviousQueryToken));
+    return;
+  }
+
+  std::vector<std::pair<std::string, size_t>> token_and_ids;
+  EncodeRawString(prev_query, &token_and_ids);
+
+  for (const auto& pair : token_and_ids) {
+    prev_query_token_ids->push_back(pair.second);
+  }
+}
+
+void OnDeviceTailTokenizer::CreatePrefixTokenization(
+    const std::string& prefix,
+    OnDeviceTailTokenization* tokenization) const {
+  std::vector<std::pair<std::string, size_t>> token_and_ids;
+
+  EncodeRawString(prefix, &token_and_ids);
+  if (token_and_ids.empty()) {
+    return;
+  }
+
+  // Checks if the last token is ambiguous.
+  size_t num_unambiguous = token_and_ids.size();
+  if (IsAmbiguousToken(token_and_ids[token_and_ids.size() - 1].first)) {
+    num_unambiguous--;
+    tokenization->constraint_prefix =
+        token_and_ids[token_and_ids.size() - 1].first;
+  }
+
+  // Always add begin query token at the front of the prefix.
+  tokenization->unambiguous_ids.push_back(TokenToID(kBeginQueryToken));
+  for (size_t i = 0; i < num_unambiguous; ++i) {
+    tokenization->unambiguous_prefix += token_and_ids[i].first;
+    tokenization->unambiguous_ids.push_back(token_and_ids[i].second);
+  }
+}
+
+void OnDeviceTailTokenizer::InsertTokenToMaps(const std::string& token) {
+  DCHECK(!token.empty());
+  token_to_id_.insert({token, id_to_token_.size()});
+  id_to_token_.push_back(std::move(token));
+  DCHECK_EQ(token_to_id_.size(), id_to_token_.size());
+}
---
+++ components/omnibox/browser/on_device_tail_tokenizer_unittest.cc
@@ -0,0 +1,105 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/on_device_tail_tokenizer.h"
+
+#include <string>
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "base/path_service.h"
+#include "base/strings/utf_string_conversions.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::ElementsAreArray;
+
+namespace {
+
+base::FilePath GetTestVocabFilePath() {
+  base::FilePath file_path;
+  base::PathService::Get(base::DIR_SOURCE_ROOT, &file_path);
+  file_path =
+      file_path.AppendASCII("components/test/data/omnibox/vocab_test.txt");
+
+  return file_path;
+}
+
+}  // namespace
+
+class OnDeviceTailTokenizerTest : public ::testing::Test {
+ public:
+  OnDeviceTailTokenizerTest() { tokenizer_.Reset(); }
+
+ protected:
+  OnDeviceTailTokenizer tokenizer_;
+};
+
+TEST_F(OnDeviceTailTokenizerTest, CreatePrefixTokenization) {
+  tokenizer_.Init(GetTestVocabFilePath());
+
+  EXPECT_TRUE(tokenizer_.IsReady());
+
+  {
+    OnDeviceTailTokenization tokenization;
+    // Expect tokens ["n", "j", " ", "do", "c"].
+    // See OnDeviceTailTokenizer::EncodeRawString for details and simplified
+    // examples about how ID sequences are determined.
+    tokenizer_.CreatePrefixTokenization("nj doc", &tokenization);
+    EXPECT_THAT(tokenization.unambiguous_ids,
+                testing::ElementsAreArray({257, 110, 106, 32, 297}));
+    EXPECT_EQ("c", tokenization.constraint_prefix);
+    EXPECT_EQ("nj do", tokenization.unambiguous_prefix);
+  }
+
+  {
+    OnDeviceTailTokenization tokenization;
+    // Expect tokens ["re", "mi", "t", "ly", " ", "log", "in"].
+    tokenizer_.CreatePrefixTokenization("remitly login", &tokenization);
+    EXPECT_THAT(tokenization.unambiguous_ids,
+                testing::ElementsAreArray({257, 414, 366, 116, 363, 32, 521}));
+    EXPECT_EQ("in", tokenization.constraint_prefix);
+    EXPECT_EQ("remitly log", tokenization.unambiguous_prefix);
+  }
+
+  {
+    OnDeviceTailTokenization tokenization;
+    // Expect tokens
+    // ["us", " ", "pa", "ss", "po", "rt", " ", "ap", "pl", "ica", "tio", "n"]
+    tokenizer_.CreatePrefixTokenization("us passport application",
+                                        &tokenization);
+    EXPECT_THAT(tokenization.unambiguous_ids,
+                testing::ElementsAreArray({257, 456, 32, 402, 434, 407, 424, 32,
+                                           270, 406, 507, 549}));
+    EXPECT_EQ("n", tokenization.constraint_prefix);
+    EXPECT_EQ("us passport applicatio", tokenization.unambiguous_prefix);
+  }
+}
+
+TEST_F(OnDeviceTailTokenizerTest, TokenizePrevQuery) {
+  tokenizer_.Init(GetTestVocabFilePath());
+
+  EXPECT_TRUE(tokenizer_.IsReady());
+  {
+    std::vector<size_t> token_ids;
+    tokenizer_.TokenizePrevQuery("facebook", &token_ids);
+
+    // Expect tokens: ["fa", "ce", "bo", "ok"]
+    EXPECT_EQ(4, (int)token_ids.size());
+    EXPECT_THAT(token_ids, ElementsAreArray({317, 285, 281, 390}));
+    EXPECT_EQ("fa", tokenizer_.IDToToken(token_ids[0]));
+  }
+
+  {
+    std::vector<size_t> token_ids;
+    tokenizer_.TokenizePrevQuery("matching gym outfits", &token_ids);
+
+    // Expect tokens:
+    // ["ma", "t", "chi", "ng", " ", "g", "y", "m", " ", "out", "fi", "ts"]
+    EXPECT_EQ(12, (int)token_ids.size());
+    EXPECT_THAT(token_ids, ElementsAreArray({364, 116, 488, 375, 32, 103, 121,
+                                             109, 32, 533, 320, 443}));
+    EXPECT_EQ("ma", tokenizer_.IDToToken(token_ids[0]));
+  }
+}
