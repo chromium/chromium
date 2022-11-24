9e24fbe2ff6d59db95b832da0a65e74f8d8c7e27
Austin Sullivan <asully@chromium.org>
2022-11-24 07:19:42 +0000

    [M109] FSA: Re-enable overwriting moves
    Partial revert of https://crrev.com/c/3930658. Throwing on overwriting
    moves does not match POSIX. See discussion on the spec:
    https://github.com/whatwg/fs/pull/10#issuecomment-1322993643
    
    This CL is being cherry-picked to M109 to ensure overwriting moves
    are allowed in that milestone (which matches the previous behavior)
    
    (cherry picked from commit 0eeab374a618a88533ef651e88f38e67857277c4)
    
    Bug: 1366652, 1381621
    Change-Id: Ic27fb61e7598c4feb8ee770ceb78fe1fbfd28cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048780
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1074882}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4049684
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5414@{#223}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- content/browser/file_system_access/file_system_access_handle_base.cc
+++ content/browser/file_system_access/file_system_access_handle_base.cc
@@ -32,9 +32,13 @@
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_error.mojom-forward.h"
 
 namespace features {
+// TODO(crbug.com/1381621): This feature was disabled since it does not match
+// standard POSIX behavior. We should explore adding a flag to allow opting in
+// to overwriting moves. See discussion at
+// https://github.com/whatwg/fs/pull/10#issuecomment-1322993643.
 BASE_FEATURE(kFileSystemAccessDoNotOverwriteOnMove,
              "FileSystemAccessDoNotOverwriteOnMove",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace features
 
 namespace content {
