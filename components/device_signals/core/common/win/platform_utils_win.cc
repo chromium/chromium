e9a9cfdbbf04a6a71aaa1ea212408429c9928542
Sebastien Lalancette <seblalancette@chromium.org>
2022-11-26 02:12:22 +0000

    [DTC] Make CrowdStrike Agent Signal More Reusable
    Extract logic behind a utility function, which may be extended later
    for Mac or Linux support.
    
    Change-Id: I74efbd4f453cdafdf162b7b84d40c992e8c5833e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4054064
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1075855}

--- components/device_signals/core/common/win/platform_utils_win.cc
+++ components/device_signals/core/common/win/platform_utils_win.cc
@@ -9,12 +9,26 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/process/process.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/win/registry.h"
 #include "components/device_signals/core/common/common_types.h"
+#include "components/device_signals/core/common/signals_constants.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace device_signals {
 
 namespace {
+
+constexpr wchar_t kCSAgentRegPath[] =
+    L"SYSTEM\\CurrentControlSet\\services\\CSAgent\\Sim";
+
+// CU is the registry value containing the customer ID.
+constexpr wchar_t kCSCURegKey[] = L"CU";
+
+// AG is the registry value containing the agent ID.
+constexpr wchar_t kCSAGRegKey[] = L"AG";
 
 // Helper function for expanding all environment variables in `path`.
 absl::optional<std::wstring> ExpandEnvironmentVariables(
@@ -33,6 +47,27 @@ absl::optional<std::wstring> ExpandEnvironmentVariables(
       return path_expanded.substr(0, result - 1);
     path_len = result;
   } while (path_len < kMaxBuffer);
+
+  return absl::nullopt;
+}
+
+absl::optional<std::string> GetHexStringRegValue(
+    const base::win::RegKey& key,
+    const std::wstring& reg_key_name) {
+  DWORD type = REG_NONE;
+  DWORD size = 0;
+  auto res = key.ReadValue(reg_key_name.c_str(), nullptr, &size, &type);
+  if (res == ERROR_SUCCESS && type == REG_BINARY) {
+    std::vector<uint8_t> raw_bytes(size);
+    res = key.ReadValue(reg_key_name.c_str(), raw_bytes.data(), &size, &type);
+
+    if (res == ERROR_SUCCESS) {
+      // Converting the values to lowercase specifically for CrowdStrike as
+      // some of their APIs only accept the lowercase version.
+      return base::ToLowerASCII(
+          base::HexEncode(raw_bytes.data(), raw_bytes.size()));
+    }
+  }
 
   return absl::nullopt;
 }
@@ -69,6 +104,33 @@ absl::optional<base::FilePath> GetProcessExePath(base::ProcessId pid) {
   }
 
   return base::FilePath(path_string);
+}
+
+absl::optional<CrowdStrikeSignals> GetCrowdStrikeSignals() {
+  base::win::RegKey key;
+  auto result = key.Open(HKEY_LOCAL_MACHINE, kCSAgentRegPath,
+                         KEY_QUERY_VALUE | KEY_WOW64_64KEY);
+
+  if (result == ERROR_SUCCESS && key.Valid()) {
+    base::Value::Dict crowdstrike_info;
+
+    auto customer_id = GetHexStringRegValue(key, kCSCURegKey);
+    if (customer_id) {
+      crowdstrike_info.Set(names::kCustomerId, customer_id.value());
+    }
+
+    auto agent_id = GetHexStringRegValue(key, kCSAGRegKey);
+    if (agent_id) {
+      crowdstrike_info.Set(names::kAgentId, agent_id.value());
+    }
+
+    if (customer_id || agent_id) {
+      return CrowdStrikeSignals{customer_id.value_or(std::string()),
+                                agent_id.value_or(std::string())};
+    }
+  }
+
+  return absl::nullopt;
 }
 
 }  // namespace device_signals
