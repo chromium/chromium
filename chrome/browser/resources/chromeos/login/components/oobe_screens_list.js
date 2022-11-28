a4d33b850f2d3ec482809d7ca1fbd27dc73a72f5
Brahim Chikhaoui <bchikhaoui@google.com>
2022-11-28 06:54:20 +0000

    OOBE - Add a new screen_list_element for choobe
    - create new screen_list element for choobe
    to display optional screens.
    
    Bug: b:260188531
    Change-Id: I1afe952895fbad621839f936749bda0f8e4341f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4051163
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076080}

---
+++ chrome/browser/resources/chromeos/login/components/oobe_screens_list.js
@@ -0,0 +1,77 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
+import '//resources/cr_elements/icons.html.js';
+
+import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from './behaviors/oobe_i18n_behavior.m.js';
+
+
+/**
+ *  A single screen item.
+ * @typedef {{
+ *   icon: String,
+ *   title: String,
+ *   screenID: String,
+ *   selected: Boolean,
+ * }}
+ */
+export let ScreenItem;
+
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {OobeI18nBehaviorInterface}
+ */
+const OobeScreensListBase = mixinBehaviors([OobeI18nBehavior], PolymerElement);
+
+/**
+ * @polymer
+ */
+export class OobeScreensList extends OobeScreensListBase {
+  static get is() {
+    return 'oobe-screens-list';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      /**
+       * List of screens to display.
+       * @type {!Array<ScreenItem>}
+       */
+      screensList: {
+        type: Array,
+        value: [],
+      },
+      /**
+       * Number of screens selected.
+       */
+      screensSelected: {
+        type: Number,
+        value: 0,
+        notify: true,
+      },
+    };
+  }
+
+  onclick_(e) {
+    var selected = e.model.item.selected;
+    e.model.item.selected = !selected;
+    e.currentTarget.setAttribute('checked', !selected);
+    if (!selected) {
+      this.screensSelected++;
+    } else {
+      this.screensSelected--;
+    }
+  }
+}
+
+customElements.define(OobeScreensList.is, OobeScreensList);
