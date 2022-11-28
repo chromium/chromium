8255f4190823fe704c4a16694a2ab16b510b8cb1
Alex Moshchuk <alexmos@chromium.org>
2022-11-28 21:35:44 +0000

    [M109] Reset source SiteInstance before scheduling PDF navigations in subframes.
    This CL fixes a SiteInstance/BrowserContext lifetime issue in
    PdfNavigationThrottle::WillStartRequest(), which cancels certain
    subframe PDF navigations and schedules replacement navigations with
    slightly tweaked params via a PostTask.  The PostTask takes in
    OpenURLParams, which contains the source SiteInstance in a
    scoped_refptr.  Unfortunately, issue 1382761 shows that the
    BrowserContext can get destroyed after the task is scheduled but
    before it runs, and even though the task uses a WebContents WeakPtr to
    return early in that case, the task's OpenURLParams would only get
    destroyed and decrement the source SiteInstance's refcount at the time
    of that early return, which is already after the BrowserContext is
    destroyed.  When the (source) SiteInstance destructor runs and tries
    to use the SiteInstance's BrowserContext, things blow up.
    
    As a short-term fix, we can avoid keeping the source SiteInstance
    alive longer than its BrowserContext by not passing it through
    OpenURLParams, but rather setting it directly when the task runs.
    This is possible because in this case the source SiteInstance should
    always be the SiteInstance of the PDF extension loaded in the guest's
    main frame.
    
    Longer-term, we should find a more systematic way to fix these
    problems, for example by not exposing refcounting of SiteInstances
    outside of //content or introducing an API for scheduling navigations
    that is robust against BrowserContext destruction.  See the bug for
    more details and other ideas.
    
    (cherry picked from commit 9f9db7e8406998b525d5fa1786800ce2e3fc3215)
    
    Bug: 1382761
    Change-Id: Ie24e12534c0c8a990c62279a45f7211d2a93a212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043432
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1074889}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4060150
    Cr-Commit-Position: refs/branch-heads/5414@{#266}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- components/pdf/browser/pdf_navigation_throttle.cc
+++ components/pdf/browser/pdf_navigation_throttle.cc
@@ -73,6 +73,29 @@ PdfNavigationThrottle::WillStartRequest() {
   params.is_renderer_initiated = false;
   params.is_pdf = true;
 
+  // Reset the source SiteInstance.  This is a workaround for a lifetime bug:
+  // leaving the source SiteInstance in OpenURLParams could inadvertently
+  // prolong the SiteInstance's lifetime beyond the lifetime of the
+  // BrowserContext it's associated with.  The BrowserContext could get
+  // destroyed after the task below is scheduled but before it runs (see
+  // https://crbug.com/1382761), and even though the task uses a WebContents
+  // WeakPtr to return early in that case, the task's OpenURLParams would only
+  // get destroyed and decrement the source SiteInstance's refcount at the time
+  // of that early return, which is already after the BrowserContext is
+  // destroyed.  This can cause logic in the SiteInstance destructor to trip up
+  // if it tries to use the SiteInstance's BrowserContext.
+  //
+  // Fortunately, the source SiteInstance of this navigation should always
+  // correspond to the PDF extension loaded in the primary main frame of
+  // `contents`. Hence, if the navigation task does run and does not get
+  // canceled due to WebContents becoming null, we can restore the source
+  // SiteInstance at that point.
+  //
+  // TODO(crbug.com/1382761): This should be fixed in a more systematic way.
+  DCHECK_EQ(params.source_site_instance,
+            contents->GetPrimaryMainFrame()->GetSiteInstance());
+  params.source_site_instance.reset();
+
   base::SequencedTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::BindOnce(
@@ -80,11 +103,18 @@ PdfNavigationThrottle::WillStartRequest() {
              const content::OpenURLParams& params) {
             if (!web_contents)
               return;
+
+            // Restore the source SiteInstance that was cleared out of the
+            // original OpenURLParams.
+            content::OpenURLParams new_params = params;
+            new_params.source_site_instance =
+                web_contents->GetPrimaryMainFrame()->GetSiteInstance();
+
             // `MimeHandlerViewGuest` navigates its embedder for calls to
             // `WebContents::OpenURL()`, so use `LoadURLWithParams()` directly
             // instead.
             web_contents->GetController().LoadURLWithParams(
-                content::NavigationController::LoadURLParams(params));
+                content::NavigationController::LoadURLParams(new_params));
 
             // Note that we don't need to register the stream's URL loader as a
             // subresource, as `MimeHandlerViewGuest::ReadyToCommitNavigation()`
