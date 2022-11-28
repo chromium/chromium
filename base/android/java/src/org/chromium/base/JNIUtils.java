345d44c4dd7d7f7d9dc9f3ecc44a8a6eec57103e
Andrew Grieve <agrieve@chromium.org>
2022-11-28 23:35:47 +0000

    Split vr's generate_jni() target into two
    This should prevent the default classloader from being cached for the vr
    split, which then causes incorrect values when installing the DFM on T+
    devices.
    
    Bug: b/258453540
    Change-Id: Idd2753aa6fd4539dce9949a5289e99d5c3c90eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062308
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076505}

--- base/android/java/src/org/chromium/base/JNIUtils.java
+++ base/android/java/src/org/chromium/base/JNIUtils.java
@@ -4,6 +4,7 @@
 
 package org.chromium.base;
 
+import android.os.Build;
 import android.text.TextUtils;
 
 import org.chromium.base.annotations.CalledByNative;
@@ -41,6 +42,10 @@ public class JNIUtils {
             if (isInstalled) {
                 return BundleUtils.getOrCreateSplitClassLoader(splitName);
             } else {
+                assert Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU
+                    : "Should not hit splitcompat mode for Android T+. You might have a "
+                                + "generate_jni() target that declares split_name, but includes "
+                                + "a .java file from the base split (https://crbug.com/1394148).";
                 // Split was installed by PlayCore in "compat" mode, meaning that our base module's
                 // ClassLoader was patched to add the splits' dex file to it.
             }
