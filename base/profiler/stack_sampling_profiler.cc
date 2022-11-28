ea36f1c35b104069275125bcfea5a233abc69ab8
Hailey Wang <haileywang@google.com>
2022-11-28 23:30:01 +0000

    Revert "Enable `base::StackSamplingProfiler` on ARM64 on Android."
    This reverts commit 0fa63ab13e8a5a7613d70b0a6573026356a7c691.
    
    Reason for revert: Suspected for breaking ThreadProfilerPlatformConfigurationTest.IsSupported on arm64.
    See crbug.com/1394169.
    
    Original change's description:
    > Enable `base::StackSamplingProfiler` on ARM64 on Android.
    >
    > In and of itself, this changes does not do much, because
    > the users of this class on Android -- Tracing
    > (`TracingSamplerProfiler`) and Stack Sampled Metrics
    > (`ThreadProfiler`) -- do not enable stack sampling on
    > ARM64 on Android yet.
    >
    > Bug: 1392158
    > Change-Id: I2fdcfdd8dcbe08211b72bdf0c4567eea2f5b1dbd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043661
    > Reviewed-by: Eric Seckler <eseckler@chromium.org>
    > Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1075376}
    
    Bug: 1392158
    Change-Id: I144647a3554c7e13558b60723d98da1eb95e681c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062390
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1076501}

--- base/profiler/stack_sampling_profiler.cc
+++ base/profiler/stack_sampling_profiler.cc
@@ -760,13 +760,12 @@ TimeTicks StackSamplingProfiler::TestPeer::GetNextSampleTime(
 }
 
 // static
-// The profiler is currently supported for Windows x64, macOS, iOS 64-bit,
-// Android ARM32, and Android ARM64.
+// The profiler is currently supported for Windows x64, macOS, iOS 64-bit, and
+// Android ARM32.
 bool StackSamplingProfiler::IsSupportedForCurrentPlatform() {
-#if (BUILDFLAG(IS_WIN) && defined(ARCH_CPU_X86_64)) || BUILDFLAG(IS_MAC) || \
-    (BUILDFLAG(IS_IOS) && defined(ARCH_CPU_64_BITS)) ||                     \
-    (BUILDFLAG(IS_ANDROID) &&                                               \
-     (BUILDFLAG(ENABLE_ARM_CFI_TABLE) || defined(ARCH_CPU_ARM64)))
+#if (BUILDFLAG(IS_WIN) && defined(ARCH_CPU_X86_64)) || BUILDFLAG(IS_MAC) ||  \
+    (BUILDFLAG(IS_IOS) && defined(ARCH_CPU_64_BITS)) ||                      \
+    (BUILDFLAG(IS_ANDROID) && BUILDFLAG(ENABLE_ARM_CFI_TABLE))
 #if BUILDFLAG(IS_WIN)
   // Do not start the profiler when Application Verifier is in use; running them
   // simultaneously can cause crashes and has no known use case.
