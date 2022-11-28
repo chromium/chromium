c8c9a8ef521c7801db0b3523380375ed3881fa86
Santiago Castano Moreno <scmoreno@google.com>
2022-11-28 20:25:16 +0000

    Reporting: Add croshealthd memory sampler handler.
    (7/9) Continuation of Refactoring cros_healthd_metric_sampler. Added the
    Memory category result handler.
    
    Bug: b:255631907
    
    Change-Id: I6bb8f730650c045d6caeadcd52a0a7da36ce1054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4043425
    Reviewed-by: Alberto Juarez <albertojuarez@google.com>
    Commit-Queue: Santiago Castano <scmoreno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1076386}

--- chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.cc
@@ -10,6 +10,7 @@
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_bus_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_cpu_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_input_sampler_handler.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_memory_sampler_handler.h"
 #include "chromeos/ash/services/cros_healthd/public/cpp/service_connection.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -18,92 +19,6 @@ namespace reporting {
 namespace {
 
 namespace cros_healthd = ::ash::cros_healthd::mojom;
-
-MemoryEncryptionState TranslateMemoryEncryptionState(
-    cros_healthd::EncryptionState encryption_state) {
-  switch (encryption_state) {
-    case cros_healthd::EncryptionState::kUnknown:
-      return MEMORY_ENCRYPTION_STATE_UNKNOWN;
-    case cros_healthd::EncryptionState::kEncryptionDisabled:
-      return MEMORY_ENCRYPTION_STATE_DISABLED;
-    case cros_healthd::EncryptionState::kTmeEnabled:
-      return MEMORY_ENCRYPTION_STATE_TME;
-    case cros_healthd::EncryptionState::kMktmeEnabled:
-      return MEMORY_ENCRYPTION_STATE_MKTME;
-  }
-
-  NOTREACHED();
-}
-
-MemoryEncryptionAlgorithm TranslateMemoryEncryptionAlgorithm(
-    cros_healthd::CryptoAlgorithm encryption_algorithm) {
-  switch (encryption_algorithm) {
-    case cros_healthd::CryptoAlgorithm::kUnknown:
-      return MEMORY_ENCRYPTION_ALGORITHM_UNKNOWN;
-    case cros_healthd::CryptoAlgorithm::kAesXts128:
-      return MEMORY_ENCRYPTION_ALGORITHM_AES_XTS_128;
-    case cros_healthd::CryptoAlgorithm::kAesXts256:
-      return MEMORY_ENCRYPTION_ALGORITHM_AES_XTS_256;
-  }
-
-  NOTREACHED();
-}
-
-void HandleMemoryResult(OptionalMetricCallback callback,
-                        CrosHealthdMetricSampler::MetricType metric_type,
-                        cros_healthd::TelemetryInfoPtr result) {
-  absl::optional<MetricData> metric_data;
-  const auto& memory_result = result->memory_result;
-
-  if (!memory_result.is_null()) {
-    switch (memory_result->which()) {
-      case cros_healthd::MemoryResult::Tag::kError: {
-        DVLOG(1) << "cros_healthd: Error getting memory info: "
-                 << memory_result->get_error()->msg;
-        break;
-      }
-
-      case cros_healthd::MemoryResult::Tag::kMemoryInfo: {
-        const auto& memory_info = memory_result->get_memory_info();
-        if (memory_result.is_null()) {
-          DVLOG(1) << "Null MemoryInfo from cros_healthd";
-          break;
-        }
-
-        // Gather memory info.
-        if (metric_type == CrosHealthdMetricSampler::MetricType::kInfo) {
-          metric_data = absl::make_optional<MetricData>();
-          auto* const memory_encryption_info_out =
-              metric_data->mutable_info_data()
-                  ->mutable_memory_info()
-                  ->mutable_tme_info();
-          const auto* const memory_encryption_info =
-              memory_info->memory_encryption_info.get();
-
-          if (memory_encryption_info) {
-            memory_encryption_info_out->set_encryption_state(
-                TranslateMemoryEncryptionState(
-                    memory_encryption_info->encryption_state));
-            memory_encryption_info_out->set_encryption_algorithm(
-                TranslateMemoryEncryptionAlgorithm(
-                    memory_encryption_info->active_algorithm));
-            memory_encryption_info_out->set_max_keys(
-                memory_encryption_info->max_key_number);
-            memory_encryption_info_out->set_key_length(
-                memory_encryption_info->key_length);
-          } else {
-            // If encryption info isn't set, mark it as disabled.
-            memory_encryption_info_out->set_encryption_state(
-                MEMORY_ENCRYPTION_STATE_DISABLED);
-          }
-        }
-        break;
-      }
-    }
-  }
-
-  std::move(callback).Run(std::move(metric_data));
-}
 
 void HandleDisplayResult(OptionalMetricCallback callback,
                          CrosHealthdMetricSampler::MetricType metric_type,
@@ -275,7 +190,8 @@ void OnHealthdInfoReceived(OptionalMetricCallback callback,
       break;
     }
     case cros_healthd::ProbeCategoryEnum::kMemory: {
-      HandleMemoryResult(std::move(callback), metric_type, std::move(result));
+      CrosHealthdMemorySamplerHandler handler = CrosHealthdMemorySamplerHandler();
+      handler.HandleResult(std::move(result), std::move(callback));
       break;
     }
     case cros_healthd::ProbeCategoryEnum::kBootPerformance: {
