4df83114a9d6448ec744b769c7b60353fede669a
wenyu zhang <zhangwenyu@google.com>
2022-11-29 20:49:48 +0000

    accelerators: Add AcceleratorTracker to record metrics for any shortcuts
    AcceleratorTracker class allows stakeholders to record metrics for any
    key combinations.
    
    - The class overrides OnKeyEvent function from ui::EventHandler class.
    - The class uses a FlatMap to store the mapping between key
    event to user action metrics string.
    
    Design doc: go/accelerator-tracker-dd
    
    Bug: b/260237218
    Test: ash/accelerators/accelerator_tracker_unittest.cc
    Change-Id: Ifec5ef06f6a81a1ab6c4e95e66d284e7efb572a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4040022
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1077045}

---
+++ ash/accelerators/accelerator_tracker.cc
@@ -0,0 +1,32 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/accelerators/accelerator_tracker.h"
+
+#include "base/metrics/user_metrics.h"
+#include "ui/events/event.h"
+
+namespace ash {
+
+AcceleratorTracker::AcceleratorTracker(
+    base::span<const TrackerDataActionPair> tracker_data_list) {
+  for (const auto& [tracker_data, user_action_name] : tracker_data_list) {
+    accelerator_tracker_map_[tracker_data] = user_action_name;
+  }
+}
+
+AcceleratorTracker::~AcceleratorTracker() = default;
+
+void AcceleratorTracker::OnKeyEvent(ui::KeyEvent* event) {
+  TrackerData trackerData(event->type() == ui::ET_KEY_PRESSED
+                              ? KeyState::PRESSED
+                              : KeyState::RELEASED,
+                          event->key_code(), event->flags());
+  const auto it = accelerator_tracker_map_.find(trackerData);
+  if (it != accelerator_tracker_map_.end()) {
+    base::RecordAction(base::UserMetricsAction(it->second.data()));
+  }
+}
+
+}  // namespace ash
---
+++ ash/accelerators/accelerator_tracker.h
@@ -0,0 +1,75 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_ACCELERATORS_ACCELERATOR_TRACKER_H_
+#define ASH_ACCELERATORS_ACCELERATOR_TRACKER_H_
+
+#include "ash/ash_export.h"
+#include "base/containers/flat_map.h"
+#include "base/containers/span.h"
+#include "base/strings/string_piece_forward.h"
+#include "ui/base/accelerators/accelerator.h"
+#include "ui/events/event_handler.h"
+
+namespace ash {
+
+using KeyState = ui::Accelerator::KeyState;
+
+// A data structure to identify a tracking key event.
+struct TrackerData {
+  constexpr TrackerData(KeyState key_state,
+                        ui::KeyboardCode key_code,
+                        int flags)
+      : key_state(key_state), key_code(key_code), flags(flags) {}
+
+  KeyState key_state;
+  ui::KeyboardCode key_code;
+  int flags;
+
+  // Comparator in the base::flat_map where TrackerData is used as the
+  // key. The order of comparison follows ui::Accelerator.
+  constexpr bool operator<(const TrackerData& rhs) const {
+    if (key_code != rhs.key_code)
+      return key_code < rhs.key_code;
+    if (key_state != rhs.key_state)
+      return key_state < rhs.key_state;
+    return flags < rhs.flags;
+  }
+};
+
+using TrackerDataActionPair = std::pair<TrackerData, base::StringPiece>;
+
+// A list of pair between TrackerData and user action string.
+// Add the key combination you want to track here.
+// Make sure the user action string starts with "AccelTracker_"
+// to differentiate with the real accelerators.
+// Also document the user action in tools/metrics/actions/actions.xml.
+constexpr TrackerDataActionPair kAcceleratorTrackerList[] = {
+    // Example entry.
+    // {{KeyState::PRESSED, ui::VKEY_C, ui::EF_ALT_DOWN},
+    // "AccelTracker_Alt_Copy"},
+};
+
+// AcceleratorTracker is a Shell pretarget EventHandler that only records user
+// action metrics for any key combinations and does not consume the inputted
+// events or block them from propagating.
+class ASH_EXPORT AcceleratorTracker : public ui::EventHandler {
+ public:
+  explicit AcceleratorTracker(
+      base::span<const TrackerDataActionPair> tracker_data_list);
+  AcceleratorTracker(const AcceleratorTracker&) = delete;
+  AcceleratorTracker& operator=(const AcceleratorTracker&) = delete;
+  ~AcceleratorTracker() override;
+
+  // ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
+
+ private:
+  // Used to lookup for the user action string given a key event.
+  base::flat_map<TrackerData, base::StringPiece> accelerator_tracker_map_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_ACCELERATORS_ACCELERATOR_TRACKER_H_
