58a7bc02df0dc5fa6781d1c922909d267892806a
Charlie Harrison <csharrison@chromium.org>
2022-11-29 01:54:00 +0000

    Expand base64 tests to test 16 bit inputs
    Bug: None
    Change-Id: I730d482a9877853ea371586348ff7da80d82de1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4048034
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076588}

--- third_party/blink/renderer/platform/wtf/text/base64_test.cc
+++ third_party/blink/renderer/platform/wtf/text/base64_test.cc
@@ -7,6 +7,7 @@
 #include "base/containers/span.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -38,47 +39,61 @@ TEST(Base64Test, Encode) {
 TEST(Base64Test, DecodeNoPaddingValidation) {
   struct {
     const char* in;
-    Vector<char> expected_out;
+    const char* expected_out;
   } kTestCases[] = {
       // Inputs that are multiples of 4 always succeed.
-      {"abcd", {'i', '\xB7', '\x1D'}},
-      {"abc=", {'i', '\xB7'}},
-      {"abcdefgh", {'i', '\xB7', '\x1D', 'y', '\xF8', '!'}},
+      {"abcd", "i\xB7\x1D"},
+      {"abc=", "i\xB7"},
+      {"abcdefgh", "i\xB7\x1Dy\xF8!"},
 
       // Lack of proper padding (to a multiple of 4) always succeeds if
       // len % 4 != 1.
-      {"abcdef", {'i', '\xB7', '\x1D', 'y'}},
-      {"abc", {'i', '\xB7'}},
-      {"ab", {'i'}},
+      {"abcdef", "i\xB7\x1Dy"},
+      {"abc", "i\xB7"},
+      {"ab", "i"},
 
       // Invalid padding is ignored if kNoPaddingValidation is set.
-      {"abcd=", {'i', '\xB7', '\x1D'}},
-      {"abcd==", {'i', '\xB7', '\x1D'}},
-      {"abcd===", {'i', '\xB7', '\x1D'}},
-      {"abcd==============", {'i', '\xB7', '\x1D'}},
+      {"abcd=", "i\xB7\x1D"},
+      {"abcd==", "i\xB7\x1D"},
+      {"abcd===", "i\xB7\x1D"},
+      {"abcd==============", "i\xB7\x1D"},
+      {"=", ""},
 
-      // Whitespace should not be allow if kNoPaddingValidation is set.
-      {" a bcd", {}},
-      {"ab\t\tc=", {}},
-      {"ab c\ndefgh ", {}},
+      // Whitespace should not be allowed if kNoPaddingValidation is set.
+      {" a bcd", nullptr},
+      {"ab\t\tc=", nullptr},
+      {"ab c\ndefgh ", nullptr},
 
       // Failures that should apply in all decoding modes.
-      {"abc&", {}},
-      {"abcde", {}},
-      {"a", {}},
-      {"=", {}},
+      {"abc&", nullptr},
+      {"abcde", nullptr},
+      {"a", nullptr},
 
       // Empty string should yield an empty result.
-      {"", {}},
+      {"", ""},
   };
 
   for (const auto& test : kTestCases) {
+    SCOPED_TRACE(::testing::Message() << test.in);
     Vector<char> out;
-    bool success = Base64Decode(String(test.in), out);
-    if (success) {
-      EXPECT_EQ(test.expected_out, out) << test.in;
-    } else {
-      EXPECT_TRUE(test.expected_out.empty()) << test.in;
+    String in = String(test.in);
+    bool expected_success = test.expected_out != nullptr;
+    Vector<char> expected_out;
+    if (expected_success) {
+      expected_out.insert(0, test.expected_out, strlen(test.expected_out));
+    }
+
+    bool success_8bit = Base64Decode(in, out);
+    EXPECT_EQ(expected_success, success_8bit);
+    if (expected_success) {
+      EXPECT_EQ(expected_out, out);
+    }
+    out.clear();
+    in.Ensure16Bit();
+    bool success_16bit = Base64Decode(in, out);
+    EXPECT_EQ(expected_success, success_16bit);
+    if (expected_success) {
+      EXPECT_EQ(expected_out, out);
     }
   }
 }
@@ -86,48 +101,61 @@ TEST(Base64Test, DecodeNoPaddingValidation) {
 TEST(Base64Test, ForgivingBase64Decode) {
   struct {
     const char* in;
-    Vector<char> expected_out;
+    const char* expected_out;
   } kTestCases[] = {
       // Inputs that are multiples of 4 always succeed.
-      {"abcd", {'i', '\xB7', '\x1D'}},
-      {"abc=", {'i', '\xB7'}},
-      {"abcdefgh", {'i', '\xB7', '\x1D', 'y', '\xF8', '!'}},
+      {"abcd", "i\xB7\x1D"},
+      {"abc=", "i\xB7"},
+      {"abcdefgh", "i\xB7\x1Dy\xF8!"},
 
       // Lack of proper padding (to a multiple of 4) always succeeds if
       // len % 4 != 1.
-      {"abcdef", {'i', '\xB7', '\x1D', 'y'}},
-      {"abc", {'i', '\xB7'}},
-      {"ab", {'i'}},
+      {"abcdef", "i\xB7\x1Dy"},
+      {"abc", "i\xB7"},
+      {"ab", "i"},
 
       // Invalid padding causes failure if kForgiving is set.
-      {"abcd=", {}},
-      {"abcd==", {}},
-      {"abcd===", {}},
-      {"abcd==============", {}},
+      {"abcd=", nullptr},
+      {"abcd==", nullptr},
+      {"abcd===", nullptr},
+      {"abcd==============", nullptr},
+      {"=", nullptr},
 
       // Whitespace should be allow if kForgiving is set.
-      {" a bcd", {'i', '\xB7', '\x1D'}},
-      {"ab\t\tc=", {'i', '\xB7'}},
-      {"ab c\ndefgh ", {'i', '\xB7', '\x1D', 'y', '\xF8', '!'}},
+      {" a bcd", "i\xB7\x1D"},
+      {"ab\t\tc=", "i\xB7"},
+      {"ab c\ndefgh", "i\xB7\x1Dy\xF8!"},
 
       // Failures that should apply in all decoding modes.
-      {"abc&", {}},
-      {"abcde", {}},
-      {"a", {}},
-      {"=", {}},
+      {"abc&", nullptr},
+      {"abcde", nullptr},
+      {"a", nullptr},
 
       // Empty string should yield an empty result.
-      {"", {}},
+      {"", ""},
   };
 
   for (const auto& test : kTestCases) {
+    SCOPED_TRACE(::testing::Message() << test.in);
     Vector<char> out;
-    bool success =
-        Base64Decode(String(test.in), out, Base64DecodePolicy::kForgiving);
-    if (success) {
-      EXPECT_EQ(test.expected_out, out) << test.in;
-    } else {
-      EXPECT_TRUE(test.expected_out.empty()) << test.in;
+    String in = String(test.in);
+    bool expected_success = test.expected_out != nullptr;
+    Vector<char> expected_out;
+    if (expected_success) {
+      expected_out.insert(0, test.expected_out, strlen(test.expected_out));
+    }
+
+    bool success_8bit = Base64Decode(in, out, Base64DecodePolicy::kForgiving);
+    EXPECT_EQ(expected_success, success_8bit);
+    if (expected_success) {
+      EXPECT_EQ(expected_out, out);
+    }
+    out.clear();
+    in.Ensure16Bit();
+    bool success_16bit = Base64Decode(in, out, Base64DecodePolicy::kForgiving);
+    EXPECT_EQ(expected_success, success_16bit);
+    if (expected_success) {
+      EXPECT_EQ(expected_out, out);
     }
   }
 }
