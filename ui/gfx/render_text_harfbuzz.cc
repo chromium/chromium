a4c5875416bd79bccc9f32332575ce1c5b027f57
Elaine Chien <elainechien@chromium.org>
2022-11-29 15:59:01 +0000

    Revert "Reland: Report crash-dump for GetFontFallbacks that succeed on Windows"
    This reverts commit 785840f3e871e451f0108965699385496e641040.
    
    Reason for revert: Collected crash dumps on canary. Revert before
    dev cut.
    
    Original change's description:
    > Reland: Report crash-dump for GetFontFallbacks that succeed on Windows
    >
    > This is a reland of crrev.com/c/2049126
    >
    > Bug: 995789
    > Change-Id: I0a6674e92763fb3a4f7e1a5a3f18a3876cafc4ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4033786
    > Auto-Submit: Elaine Chien <elainechien@chromium.org>
    > Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    > Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1072934}
    
    Bug: 995789
    Change-Id: If5693b944943ea217d0ea2231ecd7827b3249a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062933
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076874}

--- ui/gfx/render_text_harfbuzz.cc
+++ ui/gfx/render_text_harfbuzz.cc
@@ -11,7 +11,6 @@
 #include "base/containers/contains.h"
 #include "base/containers/lru_cache.h"
 #include "base/containers/span.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/hash/hash.h"
 #include "base/i18n/base_i18n_switches.h"
@@ -2137,16 +2136,6 @@ void RenderTextHarfBuzz::ShapeRuns(
                            TRACE_STR_COPY(font_name.c_str()),
                            "primary_font_name", primary_font.GetFontName());
       RecordShapeRunsFallback(ShapeRunFallback::FALLBACKS);
-#if BUILDFLAG(IS_WIN)
-      // Resolving fallback fonts using the registry keys on windows will be
-      // deprecated and removed (see: http://crbug.com/995789). The crashes
-      // reported here should be fixed before deprecating the code.
-      static bool is_first_crash = true;
-      if (is_first_crash) {
-        is_first_crash = false;
-        base::debug::DumpWithoutCrashing();
-      }
-#endif
       return;
     }
   }
