19b73bf8ae506cc91054fe6de539750240771173
Nigel Tao <nigeltao@chromium.org>
2022-11-29 06:17:59 +0000

    Remove VolumeManager::RestoreProvidedFileSystems
    Prior to this commit, there were two relatively-independent "restore
    FSP (File System Provider)" mechanisms.
    
    One is in ash::file_system_provider::Service::RestoreFileSystems in
    chrome/browser/ash/file_system_provider/service.cc, which is not
    affected by this commit.
    
    Two is in file_manager::VolumeManager::RestoreProvidedFileSystems in
    chrome/browser/ash/file_manager/volume_manager.cc which is removed by
    this commit.
    
    The two are redundant. Furthermore, doing the restoration twice means
    that ExternalMountPoints::RegisterFileSystem gets called twice, both
    in VolumeManager::OnProvidedFileSystemMount, for the Fusebox-flavored
    FSP volume. The second RegFS call leads to a DCHECK failure.
    
    For the regular (non-Fusebox-flavored) FSP volume, VolumeManager is not
    responsible for calling RegisterFileSystem. RegFS is instead called by
    ash::file_system_provider::Service::MountFileSystemInternal which is
    called by the previously mentioned
    ash::file_system_provider::Service::RestoreFileSystems and that is only
    called once.
    
    BUG=None
    TEST=manually install a persistent FSP and check it's still auto-loaded
    
    Change-Id: Ia542015e9048545d7d045eced04eb8486f59e115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4025399
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1076680}

--- chrome/browser/ash/file_manager/volume_manager.cc
+++ chrome/browser/ash/file_manager/volume_manager.cc
@@ -755,16 +755,9 @@ void VolumeManager::Initialize() {
   // Subscribe to ARC DocumentsProvider events about roots.
   documents_provider_root_manager_->AddObserver(this);
 
-  // Subscribe to FileSystemProviderService and register currently mounted
-  // volumes for the profile.
+  // Subscribe to FileSystemProviderService.
   if (file_system_provider_service_) {
     file_system_provider_service_->AddObserver(this);
-
-    auto restore_provided_file_systems =
-        base::BindOnce(&VolumeManager::RestoreProvidedFileSystems,
-                       weak_ptr_factory_.GetWeakPtr());
-    content::GetUIThreadTaskRunner({})->PostTask(
-        FROM_HERE, std::move(restore_provided_file_systems));
   }
 
   // Subscribe to Profile Preference change.
@@ -1280,19 +1273,6 @@ void VolumeManager::OnRenameEvent(
   }
 
   NOTREACHED() << "Unexpected RenameEvent " << event;
-}
-
-void VolumeManager::RestoreProvidedFileSystems() {
-  DCHECK(file_system_provider_service_);
-
-  std::vector<ash::file_system_provider::ProvidedFileSystemInfo>
-      file_system_info_list =
-          file_system_provider_service_->GetProvidedFileSystemInfoList();
-  for (const auto& file_system_info : file_system_info_list) {
-    OnProvidedFileSystemMount(file_system_info,
-                              ash::file_system_provider::MOUNT_CONTEXT_RESTORE,
-                              base::File::FILE_OK);
-  }
 }
 
 void VolumeManager::OnProvidedFileSystemMount(
