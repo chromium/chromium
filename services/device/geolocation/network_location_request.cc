0fe03e6f4ca19ac9723e680a0c357bf659fff1b7
Ashley Newson <ashleynewson@chromium.org>
2022-11-30 10:55:23 +0000

    [CodeHealth] Migrate to Value::Dict in network_location_request.cc
    Migrate from deprecated base::DictionaryValue to base::Value::Dict.
    
    Remove obsolete GetAsDouble helper function.
    
    Bug: 1187061
    Change-Id: If4ac60fbd2486517d9933b430d78e01d833755e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061084
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077381}

--- services/device/geolocation/network_location_request.cc
+++ services/device/geolocation/network_location_request.cc
@@ -31,6 +31,7 @@
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace device {
 namespace {
@@ -110,7 +111,7 @@ bool ParseServerResponse(const std::string& response_body,
                          mojom::Geoposition* position);
 void AddWifiData(const WifiData& wifi_data,
                  int age_milliseconds,
-                 base::DictionaryValue* request);
+                 base::Value::Dict& request);
 }  // namespace
 
 NetworkLocationRequest::NetworkLocationRequest(
@@ -239,8 +240,8 @@ void FormUploadData(const WifiData& wifi_data,
       age = static_cast<int>(delta_ms);
   }
 
-  base::DictionaryValue request;
-  AddWifiData(wifi_data, age, &request);
+  base::Value::Dict request;
+  AddWifiData(wifi_data, age, request);
   base::JSONWriter::Write(request, upload_data);
 }
 
@@ -260,9 +261,7 @@ void AddInteger(const std::string& property_name,
 
 void AddWifiData(const WifiData& wifi_data,
                  int age_milliseconds,
-                 base::DictionaryValue* request) {
-  DCHECK(request);
-
+                 base::Value::Dict& request) {
   if (wifi_data.access_point_data.empty())
     return;
 
@@ -286,8 +285,7 @@ void AddWifiData(const WifiData& wifi_data,
     wifi_access_point_list.Append(std::move(wifi_dict));
   }
   if (!wifi_access_point_list.empty())
-    request->GetDict().Set("wifiAccessPoints",
-                           base::Value(std::move(wifi_access_point_list)));
+    request.Set("wifiAccessPoints", std::move(wifi_access_point_list));
 }
 
 void FormatPositionError(const GURL& server_url,
@@ -361,29 +359,6 @@ void GetLocationFromResponse(int net_error,
   RecordUmaEvent(NETWORK_LOCATION_REQUEST_EVENT_RESPONSE_SUCCESS);
 }
 
-// Numeric values without a decimal point have type integer and IsDouble() will
-// return false. This is convenience function for detecting integer or floating
-// point numeric values. Note that isIntegral() includes boolean values, which
-// is not what we want.
-bool GetAsDouble(const base::DictionaryValue& object,
-                 const std::string& property_name,
-                 double* out) {
-  DCHECK(out);
-  const base::Value* value = NULL;
-  if (!object.Get(property_name, &value))
-    return false;
-  DCHECK(value);
-  if (value->is_int()) {
-    *out = value->GetInt();
-    return true;
-  }
-  if (value->is_double()) {
-    *out = value->GetDouble();
-    return true;
-  }
-  return false;
-}
-
 bool ParseServerResponse(const std::string& response_body,
                          const base::Time& wifi_timestamp,
                          mojom::Geoposition* position) {
@@ -408,25 +383,24 @@ bool ParseServerResponse(const std::string& response_body,
   }
   base::Value response_value = std::move(*response_result);
 
-  if (!response_value.is_dict()) {
+  const base::Value::Dict* response_object = response_value.GetIfDict();
+  if (!response_object) {
     VLOG(1) << "ParseServerResponse() : Unexpected response type "
             << response_value.type();
     return false;
   }
-  const base::DictionaryValue* response_object =
-      static_cast<base::DictionaryValue*>(&response_value);
 
   // Get the location
-  const base::Value* location_value = NULL;
-  if (!response_object->Get(kLocationString, &location_value)) {
+  const base::Value* location_value = response_object->Find(kLocationString);
+  if (!location_value) {
     VLOG(1) << "ParseServerResponse() : Missing location attribute.";
     // GLS returns a response with no location property to represent
     // no fix available; return true to indicate successful parse.
     return true;
   }
-  DCHECK(location_value);
 
-  if (!location_value->is_dict()) {
+  const base::Value::Dict* location_object = location_value->GetIfDict();
+  if (!location_object) {
     if (!location_value->is_none()) {
       VLOG(1) << "ParseServerResponse() : Unexpected location type "
               << location_value->type();
@@ -436,24 +410,27 @@ bool ParseServerResponse(const std::string& response_body,
     }
     return true;  // Successfully parsed response containing no fix.
   }
-  const base::DictionaryValue* location_object =
-      static_cast<const base::DictionaryValue*>(location_value);
 
   // latitude and longitude fields are always required.
-  double latitude = 0;
-  double longitude = 0;
-  if (!GetAsDouble(*location_object, kLatitudeString, &latitude) ||
-      !GetAsDouble(*location_object, kLongitudeString, &longitude)) {
+  absl::optional<double> latitude =
+      location_object->FindDouble(kLatitudeString);
+  absl::optional<double> longitude =
+      location_object->FindDouble(kLongitudeString);
+  if (!latitude || !longitude) {
     VLOG(1) << "ParseServerResponse() : location lacks lat and/or long.";
     return false;
   }
   // All error paths covered: now start actually modifying postion.
-  position->latitude = latitude;
-  position->longitude = longitude;
+  position->latitude = *latitude;
+  position->longitude = *longitude;
   position->timestamp = wifi_timestamp;
 
   // Other fields are optional.
-  GetAsDouble(*response_object, kAccuracyString, &position->accuracy);
+  absl::optional<double> accuracy =
+      response_object->FindDouble(kAccuracyString);
+  if (accuracy) {
+    position->accuracy = *accuracy;
+  }
 
   return true;
 }
