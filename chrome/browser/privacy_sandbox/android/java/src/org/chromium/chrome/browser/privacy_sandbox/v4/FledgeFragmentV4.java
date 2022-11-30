4b4f414d8fdeac3a874caf8db775f9d588df876b
Rubin Deliallisi <rubindl@chromium.org>
2022-11-30 14:38:50 +0000

    Privacy Sandbox Settings: Fledge toggle logic
    - Connect the Fledge toggle to the correct preference.
    - Add integration tests to confirm the behavior.
    - Render tests are still not possible at this point.
    
    Bug: 1378703, b/254410793
    Change-Id: I86c132ebdb64d957b6051c291002d0f246fbcf7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4065964
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077464}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/FledgeFragmentV4.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/FledgeFragmentV4.java
@@ -6,20 +6,57 @@ package org.chromium.chrome.browser.privacy_sandbox.v4;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
 
+import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxSettingsBaseFragment;
 import org.chromium.chrome.browser.privacy_sandbox.R;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.components.prefs.PrefService;
+import org.chromium.components.user_prefs.UserPrefs;
 
 /**
  * Fragment for the Privacy Sandbox -> Fledge preferences.
  */
-public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment {
+public class FledgeFragmentV4 extends PrivacySandboxSettingsBaseFragment
+        implements Preference.OnPreferenceChangeListener {
+    private static final String FLEDGE_TOGGLE_PREFERENCE = "fledge_toggle";
+
+    private ChromeSwitchPreference mFledgeTogglePreference;
+
+    static boolean isFledgePrefEnabled() {
+        PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
+        return prefService.getBoolean(Pref.PRIVACY_SANDBOX_M1_FLEDGE_ENABLED);
+    }
+
+    static void setFledgePrefEnabled(boolean isEnabled) {
+        PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
+        prefService.setBoolean(Pref.PRIVACY_SANDBOX_M1_FLEDGE_ENABLED, isEnabled);
+    }
+
     @Override
     public void onCreatePreferences(@Nullable Bundle bundle, @Nullable String s) {
         super.onCreatePreferences(bundle, s);
         getActivity().setTitle(R.string.settings_fledge_page_title);
         SettingsUtils.addPreferencesFromResource(this, R.xml.fledge_preference_v4);
+
+        mFledgeTogglePreference = findPreference(FLEDGE_TOGGLE_PREFERENCE);
+        mFledgeTogglePreference.setChecked(isFledgePrefEnabled());
+        mFledgeTogglePreference.setOnPreferenceChangeListener(this);
+        // TODO(http://b/254411473): Make the preference managed.
+    }
+
+    @Override
+    public boolean onPreferenceChange(@NonNull Preference preference, Object value) {
+        if (preference.getKey().equals(FLEDGE_TOGGLE_PREFERENCE)) {
+            setFledgePrefEnabled((boolean) value);
+            return true;
+        }
+
+        return false;
     }
 }
