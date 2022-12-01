3fcb5672fc13f536f8d34d53c94db03ba0ce6b96
Jayson Adams <shrike@chromium.org>
2022-12-01 22:20:49 +0000

    Revert "[Mac System Hotkeys] Skip undocumented AppleSymbolicHotKeys"
    This reverts commit dab8a4b94bf7911902f29a3fa0066b7d95fe02cf.
    
    Reason for revert: leads to crashing in the app shim. See crbug.com/1395153.
    
    
    Original change's description:
    > [Mac System Hotkeys] Skip undocumented AppleSymbolicHotKeys
    >
    > When you change the shortcuts for Spaces Left and Spaces Right, macOS
    > inserts your new shortcuts at slots 79 and 81 in AppleSymbolicHotKeys.
    > For some reason, it also inserts "shifted" versions of these
    > shortcuts at slots 80 and 82. It's unclear why these shifted versions
    > exist because the shortcuts don't seem to do anything. They could be
    > entries macOS adds for some reason but knows to skip (not expecting
    > any non-system application to read this file). Treating these
    > shortcuts as valid creates unexpected behavior. For example, in the
    > case of "Spaces Left" being mapped to Option Left Arrow, this extra
    > shortcut causes Chrome to silently ignore Shift-Option Left Arrow,
    > the shortcut which appends the current text selection by one word to
    > the left. This cl works around the problem by changing the
    > SystemHotkeyMap to ignore these two undocumented shortcuts.
    >
    > This cl also forces an eventual reload of the SystemHotkeyMap after
    > the user switches away from Chrome and back, to ensure any new
    > shortcut settings are recognized.
    >
    > Bug: 874219
    >
    > Test: out/Default/content_unittests \
    >     --gtest_filter=SystemHotkeyMapTest.IgnoreUndocumentedShortcutEntries
    >
    > Change-Id: Ifc17ec48877de8aeae01244794ec5fcf2444fe5c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3908188
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Jayson Adams <shrike@chromium.org>
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1077678}
    
    Bug: 874219
    Change-Id: Ie7c250f675de4c0194a25463a51c802d505850b2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072805
    Owners-Override: Kevin Marshall <kmarshall@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jayson Adams <shrike@chromium.org>
    Reviewed-by: Tom Burgin <bur@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078313}

--- content/browser/cocoa/system_hotkey_helper_mac.h
+++ content/browser/cocoa/system_hotkey_helper_mac.h
@@ -5,20 +5,12 @@
 #ifndef CONTENT_BROWSER_COCOA_SYSTEM_HOTKEY_HELPER_MAC_H_
 #define CONTENT_BROWSER_COCOA_SYSTEM_HOTKEY_HELPER_MAC_H_
 
-#include "base/callback.h"
-#include "base/files/file_path.h"
-#include "content/common/content_export.h"
-
 namespace content {
 
 class SystemHotkeyMap;
 
 // Guaranteed to not be NULL.
-CONTENT_EXPORT SystemHotkeyMap* GetSystemHotkeyMap();
-
-CONTENT_EXPORT void SetSystemHotkeyPlistPathForTesting(
-    base::FilePath& file_path);
-CONTENT_EXPORT void WaitForEventsForTesting();
+SystemHotkeyMap* GetSystemHotkeyMap();
 
 }  // namespace content
 
