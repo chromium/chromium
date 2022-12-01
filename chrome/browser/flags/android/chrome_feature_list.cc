01193226674627ecb8d3d2eca6d1fc1d5d726cfb
Lijin Shen <lazzzis@google.com>
2022-12-01 18:54:12 +0000

    Launch InfobarScrollOptimization
    Bug: 1344189
    Change-Id: I371cdde5b8499f448180e8f80b3db9470d569c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4068719
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078182}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -796,7 +796,7 @@ BASE_FEATURE(kIncognitoScreenshot,
 
 BASE_FEATURE(kInfobarScrollOptimization,
              "InfobarScrollOptimization",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kInstantStart, "InstantStart", base::FEATURE_DISABLED_BY_DEFAULT);
 
