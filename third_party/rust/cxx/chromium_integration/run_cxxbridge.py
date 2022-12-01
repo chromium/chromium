18100eb6f0b29cac40b524b652c3eedbaf949eb4
Adrian Taylor <adetaylor@chromium.org>
2022-12-01 07:10:04 +0000

    [rust] Switch back from autocxx to cxx
    This reverts commit 3309b582b20cd1c146598304df303523cab20701, with
    modifications.
    
    For now, Chromium will be using cxx for Rust <-> C++ interop. It's
    possible that in the future we will use a scheme to generate Rust
    bindings to pre-existing C++ code (such as bindgen, autocxx, or
    rs_bindings_from_cc) but at the moment the complexity is unwarranted and
    provides a maintenance burden as we keep such tools up to date with
    clang.
    
    This is the first in multiple commits - subsequent commits will
    remove autocxx itself, but this one has all the fiddly stuff around
    .gn files.
    
    Bug: 1381552, 1386212, 1296155
    Change-Id: I731024699d9c0d638d7066965d0fa13dc52a0ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4037885
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077900}

---
+++ third_party/rust/cxx/chromium_integration/run_cxxbridge.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env vpython3
+
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+# Set up path to load build_utils.py which enables us to do
+# atomic output that's maximally compatible with ninja.
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir,
+                 os.pardir, os.pardir, os.pardir, 'build', 'android', 'gyp'))
+from util import build_utils
+
+import argparse
+import subprocess
+
+
+def run(exe, args, output, is_header):
+  cmdargs = [os.path.abspath(exe)]
+  cmdargs.extend(args)
+  if is_header:
+    cmdargs.extend(["--header"])
+  job = subprocess.run(cmdargs, capture_output=True)
+  messages = job.stderr.decode('utf-8')
+  if messages.rstrip():
+    print(messages, file=sys.stderr)
+  if job.returncode != 0:
+    return job.returncode
+  with build_utils.AtomicOutput(output) as output:
+    output.write(job.stdout)
+  return 0
+
+
+def main():
+  parser = argparse.ArgumentParser("run_cxxbridge.py")
+  parser.add_argument("--exe", help="Path to cxxbridge", required=True),
+  parser.add_argument("--cc", help="output cc file", required=True)
+  parser.add_argument("--header", help="output h file", required=True)
+  parser.add_argument('args',
+                      metavar='args',
+                      nargs='+',
+                      help="Args to pass through")
+  args = parser.parse_args()
+  v = run(args.exe, args.args, args.cc, False)
+  if v != 0:
+    return v
+  return run(args.exe, args.args, args.header, True)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
