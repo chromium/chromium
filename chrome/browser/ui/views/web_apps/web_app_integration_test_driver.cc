3e4e964229c354582ef2c5eb69b6dc6264c5fa83
Clifford Cheng <cliffordcheng@chromium.org>
2022-12-01 19:01:37 +0000

    [dpwa] Adds a callback when Mac shim has finished startup or has been cancelled.
    The action for LaunchFile is also updated to make use of this new
    callback in test.
    
    Bug: 1349953
    Change-Id: Icc733107066c5c6e85e3c24aed19096988eec3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064597
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078188}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -149,6 +149,7 @@
 #if BUILDFLAG(IS_MAC)
 #include <ImageIO/ImageIO.h>
 #include "chrome/browser/apps/app_shim/app_shim_manager_mac.h"
+#include "chrome/browser/apps/app_shim/web_app_shim_manager_delegate_mac.h"
 #include "chrome/browser/chrome_browser_main.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/web_applications/app_shim_registry_mac.h"
@@ -275,7 +276,9 @@ base::flat_map<Site, SiteConfig> g_site_configs = {
       .relative_manifest_id = "webapps_integration/file_handler/basic.html",
       .app_name = "File Handler",
       .wco_not_enabled_title = u"File Handler",
-      .icon_color = SK_ColorBLACK}},
+      .icon_color = SK_ColorBLACK,
+      .alternate_titles = {"File Handler - Text Handler",
+                           "File Handler - Image Handler"}}},
     {Site::kNoServiceWorker,
      {.relative_url = "/webapps_integration/site_no_service_worker/basic.html",
       .relative_manifest_id =
@@ -1185,9 +1188,14 @@ void WebAppIntegrationTestDriver::LaunchFileExpectDialog(
   FileHandlerLaunchDialogView::SetDefaultRememberSelectionForTesting(
       ask_again == AskAgainOptions::kRemember);
 
-  LaunchFile(site, files_options);
+  base::RunLoop run_loop;
+#if BUILDFLAG(IS_MAC)
+  web_app::SetMacShimStartupDoneCallbackForTesting(run_loop.QuitClosure());
+#else
+  web_app::startup::SetStartupDoneCallbackForTesting(run_loop.QuitClosure());
+#endif
 
-  BrowserAddedWaiter browser_added_waiter;
+  LaunchFile(site, files_options);
 
   // Check the file handling dialog shows up.
   views::Widget* widget = waiter.WaitIfNeededAndGet();
@@ -1206,13 +1214,7 @@ void WebAppIntegrationTestDriver::LaunchFileExpectDialog(
   // File handling dialog should be destroyed after choosing the action.
   widget->CloseWithReason(close_reason);
   destroyed_waiter.Wait();
-
-  if (allow_deny == AllowDenyOptions::kAllow) {
-    browser_added_waiter.Wait();
-    app_browser_ = browser_added_waiter.browser_added();
-    ActivateBrowserAndWait(app_browser_);
-    EXPECT_EQ(app_browser()->app_controller()->app_id(), app_id);
-  }
+  run_loop.Run();
   AfterStateChangeAction();
 }
 
@@ -1221,17 +1223,20 @@ void WebAppIntegrationTestDriver::LaunchFileExpectNoDialog(
     FilesOptions files_options) {
   BeforeStateChangeAction(__FUNCTION__);
   AppId app_id = GetAppIdBySiteMode(site);
-  BrowserAddedWaiter browser_added_waiter;
+  base::RunLoop run_loop;
+#if BUILDFLAG(IS_MAC)
+  web_app::SetMacShimStartupDoneCallbackForTesting(run_loop.QuitClosure());
+#else
+  web_app::startup::SetStartupDoneCallbackForTesting(run_loop.QuitClosure());
+#endif
+
   LaunchFile(site, files_options);
 
   // If the user previously denied access to open files with this app, a window
   // is still opened for the app. The only difference is that no files would
   // have been passed to the app. Either way, we should always wait for a
-  // browser to be added.
-  browser_added_waiter.Wait();
-  app_browser_ = browser_added_waiter.browser_added();
-  ActivateBrowserAndWait(app_browser_);
-  EXPECT_EQ(app_browser()->app_controller()->app_id(), app_id);
+  // window / tab to be added.
+  run_loop.Run();
 
   AfterStateChangeAction();
 }
