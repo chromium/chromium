6dd7374b58129674419671ff3f20126ebf8d2708
Rubin Deliallisi <rubindl@chromium.org>
2022-12-01 11:34:45 +0000

    Privacy Sandbox Settings: Topics page empty/disabled logic
    - Connects the empty page and disabled feature UI elements with pref toggle.
    
    Bug: 1378703, b/254412761, b/254412762
    Change-Id: I92af723a630cb7a07f088e002f07c1b79b6a0876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4061925
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1077978}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4.java
@@ -5,17 +5,21 @@
 package org.chromium.chrome.browser.privacy_sandbox.v4;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxSettingsBaseFragment;
 import org.chromium.chrome.browser.privacy_sandbox.R;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.browser_ui.settings.ChromeBasePreference;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.components.browser_ui.settings.TextMessagePreference;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
 
@@ -25,8 +29,18 @@ import org.chromium.components.user_prefs.UserPrefs;
 public class TopicsFragmentV4 extends PrivacySandboxSettingsBaseFragment
         implements Preference.OnPreferenceChangeListener {
     private static final String TOPICS_TOGGLE_PREFERENCE = "topics_toggle";
+    private static final String CURRENT_TOPICS_PREFERENCE = "current_topics";
+    private static final String EMPTY_TOPICS_PREFERENCE = "topics_empty";
+    private static final String DISABLED_TOPICS_PREFERENCE = "topics_disabled";
+    private static final String BLOCKED_TOPICS_PREFERENCE = "blocked_topics";
+    private static final String TOPICS_PAGE_FOOTER_PREFERENCE = "topics_page_footer";
 
     private ChromeSwitchPreference mTopicsTogglePreference;
+    private PreferenceCategory mCurrentTopicsPreference;
+    private TextMessagePreference mEmptyTopicsPreference;
+    private TextMessagePreference mDisabledTopicsPreference;
+    private ChromeBasePreference mBlockedTopicsPreference;
+    private TextMessagePreference mTopicsPageFooterPreference;
 
     static boolean isTopicsPrefEnabled() {
         PrefService prefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
@@ -45,18 +59,61 @@ public class TopicsFragmentV4 extends PrivacySandboxSettingsBaseFragment
         SettingsUtils.addPreferencesFromResource(this, R.xml.topics_preference_v4);
 
         mTopicsTogglePreference = findPreference(TOPICS_TOGGLE_PREFERENCE);
+        mCurrentTopicsPreference = findPreference(CURRENT_TOPICS_PREFERENCE);
+        mEmptyTopicsPreference = findPreference(EMPTY_TOPICS_PREFERENCE);
+        mDisabledTopicsPreference = findPreference(DISABLED_TOPICS_PREFERENCE);
+        mBlockedTopicsPreference = findPreference(BLOCKED_TOPICS_PREFERENCE);
+        mTopicsPageFooterPreference = findPreference(TOPICS_PAGE_FOOTER_PREFERENCE);
+
         mTopicsTogglePreference.setChecked(isTopicsPrefEnabled());
         mTopicsTogglePreference.setOnPreferenceChangeListener(this);
         // TODO(http://b/254411473): Make the preference managed.
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Disable animations of preference changes.
+        getListView().setItemAnimator(null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreferenceVisibility();
+    }
+
+    @Override
     public boolean onPreferenceChange(@NonNull Preference preference, Object value) {
         if (preference.getKey().equals(TOPICS_TOGGLE_PREFERENCE)) {
             setTopicsPrefEnabled((boolean) value);
+            updatePreferenceVisibility();
             return true;
         }
 
         return false;
+    }
+
+    private void updatePreferenceVisibility() {
+        if (!isTopicsPrefEnabled()) {
+            mDisabledTopicsPreference.setVisible(true);
+
+            mCurrentTopicsPreference.setVisible(false);
+            mEmptyTopicsPreference.setVisible(false);
+            mTopicsPageFooterPreference.setVisible(false);
+            mBlockedTopicsPreference.setDividerAllowedBelow(false);
+            return;
+        }
+
+        if (mCurrentTopicsPreference.getPreferenceCount() == 0) {
+            mEmptyTopicsPreference.setVisible(true);
+
+            mCurrentTopicsPreference.setVisible(false);
+            mDisabledTopicsPreference.setVisible(false);
+            mTopicsPageFooterPreference.setVisible(false);
+            mBlockedTopicsPreference.setDividerAllowedBelow(false);
+            return;
+        }
     }
 }
--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4Test.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/v4/TopicsFragmentV4Test.java
@@ -6,9 +6,11 @@ package org.chromium.chrome.browser.privacy_sandbox.v4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -18,8 +20,11 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import static org.chromium.ui.test.util.ViewUtils.onViewWaiting;
+
 import android.view.View;
 
+import androidx.annotation.StringRes;
 import androidx.test.filters.SmallTest;
 
 import org.hamcrest.Matcher;
@@ -30,6 +35,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -39,10 +45,13 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
-import org.chromium.ui.test.util.ViewUtils;
+import org.chromium.ui.test.util.RenderTestRule;
+
+import java.io.IOException;
 
 /**
  * Tests {@link TopicsFragmentV4}
@@ -54,6 +63,12 @@ import org.chromium.ui.test.util.ViewUtils;
 public final class TopicsFragmentV4Test {
     @Rule
     public ChromeBrowserTestRule mChromeBrowserTestRule = new ChromeBrowserTestRule();
+
+    @Rule
+    public ChromeRenderTestRule mRenderTestRule =
+            ChromeRenderTestRule.Builder.withPublicCorpus()
+                    .setBugComponent(RenderTestRule.Component.UI_SETTINGS_PRIVACY)
+                    .build();
 
     @Rule
     public SettingsActivityTestRule<TopicsFragmentV4> mSettingsActivityTestRule =
@@ -69,13 +84,20 @@ public final class TopicsFragmentV4Test {
 
     private void startTopicsSettings() {
         mSettingsActivityTestRule.startSettingsActivity();
-        ViewUtils.onViewWaiting(withText(R.string.settings_topics_page_title));
+        onViewWaiting(withText(R.string.settings_topics_page_title));
     }
 
     private Matcher<View> getTopicsToggleMatcher() {
         return allOf(withId(R.id.switchWidget),
                 withParent(withParent(
                         hasDescendant(withText(R.string.settings_topics_page_toggle_label)))));
+    }
+
+    private View getRootView(@StringRes int text) {
+        View[] view = {null};
+        onView(withText(text)).check(((v, e) -> view[0] = v.getRootView()));
+        TestThreadUtils.runOnUiThreadBlocking(() -> RenderTestRule.sanitize(view[0]));
+        return view[0];
     }
 
     private void setTopicsPrefEnabled(boolean isEnabled) {
@@ -86,6 +108,25 @@ public final class TopicsFragmentV4Test {
     private boolean isTopicsPrefEnabled() {
         return TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> TopicsFragmentV4.isTopicsPrefEnabled());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"RenderTest"})
+    public void testRenderTopicsOff() throws IOException {
+        setTopicsPrefEnabled(false);
+        startTopicsSettings();
+        mRenderTestRule.render(getRootView(R.string.settings_topics_page_title), "topics_page_off");
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"RenderTest"})
+    public void testRenderTopicsEmpty() throws IOException {
+        setTopicsPrefEnabled(true);
+        startTopicsSettings();
+        mRenderTestRule.render(
+                getRootView(R.string.settings_topics_page_title), "topics_page_empty");
     }
 
     @Test
@@ -110,7 +151,12 @@ public final class TopicsFragmentV4Test {
         setTopicsPrefEnabled(false);
         startTopicsSettings();
         onView(getTopicsToggleMatcher()).perform(click());
+
         assertTrue(isTopicsPrefEnabled());
+        onViewWaiting(withText(R.string.settings_topics_page_current_topics_description_empty))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.settings_topics_page_current_topics_description_disabled))
+                .check(doesNotExist());
     }
 
     @Test
@@ -119,6 +165,11 @@ public final class TopicsFragmentV4Test {
         setTopicsPrefEnabled(true);
         startTopicsSettings();
         onView(getTopicsToggleMatcher()).perform(click());
+
         assertFalse(isTopicsPrefEnabled());
+        onViewWaiting(withText(R.string.settings_topics_page_current_topics_description_disabled))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.settings_topics_page_current_topics_description_empty))
+                .check(doesNotExist());
     }
 }
