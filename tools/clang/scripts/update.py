db5d0a9e9f93ca3eda271bde74355986c50f273e
Zequan Wu <zequanwu@google.com>
2022-12-01 19:46:52 +0000

    Roll clang llvmorg-16-init-10736-ged9638c4-1 : llvmorg-16-init-12251-g87d0ff91-1
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/+log/ed9638c4..87d0ff91
    
    Ran `tools/clang/scripts/upload_revision.py 87d0ff91176221894604aa07fc0e2b8492140f7d`.
    
    Bug: 1394139
    Change-Id: Id2493976b58678782680abe246508b38bdc99438
    Cq-Include-Trybots: chromium/try:android-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst
    Cq-Include-Trybots: chromium/try:win-asan
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063683
    Auto-Submit: Zequan Wu <zequanwu@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078222}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,7 +35,7 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-16-init-10736-ged9638c4'
+CLANG_REVISION = 'llvmorg-16-init-12251-g87d0ff91'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
