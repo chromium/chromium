472297a091b35e91e270449e16e749822130e627
Fredrik Söderquist <fs@opera.com>
2022-12-01 09:58:33 +0000

    Perform source selection when adding/remove an <img> to a <picture>
    When an <img> is removed from a <picture> parent, or when it is added to
    a <picture> parent, a source selection should take place, since these
    count as "relevant mutations" [1].
    
    For the "insert" case, only <source> elements were considered previously.
    For the "remove" case no selection was trigger at all.
    
    Change the "insert" case to call SelectSourceURL(), and add handling in
    the "remove" case to do the same.
    
    In HTMLImageElement::RemovedFrom(), also move the unregistration of
    listeners from the <source>s to be symmetric with
    HTMLImageElement::InsertedInto() - i.e always do it if there is/was a
    <picture> parent.
    
    [1] https://html.spec.whatwg.org/multipage/embedded-content.html#the-img-element:html-element-insertion-steps
    
    Fixed: 1393688
    Change-Id: I7dcab791c8bff71149ec449106f9e25519ff3706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4064460
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1077940}

--- third_party/blink/renderer/core/html/html_image_element.cc
+++ third_party/blink/renderer/core/html/html_image_element.cc
@@ -505,18 +505,10 @@ Node::InsertionNotificationRequest HTMLImageElement::InsertedInto(
     was_added_to_picture_parent = picture_parent == insertion_point;
   }
 
-  bool image_was_modified = false;
-  if (GetDocument().IsActive() && was_added_to_picture_parent) {
-    ImageCandidate candidate = FindBestFitImageFromPictureParent();
-    if (!candidate.IsEmpty()) {
-      InvalidateAttributeMapping();
-      SetBestFitURLAndDPRFromImageCandidate(candidate);
-      image_was_modified = true;
-    }
-  }
-
-  if (image_was_modified || GetImageLoader().ShouldUpdateOnInsertedInto(
-                                insertion_point, referrer_policy_)) {
+  if (was_added_to_picture_parent) {
+    SelectSourceURL(ImageLoader::kUpdateIgnorePreviousError);
+  } else if (GetImageLoader().ShouldUpdateOnInsertedInto(insertion_point,
+                                                         referrer_policy_)) {
     GetImageLoader().UpdateFromElement(ImageLoader::kUpdateNormal,
                                        referrer_policy_);
   }
@@ -524,14 +516,18 @@ Node::InsertionNotificationRequest HTMLImageElement::InsertedInto(
 }
 
 void HTMLImageElement::RemovedFrom(ContainerNode& insertion_point) {
-  InvalidateAttributeMapping();
   if (!form_ || NodeTraversal::HighestAncestorOrSelf(*form_.Get()) !=
                     NodeTraversal::HighestAncestorOrSelf(*this))
     ResetFormOwner();
-  if (listener_) {
+  if (listener_)
     GetDocument().GetMediaQueryMatcher().RemoveViewportListener(listener_);
-    if (auto* picture_parent = DynamicTo<HTMLPictureElement>(parentNode()))
-      picture_parent->RemoveListenerFromSourceChildren();
+  bool was_removed_from_parent = !parentNode();
+  auto* picture_parent = DynamicTo<HTMLPictureElement>(
+      was_removed_from_parent ? &insertion_point : parentNode());
+  if (picture_parent) {
+    picture_parent->RemoveListenerFromSourceChildren();
+    if (was_removed_from_parent)
+      SelectSourceURL(ImageLoader::kUpdateIgnorePreviousError);
   }
   HTMLElement::RemovedFrom(insertion_point);
 }
