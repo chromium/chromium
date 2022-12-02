c581210ab4ec772e7f3e2894dcf1a51d3c28e4cd
Stepan Khapugin <stkhapugin@chromium.org>
2022-12-02 16:50:00 +0000

    [iOS] Reenable testEmptyOmnibox.
    Reenable this test.
    
    Bug: 1243837
    Change-Id: Ib18c6afd1e26310dbdb93889302a5ffd7fa3d98e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066657
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1078633}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -813,8 +813,7 @@ void FocusFakebox() {
 // displayed. Paste button should be hidden when pasteboard is empty otherwise
 // it should be displayed. Select & SelectAll buttons should be hidden when the
 // omnibox is empty.
-// TODO(crbug.com/1209342): test failing on device
-- (void)DISABLED_testEmptyOmnibox {
+- (void)testEmptyOmnibox {
   // Focus omnibox.
   [self focusFakebox];
   [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
