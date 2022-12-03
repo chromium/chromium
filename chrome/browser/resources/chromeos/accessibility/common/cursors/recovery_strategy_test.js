0e2855c6e8740ad9716cd54388c9f0f86decf647
Anastasia Helfinstein <anastasi@google.com>
2022-12-03 02:59:35 +0000

    [a11y-extensions] Move RecoveryStrategyTest to use common base
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:260599332
    Test: cq
    Change-Id: Iccd87c47b27dcd80d56e3ac6d13c9fc144720065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076946
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1078894}

--- chrome/browser/resources/chromeos/accessibility/common/cursors/recovery_strategy_test.js
+++ chrome/browser/resources/chromeos/accessibility/common/cursors/recovery_strategy_test.js
@@ -3,19 +3,12 @@
 // found in the LICENSE file.
 
 // Include test fixture.
-GEN_INCLUDE([
-  '//chrome/browser/resources/chromeos/accessibility/chromevox/testing/chromevox_next_e2e_test_base.js',
-]);
+GEN_INCLUDE(['../testing/common_e2e_test_base.js']);
 
 /**
  * Test fixture for recovery strategy tests.
  */
-AccessibilityExtensionRecoveryStrategyTest =
-    class extends ChromeVoxNextE2ETest {
-  constructor() {
-    super();
-  }
-
+AccessibilityExtensionRecoveryStrategyTest = class extends CommonE2ETestBase {
   /** @override */
   async setUpDeferred() {
     await super.setUpDeferred();
@@ -26,6 +19,7 @@ AccessibilityExtensionRecoveryStrategyTest =
           'TreePathRecoveryStrategy',
         ],
         '/common/cursors/recovery_strategy.js');
+    globalThis.RoleType = chrome.automation.RoleType;
   }
 };
 
