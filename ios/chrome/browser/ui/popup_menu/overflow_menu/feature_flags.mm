a357b5bebcd87fd70f4395f6fb14247f0b796efd
Benjamin Williams <bwwilliams@google.com>
2022-12-05 20:52:49 +0000

    [ios] Make kSmartSortingNewOverflowMenu FEATURE_ENABLED_BY_DEFAULT
    go/launch/4200850
    
    Change-Id: I3147abb8910b169a2336b6ff7739a53646365d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4072505
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1079387}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.mm
@@ -16,7 +16,7 @@ BASE_FEATURE(kNewOverflowMenu,
 
 BASE_FEATURE(kSmartSortingNewOverflowMenu,
              "kSmartSortingNewOverflowMenu",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNewOverflowMenuShareChromeAction,
              "kNewOverflowMenuShareChromeAction",
