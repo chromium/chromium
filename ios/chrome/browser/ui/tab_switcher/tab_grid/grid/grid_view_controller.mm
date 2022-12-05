92b0e2d8a1b7c482a4f3f4d979f97c2a64b8a7a1
Gauthier Ambard <gambard@chromium.org>
2022-12-05 17:47:11 +0000

    [iOS] Don't reload the TabGrid when entering edit
    When the user enters the mode to select tabs in bulk, the animation
    should not reload the section as this is creating a visible flash
    especially when entering it through the context menu.
    
    To avoid that, change the state of the cells directly.
    
    Fixed: b/260197392
    Change-Id: I5c1fdbd8ba57ff7addfc97e07ef8b71ff508f572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4079013
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079268}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -309,7 +309,9 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     return;
   }
 
+  TabGridMode previousMode = _mode;
   _mode = mode;
+
   // TODO(crbug.com/1300369): Enable dragging items from search results.
   self.collectionView.dragInteractionEnabled = (_mode != TabGridModeSearch);
   self.emptyStateView.tabGridMode = _mode;
@@ -332,21 +334,36 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
           return;
         }
 
-        NSRange allSectionsRange = NSMakeRange(
-            /*location=*/0, strongSelf.collectionView.numberOfSections);
-        NSIndexSet* allSectionsIndexSet =
-            [NSIndexSet indexSetWithIndexesInRange:allSectionsRange];
-        [strongSelf.collectionView reloadSections:allSectionsIndexSet];
-        // Scroll to the selected item here, so the animation of reloading and
-        // scrolling happens at once.
+        if (mode == TabGridModeSelection && previousMode == TabGridModeNormal) {
+          // If the grid is switching from normal to selected state don't
+          // reload the whole table view to avoid having a flash, particularly
+          // visible when using context menu.
+          for (UITableViewCell* cell in strongSelf.collectionView
+                   .visibleCells) {
+            GridCell* gridCell = base::mac::ObjCCast<GridCell>(cell);
+            gridCell.state = mode == TabGridModeSelection
+                                 ? GridCellStateEditingUnselected
+                                 : GridCellStateNotEditing;
+          }
+        } else {
+          NSRange allSectionsRange = NSMakeRange(
+              /*location=*/0, strongSelf.collectionView.numberOfSections);
+          NSIndexSet* allSectionsIndexSet =
+              [NSIndexSet indexSetWithIndexesInRange:allSectionsRange];
+          [strongSelf.collectionView reloadSections:allSectionsIndexSet];
+        }
         NSUInteger selectedIndex = strongSelf.selectedIndex;
-        if (mode == TabGridModeNormal && selectedIndex != NSNotFound) {
+        if (previousMode != TabGridModeSelection && mode == TabGridModeNormal &&
+            selectedIndex != NSNotFound) {
+          // Scroll to the selected item here, so the animation of reloading and
+          // scrolling happens at once.
           [strongSelf.collectionView
               scrollToItemAtIndexPath:CreateIndexPath(selectedIndex)
                      atScrollPosition:UICollectionViewScrollPositionTop
                              animated:NO];
         }
       }
+
                completion:nil];
 
   if (mode == TabGridModeNormal) {
