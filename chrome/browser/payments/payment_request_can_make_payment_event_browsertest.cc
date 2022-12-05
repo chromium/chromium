3340ad8825f377d097c50edb01bd04efdbabf580
Rouslan Solomakhin <rouslan@chromium.org>
2022-12-05 20:43:32 +0000

    [Web Payment] Use test installer for "canmakepayment" event test
    Before this patch, the browsertest for "canmakepayment" event installed
    payment apps using the PaymentInstruments API that is planned to be
    removed. The test also assigned multiple payment method identifiers to
    each payment app, unlike the preferred JIT (just-in-time) installer that
    always assigns exactly one payment method identifier to each payment
    app.
    
    This patch changes the "canmakepayment" event test to use the test
    installer and to assign exactly one payment method to each payment app,
    identical to how JIT payment app installer works.
    
    Bug: 1327265
    Change-Id: I7fc87d9a2da3755329e64aa85b2939e6f0e63637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4076486
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079374}

--- chrome/browser/payments/payment_request_can_make_payment_event_browsertest.cc
+++ chrome/browser/payments/payment_request_can_make_payment_event_browsertest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/test/payments/payment_app_install_util.h"
 #include "chrome/test/payments/payment_request_platform_browsertest_base.h"
 #include "content/public/test/browser_test.h"
 
@@ -10,37 +11,18 @@
 
 namespace payments {
 
-class PaymentRequestCanMakePaymentEventTest
-    : public PaymentRequestPlatformBrowserTestBase {};
+using PaymentRequestCanMakePaymentEventTest =
+    PaymentRequestPlatformBrowserTestBase;
 
-// A payment handler with two standardized payment methods ("interledger" and
-// "basic-card") and one URL-based payment method (its own scope) does not
+// A payment handler with one URL-based payment method (its own origin) does not
 // receive a "canmakepayment" event from a PaymentRequest for "interledger"
 // payment method.
-IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentEventTest,
-                       TwoStandardOneUrl) {
-  NavigateTo("a.com", "/payment_handler_installer.html");
-  EXPECT_EQ("success",
-            content::EvalJs(GetActiveWebContents(),
-                            "install('can_make_payment_true_responder.js', "
-                            "['interledger', 'basic-card'], true)"));
+IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentEventTest, OneUrl) {
+  std::string method;
+  InstallPaymentApp("a.com", "/can_make_payment_true_responder.js", &method);
+  EXPECT_EQ(0U, method.find("https://"));
+
   NavigateTo("b.com", "/has_enrolled_instrument_checker.html");
-
-  EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(),
-                                     "hasEnrolledInstrument('interledger')"));
-}
-
-// A payment handler with two standardized payment methods ("interledger" and
-// "basic-card") does not receive a "canmakepayment" event from a PaymentRequest
-// for "interledger" payment method.
-IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentEventTest, TwoStandard) {
-  NavigateTo("a.com", "/payment_handler_installer.html");
-  EXPECT_EQ("success",
-            content::EvalJs(GetActiveWebContents(),
-                            "install('can_make_payment_true_responder.js', "
-                            "['interledger', 'basic-card'], false)"));
-  NavigateTo("b.com", "/has_enrolled_instrument_checker.html");
-
   EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(),
                                      "hasEnrolledInstrument('interledger')"));
 }
@@ -49,13 +31,14 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentEventTest, TwoStandard) {
 // not receive a "canmakepayment" event from a PaymentRequest for "interledger"
 // payment method.
 IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentEventTest, OneStandard) {
-  NavigateTo("a.com", "/payment_handler_installer.html");
-  EXPECT_EQ("success",
-            content::EvalJs(GetActiveWebContents(),
-                            "install('can_make_payment_true_responder.js', "
-                            "['interledger'], false)"));
-  NavigateTo("b.com", "/has_enrolled_instrument_checker.html");
+  ASSERT_TRUE(
+      PaymentAppInstallUtil::InstallPaymentAppForPaymentMethodIdentifier(
+          *GetActiveWebContents(),
+          https_server()->GetURL("a.com",
+                                 "/can_make_payment_true_responder.js"),
+          "interledger", PaymentAppInstallUtil::IconInstall::kWithIcon));
 
+  NavigateTo("b.com", "/has_enrolled_instrument_checker.html");
   EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(),
                                      "hasEnrolledInstrument('interledger')"));
 }
