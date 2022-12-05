7e40330a51c22aaca872164fa663383980030305
Eric Willigers <ericwilligers@chromium.org>
2022-12-05 02:01:39 +0000

    Lacros web apps: Test launching in a tab
    We verify that
    - web apps launched in browser tabs do not appear in the Shelf.
    - web apps launched in windows do appear in the Shelf.
    
    Bug: 1208258
    Change-Id: Iff583c2d17578f4e4a62d78c88502d90034c4dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074453
    Commit-Queue: Louise Brett <loubrett@google.com>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Louise Brett <loubrett@google.com>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079055}

--- chrome/browser/ui/web_applications/lacros_web_app_shelf_browsertest.cc
+++ chrome/browser/ui/web_applications/lacros_web_app_shelf_browsertest.cc
@@ -2,19 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/lacros/browser_test_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/ui/web_applications/test/web_app_navigation_browsertest.h"
 #include "chrome/browser/web_applications/test/app_registry_cache_waiter.h"
+#include "chrome/browser/web_applications/test/service_worker_registration_waiter.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/test/web_app_test_observers.h"
 #include "chrome/browser/web_applications/user_display_mode.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/crosapi/mojom/test_controller.mojom-test-utils.h"
 #include "chromeos/crosapi/mojom/test_controller.mojom.h"
 #include "chromeos/lacros/lacros_service.h"
@@ -22,6 +34,7 @@
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
 using crosapi::mojom::ShelfItemState;
@@ -30,6 +43,23 @@ namespace {
 
 constexpr char kFirstAppUrlHost[] = "first-pwa.test";
 constexpr char kSecondAppUrlHost[] = "second-pwa.test";
+
+// Polls until the command has the expected state.
+void WaitForAppMenuCommandState(int command_id,
+                                Browser* browser,
+                                web_app::AppMenuCommandState command_state) {
+  base::RunLoop run_loop;
+  base::RepeatingTimer timer;
+  constexpr base::TimeDelta kPollingInterval = base::Milliseconds(1000);
+  timer.Start(FROM_HERE, kPollingInterval, base::BindLambdaForTesting([&]() {
+                if (web_app::GetAppMenuCommandState(command_id, browser) ==
+                    command_state) {
+                  timer.Stop();
+                  run_loop.Quit();
+                }
+              }));
+  run_loop.Run();
+}
 
 }  // namespace
 
@@ -205,6 +235,119 @@ IN_PROC_BROWSER_TEST_F(LacrosWebAppShelfBrowserTest, RunningInTab) {
 
   test::UninstallWebApp(profile(), app1_id);
   test::UninstallWebApp(profile(), app2_id);
+}
+
+// Tests that a web page without a manifest may be used to create a shortcut.
+// Tests that a web page with a manifest etc. may be used to install a PWA.
+// Tests that opening a shortcut in a tab does not make it appear in the Shelf.
+// Tests that web apps opened in windows do appear in the Shelf.
+IN_PROC_BROWSER_TEST_F(LacrosWebAppShelfBrowserTest, CreateShortcut) {
+  if (!IsServiceAvailable())
+    GTEST_SKIP();
+
+  ASSERT_TRUE(embedded_test_server()->Start());
+  crosapi::mojom::TestController* const test_controller =
+      chromeos::LacrosService::Get()
+          ->GetRemote<crosapi::mojom::TestController>()
+          .get();
+  auto& sync_bridge = WebAppProvider::GetForTest(profile())->sync_bridge();
+
+  GURL app1_url(
+      embedded_test_server()->GetURL("/banners/scope_a/no_manifest.html"));
+  GURL app2_url(
+      embedded_test_server()->GetURL("/banners/scope_b/scope_b.html"));
+  AppId app1_id;
+  Browser* app1_browser;
+  {
+    web_app::ServiceWorkerRegistrationWaiter registration_waiter(profile(),
+                                                                 app1_url);
+    ASSERT_TRUE(
+        AddTabAtIndex(/*index=*/1, app1_url, ui::PAGE_TRANSITION_TYPED));
+    registration_waiter.AwaitRegistration();
+
+    ASSERT_TRUE(
+        AddTabAtIndex(/*index=*/2, app2_url, ui::PAGE_TRANSITION_TYPED));
+    WaitForAppMenuCommandState(IDC_INSTALL_PWA, browser(), kEnabled);
+
+    // Install app1 shortcut.
+    browser()->tab_strip_model()->ActivateTabAt(/*index=*/1);
+    EXPECT_EQ(GetAppMenuCommandState(IDC_CREATE_SHORTCUT, browser()), kEnabled);
+    EXPECT_EQ(GetAppMenuCommandState(IDC_INSTALL_PWA, browser()), kNotPresent);
+
+    chrome::SetAutoAcceptWebAppDialogForTesting(
+        /*auto_accept=*/true,
+        /*auto_open_in_window=*/true);
+    ui_test_utils::BrowserChangeObserver browser_change_observer(
+        nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
+    WebAppTestInstallObserver install_observer(profile());
+    install_observer.BeginListening();
+    CHECK(chrome::ExecuteCommand(browser(), IDC_CREATE_SHORTCUT));
+    app1_id = install_observer.Wait();
+    app1_browser = browser_change_observer.Wait();
+    EXPECT_TRUE(AppBrowserController::IsForWebApp(app1_browser, app1_id));
+    chrome::SetAutoAcceptWebAppDialogForTesting(
+        /*auto_accept=*/false,
+        /*auto_open_in_window=*/false);
+  }
+
+  ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+      app1_id, static_cast<uint32_t>(ShelfItemState::kActive)));
+  ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+      app_constants::kLacrosAppId,
+      static_cast<uint32_t>(ShelfItemState::kRunning)));
+
+  // Launch app1 in a browser tab (only).
+  {
+    sync_bridge.SetAppUserDisplayMode(app1_id, UserDisplayMode::kBrowser,
+                                      /*is_user_action=*/false);
+    AppWindowModeWaiter(profile(), app1_id, apps::WindowMode::kBrowser).Await();
+
+    app1_browser->window()->Close();
+
+    ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+        app1_id, static_cast<uint32_t>(ShelfItemState::kNormal)));
+
+    ui_test_utils::TabAddedWaiter waiter(browser());
+    test_controller->LaunchAppFromAppList(app1_id);
+    waiter.Wait();
+  }
+
+  // Install app2 PWA.
+  AppId app2_id;
+  Browser* app2_browser;
+  {
+    browser()->tab_strip_model()->ActivateTabAt(/*index=*/1);
+    EXPECT_EQ(GetAppMenuCommandState(IDC_CREATE_SHORTCUT, browser()), kEnabled);
+    EXPECT_EQ(GetAppMenuCommandState(IDC_INSTALL_PWA, browser()), kEnabled);
+
+    chrome::SetAutoAcceptPWAInstallConfirmationForTesting(/*auto_accept=*/true);
+    ui_test_utils::BrowserChangeObserver browser_change_observer(
+        nullptr, ui_test_utils::BrowserChangeObserver::ChangeType::kAdded);
+    WebAppTestInstallObserver observer(profile());
+    observer.BeginListening();
+    CHECK(chrome::ExecuteCommand(browser(), IDC_INSTALL_PWA));
+    app2_id = observer.Wait();
+    app2_browser = browser_change_observer.Wait();
+    EXPECT_TRUE(AppBrowserController::IsForWebApp(app2_browser, app2_id));
+    chrome::SetAutoAcceptPWAInstallConfirmationForTesting(
+        /*auto_accept=*/false);
+  }
+
+  // App1 is open in a tab, but does not appear in the shelf.
+  EXPECT_EQ(
+      browser()->tab_strip_model()->GetActiveWebContents()->GetVisibleURL(),
+      app1_url);
+  EXPECT_EQ(
+      app2_browser->tab_strip_model()->GetActiveWebContents()->GetVisibleURL(),
+      app2_url);
+  ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+      app2_id, static_cast<uint32_t>(ShelfItemState::kActive)));
+  ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+      app1_id, static_cast<uint32_t>(ShelfItemState::kNormal)));
+  ASSERT_TRUE(browser_test_util::WaitForShelfItemState(
+      app_constants::kLacrosAppId,
+      static_cast<uint32_t>(ShelfItemState::kRunning)));
+  ASSERT_TRUE(browser_test_util::WaitForShelfItem(app1_id, /*exists=*/false));
 }
 
 }  // namespace web_app
