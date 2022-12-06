5bf8b001af6ea9413037fabff149c30679a91290
Maksim Sisov <msisov@igalia.com>
2022-12-06 16:37:57 +0000

    Reland "Reland "Reland "ozone/wayland: enable ANGLE.""""
    This is a reland of commit 96a9f0a33fd44716a6581a39f632c40e4330d26c
    
    Original change's description:
    > Reland "Reland "ozone/wayland: enable ANGLE."""
    >
    > Another try to land enable angle after some fixes.
    >
    > Bug: 1231934
    > Change-Id: I66b18f67bb70c3ae56a2cf9aeaebc48e05448fdf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3467075
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Commit-Queue: Maksim Sisov <msisov@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1016291}
    
    Bug: 1231934
    Change-Id: I0394e860a19469dbb5900d83b8b52daeaf43ea1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3732707
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079808}

--- ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
+++ ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
@@ -16,6 +16,7 @@
 #ifndef EGL_ANGLE_platform_angle
 #define EGL_ANGLE_platform_angle 1
 #define EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE 0x348F
+#define EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE 0x3209
 #endif /* EGL_ANGLE_platform_angle */
 
 #ifndef EGL_ANGLE_platform_angle_vulkan
@@ -23,9 +24,21 @@
 #define EGL_PLATFORM_VULKAN_DISPLAY_MODE_HEADLESS_ANGLE 0x34A5
 #endif /* EGL_ANGLE_platform_angle_vulkan */
 
+#ifndef EGL_ANGLE_platform_angle_device_type_egl_angle
+#define EGL_ANGLE_platform_angle_device_type_egl_angle
+#define EGL_PLATFORM_ANGLE_DEVICE_TYPE_EGL_ANGLE 0x348E
+#endif /* EGL_ANGLE_platform_angle_device_type_egl_angle */
+
+#ifndef EGL_ANGLE_platform_angle_opengl
+#define EGL_ANGLE_platform_angle_opengl 1
+#define EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE 0x320D
+#define EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE 0x320E
+#endif /* EGL_ANGLE_platform_angle_opengl */
+
 namespace ui {
 
 WaylandGLEGLUtility::WaylandGLEGLUtility() = default;
+
 WaylandGLEGLUtility::~WaylandGLEGLUtility() = default;
 
 void WaylandGLEGLUtility::GetAdditionalEGLAttributes(
@@ -37,6 +50,17 @@ void WaylandGLEGLUtility::GetAdditionalEGLAttributes(
         EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE);
     display_attributes->push_back(
         EGL_PLATFORM_VULKAN_DISPLAY_MODE_HEADLESS_ANGLE);
+    return;
+  }
+
+  if (std::find(display_attributes->begin(), display_attributes->end(),
+                EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE) !=
+          display_attributes->end() ||
+      std::find(display_attributes->begin(), display_attributes->end(),
+                EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE) !=
+          display_attributes->end()) {
+    display_attributes->push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE);
+    display_attributes->push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_EGL_ANGLE);
     return;
   }
 }
--- ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
+++ ui/ozone/platform/wayland/gpu/wayland_surface_factory.cc
@@ -196,8 +196,14 @@ WaylandSurfaceFactory::GetAllowedGLImplementations() {
   if (egl_implementation_) {
     impls.emplace_back(
         gl::GLImplementationParts(gl::kGLImplementationEGLGLES2));
+    // Add only supported ANGLE implementations. Otherwise, angle-vulkan might
+    // be requested, which is not supported with this backend yet.
     impls.emplace_back(
         gl::GLImplementationParts(gl::ANGLEImplementation::kSwiftShader));
+    impls.emplace_back(
+        gl::GLImplementationParts(gl::ANGLEImplementation::kOpenGL));
+    impls.emplace_back(
+        gl::GLImplementationParts(gl::ANGLEImplementation::kOpenGLES));
   }
   return impls;
 }
