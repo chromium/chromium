f25c309122d81bde637efa495cc024aa916fdaa5
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-12-06 00:16:02 +0000

    Import wpt@fef88e5fc084a92a2b5e8d196e4ce15694320bb8
    Using wpt-import in Chromium e05f074ee710b56e2ff9c044e062808e754aebd2.
    With Chromium commits locally applied on WPT:
    bb25221bd7 "VT: Fix inline with filter overflow calculation."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I8d086a59c41b55a7d53d249979dd455224a49665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080313
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1079543}

--- third_party/blink/web_tests/external/wpt/fetch/orb/resources/utils.js
+++ third_party/blink/web_tests/external/wpt/fetch/orb/resources/utils.js
@@ -6,6 +6,10 @@ function contentType(type) {
   return header("Content-Type", type);
 }
 
+function contentTypeOptions(type) {
+  return header("X-Content-Type-Options", type);
+}
+
 function fetchORB(file, options, ...pipe) {
   return fetch(`${file}${pipe.length ? `?pipe=${pipe.join("|")}` : ""}`, {
     ...(options || {}),
