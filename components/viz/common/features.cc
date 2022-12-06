69e176e453d804ac5e8fca407b192198e40e93a8
Peter McNeeley <petermcneeley@chromium.org>
2022-12-06 19:33:07 +0000

    Enable Overlay prioritization by default
    Feature is still behind a flag so emergency disable is possible.
    
    Change-Id: I534b7486a18e647a37645897f202f1bd7a199067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4063624
    Reviewed-by: Kevin Haslett <khaslett@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1079913}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -35,12 +35,7 @@ namespace features {
 
 BASE_FEATURE(kEnableOverlayPrioritization,
              "EnableOverlayPrioritization",
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseMultipleOverlays,
              "UseMultipleOverlays",
@@ -241,12 +236,7 @@ BASE_FEATURE(kRendererAllocatesImages,
 );
 
 bool IsOverlayPrioritizationEnabled() {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  // DelegatedCompositing in Lacros makes this feature a no-op.
-  return false;
-#else
   return base::FeatureList::IsEnabled(kEnableOverlayPrioritization);
-#endif
 }
 
 bool IsDelegatedCompositingEnabled() {
