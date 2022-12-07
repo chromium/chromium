9179e85e0f4b379bfc08dae1016587aad250075e
Noah Rose Ledesma <noahrose@google.com>
2022-12-07 22:10:40 +0000

    Implement activity bits for Linux user-scope
    Support activity bits by providing implementations for the stubbed out
    util functions. On Linux it is not safe to assume that all of the home
    directories for users on a system are subdirectories of /home/. Thus,
    a system-scope implementation will require more research.
    
    Bug: 1384968
    Change-Id: Ibb8bc2f123502d856194fe24421e9f8942cc84d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4080203
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080592}

--- chrome/updater/activity_impl_util_linux.cc
+++ chrome/updater/activity_impl_util_linux.cc
@@ -8,19 +8,36 @@
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/notreached.h"
+#include "chrome/updater/updater_branding.h"
 #include "chrome/updater/updater_scope.h"
 
 namespace updater {
 std::vector<base::FilePath> GetHomeDirPaths(UpdaterScope scope) {
-  NOTREACHED();
+  switch (scope) {
+    case UpdaterScope::kUser: {
+      const base::FilePath path = base::GetHomeDir();
+      if (path.empty()) {
+        return {};
+      }
+      return {path};
+    }
+    case UpdaterScope::kSystem: {
+      // TODO(crbug.com/c/1384968): Implement activity bits for system-scope.
+      NOTIMPLEMENTED();
+      return {};
+    }
+  }
   return {};
 }
 
 base::FilePath GetActiveFile(const base::FilePath& home_dir,
                              const std::string& id) {
-  NOTREACHED();
-  return base::FilePath();
+  return home_dir.AppendASCII(".local")
+      .AppendASCII(COMPANY_SHORTNAME_STRING)
+      .AppendASCII(PRODUCT_FULLNAME_STRING)
+      .AppendASCII("Actives")
+      .AppendASCII(id);
 }
-
 }  // namespace updater
