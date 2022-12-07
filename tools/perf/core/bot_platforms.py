8d6a5e4b691c356ebdbc2f7caa087265592ca48b
Rohan Pavone <rohpavone@chromium.org>
2022-12-07 21:17:32 +0000

    [fuchsia][perf] Remove media.desktop from Atlas.
    These tests are failing due to a Flatland disconnect. Not sure why,
    but disabling for now to get the builder green.
    
    Change-Id: Ic2ef4497e566c88e54b9ea74b7639e3a3c5ba680
    Bug: 1399288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085248
    Quick-Run: Rohan Pavone <rohpavone@chromium.org>
    Auto-Submit: Rohan Pavone <rohpavone@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080554}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -508,7 +508,6 @@ _FUCHSIA_SHERLOCK_PERF_FYI_BENCHMARK_CONFIGS = PerfSuite([
 _FUCHSIA_ATLAS_PERF_FYI_BENCHMARK_CONFIGS = PerfSuite([
     _GetBenchmarkConfig('system_health.common_desktop'),
     _GetBenchmarkConfig('power.desktop'),
-    _GetBenchmarkConfig('media.desktop'),
     _GetBenchmarkConfig('speedometer'),
     _GetBenchmarkConfig('speedometer2'),
     _GetBenchmarkConfig('jetstream'),
