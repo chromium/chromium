da60360cb958cc6d236f0936e48a6f77938d9d18
Chu-Hsuan Yang <chuhsuan@chromium.org>
2022-12-07 04:45:53 +0000

    CCA: Use default error stack string
    Remove the code for parsing and formatting error stacks. Use default
    error stack from v8.
    
    Bug: None
    Test: Trigger uncaught errors and rejections, watch `reportError` and logs in console.
    Change-Id: Ib2fc69c5da65cbdb27c4f5262f7bb4cc5956ffec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4074348
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080174}

--- ash/webui/camera_app_ui/resources/js/error.ts
+++ ash/webui/camera_app_ui/resources/js/error.ts
@@ -21,96 +21,15 @@ export interface StackFrame {
 
 const PRODUCT_NAME = 'ChromeOS_CameraApp';
 
-/**
- * Converts v8 CallSite object to StackFrame.
- */
-function toStackFrame(callsite: CallSite): StackFrame {
-  // TODO(crbug.com/1072700): Handle native frame.
-  let fileName = callsite.getFileName() ?? 'unknown';
-  if (fileName.startsWith(window.location.origin)) {
-    fileName = fileName.substring(window.location.origin.length + 1);
-  }
-  function ensureNumber(n: number|undefined) {
-    return n === undefined ? -1 : n;
-  }
+function parseTopFrameInfo(stackTrace: string): StackFrame {
+  const regex = /at (\[?\w+\]? |)\(?(.+):(\d+):(\d+)/;
+  const match = stackTrace.match(regex) ?? ['', '', '', '-1', '-1'] as const;
   return {
-    fileName,
-    funcName: callsite.getFunctionName() ?? '[Anonymous]',
-    lineNo: ensureNumber(callsite.getLineNumber()),
-    colNo: ensureNumber(callsite.getColumnNumber()),
+    funcName: match[1].trim(),
+    fileName: match[2],
+    lineNo: Number(match[3]),
+    colNo: Number(match[4]),
   };
-}
-
-function parseStackTrace(stackTrace: string): StackFrame[] {
-  const regex = /at (\[?\w+\]? )?\(?(.+):(\d+):(\d+)/g;
-  const frames: StackFrame[] = [];
-  for (const m of stackTrace.matchAll(regex)) {
-    frames.push({
-      funcName: m[1]?.trim() ?? '',
-      fileName: m[2],
-      lineNo: Number(m[3]),
-      colNo: Number(m[4]),
-    });
-  }
-  return frames;
-}
-
-/**
- * Gets stack frames from error.
- *
- * @return Return null if failed to get frames from error.
- */
-function getStackFrames(error: Error): StackFrame[] {
-  const prevPrepareStackTrace = Error.prepareStackTrace;
-  Error.prepareStackTrace = (_error, stack) => {
-    try {
-      return stack.map(toStackFrame);
-    } catch (e) {
-      console.warn('Failed to prepareStackTrace', e);
-      return [];
-    }
-  };
-
-  let frames: StackFrame[] = [];
-  if (typeof error.stack === 'string') {
-    // TODO(b/223324206): There is a known issue that when reporting error from
-    // intent instance, the type from |error.stack| will be a string instead.
-    frames = parseStackTrace(error.stack);
-  } else if (Array.isArray(error.stack)) {
-    // Generally, error.stack returns whatever Error.prepareStackTrace returns.
-    // Since we override Error.prepareStackTrace to return StackFrame[] here,
-    // using "as unknown" first so that we can cast the type to StackFrame[].
-    frames = error.stack as unknown as StackFrame[];
-  }
-  Error.prepareStackTrace = prevPrepareStackTrace;
-  return frames;
-}
-
-/**
- * Gets the description text for an error.
- */
-function getErrorDescription(error: Error): string {
-  return `${error.name}: ${error.message}`;
-}
-
-/**
- * Gets formatted string stack from error.
- */
-function formatErrorStack(error: Error, frames: StackFrame[]): string {
-  const errorDesc = getErrorDescription(error);
-  return errorDesc +
-      frames
-          .map(({fileName, funcName, lineNo, colNo}) => {
-            let position = '';
-            if (lineNo !== -1) {
-              position = `:${lineNo}`;
-              if (colNo !== -1) {
-                position += `:${colNo}`;
-              }
-            }
-            return `\n    at ${funcName} (${fileName}${position})`;
-          })
-          .join('');
 }
 
 const appWindow = window.appWindow;
@@ -120,14 +39,11 @@ const appWindow = window.appWindow;
  */
 export function initialize(): void {
   window.addEventListener('unhandledrejection', (e) => {
-    reportError(
-        ErrorType.UNCAUGHT_PROMISE, ErrorLevel.ERROR,
-        assertInstanceof(e.reason, Error));
+    reportError(ErrorType.UNCAUGHT_PROMISE, ErrorLevel.ERROR, e.reason);
   });
-  window.onerror = (_event, _source, _lineno, _colno, error) => {
-    reportError(ErrorType.UNCAUGHT_ERROR, ErrorLevel.ERROR, error);
-    return true;
-  };
+  window.addEventListener('error', (e) => {
+    reportError(ErrorType.UNCAUGHT_ERROR, ErrorLevel.ERROR, e.error);
+  });
 }
 
 /**
@@ -141,34 +57,32 @@ const triggeredErrorSet = new Set<string>();
  * metrics in non test run.
  */
 export function reportError(
-    type: ErrorType, level: ErrorLevel, errorRaw: unknown): void {
+    errorType: ErrorType, level: ErrorLevel, errorRaw: unknown): void {
   const error = assertInstanceof(errorRaw, Error);
-  // Uncaught promise is already logged in console.
-  if (type !== ErrorType.UNCAUGHT_PROMISE) {
+  // Uncaught errors will be logged to the console by browser.
+  if (![ErrorType.UNCAUGHT_ERROR, ErrorType.UNCAUGHT_PROMISE].includes(
+          errorType)) {
     if (level === ErrorLevel.ERROR) {
-      console.error(type, error);
+      console.error(errorType, error);
     } else if (level === ErrorLevel.WARNING) {
-      console.warn(type, error);
+      console.warn(errorType, error);
     }
   }
 
   const time = Date.now();
-  const frames = getStackFrames(error);
   const errorName = error.name;
-  const errorDesc = getErrorDescription(error);
-  const {fileName = '', lineNo = 0, colNo = 0, funcName = ''} =
-      frames.length > 0 ? frames[0] : {};
+  const stackStr = error.stack ?? '';
+  const {fileName, lineNo, colNo, funcName} = parseTopFrameInfo(stackStr);
 
-  const hash = [errorName, fileName, String(lineNo), String(colNo)].join(',');
+  const hash = `${errorName},${fileName},${lineNo},${colNo}`;
   if (triggeredErrorSet.has(hash)) {
     return;
   }
   triggeredErrorSet.add(hash);
 
-  const stackStr = formatErrorStack(error, frames);
   if (appWindow !== null) {
     appWindow.reportError({
-      type,
+      type: errorType,
       level,
       stack: stackStr,
       time,
@@ -177,7 +91,7 @@ export function reportError(
     return;
   }
   metrics.sendErrorEvent({
-    type,
+    type: errorType,
     level,
     errorName,
     fileName,
@@ -194,10 +108,10 @@ export function reportError(
   const params = {
     product: PRODUCT_NAME,
     url: self.location.href,
-    message: `${type}: ${errorDesc}`,
-    lineNumber: lineNo || 0,
-    stackTrace: stackStr || '',
-    columnNumber: colNo || 0,
+    message: `${errorType}: ${errorName}: ${error.message}`,
+    lineNumber: lineNo,
+    stackTrace: stackStr,
+    columnNumber: colNo,
   };
 
   chrome.crashReportPrivate.reportError(
