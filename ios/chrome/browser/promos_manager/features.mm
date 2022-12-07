dd370345c91955c4ad75d9e4978649c8f654bafb
Benjamin Williams <bwwilliams@google.com>
2022-12-07 17:38:59 +0000

    Enable kFullscreenPromosManager by default.
    Three promos (App Store Rating, Restore Shorty, and What's New) use the
    new Promos Manager. Each of these promos has a finch flag that'll be
    used during its rollout.
    
    However, the flag kFullscreenPromosManager still exists as a killswitch for the Promos Manager.
    
    Bug: 1378699
    Change-Id: I5e02c57b6709a6571753cc9feb06354a86d1d652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3982955
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1080402}

--- ios/chrome/browser/promos_manager/features.mm
+++ ios/chrome/browser/promos_manager/features.mm
@@ -12,7 +12,7 @@
 
 BASE_FEATURE(kFullscreenPromosManager,
              "FullscreenPromosManager",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFullscreenPromosManagerSkipInternalLimits,
              "FullscreenPromosManagerSkipInternalLimits",
