f2c9b3a0ce685dd8fea6f899ff20ad3292ac63d2
vikas soni <vikassoni@chromium.org>
2022-12-07 20:22:58 +0000

    Revert "[Dr-Dc] Fix vulkan memory issue."
    This reverts commit 55f7f1fb0d57846ab92f9ef95da228c2b8785e82.
    
    Reason for revert: This CL is resulting in increase in skia effective memory size by 25%. crbug.com/1396279
    
    Original change's description:
    > [Dr-Dc] Fix vulkan memory issue.
    >
    > DrDc in vulkan mode was consuming more memory as reported by
    > Skia.VulkanMemoryAllocator.AmountAllocated. This is due to the fact that
    > DrDc thread GrContext was not being cleaned up periodically.
    >
    > This CL adds ScheduleGrContextCleanup() on every
    > FinishPaintRenderPass().
    >
    > Bug: 1396096
    > Change-Id: Ib6dcc295b21459ea449c3e318bd68df452096266
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081926
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Auto-Submit: vikas soni <vikassoni@chromium.org>
    > Commit-Queue: vikas soni <vikassoni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1079615}
    
    Bug: 1396096, 1396279
    Change-Id: I560f5a1c9fbc052d2519d1892024f23c2ab0ef96
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4084152
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080523}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -625,10 +625,6 @@ void SkiaOutputSurfaceImplOnGpu::FinishPaintRenderPass(
     return;
   }
 
-  // Note that when CompositorGpuThread is disabled, this cleanup for gpu main
-  // thread context also happens in raster decoder.
-  dependency_->ScheduleGrContextCleanup();
-
   // Only overlayed images require end_semaphore synchronization.
   DCHECK(is_overlay || end_semaphores.empty());
 
