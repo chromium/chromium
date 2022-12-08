77ada46b4fef6e4aeda743628b4b96ffd22a148c
Andrés Olivares <andoli@chromium.org>
2022-12-08 13:34:19 +0000

    Add tests for advanced tracing instrumentation trace events
    Bug: 1383130
    Change-Id: I2bf9b93593887e90d205586bbbcbbaf8388d52a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085776
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080889}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/advanced-painting-instrumentation.js
@@ -0,0 +1,43 @@
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      'Tests the data of advanced painting instrumentation trace events');
+
+  const TracingHelper =
+      await testRunner.loadScript('../resources/tracing-test.js');
+  const tracingHelper = new TracingHelper(testRunner, session);
+
+  await dp.Page.enable();
+  await tracingHelper.startTracing(
+      'disabled-by-default-cc.debug,disabled-by-default-viz.quads,disabled-by-default-devtools.timeline.layers,disabled-by-default-devtools.timeline.picture');
+  dp.Page.navigate({
+    url:
+        'http://127.0.0.1:8000/inspector-protocol/resources/iframe-navigation.html'
+  });
+
+  // Wait for the DOM to be interactive.
+  await dp.Page.onceLoadEventFired();
+
+  await tracingHelper.stopTracing(
+      /disabled-by-default-(devtools\.timeline\.)?(cc\.debug|layers|picture)/);
+
+  const LayerTreeHostImpl = tracingHelper.findEvent(
+      'cc::LayerTreeHostImpl', TracingHelper.Phase.SNAPSHOT_OBJECT);
+  const DisplayItemList = tracingHelper.findEvent(
+      'cc::DisplayItemList', TracingHelper.Phase.SNAPSHOT_OBJECT);
+
+  testRunner.log('Got a LayerTreeHostImpl Event:');
+  testRunner.log(`type of device_viewport_size: ${
+      typeof LayerTreeHostImpl.args.snapshot.device_viewport_size}`);
+  testRunner.log(`type of layers: ${
+      typeof LayerTreeHostImpl.args.snapshot.active_tree.layers}`);
+
+  testRunner.log('Got a DisplayItemList Event');
+  testRunner.log(
+      `type of params: ${typeof DisplayItemList.args.snapshot.params}`);
+  testRunner.log(`type of layer_rect: ${
+      typeof DisplayItemList.args.snapshot.params.layer_rect}`);
+  testRunner.log(
+      `type of skp64: ${typeof DisplayItemList.args.snapshot.skp64}`);
+
+  testRunner.completeTest();
+})
