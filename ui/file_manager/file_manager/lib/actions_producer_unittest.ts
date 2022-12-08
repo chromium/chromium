5004fe959d937cf863e12cba484e1535848234d4
Luciano Pacheco <lucmult@chromium.org>
2022-12-08 01:35:32 +0000

    Files F2: Add keyedKeepFirst concurrency model
    The keyedKeepFirst() concurrency model avoid starting a new
    ActionsProducer if multiple requests with the same key are done.
    
    It starts a new run if a request with a new key is done or the request
    of the same key has already finished.
    
    This will be used for FileTasks to keep the current behavior of only
    requesting new FileTasks when requested with a new set of FileKeys.
    
    Bug: b:228112698
    Change-Id: Iceda83403464a84a0fc70fde7a67f560b2acabab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083033
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080706}

--- ui/file_manager/file_manager/lib/actions_producer_unittest.ts
+++ ui/file_manager/file_manager/lib/actions_producer_unittest.ts
@@ -1,11 +1,12 @@
 // Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import {assertEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
+import {assertEquals, assertNotReached, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 import {waitUntil} from '../common/js/test_error_reporting.js';
 
-import {keepLatest} from './concurrency_models.js';
+import {ConcurrentActionInvalidatedError} from './actions_producer.js';
+import {keepLatest, keyedKeepFirst} from './concurrency_models.js';
 import {actionsProducerSuccess, TestAction} from './for_tests.js';
 
 /**
@@ -65,6 +66,65 @@ export async function testKeepLatest(done: () => void) {
   // and 1 for the final action.
   await waitUntil(
       () => results.filter(r => r.payload === 'second-action').length === 4);
+
+  done();
+}
+
+/**
+ * Tests the keyedKeepFirst() concurrency model. Checks that the first call
+ * with the same key, doesn't start a new AP, and calls with a new key cancels
+ * the previous APs and starts a new AP.
+ */
+export async function testKeyedKeepFirst(done: () => void) {
+  const action = keyedKeepFirst(actionsProducerSuccess, (payload: string) => {
+    return `key-${payload}`;
+  });
+
+  // `first` here is a generator,  an ActionsProducer, wrapped in the
+  // concurrency model `keyedKeepFirst`.
+  const first = action('file-key-1');
+
+  // Starts consuming the generated actions and can start another one in
+  // parallel.
+  const {value} = await first.next();
+  assertEquals(value!.type, 'step#0');
+  assertEquals(value!.payload, 'file-key-1');
+
+  // Make a second call with the same key, which finishes in the first iteration
+  // without generating any action.
+  const second = action('file-key-1');
+  const secondValue = await second.next();
+  assertEquals(secondValue!.value, undefined);
+  assertEquals(secondValue!.done, true);
+
+  // Start a new call with different key.
+  // Array to collect all the generated actions.
+  const results: TestAction[] = [];
+  const third = action('another-key-2');
+  const thirdValue = await third.next();
+  assertEquals(thirdValue!.value!.type, 'step#0');
+  assertEquals(thirdValue!.value!.payload, 'another-key-2');
+  results.push(thirdValue.value!);
+
+  // At this point `first` should be cancelled by the keyedKeepFirst().
+  // However, the exception only shows up in the next iteration of the AP.
+  try {
+    await first.next();
+    assertNotReached('`first` should have failed');
+  } catch (error) {
+    assertTrue(error instanceof ConcurrentActionInvalidatedError);
+    // Check that the generator is in `done=true` state.
+    const {value, done} = await first.next();
+    assertEquals(value, undefined);
+    assertEquals(done, true);
+  }
+
+  // Await for the third generator to be fully consumed.
+  await consumeGenerator(third, results);
+  // The third action generates 4 results: 1 at start, 2 from the args [2,
+  // 2] / and 1 for the final action.
+  await waitUntil(
+      () => results.filter(r => r.payload === 'another-key-2').length === 4);
 
   done();
 }
--- ui/file_manager/file_manager/lib/concurrency_models.ts
+++ ui/file_manager/file_manager/lib/concurrency_models.ts
@@ -46,3 +46,54 @@ export function keepLatest<T extends BaseAction, Args extends any[]>(
   }
   return wrap;
 }
+
+/**
+ * While the key is the same it doesn't start a new Actions Producer (AP).
+ *
+ * If the key changes, then it cancels the previous one and starts a new one.
+ *
+ * If there is no other running AP, then it just starts a new one.
+ */
+export function keyedKeepFirst<T extends BaseAction, Args extends any[]>(
+    actionsProducer: ActionsProducer<T, Args>,
+    generateKey: (...args: Args) => string): ActionsProducer<T, Args> {
+  // Scope #1: Initial setup.
+  // Key for the current AP.
+  let inFlightKey: string|null = null;
+
+  async function* wrap(...args: Args): ActionsProducerGen<T> {
+    // Scope #2: Per-call to the ActionsProducer.
+    const key = generateKey(...args);
+    // One already exists, just leave that finish.
+    if (inFlightKey && inFlightKey === key) {
+      return;
+    }
+
+    // This will force the previously running AP to cancel when yielding.
+    inFlightKey = key;
+
+    const generator = actionsProducer(...args);
+    try {
+      for await (const producedAction of generator) {
+        // Scope #3: The generated action.
+        if (inFlightKey && inFlightKey !== key) {
+          const error = new ConcurrentActionInvalidatedError(
+              `ActionsProducer invalidated running key: ${key} current: ${
+                  inFlightKey}:`);
+          generator.throw(error);
+          throw error;
+        }
+        yield producedAction;
+      }
+    } catch (error) {
+      if (error instanceof ConcurrentActionInvalidatedError) {
+        // This error we don't want to clear the `inFlightKey`.
+        throw error;
+      }
+    }
+
+    // Clear the key if it wasn't invalidated.
+    inFlightKey = null;
+  }
+  return wrap;
+}
