e8fde5610736fafb8512853bba433fddb6eb345d
Travis Skare <skare@chromium.org>
2022-12-08 19:49:48 +0000

    [Cormorant] Handle openSuggestionUrl for back-of-card menu
    Bug: 1396409
    Change-Id: I25ca9a966c009f44f449b36f955a499ac677526a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086249
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081053}

--- chrome/android/java/src/org/chromium/chrome/browser/app/creator/CreatorActionDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/creator/CreatorActionDelegateImpl.java
@@ -5,11 +5,17 @@
 package org.chromium.chrome.browser.app.creator;
 
 import org.chromium.base.Callback;
+import org.chromium.base.Log;
 import org.chromium.chrome.browser.feed.FeedActionDelegate;
+import org.chromium.chrome.browser.tab.TabLaunchType;
+import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
 import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.ui.mojom.WindowOpenDisposition;
 
 /** Implements some actions for the Feed */
 public class CreatorActionDelegateImpl implements FeedActionDelegate {
+    private static final String TAG = "Cormorant";
+
     public CreatorActionDelegateImpl() {}
     @Override
     public void downloadPage(String url) {
@@ -19,13 +25,21 @@ public class CreatorActionDelegateImpl implements FeedActionDelegate {
     @Override
     public void openSuggestionUrl(int disposition, LoadUrlParams params, boolean inGroup,
             Runnable onPageLoaded, Callback<VisitResult> onVisitComplete) {
+        // Back of card actions
+        if (disposition == WindowOpenDisposition.NEW_FOREGROUND_TAB
+                || disposition == WindowOpenDisposition.NEW_BACKGROUND_TAB
+                || disposition == WindowOpenDisposition.OFF_THE_RECORD) {
+            boolean offTheRecord = (disposition == WindowOpenDisposition.OFF_THE_RECORD);
+            new TabDelegate(offTheRecord).createNewTab(params, TabLaunchType.FROM_LINK, null);
+            return;
+        }
+
         // TODO(crbug.com/1395448) open in ephemeral tab or thin web view.
+        Log.w(TAG, "OpenSuggestionUrl: Unhandled disposition " + disposition);
     }
 
     @Override
-    public void openUrl(int disposition, LoadUrlParams params) {
-        // TODO(crbug.com/1395448) open in ephemeral tab or thin web view.
-    }
+    public void openUrl(int disposition, LoadUrlParams params) {}
 
     @Override
     public void openHelpPage() {
@@ -39,7 +53,7 @@ public class CreatorActionDelegateImpl implements FeedActionDelegate {
 
     @Override
     public void openCrow(String url) {
-        // Not sure if this needs to be implemented.
+        // Unused; feature is deprecated and does not launch from the feed.
     }
 
     @Override
