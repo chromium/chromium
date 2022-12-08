646f5ad6525278495bd03c49eee4d5bf308d0863
Tibor Goldschwendt <tiborg@chromium.org>
2022-12-08 16:05:18 +0000

    [cc-side-panel] Propagate color theming changes to front-end
    With this change, the side panel handler listens for theming updates and
    sends the theme color and other theming information to the front-end to
    be displayed in follow-up CLs.
    
    Change-Id: I630419438b5899c92425784f4b41606bf22de963
    Bug: 1384229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4073963
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1080943}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler.h
@@ -6,13 +6,18 @@
 #define CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_CUSTOMIZE_CHROME_CUSTOMIZE_CHROME_PAGE_HANDLER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/search/background/ntp_background_service.h"
 #include "chrome/browser/search/background/ntp_background_service_observer.h"
 #include "chrome/browser/search/background/ntp_custom_background_service.h"
+#include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/themes/theme_service_observer.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "ui/native_theme/native_theme.h"
+#include "ui/native_theme/native_theme_observer.h"
 
 namespace content {
 class WebContents;
@@ -22,7 +27,9 @@ class Profile;
 
 class CustomizeChromePageHandler
     : public side_panel::mojom::CustomizeChromePageHandler,
-      public NtpBackgroundServiceObserver {
+      public NtpBackgroundServiceObserver,
+      public ui::NativeThemeObserver,
+      public ThemeServiceObserver {
  public:
   CustomizeChromePageHandler(
       mojo::PendingReceiver<side_panel::mojom::CustomizeChromePageHandler>
@@ -46,6 +53,12 @@ class CustomizeChromePageHandler
   void UpdateTheme() override;
 
  private:
+  // ui::NativeThemeObserver:
+  void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
+
+  // ThemeServiceObserver:
+  void OnThemeChanged() override;
+
   bool IsCustomLinksEnabled() const;
   bool IsShortcutsVisible() const;
 
@@ -61,6 +74,12 @@ class CustomizeChromePageHandler
   raw_ptr<NtpBackgroundService> ntp_background_service_;
   GetBackgroundCollectionsCallback background_collections_callback_;
   base::TimeTicks background_collections_request_start_time_;
+  raw_ptr<ThemeService> theme_service_;
+
+  base::ScopedObservation<ui::NativeTheme, ui::NativeThemeObserver>
+      native_theme_observation_{this};
+  base::ScopedObservation<ThemeService, ThemeServiceObserver>
+      theme_service_observation_{this};
 
   mojo::Remote<side_panel::mojom::CustomizeChromePage> page_;
   mojo::Receiver<side_panel::mojom::CustomizeChromePageHandler> receiver_;
