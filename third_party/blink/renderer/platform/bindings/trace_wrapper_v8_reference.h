0c830b2a16593ec2a5a113078b77cba15a37c517
Michael Lippautz <mlippautz@chromium.org>
2022-12-09 10:33:43 +0000

    TraceWrapperV8Reference: Temporarily disable move via memcpy
    Evaluate impact on crash rate.
    
    Bug: chromium:1322114
    Change-Id: Idec62ce87d0b3649bf1cb10ce607d1d5913591e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4092183
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081392}

--- third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h
+++ third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h
@@ -47,7 +47,9 @@ struct VectorTraits<blink::TraceWrapperV8Reference<T>>
   // v8::TracedReference assumes that references uniquely point to an internal
   // node.
   static constexpr bool kCanCopyWithMemcpy = false;
-  static constexpr bool kCanMoveWithMemcpy = true;
+  // TODO(chromium:1322114): Temporarily disable move with memcpy to evaluate
+  // impact on crashers. Move should always be followed by a clear (non-dtor).
+  static constexpr bool kCanMoveWithMemcpy = false;
 
   // TraceWrapperV8Reference supports concurrent tracing.
   static constexpr bool kCanTraceConcurrently = true;
