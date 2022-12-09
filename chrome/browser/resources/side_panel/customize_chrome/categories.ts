14827ba994f996e9c1634ceb737059995f2d0d52
Riley Tatum <rtatum@google.com>
2022-12-09 23:52:44 +0000

    [NTP][Panorama] Add Collection thumbnail image
    Added the preview/thumbnail image for each collection to the collections
    page. Since they all have a larger width than height, I made the height
    100%, centered the image in the square,  and let the width stay auto to
    be cut off on the sides.
    
    screenshot: screenshot/8tfBqRpDWmP2s6r
    
    Bug: 1384251
    Change-Id: Iddf8576194d20a7a8376dc469142bcbba8368605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4086570
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081720}

--- chrome/browser/resources/side_panel/customize_chrome/categories.ts
+++ chrome/browser/resources/side_panel/customize_chrome/categories.ts
@@ -1,6 +1,7 @@
 // Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 import 'chrome://resources/cr_elements/cr_grid/cr_grid.js';
 import 'chrome://resources/cr_elements/cr_icons.css.js';
