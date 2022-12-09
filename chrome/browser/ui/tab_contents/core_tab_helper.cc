12c67cb2e172dbc7ce245d1bdf9238ae23a21dfd
Ali Stanfield <stanfield@google.com>
2022-12-09 18:24:47 +0000

    [M108] Fix bug where side panel is used for reverse image search.
    (cherry picked from commit 0b5d87322954049caaa89285194352197b55f5fc)
    
    Bug: 1399531, b/261752967
    Change-Id: I5df394986e92ad51bcb2fcb8e41fb65188eee944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4088529
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1080981}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4090026
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5359@{#1148}
    Cr-Branched-From: 27d3765d341b09369006d030f83f582a29eb57ae-refs/heads/main@{#1058933}

--- chrome/browser/ui/tab_contents/core_tab_helper.cc
+++ chrome/browser/ui/tab_contents/core_tab_helper.cc
@@ -139,6 +139,7 @@ bool CoreTabHelper::IsSidePanelEnabled() {
 
 bool CoreTabHelper::IsSidePanelEnabledFor3PDse() {
   return IsSidePanelEnabled() &&
+         !search::DefaultSearchProviderIsGoogle(GetTemplateURLService()) &&
          base::FeatureList::IsEnabled(features::kUnifiedSidePanel) &&
          lens::features::GetEnableImageSearchUnifiedSidePanelFor3PDse();
 }
