ca1c20b3e0ec56879fff3eaad4e29f3fb55b8aeb
Denny Huang <dennyh@google.com>
2022-12-12 20:14:34 +0000

    diagnostics: Introduce eMMC Lifetime routine
    cros_healthd introduces a new routine "emmc_lifetime" for checking eMMC
    lifetime. Add corresponding mojo interface and unit test for
    DeviceCommandRunRoutineJob.
    
    Bug: b:260186120
    Test: CQ
    Test: unit_tests -ozone-platform=headless --gtest_filter="DeviceCommandRunRoutineJobTest.*"
    Change-Id: I9a83cdd2bdacc00afc6446802ec3832f5c25163c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4050923
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Denny Huang <dennyh@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081987}

--- chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
+++ chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h
@@ -333,6 +333,7 @@ class FakeCrosHealthd final : public mojom::CrosHealthdServiceFactory,
       mojom::LedColor color,
       mojo::PendingRemote<mojom::LedLitUpRoutineReplier> replier,
       RunLedLitUpRoutineCallback callback) override;
+  void RunEmmcLifetimeRoutine(RunEmmcLifetimeRoutineCallback callback) override;
 
   // CrosHealthdEventService overrides:
   void AddBluetoothObserver(
