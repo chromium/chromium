a6d09efe268017b8ba04585b64c809ba39761785
Scott Violet <sky@chromium.org>
2022-12-12 17:56:45 +0000

    blink: make TextControlElement::SetSuggestedValue early out if possible
    This consists of two changes to TextControlElement::SetSuggestedValue:
    1. Avoid calling maxLength() for an empty string.
       HTMLInputStream::SetValue always calls this function with an empty
       value first, and maxLength() is non-trivial, so avoid if possible.
    2. Early out if new value matches existing value.
    
    This patch is part of a bigger set of patches. Combined patches are
    here: https://chromium-review.googlesource.com/c/chromium/src/+/4082153
    
    Change-Id: If51ccb074c55c622018d0777d838b0e1edef2144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081309
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081921}

--- third_party/blink/renderer/core/html/forms/text_control_element.cc
+++ third_party/blink/renderer/core/html/forms/text_control_element.cc
@@ -1067,7 +1067,13 @@ void TextControlElement::SetAutofillValue(const String& value,
 
 // TODO(crbug.com/772433): Create and use a new suggested-value element instead.
 void TextControlElement::SetSuggestedValue(const String& value) {
-  suggested_value_ = value.Substring(0, maxLength());
+  // Avoid calling maxLength() if possible as it's non-trivial.
+  const String new_suggested_value =
+      value.empty() ? value : value.Substring(0, maxLength());
+  if (new_suggested_value == suggested_value_)
+    return;
+  suggested_value_ = new_suggested_value;
+
   if (!suggested_value_.empty() && !InnerEditorValue().empty()) {
     // If there is an inner editor value, hide it so the suggested value can be
     // shown to the user.
