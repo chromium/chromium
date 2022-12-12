e3a189dd3f40c44817b0d86990990646cd31b556
Scott Violet <sky@chromium.org>
2022-12-12 20:56:43 +0000

    blink: minor optimization to Range
    This changes Range::NodeWillBeRemoved to reuse the calculated
    value from the start if collapsed.
    
    This patch is part of a bigger set of patches. Combined patches are
    here: https://chromium-review.googlesource.com/c/chromium/src/+/4082153
    
    Change-Id: I31a4e9063e9592ae48376beb2c914483ae65048e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4094076
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082007}

--- third_party/blink/renderer/core/dom/range.cc
+++ third_party/blink/renderer/core/dom/range.cc
@@ -1414,19 +1414,21 @@ void Range::FixupRemovedChildrenAcrossShadowBoundary(ContainerNode& container) {
   BoundaryShadowNodeChildrenWillBeRemoved(end_, container);
 }
 
-static inline void BoundaryNodeWillBeRemoved(RangeBoundaryPoint& boundary,
+// Returns true if `boundary` was modified.
+static inline bool BoundaryNodeWillBeRemoved(RangeBoundaryPoint& boundary,
                                              Node& node_to_be_removed) {
   if (boundary.ChildBefore() == node_to_be_removed) {
     boundary.ChildBeforeWillBeRemoved();
-    return;
+    return true;
   }
 
   for (Node* n = &boundary.Container(); n; n = n->parentNode()) {
     if (n == node_to_be_removed) {
       boundary.SetToBeforeChild(node_to_be_removed);
-      return;
+      return true;
     }
   }
+  return false;
 }
 
 static inline void BoundaryShadowNodeWillBeRemoved(RangeBoundaryPoint& boundary,
@@ -1450,8 +1452,14 @@ void Range::NodeWillBeRemoved(Node& node) {
   // should change following if-statement to DCHECK(!node->parentNode).
   if (!node.parentNode())
     return;
-  BoundaryNodeWillBeRemoved(start_, node);
-  BoundaryNodeWillBeRemoved(end_, node);
+  const bool is_collapsed = collapsed();
+  const bool start_updated = BoundaryNodeWillBeRemoved(start_, node);
+  if (is_collapsed) {
+    if (start_updated)
+      end_ = start_;
+  } else {
+    BoundaryNodeWillBeRemoved(end_, node);
+  }
 }
 
 void Range::FixupRemovedNodeAcrossShadowBoundary(Node& node) {
