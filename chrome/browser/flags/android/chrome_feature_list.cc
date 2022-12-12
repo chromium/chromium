0865c724409204b0d640b483e4218c7e1d59a035
Donn Denman <donnd@chromium.org>
2022-12-12 18:52:22 +0000

    [CCT] Default-enable Histogram recording of usage
    Default-enables the CCTFeatureUsage Feature, which logs a big
    histogram for CCT usage.
    
    BUG=1350137
    
    Change-Id: Ia96f05ee0f5262304a5f533c8c842d677383f02c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4089609
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081944}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -561,7 +561,7 @@ BASE_FEATURE(kCCTNewDownloadTab,
 
 BASE_FEATURE(kCCTFeatureUsage,
              "CCTFeatureUsage",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTIncognito, "CCTIncognito", base::FEATURE_ENABLED_BY_DEFAULT);
 
