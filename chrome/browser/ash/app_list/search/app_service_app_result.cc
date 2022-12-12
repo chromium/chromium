8524687d2de3a7df626d56e7dfae3073e4bb74a2
Lauren Commeignes <laurencom@chromium.org>
2022-12-12 16:31:16 +0000

    Remove references to kInternalAppIdContinue within App Service App Result
    Bug: b/261641766
    Change-Id: Ib5502641a149976f2f8c0d9abd30bb248fae97cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081439
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1081780}

--- chrome/browser/ash/app_list/search/app_service_app_result.cc
+++ chrome/browser/ash/app_list/search/app_service_app_result.cc
@@ -5,28 +5,22 @@
 #include "chrome/browser/ash/app_list/search/app_service_app_result.h"
 
 #include "ash/public/cpp/app_list/app_list_types.h"
-#include "ash/public/cpp/app_list/internal_app_id_constants.h"
 #include "ash/public/cpp/shelf_item_delegate.h"
 #include "ash/public/cpp/shelf_model.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
-#include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/apps/app_service/metrics/app_service_metrics.h"
 #include "chrome/browser/ash/app_list/app_service/app_service_context_menu.h"
-#include "chrome/browser/ash/app_list/internal_app/internal_app_metadata.h"
 #include "chrome/browser/ash/app_list/search/common/icon_constants.h"
-#include "chrome/browser/favicon/large_icon_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
-#include "components/favicon/core/large_icon_service.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/app_update.h"
 #include "extensions/common/extension.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/window_open_disposition_utils.h"
 
 namespace app_list {
 
@@ -85,9 +79,6 @@ AppServiceAppResult::AppServiceAppResult(Profile* profile,
       set_id(app_id);
       break;
   }
-
-  if (IsSuggestionChip(id()))
-    HandleSuggestionChip(profile);
 }
 
 AppServiceAppResult::~AppServiceAppResult() = default;
@@ -152,15 +143,6 @@ void AppServiceAppResult::ExecuteLaunchCommand(int event_flags) {
 
 void AppServiceAppResult::Launch(int event_flags,
                                  apps::LaunchSource launch_source) {
-  if (id() == ash::kInternalAppIdContinueReading &&
-      url_for_continuous_reading_.is_valid()) {
-    apps::RecordAppLaunch(id(), launch_source);
-    controller()->OpenURL(profile(), url_for_continuous_reading_,
-                          ui::PAGE_TRANSITION_GENERATED,
-                          ui::DispositionFromEventFlags(event_flags));
-    return;
-  }
-
   apps::AppServiceProxy* proxy =
       apps::AppServiceProxyFactory::GetForProfile(profile());
 
@@ -236,94 +218,6 @@ void AppServiceAppResult::OnLoadIcon(bool chip, apps::IconValuePtr icon_value) {
     constexpr bool allow_placeholder_icon = false;
     CallLoadIcon(chip, allow_placeholder_icon);
   }
-}
-
-void AppServiceAppResult::HandleSuggestionChip(Profile* profile) {
-  if (id() == ash::kInternalAppIdContinueReading) {
-    large_icon_service_ =
-        LargeIconServiceFactory::GetForBrowserContext(profile);
-    UpdateContinueReadingFavicon(/*continue_to_google_server=*/true);
-  }
-}
-
-void AppServiceAppResult::UpdateContinueReadingFavicon(
-    bool continue_to_google_server) {
-  std::u16string title;
-  GURL url;
-  if (app_list::HasRecommendableForeignTab(profile(), &title, &url,
-                                           /*test_delegate=*/nullptr)) {
-    url_for_continuous_reading_ = url;
-
-    // Foreign tab could be updated since the title was set the last time.
-    // Update the title every time.
-    // TODO(wutao): If |title| is empty, use default title string.
-    if (!title.empty())
-      SetTitle(title);
-
-    // Desired size of the icon. If not available, a smaller one will be used.
-    constexpr int min_source_size_in_pixel = 16;
-    constexpr int desired_size_in_pixel = 32;
-    large_icon_service_->GetLargeIconImageOrFallbackStyleForPageUrl(
-        url_for_continuous_reading_, min_source_size_in_pixel,
-        desired_size_in_pixel,
-        base::BindOnce(&AppServiceAppResult::OnGetFaviconFromCacheFinished,
-                       weak_ptr_factory_.GetWeakPtr(),
-                       continue_to_google_server),
-        &task_tracker_);
-  }
-}
-
-void AppServiceAppResult::OnGetFaviconFromCacheFinished(
-    bool continue_to_google_server,
-    const favicon_base::LargeIconImageResult& image_result) {
-  if (!image_result.image.IsEmpty()) {
-    // Continue Reading app will only be shown in suggestion chip.
-    SetChipIcon(*image_result.image.ToImageSkia());
-    // Update the time when the icon was last requested to postpone the
-    // automatic eviction of the favicon from the favicon database.
-    large_icon_service_->TouchIconFromGoogleServer(image_result.icon_url);
-    return;
-  }
-
-  if (!continue_to_google_server)
-    return;
-
-  // Try to fetch the favicon from a Google favicon server.
-  net::NetworkTrafficAnnotationTag traffic_annotation =
-      net::DefineNetworkTrafficAnnotation("app_suggestion_get_favicon", R"(
-        semantics {
-          sender: "App Suggestion"
-          description:
-            "Sends a request to a Google server to retrieve the favicon bitmap "
-            "for an article suggestion on the Launcher (URLs are public and "
-            "provided by Google)."
-          trigger:
-            "A request can be sent if Chrome does not have a favicon for a "
-            "particular page."
-          data: "Page URL and desired icon size."
-          destination: GOOGLE_OWNED_SERVICE
-        }
-        policy {
-          cookies_allowed: NO
-          setting: "This feature cannot be disabled by settings."
-          policy_exception_justification: "Not implemented."
-        })");
-  large_icon_service_
-      ->GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
-          url_for_continuous_reading_,
-          /*may_page_url_be_private=*/false,
-          /*should_trim_page_url_path=*/false, traffic_annotation,
-          base::BindOnce(
-              &AppServiceAppResult::OnGetFaviconFromGoogleServerFinished,
-              weak_ptr_factory_.GetWeakPtr()));
-}
-
-void AppServiceAppResult::OnGetFaviconFromGoogleServerFinished(
-    favicon_base::GoogleFaviconServerRequestStatus status) {
-  if (status != favicon_base::GoogleFaviconServerRequestStatus::SUCCESS)
-    return;
-
-  UpdateContinueReadingFavicon(/*continue_to_google_server=*/false);
 }
 
 }  // namespace app_list
--- chrome/browser/ash/app_list/search/app_service_app_result.h
+++ chrome/browser/ash/app_list/search/app_service_app_result.h
@@ -11,24 +11,13 @@
 #include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/ash/app_list/search/app_result.h"
-#include "components/favicon_base/favicon_types.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/icon_cache.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
-#include "url/gurl.h"
 
 class AppListControllerDelegate;
 class Profile;
-
-namespace favicon {
-class LargeIconService;
-}  // namespace favicon
-
-namespace favicon_base {
-struct LargeIconImageResult;
-}  // namespace favicon_base
-
 namespace app_list {
 
 class AppServiceAppResult : public AppResult {
@@ -59,24 +48,6 @@ class AppServiceAppResult : public AppResult {
   void CallLoadIcon(bool chip, bool allow_placeholder_icon);
   void OnLoadIcon(bool chip, apps::IconValuePtr icon_value);
 
-  void HandleSuggestionChip(Profile* profile);
-
-  // Get large icon image from servers and update icon for continue reading.
-  //
-  // Continue reading is a special app, which uses LargeIconService to load
-  // icon. Since it is the only app which uses the LargeIconService, leave it in
-  // search result code, not integrated to the AppService icon cache.
-  //
-  // If there is no cache hit on LargeIconService and
-  // |continue_to_google_server| is true, will try to download the icon from
-  // Google favicon server.
-  void UpdateContinueReadingFavicon(bool continue_to_google_server);
-  void OnGetFaviconFromCacheFinished(
-      bool continue_to_google_server,
-      const favicon_base::LargeIconImageResult& image);
-  void OnGetFaviconFromGoogleServerFinished(
-      favicon_base::GoogleFaviconServerRequestStatus status);
-
   apps::IconLoader* const icon_loader_;
 
   // When non-nullptr, signifies that this object is using the most recent icon
@@ -91,13 +62,6 @@ class AppServiceAppResult : public AppResult {
   std::unique_ptr<AppContextMenu> context_menu_;
 
   base::CancelableTaskTracker task_tracker_;
-
-  // The url of recommendable foreign tab, which is invalid if there is no
-  // recommendation.
-  GURL url_for_continuous_reading_;
-
-  // Used to fetch the favicon of the website |url_for_continuous_reading_|.
-  favicon::LargeIconService* large_icon_service_ = nullptr;
 
   base::WeakPtrFactory<AppServiceAppResult> weak_ptr_factory_{this};
 };
