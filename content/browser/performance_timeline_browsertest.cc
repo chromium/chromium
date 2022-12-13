32ff97fb840aaf01e9c0b8feb8c69b103f8e58c0
Hao Liu <haoliuk@chromium.org>
2022-12-13 23:42:26 +0000

    Merge performance timeline related tests
    This Cl merges 4 existing content browser test files. They are all
    performance timeline related.
    
    Change-Id: Ifc5fdbdbe7e85aaa2aaf983deb6fa11f8abc0abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4089940
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082800}

--- content/browser/performance_timeline_browsertest.cc
+++ content/browser/performance_timeline_browsertest.cc
@@ -15,8 +15,8 @@
 #include "net/dns/mock_host_resolver.h"
 
 namespace content {
-class PerformanceTimelineBackForwardCacheRestorationBrowserTest
-    : public ContentBrowserTest {
+  
+class PerformanceTimelineBrowserTest : public ContentBrowserTest {
  protected:
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -27,10 +27,173 @@ class PerformanceTimelineBackForwardCacheRestorationBrowserTest
     return static_cast<WebContentsImpl*>(shell()->web_contents());
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ContentBrowserTest::SetUpCommandLine(command_line);
+  }
+
   RenderFrameHostImpl* current_frame_host() {
     return web_contents()->GetPrimaryFrameTree().root()->current_frame_host();
   }
+};
 
+class PerformanceTimelineLCPStartTimePrecisionBrowserTest
+    : public PerformanceTimelineBrowserTest {
+ protected:
+  EvalJsResult GetIsEqualToPrecision() const {
+    std::string script =
+        content::JsReplace("isEqualToPrecision($1);", getPrecision());
+    return EvalJs(shell(), script);
+  }
+
+  int32_t getPrecision() const { return precision_; }
+
+ private:
+  int32_t precision_ = 10;
+};
+
+IN_PROC_BROWSER_TEST_F(PerformanceTimelineLCPStartTimePrecisionBrowserTest,
+                       LCPStartTimePrecision) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  const GURL url1(embedded_test_server()->GetURL(
+      "a.com", "/performance_timeline/lcp-start-time-precision.html"));
+
+  EXPECT_TRUE(NavigateToURL(shell(), url1));
+
+  EXPECT_TRUE(GetIsEqualToPrecision().ExtractBool());
+}
+
+class PerformanceTimelineNavigationIdBrowserTest
+    : public PerformanceTimelineBrowserTest {
+ protected:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ContentBrowserTest::SetUpCommandLine(command_line);
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        "--enable-blink-test-features");
+  }
+
+  [[nodiscard]] EvalJsResult GetNavigationId(const std::string& name) {
+    const char kGetPerformanceEntryTemplate[] = R"(
+        (() => {performance.mark($1);
+        return performance.getEntriesByName($1)[0].navigationId;})();
+    )";
+    std::string script = content::JsReplace(kGetPerformanceEntryTemplate, name);
+    return EvalJs(shell(), script);
+  }
+};
+
+// This test case is to verify PerformanceEntry.navigationId gets incremented
+// for each back/forward cache restore.
+IN_PROC_BROWSER_TEST_F(PerformanceTimelineNavigationIdBrowserTest,
+                       BackForwardCacheRestore) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  const GURL url1(embedded_test_server()->GetURL("a.com", "/title1.html"));
+  const GURL url2(embedded_test_server()->GetURL("b.com", "/title1.html"));
+
+  EXPECT_TRUE(NavigateToURL(shell(), url1));
+
+  EXPECT_EQ(1, GetNavigationId("first_nav"));
+  // Navigate away and back 3 times. The 1st time is to verify the
+  // navigation id is incremented. The 2nd time is to verify that the id is
+  // incremented on the same restored document. The 3rd time is to
+  // verify the increment does not stop at 2.
+  RenderFrameHostImplWrapper rfh_a(current_frame_host());
+  for (int i = 1; i <= 3; i++) {
+    // Navigate away
+    ASSERT_TRUE(NavigateToURL(shell(), url2));
+
+    // Verify `rfh_a` is stored in back/forward cache in case back/forward cache
+    // feature is enabled.
+    if (IsBackForwardCacheEnabled())
+      ASSERT_TRUE(rfh_a->IsInBackForwardCache());
+    else {
+      // Verify `rfh_a` is deleted in case back/forward cache feature is
+      // disabled.
+      ASSERT_TRUE(rfh_a.WaitUntilRenderFrameDeleted());
+    }
+
+    // Navigate back.
+    ASSERT_TRUE(HistoryGoBack(web_contents()));
+
+    // Verify navigation id is incremented each time in case back/forward
+    // cache feature is enabled. Verify navigation id is always 0 in case
+    // back/forward cache feature is not enabled.
+    EXPECT_EQ(IsBackForwardCacheEnabled() ? i + 1 : 1,
+              GetNavigationId("subsequent_nav" + base::NumberToString(i)));
+  }
+}
+
+// This test case is to verify the navigation id of a frame does not increment
+// if the page load is not a back/forward cache restore, even with the
+// back/forward cache feature enabled.
+IN_PROC_BROWSER_TEST_F(PerformanceTimelineNavigationIdBrowserTest,
+                       NonBackForwardCacheRestore) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  const GURL url1(embedded_test_server()->GetURL("a.com", "/title1.html"));
+  const GURL url2(embedded_test_server()->GetURL("b.com", "/title1.html"));
+
+  EXPECT_TRUE(NavigateToURL(shell(), url1));
+
+  EXPECT_EQ(1, GetNavigationId("first_nav"));
+
+  // Make `rfh_a`ineligible for back/forward cache so that the subsequent page
+  // load is not a back/forward restore.
+  RenderFrameHostImplWrapper rfh_a(current_frame_host());
+  DisableBFCacheForRFHForTesting(rfh_a.get());
+
+  // Navigate away.
+  ASSERT_TRUE(NavigateToURL(shell(), url2));
+
+  // Verify `rfh_a` is not in the back/forward cache.
+  ASSERT_TRUE(rfh_a.WaitUntilRenderFrameDeleted());
+
+  // Navigate back.
+  ASSERT_TRUE(HistoryGoBack(web_contents()));
+
+  // Verify navigation id is not incremented.
+  EXPECT_EQ(1, GetNavigationId("subsequent_nav"));
+}
+
+class PerformanceTimelinePrefetchTransferSizeBrowserTest
+    : public PerformanceTimelineBrowserTest {
+ protected:
+  EvalJsResult Prefetch() {
+    std::string script = R"(
+        (() => {
+          return addPrefetch();
+        })();
+    )";
+    return EvalJs(shell(), script);
+  }
+  [[nodiscard]] EvalJsResult GetTransferSize() {
+    std::string script = R"(
+        (() => {
+          return performance.getEntriesByType('navigation')[0].transferSize;
+        })();
+    )";
+    return EvalJs(shell(), script);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(PerformanceTimelinePrefetchTransferSizeBrowserTest,
+                       PrefetchTransferSize) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  const GURL prefetch_url(
+      embedded_test_server()->GetURL("a.com", "/title1.html"));
+  const GURL landing_url(embedded_test_server()->GetURL(
+      "a.com", "/performance_timeline/prefetch.html"));
+
+  EXPECT_TRUE(NavigateToURL(shell(), landing_url));
+  Prefetch();
+  EXPECT_TRUE(NavigateToURL(shell(), prefetch_url));
+  // Navigate to a prefetched url should result in a navigation timing entry
+  // with 0 transfer size.
+  EXPECT_EQ(0, GetTransferSize());
+}
+
+class PerformanceTimelineBackForwardCacheRestorationBrowserTest
+    : public PerformanceTimelineBrowserTest {
+ protected:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     ContentBrowserTest::SetUpCommandLine(command_line);
     base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
@@ -165,4 +328,5 @@ IN_PROC_BROWSER_TEST_F(
   // droppedEntriesCount is expected to be 2.
   EXPECT_EQ(2, GetDroppedEntriesCount().ExtractInt());
 }
+  
 }  // namespace content
