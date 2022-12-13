f5679d923d12860a05e87900bc62528e9795417e
Gabriel Oliveira <gabolvr@google.com>
2022-12-13 13:01:12 +0000

    [Privacy Sandbox] Add current topics section to Topics settings subpage
    Screenshots: https://crbug.com/1378703#c137
    
    Bug: 1378703, b/254412594, b/254413437, b/254412602, b/254412046
    Change-Id: I091d463bd421f05271fc0c889fd26df191d8a231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097189
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1082413}

--- chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_topics_subpage.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_sandbox/privacy_sandbox_topics_subpage.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import '../../controls/settings_toggle_button.js';
 import '../../prefs/prefs.js';
 
@@ -9,6 +10,7 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 
 import {SettingsToggleButtonElement} from '../../controls/settings_toggle_button.js';
 import {PrefsMixin} from '../../prefs/prefs_mixin.js';
+import {PrivacySandboxBrowserProxy, PrivacySandboxBrowserProxyImpl, PrivacySandboxInterest, TopicsState} from '../../privacy_sandbox/privacy_sandbox_browser_proxy.js';
 
 import {getTemplate} from './privacy_sandbox_topics_subpage.html.js';
 
@@ -40,7 +42,54 @@ export class SettingsPrivacySandboxTopicsSubpageElement extends
         type: Object,
         notify: true,
       },
+
+      topicsList_: {
+        type: Array,
+        value() {
+          return [];
+        },
+      },
+
+      /**
+       * Used to determine that the Topics list was already fetched and to
+       * display the current topics description only after the list is loaded,
+       * to avoid displaying first the description for an empty list since the
+       * array is empty at first when the page is loaded and switching to the
+       * default description once the list is fetched.
+       */
+      isTopicsListLoaded_: {
+        type: Boolean,
+        value: false,
+      },
     };
+  }
+
+  private topicsList_: PrivacySandboxInterest[];
+  private isTopicsListLoaded_: boolean;
+  private privacySandboxBrowserProxy_: PrivacySandboxBrowserProxy =
+      PrivacySandboxBrowserProxyImpl.getInstance();
+
+  override ready() {
+    super.ready();
+
+    this.privacySandboxBrowserProxy_.getTopicsState().then(
+        state => this.onTopicsStateChanged_(state));
+  }
+
+  private onTopicsStateChanged_(state: TopicsState) {
+    this.topicsList_ = state.topTopics.map(topic => {
+      return {topic, removed: false};
+    });
+    this.isTopicsListLoaded_ = true;
+  }
+
+  private isTopicsEnabledAndLoaded_(): boolean {
+    return this.getPref('privacy_sandbox.m1.topics_enabled').value &&
+        this.isTopicsListLoaded_;
+  }
+
+  private isTopicsListEmpty_(): boolean {
+    return this.topicsList_.length === 0;
   }
 }
 
