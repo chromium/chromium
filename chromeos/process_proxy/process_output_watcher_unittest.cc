c7ddfb6c65061c103b85ef20ec390439c3e9b995
Sergey Ulanov <sergeyu@chromium.org>
2022-12-13 05:04:02 +0000

    [Fuchsia] Disable all tests in ProcessOutputWatcherTest
    All tests in ProcessOutputWatcherTest appear to be flacky, but only some
    of them were disabled. This change disales all of them.
    
    Bug: 1395483
    Change-Id: I83ec525f6b78815113cf00a33cefab026ec88ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099211
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Owners-Override: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082288}

--- chromeos/process_proxy/process_output_watcher_unittest.cc
+++ chromeos/process_proxy/process_output_watcher_unittest.cc
@@ -200,7 +200,7 @@ class ProcessOutputWatcherTest : public testing::Test {
   std::vector<TestCase> exp;
 };
 
-TEST_F(ProcessOutputWatcherTest, OutputWatcher) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_OutputWatcher) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("t", false));
   test_cases.push_back(TestCase("testing output\n", false));
@@ -215,7 +215,7 @@ TEST_F(ProcessOutputWatcherTest, OutputWatcher) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, SplitUTF8Character) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_SplitUTF8Character) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("test1\xc2", false, "test1"));
   test_cases.push_back(TestCase("\xb5test1", false, "\xc2\xb5test1"));
@@ -223,7 +223,7 @@ TEST_F(ProcessOutputWatcherTest, SplitUTF8Character) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, SplitSoleUTF8Character) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_SplitSoleUTF8Character) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("\xc2", false, ""));
   test_cases.push_back(TestCase("\xb5", false, "\xc2\xb5"));
@@ -231,7 +231,7 @@ TEST_F(ProcessOutputWatcherTest, SplitSoleUTF8Character) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, SplitUTF8CharacterLength3) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_SplitUTF8CharacterLength3) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("test3\xe2\x82", false, "test3"));
   test_cases.push_back(TestCase("\xac", false, "\xe2\x82\xac"));
@@ -239,7 +239,7 @@ TEST_F(ProcessOutputWatcherTest, SplitUTF8CharacterLength3) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, SplitSoleUTF8CharacterThreeWays) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_SplitSoleUTF8CharacterThreeWays) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("\xe2", false, ""));
   test_cases.push_back(TestCase("\x82", false, ""));
@@ -256,14 +256,14 @@ TEST_F(ProcessOutputWatcherTest, DISABLED_EndsWithThreeByteUTF8Character) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, SoleThreeByteUTF8Character) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_SoleThreeByteUTF8Character) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("\xe2\x82\xac", false, "\xe2\x82\xac"));
 
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, HasThreeByteUTF8Character) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_HasThreeByteUTF8Character) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(
       TestCase("test\xe2\x82\xac_", false, "test\xe2\x82\xac_"));
@@ -279,7 +279,7 @@ TEST_F(ProcessOutputWatcherTest, DISABLED_MultiByteUTF8CharNullTerminated) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, MultipleMultiByteUTF8Characters) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_MultipleMultiByteUTF8Characters) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(
       TestCase("test\xe2\x82\xac\xc2", false, "test\xe2\x82\xac"));
@@ -288,7 +288,7 @@ TEST_F(ProcessOutputWatcherTest, MultipleMultiByteUTF8Characters) {
   RunTest(test_cases);
 }
 
-TEST_F(ProcessOutputWatcherTest, ContainsInvalidUTF8) {
+TEST_F(ProcessOutputWatcherTest, DISABLED_ContainsInvalidUTF8) {
   std::vector<TestCase> test_cases;
   test_cases.push_back(TestCase("\xc2_", false, "\xc2_"));
 
