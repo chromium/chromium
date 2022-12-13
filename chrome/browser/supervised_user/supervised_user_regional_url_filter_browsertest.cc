7074c3975966587cdeaf17c0883458a901d0b7f0
Tomasz Jurkiewicz <tju@google.com>
2022-12-13 13:43:31 +0000

    Creates a browser (integration) test to prove that regional settings are passed down to the URL classification backends.
    As a side effect: adds monitoring mechanism for the kids chrome
    management client (otherwise tests fail, because access token required
    to RPC is impossible to get).
    
    Bug: 1383973
    Change-Id: I394e047149818ff809d383e0e016604375403658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4085090
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082428}

---
+++ chrome/browser/supervised_user/supervised_user_regional_url_filter_browsertest.cc
@@ -0,0 +1,134 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "base/command_line.h"
+#include "base/functional/bind.h"
+#include "base/strings/string_piece.h"
+#include "base/strings/string_piece_forward.h"
+#include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
+#include "chrome/browser/supervised_user/kids_chrome_management/kids_chrome_management_client.h"
+#include "chrome/browser/supervised_user/kids_chrome_management/kids_chrome_management_client_factory.h"
+#include "chrome/browser/supervised_user/kids_chrome_management/kidschromemanagement_messages.pb.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/mixin_based_in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/variations/variations_switches.h"
+#include "content/public/test/browser_test.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/network/public/cpp/network_switches.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "url/gurl.h"
+
+namespace {
+
+using ::kids_chrome_management::ClassifyUrlRequest;
+using ::testing::_;
+using ::testing::NiceMock;
+using ::testing::Pointee;
+
+// Surprisingly, we don't have proto-comparators from gtest available. Remove
+// once they're available.
+MATCHER_P(EqualsProto,
+          message,
+          "Match a proto Message equal to the matcher's argument.") {
+  std::string expected_serialized, actual_serialized;
+  message.SerializeToString(&expected_serialized);
+  arg.SerializeToString(&actual_serialized);
+  return expected_serialized == actual_serialized;
+}
+
+// The region code for variations service (any should work).
+constexpr base::StringPiece kRegionCode = "jp";
+
+// Tests custom filtering logic based on regions, for supervised users.
+class SupervisedUserRegionalURLFilterTest
+    : public MixinBasedInProcessBrowserTest {
+ public:
+  SupervisedUserRegionalURLFilterTest() = default;
+  ~SupervisedUserRegionalURLFilterTest() override = default;
+
+ protected:
+  class MockKidsChromeManagementClient : public KidsChromeManagementClient {
+   public:
+    explicit MockKidsChromeManagementClient(Profile* profile)
+        : KidsChromeManagementClient(profile) {
+      // Without forwarding the call to the real implementation, the browser
+      // hangs and the test times out.
+      ON_CALL(*this, ClassifyURL)
+          .WillByDefault(
+              [this](std::unique_ptr<ClassifyUrlRequest> request_proto,
+                     ::KidsChromeManagementClient::KidsChromeManagementCallback
+                         callback) {
+                KidsChromeManagementClient::ClassifyURL(
+                    std::move(request_proto), std::move(callback));
+              });
+    }
+
+    MOCK_METHOD(
+        void,
+        ClassifyURL,
+        (std::unique_ptr<ClassifyUrlRequest> request_proto,
+         ::KidsChromeManagementClient::KidsChromeManagementCallback callback),
+        (override));
+
+    static std::unique_ptr<KeyedService> MakeUnique(
+        content::BrowserContext* context) {
+      return std::make_unique<NiceMock<MockKidsChromeManagementClient>>(
+          static_cast<Profile*>(context));
+    }
+  };
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ASSERT_TRUE(embedded_test_server()->Started());
+    std::string host_port = embedded_test_server()->host_port_pair().ToString();
+
+    // Remap all URLs in context of this test to the test server.
+    command_line->AppendSwitchASCII(network::switches::kHostResolverRules,
+                                    "MAP *.example.com " + host_port);
+
+    command_line->AppendSwitchASCII(
+        variations::switches::kVariationsOverrideCountry, kRegionCode);
+    MixinBasedInProcessBrowserTest::SetUpCommandLine(command_line);
+  }
+
+  void SetUpOnMainThread() override {
+    MixinBasedInProcessBrowserTest::SetUpOnMainThread();
+    logged_in_user_mixin_.LogInUser();
+
+    kids_chrome_management_client_ =
+        static_cast<MockKidsChromeManagementClient*>(
+            KidsChromeManagementClientFactory::GetInstance()
+                ->SetTestingFactoryAndUse(
+                    browser()->profile(),
+                    base::BindRepeating(
+                        &MockKidsChromeManagementClient::MakeUnique)));
+  }
+
+  MockKidsChromeManagementClient* kids_chrome_management_client_;
+  ash::LoggedInUserMixin logged_in_user_mixin_{
+      &mixin_host_, ash::LoggedInUserMixin::LogInType::kChild,
+      embedded_test_server(), this};
+};
+
+// Verifies that the regional setting is passed to the RPC backend.
+IN_PROC_BROWSER_TEST_F(SupervisedUserRegionalURLFilterTest, RegionIsAdded) {
+  std::string url_to_classify =
+      "http://www.example.com/simple.html";  // The hostname must be handled by
+                                             // embedded server, see {@link
+                                             // SetUpCommandLine}.
+
+  ClassifyUrlRequest expected;
+  expected.set_region_code(std::string(kRegionCode));
+  expected.set_url(url_to_classify);
+  EXPECT_CALL(*kids_chrome_management_client_,
+              ClassifyURL(Pointee(EqualsProto(expected)), /* callback= */ _));
+
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL(url_to_classify)));
+}
+
+}  // namespace
