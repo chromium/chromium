47100e88429b4101dfc9da732e3c828b1c974a17
Camden King <camdenking@google.com>
2022-12-13 05:43:34 +0000

    [dPWA] Renaming WebAppProvider::sync_bridge to sync_bridge_unsafe
    This CL is migrating usage of WebAppProvider::sync_bridge to
    WebAppProvider::sync_bridge_unsafe before WebAppProvider::sync_bridge
    is removed.
    
    For safe access use locks (see go/access-webappprovider-through-locks).
    
    More info linked in crbug.com/1399641.
    
    There is no functional change in this CL.
    
    This CL was uploaded by git cl split.
    
    R=cmp@chromium.org
    
    Bug: 1399641, b/260863676
    Change-Id: Ie5cf84e4b6f95f2e4f0216df6358a66e1dc38ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098368
    Auto-Submit: Camden King <camdenking@google.com>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082301}

--- chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
@@ -389,7 +389,7 @@ IN_PROC_BROWSER_TEST_F(PwaInstallViewBrowserTest,
 
   // Change launch container to open in tab.
   web_app::WebAppProvider::GetForTest(browser()->profile())
-      ->sync_bridge()
+      ->sync_bridge_unsafe()
       .SetAppUserDisplayMode(app_id, web_app::UserDisplayMode::kBrowser,
                              /*is_user_action=*/false);
 
