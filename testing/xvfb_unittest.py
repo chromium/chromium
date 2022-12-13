3e50f0c79a7602b366b9dadb1464737616a4b92b
Erik Staab <estaab@google.com>
2022-12-13 22:58:25 +0000

    Reduce flakiness in xvfb unittests
    This adds a short sleep to give time for the subprocess to register
    signal handlers before the test sends it a signal. This doesn't add
    additional run time because the subprocess already waits 2s before
    exiting.
    
    See experiment results in crbug.com/1400578.
    
    Bug: 1400578
    Change-Id: I87382dc5383c3a091e008c957bf98be822f91181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098812
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082782}

--- testing/xvfb_unittest.py
+++ testing/xvfb_unittest.py
@@ -106,6 +106,8 @@ class XvfbTest(unittest.TestCase):
 
   def test_send_sigint(self):
     self._proc = launch_process(['--sleep'])
+    # Give time for subprocess to install signal handlers
+    time.sleep(.3)
     send_signal(self._proc, signal.SIGINT, 1)
     sig = read_subprocess_message(self._proc, 'Signal :')
     self.assertIsNotNone(sig) # OpenBox likely failed to start
@@ -113,6 +115,8 @@ class XvfbTest(unittest.TestCase):
 
   def test_send_sigterm(self):
     self._proc = launch_process(['--sleep'])
+    # Give time for subprocess to install signal handlers
+    time.sleep(.3)
     send_signal(self._proc, signal.SIGTERM, 1)
     sig = read_subprocess_message(self._proc, 'Signal :')
     self.assertIsNotNone(sig) # OpenBox likely failed to start
