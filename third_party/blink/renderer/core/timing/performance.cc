f016b84656ae29777cf577b0363fb3ceb88174ca
Jaspreet Sandhu <jaspreetsandhu@google.com>
2022-12-14 22:33:43 +0000

    Use SecurityOrigin when getting cross-frame same-origin performance entries
    This change adds logic to compare child-frame origins with the frame that calls the Performance Timeline methods, instead of defaulting to the top of the frame tree.
    
    This addresses the case where a remote frame, that isn't the top-level frame, tries to get entries from a subframe that is local to the top-level frame.
    
    Change-Id: I7f6a10f7c3438b970746078b3e97f5c05f13af6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4097824
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Jaspreet Sandhu <jaspreetsandhu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083392}

--- third_party/blink/renderer/core/timing/performance.cc
+++ third_party/blink/renderer/core/timing/performance.cc
@@ -483,41 +483,49 @@ PerformanceEntryVector Performance::GetEntriesWithChildFrames(
   if (!window) {
     return entries;
   }
-  LocalFrame* parent_frame = window->GetFrame();
-  if (!parent_frame) {
+  LocalFrame* root_frame = window->GetFrame();
+  if (!root_frame) {
     return entries;
   }
+  const SecurityOrigin* root_origin =
+      ExecutionContext::From(script_state)->GetSecurityOrigin();
 
   HeapDeque<Member<Frame>> queue;
-  queue.push_back(parent_frame);
+  queue.push_back(root_frame);
 
   while (!queue.empty()) {
     Frame* current_frame = queue.TakeFirst();
 
-    if (LocalFrame* local_frame = DynamicTo<LocalFrame>(current_frame);
-        local_frame && !local_frame->IsCrossOriginToNearestMainFrame()) {
+    if (LocalFrame* local_frame = DynamicTo<LocalFrame>(current_frame)) {
       // Get the Performance object from the current frame.
       LocalDOMWindow* current_window = local_frame->DomWindow();
       // As we verified that the frame this was called with is not detached when
       // entring this loop, we can assume that all its children are also not
       // detached, and hence have a window object.
       DCHECK(current_window);
-      WindowPerformance* window_performance =
-          DOMWindowPerformance::performance(*current_window);
 
-      // Get the performance entries based on entry_type input.
-      PerformanceEntryVector current_entries;
-      if (entry_type.IsNull()) {
-        current_entries = window_performance->GetEntriesForCurrentFrame();
-      } else {
-        current_entries =
-            window_performance->GetEntriesByTypeForCurrentFrame(entry_type);
+      // Validate that the child frame's origin is the same as the root
+      // frame.
+      const SecurityOrigin* current_origin =
+          current_window->GetSecurityOrigin();
+      if (root_origin->IsSameOriginWith(current_origin)) {
+        WindowPerformance* window_performance =
+            DOMWindowPerformance::performance(*current_window);
+
+        // Get the performance entries based on entry_type input.
+        PerformanceEntryVector current_entries;
+        if (entry_type.IsNull()) {
+          current_entries = window_performance->GetEntriesForCurrentFrame();
+        } else {
+          current_entries =
+              window_performance->GetEntriesByTypeForCurrentFrame(entry_type);
+        }
+
+        entries.AppendVector(current_entries);
       }
-
-      entries.AppendVector(current_entries);
     }
 
-    // Add both Local and Remote Frames to the queue.
+    // Add both Local and Remote Frame children to the queue.
     for (Frame* child = current_frame->FirstChild(); child;
          child = child->NextSibling()) {
       queue.push_back(child);
---
+++ third_party/blink/web_tests/virtual/not-site-per-process-nonexclusive/README.md
@@ -0,0 +1,5 @@
+# virtual/not-site-per-process
+
+## Summary
+
+See virtual/not-site-per-process. This virtual test suite removes the `exclusive_tests: "ALL"` configuration, and specifies tests that should be run both with and without site isolation enabled.
