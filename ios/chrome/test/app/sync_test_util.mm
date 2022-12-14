443cfef23e5a37535b83a6b555d3a3df5a3fafef
Vincent Boisselle <vincb@google.com>
2022-12-14 15:21:14 +0000

    [iOS][signin] Set a persistent dir for the Sync Fake Server
    This is to make sure that, across browser sessions, the data stored
    in the server match the data of the client. This is particularly the
    case for the birthday, which has to match between the client and the
    server. The client cannot persist a birthday that is different from
    the one provided by the server.
    
    The data in the client is already persisted in the user data
    directory.
    
    Currently, the server data lives as long as the browser process
    because the Fake Server is using a tmp directory.
    
    Keeping the server data across browser sessions will allow performing
    more complex test cases that need the Sync state to persist across
    browser sessions within the same test case.
    
    The
    UserPolicyTestCase/testUserPolicyNotificationWithAcceptChoice test case
    is a an example of a test that requires to relaunch the browser with
    the same Sync state as the previous session. It tests a particular
    startup sequence that requires Sync ON. From time to time, the test
    would fail because of a mismatch between the birthday provided by
    the server and the one store by the client. Sync is turned off when
    there is a mismatch, which breaks the test (which assumes that
    Sync is turned ON at that moment). Sync calls are random, explaining
    the flakes (no sync call, test fine).
    
    Considering that the app data directory is already used to persist the
    Sync Fake Server data in Desktop browser tests, we can assume that
    using the same approach on iOS won't have bad side effects.
    
    Bug: 1400633
    Change-Id: I4d00c09ca89dac4859b2f3b606797793a5b94be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4098551
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1083098}

--- ios/chrome/test/app/sync_test_util.mm
+++ ios/chrome/test/app/sync_test_util.mm
@@ -9,8 +9,10 @@
 
 #import "base/bind.h"
 #import "base/check.h"
+#import "base/files/file_path.h"
 #import "base/guid.h"
 #import "base/memory/ptr_util.h"
+#import "base/path_service.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
 #import "base/test/ios/wait_util.h"
@@ -90,7 +92,11 @@ bool IsFakeSyncServerSetUp() {
 
 void SetUpFakeSyncServer() {
   DCHECK(!gSyncFakeServer);
-  gSyncFakeServer = new fake_server::FakeServer();
+
+  base::FilePath dir_path;
+  DCHECK(base::PathService::Get(base::DIR_APP_DATA, &dir_path))
+      << "couldn't create directory for Sync Fake Server";
+  gSyncFakeServer = new fake_server::FakeServer(dir_path);
   OverrideSyncNetwork(fake_server::CreateFakeServerHttpPostProviderFactory(
       gSyncFakeServer->AsWeakPtr()));
 }
