ef771fc1bfc5d69d937d30f16d56e796e939f145
liqining.keiling <liqining.keeling@bytedance.com>
2022-12-14 08:44:26 +0000

    AppHome: Implements uninstall function on apps' action menu.
    For background & design document, see following document:
     https://docs.google.com/document/d/1I_Ot2Wuyyztt1YIE61rpkeXewaMnPzfMNF-6qCbdtVw/edit#heading=h.q3ma6p1wdg55
    
    Bug: 1350406
    Change-Id: I356a0589be5cd67ba2b32b983e721655b44b221a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4083053
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1082976}

--- chrome/browser/resources/app_home/app_list.ts
+++ chrome/browser/resources/app_home/app_list.ts
@@ -50,6 +50,8 @@ export class AppListElement extends PolymerElement {
   private apps_: AppInfo[];
   private mojoEventTarget_: PageCallbackRouter;
   private listenerIds_: number[];
+  // The app context menu that's currently click opened by user.
+  private selectedActionMenuModel_: ActionMenuModel|null = null;
 
   constructor() {
     super();
@@ -114,7 +116,11 @@ export class AppListElement extends PolymerElement {
   }
 
   private onUninstallItemClick_() {
-    this.$.menu.close();
+    if (this.selectedActionMenuModel_?.data.id) {
+      BrowserProxy.getInstance().handler.uninstallApp(
+          this.selectedActionMenuModel_?.data.id);
+    }
+    this.closeMenu_();
   }
 
   private onAppSettingsItemClick_() {
@@ -122,10 +128,16 @@ export class AppListElement extends PolymerElement {
   }
 
   private onOpenMenu_(event: OpenMenuEvent) {
+    this.selectedActionMenuModel_ = event.detail;
     this.$.menu.showAtPosition({
       top: event.detail.event.clientY,
       left: event.detail.event.clientX,
     });
+  }
+
+  private closeMenu_() {
+    this.selectedActionMenuModel_ = null;
+    this.$.menu.close();
   }
 }
 
