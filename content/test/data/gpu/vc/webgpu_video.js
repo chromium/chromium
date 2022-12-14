f928ec4af9ecaf15f13acb51c42228af724be8b6
Yan,Shaobo <shaobo.yan@intel.com>
2022-12-14 06:33:54 +0000

    Use textureSampleBaseClampToEdge() to sample texture_external
    Wgsl spec updates breaks vc samples. Current spec requires using
    textureSampleBaseClampToEdge() to sample texture_external.
    
    Bug:chromium:1289482
    
    Change-Id: I51940aae72ba4b9d0f790c338c449fc52d7dfc00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4093493
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1082940}

--- content/test/data/gpu/vc/webgpu_video.js
+++ content/test/data/gpu/vc/webgpu_video.js
@@ -53,7 +53,7 @@ struct VertexOutput {
 
 @fragment
 fn main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {
-  return textureSampleLevel(myTexture, mySampler, fragUV);
+  return textureSampleBaseClampToEdge(myTexture, mySampler, fragUV);
 }
 `,
 
