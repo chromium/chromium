b6c7a241fed1b9af3c486b606538a3cd4993cbba
Andrés Olivares <andoli@chromium.org>
2022-12-15 14:58:28 +0000

    Add tests for Garbage Collection trace events
    Bug: 1383130
    Change-Id: Ic4a9a179899a52da1261425ff0b9548d2380f184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4106548
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083743}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/garbage-collector.js
@@ -0,0 +1,27 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      'Tests the data of Garbage Collection trace events');
+
+  const TracingHelper =
+      await testRunner.loadScript('../resources/tracing-test.js');
+  const tracingHelper = new TracingHelper(testRunner, session);
+  const Phase = TracingHelper.Phase;
+  await tracingHelper.startTracing('devtools.timeline,v8');
+  await session.evaluateAsync(`
+        GCController.minorCollect();
+        GCController.collectAll();
+    `);
+  await tracingHelper.stopTracing(/devtools\.timeline|v8/);
+  const minorGC = tracingHelper.findEvent('MinorGC', Phase.COMPLETE);
+  const majorGC = tracingHelper.findEvent('MajorGC', Phase.COMPLETE);
+
+  testRunner.log('Got a MajorGC event');
+  tracingHelper.logEventShape(majorGC);
+  testRunner.log('Got a MinorGC event');
+  tracingHelper.logEventShape(minorGC);
+  testRunner.completeTest();
+})
