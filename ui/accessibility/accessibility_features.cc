f8bd7886bc989c536affc76d75cf9f266806b43e
Josiah Krutz <josiahk@google.com>
2022-12-15 02:01:39 +0000

    [Select-to-Speak] Enable Select-to-Speak Settings in Chrome OS Settings
    Enables Select-to-Speak Settings in Chrome OS Settings, replacing the
    link to the old Select-to-Speak Extension Options Settings page, from
    the Text-to-speech Chrome OS Accessibility Settings page.
    go/select-to-speak-and-chromevox-page-migration
    
    AX-Relnotes: N/A.
    Bug: 1354821
    Change-Id: I8ae97a4b82be0b63944713371357aebe3c3ed31f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4107550
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083508}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -185,7 +185,7 @@ bool AreExperimentalAccessibilityColorEnhancementSettingsEnabled() {
 
 BASE_FEATURE(kAccessibilitySelectToSpeakPageMigration,
              "AccessibilitySelectToSpeakPageMigration",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsAccessibilitySelectToSpeakPageMigrationEnabled() {
   return base::FeatureList::IsEnabled(
