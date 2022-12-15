ac3617e2a610800faf0f7fbe46407728dc90749d
Nasko Oskov <nasko@chromium.org>
2022-12-15 21:38:41 +0000

    Add a teardown check in ChildProcessSecurityPolicy unit tests.
    This CL adds a check at teardown time for unit tests to ensure we don't
    have residual state between tests. It helps catch issues like the one
    fixed in r1083411.
    
    Bug: 1286533, 1394513
    Change-Id: If8d26f20f75130c868fda978117d8aa40181b330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109353
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083991}

--- content/browser/child_process_security_policy_unittest.cc
+++ content/browser/child_process_security_policy_unittest.cc
@@ -130,6 +130,14 @@ class ChildProcessSecurityPolicyTest : public testing::Test {
   }
 
   void TearDown() override {
+    auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
+    {
+      base::AutoLock lock(policy->lock_);
+      EXPECT_EQ(0u, policy->security_state_.size())
+          << "ChildProcessSecurityPolicy should not be tracking any processes "
+          << "at test shutdown.  Did you forget to call Remove() at the end of "
+          << "a test?";
+    }
     test_browser_client_.ClearSchemes();
     SetBrowserClientForTesting(old_browser_client_);
   }
