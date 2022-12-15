26eeb1dcfa7287e1034125a9c7bb8f262c79acd2
Jack Yammine <jyammine@google.com>
2022-12-15 12:29:00 +0000

    [fyfre]Add dialog view dark mode
    Bug: 1374702
    Change-Id: Ie253d7b0be95f2ed6d48ba100fa2dd8906193561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4100747
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083686}

--- chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
+++ chrome/browser/ui/webui/signin/sync_confirmation_ui.cc
@@ -216,9 +216,13 @@ void SyncConfirmationUI::InitializeForSyncConfirmation(
         isSigninInterceptFre
             ? IDS_SYNC_CONFIRMATION_TANGIBLE_SYNC_INFO_TITLE_SIGNIN_INTERCEPT
             : IDS_SYNC_CONFIRMATION_TANGIBLE_SYNC_INFO_TITLE;
+
     illustration_path = "images/tangible_sync_illustration.svg";
+    illustration_dark_path = "images/tangible_sync_illustration_dark.svg";
     illustration_id =
         IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_TANGIBLE_SYNC_ILLUSTRATION_SVG;
+    illustration_dark_id =
+        IDR_SIGNIN_SYNC_CONFIRMATION_IMAGES_TANGIBLE_SYNC_ILLUSTRATION_DARK_SVG;
   }
 
   // Registering and resolving the strings with placeholders
