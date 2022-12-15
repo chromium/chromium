0611a4c1cb411955c404d41a093958e393d3fdbb
Lingqi Chi <lingqi@chromium.org>
2022-12-15 01:59:17 +0000

    Prerender: Grant interfaces that can make sync IPCs during activation
    Prerender has to preform two-stage Mojo capability control to ensure
    the status on the browser process and renderer processes are on sync.
    For the second stage, prerender allows interfaces that can make sync
    IPCs whose policies should be kCancel/kGrant to make IPCs.
    However, some interfaces are treated in a different way: We defer the
    binding requests on the renderer side, and resume them upon renderer
    activation, so we can mark them kDefer. For these interfaces, they
    should be treated as kGrant interfaces during the second stage.
    
    So this CL fixes this issue which causes renderer hangs.
    
    It is unfortunate that we have to add some hardcode rules for these
    interfaces. This CL Left some TODOs that requires
    I533dd0f0438d493ad8a7a74479526827bf93e1d9 to land first.
    
    Bug: 1385623
    Change-Id: I180c6f8b0cc37c4157346cc5a556cc07f8c2b1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4082358
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1083506}

--- content/browser/mojo_binder_policy_applier.h
+++ content/browser/mojo_binder_policy_applier.h
@@ -9,6 +9,7 @@
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
 #include "content/browser/mojo_binder_policy_map_impl.h"
 #include "content/common/content_export.h"
@@ -111,11 +112,17 @@ class CONTENT_EXPORT MojoBinderPolicyApplier {
       MojoBinderNonAssociatedPolicy::kDefer;
   // Maps Mojo interface name to its policy.
   const raw_ref<const MojoBinderPolicyMapImpl> policy_map_;
+
   // Will be executed upon a request for a kCancel interface.
   base::OnceCallback<void(const std::string& interface_name)> cancel_callback_;
   Mode mode_ = Mode::kEnforce;
+
   // Stores binders which are delayed running.
   std::vector<base::OnceClosure> deferred_binders_;
+
+  // Stores binders that can be used to send synchronous messages but
+  // are delayed running.
+  std::vector<base::OnceClosure> deferred_sync_binders_;
 };
 
 }  // namespace content
