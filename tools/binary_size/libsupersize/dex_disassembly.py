b4710e6a260dfdf0fc969948e84b68c7fb8e6438
Andreea Costinas <acostinas@google.com>
2022-12-16 15:39:30 +0000

    Speculative revert "[SuperSize] Parse improved R8 disassembly format."
    This reverts commit 951f0b8fea37ee425f558bd6ffe3bc7ebadc05eb.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/try/android-binary-size/1332277/overview
    
    Original change's description:
    > [SuperSize] Parse improved R8 disassembly format.
    >
    > SuperSize-save_diff can store (for top-changed methods) Dalvik bytecode
    > extracted from R8's disassemble tool. Previously, class names were
    > deobfuscated by parsing and using .mapping files.
    >
    > As seen in b/221855899 recent (2022-11) R8 disassembly output was
    > changed to deobfuscate method params. However, this output became
    > incompatible with SuperSize's parser.
    >
    > This CL updates SuperSize's R8 disassembly parser:
    > * Adapt to the new format.
    > * Refactor to enable input streaming, while decoupling raw data
    >   extraction from interpretation.
    > * Add heuristics to better handle ambiguity for methods (" <OR> ").
    >
    > Details:
    > * dex_disassembly.py:
    >   * Add class _CachedApkDisassembler to encapsulate .mapping file
    >     assignment, R8 disassemble call, |class_obj_map| (from class name
    >     to DexClass object) calculation, and lookup / caching of results.
    >   * Add constant |_DISASSEMBLED_METHOD_QUOTA|.
    >   * Rewrite main() tester to use an interactive shell to examine
    >     disassembly data.
    >   * Remove method return & param types deobfuscation.
    > * Add r8_disassembly.py:
    >   * Add {DexMethod, DexClass} which were previously
    >     {_Method, _Class} in dex_disassembly.py.
    >   * Parsing: decouple raw data extraction from interpretation:
    >     interpretation  :
    >     * Raw data extraction:
    >       * Add class _WrapPeekableNoNewLine to enable single-line look-
    >         ahead for streaming input processing.
    >       * Add _ExtractMethodInfo() to parse "# Method" blocks and emit
    >         {|method_str|, |residual_str|, |signature|, |byte_code|}.
    >       * Add _ExtractClassNameAndMethodInfos to emit |class_name| and
    >         result list from _ExtractMethodInfo().
    >     * Interpretation:
    >       * Add _SplitMethod() to get types and names of method signature.
    >       * Add _CompareSignatures() to heuristically score similarity
    >         between two method signatures, to handle ambiguous results.
    >       * Handle the potential of "broken" method signature.
    >       * Add _ExtractMethodLegacy() for backward compatibility, where R8
    >         disassembly "Method" data had method name only.
    >       * Add _ExtractMethod() to interpret new data and resolve
    >         ambiguity.
    >   * Add Parse() to replace dex_disassembly._ParseDisassembly():
    >     * Parses R8 disassembly output into |class_obj_map| (deobfuscated).
    >     * Calls the above while tracking and printing stats.
    >     * (New) Returns |anomalies| to aid future analysis.
    > * Update tests (adapted from from crrev.com/c/4024396).
    >
    >
    > Bug: 1395097
    > Change-Id: I6651e649e9ea0ce10d32ed5c7febfa42d8ac5f33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4070824
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1084140}
    
    Bug: 1395097
    Change-Id: I5610475e479d4795737634ba0837accf768f2c2c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111050
    Commit-Queue: Andreea Costinas <acostinas@google.com>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Owners-Override: Andreea Costinas <acostinas@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084324}

--- tools/binary_size/libsupersize/dex_disassembly.py
+++ tools/binary_size/libsupersize/dex_disassembly.py
@@ -4,100 +4,151 @@
 # found in the LICENSE file.
 """Class to get the dex disassembly for symbols."""
 
-import argparse
-import code
+import dataclasses
 import difflib
-import functools
-import io
+import json
 import logging
 import os
+import pathlib
 import re
-import readline  # Makes code.InteractiveConsole works better.
 import subprocess
 import sys
 import tempfile
 import zipfile
 
-import r8_disassembly
+import archive
+import dex_deobfuscate
+import models
 import path_util
 import zip_util
 
-
-_DISASSEMBLED_METHOD_QUOTA = 10
 _SYMBOL_FULL_NAME_RE = re.compile(r'(.*?)#(.*?)\((.*?)\):? ?(.*)')
+_R8_OUTPUT_RE = re.compile(r"'([^']+)'")
+_R8_PARAM_RE = re.compile(r'\(.*?\)')
 
 
-class _CachedApkDisassembler:
-  def __init__(self):
-    self._proguard_mapping_file_path_lookup = {}
-
-  def AssignProguardMappingPath(self, apk_file_path,
-                                proguard_mapping_file_path):
-    self._proguard_mapping_file_path_lookup[apk_file_path] = (
-        proguard_mapping_file_path)
-
-  def _DisassembleApk(self, mapping, apk_path):
-    r8_path = path_util.GetR8Path()
-    r8_output = None
-    # Temporary hack until next R8 roll:
-    # Prevents R8 failing due to assets/webapk7.dex.
-    with tempfile.NamedTemporaryFile(mode='wb', suffix='.apk') as tmp_file:
-      with zipfile.ZipFile(tmp_file, 'w') as dst_zip:
-        with zipfile.ZipFile(apk_path) as src_zip:
-          for info in src_zip.infolist():
-            if info.filename.startswith('classes'):
-              dst_zip.writestr(info, src_zip.read(info))
-      tmp_file.flush()
-
-      cmd = [
-          'java', '-cp', r8_path, 'com.android.tools.r8.Disassemble',
-          '--pg-map', mapping, tmp_file.name
-      ]
-      try:
-        r8_output = subprocess.check_output(cmd, encoding='utf8')
-      except subprocess.CalledProcessError:
-        logging.debug('Running R8 failed on APK: %s', apk_path)
-
-    return r8_output
-
-  @functools.lru_cache(None)
-  def GetForApkAndSplit(self, apk_file_path, split_name):
-    proguard_mapping_file_path = (
-        self._proguard_mapping_file_path_lookup[apk_file_path])
-    r8_output = None
-    if split_name:
-      logging.info('Creating disassembly for APK split: %s', split_name)
-      with zip_util.UnzipToTemp(
-          apk_file_path, f'splits/{split_name}-master.apk') as split_path:
-        r8_output = self._DisassembleApk(proguard_mapping_file_path, split_path)
-    elif apk_file_path.endswith('.apk'):
-      logging.info('Creating disassembly for APK: %s', apk_file_path)
-      r8_output = self._DisassembleApk(proguard_mapping_file_path,
-                                       apk_file_path)
-    if r8_output is None:
-      return None
-    class_obj_map, _ = r8_disassembly.Parse(io.StringIO(r8_output))
-    return class_obj_map
+@dataclasses.dataclass
+class _Method:
+  name: str
+  class_name: str
+  param_types: list = None
+  return_type: str = None
+  bytecode: list = None
 
 
-def _ExtractDisassemblyForMethod(class_obj_map, method):
+class _Class:
+  def __init__(self, name):
+    self.name = name
+    self.methods = []
+
+  def _FindMethodByteCode(self, class_name, method_name, param_types,
+                          return_type):
+    for method in self.methods:
+      if (method.name == method_name and method.class_name == class_name
+          and method.return_type == return_type
+          and method.param_types == param_types):
+        return method.bytecode
+    return None
+
+
+def _DisassembleApk(mapping, apk_path):
+  r8_path = path_util.GetR8Path()
+  r8_output = None
+  # Temporary hack until next R8 roll:
+  # Prevents R8 failing due to assets/webapk7.dex.
+  with tempfile.NamedTemporaryFile(mode='wb', suffix='.apk') as tmp_file:
+    with zipfile.ZipFile(tmp_file, 'w') as dst_zip:
+      with zipfile.ZipFile(apk_path) as src_zip:
+        for info in src_zip.infolist():
+          if info.filename.startswith('classes'):
+            dst_zip.writestr(info, src_zip.read(info))
+    tmp_file.flush()
+
+    cmd = [
+        'java', '-cp', r8_path, 'com.android.tools.r8.Disassemble', '--pg-map',
+        mapping, tmp_file.name
+    ]
+    try:
+      r8_output = subprocess.check_output(cmd, encoding='utf8')
+    except subprocess.CalledProcessError:
+      logging.debug('Running R8 failed on APK: %s', apk_path)
+
+  return r8_output
+
+
+def _ParseDisassembly(disassembly):
+  disassembly = disassembly.splitlines(keepends=True)
+  classes = {}
+  current_class = None
+  current_method = None
+  reading_positions = False
+  reading_methods = False
+  for idx, line in enumerate(disassembly):
+    line = line.strip()
+    if line.startswith('# Class:'):
+      # New class started, no longer reading methods.
+      reading_methods = False
+      # Example of line: '# Class: \'className\''
+      match = _R8_OUTPUT_RE.search(line)
+      if match:
+        current_class = _Class(match.group(1))
+        classes[current_class.name] = current_class
+    elif line.startswith('# Method:') and current_class:
+      # Example of line: '# Method: \'methodName\':'
+      match = _R8_OUTPUT_RE.search(line)
+      if match:
+        reading_methods = True
+        current_method = _Method(match.group(1), current_class.name)
+        current_class.methods.append(current_method)
+    elif reading_methods and line.startswith('registers:'):
+      # Example of line: 'registers: 1, inputs: 1, outputs: 1'
+      assert idx > 0
+      header = disassembly[idx - 1]
+      # Example of header: 'returnType className.methodName(param1, param2)'
+      return_type = header.split()[0]
+      current_method.return_type = return_type
+      match = _R8_PARAM_RE.search(header)
+      if match:
+        params = (match.group(0))[1:-1]
+        current_method.param_types = params.split(', ') if params else []
+        bytecode_start = idx
+        reading_positions = True
+    elif reading_positions and line == '':
+      current_method.bytecode = disassembly[bytecode_start:idx]
+      reading_positions = False
+  return classes
+
+
+def _ChangeObfusactedNames(disassembly, class_deobfuscate_map):
+  for _, value in disassembly.items():
+    for method in value.methods:
+      method.return_type = class_deobfuscate_map.get(method.return_type,
+                                                     method.return_type)
+      if method.param_types:
+        for idx, param in enumerate(method.param_types):
+          method.param_types[idx] = class_deobfuscate_map.get(param, param)
+  return disassembly
+
+
+def _ComputeDisassemblyForSymbol(deobfuscated_disassembly, symbol_full_name):
   param_types = None
   return_type = None
   bytecode = None
-  # Example of method:
+  # Example of symbol_full_name:
   # className#methodName(param1,param2): returnType
-  m = _SYMBOL_FULL_NAME_RE.match(method)
+  m = _SYMBOL_FULL_NAME_RE.match(symbol_full_name)
   if m:
     class_name, method_name, param_types, return_type = m.groups()
     param_types = param_types.split(',') if param_types else []
-    class_obj = class_obj_map.get(class_name)
-    if class_obj is not None:
-      bytecode = class_obj.FindMethodByteCode(class_name, method_name,
-                                              param_types, return_type)
+    disassembly = deobfuscated_disassembly.get(class_name)
+    if disassembly is not None:
+      bytecode = disassembly._FindMethodByteCode(class_name, method_name,
+                                                 param_types, return_type)
   return bytecode
 
 
-def _ComputeDisassemblyForSymbol(symbol, path_resolver, apk_disassembler_cache):
+def _CaptureDisassemblyForSymbol(symbol, apk_to_disassembly, path_resolver,
+                                 dex_deobfuscator_cache):
   logging.debug('Attempting to capture disassembly for symbol %s',
                 symbol.full_name)
   container = symbol.container
@@ -109,12 +160,28 @@ def _ComputeDisassemblyForSymbol(symbol, path_resolver, apk_disassembler_cache):
   proguard_mapping_file_path = path_resolver(proguard_mapping_file_name)
   apk_file_name = container.metadata['apk_file_name']
   apk_file_path = path_resolver(apk_file_name)
-  split_name = container.metadata.get('apk_split_name')  # Can be None.
-  apk_disassembler_cache.AssignProguardMappingPath(apk_file_path,
-                                                   proguard_mapping_file_path)
-  class_obj_map = apk_disassembler_cache.GetForApkAndSplit(
-      apk_file_path, split_name)
-  return _ExtractDisassemblyForMethod(class_obj_map, symbol.full_name)
+  split_name = container.metadata.get('apk_split_name')
+  cache_key = (apk_file_path, split_name)
+  disassembly = apk_to_disassembly.get(cache_key)
+  if disassembly is None:
+    r8_output = None
+    if split_name:
+      logging.info('Creating disassmebly for APK split: %s', split_name)
+      with zip_util.UnzipToTemp(
+          apk_file_path, f'splits/{split_name}-master.apk') as split_path:
+        r8_output = _DisassembleApk(proguard_mapping_file_path, split_path)
+    elif apk_file_path.endswith('.apk'):
+      logging.info('Creating disassmebly for APK: %s', apk_file_name)
+      r8_output = _DisassembleApk(proguard_mapping_file_path, apk_file_path)
+    if r8_output is None:
+      return None
+    class_deobfuscation_map = (
+        dex_deobfuscator_cache.GetForMappingFile(proguard_mapping_file_path))
+    logging.debug('Changing obfuscated names...')
+    disassembly = _ChangeObfusactedNames(_ParseDisassembly(r8_output),
+                                         class_deobfuscation_map)
+    apk_to_disassembly[cache_key] = disassembly
+  return _ComputeDisassemblyForSymbol(disassembly, symbol.full_name)
 
 
 def _CreateUnifiedDiff(name, before, after):
@@ -132,22 +199,25 @@ def _CreateUnifiedDiff(name, before, after):
 def _AddUnifiedDiff(top_changed_symbols, before_path_resolver,
                     after_path_resolver):
   # Counter used to skip over symbols where we couldn't find the disassembly.
-  counter = _DISASSEMBLED_METHOD_QUOTA
+  counter = 10
   before = None
   after = None
-  after_apk_disassembler_cache = _CachedApkDisassembler()
-  before_apk_disassembler_cache = _CachedApkDisassembler()
+  before_apk_to_disassembly = {}
+  after_apk_to_disassembly = {}
+  dex_deobfuscator_cache = dex_deobfuscate.CachedDexDeobfuscators()
   for symbol in top_changed_symbols:
     logging.debug('Symbols to go: %d', counter)
-    after = _ComputeDisassemblyForSymbol(symbol.after_symbol,
+    after = _CaptureDisassemblyForSymbol(symbol.after_symbol,
+                                         after_apk_to_disassembly,
                                          after_path_resolver,
-                                         after_apk_disassembler_cache)
+                                         dex_deobfuscator_cache)
     if after is None:
       continue
     if symbol.before_symbol:
-      before = _ComputeDisassemblyForSymbol(symbol.before_symbol,
+      before = _CaptureDisassemblyForSymbol(symbol.before_symbol,
+                                            before_apk_to_disassembly,
                                             before_path_resolver,
-                                            before_apk_disassembler_cache)
+                                            dex_deobfuscator_cache)
     else:
       before = None
     logging.info('Adding disassembly for: %s', symbol.full_name)
@@ -156,6 +226,7 @@ def _AddUnifiedDiff(top_changed_symbols, before_path_resolver,
     counter -= 1
     if counter == 0:
       break
+
 
 def _GetTopChangedSymbols(delta_size_info):
   def filter_symbol(symbol):
@@ -179,7 +250,7 @@ def AddDisassembly(delta_size_info, before_path_resolver, after_path_resolver):
   """Adds disassembly diffs to top changed dex symbols.
 
     Adds the unified diff on the "before" and "after" disassembly to the
-    top |_DISASSEMBLED_METHOD_QUOTA| changed symbols.
+    top 10 changed symbols.
 
     Args:
       delta_size_info: DeltaSizeInfo Object we are adding disassembly to.
@@ -188,29 +259,37 @@ def AddDisassembly(delta_size_info, before_path_resolver, after_path_resolver):
   """
   logging.info('Computing top changed symbols')
   top_changed_symbols = _GetTopChangedSymbols(delta_size_info)
-  logging.info('Adding disassembly to top %d changed dex symbols',
-               _DISASSEMBLED_METHOD_QUOTA)
+  logging.info('Adding disassembly to top 10 changed dex symbols')
   _AddUnifiedDiff(top_changed_symbols, before_path_resolver,
                   after_path_resolver)
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('apk_path', type=str)
-  parser.add_argument('mapping_file_path', type=str)
-  args = parser.parse_args()
-  logging.basicConfig(level=logging.DEBUG)
+  # Gets disassembly for symbol.
+  size_file_path = sys.argv[1]
+  symbol_full_name = sys.argv[2]
+  mapping_file_name = sys.argv[3]
+  apk_dir = sys.argv[4]
+  path_resolver = lambda x: os.path.join(apk_dir, x)
+  size_info = archive.LoadAndPostProcessSizeInfo(size_file_path)
+  matched_symbols = [
+      sym for sym in size_info.raw_symbols if sym.full_name == symbol_full_name
+  ]
+  if not matched_symbols:
+    print(f'Symbol {symbol_full_name} not found')
+    return
 
-  logging.info('Loading %s and %s...', args.apk_path, args.mapping_file_path)
-  apk_disassembler_cache = _CachedApkDisassembler()
-  apk_disassembler_cache.AssignProguardMappingPath(args.apk_path,
-                                                   args.mapping_file_path)
-  class_obj_map = apk_disassembler_cache.GetForApkAndSplit(args.apk_path, None)
-  variables = {'class_obj_map': class_obj_map}
-  banner = []
-  banner.append('=' * 80)
-  banner.append('class_obj_map: {method: archive_util.DexClass obj}')
-  code.InteractiveConsole(variables).interact('\n'.join(banner))
+  dex_deobfuscator_cache = dex_deobfuscate.CachedDexDeobfuscators()
+  for i, sym in enumerate(matched_symbols):
+    if i > 0:
+      print('-' * 80)
+    sym.container.metadata[
+        models.METADATA_PROGUARD_MAPPING_FILENAME] = mapping_file_name
+    bytecode = _CaptureDisassemblyForSymbol(sym, {}, path_resolver,
+                                            dex_deobfuscator_cache)
+    for line in bytecode:
+      print(line, end='')
+  return
 
 
 if __name__ == '__main__':
