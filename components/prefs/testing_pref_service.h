ef3a221dce02415c40cd75f3469a179514511452
Lei Zhang <thestig@chromium.org>
2022-12-16 20:16:17 +0000

    Avoid an unnecessary copy in TestingPrefServiceBase::SetPref().
    Since TestingPrefServiceBase::SetPref() already owns the
    std::unique_ptr<base::Value> argument, just convert it to base::Value
    instead of cloning it.
    
    Change-Id: I7bbef6d43951ee383a0071cafa8fa52872a62625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4109930
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084491}

--- components/prefs/testing_pref_service.h
+++ components/prefs/testing_pref_service.h
@@ -364,7 +364,7 @@ void TestingPrefServiceBase<SuperPrefService, ConstructionPrefRegistry>::
     SetPref(TestingPrefStore* pref_store,
             const std::string& path,
             std::unique_ptr<base::Value> value) {
-  pref_store->SetValue(path, value->Clone(),
+  pref_store->SetValue(path, base::Value::FromUniquePtrValue(std::move(value)),
                        WriteablePrefStore::DEFAULT_PREF_WRITE_FLAGS);
 }
 
