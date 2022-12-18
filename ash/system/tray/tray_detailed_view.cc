6c5f69481d4b04306b3202687ebf45448d2c6e37
James Cook <jamescook@chromium.org>
2022-12-18 19:56:28 +0000

    Fix centering of quick settings "Cast screen" title label
    The "Cast screen" detail page has a back button, but does not have any
    buttons to the right of the page title. The TriView END container
    therefore defaults to not visible. We add a border to the END container
    to center this label, therefore we need to make the container visible.
    
    Before: https://screenshot.googleplex.com/AGYtkyey9zjsGPq
    After: https://screenshot.googleplex.com/3e7a5MRj979QLHz
    
    Bug: b:252872586
    Change-Id: I994a378cd6c60c0863c2a01b59e5c23ab1e49599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114748
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084799}

--- ash/system/tray/tray_detailed_view.cc
+++ ash/system/tray/tray_detailed_view.cc
@@ -418,9 +418,12 @@ void TrayDetailedView::CreateTitleRow(int string_id) {
   int start_width = start_view->GetPreferredSize().width();
   int end_width = end_view->GetPreferredSize().width();
   if (start_width < end_width) {
+    DCHECK(start_view->GetVisible());
     start_view->SetBorder(views::CreateEmptyBorder(
         gfx::Insets::TLBR(0, 0, 0, end_width - start_width)));
   } else {
+    // Ensure the end container is visible, even if it has no buttons.
+    tri_view_->SetContainerVisible(TriView::Container::END, true);
     end_view->SetBorder(views::CreateEmptyBorder(
         gfx::Insets::TLBR(0, start_width - end_width, 0, 0)));
   }
