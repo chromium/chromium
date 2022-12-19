7f0aad0ea9d9757ee1db7c3c00ab722653800a9a
Wenbo Jie <wenbojie@chromium.org>
2022-12-19 03:46:44 +0000

    [FilesRefresh] Add global CSS classes to the labs page
    Support the following global classes on Labs page so it will be
    eaiser to test the appiness behaviour on Lbas:
    * .pointer-active
    * .focus-outline-visible
    * .drog-drop-active
    
    Demo: http://go/scrcast/NTQwODE2NzI2NjYxNTI5Nnw5NDAwOGMxNS0xOA
    
    Bug: b:242938039
    Change-Id: I1503d424f1fc306e31c10f07ec9ce7af69e2b07f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111664
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084834}

--- ash/webui/file_manager/resources/labs/main.ts
+++ ash/webui/file_manager/resources/labs/main.ts
@@ -7,4 +7,36 @@ import 'chrome://file-manager/widgets/xf_icon.js';
 import 'chrome://file-manager/widgets/xf_tree.js';
 import 'chrome://file-manager/widgets/xf_tree_item.js';
 
-console.log('main ts is here.');
+import {FocusOutlineManager} from 'chrome://resources/js/focus_outline_manager.js';
+
+const rootElement = document.documentElement;
+// Add global focus-outline-visible handler.
+FocusOutlineManager.forDocument(document);
+
+// Add global pointer-active handler.
+['pointerdown', 'pointerup', 'dragend', 'touchend'].forEach((eventType) => {
+  document.addEventListener(eventType, (e) => {
+    if (/down$/.test(e.type) === false) {
+      rootElement.classList.toggle('pointer-active', false);
+    } else if (
+        (e as PointerEvent).pointerType !==
+        'touch') {  // http://crbug.com/1311472
+      rootElement.classList.toggle('pointer-active', true);
+    }
+  }, true);
+});
+
+// Add global drag-drop-active handler.
+let activeDropTarget: EventTarget|null = null;
+['dragenter', 'dragleave', 'drop'].forEach((eventType) => {
+  document.addEventListener(eventType, (event) => {
+    const dragDropActive = 'drag-drop-active';
+    if (event.type === 'dragenter') {
+      rootElement.classList.add(dragDropActive);
+      activeDropTarget = event.target;
+    } else if (activeDropTarget === event.target) {
+      rootElement.classList.remove(dragDropActive);
+      activeDropTarget = null;
+    }
+  });
+});
