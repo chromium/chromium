64453e8143ed687e99dc3f5d773d6d286338be10
François Doray <fdoray@chromium.org>
2022-12-19 05:49:48 +0000

    Remove TabMetricsLogger.
    The UKM data generated by TabMetricsLogger is not used.
    
    Bug: 784639
    Change-Id: I121ba04b49affedd55f500713f627c73b152e28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4062908
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1084845}

--- chrome/browser/ash/power/ml/user_activity_manager.cc
+++ chrome/browser/ash/power/ml/user_activity_manager.cc
@@ -19,7 +19,6 @@
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/resource_coordinator/tab_metrics_logger.h"
 #include "chrome/browser/tab_contents/form_interaction_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -28,6 +27,7 @@
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "chromeos/constants/devicetype.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
+#include "components/site_engagement/content/site_engagement_service.h"
 #include "ui/aura/client/aura_constants.h"
 
 namespace ash {
@@ -116,6 +116,24 @@ bool ShouldUseLacrosFeatures() {
   }
 
   return false;
+}
+
+int GetRoundedOrInvalidEngagementScore(content::WebContents* contents) {
+  if (!site_engagement::SiteEngagementService::IsEnabled()) {
+    return -1;
+  }
+
+  auto* service = site_engagement::SiteEngagementService::Get(
+      contents->GetBrowserContext());
+  DCHECK(service);
+
+  // Scores range from 0 to 100. Round down to a multiple of 10 to conform to
+  // privacy guidelines.
+  double raw_score = service->GetScore(contents->GetVisibleURL());
+  int rounded_score = static_cast<int>(raw_score / 10) * 10;
+  DCHECK_LE(0, rounded_score);
+  DCHECK_GE(100, rounded_score);
+  return rounded_score;
 }
 
 }  // namespace
@@ -585,8 +603,7 @@ TabProperty UserActivityManager::UpdateOpenTabURL() {
       // Domain could be empty.
       property.domain = contents->GetLastCommittedURL().host();
       // Engagement score could be -1 if engagement service is disabled.
-      property.engagement_score =
-          TabMetricsLogger::GetSiteEngagementScore(contents);
+      property.engagement_score = GetRoundedOrInvalidEngagementScore(contents);
       property.has_form_entry =
           FormInteractionTabHelper::FromWebContents(contents)
               ->had_form_interaction();
--- chrome/browser/lacros/web_page_info_lacros.cc
+++ chrome/browser/lacros/web_page_info_lacros.cc
@@ -5,17 +5,35 @@
 #include "chrome/browser/lacros/web_page_info_lacros.h"
 
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/resource_coordinator/tab_metrics_logger.h"
 #include "chrome/browser/tab_contents/form_interaction_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chromeos/lacros/lacros_service.h"
+#include "components/site_engagement/content/site_engagement_service.h"
 #include "ui/aura/window.h"
 
 namespace crosapi {
 namespace {
+
+int GetRoundedOrInvalidEngagementScore(content::WebContents* contents) {
+  if (!site_engagement::SiteEngagementService::IsEnabled()) {
+    return -1;
+  }
+
+  auto* service = site_engagement::SiteEngagementService::Get(
+      contents->GetBrowserContext());
+  DCHECK(service);
+
+  // Scores range from 0 to 100. Round down to a multiple of 10 to conform to
+  // privacy guidelines.
+  double raw_score = service->GetScore(contents->GetVisibleURL());
+  int rounded_score = static_cast<int>(raw_score / 10) * 10;
+  DCHECK_LE(0, rounded_score);
+  DCHECK_GE(100, rounded_score);
+  return rounded_score;
+}
 
 mojom::WebPageInfoPtr PopulateWebPageInfoFromBrowser(const Browser* browser) {
   if (browser->profile()->IsOffTheRecord())
@@ -42,7 +60,7 @@ mojom::WebPageInfoPtr PopulateWebPageInfoFromBrowser(const Browser* browser) {
   web_page_info->domain = contents->GetLastCommittedURL().host();
   // Engagement score could be -1 if engagement service is disabled.
   web_page_info->engagement_score =
-      TabMetricsLogger::GetSiteEngagementScore(contents);
+      GetRoundedOrInvalidEngagementScore(contents);
   web_page_info->has_form_entry =
       FormInteractionTabHelper::FromWebContents(contents)
           ->had_form_interaction();
