4089523917d698b8770b95d20b36060d87069ba5
Nasko Oskov <nasko@chromium.org>
2022-12-19 18:36:03 +0000

    Enable RestrictCanAccessDataForOriginToUIThread feature by default
    Enable the RestrictCanAccessDataForOriginToUIThread feature by default
    on the Canary channel for a few days to verify we don't have some case
    we have overlooked and lack test coverage for.
    
    Bug: 1286501, 1286533
    Change-Id: I6be06bddd6199052b0909a6afe04cee2da2966fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116750
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085026}

--- content/common/features.cc
+++ content/common/features.cc
@@ -24,7 +24,7 @@ BASE_FEATURE(kQueueNavigationsWhileWaitingForCommit,
 
 BASE_FEATURE(kRestrictCanAccessDataForOriginToUIThread,
              "RestrictCanAccessDataForOriginToUIThread",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSpeculativeServiceWorkerStartup,
              "SpeculativeServiceWorkerStartup",
