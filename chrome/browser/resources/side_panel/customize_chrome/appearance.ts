1ee503fbac57a52a7b6a54a09b400a7006310696
Riley Tatum <rtatum@google.com>
2022-12-20 00:35:08 +0000

    [NTP][Panorama] Add Reset to Classic Chrome button
    Styling is not included in this CL. This is purely adding the button
    and functionality so I can finish crbug/1400721.
    
    Screenshots
    With Background: screenshot/ANSa6EidcNyiKnD
    Without Background: screenshot/3DsznwokHXbdndq
    
    Bug: 1401124
    Change-Id: I3236b20f536241ad315ec8b5eb7ee74ac8b097fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4114466
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1085210}

--- chrome/browser/resources/side_panel/customize_chrome/appearance.ts
+++ chrome/browser/resources/side_panel/customize_chrome/appearance.ts
@@ -4,14 +4,19 @@
 
 import './colors.js';
 import './theme_snapshot.js';
+import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 
+import {assert} from 'chrome://resources/js/assert_ts.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './appearance.html.js';
+import {CustomizeChromePageCallbackRouter, CustomizeChromePageHandlerInterface, Theme} from './customize_chrome.mojom-webui.js';
+import {CustomizeChromeApiProxy} from './customize_chrome_api_proxy.js';
 
 export interface AppearanceElement {
   $: {
     editThemeButton: HTMLButtonElement,
+    setClassicChromeButton: HTMLElement,
   };
 }
 
@@ -25,11 +30,56 @@ export class AppearanceElement extends PolymerElement {
   }
 
   static get properties() {
-    return {};
+    return {
+      theme_: Object,
+
+      showClassicChromeButton_: {
+        type: Boolean,
+        value: false,
+        computed: 'computeShowClassicChromeButton_(theme_)',
+      },
+    };
+  }
+
+  private theme_: Theme|undefined = undefined;
+  private setThemeListenerId_: number|null = null;
+  private showClassicChromeButton_: boolean;
+
+  private callbackRouter_: CustomizeChromePageCallbackRouter;
+  private pageHandler_: CustomizeChromePageHandlerInterface;
+
+  constructor() {
+    super();
+    this.pageHandler_ = CustomizeChromeApiProxy.getInstance().handler;
+    this.callbackRouter_ = CustomizeChromeApiProxy.getInstance().callbackRouter;
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.setThemeListenerId_ =
+        this.callbackRouter_.setTheme.addListener((theme: Theme) => {
+          this.theme_ = theme;
+        });
+    this.pageHandler_.updateTheme();
+  }
+
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    assert(this.setThemeListenerId_);
+    this.callbackRouter_.removeListener(this.setThemeListenerId_);
+  }
+
+  private computeShowClassicChromeButton_(): boolean {
+    return !!(this.theme_ && this.theme_.backgroundImage);
   }
 
   private onEditThemeClicked_() {
     this.dispatchEvent(new Event('edit-theme-click'));
+  }
+
+  private onSetClassicChromeClicked_() {
+    this.pageHandler_.setClassicChromeDefaultTheme();
   }
 }
 
--- chrome/test/data/webui/side_panel/customize_chrome/appearance_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/appearance_test.ts
@@ -6,16 +6,35 @@ import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://customize-chrome-side-panel.top-chrome/appearance.js';
 
 import {AppearanceElement} from 'chrome://customize-chrome-side-panel.top-chrome/appearance.js';
-import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {CustomizeChromePageCallbackRouter, CustomizeChromePageHandlerRemote, CustomizeChromePageRemote} from 'chrome://customize-chrome-side-panel.top-chrome/customize_chrome.mojom-webui.js';
+import {CustomizeChromeApiProxy} from 'chrome://customize-chrome-side-panel.top-chrome/customize_chrome_api_proxy.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
+
+import {createBackgroundImage, createTheme, installMock} from './test_support.js';
 
 suite('AppearanceTest', () => {
   let appearanceElement: AppearanceElement;
+  let callbackRouterRemote: CustomizeChromePageRemote;
+  let handler: TestBrowserProxy<CustomizeChromePageHandlerRemote>;
 
   setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    handler = installMock(
+        CustomizeChromePageHandlerRemote,
+        (mock: CustomizeChromePageHandlerRemote) =>
+            CustomizeChromeApiProxy.setInstance(
+                mock, new CustomizeChromePageCallbackRouter()));
+    callbackRouterRemote = CustomizeChromeApiProxy.getInstance()
+                               .callbackRouter.$.bindNewPipeAndPassRemote();
+    // Set result for getChromeColors for the mock handler. Otherwise, it
+    // crashes since colors is a child of appearance and needs a Promise
+    // from getChromeColors.
+    handler.setResultFor('getChromeColors', Promise.resolve([]));
     appearanceElement = document.createElement('customize-chrome-appearance');
     document.body.appendChild(appearanceElement);
+    await handler.whenCalled('getChromeColors');
   });
 
   test('appearance edit button creates event', async () => {
@@ -23,5 +42,35 @@ suite('AppearanceTest', () => {
     appearanceElement.$.editThemeButton.click();
     const event = await eventPromise;
     assertTrue(!!event);
+  });
+
+  test('classic chrome button shows with background image', async () => {
+    const theme = createTheme();
+    theme.backgroundImage = createBackgroundImage('chrome://theme/foo');
+
+    callbackRouterRemote.setTheme(theme);
+    await callbackRouterRemote.$.flushForTesting();
+
+    assertTrue(!appearanceElement.$.setClassicChromeButton.hidden);
+  });
+
+  test('classic chrome button does not show with classic chrome', async () => {
+    const theme = createTheme();
+
+    callbackRouterRemote.setTheme(theme);
+    await callbackRouterRemote.$.flushForTesting();
+
+    assertTrue(appearanceElement.$.setClassicChromeButton.hidden);
+  });
+
+  test('classic chrome button sets theme to classic chrome', async () => {
+    const theme = createTheme();
+    theme.backgroundImage = createBackgroundImage('chrome://theme/foo');
+
+    callbackRouterRemote.setTheme(theme);
+    await callbackRouterRemote.$.flushForTesting();
+
+    appearanceElement.$.setClassicChromeButton.click();
+    assertEquals(1, handler.getCallCount('setClassicChromeDefaultTheme'));
   });
 });
