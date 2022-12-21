1c7cba864d4152850ce126705d15ec345a831c59
Jeevan Shikaram <jshikaram@chromium.org>
2022-12-21 07:50:26 +0000

    [APS] Add method to convert manifest to WebAppInstallInfo.
    This CL adds a method to convert a manifest in the form of a parsed JSON
    dictionary into a WebAppInstallInfo.
    
    The method follows the URL resolution rules laid out in
    https://www.w3.org/TR/appmanifest.
    
    Bug: b/261632289
    Change-Id: I9685b20ca4506fbae6fdbe9beefffacb88052e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4117937
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1085810}

--- chrome/browser/apps/app_preload_service/web_app_preload_installer.cc
+++ chrome/browser/apps/app_preload_service/web_app_preload_installer.cc
@@ -6,14 +6,18 @@
 
 #include <memory>
 
+#include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/commands/install_from_info_command.h"
+#include "chrome/browser/web_applications/user_display_mode.h"
 #include "chrome/browser/web_applications/web_app_command_manager.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "components/webapps/browser/install_result_code.h"
+#include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 
 namespace apps {
 
@@ -21,6 +25,92 @@ WebAppPreloadInstaller::WebAppPreloadInstaller(Profile* profile)
     : profile_(profile) {}
 
 WebAppPreloadInstaller::~WebAppPreloadInstaller() = default;
+
+std::unique_ptr<WebAppInstallInfo>
+WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+    GURL document_url,
+    GURL original_manifest_url,
+    base::Value::Dict& manifest) {
+  auto install_info = std::make_unique<WebAppInstallInfo>();
+
+  // Shared error message
+  constexpr char kSharedError[] =
+      "Unable to convert manifest into WebAppInstallInfo. Field that was not "
+      "defined or was the wrong type: ";
+
+  if (!document_url.is_valid()) {
+    VLOG(1) << kSharedError << "Document URL";
+    return nullptr;
+  }
+
+  if (!original_manifest_url.is_valid()) {
+    VLOG(1) << kSharedError << "Original Manifest URL";
+    return nullptr;
+  }
+
+  // Manifest keys
+  constexpr char kName[] = "name";
+  constexpr char kStartUrl[] = "start_url";
+  constexpr char kManifestId[] = "id";
+  constexpr char kScope[] = "scope";
+
+  // Title
+  auto* title = manifest.FindString(kName);
+  if (!title) {
+    VLOG(1) << kSharedError << kName;
+    return nullptr;
+  }
+  install_info->title = base::UTF8ToUTF16(*title);
+
+  // Start URL
+  auto* start_url = manifest.FindString(kStartUrl);
+  if (!start_url) {
+    VLOG(1) << kSharedError << kStartUrl;
+    return nullptr;
+  }
+  install_info->start_url = original_manifest_url.Resolve(*start_url);
+  if (!install_info->start_url.is_valid()) {
+    VLOG(1) << kSharedError << kStartUrl << ". Value: " << *start_url;
+    return nullptr;
+  }
+  if (!url::IsSameOriginWith(document_url, install_info->start_url)) {
+    VLOG(1) << kSharedError << kStartUrl << ". Value: " << *start_url
+            << ". Mismatch in origin with document url: " << document_url;
+    return nullptr;
+  }
+
+  // Manifest ID
+  auto* manifest_id = manifest.FindString(kManifestId);
+  if (manifest_id) {
+    GURL start_url_origin = install_info->start_url.GetWithEmptyPath();
+    GURL processed_id = start_url_origin.Resolve(*manifest_id);
+    if (processed_id.is_valid() &&
+        url::IsSameOriginWith(start_url_origin, processed_id)) {
+      install_info->manifest_id = processed_id.spec().substr(
+          processed_id.GetWithEmptyPath().spec().size());
+    }
+  }
+
+  // Scope
+  auto* scope = manifest.FindString(kScope);
+  if (scope) {
+    install_info->scope = original_manifest_url.Resolve(*scope);
+  }
+
+  if (install_info->scope.is_empty() || !install_info->scope.is_valid() ||
+      !url::IsSameOriginWith(install_info->start_url, install_info->scope) ||
+      !base::StartsWith(install_info->start_url.path(),
+                        install_info->scope.path(),
+                        base::CompareCase::SENSITIVE)) {
+    install_info->scope = install_info->start_url;
+  }
+
+  // Display mode
+  install_info->display_mode = blink::mojom::DisplayMode::kStandalone;
+  install_info->user_display_mode = web_app::UserDisplayMode::kStandalone;
+
+  return install_info;
+}
 
 void WebAppPreloadInstaller::InstallApp(
     const PreloadAppDefinition& app,
--- chrome/browser/apps/app_preload_service/web_app_preload_installer_unittest.cc
+++ chrome/browser/apps/app_preload_service/web_app_preload_installer_unittest.cc
@@ -4,12 +4,15 @@
 
 #include "chrome/browser/apps/app_preload_service/web_app_preload_installer.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/test_future.h"
+#include "base/test/values_test_util.h"
 #include "chrome/browser/apps/app_preload_service/preload_app_definition.h"
 #include "chrome/browser/apps/app_preload_service/proto/app_provisioning.pb.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/user_display_mode.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
@@ -17,6 +20,7 @@
 #include "components/services/app_service/public/cpp/app_update.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 
 namespace apps {
 
@@ -152,6 +156,159 @@ TEST_F(WebAppPreloadInstallerTest, GetAppId) {
 
   ASSERT_EQ(installer.GetAppId(PreloadAppDefinition(app)),
             "apignacaigpffemhdbhmnajajaccbckh");
+}
+
+TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfo) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "https://example.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  ASSERT_TRUE(install_info);
+  EXPECT_EQ(install_info->manifest_id, "manifest_id");
+  EXPECT_EQ(install_info->title, u"Peanut Types");
+  EXPECT_EQ(install_info->start_url, "https://example.com/index.html");
+  EXPECT_EQ(install_info->scope, "https://example.com/");
+  EXPECT_EQ(install_info->display_mode, blink::mojom::DisplayMode::kStandalone);
+  EXPECT_EQ(install_info->user_display_mode,
+            web_app::UserDisplayMode::kStandalone);
+}
+
+TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfoRelativeId) {
+  constexpr char manifest[] = R"({
+    "id": "/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "https://example.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  ASSERT_TRUE(install_info);
+  EXPECT_EQ(install_info->manifest_id, "manifest_id");
+}
+
+TEST_F(WebAppPreloadInstallerTest,
+       ManifestToWebAppInstallInfoInvalidManifestUrl) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "invalid";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  EXPECT_FALSE(install_info);
+}
+
+TEST_F(WebAppPreloadInstallerTest,
+       ManifestToWebAppInstallInfoInvalidDocumentUrl) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "invalid";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  EXPECT_FALSE(install_info);
+}
+
+TEST_F(WebAppPreloadInstallerTest,
+       ManifestToWebAppInstallInfoNoNameInManifest) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "https://example.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  EXPECT_FALSE(install_info);
+}
+
+TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfoNoManifestId) {
+  constexpr char manifest[] = R"({
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "https://example.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  ASSERT_TRUE(install_info);
+  EXPECT_FALSE(install_info->manifest_id);
+  EXPECT_EQ(install_info->title, u"Peanut Types");
+  EXPECT_EQ(install_info->start_url, "https://example.com/index.html");
+  EXPECT_EQ(install_info->scope, "https://example.com/");
+  EXPECT_EQ(install_info->display_mode, blink::mojom::DisplayMode::kStandalone);
+  EXPECT_EQ(install_info->user_display_mode,
+            web_app::UserDisplayMode::kStandalone);
+}
+
+TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfoInvalidStartUrl) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "https://exampleCDN.com/index.html",
+    "scope": "/"
+  })";
+  constexpr char manifest_url[] = "https://exampleCDN.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  EXPECT_FALSE(install_info);
+}
+
+TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfoInvalidScope) {
+  constexpr char manifest[] = R"({
+    "id": "https://example.com/manifest_id",
+    "name": "Peanut Types",
+    "start_url": "/index.html",
+    "scope": "https://otherexample.com/"
+  })";
+  constexpr char manifest_url[] = "https://example.com/manifest.json";
+  constexpr char document_url[] = "https://example.com/";
+
+  base::Value parsed_manifest = base::test::ParseJson(manifest);
+  auto install_info = WebAppPreloadInstaller::ManifestToWebAppInstallInfo(
+      GURL(document_url), GURL(manifest_url), parsed_manifest.GetDict());
+
+  ASSERT_TRUE(install_info);
+  EXPECT_EQ(install_info->scope, install_info->start_url);
 }
 
 }  // namespace apps
