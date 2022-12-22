f51bcdcfffa6ea89f81399ec54168810937a4ec4
Sophia Lin <sophialin@google.com>
2022-12-22 00:49:58 +0000

    guest_os: Store container token in GuestOsSessionTracker
    This token will be stored in a map to GuestIds, which will be used to
    uniquely identify guests with their vm_name and container_name. This
    guestId will later be used to identify the guest's app windows as
    described in go/guestos-shelf-icons
    
    TEST=CQ, unittests
    
    Bug: b:244651040
    Change-Id: I5513c42486b6f9d7375ef3b5308ed39237d228f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116314
    Commit-Queue: Sophia Lin <sophialin@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086174}

--- chrome/browser/ash/guest_os/guest_os_session_tracker.h
+++ chrome/browser/ash/guest_os/guest_os_session_tracker.h
@@ -74,6 +74,11 @@ class GuestOsSessionTracker : protected ash::ConciergeClient::VmObserver,
   absl::optional<vm_tools::concierge::VmInfo> GetVmInfo(
       const std::string& vm_name);
 
+  // Given a container_token for a running guest, returns its GuestId. Returns
+  // nullopt if the token isn't recognised.
+  absl::optional<GuestId> GetGuestIdForToken(
+      const std::string& container_token);
+
   // Returns true if a guest is running, false otherwise.
   bool IsRunning(const GuestId& id);
 
@@ -106,19 +111,22 @@ class GuestOsSessionTracker : protected ash::ConciergeClient::VmObserver,
   void OnGetGarconSessionInfo(
       std::string vm_name,
       std::string container_name,
+      std::string container_token,
       absl::optional<vm_tools::cicerone::GetGarconSessionInfoResponse>
           response);
   void HandleNewGuest(const std::string& vm_name,
                       const std::string& container_name,
+                      const std::string& container_token,
                       const std::string& username,
                       const std::string& homedir,
                       const std::string& ipv4_address,
-                      uint32_t sftp_vsock_port);
+                      const uint32_t& sftp_vsock_port);
   void HandleContainerShutdown(const std::string& vm_name,
                                const std::string& container_name);
   std::string owner_id_;
   base::flat_map<std::string, vm_tools::concierge::VmInfo> vms_;
   base::flat_map<GuestId, GuestInfo> guests_;
+  base::flat_map<std::string, GuestId> tokens_to_guests_;
 
   base::flat_map<GuestId,
                  std::unique_ptr<base::OnceCallbackList<void(GuestInfo)>>>
