97df6f551756705075f30249b649b241b0a68065
Thiago Perrotta <tperrotta@chromium.org>
2022-12-23 06:55:27 +0000

    Add test to ensure the Chrome for Testing info bar appears in every tab
    Design document: https://goo.gle/chrome-for-testing#bookmark=id.8oia0mpldm1d
    
    Bug: 1336611
    Change-Id: I275294871f308ed688062df601dd00ef09f6b7e0
    Follow-up-of: chromium:4116670
    Tested: `tools/autotest.py -C out/Default chrome/browser/chrome_for_testing/chrome_for_testing_info_bar_browsertest.cc --gtest_filter="ChromeForTestingInfoBarTest.InfoBarAppearsInEveryTab" --single-process-tests`
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4121997
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1086663}

--- chrome/browser/chrome_for_testing/chrome_for_testing_info_bar_browsertest.cc
+++ chrome/browser/chrome_for_testing/chrome_for_testing_info_bar_browsertest.cc
@@ -4,28 +4,26 @@
 
 #include <utility>
 
-#include "base/command_line.h"
-#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 #include "components/infobars/core/infobar.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/test_utils.h"
 
 namespace {
 
 class ChromeForTestingInfoBarTest : public InProcessBrowserTest {
  public:
   ChromeForTestingInfoBarTest() = default;
+  ~ChromeForTestingInfoBarTest() override = default;
 
   ChromeForTestingInfoBarTest(const ChromeForTestingInfoBarTest&) = delete;
   ChromeForTestingInfoBarTest& operator=(const ChromeForTestingInfoBarTest&) =
       delete;
-
-  ~ChromeForTestingInfoBarTest() override = default;
 
  protected:
   infobars::ContentInfoBarManager* GetInfoBarManagerFromTabIndex(
@@ -53,4 +51,24 @@ IN_PROC_BROWSER_TEST_F(ChromeForTestingInfoBarTest, InfoBarAppears) {
 
   EXPECT_FALSE(test_infobar->IsCloseable());
   EXPECT_FALSE(test_infobar->ShouldAnimate());
+}
+
+IN_PROC_BROWSER_TEST_F(ChromeForTestingInfoBarTest, InfoBarAppearsInEveryTab) {
+  // Open a second tab in the same window.
+  chrome::AddTabAt(browser(), GURL("about:blank"), -1, true);
+
+  const unsigned number_of_tabs = browser()->tab_strip_model()->count();
+  EXPECT_EQ(2u, number_of_tabs);
+
+  // Verify that the info bar is shown in every tab.
+  for (unsigned i = 0; i < number_of_tabs; ++i) {
+    infobars::ContentInfoBarManager* infobar_manager =
+        GetInfoBarManagerFromTabIndex(i);
+    ASSERT_EQ(1u, infobar_manager->infobar_count());
+
+    auto* test_infobar = infobar_manager->infobar_at(0)->delegate();
+    ASSERT_EQ(ConfirmInfoBarDelegate::InfoBarIdentifier::
+                  CHROME_FOR_TESTING_INFOBAR_DELEGATE,
+              test_infobar->GetIdentifier());
+  }
 }
