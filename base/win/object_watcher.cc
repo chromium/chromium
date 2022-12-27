88f0676ce08098d410b12a4087d132fa9c28f426
Ankush Singh <ankushkush@google.com>
2022-12-27 17:25:08 +0000

    [CodeHealth] Triage use of UnsafeDanglingUntriaged ptr in ObjectWatcher
    This CL is part of a larger cleanup effort to triage dangling pointers
    and ensure they aren't the cause of memory issues in Chrome. See:
    https://docs.google.com/document/d/164SnySRX6MZgFkX8qLLLClTnZ0S8s5wLiNFWNyQ8f4E/edit.
    
    This change simply replaces UnsafeDanglingUntriaged with UnsafeDangling
    with the conclusion that for all the non-test usages, the lifetime of
    the passed resource exceeds the lifetime of the watcher, and hence this
    is safe.
    
    Bug: 1381413
    Change-Id: I5cf277bc9c05a900b48ca888abc1b28ff3e9b005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122654
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087131}

--- base/win/object_watcher.cc
+++ base/win/object_watcher.cc
@@ -93,7 +93,9 @@ bool ObjectWatcher::StartWatchingInternal(HANDLE object,
   // DoneWaiting can be synchronously called from RegisterWaitForSingleObject,
   // so set up all state now.
   callback_ = BindRepeating(&ObjectWatcher::Signal, weak_factory_.GetWeakPtr(),
-                            base::UnsafeDanglingUntriaged(delegate));
+                            // For all non-test usages, the delegate's lifetime
+                            // exceeds object_watcher's. This should be safe.
+                            delegate);
   object_ = object;
 
   if (!RegisterWaitForSingleObject(&wait_object_, object, DoneWaiting, this,
