c4c099524bef195d9f6145eeed7f178a9f21bfa2
Nihar Damarasingu <nihardamar@google.com>
2022-12-27 21:54:34 +0000

    remove wpt_common as dependency from finch tests
    With the end goal of removing the wpt_common.py file, in this CL we are
    moving the wpt_common functionality to run_finch_smoke_tests_android.
    
    Bug:1356318
    
    Change-Id: I228a66c145375b141c7e9330d1e9accfbd4e85cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111418
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1087187}

--- testing/scripts/run_finch_smoke_tests_android.py
+++ testing/scripts/run_finch_smoke_tests_android.py
@@ -56,8 +56,9 @@ if 'compile_targets' not in sys.argv:
   import aw_variations_seed_pb2
 
 import devil_chromium
-import wpt_common
 
+from blinkpy.common.host import Host
+from blinkpy.common.path_finder import PathFinder
 from blinkpy.web_tests.models import test_failures
 from blinkpy.web_tests.port.android import (
     ANDROID_WEBLAYER, ANDROID_WEBVIEW, CHROME_ANDROID)
@@ -114,10 +115,17 @@ def _merge_results_dicts(dict_to_merge, test_results_dict):
 
 
 # pylint: disable=super-with-arguments, abstract-method
-class FinchTestCase(wpt_common.BaseWptScriptAdapter):
+class FinchTestCase(common.BaseIsolatedScriptArgsAdapter):
 
   def __init__(self, device):
+    self.host = Host()
+    self.fs = self.host.filesystem
+    self.path_finder = PathFinder(self.fs)
+    self.port = self.host.port_factory.get()
     super(FinchTestCase, self).__init__()
+    self._parser = self._override_options(self._parser)
+    self._include_filename = None
+    self.layout_test_results_subdir = 'layout-test-results'
     self._device = device
     self.parse_args()
     self._browser_apk_helper = apk_helper.ToHelper(self.options.browser_apk)
@@ -169,6 +177,89 @@ class FinchTestCase(wpt_common.BaseWptScriptAdapter):
   def finch_seed_download_args(cls):
     return []
 
+  def generate_test_output_args(self, output):
+        return ['--log-chromium=%s' % output]
+
+  def generate_test_filter_args(self, test_filter_str):
+        included_tests, excluded_tests = \
+            self._resolve_tests_from_isolate_filter(test_filter_str)
+        include_file, self._include_filename = self.fs.open_text_tempfile()
+        with include_file:
+            for test in included_tests:
+                include_file.write(test)
+                include_file.write('\n')
+        wpt_args = ['--include-file=%s' % self._include_filename]
+        for test in excluded_tests:
+            wpt_args.append('--exclude=%s' % test)
+        return wpt_args
+
+  def _override_options(self, base_parser):
+        """Create a parser that overrides existing options.
+
+        `argument.ArgumentParser` can extend other parsers and override their
+        options, with the caveat that the child parser only inherits options
+        that the parent had at the time of the child's initialization. There is
+        not a clean way to add option overrides in `add_extra_arguments`, where
+        the provided parser is only passed up the inheritance chain, so we add
+        overridden options here at the very end.
+
+        See Also:
+            https://docs.python.org/3/library/argparse.html#parents
+        """
+        parser = argparse.ArgumentParser(
+            parents=[base_parser],
+            # Allow overriding existing options in the parent parser.
+            conflict_handler='resolve',
+            epilog=('All unrecognized arguments are passed through '
+                    "to wptrunner. Use '--wpt-help' to see wptrunner's usage."),
+        )
+        parser.add_argument(
+            '--isolated-script-test-repeat',
+            '--repeat',
+            '--gtest_repeat',
+            metavar='REPEAT',
+            type=int,
+            default=1,
+            help='Number of times to run the tests')
+        parser.add_argument(
+            '--isolated-script-test-launcher-retry-limit',
+            '--test-launcher-retry-limit',
+            '--retry-unexpected',
+            metavar='RETRIES',
+            type=int,
+            help=(
+                'Maximum number of times to rerun unexpectedly failed tests. '
+                'Defaults to 3 unless given an explicit list of tests to run.'))
+        # `--gtest_filter` and `--isolated-script-test-filter` have slightly
+        # different formats and behavior, so keep them as separate options.
+        # See: crbug/1316164#c4
+
+        # TODO(crbug.com/1356318): This is a temporary hack to hide the
+        # inherited '--xvfb' option and force Xvfb to run always.
+        parser.add_argument('--xvfb', action='store_true', default=True,
+                            help=argparse.SUPPRESS)
+        return parser
+
+  def generate_test_repeat_args(self, repeat_count):
+    return ['--repeat=%d' % repeat_count]
+
+  def generate_test_launcher_retry_limit_args(self, retry_limit):
+    return ['--retry-unexpected=%d' % retry_limit]
+
+  def generate_sharding_args(self, total_shards, shard_index):
+    return ['--total-chunks=%d' % total_shards,
+            # shard_index is 0-based but WPT's this-chunk to be 1-based
+            '--this-chunk=%d' % (shard_index + 1),
+            # The default sharding strategy is to shard by directory. But
+            # we want to hash each test to determine which shard runs it.
+            # This allows running individual directories that have few
+            # tests across many shards.
+            '--chunk-type=hash']
+
+  def clean_up_after_test_run(self):
+    if self._include_filename:
+        self.fs.remove(self._include_filename)
+
   def new_seed_downloaded(self):
     # TODO(crbug.com/1285152): Implement seed download test
     # for Chrome and WebLayer.
@@ -208,6 +299,19 @@ class FinchTestCase(wpt_common.BaseWptScriptAdapter):
                      'Running tests on the default finch seed')
       self.options.finch_seed_path = self.default_finch_seed_path
 
+  @property
+  def output_directory(self):
+      return self.path_finder.path_from_chromium_base('out',
+                                                      self.options.target)
+
+  @property
+  def mojo_js_directory(self):
+      return self.fs.join(self.output_directory, 'gen')
+
+  @property
+  def wpt_output(self):
+      return self.options.isolated_script_test_output
+
   def __enter__(self):
     self._device.EnableRoot()
     # Run below commands to ensure that the device can download a seed
@@ -227,7 +331,11 @@ class FinchTestCase(wpt_common.BaseWptScriptAdapter):
 
   @property
   def rest_args(self):
-    rest_args = super(FinchTestCase, self).rest_args
+    unknown_args = super(FinchTestCase, self).rest_args
+
+    rest_args = list()
+
+    rest_args.extend(self.wpt_rest_args(unknown_args))
 
     rest_args.extend([
       '--webdriver-arg=--disable-build-check',
@@ -249,6 +357,91 @@ class FinchTestCase(wpt_common.BaseWptScriptAdapter):
     for test in self.tests:
       rest_args.extend(['--include', test])
 
+    return rest_args
+
+  @property
+  def wpt_binary(self):
+    default_wpt_binary = os.path.join(
+        common.SRC_DIR, "third_party", "wpt_tools", "wpt", "wpt")
+    return os.environ.get("WPT_BINARY", default_wpt_binary)
+
+  @property
+  def wpt_root_dir(self):
+      return self.path_finder.path_from_web_tests(
+          self.path_finder.wpt_prefix())
+
+  @property
+  def _wpt_run_args(self):
+      """The start of a 'wpt run' command."""
+      return [
+          self.wpt_binary,
+          # Use virtualenv packages installed by vpython, not wpt.
+          '--venv=%s' % self.path_finder.chromium_base(),
+          '--skip-venv-setup',
+          'run',
+      ]
+
+  def process_and_upload_results(self):
+    command = [
+        self.select_python_executable(),
+        os.path.join(BLINK_TOOLS, 'wpt_process_results.py'),
+        '--target',
+        self.options.target,
+        '--web-tests-dir',
+        BLINK_WEB_TESTS,
+        '--artifacts-dir',
+        os.path.join(os.path.dirname(self.wpt_output),
+                      self.layout_test_results_subdir),
+        '--wpt-results',
+        self.wpt_output,
+    ]
+    if self.options.verbose:
+        command.append('--verbose')
+
+    return common.run_command(command)
+
+  def wpt_rest_args(self, unknown_args):
+    rest_args = list(self._wpt_run_args)
+    rest_args.extend([
+        '--no-pause-after-test',
+        '--no-capture-stdio',
+        '--no-manifest-download',
+        '--tests=%s' % self.wpt_root_dir,
+        '--metadata=%s' % self.wpt_root_dir,
+        '--mojojs-path=%s' % self.mojo_js_directory,
+    ])
+
+    if self.options.default_exclude:
+        rest_args.extend(['--default-exclude'])
+
+    if self.options.verbose >= 3:
+        rest_args.extend([
+            '--log-mach=-',
+            '--log-mach-level=debug',
+            '--log-mach-verbose',
+        ])
+    if self.options.verbose >= 4:
+        rest_args.extend([
+            '--webdriver-arg=--verbose',
+            '--webdriver-arg="--log-path=-"',
+        ])
+
+    rest_args.append(self.wpt_product_name())
+    # We pass through unknown args as late as possible so that they can
+    # override earlier options. It also allows users to pass test names as
+    # positional args, which must not have option strings between them.
+    for unknown_arg in unknown_args:
+        # crbug/1274933#c14: Some developers had used the end-of-options
+        # marker '--' to pass through arguments to wptrunner.
+        # crrev.com/c/3573284 makes this no longer necessary.
+        if unknown_arg == '--':
+            logger.warning(
+                'Unrecognized options will automatically fall through '
+                'to wptrunner.')
+            logger.warning(
+                "There is no need to use the end-of-options marker '--'.")
+        else:
+            rest_args.append(unknown_arg)
     return rest_args
 
   @classmethod
@@ -299,6 +492,22 @@ class FinchTestCase(wpt_common.BaseWptScriptAdapter):
 
   def add_extra_arguments(self, parser):
     super(FinchTestCase, self).add_extra_arguments(parser)
+    parser.add_argument(
+      '-t',
+      '--target',
+      default='Release',
+      help='Target build subdirectory under //out')
+    parser.add_argument(
+      '--default-exclude',
+      action='store_true',
+      help=('Only run the tests explicitly given in arguments '
+            '(can run no tests, which will exit with code 0)'))
+    parser.add_argument(
+      '-v',
+      '--verbose',
+      action='count',
+      default=0,
+      help='Increase verbosity')
     self.add_product_specific_argument_groups(parser)
     self.add_common_arguments(parser)
 
