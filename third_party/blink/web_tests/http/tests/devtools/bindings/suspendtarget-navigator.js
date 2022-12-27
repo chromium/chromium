e89161928fb4225c7f4c31f15da33a0398a308bc
Benedikt Meurer <bmeurer@chromium.org>
2022-12-27 07:35:59 +0000

    [DevTools] Rebaseline and reenable suspendtarget-navigator.js.
    This removes the data races from the tests waiting for sourcemapped
    resources to appear.
    
    Bug: chromium:1403362, chromium:508270
    Change-Id: I1222089b94b104a84853c34a4c90e0ad4230d1cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120288
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087044}

--- third_party/blink/web_tests/http/tests/devtools/bindings/suspendtarget-navigator.js
+++ third_party/blink/web_tests/http/tests/devtools/bindings/suspendtarget-navigator.js
@@ -14,9 +14,13 @@
   SourcesTestRunner.dumpNavigatorView(sourcesNavigator, false);
 
   TestRunner.markStep('attachFramesAndWaitForSourceMaps');
+  await BindingsTestRunner.attachFrame('frame1', './resources/sourcemap-frame.html', '_test_create-iframe1.js'),
   await Promise.all([
-    BindingsTestRunner.attachFrame('frame1', './resources/sourcemap-frame.html', '_test_create-iframe1.js'),
-    BindingsTestRunner.attachFrame('frame2', './resources/sourcemap-frame.html', '_test_create-iframe2.js'),
+    BindingsTestRunner.waitForSourceMap('sourcemap-script.js.map'),
+    BindingsTestRunner.waitForSourceMap('sourcemap-style.css.map'),
+  ]);
+  await BindingsTestRunner.attachFrame('frame2', './resources/sourcemap-frame.html', '_test_create-iframe2.js'),
+  await Promise.all([
     BindingsTestRunner.waitForSourceMap('sourcemap-script.js.map'),
     BindingsTestRunner.waitForSourceMap('sourcemap-style.css.map'),
   ]);
@@ -33,8 +37,9 @@
 
   TestRunner.markStep('Resuming targets.');
   await Promise.all([
-    SDK.targetManager.resumeAllTargets(), BindingsTestRunner.waitForSourceMap('sourcemap-script.js.map'),
-    BindingsTestRunner.waitForSourceMap('sourcemap-style.css.map')
+    SDK.targetManager.resumeAllTargets(),
+    BindingsTestRunner.waitForSourceMap('sourcemap-script.js.map'),
+    BindingsTestRunner.waitForSourceMap('sourcemap-style.css.map'),
   ]);
 
   SourcesTestRunner.dumpNavigatorView(sourcesNavigator, false);
