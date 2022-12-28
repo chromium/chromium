8991f86bc36b5e45bbc1b5c32c980b6f615e40a7
danakj <danakj@chromium.org>
2022-12-28 19:47:23 +0000

    Add copyright headers to Rust test files that missed them
    R=collinbaker@chromium.org
    
    Change-Id: I207394f752f95f05cf5db793625450ddb5cb18d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4128894
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087397}

--- build/rust/tests/test_aliased_deps/main.rs
+++ build/rust/tests/test_aliased_deps/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 fn main() {
     other_name::hello_world();
 }
--- build/rust/tests/test_aliased_deps/real_name.rs
+++ build/rust/tests/test_aliased_deps/real_name.rs
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 pub fn hello_world() {
     println!("hello world");
 }
