738ab4946ccc82bb5b04a4485d3e31decf463795
Tom Anderson <thomasanderson@chromium.org>
2022-12-28 00:38:06 +0000

    Remove a DanglingUntriaged in platform_util_linux.cc
    `object_proxy_` is owned by `bus_`, so it just needs to be unset
    before `bus_` goes away.
    
    Bug: None
    Change-Id: Id92b4efe8b711b5db59fe33fd54edc910075465d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4122986
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1087227}

--- chrome/browser/platform_util_linux.cc
+++ chrome/browser/platform_util_linux.cc
@@ -105,10 +105,10 @@ class ShowItemHelper {
   void OnAppTerminating() {
     DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
     // The browser process is about to exit. Clean up while we still can.
+    object_proxy_ = nullptr;
     if (bus_)
       bus_->ShutdownOnDBusThreadAndBlock();
     bus_.reset();
-    object_proxy_ = nullptr;
   }
 
   void CheckFileManagerRunning(Profile* profile,
@@ -278,8 +278,10 @@ class ShowItemHelper {
   }
 
   scoped_refptr<dbus::Bus> bus_;
+
+  // These proxy objects are owned by `bus_`.
   raw_ptr<dbus::ObjectProxy> dbus_proxy_ = nullptr;
-  raw_ptr<dbus::ObjectProxy, DanglingUntriaged> object_proxy_ = nullptr;
+  raw_ptr<dbus::ObjectProxy> object_proxy_ = nullptr;
 
   absl::optional<bool> prefer_filemanager_interface_;
 
