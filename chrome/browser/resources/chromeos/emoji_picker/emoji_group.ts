0b8e692b308aab92b2e2c5b900ff9a16ce1fe5c5
Keith Lee <keithlee@chromium.org>
2023-01-03 06:44:15 +0000

    Add import of paper-tooltip.js and cr-icon-button.js
    Adding this in seems to fix the issue of `this.$.tooltip.show() is not
    defined`. This seems to be because the typescript deletes the import
    line for the type, unless it is explicitly called with new. For polymer
    elements, the code has to be run for it to work correctly, so we need to
    explicitly add back the import.
    
    This was deleted in https://crrev.com/c/3920251.
    
    This also adds an import of cr-icon-button.
    
    BUG=b:264211466
    
    Change-Id: Icb38510cb0e294d8b1bf5b2a8f74ff0fbabe905a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129578
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088123}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_group.ts
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_group.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
+import 'chrome://resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
 import './emoji_variants.js';
 
 import {assertInstanceof} from 'chrome://resources/js/assert_ts.js';
