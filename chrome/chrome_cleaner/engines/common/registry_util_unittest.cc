288df013a807ba388269ac24068ccf514e567678
Will Harris <wfh@chromium.org>
2023-01-03 22:52:48 +0000

    Fix off by one error in HandlePath
    Change-Id: Ib1176a90eecb5ed0b725003a686b3538ef0cddee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133586
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088425}

--- chrome/chrome_cleaner/engines/common/registry_util_unittest.cc
+++ chrome/chrome_cleaner/engines/common/registry_util_unittest.cc
@@ -171,13 +171,13 @@ class SandboxChildProcess : public chrome_cleaner::ChildProcess {
 
 std::wstring HandlePath(HANDLE handle) {
   std::wstring full_path;
-  // The size parameter of GetFinalPathNameByHandle does NOT include the null
+  // The size parameter of GetFinalPathNameByHandle includes the null
   // terminator.
   DWORD result = ::GetFinalPathNameByHandleW(
-      handle, base::WriteInto(&full_path, MAX_PATH), MAX_PATH - 1, 0);
+      handle, base::WriteInto(&full_path, MAX_PATH + 1), MAX_PATH + 1, 0);
   if (result > MAX_PATH) {
-    result = ::GetFinalPathNameByHandle(
-        handle, base::WriteInto(&full_path, result), result - 1, 0);
+    result = ::GetFinalPathNameByHandleW(
+        handle, base::WriteInto(&full_path, result), result, 0);
   }
   if (!result) {
     PLOG(ERROR) << "Could not get full path for handle " << handle;
