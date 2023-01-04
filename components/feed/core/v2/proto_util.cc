9f9ab753801d8f0c9bd34321c2f78b97395f0da5
Jeffrey Cohen <jeffreycohen@google.com>
2023-01-04 18:08:44 +0000

    [Cormorant] Use matching g3 protos
    Bug: 1395199
    Change-Id: I24d3026f53430e5a7b8d7e761945010f83ab23bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4134090
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088816}

--- components/feed/core/v2/proto_util.cc
+++ components/feed/core/v2/proto_util.cc
@@ -13,7 +13,6 @@
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "components/feed/core/proto/v2/store.pb.h"
-#include "components/feed/core/proto/v2/web_feed_identifier_token.pb.h"
 #include "components/feed/core/proto/v2/wire/capability.pb.h"
 #include "components/feed/core/proto/v2/wire/chrome_client_info.pb.h"
 #include "components/feed/core/proto/v2/wire/feed_entry_point_data.pb.h"
@@ -21,6 +20,8 @@
 #include "components/feed/core/proto/v2/wire/feed_query.pb.h"
 #include "components/feed/core/proto/v2/wire/feed_request.pb.h"
 #include "components/feed/core/proto/v2/wire/request.pb.h"
+#include "components/feed/core/proto/v2/wire/web_feed_id.pb.h"
+#include "components/feed/core/proto/v2/wire/web_feed_identifier_token.pb.h"
 #include "components/feed/core/v2/config.h"
 #include "components/feed/core/v2/enums.h"
 #include "components/feed/core/v2/feed_stream.h"
@@ -385,9 +386,10 @@ feedwire::Request CreateFeedQueryRefreshRequest(
         ->set_web_feed_token(kChromeFollowToken);
   } else if (stream_type.IsSingleWebFeed()) {
     // A special token that requests content for the Single Web Feed.
-    webfeedidentifier::WebFeedIdentifierToken web_feed_id;
-    web_feed_id.mutable_outer_id()->mutable_inner_id()->set_web_feed_name(
-        stream_type.GetWebFeedId().c_str());
+    feedwire::WebFeedIdentifierToken web_feed_id;
+    web_feed_id.mutable_web_feed_id()
+        ->mutable_domain_web_feed_id()
+        ->set_web_feed_name(stream_type.GetWebFeedId().c_str());
 
     request.mutable_feed_request()
         ->mutable_feed_query()
