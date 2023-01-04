f96d569f64bfaf6e66773b0d822df7252c2247ea
Alex Moshchuk <alexmos@chromium.org>
2023-01-04 03:53:52 +0000

    [Sheriff] Disable flaky test: BluetoothInternalsTest.Startup_BluetoothInternals
    This appears to be flaky on all platforms. See analysis at
    https://luci-analysis.appspot.com/p/chromium/rules/d3a3b23e241f08f826a38f69926d3302
    
    Bug: 1399475
    Change-Id: I57e476505c8ba4f4b3caec5d146516517b643047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135137
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Owners-Override: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088557}

--- chrome/test/data/webui/bluetooth_internals/bluetooth_internals_browsertest.js
+++ chrome/test/data/webui/bluetooth_internals/bluetooth_internals_browsertest.js
@@ -32,7 +32,10 @@ BluetoothInternalsTest.prototype = {
   ],
 };
 
-TEST_F('BluetoothInternalsTest', 'Startup_BluetoothInternals', function() {
-  // Run all registered tests.
-  mocha.run();
-});
+// TODO(crbug.com/1399475): Flaky on all platforms.
+TEST_F(
+    'BluetoothInternalsTest', 'DISABLED_Startup_BluetoothInternals',
+    function() {
+      // Run all registered tests.
+      mocha.run();
+    });
