4ac40387dd524a24a6459c314490e8a0fb52afba
Andrew Grieve <agrieve@chromium.org>
2023-01-04 17:28:38 +0000

    Robolectric: Add Android 13 jar via 3pp
    And tweak the fetch.py script so that version does not need to be
    manually updated.
    
    Change-Id: I09ffdf48af37f3b2cc55e0d785278b700372687e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135139
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088787}

--- third_party/robolectric/3pp/fetch.py
+++ third_party/robolectric/3pp/fetch.py
@@ -4,13 +4,14 @@
 # found in the LICENSE file.
 
 import argparse
+import hashlib
 import json
-import os
+import pathlib
 
-_PATCH = 'cr0'
-_LATEST_VERSION = '12.1-robolectric-8229987.' + _PATCH
 # All instrumented jars + latest non-instrumented one.
 _ROBO_URL_FILES = {
+    'android-all-instrumented-13-robolectric-9030017-i4.jar':
+        'https://repo1.maven.org/maven2/org/robolectric/android-all-instrumented/13-robolectric-9030017-i4/android-all-instrumented-13-robolectric-9030017-i4.jar',
     'android-all-instrumented-12.1-robolectric-8229987-i4.jar':
         'https://repo1.maven.org/maven2/org/robolectric/android-all-instrumented/12.1-robolectric-8229987-i4/android-all-instrumented-12.1-robolectric-8229987-i4.jar',
     'android-all-instrumented-12-robolectric-7732740-i4.jar':
@@ -36,21 +37,28 @@ _ROBO_URL_FILES = {
     'android-all-instrumented-4.4_r1-robolectric-r2-i4.jar':
         'https://repo1.maven.org/maven2/org/robolectric/android-all-instrumented/4.4_r1-robolectric-r2-i4/android-all-instrumented-4.4_r1-robolectric-r2-i4.jar',
     'android-all-12.1-robolectric-8229987.jar':
-        'https://repo1.maven.org/maven2/org/robolectric/android-all/12.1-robolectric-8229987/android-all-12.1-robolectric-8229987.jar'
+        'https://repo1.maven.org/maven2/org/robolectric/android-all/12.1-robolectric-8229987/android-all-12.1-robolectric-8229987.jar',
 }
 
 def do_latest():
-  print(_LATEST_VERSION)
+  # Make the version change every time this file changes.
+  md5 = hashlib.md5()
+  md5.update(pathlib.Path(__file__).read_bytes())
+  file_hash = md5.hexdigest()[:10]
+  # Prefix with the first version from the dict, under the assumption
+  # new entries are added to the top. Do this to make the version
+  # string not entirely random.
+  first_file = next(iter(_ROBO_URL_FILES))
+  assert first_file.startswith('android-all-instrumented-')
+  assert first_file.endswith('.jar')
+  first_file = first_file[len('android-all-instrumented-'):-len('.jar')]
+  return f'{first_file}-{file_hash}'
 
 
-def _get_download_url(version):
-  download_urls, name = [], []
-  for robo_name, url in _ROBO_URL_FILES.items():
-    name.append(robo_name)
-    download_urls.append(url)
+def do_get_url():
   partial_manifest = {
-      'url': download_urls,
-      'name': name,
+      'url': list(_ROBO_URL_FILES.values()),
+      'name': list(_ROBO_URL_FILES),
       'ext': '.jar',
   }
   print(json.dumps(partial_manifest))
@@ -60,12 +68,11 @@ def main():
   ap = argparse.ArgumentParser()
   sub = ap.add_subparsers()
 
-  latest = sub.add_parser("latest")
+  latest = sub.add_parser('latest')
   latest.set_defaults(func=lambda _opts: do_latest())
 
-  download = sub.add_parser("get_url")
-  download.set_defaults(
-      func=lambda _opts: _get_download_url(os.environ['_3PP_VERSION']))
+  download = sub.add_parser('get_url')
+  download.set_defaults(func=lambda _opts: do_get_url())
 
   opts = ap.parse_args()
   opts.func(opts)
@@ -73,4 +80,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
