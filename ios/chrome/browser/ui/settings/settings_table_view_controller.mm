91ff82db5b20a8dca3cd0488d30292c18026f752
Gauthier Ambard <gambard@chromium.org>
2023-01-04 16:59:05 +0000

    [iOS] Fix Sync Off state
    The color should be grey, not red. The symbol was also the wrong one.
    
    (cherry picked from commit 489cf2173785480f313b07cf0e5aa874e84dcb91)
    
    Fixed: 1403477
    Change-Id: I70ea37c428690380360d4d3f1e426b0fc63c297a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4124039
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1086964}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4136770
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5481@{#118}
    Cr-Branched-From: 130f3e4d850f4bc7387cfb8d08aa993d288a67a9-refs/heads/main@{#1084008}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -1898,8 +1898,9 @@ UIImage* GetBrandedGoogleServicesSymbol() {
     case kSyncConsentOff: {
       googleSyncItem.detailText = l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
       if (UseSymbols()) {
-        googleSyncItem.iconImage = DefaultSettingsRootSymbol(kSyncErrorSymbol);
-        googleSyncItem.iconBackgroundColor = UIColor.redColor;
+        googleSyncItem.iconImage =
+            CustomSettingsRootSymbol(kSyncDisabledSymbol);
+        googleSyncItem.iconBackgroundColor = [UIColor colorNamed:kGrey400Color];
         googleSyncItem.iconTintColor = UIColor.whiteColor;
         googleSyncItem.iconCornerRadius = kColorfulBackgroundSymbolCornerRadius;
 
@@ -1911,7 +1912,6 @@ UIImage* GetBrandedGoogleServicesSymbol() {
     case kSyncOff:
     case kSyncEnabledWithNoSelectedTypes: {
       googleSyncItem.detailText = nil;
-      googleSyncItem.iconImage = [UIImage imageNamed:kSyncOffImageName];
       if (UseSymbols()) {
         googleSyncItem.iconImage =
             CustomSettingsRootSymbol(kSyncDisabledSymbol);
