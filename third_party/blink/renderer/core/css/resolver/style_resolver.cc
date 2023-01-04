1613b84affbe75ae9cbdc2513270b844406ae584
Ian Kilpatrick <ikilpatrick@chromium.org>
2023-01-04 20:10:55 +0000

    [StyleBuilder] Remove state.Style() from style_resolver.cc
    There should be no behaviour change.
    
    Bug: 1377295
    Change-Id: Ia689b82476864eab9ff77784e00c632aa3c43d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133244
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088901}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -325,7 +325,7 @@ PseudoId GetPseudoId(const Element& element, ElementRuleCollector* collector) {
 
 void UseCountLegacyOverlapping(Document& document,
                                const ComputedStyle& a,
-                               const ComputedStyle& b) {
+                               const ComputedStyleBuilder& b) {
   if (a.PerspectiveOrigin() != b.PerspectiveOrigin()) {
     document.CountUse(WebFeature::kCSSLegacyPerspectiveOrigin);
   }
@@ -335,10 +335,11 @@ void UseCountLegacyOverlapping(Document& document,
   if (a.BorderImage() != b.BorderImage()) {
     document.CountUse(WebFeature::kCSSLegacyBorderImage);
   }
-  if ((a.BorderTopWidth() != b.BorderTopWidth()) ||
-      (a.BorderRightWidth() != b.BorderRightWidth()) ||
-      (a.BorderBottomWidth() != b.BorderBottomWidth()) ||
-      (a.BorderLeftWidth() != b.BorderLeftWidth())) {
+  const ComputedStyle& b_style = *b.InternalStyle();
+  if ((a.BorderTopWidth() != b_style.BorderTopWidth()) ||
+      (a.BorderRightWidth() != b_style.BorderRightWidth()) ||
+      (a.BorderBottomWidth() != b_style.BorderBottomWidth()) ||
+      (a.BorderLeftWidth() != b_style.BorderLeftWidth())) {
     document.CountUse(WebFeature::kCSSLegacyBorderImageWidth);
   }
 }
@@ -1028,18 +1029,18 @@ scoped_refptr<ComputedStyle> StyleResolver::ResolveStyle(
     SetAnimationUpdateIfNeeded(style_recalc_context, state, *animating_element);
   }
 
-  GetDocument().AddViewportUnitFlags(state.Style()->ViewportUnitFlags());
+  GetDocument().AddViewportUnitFlags(state.StyleBuilder().ViewportUnitFlags());
 
-  if (state.Style()->HasRootFontRelativeUnits()) {
+  if (state.StyleBuilder().HasRootFontRelativeUnits()) {
     GetDocument().GetStyleEngine().SetUsesRootFontRelativeUnits(true);
   }
 
-  if (state.Style()->HasGlyphRelativeUnits()) {
+  if (state.StyleBuilder().HasGlyphRelativeUnits()) {
     GetDocument().GetStyleEngine().SetUsesGlyphRelativeUnits(true);
     UseCounter::Count(GetDocument(), WebFeature::kHasGlyphRelativeUnits);
   }
 
-  if (state.Style()->HasLineHeightRelativeUnits()) {
+  if (state.StyleBuilder().HasLineHeightRelativeUnits()) {
     GetDocument().GetStyleEngine().SetUsesLineHeightUnits(true);
   }
 
@@ -1107,7 +1108,8 @@ void StyleResolver::InitStyleAndApplyInheritance(
     ApplyInheritance(element, style_request, state);
   } else {
     state.SetStyle(InitialStyleForElement());
-    state.SetParentStyle(ComputedStyle::Clone(*state.Style()));
+    state.SetParentStyle(
+        ComputedStyle::Clone(*state.StyleBuilder().InternalStyle()));
     state.SetLayoutParentStyle(state.ParentStyle());
     if (!style_request.IsPseudoStyleRequest() &&
         element != GetDocument().documentElement()) {
@@ -1527,7 +1529,7 @@ void StyleResolver::ApplyBaseStyle(
 
 #if DCHECK_IS_ON()
     // Verify that we got the right answer.
-    scoped_refptr<ComputedStyle> incremental_style = state.TakeStyle();
+    scoped_refptr<const ComputedStyle> incremental_style = state.TakeStyle();
     ApplyBaseStyleNoCache(element, style_recalc_context, style_request, state,
                           cascade);
 
@@ -1547,8 +1549,10 @@ void StyleResolver::ApplyBaseStyle(
         state.StyleBuilder().ViewportUnitFlags() |
         incremental_style->ViewportUnitFlags());
 
+    scoped_refptr<const ComputedStyle> style_snapshot =
+        state.StyleBuilder().CloneStyle();
     DCHECK_EQ(g_null_atom, ComputeBaseComputedStyleDiff(incremental_style.get(),
-                                                        *state.Style()));
+                                                        *style_snapshot));
     // The incremental style must not contain BaseData, otherwise we'd risk
     // creating an infinite chain of BaseData/ComputedStyle in
     // ApplyAnimatedStyle.
@@ -2057,10 +2061,11 @@ StyleResolver::CacheSuccess StyleResolver::ApplyMatchedCache(
     }
     state.UpdateFont();
   }
-  // This is needed because pseudo_argument is copied to the state.Style() as
-  // part of a raredata field when copying non-inherited values from the cached
-  // result. The argument isn't a style property per se, it represents the
-  // argument to the matching element which should remain unchanged.
+  // This is needed because pseudo_argument is copied to the
+  // state.StyleBuilder() as part of a raredata field when copying
+  // non-inherited values from the cached result. The argument isn't a style
+  // property per se, it represents the argument to the matching element which
+  // should remain unchanged.
   state.StyleBuilder().SetPseudoArgument(pseudo_argument);
 
   return CacheSuccess(is_inherited_cache_hit, is_non_inherited_cache_hit, key,
@@ -2281,7 +2286,8 @@ void StyleResolver::CascadeAndApplyMatchedProperties(StyleResolverState& state,
         state.StyleBuilder().CloneStyle();
     // Re-apply all overlapping properties (both legacy and non-legacy).
     apply(CascadeFilter(CSSProperty::kOverlapping, false));
-    UseCountLegacyOverlapping(GetDocument(), *non_legacy_style, *state.Style());
+    UseCountLegacyOverlapping(GetDocument(), *non_legacy_style,
+                              state.StyleBuilder());
   }
 
   // NOTE: This flag (and the length conversion flags) need to be set before the
