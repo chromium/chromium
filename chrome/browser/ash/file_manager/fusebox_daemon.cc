9eb36787622ec875cfd2480dbcf28708882a1889
Noel Gordon <noel@chromium.org>
2023-01-04 04:09:32 +0000

    volume manager: add an instanced fusebox daemon client
    Volume manager needs an instanced version of the fusebox, so it can
    be unmounted when the last volume manager user exits [1].
    
    [1] If an incognito volume manager exits in the current system, the
    daemon unmounts and the regular volume manager can no longer use it
    (see bug).
    
    Use a scoped_refptr<FuseBoxDaemon> to manage daemon life-time, when
    returning instances to the volume manager user. When volume manager
    users exit, the reference count drops. When the last volume manager
    user exits, there are no more refs and scoped_refptr<> destroys the
    FuseBoxDaemon, which unmounts it.
    
    Not hooked up to volume manager yet: that is the next CL.
    
      the incognito window. FSP in regular files app still works (shows
      its content, all tested with the next CL).
    
    Bug: b/263194149
    Test: use FSP in regular files app, then in incognito window. Close
    Change-Id: I8836fa6ff658d2ecf2f3a9028c7f5c3c3a4f06d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127571
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1088565}

---
+++ chrome/browser/ash/file_manager/fusebox_daemon.cc
@@ -0,0 +1,96 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/file_manager/fusebox_daemon.h"
+
+#include "base/bind.h"
+#include "base/callback_helpers.h"
+#include "base/check.h"
+#include "base/logging.h"
+#include "chrome/browser/ash/fusebox/fusebox_server.h"
+
+namespace file_manager {
+
+// static
+scoped_refptr<FuseBoxDaemon> FuseBoxDaemon::GetInstance() {
+  static auto* fusebox_daemon = new FuseBoxDaemon;
+  return fusebox_daemon;
+}
+
+FuseBoxDaemon::FuseBoxDaemon() {
+  cros_disks_mount_manager_ = CrosDisksMountManager::GetInstance();
+
+  if (!cros_disks_mount_manager_) {
+    return;  // cros_disks_mount_manager_ can be null in unit_tests.
+  }
+
+  constexpr auto type = ash::MountType::kNetworkStorage;
+  constexpr auto mode = ash::MountAccessMode::kReadWrite;
+
+  cros_disks_mount_manager_->MountPath(
+      CrosDisksFuseBoxHelperURI(),
+      /*source_format*/ {},
+      /*mount_label*/ {},
+      /*options*/ {/* add fusebox --flags here */}, type, mode,
+      base::BindOnce(&FuseBoxDaemon::MountResponse,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void FuseBoxDaemon::MountResponse(ash::MountError error,
+                                  const FuseBoxMountInfo& info) {
+  if (error == ash::MountError::kSuccess) {
+    mounted_ = true;
+  } else {
+    LOG(ERROR) << CrosDisksFuseBoxHelperURI() << " mount error " << error;
+    mounted_ = (error == ash::MountError::kPathAlreadyMounted);
+  }
+
+  if (mounted_ && !pending_attach_storage_calls_.empty()) {
+    const auto queued = pending_attach_storage_calls_.size();
+    VLOG(1) << "Calling " << queued << " queued AttachStorage calls";
+    for (const auto& it : pending_attach_storage_calls_) {
+      const auto& [url, read_only] = it.second;
+      AttachStorage(it.first, url, read_only);
+    }
+  }
+
+  pending_attach_storage_calls_.clear();
+}
+
+void FuseBoxDaemon::AttachStorage(const std::string& subdir,
+                                  const std::string& url,
+                                  bool read_only) {
+  if (!mounted_) {
+    VLOG(1) << "FuseBox is not mounted: queued AttachStorage call";
+    pending_attach_storage_calls_.emplace(subdir, std::pair{url, read_only});
+    return;
+  }
+
+  if (auto* fusebox_server = fusebox::Server::GetInstance()) {
+    fusebox_server->RegisterFSURLPrefix(subdir, url, read_only);
+  } else {
+    VLOG(1) << "No FuseBox server available for AttachStorage";
+  }
+}
+
+void FuseBoxDaemon::DetachStorage(const std::string& subdir) {
+  pending_attach_storage_calls_.erase(subdir);
+
+  if (auto* fusebox_server = fusebox::Server::GetInstance()) {
+    fusebox_server->UnregisterFSURLPrefix(subdir);
+  } else {
+    VLOG(1) << "No FuseBox server available for DetachStorage";
+  }
+}
+
+FuseBoxDaemon::~FuseBoxDaemon() {
+  if (!cros_disks_mount_manager_) {
+    return;  // cros_disks_mount_manager_ can be null in unit_tests.
+  }
+
+  cros_disks_mount_manager_->UnmountPath(CrosDisksFuseBoxHelperURI(),
+                                         base::DoNothing());
+}
+
+}  // namespace file_manager
---
+++ chrome/browser/ash/file_manager/fusebox_daemon.h
@@ -0,0 +1,68 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_FILE_MANAGER_FUSEBOX_DAEMON_H_
+#define CHROME_BROWSER_ASH_FILE_MANAGER_FUSEBOX_DAEMON_H_
+
+#include <map>
+#include <string>
+
+#include "base/component_export.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "chromeos/ash/components/disks/disk_mount_manager.h"
+
+namespace file_manager {
+
+class COMPONENT_EXPORT(ASH_DBUS_CROS_DISKS) FuseBoxDaemon
+    : public base::RefCounted<FuseBoxDaemon> {
+ public:
+  // Returns the fusebox daemon. Creates the fusebox daemon if needed.
+  static scoped_refptr<FuseBoxDaemon> GetInstance();
+
+  // Attach fusebox storage: adds fusebox daemon <mount-point>/subdir used to
+  // serve the content of the Chrome storage::FileSystemURL |url| via FUSE to
+  // the Linux file system. The <mount-point>/subdir content is read-write by
+  // default: use |read_only| true to make the content read-only.
+  void AttachStorage(const std::string& subdir,
+                     const std::string& url,
+                     bool read_only);
+
+  // Detach fusebox storage: removes fusebox daemon <mount-point>/subdir.
+  void DetachStorage(const std::string& subdir);
+
+ private:
+  friend class base::RefCounted<FuseBoxDaemon>;
+
+  FuseBoxDaemon();
+  ~FuseBoxDaemon();
+
+  // Cros-disks mount manager.
+  using CrosDisksMountManager = ::ash::disks::DiskMountManager;
+  CrosDisksMountManager* cros_disks_mount_manager_ = nullptr;
+
+  // FuseBox daemon URI: cros-disks URI protocol is fusebox://<mount-point>.
+  static char* CrosDisksFuseBoxHelperURI() {
+    static char kCrosDisksFuseBoxHelperURI[] = "fusebox://fusebox";
+    return kCrosDisksFuseBoxHelperURI;
+  }
+
+  // Construction creates and mounts the cros-disks fusebox daemon.
+  using FuseBoxMountInfo = ::ash::disks::DiskMountManager::MountPoint;
+  void MountResponse(ash::MountError error, const FuseBoxMountInfo& info);
+
+  // True if the fusebox daemon was successfully mounted.
+  bool mounted_ = false;
+
+  // List of `AttachStorage` calls prior to the fusebox daemon mount.
+  using UrlReadOnlyPair = std::pair<std::string, bool>;
+  std::map<std::string, UrlReadOnlyPair> pending_attach_storage_calls_;
+
+  // base::WeakPtr{this} factory.
+  base::WeakPtrFactory<FuseBoxDaemon> weak_ptr_factory_{this};
+};
+
+}  // namespace file_manager
+
+#endif  // CHROME_BROWSER_ASH_FILE_MANAGER_FUSEBOX_DAEMON_H_
