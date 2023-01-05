044261fdfed803a2c6be53b4d1846b087cb16692
Sneha Nagpaul <snehanagpaul@google.com>
2023-01-05 02:18:58 +0000

    [M110][URLFiltering] Report a UrlFilteringInterstitial Event on bypassing Enterprise Warn Interstitial
    (cherry picked from commit 364529a2350b12d6f1463354a070af290f5c6029)
    
    Bug: 1402746
    Change-Id: I0706c764584acbed307c2e3d390d283c811e65b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116021
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1085626}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133369
    Cr-Commit-Position: refs/branch-heads/5481@{#139}
    Cr-Branched-From: 130f3e4d850f4bc7387cfb8d08aa993d288a67a9-refs/heads/main@{#1084008}

--- components/safe_browsing/content/browser/ui_manager.cc
+++ components/safe_browsing/content/browser/ui_manager.cc
@@ -314,12 +314,23 @@ void SafeBrowsingUIManager::OnBlockingPageDone(
   BaseUIManager::OnBlockingPageDone(resources, proceed, web_contents,
                                     main_frame_url, showed_interstitial);
   if (proceed && !resources.empty()) {
+#if !BUILDFLAG(IS_ANDROID)
+    if (base::FeatureList::IsEnabled((kRealTimeUrlFilteringForEnterprise)) &&
+        resources[0].threat_type ==
+            safe_browsing::SB_THREAT_TYPE_MANAGED_POLICY_WARN) {
+      delegate_->TriggerUrlFilteringInterstitialExtensionEventIfDesired(
+          web_contents, main_frame_url, "ENTERPRISE_WARNED_BYPASS",
+          resources[0].rt_lookup_response);
+      return;
+    }
+#endif
     delegate_->TriggerSecurityInterstitialProceededExtensionEventIfDesired(
         web_contents, main_frame_url,
         GetThreatTypeStringForInterstitial(resources[0].threat_type),
         /*net_error_code=*/0);
   }
 }
+
 // Static.
 GURL SafeBrowsingUIManager::GetMainFrameAllowlistUrlForResourceForTesting(
     const security_interstitials::UnsafeResource& resource) {
