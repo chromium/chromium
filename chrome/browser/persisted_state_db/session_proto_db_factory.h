51b2a4f385dd8916a1b44a3302c0f095705735c2
David Maunder <davidjm@google.com>
2023-01-05 22:52:37 +0000

    Post to UI thread TaskRunner on FailedToInit in SessionProtoDB
    We are currently posting to the wrong thread - we should post
    to the UI thread, not a background thread.
    
    (cherry picked from commit 853c60880ee7ef9ef620ac7e42c57726092d2d6f)
    
    Bug: 1394494
    Change-Id: I4e4f5170818fe21f9d63852b42fac53c5f31329d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4088730
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1082022}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140198
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Auto-Submit: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen <wychen@chromium.org>
    Owners-Override: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/branch-heads/5359@{#1287}
    Cr-Branched-From: 27d3765d341b09369006d030f83f582a29eb57ae-refs/heads/main@{#1058933}

--- chrome/browser/persisted_state_db/session_proto_db_factory.h
+++ chrome/browser/persisted_state_db/session_proto_db_factory.h
@@ -12,6 +12,8 @@
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/session_proto_db/session_proto_db.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -109,32 +111,37 @@ KeyedService* SessionProtoDBFactory<T>::BuildServiceInstanceFor(
     return new SessionProtoDB<T>(
         proto_database_provider,
         context->GetPath().AppendASCII(kPersistedStateDBFolder),
-        leveldb_proto::ProtoDbType::PERSISTED_STATE_DATABASE);
+        leveldb_proto::ProtoDbType::PERSISTED_STATE_DATABASE,
+        content::GetUIThreadTaskRunner({}));
   } else if (std::is_base_of<
                  commerce_subscription_db::CommerceSubscriptionContentProto,
                  T>::value) {
     return new SessionProtoDB<T>(
         proto_database_provider,
         context->GetPath().AppendASCII(kCommerceSubscriptionDBFolder),
-        leveldb_proto::ProtoDbType::COMMERCE_SUBSCRIPTION_DATABASE);
+        leveldb_proto::ProtoDbType::COMMERCE_SUBSCRIPTION_DATABASE,
+        content::GetUIThreadTaskRunner({}));
 #if !BUILDFLAG(IS_ANDROID)
   } else if (std::is_base_of<cart_db::ChromeCartContentProto, T>::value) {
     return new SessionProtoDB<T>(
         proto_database_provider,
         context->GetPath().AppendASCII(kChromeCartDBFolder),
-        leveldb_proto::ProtoDbType::CART_DATABASE);
+        leveldb_proto::ProtoDbType::CART_DATABASE,
+        content::GetUIThreadTaskRunner({}));
   } else if (std::is_base_of<coupon_db::CouponContentProto, T>::value) {
     return new SessionProtoDB<T>(
         proto_database_provider,
         context->GetPath().AppendASCII(kCouponDBFolder),
-        leveldb_proto::ProtoDbType::COUPON_DATABASE);
+        leveldb_proto::ProtoDbType::COUPON_DATABASE,
+        content::GetUIThreadTaskRunner({}));
 #else
   } else if (std::is_base_of<merchant_signal_db::MerchantSignalContentProto,
                              T>::value) {
     return new SessionProtoDB<T>(
         proto_database_provider,
         context->GetPath().AppendASCII(kMerchantTrustSignalDBFolder),
-        leveldb_proto::ProtoDbType::MERCHANT_TRUST_SIGNAL_DATABASE);
+        leveldb_proto::ProtoDbType::MERCHANT_TRUST_SIGNAL_DATABASE,
+        content::GetUIThreadTaskRunner({}));
 #endif
   } else {
     // Must add in leveldb_proto::ProtoDbType and database directory folder for
