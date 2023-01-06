f17a2471ccb7fd27aca9b8ffe0bed9af3aad51fd
Cammie Smith Barnes <cammie@chromium.org>
2023-01-06 16:58:46 +0000

    Revert "Optimize NOTREACHED() in official builds"
    This reverts commit 739790ea57d2d3a0af4a977477d0baf110835ddc.
    
    Reason for revert: Suspected culprit for crbug.com/1405453
    
    Original change's description:
    > Optimize NOTREACHED() in official builds
    >
    > This discards __FILE__, __LINE__ and stream parameters to NOTREACHED()
    > when CHECK() is optimized.
    >
    > This reclaims ~60% of compressed-size gains on Fuchsia official which
    > could make it more palatable to run for a full milestone (still TBD). It
    > reclaims ~85% on Android according to the Chromium Binary Size bot.
    >
    > Because these were previously on on ChromeOS but not optimized, this
    > likely saves at least half a megabyte on ChromeOS (assuming it has more
    > size from NOTREACHED() than Android does). Either way the gains on
    > ChromeOS should be significant, hope someone notices. :)
    >
    > This also, very surprisingly, addresses most if not all of a
    > blink_perf.accessibility regression where the sum of
    > RenderAccessibilityImpl::SendPendingAccessibilityEvents is reduced from
    > 5,031.085 ms to 4,614.637ms. Perhaps the code generated from a
    > NOTREACHED() with streaming arguments is significant in a critical path?
    > Color me very surprised.
    >
    > Bug: 1401972, 1401973
    > Change-Id: Ib50b6a8fb56d7b201ddfee61c869d52c8a134140
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4140082
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: danakj <danakj@chromium.org>
    > Auto-Submit: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1089709}
    
    Bug: 1401972, 1401973
    Change-Id: I606f4d74d143c11f00dbe15296da4bb3d0c8ba29
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4142281
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    Owners-Override: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089758}

--- base/check.cc
+++ base/check.cc
@@ -201,10 +201,6 @@ NotReachedError NotReachedError::NotReached(const char* file, int line) {
   return NotReachedError(log_message);
 }
 
-void NotReachedError::TriggerNotReached() {
-  NotReached("", -1);
-}
-
 NotReachedError::~NotReachedError() = default;
 
 void RawCheck(const char* message) {
