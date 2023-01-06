56ddb4e4bcf14074a02ea4e699baf8f6cb4945b0
Vivian Pao <vpao@google.com>
2023-01-06 04:24:19 +0000

    Enable kGuestOsFileTasksUseAppService flag
    Bug: 1340095
    Change-Id: Ib42f9fad384dc31c5d131db8a0377bba34a08739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135386
    Commit-Queue: Vivian Pao <vpao@google.com>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089611}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2204,7 +2204,7 @@ BASE_FEATURE(kArcFileTasksUseAppService,
 // apps.
 BASE_FEATURE(kGuestOsFileTasksUseAppService,
              "GuestOsFileTasksUseAppService",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 ////////////////////////////////////////////////////////////////////////////////
 
