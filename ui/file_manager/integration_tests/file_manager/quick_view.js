71e2af84b665f7c1be5fb9b0d1c8b65768003d3b
Alex Danilo <adanilo@chromium.org>
2023-01-06 01:38:07 +0000

    Files app: Fix Quickview audio player background in dark mode
    Bug: b/245854340
    Tests: browser_tests --gtest_filter="QuickView*"
    Change-Id: I451abaf7eabc8ad437a0e5f52ed3b9af7d62bf9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135181
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089567}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -1404,8 +1404,13 @@ testcase.openQuickViewAudio = async () => {
   const backgroundColor = await remoteCall.executeJsInPreviewTag(
       appId, preview, getBackgroundStyle);
 
-  // Check: the preview body backgroundColor should be transparent black.
-  chrome.test.assertEq('rgba(0, 0, 0, 0)', backgroundColor[0]);
+  if (await isDarkModeEnabled()) {
+    // Check: the preview body backgroundColor should be black.
+    chrome.test.assertEq('rgb(0, 0, 0)', backgroundColor[0]);
+  } else {
+    // Check: the preview body backgroundColor should be transparent black.
+    chrome.test.assertEq('rgba(0, 0, 0, 0)', backgroundColor[0]);
+  }
 
   // Check: the correct mimeType should be displayed.
   const mimeType = await getQuickViewMetadataBoxField(appId, 'Type');
@@ -1453,8 +1458,13 @@ testcase.openQuickViewAudioOnDrive = async () => {
   const backgroundColor = await remoteCall.executeJsInPreviewTag(
       appId, preview, getBackgroundStyle);
 
-  // Check: the preview body backgroundColor should be transparent black.
-  chrome.test.assertEq('rgba(0, 0, 0, 0)', backgroundColor[0]);
+  if (await isDarkModeEnabled()) {
+    // Check: the preview body backgroundColor should be black.
+    chrome.test.assertEq('rgb(0, 0, 0)', backgroundColor[0]);
+  } else {
+    // Check: the preview body backgroundColor should be transparent black.
+    chrome.test.assertEq('rgba(0, 0, 0, 0)', backgroundColor[0]);
+  }
 };
 
 /**
