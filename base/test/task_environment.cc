2be61ae3f343c114dfabe9ddec8d88b1fea9fd54
Tom Sepez <tsepez@chromium.org>
2023-01-07 00:16:43 +0000

    Mark two more DanglingUntriaged ptrs in MockTestEnvironment
    Follow-on from https://crrev.com/c/4137072, these two show up
    in subsequent base_unittests.
    
    Change-Id: I7da1aea2820e1c7fd8181667412622e002e472a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144201
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1089969}

--- base/test/task_environment.cc
+++ base/test/task_environment.cc
@@ -344,8 +344,9 @@ class TaskEnvironment::MockTimeDomain : public sequence_manager::TimeDomain {
  private:
   SEQUENCE_CHECKER(sequence_checker_);
 
-  raw_ptr<internal::ThreadPoolImpl> thread_pool_ = nullptr;
-  raw_ptr<const TestTaskTracker> thread_pool_task_tracker_ = nullptr;
+  raw_ptr<internal::ThreadPoolImpl, DanglingUntriaged> thread_pool_ = nullptr;
+  raw_ptr<const TestTaskTracker, DanglingUntriaged> thread_pool_task_tracker_ =
+      nullptr;
 
   const raw_ptr<sequence_manager::internal::SequenceManagerImpl,
                 DanglingUntriaged>
