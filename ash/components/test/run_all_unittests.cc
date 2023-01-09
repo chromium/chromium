97e46b1515f682a492c902c2fc8d1fad61e10648
Henrique Ferreiro <hferreiro@igalia.com>
2023-01-09 18:52:16 +0000

    Remove unneeded code from ash/components/test/
    Arc is the only Ash component remaining in ash/components/, while all
    others have been migrated away to chromeos/ash/components/. Some of the
    supporting test code can be removed.
    
    This CL is part of the ChromeOS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: Ic5e641b3a10b0080fe77e8cb3c2dadbd7ac63e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4125955
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090427}

--- ash/components/test/run_all_unittests.cc
+++ ash/components/test/run_all_unittests.cc
@@ -3,23 +3,13 @@
 // found in the LICENSE file.
 
 #include "base/test/launcher/unit_test_launcher.h"
-#include "base/threading/thread.h"
 #include "chromeos/ash/components/test/ash_test_suite.h"
 #include "mojo/core/embedder/embedder.h"
-#include "mojo/core/embedder/scoped_ipc_support.h"
 
 int main(int argc, char** argv) {
   ash::AshTestSuite test_suite(argc, argv);
 
   mojo::core::Init();
-  // The IPC thread is necessary for the window service.
-  base::Thread ipc_thread("IPC thread");
-  ipc_thread.StartWithOptions(
-      base::Thread::Options(base::MessagePumpType::IO, 0));
-  mojo::core::ScopedIPCSupport ipc_support(
-      ipc_thread.task_runner(),
-      mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
-
   return base::LaunchUnitTests(
       argc, argv,
       base::BindOnce(&ash::AshTestSuite::Run, base::Unretained(&test_suite)));
