433d1422a528f478151c958d35ffa8c16ad99a22
Peter Wen <wnwen@chromium.org>
2023-01-09 20:41:29 +0000

    Android: Switch url to use all.json once available
    Once all.json is available, switch to it instead of
    monochrome_public_bundle.json. The benefit of all.json is that it
    includes test class files as well as all other java targets producing a
    jar file. This improves the current state where only transitive deps of
    monochrome_public_bundle are included in dep analysis.
    
    Bug: 1392165
    Change-Id: I65b53b672daf409d193aae2cbfced6a6d7e8222d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148867
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090485}

--- tools/android/dependency_analysis/js/src/load_graph.js
+++ tools/android/dependency_analysis/js/src/load_graph.js
@@ -4,7 +4,7 @@
 
 // For ease of development, we load the graph from this hardcoded location by
 // default. This should be changed when a snapshot picker is implemented.
-const LATEST_GRAPH = 'https://storage.googleapis.com/clank-dependency-graphs/latest/monochrome_public_bundle.json';
+const LATEST_GRAPH = 'https://storage.googleapis.com/clank-dependency-graphs/latest/all.json';
 
 // We serve our testing data on localhost:8888 as a
 // fallback that will be triggered if CORS blocks the request. In production,
