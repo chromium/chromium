79a02bd4189dfb5c4399787e8814edebc443d8e8
Thomas Lukaszewicz <tluk@chromium.org>
2023-01-10 22:06:14 +0000

    Update ref colors to use material_color_utilities
    Removed newline from README.chromium that was causing parse
    failures.
    
    Bug: None
    Change-Id: I245f30279a17e800c98e98929d9b79b470f80c0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4149770
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091031}

--- ui/color/ref_color_mixer.cc
+++ ui/color/ref_color_mixer.cc
@@ -5,6 +5,7 @@
 #include "ui/color/ref_color_mixer.h"
 
 #include "base/logging.h"
+#include "third_party/material_color_utilities/src/cpp/palettes/core.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_mixer.h"
@@ -24,93 +25,92 @@ void AddRefColorMixer(ColorProvider* provider,
   // TODO(skau): Before this launches, make sure this is always populated.
   SkColor seed_color = key.user_color.value_or(gfx::kGoogleBlue400);
 
-  // TODO(skau): Replace with official implementation when available.
-  ToneMap tone_map = GetTempPalette(seed_color);
+  auto palette = material_color_utilities::CorePalette::Of(seed_color);
 
   ColorMixer& mixer = provider->AddMixer();
-  mixer[kColorRefPrimary0] = {tone_map.primary[Luma::k0]};
-  mixer[kColorRefPrimary10] = {tone_map.primary[Luma::k10]};
-  mixer[kColorRefPrimary20] = {tone_map.primary[Luma::k20]};
-  mixer[kColorRefPrimary30] = {tone_map.primary[Luma::k30]};
-  mixer[kColorRefPrimary40] = {tone_map.primary[Luma::k40]};
-  mixer[kColorRefPrimary50] = {tone_map.primary[Luma::k50]};
-  mixer[kColorRefPrimary60] = {tone_map.primary[Luma::k60]};
-  mixer[kColorRefPrimary70] = {tone_map.primary[Luma::k70]};
-  mixer[kColorRefPrimary80] = {tone_map.primary[Luma::k80]};
-  mixer[kColorRefPrimary90] = {tone_map.primary[Luma::k90]};
-  mixer[kColorRefPrimary95] = {tone_map.primary[Luma::k95]};
-  mixer[kColorRefPrimary99] = {tone_map.primary[Luma::k99]};
-  mixer[kColorRefPrimary100] = {tone_map.primary[Luma::k100]};
+  mixer[kColorRefPrimary0] = {palette.primary().get(0)};
+  mixer[kColorRefPrimary10] = {palette.primary().get(10)};
+  mixer[kColorRefPrimary20] = {palette.primary().get(20)};
+  mixer[kColorRefPrimary30] = {palette.primary().get(30)};
+  mixer[kColorRefPrimary40] = {palette.primary().get(40)};
+  mixer[kColorRefPrimary50] = {palette.primary().get(50)};
+  mixer[kColorRefPrimary60] = {palette.primary().get(60)};
+  mixer[kColorRefPrimary70] = {palette.primary().get(70)};
+  mixer[kColorRefPrimary80] = {palette.primary().get(80)};
+  mixer[kColorRefPrimary90] = {palette.primary().get(90)};
+  mixer[kColorRefPrimary95] = {palette.primary().get(95)};
+  mixer[kColorRefPrimary99] = {palette.primary().get(99)};
+  mixer[kColorRefPrimary100] = {palette.primary().get(100)};
 
-  mixer[kColorRefSecondary0] = {tone_map.secondary[Luma::k0]};
-  mixer[kColorRefSecondary10] = {tone_map.secondary[Luma::k10]};
-  mixer[kColorRefSecondary20] = {tone_map.secondary[Luma::k20]};
-  mixer[kColorRefSecondary30] = {tone_map.secondary[Luma::k30]};
-  mixer[kColorRefSecondary40] = {tone_map.secondary[Luma::k40]};
-  mixer[kColorRefSecondary50] = {tone_map.secondary[Luma::k50]};
-  mixer[kColorRefSecondary60] = {tone_map.secondary[Luma::k60]};
-  mixer[kColorRefSecondary70] = {tone_map.secondary[Luma::k70]};
-  mixer[kColorRefSecondary80] = {tone_map.secondary[Luma::k80]};
-  mixer[kColorRefSecondary90] = {tone_map.secondary[Luma::k90]};
-  mixer[kColorRefSecondary95] = {tone_map.secondary[Luma::k95]};
-  mixer[kColorRefSecondary99] = {tone_map.secondary[Luma::k99]};
-  mixer[kColorRefSecondary100] = {tone_map.secondary[Luma::k100]};
+  mixer[kColorRefSecondary0] = {palette.secondary().get(0)};
+  mixer[kColorRefSecondary10] = {palette.secondary().get(10)};
+  mixer[kColorRefSecondary20] = {palette.secondary().get(20)};
+  mixer[kColorRefSecondary30] = {palette.secondary().get(30)};
+  mixer[kColorRefSecondary40] = {palette.secondary().get(40)};
+  mixer[kColorRefSecondary50] = {palette.secondary().get(50)};
+  mixer[kColorRefSecondary60] = {palette.secondary().get(60)};
+  mixer[kColorRefSecondary70] = {palette.secondary().get(70)};
+  mixer[kColorRefSecondary80] = {palette.secondary().get(80)};
+  mixer[kColorRefSecondary90] = {palette.secondary().get(90)};
+  mixer[kColorRefSecondary95] = {palette.secondary().get(95)};
+  mixer[kColorRefSecondary99] = {palette.secondary().get(99)};
+  mixer[kColorRefSecondary100] = {palette.secondary().get(100)};
 
-  mixer[kColorRefTertiary0] = {tone_map.tertiary[Luma::k0]};
-  mixer[kColorRefTertiary10] = {tone_map.tertiary[Luma::k10]};
-  mixer[kColorRefTertiary20] = {tone_map.tertiary[Luma::k20]};
-  mixer[kColorRefTertiary30] = {tone_map.tertiary[Luma::k30]};
-  mixer[kColorRefTertiary40] = {tone_map.tertiary[Luma::k40]};
-  mixer[kColorRefTertiary50] = {tone_map.tertiary[Luma::k50]};
-  mixer[kColorRefTertiary60] = {tone_map.tertiary[Luma::k60]};
-  mixer[kColorRefTertiary70] = {tone_map.tertiary[Luma::k70]};
-  mixer[kColorRefTertiary80] = {tone_map.tertiary[Luma::k80]};
-  mixer[kColorRefTertiary90] = {tone_map.tertiary[Luma::k90]};
-  mixer[kColorRefTertiary95] = {tone_map.tertiary[Luma::k95]};
-  mixer[kColorRefTertiary99] = {tone_map.tertiary[Luma::k99]};
-  mixer[kColorRefTertiary100] = {tone_map.tertiary[Luma::k100]};
+  mixer[kColorRefTertiary0] = {palette.tertiary().get(0)};
+  mixer[kColorRefTertiary10] = {palette.tertiary().get(10)};
+  mixer[kColorRefTertiary20] = {palette.tertiary().get(20)};
+  mixer[kColorRefTertiary30] = {palette.tertiary().get(30)};
+  mixer[kColorRefTertiary40] = {palette.tertiary().get(40)};
+  mixer[kColorRefTertiary50] = {palette.tertiary().get(50)};
+  mixer[kColorRefTertiary60] = {palette.tertiary().get(60)};
+  mixer[kColorRefTertiary70] = {palette.tertiary().get(70)};
+  mixer[kColorRefTertiary80] = {palette.tertiary().get(80)};
+  mixer[kColorRefTertiary90] = {palette.tertiary().get(90)};
+  mixer[kColorRefTertiary95] = {palette.tertiary().get(95)};
+  mixer[kColorRefTertiary99] = {palette.tertiary().get(99)};
+  mixer[kColorRefTertiary100] = {palette.tertiary().get(100)};
 
-  mixer[kColorRefError0] = {tone_map.error[Luma::k0]};
-  mixer[kColorRefError10] = {tone_map.error[Luma::k10]};
-  mixer[kColorRefError20] = {tone_map.error[Luma::k20]};
-  mixer[kColorRefError30] = {tone_map.error[Luma::k30]};
-  mixer[kColorRefError40] = {tone_map.error[Luma::k40]};
-  mixer[kColorRefError50] = {tone_map.error[Luma::k50]};
-  mixer[kColorRefError60] = {tone_map.error[Luma::k60]};
-  mixer[kColorRefError70] = {tone_map.error[Luma::k70]};
-  mixer[kColorRefError80] = {tone_map.error[Luma::k80]};
-  mixer[kColorRefError90] = {tone_map.error[Luma::k90]};
-  mixer[kColorRefError95] = {tone_map.error[Luma::k95]};
-  mixer[kColorRefError99] = {tone_map.error[Luma::k99]};
-  mixer[kColorRefError100] = {tone_map.error[Luma::k100]};
+  mixer[kColorRefError0] = {palette.error().get(0)};
+  mixer[kColorRefError10] = {palette.error().get(10)};
+  mixer[kColorRefError20] = {palette.error().get(20)};
+  mixer[kColorRefError30] = {palette.error().get(30)};
+  mixer[kColorRefError40] = {palette.error().get(40)};
+  mixer[kColorRefError50] = {palette.error().get(50)};
+  mixer[kColorRefError60] = {palette.error().get(60)};
+  mixer[kColorRefError70] = {palette.error().get(70)};
+  mixer[kColorRefError80] = {palette.error().get(80)};
+  mixer[kColorRefError90] = {palette.error().get(90)};
+  mixer[kColorRefError95] = {palette.error().get(95)};
+  mixer[kColorRefError99] = {palette.error().get(99)};
+  mixer[kColorRefError100] = {palette.error().get(100)};
 
-  mixer[kColorRefNeutral0] = {tone_map.neutral1[Luma::k0]};
-  mixer[kColorRefNeutral10] = {tone_map.neutral1[Luma::k10]};
-  mixer[kColorRefNeutral20] = {tone_map.neutral1[Luma::k20]};
-  mixer[kColorRefNeutral30] = {tone_map.neutral1[Luma::k30]};
-  mixer[kColorRefNeutral40] = {tone_map.neutral1[Luma::k40]};
-  mixer[kColorRefNeutral50] = {tone_map.neutral1[Luma::k50]};
-  mixer[kColorRefNeutral60] = {tone_map.neutral1[Luma::k60]};
-  mixer[kColorRefNeutral70] = {tone_map.neutral1[Luma::k70]};
-  mixer[kColorRefNeutral80] = {tone_map.neutral1[Luma::k80]};
-  mixer[kColorRefNeutral90] = {tone_map.neutral1[Luma::k90]};
-  mixer[kColorRefNeutral95] = {tone_map.neutral1[Luma::k95]};
-  mixer[kColorRefNeutral99] = {tone_map.neutral1[Luma::k99]};
-  mixer[kColorRefNeutral100] = {tone_map.neutral1[Luma::k100]};
+  mixer[kColorRefNeutral0] = {palette.neutral().get(0)};
+  mixer[kColorRefNeutral10] = {palette.neutral().get(10)};
+  mixer[kColorRefNeutral20] = {palette.neutral().get(20)};
+  mixer[kColorRefNeutral30] = {palette.neutral().get(30)};
+  mixer[kColorRefNeutral40] = {palette.neutral().get(40)};
+  mixer[kColorRefNeutral50] = {palette.neutral().get(50)};
+  mixer[kColorRefNeutral60] = {palette.neutral().get(60)};
+  mixer[kColorRefNeutral70] = {palette.neutral().get(70)};
+  mixer[kColorRefNeutral80] = {palette.neutral().get(80)};
+  mixer[kColorRefNeutral90] = {palette.neutral().get(90)};
+  mixer[kColorRefNeutral95] = {palette.neutral().get(95)};
+  mixer[kColorRefNeutral99] = {palette.neutral().get(99)};
+  mixer[kColorRefNeutral100] = {palette.neutral().get(100)};
 
-  mixer[kColorRefNeutralVariant0] = {tone_map.neutral2[Luma::k0]};
-  mixer[kColorRefNeutralVariant10] = {tone_map.neutral2[Luma::k10]};
-  mixer[kColorRefNeutralVariant20] = {tone_map.neutral2[Luma::k20]};
-  mixer[kColorRefNeutralVariant30] = {tone_map.neutral2[Luma::k30]};
-  mixer[kColorRefNeutralVariant40] = {tone_map.neutral2[Luma::k40]};
-  mixer[kColorRefNeutralVariant50] = {tone_map.neutral2[Luma::k50]};
-  mixer[kColorRefNeutralVariant60] = {tone_map.neutral2[Luma::k60]};
-  mixer[kColorRefNeutralVariant70] = {tone_map.neutral2[Luma::k70]};
-  mixer[kColorRefNeutralVariant80] = {tone_map.neutral2[Luma::k80]};
-  mixer[kColorRefNeutralVariant90] = {tone_map.neutral2[Luma::k90]};
-  mixer[kColorRefNeutralVariant95] = {tone_map.neutral2[Luma::k95]};
-  mixer[kColorRefNeutralVariant99] = {tone_map.neutral2[Luma::k99]};
-  mixer[kColorRefNeutralVariant100] = {tone_map.neutral2[Luma::k100]};
+  mixer[kColorRefNeutralVariant0] = {palette.neutral_variant().get(0)};
+  mixer[kColorRefNeutralVariant10] = {palette.neutral_variant().get(10)};
+  mixer[kColorRefNeutralVariant20] = {palette.neutral_variant().get(20)};
+  mixer[kColorRefNeutralVariant30] = {palette.neutral_variant().get(30)};
+  mixer[kColorRefNeutralVariant40] = {palette.neutral_variant().get(40)};
+  mixer[kColorRefNeutralVariant50] = {palette.neutral_variant().get(50)};
+  mixer[kColorRefNeutralVariant60] = {palette.neutral_variant().get(60)};
+  mixer[kColorRefNeutralVariant70] = {palette.neutral_variant().get(70)};
+  mixer[kColorRefNeutralVariant80] = {palette.neutral_variant().get(80)};
+  mixer[kColorRefNeutralVariant90] = {palette.neutral_variant().get(90)};
+  mixer[kColorRefNeutralVariant95] = {palette.neutral_variant().get(95)};
+  mixer[kColorRefNeutralVariant99] = {palette.neutral_variant().get(99)};
+  mixer[kColorRefNeutralVariant100] = {palette.neutral_variant().get(100)};
 }
 
 }  // namespace ui
