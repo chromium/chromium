4c642b52e4b671b4549f0b26712349018e805064
Tim Sergeant <tsergeant@chromium.org>
2023-01-10 23:42:05 +0000

    APS: Convert web app installation unit tests to browser tests
    Web app installation is moving to a new flow using a WebContents, so
    these test cases need to run in a full browser test environment.
    
    There is no change to the test content except for switching to an
    EmbeddedTestServer.
    
    Bug: 1402583, b/259151912
    Change-Id: If2f853710ab4dcbb1b18105b3b8e4c7b9ce2b032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150451
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091075}

---
+++ chrome/browser/apps/app_preload_service/web_app_preload_installer_browsertest.cc
@@ -0,0 +1,266 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/functional/bind.h"
+#include "base/strings/strcat.h"
+#include "base/test/test_future.h"
+#include "chrome/browser/apps/app_preload_service/preload_app_definition.h"
+#include "chrome/browser/apps/app_preload_service/proto/app_provisioning.pb.h"
+#include "chrome/browser/apps/app_preload_service/web_app_preload_installer.h"
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/web_app_helpers.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "components/services/app_service/public/cpp/app_registry_cache.h"
+#include "components/services/app_service/public/cpp/app_types.h"
+#include "components/services/app_service/public/cpp/app_update.h"
+#include "content/public/test/browser_test.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_request.h"
+#include "net/test/embedded_test_server/http_response.h"
+
+namespace apps {
+
+class WebAppPreloadInstallerBrowserTest : public InProcessBrowserTest {
+ public:
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+
+    https_server_.RegisterRequestHandler(
+        base::BindRepeating(&WebAppPreloadInstallerBrowserTest::HandleRequest,
+                            base::Unretained(this)));
+
+    ASSERT_TRUE(https_server()->Start());
+  }
+
+  std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
+      const net::test_server::HttpRequest& request) {
+    if (request.relative_url != "/manifest.json" || manifest_.empty()) {
+      return nullptr;
+    }
+
+    auto response = std::make_unique<net::test_server::BasicHttpResponse>();
+    response->set_code(net::HTTP_OK);
+    response->set_content_type("application/json");
+    response->set_content(manifest_);
+    return response;
+  }
+
+  void SetManifestResponse(std::string manifest) { manifest_ = manifest; }
+
+  Profile* profile() { return browser()->profile(); }
+
+  net::EmbeddedTestServer* https_server() { return &https_server_; }
+
+  AppRegistryCache& app_registry_cache() {
+    auto* proxy = AppServiceProxyFactory::GetForProfile(browser()->profile());
+    return proxy->AppRegistryCache();
+  }
+
+ private:
+  net::EmbeddedTestServer https_server_;
+  std::string manifest_;
+};
+
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest, InstallOemApp) {
+  WebAppPreloadInstaller installer(profile());
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("Example App");
+  app.set_package_id("web:https://www.example.com/index.html");
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_original_manifest_url(
+      "https://www.example.com/manifest.json");
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+
+  SetManifestResponse(R"({
+    "name": "Example App",
+    "start_url": "/index.html",
+    "scope": "/"
+  })");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_TRUE(result.Get());
+
+  auto app_id = web_app::GenerateAppId(
+      absl::nullopt, GURL("https://www.example.com/index.html"));
+  bool found =
+      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {
+        EXPECT_EQ(update.Name(), "Example App");
+        EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
+      });
+  ASSERT_TRUE(found);
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
+                       InstallWithManifestId) {
+  WebAppPreloadInstaller installer(profile());
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("Example App");
+  app.set_package_id("web:https://www.example.com/manifest_id");
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_original_manifest_url(
+      "https://www.example.com/manifest.json");
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+
+  SetManifestResponse(R"({
+    "id": "manifest_id",
+    "name": "Example App",
+    "start_url": "/index.html",
+    "scope": "/"
+  })");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_TRUE(result.Get());
+
+  // The generated app ID should take the manifest ID into account.
+  auto app_id = web_app::GenerateAppId(
+      "manifest_id", GURL("https://www.example.com/index.html"));
+  ASSERT_TRUE(
+      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {}));
+}
+
+// Reinstalling an existing user-installed app should not overwrite manifest
+// data, but will add the OEM install reason.
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest, InstallOverUserApp) {
+  constexpr char kStartUrl[] = "https://www.example.com/";
+  constexpr char kOriginalManifestUrl[] =
+      "https://www.example.com/manifest.json";
+  constexpr char kUserAppName[] = "User Installed App";
+
+  WebAppPreloadInstaller installer(profile());
+
+  auto app_id = web_app::test::InstallDummyWebApp(profile(), kUserAppName,
+                                                  GURL(kStartUrl));
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("OEM Installed app");
+  app.set_package_id(base::StrCat({"web:", kStartUrl}));
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+  web_extras->set_original_manifest_url(kOriginalManifestUrl);
+
+  SetManifestResponse(R"({
+    "name": "OEM Installed app",
+    "start_url": "/"
+  })");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_TRUE(result.Get());
+
+  bool found =
+      app_registry_cache().ForOneApp(app_id, [&](const AppUpdate& update) {
+        EXPECT_EQ(update.Name(), kUserAppName);
+        EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
+      });
+  ASSERT_TRUE(found);
+}
+
+// The manifest id in the proto does not match the calculated manifest id.
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
+                       InstallMismatchedDataManifestId) {
+  WebAppPreloadInstaller installer(profile());
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("Example App");
+  app.set_package_id("web:https://www.example.com/manifest_id");
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_original_manifest_url(
+      "https://www.example.com/manifest.json");
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+
+  SetManifestResponse(R"({
+    "name": "Example App",
+    "start_url": "/index.html",
+    "scope": "/"
+  })");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_FALSE(result.Get());
+
+  auto app_id = web_app::GenerateAppId(
+      absl::nullopt, GURL("https://www.example.com/index.html"));
+  bool found =
+      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
+  ASSERT_FALSE(found);
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
+                       ManifestFileIsNotJSON) {
+  WebAppPreloadInstaller installer(profile());
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("Example App");
+  app.set_package_id("web:https://www.example.com/manifest_id");
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_original_manifest_url(
+      "https://www.example.com/manifest.json");
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+
+  SetManifestResponse("INVALID");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_FALSE(result.Get());
+
+  auto app_id = web_app::GenerateAppId(
+      absl::nullopt, GURL("https://www.example.com/index.html"));
+  bool found =
+      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
+  ASSERT_FALSE(found);
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppPreloadInstallerBrowserTest,
+                       ManifestFileIsHasMissingFields) {
+  WebAppPreloadInstaller installer(profile());
+
+  proto::AppProvisioningListAppsResponse_App app;
+  app.set_name("Example App");
+  app.set_package_id("web:https://www.example.com/manifest_id");
+  app.set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+
+  auto* web_extras = app.mutable_web_extras();
+  web_extras->set_original_manifest_url(
+      "https://www.example.com/manifest.json");
+  web_extras->set_manifest_url(https_server()->GetURL("/manifest.json").spec());
+
+  SetManifestResponse(R"({
+    "is_valid": "no."
+  })");
+
+  base::test::TestFuture<bool> result;
+  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
+  ASSERT_FALSE(result.Get());
+
+  auto app_id = web_app::GenerateAppId(
+      absl::nullopt, GURL("https://www.example.com/index.html"));
+  bool found =
+      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
+  ASSERT_FALSE(found);
+}
+
+}  // namespace apps
--- chrome/browser/apps/app_preload_service/web_app_preload_installer_unittest.cc
+++ chrome/browser/apps/app_preload_service/web_app_preload_installer_unittest.cc
@@ -4,25 +4,14 @@
 
 #include "chrome/browser/apps/app_preload_service/web_app_preload_installer.h"
 
-#include "base/strings/strcat.h"
-#include "base/strings/utf_string_conversions.h"
-#include "base/test/test_future.h"
 #include "base/test/values_test_util.h"
 #include "chrome/browser/apps/app_preload_service/preload_app_definition.h"
 #include "chrome/browser/apps/app_preload_service/proto/app_provisioning.pb.h"
-#include "chrome/browser/apps/app_service/app_service_proxy.h"
-#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/test/base/testing_profile.h"
-#include "components/services/app_service/public/cpp/app_registry_cache.h"
-#include "components/services/app_service/public/cpp/app_types.h"
-#include "components/services/app_service/public/cpp/app_update.h"
 #include "content/public/test/browser_task_environment.h"
-#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
-#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
-#include "services/network/test/test_url_loader_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 
@@ -33,142 +22,17 @@ class WebAppPreloadInstallerTest : public testing::Test {
   void SetUp() override {
     testing::Test::SetUp();
 
-    TestingProfile::Builder profile_builder;
-    profile_builder.SetSharedURLLoaderFactory(
-        base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
-            &url_loader_factory_));
-    profile_ = profile_builder.Build();
+    profile_ = std::make_unique<TestingProfile>();
 
     web_app::test::AwaitStartWebAppProviderAndSubsystems(profile());
   }
 
   Profile* profile() { return profile_.get(); }
 
-  AppRegistryCache& app_registry_cache() {
-    auto* proxy = AppServiceProxyFactory::GetForProfile(profile());
-    return proxy->AppRegistryCache();
-  }
-
-  network::TestURLLoaderFactory url_loader_factory_;
-
  private:
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
-  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
 };
-
-TEST_F(WebAppPreloadInstallerTest, InstallOemApp) {
-  WebAppPreloadInstaller installer(profile());
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("Example App");
-  app.set_package_id("web:https://www.example.com/index.html");
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_original_manifest_url(
-      "https://www.example.com/manifest.json");
-  web_extras->set_manifest_url(
-      "https://meltingpot.googleusercontent.com/manifest.json");
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    "name": "Example App",
-    "start_url": "/index.html",
-    "scope": "/"
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_TRUE(result.Get());
-
-  auto app_id = web_app::GenerateAppId(
-      absl::nullopt, GURL("https://www.example.com/index.html"));
-  bool found =
-      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {
-        EXPECT_EQ(update.Name(), "Example App");
-        EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
-      });
-  ASSERT_TRUE(found);
-}
-
-TEST_F(WebAppPreloadInstallerTest, InstallWithManifestId) {
-  WebAppPreloadInstaller installer(profile());
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("Example App");
-  app.set_package_id("web:https://www.example.com/manifest_id");
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_original_manifest_url(
-      "https://www.example.com/manifest.json");
-  web_extras->set_manifest_url(
-      "https://meltingpot.googleusercontent.com/manifest.json");
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    "id": "manifest_id",
-    "name": "Example App",
-    "start_url": "/index.html",
-    "scope": "/"
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_TRUE(result.Get());
-
-  // The generated app ID should take the manifest ID into account.
-  auto app_id = web_app::GenerateAppId(
-      "manifest_id", GURL("https://www.example.com/index.html"));
-  ASSERT_TRUE(
-      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {}));
-}
-
-// Reinstalling an existing user-installed app should not overwrite manifest
-// data, but will add the OEM install reason.
-TEST_F(WebAppPreloadInstallerTest, InstallOverUserApp) {
-  constexpr char kStartUrl[] = "https://www.example.com/";
-  constexpr char kManifestUrl[] =
-      "https://meltingpot.googleusercontent.com/manifest.json";
-  constexpr char kOriginalManifestUrl[] =
-      "https://www.example.com/manifest.json";
-  constexpr char kUserAppName[] = "User Installed App";
-
-  WebAppPreloadInstaller installer(profile());
-
-  auto app_id = web_app::test::InstallDummyWebApp(profile(), kUserAppName,
-                                                  GURL(kStartUrl));
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("OEM Installed app");
-  app.set_package_id(base::StrCat({"web:", kStartUrl}));
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_manifest_url(kManifestUrl);
-  web_extras->set_original_manifest_url(kOriginalManifestUrl);
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    "name": "OEM Installed app",
-    "start_url": "/"
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_TRUE(result.Get());
-
-  bool found =
-      app_registry_cache().ForOneApp(app_id, [&](const AppUpdate& update) {
-        EXPECT_EQ(update.Name(), kUserAppName);
-        EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
-      });
-  ASSERT_TRUE(found);
-}
 
 TEST_F(WebAppPreloadInstallerTest, GetAppId) {
   WebAppPreloadInstaller installer(profile());
@@ -348,102 +212,6 @@ TEST_F(WebAppPreloadInstallerTest, ManifestToWebAppInstallInfoNoScope) {
 
   ASSERT_TRUE(install_info);
   EXPECT_EQ(install_info->scope, install_info->start_url);
-}
-
-// The manifest id in the proto does not match the calculated manifest id.
-TEST_F(WebAppPreloadInstallerTest, InstallMismatchedDataManifestId) {
-  WebAppPreloadInstaller installer(profile());
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("Example App");
-  app.set_package_id("web:https://www.example.com/manifest_id");
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_original_manifest_url(
-      "https://www.example.com/manifest.json");
-  web_extras->set_manifest_url(
-      "https://meltingpot.googleusercontent.com/manifest.json");
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    "name": "Example App",
-    "start_url": "/index.html",
-    "scope": "/"
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_FALSE(result.Get());
-
-  auto app_id = web_app::GenerateAppId(
-      absl::nullopt, GURL("https://www.example.com/index.html"));
-  bool found =
-      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
-  ASSERT_FALSE(found);
-}
-
-TEST_F(WebAppPreloadInstallerTest, ManifestFileIsNotJSON) {
-  WebAppPreloadInstaller installer(profile());
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("Example App");
-  app.set_package_id("web:https://www.example.com/manifest_id");
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_original_manifest_url(
-      "https://www.example.com/manifest.json");
-  web_extras->set_manifest_url(
-      "https://meltingpot.googleusercontent.com/manifest.json");
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    INVALID
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_FALSE(result.Get());
-
-  auto app_id = web_app::GenerateAppId(
-      absl::nullopt, GURL("https://www.example.com/index.html"));
-  bool found =
-      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
-  ASSERT_FALSE(found);
-}
-
-TEST_F(WebAppPreloadInstallerTest, ManifestFileIsHasMissingFields) {
-  WebAppPreloadInstaller installer(profile());
-
-  proto::AppProvisioningListAppsResponse_App app;
-  app.set_name("Example App");
-  app.set_package_id("web:https://www.example.com/manifest_id");
-  app.set_install_reason(
-      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
-
-  auto* web_extras = app.mutable_web_extras();
-  web_extras->set_original_manifest_url(
-      "https://www.example.com/manifest.json");
-  web_extras->set_manifest_url(
-      "https://meltingpot.googleusercontent.com/manifest.json");
-
-  url_loader_factory_.AddResponse(
-      "https://meltingpot.googleusercontent.com/manifest.json", R"({
-    "is_valid": "no."
-  })");
-
-  base::test::TestFuture<bool> result;
-  installer.InstallApp(PreloadAppDefinition(app), result.GetCallback());
-  ASSERT_FALSE(result.Get());
-
-  auto app_id = web_app::GenerateAppId(
-      absl::nullopt, GURL("https://www.example.com/index.html"));
-  bool found =
-      app_registry_cache().ForOneApp(app_id, [](const AppUpdate& update) {});
-  ASSERT_FALSE(found);
 }
 
 }  // namespace apps
