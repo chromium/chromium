2c12f88ea8c62c6d4537543139e8ec053deda6b3
Morten Stenshorne <mstensho@chromium.org>
2023-01-10 20:36:52 +0000

    Store more information on table break tokens.
    This way we avoid O(n^m) performance complexity [1] in subsequent
    fragments. When performing the initial table layout measure pass for
    non-first fragments, we are pretty much guaranteed to miss the measure
    cache, because of CL:3967751. This is something we cannot afford when
    nested tables are involved. The whole ComputeRows() design depends on
    cache hits, or we'd descend all the way to the innermost table for each
    table we attempt to lay out.
    
    [1] O(n^m): 'n' is the "amount of content" inside the tables, 'm' is the
    number of tables nested.
    
    This makes perf_tests/layout/multicol/deeply-nested-tables-2.html
    about 700 times faster.
    
    Bug: 1400918
    Change-Id: Ib7fafe2b13f6f7ba35f74b3f49937d4d621178ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4151250
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090986}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_break_token_data.h
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_break_token_data.h
@@ -6,18 +6,34 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_TABLE_NG_TABLE_BREAK_TOKEN_DATA_H_
 
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token_data.h"
+#include "third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm_types.h"
 
 namespace blink {
 
 struct NGTableBreakTokenData final : NGBlockBreakTokenData {
-  NGTableBreakTokenData(const NGBlockBreakTokenData* break_token_data,
-                        LayoutUnit consumed_table_box_block_size,
-                        bool has_entered_table_box,
-                        bool is_past_table_box)
+  NGTableBreakTokenData(
+      const NGBlockBreakTokenData* break_token_data,
+      const NGTableTypes::Rows& rows,
+      const NGTableTypes::CellBlockConstraints& cell_block_constraints,
+      const NGTableTypes::Sections& sections,
+      LayoutUnit total_table_min_block_size,
+      LayoutUnit consumed_table_box_block_size,
+      bool has_entered_table_box,
+      bool is_past_table_box)
       : NGBlockBreakTokenData(kTableBreakTokenData, break_token_data),
+        rows(rows),
+        cell_block_constraints(cell_block_constraints),
+        sections(sections),
+        total_table_min_block_size(total_table_min_block_size),
         consumed_table_box_block_size(consumed_table_box_block_size),
         has_entered_table_box(has_entered_table_box),
         is_past_table_box(is_past_table_box) {}
+  // Table layout information that will be the same for all fragments to be
+  // generated (but potentially very expensive to calculate).
+  NGTableTypes::Rows rows;
+  NGTableTypes::CellBlockConstraints cell_block_constraints;
+  NGTableTypes::Sections sections;
+  LayoutUnit total_table_min_block_size;
 
   // Similar to |consumed_block_size|, but it only counts space consumed by the
   // table box itself, i.e. excluding any captions.
