fb9173c6ef3444766ef3da7b0aa769229939b1d2
Joemer Ramos <joemerramos@chromium.org>
2023-01-10 20:35:32 +0000

    [iOS] Fix for UAF in BrowserObserverBridge::BrowserDestroyed
    The observer pattern in BrowserObserverBridge::BrowserDestroyed calls
    browser_observation_.Reset(); after the browser is destroyed. This
    causes a UAF in places like GridContextMenuHelper where the browser
    observer tries to use the reset function when the browser is already
    destroyed.
    
    (cherry picked from commit 52f2250a4c6dc8c1e9d4bcd07e82327ed60f41a4)
    
    Bug: 1405807
    Change-Id: I1bb93c6c96b77cfa7e2ffb1418d6489e68fb7940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4143891
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1090376}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4152773
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5481@{#206}
    Cr-Branched-From: 130f3e4d850f4bc7387cfb8d08aa993d288a67a9-refs/heads/main@{#1084008}

--- ios/chrome/browser/main/browser_observer_bridge.mm
+++ ios/chrome/browser/main/browser_observer_bridge.mm
@@ -19,6 +19,10 @@ BrowserObserverBridge::BrowserObserverBridge(Browser* browser,
 BrowserObserverBridge::~BrowserObserverBridge() {}
 
 void BrowserObserverBridge::BrowserDestroyed(Browser* browser) {
-  [observer_ browserDestroyed:browser];
+  DCHECK(browser_observation_.IsObservingSource(browser));
   browser_observation_.Reset();
+
+  // The following invocation may lead to the destruction
+  // of `this`. No code should follow.
+  [observer_ browserDestroyed:browser];
 }
