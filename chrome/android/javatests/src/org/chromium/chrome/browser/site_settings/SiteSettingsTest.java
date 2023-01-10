2d988a9335196409f94cb0f22d4c3508cbc46f2f
Boris Sazonov <bsazonov@chromium.org>
2023-01-10 11:29:46 +0000

    Reland "Sheriff: Disable flaky SiteSettingsTest case"
    This reverts commit 376880fd70fa581ba4f0abf3a8bf458b6b1967b2.
    
    Reason for revert: still flaky (see https://crbug.com/1395173#c8).
    
    Original change's description:
    > Revert "Sheriff: Disable flaky SiteSettingsTest case"
    >
    > This reverts commit 116b4d72e0e640c8cca51cf7ffe9991fa70a945d.
    >
    > Reason for revert: crbug.com/1404183 Test reviver says the test is running fine. Let's give it a try
    >
    > Original change's description:
    > > Sheriff: Disable flaky SiteSettingsTest case
    > >
    > > This disables SiteSettingsTest.testSiteExceptionSiteDataBlocked since
    > > this appears to be flaky on several bots.
    > >
    > > Bug: 1395173
    > > Test: None
    > > Change-Id: Ia2583049ad8caad3129e48f5d085a6f1fada1ca0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4099037
    > > Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    > > Owners-Override: Nate Fischer <ntfschr@chromium.org>
    > > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > > Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1082557}
    >
    > Bug: 1395173, 1404183
    > Change-Id: I33a5edb676d4ff7bdb043fc5755a1d0dec52963c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4129555
    > Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1087764}
    
    Bug: 1395173, 1404183
    Change-Id: Iefefa258d4b32bf9bdaca160bfca7ecc2d1b3ae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4151548
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1090766}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -690,6 +690,7 @@ public class SiteSettingsTest {
     @SmallTest
     @Feature({"Preferences"})
     @EnableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS_4)
+    @DisabledTest(message = "https://crbug.com/1395173")
     public void testSiteExceptionSiteDataBlocked() throws Exception {
         setGlobalToggleForCategory(SiteSettingsCategory.Type.SITE_DATA, true);
 
