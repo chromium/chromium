c323d4f3bbbe972b3d7e7203f720f07f88deeb37
Avi Drissman <avi@chromium.org>
2023-01-11 16:12:43 +0000

    Update backup exclusion to use more recent API
    Backup exclusion can be done by path or as an attribute of an existing
    file. Exclusion by path requires root permission, so Chromium never
    does that.
    
    What Chromium does is exclusion as an attribute of an existing file,
    which (verified back to 10.13) works by attaching an xattr of the name
    `com.apple.metadata:com_apple_backup_excludeItem` to the file. This
    is done by the CoreServices functions `CSBackupSetItemExcluded()` and
    `CSBackupIsItemExcluded()`, introduced in Mac OS X 10.5.
    
    However, Mac OS X 10.6 / iOS 4 introduced a new system called
    "resource properties" for getting/setting properties of filesystem
    URLs. And in OS X 10.8 / iOS 5.1 the resource property key
    `NSURLIsExcludedFromBackupKey` was introduced to API, to expose the
    backup exclusion property as a resource property. And indeed, on the
    Mac, it uses the same backing xattr as the Core Services API uses, and
    is therefore interoperable with it.
    
    Given all this, it makes sense to switch the Mac code to use the same,
    more modern, APIs as the iOS code uses. As this is the same API used
    to access properties in general, it is more well-known by developers
    and is more future-proof.
    
    Minor code cleanup is done while we're here.
    
    Bug: none
    Change-Id: I0cf54b365d46298ec941ab35b5aaf17347a9955e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4153173
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091352}

--- base/mac/backup_util.mm
+++ base/mac/backup_util.mm
@@ -4,16 +4,13 @@
 
 #include "base/mac/backup_util.h"
 
-#include <CoreServices/CoreServices.h>
 #import <Foundation/Foundation.h>
 
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
-#include "base/mac/mac_logging.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
-#include "build/build_config.h"
 
 namespace base::mac {
 
@@ -21,11 +18,8 @@ bool GetBackupExclusion(const FilePath& file_path) {
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
 
-#if BUILDFLAG(IS_MAC)
-  return CSBackupIsItemExcluded(FilePathToCFURL(file_path), nullptr);
-#elif BUILDFLAG(IS_IOS)
   NSURL* file_url = FilePathToNSURL(file_path);
-  DCHECK([[NSFileManager defaultManager] fileExistsAtPath:file_url.path]);
+  DCHECK([file_url checkPromisedItemIsReachableAndReturnError:nil]);
 
   NSError* error = nil;
   NSNumber* value = nil;
@@ -33,12 +27,11 @@ bool GetBackupExclusion(const FilePath& file_path) {
                                      forKey:NSURLIsExcludedFromBackupKey
                                       error:&error];
   if (!success) {
-    LOG(ERROR) << base::SysNSStringToUTF8([error description]);
+    LOG(ERROR) << base::SysNSStringToUTF8(error.description);
     return false;
   }
 
   return value && value.boolValue;
-#endif
 }
 
 namespace {
@@ -47,31 +40,15 @@ bool SetBackupState(const FilePath& file_path, bool excluded) {
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
 
-#if BUILDFLAG(IS_MAC)
-  // When excludeByPath is true the application must be running with root
-  // privileges (admin for 10.6 and earlier) but the URL does not have to
-  // already exist. When excludeByPath is false the URL must already exist but
-  // can be used in non-root (or admin as above) mode. We use false so that
-  // non-root (or admin) users don't get their TimeMachine drive filled up with
-  // unnecessary backups.
-  OSStatus os_err =
-      CSBackupSetItemExcluded(FilePathToCFURL(file_path), excluded,
-                              /*excludeByPath=*/FALSE);
-  OSSTATUS_DLOG_IF(WARNING, os_err != noErr, os_err)
-      << "Failed to set backup exclusion for file '"
-      << file_path.value().c_str() << "'";
-  return os_err == noErr;
-#elif BUILDFLAG(IS_IOS)
   NSURL* file_url = FilePathToNSURL(file_path);
-  DCHECK([[NSFileManager defaultManager] fileExistsAtPath:file_url.path]);
+  DCHECK([file_url checkPromisedItemIsReachableAndReturnError:nil]);
 
   NSError* error = nil;
   BOOL success = [file_url setResourceValue:@(excluded)
                                      forKey:NSURLIsExcludedFromBackupKey
                                       error:&error];
-  LOG_IF(WARNING, !success) << base::SysNSStringToUTF8([error description]);
+  LOG_IF(WARNING, !success) << base::SysNSStringToUTF8(error.description);
   return success;
-#endif
 }
 
 }  // namespace
