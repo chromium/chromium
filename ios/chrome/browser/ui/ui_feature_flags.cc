e8ef469064cdcf526f642a79a39a296057ad935c
Quentin Pubert <qpubert@google.com>
2023-01-11 11:31:43 +0000

    [iOS] Enable 3P intents in Incognito by default
    This CL sets the `kIOS3PIntentsInIncognito` feature enabled by default.
    Also, the field trial testing config entry of the feature is removed, as
    per Finch best practices.
    
    Fixed: 1406467
    Change-Id: Ic61bbaff62eb922b10d511baa5d09b38a58106e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154711
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1091261}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -41,7 +41,7 @@ BASE_FEATURE(kIncognitoNtpRevamp,
 
 BASE_FEATURE(kIOS3PIntentsInIncognito,
              "IOS3pIntentsInIncognito",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDefaultBrowserFullscreenPromoExperiment,
              "DefaultBrowserFullscreenPromoExperiment",
