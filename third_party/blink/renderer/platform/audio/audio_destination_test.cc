8f62b59159b0e03ec72920f3412912f58c2e298e
Michael Wilson <mjwilson@chromium.org>
2023-01-11 03:42:14 +0000

    Start modernizing audio_destination_test.cc
    As part of an effort to improve WebAudio unit testing, this CL does
    the following:
    - Use MOCK_METHOD on MockWebAudioDevice methods
    - Maintain a reference to the MockWebAudioDevice so EXPECT calls can
      be made on it
    - Split the single large resampling unit test into a parameterized
      unit test
    - Add sequenced EXPECT_CALL statements on the MockWebAudioDevice
    
    Change-Id: Ib06c089ca8b50039ad80b1ba1e674d9620a0190b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113003
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Dmitrii Kuragin <kuragin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091166}

--- third_party/blink/renderer/platform/audio/audio_destination_test.cc
+++ third_party/blink/renderer/platform/audio/audio_destination_test.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "media/base/audio_glitch_info.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/web_audio_device.h"
 #include "third_party/blink/public/platform/web_audio_latency_hint.h"
@@ -20,17 +21,20 @@ namespace blink {
 
 namespace {
 
-const blink::LocalFrameToken kFrameToken;
+using ::testing::_;
+using ::testing::InSequence;
+
+const LocalFrameToken kFrameToken;
 
 class MockWebAudioDevice : public WebAudioDevice {
  public:
   explicit MockWebAudioDevice(double sample_rate, int frames_per_buffer)
       : sample_rate_(sample_rate), frames_per_buffer_(frames_per_buffer) {}
 
-  void Start() override {}
-  void Stop() override {}
-  void Pause() override {}
-  void Resume() override {}
+  MOCK_METHOD(void, Start, (), (override));
+  MOCK_METHOD(void, Stop, (), (override));
+  MOCK_METHOD(void, Pause, (), (override));
+  MOCK_METHOD(void, Resume, (), (override));
   double SampleRate() override { return sample_rate_; }
   int FramesPerBuffer() override { return frames_per_buffer_; }
 
@@ -41,21 +45,38 @@ class MockWebAudioDevice : public WebAudioDevice {
 
 class TestPlatform : public TestingPlatformSupport {
  public:
+  TestPlatform() {
+    webaudio_device_ = std::make_unique<MockWebAudioDevice>(
+        AudioHardwareSampleRate(), AudioHardwareBufferSize());
+  }
+
   std::unique_ptr<WebAudioDevice> CreateAudioDevice(
       const WebAudioSinkDescriptor& sink_descriptor,
       unsigned number_of_output_channels,
       const WebAudioLatencyHint& latency_hint,
       media::AudioRendererSink::RenderCallback*) override {
-    return std::make_unique<MockWebAudioDevice>(AudioHardwareSampleRate(),
-                                                AudioHardwareBufferSize());
+    CHECK(webaudio_device_ != nullptr)
+        << "Calling CreateAudioDevice (via AudioDestination::Create) multiple "
+           "times in one test is not supported.";
+    return std::move(webaudio_device_);
   }
 
   double AudioHardwareSampleRate() override { return 44100; }
   size_t AudioHardwareBufferSize() override { return 512; }
   unsigned AudioHardwareOutputChannels() override { return 2; }
+
+  const MockWebAudioDevice& web_audio_device() {
+    CHECK(webaudio_device_ != nullptr)
+        << "Finish setting up expectations before calling CreateAudioDevice "
+           "(via AudioDestination::Create).";
+    return *webaudio_device_;
+  }
+
+ private:
+  std::unique_ptr<MockWebAudioDevice> webaudio_device_;
 };
 
-class AudioCallback : public blink::AudioIOCallback {
+class AudioCallback : public AudioIOCallback {
  public:
   void Render(AudioBus*,
               uint32_t frames_to_process,
@@ -68,47 +89,65 @@ class AudioCallback : public blink::AudioIOCallback {
   int frames_processed_ = 0;
 };
 
-void CountWASamplesProcessedForRate(absl::optional<float> sample_rate) {
-  WebAudioLatencyHint latency_hint(WebAudioLatencyHint::kCategoryInteractive);
-  AudioCallback callback;
+class AudioDestinationTest
+    : public ::testing::TestWithParam<absl::optional<float>> {
+ public:
+  void CountWASamplesProcessedForRate(absl::optional<float> sample_rate) {
+    WebAudioLatencyHint latency_hint(WebAudioLatencyHint::kCategoryInteractive);
+    AudioCallback callback;
 
-  const int channel_count = Platform::Current()->AudioHardwareOutputChannels();
-  const size_t request_frames = Platform::Current()->AudioHardwareBufferSize();
+    const int channel_count =
+        Platform::Current()->AudioHardwareOutputChannels();
+    const size_t request_frames =
+        Platform::Current()->AudioHardwareBufferSize();
 
-  // Assume the default audio device. (i.e. the empty string)
-  WebAudioSinkDescriptor sink_descriptor(WebString::FromUTF8(""), kFrameToken);
+    // Assume the default audio device. (i.e. the empty string)
+    WebAudioSinkDescriptor sink_descriptor(WebString::FromUTF8(""),
+                                           kFrameToken);
 
-  // TODO(https://crbug.com/988121) Replace 128 with the appropriate
-  // AudioContextRenderSizeHintCategory.
-  scoped_refptr<AudioDestination> destination = AudioDestination::Create(
-      callback, sink_descriptor, channel_count, latency_hint, sample_rate, 128);
-  destination->Start();
+    // TODO(https://crbug.com/988121) Replace 128 with the appropriate
+    // AudioContextRenderSizeHintCategory.
+    scoped_refptr<AudioDestination> destination =
+        AudioDestination::Create(callback, sink_descriptor, channel_count,
+                                 latency_hint, sample_rate, 128);
+    destination->Start();
 
-  destination->Render(
-      base::TimeDelta::Min(), base::TimeTicks::Now(), {},
-      media::AudioBus::Create(channel_count, request_frames).get());
+    destination->Render(
+        base::TimeDelta::Min(), base::TimeTicks::Now(), {},
+        media::AudioBus::Create(channel_count, request_frames).get());
 
-  int exact_frames_required =
-      std::ceil(request_frames * destination->SampleRate() /
-                Platform::Current()->AudioHardwareSampleRate());
-  int expected_frames_processed =
-      std::ceil(exact_frames_required /
-                static_cast<double>(destination->RenderQuantumFrames())) *
-      destination->RenderQuantumFrames();
+    int exact_frames_required =
+        std::ceil(request_frames * destination->SampleRate() /
+                  Platform::Current()->AudioHardwareSampleRate());
+    int expected_frames_processed =
+        std::ceil(exact_frames_required /
+                  static_cast<double>(destination->RenderQuantumFrames())) *
+        destination->RenderQuantumFrames();
 
-  EXPECT_EQ(expected_frames_processed, callback.frames_processed_);
-}
+    EXPECT_EQ(expected_frames_processed, callback.frames_processed_);
+  }
+};
 
-TEST(AudioDestinationTest, ResamplingTest) {
+TEST_P(AudioDestinationTest, ResamplingTest) {
   ScopedTestingPlatformSupport<TestPlatform> platform;
+  {
+    InSequence s;
 
-  CountWASamplesProcessedForRate(absl::optional<float>());
-  CountWASamplesProcessedForRate(8000);
-  CountWASamplesProcessedForRate(24000);
-  CountWASamplesProcessedForRate(44100);
-  CountWASamplesProcessedForRate(48000);
-  CountWASamplesProcessedForRate(384000);
+    EXPECT_CALL(platform->web_audio_device(), Start).Times(1);
+    EXPECT_CALL(platform->web_audio_device(), Stop).Times(1);
+  }
+
+  CountWASamplesProcessedForRate(GetParam());
 }
+
+INSTANTIATE_TEST_SUITE_P(/* no label */,
+                         AudioDestinationTest,
+                         ::testing::Values(absl::optional<float>(),
+                                           8000,
+                                           24000,
+                                           44100,
+                                           48000,
+                                           384000));
 
 }  // namespace
 
