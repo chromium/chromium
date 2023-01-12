1fdfe82d88d6d75ac43a6c6eaeb5898667b5973a
Dana Fried <dfried@chromium.org>
2023-01-12 20:24:37 +0000

    [Test Automation] Re-enabled potentially deflaked test.
    This test had previously been flaky on several platforms, and was
    disabled on LaCrOS. It was rewritten in Kombucha and that fixed all of
    the enabled platforms, but we should try to re-enable for LaCrOS as
    well.
    
    Bug: 1382582
    Change-Id: I3e307350067f2b90e29407823ed787983ea49565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4144265
    Reviewed-by: Lindsay Pasricha <lindsayw@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092017}

--- chrome/browser/ui/views/side_search/unified_side_search_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/side_search/unified_side_search_controller_interactive_uitest.cc
@@ -891,6 +891,11 @@ class SideSearchAutoTriggeringBrowserTest
     });
   }
 
+  void SetUp() override {
+    set_open_about_blank_on_browser_launch(true);
+    SideSearchFeatureEngagementTest::SetUp();
+  }
+
   void SetUpOnMainThread() override {
     SideSearchFeatureEngagementTest::SetUpOnMainThread();
     private_test_impl().DoTestSetUp();
@@ -926,17 +931,8 @@ class SideSearchAutoTriggeringBrowserTest
   feature_engagement::test::ScopedIphFeatureList feature_list_;
 };
 
-// TODO(crbug.com/1368921): Flaky on lacros.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#define MAYBE_SidePanelAutoTriggersAfterReturningToAPreviousSRP \
-  DISABLED_SidePanelAutoTriggersAfterReturningToAPreviousSRP
-#else
-#define MAYBE_SidePanelAutoTriggersAfterReturningToAPreviousSRP \
-  SidePanelAutoTriggersAfterReturningToAPreviousSRP
-#endif
-IN_PROC_BROWSER_TEST_F(
-    SideSearchAutoTriggeringBrowserTest,
-    MAYBE_SidePanelAutoTriggersAfterReturningToAPreviousSRP) {
+IN_PROC_BROWSER_TEST_F(SideSearchAutoTriggeringBrowserTest,
+                       SidePanelAutoTriggersAfterReturningToAPreviousSRP) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kPrimaryTabId);
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSidePanelWebContentsId);
 
