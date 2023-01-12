3ca99106e2bf3fcb0452d0a9851b8267f7ae012a
Dana Fried <dfried@chromium.org>
2023-01-12 18:28:24 +0000

    [Test Automation/User Education] Instrument Mac RenderView context menus
    This CL fixes two issues with running and testing render view context
    menus on Mac (these are the default context menus when right-clicking on
    a web page).
    
    First: while other context menus (tabs, bookmarks, etc.) had been
    instrumented for the following:
     - Display the "New" badge
     - Highlight during Tutorial/IPH
     - Attach help bubbles during Tutorials
     - Find and select menu items during Kombucha tests
    
    The render view context menu, which did not use a cocoa menu delegate,
    did not include these improvements.
    
    This CL takes the delegate object which provides all of this
    functionality and promotes it to its own file with a public API. It
    then adds it to the render view context menu, which immediately
    enables all of the missing functionality.
    
    This was added because it was one of the reasons a test we were trying
    to add was failing; that test (along with related tests which are
    unrelated to this bug) will be added in a downstream CL.
    
    Change-Id: I768ea0b8246b26923261b9d0981ef42dae4321f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159894
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091941}

---
+++ ui/views/controls/menu/menu_controller_cocoa_delegate_impl.h
@@ -0,0 +1,19 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
+#define UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
+
+#import "base/mac/scoped_nsobject.h"
+#import "ui/base/cocoa/menu_controller.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/views/views_export.h"
+
+VIEWS_EXPORT
+@interface MenuControllerCocoaDelegateImpl
+    : NSObject <MenuControllerCocoaDelegate>
+- (void)setAnchorRect:(gfx::Rect)rect;
+@end
+
+#endif  // UI_VIEWS_CONTROLS_MENU_MENU_CONTROLLER_COCOA_DELEGATE_IMPL_H_
