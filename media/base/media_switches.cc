e788b9d5ccbd951a6945dd6dfa41178350c2fcc0
Olga Sharonova <olka@chromium.org>
2023-01-12 09:25:22 +0000

    Enable Chrome-Wide echo cancellation by default on Mac
    Already enabled on Win and Linux.
    
    Bug: 1215049
    Change-Id: Ifdf3a399ed4cfec86700fb509383ac98e2716c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4158131
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091738}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -407,23 +407,15 @@ BASE_FEATURE(kCdmProcessSiteIsolation,
 // playback going to a specific output device in the audio service.
 BASE_FEATURE(kChromeWideEchoCancellation,
              "ChromeWideEchoCancellation",
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
              base::FEATURE_ENABLED_BY_DEFAULT);
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
 
 // If non-zero, audio processing is done on a dedicated processing thread which
 // receives audio from the audio capture thread via a fifo of a specified size.
 // Zero fifo size means the usage of such processing thread is disabled and
 // processing is done on the audio capture thread itself.
 const base::FeatureParam<int> kChromeWideEchoCancellationProcessingFifoSize{
-  &kChromeWideEchoCancellation, "processing_fifo_size",
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
-      110  // Default value for the enabled feature.
-#else
-      0
-#endif
+    &kChromeWideEchoCancellation, "processing_fifo_size",
+    110  // Default value for the enabled feature.
 };
 
 // When audio processing is done in the audio process, at the renderer side IPC
