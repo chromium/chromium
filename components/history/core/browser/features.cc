85aac5ea5acf0bb2a6574d8b48851df854c56b02
Ender <ender@google.com>
2023-01-12 18:20:41 +0000

    Enable Organic Queries on Android by default.
    Bug: 1317829
    Change-Id: I4f765d05817587e7a4e44212aca0c668a802e55d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159033
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091938}

--- components/history/core/browser/features.cc
+++ components/history/core/browser/features.cc
@@ -4,30 +4,45 @@
 
 #include "components/history/core/browser/features.h"
 
+#include "build/build_config.h"
 #include "components/history/core/browser/top_sites_impl.h"
 
 namespace history {
+namespace {
+constexpr auto kOrganicRepeatableQueriesDefaultValue =
+    BUILDFLAG(IS_ANDROID) ? base::FEATURE_ENABLED_BY_DEFAULT
+                          : base::FEATURE_DISABLED_BY_DEFAULT;
+
+// Specifies the scaling behavior, i.e. whether the relevance scales of the
+// top sites and repeatable queries should be first aligned.
+// The default behavior is to mix the two lists as is.
+constexpr bool kScaleRepeatableQueriesScoresDefaultValue =
+    BUILDFLAG(IS_ANDROID) ? true : false;
+
+// Defines the maximum number of repeatable queries that can be shown.
+// The default behavior is having no limit, i.e., the number of the tiles.
+constexpr int kMaxNumRepeatableQueriesDefaultValue =
+    BUILDFLAG(IS_ANDROID) ? 4 : kTopSitesNumber;
+}  // namespace
 
 // If enabled, the most repeated queries from the user browsing history are
 // shown in the Most Visited tiles.
 BASE_FEATURE(kOrganicRepeatableQueries,
              "OrganicRepeatableQueries",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             kOrganicRepeatableQueriesDefaultValue);
 
 // The maximum number of repeatable queries to show in the Most Visited tiles.
-// The default behavior is having no limit, i.e., the number of the tiles.
 const base::FeatureParam<int> kMaxNumRepeatableQueries(
     &kOrganicRepeatableQueries,
     "MaxNumRepeatableQueries",
-    kTopSitesNumber);
+    kMaxNumRepeatableQueriesDefaultValue);
 
 // Whether the scores for the repeatable queries and the most visited sites
 // should first be scaled to an equivalent range before mixing.
-// The default behavior is to mix the two lists as is.
 const base::FeatureParam<bool> kScaleRepeatableQueriesScores(
     &kOrganicRepeatableQueries,
     "ScaleRepeatableQueriesScores",
-    false);
+    kScaleRepeatableQueriesScoresDefaultValue);
 
 // Whether a repeatable query should precede a most visited site with equal
 // score. The default behavior is for the sites to precede the queries.
