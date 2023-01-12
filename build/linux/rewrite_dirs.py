3dab32e0864993aa21861819240ccaa00bebdd23
Takuto Ikuta <tikuta@chromium.org>
2023-01-12 18:52:00 +0000

    build: update shebang to python3
    This assumes that CQ has enough coverage for these scripts if they are
    important.
    
    Derived from https://crrev.com/c/4078643/4/build/android/pylib/symbols/mock_addr2line/mock_addr2line#1
    
    This is made by
    $ rg -l '#!.*python$' build | \
      xargs sed -i 's/\(#!.*\)python$/\1python3/'
    
    Bug: 1406153
    Change-Id: I3df47e000c690ce8af341ff317539c4faa58e3bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159758
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1091953}

--- build/linux/rewrite_dirs.py
+++ build/linux/rewrite_dirs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2011 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
