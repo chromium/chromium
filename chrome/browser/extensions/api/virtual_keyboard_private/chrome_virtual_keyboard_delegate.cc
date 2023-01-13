6ccb388d17cd0cdc403d218f7164b989f8032527
Kush Sinha <sinhak@chromium.org>
2023-01-13 03:07:48 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /chrome/browser/extensions/api/virtual_keyboard_private
    
    This CL was uploaded by git cl split.
    
    R=yhanada@chromium.org
    
    Bug: 1406815
    Change-Id: I0c31ac2281075d6c690286c4506c398a60967d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4161724
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092221}

--- chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
+++ chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
@@ -20,6 +20,7 @@
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
+#include "base/values.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "chrome/browser/ash/login/ui/user_adding_screen.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -499,14 +500,15 @@ void ChromeVirtualKeyboardDelegate::OnGetHistoryValuesAfterItemsUpdated(
   // Broadcast an api event for each updated item.
   for (auto& item : updated_items.GetList()) {
     keyboard_api::ClipboardItem clipboard_item;
-    if (item.FindKey("imageData")) {
-      clipboard_item.image_data = item.FindKey("imageData")->GetString();
+    const base::Value::Dict& dict = item.GetDict();
+    if (dict.FindString("imageData")) {
+      clipboard_item.image_data = *dict.FindString("imageData");
     }
-    if (item.FindKey("textData")) {
-      clipboard_item.text_data = item.FindKey("textData")->GetString();
+    if (dict.FindString("textData")) {
+      clipboard_item.text_data = *dict.FindString("textData");
     }
-    if (item.FindKey("idToken")) {
-      clipboard_item.id = item.FindKey("idToken")->GetString();
+    if (dict.FindString("idToken")) {
+      clipboard_item.id = *dict.FindString("textData");
     }
 
     auto item_value =
