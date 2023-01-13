2a2ca7fb813e3e295b89b8e4436377778b46e90d
Arthur Sonzogni <arthursonzogni@chromium.org>
2023-01-13 13:41:36 +0000

    Avoid dangling pointer in PrimaryAccountPolicyManager
    This is intended to be merged to M110, and maybe M109.
    
    This patch is a no-op. There was a "safe" dangling pointer. Safe,
    because its value was compared against a non dangling pointer
    value before being used.
    
    We still want to detect and ban dangling pointers in invoked callback.
    See:
    https://docs.google.com/document/d/164SnySRX6MZgFkX8qLLLClTnZ0S8s5wLiNFWNyQ8f4E/edit
    
    This patch passes it as a WeakPtr. This is slightly better, because it
    could avoid the very unlikely event of Browser being deleted and new
    one being allocated at the very same address, caused the function
    to proceed wrongfully.
    
    Bug: 1406925
    Change-Id: I327f524e2c98c68572a151cd269935742f77024b
    Fixed: 1406925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165081
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1092368}

--- chrome/browser/signin/primary_account_policy_manager.cc
+++ chrome/browser/signin/primary_account_policy_manager.cc
@@ -97,7 +97,7 @@ class PrimaryAccountPolicyManager::DeleteProfileDialogManager
     base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE,
         base::BindOnce(&DeleteProfileDialogManager::ShowDeleteProfileDialog,
-                       weak_factory_.GetWeakPtr(), browser));
+                       weak_factory_.GetWeakPtr(), browser->AsWeakPtr()));
   }
 
   // Called immediately after a browser becomes not active.
@@ -112,21 +112,23 @@ class PrimaryAccountPolicyManager::DeleteProfileDialogManager
   }
 
  private:
-  void ShowDeleteProfileDialog(Browser* browser) {
+  void ShowDeleteProfileDialog(base::WeakPtr<Browser> active_browser) {
     // Block opening dialog from nested task.
     static bool is_dialog_shown = false;
     if (is_dialog_shown)
       return;
     base::AutoReset<bool> auto_reset(&is_dialog_shown, true);
 
-    // Check that |browser| is still active.
-    if (!active_browser_ || active_browser_ != browser)
+    // Check the |active_browser_| hasn't changed while waiting for the task to
+    // be executed.
+    if (!active_browser_ || active_browser_ != active_browser.get()) {
       return;
+    }
 
     // Show the dialog.
-    DCHECK(browser->window()->GetNativeWindow());
+    DCHECK(active_browser_->window()->GetNativeWindow());
     chrome::MessageBoxResult result = chrome::ShowWarningMessageBox(
-        browser->window()->GetNativeWindow(),
+        active_browser_->window()->GetNativeWindow(),
         l10n_util::GetStringUTF16(IDS_PROFILE_WILL_BE_DELETED_DIALOG_TITLE),
         l10n_util::GetStringFUTF16(
             IDS_PROFILE_WILL_BE_DELETED_DIALOG_DESCRIPTION,
@@ -138,12 +140,13 @@ class PrimaryAccountPolicyManager::DeleteProfileDialogManager
       case chrome::MessageBoxResult::MESSAGE_BOX_RESULT_NO: {
         // If the warning dialog is automatically dismissed or the user closed
         // the dialog by clicking on the close "X" button, then re-present the
-        // dialog (the user should not be able to interact with the browser
-        // window as the profile must be deleted).
+        // dialog (the user should not be able to interact with the
+        // `active_browser_` window as the profile must be deleted).
         base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
             FROM_HERE,
             base::BindOnce(&DeleteProfileDialogManager::ShowDeleteProfileDialog,
-                           weak_factory_.GetWeakPtr(), browser));
+                           weak_factory_.GetWeakPtr(),
+                           active_browser_->AsWeakPtr()));
         break;
       }
       case chrome::MessageBoxResult::MESSAGE_BOX_RESULT_YES:
