c0eeb7eac05aff9b4adc209d7150a08a87e9fab3
Victor Vianna <victorvianna@google.com>
2023-01-13 16:27:28 +0000

    Revert "simplify clobber.py with gn clean command"
    This reverts commit ca9403a4e70cf450e69e97f5211859117db396a3.
    
    Reason for revert: Folks can't run gclient sync on ToT, crbug.com/1406628#c36
    
    Original change's description:
    > simplify clobber.py with gn clean command
    >
    > The previous implementation of clobber.py doesn't work well with symlinked or mounted build dirs.
    > This CL refactors the script to use `gn clean` command to work on those build dirs.
    >
    > Bug: 1406628
    > Fixed: 1407028
    > Change-Id: I17c61b7e67075cb1ccef79692632564c667566ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4164159
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Commit-Queue: Junji Watanabe <jwata@google.com>
    > Auto-Submit: Junji Watanabe <jwata@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1092301}
    
    Bug: 1406628
    Change-Id: I0e41d9bf6283e9afdc5623af3ca76f94fb0ae1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4164783
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Quick-Run: Victor Vianna <victorvianna@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1092438}

--- build/clobber_unittest.py
+++ build/clobber_unittest.py
@@ -7,33 +7,125 @@ import os
 import pathlib
 import shutil
 import tempfile
+import textwrap
 import unittest
+from unittest import mock
 
 import clobber
 
 
-class TestClean(unittest.TestCase):
+class TestExtractBuildCommand(unittest.TestCase):
+  def setUp(self):
+    _, self.build_ninja_path = tempfile.mkstemp(text=True)
+
+  def tearDown(self):
+    os.remove(self.build_ninja_path)
+
+  def test_normal_extraction(self):
+    build_ninja_file_contents = textwrap.dedent("""
+        ninja_required_version = 1.7.2
+
+        rule gn
+          command = ../../buildtools/gn --root=../.. -q --regeneration gen .
+          pool = console
+          description = Regenerating ninja files
+
+        build build.ninja.stamp: gn
+          generator = 1
+          depfile = build.ninja.d
+
+        build build.ninja: phony build.ninja.stamp
+          generator = 1
+
+        pool build_toolchain_action_pool
+          depth = 72
+
+        pool build_toolchain_link_pool
+          depth = 23
+
+        subninja toolchain.ninja
+        subninja clang_newlib_x64/toolchain.ninja
+        subninja glibc_x64/toolchain.ninja
+        subninja irt_x64/toolchain.ninja
+        subninja nacl_bootstrap_x64/toolchain.ninja
+        subninja newlib_pnacl/toolchain.ninja
+
+        build blink_python_tests: phony obj/blink_python_tests.stamp
+        build blink_tests: phony obj/blink_tests.stamp
+
+        default all
+    """)  # Based off of a standard linux build dir.
+    with open(self.build_ninja_path, 'w') as f:
+      f.write(build_ninja_file_contents)
+
+    expected_build_ninja_file_contents = textwrap.dedent("""
+        ninja_required_version = 1.7.2
+
+        rule gn
+          command = ../../buildtools/gn --root=../.. -q --regeneration gen .
+          pool = console
+          description = Regenerating ninja files
+
+        build build.ninja.stamp: gn
+          generator = 1
+          depfile = build.ninja.d
+
+        build build.ninja: phony build.ninja.stamp
+          generator = 1
+
+    """)
+
+    self.assertEqual(clobber.extract_gn_build_commands(self.build_ninja_path),
+                     expected_build_ninja_file_contents)
+
+  def test_unexpected_format(self):
+    # No "build build.ninja:" line should make it return an empty string.
+    build_ninja_file_contents = textwrap.dedent("""
+        ninja_required_version = 1.7.2
+
+        rule gn
+          command = ../../buildtools/gn --root=../.. -q --regeneration gen .
+          pool = console
+          description = Regenerating ninja files
+
+        subninja toolchain.ninja
+
+        build blink_python_tests: phony obj/blink_python_tests.stamp
+        build blink_tests: phony obj/blink_tests.stamp
+
+    """)
+    with open(self.build_ninja_path, 'w') as f:
+      f.write(build_ninja_file_contents)
+
+    self.assertEqual(clobber.extract_gn_build_commands(self.build_ninja_path),
+                     '')
+
+
+class TestDelete(unittest.TestCase):
   def setUp(self):
     self.build_dir = tempfile.mkdtemp()
+
+    pathlib.Path(os.path.join(self.build_dir, 'build.ninja')).touch()
+    pathlib.Path(os.path.join(self.build_dir, 'build.ninja.d')).touch()
 
   def tearDown(self):
     shutil.rmtree(self.build_dir)
 
-  def test_gn_clean_ok(self):
-    pathlib.Path(os.path.join(self.build_dir, 'build.ninja')).touch()
-    pathlib.Path(os.path.join(self.build_dir, 'build.ninja.d')).touch()
-
+  def test_delete_build_dir_full(self):
     # Create a dummy file in the build dir and ensure it gets removed.
     dummy_file = os.path.join(self.build_dir, 'dummy')
     pathlib.Path(dummy_file).touch()
 
-    clobber._clean_build_dir(self.build_dir)
+    clobber.delete_build_dir(self.build_dir)
+
     self.assertFalse(os.path.exists(dummy_file))
 
-  def test_gn_clean_fail(self):
-    # gn clean fails without build.ninja.
-    # clean_build_dir() regenerates build.ninja internally.
-    clobber._clean_build_dir(self.build_dir)
+  def test_delete_build_dir_fail(self):
+    # Make delete_dir() throw to ensure it's handled gracefully.
+
+    with mock.patch('clobber.delete_dir', side_effect=OSError):
+      with self.assertRaises(OSError):
+        clobber.delete_build_dir(self.build_dir)
 
 
 if __name__ == '__main__':
