790e5e763969997e6a9af593c3ab8d6978b671ec
Ben Becker <benbecker@chromium.org>
2023-01-17 23:57:31 +0000

    CloseAll: Begin closing window after moving it
    This CL changes the order in which we initiate closing windows in
    CloseAll. Previously, we would start closing a window and then move it
    to the UnparentedContainer. However, for some windows, such as ARC
    windows, the window loses its root window immediately after closing is
    initiated, which means that we are not able to access the
    UnparentedContainer for its root window after we initiate its closing.
    We fix this by moving the call to its widget's Close operation to be
    after we reparent the window.
    
    Bug: b/255240550
    Change-Id: Ic852f602884fbb2ed4c3a5a4555f38189a8bcdbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4166192
    Commit-Queue: Ben Becker <benbecker@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093619}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -1914,8 +1914,6 @@ void DesksController::FinalizeDeskRemoval(RemovedDeskData* removed_desk_data) {
     views::Widget* widget = views::Widget::GetWidgetForNativeView(window);
     DCHECK(widget);
 
-    widget->Close();
-
     // `widget->Close();` calls the underlying `native_widget_->Close()` which
     // will schedule `native_widget_->CloseNow()` as an async task. Only
     // when `native_widget_->CloseNow()` finishes running, the window will
@@ -1940,6 +1938,11 @@ void DesksController::FinalizeDeskRemoval(RemovedDeskData* removed_desk_data) {
           ->GetChildById(kShellWindowId_UnparentedContainer)
           ->AddChild(window);
     }
+
+    // We need to ensure that `widget->Close()` is called after we move the
+    // windows to the unparented container because some windows lose access to
+    // their root window immediately when their widget starts closing.
+    widget->Close();
   }
 
   // Schedules a delayed task to forcefully close all windows that have not
