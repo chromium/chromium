a6664e53add11fff32d44dbb8aed7b6e08d62be9
Viktoriia Kovalova <vkovalova@google.com>
2023-01-17 17:00:57 +0000

    [Kiosk] Destruct WebKioskAppManager before Profile
    Fixing the destruction order of WebKioskAppManager. Destruction of
    WebKioskAppManager should happen before Profile destruction, since
    manager is owning kiosk session, which stores the pointer to the
    profile.
    
    BUG=b:238726374
    TEST=checked on DUT with auto-launched kiosk and manual launch from menu
    
    Change-Id: I55c3218c04cc8188c6a0a6c08cb309d7f6f636c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4173571
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Viktoriia Kovalova <vkovalova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1093365}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -1597,6 +1597,11 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
   // vc_app_service_client_ has to be destructed before PostMainMessageLoopRun.
   vc_app_service_client_.reset();
 
+  // Has a dependency on Profile, so it needs to be destroyed before Profile
+  // gets destroyed during ProfileManager destruction, which happens inside
+  // PostMainMessageLoop below.
+  web_kiosk_app_manager_.reset();
+
   // NOTE: Closes ash and destroys `Shell`.
   ChromeBrowserMainPartsLinux::PostMainMessageLoopRun();
 
@@ -1607,7 +1612,6 @@ void ChromeBrowserMainPartsAsh::PostMainMessageLoopRun() {
 
   // Destroy classes that may have ash observers or dependencies.
   arc_kiosk_app_manager_.reset();
-  web_kiosk_app_manager_.reset();
   chrome_keyboard_controller_client_.reset();
 
   // All ARC related modules should have been shut down by this point, so
