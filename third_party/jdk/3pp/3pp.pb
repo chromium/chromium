8fcc05ded3f50060b758aaf66a6c192c8bd48091
Mohamed Heikal <mheikal@chromium.org>
2023-01-17 19:23:20 +0000

    Automatically upload new jdk releases to cipd
    Bug: 1402708
    Change-Id: I828c64375b44eefbf9fe3c657f439834dad3a6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174392
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093448}

--- third_party/jdk/3pp/3pp.pb
+++ third_party/jdk/3pp/3pp.pb
@@ -4,13 +4,7 @@
 
 create {
   source {
-    url {
-      # See the link "[Binaries]" in
-      # https://wiki.openjdk.java.net/display/JDKUpdates/JDK11u#JDK11u-Releases
-      download_url: "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz"
-      version: "11.0.15+10"
-    }
-    patch_version: 'cr0'
+    script { name: "fetch.py" }
     unpack_archive: true
     subdir: 'current'
   }
---
+++ third_party/jdk/3pp/fetch.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import hashlib
+import json
+import pathlib
+import urllib.request
+
+# JDK11 releases can be found from the following urls:
+# https://wiki.openjdk.java.net/display/JDKUpdates/JDK11u#JDK11u-Releases
+# https://github.com/adoptium/temurin11-binaries/releases/
+
+_LATEST_URL = 'https://api.github.com/repos/adoptium/temurin11-binaries/releases/latest'
+
+
+def get_latest_release():
+    """Returns data of the latest release."""
+    return json.load(urllib.request.urlopen(_LATEST_URL))
+
+
+def do_latest():
+    # Make the version change every time this file changes.
+    md5 = hashlib.md5()
+    md5.update(pathlib.Path(__file__).read_bytes())
+    file_hash = md5.hexdigest()[:10]
+    release = get_latest_release()
+    print('{}.{}'.format(release['name'], file_hash))
+
+
+def do_get_url():
+    release = get_latest_release()
+    # version without file hash.
+    version = release['name']
+    release_url = None
+    for asset in release['assets']:
+        name = asset['name']
+        if 'jdk_x64_linux_' in name and name.endswith('.tar.gz'):
+            if release_url is not None:
+                raise Exception(
+                    'Multiple valid assets found: \n{}\n{}\n'.format(
+                        release_url, asset['browser_download_url']))
+            release_url = asset['browser_download_url']
+
+    if release_url is None:
+        raise Exception(f'jdk_x64_linux asset not found for {version}.')
+
+    partial_manifest = {
+        'url': [release_url],
+        'ext': '.tar.gz',
+    }
+    print(json.dumps(partial_manifest))
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    sub = ap.add_subparsers(required=True)
+
+    latest = sub.add_parser("latest")
+    latest.set_defaults(func=do_latest)
+
+    download = sub.add_parser("get_url")
+    download.set_defaults(func=do_get_url)
+
+    opts = ap.parse_args()
+    opts.func()
+
+
+if __name__ == '__main__':
+    main()
