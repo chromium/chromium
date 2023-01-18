6a54b31bd7d676058ea29fcc1336cef8c2c913f0
Mitsuru Oshima <oshima@chromium.org>
2023-01-18 17:18:58 +0000

    Remove unnecessary mojo code
    that was added for mustash
    
    Bug: None
    Change-Id: If8925315475d15db09e95ec0ebe585f0782d37cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4133551
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093933}

--- ash/test/ash_unittests.cc
+++ ash/test/ash_unittests.cc
@@ -3,13 +3,10 @@
 // found in the LICENSE file.
 
 #include "base/functional/bind.h"
-#include "base/message_loop/message_pump_type.h"
 #include "base/test/launcher/unit_test_launcher.h"
-#include "base/threading/thread.h"
 #include "chromeos/ash/components/test/ash_test_suite.h"
 #include "components/viz/test/test_gpu_service_holder.h"
 #include "mojo/core/embedder/embedder.h"
-#include "mojo/core/embedder/scoped_ipc_support.h"
 
 namespace ash {
 
@@ -29,14 +26,6 @@ int main(int argc, char** argv) {
   ash::AshTestSuite test_suite(argc, argv);
 
   mojo::core::Init();
-  // The IPC thread is necessary for the window service.
-  base::Thread ipc_thread("IPC thread");
-  ipc_thread.StartWithOptions(
-      base::Thread::Options(base::MessagePumpType::IO, 0));
-  mojo::core::ScopedIPCSupport ipc_support(
-      ipc_thread.task_runner(),
-      mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
-
   return base::LaunchUnitTests(
       argc, argv,
       base::BindOnce(&ash::AshTestSuite::Run, base::Unretained(&test_suite)));
