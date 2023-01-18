917643678a3159acb2ea20057113952d09c846a0
Peng Huang <penghuang@chromium.org>
2023-01-18 15:49:09 +0000

    Remove ServiceFontManager::AssertHandle()
    Bug: 1406362
    Change-Id: I89d841b51934fc85664866d9baf254b355f32ab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177210
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1093886}

--- gpu/command_buffer/service/service_font_manager.h
+++ gpu/command_buffer/service/service_font_manager.h
@@ -49,7 +49,6 @@ class GPU_GLES2_EXPORT ServiceFontManager
   bool AddHandle(SkDiscardableHandleId handle_id,
                  ServiceDiscardableHandle handle);
   bool DeleteHandle(SkDiscardableHandleId handle_id);
-  void AssertHandle(SkDiscardableHandleId handle_id);
 
   base::Lock lock_;
 
