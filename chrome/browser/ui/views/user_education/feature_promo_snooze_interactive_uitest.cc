c38949e72599fe5efc868fb16a58f86ce1c7cc8a
Dana Fried <dfried@chromium.org>
2023-01-18 23:35:02 +0000

    [User Education] Add behavioral tests for IPH snooze.
    Upgrades IPH snooze interaction tests to Kombucha.
    
    Adds a test for the case where the anchor of a bubble is destroyed,
    closing the bubble.
    
    Change-Id: Id35de7d2f5603e412be3c621cd383212e1ad08b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4160028
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094149}

--- chrome/browser/ui/views/user_education/feature_promo_snooze_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/feature_promo_snooze_interactive_uitest.cc
@@ -3,9 +3,11 @@
 // found in the LICENSE file.
 
 #include <memory>
+#include <utility>
 
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
@@ -14,9 +16,10 @@
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/user_education/browser_feature_promo_controller.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/feature_engagement/test/mock_tracker.h"
+#include "components/feature_engagement/test/scoped_iph_feature_list.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/user_education/common/feature_promo_snooze_service.h"
@@ -48,23 +51,27 @@ BASE_FEATURE(kSnoozeTestFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 }
 
-class FeaturePromoSnoozeInteractiveTest : public InProcessBrowserTest {
+class FeaturePromoSnoozeInteractiveTest : public InteractiveBrowserTest {
  public:
   FeaturePromoSnoozeInteractiveTest() {
     subscription_ = BrowserContextDependencyManager::GetInstance()
                         ->RegisterCreateServicesCallbackForTesting(
                             base::BindRepeating(RegisterMockTracker));
+    scoped_feature_list_.InitAndEnableFeatures({kSnoozeTestFeature});
   }
+  ~FeaturePromoSnoozeInteractiveTest() override = default;
 
   void SetUpOnMainThread() override {
+    InteractiveBrowserTest::SetUpOnMainThread();
+
     mock_tracker_ =
         static_cast<NiceMock<feature_engagement::test::MockTracker>*>(
             feature_engagement::TrackerFactory::GetForBrowserContext(
                 browser()->profile()));
     ASSERT_TRUE(mock_tracker_);
 
-    promo_controller_ = BrowserView::GetBrowserViewForBrowser(browser())
-                            ->GetFeaturePromoController();
+    promo_controller_ = static_cast<BrowserFeaturePromoController*>(
+        browser()->window()->GetFeaturePromoController());
     snooze_service_ = promo_controller_->snooze_service();
 
     if (!promo_controller_->registry()->IsFeatureRegistered(
@@ -77,110 +84,89 @@ class FeaturePromoSnoozeInteractiveTest : public InProcessBrowserTest {
   }
 
  protected:
-  void ClickButton(views::Button* button) {
-    // TODO(crbug.com/1135850): switch back to MoveMouseToCenterAndPress when
-    // fixed.
-    ui::MouseEvent mouse_press(ui::ET_MOUSE_PRESSED, gfx::PointF(),
-                               gfx::PointF(), ui::EventTimeForNow(),
-                               ui::EF_LEFT_MOUSE_BUTTON,
-                               ui::EF_LEFT_MOUSE_BUTTON);
-    button->OnMouseEvent(&mouse_press);
+  using SnoozeData = user_education::FeaturePromoSnoozeService::SnoozeData;
 
-    ui::MouseEvent mouse_release(ui::ET_MOUSE_RELEASED, gfx::PointF(),
-                                 gfx::PointF(), ui::EventTimeForNow(),
-                                 ui::EF_LEFT_MOUSE_BUTTON,
-                                 ui::EF_LEFT_MOUSE_BUTTON);
-    button->OnMouseEvent(&mouse_release);
+  auto CheckSnoozePrefs(bool is_dismissed, int show_count, int snooze_count) {
+    return Check(base::BindLambdaForTesting(
+        [this, is_dismissed, show_count, snooze_count]() {
+          auto data = snooze_service_->ReadSnoozeData(kSnoozeTestFeature);
+
+          if (!data.has_value()) {
+            return false;
+          }
+
+          EXPECT_EQ(data->is_dismissed, is_dismissed);
+          EXPECT_EQ(data->show_count, show_count);
+          EXPECT_EQ(data->snooze_count, snooze_count);
+
+          // last_show_time is only meaningful if a show has occurred.
+          if (data->show_count > 0) {
+            EXPECT_GE(data->last_show_time, last_show_time_.first);
+            EXPECT_LE(data->last_show_time, last_show_time_.second);
+          }
+
+          // last_snooze_time is only meaningful if a snooze has occurred.
+          if (data->snooze_count > 0) {
+            EXPECT_GE(data->last_snooze_time, last_snooze_time_.first);
+            EXPECT_LE(data->last_snooze_time, last_snooze_time_.second);
+          }
+
+          return !testing::Test::HasNonfatalFailure();
+        }));
   }
 
-  bool HasSnoozePrefs(const base::Feature& iph_feature) {
-    return snooze_service_->ReadSnoozeData(iph_feature).has_value();
-  }
-
-  void CheckSnoozePrefs(const base::Feature& iph_feature,
-                        bool is_dismissed,
-                        int show_count,
-                        int snooze_count,
-                        base::Time last_show_time_min,
-                        base::Time last_show_time_max,
-                        base::Time last_snooze_time_min,
-                        base::Time last_snooze_time_max) {
-    auto data = snooze_service_->ReadSnoozeData(iph_feature);
-
-    // If false, adds a failure and returns early from this function.
-    ASSERT_TRUE(data.has_value());
-
-    EXPECT_EQ(data->is_dismissed, is_dismissed);
-    EXPECT_EQ(data->show_count, show_count);
-    EXPECT_EQ(data->snooze_count, snooze_count);
-
-    // last_show_time is only meaningful if a show has occurred.
-    if (data->show_count > 0) {
-      EXPECT_GE(data->last_show_time, last_show_time_min);
-      EXPECT_LE(data->last_show_time, last_show_time_max);
-    }
-
-    // last_snooze_time is only meaningful if a snooze has occurred.
-    if (data->snooze_count > 0) {
-      EXPECT_GE(data->last_snooze_time, last_snooze_time_min);
-      EXPECT_LE(data->last_snooze_time, last_snooze_time_max);
-    }
-  }
-
-  void SetSnoozePrefs(const base::Feature& iph_feature,
-                      bool is_dismissed,
-                      absl::optional<int> show_count,
-                      int snooze_count,
-                      absl::optional<base::Time> last_show_time,
-                      base::Time last_snooze_time,
-                      base::TimeDelta last_snooze_duration) {
-    user_education::FeaturePromoSnoozeService::SnoozeData data;
-    data.is_dismissed = is_dismissed;
-    if (show_count)
-      data.show_count = *show_count;
-    data.snooze_count = snooze_count;
-    if (last_show_time)
-      data.last_show_time = *last_show_time;
-    data.last_snooze_time = last_snooze_time;
-    data.last_snooze_duration = last_snooze_duration;
-    snooze_service_->SaveSnoozeData(iph_feature, data);
+  auto SetSnoozePrefs(const SnoozeData& data) {
+    return Do(base::BindLambdaForTesting([this, data] {
+      snooze_service_->SaveSnoozeData(kSnoozeTestFeature, data);
+    }));
   }
 
   // Tries to show tab groups IPH by meeting the trigger conditions. If
   // |should_show| is true it checks that it was shown. If false, it
   // checks that it was not shown.
-  void AttemptIPH(bool should_show) {
-    if (should_show) {
-      EXPECT_CALL(*mock_tracker_, ShouldTriggerHelpUI(Ref(kSnoozeTestFeature)))
-          .WillOnce(Return(true));
-    } else {
-      EXPECT_CALL(*mock_tracker_, ShouldTriggerHelpUI(Ref(kSnoozeTestFeature)))
-          .Times(0);
-    }
+  auto AttemptIPH(bool should_show) {
+    return Do(base::BindLambdaForTesting([this, should_show]() {
+      if (should_show) {
+        last_show_time_.first = base::Time::Now();
+        EXPECT_CALL(*mock_tracker_,
+                    ShouldTriggerHelpUI(Ref(kSnoozeTestFeature)))
+            .WillOnce(Return(true));
+      } else {
+        EXPECT_CALL(*mock_tracker_,
+                    ShouldTriggerHelpUI(Ref(kSnoozeTestFeature)))
+            .Times(0);
+      }
 
-    ASSERT_EQ(should_show,
-              promo_controller_->MaybeShowPromo(kSnoozeTestFeature));
-    ASSERT_EQ(should_show,
-              promo_controller_->IsPromoActive(kSnoozeTestFeature));
+      ASSERT_EQ(should_show,
+                promo_controller_->MaybeShowPromo(kSnoozeTestFeature));
+      ASSERT_EQ(should_show,
+                promo_controller_->IsPromoActive(kSnoozeTestFeature));
 
-    // If shown, Tracker::Dismissed should be called eventually.
-    if (should_show) {
-      EXPECT_CALL(*mock_tracker_, Dismissed(Ref(kSnoozeTestFeature)));
-    }
+      // If shown, Tracker::Dismissed should be called eventually.
+      if (should_show) {
+        EXPECT_CALL(*mock_tracker_, Dismissed(Ref(kSnoozeTestFeature)));
+        last_show_time_.second = base::Time::Now();
+      }
+    }));
   }
 
-  user_education::HelpBubbleView* GetPromoBubbleView() {
-    return promo_controller_->promo_bubble_for_testing()
-        ->AsA<user_education::HelpBubbleViews>()
-        ->bubble_view();
+  auto SnoozeIPH() {
+    return Steps(
+        Do(base::BindLambdaForTesting(
+            [this]() { last_snooze_time_.first = base::Time::Now(); })),
+        PressButton(
+            user_education::HelpBubbleView::kFirstNonDefaultButtonIdForTesting),
+        WaitForHide(
+            user_education::HelpBubbleView::kHelpBubbleElementIdForTesting),
+        Do(base::BindLambdaForTesting(
+            [this]() { last_snooze_time_.second = base::Time::Now(); })));
   }
 
-  views::Button* GetSnoozeButtonForTesting() {
-    return GetPromoBubbleView()->GetNonDefaultButtonForTesting(0);
-  }
-
-  views::Button* GetDismissButtonForTesting() {
-    return GetPromoBubbleView()->GetDefaultButtonForTesting();
+  auto DismissIPH() {
+    return Steps(
+        PressButton(user_education::HelpBubbleView::kDefaultButtonIdForTesting),
+        WaitForHide(
+            user_education::HelpBubbleView::kHelpBubbleElementIdForTesting));
   }
 
   raw_ptr<NiceMock<feature_engagement::test::MockTracker>, DanglingUntriaged>
@@ -208,159 +194,120 @@ class FeaturePromoSnoozeInteractiveTest : public InProcessBrowserTest {
     return mock_tracker;
   }
 
-  base::test::ScopedFeatureList scoped_feature_list_;
+  std::pair<base::Time, base::Time> last_show_time_;
+  std::pair<base::Time, base::Time> last_snooze_time_;
 
+  feature_engagement::test::ScopedIphFeatureList scoped_feature_list_;
   base::CallbackListSubscription subscription_;
 };
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        DismissDoesNotSnooze) {
-  base::Time show_time_min = base::Time::Now();
-  ASSERT_NO_FATAL_FAILURE(AttemptIPH(true));
-  base::Time show_time_max = base::Time::Now();
-
-  ClickButton(GetDismissButtonForTesting());
-  CheckSnoozePrefs(kSnoozeTestFeature,
-                   /* is_dismiss */ true,
-                   /* show_count */ 1,
-                   /* snooze_count */ 0,
-                   /* last_show_time_min */ show_time_min,
-                   /* last_show_time_max */ show_time_max,
-                   /* last_snooze_time_min */ base::Time(),
-                   /* last_snooze_time_max */ base::Time());
+  RunTestSequence(AttemptIPH(true), DismissIPH(),
+                  CheckSnoozePrefs(/* is_dismiss */ true,
+                                   /* show_count */ 1,
+                                   /* snooze_count */ 0));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        SnoozeSetsCorrectTime) {
-  base::Time show_time_min = base::Time::Now();
-  ASSERT_NO_FATAL_FAILURE(AttemptIPH(true));
-  base::Time show_time_max = base::Time::Now();
-
-  base::Time snooze_time_min = base::Time::Now();
-  ClickButton(GetSnoozeButtonForTesting());
-  base::Time snooze_time_max = base::Time::Now();
-
-  CheckSnoozePrefs(kSnoozeTestFeature,
-                   /* is_dismiss */ false,
-                   /* show_count */ 1,
-                   /* snooze_count */ 1,
-                   /* last_show_time_min */ show_time_min,
-                   /* last_show_time_max */ show_time_max,
-                   /* last_snooze_time_min */ snooze_time_min,
-                   /* last_snooze_time_max */ snooze_time_max);
+  RunTestSequence(AttemptIPH(true), SnoozeIPH(),
+                  CheckSnoozePrefs(/* is_dismiss */ false,
+                                   /* show_count */ 1,
+                                   /* snooze_count */ 1));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest, CanReSnooze) {
   // Simulate the user snoozing the IPH.
-  base::TimeDelta snooze_duration = base::Hours(26);
-  base::Time snooze_time = base::Time::Now() - snooze_duration;
-  base::Time show_time = snooze_time - base::Seconds(1);
-  SetSnoozePrefs(kSnoozeTestFeature,
-                 /* is_dismiss */ false,
-                 /* show_count */ 1,
-                 /* snooze_count */ 1,
-                 /* last_show_time */ show_time,
-                 /* last_snooze_time */ snooze_time,
-                 /* last_snooze_duration */ snooze_duration);
+  SnoozeData data;
+  data.is_dismissed = false;
+  data.show_count = 1;
+  data.snooze_count = 1;
+  data.last_snooze_duration = base::Hours(26);
+  data.last_snooze_time = base::Time::Now() - data.last_snooze_duration;
+  data.last_show_time = data.last_snooze_time - base::Seconds(1);
 
-  base::Time show_time_min = base::Time::Now();
-  ASSERT_NO_FATAL_FAILURE(AttemptIPH(true));
-  base::Time show_time_max = base::Time::Now();
-
-  base::Time snooze_time_min = base::Time::Now();
-  ClickButton(GetSnoozeButtonForTesting());
-  base::Time snooze_time_max = base::Time::Now();
-
-  CheckSnoozePrefs(kSnoozeTestFeature,
-                   /* is_dismiss */ false,
-                   /* show_count */ 2,
-                   /* snooze_count */ 2,
-                   /* last_show_time_min */ show_time_min,
-                   /* last_show_time_max */ show_time_max,
-                   /* last_snooze_time_min */ snooze_time_min,
-                   /* last_snooze_time_max */ snooze_time_max);
+  RunTestSequence(SetSnoozePrefs(data), AttemptIPH(true), SnoozeIPH(),
+                  CheckSnoozePrefs(/* is_dismiss */ false,
+                                   /* show_count */ 2,
+                                   /* snooze_count */ 2));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        DoesNotShowIfDismissed) {
-  // Simulate the user dismissing the IPH.
-  SetSnoozePrefs(kSnoozeTestFeature,
-                 /* is_dismiss */ true,
-                 /* show_count */ 1,
-                 /* snooze_count */ 0,
-                 /* last_show_time */ base::Time(),
-                 /* last_snooze_time */ base::Time(),
-                 /* last_snooze_duration */ base::TimeDelta());
+  SnoozeData data;
+  data.is_dismissed = true;
+  data.show_count = 1;
+  data.snooze_count = 0;
 
-  AttemptIPH(false);
+  RunTestSequence(SetSnoozePrefs(data), AttemptIPH(false));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        DoesNotShowBeforeSnoozeDuration) {
-  // Simulate a very recent snooze.
-  base::TimeDelta snooze_duration = base::Hours(26);
-  base::Time snooze_time = base::Time::Now();
-  base::Time show_time = snooze_time - base::Seconds(1);
-  SetSnoozePrefs(kSnoozeTestFeature,
-                 /* is_dismiss */ false,
-                 /* show_count */ 1,
-                 /* snooze_count */ 1,
-                 /* last_show_time */ show_time,
-                 /* last_snooze_time */ snooze_time,
-                 /* last_snooze_duration */ snooze_duration);
+  SnoozeData data;
+  data.is_dismissed = false;
+  data.show_count = 1;
+  data.snooze_count = 1;
+  data.last_snooze_duration = base::Hours(26);
+  data.last_snooze_time = base::Time::Now();
+  data.last_show_time = data.last_snooze_time - base::Seconds(1);
 
-  AttemptIPH(false);
+  RunTestSequence(SetSnoozePrefs(data), AttemptIPH(false));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest, EndPromoSetsPrefs) {
-  base::Time show_time_min = base::Time::Now();
-  ASSERT_NO_FATAL_FAILURE(AttemptIPH(true));
-  base::Time show_time_max = base::Time::Now();
-
-  promo_controller_->EndPromo(kSnoozeTestFeature);
-
-  CheckSnoozePrefs(kSnoozeTestFeature,
-                   /* is_dismiss */ false,
-                   /* show_count */ 1,
-                   /* snooze_count */ 0,
-                   /* last_show_time_min */ show_time_min,
-                   /* last_show_time_max */ show_time_max,
-                   /* last_snooze_time_min */ base::Time(),
-                   /* last_snooze_time_max */ base::Time());
+  RunTestSequence(
+      AttemptIPH(true), Do(base::BindLambdaForTesting([this]() {
+        promo_controller_->EndPromo(kSnoozeTestFeature);
+      })),
+      WaitForHide(
+          user_education::HelpBubbleView::kHelpBubbleElementIdForTesting),
+      CheckSnoozePrefs(/* is_dismiss */ false,
+                       /* show_count */ 1,
+                       /* snooze_count */ 0));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        WidgetCloseSetsPrefs) {
-  base::Time show_time_min = base::Time::Now();
-  ASSERT_NO_FATAL_FAILURE(AttemptIPH(true));
-  base::Time show_time_max = base::Time::Now();
+  RunTestSequence(
+      AttemptIPH(true),
+      WithView(user_education::HelpBubbleView::kHelpBubbleElementIdForTesting,
+               base::BindOnce([](user_education::HelpBubbleView* bubble) {
+                 bubble->GetWidget()->CloseWithReason(
+                     views::Widget::ClosedReason::kEscKeyPressed);
+               })),
+      WaitForHide(
+          user_education::HelpBubbleView::kHelpBubbleElementIdForTesting),
+      CheckSnoozePrefs(/* is_dismiss */ false,
+                       /* show_count */ 1,
+                       /* snooze_count */ 0));
+}
 
-  auto* const bubble = GetPromoBubbleView();
-  bubble->GetWidget()->CloseWithReason(
-      views::Widget::ClosedReason::kEscKeyPressed);
-  CheckSnoozePrefs(kSnoozeTestFeature,
-                   /* is_dismiss */ false,
-                   /* show_count */ 1,
-                   /* snooze_count */ 0,
-                   /* last_show_time_min */ show_time_min,
-                   /* last_show_time_max */ show_time_max,
-                   /* last_snooze_time_min */ base::Time(),
-                   /* last_snooze_time_max */ base::Time());
+IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest, AnchorHideSetsPrefs) {
+  RunTestSequence(
+      AttemptIPH(true),
+      WithView(user_education::HelpBubbleView::kHelpBubbleElementIdForTesting,
+               base::BindOnce([](user_education::HelpBubbleView* bubble) {
+                 // This should yank the bubble out from under us.
+                 bubble->GetAnchorView()->SetVisible(false);
+               })),
+      WaitForHide(
+          user_education::HelpBubbleView::kHelpBubbleElementIdForTesting),
+      CheckSnoozePrefs(/* is_dismiss */ false,
+                       /* show_count */ 1,
+                       /* snooze_count */ 0));
 }
 
 IN_PROC_BROWSER_TEST_F(FeaturePromoSnoozeInteractiveTest,
                        WorkWithoutNonClickerData) {
+  SnoozeData data;
+  data.is_dismissed = false;
+  data.snooze_count = 1;
+  data.last_snooze_duration = base::Hours(26);
+  data.last_snooze_time = base::Time::Now() - data.last_snooze_duration;
+
   // Non-clicker policy shipped pref entries that don't exist before.
   // Make sure empty entries are properly handled.
-  base::TimeDelta snooze_duration = base::Hours(26);
-  base::Time snooze_time = base::Time::Now() - snooze_duration;
-  SetSnoozePrefs(kSnoozeTestFeature,
-                 /* is_dismiss */ false,
-                 /* show_count */ absl::nullopt,
-                 /* snooze_count */ 1,
-                 /* last_show_time */ absl::nullopt,
-                 /* last_snooze_time */ snooze_time,
-                 /* last_snooze_duration */ snooze_duration);
-
-  AttemptIPH(true);
+  RunTestSequence(SetSnoozePrefs(data), AttemptIPH(true));
 }
