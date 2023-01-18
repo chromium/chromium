d1f44c2e48c78aee0fb9b0132a12de24adfa6606
Marcello Salomao <msalomao@google.com>
2023-01-18 08:44:36 +0000

    Merge to M109: [Files app] Fix dependency chain breaking Files on Kiosk mode
    Importing metadata_model.js from drive_sync_handler.js was breaking
    Files app when running on Kiosk mode (see bug for more information).
    
    Since the import was just used for typing, converting it to an extern
    and replacing the import fixes the issue in a safe way that can be
    backported to M108+.
    
    (cherry picked from commit d1b6eb90017542fc575872560cc5a05b2cebe61b)
    
    Bug: b/260192017
    Test: Manual
    Change-Id: I92e13117114bec4aaffdc39baf384c9270680357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4105141
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1082926}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4127742
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5414@{#1401}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- ui/file_manager/file_manager/background/js/drive_sync_handler.js
+++ ui/file_manager/file_manager/background/js/drive_sync_handler.js
@@ -14,7 +14,7 @@ import {xfm} from '../../common/js/xfm.js';
 import {DriveSyncHandler} from '../../externs/background/drive_sync_handler.js';
 import {ProgressCenter} from '../../externs/background/progress_center.js';
 import {DriveDialogControllerInterface} from '../../externs/drive_dialog_controller.js';
-import {MetadataModel} from '../../foreground/js/metadata/metadata_model.js';
+import {MetadataModelInterface} from '../../externs/metadata_model.js';
 
 import {fileOperationUtil} from './file_operation_util.js';
 
@@ -37,7 +37,7 @@ export class DriveSyncHandlerImpl extends EventTarget {
 
     /**
      * The metadata model to notify entries have changed.
-     * @type {?MetadataModel}
+     * @type {?MetadataModelInterface}
      * @private
      */
     this.metadataModel_ = null;
@@ -195,7 +195,7 @@ export class DriveSyncHandlerImpl extends EventTarget {
   }
 
   /**
-   * @param {!MetadataModel} model
+   * @param {!MetadataModelInterface} model
    */
   set metadataModel(model) {
     this.metadataModel_ = model;
