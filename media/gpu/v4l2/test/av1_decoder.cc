9c5ae90aeabd8c51063995043d1dfd166724d2b8
Steve Cho <stevecho@google.com>
2023-01-18 20:51:03 +0000

    media/gpu/v4l2: AV1 platform decoding changes for AV1 uAPI v4
    To reflect the AV1 uAPI v4 update in [1], this CL updates the platform
    decoding to fit the new uAPI:
    - v4l2_av1_global_motion.params[] type changed from __u32 to __s32
    - v4l2_ctrl_av1_frame.ref_frame_idx type changed from __u8 to __s8
    
    This commit is needed before updating the system headers.
    
    [1] https://lore.kernel.org/linux-media/20230103154832.6982-1-daniel.almeida@collabora.com/
    
    Bug: b:265063647
    TEST: tast run <DUT_IP> video.PlatformDecoding.v4l2_stateless_av1
    Change-Id: I352564fbd812faf61d63a64c9ae3e118424a1eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4159211
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094052}

--- media/gpu/v4l2/test/av1_decoder.cc
+++ media/gpu/v4l2/test/av1_decoder.cc
@@ -475,14 +475,23 @@ void FillGlobalMotionParams(
     constexpr auto kNumGlobalMotionParams = std::size(decltype(gm.params){});
 
     for (size_t j = 0; j < kNumGlobalMotionParams; ++j) {
-      // TODO(b/247611513): Remove separate handling when gm.params[j] < 0 if
-      // V4L2 AV1 uAPI decides to make an update to make this param consistent
-      // with definition in libgav1 parser
-      if (gm.params[j] < 0) {
-        v4l2_gm->params[i][j] =
-            base::checked_cast<uint32_t>(UINT32_MAX + gm.params[j] + 1);
-      } else
-        v4l2_gm->params[i][j] = base::checked_cast<uint32_t>(gm.params[j]);
+      // TODO(b/265204534): V4L2 AV1 uAPI v4 changed |params|'s data type from
+      // uint32_t to int32_t. Remove separate handling for gm.params[j] < 0 case
+      // when the kernel related change lands.
+      static_assert(
+          std::is_same<decltype(v4l2_gm->params[0][0]), uint32_t&>::value ||
+              std::is_same<decltype(v4l2_gm->params[0][0]), int32_t&>::value,
+          "v4l2_av1_global_motion::params must be either uint32_t or int32_t");
+      if (std::is_same<decltype(v4l2_gm->params[0][0]), uint32_t&>::value) {
+        if (gm.params[j] < 0) {
+          v4l2_gm->params[i][j] =
+              base::checked_cast<uint32_t>(UINT32_MAX + gm.params[j] + 1);
+        } else {
+          v4l2_gm->params[i][j] = base::checked_cast<uint32_t>(gm.params[j]);
+        }
+      } else {
+        v4l2_gm->params[i][j] = gm.params[j];
+      }
     }
 
     conditionally_set_flags(&v4l2_gm->invalid, !libgav1::SetupShear(&gm),
@@ -864,9 +873,24 @@ void Av1Decoder::SetupFrameParams(
   static_assert(std::size(decltype(v4l2_frame_params->ref_frame_idx){}) ==
                     libgav1::kNumInterReferenceFrameTypes,
                 "Invalid size of |ref_frame_idx| array");
-  for (size_t i = 0; i < libgav1::kNumInterReferenceFrameTypes; i++)
-    v4l2_frame_params->ref_frame_idx[i] =
-        base::checked_cast<__u8>(frm_header.reference_frame_index[i]);
+  for (size_t i = 0; i < libgav1::kNumInterReferenceFrameTypes; i++) {
+    // TODO(b/265204534): V4L2 AV1 uAPI v4 changed |ref_frame_idx|'s data type
+    // from uint8_t to int8_t. Remove separate handling when the kernel related
+    // change lands.
+    static_assert(
+        std::is_same<decltype(v4l2_frame_params->ref_frame_idx[0]),
+                     uint8_t&>::value ||
+            std::is_same<decltype(v4l2_frame_params->ref_frame_idx[0]),
+                         int8_t&>::value,
+        "|ref_frame_idx| must be either uint8_t or int8_t");
+    if (std::is_same<decltype(v4l2_frame_params->ref_frame_idx[0]),
+                     uint8_t&>::value) {
+      v4l2_frame_params->ref_frame_idx[i] =
+          base::checked_cast<__u8>(frm_header.reference_frame_index[i]);
+    } else {
+      v4l2_frame_params->ref_frame_idx[i] = frm_header.reference_frame_index[i];
+    }
+  }
 
   v4l2_frame_params->skip_mode_frame[0] =
       base::checked_cast<__u8>(frm_header.skip_mode_frame[0]);
