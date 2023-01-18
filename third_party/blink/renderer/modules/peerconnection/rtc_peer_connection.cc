3534c5277fd7583fc3d55f2c491e8441bb1217ff
Harald Alvestrand <hta@chromium.org>
2023-01-18 08:02:06 +0000

    [Merge M109] Delete PeerConnectionHandler in PeerConnection finalizer
    Also guard against removal of PC during PeerConnectionHandler
    call that may cause garbage collection.
    
    (cherry picked from commit 5066dd66309d884762e5fb9be04b59582893d09a)
    
    Bug: chromium:1405256
    Change-Id: I9adf7b219e2026e07ccc0868c1a85f3b35cd9d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154578
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1091801}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4176392
    Auto-Submit: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5414@{#1400}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
@@ -643,10 +643,11 @@ RTCPeerConnection::~RTCPeerConnection() {
 }
 
 void RTCPeerConnection::Dispose() {
-  // Promptly clears the handler's pointer to |this|
+  // Promptly clears the handler
   // so that content/ doesn't access it in a lazy sweeping phase.
+  // Other references to the handler use a weak pointer, preventing access.
   if (peer_handler_) {
-    peer_handler_->CloseAndUnregister();
+    peer_handler_.reset();
   }
 }
 
