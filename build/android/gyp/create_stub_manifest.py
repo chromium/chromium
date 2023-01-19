5fe5a29c2403a5d3bcf1f88469a25335d9f5551c
Andrew Grieve <agrieve@chromium.org>
2023-01-19 19:47:06 +0000

    Android: Add <static-library> tag to trichrome library system stub apk
    It seems as though this tag is needed in the stubs.
    
    Bug: 1408164
    Change-Id: I9d573a888470dd8734e2f70eff1993def511071b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181485
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094614}

---
+++ build/android/gyp/create_stub_manifest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Generates AndroidManifest.xml for a -Stub.apk."""
+
+import argparse
+import pathlib
+
+_MAIN_TEMPLATE = """\
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="will.be.replaced"
+    android:versionCode="1"
+    android:versionName="1.0">
+  <application android:label="APK Stub">{}</application>
+</manifest>
+"""
+
+_STATIC_LIBRARY_TEMPLATE = """
+    <static-library android:name="{}" android:version="1" />
+"""
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--static-library-name')
+  parser.add_argument('--output', required=True)
+  args = parser.parse_args()
+
+  static_library_part = ''
+  if args.static_library_name:
+    static_library_part = _STATIC_LIBRARY_TEMPLATE.format(
+        args.static_library_name)
+
+  data = _MAIN_TEMPLATE.format(static_library_part)
+  pathlib.Path(args.output).write_text(data, encoding='utf8')
+
+
+if __name__ == '__main__':
+  main()
