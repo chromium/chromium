655e190d52b6abd34173474e38eb7498ff93bb48
Olesia Marukhno <olesiamarukhno@google.com>
2023-01-19 14:57:37 +0000

    [privacy sandbox] Fix the layout bug in the page info subpage
    The page info subpage doesn't calculate the preferred size correctly which leads to topics labels being cut off. For an unknown reason, setting additional 1px vertical padding fixes the bug. This is a temporary fix and we will investigate more on the source of the bug.
    
    Bug: 1378703
    Change-Id: I7cbcd24214da02a6b296b71372dda34e67855626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177127
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1094461}

--- chrome/browser/ui/views/page_info/page_info_ad_personalization_content_view.cc
+++ chrome/browser/ui/views/page_info/page_info_ad_personalization_content_view.cc
@@ -12,10 +12,12 @@
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/views/controls/rich_hover_button.h"
+#include "chrome/browser/ui/views/page_info/page_info_main_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_view_factory.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
@@ -26,11 +28,18 @@ PageInfoAdPersonalizationContentView::PageInfoAdPersonalizationContentView(
     PageInfo* presenter,
     ChromePageInfoUiDelegate* ui_delegate)
     : presenter_(presenter), ui_delegate_(ui_delegate) {
+  ChromeLayoutProvider* layout_provider = ChromeLayoutProvider::Get();
+  const auto button_insets =
+      layout_provider->GetInsetsMetric(INSETS_PAGE_INFO_HOVER_BUTTON);
+  const int vertical_distance =
+      layout_provider->GetDistanceMetric(DISTANCE_CONTROL_LIST_VERTICAL);
+
   SetLayoutManager(std::make_unique<views::FlexLayout>())
       ->SetOrientation(views::LayoutOrientation::kVertical);
   info_container_ = AddChildView(std::make_unique<views::View>());
   info_container_->SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical));
+      views::BoxLayout::Orientation::kVertical, button_insets,
+      vertical_distance));
 
   AddChildView(PageInfoViewFactory::CreateSeparator());
   const auto manage_button_id =
@@ -67,9 +76,6 @@ void PageInfoAdPersonalizationContentView::SetAdPersonalizationInfo(
   // The whole section shouldn't be visible when info is empty.
   DCHECK(!info.is_empty());
 
-  ChromeLayoutProvider* layout_provider = ChromeLayoutProvider::Get();
-  const auto button_insets =
-      layout_provider->GetInsetsMetric(INSETS_PAGE_INFO_HOVER_BUTTON);
   int message_id;
   if (info.has_joined_user_to_interest_group && !info.accessed_topics.empty()) {
     message_id =
@@ -93,20 +99,20 @@ void PageInfoAdPersonalizationContentView::SetAdPersonalizationInfo(
           views::style::STYLE_SECONDARY));
   description_label->SetMultiLine(true);
   description_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  description_label->SetProperty(views::kMarginsKey, button_insets);
+  // TODO(crbug.com/1378703): Figure out why without additional horizontal
+  // margin the size is being calculated incorrectly and the topics labels are
+  // being cut off.
+  description_label->SetProperty(views::kMarginsKey, gfx::Insets::VH(0, 1));
 
   if (!info.accessed_topics.empty()) {
     std::vector<std::u16string> topic_names;
     for (const auto& topic : info.accessed_topics) {
-      topic_names.push_back(topic.GetLocalizedRepresentation());
+      auto* topic_label =
+          info_container_->AddChildView(std::make_unique<views::Label>(
+              topic.GetLocalizedRepresentation(), views::style::CONTEXT_LABEL,
+              views::style::STYLE_PRIMARY));
+      topic_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
     }
-    auto* topic_label =
-        info_container_->AddChildView(std::make_unique<views::Label>(
-            base::JoinString(topic_names, u"\n"), views::style::CONTEXT_LABEL,
-            views::style::STYLE_PRIMARY));
-    topic_label->SetMultiLine(true);
-    topic_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-    topic_label->SetProperty(views::kMarginsKey, button_insets);
   }
 
   PreferredSizeChanged();
