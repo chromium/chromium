a1d61967bdeecb9925512bdfaf2125e7a8d6e82a
Salman <salmanmalik@chromium.org>
2023-01-19 13:47:31 +0000

    remoting: Remove remoting_use_wayland from installer
    Bug: chromium:1291247
    Change-Id: I406101a63b9c99b6e925c54d3a2c195e45e549b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179273
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Salman Malik <salmanmalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094430}

--- remoting/host/installer/linux/build_deb.py
+++ remoting/host/installer/linux/build_deb.py
@@ -7,22 +7,12 @@ import os
 import subprocess
 import sys
 
-REMOTING_USE_WAYLAND_OPT = "--remoting_use_wayland"
-
 
 def main():
   this_dir = os.path.dirname(os.path.abspath(__file__))
   build_deb_script = os.path.join(this_dir, 'build-deb.sh')
-  if (REMOTING_USE_WAYLAND_OPT in sys.argv):
-    proc_env = os.environ.copy()
-    proc_env["REMOTING_USE_WAYLAND"] = "1"
-    args = sys.argv[1:]
-    args.remove(REMOTING_USE_WAYLAND_OPT)
-    proc = subprocess.Popen([build_deb_script] + args, stdout=subprocess.PIPE,
-                            env=proc_env)
-  else:
-    proc = subprocess.Popen([build_deb_script] + sys.argv[1:],
-                            stdout=subprocess.PIPE)
+  proc = subprocess.Popen([build_deb_script] + sys.argv[1:],
+                          stdout=subprocess.PIPE)
   out, _ = proc.communicate()
   sys.stdout.write(out.decode('utf8').strip())
   return proc.returncode
