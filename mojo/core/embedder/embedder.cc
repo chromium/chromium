3225b0b53ab0d27a0d28e2f8e5a2d3616cbded4e
David Bienvenu <davidbienvenu@chromium.org>
2023-01-20 00:31:00 +0000

    win7dep: Remove Win7/8 code from mojo for ipcz
    Chrome has deprecated support for Win7/8. If Mojo wants to
    do the same, this CL removes the code to disable ipcz on
    Win7.
    
    Bug: 1385856
    Change-Id: Ibd0fa319f2aac6ce8810cf0a7ebde11841b15171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181072
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094786}

--- mojo/core/embedder/embedder.cc
+++ mojo/core/embedder/embedder.cc
@@ -34,12 +34,7 @@
         // BUILDFLAG(IS_ANDROID)
 #endif  // !BUILDFLAG(IS_NACL)
 
-#if BUILDFLAG(IS_WIN)
-#include "base/win/windows_version.h"
-#endif
-
-namespace mojo {
-namespace core {
+namespace mojo::core {
 
 namespace {
 
@@ -80,23 +75,12 @@ void InitFeatures() {
       base::FeatureList::IsEnabled(kMojoAvoidRandomPipeId));
 
   if (base::FeatureList::IsEnabled(kMojoIpcz)) {
-    EnableMojoIpczIfSupported();
+    EnableMojoIpcz();
   }
 }
 
-void EnableMojoIpczIfSupported() {
-#if BUILDFLAG(IS_WIN)
-  // TODO(https://crbug.com/1299283): Sandboxed processes on Windows versions
-  // older than 8.1 require some extra (not yet implemented) setup for ipcz to
-  // work properly. This is omitted for early experimentation.
-  const bool kIsIpczSupported =
-      base::win::GetVersion() >= base::win::Version::WIN8_1;
-#else
-  const bool kIsIpczSupported = true;
-#endif
-  if (kIsIpczSupported) {
-    g_mojo_ipcz_enabled.store(true, std::memory_order_release);
-  }
+void EnableMojoIpcz() {
+  g_mojo_ipcz_enabled.store(true, std::memory_order_release);
 }
 
 void Init(const Configuration& configuration) {
@@ -177,5 +161,4 @@ IpczDriverHandle CreateIpczTransportFromEndpoint(
   return ipcz_driver::ObjectBase::ReleaseAsHandle(std::move(transport));
 }
 
-}  // namespace core
-}  // namespace mojo
+}  // namespace mojo::core
--- mojo/core/embedder/embedder.h
+++ mojo/core/embedder/embedder.h
@@ -17,8 +17,7 @@
 #include "mojo/public/cpp/platform/platform_channel_endpoint.h"
 #include "third_party/ipcz/include/ipcz/ipcz.h"
 
-namespace mojo {
-namespace core {
+namespace mojo::core {
 
 // Basic configuration/initialization ------------------------------------------
 
@@ -51,14 +50,13 @@ scoped_refptr<base::SingleThreadTaskRunner> GetIOTaskRunner();
 // base::Features inside of Mojo.
 COMPONENT_EXPORT(MOJO_CORE_EMBEDDER) void InitFeatures();
 
-// Enables MojoIpcz if it is supported on the current platform. Called before
-// Init() is called. Only call this if the current program doesn't have
-// base::FeatureList integration, since otherwise InitFeatures() will do the
-// work.
+// Enables MojoIpcz. Called before Init() is called. Only call this if the
+// current program doesn't have base::FeatureList integration, since otherwise
+// InitFeatures() will do the work.
 //
 // TODO(crbug.com/1299283): Remove once MojoIpcz becomes the default
 // implementation.
-COMPONENT_EXPORT(MOJO_CORE_EMBEDDER) void EnableMojoIpczIfSupported();
+COMPONENT_EXPORT(MOJO_CORE_EMBEDDER) void EnableMojoIpcz();
 
 // Indicates whether the ipcz-based Mojo implementation is enabled. This can be
 // done by enabling the MojoIpcz feature.
@@ -90,7 +88,6 @@ IpczDriverHandle CreateIpczTransportFromEndpoint(
     const TransportEndpointTypes& endpoint_types,
     base::Process remote_process = base::Process());
 
-}  // namespace core
-}  // namespace mojo
+}  // namespace mojo::core
 
 #endif  // MOJO_CORE_EMBEDDER_EMBEDDER_H_
