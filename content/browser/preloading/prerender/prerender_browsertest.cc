db59de32d257ae4daf83e5fd5b74d01b17b42775
Hiroki Nakagawa <nhiroki@chromium.org>
2023-01-20 01:43:35 +0000

    Sheriff: Disable CancelPrerenderWhenIsOverridingUserAgentDiffers on Mac
    Bug: 1408911
    Change-Id: I0524a55b79b93a3c37483ac864e583d2d1e07bb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177720
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1094824}

--- content/browser/preloading/prerender/prerender_browsertest.cc
+++ content/browser/preloading/prerender/prerender_browsertest.cc
@@ -8599,8 +8599,16 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                   ->has_received_user_gesture_before_nav());
 }
 
+// TODO(https://crbug.com/1408911): This test is flaky on Mac bots.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_CancelPrerenderWhenIsOverridingUserAgentDiffers \
+  DISABLED_CancelPrerenderWhenIsOverridingUserAgentDiffers
+#else
+#define MAYBE_CancelPrerenderWhenIsOverridingUserAgentDiffers \
+  CancelPrerenderWhenIsOverridingUserAgentDiffers
+#endif
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
-                       CancelPrerenderWhenIsOverridingUserAgentDiffers) {
+                       MAYBE_CancelPrerenderWhenIsOverridingUserAgentDiffers) {
   const std::string user_agent_override = "foo";
 
   // Navigate to an initial page.
