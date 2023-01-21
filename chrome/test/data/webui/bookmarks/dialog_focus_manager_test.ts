7a1afc0ac1e0fca2a95fd80ed6422306c070f7dd
Cole Horvitz <colehorvitz@chromium.org>
2023-01-21 03:48:53 +0000

    Bookmarks: Migrate dialog_focus_manager_test.js to TypeScript
    Bug: 1401496
    Change-Id: Ie3ec4f2723f91913d72370f0885f352f6f61ec1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4181005
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095331}

--- chrome/test/data/webui/bookmarks/dialog_focus_manager_test.ts
+++ chrome/test/data/webui/bookmarks/dialog_focus_manager_test.ts
@@ -2,24 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {DialogFocusManager, MenuSource} from 'chrome://bookmarks/bookmarks.js';
+import {BookmarksCommandManagerElement, BookmarksItemElement, BookmarksListElement, DialogFocusManager, MenuSource} from 'chrome://bookmarks/bookmarks.js';
+import {getDeepActiveElement} from 'chrome://resources/js/util_ts.js';
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {assertEquals, assertFalse, assertNotEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
 import {TestCommandManager} from './test_command_manager.js';
 import {TestStore} from './test_store.js';
 import {createFolder, createItem, getAllFoldersOpenState, replaceBody, testTree} from './test_util.js';
 
-suite('DialogFocusManager', function() {
-  let list;
-  let store;
-  let items;
-  let commandManager;
-  let dialogFocusManager;
 
-  function keydown(el, key) {
-    keyDownOn(el, '', '', key);
+suite('DialogFocusManager', function() {
+  let list: BookmarksListElement;
+  let store: TestStore;
+  let items: NodeListOf<BookmarksItemElement>;
+  let commandManager: BookmarksCommandManagerElement;
+  let dialogFocusManager: DialogFocusManager;
+
+  function keydown(el: HTMLElement, key: string) {
+    keyDownOn(el, 0, '', key);
   }
 
   setup(function() {
@@ -45,7 +48,7 @@ suite('DialogFocusManager', function() {
     list.style.position = 'absolute';
     replaceBody(list);
     flush();
-    items = list.root.querySelectorAll('bookmarks-item');
+    items = list.shadowRoot!.querySelectorAll('bookmarks-item');
 
     commandManager = new TestCommandManager().getCommandManager();
     document.body.appendChild(commandManager);
@@ -55,68 +58,66 @@ suite('DialogFocusManager', function() {
   });
 
   test('restores focus on dialog dismissal', async function() {
-    const focusedItem = items[0];
+    const focusedItem = items[0]!;
     focusedItem.focus();
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
 
     commandManager.openCommandMenuAtPosition(0, 0, MenuSource.ITEM);
-    const dropdown = commandManager.$.dropdown.getIfExists();
+    const dropdown = commandManager.$.dropdown.getIfExists()!;
 
     assertTrue(dropdown.open);
-    assertNotEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertNotEquals(focusedItem, getDeepActiveElement());
 
     keydown(dropdown, 'Escape');
     assertFalse(dropdown.open);
 
-    await Promise.all([
-      eventToPromise('close', dropdown),
-      eventToPromise('focus', focusedItem),
-    ]);
+    await eventToPromise('close', dropdown);
 
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
   });
 
   test('restores focus after stacked dialogs', async () => {
-    const focusedItem = items[0];
+    const focusedItem = items[0]!;
     focusedItem.focus();
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
 
     commandManager.openCommandMenuAtPosition(0, 0, MenuSource.ITEM);
     const dropdown = commandManager.$.dropdown.getIfExists();
-    dropdown.close();
-    assertNotEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertTrue(!!dropdown);
 
     const editDialog = commandManager.$.editDialog.get();
-    editDialog.showEditDialog(store.data.nodes['2']);
+    editDialog.showEditDialog(store.data.nodes['2']!);
+    dropdown.close();
+    assertNotEquals(focusedItem, getDeepActiveElement());
 
     await eventToPromise('close', dropdown);
-    editDialog.onCancelButtonTap_();
-    assertNotEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    editDialog.$.dialog.cancel();
+    assertNotEquals(focusedItem, getDeepActiveElement());
 
     await eventToPromise('close', editDialog);
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
   });
 
   test('restores focus after multiple shows of same dialog', async () => {
-    let focusedItem = items[0];
+    let focusedItem = items[0]!;
     focusedItem.focus();
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
 
     commandManager.openCommandMenuAtPosition(0, 0, MenuSource.ITEM);
-    assertNotEquals(focusedItem, dialogFocusManager.getFocusedElement_());
-    const dropdown = commandManager.$.dropdown.getIfExists();
+    assertNotEquals(focusedItem, getDeepActiveElement());
+    const dropdown = commandManager.$.dropdown.getIfExists()!;
     dropdown.close();
 
-    focusedItem = items[3];
+    focusedItem = items[3]!;
     focusedItem.focus();
     commandManager.openCommandMenuAtPosition(0, 0, MenuSource.ITEM);
 
     await eventToPromise('close', dropdown);
     assertTrue(dropdown.open);
+    assertNotEquals(focusedItem, getDeepActiveElement());
     dropdown.close();
-    assertNotEquals(focusedItem, dialogFocusManager.getFocusedElement_());
 
     await eventToPromise('close', dropdown);
-    assertEquals(focusedItem, dialogFocusManager.getFocusedElement_());
+    assertEquals(focusedItem, getDeepActiveElement());
   });
 });
