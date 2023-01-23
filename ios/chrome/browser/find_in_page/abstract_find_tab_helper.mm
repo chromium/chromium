3d27674559f3bec18bbf8f3e881e42af9c32e24a
Quentin Pubert <qpubert@google.com>
2023-01-23 17:23:04 +0000

    [iOS] Add Native Find in Page tab helper
    This CL adds the Native Find in Page tab helper, which corresponds to
    the `JavaScriptFindTabHelper` in the old JavaScript FiP implementation.
    It manages a `FindInPageController` when the web state is realized.
    
    Fixed: 1408166
    Change-Id: I969a99354f060c583ee3a0ac261ac0a9f2825f3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3963061
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095685}

--- ios/chrome/browser/find_in_page/abstract_find_tab_helper.mm
+++ ios/chrome/browser/find_in_page/abstract_find_tab_helper.mm
@@ -4,7 +4,9 @@
 
 #import "ios/chrome/browser/find_in_page/abstract_find_tab_helper.h"
 
+#import "ios/chrome/browser/find_in_page/find_tab_helper.h"
 #import "ios/chrome/browser/find_in_page/java_script_find_tab_helper.h"
+#import "ios/public/provider/chrome/browser/find_in_page/find_in_page_api.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -12,8 +14,8 @@
 
 AbstractFindTabHelper* GetConcreteFindTabHelperFromWebState(
     web::WebState* web_state) {
-  // TODO(crbug.com/1408166): Once the Native Find in Page class `FindTabHelper`
-  // exists, If `ios::provider::IsNativeFindInPageEnabled()`, return
-  // `FindTabHelper::FromWebState(web_state)` instead.
+  if (ios::provider::IsNativeFindInPageEnabled()) {
+    return FindTabHelper::FromWebState(web_state);
+  }
   return JavaScriptFindTabHelper::FromWebState(web_state);
 }
