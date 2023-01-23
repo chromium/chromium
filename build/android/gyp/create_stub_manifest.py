dfa9abc6929f0446d33390565dc5c8221030488a
Andrew Grieve <agrieve@chromium.org>
2023-01-23 15:09:05 +0000

    [M109] Android: Add versions to system image stubs
    I don't think versionCode is required, but it's nice to have set for
    inspection purposes. The main fix here is that the <static-library> tag
    needs its version set since the it's a part of the package_name for the
    apk.
    
    (cherry picked from commit 39debde76e509774287a655285d8556a9b8dc634)
    
    Bug: 1408164
    Change-Id: I36b797c3e830cba7f6197f8219a01703b47ef536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4184801
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1095202}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4187275
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Ben Mason <benmason@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5414@{#1451}
    Cr-Branched-From: 4417ee59d7bf6df7a9c9ea28f7722d2ee6203413-refs/heads/main@{#1070088}

--- build/android/gyp/create_stub_manifest.py
+++ build/android/gyp/create_stub_manifest.py
@@ -11,28 +11,27 @@ import pathlib
 _MAIN_TEMPLATE = """\
 <?xml version="1.0" encoding="utf-8"?>
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="will.be.replaced"
-    android:versionCode="1"
-    android:versionName="1.0">
+    package="will.be.replaced">
   <application android:label="APK Stub">{}</application>
 </manifest>
 """
 
 _STATIC_LIBRARY_TEMPLATE = """
-    <static-library android:name="{}" android:version="1" />
+    <static-library android:name="{}" android:version="{}" />
 """
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--static-library-name')
+  parser.add_argument('--static-library-version')
   parser.add_argument('--output', required=True)
   args = parser.parse_args()
 
   static_library_part = ''
   if args.static_library_name:
     static_library_part = _STATIC_LIBRARY_TEMPLATE.format(
-        args.static_library_name)
+        args.static_library_name, args.static_library_version)
 
   data = _MAIN_TEMPLATE.format(static_library_part)
   pathlib.Path(args.output).write_text(data, encoding='utf8')
