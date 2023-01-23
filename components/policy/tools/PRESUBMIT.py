f7549e6f448ab2d3e29e122078f6ebd0bdb5cd48
Roman Sorokin <rsorokin@chromium.org>
2023-01-23 09:41:48 +0000

    [policy presubmit] Remove calls to other presubmit functions
    The functions "CheckChangeOnCommit" and "CheckChangeOnUpload" are
    deleted anyway. The presubmit is broken atm. I looked into possibility
    to fix it - but FWIU it does not make sense anyway. For example, if we
    wanted to run checks on the syntax_check_policy_template_json.py changes
    - the presubmit from components/policy/resources won't help. It verifies
    the modified policy only, so CheckModifiedPolicies won't run. Ideally we
    would have CheckAllPolicies for such changes.
    
    BUG=b:266377723
    
    Change-Id: I6e636abf951c2a679cbd7e16226cb7f217295c75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4177154
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095539}

--- components/policy/tools/PRESUBMIT.py
+++ components/policy/tools/PRESUBMIT.py
@@ -7,31 +7,6 @@ import sys
 
 USE_PYTHON3 = True
 
-
-def _RunOtherPresubmit(function_name, input_api, output_api):
-  # Apply the PRESUBMIT for components/policy/resources to run the syntax check
-  component_resources_path = os.path.join('components', 'policy', 'resources')
-
-  # need to add components/policy/resources because PRESUBMIT.py there
-  # imports policy_templates module in the directory.
-  component_resources_path_absolute = os.path.join(
-      input_api.change.RepositoryRoot(), component_resources_path)
-  if (component_resources_path_absolute not in sys.path):
-    sys.path.append(component_resources_path_absolute)
-
-  # Skip the presubmit if //components/policy/resources is changed as well.
-  if any(component_resources_path in os.path.dirname(changed_file.LocalPath())
-         for changed_file in input_api.change.AffectedFiles()):
-    return []
-
-  presubmit_path = os.path.join(component_resources_path_absolute,
-                                'PRESUBMIT.py')
-  presubmit_content = input_api.ReadFile(presubmit_path)
-  global_vars = {}
-  exec(presubmit_content, global_vars)
-  return global_vars[function_name](input_api, output_api)
-
-
 def _RunPythonUnitTests(input_api, output_api):
   tests = input_api.canned_checks.GetUnitTestsInDirectory(
       input_api,
@@ -43,12 +18,8 @@ def _RunPythonUnitTests(input_api, output_api):
 
 
 def CheckChangeOnUpload(input_api, output_api):
-  output = _RunOtherPresubmit("CheckChangeOnUpload", input_api, output_api)
-  output.extend(_RunPythonUnitTests(input_api, output_api))
-  return output
+  return _RunPythonUnitTests(input_api, output_api)
 
 
 def CheckChangeOnCommit(input_api, output_api):
-  output = _RunOtherPresubmit("CheckChangeOnCommit", input_api, output_api)
-  output.extend(_RunPythonUnitTests(input_api, output_api))
-  return output
+  return _RunPythonUnitTests(input_api, output_api)
