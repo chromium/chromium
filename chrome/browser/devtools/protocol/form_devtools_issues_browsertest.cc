f651e921abd3ae5e56d989d7076230c77dd7d394
Bruno Braga <brunobraga@google.com>
2023-01-23 13:02:20 +0000

    [Autofill in Devtools] Introduce full browser tests to assert devtools issues.
    Please note that I am also fixing a type on the duplicate id issue.
    
    Bug: 1399414
    Change-Id: I117b8034bd1f16bab659f7211cb8c0e052ad636d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4174001
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095593}

---
+++ chrome/browser/devtools/protocol/form_devtools_issues_browsertest.cc
@@ -0,0 +1,83 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <tuple>
+
+#include "chrome/browser/devtools/protocol/devtools_protocol_test_support.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/content_browser_test_utils.h"
+
+namespace content {
+
+namespace {
+class AutofillFormDevtoolsProtocolTest : public DevToolsProtocolTestBase {
+ public:
+  AutofillFormDevtoolsProtocolTest() = default;
+
+  void NavigateToFormPageAndEnableAudits() {
+    GURL test_url =
+        GetTestUrl("autofill", "autofill_form_devtools_issues_test.html");
+    EXPECT_TRUE(ui_test_utils::NavigateToURL(browser(), test_url));
+    EXPECT_TRUE(content::WaitForLoadStop(web_contents()));
+
+    Attach();
+    SendCommandSync("Audits.enable");
+  }
+
+  base::Value::Dict WaitForGenericIssueAdded(const std::string& error_type) {
+    auto matcher = [](const std::string& error_type,
+                      const base::Value::Dict& params) {
+      const std::string* maybe_error_type = params.FindStringByDottedPath(
+          "issue.details.genericIssueDetails.errorType");
+      return maybe_error_type && *maybe_error_type == error_type;
+    };
+
+    base::Value::Dict notification = WaitForMatchingNotification(
+        "Audits.issueAdded", base::BindRepeating(matcher, error_type));
+
+    EXPECT_EQ(*notification.FindStringByDottedPath("issue.code"),
+              "GenericIssue");
+
+    return notification;
+  }
+};
+}  // namespace
+
+IN_PROC_BROWSER_TEST_F(AutofillFormDevtoolsProtocolTest,
+                       FormHasLabelAssociatedToNameAttribute) {
+  NavigateToFormPageAndEnableAudits();
+  base::Value::Dict notification =
+      WaitForGenericIssueAdded("FormLabelForNameError");
+  EXPECT_TRUE(notification
+                  .FindIntByDottedPath(
+                      "issue.details.genericIssueDetails.violatingNodeId")
+                  .has_value());
+}
+
+IN_PROC_BROWSER_TEST_F(AutofillFormDevtoolsProtocolTest,
+                       FormHasInputsWithDuplicateId) {
+  NavigateToFormPageAndEnableAudits();
+  base::Value::Dict notification =
+      WaitForGenericIssueAdded("FormDuplicateIdForInputError");
+  EXPECT_TRUE(notification
+                  .FindIntByDottedPath(
+                      "issue.details.genericIssueDetails.violatingNodeId")
+                  .has_value());
+}
+
+IN_PROC_BROWSER_TEST_F(AutofillFormDevtoolsProtocolTest,
+                       FormHasInputWithNoLabels) {
+  NavigateToFormPageAndEnableAudits();
+  base::Value::Dict notification =
+      WaitForGenericIssueAdded("FormInputWithNoLabelError");
+  EXPECT_TRUE(notification
+                  .FindIntByDottedPath(
+                      "issue.details.genericIssueDetails.violatingNodeId")
+                  .has_value());
+}
+
+}  // namespace content
