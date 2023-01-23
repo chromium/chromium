4c3efb443e90cc7c55e371a57cb36f95a54b0c67
Stefano Duo <stefanoduo@google.com>
2023-01-23 14:17:14 +0000

    Reland "Reland "Reland "Reland "Check Cronet's dependencies during cronet_package creation""""
    This is a reland of commit 44ab285581135ec702e6ccfc57300a76ca804a9f
    Reason for reland:
    * Ignore `gn desc` output lines which are not dependencies
    * Ignore builds with with is_cronet_build == false
    
    Original change's description:
    > Reland "Reland "Reland "Check Cronet's dependencies during cronet_package creation"""
    >
    > This is a reland of commit 3c176cfaeada8e2d707568d18ac3b84a81a4a2e8
    >
    > Update dependencies.txt to list //clank/third_party/google3
    >
    > Original change's description:
    > > Reland "Reland "Check Cronet's dependencies during cronet_package creation""
    > >
    > > This is a reland of commit 5d50955677cfbde6d427cd1ca30ddbf1e805a7e1
    > >
    > > This fixes:
    > > * off-by-one index math in normalize_third_party_dep's
    > > * updates dependencies.txt //third_party/ce -> //third_party/ced
    > >
    > > Original change's description:
    > > > Reland "Check Cronet's dependencies during cronet_package creation"
    > > >
    > > > This is a reland of commit 5e289db9ef555d6118e4bedfcc069a0cb3340451
    > > >
    > > > Add the dependencies that show up when Cronet is compiled with
    > > > WebView's GN args:
    > > > * //net/tools/huffman_trie
    > > > * //net/tools/transport_security_state_generator
    > > > * //third_party/ce
    > > >
    > > > This will stop the test from complaining while we solve the
    > > > underlying issue.
    > > >
    > > > Original change's description:
    > > > > Check Cronet's dependencies during cronet_package creation
    > > > >
    > > > > Add new dependencies_checks action rule that is executed while
    > > > > building cronet_package. This action generates the full list of Cronet
    > > > > dependencies and compares that against known ones (stored in a new txt
    > > > > file, similarly to how we handle Cronet's APIs).
    > > > >
    > > > > third_party dependencies are handled in a special way: we stop at
    > > > > their top level directory and don't dig further, unless they
    > > > > internally bundle an additional third_party library. This is to reduce
    > > > > breakages during third_party rolls. Examples:
    > > > > - //third_party/foo/bar and //third_party/foo/baz will both be
    > > > >   reported as just //third_party/foo
    > > > > - //third_party/foo/third_party/bar will still be reported as is
    > > > >
    > > > > Bug: 1400233
    > > > > Change-Id: I79b144feb8451621c389aedfc1169a090fc5edd6
    > > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4081083
    > > > > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > > > > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > > > > Cr-Commit-Position: refs/heads/main@{#1086068}
    > > >
    > > > Bug: 1400233, 1403196
    > > > Change-Id: Ibeff987da53cc72f57cf23b9bfce7f8b4def223a
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116779
    > > > Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    > > > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1086404}
    > >
    > > Bug: 1400233, 1403196
    > > Change-Id: I260bb8328c4c82b34677bf9f9b7c8c3331c2b607
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4116787
    > > Reviewed-by: Garrett Beaty <gbeaty@google.com>
    > > Auto-Submit: Stefano Duo <stefanoduo@google.com>
    > > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1086498}
    >
    > Bug: 1400233, 1403196
    > Change-Id: I4529ab9eb9b0b6bcdba83a06b8daaa1ef006f19b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4123849
    > Reviewed-by: Garrett Beaty <gbeaty@google.com>
    > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1087356}
    
    Bug: 1400233, 1403196
    Change-Id: Ie2045015ef4c4c96f23905395dcb4d7807a7ef43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4156074
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095612}

---
+++ components/cronet/tools/check_cronet_dependencies.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""check_cronet_dependencies.py - Keep track of Cronet's dependencies."""
+
+import argparse
+import os
+import subprocess
+import sys
+
+REPOSITORY_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
+
+sys.path.insert(0, os.path.join(REPOSITORY_ROOT, 'build/android/gyp'))
+from util import build_utils  # pylint: disable=wrong-import-position
+
+
+def normalize_third_party_dep(dependency):
+  third_party_str = 'third_party/'
+  if third_party_str not in dependency:
+    raise ValueError('Dependency is not third_party dependency')
+  root_end_index = dependency.rfind(third_party_str) + len(third_party_str)
+  dependency_name_end_index = dependency.find("/", root_end_index)
+  if dependency_name_end_index == -1:
+    return dependency
+  return dependency[:dependency_name_end_index]
+
+
+def dedup_third_party_deps_internal(dependencies, root_deps=None):
+  if root_deps is None:
+    root_deps = set()
+  deduped_deps = []
+  for dependency in dependencies:
+    # crbug.com(1406537): `gn desc deps` can spit out non-deps stuff if it finds unknown
+    # GN args
+    if not dependency or not dependency.startswith('//'):
+      continue
+    if dependency[-1] == '/':
+      raise ValueError('Dependencies must not have a trailing forward slash')
+
+    if 'third_party/' not in dependency:
+      # We don't apply any filtering to non third_party deps.
+      deduped_deps.append(dependency)
+      continue
+
+    # Take the last occurrence to consider //third_party/foo and
+    # //third_party/foo/third_party/bar as two distinct dependencies.
+    third_party_dep_segments = dependency.split('third_party/')
+    third_party_dep = third_party_dep_segments[-1]
+    if '/' not in third_party_dep:
+      # Root dependencies are always unique.
+      # Note: We append the amount of splits to differentiate between
+      # //third_party/foo and //third_party/bar/third_party/foo.
+      root_dep = str(len(third_party_dep_segments)) + third_party_dep
+      root_deps.add(root_dep)
+      deduped_deps.append(normalize_third_party_dep(dependency))
+    else:
+      third_party_dep_root = (str(len(third_party_dep_segments)) +
+                              third_party_dep.split('/')[0])
+      if third_party_dep_root not in root_deps:
+        root_deps.add(third_party_dep_root)
+        deduped_deps.append(normalize_third_party_dep(dependency))
+  return (deduped_deps, root_deps)
+
+
+def dedup_third_party_deps(old_dependencies, new_dependencies):
+  """Maintains only a single target for each third_party dependency."""
+  (_, root_deps) = dedup_third_party_deps_internal(old_dependencies)
+  (deduped_deps, _) = dedup_third_party_deps_internal(new_dependencies,
+                                                      root_deps=root_deps)
+  return '\n'.join(deduped_deps)
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      prog='Check cronet dependencies',
+      description=
+      "Checks whether Cronet's current dependencies match the known ones.")
+  parser.add_argument(
+      '--new_dependencies_script',
+      type=str,
+      help='Relative path to the script that outputs new dependencies',
+      required=True,
+  )
+  parser.add_argument(
+      '--old_dependencies',
+      type=str,
+      help='Relative path to file that contains the old dependencies',
+      required=True,
+  )
+  parser.add_argument(
+      '--stamp',
+      type=str,
+      help='Path to touch on success',
+  )
+  parser.add_argument('--is_cronet_build',
+                      action='store_true',
+                      help='Whether this is an official Cronet build or not')
+  parser.set_defaults(is_cronet_build=False)
+  args = parser.parse_args()
+
+  if not args.is_cronet_build:
+    build_utils.Touch(args.stamp)
+    return
+
+  new_dependencies = subprocess.check_output(
+      [args.new_dependencies_script, args.old_dependencies]).decode('utf-8')
+  with open(args.old_dependencies, 'r') as f:
+    new_dependencies = dedup_third_party_deps(f.read().splitlines(),
+                                              new_dependencies.splitlines())
+  if new_dependencies != '':
+    print('New dependencies detected:')
+    print(new_dependencies)
+    print('Please update: ' + args.old_dependencies)
+    sys.exit(-1)
+  else:
+    build_utils.Touch(args.stamp)
+
+
+if __name__ == '__main__':
+  main()
