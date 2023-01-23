1ea45d11b9dadfa8467d3578c816047fbf4d9453
Mike Wittman <wittman@chromium.org>
2023-01-23 21:46:06 +0000

    Revert "[code health] Remove host created notification use by ClientConnectionManager"
    This reverts commit fa3c9b5f4fa49c8143af3948ca8ad343c472ff38.
    
    Reason for revert: may be causing crashes on creation observation call
    
    Original change's description:
    > [code health] Remove host created notification use by ClientConnectionManager
    >
    > Removes the use of the render process host created notification
    > in ClientConnectionManager. Usage is replaced with the
    > RenderProcessHostCreationObserver.
    >
    > Bug: 357627
    > Change-Id: Id00aa4ffd4602f0a4c9bcb959aff1e807978df50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179548
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095271}
    
    Bug: 357627, 1409290
    Change-Id: Ic0fa41068d66fc2d91381912a655e400f2b221bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189878
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095834}

--- components/heap_profiling/multi_process/client_connection_manager.h
+++ components/heap_profiling/multi_process/client_connection_manager.h
@@ -13,7 +13,6 @@
 #include "content/public/browser/child_process_data.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/render_process_host_creation_observer.h"
 
 namespace content {
 class RenderProcessHost;
@@ -42,10 +41,8 @@ enum class Mode;
 // This class can be subclassed for exactly one reason: to allow embedders to
 // override AllowedToProfileRenderer in order to prevent incognito renderers
 // from being profiled.
-class ClientConnectionManager
-    : public content::BrowserChildProcessObserver,
-      public content::RenderProcessHostCreationObserver,
-      content::NotificationObserver {
+class ClientConnectionManager : public content::BrowserChildProcessObserver,
+                                content::NotificationObserver {
  public:
   // The owner of this instance must guarantee that |controller_| outlives this
   // class.
@@ -89,9 +86,6 @@ class ClientConnectionManager
 
   void StartProfilingNonRendererChild(const content::ChildProcessData& data);
 
-  // content::RenderProcessHostCreationObserver
-  void OnRenderProcessHostCreated(content::RenderProcessHost* host) override;
-
   // NotificationObserver
   // Observe connection of renderer child processes.
   void Observe(int type,
@@ -114,11 +108,11 @@ class ClientConnectionManager
   // only be accessed on the UI thread and their values should be considered
   // opaque.
   //
-  // Semantically, the elements must be something that identifies which specific
-  // RenderProcess is being profiled. When the underlying RenderProcess goes
-  // away, the element must be removed. The RenderProcessHost pointer and the
-  // RenderProcessHostCreationObserver notification can be used to provide these
-  // semantics.
+  // Semantically, the elements must be something that identifies which
+  // specific RenderProcess is being profiled. When the underlying RenderProcess
+  // goes away, the element must be removed. The RenderProcessHost
+  // pointer and the NOTIFICATION_RENDERER_PROCESS_CREATED notification can be
+  // used to provide these semantics.
   //
   // This variable represents renderers that have been instructed to start
   // profiling - it does not reflect whether a renderer is currently still being
