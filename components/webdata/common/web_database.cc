78e2e779b44455dcdb0632abc6fe3e8c853ab7fc
Florian Leimgruber <fleimgruber@google.com>
2023-01-23 15:50:53 +0000

    Add "initial_creator_id" and "last_modifier_id" to AutofillProfile.
    This CL implements the proposal of go/leipzig-origin-and-last-modifier.
    In particular, the "initial_creator" and "last_modifier" is added to:
    - ContactInfoSpecifics proto
    - Proto conversion utils
    - AutofillTable
    - AutofillProfile
    This enables syncing this information.
    
    No logic is added to actually set these values. For the initial_creator
    this is not necessary yet, since at this stage of the project no new
    kAccount profiles can be created from within Autofill.
    Updating the last_modifier correctly will be addressed in a follow-up.
    
    The AutofillSyncBridge and corresponding proto are not updated, since
    this information is only relevant for kAccount profiles.
    
    Bug: 1348294
    Change-Id: I32dbcca4200c8b27bedf72dd106f1a04f08bac0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4110987
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095653}

--- components/webdata/common/web_database.cc
+++ components/webdata/common/web_database.cc
@@ -13,7 +13,7 @@
 // corresponding changes must happen in the unit tests, and new migration test
 // added.  See |WebDatabaseMigrationTest::kCurrentTestedVersionNumber|.
 // static
-const int WebDatabase::kCurrentVersionNumber = 109;
+const int WebDatabase::kCurrentVersionNumber = 110;
 
 const int WebDatabase::kDeprecatedVersionNumber = 82;
 
