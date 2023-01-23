d7e64dc5e53467aa83491947e0aafa82343c2a88
Kush Sinha <sinhak@chromium.org>
2023-01-23 17:21:32 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /components/subresource_filter
    
    This CL was uploaded by git cl split.
    
    Bug: 1406815
    Change-Id: I0587040353f2c235f070bb3282ce660d1dc3f1d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4164981
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095683}

--- components/subresource_filter/tools/filter_tool.cc
+++ components/subresource_filter/tools/filter_tool.cc
@@ -86,13 +86,12 @@ const url_pattern_index::flat::UrlRule* FindMatchingUrlRule(
   return filter.FindMatchingUrlRule(request_url, type);
 }
 
-const std::string& ExtractStringFromDictionary(base::Value* dictionary,
-                                               const std::string& key) {
-  DCHECK(dictionary->is_dict());
-
-  const base::Value* found = dictionary->FindKey(key);
+const std::string& ExtractStringFromDictionary(
+    const base::Value::Dict& dictionary,
+    const std::string& key) {
+  const std::string* found = dictionary.FindString(key);
   CHECK(found);
-  return found->GetString();
+  return *found;
 }
 
 }  // namespace
@@ -166,12 +165,13 @@ void FilterTool::MatchBatchImpl(std::istream* request_stream,
         base::JSONReader::ReadDeprecated(line);
     CHECK(dictionary);
 
+    DCHECK(dictionary->is_dict());
     const std::string& origin =
-        ExtractStringFromDictionary(dictionary.get(), "origin");
+        ExtractStringFromDictionary(dictionary->GetDict(), "origin");
     const std::string& request_url =
-        ExtractStringFromDictionary(dictionary.get(), "request_url");
+        ExtractStringFromDictionary(dictionary->GetDict(), "request_url");
     const std::string& request_type =
-        ExtractStringFromDictionary(dictionary.get(), "request_type");
+        ExtractStringFromDictionary(dictionary->GetDict(), "request_type");
 
     bool blocked;
     const url_pattern_index::flat::UrlRule* rule =
