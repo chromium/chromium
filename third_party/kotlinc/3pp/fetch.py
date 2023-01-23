3bdf0c20423baabfce30b8f59dc98beee4f0bcfc
Peter Wen <wnwen@chromium.org>
2023-01-23 18:26:17 +0000

    Android: Always fetch the latest kotlinc for 3pp
    Similar to //third_party/jdk/3pp/fetch.py.
    
    Bug: 1401205
    Change-Id: I74307c9e9bbb0e41166f8bad9f97641b23ef869a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4182245
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1095716}

--- third_party/kotlinc/3pp/fetch.py
+++ third_party/kotlinc/3pp/fetch.py
@@ -4,25 +4,58 @@
 # found in the LICENSE file.
 
 import argparse
+import hashlib
 import json
 import os
+import pathlib
+import urllib.request
 
-_FILE_URL = 'https://github.com/JetBrains/kotlin/releases/download/v{0}/kotlin-compiler-{0}.zip'
-_FILE_NAME = 'kotlinc.zip'
-_VERSION = '1.7.21'
-_PATCH_VERSION = '-cr1'
+# Releases can be found: https://github.com/JetBrains/kotlin/releases/
+_LATEST_URL = 'https://api.github.com/repos/JetBrains/kotlin/releases/latest'
+
+
+def get_latest_release():
+    """Returns data of the latest release."""
+    return json.load(urllib.request.urlopen(_LATEST_URL))
 
 
 def do_latest():
-    print(_VERSION + _PATCH_VERSION)
+    # Make the version change every time this file changes.
+    md5 = hashlib.md5()
+    md5.update(pathlib.Path(__file__).read_bytes())
+    file_hash = md5.hexdigest()[:10]
+    release = get_latest_release()
+    print('{}.{}'.format(release['name'], file_hash))
 
 
-def get_download_url(version):
-    if version.endswith(_PATCH_VERSION):
-      version = version[:-len(_PATCH_VERSION)]
+def get_download_url(original_version):
+    release = get_latest_release()
+    latest_release_name = release['name']
+    version_release_name = original_version.rsplit('.', 1)[0]
+    assert latest_release_name == version_release_name, (
+        f'The latest release name is {latest_release_name} but the one given '
+        f'via the version parameter is {version_release_name}.')
+
+    # version without file hash.
+    version = release['name']
+    release_url = None
+    file_name = None
+    for asset in release['assets']:
+        name = asset['name']
+        if 'kotlin-compiler-' in name and name.endswith('.zip'):
+            if release_url is not None:
+                raise Exception(
+                    'Multiple valid assets found: \n{}\n{}\n'.format(
+                        release_url, asset['browser_download_url']))
+            file_name = name
+            release_url = asset['browser_download_url']
+
+    if release_url is None:
+        raise Exception(f'kotlin-compiler asset not found for {version}.')
+
     partial_manifest = {
-        'url': [_FILE_URL.format(version)],
-        'name': [_FILE_NAME],
+        'url': [release_url],
+        'name': [file_name],
         'ext': '.zip',
     }
     print(json.dumps(partial_manifest))
