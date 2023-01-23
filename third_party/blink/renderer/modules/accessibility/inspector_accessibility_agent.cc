83f0341575af61a4521f67e18eb7f1ea41e115ef
David Tseng <dtseng@google.com>
2023-01-23 20:45:15 +0000

    Replace heading_level with hierarchical_level
    Heading, in this context, misleads one to believe
    ax::mojom::IntAttribute::kHierarchicalLevel
    only applies to headings. It does not[1].
    
    This is a pure cleanup and has no functional change.
    
    1. https://w3c.github.io/aria/#aria-level
    
    R=abigailbklein@google.com
    
    Bug: none
    Test: cq
    Change-Id: I59655f9799759a9bf0466ee60420cdc4b56c9196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188657
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1095797}

--- third_party/blink/renderer/modules/accessibility/inspector_accessibility_agent.cc
+++ third_party/blink/renderer/modules/accessibility/inspector_accessibility_agent.cc
@@ -237,11 +237,11 @@ void FillWidgetProperties(AXObject& ax_object,
 
   AddHasPopupProperty(node_data.GetHasPopup(), properties);
 
-  const int heading_level = node_data.GetIntAttribute(
+  const int hierarchical_level = node_data.GetIntAttribute(
       ax::mojom::blink::IntAttribute::kHierarchicalLevel);
-  if (heading_level > 0) {
-    properties.emplace_back(
-        CreateProperty(AXPropertyNameEnum::Level, CreateValue(heading_level)));
+  if (hierarchical_level > 0) {
+    properties.emplace_back(CreateProperty(AXPropertyNameEnum::Level,
+                                           CreateValue(hierarchical_level)));
   }
 
   if (RoleAllowsMultiselectable(role)) {
