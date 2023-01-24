3e1deb4faf409301bd1c8b2dbff0fbfe84939e8b
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-01-24 11:36:02 +0000

    Clean-up database in indexed db test via js API
    Bug: 1366717
    Change-Id: Ic8df55abb437f957c605857c87aeefa6a0bdddc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4187597
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096110}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/indexed-db-storage-key-track-untrack.js
@@ -1,47 +1,25 @@
 (async function(testRunner) {
+  const before = Date.now();
   const {dp, session} = await testRunner.startBlank(
       `Tests that tracking and untracking IndexedDB for storage key works\n`);
-
   await dp.Page.enable();
-  const protocolMessages = [];
-  const originalDispatchMessage = DevToolsAPI.dispatchMessage;
-  const originalSendCommand = DevToolsAPI._sendCommand;
-  DevToolsAPI.dispatchMessage = (message) => {
-    protocolMessages.push(message);
-    originalDispatchMessage(message);
-  };
-  DevToolsAPI._sendCommand = (sessionId, method, params) => {
-    protocolMessages.push({sessionId, method, params});
-    return originalSendCommand(sessionId, method, params);
-  }
-  window.onerror = (msg) => testRunner.log('onerror: ' + msg);
-  window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
-  let errorForLog = new Error();
-  setTimeout(() => {
-    testRunner.log(protocolMessages);
-    testRunner.die('Timeout', errorForLog);
-  }, 5000);
-
   const frameId = (await dp.Page.getResourceTree()).result.frameTree.frame.id;
-  errorForLog = new Error();
   const storageKey = (await dp.Storage.getStorageKeyForFrame({
                        frameId: frameId
                      })).result.storageKey;
-  errorForLog = new Error();
   await dp.Storage.trackIndexedDBForStorageKey({storageKey});
-  errorForLog = new Error();
   const listUpdatedPromise = dp.Storage.onceIndexedDBListUpdated(
       message => {return `indexedDB list updated for storage key ${
           message.params.storageKey}`});
   const contentUpdatedPromise = dp.Storage.onceIndexedDBContentUpdated(
       message => {return `indexedDB content updated for ${message.params}`});
-
   testRunner.log(`Open database, object store and set value`);
+  const id = Math.random();
 
   // Create database, objectStore and add a key-value pair.
   const valuePromise = session.evaluateAsync(`
     new Promise(async resolve => {
-      const request = window.indexedDB.open("test-database");
+      const request = window.indexedDB.open("test-database${id}");
       request.onerror = (event) => {
         resolve('failed to create a database');
       };
@@ -54,14 +32,15 @@
     })
   `);
 
-  testRunner.log(await Promise.all(
-      [listUpdatedPromise, contentUpdatedPromise, valuePromise]));
-  errorForLog = new Error();
+  const [listUpdatedEvent, contentUpdatedEvent, value] = await Promise.all(
+    [listUpdatedPromise, contentUpdatedPromise, valuePromise]);
+  testRunner.log(listUpdatedEvent);
+  testRunner.log(contentUpdatedEvent, "Title", ['databaseName', 'sessionId']);
+  testRunner.log(value);
 
   testRunner.log('\nUntrack IndexedDB for storage key');
 
   await dp.Storage.untrackIndexedDBForStorageKey({storageKey});
-  errorForLog = new Error();
   dp.Storage.onIndexedDBListUpdated(message => {message.params.storageKey});
   dp.Storage.onIndexedDBContentUpdated(message => {message.params});
 
@@ -70,7 +49,7 @@
   // Open database, objectStore and add another value.
   const oneMoreValue = await session.evaluateAsync(`
     new Promise(async resolve => {
-      const openreq = window.indexedDB.open("test-database");
+      const openreq = window.indexedDB.open("test-database${id}");
       openreq.onerror = (event) => {
         resolve("not able to open database");
       }
@@ -82,13 +61,22 @@
       };
     })
   `);
-  errorForLog = new Error();
 
   testRunner.log(oneMoreValue);
 
-  // Clean up
-  await dp.IndexedDB.deleteDatabase({storageKey, databaseName: "test-database"});
-  errorForLog = new Error();
+    // Clean up
+  try {
+    await session.evaluateAsync(`
+      new Promise(async (resolve, reject) => {
+        const req = window.indexedDB.deleteDatabase("test-database${id}");
+        req.onsuccess = resolve;
+        req.onerror = reject;
+      });
+    `);
+  } catch (e) {
+    testRunner.log(e);
+  } finally {
+    testRunner.completeTest();
+  }
 
-  testRunner.completeTest();
 })
