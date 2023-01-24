fef3c6472ecbed0d7547bfff263e5a36e6999139
David Awogbemila <awogbemila@chromium.org>
2023-01-24 22:27:43 +0000

    Revert Scroll Unification to disabled by default
    ScrollUnification is causing a significantly higher crashrate  in
    stable, so we are reverting it to disabled by default. We some time to
    pass between landing this patch and its hitting stable. In that time, we
    will monitor the crashrate on canary, dev and beta to verify that the
    preunification path is not broken.
    
    Bug: 1409965
    Change-Id: Ic0279e09beaec455d2bef401bab9b2c3026462f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191405
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096434}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -46,7 +46,7 @@ BASE_FEATURE(kRemoveMobileViewportDoubleTap,
 
 BASE_FEATURE(kScrollUnification,
              "ScrollUnification",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSchedulerSmoothnessForAnimatedScrolls,
              "SmoothnessModeForAnimatedScrolls",
