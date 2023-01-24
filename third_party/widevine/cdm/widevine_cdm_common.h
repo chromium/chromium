60d3c0072a6cb0f1b37c1179d295e16b9719782b
Xiaohan Wang <xhwang@chromium.org>
2023-01-24 22:23:42 +0000

    widevine: Use "inline constexpr" for consts
    This is the new recommendation. See discussions at:
    https://groups.google.com/a/chromium.org/g/chromium-dev/c/jROTxMo_m2Q/m/Boevr6kfBgAJ
    
    Change-Id: Iac68300ee3821585c425a0f4529f74a20c6d958b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191042
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096428}

--- third_party/widevine/cdm/widevine_cdm_common.h
+++ third_party/widevine/cdm/widevine_cdm_common.h
@@ -11,29 +11,31 @@
 // Default constants common to all Widevine CDMs.
 
 // "alpha" is a temporary name until a convention is defined.
-const char kWidevineKeySystem[] = "com.widevine.alpha";
+inline constexpr char kWidevineKeySystem[] = "com.widevine.alpha";
 
 #if BUILDFLAG(IS_WIN)
 // An sub key system of `kWidevineKeySystem` only used in experiments.
-const char kWidevineExperimentKeySystem[] = "com.widevine.alpha.experiment";
+inline constexpr char kWidevineExperimentKeySystem[] =
+    "com.widevine.alpha.experiment";
 #endif  // BUILDFLAG(IS_WIN)
 
 // Widevine CDM files are in a directory with this name. This path is also
 // hardcoded in some build files and changing it requires changing the build
 // files as well.
-const char kWidevineCdmBaseDirectory[] = "WidevineCdm";
+inline constexpr char kWidevineCdmBaseDirectory[] = "WidevineCdm";
 
 // Media Foundation Widevine CDM files are in a directory with this name.
-const char kMediaFoundationWidevineCdmBaseDirection[] =
+inline constexpr char kMediaFoundationWidevineCdmBaseDirection[] =
     "MediaFoundationWidevineCdm";
 
 // This name is used by UMA. Do not change it!
-const char kWidevineKeySystemNameForUMA[] = "Widevine";
+inline constexpr char kWidevineKeySystemNameForUMA[] = "Widevine";
 
 // Name of the CDM library.
-const char kWidevineCdmLibraryName[] = "widevinecdm";
+inline constexpr char kWidevineCdmLibraryName[] = "widevinecdm";
 
-const char kWidevineCdmDisplayName[] = "Widevine Content Decryption Module";
+inline constexpr char kWidevineCdmDisplayName[] =
+    "Widevine Content Decryption Module";
 
 // TODO(crbug.com/1231162): Remove the string identifier once we've migrated off
 // of the PluginPrivateFileSystem.
@@ -43,18 +45,19 @@ const char kWidevineCdmDisplayName[] = "Widevine Content Decryption Module";
 // the CDM running as a pepper plugin, this ID is based on the pepper plugin
 // MIME type. Changing this will result in any existing saved files becoming
 // inaccessible.
-const media::CdmType kWidevineCdmType{0x05d908e5dcca9960ull,
-                                      0xcd92d30eac98157aull};
+inline constexpr media::CdmType kWidevineCdmType{0x05d908e5dcca9960ull,
+                                                 0xcd92d30eac98157aull};
 
 // Constants specific to Windows MediaFoundation-based Widevine CDM library.
 #if BUILDFLAG(IS_WIN)
-const char kMediaFoundationWidevineCdmLibraryName[] = "Google.Widevine.CDM";
-const char kMediaFoundationWidevineCdmDisplayName[] =
+inline constexpr char kMediaFoundationWidevineCdmLibraryName[] =
+    "Google.Widevine.CDM";
+inline constexpr char kMediaFoundationWidevineCdmDisplayName[] =
     "Google Widevine Windows CDM";
 // TODO(crbug.com/1231162): Remove the string identifier once we've migrated off
 // of the PluginPrivateFileSystem.
-const media::CdmType kMediaFoundationWidevineCdmType{0x8e73dec793bf5adcull,
-                                                     0x27e572c9a1fd930eull};
+inline constexpr media::CdmType kMediaFoundationWidevineCdmType{
+    0x8e73dec793bf5adcull, 0x27e572c9a1fd930eull};
 #endif  // BUILDFLAG(IS_WIN)
 
 #endif  // WIDEVINE_CDM_WIDEVINE_CDM_COMMON_H_
