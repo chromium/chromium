cc5a52239a15167dc2ed0d228f98f430c909b9ff
Christian Xu <christianxu@google.com>
2023-01-24 16:55:48 +0000

    [iOS] Refactor fade truncating label
    Refactor FadeTruncatingLabel for future work to support multiline fade
    truncating label.
    
    Bug: 1404943
    Change-Id: Iae8c30120d7541dac3fdc3cd2a622cedbdf3011b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4165362
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096227}

--- ios/chrome/browser/ui/elements/fade_truncating_label.mm
+++ ios/chrome/browser/ui/elements/fade_truncating_label.mm
@@ -12,6 +12,77 @@
 #error "This file requires ARC support."
 #endif
 
+namespace {
+
+/// Creates a gradient opacity mask based on direction of `truncate_mode` for
+/// `rect`.
+UIImage* CreateLinearGradient(CGRect rect, BOOL fade_left, BOOL fade_right) {
+  // Create an opaque context.
+  CGColorSpaceRef color_space = CGColorSpaceCreateDeviceGray();
+  CGContextRef context = CGBitmapContextCreate(
+      nullptr, rect.size.width, rect.size.height, 8, 4 * rect.size.width,
+      color_space, kCGImageAlphaNone);
+
+  // White background will mask opaque, black gradient will mask transparent.
+  CGContextSetFillColorWithColor(context, [UIColor whiteColor].CGColor);
+  CGContextFillRect(context, rect);
+
+  // Create gradient from white to black.
+  CGFloat locs[2] = {0.0f, 1.0f};
+  CGFloat components[4] = {1.0f, 1.0f, 0.0f, 1.0f};
+  CGGradientRef gradient =
+      CGGradientCreateWithColorComponents(color_space, components, locs, 2);
+  CGColorSpaceRelease(color_space);
+
+  // Draw head and/or tail gradient.
+  CGFloat fade_width =
+      std::min(rect.size.height * 2, (CGFloat)floor(rect.size.width / 4));
+  CGFloat minX = CGRectGetMinX(rect);
+  CGFloat maxX = CGRectGetMaxX(rect);
+  if (fade_right) {
+    CGFloat start_x = maxX - fade_width;
+    CGPoint start_point = CGPointMake(start_x, CGRectGetMidY(rect));
+    CGPoint end_point = CGPointMake(maxX, CGRectGetMidY(rect));
+    CGContextDrawLinearGradient(context, gradient, start_point, end_point, 0);
+  }
+  if (fade_left) {
+    CGFloat start_x = minX + fade_width;
+    CGPoint start_point = CGPointMake(start_x, CGRectGetMidY(rect));
+    CGPoint end_point = CGPointMake(minX, CGRectGetMidY(rect));
+    CGContextDrawLinearGradient(context, gradient, start_point, end_point, 0);
+  }
+  CGGradientRelease(gradient);
+
+  // Clean up, return image.
+  CGImageRef ref = CGBitmapContextCreateImage(context);
+  UIImage* image = [UIImage imageWithCGImage:ref];
+  CGImageRelease(ref);
+  CGContextRelease(context);
+  return image;
+}
+
+/// Add attributes to a copy of `attributed_string`.
+NSAttributedString* AttributedStringCopyWithAttributes(
+    NSAttributedString* attributed_string,
+    NSLineBreakMode line_break_mode,
+    NSTextAlignment text_alignment,
+    BOOL force_left_to_right) {
+  NSMutableAttributedString* text_copy = attributed_string.mutableCopy;
+  NSMutableParagraphStyle* text_style =
+      [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+  text_style.lineBreakMode = line_break_mode;
+  text_style.alignment = text_alignment;
+  if (force_left_to_right) {
+    text_style.baseWritingDirection = NSWritingDirectionLeftToRight;
+  }
+  [text_copy addAttribute:NSParagraphStyleAttributeName
+                    value:text_style
+                    range:NSMakeRange(0, attributed_string.length)];
+  return text_copy;
+}
+
+}  // namespace
+
 @interface FadeTruncatingLabel ()
 
 // Gradient used to create fade effect. Changes based on view.frame size.
@@ -49,36 +120,10 @@
        !CGSizeEqualToSize([self.gradient size], self.bounds.size))) {
     CGRect rect =
         CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height);
-    self.gradient = [self linearGradientWithRect:rect];
+    self.gradient = CreateLinearGradient(
+        rect, /*fade_left=*/self.truncateMode & FadeTruncatingHead,
+        /*fade_right=*/self.truncateMode & FadeTruncatingTail);
   }
-}
-
-// Draw fade gradient mask if attributedText is wider than rect.
-- (void)drawTextInRect:(CGRect)requestedRect {
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  CGContextSaveGState(context);
-
-  if ([self.attributedText size].width > requestedRect.size.width)
-    CGContextClipToMask(context, self.bounds, [self.gradient CGImage]);
-
-  // Add the specified line break and alignment attributes to attributedText and
-  // draw the result.
-  NSMutableAttributedString* attributedString =
-      [self.attributedText mutableCopy];
-  NSMutableParagraphStyle* textStyle =
-      [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-  textStyle.lineBreakMode = self.lineBreakMode;
-  textStyle.alignment = self.textAlignment;
-  // URLs have their text direction set to to LTR (avoids RTL characters
-  // making the URL render from right to left, as per RFC 3987 Section 4.1).
-  if (self.displayAsURL)
-    textStyle.baseWritingDirection = NSWritingDirectionLeftToRight;
-  [attributedString addAttribute:NSParagraphStyleAttributeName
-                           value:textStyle
-                           range:NSMakeRange(0, [self.text length])];
-  [attributedString drawInRect:requestedRect];
-
-  CGContextRestoreGState(context);
 }
 
 - (void)setTextAlignment:(NSTextAlignment)textAlignment {
@@ -98,50 +143,52 @@
   [super setTextAlignment:textAlignment];
 }
 
-// Create gradient opacity mask based on direction.
-- (UIImage*)linearGradientWithRect:(CGRect)rect {
-  // Create an opaque context.
-  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
-  CGContextRef context =
-      CGBitmapContextCreate(nullptr, rect.size.width, rect.size.height, 8,
-                            4 * rect.size.width, colorSpace, kCGImageAlphaNone);
+#pragma mark - Text Drawing
 
-  // White background will mask opaque, black gradient will mask transparent.
-  CGContextSetFillColorWithColor(context, [UIColor whiteColor].CGColor);
-  CGContextFillRect(context, rect);
+/// Draws `attributedText` in `requestedRect` and apply gradient mask if the
+/// text is wider than rect.
+- (void)drawTextInRect:(CGRect)requestedRect {
+  NSAttributedString* configuredString =
+      [self attributedString:self.attributedText
+           withLineBreakMode:self.lineBreakMode];
 
-  // Create gradient from white to black.
-  CGFloat locs[2] = {0.0f, 1.0f};
-  CGFloat components[4] = {1.0f, 1.0f, 0.0f, 1.0f};
-  CGGradientRef gradient =
-      CGGradientCreateWithColorComponents(colorSpace, components, locs, 2);
-  CGColorSpaceRelease(colorSpace);
+  // Draw fade gradient mask if attributedText is wider than rect.
+  BOOL shouldApplyGradient =
+      self.attributedText.size.width > requestedRect.size.width;
 
-  // Draw head and/or tail gradient.
-  CGFloat fadeWidth =
-      std::min(rect.size.height * 2, (CGFloat)floor(rect.size.width / 4));
-  CGFloat minX = CGRectGetMinX(rect);
-  CGFloat maxX = CGRectGetMaxX(rect);
-  if (self.truncateMode & FadeTruncatingTail) {
-    CGFloat startX = maxX - fadeWidth;
-    CGPoint startPoint = CGPointMake(startX, CGRectGetMidY(rect));
-    CGPoint endPoint = CGPointMake(maxX, CGRectGetMidY(rect));
-    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+  [self drawAttributedString:configuredString
+                      inRect:requestedRect
+               applyGradient:shouldApplyGradient];
+}
+
+#pragma mark Text Drawing Private
+
+/// Draws `attributedString` in `requestedRect` and `applyGradient`.
+- (void)drawAttributedString:(NSAttributedString*)attributedString
+                      inRect:(CGRect)requestedRect
+               applyGradient:(BOOL)applyGradient {
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  CGContextSaveGState(context);
+
+  if (applyGradient) {
+    CGContextClipToMask(context, requestedRect, [self.gradient CGImage]);
   }
-  if (self.truncateMode & FadeTruncatingHead) {
-    CGFloat startX = minX + fadeWidth;
-    CGPoint startPoint = CGPointMake(startX, CGRectGetMidY(rect));
-    CGPoint endPoint = CGPointMake(minX, CGRectGetMidY(rect));
-    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
-  }
-  CGGradientRelease(gradient);
+  [attributedString drawInRect:requestedRect];
 
-  // Clean up, return image.
-  CGImageRef ref = CGBitmapContextCreateImage(context);
-  UIImage* image = [UIImage imageWithCGImage:ref];
-  CGImageRelease(ref);
-  CGContextRelease(context);
-  return image;
+  CGContextRestoreGState(context);
+}
+
+#pragma mark - Private methods
+
+/// Adds specified attributes to a copy of `attributedString` and sets line
+/// break mode to `lineBreakMode`.
+- (NSAttributedString*)attributedString:(NSAttributedString*)attributedString
+                      withLineBreakMode:(NSLineBreakMode)lineBreakMode {
+  // URLs have their text direction set to to LTR (avoids RTL characters
+  // making the URL render from right to left, as per RFC 3987 Section 4.1).
+  return AttributedStringCopyWithAttributes(
+      attributedString, lineBreakMode, self.textAlignment,
+      /*force_left_to_right=*/self.displayAsURL);
 }
 
 @end
