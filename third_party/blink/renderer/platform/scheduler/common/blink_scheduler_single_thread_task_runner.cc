85310c1782bb4200b01ff1236a38c1ad7ee9cafa
Scott Haseley <shaseley@chromium.org>
2023-01-24 19:27:26 +0000

    Revert "[scheduler] Add DumpWithoutCrashing for DeleteSoon() leaks"
    This reverts commit e257d19ccd4a09b132e4499159a9b4475594ebf8.
    
    Reason for revert: This generated a number of crash reports related to service worker client destruction (crbug.com/1409817). Removing the DumpWithoutCrashing until those are addressed.
    
    Original change's description:
    > [scheduler] Add DumpWithoutCrashing for DeleteSoon() leaks
    >
    > The scheduler uses custom SingleThreadTaskRunner that specializes
    > DeleteSoon() and ReleaseSoon() memory leaks where it can, but leaks
    > can still occur if posting to a task runner that is associated with a
    > thread that has been shut down. This CL adds a DumpWithoutCrashing to
    > flush out such cases if they exist so they can be fixed.
    >
    > Bug: 1376851
    > Change-Id: Ied7ed87c3e1e54c570b03657dd9e055b96ed5fea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4179031
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Commit-Queue: Scott Haseley <shaseley@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095771}
    
    Bug: 1376851
    Change-Id: I0350d6618b978e8dfaf7ca8ce18462d663cff002
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4192889
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096305}

--- third_party/blink/renderer/platform/scheduler/common/blink_scheduler_single_thread_task_runner.cc
+++ third_party/blink/renderer/platform/scheduler/common/blink_scheduler_single_thread_task_runner.cc
@@ -7,7 +7,6 @@
 #include <memory>
 #include <utility>
 
-#include "base/debug/dump_without_crashing.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/single_thread_task_runner.h"
@@ -65,10 +64,9 @@ class DeleteHelper {
       Delete();
     } else {
       // The deleter task couldn't be posted to the intended thread, so the only
-      // safe thing to do is leak the object. As leaks of this type should not
-      // be permitted, flush out any places where this happens so they can be
-      // fixed.
-      base::debug::DumpWithoutCrashing();
+      // safe thing to do is leak the object.
+      // TODO(crbug.com/1376851): Add a CHECK, DumpWithoutCrashing, or trace
+      // event to determine if leaks still occur.
     }
   }
 
