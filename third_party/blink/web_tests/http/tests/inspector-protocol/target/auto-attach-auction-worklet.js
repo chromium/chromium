98e0db49249979b5b739a8fd749c6c86f1e00768
Matt Menke <mmenke@chromium.org>
2023-01-25 00:15:35 +0000

    [FLEDGE] Run reporting scripts after auction completes.
    If the frame that ran the auction is destroyed before reporting scripts
    complete, no reports are sent. I plan to add histograms for that in a
    future CL.
    
    There are still a lot of cleanups to do - moving all reporting network
    calls into InterestGroupAuction, and add unit tests for it, is next in
    my queue.
    
    Bug: 1394777
    Change-Id: I27f7ff5678b0ea12587c734ccf49425cd7c92ba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4148715
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096513}

--- third_party/blink/web_tests/http/tests/inspector-protocol/target/auto-attach-auction-worklet.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/auto-attach-auction-worklet.js
@@ -18,6 +18,11 @@
     return input;
   }
 
+  let detachedBidderWorklets = 0;
+  let resolveWaitForSecondBidderWorkletDetachedPromise;
+  const waitForSecondBidderWorkletDetachedPromise = new Promise((resolve, reject)=>{
+    resolveWaitForSecondBidderWorkletDetachedPromise = resolve;
+  });
   bp.Target.onAttachedToTarget(event => {
     const params = event.params;
     testRunner.log(
@@ -28,7 +33,13 @@
   });
 
   bp.Target.onDetachedFromTarget(event => {
-    testRunner.log('Dettached: ' + urlForSession.get(event.params.sessionId));
+    let url = urlForSession.get(event.params.sessionId);
+    testRunner.log('Detached: ' + url);
+    if (url.endsWith("fledge_bidding_logic.js.php")) {
+      ++detachedBidderWorklets;
+      if (detachedBidderWorklets == 2)
+        resolveWaitForSecondBidderWorkletDetachedPromise();
+    }
   });
 
   const base = 'https://a.test:8443/inspector-protocol/resources/'
@@ -46,11 +57,18 @@
   const winner1 = await page1_session.evaluateAsync(auctionJs);
   testRunner.log('Auction winner:' + handleUrn(winner1));
 
-  // Ask to auto-attach related before re-running the auction.
+  // Ask to auto-attach related before re-running the auction. Use another page to
+  // avoid reporting events that happen after the auction completes.
+  const page2 = await testRunner.createPage({url: base + 'fledge_join.html'});
+  const page2_session = await page2.createSession();
   await bp.Target.autoAttachRelated(
-      {targetId: page1.targetId(), waitForDebuggerOnStart: false});
+      {targetId: page2.targetId(), waitForDebuggerOnStart: false});
 
-  const winner2 = await page1_session.evaluateAsync(auctionJs);
+  const winner2 = await page2_session.evaluateAsync(auctionJs);
+  // If FLEDGE is enabled, need to wait for post-auction scripts to run, to
+  // ensure a consistent number of FLEDGE scripts have been invoked.
+  if (detachedBidderWorklets > 0)
+    await waitForSecondBidderWorkletDetachedPromise;
   testRunner.log('Auction winner:' + handleUrn(winner2));
   testRunner.log('DONE');
   testRunner.completeTest();
