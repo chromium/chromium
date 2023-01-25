8e2f3833f3cd0fd8afceede2e6f4604fbcb7e98c
Brian Sheedy <bsheedy@chromium.org>
2023-01-25 00:57:36 +0000

    Switch build/skia_gold_common presubmit to v2
    Switches the build/skia_gold_common PRESUBMIT.py file to presubmit
    version 2.0.0. There should be no functional change as a result of this.
    
    Bug: 1324771
    Change-Id: I39046a7a16615e2bebbef08d4f45b3f68b6fd42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193333
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096537}

--- build/skia_gold_common/PRESUBMIT.py
+++ build/skia_gold_common/PRESUBMIT.py
@@ -9,31 +9,31 @@ for more details on the presubmit API built into depot_tools.
 
 USE_PYTHON3 = True
 
+PRESUBMIT_VERSION = '2.0.0'
 
-def CommonChecks(input_api, output_api):
-  output = []
+
+def _GetSkiaGoldEnv(input_api):
+  """Gets the common environment for running Skia Gold tests."""
   build_path = input_api.os_path.join(input_api.PresubmitLocalPath(), '..')
   skia_gold_env = dict(input_api.environ)
   skia_gold_env.update({
       'PYTHONPATH': build_path,
       'PYTHONDONTWRITEBYTECODE': '1',
   })
-  output.extend(
-      input_api.canned_checks.RunUnitTestsInDirectory(
-          input_api,
-          output_api,
-          input_api.PresubmitLocalPath(), [r'^.+_unittest\.py$'],
-          env=skia_gold_env,
-          run_on_python2=False,
-          skip_shebang_check=True))
-  output.extend(
-      input_api.canned_checks.RunPylint(input_api, output_api, version='2.7'))
-  return output
+  return skia_gold_env
 
 
-def CheckChangeOnUpload(input_api, output_api):
-  return CommonChecks(input_api, output_api)
+def CheckSkiaGoldCommonUnittests(input_api, output_api):
+  """Runs the unittests for the build/skia_gold_common/ directory."""
+  return input_api.canned_checks.RunUnitTestsInDirectory(
+      input_api,
+      output_api,
+      input_api.PresubmitLocalPath(), [r'^.+_unittest\.py$'],
+      env=_GetSkiaGoldEnv(input_api),
+      run_on_python2=False,
+      skip_shebang_check=True)
 
 
-def CheckChangeOnCommit(input_api, output_api):
-  return CommonChecks(input_api, output_api)
+def CheckPylint(input_api, output_api):
+  """Runs pylint on all directory content and subdirectories."""
+  return input_api.canned_checks.RunPylint(input_api, output_api, version='2.7')
