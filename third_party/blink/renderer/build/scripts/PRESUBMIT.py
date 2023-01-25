3c4ce781a7d6abd8b67d55258933d9cad7c207f5
Russ Hamilton <behamilton@google.com>
2023-01-25 00:47:44 +0000

    Fix failed presubmit for make_permissions_policy_features
    Also modify the presubmit to include the template file in the affected_list so that this issue will not re-occur in the future.
    
    Change-Id: I153536cf4b276ed109096cb8353d8c57251f1a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193408
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1096532}

--- third_party/blink/renderer/build/scripts/PRESUBMIT.py
+++ third_party/blink/renderer/build/scripts/PRESUBMIT.py
@@ -50,8 +50,12 @@ def _RunTests(input_api, output_api):
         'file_name': 'make_document_policy_features_tests.py',
         'affected_list': [r'.*make_document_policy_features.*']
     }, {
-        'file_name': 'make_permissions_policy_features_tests.py',
-        'affected_list': [r'.*make_permissions_policy_features.*']
+        'file_name':
+        'make_permissions_policy_features_tests.py',
+        'affected_list': [
+            r'.*make_permissions_policy_features.*',
+            'templates/third_party/permissions_policy_features.cc.tmpl',
+        ]
     }]
     test_commands = []
     for test in tests:
