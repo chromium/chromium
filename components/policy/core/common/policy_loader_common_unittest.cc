13f98bd56c51a4e3370f54bbf02373d42ba10154
Roman Sorokin <rsorokin@chromium.org>
2023-01-25 15:23:18 +0000

    [Code Health] Migrate FindKey to new base::Value::Dict API
    Migrate FindKey to new base::Value::Dict API in
    policy_loader_common_unittest.cc
    
    Bug: 1406815
    Change-Id: I67a91360f82c57ed44e32ca504683e0b1fba2815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194268
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1096769}

--- components/policy/core/common/policy_loader_common_unittest.cc
+++ components/policy/core/common/policy_loader_common_unittest.cc
@@ -16,28 +16,28 @@
 
 namespace policy {
 namespace {
-base::Value ToListValue(const std::vector<std::string>& values) {
+base::Value::List ToList(const std::vector<std::string>& values) {
   base::Value::List storage;
   storage.reserve(values.size());
   for (const auto& value : values)
     storage.Append(value);
 
-  return base::Value(std::move(storage));
+  return storage;
 }
 
-base::Value ToDictValue(const std::string& json) {
-  absl::optional<base::Value> value = base::JSONReader::Read(json);
-  return std::move(value.value());
+base::Value::Dict ToDict(const std::string& json) {
+  return base::JSONReader::Read(json).value().TakeDict();
 }
 
 }  // namespace
 
 class SensitivePolicyFilterTest : public ::testing::Test {
  public:
-  void AddNewPolicy(const std::string& name, const base::Value& value) {
+  void AddNewPolicy(const std::string& name, base::Value value) {
     policies_.Set(name, PolicyLevel::POLICY_LEVEL_MANDATORY,
                   PolicyScope::POLICY_SCOPE_MACHINE,
-                  PolicySource::POLICY_SOURCE_PLATFORM, value.Clone(), nullptr);
+                  PolicySource::POLICY_SOURCE_PLATFORM, std::move(value),
+                  nullptr);
   }
 
   PolicyMap* policies() { return &policies_; }
@@ -60,10 +60,10 @@ TEST_F(SensitivePolicyFilterTest, TestSimplePolicyFilter) {
 }
 
 TEST_F(SensitivePolicyFilterTest, TestExtensionInstallForceListFilter) {
-  base::Value policy = ToListValue(
-      {"extension0", "extension1;example.com", "extension2;",
-       "extension3;https://clients2.google.com/service/update2/crx"});
-  AddNewPolicy(key::kExtensionInstallForcelist, policy);
+  base::Value::List policy =
+      ToList({"extension0", "extension1;example.com", "extension2;",
+              "extension3;https://clients2.google.com/service/update2/crx"});
+  AddNewPolicy(key::kExtensionInstallForcelist, base::Value(std::move(policy)));
 
   EXPECT_TRUE(policies()->Get(key::kExtensionInstallForcelist));
 
@@ -72,14 +72,14 @@ TEST_F(SensitivePolicyFilterTest, TestExtensionInstallForceListFilter) {
   const auto* actual_filtered_policy = policies()->GetValue(
       key::kExtensionInstallForcelist, base::Value::Type::LIST);
   ASSERT_TRUE(actual_filtered_policy);
-  base::Value expected_filtered_policy = ToListValue(
+  base::Value::List expected_filtered_policy = ToList(
       {"extension0", "[BLOCKED]extension1;example.com", "[BLOCKED]extension2;",
        "extension3;https://clients2.google.com/service/update2/crx"});
   EXPECT_EQ(expected_filtered_policy, *actual_filtered_policy);
 }
 
 TEST_F(SensitivePolicyFilterTest, TestExtensionSettingsFilter) {
-  base::Value policy = ToDictValue(R"({
+  base::Value::Dict policy = ToDict(R"({
     "*": {
       "installation_mode": "force_installed",
       "update_url": "https://example.com"
@@ -108,22 +108,23 @@ TEST_F(SensitivePolicyFilterTest, TestExtensionSettingsFilter) {
     },
     "invalid": "settings"
   })");
-  AddNewPolicy(key::kExtensionSettings, policy);
+  AddNewPolicy(key::kExtensionSettings, base::Value(policy.Clone()));
 
   EXPECT_TRUE(policies()->Get(key::kExtensionSettings));
 
   FilterSensitivePolicies(policies());
 
-  const auto* filtered_policy =
-      policies()->GetValue(key::kExtensionSettings, base::Value::Type::DICT);
-  ASSERT_TRUE(filtered_policy);
-  EXPECT_EQ(policy.DictSize(), filtered_policy->DictSize());
-  for (const auto entry : policy.DictItems()) {
+  const base::Value::Dict& filtered_policy =
+      policies()
+          ->GetValue(key::kExtensionSettings, base::Value::Type::DICT)
+          ->GetDict();
+  EXPECT_EQ(policy.size(), filtered_policy.size());
+  for (const auto entry : policy) {
     std::string extension = entry.first;
     if (extension == "extension4" || extension == "extension5")
       extension = "[BLOCKED]" + extension;
 
-    const base::Value* filtered_setting = filtered_policy->FindKey(extension);
+    const base::Value* filtered_setting = filtered_policy.Find(extension);
     ASSERT_TRUE(filtered_setting);
     EXPECT_EQ(entry.second, *filtered_setting)
         << "Mismatch for extension: " + extension;
