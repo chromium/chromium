12906078e71207c4067c625c0423120d18feec39
Anthony Vallée-Dubois <anthonyvd@chromium.org>
2023-01-25 20:58:20 +0000

    Revert "add unit test for floating workspace v2"
    This reverts commit 6ddbfc7cd545055bff716bf904832ce9342be89e.
    
    Reason for revert: Failing on some Linux bots: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-cfm-rel/37348/overview
    
    Original change's description:
    > add unit test for floating workspace v2
    >
    > Bug: b:266497248
    > Change-Id: I1e3487685720d0920a0c099a653f348f795c000e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189047
    > Commit-Queue: Geng Li <ligeng@google.com>
    > Reviewed-by: Yanzhu Du <yzd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1096917}
    
    Bug: b:266497248
    Change-Id: Ia08dc6285c2cff40a3546ea001a5b72d8b41abc7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4188994
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Owners-Override: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1096975}

--- chrome/browser/ash/floating_workspace/floating_workspace_service.cc
+++ chrome/browser/ash/floating_workspace/floating_workspace_service.cc
@@ -78,9 +78,7 @@ void FloatingWorkspaceService::InitForTest(
       InitForV1();
       break;
     case TestFloatingWorkspaceVersion::kFloatingWorkspaceV2Enabled:
-      // For testings we don't need to add itself to observer list of
-      // DeskSyncBridge, tests can be done by calling
-      // EntriesAddedOrUpdatedRemotely directly so InitForV2 can be skipped.
+      InitForV2();
       break;
   }
 }
@@ -274,11 +272,6 @@ void FloatingWorkspaceService::RestoreFloatingWorkspaceTemplate(
     return;
   }
 
-  LaunchFloatingWorkspaceTemplate(desk_template);
-}
-
-void FloatingWorkspaceService::LaunchFloatingWorkspaceTemplate(
-    const DeskTemplate* desk_template) {
   DesksClient::Get()->LaunchDeskTemplate(
       desk_template->uuid(),
       base::BindOnce(&FloatingWorkspaceService::OnTemplateLaunched,
