0e6de52aff9a838c1e869af33ce9eda744648b5e
Brian Sheedy <bsheedy@chromium.org>
2023-01-25 21:53:11 +0000

    Re-enable Telemetry stack tests
    Re-enables all the tests in //tools/perf/core/stacktrace_unittest.py.
    The minidump code backing this was improved a long time ago and similar
    tests testing slightly different code paths have been stable for a while.
    
    Bug: 1342706
    Change-Id: I3b0ca998ef595f45f0710b69779b17fff8b85b95
    Fixed: 971998, 820282, 706328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193337
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097017}

--- tools/perf/core/stacktrace_unittest.py
+++ tools/perf/core/stacktrace_unittest.py
@@ -12,43 +12,36 @@ from telemetry.testing import tab_test_case
 
 class TabStackTraceTest(tab_test_case.TabTestCase):
 
-  # Stack traces do not currently work on 10.6, but they are also being
-  # disabled shortly so just disable it for now.
-  # All platforms except chromeos should at least have a valid minidump.
-  # Disabled on Windows: https://crbug.com/971998.
-  @decorators.Disabled('snowleopard', 'chromeos', 'win',
-                       'android-nougat')  # Flaky: https://crbug.com/1342706
+  # Stack traces do not currently work on Mac 10.6.
+  @decorators.Isolated
+  @decorators.Disabled('snowleopard', 'chromeos-local')
   def testValidDump(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=10)
     self.assertTrue(c.exception.is_valid_dump)
 
-  # Stack traces aren't working on Android yet.
-  # Disabled on Linux and Mac, flaky: https://crbug.com/820282.
-  @decorators.Disabled('all')
+  @decorators.Isolated
+  @decorators.Disabled('chromeos-local')
   def testCrashSymbols(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
-      self._tab.Navigate('chrome://crash', timeout=5)
+      self._tab.Navigate('chrome://crash', timeout=10)
     self.assertIn('CrashIntentionally', '\n'.join(c.exception.stack_trace))
 
   # Some platforms do not support full stack traces, this test requires only
   # minimal symbols to be available.
-  # Disabled on win due to crbug.com/706328.
-  # Disabled on Linux and Mac, flaky: https://crbug.com/820282.
-  @decorators.Disabled('all')
+  @decorators.Isolated
+  @decorators.Disabled('chromeos-local')
   def testCrashMinimalSymbols(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
-      self._tab.Navigate('chrome://crash', timeout=5)
+      self._tab.Navigate('chrome://crash', timeout=10)
     self.assertIn('HandleRendererDebugURL',
                   '\n'.join(c.exception.stack_trace))
 
   # The breakpad file specific test only apply to platforms which use the
   # breakpad symbol format. This also must be tested in isolation because it can
   # potentially interfere with other tests symbol parsing.
-  # @decorators.Enabled('mac', 'linux')
-  # Disabled tests due to flakiness: http://crbug.com/820282
-  @decorators.Disabled('all')
   @decorators.Isolated
+  @decorators.Enabled('linux')
   def testBadBreakpadFileIgnored(self):
     # pylint: disable=protected-access
     executable_path = self._browser._browser_backend._executable
@@ -61,15 +54,7 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
       f.write('MODULE PLATFORM ARCH %s %s' % (garbage_hash, executable))
       f.flush()
       with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
-        self._tab.Navigate('chrome://crash', timeout=5)
-      # The symbol directory should now symbols for out executable.
-      tmp_dir = os.path.join(self._browser._browser_backend._tmp_minidump_dir)
-      symbol_dir = os.path.join(tmp_dir, 'symbols')
-      self.assertTrue(os.path.isdir(symbol_dir))
-
-      # Bad breakpad file should not be in the symbol directory
-      garbage_symbol_dir = os.path.join(symbol_dir, executable, garbage_hash)
-      self.assertFalse(os.path.isdir(garbage_symbol_dir))
+        self._tab.Navigate('chrome://crash', timeout=10)
 
       # Stack trace should still work.
       self.assertIn('CrashIntentionally', '\n'.join(c.exception.stack_trace))
