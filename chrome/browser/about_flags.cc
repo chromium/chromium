ee64bdb65daf2f36549a28fe743d51c7b8876a1c
Brandon Jones <bajones@chromium.org>
2023-01-26 05:32:03 +0000

    Enable WebGPU for non-component Android builds
    Non-component Android builds were being blocked by ANGLE producing
    multiple .so files, but a previous Dawn change has temporarily
    disabled ANGLE in Dawn on Android, clearing the way for this change.
    
    Also activates the "Enable Unsafe WebGPU" and "WebGPU Developer
    Features" flags in about:flags.
    
    This commit is expected to increase the binary size of Chrome on Android
    by about 1.5Mb. This is mostly due to the new inclusion of the Dawn
    library, which provides Chrome's cross-platform implementation of the
    WebGPU API. Because WebGPU is a large new feature a proportional size
    increase is to be expected. By comparison a roughly equivalent library,
    ANGLE, also adds approximately the same amount to the binary size.
    
    See https://groups.google.com/a/google.com/g/webgpu-developers/c/g2NB5rR2R2A/m/73b9HQC2AgAJ
    for further discussion about the size impact.
    
    Bug: dawn:286
    Binary-Size: Added a new library to enable large feature.
    Change-Id: Iacf7fd14831565a8888264bb7d8a514864af0f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3914870
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097250}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6711,14 +6711,12 @@ const FeatureEntry kFeatureEntries[] = {
          browsing_data::features::kEnableRemovingAllThirdPartyCookies)},
 
     {"enable-unsafe-webgpu", flag_descriptions::kUnsafeWebGPUName,
-     flag_descriptions::kUnsafeWebGPUDescription,
-     kOsMac | kOsLinux | kOsCrOS | kOsLacros | kOsWin | kOsFuchsia,
+     flag_descriptions::kUnsafeWebGPUDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kEnableUnsafeWebGPU)},
 
     {"enable-webgpu-developer-features",
      flag_descriptions::kWebGpuDeveloperFeaturesName,
-     flag_descriptions::kWebGpuDeveloperFeaturesDescription,
-     kOsMac | kOsLinux | kOsCrOS | kOsLacros | kOsWin | kOsFuchsia,
+     flag_descriptions::kWebGpuDeveloperFeaturesDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kEnableWebGPUDeveloperFeatures)},
 
 #if BUILDFLAG(IS_ANDROID)
