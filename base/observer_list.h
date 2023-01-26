5d8d0d2168d15553ac5d4206f71cabd44b27cdeb
Keren Zhu <kerenzhu@chromium.org>
2023-01-26 22:45:29 +0000

    CHECK on duplicate AddObserver() calls in ObserverList
    There're observed crashes because of duplicate AddObserver() calls. See
    crbug.com/1410397#c2. This may lead to obscure bugs if not captured,
    therefore replace NOTREACHED with a CHECK so that it will always crash
    on release builds.
    
    Bug: None
    Change-Id: I23053675d33d2d6fc9cb2ecc7d083f03b6ec60c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4197957
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097657}

--- base/observer_list.h
+++ base/observer_list.h
@@ -272,10 +272,7 @@ class ObserverList {
   // Precondition: !HasObserver(obs)
   void AddObserver(ObserverType* obs) {
     DCHECK(obs);
-    if (HasObserver(obs)) {
-      NOTREACHED() << "Observers can only be added once!";
-      return;
-    }
+    CHECK(!HasObserver(obs)) << "Observers can only be added once!";
     observers_count_++;
     observers_.emplace_back(ObserverStorageType(obs));
   }
