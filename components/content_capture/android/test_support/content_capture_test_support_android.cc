ebfb056e182eaa6f0f5d9c78d6182ab7fdbf09e9
Kush Sinha <sinhak@chromium.org>
2023-01-26 04:39:00 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /components/content_capture
    
    This CL was uploaded by git cl split.
    
    Bug: 1406815
    Change-Id: I56ea1389b34e5e614b05992a688e2e9592b87565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193158
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097240}

--- components/content_capture/android/test_support/content_capture_test_support_android.cc
+++ components/content_capture/android/test_support/content_capture_test_support_android.cc
@@ -4,13 +4,17 @@
 
 #include "components/content_capture/android/test_support/jni_headers/ContentCaptureTestSupport_jni.h"
 
+#include <string>
+
 #include "base/android/jni_string.h"
+#include "base/check_deref.h"
 #include "base/json/json_reader.h"
 #include "base/notreached.h"
 #include "base/values.h"
 #include "components/content_capture/browser/content_capture_receiver.h"
 #include "components/content_capture/browser/onscreen_content_provider.h"
 #include "content/public/browser/web_contents.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/mojom/favicon/favicon_url.mojom.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -51,22 +55,28 @@ static void JNI_ContentCaptureTestSupport_SimulateDidUpdateFaviconURL(
   CHECK(root);
   CHECK(root->is_list());
   std::vector<blink::mojom::FaviconURLPtr> favicon_urls;
-  for (const base::Value& icon : root->GetList()) {
+  for (const base::Value& icon_val : root->GetList()) {
+    const base::Value::Dict& icon = icon_val.GetDict();
     std::vector<gfx::Size> sizes;
     // The sizes is optional.
-    if (auto* icon_sizes = icon.FindKey("sizes")) {
-      for (const base::Value& size : icon_sizes->GetList()) {
-        CHECK(size.FindKey("width"));
-        CHECK(size.FindKey("height"));
-        sizes.emplace_back(size.FindKey("width")->GetInt(),
-                           size.FindKey("height")->GetInt());
+    if (const base::Value::List* icon_sizes = icon.FindList("sizes")) {
+      for (const base::Value& size_val : CHECK_DEREF(icon_sizes)) {
+        const base::Value::Dict& size = size_val.GetDict();
+
+        const absl::optional<int> width = size.FindInt("width");
+        const absl::optional<int> height = size.FindInt("height");
+        CHECK(width);
+        CHECK(height);
+        sizes.emplace_back(width.value(), height.value());
       }
     }
-    CHECK(icon.FindKey("url"));
-    CHECK(icon.FindKey("type"));
-    favicon_urls.push_back(blink::mojom::FaviconURL::New(
-        GURL(*icon.FindKey("url")->GetIfString()),
-        ToType(*icon.FindKey("type")->GetIfString()), sizes));
+
+    const std::string* url = icon.FindString("url");
+    const std::string* type = icon.FindString("type");
+    CHECK(url);
+    CHECK(type);
+    favicon_urls.push_back(
+        blink::mojom::FaviconURL::New(GURL(*url), ToType(*type), sizes));
   }
   CHECK(!favicon_urls.empty());
   provider->NotifyFaviconURLUpdatedForTesting(
