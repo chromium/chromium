a165bf5709af51628666bcfd630ac449cd9f226a
Jean-Philippe Gravel <jpgravel@chromium.org>
2023-01-26 03:49:41 +0000

    Break the canvas WPT tool scripts into smaller functions.
    All of the code was in a single huge function, making it hard to
    navigate, modify and maintain.
    
    Change-Id: Idf1f29f2dc528192657bdd8c05cbd674e856de61
    Bug: 1409873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4193469
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1097227}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutils.py
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutils.py
@@ -56,70 +56,251 @@ class InvalidTestDefinitionError(Error):
     """Raised on invalid test definition."""
 
 
+def _simpleEscapeJS(string: str) -> str:
+    return string.replace('\\', '\\\\').replace('"', '\\"')
+
+
+def _escapeJS(string: str) -> str:
+    string = _simpleEscapeJS(string)
+    # Kind of an ugly hack, for nicer failure-message output.
+    string = re.sub(r'\[(\w+)\]', r'[\\""+(\1)+"\\"]', string)
+    return string
+
+
+def _expand_nonfinite(method: str, argstr: str, tail: str) -> str:
+    """
+    >>> print _expand_nonfinite('f', '<0 a>, <0 b>', ';')
+    f(a, 0);
+    f(0, b);
+    f(a, b);
+    >>> print _expand_nonfinite('f', '<0 a>, <0 b c>, <0 d>', ';')
+    f(a, 0, 0);
+    f(0, b, 0);
+    f(0, c, 0);
+    f(0, 0, d);
+    f(a, b, 0);
+    f(a, b, d);
+    f(a, 0, d);
+    f(0, b, d);
+    """
+    # argstr is "<valid-1 invalid1-1 invalid2-1 ...>, ..." (where usually
+    # 'invalid' is Infinity/-Infinity/NaN).
+    args = []
+    for arg in argstr.split(', '):
+        match = re.match('<(.*)>', arg)
+        if match is None:
+            raise InvalidTestDefinitionError(
+                f"Expected arg to match format '<(.*)>', but was: {arg}")
+        a = match.group(1)
+        args.append(a.split(' '))
+    calls = []
+    # Start with the valid argument list.
+    call = [args[j][0] for j in range(len(args))]
+    # For each argument alone, try setting it to all its invalid values:
+    for i in range(len(args)):
+        for a in args[i][1:]:
+            c2 = call[:]
+            c2[i] = a
+            calls.append(c2)
+    # For all combinations of >= 2 arguments, try setting them to their
+    # first invalid values. (Don't do all invalid values, because the
+    # number of combinations explodes.)
+    def f(c: List[str], start: int, depth: int) -> None:
+        for i in range(start, len(args)):
+            if len(args[i]) > 1:
+                a = args[i][1]
+                c2 = c[:]
+                c2[i] = a
+                if depth > 0:
+                    calls.append(c2)
+                f(c2, i + 1, depth + 1)
+
+    f(call, 0, 0)
+
+    return '\n'.join('%s(%s)%s' % (method, ', '.join(c), tail) for c in calls)
+
+
+def _get_test_sub_dir(name: str, name_to_sub_dir: Mapping[str, str]) -> str:
+    for prefix in sorted(name_to_sub_dir.keys(), key=len, reverse=True):
+        if name.startswith(prefix):
+            return name_to_sub_dir[prefix]
+    raise InvalidTestDefinitionError(
+        'Test "%s" has no defined target directory mapping' % name)
+
+
+def _expand_test_code(code: str) -> str:
+    code = re.sub(r'@nonfinite ([^(]+)\(([^)]+)\)(.*)', lambda m:
+                  _expand_nonfinite(m.group(1), m.group(2), m.group(3)),
+                  code)  # Must come before '@assert throws'.
+
+    code = re.sub(r'@assert pixel (\d+,\d+) == (\d+,\d+,\d+,\d+);',
+                  r'_assertPixel(canvas, \1, \2);', code)
+
+    code = re.sub(r'@assert pixel (\d+,\d+) ==~ (\d+,\d+,\d+,\d+);',
+                  r'_assertPixelApprox(canvas, \1, \2, 2);', code)
+
+    code = re.sub(r'@assert pixel (\d+,\d+) ==~ (\d+,\d+,\d+,\d+) \+/- (\d+);',
+                  r'_assertPixelApprox(canvas, \1, \2, \3);', code)
+
+    code = re.sub(r'@assert throws (\S+_ERR) (.*);',
+                  r'assert_throws_dom("\1", function() { \2; });', code)
+
+    code = re.sub(r'@assert throws (\S+Error) (.*);',
+                  r'assert_throws_js(\1, function() { \2; });', code)
+
+    code = re.sub(
+        r'@assert (.*) === (.*);', lambda m: '_assertSame(%s, %s, "%s", "%s");'
+        % (m.group(1), m.group(2), _escapeJS(m.group(1)), _escapeJS(m.group(2))
+           ), code)
+
+    code = re.sub(
+        r'@assert (.*) !== (.*);', lambda m:
+        '_assertDifferent(%s, %s, "%s", "%s");' % (m.group(1), m.group(
+            2), _escapeJS(m.group(1)), _escapeJS(m.group(2))), code)
+
+    code = re.sub(
+        r'@assert (.*) =~ (.*);', lambda m: 'assert_regexp_match(%s, %s);' % (
+            m.group(1), m.group(2)), code)
+
+    code = re.sub(
+        r'@assert (.*);', lambda m: '_assert(%s, "%s");' % (m.group(
+            1), _escapeJS(m.group(1))), code)
+
+    code = re.sub(r' @moz-todo', '', code)
+
+    code = re.sub(r'@moz-UniversalBrowserRead;', '', code)
+
+    assert ('@' not in code)
+
+    return code
+
+
+def _generate_test(test: Mapping[str, str], templates: Mapping[str, str],
+                   sub_dir: str, test_output_dir: str, image_output_dir: str,
+                   is_offscreen_canvas: bool):
+    name = test['name']
+
+    if test.get('expected', '') == 'green' and re.search(
+            r'@assert pixel .* 0,0,0,0;', test['code']):
+        print('Probable incorrect pixel test in %s' % name)
+
+    code = _expand_test_code(test['code'])
+
+    expectation_html = ''
+    if 'expected' in test and test['expected'] is not None:
+        expected = test['expected']
+        expected_img = None
+        if expected == 'green':
+            expected_img = '/images/green-100x50.png'
+        elif expected == 'clear':
+            expected_img = '/images/clear-100x50.png'
+        else:
+            if ';' in expected:
+                print('Found semicolon in %s' % name)
+            expected = re.sub(
+                r'^size (\d+) (\d+)',
+                r'surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, \1, \2)'
+                r'\ncr = cairo.Context(surface)', expected)
+
+            expected += ("\nsurface.write_to_png('%s.png')\n" %
+                         os.path.join(image_output_dir, sub_dir, name))
+            eval(compile(expected, '<test %s>' % name, 'exec'), {},
+                 {'cairo': cairo})
+            expected_img = '%s.png' % name
+
+        if expected_img:
+            expectation_html = (
+                '<p class="output expectedtext">Expected output:<p>'
+                '<img src="%s" class="output expected" id="expected" '
+                'alt="">' % expected_img)
+
+    canvas = test.get('canvas', 'width="100" height="50"')
+
+    notes = '<p class="notes">%s' % test['notes'] if 'notes' in test else ''
+
+    timeout = ('\n<meta name="timeout" content="%s">' %
+               test['timeout'] if 'timeout' in test else '')
+
+    scripts = ''
+    for s in test.get('scripts', []):
+        scripts += '<script src="%s"></script>\n' % (s)
+
+    images = ''
+    for src in test.get('images', []):
+        img_id = src.split('/')[-1]
+        if '/' not in src:
+            src = '../images/%s' % src
+        images += '<img src="%s" id="%s" class="resource">\n' % (src, img_id)
+    for src in test.get('svgimages', []):
+        img_id = src.split('/')[-1]
+        if '/' not in src:
+            src = '../images/%s' % src
+        images += ('<svg><image xlink:href="%s" id="%s" class="resource">'
+                   '</svg>\n' % (src, img_id))
+    images = images.replace('../images/', '/images/')
+
+    fonts = ''
+    fonthack = ''
+    for font in test.get('fonts', []):
+        fonts += ('@font-face {\n  font-family: %s;\n'
+                  '  src: url("/fonts/%s.ttf");\n}\n' % (font, font))
+        # Browsers require the font to actually be used in the page.
+        if test.get('fonthack', 1):
+            fonthack += ('<span style="font-family: %s; position: '
+                         'absolute; visibility: hidden">A</span>\n' % font)
+    if fonts:
+        fonts = '<style>\n%s</style>\n' % fonts
+
+    fallback = test.get('fallback',
+                        '<p class="fallback">FAIL (fallback content)</p>')
+
+    desc = test.get('desc', '')
+    escaped_desc = _simpleEscapeJS(desc)
+
+    attributes = test.get('attributes', '')
+    if attributes:
+        context_args = "'2d', %s" % attributes.strip()
+        attributes = ', ' + attributes.strip()
+    else:
+        context_args = "'2d'"
+
+    template_params = {
+        'name': name,
+        'desc': desc,
+        'escaped_desc': escaped_desc,
+        'notes': notes,
+        'images': images,
+        'fonts': fonts,
+        'fonthack': fonthack,
+        'timeout': timeout,
+        'canvas': canvas,
+        'expected': expectation_html,
+        'code': code,
+        'scripts': scripts,
+        'fallback': fallback,
+        'attributes': attributes,
+        'context_args': context_args
+    }
+
+    test_path = os.path.join(test_output_dir, sub_dir, name)
+    if 'manual' in test:
+        test_path += '-manual'
+
+    if is_offscreen_canvas:
+        f = codecs.open(f'{test_path}.html', 'w', 'utf-8')
+        f.write(templates['w3coffscreencanvas'] % template_params)
+        timeout = ('// META: timeout=%s\n' %
+                   test['timeout'] if 'timeout' in test else '')
+        template_params['timeout'] = timeout
+        f = codecs.open(f'{test_path}.worker.js', 'w', 'utf-8')
+        f.write(templates['w3cworker'] % template_params)
+    else:
+        f = codecs.open(f'{test_path}.html', 'w', 'utf-8')
+        f.write(templates['w3ccanvas'] % template_params)
+
+
 def genTestUtils(TESTOUTPUTDIR: str, IMAGEOUTPUTDIR: str, TEMPLATEFILE: str,
                  NAME2DIRFILE: str, ISOFFSCREENCANVAS: bool) -> None:
-    def simpleEscapeJS(string: str) -> str:
-        return string.replace('\\', '\\\\').replace('"', '\\"')
-
-    def escapeJS(string: str) -> str:
-        string = simpleEscapeJS(string)
-        # Kind of an ugly hack, for nicer failure-message output.
-        string = re.sub(r'\[(\w+)\]', r'[\\""+(\1)+"\\"]', string)
-        return string
-
-    def expand_nonfinite(method: str, argstr: str, tail: str) -> str:
-        """
-        >>> print expand_nonfinite('f', '<0 a>, <0 b>', ';')
-        f(a, 0);
-        f(0, b);
-        f(a, b);
-        >>> print expand_nonfinite('f', '<0 a>, <0 b c>, <0 d>', ';')
-        f(a, 0, 0);
-        f(0, b, 0);
-        f(0, c, 0);
-        f(0, 0, d);
-        f(a, b, 0);
-        f(a, b, d);
-        f(a, 0, d);
-        f(0, b, d);
-        """
-        # argstr is "<valid-1 invalid1-1 invalid2-1 ...>, ..." (where usually
-        # 'invalid' is Infinity/-Infinity/NaN).
-        args = []
-        for arg in argstr.split(', '):
-            match = re.match('<(.*)>', arg)
-            if match is None:
-                raise InvalidTestDefinitionError(
-                    f"Expected arg to match format '<(.*)>', but was: {arg}")
-            a = match.group(1)
-            args.append(a.split(' '))
-        calls = []
-        # Start with the valid argument list.
-        call = [args[j][0] for j in range(len(args))]
-        # For each argument alone, try setting it to all its invalid values:
-        for i in range(len(args)):
-            for a in args[i][1:]:
-                c2 = call[:]
-                c2[i] = a
-                calls.append(c2)
-        # For all combinations of >= 2 arguments, try setting them to their
-        # first invalid values. (Don't do all invalid values, because the
-        # number of combinations explodes.)
-        def f(c: List[str], start: int, depth: int) -> None:
-            for i in range(start, len(args)):
-                if len(args[i]) > 1:
-                    a = args[i][1]
-                    c2 = c[:]
-                    c2[i] = a
-                    if depth > 0:
-                        calls.append(c2)
-                    f(c2, i + 1, depth + 1)
-
-        f(call, 0, 0)
-
-        return '\n'.join('%s(%s)%s' % (method, ', '.join(c), tail)
-                         for c in calls)
-
     # Run with --test argument to run unit tests
     if len(sys.argv) > 1 and sys.argv[1] == '--test':
         doctest = importlib.import_module('doctest')
@@ -157,60 +338,6 @@ def genTestUtils(TESTOUTPUTDIR: str, IMAGEOUTPUTDIR: str, TEMPLATEFILE: str,
         except FileExistsError:
             pass  # Ignore if it already exists.
 
-    def get_test_sub_dir(name: str) -> str:
-        for prefix in sorted(name_to_sub_dir.keys(), key=len, reverse=True):
-            if name.startswith(prefix):
-                return name_to_sub_dir[prefix]
-        raise InvalidTestDefinitionError(
-            'Test "%s" has no defined target directory mapping' % name)
-
-    def expand_test_code(code: str) -> str:
-        code = re.sub(r'@nonfinite ([^(]+)\(([^)]+)\)(.*)', lambda m:
-                      expand_nonfinite(m.group(1), m.group(2), m.group(3)),
-                      code)  # Must come before '@assert throws'.
-
-        code = re.sub(r'@assert pixel (\d+,\d+) == (\d+,\d+,\d+,\d+);',
-                      r'_assertPixel(canvas, \1, \2);', code)
-
-        code = re.sub(r'@assert pixel (\d+,\d+) ==~ (\d+,\d+,\d+,\d+);',
-                      r'_assertPixelApprox(canvas, \1, \2, 2);', code)
-
-        code = re.sub(
-            r'@assert pixel (\d+,\d+) ==~ (\d+,\d+,\d+,\d+) \+/- (\d+);',
-            r'_assertPixelApprox(canvas, \1, \2, \3);', code)
-
-        code = re.sub(r'@assert throws (\S+_ERR) (.*);',
-                      r'assert_throws_dom("\1", function() { \2; });', code)
-
-        code = re.sub(r'@assert throws (\S+Error) (.*);',
-                      r'assert_throws_js(\1, function() { \2; });', code)
-
-        code = re.sub(
-            r'@assert (.*) === (.*);', lambda m:
-            '_assertSame(%s, %s, "%s", "%s");' % (m.group(1), m.group(
-                2), escapeJS(m.group(1)), escapeJS(m.group(2))), code)
-
-        code = re.sub(
-            r'@assert (.*) !== (.*);', lambda m:
-            '_assertDifferent(%s, %s, "%s", "%s");' % (m.group(1), m.group(
-                2), escapeJS(m.group(1)), escapeJS(m.group(2))), code)
-
-        code = re.sub(
-            r'@assert (.*) =~ (.*);', lambda m: 'assert_regexp_match(%s, %s);'
-            % (m.group(1), m.group(2)), code)
-
-        code = re.sub(
-            r'@assert (.*);', lambda m: '_assert(%s, "%s");' % (m.group(
-                1), escapeJS(m.group(1))), code)
-
-        code = re.sub(r' @moz-todo', '', code)
-
-        code = re.sub(r'@moz-UniversalBrowserRead;', '', code)
-
-        assert ('@' not in code)
-
-        return code
-
     used_tests = {}
     for test in tests:
         name = test['name']
@@ -220,125 +347,8 @@ def genTestUtils(TESTOUTPUTDIR: str, IMAGEOUTPUTDIR: str, TEMPLATEFILE: str,
             print('Test %s is defined twice' % name)
         used_tests[name] = 1
 
-        sub_dir = get_test_sub_dir(name)
-
-        if test.get('expected', '') == 'green' and re.search(
-                r'@assert pixel .* 0,0,0,0;', test['code']):
-            print('Probable incorrect pixel test in %s' % name)
-
-        code = expand_test_code(test['code'])
-
-        expectation_html = ''
-        if 'expected' in test and test['expected'] is not None:
-            expected = test['expected']
-            expected_img = None
-            if expected == 'green':
-                expected_img = '/images/green-100x50.png'
-            elif expected == 'clear':
-                expected_img = '/images/clear-100x50.png'
-            else:
-                if ';' in expected:
-                    print('Found semicolon in %s' % name)
-                expected = re.sub(
-                    r'^size (\d+) (\d+)',
-                    r'surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, \1, \2)'
-                    r'\ncr = cairo.Context(surface)', expected)
-
-                expected += ("\nsurface.write_to_png('%s.png')\n" %
-                             os.path.join(IMAGEOUTPUTDIR, sub_dir, name))
-                eval(compile(expected, '<test %s>' % name, 'exec'), {},
-                     {'cairo': cairo})
-                expected_img = '%s.png' % name
-
-            if expected_img:
-                expectation_html = (
-                    '<p class="output expectedtext">Expected output:<p>'
-                    '<img src="%s" class="output expected" id="expected" '
-                    'alt="">' % expected_img)
-
-        canvas = test.get('canvas', 'width="100" height="50"')
-
-        notes = '<p class="notes">%s' % test['notes'] if 'notes' in test else ''
-
-        timeout = ('\n<meta name="timeout" content="%s">' %
-                   test['timeout'] if 'timeout' in test else '')
-
-        scripts = ''
-        for s in test.get('scripts', []):
-            scripts += '<script src="%s"></script>\n' % (s)
-
-        images = ''
-        for src in test.get('images', []):
-            img_id = src.split('/')[-1]
-            if '/' not in src:
-                src = '../images/%s' % src
-            images += '<img src="%s" id="%s" class="resource">\n' % (src,
-                                                                     img_id)
-        for src in test.get('svgimages', []):
-            img_id = src.split('/')[-1]
-            if '/' not in src:
-                src = '../images/%s' % src
-            images += ('<svg><image xlink:href="%s" id="%s" class="resource">'
-                       '</svg>\n' % (src, img_id))
-        images = images.replace('../images/', '/images/')
-
-        fonts = ''
-        fonthack = ''
-        for font in test.get('fonts', []):
-            fonts += ('@font-face {\n  font-family: %s;\n'
-                      '  src: url("/fonts/%s.ttf");\n}\n' % (font, font))
-            # Browsers require the font to actually be used in the page.
-            if test.get('fonthack', 1):
-                fonthack += ('<span style="font-family: %s; position: '
-                             'absolute; visibility: hidden">A</span>\n' % font)
-        if fonts:
-            fonts = '<style>\n%s</style>\n' % fonts
-
-        fallback = test.get('fallback',
-                            '<p class="fallback">FAIL (fallback content)</p>')
-
-        desc = test.get('desc', '')
-        escaped_desc = simpleEscapeJS(desc)
-
-        attributes = test.get('attributes', '')
-        if attributes:
-            context_args = "'2d', %s" % attributes.strip()
-            attributes = ', ' + attributes.strip()
-        else:
-            context_args = "'2d'"
-
-        template_params = {
-            'name': name,
-            'desc': desc,
-            'escaped_desc': escaped_desc,
-            'notes': notes,
-            'images': images,
-            'fonts': fonts,
-            'fonthack': fonthack,
-            'timeout': timeout,
-            'canvas': canvas,
-            'expected': expectation_html,
-            'code': code,
-            'scripts': scripts,
-            'fallback': fallback,
-            'attributes': attributes,
-            'context_args': context_args
-        }
-
-        test_path = os.path.join(TESTOUTPUTDIR, sub_dir, name)
-        if 'manual' in test:
-            test_path += '-manual'
-
-        if ISOFFSCREENCANVAS:
-            f = codecs.open(f'{test_path}.html', 'w', 'utf-8')
-            f.write(templates['w3coffscreencanvas'] % template_params)
-            timeout = ('// META: timeout=%s\n' %
-                       test['timeout'] if 'timeout' in test else '')
-            template_params['timeout'] = timeout
-            f = codecs.open(f'{test_path}.worker.js', 'w', 'utf-8')
-            f.write(templates['w3cworker'] % template_params)
-        else:
-            f = codecs.open(f'{test_path}.html', 'w', 'utf-8')
-            f.write(templates['w3ccanvas'] % template_params)
+        sub_dir = _get_test_sub_dir(name, name_to_sub_dir)
+        _generate_test(test, templates, sub_dir, TESTOUTPUTDIR, IMAGEOUTPUTDIR,
+                       ISOFFSCREENCANVAS)
 
     print()
