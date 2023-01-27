6d20800b8ddfe05ceb8877f2757226169101127d
Sergey Ulanov <sergeyu@chromium.org>
2023-01-27 23:02:33 +0000

    Use IsLowEndDevice() to determine demuxer memory limits
    Previously low-end demuxer limits were hard-coded for Fuchsia, while all
    other platforms used default limit (except Android and CastOS). This
    change makes the default behavior to adjust the limits based on
    
    making it consistent with desktop platforms.
    
    base: :SysInfo::IsLowEndDevice(), and applies the same logic on Fuchsia,
    Change-Id: I324bdca9c684a86de14a593af18cef25425ce4fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200829
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098166}

--- media/base/demuxer_memory_limit_default.cc
+++ media/base/demuxer_memory_limit_default.cc
@@ -4,17 +4,23 @@
 
 #include "media/base/demuxer_memory_limit.h"
 
+#include "base/system/sys_info.h"
+
 namespace media {
 
 size_t GetDemuxerStreamAudioMemoryLimit(
     const AudioDecoderConfig* /*audio_config*/) {
-  return internal::kDemuxerStreamAudioMemoryLimitDefault;
+  return base::SysInfo::IsLowEndDevice()
+             ? internal::kDemuxerStreamAudioMemoryLimitLow
+             : internal::kDemuxerStreamAudioMemoryLimitDefault;
 }
 
 size_t GetDemuxerStreamVideoMemoryLimit(
     Demuxer::DemuxerTypes /*demuxer_type*/,
     const VideoDecoderConfig* /*video_config*/) {
-  return internal::kDemuxerStreamVideoMemoryLimitDefault;
+  return base::SysInfo::IsLowEndDevice()
+             ? internal::kDemuxerStreamVideoMemoryLimitLow
+             : internal::kDemuxerStreamVideoMemoryLimitDefault;
 }
 
 size_t GetDemuxerMemoryLimit(Demuxer::DemuxerTypes demuxer_type) {
