e7ecb1d080bedd53cf5419f79084361871e89365
Frank Liberato <liberato@chromium.org>
2023-01-27 18:42:36 +0000

    Disallow locking aspect ratio for Document Picture in Picture.
    This CL removes the `lockAspectRatio` option from blink.  It does
    not remove the plumbing in the browser side, to avoid a conflict
    with an ongoing refactor.  The browser side will be removed as
    part of a follow-up.
    
    Corresponding spec change:
    https://github.com/WICG/document-picture-in-picture/pull/20
    
    Bug: 1410379
    Change-Id: I3dcc62c1247b44b37490826c3fc03c825e828095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194966
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098016}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -6277,7 +6277,8 @@ WebView* RenderFrameImpl::CreateNewWindow(
     pip_mojom_opts->width = pip_options->width;
     pip_mojom_opts->height = pip_options->height;
     pip_mojom_opts->initial_aspect_ratio = pip_options->initial_aspect_ratio;
-    pip_mojom_opts->lock_aspect_ratio = pip_options->lock_aspect_ratio;
+    // TODO(crbug.com/1410379): Remove this from mojom and the browser side.
+    pip_mojom_opts->lock_aspect_ratio = false;
     params->pip_options = std::move(pip_mojom_opts);
   }
 
