190f210163a28a8214fab849baa19859aec65e90
Paul Moy <pmoy@chromium.org>
2023-01-27 21:48:48 +0000

    scanning: add service type unit tests
    Test that CreateSaneScanner returns a valid scanner for each supported
    service type.
    
    Bug: b:265954504
    Change-Id: I4897f30c691e5b99eccb9e351a0848413effa643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198460
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098124}

--- chrome/browser/ash/scanning/zeroconf_scanner_detector_utils_unittest.cc
+++ chrome/browser/ash/scanning/zeroconf_scanner_detector_utils_unittest.cc
@@ -15,6 +15,13 @@ namespace ash {
 
 namespace {
 
+// POD struct for CreateSaneScannerServiceTypeTest.
+struct CreateSaneScannerServiceTypeTestParams {
+  std::string service_type;
+  ScanProtocol protocol;
+  std::string device_name;
+};
+
 // Constructs a net::IPAddress from `str`. The input must be a valid IP format.
 net::IPAddress IpAddressFromString(const std::string& str) {
   net::IPAddress ip_addr;
@@ -23,6 +30,63 @@ net::IPAddress IpAddressFromString(const std::string& str) {
 }
 
 }  // namespace
+
+class CreateSaneScannerServiceTypeTest
+    : public testing::Test,
+      public testing::WithParamInterface<
+          CreateSaneScannerServiceTypeTestParams> {
+ public:
+  CreateSaneScannerServiceTypeTest() = default;
+  CreateSaneScannerServiceTypeTest(const CreateSaneScannerServiceTypeTest&) =
+      delete;
+  CreateSaneScannerServiceTypeTest& operator=(
+      const CreateSaneScannerServiceTypeTest&) = delete;
+
+  const CreateSaneScannerServiceTypeTestParams params() const {
+    return GetParam();
+  }
+};
+
+// Test that a scanner can be created for each supported service type.
+TEST_P(CreateSaneScannerServiceTypeTest, SupportedServiceType) {
+  net::IPAddress ip_address = IpAddressFromString("101.102.103.104");
+
+  absl::optional<Scanner> maybe_scanner = CreateSaneScanner(
+      "EPSON scanner", params().service_type, "eSCL", ip_address, 8080);
+
+  ASSERT_TRUE(maybe_scanner.has_value());
+  auto scanner = maybe_scanner.value();
+
+  EXPECT_EQ(scanner.display_name, "EPSON scanner");
+
+  auto device_names = scanner.device_names[params().protocol];
+  ASSERT_TRUE(device_names.size() > 0);
+  EXPECT_EQ(device_names.begin()->device_name, params().device_name);
+
+  ASSERT_TRUE(scanner.ip_addresses.size() > 0);
+  EXPECT_EQ(*scanner.ip_addresses.begin(), ip_address);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    CreateSaneScannerServiceTypeTest,
+    testing::Values(
+        CreateSaneScannerServiceTypeTestParams{
+            /*service_type=*/ZeroconfScannerDetector::kEsclServiceType,
+            /*protocol=*/ScanProtocol::kEscl,
+            /*device_name=*/
+            "airscan:escl:EPSON scanner:http://101.102.103.104:8080/eSCL/"},
+        CreateSaneScannerServiceTypeTestParams{
+            /*service_type=*/ZeroconfScannerDetector::kEsclsServiceType,
+            /*protocol=*/ScanProtocol::kEscls,
+            /*device_name=*/
+            "airscan:escl:EPSON scanner:https://101.102.103.104:8080/eSCL/"},
+        CreateSaneScannerServiceTypeTestParams{
+            /*service_type=*/ZeroconfScannerDetector::
+                kGenericScannerServiceType,
+            /*protocol=*/ScanProtocol::kLegacyNetwork,
+            /*device_name=*/
+            "epsonds:net:101.102.103.104"}));
 
 class CreateSaneScannerSlashTest
     : public testing::Test,
