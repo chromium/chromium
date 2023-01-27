ec7dcb11cf6c9f61c8f22dc74dfb9a11fc8240bf
Keybo Qian <keybo@google.com>
2023-01-27 18:38:56 +0000

    Add Chrome branch_day builder service account to the known bots list.
    Service account for automating the Chrome branch day task. For more details, please see the Monorail issue below.
    
    Bug: 1309662
    Change-Id: I75c23cd7de2f65b80a8a381e6d81e738924cc355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4198789
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Auto-Submit: Keybo Qian <keybo@google.com>
    Commit-Queue: Keybo Qian <keybo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1098013}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1624,7 +1624,7 @@ _KNOWN_ROBOTS = set(
                     'lacros-version-skew-roller', 'skylab-test-cros-roller',
                     'infra-try-recipes-tester', 'lacros-tracking-roller',
                     'lacros-sdk-version-roller', 'chrome-automated-expectation',
-                    'chromium-automated-expectation')
+                    'chromium-automated-expectation', 'chrome-branch-day')
   ) | set('%s@skia-public.iam.gserviceaccount.com' % s
           for s in ('chromium-autoroll', 'chromium-release-autoroll')
   ) | set('%s@skia-corp.google.com.iam.gserviceaccount.com' % s
