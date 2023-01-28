1087931c331ba26d098c7408f0a3a6bb75a248ce
Nancy Wang <nancylingwang@chromium.org>
2023-01-28 02:26:10 +0000

    Enable the kUnifiedAppServiceIconLoading flag.
    BUG=1380608
    
    Change-Id: I23182de2ebec5f769859a50097855d5ff28f49dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4135357
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098230}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -8,7 +8,7 @@ namespace apps {
 
 BASE_FEATURE(kUnifiedAppServiceIconLoading,
              "UnifiedAppServiceIconLoading",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAppServiceStorage,
              "AppServiceStorage",
