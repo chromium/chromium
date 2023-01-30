2e5ee120d7fc5df4b6c101e88d03a5f62a73d8b8
Mason Freed <masonf@chromium.org>
2023-01-30 18:40:27 +0000

    Update popover post-toggle event naming and behavior
    This CL updates the post-toggle event in the following ways:
     1. Rename the 'aftertoggle' event to 'toggle'.
     2. Rename PopoverToggleEvent to ToggleEvent.
     3. Rename the currentState attribute to oldState.
     4. Add event coalescing behavior. If two transitions occur before the
        first 'toggle' event has been fired, cancel the first event and
        queue a replacement that has oldState === newState.
    
    These changes were driven by the corresponding changes to the spec PR:
      https://github.com/whatwg/html/pull/8717
    
    Bug: 1307772
    Change-Id: Iabc5a9093d7cef3bbd6e54e488d8e571c51ea568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4195120
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1098728}

---
+++ third_party/blink/renderer/core/events/toggle_event.cc
@@ -0,0 +1,52 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/events/toggle_event.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/v8_toggle_event_init.h"
+#include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/event_interface_names.h"
+
+namespace blink {
+
+ToggleEvent::ToggleEvent() = default;
+
+ToggleEvent::ToggleEvent(const AtomicString& type,
+                         Event::Cancelable cancelable,
+                         const String& old_state,
+                         const String& new_state)
+    : Event(type, Bubbles::kNo, cancelable),
+      old_state_(old_state),
+      new_state_(new_state) {}
+
+ToggleEvent::ToggleEvent(const AtomicString& type,
+                         const ToggleEventInit* initializer)
+    : Event(type, initializer) {
+  if (initializer->hasOldState()) {
+    old_state_ = initializer->oldState();
+  }
+  if (initializer->hasNewState()) {
+    new_state_ = initializer->newState();
+  }
+}
+
+ToggleEvent::~ToggleEvent() = default;
+
+const String& ToggleEvent::oldState() const {
+  return old_state_;
+}
+
+const String& ToggleEvent::newState() const {
+  return new_state_;
+}
+
+const AtomicString& ToggleEvent::InterfaceName() const {
+  return event_interface_names::kToggleEvent;
+}
+
+void ToggleEvent::Trace(Visitor* visitor) const {
+  Event::Trace(visitor);
+}
+
+}  // namespace blink
