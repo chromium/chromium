1d6a179e99a961ad1beee18e8ff843b62273622f
Mihai Sardarescu <msarda@chromium.org>
2023-01-31 02:07:44 +0000

    [iOS] Remove checkbox_button as it is no longer used
    The welcome screen was redesigned and the checkbox button is no longer
    used. This CL removes it.
    
    The screenshot of the showcase UI after the removal of the checkbox:
    https://drive.google.com/file/d/193jn--P9V49EdZsRCcAWPmYmH8HxhaJO/view?usp=sharing
    
    Bug: 1407658
    
    Change-Id: I89af66c7fcb5faba58db048692cb26a8ea1760a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4202163
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099053}

--- ios/showcase/first_run/sc_first_run_default_screen_view_controller.mm
+++ ios/showcase/first_run/sc_first_run_default_screen_view_controller.mm
@@ -4,18 +4,11 @@
 
 #import "ios/showcase/first_run/sc_first_run_default_screen_view_controller.h"
 
-#import "ios/chrome/browser/ui/first_run/welcome/checkbox_button.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-@interface SCFirstRunDefaultScreenViewController ()
-
-@property(nonatomic, strong) CheckboxButton* checkboxButton;
-
-@end
 
 @implementation SCFirstRunDefaultScreenViewController
 
@@ -41,43 +34,7 @@
   label.translatesAutoresizingMaskIntoConstraints = NO;
   label.adjustsFontForContentSizeCategory = YES;
   [self.specificContentView addSubview:label];
-
-  self.checkboxButton = [[CheckboxButton alloc] initWithFrame:CGRectZero];
-  self.checkboxButton.translatesAutoresizingMaskIntoConstraints = NO;
-  self.checkboxButton.labelText =
-      @"This is a label explaining what the checkbox is for. Tap this to "
-      @"toggle the checkbox!";
-  self.checkboxButton.selected = YES;
-  [self.checkboxButton addTarget:self
-                          action:@selector(didTapCheckboxButton)
-                forControlEvents:UIControlEventTouchUpInside];
-  [self.specificContentView addSubview:self.checkboxButton];
-
-  [NSLayoutConstraint activateConstraints:@[
-    [label.topAnchor
-        constraintGreaterThanOrEqualToAnchor:self.specificContentView
-                                                 .topAnchor],
-    [label.centerXAnchor
-        constraintEqualToAnchor:self.specificContentView.centerXAnchor],
-    [label.widthAnchor
-        constraintLessThanOrEqualToAnchor:self.specificContentView.widthAnchor],
-
-    [self.checkboxButton.topAnchor
-        constraintGreaterThanOrEqualToAnchor:label.bottomAnchor
-                                    constant:16],
-    [self.checkboxButton.centerXAnchor
-        constraintEqualToAnchor:self.specificContentView.centerXAnchor],
-    [self.checkboxButton.widthAnchor
-        constraintEqualToAnchor:self.specificContentView.widthAnchor],
-    [self.checkboxButton.bottomAnchor
-        constraintEqualToAnchor:self.specificContentView.bottomAnchor],
-  ]];
-
   [super viewDidLoad];
-}
-
-- (void)didTapCheckboxButton {
-  self.checkboxButton.selected = !self.checkboxButton.selected;
 }
 
 @end
