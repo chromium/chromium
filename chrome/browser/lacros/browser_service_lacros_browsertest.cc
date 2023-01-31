89c45bdd58b9c9031047f199b4b82696db4b1659
Emmanuel Arias Soto <eariassoto@google.com>
2023-01-31 09:53:34 +0000

    code health: Replace base::RunLoop for base::test::TestFuture
    This change replaces the use of base::RunLoop to test async code with
    the special wrapper base::test::TestFuture to improve clarity
    without modifying the existing wait mechanism.
    
    Bug: b:266783937
    Test: ./lacros_chrome_browsertests_run_in_series --gtest_filter=BrowserServiceLacrosBrowserTest.*
    Change-Id: Ibedd29de60aa1bd32c7ef2069c2df937f0a74c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4200231
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1099151}

--- chrome/browser/lacros/browser_service_lacros_browsertest.cc
+++ chrome/browser/lacros/browser_service_lacros_browsertest.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/chromeos/app_mode/app_session.h"
 #include "chrome/browser/lacros/app_mode/kiosk_session_service_lacros.h"
 #include "chrome/browser/lacros/browser_service_lacros.h"
@@ -122,19 +122,21 @@ class BrowserServiceLacrosBrowserTest : public InProcessBrowserTest {
   }
 
   void NewWindowSync(bool incognito, bool should_trigger_session_restore) {
-    base::RunLoop run_loop;
+    base::test::TestFuture<void> new_window_future;
     browser_service()->NewWindow(
         incognito, should_trigger_session_restore,
         display::Screen::GetScreen()->GetDisplayForNewWindows().id(),
-        run_loop.QuitClosure());
-    run_loop.Run();
+        new_window_future.GetCallback());
+    ASSERT_TRUE(new_window_future.Wait())
+        << "NewWindow did not trigger the callback.";
   }
 
   void NewTabSync(bool should_trigger_session_restore) {
-    base::RunLoop run_loop;
+    base::test::TestFuture<void> new_tab_future;
     browser_service()->NewTab(should_trigger_session_restore,
-                              run_loop.QuitClosure());
-    run_loop.Run();
+                              new_tab_future.GetCallback());
+    ASSERT_TRUE(new_tab_future.Wait())
+        << "NewWindow did not trigger the callback.";
   }
 
   BrowserServiceLacros* browser_service() const {
@@ -394,10 +396,12 @@ IN_PROC_BROWSER_TEST_F(BrowserServiceLacrosWindowlessBrowserTest,
   EXPECT_EQ(0u, BrowserList::GetInstance()->size());
 
   // Trigger a new tab with session restore.
-  base::RunLoop run_loop;
-  testing::SessionsRestoredWaiter restore_waiter(run_loop.QuitClosure(), 1);
+  base::test::TestFuture<void> restore_waiter_future;
+  testing::SessionsRestoredWaiter restore_waiter(
+      restore_waiter_future.GetCallback(), 1);
   NewTabSync(/*should_trigger_session_restore=*/true);
-  run_loop.Run();
+  ASSERT_TRUE(restore_waiter_future.Wait())
+      << "restore_waiter did not trigger the callback.";
 
   EXPECT_EQ(1u, BrowserList::GetInstance()->size());
   auto* new_browser = chrome::FindBrowserWithProfile(profile);
@@ -495,14 +499,15 @@ IN_PROC_BROWSER_TEST_F(BrowserServiceLacrosNonSyncingProfilesBrowserTest,
   histogram_tester().ExpectTotalCount(
       "Profile.LacrosPrimaryProfileFirstRunEntryPoint", 0);
 
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> new_window_future;
   browser_service()->NewWindow(
       /*incognito=*/false, /*should_trigger_session_restore=*/false,
       display::Screen::GetScreen()->GetDisplayForNewWindows().id(),
-      /*callback=*/run_loop.QuitClosure());
+      /*callback=*/new_window_future.GetCallback());
   profiles::testing::CompleteLacrosFirstRun(LoginUIService::ABORT_SYNC);
 
-  run_loop.Run();
+  ASSERT_TRUE(new_window_future.Wait())
+      << "NewWindow did not trigger the callback.";
 
   EXPECT_EQ(1u, BrowserList::GetInstance()->size());
   histogram_tester().ExpectUniqueSample(
@@ -523,14 +528,15 @@ IN_PROC_BROWSER_TEST_F(BrowserServiceLacrosNonSyncingProfilesBrowserTest,
   histogram_tester().ExpectTotalCount(
       "Profile.LacrosPrimaryProfileFirstRunEntryPoint", 0);
 
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> new_window_future;
   browser_service()->NewWindow(
       /*incognito=*/false, /*should_trigger_session_restore=*/false,
       display::Screen::GetScreen()->GetDisplayForNewWindows().id(),
-      /*callback=*/run_loop.QuitClosure());
+      /*callback=*/new_window_future.GetCallback());
   profiles::testing::CompleteLacrosFirstRun(LoginUIService::UI_CLOSED);
 
-  run_loop.Run();
+  ASSERT_TRUE(new_window_future.Wait())
+      << "NewWindow did not trigger the callback.";
 
   EXPECT_EQ(0u, BrowserList::GetInstance()->size());
   histogram_tester().ExpectUniqueSample(
@@ -551,13 +557,13 @@ IN_PROC_BROWSER_TEST_F(BrowserServiceLacrosNonSyncingProfilesBrowserTest,
   histogram_tester().ExpectTotalCount(
       "Profile.LacrosPrimaryProfileFirstRunEntryPoint", 0);
 
-  base::RunLoop run_loop;
+  base::test::TestFuture<void> new_tab_future;
   browser_service()->NewTab(
       /*should_trigger_session_restore=*/false,
-      /*callback=*/run_loop.QuitClosure());
+      /*callback=*/new_tab_future.GetCallback());
   profiles::testing::CompleteLacrosFirstRun(LoginUIService::ABORT_SYNC);
 
-  run_loop.Run();
+  ASSERT_TRUE(new_tab_future.Wait()) << "NewTab did not trigger the callback.";
 
   EXPECT_EQ(1u, BrowserList::GetInstance()->size());
   histogram_tester().ExpectUniqueSample(
