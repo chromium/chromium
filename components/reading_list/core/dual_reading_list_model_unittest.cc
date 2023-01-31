1775e55e12481627d8735a5d9892e8d432db8c6c
Mahmoud Rashad <mmrashad@google.com>
2023-01-31 00:57:39 +0000

    [ReadingList] Implement DualReadingListModel::size()
    This CL implements DualReadingListModel::size() naively, the model hides
    behind kReadingListEnableDualReadingListModel which is disabled by
    default.
    No behavioral changes.
    
    Bug: 1402196
    Change-Id: I5ee54f0c31882f8a627202248f0c92f2ccd46b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4191744
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099018}

--- components/reading_list/core/dual_reading_list_model_unittest.cc
+++ components/reading_list/core/dual_reading_list_model_unittest.cc
@@ -7,6 +7,7 @@
 #include "base/test/simple_test_clock.h"
 #include "components/reading_list/core/fake_reading_list_model_storage.h"
 #include "components/reading_list/core/mock_reading_list_model_observer.h"
+#include "components/reading_list/core/reading_list_entry.h"
 #include "components/reading_list/core/reading_list_model_impl.h"
 #include "components/sync/base/storage_type.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,6 +31,7 @@ class DualReadingListModelTest : public testing::Test {
     auto local_or_syncable_model = std::make_unique<ReadingListModelImpl>(
         std::move(local_or_syncable_model_storage),
         syncer::StorageType::kUnspecified, &clock_);
+    local_or_syncable_model_ptr_ = local_or_syncable_model.get();
 
     auto account_model_storage =
         std::make_unique<FakeReadingListModelStorage>();
@@ -37,10 +39,34 @@ class DualReadingListModelTest : public testing::Test {
     auto account_model = std::make_unique<ReadingListModelImpl>(
         std::move(account_model_storage), syncer::StorageType::kUnspecified,
         &clock_);
+    account_model_ptr_ = account_model.get();
 
     dual_model_ = std::make_unique<reading_list::DualReadingListModel>(
         std::move(local_or_syncable_model), std::move(account_model));
     dual_model_->AddObserver(&observer_);
+  }
+
+  bool ResetStorageAndTriggerLoadCompletion(
+      const std::vector<GURL>& initial_local_urls = {},
+      const std::vector<GURL>& initial_account_urls = {}) {
+    ResetStorage();
+
+    std::vector<ReadingListEntry> initial_local_entries;
+    for (const auto& url : initial_local_urls) {
+      initial_local_entries.emplace_back(url, "Title for " + url.spec(),
+                                         clock_.Now());
+    }
+
+    std::vector<ReadingListEntry> initial_account_entries;
+    for (const auto& url : initial_account_urls) {
+      initial_account_entries.emplace_back(url, "Title for " + url.spec(),
+                                           clock_.Now());
+    }
+
+    return local_or_syncable_model_storage_ptr_->TriggerLoadCompletion(
+               std::move(initial_local_entries)) &&
+           account_model_storage_ptr_->TriggerLoadCompletion(
+               std::move(initial_account_entries));
   }
 
   size_t UnreadSize() {
@@ -72,6 +98,9 @@ class DualReadingListModelTest : public testing::Test {
   base::WeakPtr<FakeReadingListModelStorage>
       local_or_syncable_model_storage_ptr_;
   base::WeakPtr<FakeReadingListModelStorage> account_model_storage_ptr_;
+  // Owned by `dual_model_` and guaranteed to exist while `dual_model_` exists.
+  base::raw_ptr<const ReadingListModelImpl> local_or_syncable_model_ptr_;
+  base::raw_ptr<const ReadingListModelImpl> account_model_ptr_;
   std::unique_ptr<reading_list::DualReadingListModel> dual_model_;
 };
 
@@ -98,6 +127,30 @@ TEST_F(DualReadingListModelTest, ModelLoadFailure) {
       base::unexpected("Fake error")));
   ASSERT_TRUE(account_model_storage_ptr_->TriggerLoadCompletion());
   EXPECT_FALSE(dual_model_->loaded());
+}
+
+TEST_F(DualReadingListModelTest, ReturnAccountModelSize) {
+  ASSERT_TRUE(ResetStorageAndTriggerLoadCompletion(
+      /*initial_local_urls=*/{}, {GURL("https://url.com")}));
+  ASSERT_EQ(0ul, local_or_syncable_model_ptr_->size());
+  ASSERT_EQ(1ul, account_model_ptr_->size());
+  EXPECT_EQ(1ul, dual_model_->size());
+}
+
+TEST_F(DualReadingListModelTest, ReturnLocalModelSize) {
+  ASSERT_TRUE(ResetStorageAndTriggerLoadCompletion(
+      {GURL("https://url.com")}, /*initial_account_urls=*/{}));
+  ASSERT_EQ(1ul, local_or_syncable_model_ptr_->size());
+  ASSERT_EQ(0ul, account_model_ptr_->size());
+  EXPECT_EQ(1ul, dual_model_->size());
+}
+
+TEST_F(DualReadingListModelTest, ReturnKeysSize) {
+  ASSERT_TRUE(ResetStorageAndTriggerLoadCompletion({GURL("https://url1.com")},
+                                                   {GURL("https://url2.com")}));
+  ASSERT_EQ(1ul, local_or_syncable_model_ptr_->size());
+  ASSERT_EQ(1ul, account_model_ptr_->size());
+  EXPECT_EQ(2ul, dual_model_->size());
 }
 
 }  // namespace
