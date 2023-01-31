41acb85e32381a6761080461c2ad8ddf06fb06c0
Anna Tsvirchkova <atsvirchkova@google.com>
2023-01-31 12:29:08 +0000

    [TTFCC] Change chrome inforbar icon
    The change of the icon has been agreed with the UX Android team.
    Before/after icon: https://bugs.chromium.org/p/chromium/issues/detail?id=1247698#c73
    
    Bug: 1247698
    Change-Id: Iaaf193467a913150c39768aa89d605ff9eb156ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4194848
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099203}

--- chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
+++ chrome/browser/touch_to_fill/payments/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillCreditCardRenderTest.java
@@ -67,7 +67,7 @@ public class TouchToFillCreditCardRenderTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(4)
+                    .setRevision(5)
                     .setBugComponent(Component.UI_BROWSER_AUTOFILL)
                     .build();
 
