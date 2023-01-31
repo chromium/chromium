9ab12ebf912db397cafc9dc0aa675e504ae52fa0
Jeremy Roman <jbroman@chromium.org>
2023-01-31 16:16:05 +0000

    Remove non-standard JSON comment support from the trusttoken attribute.
    This feature is unlaunched and it seems unlikely that there is
    substantial use depending on comments.
    
    Bug: 1264024
    Change-Id: I87a66136b19aec522dfb61717734ce6338d21c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4206593
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099272}

--- third_party/blink/renderer/core/html/html_iframe_element.cc
+++ third_party/blink/renderer/core/html/html_iframe_element.cc
@@ -485,10 +485,9 @@ HTMLIFrameElement::ConstructTrustTokenParams() const {
   if (!trust_token_)
     return nullptr;
 
-  // TODO(crbug.com/1264024): Deprecate JSON comments here, if possible.
   JSONParseError parse_error;
   std::unique_ptr<JSONValue> parsed_attribute =
-      ParseJSONWithCommentsDeprecated(trust_token_, &parse_error);
+      ParseJSON(trust_token_, &parse_error);
   if (!parsed_attribute) {
     GetDocument().AddConsoleMessage(MakeGarbageCollected<ConsoleMessage>(
         mojom::blink::ConsoleMessageSource::kOther,
