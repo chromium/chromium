21f84c617ad57db20ee58d67a0914e69db759c5a
Frank Liberato <liberato@chromium.org>
2023-02-01 18:23:16 +0000

    Use content-PiP transition more often on Android.
    When a video is entirely on-screen when doing a transition into
    Picture-in-Picture, we sometimes elect to use the visually nicer
    "content PiP" transition based on a screen shot of the video
    before it enters PiP.  Otherwise, we use the standard grey
    transition from off-screen.
    
    However, some sites position videos so that they extend off of
    the screen area by a few pixels.  This CL makes us use the
    content PiP transition anyway; it still looks nicer than the
    grey one even if the screen shot is off by a few pixels.  In
    practice, it's hard to notice that it's not exact.
    
    Bug: 1411517
    Change-Id: I0d73d37b239f5a51d59c0d3d43ce41e823cba8e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4205199
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099951}

--- chrome/browser/ui/android/overlay/overlay_window_android.cc
+++ chrome/browser/ui/android/overlay/overlay_window_android.cc
@@ -38,27 +38,48 @@ OverlayWindowAndroid::OverlayWindowAndroid(
 
   auto* web_contents = controller_->GetWebContents();
 
+  // Compute the screen position of the video, and see if it fits inside the
+  // WebContents or if it's clipped / off-screen.  If it's onscreen, then T and
+  // later, Android can do a nicer animated transition to PiP with a screen
+  // capture of the video.  However, if the video is clipped / offscreen, then
+  // it'll look nicer to use the default light grey transition.
+
+  // We provide a small buffer for what "clipped" means, rather than enforcing
+  // it strictly.  It'll still look fine while allowing small positioning errors
+  // that sites sometimes make.  See https://crbug.com/1411517 for an example.
+
+  // The java side will ignore any source bounds that are not on the screen for
+  // the source rect hint. It will use the aspect ratio only in that case.  We
+  // set the x position to be <0 to ensure this, to skip the transition.
+
+  // Get the size of the video, and inset it to provide some slack.
   gfx::Rect source_bounds = controller_->GetSourceBounds();
+  gfx::Rect smaller_source_bounds = source_bounds;
+  constexpr int inset_size = 4;  // pixels on each side
+  smaller_source_bounds.Inset(inset_size);
+
+  // Get the size of the WebContents, and convert to pixels.
   gfx::Rect unscaled_content_bounds = web_contents->GetContainerBounds();
   auto* native_view = web_contents->GetNativeView();
-  // The java side will ignore any source bounds that are not on the screen for
-  // the source rect hint. It will use the aspect ratio only in that case.  If
-  // it's not entirely on-screen, then skip it.
   const float dip_scale = native_view->GetDipScale();
   gfx::Rect content_bounds(unscaled_content_bounds.x() * dip_scale,
                            unscaled_content_bounds.y() * dip_scale,
                            unscaled_content_bounds.width() * dip_scale,
                            unscaled_content_bounds.height() * dip_scale);
-  const bool out_of_bounds = !content_bounds.Contains(source_bounds);
+  const bool out_of_bounds = !content_bounds.Contains(smaller_source_bounds);
 
   if (!out_of_bounds) {
-    // Convert to screen space.
+    // Use the newer transition, if available.
+    // Convert to screen space.  Since the comparison was with the inset source
+    // bounds, clamp the real source bounds to the container.
+    source_bounds.Intersect(content_bounds);
     gfx::PointF offset = native_view->GetLocationOnScreen(0, 0);
     source_bounds.Offset(
         static_cast<int>(offset.x()),
         static_cast<int>(offset.y()) +
             native_view->content_offset() * native_view->GetDipScale());
   } else {
+    // Use the old transition.
     // Slide this offscreen, while keeping the aspect ratio the same.
     source_bounds.set_x(-1);
   }
