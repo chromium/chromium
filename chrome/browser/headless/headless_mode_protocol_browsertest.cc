c1ccb66209203ff709815beb70a1851f25b75268
Peter Kvitek <kvitekp@chromium.org>
2023-02-01 07:26:05 +0000

    [headless] Added protocol test to verify CDP Page.startScreencast
    In headless mode browser platform windows are not visible, so if compositor detects this, it may decide to optimize frames away.
    
    This CL adds a test to ensure that screen cast works in new headless
    mode.
    
    Change-Id: Ib8f602095b29b224b43fdbe11bc18cf4be5dfdc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4210941
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099696}

--- chrome/browser/headless/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/headless_mode_protocol_browsertest.cc
@@ -10,6 +10,7 @@
 #include "base/json/json_writer.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/path_service.h"
+#include "build/build_config.h"
 #include "chrome/browser/headless/test/headless_browser_test_utils.h"
 #include "content/public/common/content_switches.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -198,5 +199,14 @@ void HeadlessModeProtocolBrowserTest::OnConsoleAPICalled(
 HEADLESS_MODE_PROTOCOL_TEST(DomFocus, "input/dom-focus.js")
 HEADLESS_MODE_PROTOCOL_TEST(FocusBlurNotifications,
                             "input/focus-blur-notifications.js")
+
+// https://crbug.com/1411976
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ScreencastBasics DISABLED_ScreencastBasics
+#else
+#define MAYBE_ScreencastBasics ScreencastBasics
+#endif
+HEADLESS_MODE_PROTOCOL_TEST(MAYBE_ScreencastBasics,
+                            "sanity/screencast-basics.js")
 
 }  // namespace headless
