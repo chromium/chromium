903a05e6e50ac344b63245c4524a96daae17aa03
Ewann Pelle <ewannpv@chromium.org>
2023-02-01 17:51:06 +0000

    [iOS][PT] Moves feature helpers in browser/tabs
    This CL moves Pinned Tabs helpers in browser/tabs.
    This allows to use those helpers outside the browser/ui folder.
    
    Bug: 1382015
    Change-Id: Icbe083a08281530e8c7c59b5f94ec17432caba90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208860
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099935}

--- ios/chrome/browser/tabs/features.mm
+++ ios/chrome/browser/tabs/features.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/pinned_tabs/features.h"
+#import "ios/chrome/browser/tabs/features.h"
 
 #import "base/mac/foundation_util.h"
 #import "base/metrics/field_trial_params.h"
