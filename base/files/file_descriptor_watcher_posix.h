7f4e8df39fcba4a0739a443dac5b05f0372a3ff9
Kalvin Lee <kdlee@chromium.org>
2023-02-01 14:01:36 +0000

    MTECheckedPtr: Unmark `Watcher` member
    Without the degradation template, MessagePumpForIOOnOtherThread/
    FileDescriptorWatcherTest.DeleteControllerAfterDeleteMessagePumpForIO/0
    (from base_unittests) fails. I believe this is not an issue.
    
    The dangling `watcher_` is only subject to the crashing extraction via
    `~Controller`, bound to the posted `delete_task`. This task, with a
    dangling `Watcher*`, is posted but never runs, as per the comment in
    `Watcher::WillDestroyCurrentMessageLoop()`. This can be verified by
    binding `nullptr` to the `delete_task` instead and `CHECK(watcher)`
    inside the same, which doesn't fire in this test.
    
    Bug: 1349261
    Change-Id: I6dbc255bccd509a78736dfa4fc86441b628ae5b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4211893
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099810}

--- base/files/file_descriptor_watcher_posix.h
+++ base/files/file_descriptor_watcher_posix.h
@@ -75,9 +75,7 @@ class BASE_EXPORT FileDescriptorWatcher {
     // Controller is deleted, ownership of |watcher_| is transfered to a delete
     // task posted to the MessageLoopForIO. This ensures that |watcher_| isn't
     // deleted while it is being used by the MessageLoopForIO.
-    //
-    // TODO(crbug.com/1298696): Breaks base_unittests.
-    raw_ptr<Watcher, DanglingUntriagedDegradeToNoOpWhenMTE> watcher_;
+    raw_ptr<Watcher, DanglingUntriaged> watcher_;
 
     // An event for the watcher to notify controller that it's destroyed.
     // As the |watcher_| is owned by Controller, always outlives the Watcher.
