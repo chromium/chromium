ee4329092e2f4bcb6bf25ddacbee6ec9f7987d10
Paul Semel <paulsemel@chromium.org>
2023-02-01 12:37:33 +0000

    DanglingPtr: triage unretained dangling ptr
    Doc:
    https://docs.google.com/document/d/1YQCh_mezoSF1fWS9TaMEh0Whumb8eNe0KWV8MBs7sFs
    
    We detected those callbacks are invoked using dangling pointers.
    This is something we would like to detect and potentially ban.
    
    In the meantime, we need to annotate them using:
    base::UnsafeDanglingUntriaged
    
    Outside of the experiment, it is a no-op.
    Within the experiment, it skips DumpWithoutCrashing.
    
    Bug: 1291138,1412054
    Change-Id: I8da9ae4b6f6e87e388bf593966be9e9b1076e6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208836
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1099780}

--- media/capture/video/win/video_capture_device_factory_win.cc
+++ media/capture/video/win/video_capture_device_factory_win.cc
@@ -712,9 +712,10 @@ void VideoCaptureDeviceFactoryWin::FoundAllDevicesUWP(
   FindAndSetDefaultVideoCamera(&devices_info);
 
   origin_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&VideoCaptureDeviceFactoryWin::DeviceInfoReady,
-                                base::Unretained(this), std::move(devices_info),
-                                std::move(result_callback)));
+      FROM_HERE,
+      base::BindOnce(&VideoCaptureDeviceFactoryWin::DeviceInfoReady,
+                     base::UnsafeDanglingUntriaged(this),
+                     std::move(devices_info), std::move(result_callback)));
 
   auto it = async_ops_.find(operation);
   DCHECK(it != async_ops_.end());
