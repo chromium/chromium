dda88a69f8472b9910606f0518288f77ba8b487c
Hira Mahmood <hiramahmood@google.com>
2023-02-02 21:39:01 +0000

    Reland "[iOS] Update consumer through CredentialProviderPromoMediator"
    This is a reland of commit 6259376a274c890ac3fa55bb1c782c0ae3e9131f
    
    The original CL was reverted because of the Lottie dependency
    (crbug.com/1412342). This reland removes the Lottie dependencies
    because it wasn’t needed in this CL and left unintentionally.
    
    Original change's description:
    > [iOS] Update consumer through CredentialProviderPromoMediator
    >
    > This change implements the CredentialProviderPromoMediator class to
    > set the state of the consumer through the consumer protocol.
    >
    > Bug: 1402963
    > Change-Id: I2bd30e4be80fc14b66d45a907b6f69e625780de4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4154425
    > Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1100142}
    
    Bug: 1402963
    Change-Id: I4822d7508f85ca0eb8da9ec8a43ebd90deab5446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215821
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100648}

--- ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_mediator.mm
+++ ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_mediator.mm
@@ -4,13 +4,24 @@
 
 #import "ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_mediator.h"
 
+#import "base/files/file_path.h"
+#import "base/path_service.h"
 #import "components/password_manager/core/browser/password_manager_util.h"
 #import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/credential_provider_promo/features.h"
+#import "ios/chrome/browser/ui/credential_provider_promo/credential_provider_promo_constants.h"
+#import "ios/chrome/grit/ios_google_chrome_strings.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ios/public/provider/chrome/browser/branded_images/branded_images_api.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+NSString* const kFirstStepAnimation = @"CPE_promo_animation_edu";
+}  // namespace
 
 @interface CredentialProviderPromoMediator ()
 
@@ -42,7 +53,77 @@
 }
 
 - (void)configureConsumerWithTrigger:(CredentialProviderPromoTrigger)trigger {
-  // TODO(crbug.com/1392116): configure view controller
+  CredentialProviderPromoSource source;
+  CredentialProviderPromoContext context =
+      CredentialProviderPromoContext::kFirstStep;
+  switch (trigger) {
+    case CredentialProviderPromoTrigger::PasswordCopied:
+      source = CredentialProviderPromoSource::kPasswordCopied;
+      [self setAnimationWithContext:context];
+      break;
+    case CredentialProviderPromoTrigger::PasswordSaved:
+      source = CredentialProviderPromoSource::kPasswordSaved;
+      break;
+    case CredentialProviderPromoTrigger::SuccessfulLoginUsingExistingPassword:
+      source = CredentialProviderPromoSource::kAutofillUsed;
+      break;
+    case CredentialProviderPromoTrigger::RemindMeLater:
+      source = CredentialProviderPromoSource::kRemindLaterSelected;
+      break;
+  }
+
+  [self setTextAndImageWithContext:context source:source];
+}
+
+#pragma mark - Private
+
+// Sets the animation to the consumer that corresponds to the value of
+// `context`. Depending on the value of `context`, either the 'first step' or
+// 'learn more' animation path is set.
+- (void)setAnimationWithContext:(CredentialProviderPromoContext)context {
+  NSString* animationName;
+  if (context == CredentialProviderPromoContext::kFirstStep) {
+    animationName = kFirstStepAnimation;
+  } else {
+    // TODO(crbug.com/1392116): set animation for
+    // CredentialProviderPromoContext::kLearnMore.
+    animationName = kFirstStepAnimation;
+  }
+  NSString* path = [[NSBundle mainBundle] pathForResource:animationName
+                                                   ofType:@"json"];
+  [self.consumer setAnimation:path];
+}
+
+// Sets the text and image to the consumer. The text set depends on the value of
+// `context`. When `source` is kPasswordCopied, no image is set.
+- (void)setTextAndImageWithContext:(CredentialProviderPromoContext)context
+                            source:(CredentialProviderPromoSource)source {
+  if (context == CredentialProviderPromoContext::kFirstStep) {
+    NSString* titleString =
+        l10n_util::GetNSString(IDS_IOS_CREDENTIAL_PROVIDER_PROMO_INITIAL_TITLE);
+    NSString* subtitleString = l10n_util::GetNSString(
+        IDS_IOS_CREDENTIAL_PROVIDER_PROMO_INITIAL_SUBTITLE);
+    NSString* primaryActionString =
+        l10n_util::GetNSString(IDS_IOS_CREDENTIAL_PROVIDER_PROMO_LEARN_HOW);
+    NSString* secondaryActionString =
+        l10n_util::GetNSString(IDS_IOS_CREDENTIAL_PROVIDER_PROMO_NO_THANKS);
+    NSString* tertiaryActionString = l10n_util::GetNSString(
+        IDS_IOS_CREDENTIAL_PROVIDER_PROMO_REMIND_ME_LATER);
+    UIImage* image = ios::provider::GetBrandedImage(
+        ios::provider::BrandedImage::kPasswordSuggestionKey);
+    if (source == CredentialProviderPromoSource::kPasswordCopied) {
+      image = nil;
+    }
+    [self.consumer setTitleString:titleString
+                   subtitleString:subtitleString
+              primaryActionString:primaryActionString
+            secondaryActionString:secondaryActionString
+             tertiaryActionString:tertiaryActionString
+                            image:image];
+  } else {
+    // TODO(crbug.com/1392116): configure for
+    // CredentialProviderPromoContext::kLearnMore.
+  }
 }
 
 @end
