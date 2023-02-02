8ce6563e55208c68a7e18d3001a70876dafc4856
Tim Sergeant <tsergeant@chromium.org>
2023-02-02 03:23:18 +0000

    APS: Add integration tests for multiple apps and retrying failures
    Bug: b/265357926
    Change-Id: Ic54880f8c7d7e1ba9aabf42c68cd7b81b304fffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4215852
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100254}

--- chrome/browser/apps/app_preload_service/app_preload_service_browsertest.cc
+++ chrome/browser/apps/app_preload_service/app_preload_service_browsertest.cc
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <map>
 #include <string>
 
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/apps/app_preload_service/app_preload_service.h"
@@ -25,6 +27,10 @@
 #include "net/dns/mock_host_resolver.h"
 
 namespace apps {
+
+namespace {
+constexpr char kDefaultManifestUrl[] = "/manifest.json";
+}
 
 class AppPreloadServiceBrowserTest : public InProcessBrowserTest {
  public:
@@ -55,14 +61,13 @@ class AppPreloadServiceBrowserTest : public InProcessBrowserTest {
 
   std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
       const net::test_server::HttpRequest& request) {
-    if (request.relative_url == "/manifest.json" && !manifest_.empty()) {
+    if (manifest_responses_.contains(request.relative_url)) {
       auto response = std::make_unique<net::test_server::BasicHttpResponse>();
       response->set_code(net::HTTP_OK);
       response->set_content_type("application/json");
-      response->set_content(manifest_);
+      response->set_content(manifest_responses_[request.relative_url]);
       return response;
     }
-
     if (request.relative_url == "/v1/app_provisioning/apps?alt=proto" &&
         apps_proto_.has_value()) {
       auto response = std::make_unique<net::test_server::BasicHttpResponse>();
@@ -89,7 +94,12 @@ class AppPreloadServiceBrowserTest : public InProcessBrowserTest {
                                            nullptr);
   }
 
-  void SetManifestResponse(std::string manifest) { manifest_ = manifest; }
+  // Sets the test server response for `relative_url` to the given JSON
+  // `manifest` string.
+  void SetManifestResponse(const std::string& relative_url,
+                           const std::string& manifest) {
+    manifest_responses_[relative_url] = manifest;
+  }
 
   void SetAppProvisioningResponse(
       proto::AppProvisioningListAppsResponse response) {
@@ -108,7 +118,7 @@ class AppPreloadServiceBrowserTest : public InProcessBrowserTest {
  private:
   base::test::ScopedFeatureList feature_list_;
   net::EmbeddedTestServer https_server_;
-  std::string manifest_;
+  std::map<std::string, std::string> manifest_responses_;
   absl::optional<proto::AppProvisioningListAppsResponse> apps_proto_;
 };
 
@@ -121,17 +131,19 @@ IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, OemWebAppInstall) {
       proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
 
   app->mutable_web_extras()->set_manifest_url(
-      https_server()->GetURL("/manifest.json").spec());
+      https_server()->GetURL(kDefaultManifestUrl).spec());
   app->mutable_web_extras()->set_original_manifest_url(
       "https://www.example.com/");
 
-  SetAppProvisioningResponse(response);
-  SetManifestResponse(AddIconToManifest(R"({
+  const std::string kManifest = AddIconToManifest(R"({
     "id": "id",
     "name": "Example App",
     "start_url": "/index.html",
     "icons": $1
-  })"));
+  })");
+
+  SetAppProvisioningResponse(response);
+  SetManifestResponse(kDefaultManifestUrl, kManifest);
 
   base::test::TestFuture<bool> result;
   auto* service = AppPreloadService::Get(profile());
@@ -158,7 +170,7 @@ IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, IgnoreDefaultAppInstall) {
       proto::AppProvisioningListAppsResponse::INSTALL_REASON_DEFAULT);
 
   app->mutable_web_extras()->set_manifest_url(
-      https_server()->GetURL("/manifest.json").spec());
+      https_server()->GetURL(kDefaultManifestUrl).spec());
   app->mutable_web_extras()->set_original_manifest_url(
       "https://peanuttypes.com/app");
 
@@ -184,12 +196,12 @@ IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, InstallOverUserApp) {
   constexpr char kOriginalManifestUrl[] =
       "https://www.example.com/manifest.json";
   constexpr char kUserAppName[] = "User Installed App";
-  constexpr char kManifest[] = R"({
+  const std::string kManifest = AddIconToManifest(R"({
     "id": "manifest_id",
     "name": "OEM Installed app",
     "start_url": "/",
     "icons": $1
-  })";
+  })");
 
   auto app_id = web_app::test::InstallDummyWebApp(profile(), kUserAppName,
                                                   GURL(kResolvedManifestId));
@@ -202,11 +214,11 @@ IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, InstallOverUserApp) {
   app->set_install_reason(
       proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
   app->mutable_web_extras()->set_manifest_url(
-      https_server()->GetURL("/manifest.json").spec());
+      https_server()->GetURL(kDefaultManifestUrl).spec());
   app->mutable_web_extras()->set_original_manifest_url(kOriginalManifestUrl);
 
   SetAppProvisioningResponse(response);
-  SetManifestResponse(AddIconToManifest(kManifest));
+  SetManifestResponse(kDefaultManifestUrl, kManifest);
 
   base::test::TestFuture<bool> result;
   auto* service = AppPreloadService::Get(profile());
@@ -218,6 +230,144 @@ IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, InstallOverUserApp) {
                    .ForOneApp(app_id, [](const AppUpdate& update) {
                      EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
                    });
+  ASSERT_TRUE(found);
+}
+
+// Verifies that multiple OEM apps can be installed at once.
+IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, InstallMultipleOemApps) {
+  constexpr char kOriginalManifestUrl1[] = "https://www.foo.com/manifest.json";
+  constexpr char kOriginalManifestUrl2[] = "https://www.bar.com/manifest.json";
+
+  proto::AppProvisioningListAppsResponse response;
+  auto* app1 = response.add_apps_to_install();
+
+  app1->set_name("Foo");
+  app1->set_package_id("web:https://www.foo.com/");
+  app1->set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+  app1->mutable_web_extras()->set_manifest_url(
+      https_server()->GetURL("/manifest/foo.json").spec());
+  app1->mutable_web_extras()->set_original_manifest_url(kOriginalManifestUrl1);
+
+  const std::string kManifest1 = AddIconToManifest(R"({
+    "name": "Foo",
+    "start_url": "/",
+    "icons": $1
+  })");
+
+  SetManifestResponse("/manifest/foo.json", kManifest1);
+
+  auto* app2 = response.add_apps_to_install();
+
+  app2->set_name("Bar");
+  app2->set_package_id("web:https://www.bar.com/");
+  app2->set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+  app2->mutable_web_extras()->set_manifest_url(
+      https_server()->GetURL("/manifest/bar.json").spec());
+  app2->mutable_web_extras()->set_original_manifest_url(kOriginalManifestUrl2);
+
+  const std::string kManifest2 = AddIconToManifest(R"({
+    "name": "Bar",
+    "start_url": "/",
+    "icons": $1
+  })");
+
+  SetManifestResponse("/manifest/bar.json", kManifest2);
+
+  SetAppProvisioningResponse(response);
+
+  base::test::TestFuture<bool> result;
+  auto* service = AppPreloadService::Get(profile());
+  service->StartFirstLoginFlowForTesting(result.GetCallback());
+  ASSERT_TRUE(result.Get());
+
+  auto app_id1 =
+      web_app::GenerateAppId(absl::nullopt, GURL("https://www.foo.com/"));
+  bool found =
+      app_registry_cache().ForOneApp(app_id1, [](const AppUpdate& update) {
+        EXPECT_EQ(update.Name(), "Foo");
+        EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
+      });
+  ASSERT_TRUE(found);
+
+  auto app_id2 =
+      web_app::GenerateAppId(absl::nullopt, GURL("https://www.bar.com/"));
+  found = app_registry_cache().ForOneApp(app_id2, [](const AppUpdate& update) {
+    EXPECT_EQ(update.Name(), "Bar");
+    EXPECT_EQ(update.InstallReason(), InstallReason::kOem);
+  });
+  ASSERT_TRUE(found);
+}
+
+// Verifies that failed installations are retried on the next login flow, and
+// already installed apps are ignored.
+IN_PROC_BROWSER_TEST_F(AppPreloadServiceBrowserTest, RetryFailedApps) {
+  constexpr char kOriginalManifestUrl1[] = "https://www.foo.com/manifest.json";
+  constexpr char kOriginalManifestUrl2[] = "https://www.bar.com/manifest.json";
+
+  proto::AppProvisioningListAppsResponse response;
+  auto* app1 = response.add_apps_to_install();
+
+  app1->set_name("Foo");
+  app1->set_package_id("web:https://www.foo.com/");
+  app1->set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+  app1->mutable_web_extras()->set_manifest_url(
+      https_server()->GetURL("/manifest/foo.json").spec());
+  app1->mutable_web_extras()->set_original_manifest_url(kOriginalManifestUrl1);
+
+  const std::string kManifest1 = AddIconToManifest(R"({
+    "name": "Foo",
+    "start_url": "/",
+    "icons": $1
+  })");
+
+  auto* app2 = response.add_apps_to_install();
+
+  app2->set_name("Bar");
+  app2->set_package_id("web:https://www.bar.com/");
+  app2->set_install_reason(
+      proto::AppProvisioningListAppsResponse::INSTALL_REASON_OEM);
+  app2->mutable_web_extras()->set_manifest_url(
+      https_server()->GetURL("/manifest/bar.json").spec());
+  app2->mutable_web_extras()->set_original_manifest_url(kOriginalManifestUrl2);
+
+  const std::string kManifest2 = AddIconToManifest(R"({
+    "name": "Bar",
+    "start_url": "/",
+    "icons": $1
+  })");
+
+  SetAppProvisioningResponse(response);
+
+  // foo.json installs successfully but bar.json gives an error.
+  SetManifestResponse("/manifest/foo.json", kManifest1);
+  SetManifestResponse("/manifest/bar.json", "");
+
+  base::test::TestFuture<bool> result;
+  auto* service = AppPreloadService::Get(profile());
+  service->StartFirstLoginFlowForTesting(result.GetCallback());
+  ASSERT_FALSE(result.Get());
+
+  // bar.json should be retried, and will now succeed. foo.json is skipped
+  // (ignoring the error it would give), and so the whole flow is successful.
+  SetManifestResponse("/manifest/foo.json", "");
+  SetManifestResponse("/manifest/bar.json", kManifest2);
+
+  base::test::TestFuture<bool> result2;
+  service->StartFirstLoginFlowForTesting(result2.GetCallback());
+  ASSERT_TRUE(result2.Get());
+
+  // Both apps should now be installed.
+  auto app_id1 =
+      web_app::GenerateAppId(absl::nullopt, GURL("https://www.foo.com/"));
+  bool found = app_registry_cache().ForOneApp(app_id1, [](const AppUpdate&) {});
+  ASSERT_TRUE(found);
+
+  auto app_id2 =
+      web_app::GenerateAppId(absl::nullopt, GURL("https://www.bar.com/"));
+  found = app_registry_cache().ForOneApp(app_id2, [](const AppUpdate&) {});
   ASSERT_TRUE(found);
 }
 
