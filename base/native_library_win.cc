e279161ddcea78accf98e1e07b1956dbcc0430e5
David Bienvenu <davidbienvenu@chromium.org>
2023-02-03 15:56:45 +0000

    win7dep: use LOAD_LIBRARY_SEARCH_SYSTEM32 unconditionally
    Now that Chrome no longer supports Win7, we can use
    LOAD_LIBRARY_SEARCH_SYSTEM32 unconditionally.
    
    Bug: 1408307
    Change-Id: Iee9abacc88c0cfe53d5fdaf9f612df7540f47b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216613
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100997}

--- base/native_library_win.cc
+++ base/native_library_win.cc
@@ -34,11 +34,11 @@ enum LoadLibraryResult {
   // LoadLibraryW is used but fails as well.
   FAIL_AND_FAIL,
   // LoadLibraryExW API/flags are unavailabe to use, then LoadLibraryW is used
-  // and succeeds.
-  UNAVAILABLE_AND_SUCCEED,
+  // and succeeds. Pre-Win10-only.
+  UNAVAILABLE_AND_SUCCEED_OBSOLETE,
   // LoadLibraryExW API/flags are unavailabe to use, then LoadLibraryW is used
-  // but fails.
-  UNAVAILABLE_AND_FAIL,
+  // but fails.  Pre-Win10-only.
+  UNAVAILABLE_AND_FAIL_OBSOLETE,
   // Add new items before this one, always keep this one at the end.
   END
 };
@@ -49,39 +49,11 @@ void LogLibrarayLoadResultToUMA(LoadLibraryResult result) {
                             LoadLibraryResult::END);
 }
 
-// A helper method to check if AddDllDirectory method is available, thus
-// LOAD_LIBRARY_SEARCH_* flags are available on systems.
-bool AreSearchFlagsAvailable() {
-  // The LOAD_LIBRARY_SEARCH_* flags are available on systems that have
-  // KB2533623 installed. To determine whether the flags are available, use
-  // GetProcAddress to get the address of the AddDllDirectory,
-  // RemoveDllDirectory, or SetDefaultDllDirectories function. If GetProcAddress
-  // succeeds, the LOAD_LIBRARY_SEARCH_* flags can be used with LoadLibraryEx.
-  // https://msdn.microsoft.com/en-us/library/windows/desktop/ms684179(v=vs.85).aspx
-  // The LOAD_LIBRARY_SEARCH_* flags are used in the LoadNativeLibraryHelper
-  // method.
-  static const auto add_dll_dir_func =
-      reinterpret_cast<decltype(::AddDllDirectory)*>(
-          GetProcAddress(GetModuleHandle(L"kernel32.dll"), "AddDllDirectory"));
-  return !!add_dll_dir_func;
-}
-
 // A helper method to encode the library loading result to enum
 // LoadLibraryResult.
-LoadLibraryResult GetLoadLibraryResult(bool are_search_flags_available,
-                                       bool has_load_library_succeeded) {
-  LoadLibraryResult result;
-  if (are_search_flags_available) {
-    if (has_load_library_succeeded)
-      result = LoadLibraryResult::FAIL_AND_SUCCEED;
-    else
-      result = LoadLibraryResult::FAIL_AND_FAIL;
-  } else if (has_load_library_succeeded) {
-    result = LoadLibraryResult::UNAVAILABLE_AND_SUCCEED;
-  } else {
-    result = LoadLibraryResult::UNAVAILABLE_AND_FAIL;
-  }
-  return result;
+LoadLibraryResult GetLoadLibraryResult(bool has_load_library_succeeded) {
+  return has_load_library_succeeded ? LoadLibraryResult::FAIL_AND_SUCCEED
+                                    : LoadLibraryResult::FAIL_AND_FAIL;
 }
 
 NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
@@ -96,28 +68,26 @@ NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
   // while it holds the LoaderLock.
   SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY_REPEATEDLY();
 
-  HMODULE module = nullptr;
+  HMODULE module_handle = nullptr;
 
   // This variable records the library loading result.
   LoadLibraryResult load_library_result = LoadLibraryResult::SUCCEED;
 
-  bool are_search_flags_available = AreSearchFlagsAvailable();
-  if (are_search_flags_available) {
-    // LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR flag is needed to search the library
-    // directory as the library may have dependencies on DLLs in this
-    // directory.
-    module = ::LoadLibraryExW(
-        library_path.value().c_str(), nullptr,
-        LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR | LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
-    // If LoadLibraryExW succeeds, log this metric and return.
-    if (module) {
-      LogLibrarayLoadResultToUMA(load_library_result);
-      return module;
-    }
-    // GetLastError() needs to be called immediately after
-    // LoadLibraryExW call.
-    if (error)
-      error->code = ::GetLastError();
+  // LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR flag is needed to search the library
+  // directory as the library may have dependencies on DLLs in this
+  // directory.
+  module_handle = ::LoadLibraryExW(
+      library_path.value().c_str(), nullptr,
+      LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR | LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+  // If LoadLibraryExW succeeds, log this metric and return.
+  if (module_handle) {
+    LogLibrarayLoadResultToUMA(load_library_result);
+    return module_handle;
+  }
+  // GetLastError() needs to be called immediately after
+  // LoadLibraryExW call.
+  if (error) {
+    error->code = ::GetLastError();
   }
 
   // If LoadLibraryExW API/flags are unavailable or API call fails, try
@@ -134,20 +104,20 @@ NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
       restore_directory = true;
     }
   }
-  module = ::LoadLibraryW(library_path.value().c_str());
+  module_handle = ::LoadLibraryW(library_path.value().c_str());
 
   // GetLastError() needs to be called immediately after LoadLibraryW call.
-  if (!module && error)
+  if (!module_handle && error) {
     error->code = ::GetLastError();
+  }
 
   if (restore_directory)
     SetCurrentDirectory(current_directory);
 
   // Get the library loading result and log it to UMA.
-  LogLibrarayLoadResultToUMA(
-      GetLoadLibraryResult(are_search_flags_available, !!module));
+  LogLibrarayLoadResultToUMA(GetLoadLibraryResult(!!module_handle));
 
-  return module;
+  return module_handle;
 }
 
 NativeLibrary LoadSystemLibraryHelper(const FilePath& library_path,
@@ -159,17 +129,13 @@ NativeLibrary LoadSystemLibraryHelper(const FilePath& library_path,
   BOOL module_found =
       ::GetModuleHandleExW(0, library_path.value().c_str(), &module);
   if (!module_found) {
-    bool are_search_flags_available = AreSearchFlagsAvailable();
-    // Prefer LOAD_LIBRARY_SEARCH_SYSTEM32 to avoid DLL preloading attacks.
-    DWORD flags = are_search_flags_available ? LOAD_LIBRARY_SEARCH_SYSTEM32
-                                             : LOAD_WITH_ALTERED_SEARCH_PATH;
-    module = ::LoadLibraryExW(library_path.value().c_str(), nullptr, flags);
+    module = ::LoadLibraryExW(library_path.value().c_str(), nullptr,
+                              LOAD_LIBRARY_SEARCH_SYSTEM32);
 
     if (!module && error)
       error->code = ::GetLastError();
 
-    LogLibrarayLoadResultToUMA(
-        GetLoadLibraryResult(are_search_flags_available, !!module));
+    LogLibrarayLoadResultToUMA(GetLoadLibraryResult(!!module));
   }
 
   return module;
