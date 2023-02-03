8ad7b6850aa010564820cef6259f66178712eb60
Christian Xu <christianxu@google.com>
2023-02-03 16:36:00 +0000

    [iOS] Disable ToggleAllowChromeSigninWithPromoSigninClearData egtest
    Test is failing on multiple devices.
    The view is blocked by the "what's new" promo
    
    Bug: 1412776
    Change-Id: I5667cd16c8a8db7d95779b76dc1563b0464e72d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221693
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Owners-Override: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101014}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_egtest.mm
@@ -189,7 +189,9 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 // Tests that disabling the "Allow Chrome sign-in" > "Clear Data" option blocks
 // the user from signing in to Chrome through the promo sign-in until it is
 // re-enabled.
-- (void)testToggleAllowChromeSigninWithPromoSigninClearData {
+// TODO(crbug.com/1412776): Test fails due to the view being blocked by the
+// what's new promo.
+- (void)DISABLED_testToggleAllowChromeSigninWithPromoSigninClearData {
   // User is signed-in and syncing.
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
