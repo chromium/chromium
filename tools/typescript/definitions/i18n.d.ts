1cd23265b19341758ea7223a912e248dd3b20677
Jacob Francis <francisjp@google.com>
2023-02-03 18:36:17 +0000

    Typescript: Created definition files for chrome.storage/i18n APIs
    - Created storage_mv2.d.ts which adds definitions to be used by
      manifest v2 extensions, which can't use the Promise-based extension
      APIs unlike manifest v3 extensions or other WebUI code.
    
    - Created definitions for chrome.i18n
    
    This CL is in preparation for chrome accessibility extensions
    switch to Typescript.
    
    Bug: b/267512305
    Change-Id: I4451e4044018f1312753ce00b65de70e220975d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214652
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101101}

---
+++ tools/typescript/definitions/i18n.d.ts
@@ -0,0 +1,30 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Definitions for chrome.i18n API */
+// TODO(crbug.com/1203307): Auto-generate this file.
+declare namespace chrome {
+  export namespace i18n {
+
+    export function getAcceptLanguages(callback: (languages: string[]) => void):
+        void;
+
+    export function getMessage(
+        messageName: string, args?: string|string[],
+        options?: {escapeLt: boolean}): string;
+
+    export function getUILanguage(): string;
+
+    interface DetectLanguageResult {
+      isReliable: boolean;
+      languages: Array<{
+        language: string,
+        percentage: number,
+      }>;
+    }
+
+    export function detectLanguage(
+        text: string, callback: (result: DetectLanguageResult) => void): void;
+  }
+}
