90550f0e34f0be499f6a0b4455560065681f4fe1
jhtin <jhtin@chromium.org>
2023-02-03 03:41:23 +0000

    English India with Rupee: Add config for IME and settings
    This uses the in(eng) xkb keyboard, which is the US keyboard with a
    a Rupee symbol on Altgr-4.
    
    On the decoder side cl/501734586 will handle the virtual keyboard code.
    
    go/tscreen/a7dfbdc669259be69a0fbd248042fe83bf2bbcb6
    
    Bug: b/265087163
    Change-Id: Id32c7fd82d909f650b3a3ed6bffce0b8d68a7ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4151357
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1100824}

--- chrome/browser/resources/settings/chromeos/os_languages_page/input_method_settings.ts
+++ chrome/browser/resources/settings/chromeos/os_languages_page/input_method_settings.ts
@@ -130,6 +130,7 @@ export function getInputMethodSettings(
     'xkb:us::msa': [SettingsType.BASIC_SETTINGS],
 
     // ENGLISH_BASIC_WITH_AUTOSHIFT_SETTINGS
+    'xkb:in::eng': [SettingsType.ENGLISH_BASIC_WITH_AUTOSHIFT_SETTINGS],
     'xkb:pk::eng': [SettingsType.ENGLISH_BASIC_WITH_AUTOSHIFT_SETTINGS],
     'xkb:za:gb:eng': [SettingsType.ENGLISH_BASIC_WITH_AUTOSHIFT_SETTINGS],
   };
