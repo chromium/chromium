336ffbbef9f6eec46cb1083aa9088e5b435d0b2a
Petro Akzhygitov <pakzhygitov@google.com>
2023-02-03 11:58:12 +0000

    Added logging of new tab-related metrics.
    – Number of duplicate tabs
    – Number of old/inactive tabs
    – The age of all open tabs
    
    (cherry picked from commit cc1c077ed6dced89cc6f7c29f810bc1cf79276a0)
    
    Bug: 1411663 (b/262494271)
    Change-Id: I8f6062217fae0a7218b08487321c8fe0b3a863ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4066012
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1099181}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217551
    Cr-Commit-Position: refs/branch-heads/5563@{#138}
    Cr-Branched-From: 3ac59a6729cdb287a7ee629a0004c907ec1b06dc-refs/heads/main@{#1097615}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -9,6 +9,7 @@
 
 #import "base/functional/bind.h"
 #import "base/metrics/histogram_functions.h"
+#import "base/metrics/histogram_macros.h"
 #import "base/metrics/user_metrics_action.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/task/thread_pool.h"
@@ -96,6 +97,19 @@ enum class ColdStartType : int {
   // Unknown device restore and Chrome upgrade.
   kUnknownDeviceRestoreAndChromeUpgrade = 6,
   kMaxValue = kUnknownDeviceRestoreAndChromeUpgrade,
+};
+
+// Enum representing the existing set of all open tabs age scenarios. Current
+// values should not be renumbered. Please keep in sync with
+// "IOSAllOpenTabsAge" in src/tools/metrics/histograms/enums.xml.
+enum class TabsAgeGroup {
+  kLessThanOneDay = 0,
+  kOneToThreeDays = 1,
+  kThreeToSevenDays = 2,
+  kSevenToFourteenDays = 3,
+  kFourteenToThirtyDays = 4,
+  kMoreThanThirtyDays = 5,
+  kMaxValue = kMoreThanThirtyDays,
 };
 
 // Returns time delta since app launch as retrieved from kernel info about
@@ -299,6 +313,17 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
 // testing.
 + (void)recordNumLiveNTPTabAtResume:(int)numTabs;
 
+// Logs the number of old (inactive for more than 7 days) tabs with
+// UMAHistogramCount100 and allows testing.
++ (void)recordNumOldTabAtStartup:(int)numTabs;
+// Logs the number of duplicated tabs with UMAHistogramCount100 and allows
+// testing.
++ (void)recordNumDuplicatedTabAtStartup:(int)numTabs;
+// Logs the age (time elapsed since creation) of each tab  and allows testing.
++ (void)recordTabsAgeAtStartup:(const std::vector<base::TimeDelta>&)tabsAge;
+// Returns a corresponding TabAgeGroup for provided `timeSinceCreation` time.
++ (TabsAgeGroup)tabsAgeGroupFromTimeSinceCreation:
+    (base::TimeDelta)timeSinceCreation;
 @end
 
 @implementation MetricsMediator
@@ -376,6 +401,12 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
   int numTabs = 0;
   int numNTPTabs = 0;
   int numLiveNTPTabs = 0;
+  int numOldTabs = 0;
+  int numDuplicatedTabs = 0;
+
+  NSMutableSet* uniqueURLs = [NSMutableSet set];
+  std::vector<base::TimeDelta> timesSinceCreation;
+
   for (SceneState* scene in scenes) {
     if (!scene.interfaceProvider) {
       // The scene might not yet be initiated.
@@ -384,19 +415,50 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
       continue;
     }
 
-    const WebStateList* web_state_list =
+    const WebStateList* webStateList =
         scene.interfaceProvider.mainInterface.browser->GetWebStateList();
-    numTabs += web_state_list->count();
-    for (int i = 0; i < web_state_list->count(); i++) {
-      if (IsURLNewTabPage(web_state_list->GetWebStateAt(i)->GetVisibleURL())) {
+    numTabs += webStateList->count();
+
+    const base::Time now = base::Time::Now();
+    const int webStateListCount = webStateList->count();
+
+    for (int i = 0; i < webStateListCount; i++) {
+      web::WebState* webState = webStateList->GetWebStateAt(i);
+      const bool wasWebStateRealized = webState->IsRealized();
+      const GURL& URL = webState->GetVisibleURL();
+
+      // Count NTPs.
+      if (IsURLNewTabPage(URL)) {
         numNTPTabs++;
       }
+
+      // Count duplicate URLs.
+      NSString* URLString = base::SysUTF8ToNSString(URL.GetWithoutRef().spec());
+      if ([uniqueURLs containsObject:URLString]) {
+        numDuplicatedTabs++;
+      } else {
+        [uniqueURLs addObject:URLString];
+      }
+
+      // Count old (e.g. more than 7 days inactive) tabs.
+      if (now - webState->GetLastActiveTime() > base::Days(7)) {
+        numOldTabs++;
+      }
+
+      // Calculate the age (time elapsed since creation) of WebState.
+      base::TimeDelta timeSinceCreation = now - webState->GetCreationTime();
+      timesSinceCreation.push_back(timeSinceCreation);
+
+      DCHECK_EQ(wasWebStateRealized, webState->IsRealized());
     }
   }
 
   if (startupInformation.isColdStart) {
     [self recordNumTabAtStartup:numTabs];
     [self recordNumNTPTabAtStartup:numNTPTabs];
+    [self recordNumOldTabAtStartup:numOldTabs];
+    [self recordNumDuplicatedTabAtStartup:numDuplicatedTabs];
+    [self recordTabsAgeAtStartup:timesSinceCreation];
   } else {
     [self recordNumTabAtResume:numTabs];
     [self recordNumNTPTabAtResume:numNTPTabs];
@@ -609,6 +671,47 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
 
 + (void)recordNumLiveNTPTabAtResume:(int)numTabs {
   base::UmaHistogramCounts100("Tabs.LiveNTPCountAtResume", numTabs);
+}
+
++ (void)recordNumOldTabAtStartup:(int)numTabs {
+  base::UmaHistogramCounts100("Tabs.UnusedCountAtStartup", numTabs);
+}
+
++ (void)recordNumDuplicatedTabAtStartup:(int)numTabs {
+  base::UmaHistogramCounts100("Tabs.DuplicatesCountAtStartup", numTabs);
+}
+
++ (void)recordTabsAgeAtStartup:(const std::vector<base::TimeDelta>&)tabsAge {
+  for (const auto timeSinceCreation : tabsAge) {
+    TabsAgeGroup tabsAgeGroup =
+        [self tabsAgeGroupFromTimeSinceCreation:timeSinceCreation];
+    UMA_HISTOGRAM_ENUMERATION("Tabs.TimeSinceCreationAtStartup", tabsAgeGroup);
+  }
+}
+
++ (TabsAgeGroup)tabsAgeGroupFromTimeSinceCreation:
+    (base::TimeDelta)timeSinceCreation {
+  if (timeSinceCreation < base::Days(1)) {
+    return TabsAgeGroup::kLessThanOneDay;
+  }
+
+  if (timeSinceCreation < base::Days(3)) {
+    return TabsAgeGroup::kOneToThreeDays;
+  }
+
+  if (timeSinceCreation < base::Days(7)) {
+    return TabsAgeGroup::kThreeToSevenDays;
+  }
+
+  if (timeSinceCreation < base::Days(14)) {
+    return TabsAgeGroup::kSevenToFourteenDays;
+  }
+
+  if (timeSinceCreation < base::Days(30)) {
+    return TabsAgeGroup::kFourteenToThirtyDays;
+  }
+
+  return TabsAgeGroup::kMoreThanThirtyDays;
 }
 
 @end
