71fcacb0637bc0fb457262a0f34c80dcb2128f12
Caroline Rising <corising@chromium.org>
2023-02-06 19:39:22 +0000

    Notes: update icon used in side panel dropdown menu.
    The filled icon is being used, UX instead prefers the outline.
    
    Bug: 1409894
    Change-Id: I5fe95aedcb7bf7e404553247012499e0223509e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225217
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101720}

--- chrome/browser/ui/views/side_panel/user_note/user_note_ui_coordinator.cc
+++ chrome/browser/ui/views/side_panel/user_note/user_note_ui_coordinator.cc
@@ -30,7 +30,7 @@ void UserNoteUICoordinator::CreateAndRegisterEntry(
   global_registry->Register(std::make_unique<SidePanelEntry>(
       SidePanelEntry::Id::kUserNote,
       l10n_util::GetStringUTF16(IDS_USER_NOTE_TITLE),
-      ui::ImageModel::FromVectorIcon(kNoteIcon, ui::kColorIcon,
+      ui::ImageModel::FromVectorIcon(kNoteOutlineIcon, ui::kColorIcon,
                                      /*icon_size=*/16),
       base::BindRepeating(&UserNoteUICoordinator::CreateUserNotesWebUIView,
                           base::Unretained(this))));
