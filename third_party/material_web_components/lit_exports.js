ffc5ec2e694eef0df5ad02a0a4bbc402c487df2d
Zain Afzal <zafzal@google.com>
2023-02-06 06:25:17 +0000

    Reland "Bundle lit resources."
    This reverts commit cfc0940c4c7cef2317301af0fa39ac43f455eb8c.
    
    "Bundle lit resources" was originally landed in crrev/4113089 but caused
    various build problems. This CL relands this cl with the following
    changes to address build failures:
    
    * Adds missing deps in third_party/material_web_components:library.
    * Ensures files_manager can build with optimize_webui = true. Turns out
    that optimize_webui doesn't output it's files to target_gen_dir but
    rather outputs them to the same directory as the inputs. Previously this
    was partially overridable with the out_files argument but this argument
    has since been removed and the file will only be available aside the
    input file. Updated file paths to match.
    
    Bug: b/229804752
    Change-Id: I122af25be0a685fabf1e773b2170c2d9992406d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221121
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1101460}

---
+++ third_party/material_web_components/lit_exports.js
@@ -0,0 +1,24 @@
+/** Simple file to export all lit resources we care about for easy bundling. */
+
+export * from "./components-chromium/node_modules/lit/index.js";
+export * from "./components-chromium/node_modules/lit/decorators.js";
+export * from "./components-chromium/node_modules/lit/directives/async-append.js";
+export * from "./components-chromium/node_modules/lit/directives/async-replace.js";
+export * from "./components-chromium/node_modules/lit/directives/cache.js";
+export * from "./components-chromium/node_modules/lit/directives/choose.js";
+export * from "./components-chromium/node_modules/lit/directives/class-map.js";
+export * from "./components-chromium/node_modules/lit/directives/guard.js";
+export * from "./components-chromium/node_modules/lit/directives/if-defined.js";
+export * from "./components-chromium/node_modules/lit/directives/join.js";
+export * from "./components-chromium/node_modules/lit/directives/keyed.js";
+export * from "./components-chromium/node_modules/lit/directives/live.js";
+export * from "./components-chromium/node_modules/lit/directives/map.js";
+export * from "./components-chromium/node_modules/lit/directives/range.js";
+export * from "./components-chromium/node_modules/lit/directives/ref.js";
+export * from "./components-chromium/node_modules/lit/directives/repeat.js";
+export * from "./components-chromium/node_modules/lit/directives/style-map.js";
+export * from "./components-chromium/node_modules/lit/directives/template-content.js";
+export * from "./components-chromium/node_modules/lit/directives/unsafe-html.js";
+export * from "./components-chromium/node_modules/lit/directives/unsafe-svg.js";
+export * from "./components-chromium/node_modules/lit/directives/until.js";
+export * from "./components-chromium/node_modules/lit/directives/when.js";
