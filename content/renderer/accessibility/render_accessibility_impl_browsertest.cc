9aac4462f11504dec3b022fb0982967903e91e99
Kent Tamura <tkent@chromium.org>
2023-02-06 06:19:14 +0000

    DeferredShaping: Disable the feature in all tests
    Bug: 1259085
    Change-Id: I6d00438b9dd44305b670125915dd2e07077f67d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4220915
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101458}

--- content/renderer/accessibility/render_accessibility_impl_browsertest.cc
+++ content/renderer/accessibility/render_accessibility_impl_browsertest.cc
@@ -300,10 +300,6 @@ class RenderAccessibilityImplTest : public RenderViewTest {
     blink::WebRuntimeFeatures::EnableAccessibilityExposeHTMLElement(true);
 
     RenderViewTest::SetUp();
-    // TestBoundsFor*FixedNodeAfterScroll doesn't work with DeferredShaping,
-    // which triggers layout of shaping-deferred elements on scrollTo().
-    blink::WebRuntimeFeatures::EnableFeatureFromString("DeferredShaping",
-                                                       false);
 
     sink_ = &render_thread_->sink();
 
