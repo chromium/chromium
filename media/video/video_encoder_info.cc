b206753b43d1f752da25bfa086e5fcb832447fe7
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2023-02-06 21:50:41 +0000

    Let compiler generate operator==() for VideoEncoderInfo
    While adding new fields to VideoEncoderInfo in
    99532354a68f3c514274ad54e112363d519144d4 it became apparent that it's
    easy for operator==() to go out of sync with the set of fields. Having
    the compiler generate it avoids the issue.
    
    Let's also do this for ResolutionBitrateLimit while we're here.
    
    Bug: 1383643
    Change-Id: I0f937e0bc59aae57c8c85cc465fa9636f5e238ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4120178
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101797}

--- media/video/video_encoder_info.cc
+++ media/video/video_encoder_info.cc
@@ -23,31 +23,4 @@ VideoEncoderInfo::VideoEncoderInfo() = default;
 VideoEncoderInfo::VideoEncoderInfo(const VideoEncoderInfo&) = default;
 VideoEncoderInfo::~VideoEncoderInfo() = default;
 
-bool operator==(const ResolutionBitrateLimit& l,
-                const ResolutionBitrateLimit& r) {
-  return l.frame_size == r.frame_size &&
-         l.min_start_bitrate_bps == r.min_start_bitrate_bps &&
-         l.min_bitrate_bps == r.min_bitrate_bps &&
-         l.max_bitrate_bps == r.max_bitrate_bps;
-}
-
-bool operator==(const VideoEncoderInfo& l, const VideoEncoderInfo& r) {
-  for (size_t i = 0; i < VideoEncoderInfo::kMaxSpatialLayers; ++i) {
-    if (l.fps_allocation[i] != r.fps_allocation[i])
-      return false;
-  }
-
-  return l.implementation_name == r.implementation_name &&
-         l.frame_delay == r.frame_delay &&
-         l.input_capacity == r.input_capacity &&
-         l.supports_native_handle == r.supports_native_handle &&
-         l.has_trusted_rate_controller == r.has_trusted_rate_controller &&
-         l.is_hardware_accelerated == r.is_hardware_accelerated &&
-         l.supports_simulcast == r.supports_simulcast &&
-         l.reports_average_qp == r.reports_average_qp &&
-         l.requested_resolution_alignment == r.requested_resolution_alignment &&
-         l.apply_alignment_to_all_simulcast_layers ==
-             r.apply_alignment_to_all_simulcast_layers &&
-         l.resolution_bitrate_limits == r.resolution_bitrate_limits;
-}
 }  // namespace media
--- media/video/video_encoder_info.h
+++ media/video/video_encoder_info.h
@@ -6,6 +6,7 @@
 #define MEDIA_VIDEO_VIDEO_ENCODER_INFO_H_
 
 #include <stdint.h>
+#include <array>
 #include <string>
 #include <vector>
 
@@ -27,6 +28,9 @@ struct MEDIA_EXPORT ResolutionBitrateLimit {
                          int max_bitrate_bps);
   ~ResolutionBitrateLimit();
 
+  friend bool operator==(const ResolutionBitrateLimit&,
+                         const ResolutionBitrateLimit&) = default;
+
   gfx::Size frame_size;
   int min_start_bitrate_bps = 0;
   int min_bitrate_bps = 0;
@@ -39,6 +43,9 @@ struct MEDIA_EXPORT VideoEncoderInfo {
   VideoEncoderInfo();
   VideoEncoderInfo(const VideoEncoderInfo&);
   ~VideoEncoderInfo();
+
+  friend bool operator==(const VideoEncoderInfo&,
+                         const VideoEncoderInfo&) = default;
 
   std::string implementation_name;
 
@@ -66,14 +73,10 @@ struct MEDIA_EXPORT VideoEncoderInfo {
   uint32_t requested_resolution_alignment = 1;
   bool apply_alignment_to_all_simulcast_layers = false;
 
-  std::vector<uint8_t> fps_allocation[kMaxSpatialLayers];
+  std::array<std::vector<uint8_t>, kMaxSpatialLayers> fps_allocation;
   std::vector<ResolutionBitrateLimit> resolution_bitrate_limits;
 };
 
-MEDIA_EXPORT bool operator==(const ResolutionBitrateLimit& l,
-                             const ResolutionBitrateLimit& r);
-MEDIA_EXPORT bool operator==(const VideoEncoderInfo& l,
-                             const VideoEncoderInfo& r);
 }  // namespace media
 
 #endif  // MEDIA_VIDEO_VIDEO_ENCODER_INFO_H_
