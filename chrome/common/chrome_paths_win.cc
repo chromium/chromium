ee3e07d733fc5694049a2aa88dc434b4c38d1d08
David Bienvenu <davidbienvenu@chromium.org>
2023-02-06 16:32:33 +0000

    win7dep: Call SHGetKnownFolderPath directly
    Since Win7 is no longer supported, we can call SHGetKnownFolderPath
    directly.
    
    Bug: 1408307
    Change-Id: Iddb3e4831de6c23a91e6ae283d7eeea871ab9694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4221497
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101616}

--- chrome/common/chrome_paths_win.cc
+++ chrome/common/chrome_paths_win.cc
@@ -32,8 +32,8 @@ bool GetUserDirectory(int csidl_folder, base::FilePath* result) {
   // so we don't bother handling it.
   wchar_t path_buf[MAX_PATH];
   path_buf[0] = 0;
-  if (FAILED(SHGetFolderPath(NULL, csidl_folder, NULL,
-                             SHGFP_TYPE_CURRENT, path_buf))) {
+  if (FAILED(::SHGetFolderPath(nullptr, csidl_folder, nullptr,
+                               SHGFP_TYPE_CURRENT, path_buf))) {
     return false;
   }
   *result = base::FilePath(path_buf);
@@ -80,16 +80,13 @@ bool GetUserDownloadsDirectorySafe(base::FilePath* result) {
   return true;
 }
 
-// On Vista and higher, use the downloads known folder. Since it can be
-// relocated to point to a "dangerous" folder, callers should validate that the
-// returned path is not dangerous before using it.
+// Get the downloads known folder. Since it can be relocated to point to a
+// "dangerous" folder, callers should validate that the returned path is not
+// dangerous before using it.
 bool GetUserDownloadsDirectory(base::FilePath* result) {
-  typedef HRESULT (WINAPI *GetKnownFolderPath)(
-      REFKNOWNFOLDERID, DWORD, HANDLE, PWSTR*);
-  GetKnownFolderPath f = reinterpret_cast<GetKnownFolderPath>(
-      GetProcAddress(GetModuleHandle(L"shell32.dll"), "SHGetKnownFolderPath"));
   base::win::ScopedCoMem<wchar_t> path_buf;
-  if (f && SUCCEEDED(f(FOLDERID_Downloads, 0, NULL, &path_buf))) {
+  if (SUCCEEDED(
+          ::SHGetKnownFolderPath(FOLDERID_Downloads, 0, nullptr, &path_buf))) {
     *result = base::FilePath(std::wstring(path_buf));
     return true;
   }
