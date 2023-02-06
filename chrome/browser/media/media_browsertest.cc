38e88b54eceb7cbc73967b23cc81cf2502747f14
Fr <beaufort.francois@gmail.com>
2023-02-06 20:39:28 +0000

    Revert "Reland "Reject getDisplayMedia calls without user activation""
    This reverts commit cdf104c2c9bec928fe578719836cab8d5815759e.
    
    Reason for revert: This change breaks websites
    
    Original change's description:
    > Reland "Reject getDisplayMedia calls without user activation"
    >
    > This reverts commit 085deeae4f99ba12f7982aae69f6576f7f18b9c1.
    >
    > Reason for revert: This CL also now fixes permission policy tests
    > by making them run in sequences so that the emulated click is not
    > lost across tests.
    >
    > Original change's description:
    > > Revert "Reject getDisplayMedia calls without user activation"
    > >
    > > This reverts commit 5fbd1e8cdadc327dedfad9ce896e24c562e59fbf.
    > >
    > > Reason for revert: Reverted by Sheriff on suspicion of causing a test failure. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/110084/test-results?sortby=&groupby=&q=ExactID%3Aninja%3A%2F%2F%3Ablink_wpt_tests%2Fexternal%2Fwpt%2Fscreen-capture%2Fpermissions-policy-audio%2Bvideo.https.sub.html+VHash%3A9f5ebdc6f73354e7.
    > >
    > > The test failure lists getDisplayMedia failing as the cause, and this CL is in the blame set and looks related.
    > >
    > > Original change's description:
    > > > Reject getDisplayMedia calls without user activation
    > > >
    > > > This CL makes sure getDisplayMedia() returned promise reject with
    > > > InvalidStateError when called without user activation.
    > > > Note that speculation rules tests are updated as well[1].
    > > >
    > > > [1] https://github.com/WICG/nav-speculation/issues/225
    > > >
    > > > Intent to remove: https://groups.google.com/a/chromium.org/g/blink-dev/c/YGmuAVOqftI
    > > >
    > > > Bug: 1198918
    > > > Change-Id: I530761d95e6db412582d0f19c0170803516c33f7
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4096797
    > > > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > > > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > > > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > > > Commit-Queue: Fr <beaufort.francois@gmail.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1086033}
    > >
    > > Bug: 1198918
    > > Change-Id: I14d0e47ee406aa09b6eec763e94d8e83f282a04d
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4119423
    > > Auto-Submit: Peter Williamson <petewil@chromium.org>
    > > Quick-Run: Peter Williamson <petewil@chromium.org>
    > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Owners-Override: Peter Williamson <petewil@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1086147}
    >
    > Bug: 1198918
    > Change-Id: I456423c03c1430399f4b44e275a77e0411f6bda7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4125755
    > Commit-Queue: Fr <beaufort.francois@gmail.com>
    > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1095545}
    
    (cherry picked from commit a9d584e30a2e92ceec43894f0dbfae60c1546ecb)
    
    Bug: 1198918,1412364
    Bug: b:267183725
    Change-Id: I4eba562780cd15a9d7e50663d6b06fad3714af42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4217651
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1100530}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4218370
    Cr-Commit-Position: refs/branch-heads/5563@{#207}
    Cr-Branched-From: 3ac59a6729cdb287a7ee629a0004c907ec1b06dc-refs/heads/main@{#1097615}

--- chrome/browser/media/media_browsertest.cc
+++ chrome/browser/media/media_browsertest.cc
@@ -57,8 +57,7 @@ void MediaBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
 void MediaBrowserTest::RunMediaTestPage(const std::string& html_page,
                                         const base::StringPairs& query_params,
                                         const std::string& expected_title,
-                                        bool http,
-                                        bool with_transient_activation) {
+                                        bool http) {
   GURL gurl;
   std::string query = media::GetURLQueryString(query_params);
   std::unique_ptr<net::EmbeddedTestServer> http_test_server;
@@ -73,14 +72,12 @@ void MediaBrowserTest::RunMediaTestPage(const std::string& html_page,
     gurl = content::GetFileUrlWithQuery(media::GetTestDataFilePath(html_page),
                                         query);
   }
-  std::string final_title =
-      RunTest(gurl, expected_title, with_transient_activation);
+  std::string final_title = RunTest(gurl, expected_title);
   EXPECT_EQ(expected_title, final_title);
 }
 
 std::string MediaBrowserTest::RunTest(const GURL& gurl,
-                                      const std::string& expected_title,
-                                      bool with_transient_activation) {
+                                      const std::string& expected_title) {
   DVLOG(0) << base::TimeFormatTimeOfDayWithMilliseconds(base::Time::Now())
            << " Running test URL: " << gurl;
 
@@ -89,12 +86,6 @@ std::string MediaBrowserTest::RunTest(const GURL& gurl,
       base::ASCIIToUTF16(expected_title));
   AddWaitForTitles(&title_watcher);
   CHECK(ui_test_utils::NavigateToURL(browser(), gurl));
-
-  if (with_transient_activation) {
-    content::WebContents* tab =
-        browser()->tab_strip_model()->GetActiveWebContents();
-    EXPECT_TRUE(content::ExecJs(tab, "runTest()"));
-  }
   std::u16string result = title_watcher.WaitAndGetTitle();
   return base::UTF16ToASCII(result);
 }
