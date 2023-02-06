33f5ecf5d4ac9c04e6e774d9359a31c870e2ae95
Ari Chivukula <arichiv@chromium.org>
2023-02-06 20:19:33 +0000

    [StorageKey] Remove comments about moving LocalStorage paths
    I thought that it would be better to move the local storage specific
    serialization and deserialization code to the local storage dir, but
    upon further examination that would simply require duplicate testing and
    fuzzing infra. It seems safer to keep this in the same place even if
    it's unfortunate.
    
    Bug: 1410254
    Change-Id: Ie39d6eb1d267ef197201ce6f435459d3e37b6d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4226191
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101750}

--- third_party/blink/public/common/storage_key/storage_key.h
+++ third_party/blink/public/common/storage_key/storage_key.h
@@ -127,7 +127,6 @@ class BLINK_COMMON_EXPORT StorageKey {
   // Transforms a string in the format used for localStorage (without trailing
   // slashes) into a StorageKey if possible.
   // Prefer Deserialize() for uses other than localStorage.
-  // TODO(https://crbug.com/1410254): Move this to LocalStorage code.
   static absl::optional<StorageKey> DeserializeForLocalStorage(
       base::StringPiece in);
 
@@ -188,7 +187,6 @@ class BLINK_COMMON_EXPORT StorageKey {
   // Serializes into a string in the format used for localStorage (without
   // trailing slashes). Prefer Serialize() for uses other than localStorage. Do
   // not call if `origin_` is opaque.
-  // TODO(https://crbug.com/1410254): Move this to LocalStorage code.
   std::string SerializeForLocalStorage() const;
 
   // `IsThirdPartyContext` returns true if the StorageKey is for a context that
