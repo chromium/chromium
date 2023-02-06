3cc0f5fd92e07a2fd6ab00a133d3e041c1c4d5d4
Lambros Lambrou <lambroslambrou@chromium.org>
2023-02-06 22:10:12 +0000

    [remoting] Remove log-handler from the macOS host process.
    In April 2015, a log-handler for os_log() was added to the host process
    in revision 25cef11863014bc8ff4a8ff16d8570ad9c62bedc. This was needed
    because launchd-started processes would typically have stderr
    redirected to /dev/null. The host process was previously configured to
    log to a specific file, but it caused issues in some configurations
    if the process did not have write permissions to that file.
    
    In November 2015, an improvement was made to Chrome's logging on MacOS
    (revision 4c7449cca7681e0428d7206554ebe7eb65cfac3d). This detects if
    stderr has been redirected to /dev/null, and uses os_log() if so.
    
    This later change made the previous one unnecessary. Since then,
    Chrome's logging is smarter about when to use os_log() and the
    host process has written log messages twice to the system journal.
    This CL removes the log-handler that is no longer needed.
    
    Bug: 1412261
    Change-Id: Ic35cd9385f457825a1fdf4e1e52cbfb7cbc2933d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225852
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101811}

--- remoting/base/logging_mac.cc
+++ remoting/base/logging_mac.cc
@@ -4,65 +4,9 @@
 
 #include "remoting/base/logging_internal.h"
 
-#include <os/log.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 #include "base/logging.h"
-#include "base/scoped_generic.h"
-#include "base/strings/string_number_conversions.h"
 
 namespace remoting {
-
-namespace {
-
-const char kChromotingLoggingFacility[] = "org.chromium.chromoting";
-
-bool LogMessageToOsLog(logging::LogSeverity severity,
-                       const char* file,
-                       int line,
-                       size_t message_start,
-                       const std::string& message) {
-  const class OSLog {
-   public:
-    explicit OSLog()
-        : os_log_(
-              os_log_create(kChromotingLoggingFacility, "chromium_logging")) {}
-    OSLog(const OSLog&) = delete;
-    OSLog& operator=(const OSLog&) = delete;
-    ~OSLog() { os_release(os_log_); }
-    os_log_t get() const { return os_log_; }
-
-   private:
-    os_log_t os_log_;
-  } log;
-
-  const os_log_type_t os_log_type = [](logging::LogSeverity severity) {
-    switch (severity) {
-      case logging::LOGGING_INFO:
-        return OS_LOG_TYPE_INFO;
-      case logging::LOGGING_WARNING:
-        return OS_LOG_TYPE_DEFAULT;
-      case logging::LOGGING_ERROR:
-        return OS_LOG_TYPE_ERROR;
-      case logging::LOGGING_FATAL:
-        return OS_LOG_TYPE_FAULT;
-      case logging::LOGGING_VERBOSE:
-        return OS_LOG_TYPE_DEBUG;
-      default:
-        return OS_LOG_TYPE_DEFAULT;
-    }
-  }(severity);
-
-  os_log_with_type(log.get(), os_log_type, "%{public}s",
-                   message.c_str() + message_start);
-
-  // Don't prevent message from being logged by traditional means.
-  return false;
-}
-
-}  // namespace
 
 void InitHostLogging() {
   InitHostLoggingCommon();
@@ -72,9 +16,6 @@ void InitHostLogging() {
   settings.logging_dest =
       logging::LOG_TO_SYSTEM_DEBUG_LOG | logging::LOG_TO_STDERR;
   logging::InitLogging(settings);
-
-  // Write logs to syslog as well.
-  logging::SetLogMessageHandler(LogMessageToOsLog);
 }
 
 }  // namespace remoting
