bf07ef102e7f07eeb3a74df7656aa3b0e785d320
Kush Sinha <sinhak@chromium.org>
2023-02-06 10:32:03 +0000

    CodeHealth: Migrate `base::Value::FindKey()` to v2 API
    Migrate `base::Value::FindKey()` to `base::Value::Dict::Find()` and
    `base::Value::Dict::contains()` in -
    /chrome/browser/extensions/api/web_request
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    Bug: 1406815
    Change-Id: I834f71d0c7f623a41b52041c46c9b533743c4c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4219761
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Quick-Run: Kush Sinha <sinhak@chromium.org>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1101508}

--- chrome/browser/extensions/api/web_request/web_request_event_details_unittest.cc
+++ chrome/browser/extensions/api/web_request/web_request_event_details_unittest.cc
@@ -4,6 +4,7 @@
 
 #include <memory>
 
+#include "base/check_deref.h"
 #include "base/values.h"
 #include "extensions/browser/api/web_request/web_request_api_constants.h"
 #include "extensions/browser/api/web_request/web_request_api_helpers.h"
@@ -38,17 +39,19 @@ TEST(WebRequestEventDetailsTest, SetResponseHeaders) {
     details.SetResponseHeaders(request_info, headers.get());
     base::Value::Dict dict =
         details.GetFilteredDict(kFilter, nullptr, std::string(), false);
-    base::Value* filtered_headers = dict.Find("responseHeaders");
+    const base::Value::List* filtered_headers =
+        dict.FindList("responseHeaders");
     ASSERT_TRUE(filtered_headers);
-    ASSERT_EQ(2u, filtered_headers->GetList().size());
-    EXPECT_EQ("Key1",
-              filtered_headers->GetList()[0].FindKey("name")->GetString());
-    EXPECT_EQ("Value1",
-              filtered_headers->GetList()[0].FindKey("value")->GetString());
+    ASSERT_EQ(2u, filtered_headers->size());
+    const base::Value::Dict& first_header =
+        CHECK_DEREF(filtered_headers)[0].GetDict();
+    const base::Value::Dict& second_header =
+        CHECK_DEREF(filtered_headers)[1].GetDict();
+    EXPECT_EQ("Key1", CHECK_DEREF(first_header.FindString("name")));
+    EXPECT_EQ("Value1", CHECK_DEREF(first_header.FindString("value")));
     EXPECT_EQ("X-Chrome-ID-Consistency-Response",
-              filtered_headers->GetList()[1].FindKey("name")->GetString());
-    EXPECT_EQ("Value2",
-              filtered_headers->GetList()[1].FindKey("value")->GetString());
+              CHECK_DEREF(second_header.FindString("name")));
+    EXPECT_EQ("Value2", CHECK_DEREF(second_header.FindString("value")));
   }
 
   {
@@ -60,13 +63,13 @@ TEST(WebRequestEventDetailsTest, SetResponseHeaders) {
     gaia_details.SetResponseHeaders(gaia_request_info, headers.get());
     base::Value::Dict dict =
         gaia_details.GetFilteredDict(kFilter, nullptr, std::string(), false);
-    base::Value* filtered_headers = dict.Find("responseHeaders");
+    base::Value::List* filtered_headers = dict.FindList("responseHeaders");
     ASSERT_TRUE(filtered_headers);
-    ASSERT_EQ(1u, filtered_headers->GetList().size());
-    EXPECT_EQ("Key1",
-              filtered_headers->GetList()[0].FindKey("name")->GetString());
-    EXPECT_EQ("Value1",
-              filtered_headers->GetList()[0].FindKey("value")->GetString());
+    ASSERT_EQ(1u, filtered_headers->size());
+    const base::Value::Dict& header =
+        CHECK_DEREF(filtered_headers)[0].GetDict();
+    EXPECT_EQ("Key1", CHECK_DEREF(header.FindString("name")));
+    EXPECT_EQ("Value1", CHECK_DEREF(header.FindString("value")));
   }
 }
 
