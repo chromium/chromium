ff4db78e0b7efd9d1a2fe9862e5949d7284844d3
Olesia Marukhno <olesiamarukhno@google.com>
2023-02-08 10:53:25 +0000

    [privacy sandbox] Update ad privacy icon
    (cherry picked from commit df785817af12ba9ab764354365138ce0983841f8)
    
    Bug: 1413141
    Change-Id: Idf83f11c116966b0640e0aad38071b9d93270f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4223382
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1101561}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231772
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5563@{#256}
    Cr-Branched-From: 3ac59a6729cdb287a7ee629a0004c907ec1b06dc-refs/heads/main@{#1097615}

--- chrome/browser/ui/views/page_info/page_info_main_view.cc
+++ chrome/browser/ui/views/page_info/page_info_main_view.cc
@@ -654,10 +654,6 @@ PageInfoMainView::CreateAdPersonalizationSection() {
       base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)
           ? IDS_PAGE_INFO_AD_PRIVACY_TOOLTIP
           : IDS_PAGE_INFO_AD_PERSONALIZATION_TOOLTIP;
-  const auto icon =
-      base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)
-          ? PageInfoViewFactory::GetPrivacySandboxIcon()
-          : PageInfoViewFactory::GetAdPersonalizationIcon();
   ads_personalization_section
       ->AddChildView(std::make_unique<RichHoverButton>(
           base::BindRepeating(
@@ -665,7 +661,8 @@ PageInfoMainView::CreateAdPersonalizationSection() {
                 view->navigation_handler_->OpenAdPersonalizationPage();
               },
               this),
-          icon, l10n_util::GetStringUTF16(header_id), std::u16string(),
+          PageInfoViewFactory::GetAdPersonalizationIcon(),
+          l10n_util::GetStringUTF16(header_id), std::u16string(),
           l10n_util::GetStringUTF16(tooltip_id), std::u16string(),
           PageInfoViewFactory::GetOpenSubpageIcon()))
       ->SetID(PageInfoViewFactory::VIEW_ID_PAGE_INFO_AD_PERSONALIZATION_BUTTON);
