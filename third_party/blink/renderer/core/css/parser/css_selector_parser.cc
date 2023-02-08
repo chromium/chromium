fb53f0b9dec5323ad884efa83f12d2569fcefbd4
Steinar H. Gunderson <sesse@chromium.org>
2023-02-08 11:49:08 +0000

    Fix so that @supports selector(&) passes if CSSNesting flag is off.
    We'd parse & as a valid ident even if the CSSNesting flag was off,
    which gives false positives for @supports selector(&).
    
    To verify, run:
    
      third_party/blink/tools/run_web_tests.py \
          --additional-driver-flag=--disable-blink-features=CSSNesting \
          external/wpt/css/css-nesting/supports-is-consistent.html
    
    Fixed: 1414012
    Change-Id: I0d7c3babee26f7aeb4df4bdcfce84a1b7aa3ccfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4231773
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102638}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -1168,7 +1168,8 @@ bool CSSSelectorParser::ConsumeSimpleSelector(CSSParserTokenRange& range) {
     ok = ConsumeAttribute(range);
   } else if (token.GetType() == kColonToken) {
     ok = ConsumePseudo(range);
-  } else if (token.GetType() == kDelimiterToken && token.Delimiter() == '&') {
+  } else if (token.GetType() == kDelimiterToken && token.Delimiter() == '&' &&
+             RuntimeEnabledFeatures::CSSNestingEnabled()) {
     ok = ConsumeNestingParent(range);
   } else {
     return false;
