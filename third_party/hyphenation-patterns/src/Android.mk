0720c691bcdc16a9438e249642aeabbe498c2da4
Koji Ishii <kojii@chromium.org>
2023-02-08 05:03:34 +0000

    Update hyphenation-patterns from android.googlesource.com
    This patch updates hyphenation-patterns from the ToT of the
    android.googlesource.com.
    
    Following patterns are added in this update:
    - af (Afrikaans)
    - am/mul-ethi (Amharic, languages written in the Ethiopic script)
    - cs (Czech)
    - el (Modern Greek, monotonic)
    - gl (Galician)
    - it (Italian)
    - ka (Georgian)
    - lt (Lithuanian)
    - lv (Latvian)
    - nl (Dutch)
    - ru (Russian)
    - sk (Czechoslovak)
    - sq (Albanian)
    - sv (Swedish)
    - uk (Ukrainian)
    
    Bug: 1361453, 1178617, 1342901, 1407411
    Change-Id: Ieff02cdf2b9f80f5cf85ff84f292e690e2b986bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4189277
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102567}

--- third_party/hyphenation-patterns/src/Android.mk
+++ third_party/hyphenation-patterns/src/Android.mk
@@ -18,16 +18,20 @@ ifneq ($(TARGET_BUILD_PDK),true)
 LOCAL_PATH := $(call my-dir)
 
 pattern_locales := \
+    af/af \
+    am/mul-ethi \
     as/as \
     be/be \
     bn/bn \
     bg/bg \
+    cs/cs \
     cu/cu \
     cy/cy \
     da/da \
     de/de-1901 \
     de/de-1996 \
     de/de-ch-1901 \
+    el/el \
     en-GB/en-gb \
     en-US/en-us \
     es/es \
@@ -36,25 +40,36 @@ pattern_locales := \
     Ethi/und-ethi \
     fr/fr \
     ga/ga \
+    gl/gl \
     gu/gu \
     hi/hi \
     hr/hr \
     hu/hu \
     hy/hy \
+    it/it \
+    ka/ka \
     kn/kn \
     la/la \
+    lt/lt \
+    lv/lv \
     ml/ml \
     mn/mn-cyrl \
     mr/mr \
     nb/nb \
+    nl/nl \
     nn/nn \
     or/or \
     pa/pa \
     pt/pt \
+    ru/ru \
+    sk/sk \
     sl/sl \
+    sq/sq \
+    sv/sv \
     ta/ta \
     te/te \
-    tk/tk
+    tk/tk \
+    uk/uk
 
 # TODO: we have data for sa/sa, but it requires special case handling for case
 # folding and normalization, so don't build it until that's fixed.
@@ -73,6 +88,9 @@ define build-one-pattern-module
 $(eval include $(CLEAR_VARS))\
 $(eval LOCAL_MODULE := $(addprefix hyph-, $(2)))\
 $(eval LOCAL_SRC_FILES := $(addprefix $(1)/hyph-, $(addprefix $(2), .pat.txt .chr.txt .hyp.txt)))\
+$(eval LOCAL_LICENSE_KINDS := SPDX-license-identifier-MIT SPDX-license-identifier-BSD-3-Clause SPDX-license-identifier-FSFAP SPDX-license-identifier-Unicode-DFS SPDX-license-identifier-LGPL-2.1 SPDX-license-identifier-MPL)\
+$(eval LOCAL_LICENSE_CONDITIONS := notice reciprocal restricted)\
+$(eval LOCAL_NOTICE_FILE := $(wildcard $(addprefix $(LOCAL_PATH)/$(1), /NOTICE /LICENSE)))\
 $(eval include $(BUILD_HYB))\
 $(eval include $(CLEAR_VARS))\
 $(eval LOCAL_MODULE := $(addprefix $(addprefix hyph-, $(2)), .lic.txt))\
@@ -83,7 +101,7 @@ $(eval LOCAL_MODULE_PATH := $(TARGET_OUT)/usr/hyphen-data)\
 $(eval include $(BUILD_PREBUILT))
 endef
 
-$(foreach l, $(pattern_locales), $(call build-one-pattern-module, $(dir $(l)), $(notdir $l)))
+$(foreach l, $(pattern_locales), $(call build-one-pattern-module,$(dir $(l)),$(notdir $l)))
 build-one-pattern-module :=
 pattern_locales :=
 
