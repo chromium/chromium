8d1e3e3bb20e1e0959439c423abad3e09e5a0193
Anastasia Helfinstein <anastasi@google.com>
2023-02-08 16:37:49 +0000

    [ChromeVox] Adjust speech properties directly from CommandHandler
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: If3f492b0e8f4723a35432e70e73a051eb993f66f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4227348
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1102750}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
@@ -115,22 +115,22 @@ export class CommandHandler extends CommandHandlerInterface {
         LogManager.logTreeDump();
         break;
       case Command.DECREASE_TTS_RATE:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.RATE, false);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.RATE, false);
         return false;
       case Command.INCREASE_TTS_RATE:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.RATE, true);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.RATE, true);
         return false;
       case Command.DECREASE_TTS_PITCH:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.PITCH, false);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.PITCH, false);
         return false;
       case Command.INCREASE_TTS_PITCH:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.PITCH, true);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.PITCH, true);
         return false;
       case Command.DECREASE_TTS_VOLUME:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.VOLUME, false);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.VOLUME, false);
         return false;
       case Command.INCREASE_TTS_VOLUME:
-        this.increaseOrDecreaseSpeechProperty_(TtsSettings.VOLUME, true);
+        ChromeVox.tts.increaseOrDecreaseProperty(TtsSettings.VOLUME, true);
         return false;
       case Command.STOP_SPEECH:
         ChromeVox.tts.stop();
@@ -799,17 +799,6 @@ export class CommandHandler extends CommandHandlerInterface {
    */
   onFinishCommand() {
     SmartStickyMode.instance.stopIgnoringRangeChanges();
-  }
-
-  /**
-   * Increase or decrease a speech property and make an announcement.
-   * @param {string} propertyName The name of the property to change.
-   * @param {boolean} increase If true, increases the property value by one
-   *     step size, otherwise decreases.
-   * @private
-   */
-  increaseOrDecreaseSpeechProperty_(propertyName, increase) {
-    ChromeVox.tts.increaseOrDecreaseProperty(propertyName, increase);
   }
 
   /**
