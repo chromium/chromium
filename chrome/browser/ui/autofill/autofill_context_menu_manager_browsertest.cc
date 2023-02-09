91737a16d8416c788eeefedd1279f4da96e08413
Norge Vizcay <vizcay@google.com>
2023-02-09 11:49:02 +0000

    [AutofillFeedback] Add autofill feedback browser tests.
    The browser tests make sure that on non-chromeos Desktop platforms the feedback dialog is properly shown and that it receives as parameter the autofill metadata.
    
    Bug: 1407646
    Change-Id: I0ff8f51e670aa3f774d186202509ea1f7f2bd371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4208800
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103221}

--- chrome/browser/ui/autofill/autofill_context_menu_manager_browsertest.cc
+++ chrome/browser/ui/autofill/autofill_context_menu_manager_browsertest.cc
@@ -4,13 +4,20 @@
 
 #include "chrome/browser/ui/autofill/autofill_context_menu_manager.h"
 
+#include "base/json/json_reader.h"
+#include "base/run_loop.h"
+#include "base/task/single_thread_task_runner.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/webui/feedback/feedback_dialog.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/autofill/content/browser/content_autofill_driver.h"
+#include "components/autofill/core/browser/autofill_feedback_data.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/prefs/testing_pref_service.h"
 #include "content/public/test/browser_test.h"
@@ -50,6 +57,23 @@ class AutofillContextMenuManagerFeedbackUIBrowserTest
   base::test::ScopedFeatureList feature_;
 };
 
+// Awaits for the feedback dialog to be active. `callback` gets triggered
+// once the dialog is shown.
+void EnsureFeedbackAppUIShown(FeedbackDialog* feedback_dialog,
+                              base::OnceClosure callback) {
+  auto* widget = feedback_dialog->GetWidget();
+  ASSERT_NE(nullptr, widget);
+  if (widget->IsActive()) {
+    std::move(callback).Run();
+  } else {
+    base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&EnsureFeedbackAppUIShown, feedback_dialog,
+                       std::move(callback)),
+        base::Seconds(1));
+  }
+}
+
 IN_PROC_BROWSER_TEST_F(AutofillContextMenuManagerFeedbackUIBrowserTest,
                        FeedbackUIIsRequested) {
   base::HistogramTester histogram_tester;
@@ -72,5 +96,66 @@ IN_PROC_BROWSER_TEST_F(AutofillContextMenuManagerFeedbackUIBrowserTest,
       browser()->tab_strip_model()->GetActiveWebContents();
   tab->Close();
 }
+
+#if !BUILDFLAG(IS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(AutofillContextMenuManagerFeedbackUIBrowserTest,
+                       DisplaysFeedbackDialogUI) {
+  base::RunLoop run_loop;
+  // Test that no feedback dialog exists.
+  ASSERT_EQ(nullptr, FeedbackDialog::GetInstanceForTest());
+
+  autofill_context_menu_manager_->ExecuteCommand(
+      AutofillContextMenuManager::CommandId(
+          IDC_CONTENT_CONTEXT_AUTOFILL_FEEDBACK));
+
+  FeedbackDialog* feedback_dialog = FeedbackDialog::GetInstanceForTest();
+  // Test that a feedback dialog object has been created.
+  ASSERT_NE(nullptr, feedback_dialog);
+
+  // The feedback app starts invisible until after a screenshot has been taken
+  // via JS on the UI side. Afterward, JS will send a request to show the app
+  // window via a message handler.
+  EnsureFeedbackAppUIShown(feedback_dialog, run_loop.QuitClosure());
+  run_loop.Run();
+
+  // Test that the feedback app is visible now.
+  EXPECT_TRUE(feedback_dialog->GetWidget()->IsVisible());
+
+  // Close the feedback dialog.
+  feedback_dialog->GetWidget()->Close();
+}
+
+IN_PROC_BROWSER_TEST_F(AutofillContextMenuManagerFeedbackUIBrowserTest,
+                       FeedbackDialogArgsAutofillMetadata) {
+  AutofillManager* manager = ContentAutofillDriver::GetForRenderFrameHost(
+                                 web_contents()->GetPrimaryMainFrame())
+                                 ->autofill_manager();
+  ASSERT_TRUE(manager);
+  std::string expected_metadata;
+  base::JSONWriter::Write(data_logs::FetchAutofillFeedbackData(manager),
+                          &expected_metadata);
+
+  // Test that none feedback dialog exists.
+  ASSERT_EQ(nullptr, FeedbackDialog::GetInstanceForTest());
+
+  // Display feedback dialog.
+  autofill_context_menu_manager_->ExecuteCommand(
+      AutofillContextMenuManager::CommandId(
+          IDC_CONTENT_CONTEXT_AUTOFILL_FEEDBACK));
+
+  ui::WebDialogDelegate* feedback_dialog = FeedbackDialog::GetInstanceForTest();
+  // Test that a feedback dialog object has been created.
+  ASSERT_NE(nullptr, feedback_dialog);
+
+  // Extract autofill metadata from dialog arguments and check for correctness.
+  std::string dialog_args_str = feedback_dialog->GetDialogArgs();
+  absl::optional<base::Value> value = base::JSONReader::Read(dialog_args_str);
+  ASSERT_TRUE(value.has_value() && value->is_dict());
+  const std::string* autofill_metadata =
+      value->GetDict().FindString("autofillMetadata");
+  ASSERT_TRUE(autofill_metadata);
+  EXPECT_EQ(*autofill_metadata, expected_metadata);
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace autofill
