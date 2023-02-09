19b4c1ae0cdd77b556d3af228f572e27f559bcae
Bruce Dawson <brucedawson@chromium.org>
2023-02-09 18:37:05 +0000

    Switch windbg_common.bat to python3
    tools\win\windbg64.bat and tools\win\windbg32.bat can be used to launch
    the version of Windbg packaged in the Chromium toolchain used by Google
    employees, however the batch file was invoking python instead of
    python3. This corrects that.
    
    The change is necessary because build\vs_toolchain.py ends up being
    invoked and this script is no longer Python 2 compatible.
    
    Change-Id: I24f63e9bac4e4b990c78bdd602ea6991b9dd1e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4236516
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1103379}

--- tools/win/windbg_common.bat
+++ tools/win/windbg_common.bat
@@ -4,7 +4,7 @@ REM found in the LICENSE file.
 
 REM Helper batch file for windbg32.bat and windbg64.bat
 
-FOR /f "usebackq tokens=*" %%a in (`python %~dp0win_sdk_dir.py`) do set win_sdk_dir=%%a
+FOR /f "usebackq tokens=*" %%a in (`python3 %~dp0win_sdk_dir.py`) do set win_sdk_dir=%%a
 echo When debugging processes pass -g -G to suppress process start/end breakpoints, and pass -o to debug child processes.
 echo Pass -z to load crash dumps.
 echo When launching processes you should use windbg32.bat for x86 processes and you must use windbg64.bat for x64 processes.
