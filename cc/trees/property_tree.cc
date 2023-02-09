f6b69070173078cc3cee39f63f29bde6abef99cf
Bo Liu <boliu@chromium.org>
2023-02-09 15:20:30 +0000

    cc: Fix scroll past 0 during overscroll
    When to_screen_is_potentially_animated is true, UndoSnapping and
    UpdateSnapping are not matched, which can cause incorrect snapping to
    build up during animation. Fix by resetting snap_amount at the end
    of UndoSnapping.
    
    Bug: 1410234
    Change-Id: I87cceb3b1592e8b69323b0458f099aea915b7d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222637
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103277}

--- cc/trees/property_tree.cc
+++ cc/trees/property_tree.cc
@@ -29,6 +29,7 @@
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/gfx/geometry/transform_util.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 
 namespace cc {
 
@@ -618,6 +619,7 @@ void TransformTree::UndoSnapping(TransformNode* node) {
   // We need to undo it and use the un-snapped transform to compute current
   // target and screen space transforms.
   node->to_parent.Translate(-node->snap_amount.x(), -node->snap_amount.y());
+  node->snap_amount = gfx::Vector2dF();
 }
 
 void TransformTree::UpdateSnapping(TransformNode* node) {
