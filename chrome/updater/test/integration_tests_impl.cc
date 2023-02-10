26be3190c6f93395aa816cf006dbdeb17491d70c
Noah Rose Ledesma <noahrose@google.com>
2023-02-10 17:44:18 +0000

    Increase time between calls in UpdateServiceStress
    The UpdateServiceStress is looping too tightly for the Linux
    implementation to handle. Increasing the delay between successive calls
    should reduce flakiness.
    
    Bug: 1413523
    Change-Id: Id58b181c088c186ef1128b4ca64b633aac7e2021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4241377
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1103920}

--- chrome/updater/test/integration_tests_impl.cc
+++ chrome/updater/test/integration_tests_impl.cc
@@ -615,7 +615,12 @@ void StressUpdateService(UpdaterScope scope) {
   int n = 10;
 
   // Delay in milliseconds between successive cycles.
+#if BUILDFLAG(IS_LINUX)
+  // Looping too tightly causes socket connections to be dropped on Linux.
+  const int kDelayBetweenLoopsMS = 10;
+#else
   const int kDelayBetweenLoopsMS = 0;
+#endif
 
   // Runs on the main sequence.
   auto loop_closure = [&]() {
