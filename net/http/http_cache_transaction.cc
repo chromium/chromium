1394d62563ddf789209ef14acd91d9aa25d3a5b9
Adam Rice <ricea@chromium.org>
2023-02-13 12:59:55 +0000

    Debug DCHECK failure in http_cache_transaction
    In RecordHistograms() in HttpCache::Transaction the status has been
    observed to be ENTRY_USED when the request has been sent, which is
    unexpected. Change DCHECKs to CHECKs to verify this.
    
    Also CHECK fail if changing status to ENTRY_USED after
    send_request_since has been set, or setting send_request_since when the
    status is ENTRY_USED.
    
    This CL is expected to cause crashes. It can be reverted if the crash
    rate is too high.
    
    BUG=1409150
    
    Change-Id: Ic69c54ee11d212b69f02f745cd7b27f0ad80dd77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4216083
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104447}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -1862,6 +1862,10 @@ int HttpCache::Transaction::DoSendRequest() {
   DCHECK(mode_ & WRITE || mode_ == NONE);
   DCHECK(!network_trans_.get());
 
+  // We do not expect to send a request if the cache entry was used.
+  // TODO(https://crbug.com/1409150): If this CHECK fails investigate how we got
+  // here. Remove the CHECK.
+  CHECK_NE(cache_entry_status_, CacheEntryStatus::ENTRY_USED);
   send_request_since_ = TimeTicks::Now();
 
   // Create a network transaction.
@@ -3855,6 +3859,12 @@ void HttpCache::Transaction::UpdateCacheEntryStatus(
     return;
   DCHECK(cache_entry_status_ == CacheEntryStatus::ENTRY_UNDEFINED ||
          new_cache_entry_status == CacheEntryStatus::ENTRY_OTHER);
+  // If we are using the entry then we should not have sent a request.
+  // TODO(https://crbug.com/1409150): If this CHECK fires then trace when the
+  // request was sent. Remove this CHECK.
+  if (new_cache_entry_status == CacheEntryStatus::ENTRY_USED) {
+    CHECK(send_request_since_.is_null());
+  }
   cache_entry_status_ = new_cache_entry_status;
   SyncCacheEntryStatusToResponse();
 }
@@ -3975,15 +3985,14 @@ void HttpCache::Transaction::RecordHistograms() {
 
   bool did_send_request = !send_request_since_.is_null();
 
-  // TODO(ricea): Understand why this DCHECK is failing in the wild, fix it, and
+  // TODO(ricea): Understand why this CHECK is failing in the wild, fix it, and
   // remove it. See https://crbug.com/1409150.
-  // TODO(ricea): Change this to a CHECK on HEAD after merging this fix to M111.
   if (did_send_request) {
-    DCHECK_NE(cache_entry_status_, CacheEntryStatus::ENTRY_USED);
+    CHECK_NE(cache_entry_status_, CacheEntryStatus::ENTRY_USED);
   }
-  // This DCHECK() should not fire, because the one above should catch all the
+  // This CHECK() should not fire, because the one above should catch all the
   // erroneous cases.
-  DCHECK(
+  CHECK(
       (did_send_request &&
        (cache_entry_status_ == CacheEntryStatus::ENTRY_NOT_IN_CACHE ||
         cache_entry_status_ == CacheEntryStatus::ENTRY_VALIDATED ||
