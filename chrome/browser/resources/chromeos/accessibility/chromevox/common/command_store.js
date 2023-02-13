4dde82ec2b0d66ec66f8aab4c98993b1bd47dd08
Anastasia Helfinstein <anastasi@google.com>
2023-02-13 22:22:07 +0000

    [ChromeVox] Make COMMAND_DATA private to the module
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Test: cq
    Change-Id: Ic6b75dcffa9d497398b2c1a1eb23242079bfdf97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4242536
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1104712}

--- chrome/browser/resources/chromeos/accessibility/chromevox/common/command_store.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/command_store.js
@@ -12,7 +12,7 @@
  * If you are looking to add a user command, follow the below steps for best
  * integration with existing components:
  * 1. Add the command to the |Command| enum.
- * 2. Add a command below in CommandStore.COMMAND_DATA. Fill in each of the
+ * 2. Add a command below in COMMAND_DATA. Fill in each of the
  * relevant JSON keys.
  * Be sure to add a msg id and define it in chromevox/messages/messages.js which
  * describes the command. Please also add a category msg id so that the command
@@ -29,7 +29,7 @@ export class CommandStore {
    * @return {string|undefined} The message id, if any.
    */
   static messageForCommand(command) {
-    return (CommandStore.COMMAND_DATA[command] || {}).msgId;
+    return (COMMAND_DATA[command] || {}).msgId;
   }
 
   /**
@@ -38,7 +38,7 @@ export class CommandStore {
    * @return {string|undefined} The category, if any.
    */
   static categoryForCommand(command) {
-    return (CommandStore.COMMAND_DATA[command] || {}).category;
+    return (COMMAND_DATA[command] || {}).category;
   }
 
   /**
@@ -47,8 +47,8 @@ export class CommandStore {
    * @return {!Command|undefined} The command, if any.
    */
   static commandForMessage(msgId) {
-    for (const commandName in CommandStore.COMMAND_DATA) {
-      const command = CommandStore.COMMAND_DATA[commandName];
+    for (const commandName in COMMAND_DATA) {
+      const command = COMMAND_DATA[commandName];
       if (command.msgId === msgId) {
         return commandName;
       }
@@ -62,8 +62,8 @@ export class CommandStore {
    */
   static commandsForCategory(category) {
     const ret = [];
-    for (const cmd in CommandStore.COMMAND_DATA) {
-      const struct = CommandStore.COMMAND_DATA[cmd];
+    for (const cmd in COMMAND_DATA) {
+      const struct = COMMAND_DATA[cmd];
       if (category === struct.category) {
         ret.push(cmd);
       }
@@ -73,13 +73,11 @@ export class CommandStore {
 
   /**
    * @param {!Command} command The command to query.
-   * @return {boolean} Whether or not this command is denied in the OOBE.
+   * @return {boolean} Whether this command is denied in signed out contexts.
    */
   static denySignedOut(command) {
-    if (!CommandStore.COMMAND_DATA[command]) {
-      return false;
-    }
-    return Boolean(CommandStore.COMMAND_DATA[command].denySignedOut);
+    return Boolean(COMMAND_DATA[command]) &&
+        Boolean(COMMAND_DATA[command].denySignedOut);
   }
 }
 
@@ -304,7 +302,7 @@ export const CommandCategory = {
  *  denySignedOut: Explicitly denies this command when on chrome://oobe/* or
  *             other signed-out contexts. Defaults to false.
  */
-CommandStore.COMMAND_DATA = {
+const COMMAND_DATA = {
   [Command.TOGGLE_STICKY_MODE]: {
     announce: false,
     msgId: 'toggle_sticky_mode',
--- chrome/browser/resources/chromeos/accessibility/chromevox/common/permission_checker.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/common/permission_checker.js
@@ -43,8 +43,7 @@ export class PermissionChecker {
       return true;
     }
 
-    return !CommandStore.COMMAND_DATA[command] ||
-        !CommandStore.COMMAND_DATA[command].denySignedOut;
+    return !CommandStore.denySignedOut(command);
   }
 
   /** @private */
