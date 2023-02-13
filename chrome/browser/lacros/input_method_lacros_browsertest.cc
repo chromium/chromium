c9ac70507821556590673a6d99e56c38bc93d099
Darren Shen <shend@chromium.org>
2023-02-13 22:14:30 +0000

    inputs: Add tests for web input events in lacros browser tests.
    Allow lacros browser tests to listen for web input events to verify that
    the web events received by web apps is the same as Ash.
    
    Bug: 238838841
    Change-Id: I55960f3673d1cd08042078ee149ceec904e9073e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4199792
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104706}

--- chrome/browser/lacros/input_method_lacros_browsertest.cc
+++ chrome/browser/lacros/input_method_lacros_browsertest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/strings/strcat.h"
+#include "base/test/values_test_util.h"
 #include "chrome/browser/lacros/browser_test_util.h"
 #include "chrome/browser/ui/lacros/window_utility.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -90,6 +91,164 @@ std::string RenderAutofocusedInputFieldInLacros(Browser* browser) {
 
 content::WebContents* GetActiveWebContents(Browser* browser) {
   return browser->tab_strip_model()->GetActiveWebContents();
+}
+
+auto IsKeyboardEvent(const base::StringPiece type,
+                     const base::StringPiece key,
+                     const base::StringPiece code,
+                     int key_code) {
+  return base::test::IsJson(content::JsReplace(
+      R"({
+        "type": $1,
+        "key": $2,
+        "code": $3,
+        "keyCode": $4,
+      })",
+      type, key, code, key_code));
+}
+
+auto IsKeyDownEvent(const base::StringPiece key,
+                    const base::StringPiece code,
+                    int key_code) {
+  return IsKeyboardEvent("keydown", key, code, key_code);
+}
+
+auto IsKeyUpEvent(const base::StringPiece key,
+                  const base::StringPiece code,
+                  int key_code) {
+  return IsKeyboardEvent("keyup", key, code, key_code);
+}
+
+auto IsKeyPressEvent(const base::StringPiece key,
+                     const base::StringPiece code,
+                     int key_code) {
+  return IsKeyboardEvent("keypress", key, code, key_code);
+}
+
+auto IsCompositionEvent(const base::StringPiece type,
+                        const base::StringPiece data) {
+  return base::test::IsJson(content::JsReplace(
+      R"({
+        "type": $1,
+        "data": $2,
+      })",
+      type, data));
+}
+
+auto IsCompositionStartEvent() {
+  return IsCompositionEvent("compositionstart", "");
+}
+
+auto IsCompositionUpdateEvent(const base::StringPiece data) {
+  return IsCompositionEvent("compositionupdate", data);
+}
+
+auto IsCompositionEndEvent() {
+  return IsCompositionEvent("compositionend", "");
+}
+
+enum class CompositionState { kComposing, kNotComposing };
+
+auto IsInputEvent(const base::StringPiece type,
+                  const base::StringPiece input_type,
+                  const absl::optional<base::StringPiece> data,
+                  CompositionState composition_state) {
+  const bool is_composing = composition_state == CompositionState::kComposing;
+
+  if (!data.has_value()) {
+    return base::test::IsJson(content::JsReplace(
+        R"({
+        "type": $1,
+        "inputType": $2,
+        "data": null,
+        "isComposing": $3,
+      })",
+        type, input_type, is_composing));
+  }
+
+  return base::test::IsJson(content::JsReplace(
+      R"({
+        "type": $1,
+        "inputType": $2,
+        "data": $3,
+        "isComposing": $4,
+      })",
+      type, input_type, *data, is_composing));
+}
+
+auto IsBeforeInputEvent(const base::StringPiece input_type,
+                        const absl::optional<base::StringPiece> data,
+                        CompositionState composition_state) {
+  return IsInputEvent("beforeinput", input_type, data, composition_state);
+}
+
+auto IsInputEvent(const base::StringPiece input_type,
+                  const absl::optional<base::StringPiece> data,
+                  CompositionState composition_state) {
+  return IsInputEvent("input", input_type, data, composition_state);
+}
+
+class InputEventListener {
+ public:
+  explicit InputEventListener(content::WebContents* web_contents)
+      : messages_(web_contents) {}
+
+  base::Value WaitForMessage() {
+    std::string event_message;
+    if (!messages_.WaitForMessage(&event_message)) {
+      return {};
+    }
+
+    return base::test::ParseJson(event_message);
+  }
+
+  bool HasMessages() { return messages_.HasMessages(); }
+
+ private:
+  content::DOMMessageQueue messages_;
+};
+
+// Listens for web input events from `element_id`.
+InputEventListener ListenForInputEvents(content::WebContents* web_content,
+                                        base::StringPiece element_id) {
+  const std::string script = content::JsReplace(
+      R"(elem = document.getElementById($1);
+         function extractEventData(e) {
+           if (e instanceof CompositionEvent) {
+             return {type: e.type, data: e.data};
+           }
+           if (e instanceof InputEvent) {
+             return {
+               type: e.type,
+               isComposing: e.isComposing,
+               inputType: e.inputType,
+               data: e.data
+             };
+           }
+           if (e instanceof KeyboardEvent) {
+             return {
+               type: e.type,
+               key: e.key,
+               code: e.code,
+               keyCode: e.keyCode
+             };
+           }
+           return {};
+         }
+         function recordEvent(e) {
+           window.domAutomationController.send(extractEventData(e));
+         }
+         elem.addEventListener('keydown', recordEvent);
+         elem.addEventListener('keypress', recordEvent);
+         elem.addEventListener('keyup', recordEvent);
+         elem.addEventListener('beforeinput', recordEvent);
+         elem.addEventListener('input', recordEvent);
+         elem.addEventListener('compositionstart', recordEvent);
+         elem.addEventListener('compositionupdate', recordEvent);
+         elem.addEventListener('compositionend', recordEvent);)",
+      element_id);
+  std::ignore = ExecJs(web_content, script);
+  return InputEventListener(web_content);
 }
 
 // Waits for the contents of an input field with ID `element_id` to become
@@ -311,6 +470,41 @@ IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
+                       CommitTextTriggersWebEvents) {
+  mojo::Remote<InputMethodTestInterface> input_method =
+      BindInputMethodTestInterface(
+          {InputMethodTestInterface::MethodMinVersions::kWaitForFocusMinVersion,
+           InputMethodTestInterface::MethodMinVersions::
+               kKeyEventHandledMinVersion});
+  if (!input_method.is_bound()) {
+    GTEST_SKIP() << "Unsupported ash version";
+  }
+  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+  InputMethodTestInterfaceAsyncWaiter input_method_async_waiter(
+      input_method.get());
+  input_method_async_waiter.WaitForFocus();
+  InputEventListener event_listener =
+      ListenForInputEvents(GetActiveWebContents(browser()), id);
+
+  input_method_async_waiter.CommitText("hello");
+  input_method_async_waiter.CommitText(" world");
+
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsBeforeInputEvent("insertText", "hello",
+                                 CompositionState::kNotComposing));
+  EXPECT_THAT(
+      event_listener.WaitForMessage(),
+      IsInputEvent("insertText", "hello", CompositionState::kNotComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsBeforeInputEvent("insertText", " world",
+                                 CompositionState::kNotComposing));
+  EXPECT_THAT(
+      event_listener.WaitForMessage(),
+      IsInputEvent("insertText", " world", CompositionState::kNotComposing));
+  EXPECT_FALSE(event_listener.HasMessages());
+}
+
+IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
                        SetCompositionInsertsCompositionInEmptyInputField) {
   mojo::Remote<InputMethodTestInterface> input_method =
       BindInputMethodTestInterface(
@@ -423,6 +617,46 @@ IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
+                       SetCompositionTriggersWebEvents) {
+  mojo::Remote<InputMethodTestInterface> input_method =
+      BindInputMethodTestInterface(
+          {InputMethodTestInterface::MethodMinVersions::kWaitForFocusMinVersion,
+           InputMethodTestInterface::MethodMinVersions::
+               kKeyEventHandledMinVersion});
+  if (!input_method.is_bound()) {
+    GTEST_SKIP() << "Unsupported ash version";
+  }
+  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+  InputMethodTestInterfaceAsyncWaiter input_method_async_waiter(
+      input_method.get());
+  input_method_async_waiter.WaitForFocus();
+  InputEventListener event_listener =
+      ListenForInputEvents(GetActiveWebContents(browser()), id);
+
+  input_method_async_waiter.SetComposition("hello", 4);
+  input_method_async_waiter.SetComposition("", 0);
+
+  EXPECT_THAT(event_listener.WaitForMessage(), IsCompositionStartEvent());
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsBeforeInputEvent("insertCompositionText", "hello",
+                                 CompositionState::kComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsCompositionUpdateEvent("hello"));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsInputEvent("insertCompositionText", "hello",
+                           CompositionState::kComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsBeforeInputEvent("insertCompositionText", "",
+                                 CompositionState::kComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(), IsCompositionUpdateEvent(""));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsInputEvent("insertCompositionText", absl::nullopt,
+                           CompositionState::kComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(), IsCompositionEndEvent());
+  EXPECT_FALSE(event_listener.HasMessages());
+}
+
+IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
                        SendKeyEventNotHandledTypesInEmptyTextField) {
   mojo::Remote<InputMethodTestInterface> input_method =
       BindInputMethodTestInterface(
@@ -518,6 +752,40 @@ IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
 
   EXPECT_TRUE(WaitUntilInputFieldHasText(GetActiveWebContents(browser()), id,
                                          "ㅎ", gfx::Range(1)));
+}
+
+IN_PROC_BROWSER_TEST_F(InputMethodLacrosBrowserTest,
+                       SendKeyEventTriggersWebEvents) {
+  mojo::Remote<InputMethodTestInterface> input_method =
+      BindInputMethodTestInterface(
+          {InputMethodTestInterface::MethodMinVersions::kWaitForFocusMinVersion,
+           InputMethodTestInterface::MethodMinVersions::
+               kKeyEventHandledMinVersion});
+  if (!input_method.is_bound()) {
+    GTEST_SKIP() << "Unsupported ash version";
+  }
+  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+  InputMethodTestInterfaceAsyncWaiter input_method_async_waiter(
+      input_method.get());
+  input_method_async_waiter.WaitForFocus();
+  InputEventListener event_listener =
+      ListenForInputEvents(GetActiveWebContents(browser()), id);
+
+  SendKeyEventSync(input_method_async_waiter,
+                   CreateKeyPressEvent('a', ui::DomCode::US_A));
+  SendKeyEventSync(input_method_async_waiter,
+                   CreateKeyReleaseEvent('a', ui::DomCode::US_A));
+
+  EXPECT_THAT(event_listener.WaitForMessage(), IsKeyDownEvent("a", "KeyA", 65));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsKeyPressEvent("a", "KeyA", 97));
+  EXPECT_THAT(
+      event_listener.WaitForMessage(),
+      IsBeforeInputEvent("insertText", "a", CompositionState::kNotComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(),
+              IsInputEvent("insertText", "a", CompositionState::kNotComposing));
+  EXPECT_THAT(event_listener.WaitForMessage(), IsKeyUpEvent("a", "KeyA", 65));
+  EXPECT_FALSE(event_listener.HasMessages());
 }
 
 }  // namespace
