fca1bf5086275292c43b58e61b40ffdc2a5b5564
Noah Rose Ledesma <noahrose@google.com>
2023-02-14 18:28:48 +0000

    Implement system-scope activity bits for Linux
    On Linux it is not safe to assume that all of the home
    directories for users on a system are subdirectories of /home/. Thus, we
    take a more hermetic approach by parsing /etc/passwd to query the home
    directories for all users.
    
    Fixed: 1384968
    Change-Id: I3855143d9070bdae55bcc39d7120ab19e12e862c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4111080
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105160}

--- chrome/updater/activity_impl_util_linux.cc
+++ chrome/updater/activity_impl_util_linux.cc
@@ -9,11 +9,44 @@
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/notreached.h"
+#include "base/ranges/algorithm.h"
+#include "base/strings/string_split.h"
 #include "chrome/updater/updater_branding.h"
 #include "chrome/updater/updater_scope.h"
 
 namespace updater {
+namespace {
+// Attempts to discover all of the home directories on the system by parsing
+// /etc/passwd.
+std::vector<base::FilePath> ReadHomeDirsFromPasswd() {
+  std::string passwd_contents;
+  if (!base::ReadFileToString(base::FilePath("/etc/passwd"),
+                              &passwd_contents)) {
+    return {};
+  }
+
+  // /etc/passwd contains one line for each user account, with seven
+  // fields delimited by colons.
+  std::vector<base::FilePath> home_dirs;
+  base::ranges::transform(
+      base::SplitString(passwd_contents, "\n",
+                        base::WhitespaceHandling::TRIM_WHITESPACE,
+                        base::SplitResult::SPLIT_WANT_NONEMPTY),
+      std::back_inserter(home_dirs), [](const std::string& line) {
+        std::vector<std::string> entries = base::SplitString(
+            line, ":", base::WhitespaceHandling::KEEP_WHITESPACE,
+            base::SplitResult::SPLIT_WANT_ALL);
+        // The sixth entry (index 5) of the line will be the user's home
+        // directory.
+        return entries.size() == 7 ? base::FilePath(entries[5])
+                                   : base::FilePath();
+      });
+  // Remove invalid paths
+  base::ranges::remove(home_dirs, base::FilePath());
+  return home_dirs;
+}
+}  // namespace
+
 std::vector<base::FilePath> GetHomeDirPaths(UpdaterScope scope) {
   switch (scope) {
     case UpdaterScope::kUser: {
@@ -24,9 +57,7 @@ std::vector<base::FilePath> GetHomeDirPaths(UpdaterScope scope) {
       return {path};
     }
     case UpdaterScope::kSystem: {
-      // TODO(crbug.com/c/1384968): Implement activity bits for system-scope.
-      NOTIMPLEMENTED();
-      return {};
+      return ReadHomeDirsFromPasswd();
     }
   }
   return {};
