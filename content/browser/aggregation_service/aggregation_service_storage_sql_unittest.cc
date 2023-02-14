f1a58a64a7e25c14d64f308de0a7f33969b61ca2
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-14 01:27:32 +0000

    Check return value of sql::MetaTable::SetVersionNumber/SetCompatibleVersionNumber
    Without this, it is possible for a schema to be partially initialized,
    e.g. the tables are changed but the version is not, which could lead to
    unintended behavior, including crashes.
    
    In the future, these methods will be marked [[nodiscard]] to help
    prevent erroneous usage.
    
    This CL was uploaded by git cl split.
    
    R=johnidel@chromium.org
    
    Bug: 1414092
    Change-Id: Ia0efee0c4f0fad9fe153a94eeb3eaf0a2899a5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244781
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1104823}

--- content/browser/aggregation_service/aggregation_service_storage_sql_unittest.cc
+++ content/browser/aggregation_service/aggregation_service_storage_sql_unittest.cc
@@ -444,8 +444,8 @@ TEST_F(AggregationServiceStorageSqlTest, VersionTooNew_RazesDB) {
     // The values here are irrelevant, as the meta table already exists.
     ASSERT_TRUE(meta.Init(&raw_db, /*version=*/1, /*compatible_version=*/1));
 
-    meta.SetVersionNumber(meta.GetVersionNumber() + 1);
-    meta.SetCompatibleVersionNumber(meta.GetVersionNumber() + 1);
+    ASSERT_TRUE(meta.SetVersionNumber(meta.GetVersionNumber() + 1));
+    ASSERT_TRUE(meta.SetCompatibleVersionNumber(meta.GetVersionNumber() + 1));
   }
 
   // The DB should be razed because the version is too new.
