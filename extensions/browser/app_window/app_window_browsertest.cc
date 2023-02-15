623c41cdbe8be090e9299d76f76f3e5b2cf4d36c
John Abd-El-Malek <jam@chromium.org>
2023-02-15 18:38:00 +0000

    Disable AppWindowBrowserTest.ShouldShowStaleContentOnEviction as it's flaky.
    Bug: 1416579
    Change-Id: I6dd36fce18e05e76a4b7d7b3cb54407b2ce74e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4256110
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105755}

--- extensions/browser/app_window/app_window_browsertest.cc
+++ extensions/browser/app_window/app_window_browsertest.cc
@@ -72,7 +72,9 @@ IN_PROC_BROWSER_TEST_F(AppWindowBrowserTest, FrameInsetsForNoFrame) {
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 
-IN_PROC_BROWSER_TEST_F(AppWindowBrowserTest, ShouldShowStaleContentOnEviction) {
+// Disabled due to flake. https://crbug.com/1416579
+IN_PROC_BROWSER_TEST_F(AppWindowBrowserTest,
+                       DISABLED_ShouldShowStaleContentOnEviction) {
   AppWindow* app_window = CreateTestAppWindow("{}");
   // Make sure that a surface gets embedded in the frame evictor of the
   // DelegateFrameHost.
