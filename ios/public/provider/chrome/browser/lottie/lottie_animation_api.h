f25bd4ce15179d6066891c6a1ae8d1a899d14e7f
Cheick Cisse <cheickcisse@google.com>
2023-02-15 15:52:40 +0000

    [iOS] Add Lottie Animation Provider API
    This CL adds a new provider API for Lottie, giving access to the Lottie ChromeInternal framework. The provider will be used to create and render animations.
    
    API added:
    - play: to play the animation
    - stop: to stop the animation
    - setColorValue: to dynamically set colors,
    - setDictionaryTextProvider: to dynamically set texts
    - isAnimationPlaying: whether or not the animation is rendering
    - animationView: returns the animation view.
    
    Change-Id: I2f3b1cca3180ee55d09bc678f4128eaae209e207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4241220
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1105670}

---
+++ ios/public/provider/chrome/browser/lottie/lottie_animation_api.h
@@ -0,0 +1,46 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_API_H_
+
+#import <UIKit/UIKit.h>
+
+@class LottieAnimationConfiguration;
+
+// A wrapper that can facilitate communication with downstream.
+@protocol LottieAnimation <NSObject>
+
+// Called to plays the lottie animation.
+- (void)play;
+
+// Called to stop the lottie animation.
+- (void)stop;
+
+// Called to set a color for a key path.
+- (void)setColorValue:(UIColor*)color forKeypath:(NSString*)keypath;
+
+// Called to set a dictionary text provider.
+- (void)setDictionaryTextProvider:
+    (NSDictionary<NSString*, NSString*>*)dictionaryTextProvider;
+
+// Called to check if the animation is playing.
+- (BOOL)isAnimationPlaying;
+
+// Returns the lottie animation view.
+- (UIView*)animationView;
+
+@end
+
+namespace ios {
+namespace provider {
+
+// Generate an instance of LottieAnimation.
+id<LottieAnimation> GenerateLottieAnimation(
+    LottieAnimationConfiguration* config);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_API_H_
---
+++ ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h
@@ -0,0 +1,26 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_CONFIGURATION_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_CONFIGURATION_H_
+
+#import <UIKit/UIKit.h>
+
+// Configuration parameters for LottieAnimation.
+@interface LottieAnimationConfiguration : NSObject
+
+// Path to the json animation file.
+@property(nonatomic, copy) NSString* path;
+// Subdirectory to the json animation file.
+@property(nonatomic, copy) NSString* subdirectory;
+// Configuration parameters for LottieAnimation.
+@property(nonatomic, strong) UIView* view;
+// The bundle in which the animation is located.
+@property(nonatomic, strong) NSBundle* bundle;
+// The loop behavior of the animation.
+@property(nonatomic, assign) CGFloat loopAnimationCount;
+
+@end
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LOTTIE_LOTTIE_ANIMATION_CONFIGURATION_H_
