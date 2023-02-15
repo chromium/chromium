2e477fd38998a022b05bec2f438fbee9ed5fd57b
Aliona DANGLA <alionadangla@google.com>
2023-02-15 19:19:13 +0000

    [iOS] Add more tests to inactive tabs utils
    Follow up to http://go/crrv/c/4249399
    
    Add test with complicated list:
    * Un-ordered
    * Active and inactive
    * Pinned
    
    Bug: 1414048
    Change-Id: I65e8f941f50aad96abf69a80f96c919f31cc5b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4254825
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1105790}

--- ios/chrome/browser/tabs/inactive_tabs/utils_unittest.mm
+++ ios/chrome/browser/tabs/inactive_tabs/utils_unittest.mm
@@ -8,6 +8,7 @@
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/main/test_browser.h"
+#import "ios/chrome/browser/tabs/features.h"
 #import "ios/chrome/browser/tabs/inactive_tabs/features.h"
 #import "ios/chrome/browser/tabs/inactive_tabs/utils.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
@@ -49,6 +50,16 @@ class InactiveTabsUtilsTest : public PlatformTest {
     web_state->SetLastActiveTime(base::Time::Now() -
                                  base::Days(inactivity_days_number));
     return web_state;
+  }
+
+  void CheckOrder(WebStateList* web_state_list,
+                  std::vector<int> expected_inactivity_days) {
+    for (int index = 0; index < web_state_list->count(); index++) {
+      web::WebState* current_web_state = web_state_list->GetWebStateAt(index);
+      int time_since_last_activation =
+          (base::Time::Now() - current_web_state->GetLastActiveTime()).InDays();
+      EXPECT_EQ(time_since_last_activation, expected_inactivity_days[index]);
+    }
   }
 };
 
@@ -204,4 +215,156 @@ TEST_F(InactiveTabsUtilsTest, RestoreAllInactive) {
 
   EXPECT_EQ(active_web_state_list->count(), 1);
   EXPECT_EQ(inactive_web_state_list->count(), 0);
+}
+
+// Ensure that all moving functions are working with complicated lists (multiple
+// tabs, un-ordered, pinned tabs).
+TEST_F(InactiveTabsUtilsTest, ComplicatedMove) {
+  // No inactive tabs on iPad.
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET) {
+    return;
+  }
+  base::test::ScopedFeatureList feature_list;
+  std::map<std::string, std::string> parameters;
+  parameters[kTabInactivityThresholdParameterName] =
+      kTabInactivityThresholdOneWeekParam;
+  feature_list.InitWithFeaturesAndParameters(
+      {
+          /* Enabled features */
+          {kTabInactivityThreshold, {parameters}},
+          {kEnablePinnedTabs, {}},
+      },
+      {/* Disabled features */});
+
+  WebStateList* active_web_state_list = browser_active_->GetWebStateList();
+  WebStateList* inactive_web_state_list = browser_inactive_->GetWebStateList();
+
+  EXPECT_EQ(active_web_state_list->count(), 0);
+  EXPECT_EQ(inactive_web_state_list->count(), 0);
+
+  // Add a new inactive and active tabs in the inactive browser.
+  inactive_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(10),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(30),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(
+      0, CreateInactiveTab(2), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(16),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+
+  // Add a new inactive and active tabs in the active browser.
+  active_web_state_list->InsertWebState(0, CreateInactiveTab(22),
+                                        WebStateList::INSERT_ACTIVATE,
+                                        WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateInactiveTab(9), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateInactiveTab(18), WebStateList::INSERT_PINNED, WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateInactiveTab(3), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+
+  EXPECT_EQ(active_web_state_list->count(), 6);
+  EXPECT_EQ(inactive_web_state_list->count(), 6);
+
+  // Pinned first (18) and then creation order (22, 0, 9, 0, 3).
+  std::vector<int> expected_active_last_activity_order_before = {18, 22, 0,
+                                                                 9,  0,  3};
+  CheckOrder(active_web_state_list, expected_active_last_activity_order_before);
+  // Creation order.
+  std::vector<int> expected_inactive_last_activity_order_before = {0, 10, 30,
+                                                                   2, 16, 0};
+  CheckOrder(inactive_web_state_list,
+             expected_inactive_last_activity_order_before);
+
+  MoveTabsFromActiveToInactive(browser_active_.get(), browser_inactive_.get());
+
+  EXPECT_EQ(active_web_state_list->count(), 4);
+  EXPECT_EQ(inactive_web_state_list->count(), 8);
+
+  // "old" inactive first (0, 10, 30, 2, 16, 0) and finally "new" inactive from
+  // active list (22, 9).
+  std::vector<int> expected_inactive_last_activity_order1 = {0,  10, 30, 2,
+                                                             16, 0,  22, 9};
+  CheckOrder(inactive_web_state_list, expected_inactive_last_activity_order1);
+
+  // Pinned first (18) and then active (0, 0, 3).
+  std::vector<int> expected_active_last_activity_order1 = {18, 0, 0, 3};
+  CheckOrder(active_web_state_list, expected_active_last_activity_order1);
+
+  MoveTabsFromInactiveToActive(browser_inactive_.get(), browser_active_.get());
+
+  EXPECT_EQ(active_web_state_list->count(), 7);
+  EXPECT_EQ(inactive_web_state_list->count(), 5);
+
+  // All active (< 8 days of inactivity) are removed.
+  std::vector<int> expected_inactive_last_activity_order2 = {10, 30, 16, 22, 9};
+  CheckOrder(inactive_web_state_list, expected_inactive_last_activity_order2);
+
+  // Pinned first (18) then "new" active from the inactive (0, 2, 0) then "old"
+  // active (0, 0, 3).
+  std::vector<int> expected_active_last_activity_order2 = {18, 0, 2, 0,
+                                                           0,  0, 3};
+  CheckOrder(active_web_state_list, expected_active_last_activity_order2);
+}
+
+// Ensure that restore function is working with complicated lists (multiple
+// tabs, un-ordered, pinned tabs).
+TEST_F(InactiveTabsUtilsTest, ComplicatedRestore) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      {/* Enabled features */ kEnablePinnedTabs},
+      {/* Disabled features */ kTabInactivityThreshold});
+
+  WebStateList* active_web_state_list = browser_active_->GetWebStateList();
+  WebStateList* inactive_web_state_list = browser_inactive_->GetWebStateList();
+
+  EXPECT_EQ(active_web_state_list->count(), 0);
+  EXPECT_EQ(inactive_web_state_list->count(), 0);
+
+  // Add a new inactive and active tabs in the inactive browser.
+  inactive_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(10),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(30),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(
+      0, CreateInactiveTab(2), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  inactive_web_state_list->InsertWebState(0, CreateInactiveTab(16),
+                                          WebStateList::INSERT_ACTIVATE,
+                                          WebStateOpener());
+  inactive_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+
+  // Add pinned and active tab in the active browser.
+  active_web_state_list->InsertWebState(
+      0, CreateActiveTab(), WebStateList::INSERT_ACTIVATE, WebStateOpener());
+  active_web_state_list->InsertWebState(
+      0, CreateInactiveTab(18), WebStateList::INSERT_PINNED, WebStateOpener());
+
+  EXPECT_EQ(active_web_state_list->count(), 2);
+  EXPECT_EQ(inactive_web_state_list->count(), 6);
+
+  RestoreAllInactiveTabs(browser_inactive_.get(), browser_active_.get());
+
+  EXPECT_EQ(active_web_state_list->count(), 8);
+  EXPECT_EQ(inactive_web_state_list->count(), 0);
+
+  // Pinned first (18) then inactive (0, 10, 30, 2, 16, 0) and finally active
+  // (0).
+  std::vector<int> expected_last_activity_order = {18, 0, 10, 30, 2, 16, 0, 0};
+  CheckOrder(active_web_state_list, expected_last_activity_order);
 }
