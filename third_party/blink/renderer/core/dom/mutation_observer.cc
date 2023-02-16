4f9e3e746df0f2ac2980602ce178d4860b1ad01f
Scott Haseley <shaseley@chromium.org>
2023-02-16 18:00:32 +0000

    [dom] Remove unused suspended_mutation_observers_
    Found while reviewing related dom PR. The suspended observers logic was
    removed in crrev.com/c/1951133, along with the associated
    MutationObserverSet static local. The set was reintroduced in
    crrev.com/c/3937130 but is not being used.
    
    Bug: None
    Change-Id: I472ba5f5cf8063a0e78cf490e55c4c6d03f257b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4257065
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106295}

--- third_party/blink/renderer/core/dom/mutation_observer.cc
+++ third_party/blink/renderer/core/dom/mutation_observer.cc
@@ -80,7 +80,6 @@ class MutationObserverAgentData
   void Trace(Visitor* visitor) const override {
     Supplement<Agent>::Trace(visitor);
     visitor->Trace(active_mutation_observers_);
-    visitor->Trace(suspended_mutation_observers_);
     visitor->Trace(active_slot_change_list_);
   }
 
@@ -135,7 +134,6 @@ class MutationObserverAgentData
  private:
   // For MutationObserver.
   MutationObserverSet active_mutation_observers_;
-  MutationObserverSet suspended_mutation_observers_;
   SlotChangeList active_slot_change_list_;
 };
 
