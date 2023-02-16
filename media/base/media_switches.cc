342c20b2e2471705098134daa1a80b3e19315c81
Jordan Bayles <jophba@chromium.org>
2023-02-16 21:56:14 +0000

    Revert "[Content Capture] HiDPI enabled by default for M111"
    This reverts commit f08f4ae4898c2ce050ae2b8be84d88cdae24f169.
    
    Reason for revert: extending the experiment, default in M112.
    
    Original change's description:
    > [Content Capture] HiDPI enabled by default for M111
    >
    > This patch moves the HiDPI feature to be enabled by default for M111.
    >
    > Bug: 1324757
    > Change-Id: I351da548afe02a16abb7b915e3b87084d81b7dba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4150436
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    > Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1091620}
    
    Bug: 1324757
    Change-Id: I7c5d31b64892b7913c006e702bf2891aa09a3382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237501
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106448}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -885,7 +885,7 @@ BASE_FEATURE(kWakeLockOptimisationHiddenMuted,
 // while capturing a low-resolution tab.
 BASE_FEATURE(kWebContentsCaptureHiDpi,
              "WebContentsCaptureHiDPI",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Enables handling of hardware media keys for controlling media.
 BASE_FEATURE(kHardwareMediaKeyHandling,
