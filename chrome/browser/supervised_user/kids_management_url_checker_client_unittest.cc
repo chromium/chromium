d4b6f689d7071fd7efd9603b30074464a8fc27e6
Duong Dac <ddac@chromium.org>
2023-02-16 17:22:02 +0000

    Move KidsManagementURLCheckerClient to //components/supervised_user
    Refactor to prepare for supporting parental control on iOS.
    
    Bug: b/267471937
    Change-Id: I20a4dafe4c2ee9f7ecb32f1723b961376179f4f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4245920
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106264}

--- chrome/browser/supervised_user/kids_management_url_checker_client_unittest.cc
+++ chrome/browser/supervised_user/kids_management_url_checker_client_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/supervised_user/kids_management_url_checker_client.h"
+#include "components/supervised_user/core/browser/kids_management_url_checker_client.h"
 
 #include <memory>
 #include <utility>
