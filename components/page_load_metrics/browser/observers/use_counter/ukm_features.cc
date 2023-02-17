61f9b382c811a49e8dd6e791ee13b651d1e9998d
Mathias Bynens <mathias@chromium.org>
2023-02-17 15:28:29 +0000

    Remove obsolete UseCounters from UKM allowlist
    Unlike the XML file (//tools/metrics/ukm/ukm.xml), which is used for
    metadata for the dashboards, this list is purely an allowlist of
    what’s sent to UKM. There’s no reason to retain any entries that are
    no longer being emitted by the code.
    
    Bug: none
    Change-Id: I477adf378de9f831c28c9bfd5c1262f6323035e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4264108
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1106789}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -22,6 +22,8 @@
 // estimate "percentage of page views using a feature". Note that the lack of
 // sub-sampling is the reason why this event must only be used for rare
 // features.
+//
+// Obsolete UseCounters (kOBSOLETE_*) should be removed from this list.
 
 using WebFeature = blink::mojom::WebFeature;
 
@@ -55,8 +57,6 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kCredentialManagerGetPublicKeyCredential,
           WebFeature::kCredentialManagerMakePublicKeyCredentialSuccess,
           WebFeature::kCredentialManagerGetPublicKeyCredentialSuccess,
-          WebFeature::kOBSOLETE_U2FCryptotokenRegister,
-          WebFeature::kOBSOLETE_U2FCryptotokenSign,
           WebFeature::kTextToSpeech_Speak,
           WebFeature::kTextToSpeech_SpeakDisallowedByAutoplay,
           WebFeature::kCSSEnvironmentVariable_SafeAreaInsetTop,
@@ -191,10 +191,6 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kTextDetectorDetect,
           WebFeature::kV8SharedArrayBufferConstructedWithoutIsolation,
           WebFeature::kV8HTMLVideoElement_GetVideoPlaybackQuality_Method,
-          WebFeature::kOBSOLETE_RTCPeerConnectionConstructedWithPlanB,
-          WebFeature::kOBSOLETE_RTCPeerConnectionConstructedWithUnifiedPlan,
-          WebFeature::kOBSOLETE_RTCPeerConnectionUsingComplexPlanB,
-          WebFeature::kOBSOLETE_RTCPeerConnectionUsingComplexUnifiedPlan,
           WebFeature::kWebPImage,
           WebFeature::kAVIFImage,
           WebFeature::kGetDisplayMedia,
