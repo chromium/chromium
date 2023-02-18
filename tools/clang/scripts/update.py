1cee151e77fb6a96bbdbc0afe91c127cd50fe1ca
Nico Weber <thakis@chromium.org>
2023-02-18 01:58:28 +0000

    Roll clang+rust llvmorg-17-init-2082-g6d4a674a-1 : llvmorg-17-init-2387-g68e81d7e-1 / [skipping Rust]
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/+log/6d4a674a..68e81d7e
    
    Ran: ./tools/clang/scripts/upload_revision.py 68e81d7e45c7ea1e2427bf6ac12ac5702693d5e9
    
    Bug: 1416802
    Change-Id: Iaafd2f27aa2d2d8e403b33e4e8e699cc9170c37d
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst
    Cq-Include-Trybots: chromium/try:win-asan
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-dbg
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-rel
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-dbg
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4259585
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1107109}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,7 +35,7 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-17-init-2082-g6d4a674a'
+CLANG_REVISION = 'llvmorg-17-init-2387-g68e81d7e'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
