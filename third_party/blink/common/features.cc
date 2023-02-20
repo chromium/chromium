04eb9e094d18b9b4333bc5666c1216ae1db4c69b
W. James MacLean <wjmaclean@chromium.org>
2023-02-20 15:52:36 +0000

    Re-enable same-process, cross-origin iframe throttling.
    This CL re-enables throttling of same-process, cross-origin iframes to
    make the throttling consistent between same-process and cross-process
    iframes. The initial attempt to enable this fix, at
    
    https://chromium-review.googlesource.com/c/chromium/src/+/2877953
    
    was reverted due to it causing problems with some websites. Those
    issues have since been resolved, and an enterprise policy added at
    
    https://chromium-review.googlesource.com/c/chromium/src/+/3966873 and
    https://chromium-review.googlesource.com/c/chromium/src/+/4255609
    
    to allow short-term opting out of this feature.
    
    Bug: 958475
    Change-Id: I8ddf692de2472a2646da62f578b8adb1c30db6a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4262647
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107471}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1699,7 +1699,7 @@ BASE_FEATURE(kUseThreadPoolForMediaStreamVideoTaskRunner,
 
 BASE_FEATURE(kThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,
              "ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframesEnabled() {
   static bool throttling_disabled =
