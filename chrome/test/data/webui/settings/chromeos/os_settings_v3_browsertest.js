812d0d3848726747661602ddfd30d1aafd412955
Alexander Hendrich <hendrich@chromium.org>
2023-02-20 16:19:52 +0000

    Disable OSSettingsOsSettingsPageV3Test.All on debug builds
    https://crrev.com/c/4218612 incorrectly disabled the device settings
    page (OSSettingsDevicePageV3Test) instead.
    
    Bug: 1411677
    Change-Id: I3cf46759688c17123d77c09597d2a1886ada46e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4271805
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107479}

--- chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_v3_browsertest.js
@@ -53,17 +53,8 @@ var OSSettingsDevicePageV3Test = class extends OSSettingsV3BrowserTest {
   }
 };
 
-// TODO(https://crbug.com/1411677): Re-enable in debug builds once test suite
-// does not time out anymore.
-TEST_F_WITH_PREAMBLE(
-    `
-#if !defined(NDEBUG)
-#define MAYBE_All DISABLED_All
-#else
-#define MAYBE_All All
-#endif
-    `,
-    'OSSettingsDevicePageV3Test', 'MAYBE_All',
+TEST_F(
+    'OSSettingsDevicePageV3Test', 'All',
     () => mocha.grep('/^((?!arrow_key_arrangement_disabled).)*$/').run());
 
 // TODO(crbug.com/1347746): move this to the generic test lists below after the
@@ -618,6 +609,11 @@ function registerTest(testName, module, featureList) {
     TEST_F(className, 'OfficialBuild' || 'All', () => {
       mocha.grep('SearchFeedback_OfficialBuild').run();
     });
+    GEN('#endif');
+  } else if (testName === 'OsSettingsPage') {
+    // TODO(crbug.com/1411677): times out (flaky) debug builds
+    GEN('#if defined(NDEBUG)');
+    TEST_F(className, 'All', () => mocha.run());
     GEN('#endif');
   } else {
     TEST_F(className, 'All', () => mocha.run());
