6e3132fc1ac3664bde3b8699013d0adbc16d9498
Claudio DeSouza <cdesouza@igalia.com>
2023-02-20 18:52:48 +0000

    [CodeHealth] Removing JSONReader::ReadDeprecated (7/30)
    This CL removes uses of JSONReader::ReadDeprecated from under
    /chrome/browser/ui/webui/settings.
    
    This CL was uploaded by git cl split.
    
    R=msramek@chromium.org
    
    Bug: 1339314
    Change-Id: I4100c0cba8f622997c11994248f42c207bd994c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265833
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107515}

--- chrome/browser/ui/webui/settings/site_settings_helper_unittest.cc
+++ chrome/browser/ui/webui/settings/site_settings_helper_unittest.cc
@@ -826,10 +826,10 @@ class SiteSettingsHelperChooserExceptionTest : public testing::Test {
                                            *ephemeral_device_info);
 
     // Add the policy granted permissions for testing.
-    auto policy_value = base::JSONReader::ReadDeprecated(kUsbPolicySetting);
+    auto policy_value = base::JSONReader::Read(kUsbPolicySetting);
     DCHECK(policy_value);
     profile()->GetPrefs()->Set(prefs::kManagedWebUsbAllowDevicesForUrls,
-                               *policy_value);
+                               std::move(*policy_value));
   }
 
   device::FakeUsbDeviceManager device_manager_;
