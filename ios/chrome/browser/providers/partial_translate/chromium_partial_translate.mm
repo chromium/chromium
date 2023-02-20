6ac370da63a41607600f17312f1b01302429ad64
Olivier Robin <olivierrobin@google.com>
2023-02-20 16:05:57 +0000

    [IOS] Create partial translate provider API and chromium implementations
    This CL is part of a multi-CL change:
     => https://crrev.com/c/4261151
      - https://crrev.com/i/5474489
      - https://crrev.com/c/4265031
    
    Bug: 1417560
    Change-Id: Ib7c8cf4079243d68ba2f1ff332ff4334d440cd1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261151
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107476}

---
+++ ios/chrome/browser/providers/partial_translate/chromium_partial_translate.mm
@@ -0,0 +1,23 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+#import "ios/public/provider/chrome/browser/partial_translate/partial_translate_api.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+id<PartialTranslateController> NewPartialTranslateController(
+    NSString* source_text,
+    const CGRect& anchor,
+    BOOL incognito) {
+  // Partial translate is not supported in Chromium.
+  return nil;
+}
+
+NSUInteger PartialTranslateLimitMaxCharacters() {
+  return 0;
+}
---
+++ ios/public/provider/chrome/browser/partial_translate/partial_translate_api.h
@@ -0,0 +1,38 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PARTIAL_TRANSLATE_PARTIAL_TRANSLATE_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PARTIAL_TRANSLATE_PARTIAL_TRANSLATE_API_H_
+
+#import <UIKit/UIKit.h>
+
+@protocol PartialTranslateController <NSObject>
+
+// Presents the PartialTranslateController on top of viewController.
+// `flowCompletionHandler` will be called at the end of the feature either
+// - immediately with a value of NO if it fails
+// - after the PartialTranslateController is dismissed with a value of YES if it
+// succeeded. Note: Once this method is called, it is the responsibility of the
+// caller to retain the PartialTranslateController until `flowCompletionHandler`
+// is called.
+- (void)presentOnViewController:(UIViewController*)viewController
+          flowCompletionHandler:(void (^)(BOOL))flowCompletionHandler;
+
+@end
+
+// Creates a PartialTranslateController to present the translate string for
+// `source_text`. `anchor` is the position of `sourceText` in window
+// coordinates. `incognito` is true if `source_text` was retrieved in an
+// incognito tab.
+id<PartialTranslateController> NewPartialTranslateController(
+    NSString* source_text,
+    const CGRect& anchor,
+    BOOL incognito);
+
+// The maximum length for the partial translate feature. Creating a
+// PartialTranslateController with a longer string and trying to present it will
+// fail.
+NSUInteger PartialTranslateLimitMaxCharacters();
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PARTIAL_TRANSLATE_PARTIAL_TRANSLATE_API_H_
