26840a195fb063c10844e3a5b2f437d986bb74ea
Peter Kvitek <kvitekp@chromium.org>
2023-02-21 19:57:38 +0000

    [headless] Implemented graceful shutdown signal handling
    Without graceful shutdown headless fails to write cookies and update local storage.
    
    This CL adds SIGTERM, SIGINT and SIGHUP handling which triggers browser shutdown once on Linux. Windows and Mac will be taken care of in follow up CLs.
    
    Bug: 771830
    Change-Id: I833d6aa9bf7167c687c11f801299e2eefcb63034
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265202
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107898}

--- headless/lib/browser/headless_browser_main_parts_linux.cc
+++ headless/lib/browser/headless_browser_main_parts_linux.cc
@@ -4,26 +4,129 @@
 
 #include "headless/lib/browser/headless_browser_main_parts.h"
 
+#include <signal.h>
+#include <unistd.h>
+
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
+#include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/no_destructor.h"
+#include "base/task/single_thread_task_runner.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "device/bluetooth/dbus/bluez_dbus_manager.h"
+#include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
+#include "headless/lib/browser/headless_browser_impl.h"
 
 #if !BUILDFLAG(IS_CHROMEOS)
 #include "base/command_line.h"
 #include "components/os_crypt/key_storage_config_linux.h"
 #include "components/os_crypt/os_crypt.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
 #include "headless/public/switches.h"
 #endif
 
+#if defined(USE_DBUS) && !BUILDFLAG(IS_CHROMEOS_ASH)
+#include "device/bluetooth/dbus/bluez_dbus_manager.h"
+#endif
+
 namespace headless {
+
+namespace {
+
+class BrowserShutdownHandler {
+ public:
+  BrowserShutdownHandler(const BrowserShutdownHandler&) = delete;
+  BrowserShutdownHandler& operator=(const BrowserShutdownHandler&) = delete;
+
+  static void Install(base::OnceClosure shutdown_callback) {
+    GetInstance().Init(content::GetUIThreadTaskRunner({}),
+                       std::move(shutdown_callback));
+
+    // We need to handle SIGTERM, because that is how many POSIX-based distros
+    // ask processes to quit gracefully at shutdown time.
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = SIGTERMHandler;
+    PCHECK(sigaction(SIGTERM, &action, nullptr) == 0);
+
+    // Also handle SIGINT - when the user terminates the browser via Ctrl+C. If
+    // the browser process is being debugged, GDB will catch the SIGINT first.
+    action.sa_handler = SIGINTHandler;
+    PCHECK(sigaction(SIGINT, &action, nullptr) == 0);
+
+    // And SIGHUP, for when the terminal disappears. On shutdown, many Linux
+    // distros send SIGHUP, SIGTERM, and then SIGKILL.
+    action.sa_handler = SIGHUPHandler;
+    PCHECK(sigaction(SIGHUP, &action, nullptr) == 0);
+  }
+
+ private:
+  friend class base::NoDestructor<BrowserShutdownHandler>;
+
+  BrowserShutdownHandler() = default;
+  ~BrowserShutdownHandler() = default;
+
+  static BrowserShutdownHandler& GetInstance() {
+    static base::NoDestructor<BrowserShutdownHandler> instance;
+    return *instance;
+  }
+
+  void Init(scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+            base::OnceClosure shutdown_callback) {
+    task_runner_ = std::move(task_runner);
+    shutdown_callback_ = std::move(shutdown_callback);
+  }
+
+  void Shutdown(int signal) {
+    if (shutdown_callback_) {
+      if (!task_runner_->PostTask(FROM_HERE, std::move(shutdown_callback_))) {
+        RAW_LOG(WARNING, "No valid task runner, exiting ungracefully.");
+        kill(getpid(), signal);
+      }
+    }
+  }
+
+  static void SIGHUPHandler(int signal) {
+    RAW_CHECK(signal == SIGHUP);
+    ShutdownHandler(signal);
+  }
+
+  static void SIGINTHandler(int signal) {
+    RAW_CHECK(signal == SIGINT);
+    ShutdownHandler(signal);
+  }
+
+  static void SIGTERMHandler(int signal) {
+    RAW_CHECK(signal == SIGTERM);
+    ShutdownHandler(signal);
+  }
+
+  static void ShutdownHandler(int signal) {
+    // Reinstall the default handler. We have only one shot at graceful
+    // shutdown.
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = SIG_DFL;
+    RAW_CHECK(sigaction(signal, &action, nullptr) == 0);
+
+    GetInstance().Shutdown(signal);
+  }
+
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  base::OnceClosure shutdown_callback_;
+};
+
+}  // namespace
 
 #if !BUILDFLAG(IS_CHROMEOS)
 constexpr char kProductName[] = "HeadlessChrome";
 #endif
 
 void HeadlessBrowserMainParts::PostCreateMainMessageLoop() {
+  BrowserShutdownHandler::Install(
+      base::BindOnce(&HeadlessBrowserImpl::Shutdown, browser_->GetWeakPtr()));
+
 #if defined(USE_DBUS) && !BUILDFLAG(IS_CHROMEOS_ASH)
   bluez::BluezDBusManager::Initialize(/*system_bus=*/nullptr);
 #endif
