c4316871ef46865db332d4688bbef656b9041775
Jonathan Ross <jonross@chromium.org>
2023-02-21 23:19:31 +0000

    Enable OnShowWithPageVisibility by default
    When implementing Android Tab Change Metrics:
        https://chromium-review.googlesource.com/c/chromium/src/+/3894618
    We added a refactoring behind OnShowWithPageVisibility. So that we
    could do a rollout to confirm seeing similar shifts as with the
    original change to TabSwitch2 on desktop.
    
    Data has shifted as expected, and we would like to enable the feature
    by default. So that we can then go and clear out the old code path.
    
    Bug: 1253938
    Change-Id: I003c51f96c105d2ea44e36a58c3c98bb5fbe258b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4275763
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1107994}

--- content/common/features.cc
+++ content/common/features.cc
@@ -15,7 +15,7 @@ BASE_FEATURE(kADPFForBrowserIOThread,
 
 BASE_FEATURE(kOnShowWithPageVisibility,
              "OnShowWithPageVisibility",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOptimizeImmHideCalls,
              "OptimizeImmHideCalls",
