f84a272950254cc84144b43c940d0da345766d6e
Pavol Marko <pmarko@google.com>
2023-02-21 23:25:54 +0000

    Wait for cert resolved before verifying EAP.Identity
    The test that this CL touches wants to verify that after resolving a
    client certificate for an EAP WiFi network, the Identity field - which
    contains "resolved client cert based" placeholders in the policy,
    contains the replaced placeholder values from the resolved cert.
    
    So far the test assumed that after network policy application the client
    certificate resolution is always already done, but this seems to have
    been a wrong assumption, unsurprisingly (-> b/268532127).
    
    Attempt to fix the flakiness by waiting for the certificate to be
    resolved first.
    
    Bug: b/268532127
    Change-Id: I753e7de7b6390ecbce153da463221c42b78016ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261173
    Auto-Submit: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108001}

--- chrome/browser/ash/policy/networking/network_policy_application_browsertest.cc
+++ chrome/browser/ash/policy/networking/network_policy_application_browsertest.cc
@@ -1278,9 +1278,8 @@ IN_PROC_BROWSER_TEST_F(NetworkPolicyApplicationTest,
 // Configures a network that uses variable expansions with variables based on a
 // client certificate selected using a CertificatePattern.
 // The network is device-wide because that is easier to set up in the test.
-// TODO(https://crbug.com/1414476): Flaky test.
 IN_PROC_BROWSER_TEST_F(NetworkPolicyApplicationTest,
-                       DISABLED_DevicePolicyCertBasedVariableExpansions) {
+                       DevicePolicyCertBasedVariableExpansions) {
   const char* kCertKeyFilename = "client_3.pk8";
   const char* kCertFilename = "client_3.pem";
   const char* kCertIssuerCommonName = "E CA";
@@ -1300,6 +1299,9 @@ IN_PROC_BROWSER_TEST_F(NetworkPolicyApplicationTest,
                                   /*issuer_common_name=*/kCertIssuerCommonName,
                                   /*identity=*/kIdentityPolicyValue),
       /*wait_applied=*/true);
+  ServicePropertyValueWatcher(shill_service_client_test_, kServiceWifi1,
+                              shill::kEapCertIdProperty)
+      .WaitForNonEmptyValue();
 
   {
     const base::Value::Dict* wifi_service_properties =
@@ -1330,6 +1332,9 @@ IN_PROC_BROWSER_TEST_F(NetworkPolicyApplicationTest,
                                     ->username_hash();
   shill_profile_client_test_->AddProfile(kUserProfilePath, user_hash);
 
+  // Note that while a policy is used here that uses a ClientCertPattern, no
+  // client certificate is resolved (because no such certificate was imported).
+  // Still, the "user-specific variable" should be expanded.
   SetUserOpenNetworkConfiguration(
       user_hash,
       OncPolicyToSelectClientCert("{UserLevelWifiGuid}", "UserLevelWifiSsid",
