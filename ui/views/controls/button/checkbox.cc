d2662918cb6082b93bdaead9059264192d583ad6
Joseph Park <josephjoopark@chromium.org>
2023-02-22 19:20:09 +0000

    CR 2023: Update check image for checkbox
    Bug: 1400018
    Change-Id: Ic30ad938ab12e1e0e3e14cbb19d64d4300a0d380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4171732
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108482}

--- ui/views/controls/button/checkbox.cc
+++ ui/views/controls/button/checkbox.cc
@@ -211,6 +211,10 @@ SkColor Checkbox::GetIconImageColor(int icon_state) const {
 }
 
 const gfx::VectorIcon& Checkbox::GetVectorIcon() const {
+  if (features::IsChromeRefresh2023()) {
+    return GetChecked() ? kCheckboxActiveCr2023Icon : kCheckboxNormalIcon;
+  }
+
   return GetChecked() ? kCheckboxActiveIcon : kCheckboxNormalIcon;
 }
 
