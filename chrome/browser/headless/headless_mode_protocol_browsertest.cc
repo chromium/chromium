bf854d7a0c59b5ee3d1fbaf939c212dd16fb1a36
Luciano Pacheco <lucmult@chromium.org>
2023-02-22 00:40:35 +0000

    Sheriff: Disable flake test on Mac: HeadlessModeProtocolBrowserTest.InputClipboardOps
    Bug: 1416882
    Change-Id: I5f22ff8615209797b07c3f74542776c0bb1727a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279061
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108058}

--- chrome/browser/headless/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/headless_mode_protocol_browsertest.cc
@@ -199,7 +199,14 @@ void HeadlessModeProtocolBrowserTest::OnConsoleAPICalled(
 HEADLESS_MODE_PROTOCOL_TEST(DomFocus, "input/dom-focus.js")
 HEADLESS_MODE_PROTOCOL_TEST(FocusBlurNotifications,
                             "input/focus-blur-notifications.js")
-HEADLESS_MODE_PROTOCOL_TEST(InputClipboardOps, "input/input-clipboard-ops.js")
+// TODO(crbug.com/1416882): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_InputClipboardOps DISABLED_InputClipboardOps
+#else
+#define MAYBE_InputClipboardOps InputClipboardOps
+#endif
+HEADLESS_MODE_PROTOCOL_TEST(MAYBE_InputClipboardOps,
+                            "input/input-clipboard-ops.js")
 
 // https://crbug.com/1411976
 #if BUILDFLAG(IS_WIN)
