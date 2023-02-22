6f4e9a5bcd64f51cd4a871294fbb4a16d2eee0c6
Tamir Duberstein <tamird@google.com>
2023-02-22 21:25:28 +0000

    [fuchsia] Update local-sdk.py to use python3
    python is no longer available on gLinux by default.
    
    Change-Id: I6c434aa33e069329c153ec3399fb1d085eb1637e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4284277
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Auto-Submit: Tamir Duberstein <tamird@google.com>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108573}

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright 2017 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-from __future__ import print_function
 
 import hashlib
 import json
