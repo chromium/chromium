4d317822ecea304b3d19a724f649235f12eda31b
Adam Psarouthakis <psarouthakis@google.com>
2023-02-22 18:14:51 +0000

    Persistence 100% Launch
    Enable kExtensionTelemetryPersistence by default and remove the extension telemetry persistence experiment from the fieldtrial_testing_config.json. After testing the Persister feature in 50% stable, the team is ready to take the experiment to 100%.
    
    Bug: 4200784
    Change-Id: I47e9418945c24e46586974a9a26546ef31e67df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4261395
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108436}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -112,7 +112,7 @@ BASE_FEATURE(kExtensionTelemetryCookiesGetAllSignal,
 
 BASE_FEATURE(kExtensionTelemetryPersistence,
              "SafeBrowsingExtensionTelemetryPersistence",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryConfiguration,
              "SafeBrowsingExtensionTelemetryConfiguration",
