a040be21c23b7e59e54e538d2c857d5b3f3c104b
Ben Joyce <bjoyce@google.com>
2023-02-22 22:31:58 +0000

    Update path for deprecated mockito method.
    Location has been moved to ArgumentMatchers in mockito 5.1.1
    
    Bug: 1340593
    Change-Id: I751ced650c5cf8a03c759bcb884731d8ec00328c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4282943
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1108615}

--- chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinatorTest.java
+++ chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinatorTest.java
@@ -6,8 +6,8 @@ package org.chromium.chrome.browser.creator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorMediatorTest.java
+++ chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorMediatorTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
