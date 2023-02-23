4edcb536605e93c6fe5d464599cf38f6ec5fa3b5
Chris Lu <thegreenfrog@chromium.org>
2023-02-23 22:55:48 +0000

    [ios] Remove updateStickyElements call in -feedLayoutDidEndUpdates
    Updating the state of elements that could stick to the top of the NTP
    are handled well in scrollViewDidScroll:, which is called in response
    to the feed updating after a card removal, for example. It is not
    necessary to do it in -feedLayoutDidEndUpdates even though it is the
    correct signal source for it. This was added to fix layout issues in response to a card removal.
    
    Video: https://drive.google.com/file/d/1a2Eaye9WdCc9IxTBzegbBjvF3aIMYaFT/view?usp=sharing
    
    Bug: 1406940
    Change-Id: Ie27f8100edfcbb7538085d708f4f6f60f70e92cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285598
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109265}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -540,11 +540,6 @@ const CGFloat kShiftTilesUpAnimationDuration = 0.1;
   if (self.hasSavedOffsetFromPreviousScrollState) {
     [self setContentOffset:self.savedScrollOffset];
   }
-  [self updateStickyElements];
-}
-
-- (void)updateStickyElements {
-  [self handleStickyElementsForScrollPosition:[self scrollPosition] force:YES];
 }
 
 #pragma mark - NewTabPageConsumer
