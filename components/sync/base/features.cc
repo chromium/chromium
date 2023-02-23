01a9060fd457389bc08b83e514815644dcc7dbaa
Ankush Singh <ankushkush@google.com>
2023-02-23 15:42:56 +0000

    [sync] Enable SyncAllowClearingMetadataWhenDataTypeIsStopped by default
    OnSyncStopping is not called for data types which are already stopped.
    This prevents clearing of metadata for such types. This CL enables use
    of the newly introduced method ClearMetadataWhileStopped() in such
    cases.
    
    Bug: 897628
    Change-Id: I754309febcde443bbcc9483356524ae334845c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4225454
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1108958}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -120,7 +120,7 @@ BASE_FEATURE(kSyncDoNotPropagateBrowserShutdownToDataTypes,
 
 BASE_FEATURE(kSyncAllowClearingMetadataWhenDataTypeIsStopped,
              "SyncAllowClearingMetadataWhenDataTypeIsStopped",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncEnableLoadModelsTimeout,
              "SyncEnableLoadModelsTimeout",
