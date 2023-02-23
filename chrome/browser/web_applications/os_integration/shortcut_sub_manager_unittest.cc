e454ba72f3f1b2b9522416a0d16ebe16423ca6f8
Dibyajyoti Pal <dibyapal@google.com>
2023-02-23 18:46:37 +0000

    [dPWA] Convert shortcut sub managers to unit-tests.
    This CL converts the shortcut sub manager browser tests to unit tests
    using WebAppTest. The only difference is the setting up of mock
    icons to be passed in during the installation of a web_app, but the
    test cases and assertions still stay the same.
    
    Bug: 1295044, b/269623320
    Change-Id: I7169e79872f465cada8fd9826c4471f2a1c21368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285496
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109094}

---
+++ chrome/browser/web_applications/os_integration/shortcut_sub_manager_unittest.cc
@@ -0,0 +1,200 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <map>
+#include <memory>
+#include <utility>
+
+#include "base/files/file_util.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
+#include "chrome/browser/web_applications/os_integration/os_integration_test_override.h"
+#include "chrome/browser/web_applications/proto/web_app_os_integration_state.pb.h"
+#include "chrome/browser/web_applications/test/fake_web_app_provider.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/test/web_app_test.h"
+#include "chrome/browser/web_applications/test/web_app_test_utils.h"
+#include "chrome/browser/web_applications/web_app_command_scheduler.h"
+#include "chrome/browser/web_applications/web_app_icon_generator.h"
+#include "chrome/browser/web_applications/web_app_install_info.h"
+#include "chrome/browser/web_applications/web_app_install_params.h"
+#include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/common/chrome_features.h"
+#include "components/sync/base/time.h"
+#include "components/webapps/browser/install_result_code.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "url/gurl.h"
+
+namespace web_app {
+
+using ::testing::Eq;
+using ::testing::IsFalse;
+
+namespace {
+
+class ShortcutSubManagerTest
+    : public WebAppTest,
+      public ::testing::WithParamInterface<OsIntegrationSubManagersState> {
+ public:
+  const GURL kWebAppUrl = GURL("https://example.com/path/index.html");
+  const int kTotalIconSizes = 9;
+
+  ShortcutSubManagerTest() = default;
+  ~ShortcutSubManagerTest() override = default;
+
+  void SetUp() override {
+    WebAppTest::SetUp();
+    {
+      base::ScopedAllowBlockingForTesting allow_blocking;
+      test_override_ =
+          OsIntegrationTestOverride::OverrideForTesting(base::GetHomeDir());
+    }
+    if (GetParam() == OsIntegrationSubManagersState::kSaveStateToDB) {
+      scoped_feature_list_.InitAndEnableFeatureWithParameters(
+          features::kOsIntegrationSubManagers, {{"stage", "write_config"}});
+    } else if (GetParam() ==
+               OsIntegrationSubManagersState::kSaveStateAndExecute) {
+      scoped_feature_list_.InitAndEnableFeatureWithParameters(
+          features::kOsIntegrationSubManagers,
+          {{"stage", "execute_and_write_config"}});
+    } else {
+      scoped_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{},
+          /*disabled_features=*/{features::kOsIntegrationSubManagers});
+    }
+
+    provider_ = FakeWebAppProvider::Get(profile());
+
+    auto file_handler_manager =
+        std::make_unique<WebAppFileHandlerManager>(profile());
+    auto protocol_handler_manager =
+        std::make_unique<WebAppProtocolHandlerManager>(profile());
+    auto shortcut_manager = std::make_unique<WebAppShortcutManager>(
+        profile(), /*icon_manager=*/nullptr, file_handler_manager.get(),
+        protocol_handler_manager.get());
+    auto os_integration_manager = std::make_unique<OsIntegrationManager>(
+        profile(), std::move(shortcut_manager), std::move(file_handler_manager),
+        std::move(protocol_handler_manager), /*url_handler_manager=*/nullptr);
+
+    provider_->SetOsIntegrationManager(std::move(os_integration_manager));
+    test::AwaitStartWebAppProviderAndSubsystems(profile());
+  }
+
+  void TearDown() override {
+    // Blocking required due to file operations in the shortcut override
+    // destructor.
+    test::UninstallAllWebApps(profile());
+    {
+      base::ScopedAllowBlockingForTesting allow_blocking;
+      test_override_.reset();
+    }
+    WebAppTest::TearDown();
+  }
+
+  web_app::AppId InstallWebAppWithShortcuts(
+      std::map<SquareSizePx, SkBitmap> icon_map) {
+    std::unique_ptr<WebAppInstallInfo> info =
+        std::make_unique<WebAppInstallInfo>();
+    info->start_url = kWebAppUrl;
+    info->title = u"Test App";
+    info->user_display_mode = web_app::mojom::UserDisplayMode::kStandalone;
+    info->icon_bitmaps.any = std::move(icon_map);
+    base::test::TestFuture<const AppId&, webapps::InstallResultCode> result;
+    provider().scheduler().InstallFromInfoWithParams(
+        std::move(info), /*overwrite_existing_manifest_fields=*/true,
+        webapps::WebappInstallSource::OMNIBOX_INSTALL_ICON,
+        result.GetCallback(), WebAppInstallParams());
+    bool success = result.Wait();
+    EXPECT_TRUE(success);
+    if (!success) {
+      return AppId();
+    }
+    EXPECT_EQ(result.Get<webapps::InstallResultCode>(),
+              webapps::InstallResultCode::kSuccessNewInstall);
+    return result.Get<AppId>();
+  }
+
+ protected:
+  WebAppProvider& provider() { return *provider_; }
+  SkBitmap CreateSolidColorIcon(int size, SkColor color) {
+    SkBitmap bitmap;
+    bitmap.allocN32Pixels(size, size);
+    bitmap.eraseColor(color);
+    return bitmap;
+  }
+
+ private:
+  raw_ptr<FakeWebAppProvider> provider_;
+  base::test::ScopedFeatureList scoped_feature_list_;
+  std::unique_ptr<OsIntegrationTestOverride::BlockingRegistration>
+      test_override_;
+};
+
+TEST_P(ShortcutSubManagerTest, ConfigureAppInstall) {
+  std::map<SquareSizePx, SkBitmap> icon_map;
+  icon_map[icon_size::k16] = CreateSolidColorIcon(icon_size::k16, SK_ColorBLUE);
+  icon_map[icon_size::k24] = CreateSolidColorIcon(icon_size::k24, SK_ColorRED);
+  icon_map[icon_size::k128] =
+      CreateSolidColorIcon(icon_size::k128, SK_ColorGREEN);
+  icon_map[icon_size::k512] =
+      CreateSolidColorIcon(icon_size::k512, SK_ColorYELLOW);
+  const AppId& app_id = InstallWebAppWithShortcuts(std::move(icon_map));
+
+  auto state =
+      provider().registrar_unsafe().GetAppCurrentOsIntegrationState(app_id);
+  ASSERT_TRUE(state.has_value());
+  const proto::WebAppOsIntegrationState& os_integration_state = state.value();
+
+  if (AreOsIntegrationSubManagersEnabled()) {
+    ASSERT_THAT(state.value().shortcut().title(), testing::Eq("Test App"));
+    ASSERT_THAT(state.value().shortcut().icon_data_any_size(),
+                testing::Eq(kTotalIconSizes));
+
+    for (const proto::ShortcutIconData& icon_time_map_data :
+         state.value().shortcut().icon_data_any()) {
+      ASSERT_THAT(
+          syncer::ProtoTimeToTime(icon_time_map_data.timestamp()).is_null(),
+          testing::IsFalse());
+    }
+  } else {
+    ASSERT_FALSE(os_integration_state.has_shortcut());
+  }
+
+  // TODO(dmurph): Implement shortcut & color detection if
+  // `AreSubManagersExecuteEnabled()` returns true. https://crbug.com/1404032.
+}
+
+TEST_P(ShortcutSubManagerTest, ConfigureAppUninstall) {
+  std::map<SquareSizePx, SkBitmap> icon_map;
+  icon_map[icon_size::k16] = CreateSolidColorIcon(icon_size::k16, SK_ColorBLUE);
+  icon_map[icon_size::k24] = CreateSolidColorIcon(icon_size::k24, SK_ColorRED);
+  icon_map[icon_size::k128] =
+      CreateSolidColorIcon(icon_size::k128, SK_ColorGREEN);
+  icon_map[icon_size::k512] =
+      CreateSolidColorIcon(icon_size::k512, SK_ColorYELLOW);
+  const AppId& app_id = InstallWebAppWithShortcuts(std::move(icon_map));
+
+  test::UninstallAllWebApps(profile());
+  auto state =
+      provider().registrar_unsafe().GetAppCurrentOsIntegrationState(app_id);
+  ASSERT_FALSE(state.has_value());
+
+  // TODO(dmurph): Implement shortcut & color detection if
+  // `AreSubManagersExecuteEnabled()` returns true. https://crbug.com/1404032.
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    ShortcutSubManagerTest,
+    ::testing::Values(OsIntegrationSubManagersState::kSaveStateToDB,
+                      OsIntegrationSubManagersState::kDisabled),
+    test::GetOsIntegrationSubManagersTestName);
+
+}  // namespace
+
+}  // namespace web_app
