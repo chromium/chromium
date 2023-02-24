50fa7486578c7963905178499af04817a95a5e7d
Mason Freed <masonf@chromium.org>
2023-02-24 23:41:56 +0000

    Two comment-only changes to a11y code
    This goes along with the [1] CL, and is a comment-only change.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4288730
    
    Bug: 1307772
    Change-Id: I66430afa202beb7309575e33eb1786c1824512d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292138
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109885}

--- ui/accessibility/platform/ax_platform_node_base.cc
+++ ui/accessibility/platform/ax_platform_node_base.cc
@@ -1256,8 +1256,7 @@ void AXPlatformNodeBase::ComputeAttributes(PlatformAttributeList* attributes) {
       case ax::mojom::DescriptionFrom::kPopoverAttribute:
         // The following types of markup are mapped to "tooltip":
         // * The title attribute.
-        // * A related popover=something related via popovertoggletarget /
-        // popovershowtarget / popoverhidetarget.
+        // * A popover=something related via the `popovertarget` attribute.
         // * A tooltip related via aria-describedby (see kRelatedElement above).
         from = "tooltip";
         break;
