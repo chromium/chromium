249a3715eaa3076f1f1d7aca014de9a43bfb1a56
Saifuddin Hitawala <hitawala@chromium.org>
2023-02-24 19:08:11 +0000

    Multiplanar: Add fallback to Gles2 for LazySharedContextState
    When creating LazySharedContextState for passthrough command decoder,
    we need to add fallback for GLES 2.0 version if it fails to create
    context with GLES 3.0. This change adds the fallback creation
    option.
    
    Bug: 1366486
    Change-Id: I75ac56f4d6a4f3fdaec2a0b02f5bbf0fb868b18c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285979
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109698}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
@@ -2578,6 +2578,13 @@ bool GLES2DecoderPassthroughImpl::LazySharedContextState::Initialize() {
   auto gl_context = gl::init::CreateGLContext(impl_->context_->share_group(),
                                               gl_surface.get(), attribs);
   if (!gl_context) {
+    LOG(ERROR) << "Failed to create GLES3 context, fallback to GLES2.";
+    attribs.client_major_es_version = 2;
+    attribs.client_minor_es_version = 0;
+    gl_context = gl::init::CreateGLContext(impl_->context_->share_group(),
+                                           gl_surface.get(), attribs);
+  }
+  if (!gl_context) {
     impl_->InsertError(
         GL_INVALID_OPERATION,
         "ContextResult::kFatalFailure: Failed to create GL Context "
