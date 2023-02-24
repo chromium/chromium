30ad43c11d3ece3b80a4fbbf3f15954437dc51bb
Colin Blundell <blundell@chromium.org>
2023-02-24 08:28:56 +0000

    [GL] Fold NativePixmapEGLBindingHelper into NativePixmapEGLBinding
    This helper class was extracted from GLImageNativePixmap to facilitate
    layering the former on top of NativePixmapEGLBinding. Now that that
    relayering is complete, we can fold the helper class into
    NativePixmapEGLBinding.
    
    Bug: 1412692
    Change-Id: I7511d639ac799c5d6526b2610f9d21f43bb6d171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4279703
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109471}

--- ui/ozone/common/native_pixmap_egl_binding.h
+++ ui/ozone/common/native_pixmap_egl_binding.h
@@ -7,27 +7,27 @@
 
 #include <memory>
 
+#include "base/threading/thread_checker.h"
+#include "ui/gfx/color_space.h"
 #include "ui/gfx/native_pixmap.h"
+#include "ui/gl/scoped_egl_image.h"
 #include "ui/ozone/public/native_pixmap_gl_binding.h"
-
-namespace gfx {
-class ColorSpace;
-}
-
-namespace gl {
-class NativePixmapEGLBindingHelper;
-}
 
 namespace ui {
 
 // A binding maintained between NativePixmap and GL Textures in Ozone.
 class NativePixmapEGLBinding : public NativePixmapGLBinding {
  public:
-  NativePixmapEGLBinding(
-      std::unique_ptr<gl::NativePixmapEGLBindingHelper> binding_helper,
-      gfx::BufferFormat format);
+  NativePixmapEGLBinding(const gfx::Size& size,
+                         gfx::BufferFormat format,
+                         gfx::BufferPlane plane);
   ~NativePixmapEGLBinding() override;
 
+  // Create an EGLImage from a given NativePixmap and plane and bind
+  // |texture_id| to |target| followed by binding the image to |target|. The
+  // color space is for the external sampler: When we sample the YUV buffer as
+  // RGB, we need to tell it the encoding (BT.601, BT.709, or BT.2020) and range
+  // (limited or null), and |color_space| conveys this.
   static std::unique_ptr<NativePixmapGLBinding> Create(
       scoped_refptr<gfx::NativePixmap> pixmap,
       gfx::BufferFormat plane_format,
@@ -42,9 +42,20 @@ class NativePixmapEGLBinding : public NativePixmapGLBinding {
   GLenum GetDataType() override;
 
  private:
-  std::unique_ptr<gl::NativePixmapEGLBindingHelper> binding_helper_;
+  // Create an EGLImage from a given NativePixmap and bind |texture_id| to
+  // |target| followed by binding the image to |target|. This EGLImage can be
+  // converted to a GL texture.
+  bool InitializeFromNativePixmap(scoped_refptr<gfx::NativePixmap> pixmap,
+                                  const gfx::ColorSpace& color_space,
+                                  GLenum target,
+                                  GLuint texture_id);
 
+  gl::ScopedEGLImage egl_image_;
+  const gfx::Size size_;
+  THREAD_CHECKER(thread_checker_);
   gfx::BufferFormat format_;
+  scoped_refptr<gfx::NativePixmap> pixmap_;
+  gfx::BufferPlane plane_;
 };
 
 }  // namespace ui
