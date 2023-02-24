1c2b6f87458b6f1596da0f71ad19977f233e37b1
Ari Chivukula <arichiv@chromium.org>
2023-02-24 17:46:58 +0000

    Fix blink_storage_key_fuzzer: Null-dereference READ
    I'm not entirely sure what's causing the issue in the fuzzer as I cannot
    reproduce locally, but let's try using the other mojom serialization
    path that doesn't involving passing message memory around. If the issue
    occurs with this codepath it should be easier to trace down.
    
    Bug: 1419014
    Change-Id: I8d9a6b7d0b874ac1f130be21f0332514ef321b33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289300
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109648}

--- third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
+++ third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
@@ -39,20 +39,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
 
     // Test mojom conversion path.
-    mojo::Message message =
-        blink::mojom::StorageKey::SerializeAsMessage(&*maybe_storage_key);
-    mojo::ScopedMessageHandle handle = message.TakeMojoMessage();
-    message = mojo::Message::CreateFromMessageHandle(&handle);
+    std::vector<uint8_t> mojom_serialized =
+        blink::mojom::StorageKey::Serialize(&*maybe_storage_key);
+    WTF::Vector<uint8_t> mojom_serialized_as_wtf;
+    mojom_serialized_as_wtf.AppendRange(mojom_serialized.begin(),
+                                        mojom_serialized.end());
     blink::BlinkStorageKey mojom_blink_storage_key;
-    blink::mojom::blink::StorageKey::DeserializeFromMessage(
-        std::move(message), &mojom_blink_storage_key);
-    message = blink::mojom::blink::StorageKey::SerializeAsMessage(
-        &mojom_blink_storage_key);
-    handle = message.TakeMojoMessage();
-    message = mojo::Message::CreateFromMessageHandle(&handle);
+    blink::mojom::blink::StorageKey::Deserialize(mojom_serialized_as_wtf,
+                                                 &mojom_blink_storage_key);
+    WTF::Vector<uint8_t> mojom_blink_serialized =
+        blink::mojom::blink::StorageKey::Serialize(&mojom_blink_storage_key);
+    std::vector<uint8_t> mojom_blink_serialized_as_std(
+        mojom_blink_serialized.begin(), mojom_blink_serialized.end());
     blink::StorageKey mojom_storage_key;
-    blink::mojom::StorageKey::DeserializeFromMessage(std::move(message),
-                                                     &mojom_storage_key);
+    blink::mojom::StorageKey::Deserialize(mojom_blink_serialized_as_std,
+                                          &mojom_storage_key);
     assert(maybe_storage_key->ExactMatchForTesting(mojom_storage_key));
 
     // Test type conversion path.
