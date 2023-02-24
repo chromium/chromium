aca4b4e75254f74795429bbd9466f60c5805860e
Rebekah Potter <rbpotter@chromium.org>
2023-02-24 16:58:00 +0000

    WebUI: Serve mojo resources from WebUI resources grd
    Serve mojo resources in a consistent way with other WebUI resources by
    generating a grdp for the mojom-webui files that is included in
    webui_resources.grd.
    
    Remove mojom-webui files, which are used only by WebUI and not other
    mojo JS users, from other grd files that were previously used to
    serve them in shared_resources_data_source.
    
    This allows new shared mojom-webui bindings to be listed in only
    one location (ui/webui/resources/mojo/), rather than listed in the
    shared resources data source, in a grd file, and in
    ui/webui/resources/mojo. This is also a prerequisite step for
    migrating the shared mojom-webui bindings files to TypeScript.
    
    Bug: 1417347, 1132403
    Change-Id: Iee8a86e649189800b108e532ee260c8bb0ff1a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285237
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1109622}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -8,14 +8,8 @@
 
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "content/grit/content_resources.h"
-#include "content/grit/content_resources_map.h"
 #include "content/public/common/url_constants.h"
-#include "mojo/public/js/grit/mojo_bindings_resources.h"
-#include "mojo/public/js/grit/mojo_bindings_resources_map.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
-#include "skia/grit/skia_resources.h"
-#include "skia/grit/skia_resources_map.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/resources/grit/webui_resources.h"
 #include "ui/resources/grit/webui_resources_map.h"
@@ -26,60 +20,39 @@
 #include "chromeos/ash/grit/ash_resources_map.h"
 #include "chromeos/grit/chromeos_resources.h"
 #include "chromeos/grit/chromeos_resources_map.h"
+#include "content/grit/content_resources.h"
+#include "content/grit/content_resources_map.h"
+#include "mojo/public/js/grit/mojo_bindings_resources.h"
+#include "mojo/public/js/grit/mojo_bindings_resources_map.h"
 #endif
 
 namespace content {
 
 namespace {
 
-const std::set<int> GetContentResourceIds() {
-  return std::set<int> {
-    IDR_GEOMETRY_MOJOM_WEBUI_JS, IDR_IMAGE_MOJOM_WEBUI_JS,
-        IDR_ORIGIN_MOJO_WEBUI_JS, IDR_RANGE_MOJOM_WEBUI_JS,
-        IDR_TOKEN_MOJO_WEBUI_JS, IDR_UI_ACCELERATORS_MOJOM_WEBUI_JS,
-        IDR_UI_EVENT_MOJOM_WEBUI_JS, IDR_UI_LATENCY_INFO_MOJOM_WEBUI_JS,
-        IDR_UI_WINDOW_OPEN_DISPOSITION_MOJO_WEBUI_JS, IDR_URL_MOJOM_WEBUI_JS,
-        IDR_VULKAN_INFO_MOJO_JS, IDR_VULKAN_TYPES_MOJO_JS,
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-        IDR_ORIGIN_MOJO_JS, IDR_UI_WINDOW_OPEN_DISPOSITION_MOJO_JS,
-        IDR_UNGUESSABLE_TOKEN_MOJO_JS, IDR_URL_MOJO_JS,
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
+const std::set<int> GetContentResourceIds() {
+  return std::set<int>{
+      IDR_ORIGIN_MOJO_JS,
+      IDR_UI_WINDOW_OPEN_DISPOSITION_MOJO_JS,
+      IDR_UNGUESSABLE_TOKEN_MOJO_JS,
+      IDR_URL_MOJO_JS,
   };
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 const std::set<int> GetChromeosMojoResourceIds() {
   return std::set<int>{
-      IDR_IP_ADDRESS_MOJOM_WEBUI_JS,
-      IDR_NETWORK_CONFIG_CONSTANTS_MOJOM_WEBUI_JS,
-      IDR_NETWORK_CONFIG_MOJOM_WEBUI_JS,
-      IDR_NETWORK_CONFIG_TYPES_MOJOM_WEBUI_JS,
       IDR_NETWORK_DIAGNOSTICS_MOJOM_LITE_JS,
-      IDR_NETWORK_DIAGNOSTICS_MOJOM_WEBUI_JS,
-      IDR_NETWORK_HEALTH_MOJOM_WEBUI_JS,
-      IDR_NETWORK_HEALTH_TYPES_MOJOM_WEBUI_JS,
   };
 }
 
 const std::set<int> GetAshMojoResourceIds() {
   return std::set<int>{
-      IDR_AUTH_FACTOR_CONFIG_MOJOM_WEBUI_JS,
-      IDR_BLUETOOTH_CONFIG_MOJOM_WEBUI_JS,
-      IDR_CELLULAR_SETUP_MOJOM_WEBUI_JS,
-      IDR_ESIM_MANAGER_MOJOM_WEBUI_JS,
-      IDR_HOTSPOT_CONFIG_MOJOM_WEBUI_JS,
       IDR_MULTIDEVICE_DEVICE_SYNC_MOJOM_LITE_JS,
-      IDR_MULTIDEVICE_DEVICE_SYNC_MOJOM_WEBUI_JS,
       IDR_MULTIDEVICE_MULTIDEVICE_SETUP_MOJOM_LITE_JS,
-      IDR_MULTIDEVICE_MULTIDEVICE_SETUP_MOJOM_WEBUI_JS,
       IDR_MULTIDEVICE_MULTIDEVICE_TYPES_MOJOM_LITE_JS,
-      IDR_MULTIDEVICE_MULTIDEVICE_TYPES_MOJOM_WEBUI_JS,
-      IDR_NEARBY_NEARBY_SHARE_SETTINGS_MOJOM_WEBUI_JS,
-      IDR_NEARBY_NEARBY_SHARE_TARGET_TYPES_MOJOM_WEBUI_JS,
   };
 }
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 // Adds all resources with IDs in |resource_ids| to |resources_map|.
 void AddResources(const std::set<int>& resource_ids,
@@ -96,24 +69,26 @@ void AddResources(const std::set<int>& resource_ids,
     source->AddResourcePath(resource.path, resource.id);
   }
 }
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 void PopulateSharedResourcesDataSource(WebUIDataSource* source) {
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::WorkerSrc, "worker-src blob: 'self';");
 
-  // Note: Don't put generated Mojo bindings here. Please explicitly add them to
-  // each WebUI's own data source.
-
-  AddResources(GetContentResourceIds(), kContentResources,
-               kContentResourcesSize, source);
+  // Note: Don't put generated Mojo bindings here. Mojo bindings should be
+  // included in the either the ts_library() target for the UI using them (if
+  // they are only used by one UI) or in //ui/webui/resources/mojo:library
+  // (if used by multiple UIs).
   source->AddResourcePaths(
       base::make_span(kWebuiResources, kWebuiResourcesSize));
-  source->AddResourcePaths(
-      base::make_span(kMojoBindingsResources, kMojoBindingsResourcesSize));
-  source->AddResourcePaths(base::make_span(kSkiaResources, kSkiaResourcesSize));
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   source->AddResourcePaths(
       base::make_span(kAshWebuiCommonResources, kAshWebuiCommonResourcesSize));
+  // Deprecated -lite style mojo bindings.
+  source->AddResourcePaths(
+      base::make_span(kMojoBindingsResources, kMojoBindingsResourcesSize));
+  AddResources(GetContentResourceIds(), kContentResources,
+               kContentResourcesSize, source);
   AddResources(GetChromeosMojoResourceIds(), kChromeosResources,
                kChromeosResourcesSize, source);
   AddResources(GetAshMojoResourceIds(), kAshResources, kAshResourcesSize,
