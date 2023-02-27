845c6e906e6b5d218464d7e6c890ef3cdf3288db
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-02-27 16:25:58 +0000

    [fyfre] Improve the name of the session-tagging feature
    ForYouFreStudy => ForYouFreSyntheticTrialRegistration
    
    The new name aims to be better at differentiating from the
    ForYouFre feature and conveying its purpose
    
    Bug: 1402712
    Change-Id: If03f1cd025f73b9f920c6e5926e1c04ae6dd339d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294733
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110362}

--- chrome/browser/signin/signin_features.cc
+++ chrome/browser/signin/signin_features.cc
@@ -31,16 +31,20 @@ const base::FeatureParam<SigninPromoVariant> kForYouFreSignInPromoVariant{
     /*default_value=*/SigninPromoVariant::kSignIn,
     /*options=*/&kSignInPromoVariantOptions};
 
-// Feature that indicates that we should put the client in a study group to
-// be able to look at metrics in the long term. Does not affect the client's
-// behavior by itself, instead this is done through the `kForYouFre` feature.
-BASE_FEATURE(kForYouFreStudy,
-             "ForYouFreStudy",
+// Feature that indicates that we should put the client in a study group
+// (provided through `kForYouFreStudyGroup`) to be able to look at metrics in
+// the long term. Does not affect the client's behavior by itself, instead this
+// is done through the `kForYouFre` feature.
+BASE_FEATURE(kForYouFreSyntheticTrialRegistration,
+             "ForYouFreSyntheticTrialRegistration",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
 // String that refers to the study group in which this install was enrolled.
-// Used to implement the sticky experiment tracking.
+// Used to implement the sticky experiment tracking. If the value is an empty
+// string, we don't register the client.
 const base::FeatureParam<std::string> kForYouFreStudyGroup{
-    &kForYouFreStudy, /*name=*/"group_name", /*default_value=*/""};
+    &kForYouFreSyntheticTrialRegistration, /*name=*/"group_name",
+    /*default_value=*/""};
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_ANDROID)
 
