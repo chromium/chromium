4f8164a26509572a0915e824f4e60545502e58ed
Andrew Paseltiner <apaseltiner@chromium.org>
2023-02-27 14:54:35 +0000

    Remove obsolete TODO for attestation storage
    This should have been removed in crrev.com/c/4133406.
    
    Bug: 1401347
    Change-Id: I70275d44d16d985c74bac5ed88ddc5c31b315d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4292680
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Quick-Run: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110324}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
@@ -513,7 +513,6 @@ void AttributionDataHostManagerImpl::SourceDataAvailable(
 void AttributionDataHostManagerImpl::TriggerDataAvailable(
     attribution_reporting::SuitableOrigin reporting_origin,
     attribution_reporting::TriggerRegistration data,
-    // TODO(crbug.com/1401347): Propagate `attestation` to storage.
     absl::optional<network::TriggerAttestation> attestation) {
   // This is validated by the Mojo typemapping.
   DCHECK(reporting_origin.IsValid());
