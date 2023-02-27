13ac5864e515b5915d2675b35e6946ca29bead78
Matt Menke <mmenke@chromium.org>
2023-02-27 18:54:29 +0000

    Migrate extensions code off of Value::GetKeyOfType().
    Value::GetKeyOfType() is deprecated, in favor of the Value::Dict::Find
    family of functions.
    
    Bug: 1187001
    Change-Id: Ifdc92318d87b92080330d7e2acb8302421b451db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290163
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110473}

--- chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api_unittest.cc
+++ chrome/browser/extensions/api/enterprise_hardware_platform/enterprise_hardware_platform_api_unittest.cc
@@ -66,19 +66,17 @@ TEST_F(EnterpriseHardwarePlatformAPITest, GetHardwarePlatformInfoAllowed) {
                                                        browser_context());
   ASSERT_TRUE(result);
   ASSERT_TRUE(result->is_dict());
-  ASSERT_EQ(result->DictSize(), 2u);
+  const base::Value::Dict& result_dict = result->GetDict();
+  ASSERT_EQ(result_dict.size(), 2u);
 
-  const base::Value* val =
-      result->FindKeyOfType("manufacturer", base::Value::Type::STRING);
-  ASSERT_TRUE(val);
-  const std::string& manufacturer = val->GetString();
+  const std::string* manufacturer = result_dict.FindString("manufacturer");
+  ASSERT_TRUE(manufacturer);
 
-  val = result->FindKeyOfType("model", base::Value::Type::STRING);
-  ASSERT_TRUE(val);
-  const std::string& model = val->GetString();
+  const std::string* model = result_dict.FindString("model");
+  ASSERT_TRUE(model);
 
-  EXPECT_FALSE(manufacturer.empty());
-  EXPECT_FALSE(model.empty());
+  EXPECT_FALSE(manufacturer->empty());
+  EXPECT_FALSE(model->empty());
 }
 
 TEST_F(EnterpriseHardwarePlatformAPITest,
--- extensions/browser/api/lock_screen_data/lock_screen_item_storage.cc
+++ extensions/browser/api/lock_screen_data/lock_screen_item_storage.cc
@@ -620,9 +620,9 @@ std::set<std::string> LockScreenItemStorage::GetExtensionsWithDataItems(
     if (it.second.is_int() && (include_empty || it.second.GetInt() > 0)) {
       result.insert(it.first);
     } else if (it.second.is_dict()) {
-      const base::Value* count = it.second.FindKeyOfType(
-          kExtensionItemCountPrefKey, base::Value::Type::INTEGER);
-      if (include_empty || (count && count->GetInt() > 0)) {
+      absl::optional<int> count =
+          it.second.GetDict().FindInt(kExtensionItemCountPrefKey);
+      if (include_empty || (count && *count > 0)) {
         result.insert(it.first);
       }
     }
