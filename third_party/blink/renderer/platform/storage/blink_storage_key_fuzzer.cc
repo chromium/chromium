46ae70d6d3f0124bebf752b9bd7d177e69f7040b
Ari Chivukula <arichiv@chromium.org>
2023-02-27 17:51:09 +0000

    [StorageKey] Fix WTF allocations in fuzzer
    Turns out this memory space is special and needs to be initialized
    before use otherwise it'll always SEGV.
    
    Bug: 1419682
    Change-Id: I426037f0498d548a1e16b95944f54f6c0c06e3f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294393
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110428}

--- third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
+++ third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
@@ -6,18 +6,19 @@
 #include "base/i18n/icu_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "mojo/core/embedder/embedder.h"
-#include "mojo/public/cpp/bindings/message.h"
 #include "net/base/features.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/common/storage_key/storage_key_mojom_traits.h"
 #include "third_party/blink/public/mojom/storage_key/storage_key.mojom-shared.h"
 #include "third_party/blink/renderer/platform/storage/blink_storage_key.h"
 #include "third_party/blink/renderer/platform/storage/blink_storage_key_mojom_traits.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 
 struct Environment {
   Environment() {
     CHECK(base::i18n::InitializeICU());
     mojo::core::Init();
+    WTF::Partitions::Initialize();
   }
   // used by ICU integration.
   base::AtExitManager at_exit_manager;
@@ -45,15 +46,15 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     mojom_serialized_as_wtf.AppendRange(mojom_serialized.begin(),
                                         mojom_serialized.end());
     blink::BlinkStorageKey mojom_blink_storage_key;
-    blink::mojom::blink::StorageKey::Deserialize(mojom_serialized_as_wtf,
-                                                 &mojom_blink_storage_key);
+    assert(blink::mojom::blink::StorageKey::Deserialize(
+        mojom_serialized_as_wtf, &mojom_blink_storage_key));
     WTF::Vector<uint8_t> mojom_blink_serialized =
         blink::mojom::blink::StorageKey::Serialize(&mojom_blink_storage_key);
     std::vector<uint8_t> mojom_blink_serialized_as_std(
         mojom_blink_serialized.begin(), mojom_blink_serialized.end());
     blink::StorageKey mojom_storage_key;
-    blink::mojom::StorageKey::Deserialize(mojom_blink_serialized_as_std,
-                                          &mojom_storage_key);
+    assert(blink::mojom::StorageKey::Deserialize(mojom_blink_serialized_as_std,
+                                                 &mojom_storage_key));
     assert(maybe_storage_key->ExactMatchForTesting(mojom_storage_key));
 
     // Test type conversion path.
