c007c1ff6566fb4fd8804a67cb9b904e494c9d95
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-02-27 07:01:51 +0000

    Fix ResourceType enum mismatch between C++ and enum.xml.
    The inconsistency happened due to removal of kImportResource and
    addition of kSpeculationRules.
    
    The removal of kImportResource happened in
    https://crrev.com/c/2798954.
    This confused the several enum values but it should no longer matter.
    This is because the change was old (submitted on Apr 10, 2021) and
    recent data that can be used for analysis are recorded based on the
    current enum values.
    
    The addition of kSpeculationRules happened in
    https://crrev.com/c/3914671.
    This moved the enum value of kMock but this doesn't matter as kMock is
    used only for testing, and should not be recorded as real data.
    
    Bug: 1419759
    Change-Id: Ie8a1a0154a454d326ce591d3c668f58180f55ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290073
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110196}

--- third_party/blink/renderer/platform/loader/fetch/resource.h
+++ third_party/blink/renderer/platform/loader/fetch/resource.h
@@ -74,24 +74,26 @@ class ResourceLoader;
 class ResponseBodyLoaderDrainableInterface;
 class SecurityOrigin;
 
-// |ResourceType| enum values are used in UMAs, so do not change the values of
-// existing types. When adding a new type, append it at the end.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// When adding a new type, append it at the end, and also update the
+// `ResourceType` enum in `tools/metrics/histograms/enums.xml`.
 enum class ResourceType : uint8_t {
   // We do not have kMainResource anymore, which used to have zero value.
   kImage = 1,
-  kCSSStyleSheet,
-  kScript,
-  kFont,
-  kRaw,
-  kSVGDocument,
-  kXSLStyleSheet,
-  kLinkPrefetch,
-  kTextTrack,
-  kAudio,
-  kVideo,
-  kManifest,
-  kSpeculationRules,
-  kMock,  // Only for testing
+  kCSSStyleSheet = 2,
+  kScript = 3,
+  kFont = 4,
+  kRaw = 5,
+  kSVGDocument = 6,
+  kXSLStyleSheet = 7,
+  kLinkPrefetch = 8,
+  kTextTrack = 9,
+  kAudio = 10,
+  kVideo = 11,
+  kManifest = 12,
+  kSpeculationRules = 13,
+  kMock = 14,  // Only for testing
   kMaxValue = kMock
 };
 
