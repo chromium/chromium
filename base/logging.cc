280eb4f65934f49a0c71d14bca52c31c11a64da6
Marshall Greenblatt <marshall@chromium.org>
2023-02-28 00:18:44 +0000

    win: Add missing process_handle.h include
    Fixes the following error:
    
    ../../base/logging.cc(995,24): error: no member named 'GetUniqueIdForProcess' in namespace 'base'
          stream_ << base::GetUniqueIdForProcess() << ':';
                     ~~~~~~^
    1 error generated.
    
    Change-Id: Ibe3f11956ca491456685db446137b8e39248e696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294575
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Marshall Greenblatt <marshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110671}

--- base/logging.cc
+++ base/logging.cc
@@ -20,6 +20,7 @@
 #include "base/debug/crash_logging.h"
 #include "base/immediate_crash.h"
 #include "base/pending_task.h"
+#include "base/process/process_handle.h"
 #include "base/strings/string_piece.h"
 #include "base/task/common/task_annotator.h"
 #include "base/trace_event/base_tracing.h"
@@ -72,7 +73,6 @@ typedef HANDLE FileHandle;
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include "base/process/process_handle.h"
 #define MAX_PATH PATH_MAX
 typedef FILE* FileHandle;
 #endif
