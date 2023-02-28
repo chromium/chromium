50ba8df7c973a260b43a299dfe385067288a2d7b
Ben Segall <bentekkie@google.com>
2023-02-28 14:07:47 +0000

    Add quiet mode to fetch_reclient_cfgs.py
    Running without cipd logged in results in:
    
    `--quiet=false`:
    ```
    fetch reclient_cfgs for RBE project rbe-chrome-untrusted...
    ensure infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang revision/llvmorg-17-init-2387-g68e81d7e-1 in /usr/local/google/home/bentekkie/Workspace/chromium/src/buildtools/reclient_cfgs/chromium-browser-clang
    [P1754601 14:37:05.349 resolver.go:175 D] Resolving package infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang@revision/llvmorg-17-init-2387-g68e81d7e-1 ...
    [P1754601 14:37:05.350 client.go:453 D] RPC chrome-infra-packages.appspot.com/cipd.Repository.ResolveVersion {"host":"chrome-infra-packages.appspot.com", "method":"ResolveVersion", "service":"cipd.Repository"}
    [P1754601 14:37:05.426 resolver.go:180 D] Failed to resolve package infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang@revision/llvmorg-17-init-2387-g68e81d7e-1: prefix "infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang" doesn't exist or not visible to unauthenticated callers, run `cipd auth-login` to login or relogin
    Errors:
      failed to resolve infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang@revision/llvmorg-17-init-2387-g68e81d7e-1 (line 3): prefix "infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang" doesn't exist or not visible to unauthenticated callers, run `cipd auth-login` to login or relogin
    ```
    
    `--quiet=true`:
    ```
    Errors:
      failed to resolve infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang@revision/llvmorg-17-init-2387-g68e81d7e-1 (line 3): prefix "infra_internal/rbe/reclient_cfgs/rbe-chrome-untrusted/chromium-browser-clang" doesn't exist or not visible to unauthenticated callers, run `cipd auth-login` to login or relogin
    ```
    
    
    Change-Id: I37ab9023ab3df294d961772a576714812d2ac3ac
    Bug: b/270201776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288245
    Commit-Queue: Ben Segall <bentekkie@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1110928}

--- buildtools/reclient_cfgs/fetch_reclient_cfgs.py
+++ buildtools/reclient_cfgs/fetch_reclient_cfgs.py
@@ -7,6 +7,7 @@
 
 import argparse
 import glob
+import logging
 import os
 import posixpath
 import re
@@ -32,16 +33,24 @@ def NaclRevision():
         cwd= nacl_dir,
     ).decode('utf-8').strip()
 
-def CipdEnsure(pkg_name, ref, directory):
-    print('ensure %s %s in %s' % (pkg_name, ref, directory))
+class CipdError(Exception):
+  """Raised by fetch_reclient_cfgs on fatal cipd error."""
+
+def CipdEnsure(pkg_name, ref, directory, quiet):
+    logging.info('ensure %s %s in %s' % (pkg_name, ref, directory))
+    log_level = 'warning' if quiet else 'debug'
     ensure_file = """
 $ParanoidMode CheckPresence
 {pkg} {ref}
-""".format(pkg=pkg_name, ref=ref).encode('utf-8')
-    output = subprocess.check_output(
-        ' '.join(['cipd', 'ensure', '-log-level=debug', '-root', directory,
-                  '-ensure-file', '-']), shell=True, input=ensure_file)
-    print(output)
+""".format(pkg=pkg_name, ref=ref)
+    try:
+      output = subprocess.check_output(
+          ' '.join(['cipd', 'ensure', '-log-level=' + log_level,
+                    '-root', directory, '-ensure-file', '-']),
+          shell=True, input=ensure_file, stderr=subprocess.STDOUT, text=True)
+      logging.info(output)
+    except subprocess.CalledProcessError as e:
+      raise CipdError(e.output) from e
 
 def RbeProjectFromEnv():
     instance = os.environ.get('RBE_instance')
@@ -60,13 +69,21 @@ def main():
     parser.add_argument('--cipd_prefix',
                         help='cipd package name prefix',
                         default='infra_internal/rbe/reclient_cfgs')
+    parser.add_argument(
+        '--quiet',
+        help='Suppresses info logs',
+        action='store_true')
 
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.WARNING if args.quiet else logging.INFO,
+                        format="%(message)s")
+
     if not args.rbe_project:
-        print('RBE project is not specified')
+        logging.warning('RBE project is not specified')
         return 1
 
-    print('fetch reclient_cfgs for RBE project %s...' % args.rbe_project)
+    logging.info('fetch reclient_cfgs for RBE project %s...' % args.rbe_project)
 
     cipd_prefix = posixpath.join(args.cipd_prefix, args.rbe_project)
 
@@ -78,15 +95,20 @@ def main():
     for toolchain in tool_revisions:
       revision = tool_revisions[toolchain]
       if not revision:
-        print('failed to detect %s revision' % toolchain)
+        logging.info('failed to detect %s revision' % toolchain)
         continue
 
       toolchain_root = os.path.join(THIS_DIR, toolchain)
       cipd_ref = 'revision/' + revision
       # 'cipd ensure' initializes the directory.
-      CipdEnsure(posixpath.join(cipd_prefix, toolchain),
-                  ref=cipd_ref,
-                  directory=toolchain_root)
+      try:
+        CipdEnsure(posixpath.join(cipd_prefix, toolchain),
+                    ref=cipd_ref,
+                    directory=toolchain_root,
+                    quiet=args.quiet)
+      except CipdError as e:
+         logging.warning(e)
+         return 1
       # support legacy (win-cross-experiments) and new (win-cross)
       # TODO(crbug.com/1407557): drop -experiments support
       wcedir = os.path.join(THIS_DIR, 'win-cross', toolchain)
@@ -103,7 +125,7 @@ def main():
                   if os.path.exists(fname):
                     os.chmod(fname, 0o777)
                     os.remove(fname)
-                  print('Copy from %s to %s...' % (cfg, fname))
+                  logging.info('Copy from %s to %s...' % (cfg, fname))
                   shutil.copy(cfg, fname)
 
     return 0
