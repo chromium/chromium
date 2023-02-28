f901942248e09b1f009947e1865c9e883ebfaee2
Ari Chivukula <arichiv@chromium.org>
2023-02-28 16:52:43 +0000

    Remove expired histograms V8.MemoryExternalFragmentationTotal, V8.MemoryHeapCommitted, V8.MemoryHeapSampleTotalUsed, V8.MemoryHeapUsed
    These histograms haven't been recorded since 2018-21 so removal seems to be
    the correct path here. This is a code-health task.
    
    Bug: 1384707
    Change-Id: I0e0588fa14b2fcb24f2c60ab36e509ad7f948e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295457
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111006}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -377,11 +377,7 @@ static bool IsSingleProcess() {
 }  // namespace
 
 RenderThreadImpl::HistogramCustomizer::HistogramCustomizer() {
-  custom_histograms_.insert("V8.MemoryExternalFragmentationTotal");
   custom_histograms_.insert("V8.MemoryHeapSampleTotalCommitted");
-  custom_histograms_.insert("V8.MemoryHeapSampleTotalUsed");
-  custom_histograms_.insert("V8.MemoryHeapUsed");
-  custom_histograms_.insert("V8.MemoryHeapCommitted");
 }
 
 RenderThreadImpl::HistogramCustomizer::~HistogramCustomizer() {}
