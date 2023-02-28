a773cd87027ad1aeb28a0a9523f5b8c1f02c4744
Daniel White <danieltwhite@google.com>
2023-02-28 15:44:11 +0000

    [iOS] Increase Price Tracking Item's Overflow Menu Tappable Area
    Three additional unittests were added to increase the price
    tracking feature's overall test coverage. These unittests ensure proper
    TableViewCell configuration and that the menu button's effective
    tappable area is 44x44pts. Through these unittests, it was identified
    that the price tracking item's overflow menu's tappable area was only
    43x43pts. In this CL, the menu button's tappable area was increased
    to be the desired size of 44x44pts.
    
    Bug: 1412097, 1420007
    Change-Id: I2c040e42260ee0d6ddd5d45478ded6981888d44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294536
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1110962}

--- ios/chrome/browser/ui/price_notifications/cells/price_notifications_table_view_item_unittest.mm
+++ ios/chrome/browser/ui/price_notifications/cells/price_notifications_table_view_item_unittest.mm
@@ -7,8 +7,11 @@
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/ui/price_notifications/cells/price_notifications_price_chip_view.h"
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
+#import "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -77,4 +80,68 @@ TEST_F(PriceNotificationsTableViewItemTest,
   EXPECT_EQ([cell.priceNotificationsChip.backgroundColor
                 isEqual:[UIColor colorNamed:kGreen100Color]],
             true);
+}
+
+// Ensures that the PriceNotificationsTableViewCell's Menu Button can be tapped
+// if the users taps anywhere within a 44pt x 44pt area around the menu button.
+TEST_F(PriceNotificationsTableViewItemTest,
+       MenuButtonIsTappedWithPointsWithinBoundary) {
+  PriceNotificationsTableViewCell* cell =
+      [[PriceNotificationsTableViewCell alloc]
+            initWithStyle:UITableViewCellStyleDefault
+          reuseIdentifier:nil];
+  CGPoint valid_points[] = {CGPointMake(0, 0),    CGPointMake(10, 5),
+                            CGPointMake(20, -10), CGPointMake(22, 0),
+                            CGPointMake(-22, 0),  CGPointMake(0, 22),
+                            CGPointMake(0, -22),  CGPoint(22, -22)};
+
+  for (CGPoint point : valid_points) {
+    EXPECT_TRUE([cell.menuButton pointInside:point withEvent:nil]);
+  }
+}
+
+// Ensures that the PriceNotificationsTableViewCell's Menu Button is not
+// activated if the users taps anywhere outside of a 44pt x 44pt area around the
+// menu button.
+TEST_F(PriceNotificationsTableViewItemTest,
+       MenuButtonIsTappedWithPointsOutsideBoundary) {
+  PriceNotificationsTableViewCell* cell =
+      [[PriceNotificationsTableViewCell alloc]
+            initWithStyle:UITableViewCellStyleDefault
+          reuseIdentifier:nil];
+  CGPoint valid_points[] = {CGPointMake(23, 0),   CGPointMake(-24, 0),
+                            CGPointMake(0, 23),   CGPointMake(0, -24),
+                            CGPointMake(23, -23), CGPointMake(100, 0)};
+
+  for (CGPoint point : valid_points) {
+    EXPECT_FALSE([cell.menuButton pointInside:point withEvent:nil]);
+  }
+}
+
+// Ensures that a PriceNotificationTableViewItem can properly configure a
+// PriceNotificaitonTableViewCell.
+TEST_F(PriceNotificationsTableViewItemTest,
+       PriceNotificationTableViewCellIsConfiguredByItem) {
+  PriceNotificationsTableViewItem* item =
+      [[PriceNotificationsTableViewItem alloc] initWithType:0];
+  item.title = @"Test Item";
+  item.entryURL = GURL("https://example.com/product/x");
+  item.tracking = NO;
+  item.loading = YES;
+  item.currentPrice = @"$1.00";
+  item.previousPrice = nil;
+  item.productImage = UIGraphicsGetImageFromCurrentImageContext();
+  PriceNotificationsTableViewCell* cell =
+      [[PriceNotificationsTableViewCell alloc]
+            initWithStyle:UITableViewCellStyleDefault
+          reuseIdentifier:nil];
+
+  [item configureCell:cell withStyler:[[ChromeTableViewStyler alloc] init]];
+
+  EXPECT_NSEQ(item.title, cell.titleLabel.text);
+  EXPECT_NSEQ(cell.URLLabel.text, @"example.com");
+  EXPECT_TRUE(cell.isLoading);
+
+  [cell prepareForReuse];
+  EXPECT_FALSE(cell.isLoading);
 }
