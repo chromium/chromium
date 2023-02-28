21c27eddb0b931269bad49e85f06a5f719a3c8a2
Hira Mahmood <hiramahmood@google.com>
2023-02-28 17:44:56 +0000

    [iOS] Declare interface for util functions between BYOT model and UI.
    This CL declares the interface for the functions that will
    be used by the Bring Your Own Tabs UI to interact with the model.
    
    Design doc for reference: go/bring-android-tabs-dd
    
    Fixed: 1418109
    Change-Id: I2612781524a41b5191dd4d1420fdf32f866c5eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4289876
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111045}

---
+++ ios/chrome/browser/ui/recent_tabs/bring_android_tabs/bring_android_tabs_util.cc
@@ -0,0 +1,19 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/ui/recent_tabs/bring_android_tabs/bring_android_tabs_util.h"
+
+std::vector<std::unique_ptr<synced_sessions::DistantTab>>
+PromptTabsForAndroidSwitcher() {
+  // TODO(crbug.com/1418114): Add implementation.
+  return std::vector<std::unique_ptr<synced_sessions::DistantTab>>();
+}
+
+void OnPromptDisplayed() {
+  // TODO(crbug.com/1418114): Add implementation.
+}
+
+void OnUserInteractWithPrompt() {
+  // TODO(crbug.com/1418114): Add implementation.
+}
