57d7f8b4d3d9d0c782f8ad207c4f7472ee4917c2
Kevin Lubick <kjlubick@chromium.org>
2023-03-01 17:04:34 +0000

    SK_SUPPORT_GPU -> SK_GANESH
    Follow-up to https://skia-review.googlesource.com/c/skia/+/650116
    
    Change-Id: I9a6ac6bedc2a13f1a442944eed928a9d0853de85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294869
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111640}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -105,14 +105,15 @@
 //#define SK_R32_SHIFT    16
 
 
-/* Determines whether to build code that supports the GPU backend. Some classes
+/* Determines whether to build code that supports the Ganesh GPU backend. Some classes
    that are not GPU-specific, such as SkShader subclasses, have optional code
-   that is used allows them to interact with the GPU backend. If you'd like to
-   omit this code set SK_SUPPORT_GPU to 0. This also allows you to omit the gpu
-   directories from your include search path when you're not building the GPU
-   backend. Defaults to 1 (build the GPU code).
- */
-//#define SK_SUPPORT_GPU 1
+   that is used allows them to interact with this GPU backend. If you'd like to
+   include this code, include -DSK_GANESH in your cflags or uncomment below.
+   Defaults to not set (No Ganesh GPU backend).
+   This define affects the ABI of Skia, so make sure it matches the client which uses
+   the compiled version of Skia.
+*/
+// #define SK_GANESH
 
 /* Skia makes use of histogram logging macros to trace the frequency of
  * events. By default, Skia provides no-op versions of these macros.
