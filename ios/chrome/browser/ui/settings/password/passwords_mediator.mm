33abfaa720b24b411dc91ef9b9390b392e109630
Noémie St-Onge <noemies@google.com>
2023-03-01 19:03:13 +0000

    [iOS] Notify PasswordCheckupConsumer with necessary information
    These changes add methods to the PasswordCheckupConsumer and use them in
    the PasswordCheckupMediator so that the consumer is provided with the
    necessary information (see mocks) when needed. Unit tests were also
    added to test that the consumer is correctly notified.
    
    Password Checkup mocks:
    https://docs.google.com/presentation/d/1FUvWpYKB4mVWZD4HyN1dwHV8ujI-NZwQnhs5lZJFnHc/edit#slide=id.g19fe51cde0e_0_163
    
    Bug: 1406540
    Change-Id: I921663775f302c3441e182dd2453f4cab6df1130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4250718
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111738}

--- ios/chrome/browser/ui/settings/password/passwords_mediator.mm
+++ ios/chrome/browser/ui/settings/password/passwords_mediator.mm
@@ -300,7 +300,7 @@ bool IsPasswordCheckupEnabled() {
 }
 
 - (void)insecureCredentialsDidChange {
-  // Compromised passwords changes has no effect on UI while check is running.
+  // Insecure password changes have no effect on UI while check is running.
   if (_passwordCheckManager->GetPasswordCheckState() ==
       PasswordCheckState::kRunning)
     return;
@@ -341,8 +341,7 @@ bool IsPasswordCheckupEnabled() {
   }
 }
 
-// Updates the `_consumer` Password Check UI State and Unmuted Compromised
-// Passwords.
+// Updates the `_consumer` Password Check UI State and Insecure Passwords.
 - (void)updateConsumerPasswordCheckState:
     (PasswordCheckState)passwordCheckState {
   DCHECK(self.consumer);
