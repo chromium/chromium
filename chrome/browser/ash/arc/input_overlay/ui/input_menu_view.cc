65d0288d0ffa574e64aac145207f2c447b62ff74
Cici Ruan <cuicuiruan@google.com>
2023-03-01 00:46:50 +0000

    arc/gio:Use a new feedback form link for AlphaV2
    This CL uses a new feedback form for AlphaV2 and keeps the old feedback
    form for Alpha.
    
    Bug: b/271026927
    Test: manual - new form link opened for AlphaV2
    Change-Id: I84bc3708bb35e26cdc807fe8a8a923f041485776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295303
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1111312}

--- chrome/browser/ash/arc/input_overlay/ui/input_menu_view.cc
+++ chrome/browser/ash/arc/input_overlay/ui/input_menu_view.cc
@@ -81,9 +81,13 @@ constexpr int kAlphaSidePadding = 4;
 constexpr int kAlphaLeftMargin = 8;
 constexpr int kAlphaLeftMarginSmall = 4;
 
-constexpr char kFeedbackUrl[] =
+constexpr char kFeedbackUrlAlpha[] =
     "https://docs.google.com/forms/d/e/"
     "1FAIpQLSfL3ttPmopJj65P4EKr--SA18Sc9bbQVMnd0oueMhJu_42TbA/"
+    "viewform?usp=pp_url";
+constexpr char kFeedbackUrl[] =
+    "https://docs.google.com/forms/d/e/"
+    "1FAIpQLScj0zFT7e5c0k_-ZaoFvZeRuCQF4KdgaentLW1IH8sBSlH6Mg/"
     "viewform?usp=pp_url";
 // Entry for the survey form from above link.
 constexpr char kGamePackageName[] = "entry.435412983";
@@ -93,7 +97,7 @@ constexpr char kOsVersion[] = "entry.1961594320";
 // Pass |package_name| by value because the focus will be changed to the
 // browser.
 GURL GetAssembleUrl(std::string package_name) {
-  GURL url(kFeedbackUrl);
+  GURL url(AllowReposition() ? kFeedbackUrl : kFeedbackUrlAlpha);
   url = net::AppendQueryParameter(url, kGamePackageName, package_name);
   url = net::AppendQueryParameter(url, kBoardName,
                                   base::SysInfo::HardwareModelName());
