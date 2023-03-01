e9acf2a19d2a22e7ffbd4140e2ba89baccb658c8
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-01 09:58:46 +0000

    [Passwords Grouping] Handle not encoded characters
    This CL fixes an issue when signon_realm contains ' ' or other
    characters which require encoding.
    
    (cherry picked from commit 1cb789ccdba3b01b99486927f76e4d499c4bb81a)
    
    Fixed: 1420025
    Change-Id: I0718d39f8fb60b1baaee04e56d7bfd6a86b32cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295305
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1110998}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297915
    Reviewed-by: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/branch-heads/5615@{#91}
    Cr-Branched-From: 9c6408ef696e83a9936b82bbead3d41c93c82ee4-refs/heads/main@{#1109224}

--- components/password_manager/core/browser/ui/passwords_grouper.cc
+++ components/password_manager/core/browser/ui/passwords_grouper.cc
@@ -22,18 +22,15 @@ namespace password_manager {
 namespace {
 
 // Returns signon_realm for regular forms and formatted url for federated forms.
-std::string GetSignonRealm(const PasswordForm& form) {
+std::string GetFacetRepresentation(const PasswordForm& form) {
   std::string result = form.signon_realm;
   if (form.IsFederatedCredential()) {
     result = base::UTF16ToUTF8(url_formatter::FormatUrlForSecurityDisplay(
         form.url, url_formatter::SchemeDisplay::SHOW));
   }
-  // Remove trailing '/' because FacetURIs don't have it.
-  if (base::EndsWith(result, "/")) {
-    return result.substr(0, result.size() - 1);
-  }
 
-  return result;
+  return FacetURI::FromPotentiallyInvalidSpec(result)
+      .potentially_invalid_spec();
 }
 
 // An implementation of the disjoint-set data structure
@@ -194,7 +191,7 @@ std::vector<std::string> ExtractSignonRealms(
         sort_key_to_password_forms) {
   std::vector<std::string> result;
   for (const auto& element : sort_key_to_password_forms) {
-    result.push_back(GetSignonRealm(element.second));
+    result.push_back(GetFacetRepresentation(element.second));
   }
   return result;
 }
@@ -372,10 +369,10 @@ void PasswordsGrouper::GroupPasswordsImpl(
     if (form.blocked_by_user) {
       continue;
     }
-    std::string signon_realm = GetSignonRealm(form);
+    std::string facet_uri = GetFacetRepresentation(form);
 
-    DCHECK(map_facet_to_group_id.contains(signon_realm));
-    GroupId group_id = map_facet_to_group_id[signon_realm];
+    DCHECK(map_facet_to_group_id.contains(facet_uri));
+    GroupId group_id = map_facet_to_group_id[facet_uri];
 
     // Store group id for sign-on realm.
     map_signon_realm_to_group_id[SignonRealm(form.signon_realm)] = group_id;
