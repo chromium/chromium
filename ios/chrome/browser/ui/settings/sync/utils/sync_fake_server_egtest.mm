8145c9ba42fe6fe3041861c8da750cdce2221ce6
Huiting Yu <huitingyu@google.com>
2023-03-01 16:23:06 +0000

    [ios] disable testSyncCheckDifferentCacheGuid_SignOutAndSignIn
    Bug: 1420598
    
    Change-Id: I9eb7933a89a2d4ac3b27c4cf1636799297ee9a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299067
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Owners-Override: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1111626}

--- ios/chrome/browser/ui/settings/sync/utils/sync_fake_server_egtest.mm
+++ ios/chrome/browser/ui/settings/sync/utils/sync_fake_server_egtest.mm
@@ -168,7 +168,8 @@ void WaitForAutofillProfileLocallyPresent(const std::string& guid,
 
 // Tests that the local cache guid changes when the user signs out and then
 // signs back in with the same account.
-- (void)testSyncCheckDifferentCacheGuid_SignOutAndSignIn {
+// TODO(crbug.com/1420598): This test is regularly failing.
+- (void)DISABLED_testSyncCheckDifferentCacheGuid_SignOutAndSignIn {
   // Sign in a fake identity, and store the initial sync guid.
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
