e7dd4ae1f51ffcf6db1badb2dcb376b3041f4c02
Matt Menke <mmenke@chromium.org>
2023-03-02 14:38:11 +0000

    Migrate Value::GetKeyOfType() calls in /ios/chrome/browser/autofill/automation
    Value::GetKeyOfType() is deprecated in favor of the
    Value::Dict::Find family of methods.
    
    This CL was uploaded by git cl split.
    
    R=noyau@chromium.org
    
    Bug: 1187001
    Change-Id: Icbd16ae6ce82dad5740d36fb17a5a44c847a0680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4294755
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112200}

--- ios/chrome/browser/autofill/automation/automation_egtest.mm
+++ ios/chrome/browser/autofill/automation/automation_egtest.mm
@@ -107,11 +107,10 @@ base::Value RecipeJsonToValue(const std::string& recipe_json) {
   GREYAssertNil(error, error.localizedDescription);
 
   // Extract the starting URL.
-  base::Value* startURLValue =
-      recipeRoot.FindKeyOfType("startingURL", base::Value::Type::STRING);
+  std::string* startURLValue = recipeRoot.GetDict().FindString("startingURL");
   GREYAssert(startURLValue, @"Test file is missing startingURL.");
 
-  const std::string startURLString(startURLValue->GetString());
+  const std::string startURLString(*startURLValue);
   GREYAssert(!startURLString.empty(), @"startingURL is an empty value.");
 
   _startURL = GURL(startURLString);
