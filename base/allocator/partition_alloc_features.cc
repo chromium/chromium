ec53a834a53b2d2f780e83614036a8dc89a247b5
Pâris <pmeuleman@chromium.org>
2023-03-02 04:10:32 +0000

    DPD/BRP: Split build flag enabling DPD/BRP feature flags
    Having BRP feature flag activated independently of DPD's feature flag
    will allow us to enable BRP before DPD while rolling out on the CQ bots.
    This is helpful as enabling BRP will activate the DPD with a no-op
    behavior if DPD's build flag is enabled. This allows us to evaluate
    accurately the performance impact on the bots without having any
    behavioral impact (crashes).
    
    The feature flags were also renamed and their description updated
    following Dana's suggestion in crrev.com/c/4272229 that they could
    be used in local builds.
    
    Bug: 1420513
    Change-Id: I49dbb0af291df7d880f38d4b866d8dd09d4b20bf
    Fixed: 1420513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4295571
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112047}

--- base/allocator/partition_alloc_features.cc
+++ base/allocator/partition_alloc_features.cc
@@ -32,7 +32,7 @@ const base::FeatureParam<UnretainedDanglingPtrMode>
 
 BASE_FEATURE(kPartitionAllocDanglingPtr,
              "PartitionAllocDanglingPtr",
-#if BUILDFLAG(ENABLE_DANGLING_RAW_PTR_FEATURE_FLAGS_FOR_BOTS)
+#if BUILDFLAG(ENABLE_DANGLING_RAW_PTR_FEATURE_FLAG)
              FEATURE_ENABLED_BY_DEFAULT
 #else
              FEATURE_DISABLED_BY_DEFAULT
@@ -104,8 +104,8 @@ BASE_FEATURE(kPartitionAllocLargeEmptySlotSpanRing,
 
 BASE_FEATURE(kPartitionAllocBackupRefPtr,
              "PartitionAllocBackupRefPtr",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) ||                \
-    BUILDFLAG(ENABLE_DANGLING_RAW_PTR_FEATURE_FLAGS_FOR_BOTS) || \
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) ||    \
+    BUILDFLAG(ENABLE_BACKUP_REF_PTR_FEATURE_FLAG) || \
     (BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && BUILDFLAG(IS_LINUX))
              FEATURE_ENABLED_BY_DEFAULT
 #else
@@ -124,7 +124,8 @@ constexpr FeatureParam<BackupRefPtrEnabledProcesses>::Option
 const base::FeatureParam<BackupRefPtrEnabledProcesses>
     kBackupRefPtrEnabledProcessesParam {
   &kPartitionAllocBackupRefPtr, "enabled-processes",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) || \
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) ||    \
+    BUILDFLAG(ENABLE_BACKUP_REF_PTR_FEATURE_FLAG) || \
     (BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && BUILDFLAG(IS_LINUX))
       BackupRefPtrEnabledProcesses::kNonRenderer,
 #else
