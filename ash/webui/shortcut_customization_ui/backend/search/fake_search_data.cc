0e3e578ba39e237d660e2b461d08ca775e6a4265
Mahmoud Rashad <mmrashad@google.com>
2023-03-02 11:47:32 +0000

    Revert "shortcuts: Add SearchConceptRegistry and SearchConcept"
    This reverts commit fae59bcddd9c2331571babd6820d9ea68dce2a88.
    
    Reason for revert: This change introduced SearchConceptRegistryTest.AddAndRemove which is consistently failing on builder "chromium/ci/Linux Chromium OS ASan LSan Tests" (crbug.com/1420925).
    
    Original change's description:
    > shortcuts: Add SearchConceptRegistry and SearchConcept
    >
    > This CL adds a new class called SearchConceptRegistry that is
    > responsible for managing incoming search concepts and
    > registering them with the LocalSearchService.
    >
    > This CL also adds a new struct `SearchConcept` that represents
    > something that can be searched for; specifically, in the case of
    > shortcuts, it represents a shortcut and all of its accelerators.
    >
    > See the main design doc in the bug (b/266085562) for full context.
    > Note that in the design doc, this class is called SearchTagRegistry.
    > I changed the name for consistency.
    >
    > Test: ash_webui_unittests --gtest_filter=SearchHandlerTest.*
    > Bug: b:266085562
    > Change-Id: I2208980f4187fed0a692f7eb2bc06aebeecb4ace
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244349
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: Camden Bickel <cambickel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1111748}
    
    Bug: 1420925, b:266085562
    Change-Id: I474dbeafb408009ab6a7979c60d67ab55d37c453
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303171
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112142}

--- ash/webui/shortcut_customization_ui/backend/search/fake_search_data.cc
+++ ash/webui/shortcut_customization_ui/backend/search/fake_search_data.cc
@@ -5,15 +5,12 @@
 #include <vector>
 
 #include "ash/public/mojom/accelerator_info.mojom-forward.h"
-#include "ash/public/mojom/accelerator_info.mojom-shared.h"
-#include "ash/webui/shortcut_customization_ui/backend/search/fake_search_data.h"
 #include "ash/webui/shortcut_customization_ui/backend/search/search.mojom.h"
-#include "ash/webui/shortcut_customization_ui/backend/search/search_concept.h"
 #include "ui/base/accelerators/accelerator.h"
 
 namespace ash::shortcut_ui::fake_search_data {
 
-ash::mojom::AcceleratorInfoPtr CreateFakeStandardAcceleratorInfo() {
+ash::mojom::AcceleratorInfoPtr CreateFakeAcceleratorInfo() {
   return ash::mojom::AcceleratorInfo::New(
       /*type=*/ash::mojom::AcceleratorType::kDefault,
       /*state=*/ash::mojom::AcceleratorState::kEnabled,
@@ -26,20 +23,19 @@ ash::mojom::AcceleratorInfoPtr CreateFakeStandardAcceleratorInfo() {
 
 std::vector<ash::mojom::AcceleratorInfoPtr> CreateFakeAcceleratorInfoList() {
   std::vector<ash::mojom::AcceleratorInfoPtr> accelerator_info_list;
-  accelerator_info_list.push_back(CreateFakeStandardAcceleratorInfo());
+  accelerator_info_list.push_back(CreateFakeAcceleratorInfo());
   return accelerator_info_list;
 }
 
 ash::mojom::AcceleratorLayoutInfoPtr CreateFakeAcceleratorLayoutInfo(
     const std::u16string& description,
     ash::mojom::AcceleratorSource source,
-    uint32_t action,
-    ash::mojom::AcceleratorLayoutStyle style) {
+    uint32_t action) {
   return ash::mojom::AcceleratorLayoutInfo::New(
       /*category=*/ash::mojom::AcceleratorCategory::kDebug,
       /*sub_category=*/ash::mojom::AcceleratorSubcategory::kGeneral,
       /*description=*/description,
-      /*style=*/style,
+      /*style=*/ash::mojom::AcceleratorLayoutStyle::kDefault,
       /*source=*/source,
       /*action=*/action);
 }
@@ -51,17 +47,13 @@ CreateFakeSearchResultList() {
   search_results.push_back(shortcut_customization::mojom::SearchResult::New(
       /*accelerator_layout_info=*/CreateFakeAcceleratorLayoutInfo(
           /*description=*/u"first result",
-          /*source=*/ash::mojom::AcceleratorSource::kAsh,
-          /*action=*/FakeActionIds::kAction1,
-          /*style=*/ash::mojom::AcceleratorLayoutStyle::kDefault),
+          /*source=*/ash::mojom::AcceleratorSource::kAsh, /*action=*/1),
       /*accelerator_infos=*/CreateFakeAcceleratorInfoList(),
       /*relevance_score=*/0.5));
   search_results.push_back(shortcut_customization::mojom::SearchResult::New(
       /*accelerator_layout_info=*/CreateFakeAcceleratorLayoutInfo(
           /*description=*/u"second result",
-          /*source=*/ash::mojom::AcceleratorSource::kAsh,
-          /*action=*/FakeActionIds::kAction2,
-          /*style=*/ash::mojom::AcceleratorLayoutStyle::kDefault),
+          /*source=*/ash::mojom::AcceleratorSource::kAsh, /*action=*/2),
       /*accelerator_infos=*/CreateFakeAcceleratorInfoList(),
       /*relevance_score=*/0.5));
 
