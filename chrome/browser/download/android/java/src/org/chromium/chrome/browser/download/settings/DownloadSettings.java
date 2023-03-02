37aa01c579417038d73e7eb300b096f3678d8c2d
Ted Choc <tedchoc@chromium.org>
2023-03-02 19:32:39 +0000

    Pass in the profile to DownloadSettings.
    BUG=1410601
    
    Change-Id: I0dc2a7bcb832d0fc72de9fb2780b199c6b6fe465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303523
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112364}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettings.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettings.java
@@ -16,6 +16,7 @@ import org.chromium.chrome.browser.download.DownloadPromptStatus;
 import org.chromium.chrome.browser.download.R;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
+import org.chromium.chrome.browser.settings.ProfileDependentSetting;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.ManagedPreferenceDelegate;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
@@ -25,11 +26,12 @@ import org.chromium.components.user_prefs.UserPrefs;
 /**
  * Fragment containing Download settings.
  */
-public class DownloadSettings
-        extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+public class DownloadSettings extends PreferenceFragmentCompat
+        implements Preference.OnPreferenceChangeListener, ProfileDependentSetting {
     public static final String PREF_LOCATION_CHANGE = "location_change";
     public static final String PREF_LOCATION_PROMPT_ENABLED = "location_prompt_enabled";
 
+    private Profile mProfile;
     private PrefService mPrefService;
     private DownloadLocationPreference mLocationChangePref;
     private ChromeSwitchPreference mLocationPromptEnabledPref;
@@ -39,7 +41,7 @@ public class DownloadSettings
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, String s) {
         getActivity().setTitle(R.string.menu_downloads);
         SettingsUtils.addPreferencesFromResource(this, R.xml.download_preferences);
-        mPrefService = UserPrefs.get(Profile.getLastUsedRegularProfile());
+        mPrefService = UserPrefs.get(mProfile);
 
         boolean locationManaged = DownloadDialogBridge.isLocationDialogManaged();
 
@@ -73,6 +75,11 @@ public class DownloadSettings
     public void onResume() {
         super.onResume();
         updateDownloadSettings();
+    }
+
+    @Override
+    public void setProfile(Profile profile) {
+        mProfile = profile;
     }
 
     private void updateDownloadSettings() {
