cfbf6533e14c0b3f52f4f97c710264960f1df344
Timofey Chudakov <tchudakov@google.com>
2023-03-02 18:50:51 +0000

    [AF Android] Remove footer from dropdown window.
    Remove footer from dropdown_window.xml layout
    and corresponding functionality on the Java side.
    This is done as part of the efforts to remove
    autofill popup refresh layout, which hasn't been
    launched.
    
    Bug: 1419174
    Change-Id: Ia7d246ae03d5438753296c3597d16f6646467938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4291109
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112328}

--- ui/android/java/src/org/chromium/ui/DropdownPopupWindowInterface.java
+++ ui/android/java/src/org/chromium/ui/DropdownPopupWindowInterface.java
@@ -4,7 +4,6 @@
 
 package org.chromium.ui;
 
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -66,13 +65,6 @@ public interface DropdownPopupWindowInterface {
      * @param clickListener Listener to register.
      */
     void setOnItemClickListener(AdapterView.OnItemClickListener clickListener);
-
-    /**
-     * Adds a non-scrolling View beneath the list. This View will be separated from the main list
-     * by a single divider.
-     * TODO(crbug.com/896349): This currently only works when called before show().
-     */
-    void setFooterView(View footerView);
 
     /**
      * Show the popup. Will have no effect if the popup is already showing.
