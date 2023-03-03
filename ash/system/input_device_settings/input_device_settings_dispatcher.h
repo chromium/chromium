6b0dc85e57aec1446a614e5694083a77f7a67520
David Padlipsky <dpad@google.com>
2023-03-03 01:18:36 +0000

    Settings Split: Implement dispatcher to update gestures library
    Bug: b/241965700
    Change-Id: Iccd9b7dba69dd69c3e57de79ba3f97c8b50609a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4244484
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1112586}

--- ash/system/input_device_settings/input_device_settings_dispatcher.h
+++ ash/system/input_device_settings/input_device_settings_dispatcher.h
@@ -7,17 +7,37 @@
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/input_device_settings_controller.h"
+#include "ash/public/mojom/input_device_settings.mojom.h"
+#include "ui/ozone/public/input_controller.h"
 
 namespace ash {
 
 class ASH_EXPORT InputDeviceSettingsDispatcher
     : InputDeviceSettingsController::Observer {
  public:
-  InputDeviceSettingsDispatcher();
+  explicit InputDeviceSettingsDispatcher(ui::InputController* input_controller);
   InputDeviceSettingsDispatcher(const InputDeviceSettingsDispatcher&) = delete;
   InputDeviceSettingsDispatcher& operator=(
       const InputDeviceSettingsDispatcher&) = delete;
   ~InputDeviceSettingsDispatcher() override;
+
+  // InputDeviceSettingsController::Observer:
+  void OnMouseConnected(const mojom::Mouse& mouse) override;
+  void OnMouseSettingsUpdated(const mojom::Mouse& mouse) override;
+  void OnTouchpadConnected(const mojom::Touchpad& touchpad) override;
+  void OnTouchpadSettingsUpdated(const mojom::Touchpad& touchpad) override;
+  void OnPointingStickConnected(
+      const mojom::PointingStick& pointing_stick) override;
+  void OnPointingStickSettingsUpdated(
+      const mojom::PointingStick& pointing_stick) override;
+
+ private:
+  void DispatchMouseSettings(const mojom::Mouse& mouse);
+  void DispatchPointingStickSettings(
+      const mojom::PointingStick& pointing_stick);
+  void DispatchTouchpadSettings(const mojom::Touchpad& touchpad);
+
+  raw_ptr<ui::InputController> input_controller_;  // Not Owned
 };
 
 }  // namespace ash
