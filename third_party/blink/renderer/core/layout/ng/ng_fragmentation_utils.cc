f63bc8544089358d953433283f223809cca840fe
Morten Stenshorne <mstensho@chromium.org>
2023-03-03 23:10:50 +0000

    Disallow breaks before trailing border/padding in table cells.
    We normally allow a last-resort break between the content box block-end
    and any block-end border/padding, if that can help avoid fragmentainer
    overflow. Don't do this for table cells, as that upsets the row
    stretching machinery. See how PlaceCells() is rerun in
    NGTableRowLayoutAlgorithm::Layout() with a new row size.
    
    This is a simple fix. A more elaborate fix would be to store a flag in
    the break token, saying that we're past the content box (we already have
    one for being past the *border* box), and just keep on allowing such
    breaks before trailing border/padding. But it just doesn't seem worth it
    at this point.
    
    Bug: 1420637
    Change-Id: Ie43f3af898bab5cbf9a062232383adf307376ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4303193
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113046}

--- third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
@@ -518,13 +518,18 @@ NGBreakStatus FinishFragmentation(NGBlockNode node,
     // different pages, lest it be clipped and lost.
     //
     // There is a last-resort breakpoint before trailing border and padding, so
-    // first check if we can break there and still make progress.
+    // first check if we can break there and still make progress. Don't allow a
+    // break here for table cells, though, as that might disturb the row
+    // stretching machinery, causing an infinite loop. We'd add the stretch
+    // amount to the block-size to the content box of the table cell, even
+    // though we're past it.
     DCHECK_GE(desired_intrinsic_block_size, trailing_border_padding);
     DCHECK_GE(desired_block_size, trailing_border_padding);
 
     LayoutUnit subtractable_border_padding;
-    if (desired_block_size > trailing_border_padding)
+    if (desired_block_size > trailing_border_padding && !node.IsTableCell()) {
       subtractable_border_padding = trailing_border_padding;
+    }
 
     LayoutUnit modified_intrinsic_block_size = std::max(
         space_left, desired_intrinsic_block_size - subtractable_border_padding);
