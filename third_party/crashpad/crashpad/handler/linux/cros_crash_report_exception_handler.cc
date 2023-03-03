bff6ac9a6a9bd7fea807cd163859e8fa549cb1f9
Miriam Zimmerman <mutexlox@google.com>
2023-03-03 21:28:45 +0000

    Update Crashpad to 707d0d4dac6717d46e4d5e82f88dfea19df38c5a
    9830fbf3b426 ios: Suppress log-if-missing for kSourceVersion
                 intermediate dump key
    90bba04e2235 Fix some accidental uses of argument-dependent lookup
    707d0d4dac67 Restrict new crash_reporter flag to valid versions
    
    BUG=chromium:1420445
    TEST=CQ
    
    Change-Id: Ida76c8885e0fa770c86f1ef486eac9992b9a0ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4307653
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1112988}

--- third_party/crashpad/crashpad/handler/linux/cros_crash_report_exception_handler.cc
+++ third_party/crashpad/crashpad/handler/linux/cros_crash_report_exception_handler.cc
@@ -18,6 +18,9 @@
 
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
+#if defined(CRASHPAD_IS_IN_CHROMIUM)
+#include "base/system/sys_info.h"
+#endif // CRASHPAD_IS_IN_CHROMIUM
 #include "client/settings.h"
 #include "handler/linux/capture_snapshot.h"
 #include "handler/minidump_to_upload_parameters.h"
@@ -247,15 +250,28 @@ bool CrosCrashReportExceptionHandler::HandleExceptionWithConnection(
   // crash_reporter needs to know the pid and uid of the crashing process.
   std::vector<std::string> argv({"/sbin/crash_reporter"});
 
-  // Used to distinguish between non-fatal and fatal crashes.
-  const ExceptionSnapshot* const exception_snapshot = snapshot->Exception();
-  if (exception_snapshot) {
-    // convert to int32, since crashpad uses -1 as a signal for non-fatal
-    // crashes.
-    argv.push_back(base::StringPrintf(
-        "--chrome_signal=%d",
-        static_cast<int32_t>(exception_snapshot->Exception())));
+#if defined(CRASHPAD_IS_IN_CHROMIUM)
+  int32_t major_version = 0, minor_version = 0, bugfix_version = 0;
+  base::SysInfo::OperatingSystemVersionNumbers(
+      &major_version, &minor_version, &bugfix_version);
+  // The version on which https://crrev.com/c/4265753 landed.
+  constexpr int32_t kFixedVersion = 15363;
+  // TODO(https://crbug.com/1420445): Remove this check (and the
+  // CRASHPAD_IS_IN_CHROMIUM defines) when M115 branches.
+  // (Lacros is guaranteed not to be more than 2 milestones ahead of ash, and
+  // M113 on ash has the relevant crash_reporter change.)
+  if (major_version >= kFixedVersion) {
+    // Used to distinguish between non-fatal and fatal crashes.
+    const ExceptionSnapshot* const exception_snapshot = snapshot->Exception();
+    if (exception_snapshot) {
+      // convert to int32, since crashpad uses -1 as a signal for non-fatal
+      // crashes.
+      argv.push_back(base::StringPrintf(
+          "--chrome_signal=%d",
+          static_cast<int32_t>(exception_snapshot->Exception())));
+    }
   }
+#endif // CRASHPAD_IS_IN_CHROMIUM
 
   argv.push_back("--chrome_memfd=" + std::to_string(file_writer.fd()));
 
