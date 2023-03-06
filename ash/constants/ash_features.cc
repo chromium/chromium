53d93f4dffdbd9c75eef371d42f91c981f87e9dd
Moteva <moteva@google.com>
2023-03-06 22:57:27 +0000

    CrOS Settings: Enable feedback app in settings search feature flag by default.
    Bug= b:258521770
    TEST= the button should show up by default without having to turn on the
    feature flag.
    
    Change-Id: Iaea08f819f3234bc52b41da4cc84d23db029a046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4312049
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113635}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1521,7 +1521,7 @@ BASE_FEATURE(kOsSettingsAppBadgingToggle,
 // are returned.
 BASE_FEATURE(kOsSettingsSearchFeedback,
              "OsSettingsSearchFeedback",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOverviewButton,
              "OverviewButton",
