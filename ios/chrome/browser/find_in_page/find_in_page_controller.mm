6701feb5be97ad352d175490f2316a564706ea57
Quentin Pubert <qpubert@google.com>
2023-03-06 11:37:29 +0000

    [iOS] Announce Native Find in Page results with Voiceover
    In order to make Native Find in Page more accessible, this CL makes
    VoiceOver announce "Result K of N" where K is the currently highlighted
    match and N is the total number of matches, whenever the value of K
    changes. Whenever the value of N changes to 0, VoiceOver announces there
    are no results.
    
    Fixed: 1421051
    Change-Id: I74266fea86d792e9b4802cc37b7bdb4585768360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4306725
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113318}

--- ios/chrome/browser/find_in_page/find_in_page_controller.mm
+++ ios/chrome/browser/find_in_page/find_in_page_controller.mm
@@ -4,6 +4,10 @@
 
 #import "ios/chrome/browser/find_in_page/find_in_page_controller.h"
 
+#import <UIKit/UIKit.h>
+
+#import "base/i18n/number_formatting.h"
+#import "components/strings/grit/components_strings.h"
 #import "components/ukm/ios/ukm_url_recorder.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_model.h"
 #import "ios/chrome/browser/find_in_page/find_in_page_response_delegate.h"
@@ -13,6 +17,7 @@
 #import "ios/web/public/find_in_page/find_in_page_manager_delegate_bridge.h"
 #import "ios/web/public/web_state.h"
 #import "services/metrics/public/cpp/ukm_builders.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -22,6 +27,11 @@ namespace {
 // Keeps find in page search term to be shared between different tabs. Never
 // reset, not stored on disk.
 NSString* gSearchTerm;
+// Accessibility announcement delay, so VoiceOver does not cancel the context
+// string announcement when a new match has been selected.
+// TODO(crbug.com/1395828): This is a temporary workaround. The context string
+// announcement might still fail. A retry mechanism needs to be implemented.
+constexpr int64_t kResultsAnnouncementDelayInNanoseconds = 0.1 * NSEC_PER_SEC;
 }  // namespace
 
 @interface FindInPageController () <CRWFindInPageManagerDelegate>
@@ -128,6 +138,23 @@ NSString* gSearchTerm;
   }
   [self.findInPageModel updateQuery:query matches:matchCount];
   [self.responseDelegate findDidFinishWithUpdatedModel:self.findInPageModel];
+
+  // Announce there are no results with Voiceover if
+  // 1. Native Find in Page uses the Chrome Find Bar UI,
+  // 2. search finished with 0 matches,
+  // 3. and the query was not empty.
+  if (ios::provider::IsNativeFindInPageWithChromeFindBar() && matchCount == 0 &&
+      query.length > 0) {
+    NSString* resultsCountAnnouncementString =
+        l10n_util::GetNSString(IDS_ACCESSIBLE_FIND_IN_PAGE_NO_RESULTS);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                                 kResultsAnnouncementDelayInNanoseconds),
+                   dispatch_get_main_queue(), ^{
+                     UIAccessibilityPostNotification(
+                         UIAccessibilityAnnouncementNotification,
+                         resultsCountAnnouncementString);
+                   });
+  }
 }
 
 - (void)findInPageManager:(web::AbstractFindInPageManager*)manager
@@ -137,6 +164,22 @@ NSString* gSearchTerm;
   // In the model, indices start at 1, hence `index + 1`.
   [self.findInPageModel updateIndex:index + 1 atPoint:CGPointZero];
   [self.responseDelegate findDidFinishWithUpdatedModel:self.findInPageModel];
+
+  if (ios::provider::IsNativeFindInPageWithChromeFindBar()) {
+    // If Native Find in Page uses the Chrome Find Bar UI, announce the new
+    // selected match with Voiceover.
+    NSString* resultsCountAnnouncementString = l10n_util::GetNSStringF(
+        IDS_ACCESSIBLE_FIND_IN_PAGE_COUNT,
+        base::FormatNumber(self.findInPageModel.currentIndex),
+        base::FormatNumber(self.findInPageModel.matches));
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                                 kResultsAnnouncementDelayInNanoseconds),
+                   dispatch_get_main_queue(), ^{
+                     UIAccessibilityPostNotification(
+                         UIAccessibilityAnnouncementNotification,
+                         resultsCountAnnouncementString);
+                   });
+  }
 }
 
 - (void)userDismissedFindNavigatorForManager:
