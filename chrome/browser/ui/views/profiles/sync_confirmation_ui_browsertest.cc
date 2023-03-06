5573c463918e7094b88f5804279f8c766c9b3938
Huanpo Lin <robertlin@chromium.org>
2023-03-06 04:04:56 +0000

    Revert "sheriff: Disable SyncConfirmationUIDialogPixelTest.InvokeUi_default"
    This reverts commit 89bcc75088e079832943939031b386b80727cccb.
    
    Reason for revert:
    The test was causing error was AutofillContextMenuManagerFeedbackUIBrowserTest.DisplaysFeedbackDialogUI instead  of SyncConfirmationUIDialogPixelTest.InvokeUi_default.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28Wayland%29/b8788575737049211041
    This test could be disabled by mistake, so this revert is created.
    
    Original change's description:
    > sheriff: Disable SyncConfirmationUIDialogPixelTest.InvokeUi_default
    >
    > Bug: 1417934
    > Change-Id: Ic9944e8a38a04a488072831174272f6a6377c435
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4273449
    > Reviewed-by: Takashi Sakamoto <tasak@google.com>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Owners-Override: Robert Lin <robertlin@google.com>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1107612}
    
    Bug: 1417934
    Change-Id: Ie9af1dc6d224cbe137ffc35b203b0d96d77d6ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4311383
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113235}

--- chrome/browser/ui/views/profiles/sync_confirmation_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/sync_confirmation_ui_browsertest.cc
@@ -294,9 +294,7 @@ class SyncConfirmationUIDialogPixelTest
   std::unique_ptr<base::ScopedEnvironmentVariableOverride> scoped_env_override_;
 };
 
-// TODO(crbug.com/1417934): Re-enable this test
-IN_PROC_BROWSER_TEST_P(SyncConfirmationUIDialogPixelTest,
-                       DISABLED_InvokeUi_default) {
+IN_PROC_BROWSER_TEST_P(SyncConfirmationUIDialogPixelTest, InvokeUi_default) {
   ShowAndVerifyUi();
 }
 
