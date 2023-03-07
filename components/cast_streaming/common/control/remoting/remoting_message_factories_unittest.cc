704d0287d3debbc60d3593dc03c48e96ad3d9bcb
Jordan Bayles <jophba@chromium.org>
2023-03-07 00:34:39 +0000

    [Media Remoting] populate extra_data with aac_extra_data hack
    This patch resolves an issue where receivers are not getting the AAC
    extra data field, due to an ongoing hacky issue in Chrome where, in
    order to preserve historical behavior, when the codec is set to AAC
    the extra data is placed in a separate aac_extra_data field on desktop
    platforms.
    
    This patch changes the way that media audio decoder configs are
    serialized to and deserialized from Open Screen's remoting protobuf
    message type, so that the extra_data protobuf field is populated from
    the config's aac_extra_data field when the codec is set to AAC.
    
    Bug: b/232554982, 1250841
    Change-Id: Id15eba6d27f3f8af6491b2ed0b0d6463a164791f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4296693
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1113690}

--- components/cast_streaming/common/control/remoting/remoting_message_factories_unittest.cc
+++ components/cast_streaming/common/control/remoting/remoting_message_factories_unittest.cc
@@ -90,7 +90,7 @@ TEST_F(RemotingMessageFactoriesTest, CreateMessageForBufferingStateChange) {
 TEST_F(RemotingMessageFactoriesTest, CreateMessageForAudioConfigChange) {
   const char extra_data[4] = {'A', 'C', 'E', 'G'};
   media::AudioDecoderConfig audio_config(
-      media::AudioCodec::kAAC, media::kSampleFormatF32,
+      media::AudioCodec::kOpus, media::kSampleFormatF32,
       media::CHANNEL_LAYOUT_MONO, 48000,
       std::vector<uint8_t>(std::begin(extra_data), std::end(extra_data)),
       media::EncryptionScheme::kUnencrypted);
