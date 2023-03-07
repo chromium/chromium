bd1e88596844614a0b081d7ff2820e64fcee59a0
Tom Sepez <tsepez@chromium.org>
2023-03-07 19:56:40 +0000

    Avoid momentary dangling ptr in GLApiTest::InitializeAPI()
    Introduce a TerminateAPI() to remove objects in the correct order
    before overwriting them in a second InitializeAPI() call.
    
    Bug: 1401495
    Change-Id: Ia22b30fa2bda0bd765dc6725902dc4b6c8c10021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4288427
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114119}

--- ui/gl/gl_api_unittest.cc
+++ ui/gl/gl_api_unittest.cc
@@ -19,6 +19,11 @@ namespace gl {
 
 class GLApiTest : public testing::Test {
  public:
+  static GLFunctionPointerType GL_BINDING_CALL
+  FakeGLGetProcAddress(const char* proc) {
+    return reinterpret_cast<GLFunctionPointerType>(0x1);
+  }
+
   void SetUp() override {
     fake_extension_string_ = "";
     fake_version_string_ = "";
@@ -29,15 +34,8 @@ class GLApiTest : public testing::Test {
         static_cast<GLGetProcAddressProc>(&FakeGLGetProcAddress));
   }
 
-  static GLFunctionPointerType GL_BINDING_CALL
-  FakeGLGetProcAddress(const char* proc) {
-    return reinterpret_cast<GLFunctionPointerType>(0x1);
-  }
-
   void TearDown() override {
-    api_.reset(nullptr);
-    driver_.reset(nullptr);
-
+    TerminateAPI();
     SetGLImplementation(kGLImplementationNone);
     fake_extension_string_ = "";
     fake_version_string_ = "";
@@ -68,6 +66,11 @@ class GLApiTest : public testing::Test {
 
     driver_->InitializeDynamicBindings(version.get(), extension_set);
     api_->set_version(std::move(version));
+  }
+
+  void TerminateAPI() {
+    api_.reset();
+    driver_.reset();
   }
 
   void SetFakeExtensionString(const char* fake_string) {
@@ -140,6 +143,7 @@ TEST_F(GLApiTest, DisabledExtensionStringTest) {
   SetFakeExtensionString(kFakeExtensions);
   InitializeAPI(nullptr);
   EXPECT_STREQ(kFakeExtensions, GetExtensions());
+  TerminateAPI();
 
   InitializeAPI(kFakeDisabledExtensions);
   EXPECT_STREQ(kFilteredExtensions, GetExtensions());
@@ -154,6 +158,7 @@ TEST_F(GLApiTest, DisabledExtensionBitTest) {
   SetFakeExtensionStrings(kFakeExtensions, std::size(kFakeExtensions));
   InitializeAPI(nullptr);
   EXPECT_TRUE(driver_->ext.b_GL_ARB_timer_query);
+  TerminateAPI();
 
   InitializeAPI(kFakeDisabledExtensions);
   EXPECT_FALSE(driver_->ext.b_GL_ARB_timer_query);
@@ -174,11 +179,11 @@ TEST_F(GLApiTest, DisabledExtensionStringIndexTest) {
 
   SetFakeExtensionStrings(kFakeExtensions, std::size(kFakeExtensions));
   InitializeAPI(nullptr);
-
   EXPECT_EQ(std::size(kFakeExtensions), GetNumExtensions());
   for (uint32_t i = 0; i < std::size(kFakeExtensions); ++i) {
     EXPECT_STREQ(kFakeExtensions[i], GetExtensioni(i));
   }
+  TerminateAPI();
 
   InitializeAPI(kFakeDisabledExtensions);
   EXPECT_EQ(std::size(kFilteredExtensions), GetNumExtensions());
