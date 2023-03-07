cc0e2f3e6c5568d1af3fac037c0885502505d0d2
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-03-07 19:10:31 +0000

    Remove logging from test, remove test from Slow tests
    Bug: 1410608
    Change-Id: I601bf62de4f06c34d95d288d89184e94db4420f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315884
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114089}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-get-items-by-origin.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/dom-storage-get-items-by-origin.js
@@ -2,39 +2,15 @@
   const {dp} = await testRunner.startBlank(
       `Tests that getting DOMStorage items by origin doesn't return an error response\n`);
 
-  const protocolMessages = [];
-  const originalDispatchMessage = DevToolsAPI.dispatchMessage;
-  const originalSendCommand = DevToolsAPI._sendCommand;
-  DevToolsAPI.dispatchMessage = (message) => {
-    protocolMessages.push(message);
-    originalDispatchMessage(message);
-  };
-  DevToolsAPI._sendCommand = (sessionId, method, params) => {
-    protocolMessages.push({sessionId, method, params});
-    return originalSendCommand(sessionId, method, params);
-  }
-  window.onerror = (msg) => testRunner.log('onerror: ' + msg);
-  window.onunhandledrejection = (e) => testRunner.log('onunhandledrejection: ' + e.reason);
-  let errorForLog = new Error();
-  setTimeout(() => {
-    testRunner.log(protocolMessages);
-    testRunner.die('Took longer than 25s', errorForLog);
-  }, 25000);
-
   await dp.DOMStorage.enable();
-  errorForLog = new Error();
   await dp.Page.enable();
-  errorForLog = new Error();
 
   const securityOrigin = (await dp.Page.getResourceTree()).result.frameTree.frame.securityOrigin;
-  errorForLog = new Error();
   const storageId = {securityOrigin, isLocalStorage: true};
   // clear storage to avoid leakage from other tests
   await dp.DOMStorage.clear({storageId});
-  errorForLog = new Error();
 
   const items = (await dp.DOMStorage.getDOMStorageItems({storageId})).result;
-  errorForLog = new Error();
 
   testRunner.log("Get DOM storage items");
   testRunner.log(items);
