50d9365f2d578157119687f9d9e0c735ab6edf81
Tanmoy Mollik <triploblastic@chromium.org>
2023-03-07 13:35:10 +0000

    [IOS][Bkmrks] Fix logic in bookmarks folder chooser coordinator
    This cl is a one-liner. Changes the logic to depend on
    _baseNavigationController instead of _navigationController because
    the latter is initialized later in code. The former is the signal
    used to determine if the latter should be initialized or not.
    
    Bug: 1418642
    Change-Id: I7af1eff982cc7aaa6b197b789be80049cba5dc98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315064
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1113901}

--- ios/chrome/browser/ui/bookmarks/folder_chooser/bookmarks_folder_chooser_coordinator.mm
+++ ios/chrome/browser/ui/bookmarks/folder_chooser/bookmarks_folder_chooser_coordinator.mm
@@ -113,7 +113,7 @@
       initWithBookmarkModel:model
            allowsNewFolders:_allowsNewFolders
                 editedNodes:_hiddenNodes
-               allowsCancel:_navigationController != nil
+               allowsCancel:!_baseNavigationController
              selectedFolder:_selectedFolder
                     browser:self.browser];
   _viewController.delegate = self;
