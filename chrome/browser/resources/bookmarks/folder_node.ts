fe91194f82c9e07b2043c6fab22f69e6359e0854
Di Zhang <dizhangg@chromium.org>
2023-03-08 23:03:01 +0000

    Revert "Fix focus gets lost on chrome://bookmarks page with KeyboardFocusableScrollers enabled"
    This reverts commit 4976c8b13a27a20960e5d8212f728ef151dea7ff.
    
    Reason for revert: Changing method to fix these regressions.
    
    Original change's description:
    > Fix focus gets lost on chrome://bookmarks page with KeyboardFocusableScrollers enabled
    >
    > With feature KeyboardFocusableScrollers enabled,
    > chrome://bookmarks has a focus regression. When focusing from
    > omnibox by tab twice,there is a blue border on the <bookmark-app>
    > element. Since this focus is on a shadow root, we need to set
    > delegatesFocus: true on the <bookmarks-folder-node> to avoid this
    > regression.
    >
    > Change-Id: I48e4035e18920cfbda8c7df32d5887f976280b51
    > Bug: 906857
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4222341
    > Reviewed-by: Di Zhang <dizhangg@chromium.org>
    > Commit-Queue: Di Zhang <dizhangg@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1101943}
    
    Bug: 906857
    Change-Id: Ie17e2ba07d9251aa2953bdd692c9b262e85c4e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4317175
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114835}

--- chrome/browser/resources/bookmarks/folder_node.ts
+++ chrome/browser/resources/bookmarks/folder_node.ts
@@ -32,14 +32,6 @@ export interface BookmarksFolderNodeElement {
 }
 
 export class BookmarksFolderNodeElement extends BookmarksFolderNodeElementBase {
-  constructor() {
-    super();
-    // TODO(dizhangg): With KeyboardFocusableScrollers enabled, need
-    // delegatesFocus=true so bookmarks-folder-node shadow root doesn't get
-    // focused.
-    this.attachShadow({mode: 'open', delegatesFocus: true});
-  }
-
   static get is() {
     return 'bookmarks-folder-node';
   }
