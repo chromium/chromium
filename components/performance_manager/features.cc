bb1a6a0c3cc21129dd8444ce7999cb909b1bd9de
Ming-Ying Chung <mych@chromium.org>
2023-03-08 21:30:08 +0000

    [bfcache] Allow using `BFCachePolicy` on Android
    This change does not enable the policy on Android. Rather it replaces
    the build guard with feature guard. Subsequent actions should be done
    from the server side with a proper Finch experiment.
    
    Bug: 1395281,1225070
    Change-Id: Ic7aff64e7130055a7b5ec0deb6a26db1c2d29f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318682
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114770}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -87,7 +87,12 @@ const base::FeatureParam<int>
 
 BASE_FEATURE(kBFCachePerformanceManagerPolicy,
              "BFCachePerformanceManagerPolicy",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+#if !BUILDFLAG(IS_ANDROID)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kUrgentPageDiscarding,
              "UrgentPageDiscarding",
