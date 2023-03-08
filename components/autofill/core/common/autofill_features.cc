e9daf4cea82d3c946fc37523ae6c4e5dea1cb115
Vidhan <vidhanj@google.com>
2023-03-08 09:34:14 +0000

    Enable feature AutofillEnableProfileDeduplication by default
    Bug: 1168067
    Change-Id: Idee9b4de25dfbd88cf118f3f702f5cafd254c958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4316203
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114447}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -249,7 +249,7 @@ BASE_FEATURE(kAutofillEnableLabelPrecedenceForTurkishAddresses,
 // startup, once per chrome version.
 BASE_FEATURE(kAutofillEnableProfileDeduplication,
              "AutofillEnableProfileDeduplication",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Controls if Autofill supports merging subset names.
 // TODO(crbug.com/1098943): Remove once launched.
