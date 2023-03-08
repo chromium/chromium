92d9dcb57f51996be79ec473aa8f9f89c10c4e39
Yuzu Saijo <yuzus@chromium.org>
2023-03-08 07:14:27 +0000

    Revert "[bfcache] Enable JavaScript eviction NOTREACHED()"
    This reverts commit 9f9ccf7180e26c143617b3ac9af64694b1a805b0.
    
    Reason for revert: Crashes reported
    Bug: 1422421
    
    Original change's description:
    > [bfcache] Enable JavaScript eviction NOTREACHED()
    >
    > We fixed most of the JavaScript eviction that was observed in the wild (should be now less than 3% occurrence of what was observed during investigation).
    > So we are going to enable NOTREACHED() when the page is evicted because of JavaScript execution.
    >
    > We are going to disable the flag if we see any spikes.
    >
    > Design doc: https://docs.google.com/document/d/1uf5KOSHzVlh1zSE_PeCD1IxP_Fu-E5rS7jsLmpzCDVo/edit#
    > Bug:1379227
    >
    > Change-Id: Ie4d1a8bca2f81efa6b49e008109b480428f1c24a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4297453
    > Commit-Queue: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1113807}
    
    Bug: 1379227
    Change-Id: I29ca45594af7278d8f200c5cf63127ad9421d378
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4318325
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1114417}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -106,7 +106,7 @@ BASE_FEATURE(kBackForwardCacheDedicatedWorker,
 
 BASE_FEATURE(kBackForwardCacheNotReachedOnJavaScriptExecution,
              "BackForwardCacheNotReachedOnJavaScriptExecution",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Allows pages with keepalive requests to stay eligible for the back/forward
 // cache. See https://crbug.com/1347101 for more details.
