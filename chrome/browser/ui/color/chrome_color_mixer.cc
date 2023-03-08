bcc4ca123a79d0f8089c21454925fbe88b8b1339
Dana Fried <dfried@chromium.org>
2023-03-08 14:52:25 +0000

    [User Education] Fix help bubble colors in dark mode.
    All blue colors were changed to match the system accent color in a
    sweeping change in 2022, but this was incorrect for help bubbles, which
    are spec'd to always be blue in all modes.
    
    This change reverts the modification for help bubble elements only.
    
    Bug: 1422407
    Change-Id: Iba198100f7b98bab665ff7885b2e3fcd6225ff56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315267
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114525}

--- chrome/browser/ui/color/chrome_color_mixer.cc
+++ chrome/browser/ui/color/chrome_color_mixer.cc
@@ -203,11 +203,9 @@ void AddChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorEyedropperCentralPixelInnerRing] = {SK_ColorBLACK};
   mixer[kColorEyedropperCentralPixelOuterRing] = {SK_ColorWHITE};
   mixer[kColorEyedropperGrid] = {SK_ColorGRAY};
-  mixer[kColorFeaturePromoBubbleBackground] = ui::PickGoogleColor(
-      ui::kColorAccent, kColorFeaturePromoBubbleForeground, 5.3f);
+  mixer[kColorFeaturePromoBubbleBackground] = {gfx::kGoogleBlue700};
   mixer[kColorFeaturePromoBubbleButtonBorder] = {gfx::kGoogleGrey300};
-  mixer[kColorFeaturePromoBubbleCloseButtonInkDrop] = ui::PickGoogleColor(
-      ui::kColorAccent, kColorFeaturePromoBubbleBackground, 2.5f);
+  mixer[kColorFeaturePromoBubbleCloseButtonInkDrop] = {gfx::kGoogleBlue300};
   mixer[kColorFeaturePromoBubbleDefaultButtonBackground] = {
       kColorFeaturePromoBubbleForeground};
   mixer[kColorFeaturePromoBubbleDefaultButtonForeground] = {
