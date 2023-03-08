20713fa52416d4670068a96b935aca4d668566b5
Li Lin <llin@chromium.org>
2023-03-08 23:55:09 +0000

    Send oauth token for requests that use credentials.
    Credentials is needed for get video info request even if it includes
    oauth token to allow writing a auth token to cookie that is used in
    streaming requests.
    
    Previous, all traffic includes credentials/cookie, this CL annotate
    the requests that are marked as use credentials to allow cookie. All
    other traffic won't allow cookie.
    
    A CL on the Google3 side will update to only use credentials for
    get video info request.
    
    Bug: b/262746082
    Change-Id: Ie4eafed0425ee88a2d32b1b67b799a5a134b5fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4214468
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1114871}

--- ash/webui/projector_app/projector_xhr_sender.h
+++ ash/webui/projector_app/projector_xhr_sender.h
@@ -50,9 +50,17 @@ class ProjectorXhrSender {
   virtual ~ProjectorXhrSender();
 
   // Send XHR request and trigger the callback when complete.
-  // This will attempt to fetch OAuth token for the provided email address
-  // and fallback to primary account email if account_email is not provided
-  // or ProjectorViewerUseSecondaryAccount flag is disabled.
+  // There are a few credentials could be used for authorizing a request:
+  // 1. API Key: when `use_api_key` is true, it is a anonymous request. End
+  // user credentials will be ignored.
+  // 2. Account email: By default, primary account email is used to get OAuth
+  // token for authorizing the request. If `account_email` is specified, it
+  // will be used instead.
+  // 3. Credentials: when `use_credentials` is true, the request will be sent
+  // with cookie alongs with auth token. One use case is allowing
+  // get_video_info response to add streaming auth token in cookie. There is
+  // no use case for sending requests with credentials only (without oauth
+  // token).
   virtual void Send(const GURL& url,
                     const std::string& method,
                     const std::string& request_body,
@@ -68,6 +76,7 @@ class ProjectorXhrSender {
                                      const std::string& method,
                                      const std::string& request_body,
                                      const base::Value::Dict& headers,
+                                     bool use_credentials,
                                      SendRequestCallback callback,
                                      const std::string& email,
                                      GoogleServiceAuthError error,
@@ -78,6 +87,7 @@ class ProjectorXhrSender {
                    const std::string& request_body,
                    const std::string& token,
                    const base::Value::Dict& headers,
+                   bool allow_cookie,
                    SendRequestCallback callback);
 
   // Triggered when an XHR request completed.
