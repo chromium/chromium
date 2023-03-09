2b28506b7aa5e5d1ac941c0b6f46b530d1af2909
Gauthier Ambard <gambard@chromium.org>
2023-03-09 10:50:08 +0000

    [iOS] Prepare feature file to be moved
    This CL prepares the feature flag to be moved to shared folder and
    renamed.
    For downstream compatibility.
    
    Bug: 1319852
    Change-Id: I0a807cda392a68815f4fba8266cef12ca067cde2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320709
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115043}

---
+++ ios/chrome/browser/shared/public/features/features.h
@@ -0,0 +1,11 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_PUBLIC_FEATURES_FEATURES_H_
+#define IOS_CHROME_BROWSER_SHARED_PUBLIC_FEATURES_FEATURES_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/ui_feature_flags.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_PUBLIC_FEATURES_FEATURES_H_
