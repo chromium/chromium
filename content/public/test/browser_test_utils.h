de26cb393b0508c2ff84c0441f50a0aafe21d759
Chris Fredrickson <cfredric@chromium.org>
2023-03-09 03:10:50 +0000

    Add warning to USE_MANUAL_REPLY documentation
    I recently found (and fixed, https://crrev.com/c/4318742) a bug caused
    by a script calling domAutomationController.send more than once, and
    consequently corrupting/offsetting results from future calls to EvalJs.
    Users ought to be warned that this is a footgun, and they should prefer
    to use promises instead.
    
    Bug: 1422660
    Change-Id: I4a135faa5df0500c101aafd9c9506e99457d103b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4321088
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1114934}

--- content/public/test/browser_test_utils.h
+++ content/public/test/browser_test_utils.h
@@ -834,6 +834,11 @@ enum EvalJsOptions {
   // script will call domAutomationController.send() with the completion
   // value. Setting this bit will disable that, requiring |script| to provide
   // its own call to domAutomationController.send() instead.
+  //
+  // Beware that if your script calls domAutomationController.send more than
+  // once, it can interfere with the results obtained by future calls to EvalJs.
+  // It is safer to use Promise resolution rather than
+  // domAutomationController.send.
   EXECUTE_SCRIPT_USE_MANUAL_REPLY = (1 << 1),
 
   // By default, when the script passed to EvalJs evaluates to a Promise, the
