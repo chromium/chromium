349d217fdbd1eb9e732db513597bfe1cdc7174b2
Christos Froussios <cfroussios@chromium.org>
2023-03-09 02:52:58 +0000

    Revert "[QuickStart] Thread SharedSecret down into AuthenticatedConnection"
    This reverts commit 42ed2b2ce551b458d4399db99b1f2518ad755404.
    
    Reason for revert:
    Suspected of breaking TargetDeviceBootstrapControllerTest.* tests on Linux ChromiumOS MSan Tests
    It possibly broke MultiDeviceNotificationPresenterTest.* too.
    
    First failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/34379/overview
    
    
    
    Original change's description:
    > [QuickStart] Thread SharedSecret down into AuthenticatedConnection
    >
    > This change threads SharedSecret and SessionID into Authenticated
    > Connection, as it is required for future messages to be sent from
    > the AuthenticatedConnection
    >
    > TEST=Updated Unit Tests
    >
    > Change-Id: Ia95386fb5d45a84c70d8e58ac858f0f61155e931
    > Bug: b/234655072
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320665
    > Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    > Reviewed-by: Curt Clemens <cclem@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1114798}
    
    Bug: b/234655072
    Change-Id: I3721d2d281027a12a620bbb3d8f4452138284d47
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323277
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Owners-Override: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Auto-Submit: Christos Froussios <cfroussios@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1114927}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/incoming_connection.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/incoming_connection.cc
@@ -15,15 +15,17 @@ namespace ash::quick_start {
 IncomingConnection::IncomingConnection(NearbyConnection* nearby_connection,
                                        RandomSessionId session_id,
                                        const std::string& authentication_token)
-    : Connection(nearby_connection, session_id),
+    : Connection(nearby_connection),
       random_session_id_(session_id),
-      pin_(DerivePin(authentication_token)) {}
+      pin_(DerivePin(authentication_token)) {
+  crypto::RandBytes(shared_secret_);
+}
 
 IncomingConnection::IncomingConnection(NearbyConnection* nearby_connection,
                                        RandomSessionId session_id,
                                        const std::string& authentication_token,
-                                       SharedSecret shared_secret)
-    : Connection(nearby_connection, session_id, shared_secret),
+                                       std::array<uint8_t, 32> shared_secret)
+    : Connection(nearby_connection),
       random_session_id_(session_id),
       shared_secret_(shared_secret),
       pin_(DerivePin(authentication_token)) {}
