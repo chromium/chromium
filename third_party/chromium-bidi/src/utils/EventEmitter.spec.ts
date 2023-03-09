d7a01683fb6c967e1303fc65e7fe9ac3b4c1240e
Thiago Perrotta <tperrotta@chromium.org>
2023-03-09 00:02:28 -0500

    enforce copyright notice on all js/ts/py files and fix missing ones (#520)

--- third_party/chromium-bidi/src/utils/EventEmitter.spec.ts
+++ third_party/chromium-bidi/src/utils/EventEmitter.spec.ts
@@ -1,5 +1,6 @@
 /**
- * Copyright 2020 Google Inc. All rights reserved.
+ * Copyright 2020 Google LLC.
+ * Copyright (c) Microsoft Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
