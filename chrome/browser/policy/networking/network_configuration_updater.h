148f8a073813914fe0de89b7785d5750b3bb5520
Pavol Marko <pmarko@google.com>
2023-03-09 12:08:55 +0000

    Split ONC recommended workaround handling and move to anon namespace
    As a preparation for adding histogram support, make the handling of
    "Mark Ethernet ONC fields as Recommended for backwards compatibility"
    more manageable by splitting it into smaller functions and move it into
    a anonymous namespace while we're at it.
    
    No functional change.
    
    Bug: b:271988376
    Change-Id: I0cabb2af21a3f20a8e5cdf8f7b578565d52e22c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315789
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115070}

--- chrome/browser/policy/networking/network_configuration_updater.h
+++ chrome/browser/policy/networking/network_configuration_updater.h
@@ -112,23 +112,6 @@ class NetworkConfigurationUpdater : public ash::PolicyCertificateProvider,
   // Apply the observed policy, i.e. both networks and certificates.
   void ApplyPolicy();
 
-  // Marks IP Address config fields as "Recommended" for Ethernet network
-  // configs without authentication. The reason is that Chrome OS used to treat
-  // Ethernet networks without authentication as unmanaged, so users were able
-  // to edit the IP address even if there was a policy for Ethernet. This
-  // behavior should be preserved for now to not break existing use cases.
-  // TODO(https://crbug.com/931412): Remove this when the server sets
-  // "Recommended".
-  void MarkFieldsAsRecommendedForBackwardsCompatibility(
-      base::Value::List& network_configs_onc);
-
-  // Sets the "Recommended" list of recommended field names in |onc_value|,
-  // which must be a dictionary, to |recommended_field_names|. If a
-  // "Recommended" list already existed in |onc_dict|, it's replaced.
-  void SetRecommended(
-      base::Value::Dict& onc_dict,
-      std::initializer_list<base::StringPiece> recommended_field_names);
-
   std::string LogHeader() const;
 
   // Imports the certificates part of the policy.
