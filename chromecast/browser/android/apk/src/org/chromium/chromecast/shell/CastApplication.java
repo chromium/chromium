3427f2fb9242f8bc213e4cfec69e106bf2cbe7a7
Sean Topping <seantopping@chromium.org>
2023-03-10 22:21:28 +0000

    [Chromecast] Fixes for CastShell APK
    * Add a file with default GN args for Cast Android.
    * Introduce CastApplication.java as a new entry point for the Cast Shell
      APK, launchable via the START_CAST_BROWSER Intent.
    * Start Cast Browser in foreground by using a hidden notification,
      since this is required for displaying web content.
    * Add Android manifest permissions to permit starting activities from
      background.
    * Gets rid of default command line arguments for the Cast browser.
      Instead, just supply arguments from the Cast service via Intent.
    * Use BlinkMessagePortAdapter to fix a crash.
    * Add use_message_port_core build arg to bypass a Blink WebMessage
      incompatibility due to crrev.com/c/3585178
    
    Bug: b/268256423
    Test: Build cast_shell_apk with cast_shell_android.gn args
    Test: Launch cast_shell_apk with START_CAST_BROWSER Intent.
    Test: Launch cast_shell_apk, load web content via Mojo.
    Change-Id: I31fbcd7fe8f8331e38b2777d9f95136c14b4aaf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4234349
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115944}

---
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastApplication.java
@@ -0,0 +1,49 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromecast.shell;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.BundleUtils;
+import org.chromium.base.ContextUtils;
+import org.chromium.base.PathUtils;
+import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.library_loader.LibraryProcessType;
+import org.chromium.ui.base.ResourceBundle;
+
+/**
+ * Entry point for the Android cast shell application.  Handles initialization of information that
+ * needs to be shared across the main activity and the child services created.
+ */
+public class CastApplication extends Application {
+    private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "cast_shell";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ContextUtils.initApplicationContext(this);
+        BundleUtils.setIsBundle(ProductConfig.IS_BUNDLE);
+        ResourceBundle.setAvailablePakLocales(ProductConfig.LOCALES);
+        LibraryLoader.getInstance().setLinkerImplementation(ProductConfig.USE_CHROMIUM_LINKER);
+        LibraryLoader.getInstance().setLibraryProcessType(isBrowserProcess()
+                        ? LibraryProcessType.PROCESS_BROWSER
+                        : LibraryProcessType.PROCESS_CHILD);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
+        ApplicationStatus.initialize(this);
+    }
+
+    private static boolean isBrowserProcess() {
+        return BundleUtils.isBundle()
+                ? ContextUtils.getProcessName().contains("cast_browser_process")
+                : !ContextUtils.getProcessName().contains(":");
+    }
+}
--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserService.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserService.java
@@ -4,10 +4,16 @@
 
 package org.chromium.chromecast.shell;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 
 /**
@@ -24,6 +30,41 @@ import org.chromium.base.Log;
 public class CastBrowserService extends Service {
     private static final String TAG = "CastBrowserService";
 
+    // Provide a unique ID for the notification channel used by the foreground service
+    // notification.
+    // This can be anything as long as it's unique within the application.
+    private static final String UNIQUE_CHANNEL_ID = "org.chromium.chromecast.shell.cast_channel";
+
+    // Title for the app notification. This doesn't have to correspond to the name of the app
+    // itself.
+    private static final String APP_NOTIFICATION_NAME = "Chrome Web Runtime for Cast";
+
+    // Provide a unique ID for the foreground service notification. This can be anything as long
+    // as it's unique within the application.
+    private static final int UNIQUE_NOTIFICATION_ID = 0x40;
+
+    private Notification mNotification;
+
+    private static Notification createNotification() {
+        // Create notification channel to prevent notification warnings.
+        NotificationChannelCompat.Builder builder =
+                new NotificationChannelCompat
+                        .Builder(UNIQUE_CHANNEL_ID, NotificationManagerCompat.IMPORTANCE_NONE)
+                        .setName(APP_NOTIFICATION_NAME);
+        NotificationChannelCompat channel = builder.build();
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(ContextUtils.getApplicationContext());
+        notificationManager.createNotificationChannel(channel);
+        // This notification is not actually shown anywhere, but is required for calls to
+        // startForeground.
+        return new NotificationCompat
+                .Builder(ContextUtils.getApplicationContext(), UNIQUE_CHANNEL_ID)
+                .setContentTitle(APP_NOTIFICATION_NAME)
+                .setContentText("Ready to Cast.")
+                .build();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "onBind");
@@ -33,6 +74,10 @@ public class CastBrowserService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flag, int startId) {
         Log.d(TAG, "onStartCommand");
+        if (mNotification == null) {
+            mNotification = createNotification();
+        }
+        startForeground(UNIQUE_NOTIFICATION_ID, mNotification);
         CastBrowserHelper.initializeBrowser(getApplicationContext(), intent);
         return START_NOT_STICKY;
     }
