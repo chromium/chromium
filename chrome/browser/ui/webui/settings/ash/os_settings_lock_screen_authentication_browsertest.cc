b0c0d15a351fd18eb6baa2ec4e6f1850d55a7646
Martin Bidlingmaier <mbid@google.com>
2023-03-10 12:04:34 +0000

    Move cros settings test fixtures into common base
    This commit refactors duplicated code of browser test fixtures for tests
    using the settings test api into a common base class.
    
    
    Bug: b:243696986
    Change-Id: I04a669a90b4e2a75e2003d7369050b745580ea82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4320927
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115644}

--- chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_authentication_browsertest.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_authentication_browsertest.cc
@@ -2,57 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/login/test/cryptohome_mixin.h"
-#include "chrome/browser/ui/webui/settings/ash/os_settings_browser_test_mixin.h"
-#include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/base/mixin_based_in_process_browser_test.h"
-#include "chromeos/ash/components/dbus/userdataauth/fake_userdataauth_client.h"
-#include "components/account_id/account_id.h"
-#include "components/user_manager/user_names.h"
+#include "chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_browser_test_base.h"
 #include "content/public/test/browser_test.h"
-
-namespace {
-
-const char kCorrectPassword[] = "correct-password";
-const char kIncorrectPassword[] = "incorrect-password";
-
-}  // namespace
 
 namespace ash::settings {
 
 // Test of the authentication dialog in the lock screen page in os-settings.
 class OSSettingsLockScreenAuthenticationTest
-    : public MixinBasedInProcessBrowserTest {
+    : public OSSettingsLockScreenBrowserTestBase {
  public:
-  OSSettingsLockScreenAuthenticationTest() = default;
-
-  void SetUpOnMainThread() override {
-    MixinBasedInProcessBrowserTest::SetUpOnMainThread();
-    FakeUserDataAuthClient::TestApi::Get()->set_enable_auth_check(true);
-
-    auto account = AccountId::FromUserEmail(user_manager::kStubUserEmail);
-    cryptohome_.MarkUserAsExisting(account);
-    cryptohome_.AddGaiaPassword(account, kCorrectPassword);
-  }
-
-  // Opens the ChromeOS settings app and goes to the "lock screen" section.
-  // Does not enter a password.
-  mojom::LockScreenSettingsAsyncWaiter OpenLockScreenSettings() {
-    os_settings_driver_remote_ =
-        mojo::Remote{os_settings_mixin_.OpenOSSettings()};
-    lock_screen_settings_remote_ = mojo::Remote{
-        mojom::OSSettingsDriverAsyncWaiter{os_settings_driver_remote_.get()}
-            .GoToLockScreenSettings()};
-    return mojom::LockScreenSettingsAsyncWaiter{
-        lock_screen_settings_remote_.get()};
-  }
-
- private:
-  ash::CryptohomeMixin cryptohome_{&mixin_host_};
-  OSSettingsBrowserTestMixin os_settings_mixin_{&mixin_host_};
-
-  mojo::Remote<mojom::OSSettingsDriver> os_settings_driver_remote_;
-  mojo::Remote<mojom::LockScreenSettings> lock_screen_settings_remote_;
+  static constexpr const char* kCorrectPassword =
+      OSSettingsLockScreenBrowserTestBase::kPassword;
+  static constexpr char kIncorrectPassword[] = "incorrect-password";
 };
 
 IN_PROC_BROWSER_TEST_F(OSSettingsLockScreenAuthenticationTest,
