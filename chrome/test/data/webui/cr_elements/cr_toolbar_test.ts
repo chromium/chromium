af41f745d527c78a8e7be9de451f1460aee379c2
Viktor Semeniuk <vsemeniuk@google.com>
2023-03-10 21:18:41 +0000

    [Password Manager] Using password manager logo instead of Chrome logo
    Mock: https://screenshot.googleplex.com/5GojvzeghsSBGv2
    Impl: https://screenshot.googleplex.com/AZ8DYNUGLE95KrZ
    
    Bug: 1350947
    Change-Id: I7f5827d9ebcb830c74f9e06d9b3494f8afa9e6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4315845
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115910}

--- chrome/test/data/webui/cr_elements/cr_toolbar_test.ts
+++ chrome/test/data/webui/cr_elements/cr_toolbar_test.ts
@@ -8,7 +8,9 @@
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 
 import {CrToolbarElement} from 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
-import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTrustedHtml} from 'chrome://webui-test/trusted_html.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 // clang-format on
 
@@ -17,15 +19,36 @@ suite('cr-toolbar', function() {
 
   setup(function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
-    toolbar = document.createElement('cr-toolbar');
-    document.body.appendChild(toolbar);
   });
 
   test('AlwaysShowLogo', function() {
+    toolbar = document.createElement('cr-toolbar');
+    document.body.appendChild(toolbar);
+
     toolbar.narrow = true;
     assertFalse(isVisible(toolbar.shadowRoot!.querySelector('picture')));
 
     toolbar.alwaysShowLogo = true;
     assertTrue(isVisible(toolbar.shadowRoot!.querySelector('picture')));
+  });
+
+  test('Custom logo', function() {
+    document.body.innerHTML = getTrustedHtml(`
+      <cr-toolbar id="toolbar">
+        <div id="logo" slot="product-logo">New logo</div>
+      </cr-toolbar>
+    `);
+    flush();
+
+    const toolbar = document.body.querySelector('cr-toolbar');
+    assertTrue(!!toolbar);
+
+    // There is no picture element because it's replaced with slot.
+    assertFalse(isVisible(toolbar.shadowRoot!.querySelector('picture')));
+
+    const newLogo = toolbar.querySelector('#logo');
+    assertTrue(!!newLogo);
+    assertEquals('product-logo', newLogo.assignedSlot!.name);
+    assertEquals('New logo', newLogo.textContent!.trim());
   });
 });
