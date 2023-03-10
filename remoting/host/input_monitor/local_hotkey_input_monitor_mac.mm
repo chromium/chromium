040ad38430bedf480d8369050388a758e4c172bf
Avi Drissman <avi@chromium.org>
2023-03-10 00:05:04 +0000

    Remove GTM usage from remoting/
    There is now NSEvent API to do this (as of 10.6) so switch to that
    API rather than using GTM.
    
    Also, do other modernization cleanup.
    
    Bug: 1423089
    Change-Id: I5a3c79129cea3675d5d8d481ed5cd3ce434ec255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4326949
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1115450}

--- remoting/host/input_monitor/local_hotkey_input_monitor_mac.mm
+++ remoting/host/input_monitor/local_hotkey_input_monitor_mac.mm
@@ -4,10 +4,8 @@
 
 #include "remoting/host/input_monitor/local_hotkey_input_monitor.h"
 
-#include "base/memory/raw_ptr.h"
-#import "base/task/single_thread_task_runner.h"
-
 #import <AppKit/AppKit.h>
+#import <Carbon/Carbon.h>
 
 #include <cstdint>
 #include <utility>
@@ -18,17 +16,15 @@
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "base/synchronization/lock.h"
+#import "base/task/single_thread_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
-#import "third_party/google_toolbox_for_mac/src/AppKit/GTMCarbonEvent.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
-
-// Esc Key Code is 53.
-// http://boredzo.org/blog/wp-content/uploads/2007/05/IMTx-virtual-keycodes.pdf
-static const NSUInteger kEscKeyCode = 53;
 
 namespace remoting {
 namespace {
@@ -65,60 +61,53 @@ class LocalHotkeyInputMonitorMac : public LocalHotkeyInputMonitor {
 }  // namespace
 }  // namespace remoting
 
-@interface LocalHotkeyInputMonitorManager : NSObject {
- @private
-  GTMCarbonHotKey* _hotKey;
-  raw_ptr<remoting::LocalHotkeyInputMonitorMac::EventHandler> _monitor;
-}
+@interface LocalHotkeyInputMonitorManager : NSObject
 
 - (instancetype)initWithMonitor:
     (remoting::LocalHotkeyInputMonitorMac::EventHandler*)monitor;
 
-// Called when the hotKey is hit.
-- (void)hotKeyHit:(GTMCarbonHotKey*)hotKey;
-
-// Must be called when the LocalHotkeyInputMonitorManager is no longer needed.
-// Similar to NSTimer in that more than a simple release is required.
-- (void)invalidate;
-
 @end
 
-@implementation LocalHotkeyInputMonitorManager
+@implementation LocalHotkeyInputMonitorManager {
+  id _eventMonitor;
+
+  raw_ptr<remoting::LocalHotkeyInputMonitorMac::EventHandler> _monitor;
+}
 
 - (instancetype)initWithMonitor:
     (remoting::LocalHotkeyInputMonitorMac::EventHandler*)monitor {
   if ((self = [super init])) {
     _monitor = monitor;
 
-    GTMCarbonEventDispatcherHandler* handler =
-        [GTMCarbonEventDispatcherHandler sharedEventDispatcherHandler];
-    _hotKey = [handler
-        registerHotKey:kEscKeyCode
-             modifiers:(NSEventModifierFlagOption | NSEventModifierFlagControl)
-                target:self
-                action:@selector(hotKeyHit:)
-              userInfo:nil
-           whenPressed:YES];
-    if (!_hotKey) {
-      LOG(ERROR) << "registerHotKey failed.";
-      [self release];
-      return nil;
-    }
+    auto eventHandler = ^NSEvent*(NSEvent* event) {
+      const NSEventModifierFlags requiredModifiers =
+          NSEventModifierFlagOption | NSEventModifierFlagControl;
+      if ((event.keyCode == kVK_Escape) &&
+          (event.modifierFlags & requiredModifiers)) {
+        // Trigger the callback.
+        _monitor->OnDisconnectShortcut();
+
+        // Stop the event propagation.
+        return nil;
+      }
+
+      // Otherwise, let the event continue propagating.
+      return event;
+    };
+
+    _eventMonitor =
+        [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown
+                                              handler:eventHandler];
   }
   return self;
 }
 
-- (void)hotKeyHit:(GTMCarbonHotKey*)hotKey {
-  _monitor->OnDisconnectShortcut();
-}
-
-- (void)invalidate {
-  if (_hotKey) {
-    GTMCarbonEventDispatcherHandler* handler =
-        [GTMCarbonEventDispatcherHandler sharedEventDispatcherHandler];
-    [handler unregisterHotKey:_hotKey];
-    _hotKey = nullptr;
+- (void)dealloc {
+  if (_eventMonitor) {
+    [NSEvent removeMonitor:_eventMonitor];
   }
+
+  [super dealloc];
 }
 
 @end
@@ -156,7 +145,7 @@ class LocalHotkeyInputMonitorMac::Core
   // Task runner on which |window_| is created.
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
 
-  LocalHotkeyInputMonitorManager* manager_;
+  base::scoped_nsobject<LocalHotkeyInputMonitorManager> manager_;
 
   // Invoked in the |caller_task_runner_| thread to report session disconnect
   // requests.
@@ -184,7 +173,6 @@ LocalHotkeyInputMonitorMac::Core::Core(
     base::OnceClosure disconnect_callback)
     : caller_task_runner_(caller_task_runner),
       ui_task_runner_(ui_task_runner),
-      manager_(nil),
       disconnect_callback_(std::move(disconnect_callback)) {
   DCHECK(disconnect_callback_);
 }
@@ -204,21 +192,19 @@ void LocalHotkeyInputMonitorMac::Core::Stop() {
 }
 
 LocalHotkeyInputMonitorMac::Core::~Core() {
-  DCHECK_EQ(manager_, nil);
+  DCHECK_EQ(manager_.get(), nil);
 }
 
 void LocalHotkeyInputMonitorMac::Core::StartOnUiThread() {
   DCHECK(ui_task_runner_->BelongsToCurrentThread());
 
-  manager_ = [[LocalHotkeyInputMonitorManager alloc] initWithMonitor:this];
+  manager_.reset([[LocalHotkeyInputMonitorManager alloc] initWithMonitor:this]);
 }
 
 void LocalHotkeyInputMonitorMac::Core::StopOnUiThread() {
   DCHECK(ui_task_runner_->BelongsToCurrentThread());
 
-  [manager_ invalidate];
-  [manager_ release];
-  manager_ = nil;
+  manager_.reset();
 }
 
 void LocalHotkeyInputMonitorMac::Core::OnDisconnectShortcut() {
