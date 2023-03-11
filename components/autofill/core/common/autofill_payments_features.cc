1f4bd99a7a7fbdacb30531f8b67c58dba0a782b9
Vipul Vinod Koul <koulvipul@google.com>
2023-03-11 00:21:52 +0000

    Setting the AutofillUpstreamAllowAllEmailDomains flag to be disabled by default for all platforms along with re-adding the previously launched platforms in fieldtrial config.
    Change-Id: I2a054c67b0317c7b688c7e9eef1ebabab0d332ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4329955
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1115994}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -237,12 +237,7 @@ BASE_FEATURE(kAutofillUpstreamAllowAdditionalEmailDomains,
 // the user's email domain.
 BASE_FEATURE(kAutofillUpstreamAllowAllEmailDomains,
              "AutofillUpstreamAllowAllEmailDomains",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_DISABLED_BY_DEFAULT
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // When enabled, sets the OAuth2 token in GetUploadDetails requests to Google
 // Payments, in order to provide a better experience for users with server-side
