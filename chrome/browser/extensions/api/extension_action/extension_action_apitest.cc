2681bdcef7fa629c492229fa2efaae841e22797d
Kapil Gupta <kapilgupta@microsoft.com>
2023-03-11 07:25:10 +0000

    Fixing the ExtensionActionAPITest.SetIconInWorkerWithInvalidPath test
    This CL fixes the flaky test for 'setIcon' extension action, on mac.
    Tests multipleWithCallback() and multipleWithPromise() within
    SetIconInWorkerWithInvalidPath test fails.
    FailureReason:
    [INFO:CONSOLE(0)] "[SUCCESS] singleWithCallback", source:  (0)
    [INFO:CONSOLE(0)] "[SUCCESS] singleWithPromise", source:  (0)
    [INFO:CONSOLE(0)] "[SUCCESS] multipleWithCallback", source:  (0)
    [INFO:CONSOLE(0)] "[FAIL] multipleWithPromise: API Test Error in
    multipleWithPromise
    Actual: Error: Failed to set icon 'also_does_not_exist.png': Failed to
    fetch
    Expected: Error: Failed to set icon 'does_not_exist.png': Failed to
    fetch
    
    Extension Action setIcon codeflow:
    The setIcon function in set_icon.js, calls loadImagePath() function
    which calls an async function loadImagePathForServiceWorker() for each
    of the image 'path`in |details| param.
    Since it's async,the error callback can come for either of the images.
    
    Fix:
    Since the error message in failure callbackis specific to image path,
    in the test, we should expect error message for either of the two images
    in multiplePaths object, rather than only for first image.
    
    
    Bug: 1383941
    Change-Id: I223fb94918e38b2699635b6baca258b6c1a185c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4259885
    Commit-Queue: Kapil Gupta <kapilgupta@microsoft.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116051}

--- chrome/browser/extensions/api/extension_action/extension_action_apitest.cc
+++ chrome/browser/extensions/api/extension_action/extension_action_apitest.cc
@@ -1080,15 +1080,7 @@ IN_PROC_BROWSER_TEST_P(MultiActionAPITest, SetIconInTabWithInvalidPath) {
 // Tests calling setIcon() in the service worker with an invalid icon paths
 // specified. Regression test for https://crbug.com/1262029. Regression test for
 // https://crbug.com/1372518.
-// Flaky (https://crbug.com/1383903)
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_SetIconInWorkerWithInvalidPath \
-  DISABLED_SetIconInWorkerWithInvalidPath
-#else
-#define MAYBE_SetIconInWorkerWithInvalidPath SetIconInWorkerWithInvalidPath
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionActionAPITest,
-                       MAYBE_SetIconInWorkerWithInvalidPath) {
+IN_PROC_BROWSER_TEST_F(ExtensionActionAPITest, SetIconInWorkerWithInvalidPath) {
   constexpr char kManifestTemplate[] =
       R"({
            "name": "Bad Icon Path In Worker",
@@ -1100,6 +1092,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionActionAPITest,
 
   constexpr char kBackgroundJs[] =
       R"(let expectedError = "%s";
+         let anotherExpectedError = "%s";
          const singlePath = 'does_not_exist.png';
          const multiplePaths = {
            16: 'does_not_exist.png',
@@ -1119,28 +1112,43 @@ IN_PROC_BROWSER_TEST_F(ExtensionActionAPITest,
                  'Error: ' + expectedError);
              chrome.test.succeed();
            },
+           /*
+              Multiple icons are loaded asynchronously and either one could
+              end up failing first. However only the first error is emitted,
+              we check against both possibilities.
+           */
            function multipleWithCallback() {
-             chrome.action.setIcon({path: multiplePaths}, () => {
-               chrome.test.assertLastError(expectedError);
+             chrome.action.setIcon({ path: multiplePaths }, () => {
+               let errorMessage = chrome.runtime.lastError.message;
+               chrome.test.assertTrue(errorMessage === expectedError
+                 || errorMessage === anotherExpectedError);
                chrome.test.succeed();
              });
            },
-           async function multipleWithPromise() {
-             await chrome.test.assertPromiseRejects(
-                 chrome.action.setIcon({path: multiplePaths}),
-                 'Error: ' + expectedError);
-             chrome.test.succeed();
+           function multipleWithPromise() {
+             chrome.action.setIcon({ path: multiplePaths })
+               .then(() => {
+                 chrome.test.fail();
+               })
+               .catch((error) => {
+                 chrome.test.assertTrue(error.message === expectedError
+                   || error.message === anotherExpectedError);
+                 chrome.test.succeed();
+               });
            }
          ]);)";
 
   constexpr char kExpectedError[] =
       "Failed to set icon 'does_not_exist.png': Failed to fetch";
+  constexpr char kAnotherExpectedError[] =
+      "Failed to set icon 'also_does_not_exist.png': Failed to fetch";
 
   TestExtensionDir test_dir;
   test_dir.WriteManifest(kManifestTemplate);
 
-  test_dir.WriteFile(FILE_PATH_LITERAL("worker.js"),
-                     base::StringPrintf(kBackgroundJs, kExpectedError));
+  test_dir.WriteFile(
+      FILE_PATH_LITERAL("worker.js"),
+      base::StringPrintf(kBackgroundJs, kExpectedError, kAnotherExpectedError));
 
   // Calling setIcon with an invalid path in a service worker context should
   // reject the promise or call the callback with lastError set.
