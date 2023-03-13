621bf2966a4e6bd6e7ba98e019692e6f5cc86a2b
Luc Nguyen <lucnguyen@google.com>
2023-03-13 19:40:15 +0000

    [Code Health] Fix HttpCache.HardReset
    This histogram is obsolete and  no longer exists in the XML files. It
    is still being emitted and sent to servers, causing a small amount of
    wasted storage page.
    
    However, tests depend on this histogram for proper timing, so change
    the histogram into a local histogram (a histogram that is not sent to
    our servers).
    
    Bug: 1423446
    Change-Id: I608e484f4b0f334d3782438ebca1ff0b45e20b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4331138
    Auto-Submit: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116544}

--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -19,6 +19,7 @@
 #include "base/memory/ref_counted.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_macros_local.h"
 #include "base/pickle.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/strcat.h"
@@ -104,7 +105,7 @@ disk_cache::BackendResult HttpCache::DefaultBackend::CreateBackend(
   disk_cache::ResetHandling reset_handling =
       hard_reset_ ? disk_cache::ResetHandling::kReset
                   : disk_cache::ResetHandling::kResetOnError;
-  UMA_HISTOGRAM_BOOLEAN("HttpCache.HardReset", hard_reset_);
+  LOCAL_HISTOGRAM_BOOLEAN("HttpCache.HardReset", hard_reset_);
 #if BUILDFLAG(IS_ANDROID)
   if (app_status_listener_) {
     return disk_cache::CreateCacheBackend(
