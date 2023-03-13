acdc9e4b45aa763211a7c7154cd997278f514d0b
dpapad <dpapad@chromium.org>
2023-03-13 15:52:03 +0000

    CrOS Kerberos dialog: Remove custom tsconfig_base.json file.
    The file was already not being used by [1] and was probably an
    accidental left-over in http://crrev.com/c/4283997.
    
    Also doing a few minor cleanups:
     - Remove unnecessary usage of I18nMixin
     - Use cr-dialog's show-on-attach, remove connectedCallback override.
     - Fix some indentation and formatting.
    
    This is part of the effort to reduce the amount of custom
    tsconfig_base.json files in the codebase.
    
    [1] chrome/browser/resources/chromeos/kerberos:build_ts
    
    Bug: 1412158
    Change-Id: I57143126ab60de466a1cdc56721cd7f0a05ed483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327125
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116399}

--- chrome/browser/resources/chromeos/kerberos/kerberos_settings_redirect.ts
+++ chrome/browser/resources/chromeos/kerberos/kerberos_settings_redirect.ts
@@ -6,35 +6,17 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 
-import {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
-import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './kerberos_settings_redirect.html.js';
 
-
-interface KerberosSettingsRedirectElement {
-  $: {
-    redirectDialog: CrDialogElement,
-  };
-}
-
-const KerberosSettingsRedirectElementBase = I18nMixin(PolymerElement);
-
-class KerberosSettingsRedirectElement extends
-    KerberosSettingsRedirectElementBase {
+class KerberosSettingsRedirectElement extends PolymerElement {
   static get is() {
     return 'kerberos-settings-redirect';
   }
 
   static get template() {
     return getTemplate();
-  }
-
-  override connectedCallback(): void {
-    super.connectedCallback();
-
-    this.$.redirectDialog.showModal();
   }
 
   private onVisitWithoutTicket_(): void {
