127d50ff4d10e62dc0cd49bc08512134a55b6546
Martin Bidlingmaier <mbid@google.com>
2023-03-13 14:42:06 +0000

    Verify UMA in os-settings lock screen auth browsertest
    Bug: b:243696986
    Change-Id: Ib9c0bab0922fdbc3e1fe6e79c8d64d09ac849306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4323320
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116362}

--- chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_authentication_browsertest.cc
+++ chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_authentication_browsertest.cc
@@ -2,8 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/metrics/histogram_base.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/ui/webui/settings/ash/os_settings_lock_screen_browser_test_base.h"
 #include "content/public/test/browser_test.h"
+
+namespace {
+
+// Name and value of the metric that records authentication on the lock screen
+// page.
+const char kPinUnlockUmaHistogramName[] = "Settings.PinUnlockSetup";
+const base::HistogramBase::Sample kEnterPasswordCorrectly = 1;
+
+}  // namespace
 
 namespace ash::settings {
 
@@ -18,19 +29,39 @@ class OSSettingsLockScreenAuthenticationTest
 
 IN_PROC_BROWSER_TEST_F(OSSettingsLockScreenAuthenticationTest,
                        SuccessfulUnlock) {
+  base::HistogramTester histograms;
   auto lock_screen_settings = OpenLockScreenSettings();
   lock_screen_settings.AssertAuthenticated(false);
+  histograms.ExpectBucketCount(kPinUnlockUmaHistogramName,
+                               kEnterPasswordCorrectly, 0);
+
   lock_screen_settings.Authenticate(kCorrectPassword);
+
   lock_screen_settings.AssertAuthenticated(true);
+  histograms.ExpectBucketCount(kPinUnlockUmaHistogramName,
+                               kEnterPasswordCorrectly, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsLockScreenAuthenticationTest, FailedUnlock) {
+  base::HistogramTester histograms;
   auto lock_screen_settings = OpenLockScreenSettings();
+
   lock_screen_settings.AssertAuthenticated(false);
+  histograms.ExpectBucketCount(kPinUnlockUmaHistogramName,
+                               kEnterPasswordCorrectly, 0);
+
   lock_screen_settings.AuthenticateIncorrectly(kIncorrectPassword);
+
   lock_screen_settings.AssertAuthenticated(false);
+  histograms.ExpectBucketCount(kPinUnlockUmaHistogramName,
+                               kEnterPasswordCorrectly, 0);
+
+  // Check that we can still authenticate after an unsuccessful attempt:
   lock_screen_settings.Authenticate(kCorrectPassword);
+
   lock_screen_settings.AssertAuthenticated(true);
+  histograms.ExpectBucketCount(kPinUnlockUmaHistogramName,
+                               kEnterPasswordCorrectly, 1);
 }
 
 }  // namespace ash::settings
