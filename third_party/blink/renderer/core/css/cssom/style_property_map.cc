26bfa5807606f6c7fc3ec575123e53894215c49c
Steinar H. Gunderson <sesse@chromium.org>
2023-03-14 15:53:57 +0000

    [M108-LTS] In Typed CSSOM, reject adding to something that is not a list.
    M108 merge issues:
      third_party/blink/renderer/core/css/cssom/style_property_map.cc:
        The check before the added IsValueList check isn't present in 108
    
    (cherry picked from commit 7301cf1e40fdd97594ea491676b867cf4e577edc)
    
    Fixed: 1417176
    Change-Id: Idef1a81af46d334c181979778c28f19ce6369718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4293477
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1110281}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4306796
    Commit-Queue: Zakhar Voit <voit@google.com>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Owners-Override: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5359@{#1405}
    Cr-Branched-From: 27d3765d341b09369006d030f83f582a29eb57ae-refs/heads/main@{#1058933}

--- third_party/blink/renderer/core/css/cssom/style_property_map.cc
+++ third_party/blink/renderer/core/css/cssom/style_property_map.cc
@@ -365,6 +365,17 @@ void StylePropertyMap::append(
 
   CSSValueList* current_value = nullptr;
   if (const CSSValue* css_value = GetProperty(property_id)) {
+    if (!css_value->IsValueList()) {
+      // The standard doesn't seem to cover this explicitly
+      // (https://github.com/w3c/css-houdini-drafts/issues/823),
+      // but the only really reasonable solution seems to be
+      // to throw a TypeError.
+      //
+      // This covers e.g. system-wide CSS keywords, like inherit.
+      exception_state.ThrowTypeError(
+          "Cannot append to something that is not a list");
+      return;
+    }
     current_value = To<CSSValueList>(css_value)->Copy();
   } else {
     current_value = CssValueListForPropertyID(property_id);
