12d1d85762f632142b3a432dd04cfccfbb59342e
Mustafa Emre Acer <meacer@chromium.org>
2023-03-14 04:41:37 +0000

    Disable HTTPS Upgrades in prerender tests
    This CL temporarily disables kHttpsUpgrades feature in tests in
    preparation of enabling the feature by default on tip of tree.
    
    The tests will be fixed properly in future CLs and re-enable
    the HTTPS Upgrades feature.
    
    This CL was uploaded by git cl split. The original CL is at
    https://chromium-review.googlesource.com/c/chromium/src/+/4312951
    
    R=lingqi@chromium.org
    
    Bug: 1424194
    Change-Id: If56b49d93bbe2700cf0dd555796c78eaaaa28a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4335861
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116780}

--- chrome/browser/preloading/prerender/prerender_browsertest.cc
+++ chrome/browser/preloading/prerender/prerender_browsertest.cc
@@ -8,10 +8,12 @@
 #include "base/functional/bind.h"
 #include "base/path_service.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/prefetch/prefetch_prefs.h"
 #include "chrome/browser/preloading/prerender/prerender_utils.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/prefs/pref_service.h"
@@ -316,8 +318,11 @@ IN_PROC_BROWSER_TEST_F(PrerenderHoldbackBrowserTest,
 class PrerenderMainFrameNavigationBrowserTest : public PrerenderBrowserTest {
  public:
   PrerenderMainFrameNavigationBrowserTest() {
-    feature_list_.InitAndEnableFeature(
-        blink::features::kPrerender2MainFrameNavigation);
+    feature_list_.InitWithFeatures(
+        {blink::features::kPrerender2MainFrameNavigation},
+        // TODO(crbug.com/1394910): Use HTTPS URLs in tests to avoid having to
+        // disable this feature.
+        {features::kHttpsUpgrades});
   }
 
  private:
