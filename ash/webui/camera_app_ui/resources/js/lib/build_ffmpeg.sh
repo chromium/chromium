187db51d0a318bf496b488318176b46b3ba48067
Kam Kwankajornkiet <kamchonlathorn@google.com>
2023-03-14 04:50:39 +0000

    CCA: Add h264 demuxer to FFmpeg
    To record time-lapse video using WebCodecs approach, h264 demuxer is
    added to FFmpeg to allow saving a video file from h264
    EncodedVideoChunks. The new FFmpeg wasm file size increases 2 kb.
    
    Also add a patch to allow reading the video dimension defined from JS to
    avoid using decoder to read first few frames to extract the metadata.
    
    Bug: b:236800499
    Test: tast run <DUT> camera.CCAUIRecordVideo*
    Change-Id: I062d3407b73f47e5db088fabc5c255b28ecf8dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4270315
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116784}

--- ash/webui/camera_app_ui/resources/js/lib/build_ffmpeg.sh
+++ ash/webui/camera_app_ui/resources/js/lib/build_ffmpeg.sh
@@ -22,6 +22,7 @@ do_copy_ffmpeg() {
 
   # checkout specific commit to keep code base consistent to the patch
   # TODO(b/199980849): Fix pthread issue to be able to use latest FFMpeg update.
+  # TODO(b/199980849): Remove video_size patch once the PR is sent upstream.
   local build_commit=90810bb
   (
     cd $ffmpeg_dir;
@@ -85,6 +86,7 @@ do_configure() {
     --enable-swscale
     --enable-protocol=file
     --enable-protocol=pipe
+    --enable-demuxer=h264
     --enable-demuxer=matroska
     --enable-demuxer=rawvideo
     --enable-muxer=gif
