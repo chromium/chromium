62d0624a0961ce9ee2fc0f4f8427b0a3bcc53fea
Luc Nguyen <lucnguyen@google.com>
2023-03-14 00:14:58 +0000

    [Code Health] Remove Settings.JsonDataReadSizeKilobytes.*
    Although the histogram is expired, we are still receiving data from
    one of its "variant" on the serverside. In particular:
    Settings.JsonDataReadSizeKilobytes.Network_Persistent_State
    
    This is because the "Network_Persistent_State" suffix is not in
    histogram_suffixes_list.xml, so it is not being detected as
    expired, and is hence being sent.
    
    Since the histogram is intended to be expired, just remove it.
    
    For reference, documented old values @ go/etqpo (Google-internal).
    
    Bug: 1423446
    Change-Id: Ia7be074927eaae6f2f46ec1b53b23237aad64e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327823
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1116706}

--- components/prefs/json_pref_store.cc
+++ components/prefs/json_pref_store.cc
@@ -106,24 +106,6 @@ PersistentPrefStore::PrefReadError HandleReadErrors(
   return PersistentPrefStore::PREF_READ_ERROR_NONE;
 }
 
-// Records a sample for |size| in the Settings.JsonDataReadSizeKilobytes
-// histogram suffixed with the base name of the JSON file under |path|.
-void RecordJsonDataSizeHistogram(const base::FilePath& path, size_t size) {
-  std::string spaceless_basename;
-  base::ReplaceChars(path.BaseName().MaybeAsASCII(), " ", "_",
-                     &spaceless_basename);
-
-  // The histogram below is an expansion of the UMA_HISTOGRAM_CUSTOM_COUNTS
-  // macro adapted to allow for a dynamically suffixed histogram name.
-  // Note: The factory creates and owns the histogram.
-  // This histogram is expired but the code was intentionally left behind so
-  // it can be re-enabled on Stable in a single config tweak if needed.
-  base::HistogramBase* histogram = base::Histogram::FactoryGet(
-      "Settings.JsonDataReadSizeKilobytes." + spaceless_basename, 1, 10000, 50,
-      base::HistogramBase::kUmaTargetedHistogramFlag);
-  histogram->Add(static_cast<int>(size) / 1024);
-}
-
 std::unique_ptr<JsonPrefStore::ReadResult> ReadPrefsFromDisk(
     const base::FilePath& path) {
   int error_code;
@@ -135,9 +117,6 @@ std::unique_ptr<JsonPrefStore::ReadResult> ReadPrefsFromDisk(
       HandleReadErrors(read_result->value.get(), path, error_code, error_msg);
   read_result->no_dir = !base::PathExists(path.DirName());
   read_result->num_bytes_read = deserializer.get_last_read_size();
-
-  if (read_result->error == PersistentPrefStore::PREF_READ_ERROR_NONE)
-    RecordJsonDataSizeHistogram(path, deserializer.get_last_read_size());
 
   return read_result;
 }
