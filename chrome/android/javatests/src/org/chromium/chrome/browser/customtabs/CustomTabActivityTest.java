b26c6b350a5ab07dff1fb55777a285214843edb5
Ritika Gupta <ritikagup@google.com>
2023-03-15 15:54:58 +0000

    [M112][Sheriff] Disable flaky customtabs testMultipleActionButtons test.
    (cherry picked from commit f1b2467d001ee0f1a6f2f15f4234bdf28fe314a9)
    
    Change-Id: Ia06ecf7a134676728379e19b0237f6247fecfd9d
    Bug: 1420991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4312941
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1113594}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4340971
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/branch-heads/5615@{#515}
    Cr-Branched-From: 9c6408ef696e83a9936b82bbead3d41c93c82ee4-refs/heads/main@{#1109224}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -502,6 +502,7 @@ public class CustomTabActivityTest {
     @Test
     @SmallTest
     @Feature({"UiCatalogue"})
+    @DisabledTest(message = "https://crbug.com/1420991")
     public void testMultipleActionButtons() throws TimeoutException {
         Bitmap expectedIcon1 = createVectorDrawableBitmap(R.drawable.ic_content_copy_black, 48, 48);
         Bitmap expectedIcon2 =
