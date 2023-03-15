ff94c8b58fae6dbedeab6d5bd565129df873cb9a
Ahmed Nasr <anasr@google.com>
2023-03-15 00:33:37 +0000

    Add event driven telemetry to wifi signal browser test.
    This is to avoid bugs like the one discovered in
    https://crrev.com/c/4335775
    
    Change-Id: I1e5a8bac0cbdacdeb22dce92d5c3391eedfb2658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337730
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1117296}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer_browsertest.cc
@@ -4,8 +4,12 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 
+#include "base/functional/bind.h"
+#include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/values.h"
 #include "chrome/browser/ash/login/test/cryptohome_mixin.h"
 #include "chrome/browser/ash/policy/affiliation/affiliation_mixin.h"
 #include "chrome/browser/ash/policy/affiliation/affiliation_test_helper.h"
@@ -34,6 +38,7 @@ namespace {
 
 constexpr int kSignalStrength = 50;
 constexpr char kWifiGuid[] = "wifi-guid";
+constexpr char kWifiDevicePath[] = "/device/wlan";
 constexpr char kWifiServicePath[] = "/service/wlan";
 constexpr char kWifiConfig[] =
     R"({"GUID": "%s", "Type": "wifi", "State": "online",
@@ -41,12 +46,25 @@ constexpr char kWifiConfig[] =
 constexpr int kGoodSignalStrengthRssi = -50;
 constexpr int kLowSignalStrengthRssi = -75;
 
-Record GetNextRecord(::chromeos::MissiveClientTestObserver* observer) {
+bool IsEventDrivenNetworkTelemetry(const ::reporting::Record& record) {
+  if (record.destination() != Destination::TELEMETRY_METRIC) {
+    return false;
+  }
+
+  MetricData record_data;
+  EXPECT_TRUE(record_data.ParseFromString(record.data()));
+
+  return record_data.has_telemetry_data() &&
+         record_data.telemetry_data().is_event_driven();
+}
+
+Record GetNextRecord(::chromeos::MissiveClientTestObserver* observer,
+                     Priority expectedPriority) {
   const std::tuple<Priority, Record>& enqueued_record =
       observer->GetNextEnqueuedRecord();
   Priority priority = std::get<0>(enqueued_record);
   Record record = std::get<1>(enqueued_record);
-  EXPECT_THAT(priority, Eq(Priority::SLOW_BATCH));
+  EXPECT_THAT(priority, Eq(expectedPriority));
   return record;
 }
 
@@ -79,12 +97,20 @@ class NetworkEventsBrowserTest : public ::policy::DevicePolicyCrosBrowserTest {
         std::make_unique<::ash::NetworkHandlerTestHelper>();
     network_handler_test_helper_->AddDefaultProfiles();
     network_handler_test_helper_->ResetDevicesAndServices();
+    auto* const device_client = network_handler_test_helper_->device_test();
+    device_client->AddDevice(kWifiDevicePath, shill::kTypeWifi, "wifi0");
+    base::RunLoop().RunUntilIdle();
     auto* const service_client = network_handler_test_helper_->service_test();
     service_client->AddService(kWifiServicePath, kWifiGuid, "wifi-name",
                                shill::kTypeWifi, shill::kStateOnline, true);
+    service_client->SetServiceProperty(kWifiServicePath, shill::kDeviceProperty,
+                                       base::Value(kWifiDevicePath));
     std::string service_config_good_signal =
         base::StringPrintf(kWifiConfig, kWifiGuid, kGoodSignalStrengthRssi);
     network_handler_test_helper_->ConfigureService(service_config_good_signal);
+    service_client->SetServiceProperty(
+        kWifiServicePath, shill::kProfileProperty,
+        base::Value(network_handler_test_helper_->ProfilePathUser()));
 
     ::policy::AffiliationTestHelper::LoginUser(affiliation_mixin_.account_id());
   }
@@ -97,6 +123,14 @@ class NetworkEventsBrowserTest : public ::policy::DevicePolicyCrosBrowserTest {
   void DisablePolicy() {
     scoped_testing_cros_settings_.device_settings()->SetBoolean(
         ash::kReportDeviceNetworkStatus, false);
+  }
+
+  void SetWifiSignalEventDrivenPolicy() {
+    base::Value::List telemetry_list;
+    telemetry_list.Append("network_telemetry");
+    scoped_testing_cros_settings_.device_settings()->Set(
+        ash::kReportDeviceSignalStrengthEventDrivenTelemetry,
+        base::Value(std::move(telemetry_list)));
   }
 
   std::unique_ptr<::ash::NetworkHandlerTestHelper> network_handler_test_helper_;
@@ -115,14 +149,15 @@ IN_PROC_BROWSER_TEST_F(NetworkEventsBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(NetworkEventsBrowserTest,
                        ConnectionStateAffiliatedUserAndPolicyEnabled) {
-  chromeos::MissiveClientTestObserver missive_observer_(
+  chromeos::MissiveClientTestObserver missive_event_observer(
       Destination::EVENT_METRIC);
 
   EnablePolicy();
   ash::ShillServiceClient::Get()->GetTestInterface()->SetServiceProperty(
       kWifiServicePath, shill::kStateProperty, base::Value(shill::kStateIdle));
 
-  const Record& record = GetNextRecord(&missive_observer_);
+  const Record& record =
+      GetNextRecord(&missive_event_observer, Priority::SLOW_BATCH);
   MetricData record_data;
 
   ASSERT_TRUE(record_data.ParseFromString(record.data()));
@@ -138,26 +173,37 @@ IN_PROC_BROWSER_TEST_F(NetworkEventsBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(NetworkEventsBrowserTest,
                        SignalStrengthAffiliatedUserAndPolicyEnabled) {
-  chromeos::MissiveClientTestObserver missive_observer_(
+  chromeos::MissiveClientTestObserver missive_event_observer(
       Destination::EVENT_METRIC);
+  chromeos::MissiveClientTestObserver missive_telemetry_observer(
+      base::BindRepeating(&IsEventDrivenNetworkTelemetry));
 
   const std::string service_config_low_signal =
       base::StringPrintf(kWifiConfig, kWifiGuid, kLowSignalStrengthRssi);
   network_handler_test_helper_->ConfigureService(service_config_low_signal);
 
   EnablePolicy();
+  SetWifiSignalEventDrivenPolicy();
   ash::ShillServiceClient::Get()->GetTestInterface()->SetServiceProperty(
       kWifiServicePath, shill::kSignalStrengthProperty,
       base::Value(kSignalStrength));
 
-  const Record& record = GetNextRecord(&missive_observer_);
-  MetricData record_data;
+  Record record = GetNextRecord(&missive_event_observer, Priority::SLOW_BATCH);
+  MetricData event_record_data;
 
-  ASSERT_TRUE(record_data.ParseFromString(record.data()));
-
-  // Testing event found successfully.
-  EXPECT_THAT(record_data.event_data().type(),
+  ASSERT_TRUE(event_record_data.ParseFromString(record.data()));
+  EXPECT_THAT(event_record_data.event_data().type(),
               Eq(MetricEventType::WIFI_SIGNAL_STRENGTH_LOW));
+
+  record = GetNextRecord(&missive_telemetry_observer, Priority::MANUAL_BATCH);
+  MetricData telemetry_record_data;
+
+  ASSERT_TRUE(telemetry_record_data.ParseFromString(record.data()));
+  ASSERT_TRUE(telemetry_record_data.telemetry_data().has_networks_telemetry());
+  EXPECT_FALSE(telemetry_record_data.telemetry_data()
+                   .networks_telemetry()
+                   .network_telemetry()
+                   .empty());
 }
 
 }  // namespace
