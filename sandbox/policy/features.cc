9be556dd99dd9d86fc6b983d9168e5e28aec68f6
Alex Gough <ajgo@chromium.org>
2023-03-17 18:19:34 +0000

    Allow sandboxes to access system fonts.
    This CL toggles a feature to re-enable access to system fonts
    from sandboxed processes.
    
    PDFs are not showing some fonts without the rule.
    
    Bug: 1425413
    Change-Id: Iff4b1036c26854af4a695fd70be1e79c5a632550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350648
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118775}

--- sandbox/policy/features.cc
+++ sandbox/policy/features.cc
@@ -48,9 +48,10 @@ BASE_FEATURE(kRendererFilterEnvironment,
 
 // Emergency "off switch" for removal of direct system font access from
 // sandboxed processes.
+// Enabled until https://crbug.com/1425413 is resolved.
 BASE_FEATURE(kWinSboxAllowSystemFonts,
              "WinSboxAllowSystemFonts",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
