096cd3af2b8ca8fec97a0185e8e802d3e15278b1
Changhao Han <changhaohan@chromium.org>
2023-03-17 06:59:06 +0000

    Add nestingSelectors to CSSRule
    This CL adds a optional nestingSelectors field to CSS.CSSRule. This is
    an array of nesting selectors, sorted from closest to the current rule
    to the outmost rule. We only need strings for them, hence the simple
    form.
    
    Bug: 1172985
    Change-Id: I5e28e0537958262006af0a6c519d12505cebc823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4274802
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118544}

--- third_party/blink/web_tests/inspector-protocol/css/css-nested-styles.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-nested-styles.js
@@ -1,8 +1,12 @@
 (async function(testRunner) {
-  var {dp} = await testRunner.startHTML(`
-      <link rel='stylesheet' href='${testRunner.url('resources/nested-styles.css')}'/>
+  var {dp} = await testRunner.startHTML(
+      `
+      <link rel='stylesheet' href='${
+          testRunner.url('resources/nested-styles.css')}'/>
       <div id='parent'>
-          <div id='nested'></div>
+          <div id='nested'>
+            <div id='deep-nested'></div>
+          </div>
       </div>`,
       'The test verifies functionality of protocol methods working correctly with CSS nesting.');
 
@@ -20,19 +24,38 @@
   });
   const nestedNodeId = nestedNode.result.nodeId;
 
-  const matchedStyles = await dp.CSS.getMatchedStylesForNode({nodeId: nestedNodeId});
+  const matchedStyles =
+      await dp.CSS.getMatchedStylesForNode({nodeId: nestedNodeId});
   for (const ruleMatch of matchedStyles.result.matchedCSSRules) {
     cssHelper.dumpRuleMatch(ruleMatch);
+    if (ruleMatch.rule.nestingSelectors) {
+      testRunner.log('nesting selectors: ' + ruleMatch.rule.nestingSelectors);
+    }
+  }
+
+  const deepNestedNode = await dp.DOM.querySelector({
+    nodeId: documentNodeId,
+    selector: '#deep-nested',
+  });
+  const deepNestedNodeId = deepNestedNode.result.nodeId;
+  const deepNestedMatchedStyles =
+    await dp.CSS.getMatchedStylesForNode({nodeId: deepNestedNodeId});
+    for (const ruleMatch of deepNestedMatchedStyles.result.matchedCSSRules) {
+    cssHelper.dumpRuleMatch(ruleMatch);
+    if (ruleMatch.rule.nestingSelectors) {
+      testRunner.log('nesting selectors: ' + ruleMatch.rule.nestingSelectors);
+    }
   }
 
   const styleSheetId = matchedStyles.result.matchedCSSRules.at(-1).rule.styleSheetId;
   await cssHelper.setStyleTexts(styleSheetId, false, [{
     styleSheetId,
-    range: { startLine: 1, startColumn: 11, endLine: 3, endColumn: 2 },
-    text: "\n    width: 41px;\n  ",
+    range: { startLine: 1, startColumn: 11, endLine: 11, endColumn: 2 },
+    text: "\n    width: 41px;\n\n    #deep-nested {\n    height: 42px;\n    }\n\n    #deep-nested {\n    display: grid;\n    }\n  ",
   }]);
 
-  const updatedStyles = await dp.CSS.getMatchedStylesForNode({nodeId: nestedNodeId});
+  const updatedStyles =
+      await dp.CSS.getMatchedStylesForNode({nodeId: nestedNodeId});
   for (const ruleMatch of updatedStyles.result.matchedCSSRules) {
     cssHelper.dumpRuleMatch(ruleMatch);
   }
