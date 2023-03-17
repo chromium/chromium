e98493fc71232fde6a370f4f44dcba6a9292aaa2
Simon Zünd <szuend@chromium.org>
2023-03-17 05:26:55 +0000

    Add test for resolving source mapped expressions in cheap source maps
    This test checks that resolving expression fails for line-by-line
    source maps.
    
    R=bmeurer@chromium.org
    
    Bug: 1422883
    Change-Id: I606bef3034ecb54ffebf466dbaed0deef6518be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337509
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118529}

---
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger/debugger-resolve-expression-line-by-line.js
@@ -0,0 +1,28 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests evaluation in webpack bundled scripts with 'line-by'line' source maps.\n`);
+  await TestRunner.loadLegacyModule('sources'); await TestRunner.loadTestModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+
+  // Bundle created using `npx webpack` with 'cheap-module-source-map'.
+  await TestRunner.addScriptTag('resources/resolve-expressions-webpack-bundle.js');
+
+  await SourcesTestRunner.startDebuggerTestPromise();
+  SourcesTestRunner.runTestFunctionAndWaitUntilPaused();
+
+  await TestRunner.addSnifferPromise(Sources.CallStackSidebarPane.prototype, 'updatedForTest');
+  SourcesTestRunner.waitForScriptSource('resolve-expressions-webpack-authored.js', async (uiSourceCode) => {
+    // "this.#prop" maps to the whole assignment so we should not resolve to anything in the source map.
+    let resolvedExpression = await Sources.SourceMapNamesResolver.resolveExpression(UI.context.flavor(SDK.DebuggerModel.CallFrame), 'this.#prop', uiSourceCode, 5, 17, 27);
+    TestRunner.addResult(`Resolved expression "this.#prop" to "${resolvedExpression}"`);
+
+    // "a" should work as the "a++" is the only statement on the line.
+    resolvedExpression = await Sources.SourceMapNamesResolver.resolveExpression(UI.context.flavor(SDK.DebuggerModel.CallFrame), 'a', uiSourceCode, 6, 4, 5);
+    TestRunner.addResult(`Resolved expression "a" to "${resolvedExpression}"`);
+
+    SourcesTestRunner.completeDebuggerTest();
+  });
+})();
---
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger/resources/resolve-expressions-webpack-bundle.js
@@ -0,0 +1,66 @@
+/******/ // (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Foo": () => (/* binding */ Foo),
+/* harmony export */   "fooInstance": () => (/* binding */ fooInstance),
+/* harmony export */   "testFunction": () => (/* binding */ testFunction)
+/* harmony export */ });
+
+class Foo {
+  #prop = [25, 42];
+
+  foo() {
+    let [a, b] = this.#prop;
+    a++;
+    debugger;
+    return a + b;
+  }
+}
+
+const fooInstance = new Foo();
+
+function testFunction() {
+  fooInstance.foo();
+}
+
+/******/ // })()
+;
+//# sourceMappingURL=resolve-expressions-webpack-bundle.js.map
