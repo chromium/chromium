e2183afc9ca4611b46ed0c06c9565cb8a8946b48
Peter Kotwicz <pkotwicz@chromium.org>
2023-03-17 00:28:13 +0000

    Clarify HTMLSelectMenuElement::ResetToDefaultSelection()
    This CL clarifies why multiple <option>s can be selected at the time
    that HTMLSelectMenuElement::ResetToDefaultSelection() is called.
    
    BUG=None
    
    Change-Id: If19eb88797f20bafe3abff4eb81ce88f23391eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4344091
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118453}

--- third_party/blink/renderer/core/html/forms/html_select_menu_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_menu_element.cc
@@ -1013,6 +1013,7 @@ void HTMLSelectMenuElement::ResetImpl() {
   SetNeedsValidityCheck();
 }
 
+// https://html.spec.whatwg.org/C#selectedness-setting-algorithm
 void HTMLSelectMenuElement::ResetToDefaultSelection() {
   HTMLOptionElement* first_enabled_option = nullptr;
   HTMLOptionElement* last_selected_option = nullptr;
@@ -1034,8 +1035,10 @@ void HTMLSelectMenuElement::ResetToDefaultSelection() {
   }
 
   // If no option is selected, set the selection to the first non-disabled
-  // option if it exists, or null otherwise. If two or more options are
-  // selected, set the selection to the last selected option.
+  // option if it exists, or null otherwise.
+  //
+  // If two or more options are selected, set the selection to the last
+  // selected option. ResetImpl() can temporarily select multiple options.
   if (last_selected_option) {
     SetSelectedOption(last_selected_option);
   } else {
