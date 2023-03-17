46ee103162928ee830d861502ed16496cd301e02
Viktoriia Kovalova <vkovalova@google.com>
2023-03-17 12:44:02 +0000

    Fix force-installed Chrome apps policy migration in Lacros
    Changed the buildflag so that migration was run both in ash and lacros.
    
    BUG=1421043,1202623
    
    Change-Id: Ie423d6a322027e2ca5fc4433cc582dd912e425ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4345761
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Viktoriia Kovalova <vkovalova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1118615}

--- components/policy/core/common/policy_service_impl.cc
+++ components/policy/core/common/policy_service_impl.cc
@@ -38,7 +38,7 @@
 #include "components/policy/core/common/android/policy_service_android.h"
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
 #include "components/policy/core/common/default_chrome_apps_migrator.h"
 #endif
 
@@ -320,20 +320,20 @@ void PolicyServiceImpl::MergeAndTriggerUpdates() {
   // Merge from each provider in their order of priority.
   const PolicyNamespace chrome_namespace(POLICY_DOMAIN_CHROME, std::string());
   PolicyBundle bundle;
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   DefaultChromeAppsMigrator chrome_apps_migrator;
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
   for (auto* provider : providers_) {
     PolicyBundle provided_bundle = provider->policies().Clone();
     IgnoreUserCloudPrecedencePolicies(&provided_bundle.Get(chrome_namespace));
     DowngradeMetricsReportingToRecommendedPolicy(
         &provided_bundle.Get(chrome_namespace));
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
     if (base::FeatureList::IsEnabled(
             policy::features::kDefaultChromeAppsMigration)) {
       chrome_apps_migrator.Migrate(&provided_bundle.Get(chrome_namespace));
     }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
     bundle.MergeFrom(provided_bundle);
   }
 
