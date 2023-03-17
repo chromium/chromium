c1f28a5dc527af1206b0a032e9c54b5ede723560
Lei Zhang <thestig@chromium.org>
2023-03-17 01:18:41 +0000

    CR2023: Start enabling Chrome Refresh 2023 for Print Preview
    This just applies the chrome-refresh-2023 attribute to the Print Preview
    WebUI, when the feature is enabled.
    
    Bug: 1422130
    Change-Id: I9c7e54241562413ebe5d762fe12f263f7b14e5b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348972
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1118477}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.cc
@@ -70,6 +70,7 @@
 #include "printing/print_job_constants.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
@@ -333,6 +334,10 @@ void AddPrintPreviewStrings(content::WebUIDataSource* source) {
                     l10n_util::GetStringFUTF16(
                         IDS_PRINT_PREVIEW_SYSTEM_DIALOG_OPTION, shortcut_text));
 #endif
+
+  source->AddString(
+      "chromeRefresh2023Attribute",
+      ::features::IsChromeRefresh2023() ? "chrome-refresh-2023" : "");
 
   // Register strings for the PDF viewer, so that $i18n{} replacements work.
   base::Value::Dict pdf_strings;
