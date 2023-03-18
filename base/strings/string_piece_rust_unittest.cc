ab9ac7c971c99f63bf46cfb440e854e309b3209c
Adrian Taylor <adetaylor@chromium.org>
2023-03-18 10:55:04 +0000

    [rust] Remove global Rust enablement defines
    We are aiming to enable Rust incrementally, feature-by-feature, so
    global "this toolchain has Rust" flags are unhealthy. Remove them and
    replace them with feature-by-feature flags.
    
    In particular, //base depended on Rust in a few ways even when
    'enable_rust_json' was switched off. With this CL, //base builds
    identically whether 'enable_rust' is turned on or off. Rust is only
    involved if 'enable_rust_json' is switched on.
    
    Bug: 1292038
    Change-Id: Ie7ec0f998aea87807b3301c00d89f185bbcb5fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350448
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119040}

--- base/strings/string_piece_rust_unittest.cc
+++ base/strings/string_piece_rust_unittest.cc
@@ -3,11 +3,11 @@
 // found in the LICENSE file.
 
 #include "base/strings/string_piece_rust.h"
-#include "build/rust/rust_buildflags.h"
+#include "base/rust_buildflags.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#if BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
 
 namespace base {
 namespace {
@@ -32,4 +32,4 @@ TEST(BaseStringPieceRustTest, StrToSlice) {
 }  // namespace
 }  // namespace base
 
-#endif  // BUILDFLAG(TOOLCHAIN_HAS_RUST)
+#endif  // BUILDFLAG(BUILD_RUST_BASE_CONVERSIONS)
