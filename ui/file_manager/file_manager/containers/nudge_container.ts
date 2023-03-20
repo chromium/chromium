260ca481f2ab8a3436dc587c0dd769671a8e176a
Luciano Pacheco <lucmult@chromium.org>
2023-03-20 02:51:17 +0000

    Files app & Office: Change education nudge position
    Update the educational nudge for Google Drive and OneDrive to be
    anchored in the volume label and to display "horizontally", at the end
    of the label.
    
    Bug: b:274356462
    Change-Id: I9d7d921c76b17d7a6f9b5cd9a2fb173ec566478a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4349569
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119173}

--- ui/file_manager/file_manager/containers/nudge_container.ts
+++ ui/file_manager/file_manager/containers/nudge_container.ts
@@ -359,18 +359,28 @@ export const nudgeInfo: {[type in NudgeType]: NudgeInfo} = {
     expiryDate: new Date(2023, 4, 6),
   },
   [NudgeType['ONE_DRIVE_MOVED_FILE_NUDGE']]: {
-    anchor: () => document.querySelector<HTMLSpanElement>(
-        '.tree-item[one-drive] .file-row .item-icon'),
+    anchor: () => {
+      return document
+                 .querySelector<HTMLSpanElement>(
+                     '.tree-item[one-drive] .file-row .item-icon')
+                 ?.parentElement ||
+          null;
+    },
     content: () => str('ONE_DRIVE_MOVED_FILE_NUDGE'),
-    direction: NudgeDirection.BOTTOM_ENDWARD,
+    direction: NudgeDirection.TRAILING_DOWNWARD,
     // Expire after 4 releases (expires when M120 hits Stable).
     expiryDate: new Date(2023, 12, 5),
   },
   [NudgeType['DRIVE_MOVED_FILE_NUDGE']]: {
-    anchor: () => document.querySelector<HTMLSpanElement>(
-        '.tree-item .item-icon[volume-type-icon="drive"]'),
+    anchor: () => {
+      return document
+                 .querySelector<HTMLSpanElement>(
+                     '.tree-item .item-icon[volume-type-icon="drive"]')
+                 ?.parentElement ||
+          null;
+    },
     content: () => str('DRIVE_MOVED_FILE_NUDGE'),
-    direction: NudgeDirection.BOTTOM_ENDWARD,
+    direction: NudgeDirection.TRAILING_DOWNWARD,
     // Expire after 4 releases (expires when M120 hits Stable).
     expiryDate: new Date(2023, 12, 5),
   },
