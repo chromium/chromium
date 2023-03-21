24f84eb6e959b330a6843cf2b16450967c7e554d
Jan Keitel <jkeitel@google.com>
2023-03-21 15:59:38 +0000

    Move TestViewStructure into separate class.
    TestViewStructure will be used by further tests in the future.
    
    Bug: 1425869
    Change-Id: I763c17aab35d254e4f3ee71b0caeca861362c55c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355019
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119955}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
@@ -12,15 +12,12 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
-import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.LocaleList;
-import android.os.Parcel;
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.text.TextUtils;
@@ -28,9 +25,7 @@ import android.util.Pair;
 import android.util.SparseArray;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewStructure;
 import android.view.WindowManager;
-import android.view.autofill.AutofillId;
 import android.view.autofill.AutofillValue;
 
 import androidx.annotation.RequiresApi;
@@ -66,6 +61,7 @@ import org.chromium.components.autofill.AutofillPopup;
 import org.chromium.components.autofill.AutofillProvider;
 import org.chromium.components.autofill.AutofillProviderTestHelper;
 import org.chromium.components.autofill.AutofillProviderUMA;
+import org.chromium.components.autofill.TestViewStructure;
 import org.chromium.components.autofill_public.ViewType;
 import org.chromium.components.embedder_support.util.WebResourceResponseInfo;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
@@ -110,372 +106,6 @@ public class AwAutofillTest {
 
     public static final String[] EVENT = {"VIEW_ENTERED", "VIEW_EXITED", "VALUE_CHANGED", "COMMIT",
             "CANCEL", "SESSION_STARTED", "QUERY_DONE"};
-
-    /**
-     * This class only implements the necessary methods of ViewStructure for testing.
-     */
-    private static class TestViewStructure extends ViewStructure {
-        /**
-         * Implementation of HtmlInfo.
-         */
-        public static class AwHtmlInfo extends HtmlInfo {
-            private String mTag;
-            private List<Pair<String, String>> mAttributes;
-            public AwHtmlInfo(String tag, List<Pair<String, String>> attributes) {
-                mTag = tag;
-                mAttributes = attributes;
-            }
-
-            @Override
-            public List<Pair<String, String>> getAttributes() {
-                return mAttributes;
-            }
-
-            public String getAttribute(String attribute) {
-                for (Pair<String, String> pair : mAttributes) {
-                    if (attribute.equals(pair.first)) {
-                        return pair.second;
-                    }
-                }
-                return null;
-            }
-
-            @Override
-            public String getTag() {
-                return mTag;
-            }
-        }
-
-        /**
-         * Implementation of Builder
-         */
-        public static class AwBuilder extends HtmlInfo.Builder {
-            private String mTag;
-            private ArrayList<Pair<String, String>> mAttributes;
-            public AwBuilder(String tag) {
-                mTag = tag;
-                mAttributes = new ArrayList<Pair<String, String>>();
-            }
-
-            @Override
-            public HtmlInfo.Builder addAttribute(String name, String value) {
-                mAttributes.add(new Pair<String, String>(name, value));
-                return this;
-            }
-
-            @Override
-            public HtmlInfo build() {
-                return new AwHtmlInfo(mTag, mAttributes);
-            }
-        }
-
-        public TestViewStructure() {
-            mChildren = new ArrayList<TestViewStructure>();
-        }
-
-        @Override
-        public void setAlpha(float alpha) {}
-
-        @Override
-        public void setAccessibilityFocused(boolean state) {}
-
-        @Override
-        public void setCheckable(boolean state) {}
-
-        @Override
-        public void setChecked(boolean state) {
-            mChecked = state;
-        }
-
-        public boolean getChecked() {
-            return mChecked;
-        }
-
-        @Override
-        public void setActivated(boolean state) {}
-
-        @Override
-        public CharSequence getText() {
-            return null;
-        }
-
-        @Override
-        public int getTextSelectionStart() {
-            return 0;
-        }
-
-        @Override
-        public int getTextSelectionEnd() {
-            return 0;
-        }
-
-        @Override
-        public CharSequence getHint() {
-            return mHint;
-        }
-
-        @Override
-        public Bundle getExtras() {
-            if (mBundle == null) mBundle = new Bundle();
-            return mBundle;
-        }
-
-        @Override
-        public boolean hasExtras() {
-            return mBundle != null;
-        }
-
-        @Override
-        public void setChildCount(int num) {}
-
-        @Override
-        public int addChildCount(int num) {
-            int index = mChildCount;
-            mChildCount += num;
-            mChildren.ensureCapacity(mChildCount);
-            return index;
-        }
-
-        @Override
-        public int getChildCount() {
-            return mChildren.size();
-        }
-
-        @Override
-        public ViewStructure newChild(int index) {
-            TestViewStructure viewStructure = new TestViewStructure();
-            if (index < mChildren.size()) {
-                mChildren.set(index, viewStructure);
-            } else if (index == mChildren.size()) {
-                mChildren.add(viewStructure);
-            } else {
-                // Failed intentionally, we shouldn't run into this case.
-                mChildren.add(index, viewStructure);
-            }
-            return viewStructure;
-        }
-
-        public TestViewStructure getChild(int index) {
-            return mChildren.get(index);
-        }
-
-        @Override
-        public ViewStructure asyncNewChild(int index) {
-            return null;
-        }
-
-        @Override
-        public void asyncCommit() {}
-
-        @Override
-        public AutofillId getAutofillId() {
-            Parcel parcel = Parcel.obtain();
-            // Check AutofillId(Parcel) in AutofillId.java, always set parent id as 0.
-            parcel.writeInt(0);
-            parcel.writeInt(1);
-            parcel.writeInt(mId);
-
-            return AutofillId.CREATOR.createFromParcel(parcel);
-        }
-
-        @Override
-        public HtmlInfo.Builder newHtmlInfoBuilder(String tag) {
-            return new AwBuilder(tag);
-        }
-
-        @Override
-        public void setAutofillHints(String[] arg0) {
-            mAutofillHints = arg0.clone();
-        }
-
-        public String[] getAutofillHints() {
-            if (mAutofillHints == null) return null;
-            return mAutofillHints.clone();
-        }
-
-        @Override
-        public void setAutofillId(AutofillId arg0) {}
-
-        @Override
-        public void setAutofillId(AutofillId arg0, int arg1) {
-            mId = arg1;
-        }
-
-        public int getId() {
-            return mId;
-        }
-
-        @Override
-        public void setAutofillOptions(CharSequence[] arg0) {
-            mAutofillOptions = arg0.clone();
-        }
-
-        public CharSequence[] getAutofillOptions() {
-            if (mAutofillOptions == null) return null;
-            return mAutofillOptions.clone();
-        }
-
-        @Override
-        public void setAutofillType(int arg0) {
-            mAutofillType = arg0;
-        }
-
-        public int getAutofillType() {
-            return mAutofillType;
-        }
-
-        @Override
-        public void setAutofillValue(AutofillValue arg0) {
-            mAutofillValue = arg0;
-        }
-
-        public AutofillValue getAutofillValue() {
-            return mAutofillValue;
-        }
-
-        @Override
-        public void setId(int id, String packageName, String typeName, String entryName) {}
-
-        @Override
-        public void setDimens(int left, int top, int scrollX, int scrollY, int width, int height) {
-            mDimensRect = new Rect(left, top, width + left, height + top);
-            mDimensScrollX = scrollX;
-            mDimensScrollY = scrollY;
-        }
-
-        public Rect getDimensRect() {
-            return mDimensRect;
-        }
-
-        public int getDimensScrollX() {
-            return mDimensScrollX;
-        }
-
-        public int getDimensScrollY() {
-            return mDimensScrollY;
-        }
-
-        @Override
-        public void setElevation(float elevation) {}
-
-        @Override
-        public void setEnabled(boolean state) {}
-
-        @Override
-        public void setClickable(boolean state) {}
-
-        @Override
-        public void setLongClickable(boolean state) {}
-
-        @Override
-        public void setContextClickable(boolean state) {}
-
-        @Override
-        public void setFocusable(boolean state) {}
-
-        @Override
-        public void setFocused(boolean state) {}
-
-        @Override
-        public void setClassName(String className) {
-            mClassName = className;
-        }
-
-        public String getClassName() {
-            return mClassName;
-        }
-
-        @Override
-        public void setContentDescription(CharSequence contentDescription) {}
-
-        @Override
-        public void setDataIsSensitive(boolean arg0) {
-            mDataIsSensitive = arg0;
-        }
-
-        public boolean getDataIsSensitive() {
-            return mDataIsSensitive;
-        }
-
-        @Override
-        public void setHint(CharSequence hint) {
-            mHint = hint;
-        }
-
-        @Override
-        public void setHtmlInfo(HtmlInfo arg0) {
-            mAwHtmlInfo = (AwHtmlInfo) arg0;
-        }
-
-        public AwHtmlInfo getHtmlInfo() {
-            return mAwHtmlInfo;
-        }
-
-        @Override
-        public void setInputType(int arg0) {}
-
-        @Override
-        public void setLocaleList(LocaleList arg0) {}
-
-        @Override
-        public void setOpaque(boolean arg0) {}
-
-        @Override
-        public void setTransformation(Matrix matrix) {}
-
-        @Override
-        public void setVisibility(int visibility) {
-            mVisibility = visibility;
-        }
-
-        public int getVisibility() {
-            return mVisibility;
-        }
-
-        @Override
-        public void setSelected(boolean state) {}
-
-        @Override
-        public void setText(CharSequence text) {}
-
-        @Override
-        public void setText(CharSequence text, int selectionStart, int selectionEnd) {}
-
-        @Override
-        public void setTextStyle(float size, int fgColor, int bgColor, int style) {}
-
-        @Override
-        public void setTextLines(int[] charOffsets, int[] baselines) {}
-
-        @Override
-        public void setWebDomain(String webDomain) {
-            mWebDomain = webDomain;
-        }
-
-        public String getWebDomain() {
-            return mWebDomain;
-        }
-
-        private int mAutofillType;
-        private CharSequence mHint;
-        private String[] mAutofillHints;
-        private int mId;
-        private String mClassName;
-        private String mWebDomain;
-        private int mChildCount;
-        private ArrayList<TestViewStructure> mChildren;
-        private CharSequence[] mAutofillOptions;
-        private AutofillValue mAutofillValue;
-        private boolean mDataIsSensitive;
-        private AwHtmlInfo mAwHtmlInfo;
-        private boolean mChecked;
-        private Rect mDimensRect;
-        private int mDimensScrollX;
-        private int mDimensScrollY;
-        private Bundle mBundle;
-        // Initializes to the value AutofillProvider will never use.
-        private int mVisibility = View.GONE;
-    }
 
     // crbug.com/776230: On Android L, declaring variables of unsupported classes causes an error.
     // Wrapped them in a class to avoid it.
@@ -838,7 +468,7 @@ public class AwAutofillTest {
         Bundle extras = viewStructure.getExtras();
         assertEquals("Android WebView", extras.getCharSequence("VIRTUAL_STRUCTURE_PROVIDER_NAME"));
         assertTrue(0 < extras.getCharSequence("VIRTUAL_STRUCTURE_PROVIDER_VERSION").length());
-        TestViewStructure.AwHtmlInfo htmlInfoForm = viewStructure.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfoForm = viewStructure.getHtmlInfo();
         assertEquals("form", htmlInfoForm.getTag());
         assertEquals("formname", htmlInfoForm.getAttribute("name"));
 
@@ -852,7 +482,7 @@ public class AwAutofillTest {
         // The field has no scroll, should always be zero.
         assertEquals(0, child0.getDimensScrollX());
         assertEquals(0, child0.getDimensScrollY());
-        TestViewStructure.AwHtmlInfo htmlInfo0 = child0.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo0 = child0.getHtmlInfo();
         assertEquals("text", htmlInfo0.getAttribute("type"));
         assertEquals("text1", htmlInfo0.getAttribute("id"));
         assertEquals("name", htmlInfo0.getAttribute("name"));
@@ -869,7 +499,7 @@ public class AwAutofillTest {
         // The field has no scroll, should always be zero.
         assertEquals(0, child1.getDimensScrollX());
         assertEquals(0, child1.getDimensScrollY());
-        TestViewStructure.AwHtmlInfo htmlInfo1 = child1.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo1 = child1.getHtmlInfo();
         assertEquals("checkbox", htmlInfo1.getAttribute("type"));
         assertEquals("checkbox1", htmlInfo1.getAttribute("id"));
         assertEquals("showpassword", htmlInfo1.getAttribute("name"));
@@ -886,7 +516,7 @@ public class AwAutofillTest {
         // The field has no scroll, should always be zero.
         assertEquals(0, child2.getDimensScrollX());
         assertEquals(0, child2.getDimensScrollY());
-        TestViewStructure.AwHtmlInfo htmlInfo2 = child2.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo2 = child2.getHtmlInfo();
         assertEquals("month", htmlInfo2.getAttribute("name"));
         assertEquals("select1", htmlInfo2.getAttribute("id"));
         CharSequence[] options = child2.getAutofillOptions();
@@ -902,7 +532,7 @@ public class AwAutofillTest {
         // The field has no scroll, should always be zero.
         assertEquals(0, child3.getDimensScrollX());
         assertEquals(0, child3.getDimensScrollY());
-        TestViewStructure.AwHtmlInfo htmlInfo3 = child3.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo3 = child3.getHtmlInfo();
         assertEquals("textarea1", htmlInfo3.getAttribute("name"));
 
         // Autofill form and verify filled values.
@@ -1645,27 +1275,27 @@ public class AwAutofillTest {
         assertEquals(totalControls, viewStructure.getChildCount());
 
         TestViewStructure child0 = viewStructure.getChild(0);
-        TestViewStructure.AwHtmlInfo htmlInfo0 = child0.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo0 = child0.getHtmlInfo();
         assertEquals("ADDRESS_HOME_LINE1", htmlInfo0.getAttribute("ua-autofill-hints"));
 
         TestViewStructure child1 = viewStructure.getChild(1);
-        TestViewStructure.AwHtmlInfo htmlInfo1 = child1.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo1 = child1.getHtmlInfo();
         assertEquals("ADDRESS_HOME_CITY", htmlInfo1.getAttribute("ua-autofill-hints"));
 
         TestViewStructure child2 = viewStructure.getChild(2);
-        TestViewStructure.AwHtmlInfo htmlInfo2 = child2.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo2 = child2.getHtmlInfo();
         assertEquals("ADDRESS_HOME_STATE", htmlInfo2.getAttribute("ua-autofill-hints"));
 
         TestViewStructure child3 = viewStructure.getChild(3);
-        TestViewStructure.AwHtmlInfo htmlInfo3 = child3.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo3 = child3.getHtmlInfo();
         assertEquals("ADDRESS_HOME_ZIP", htmlInfo3.getAttribute("ua-autofill-hints"));
 
         TestViewStructure child4 = viewStructure.getChild(4);
-        TestViewStructure.AwHtmlInfo htmlInfo4 = child4.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo4 = child4.getHtmlInfo();
         assertNull(htmlInfo4.getAttribute("ua-autofill-hints"));
 
         TestViewStructure child5 = viewStructure.getChild(5);
-        TestViewStructure.AwHtmlInfo htmlInfo5 = child5.getHtmlInfo();
+        TestViewStructure.TestHtmlInfo htmlInfo5 = child5.getHtmlInfo();
         assertEquals("ADDRESS_HOME_COUNTRY", htmlInfo5.getAttribute("ua-autofill-hints"));
     }
 
---
+++ components/android_autofill/browser/test_support/java/src/org/chromium/components/autofill/TestViewStructure.java
@@ -0,0 +1,391 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.autofill;
+
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.LocaleList;
+import android.os.Parcel;
+import android.util.Pair;
+import android.view.View;
+import android.view.ViewStructure;
+import android.view.autofill.AutofillId;
+import android.view.autofill.AutofillValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class only implements the necessary methods of ViewStructure for testing.
+ */
+public class TestViewStructure extends ViewStructure {
+    /**
+     * Test implementation of {@link HtmlInfo}.
+     */
+    public static class TestHtmlInfo extends HtmlInfo {
+        private String mTag;
+        private List<Pair<String, String>> mAttributes;
+        public TestHtmlInfo(String tag, List<Pair<String, String>> attributes) {
+            mTag = tag;
+            mAttributes = attributes;
+        }
+
+        @Override
+        public List<Pair<String, String>> getAttributes() {
+            return mAttributes;
+        }
+
+        public String getAttribute(String attribute) {
+            for (Pair<String, String> pair : mAttributes) {
+                if (attribute.equals(pair.first)) {
+                    return pair.second;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public String getTag() {
+            return mTag;
+        }
+    }
+
+    /**
+     * Test implementation of {@link HtmlInfo.Builder}.
+     */
+    public static class TestBuilder extends HtmlInfo.Builder {
+        private String mTag;
+        private ArrayList<Pair<String, String>> mAttributes;
+        public TestBuilder(String tag) {
+            mTag = tag;
+            mAttributes = new ArrayList<Pair<String, String>>();
+        }
+
+        @Override
+        public HtmlInfo.Builder addAttribute(String name, String value) {
+            mAttributes.add(new Pair<String, String>(name, value));
+            return this;
+        }
+
+        @Override
+        public HtmlInfo build() {
+            return new TestHtmlInfo(mTag, mAttributes);
+        }
+    }
+
+    public TestViewStructure() {
+        mChildren = new ArrayList<TestViewStructure>();
+    }
+
+    @Override
+    public void setAlpha(float alpha) {}
+
+    @Override
+    public void setAccessibilityFocused(boolean state) {}
+
+    @Override
+    public void setCheckable(boolean state) {}
+
+    @Override
+    public void setChecked(boolean state) {
+        mChecked = state;
+    }
+
+    public boolean getChecked() {
+        return mChecked;
+    }
+
+    @Override
+    public void setActivated(boolean state) {}
+
+    @Override
+    public CharSequence getText() {
+        return null;
+    }
+
+    @Override
+    public int getTextSelectionStart() {
+        return 0;
+    }
+
+    @Override
+    public int getTextSelectionEnd() {
+        return 0;
+    }
+
+    @Override
+    public CharSequence getHint() {
+        return mHint;
+    }
+
+    @Override
+    public Bundle getExtras() {
+        if (mBundle == null) mBundle = new Bundle();
+        return mBundle;
+    }
+
+    @Override
+    public boolean hasExtras() {
+        return mBundle != null;
+    }
+
+    @Override
+    public void setChildCount(int num) {}
+
+    @Override
+    public int addChildCount(int num) {
+        int index = mChildCount;
+        mChildCount += num;
+        mChildren.ensureCapacity(mChildCount);
+        return index;
+    }
+
+    @Override
+    public int getChildCount() {
+        return mChildren.size();
+    }
+
+    @Override
+    public ViewStructure newChild(int index) {
+        TestViewStructure viewStructure = new TestViewStructure();
+        if (index < mChildren.size()) {
+            mChildren.set(index, viewStructure);
+        } else if (index == mChildren.size()) {
+            mChildren.add(viewStructure);
+        } else {
+            // Failed intentionally, we shouldn't run into this case.
+            mChildren.add(index, viewStructure);
+        }
+        return viewStructure;
+    }
+
+    public TestViewStructure getChild(int index) {
+        return mChildren.get(index);
+    }
+
+    @Override
+    public ViewStructure asyncNewChild(int index) {
+        return null;
+    }
+
+    @Override
+    public void asyncCommit() {}
+
+    @Override
+    public AutofillId getAutofillId() {
+        // Check AutofillId.java for more information:
+        // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/android/view/autofill/AutofillId.java
+        Parcel parcel = Parcel.obtain();
+        // The host View - set to 0 by default.
+        parcel.writeInt(0);
+        // Corresponds to FLAG_IS_VIRTUAL_INT and indicates that the virtual id
+        // that comes next is an int.
+        parcel.writeInt(1);
+        // The actual virtual id.
+        parcel.writeInt(mId);
+        parcel.setDataPosition(0);
+
+        return AutofillId.CREATOR.createFromParcel(parcel);
+    }
+
+    @Override
+    public HtmlInfo.Builder newHtmlInfoBuilder(String tag) {
+        return new TestBuilder(tag);
+    }
+
+    @Override
+    public void setAutofillHints(String[] arg0) {
+        mAutofillHints = arg0.clone();
+    }
+
+    public String[] getAutofillHints() {
+        if (mAutofillHints == null) return null;
+        return mAutofillHints.clone();
+    }
+
+    @Override
+    public void setAutofillId(AutofillId arg0) {}
+
+    @Override
+    public void setAutofillId(AutofillId arg0, int arg1) {
+        mId = arg1;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    @Override
+    public void setAutofillOptions(CharSequence[] arg0) {
+        mAutofillOptions = arg0.clone();
+    }
+
+    public CharSequence[] getAutofillOptions() {
+        if (mAutofillOptions == null) return null;
+        return mAutofillOptions.clone();
+    }
+
+    @Override
+    public void setAutofillType(int arg0) {
+        mAutofillType = arg0;
+    }
+
+    public int getAutofillType() {
+        return mAutofillType;
+    }
+
+    @Override
+    public void setAutofillValue(AutofillValue arg0) {
+        mAutofillValue = arg0;
+    }
+
+    public AutofillValue getAutofillValue() {
+        return mAutofillValue;
+    }
+
+    @Override
+    public void setId(int id, String packageName, String typeName, String entryName) {}
+
+    @Override
+    public void setDimens(int left, int top, int scrollX, int scrollY, int width, int height) {
+        mDimensRect = new Rect(left, top, width + left, height + top);
+        mDimensScrollX = scrollX;
+        mDimensScrollY = scrollY;
+    }
+
+    public Rect getDimensRect() {
+        return mDimensRect;
+    }
+
+    public int getDimensScrollX() {
+        return mDimensScrollX;
+    }
+
+    public int getDimensScrollY() {
+        return mDimensScrollY;
+    }
+
+    @Override
+    public void setElevation(float elevation) {}
+
+    @Override
+    public void setEnabled(boolean state) {}
+
+    @Override
+    public void setClickable(boolean state) {}
+
+    @Override
+    public void setLongClickable(boolean state) {}
+
+    @Override
+    public void setContextClickable(boolean state) {}
+
+    @Override
+    public void setFocusable(boolean state) {}
+
+    @Override
+    public void setFocused(boolean state) {}
+
+    @Override
+    public void setClassName(String className) {
+        mClassName = className;
+    }
+
+    public String getClassName() {
+        return mClassName;
+    }
+
+    @Override
+    public void setContentDescription(CharSequence contentDescription) {}
+
+    @Override
+    public void setDataIsSensitive(boolean arg0) {
+        mDataIsSensitive = arg0;
+    }
+
+    public boolean getDataIsSensitive() {
+        return mDataIsSensitive;
+    }
+
+    @Override
+    public void setHint(CharSequence hint) {
+        mHint = hint;
+    }
+
+    @Override
+    public void setHtmlInfo(HtmlInfo arg0) {
+        mHtmlInfo = (TestHtmlInfo) arg0;
+    }
+
+    public TestHtmlInfo getHtmlInfo() {
+        return mHtmlInfo;
+    }
+
+    @Override
+    public void setInputType(int arg0) {}
+
+    @Override
+    public void setLocaleList(LocaleList arg0) {}
+
+    @Override
+    public void setOpaque(boolean arg0) {}
+
+    @Override
+    public void setTransformation(Matrix matrix) {}
+
+    @Override
+    public void setVisibility(int visibility) {
+        mVisibility = visibility;
+    }
+
+    public int getVisibility() {
+        return mVisibility;
+    }
+
+    @Override
+    public void setSelected(boolean state) {}
+
+    @Override
+    public void setText(CharSequence text) {}
+
+    @Override
+    public void setText(CharSequence text, int selectionStart, int selectionEnd) {}
+
+    @Override
+    public void setTextStyle(float size, int fgColor, int bgColor, int style) {}
+
+    @Override
+    public void setTextLines(int[] charOffsets, int[] baselines) {}
+
+    @Override
+    public void setWebDomain(String webDomain) {
+        mWebDomain = webDomain;
+    }
+
+    public String getWebDomain() {
+        return mWebDomain;
+    }
+
+    private int mAutofillType;
+    private CharSequence mHint;
+    private String[] mAutofillHints;
+    private int mId;
+    private String mClassName;
+    private String mWebDomain;
+    private int mChildCount;
+    private ArrayList<TestViewStructure> mChildren;
+    private CharSequence[] mAutofillOptions;
+    private AutofillValue mAutofillValue;
+    private boolean mDataIsSensitive;
+    private TestHtmlInfo mHtmlInfo;
+    private boolean mChecked;
+    private Rect mDimensRect;
+    private int mDimensScrollX;
+    private int mDimensScrollY;
+    private Bundle mBundle;
+    // Initializes to the value AutofillProvider will never use.
+    private int mVisibility = View.GONE;
+}
