7d4f8263761bff550ca48593cbcdc1b7ccb5c41a
Lei Zhang <thestig@chromium.org>
2023-03-21 17:47:43 +0000

    [PA] Trim compiler_specific.h
    PartitionAllocator's compiler_specific.h forked from Chromium's copy,
    but many macros are not used. Remove them.
    
    Change-Id: I7149427a74426916512b8134e4d1127430e6aeba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348553
    Reviewed-by: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120043}

--- base/allocator/partition_allocator/partition_alloc_base/compiler_specific.h
+++ base/allocator/partition_allocator/partition_alloc_base/compiler_specific.h
@@ -7,19 +7,6 @@
 
 #include "build/build_config.h"
 
-// This is a wrapper around `__has_cpp_attribute`, which can be used to test for
-// the presence of an attribute. In case the compiler does not support this
-// macro it will simply evaluate to 0.
-//
-// References:
-// https://wg21.link/sd6#testing-for-the-presence-of-an-attribute-__has_cpp_attribute
-// https://wg21.link/cpp.cond#:__has_cpp_attribute
-#if defined(__has_cpp_attribute)
-#define PA_HAS_CPP_ATTRIBUTE(x) __has_cpp_attribute(x)
-#else
-#define PA_HAS_CPP_ATTRIBUTE(x) 0
-#endif
-
 // A wrapper around `__has_attribute`, similar to HAS_CPP_ATTRIBUTE.
 #if defined(__has_attribute)
 #define PA_HAS_ATTRIBUTE(x) __has_attribute(x)
@@ -90,20 +77,6 @@
 #define PA_ALIGNAS(byte_alignment) __attribute__((aligned(byte_alignment)))
 #endif
 
-// In case the compiler supports it NO_UNIQUE_ADDRESS evaluates to the C++20
-// attribute [[no_unique_address]]. This allows annotating data members so that
-// they need not have an address distinct from all other non-static data members
-// of its class.
-//
-// References:
-// * https://en.cppreference.com/w/cpp/language/attributes/no_unique_address
-// * https://wg21.link/dcl.attr.nouniqueaddr
-#if PA_HAS_CPP_ATTRIBUTE(no_unique_address)
-#define PA_NO_UNIQUE_ADDRESS [[no_unique_address]]
-#else
-#define PA_NO_UNIQUE_ADDRESS
-#endif
-
 // Tells the compiler a function is using a printf-style format string.
 // |format_param| is the one-based index of the format string parameter;
 // |dots_param| is the one-based index of the "..." parameter.
@@ -116,13 +89,6 @@
 #else
 #define PA_PRINTF_FORMAT(format_param, dots_param)
 #endif
-
-// WPRINTF_FORMAT is the same, but for wide format strings.
-// This doesn't appear to yet be implemented in any compiler.
-// See http://gcc.gnu.org/bugzilla/show_bug.cgi?id=38308 .
-#define PA_WPRINTF_FORMAT(format_param, dots_param)
-// If available, it would look like:
-//   __attribute__((format(wprintf, format_param, dots_param)))
 
 // Sanitizers annotations.
 #if PA_HAS_ATTRIBUTE(no_sanitize)
@@ -140,26 +106,9 @@
 // Use this to annotate code that deliberately reads uninitialized data, for
 // example a GC scavenging root set pointers from the stack.
 #define PA_MSAN_UNPOISON(p, size) __msan_unpoison(p, size)
-
-// Check a memory region for initializedness, as if it was being used here.
-// If any bits are uninitialized, crash with an MSan report.
-// Use this to sanitize data which MSan won't be able to track, e.g. before
-// passing data to another process via shared memory.
-#define PA_MSAN_CHECK_MEM_IS_INITIALIZED(p, size) \
-  __msan_check_mem_is_initialized(p, size)
 #else  // MEMORY_SANITIZER
 #define PA_MSAN_UNPOISON(p, size)
-#define PA_MSAN_CHECK_MEM_IS_INITIALIZED(p, size)
 #endif  // MEMORY_SANITIZER
-
-// Macro useful for writing cross-platform function pointers.
-#if !defined(PA_CDECL)
-#if BUILDFLAG(IS_WIN)
-#define PA_CDECL __cdecl
-#else  // BUILDFLAG(IS_WIN)
-#define PA_CDECL
-#endif  // BUILDFLAG(IS_WIN)
-#endif  // !defined(PA_CDECL)
 
 // Macro for hinting that an expression is likely to be false.
 #if !defined(PA_UNLIKELY)
@@ -178,23 +127,6 @@
 #endif  // defined(COMPILER_GCC)
 #endif  // !defined(PA_LIKELY)
 
-// Compiler feature-detection.
-// clang.llvm.org/docs/LanguageExtensions.html#has-feature-and-has-extension
-#if defined(__has_feature)
-#define PA_HAS_FEATURE(FEATURE) __has_feature(FEATURE)
-#else
-#define PA_HAS_FEATURE(FEATURE) 0
-#endif
-
-#if defined(COMPILER_GCC)
-#define PA_PRETTY_FUNCTION __PRETTY_FUNCTION__
-#elif defined(COMPILER_MSVC)
-#define PA_PRETTY_FUNCTION __FUNCSIG__
-#else
-// See https://en.cppreference.com/w/c/language/function_definition#func
-#define PA_PRETTY_FUNCTION __func__
-#endif
-
 #if !defined(PA_CPU_ARM_NEON)
 #if defined(__arm__)
 #if !defined(__ARMEB__) && !defined(__ARM_EABI__) && !defined(__EABI__) && \
@@ -211,63 +143,6 @@
 #if defined(__mips_msa) && defined(__mips_isa_rev) && (__mips_isa_rev >= 5)
 #define PA_HAVE_MIPS_MSA_INTRINSICS 1
 #endif
-#endif
-
-#if defined(__clang__) && PA_HAS_ATTRIBUTE(uninitialized)
-// Attribute "uninitialized" disables -ftrivial-auto-var-init=pattern for
-// the specified variable.
-// Library-wide alternative is
-// 'configs -= [ "//build/config/compiler:default_init_stack_vars" ]' in .gn
-// file.
-//
-// See "init_stack_vars" in build/config/compiler/BUILD.gn and
-// http://crbug.com/977230
-// "init_stack_vars" is enabled for non-official builds and we hope to enable it
-// in official build in 2020 as well. The flag writes fixed pattern into
-// uninitialized parts of all local variables. In rare cases such initialization
-// is undesirable and attribute can be used:
-//   1. Degraded performance
-// In most cases compiler is able to remove additional stores. E.g. if memory is
-// never accessed or properly initialized later. Preserved stores mostly will
-// not affect program performance. However if compiler failed on some
-// performance critical code we can get a visible regression in a benchmark.
-//   2. memset, memcpy calls
-// Compiler may replaces some memory writes with memset or memcpy calls. This is
-// not -ftrivial-auto-var-init specific, but it can happen more likely with the
-// flag. It can be a problem if code is not linked with C run-time library.
-//
-// Note: The flag is security risk mitigation feature. So in future the
-// attribute uses should be avoided when possible. However to enable this
-// mitigation on the most of the code we need to be less strict now and minimize
-// number of exceptions later. So if in doubt feel free to use attribute, but
-// please document the problem for someone who is going to cleanup it later.
-// E.g. platform, bot, benchmark or test name in patch description or next to
-// the attribute.
-#define PA_STACK_UNINITIALIZED [[clang::uninitialized]]
-#else
-#define PA_STACK_UNINITIALIZED
-#endif
-
-// Attribute "no_stack_protector" disables -fstack-protector for the specified
-// function.
-//
-// "stack_protector" is enabled on most POSIX builds. The flag adds a canary
-// to each stack frame, which on function return is checked against a reference
-// canary. If the canaries do not match, it's likely that a stack buffer
-// overflow has occurred, so immediately crashing will prevent exploitation in
-// many cases.
-//
-// In some cases it's desirable to remove this, e.g. on hot functions, or if
-// we have purposely changed the reference canary.
-#if defined(COMPILER_GCC) || defined(__clang__)
-#if PA_HAS_ATTRIBUTE(__no_stack_protector__)
-#define PA_NO_STACK_PROTECTOR __attribute__((__no_stack_protector__))
-#else
-#define PA_NO_STACK_PROTECTOR \
-  __attribute__((__optimize__("-fno-stack-protector")))
-#endif
-#else
-#define PA_NO_STACK_PROTECTOR
 #endif
 
 // The ANALYZER_ASSUME_TRUE(bool arg) macro adds compiler-specific hints
@@ -339,15 +214,6 @@ inline constexpr bool AnalyzerAssumeTrue(bool arg) {
 #define PA_TRIVIAL_ABI
 #endif
 
-// Marks a member function as reinitializing a moved-from variable.
-// See also
-// https://clang.llvm.org/extra/clang-tidy/checks/bugprone-use-after-move.html#reinitialization
-#if defined(__clang__) && PA_HAS_ATTRIBUTE(reinitializes)
-#define PA_REINITIALIZES_AFTER_MOVE [[clang::reinitializes]]
-#else
-#define PA_REINITIALIZES_AFTER_MOVE
-#endif
-
 // Requires constant initialization. See constinit in C++20. Allows to rely on a
 // variable being initialized before execution, and not requiring a global
 // constructor.
@@ -359,10 +225,8 @@ inline constexpr bool AnalyzerAssumeTrue(bool arg) {
 #endif
 
 #if defined(__clang__)
-#define PA_GSL_OWNER [[gsl::Owner]]
 #define PA_GSL_POINTER [[gsl::Pointer]]
 #else
-#define PA_GSL_OWNER
 #define PA_GSL_POINTER
 #endif
 
