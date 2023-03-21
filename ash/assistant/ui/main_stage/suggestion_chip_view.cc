c7c2caee5f5c814fa7dd32ee3672d0e01b8917d2
Yuki Awano <yawano@google.com>
2023-03-21 04:29:59 +0000

    Extract ChipView from SuggestionChipView
    - Extract ChipView as a general view used to show a chip UI. General UI
      logics are extracted from SuggestionChipView to ChipView.
    - Assistant suggestion related logics are preserved in
      SuggestionChipView.
    
    Bug: b:272370530
    Test: Build passes.
    Change-Id: I2f7991846d616c1ffc4f6850a8e0d1c3a96e86c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355875
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119771}

--- ash/assistant/ui/main_stage/suggestion_chip_view.cc
+++ ash/assistant/ui/main_stage/suggestion_chip_view.cc
@@ -4,216 +4,45 @@
 
 #include "ash/assistant/ui/main_stage/suggestion_chip_view.h"
 
-#include <algorithm>
 #include <memory>
-#include <utility>
 
-#include "ash/assistant/ui/assistant_ui_constants.h"
 #include "ash/assistant/ui/assistant_view_delegate.h"
 #include "ash/assistant/ui/assistant_view_ids.h"
-#include "ash/assistant/ui/colors/assistant_colors.h"
+#include "ash/assistant/ui/main_stage/chip_view.h"
 #include "ash/assistant/util/resource_util.h"
-#include "ash/style/ash_color_id.h"
 #include "base/functional/bind.h"
 #include "base/strings/utf_string_conversions.h"
-#include "third_party/skia/include/core/SkColor.h"
-#include "ui/accessibility/ax_enums.mojom.h"
-#include "ui/accessibility/ax_node_data.h"
+#include "chromeos/ash/services/libassistant/public/cpp/assistant_suggestion.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/color/color_id.h"
-#include "ui/color/color_provider.h"
-#include "ui/gfx/canvas.h"
-#include "ui/gfx/color_palette.h"
-#include "ui/gfx/font_list.h"
-#include "ui/views/background.h"
-#include "ui/views/border.h"
-#include "ui/views/controls/button/button.h"
-#include "ui/views/controls/focus_ring.h"
-#include "ui/views/controls/highlight_path_generator.h"
-#include "ui/views/controls/image_view.h"
-#include "ui/views/controls/label.h"
-#include "ui/views/layout/box_layout.h"
-#include "ui/views/layout/layout_provider.h"
+#include "url/gurl.h"
 
 namespace ash {
-
-namespace {
-
-using assistant::util::ResourceLinkType;
-
-// Appearance.
-constexpr int kStrokeWidthDip = 1;
-constexpr int kFocusedStrokeWidthDip = 2;
-
-constexpr int kIconMarginDip = 8;
-constexpr int kIconSizeDip = 16;
-constexpr int kChipPaddingDip = 16;
-constexpr int kPreferredHeightDip = 32;
-
-}  // namespace
-
-// SuggestionChipView ----------------------------------------------------------
 
 SuggestionChipView::SuggestionChipView(AssistantViewDelegate* delegate,
                                        const AssistantSuggestion& suggestion)
     : delegate_(delegate), suggestion_id_(suggestion.id) {
-  InitLayout(suggestion);
+  SetText(base::UTF8ToUTF16(suggestion.text));
+
+  const GURL& url = suggestion.icon_url;
+  if (assistant::util::IsResourceLinkType(
+          url, assistant::util::ResourceLinkType::kIcon)) {
+    // Handle local images.
+    SetIcon(assistant::util::CreateVectorIcon(url, ChipView::kIconSizeDip));
+  } else if (!suggestion.icon_url.is_empty()) {
+    // In the case of a remote image, this prevents layout jank that would
+    // otherwise occur if we updated the view visibility only after the image
+    // downloaded.
+    MakeIconVisible();
+
+    // Handle remote images.
+    delegate_->DownloadImage(url, base::BindOnce(&SuggestionChipView::SetIcon,
+                                                 weak_factory_.GetWeakPtr()));
+  }
 }
 
 SuggestionChipView::~SuggestionChipView() = default;
 
-gfx::Size SuggestionChipView::CalculatePreferredSize() const {
-  const int preferred_width = views::View::CalculatePreferredSize().width();
-  return gfx::Size(preferred_width, GetHeightForWidth(preferred_width));
-}
-
-int SuggestionChipView::GetHeightForWidth(int width) const {
-  return kPreferredHeightDip;
-}
-
-void SuggestionChipView::ChildVisibilityChanged(views::View* child) {
-  // When icon visibility is modified we need to update layout padding.
-  if (child == icon_view_) {
-    const int padding_left_dip =
-        icon_view_->GetVisible() ? kIconMarginDip : kChipPaddingDip;
-    layout_manager_->set_inside_border_insets(
-        gfx::Insets::TLBR(0, padding_left_dip, 0, kChipPaddingDip));
-  }
-  PreferredSizeChanged();
-}
-
-void SuggestionChipView::InitLayout(const AssistantSuggestion& suggestion) {
-  const std::u16string text = base::UTF8ToUTF16(suggestion.text);
-
-  // Accessibility.
-  SetAccessibleName(text);
-
-  // Focus.
-  // 1. Dark light mode is OFF
-  // We change background color of a suggestion chip view. No focus ring is
-  // used.
-  // 2. Dark light mode is ON
-  // We use focus ring. No background color change with focus.
-  SetFocusBehavior(FocusBehavior::ALWAYS);
-  SetInstallFocusRingOnFocus(true);
-
-  views::FocusRing* focus_ring = views::FocusRing::Get(this);
-  focus_ring->SetColorId(ui::kColorAshFocusRing);
-  focus_ring->SetHaloThickness(kFocusedStrokeWidthDip);
-  focus_ring->SetHaloInset(0.0f);
-
-  // Path is used for the focus ring, i.e. path is not necessary for dark and
-  // light mode flag off case. But we always install this as it shouldn't be a
-  // problem even if we provide the path to the UI framework.
-  const int radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
-      views::Emphasis::kMaximum, size());
-  views::InstallRoundRectHighlightPathGenerator(this, gfx::Insets(), radius);
-
-  // Layout.
-  // Note that padding differs depending on icon visibility.
-  const int padding_left_dip =
-      suggestion.icon_url.is_empty() ? kChipPaddingDip : kIconMarginDip;
-
-  layout_manager_ = SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kHorizontal,
-      gfx::Insets::TLBR(0, padding_left_dip, 0, kChipPaddingDip),
-      kIconMarginDip));
-
-  layout_manager_->set_cross_axis_alignment(
-      views::BoxLayout::CrossAxisAlignment::kCenter);
-
-  // Icon.
-  icon_view_ = AddChildView(std::make_unique<views::ImageView>());
-  icon_view_->SetImageSize(gfx::Size(kIconSizeDip, kIconSizeDip));
-  icon_view_->SetPreferredSize(gfx::Size(kIconSizeDip, kIconSizeDip));
-
-  const GURL& url = suggestion.icon_url;
-  if (assistant::util::IsResourceLinkType(url, ResourceLinkType::kIcon)) {
-    // Handle local images.
-    icon_view_->SetImage(assistant::util::CreateVectorIcon(url, kIconSizeDip));
-  } else if (!suggestion.icon_url.is_empty()) {
-    // Handle remote images.
-    delegate_->DownloadImage(url, base::BindOnce(&SuggestionChipView::SetIcon,
-                                                 weak_factory_.GetWeakPtr()));
-  } else {
-    // Only hide the view if we have neither a local or a remote image. In the
-    // case of a remote image, this prevents layout jank that would otherwise
-    // occur if we updated the view visibility only after the image downloaded.
-    icon_view_->SetVisible(false);
-  }
-
-  // Text.
-  text_view_ = AddChildView(std::make_unique<views::Label>());
-  text_view_->SetID(kSuggestionChipViewLabel);
-  text_view_->SetAutoColorReadabilityEnabled(false);
-  text_view_->SetSubpixelRenderingEnabled(false);
-  const gfx::FontList& font_list = assistant::ui::GetDefaultFontList();
-  text_view_->SetFontList(font_list.Derive(
-      /*size_delta=*/1, font_list.GetFontStyle(), gfx::Font::Weight::MEDIUM));
-  SetText(text);
-}
-
-bool SuggestionChipView::OnKeyPressed(const ui::KeyEvent& event) {
-  if (event.key_code() == ui::VKEY_SPACE)
-    return false;
-  return Button::OnKeyPressed(event);
-}
-
-void SuggestionChipView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
-  Button::OnBoundsChanged(previous_bounds);
-
-  // If there is no change in height, we don't need to do anything as the code
-  // below is to update corner radius values.
-  if (height() == previous_bounds.height())
-    return;
-
-  const int radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
-      views::Emphasis::kMaximum, size());
-
-  // Only set the border if a ColorProvider is available. Otherwise, we cannot
-  // compute the stroke color.
-  auto* color_provider = GetColorProvider();
-  if (color_provider) {
-    SetBorder(views::CreateRoundedRectBorder(
-        kStrokeWidthDip, radius,
-        color_provider->GetColor(kColorAshSeparatorColor)));
-  }
-
-  views::InstallRoundRectHighlightPathGenerator(this, gfx::Insets(), radius);
-}
-
-void SuggestionChipView::OnThemeChanged() {
-  views::Button::OnThemeChanged();
-
-  auto* color_provider = GetColorProvider();
-  DCHECK(color_provider);
-  text_view_->SetEnabledColor(
-      color_provider->GetColor(kColorAshSuggestionChipViewTextView));
-  const int radius = views::LayoutProvider::Get()->GetCornerRadiusMetric(
-      views::Emphasis::kMaximum, size());
-  SetBorder(views::CreateRoundedRectBorder(
-      kStrokeWidthDip, radius,
-      color_provider->GetColor(kColorAshSeparatorColor)));
-}
-
-void SuggestionChipView::SetIcon(const gfx::ImageSkia& icon) {
-  icon_view_->SetImage(icon);
-  icon_view_->SetVisible(!icon.isNull());
-}
-
-gfx::ImageSkia SuggestionChipView::GetIcon() const {
-  return icon_view_->GetImage();
-}
-
-void SuggestionChipView::SetText(const std::u16string& text) {
-  text_view_->SetText(text);
-}
-
-const std::u16string& SuggestionChipView::GetText() const {
-  return text_view_->GetText();
-}
-
-BEGIN_METADATA(SuggestionChipView, views::Button)
+BEGIN_METADATA(SuggestionChipView, ChipView)
 END_METADATA
 
 }  // namespace ash
--- ash/assistant/ui/main_stage/suggestion_chip_view.h
+++ ash/assistant/ui/main_stage/suggestion_chip_view.h
@@ -5,23 +5,19 @@
 #ifndef ASH_ASSISTANT_UI_MAIN_STAGE_SUGGESTION_CHIP_VIEW_H_
 #define ASH_ASSISTANT_UI_MAIN_STAGE_SUGGESTION_CHIP_VIEW_H_
 
+#include "ash/assistant/ui/main_stage/chip_view.h"
 #include "base/component_export.h"
+#include "base/memory/weak_ptr.h"
+#include "base/unguessable_token.h"
 #include "chromeos/ash/services/libassistant/public/cpp/assistant_suggestion.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/views/controls/button/button.h"
-
-namespace views {
-class BoxLayout;
-class ImageView;
-class Label;
-}  // namespace views
 
 namespace ash {
 
 class AssistantViewDelegate;
 
-// View representing a suggestion chip.
-class COMPONENT_EXPORT(ASSISTANT_UI) SuggestionChipView : public views::Button {
+// View representing a suggestion chip for Assistant.
+class COMPONENT_EXPORT(ASSISTANT_UI) SuggestionChipView : public ChipView {
  public:
   using AssistantSuggestion = assistant::AssistantSuggestion;
 
@@ -33,32 +29,11 @@ class COMPONENT_EXPORT(ASSISTANT_UI) SuggestionChipView : public views::Button {
   SuggestionChipView& operator=(const SuggestionChipView&) = delete;
   ~SuggestionChipView() override;
 
-  // views::View:
-  gfx::Size CalculatePreferredSize() const override;
-  int GetHeightForWidth(int width) const override;
-  void ChildVisibilityChanged(views::View* child) override;
-  bool OnKeyPressed(const ui::KeyEvent& event) override;
-  void OnThemeChanged() override;
-  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
-
-  void SetIcon(const gfx::ImageSkia& icon);
-  gfx::ImageSkia GetIcon() const;
-
-  void SetText(const std::u16string& text);
-  const std::u16string& GetText() const;
-
   const base::UnguessableToken& suggestion_id() const { return suggestion_id_; }
 
  private:
-  void InitLayout(const AssistantSuggestion& suggestion);
-
   AssistantViewDelegate* const delegate_;
-
   const base::UnguessableToken suggestion_id_;
-
-  views::BoxLayout* layout_manager_;  // Owned by view hierarchy.
-  views::ImageView* icon_view_;       // Owned by view hierarchy.
-  views::Label* text_view_;           // Owned by view hierarchy.
 
   base::WeakPtrFactory<SuggestionChipView> weak_factory_{this};
 };
