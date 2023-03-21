43bf25726aac39f7c7d01b2c4d9831fdcfc7ddd0
Ankush Singh <ankushkush@google.com>
2023-03-21 17:18:47 +0000

    [sync] Enable SyncEnforceBookmarksCountLimit feature by default
    Bug: 1347466
    Change-Id: I9096024674ee150384d7fc4fee5a53d17940f16d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356858
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120018}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -108,7 +108,7 @@ BASE_FEATURE(kSyncEnableContactInfoDataTypeForCustomPassphraseUsers,
 
 BASE_FEATURE(kSyncEnforceBookmarksCountLimit,
              "SyncEnforceBookmarksCountLimit",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncDoNotPropagateBrowserShutdownToDataTypes,
              "SyncDoNotPropagateBrowserShutdownToDataTypes",
