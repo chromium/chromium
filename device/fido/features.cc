ce3b68af33407bb037ff0ddc1fcb30e3a88ea65b
Adam Langley <agl@chromium.org>
2023-03-21 19:31:46 +0000

    webauthn: advertise prf feature as hybrid authenticator.
    Play Services has updated to support this feature now.
    
    Bug: 1106961
    Change-Id: I97f7221480ae458b9b1401f966d40ae108f839a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4356440
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120121}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -57,10 +57,10 @@ BASE_FEATURE(kWebAuthnCredProtectThree,
              "WebAuthenticationCredProtectThree",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Added in M112. Disabled while we wait for Play Services support.
+// Added in M112. Enabled in M113. Remove in or after M116.
 BASE_FEATURE(kWebAuthnPRFAsAuthenticator,
              "WebAuthenticationPRFAsAuthenticator",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Added in M113. Remove in or after M116.
 BASE_FEATURE(kWebAuthnMacPlatformAuthenticatorOptionalUv,
