eefe8de8b53f4018986c855cf2d0fb6eb6f1d042
Andrew Williams <awillia@chromium.org>
2023-03-21 18:55:25 +0000

    [CacheStorage] Remove NOINLINE needed to prevent compiler bug on x86
    The compiler bug has been fixed:
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=1353198#c9
    
    I confirmed manually using these instructions:
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=1353198#c6
    
    Bug: 1353198
    Change-Id: Iaa13d7f4cf6e42c4f7b083c5a1318174eff7ca62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357615
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120093}

--- content/browser/cache_storage/cache_storage_manager.h
+++ content/browser/cache_storage/cache_storage_manager.h
@@ -131,10 +131,7 @@ class CONTENT_EXPORT CacheStorageManager
  protected:
   friend class base::RefCounted<CacheStorageManager>;
 
-  // TODO(https://crbug.com/1353198): NOINLINE here is needed to prevent a crash
-  // when ThinLTO is enabled on x86 Android chrome-branded release-mode builds.
-  // Remove this once the underlying issue is resolved.
-  NOINLINE CacheStorageManager(
+  CacheStorageManager(
       const base::FilePath& path,
       scoped_refptr<base::SequencedTaskRunner> cache_task_runner,
       scoped_refptr<base::SequencedTaskRunner> scheduler_task_runner,
