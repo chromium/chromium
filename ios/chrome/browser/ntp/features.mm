3404d4f78652289d1930cd679e0f8949306d2112
Ed Chin <edchin@google.com>
2023-03-21 16:11:06 +0000

    [ios][feed] Cleanup deprecated API
    Will land after https://crrev.com/i/5629789
    
    Change-Id: I515e84d03e1d3361ab001a332f8ac6e47042143c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355254
    Commit-Queue: edchin <edchin@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1119963}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -198,20 +198,6 @@ double GetBackgroundRefreshMaxAgeInSeconds() {
       /*default=*/0);
 }
 
-bool IsFeedRefreshPostFeedSessionEnabled() {
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kEnableFeedInvisibleForegroundRefresh,
-      kEnableFeedSessionCloseForegroundRefresh,
-      /*default=*/false);
-}
-
-bool IsFeedRefreshOnAppBackgroundingEnabled() {
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kEnableFeedInvisibleForegroundRefresh,
-      kEnableFeedAppCloseForegroundRefresh,
-      /*default=*/false);
-}
-
 bool IsFeedSessionCloseForegroundRefreshEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kEnableFeedInvisibleForegroundRefresh,
