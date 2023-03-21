9fa5a7fb6a00683efc5db190bb9a88a5eea065b5
Mohamed Heikal <mheikal@chromium.org>
2023-03-21 17:29:30 +0000

    Move mockito property setter outside class runner
    Some tests run without a class runner. We should set the mockito output
    location inside BaseChromiumAndroidJunitRunner instead.
    
    Bug: 1223832
    Change-Id: I8afdc9b3bbd735363318e6d7226e3a4ba0a77b26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357980
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120027}

--- base/test/android/javatests/src/org/chromium/base/test/BaseChromiumAndroidJUnitRunner.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseChromiumAndroidJUnitRunner.java
@@ -199,6 +199,12 @@ public class BaseChromiumAndroidJUnitRunner extends AndroidJUnitRunner {
             checkOrDeleteOnDiskSharedPreferences(false);
             clearDataDirectory(sInMemorySharedPreferencesContext);
             InstrumentationRegistry.getInstrumentation().setInTouchMode(true);
+            // Mockito sometimes gets confused about where to put mocks. Just tell it
+            // explicitly. Hopefully only temporary while we are migrating our test
+            // infra to use the latest androidx.test:runner. See
+            // https://crbug.com/1223832
+            System.setProperty("org.mockito.android.target",
+                    InstrumentationRegistry.getTargetContext().getCacheDir().getPath());
             super.onStart();
         }
     }
