35112e61f5940730f6411a261b768c8aa8ae56cb
Brian Osman <brianosman@google.com>
2023-03-22 12:48:29 +0000

    Always use Skia's analytic AA in (all) GPU pixel tests
    First CL (https://crrev.com/c/4337979) only targeted unaccelerated
    tests. There are a few tests that use unaccelerated canvas with GPU
    compositing, so these are also affected by the switch.
    
    Triaged image: https://chrome-public-gold.skia.org/detail?grouping=name%3DPixel_OffscreenCanvasUnaccelerated2DGPUCompositing%26source_type%3Dchrome-gpu&digest=138368928a19d20b50ae96de63a6ae81&changelist_id=4352809&crs=gerrit
    
    GPU Wrangler: Please ping me to triage new images (likely to arrive
    on Mac retina bots), before reverting. (Or triage them if you're
    comfortable doing so -- they will have subtle, expected diffs on
    geometry edges).
    
    Bug: chromium:1421297
    Change-Id: I32cbab223370932d27cd8f38c9400694ce392191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4352809
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120479}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -620,6 +620,8 @@ class PixelTestPages():
   def ExperimentalCanvasFeaturesPages(base_name: str) -> List[PixelTestPage]:
     browser_args = [
         cba.ENABLE_EXPERIMENTAL_WEB_PLATFORM_FEATURES,
+        # Temporarily force pixel tests to use AAA (https://crbug.com/1421297)
+        '--force-skia-analytic-aa',
     ]
     accelerated_args = [
         cba.DISABLE_SOFTWARE_COMPOSITING_FALLBACK,
@@ -627,9 +629,6 @@ class PixelTestPages():
     unaccelerated_args = [
         cba.DISABLE_ACCELERATED_2D_CANVAS,
         cba.DISABLE_GPU_COMPOSITING,
-        # Temporarily force unaccelerated (CPU) tests to use AAA
-        # (https://crbug.com/1421297)
-        '--force-skia-analytic-aa',
     ]
 
     return [
