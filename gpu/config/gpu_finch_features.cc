88f5d86a3e1885c504a3e8853f2d4aefa5d180fe
Justin Novosad <junov@chromium.org>
2023-03-22 22:10:06 +0000

    Enable OOPR-Canvas by default on Windows and arm64-mac
    Bug: 1240756
    Change-Id: Ie3b1c544d3acb132b8a4a2a12a44474d41807098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362105
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120819}

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -146,7 +146,8 @@ BASE_FEATURE(kDefaultEnableGpuRasterization,
 // Enables the use of out of process rasterization for canvas.
 BASE_FEATURE(kCanvasOopRasterization,
              "CanvasOopRasterization",
-#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_IOS)
+#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_IOS) || BUILDFLAG(IS_WIN) || \
+    (BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64))
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
