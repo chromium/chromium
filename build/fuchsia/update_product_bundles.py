764dcb6aedcb69b49f21240aeef1467c5c5bb504
Chong Gu <chonggu@google.com>
2023-03-22 20:27:15 +0000

    [Fuchsia] remove ffx_session.py and log_manager.py
    This concludes the removal of all duplicate code that existed in
    //build/fuchsia.
    
    Bug: 1405077
    Change-Id: I2f35e634154898f42e9197f228b95e0cab662105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357789
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Auto-Submit: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1120762}

--- build/fuchsia/update_product_bundles.py
+++ build/fuchsia/update_product_bundles.py
@@ -15,13 +15,12 @@ import subprocess
 import sys
 
 from contextlib import ExitStack
-import ffx_session
-import log_manager
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              'test')))
 
 import common
+import ffx_integration
 
 _PRODUCT_BUNDLES = [
     'core.x64-dfv2',
@@ -77,26 +76,22 @@ def get_hash_from_sdk():
     return json.load(f)['id']
 
 
-def remove_repositories(repo_names_to_remove, ffx_runner):
+def remove_repositories(repo_names_to_remove):
   """Removes given repos from repo list.
   Repo MUST be present in list to succeed.
 
   Args:
     repo_names_to_remove: List of repo names (as strings) to remove.
-    ffx_runner: ffx_session.FfxRunner instance to run the command.
   """
   for repo_name in repo_names_to_remove:
-    ffx_runner.run_ffx(('repository', 'remove', repo_name), check=True)
+    common.run_ffx_command(('repository', 'remove', repo_name), check=True)
 
 
-def get_repositories(ffx_runner):
+def get_repositories():
   """Lists repositories that are available on disk.
 
   Also prunes repositories that are listed, but do not have an actual packages
   directory.
-
-  Args:
-    ffx_runner: An FfxRunner instance.
 
   Returns:
     List of dictionaries containing info about the repositories. They have the
@@ -111,7 +106,9 @@ def get_repositories(ffx_runner):
   """
 
   repos = json.loads(
-      ffx_runner.run_ffx(('--machine', 'json', 'repository', 'list')).strip())
+      common.run_ffx_command(('--machine', 'json', 'repository', 'list'),
+                             check=True,
+                             capture_output=True).stdout.strip())
   to_prune = set()
   sdk_root_abspath = os.path.abspath(os.path.dirname(common.SDK_ROOT))
   for repo in repos:
@@ -126,25 +123,22 @@ def get_repositories(ffx_runner):
 
   repos = [repo for repo in repos if repo['name'] not in to_prune]
 
-  remove_repositories(to_prune, ffx_runner)
+  remove_repositories(to_prune)
   return repos
 
 
-def update_repositories_list(ffx_runner):
+def update_repositories_list():
   """Used to prune stale repositories."""
-  get_repositories(ffx_runner)
+  get_repositories()
 
 
-def remove_product_bundle(product_bundle, ffx_runner):
+def remove_product_bundle(product_bundle):
   """Removes product-bundle given."""
-  ffx_runner.run_ffx(('product-bundle', 'remove', '-f', product_bundle))
+  common.run_ffx_command(('product-bundle', 'remove', '-f', product_bundle))
 
 
-def get_product_bundle_urls(ffx_runner):
+def get_product_bundle_urls():
   """Retrieves URLs of available product-bundles.
-
-  Args:
-    ffx_runner: An FfxRunner instance.
 
   Returns:
     List of dictionaries of structure, indicating whether the product-bundle
@@ -155,8 +149,8 @@ def get_product_bundle_urls(ffx_runner):
     }
   """
   # TODO(fxb/115328): Replaces with JSON API when available.
-  bundles = ffx_runner.run_ffx(('product-bundle', 'list'), check=True)
-
+  bundles = common.run_ffx_command(('product-bundle', 'list'),
+                                   capture_output=True).stdout.strip()
   urls = [
       line.strip() for line in bundles.splitlines() if 'gs://fuchsia' in line
   ]
@@ -170,23 +164,20 @@ def get_product_bundle_urls(ffx_runner):
   return structured_urls
 
 
-def keep_product_bundles_by_sdk_version(sdk_version, ffx_runner):
+def keep_product_bundles_by_sdk_version(sdk_version):
   """Prunes product bundles not containing the sdk_version given."""
-  urls = get_product_bundle_urls(ffx_runner)
+  urls = get_product_bundle_urls()
   for url in urls:
     if url['downloaded'] and sdk_version not in url['url']:
-      remove_product_bundle(url['url'], ffx_runner)
+      remove_product_bundle(url['url'])
 
 
-def get_product_bundles(ffx_runner):
+def get_product_bundles():
   """Lists all downloaded product-bundles for the given SDK.
 
   Cross-references the repositories with downloaded packages and the stated
   downloaded product-bundles to validate whether or not a product-bundle is
   present. Prunes invalid product-bundles with each call as well.
-
-  Args:
-    ffx_runner: An FfxRunner instance.
 
   Returns:
     List of strings of product-bundle names downloaded and that FFX is aware
@@ -194,16 +185,13 @@ def get_product_bundles(ffx_runner):
   """
   downloaded_bundles = []
 
-  for url in get_product_bundle_urls(ffx_runner):
+  for url in get_product_bundle_urls():
     if url['downloaded']:
       # The product is separated by a #
       product = url['url'].split('#')
       downloaded_bundles.append(product[1])
 
-  # For each downloaded bundle, need to verify whether ffx repository believes
-  # it exists.
-  to_prune_bundles_index = []
-  repos = get_repositories(ffx_runner)
+  repos = get_repositories()
 
   # Some repo names do not match product-bundle names due to underscores.
   # Normalize them both.
@@ -216,19 +204,19 @@ def get_product_bundles(ffx_runner):
     if name.replace('-', '_') in repo_names:
       return True
 
-    remove_product_bundle(name, ffx_runner)
+    remove_product_bundle(name)
     return False
 
   return list(filter(bundle_is_active, downloaded_bundles))
 
 
-def download_product_bundle(product_bundle, ffx_runner):
+def download_product_bundle(product_bundle):
   """Download product bundles using the SDK."""
   # This also updates the repository list, in case it is stale.
-  update_repositories_list(ffx_runner)
+  update_repositories_list()
 
   try:
-    ffx_runner.run_ffx(
+    common.run_ffx_command(
         ('product-bundle', 'get', product_bundle, '--force-repo'))
   except subprocess.CalledProcessError as cpe:
     logging.error('Product bundle download has failed. ' +
@@ -236,22 +224,19 @@ def download_product_bundle(product_bundle, ffx_runner):
     raise
 
 
-def get_current_signature(ffx_runner):
+def get_current_signature():
   """Determines the SDK version of the product-bundles associated with the SDK.
 
   Parses this information from the URLs of the product-bundle.
 
-  Args:
-    ffx_runner: An FfxRunner instance.
-
   Returns:
     An SDK version string, or None if no product-bundle versions are downloaded.
   """
-  product_bundles = get_product_bundles(ffx_runner)
+  product_bundles = get_product_bundles()
   if not product_bundles:
     logging.info('No product bundles - signature will default to None')
     return None
-  product_bundle_urls = get_product_bundle_urls(ffx_runner)
+  product_bundle_urls = get_product_bundle_urls()
 
   # Get the numbers, hope they're the same.
   signatures = set()
@@ -299,11 +284,10 @@ def main():
                      'found in the DEPS file.')
 
   with ExitStack() as stack:
-    ffx_runner = ffx_session.FfxRunner(log_manager.LogManager(None))
 
     # Re-set the directory to which product bundles are downloaded so that
     # these bundles are located inside the Chromium codebase.
-    ffx_runner.run_ffx(
+    common.run_ffx_command(
         ('config', 'set', 'pbms.storage.path', common.IMAGES_ROOT))
 
     logging.debug('Checking for override file')
@@ -316,16 +300,17 @@ def main():
         pb_metadata = f.read().strip().split('\n')
         pb_metadata.append('{sdk.root}/*.json')
       stack.enter_context(
-          ffx_runner.scoped_config('pbms.metadata', json.dumps((pb_metadata))))
+          ffx_integration.ScopedFfxConfig('pbms.metadata',
+                                          json.dumps((pb_metadata))))
       logging.debug('Applied overrides')
 
     logging.debug('Getting new SDK hash')
     new_sdk_hash = get_hash_from_sdk()
-    keep_product_bundles_by_sdk_version(new_sdk_hash, ffx_runner)
+    keep_product_bundles_by_sdk_version(new_sdk_hash)
     logging.debug('Checking for current signature')
-    curr_signature = get_current_signature(ffx_runner)
+    curr_signature = get_current_signature()
 
-    current_images = get_product_bundles(ffx_runner)
+    current_images = get_product_bundles()
 
     # If SDK versions match, remove the product bundles that are no longer
     # needed and download missing ones.
@@ -335,34 +320,30 @@ def main():
       for image in current_images:
         if image not in new_product_bundles:
           logging.debug('Removing no longer needed Fuchsia image %s' % image)
-          remove_product_bundle(image, ffx_runner)
+          remove_product_bundle(image)
 
       bundles_to_download = set(new_product_bundles) - \
                             set(current_images)
       for bundle in bundles_to_download:
         logging.debug('Downloading image: %s', image)
-        download_product_bundle(bundle, ffx_runner)
+        download_product_bundle(bundle)
 
       return 0
 
     # If SDK versions do not match, remove all existing product bundles
     # and download the ones required.
     for pb in current_images:
-      remove_product_bundle(pb, ffx_runner)
+      remove_product_bundle(pb)
 
     logging.debug('Make clean images root')
-    curr_subdir = []
-    if os.path.exists(common.IMAGES_ROOT):
-      curr_subdir = os.listdir(common.IMAGES_ROOT)
     common.make_clean_directory(common.IMAGES_ROOT)
 
     for pb in new_product_bundles:
       logging.debug('Downloading bundle: %s', pb)
-      download_product_bundle(pb, ffx_runner)
+      download_product_bundle(pb)
 
-    current_pb = get_product_bundles(ffx_runner)
+    current_pb = get_product_bundles()
 
-    diff = set(current_pb) - set(new_product_bundles)
     assert set(current_pb) == set(new_product_bundles), (
         'Failed to download expected set of product-bundles. '
         f'Expected {new_product_bundles}, got {current_pb}')
--- build/fuchsia/update_product_bundles_test.py
+++ build/fuchsia/update_product_bundles_test.py
@@ -12,7 +12,6 @@ from unittest import mock
 
 from parameterized import parameterized
 
-import ffx_session
 import update_product_bundles
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
@@ -22,6 +21,14 @@ import common
 
 
 class TestUpdateProductBundles(unittest.TestCase):
+  def setUp(self):
+    ffx_mock = mock.Mock()
+    ffx_mock.returncode = 0
+    self._ffx_patcher = mock.patch('common.run_ffx_command',
+                                   return_value=ffx_mock)
+    self._ffx_mock = self._ffx_patcher.start()
+    self.addCleanup(self._ffx_mock.stop)
+
   def testConvertToProductBundleDefaultsUnknownImage(self):
     self.assertEqual(
         update_product_bundles.convert_to_product_bundle(['unknown-image']),
@@ -62,13 +69,11 @@ class TestUpdateProductBundles(unittest.TestCase):
 
     self.assertRaises(RuntimeError, update_product_bundles.get_hash_from_sdk)
 
-  def testRemoveRepositoriesRunsRemoveOnGivenRepos(self):
-    ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
+  @mock.patch('common.run_ffx_command')
+  def testRemoveRepositoriesRunsRemoveOnGivenRepos(self, ffx_mock):
+    update_product_bundles.remove_repositories(['foo', 'bar', 'fizz', 'buzz'])
 
-    update_product_bundles.remove_repositories(['foo', 'bar', 'fizz', 'buzz'],
-                                               ffx_runner)
-
-    ffx_runner.run_ffx.assert_has_calls([
+    ffx_mock.assert_has_calls([
         mock.call(('repository', 'remove', 'foo'), check=True),
         mock.call(('repository', 'remove', 'bar'), check=True),
         mock.call(('repository', 'remove', 'fizz'), check=True),
@@ -80,8 +85,7 @@ class TestUpdateProductBundles(unittest.TestCase):
   def testGetRepositoriesPrunesReposThatDoNotExist(self, mock_abspath,
                                                    mock_exists):
     with mock.patch('common.SDK_ROOT', 'some/path'):
-      ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
-      ffx_runner.run_ffx.return_value = json.dumps([{
+      self._ffx_mock.return_value.stdout = json.dumps([{
           "name": "terminal.qemu-x64",
           "spec": {
               "type": "pm",
@@ -97,34 +101,30 @@ class TestUpdateProductBundles(unittest.TestCase):
       mock_exists.side_effect = [True, False]
       mock_abspath.side_effect = lambda x: x
 
-      self.assertEqual(update_product_bundles.get_repositories(ffx_runner),
-                       [{
-                           "name": "terminal.qemu-x64",
-                           "spec": {
-                               "type": "pm",
-                               "path": "some/path/that/exists"
-                           }
-                       }])
+      self.assertEqual(update_product_bundles.get_repositories(), [{
+          "name": "terminal.qemu-x64",
+          "spec": {
+              "type": "pm",
+              "path": "some/path/that/exists"
+          }
+      }])
 
-      ffx_runner.run_ffx.assert_has_calls([
-          mock.call(('--machine', 'json', 'repository', 'list')),
+      self._ffx_mock.assert_has_calls([
+          mock.call(('--machine', 'json', 'repository', 'list'),
+                    capture_output=True,
+                    check=True),
           mock.call(('repository', 'remove', 'workstation-eng.chromebook-x64'),
                     check=True)
       ])
 
   def testRemoveProductBundle(self):
-    ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
+    update_product_bundles.remove_product_bundle('some-bundle-foo-bar')
 
-    update_product_bundles.remove_product_bundle('some-bundle-foo-bar',
-                                                 ffx_runner)
-
-    ffx_runner.run_ffx.assert_called_once_with(
+    self._ffx_mock.assert_called_once_with(
         ('product-bundle', 'remove', '-f', 'some-bundle-foo-bar'))
 
   def _InitFFXRunWithProductBundleList(self, sdk_version='10.20221114.2.1'):
-    ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
-
-    ffx_runner.run_ffx.return_value = f"""
+    self._ffx_mock.return_value.stdout = f"""
   gs://fuchsia/{sdk_version}/bundles.json#workstation_eng.qemu-x64
   gs://fuchsia/{sdk_version}/bundles.json#workstation_eng.chromebook-x64-dfv2
 * gs://fuchsia/{sdk_version}/bundles.json#workstation_eng.chromebook-x64
@@ -134,11 +134,10 @@ class TestUpdateProductBundles(unittest.TestCase):
 
 *No need to fetch with `ffx product-bundle get ...`.
     """
-    return ffx_runner
 
   def testGetProductBundleUrlsMarksDesiredAsDownloaded(self):
-    urls = update_product_bundles.get_product_bundle_urls(
-        self._InitFFXRunWithProductBundleList())
+    self._InitFFXRunWithProductBundleList()
+    urls = update_product_bundles.get_product_bundle_urls()
     expected_urls = [{
         'url':
         'gs://fuchsia/10.20221114.2.1/bundles.json#workstation_eng.qemu-x64',
@@ -169,7 +168,7 @@ class TestUpdateProductBundles(unittest.TestCase):
 
   @mock.patch('update_product_bundles.get_repositories')
   def testGetProductBundlesExtractsProductBundlesFromURLs(self, mock_get_repos):
-    ffx_runner = self._InitFFXRunWithProductBundleList()
+    self._InitFFXRunWithProductBundleList()
     mock_get_repos.return_value = [{
         'name': 'workstation-eng.chromebook-x64'
     }, {
@@ -179,7 +178,7 @@ class TestUpdateProductBundles(unittest.TestCase):
     }]
 
     self.assertEqual(
-        set(update_product_bundles.get_product_bundles(ffx_runner)),
+        set(update_product_bundles.get_product_bundles()),
         set([
             'workstation_eng.chromebook-x64',
             'terminal.qemu-x64',
@@ -189,7 +188,7 @@ class TestUpdateProductBundles(unittest.TestCase):
   @mock.patch('update_product_bundles.get_repositories')
   def testGetProductBundlesExtractsProductBundlesFromURLsFiltersMissingRepos(
       self, mock_get_repos):
-    ffx_runner = self._InitFFXRunWithProductBundleList()
+    self._InitFFXRunWithProductBundleList()
 
     # This will be missing two repos from the bundle list:
     # core and terminal.qemu-x64
@@ -201,34 +200,34 @@ class TestUpdateProductBundles(unittest.TestCase):
         'name': 'terminal.qemu-arm64'
     }]
 
-    self.assertEqual(update_product_bundles.get_product_bundles(ffx_runner),
+    self.assertEqual(update_product_bundles.get_product_bundles(),
                      ['workstation_eng.chromebook-x64'])
-    ffx_runner.run_ffx.assert_has_calls([
+    self._ffx_mock.assert_has_calls([
         mock.call(('product-bundle', 'remove', '-f', 'terminal.qemu-x64')),
         mock.call(('product-bundle', 'remove', '-f', 'core.x64-dfv2')),
     ],
-                                        any_order=True)
+                                    any_order=True)
 
+  @mock.patch('common.run_ffx_command')
   @mock.patch('update_product_bundles.update_repositories_list')
   def testDownloadProductBundleUpdatesRepoListBeforeCall(
-      self, mock_update_repo):
-    ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
+      self, mock_update_repo, mock_ffx):
     mock_sequence = mock.Mock()
     mock_sequence.attach_mock(mock_update_repo, 'update_repo_list')
-    mock_sequence.attach_mock(ffx_runner.run_ffx, 'run_ffx')
+    mock_sequence.attach_mock(mock_ffx, 'run_ffx_command')
 
-    update_product_bundles.download_product_bundle('some-bundle', ffx_runner)
+    update_product_bundles.download_product_bundle('some-bundle')
 
     mock_sequence.assert_has_calls([
-        mock.call.update_repo_list(ffx_runner),
-        mock.call.run_ffx(
+        mock.call.update_repo_list(),
+        mock.call.run_ffx_command(
             ('product-bundle', 'get', 'some-bundle', '--force-repo'))
     ])
 
+  @mock.patch('common.run_ffx_command')
   @mock.patch('update_product_bundles.get_product_bundle_urls')
   def testFilterProductBundleURLsRemovesBundlesWithoutGivenString(
-      self, mock_get_urls):
-    ffx_runner = mock.create_autospec(ffx_session.FfxRunner, instance=True)
+      self, mock_get_urls, mock_ffx):
     mock_get_urls.return_value = [
         {
             'url': 'some-url-has-buzz',
@@ -243,27 +242,25 @@ class TestUpdateProductBundles(unittest.TestCase):
             'downloaded': False,
         },
     ]
-    update_product_bundles.keep_product_bundles_by_sdk_version(
-        'buzz', ffx_runner)
-    ffx_runner.run_ffx.assert_called_once_with(
+    update_product_bundles.keep_product_bundles_by_sdk_version('buzz')
+    mock_ffx.assert_called_once_with(
         ('product-bundle', 'remove', '-f', 'some-url-to-remove-has-foo'))
 
   @mock.patch('update_product_bundles.get_repositories')
   def testGetCurrentSignatureReturnsNoneIfNoProductBundles(
       self, mock_get_repos):
-    ffx_runner = self._InitFFXRunWithProductBundleList()
+    self._InitFFXRunWithProductBundleList()
 
     # Forces no product-bundles
     mock_get_repos.return_value = []
 
     # Mutes logs
     with self.assertLogs():
-      self.assertIsNone(
-          update_product_bundles.get_current_signature(ffx_runner))
+      self.assertIsNone(update_product_bundles.get_current_signature())
 
   @mock.patch('update_product_bundles.get_repositories')
   def testGetCurrentSignatureParsesVersionCorrectly(self, mock_get_repos):
-    ffx_runner = self._InitFFXRunWithProductBundleList()
+    self._InitFFXRunWithProductBundleList()
     mock_get_repos.return_value = [{
         'name': 'workstation-eng.chromebook-x64'
     }, {
@@ -271,20 +268,19 @@ class TestUpdateProductBundles(unittest.TestCase):
     }]
 
     self.assertEqual('10.20221114.2.1',
-                     update_product_bundles.get_current_signature(ffx_runner))
+                     update_product_bundles.get_current_signature())
 
   @mock.patch('update_product_bundles.get_repositories')
   def testGetCurrentSignatureParsesCustomArtifactsCorrectlys(
       self, mock_get_repos):
-    ffx_runner = self._InitFFXRunWithProductBundleList(sdk_version='51390009')
+    self._InitFFXRunWithProductBundleList(sdk_version='51390009')
     mock_get_repos.return_value = [{
         'name': 'workstation-eng.chromebook-x64'
     }, {
         'name': 'terminal.qemu-x64'
     }]
 
-    self.assertEqual('51390009',
-                     update_product_bundles.get_current_signature(ffx_runner))
+    self.assertEqual('51390009', update_product_bundles.get_current_signature())
 
 
 if __name__ == '__main__':
