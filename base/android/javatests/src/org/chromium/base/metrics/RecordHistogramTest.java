0575845952c90fdb3aabb9602c45409bea2d00d7
Henrique Nakashima <hnakashima@chromium.org>
2023-03-22 20:42:23 +0000

    [Android] Move HistogramDelta to RecordHistogramTest
    Tests should now use HistogramWatcher and all usages except for
    RecordHistogramTest have been migrated.
    
    RecordHistogramTest can still benefit from HistogramDelta since
    getHistogramValueCountForTesting() needs test coverage too, so
    HistogramDelta becomes an inner class.
    
    Bug: 1412882
    Change-Id: Ia536f7f7055e76b24ecd6f68ec8ec151a9fed6cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4265479
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120772}

--- base/android/javatests/src/org/chromium/base/metrics/RecordHistogramTest.java
+++ base/android/javatests/src/org/chromium/base/metrics/RecordHistogramTest.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.MetricsUtils.HistogramDelta;
 
 /**
  * Tests for the Java API for recording UMA histograms.
@@ -192,5 +191,36 @@ public class RecordHistogramTest {
         Assert.assertEquals(2, zeroCount.getDelta());
         Assert.assertEquals(0, oneCount.getDelta());
         Assert.assertEquals(1, twoCount.getDelta());
+    }
+
+    /**
+     * Helper class that snapshots the given bucket of the given UMA histogram on its creation,
+     * allowing to inspect the number of samples recorded during its lifetime.
+     */
+    private static class HistogramDelta {
+        private final String mHistogram;
+        private final int mSampleValue;
+
+        private final int mInitialCount;
+
+        private int get() {
+            return RecordHistogram.getHistogramValueCountForTesting(mHistogram, mSampleValue);
+        }
+
+        /**
+         * Snapshots the given bucket of the given histogram.
+         * @param histogram name of the histogram to snapshot
+         * @param sampleValue the bucket that contains this value will be snapshot
+         */
+        public HistogramDelta(String histogram, int sampleValue) {
+            mHistogram = histogram;
+            mSampleValue = sampleValue;
+            mInitialCount = get();
+        }
+
+        /** Returns the number of samples of the snapshot bucket recorded since creation */
+        public int getDelta() {
+            return get() - mInitialCount;
+        }
     }
 }
