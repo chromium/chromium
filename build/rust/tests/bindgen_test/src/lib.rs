2704599d22a47be73180c4d5f699a5ef9e9ab9b7
danakj <danakj@chromium.org>
2023-03-22 20:51:16 +0000

    Avoid non-snake-case warnings in bindgen test against stdint.h
    On mac, stdint.h generates a _mbstateL symbol, which causes a
    non-snake-case warning. Silence this like we do for other non-
    normative rust names that come from bindgen.
    
    Bug: 1386212
    Change-Id: I0116f7831368404ee55a9ec637bc713bf26f1eef
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-rel,android-rust-arm64-dbg,linux-rust-x64-dbg,linux-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4362578
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120777}

--- build/rust/tests/bindgen_test/src/lib.rs
+++ build/rust/tests/bindgen_test/src/lib.rs
@@ -4,6 +4,7 @@
 
 mod c_ffi {
     #![allow(dead_code)]
+    #![allow(non_snake_case)]
     #![allow(non_camel_case_types)]
     #![allow(non_upper_case_globals)]
     include!(env!("BINDGEN_RS_FILE"));
