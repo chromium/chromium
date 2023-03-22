599c7711b31b706e84a7ccfbaf1f39886e7fea35
Brian Sheedy <bsheedy@chromium.org>
2023-03-22 15:52:57 +0000

    Surface heartbeat elapsed time
    Updates the WebGL heartbeat error handling to include the elapsed time
    when reporting certain errors. This should help us figure out whether a
    test's long runtime is due to the test actually taking a while before
    failing or due to post-failure info collection.
    
    Bug: 1354797
    Change-Id: Ia57c4e48123c27823074531b5149153e88c4692a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4357760
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120562}

--- content/test/gpu/gpu_tests/webgl_conformance_integration_test_base.py
+++ content/test/gpu/gpu_tests/webgl_conformance_integration_test_base.py
@@ -306,7 +306,9 @@ class WebGLConformanceIntegrationTestBase(
         raise RuntimeError('Received unknown message type %s' % response_type)
     except websocket_server.WebsocketReceiveMessageTimeoutError:
       logging.error(
-          'Timed out waiting for websocket message, checking for hung renderer')
+          'Timed out waiting for websocket message (%.3f seconds since test '
+          'start), checking for hung renderer',
+          time.time() - start_time)
       # Telemetry has some code to automatically crash the renderer and GPU
       # processes if it thinks that the renderer is hung. So, execute some
       # trivial JavaScript now to hit that code if we got the timeout because of
@@ -319,8 +321,8 @@ class WebGLConformanceIntegrationTestBase(
       raise
     except websocket_server.ClientClosedConnectionError as e:
       raise RuntimeError(
-          'Detected closed websocket - likely caused by a renderer '
-          'crash') from e
+          'Detected closed websocket (%.3f seconds since test start) - likely '
+          'caused by a renderer crash' % (time.time() - start_time)) from e
     finally:
       self.__class__.websocket_server.ClearCurrentConnection()
 
