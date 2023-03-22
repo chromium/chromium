d63c42a8e9c49facaa7b21fb03fb8f2ce520d224
Ken Rockot <rockot@google.com>
2023-03-22 23:42:42 +0000

    Fix mojo_perftests
    Use the ScopedMojoSupport helper which properly initializes Mojo
    in subprocesses when ipcz is enabled.
    
    Bug: None
    Change-Id: I8bcc955286ac9414f367661687ea04953fb4a78d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363738
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1120883}

--- mojo/core/test/run_all_perftests.cc
+++ mojo/core/test/run_all_perftests.cc
@@ -2,25 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/base_switches.h"
+#include "base/check.h"
 #include "base/command_line.h"
-#include "base/test/multiprocess_test.h"
+#include "base/feature_list.h"
 #include "base/test/perf_test_suite.h"
-#include "base/test/test_io_thread.h"
-#include "mojo/core/embedder/embedder.h"
-#include "mojo/core/embedder/scoped_ipc_support.h"
-#include "mojo/core/test/multiprocess_test_helper.h"
+#include "mojo/core/test/scoped_mojo_support.h"
 #include "mojo/core/test/test_support_impl.h"
 #include "mojo/public/tests/test_support_private.h"
 
 int main(int argc, char** argv) {
+  CHECK(base::CommandLine::Init(argc, argv));
+  const auto& cmd = *base::CommandLine::ForCurrentProcess();
+  auto features = std::make_unique<base::FeatureList>();
+  features->InitializeFromCommandLine(
+      cmd.GetSwitchValueASCII(switches::kEnableFeatures),
+      cmd.GetSwitchValueASCII(switches::kDisableFeatures));
+  base::FeatureList::SetInstance(std::move(features));
+
   base::PerfTestSuite test(argc, argv);
-
-  mojo::core::Init();
-  base::TestIOThread test_io_thread(base::TestIOThread::kAutoStart);
-  mojo::core::ScopedIPCSupport ipc_support(
-      test_io_thread.task_runner(),
-      mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
+  mojo::core::test::ScopedMojoSupport mojo_support;
   mojo::test::TestSupport::Init(new mojo::core::test::TestSupportImpl());
-
   return test.Run();
 }
