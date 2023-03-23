9ad818c4516a64cfca8971965d768c9bdf429040
Will Harris <wfh@chromium.org>
2023-03-23 19:28:50 +0000

    Handle no network time availability in DetectOutdatedInstall
    Previous to this CL, it was possible that network time would fail
    to return a valid value (e.g. it was still initializing) and
    a local time would be used instead.
    
    This local time cannot be trusted to always be before the
    build date of the running binary, as it might be incorrect.
    
    As a result, the DumpWithoutCrashing was hitting for these cases,
    and a nasty side effect of this was that for these users the
    outdated_build_timer_ was never being started, so they would
    never get the outdated prompt even if local time caught up with
    build time, or network time became available.
    
    This CL adds an additional check to only emit a DumpWithoutCrashing
    and return early, if the time was successfully obtained from the
    network and thus can be fully trusted.
    
    For the case where local time is in the future, and network time
    cannot be obtained, the code will retry on the outdated_build_timer_
    and eventually still hit the UpgradeDetected code path, once
    their local time catches up with build time + kOutdatedBuildAge,
    or network time becomes available (whichever happens sooner).
    
    Once it has been determined that the DumpWithoutCrashing is no
    longer hitting, then it can be replaced by a NOTREACHED.
    
    BUG=1407664
    
    Change-Id: I0c9fdc842265f67b878ebdad8cbd3ecd83e6736c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367300
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121322}

--- chrome/browser/upgrade_detector/upgrade_detector_impl.cc
+++ chrome/browser/upgrade_detector/upgrade_detector_impl.cc
@@ -240,29 +240,32 @@ void UpgradeDetectorImpl::StartOutdatedBuildDetector() {
 
 void UpgradeDetectorImpl::DetectOutdatedInstall() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::Time network_time;
+  base::Time current_time;
   base::TimeDelta uncertainty;
-  if (g_browser_process->network_time_tracker()->GetNetworkTime(&network_time,
+  bool is_network_time = true;
+  if (g_browser_process->network_time_tracker()->GetNetworkTime(&current_time,
                                                                 &uncertainty) !=
       network_time::NetworkTimeTracker::NETWORK_TIME_AVAILABLE) {
     // When network time has not been initialized yet, simply rely on the
     // machine's current time.
-    network_time = base::Time::Now();
+    is_network_time = false;
+    current_time = base::Time::Now();
   }
 
-  if (network_time.is_null() || build_date_.is_null() ||
-      (!simulating_outdated_ && build_date_ > network_time)) {
-    // TODO(crbug.com/1407664): Figure out why this is failing and either fix it
-    // and turn the conditional above into a CHECK or document how this can fail
-    // in the wild and either keep the DumpWithoutCrashing() or remove it.
+  CHECK(!current_time.is_null());
+  CHECK(!build_date_.is_null());
+
+  if (!simulating_outdated_ && is_network_time && build_date_ > current_time) {
     base::Time build_date = build_date_;
-    base::debug::Alias(&network_time);
+    base::debug::Alias(&current_time);
     base::debug::Alias(&build_date);
+    // TODO(crbug.com/1407664): Once this is shown to no longer be hitting,
+    // change this to a NOTREACHED_NORETURN().
     base::debug::DumpWithoutCrashing();
     return;
   }
 
-  if (network_time - build_date_ > kOutdatedBuildAge) {
+  if (current_time - build_date_ > kOutdatedBuildAge) {
     UpgradeDetected(is_auto_update_enabled_
                         ? UPGRADE_NEEDED_OUTDATED_INSTALL
                         : UPGRADE_NEEDED_OUTDATED_INSTALL_NO_AU);
