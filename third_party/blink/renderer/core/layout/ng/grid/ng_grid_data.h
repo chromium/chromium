7764e6126b3d8117a380c72399cec63ad52362f5
Ethan Jimenez <ethavar@microsoft.com>
2023-03-23 18:39:10 +0000

    [GridNG] Introduce NGGridSubtree as a base class for NGGridLayoutSubtree
    1. Introducing `NGGridSubtree`, an abstract class that will provide grid
       tree traversal logic that will be shared by `NGGridLayoutSubtree` and
       the upcoming subtree class for `NGGridSizingTree`.
    
    2. Moving most of the comments explaining the grid tree structure to the
       new `ng_grid_subtree.h`, which now centralizes the traversal logic.
    
    3. Consolidating the `layout_data_` and `subtree_size_` vectors in
       `NGGridLayoutTree` into a single new vector `tree_data_`.
    
    Bug: 618969
    Change-Id: I21fc3321c1d3254f89632599fb556d9419daec10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363609
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1121296}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_data.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_data.h
@@ -7,6 +7,7 @@
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_line_resolver.h"
+#include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_subtree.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_track_collection.h"
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -178,28 +179,9 @@ class CORE_EXPORT NGGridLayoutData {
 };
 
 // Subgrid layout relies on the root grid to perform the track sizing algorithm
-// for every level of nested subgrids. This class is a collection of finalized
-// layout data of every grid/subgrid in the entire grid tree, which will be
+// for every level of nested subgrids. This class contains the finalized layout
+// data of every node in a grid tree (see `ng_grid_subtree.h`), which will be
 // passed down to the constraint space of a subgrid to perform layout.
-//
-// The tree is represented by two vectors satisfying the following conditions:
-//   - The nodes in the tree are indexed using preorder traversal.
-//   - Each subtree is guaranteed to be contained in a single contiguous range.
-//   - We can iterate over a node's children by skipping over their subtrees;
-//   i.e., the first child of a node `k` is always at position `k+1`, the next
-//   sibling comes `subtree_size_[k+1]` positions later, and so on.
-//
-//         (0)
-//        /   \
-//     (1)     (7)
-//     / \     / \
-//   (2) (5) (8) (9)
-//   / \   \
-// (3) (4) (6)        (0)
-//                       (1)               (7)
-//                          (2)      (5)      (8)(9)
-//                             (3)(4)   (6)
-//   subtree_size_ = [10, 6, 3, 1, 1, 2, 1, 3, 1, 1]
 //
 // Note that this class allows subtrees to be compared for equality; this is
 // important because when we store this tree within a constraint space we want
@@ -207,28 +189,32 @@ class CORE_EXPORT NGGridLayoutData {
 // whether the provided subtree's track were sized exactly the same.
 class NGGridLayoutTree : public RefCounted<NGGridLayoutTree> {
  public:
-  explicit NGGridLayoutTree(wtf_size_t size = 1) {
-    subtree_size_.ReserveInitialCapacity(size);
-    layout_data_.ReserveInitialCapacity(size);
+  struct GridTreeNode {
+    NGGridLayoutData layout_data;
+    wtf_size_t subtree_size;
+  };
+
+  explicit NGGridLayoutTree(wtf_size_t initial_capacity) {
+    tree_data_.ReserveInitialCapacity(initial_capacity);
+  }
+
+  void Append(const NGGridLayoutData& layout_data, wtf_size_t subtree_size) {
+    GridTreeNode grid_node_data{layout_data, subtree_size};
+    tree_data_.emplace_back(std::move(grid_node_data));
   }
 
   bool AreSubtreesEqual(wtf_size_t subtree_root,
                         const NGGridLayoutTree& other,
                         wtf_size_t other_subtree_root) const {
-    DCHECK_LT(other_subtree_root, other.subtree_size_.size());
-    DCHECK_LT(subtree_root, subtree_size_.size());
+    const wtf_size_t subtree_size = SubtreeSize(subtree_root);
 
-    const wtf_size_t subtree_size = subtree_size_[subtree_root];
-    if (subtree_size != other.subtree_size_[other_subtree_root]) {
+    if (subtree_size != other.SubtreeSize(other_subtree_root)) {
       return false;
     }
 
-    DCHECK_LE(other_subtree_root + subtree_size, other.layout_data_.size());
-    DCHECK_LE(subtree_root + subtree_size, layout_data_.size());
-
     for (wtf_size_t i = 0; i < subtree_size; ++i) {
-      if (other.layout_data_[other_subtree_root + i] !=
-          layout_data_[subtree_root + i]) {
+      if (other.LayoutData(other_subtree_root + i) !=
+          LayoutData(subtree_root + i)) {
         return false;
       }
     }
@@ -236,58 +222,27 @@ class NGGridLayoutTree : public RefCounted<NGGridLayoutTree> {
   }
 
   const NGGridLayoutData& LayoutData(wtf_size_t index) const {
-    DCHECK_LT(index, layout_data_.size());
-    return layout_data_[index];
+    DCHECK_LT(index, tree_data_.size());
+    return tree_data_[index].layout_data;
   }
 
-  wtf_size_t Size() const {
-    DCHECK_EQ(layout_data_.size(), subtree_size_.size());
-    return layout_data_.size();
-  }
+  wtf_size_t Size() const { return tree_data_.size(); }
 
   wtf_size_t SubtreeSize(wtf_size_t index) const {
-    DCHECK_LT(index, subtree_size_.size());
-    return subtree_size_[index];
-  }
-
-  void Append(const NGGridLayoutData& layout_data, wtf_size_t subtree_size) {
-    subtree_size_.emplace_back(subtree_size);
-    layout_data_.emplace_back(layout_data);
+    DCHECK_LT(index, tree_data_.size());
+    return tree_data_[index].subtree_size;
   }
 
  private:
-  // Holds the finalized layout data of each grid in the tree.
-  Vector<NGGridLayoutData, 16> layout_data_;
-
-  // The size of the subtree rooted at each grid node. For a given index `k`,
-  // this means that the range [k, k + subtree_size_[k]) in both vectors
-  // represent the data of the subtree rooted at grid node `k`.
-  Vector<wtf_size_t, 16> subtree_size_;
+  Vector<GridTreeNode, 16> tree_data_;
 };
 
 // This class represents a subtree in a `NGGridLayoutTree` and mostly serves two
 // purposes: provide seamless iteration over the tree structure and compare
 // input subtrees to invalidate a subgrid's cached layout result.
-//
-// A subtree is represented by a pointer to the original layout tree's data (see
-// `NGGridLayoutTree` description) and the index of the subtree's root. However,
-// in order to iterate over the siblings of a given subtree we need to store the
-// index of the next sibling of its parent, aka the parent's end index, so that
-// the iterator doesn't traverse outside of the parent's subtree, e.g.:
-//
-//                                 (0)
-//                                    (1)               (7)
-//                                       (2)      (5)      (8)(9)
-//                                          (3)(4)   (6)
-//   layout_tree_.subtree_size_ = [10, 6, 3, 1, 1, 2, 1, 3, 1, 1]
-//
-// We can compute the next sibling of a subtree rooted at index 2 by adding the
-// subtree size at that index (2 + 3 = 5). On the other hand, when we want to
-// compute the next sibling for the subtree at index 5, adding the subtree size
-// (5 + 2) it's equal to its parent's next sibling (aka parent's end index), so
-// we can determine that such subtree doesn't have a next sibling.
-
-class NGGridLayoutSubtree {
+class NGGridLayoutSubtree
+    : public NGGridSubtree<NGGridLayoutSubtree,
+                           scoped_refptr<const NGGridLayoutTree>> {
   DISALLOW_NEW();
 
  public:
@@ -295,73 +250,31 @@ class NGGridLayoutSubtree {
 
   explicit NGGridLayoutSubtree(
       scoped_refptr<const NGGridLayoutTree>&& layout_tree)
-      : layout_tree_(std::move(layout_tree)), subtree_root_(0) {
-    DCHECK(layout_tree_);
-    parent_end_index_ = layout_tree_->Size();
-  }
+      : NGGridSubtree(std::move(layout_tree)) {}
 
-  NGGridLayoutSubtree(const scoped_refptr<const NGGridLayoutTree>& layout_tree,
-                      wtf_size_t parent_end_index,
-                      wtf_size_t subtree_root) {
-    DCHECK(layout_tree);
-    DCHECK_LE(parent_end_index, layout_tree->Size());
-    DCHECK_LE(subtree_root, parent_end_index);
-
-    // If the subtree root is beyond the parent's end index, we will keep this
-    // instance as a null subtree to indicate the end iterator for siblings.
-    if (subtree_root < parent_end_index) {
-      layout_tree_ = layout_tree;
-      parent_end_index_ = parent_end_index;
-      subtree_root_ = subtree_root;
-    }
-  }
-
-  explicit operator bool() const { return static_cast<bool>(layout_tree_); }
+  NGGridLayoutSubtree(wtf_size_t parent_end_index,
+                      wtf_size_t subtree_root,
+                      const scoped_refptr<const NGGridLayoutTree>& layout_tree)
+      : NGGridSubtree(parent_end_index, subtree_root, layout_tree) {}
 
   // This method is meant to be used for layout invalidation, so we only care
   // about comparing the layout data of both subtrees.
   bool operator==(const NGGridLayoutSubtree& other) const {
-    return (layout_tree_ && other.layout_tree_)
-               ? layout_tree_->AreSubtreesEqual(
-                     subtree_root_, *other.layout_tree_, other.subtree_root_)
-               : !layout_tree_ && !other.layout_tree_;
-  }
-
-  // First child is always at `subtree_root_ + 1` (see `NGGridLayoutTree`).
-  NGGridLayoutSubtree FirstChild() const {
-    return NGGridLayoutSubtree(layout_tree_,
-                               /* parent_end_index */ NextSiblingIndex(),
-                               /* subtree_root */ subtree_root_ + 1);
-  }
-
-  NGGridLayoutSubtree NextSibling() const {
-    return NGGridLayoutSubtree(layout_tree_,
-                               /* parent_end_index */ parent_end_index_,
-                               /* subtree_root */ NextSiblingIndex());
+    return (grid_tree_ && other.grid_tree_)
+               ? grid_tree_->AreSubtreesEqual(subtree_root_, *other.grid_tree_,
+                                              other.subtree_root_)
+               : !grid_tree_ && !other.grid_tree_;
   }
 
   const NGGridLayoutData& LayoutData() const {
-    DCHECK(layout_tree_);
-    return layout_tree_->LayoutData(subtree_root_);
+    DCHECK(grid_tree_);
+    return grid_tree_->LayoutData(subtree_root_);
   }
-
- private:
-  wtf_size_t NextSiblingIndex() const {
-    DCHECK(layout_tree_);
-    return subtree_root_ + layout_tree_->SubtreeSize(subtree_root_);
-  }
-
-  // Pointer to the layout tree data shared by multiple subtree instances.
-  scoped_refptr<const NGGridLayoutTree> layout_tree_;
-
-  // Index of the next sibling of this subtree's parent; used to avoid iterating
-  // outside of the parent's subtree when computing this subtree's next sibling.
-  wtf_size_t parent_end_index_{kNotFound};
-
-  // Index of this subtree's root node.
-  wtf_size_t subtree_root_{kNotFound};
 };
 
 }  // namespace blink
+
+WTF_ALLOW_CLEAR_UNUSED_SLOTS_WITH_MEM_FUNCTIONS(
+    blink::NGGridLayoutTree::GridTreeNode)
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_GRID_NG_GRID_DATA_H_
