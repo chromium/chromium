3c72061b86ba98ee74e0a6f1206bb17e645c055f
Nicolas Dossou-Gbete <dgn@chromium.org>
2023-03-23 13:42:17 +0000

    [fyfre] Disable FirstRunServiceCohortBrowserTest.GroupViaPrefs on Mac
    Bug: 1427094
    Change-Id: I534b3bf0b131950f9b96bc92f657a29f79346008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4365681
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1121133}

--- chrome/browser/ui/startup/first_run_service_browsertest.cc
+++ chrome/browser/ui/startup/first_run_service_browsertest.cc
@@ -490,15 +490,26 @@ IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest,
                                                   kStudyTestGroupName1));
 }
 
+// Flaky on Mac: crbug.com/1427094
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_PRE_PRE_GroupViaPrefs DISABLED_PRE_PRE_GroupViaPrefs
+#define MAYBE_PRE_GroupViaPrefs DISABLED_PRE_GroupViaPrefs
+#define MAYBE_GroupViaPrefs DISABLED_GroupViaPrefs
+#else
+#define MAYBE_PRE_PRE_GroupViaPrefs PRE_PRE_GroupViaPrefs
+#define MAYBE_PRE_GroupViaPrefs PRE_GroupViaPrefs
+#define MAYBE_GroupViaPrefs GroupViaPrefs
+#endif
 IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest,
-                       PRE_PRE_GroupViaPrefs) {
+                       MAYBE_PRE_PRE_GroupViaPrefs) {
   // Setting the pref, we expect it to get picked up in an upcoming startup.
   PrefService* local_state = g_browser_process->local_state();
   local_state->SetString(prefs::kFirstRunStudyGroup, kStudyTestGroupName2);
 
   EXPECT_FALSE(variations::HasSyntheticTrial("ForYouFreSynthetic"));
 }
-IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest, PRE_GroupViaPrefs) {
+IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest,
+                       MAYBE_PRE_GroupViaPrefs) {
   // The synthetic group should not be registered yet since we didn't go through
   // the FRE.
   EXPECT_FALSE(variations::HasSyntheticTrial("ForYouFreSynthetic"));
@@ -507,7 +518,7 @@ IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest, PRE_GroupViaPrefs) {
   PrefService* local_state = g_browser_process->local_state();
   local_state->SetBoolean(prefs::kFirstRunFinished, true);
 }
-IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest, GroupViaPrefs) {
+IN_PROC_BROWSER_TEST_F(FirstRunServiceCohortBrowserTest, MAYBE_GroupViaPrefs) {
   // The registered group is read from the prefs, not from the feature param.
   EXPECT_TRUE(variations::IsInSyntheticTrialGroup("ForYouFreSynthetic",
                                                   kStudyTestGroupName2));
