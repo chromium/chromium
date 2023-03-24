32f05a69472e112aee2cf3da1ce81881d93a097c
Louis Romero <lpromero@google.com>
2023-03-24 12:48:40 +0000

    Explicitly import the bridging header in inactive_tabs_button_ui_swift
    Prior to this CL, the following line warning appeared:
    
    ```
    [16488/17140] TOOL_VERSION=1668643813 python3 ../../build/toolchain/ios/swiftc.py -module-name ios_c...ft ../../ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button_header.swift
    ../../ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button.swift:6:8: warning: implicit import of bridging header 'swift_bridge.h' via module 'ios_chrome_common_ui_colors_swift' is deprecated and will be removed in a later version of Swift
    import ios_chrome_common_ui_colors_swift
    ```
    
    Offending line:
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button.swift;l=6;drc=ab66e85c5250c2b17656ed45cc7deaa45143ac58
    
    This was because the target `inactive_tabs_button_ui_swift` had a
    dependency on `//ios/chrome/common/ui/colors:swift` but didn't
    explicitly import that target's bridging header in its own briding
    header.
    
    Fixed: 1424414
    Change-Id: Iaca047de8f8c1f3a63034390829cad31dbe8c68f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370080
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121638}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button_ui_swift_bridge.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/inactive_tabs/inactive_tabs_button_ui_swift_bridge.h
@@ -5,10 +5,15 @@
 #ifndef IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_BUTTON_UI_SWIFT_BRIDGE_H_
 #define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_BUTTON_UI_SWIFT_BRIDGE_H_
 
-// Bridging header between Swift and Obj-C. These types/includes need to be pure
+// Bridging header between Swift and Obj-C. These types/imports need to be pure
 // Obj-C and have no C++ in them.
 
 #import "ios/chrome/grit/ios_strings.h"
+
+// Explicitly import the bridging header of the Swift dependencies, as the
+// implicit import of these bridging header is deprecated and will be removed in
+// a later version of Swift.
+#import "ios/chrome/common/ui/colors/swift_bridge.h"
 #import "ui/base/l10n/l10n_util_mac_bridge.h"
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_INACTIVE_TABS_INACTIVE_TABS_BUTTON_UI_SWIFT_BRIDGE_H_
