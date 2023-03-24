88bfdf42f2596b7df93e9b191957c1806135ff11
Wes Okuhara <wesokuhara@google.com>
2023-03-24 19:30:27 +0000

    CrOS Settings: Dynamic color support for Nearby share page template
    Light mode: http://screen/4HNwhE9Ji6NuvEP.png
    Dark mode: http://screen/8hR8NepWptRCch9.png
    
    Bug: b/266837484
    Change-Id: I3181196809cb5a9d5c04d6a4e349cecca5226549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363368
    Reviewed-by: Pu Shi <pushi@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1121872}

--- chrome/browser/resources/nearby_share/shared/nearby_page_template.ts
+++ chrome/browser/resources/nearby_share/shared/nearby_page_template.ts
@@ -9,6 +9,10 @@
  */
 
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+// <if expr='chromeos_ash'>
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
+
+// </if>
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
