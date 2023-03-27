44d14dacb50f55acb51940cdb322d4e4ee994030
danakj <danakj@chromium.org>
2023-03-27 18:29:42 +0000

    Disable Rust tests that fail for benign reasons with LLVM 17.
    tests/ui/abi/stack-protector.rs:
    https://github.com/rust-lang/rust/issues/109671 the test is being
    optimized in newer LLVM which breaks its expectations.
    
    tests/ui/backtrace.rs:
    https://github.com/rust-lang/rust/issues/109672 the second panic in a
    double-panic is being optimized out (reasonably correctly) by newer
    LLVM.
    
    Bug: b/274802621, 1401042
    Change-Id: I2be621f38dd1630c13eaf9128c76e0d17d684367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375277
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122545}

--- tools/rust/build_rust.py
+++ tools/rust/build_rust.py
@@ -63,18 +63,25 @@ from update_rust import (RUST_REVISION, RUST_TOOLCHAIN_OUT_DIR,
                          VERSION_STAMP_PATH, GetPackageVersionForBuild)
 
 EXCLUDED_TESTS = [
-    # Temporarily disabled due to https://github.com/rust-lang/rust/issues/94322
-    os.path.join('tests', 'ui', 'numeric', 'numeric-cast.rs'),
-    # Temporarily disabled due to https://github.com/rust-lang/rust/issues/96497
+    # https://github.com/rust-lang/rust/issues/45222 which appears to have
+    # regressed as of a recent LLVM update. This test is purely performance
+    # related, not correctness.
+    os.path.join('tests', 'codegen', 'issue-45222.rs'),
+    # https://github.com/rust-lang/rust/issues/96497
     os.path.join('tests', 'codegen', 'issue-96497-slice-size-nowrap.rs'),
     # TODO(crbug.com/1347563): Re-enable when fixed.
     os.path.join('tests', 'codegen', 'sanitizer-cfi-emit-type-checks.rs'),
     os.path.join('tests', 'codegen',
                  'sanitizer-cfi-emit-type-metadata-itanium-cxx-abi.rs'),
-    # Temporarily disabled due to https://github.com/rust-lang/rust/issues/45222
-    # which appears to have regressed as of a recent LLVM update. This test is
-    # purely performance related, not correctness.
-    os.path.join('tests', 'codegen', 'issue-45222.rs')
+    # https://github.com/rust-lang/rust/issues/109671 the test is being
+    # optimized in newer LLVM which breaks its expectations.
+    os.path.join('tests', 'ui', 'abi', 'stack-protector.rs'),
+    # https://github.com/rust-lang/rust/issues/109672 the second panic in a
+    # double-panic is being optimized out (reasonably correctly) by newer LLVM.
+    os.path.join('tests', 'ui', 'backtrace.rs'),
+    # https://github.com/rust-lang/rust/issues/94322 large output from
+    # compiletests is breaking json parsing of the results.
+    os.path.join('tests', 'ui', 'numeric', 'numeric-cast.rs'),
 ]
 EXCLUDED_TESTS_WINDOWS = [
     # https://github.com/rust-lang/rust/issues/96464
