da0c81eb6ad472ef380b8bd25a5f5749b3d89c06
Mikel Astiz <mastiz@chromium.org>
2023-03-27 14:34:44 +0000

    [UPM] Point TODOs to appropriate bug
    The newer bug, https://crbug.com/1312392, is more appropriate to track
    these TODOs.
    
    Change-Id: If61bd5561b6f1764e38cd912619c21855f0e672c
    Bug: 1312392
    Fixed: 1260837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369977
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122389}

--- chrome/browser/password_manager/android/password_sync_controller_delegate_android.cc
+++ chrome/browser/password_manager/android/password_sync_controller_delegate_android.cc
@@ -65,11 +65,11 @@ void PasswordSyncControllerDelegateAndroid::OnSyncStarting(
   // we either couldn't obtain sync status before OnSyncStarting was called, or
   // sync was already active and this is called on browser start up. In either
   // case we shouldn't react.
-  // TODO(crbug.com/1260837): Record whether OnSyncStarting is called before
+  // TODO(crbug.com/1312392): Record whether OnSyncStarting is called before
   // |is_sync_enabled_| holds value.
   if (is_sync_enabled_.has_value() &&
       is_sync_enabled_.value() == IsSyncEnabled(false)) {
-    // TODO(crbug.com/1260837): Sync was enabled. Move passwords from local
+    // TODO(crbug.com/1312392): Sync was enabled. Move passwords from local
     // storage to syncing storage.
     NOTIMPLEMENTED();
   }
@@ -99,7 +99,7 @@ void PasswordSyncControllerDelegateAndroid::OnSyncStopping(
       // sync data in the dashboard (birthday reset) or, at least in theory, the
       // sync server reporting that all sync metadata is obsolete (i.e.
       // CLIENT_DATA_OBSOLETE in the sync protocol).
-      // TODO(crbug.com/1260837): Sync was disabled. Move passwords from syncing
+      // TODO(crbug.com/1312392): Sync was disabled. Move passwords from syncing
       // storage to local storage.
       NOTIMPLEMENTED();
       is_sync_enabled_ = IsSyncEnabled(false);
