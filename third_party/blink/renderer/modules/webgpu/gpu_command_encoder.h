4180640b9f95fc191af013c59774f07c5c6a4cda
Jiawei Shao <jiawei.shao@intel.com>
2023-03-27 03:22:18 +0000

    WebGPU: add [NoAllocDirectCall] on GPUCommandEncoder functions
    This patch adds [NoAllocDirectCall] on all the member functions of
    GPUCommandEncoder that are qualified to be V8 fast apis and the
    related blink web tests.
    
    This patch also adds a check to the existing WebGPU fast API tests
    to fail the test when no expected errors are thrown during the
    execution of the test functions.
    
    Bug: chromium:1417558
    Change-Id: I723bb567b6ad8c3f9f0187cfc799d0005d8a086d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368805
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1122240}

--- third_party/blink/renderer/modules/webgpu/gpu_command_encoder.h
+++ third_party/blink/renderer/modules/webgpu/gpu_command_encoder.h
@@ -8,6 +8,7 @@
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.h"
 
 namespace blink {
 
@@ -22,7 +23,8 @@ class GPURenderPassDescriptor;
 class GPURenderPassEncoder;
 class GPUImageCopyTexture;
 
-class GPUCommandEncoder : public DawnObject<WGPUCommandEncoder> {
+class GPUCommandEncoder : public DawnObject<WGPUCommandEncoder>,
+                          public NoAllocDirectCallHost {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
