a816eacd2f165713236c50ae00e7705e028108c9
Gauthier Ambard <gambard@chromium.org>
2023-03-27 07:23:15 +0000

    [iOS] Prepare to move AlertCoordinator
    For downstream compatibility.
    
    Bug: 1319852
    Change-Id: Ia327c55d3ccb6438e7e1f0b0f81e26c51c6af153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370068
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122291}

---
+++ ios/chrome/browser/shared/coordinator/alert/alert_coordinator.h
@@ -0,0 +1,11 @@
+// Copyright 2016 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_COORDINATOR_ALERT_ALERT_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_SHARED_COORDINATOR_ALERT_ALERT_COORDINATOR_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_COORDINATOR_ALERT_ALERT_COORDINATOR_H_
