7038d454573c6e9a63b1e6d8c07e423619ceacd8
Yuzu Saijo <yuzus@chromium.org>
2023-03-27 05:24:56 +0000

    [sheriff] Disable a flaky ReadingList test
    Bug: 1427934
    Change-Id: I5f9e4139ad452a52ee558e14759fa9a904a8e140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369554
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122255}

--- ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_egtest.mm
@@ -1209,7 +1209,8 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
 }
 
 // Tests the Mark as Read/Unread context menu action for a reading list entry.
-- (void)testContextMenuMarkAsReadAndBack {
+// TODO(crbug.com/1427934): Flaky.
+- (void)DISABLED_testContextMenuMarkAsReadAndBack {
   AddEntriesAndOpenReadingList();
 
   AssertAllEntriesVisible();
