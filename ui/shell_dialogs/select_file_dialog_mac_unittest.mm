8f2cb52521c7a5528bfa639e002bdeaa96184162
Yuzu Saijo <yuzus@chromium.org>
2023-03-27 00:05:37 +0000

    [sheriff] Disable a flaky shell_dialogs test
    This CL disables a flaky shell_dialogs test.
    
    Bug: 1427906
    Change-Id: Ifc718ee1205cb1ba010249faed0e80097e9a23d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4369249
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Yuzu Saijo <yuzus@chromium.org>
    Owners-Override: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122210}

--- ui/shell_dialogs/select_file_dialog_mac_unittest.mm
+++ ui/shell_dialogs/select_file_dialog_mac_unittest.mm
@@ -524,7 +524,8 @@ TEST_F(SelectFileDialogMacTest, KeepExtensionVisible) {
   EXPECT_FALSE(panel.extensionHidden);
 }
 
-TEST_F(SelectFileDialogMacTest, DontCrashWithBogusExtension) {
+// TODO(crbug.com/1427906): This has been flaky.
+TEST_F(SelectFileDialogMacTest, DISABLED_DontCrashWithBogusExtension) {
   SelectFileDialog::FileTypeInfo file_type_info;
   file_type_info.extensions = {{"bogus type", "j.pg"}};
 
