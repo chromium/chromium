8192bafd43075d321b096f42e1d0b338f20466a9
Herb Derby <herb@google.com>
2023-03-27 16:52:35 +0000

    Enable the new streaming code
    Remove SK_SUPPORT_LEGACY_STRIKE_SERIALIZATION from Skia's config.
    
    Testing:
    Built Chrome, and tried several sites:
      Google Search
      New York Times
      Wikipedia entry for cat in English, Thai, and Chinese
      news.cn
      espn
    
    There were no serialization errors logged in chrome://gpu
    
    Bug: skia:14163
    Change-Id: If89e1d401e12eb8f27d006429ae82e11938d0e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4374637
    Commit-Queue: Herb Derby <herb@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122475}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -224,7 +224,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 #define SK_USE_LEGACY_MIPMAP_BUILDER
 
 // Use the original std::vector based serializer
-#define SK_SUPPORT_LEGACY_STRIKE_SERIALIZATION
+// Remove when new streaming support operations has been verified.
+// #define SK_SUPPORT_LEGACY_STRIKE_SERIALIZATION
 
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
