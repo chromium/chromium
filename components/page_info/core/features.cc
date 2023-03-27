303524f8b9361cdbd0019985b11b95b5fa743213
Christian Dullweber <dullweber@chromium.org>
2023-03-27 10:03:16 +0000

    AboutThisSite: Launch ATP on Desktop
    Bug: 1318000
    Change-Id: I80e4b64aeccf8b14d4a6af923093b8e2a49c903e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4290333
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1122326}

--- components/page_info/core/features.cc
+++ components/page_info/core/features.cc
@@ -56,11 +56,7 @@ const base::FeatureParam<bool> kShowSampleContent{&kPageInfoAboutThisSiteEn,
 
 BASE_FEATURE(kPageInfoAboutThisSiteMoreInfo,
              "PageInfoAboutThisSiteMoreInfo",
-#if BUILDFLAG(IS_ANDROID)
              base::FEATURE_ENABLED_BY_DEFAULT);
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
 
 #if !BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kPageInfoAboutThisSiteKeepSidePanelOnSameTabNavs,
