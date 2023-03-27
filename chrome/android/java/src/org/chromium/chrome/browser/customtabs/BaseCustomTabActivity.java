ff37be0a680f902f6b4755ece32e1e19090d1c31
Jinsuk Kim <jinsukkim@chromium.org>
2023-03-27 22:41:50 +0000

    [Pcct-SideSheet] Perform always custom close animation
    A logic performing the custom closing animation (side or bottom) was
    triggered only when a client app specifies the exit animation
    resource, the intention being that the animation will be overridden
    only when present, while just letting the system handle the default
    animation if not specified by the client app.
    
    This works for bottom sheet where the default animation was
    slide-down, but not for side sheet which has 2 options which should
    match the opening animation.
    
    This CL simplifies the logic so the custom animation will always be
    performed regardless of the presence of the exit animation resource.
    Both bottom/side sheet will take care of the closing animation
    without delegating it to the system.
    
    Bug: 1427620
    Change-Id: Ia6db50d37d36583cd780ff879e7fb630e5682acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375737
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1122711}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -487,8 +487,7 @@ public abstract class BaseCustomTabActivity extends ChromeActivity<BaseCustomTab
             // CustomTabActivityNavigationController#FinishHandler. Pass the mode enum into
             // CustomTabActivityModule, so that it can provide the correct implementation.
             getComponent().resolveTwaFinishHandler().onFinish(defaultBehavior);
-        } else if (intentDataProvider.isPartialCustomTab()
-                && intentDataProvider.shouldAnimateOnFinish()) {
+        } else if (intentDataProvider.isPartialCustomTab()) {
             // WebContents is missing during the close animation due to android:windowIsTranslucent.
             // We let partial CCT handle the animation.
             mBaseCustomTabRootUiCoordinator.handleCloseAnimation(defaultBehavior);
