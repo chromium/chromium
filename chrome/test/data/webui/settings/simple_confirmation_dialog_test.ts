41f57f3e972bf0e8a91253eeb9997417f0cd811c
dpapad <dpapad@chromium.org>
2023-03-27 19:51:02 +0000

    [M113 Merge] Settings: Restore primary button in destructive confirmation dialogs.
    Restoring the previous behavior (before http://crrev.com/c/4348622),
    while the discussion with UX about the proper long term pattern is still
    in progress.
    
    Also adding dedicated unit tests which were previously missing.
    
    (cherry picked from commit 9ed5c17d92d919c1570c1d6de83618e06e604810)
    
    Bug: 1427662
    Change-Id: I1fcf73609e80c321bbdea2da1bb3c4a2a9ef348e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4371258
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1122026}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375217
    Commit-Queue: Jared Saul <jsaul@google.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#48}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

---
+++ chrome/test/data/webui/settings/simple_confirmation_dialog_test.ts
@@ -0,0 +1,57 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://settings/lazy_load.js';
+
+import {SettingsSimpleConfirmationDialogElement} from 'chrome://settings/lazy_load.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
+
+suite('settings-simple-confirmation-dialog', function() {
+  let dialog: SettingsSimpleConfirmationDialogElement;
+
+  setup(function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    dialog = document.createElement('settings-simple-confirmation-dialog');
+    document.body.appendChild(dialog);
+  });
+
+  test('strings', function() {
+    dialog.titleText = 'fooTitle';
+    dialog.bodyText = 'fooBody';
+    dialog.confirmText = 'fooConfirm';
+
+    const title = dialog.shadowRoot!.querySelector('[slot=title]');
+    assertTrue(!!title);
+    assertEquals(dialog.titleText, title.textContent);
+
+    const body = dialog.shadowRoot!.querySelector('[slot=body]');
+    assertTrue(!!body);
+    assertEquals(dialog.bodyText, body.textContent!.trim());
+
+    assertEquals(dialog.confirmText, dialog.$.confirm.textContent!.trim());
+  });
+
+  test('noPrimaryButton', function() {
+    assertFalse(dialog.noPrimaryButton);
+    assertTrue(dialog.$.confirm.classList.contains('action-button'));
+
+    dialog.noPrimaryButton = true;
+    assertFalse(dialog.$.confirm.classList.contains('action-button'));
+  });
+
+  test('wasConfirmed_false', async function() {
+    const whenClosed = eventToPromise('close', dialog);
+    dialog.$.cancel.click();
+    await whenClosed;
+    assertFalse(dialog.wasConfirmed());
+  });
+
+  test('wasConfirmed_true', async function() {
+    const whenClosed = eventToPromise('close', dialog);
+    dialog.$.confirm.click();
+    await whenClosed;
+    assertTrue(dialog.wasConfirmed());
+  });
+});
