7cece19e190e8d3089683570c3ef3f657c99f08b
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-03-27 21:38:34 +0000

    [Extensions] Clean up extension_features.h/cc
    Clean up the base::Features entries in
    //extensions/common/extension_features.h/cc. In particular:
    * Add some general comments for guidance
    * Alphabetize existing entries. Previously, the order was largely random
      (and ordering chronologically doesn't make as much sense, as features
      may be enabled / tested / rolled out at dramatically different rates)
    
    Bug: None
    Change-Id: I365693295e0265dcdb3795ab4785da61a1366cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4364719
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1122665}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -7,48 +7,20 @@
 
 namespace extensions_features {
 
+///////////////////////////////////////////////////////////////////////////////
+// API Features
+///////////////////////////////////////////////////////////////////////////////
+
 // Controls the availability of the runtime.getContexts() API.
 BASE_FEATURE(kApiRuntimeGetContexts,
              "ApiRuntimeGetContexts",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Controls whether we show an install friction dialog when an Enhanced Safe
-// Browsing user tries to install an extension that is not included in the
-// Safe Browsing CRX allowlist. This feature also controls if we show a warning
-// in 'chrome://extensions' for extensions not included in the allowlist.
-BASE_FEATURE(kSafeBrowsingCrxAllowlistShowWarnings,
-             "SafeBrowsingCrxAllowlistShowWarnings",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+///////////////////////////////////////////////////////////////////////////////
+// Other Features
+///////////////////////////////////////////////////////////////////////////////
 
-// Automatically disable extensions not included in the Safe Browsing CRX
-// allowlist if the user has turned on Enhanced Safe Browsing (ESB). The
-// extensions can be disabled at ESB opt-in time or when an extension is moved
-// out of the allowlist.
-BASE_FEATURE(kSafeBrowsingCrxAllowlistAutoDisable,
-             "SafeBrowsingCrxAllowlistAutoDisable",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// Forces requests to go through WebRequestProxyingURLLoaderFactory.
-BASE_FEATURE(kForceWebRequestProxyForTest,
-             "ForceWebRequestProxyForTest",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// Enables the UI in the install prompt which lets a user choose to withhold
-// requested host permissions by default.
-BASE_FEATURE(kAllowWithholdingExtensionPermissionsOnInstall,
-             "AllowWithholdingExtensionPermissionsOnInstall",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// Enables support for the "match_origin_as_fallback" property in content
-// scripts.
-BASE_FEATURE(kContentScriptsMatchOriginAsFallback,
-             "ContentScriptsMatchOriginAsFallback",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-// Reports Extensions.WebRequest.KeepaliveRequestFinished when enabled.
-BASE_FEATURE(kReportKeepaliveUkm,
-             "ReportKeepaliveUkm",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+// For historical reasons, this includes some APIs. Please don't add more.
 
 // Whether extension contexts can use SharedArrayBuffers unconditionally (i.e.
 // without requiring cross origin isolation).
@@ -57,18 +29,18 @@ BASE_FEATURE(kAllowSharedArrayBuffersUnconditionally,
              "AllowSharedArrayBuffersUnconditionally",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// When enabled, causes Manifest V3 (and greater) extensions to use structured
-// cloning (instead of JSON serialization) for extension messaging, except when
-// communicating with native messaging hosts.
-BASE_FEATURE(kStructuredCloningForMV3Messaging,
-             "StructuredCloningForMV3Messaging",
+// Enables the UI in the install prompt which lets a user choose to withhold
+// requested host permissions by default.
+BASE_FEATURE(kAllowWithholdingExtensionPermissionsOnInstall,
+             "AllowWithholdingExtensionPermissionsOnInstall",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// When enabled, causes extensions to allow access to certain APIs only if the
-// user is in the developer mode.
-BASE_FEATURE(kRestrictDeveloperModeAPIs,
-             "RestrictDeveloperModeAPIs",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+// If enabled, calls RenderFrame::SetAllowsCrossBrowsingInstanceFrameLookup() in
+// DidCreateScriptContext() instead of DidCommitProvisionalLoad() to avoid
+// creating the script context too early which can be bad for performance.
+BASE_FEATURE(kAvoidEarlyExtensionScriptContextCreation,
+             "AvoidEarlyExtensionScriptContextCreation",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // When enabled, then bad_message::ReceivedBadMessage will be called when
 // browser receives an IPC from a content script and the IPC that unexpectedly
@@ -79,9 +51,10 @@ BASE_FEATURE(kCheckingNoExtensionIdInExtensionIpcs,
              "EMF_NO_EXTENSION_ID_FOR_EXTENSION_SOURCE",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Controls whether extensions can use the new favicon fetching in Manifest V3.
-BASE_FEATURE(kNewExtensionFaviconHandling,
-             "ExtensionsNewFaviconHandling",
+// Enables support for the "match_origin_as_fallback" property in content
+// scripts.
+BASE_FEATURE(kContentScriptsMatchOriginAsFallback,
+             "ContentScriptsMatchOriginAsFallback",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Determine if dynamic extension URLs are handled and redirected.
@@ -89,38 +62,9 @@ BASE_FEATURE(kExtensionDynamicURLRedirection,
              "ExtensionDynamicURLRedirection",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables enhanced site control for extensions and allowing the user to control
-// site permissions.
-BASE_FEATURE(kExtensionsMenuAccessControl,
-             "ExtensionsMenuAccessControl",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// If enabled, calls RenderFrame::SetAllowsCrossBrowsingInstanceFrameLookup() in
-// DidCreateScriptContext() instead of DidCommitProvisionalLoad() to avoid
-// creating the script context too early which can be bad for performance.
-BASE_FEATURE(kAvoidEarlyExtensionScriptContextCreation,
-             "AvoidEarlyExtensionScriptContextCreation",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-// The feature enabling offscreen documents in Manifest V3 extensions.
-BASE_FEATURE(kExtensionsOffscreenDocuments,
-             "ExtensionsOffscreenDocuments",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-// If enabled, allows APIs used by the webstore to be exposed on the URL for the
-// new webstore.
-BASE_FEATURE(kNewWebstoreDomain,
-             "NewWebstoreDomain",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Side panel API availability.
 BASE_FEATURE(kExtensionSidePanelIntegration,
              "ExtensionSidePanelIntegration",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-// File Handlers.
-BASE_FEATURE(kExtensionWebFileHandlers,
-             "ExtensionWebFileHandlers",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // IsValidSourceUrl enforcement for ExtensionHostMsg_OpenChannelToExtension IPC.
@@ -128,13 +72,10 @@ BASE_FEATURE(kExtensionSourceUrlEnforcement,
              "ExtensionSourceUrlEnforcement",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Controls the <webview> tag behaviour changes proposed as part of the guest
-// view MPArch migration. See
-// https://docs.google.com/document/d/1RVbtvklXUg9QCNvMT0r-1qDwJNeQFGoTCOD1Ur9mDa4/edit?usp=sharing
-// for details.
-BASE_FEATURE(kWebviewTagMPArchBehavior,
-             "WebviewTagMPArchBehavior",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+// File Handlers.
+BASE_FEATURE(kExtensionWebFileHandlers,
+             "ExtensionWebFileHandlers",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, only manifest v3 extensions is allowed while v2 will be disabled.
 // Note that this feature is now only checked by `ExtensionManagement` which
@@ -145,18 +86,10 @@ BASE_FEATURE(kExtensionsManifestV3Only,
              "ExtensionsManifestV3Only",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// If enabled, the minimum MV3 Content-Security-Policy will include
-// 'inline-speculation-rules' source in the script-src.
-// See https://crbug.com/1382361 to track the launch status.
-BASE_FEATURE(kMinimumMV3CSPWithInlineSpeculationRules,
-             "MinimumMV3CSPWithInlineSpeculationRules",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-// If enabled, APIs of the Telemetry Extension platform that have pending
-// approval will be enabled. Read more about the platform here:
-// https://chromium.googlesource.com/chromium/src/+/master/docs/telemetry_extension/README.md.
-BASE_FEATURE(kTelemetryExtensionPendingApprovalApi,
-             "TelemetryExtensionPendingApprovalApi",
+// Enables enhanced site control for extensions and allowing the user to control
+// site permissions.
+BASE_FEATURE(kExtensionsMenuAccessControl,
+             "ExtensionsMenuAccessControl",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, user permitted sites are granted access. This should only happen
@@ -165,5 +98,82 @@ BASE_FEATURE(kTelemetryExtensionPendingApprovalApi,
 BASE_FEATURE(kExtensionsMenuAccessControlWithPermittedSites,
              "ExtensionsMenuAccessControlWithPermittedSitesName",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+// The feature enabling offscreen documents in Manifest V3 extensions.
+BASE_FEATURE(kExtensionsOffscreenDocuments,
+             "ExtensionsOffscreenDocuments",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// Forces requests to go through WebRequestProxyingURLLoaderFactory.
+BASE_FEATURE(kForceWebRequestProxyForTest,
+             "ForceWebRequestProxyForTest",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// If enabled, the minimum MV3 Content-Security-Policy will include
+// 'inline-speculation-rules' source in the script-src.
+// See https://crbug.com/1382361 to track the launch status.
+BASE_FEATURE(kMinimumMV3CSPWithInlineSpeculationRules,
+             "MinimumMV3CSPWithInlineSpeculationRules",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// Controls whether extensions can use the new favicon fetching in Manifest V3.
+BASE_FEATURE(kNewExtensionFaviconHandling,
+             "ExtensionsNewFaviconHandling",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// If enabled, allows APIs used by the webstore to be exposed on the URL for the
+// new webstore.
+BASE_FEATURE(kNewWebstoreDomain,
+             "NewWebstoreDomain",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kReportKeepaliveUkm,
+             "ReportKeepaliveUkm",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// When enabled, causes extensions to allow access to certain APIs only if the
+// user is in the developer mode.
+BASE_FEATURE(kRestrictDeveloperModeAPIs,
+             "RestrictDeveloperModeAPIs",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Reports Extensions.WebRequest.KeepaliveRequestFinished when enabled.
+// Automatically disable extensions not included in the Safe Browsing CRX
+// allowlist if the user has turned on Enhanced Safe Browsing (ESB). The
+// extensions can be disabled at ESB opt-in time or when an extension is moved
+// out of the allowlist.
+BASE_FEATURE(kSafeBrowsingCrxAllowlistAutoDisable,
+             "SafeBrowsingCrxAllowlistAutoDisable",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Controls whether we show an install friction dialog when an Enhanced Safe
+// Browsing user tries to install an extension that is not included in the
+// Safe Browsing CRX allowlist. This feature also controls if we show a warning
+// in 'chrome://extensions' for extensions not included in the allowlist.
+BASE_FEATURE(kSafeBrowsingCrxAllowlistShowWarnings,
+             "SafeBrowsingCrxAllowlistShowWarnings",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+// When enabled, causes Manifest V3 (and greater) extensions to use structured
+// cloning (instead of JSON serialization) for extension messaging, except when
+// communicating with native messaging hosts.
+BASE_FEATURE(kStructuredCloningForMV3Messaging,
+             "StructuredCloningForMV3Messaging",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// If enabled, APIs of the Telemetry Extension platform that have pending
+// approval will be enabled. Read more about the platform here:
+// https://chromium.googlesource.com/chromium/src/+/master/docs/telemetry_extension/README.md.
+BASE_FEATURE(kTelemetryExtensionPendingApprovalApi,
+             "TelemetryExtensionPendingApprovalApi",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Controls the <webview> tag behaviour changes proposed as part of the guest
+// view MPArch migration. See
+// https://docs.google.com/document/d/1RVbtvklXUg9QCNvMT0r-1qDwJNeQFGoTCOD1Ur9mDa4/edit?usp=sharing
+// for details.
+BASE_FEATURE(kWebviewTagMPArchBehavior,
+             "WebviewTagMPArchBehavior",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace extensions_features
--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -9,59 +9,92 @@
 
 namespace extensions_features {
 
-// TODO(devlin):
-// 1) Alphabetize this list. Right now, it's just random.
-// 2) Add guidance that API-related features should be in a defined form,
-//    kApi<Namespace><Method>.
+///////////////////////////////////////////////////////////////////////////////
+// README!
+// * Please keep these features alphabetized. One exception: API features go
+//   at the top so that they are visibly grouped together.
+// * Adding a new feature for an extension API? Great!
+//   Please use the naming style `kApi<Namespace><Method>`, e.g.
+//   `kApiTabsCreate`.
+//   Note that if you are using the features.json files to restrict your
+//   API with the feature (which is usually best practice if you are introducing
+//   any new features), you will also have to add the feature entry to the list
+//   in extensions/common/features/feature_flags.cc so the features system can
+//   detect it.
+// * Naming Tips: Even though this file is unique to extensions, base::Features
+//   have to be globally unique. Thus, it's often best to give features very
+//   specific names (often including "Extension", unlike many C++ class names)
+//   since namespacing doesn't otherwise exist.
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// API Features
+///////////////////////////////////////////////////////////////////////////////
+
+// NOTE(devlin): If there are consistently enough of these in flux, it might
+// make sense to have their own file.
 
 BASE_DECLARE_FEATURE(kApiRuntimeGetContexts);
 
-BASE_DECLARE_FEATURE(kSafeBrowsingCrxAllowlistShowWarnings);
-BASE_DECLARE_FEATURE(kSafeBrowsingCrxAllowlistAutoDisable);
+///////////////////////////////////////////////////////////////////////////////
+// Other Features
+///////////////////////////////////////////////////////////////////////////////
 
-BASE_DECLARE_FEATURE(kForceWebRequestProxyForTest);
-
-BASE_DECLARE_FEATURE(kAllowWithholdingExtensionPermissionsOnInstall);
-
-BASE_DECLARE_FEATURE(kContentScriptsMatchOriginAsFallback);
-
-BASE_DECLARE_FEATURE(kReportKeepaliveUkm);
+// For historical reasons, this includes some APIs. Please don't add more.
 
 BASE_DECLARE_FEATURE(kAllowSharedArrayBuffersUnconditionally);
 
-BASE_DECLARE_FEATURE(kStructuredCloningForMV3Messaging);
-
-BASE_DECLARE_FEATURE(kRestrictDeveloperModeAPIs);
-
-BASE_DECLARE_FEATURE(kCheckingNoExtensionIdInExtensionIpcs);
-
-BASE_DECLARE_FEATURE(kNewExtensionFaviconHandling);
-
-BASE_DECLARE_FEATURE(kExtensionDynamicURLRedirection);
-
-BASE_DECLARE_FEATURE(kExtensionsMenuAccessControl);
+BASE_DECLARE_FEATURE(kAllowWithholdingExtensionPermissionsOnInstall);
 
 BASE_DECLARE_FEATURE(kAvoidEarlyExtensionScriptContextCreation);
 
-BASE_DECLARE_FEATURE(kExtensionsOffscreenDocuments);
+BASE_DECLARE_FEATURE(kCheckingNoExtensionIdInExtensionIpcs);
 
-BASE_DECLARE_FEATURE(kNewWebstoreDomain);
+BASE_DECLARE_FEATURE(kContentScriptsMatchOriginAsFallback);
+
+BASE_DECLARE_FEATURE(kExtensionDynamicURLRedirection);
 
 BASE_DECLARE_FEATURE(kExtensionSidePanelIntegration);
 
-BASE_DECLARE_FEATURE(kExtensionWebFileHandlers);
-
 BASE_DECLARE_FEATURE(kExtensionSourceUrlEnforcement);
 
-BASE_DECLARE_FEATURE(kWebviewTagMPArchBehavior);
+BASE_DECLARE_FEATURE(kExtensionWebFileHandlers);
 
 BASE_DECLARE_FEATURE(kExtensionsManifestV3Only);
 
+BASE_DECLARE_FEATURE(kExtensionsMenuAccessControl);
+
+BASE_DECLARE_FEATURE(kExtensionsMenuAccessControlWithPermittedSites);
+
+BASE_DECLARE_FEATURE(kExtensionsOffscreenDocuments);
+
+BASE_DECLARE_FEATURE(kForceWebRequestProxyForTest);
+
 BASE_DECLARE_FEATURE(kMinimumMV3CSPWithInlineSpeculationRules);
+
+BASE_DECLARE_FEATURE(kNewExtensionFaviconHandling);
+
+BASE_DECLARE_FEATURE(kNewWebstoreDomain);
+
+BASE_DECLARE_FEATURE(kReportKeepaliveUkm);
+
+BASE_DECLARE_FEATURE(kRestrictDeveloperModeAPIs);
+
+BASE_DECLARE_FEATURE(kSafeBrowsingCrxAllowlistAutoDisable);
+
+BASE_DECLARE_FEATURE(kSafeBrowsingCrxAllowlistShowWarnings);
+
+BASE_DECLARE_FEATURE(kStructuredCloningForMV3Messaging);
 
 BASE_DECLARE_FEATURE(kTelemetryExtensionPendingApprovalApi);
 
-BASE_DECLARE_FEATURE(kExtensionsMenuAccessControlWithPermittedSites);
+BASE_DECLARE_FEATURE(kWebviewTagMPArchBehavior);
+
+///////////////////////////////////////////////////////////////////////////////
+// STOP!
+// Please don't just add your new feature down here.
+// See the guidance at the top of this file.
+///////////////////////////////////////////////////////////////////////////////
 
 }  // namespace extensions_features
 
