f5bc64239a4549858f469f28ea26392facbf0b32
Sky Malice <skym@chromium.org>
2023-03-28 17:45:37 +0000

    Disable flaky #testOnCreateSchedulesUpdater.
    Bug: 1428048
    Change-Id: Iec95e3fa051172f53d53136cdb6caafa38eb7868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4376026
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123138}

--- android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
@@ -33,6 +33,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.FileUtils;
 import org.chromium.base.PathUtils;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.background_task_scheduler.TaskIds;
 import org.chromium.components.component_updater.IComponentsProviderService;
@@ -283,6 +284,7 @@ public class ComponentsProviderServiceTest {
 
         @Test
         @SmallTest
+        @DisabledTest(message = "https://crbug.com/1428048")
         public void testOnCreateSchedulesUpdater() throws Exception {
             JobScheduler jobScheduler =
                     (JobScheduler) ContextUtils.getApplicationContext().getSystemService(
