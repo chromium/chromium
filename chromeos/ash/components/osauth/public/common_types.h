313dcbbbd672a2ca92baa833da788b0519da9993
Denis Kuznetsov <antrim@chromium.org>
2023-03-28 16:35:47 +0000

    Add interface for AuthFactorEngine
    Bug: b:271248265
    Change-Id: I87c4876e694220a9158623e72b4cd360a7a709c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348498
    Reviewed-by: Martin Bidlingmaier <mbid@google.com>
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123089}

--- chromeos/ash/components/osauth/public/common_types.h
+++ chromeos/ash/components/osauth/public/common_types.h
@@ -19,6 +19,37 @@ namespace ash {
 // replace it with StrongAlias or UnguessableToken.
 using AuthProofToken = std::string;
 
+// Authentication can be required for different scenarios,
+// with some specifics or trade-offs. This enumeration allows
+// to distinguish such scenarios when requesting authentication.
+enum class AuthPurpose {
+  kLogin,             // Authentication to sign in.
+  kAuthSettings,      // Access to the section of os://settings.
+  kScreenUnlock,      // Removing the lock screen.
+  kWebAuthN,          // Local user verification in WebAuthN flow,
+                      // where ChromeOS device serves as FIDO2 Authenticator.
+  kUserVerification,  // Local user verification e.g. in Chrome password
+                      // manager.
+};
+
+// Authentication factors (and their implementations) that can be used
+// for interactive authentication in Ash.
+// Not to be confused with cryptohome AuthFactors:
+//   * Some factors (like SmartLock) are implemented without cryptohome;
+//   * Some cryptohome factors (like Kiosk) are not used for regular user
+//     authentication;
+//   * Multiple factors (e.g. GAIA password and Local password) might be
+//     backed by the same cryptohome factor.
+enum class AshAuthFactor {
+  kGaiaPassword,
+  kCryptohomePin,
+  kSmartCard,
+  kSmartUnlock,
+  kRecovery,
+  kLegacyPin,
+  kLegacyFingerprint,
+};
+
 }  // namespace ash
 
 #endif  // CHROMEOS_ASH_COMPONENTS_OSAUTH_PUBLIC_COMMON_TYPES_H_
