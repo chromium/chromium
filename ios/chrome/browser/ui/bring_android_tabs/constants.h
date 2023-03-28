f3a74baef759222f97282d39354fb1982f7aaeb9
ginnnnnnny <ginnyhuang@chromium.org>
2023-03-28 22:01:48 +0000

    [iOS][byot] Adds constants file to store AXId
    Bug: 1418120,1418117
    Change-Id: If5fde8ed03ffc9a5bf4a48572f28239585f99b6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377382
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123301}

---
+++ ios/chrome/browser/ui/bring_android_tabs/constants.h
@@ -0,0 +1,18 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+// Accessibility identifier for the "Bring Android Tabs" confirmation alert
+// prompt view controller.
+extern NSString* const kBringAndroidTabsPromptConfirmationAlertAXId;
+
+// Accessibility identifier for the "Bring Android Tabs" bottom message prompt
+// view controller.
+extern NSString* const kBringAndroidTabsPromptBottomMessageAXId;
+
+#endif  // IOS_CHROME_BROWSER_UI_BRING_ANDROID_TABS_CONSTANTS_H_
---
+++ ios/chrome/browser/ui/bring_android_tabs/constants.mm
@@ -0,0 +1,15 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/bring_android_tabs/constants.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+NSString* const kBringAndroidTabsPromptConfirmationAlertAXId =
+    @"kBringAndroidTabsPromptConfirmationAlertAccessibilityIdentifier";
+
+NSString* const kBringAndroidTabsPromptBottomMessageAXId =
+    @"kBringAndroidTabsPromptBottomMessageAccessibilityIdentifier";
