995b45879fd5cdefbf09177ad7cb9c3a74367605
Morten Stenshorne <mstensho@chromium.org>
2023-03-28 20:20:10 +0000

    [RemoveLegacy] Copy over some LayoutTable documentation.
    LayoutTable is going away, so copy most of its documentation (except for
    the effectiveColumns stuff) over to LayoutNGTable.
    
    Bug: 1229581
    Change-Id: Ic4f295d27ebb5370adef76ec5cd250345a1460d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377224
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123242}

--- third_party/blink/renderer/core/layout/ng/table/layout_ng_table.h
+++ third_party/blink/renderer/core/layout/ng/table/layout_ng_table.h
@@ -16,6 +16,68 @@ namespace blink {
 
 class NGTableBorders;
 
+// LayoutNGTable is the LayoutObject associated with
+// display: table or inline-table.
+//
+// LayoutNGTable is the coordinator for determining the overall table structure.
+// The reason is that LayoutNGTableSection children have a local view over what
+// their structure is but don't account for other LayoutNGTableSection. Thus
+// LayoutNGTable helps keep consistency across LayoutNGTableSection.
+//
+// LayoutNGTable expects only 3 types of children:
+// - zero or more LayoutNGTableColumn
+// - zero or more LayoutNGTableCaption
+// - zero or more LayoutNGTableSection
+// This is aligned with what HTML5 expects:
+// https://html.spec.whatwg.org/C/#the-table-element
+// with one difference: we allow more than one caption as we follow what
+// CSS expects (https://bugs.webkit.org/show_bug.cgi?id=69773).
+// Those expectations are enforced by LayoutNGTable::AddChild, that wraps
+// unknown children into an anonymous LayoutNGTableSection. This is what the
+// "generate missing child wrapper" step in CSS mandates in
+// http://www.w3.org/TR/CSS21/tables.html#anonymous-boxes.
+//
+// LayoutNGTable assumes a pretty strict structure that is mandated by CSS:
+// (note that this structure in HTML is enforced by the HTML5 Parser).
+//
+//                 LayoutNGTable
+//                 |          |
+//  LayoutNGTableSection    LayoutNGTableCaption
+//                 |
+//      LayoutNGTableRow
+//                 |
+//     LayoutNGTableCell
+//
+// This means that we have to generate some anonymous table wrappers in order to
+// satisfy the structure. See again
+// http://www.w3.org/TR/CSS21/tables.html#anonymous-boxes.
+// The anonymous table wrappers are inserted in LayoutNGTable::AddChild,
+// LayoutNGTableSection::AddChild, LayoutNGTableRow::AddChild and
+// LayoutObject::AddChild.
+//
+// Note that this yields to interesting issues in the insertion code. The DOM
+// code is unaware of the anonymous LayoutObjects and thus can insert
+// LayoutObjects into a different part of the layout tree. An example is:
+//
+// <!DOCTYPE html>
+// <style>
+// tablerow { display: table-row; }
+// tablecell { display: table-cell; border: 5px solid purple; }
+// </style>
+// <tablerow id="firstRow">
+//     <tablecell>Short first row.</tablecell>
+// </tablerow>
+// <tablecell id="cell">Long second row, shows the table structure.</tablecell>
+//
+// The page generates a single anonymous table (LayoutNGTable) and table row
+// group (LayoutNGTableSection) to wrap the <tablerow> (#firstRow) and an
+// anonymous table row (LayoutNGTableRow) for the second <tablecell>. It is
+// possible for JavaScript to insert a new element between these 2 <tablecell>
+// (using Node.insertBefore), requiring us to split the anonymous table (or the
+// anonymous table row group) in 2. Also note that even though the second
+// <tablecell> and <tablerow> are siblings in the DOM tree, they are not in the
+// layout tree.
+//
 // Invalidation: LayoutNGTable differences from block invalidation:
 //
 // Cached collapsed borders:
