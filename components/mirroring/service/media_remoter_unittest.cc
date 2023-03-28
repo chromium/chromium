d0bcf50a863c84867842aecf89b5f713290af2a5
Muyao Xu <muyaoxu@google.com>
2023-03-28 21:58:56 +0000

    MediaRemoter should not change its state from REMOTING_DISABLED
    MediaRemoter enters the REMOTING_DISABLED state when it fails to start
    remoting or when it encounters error. Once it's at the REMOTING_DISBALED
    state, it should keep its state unchanged, otherwise it might attempt
    to restart Remoting and fail again.
    
    (cherry picked from commit dd09d9fd143d62b84e0acb6f6eba12ff49c6e098)
    
    Bug: b/266702889
    Change-Id: Ib56b1da99fc3b125923d2747d0ba02adefbd91f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367747
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1122564}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4375111
    Auto-Submit: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#82}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- components/mirroring/service/media_remoter_unittest.cc
+++ components/mirroring/service/media_remoter_unittest.cc
@@ -233,8 +233,11 @@ class MediaRemoterTest : public mojom::CastMessageChannel,
   }
 
   // Signals that mirroring is resumed successfully.
-  void MirroringResumed() {
-    EXPECT_CALL(remoting_source_, OnSinkAvailable(_));
+  void MirroringResumed(bool is_remoting_disabled) {
+    // When MediaRemoter is in the REMOTING_DISABLED state, it should not notify
+    // its remoting_source_ about available sinks.
+    EXPECT_CALL(remoting_source_, OnSinkAvailable(_))
+        .Times(is_remoting_disabled ? 0 : 1);
     media_remoter_->OnMirroringResumed();
     task_environment_.RunUntilIdle();
     Mock::VerifyAndClear(&remoting_source_);
@@ -361,7 +364,7 @@ TEST_P(MediaRemoterTest, StopRemotingWhileStarting) {
   StopRemoting();
 
   // Signals that successfully switch to mirroring.
-  MirroringResumed();
+  MirroringResumed(/* is_remoting_disabled */ false);
   // Now remoting can be started again.
   StartRemoting();
 }
@@ -370,6 +373,8 @@ TEST_P(MediaRemoterTest, RemotingStartFailed) {
   CreateRemoter();
   StartRemoting();
   RemotingStartFailed();
+  StopRemoting();
+  MirroringResumed(/* is_remoting_disabled */ true);
 }
 
 TEST_P(MediaRemoterTest, SwitchBetweenMultipleSessions) {
@@ -386,7 +391,7 @@ TEST_P(MediaRemoterTest, SwitchBetweenMultipleSessions) {
 
   // Stop the remoting session and switch to mirroring.
   StopRemoting();
-  MirroringResumed();
+  MirroringResumed(/* is_remoting_disabled */ false);
 
   // Switch to remoting again.
   StartRemoting();
@@ -396,7 +401,7 @@ TEST_P(MediaRemoterTest, SwitchBetweenMultipleSessions) {
 
   // Switch to mirroring again.
   StopRemoting();
-  MirroringResumed();
+  MirroringResumed(/* is_remoting_disabled */ false);
 }
 
 INSTANTIATE_TEST_SUITE_P(MediaRemoter,
