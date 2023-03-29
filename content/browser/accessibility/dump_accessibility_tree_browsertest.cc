c3fb2d559efe6813ac8f8724e9f976bdeeccde23
thefrog <thefrog@chromium.org>
2023-03-29 19:08:26 +0000

    [Sheriff] Disable flaky test AccessibilityFencedFrameScrollable
    Test failing on builder chrome/ci/linux-chromeos-chrome.
    
    Bug: 1428918
    AX-Relnotes: n/a.
    Change-Id: I233a4eea6c459234d60fbb4659ae1ae4fe717ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381249
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: thefrog <thefrog@chromium.org>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123741}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2058,8 +2058,16 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::ValuesIn(ui::AXInspectTestHelper::TreeTestPasses()),
     DumpAccessibilityTreeTestPassToString());
 
+// TODO(crbug.com/1428918): Re-enable this test.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_AccessibilityFencedFrameScrollable \
+  DISABLED_AccessibilityFencedFrameScrollable
+#else
+#define MAYBE_AccessibilityFencedFrameScrollable \
+  AccessibilityFencedFrameScrollable
+#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeFencedFrameTest,
-                       AccessibilityFencedFrameScrollable) {
+                       MAYBE_AccessibilityFencedFrameScrollable) {
   RunHtmlTest(FILE_PATH_LITERAL("fencedframe-scrollable-mparch.html"));
 }
 
