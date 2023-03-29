728c227686488cdeb1f480ed70211deb246c5c4a
thefrog <thefrog@chromium.org>
2023-03-29 13:39:21 +0000

    Revert "Reland "SearchPre: [4.2]Add final status metrics for response reader""
    This reverts commit 23fe1793c1bdb675f21ead8111d1be9c7d590c37.
    
    Reason for revert: Two tests failing:
     - NoCancelSearchPreloadUnifiedFallbackBrowserTest.OpenPrefetchedResponseInBackgroundedTab
     - SearchPreloadUnifiedFallbackBrowserTest.PrerenderDiscardedBeforeServingData
    
    More details described at crbug.com/1428901.
    
    Original change's description:
    > Reland "SearchPre: [4.2]Add final status metrics for response reader"
    >
    > This is a reland of commit ac79d597e15d092c2c04e5d87e49e21e0698019d
    > Compared to its initial version, this CL compresses several status
    > into one. The reader might end up different statuses given different
    > order of executing Mojo tasks, and cannot guarantee the same status
    > will be recorded every time.
    > In this case, this CL merges the similar ones into kServingError.
    >
    > This CL also fixes a race condition.
    > Since reader is deleted as an asynchronous task, while loader can be
    > deleted synchronously, we cannot guarantee that reader is destroyed
    > before loader, and need to pass a weakptr instead of a raw pointer.
    >
    >
    > Original change's description:
    > > SearchPre: [4.2]Add final status metrics for response reader
    > >
    > > This CL adds a metric to track ResponseReader's status in different
    > > stages.
    > > This CL also changes the order of informing different clients of
    > > URLLoaderCompletionStatus, so then prerender can always receive the
    > > status.
    > >
    > > Bug: 1400881
    > > Change-Id: I92520d53b58416c02b1170690e8115cf55934187
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4344850
    > > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1120502}
    >
    > Bug: 1400881
    > Change-Id: I331e608cf12e2399cd866c9bae1bedc297e1d46f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4364320
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1123491}
    
    Bug: 1400881
    Change-Id: I3b66344845e2b004e8e340723a069ce040857914
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381486
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Owners-Override: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123553}

--- chrome/browser/preloading/prefetch/search_prefetch/streaming_search_prefetch_url_loader.h
+++ chrome/browser/preloading/prefetch/search_prefetch/streaming_search_prefetch_url_loader.h
@@ -39,32 +39,13 @@ class StreamingSearchPrefetchURLLoader : public network::mojom::URLLoader,
   // `StreamingSearchPrefetchURLLoader`'s data cache.
   class ResponseReader : public network::mojom::URLLoader {
    public:
-    // These values are persisted to logs. Entries should not be renumbered and
-    // numeric values should never be reused. Recorded as
-    // SearchPrefetchResponseDataReaderStatus in logs.
-    enum class ResponseDataReaderStatus {
-      kCreated = 0,
-      // This reader failed to push data to its clients. This is usually caused
-      // by the clients refused to receive data after destruction.
-      kServingError = 1,
-      // The loader receives an error from the network and terminates this
-      // reader.
-      kNetworkError = 2,
-      // For a success serving case.
-      kCompleted = 3,
-      // Its owner deletes this instance before this instance encounters any
-      // issues or completes serving.
-      kCanceledByLoader = 4,
-      kMaxValue = kCanceledByLoader,
-    };
-
     ResponseReader(
         mojo::PendingReceiver<network::mojom::URLLoader> forward_receiver,
         mojo::PendingRemote<network::mojom::URLLoaderClient> forwarding_client,
         base::OnceCallback<void(ResponseReader*)>
             forwarding_disconnection_callback,
         absl::optional<network::URLLoaderCompletionStatus>,
-        base::WeakPtr<StreamingSearchPrefetchURLLoader> loader);
+        StreamingSearchPrefetchURLLoader* loader);
     ~ResponseReader() override;
 
     // Not copyable nor movable.
@@ -100,7 +81,6 @@ class StreamingSearchPrefetchURLLoader : public network::mojom::URLLoader,
     // Called by `StreamingSearchPrefetchURLLoader` to inform it of the
     // received response.
     void OnStatusCodeReady(const network::URLLoaderCompletionStatus& status);
-    void OnDestroyed();
 
    private:
     // Checks if all data have be pushed to its consumer and the corresponding
@@ -119,9 +99,6 @@ class StreamingSearchPrefetchURLLoader : public network::mojom::URLLoader,
     //            write_position_         end of response body.
     int write_position_ = 0;
 
-    // Tracking the current status.
-    ResponseDataReaderStatus status_ = ResponseDataReaderStatus::kCreated;
-
     // Data pipe for pushing the received response to the client.
     mojo::ScopedDataPipeProducerHandle producer_handle_;
     std::unique_ptr<mojo::SimpleWatcher> handle_watcher_;
@@ -134,12 +111,11 @@ class StreamingSearchPrefetchURLLoader : public network::mojom::URLLoader,
     // loader to delete itself.
     base::OnceCallback<void(ResponseReader*)> disconnection_callback_;
 
-    // 'loader_' owns `this`. Note that `this` would be deleted asynchronously
-    // and `loader_` can be deleted synchronously, their destruction order is
-    // not guaranteed so here is a weakptr.
+    // 'loader_' owns this, and guarantees that `loader_` won't be deleted
+    // before deleting `this`, so it is safe to keep a ref.
     // TODO(crbug.com/1400881): This breaks the hierarchy. Will be replaced soon
     // after refactoring.
-    base::WeakPtr<StreamingSearchPrefetchURLLoader> loader_;
+    raw_ptr<StreamingSearchPrefetchURLLoader> loader_;
 
     // Records the completion status for the corresponding network loader.
     absl::optional<network::URLLoaderCompletionStatus>
@@ -308,7 +284,7 @@ class StreamingSearchPrefetchURLLoader : public network::mojom::URLLoader,
   // the navigation stack.
   raw_ptr<SearchPrefetchRequest> streaming_prefetch_request_;
 
-  // Whether we are serving from |body_content_|.
+  // Whether we are serving from |bdoy_content_|.
   bool serving_from_data_ = false;
 
   // The status returned from |network_url_loader_|.
