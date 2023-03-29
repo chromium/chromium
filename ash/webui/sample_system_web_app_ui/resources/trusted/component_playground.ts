d1b76ed57cd8cf6dae436518bbb1872325810295
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-03-29 00:44:42 +0000

    cros-components: Add inferred path mappings for lit, cros_components, and mwc
    One of reasons we have ash_ts_library is so that we can inject
    path mappings for lit, cros_components, and mwc. Now that we can
    infer type mappings from deps, we can remove that part of
    ash_ts_library.
    
    This partially unblocks removing ash_ts_library and potentially
    unblocks moving to build_webui() which would unblock source maps.
    
    Bug: b/264070221
    Change-Id: I05c35b589c6b1aa033ae04dd67c5a4ae2f6ea2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355362
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123388}

--- ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
+++ ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
@@ -2,7 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cros_components/button/button.js';
+// Examples of importing lit, cros components, and @material elements.
+import {Button} from 'chrome://resources/cros_components/button/button.js';
+import {MdTextButton} from 'chrome://resources/mwc/@material/web/button/text-button.js';
+import {html} from 'chrome://resources/mwc/lit/index.js';
+
+// Log them to avoid unused import errors.
+console.info(html);
+console.info(Button);
+console.info(MdTextButton);
 
 customElements.whenDefined('cros-button').then(() => {
   document.documentElement.classList.remove('loading');
