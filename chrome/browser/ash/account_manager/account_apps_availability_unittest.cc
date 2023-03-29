346da2f6767f0d8edf655b165d1725fa47c031e8
Tom Sepez <tsepez@chromium.org>
2023-03-29 15:21:16 +0000

    Avoid duplicate allocation in AccountAppsAvailabilityTest::SetUp()
    The local `fake_user_manager` is unused otherwise, and on the next
    line there is a bare `new`. This probably isn't what was intended.
    
    Change-Id: I09d67625ad88f7e672b966bf0704522f67cee21f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4378837
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123598}

--- chrome/browser/ash/account_manager/account_apps_availability_unittest.cc
+++ chrome/browser/ash/account_manager/account_apps_availability_unittest.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ash/account_manager/account_apps_availability.h"
 
 #include <memory>
+#include <utility>
 
 #include "ash/constants/ash_features.h"
 #include "base/test/bind.h"
@@ -92,9 +93,9 @@ class AccountAppsAvailabilityTest : public testing::Test {
     AccountAppsAvailability::RegisterPrefs(pref_service_->registry());
 
     auto fake_user_manager = std::make_unique<user_manager::FakeUserManager>();
-    fake_user_manager_ = new user_manager::FakeUserManager();
+    fake_user_manager_ = fake_user_manager.get();
     scoped_user_manager_ = std::make_unique<user_manager::ScopedUserManager>(
-        base::WrapUnique(fake_user_manager_));
+        std::move(fake_user_manager));
 
     primary_account_ = identity_test_env()->MakePrimaryAccountAvailable(
         kPrimaryAccountEmail, signin::ConsentLevel::kSignin);
