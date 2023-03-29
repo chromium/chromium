8d23901c207cd3283177e8aefb08bd3110c7bb4c
Mohamed Omar <mohamedaomar@google.com>
2023-03-29 17:06:14 +0000

    Added waiter for remote command results
    BUG=b:273435768
    TEST=unit_tests
    
    
    Add request handler for remote command requests
    
    Added request handler for remote commands and
    modified the remote commands state.
    This CL is to support remote commands testing for both browser tests
    and tast tests.
    
    BUG=b:255735015
    TEST=unit_tests
    
    Change-Id: I9ad57acab12f22f2089d7324bec03023aae17c0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4360294
    Commit-Queue: Mohamed Omar <mohamedaomar@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123669}

---
+++ components/policy/test_support/remote_commands_result_waiter.cc
@@ -0,0 +1,54 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/policy/test_support/remote_commands_result_waiter.h"
+
+#include "base/logging.h"
+#include "base/run_loop.h"
+#include "components/policy/proto/device_management_backend.pb.h"
+#include "components/policy/test_support/remote_commands_state.h"
+
+namespace em = enterprise_management;
+
+namespace policy {
+
+RemoteCommandsResultWaiter::RemoteCommandsResultWaiter(
+    RemoteCommandsState* remote_commands_state,
+    int command_id)
+    : remote_commands_state_(remote_commands_state), command_id_(command_id) {
+  remote_commands_state_->AddObserver(this);
+}
+
+RemoteCommandsResultWaiter::~RemoteCommandsResultWaiter() {
+  remote_commands_state_->RemoveObserver(this);
+}
+
+void RemoteCommandsResultWaiter::Wait() {
+  em::RemoteCommandResult result;
+  if (remote_commands_state_->GetRemoteCommandResult(command_id_, &result)) {
+    // No need to wait, result is already available.
+    return;
+  }
+  run_loop_.Run();
+}
+
+em::RemoteCommandResult RemoteCommandsResultWaiter::WaitAndGetResult() {
+  Wait();
+
+  em::RemoteCommandResult result;
+  const bool result_available =
+      remote_commands_state_->GetRemoteCommandResult(command_id_, &result);
+  // The result must be available now that the `run_loop_` has quit.
+  CHECK(result_available);
+  return result;
+}
+
+void RemoteCommandsResultWaiter::OnRemoteCommandResultAvailable(
+    int command_id) {
+  if (command_id_ == command_id) {
+    run_loop_.Quit();
+  }
+}
+
+}  // namespace policy
---
+++ components/policy/test_support/remote_commands_result_waiter.h
@@ -0,0 +1,36 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_POLICY_TEST_SUPPORT_REMOTE_COMMANDS_RESULT_WAITER_H_
+#define COMPONENTS_POLICY_TEST_SUPPORT_REMOTE_COMMANDS_RESULT_WAITER_H_
+
+#include "base/run_loop.h"
+#include "components/policy/proto/device_management_backend.pb.h"
+#include "components/policy/test_support/remote_commands_state.h"
+
+namespace policy {
+
+// Blocks till the remote command result is available.
+class RemoteCommandsResultWaiter : public RemoteCommandsState::Observer {
+ public:
+  RemoteCommandsResultWaiter(RemoteCommandsState* remote_commands_state,
+                             int command_id);
+
+  ~RemoteCommandsResultWaiter() override;
+
+  void Wait();
+
+  enterprise_management::RemoteCommandResult WaitAndGetResult();
+
+ private:
+  void OnRemoteCommandResultAvailable(int command_id) override;
+
+  RemoteCommandsState* const remote_commands_state_;
+  const int command_id_;
+  base::RunLoop run_loop_;
+};
+
+}  // namespace policy
+
+#endif  // COMPONENTS_POLICY_TEST_SUPPORT_REMOTE_COMMANDS_RESULT_WAITER_H_
