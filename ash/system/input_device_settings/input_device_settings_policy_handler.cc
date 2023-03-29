94c584331032d6e9507c6cbed8e72872a9518525
David Padlipsky <dpad@google.com>
2023-03-29 22:26:00 +0000

    Settings Split: Add initialization to policy handler
    Bug: b/241965700
    Change-Id: I8502bdca05e56517ca6ccf3fe8848574469d1ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4363163
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123892}

--- ash/system/input_device_settings/input_device_settings_policy_handler.cc
+++ ash/system/input_device_settings/input_device_settings_policy_handler.cc
@@ -4,9 +4,38 @@
 
 #include "ash/system/input_device_settings/input_device_settings_policy_handler.h"
 
+#include "ash/constants/ash_pref_names.h"
+#include "components/prefs/pref_change_registrar.h"
+#include "components/prefs/pref_service.h"
+
 namespace ash {
 
 InputDeviceSettingsPolicyHandler::InputDeviceSettingsPolicyHandler() = default;
 InputDeviceSettingsPolicyHandler::~InputDeviceSettingsPolicyHandler() = default;
+
+void InputDeviceSettingsPolicyHandler::Initialize(PrefService* pref_service) {
+  pref_change_registrar_.Init(pref_service);
+
+  pref_change_registrar_.Add(
+      prefs::kPrimaryMouseButtonRight,
+      base::BindRepeating(
+          &InputDeviceSettingsPolicyHandler::OnMousePoliciesChanged,
+          base::Unretained(this)));
+  pref_change_registrar_.Add(
+      prefs::kSendFunctionKeys,
+      base::BindRepeating(
+          &InputDeviceSettingsPolicyHandler::OnKeyboardPoliciesChanged,
+          base::Unretained(this)));
+}
+
+void InputDeviceSettingsPolicyHandler::OnKeyboardPoliciesChanged(
+    const std::string& pref_name) {
+  // TODO(dpad): Implement retrieval of policy status.
+}
+
+void InputDeviceSettingsPolicyHandler::OnMousePoliciesChanged(
+    const std::string& pref_name) {
+  // TODO(dpad): Implement retrieval of policy status.
+}
 
 }  // namespace ash
--- ash/system/input_device_settings/input_device_settings_policy_handler.h
+++ ash/system/input_device_settings/input_device_settings_policy_handler.h
@@ -6,6 +6,9 @@
 #define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_INPUT_DEVICE_SETTINGS_POLICY_HANDLER_H_
 
 #include "ash/ash_export.h"
+#include "components/prefs/pref_change_registrar.h"
+
+class PrefService;
 
 namespace ash {
 
@@ -17,6 +20,15 @@ class ASH_EXPORT InputDeviceSettingsPolicyHandler {
   InputDeviceSettingsPolicyHandler& operator=(
       const InputDeviceSettingsPolicyHandler&) = delete;
   ~InputDeviceSettingsPolicyHandler();
+
+  void Initialize(PrefService* pref_service);
+
+ private:
+  void OnKeyboardPoliciesChanged(const std::string& pref_name);
+  void OnMousePoliciesChanged(const std::string& pref_name);
+
+  // Used to track preferences which may be controlled by enterprise policies.
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 }  // namespace ash
