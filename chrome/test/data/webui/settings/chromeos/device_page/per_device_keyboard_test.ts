62f6d2e026d77ed740d98673103dd7839ed129a3
Ilkin Safarli <safarli@google.com>
2023-03-29 04:07:58 +0000

    CrOS Settings: Convert per_device_keyboard browser test to TS
    Bug: b:270728282
    Test: browser_tests --gtest_filter="OSSettingsDevicePagePerDeviceKeyboardV3*"
    Change-Id: Iaa4ce022aaddb41bb15153e71991e3358415f1db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4370633
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ilkin Safarli <safarli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123444}

---
+++ chrome/test/data/webui/settings/chromeos/device_page/per_device_keyboard_test.ts
@@ -0,0 +1,187 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {DevicePageBrowserProxyImpl, fakeKeyboards, fakeKeyboards2, Router, routes, SettingsPerDeviceKeyboardElement, SettingsSliderElement} from 'chrome://os-settings/chromeos/os_settings.js';
+import {CrLinkRowElement} from 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import {assert} from 'chrome://resources/js/assert_ts.js';
+import {pressAndReleaseKeyOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {isVisible} from 'chrome://webui-test/test_util.js';
+
+import {TestDevicePageBrowserProxy} from './test_device_page_browser_proxy.js';
+
+suite('<settings-per-device-keyboard>', () => {
+  let perDeviceKeyboardPage: SettingsPerDeviceKeyboardElement;
+  let devicePageBrowserProxy: TestDevicePageBrowserProxy;
+
+  function getFakeAutoRepeatPrefs() {
+    return {
+      settings: {
+        language: {
+          xkb_auto_repeat_enabled_r2: {
+            key: 'prefs.settings.language.xkb_auto_repeat_enabled_r2',
+            type: chrome.settingsPrivate.PrefType.BOOLEAN,
+            value: true,
+          },
+          xkb_auto_repeat_delay_r2: {
+            key: 'settings.language.xkb_auto_repeat_delay_r2',
+            type: chrome.settingsPrivate.PrefType.NUMBER,
+            value: 500,
+          },
+          xkb_auto_repeat_interval_r2: {
+            key: 'settings.language.xkb_auto_repeat_interval_r2',
+            type: chrome.settingsPrivate.PrefType.NUMBER,
+            value: 500,
+          },
+        },
+      },
+    };
+  }
+
+  setup(async () => {
+    devicePageBrowserProxy = new TestDevicePageBrowserProxy();
+    DevicePageBrowserProxyImpl.setInstanceForTesting(devicePageBrowserProxy);
+    perDeviceKeyboardPage =
+        document.createElement('settings-per-device-keyboard');
+    assert(perDeviceKeyboardPage);
+    perDeviceKeyboardPage.set('prefs', getFakeAutoRepeatPrefs());
+    perDeviceKeyboardPage.set('keyboards', fakeKeyboards);
+    document.body.appendChild(perDeviceKeyboardPage);
+    await flushTasks();
+  });
+
+  teardown(() => {
+    perDeviceKeyboardPage.remove();
+    Router.getInstance().resetRouteForTesting();
+  });
+
+  test('Keyboard page updates with new keyboards', async () => {
+    let subsections = perDeviceKeyboardPage.shadowRoot!.querySelectorAll(
+        'settings-per-device-keyboard-subsection');
+    assertEquals(fakeKeyboards.length, subsections.length);
+
+    // Check the number of subsections when the keyboard list is updated.
+    perDeviceKeyboardPage.set('keyboards', fakeKeyboards2);
+    await flushTasks();
+    subsections = perDeviceKeyboardPage.shadowRoot!.querySelectorAll(
+        'settings-per-device-keyboard-subsection');
+    assertEquals(fakeKeyboards2.length, subsections.length);
+  });
+
+  test(
+      'Display correct name used for internal/external keyboards', async () => {
+        const subsections = perDeviceKeyboardPage.shadowRoot!.querySelectorAll(
+            'settings-per-device-keyboard-subsection');
+        for (let i = 0; i < subsections.length; i++) {
+          const name =
+              subsections[i]!.shadowRoot!.querySelector('h2')!.textContent;
+          if (fakeKeyboards[i]!.isExternal) {
+            assertEquals(fakeKeyboards[i]!.name, name);
+          } else {
+            assertTrue(subsections[i]!.i18nExists('builtInKeyboardName'));
+            assertEquals('Built-in Keyboard', name);
+          }
+        }
+      });
+
+  test('Auto repeat toggle and slides', async () => {
+    const collapse =
+        perDeviceKeyboardPage.shadowRoot!.querySelector('iron-collapse');
+    assert(collapse);
+    assertTrue(collapse.opened);
+
+    assertEquals(
+        500,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>('#delaySlider')!.pref!.value);
+    assertEquals(
+        500,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>(
+                '#repeatRateSlider')!.pref!.value);
+
+    // Test interaction with the settings-slider's underlying cr-slider.
+    pressAndReleaseKeyOn(
+        perDeviceKeyboardPage.shadowRoot!.querySelector('#delaySlider')!
+            .shadowRoot!.querySelector('cr-slider')!,
+        37, [], 'ArrowLeft');
+    pressAndReleaseKeyOn(
+        perDeviceKeyboardPage.shadowRoot!.querySelector('#repeatRateSlider')!
+            .shadowRoot!.querySelector('cr-slider')!,
+        39, [], 'ArrowRight');
+    await flushTasks();
+    assertEquals(
+        1000,
+        perDeviceKeyboardPage.get(
+            'prefs.settings.language.xkb_auto_repeat_delay_r2.value'));
+    assertEquals(
+        300,
+        perDeviceKeyboardPage.get(
+            'prefs.settings.language.xkb_auto_repeat_interval_r2.value'));
+
+    // Test sliders change when prefs change.
+    perDeviceKeyboardPage.set(
+        'prefs.settings.language.xkb_auto_repeat_delay_r2.value', 1500);
+    await flushTasks();
+    assertEquals(
+        1500,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>('#delaySlider')!.pref!.value);
+    perDeviceKeyboardPage.set(
+        'prefs.settings.language.xkb_auto_repeat_interval_r2.value', 2000);
+    await flushTasks();
+    assertEquals(
+        2000,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>(
+                '#repeatRateSlider')!.pref!.value);
+
+    // Test sliders round to nearest value when prefs change.
+    perDeviceKeyboardPage.set(
+        'prefs.settings.language.xkb_auto_repeat_delay_r2.value', 600);
+    await flushTasks();
+    assertEquals(
+        500,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>('#delaySlider')!.pref!.value);
+    perDeviceKeyboardPage.set(
+        'prefs.settings.language.xkb_auto_repeat_interval_r2.value', 45);
+    await flushTasks();
+    assertEquals(
+        50,
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<SettingsSliderElement>(
+                '#repeatRateSlider')!.pref!.value);
+
+    perDeviceKeyboardPage.set(
+        'prefs.settings.language.xkb_auto_repeat_enabled_r2.value', false);
+    assertFalse(collapse.opened);
+  });
+
+  test('Open keyboard shortcut viewer', async () => {
+    perDeviceKeyboardPage.shadowRoot!
+        .querySelector<CrLinkRowElement>('#keyboardShortcutViewer')!.click();
+    assertEquals(
+        1, devicePageBrowserProxy.getCallCount('showKeyboardShortcutViewer'));
+  });
+
+  test('Navigate to input tab', async () => {
+    perDeviceKeyboardPage.shadowRoot!
+        .querySelector<CrLinkRowElement>('#showLanguagesInput')!.click();
+    assertEquals(routes.OS_LANGUAGES_INPUT, Router.getInstance().currentRoute);
+  });
+
+  test('Help message shown when no keyboards are connected', async () => {
+    perDeviceKeyboardPage.set('keyboards', []);
+    await flushTasks();
+    assertTrue(isVisible(perDeviceKeyboardPage.shadowRoot!.querySelector(
+        '#noKeyboardsConnectedContainer')));
+    assertEquals(
+        'No keyboard detected',
+        perDeviceKeyboardPage.shadowRoot!
+            .querySelector<HTMLElement>(
+                '#noKeyboardsConnectedMessage')!.innerText.trim());
+  });
+});
