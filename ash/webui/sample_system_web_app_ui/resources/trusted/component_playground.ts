9446dc4850bddf03181efad77b72ca89d4c8e304
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-03-29 01:09:46 +0000

    Revert "cros-components: Add inferred path mappings for lit, cros_components, and mwc"
    This reverts commit d1b76ed57cd8cf6dae436518bbb1872325810295.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8785332456141021665
    
    Sample failed build: https://ci.chromium.org/b/8785332456141021665
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8785332456141021665&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4355362
    
    Original change's description:
    > cros-components: Add inferred path mappings for lit, cros_components, and mwc
    >
    > One of reasons we have ash_ts_library is so that we can inject
    > path mappings for lit, cros_components, and mwc. Now that we can
    > infer type mappings from deps, we can remove that part of
    > ash_ts_library.
    >
    > This partially unblocks removing ash_ts_library and potentially
    > unblocks moving to build_webui() which would unblock source maps.
    >
    > Bug: b/264070221
    > Change-Id: I05c35b589c6b1aa033ae04dd67c5a4ae2f6ea2ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4355362
    > Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: Zain Afzal <zafzal@google.com>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1123388}
    >
    
    Bug: b/264070221
    Change-Id: Ib754fbaab62c40121a31c83d94589a91852440e4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379899
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1123394}

--- ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
+++ ash/webui/sample_system_web_app_ui/resources/trusted/component_playground.ts
@@ -2,15 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Examples of importing lit, cros components, and @material elements.
-import {Button} from 'chrome://resources/cros_components/button/button.js';
-import {MdTextButton} from 'chrome://resources/mwc/@material/web/button/text-button.js';
-import {html} from 'chrome://resources/mwc/lit/index.js';
-
-// Log them to avoid unused import errors.
-console.info(html);
-console.info(Button);
-console.info(MdTextButton);
+import 'chrome://resources/cros_components/button/button.js';
 
 customElements.whenDefined('cros-button').then(() => {
   document.documentElement.classList.remove('loading');
--- tools/typescript/path_mappings.py
+++ tools/typescript/path_mappings.py
@@ -36,34 +36,29 @@ def _add_ui_webui_resources_mappings(path_mappings, root_gen_dir):
 
 
 def _add_third_party_polymer_mappings(path_mappings, root_src_dir):
+  # Use path.join() below, since root_src_dir can end with trailing slashes.
   path_mappings[f'//third_party/polymer/v3_0:library'] = [
-      ('//resources/polymer/v3_0/polymer/polymer_bundled.min.js',
-       f'{root_src_dir}/third_party/polymer/v3_0/components-chromium/polymer/polymer.d.ts'
-       ),
-      ('//resources/polymer/v3_0/*',
-       f'{root_src_dir}/third_party/polymer/v3_0/components-chromium/*')
+      (
+          f'//resources/polymer/v3_0/polymer/polymer_bundled.min.js',
+          path.join(
+              root_src_dir,
+              'third_party/polymer/v3_0/components-chromium/polymer/polymer.d.ts'
+          ),
+      ),
+      (
+          f'//resources/polymer/v3_0/*',
+          path.join(root_src_dir,
+                    'third_party/polymer/v3_0/components-chromium/*'),
+      ),
   ]
 
 
 # Ash-only
-def _add_ash_mappings(path_mappings, root_gen_dir, root_src_dir):
+def _add_ash_webui_resources_mappings(path_mappings, root_gen_dir):
   path_mappings['//ash/webui/common/resources:build_ts'] = [(
       '//resources/ash/common/*',
       f'{root_gen_dir}/ash/webui/common/resources/preprocessed/*',
   )]
-  path_mappings['//third_party/cros-components:cros_components_ts'] = [(
-      '//resources/cros_components/*',
-      f'{root_gen_dir}/ui/webui/resources/tsc/cros_components/*',
-  )]
-  path_mappings['//third_party/material_web_components:library'] = [(
-      '//resources/mwc/@material/*',
-      f'{root_src_dir}/third_party/material_web_components/components-chromium/'
-      'node_modules/@material/*',
-  )]
-  path_mappings['//third_party/material_web_components:bundle_lit_ts'] = [
-      ('//resources/mwc/lit/index.js',
-       f'{root_src_dir}/third_party/material_web_components/lit_exports.d.ts')
-  ]
 
 
 def GetDepToPathMappings(root_gen_dir, root_src_dir, platform):
@@ -73,6 +68,6 @@ def GetDepToPathMappings(root_gen_dir, root_src_dir, platform):
   _add_third_party_polymer_mappings(path_mappings, root_src_dir)
 
   if platform == 'chromeos_ash':
-    _add_ash_mappings(path_mappings, root_gen_dir, root_src_dir)
+    _add_ash_webui_resources_mappings(path_mappings, root_gen_dir)
 
   return path_mappings
