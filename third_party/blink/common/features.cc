bfe804da1a0f79d803a032dd53f7c55f240743ac
Tove Petersson <tovep@chromium.org>
2023-03-29 08:25:31 +0000

    Enabled SplitUserMediaQueues by default + remove field trials
    Analysis of metrics: https://crbug.com/1373032#c9
    
    Bug: 1373032
    Change-Id: I386dd33ded7044f5c8ea7c2d947e895235f3041d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373707
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1123492}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1647,7 +1647,7 @@ BASE_FEATURE(kForceHighPerformanceGPUForWebGL,
 
 BASE_FEATURE(kSplitUserMediaQueues,
              "SplitUserMediaQueues",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTextCodecCJKEnabled,
              "TextCodecCJKEnabled",
