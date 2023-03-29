d793e55b8f5cd81e632c4fa87b1eb532363663b5
Florian Leimgruber <fleimgruber@google.com>
2023-03-29 15:06:30 +0000

    Annotate AlternativeStateNameMap member variables with GUARDED_BY().
    As suggested by this comment: https://chromium-review.googlesource.com/c/chromium/src/+/4352658/comment/730e18e0_bb676cd4/
    
    Bug: 1425951
    Change-Id: Ic158a4a9069e96d2d7496bb5d2f91b866f1acd61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4377219
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1123589}

--- components/autofill/core/browser/geo/alternative_state_name_map.h
+++ components/autofill/core/browser/geo/alternative_state_name_map.h
@@ -10,6 +10,7 @@
 #include "base/i18n/case_conversion.h"
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
+#include "base/thread_annotations.h"
 #include "base/types/strong_alias.h"
 #include "components/autofill/core/browser/proto/states.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -163,22 +164,22 @@ class AlternativeStateNameMap {
   // allowed to construct the class.
   friend class base::NoDestructor<AlternativeStateNameMap>;
 
+  // TODO(crbug.com/1425951): Remove lock.
+  mutable base::Lock lock_;
+
   // A map that stores the alternative state names. The map is keyed
   // by the country_code and the canonical state name (or
   // normalized_state_value_from_profile in case no canonical state name is
   // known) while the value is the StateEntry object.
   std::map<std::pair<CountryCode, CanonicalStateName>, StateEntry>
-      localized_state_names_map_;
+      localized_state_names_map_ GUARDED_BY(lock_);
 
   // The map is keyed by the country_code and the abbreviation or
   // canonical name or the alternative name of the state.
   std::map<std::pair<CountryCode, StateName>,
            CanonicalStateName,
            CaseInsensitiveLessComparator>
-      localized_state_names_reverse_lookup_map_;
-
-  // TODO(crbug.com/1425951): Remove lock.
-  mutable base::Lock lock_;
+      localized_state_names_reverse_lookup_map_ GUARDED_BY(lock_);
 };
 
 }  // namespace autofill
