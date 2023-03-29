0fea13c9c895f4783ba04ac17c888cad6d4f751c
Henrique Nakashima <hnakashima@chromium.org>
2023-03-29 20:50:15 +0000

    [Android] Re-enable two TabModelImplTest test cases
    - #validIndexAfterRestored_FromPreviousActivity_WithIncognitoTabs
    - #hasOtherRelatedTabs_detectMergedTabs
    
    They look clean on Test Reviver and locally, failures are from bots
    that don't exist anymore.
    
    Bug: 1331169,1331424,1182156,1190854
    
    Change-Id: If770476c9f0147cee1780b32f10b4b3874754381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382845
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1123827}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
@@ -22,7 +22,6 @@ import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -133,7 +132,6 @@ public class TabModelImplTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1182156")
     public void validIndexAfterRestored_FromPreviousActivity_WithIncognitoTabs() {
         createTabs(1, true, mTestUrl);
 
@@ -153,7 +151,6 @@ public class TabModelImplTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID})
-    @DisabledTest(message = "https://crbug.com/1190854")
     public void hasOtherRelatedTabs_detectMergedTabs() throws Exception {
         createTabs(3, false, mTestUrl);
 
