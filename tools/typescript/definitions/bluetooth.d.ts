421e1d3efcc1dd8d26e62adca3ce163b7c64edd4
Josiah Krutz <josiahk@google.com>
2023-03-30 19:25:09 +0000

    Add bluetooth and bluetooth_private typescript definitions
    Update bluetooth and bluetooth_private typescript definitions, to use in the new ChromeVox settings page in ChromeOS Settings.
    
    Fixed: b/274982284
    AX-Relnotes: N/A.
    Change-Id: I3149afcc6666361bd4475b40129a36e826300090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4368859
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124356}

--- tools/typescript/definitions/bluetooth.d.ts
+++ tools/typescript/definitions/bluetooth.d.ts
@@ -4,6 +4,66 @@
 
 /** @fileoverview Type definitions for chrome.bluetooth API. */
 
-declare namespace chrome.bluetooth {
-  export enum Transport {}
+import {ChromeEvent} from './chrome_event.js';
+
+declare global {
+  export namespace chrome {
+    export namespace bluetooth {
+      export enum VendorIdSource {
+        BLUETOOTH = 'bluetooth',
+        USB = 'usb',
+      }
+
+      export enum DeviceType {
+        COMPUTER = 'computer',
+        PHONE = 'phone',
+        MODEM = 'modem',
+        AUDIO = 'audio',
+        CAR_AUDIO = 'carAudio',
+        VIDEO = 'video',
+        PERIPHERAL = 'peripheral',
+        JOYSTICK = 'joystick',
+        GAMEPAD = 'gamepad',
+        KEYBOARD = 'keyboard',
+        MOUSE = 'mouse',
+        TABLET = 'tablet',
+        KEYBOARD_MOUSE_COMBO = 'keyboardMouseCombo',
+      }
+
+      export enum Transport {
+        INVALID = 'invalid',
+        CLASSIC = 'classic',
+        LE = 'le',
+        DUAL = 'dual',
+      }
+
+      export interface Device {
+        address: string;
+        name?: string;
+        deviceClass?: number;
+        vendorIdSource?: VendorIdSource;
+        vendorId?: number;
+        productId?: number;
+        deviceId?: number;
+        type?: DeviceType;
+        paired?: boolean;
+        connected?: boolean;
+        connecting?: boolean;
+        connectable?: boolean;
+        uuids?: string[];
+        inquiryRssi?: number;
+        inquiryTxPower?: number;
+        transport?: Transport;
+        batteryPercentage?: number;
+      }
+
+      export function getDevices(): Promise<Device[]>;
+      export function startDiscovery(): Promise<void>;
+      export function stopDiscovery(): Promise<void>;
+
+      export const onDeviceAdded: ChromeEvent<(device: Device) => void>;
+      export const onDeviceChanged: ChromeEvent<(device: Device) => void>;
+      export const onDeviceRemoved: ChromeEvent<(device: Device) => void>;
+    }
+  }
 }
