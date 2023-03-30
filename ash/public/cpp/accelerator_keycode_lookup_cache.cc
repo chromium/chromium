ac3bddacdac905d61dfadf720e7d6b0f8cadedd4
Jimmy <jimmyxgong@chromium.org>
2023-03-30 17:26:11 +0000

    [M113 merge] shortcuts: Cache Keycode to DomKey string conversions
    Improves performance of looking up the Domkey string from a
    Keycode. Previously, it would perform an inefficient O(N) lookup
    for every keycode. This change adds a cache so that lookups are
    amortized down closer to constant lookup time.
    
    Since the cache is IME-dependent, anytime the IME changes the cache
    is cleared and will have to be rebuilt.
    
    This also has the side effect of reducing the spammy logs from
    DomCode -> DomKey lookups.
    
    (cherry picked from commit 517a61e3ab61d480777aa33942f30461f859951f)
    
    Test: ash_unittests
    Bug: b:271095602 b:216049298
    Change-Id: I19d90ce66c76c11da78019e4f66a65037706be72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4299984
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1121820}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4382245
    Cr-Commit-Position: refs/branch-heads/5672@{#146}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

---
+++ ash/public/cpp/accelerator_keycode_lookup_cache.cc
@@ -0,0 +1,65 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/accelerator_keycode_lookup_cache.h"
+
+#include <string>
+
+#include "base/no_destructor.h"
+#include "ui/base/ime/ash/input_method_manager.h"
+#include "ui/events/keycodes/keyboard_codes_posix.h"
+
+namespace ash {
+namespace {
+
+AcceleratorKeycodeLookupCache* g_instance = nullptr;
+
+}  // namespace
+
+AcceleratorKeycodeLookupCache* AcceleratorKeycodeLookupCache::Get() {
+  return g_instance;
+}
+
+AcceleratorKeycodeLookupCache::AcceleratorKeycodeLookupCache() {
+  CHECK(!g_instance);
+  g_instance = this;
+
+  DCHECK(input_method::InputMethodManager::Get());
+  input_method::InputMethodManager::Get()->AddObserver(this);
+}
+
+AcceleratorKeycodeLookupCache::~AcceleratorKeycodeLookupCache() {
+  CHECK(input_method::InputMethodManager::Get());
+  input_method::InputMethodManager::Get()->RemoveObserver(this);
+
+  CHECK_EQ(g_instance, this);
+  g_instance = nullptr;
+}
+
+void AcceleratorKeycodeLookupCache::InputMethodChanged(
+    input_method::InputMethodManager* manager,
+    Profile* profile,
+    bool show_message) {
+  key_code_to_string16_cache_.clear();
+}
+
+absl::optional<std::u16string> AcceleratorKeycodeLookupCache::Find(
+    ui::KeyboardCode key_code) {
+  const auto& found_iter = key_code_to_string16_cache_.find(key_code);
+  if (found_iter == key_code_to_string16_cache_.end()) {
+    return absl::nullopt;
+  }
+  return found_iter->second;
+}
+
+void AcceleratorKeycodeLookupCache::InsertOrAssign(ui::KeyboardCode key_code,
+                                                   std::u16string description) {
+  key_code_to_string16_cache_.insert_or_assign(key_code, description);
+}
+
+void AcceleratorKeycodeLookupCache::Clear() {
+  key_code_to_string16_cache_.clear();
+}
+
+}  // namespace ash
---
+++ ash/public/cpp/accelerator_keycode_lookup_cache.h
@@ -0,0 +1,55 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PUBLIC_CPP_ACCELERATOR_KEYCODE_LOOKUP_CACHE_H_
+#define ASH_PUBLIC_CPP_ACCELERATOR_KEYCODE_LOOKUP_CACHE_H_
+
+#include <map>
+#include <string>
+
+#include "ash/public/cpp/ash_public_export.h"
+#include "ui/base/ime/ash/input_method_manager.h"
+#include "ui/events/keycodes/keyboard_codes_posix.h"
+
+namespace ash {
+
+// Thin class that observe IME change events and updates the static cache for
+// more efficient lookups for `KeycodeToKeyString()`.
+class ASH_PUBLIC_EXPORT AcceleratorKeycodeLookupCache
+    : public input_method::InputMethodManager::Observer {
+ public:
+  // Get the singleton instance.
+  static AcceleratorKeycodeLookupCache* Get();
+
+  AcceleratorKeycodeLookupCache();
+  AcceleratorKeycodeLookupCache(const AcceleratorKeycodeLookupCache&) = delete;
+  AcceleratorKeycodeLookupCache& operator=(
+      const AcceleratorKeycodeLookupCache&) = delete;
+  ~AcceleratorKeycodeLookupCache() override;
+
+  // InputMethodManager::Observer
+  // The cache is invalid when the IME changes, so this observer notifies us
+  // when to clear the cache.
+  void InputMethodChanged(input_method::InputMethodManager* manager,
+                          Profile* profile,
+                          bool show_message) override;
+
+  absl::optional<std::u16string> Find(ui::KeyboardCode key_code);
+
+  void InsertOrAssign(ui::KeyboardCode key_code, std::u16string description);
+
+  void Clear();
+
+ private:
+  friend class AcceleratorKeycodeLookupCacheTest;
+
+  // A cache of Keycode to string16 so that we don't have to perform a
+  // reverse lookup for the DomKey for every accelerator.
+  // On IME changes, this cache resets.
+  std::map<ui::KeyboardCode, std::u16string> key_code_to_string16_cache_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_PUBLIC_CPP_ACCELERATOR_KEYCODE_LOOKUP_CACHE_H_
