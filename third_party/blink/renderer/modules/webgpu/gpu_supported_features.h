3fab2be3979ed33000e82e3bef38d60f5869d6b4
Piotr Tworek <ptworek@vewd.com>
2023-03-31 10:29:23 +0000

    IWYU: Add bitset in gpu_supported_features.h.
    The usage of std::bitset was added recently in
    129d91c8400f1205f92a10442596b0cd19f859f1 "Add GPUSupportedFeatures::has()
    fast path". Unfortunately the patch failed to include header
    declaring std::bitset. This results in build failures when using
    libstdc++ instead of chromium bundled libc++.
    
    Bug: 957519
    Change-Id: I7ee27dc8aa48da8ae63154868b1148448220aad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379221
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1124651}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_features.h
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_features.h
@@ -5,6 +5,8 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_SUPPORTED_FEATURES_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_SUPPORTED_FEATURES_H_
 
+#include <bitset>
+
 #include "third_party/blink/renderer/bindings/core/v8/iterable.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_feature_name.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_sync_iterator_gpu_supported_features.h"
