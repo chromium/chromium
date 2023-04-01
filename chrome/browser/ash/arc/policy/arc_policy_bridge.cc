eca98c6e06fcbdd16af9b6cd3aea4b1f2cb0fcd4
Muhammad Hasan Khan <mhasank@google.com>
2023-04-01 01:25:55 +0000

    arc: Convert app availability from OPTIONAL to AVAILABLE
    OPTIONAL and AVAILABLE are synonymous. In this change we're updating the
    policy_bridge to convert OPTIONAL to AVAILABLE so that DPC will only
    deal with one type.
    
    BUG=b:244605914
    TEST=run_unit_tests --gtest_filter='ArcPolicyBridgeTest*'
    TEST=deploy to DUT and run arc.Unicorn* tests
    
    Change-Id: I5b41a9f8d507078a846ce8332b9dc6db5f46f97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4390603
    Auto-Submit: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Yao Li <yaohuali@google.com>
    Commit-Queue: Yao Li <yaohuali@google.com>
    Cr-Commit-Position: refs/heads/main@{#1124995}

--- chrome/browser/ash/arc/policy/arc_policy_bridge.cc
+++ chrome/browser/ash/arc/policy/arc_policy_bridge.cc
@@ -54,6 +54,8 @@ constexpr char kPrivateKeySelectionEnabled[] = "privateKeySelectionEnabled";
 constexpr char kChoosePrivateKeyRules[] = "choosePrivateKeyRules";
 constexpr char kPolicyAppInstallType[] = "installType";
 constexpr char kPolicyAppInstallTypeForceInstalled[] = "FORCE_INSTALLED";
+constexpr char kPolicyAppInstallTypeAvailable[] = "AVAILABLE";
+constexpr char kPolicyAppInstallTypeOptional[] = "OPTIONAL";
 
 // invert_bool_value: If the Chrome policy and the ARC policy with boolean value
 // have opposite semantics, set this to true so the bool is inverted before
@@ -347,6 +349,28 @@ void MapChromeToArcPolicies(base::Value::Dict& filtered_policies,
                 /* invert_bool_value */ true, &filtered_policies);
 }
 
+static void ConvertDeprecatedAppInstallTypes(
+    base::Value::Dict& filtered_policies) {
+  base::Value::List* apps =
+      filtered_policies.FindList(policy_util::kArcPolicyKeyApplications);
+  if (apps == nullptr) {
+    return;
+  }
+
+  for (base::Value& app : *apps) {
+    if (!app.is_dict()) {
+      NOTREACHED() << "App object is not a dict.";
+      continue;
+    }
+
+    base::Value::Dict& dict = app.GetDict();
+    if (*dict.FindString(kPolicyAppInstallType) ==
+        kPolicyAppInstallTypeOptional) {
+      dict.Set(kPolicyAppInstallType, kPolicyAppInstallTypeAvailable);
+    }
+  }
+}
+
 void OverrideArcPolicies(base::Value::Dict& filtered_policies,
                          const policy::PolicyMap& policy_map,
                          const std::string& guid,
@@ -409,6 +433,7 @@ base::Value::Dict GetFilteredDictPolicies(
 
   OverrideArcPolicies(filtered_policies, policy_map, guid, is_affiliated,
                       profile);
+  ConvertDeprecatedAppInstallTypes(filtered_policies);
   return filtered_policies;
 }
 
