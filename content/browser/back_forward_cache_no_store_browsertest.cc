b6745f8b654dccaeb2ede5ae1b802eb0eb004fd6
Mingyu Lei <leimy@chromium.org>
2023-04-03 05:28:37 +0000

    Add browser test for the immediate back navigation case which will cause crash for now
    Bug: 1429151
    Change-Id: I477f4c6e898a179fef5e0db15d488d4d8f1c895f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4393244
    Commit-Queue: Mingyu Lei <leimy@google.com>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125192}

--- content/browser/back_forward_cache_no_store_browsertest.cc
+++ content/browser/back_forward_cache_no_store_browsertest.cc
@@ -1376,6 +1376,46 @@ IN_PROC_BROWSER_TEST_F(
                   BlockListedFeatures()));
 }
 
+// Test that a page with `Cache-control: no-store` header gets evicted without
+// crashes if some cookie is modified immediately before the back navigation.
+// TODO(crbug.com/1429151): Re-enable after fixing the race condition.
+IN_PROC_BROWSER_TEST_F(
+    BackForwardCacheBrowserTestRestoreCacheControlNoStoreUnlessCookieChange,
+    DISABLED_PagesWithCacheControlNoStoreNotBFCachedWithCookieSetImmediatelyBeforeNavigateBack) {
+  CreateHttpsServer();
+  net::test_server::ControllableHttpResponse response(https_server(),
+                                                      "/title1.html");
+  ASSERT_TRUE(https_server()->Start());
+
+  GURL url_a(https_server()->GetURL("a.com", "/title1.html"));
+  GURL url_a_2(https_server()->GetURL("a.com", "/title2.html"));
+  GURL url_b(https_server()->GetURL("b.com", "/title1.html"));
+
+  // 1) Load the document and specify no-store for the main resource.
+  TestNavigationObserver observer(web_contents());
+  shell()->LoadURL(url_a);
+  RenderFrameHostImplWrapper rfh_a(current_frame_host());
+  response.WaitForRequest();
+  response.Send(kResponseWithNoCache);
+  response.Done();
+  observer.Wait();
+  rfh_a->GetBackForwardCacheMetrics()->SetObserverForTesting(this);
+
+  // 2) Navigate away, and set a cookie from the new page.
+  EXPECT_TRUE(NavigateToURL(shell(), url_a_2));
+  EXPECT_TRUE(ExecJs(shell(), "document.cookie='foo=bar'"));
+
+  // 3) Go back. |rfh_a| should be evicted upon restoration.
+  ASSERT_TRUE(HistoryGoBack(web_contents()));
+  ExpectNotRestored({NotRestoredReason::kCacheControlNoStoreCookieModified}, {},
+                    {}, {}, {}, FROM_HERE);
+  EXPECT_THAT(GetTreeResult()->GetDocumentResult(),
+              MatchesDocumentResult(
+                  NotRestoredReasons(
+                      NotRestoredReason::kCacheControlNoStoreCookieModified),
+                  BlockListedFeatures()));
+}
+
 class BackForwardCacheBrowserTestRestoreUnlessHTTPOnlyCookieChange
     : public BackForwardCacheBrowserTest {
  protected:
