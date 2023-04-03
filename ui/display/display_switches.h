1eb23e52ae01c2d0e33bbd231728b021b5145499
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-04-03 23:30:03 +0000

    [Rounded-display] Adds `display-properties` switch.
    Rounded display panel radii is specified through a commandline switch.
    This CL add the switch name as a constant.
    
    We will parse the radii information in a followup CL.
    
    Bug: 1349873
    Change-Id: I2141a67c3063728bd349120366b6513ded873d78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4337963
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125692}

--- ui/display/display_switches.h
+++ ui/display/display_switches.h
@@ -25,6 +25,7 @@ DISPLAY_EXPORT extern const char kSecondaryDisplayLayout[];
 DISPLAY_EXPORT extern const char kUseFirstDisplayAsInternal[];
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+DISPLAY_EXPORT extern const char kDisplayProperties[];
 DISPLAY_EXPORT extern const char kEnableUnifiedDesktop[];
 
 // TODO(oshima): Remove once all tests are fixed. crbug.com/1225308.
