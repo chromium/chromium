f744f8804af3a087c4dd39747926928f2c704f7f
Colin Kincaid <ckincaid@chromium.org>
2023-04-03 18:47:32 +0000

    multipaste: Update typography for Jelly
    This CL updates clipboard history menu typography and text color when
    Jelly is enabled. When Jelly is not enabled, behavior is unchanged.
    
    Spec for the change: http://shortn/_Nmb5GuZ0KX
    
    Dark mode:
    Menu w/o Jelly: https://screenshot.googleplex.com/52fKWbB7NC82moH
    Menu w/ Jelly: https://screenshot.googleplex.com/9866tcxMdfALFkP
    
    Light mode:
    Menu w/o Jelly: https://screenshot.googleplex.com/6hAswKpZaCvZdtg
    Menu w/ Jelly: https://screenshot.googleplex.com/3cyUKaa4PvL3XTZ
    
    Bug: b/276492247
    Change-Id: Id7602dea19c5c9b29caf707fe9bc7c6ddb0a5d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4391042
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125474}

--- ash/clipboard/views/clipboard_history_label.cc
+++ ash/clipboard/views/clipboard_history_label.cc
@@ -5,6 +5,8 @@
 #include "ash/clipboard/views/clipboard_history_label.h"
 
 #include "ash/clipboard/views/clipboard_history_view_constants.h"
+#include "ash/style/typography.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 
@@ -13,8 +15,12 @@ ClipboardHistoryLabel::ClipboardHistoryLabel(const std::u16string& text)
     : views::Label(text) {
   SetPreferredSize(
       gfx::Size(INT_MAX, ClipboardHistoryViews::kLabelPreferredHeight));
-  SetFontList(views::style::GetFont(views::style::CONTEXT_TOUCH_MENU,
-                                    views::style::STYLE_PRIMARY));
+  if (chromeos::features::IsJellyEnabled()) {
+    TypographyProvider::Get()->StyleLabel(TypographyToken::kCrosBody1, *this);
+  } else {
+    SetFontList(views::style::GetFont(views::style::CONTEXT_TOUCH_MENU,
+                                      views::style::STYLE_PRIMARY));
+  }
   SetMultiLine(false);
   SetHorizontalAlignment(gfx::ALIGN_LEFT);
   SetAutoColorReadabilityEnabled(false);
