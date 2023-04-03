3cfc2ed0fc4778cfd9e6133b784f611967c0188c
Sylvain Defresne <sdefresne@chromium.org>
2023-04-03 13:03:31 +0000

    [ios][web] Remove duplicate call to -[CRWWebRequestController stop]
    The duplicate call was added a long time ago (there were many other
    cleanup of other objects happening between the two calls then which
    made it difficult to see the duplication).
    
    As -[CRWWebRequestController stop] is idempotent, it is not too bad
    that the method was called twice, but there is no need to called it
    twice either.
    
        Note: https://crrev.com/c/1692741 is the original CL adding the
        redundant line and it is difficult to stop it.
    
    Remove the second call as redundant.
    
    Bug: none
    Change-Id: I244f8fe8afe27464058cee87e53b885af69a00e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386460
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125301}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -547,7 +547,6 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
   [self.UIHandler close];
   [self.jsNavigationHandler close];
   [self.requestController close];
-  [self.requestController close];
   [self.webViewNavigationObserver close];
 
   // Mark the destruction sequence has started, in case someone else holds a
