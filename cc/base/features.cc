c7f5491a8094eb46f1a5058ee6e750a5a6ba3962
Benoit Lize <lizeb@chromium.org>
2023-04-03 15:07:18 +0000

    [cc] Enable ReclaimResourcesFlushInBackground by default
    From the stable experiment, this saves a lot of memory (up to >100MiB of
    GPU process PMF) on desktop and Android, without a performance cost.
    
    See https://screenshot.googleplex.com/AHSnpBdWEfpkA45 for instance on Windows.
    
    Bug: 1355820
    Change-Id: I118479092460c7ad0ce58da8ef8dc46152016a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394788
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125357}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -120,7 +120,7 @@ BASE_FEATURE(kUIEnableSharedImageCacheForGpu,
 
 BASE_FEATURE(kReclaimResourcesFlushInBackground,
              "ReclaimResourcesFlushInBackground",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMoreAggressiveSolidColorDetection,
              "MoreAggressiveSolidColorDetection",
