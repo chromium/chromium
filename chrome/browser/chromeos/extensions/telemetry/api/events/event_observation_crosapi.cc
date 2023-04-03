5de0fde1483ee72d12e30d869c609321af0379f9
Bastian Kersting <bkersting@google.com>
2023-04-03 18:00:59 +0000

    [Telemetry] Add EventObservationCrosapi class
    This change adds a class which represents a single
    Event observation on the chrome extension side.
    
    This is part of a bigger effort to add an event interface
    to the Telemetry Extension. For more context visit
    go/design-sketch-event-api.
    
    Bug: b/268125453, b/268125594
    Test: extensions_unittests --gtest_filter=
    *TelemetryExtensionEventObservationCrosapiTest*
    
    Change-Id: I12e59eb7e1b7592f66aa007304912fa0e9f867aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4381556
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1125430}

---
+++ chrome/browser/chromeos/extensions/telemetry/api/events/event_observation_crosapi.cc
@@ -0,0 +1,84 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/extensions/telemetry/api/events/event_observation_crosapi.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/logging.h"
+#include "base/values.h"
+#include "chrome/browser/chromeos/extensions/telemetry/api/events/events_api_converters.h"
+#include "chrome/common/chromeos/extensions/api/events.h"
+#include "chromeos/crosapi/mojom/telemetry_event_service.mojom.h"
+#include "content/public/browser/browser_context.h"
+#include "extensions/browser/event_router.h"
+#include "extensions/browser/extension_event_histogram_value.h"
+#include "extensions/common/extension_id.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+
+namespace chromeos {
+
+namespace {
+
+class DefaultEventDelegate : public EventObservationCrosapi::Delegate {
+ public:
+  explicit DefaultEventDelegate(content::BrowserContext* context)
+      : browser_context_(context) {}
+  ~DefaultEventDelegate() override = default;
+
+  void OnAudioJackEvent(const extensions::ExtensionId& extension_id,
+                        api::os_events::AudioJackEventInfo info) override {
+    base::Value::List args;
+    args.Append(info.ToValue());
+
+    auto event = std::make_unique<extensions::Event>(
+        extensions::events::OS_EVENTS_ON_AUDIO_JACK_EVENT,
+        api::os_events::OnAudioJackEvent::kEventName, std::move(args),
+        browser_context_);
+
+    extensions::EventRouter::Get(browser_context_)
+        ->DispatchEventToExtension(extension_id, std::move(event));
+  }
+
+ private:
+  raw_ptr<content::BrowserContext> browser_context_;
+};
+
+}  // namespace
+
+EventObservationCrosapi::EventObservationCrosapi(
+    const extensions::ExtensionId& extension_id,
+    content::BrowserContext* context)
+    : extension_id_(extension_id),
+      receiver_(this),
+      delegate_(std::make_unique<DefaultEventDelegate>(context)),
+      browser_context_(context) {}
+
+EventObservationCrosapi::~EventObservationCrosapi() = default;
+
+void EventObservationCrosapi::OnEvent(
+    crosapi::mojom::TelemetryEventInfoPtr info) {
+  switch (info->which()) {
+    case crosapi::mojom::internal::TelemetryEventInfo_Data::
+        TelemetryEventInfo_Tag::kAudioJackEventInfo: {
+      delegate_->OnAudioJackEvent(
+          extension_id_,
+          converters::ConvertEventPtr<api::os_events::AudioJackEventInfo>(
+              std::move(info->get_audio_jack_event_info())));
+      break;
+    }
+    case crosapi::mojom::internal::TelemetryEventInfo_Data::
+        TelemetryEventInfo_Tag::kDefaultType:
+      LOG(WARNING) << "Got unknown event category";
+      break;
+  }
+}
+
+mojo::PendingRemote<crosapi::mojom::TelemetryEventObserver>
+EventObservationCrosapi::GetRemote() {
+  return receiver_.BindNewPipeAndPassRemote();
+}
+
+}  // namespace chromeos
---
+++ chrome/browser/chromeos/extensions/telemetry/api/events/event_observation_crosapi_unittest.cc
@@ -0,0 +1,104 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/extensions/telemetry/api/events/event_observation_crosapi.h"
+
+#include <memory>
+#include <tuple>
+#include <utility>
+
+#include "base/test/repeating_test_future.h"
+#include "chrome/common/chromeos/extensions/api/events.h"
+#include "chromeos/crosapi/mojom/telemetry_event_service.mojom.h"
+#include "extensions/browser/api_unittest.h"
+#include "extensions/common/extension_id.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace {
+
+class EventDelegate : public EventObservationCrosapi::Delegate {
+ public:
+  ~EventDelegate() override = default;
+
+  // EventManager::Delegate:
+  void OnAudioJackEvent(const extensions::ExtensionId& extension_id,
+                        api::os_events::AudioJackEventInfo info) override {
+    future_.AddValue(std::make_tuple(extension_id, std::move(info)));
+  }
+
+  std::tuple<extensions::ExtensionId, api::os_events::AudioJackEventInfo>
+  WaitAndGetData() {
+    return future_.Take();
+  }
+
+ private:
+  base::test::RepeatingTestFuture<
+      std::tuple<extensions::ExtensionId, api::os_events::AudioJackEventInfo>>
+      future_;
+};
+
+}  // namespace
+
+class TelemetryExtensionEventObservationCrosapiTest
+    : public extensions::ApiUnitTest {
+ public:
+  TelemetryExtensionEventObservationCrosapiTest() = default;
+
+  void SetUp() override {
+    extensions::ApiUnitTest::SetUp();
+
+    event_observation_ = std::make_unique<EventObservationCrosapi>(
+        extension()->id(), browser_context());
+
+    event_delegate_ = new EventDelegate();
+    event_observation_->SetDelegateForTesting(event_delegate_);
+  }
+
+ protected:
+  EventObservationCrosapi* GetEventRouter() { return event_observation_.get(); }
+
+  EventDelegate* GetEventDelegate() { return event_delegate_; }
+
+  mojo::Remote<crosapi::mojom::TelemetryEventObserver>& GetRemote() {
+    return remote_;
+  }
+
+  void Bind(mojo::PendingRemote<crosapi::mojom::TelemetryEventObserver>
+                pending_remote) {
+    remote_.Bind(std::move(pending_remote));
+  }
+
+ private:
+  // The observation and its delegate live as long as the test itself.
+  std::unique_ptr<EventObservationCrosapi> event_observation_;
+  raw_ptr<EventDelegate> event_delegate_;
+
+  mojo::Remote<crosapi::mojom::TelemetryEventObserver> remote_;
+};
+
+TEST_F(TelemetryExtensionEventObservationCrosapiTest, CanObserveEvent) {
+  Bind(GetEventRouter()->GetRemote());
+
+  auto audio_info = crosapi::mojom::TelemetryAudioJackEventInfo::New();
+  audio_info->state = crosapi::mojom::TelemetryAudioJackEventInfo::State::kAdd;
+
+  auto info = crosapi::mojom::TelemetryEventInfo::NewAudioJackEventInfo(
+      std::move(audio_info));
+
+  GetRemote()->OnEvent(std::move(info));
+
+  // Flush so that the result shows up.
+  GetRemote().FlushForTesting();
+
+  auto result = GetEventDelegate()->WaitAndGetData();
+
+  EXPECT_EQ(std::get<0>(result), extension()->id());
+  EXPECT_EQ(std::get<1>(result).event_state,
+            api::os_events::AudioJackEventState::kAdd);
+}
+
+}  // namespace chromeos
