6edce24cd8eaea17bb229f70e7cffc17ec444d3d
Andrew Grieve <agrieve@chromium.org>
2023-04-04 23:56:45 +0000

    Android: Update filters that decide what files to sideload for tests
    Noticed on a bot that it was taking 40s to run step:
    "Pushing 3010 files via .zip of size 134425932"
    
    Bot: https://ci.chromium.org/ui/p/chromium/builders/ci/android-13-x64-rel/861/overview
    
    After this change:
    "Pushing 2 files via .zip of size 19492422"
    
    Bug: 752610
    Change-Id: I45036638e056eb55e24d5abc2f96c6b6c755c5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396444
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126325}

--- build/android/pylib/utils/device_dependencies.py
+++ build/android/pylib/utils/device_dependencies.py
@@ -7,16 +7,15 @@ import re
 
 from pylib import constants
 
-
 _EXCLUSIONS = [
     re.compile(r'.*OWNERS'),  # Should never be included.
     re.compile(r'.*\.crx'),  # Chrome extension zip files.
-    re.compile(os.path.join('.*',
-                            r'\.git.*')),  # Any '.git*' directories/files.
+    re.compile(r'.*/\.git.*'),  # Any '.git*' directories/files.
     re.compile(r'.*\.so'),  # Libraries packed into .apk.
     re.compile(r'.*Mojo.*manifest\.json'),  # Some source_set()s pull these in.
     re.compile(r'.*\.py'),  # Some test_support targets include python deps.
     re.compile(r'.*\.apk'),  # Should be installed separately.
+    re.compile(r'.*\.jar'),  # Never need java intermediates.
     re.compile(r'.*lib.java/.*'),  # Never need java intermediates.
 
     # Test filter files:
@@ -30,21 +29,27 @@ _EXCLUSIONS = [
 
     # v8's blobs and icu data get packaged into APKs.
     re.compile(r'.*snapshot_blob.*\.bin'),
-    re.compile(r'.*icudtl.bin'),
+    re.compile(r'.*icudtl\.bin'),
 
     # Scripts that are needed by swarming, but not on devices:
     re.compile(r'.*llvm-symbolizer'),
-    re.compile(r'.*md5sum_bin'),
-    re.compile(os.path.join('.*', 'development', 'scripts', 'stack')),
+    re.compile(r'.*md5sum_(?:bin|dist)'),
+    re.compile(r'.*/development/scripts/stack'),
+    re.compile(r'.*/build/android/pylib/symbols'),
+    re.compile(r'.*/build/android/stacktrace'),
 
     # Required for java deobfuscation on the host:
     re.compile(r'.*build/android/stacktrace/.*'),
     re.compile(r'.*third_party/jdk/.*'),
     re.compile(r'.*third_party/proguard/.*'),
 
+    # Our tests don't need these.
+    re.compile(r'.*/devtools-frontend/src/front_end/.*'),
+
     # Build artifacts:
     re.compile(r'.*\.stamp'),
-    re.compile(r'.*.pak\.info'),
+    re.compile(r'.*\.pak\.info'),
+    re.compile(r'.*\.build_config.json'),
     re.compile(r'.*\.incremental\.json'),
 ]
 
