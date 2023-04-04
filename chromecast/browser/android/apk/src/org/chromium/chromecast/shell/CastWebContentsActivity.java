c5764052cb9a113799312f694305bdc005b4bdd7
Brandon Mousseau <bmoose@google.com>
2023-04-04 23:20:07 +0000

    [Chromecast] End cast session if task removed
    If the task for CastWebContentsActivity is removed, we should
    proactively stop the Cast session. This is accomplished by implementing
    TaskRemovedMonitorService, which is started and stopped from
    CastWebContentsActivity. The Service listens for the onTaskRemoved
    callback and stops the Cast session in response.
    
    Bug: internal b/269776321
    Test: cast_shell_junit_tests
    Change-Id: Ib00ac46e58d654b82589b2d873b8b5d80b219b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4397167
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Brandon Mousseau <bmoose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126309}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
@@ -100,6 +100,9 @@ public class CastWebContentsActivity extends Activity {
     @Nullable
     private CastWebContentsSurfaceHelper mSurfaceHelper;
 
+    // SessionId provided in the original Intent used to start the Activity.
+    private String mRootSessionId;
+
     private boolean mAllowPictureInPicture;
     private boolean mIsInPictureInPictureMode;
 
@@ -230,9 +233,17 @@ public class CastWebContentsActivity extends Activity {
                 .subscribe(Observer.onOpen(
                         Both.adapt(CastWebContentsSurfaceHelper::onNewStartParams)));
 
+        mGotIntentState.and(Observable.not(mIsFinishingState))
+                .map(Both::getFirst)
+                .map(CastWebContentsIntentUtils::getSessionId)
+                .subscribe(Observer.onOpen(sessionId -> {
+                    TaskRemovedMonitorService.start(mRootSessionId, sessionId);
+                }));
+
         mIsFinishingState.subscribe(Observer.onOpen((String reason) -> {
             if (DEBUG) Log.d(TAG, "Finishing activity: " + reason);
             mSurfaceHelperState.reset();
+            TaskRemovedMonitorService.stop();
             finishAndRemoveTask();
         }));
 
@@ -251,6 +262,7 @@ public class CastWebContentsActivity extends Activity {
     protected void onCreate(final Bundle savedInstanceState) {
         if (DEBUG) Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
+        mRootSessionId = CastWebContentsIntentUtils.getSessionId(getIntent());
         mCreatedState.set(Unit.unit());
         mGotIntentState.set(getIntent());
 
