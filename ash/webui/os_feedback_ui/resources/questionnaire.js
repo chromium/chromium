2e81b10420bf7fd77edc1c6c55fce48861dc12d4
Billy Zhao <billyzhao@chromium.org>
2023-04-04 22:45:47 +0000

    feedback: Add timestamp to WiFi questionnaire
    Having approximate timestamps can help make sure developers are analyzing the correct impacted logs.
    
    Bug: b:276762754
    Change-Id: Iaa778ec06af6c5819dc428b5af17014d80e40493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4395545
    Commit-Queue: Billy Zhao <billyzhao@chromium.org>
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1126290}

--- ash/webui/os_feedback_ui/resources/questionnaire.js
+++ ash/webui/os_feedback_ui/resources/questionnaire.js
@@ -121,6 +121,7 @@ export const domainQuestions = {
     questionBluetoothOtherDevices,
   ],
   'wifi': [
+    questionGeneralTimestamp,
     questionWifiTypeOfIssue,
     questionWifiConnectedButNotInternet,
     questionWifiNetworkWorkingBefore,
