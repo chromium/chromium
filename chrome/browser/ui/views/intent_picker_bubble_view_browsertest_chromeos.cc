0ba942ad73ad68f2309e1b912fbdb7bfc18a0673
Rune Lillesveen <futhark@chromium.org>
2023-04-04 14:00:04 +0000

    Disable flaky AppLaunchURLCancelsPrerendering
    See e.g.:
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/50286
    
    Bug: 1428425
    Change-Id: Id10183d390bd2958aff17a0fd3714206b20d41e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396392
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Owners-Override: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125952}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest_chromeos.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest_chromeos.cc
@@ -1281,8 +1281,15 @@ class IntentPickerBubbleViewPrerenderingBrowserTestChromeOS
 // Simulates prerendering an app URL that the user has opted into always
 // launching an app window for. In this case, the prerender should be canceled
 // and the app shouldn't be opened.
+// TODO(https://crbug.com/1428425): flakily times out
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_AppLaunchURLCancelsPrerendering \
+  DISABLED_AppLaunchURLCancelsPrerendering
+#else
+#define MAYBE_AppLaunchURLCancelsPrerendering AppLaunchURLCancelsPrerendering
+#endif
 IN_PROC_BROWSER_TEST_F(IntentPickerBubbleViewPrerenderingBrowserTestChromeOS,
-                       AppLaunchURLCancelsPrerendering) {
+                       MAYBE_AppLaunchURLCancelsPrerendering) {
   // Prerendering is currently limited to same-origin pages so we need to start
   // it from an arbitrary page on the same origin, rather than about:blank.
   const GURL kInitialUrl = embedded_test_server()->GetURL("/empty.html");
