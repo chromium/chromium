fdd79a4d7cc78213abc536dc776111f1cd85c364
Alex Ilin <alexilin@chromium.org>
2023-04-04 10:27:57 +0000

    [BSC] Registration jwt in StartAuthCodeForOAuth2TokenExchange
    This CL adds support for extra `binding_registration_token` parameter
    to GaiaAuthFetcher::StartAuthCodeForOAuth2TokenExchange.
    
    `binding_registration_token` parameter allows binding the LST token
    to a device with unexportable signing keys.
    
    google_apis layer treats `binding_registration_token` as an opaque
    string that could be optionally attached to a GetToken request.
    
    The server indicates whether the binding was successful in the
    response body. This value is propagated to GaiaAuthConsumer.
    
    Bug: b/274463812
    Change-Id: I3031567aeedac03ab44a4da8b38d0d24d9a4bcac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4379878
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125890}

--- google_apis/gaia/gaia_auth_consumer.cc
+++ google_apis/gaia/gaia_auth_consumer.cc
@@ -9,12 +9,14 @@ GaiaAuthConsumer::ClientOAuthResult::ClientOAuthResult(
     const std::string& new_access_token,
     int new_expires_in_secs,
     bool new_is_child_account,
-    bool new_is_under_advanced_protection)
+    bool new_is_under_advanced_protection,
+    bool new_is_bound_to_key)
     : refresh_token(new_refresh_token),
       access_token(new_access_token),
       expires_in_secs(new_expires_in_secs),
       is_child_account(new_is_child_account),
-      is_under_advanced_protection(new_is_under_advanced_protection) {}
+      is_under_advanced_protection(new_is_under_advanced_protection),
+      is_bound_to_key(new_is_bound_to_key) {}
 
 GaiaAuthConsumer::ClientOAuthResult::ClientOAuthResult(
     const ClientOAuthResult& other) = default;
