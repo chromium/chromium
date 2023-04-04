68651c20804e60bb7e679d4c2fafb06cd5e2262e
Liza Bipin <mlbipin@google.com>
2023-04-04 15:06:43 +0000

    [Code Health] Migrate base::Value::SetBoolKey to v2 API
    Bug: 1187001
    Change-Id: I357e4bf557460b3a6a3253cc3c6a63d9a47cc728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394433
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1125984}

--- components/proxy_config/proxy_policy_handler_unittest.cc
+++ components/proxy_config/proxy_policy_handler_unittest.cc
@@ -211,9 +211,9 @@ TEST_F(ProxyPolicyHandlerTest, PacScriptProxyMode) {
 // ProxyPacMandatory can be set only via ProxySettings.
 TEST_F(ProxyPolicyHandlerTest, PacScriptProxyModeWithPacMandatory) {
   base::Value proxy_settings(base::Value::Type::DICT);
-  proxy_settings.SetStringKey(kProxyPacUrl, "http://short.org/proxy.pac");
-  proxy_settings.SetStringKey(kProxyMode, ProxyPrefs::kPacScriptProxyModeName);
-  proxy_settings.SetBoolKey(kProxyPacMandatory, true);
+  proxy_settings.GetDict().Set(kProxyPacUrl, "http://short.org/proxy.pac");
+  proxy_settings.GetDict().Set(kProxyMode, ProxyPrefs::kPacScriptProxyModeName);
+  proxy_settings.GetDict().Set(kProxyPacMandatory, true);
 
   PolicyMap policy;
   policy.Set(kProxySettings, POLICY_LEVEL_MANDATORY, POLICY_SCOPE_USER,
