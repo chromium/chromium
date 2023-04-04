1309a9ce4cde267863b899bd30c0b44852732023
Shakti Sahu <shaktisahu@chromium.org>
2023-04-04 01:30:05 +0000

    [CSC] Update companion proto to match server side
    Bug: None
    Change-Id: I7c499dedce4865e75aee2e2c1c7bcf493e57b482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4392889
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1125744}

--- chrome/browser/ui/webui/side_panel/companion/companion_url_builder_unittest.cc
+++ chrome/browser/ui/webui/side_panel/companion/companion_url_builder_unittest.cc
@@ -62,7 +62,7 @@ class CompanionUrlBuilderTest : public testing::Test {
     GURL companion_url = url_builder_->BuildCompanionURL(page_url);
 
     // Deserialize the query param into protobuf.
-    companion::proto::QueryParams proto =
+    companion::proto::CompanionUrlParams proto =
         DeserializeCompanionRequest(companion_url);
 
     if (expect_was_sent) {
@@ -73,11 +73,12 @@ class CompanionUrlBuilderTest : public testing::Test {
 
     EXPECT_TRUE(proto.has_msbb_enabled());
   }
-  // Deserialize the query param into proto::QueryParams.
-  proto::QueryParams DeserializeCompanionRequest(GURL companion_url) {
-    companion::proto::QueryParams proto;
+  // Deserialize the query param into proto::CompanionUrlParams.
+  proto::CompanionUrlParams DeserializeCompanionRequest(GURL companion_url) {
+    companion::proto::CompanionUrlParams proto;
     std::string url_param;
-    EXPECT_TRUE(net::GetValueForKeyInQuery(companion_url, "query", &url_param));
+    EXPECT_TRUE(net::GetValueForKeyInQuery(companion_url, "companion_query",
+                                           &url_param));
     auto base64_decoded = base::Base64Decode(url_param);
     auto serialized_proto = std::string(base64_decoded.value().begin(),
                                         base64_decoded.value().end());
@@ -107,7 +108,7 @@ TEST_F(CompanionUrlBuilderTest, MsbbOff) {
   EXPECT_EQ(value, kOrigin);
 
   // Deserialize the query param into protobuf.
-  companion::proto::QueryParams proto =
+  companion::proto::CompanionUrlParams proto =
       DeserializeCompanionRequest(companion_url);
 
   // URL shouldn't be sent when MSBB is off.
@@ -130,7 +131,7 @@ TEST_F(CompanionUrlBuilderTest, MsbbOn) {
   EXPECT_EQ(value, kOrigin);
 
   // Deserialize the query param into protobuf.
-  companion::proto::QueryParams proto =
+  companion::proto::CompanionUrlParams proto =
       DeserializeCompanionRequest(companion_url);
 
   // Verify fields inside protobuf.
@@ -176,7 +177,7 @@ TEST_F(CompanionUrlBuilderTest, WithTextQuery) {
   EXPECT_EQ(value, kOrigin);
 
   // Deserialize the query param into protobuf.
-  companion::proto::QueryParams proto =
+  companion::proto::CompanionUrlParams proto =
       DeserializeCompanionRequest(companion_url);
 
   EXPECT_EQ(proto.page_url(), page_url.spec());
@@ -195,7 +196,7 @@ TEST_F(CompanionUrlBuilderTest, WithoutTextQuery) {
   EXPECT_EQ(value, kOrigin);
 
   // Deserialize the query param into protobuf.
-  companion::proto::QueryParams proto =
+  companion::proto::CompanionUrlParams proto =
       DeserializeCompanionRequest(companion_url);
 
   EXPECT_EQ(proto.page_url(), page_url.spec());
