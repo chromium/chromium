c38e65a9e3f7dc6edb9de39e8bb1bf0e475771a6
Sylvain Defresne <sdefresne@chromium.org>
2023-04-04 13:35:36 +0000

    [ios] Check BrowserListObserver is unregistered before destruction
    Change BrowserListObserver to inherit from base::CheckedObserver and
    check that IsInObserverList() returns false in the destructor. Change
    the ObserverList<...>::Unchecked to ObserverList<...> after that.
    
    Bug: 1421255
    Change-Id: Ia92c7dfdc8606044bce0ac579d2abc8ecab3f376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4396387
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1125940}

--- ios/chrome/browser/main/browser_list_impl.h
+++ ios/chrome/browser/main/browser_list_impl.h
@@ -42,8 +42,7 @@ class BrowserListImpl : public BrowserList, public BrowserObserver {
   std::set<Browser*> browsers_;
   std::set<Browser*> incognito_browsers_;
 
-  // TODO(crbug.com/1421255): The Observers should be checked.
-  base::ObserverList<BrowserListObserver, true>::Unchecked observers_;
+  base::ObserverList<BrowserListObserver, true> observers_;
 };
 
 #endif  // IOS_CHROME_BROWSER_MAIN_BROWSER_LIST_IMPL_H_
