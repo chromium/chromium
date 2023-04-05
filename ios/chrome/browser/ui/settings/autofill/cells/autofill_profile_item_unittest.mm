6f5d7cc81dba351c256cb987a4c9658fce64698a
Vidhan Jain <vidhanj@google.com>
2023-04-05 14:11:57 +0000

    [iOS] Indicate local address by appending cloud slash image in the cell
    Screenshots:
    1. Without image: https://screenshot.googleplex.com/3nSYxuW4vUBVjet
    2. With image: https://screenshot.googleplex.com/9aG9VTsfufP9yWG.png
    
    Mocks: https://screenshot.googleplex.com/B8JBYbEe8vwVL6u.png
    
    Bug: 1407666
    Change-Id: I8c63afcb8ed57692d6b2f4868ff365f50f835c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4392572
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126541}

---
+++ ios/chrome/browser/ui/settings/autofill/cells/autofill_profile_item_unittest.mm
@@ -0,0 +1,63 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/settings/autofill/cells/autofill_profile_item.h"
+
+#import "base/mac/foundation_util.h"
+#import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_styler.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/gtest_mac.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+using AutofillProfileItemTest = PlatformTest;
+}
+
+// Tests that the UILabel is set properly after a call to
+// `configureCell:` and the image are visible.
+TEST_F(AutofillProfileItemTest, ItemProperties) {
+  NSString* text = @"Cell text";
+  NSString* detailText = @"Detail text";
+
+  AutofillProfileItem* item = [[AutofillProfileItem alloc] initWithType:0];
+  item.title = text;
+  item.detailText = detailText;
+  item.image = [[UIImage alloc] init];
+
+  id cell = [[[item cellClass] alloc] init];
+  ASSERT_TRUE([cell isMemberOfClass:[AutofillProfileCell class]]);
+
+  AutofillProfileCell* imageCell =
+      base::mac::ObjCCastStrict<AutofillProfileCell>(cell);
+  EXPECT_FALSE(imageCell.textLabel.text);
+  EXPECT_FALSE(imageCell.detailTextLabel.text);
+  EXPECT_FALSE(imageCell.imageView.image);
+
+  [item configureCell:cell withStyler:[[ChromeTableViewStyler alloc] init]];
+  EXPECT_NSEQ(text, imageCell.textLabel.text);
+  EXPECT_NSEQ(detailText, imageCell.detailTextLabel.text);
+  EXPECT_FALSE(imageCell.imageView.isHidden);
+}
+
+// Tests that the imageView is not visible if no image is set.
+TEST_F(AutofillProfileItemTest, ItemImageViewHidden) {
+  NSString* text = @"Cell text";
+
+  AutofillProfileItem* item = [[AutofillProfileItem alloc] initWithType:0];
+  item.title = text;
+
+  id cell = [[[item cellClass] alloc] init];
+  ASSERT_TRUE([cell isMemberOfClass:[AutofillProfileCell class]]);
+
+  AutofillProfileCell* imageCell =
+      base::mac::ObjCCastStrict<AutofillProfileCell>(cell);
+  EXPECT_FALSE(item.image);
+  [item configureCell:cell withStyler:[[ChromeTableViewStyler alloc] init]];
+  EXPECT_FALSE(item.image);
+  EXPECT_TRUE(imageCell.imageView.isHidden);
+}
