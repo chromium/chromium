e4912d49a5e6efe4625acae046e041f5337d3573
Gauthier Ambard <gambard@chromium.org>
2023-04-05 12:59:58 +0000

    [iOS] Prepare to move symbols
    For downstream compatibility.
    
    Bug: 1319852
    Change-Id: I9fdfc27407c39c8d58c85992aac111405a0d2ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4401581
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126504}

---
+++ ios/chrome/browser/shared/ui/symbols/symbols.h
@@ -0,0 +1,11 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_SYMBOLS_SYMBOLS_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_SYMBOLS_SYMBOLS_H_
+
+// Temporary include.
+#import "ios/chrome/browser/ui/icons/symbols.h"
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_SYMBOLS_SYMBOLS_H_
