4ad3f6da025f92b25fbb913da10289480fc329f2
Samar <samarchehade@google.com>
2023-04-05 14:37:03 +0000

    [Signin][Android] Re-enable newly passing tests.
    Fixed: 1337069,1331277,1295396
    Change-Id: I4bb77778d45dfc7ea03b6d384f61a75853085f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400952
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Quick-Run: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1126560}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -348,7 +348,6 @@ public class FirstRunIntegrationTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1295396")
     public void testAbortFirstRun() throws Exception {
         launchViewIntent(TEST_URL);
         Activity chromeLauncherActivity = waitForActivity(ChromeLauncherActivity.class);
@@ -632,7 +631,6 @@ public class FirstRunIntegrationTest {
     @MediumTest
     // TODO(https://crbug.com/1111490): Change this test case when policy can handle cases when ToS
     // is accepted in Browser App.
-    @DisabledTest(message = "https://crbug.com/1331277")
     public void testSkipTosPage_WithCctPolicy() throws Exception {
         skipTosDialogViaPolicy();
         FirstRunStatus.setSkipWelcomePage(true);
