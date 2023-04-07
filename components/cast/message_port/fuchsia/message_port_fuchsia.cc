4b0dd6f2eb1fc4ceb0a5e2e39ce4616bb145dc2b
Zijie He <zijiehe@google.com>
2023-04-07 23:11:52 +0000

    [Fuchsia] Replace DCHECK with CHECK in CastStreaming/MessagePortFuchsia
    This change is a follow-up of the discussion in http://crbug.com/1420075
    since we disable DCHECK on smart display builders. So the change can
    detect potential uncleared states if one function is double-called or
    executed in a wrong order, e.g. a call after being destructed.
    Meanwhile this change initializes the `receiver_` to nullptr.
    
    Bug: 1420075, b/230897148
    Change-Id: I0514435cc71efa7a6d021819bd66d7128322adc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4409311
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Rohan Pavone <rohpavone@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1127832}

--- components/cast/message_port/fuchsia/message_port_fuchsia.cc
+++ components/cast/message_port/fuchsia/message_port_fuchsia.cc
@@ -28,8 +28,8 @@ class MessagePortFuchsiaClient : public MessagePortFuchsia {
 
   // MessagePortFuchsia implementation.
   fidl::InterfaceHandle<::fuchsia::web::MessagePort> TakeClientHandle() final {
-    DCHECK(!receiver_);
-    DCHECK(port_.is_bound());
+    CHECK(!receiver_);
+    CHECK(port_.is_bound());
     return port_.Unbind();
   }
 
@@ -41,8 +41,8 @@ class MessagePortFuchsiaClient : public MessagePortFuchsia {
 
   // MessagePort implementation.
   void SetReceiver(cast_api_bindings::MessagePort::Receiver* receiver) final {
-    DCHECK(receiver);
-    DCHECK(!receiver_);
+    CHECK(receiver);
+    CHECK(!receiver_);
     receiver_ = receiver;
     port_.set_error_handler(
         [this](zx_status_t status) { MessagePortFuchsia::OnZxError(status); });
@@ -101,8 +101,8 @@ class MessagePortFuchsiaClient : public MessagePortFuchsia {
   }
 
   void ReadNextMessage() {
-    DCHECK(receiver_);
-    DCHECK(port_);
+    CHECK(receiver_);
+    CHECK(port_);
 
     port_->ReceiveMessage(
         fit::bind_member(this, &MessagePortFuchsiaClient::OnMessageReady));
@@ -140,8 +140,8 @@ class MessagePortFuchsiaServer : public MessagePortFuchsia,
 
   // MessagePort implementation.
   void SetReceiver(cast_api_bindings::MessagePort::Receiver* receiver) final {
-    DCHECK(receiver);
-    DCHECK(!receiver_);
+    CHECK(receiver);
+    CHECK(!receiver_);
     receiver_ = receiver;
     binding_.set_error_handler(
         [this](zx_status_t status) { MessagePortFuchsia::OnZxError(status); });
@@ -233,7 +233,7 @@ std::unique_ptr<MessagePort> MessagePortFuchsia::Create(
 }
 
 MessagePortFuchsia* MessagePortFuchsia::FromMessagePort(MessagePort* port) {
-  DCHECK(port);
+  CHECK(port);
   // This is safe because there is one MessagePort implementation per platform
   // and this is called internally to the implementation.
   return static_cast<MessagePortFuchsia*>(port);
@@ -253,7 +253,7 @@ fuchsia::web::WebMessage MessagePortFuchsia::CreateWebMessage(
       MessagePortFuchsia* port_fuchsia = FromMessagePort(port.get());
       PortType port_type = port_fuchsia->port_type_;
 
-      DCHECK_EQ(expected_port_type, port_type)
+      CHECK_EQ(expected_port_type, port_type)
           << "Only one implementation of MessagePortFuchsia can be transmitted "
              "in the same message.";
       if (expected_port_type != port_type) {
@@ -294,7 +294,7 @@ MessagePortFuchsia::~MessagePortFuchsia() = default;
 absl::optional<fuchsia::web::FrameError>
 MessagePortFuchsia::ExtractAndHandleMessageFromFidl(
     fuchsia::web::WebMessage message) {
-  DCHECK(receiver_);
+  CHECK(receiver_);
   if (!message.has_data()) {
     return fuchsia::web::FrameError::NO_DATA_IN_MESSAGE;
   }
@@ -334,7 +334,7 @@ void MessagePortFuchsia::OnZxError(zx_status_t status) {
 }
 
 void MessagePortFuchsia::ReportPipeError() {
-  DCHECK(receiver_);
+  CHECK(receiver_);
   receiver_->OnPipeError();
 }
 
@@ -346,7 +346,7 @@ bool MessagePortFuchsia::PostMessage(base::StringPiece message) {
 bool MessagePortFuchsia::PostMessageWithTransferables(
     base::StringPiece message,
     std::vector<std::unique_ptr<MessagePort>> ports) {
-  DCHECK(receiver_);
+  CHECK(receiver_);
   message_queue_.emplace_back(CreateWebMessage(message, std::move(ports)));
 
   // Start draining the queue if it was empty beforehand.
