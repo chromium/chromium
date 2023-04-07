70f445f1ffd932cc9c1b4d85a65cee8ef8f33d70
Jennifer Serrano <jennserrano@google.com>
2023-04-07 23:04:20 +0000

    [M112 Merge] Revert "Phone Hub: Modify Phone Hub tray to show nudge."
    This reverts commit cfaa812836b0858f55ba7a719ed013af85737fb4.
    
    Reason for revert: causing crash see b/273532240
    
    Original change's description:
    > Phone Hub: Modify Phone Hub tray to show nudge.
    >
    > Modify Phone Hub tray methods to display Phone Hub nudge if nudge flag is on. Also, hide the nudge when Phone Hub icon is clicked.
    >
    > UI:
    > (Recording of nudge being hidden after clicking on Phone Hub icon)
    > https://drive.google.com/file/d/1P9yVCb-aRB7IXY2gqHhorPFXbojnpckz/view?usp=sharing
    > https://screenshot.googleplex.com/6DLVFX739n9Us6a
    >
    > Change-Id: I81ea423963ce395521e749a96b0ac7db33474020
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4163684
    > Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1098164}
    
    Bug: b/273532240
    
    (cherry picked from commit d5fc6ee4bf81b88df3e060857213278229885053)
    
    Change-Id: I9e5a60fc2d39926e211a6eaf312596dc67fd7f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4347815
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1118820}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408992
    Cr-Commit-Position: refs/branch-heads/5615@{#1167}
    Cr-Branched-From: 9c6408ef696e83a9936b82bbead3d41c93c82ee4-refs/heads/main@{#1109224}

--- ash/system/phonehub/phone_hub_tray.cc
+++ ash/system/phonehub/phone_hub_tray.cc
@@ -60,23 +60,11 @@ constexpr int kIconSpacing = 12;
 constexpr auto kBubblePadding =
     gfx::Insets::TLBR(0, 0, kBubbleBottomPaddingDip, 0);
 
-bool IsInUserSession() {
-  SessionControllerImpl* session_controller =
-      Shell::Get()->session_controller();
-  return session_controller->GetSessionState() ==
-             session_manager::SessionState::ACTIVE &&
-         !session_controller->IsRunningInAppMode();
-}
-
 }  // namespace
 
 PhoneHubTray::PhoneHubTray(Shelf* shelf)
     : TrayBackgroundView(shelf, TrayBackgroundViewCatalogName::kPhoneHub),
-      ui_controller_(new PhoneHubUiController()),
-      phone_hub_nudge_controller_(
-          features::IsPhoneHubNudgeEnabled()
-              ? std::make_unique<PhoneHubNudgeController>()
-              : nullptr) {
+      ui_controller_(new PhoneHubUiController()) {
   // By default, if the individual buttons did not handle the event consider it
   // as a phone hub icon event.
   SetPressedCallback(base::BindRepeating(&PhoneHubTray::PhoneHubIconActivated,
@@ -397,14 +385,7 @@ void PhoneHubTray::CloseBubble() {
 void PhoneHubTray::UpdateVisibility() {
   DCHECK(ui_controller_.get());
   auto ui_state = ui_controller_->ui_state();
-  SetVisiblePreferred(ui_state != PhoneHubUiController::UiState::kHidden &&
-                      IsInUserSession());
-  if (features::IsPhoneHubNudgeEnabled() && IsInUserSession()) {
-    if (ui_state == PhoneHubUiController::UiState::kOnboardingWithoutPhone) {
-      phone_hub_nudge_controller_->ShowNudge();
-      // TODO (b/266853434): Animation of icon.
-    }
-  }
+  SetVisiblePreferred(ui_state != PhoneHubUiController::UiState::kHidden);
 }
 
 void PhoneHubTray::UpdateHeaderVisibility() {
@@ -432,11 +413,8 @@ void PhoneHubTray::PhoneHubIconActivated(const ui::Event& event) {
   // Simply toggle between visible/invisibvle
   if (bubble_ && bubble_->bubble_view()->GetVisible()) {
     CloseBubble();
-    return;
-  }
-  ShowBubble();
-  if (features::IsPhoneHubNudgeEnabled()) {
-    phone_hub_nudge_controller_->HideNudge();
+  } else {
+    ShowBubble();
   }
 }
 
