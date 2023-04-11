86331eec83edcec7d150fd9b2cc0b06fc45ac03e
Hiroki Nakagawa <nhiroki@chromium.org>
2023-04-11 14:30:29 +0000

    [Merge to M113] Prerender: Take PrerenderHost via FrameTree::Delegate
    This CL makes PrerenderHostRegistry::DidStartNavigation() take
    PrerenderHost corresponding to the navigation via FrameTree::Delegate.
    
    Before this CL, DidStartNavigation() took the host via
    PrerenderHost::GetPrerenderHostFromFrameTreeNode(), but this returned a
    nullptr when it is scheduled to delete PrerenderHost by
    PrerenderHostRegistry::ScheduleToDeleteAbandonedHost() and resulted in
    CHECK failure. This CL fixes it.
    
    (cherry picked from commit 5e85b9f7c41eb9cc74807bfdaf25280a5b5f0077)
    
    Bug: 1427469
    Change-Id: If0021524fde9f2d23caeeb792ed29b550f6319ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410280
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1128102}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4414115
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#452}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- content/browser/preloading/prerender/prerender_host_registry.cc
+++ content/browser/preloading/prerender/prerender_host_registry.cc
@@ -789,11 +789,11 @@ void PrerenderHostRegistry::DidStartNavigation(
     return;
   }
 
-  // PrerenderHost owns ongoing `navigation_request` indirectly until it is
-  // ready to commit, so `prerender_host` should always be non-null here.
-  auto* prerender_host = PrerenderHost::GetPrerenderHostFromFrameTreeNode(
-      *navigation_request->frame_tree_node());
-  DCHECK(prerender_host);
+  // This navigation is running on the main frame in the prerendered page, so
+  // its FrameTree::Delegate should be PrerenderHost.
+  auto* prerender_host = static_cast<PrerenderHost*>(
+      navigation_request->frame_tree_node()->frame_tree().delegate());
+  CHECK(prerender_host);
 
   prerender_host->DidStartNavigation(navigation_handle);
 }
