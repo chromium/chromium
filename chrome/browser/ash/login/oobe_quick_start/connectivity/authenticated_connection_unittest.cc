73f3c0cff66335d82ac8348a172a55af0a88f1a2
Melissa Zhang <melzhang@chromium.org>
2023-04-11 00:58:14 +0000

    Revert "[QuickStart] Generate shared secret and send in wifi request"
    This reverts commit 978b7e36dae2ca509ec14a68489aaf3d28b818df.
    
    Reason for revert: Breaking https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/35067/overview
    
    Original change's description:
    > [QuickStart] Generate shared secret and send in wifi request
    >
    > There should be a secondary shared secret created that will be used if the target device reboots. Shared secret is sent in the wifi request. Creating and sending the secondary shared secret is done in this CL.
    >
    > TEST= Modified current unit test to just check if the request payload contains a shared secret.
    >
    > bug: b/276764698
    >
    > Change-Id: I5b0efd892005b96fdb95d36ec83275dd5f48dbf5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4408380
    > Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    > Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1128304}
    
    Change-Id: I91db8b2a4547a5d5cfc64901d3522f78d773831d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410545
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Melissa Zhang <melzhang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1128430}

--- chrome/browser/ash/login/oobe_quick_start/connectivity/authenticated_connection_unittest.cc
+++ chrome/browser/ash/login/oobe_quick_start/connectivity/authenticated_connection_unittest.cc
@@ -139,9 +139,11 @@ TEST_F(AuthenticatedConnectionTest, RequestWifiCredentials) {
   EXPECT_TRUE(wifi_request_payload.FindBool("request_wifi"));
   EXPECT_EQ(wifi_request_payload.FindInt("SESSION_ID"), session_id);
 
-  // TODO(b/234655072): Create kSecondarySharedSecret const and check value
-  // equals after AuthenticatedConnection refactor is merged.
-  EXPECT_TRUE(wifi_request_payload.FindString("shared_secret"));
+  std::string shared_secret_str(kSharedSecret.begin(), kSharedSecret.end());
+  std::string shared_secret_base64;
+  base::Base64Encode(shared_secret_str, &shared_secret_base64);
+  EXPECT_EQ(*wifi_request_payload.FindString("shared_secret"),
+            shared_secret_base64);
 }
 
 TEST_F(AuthenticatedConnectionTest, RequestAccountTransferAssertion) {
