f1363baa7169bbca93adcf4a15f28686307e938e
Lauren Winston <lwinston@google.com>
2023-04-11 00:54:39 +0000

    [Read Anything] Ensure ReadAnythingAppModel has proper test coverage.
    From code coverage analysis, this should be the final CL in breaking
    ReadAnythingModel tests out from the controller.
    
    Bug: 1266555
    Change-Id: I3b7285b82ae284156e4462ac7acc614450bab509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4400434
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128429}

--- chrome/renderer/accessibility/read_anything_app_model_browsertest.cc
+++ chrome/renderer/accessibility/read_anything_app_model_browsertest.cc
@@ -77,6 +77,14 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
         line_spacing_enum, letter_spacing_enum));
   }
 
+  void SetLineAndLetterSpacing(
+      read_anything::mojom::LetterSpacing letter_spacing,
+      read_anything::mojom::LineSpacing line_spacing) {
+    model_->OnThemeChanged(read_anything::mojom::ReadAnythingTheme::New(
+        "Arial", 15.0, SkColorSetRGB(0x33, 0x40, 0x36),
+        SkColorSetRGB(0xDF, 0xD2, 0x63), line_spacing, letter_spacing));
+  }
+
   void AccessibilityEventReceived(
       const std::vector<ui::AXTreeUpdate>& updates) {
     AccessibilityEventReceived(updates[0].tree_data.tree_id, updates);
@@ -110,6 +118,16 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
 
   float LetterSpacing() { return model_->letter_spacing(); }
 
+  bool DistillationInProgress() { return model_->distillation_in_progress(); }
+
+  bool HasSelection() { return model_->has_selection(); }
+
+  ui::AXNodeID StartNodeId() { return model_->start_node_id(); }
+  ui::AXNodeID EndNodeId() { return model_->end_node_id(); }
+
+  int32_t StartOffset() { return model_->start_offset(); }
+  int32_t EndOffset() { return model_->end_offset(); }
+
   bool IsNodeIgnoredForReadAnything(ui::AXNodeID ax_node_id) {
     return model_->IsNodeIgnoredForReadAnything(ax_node_id);
   }
@@ -131,6 +149,10 @@ class ReadAnythingAppModelTest : public ChromeRenderViewTest {
 
   void Reset(const std::vector<ui::AXNodeID>& content_node_ids) {
     model_->Reset(content_node_ids);
+  }
+
+  bool ContentNodeIdsContains(ui::AXNodeID ax_node_id) {
+    return base::Contains(model_->content_node_ids(), ax_node_id);
   }
 
   bool DisplayNodeIdsContains(ui::AXNodeID ax_node_id) {
@@ -584,4 +606,127 @@ TEST_F(ReadAnythingAppModelTest, SelectionNodeIdsContains_BackwardSelection) {
   EXPECT_TRUE(SelectionNodeIdsContains(2));
   EXPECT_TRUE(SelectionNodeIdsContains(3));
   EXPECT_FALSE(SelectionNodeIdsContains(4));
+}
+
+TEST_F(ReadAnythingAppModelTest, SetTheme_LineAndLetterSpacingCorrect) {
+  SetLineAndLetterSpacing(read_anything::mojom::LetterSpacing::kStandard,
+                          read_anything::mojom::LineSpacing::kLoose);
+  ASSERT_EQ(LineSpacing(), 1.5);
+  ASSERT_EQ(LetterSpacing(), 0);
+
+  // Ensure the line and letter spacing are updated.
+  SetLineAndLetterSpacing(read_anything::mojom::LetterSpacing::kWide,
+                          read_anything::mojom::LineSpacing::kVeryLoose);
+  ASSERT_EQ(LineSpacing(), 2.0);
+  ASSERT_EQ(LetterSpacing(), 0.05f);
+}
+
+TEST_F(ReadAnythingAppModelTest, Reset_ResetsState) {
+  // Initial state.
+  ui::AXTreeUpdate update;
+  SetUpdateTreeID(&update);
+  update.nodes.resize(3);
+  update.nodes[0].id = 4;
+  update.nodes[0].child_ids = {5, 6};
+  update.nodes[1].id = 5;
+  update.nodes[2].id = 6;
+  AccessibilityEventReceived({update});
+  ProcessDisplayNodes({3, 4});
+  SetDistillationInProgress(true);
+
+  // Assert initial state before resetting.
+  ASSERT_TRUE(DistillationInProgress());
+
+  ASSERT_TRUE(DisplayNodeIdsContains(1));
+  ASSERT_TRUE(DisplayNodeIdsContains(3));
+  ASSERT_TRUE(DisplayNodeIdsContains(4));
+  ASSERT_TRUE(DisplayNodeIdsContains(5));
+  ASSERT_TRUE(DisplayNodeIdsContains(6));
+
+  Reset({1, 2});
+
+  // Assert reset state.
+  ASSERT_FALSE(DistillationInProgress());
+
+  ASSERT_TRUE(ContentNodeIdsContains(1));
+  ASSERT_TRUE(ContentNodeIdsContains(2));
+
+  ASSERT_FALSE(DisplayNodeIdsContains(1));
+  ASSERT_FALSE(DisplayNodeIdsContains(3));
+  ASSERT_FALSE(DisplayNodeIdsContains(4));
+  ASSERT_FALSE(DisplayNodeIdsContains(5));
+  ASSERT_FALSE(DisplayNodeIdsContains(6));
+
+  // Calling reset with different content nodes updates the content nodes.
+  Reset({5, 4});
+  ASSERT_FALSE(ContentNodeIdsContains(1));
+  ASSERT_FALSE(ContentNodeIdsContains(2));
+  ASSERT_TRUE(ContentNodeIdsContains(5));
+  ASSERT_TRUE(ContentNodeIdsContains(4));
+}
+
+TEST_F(ReadAnythingAppModelTest, Reset_ResetsSelectionState) {
+  // Initial state.
+  ui::AXTreeUpdate update;
+  SetUpdateTreeID(&update);
+  update.tree_data.sel_anchor_object_id = 3;
+  update.tree_data.sel_focus_object_id = 2;
+  update.tree_data.sel_anchor_offset = 0;
+  update.tree_data.sel_focus_offset = 0;
+  update.tree_data.sel_is_backward = true;
+  AccessibilityEventReceived({update});
+  ProcessSelection();
+
+  // Assert initial selection state.
+  ASSERT_TRUE(SelectionNodeIdsContains(1));
+  ASSERT_TRUE(SelectionNodeIdsContains(2));
+  ASSERT_TRUE(SelectionNodeIdsContains(3));
+
+  ASSERT_TRUE(HasSelection());
+
+  ASSERT_NE(StartOffset(), -1);
+  ASSERT_NE(EndOffset(), -1);
+
+  ASSERT_NE(StartNodeId(), ui::kInvalidAXNodeID);
+  ASSERT_NE(EndNodeId(), ui::kInvalidAXNodeID);
+
+  Reset({1, 2});
+
+  // Assert reset selection state.
+  ASSERT_FALSE(SelectionNodeIdsContains(1));
+  ASSERT_FALSE(SelectionNodeIdsContains(2));
+  ASSERT_FALSE(SelectionNodeIdsContains(3));
+
+  ASSERT_FALSE(HasSelection());
+
+  ASSERT_EQ(StartOffset(), -1);
+  ASSERT_EQ(EndOffset(), -1);
+
+  ASSERT_EQ(StartNodeId(), ui::kInvalidAXNodeID);
+  ASSERT_EQ(EndNodeId(), ui::kInvalidAXNodeID);
+}
+
+TEST_F(ReadAnythingAppModelTest, PostProcessSelection_SelectionStateCorrect) {
+  // Initial state.
+  ui::AXTreeUpdate update;
+  SetUpdateTreeID(&update);
+  update.tree_data.sel_anchor_object_id = 2;
+  update.tree_data.sel_focus_object_id = 3;
+  update.tree_data.sel_anchor_offset = 0;
+  update.tree_data.sel_focus_offset = 0;
+  update.tree_data.sel_is_backward = false;
+  AccessibilityEventReceived({update});
+  ProcessSelection();
+
+  ASSERT_TRUE(HasSelection());
+
+  ASSERT_TRUE(SelectionNodeIdsContains(1));
+  ASSERT_TRUE(SelectionNodeIdsContains(2));
+  ASSERT_TRUE(SelectionNodeIdsContains(3));
+
+  ASSERT_EQ(StartOffset(), 0);
+  ASSERT_EQ(EndOffset(), 0);
+
+  ASSERT_EQ(StartNodeId(), 2);
+  ASSERT_EQ(EndNodeId(), 3);
 }
