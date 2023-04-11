e8a5a5306a1d1d86dfd4027cb77184f6a2dbd966
Ashley Prasad <ashleydp@google.com>
2023-04-11 23:55:28 +0000

    navigation-view: use sys color
    Use CrOS Sys highlight token when available to support jelly colors.
    
    Screenshot (w/ jelly): http://screen/5XFaMkiy2vFsBt2.png
    Screenshot (w/o jelly): http://screen/6eVrC9ga3N58uc2.png
    
    Bug: b:275718322, b:275718921
    Test: Manually verify UI.
    Change-Id: I86de66c1c53bf3e98fc9b40e2db4fb6f63ebb585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4412716
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1128990}

--- ash/webui/shortcut_customization_ui/resources/js/shortcut_customization_app.ts
+++ ash/webui/shortcut_customization_ui/resources/js/shortcut_customization_app.ts
@@ -114,6 +114,7 @@ export class ShortcutCustomizationAppElement extends
       // `cros_styles.css` with `theme/colors.css` directly in `index.html`.
       document.querySelector('link[href*=\'cros_styles.css\']')
           ?.setAttribute('href', 'chrome://theme/colors.css?sets=legacy,sys');
+      document.body.classList.add('jelly-enabled');
       startColorChangeUpdater();
     }
 
