9933547b5124508232f09e6e1c0d09e0999dac53
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-04-11 04:45:57 +0000

    [M113] Stop supporting { handleEvent }.
    Make the code aligned with the following specification update:
    https://github.com/w3c/ServiceWorker/pull/1676
    
    With the previous specification and code, event listener vector
    can be modified during the GetEffectiveFunction execution, which may
    bring unexpected vector state.
    
    (cherry picked from commit 5105ce37a6853d52ec97894bf6969b3c29a23afd)
    
    Change-Id: I732c4c9ab2caebc49a7f4ef52640df7b8476d838
    Bug: 1429201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4394402
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1126483}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411225
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#442}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
@@ -2628,7 +2628,7 @@ ServiceWorkerGlobalScope::FetchHandlerType() {
   }
 
   ScriptState* script_state = ScriptController()->GetScriptState();
-  // Do not remove this, |scope| is needed by `GetEffectiveFunction`.
+  // Do not remove this, |scope| is needed by `GetListenerObject`.
   ScriptState::Scope scope(script_state);
 
   // TODO(crbug.com/1349613): revisit the way to implement this.
@@ -2636,8 +2636,8 @@ ServiceWorkerGlobalScope::FetchHandlerType() {
   for (RegisteredEventListener& e : *elv) {
     EventTarget* et = EventTarget::Create(script_state);
     v8::Local<v8::Value> v =
-        To<JSBasedEventListener>(e.Callback())->GetEffectiveFunction(*et);
-    if (!v->IsFunction() ||
+        To<JSBasedEventListener>(e.Callback())->GetListenerObject(*et);
+    if (v.IsEmpty() || !v->IsFunction() ||
         !v.As<v8::Function>()->Experimental_IsNopFunction()) {
       return mojom::blink::ServiceWorkerFetchHandlerType::kNotSkippable;
     }
