cad540d79a25983757499335da16c6610ca8d870
Wayne Jackson Jr <wbjacksonjr@google.com>
2023-04-11 15:58:03 +0000

    [webview][clank] Enable extended keyboard shortcuts by default
    We plan to cherry-pick this into M113 once all the necessary approvals have been granted.
    
    (cherry picked from commit 6f97f49730ac176e45936b8e49433f1ef21d44a1)
    
    Bug: 1429611
    Change-Id: I75313015386bbc67b8b9589f3cc5dc034b369e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4387859
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1125363}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4405841
    Cr-Commit-Position: refs/branch-heads/5672@{#460}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1490,7 +1490,7 @@ BASE_FEATURE(kStylusWritingToInput,
 
 BASE_FEATURE(kAndroidExtendedKeyboardShortcuts,
              "AndroidExtendedKeyboardShortcuts",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kStylusPointerAdjustment,
              "StylusPointerAdjustment",
