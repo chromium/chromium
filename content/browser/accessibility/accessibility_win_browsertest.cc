6dff4a87a3cd4733cedbeb158f3130097679c94e
Lei Zhang <thestig@chromium.org>
2023-04-12 00:18:54 +0000

    Add a TODO for a disabled AccessibilityWinUIABrowserTest test case
    Add a comment to give context as to why the test case is disabled. This
    should have been part of crrev.com/1128878.
    
    Bug: 1432289
    Change-Id: I97cf5d72e1f7092a32a9468d10d8e98159fafa57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416721
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129009}

--- content/browser/accessibility/accessibility_win_browsertest.cc
+++ content/browser/accessibility/accessibility_win_browsertest.cc
@@ -5607,6 +5607,7 @@ IN_PROC_BROWSER_TEST_F(AccessibilityWinUIABrowserTest,
   EXPECT_NE(nullptr, text_pattern_unknown.Get());
 }
 
+// TODO(crbug.com/1432289): Fix this failing test.
 IN_PROC_BROWSER_TEST_F(AccessibilityWinUIABrowserTest,
                        DISABLED_AsyncContentLoadedEventOnDocumentLoad) {
   // Load the page.
