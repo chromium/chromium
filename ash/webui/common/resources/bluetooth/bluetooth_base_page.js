b98c33568dcd7f42811db3adee060d383a15366b
Wes Okuhara <wesokuhara@google.com>
2023-04-12 00:42:54 +0000

    CrOS Bluetooth: Dynamic color for pairing dialog cancel button
    This appears to only apply for the bluetooth pairing dialog
    opened from CrOS Settings.
    Dynamic colors are not yet supported for the dialog if opened from
    Quick Settings (chrome://bluetooth-pairing). This WebUI still needs effort to support dynamic color.
    
    Light mode: http://screen/AW3cCmTKxLSpApN.png
    Dark mode: http://screen/ADoEYVoFAaShYKe.png
    
    Bug: b/277798774, b/274649523
    Change-Id: Ifd0607c8ddcd3f8c60135a531b56ce9d0bbff8d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4415692
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129020}

--- ash/webui/common/resources/bluetooth/bluetooth_base_page.js
+++ ash/webui/common/resources/bluetooth/bluetooth_base_page.js
@@ -11,10 +11,11 @@ import '//resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 import '//resources/polymer/v3_0/paper-progress/paper-progress.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
 
+import {assertNotReached} from '//resources/ash/common/assert.js';
 import {focusWithoutInk} from '//resources/ash/common/focus_without_ink_js.js';
 import {I18nBehavior, I18nBehaviorInterface} from '//resources/ash/common/i18n_behavior.js';
-import {assertNotReached} from '//resources/ash/common/assert.js';
 import {afterNextRender, html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './bluetooth_base_page.html.js';
