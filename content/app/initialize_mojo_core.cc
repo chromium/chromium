cf7e631eccc364155853b260b8193c91cc83cb8c
Mikhail Khokhlov <khokhlov@google.com>
2023-04-12 17:15:51 +0000

    Introduce a feature flag for direct memory allocations on Android
    Sandboxed processes on Android can allocate shared memory directly,
    so hooks are not necessary. We want to test if removing hooks on
    Android can improve performance metrics.
    
    Bug: 1432615
    Change-Id: I0d061e4a2752a640e9d2a9622800a6668ba55cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4418370
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129329}

--- content/app/initialize_mojo_core.cc
+++ content/app/initialize_mojo_core.cc
@@ -13,6 +13,7 @@
 #include "content/public/common/content_switches.h"
 #include "mojo/core/embedder/configuration.h"
 #include "mojo/core/embedder/embedder.h"
+#include "mojo/core/embedder/features.h"
 #include "mojo/public/c/system/functions.h"
 #include "mojo/public/c/system/types.h"
 #include "mojo/public/cpp/base/shared_memory_utils.h"
@@ -55,6 +56,10 @@ void InitializeMojoCore() {
     // On Windows it's not necessary to broker shared memory allocation, as
     // even sandboxed processes can allocate their own without trouble.
     config.force_direct_shared_memory_allocation = true;
+#elif BUILDFLAG(IS_ANDROID)
+    // On Android we run a Finch experiment testing direct memory allocation.
+    config.force_direct_shared_memory_allocation = base::FeatureList::IsEnabled(
+        mojo::core::kMojoDirectSharedMemoryAndroid);
 #endif
   }
 
