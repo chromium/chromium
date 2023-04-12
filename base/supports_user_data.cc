ae276208ab985df76274c7436ab716d235f7f0d5
Daniel Cheng <dcheng@chromium.org>
2023-04-12 04:03:34 +0000

    Guard against reentrancy in SupportsUserData::RemoveUserData().
    Bug: 1414218
    Change-Id: I8a0e97539433c1d86557d2bfa899ea7cfd374412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416617
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129075}

--- base/supports_user_data.cc
+++ base/supports_user_data.cc
@@ -75,7 +75,26 @@ void SupportsUserData::SetUserData(const void* key,
 
 void SupportsUserData::RemoveUserData(const void* key) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  absl::visit([key](auto& map) { map.erase(key); }, user_data_);
+  absl::visit(
+      [key](auto& map) {
+        auto it = map.find(key);
+        if (it != map.end()) {
+          // Remove the entry from the map before deleting `owned_data` to avoid
+          // reentrancy issues when `owned_data` owns `this`. Otherwise:
+          //
+          // 1. `RemoveUserData()` calls `erase()`.
+          // 2. `erase()` deletes `owned_data`.
+          // 3. `owned_data` deletes `this`.
+          //
+          // At this point, `erase()` is still on the stack even though the
+          // backing map (owned by `this`) has already been destroyed, and it
+          // may simply crash, cause a use-after-free, or any other number of
+          // interesting things.
+          auto owned_data = std::move(it->second);
+          map.erase(it);
+        }
+      },
+      user_data_);
 }
 
 void SupportsUserData::DetachFromSequence() {
