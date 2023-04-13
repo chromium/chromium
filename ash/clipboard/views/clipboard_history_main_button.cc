3c7d7d5377dceebfe3c0bade02c7ded4f9cb778e
Colin Kincaid <ckincaid@chromium.org>
2023-04-13 18:32:43 +0000

    multipaste: Update item hover colors for Jelly
    This CL changes the clipboard history item hover color to use Jelly
    colors. The UI this change produces will only exist for the one
    milestone difference between Jelly's launch and the clipboard history
    refresh launch.
    
    Jelly disabled:
    Dark mode before: https://screenshot.googleplex.com/C2uLswJXUwzq7Ke
    Dark mode after: https://screenshot.googleplex.com/9tXcxs7HEtbUrPx
    Light mode before: https://screenshot.googleplex.com/6c2ZozcxneHCfXM
    Light mode after: https://screenshot.googleplex.com/9vyf3dpWYp2x35x
    
    Jelly enabled:
    Dark mode before: https://screenshot.googleplex.com/7tYkYq9zfSZDiTq
    Dark mode after: https://screenshot.googleplex.com/Ad7CoyHF3oLjiis
    Light mode before: https://screenshot.googleplex.com/8zuy8uwBGv6LcQC
    Light mode after: https://screenshot.googleplex.com/47GLENos2Xy9K3P
    
    Bug: b/277946871
    Change-Id: Ib7bc07b337b2ad98ff1318f9cec2b128c4a59ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4419990
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1129987}

--- ash/clipboard/views/clipboard_history_main_button.cc
+++ ash/clipboard/views/clipboard_history_main_button.cc
@@ -6,12 +6,13 @@
 
 #include "ash/clipboard/clipboard_history_util.h"
 #include "ash/clipboard/views/clipboard_history_item_view.h"
-#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/style/scoped_light_mode_as_default.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/style_util.h"
 #include "base/functional/bind.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/color/color_provider.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -112,12 +113,11 @@ void ClipboardHistoryMainButton::PaintButtonContents(gfx::Canvas* canvas) {
   // is not enabled, and light mode is the default color of NativeTheme. If
   // dark/light mode is enabled, the background color of the context menus
   // inside SystemUI will be overridden to align with current system color mode.
-  const SkColor color =
-      features::IsDarkLightModeEnabled()
-          ? GetColorProvider()->GetColor(kColorAshInkDrop)
-          : SkColorSetA(SK_ColorBLACK,
-                        StyleUtil::kLightInkDropOpacity * SK_AlphaOPAQUE);
-  flags.setColor(color);
+  const auto color_id =
+      chromeos::features::IsJellyEnabled()
+          ? static_cast<ui::ColorId>(cros_tokens::kCrosSysHoverOnSubtle)
+          : kColorAshInkDrop;
+  flags.setColor(GetColorProvider()->GetColor(color_id));
   flags.setStyle(cc::PaintFlags::kFill_Style);
   canvas->DrawRect(GetLocalBounds(), flags);
 }
