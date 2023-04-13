8b9d6ddd099f0eb6257943c0a4ad25157b5aef68
Illia Klimov <elklm@google.com>
2023-04-13 09:00:54 +0000

    Do not preignore permission requests if a document was detached
    (cherry picked from commit 55d37438b53f66b039d62e577ce9ad85ec62f337)
    
    Bug: 1426851
    Change-Id: Ia69edd48d7670b5e897e66d459265c2da81e6810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4373799
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1123522}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420233
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5615@{#1247}
    Cr-Branched-From: 9c6408ef696e83a9936b82bbead3d41c93c82ee4-refs/heads/main@{#1109224}

--- components/permissions/permission_request_manager.cc
+++ components/permissions/permission_request_manager.cc
@@ -382,6 +382,10 @@ void PermissionRequestManager::PreemptAndRequeueCurrentRequest() {
   for (auto* current_request : requests_) {
     pending_permission_requests_.Push(current_request);
   }
+
+  // Because the order of the requests is changed, we should not preignore it.
+  preignore_timer_.AbandonAndStop();
+
   requests_.clear();
 }
 
@@ -639,6 +643,11 @@ void PermissionRequestManager::PreIgnoreQuietPrompt() {
 
 void PermissionRequestManager::PreIgnoreQuietPromptInternal() {
   DCHECK(!requests_.empty());
+
+  if (requests_.empty()) {
+    // If `requests_` was cleared then there is nothing preignore.
+    return;
+  }
 
   std::vector<PermissionRequest*>::iterator requests_iter;
   for (requests_iter = requests_.begin(); requests_iter != requests_.end();
@@ -992,6 +1001,11 @@ void PermissionRequestManager::FinalizeCurrentRequests(
     validated_requests_set_.erase(*requests_iter);
     request_sources_map_.erase(*requests_iter);
   }
+
+  // No need to execute the preignore logic as we canceling currently active
+  // requests anyway.
+  preignore_timer_.AbandonAndStop();
+
   requests_.clear();
 
   for (Observer& observer : observer_list_)
