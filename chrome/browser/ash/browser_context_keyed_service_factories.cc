35160d68cf0447426e73d088c9a04cba57e083d1
Maciek Swiech <drmasquatch@google.com>
2023-04-13 15:04:59 +0000

    Reland "Always start SyncAppsyncService"
    This reverts commit 25bb9cfe72abb3acbb942de10d85373bcef068a7.
    
    Initial patch caused a failure in login.ExistingUser due to canonical
    email, this revision should fix.
    
    Bug: chromium:1193485
    Change-Id: If70d083eaa8db2b2fd9e1ab550b1b3926fd717d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4418751
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Maciek Swiech <drmasquatch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1129848}

--- chrome/browser/ash/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/browser_context_keyed_service_factories.cc
@@ -92,6 +92,7 @@
 #include "chrome/browser/ash/remote_apps/remote_apps_manager_factory.h"
 #include "chrome/browser/ash/secure_channel/nearby_connector_factory.h"
 #include "chrome/browser/ash/smb_client/smb_service_factory.h"
+#include "chrome/browser/ash/sync/sync_appsync_service_factory.h"
 #include "chrome/browser/ash/sync/sync_error_notifier_factory.h"
 #include "chrome/browser/ash/sync/sync_mojo_service_factory_ash.h"
 #include "chrome/browser/ash/tether/tether_service_factory.h"
@@ -190,6 +191,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   file_manager::VolumeManagerFactory::GetInstance();
   file_system_provider::ServiceFactory::GetInstance();
   full_restore::FullRestoreServiceFactory::GetInstance();
+  GlanceablesKeyedServiceFactory::GetInstance();
   guest_os::GuestOsMimeTypesServiceFactory::GetInstance();
   guest_os::GuestOsRegistryServiceFactory::GetInstance();
   guest_os::GuestOsServiceFactory::GetInstance();
@@ -220,11 +222,11 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   secure_channel::NearbyConnectorFactory::GetInstance();
   sharesheet::SharesheetServiceFactory::GetInstance();
   smb_client::SmbServiceFactory::GetInstance();
+  SyncAppsyncServiceFactory::GetInstance();
   SyncedPrintersManagerFactory::GetInstance();
   SyncMojoServiceFactoryAsh::GetInstance();
   tether::TetherServiceFactory::GetInstance();
   TtsEngineExtensionObserverChromeOS::EnsureFactoryBuilt();
-  GlanceablesKeyedServiceFactory::GetInstance();
 }
 
 }  // namespace ash
