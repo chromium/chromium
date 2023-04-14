251fd882572b0131edd1ebb58adddfab401b0bb3
Cole Horvitz <colehorvitz@chromium.org>
2023-04-14 18:26:59 +0000

    WebUI: Add minify_js build rule.
    This CL creates a build rule and Python script which minifies JS code.
    Previously, we only had optimize_webui which bundles and minifies code,
    but this rule allows for the minifying of code which does not get
    bundled.
    
    Bug: 1431253
    Change-Id: I1cffd93ebd377830193e5d03c1f686cfcf5f5fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4411870
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130570}

---
+++ ui/webui/resources/tools/tests/minify_js/bar.js
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// comments should be removed
+const foo = {
+    'bar': 0,
+    baz: 5,
+};
+const qux = foo.bar + foo.baz;
---
+++ ui/webui/resources/tools/tests/minify_js/bar_expected.js
@@ -0,0 +1,4 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+const foo={bar:0,baz:5};const qux=foo.bar+foo.baz;
