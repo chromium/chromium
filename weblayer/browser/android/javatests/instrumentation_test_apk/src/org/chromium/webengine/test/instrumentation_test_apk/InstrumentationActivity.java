1e722c684e589f9a0a0f4f17ab491ce158ee160a
Susanne Westphal <swestphal@google.com>
2023-04-14 09:59:10 +0000

    [WebEngine] Use separate Application to run instrumentation tests
    Bug: 1427151
    Change-Id: I96365f09fcbe1b7ca53d1ec4eb80ffda6300b431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4422653
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130353}

--- weblayer/browser/android/javatests/instrumentation_test_apk/src/org/chromium/webengine/test/instrumentation_test_apk/InstrumentationActivity.java
+++ weblayer/browser/android/javatests/instrumentation_test_apk/src/org/chromium/webengine/test/instrumentation_test_apk/InstrumentationActivity.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.webengine.shell;
+package org.chromium.webengine.test.instrumentation_test_apk;
 
 import android.content.Intent;
 import android.os.Bundle;
