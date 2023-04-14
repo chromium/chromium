a5806b237e2ab12a67151a02eafc5d2e4f883d0f
Will Harris <wfh@chromium.org>
2023-04-14 17:33:12 +0000

    Add provider interface to OSCryptAsync.
    Providers are responsible for providing keys to OSCryptAsync
    and those keys end up in the KeyRing for the Encryptor that
    gets returned from OSCryptAsync::GetInstance. Providers supply
    a 'tag' that is stored with any data secured by the provider's
    key, and also a 'precedence' which determines which provider
    is selected as the default one during encryption.
    
    This CL adds the provider interface for async initialization
    of these keys.
    
    This CL also adds the implementation in OSCryptAsync to
    accept these providers at construction time, and then iterate
    over these providers to obtain their keys when GetInstance
    is called.
    
    Tests are added for various types of provider including
    providers with different keys, slow ones and failing ones, to
    verify that OSCryptAsync handles them all correctly.
    
    BUG=1373092
    
    Change-Id: I57a192befbc2cc2f014e8699ef80326f64d0ce77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4348554
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1130541}

--- components/os_crypt/async/browser/os_crypt_async.cc
+++ components/os_crypt/async/browser/os_crypt_async.cc
@@ -4,30 +4,142 @@
 
 #include "components/os_crypt/async/browser/os_crypt_async.h"
 
+#include <memory>
+#include <vector>
+
 #include "base/callback_list.h"
+#include "base/check_op.h"
+#include "base/dcheck_is_on.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/ranges/algorithm.h"
 #include "base/sequence_checker.h"
+#include "components/os_crypt/async/browser/key_provider.h"
 #include "components/os_crypt/async/common/encryptor.h"
 
 namespace os_crypt_async {
 
-OSCryptAsync::OSCryptAsync() = default;
+namespace {
+
+// Sorts `input_providers` by the `Precedence`, from lowest (beginning of
+// vector) to highest (at the end), then returns a new vector that contains just
+// the KeyProvider unique_ptrs in that new order.
+std::vector<std::unique_ptr<KeyProvider>> SortProviders(
+    std::vector<std::pair<OSCryptAsync::Precedence,
+                          std::unique_ptr<KeyProvider>>> input_providers) {
+  std::vector<std::unique_ptr<KeyProvider>> providers;
+  if (input_providers.empty()) {
+    return providers;
+  }
+
+  base::ranges::sort(input_providers, [](const auto& a, const auto& b) {
+    return a.first < b.first;
+  });
+
+#if DCHECK_IS_ON()
+  for (auto it = input_providers.cbegin(); it != input_providers.cend() - 1;
+       ++it) {
+    DCHECK_NE(it->first, (it + 1)->first)
+        << "Cannot have two providers with same precedence.";
+  }
+#endif  // DCHECK_IS_ON()
+
+  base::ranges::transform(std::make_move_iterator(input_providers.begin()),
+                          std::make_move_iterator(input_providers.end()),
+                          std::back_inserter(providers), [](auto provider) {
+                            return std::move(provider.second);
+                          });
+
+  return providers;
+}
+
+}  // namespace
+
+OSCryptAsync::OSCryptAsync(
+    std::vector<std::pair<Precedence, std::unique_ptr<KeyProvider>>> providers)
+    : providers_(SortProviders(std::move(providers))) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (providers_.empty()) {
+    encryptor_instance_ = base::WrapUnique<Encryptor>(new Encryptor());
+    is_initialized_ = true;
+  }
+}
+
 OSCryptAsync::~OSCryptAsync() = default;
 
-OSCryptAsync::OSCryptAsync(OSCryptAsync&& other) = default;
-OSCryptAsync& OSCryptAsync::operator=(OSCryptAsync&& other) = default;
+// CallbackHelper is needed so the sequence checker member can be accessed in
+// the callback, which it can't from a lambda without breaking the
+// sequence_checker abstraction.
+void OSCryptAsync::CallbackHelper(InitCallback callback) const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  std::move(callback).Run(encryptor_instance_->Clone(), /*result=*/true);
+}
+
+void OSCryptAsync::HandleKey(ProviderIterator current,
+                             const std::string& tag,
+                             absl::optional<Encryptor::Key> key) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (key && !tag.empty()) {
+#if DCHECK_IS_ON()
+    for (const auto& [key_name, key_value] : key_ring_) {
+      // Check for overlapping names. Two providers called TEST and TEST2 are
+      // likely incorrectly named, since TEST might try to decrypt TEST2's data.
+      DCHECK(!std::equal(key_name.begin(), key_name.end(), tag.begin()))
+          << "Tags must not overlap.";
+      DCHECK(!std::equal(tag.begin(), tag.end(), key_name.begin()))
+          << "Tags must not overlap.";
+    }
+#endif  // DCHECK_IS_ON()
+    key_ring_.emplace(tag, std::move(*key));
+    if ((*current)->UseForEncryption()) {
+      provider_for_encryption_ = tag;
+    }
+  } else {
+    // TODO(crbug.com/1373092): Return errors back via a callback.
+    LOG(WARNING) << "Provider " << tag << " failed to return a key.";
+  }
+
+  if (++current == providers_.end()) {
+    encryptor_instance_ = base::WrapUnique<Encryptor>(
+        new Encryptor(std::move(key_ring_), provider_for_encryption_));
+    callbacks_.Notify();
+    is_initialized_ = true;
+    is_initializing_ = false;
+    return;
+  }
+
+  (*current)->GetKey(base::BindOnce(&OSCryptAsync::HandleKey,
+                                    weak_factory_.GetWeakPtr(), current));
+}
 
 base::CallbackListSubscription OSCryptAsync::GetInstance(
     InitCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (!is_initialized_) {
-    encryptor_instance_ = base::WrapUnique<Encryptor>(new Encryptor());
-    is_initialized_ = true;
+  if (is_initialized_) {
+    DCHECK(!is_initializing_);
+    std::move(callback).Run(encryptor_instance_->Clone(), /*result=*/true);
+    return base::CallbackListSubscription();
   }
 
-  std::move(callback).Run(encryptor_instance_->Clone(), true);
-  return base::CallbackListSubscription();
+  auto subscription = callbacks_.Add(
+      base::BindOnce(&OSCryptAsync::CallbackHelper, weak_factory_.GetWeakPtr(),
+                     std::move(callback)));
+
+  if (is_initializing_) {
+    return subscription;
+  }
+
+  DCHECK(key_ring_.empty());
+  is_initializing_ = true;
+
+  const ProviderIterator start = providers_.cbegin();
+
+  (*start)->GetKey(base::BindOnce(&OSCryptAsync::HandleKey,
+                                  weak_factory_.GetWeakPtr(), start));
+
+  return subscription;
 }
 
 }  // namespace os_crypt_async
--- components/os_crypt/async/browser/os_crypt_async_unittest.cc
+++ components/os_crypt/async/browser/os_crypt_async_unittest.cc
@@ -4,31 +4,205 @@
 
 #include "components/os_crypt/async/browser/os_crypt_async.h"
 
+#include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
+#include "base/test/gtest_util.h"
 #include "base/test/task_environment.h"
+#include "components/os_crypt/async/browser/key_provider.h"
+#include "components/os_crypt/async/common/algorithm.mojom.h"
 #include "components/os_crypt/async/common/encryptor.h"
+#include "components/os_crypt/sync/os_crypt.h"
+#include "components/os_crypt/sync/os_crypt_mocker.h"
+#include "crypto/hkdf.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace os_crypt_async {
 
 class OSCryptAsyncTest : public ::testing::Test {
  protected:
-  OSCryptAsync factory_;
+  using ProviderList =
+      std::vector<std::pair<size_t, std::unique_ptr<KeyProvider>>>;
+
+  Encryptor GetInstanceSync(OSCryptAsync& factory) {
+    base::RunLoop run_loop;
+    absl::optional<Encryptor> encryptor;
+    auto sub = factory.GetInstance(base::BindLambdaForTesting(
+        [&](Encryptor encryptor_param, bool success) {
+          EXPECT_TRUE(success);
+          encryptor.emplace(std::move(encryptor_param));
+          run_loop.Quit();
+        }));
+    run_loop.Run();
+    return std::move(*encryptor);
+  }
+
+  base::test::TaskEnvironment task_environment_;
+};
+
+class TestKeyProvider : public KeyProvider {
+ public:
+  explicit TestKeyProvider(const std::string& name = "TEST",
+                           bool use_for_encryption = true)
+      : name_(name), use_for_encryption_(use_for_encryption) {}
+
+ protected:
+  Encryptor::Key GenerateKey() {
+    // Make the key derive from the name to ensure different providers have
+    // different keys.
+    std::string key = crypto::HkdfSha256(name_, "salt", "info",
+                                         Encryptor::Key::kAES256GCMKeySize);
+    return Encryptor::Key(std::vector<uint8_t>(key.begin(), key.end()),
+                          mojom::Algorithm::kAES256GCM);
+  }
+
+  const std::string name_;
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  void GetKey(KeyCallback callback) override {
+    std::move(callback).Run(name_, GenerateKey());
+  }
+
+  bool UseForEncryption() override { return use_for_encryption_; }
+
+  const bool use_for_encryption_;
+};
+
+TEST_F(OSCryptAsyncTest, EncryptHeader) {
+  const std::string kTestProviderName("TEST");
+  ProviderList providers;
+  providers.emplace_back(std::make_pair(
+      10u, std::make_unique<TestKeyProvider>(kTestProviderName)));
+  OSCryptAsync factory(std::move(providers));
+  Encryptor encryptor = GetInstanceSync(factory);
+
+  auto ciphertext = encryptor.EncryptString("secrets");
+  ASSERT_TRUE(std::equal(kTestProviderName.cbegin(), kTestProviderName.cend(),
+                         ciphertext->cbegin()));
+}
+
+TEST_F(OSCryptAsyncTest, TwoProvidersBothEnabled) {
+  absl::optional<std::vector<uint8_t>> ciphertext;
+  {
+    const std::string kFooProviderName("FOO");
+    ProviderList providers;
+    providers.emplace_back(
+        /*precedence=*/10u, std::make_unique<TestKeyProvider>(
+                                kFooProviderName, /*use_for_encryption=*/true));
+    providers.emplace_back(
+        /*precedence=*/5u,
+        std::make_unique<TestKeyProvider>("BAR", /*use_for_encryption=*/true));
+    OSCryptAsync factory(std::move(providers));
+    Encryptor encryptor = GetInstanceSync(factory);
+
+    ciphertext = encryptor.EncryptString("secrets");
+    ASSERT_TRUE(ciphertext);
+    // The higher of the two providers should have been picked for data
+    // encryption.
+    EXPECT_TRUE(std::equal(kFooProviderName.cbegin(), kFooProviderName.cend(),
+                           ciphertext->cbegin()));
+  }
+  // Check that provider precedence does not matter for decrypt.
+  {
+    ProviderList providers;
+    providers.emplace_back(
+        /*precedence=*/5u,
+        std::make_unique<TestKeyProvider>("FOO", /*use_for_encryption=*/true));
+    // BAR is the preferred provider since it has the higher precedence.
+    providers.emplace_back(
+        /*precedence=*/10u,
+        std::make_unique<TestKeyProvider>("BAR", /*use_for_encryption=*/true));
+    OSCryptAsync factory(std::move(providers));
+    Encryptor encryptor = GetInstanceSync(factory);
+
+    auto plaintext = encryptor.DecryptData(*ciphertext);
+    // The correct provider based on the encrypted data header should have been
+    // picked for data decryption.
+    ASSERT_TRUE(plaintext);
+    EXPECT_EQ("secrets", *plaintext);
+  }
+}
+
+TEST_F(OSCryptAsyncTest, TwoProvidersOneEnabled) {
+  absl::optional<std::vector<uint8_t>> ciphertext;
+  {
+    const std::string kBarProviderName("BAR");
+    ProviderList providers;
+    providers.emplace_back(
+        /*precedence=*/10u,
+        std::make_unique<TestKeyProvider>("FOO", /*use_for_encryption=*/false));
+    providers.emplace_back(
+        /*precedence=*/5u, std::make_unique<TestKeyProvider>(
+                               kBarProviderName, /*use_for_encryption=*/true));
+    OSCryptAsync factory(std::move(providers));
+    Encryptor encryptor = GetInstanceSync(factory);
+
+    ciphertext = encryptor.EncryptString("secrets");
+    ASSERT_TRUE(ciphertext);
+    // Despite FOO being higher than BAR, BAR is chosen for encryption because
+    // FOO is not enabled for encryption.
+    EXPECT_TRUE(std::equal(kBarProviderName.cbegin(), kBarProviderName.cend(),
+                           ciphertext->cbegin()));
+  }
+  // Check that even with no enabled providers, data can still be decrypted by
+  // any registered provider.
+  {
+    ProviderList providers;
+    // Neither is enabled for encrypt.
+    providers.emplace_back(
+        /*precedence=*/5u,
+        std::make_unique<TestKeyProvider>("FOO", /*use_for_encryption=*/false));
+    providers.emplace_back(
+        /*precedence=*/10u,
+        std::make_unique<TestKeyProvider>("BAR", /*use_for_encryption=*/false));
+    OSCryptAsync factory(std::move(providers));
+    Encryptor encryptor = GetInstanceSync(factory);
+
+    auto plaintext = encryptor.DecryptData(*ciphertext);
+    // The correct provider based on the encrypted data header should have been
+    // picked for data decryption.
+    ASSERT_TRUE(plaintext);
+    EXPECT_EQ("secrets", *plaintext);
+  }
+}
+
+class SlowTestKeyProvider : public TestKeyProvider {
+ public:
+  explicit SlowTestKeyProvider(base::TimeDelta sleep_time)
+      : sleep_time_(sleep_time) {}
+
+ private:
+  void GetKey(KeyCallback callback) override {
+    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(
+            [](KeyCallback callback, Encryptor::Key key,
+               const std::string& name) {
+              std::move(callback).Run(name, std::move(key));
+            },
+            std::move(callback), GenerateKey(), name_),
+        sleep_time_);
+  }
+
+  const base::TimeDelta sleep_time_;
 };
 
 // This test verifies that GetInstanceAsync can correctly handle multiple queued
 // requests for an instance for a slow init.
 TEST_F(OSCryptAsyncTest, MultipleCalls) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<SlowTestKeyProvider>(base::Seconds(1)));
+  OSCryptAsync factory(std::move(providers));
+
   size_t calls = 0;
   const size_t kExpectedCalls = 10;
   base::RunLoop run_loop;
   std::list<base::CallbackListSubscription> subs;
   for (size_t call = 0; call < kExpectedCalls; call++) {
-    subs.push_back(factory_.GetInstance(base::BindLambdaForTesting(
+    subs.push_back(factory.GetInstance(base::BindLambdaForTesting(
         [&calls, &run_loop](Encryptor encryptor, bool success) {
           calls++;
           if (calls == kExpectedCalls) {
@@ -38,6 +212,165 @@ TEST_F(OSCryptAsyncTest, MultipleCalls) {
   }
   run_loop.Run();
   EXPECT_EQ(calls, kExpectedCalls);
+}
+
+// This test verifies that if the subscription from CallbackList moves out of
+// scope, then the callback never occurs.
+TEST_F(OSCryptAsyncTest, SubscriptionCancelled) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<SlowTestKeyProvider>(base::Seconds(1)));
+  OSCryptAsync factory(std::move(providers));
+
+  {
+    auto sub = factory.GetInstance(
+        base::BindOnce([](Encryptor encryptor, bool success) {
+          // This should not be called, as the subscription went out of scope.
+          NOTREACHED();
+        }));
+  }
+
+  // Complete the init on a nested RunLoop.
+  base::RunLoop run_loop;
+  auto sub = factory.GetInstance(
+      base::BindLambdaForTesting([&](Encryptor encryptor_param, bool success) {
+        EXPECT_TRUE(success);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
+
+class FailingKeyProvider : public TestKeyProvider {
+ private:
+  void GetKey(KeyCallback callback) override {
+    std::move(callback).Run("", absl::nullopt);
+  }
+};
+
+// Some tests require a working OSCrypt.
+class OSCryptAsyncTestWithOSCrypt : public OSCryptAsyncTest {
+ protected:
+  void SetUp() override { OSCryptMocker::SetUp(); }
+
+  void TearDown() override {
+    OSCryptMocker::TearDown();
+#if BUILDFLAG(IS_WIN)
+    OSCrypt::ResetStateForTesting();
+#endif  // BUILDFLAG(IS_WIN)
+  }
+};
+
+// This test merely verifies that OSCryptAsync can operate with no key providers
+// and return a valid Encryptor with no keys, and that it can interop with
+// OSCrypt. The rest of the encryption tests for this mode are located in
+// encryptor_unittest.cc.
+TEST_F(OSCryptAsyncTestWithOSCrypt, Empty) {
+  ProviderList providers;
+  OSCryptAsync factory(std::move(providers));
+  Encryptor encryptor = GetInstanceSync(factory);
+  std::string ciphertext;
+  EXPECT_TRUE(OSCrypt::EncryptString("secrets", &ciphertext));
+  std::string plaintext;
+  EXPECT_TRUE(encryptor.DecryptString(ciphertext, &plaintext));
+  EXPECT_EQ("secrets", plaintext);
+}
+
+TEST_F(OSCryptAsyncTestWithOSCrypt, FailingKeyProvider) {
+  ProviderList providers;
+  providers.emplace_back(/*precedence=*/10u,
+                         std::make_unique<FailingKeyProvider>());
+  OSCryptAsync factory(std::move(providers));
+  // TODO: Work out how best to handle provider failures.
+  Encryptor encryptor = GetInstanceSync(factory);
+
+  {
+    // Encryption should still work, because an empty Encryptor is made which
+    // falls back to OSCrypt.
+    auto ciphertext = encryptor.EncryptString("secrets");
+    EXPECT_TRUE(ciphertext);
+    std::string plaintext;
+    EXPECT_TRUE(OSCrypt::DecryptString(
+        std::string(ciphertext->cbegin(), ciphertext->cend()), &plaintext));
+    EXPECT_EQ("secrets", plaintext);
+  }
+  {
+    std::string ciphertext;
+    EXPECT_TRUE(OSCrypt::EncryptString("secrets", &ciphertext));
+    std::string plaintext;
+    // Decryption falls back to OSCrypt if there are no matching providers. In
+    // this case, there are no providers at all.
+    EXPECT_TRUE(encryptor.DecryptString(ciphertext, &plaintext));
+    EXPECT_EQ("secrets", plaintext);
+  }
+}
+
+using OSCryptAsyncDeathTest = OSCryptAsyncTest;
+
+TEST_F(OSCryptAsyncDeathTest, SamePrecedence) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/5u,
+      std::make_unique<TestKeyProvider>("A", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/20u,
+      std::make_unique<TestKeyProvider>("B", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/5u,
+      std::make_unique<TestKeyProvider>("C", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<TestKeyProvider>("D", /*use_for_encryption=*/true));
+  EXPECT_DCHECK_DEATH_WITH({ OSCryptAsync factory(std::move(providers)); },
+                           "Cannot have two providers with same precedence.");
+}
+
+TEST_F(OSCryptAsyncDeathTest, SameName) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/5u,
+      std::make_unique<TestKeyProvider>("TEST", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<TestKeyProvider>("TEST", /*use_for_encryption=*/true));
+  EXPECT_DCHECK_DEATH_WITH(
+      {
+        OSCryptAsync factory(std::move(providers));
+        std::ignore = GetInstanceSync(factory);
+      },
+      "Tags must not overlap.");
+}
+
+TEST_F(OSCryptAsyncDeathTest, OverlappingNames) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/5u,
+      std::make_unique<TestKeyProvider>("TEST", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<TestKeyProvider>("TEST2", /*use_for_encryption=*/true));
+  EXPECT_DCHECK_DEATH_WITH(
+      {
+        OSCryptAsync factory(std::move(providers));
+        std::ignore = GetInstanceSync(factory);
+      },
+      "Tags must not overlap.");
+}
+
+TEST_F(OSCryptAsyncDeathTest, OverlappingNamesBackwards) {
+  ProviderList providers;
+  providers.emplace_back(
+      /*precedence=*/5u,
+      std::make_unique<TestKeyProvider>("TEST2", /*use_for_encryption=*/true));
+  providers.emplace_back(
+      /*precedence=*/10u,
+      std::make_unique<TestKeyProvider>("TEST", /*use_for_encryption=*/true));
+  EXPECT_DCHECK_DEATH_WITH(
+      {
+        OSCryptAsync factory(std::move(providers));
+        std::ignore = GetInstanceSync(factory);
+      },
+      "Tags must not overlap.");
 }
 
 }  // namespace os_crypt_async
