4aba47f57810ad8b968a1759a749bb7eb5630b7c
Dana Fried <dfried@chromium.org>
2023-04-17 18:54:57 +0000

    [m113] Avoid CHECK during IPH launch.
    For some reason, we are failing to retrieve the F6 accelerator from the
    browser on Windows when launching an IPH from the NTP.
    
    This appears to be due to changing a NOTREACHED to a CHECK. This change
    reverts the previous change, making it a NOTREACHED again.
    
    We can look into this in more depth later when it is not holding up a
    release.
    
    (cherry picked from commit 40142ee5d5f4640b43fada9038816f7356b68feb)
    
    Bug: 1432803
    Change-Id: Ia0d63dffd6b33a4360bbda2adc72d209660d2437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4420220
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1130027}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4434669
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#657}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- chrome/browser/ui/views/user_education/browser_feature_promo_controller.cc
+++ chrome/browser/ui/views/user_education/browser_feature_promo_controller.cc
@@ -97,7 +97,17 @@ std::u16string BrowserFeaturePromoController::GetTutorialScreenReaderHint()
 #else
   static const auto kAccelerator = IDC_FOCUS_NEXT_PANE;
 #endif
-  CHECK(browser_view_->GetAccelerator(kAccelerator, &accelerator));
+  if (browser_view_->GetAccelerator(kAccelerator, &accelerator)) {
+    accelerator_text = accelerator.GetShortcutText();
+  } else {
+    // TODO(crbug.com/1432803): GetAccelerator appears to be failing
+    // sporadically on Windows, for unknown reasons. Since we can't have this
+    // code crashing in release, it's being returned to the original NOTREACHED
+    // before everything was changed to CHECKs. This bug will continue to be
+    // researched for a more correct fix.
+    accelerator_text = u"F6";
+    NOTREACHED();
+  }
   return l10n_util::GetStringFUTF16(IDS_FOCUS_HELP_BUBBLE_TUTORIAL_DESCRIPTION,
                                     accelerator.GetShortcutText());
 }
