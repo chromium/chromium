d501ba212f3a0470bedd7646ed884f036700cf7d
Keishi Hattori <keishi@chromium.org>
2023-04-17 11:04:51 +0000

    [BRP] Adjust MemoryReclaimer experiment
    Adjust MemoryReclaimer experiment so that the default group with
    brp-mode/enabled keeps the old behavior of not registering with
    MemoryReclaimer. Setting brp-mode/enabled-with-memory-reclaimer will
    register the main partition with brp to MemoryReclaimer. Setting
    brp-mode/disabled-but-2-way-split-with-memory-reclaimer will just split
    the partition and registers it to MemoryReclaimer.
    
    (cherry picked from commit 7381718ae6b2c3324073aca09985caebefc2437f)
    
    Bug: 1431379
    Change-Id: I75a77732598d37d05aae7d631092e4dfe280c840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4416787
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1129706}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431684
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5672@{#636}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- base/allocator/partition_alloc_features.h
+++ base/allocator/partition_alloc_features.h
@@ -90,13 +90,16 @@ enum class BackupRefPtrMode {
   // Same as kEnabled but without zapping quarantined objects.
   kEnabledWithoutZapping,
 
-  // Same as kEnabled but without registering the main partition to memory
-  // reclaimer.
-  kEnabledWithoutMemoryReclaimer,
+  // Same as kEnabled but registers the main partition to memory reclaimer.
+  kEnabledWithMemoryReclaimer,
 
   // BRP is disabled, but the main partition is split out, as if BRP was enabled
   // in the "previous slot" mode.
   kDisabledButSplitPartitions2Way,
+
+  // Same as kDisabledButSplitPartitions2Way but registers the main partition to
+  // memory reclaimer.
+  kDisabledButSplitPartitions2WayWithMemoryReclaimer,
 
   // BRP is disabled, but the main partition *and* aligned partition are split
   // out, as if BRP was enabled in the "before allocation" mode.
