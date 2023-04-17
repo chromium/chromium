e1225f1bf5d0c48f56e99216de4c4e5ab641ce09
Claudio DeSouza <cdesouza@igalia.com>
2023-04-17 21:00:03 +0000

    Replace base::GUID uses with base::Uuid (114/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /services/service_manager/public/cpp
    
    This CL was uploaded by git cl split.
    
    R=jam@chromium.org
    
    Bug: 1428566
    Change-Id: I2d5704e40c9aae3f994ab8e832ff201a19df5aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4430433
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131493}

--- services/service_manager/public/cpp/test/test_connector_factory.cc
+++ services/service_manager/public/cpp/test/test_connector_factory.cc
@@ -8,7 +8,6 @@
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
