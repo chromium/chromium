2e9a54b68ad63c66c90143dd4a5e9a27606c70bf
Jérôme Lebel <jlebel@chromium.org>
2023-04-17 10:33:53 +0000

    [iOS] Disconnect observers for ClearBrowsingDataTableViewController
    ClearBrowsingDataCoordinator needs to be stopped from
    SettingsNavigationController.
    Need to call -[ClearBrowsingDataTableViewController
    prepareForDismissal], from ClearBrowsingDataCoordinator.
    
    (cherry picked from commit bb4b00e6c1da696f907ab4deed937d80e66c1191)
    
    Fixed: 1429513
    Change-Id: I68c6334f2736344ede1f4f969c9cf980ab826ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4410800
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1128571}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4427808
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#634}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_coordinator.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_coordinator.mm
@@ -57,6 +57,9 @@
 }
 
 - (void)stop {
+  [self.viewController prepareForDismissal];
+  self.viewController.delegate = nil;
+  self.viewController.dispatcher = nil;
   self.viewController = nil;
 }
 
