5c18395d786be6a26cec0118a5a835097a9d1f7b
Chen-Yu Tsai <wenst@chromium.org>
2023-04-18 07:48:57 +0000

    media/gpu/v4l2: Copy V4L2VideoDecoderDelegateH265 from V4L2VideoDecoderDelegateH264
    Use the H.264 V4L2 decoder delegate as a base for developing the H.265
    version. The codec structures and decoding flows are quite similar.
    
    Copy the files, and fix up names and header file name. The rest of the
    code will be fixed up one parameter at a time.
    
    Bug: b:261127540
    TEST=None
    
    Change-Id: Id314cba282cdc900e243ec15c86a31f5eb2dbeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4113218
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1131730}

---
+++ media/gpu/v4l2/v4l2_video_decoder_delegate_h265.h
@@ -0,0 +1,73 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_DELEGATE_H265_H_
+#define MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_DELEGATE_H265_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/memory/scoped_refptr.h"
+#include "media/gpu/h265_decoder.h"
+#include "media/gpu/h265_dpb.h"
+
+namespace media {
+
+class V4L2Device;
+class V4L2DecodeSurface;
+class V4L2DecodeSurfaceHandler;
+struct V4L2VideoDecoderDelegateH265Private;
+
+class V4L2VideoDecoderDelegateH265 : public H265Decoder::H265Accelerator {
+ public:
+  using Status = H265Decoder::H265Accelerator::Status;
+
+  explicit V4L2VideoDecoderDelegateH265(
+      V4L2DecodeSurfaceHandler* surface_handler,
+      V4L2Device* device);
+
+  V4L2VideoDecoderDelegateH265(const V4L2VideoDecoderDelegateH265&) = delete;
+  V4L2VideoDecoderDelegateH265& operator=(const V4L2VideoDecoderDelegateH265&) =
+      delete;
+
+  ~V4L2VideoDecoderDelegateH265() override;
+
+  // H265Decoder::H265Accelerator implementation.
+  scoped_refptr<H265Picture> CreateH265Picture() override;
+  Status SubmitFrameMetadata(const H265SPS* sps,
+                             const H265PPS* pps,
+                             const H265DPB& dpb,
+                             const H265Picture::Vector& ref_pic_listp0,
+                             const H265Picture::Vector& ref_pic_listb0,
+                             const H265Picture::Vector& ref_pic_listb1,
+                             scoped_refptr<H265Picture> pic) override;
+  Status SubmitSlice(const H265PPS* pps,
+                     const H265SliceHeader* slice_hdr,
+                     const H265Picture::Vector& ref_pic_list0,
+                     const H265Picture::Vector& ref_pic_list1,
+                     scoped_refptr<H265Picture> pic,
+                     const uint8_t* data,
+                     size_t size,
+                     const std::vector<SubsampleEntry>& subsamples) override;
+  Status SubmitDecode(scoped_refptr<H265Picture> pic) override;
+  bool OutputPicture(scoped_refptr<H265Picture> pic) override;
+  void Reset() override;
+
+ private:
+  std::vector<scoped_refptr<V4L2DecodeSurface>> H265DPBToV4L2DPB(
+      const H265DPB& dpb);
+  scoped_refptr<V4L2DecodeSurface> H265PictureToV4L2DecodeSurface(
+      H265Picture* pic);
+
+  V4L2DecodeSurfaceHandler* const surface_handler_;
+  V4L2Device* const device_;
+
+  // Contains the kernel-specific structures that we don't want to expose
+  // outside of the compilation unit.
+  const std::unique_ptr<V4L2VideoDecoderDelegateH265Private> priv_;
+};
+
+}  // namespace media
+
+#endif  // MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_DELEGATE_H265_H_
