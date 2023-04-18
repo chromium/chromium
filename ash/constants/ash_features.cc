7947ad70f1a36b003ab91e54f91c2db35317648b
Wen-Chien Wang <wcwang@chromium.org>
2023-04-18 00:58:50 +0000

    Enable folder UI refresh by default
    Bug: b:262003933
    Change-Id: I3d3562f9f488bdb7e2158852b2125a45edfb3865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438091
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Auto-Submit: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131611}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -115,7 +115,7 @@ BASE_FEATURE(kApnRevamp, "ApnRevamp", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAppCollectionFolderRefresh,
              "AppCollectionFolderRefresh",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAppLaunchAutomation,
              "AppLaunchAutomation",
