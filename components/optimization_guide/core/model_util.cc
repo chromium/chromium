340c53e96da223fed6e6f5841db4c7645f50ec5b
Sophie Chang <sophiechang@chromium.org>
2023-04-18 02:22:23 +0000

    Add a new opt target for history clusters module ranking
    Bug: b/276761636
    Change-Id: If60c892327978091d2aefe234d1ea0a2b2db7c41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4438169
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1131646}

--- components/optimization_guide/core/model_util.cc
+++ components/optimization_guide/core/model_util.cc
@@ -96,6 +96,9 @@ std::string GetStringNameForOptimizationTarget(
       return "SegmentationAdaptiveToolbar";
     case proto::OPTIMIZATION_TARGET_SEGMENTATION_TABLET_PRODUCTIVITY_USER:
       return "SegmentationTabletProductivityUser";
+    case proto::
+        OPTIMIZATION_TARGET_NEW_TAB_PAGE_HISTORY_CLUSTERS_MODULE_RANKING:
+      return "NewTabPageHistoryClustersModuleRanking";
       // Whenever a new value is added, make sure to add it to the OptTarget
       // variant list in
       // //tools/metrics/histograms/metadata/optimization/histograms.xml.
