5cc494c6dad9ac3860abfa318f580cab4d7fca3f
Lionel Koenig Gélas <lionelk@google.com>
2023-04-19 12:41:40 +0000

    Revert "Fix thread DCHECK for CrOS amplitude peak detection"
    This reverts commit 2ae18aaeb701d09f23691d6581bf3d3b4da84f7c.
    
    Reason for revert: This is causing crash.
    
    Original change's description:
    > Fix thread DCHECK for CrOS amplitude peak detection
    >
    > This fix the DCHECK on ChromeOS when Tracing audio with amplitude peak
    > detection: The callback are on a different thread for each stream (CRAS
    > client). The solution used is the same as the one used on the Windows
    > Low Latency audio manager.
    >
    > Bug: 1425455
    > Change-Id: I8f615caa7647224ae2678b25936b451d8b62bbc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4432436
    > Commit-Queue: Lionel Koenig Gélas <lionelk@google.com>
    > Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1132082}
    
    Bug: 1425455
    Change-Id: I595bd9ef34a4d366db0ba94d69ddb51d490a6965
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4444633
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Lionel Koenig Gélas <lionelk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132437}

--- media/audio/cras/cras_unified.cc
+++ media/audio/cras/cras_unified.cc
@@ -114,7 +114,10 @@ CrasUnifiedStream::CrasUnifiedStream(
       output_bus_(AudioBus::Create(params)),
       pin_device_(GetDevicePin(manager, device_id)),
       glitch_reporter_(SystemGlitchReporter::StreamType::kRender),
-      log_callback_(std::move(log_callback)) {
+      log_callback_(std::move(log_callback)),
+      peak_detector_(base::BindRepeating(&AudioManager::TraceAmplitudePeak,
+                                         base::Unretained(manager_),
+                                         /*trace_start=*/false)) {
   DCHECK(manager_);
   DCHECK_GT(params_.channels(), 0);
 }
@@ -260,10 +263,6 @@ void CrasUnifiedStream::Start(AudioSourceCallback* callback) {
   // Done with config params.
   libcras_stream_params_destroy(stream_params);
 
-  peak_detector_ = std::make_unique<AmplitudePeakDetector>(base::BindRepeating(
-      &AudioManager::TraceAmplitudePeak, base::Unretained(manager_),
-      /*trace_start=*/false));
-
   is_playing_ = true;
 
   ReportStreamStartResult(StreamStartResult::kCallbackStartSuccess);
@@ -276,8 +275,6 @@ void CrasUnifiedStream::Stop() {
 
   // Removing the stream from the client stops audio.
   libcras_client_rm_stream(client_, stream_id_);
-
-  peak_detector_.reset();
 
   ReportAndResetStats();
 
@@ -338,7 +335,7 @@ uint32_t CrasUnifiedStream::WriteAudio(size_t frames,
       delay, base::TimeTicks::Now(), glitch_info_accumulator_.GetAndReset(),
       output_bus_.get());
 
-  peak_detector_->FindPeak(output_bus_.get());
+  peak_detector_.FindPeak(output_bus_.get());
 
   // Note: If this ever changes to output raw float the data must be clipped and
   // sanitized since it may come from an untrusted source such as NaCl.
