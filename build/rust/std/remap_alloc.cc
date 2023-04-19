095e9d3a3d9c9044382d954e282b5ce6883a8d0d
danakj <danakj@chromium.org>
2023-04-19 17:39:26 +0000

    Ensure allocations go through ParitionAlloc by not calling back to Rust
    The allocation hooks were calling back to the Rust global allocator
    but on Windows, the Rust stdlib implements its own allocator on top
    of the process heap which can't be intercepted by PartitionAlloc.
    
    So, instead, go through the system allocation methods like malloc()
    and friends, mimicing the behaviour of the Rust global allocator on
    unix to handle large alignments.
    
    On Windows, we use `_aligned_malloc()` and `free()` together which is
    incorrect with the Win32 system libs, but is okay with PartitionAlloc
    (and is also what is done in base::AlignedMemory).
    
    Also fix the signature of __rust_dealloc: https://github.com/rust-lang/rust/blob/master/library/alloc/src/alloc.rs#L33
    
    R=lizeb@chromium.org
    
    Bug: 1433352
    Change-Id: I2b034a0c2f39962424cd2b2ff95f3a6e85346e74
    Cq-Include-Trybots: luci.chromium.try:linux-rust-x64-rel,linux-rust-x64-dbg,android-rust-arm64-rel,android-rust-arm64-dbg,android-rust-arm32-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4434189
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132576}

--- build/rust/std/remap_alloc.cc
+++ build/rust/std/remap_alloc.cc
@@ -2,10 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
-#include <stdlib.h>
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
 
+#include "build/build_config.h"
 #include "build/rust/std/immediate_crash.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include <malloc.h>
+#endif
 
 // When linking a final binary, rustc has to pick between either:
 // * The default Rust allocator
@@ -37,16 +44,18 @@
 // We're not (always) using rustc for final linking. For cases where we're not
 // Rustc as the final linker, we'll define those symbols here instead.
 //
-// In future, we may wish to do something different from using the Rust
-// default allocator (e.g. explicitly redirect to PartitionAlloc). We could
-// do that here, or we could build a crate with a #[global_allocator] and
-// redirect these symbols to that crate instead. The advantage of the latter
-// is that it would work equally well for those cases where rustc is doing
-// the final linking. At present, this is not necessary because
-// PartitionAlloc-Everywhere successfully handles the calls to malloc which
-// result from passing through this code. We might want to call into
-// PA directly if we wished for Rust allocations to be in a different
-// partition, or similar, in future.
+// The Rust stdlib on Windows uses GetProcessHeap() which will bypass
+// PartitionAlloc, so we do not forward these functions back to the stdlib.
+// Instead, we pass them to PartitionAlloc, while replicating functionality from
+// the unix stdlib to allow them to provide their increased functionality on top
+// of the system functions.
+//
+// In future, we may build a crate with a #[global_allocator] and
+// redirect these symbols back to Rust in order to use to that crate instead.
+//
+// Instead of going through system functions like malloc() we may want to call
+// into PA directly if we wished for Rust allocations to be in a different
+// partition, or similar, in the future.
 //
 // They're weak symbols, because this file will sometimes end up in targets
 // which are linked by rustc, and thus we would otherwise get duplicate
@@ -55,11 +64,6 @@
 
 extern "C" {
 
-void* __rdl_alloc(size_t, size_t);
-void __rdl_dealloc(void*);
-void* __rdl_realloc(void*, size_t, size_t, size_t);
-void* __rdl_alloc_zeroed(size_t, size_t);
-
 #ifdef COMPONENT_BUILD
 #define REMAP_ALLOC_ATTRIBUTES \
   __attribute__((visibility("default"))) __attribute__((weak))
@@ -67,26 +71,78 @@ void* __rdl_alloc_zeroed(size_t, size_t);
 #define REMAP_ALLOC_ATTRIBUTES __attribute__((weak))
 #endif  // COMPONENT_BUILD
 
-void* REMAP_ALLOC_ATTRIBUTES __rust_alloc(size_t a, size_t b) {
-  return __rdl_alloc(a, b);
+void* REMAP_ALLOC_ATTRIBUTES __rust_alloc(size_t size, size_t align) {
+  // This mirrors kMaxSupportedAlignment from
+  // base/allocator/partition_allocator/partition_alloc_constants.h.
+  // ParitionAlloc will crash if given an alignment larger than this.
+  constexpr size_t max_align = (1 << 21) / 2;
+  if (align > max_align) {
+    return nullptr;
+  }
+
+  if (align <= alignof(std::max_align_t)) {
+    return malloc(size);
+  } else {
+    // Note: PartitionAlloc by default will route aligned allocations back to
+    // malloc() (the fast path) if they are for a small enough alignment. So we
+    // just unconditionally use aligned allocation functions here.
+    // https://source.chromium.org/chromium/chromium/src/+/refs/heads/main:base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.cc;l=219-226;drc=31d99ff4aa0cc0b75063325ff243e911516a5a6a
+
+#if defined(COMPILER_MSVC)
+    // Because we use PartitionAlloc() as the allocator, free() is able to find
+    // this allocation, instead of the usual requirement to use _aligned_free().
+    return _aligned_malloc(size, align);
+#elif BUILDFLAG(IS_ANDROID)
+    // Android has no posix_memalign() exposed:
+    // https://source.chromium.org/chromium/chromium/src/+/main:base/memory/aligned_memory.cc;l=24-30;drc=e4622aaeccea84652488d1822c28c78b7115684f
+    return memalign(align, size);
+#else
+    // The `align` from Rust is always a power of 2:
+    // https://doc.rust-lang.org/std/alloc/struct.Layout.html#method.from_size_align.
+    //
+    // We get here only if align > alignof(max_align_t), which guarantees that
+    // the alignment is both a power of 2 and even, which is required by
+    // posix_memalign().
+    //
+    // The PartitionAlloc impl requires that the alignment is at least the same
+    // as pointer-alignment. std::max_align_t is at least pointer-aligned as
+    // well, so we satisfy that.
+    void* p;
+    auto ret = posix_memalign(&p, align, size);
+    return ret == 0 ? p : nullptr;
+#endif
+  }
 }
 
-void REMAP_ALLOC_ATTRIBUTES __rust_dealloc(void* a) {
-  __rdl_dealloc(a);
+void REMAP_ALLOC_ATTRIBUTES __rust_dealloc(void* p, size_t size, size_t align) {
+  free(p);
 }
 
-void* REMAP_ALLOC_ATTRIBUTES __rust_realloc(void* a,
-                                            size_t b,
-                                            size_t c,
-                                            size_t d) {
-  return __rdl_realloc(a, b, c, d);
+void* REMAP_ALLOC_ATTRIBUTES __rust_realloc(void* p,
+                                            size_t old_size,
+                                            size_t align,
+                                            size_t new_size) {
+  if (align <= alignof(std::max_align_t)) {
+    return realloc(p, new_size);
+  } else {
+    void* out = __rust_alloc(align, new_size);
+    memcpy(out, p, std::min(old_size, new_size));
+    return out;
+  }
 }
 
-void* REMAP_ALLOC_ATTRIBUTES __rust_alloc_zeroed(size_t a, size_t b) {
-  return __rdl_alloc_zeroed(a, b);
+void* REMAP_ALLOC_ATTRIBUTES __rust_alloc_zeroed(size_t size, size_t align) {
+  if (align <= alignof(std::max_align_t)) {
+    return calloc(size, 1);
+  } else {
+    void* p = __rust_alloc(size, align);
+    memset(p, 0, size);
+    return p;
+  }
 }
 
-void REMAP_ALLOC_ATTRIBUTES __rust_alloc_error_handler(size_t a, size_t b) {
+void REMAP_ALLOC_ATTRIBUTES __rust_alloc_error_handler(size_t size,
+                                                       size_t align) {
   IMMEDIATE_CRASH();
 }
 
