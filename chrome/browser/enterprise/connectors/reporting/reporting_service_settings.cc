7a12fc504ca653ad919a0b57f3bf82b803dc58ec
Nasser Al-shawwa <alshawwa@chromium.org>
2023-04-19 23:46:14 +0000

    Enable BrowserCrashEventsEnabled by default
    Bug: b/274807136
    Change-Id: Icf156a39a2fce24d209a13fb6e179a9436b68297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450408
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132789}

--- chrome/browser/enterprise/connectors/reporting/reporting_service_settings.cc
+++ chrome/browser/enterprise/connectors/reporting/reporting_service_settings.cc
@@ -16,7 +16,7 @@ BASE_FEATURE(kExtensionEventsEnabled,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kBrowserCrashEventsEnabled,
              "BrowserCrashEventsEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 namespace {
 
 constexpr char kReportingConnectorUrlFlag[] = "reporting-connector-url";
