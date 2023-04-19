2b054d07cc52004156b538cfb074617d73c01480
Zoraiz Naeem <zoraiznaeem@chromium.org>
2023-04-19 01:35:13 +0000

    [Rounded-Display] Updates multiple overlays logic for rounded-displays.
    This CL updates the multiple overlay processor to handle candidates
    with rounded-display mask textures.
    
    Currently the overlay processor will reject the candidates with
    rounded-display masks as these textures have no damage over consecutive
    frames therefore it is less power efficient to promote these textures.
    Candidates with rounded-display masks are given a special priority in
    the system that promote them for correctness rather than efficient.
    
    This CL also adds fallback strategies in case candidates with
    rounded-display mask textures fail DRM page-flip test.
    
    Bug: 1262970
    Test: Covered by unittests
    Change-Id: Ia99fec6932b2f639fc3c13b5df30d7d6d63fb5f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4285130
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Kevin Haslett <khaslett@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132281}

--- components/viz/service/display/overlay_strategy_single_on_top.cc
+++ components/viz/service/display/overlay_strategy_single_on_top.cc
@@ -20,21 +20,27 @@ namespace {
 using OverlayProposedCandidateIndex =
     std::vector<OverlayProposedCandidate>::size_type;
 
-// Returns true if `rounded_corner_candidate` occludes any SingleOnTop
-// candidates in the list between `single_on_top_candidates_start` (inclusively)
-// and `single_on_top_candidates_end` (exclusively).
-bool OccludesAnyOtherSingleOnTopOverlayCandidate(
-    const OverlayProposedCandidate& rounded_corner_candidate,
-    const std::vector<OverlayProposedCandidate>* candidates,
+// Calculates and caches for the candidates in the list between
+// `single_on_top_candidates_start` (inclusively) and
+// `single_on_top_candidates_end` (exclusively) if they are occluded by
+// `candidate_with_display_masks`.
+//
+// Returns true if `candidate_with_display_masks` does occlude any other
+// candidate.
+bool CalculateOcclusionByRoundedDisplayMaskCandidate(
+    OverlayProposedCandidate& candidate_with_display_masks,
+    std::vector<OverlayProposedCandidate>* candidates,
     OverlayProposedCandidateIndex single_on_top_candidates_begin,
     OverlayProposedCandidateIndex single_on_top_candidates_end) {
-  DCHECK(rounded_corner_candidate.candidate.has_rounded_display_masks);
+  DCHECK(candidate_with_display_masks.candidate.has_rounded_display_masks);
 
   auto mask_bounds = OverlayProposedCandidate::GetRoundedDisplayMasksBounds(
-      rounded_corner_candidate);
+      candidate_with_display_masks);
+
+  bool intersects_candidate = false;
   for (OverlayProposedCandidateIndex i = single_on_top_candidates_begin;
        i < single_on_top_candidates_end; i++) {
-    const auto& overlap_candidate = candidates->at(i);
+    auto& overlap_candidate = candidates->at(i);
 
     // The rects are rounded as they're snapped by the compositor to pixel
     // unless it is AA'ed, in which case, it won't be overlaid.
@@ -47,12 +53,18 @@ bool OccludesAnyOtherSingleOnTopOverlayCandidate(
     // masks.
     for (const gfx::Rect& mask_bound : mask_bounds) {
       if (mask_bound.Intersects(overlap_rect)) {
-        return true;
+        intersects_candidate = true;
+
+        // Cache the occluding `candidate_with_display_masks` for the
+        // overlap_candidate.
+        overlap_candidate.occluding_mask_keys.insert(
+            OverlayProposedCandidate::ToProposeKey(
+                candidate_with_display_masks));
       }
     }
   }
 
-  return false;
+  return intersects_candidate;
 }
 
 }  // namespace
@@ -96,6 +108,10 @@ void OverlayStrategySingleOnTop::Propose(
     if (candidate_factory.FromDrawQuad(*quads_with_masks_iter, candidate) ==
             OverlayCandidate::CandidateStatus::kSuccess &&
         !candidate.has_mask_filter && candidate.has_rounded_display_masks) {
+      // Candidates with rounded-display masks should not overlap any other quad
+      // with rounded-display masks.
+      DCHECK(!candidate_factory.IsOccluded(candidate, quad_list->begin(),
+                                           quads_with_masks_iter));
       candidates_with_masks.emplace_back(quads_with_masks_iter, candidate,
                                          this);
       quads_with_masks_iter++;
@@ -129,7 +145,9 @@ void OverlayStrategySingleOnTop::Propose(
   // To save power we can skip promoting candidates with rounded display mask
   // rects if they do not occlude any other SingleOnTop candidate.
   for (auto& mask_candidate : candidates_with_masks) {
-    if (OccludesAnyOtherSingleOnTopOverlayCandidate(
+    // We mark all the SingleOnTop candidates that are occluded by any mask
+    // rounded-display masks to be later used in overlay processing.
+    if (CalculateOcclusionByRoundedDisplayMaskCandidate(
             mask_candidate, candidates, single_on_top_candidates_begin,
             candidates->size())) {
       candidates->push_back(mask_candidate);
