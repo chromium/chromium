b4477af787c4d2ecd5694466f00e81ad3176879b
Wes Okuhara <wesokuhara@google.com>
2023-04-19 23:27:54 +0000

    CrOS Settings: Dynamic color support for nearby onboarding dialog
    Also updates the Device visibility button to use icon slots.
    
    Light mode: http://screen/5uozD6tFmDNQaMk.png
    Dark mode: http://screen/9Wk6N7RojwFQ6PU.png
    
    Bug: b/277798508
    Change-Id: Ib16a0daf94a2dcb878a140d20ea28738c60a62f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443040
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Pu Shi <pushi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1132776}

--- chrome/browser/resources/nearby_share/shared/nearby_onboarding_one_page.ts
+++ chrome/browser/resources/nearby_share/shared/nearby_onboarding_one_page.ts
@@ -15,6 +15,10 @@ import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import './nearby_page_template.js';
+// <if expr='chromeos_ash'>
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.css.js';
+
+// </if>
 
 import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
