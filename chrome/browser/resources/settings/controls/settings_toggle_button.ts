af72d48c8e2ce6ac63236f3e8711f336546bd9f6
dpapad <dpapad@chromium.org>
2023-04-19 17:49:06 +0000

    Settings: Remove settings-control-label-spacing from settings_vars.css
    This CSS variable was only used once in CrOS Settings and once in
    Browser Settings, not enough times to justify the abstraction or
    establish a pattern.
    
    With this removal c/b/r/settings/controls/ no longer depends on
    settings_vars.css at all, and can now more easily be moved to the
    "limited sharing" mechanism at c/b/r/settings_shared/.
    
    Bug: 1431477
    Change-Id: I66dd467195cf7cad0e0b91834030a4956d5021af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4442829
    Reviewed-by: Cole Horvitz <colehorvitz@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132581}

--- chrome/browser/resources/settings/controls/settings_toggle_button.ts
+++ chrome/browser/resources/settings/controls/settings_toggle_button.ts
@@ -12,7 +12,6 @@ import '//resources/cr_elements/action_link.css.js';
 import '//resources/cr_elements/cr_toggle/cr_toggle.js';
 import '//resources/cr_elements/policy/cr_policy_pref_indicator.js';
 import '//resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
-import '../settings_vars.css.js';
 
 import {CrToggleElement} from '//resources/cr_elements/cr_toggle/cr_toggle.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
