97ff14561025aeae95aab2caf6e10673c477ab1f
David Bienvenu <davidbienvenu@chromium.org>
2023-04-19 14:49:27 +0000

    win: skip views_examples_unittests if host is in dark mode
    views_examples_unittests fail if the host is in Windows dark mode,
    because the golden files are for light mode. This CL makes us skip
    the test if the native theme is in dark mode.
    
    Bug: 1434470
    Change-Id: Idd87ec1dfbf114ec51b03f52340399c79eeb221f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4443439
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1132498}

--- ui/views/examples/examples_unittest.cc
+++ ui/views/examples/examples_unittest.cc
@@ -7,9 +7,18 @@
 #include "ui/views/examples/examples_exit_code.h"
 #include "ui/views/examples/examples_main_proc.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "ui/native_theme/native_theme_win.h"
+#endif
+
 namespace views::examples {
 
 TEST(ExamplesTest, TestViewsExamplesLaunches) {
+#if BUILDFLAG(IS_WIN)
+  if (ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()) {
+    GTEST_SKIP() << "Host is in dark mode; skipping test";
+  }
+#endif
   const ExamplesExitCode exit_code = ExamplesMainProc(true);
   // Check the status of the Skia Gold comparison.
   EXPECT_EQ(ExamplesExitCode::kSucceeded, exit_code);
