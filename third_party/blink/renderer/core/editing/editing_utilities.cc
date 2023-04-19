a6e3e44cbd37018f09c39dd67a1e81cbe3833e40
Koji Ishii <kojii@chromium.org>
2023-04-19 13:52:37 +0000

    [M113] Revert "[balance-text] Replace `WhiteSpace()` in `IsTabHTMLSpanElement`"
    This reverts commit a689d7b6339bcc78fad9709380e4d45de4945e80.
    
    Reason for revert: crbug.com/1431935
    
    `IsTabHTMLSpanElement` tries to detect spans created by
    `CreateTabSpanElement`, which sets `white-space:pre`[1].
    
    The CL changed it to include `pre-wrap` as well, which caused
    a regression in crbug.com/1431935.
    
    Adding `&& !style.ShouldWrapLine()` can also fix, but this
    code is really about detecting an artificial span
    `CreateTabSpanElement` created, so making it semantics doesn't
    have much meanings. Simple revert should be fine and safe.
    
    At the point of the CL, the `WhiteSpace()` function was likely
    to be slower, but further development made it not slow, so
    using `WhiteSpace()` is ok, as long as it doesn't have to
    worry about when `text-wrap` is set to new values.
    
    Also note that the original logic can misdetect spans created
    by authors or JS. crbug.com/1431935#c5 indicates editing JS
    libraries have implemented browser-specific behaviors that
    keeping the original behavior looks more important than making
    the detection more correct.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/editing/editing_utilities.cc;l=1149?q=CreateTabSpanElement&ss=chromium
    
    
    Original change's description:
    > [balance-text] Replace `WhiteSpace()` in `IsTabHTMLSpanElement`
    >
    > This patch replaces `WhiteSpace() == Pre` in
    > `IsTabHTMLSpanElement` with more semantical function
    > `IsCollapsibleWhiteSpace` to prepare for refactoring the
    > `white-space` property to a shorthand.
    >
    > This code was added in crrev.com/c/697044 to check if a TAB
    > character is preserved. As part of the efforts to change the
    > `white-space` property to a shorthand, this change eliminates
    > the direct access to the `WhiteSpace()` property.
    >
    > This change will apply the fix in crrev.com/c/697044 not only
    > to `pre` but also to `pre-wrap` and `break-spaces` where TAB
    > characters are also preserved.
    >
    > Bug: 1251079
    > Change-Id: I26c94f93800471803a285383ca6ca520740a12e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4233237
    > Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1103125}
    
    (cherry picked from commit 50a582ed9edec30160ad2e6567f8c070246da387)
    
    Bug: 1431935, 1251079
    Change-Id: Ica0a13d863a5a3f1feb23cddeffc74abe809649b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4439170
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1131740}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4445228
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#719}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- third_party/blink/renderer/core/editing/editing_utilities.cc
+++ third_party/blink/renderer/core/editing/editing_utilities.cc
@@ -1129,7 +1129,7 @@ bool IsTabHTMLSpanElement(const Node* node) {
   // See crbug.com/590369 for details.
   node->GetDocument().UpdateStyleAndLayoutTree();
   const ComputedStyle* style = node->GetComputedStyle();
-  return style && !style->IsCollapsibleWhiteSpace('\t');
+  return style && style->WhiteSpace() == EWhiteSpace::kPre;
 }
 
 bool IsTabHTMLSpanElementTextNode(const Node* node) {
