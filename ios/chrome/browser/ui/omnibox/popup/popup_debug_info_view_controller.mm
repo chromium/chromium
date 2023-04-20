4ff2d3b305aa2bb8c44096595840de3541739f53
Christian Xu <christianxu@google.com>
2023-04-20 14:36:15 +0000

    [iOS][Omnibox][DebugUI] Add force variation id helper
    Add a text field and text views in the debug info view to generate
    strings needed to force a variation id.
    
    Demo:
    https://drive.google.com/file/d/1KmFIm8yfWQUJt_TSZQCMFJqsGliZetJs/view?usp=sharing
    
    Bug: none
    Change-Id: I7817a9895feb18896db295d760ad5cfdac5e356a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4440928
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133110}

--- ios/chrome/browser/ui/omnibox/popup/popup_debug_info_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/popup_debug_info_view_controller.mm
@@ -3,15 +3,92 @@
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/omnibox/popup/popup_debug_info_view_controller.h"
+#import "components/variations/variations_switches.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@interface PopupDebugInfoViewController ()
+namespace {
 
-@property(nonatomic, strong) UITextView* variationIDTextView;
+/// Debug text view used to display text that can be selected.
+UITextView* DebugTextView() {
+  UITextView* textView = [[UITextView alloc] init];
+  textView.translatesAutoresizingMaskIntoConstraints = NO;
+  textView.editable = NO;
+  textView.scrollEnabled = NO;
+  textView.font = [UIFont systemFontOfSize:15];
+  return textView;
+}
+
+/// Text field used to input variation id.
+UITextField* ForceVariationTextField() {
+  UITextField* textField = [[UITextField alloc] init];
+  textField.translatesAutoresizingMaskIntoConstraints = NO;
+  textField.borderStyle = UITextBorderStyleBezel;
+  textField.backgroundColor = UIColor.lightGrayColor;
+  textField.keyboardType = UIKeyboardTypeNumberPad;
+  textField.placeholder = @"Force variation ID";
+  return textField;
+}
+
+/// Stack view containing variation id information.
+UIStackView* VariationStackView() {
+  UIStackView* stackView = [[UIStackView alloc] init];
+  stackView.translatesAutoresizingMaskIntoConstraints = NO;
+  stackView.axis = UILayoutConstraintAxisVertical;
+  stackView.distribution = UIStackViewDistributionFill;
+  stackView.spacing = 10;
+  return stackView;
+}
+
+/// Button to open app settings.
+UIButton* SettingsButton() {
+  UIAction* openSettings = [UIAction actionWithHandler:^(UIAction* action) {
+    NSURL* url =
+        [[NSURL alloc] initWithString:UIApplicationOpenSettingsURLString];
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:nil];
+  }];
+  UIButtonConfiguration* configuration =
+      [UIButtonConfiguration grayButtonConfiguration];
+  configuration.title = @"Open iOS Settings";
+  UIImageSymbolConfiguration* config = [UIImageSymbolConfiguration
+      configurationWithWeight:UIImageSymbolWeightLight];
+  configuration.image = DefaultSymbolWithConfiguration(@"gear.circle", config);
+  UIButton* button = [UIButton buttonWithConfiguration:configuration
+                                         primaryAction:openSettings];
+  button.translatesAutoresizingMaskIntoConstraints = NO;
+  return button;
+}
+
+/// Label showing instrunction to force variation Id.
+UILabel* VariationInstructionLabel() {
+  UILabel* label = [[UILabel alloc] init];
+  label.translatesAutoresizingMaskIntoConstraints = NO;
+  label.numberOfLines = 0;
+  label.text = @"Copy these in iOS Settings > Experimental Settings > EXTRA "
+               @"FLAGS (ONE PER LINE)";
+  return label;
+}
+
+}  // namespace
+
+@interface PopupDebugInfoViewController () <UITextFieldDelegate>
+
+@property(nonatomic, strong) UITextView* activeVariationIDTextView;
+@property(nonatomic, strong) UITextField* variationIDTextField;
+@property(nonatomic, strong) UITextView* enableVariationIDTextView;
+@property(nonatomic, strong) UITextView* disableVariationIDsTextView;
+
+@property(nonatomic, strong) UILabel* variationInstructionLabel;
+@property(nonatomic, strong) UIButton* settingsButton;
+
+@property(nonatomic, strong) NSArray<NSNumber*>* activeVariationIDs;
+@property(nonatomic, strong) UIStackView* variationStackView;
 
 @end
 
@@ -19,7 +96,19 @@
 
 - (instancetype)init {
   if (self = [super initWithNibName:nil bundle:nil]) {
-    self.variationIDTextView = [[UITextView alloc] init];
+    _activeVariationIDs = @[];
+    _variationStackView = VariationStackView();
+    _activeVariationIDTextView = DebugTextView();
+    _variationIDTextField = ForceVariationTextField();
+    _variationIDTextField.delegate = self;
+    _settingsButton = SettingsButton();
+    _variationInstructionLabel = VariationInstructionLabel();
+    _enableVariationIDTextView = DebugTextView();
+    _disableVariationIDsTextView = DebugTextView();
+
+    [_variationIDTextField addTarget:self
+                              action:@selector(textFieldDidChange:)
+                    forControlEvents:UIControlEventEditingChanged];
   }
   return self;
 }
@@ -27,10 +116,26 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.variationIDTextView.editable = NO;
-  self.variationIDTextView.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.variationIDTextView];
-  AddSameConstraints(self.view, self.variationIDTextView);
+  UIScrollView* scrollView = [[UIScrollView alloc] init];
+  scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+  scrollView.backgroundColor = UIColor.systemBackgroundColor;
+  [self.view addSubview:scrollView];
+  AddSameConstraints(self.view, scrollView);
+
+  UIStackView* stackView = self.variationStackView;
+  [scrollView addSubview:stackView];
+  AddSameConstraints(stackView, scrollView);
+  [NSLayoutConstraint activateConstraints:@[
+    [scrollView.widthAnchor constraintEqualToAnchor:stackView.widthAnchor],
+    [scrollView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor],
+  ]];
+
+  [stackView addArrangedSubview:self.activeVariationIDTextView];
+  [stackView addArrangedSubview:self.variationIDTextField];
+  [stackView addArrangedSubview:self.settingsButton];
+  [stackView addArrangedSubview:self.variationInstructionLabel];
+  [stackView addArrangedSubview:self.enableVariationIDTextView];
+  [stackView addArrangedSubview:self.disableVariationIDsTextView];
 
   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]
       initWithBarButtonSystemItem:UIBarButtonSystemItemDone
@@ -38,11 +143,55 @@
                            action:@selector(doneButtonPressed)];
 }
 
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textField:(UITextField*)textField
+    shouldChangeCharactersInRange:(NSRange)range
+                replacementString:(NSString*)string {
+  if (textField == self.variationIDTextField) {
+    if (!string.length) {
+      return YES;
+    }
+    NSCharacterSet* nonDecimalDigit =
+        [NSCharacterSet decimalDigitCharacterSet].invertedSet;
+    if ([string rangeOfCharacterFromSet:nonDecimalDigit].location ==
+        NSNotFound) {
+      return YES;
+    }
+
+    // Allow pasting variation ids with a leading t.
+    if ([string characterAtIndex:0] == 't') {
+      NSString* substring = [string substringFromIndex:1];
+      if ([substring intValue]) {
+        textField.text = substring;
+      }
+    }
+    return NO;
+  }
+  return YES;
+}
+
+- (void)textFieldDidChange:(id)sender {
+  [self updateForceVariationTextViews];
+}
+
 #pragma mark - PopupDebugInfoConsumer
 
 - (void)setVariationIDString:(NSString*)string {
-  self.variationIDTextView.text =
-      [NSString stringWithFormat:@"Variation IDs:%@", string];
+  NSCharacterSet* whitespaceSet =
+      [NSCharacterSet whitespaceAndNewlineCharacterSet];
+  NSString* trimmedString =
+      [string stringByTrimmingCharactersInSet:whitespaceSet];
+
+  self.activeVariationIDTextView.text =
+      [NSString stringWithFormat:@"Active variation IDs: %@", trimmedString];
+
+  NSArray<NSString*>* stringIds =
+      [trimmedString componentsSeparatedByString:@" "];
+  self.activeVariationIDs = [[stringIds valueForKey:@"intValue"]
+      filteredArrayUsingPredicate:[NSPredicate
+                                      predicateWithFormat:@"SELF != 0"]];
+  [self updateForceVariationTextViews];
 }
 
 #pragma mark - AutocompleteControllerObserver
@@ -74,6 +223,41 @@
 - (void)doneButtonPressed {
   [self.presentingViewController dismissViewControllerAnimated:YES
                                                     completion:nil];
+}
+
+- (void)updateForceVariationTextViews {
+  NSInteger forcedId = [self.variationIDTextField.text intValue];
+  NSString* enabledVariationString = @"";
+  NSString* disabledVariationString = @"";
+
+  if (forcedId > 0) {
+    enabledVariationString = [NSString
+        stringWithFormat:@"%s=t%ld", variations::switches::kForceVariationIds,
+                         forcedId];
+
+    NSArray* disabledIds = [self.activeVariationIDs
+        filteredArrayUsingPredicate:
+            [NSPredicate
+                predicateWithFormat:@"SELF != %@",
+                                    [NSNumber numberWithInteger:forcedId]]];
+    if (disabledIds.count) {
+      disabledVariationString = [NSString
+          stringWithFormat:@"%s=t%@",
+                           variations::switches::kForceDisableVariationIds,
+                           [disabledIds componentsJoinedByString:@",t"]];
+    }
+  }
+
+  self.enableVariationIDTextView.text = enabledVariationString;
+  self.disableVariationIDsTextView.text = disabledVariationString;
+  self.enableVariationIDTextView.hidden =
+      !self.enableVariationIDTextView.text.length;
+  self.disableVariationIDsTextView.hidden =
+      !self.disableVariationIDsTextView.text.length;
+  self.variationInstructionLabel.hidden =
+      self.enableVariationIDTextView.hidden &&
+      self.disableVariationIDsTextView.hidden;
+  self.settingsButton.hidden = self.variationInstructionLabel.hidden;
 }
 
 @end
