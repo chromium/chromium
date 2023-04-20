ad7a0ca590823b504728566c18b9a7db2bf99648
Jérôme Lebel <jlebel@chromium.org>
2023-04-20 14:41:26 +0000

    [iOS][Bkmrk] Move promo code into i/c/b/ui/b/home
    Moving:
    i/c/b/u/bookmarks/{ => home}/bookmark_promo_controller.h
    i/c/b/u/bookmarks/{ => home}/bookmark_promo_controller.mm
    i/c/b/u/bookmarks/{ => home}/bookmarks_account_storage_promo_egtest.mm
    i/c/b/u/bookmarks/{ => home}/bookmarks_promo_egtest.mm
    
    The promo is only needed by bookmark home.
    
    No behaviour changes.
    
    Change-Id: I39e4b18b4a8aa76932cbc8bff26cc621c7aee5ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4447963
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133114}

--- ios/chrome/browser/ui/bookmarks/home/bookmark_promo_controller.mm
+++ ios/chrome/browser/ui/bookmarks/home/bookmark_promo_controller.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/bookmarks/bookmark_promo_controller.h"
+#import "ios/chrome/browser/ui/bookmarks/home/bookmark_promo_controller.h"
 
 #import <memory>
 
