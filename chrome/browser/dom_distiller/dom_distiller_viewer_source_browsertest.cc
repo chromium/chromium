ef1a63f8ac7ae9eb1ae61e4949c73e4a8ed741d6
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 20:03:36 +0000

    Replace base::GUID uses with base::Uuid (143/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/dom_distiller
    
    This CL was uploaded by git cl split.
    
    R=mdjones@chromium.org
    
    Bug: 1428566
    Change-Id: I3ef5fadca7a02c5eb7f3acd5f8cd96c4c8367ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455075
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1133318}

--- chrome/browser/dom_distiller/dom_distiller_viewer_source_browsertest.cc
+++ chrome/browser/dom_distiller/dom_distiller_viewer_source_browsertest.cc
@@ -9,7 +9,6 @@
 
 #include "base/command_line.h"
 #include "base/functional/bind.h"
-#include "base/guid.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
