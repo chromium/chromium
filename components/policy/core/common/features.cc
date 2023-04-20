75e6975ddfe28cc5b103b26aec144e23e286193f
Salma Elmahallawy <esalma@google.com>
2023-04-20 17:19:30 +0000

    Reland "[PolicyLogs] Enabling Android Policy Logs By Default"
    This is a reland of coomit c8a426a55d61aa4610848780c06c00b794842f5f
    
    The issue was fixed by crrev.com/c/4441968.
    
    Bug: b/251799119
    Change-Id: I9e6466cf23a07274b9f925ee34703539b22c31ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450270
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133190}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -37,7 +37,7 @@ BASE_FEATURE(kPolicyLogsPageAndroid,
 
 BASE_FEATURE(kSafeSitesFilterBehaviorPolicyAndroid,
              "SafeSitesFilterBehaviorPolicyAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE(kPolicyMergeMultiSource,
