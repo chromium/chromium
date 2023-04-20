d3b5441b64853d4aeaa5307d17dc76170a0a433d
Claudio DeSouza <cdesouza@igalia.com>
2023-04-20 23:17:45 +0000

    Replace base::GUID uses with base::Uuid (158/N)
    `base::GUID` has been renamed, and is set to be  removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also also having
    their uses removed wherever they occur.
    
    Changes for path: /chrome/browser/policy
    
    This CL was uploaded by git cl split.
    
    R=ftirelo@chromium.org
    
    Bug: 1428566
    Change-Id: I470240cdee5f4a9742ca612f302a3e4342a1fb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4454363
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133446}

--- chrome/browser/policy/device_account_initializer.cc
+++ chrome/browser/policy/device_account_initializer.cc
@@ -10,7 +10,6 @@
 #include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "base/guid.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/task/sequenced_task_runner.h"
