e1e0394f5bebf7a9e10050a40ca717b830f83130
“Hailey <haileywang@google.com>
2023-04-20 00:02:24 +0000

    [Sheriff] Disable flaky test in Omnibox
    Bug: 1434836
    Change-Id: I80ed0657630732b9e744c4ff761b093b0ea2ee59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4450215
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1132804}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsTest.java
@@ -33,6 +33,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.browser.autofill.settings.AutofillPaymentMethodsFragment;
 import org.chromium.chrome.browser.browsing_data.ClearBrowsingDataTabsFragment;
@@ -343,6 +344,7 @@ public class OmniboxPedalsTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1434836 - IncognitoTab version is flaky")
     public void testOpenIncognitoTabOmniboxPedalSuggestion() throws InterruptedException {
         HistogramWatcher histogramWatcher =
                 newHistogramExpectations(OmniboxPedalType.LAUNCH_INCOGNITO);
