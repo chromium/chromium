299c47fd9a93d0406d915d93ca2163b7f19bf111
Ben Reich <benreich@chromium.org>
2023-04-21 08:02:44 +0000

    [FilesBulkPinning] Update test to use assertDeepEquals
    The previous test was messing up the asynchronous nature of the call and
    not providing an easy way to show errors. This updates to use a
    synchronous way of doing it and makes the error message slightly better.
    
    Bug: b:275635808
    Test: browser_tests --gtest_filter=FileManagerJsTest.ReducerBulkPinning
    Change-Id: Ie85d6520d47b365bb5032e6679b845093c74c7b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457653
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1133654}

--- ui/file_manager/file_manager/state/reducers/bulk_pinning_unittest.ts
+++ ui/file_manager/file_manager/state/reducers/bulk_pinning_unittest.ts
@@ -2,15 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertDeepEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
+
 import {updateBulkPinProgress} from '../actions/bulk_pinning.js';
-import {setupStore, waitDeepEquals} from '../for_tests.js';
+import {setupStore} from '../for_tests.js';
 
 /**
  * Tests that bulk pin progress updates the store and overwrites existing values
  * on each update.
  */
 export async function testUpdateBulkPinProgress(done: () => void) {
-  const bulkPinProgress: chrome.fileManagerPrivate.BulkPinProgress = {
+  const want: chrome.fileManagerPrivate.BulkPinProgress = {
     stage: chrome.fileManagerPrivate.BulkPinStage.STOPPED,
     freeSpaceBytes: 100,
     requiredSpaceBytes: 100,
@@ -21,17 +23,23 @@ export async function testUpdateBulkPinProgress(done: () => void) {
 
   // Dispatch an action to update bulk pin progress.
   const store = setupStore();
-  store.dispatch(updateBulkPinProgress(bulkPinProgress));
+  store.dispatch(updateBulkPinProgress(want));
 
   // Expect the bulk pin progress to be updated.
-  waitDeepEquals(store, bulkPinProgress, (state) => state.bulkPinning);
+  const firstState = store.getState().bulkPinning;
+  assertDeepEquals(
+      want, firstState,
+      `1. ${JSON.stringify(want)} != ${JSON.stringify(firstState)}`);
 
   // Dispatch another action to change the stage to `SYNCING`.
-  bulkPinProgress.stage = chrome.fileManagerPrivate.BulkPinStage.SYNCING;
-  store.dispatch(updateBulkPinProgress(bulkPinProgress));
+  want.stage = chrome.fileManagerPrivate.BulkPinStage.SYNCING;
+  store.dispatch(updateBulkPinProgress(want));
 
   // Expect the bulk pin progress to equal the new state.
-  waitDeepEquals(store, bulkPinProgress, (state) => state.bulkPinning);
+  const secondState = store.getState().bulkPinning;
+  assertDeepEquals(
+      want, secondState,
+      `2. ${JSON.stringify(want)} != ${JSON.stringify(secondState)}`);
 
   done();
 }
