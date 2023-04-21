0f6efde0b25ae5077e73a64b57b80147fd3b7173
Joey Arhar <jarhar@chromium.org>
2023-04-21 20:28:33 +0000

    M113: Cherry pick libxml CVE fix
    This patch cherry-picks a fix for [CVE-2023-29469] from libxml:
    https://gitlab.gnome.org/GNOME/libxml2/-/commit/547edbf1cbdccd46b2e8ff322a456eaa5931c5df
    
    I cherry-picked these by going into my libxml checkout, checking out the
    commit that libxml is at for this M113 branch, cherry-picking the CVE
    fixes, then running the roll script on all platforms.
    
    Bug: 1433328
    Change-Id: Iaee58b0890f7190386cca3e430286f39ccbbdb02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457227
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#793}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- third_party/libxml/src/dict.c
+++ third_party/libxml/src/dict.c
@@ -431,7 +431,8 @@ static unsigned long
 xmlDictComputeFastKey(const xmlChar *name, int namelen, int seed) {
     unsigned long value = seed;
 
-    if (name == NULL) return(0);
+    if ((name == NULL) || (namelen <= 0))
+        return(value);
     value += *name;
     value <<= 5;
     if (namelen > 10) {
