6ceceb405ca7708b5facbab25beaffcad1847036
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-04-22 00:49:44 +0000

    [Extensions] Add new UserScriptWorldBrowserTest
    We're going to have a smattering of user script world-related
    browsertests, so it doesn't make sense to put them all in a file like
    script_execution_browsertest.cc (since they don't really exercise
    ScriptExecutor).
    
    Create a new user_script_world_browsertest.cc file to hold these, and
    move ScriptExecutorBrowserTest.UserScriptWorldExecution there, renamed
    UserScriptWorldBrowserTest.LimitedAPIsAreAvailableInUserScriptWorlds.
    
    Bug: 1429408
    Change-Id: Ie9ecdfa49ae3ff843c26bbef6e78544d20bc251d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457283
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1134137}

---
+++ chrome/browser/extensions/user_script_world_browsertest.cc
@@ -0,0 +1,174 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "base/test/values_test_util.h"
+#include "base/values.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/test_navigation_observer.h"
+#include "extensions/browser/script_executor.h"
+#include "extensions/common/extension_builder.h"
+#include "net/dns/mock_host_resolver.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace extensions {
+
+class UserScriptWorldBrowserTest : public ExtensionApiTest {
+ public:
+  UserScriptWorldBrowserTest() = default;
+  UserScriptWorldBrowserTest(const UserScriptWorldBrowserTest&) = delete;
+  UserScriptWorldBrowserTest& operator=(const UserScriptWorldBrowserTest&) =
+      delete;
+  ~UserScriptWorldBrowserTest() override = default;
+
+  // ExtensionApiTest:
+  void SetUpOnMainThread() override {
+    ExtensionBrowserTest::SetUpOnMainThread();
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(embedded_test_server()->Start());
+  }
+
+  // Executes the given `script` in a user script world associated with the
+  // `extension`, returning the script result. This expects the script to
+  // succeed (i.e., not throw an error) and runs the script in the primary main
+  // frame of the active WebContents.
+  base::Value ExecuteScriptInUserScriptWorld(const std::string& script,
+                                             const Extension& extension) {
+    SCOPED_TRACE(script.c_str());
+
+    ScriptExecutor script_executor(GetActiveWebContents());
+    base::RunLoop run_loop;
+    std::vector<ScriptExecutor::FrameResult> script_results;
+    auto on_complete =
+        [&run_loop, &script_results](
+            std::vector<ScriptExecutor::FrameResult> frame_results) {
+          script_results = std::move(frame_results);
+          run_loop.Quit();
+        };
+
+    std::vector<mojom::JSSourcePtr> sources;
+    sources.push_back(mojom::JSSource::New(script, GURL()));
+    script_executor.ExecuteScript(
+        mojom::HostID(mojom::HostID::HostType::kExtensions, extension.id()),
+        mojom::CodeInjection::NewJs(mojom::JSInjection::New(
+            std::move(sources), mojom::ExecutionWorld::kUserScript,
+            blink::mojom::WantResultOption::kWantResult,
+            blink::mojom::UserActivationOption::kDoNotActivate,
+            blink::mojom::PromiseResultOption::kAwait)),
+        ScriptExecutor::SPECIFIED_FRAMES, {ExtensionApiFrameIdMap::kTopFrameId},
+        ScriptExecutor::DONT_MATCH_ABOUT_BLANK,
+        mojom::RunLocation::kDocumentIdle, ScriptExecutor::DEFAULT_PROCESS,
+        GURL() /* webview_src */, base::BindLambdaForTesting(on_complete));
+    run_loop.Run();
+
+    if (script_results.size() != 1) {
+      ADD_FAILURE() << "Incorrect script execution result count: "
+                    << script_results.size();
+      return base::Value();
+    }
+
+    ScriptExecutor::FrameResult& frame_result = script_results[0];
+    if (!frame_result.error.empty()) {
+      ADD_FAILURE() << "Unexpected script error: " << frame_result.error;
+      return base::Value();
+    }
+
+    if (frame_result.value.is_none()) {
+      ADD_FAILURE() << "Null return value";
+      return base::Value();
+    }
+
+    return std::move(frame_result.value);
+  }
+
+  // Navigates the active web contents to `url`, waiting for the navigation to
+  // (successfully) complete.
+  void NavigateToURL(const GURL& url) {
+    content::TestNavigationObserver nav_observer(GetActiveWebContents());
+    ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+    nav_observer.Wait();
+    EXPECT_TRUE(nav_observer.last_navigation_succeeded());
+  }
+
+  // Loads and returns an extension with the given `host_permission`.
+  const Extension* LoadExtensionWithHostPermission(
+      const std::string& host_permission) {
+    scoped_refptr<const Extension> extension =
+        ExtensionBuilder("extension")
+            .SetManifestVersion(3)
+            .SetManifestKey("host_permissions",
+                            base::Value::List().Append(host_permission))
+            .Build();
+    extension_service()->AddExtension(extension.get());
+    EXPECT_TRUE(
+        extension_registry()->enabled_extensions().GetByID(extension->id()));
+    return extension.get();
+  }
+
+  content::WebContents* GetActiveWebContents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+};
+
+// Tests that a user script world for an extension executed in an
+// isolated world and has limited access to extension APIs.
+IN_PROC_BROWSER_TEST_F(UserScriptWorldBrowserTest,
+                       LimitedAPIsAreAvailableInUserScriptWorlds) {
+  const Extension* extension =
+      LoadExtensionWithHostPermission("http://example.com/*");
+
+  GURL example_com =
+      embedded_test_server()->GetURL("example.com", "/simple.html");
+
+  NavigateToURL(example_com);
+
+  content::WebContents* web_contents = GetActiveWebContents();
+  content::RenderFrameHost* main_frame = web_contents->GetPrimaryMainFrame();
+
+  // Set a flag in the main world of the page. This will allow us to verify
+  // the new script is running in an isolated world.
+  constexpr char kSetFlagScript[] = "window.mainWorldFlag = 'executionFlag';";
+  // NOTE: We use ExecuteScript() (and not EvalJs or ExecJs) because we
+  // explicitly *need* this to happen in the main world for the test.
+  EXPECT_TRUE(content::ExecuteScript(main_frame, kSetFlagScript));
+
+  // Inject a script into a user script world. The script will return the
+  // values of both the main world flag (set above) and all properties exposed
+  // on `chrome.runtime`.
+  static constexpr char kScriptSource[] =
+      R"(let result = {};
+         result.mainWorldFlag = window.mainWorldFlag || '<no flag>';
+         result.chromeKeys =
+             chrome ? Object.keys(chrome).sort() : '<no chrome>';
+         result.runtimeKeys = chrome && chrome.runtime ?
+             Object.keys(chrome.runtime).sort() : '<no runtime>';
+         result;)";
+
+  base::Value script_result =
+      ExecuteScriptInUserScriptWorld(kScriptSource, *extension);
+
+  // Verify the expected results. Since the user script world is less
+  // privileged, it shouldn't have access to most runtime APIs (such as reload,
+  // onStartup, getManifest, etc).
+  static constexpr char kExpectedJson[] =
+      R"({
+           "mainWorldFlag": "<no flag>",
+           "chromeKeys": ["csi", "loadTimes", "runtime"],
+           "runtimeKeys": ["ContextType", "OnInstalledReason",
+                           "OnRestartRequiredReason", "PlatformArch",
+                           "PlatformNaclArch", "PlatformOs",
+                           "RequestUpdateCheckStatus",
+                           "connect", "id", "onConnect", "onMessage",
+                           "sendMessage"]
+         })";
+  EXPECT_THAT(script_result, base::test::IsJson(kExpectedJson));
+}
+
+}  // namespace extensions
