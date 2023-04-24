ec04c35e22634423785eb1bfc04f01f4a7530793
Michael Thiessen <mthiesse@chromium.org>
2023-04-24 20:20:53 +0000

    Kill switch flip for BlockFrameRenavigations
    Caused a regression with new tab external navigation on Stable, used
    commonly by Firebase Dynamic Links.
    
    (cherry picked from commit 004b542af456306841758bb21f1ee1f380bdb843)
    
    Bug: 1433137
    Change-Id: Ic4f6e98cc65c986292266df622f11b1b8e222eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456289
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1133337}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469068
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5672@{#875}
    Cr-Branched-From: 5f2a72468eda1eb945b3b5a2298b5d1cd678521e-refs/heads/main@{#1121455}

--- components/external_intents/android/external_intents_features.cc
+++ components/external_intents/android/external_intents_features.cc
@@ -40,7 +40,7 @@ BASE_FEATURE(kBlockSubframeIntentToSelf,
 
 BASE_FEATURE(kBlockFrameRenavigations,
              "BlockFrameRenavigations",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDoNotRequireSpecializedCCTHandler,
              "DoNotRequireSpecializedCCTHandler",
