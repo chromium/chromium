42d270b28cb59c5dd4e35d450bdda458d6dde005
seanmccullough <seanmccullough@google.com>
2023-04-24 22:13:15 +0000

    [perf bots] re-shard win-10-laptop-low-end due to failing disks
    Steps I took to produce this CL:
    
    - edited `tools/perf/core/bot_platforms.py`
    - ran `tools/perf/generate_perf_data`
    - ran `tools/perf/generate_perf_sharding.py update -o`
    
    Bug: b:278947510
    Change-Id: If929f5fa7436b134e4336b023a0d983ce99a9dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455843
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Sean McCullough <seanmccullough@google.com>
    Cr-Commit-Position: refs/heads/main@{#1134869}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -658,8 +658,8 @@ WIN_10_LOW_END = PerfPlatform(
     'Low end windows 10 HP laptops. HD Graphics 5500, x86-64-i3-5005U, '
     'SSD, 4GB RAM.',
     _WIN_10_LOW_END_BENCHMARK_CONFIGS,
-    # TODO(crbug.com/1305291): Increase the count back to 46 when issue fixed.
-    40,
+    # TODO(b/278947510): Increase the count when m.2 disks stop failing.
+    20,
     'win')
 WIN_10_LOW_END_PGO = PerfPlatform(
     'win-10_laptop_low_end-perf-pgo',
