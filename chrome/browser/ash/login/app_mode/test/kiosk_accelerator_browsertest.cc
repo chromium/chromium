c6f40781369e4878b298e736f9d3763a5262a3c9
Edman Anjos <edmanp@google.com>
2023-04-25 16:39:39 +0000

    kiosk: Move and update zoom accelerator tests from kiosk_device_owned_browsertest
    The accelerator behavior in the previous ZoomSupport tests is not
    specific to device owned (consumer kiosk) scenarios.
    
    Move the test to kiosk_accelerator_browsertest instead.
    
    The test also relied on chrome.test extension APIs to communicate with
    the kiosk chrome app. The new implementation does not.
    
    Bug: b:277914233
    Test: tools/autotest.py -C out_/Default --run_all chrome/browser/ash/login/app_mode/test/kiosk_accelerator_browsertest.cc
    Change-Id: I49365c33fde2daf6c05520cd77b95425f3881bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4474328
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Edman Anjos <edman@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135271}

--- chrome/browser/ash/login/app_mode/test/kiosk_accelerator_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_accelerator_browsertest.cc
@@ -4,6 +4,7 @@
 
 #include "ash/accelerators/accelerator_controller_impl.h"
 #include "ash/shell.h"
+#include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "chrome/browser/app_mode/test/accelerator_helpers.h"
 #include "chrome/browser/ash/app_mode/app_session_ash.h"
@@ -12,14 +13,36 @@
 #include "chrome/browser/ash/login/app_mode/test/ash_accelerator_helpers.h"
 #include "chrome/browser/ash/login/app_mode/test/test_browser_closed_waiter.h"
 #include "chrome/browser/ash/login/app_mode/test/web_kiosk_base_test.h"
+#include "chrome/browser/ash/login/test/test_predicate_waiter.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
 
-// Verify accelerators do not work in Kiosk sessions in Ash.
+namespace {
+
+int WindowWidth(content::WebContents* web_contents) {
+  return content::EvalJs(web_contents, "window.innerWidth").ExtractInt();
+}
+
+int WindowWidthAfterChange(content::WebContents* web_contents,
+                           int initial_width) {
+  test::TestPredicateWaiter(
+      base::BindRepeating(
+          [](content::WebContents* web_contents, int initial_width) {
+            return initial_width != WindowWidth(web_contents);
+          },
+          web_contents, initial_width))
+      .Wait();
+  return WindowWidth(web_contents);
+}
+
+}  // anonymous namespace
+
+// Verifies accelerator behavior in Kiosk sessions in Ash.
 class WebKioskAcceleratorTest : public WebKioskBaseTest {};
 
 IN_PROC_BROWSER_TEST_F(WebKioskAcceleratorTest, AcceleratorsDontCloseSession) {
@@ -35,7 +58,38 @@ IN_PROC_BROWSER_TEST_F(WebKioskAcceleratorTest, AcceleratorsDontCloseSession) {
   ASSERT_FALSE(WebKioskAppManager::Get()->app_session()->is_shutting_down());
 }
 
-// Verify accelerators work in regular non-kiosk sessions in Ash.
+IN_PROC_BROWSER_TEST_F(WebKioskAcceleratorTest, ZoomAccelerators) {
+  InitializeRegularOnlineKiosk();
+  SelectFirstBrowser();
+
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
+  ASSERT_NE(browser_view, nullptr);
+  content::WebContents* web_contents = browser_view->GetActiveWebContents();
+  ASSERT_NE(web_contents, nullptr);
+
+  int initial_width = WindowWidthAfterChange(web_contents, 0);
+  ASSERT_GT(initial_width, 0);
+
+  // Zoom in, content becomes larger and window width becomes smaller.
+  browser_view->AcceleratorPressed(
+      ui::Accelerator(ui::VKEY_ADD, ui::EF_CONTROL_DOWN));
+  int zoomed_in_width = WindowWidthAfterChange(web_contents, initial_width);
+  ASSERT_LT(zoomed_in_width, initial_width);
+
+  // Restore zoom, window width becomes |initial_width|.
+  browser_view->AcceleratorPressed(
+      ui::Accelerator(ui::VKEY_0, ui::EF_CONTROL_DOWN));
+  int restored_width = WindowWidthAfterChange(web_contents, zoomed_in_width);
+  ASSERT_EQ(restored_width, initial_width);
+
+  // Zoom out, content becomes smaller and window width becomes larger.
+  browser_view->AcceleratorPressed(
+      ui::Accelerator(ui::VKEY_SUBTRACT, ui::EF_CONTROL_DOWN));
+  int zoomed_out_width = WindowWidthAfterChange(web_contents, zoomed_in_width);
+  ASSERT_GT(zoomed_out_width, initial_width);
+}
+
+// Verifies accelerators work in regular non-kiosk sessions in Ash.
 class NonKioskAcceleratorTest : public InProcessBrowserTest {};
 
 IN_PROC_BROWSER_TEST_F(NonKioskAcceleratorTest, CloseTabAccelerator) {
