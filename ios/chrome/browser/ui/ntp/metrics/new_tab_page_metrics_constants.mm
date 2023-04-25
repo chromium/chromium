cce06000ecff8e45d6402ae7be725fd54c191df2
Guillem Perez <guiperez@google.com>
2023-04-25 22:41:10 +0000

    [iOS] Refactor new tab page metrics constants to a constants file
    Moving all the histograms and actions to a constants file.
    
    Bug: 1431193
    Change-Id: I5c152f042b1aea2c3328eee98f054faff0e34e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4475949
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135600}

---
+++ ios/chrome/browser/ui/ntp/metrics/new_tab_page_metrics_constants.mm
@@ -0,0 +1,27 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/ntp/metrics/new_tab_page_metrics_constants.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+#pragma mark - Actions
+
+const char kFakeboxNTPTappedAction[] = "MobileFakeboxNTPTapped";
+const char kFakeViewNTPTappedAction[] = "MobileFakeViewNTPTapped";
+const char kMostVisitedVoiceSearchAction[] = "MobileNTPMostVisitedVoiceSearch";
+const char kNTPEntrypointTappedAction[] =
+    "Mobile.LensIOS.NewTabPageEntrypointTapped";
+const char kNTPIdentityDiscTappedAction[] = "MobileNTPIdentityDiscTapped";
+
+#pragma mark - Histograms
+
+const char kHomeActionOnNTPHistogram[] = "IOS.Home.ActionOnNTP";
+const char kHomeActionOnStartSurfaceHistogram[] =
+    "IOS.Home.ActionOnStartSurface";
+const char kNTPTimeSpentHistogram[] = "NewTabPage.TimeSpent";
+const char kNTPImpressionHistogram[] = "IOS.NTP.Impression";
+const char kNTPOverscrollActionHistogram[] = "IOS.NTP.OverscrollAction";
