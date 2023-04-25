31bec755861297493d0adf4515f789adc7a94180
Wissem Gamra <wiss@chromium.org>
2023-04-25 17:16:10 +0000

    [Cormorant] Apply FeedStreamViewResizer to the Cormorant Feed
    In order to not take over the whole vie in a tablet setting, we apply
    the same view resizer to Cormorant as the main feed. Tracking bug on
    buganizer is b/277367458
    
    Change-Id: I1814c4c449a7735205123dc0512470f48ef50484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4457506
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Wissem Gamra <wiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135316}

--- chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinator.java
+++ chrome/browser/creator/android/java/src/org/chromium/chrome/browser/creator/CreatorCoordinator.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.OnScrollListener;
@@ -27,6 +28,7 @@ import org.chromium.chrome.browser.feed.FeedContentFirstLoadWatcher;
 import org.chromium.chrome.browser.feed.FeedListContentManager;
 import org.chromium.chrome.browser.feed.FeedListContentManager.FeedContent;
 import org.chromium.chrome.browser.feed.FeedStream;
+import org.chromium.chrome.browser.feed.FeedStreamViewResizer;
 import org.chromium.chrome.browser.feed.FeedSurfaceScopeDependencyProviderImpl;
 import org.chromium.chrome.browser.feed.FeedSurfaceTracker;
 import org.chromium.chrome.browser.feed.NativeViewListRenderer;
@@ -54,6 +56,7 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetControllerFacto
 import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
+import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
 import org.chromium.components.browser_ui.widget.scrim.ScrimCoordinator;
 import org.chromium.components.embedder_support.view.ContentView;
 import org.chromium.components.url_formatter.UrlFormatter;
@@ -86,6 +89,7 @@ public class CreatorCoordinator implements FeedAutoplaySettingsDelegate,
     private CreatorTabMediator mTabMediator;
     private Activity mActivity;
     private FeedListContentManager mContentManager;
+    private UiConfig mUiConfig;
     private RecyclerView mRecyclerView;
     private View mProfileView;
     private ViewGroup mLayoutView;
@@ -121,6 +125,8 @@ public class CreatorCoordinator implements FeedAutoplaySettingsDelegate,
     private final UnownedUserDataSupplier<ShareDelegate> mBottomsheetShareDelegateSupplier;
     private GURL mBottomSheetUrl;
     private int mEntryPoint;
+
+    private @Nullable FeedStreamViewResizer mStreamViewResizer;
 
     private static final String CREATOR_PROFILE_ID = "CreatorProfileView";
     private static final String CREATOR_PRIVACY_ID = "CreatorPrivacyId";
@@ -169,6 +175,9 @@ public class CreatorCoordinator implements FeedAutoplaySettingsDelegate,
         mCreatorViewGroup =
                 (ViewGroup) LayoutInflater.from(mActivity).inflate(R.layout.creator_activity, null);
         mLayoutView = mCreatorViewGroup.findViewById(R.id.creator_layout);
+        mUiConfig = new UiConfig(mLayoutView);
+        mStreamViewResizer =
+                FeedStreamViewResizer.createAndAttach(mActivity, mRecyclerView, mUiConfig);
         mLayoutView.addView(mRecyclerView);
 
         // Generate Creator Model
