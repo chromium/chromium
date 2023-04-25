bc84b11c2d3c9e7483706bffef892ee304722839
Mingyu Lei <leimy@chromium.org>
2023-04-25 17:59:20 +0000

    IPH [2]: Implement ChainedBackNavigationTracker
    This CL creates `chrome::ChainedBackNavigationTracker` that contains
    a set of methods to help to record the number of chained back
    navigations.
    
    If two history navigation is performed one after another, and the
    interval between them is smaller than the threshold, they are
    considered chained, and the chain length is 2. The chain is broken
    if the interval between two navigations is too long, or there is any
    non history navigation in between.
    
    Please refer to the linked doc for more context.
    
    IPH experiment doc: https://docs.google.com/document/d/1P2_t9cLmseOlCPB59Y2HnEzYcGZrAy-Q_gOBzP4p_u4/edit?usp=sharing&resourcekey=0-PuBuh64jkNdKS3JOTF1-wg
    Implementation doc: https://docs.google.com/document/d/13wbzJZgNAirDd7sAW66nQTJ1Al-3gwOAVTyoYHAYbF8/edit?usp=sharing
    
    Bug: 1412691
    Change-Id: Ifa92cf776f6d5b7eef40366ff4fe06ca49e3f5e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4431712
    Auto-Submit: Mingyu Lei <leimy@google.com>
    Commit-Queue: Mingyu Lei <leimy@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135379}

---
+++ chrome/browser/chained_back_navigation_tracker.cc
@@ -0,0 +1,81 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chained_back_navigation_tracker.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/navigation_handle.h"
+#include "third_party/blink/public/mojom/navigation/navigation_params.mojom-shared.h"
+#include "ui/base/page_transition_types.h"
+
+namespace chrome {
+
+ChainedBackNavigationTracker::ChainedBackNavigationTracker(
+    content::WebContents* contents)
+    : content::WebContentsObserver(contents),
+      content::WebContentsUserData<ChainedBackNavigationTracker>(*contents) {}
+
+ChainedBackNavigationTracker::~ChainedBackNavigationTracker() = default;
+
+void ChainedBackNavigationTracker::DidStartNavigation(
+    content::NavigationHandle* navigation) {
+  if (navigation->GetNavigationEntry() &&
+      (navigation->GetNavigationEntry()->GetTransitionType() &
+       ui::PageTransition::PAGE_TRANSITION_FORWARD_BACK) &&
+      !navigation->IsRendererInitiated()) {
+    IncrementChainedBackNavigationCount();
+  } else {
+    ResetChainedBackNavigationCount();
+  }
+}
+
+void ChainedBackNavigationTracker::IncrementChainedBackNavigationCount() {
+  if (chained_back_navigation_count_ == 0 ||
+      (base::TimeTicks::Now() - last_back_navigation_time_).InMilliseconds() <=
+          kMaxChainedBackNavigationIntervalInMilliseconds) {
+    chained_back_navigation_count_++;
+  } else {
+    chained_back_navigation_count_ = 1u;
+  }
+  last_back_navigation_time_ = base::TimeTicks::Now();
+}
+
+void ChainedBackNavigationTracker::ResetChainedBackNavigationCount() {
+  chained_back_navigation_count_ = 0;
+  last_back_navigation_time_ = base::TimeTicks();
+}
+
+void ChainedBackNavigationTracker::
+    RecordBackButtonClickForChainedBackNavigation() {
+  if (chained_back_button_click_count_ == 0 ||
+      (base::TimeTicks::Now() - last_back_button_click_time_)
+              .InMilliseconds() <=
+          kMaxChainedBackNavigationIntervalInMilliseconds) {
+    chained_back_button_click_count_++;
+  } else {
+    chained_back_button_click_count_ = 1u;
+  }
+  last_back_button_click_time_ = base::TimeTicks::Now();
+}
+
+bool ChainedBackNavigationTracker::IsChainedBackNavigationRecentlyPerformed()
+    const {
+  return (base::TimeTicks::Now() - last_back_navigation_time_)
+                 .InMilliseconds() <=
+             kMaxChainedBackNavigationIntervalInMilliseconds &&
+         chained_back_navigation_count_ >= kMinimumChainedBackNavigationLength;
+}
+
+bool ChainedBackNavigationTracker::
+    IsBackButtonChainedBackNavigationRecentlyPerformed() const {
+  return IsChainedBackNavigationRecentlyPerformed() &&
+         (base::TimeTicks::Now() - last_back_button_click_time_)
+                 .InMilliseconds() <=
+             kMaxChainedBackNavigationIntervalInMilliseconds &&
+         chained_back_button_click_count_ >=
+             kMinimumChainedBackNavigationLength;
+}
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(ChainedBackNavigationTracker);
+
+}  // namespace chrome
