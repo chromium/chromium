369e3dd90fd7c7ad0d91d55a3719f3205eab5b63
Ali Juma <ajuma@chromium.org>
2023-04-25 16:36:57 +0000

    [CodeHealthRotation] Replace GC-unsafe fields in ServiceWorkerRegistration
    This replaces mojo::Associated{Remove,Receiver} fields in
    ServiceWorkerRegistration with their GC'd equivalent,
    HeapMojoAssociated{Remote,Receiver}.
    
    Bug: 1432856
    Change-Id: I873a799637646652b8cbbac38becd48427b721a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4435309
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135267}

--- third_party/blink/renderer/modules/service_worker/service_worker_registration.h
+++ third_party/blink/renderer/modules/service_worker/service_worker_registration.h
@@ -6,8 +6,6 @@
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SERVICE_WORKER_SERVICE_WORKER_REGISTRATION_H_
 
 #include <memory>
-#include "mojo/public/cpp/bindings/associated_receiver.h"
-#include "mojo/public/cpp/bindings/associated_remote.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_registration.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_registration_object_info.h"
 #include "third_party/blink/public/platform/web_vector.h"
@@ -18,6 +16,8 @@
 #include "third_party/blink/renderer/modules/service_worker/navigation_preload_manager.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker.h"
 #include "third_party/blink/renderer/platform/heap/prefinalizer.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_remote.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/gc_plugin.h"
@@ -131,16 +131,15 @@ class ServiceWorkerRegistration final
   // to the Mojo connection. It is bound on the
   // main thread for service worker clients (document), and is bound on the
   // service worker thread for service worker execution contexts.
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::AssociatedRemote<mojom::blink::ServiceWorkerRegistrationObjectHost>
+  HeapMojoAssociatedRemote<mojom::blink::ServiceWorkerRegistrationObjectHost>
       host_;
   // |receiver_| receives messages from the ServiceWorkerRegistrationObjectHost
   // in the browser process. It is bound on the main thread for service worker
   // clients (document), and is bound on the service worker thread for service
   // worker execution contexts.
-  GC_PLUGIN_IGNORE("https://crbug.com/1381979")
-  mojo::AssociatedReceiver<mojom::blink::ServiceWorkerRegistrationObject>
-      receiver_{this};
+  HeapMojoAssociatedReceiver<mojom::blink::ServiceWorkerRegistrationObject,
+                             ServiceWorkerRegistration>
+      receiver_;
 
   bool stopped_;
 };
