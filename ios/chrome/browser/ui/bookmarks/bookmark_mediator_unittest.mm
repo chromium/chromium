a955454619d9c5a90016d6eba00eb6feb11c0d08
Menghan YANG <myuu@google.com>
2023-04-25 11:11:41 +0000

    [iOS] Use string IDs instead of raw strings in bookmark tests
    The use of raw string for snackbar text tests make the tests fragile,
    since they fail if the string content is updated, or if the tests are
    executed on devices with non-English default language.
    
    This CL replaces the raw strings by string ID to prevent failure in
    those cases.
    
    Fixed: 1432496
    Change-Id: Ie87680621bed75f1cd89f689ae3ba5089a3813b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4465969
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135130}

--- ios/chrome/browser/ui/bookmarks/bookmark_mediator_unittest.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_mediator_unittest.mm
@@ -4,7 +4,9 @@
 
 #import "ios/chrome/browser/ui/bookmarks/bookmark_mediator.h"
 
+#import "base/i18n/message_formatter.h"
 #import "base/strings/sys_string_conversions.h"
+#import "base/strings/utf_string_conversions.h"
 #import "base/test/scoped_feature_list.h"
 #import "components/sync/driver/sync_service.h"
 #import "ios/chrome/browser/application_context/application_context.h"
@@ -20,7 +22,9 @@
 #import "ios/chrome/browser/sync/sync_service_factory.h"
 #import "ios/chrome/browser/sync/sync_setup_service.h"
 #import "ios/chrome/browser/sync/sync_setup_service_factory.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest_mac.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -43,6 +47,10 @@ class FakeSyncSetupService : public SyncSetupService {
 };
 
 namespace {
+
+NSString* const kFolderName = @"folder name";
+NSString* const kDefaultFolderName = @"default folder";
+NSString* const kEmail = @"foo1@gmail.com";
 
 class BookmarkMediatorUnitTest : public BookmarkIOSUnitTestSupport {
   void SetUp() override {
@@ -80,6 +88,42 @@ class BookmarkMediatorUnitTest : public BookmarkIOSUnitTestSupport {
     }
   }
 
+  NSString* GetSavedToDeviceText(int count) {
+    std::u16string pattern =
+        l10n_util::GetStringUTF16(IDS_IOS_BOOKMARK_PAGE_SAVED);
+    std::u16string message = base::i18n::MessageFormatter::FormatWithNamedArgs(
+        pattern, "count", count);
+    return base::SysUTF16ToNSString(message);
+  }
+
+  NSString* GetSavedToFolderText(int count, NSString* folder_name) {
+    std::u16string pattern =
+        l10n_util::GetStringUTF16(IDS_IOS_BOOKMARK_PAGE_SAVED_FOLDER);
+    std::u16string message = base::i18n::MessageFormatter::FormatWithNamedArgs(
+        pattern, "count", count, "title",
+        base::SysNSStringToUTF16(folder_name));
+    return base::SysUTF16ToNSString(message);
+  }
+
+  NSString* GetSavedToAccountText(int count, NSString* email) {
+    std::u16string pattern =
+        l10n_util::GetStringUTF16(IDS_IOS_BOOKMARK_PAGE_SAVED_INTO_ACCOUNT);
+    std::u16string message = base::i18n::MessageFormatter::FormatWithNamedArgs(
+        pattern, "count", count, "email", base::SysNSStringToUTF16(email));
+    return base::SysUTF16ToNSString(message);
+  }
+
+  NSString* GetSavedToFolderToAccountText(int count,
+                                          NSString* folder_name,
+                                          NSString* email) {
+    std::u16string pattern = l10n_util::GetStringUTF16(
+        IDS_IOS_BOOKMARK_PAGE_SAVED_INTO_ACCOUNT_FOLDER);
+    std::u16string message = base::i18n::MessageFormatter::FormatWithNamedArgs(
+        pattern, "count", count, "title", base::SysNSStringToUTF16(folder_name),
+        "email", base::SysNSStringToUTF16(email));
+    return base::SysUTF16ToNSString(message);
+  }
+
   BookmarkMediator* mediator_;
   ChromeAccountManagerService* account_manager_service_;
   AuthenticationService* authentication_service_;
@@ -89,134 +133,149 @@ class BookmarkMediatorUnitTest : public BookmarkIOSUnitTestSupport {
 };
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedOutNoFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
                                                      title:nil
-                                                     count:1],
-              @"Bookmarked");
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedOutNoFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
                                                      title:nil
-                                                     count:1],
-              @"Bookmarked");
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedOutDefaultFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
-                                                     title:@"default folder"
-                                                     count:1],
-              @"Bookmarked");
+                                                     title:kDefaultFolderName
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedOutDefaultFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
-                                                     title:@"default folder"
-                                                     count:1],
-              @"Bookmarked");
+                                                     title:kDefaultFolderName
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedOutInFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:YES
-                                                     title:@"folder name"
-                                                     count:1],
-              @"Bookmarked to \"folder name\"");
+                                                     title:kFolderName
+                                                     count:bookmark_count],
+              GetSavedToFolderText(bookmark_count, kFolderName));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedOutInFolder) {
+  constexpr int bookmark_count = 1;
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:YES
-                                                     title:@"folder name"
-                                                     count:1],
-              @"Bookmarked to \"folder name\"");
+                                                     title:kFolderName
+                                                     count:bookmark_count],
+              GetSavedToFolderText(bookmark_count, kFolderName));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedInNoFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
                                                      title:nil
-                                                     count:1],
-              @"Bookmarked");
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInNoFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
                                                      title:nil
-                                                     count:1],
-              @"Bookmark saved in your Google Account, foo1@gmail.com");
+                                                     count:bookmark_count],
+              GetSavedToAccountText(bookmark_count, kEmail));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInNoFolderPlural) {
+  constexpr int bookmark_count = 2;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
                                                      title:nil
-                                                     count:2],
-              @"Bookmarks saved in your Google Account, foo1@gmail.com");
+                                                     count:bookmark_count],
+              GetSavedToAccountText(bookmark_count, kEmail));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedInDefaultFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
-                                                     title:@"default folder"
-                                                     count:1],
-              @"Bookmarked");
+                                                     title:kDefaultFolderName
+                                                     count:bookmark_count],
+              GetSavedToDeviceText(bookmark_count));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInDefaultFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
-                                                     title:@"default folder"
-                                                     count:1],
-              @"Bookmark saved in your Google Account, foo1@gmail.com");
+                                                     title:kDefaultFolderName
+                                                     count:bookmark_count],
+              GetSavedToAccountText(bookmark_count, kEmail));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInDefaultFolderPlural) {
+  constexpr int bookmark_count = 2;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:NO
-                                                     title:@"default folder"
-                                                     count:2],
-              @"Bookmarks saved in your Google Account, foo1@gmail.com");
+                                                     title:kDefaultFolderName
+                                                     count:bookmark_count],
+              GetSavedToAccountText(bookmark_count, kEmail));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagDisabledSignedInInFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(false);
   ASSERT_NSEQ([mediator_ messageForAddingBookmarksInFolder:YES
-                                                     title:@"folder name"
-                                                     count:1],
-              @"Bookmarked to \"folder name\"");
+                                                     title:kFolderName
+                                                     count:bookmark_count],
+              GetSavedToFolderText(bookmark_count, kFolderName));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInInFolder) {
+  constexpr int bookmark_count = 1;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ(
       [mediator_ messageForAddingBookmarksInFolder:YES
-                                             title:@"folder name"
-                                             count:1],
-      @"Bookmark saved to \"folder name\" in your account, foo1@gmail.com");
+                                             title:kFolderName
+                                             count:bookmark_count],
+      GetSavedToFolderToAccountText(bookmark_count, kFolderName, kEmail));
 }
 
 TEST_F(BookmarkMediatorUnitTest, TestFlagEnabledSignedInInFolderPlural) {
+  constexpr int bookmark_count = 2;
   SignInAndSync();
   setEmailInSnackbarFlag(true);
   ASSERT_NSEQ(
       [mediator_ messageForAddingBookmarksInFolder:YES
-                                             title:@"folder name"
-                                             count:2],
-      @"Bookmarks saved to \"folder name\" in your account, foo1@gmail.com");
+                                             title:kFolderName
+                                             count:bookmark_count],
+      GetSavedToFolderToAccountText(bookmark_count, kFolderName, kEmail));
 }
 
 }  // namespace
