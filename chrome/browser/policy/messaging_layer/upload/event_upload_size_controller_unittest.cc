0c9ab027727d339ce9bdfcff4b4551b38d2868c1
Hong Xu <xuhong@chromium.org>
2023-04-25 19:02:53 +0000

    Add unit test for BuildEncryptedRecords
    Bug: b:279469272
    Fixed: b:279469272
    Change-Id: I5415d2c222097d7e597a89244b0a82716767db26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471813
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135443}

--- chrome/browser/policy/messaging_layer/upload/event_upload_size_controller_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/event_upload_size_controller_unittest.cc
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstddef>
+#include <algorithm>
+#include <cstdint>
 #include <string>
 
 #include "chrome/browser/policy/messaging_layer/upload/event_upload_size_controller.h"
@@ -10,15 +11,40 @@
 #include "components/reporting/proto/synced/record.pb.h"
 #include "content/public/test/browser_task_environment.h"
 
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace reporting {
+
+using ::testing::SizeIs;
 
 // Testing |EventUploadSizeController|. Since a big portion of the class is
 // simply applying a formula, there is no reason to repeat the formula here.
 // This test focuses on the dynamic elements (such as |AccountForRecord|).
 class EventUploadSizeControllerTest : public ::testing::Test {
  protected:
+  // Computes the maximum number of records before |IsMaximumUploadSizeReached|
+  // returns true:
+  //    |IsMaximumUploadSizeReached| return false if < max_num_of_records
+  //    records are accounted for.
+  //    Otherwise, |IsMaximumUploadSizeReached| returns true.
+  // record_reserve is the space needed for each record. That is, the size of
+  // the record if record_copy is not present, or the size of the record + max
+  // file upload buffer size if record_copy is present.
+  static uint64_t ComputeMaxNumOfRecords(
+      const EventUploadSizeController& controller,
+      const NetworkConditionService& network_condition_service,
+      uint64_t record_reserve) {
+    // This number may change from time to time if we adapt the formula in the
+    // future.
+    const uint64_t max_upload_size =
+        controller.ComputeMaxUploadSize(network_condition_service);
+    LOG(INFO) << "The computed max upload size is " << max_upload_size;
+
+    return std::max<uint64_t>(
+        1u, (max_upload_size + record_reserve - 1u) / record_reserve);
+  }
+
   void SetUp() override {
     // Default is disabled
     ASSERT_FALSE(EventUploadSizeController::Enabler::Get());
@@ -27,6 +53,8 @@ class EventUploadSizeControllerTest : public ::testing::Test {
     EventUploadSizeController::Enabler::Set(true);
     ASSERT_TRUE(EventUploadSizeController::Enabler::Get());
   }
+
+  void TearDown() override { EventUploadSizeController::Enabler::Set(false); }
 
   // Needed by NetworkConditionService.
   content::BrowserTaskEnvironment task_environment_;
@@ -44,23 +72,12 @@ TEST_F(EventUploadSizeControllerTest, AccountForRecordAddUp) {
       /*new_events_rate=*/1U,
       /*remaining_storage_capacity=*/std::numeric_limits<uint64_t>::max(),
       /*max_file_upload_buffer_size=*/1024UL);
-  // This number may change from time to time if we adapt the formula in the
-  // future.
-  const uint64_t max_upload_size =
-      event_upload_size_controller.ComputeMaxUploadSize(
-          network_condition_service);
-  LOG(INFO) << "The computed max upload size is " << max_upload_size;
-
   EncryptedRecord record;
   record.set_encrypted_wrapped_record(std::string(100, 'A'));
   const auto record_size = record.ByteSizeLong();
-  // Maximum number of records of size of "record" before
-  // |IsMaximumUploadSizeReached| returns true:
-  //    |IsMaximumUploadSizeReached| return false if < max_num_of_records
-  //    |records are accounted for.
-  //    Otherwise, |IsMaximumUploadSizeReached| returns true.
-  const uint64_t max_num_of_records =
-      max_upload_size / record_size + (max_upload_size % record_size != 0);
+  const uint64_t max_num_of_records = ComputeMaxNumOfRecords(
+      event_upload_size_controller, network_condition_service, record_size);
+
   // A sanity check. Because the formula may change from time to time, the
   // following is satisfied for the thoroughness of this test.
   ASSERT_GE(max_num_of_records, 2U)
@@ -95,7 +112,7 @@ TEST_F(EventUploadSizeControllerTest, AccountForFileUpload) {
       network_condition_service,
       /*new_events_rate=*/1U,
       /*remaining_storage_capacity=*/std::numeric_limits<uint64_t>::max(),
-      /*max_file_upload_buffer_size*/ 1024UL);
+      /*max_file_upload_buffer_size=*/1024UL);
   // This number may change from time to time if we adapt the formula in the
   // future.
   const uint64_t max_upload_size =
@@ -117,7 +134,116 @@ TEST_F(EventUploadSizeControllerTest, AccountForFileUpload) {
   ASSERT_FALSE(event_upload_size_controller.IsMaximumUploadSizeReached());
 }
 
-// TODO(b/214039157): Add test for |BuildEncryptedRecords|, including file
-// upload buffer accounting.
+// Tests BuildEncryptedRecords when all input are returned.
+TEST_F(EventUploadSizeControllerTest, BuildEncryptedRecordsAll) {
+  TestingNetworkConditionService network_condition_service(&task_environment_);
+
+  google::protobuf::RepeatedPtrField<EncryptedRecord> encrypted_records;
+  encrypted_records.Add()->set_encrypted_wrapped_record("abc");
+  encrypted_records.Add()->set_encrypted_wrapped_record("de");
+  encrypted_records.Add()->set_encrypted_wrapped_record("fghi");
+
+  const auto results = EventUploadSizeController::BuildEncryptedRecords(
+      encrypted_records,
+      // This controller would let all records pass.
+      ::reporting::EventUploadSizeController(
+          network_condition_service, /*new_events_rate=*/1u,
+          /*remaining_storage_capacity=*/std::numeric_limits<uint64_t>::max(),
+          /*max_file_upload_buffer_size=*/0U));
+
+  ASSERT_THAT(results, SizeIs(static_cast<size_t>(encrypted_records.size())));
+  for (size_t i = 0; i < results.size(); ++i) {
+    std::string result_str, expected_str;
+    results[i].SerializeToString(&result_str);
+    encrypted_records[i].SerializeToString(&expected_str);
+    EXPECT_EQ(result_str, expected_str);
+  }
+}
+
+// Tests BuildEncryptedRecords when only part of the input are returned.
+TEST_F(EventUploadSizeControllerTest, BuildEncryptedRecordsPart) {
+  static constexpr size_t kNumOfRecords = 50u;
+  TestingNetworkConditionService network_condition_service(&task_environment_);
+  network_condition_service.SetUploadRate(10);
+  EventUploadSizeController event_upload_size_controller(
+      network_condition_service, /*new_events_rate=*/2u,
+      /*remaining_storage_capacity=*/std::numeric_limits<uint64_t>::max(),
+      // The following is not taking effects because no record has record_copy.
+      /*max_file_upload_buffer_size=*/std::numeric_limits<uint64_t>::max());
+
+  google::protobuf::RepeatedPtrField<EncryptedRecord> encrypted_records;
+  for (size_t i = 0; i < kNumOfRecords; ++i) {
+    encrypted_records.Add()->set_encrypted_wrapped_record(
+        std::string(100, 'a'));
+  }
+
+  const uint64_t expected_num_of_records = ComputeMaxNumOfRecords(
+      event_upload_size_controller, network_condition_service,
+      encrypted_records[0].ByteSizeLong());
+
+  // Ensure we are dealing with a range of number of records that we would like
+  // to test.
+  ASSERT_GE(expected_num_of_records, 2u);
+  ASSERT_LT(expected_num_of_records, kNumOfRecords);
+
+  const auto results = EventUploadSizeController::BuildEncryptedRecords(
+      encrypted_records, std::move(event_upload_size_controller));
+
+  ASSERT_THAT(results, SizeIs(expected_num_of_records));
+  for (size_t i = 0; i < expected_num_of_records; ++i) {
+    std::string result_str, expected_str;
+    results[i].SerializeToString(&result_str);
+    encrypted_records[i].SerializeToString(&expected_str);
+    EXPECT_EQ(result_str, expected_str);
+  }
+}
+
+// Tests BuildEncryptedRecords when only part of the input are returned and
+// record_copy is present (thus upload buffer is used).
+TEST_F(EventUploadSizeControllerTest, BuildEncryptedRecordsPartWithRecordCopy) {
+  static constexpr size_t kNumOfRecords = 50u;
+  static constexpr size_t kMaxFileUploadBufferSize = 512u;
+
+  TestingNetworkConditionService network_condition_service(&task_environment_);
+  network_condition_service.SetUploadRate(10);
+  EventUploadSizeController event_upload_size_controller(
+      network_condition_service, /*new_events_rate=*/2u,
+      /*remaining_storage_capacity=*/std::numeric_limits<uint64_t>::max(),
+      /*max_file_upload_buffer_size=*/kMaxFileUploadBufferSize);
+
+  google::protobuf::RepeatedPtrField<EncryptedRecord> encrypted_records;
+  for (size_t i = 0; i < kNumOfRecords; ++i) {
+    auto* record = encrypted_records.Add();
+    record->set_encrypted_wrapped_record(std::string(100, 'a'));
+    *record->mutable_record_copy() = Record();
+  }
+
+  const uint64_t expected_num_of_records = ComputeMaxNumOfRecords(
+      event_upload_size_controller, network_condition_service,
+      encrypted_records[0].ByteSizeLong() + kMaxFileUploadBufferSize);
+
+  // Ensure we are dealing with a range of number of records that we would like
+  // to test.
+  ASSERT_GE(expected_num_of_records, 2u);
+  ASSERT_LT(expected_num_of_records, kNumOfRecords);
+  // Ensure that we are testing a different expected number of records from
+  // when file upload buffer is not used. This is to prevent this test from
+  // passing even if max file upload buffer size is not taken into account.
+  ASSERT_NE(expected_num_of_records,
+            ComputeMaxNumOfRecords(event_upload_size_controller,
+                                   network_condition_service,
+                                   encrypted_records[0].ByteSizeLong()));
+
+  const auto results = EventUploadSizeController::BuildEncryptedRecords(
+      encrypted_records, std::move(event_upload_size_controller));
+
+  ASSERT_THAT(results, SizeIs(expected_num_of_records));
+  for (size_t i = 0; i < expected_num_of_records; ++i) {
+    std::string result_str, expected_str;
+    results[i].SerializeToString(&result_str);
+    encrypted_records[i].SerializeToString(&expected_str);
+    EXPECT_EQ(result_str, expected_str);
+  }
+}
 
 }  // namespace reporting
