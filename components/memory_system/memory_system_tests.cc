4fab2b2c6da3e2b09a14d472dd22182ce2fbf565
Richard Townsend <Richard.Townsend@arm.com>
2023-04-26 15:49:46 +0000

    opt components_browsertests into synchronous MTE
    Bug: 1137393
    Change-Id: I7f23a9e6731ec4c6387bf6ca9841635a9d5c292d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4441010
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1135931}

---
+++ components/memory_system/memory_system_tests.cc
@@ -0,0 +1,29 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/allocator/partition_allocator/tagging.h"
+#include "base/cpu.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+
+namespace memory_system {
+
+// This test suite contains a check that components_browsertests starts up under
+// the correct MTE mode. It's only relevant on Android systems.
+
+class MemorySystemTest : public ::testing::Test {};
+
+TEST_F(MemorySystemTest, VerifyCorrectMTEMode) {
+  // Check that components_browsertests starts up in MTE synchronous mode as
+  // long as the hardware supports it.
+  base::CPU cpu;
+  if (cpu.has_mte()) {
+    ASSERT_EQ(partition_alloc::internal::GetMemoryTaggingModeForCurrentThread(),
+              partition_alloc::TagViolationReportingMode::kSynchronous);
+  } else {
+    GTEST_SKIP();
+  }
+}
+
+}  // namespace memory_system
