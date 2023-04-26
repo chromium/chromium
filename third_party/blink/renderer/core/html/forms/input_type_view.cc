6fe8ea3052841f5cb199b2998b828e5ddf5a1361
Kent Tamura <tkent@chromium.org>
2023-04-26 15:59:33 +0000

    DateInputInlineBlock: Fix rendering with display:inline
    We should not use LayoutInline for <input>.
    
    Bug: 970249, 1432710
    Change-Id: I2e1b1b1cf2e474263707b630682ab733475e4843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4478351
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135940}

--- third_party/blink/renderer/core/html/forms/input_type_view.cc
+++ third_party/blink/renderer/core/html/forms/input_type_view.cc
@@ -36,7 +36,7 @@
 #include "third_party/blink/renderer/core/html/forms/form_controller.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
-#include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_block_flow.h"
 
 namespace blink {
 
@@ -95,6 +95,11 @@ HTMLFormElement* InputTypeView::FormForSubmission() const {
 
 LayoutObject* InputTypeView::CreateLayoutObject(
     const ComputedStyle& style) const {
+  // Avoid LayoutInline, which can be split to multiple lines.
+  if (RuntimeEnabledFeatures::DateInputInlineBlockEnabled() &&
+      style.IsDisplayInlineType() && !style.IsDisplayReplacedType()) {
+    return MakeGarbageCollected<LayoutNGBlockFlow>(&GetElement());
+  }
   return LayoutObject::CreateObject(&GetElement(), style);
 }
 
