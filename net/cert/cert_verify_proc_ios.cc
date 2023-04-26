cc1c97c276e2da043120edeca407ded3f8d67855
Matt Mueller <mattm@chromium.org>
2023-04-26 00:31:56 +0000

    Add ScopedTestKnownRoot helper for marking a test cert as known root
    Change-Id: If491ca75c4ef258ed592016313abf0cd1e5ad786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4469297
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1135649}

--- net/cert/cert_verify_proc_ios.cc
+++ net/cert/cert_verify_proc_ios.cc
@@ -20,6 +20,7 @@
 #include "net/cert/known_roots.h"
 #include "net/cert/test_root_certs.h"
 #include "net/cert/x509_certificate.h"
+#include "net/cert/x509_util.h"
 #include "net/cert/x509_util_apple.h"
 
 using base::ScopedCFTypeRef;
@@ -505,6 +506,13 @@ int CertVerifyProcIOS::VerifyInternal(
 
   if (IsCertStatusError(verify_result->cert_status))
     return MapCertStatusToNetError(verify_result->cert_status);
+
+  if (TestRootCerts::HasInstance() &&
+      !verify_result->verified_cert->intermediate_buffers().empty() &&
+      TestRootCerts::GetInstance()->IsKnownRoot(x509_util::CryptoBufferAsSpan(
+          verify_result->verified_cert->intermediate_buffers().back().get()))) {
+    verify_result->is_issued_by_known_root = true;
+  }
 
   LogNameNormalizationMetrics(".IOS", verify_result->verified_cert.get(),
                               verify_result->is_issued_by_known_root);
