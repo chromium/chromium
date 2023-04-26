2c935d81ed5407bd235fc54dc4a0037941022ce6
Sunny Sachanandani <sunnyps@chromium.org>
2023-04-26 17:38:20 +0000

    canvas: Enable OneCopyCanvasCapture on Windows by default
    The feature needs to be enabled by default on trunk before it can be
    launched via Finch.
    
    Bug: 1317785
    Change-Id: I68b6ea5979e401164fbbbbcffbc717a5717a83df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4477465
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136009}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -125,7 +125,7 @@ namespace {
 // TODO(https://crbug.com/1298812): Investigate why this fails on Windows.
 BASE_FEATURE(kOneCopyCanvasCapture,
              "OneCopyCanvasCapture",
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
