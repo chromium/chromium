51cd8e2aa50de655be6fc670f1b9268e0e5b5857
Petro Akzhygitov <pakzhygitov@google.com>
2023-04-26 12:49:01 +0000

    Updated default MDCSnackbar's font to support the Dynamic Types.
    Fixed: 1439946
    Change-Id: Ia7cc9614b1a732aaaaa6d6c96bfcdb333c9593fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476690
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1135852}

--- ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
+++ ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
@@ -38,6 +38,13 @@
 
 - (void)start {
   DCHECK(self.browser);
+
+  // Set the font which supports the Dynamic Type.
+  UIFont* defaultSnackbarFont =
+      [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+  [[MDCSnackbarManager defaultManager] setMessageFont:defaultSnackbarFont];
+  [[MDCSnackbarManager defaultManager] setButtonFont:defaultSnackbarFont];
+
   CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
   [dispatcher startDispatchingToTarget:self
                            forProtocol:@protocol(SnackbarCommands)];
