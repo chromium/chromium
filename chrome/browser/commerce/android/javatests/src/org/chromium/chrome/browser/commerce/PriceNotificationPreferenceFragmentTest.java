101682763a51017d0637d676ed0183ec6566900a
Matt Jones <mdjones@chromium.org>
2023-04-27 16:21:04 +0000

    Ensure there is a primary account in price notification settings
    This patch ensures that there is a primary account before trying to
    access the email address to show in the toggle text. Since sync or
    another activity can cause the primary account to be null, we need to
    handle that case. Now, if null, we simply hide the toggle.
    
    Bug: 1433360
    Change-Id: I305e9c04233c1837f3149cbdf454b4c98b25f108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4471858
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136579}

---
+++ chrome/browser/commerce/android/javatests/src/org/chromium/chrome/browser/commerce/PriceNotificationPreferenceFragmentTest.java
@@ -0,0 +1,115 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.commerce;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.preferences.Pref;
+import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
+import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
+import org.chromium.components.prefs.PrefService;
+import org.chromium.components.signin.base.CoreAccountInfo;
+import org.chromium.components.signin.identitymanager.ConsentLevel;
+import org.chromium.components.signin.identitymanager.IdentityManager;
+
+/**
+ * Tests for the price notification setting UI. These tests are not batched because each requires
+ * restarting the settings activity.
+ */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DoNotBatch(reason = "Layout and behavior are dependent on setup params for the activity.")
+public class PriceNotificationPreferenceFragmentTest {
+    public final SettingsActivityTestRule<PriceNotificationPreferenceFragment> mTestRule =
+            new SettingsActivityTestRule<>(PriceNotificationPreferenceFragment.class);
+
+    public final ChromeTabbedActivityTestRule mActivityTestRule =
+            new ChromeTabbedActivityTestRule();
+
+    @Mock
+    private IdentityServicesProvider mIdentityServicesProvider;
+
+    @Mock
+    private IdentityManager mIdentityManager;
+
+    @Mock
+    private PrefService mPrefs;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        // Make sure the browser is set up correctly prior to mocking everything for settings.
+        mActivityTestRule.startMainActivityOnBlankPage();
+
+        when(mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SYNC))
+                .thenReturn(CoreAccountInfo.createFromEmailAndGaiaId("user@example.com", "12345"));
+        when(mIdentityServicesProvider.getIdentityManager(any())).thenReturn(mIdentityManager);
+
+        IdentityServicesProvider.setInstanceForTests(mIdentityServicesProvider);
+    }
+
+    @Test
+    @SmallTest
+    @Feature("PriceTrackingSettings")
+    public void testEmailPreferenceToggleVisibleWithValidAccount() {
+        // The test suite setup includes a mock account, no extra work needs to be done before
+        // starting.
+        mTestRule.startSettingsActivity();
+
+        Assert.assertTrue(getEmailNotificationsSwitch().isVisible());
+    }
+
+    @Test
+    @SmallTest
+    @Feature("PriceTrackingSettings")
+    public void testEmailPreferenceToggleInvisibleIfNoAccount() {
+        when(mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SYNC)).thenReturn(null);
+
+        mTestRule.startSettingsActivity();
+
+        Assert.assertFalse(getEmailNotificationsSwitch().isVisible());
+    }
+
+    @Test
+    @SmallTest
+    @Feature("PriceTrackingSettings")
+    public void testToggleUpdatesEmailPref() {
+        mTestRule.startSettingsActivity();
+        mTestRule.getFragment().setPrefServiceForTesting(mPrefs);
+
+        Assert.assertFalse(getEmailNotificationsSwitch().isChecked());
+
+        ThreadUtils.runOnUiThreadBlocking(() -> getEmailNotificationsSwitch().performClick());
+        verify(mPrefs).setBoolean(Pref.PRICE_EMAIL_NOTIFICATIONS_ENABLED, true);
+
+        ThreadUtils.runOnUiThreadBlocking(() -> getEmailNotificationsSwitch().performClick());
+        verify(mPrefs).setBoolean(Pref.PRICE_EMAIL_NOTIFICATIONS_ENABLED, false);
+    }
+
+    private ChromeSwitchPreference getEmailNotificationsSwitch() {
+        return (ChromeSwitchPreference) mTestRule.getFragment().findPreference(
+                PriceNotificationPreferenceFragment.PREF_EMAIL_NOTIFICATIONS);
+    }
+}
