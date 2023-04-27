cbf83475668794bc672a1222d2426cef59ab207d
Dibyajyoti Pal <dibyapal@google.com>
2023-04-27 23:45:45 +0000

    [dPWA] Disable external pref migration
    This CL disables the external pref migration since the metrics seem
    to be looking alright.
    
    There was a typo in the install URL migration metric which wasn't caught
    by tests or UMA, so there are no metrics recorded for that
    unfortunately. I modified the logic to add the correct flag, so that
    will be measured soon.
    
    Bug: 1339965, b/278875969
    Change-Id: Iecc8b808817ceaaa4caadcbe0368674ea12eb40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484888
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136869}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -815,7 +815,7 @@ BASE_FEATURE(kMetricsSettingsAndroid,
 
 BASE_FEATURE(kMigrateExternalPrefsToWebAppDB,
              "MigrateExternalPrefsToWebAppDB",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMoveWebApp,
              "MoveWebApp",
