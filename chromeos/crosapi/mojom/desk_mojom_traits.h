dbdf2e9e672a62ba8554b7fa4f22f588e50cdadd
Claudio DeSouza <cdesouza@igalia.com>
2023-04-27 18:31:36 +0000

    Replace base::GUID uses with base::Uuid (218/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chromeos/crosapi/mojom
    
    This CL was uploaded by git cl split.
    
    R=rbock@google.com
    
    Bug: 1428566
    Change-Id: I7f4c3a27cd133e558d6a0635c8879feb05321143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4484517
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1136684}

--- chromeos/crosapi/mojom/desk_mojom_traits.h
+++ chromeos/crosapi/mojom/desk_mojom_traits.h
@@ -5,17 +5,17 @@
 #ifndef CHROMEOS_CROSAPI_MOJOM_DESK_MOJOM_TRAITS_H_
 #define CHROMEOS_CROSAPI_MOJOM_DESK_MOJOM_TRAITS_H_
 
-#include "base/guid.h"
+#include "base/uuid.h"
 #include "chromeos/crosapi/mojom/desk.mojom.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 
 namespace mojo {
 
 template <>
-class StructTraits<crosapi::mojom::GUIDDataView, base::GUID> {
+class StructTraits<crosapi::mojom::GUIDDataView, base::Uuid> {
  public:
-  static std::string lowercase(const base::GUID& guid);
-  static bool Read(crosapi::mojom::GUIDDataView guid, base::GUID* out_guid);
+  static std::string lowercase(const base::Uuid& guid);
+  static bool Read(crosapi::mojom::GUIDDataView guid, base::Uuid* out_guid);
 };
 
 }  // namespace mojo
