5861ae50c916e215fb192f9784648942b0698168
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2023-04-27 23:10:34 +0000

    Fix invalid pointer read in UsedColorSchemeScrollbars()
    The CL switches the usage of ComputedStyle& to ComputedStyle* in
    PaintLayerScrollableArea::UsedColorSchemeScrollbars().
    
    The issue happens in the middle of the style update after
    PaintLayerScrollableArea::UsedColorSchemeScrollbars is called from
    ScrollableArea::RecalculateScrollbarOverlayColorTheme. Please see the
    example stack trace in the CrBug description.
    
    Bug: 1439913
    Change-Id: I9c113d97c444701af47432d4be5adfc2450fb850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4476566
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1136852}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -1202,7 +1202,8 @@ mojom::blink::ColorScheme PaintLayerScrollableArea::UsedColorSchemeScrollbars()
       !GetPageScrollbarTheme().UsesOverlayScrollbars()) {
     const Document& document = GetLayoutBox()->GetDocument();
     if (document.documentElement() &&
-        document.documentElement()->ComputedStyleRef().ColorScheme().empty() &&
+        document.documentElement()->GetComputedStyle() &&
+        document.documentElement()->GetComputedStyle()->ColorScheme().empty() &&
         document.GetStyleEngine().GetPageColorSchemes() ==
             static_cast<ColorSchemeFlags>(ColorSchemeFlag::kNormal) &&
         document.GetPreferredColorScheme() ==
