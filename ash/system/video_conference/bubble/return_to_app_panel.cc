53880ad8d08faef7520cbf69865a3e2ac655070a
Andre Le <leandre@chromium.org>
2023-04-27 17:21:51 +0000

    Reland "VC UI: Add performance metrics for return to app animations."
    This reverts commit b1c44939bc0904b6b30d74b8e31ffb007d77f1cb.
    
    Fix in the reland: The test failed because throughput data might be collected asynchronously before we initialize HistogramTester. This reland adds an extra wait step to take care of this.
    
    Original change's description:
    > Revert "VC UI: Add performance metrics for return to app animations."
    >
    > This reverts commit 3f704f30744600788f8bf44e1af6e40dd49fc850.
    >
    > Reason for revert: test ReturnToAppPanelTest.CollapseAnimation is failing on multiple builders:
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/32159/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/65948/overview
    >
    > Original change's description:
    > > VC UI: Add performance metrics for return to app animations.
    > >
    > > Bug: b:267373906
    > > Change-Id: I41a9896bb88fe15c69eb187e7203a06659944581
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4448847
    > > Reviewed-by: Tony Yeoman <tby@chromium.org>
    > > Commit-Queue: Andre Le <leandre@chromium.org>
    > > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1133186}
    >
    > Bug: b:267373906
    > Change-Id: Ifc9fa68d1c324934bca77fa799e9c1928489e529
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4455014
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: David Baron <dbaron@chromium.org>
    > Auto-Submit: David Baron <dbaron@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1133301}
    
    Bug: b:267373906
    Change-Id: I3f568fa27ee473f68c9a6cbc124c76fe7661dda0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4456510
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1136617}

--- ash/system/video_conference/bubble/return_to_app_panel.cc
+++ ash/system/video_conference/bubble/return_to_app_panel.cc
@@ -7,13 +7,16 @@
 #include <memory>
 #include <string>
 
+#include "ash/public/cpp/metrics_util.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/video_conference/bubble/bubble_view_ids.h"
 #include "ash/system/video_conference/video_conference_tray_controller.h"
+#include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
@@ -21,8 +24,11 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/compositor/animation_throughput_reporter.h"
+#include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
+#include "ui/compositor/throughput_tracker.h"
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/canvas.h"
@@ -34,6 +40,7 @@
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/view_class_properties.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash::video_conference {
 
@@ -51,8 +58,38 @@ const int kReturnToAppIconSize = 20;
 
 constexpr auto kPanelBoundsChangeAnimationDuration = base::Milliseconds(200);
 
+void StartReportLayerAnimationSmoothness(
+    const std::string& animation_histogram_name,
+    int smoothness) {
+  if (animation_histogram_name.empty()) {
+    return;
+  }
+  base::UmaHistogramPercentage(animation_histogram_name, smoothness);
+}
+
+void StartRecordAnimationSmoothness(
+    views::Widget* widget,
+    absl::optional<ui::ThroughputTracker>& tracker) {
+  // `widget` may not exist in tests.
+  if (!widget) {
+    return;
+  }
+
+  tracker.emplace(widget->GetCompositor()->RequestNewThroughputTracker());
+  tracker->Start(
+      ash::metrics_util::ForSmoothness(base::BindRepeating([](int smoothness) {
+        base::UmaHistogramPercentage(
+            "Ash.VideoConference.ReturnToAppPanel.BoundsChange."
+            "AnimationSmoothness",
+            smoothness);
+      })));
+}
+
 // Performs fade in/fade out animation using `AnimationBuilder`.
-void FadeInView(views::View* view, int delay_in_ms, int duration_in_ms) {
+void FadeInView(views::View* view,
+                int delay_in_ms,
+                int duration_in_ms,
+                const std::string& animation_histogram_name) {
   // If we are in testing with animation (non zero duration), we shouldn't have
   // delays so that we can properly track when animation is completed in test.
   if (ui::ScopedAnimationDurationScaleMode::duration_multiplier() ==
@@ -62,6 +99,11 @@ void FadeInView(views::View* view, int delay_in_ms, int duration_in_ms) {
 
   // The view must have a layer to perform animation.
   CHECK(view->layer());
+
+  ui::AnimationThroughputReporter reporter(
+      view->layer()->GetAnimator(),
+      metrics_util::ForSmoothness(base::BindRepeating(
+          &StartReportLayerAnimationSmoothness, animation_histogram_name)));
 
   views::AnimationBuilder()
       .SetPreemptionStrategy(
@@ -75,7 +117,8 @@ void FadeInView(views::View* view, int delay_in_ms, int duration_in_ms) {
 }
 
 void FadeOutView(views::View* view,
-                 base::WeakPtr<ReturnToAppPanel> parent_weak_ptr) {
+                 base::WeakPtr<ReturnToAppPanel> parent_weak_ptr,
+                 const std::string& animation_histogram_name) {
   auto on_animation_ended = base::BindOnce(
       [](base::WeakPtr<ReturnToAppPanel> parent_weak_ptr, views::View* view) {
         if (parent_weak_ptr) {
@@ -90,6 +133,11 @@ void FadeOutView(views::View* view,
 
   // The view must have a layer to perform animation.
   CHECK(view->layer());
+
+  ui::AnimationThroughputReporter reporter(
+      view->layer()->GetAnimator(),
+      metrics_util::ForSmoothness(base::BindRepeating(
+          &StartReportLayerAnimationSmoothness, animation_histogram_name)));
 
   view->SetVisible(true);
   views::AnimationBuilder()
@@ -325,7 +373,9 @@ void ReturnToAppButton::OnButtonClicked(const base::UnguessableToken& id) {
   expand_indicator_->SetTooltipText(l10n_util::GetStringUTF16(tooltip_text_id));
 
   if (icons_container_->GetVisible()) {
-    FadeInView(icons_container_, /*delay_in_ms=*/100, /*duration_in_ms=*/100);
+    FadeInView(icons_container_, /*delay_in_ms=*/100, /*duration_in_ms=*/100,
+               /*animation_histogram_name=*/
+               "Ash.VideoConference.SummaryIcons.FadeIn.AnimationSmoothness");
   }
 }
 
@@ -358,6 +408,7 @@ void ReturnToAppPanel::ReturnToAppContainer::StartExpandCollapseAnimation() {
   CHECK(!animation_->is_animating());
 
   animation_->Start();
+  StartRecordAnimationSmoothness(GetWidget(), throughput_tracker_);
 }
 
 void ReturnToAppPanel::ReturnToAppContainer::AnimationProgressed(
@@ -368,6 +419,12 @@ void ReturnToAppPanel::ReturnToAppContainer::AnimationProgressed(
 void ReturnToAppPanel::ReturnToAppContainer::AnimationEnded(
     const gfx::Animation* animation) {
   PreferredSizeChanged();
+
+  if (throughput_tracker_) {
+    // Reset `throughput_tracker_` to record animation smoothness.
+    throughput_tracker_->Stop();
+    throughput_tracker_.reset();
+  }
 }
 
 void ReturnToAppPanel::ReturnToAppContainer::AnimationCanceled(
@@ -443,9 +500,14 @@ void ReturnToAppPanel::OnExpandedStateChanged(bool expanded) {
     child->SetVisible(expanded);
 
     if (expanded) {
-      FadeInView(child, /*delay_in_ms=*/50, /*duration_in_ms=*/150);
+      FadeInView(
+          child, /*delay_in_ms=*/50,
+          /*duration_in_ms=*/150, /*animation_histogram_name=*/
+          "Ash.VideoConference.ReturnToAppButton.FadeIn.AnimationSmoothness");
     } else {
-      FadeOutView(child, weak_ptr_factory_.GetWeakPtr());
+      FadeOutView(
+          child, weak_ptr_factory_.GetWeakPtr(), /*animation_histogram_name=*/
+          "Ash.VideoConference.ReturnToAppButton.FadeOut.AnimationSmoothness");
     }
   }
 
