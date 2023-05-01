ae4b779de214089968fe40e0531d8639e45b3f31
Alexis Hétu <sugoi@chromium.org>
2023-05-01 20:59:04 +0000

    [iOS] Add proper reauthentication to Password Bottom Sheet
    Upon using a suggestion from the Password Bottom Sheet, users
    will now be required to authenticate.
    
    Bug: 1422362
    Change-Id: Ib47082993f67796a75f1cdeb7c579c8147f92e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4474986
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138004}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
@@ -22,9 +22,12 @@
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
 #import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #import "ios/chrome/common/ui/favicon/favicon_constants.h"
+#import "ios/chrome/common/ui/reauthentication/reauthentication_protocol.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/web_state.h"
 #import "ios/web/public/web_state_observer_bridge.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -166,7 +169,27 @@
   DCHECK(row >= 0);
 
   FormSuggestion* suggestion = [self.suggestions objectAtIndex:row];
-  [self.suggestionsProvider didSelectSuggestion:suggestion];
+
+  if (!suggestion.requiresReauth) {
+    [self selectSuggestion:suggestion];
+    return;
+  }
+  if ([_reauthenticationModule canAttemptReauth]) {
+    __weak __typeof(self) weakSelf = self;
+    auto completionHandler = ^(ReauthenticationResult result) {
+      if (result != ReauthenticationResult::kFailure) {
+        [weakSelf selectSuggestion:suggestion];
+      }
+    };
+
+    NSString* reason = l10n_util::GetNSString(IDS_IOS_AUTOFILL_REAUTH_REASON);
+    [_reauthenticationModule
+        attemptReauthWithLocalizedReason:reason
+                    canReusePreviousAuth:YES
+                                 handler:completionHandler];
+  } else {
+    [self selectSuggestion:suggestion];
+  }
 }
 
 - (void)refocus {
@@ -259,6 +282,11 @@
 }
 
 #pragma mark - Private
+
+// Perform suggestion selection
+- (void)selectSuggestion:(FormSuggestion*)suggestion {
+  [self.suggestionsProvider didSelectSuggestion:suggestion];
+}
 
 // Returns the display description at a given row in the table view.
 - (NSString*)descriptionAtRow:(NSInteger)row {
