6d90f6003ef378f13c29f604e9545aff155c03ce
Ella Ge <eirage@chromium.org>
2023-05-01 21:05:52 +0000

    Remove UMA Webapp.NavigationStatus and WebappActiveTabUmaTracker.java
    The UMA expired 1.5 years ago.
    
    Bug: 1249130
    Change-Id: I839cfc9651c23c5d99df13444d2d4e07420b55a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493712
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138009}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkInitializationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkInitializationTest.java
@@ -14,8 +14,8 @@ import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.browserservices.ui.SharedActivityCoordinator;
 import org.chromium.chrome.browser.browserservices.ui.controller.webapps.WebappDisclosureController;
@@ -38,6 +38,7 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DoNotBatch(reason = "Activity initialzation test")
 public class WebApkInitializationTest {
     /**
      * {@link ActivityLifecycleDispatcher} wrapper which tracks {@link LifecycleObserver}
@@ -154,9 +155,5 @@ public class WebApkInitializationTest {
                 WebApkActivityLifecycleUmaTracker.class.getName()));
         assertTrue(
                 registeredObserverClassNames.contains(SharedActivityCoordinator.class.getName()));
-
-        // Test that WebappActiveTabUmaTracker is hooked up.
-        assertTrue(0 < RecordHistogram.getHistogramTotalCountForTesting(
-                           WebappActiveTabUmaTracker.HISTOGRAM_NAVIGATION_STATUS));
     }
 }
