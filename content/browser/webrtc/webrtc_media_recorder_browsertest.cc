359c5e3a5a5fe33d10f14ff2608dff1d91eb8179
Markus Handell <handellm@google.com>
2023-05-02 11:43:54 +0000

    ResumeAndDataAvailable: Disable for Android.
    Fixed: chromium:1432939
    Change-Id: Ib4dbe703dfa938d9ceb717b02b78541c0bfdce52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4498389
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Markus Handell <handellm@google.com>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138270}

--- content/browser/webrtc/webrtc_media_recorder_browsertest.cc
+++ content/browser/webrtc/webrtc_media_recorder_browsertest.cc
@@ -102,7 +102,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcMediaRecorderTest, NoResumeWhenRecorderInactive) {
   MakeTypicalCall("testIllegalResumeThrowsDOMError();", kMediaRecorderHtmlFile);
 }
 
-#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
+// TODO(crbug.com/1432939): Seems the test is not working quite well on
+// android-12l-x64-dbg-tests.
+#if (BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)) || BUILDFLAG(IS_ANDROID)
 // https://crbug.com/1222675
 #define MAYBE_ResumeAndDataAvailable DISABLED_ResumeAndDataAvailable
 #else
