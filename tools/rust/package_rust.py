a6930d4fb75f6d047dc9ed474c8782961846c81c
danakj <danakj@chromium.org>
2023-05-02 13:13:17 +0000

    Compress the Rust toolchain with xz instead of gzip
    Bug: 1439640
    Change-Id: I250dd11c73aa2d1add84b6bea7add74f66447e20
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496406
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138304}

--- tools/rust/package_rust.py
+++ tools/rust/package_rust.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 import argparse
+import lzma
 import os
 import platform
 import shutil
@@ -21,7 +22,7 @@ from update import (CHROMIUM_DIR)
 
 PACKAGE_VERSION = GetLatestRevision()
 BUILDLOG_NAME = f'rust-buildlog-{PACKAGE_VERSION}.txt'
-RUST_TOOLCHAIN_PACKAGE_NAME = f'rust-toolchain-{PACKAGE_VERSION}.tgz'
+RUST_TOOLCHAIN_PACKAGE_NAME = f'rust-toolchain-{PACKAGE_VERSION}.tar.xz'
 
 
 def BuildCrubit(build_mac_arm):
@@ -90,9 +91,10 @@ def main():
 
     BuildCrubit(args.build_mac_arm)
 
-    with tarfile.open(
-            os.path.join(THIRD_PARTY_DIR, RUST_TOOLCHAIN_PACKAGE_NAME),
-            'w:gz') as tar:
+    with tarfile.open(os.path.join(THIRD_PARTY_DIR,
+                                   RUST_TOOLCHAIN_PACKAGE_NAME),
+                      'w:xz',
+                      preset=9 | lzma.PRESET_EXTREME) as tar:
         tar.add(RUST_TOOLCHAIN_OUT_DIR, arcname='rust-toolchain')
 
     os.chdir(THIRD_PARTY_DIR)
--- tools/rust/update_rust.py
+++ tools/rust/update_rust.py
@@ -33,7 +33,7 @@ sys.path.append(
 # These fields are written by //tools/clang/scripts/upload_revision.py, and
 # should not be changed manually.
 RUST_REVISION = '17c11672167827b0dd92c88ef69f24346d1286dd'
-RUST_SUB_REVISION = 1
+RUST_SUB_REVISION = 2
 
 # Trunk on 2022-10-15.
 #
@@ -65,7 +65,7 @@ CRUBIT_SUB_REVISION = 1
 # TODO(lukasza): Include CRUBIT_REVISION and CRUBIT_SUB_REVISION once we
 # include Crubit binaries in the generated package.  See also a TODO comment
 # in BuildCrubit in package_rust.py.
-FALLBACK_REVISION = '17c11672167827b0dd92c88ef69f24346d1286dd-1-llvmorg-17-init-8029-g27f27d15-1'
+FALLBACK_REVISION = '17c11672167827b0dd92c88ef69f24346d1286dd-2-llvmorg-17-init-8029-g27f27d15-4'
 
 # Hash of src/stage0.json, which itself contains the stage0 toolchain hashes.
 # We trust the Rust build system checks, but to ensure it is not tampered with
@@ -170,11 +170,11 @@ def main():
     platform_prefix = GetPlatformUrlPrefix(GetDefaultHostOs())
 
     version = GetLatestRevision()
-    url = f'{platform_prefix}rust-toolchain-{version}.tgz'
+    url = f'{platform_prefix}rust-toolchain-{version}.tar.xz'
     if not CheckUrl(url):
         print("Latest Rust toolchain not found. Using fallback revision.")
         version = FALLBACK_REVISION
-        url = f'{platform_prefix}rust-toolchain-{version}.tgz'
+        url = f'{platform_prefix}rust-toolchain-{version}.tar.xz'
         if not CheckUrl(url):
             print('error: Could not find Rust toolchain package')
             return 1
