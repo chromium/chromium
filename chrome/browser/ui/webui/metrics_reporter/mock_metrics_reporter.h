e355a60a4a79bf734be5b89ac9c52c41c0f01ffe
Simon Hangl <simonha@google.com>
2023-05-02 15:51:06 +0000

    [CodeHealth] Fixed mock method and formatting
    Fix for /chrome/browser/ui/webui/metrics_reporter
    
    This CL was uploaded by git cl split.
    
    R=yuhengh@chromium.org
    
    Bug: b/280051002
    Change-Id: I1970e7556840c050d579d6f89ebffa87b9bd3764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4488947
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Auto-Submit: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138391}

--- chrome/browser/ui/webui/metrics_reporter/mock_metrics_reporter.h
+++ chrome/browser/ui/webui/metrics_reporter/mock_metrics_reporter.h
@@ -14,17 +14,20 @@ class MockMetricsReporter : public MetricsReporter {
   MockMetricsReporter();
   ~MockMetricsReporter() override;
 
-  MOCK_METHOD1(Mark, void(const std::string&));
-  MOCK_METHOD2(Measure,
-               void(const std::string&, MetricsReporter::MeasureCallback));
-  MOCK_METHOD3(Measure,
-               void(const std::string&,
-                    const std::string&,
-                    MetricsReporter::MeasureCallback));
-  MOCK_METHOD2(HasMark,
-               void(const std::string&, MetricsReporter::HasMarkCallback));
-  MOCK_METHOD1(HasLocalMark, bool(const std::string&));
-  MOCK_METHOD1(ClearMark, void(const std::string&));
+  MOCK_METHOD(void, Mark, (const std::string&));
+  MOCK_METHOD(void,
+              Measure,
+              (const std::string&, MetricsReporter::MeasureCallback));
+  MOCK_METHOD(void,
+              Measure,
+              (const std::string&,
+               const std::string&,
+               MetricsReporter::MeasureCallback));
+  MOCK_METHOD(void,
+              HasMark,
+              (const std::string&, MetricsReporter::HasMarkCallback));
+  MOCK_METHOD(bool, HasLocalMark, (const std::string&));
+  MOCK_METHOD(void, ClearMark, (const std::string&));
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_METRICS_REPORTER_MOCK_METRICS_REPORTER_H_
