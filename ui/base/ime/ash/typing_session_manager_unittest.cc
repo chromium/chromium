ce06412dc57993834d6794f3f43ebd08e9d27325
Claudio DeSouza <cdesouza@igalia.com>
2023-05-02 23:48:27 +0000

    Replace base::GUID uses with base::Uuid (251/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /ui/base/ime
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: Ib699ff91ca34c3076d1adabe7d55b8ca6dbd21f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4482451
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1138682}

--- ui/base/ime/ash/typing_session_manager_unittest.cc
+++ ui/base/ime/ash/typing_session_manager_unittest.cc
@@ -7,7 +7,6 @@
 #include <memory>
 #include <utility>
 
-#include "base/guid.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/simple_test_clock.h"
