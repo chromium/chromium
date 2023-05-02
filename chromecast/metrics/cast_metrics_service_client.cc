ff64d7cf1d56ef112418f5921cb1fac743206a4e
Claudio DeSouza <cdesouza@igalia.com>
2023-05-02 19:36:57 +0000

    Replace base::GUID uses with base::Uuid (212/N)
    `base::GUID` has been renamed, and is set to be removed. This CL is
    part of a batch of changes replacing all uses of `base::GUID`, with
    `base::Uuid`, and its associated counterparts.
    
    Additionally, deprecated functions in `base/uuid.h` are also having
    their uses removed wherever they occur.
    
    Changes for path: /chromecast/metrics
    
    This CL was uploaded by git cl split.
    
    Bug: 1428566
    Change-Id: I88ab6f7c4f98a541426ca4e29580df6e59dee925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4481325
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Auto-Submit: Claudio DeSouza <cdesouza@igalia.com>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138572}

--- chromecast/metrics/cast_metrics_service_client.cc
+++ chromecast/metrics/cast_metrics_service_client.cc
@@ -10,11 +10,11 @@
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/guid.h"
 #include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/uuid.h"
 #include "build/build_config.h"
 #include "chromecast/base/cast_sys_info_util.h"
 #include "chromecast/base/chromecast_switches.h"
@@ -125,7 +125,8 @@ CastMetricsServiceClient::LoadClientInfo() {
     // instead of just being FDR'ed.
     if (!pref_service_->GetString(kMetricsOldClientID).empty()) {
       // Force old client id to be regenerated. See b/9487011.
-      client_info->client_id = base::GenerateGUID();
+      client_info->client_id =
+          base::Uuid::GenerateRandomV4().AsLowercaseString();
       pref_service_->SetBoolean(prefs::kMetricsIsNewClientID, true);
       return client_info;
     }
@@ -133,7 +134,8 @@ CastMetricsServiceClient::LoadClientInfo() {
   }
 
   // Use "forced" client ID if available.
-  if (!force_client_id_.empty() && base::IsValidGUID(force_client_id_)) {
+  if (!force_client_id_.empty() &&
+      base::Uuid::ParseCaseInsensitive(force_client_id_).is_valid()) {
     client_info->client_id = force_client_id_;
     return client_info;
   }
