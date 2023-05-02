dabdb7225c737cd94b844e63561a555e19bff54b
Jiaming Cheng <jiamingc@chromium.org>
2023-05-02 01:46:11 +0000

    Add pixel tests for audio and display subpages
    Bug: b/252870214
    Change-Id: Ide1d2db8674cebe523ec0f9db41ad24554d279bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4496518
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138133}

---
+++ ash/system/brightness/display_detailed_view_pixeltest.cc
@@ -0,0 +1,54 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/constants/ash_features.h"
+#include "ash/system/unified/unified_system_tray.h"
+#include "ash/system/unified/unified_system_tray_bubble.h"
+#include "ash/test/ash_test_base.h"
+#include "ash/test/pixel/ash_pixel_differ.h"
+#include "ash/test/pixel/ash_pixel_test_init_params.h"
+#include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
+#include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
+
+namespace ash {
+
+// Pixel tests for the quick settings display detailed view.
+class DisplayDetailedViewPixelTest : public AshTestBase {
+ public:
+  DisplayDetailedViewPixelTest() {
+    feature_list_.InitWithFeatures(
+        {features::kQsRevamp, chromeos::features::kJelly}, {});
+  }
+
+  // AshTestBase:
+  absl::optional<pixel_test::InitParams> CreatePixelTestInitParams()
+      const override {
+    return pixel_test::InitParams();
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(DisplayDetailedViewPixelTest, Basics) {
+  UnifiedSystemTray* system_tray = GetPrimaryUnifiedSystemTray();
+  system_tray->ShowBubble();
+  ASSERT_TRUE(system_tray->bubble());
+
+  system_tray->bubble()
+      ->unified_system_tray_controller()
+      ->ShowDisplayDetailedView();
+
+  auto* detailed_view =
+      system_tray->bubble()->quick_settings_view()->detailed_view();
+  ASSERT_TRUE(detailed_view);
+
+  EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
+      "qs_display_detailed_view",
+      /*revision_number=*/0, detailed_view));
+}
+
+}  // namespace ash
