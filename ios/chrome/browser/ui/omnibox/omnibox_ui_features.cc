8c6dc6f510385f880f8b84bf8b4ba27a7f1308f9
Stepan Khapugin <stkhapugin@chromium.org>
2023-05-03 14:23:12 +0000

    [iOS] Enable On Focus / MV Tiles by default on iOS.
    Enables the following features by default on iOS:
    * OmniboxFocusTriggersContextualWebZeroSuggest
    * OmniboxMostVisitedTiles
    * OmniboxFocusTriggersSRPZeroSuggest
    * EnableSuggestionsScrollingOnIPad
    
    Feature OmniboxMostVisitedTilesOnSrp is already
    enabled by default.
    
    Bug: none
    Change-Id: Ia6a4d8ea523c72c6c899ff53c834bea587a91cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502612
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1138907}

--- ios/chrome/browser/ui/omnibox/omnibox_ui_features.cc
+++ ios/chrome/browser/ui/omnibox/omnibox_ui_features.cc
@@ -10,7 +10,7 @@
 
 BASE_FEATURE(kEnableSuggestionsScrollingOnIPad,
              "EnableSuggestionsScrollingOnIPad",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnablePopoutOmniboxIpad,
              "EnablePopoutOmniboxIpad",
