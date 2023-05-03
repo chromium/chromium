95e96ea08512c3d53f28f00b6a0eb458cec9a5df
Ayu Ishii <ayui@chromium.org>
2023-05-03 18:39:21 +0000

    WebSQL: deprecation message
    This change adds a deprecation message for WebSQL.
    Deprecation message approved for M115 here [0].
    Following deprecation message steps here [1].
    
    [0]https://groups.google.com/u/2/a/chromium.org/g/blink-dev/c/fWYb6evVA-w
    [1]https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/frame/deprecation/README.md?q=f:deprecation%20message%20f:README&ss=chromium
    
    Bug: 695592
    Change-Id: I890f719615307007536ec1280b9d30df47e20273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4470809
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139036}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/websql-issue.js
@@ -0,0 +1,13 @@
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startURL(
+      'http://devtools.test:8000/inspector-protocol/resources/empty.html',
+      `Tests that deprecation issues are reported`);
+
+  await dp.Audits.enable();
+  const promise = dp.Audits.onceIssueAdded();
+  session.evaluate('openDatabase("testdb", "1", "", 1024)');
+
+  const result = await promise;
+  testRunner.log(result.params, 'Inspector issue: ');
+  testRunner.completeTest();
+})
