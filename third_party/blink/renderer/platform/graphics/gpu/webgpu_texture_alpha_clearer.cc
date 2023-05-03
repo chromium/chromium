3317725018bba559a7a2b48439200154b11a35ac
Loko Kung <lokokung@google.com>
2023-05-03 09:12:56 +0000

    Update deprecated ShaderModuleWGSLDescriptor.source to .code instead.
    Change-Id: I6c3bd0ab580c1cee5ae308993b5fbbb7f9d0d5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4494183
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Auto-Submit: Loko Kung <lokokung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138844}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_texture_alpha_clearer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_texture_alpha_clearer.cc
@@ -19,7 +19,7 @@ WebGPUTextureAlphaClearer::WebGPUTextureAlphaClearer(
 
   WGPUShaderModuleWGSLDescriptor wgsl_desc = {
       .chain = {.sType = WGPUSType_ShaderModuleWGSLDescriptor},
-      .source = R"(
+      .code = R"(
     @vertex fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> @builtin(position) vec4<f32> {
         var pos = array<vec2<f32>, 3>(
             vec2<f32>(-1.0, -1.0),
