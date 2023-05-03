c13b21b9b0eccc084dbb9578eee4d86bdb2a23b3
Guillem Perez <guiperez@google.com>
2023-05-03 16:32:28 +0000

    [iOS] Fix top sync promo text alignment bug.
    Bug fix for the top sync promo text alignment. The text was aligned to
    the center by default.
    
    Bug: 1441794
    Change-Id: I49ef2ced4d95bd9c30d557751f180d68ca0942d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497519
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1138941}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
@@ -709,6 +709,7 @@ constexpr CGFloat kCompactStyleTextSize = 15.0;
       self.titleLabel.font =
           [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
       self.titleLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
+      self.titleLabel.textAlignment = NSTextAlignmentLeft;
       self.textLabel.font =
           [UIFont preferredFontForTextStyle:UIFontTextStyleCallout];
       self.textLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
