cdd608b54cf5bb49d2b7db4fed34ccd804e213f3
Shu Yang <shuyng@google.com>
2023-05-03 23:32:19 +0000

    Append or remove switch USE_MOBILE_UA on screen size change
    So that foldable will have the correct switch after fold/unfold.
    
    Bug: 1408528
    Change-Id: Ied9d86b5fc98bda13bc3ffb1cbca8317535d26a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4497557
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1139260}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -221,6 +221,7 @@ import org.chromium.components.webapps.bottomsheet.PwaBottomSheetControllerProvi
 import org.chromium.components.webxr.XrDelegate;
 import org.chromium.components.webxr.XrDelegateProvider;
 import org.chromium.content_public.browser.ContentFeatureList;
+import org.chromium.content_public.browser.DeviceUtils;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.ScreenOrientationProvider;
 import org.chromium.content_public.browser.SelectionPopupController;
@@ -1717,7 +1718,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             @Override
             public void onCurrentModeChanged(Mode currentMode) {
                 if (ChromeFeatureList.isEnabled(ChromeFeatureList.FOLDABLE_JANK_FIX)
-                        && !mBlockingDrawForAppRestart && didChangeTabletMode()) {
+                        && !mBlockingDrawForAppRestart && getTabletMode().changed) {
                     mBlockingDrawForAppRestart = true;
                     findViewById(android.R.id.content).setVisibility(View.INVISIBLE);
                     showContent();
@@ -1729,8 +1730,9 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     }
 
     private boolean maybeOnScreenSizeChange() {
-        if (didChangeTabletMode()) {
-            return onScreenLayoutSizeChange();
+        TabletMode tabletMode = getTabletMode();
+        if (tabletMode.changed) {
+            return onScreenLayoutSizeChange(tabletMode.isTablet);
         }
         return false;
     }
@@ -2784,7 +2786,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     }
 
     @VisibleForTesting
-    public boolean didChangeTabletMode() {
+    public TabletMode getTabletMode() {
         assert mConfig
                 != null : "Can not determine the tablet mode when mConfig is not initialized";
         int smallestWidth = getCurrentSmallestScreenWidth(this);
@@ -2795,14 +2797,18 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (didChangeTabletMode && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Log.i(TAG, "Current smallest screen width is: " + smallestWidth);
         }
-        return didChangeTabletMode;
+        return new TabletMode(isTablet, didChangeTabletMode);
     }
 
     /**
      * Switch between phone and tablet mode and do the tab re-parenting in the meantime.
+     * Also update switch USE_MOBILE_UA depends on whether the device is tablet sized.
+     * @param isTablet whether the current screen is tablet size.
      * @return whether screen layout change lead to a recreate.
      */
-    private boolean onScreenLayoutSizeChange() {
+    private boolean onScreenLayoutSizeChange(boolean isTablet) {
+        DeviceUtils.updateDeviceSpecificUserAgentSwitch(isTablet);
+
         if (mTabReparentingControllerSupplier.get() != null && !mIsTabReparentingPrepared) {
             mTabReparentingControllerSupplier.get().prepareTabsForReparenting();
             mIsTabReparentingPrepared = true;
@@ -2906,5 +2912,19 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             Toast.makeText(context, R.string.open_webapk_failed, Toast.LENGTH_SHORT).show();
         }
         return true;
+    }
+
+    /**
+     * Preserve whether the current screen is tablet size; and whether the tablet mode has changed.
+     */
+    @VisibleForTesting
+    public static class TabletMode {
+        public boolean isTablet;
+        public boolean changed;
+
+        TabletMode(boolean isTablet, boolean changed) {
+            this.isTablet = isTablet;
+            this.changed = changed;
+        }
     }
 }
