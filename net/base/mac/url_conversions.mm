658c389767bde6894e7da27339f86829523bdcfc
Avi Drissman <avi@chromium.org>
2023-05-04 02:50:21 +0000

    Convert /net to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    Bug: 1280317
    Change-Id: Ib46103919620831f4fbe4f4c34d1bf4d061e4c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4502755
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139329}

--- net/base/mac/url_conversions.mm
+++ net/base/mac/url_conversions.mm
@@ -6,16 +6,20 @@
 
 #import <Foundation/Foundation.h>
 
-#include "base/mac/scoped_nsobject.h"
 #include "base/strings/escape.h"
 #include "url/gurl.h"
 #include "url/url_canon.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace net {
 
 NSURL* NSURLWithGURL(const GURL& url) {
-  if (!url.is_valid())
+  if (!url.is_valid()) {
     return nil;
+  }
 
   // NSURL strictly enforces RFC 1738 which requires that certain characters
   // are always encoded. These characters are: "<", ">", """, "#", "%", "{",
@@ -39,14 +43,15 @@ NSURL* NSURLWithGURL(const GURL& url) {
   }
   GURL escaped_url = url.ReplaceComponents(replacements);
 
-  base::scoped_nsobject<NSString> escaped_url_string(
-      [[NSString alloc] initWithUTF8String:escaped_url.spec().c_str()]);
+  NSString* escaped_url_string =
+      [NSString stringWithUTF8String:escaped_url.spec().c_str()];
   return [NSURL URLWithString:escaped_url_string];
 }
 
 GURL GURLWithNSURL(NSURL* url) {
-  if (url)
-    return GURL([[url absoluteString] UTF8String]);
+  if (url) {
+    return GURL(url.absoluteString.UTF8String);
+  }
   return GURL();
 }
 
