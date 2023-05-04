846366c4abcc6492afbcc2a0f34dcae59e5a8afc
Joe Downing <joedow@google.com>
2023-05-04 21:07:56 +0000

    Add usage_stats_consent impl for Linux
    This CL introduces a usage_stats_consent impl for Linux which checks
    the host owner and opts into crash reporting if they are a Googler
    otherwise it opts out.
    
    Change-Id: Ie4acc5eb781078672185d437fd045fc4c3a4701e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503310
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139772}

---
+++ remoting/host/usage_stats_consent_linux.cc
@@ -0,0 +1,68 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/usage_stats_consent.h"
+
+#include <memory>
+#include <string>
+
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/logging.h"
+#include "base/notreached.h"
+#include "base/strings/string_util.h"
+#include "base/values.h"
+#include "remoting/base/file_path_util_linux.h"
+#include "remoting/host/config_file_watcher.h"
+#include "remoting/host/host_config.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace remoting {
+
+namespace {
+// The default email domain for Googlers. Used to determine whether the host's
+// email address is Google-internal or not.
+constexpr char kGooglerEmailDomain[] = "@google.com";
+}  // namespace
+
+bool GetUsageStatsConsent(bool* allowed, bool* set_by_policy) {
+  *set_by_policy = false;
+  *allowed = false;
+
+  std::string filename = GetHostHash() + ".json";
+  base::FilePath config_path = GetConfigDirectoryPath().Append(filename);
+  absl::optional<base::Value::Dict> config(HostConfigFromJsonFile(config_path));
+  if (!config.has_value()) {
+    LOG(ERROR) << "No host config file found.";
+    return false;
+  }
+
+  // TODO(joedow): Check kUsageStatsConsentConfigPath to enable crash reporting
+  // for non-Google Linux hosts.  Also requires modifying the native message
+  // host to write the value during setup.
+  const std::string* host_owner_ptr = config->FindString(kHostOwnerConfigPath);
+  if (!host_owner_ptr) {
+    LOG(ERROR) << "Host config has no host_owner field.";
+    return false;
+  }
+
+  *allowed = base::EndsWith(*host_owner_ptr, kGooglerEmailDomain,
+                            base::CompareCase::INSENSITIVE_ASCII);
+
+  // Indicate that |allowed| was successfully initialized.
+  return true;
+}
+
+bool IsUsageStatsAllowed() {
+  bool allowed;
+  bool set_by_policy;
+  return GetUsageStatsConsent(&allowed, &set_by_policy) && allowed;
+}
+
+bool SetUsageStatsConsent(bool allowed) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace remoting
