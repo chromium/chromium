38c46117f6cc24adca3951e21fce80ee841dce97
kylechar <kylechar@chromium.org>
2023-05-04 20:17:01 +0000

    Add WebView flag for AllowBypassRenderPassQuads
    Needed to start finch study.
    
    Bug: 1367413
    Change-Id: If6ce3f4090aa990ecb9dea949bd0f3e21faab4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507812
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139737}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -122,6 +122,8 @@ public final class ProductionSupportedFlagList {
                             + "support."),
             Flag.baseFeature(GpuFeatures.WEBVIEW_THREAD_SAFE_MEDIA,
                     "Use thread-safe media path, requires Android P."),
+            Flag.baseFeature(VizFeatures.ALLOW_BYPASS_RENDER_PASS_QUADS,
+                    "Enable bypass render pass for RenderPassDrawQuads"),
             Flag.baseFeature(VizFeatures.WEBVIEW_NEW_INVALIDATE_HEURISTIC,
                     "More robust heuristic for calling Invalidate"),
             Flag.baseFeature(
