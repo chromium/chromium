51309633154fe7f02ddecc58a59bf7c4493e2475
Noémie St-Onge <noemies@google.com>
2023-05-04 16:14:38 +0000

    [iOS] Fix compromised state of password is not updated after an edit
    This CL rolls back a change made in the PasswordDetailsMediator (see
    link below) to again use the list of insecure credentials provided by
    the IOSChromePasswordCheckManager to determine whether a credential is
    compromised or not. The list of insecure credentials was indeed
    necessary because the `_credentials` list contained in the
    PasswordDetailsMediator is not updated when a credential is edited in
    the details page. Because of that, the updates on a credential were not
    always correctly reflected in the details page. This change also removes
    the code that was locally muting a compromised password since the
    insecure credentials list can now be used instead.
    
    Link of the change made in PasswordDetailsMediator:
    https://chromium-review.googlesource.com/c/chromium/src/+/4415224/6/ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.mm#b374
    
    (cherry picked from commit db0459b5fd4dc0b3e6228da2a0e757a1c92492df)
    
    Fixed: 1440794
    Change-Id: I6b377aa3e35c3ee3c9b3f096b37a452c0709b79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4491726
    Auto-Submit: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Veronique Nguyen <veronguyen@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1138396}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503793
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/branch-heads/5735@{#245}
    Cr-Branched-From: 2f562e4ddbaf79a3f3cb338b4d1bd4398d49eb67-refs/heads/main@{#1135570}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.mm
@@ -55,25 +55,31 @@ bool MatchesRealmUsernameAndPassword(PasswordDetails* password,
 // Helper that determines if a credential should be displayed as compromised in
 // password details. Even if a credential is compromised, it is only displayed
 // as such when password details was opened from the password manager or the
-// compromised password issues page.
-bool ShouldDisplayCredentialAsCompromised(DetailsContext details_context,
-                                          const CredentialUIEntry& credential) {
+// compromised password issues page. `insecure_credentials` is the list of
+// insecure credentials provided by the `_manager`
+// (IOSChromePasswordCheckManager). This list contains the most recent version
+// of the insecure credentials and is used here to determine if the
+// `_credential` is compromised or muted.
+bool ShouldDisplayCredentialAsCompromised(
+    DetailsContext details_context,
+    const CredentialUIEntry& credential,
+    std::vector<password_manager::CredentialUIEntry> insecure_credentials) {
   switch (details_context) {
     case DetailsContext::kGeneral:
     case DetailsContext::kCompromisedIssues:
-      return password_manager::features::IsPasswordCheckupEnabled()
-                 ? IsCredentialUnmutedCompromised(credential)
-                 : IsCompromised(credential);
+      for (const auto& insecure_credential : insecure_credentials) {
+        if (credential == insecure_credential) {
+          return password_manager::features::IsPasswordCheckupEnabled()
+                     ? IsCredentialUnmutedCompromised(insecure_credential)
+                     : IsCompromised(insecure_credential);
+        }
+      }
+      return false;
     case DetailsContext::kReusedIssues:
     case DetailsContext::kWeakIssues:
     case DetailsContext::kDismissedWarnings:
       return false;
   }
-}
-
-bool SupportsMuteOperation(password_manager::InsecureType insecure_type) {
-  return (insecure_type == password_manager::InsecureType::kLeaked ||
-          insecure_type == password_manager::InsecureType::kPhished);
 }
 
 }  // namespace
@@ -282,22 +288,6 @@ bool SupportsMuteOperation(password_manager::InsecureType insecure_type) {
   }
 
   _manager->MuteCredential(*it);
-
-  // TODO(crbug.com/1359392). Once kPasswordsGrouping launches, the mediator
-  // should update the passwords model and receive the updates via
-  // SavedPasswordsPresenterObserver, instead of replicating the updates to its
-  // own copy and calling [self providePasswordsToConsumer:]. Today when the
-  // flag is disabled and the password is edited, it's impossible to identify
-  // the new object to show (sign-on realm can't be used as an id, there might
-  // be multiple credentials; nor username/password since the values changed).
-  // The lines below should be removed once this is fixed.
-  for (auto& password_issue : it->password_issues) {
-    if (!password_issue.second.is_muted.value() &&
-        SupportsMuteOperation(password_issue.first)) {
-      password_issue.second.is_muted = password_manager::IsMuted(true);
-    }
-  }
-  [self providePasswordsToConsumer];
 }
 
 #pragma mark - PasswordDetailsTableViewControllerDelegate
@@ -431,12 +421,14 @@ bool SupportsMuteOperation(password_manager::InsecureType insecure_type) {
 // Pushes password details to the consumer.
 - (void)providePasswordsToConsumer {
   NSMutableArray<PasswordDetails*>* passwords = [NSMutableArray array];
+  std::vector<password_manager::CredentialUIEntry> insecureCredentials =
+      _manager->GetInsecureCredentials();
   for (const CredentialUIEntry& credential : _credentials) {
     PasswordDetails* password =
         [[PasswordDetails alloc] initWithCredential:credential];
     password.context = _context;
-    password.compromised =
-        ShouldDisplayCredentialAsCompromised(_context, credential);
+    password.compromised = ShouldDisplayCredentialAsCompromised(
+        _context, credential, insecureCredentials);
     // Only offer moving to the account if all of these hold.
     // - The embedder of this page wants to support it.
     // - The entry was flagged as local only in the top-level view.
