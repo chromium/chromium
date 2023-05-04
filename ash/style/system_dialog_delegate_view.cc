358862bb7dc0ae576a38d5fee3adbde224f8cd70
Xiaodan Zhu <zxdan@chromium.org>
2023-05-04 01:34:19 +0000

    cros_next: Add MaterialNext dialog (part 1)
    This cl adds the system dialog view that has the MaterialNext
    style.
    
    The system dialog may have an icon, a title, description,
    additional content, and a button container. The button container
    typically has an accept and a cancel buttons, but it can also
    have an additional view.
    
    The bounds of the system dialog will be automatically adjusted
    according to its parent bounds. This part will be implemented in
    the following CL (part 2).
    
    The examples are shown in the bug comments.
    
    Bug: b:280451823
    Change-Id: Ib2d7e2f250b4959887c935c08f56b3121a3912b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500464
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139301}

---
+++ ash/style/system_dialog_delegate_view.cc
@@ -0,0 +1,324 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/style/system_dialog_delegate_view.h"
+
+#include <memory>
+
+#include "ash/strings/grit/ash_strings.h"
+#include "ash/style/pill_button.h"
+#include "ash/style/typography.h"
+#include "base/functional/bind.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/models/image_model.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/color/color_id.h"
+#include "ui/compositor/layer.h"
+#include "ui/events/event.h"
+#include "ui/gfx/geometry/insets.h"
+#include "ui/strings/grit/ui_strings.h"
+#include "ui/views/background.h"
+#include "ui/views/border.h"
+#include "ui/views/controls/image_view.h"
+#include "ui/views/controls/label.h"
+#include "ui/views/highlight_border.h"
+#include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/flex_layout_types.h"
+#include "ui/views/layout/flex_layout_view.h"
+#include "ui/views/layout/layout_types.h"
+#include "ui/views/view_class_properties.h"
+
+namespace ash {
+
+namespace {
+
+// The default color IDs of the dialog.
+constexpr ui::ColorId kBackgroundColorId = cros_tokens::kCrosSysBaseElevated;
+constexpr ui::ColorId kBodyColorId = cros_tokens::kCrosSysOnSurfaceVariant;
+constexpr ui::ColorId kIconColorId = cros_tokens::kCrosSysPrimary;
+constexpr ui::ColorId kTitleColorId = cros_tokens::kCrosSysOnSurface;
+
+// The default layout parameters of the dialog.
+constexpr gfx::Size kMinimumDialogSize = gfx::Size(296, 144);
+constexpr gfx::Size kMaximumDialogSize = gfx::Size(512, 600);
+constexpr int kRoundedCornerRadius = 20;
+constexpr gfx::Insets kBorderInsets = gfx::Insets::TLBR(32, 32, 28, 32);
+constexpr int kIconSize = 32;
+constexpr int kIconBottomPadding = 20;
+constexpr int kTitleBottomPadding = 16;
+constexpr int kDefaultAdditionalContentTopPadding = 32;
+constexpr int kButtonContainerTopPadding = 32;
+constexpr int kButtonSpacing = 8;
+constexpr int kMinimumAdditionalButtonPadding = 80;
+
+// The position of the additional content in the dialog child views.
+constexpr int kAdditionalContentID = 3;
+
+// The default fonts of the title and description.
+constexpr TypographyToken kTitleFont = TypographyToken::kCrosDisplay7;
+constexpr TypographyToken kBodyFont = TypographyToken::kCrosBody1;
+
+// Sets margins and flex layout specs to the view.
+void SetViewLayoutSpecs(
+    views::View* view,
+    const gfx::Insets& margins = gfx::Insets(),
+    const views::FlexSpecification flex_spec = views::FlexSpecification()) {
+  view->SetProperty(views::kMarginsKey, margins);
+  view->SetProperty(views::kFlexBehaviorKey, flex_spec);
+}
+
+}  // namespace
+
+//------------------------------------------------------------------------------
+// SystemDialogDelegateView::ButtonContainer:
+// The container includes an accept button and a cancel button. The buttons are
+// awlays at the right bottom corner of the dialog. The container also allows an
+// additional view to be added at the left side. Please refer to the example in
+// the header file for the container layout.
+class SystemDialogDelegateView::ButtonContainer : public views::FlexLayoutView {
+ public:
+  METADATA_HEADER(ButtonContainer);
+
+  explicit ButtonContainer(SystemDialogDelegateView* dialog_view)
+      : cancel_button_(AddChildView(std::make_unique<PillButton>(
+            base::BindRepeating(&SystemDialogDelegateView::Cancel,
+                                base::Unretained(dialog_view)),
+            l10n_util::GetStringUTF16(IDS_APP_CANCEL),
+            PillButton::Type::kSecondaryWithoutIcon))),
+        accept_button_(AddChildView(std::make_unique<PillButton>(
+            base::BindRepeating(&SystemDialogDelegateView::Accept,
+                                base::Unretained(dialog_view)),
+            l10n_util::GetStringUTF16(IDS_APP_OK),
+            PillButton::Type::kPrimaryWithoutIcon))) {
+    SetOrientation(views::LayoutOrientation::kHorizontal);
+    SetMainAxisAlignment(views::LayoutAlignment::kEnd);
+    SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
+
+    SetViewLayoutSpecs(cancel_button_,
+                       gfx::Insets::TLBR(0, 0, 0, kButtonSpacing));
+  }
+
+  ButtonContainer(const ButtonContainer&) = delete;
+  ButtonContainer& operator=(const ButtonContainer&) = delete;
+  ~ButtonContainer() override = default;
+
+  void SetAcceptText(const std::u16string& accept_text) {
+    accept_button_->SetText(accept_text);
+  }
+
+  void SetCancelText(const std::u16string& cancel_text) {
+    cancel_button_->SetText(cancel_text);
+  }
+
+  void SetAdditionalView(std::unique_ptr<views::View> additional_view) {
+    if (additional_view_) {
+      RemoveChildViewT(additional_view_);
+    }
+
+    // Create a place holder view to fill the space between the cancel button
+    // and the additional view.
+    if (!place_holder_view_) {
+      place_holder_view_ = AddChildViewAt(std::make_unique<views::View>(), 0);
+      SetViewLayoutSpecs(
+          place_holder_view_,
+          gfx::Insets::TLBR(0, 0, 0, kMinimumAdditionalButtonPadding),
+          views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
+                                   views::MaximumFlexSizeRule::kUnbounded));
+    }
+    additional_view_ = AddChildViewAt(std::move(additional_view), 0);
+  }
+
+ private:
+  // Owned by the container.
+  base::raw_ptr<PillButton> cancel_button_ = nullptr;
+  base::raw_ptr<PillButton> accept_button_ = nullptr;
+  base::raw_ptr<views::View> additional_view_ = nullptr;
+  // The view used to fill the free spaces between the additional view and
+  // cancel button.
+  base::raw_ptr<views::View> place_holder_view_ = nullptr;
+};
+
+BEGIN_METADATA(SystemDialogDelegateView, ButtonContainer, views::FlexLayoutView)
+END_METADATA
+
+//------------------------------------------------------------------------------
+// SystemDialogDelegateView:
+SystemDialogDelegateView::SystemDialogDelegateView() {
+  // Set border and background.
+  SetBorder(views::CreatePaddedBorder(
+      std::make_unique<views::HighlightBorder>(
+          kRoundedCornerRadius,
+          views::HighlightBorder::Type::kHighlightBorderOnShadow),
+      kBorderInsets));
+  SetBackground(views::CreateThemedRoundedRectBackground(kBackgroundColorId,
+                                                         kRoundedCornerRadius));
+
+  // Set shadow.
+  shadow_ = SystemShadow::CreateShadowOnNinePatchLayerForView(
+      this, SystemShadow::Type::kElevation12);
+  shadow_->SetRoundedCornerRadius(kRoundedCornerRadius);
+
+  // Use flex layout.
+  SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical)
+      .SetMainAxisAlignment(views::LayoutAlignment::kStart)
+      .SetCrossAxisAlignment(views::LayoutAlignment::kStretch)
+      .SetCollapseMargins(true);
+
+  icon_ = AddChildView(std::make_unique<views::ImageView>());
+  SetViewLayoutSpecs(icon_, gfx::Insets::TLBR(0, 0, kIconBottomPadding, 0));
+  icon_->SetProperty(views::kCrossAxisAlignmentKey,
+                     views::LayoutAlignment::kStart);
+  icon_->SetVisible(false);
+
+  // Configure icon, title, description, and button container with pre-defined
+  // layout.
+  auto* typography_provider = TypographyProvider::Get();
+  title_ = AddChildView(std::make_unique<views::Label>());
+  SetViewLayoutSpecs(title_, gfx::Insets::TLBR(0, 0, kTitleBottomPadding, 0));
+  typography_provider->StyleLabel(kTitleFont, *title_);
+  title_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  title_->SetAutoColorReadabilityEnabled(false);
+  title_->SetEnabledColorId(kTitleColorId);
+  title_->SetVisible(false);
+
+  description_ = AddChildView(std::make_unique<views::Label>());
+  SetViewLayoutSpecs(
+      description_, gfx::Insets(),
+      views::FlexSpecification(views::LayoutOrientation::kHorizontal,
+                               views::MinimumFlexSizeRule::kScaleToMinimum,
+                               views::MaximumFlexSizeRule::kUnbounded, true,
+                               views::MinimumFlexSizeRule::kPreferred));
+  typography_provider->StyleLabel(kBodyFont, *description_);
+  description_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  description_->SetMultiLine(true);
+  description_->SetAllowCharacterBreak(true);
+  description_->SetAutoColorReadabilityEnabled(false);
+  description_->SetEnabledColorId(kBodyColorId);
+  description_->SetVisible(false);
+
+  button_container_ = AddChildView(std::make_unique<ButtonContainer>(this));
+  SetViewLayoutSpecs(
+      button_container_, gfx::Insets::TLBR(kButtonContainerTopPadding, 0, 0, 0),
+      views::FlexSpecification(views::LayoutOrientation::kHorizontal,
+                               views::MinimumFlexSizeRule::kScaleToMinimum,
+                               views::MaximumFlexSizeRule::kUnbounded));
+
+  // Register the close callback.
+  RegisterWindowWillCloseCallback(
+      base::BindOnce(&SystemDialogDelegateView::Close, base::Unretained(this)));
+}
+
+SystemDialogDelegateView::~SystemDialogDelegateView() = default;
+
+void SystemDialogDelegateView::SetIcon(const gfx::VectorIcon& icon) {
+  icon_->SetImage(
+      ui::ImageModel::FromVectorIcon(icon, kIconColorId, kIconSize));
+  icon_->SetVisible(true);
+}
+
+void SystemDialogDelegateView::SetTitleText(const std::u16string& title) {
+  title_->SetText(title);
+  title_->SetVisible(!title.empty());
+}
+
+void SystemDialogDelegateView::SetDescription(
+    const std::u16string& description) {
+  description_->SetText(description);
+  description_->SetVisible(!description.empty());
+}
+
+void SystemDialogDelegateView::SetAcceptButtonText(
+    const std::u16string& accept_text) {
+  button_container_->SetAcceptText(accept_text);
+}
+
+void SystemDialogDelegateView::SetCancelButtonText(
+    const std::u16string& cancel_text) {
+  button_container_->SetCancelText(cancel_text);
+}
+
+gfx::Size SystemDialogDelegateView::GetMinimumSize() const {
+  return kMinimumDialogSize;
+}
+
+gfx::Size SystemDialogDelegateView::GetMaximumSize() const {
+  return kMaximumDialogSize;
+}
+
+void SystemDialogDelegateView::SetAdditionalContentInternal(
+    std::unique_ptr<views::View> view) {
+  // If there is an additional content, remove it.
+  if (additional_content_) {
+    RemoveChildViewT(additional_content_);
+  }
+
+  // Add additional content and move it to the specific position.
+  additional_content_ = AddChildView(std::move(view));
+  ReorderChildView(additional_content_, kAdditionalContentID);
+
+  // If there is no preset margins or the top margin is 0, set the top margin
+  // with the default padding.
+  auto* margins = additional_content_->GetProperty(views::kMarginsKey);
+  if (!margins) {
+    additional_content_->SetProperty(
+        views::kMarginsKey,
+        gfx::Insets::TLBR(kDefaultAdditionalContentTopPadding, 0, 0, 0));
+  } else if (!margins->top()) {
+    margins->set_top(kDefaultAdditionalContentTopPadding);
+  }
+
+  additional_content_->SetProperty(views::kCrossAxisAlignmentKey,
+                                   views::LayoutAlignment::kCenter);
+}
+
+void SystemDialogDelegateView::SetAdditionalViewInButtonRowInternal(
+    std::unique_ptr<views::View> view) {
+  button_container_->SetAdditionalView(std::move(view));
+}
+
+void SystemDialogDelegateView::Accept() {
+  if (!closing_dialog_) {
+    RunCallbackAndCloseDialog(
+        std::move(accept_callback_),
+        views::Widget::ClosedReason::kAcceptButtonClicked);
+  }
+}
+
+void SystemDialogDelegateView::Cancel() {
+  if (!closing_dialog_) {
+    RunCallbackAndCloseDialog(
+        std::move(cancel_callback_),
+        views::Widget::ClosedReason::kCancelButtonClicked);
+  }
+}
+
+void SystemDialogDelegateView::Close() {
+  if (!closing_dialog_ && close_callback_) {
+    std::move(close_callback_).Run();
+  }
+  closing_dialog_ = true;
+}
+
+void SystemDialogDelegateView::RunCallbackAndCloseDialog(
+    base::OnceClosure callback,
+    views::Widget::ClosedReason closed_reason) {
+  CHECK(!closing_dialog_);
+
+  if (callback) {
+    std::move(callback).Run();
+  }
+
+  if (auto* widget = GetWidget()) {
+    // Update the `closing_dialog_` before closing the widget.
+    closing_dialog_ = true;
+    widget->CloseWithReason(closed_reason);
+  }
+}
+
+BEGIN_METADATA(SystemDialogDelegateView, views::WidgetDelegateView)
+END_METADATA
+
+}  // namespace ash
---
+++ ash/style/system_dialog_delegate_view.h
@@ -0,0 +1,165 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_STYLE_SYSTEM_DIALOG_DELEGATE_VIEW_H_
+#define ASH_STYLE_SYSTEM_DIALOG_DELEGATE_VIEW_H_
+
+#include <string>
+
+#include "ash/ash_export.h"
+#include "ash/style/system_shadow.h"
+#include "base/functional/callback_forward.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/gfx/vector_icon_types.h"
+#include "ui/views/metadata/view_factory.h"
+#include "ui/views/widget/widget_delegate.h"
+
+namespace views {
+class ImageView;
+class Label;
+}  // namespace views
+
+namespace ash {
+
+// The contents of a dialog that displays information or prompts the user input.
+// A dialog may include an icon, a title, a description, additional content, and
+// a button container. The button container typically contains an accept button
+// and a cancel button, but it may also include an additional view. The layout
+// of the dialog with all the elements is shown below:
+// +----------------------------------------------------+
+// |  +----+                                            |
+// |  |    |- Icon                                      |
+// |  +----+                                            |
+// |                                                    |
+// |  Title                                             |
+// |                                                    |
+// |  Description text                                  |
+// |  +----------------------------------------------+  |
+// |  |           Additional content                 |  |
+// |  +----------------------------------------------+  |
+// |  +-----+                    +--------+ +--------+  |
+// |  |     |- Additional view   | Cancel | |   OK   |  |
+// |  +-----+                    +--------+ +--------+  |
+// +----------------------------------------------------+
+//
+// The dialog would display all or some of above elements, depending on the
+// clients' needs.
+class ASH_EXPORT SystemDialogDelegateView : public views::WidgetDelegateView {
+ public:
+  METADATA_HEADER(SystemDialogDelegateView);
+
+  SystemDialogDelegateView();
+  SystemDialogDelegateView(const SystemDialogDelegateView&) = delete;
+  SystemDialogDelegateView& operator=(const SystemDialogDelegateView&) = delete;
+  ~SystemDialogDelegateView() override;
+
+  // Sets the leading icon of the dialog. There is no icon dy default.
+  void SetIcon(const gfx::VectorIcon& icon);
+
+  // Sets title and description text. There will be no title or description if
+  // their texts are empty.
+  void SetTitleText(const std::u16string& title);
+  void SetDescription(const std::u16string& description);
+
+  // Sets the text of accept and cancel buttons. The default accept button text
+  // is "OK", and cancel button is "Cancel".
+  void SetAcceptButtonText(const std::u16string& accept_text);
+  void SetCancelButtonText(const std::u16string& cancel_text);
+
+  // Sets accept and cancel button callbacks. If the callback is not set,
+  // clicking the corresponding button would only close the dialog without
+  // performing any additional actions.
+  void SetAcceptCallback(base::OnceClosure accept_callback) {
+    accept_callback_ = std::move(accept_callback);
+  }
+  void SetCancelCallback(base::OnceClosure cancel_callback) {
+    cancel_callback_ = std::move(cancel_callback);
+  }
+
+  // Sets dialog close callback. The close callback is called when the dialog is
+  // closed without clicking the accept or cancel button. For example, when the
+  // dialog's parent window is destroyed.
+  void SetCloseCallback(base::OnceClosure close_callback) {
+    close_callback_ = std::move(close_callback);
+  }
+
+  // Sets the additional content view.
+  template <typename T>
+  T* SetAdditionalContentView(std::unique_ptr<T> view) {
+    T* raw_ptr = view.get();
+    SetAdditionalContentInternal(std::move(view));
+    return raw_ptr;
+  }
+
+  // Sets the additional view in the button container.
+  template <typename T>
+  T* SetAdditionalViewInButtonRow(std::unique_ptr<T> view) {
+    T* raw_ptr = view.get();
+    SetAdditionalViewInButtonRowInternal(std::move(view));
+    return raw_ptr;
+  }
+
+  // views::WidgetDelegateView:
+  gfx::Size GetMinimumSize() const override;
+  gfx::Size GetMaximumSize() const override;
+
+ private:
+  class ButtonContainer;
+
+  // Internal methods of adding the additional views into the dialog.
+  void SetAdditionalContentInternal(std::unique_ptr<views::View> view);
+  void SetAdditionalViewInButtonRowInternal(std::unique_ptr<views::View> view);
+
+  // The actual callbacks of accept and cancel buttons. When the accept/cancel
+  // button is clicked, the corresponding `accept_callback_`/`cancel_callback_`
+  // will be called if exists and the dialog will be closed.
+  void Accept();
+  void Cancel();
+
+  // The callback when the dialog will be closed.
+  void Close();
+
+  // Run the given `callback` and close the dialog with `closed_reason`.
+  void RunCallbackAndCloseDialog(base::OnceClosure callback,
+                                 views::Widget::ClosedReason closed_reason);
+
+  // The callbacks of the buttons and closing dialog.
+  base::OnceClosure accept_callback_;
+  base::OnceClosure cancel_callback_;
+  base::OnceClosure close_callback_;
+
+  // The view of each element owned by the dialog.
+  raw_ptr<views::ImageView> icon_ = nullptr;
+  raw_ptr<views::Label> title_ = nullptr;
+  raw_ptr<views::Label> description_ = nullptr;
+  raw_ptr<views::View> additional_content_ = nullptr;
+  raw_ptr<ButtonContainer> button_container_ = nullptr;
+
+  // The dialog shadow.
+  std::unique_ptr<SystemShadow> shadow_;
+
+  // Indicates if the dialog is being closed.
+  bool closing_dialog_ = false;
+};
+
+BEGIN_VIEW_BUILDER(ASH_EXPORT,
+                   SystemDialogDelegateView,
+                   views::WidgetDelegateView)
+VIEW_BUILDER_PROPERTY(const gfx::VectorIcon&, Icon, const gfx::VectorIcon&)
+VIEW_BUILDER_PROPERTY(const std::u16string&, TitleText)
+VIEW_BUILDER_PROPERTY(const std::u16string&, Description)
+VIEW_BUILDER_PROPERTY(const std::u16string&, AcceptButtonText)
+VIEW_BUILDER_PROPERTY(const std::u16string&, CancelButtonText)
+VIEW_BUILDER_PROPERTY(base::OnceClosure, AcceptCallback)
+VIEW_BUILDER_PROPERTY(base::OnceClosure, CancelCallback)
+VIEW_BUILDER_PROPERTY(base::OnceClosure, CloseCallback)
+VIEW_BUILDER_VIEW_TYPE_PROPERTY(views::View, AdditionalContentView)
+VIEW_BUILDER_VIEW_TYPE_PROPERTY(views::View, AdditionalViewInButtonRow)
+END_VIEW_BUILDER
+
+}  // namespace ash
+
+DEFINE_VIEW_BUILDER(ASH_EXPORT, ash::SystemDialogDelegateView)
+
+#endif  // ASH_STYLE_SYSTEM_DIALOG_DELEGATE_VIEW_H_
