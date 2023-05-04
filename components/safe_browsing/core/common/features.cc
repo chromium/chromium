0740618f03d5f38d24e921018a799e7ef51446ce
Richard Chen <richche@chromium.org>
2023-05-04 17:38:33 +0000

    CRX Telemetry: Off-store file data 100% Stable launch.
    - Enable kExtensionTelemetryFileData by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1384755
    Change-Id: Ifecf290c0f7e6ca9dd3239726e877294c4376905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505365
    Commit-Queue: Richard Chen <richche@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139648}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -108,7 +108,7 @@ BASE_FEATURE(kExtensionTelemetryPersistence,
 
 BASE_FEATURE(kExtensionTelemetryFileData,
              "SafeBrowsingExtensionTelemetryFileData",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kExtensionTelemetryFileDataMaxFilesToProcess{
     &kExtensionTelemetryFileData, "MaxFilesToProcess",
@@ -308,6 +308,7 @@ constexpr struct {
     {&kExtensionTelemetryCookiesGetAllSignal, true},
     {&kExtensionTelemetryCookiesGetSignal, true},
     {&kExtensionTelemetryDeclarativeNetRequestSignal, true},
+    {&kExtensionTelemetryFileData, true},
     {&kExtensionTelemetryPersistence, true},
     {&kExtensionTelemetryPotentialPasswordTheft, true},
     {&kExtensionTelemetryReportContactedHosts, true},
