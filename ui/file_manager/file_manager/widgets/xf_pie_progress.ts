254cf9316933f782e0a41d69af7daf1c5f02c6ad
Marcello Salomao <msalomao@google.com>
2023-05-04 07:38:56 +0000

    [MirrorSync] Smooth xf-pie-progress animations
    Add animation interpolation to xf-pie-progress so that
    it animates smoothly even when it receives low-frequency
    updates.
    
    Bug: b/274038565
    Test: Manual
    Change-Id: I2d0c18a8533344d3251c79ee45799c3c31c9674a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505555
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139377}

--- ui/file_manager/file_manager/widgets/xf_pie_progress.ts
+++ ui/file_manager/file_manager/widgets/xf_pie_progress.ts
@@ -7,6 +7,8 @@ import {css, customElement, html, property, svg, XfBase} from './xf_base.js';
 const TWO_PI = 2.0 * Math.PI;
 const HALF_PI = 0.5 * Math.PI;
 
+const DAMPENING = 0.000001;
+
 /**
  * Displays a pie shaped progress indicator.
  * Accepts a `progress` property ranging from 0 to 1.
@@ -15,6 +17,13 @@ const HALF_PI = 0.5 * Math.PI;
 export class XfPieProgress extends XfBase {
   // This should be a number between 0 and 1.
   @property({type: Number, reflect: true}) progress = 0;
+  // Progress value used for rendering that is smoothly interpolated over time
+  // until it reaches the target progress. This allows for infrequent updates
+  // that still render smoothly.
+  @property({type: Number, reflect: false}) progressSmooth = 0;
+
+  private animationFrameId: number = 0;
+  private timeLastFrame: number = 0;
 
   private size = 16;  // Size of the SVG square measured by its side length.
   private center = this.size / 2.0;  // Center of the pie circle (both X and Y).
@@ -39,15 +48,43 @@ export class XfPieProgress extends XfBase {
     return getCSS();
   }
 
+
+  interpolate = () => {
+    // Interpolate faster if close to 100% progress.
+    const boost = this.progress > 0.99 ? 10 : 1;
+    const deltaTime = Date.now() - this.timeLastFrame;
+    this.progressSmooth +=
+        (this.progress - this.progressSmooth) * DAMPENING * deltaTime * boost;
+    this.animationFrameId = requestAnimationFrame(this.interpolate);
+  };
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.progressSmooth = this.progress;
+    this.timeLastFrame = Date.now();
+    this.animationFrameId = requestAnimationFrame(this.interpolate);
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    cancelAnimationFrame(this.animationFrameId);
+  }
+
   override render() {
-    const {progress, size, center, radius} = this;
+    const {
+      progressSmooth,
+      progress,
+      size,
+      center,
+      radius,
+    } = this;
 
     let contents = svg``;
 
     if (progress === 0) {
       // Display the queued shape.
       contents = this.queuedShape;
-    } else if (progress >= 0.99) {
+    } else if (progressSmooth >= 0.99) {
       // The completed pie is easier to draw.
       contents = svg`
         <circle
@@ -62,7 +99,7 @@ export class XfPieProgress extends XfBase {
       // Finishing angle of the pie arc. Notice that the starting angle is
       // always -PI/2. I.e., the pie is drawn starting from the top of the
       // circle and it advances in a clockwise fashion.
-      const radians = TWO_PI * progress - HALF_PI;
+      const radians = TWO_PI * progressSmooth - HALF_PI;
 
       // Finishing cartesian coordinates of the pie arc. Notice that the
       // starting coordinates are always <0, -radius> (the top of the circle).
@@ -73,7 +110,7 @@ export class XfPieProgress extends XfBase {
       // Render the smaller one until we are drawing an arc with an angle
       // greater than 180 degrees. More info:
       // https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
-      const largeArcFlag = progress <= 0.5 ? '0' : '1';
+      const largeArcFlag = progressSmooth <= 0.5 ? '0' : '1';
 
       contents = svg`<circle
           class="edge"
