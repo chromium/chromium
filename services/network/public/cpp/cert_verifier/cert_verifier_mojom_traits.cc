ea215b5d094e1f23575d1b8300cc700aa6fa1de8
Matt Mueller <mattm@chromium.org>
2023-05-04 20:46:39 +0000

    Revert "Add debug aliasing and crash keys in StructTraits::Read for net::CertVerifier::RequestParams"
    Removes debugging code that is no longer needed.
    
    This reverts commit 28780dd64b8b9b3e741c393255b11402a61fef3d.
    
    Bug: 1346598
    Change-Id: I969cef642e8e3b564d547e42826cedb791fa1e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4426438
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139758}

--- services/network/public/cpp/cert_verifier/cert_verifier_mojom_traits.cc
+++ services/network/public/cpp/cert_verifier/cert_verifier_mojom_traits.cc
@@ -7,11 +7,8 @@
 #include <string>
 #include <vector>
 
-#include "base/debug/alias.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/memory/scoped_refptr.h"
 #include "net/cert/x509_certificate.h"
-#include "services/network/public/cpp/crash_keys.h"
 
 namespace mojo {
 
@@ -21,133 +18,8 @@ bool StructTraits<cert_verifier::mojom::RequestParamsDataView,
          net::CertVerifier::RequestParams* params) {
   scoped_refptr<net::X509Certificate> certificate;
   std::string hostname, ocsp_response, sct_list;
-  // TODO(https://crbug.com/1346598): Remove debug code once bug investigation
-  // is complete.
-  constexpr size_t kMaxAliasedStackDataSize = 8192;
-  if (!data.ReadCertificate(&certificate)) {
-    network::debug::SetDeserializationCrashKeyString("certificate");
-
-    ::network::mojom::X509CertificateDataView cert_data_view;
-    data.GetCertificateDataView(&cert_data_view);
-
-    bool cert_data_view_is_null = cert_data_view.is_null();
-    base::debug::Alias(&cert_data_view_is_null);
-    if (cert_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    mojo::ArrayDataView<uint8_t> array_data_view;
-    cert_data_view.GetDataDataView(&array_data_view);
-
-    bool array_data_view_is_null = array_data_view.is_null();
-    base::debug::Alias(&array_data_view_is_null);
-    if (array_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    size_t size = array_data_view.size();
-    uint8_t aliased_data[kMaxAliasedStackDataSize] = {0};
-    size_t aliased_size = std::min(size, std::size(aliased_data));
-    memcpy(aliased_data, array_data_view.data(), aliased_size);
-    base::debug::Alias(&size);
-    base::debug::Alias(&aliased_size);
-    base::debug::Alias(aliased_data);
-
-    base::debug::DumpWithoutCrashing();
-    return false;
-  }
-  if (!data.ReadHostname(&hostname)) {
-    network::debug::SetDeserializationCrashKeyString("hostname");
-
-    mojo::StringDataView string_data_view;
-    data.GetHostnameDataView(&string_data_view);
-
-    bool string_data_view_is_null = string_data_view.is_null();
-    base::debug::Alias(&string_data_view_is_null);
-    if (string_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    size_t size = string_data_view.size();
-    uint8_t aliased_data[kMaxAliasedStackDataSize] = {0};
-    size_t aliased_size = std::min(size, std::size(aliased_data));
-    memcpy(aliased_data, string_data_view.storage(), aliased_size);
-    base::debug::Alias(&size);
-    base::debug::Alias(&aliased_size);
-    base::debug::Alias(aliased_data);
-
-    base::debug::DumpWithoutCrashing();
-    return false;
-  }
-  if (!data.ReadOcspResponse(&ocsp_response)) {
-    network::debug::SetDeserializationCrashKeyString("ocsp_response");
-
-    ::mojo_base::mojom::ByteStringDataView byte_string_data_view;
-    data.GetOcspResponseDataView(&byte_string_data_view);
-
-    bool byte_string_data_view_is_null = byte_string_data_view.is_null();
-    base::debug::Alias(&byte_string_data_view_is_null);
-    if (byte_string_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    mojo::ArrayDataView<uint8_t> array_data_view;
-    byte_string_data_view.GetDataDataView(&array_data_view);
-
-    bool array_data_view_is_null = array_data_view.is_null();
-    base::debug::Alias(&array_data_view_is_null);
-    if (array_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    size_t size = array_data_view.size();
-    uint8_t aliased_data[kMaxAliasedStackDataSize] = {0};
-    size_t aliased_size = std::min(size, std::size(aliased_data));
-    memcpy(aliased_data, array_data_view.data(), aliased_size);
-    base::debug::Alias(&size);
-    base::debug::Alias(&aliased_size);
-    base::debug::Alias(aliased_data);
-
-    base::debug::DumpWithoutCrashing();
-    return false;
-  }
-  if (!data.ReadSctList(&sct_list)) {
-    network::debug::SetDeserializationCrashKeyString("sct_list");
-
-    ::mojo_base::mojom::ByteStringDataView byte_string_data_view;
-    data.GetSctListDataView(&byte_string_data_view);
-
-    bool byte_string_data_view_is_null = byte_string_data_view.is_null();
-    base::debug::Alias(&byte_string_data_view_is_null);
-    if (byte_string_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    mojo::ArrayDataView<uint8_t> array_data_view;
-    byte_string_data_view.GetDataDataView(&array_data_view);
-
-    bool array_data_view_is_null = array_data_view.is_null();
-    base::debug::Alias(&array_data_view_is_null);
-    if (array_data_view_is_null) {
-      base::debug::DumpWithoutCrashing();
-      return false;
-    }
-
-    size_t size = array_data_view.size();
-    uint8_t aliased_data[kMaxAliasedStackDataSize] = {0};
-    size_t aliased_size = std::min(size, std::size(aliased_data));
-    memcpy(aliased_data, array_data_view.data(), aliased_size);
-    base::debug::Alias(&size);
-    base::debug::Alias(&aliased_size);
-    base::debug::Alias(aliased_data);
-
-    base::debug::DumpWithoutCrashing();
+  if (!data.ReadCertificate(&certificate) || !data.ReadHostname(&hostname) ||
+      !data.ReadOcspResponse(&ocsp_response) || !data.ReadSctList(&sct_list)) {
     return false;
   }
   *params = net::CertVerifier::RequestParams(
