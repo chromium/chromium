4c67a2fee6b5ca46f86456907ce8f806337c3971
David Bertoni <dbertoni@chromium.org>
2023-05-04 18:30:32 +0000

    [Sheriff] Disable flaky SoftNavigationBrowserTest.SoftNavigation test.
    Bug: 1431821
    Change-Id: I73e7b1bb101b282cda462f6c0a588c9556dd3e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4504732
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Owners-Override: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1139672}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -2707,9 +2707,11 @@ class SoftNavigationBrowserTestWithSoftNavigationHeuristicsFlag
   base::test::ScopedFeatureList features_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(SoftNavigationBrowserTest, SoftNavigation) {
+// TODO(crbug.com/1431821): Flaky on many platforms.
+IN_PROC_BROWSER_TEST_F(SoftNavigationBrowserTest, DISABLED_SoftNavigation) {
   TestSoftNavigation(/*wait_for_second_lcp=*/false);
 }
+
 IN_PROC_BROWSER_TEST_F(
     SoftNavigationBrowserTestWithSoftNavigationHeuristicsFlag,
     SoftNavigation) {
