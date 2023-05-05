2ab2a96d3d1f668bd4ed817aa083bd2ab1d594dc
Tiago Vignatti <tvignatti@igalia.com>
2023-05-05 17:03:47 +0000

    [ios] Silence deprecated methods for app's state
    This silence logging when using these deprecated methods. App's state
    handling is kept intact though.
    
    Bug: None
    Change-Id: I47fc3a481b6e9fcc40e38bf2e7b94c4ffc896cf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4493346
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Tiago Vignatti <tvignatti@igalia.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140177}

--- ios/web_view/shell/shell_app_delegate.m
+++ ios/web_view/shell/shell_app_delegate.m
@@ -54,12 +54,12 @@
 }
 
 - (BOOL)application:(UIApplication*)application
-    shouldSaveApplicationState:(NSCoder*)coder {
+    shouldSaveSecureApplicationState:(NSCoder*)coder {
   return YES;
 }
 
 - (BOOL)application:(UIApplication*)application
-    shouldRestoreApplicationState:(NSCoder*)coder {
+    shouldRestoreSecureApplicationState:(NSCoder*)coder {
   // TODO(crbug.com/710329): Make this value configurable in the settings.
   return YES;
 }
