51b95b362804f10d3cd00431d8751e4dfbe711a3
Jaroslav Sevcik <jarin@chromium.org>
2023-05-05 10:55:19 +0000

    [test] Disable cache in inspector/network test to always get extra info
    Bug: 1442680
    Change-Id: I7f67395ca60eddb9eb9685b2ff8d67eaf67d862e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507042
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140026}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/redirect-has-extra-info.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/redirect-has-extra-info.js
@@ -2,10 +2,11 @@
   var {page, session, dp} = await testRunner.startBlank(
     `Verifies that requestWillBeSent.redirectEmittedExtraInfo matches the presence of ExtraInfo events on redirects.\n`);
 
-  dp.Network.enable();
-  dp.Page.enable();
+  await dp.Network.enable();
+  await dp.Network.setCacheDisabled({cacheDisabled: true});
+  await dp.Page.enable();
 
-  dp.Network.setRequestInterception({patterns: [{}]});
+  await dp.Network.setRequestInterception({patterns: [{}]});
   dp.Runtime.evaluate({expression: `
     document.body.innerHTML = '<iframe src="http://127.0.1.1:8000/whatever"></iframe>';
   `});
