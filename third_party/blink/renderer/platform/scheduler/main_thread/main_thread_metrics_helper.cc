84f3f93bac10555b37bdc60968a245ac3705e63b
François Doray <fdoray@chromium.org>
2023-05-05 21:40:27 +0000

    [blink scheduler] Remove the RendererScheduler.TaskTime2 histogram.
    This histogram is disabled since M110 via the ReduceCpuUtilization
    feature and is no longer considered useful.
    
    OBSOLETE_HISTOGRAM[RendererScheduler.TaskTime2]=Not actively used and CPU-intensive.
    
    Bug: 1295441
    Change-Id: Iec84de805bc951c671283e11ddc7c7d43788f6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4503348
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140362}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_metrics_helper.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_metrics_helper.cc
@@ -4,7 +4,6 @@
 
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_metrics_helper.h"
 
-#include "base/cpu_reduction_experiment.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/safe_conversions.h"
@@ -163,25 +162,6 @@ void MainThreadMetricsHelper::RecordTaskMetrics(
     queueing_delay_histograms_[static_cast<size_t>(queue->GetQueuePriority())]
         .CountMicroseconds(elapsed);
   }
-
-  // Don't log the metrics to evaluate impact of CPU reduction.
-  // This code is deemed not useful anymore (crbug.com/1181870).
-  // TODO(crbug.com/1295441: Fully remove the code once the experiment is over.
-  if (base::IsRunningCpuReductionExperiment()) {
-    return;
-  }
-
-  // WARNING: All code below must be compatible with down-sampling.
-  constexpr double kSamplingProbability = .01;
-  if (!metrics_subsampler_.ShouldSample(kSamplingProbability)) {
-    return;
-  }
-
-  base::TimeDelta duration = task_timing.wall_duration();
-  UMA_HISTOGRAM_CUSTOM_COUNTS("RendererScheduler.TaskTime2",
-                              base::saturated_cast<base::HistogramBase::Sample>(
-                                  duration.InMicroseconds()),
-                              1, 1000 * 1000, 50);
 }
 
 void MainThreadMetricsHelper::RecordMainThreadTaskLoad(base::TimeTicks time,
