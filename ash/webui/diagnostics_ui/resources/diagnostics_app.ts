86d7128896045b5ee398c3bece886a9ec2b5c1b0
Ashley Prasad <ashleydp@google.com>
2023-05-05 15:21:34 +0000

    navigation-view-panel: use cros fonts
    - Update title and navigation items to use cros fonts when jelly
      enabled.
    - Add `typography.css` to Diagnostics and Shortcut Customization.
    - Update TODO to include adding typography file directly to HTML.
    
    Screenshot: http://screen/AZKK27muv2sHP6j
    
    Bug: b:275718921
    Test: Manual verified UI.
    Change-Id: I43afec1026277326480a538fcd0a8bcebdcb6f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507833
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140133}

--- ash/webui/diagnostics_ui/resources/diagnostics_app.ts
+++ ash/webui/diagnostics_ui/resources/diagnostics_app.ts
@@ -211,8 +211,13 @@ export class DiagnosticsAppElement extends DiagnosticsAppElementBase {
     if (loadTimeData.getBoolean('isJellyEnabledForDiagnosticsApp')) {
       // TODO(b/276493287): After the Jelly experiment is launched, replace
       // `cros_styles.css` with `theme/colors.css` directly in `index.html`.
+      // Also add `theme/typography.css` to `index.html`.
       document.querySelector('link[href*=\'cros_styles.css\']')
           ?.setAttribute('href', 'chrome://theme/colors.css?sets=legacy,sys');
+      const typographyLink = document.createElement('link');
+      typographyLink.href = 'chrome://theme/typography.css';
+      typographyLink.rel = 'stylesheet';
+      document.head.appendChild(typographyLink);
       document.body.classList.add('jelly-enabled');
       startColorChangeUpdater();
     }
