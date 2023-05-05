058f9bdbd04fdbb6ca18a125a948c1d1012862ad
Joshua Pawlicki <waffles@chromium.org>
2023-05-05 17:50:23 +0000

    Sheriff: Disable All/KeepAliveURLBrowserTest.TwoConcurrentRequestsPerHost/POST
    Bug: 1442936
    Change-Id: Icf9809cf298d8eede27059b53b89383608d21d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505182
    Commit-Queue: Dan H <harringtond@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140195}

--- content/browser/loader/keep_alive_url_browsertest.cc
+++ content/browser/loader/keep_alive_url_browsertest.cc
@@ -368,13 +368,8 @@ IN_PROC_BROWSER_TEST_P(KeepAliveURLBrowserTest, OneRequest) {
 // HTTP 1.1 protocol, which `embedded_test_server()` uses by default.
 // Exceeding this limit will hang the browser.
 // TODO(crbug.com/1428502): Flaky on Fuchsia and Android.
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_FUCHSIA)
-#define MAYBE_TwoConcurrentRequestsPerHost DISABLED_TwoConcurrentRequestsPerHost
-#else
-#define MAYBE_TwoConcurrentRequestsPerHost TwoConcurrentRequestsPerHost
-#endif
 IN_PROC_BROWSER_TEST_P(KeepAliveURLBrowserTest,
-                       MAYBE_TwoConcurrentRequestsPerHost) {
+                       DISABLED_TwoConcurrentRequestsPerHost) {
   const std::string method = GetParam();
   const size_t num_requests = 2;
   auto request_handlers =
