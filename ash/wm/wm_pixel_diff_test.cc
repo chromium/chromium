01b25041761c4fa37cae7f769b1a196f06fa22cf
andrewxu <andrewxu@chromium.org>
2023-05-08 17:45:31 +0000

    Update the benchmark for WmPixelDiffTest
    Bug: b/281147762
    Change-Id: I4486a47add692c6c397992ddd90524643eb23e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510156
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Auto-Submit: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1140941}

--- ash/wm/wm_pixel_diff_test.cc
+++ ash/wm/wm_pixel_diff_test.cc
@@ -68,7 +68,7 @@ TEST_F(WmPixelDiffTest, OverviewAndDesksBarBasic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "overview_and_desks_bar_basic",
-      /*revision_number=*/2, desk_widget, overview_widget1, overview_widget2,
+      /*revision_number=*/3, desk_widget, overview_widget1, overview_widget2,
       overview_widget3));
 }
 
