96dd9d002d93f28f9f6fddc0dfe8d7f7226b77d7
Roman Sorokin <rsorokin@chromium.org>
2023-05-08 14:44:10 +0000

    [Sheriff] Disable DevTools*.TestWindowInitializedOnNavigateBack
    on lacros too. The test is flaky (see
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FDevToolsTest.TestWindowInitializedOnNavigateBack?q=VHASH%3A23ff18e9ffec706b)
    
    Bug: 1443360
    Change-Id: I032060ad42a26f2c1adb832fd20159538f7ba3cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4513057
    Commit-Queue: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Auto-Submit: Roman Sorokin <rsorokin@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Owners-Override: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1140843}

--- chrome/browser/devtools/devtools_browsertest.cc
+++ chrome/browser/devtools/devtools_browsertest.cc
@@ -2683,8 +2683,8 @@ class MockWebUIProvider
 // This tests checks that window is correctly initialized when DevTools is
 // opened while navigation through history with forward and back actions.
 // (crbug.com/627407)
-#if BUILDFLAG(IS_MAC)
-// Flaky on MAC https://crbug.com/1443360
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS_LACROS)
+// Flaky on MAC and lacros https://crbug.com/1443360
 #define MAYBE_TestWindowInitializedOnNavigateBack \
   DISABLED_TestWindowInitializedOnNavigateBack
 #else
