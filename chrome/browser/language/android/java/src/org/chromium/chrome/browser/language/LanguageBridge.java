5ea5728838c2bbb5b0dd9d3eeffe27dd837b8b24
Trevor Perrier <perrier@chromium.org>
2023-05-09 15:31:31 +0000

    [AppLanguagePrompt] Convert ULP languages to Chrome codes
    Bug: 1435584
    Change-Id: If2539b7470aecb5c031b585b882506f2a4d08371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4510341
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1141394}

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/LanguageBridge.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/LanguageBridge.java
@@ -33,7 +33,22 @@ public class LanguageBridge {
 
         Iterator<String> ulpIterator = ulpLanguages.iterator();
         if (!ulpIterator.hasNext()) return AppLanguagePromoDialog.TopULPMatchType.EMPTY;
-        return LocaleUtils.isBaseLanguageEqual(language, ulpIterator.next())
+
+        String topLanguage = ulpIterator.next();
+        // Convert ULP language to Chrome UI languages
+        switch (LocaleUtils.toBaseLanguage(topLanguage)) {
+            case "nn": // We do not support "nn" as a UI language so consider it the same as "no"
+            case "no":
+                topLanguage = "nb";
+                break;
+            case "tl":
+                topLanguage = "fil";
+                break;
+            default:
+                // use topLanguage
+        }
+
+        return LocaleUtils.isBaseLanguageEqual(language, topLanguage)
                 ? AppLanguagePromoDialog.TopULPMatchType.YES
                 : AppLanguagePromoDialog.TopULPMatchType.NO;
     }
