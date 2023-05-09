0ff9049756f588633d34d621db0a7d7509fa1021
Yoshisato Yanagisawa <yyanagisawa@google.com>
2023-05-09 05:04:40 +0000

    Revert "CrOS GMC V2: Add unit tests for quick settings media view related"
    This reverts commit 88f4f6db5783b26fc38de06882ef5edc0fef93dd.
    
    Reason for revert: Added tests are continuously failing at
    "Linux Chromium OS ASan LSan Tests"
    
    Original change's description:
    > CrOS GMC V2: Add unit tests for quick settings media view related
    > classes
    >
    > Added unit tests for these classes:
    > - QuickSettingsMediaView
    > - QuickSettingsMediaViewContainer
    > - QuickSettingsMediaViewController
    >
    > Bug: 1430637
    > Change-Id: I74ef0eab27faed21f7488bcd063a220c9438a875
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507788
    > Commit-Queue: Yiren Wang <yrw@chromium.org>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1141084}
    
    Bug: 1430637, 1443791
    Change-Id: Ie5c226ef2c581f1734714d55719d5b86e48d24db
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4515184
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1141191}

--- ash/system/unified/quick_settings_view.h
+++ ash/system/unified/quick_settings_view.h
@@ -116,9 +116,6 @@ class ASH_EXPORT QuickSettingsView : public views::View,
   UnifiedMediaControlsContainer* media_controls_container_for_testing() {
     return media_controls_container_;
   }
-  QuickSettingsMediaViewContainer* media_view_container_for_testing() {
-    return media_view_container_;
-  }
   QuickSettingsFooter* footer_for_testing() { return footer_; }
 
  private:
