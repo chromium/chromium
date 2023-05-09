0f5fad780446a44a66edc07fa2a558fc8325f17c
Ahmed Nasr <anasr@google.com>
2023-05-09 18:56:15 +0000

    Enable VPN connection reporting by default for M115 launch.
    Bug: b:281428486
    Change-Id: If1003668aa81d3581af30e00528d37a2aad4f029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518702
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141549}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_events_observer.cc
@@ -43,7 +43,7 @@ BASE_FEATURE(kEnableNetworkConnectionStateEventsReporting,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableVpnConnectionStateEventsReporting,
              "EnableVpnConnectionStateEventsReporting",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 NetworkEventsObserver::NetworkEventsObserver()
     : MojoServiceEventsObserverBase<
