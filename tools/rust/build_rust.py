615dc990ad51de55d4159ad162d154b00b6e0395
danakj <danakj@chromium.org>
2023-05-09 21:27:53 +0000

    Disable tests/codegen/vec-shrink-panik.rs globally.
    https://github.com/rust-lang/rust/issues/96464
    
    The test used to fail on Windows only but now fails everywhere. The
    Rust CI skips this test as they run in debug mode and the test does
    not run in Debug mode, so we will hit failures before upstream does
    here. This failure is likely being hit due to TOT LLVM though, which
    upstream does not use.
    
    R=hans@chromium.org
    
    Bug: 1401042
    Change-Id: Ie379559f318f3b7bd57c5db4cd21806218bcb373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517966
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141656}

--- tools/rust/build_rust.py
+++ tools/rust/build_rust.py
@@ -74,6 +74,9 @@ EXCLUDED_TESTS = [
     os.path.join('tests', 'codegen', 'sanitizer-cfi-emit-type-checks.rs'),
     os.path.join('tests', 'codegen',
                  'sanitizer-cfi-emit-type-metadata-itanium-cxx-abi.rs'),
+    # https://github.com/rust-lang/rust/issues/96464 used to be Windows only but
+    # now fails everywhere.
+    os.path.join('tests', 'codegen', 'vec-shrink-panik.rs'),
     # https://github.com/rust-lang/rust/issues/109671 the test is being
     # optimized in newer LLVM which breaks its expectations.
     os.path.join('tests', 'ui', 'abi', 'stack-protector.rs'),
@@ -85,8 +88,6 @@ EXCLUDED_TESTS = [
     os.path.join('tests', 'ui', 'numeric', 'numeric-cast.rs'),
 ]
 EXCLUDED_TESTS_WINDOWS = [
-    # https://github.com/rust-lang/rust/issues/96464
-    os.path.join('tests', 'codegen', 'vec-shrink-panik.rs'),
     # TODO(crbug.com/1442943): Re-enable when fixed.
     os.path.join('tests', 'ui', 'native-library-link-flags',
                  'msvc-non-utf8-output.rs'),
