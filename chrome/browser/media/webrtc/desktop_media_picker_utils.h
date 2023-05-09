139b784a05c786478f468248914bfd146d7f12aa
Tove Petersson <tovep@chromium.org>
2023-05-09 12:06:16 +0000

    Moved ScaleBitmap() helper to desktop_media_picker_utils.h|cc
    Bug: 1428878
    Change-Id: If4b30cae3927b6ee85b8436e8d843f40503b40d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4512976
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141301}

---
+++ chrome/browser/media/webrtc/desktop_media_picker_utils.h
@@ -0,0 +1,12 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_WEBRTC_DESKTOP_MEDIA_PICKER_UTILS_H_
+#define CHROME_BROWSER_MEDIA_WEBRTC_DESKTOP_MEDIA_PICKER_UTILS_H_
+
+#include "ui/gfx/image/image_skia.h"
+
+gfx::ImageSkia ScaleBitmap(const SkBitmap& bitmap, gfx::Size size);
+
+#endif  // CHROME_BROWSER_MEDIA_WEBRTC_DESKTOP_MEDIA_PICKER_UTILS_H_
