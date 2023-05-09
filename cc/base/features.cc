9d97f4aed8ed5869242918edcce71e4209ecdf2f
Steve Kobes <skobes@chromium.org>
2023-05-09 20:24:33 +0000

    Enable FlushGpuAtDraw on trunk.
    This experiment has had a positive impact on renderer hang rates.
    Compare:
    - go/flushgpucontrol
    - go/flushgpuenabled
    
    Furthermore it has been Finch-enabled at 1% in stable with no apparent
    regressions.
    
    Bug: 1378021,1421227
    Change-Id: Iddaeb2584688871ff9529bc658ffc42ace9a79cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500322
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141591}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -56,7 +56,7 @@ BASE_FEATURE(kMainRepaintScrollPrefersNewContent,
 
 BASE_FEATURE(kFlushGpuAtDraw,
              "FlushGpuAtDraw",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSchedulerSmoothnessForAnimatedScrolls,
              "SmoothnessModeForAnimatedScrolls",
