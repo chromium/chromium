0615c3e7dfa05595be57fb900fb99d13a308410a
Simon Zünd <szuend@chromium.org>
2023-05-10 14:07:17 +0000

    [devtools] Import test runners in http/tests/devtools/application-panel
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=jarin@chromium.org
    
    Bug: 1442410
    Change-Id: Iffa6295a6877e3f7cf1734286478a40bbe02d770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517708
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142058}

--- third_party/blink/web_tests/http/tests/devtools/application-panel/resources-panel-websql.js
+++ third_party/blink/web_tests/http/tests/devtools/application-panel/resources-panel-websql.js
@@ -2,13 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {ApplicationTestRunner} from 'application_test_runner';
+import {ConsoleTestRunner} from 'console_test_runner';
+
 (async function() {
   TestRunner.addResult(`Tests Application Panel WebSQL support.\n`);
-  await TestRunner.loadLegacyModule('console'); await TestRunner.loadTestModule('application_test_runner');
+  await TestRunner.loadLegacyModule('console');
     // Note: every test that uses a storage API must manually clean-up state from previous tests.
   await ApplicationTestRunner.resetState();
 
-  await TestRunner.loadLegacyModule('console'); await TestRunner.loadTestModule('console_test_runner');
+  await TestRunner.loadLegacyModule('console');
   await TestRunner.showPanel('resources');
   await TestRunner.evaluateInPagePromise(`
       function parse(val) {
