002fcdc0aab9e0c44d728abe5bed5149dbae9668
Chris Fredrickson <cfredric@chromium.org>
2023-05-10 04:25:03 +0000

    Replace ExecuteScript with ExecJs
    This CL was uploaded by git cl split.
    
    R=ortuno@chromium.org
    
    Bug: 1157718
    Change-Id: I1f7eb06e50a9258a78981a3692f886d733c26279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519281
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1141857}

--- chrome/browser/ash/extensions/default_keyboard_extension_browser_test.cc
+++ chrome/browser/ash/extensions/default_keyboard_extension_browser_test.cc
@@ -83,7 +83,7 @@ void DefaultKeyboardExtensionBrowserTest::RunTest(
                    base::FilePath(config.base_framework_));
   InjectJavascript(base::FilePath(config.test_dir_), file);
 
-  ASSERT_TRUE(content::ExecuteScript(web_contents, utf8_content_));
+  ASSERT_TRUE(content::ExecJs(web_contents, utf8_content_));
 
   // Inject DOM-automation test harness and run tests.
   EXPECT_TRUE(ExecuteWebUIResourceTest(web_contents));
@@ -184,7 +184,7 @@ IN_PROC_BROWSER_TEST_F(DefaultKeyboardExtensionBrowserTest, EndToEndTest) {
     base::ScopedAllowBlockingForTesting allow_io;
     ASSERT_TRUE(base::ReadFileToString(path, &script));
   }
-  EXPECT_TRUE(content::ExecuteScript(keyboard_wc, script));
+  EXPECT_TRUE(content::ExecJs(keyboard_wc, script));
   // Verify 'a' appeared on test page.
   ASSERT_EQ(true,
             content::EvalJs(browser_wc,
