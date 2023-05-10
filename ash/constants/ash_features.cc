6f76f84070314f756ced3245d43fd233b0bfeebb
James Cook <jamescook@chromium.org>
2023-05-10 18:59:51 +0000

    Add link to project docs for ash::kQsRevamp feature flag
    Now that we have a nice landing page for the project, let's link to it
    from the code!
    
    Bug: none
    Change-Id: I797b130b7db8237c4722967822838e9462b88ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521619
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142254}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1856,7 +1856,7 @@ BASE_FEATURE(kProjectorUseApiKeyForTranslation,
              "ProjectorUseApiKeyForTranslation",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enable or disable quick settings revamped view.
+// Enable or disable quick settings revamped view (go/qsrevamp).
 BASE_FEATURE(kQsRevamp, "QsRevamp", base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Controls whether the Projector Viewer supports the user experience for
