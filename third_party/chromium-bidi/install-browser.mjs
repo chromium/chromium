9f1f22f9eb7824cfcdde183c92a81d9d8eee772e
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-05-11 14:57:46 +0200

    chore: pin Chrome (#703)

---
+++ third_party/chromium-bidi/install-browser.mjs
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2023 Google LLC.
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Installs a browser defined in `.browser` using
+ * `@puppeteer/browsers` to the directory provided as the first argument
+ * (default: cwd). The executable path is written to the `executablePath` output
+ * param for GitHub actions.
+ *
+ * Examples:
+ *  - `node install-browser.mjs`
+ *  - `node install-browser.mjs /tmp/cache`
+ */
+
+import {install, computeExecutablePath} from '@puppeteer/browsers';
+import actions from '@actions/core';
+import fs from 'fs/promises';
+
+try {
+  const browserSpec = (await fs.readFile('.browser', 'utf-8')).trim();
+  const cacheDir = process.argv[2] || process.cwd();
+  // See .browser for the format.
+  const browser = browserSpec.split('@')[0];
+  const buildId = browserSpec.split('@')[1];
+  await install({
+    browser,
+    buildId,
+    cacheDir,
+  });
+  const executablePath = computeExecutablePath({
+    cacheDir,
+    browser,
+    buildId,
+  });
+  actions.setOutput('executablePath', executablePath);
+  console.log(executablePath);
+} catch (err) {
+  actions.setFailed(`Failed to download the browser: ${err.message}`);
+}
