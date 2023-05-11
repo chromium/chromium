80b02bddb30acfe09783d4a4333914119bc8c41b
Christian Flach <cmfcmf@chromium.org>
2023-05-11 08:48:59 +0000

    sheriff: Disable flaky IdleServiceTest on MacOS
    It looks like this test begun to be much more flaky after
    crrev.com/c/4407945.
    
    Bug: 1444657, 1440182
    Change-Id: I064e9baee398a99694f8fa61006ae356e29d54d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4518211
    Commit-Queue: Sonja Laurila <laurila@google.com>
    Reviewed-by: Sonja Laurila <laurila@google.com>
    Owners-Override: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Auto-Submit: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142610}

--- chrome/browser/enterprise/idle/idle_service_interactive_uitest.cc
+++ chrome/browser/enterprise/idle/idle_service_interactive_uitest.cc
@@ -392,7 +392,16 @@ IN_PROC_BROWSER_TEST_F(IdleServiceTest, MAYBE_MultiProfile) {
   EXPECT_TRUE(ProfilePicker::IsOpen());
 }
 
-IN_PROC_BROWSER_TEST_F(IdleServiceTest, MultiProfileWithDifferentThresholds) {
+// TODO(crbug.com/1444657): Flaky on MacOS
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_MultiProfileWithDifferentThresholds \
+  DISABLED_MultiProfileWithDifferentThresholds
+#else
+#define MAYBE_MultiProfileWithDifferentThresholds \
+  MultiProfileWithDifferentThresholds
+#endif
+IN_PROC_BROWSER_TEST_F(IdleServiceTest,
+                       MAYBE_MultiProfileWithDifferentThresholds) {
   // `profile` has the IdleTimeout policy set to 5 minutes.
   EXPECT_CALL(idle_time_provider(), CalculateIdleTime())
       .WillOnce(Return(base::Seconds(299)));
