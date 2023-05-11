94b029677b8d96d0872076f989b66dd2fdd66970
Venkatesh Srinivas <venkateshs@chromium.org>
2023-05-11 00:23:25 +0000

    FakeConnectionEventLogger: Switch legacy atomics to std::atomic<>
    FakeConnectionEventLogger (used in the remoting code) implemented a
    wrapper class for atomic integer types; on 64-bit builds, it used
    legacy atomics; on 32-bit builds, it used a lock. The wrapper was
    used for two fields - a count of messages, and a message size.
    
    Remove the wrapper and convert the atomics to std::atomic directly;
    std::atomic handles required locking in the runtime (if any is
    required).
    
    No behavior change expected.
    
    Bug: 1194917
    Change-Id: I28bf7591a1725a5dd801beb195658833609d0c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4350506
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Venkatesh Srinivas <venkateshs@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142500}

--- remoting/test/fake_connection_event_logger.cc
+++ remoting/test/fake_connection_event_logger.cc
@@ -4,10 +4,10 @@
 
 #include "remoting/test/fake_connection_event_logger.h"
 
+#include <atomic>
 #include <string>
 #include <utility>
 
-#include "base/atomicops.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
@@ -15,112 +15,9 @@
 #include "remoting/proto/control.pb.h"
 #include "remoting/proto/video.pb.h"
 
-#ifndef ARCH_CPU_64_BITS
-#include "base/synchronization/lock.h"
-#endif
-
 namespace remoting {
 namespace test {
 namespace {
-
-template <typename T>
-class NoBarrierAtomic {
- public:
-  T operator++() { return base::subtle::NoBarrier_AtomicIncrement(&i_, 1) - 1; }
-
-  T operator++(int) { return base::subtle::NoBarrier_AtomicIncrement(&i_, 1); }
-
-  T operator--() {
-    return base::subtle::NoBarrier_AtomicIncrement(&i_, -1) - 1;
-  }
-
-  T operator--(int) { return base::subtle::NoBarrier_AtomicIncrement(&i_, -1); }
-
-  T operator+=(T other) {
-    return base::subtle::NoBarrier_AtomicIncrement(&i_, other);
-  }
-
-  T operator-=(T other) {
-    return base::subtle::NoBarrier_AtomicIncrement(&i_, -other);
-  }
-
-  T operator*() const { return base::subtle::NoBarrier_Load(&i_); }
-
- private:
-  volatile T i_;
-};
-
-class NoBarrierAtomicInt32 : public NoBarrierAtomic<base::subtle::Atomic32> {};
-#ifdef ARCH_CPU_64_BITS
-class NoBarrierAtomicInt64 : public NoBarrierAtomic<base::subtle::Atomic64> {};
-#else  // ifdef ARCH_CPU_64_BITS
-
-using base::AutoLock;
-
-// A barriered, lock based implementation
-class NoBarrierAtomicInt64 {
- public:
-  int64_t operator++();
-  int64_t operator++(int);
-  int64_t operator--();
-  int64_t operator--(int);
-  int64_t operator+=(int64_t other);
-  int64_t operator-=(int64_t other);
-  int64_t operator*() const;
-
- private:
-  volatile int64_t i_ = 0;
-  mutable base::Lock lock_;  // field is used in operator*() const
-};
-
-int64_t NoBarrierAtomicInt64::operator++() {
-  AutoLock l(lock_);
-  const int64_t old_i = i_;
-  i_ = old_i + 1;
-  return old_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator++(int) {
-  AutoLock l(lock_);
-  const int64_t new_i = i_ + 1;
-  i_ = new_i;
-  return new_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator--() {
-  AutoLock l(lock_);
-  const int64_t old_i = i_;
-  i_ = old_i - 1;
-  return old_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator--(int) {
-  AutoLock l(lock_);
-  const int64_t new_i = i_ - 1;
-  i_ = new_i;
-  return new_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator+=(int64_t other) {
-  AutoLock l(lock_);
-  const int64_t new_i = i_ + other;
-  i_ = new_i;
-  return new_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator-=(int64_t other) {
-  AutoLock l(lock_);
-  const int64_t new_i = i_ - other;
-  i_ = new_i;
-  return new_i;
-}
-
-int64_t NoBarrierAtomicInt64::operator*() const {
-  AutoLock l(lock_);
-  return i_;
-}
-
-#endif  // ifdef ARCH_CPU_64_BITS
 
 class MessageCounter {
  public:
@@ -131,8 +28,8 @@ class MessageCounter {
   MessageCounter(const MessageCounter&) = delete;
   MessageCounter& operator=(const MessageCounter&) = delete;
 
-  int message_count() const { return *count_; }
-  int64_t message_size() const { return *size_; }
+  int message_count() const { return count_.load(std::memory_order_relaxed); }
+  int64_t message_size() const { return size_.load(std::memory_order_relaxed); }
   int last_message_size() const { return last_size_; }
   double DurationSeconds() const;
   double MessagesPerSecond() const;
@@ -145,8 +42,8 @@ class MessageCounter {
  private:
   const std::string name_;
   const std::string unit_;
-  NoBarrierAtomicInt32 count_;
-  NoBarrierAtomicInt64 size_;
+  std::atomic<int32_t> count_;
+  std::atomic<int64_t> size_;
   int last_size_ = 0;
   base::Time start_time_;
 };
@@ -182,9 +79,9 @@ void MessageCounter::LogMessage(
 }
 
 void MessageCounter::LogMessage(int size) {
-  count_++;
+  count_.fetch_add(1, std::memory_order_relaxed);
   last_size_ = size;
-  size_ += size;
+  size_.fetch_add(size, std::memory_order_relaxed);
 }
 
 void MessageCounter::DisplayStatistics(std::ostream& os) {
