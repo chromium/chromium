c1bfac99d0db36618e36f6ed6e88c52e3bf7e662
Andrés Olivares <andoli@chromium.org>
2023-05-11 09:34:07 +0000

    Deflake invalidation tracking trace event test
    The test was flaky because some events that are tested here are not
    dispatched for unknown reasons on some instances of the event execution.
    This CL aims to reduce flakiness by adding multiple attempts to the
    tracing + test scenario flow when the events that are needed aren't
    found.
    
    I took the opportunity to incorporate a flaky legacy devtools test into
    this test so that we can get rid of it. For this, the stack trace of the
    Layout event is checked to ensure it includes the function that caused
    the layout
    
    Bug: 1444416
    Change-Id: Id2df32d2b291b689154b1576b33d84764f9b8aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521104
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142625}

--- third_party/blink/web_tests/http/tests/inspector-protocol/tracing/invalidation-tracking.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/tracing/invalidation-tracking.js
@@ -1,35 +1,50 @@
 (async function(testRunner) {
-  const {session, dp} = await testRunner.startBlank(
+  const {session, dp, page} = await testRunner.startBlank(
       'Tests the data of invalidation tracking trace events');
 
   const TracingHelper =
       await testRunner.loadScript('../resources/tracing-test.js');
   const Phase = TracingHelper.Phase;
-  const tracingHelper = new TracingHelper(testRunner, session);
-  await tracingHelper.startTracing(
-      'disabled-by-default-devtools.timeline.invalidationTracking');
-  await dp.Page.navigate({
-    url: 'http://127.0.0.1:8000/inspector-protocol/resources/web-vitals.html'
-  });
+  let tracingHelper = new TracingHelper(testRunner, session);
+  let scheduleStyleInvalidationTracking;
+  let styleRecalcInvalidationTracking;
+  let styleInvalidatorInvalidationTracking;
+  let layoutInvalidationTracking;
+  let layout;
 
-  // Wait for trace events.
-  await session.evaluateAsync(`
+  const MAX_ATTEMPTS = 3;
+  for (let attempts = 0; attempts < MAX_ATTEMPTS; attempts++) {
+    await tracingHelper.startTracing(
+        'disabled-by-default-devtools.timeline.invalidationTracking,devtools.timeline,disabled-by-default-devtools.timeline.stack');
+    await dp.Page.navigate({
+      url: 'http://127.0.0.1:8000/inspector-protocol/resources/web-vitals.html'
+    });
+
+    // Wait for trace events.
+    await session.evaluateAsync(`
       new Promise((res) => {
         (new PerformanceObserver(res)).observe({entryTypes: ['layout-shift']});
       })`);
 
-  await tracingHelper.stopTracing(
-      /disabled-by-default-devtools\.timeline\.invalidationTracking/);
+    await tracingHelper.stopTracing(
+        /(disabled-by-default-)?devtools\.timeline(\.invalidationTracking|stack)?/);
 
-  const scheduleStyleInvalidationTracking = tracingHelper.findEvent(
-      'ScheduleStyleInvalidationTracking', Phase.INSTANT);
-  const styleRecalcInvalidationTracking = tracingHelper.findEvents(
-      'StyleRecalcInvalidationTracking', Phase.INSTANT);
-  const styleInvalidatorInvalidationTracking = tracingHelper.findEvent(
-      'StyleInvalidatorInvalidationTracking', Phase.INSTANT);
+    scheduleStyleInvalidationTracking = tracingHelper.findEvents(
+        'ScheduleStyleInvalidationTracking', Phase.INSTANT)[0];
+    styleRecalcInvalidationTracking = tracingHelper.findEvents(
+        'StyleRecalcInvalidationTracking', Phase.INSTANT);
+    styleInvalidatorInvalidationTracking = tracingHelper.findEvents(
+        'StyleInvalidatorInvalidationTracking', Phase.INSTANT)[0];
+    layoutInvalidationTracking = tracingHelper.findEvents(
+        'LayoutInvalidationTracking', Phase.INSTANT)[0];
+    layout = tracingHelper.findEvents(
+          'Layout', Phase.COMPLETE, e => e.args?.beginData?.stackTrace)[0];
 
-  const layoutInvalidationTracking =
-      tracingHelper.findEvent('LayoutInvalidationTracking', Phase.INSTANT);
+    if (scheduleStyleInvalidationTracking && styleRecalcInvalidationTracking &&
+        styleInvalidatorInvalidationTracking && layoutInvalidationTracking) {
+      break;
+    }
+  }
 
   testRunner.log('ScheduleStyleInvalidationTracking');
   tracingHelper.logEventShape(scheduleStyleInvalidationTracking);
@@ -42,6 +57,12 @@
 
   testRunner.log('LayoutInvalidationTracking');
   tracingHelper.logEventShape(layoutInvalidationTracking);
+
+  testRunner.log('Style recalc initiator:');
+  testRunner.log(styleRecalcInvalidationTracking[0].args?.data?.stackTrace[0].functionName);
+
+  testRunner.log('Layout initiator:');
+  testRunner.log(layout.args?.beginData?.stackTrace[0].functionName);
 
   testRunner.completeTest();
 })
