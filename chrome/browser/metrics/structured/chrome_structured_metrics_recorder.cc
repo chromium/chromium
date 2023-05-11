f7cb4ec491b30452c517d30093b21be794209464
Jong Ahn <jongahn@google.com>
2023-05-11 18:26:34 +0000

    Refactor structured metrics build into //chrome/browser.
    This is needed to avoid circular dependencies. Chrome OS specific
    metadata (ie enrollment status) depends on //chrome/browser.
    
    Bug: 279486387
    Change-Id: I359e9813aedebb357a29cf8b7e176f1fe8aa254c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4517977
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1142790}

--- chrome/browser/metrics/structured/chrome_structured_metrics_recorder.cc
+++ chrome/browser/metrics/structured/chrome_structured_metrics_recorder.cc
@@ -8,7 +8,6 @@
 
 #include "base/no_destructor.h"
 #include "base/task/sequenced_task_runner.h"
-#include "chrome/browser/metrics/structured/cros_events_processor.h"
 #include "components/metrics/structured/histogram_util.h"
 #include "components/metrics/structured/recorder.h"
 #include "components/metrics/structured/structured_metrics_features.h"
@@ -16,6 +15,7 @@
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/browser_process.h"  // nogncheck
 #include "chrome/browser/metrics/structured/ash_structured_metrics_recorder.h"  // nogncheck
+#include "chrome/browser/metrics/structured/cros_events_processor.h"  // nogncheck
 #elif BUILDFLAG(IS_CHROMEOS_LACROS)
 #include "base/task/current_thread.h"
 #include "chrome/browser/metrics/structured/lacros_structured_metrics_recorder.h"  // nogncheck
