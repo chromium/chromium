ec24ef8f087a4384f45cb6effff24e1d4c954425
Hao Wu <haowoo@google.com>
2023-05-12 21:21:58 +0000

    Add the driver fixture.
    Refactor to move driver logic into its own fixture. This prepares to create separate driver factories for different target platforms.
    
    Bug: b/279912663
    Change-Id: I4c16a63e9db74d496e546ab4d505181a1bfcccf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4525159
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Hao Wu <haowoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1143501}

--- chrome/test/variations/smoke_test.py
+++ chrome/test/variations/smoke_test.py
@@ -15,27 +15,27 @@ from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from typing import Callable
 
-DriverFactory = Callable[[str, ChromeOptions], webdriver.Remote]
 
-def test_load_simple_url(driver_factory: DriverFactory,
+def test_load_simple_url(driver_factory: fixtures.DriverFactory,
                          local_http_server: HTTPServer,
                          seed_locator: fixtures.SeedLocator):
   url = (f'http://localhost:{local_http_server.server_port}')
-  with driver_factory(seed_file=seed_locator.get_seed()) as driver:
+  with driver_factory.create_driver(
+    seed_file=seed_locator.get_seed()) as driver:
     driver.set_window_size(800, 600)
     driver.get(url)
     WebDriverWait(driver, 5).until(
       EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
 
-def test_basic_rendering(driver_factory: DriverFactory,
+def test_basic_rendering(driver_factory: fixtures.DriverFactory,
                          local_http_server: HTTPServer,
                          seed_locator: fixtures.SeedLocator,
                          skia_gold_util: fixtures.VariationsSkiaGoldUtil):
   url = (f'http://localhost:{local_http_server.server_port}')
-  with driver_factory(seed_file=seed_locator.get_seed()) as driver:
+  with driver_factory.create_driver(
+    seed_file=seed_locator.get_seed()) as driver:
     driver.set_window_size(800, 600)
     driver.get(url)
     body = WebDriverWait(driver, 5).until(
@@ -47,12 +47,12 @@ def test_basic_rendering(driver_factory: DriverFactory,
 
     assert status == 0, error_msg
 
-def test_load_crash_seed(driver_factory: DriverFactory,
+def test_load_crash_seed(driver_factory: fixtures.DriverFactory,
                          local_http_server: HTTPServer,
                          seed_locator: fixtures.SeedLocator):
   url = (f'http://localhost:{local_http_server.server_port}')
   # Launch Chrome normally.
-  with driver_factory() as driver:
+  with driver_factory.create_driver() as driver:
     driver.get("chrome://version")
     version = WebDriverWait(driver, 5).until(
       EC.presence_of_element_located((By.ID, 'version')))
@@ -60,6 +60,6 @@ def test_load_crash_seed(driver_factory: DriverFactory,
 
   # Launch again with bad seed, expecting a crash.
   with pytest.raises(WebDriverException):
-    with driver_factory(
+    with driver_factory.create_driver(
       seed_file=seed_locator.get_seed(fixtures.SeedName.CRASH)) as driver:
       driver.get(url)
