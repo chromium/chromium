d1a1a8fdd9b28feaf5d3accc0578092444a1083e
Andreas Haas <ahaas@chromium.org>
2023-05-12 09:28:29 +0000

    [v8][wasm] Increase the limit on sync compilation on the main thread
    With the introduction of the baseline compiler Liftoff and lazy
    compilation, the execution time of synchronous WebAssembly compilation
    has decreased significantly. Additionally hardware has improved since
    the definition of the original limit of 4KB. Therefore this CL
    increases the limit to 8MB.
    
    The limit was determined by performance measurements on a Google
    Pixel 1, which we considered as a representative low-end phone. This
    phone was able to compile an 8MB module in less than 200ms, which is
    the threshold for good responsiveness in the metric "interaction to
    next paint" defined by Google's Core Web Vitals.
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/nJw2zwaiJ2s/m/EYPgC5D3LwAJ
    
    Bug: chromium:1378329
    Change-Id: I3a4413da6c7f625e9b80a9da4b550b9f9ce60866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520918
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143158}

--- third_party/blink/web_tests/fast/wasm/wasm-limits-tests.js
+++ third_party/blink/web_tests/fast/wasm/wasm-limits-tests.js
@@ -2,22 +2,58 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var limit = Math.pow(2, 12);
+const limit = Math.pow(2, 23);
+
+const kWasmH0 = 0;
+const kWasmH1 = 0x61;
+const kWasmH2 = 0x73;
+const kWasmH3 = 0x6d;
+
+const kWasmV0 = 0x1;
+const kWasmV1 = 0;
+const kWasmV2 = 0;
+const kWasmV3 = 0;
+
+function wasmSignedLeb(val, max_len = 5) {
+  if (val == null) throw new Error("Leb value many not be null/undefined");
+  let res = [];
+  for (let i = 0; i < max_len; ++i) {
+    let v = val & 0x7f;
+    // If {v} sign-extended from 7 to 32 bits is equal to val, we are done.
+    if (((v << 25) >> 25) == val) {
+      res.push(v);
+      return res;
+    }
+    res.push(v | 0x80);
+    val = val >> 7;
+  }
+  throw new Error(
+      'Leb value <' + val + '> exceeds maximum length of ' + max_len);
+}
+
+function createTestBuffer(limit) {
+  const buffer = new Uint8Array(limit);
+  const header = [
+    kWasmH0, kWasmH1, kWasmH2, kWasmH3, // magic word
+    kWasmV0, kWasmV1, kWasmV2, kWasmV3, // version
+    0                                   // custom section
+  ];
+  // We calculate the section length so that the total module size is `limit`.
+  // For that we have to calculate the length of the leb encoding of the section
+  // length.
+  const sectionLength = limit - header.length -
+      wasmSignedLeb(limit).length;
+  const lengthBytes = wasmSignedLeb(sectionLength);
+  buffer.set(header);
+  buffer.set(lengthBytes, header.length);
+  return buffer;
+}
 
 function NoParameters() {
-  function ExpectTypeError(f) {
-    try {
-      f();
-    } catch (e) {
-      assert_true(e instanceof TypeError)
-      return;
-    }
-    assert_unreached();
-  }
-  ExpectTypeError(() => new WebAssembly.Module());
-  ExpectTypeError(() => new WebAssembly.Module("a"));
-  ExpectTypeError(() => new WebAssembly.Instance());
-  ExpectTypeError(() => new WebAssembly.Instance("a"));
+  assert_throws_js(TypeError, () => new WebAssembly.Module());
+  assert_throws_js(TypeError, () => new WebAssembly.Module("a"));
+  assert_throws_js(TypeError, () => new WebAssembly.Instance());
+  assert_throws_js(TypeError, () => new WebAssembly.Instance("a"));
 }
 
 function NoParameters_Promise() {
@@ -32,36 +68,40 @@ function NoParameters_Promise() {
   ]);
 }
 
-function TestBuffersAreCorrect() {
-  var buffs = createTestBuffers(limit);
-  assert_equals(buffs.small.byteLength, limit);
-  assert_equals(buffs.large.byteLength, limit + 1);
-}
-
-function compileFailsWithError(buffer, error_type) {
-  try {
-    new WebAssembly.Module(buffer);
-  } catch (e) {
-    assert_true(e instanceof error_type);
-  }
-}
-
 function TestSyncCompile() {
-  var buffs = createTestBuffers(limit);
-  assert_true(new WebAssembly.Module(buffs.small)
+  assert_true(new WebAssembly.Module(createTestBuffer(limit))
               instanceof WebAssembly.Module);
-  compileFailsWithError(buffs.large, RangeError);
+  assert_throws_js(
+      RangeError, () => new WebAssembly.Module(createTestBuffer(limit + 1)));
 }
 
 function TestPromiseCompile() {
-  return WebAssembly.compile(createTestBuffers(limit).large)
+  return WebAssembly.compile(createTestBuffer(limit + 1))
     .then(m => assert_true(m instanceof WebAssembly.Module));
 }
 
+function WorkerCode() {
+  onmessage = (event) => {
+    const buffer = event.data;
+    try {
+      let module = new WebAssembly.Module(buffer);
+      let instance = new WebAssembly.Instance(module);
+      postMessage(
+          module instanceof WebAssembly.Module &&
+          instance instanceof WebAssembly.Instance);
+    } catch (e) {
+      postMessage(false);
+    }
+  }
+}
+
 function TestWorkerCompileAndInstantiate() {
-  var worker = new Worker("wasm-limits-worker.js");
+  const workerBlob = new Blob(['(', WorkerCode.toString(), ')()']);
+  const blobUrl = blobURL = URL.createObjectURL(
+      workerBlob, {type: 'application/javascript; charset=utf-8'});
+  const worker = new Worker(blobUrl);
   return new Promise((resolve, reject) => {
-    worker.postMessage(createTestBuffers(limit).large);
+    worker.postMessage(createTestBuffer(limit + 1));
     worker.onmessage = function(event) {
       resolve(event.data);
     }
@@ -71,38 +111,34 @@ function TestWorkerCompileAndInstantiate() {
 }
 
 function TestPromiseCompileSyncInstantiate() {
-  return WebAssembly.compile(createTestBuffers(limit).large)
-    .then (m => new WebAssembly.Instance(m))
-    .then(assert_unreached,
-          e => assert_true(e instanceof RangeError));
+  return WebAssembly.compile(createTestBuffer(limit + 1))
+      .then(
+          m => assert_throws_js(RangeError, () => new WebAssembly.Instance(m)));
 }
 
 function TestPromiseCompileAsyncInstantiateFromBuffer() {
-  return WebAssembly.instantiate(createTestBuffers(limit).large)
+  return WebAssembly.instantiate(createTestBuffer(limit + 1))
     .then(i => assert_true(i.instance instanceof WebAssembly.Instance),
           assert_unreached);
 }
 
 function TestPromiseCompileAsyncInstantiateFromModule() {
-  return WebAssembly.compile(createTestBuffers(limit).large)
-    .then(m => {
-      assert_true(m instanceof WebAssembly.Module);
-      return WebAssembly.instantiate(m).
-        then(i => assert_true(i instanceof WebAssembly.Instance),
-             assert_unreached);
-    },
-    assert_unreached);
+  return WebAssembly.compile(createTestBuffer(limit + 1)).then(m => {
+    assert_true(m instanceof WebAssembly.Module);
+    return WebAssembly.instantiate(m).then(
+        i => assert_true(i instanceof WebAssembly.Instance), assert_unreached);
+  }, assert_unreached);
 }
 
 
 function TestCompileFromPromise() {
-  return Promise.resolve(createTestBuffers(limit).large)
+  return Promise.resolve(createTestBuffer(limit + 1))
     .then(WebAssembly.compile)
     .then(m => assert_true(m instanceof WebAssembly.Module))
 }
 
 function TestInstantiateFromPromise() {
-  return Promise.resolve(createTestBuffers(limit).large)
+  return Promise.resolve(createTestBuffer(limit + 1))
     .then(WebAssembly.instantiate)
     .then(pair => {
       assert_true(pair.module instanceof WebAssembly.Module);
@@ -111,7 +147,7 @@ function TestInstantiateFromPromise() {
 }
 
 function TestInstantiateFromPromiseChain() {
-  return Promise.resolve(createTestBuffers(limit).large)
+  return Promise.resolve(createTestBuffer(limit + 1))
     .then(WebAssembly.compile)
     .then(WebAssembly.instantiate)
     .then(i => assert_true(i instanceof WebAssembly.Instance))
