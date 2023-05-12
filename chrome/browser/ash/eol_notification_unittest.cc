ca911bda268b305df8897f32978edfcf9455856f
Matthew Mourgos <mmourgos@chromium.org>
2023-05-12 17:41:21 +0000

    [114 Merge] Fix TestIncentiveBeforeEolDate
    The TestIncentiveBeforeEolDate test was broken with crrev.com/c/4363231,
    which changed the eol_incentive_util to use profile->GetCreationTime().
    This caused the profile creation time to be set to the current date.
    Because the profile creation time must be more than 6 months for the eol
    incentive to show, this broke the test once May 2023 began, because the
    current date used by the test is within 6 months of November 2023 (the
    date used by the failing test).
    
    Fix this test by setting the profile creation time to a date
    which is at least 6 months before the current time (February 1 2023).
    
    Bug=b:280975476
    
    (cherry picked from commit 2496cc04cf61b61c83c31bb0daa7caf8ceb101f4)
    
    Change-Id: I1770b90d4807dc6295f1c2d3b06e2438aff32d62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4516021
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1141666}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4519361
    Cr-Commit-Position: refs/branch-heads/5735@{#538}
    Cr-Branched-From: 2f562e4ddbaf79a3f3cb338b4d1bd4398d49eb67-refs/heads/main@{#1135570}

--- chrome/browser/ash/eol_notification_unittest.cc
+++ chrome/browser/ash/eol_notification_unittest.cc
@@ -298,6 +298,12 @@ class EolIncentiveNotificationTest : public EolNotificationTest {
   void SetUp() override {
     EolNotificationTest::SetUp();
 
+    // Set the profile creation date to be at least 6 months before the current
+    // time set in these unittests, to correctly show the incentive.
+    base::Time creation_time;
+    ASSERT_TRUE(base::Time::FromUTCString("1 February 2023", &creation_time));
+    profile()->SetCreationTimeForTesting(creation_time);
+
     scoped_feature_list_.InitAndEnableFeature(ash::features::kEolIncentive);
   }
 
