d8f0a2563294524900b3fe8de69bb8bfcdacd93d
Russ Hamilton <behamilton@google.com>
2023-05-12 14:48:03 +0000

    Enable k-anonymity service storage
    This feature has been tested via Finch in Canary/Dev and Beta channels for over a month with no regressions. It should be ready for a full release.
    
    Change-Id: I8d81bdf1b8dbe7ac4066ddb1081eade492e4fc34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4523367
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143265}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -779,7 +779,7 @@ BASE_FEATURE(kKAnonymityServiceOHTTPRequests,
 // public keys.
 BASE_FEATURE(kKAnonymityServiceStorage,
              "KAnonymityServiceStorage",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kLinuxLowMemoryMonitor,
