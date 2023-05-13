fcf3eb5571554a633c932c3db4e7bcd54a5a70fa
Solomon Kinard <solomonkinard@chromium.org>
2023-05-13 01:21:33 +0000

    Extensions: file_util: Modernize code to use emplace_back & ranged loop
    Bug: None
    Change-Id: I2cf1fb543a051136ed9a7b54f02af9730b6f7406
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528397
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1143607}

--- extensions/common/file_util.cc
+++ extensions/common/file_util.cc
@@ -313,14 +313,15 @@ bool ValidateExtension(const Extension* extension,
   // not on the reserved list. We only warn, and do not block the loading of the
   // extension.
   std::string warning;
-  if (!CheckForIllegalFilenames(extension->path(), &warning))
-    warnings->push_back(InstallWarning(warning));
+  if (!CheckForIllegalFilenames(extension->path(), &warning)) {
+    warnings->emplace_back(warning);
+  }
 
   // Check that the extension does not include any Windows reserved filenames.
   std::string windows_reserved_warning;
   if (!CheckForWindowsReservedFilenames(extension->path(),
                                         &windows_reserved_warning)) {
-    warnings->push_back(InstallWarning(windows_reserved_warning));
+    warnings->emplace_back(windows_reserved_warning);
   }
 
   // Check that extensions don't include private key files.
@@ -336,10 +337,9 @@ bool ValidateExtension(const Extension* extension,
       return false;
     }
   } else {
-    for (size_t i = 0; i < private_keys.size(); ++i) {
-      warnings->push_back(InstallWarning(
-          l10n_util::GetStringFUTF8(IDS_EXTENSION_CONTAINS_PRIVATE_KEY,
-                                    private_keys[i].LossyDisplayName())));
+    for (const auto& private_key : private_keys) {
+      warnings->emplace_back(l10n_util::GetStringFUTF8(
+          IDS_EXTENSION_CONTAINS_PRIVATE_KEY, private_key.LossyDisplayName()));
     }
     // Only warn; don't block loading the extension.
   }
