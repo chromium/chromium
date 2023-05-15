318923642905a0ac518d6adc171b36bc45ea92ac
Simon Zünd <szuend@chromium.org>
2023-05-15 11:45:32 +0000

    [devtools] Import test runners in http/tests/devtools/csp
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=kprokopenko@chromium.org
    
    Bug: 1442410
    Change-Id: I4befc3f198adcf84457aa1d4b60f9048ca22a13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4530370
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144012}

--- third_party/blink/web_tests/http/tests/devtools/csp/csp-inline-warning-contains-stacktrace.js
+++ third_party/blink/web_tests/http/tests/devtools/csp/csp-inline-warning-contains-stacktrace.js
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {ConsoleTestRunner} from 'console_test_runner';
+
 (async function() {
   TestRunner.addResult(
       `This test injects an inline script from JavaScript. The resulting console error should contain a stack trace.\n`);
-  await TestRunner.loadLegacyModule('console'); await TestRunner.loadTestModule('console_test_runner');
+  await TestRunner.loadLegacyModule('console');
 
   await TestRunner.loadHTML(`
     <!DOCTYPE html>
