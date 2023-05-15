6924915e3bcbfe4cec6489cca1ba27918410d196
Avi Drissman <avi@chromium.org>
2023-05-15 18:27:27 +0000

    Convert /components/sync_device_info to use ARC
    See https://chromium.googlesource.com/chromium/src/+/main/docs/mac/arc.md
    for information about this conversion.
    
    This ended up only adding the ARC boilerplate as a marker to indicate
    that these files were audited.
    
    Bug: 1280317
    Change-Id: If76c5f1c201f7e497f6ca835bec4eea78ed75b0f
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528836
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144241}

--- components/sync_device_info/local_device_info_util_mac.mm
+++ components/sync_device_info/local_device_info_util_mac.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <SystemConfiguration/SCDynamicStoreCopySpecific.h>
+#import <SystemConfiguration/SystemConfiguration.h>
 #include <stddef.h>
 #include <sys/sysctl.h>
 
@@ -12,6 +12,10 @@
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace syncer {
 
 // Returns the Hardware model name, without trailing numbers, if
@@ -20,30 +24,34 @@ namespace syncer {
 // model, this simply returns "Unknown".
 std::string GetPersonalizableDeviceNameInternal() {
   // Do not use NSHost currentHost, as it's very slow. http://crbug.com/138570
-  SCDynamicStoreContext context = {0, NULL, NULL, NULL};
-  base::ScopedCFTypeRef<SCDynamicStoreRef> store(SCDynamicStoreCreate(
-      kCFAllocatorDefault, CFSTR("chrome_sync"), NULL, &context));
+  SCDynamicStoreContext context = {0};
+  base::ScopedCFTypeRef<SCDynamicStoreRef> store(
+      SCDynamicStoreCreate(kCFAllocatorDefault, CFSTR("chrome_sync"),
+                           /*callout=*/nullptr, &context));
   base::ScopedCFTypeRef<CFStringRef> machine_name(
-      SCDynamicStoreCopyLocalHostName(store.get()));
-  if (machine_name.get())
-    return base::SysCFStringRefToUTF8(machine_name.get());
+      SCDynamicStoreCopyLocalHostName(store));
+  if (machine_name) {
+    return base::SysCFStringRefToUTF8(machine_name);
+  }
 
   // Fall back to get computer name.
   base::ScopedCFTypeRef<CFStringRef> computer_name(
-      SCDynamicStoreCopyComputerName(store.get(), NULL));
-  if (computer_name.get())
-    return base::SysCFStringRefToUTF8(computer_name.get());
+      SCDynamicStoreCopyComputerName(store, /*nameEncoding=*/nullptr));
+  if (computer_name) {
+    return base::SysCFStringRefToUTF8(computer_name);
+  }
 
   // If all else fails, return to using a slightly nicer version of the
   // hardware model.
-  char modelBuffer[256];
-  size_t length = sizeof(modelBuffer);
-  if (!sysctlbyname("hw.model", modelBuffer, &length, NULL, 0)) {
+  char model_buffer[256];
+  size_t length = sizeof(model_buffer);
+  if (!sysctlbyname("hw.model", model_buffer, &length, nullptr, 0)) {
     for (size_t i = 0; i < length; i++) {
-      if (base::IsAsciiDigit(modelBuffer[i]))
-        return std::string(modelBuffer, 0, i);
+      if (base::IsAsciiDigit(model_buffer[i])) {
+        return std::string(model_buffer, 0, i);
+      }
     }
-    return std::string(modelBuffer, 0, length);
+    return std::string(model_buffer, 0, length);
   }
   return "Unknown";
 }
