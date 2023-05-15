702f2fa983a1aac4ab862521b3725f2e72813da4
Owen Min <zmin@chromium.org>
2023-05-15 20:51:03 +0000

    Add legacy tech report generator
    Convert report data into event proto. Timestamp will be round to UTC
    midnight.
    
    Change-Id: I6dc91eccc7c68d9b0c32f7b576a85786034f540f
    Bug: 1441215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4522306
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1144353}

---
+++ chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator.cc
@@ -0,0 +1,49 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/reporting/legacy_tech/legacy_tech_report_generator.h"
+
+namespace enterprise_reporting {
+
+LegacyTechReportGenerator::LegacyTechData::LegacyTechData() = default;
+LegacyTechReportGenerator::LegacyTechData::LegacyTechData(
+    const std::string& type,
+    const base::Time& timestamp,
+    const GURL& url,
+    const std::string& matched_url,
+    const std::string& filename,
+    uint64_t line,
+    uint64_t column)
+    : type(type),
+      timestamp(timestamp),
+      url(url),
+      matched_url(matched_url),
+      filename(filename),
+      line(line),
+      column(column) {}
+LegacyTechReportGenerator::LegacyTechData::~LegacyTechData() = default;
+
+LegacyTechReportGenerator::LegacyTechReportGenerator() = default;
+LegacyTechReportGenerator::~LegacyTechReportGenerator() = default;
+
+std::vector<std::unique_ptr<LegacyTechEvent>>
+LegacyTechReportGenerator::Generate(const RealTimeReportGenerator::Data& data) {
+  const LegacyTechData& legacy_tech_data =
+      static_cast<const LegacyTechData&>(data);
+  std::unique_ptr<LegacyTechEvent> report = std::make_unique<LegacyTechEvent>();
+  report->set_feature_id(legacy_tech_data.type);
+  // Blur timestamp for privacy.
+  report->set_event_timestamp_millis(
+      legacy_tech_data.timestamp.UTCMidnight().ToJavaTime());
+  report->set_url(legacy_tech_data.url.spec());
+  report->set_allowlisted_url_match(legacy_tech_data.matched_url);
+  report->set_filename(legacy_tech_data.filename);
+  report->set_column(legacy_tech_data.column);
+  report->set_line(legacy_tech_data.line);
+  std::vector<std::unique_ptr<LegacyTechEvent>> reports;
+  reports.push_back(std::move(report));
+  return reports;
+}
+
+}  // namespace enterprise_reporting
