c82190c97f9dba375f4814d9fb0e725b7a4e9622
Rakina Zata Amni <rakina@chromium.org>
2023-05-15 15:19:31 +0000

    [M112] Return after ReadyCommitNavigation call in CommitErrorPage if it deletes NavigationRequest
    NavigationRequest::ReadyToCommitNavigation() can cause deletion of the
    NavigationRequest, so callers should check for that possibility after
    calling the function. A caller in CommitErrorPage is missing that
    check, which this CL adds, along with a regression test.
    
    (cherry picked from commit 42db806805ef2be64ee92803d3a784631b2a7df0)
    
    Bug: 1444360
    Change-Id: I3964da4909a6709b7730d25d6497b19c098f4f21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4520493
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1143298}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4528897
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5615@{#1426}
    Cr-Branched-From: 9c6408ef696e83a9936b82bbead3d41c93c82ee4-refs/heads/main@{#1109224}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -5174,7 +5174,13 @@ void NavigationRequest::CommitErrorPage(
     }
   }
 
+  base::WeakPtr<NavigationRequest> weak_self(weak_factory_.GetWeakPtr());
   ReadyToCommitNavigation(true /* is_error */);
+  // The caller above might result in the deletion of `this`. Return immediately
+  // if so.
+  if (!weak_self) {
+    return;
+  }
 
   PopulateDocumentTokenForCrossDocumentNavigation();
   // Use a separate cache shard, and no cookies, for error pages.
