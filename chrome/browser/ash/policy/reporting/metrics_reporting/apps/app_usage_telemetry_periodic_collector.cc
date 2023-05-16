905a95b66e3d16754cecc390ab0b9e164e541ca1
Vignesh Shenvi <vshenvi@google.com>
2023-05-16 22:15:31 +0000

    Introducing a new app usage telemetry periodic collector
    This change introduces a new periodic collector for app usage telemetry
    collection and reporting. This will only control the rate of collection
    based on the new policy setting. This is to avoid data staleness because
    we do not track timestamps with the telemetry data that is being
    persisted in the pref store today. The observer component will perform
    necessary validation on allowed types before it persists data in the
    pref store.
    
    I will be renaming the AppUsageCollector and all its references
    following this change for better distinction in component
    responsibilities.
    
    Bug: b:260803048
    Change-Id: Idcfd0c428c47086d45527026a8d49743351f9f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4535336
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1144969}

---
+++ chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_periodic_collector.cc
@@ -0,0 +1,61 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_periodic_collector.h"
+
+#include <memory>
+
+#include "base/functional/bind.h"
+#include "base/time/time.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_prefs.h"
+#include "chrome/browser/chromeos/reporting/metric_default_utils.h"
+#include "components/reporting/metrics/collector_base.h"
+#include "components/reporting/metrics/metric_rate_controller.h"
+#include "components/reporting/metrics/metric_report_queue.h"
+#include "components/reporting/metrics/reporting_settings.h"
+#include "components/reporting/metrics/sampler.h"
+#include "components/reporting/proto/synced/metric_data.pb.h"
+
+namespace reporting {
+
+AppUsageTelemetryPeriodicCollector::AppUsageTelemetryPeriodicCollector(
+    Sampler* sampler,
+    MetricReportQueue* metric_report_queue,
+    ReportingSettings* reporting_settings)
+    : CollectorBase(sampler),
+      metric_report_queue_(metric_report_queue),
+      rate_controller_(std::make_unique<MetricRateController>(
+          base::BindRepeating(&AppUsageTelemetryPeriodicCollector::Collect,
+                              base::Unretained(this),
+                              /*is_event_driven=*/false),
+          reporting_settings,
+          ::ash::reporting::kReportAppUsageCollectionRateMs,
+          metrics::kDefaultAppUsageTelemetryCollectionRate,
+          /*rate_unit_to_ms=*/1)) {
+  rate_controller_->Start();
+}
+
+AppUsageTelemetryPeriodicCollector::~AppUsageTelemetryPeriodicCollector() =
+    default;
+
+void AppUsageTelemetryPeriodicCollector::OnMetricDataCollected(
+    bool is_event_driven,
+    absl::optional<MetricData> metric_data) {
+  if (!metric_data.has_value()) {
+    // No data to report.
+    return;
+  }
+  metric_data->set_timestamp_ms(base::Time::Now().ToJavaTime());
+  metric_report_queue_->Enqueue(std::move(metric_data.value()));
+}
+
+bool AppUsageTelemetryPeriodicCollector::CanCollect() const {
+  // `AppUsageCollector` performs necessary checks and validation to ensure the
+  // app is allowlisted for reporting purposes. Because we do not record the
+  // timestamp with this usage telemetry data, we report it right away to
+  // prevent staleness.
+  return true;
+}
+
+}  // namespace reporting
---
+++ chrome/browser/ash/policy/reporting/metrics_reporting/apps/app_usage_telemetry_periodic_collector.h
@@ -0,0 +1,52 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_APPS_APP_USAGE_TELEMETRY_PERIODIC_COLLECTOR_H_
+#define CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_APPS_APP_USAGE_TELEMETRY_PERIODIC_COLLECTOR_H_
+
+#include <memory>
+
+#include "base/allocator/partition_allocator/pointers/raw_ptr.h"
+#include "components/reporting/metrics/collector_base.h"
+#include "components/reporting/metrics/metric_report_queue.h"
+#include "components/reporting/metrics/reporting_settings.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace reporting {
+
+// Periodic collector that collects and reports app usage telemetry originally
+// collected by the `AppUsageCollector`. This is a standalone component that is
+// similar to the `PeriodicCollector` but only controls the collection rate
+// based on the respective policy setting. This is to avoid data staleness
+// because we do not associate usage data with a timestamp today.
+class AppUsageTelemetryPeriodicCollector : public CollectorBase {
+ public:
+  AppUsageTelemetryPeriodicCollector(Sampler* sampler,
+                                     MetricReportQueue* metric_report_queue,
+                                     ReportingSettings* reporting_settings);
+  AppUsageTelemetryPeriodicCollector(
+      const AppUsageTelemetryPeriodicCollector& other) = delete;
+  AppUsageTelemetryPeriodicCollector& operator=(
+      const AppUsageTelemetryPeriodicCollector& other) = delete;
+  ~AppUsageTelemetryPeriodicCollector() override;
+
+ protected:
+  // CollectorBase:
+  void OnMetricDataCollected(bool is_event_driven,
+                             absl::optional<MetricData> metric_data) override;
+
+  // CollectorBase:
+  bool CanCollect() const override;
+
+ private:
+  // `MetricReportQueue` used for enqueueing data collected by the sampler.
+  const raw_ptr<MetricReportQueue> metric_report_queue_;
+
+  // Component used to control collection rate based on the policy setting.
+  const std::unique_ptr<MetricRateController> rate_controller_;
+};
+
+}  // namespace reporting
+
+#endif  // CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_APPS_APP_USAGE_TELEMETRY_PERIODIC_COLLECTOR_H_
