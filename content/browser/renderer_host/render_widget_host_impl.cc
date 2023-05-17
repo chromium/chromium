185201f9ba9351055c87d5da1c4dee0b281538ac
Randolf Jung <jrandolf@chromium.org>
2023-05-17 13:39:28 +0000

    Run mouse callbacks for emulated touch events
    This CL fixes an issue where mouse callbacks do not get triggered when touch emulation from mouse is enabled.
    
    Fixed: 1415994
    Change-Id: Ifb66c74182cdddad12b4f88c34f6e5c3b51c212b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4534375
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Randolf Jung <jrandolf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145297}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1492,10 +1492,25 @@ void RenderWidgetHostImpl::ForwardMouseEventWithLatencyInfo(
   if (IsIgnoringInputEvents())
     return;
 
-  auto* touch_emulator = GetExistingTouchEmulator();
-  if (touch_emulator &&
-      touch_emulator->HandleMouseEvent(mouse_event, GetView())) {
-    return;
+  if (auto* emulator = GetExistingTouchEmulator()) {
+    if (emulator->HandleMouseEvent(
+            mouse_event, GetView(),
+            base::BindOnce(
+                [](base::WeakPtr<RenderWidgetHostImpl> host,
+                   std::unique_ptr<blink::WebMouseEvent> mouse_event,
+                   ui::LatencyInfo latency) {
+                  if (host) {
+                    host->OnMouseEventAck(
+                        MouseEventWithLatencyInfo(*mouse_event, latency),
+                        blink::mojom::InputEventResultSource(),
+                        blink::mojom::InputEventResultState::kConsumed);
+                  }
+                },
+                weak_factory_.GetWeakPtr(),
+                std::make_unique<blink::WebMouseEvent>(mouse_event),
+                latency))) {
+      return;
+    }
   }
 
   MouseEventWithLatencyInfo mouse_with_latency(mouse_event, latency);
