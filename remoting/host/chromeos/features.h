59cf01d40253227614fb677db98eb48da8a2c46d
Jeroen Dhollander <jeroendh@google.com>
2023-05-17 16:32:13 +0000

    Introduce EnableCrdAdminRemoteAccessV2 feature flag
    This will gate all further feature development
    related to launching crd remote admin to GA.
    
    Fixed: b/283088241
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I0e278d5548e2762d7fdd9a8ce2a736d8642b4cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543568
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145409}

--- remoting/host/chromeos/features.h
+++ remoting/host/chromeos/features.h
@@ -12,6 +12,11 @@ namespace remoting::features {
 // Enable to allow CRD remote admin connections when the ChromeOS device is at
 // the login screen.
 BASE_DECLARE_FEATURE(kEnableCrdAdminRemoteAccess);
+
+// Enable the V2 feature development related to launching CRD remote admin
+// to GA.
+BASE_DECLARE_FEATURE(kEnableCrdAdminRemoteAccessV2);
+
 // Force all enterprise remote connections to be remote access connections.
 // Only used for local testing until the DPanel UI supports sending remote
 // access requests.
