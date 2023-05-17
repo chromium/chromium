ea1fb9a21097a70c91c6effcae2b9223a45342d2
Simon Zünd <szuend@chromium.org>
2023-05-17 11:06:42 +0000

    [devtools] Import test runners in http/tests/devtools/oopif
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    Note that 1 test is rebaslined: The expectation references line
    numbers from the test file itself.
    
    R=wolfi@chromium.org
    
    Bug: 1442410
    Change-Id: Ic7319dc53a8a338f08328cc2592df721de919fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4542982
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1145252}

--- third_party/blink/web_tests/http/tests/devtools/oopif/oopif-performance-cpu-profiles.js
+++ third_party/blink/web_tests/http/tests/devtools/oopif/oopif-performance-cpu-profiles.js
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {PerformanceTestRunner} from 'performance_test_runner';
+
 (async function() {
   TestRunner.addResult(`Test CPU profiles are recorded for OOPIFs.\n`);
 
-  await TestRunner.loadLegacyModule('timeline'); await TestRunner.loadTestModule('performance_test_runner');
+  await TestRunner.loadLegacyModule('timeline');
   await TestRunner.showPanel('timeline');
 
   await PerformanceTestRunner.startTimeline();
