d1f4d75f7bb9f55591a47539ca3d8fa9cf62f1d3
Jonathan Lee <jonathanjlee@google.com>
2023-05-17 19:58:58 +0000

    Roll wpt tooling
    This rolls wpt to latest commit at
    https://github.com/web-platform-tests/wpt.
    REMOTE-WPT-HEAD: 9cc3b5b74daa13133551f8f06e358925bf0144d3
    
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I349880db03855f3e7f5449d21a37ae5844c360eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544200
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145556}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/wptcommandline.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/wptcommandline.py
@@ -280,7 +280,8 @@ scheme host and port.""")
                                 help="Total number of chunks to use")
     chunking_group.add_argument("--this-chunk", action="store", type=int, default=1,
                                 help="Chunk number to run")
-    chunking_group.add_argument("--chunk-type", action="store", choices=["none", "hash", "dir_hash"],
+    chunking_group.add_argument("--chunk-type", action="store",
+                                choices=["none", "hash", "id_hash", "dir_hash"],
                                 default=None, help="Chunking type to use")
 
     ssl_group = parser.add_argument_group("SSL/TLS")
