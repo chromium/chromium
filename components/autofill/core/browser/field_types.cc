af100f4ce7429a68cd25e3a7a76edb72d21221cc
Norge Vizcay <vizcay@google.com>
2023-05-17 17:55:47 +0000

    Simplify FieldTypeToStringPiece function.
    The recent introduction of kTypeNameToFieldType allows us to simplify the FieldTypeToStringPiece function by removing the huge enum.
    
    Change-Id: I54beb798f214053ac9e5027b9e743f0d132cedb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4543182
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1145477}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -5,6 +5,7 @@
 #include "components/autofill/core/browser/field_types.h"
 
 #include "base/containers/fixed_flat_map.h"
+#include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/strings/string_piece.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -14,6 +15,9 @@
 namespace autofill {
 
 // This map should be extended for every added ServerFieldType.
+// You are free to add or remove the String representation of ServerFieldType,
+// but don't change any existing values, Android WebView presents them to
+// Autofill Service as part of APIs.
 static constexpr auto kTypeNameToFieldType =
     base::MakeFixedFlatMap<base::StringPiece, ServerFieldType>(
         {{"NO_SERVER_DATA", NO_SERVER_DATA},
@@ -255,184 +259,17 @@ bool IsFillableFieldType(ServerFieldType field_type) {
 }
 
 base::StringPiece FieldTypeToStringPiece(ServerFieldType type) {
-  // You are free to add or remove the String representation of ServerFieldType,
-  // but don't change any existing values, Android WebView presents them to
-  // Autofill Service as part of APIs.
-  switch (type) {
-    case NO_SERVER_DATA:
-      return "NO_SERVER_DATA";
-    case UNKNOWN_TYPE:
-      return "UNKNOWN_TYPE";
-    case EMPTY_TYPE:
-      return "EMPTY_TYPE";
-    case NAME_HONORIFIC_PREFIX:
-      return "NAME_HONORIFIC_PREFIX";
-    case NAME_FULL_WITH_HONORIFIC_PREFIX:
-      return "NAME_FULL_WITH_HONORIFIC_PREFIX";
-    case NAME_FIRST:
-      return "NAME_FIRST";
-    case NAME_MIDDLE:
-      return "NAME_MIDDLE";
-    case NAME_LAST:
-      return "NAME_LAST";
-    case NAME_LAST_FIRST:
-      return "NAME_LAST_FIRST";
-    case NAME_LAST_CONJUNCTION:
-      return "NAME_LAST_CONJUNCTION";
-    case NAME_LAST_SECOND:
-      return "NAME_LAST_SECOND";
-    case NAME_MIDDLE_INITIAL:
-      return "NAME_MIDDLE_INITIAL";
-    case NAME_FULL:
-      return "NAME_FULL";
-    case NAME_SUFFIX:
-      return "NAME_SUFFIX";
-    case EMAIL_ADDRESS:
-      return "EMAIL_ADDRESS";
-    case PHONE_HOME_NUMBER:
-      return "PHONE_HOME_NUMBER";
-    case PHONE_HOME_NUMBER_PREFIX:
-      return "PHONE_HOME_NUMBER_PREFIX";
-    case PHONE_HOME_NUMBER_SUFFIX:
-      return "PHONE_HOME_NUMBER_SUFFIX";
-    case PHONE_HOME_CITY_CODE:
-      return "PHONE_HOME_CITY_CODE";
-    case PHONE_HOME_CITY_CODE_WITH_TRUNK_PREFIX:
-      return "PHONE_HOME_CITY_CODE_WITH_TRUNK_PREFIX";
-    case PHONE_HOME_COUNTRY_CODE:
-      return "PHONE_HOME_COUNTRY_CODE";
-    case PHONE_HOME_CITY_AND_NUMBER:
-      return "PHONE_HOME_CITY_AND_NUMBER";
-    case PHONE_HOME_CITY_AND_NUMBER_WITHOUT_TRUNK_PREFIX:
-      return "PHONE_HOME_CITY_AND_NUMBER_WITHOUT_TRUNK_PREFIX";
-    case PHONE_HOME_WHOLE_NUMBER:
-      return "PHONE_HOME_WHOLE_NUMBER";
-    case PHONE_HOME_EXTENSION:
-      return "PHONE_HOME_EXTENSION";
-    case ADDRESS_HOME_ADDRESS:
-      return "ADDRESS_HOME_ADDRESS";
-    case ADDRESS_HOME_ADDRESS_WITH_NAME:
-      return "ADDRESS_HOME_ADDRESS_WITH_NAME";
-    case ADDRESS_HOME_FLOOR:
-      return "ADDRESS_HOME_FLOOR";
-    case ADDRESS_HOME_LINE1:
-      return "ADDRESS_HOME_LINE1";
-    case ADDRESS_HOME_LINE2:
-      return "ADDRESS_HOME_LINE2";
-    case ADDRESS_HOME_LINE3:
-      return "ADDRESS_HOME_LINE3";
-    case ADDRESS_HOME_APT_NUM:
-      return "ADDRESS_HOME_APT_NUM";
-    case ADDRESS_HOME_CITY:
-      return "ADDRESS_HOME_CITY";
-    case ADDRESS_HOME_STATE:
-      return "ADDRESS_HOME_STATE";
-    case ADDRESS_HOME_ZIP:
-      return "ADDRESS_HOME_ZIP";
-    case ADDRESS_HOME_COUNTRY:
-      return "ADDRESS_HOME_COUNTRY";
-    case BIRTHDATE_DAY:
-      return "BIRTHDATE_DAY";
-    case BIRTHDATE_MONTH:
-      return "BIRTHDATE_MONTH";
-    case BIRTHDATE_4_DIGIT_YEAR:
-      return "BIRTHDATE_YEAR_4_DIGITS";
-    case CREDIT_CARD_NAME_FULL:
-      return "CREDIT_CARD_NAME_FULL";
-    case CREDIT_CARD_NAME_FIRST:
-      return "CREDIT_CARD_NAME_FIRST";
-    case CREDIT_CARD_NAME_LAST:
-      return "CREDIT_CARD_NAME_LAST";
-    case CREDIT_CARD_NUMBER:
-      return "CREDIT_CARD_NUMBER";
-    case CREDIT_CARD_EXP_MONTH:
-      return "CREDIT_CARD_EXP_MONTH";
-    case CREDIT_CARD_EXP_2_DIGIT_YEAR:
-      return "CREDIT_CARD_EXP_2_DIGIT_YEAR";
-    case CREDIT_CARD_EXP_4_DIGIT_YEAR:
-      return "CREDIT_CARD_EXP_4_DIGIT_YEAR";
-    case CREDIT_CARD_EXP_DATE_2_DIGIT_YEAR:
-      return "CREDIT_CARD_EXP_DATE_2_DIGIT_YEAR";
-    case CREDIT_CARD_EXP_DATE_4_DIGIT_YEAR:
-      return "CREDIT_CARD_EXP_DATE_4_DIGIT_YEAR";
-    case CREDIT_CARD_TYPE:
-      return "CREDIT_CARD_TYPE";
-    case CREDIT_CARD_VERIFICATION_CODE:
-      return "CREDIT_CARD_VERIFICATION_CODE";
-    case COMPANY_NAME:
-      return "COMPANY_NAME";
-    case FIELD_WITH_DEFAULT_VALUE:
-      return "FIELD_WITH_DEFAULT_VALUE";
-    case MERCHANT_EMAIL_SIGNUP:
-      return "MERCHANT_EMAIL_SIGNUP";
-    case MERCHANT_PROMO_CODE:
-      return "MERCHANT_PROMO_CODE";
-    case PASSWORD:
-      return "PASSWORD";
-    case ACCOUNT_CREATION_PASSWORD:
-      return "ACCOUNT_CREATION_PASSWORD";
-    case ADDRESS_HOME_STREET_ADDRESS:
-      return "ADDRESS_HOME_STREET_ADDRESS";
-    case ADDRESS_HOME_SORTING_CODE:
-      return "ADDRESS_HOME_SORTING_CODE";
-    case ADDRESS_HOME_DEPENDENT_LOCALITY:
-      return "ADDRESS_HOME_DEPENDENT_LOCALITY";
-    case NOT_ACCOUNT_CREATION_PASSWORD:
-      return "NOT_ACCOUNT_CREATION_PASSWORD";
-    case USERNAME:
-      return "USERNAME";
-    case USERNAME_AND_EMAIL_ADDRESS:
-      return "USERNAME_AND_EMAIL_ADDRESS";
-    case NEW_PASSWORD:
-      return "NEW_PASSWORD";
-    case PROBABLY_NEW_PASSWORD:
-      return "PROBABLY_NEW_PASSWORD";
-    case NOT_NEW_PASSWORD:
-      return "NOT_NEW_PASSWORD";
-    case CONFIRMATION_PASSWORD:
-      return "CONFIRMATION_PASSWORD";
-    case SEARCH_TERM:
-      return "SEARCH_TERM";
-    case PRICE:
-      return "PRICE";
-    case NUMERIC_QUANTITY:
-      return "NUMERIC_QUANTITY";
-    case NOT_PASSWORD:
-      return "NOT_PASSWORD";
-    case SINGLE_USERNAME:
-      return "SINGLE_USERNAME";
-    case NOT_USERNAME:
-      return "NOT_USERNAME";
-    case UPI_VPA:
-      return "UPI_VPA";
-    case ADDRESS_HOME_STREET_NAME:
-      return "ADDRESS_HOME_STREET_NAME";
-    case ADDRESS_HOME_DEPENDENT_STREET_NAME:
-      return "ADDRESS_HOME_DEPENDENT_STREET_NAME";
-    case ADDRESS_HOME_HOUSE_NUMBER:
-      return "ADDRESS_HOME_HOUSE_NUMBER";
-    case ADDRESS_HOME_STREET_AND_DEPENDENT_STREET_NAME:
-      return "ADDRESS_HOME_STREET_AND_DEPENDENT_STREET_NAME";
-    case ADDRESS_HOME_PREMISE_NAME:
-      return "ADDRESS_HOME_PREMISE_NAME";
-    case ADDRESS_HOME_SUBPREMISE:
-      return "ADDRESS_HOME_SUBPREMISE";
-    case ADDRESS_HOME_OTHER_SUBUNIT:
-      return "ADDRESS_HOME_OTHER_SUBUNIT";
-    case AMBIGUOUS_TYPE:
-      return "AMBIGUOUS_TYPE";
-    case IBAN_VALUE:
-      return "IBAN_VALUE";
-    case CREDIT_CARD_STANDALONE_VERIFICATION_CODE:
-      return "CREDIT_CARD_STANDALONE_VERIFICATION_CODE";
-    case ONE_TIME_CODE:
-      return "ONE_TIME_CODE";
-    case ADDRESS_HOME_LANDMARK:
-      return "ADDRESS_HOME_LANDMARK";
-    case ADDRESS_HOME_BETWEEN_STREETS:
-      return "ADDRESS_HOME_BETWEEN_STREETS";
-    case MAX_VALID_FIELD_TYPE:
-      return "";
+  static const base::NoDestructor<
+      base::flat_map<ServerFieldType, base::StringPiece>>
+      kFieldTypeToTypeName(
+          base::MakeFlatMap<ServerFieldType, base::StringPiece>(
+              kTypeNameToFieldType, {}, [](const auto& item) {
+                return std::make_pair(item.second, item.first);
+              }));
+
+  auto it = kFieldTypeToTypeName->find(type);
+  if (it != kFieldTypeToTypeName->end()) {
+    return it->second;
   }
   NOTREACHED_NORETURN();
 }
