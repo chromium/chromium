78ffd9be3f4a7992df344eea37883c7efce7f607
David Padlipsky <dpad@google.com>
2023-05-18 19:29:26 +0000

    Settings Split: Add devices to fake mouse blocklist
    Bug: b/241965700
    Change-Id: I4f7bf9081f5a68236bfc866539c4573e850f7606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546377
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146097}

--- ash/system/input_device_settings/input_device_settings_utils.cc
+++ ash/system/input_device_settings/input_device_settings_utils.cc
@@ -123,9 +123,13 @@ const base::Value::Dict* GetLoginScreenSettingsDict(
 bool IsKeyboardPretendingToBeMouse(const ui::InputDevice& device) {
   static constexpr auto kKeyboardsPretendingToBeMice =
       base::MakeFixedFlatSet<VendorProductId>({
+          {0x29ea, 0x0102},  // Kinesis Freestyle Edge RGB
+          {0x46d, 0xc343},   // Logitech G915 TKL (USB)
+          {0x46d, 0xb35f},   // Logitech G915 TKL (Bluetooth)
           {0x046d, 0x408a},  // Logitech MX Keys (Universal Receiver)
           {0x046d, 0xb35b},  // Logitech MX Keys (Bluetooth)
           {0x0951, 0x16e6},  // HyperX Alloy Origins Core
+          {0x1532, 0x025e},  // Razer Cynosa V2
       });
 
   return kKeyboardsPretendingToBeMice.contains(
