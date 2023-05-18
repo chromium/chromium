2c4a316697591c6b53d7885738b075db33be22ee
Chia-I Wu <olv@google.com>
2023-05-18 00:06:56 +0000

    [vulkan] GpuFenceHandle is sync fd when IS_CHROMEOS_ASH
    GpuFenceHandle is sync fd when IS_POSIX.  But there are other issues on
    wayland+vulkan so let's fix it for IS_CHROMEOS_ASH for now.
    
    Test: --enable-features=Vulkan on zork
    Bug: b:280353184, b:267347192
    Change-Id: I764a07adf782861c72fc0a50cb59fd1e0e561746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4501248
    Reviewed-by: Brian Ho <hob@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Chia-I Wu <olv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1145720}

--- gpu/vulkan/semaphore_handle.cc
+++ gpu/vulkan/semaphore_handle.cc
@@ -4,6 +4,7 @@
 
 #include "gpu/vulkan/semaphore_handle.h"
 
+#include "base/logging.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_POSIX)
@@ -38,6 +39,9 @@ SemaphoreHandle::SemaphoreHandle(gfx::GpuFenceHandle fence_handle) {
   // https://fuchsia.dev/reference/fidl/fuchsia.ui.scenic.
   Init(VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_ZIRCON_EVENT_BIT_FUCHSIA,
        std::move(fence_handle.owned_event));
+#elif BUILDFLAG(IS_CHROMEOS_ASH)
+  Init(VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT_KHR,
+       std::move(fence_handle.owned_fd));
 #elif BUILDFLAG(IS_POSIX)
   Init(VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT_KHR,
        std::move(fence_handle.owned_fd));
@@ -60,6 +64,12 @@ gfx::GpuFenceHandle SemaphoreHandle::ToGpuFenceHandle() && {
   // VkSemaphore, which can then be used to submit present work, see
   // https://fuchsia.dev/reference/fidl/fuchsia.ui.scenic.
   fence_handle.owned_event = TakeHandle();
+#elif BUILDFLAG(IS_CHROMEOS_ASH)
+  if (type_ == VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT_KHR) {
+    fence_handle.owned_fd = TakeHandle();
+  } else {
+    DLOG(ERROR) << "Unable to convert SemaphoreHandle to GpuFenceHandle";
+  }
 #elif BUILDFLAG(IS_POSIX)
   fence_handle.owned_fd = TakeHandle();
 #elif BUILDFLAG(IS_WIN)
