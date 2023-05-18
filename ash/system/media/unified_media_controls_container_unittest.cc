51fba96199e8bbc63988e9aa99897479fa30eedb
Sam Cackett <samcackett@google.com>
2023-05-18 19:30:15 +0000

    Parameterize tests for QSRevamp
    Bug: b/283219390
    Change-Id: Ia7f49c33dc2598e423d267fd015f97ceabfdb841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4546218
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Sam Cackett <samcackett@google.com>
    Cr-Commit-Position: refs/heads/main@{#1146098}

--- ash/system/media/unified_media_controls_container_unittest.cc
+++ ash/system/media/unified_media_controls_container_unittest.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "ash/system/media/media_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/status_area_widget_test_helper.h"
@@ -13,15 +14,21 @@
 #include "ash/system/unified/unified_system_tray_bubble.h"
 #include "ash/system/unified/unified_system_tray_view.h"
 #include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
 
 namespace ash {
 
-class UnifiedMediaControlsContainerTest : public AshTestBase {
+class UnifiedMediaControlsContainerTest
+    : public AshTestBase,
+      public testing::WithParamInterface<bool> {
  public:
   UnifiedMediaControlsContainerTest() = default;
   ~UnifiedMediaControlsContainerTest() override = default;
 
   void SetUp() override {
+    scoped_feature_list_.InitWithFeatureState(features::kQsRevamp,
+                                              IsQsRevampEnabled());
+
     AshTestBase::SetUp();
 
     // Ensure media tray is not pinned to shelf so that media controls
@@ -40,25 +47,65 @@ class UnifiedMediaControlsContainerTest : public AshTestBase {
         ->unified_view();
   }
 
-  UnifiedMediaControlsContainer* media_controls_container() {
-    return system_tray_view()->media_controls_container_for_testing();
+  QuickSettingsView* quick_settings_view() {
+    return StatusAreaWidgetTestHelper::GetStatusAreaWidget()
+        ->unified_system_tray()
+        ->bubble()
+        ->quick_settings_view();
   }
+
+  UnifiedMediaControlsContainer* media_controls_container() {
+    return features::IsQsRevampEnabled()
+               ? quick_settings_view()->media_controls_container_for_testing()
+               : system_tray_view()->media_controls_container_for_testing();
+  }
+
+  bool IsQsRevampEnabled() { return GetParam(); }
+
+  void ShowMediaControls() {
+    features::IsQsRevampEnabled() ? quick_settings_view()->ShowMediaControls()
+                                  : system_tray_view()->ShowMediaControls();
+  }
+
+  void ShowDetailedView() {
+    auto view = std::make_unique<views::View>();
+    features::IsQsRevampEnabled()
+        ? quick_settings_view()->SetDetailedView(std::move(view))
+        : system_tray_view()->SetDetailedView(std::move(view));
+  }
+
+  void ResetDetailedView() {
+    features::IsQsRevampEnabled() ? quick_settings_view()->ResetDetailedView()
+                                  : system_tray_view()->ResetDetailedView();
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-TEST_F(UnifiedMediaControlsContainerTest, DoNotShowControlsWhenInDetailedView) {
+INSTANTIATE_TEST_SUITE_P(All,
+                         UnifiedMediaControlsContainerTest,
+                         testing::Bool());
+
+TEST_P(UnifiedMediaControlsContainerTest, DoNotShowControlsWhenInDetailedView) {
   // Navigate to a dummy detailed view.
-  system_tray_view()->SetDetailedView(std::make_unique<views::View>());
+  ShowDetailedView();
 
   // Simulate media playing, container should still be hidden.
-  system_tray_view()->ShowMediaControls();
+  ShowMediaControls();
   EXPECT_FALSE(media_controls_container()->GetVisible());
 
   // Return back to main menu, now media controls should show.
-  system_tray_view()->ResetDetailedView();
+  ResetDetailedView();
   EXPECT_TRUE(media_controls_container()->GetVisible());
 }
 
-TEST_F(UnifiedMediaControlsContainerTest, HideControlsWhenSystemMenuCollapse) {
+TEST_P(UnifiedMediaControlsContainerTest, HideControlsWhenSystemMenuCollapse) {
+  // Quick settings is not collapsable.
+  if (features::IsQsRevampEnabled()) {
+    return;
+  }
+
   EXPECT_FALSE(media_controls_container()->GetVisible());
   system_tray_view()->SetExpandedAmount(0.0f);
 
@@ -71,9 +118,9 @@ TEST_F(UnifiedMediaControlsContainerTest, HideControlsWhenSystemMenuCollapse) {
   EXPECT_TRUE(media_controls_container()->GetVisible());
 }
 
-TEST_F(UnifiedMediaControlsContainerTest, ShowMediaControls) {
+TEST_P(UnifiedMediaControlsContainerTest, ShowMediaControls) {
   // Simulate media playing and media controls should show.
-  system_tray_view()->ShowMediaControls();
+  ShowMediaControls();
   EXPECT_TRUE(media_controls_container()->GetVisible());
 }
 
