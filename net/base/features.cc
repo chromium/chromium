d6a8a3069faecaf2b15531b40492f4bd0062ed36
David Benjamin <davidben@chromium.org>
2023-05-19 21:18:44 +0000

    Enable EncryptedClientHelloQuic by default
    This does *not* enable ECH by default as ECH for QUIC is additionally
    conditioned on the EncryptedClientHello flag. This just makes the
    existing ECH flag control both, to make managing the experiment easier.
    
    Although we haven't implemented the recovery flow on the QUIC side,
    I've confirmed the existing QUIC -> TCP fallback still works to
    recover things, so that's good enough to proceed with for now.
    
    Keeping the flag around for a little while in case of problems.
    
    Bug: 1287248
    Change-Id: Ib8cc9281a5a4e2e0533545270c317bd6211b2e86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4548561
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1146717}

--- net/base/features.cc
+++ net/base/features.cc
@@ -80,7 +80,7 @@ BASE_FEATURE(kEncryptedClientHello,
 
 BASE_FEATURE(kEncryptedClientHelloQuic,
              "EncryptedClientHelloQuic",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNetworkQualityEstimator,
              "NetworkQualityEstimator",
