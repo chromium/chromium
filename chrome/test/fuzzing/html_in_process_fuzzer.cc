d132edf06a5620d0efa3f06d2ea7e9a37a01de2b
Adrian Taylor <adetaylor@chromium.org>
2023-05-22 17:18:20 +0000

    Correctly transport payload to fuzzer.
    A previous fix for a UaF meant we ended up with zero-length HTML
    payloads.
    
    Bug: 1445542
    Change-Id: I64849ba82912a7851aead28a41da14b9f7a7334a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545865
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147305}

--- chrome/test/fuzzing/html_in_process_fuzzer.cc
+++ chrome/test/fuzzing/html_in_process_fuzzer.cc
@@ -1,6 +1,8 @@
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#include "base/functional/bind.h"
+#include "base/memory/weak_ptr.h"
 #include "base/test/bind.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/fuzzing/in_process_fuzzer.h"
@@ -21,11 +23,12 @@ class HtmlInProcessFuzzer : virtual public InProcessFuzzer {
   void SetUpOnMainThread() override;
   int Fuzz(const uint8_t* data, size_t size) override;
   static std::unique_ptr<net::test_server::HttpResponse> HandleHTTPRequest(
-      std::string response_body,
+      base::WeakPtr<HtmlInProcessFuzzer> fuzzer_weak,
       const net::test_server::HttpRequest& request);
 
   net::EmbeddedTestServer https_test_server_;
   std::string current_fuzz_case_;
+  base::WeakPtrFactory<HtmlInProcessFuzzer> weak_ptr_factory_{this};
 };
 
 REGISTER_IN_PROCESS_FUZZER(HtmlInProcessFuzzer)
@@ -35,17 +38,34 @@ void HtmlInProcessFuzzer::SetUpOnMainThread() {
   host_resolver()->AddRule("*", "127.0.0.1");
   https_test_server_.SetSSLConfig(net::EmbeddedTestServer::CERT_OK);
   https_test_server_.RegisterRequestHandler(base::BindRepeating(
-      &HtmlInProcessFuzzer::HandleHTTPRequest, current_fuzz_case_));
+      &HtmlInProcessFuzzer::HandleHTTPRequest, weak_ptr_factory_.GetWeakPtr()));
   ASSERT_TRUE(https_test_server_.Start());
 }
 
 std::unique_ptr<net::test_server::HttpResponse>
 HtmlInProcessFuzzer::HandleHTTPRequest(
-    std::string response_body,
+    base::WeakPtr<HtmlInProcessFuzzer> fuzzer_weak,
     const net::test_server::HttpRequest& request) {
   std::unique_ptr<net::test_server::BasicHttpResponse> response;
   response = std::make_unique<net::test_server::BasicHttpResponse>();
   response->set_content_type("text/html");
+  std::string response_body = "";
+  // We are running on the embedded test server's thread.
+  // We want to ask the fuzzer thread for the latest HTML payload,
+  // but there's a risk of UaF if it's being destroyed.
+  // We use a weak pointer, but we have to dereference that on the originating
+  // thread.
+  base::RunLoop run_loop;
+  base::RepeatingCallback<void()> get_payload_lambda =
+      base::BindLambdaForTesting([&]() {
+        HtmlInProcessFuzzer* fuzzer = fuzzer_weak.get();
+        if (fuzzer) {
+          response_body = fuzzer->current_fuzz_case_;
+        }
+        run_loop.Quit();
+      });
+  content::GetUIThreadTaskRunner()->PostTask(FROM_HERE, get_payload_lambda);
+  run_loop.Run();
   response->set_content(response_body);
   response->set_code(net::HTTP_OK);
   return response;
@@ -60,7 +80,7 @@ int HtmlInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
   base::RepeatingCallback<void()> run_fuzz_case_lambda =
       base::BindLambdaForTesting([&]() {
         base::IgnoreResult(ui_test_utils::NavigateToURL(browser(), test_url));
-        run_loop.QuitClosure().Run();
+        run_loop.Quit();
       });
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, run_fuzz_case_lambda);
