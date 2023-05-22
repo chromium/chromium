578ba8bb291a2c3c9f9f1f44c15eecf4fb1413f1
Thiago Perrotta <tperrotta@chromium.org>
2023-05-22 18:00:23 +0200

    preload script storage: write unit tests (#755)
    * preload script storage: write unit tests
    
    * add a unit test for remove cdp preload script
    
    Bug: #293

--- third_party/chromium-bidi/src/bidiMapper/domains/context/PreloadScriptStorage.spec.ts
+++ third_party/chromium-bidi/src/bidiMapper/domains/context/PreloadScriptStorage.spec.ts
@@ -17,14 +17,52 @@
 import {expect} from 'chai';
 import sinon from 'sinon';
 
-import {PreloadScriptStorage} from './PreloadScriptStorage';
+import * as uuid from '../../../utils/uuid.js';
 
-// TODO: expand unit tests.
+import {CdpTarget} from './cdpTarget';
+import {PreloadScriptStorage, CdpPreloadScript} from './PreloadScriptStorage';
+
+const MOCKED_UUID_1 = '00000000-0000-0000-0000-00000000000a';
+const MOCKED_UUID_2 = '00000000-0000-0000-0000-00000000000b';
+
 describe('PreloadScriptStorage', () => {
   let preloadScriptStorage: PreloadScriptStorage;
 
+  let functionDeclaration: string;
+  let sandbox: string;
+
+  let uuidStub: sinon.SinonStub;
+
+  let cdpTarget: sinon.SinonStubbedInstance<CdpTarget>;
+  let cdpTargetId: string;
+  let cdpPreloadScript1: CdpPreloadScript;
+  let cdpPreloadScript2: CdpPreloadScript;
+
   beforeEach(() => {
     preloadScriptStorage = new PreloadScriptStorage();
+
+    functionDeclaration = '() => {}';
+    sandbox = 'MY_SANDBOX';
+    cdpTargetId = 'TARGET_ID';
+
+    uuidStub = sinon
+      .stub(uuid, 'uuidv4')
+      .onFirstCall()
+      .returns(MOCKED_UUID_1)
+      .onSecondCall()
+      .returns(MOCKED_UUID_2);
+
+    cdpTarget = sinon.createStubInstance(CdpTarget);
+    sinon.stub(cdpTarget, 'targetId').get(() => cdpTargetId);
+
+    cdpPreloadScript1 = {
+      target: cdpTarget,
+      preloadScriptId: 'PRELOAD_SCRIPT_1',
+    };
+    cdpPreloadScript2 = {
+      target: cdpTarget,
+      preloadScriptId: 'PRELOAD_SCRIPT_2',
+    };
   });
 
   it('initial state', () => {
@@ -45,6 +83,151 @@ describe('PreloadScriptStorage', () => {
         contextId: '',
       })
     ).to.be.empty;
+  });
+
+  [
+    {contextDescription: 'global context', context: null},
+    {contextDescription: 'non-global context', context: 'CONTEXT_1'},
+  ].forEach(({contextDescription, context}) => {
+    it(`add preload scripts in ${contextDescription}`, () => {
+      preloadScriptStorage.addPreloadScripts(
+        context,
+        [cdpPreloadScript1, cdpPreloadScript2],
+        functionDeclaration,
+        sandbox
+      );
+
+      expect(
+        preloadScriptStorage.findPreloadScripts({contextId: context})
+      ).to.deep.equal([
+        {
+          id: MOCKED_UUID_1,
+          cdpPreloadScripts: [cdpPreloadScript1, cdpPreloadScript2],
+          contextId: context,
+          functionDeclaration,
+          sandbox,
+        },
+      ]);
+
+      expect(uuidStub.calledOnceWithExactly()).to.be.true;
+    });
+
+    it(`append cdp preload script in ${contextDescription}`, () => {
+      preloadScriptStorage.addPreloadScripts(
+        context,
+        [cdpPreloadScript1],
+        functionDeclaration,
+        sandbox
+      );
+
+      const preloadScript = preloadScriptStorage.findPreloadScripts({
+        contextId: context,
+      })[0]!;
+
+      preloadScriptStorage.appendCdpPreloadScript(
+        preloadScript,
+        cdpPreloadScript2
+      );
+
+      expect(
+        preloadScriptStorage.findPreloadScripts({contextId: context})
+      ).to.be.deep.equal([
+        {
+          id: MOCKED_UUID_1,
+          cdpPreloadScripts: [cdpPreloadScript1, cdpPreloadScript2],
+          contextId: context,
+          functionDeclaration,
+          sandbox,
+        },
+      ]);
+    });
+
+    it(`remove cdp preload script in ${contextDescription}`, () => {
+      preloadScriptStorage.addPreloadScripts(
+        context,
+        [cdpPreloadScript1],
+        functionDeclaration,
+        sandbox
+      );
+
+      preloadScriptStorage.removeCdpPreloadScripts({
+        targetId: cdpTargetId,
+      });
+
+      expect(
+        preloadScriptStorage.findPreloadScripts({contextId: context})
+      ).to.be.deep.equal([
+        {
+          id: MOCKED_UUID_1,
+          cdpPreloadScripts: [],
+          functionDeclaration,
+          sandbox,
+          contextId: context,
+        },
+      ]);
+    });
+
+    it(`remove non-existing BiDi id in ${contextDescription}`, () => {
+      preloadScriptStorage.addPreloadScripts(
+        context,
+        [cdpPreloadScript1, cdpPreloadScript2],
+        functionDeclaration,
+        sandbox
+      );
+
+      const preloadScripts = preloadScriptStorage.findPreloadScripts({
+        contextId: context,
+      });
+
+      preloadScriptStorage.removeBiDiPreloadScripts({
+        contextId: context,
+        id: `${MOCKED_UUID_1}_NON_EXISTING`,
+      });
+
+      expect(
+        preloadScriptStorage.findPreloadScripts({contextId: context})
+      ).to.be.deep.equal(preloadScripts);
+    });
+
+    [
+      {filterDescription: 'context id', filter: {contextId: null}},
+      {filterDescription: 'context ids', filter: {contextIds: [null]}},
+      {
+        filterDescription: 'bidi id',
+        filter: {id: MOCKED_UUID_1},
+      },
+    ].forEach(({filterDescription, filter}) => {
+      it(`find preload scripts in ${contextDescription} by ${filterDescription}`, () => {
+        preloadScriptStorage.addPreloadScripts(
+          null,
+          [cdpPreloadScript1],
+          functionDeclaration,
+          sandbox
+        );
+
+        expect(preloadScriptStorage.findPreloadScripts(filter)).to.deep.equal([
+          {
+            id: MOCKED_UUID_1,
+            cdpPreloadScripts: [cdpPreloadScript1],
+            contextId: null,
+            functionDeclaration,
+            sandbox,
+          },
+        ]);
+      });
+
+      it(`remove preload scripts by in ${contextDescription} by ${filterDescription}`, () => {
+        preloadScriptStorage.addPreloadScripts(
+          null,
+          [cdpPreloadScript1],
+          functionDeclaration,
+          sandbox
+        );
+        preloadScriptStorage.removeBiDiPreloadScripts(filter);
+
+        expect(preloadScriptStorage.findPreloadScripts(filter)).to.be.empty;
+      });
+    });
   });
 
   afterEach(() => {
