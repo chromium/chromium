1de1716743bfa3b7ce84b2876980a04892e3b1f0
Pavol Marko <pmarko@google.com>
2023-05-22 23:59:51 +0000

    Make ProfileNetworkContextService depend on NssService
    On platforms that use NSS, make ProfileNetworkContextService depend on
    NssService so that NSS initialization is triggered when a profile
    NetworkContext is being created.
    
    Bug: b/197065597
    Change-Id: I6cfc6277b9bec88bbb65cec05928ea68eaf3af96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4386395
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147533}

--- chrome/browser/net/profile_network_context_service_factory.cc
+++ chrome/browser/net/profile_network_context_service_factory.cc
@@ -10,6 +10,11 @@
 #include "chrome/browser/net/profile_network_context_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "crypto/crypto_buildflags.h"
+
+#if BUILDFLAG(USE_NSS_CERTS)
+#include "chrome/browser/net/nss_service_factory.h"
+#endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/certificate_provider/certificate_provider_service_factory.h"
@@ -37,6 +42,13 @@ ProfileNetworkContextServiceFactory::ProfileNetworkContextServiceFactory()
               // Guest mode.
               .WithGuest(ProfileSelection::kOwnInstance)
               .Build()) {
+#if BUILDFLAG(USE_NSS_CERTS)
+  // On platforms that use NSS, NSS should be initialized when a
+  // ProfileNetworkContextService is created to ensure that NSS trust anchors
+  // are available and NSS can be used to enumerate client certificates if
+  // requested.
+  DependsOn(NssServiceFactory::GetInstance());
+#endif
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   DependsOn(chromeos::CertificateProviderServiceFactory::GetInstance());
 #endif
