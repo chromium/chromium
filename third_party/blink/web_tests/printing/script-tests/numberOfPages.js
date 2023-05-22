c0668e8a7c9388eec909934a4202841f22845375
Morten Stenshorne <mstensho@chromium.org>
2023-05-22 13:49:39 +0000

    Avoid hard-coded page height in paged-media-test-utils.js
    Had to update one test, which relied on the page height being
    hard-coded. Now that createBlockWithRatioToPageHeight() uses the actual
    page height (rather than 600 multiplied with magic factor), the
    expectation needs to change.
    
    Change-Id: I47a2521f7c1e178493c371400937f05c5dd4fd13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545870
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147196}

--- third_party/blink/web_tests/printing/script-tests/numberOfPages.js
+++ third_party/blink/web_tests/printing/script-tests/numberOfPages.js
@@ -6,7 +6,7 @@ function test()
     createBlockWithRatioToPageHeight("secondPage", 0.6);
 
     numberOfPagesShouldBe(2);
-    numberOfPagesShouldBe(1, 1000, 10000);
+    numberOfPagesShouldBe(2, 1000, 10000);
 
     document.body.removeChild(document.getElementById("sandbox"));
 }
