26337ea5dee934bc132d4f4cb1d295dc345a2c0f
Peter McNeeley <petermcneeley@chromium.org>
2023-05-22 20:27:21 +0000

    Add Chromeos version as shader prefix key for Lacros
    ChromeOS can update independently from Lacros. This means that Lacros
    can use previous shader blobs on what amounts to an entirely new OS
    revision. While driver version info should be enough to ensure blob
    binary compatibility this has been proven to be not the case. There
    are many components that contribute to blob compatibility and these
    are will not always result in different GPU info [1].
    
    Finally ChromeOS Ash will never have this issue as it is keyed on
    its own product number which will always change with each update.
    
    Old prefix example:
    
     Chrome/115.0.5767.0-Google Inc. (Intel Open Source Technology Center)-ANGLE (Intel Open Source Technology Center, Mesa DRI Intel(R) HD Graphics 615 (KBL GT2), OpenGL ES 3.2 Mesa 21.3.9 (git-14d7da2464))-21.3.9-Mesa
    
    New prefix example:
    
    Chrome/115.0.5767.0-Google Inc. (Intel Open Source Technology Center)-ANGLE (Intel Open Source Technology Center, Mesa DRI Intel(R) HD Graphics 615 (KBL GT2), OpenGL ES 3.2 Mesa 21.3.9 (git-14d7da2464))-21.3.9-Mesa-Linux 15437.12.0
    
    [1] An example of a change that may change a blob binary compatibility
    but does not change driver version:
    https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/4349475
    
    
    (cherry picked from commit c05896d5570a6112eb96b1b03f4af86104beba26)
    
    Bug: 1444684
    Change-Id: Ide4e8b59287c9d2de001f7f2d18069d48804b1b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4545164
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1146650}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552699
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5735@{#904}
    Cr-Branched-From: 2f562e4ddbaf79a3f3cb338b4d1bd4398d49eb67-refs/heads/main@{#1135570}

--- components/viz/host/gpu_host_impl.cc
+++ components/viz/host/gpu_host_impl.cc
@@ -33,6 +33,10 @@
 #include "base/android/build_info.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/system/sys_info.h"
+#endif
+
 #if BUILDFLAG(IS_WIN)
 #include "ui/gfx/win/rendering_window_manager.h"
 #elif BUILDFLAG(IS_MAC)
@@ -415,6 +419,13 @@ std::string GpuHostImpl::GetShaderPrefixKey() {
     std::string build_fp =
         base::android::BuildInfo::GetInstance()->android_build_fp();
     shader_prefix_key_ += "-" + build_fp;
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+    // ChromeOS can update independently of Lacros and the GPU driver
+    // information is not enough to ensure blob compatibility. See
+    // crbug.com/1444684
+    std::string chromeos_version = base::SysInfo::OperatingSystemName() + " " +
+                                   base::SysInfo::OperatingSystemVersion();
+    shader_prefix_key_ += "-" + chromeos_version;
 #endif
   }
 
