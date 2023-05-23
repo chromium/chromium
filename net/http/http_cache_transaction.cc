3172ea6a85089272db6fd175db3cfa294e900b56
Adam Rice <ricea@chromium.org>
2023-05-23 02:18:36 +0000

    DCHECK: Permit did_send_request & ENTRY_USED
    In HttpCache::Transaction::RecordHistograms() it was asserted that the
    CacheEntryStatus couldn't be ENTRY_USED when did_send_request was true.
    It turns out that this is not always the case.
    
    The DCHECK isn't protecting anything important, so permit this
    combination of states.
    
    BUG=1409150,1443598
    
    Change-Id: I75aa10b07ccdb473072826c1b14cb18e7c33d1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4550908
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147620}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -3946,19 +3946,17 @@ void HttpCache::Transaction::RecordHistograms() {
 
   bool did_send_request = !send_request_since_.is_null();
 
-  // TODO(ricea): Understand why this DCHECK is failing in the wild, fix it, and
-  // remove it. See https://crbug.com/1409150.
-  if (did_send_request) {
-    DCHECK_NE(cache_entry_status_, CacheEntryStatus::ENTRY_USED);
-  }
-  // This DCHECK() should not fire, because the one above should catch all the
-  // erroneous cases.
+  // It's not clear why `did_send_request` can be true when status is
+  // ENTRY_USED. See https://crbug.com/1409150.
+  // TODO(ricea): Maybe remove ENTRY_USED from the `did_send_request` true
+  // branch once that issue is resolved.
   DCHECK(
       (did_send_request &&
        (cache_entry_status_ == CacheEntryStatus::ENTRY_NOT_IN_CACHE ||
         cache_entry_status_ == CacheEntryStatus::ENTRY_VALIDATED ||
         cache_entry_status_ == CacheEntryStatus::ENTRY_UPDATED ||
-        cache_entry_status_ == CacheEntryStatus::ENTRY_CANT_CONDITIONALIZE)) ||
+        cache_entry_status_ == CacheEntryStatus::ENTRY_CANT_CONDITIONALIZE ||
+        cache_entry_status_ == CacheEntryStatus::ENTRY_USED)) ||
       (!did_send_request &&
        (cache_entry_status_ == CacheEntryStatus::ENTRY_USED ||
         cache_entry_status_ == CacheEntryStatus::ENTRY_CANT_CONDITIONALIZE)));
