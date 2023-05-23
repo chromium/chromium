c5954816214999b3b6bcb375402f4bf5ad1650cd
Duong Dac <ddac@chromium.org>
2023-05-23 13:43:29 +0000

    Enable HideNonDisplayableAccountEmail by default
    Bug: b/273712394
    Change-Id: I524dabe8d7882a9a48d97fe2c7eb1de74141d374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4551255
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147834}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -743,7 +743,7 @@ BASE_FEATURE(kGridTabSwitcherForTablets,
 
 BASE_FEATURE(kHideNonDisplayableAccountEmail,
              "HideNonDisplayableAccountEmail",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIncognitoReauthenticationForAndroid,
              "IncognitoReauthenticationForAndroid",
