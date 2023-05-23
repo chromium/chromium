d890f7b657d6007d52a389b246ab7831fd0851ba
Camillia Smith Barnes <cammie@chromium.org>
2023-05-23 02:11:09 +0000

    Shared Storage: Enable selectURL limits by default
    We enable the entropy bit budgets for `sharedStorage.selectURL()` by
    default. Of course, whether or not these limits are enabled is moot,
    if `sharedStorage` is itself disabled.
    
    Bug: 1429680,1218540
    Change-Id: Ide9464ddb8267367d2047eb780de6650fbcd6e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4552657
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1147619}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -352,7 +352,7 @@ const base::FeatureParam<int>
 
 BASE_FEATURE(kSharedStorageSelectURLLimit,
              "SharedStorageSelectURLLimit",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 const base::FeatureParam<int> kSharedStorageSelectURLBitBudgetPerPageLoad = {
     &kSharedStorageSelectURLLimit, "SharedStorageSelectURLBitBudgetPerPageLoad",
     12};
