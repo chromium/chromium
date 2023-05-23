373d344e49c3038e1846ad47fa33f9bc739eddba
Scott Yoder <scottyoder@google.com>
2023-05-23 21:30:11 +0000

    Revert "[iOS] Update field trial config and about_flags for CPE promo"
    This reverts commit b358145ae6be07bdde6d76d4158ea4c3263bd4e5.
    
    Reason for revert: This is causing EG tests to fail. Example:
    https://ci.chromium.org/ui/p/chrome/builders/ci/iphone-device/37154/overview
    
    Original change's description:
    > [iOS] Update field trial config and about_flags for CPE promo
    >
    > This change:
    > * Updates the testing_field_trial_config.json and about_flags
    >   to add a new experiment/feature flag param. This param will allow
    >   all of the promo's triggers to be enabled at once.
    >
    > * Checks for the CPE promo feature in the password manager tests.
    >   The addition to the fieldtrial config enables the copy password
    >   arm by default, which will cause the promo to appear in some
    >   tests.
    >
    > This CL will be followed by an update to the CPE promo finch config.
    >
    > Bug: None
    > Change-Id: I4c5b9a6f517f44ff0b977a401c1582f0cae27079
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547450
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1147895}
    
    Bug: None
    Change-Id: I6db01359b8fc5c23a85fbdb6a847d6dc926c2906
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4559205
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Owners-Override: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148181}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -625,11 +625,6 @@ const FeatureEntry::FeatureParam
 const FeatureEntry::FeatureParam
     kCredentialProviderExtensionPromoOnLoginWithAutofill[] = {
         {kCredentialProviderExtensionPromoOnLoginWithAutofillParam, "true"}};
-const FeatureEntry::FeatureParam
-    kCredentialProviderExtensionPromoOnAllTriggers[] = {
-        {kCredentialProviderExtensionPromoOnLoginWithAutofillParam, "true"},
-        {kCredentialProviderExtensionPromoOnPasswordCopiedParam, "true"},
-        {kCredentialProviderExtensionPromoOnPasswordSavedParam, "true"}};
 
 const FeatureEntry::FeatureVariation
     kCredentialProviderExtensionPromoVariations[] = {
@@ -642,8 +637,6 @@ const FeatureEntry::FeatureVariation
          kCredentialProviderExtensionPromoOnLoginWithAutofill,
          std::size(kCredentialProviderExtensionPromoOnLoginWithAutofill),
          nullptr},
-        {"On all triggers", kCredentialProviderExtensionPromoOnAllTriggers,
-         std::size(kCredentialProviderExtensionPromoOnAllTriggers), nullptr},
 };
 
 const FeatureEntry::FeatureParam kIOSEditMenuPartialTranslateNoIncognito[] = {
