c7a10c1bcf51f53f366ae2c9c2c42a2078fea1ec
Jeffrey Young <cowmoo@google.com>
2023-05-23 21:42:14 +0000

    personalization: correct loading error
    On smaller window sizes, navigating back and forth from
    wallpaper subpage would result in an indexing error. This
    was due to assumptions about the timing of loading the
    wallpaper collection list and each individual collection.
    Delay assigning `this.splitCollections_` so that the
    observer functions run in the expected order.
    
    Bug: b:282840764 b:280467959
    Test: browser_tests --gtest_filter=*WallpaperCollections*
    Change-Id: I7c04a3a0af4b52407d25de09a4ce5bdba0ba7ab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547912
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148193}

--- ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
+++ ash/webui/personalization_app/resources/js/wallpaper/wallpaper_collections_element.ts
@@ -426,7 +426,9 @@ export class WallpaperCollections extends WithPersonalizationStore {
       this.tiles_ = this.tiles_.filter(tile => !isTimeOfDay(tile));
     }
 
-    this.splitCollections_ = {
+    // Delay assigning `this.splitCollections_` until the correct number of
+    // tiles are assigned.
+    const splitCollections = {
       regular: collections.filter(collection => !isTimeOfDay(collection)),
       timeOfDay,
     };
@@ -435,7 +437,7 @@ export class WallpaperCollections extends WithPersonalizationStore {
     // of day, local images, and google photos.
     const firstBackdropIndex = this.getFirstRegularBackdropTileIndex();
     const desiredNumTiles =
-        this.splitCollections_.regular.length + firstBackdropIndex;
+        splitCollections.regular.length + firstBackdropIndex;
 
     // Adjust the number of loading tiles to match the collections that just
     // came in.  There may be more (or fewer) loading tiles than necessary to
@@ -456,6 +458,10 @@ export class WallpaperCollections extends WithPersonalizationStore {
     if (this.tiles_.length > desiredNumTiles) {
       this.splice('tiles_', desiredNumTiles);
     }
+
+    // Assign `this.splitCollections_` now that
+    // `tiles_.length === desiredNumTiles`.
+    this.splitCollections_ = splitCollections;
   }
 
   /**
