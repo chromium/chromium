7571534d84e9990811ef2fb7a79efa811a76cc43
Andrey Davydov <andreydav@google.com>
2023-05-23 14:06:11 +0000

    SAML username autofill: improve detection of URL parameter
    We have a protection against adding the same URL parameter repeatedly to
    IdP URL when trying to autofill the username. Turns out it was
    mistakenly triggered for some cases with Okta where their login page url
    mentioned `username` but not as a parameter for the current page. This
    resulted in the autofill feature not working. This cl fixes it.
    
    Bug: b/273991076
    Test: browser test GaiaAuthHostSamlUsernameAutofillTest
    Change-Id: Icc5297808bf2a5621b6beadf59d9e7b3b2dc7239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538706
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1147848}

--- chrome/browser/resources/gaia_auth_host/saml_username_autofill.js
+++ chrome/browser/resources/gaia_auth_host/saml_username_autofill.js
@@ -2,16 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// <if expr="chromeos_ash">
-import {appendParam} from 'chrome://resources/ash/common/util.js';
-// </if>
-// <if expr="not chromeos_ash">
-import {appendParam} from 'chrome://resources/js/util_ts.js';
-// </if>
-
 /**
  * Try to autofill email on login page for supported identity providers
- * @param {string} url Url of IdP login page
+ * @param {string} urlAsString Url of IdP login page
  * @param {?string} urlParameterNameToAutofillUsername Url parameter name
  *     which can be used to autofill the username field
  * @param {?string} email User's email which is to be used as a username on
@@ -20,23 +13,24 @@ import {appendParam} from 'chrome://resources/js/util_ts.js';
  *     null.
  */
 export function maybeAutofillUsername(
-    url, urlParameterNameToAutofillUsername, email) {
+    urlAsString, urlParameterNameToAutofillUsername, email) {
   if (!urlParameterNameToAutofillUsername ||
       urlParameterNameToAutofillUsername.length === 0) {
     return null;
   }
-  if (!url.startsWith('https')) {
+  const url = new URL(urlAsString);
+  if (url.protocol !== 'https:') {
     return null;
   }
   if (!email) {
     return null;
   }
-  // Don't do anything if url already contains
+  // Don't do anything if url already contains parameter with a name
   // `urlParameterNameToAutofillUsername`.
-  if (url.match(urlParameterNameToAutofillUsername)) {
+  if (url.searchParams.has(urlParameterNameToAutofillUsername)) {
     return null;
   }
 
-  url = appendParam(url, urlParameterNameToAutofillUsername, email);
-  return url;
+  url.searchParams.append(urlParameterNameToAutofillUsername, email);
+  return url.href;
 }
