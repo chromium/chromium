7fd9bb3f5d67c9cc5ed0173ddc273e3911fdfc5a
Nwokedi Idika <nwokedi@chromium.org>
2023-05-24 18:54:01 +0000

    Prep AddPageLoadTokenToClientSafeBrowsingReport for 100% Launch
    - Enables feature by default.
    - Removes field trial configuration.
    
    Bug: b/264288311
    Change-Id: I4007bc040a757c807f9facecbbb98596c122bcd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563514
    Auto-Submit: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148681}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -27,7 +27,7 @@ BASE_FEATURE(kAdSamplerTriggerFeature,
 
 BASE_FEATURE(kAddPageLoadTokenToClientSafeBrowsingReport,
              "AddPageLoadTokenToClientSafeBrowsingReport",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAntiPhishingTelemetry,
              "AntiPhishingTelemetry",
