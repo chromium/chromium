466e5dcc0c3f2f4fdd6bb6c7b7f6d4f8b274b1b9
Steve Kobes <skobes@chromium.org>
2023-05-24 20:48:02 +0000

    Enable MainRepaintScrollPrefersNewContent on trunk.
    Slight improvements in scroll latency and input delay are observed on
    Windows (go/mrspnc-finch).
    
    Bug: 1418368
    Change-Id: I1896c26befda6bae375af48b221f85302c6f729b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4500603
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148750}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -47,7 +47,7 @@ BASE_FEATURE(kScrollUnification,
 
 BASE_FEATURE(kMainRepaintScrollPrefersNewContent,
              "MainRepaintScrollPrefersNewContent",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFlushGpuAtDraw,
              "FlushGpuAtDraw",
