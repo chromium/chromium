f42d08c87e18507bc0a4bc8dab55c3590de998be
Bao-Duy Tran <tranbaoduy@chromium.org>
2023-05-25 08:05:42 +0000

    Remove bespoke feature names support in ImeService::IsFeatureEnabled().
    ImeService::IsFeatureEnabled() is the implementation of the
    corresponding "C" API of CrOS IME Service Container, for use by
    CrOS 1P IME shared lib. This ensures that the API is consistent
    and only considers official CrOS feature flag names used across
    the rest of CrOS, instead of inventing its own bespoke names.
    
    At this point (now that http://cl/532373120 has landed in CrOS),
    1P IME shared lib no longer invokes the API with these bespoke
    feature flag names.
    
    Bug: b:218815885
    Change-Id: Ib007a4769759216ad3ef7d8a97a5bf9e585e86c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4541900
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1148945}

--- chromeos/ash/services/ime/ime_service.cc
+++ chromeos/ash/services/ime/ime_service.cc
@@ -136,22 +136,14 @@ void ImeService::RunInMainSequence(ImeSequencedTask task, int task_id) {
   main_task_runner_->PostTask(FROM_HERE, base::BindOnce(task, task_id));
 }
 
-// TODO(b/218815885): Use consistent feature flag names as in CrOS
-// base::Feature::name (instead of slightly-different bespoke names), and always
-// wire 1:1 to CrOS feature flags (instead of having any extra logic).
+// NOTE: Use consistent feature flag names as in CrOS base::Feature::name
+// (instead of slightly-different bespoke names), and always wire 1:1 to CrOS
+// feature flags (instead of having any extra logic).
 bool ImeService::IsFeatureEnabled(const char* feature_name) {
-  // TODO(b/218815885): Replace refs of AssistiveEmojiEnhanced with
-  // AssistEmojiEnhanced in internal code for consistency.
-  // Then remove the AssistiveEmojiEnhanced check.
-  if (strcmp(feature_name, "AssistiveEmojiEnhanced") == 0 ||
-      strcmp(feature_name, features::kAssistEmojiEnhanced.name) == 0) {
+  if (strcmp(feature_name, features::kAssistEmojiEnhanced.name) == 0) {
     return base::FeatureList::IsEnabled(features::kAssistEmojiEnhanced);
   }
-  // TODO(b/218815885): Replace refs of AssistiveMultiWord with
-  // AssistMultiWord in internal code for consistency.
-  // Then remove the AssistiveMultiWord check.
-  if (strcmp(feature_name, "AssistiveMultiWord") == 0 ||
-      strcmp(feature_name, features::kAssistMultiWord.name) == 0) {
+  if (strcmp(feature_name, features::kAssistMultiWord.name) == 0) {
     return base::FeatureList::IsEnabled(features::kAssistMultiWord);
   }
   if (strcmp(feature_name, features::kAutocorrectParamsTuning.name) == 0) {
