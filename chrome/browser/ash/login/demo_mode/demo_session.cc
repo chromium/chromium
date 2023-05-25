9ce54a568e549bb0a36a82f1e94f3581c8fcb96b
Jackson Tadie <jacksontadie@google.com>
2023-05-25 19:08:58 +0000

    Centralize access to all Demo Mode dimension values
    Demo Mode dimensions are currently attached to UMA metrics and Omaha
    demo app component requests - soon they will also be included in certain DMServer requests. Let's centralize access to them to:
    * Make locating all of their usages easier
    * Abstract away internal representation
    * Enable tacking on extra functionality/checks when accessing these values
    
    Unfortunately we can't actually prevent code from accessing the underlying prefs/features directly, but we can at least encourage using these methods everywhere instead.
    
    Bug: b/281748020
    
    Change-Id: I2d6cc6283b14c5bc251a514d0065d1a5d3268523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4526741
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1149282}

--- chrome/browser/ash/login/demo_mode/demo_session.cc
+++ chrome/browser/ash/login/demo_mode/demo_session.cc
@@ -31,6 +31,7 @@
 #include "chrome/browser/apps/platform_apps/app_load_service.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/login/demo_mode/demo_components.h"
+#include "chrome/browser/ash/login/demo_mode/demo_mode_dimensions.h"
 #include "chrome/browser/ash/login/demo_mode/demo_setup_controller.h"
 #include "chrome/browser/ash/login/users/chrome_user_manager.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
@@ -553,8 +554,7 @@ void DemoSession::SetKeyboardBrightnessToOneHundredPercentFromCurrentLevel(
 }
 
 void DemoSession::RegisterDemoModeAAExperiment() {
-  if (g_browser_process->local_state()->GetString(prefs::kDemoModeCountry) ==
-      std::string("US")) {
+  if (demo_mode::Country() == std::string("US")) {
     // The hashing salt for the AA experiment.
     std::string demo_mode_aa_experiment_hashing_salt = "fae448044d545f9c";
 
@@ -563,13 +563,10 @@ void DemoSession::RegisterDemoModeAAExperiment() {
     std::vector<std::string>::iterator it;
 
     it = std::find(best_buy_retailer_names.begin(),
-                   best_buy_retailer_names.end(),
-                   g_browser_process->local_state()->GetString(
-                       prefs::kDemoModeRetailerId));
+                   best_buy_retailer_names.end(), demo_mode::RetailerName());
     if (it != best_buy_retailer_names.end()) {
       std::string store_number_and_hash_salt =
-          g_browser_process->local_state()->GetString(prefs::kDemoModeStoreId) +
-          demo_mode_aa_experiment_hashing_salt;
+          demo_mode::StoreNumber() + demo_mode_aa_experiment_hashing_salt;
       std::string md5_store_number =
           base::MD5String(store_number_and_hash_salt);
 
