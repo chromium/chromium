7916be6bf8627b47b35361e6a51fceb55b7a756c
Leonid Baraz <lbaraz@chromium.org>
2023-05-25 02:32:35 +0000

    Implement leaky bucket rate limiter.
    Bug: b:279776541
    Change-Id: I21022b44ac5ad6483ff90fa34d72d3da1c144442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564476
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1148884}

--- components/reporting/util/rate_limiter_slide_window.cc
+++ components/reporting/util/rate_limiter_slide_window.cc
@@ -10,6 +10,7 @@
 #include "base/check.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 
 namespace reporting {
