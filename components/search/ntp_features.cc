6911ccbe2b23237faf5252ee579112d3d18d308f
Roman Arora <romanarora@chromium.org>
2023-05-25 20:39:22 +0000

    Revert "[ntp-modules] Scrollbar on overflow disabled by default"
    This reverts commit 3bb671d6b04eccf7b9f607261a9fb5136f8a0316.
    
    Reason for revert: The feature's UX/behavior is approved by leads, considered acceptable.
    
    Original change's description:
    > [ntp-modules] Scrollbar on overflow disabled by default
    >
    > Disabling the new scroll on overflow feature as it has some uncanny
    > effects on MacOS, where the content appears as if clipped and there is
    > no clear indication to the user that the module content is scrollable.
    >
    > Bug: 1429457,1447960
    > Change-Id: Iecc8280d95edb1d9dc75f529144e5222af906386
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4562528
    > Commit-Queue: Roman Arora <romanarora@chromium.org>
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1148646}
    
    Bug: 1429457,1447960
    Change-Id: I9d57019d39ee9701b0e4091cca7674566f23b8be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568744
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149340}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -151,7 +151,7 @@ BASE_FEATURE(kNtpModulesOrder,
 // If true, displays a horizontal scrollbar on overflowing modules.
 BASE_FEATURE(kNtpModulesOverflowScrollbar,
              "NtpModulesOverflowScrollbar",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, modules will be able to be reordered via dragging and dropping
 BASE_FEATURE(kNtpModulesDragAndDrop,
