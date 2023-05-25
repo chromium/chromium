7964ef37b81822a1f139a7674748141716d4a9ce
Ho Cheung <uioptt24@gmail.com>
2023-05-25 20:07:44 +0000

    Manual raw_ptr rewrite: //net
    Bug: 1446392
    Change-Id: I48a83ff9e133cfb17e2d2616fe5961f5374fd207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561049
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Auto-Submit: Ho Cheung <uioptt24@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1149320}

--- net/test/embedded_test_server/embedded_test_server.h
+++ net/test/embedded_test_server/embedded_test_server.h
@@ -15,7 +15,6 @@
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string_piece.h"
@@ -70,9 +69,7 @@ class EmbeddedTestServerHandle {
   friend class EmbeddedTestServer;
 
   explicit EmbeddedTestServerHandle(EmbeddedTestServer* test_server);
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #constexpr-ctor-field-initializer
-  RAW_PTR_EXCLUSION EmbeddedTestServer* test_server_ = nullptr;
+  raw_ptr<EmbeddedTestServer> test_server_ = nullptr;
 };
 
 // Class providing an HTTP server for testing purpose. This is a basic server
