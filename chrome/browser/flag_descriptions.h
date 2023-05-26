86f8ba8b8b3d310487a87a55c1c495d896a6cc80
mcrouse <mcrouse@google.com>
2023-05-26 23:21:50 +0000

    Reland "[CSC] Create chrome flags for Companion UXR."
    This reverts commit d90aaa6b3b7ae492ee25c84f7119a3c781517f57.
    
    Reason for revert: Fixing the enums generation issue. 
    
    Original change's description:
    > Revert "[CSC] Create chrome flags for Companion UXR."
    >
    > This reverts commit 8b1641ff7a22d69b668ab256cf48c57edb9391df.
    >
    > Reason for revert: Failing AboutFlagsHistogram on linux-chromeos-chrome.
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/33089/overview
    > PTAL: https://chromium.googlesource.com/chromium/src.git/+/HEAD/tools/metrics/histograms/README.md#flag-histograms
    >
    > Original change's description:
    > > [CSC] Create chrome flags for Companion UXR.
    > >
    > > This requires us to be able to explicitly control the clobber vs new
    > > tab behavior. We also need to be able to control the pin AND ignore
    > > the labs experiment state (e.g., when unpinned and labs is on,
    > > the UI will attempt to force the state to pinned, this overrides
    > > that behavior).
    > >
    > > Bug: b:283109243
    > > Change-Id: I981ad573ee38cb4b69bb39f008c134c4e14bbf56
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4553173
    > > Reviewed-by: Caroline Rising <corising@chromium.org>
    > > Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    > > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1148554}
    >
    > Bug: b:283109243
    > Change-Id: Ic0fcdcc8512a5e38ffbaef7b5492c766b1961294
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564057
    > Owners-Override: Ana Salazar Maldonado <anasalazar@google.com>
    > Reviewed-by: Ana Salazar Maldonado <anasalazar@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Ana Salazar Maldonado <anasalazar@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1148779}
    
    Bug: b:283109243
    Change-Id: I1785c9de3aee67a87a19ce51cc8591fa99f7f40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569767
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149978}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1104,6 +1104,9 @@ extern const char kEnableLensPingDescription[];
 extern const char kCscName[];
 extern const char kCscDescription[];
 
+extern const char kCscPinnedName[];
+extern const char kCscPinnedDescription[];
+
 extern const char kCscVssName[];
 extern const char kCscVssDescription[];
 
