acb84a2bf36b5524702f1f29053ddeb0e0a0de32
Salma Elmahallawy <esalma@google.com>
2023-05-26 22:10:49 +0000

    [M115] Enabling chrome://policy/logs by default on Android
    (cherry picked from commit 2bb10f04c0171203ae257b7f89b5681d22c0a5b1)
    
    Bug: 1449233
    Change-Id: I4d71f63767a75e0d1fe40bf389bfeb3ef95f5a45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4564053
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1149106}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571359
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5790@{#82}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -25,7 +25,7 @@ BASE_FEATURE(kListPoliciesAcceptCommaSeparatedStringsAndroid,
 
 BASE_FEATURE(kPolicyLogsPageAndroid,
              "PolicyLogsPageAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSafeSitesFilterBehaviorPolicyAndroid,
              "SafeSitesFilterBehaviorPolicyAndroid",
