76c169d6dd484ef1482b3d1faec7d9dff55ccc1f
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2023-05-26 07:14:42 +0000

    Fix handling of compiler flags with parameters
    `flag_index` was undefined in ProcessIndividualFlag() after the function
    had been split off from GetClangOptionsFromCommandLine().
    
    Change-Id: Ibe26b649b916af14db52197cda61e5ec1ca112e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4547703
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1149575}

--- tools/vim/chromium.ycm_extra_conf.py
+++ tools/vim/chromium.ycm_extra_conf.py
@@ -236,7 +236,8 @@ def GetClangCommandLineFromNinjaForSource(out_dir, filename):
       return command_line
   return None
 
-def ProcessIndividualFlag(flag, out_dir):
+
+def ProcessIndividualFlag(flag, next_token, out_dir):
   def abspath(path):
     return os.path.normpath(os.path.join(out_dir, path))
 
@@ -260,8 +261,8 @@ def ProcessIndividualFlag(flag, out_dir):
       return None
     return flag
   elif flag == '-isysroot' or flag == '-isystem' or flag == '-I':
-    if flag_index + 1 < len(clang_tokens):
-      return flag.append(abspath(clang_tokens[flag_index + 1]))
+    if next_token:
+      return flag + abspath(next_token)
   elif flag.startswith('--sysroot='):
     # On Linux we use a sysroot image.
     sysroot_path = flag.lstrip('--sysroot=')
@@ -290,7 +291,10 @@ def GetClangOptionsFromCommandLine(clang_commandline, out_dir,
   # Parse flags that are important for YCM's purposes.
   clang_tokens = shlex.split(clang_commandline)
   for flag_index, flag in enumerate(clang_tokens):
-      clang_flags.append(ProcessIndividualFlag(flag, out_dir))
+    next_token = clang_tokens[flag_index + 1] \
+        if flag_index + 1 < len(clang_tokens) \
+        else None
+    clang_flags.append(ProcessIndividualFlag(flag, next_token, out_dir))
   return clang_flags
 
 def FileCompilationCandidates(filename):
@@ -378,11 +382,11 @@ def GetClangOptionsFromDBForFilename(chrome_root, filename):
   for candidate in FileCompilationCandidates(filename):
     compilation_info = database.GetCompilationInfoForFile(candidate)
     if compilation_info:
-        # ycm_core returns a StringVector we need to convert it.
-        flags = [] + additional_flags
-        for flag in compilation_info.compiler_flags_:
-            flags.append(ProcessIndividualFlag(flag, compilation_database_folder))
-        return flags
+      # ycm_core returns a StringVector we need to convert it.
+      flags = [] + additional_flags
+      for flag in compilation_info.compiler_flags_:
+        flags.append(ProcessIndividualFlag(flag, compilation_database_folder))
+      return flags
   return None
 
 # FlagsForFile entrypoint is deprecated in YCM and has replaced by
