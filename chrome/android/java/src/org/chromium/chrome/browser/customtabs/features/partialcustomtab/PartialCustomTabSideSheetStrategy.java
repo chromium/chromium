670de7a13924babf106a154edc4100a67e73eba5
Zach Katz <katzz@google.com>
2023-05-30 23:17:14 +0000

    [PCCT-SideSheet] No shadow margins during minimization
    Bug: 1449946, 1434222
    Change-Id: I79eaed742cea5f8e274dec50107f726b4201f0c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569101
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Zach Katz <katzz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150942}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
@@ -162,9 +162,6 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
         if (mIsMaximized) {
             if (shouldDrawDividerLine()) resetCoordinatorLayoutInsets();
             setTopMargins(0, 0);
-        } else {
-            if (shouldDrawDividerLine()) drawDividerLine();
-            updateShadowOffset();
         }
 
         AnimatorUpdateListener updateListener;
@@ -219,6 +216,8 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
                     maybeResetTalkbackFocus();
                 }
                 initializeSize();
+                if (shouldDrawDividerLine()) drawDividerLine();
+                updateShadowOffset();
                 maybeInvokeResizeCallback();
             });
         }
