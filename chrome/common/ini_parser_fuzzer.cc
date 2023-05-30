db94fa681537f7fe4ee3d767e20413b9b7228828
xrosado <xrosado@google.com>
2023-05-30 21:14:56 +0000

    Small fuzzer for INIParser
    This fuzzer uses libfuzzer's capabilities to test the INIParser class
    found in 'ini_parser.cc'. Purpose of this fuzzer is mainly for studying
    fuzzer design while testing more parts of chromium.
    
    Change-Id: I29bb833a661655360f4c1f82821f39ef32284fc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4557405
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Xavier Rosado <xrosado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150861}

---
+++ chrome/common/ini_parser_fuzzer.cc
@@ -0,0 +1,15 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include <stddef.h>
+#include <stdint.h>
+#include <vector>
+#include "chrome/common/ini_parser.h"
+
+// Uses already existing DictionaryValueINIParser
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  DictionaryValueINIParser target = DictionaryValueINIParser();
+  const std::string& input = reinterpret_cast<const std::string&>(data);
+  target.Parse(input);
+  return 0;
+}
