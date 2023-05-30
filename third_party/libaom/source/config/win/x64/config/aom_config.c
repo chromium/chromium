ad9e3134166b23e6ebec4277d2e05f83717e1a41
Wan-Teh Chang <wtc@google.com>
2023-05-30 22:40:36 +0000

    Roll src/third_party/libaom/source/libaom/ 0a770ff99..d954a78fd (23 commits)
    https://aomedia.googlesource.com/aom.git/+log/0a770ff99dfb..d954a78fdae8
    
    $ git log 0a770ff99..d954a78fd --date=short --no-merges --format='%ad %ae %s'
    2023-05-26 deepa.kg Avoid redundant start MV SAD calculation
    2023-05-25 cherma.rajan Combine sfs related to txfm_rd_gate_level
    2023-05-26 remya.prakasan Fix update of skip_build_pred flag in interp filter search
    2023-05-25 jzern av1_resize_and_extend_frame_neon: fix alloc check
    2023-05-23 george.steed Add highbd_h_predictor Neon implementation and tests
    2023-05-25 marpan rtc: Add test coverage for row_tiles
    2023-05-24 salome.thirot Add Neon implementation of aom_highbd_minmax_8x8
    2023-05-22 salome.thirot Add Neon implementation of aom_highbd_avg functions
    2023-05-22 jzern aom_config.c.template: use current year in Copyright
    2023-05-23 marpan rtc-screen: Incorporate color into rd cost for IDTX
    2023-05-22 jzern namespace ARCH_* defines
    2023-05-24 narayan.kalaburgi Extend sf skip_fullpel_search_using_startmv to cpu4
    2023-05-19 konma [NEON] Optimize highbd weighted convolution functions
    2023-05-11 konma [NEON] Optimize av1_highbd_convolve_2d_scale_neon().
    2023-05-24 deepa.kg Fix compilation error for tune vmaf
    2023-05-24 diksha.singh Improve documentation for MT support for Loop Filter
    2023-05-17 sanampudi.venkatarao Introduce adjust_num_frames_for_arf_filtering sf for speed 6
    2023-05-17 jianj RTC RC: Support cdef level
    2023-04-21 deepa.kg Extend downsample SAD computation for lowres, midres
    2023-05-01 narayan.kalaburgi Extend sf skip_fullpel_search_using_startmv to cpu5
    2023-05-21 marpan rtc: Reduce thresholds for palette mode in nonrd
    2023-05-22 jzern third_party/libyuv: remove unused asm files
    2023-05-19 wtc Removed unused Neon function load_u8()
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Change-Id: I8c3326efcc881c1e5d36c75897b8bc43ceab4667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575404
    Reviewed-by: James Zern <jzern@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150918}

--- third_party/libaom/source/config/win/x64/config/aom_config.c
+++ third_party/libaom/source/config/win/x64/config/aom_config.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2023, Alliance for Open Media. All rights reserved
  *
  * This source code is subject to the terms of the BSD 2 Clause License and
  * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
