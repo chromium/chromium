ffcfa80bda63cc8e76184a8ede4b551ef9ddd947
Ahmed Nasr <anasr@google.com>
2023-05-30 20:49:20 +0000

    Add network telemetry browser test.
    Bug: b:284971510
    Change-Id: If2da4f50b6c7c16aa67a5a32b5234b694e42035e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569087
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150839}

---
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler_browsertest.cc
@@ -0,0 +1,299 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <utility>
+
+#include "base/functional/bind.h"
+#include "base/run_loop.h"
+#include "base/time/time.h"
+#include "base/values.h"
+#include "chrome/browser/ash/login/test/cryptohome_mixin.h"
+#include "chrome/browser/ash/policy/affiliation/affiliation_mixin.h"
+#include "chrome/browser/ash/policy/affiliation/affiliation_test_helper.h"
+#include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
+#include "chrome/browser/chromeos/reporting/metric_default_utils.h"
+#include "chromeos/ash/components/network/network_handler_test_helper.h"
+#include "chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h"
+#include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
+#include "chromeos/dbus/missive/missive_client_test_observer.h"
+#include "components/policy/proto/chrome_device_policy.pb.h"
+#include "components/policy/proto/device_management_backend.pb.h"
+#include "components/reporting/proto/synced/metric_data.pb.h"
+#include "components/reporting/proto/synced/record.pb.h"
+#include "components/reporting/proto/synced/record_constants.pb.h"
+#include "components/reporting/util/mock_clock.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/test_launcher.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
+
+using enterprise_management::DeviceReportingProto;
+using enterprise_management::PolicyData;
+using testing::Eq;
+using testing::SizeIs;
+using testing::StrEq;
+
+namespace reporting {
+namespace {
+
+constexpr char kWifiDevicePath[] = "/device/wlan";
+constexpr char kWifiDeviceName[] = "reporting-wifi0";
+constexpr char kAccessPointAddress[] = "00:00:5e:00:53:af";
+constexpr bool kEncryptionOn = true;
+constexpr bool kPowerManagementOn = true;
+constexpr int64_t kTxBitRateMbps = 8;
+constexpr int64_t kRxBitRateMbps = 4;
+constexpr int64_t kTxPowerDbm = 2;
+constexpr int64_t kLinkQuality = 1;
+constexpr char kWifiGuid[] = "wifi-guid";
+constexpr int kSignalStrength = 80;
+constexpr int kSignalStrengthRssi = -50;
+constexpr char kIpAddress[] = "192.168.1.240";
+constexpr char kGateway[] = "192.168.1.1";
+constexpr char kIPConfigPath[] = "ip/config/path";
+
+bool IsNetworkTelemetry(const Record& record) {
+  MetricData record_data;
+  return record.destination() == Destination::TELEMETRY_METRIC &&
+         record_data.ParseFromString(record.data()) &&
+         !record_data.telemetry_data()
+              .networks_telemetry()
+              .network_telemetry()
+              .empty();
+}
+
+void VerifyNetworkTelemetryData(const MetricData& metric_data) {
+  ASSERT_THAT(
+      metric_data.telemetry_data().networks_telemetry().network_telemetry(),
+      SizeIs(1));
+  const auto& network_telemetry =
+      metric_data.telemetry_data().networks_telemetry().network_telemetry(0);
+  EXPECT_THAT(network_telemetry.guid(), StrEq(kWifiGuid));
+  EXPECT_THAT(network_telemetry.type(), Eq(NetworkType::WIFI));
+  EXPECT_THAT(network_telemetry.connection_state(),
+              Eq(NetworkConnectionState::ONLINE));
+  EXPECT_THAT(network_telemetry.ip_address(), StrEq(kIpAddress));
+  EXPECT_THAT(network_telemetry.gateway(), StrEq(kGateway));
+  EXPECT_THAT(network_telemetry.signal_strength(), Eq(kSignalStrength));
+  EXPECT_THAT(network_telemetry.signal_strength_dbm(), Eq(kSignalStrengthRssi));
+  EXPECT_THAT(network_telemetry.tx_bit_rate_mbps(), Eq(kTxBitRateMbps));
+  EXPECT_THAT(network_telemetry.rx_bit_rate_mbps(), Eq(kRxBitRateMbps));
+  EXPECT_THAT(network_telemetry.link_quality(), Eq(kLinkQuality));
+  EXPECT_TRUE(network_telemetry.power_management_enabled());
+  EXPECT_TRUE(network_telemetry.encryption_on());
+}
+
+class NetworkTelemetrySamplerBrowserTest
+    : public ::policy::DevicePolicyCrosBrowserTest {
+ protected:
+  NetworkTelemetrySamplerBrowserTest() {
+    test::MockClock::Get();
+    crypto_home_mixin_.MarkUserAsExisting(affiliation_mixin_.account_id());
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ::policy::AffiliationTestHelper::AppendCommandLineSwitchesForLoginManager(
+        command_line);
+    ::policy::DevicePolicyCrosBrowserTest::SetUpCommandLine(command_line);
+  }
+
+  void SetUpOnMainThread() override {
+    ::policy::DevicePolicyCrosBrowserTest::SetUpOnMainThread();
+    if (::content::IsPreTest()) {
+      // Preliminary setup - set up affiliated user.
+      ::policy::AffiliationTestHelper::PreLoginUser(
+          affiliation_mixin_.account_id());
+      return;
+    }
+    SetupNetworks();
+    ::policy::AffiliationTestHelper::LoginUser(affiliation_mixin_.account_id());
+  }
+
+  void SetupNetworks() {
+    network_handler_test_helper_ =
+        std::make_unique<::ash::NetworkHandlerTestHelper>();
+    network_handler_test_helper_->AddDefaultProfiles();
+    network_handler_test_helper_->ResetDevicesAndServices();
+
+    network_handler_test_helper_->device_test()->AddDevice(
+        kWifiDevicePath, shill::kTypeWifi, kWifiDeviceName);
+    network_handler_test_helper_->device_test()->SetDeviceProperty(
+        kWifiDevicePath, shill::kInterfaceProperty,
+        base::Value(kWifiDeviceName),
+        /*notify_changed=*/true);
+    base::RunLoop().RunUntilIdle();
+
+    base::Value::Dict ip_config_properties;
+    ip_config_properties.Set(shill::kAddressProperty, kIpAddress);
+    ip_config_properties.Set(shill::kGatewayProperty, kGateway);
+    network_handler_test_helper_->ip_config_test()->AddIPConfig(
+        kIPConfigPath, std::move(ip_config_properties));
+
+    std::string service_config = base::StringPrintf(
+        R"({"GUID": "%s", "Type": "%s", "State": "%s", "Strength": %d,
+        "WiFi.SignalStrengthRssi": %d, "IPConfig": "%s"})",
+        kWifiGuid, shill::kTypeWifi, shill::kStateOnline, kSignalStrength,
+        kSignalStrengthRssi, kIPConfigPath);
+    const std::string service_path =
+        network_handler_test_helper_->ConfigureService(service_config);
+    ASSERT_FALSE(service_path.empty());
+    network_handler_test_helper_->service_test()->SetServiceProperty(
+        service_path, shill::kDeviceProperty, base::Value(kWifiDevicePath));
+    network_handler_test_helper_->service_test()->SetServiceProperty(
+        service_path, shill::kProfileProperty,
+        base::Value(network_handler_test_helper_->ProfilePathUser()));
+
+    SetWifiInterfaceData();
+  }
+
+  void SetWifiInterfaceData() {
+    auto telemetry_info = ::ash::cros_healthd::mojom::TelemetryInfo::New();
+    std::vector<::ash::cros_healthd::mojom::NetworkInterfaceInfoPtr>
+        network_interfaces;
+
+    auto wireless_link_info = ::ash::cros_healthd::mojom::WirelessLinkInfo::New(
+        kAccessPointAddress, kTxBitRateMbps, kRxBitRateMbps, kTxPowerDbm,
+        kEncryptionOn, kLinkQuality, kSignalStrengthRssi);
+    auto wireless_interface_info =
+        ::ash::cros_healthd::mojom::WirelessInterfaceInfo::New(
+            kWifiDeviceName, kPowerManagementOn, std::move(wireless_link_info));
+    network_interfaces.push_back(
+        ::ash::cros_healthd::mojom::NetworkInterfaceInfo::
+            NewWirelessInterfaceInfo(std::move(wireless_interface_info)));
+    auto network_interface_result =
+        ::ash::cros_healthd::mojom::NetworkInterfaceResult::
+            NewNetworkInterfaceInfo(std::move(network_interfaces));
+
+    telemetry_info->network_interface_result =
+        std::move(network_interface_result);
+    ::ash::cros_healthd::FakeCrosHealthd::Get()
+        ->SetProbeTelemetryInfoResponseForTesting(telemetry_info);
+  }
+
+  void SetReportNetworkStatusPolicy(bool enabled) {
+    device_reporting()->set_report_network_status(enabled);
+    policy_helper_.RefreshDevicePolicy();
+  }
+
+  void SetReportNetworkTelemetryCollectionRateMs(int64_t rate) {
+    device_reporting()->set_report_network_telemetry_collection_rate_ms(rate);
+    policy_helper_.RefreshDevicePolicy();
+  }
+
+  void Deprovision() {
+    policy_helper_.device_policy()->policy_data().set_state(
+        PolicyData::DEPROVISIONED);
+    policy_helper_.RefreshDevicePolicy();
+  }
+
+  DeviceReportingProto* device_reporting() {
+    return policy_helper_.device_policy()->payload().mutable_device_reporting();
+  }
+
+  std::unique_ptr<::ash::NetworkHandlerTestHelper> network_handler_test_helper_;
+  ::policy::DevicePolicyCrosTestHelper policy_helper_;
+  ::policy::AffiliationMixin affiliation_mixin_{&mixin_host_, &policy_helper_};
+  ::ash::CryptohomeMixin crypto_home_mixin_{&mixin_host_};
+};
+
+IN_PROC_BROWSER_TEST_F(NetworkTelemetrySamplerBrowserTest, PRE_Default) {
+  // Dummy case that sets up the affiliated user through SetUpOnMainThread
+  // PRE-condition.
+}
+
+IN_PROC_BROWSER_TEST_F(NetworkTelemetrySamplerBrowserTest, Default) {
+  ::chromeos::MissiveClientTestObserver missive_observer(
+      base::BindRepeating(&IsNetworkTelemetry));
+
+  {
+    // Initial collection, policy not set but default is true.
+    MetricData record_data;
+    test::MockClock::Get().Advance(metrics::kInitialCollectionDelay);
+    const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
+
+    EXPECT_THAT(priority, Eq(Priority::MANUAL_BATCH));
+    ASSERT_TRUE(record_data.ParseFromString(record.data()));
+    VerifyNetworkTelemetryData(record_data);
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+
+  {
+    // Collection rate policy not set, collect on default collection rate.
+    MetricData record_data;
+    test::MockClock::Get().Advance(
+        metrics::kDefaultNetworkTelemetryCollectionRate);
+    const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
+
+    EXPECT_THAT(priority, Eq(Priority::MANUAL_BATCH));
+    ASSERT_TRUE(record_data.ParseFromString(record.data()));
+    VerifyNetworkTelemetryData(record_data);
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+
+  SetReportNetworkStatusPolicy(false);
+  {
+    // Reporting is disabled, no network telemetry data should be collected.
+    MetricData record_data;
+    test::MockClock::Get().Advance(
+        metrics::kDefaultNetworkTelemetryCollectionRate);
+    base::RunLoop().RunUntilIdle();
+
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+
+  // Set collection rate policy to double the default rate.
+  base::TimeDelta collection_rate =
+      metrics::kDefaultNetworkTelemetryCollectionRate * 2;
+  SetReportNetworkTelemetryCollectionRateMs(collection_rate.InMilliseconds());
+
+  // Advance time by few hours before re-enabling the policy.
+  test::MockClock::Get().Advance(base::Hours(5));
+  SetReportNetworkStatusPolicy(true);
+  {
+    // Policy re-enabled, one immediate collection should take place.
+    MetricData record_data;
+    const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
+
+    EXPECT_THAT(priority, Eq(Priority::MANUAL_BATCH));
+    ASSERT_TRUE(record_data.ParseFromString(record.data()));
+    VerifyNetworkTelemetryData(record_data);
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+
+  {
+    // Collection rate policy set to double the default rate.
+    MetricData record_data;
+    test::MockClock::Get().Advance(
+        metrics::kDefaultNetworkTelemetryCollectionRate);
+    base::RunLoop().RunUntilIdle();
+
+    // No data collected, only half of time elapsed.
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+
+    // Advance the remaining time.
+    test::MockClock::Get().Advance(
+        collection_rate - metrics::kDefaultNetworkTelemetryCollectionRate);
+    const auto [priority, record] = missive_observer.GetNextEnqueuedRecord();
+
+    EXPECT_THAT(priority, Eq(Priority::MANUAL_BATCH));
+    ASSERT_TRUE(record_data.ParseFromString(record.data()));
+    VerifyNetworkTelemetryData(record_data);
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+
+  Deprovision();
+  {
+    // Device is deprovisioned, no network telemetry data should be collected.
+    MetricData record_data;
+    test::MockClock::Get().Advance(collection_rate);
+    base::RunLoop().RunUntilIdle();
+
+    ASSERT_FALSE(missive_observer.HasNewEnqueuedRecords());
+  }
+}
+
+}  // namespace
+}  // namespace reporting
