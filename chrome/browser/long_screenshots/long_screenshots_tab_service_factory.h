d96ad442cb83f10d7f11f0904e4da1a5c342ee47
Mikel Astiz <mastiz@chromium.org>
2023-05-30 08:07:55 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/long_screenshots
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with a trivial destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=victorvianna@google.com
    
    Bug: 925323
    Change-Id: I44b96291f716b73c2b1b709e613f8d3ecf210f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573383
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1150418}

--- chrome/browser/long_screenshots/long_screenshots_tab_service_factory.h
+++ chrome/browser/long_screenshots/long_screenshots_tab_service_factory.h
@@ -7,7 +7,7 @@
 
 #include <memory>
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/core/simple_keyed_service_factory.h"
 
@@ -30,7 +30,7 @@ class LongScreenshotsTabServiceFactory : public SimpleKeyedServiceFactory {
       const LongScreenshotsTabServiceFactory&) = delete;
 
  private:
-  friend struct base::DefaultSingletonTraits<LongScreenshotsTabServiceFactory>;
+  friend base::NoDestructor<LongScreenshotsTabServiceFactory>;
 
   LongScreenshotsTabServiceFactory();
   ~LongScreenshotsTabServiceFactory() override;
