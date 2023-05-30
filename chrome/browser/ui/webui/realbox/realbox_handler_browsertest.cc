8b3aac42deaa8b92ed5563faa9475f7aa2842956
Khalid Peer <khalidpeer@chromium.org>
2023-05-30 18:23:25 +0000

    [M115 merge][realbox] Fix NOTREACHED crashes due to icon mappings.
    In the course of updating suggestion vector icons in crrev/c/4535099
    as part of ChromeRefresh2023, the
    AutocompleteMatchVectorIconToResourceName function was not updated
    with the latest vector-icon-to-SVG mappings. As a result, a couple of
    DCHECK-enabled clients on Canary have been crashing with NOTREACHED
    errors.
    
    Due to a similar reason, the PedalVectorIconToResourceName function has
    also been triggering crashes for DCHECK-enabled clients on Canary.
    
    In order to remediate these crashes, this CL updates the
    vector-icon-to-SVG mappings to also work for the new ChromeRefresh2023
    vector icons.
    
    (cherry picked from commit 5eb06b54fb552c53b48b08b9d34dff85134792b3)
    
    Bug: 1448353, 1445439
    Change-Id: I153ff932dc88245e8c67a1daf4641b60ccfec596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4563838
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1149682}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574655
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5790@{#139}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- chrome/browser/ui/webui/realbox/realbox_handler_browsertest.cc
+++ chrome/browser/ui/webui/realbox/realbox_handler_browsertest.cc
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 #include <gtest/gtest.h>
 #include "base/check.h"
@@ -30,14 +31,23 @@
 #include "content/public/test/browser_test.h"
 #include "content/public/test/prerender_test_util.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/vector_icon_types.h"
 
 namespace {
 
-class BrowserTestWithParam : public InProcessBrowserTest,
-                             public testing::WithParamInterface<bool> {
+class BrowserTestWithParam
+    : public InProcessBrowserTest,
+      public testing::WithParamInterface<std::pair<bool, bool>> {
  public:
-  BrowserTestWithParam() = default;
+  BrowserTestWithParam() {
+    const bool is_cr23_enabled = GetParam().second;
+    if (is_cr23_enabled) {
+      scoped_feature_list_.InitAndEnableFeature(features::kChromeRefresh2023);
+    } else {
+      scoped_feature_list_.InitAndDisableFeature(features::kChromeRefresh2023);
+    }
+  }
   BrowserTestWithParam(const BrowserTestWithParam&) = delete;
   BrowserTestWithParam& operator=(const BrowserTestWithParam&) = delete;
   ~BrowserTestWithParam() override = default;
@@ -48,9 +58,14 @@ class BrowserTestWithParam : public InProcessBrowserTest,
 
 }  // namespace
 
-INSTANTIATE_TEST_SUITE_P(RealboxHandlerMatchIconTest,
+// Each value listed below represents the following:
+// {is_bookmark, is_cr23_enabled}
+INSTANTIATE_TEST_SUITE_P(RealboxHandlerIconTest,
                          BrowserTestWithParam,
-                         testing::Bool());
+                         testing::Values(std::pair<bool, bool>(true, true),
+                                         std::pair<bool, bool>(true, false),
+                                         std::pair<bool, bool>(false, true),
+                                         std::pair<bool, bool>(false, false)));
 
 // Tests that all Omnibox match vector icons map to an equivalent SVG for use in
 // the NTP Realbox.
@@ -59,7 +74,7 @@ IN_PROC_BROWSER_TEST_P(BrowserTestWithParam, MatchVectorIcons) {
        type != AutocompleteMatchType::NUM_TYPES; type++) {
     AutocompleteMatch match;
     match.type = static_cast<AutocompleteMatchType::Type>(type);
-    const bool is_bookmark = BrowserTestWithParam::GetParam();
+    const bool is_bookmark = BrowserTestWithParam::GetParam().first;
     const gfx::VectorIcon& vector_icon = match.GetVectorIcon(is_bookmark);
     const std::string& svg_name =
         RealboxHandler::AutocompleteMatchVectorIconToResourceName(vector_icon);
@@ -87,7 +102,7 @@ IN_PROC_BROWSER_TEST_P(BrowserTestWithParam, AnswerVectorIcons) {
     SuggestionAnswer answer;
     answer.set_type(answer_type);
     match.answer = answer;
-    const bool is_bookmark = BrowserTestWithParam::GetParam();
+    const bool is_bookmark = BrowserTestWithParam::GetParam().first;
     const gfx::VectorIcon& vector_icon = match.GetVectorIcon(is_bookmark);
     const std::string& svg_name =
         RealboxHandler::AutocompleteMatchVectorIconToResourceName(vector_icon);
@@ -100,11 +115,9 @@ IN_PROC_BROWSER_TEST_P(BrowserTestWithParam, AnswerVectorIcons) {
   }
 }
 
-using RealboxHandlerPedalIconTest = InProcessBrowserTest;
-
 // Tests that all Omnibox Pedal vector icons map to an equivalent SVG for use in
 // the NTP Realbox.
-IN_PROC_BROWSER_TEST_F(RealboxHandlerPedalIconTest, PedalVectorIcons) {
+IN_PROC_BROWSER_TEST_P(BrowserTestWithParam, PedalVectorIcons) {
   std::unordered_map<OmniboxPedalId, scoped_refptr<OmniboxPedal>> pedals =
       GetPedalImplementations(/*incognito=*/true, /*guest=*/false,
                               /*testing=*/true);
