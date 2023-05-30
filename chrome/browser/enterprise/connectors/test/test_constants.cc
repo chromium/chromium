7ea4282f5a00e19afd3c35b6f4725ddfcd187a0f
Sebastien Lalancette <seblalancette@chromium.org>
2023-05-30 15:16:58 +0000

    Create a Management Context Mixin
    This utility can be used to simplify the manipulation and set-up of
    cloud policies in browser tests.
    
    Bug: b:279060085
    Change-Id: Ie649d8376fc8b7a31012fefece6f01f3cf4ec472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4566832
    Reviewed-by: Leon Masopust <lmasopust@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1150573}

---
+++ chrome/browser/enterprise/connectors/test/test_constants.cc
@@ -0,0 +1,26 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/test/test_constants.h"
+
+namespace enterprise_connectors::test {
+
+const char kFakeCustomerId[] = "customer_id";
+const char kDifferentCustomerId[] = "other_customer_id";
+
+const char kTestUserId[] = "user_id";
+const char kTestUserEmail[] = "test@example.com";
+
+const char kProfileDmToken[] = "profile_dm_token";
+const char kProfileClientId[] = "profile_client_id";
+
+const char kBrowserDmToken[] = "browser_dm_token";
+const char kBrowserClientId[] = "browser_client_id";
+
+const char kDeviceDmToken[] = "device_dm_token";
+const char kDeviceClientId[] = "device_client_id";
+
+const char kEnrollmentToken[] = "enrollment_token";
+
+}  // namespace enterprise_connectors::test
