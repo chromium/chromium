3d172c9eb1718218dc06d9decb63d00d5a860a23
Allen Bauer <kylixrd@chromium.org>
2023-05-31 19:13:44 +0000

    [CR2023] Adjust price tracking view on bookmark bubble view.
    https://screenshot.googleplex.com/7FxrU5pE9zgkyHz
    
    Bug: 1422113
    Change-Id: I720a3d51813f44901db306933118a01c3746f147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568591
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151404}

--- ui/views/controls/button/toggle_button.cc
+++ ui/views/controls/button/toggle_button.cc
@@ -55,7 +55,6 @@ constexpr int kRefreshThumbInset = -4;
 constexpr int kRefreshThumbInsetSelected = -2;
 constexpr int kRefreshThumbPressedOutset = 1;
 constexpr int kRefreshHoverDiameter = 20;
-constexpr int kTrackHorizontalMarginChromeRefresh = 0;
 
 const gfx::Size GetTrackSize() {
   return features::IsChromeRefresh2023() ? kRefreshTrackSize : kTrackSize;
@@ -382,10 +381,9 @@ void ToggleButton::RemoveLayerFromRegions(ui::Layer* layer) {
 
 gfx::Size ToggleButton::CalculatePreferredSize() const {
   gfx::Rect rect(GetTrackSize());
-  rect.Inset(gfx::Insets::VH(-kTrackVerticalMargin,
-                             features::IsChromeRefresh2023()
-                                 ? kTrackHorizontalMarginChromeRefresh
-                                 : -kTrackHorizontalMargin));
+  if (!features::IsChromeRefresh2023()) {
+    rect.Inset(gfx::Insets::VH(-kTrackVerticalMargin, -kTrackHorizontalMargin));
+  }
   rect.Inset(-GetInsets());
   return rect.size();
 }
