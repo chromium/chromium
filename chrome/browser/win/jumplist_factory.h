b6552c4d896f8b38fad146543f070b1612aa429f
Mikel Astiz <mastiz@chromium.org>
2023-05-31 06:36:17 +0000

    Adopt base::NoDestructor for KeyedService factories
    For changes under /chrome/browser/win
    
    It replaces base::Singleton, following the latest recommendation
    in base/ and browser_context_keyed_service_factory.h.
    
    For factories with no logic in the destructor, it makes no difference.
    
    LSC doc:
    https://docs.google.com/document/d/1x1LqRQyfBOmpMkNQBYs7QBPSxLtuiImvmgcJYI_kaS4/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=grt@chromium.org
    
    Bug: 925323
    Change-Id: I366daa5e8734ab6b0a93f87ea8c2651dba53f3de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573534
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151089}

--- chrome/browser/win/jumplist_factory.h
+++ chrome/browser/win/jumplist_factory.h
@@ -5,7 +5,7 @@
 #ifndef CHROME_BROWSER_WIN_JUMPLIST_FACTORY_H_
 #define CHROME_BROWSER_WIN_JUMPLIST_FACTORY_H_
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 
 class Profile;
@@ -18,7 +18,7 @@ class JumpListFactory : public ProfileKeyedServiceFactory {
   static JumpListFactory* GetInstance();
 
  private:
-  friend struct base::DefaultSingletonTraits<JumpListFactory>;
+  friend base::NoDestructor<JumpListFactory>;
   JumpListFactory();
   ~JumpListFactory() override;
 
