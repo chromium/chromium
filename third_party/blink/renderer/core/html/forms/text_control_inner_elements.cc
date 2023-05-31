2837d1460870d3682751f6509ff7fc07c6935382
Luke Warlow <luke@warlow.dev>
2023-05-31 18:48:59 +0000

    Set text control inner element scrollbar-color to auto
    This prevents an issue where scrollbars are rendered inside an input
    control when scrollbar-color is in use.
    
    Web Platform Test included.
    
    Bug: 891944
    Change-Id: I1c312584479004e54ee51c5919573f4bddfd9937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575064
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151392}

--- third_party/blink/renderer/core/html/forms/text_control_inner_elements.cc
+++ third_party/blink/renderer/core/html/forms/text_control_inner_elements.cc
@@ -153,6 +153,7 @@ TextControlInnerEditorElement::CustomStyleForLayoutObject(
   style_builder.SetShouldIgnoreOverflowPropertyForInlineBlockBaseline();
 
   if (!IsA<HTMLTextAreaElement>(host)) {
+    style_builder.SetScrollbarColor(absl::nullopt);
     style_builder.SetWhiteSpace(EWhiteSpace::kPre);
     style_builder.SetOverflowWrap(EOverflowWrap::kNormal);
     style_builder.SetTextOverflow(ToTextControl(host)->ValueForTextOverflow());
