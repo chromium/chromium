cc74a5075bb5f87483e951f28a8c1bbf13f2c946
Elly Fong-Jones <ellyjones@chromium.org>
2023-05-31 16:58:22 +0000

    self-share: remove some obsolete histograms 1/n
    This change removes:
    * Sharing.MessageAckTime
    * Sharing.RemoteCopyWriteImageDetectionTime
    * Sharing.RemoteCopyWriteImageTime
    * Sharing.RemoteCopyWriteTextDetectionTime
    * Sharing.RemoteCopyWriteTextTime
    * Sharing.SendAckMessageResult
    
    OBSOLETE_HISTOGRAM[Sharing.MessageAckTime]=expired several months ago
    OBSOLETE_HISTOGRAM[Sharing.RemoteCopyWriteImageDetectionTime]=expired several months ago
    OBSOLETE_HISTOGRAM[Sharing.RemoteCopyWriteImageTime]=expired several months ago
    OBSOLETE_HISTOGRAM[Sharing.RemoteCopyWriteTextDetectionTime]=expired several months ago
    OBSOLETE_HISTOGRAM[Sharing.RemoteCopyWriteTextTime]=expired several months ago
    OBSOLETE_HISTOGRAM[Sharing.SendAckMessageResult]=expired several months ago
    
    Bug: 1447189
    Change-Id: I211623f1cee122b223ed7336a923b42db8797b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4571195
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151304}

--- chrome/browser/sharing/sharing_metrics.h
+++ chrome/browser/sharing/sharing_metrics.h
@@ -94,13 +94,6 @@ void LogSharingSelectedIndex(
     int index,
     SharingIndexType index_type = SharingIndexType::kDevice);
 
-// Logs to UMA the time from sending a FCM message from the Sharing service
-// until an ack message is received for it.
-void LogSharingMessageAckTime(chrome_browser_sharing::MessageType message_type,
-                              SharingDevicePlatform receiver_device_platform,
-                              SharingChannelType channel_type,
-                              base::TimeDelta time);
-
 // Logs to UMA the time from receiving a SharingMessage to sending
 // back an ack.
 void LogSharingMessageHandlerTime(
@@ -117,13 +110,6 @@ void LogSendSharingMessageResult(
     SharingDevicePlatform receiver_device_platform,
     SharingChannelType channel_type,
     base::TimeDelta receiver_pulse_interval,
-    SharingSendMessageResult result);
-
-// Logs to UMA result of sending an ack of a SharingMessage.
-void LogSendSharingAckMessageResult(
-    chrome_browser_sharing::MessageType message_type,
-    SharingDevicePlatform ack_receiver_device_type,
-    SharingChannelType channel_type,
     SharingSendMessageResult result);
 
 // Logs to UMA the size of the selected text for Shared Clipboard.
@@ -149,11 +135,5 @@ void LogRemoteCopyLoadImageTime(base::TimeDelta time);
 
 // Logs to UMA the time to decode an image for Remote Copy.
 void LogRemoteCopyDecodeImageTime(base::TimeDelta time);
-
-// Logs to UMA the duration of a clipboard write for Remote Copy.
-void LogRemoteCopyWriteTime(base::TimeDelta time, bool is_image);
-
-// Logs to UMA the time to detect a clipboard write for Remote Copy.
-void LogRemoteCopyWriteDetectionTime(base::TimeDelta time, bool is_image);
 
 #endif  // CHROME_BROWSER_SHARING_SHARING_METRICS_H_
