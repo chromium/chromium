61aa067bfbfae049c45494736ba9208e75bf438e
Rubin Deliallisi <rubindl@chromium.org>
2023-05-31 19:06:12 +0000

    Privacy Guide: Aux button a11y fix
    - Standardise RadioButtonWithDescriptionAndAuxButton a11y behaviour.
    
    Current Talkback speech:
    - Radio button
      "<title>, <description> for Expand <auxButtonContentDescription>"
    - Aux button
      "Expand <auxButtonContentDescription>"
    
    New Talkback speech:
    - Radio button
      "<title>, <description>"
    - Aux button
      "Expand, Button for <title>"
    
    (cherry picked from commit a9d741dc302593c05190adee22283e21063fdc87)
    
    Bug: 1446122
    Change-Id: I4488b3f3427ffe9a0c6521c8779d17e7f8a25c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570621
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1150610}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574907
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5790@{#194}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithDescriptionAndAuxButton.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithDescriptionAndAuxButton.java
@@ -7,7 +7,6 @@ package org.chromium.components.browser_ui.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageButton;
 
 import androidx.annotation.VisibleForTesting;
@@ -95,6 +94,7 @@ public class RadioButtonWithDescriptionAndAuxButton extends RadioButtonWithDescr
     protected void setViewsInternal() {
         super.setViewsInternal();
         mAuxButton = findViewById(R.id.expand_arrow);
+        getPrimaryTextView().setLabelFor(mAuxButton.getId());
     }
 
     /**
@@ -116,12 +116,6 @@ public class RadioButtonWithDescriptionAndAuxButton extends RadioButtonWithDescr
         setAuxButtonEnabled(enabled);
     }
 
-    @Override
-    public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
-        super.onInitializeAccessibilityNodeInfo(info);
-        info.setLabeledBy(mAuxButton);
-    }
-
     /**
      * Sets the enabled state of the aux button alone. This can be used if you want the aux button
      * to be enabled while the other child views are disabled or vice versa.
@@ -138,14 +132,6 @@ public class RadioButtonWithDescriptionAndAuxButton extends RadioButtonWithDescr
     public void setAuxButtonClickedListener(OnAuxButtonClickedListener listener) {
         mListener = listener;
         mAuxButton.setOnClickListener(v -> mListener.onAuxButtonClicked(getId()));
-    }
-
-    /**
-     * Set the aux button content description.
-     * @param contentDescription for aux button, used for accessibility purposes.
-     */
-    public void setAuxButtonContentDescription(String contentDescription) {
-        mAuxButton.setContentDescription(contentDescription);
     }
 
     /**
