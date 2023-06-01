f22311bcd6f742830a2084675b7bf74e011c8fc1
Michael Ershov <miersh@google.com>
2023-06-01 15:26:15 +0000

    Kcer-over-NSS: Fix queue unblocker and ListCerts
    * When the IO thread gets stopped, tasks for it are destroyed on
    their current threads. ScopedClosureRunner on destruction checks
    whether its closure is still valid and if it is doing that on a
    wrong thread, it effectively derefences the weak pointer from
    the callback on a wrong thread and crashes on the invalid
    sequence check.Make ScopedClosureRunner from KcerTokenImplNss
    unconditionally post its closure on the correct thread instead
    of trying to run it. In most cases it will work the same, in
    the described scenario this will destroy the closure without
    checking the weak pointer.
    * Check the cert_list pointer before iterating over the list in
    the ListCertsOnWorkerThread method.
    
    Bug: b:244408716
    Test: Kcer* unit tests
    Change-Id: I690722da62739c744531876966e271864e8e6f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568007
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1151877}

--- chrome/browser/chromeos/kcer_nss/kcer_token_impl_nss.cc
+++ chrome/browser/chromeos/kcer_nss/kcer_token_impl_nss.cc
@@ -335,9 +335,11 @@ void ListCertsOnWorkerThread(
 
   net::ScopedCERTCertificateList result;
 
-  for (CERTCertListNode* node = CERT_LIST_HEAD(cert_list);
-       !CERT_LIST_END(node, cert_list); node = CERT_LIST_NEXT(node)) {
-    result.push_back(net::x509_util::DupCERTCertificate(node->cert));
+  if (cert_list) {
+    for (CERTCertListNode* node = CERT_LIST_HEAD(cert_list);
+         !CERT_LIST_END(node, cert_list); node = CERT_LIST_NEXT(node)) {
+      result.push_back(net::x509_util::DupCERTCertificate(node->cert));
+    }
   }
 
   std::move(callback).Run(std::move(result));
@@ -1413,15 +1415,15 @@ base::OnceClosure KcerTokenImplNss::BlockQueueGetUnblocker() {
   CHECK(!is_blocked_);
   is_blocked_ = true;
 
-  // `unblocker` is executed either manually or on destruction.
-  base::ScopedClosureRunner unblocker(
+  auto unblock_on_io = base::BindPostTask(
+      content::GetIOThreadTaskRunner({}),
       base::BindOnce(&KcerTokenImplNss::UnblockQueueProcessNextTask,
                      weak_factory_.GetWeakPtr()));
-  // Pack `unblocker` into an IO thread bound closure, so it can be attached
-  // to a callback.
-  return base::BindPostTask(
-      content::GetIOThreadTaskRunner({}),
-      base::BindOnce(&RunUnblocker, std::move(unblocker)));
+
+  // `unblocker` is executed either manually or on destruction.
+  base::ScopedClosureRunner unblocker(std::move(unblock_on_io));
+  // Pack `unblocker` into a closure, so it can be attached to a callback.
+  return base::BindOnce(&RunUnblocker, std::move(unblocker));
 }
 
 void KcerTokenImplNss::UnblockQueueProcessNextTask() {
