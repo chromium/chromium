d405804f0edc69faacbf941a2dbe1069d5a6fb37
David Yeung <dayeung@chromium.org>
2023-06-01 16:32:50 +0000

    [CR2023] Add more icon sizes to components/vector_icons
    The default font size of these icons is 20 so we should have the 20 size icon with the correct font weight. In the process, I've made it consistent so all icons will have 16, 20 and 24.
    
    Screenshot of some of the icons used. This was tested in various icon sizes and on/off state.
    https://screenshot.googleplex.com/hPeZt7zzkDW9k7u.png
    
    Screenshots of all icons in components/vector
    https://screenshot.googleplex.com/85wcK5QXFayd28N
    
    Bug: 1422123
    Change-Id: I33435a06d338c2c2b3a9edb83b26b8e27e8f9836
    Low-Coverage-Reason: Renaming vector icons
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4568796
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151908}

--- chrome/browser/ui/views/page_info/page_info_view_factory.cc
+++ chrome/browser/ui/views/page_info/page_info_view_factory.cc
@@ -270,8 +270,8 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
                                   : &vector_icons::kPhotoChromeRefreshIcon;
         break;
       case ContentSettingsType::JAVASCRIPT:
-        icon = show_blocked_badge ? &vector_icons::kCodeOffIcon
-                                  : &vector_icons::kCodeIcon;
+        icon = show_blocked_badge ? &vector_icons::kCodeOffChromeRefreshIcon
+                                  : &vector_icons::kCodeChromeRefreshIcon;
         break;
       case ContentSettingsType::POPUPS:
         icon = show_blocked_badge ? &vector_icons::kLaunchOffChromeRefreshIcon
@@ -308,8 +308,8 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
         break;
 #endif
       case ContentSettingsType::MIDI_SYSEX:
-        icon = show_blocked_badge ? &vector_icons::kMidiOffIcon
-                                  : &vector_icons::kMidiIcon;
+        icon = show_blocked_badge ? &vector_icons::kMidiOffChromeRefreshIcon
+                                  : &vector_icons::kMidiChromeRefreshIcon;
         break;
       case ContentSettingsType::BACKGROUND_SYNC:
         icon = show_blocked_badge ? &vector_icons::kSyncOffChromeRefreshIcon
@@ -329,8 +329,8 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
                    : &vector_icons::kPageInfoContentPasteChromeRefreshIcon;
         break;
       case ContentSettingsType::SENSORS:
-        icon = show_blocked_badge ? &vector_icons::kSensorsOffIcon
-                                  : &vector_icons::kSensorsIcon;
+        icon = show_blocked_badge ? &vector_icons::kSensorsOffChromeRefreshIcon
+                                  : &vector_icons::kSensorsChromeRefreshIcon;
         break;
       case ContentSettingsType::USB_GUARD:
         icon = show_blocked_badge ? &vector_icons::kUsbOffChromeRefreshIcon
@@ -361,8 +361,8 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
                    : &vector_icons::kVrHeadsetChromeRefreshIcon;
         break;
       case ContentSettingsType::AR:
-        icon = show_blocked_badge ? &vector_icons::kViewInArOffIcon
-                                  : &vector_icons::kViewInArIcon;
+        icon = show_blocked_badge ? &vector_icons::kViewInArOffChromeRefreshIcon
+                                  : &vector_icons::kViewInArChromeRefreshIcon;
         break;
       case ContentSettingsType::WINDOW_MANAGEMENT:
         icon = show_blocked_badge
@@ -380,8 +380,8 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
                    : &vector_icons::kVideogameAssetChromeRefreshIcon;
         break;
       case ContentSettingsType::IDLE_DETECTION:
-        icon = show_blocked_badge ? &vector_icons::kDevicesOffIcon
-                                  : &vector_icons::kDevicesIcon;
+        icon = show_blocked_badge ? &vector_icons::kDevicesOffChromeRefreshIcon
+                                  : &vector_icons::kDevicesChromeRefreshIcon;
         break;
       default:
         break;
