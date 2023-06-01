92701aac108515f94b0b1820fb2d3faab744f43e
Samar <samarchehade@google.com>
2023-06-01 09:26:36 +0000

    [Signin][Android] Enable IdentityStatusConsistency by default.
    Bug: launch/4230766
    Change-Id: Ic16a5a6845ecda97370c46018e5974c757cc6aa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574386
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1151746}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -15,15 +15,6 @@ namespace switches {
 BASE_FEATURE(kGaiaIdCacheInAccountManagerFacade,
              "GaiaIdCacheInAccountManagerFacade",
              base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kIdentityStatusConsistency,
-             "IdentityStatusConsistency",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
-
-#if BUILDFLAG(IS_IOS)
-BASE_FEATURE(kIdentityStatusConsistency,
-             "IdentityStatusConsistency",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 // Clears the token service before using it. This allows simulating the
@@ -61,6 +52,10 @@ BASE_FEATURE(kForceDisableExtendedSyncPromos,
 BASE_FEATURE(kForceStartupSigninPromo,
              "ForceStartupSigninPromo",
              base::FEATURE_DISABLED_BY_DEFAULT);
+// Feature to add a signed-out avatar on the NTP.
+BASE_FEATURE(kIdentityStatusConsistency,
+             "IdentityStatusConsistency",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 // Enables a new version of the sync confirmation UI.
