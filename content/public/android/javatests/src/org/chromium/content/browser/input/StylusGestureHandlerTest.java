cbba035d55a47cb6d25c258c24de27a4acf54697
Alex Mitra <alexmitra@chromium.org>
2023-06-01 21:20:17 +0000

    Adds e2e test for stylus delete gesture
    Bug: 1442897
    Change-Id: Ib76037e16e32e6582d2ab019a7bc409baa632599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4507354
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152129}

--- content/public/android/javatests/src/org/chromium/content/browser/input/StylusGestureHandlerTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/StylusGestureHandlerTest.java
@@ -7,11 +7,17 @@ package org.chromium.content.browser.input;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 import android.graphics.PointF;
 import android.graphics.RectF;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.filters.MediumTest;
 
 import org.junit.Before;
@@ -26,7 +32,10 @@ import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.blink.mojom.StylusWritingGestureAction;
 import org.chromium.blink.mojom.StylusWritingGestureData;
+import org.chromium.content.browser.RenderCoordinatesImpl;
+import org.chromium.content.browser.webcontents.WebContentsImpl;
 import org.chromium.content_public.browser.test.ContentJUnit4ClassRunner;
+import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.gfx.mojom.Rect;
 
@@ -34,6 +43,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Tests for using a StylusGestureHandler as an InvocationHandler in place of an InputConnection in
@@ -340,6 +350,66 @@ public class StylusGestureHandlerTest {
         histogram.assertExpected();
     }
 
+    @Test
+    @LargeTest
+    public void testDeleteGestureEndToEnd()
+            throws TimeoutException, ClassNotFoundException, IllegalAccessException,
+                   InstantiationException, InvocationTargetException {
+        EditorInfo editorInfo = new EditorInfo();
+        ImeAdapterImpl imeAdapter = spy(mRule.getImeAdapter());
+        doReturn(mRule.getConnection())
+                .when(imeAdapter)
+                .onCreateInputConnection(any(EditorInfo.class), any(boolean.class));
+        doCallRealMethod().when(imeAdapter).onCreateInputConnection(any(EditorInfo.class));
+        mWrappedInputConnection = imeAdapter.onCreateInputConnection(editorInfo);
+        JavaScriptUtils.executeJavaScriptAndWaitForResult(mRule.getWebContents(),
+                "document.getElementById(\"input_text\").value = \"hello world\"");
+
+        // Gets the bounding box of the element with id: input_text.
+        float left = Float.parseFloat(
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(mRule.getWebContents(),
+                        "document.getElementById('input_text').getBoundingClientRect().left"));
+        float top = Float.parseFloat(
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(mRule.getWebContents(),
+                        "document.getElementById('input_text').getBoundingClientRect().top"));
+        float right = Float.parseFloat(
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(mRule.getWebContents(),
+                        "document.getElementById('input_text').getBoundingClientRect().right"));
+        float bottom = Float.parseFloat(
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(mRule.getWebContents(),
+                        "document.getElementById('input_text').getBoundingClientRect().bottom"));
+
+        RectF gestureRect =
+                toScreenRectF(left, top, right, bottom, (WebContentsImpl) mRule.getWebContents());
+        gestureRect.inset(1, 1); // Inset to avoid including the border.
+
+        // The following reflection creates a new DeleteGesture over the left half of the input_text
+        // element. It has word granularity and the fallback text defined in FALLBACK_TEXT.
+        Class builderClass = getBuilderForClass(Class.forName(TARGET_PACKAGE + "DeleteGesture"));
+        Map<String, Method> builderMethods = getMethodsForClass(builderClass);
+        Object builder = builderClass.newInstance();
+        builderMethods.get("setGranularity").invoke(builder, StylusGestureHandler.GRANULARITY_WORD);
+        builderMethods.get("setDeletionArea").invoke(builder, gestureRect);
+        builderMethods.get("setFallbackText").invoke(builder, FALLBACK_TEXT);
+        Object gesture = builderMethods.get("build").invoke(builder);
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            Method performHandwritingGesture =
+                    getMethodsForClass(mWrappedInputConnection.getClass())
+                            .get("performHandwritingGesture");
+            try {
+                performHandwritingGesture.invoke(mWrappedInputConnection, gesture, null, null);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                fail("Failed to call performHandwritingGesture");
+            }
+        });
+
+        // Get the text inside input_text and assert that it has been deleted.
+        assertEquals("\"\"",
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(
+                        mRule.getWebContents(), "document.getElementById(\"input_text\").value"));
+    }
+
     private static Map<String, Method> getMethodsForClass(Class<?> className) {
         Map<String, Method> methodMap = new HashMap<>();
         for (Method method : className.getDeclaredMethods()) {
@@ -384,5 +454,28 @@ public class StylusGestureHandlerTest {
             string.append((char) c);
         }
         return string.toString();
+    }
+
+    /**
+     * @param left the X value for the left edge of the rectangle.
+     * @param top the Y value for the top edge of the rectangle.
+     * @param right the X value for the right edge of the rectangle.
+     * @param bottom the Y value for the bottom edge of the rectangle.
+     * @param webContents the web contents where this rectangle is being used (for converting to
+     *         screen coordinates).
+     * @return an Android RectF object represented by the four float values in screen coordinates.
+     */
+    private static RectF toScreenRectF(
+            float left, float top, float right, float bottom, WebContentsImpl webContents) {
+        // Convert from local CSS coordinates to absolute screen coordinates.
+        RenderCoordinatesImpl rc = webContents.getRenderCoordinates();
+        int[] screenLocation = new int[2];
+        webContents.getViewAndroidDelegate().getContainerView().getLocationOnScreen(screenLocation);
+        left = rc.fromLocalCssToPix(left) + screenLocation[0];
+        top = rc.fromLocalCssToPix(top) + rc.getContentOffsetYPix() + screenLocation[1];
+        right = rc.fromLocalCssToPix(right) + screenLocation[0];
+        bottom = rc.fromLocalCssToPix(bottom) + rc.getContentOffsetYPixInt() + screenLocation[1];
+
+        return new RectF(left, top, right, bottom);
     }
 }
