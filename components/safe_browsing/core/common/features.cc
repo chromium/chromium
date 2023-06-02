12826150c1188b9ea25131ad791d26fb85880c77
Seung Jae Lim <andysjlim@chromium.org>
2023-06-02 17:40:57 +0000

    Enable ESB thresholds feature by default.
    This is part of the release for Client Side Phishing model using the ESB thresholds. The finch field trials has been removed from a previous change list.
    
    Bug: 1406979
    Change-Id: Iadcce4bde653320baba76578493fe462e933a452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4573919
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152628}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -288,7 +288,7 @@ BASE_FEATURE(kClientSideDetectionModelOptimizationGuide,
 
 BASE_FEATURE(kSafeBrowsingPhishingClassificationESBThreshold,
              "SafeBrowsingPhishingClassificationESBThreshold",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSafeBrowsingDailyPhishingReportsLimit,
              "SafeBrowsingDailyPhishingReportsLimit",
