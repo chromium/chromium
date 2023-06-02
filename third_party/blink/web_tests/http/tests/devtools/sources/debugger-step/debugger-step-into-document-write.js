18be90dd7b60228e83f0d56fc83a7fccacdaf9cf
Simon Zünd <szuend@chromium.org>
2023-06-02 05:34:48 +0000

    Import test runners in http/tests/devtools/sources/debugger-step
    This CL replaces 'TestRunner.loadTestModule' calls with import
    statements.
    
    The CL was fully auto-generated by a small regex search&replace script.
    
    R=kprokopenko@chromium.org
    
    Bug: 1442410
    Change-Id: I1b855545004e927af2b6e76a9d62afbbeab4378f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4574698
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1152365}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-step/debugger-step-into-document-write.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-step/debugger-step-into-document-write.js
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestRunner} from 'test_runner';
+import {SourcesTestRunner} from 'sources_test_runner';
+
 (async function() {
   TestRunner.addResult(
       `Tests that debugger StepInto will step into inlined scripts created by document.write().\n`);
-  await TestRunner.loadLegacyModule('sources'); await TestRunner.loadTestModule('sources_test_runner');
+  await TestRunner.loadLegacyModule('sources');
   await TestRunner.showPanel('sources');
   await TestRunner.navigatePromise(
       'resources/debugger-step-into-document-write.html')
