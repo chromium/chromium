8b7c504e3429626b4075ef5ecc8ba0163ed93a17
Abhijeet Kandalkar <abhijeet@igalia.com>
2023-06-05 20:34:06 +0000

    [ios] Enable HEVC for ios-blink
    Cq-Include-Trybots: luci.chromium.try:ios-blink-dbg-fyi
    
    Bug: 1449878
    Change-Id: If55783c8e3cab653390b7a1d2a779b07f1ad5c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582354
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1153433}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -336,12 +336,12 @@ BASE_FEATURE(kPlatformHEVCDecoderSupport,
              "PlatformHEVCDecoderSupport",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE)
 // Enables HEVC hardware accelerated encoding for Windows and Mac.
 BASE_FEATURE(kPlatformHEVCEncoderSupport,
              "PlatformHEVCEncoderSupport",
              base::FEATURE_DISABLED_BY_DEFAULT);
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE)
 #endif  // BUILDFLAG(ENABLE_PLATFORM_HEVC)
 
 // Only decode preload=metadata elements upon visibility.
