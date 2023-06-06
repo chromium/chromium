f8a3b17b237193ac533695916a53aab528e0aaa6
Aliona DANGLA <alionadangla@google.com>
2023-06-06 22:18:29 +0000

    [iOS] Ensure to test both fullscreen implementation
    This CL duplicates fullscreen egtest to ensure that both implementations
    are tested.
    
    Change-Id: Ie309d527621fe9a96e3c3419d88c651969fb4794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4588789
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154124}

--- ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
@@ -85,10 +85,17 @@ void WaitforPDFExtensionView() {
 #pragma mark - Tests
 
 // Fullscreens tests for Chrome.
-@interface FullscreenTestCase : WebHttpServerChromeTestCase
+// TODO(crbug.com/1338585): Remove the "ZZZ" when the bug is fixed.
+@interface ZZZFullscreenTestCase : WebHttpServerChromeTestCase
 @end
 
-@implementation FullscreenTestCase
+@implementation ZZZFullscreenTestCase
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_disabled.push_back(web::features::kSmoothScrollingDefault);
+  return config;
+}
 
 - (void)setUp {
   [super setUp];
@@ -406,6 +413,26 @@ void WaitforPDFExtensionView() {
   [ChromeEarlGrey tapWebStateElementWithID:@"link"];
   AssertURLIs(ErrorPageResponseProvider::GetDnsFailureUrl());
   [ChromeEarlGreyUI waitForToolbarVisible:YES];
+}
+
+@end
+
+#pragma mark - Smooth scrolling enabled Tests
+
+// Fullscreens tests for Chrome.
+@interface FullscreenSmoothScrollingTestCase : ZZZFullscreenTestCase
+@end
+
+@implementation FullscreenSmoothScrollingTestCase
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_enabled.push_back(web::features::kSmoothScrollingDefault);
+  return config;
+}
+
+// This is currently needed to prevent this test case from being ignored.
+- (void)testEmpty {
 }
 
 @end
