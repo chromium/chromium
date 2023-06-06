2e963be67f8f87012657c3c9aa786e8dd3d0c513
Wenyu Fu <wenyufu@chromium.org>
2023-06-06 18:26:10 +0000

    Revert "[Sharesheet] Remove link from long screenshot"
    This reverts commit 1831f8a8ebc6b712966c6ce983829ef451ff38e3.
    
    Reason for revert: Product decides a behavior change so this change is moot.
    
    Original change's description:
    > [Sharesheet] Remove link from long screenshot
    >
    > Bug: 1451596
    > Change-Id: Idfdae23dfd3e3107c40012da493b2fc633c28f95
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4591756
    > Reviewed-by: Sophey Dong <sophey@chromium.org>
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1153577}
    
    Bug: 1451596
    Change-Id: I9bf793c4d1ed2a2d41f35d61c26a65b7d5f42f7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594985
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1153962}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidCustomActionProvider.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidCustomActionProvider.java
@@ -152,10 +152,8 @@ class AndroidCustomActionProvider extends ChromeProvidedSharingOptionsProviderBa
                 .setDisableForMultiWindow(true)
                 .setOnClickCallback((view) -> {
                     mFeatureEngagementTracker.notifyEvent(EventConstants.SHARE_SCREENSHOT_SELECTED);
-                    // Do not add link to the screenshot for Android share sheet since there's no
-                    // custom action to remove the link from image.
                     LongScreenshotsCoordinator coordinator =
-                            LongScreenshotsCoordinator.create(mActivity, mTabProvider.get(), "",
+                            LongScreenshotsCoordinator.create(mActivity, mTabProvider.get(), mUrl,
                                     mChromeOptionShareCallback, mBottomSheetController, null);
                     coordinator.captureScreenshot();
                 })
