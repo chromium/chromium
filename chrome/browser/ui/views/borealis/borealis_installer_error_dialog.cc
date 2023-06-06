269af1d31a00ba705884d20a57448f603ce17f5a
Nicholas Hollingum <hollingum@google.com>
2023-06-06 06:36:06 +0000

    borealis: update installer-error dialog and strings
    The strings and UI were redesigned recently.
    
    Bug: b:284779004
    Change-Id: I6ab6ee580b69baacc9618c3c4cc8a5febdf0866d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4587561
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Auto-Submit: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153696}

--- chrome/browser/ui/views/borealis/borealis_installer_error_dialog.cc
+++ chrome/browser/ui/views/borealis/borealis_installer_error_dialog.cc
@@ -3,18 +3,23 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/borealis/borealis_installer_error_dialog.h"
+
 #include <memory>
 #include <string>
 
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/window_properties.h"
+#include "base/allocator/partition_allocator/pointers/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/borealis/borealis_features.h"
 #include "chrome/browser/ash/borealis/borealis_metrics.h"
 #include "chrome/browser/ash/borealis/borealis_util.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
+#include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
@@ -22,6 +27,10 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_types.h"
+#include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/color/color_id.h"
+#include "ui/color/color_provider.h"
+#include "ui/gfx/text_constants.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -30,6 +39,7 @@
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
 
 namespace views::borealis {
@@ -38,94 +48,119 @@ namespace {
 
 using InstallResult = ::borealis::BorealisInstallResult;
 
-bool ShouldAllowRetry(InstallResult result) {
-  switch (result) {
-    case InstallResult::kBorealisInstallInProgress:
-    case InstallResult::kDlcBusyError:
-    case InstallResult::kDlcNeedSpaceError:
-    case InstallResult::kOffline:
-      return true;
-    case InstallResult::kDlcInternalError:
-    case InstallResult::kDlcUnknownError:
-    case InstallResult::kDlcNeedRebootError:
-    case InstallResult::kBorealisNotAllowed:
-    case InstallResult::kDlcUnsupportedError:
-    case InstallResult::kDlcNeedUpdateError:
-    case InstallResult::kStartupFailed:
-    case InstallResult::kMainAppNotPresent:
-      return false;
-    case InstallResult::kSuccess:
-    case InstallResult::kCancelled:
-      NOTREACHED_NORETURN();
-  }
-}
+// Optionally, the dialog has an extra button so the user can act on the error.
+enum class CallToAction {
+  kNone,
+  kRetry,
+  kStorageManagement,
+};
 
-std::pair<std::u16string, absl::optional<GURL>> GetErrorMessageAndHelpLink(
-    InstallResult result) {
-  // TODO(b/256699588): Replace the below direct URLs with p-links.
+class ErrorBehaviourProvider {
+ public:
+  virtual ~ErrorBehaviourProvider() = default;
+
+  virtual CallToAction GetAction() const { return CallToAction::kNone; }
+
+  virtual std::u16string ErrorMessage() const = 0;
+};
+
+class Duplicate : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_DUPLICATE);
+  }
+};
+
+class Update : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_UPDATE);
+  }
+};
+
+class Busy : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_BUSY);
+  }
+  CallToAction GetAction() const override { return CallToAction::kRetry; }
+};
+
+class Space : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_SPACE);
+  }
+  CallToAction GetAction() const override {
+    return CallToAction::kStorageManagement;
+  }
+};
+
+class Offline : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_OFFLINE);
+  }
+  CallToAction GetAction() const override { return CallToAction::kRetry; }
+};
+
+class Startup : public ErrorBehaviourProvider {
+ public:
+  std::u16string ErrorMessage() const override {
+    return l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_STARTUP);
+  }
+  CallToAction GetAction() const override { return CallToAction::kRetry; }
+};
+
+std::unique_ptr<ErrorBehaviourProvider> GetBehaviour(InstallResult result) {
   switch (result) {
-    case InstallResult::kBorealisNotAllowed:
-    case InstallResult::kDlcUnsupportedError:
-      return {
-          l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_DISALLOWED),
-          absl::nullopt};
-    case InstallResult::kBorealisInstallInProgress:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_PROGRESS),
-              absl::nullopt};
-    case InstallResult::kDlcInternalError:
-    case InstallResult::kDlcNeedUpdateError:
-      return {
-          l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_DLC_INTERNAL),
-          GURL("https://support.google.com/chromebook/answer/177889")};
-    case InstallResult::kDlcBusyError:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_DLC_BUSY),
-              absl::nullopt};
-    case InstallResult::kDlcNeedRebootError:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_REBOOT),
-              absl::nullopt};
-    case InstallResult::kDlcNeedSpaceError:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_SPACE),
-              GURL("https://support.google.com/chromebook/answer/1061547")};
-    case InstallResult::kDlcUnknownError:
-      return {
-          l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_DLC_UNKNOWN),
-          absl::nullopt};
-    case InstallResult::kOffline:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_OFFLINE),
-              GURL("https://support.google.com/chromebook/answer/6318213")};
-    case InstallResult::kStartupFailed:
-    case InstallResult::kMainAppNotPresent:
-      return {l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_STARTUP),
-              absl::nullopt};
     case InstallResult::kSuccess:
     case InstallResult::kCancelled:
-      NOTREACHED_NORETURN();
+      DCHECK(false);
+      ABSL_FALLTHROUGH_INTENDED;
+    case InstallResult::kBorealisNotAllowed:
+      // This should not be reachable either but if some kind of dynamic
+      // permission change would occur then we can get here, so don't DCHECK.
+      return GetBehaviour(InstallResult::kDlcNeedRebootError);
+    case InstallResult::kBorealisInstallInProgress:
+      return std::make_unique<Duplicate>();
+    case InstallResult::kDlcUnsupportedError:
+    case InstallResult::kDlcInternalError:
+    case InstallResult::kDlcUnknownError:
+    case InstallResult::kDlcNeedRebootError:
+    case InstallResult::kDlcNeedUpdateError:
+      // We capture most dlc-related issues as "need update". This is not
+      // strictly true (there may not be an update available, and it may not fix
+      // it) but we don't have visibility into their cause enough to make a
+      // better recommendation.
+      return std::make_unique<Update>();
+    case InstallResult::kDlcBusyError:
+      return std::make_unique<Busy>();
+    case InstallResult::kDlcNeedSpaceError:
+      return std::make_unique<Space>();
+    case InstallResult::kOffline:
+      return std::make_unique<Offline>();
+    case InstallResult::kStartupFailed:
+    case InstallResult::kMainAppNotPresent:
+      return std::make_unique<Startup>();
   }
 }
 
 class BorealisInstallerErrorDialog : public views::DialogDelegate {
  public:
-  BorealisInstallerErrorDialog(InstallResult result, DialogCallback callback)
-      : result_(result), callback_(std::move(callback)) {
+  BorealisInstallerErrorDialog(
+      std::unique_ptr<ErrorBehaviourProvider> behaviour,
+      DialogCallback callback)
+      : behaviour_(std::move(behaviour)), callback_(std::move(callback)) {
     set_internal_name("BorealisInstallerErrorDialog");
-    if (ShouldAllowRetry(result_)) {
-      SetButtons(ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL);
-      SetButtonLabel(
-          ui::DIALOG_BUTTON_OK,
-          l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_RETRY));
-      SetButtonLabel(ui::DIALOG_BUTTON_CANCEL,
-                     l10n_util::GetStringUTF16(IDS_APP_CANCEL));
-    } else {
-      SetButtons(ui::DIALOG_BUTTON_OK);
-      SetButtonLabel(ui::DIALOG_BUTTON_OK,
-                     l10n_util::GetStringUTF16(IDS_APP_CANCEL));
-    }
+    InitializeButtons();
     InitializeView();
     SetModalType(ui::MODAL_TYPE_WINDOW);
     SetOwnedByWidget(true);
     SetShowCloseButton(false);
     set_fixed_width(ChromeLayoutProvider::Get()->GetDistanceMetric(
         views::DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH));
+    set_corner_radius(12);
 
     // Bind the callbacks to our internal dispatcher.
     SetAcceptCallback(base::BindOnce(&BorealisInstallerErrorDialog::OnDismissed,
@@ -139,7 +174,40 @@ class BorealisInstallerErrorDialog : public views::DialogDelegate {
                                     /*is_accept:=*/false));
   }
 
+  void SetIconColor(views::View* main_view) {
+    const ui::ColorProvider* color_provider = main_view->GetColorProvider();
+    CHECK(color_provider);
+    alert_icon_->SetEnabledColor(
+        color_provider->GetColor(cros_tokens::kIconColorAlert));
+  }
+
  private:
+  void InitializeButtons() {
+    switch (behaviour_->GetAction()) {
+      case CallToAction::kNone:
+        SetButtons(ui::DIALOG_BUTTON_OK);
+        SetButtonLabel(ui::DIALOG_BUTTON_OK,
+                       l10n_util::GetStringUTF16(IDS_APP_CANCEL));
+        break;
+      case CallToAction::kRetry:
+        SetButtons(ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL);
+        SetButtonLabel(ui::DIALOG_BUTTON_OK,
+                       l10n_util::GetStringUTF16(
+                           IDS_BOREALIS_INSTALLER_ERROR_BUTTON_RETRY));
+        SetButtonLabel(ui::DIALOG_BUTTON_CANCEL,
+                       l10n_util::GetStringUTF16(IDS_APP_CANCEL));
+        break;
+      case CallToAction::kStorageManagement:
+        SetButtons(ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL);
+        SetButtonLabel(ui::DIALOG_BUTTON_OK,
+                       l10n_util::GetStringUTF16(
+                           IDS_BOREALIS_INSTALLER_ERROR_BUTTON_STORAGE));
+        SetButtonLabel(ui::DIALOG_BUTTON_CANCEL,
+                       l10n_util::GetStringUTF16(IDS_APP_CANCEL));
+        break;
+    }
+  }
+
   void InitializeView() {
     auto view = std::make_unique<views::View>();
 
@@ -149,53 +217,53 @@ class BorealisInstallerErrorDialog : public views::DialogDelegate {
         provider->GetInsetsMetric(views::InsetsMetric::INSETS_DIALOG),
         provider->GetDistanceMetric(views::DISTANCE_RELATED_CONTROL_VERTICAL)));
 
-    views::ImageView* error_icon = new views::ImageView();
-    error_icon->SetImage(
-        ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-            IDR_PLUGIN_VM_INSTALLER_ERROR));
-    error_icon->SetImageSize({64, 64});
-    view->AddChildView(error_icon);
+    // TODO(b/284389804): Use TypographyToken::kCrosDisplay*
+    alert_icon_ = view->AddChildView(std::make_unique<views::Label>(
+        u"\x24D8", views::Label::CustomFont{gfx::FontList(
+                       {"Google Sans", "Roboto"}, gfx::Font::NORMAL, 20,
+                       gfx::Font::Weight::BOLD)}));
+    alert_icon_->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
 
     views::Label* title_label = new views::Label(
         l10n_util::GetStringUTF16(IDS_BOREALIS_INSTALLER_ERROR_TITLE),
         CONTEXT_IPH_BUBBLE_TITLE, views::style::STYLE_EMPHASIZED);
-    title_label->SetHorizontalAlignment(gfx::ALIGN_CENTER);
     title_label->SetMultiLine(true);
+    title_label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
     view->AddChildView(title_label);
 
-    std::pair<std::u16string, absl::optional<GURL>> message_link =
-        GetErrorMessageAndHelpLink(result_);
-    views::Label* message_label = new views::Label(message_link.first);
+    views::Label* message_label = new views::Label(behaviour_->ErrorMessage());
     message_label->SetMultiLine(true);
-    message_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+    message_label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
     view->AddChildView(message_label);
-
-    if (message_link.second.has_value()) {
-      views::Link* learn_more_link =
-          new views::Link(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
-      learn_more_link->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-      learn_more_link->SetCallback(base::BindRepeating(
-          [](GURL url) {
-            ash::NewWindowDelegate::GetPrimary()->OpenUrl(
-                url, ash::NewWindowDelegate::OpenUrlFrom::kUserInteraction,
-                ash::NewWindowDelegate::Disposition::kNewForegroundTab);
-          },
-          message_link.second.value()));
-      view->AddChildView(learn_more_link);
-    }
 
     SetContentsView(std::move(view));
   }
 
   void OnDismissed(bool is_accept) {
     DCHECK(!callback_.is_null());
-    std::move(callback_).Run(is_accept && ShouldAllowRetry(result_)
-                                 ? views::borealis::ErrorDialogChoice::kRetry
-                                 : views::borealis::ErrorDialogChoice::kExit);
+    std::move(callback_).Run(GetChoice(is_accept));
   }
 
-  InstallResult result_;
+  views::borealis::ErrorDialogChoice GetChoice(bool is_accept) {
+    switch (behaviour_->GetAction()) {
+      case CallToAction::kNone:
+        return views::borealis::ErrorDialogChoice::kExit;
+      case CallToAction::kRetry:
+        return is_accept ? views::borealis::ErrorDialogChoice::kRetry
+                         : views::borealis::ErrorDialogChoice::kExit;
+      case CallToAction::kStorageManagement:
+        if (is_accept) {
+          chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+              ProfileManager::GetPrimaryUserProfile(),
+              chromeos::settings::mojom::kStorageSubpagePath);
+        }
+        return views::borealis::ErrorDialogChoice::kExit;
+    }
+  }
+
+  std::unique_ptr<ErrorBehaviourProvider> behaviour_;
   DialogCallback callback_;
+  raw_ptr<views::Label> alert_icon_;
 
   base::WeakPtrFactory<BorealisInstallerErrorDialog> weak_factory_{this};
 };
@@ -207,11 +275,13 @@ void ShowInstallerErrorDialog(gfx::NativeView parent,
                               DialogCallback callback) {
   DCHECK(result != InstallResult::kSuccess);
   DCHECK(result != InstallResult::kCancelled);
-  views::DialogDelegate::CreateDialogWidget(
-      std::make_unique<BorealisInstallerErrorDialog>(result,
-                                                     std::move(callback)),
-      nullptr, parent)
-      ->Show();
+  auto delegate = std::make_unique<BorealisInstallerErrorDialog>(
+      GetBehaviour(result), std::move(callback));
+  BorealisInstallerErrorDialog* delegate_ptr = delegate.get();
+  views::Widget* widget = views::DialogDelegate::CreateDialogWidget(
+      std::move(delegate), nullptr, parent);
+  delegate_ptr->SetIconColor(widget->GetRootView());
+  widget->Show();
 }
 
 }  // namespace views::borealis
