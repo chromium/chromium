7eaf1a107ea7c5d04d62a99cb723fea0f2f8ae93
Leon Masopust <lmasopust@google.com>
2023-06-06 07:12:08 +0000

    DTC-BYOD: Add SignalsFilterer base class
    Add the SignalsFilterer base class which will be used by Chrome
    Browser and does nothing when Filter() is called.
    
    Bug: b:284454127
    Change-Id: Ie253bd94bb879dad112108865d0d0f51363731cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4584629
    Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Leon Masopust <lmasopust@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1153708}

---
+++ chrome/browser/enterprise/connectors/device_trust/signals/signals_filterer.cc
@@ -0,0 +1,11 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/signals/signals_filterer.h"
+
+namespace enterprise_connectors {
+
+void SignalsFilterer::Filter(base::Value::Dict& signals) {}
+
+}  // namespace enterprise_connectors
