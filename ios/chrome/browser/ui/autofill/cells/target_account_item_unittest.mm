718686b6a25fedab806769ecc49d33c9cf95a378
Vidhan Jain <vidhanj@google.com>
2023-06-06 20:35:01 +0000

    Add unit test for TargetAccountItem
    This CL is an attempt to improve test coverage for
    ios/chrome/browser/ui/autofill/cells/ by adding a unittest file for
    TargetAccountItem.
    
    Change-Id: Ic74b8df4feb1dd1069ef23ec93f5c61c0dcf78f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4593897
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154047}

---
+++ ios/chrome/browser/ui/autofill/cells/target_account_item_unittest.mm
@@ -0,0 +1,41 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/autofill/cells/target_account_item.h"
+
+#import "base/mac/foundation_util.h"
+#import "ios/chrome/browser/shared/ui/table_view/chrome_table_view_styler.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/gtest_mac.h"
+#import "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+using TargetAccountItemTest = PlatformTest;
+}
+
+// Tests that the email UILabel and avatar is set properly after a call to
+// `configureCell:`.
+TEST_F(TargetAccountItemTest, ItemProperties) {
+  NSString* email = @"test@gmail.com";
+
+  TargetAccountItem* item = [[TargetAccountItem alloc] initWithType:0];
+  item.email = email;
+  item.avatar = [[UIImage alloc] init];
+
+  id cell = [[[item cellClass] alloc] init];
+  ASSERT_TRUE([cell isMemberOfClass:[TargetAccountCell class]]);
+
+  TargetAccountCell* targetAccountCell =
+      base::mac::ObjCCastStrict<TargetAccountCell>(cell);
+  EXPECT_FALSE(targetAccountCell.emailLabel.text);
+  EXPECT_FALSE(targetAccountCell.avatarBadge.image);
+
+  [item configureCell:cell withStyler:[[ChromeTableViewStyler alloc] init]];
+  EXPECT_NSEQ(email, targetAccountCell.emailLabel.text);
+  EXPECT_FALSE(targetAccountCell.avatarBadge.isHidden);
+}
