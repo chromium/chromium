909adcb5b00131b5b18a029d47d8826421d8867f
Nicholas Hollingum <hollingum@google.com>
2023-06-06 00:39:28 +0000

    borealis: force dark mode in the splash screen
    Elements such as the beta badge and spinner would still respond to
    light/dark mode. their light-mode colors don't look good against the
    dark grey custom background.
    
    We force dark mode for the splash screen, so views chooses better
    colors.
    
    Bug: b:268143336
    Change-Id: Ia220dbf735769bae306121a18833d35110a1dd9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4569966
    Reviewed-by: Lucy Qu <lqu@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1153594}

--- chrome/browser/ui/views/borealis/borealis_splash_screen_view.cc
+++ chrome/browser/ui/views/borealis/borealis_splash_screen_view.cc
@@ -22,6 +22,7 @@
 #include "chrome/grit/generated_resources.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/message_box_view.h"
@@ -60,6 +61,11 @@ void BorealisSplashScreenView::Show(Profile* profile) {
     g_delegate->UpdateColors();
     g_delegate->GetWidget()->GetNativeWindow()->SetProperty(
         ash::kShelfIDKey, ash::ShelfID(borealis::kInstallerAppId).Serialize());
+    // Override the widget to be dark-mode permanently.
+    // This UI has custom colors to match Steam's and those are close to ash's
+    // dark mode.
+    g_delegate->GetWidget()->SetColorModeOverride(
+        {ui::ColorProviderManager::ColorMode::kDark});
   }
   g_delegate->GetWidget()->Show();
 }
