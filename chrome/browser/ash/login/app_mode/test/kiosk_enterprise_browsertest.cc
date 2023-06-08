d63b7d5ad66df54a00f95bcebace31bd10d6bdb9
Rakina Zata Amni <rakina@chromium.org>
2023-06-08 02:05:32 +0000

    Revert "Re-enable LaunchingAppThatRequiresNetworkWhilstOfflineShouldShowNetworkScreen"
    This reverts commit cff67e9bedc6e9a0c37c87e7ec436b4fabe24367.
    
    Reason for revert: Still flakily fails, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/51633/overview
    
    Original change's description:
    > Re-enable LaunchingAppThatRequiresNetworkWhilstOfflineShouldShowNetworkScreen
    >
    > This test flakily failed at the WaitForOobeScreen step, because we jumped straight into the network screen instead of holding on the splash screen for 10 seconds as expected.
    >
    > The reason we jumped straight into the network screen is the following callpath happening during installation:
    > NetworkUiController::InitializeNetwork
    >   -> AppLaunchSplashScreenHandler::UpdateAppLaunchState
    >   -> NetworkUiController::OnNetworkStateChanged
    >   -> KioskLaunchController::OnNetworkLost
    >   -> NetworkUiController::OnNetworkLostDuringInstallation
    >   -> NetworkUiController::ShowNetworkConfigureUI.
    >
    > This was fixed by https://crrev.com/c/4583229 by blocking the call from AppLaunchSplashScreenHandler::UpdateAppLaunchState to NetworkUiController::OnNetworkStateChanged.
    >
    > Bug: b:284000771
    > Test: LaunchingAppThatRequiresNetworkWhilstOfflineShouldShowNetworkScreen
    > Change-Id: I689856f33dcc40d6f9ef5f5f58166cc2f3d02ac3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4556910
    > Reviewed-by: Irina Fedorova <irfedorova@google.com>
    > Commit-Queue: Irina Fedorova <irfedorova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1154401}
    
    Bug: b:284000771
    Change-Id: I90186cbebc4558631e102edd0ae37c3197572a28
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600468
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Owners-Override: Rakina Zata Amni <rakina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154724}

--- chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
@@ -260,7 +260,7 @@ IN_PROC_BROWSER_TEST_F(KioskEnterpriseTest,
 
 IN_PROC_BROWSER_TEST_F(
     KioskEnterpriseTest,
-    LaunchingAppThatRequiresNetworkWhilstOfflineShouldShowNetworkScreen) {
+    DISABLED_LaunchingAppThatRequiresNetworkWhilstOfflineShouldShowNetworkScreen) {
   ScopedCanConfigureNetwork can_configure_network(true);
 
   // Start app launch with network portal state.
