581f50be9fc9f4d59682cf9ea851550122089e2a
Georg Neis <neis@chromium.org>
2023-06-08 23:51:32 +0000

    kiosk_utils.cc: #include own header
    The missing include is causing trouble at least for Code Search.
    See https://g3doc.corp.google.com/devtools/scythe/g3doc/known-issues.md?cl=head#function-identification
    
    Bug: b:286172230
    Change-Id: I6a0e2b2c38b065fe82cfcfae06eb23b51bfeaf96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600408
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155236}

--- chromeos/components/kiosk/kiosk_utils.cc
+++ chromeos/components/kiosk/kiosk_utils.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chromeos/components/kiosk/kiosk_utils.h"
+
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
