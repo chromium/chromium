f58c9efc6932bbdf5f037baaaf16de1dacbc2240
Nicolas MacBeth <nicolasmacbeth@google.com>
2023-06-08 15:47:49 +0000

    [Blue dot promo] Enabled by default and changed default value of flag
    Setting the default browser blue dot promo flag to enabled by default
    and set the default value to `AllDBPromosEnabled`.
    
    Bug: 1410229
    Change-Id: I993afe7778b261345257e2cd7f12407184bc9f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599057
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154930}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -12,11 +12,7 @@
 
 BASE_FEATURE(kDefaultBrowserBlueDotPromo,
              "DefaultBrowserBlueDotPromo",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kIOSPaymentsBottomSheet,
-             "IOSPaymentsBottomSheet",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 constexpr base::FeatureParam<BlueDotPromoUserGroup>::Option
     kBlueDotPromoUserGroupOptions[] = {
@@ -27,8 +23,11 @@ constexpr base::FeatureParam<BlueDotPromoUserGroup>::Option
 
 constexpr base::FeatureParam<BlueDotPromoUserGroup> kBlueDotPromoUserGroupParam{
     &kDefaultBrowserBlueDotPromo, "user-group",
-    BlueDotPromoUserGroup::kOnlyBlueDotPromoEnabled,
-    &kBlueDotPromoUserGroupOptions};
+    BlueDotPromoUserGroup::kAllDBPromosEnabled, &kBlueDotPromoUserGroupOptions};
+
+BASE_FEATURE(kIOSPaymentsBottomSheet,
+             "IOSPaymentsBottomSheet",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExpandedTabStrip,
              "ExpandedTabStrip",
