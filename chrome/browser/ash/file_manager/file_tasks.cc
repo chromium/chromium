389eb1242f7072d0a112d560963545b21d9291c0
Joel Hockey <joelhockey@chromium.org>
2023-06-08 06:34:06 +0000

    Revert "Hold off drive offline UMAs until DriveFS 76 is released"
    This reverts commit 92982fb2e23106ea37fae7c0b7b6ad0d57b78f90.
    
    Reason for revert: DriveFS 76 now landed:
    https://chrome-internal-review.googlesource.com/c/chromeos/platform/drivefs-google3/+/5979354
    
    Original change's description:
    > Hold off drive offline UMAs until DriveFS 76 is released
    >
    > We need to wait until DriveFS 76 is released with mojo implementation.
    >
    > Bug: b/281199375
    > Change-Id: Ib300a2fae7ad543eb96552810e40317961d2f721
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4570023
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Reviewed-by: Ben Reich <benreich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149549}
    
    Bug: b/281199375
    Change-Id: Ib0bf9b83184843f74644efd06311ffe31073400a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600414
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154783}

--- chrome/browser/ash/file_manager/file_tasks.cc
+++ chrome/browser/ash/file_manager/file_tasks.cc
@@ -494,7 +494,6 @@ bool ExecuteOpenInOfficeTask(Profile* profile,
       modal_parent);
 }
 
-/*
 void RecordDriveOfflineUMAsGotDocsOfflineStats(
     bool open_available,
     drive::FileError error,
@@ -566,7 +565,7 @@ void RecordDriveOfflineUMAs(Profile* profile,
     }
   }
 }
-*/
+
 }  // namespace
 
 ResultingTasks::ResultingTasks() = default;
@@ -871,8 +870,7 @@ bool ExecuteFileTask(Profile* profile,
   // TODO(crbug.com/1005640): Move recording this metric to the App Service when
   // file handling is supported there.
   apps::RecordAppLaunch(task.app_id, apps::LaunchSource::kFromFileManager);
-  // TODO(b/281199375): Hold off Drive offline UMAs until DriveFS 76.
-  // RecordDriveOfflineUMAs(profile, file_urls);
+  RecordDriveOfflineUMAs(profile, file_urls);
 
   if (auto* notifier = FileTasksNotifier::GetForProfile(profile)) {
     notifier->NotifyFileTasks(file_urls);
