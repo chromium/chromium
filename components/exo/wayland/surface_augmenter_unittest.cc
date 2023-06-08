56c7a68d81d831f0a6e23562089d25e012005ada
Andrea Orru <andreaorru@chromium.org>
2023-06-08 03:01:34 +0000

    Refactor surface_augmenter unit tests
    This change updates the surface_augmenter unit tests to follow the
    pattern of keeping client's data (including wl_surface handles)
    in a ClientData structure.
    
    Before this, part of the data was being kept in the outside scope,
    and part in ClientData.
    
    Change-Id: I04af2656cf09fae9b386a9f6d4e96744df69a475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600410
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154738}

--- components/exo/wayland/surface_augmenter_unittest.cc
+++ components/exo/wayland/surface_augmenter_unittest.cc
@@ -19,13 +19,20 @@ class ClientData : public test::TestClient::CustomData {
  public:
   // Don't leak augmented (sub)surface objects.
   ~ClientData() override {
-    if (augmented_surface_)
-      augmented_surface_destroy(augmented_surface_);
-    if (augmented_sub_surface_)
-      augmented_sub_surface_destroy(augmented_sub_surface_);
+    if (augmented_surface) {
+      augmented_surface_destroy(augmented_surface);
+    }
+    if (augmented_sub_surface) {
+      augmented_sub_surface_destroy(augmented_sub_surface);
+    }
   }
-  raw_ptr<augmented_surface, ExperimentalAsh> augmented_surface_ = nullptr;
-  raw_ptr<augmented_sub_surface, ExperimentalAsh> augmented_sub_surface_ =
+
+  std::unique_ptr<wl_surface> parent_wl_surface;
+  std::unique_ptr<wl_surface> child_wl_surface;
+  std::unique_ptr<wl_subsurface> child_wl_subsurface;
+
+  raw_ptr<augmented_surface, ExperimentalAsh> augmented_surface = nullptr;
+  raw_ptr<augmented_sub_surface, ExperimentalAsh> augmented_sub_surface =
       nullptr;
 };
 
@@ -33,12 +40,14 @@ using SurfaceAugmenterTest = test::WaylandServerTest;
 
 TEST_F(SurfaceAugmenterTest, AugmentedSubSurfacesDontSendLeaveEnter) {
   // Create a surface.
-  std::unique_ptr<wl_surface> parent_wl_surface;
   test::ResourceKey parent_surface_key;
   PostToClientAndWait([&](test::TestClient* client) {
-    parent_wl_surface.reset(wl_compositor_create_surface(client->compositor()));
+    auto data = std::make_unique<ClientData>();
+    data->parent_wl_surface.reset(
+        wl_compositor_create_surface(client->compositor()));
     parent_surface_key =
-        test::client_util::GetResourceKey(parent_wl_surface.get());
+        test::client_util::GetResourceKey(data->parent_wl_surface.get());
+    client->set_data(std::move(data));
   });
   Surface* parent_surface = test::server_util::GetUserDataForResource<Surface>(
       server_.get(), parent_surface_key);
@@ -47,20 +56,20 @@ TEST_F(SurfaceAugmenterTest, AugmentedSubSurfacesDontSendLeaveEnter) {
   EXPECT_TRUE(parent_surface->HasLeaveEnterCallbackForTesting());
   // Augment the surface and check that it still sends enter/leave events.
   PostToClientAndWait([&](test::TestClient* client) {
-    auto data = std::make_unique<ClientData>();
-    data->augmented_surface_ = surface_augmenter_get_augmented_surface(
-        client->surface_augmenter(), parent_wl_surface.get());
-    client->set_data(std::move(data));
+    ClientData* data = client->GetDataAs<ClientData>();
+    data->augmented_surface = surface_augmenter_get_augmented_surface(
+        client->surface_augmenter(), data->parent_wl_surface.get());
   });
   EXPECT_TRUE(parent_surface->HasLeaveEnterCallbackForTesting());
 
   // Create another surface.
-  std::unique_ptr<wl_surface> child_wl_surface;
   test::ResourceKey child_surface_key;
   PostToClientAndWait([&](test::TestClient* client) {
-    child_wl_surface.reset(wl_compositor_create_surface(client->compositor()));
+    ClientData* data = client->GetDataAs<ClientData>();
+    data->child_wl_surface.reset(
+        wl_compositor_create_surface(client->compositor()));
     child_surface_key =
-        test::client_util::GetResourceKey(child_wl_surface.get());
+        test::client_util::GetResourceKey(data->child_wl_surface.get());
   });
   Surface* child_surface = test::server_util::GetUserDataForResource<Surface>(
       server_.get(), child_surface_key);
@@ -69,14 +78,14 @@ TEST_F(SurfaceAugmenterTest, AugmentedSubSurfacesDontSendLeaveEnter) {
   EXPECT_TRUE(child_surface->HasLeaveEnterCallbackForTesting());
 
   // Make it a subsurface of the first one.
-  std::unique_ptr<wl_subsurface> child_wl_subsurface;
   test::ResourceKey child_subsurface_key;
   PostToClientAndWait([&](test::TestClient* client) {
-    child_wl_subsurface.reset(wl_subcompositor_get_subsurface(
-        client->subcompositor(), child_wl_surface.get(),
-        parent_wl_surface.get()));
+    ClientData* data = client->GetDataAs<ClientData>();
+    data->child_wl_subsurface.reset(wl_subcompositor_get_subsurface(
+        client->subcompositor(), data->child_wl_surface.get(),
+        data->parent_wl_surface.get()));
     child_subsurface_key =
-        test::client_util::GetResourceKey(child_wl_subsurface.get());
+        test::client_util::GetResourceKey(data->child_wl_subsurface.get());
   });
   SubSurface* child_subsurface =
       test::server_util::GetUserDataForResource<SubSurface>(
@@ -89,8 +98,8 @@ TEST_F(SurfaceAugmenterTest, AugmentedSubSurfacesDontSendLeaveEnter) {
   // Augment the subsurface and check that it doesn't send the events anymore.
   PostToClientAndWait([&](test::TestClient* client) {
     ClientData* data = client->GetDataAs<ClientData>();
-    data->augmented_sub_surface_ = surface_augmenter_get_augmented_subsurface(
-        client->surface_augmenter(), child_wl_subsurface.get());
+    data->augmented_sub_surface = surface_augmenter_get_augmented_subsurface(
+        client->surface_augmenter(), data->child_wl_subsurface.get());
   });
   EXPECT_FALSE(child_surface->HasLeaveEnterCallbackForTesting());
   EXPECT_FALSE(child_subsurface->surface()->HasLeaveEnterCallbackForTesting());
