99f829794f660563487257f91c8376e0e9936103
Colin Blundell <blundell@chromium.org>
2023-06-08 12:09:01 +0000

    [//content][Pepper] Tighten type of ivar holding VideoDecoderShim
    Have PepperVideoDecoderHost have its VDA ivar as a VideoDecoderShim
    rather than a media::VideoDecodeAccelerator to make it explicit that
    the ivar can only ever be a VideoDecoderShim.
    
    Change-Id: If0e64c3b802c9472282544bb69ab4e06d6f4f2b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4599151
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1154855}

--- content/renderer/pepper/pepper_video_decoder_host.h
+++ content/renderer/pepper/pepper_video_decoder_host.h
@@ -13,8 +13,8 @@
 #include <set>
 #include <vector>
 
+#include "content/renderer/pepper/video_decoder_shim.h"
 #include "gpu/command_buffer/common/mailbox.h"
-#include "media/video/video_decode_accelerator.h"
 #include "ppapi/c/pp_codecs.h"
 #include "ppapi/host/host_message_context.h"
 #include "ppapi/host/resource_host.h"
@@ -129,7 +129,7 @@ class PepperVideoDecoderHost : public ppapi::host::ResourceHost,
 
   media::VideoCodecProfile profile_;
 
-  std::unique_ptr<media::VideoDecodeAccelerator> decoder_;
+  std::unique_ptr<VideoDecoderShim> decoder_;
 
   bool software_fallback_allowed_ = false;
   bool software_fallback_used_ = false;
