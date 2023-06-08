cf3f665746dbb27706e70f4c61aac926b2798745
Luc Nguyen <lucnguyen@google.com>
2023-06-08 02:17:24 +0000

    Add ScopedTerminationChecker to quantify ungraceful exits
    In particular, on Android, when Chrome is backgrounded, we close the
    current log synchronously. Chrome may be killed during this time (e.g.
    user swipes the app, or OS wants to immediately reclaim some memory),
    which can cause data loss due to histograms being snapshotted and put
    into a log, but the log never being fully finalized and stored.
    
    (If Chrome is killed before we start finalizing the log, it's fine
    because all the data will be in the persistent file. If Chrome is
    killed after finalizing/storing the log, it's fine because the log
    will be stored in Local State).
    
    This CL emits a histogram before we close the log, and after we
    finished closing the log (and storing it). So the "before" and
    "after" buckets should match in count if Chrome is never killed
    during this time. But there are some flaws with this solution:
     - There may be a lagging effect since the "after" sample won't be in
       the same log as the "before" histogram.
     - There may be some bias if the latter sample, for some reason, is
       more likely to be lost. I try to protect against this by making
       ScopedTerminationChecker a no-op if the persistent histogram system
       is not in use in the current session.
    (So it seems to me that if there are problems with this histogram, it
    would be about undercounting the "after" bucket).
    
    I plan to check the data quickly. If the data looks good (i.e. the
    "before" and "after" buckets are pretty much equal so there is nothing
    much to worry about), then I will leave this as is since it's "good"
    enough. Otherwise, if they are not equal, I will implement a more
    sophisticated checker to confirm that it is accurate. In particular,
    before closing the log, allocate some small object on the persistent
    memory that contains a bool that is set to false. After closing the
    log, set this bool to true. Then the consumer of the PMA file will
    take care of emitting some histogram that indicates whether the
    ScopedTerminationChecker fully completed.
    
    This is in preparation of merging subprocess metrics on backgrounded.
    The reason why I'm doing this is because I am adding extra work to
    the "OnBackgrounded" handler, so it could possibly make the data loss
    window larger?
    
    Bug: 1293026
    Change-Id: I75de6d8536dbba8d0db03156e1190cfa62419a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4580610
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154727}

--- components/metrics/metrics_service.cc
+++ components/metrics/metrics_service.cc
@@ -213,6 +213,69 @@ class DiscardingFlattener : public base::HistogramFlattener {
   }
 };
 
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+// Emits a histogram upon instantiation, and on destruction. Used to measure how
+// often the browser is ungracefully killed between two different points. In
+// particular, currently, this is used on mobile to measure how often the
+// browser is killed while finalizing a log, right after backgrounding. This
+// scenario is prone to data loss because a histogram may have been snapshotted
+// and put into a log, but the browser was killed before it could be fully
+// finalized and stored.
+//
+// TODO(crbug/1293026): Consider improving this. In particular, the "Started"
+// bucket is emitted before finalizing the log, and the "Finished" bucket is
+// emitted after. Hence, the latter will be reported in a different log, which
+// may cause a "lag" and/or bias (e.g. if the latter log is more prone to loss).
+// A better way to do this is to allocate an object on the persistent memory
+// upon instantiation, and flip a bit in it upon destruction. A future session
+// that will consume this persistent memory should take care of emitting the
+// histogram samples.
+class ScopedTerminationChecker {
+ public:
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  enum class Status {
+    kStarted = 0,
+    kFinished = 1,
+    kMaxValue = kFinished,
+  };
+
+  explicit ScopedTerminationChecker(base::StringPiece histogram_name) {
+    // Do nothing if the persistent histogram system is not being used.
+    // Otherwise, the "Finished" bucket may be more prone to loss, which may
+    // incorrectly make it seem like the browser was killed in between the
+    // scoped code.
+    if (!base::GlobalHistogramAllocator::Get()) {
+      return;
+    }
+
+    active_ = true;
+    histogram_name_ = histogram_name;
+    base::UmaHistogramEnumeration(histogram_name_, Status::kStarted);
+  }
+
+  ScopedTerminationChecker(const ScopedTerminationChecker& other) = delete;
+  ScopedTerminationChecker& operator=(const ScopedTerminationChecker& other) =
+      delete;
+
+  ~ScopedTerminationChecker() {
+    if (!active_) {
+      return;
+    }
+    base::UmaHistogramEnumeration(histogram_name_, Status::kFinished);
+  }
+
+ private:
+  // Name of the histogram to emit to upon instantiation/destruction.
+  std::string histogram_name_;
+
+  // Whether or not this will emit histograms. In particular, if this browser
+  // session does not make use of persistent memory, this will be false, and
+  // this object will do nothing.
+  bool active_ = false;
+};
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+
 // The delay, in seconds, after starting recording before doing expensive
 // initialization work.
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
@@ -532,8 +595,12 @@ void MetricsService::OnAppEnterBackground(bool keep_recording_in_background) {
     base::UmaHistogramBoolean(
         "UMA.MetricsService.PendingOngoingLogOnBackgrounded",
         pending_ongoing_log_);
-    PushPendingLogsToPersistentStorage(
-        MetricsLogsEventManager::CreateReason::kBackgrounded);
+    {
+      ScopedTerminationChecker scoped_termination_checker(
+          "UMA.MetricsService.OnBackgroundedScopedTerminationChecker");
+      PushPendingLogsToPersistentStorage(
+          MetricsLogsEventManager::CreateReason::kBackgrounded);
+    }
     // Persisting logs closes the current log, so start recording a new log
     // immediately to capture any background work that might be done before the
     // process is killed.
