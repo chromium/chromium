d9069944511f0db870a21cc7dce95e14ee88cd61
Marco Georgaklis <mgeorgaklis@google.com>
2023-06-08 05:13:52 +0000

    Added new histogram checks to companion trigger test.
    Bug: b:273308580
    Change-Id: I282d2961e0bf8f0276e14bf9bd2ee6c8bf7e651c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4592109
    Commit-Queue: Marco Georgaklis <mgeorgaklis@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1154773}

--- tools/perf/page_sets/companion/basic_companion_story.py
+++ tools/perf/page_sets/companion/basic_companion_story.py
@@ -6,6 +6,8 @@ from page_sets.desktop_ui.multitab_story import MultiTabStory
 from page_sets.desktop_ui.ui_devtools_utils import ClickOn, PressKey
 from page_sets.companion.browser_element_identifiers import \
     kSidePanelButtonElementId, kSidePanelComboboxElementId
+from page_sets.companion.histograms import _CQ, _PROMO_EVENT, _STARTUP, \
+    _ZERO_STATE
 from page_sets.login_helpers import google_login
 
 _GOOGLE_URL = "https://www.google.com/"
@@ -13,8 +15,6 @@ _TEST_SEARCH_URL = "https://www.google.com/search?q=test"
 _TEST_PAGE_URL = "https://www.westelm.com"
 _BLANK_URL = "about:blank"
 _COMPANION_URL = "https://lens.google.com/companion?pli=1"
-_STARTUP_HISTOGRAM = "Startup.BrowserMainRunnerImplInitializeLongTime"
-_ZERO_STATE_HISTOGRAM = "SidePanel.Companion.ShowTriggered"
 
 
 class CompanionStory(MultiTabStory):
@@ -35,37 +35,39 @@ class CompanionStory(MultiTabStory):
 
   def OpenCompanion(self, action_runner):
     self.ToggleSidePanel(action_runner)
-    combobox_node_id = self._devtools.QueryNodes('id:%s' %
+    combobox_node_id = self._devtools.QueryNodes("id:%s" %
                                                  kSidePanelComboboxElementId)[0]
-    PressKey(self._devtools, combobox_node_id, ' ')
-    window_node_id = self._devtools.QueryNodes('<Window>')[0]
-    PressKey(self._devtools, window_node_id, 'ArrowUp')
-    PressKey(self._devtools, window_node_id, 'Return')
+    PressKey(self._devtools, combobox_node_id, " ")
+    window_node_id = self._devtools.QueryNodes("<Window>")[0]
+    PressKey(self._devtools, window_node_id, "ArrowUp")
+    PressKey(self._devtools, window_node_id, "Return")
 
   def LogIn(self, action_runner):
-    google_login.NewLoginGoogleAccount(action_runner, 'intelligence')
+    google_login.NewLoginGoogleAccount(action_runner, "intelligence")
 
   def SanityHistogramCheck(self, action_runner):
     action_runner.tab.Navigate(_GOOGLE_URL)
     action_runner.Wait(1)
     assert action_runner.tab.url == _GOOGLE_URL, "%s" % action_runner.tab.url
-    histogram = self.FetchHistogram(action_runner, _STARTUP_HISTOGRAM)
-    assert histogram['count'] == 1
+    histogram = self.FetchHistogram(action_runner, _STARTUP)
+    assert histogram["count"] == 1
 
-  def ZeroStateHistogramCheck(self, action_runner):
-    histogram = self.FetchHistogram(action_runner, _ZERO_STATE_HISTOGRAM)
-    assert histogram, 'Companion zero state histogram is non-existent'
+  def ConductHistogramCheck(self,
+                            action_runner,
+                            histogram_name,
+                            expected_count=None):
+    histogram = self.FetchHistogram(action_runner, histogram_name)
+    assert histogram, "%s histogram is non-existent" % histogram_name
+
+    if expected_count:
+      assert histogram['count'] == expected_count, \
+          "Expected %s, got %s for histogram %s" % (expected_count,
+                                                     histogram['count'],
+                                                     histogram_name)
 
   def FetchHistogram(self, action_runner, name):
     js = "statsCollectionController.getBrowserHistogram('%s');" % name
     return json.loads(action_runner.EvaluateJavaScript(js))
-
-  def PrimeCompanion(self, action_runner):
-    # TODO(b:280820634) Remove this once issue has been resolved
-    action_runner.tab.Navigate(_TEST_PAGE_URL)
-    action_runner.Wait(2)
-    action_runner.tab.Navigate(_COMPANION_URL)
-    action_runner.Wait(5)
 
   def InteractWithPage(self, action_runner):
     pass
@@ -77,6 +79,19 @@ class CompanionStoryBasic(CompanionStory):
   def InteractWithPage(self, action_runner):
     self.SanityHistogramCheck(action_runner)
     self.LogIn(action_runner)
-    self.PrimeCompanion(action_runner)
+    action_runner.Wait(1)
+    action_runner.tab.Navigate(_TEST_PAGE_URL)
+    action_runner.Wait(3)
+
+    # Open companion and ensure features loaded.
     self.OpenCompanion(action_runner)
-    self.ZeroStateHistogramCheck(action_runner)
+    self.ConductHistogramCheck(action_runner, _ZERO_STATE)
+    self.ConductHistogramCheck(action_runner, _PROMO_EVENT)
+    self.ConductHistogramCheck(action_runner, _CQ)
+
+
+class CompanionStoryBasicWithSearch(CompanionStory):
+  NAME = 'companion:basic_search'
+
+  def InteractWithPage(self, action_runner):
+    self.SanityHistogramCheck(action_runner)
