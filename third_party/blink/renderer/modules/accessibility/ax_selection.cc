7d87c2d9a8c6fbc717828654dd0edd32d7bcd55b
Aaron Leventhal <aleventhal@google.com>
2023-06-09 16:32:52 +0000

    [A11y] Do not assert on invalid selections in AXSelection::Select()
    Do not assert here; after all, by the time the selection action is registered, content could have changed from what the AT was basing the action on.
    
    Bug: 1450839
    Change-Id: Ie912a3c310bb11fff3c077bbb808e09cb95568fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4588573
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155565}

--- third_party/blink/renderer/modules/accessibility/ax_selection.cc
+++ third_party/blink/renderer/modules/accessibility/ax_selection.cc
@@ -349,10 +349,8 @@ void AXSelection::UpdateSelectionIfNecessary() {
 
 bool AXSelection::Select(const AXSelectionBehavior selection_behavior) {
   if (!IsValid()) {
-    // TODO(crbug.com/1409881): Make sure this no longer fires then turn this
-    // block into CHECK(IsValid());
-    DUMP_WILL_BE_NOTREACHED_NORETURN()
-        << "Trying to select an invalid accessibility selection.";
+    // By the time the selection action gets here, content could have
+    // changed from the content the action was initially prepared for.
     return false;
   }
 
@@ -425,9 +423,9 @@ bool AXSelection::Select(const AXSelectionBehavior selection_behavior) {
 }
 
 String AXSelection::ToString() const {
-  if (!IsValid())
-    return "Invalid AXSelection";
-  return "AXSelection from " + Base().ToString() + " to " + Extent().ToString();
+  String prefix = IsValid() ? "" : "Invalid ";
+  return prefix + "AXSelection from " + Base().ToString() + " to " +
+         Extent().ToString();
 }
 
 absl::optional<AXSelection::TextControlSelection>
