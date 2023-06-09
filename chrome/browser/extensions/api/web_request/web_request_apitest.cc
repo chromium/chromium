99169348cdb2fd9eebb8859e4c2aaba469049b5a
Takashi Sakamoto <tasak@google.com>
2023-06-09 03:13:14 +0000

    Sheriff: disable PersistentBackground/ExtensionWebRequestApiTestWithContextType.WebRequestSubresource tests on Mac, Linux and ChromeOS.
    The tests are flaky on Mac, Linux and ChromeOS.
    
    Bug: 1453477
    Change-Id: I0f98d71b14516f63391caa8795020857551d6bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602282
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1155322}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -959,8 +959,8 @@ IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
 // Slower and flaky tests should be isolated in the "slow" group of tests in
 // the JS file. This prevents losing test coverage for those tests that are
 // not causing timeouts and flakes.
-#if BUILDFLAG(IS_WIN) || !defined(NDEBUG) || defined(ADDRESS_SANITIZER) || \
-    defined(MEMORY_SANITIZER)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || !defined(NDEBUG) || \
+    defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER)
 #define MAYBE_WebRequestBlockingSlow DISABLED_WebRequestBlockingSlow
 #else
 #define MAYBE_WebRequestBlockingSlow WebRequestBlockingSlow
@@ -1087,15 +1087,30 @@ IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
       << message_;
 }
 
+// TODO(crbug.com/1453477): test is flaky on linux, mac and chromeos.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_WebRequestSubresourceRedirects \
+  DISABLED_WebRequestSubresourceRedirects
+#else
+#define MAYBE_WebRequestSubresourceRedirects WebRequestSubresourceRedirects
+#endif
 IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
-                       WebRequestSubresourceRedirects) {
+                       MAYBE_WebRequestSubresourceRedirects) {
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webrequest/test_subresource_redirects"))
       << message_;
 }
 
+// TODO(crbug.com/1453477): test is flaky on linux, mac and chromeos.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#define MAYBE_WebRequestSubresourceRedirectsWithExtraHeaders \
+  DISABLED_WebRequestSubresourceRedirectsWithExtraHeaders
+#else
+#define MAYBE_WebRequestSubresourceRedirectsWithExtraHeaders \
+  WebRequestSubresourceRedirectsWithExtraHeaders
+#endif
 IN_PROC_BROWSER_TEST_P(ExtensionWebRequestApiTestWithContextType,
-                       WebRequestSubresourceRedirectsWithExtraHeaders) {
+                       MAYBE_WebRequestSubresourceRedirectsWithExtraHeaders) {
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webrequest/test_subresource_redirects",
                                {.custom_arg = R"({"useExtraHeaders": true})"}))
