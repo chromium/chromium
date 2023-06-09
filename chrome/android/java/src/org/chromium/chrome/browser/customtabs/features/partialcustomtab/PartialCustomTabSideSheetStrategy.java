afccbe9758203f890c63a3fc7850d4c6b61fd3c8
Zach Katz <katzz@google.com>
2023-06-09 02:26:37 +0000

    [PCCT-SideSheet] Remove handle view in fullscreen
    - Also add a constant for UI delay used on Side Sheet.
    
    
    (cherry picked from commit 7ac95842c03e6c4dc2df852f05a79e82c02a33bd)
    
    Bug: 1450687
    Change-Id: I29e3cb787ebb0701b994c879762ddc0b6c0811e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595932
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zach Katz <katzz@google.com>
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1154472}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4602433
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5790@{#515}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
@@ -48,6 +48,7 @@ import org.chromium.ui.base.LocalizationUtils;
  */
 public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrategy {
     private static final int WINDOW_WIDTH_EXPANDED_CUTOFF_DP = 840;
+    private static final int SIDE_SHEET_UI_DELAY = 20;
     private static final float MINIMAL_WIDTH_RATIO_EXPANDED = 0.33f;
     private static final float MINIMAL_WIDTH_RATIO_MEDIUM = 0.5f;
     private static final NoAnimator NO_ANIMATOR = new NoAnimator();
@@ -194,7 +195,8 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
         if (visible) {
             // Set a slight delay in restoring the view to hide the visual glitch caused by
             // the resized web contents.
-            new Handler().postDelayed(() -> content.setVisibility(View.VISIBLE), 20);
+            new Handler().postDelayed(
+                    () -> content.setVisibility(View.VISIBLE), SIDE_SHEET_UI_DELAY);
         } else {
             content.setVisibility(View.INVISIBLE);
         }
@@ -219,7 +221,7 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
                 if (shouldDrawDividerLine()) drawDividerLine();
                 // We have a delay before showing the resized web contents so it has to be done
                 // for the shadow as well.
-                new Handler().postDelayed(this::updateShadowOffset, 20);
+                new Handler().postDelayed(this::updateShadowOffset, SIDE_SHEET_UI_DELAY);
                 maybeInvokeResizeCallback();
             });
         }
@@ -247,7 +249,7 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
 
     @Override
     protected int getHandleHeight() {
-        return mRoundedCornersPosition == ACTIVITY_SIDE_SHEET_ROUNDED_CORNERS_NONE
+        return isFullscreen() || mRoundedCornersPosition == ACTIVITY_SIDE_SHEET_ROUNDED_CORNERS_NONE
                 ? 0
                 : mToolbarCornerRadius;
     }
