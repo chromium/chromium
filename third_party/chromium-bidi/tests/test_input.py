40ddef52c9813a3a31fb99e7ce30c1818726297e
jrandolf <101637635+jrandolf@users.noreply.github.com>
2023-06-12 15:00:38 +0200

    Use `pytest.mark.skip` and `html` fixture in input tests (#839)

--- third_party/chromium-bidi/tests/test_input.py
+++ third_party/chromium-bidi/tests/test_input.py
@@ -17,7 +17,7 @@ import pytest
 from anys import ANY_STR
 from test_helpers import execute_command, goto_url
 
-SCRIPT_HTML = 'data:text/html,' + """
+SCRIPT = """
 <div style="height: 2000px; width: 10px"></div>
 <script>
     var allEvents = [];
@@ -70,8 +70,9 @@ SCRIPT_HTML = 'data:text/html,' + """
 
 
 @pytest.mark.asyncio
-async def test_input_performActionsEmitsKeyboardEvents(websocket, context_id):
-    await goto_url(websocket, context_id, SCRIPT_HTML)
+async def test_input_performActionsEmitsKeyboardEvents(websocket, context_id,
+                                                       html):
+    await goto_url(websocket, context_id, html(SCRIPT))
 
     await execute_command(
         websocket, {
@@ -171,8 +172,9 @@ async def test_input_performActionsEmitsKeyboardEvents(websocket, context_id):
 
 
 @pytest.mark.asyncio
-async def test_input_performActionsEmitsPointerEvents(websocket, context_id):
-    await goto_url(websocket, context_id, SCRIPT_HTML)
+async def test_input_performActionsEmitsPointerEvents(websocket, context_id,
+                                                      html):
+    await goto_url(websocket, context_id, html(SCRIPT))
 
     await execute_command(
         websocket, {
@@ -275,111 +277,114 @@ async def test_input_performActionsEmitsPointerEvents(websocket, context_id):
     } == result
 
 
-# TODO(jrandolf): Investigate mouse wheel flakiness. The deltaY sometimes
-# doubles and other times not. It is also not independent of the mouse.
-# @pytest.mark.asyncio
-# async def test_input_performActionsEmitsWheelEvents(websocket, context_id):
-#     await goto_url(websocket, context_id, SCRIPT_HTML)
+@pytest.mark.skip(reason="""
+        TODO(jrandolf): Investigate mouse wheel flakiness. The deltaY sometimes
+        doubles and other times not. It is also not independent of the mouse.
+    """)
+@pytest.mark.asyncio
+async def test_input_performActionsEmitsWheelEvents(websocket, context_id,
+                                                    html):
+    await goto_url(websocket, context_id, html(SCRIPT))
 
-#     await execute_command(
-#         websocket, {
-#             "method": "input.performActions",
-#             "params": {
-#                 "context": context_id,
-#                 "actions": [{
-#                     "type": "pointer",
-#                     "id": "main2",
-#                     "actions": [{
-#                         "type": "pointerDown",
-#                         "button": 0,
-#                     }, {
-#                         "type": "pointerUp",
-#                         "button": 0,
-#                     }]
-#                 }, {
-#                     "type": "wheel",
-#                     "id": "main",
-#                     "actions": [{
-#                         "type": "scroll",
-#                         "x": 0,
-#                         "y": 0,
-#                         "deltaX": 0,
-#                         "deltaY": 5,
-#                     }]
-#                 }]
-#             }
-#         })
+    await execute_command(
+        websocket, {
+            "method": "input.performActions",
+            "params": {
+                "context": context_id,
+                "actions": [{
+                    "type": "pointer",
+                    "id": "main2",
+                    "actions": [{
+                        "type": "pointerDown",
+                        "button": 0,
+                    }, {
+                        "type": "pointerUp",
+                        "button": 0,
+                    }]
+                }, {
+                    "type": "wheel",
+                    "id": "main",
+                    "actions": [{
+                        "type": "scroll",
+                        "x": 0,
+                        "y": 0,
+                        "deltaX": 0,
+                        "deltaY": 5,
+                    }]
+                }]
+            }
+        })
 
-#     result = await execute_command(
-#         websocket, {
-#             "method": "script.evaluate",
-#             "params": {
-#                 "expression": "allEvents",
-#                 "awaitPromise": False,
-#                 "resultOwnership": "root",
-#                 "target": {
-#                     "context": context_id
-#                 }
-#             }
-#         })
+    result = await execute_command(
+        websocket, {
+            "method": "script.evaluate",
+            "params": {
+                "expression": "allEvents",
+                "awaitPromise": False,
+                "resultOwnership": "root",
+                "target": {
+                    "context": context_id
+                }
+            }
+        })
 
-#     assert {
-#         'type': 'success',
-#         'result': {
-#             'type': 'array',
-#             'value': [{
-#                 'type': 'object',
-#                 'value': [['event', {
-#                     'type': 'string',
-#                     'value': 'mousedown'
-#                 }], ['button', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['buttons', {
-#                     'type': 'number',
-#                     'value': 1
-#                 }], ['clientX', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['clientY', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }]]
-#             }, {
-#                 'type': 'object',
-#                 'value': [['event', {
-#                     'type': 'string',
-#                     'value': 'wheel'
-#                 }], ['deltaX', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['deltaY', {
-#                     'type': 'number',
-#                     'value': 10
-#                 }], ['deltaZ', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }]]
-#             }, {
-#                 'type': 'object',
-#                 'value': [['event', {
-#                     'type': 'string',
-#                     'value': 'mouseup'
-#                 }], ['button', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['buttons', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['clientX', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }], ['clientY', {
-#                     'type': 'number',
-#                     'value': 0
-#                 }]]
-#             }],
-#             'handle': ANY_STR
-#         },
-#         'realm': ANY_STR
-#     } == result
+    assert {
+        'type': 'success',
+        'result': {
+            'type': 'array',
+            'value': [{
+                'type': 'object',
+                'value': [['event', {
+                    'type': 'string',
+                    'value': 'mousedown'
+                }], ['button', {
+                    'type': 'number',
+                    'value': 0
+                }], ['buttons', {
+                    'type': 'number',
+                    'value': 1
+                }], ['clientX', {
+                    'type': 'number',
+                    'value': 0
+                }], ['clientY', {
+                    'type': 'number',
+                    'value': 0
+                }]]
+            }, {
+                'type': 'object',
+                'value': [['event', {
+                    'type': 'string',
+                    'value': 'wheel'
+                }], ['deltaX', {
+                    'type': 'number',
+                    'value': 0
+                }], ['deltaY', {
+                    'type': 'number',
+                    'value': 10
+                }], ['deltaZ', {
+                    'type': 'number',
+                    'value': 0
+                }]]
+            }, {
+                'type': 'object',
+                'value': [['event', {
+                    'type': 'string',
+                    'value': 'mouseup'
+                }], ['button', {
+                    'type': 'number',
+                    'value': 0
+                }], ['buttons', {
+                    'type': 'number',
+                    'value': 0
+                }], ['clientX', {
+                    'type': 'number',
+                    'value': 0
+                }], ['clientY', {
+                    'type': 'number',
+                    'value': 0
+                }]]
+            }],
+            'handle': ANY_STR
+        },
+        'realm': ANY_STR
+    } == result
