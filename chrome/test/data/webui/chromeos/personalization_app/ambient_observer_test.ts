82a7689d5fa8053a92446a05ce871bbde76fc49d
Jason Thai <jasontt@chromium.org>
2023-06-12 20:22:37 +0000

    personalization: update ambient observer test for jelly
    Verifies that preview load performance is always executed for jelly.
    
    BUG=b/286604752
    TEST=browser_tests
    --gtest_filter="PersonalizationAppComponentAmbientObserverTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia9d2a2fca9921061afa980aa3117396b40659afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606069
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156396}

--- chrome/test/data/webui/chromeos/personalization_app/ambient_observer_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/ambient_observer_test.ts
@@ -127,14 +127,17 @@ suite('GooglePhotosPreviewLoadPerformance', () => {
     assertFalse(AmbientObserver.shouldLogPreviewsLoadPerformance);
   });
 
-  test('sets to false if topic source is not kGooglePhotos', async () => {
-    ambientProvider.ambientObserverRemote!.onTopicSourceChanged(
-        TopicSource.kArtGallery);
-    personalizationStore.expectAction(AmbientActionName.SET_TOPIC_SOURCE);
-    await personalizationStore.waitForAction(
-        AmbientActionName.SET_TOPIC_SOURCE);
-    assertFalse(AmbientObserver.shouldLogPreviewsLoadPerformance);
-  });
+  test(
+      'sets to true if topic source is not kGooglePhotos but jelly is enabled',
+      async () => {
+        loadTimeData.overrideValues({isPersonalizationJellyEnabled: true});
+        ambientProvider.ambientObserverRemote!.onTopicSourceChanged(
+            TopicSource.kArtGallery);
+        personalizationStore.expectAction(AmbientActionName.SET_TOPIC_SOURCE);
+        await personalizationStore.waitForAction(
+            AmbientActionName.SET_TOPIC_SOURCE);
+        assertTrue(AmbientObserver.shouldLogPreviewsLoadPerformance);
+      });
 
   test('sets to false if already received preview images', async () => {
     personalizationStore.data.ambient.previews = [];
