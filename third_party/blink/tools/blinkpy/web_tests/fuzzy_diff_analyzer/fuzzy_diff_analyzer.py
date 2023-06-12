f2a4262529122ce6949c430ceea5d8a9482f5a5c
Jie Sheng <jiesheng@google.com>
2023-06-12 21:47:31 +0000

    [fuzzy diff analyzer] 8/N: Create main script.
    Did an example and find one example of test, attach the
    result into the bug. There are more same cases like that.
    
    Bug: 1396136
    Change-Id: I484e4efe7c9025335959b181783d9bfa9c957eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4600880
    Commit-Queue: Jie Sheng <jiesheng@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156471}

--- third_party/blink/tools/blinkpy/web_tests/fuzzy_diff_analyzer/fuzzy_diff_analyzer.py
+++ third_party/blink/tools/blinkpy/web_tests/fuzzy_diff_analyzer/fuzzy_diff_analyzer.py
@@ -16,6 +16,10 @@ third_party/blink/tools/run_fuzzy_diff_analyzer.py \
 
 import argparse
 
+from blinkpy.web_tests.fuzzy_diff_analyzer import analyzer
+from blinkpy.web_tests.fuzzy_diff_analyzer import queries
+from blinkpy.web_tests.fuzzy_diff_analyzer import results
+
 
 def ParseArgs() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=(
@@ -40,4 +44,20 @@ def ParseArgs() -> argparse.Namespace:
 
 def main() -> int:
     args = ParseArgs()
+
+    querier_instance = queries.FuzzyDiffAnalyzerQuerier(
+        args.sample_period, args.project)
+    query_results = querier_instance.get_failed_image_comparison_ci_tests()
+
+    results_processor = results.ResultProcessor()
+    aggregated_results = results_processor.aggregate_results(query_results)
+
+    matching_analyzer = analyzer.FuzzyMatchingAnalyzer()
+    for test_name, test_data in aggregated_results.items():
+        test_result = matching_analyzer.run_analyzer(test_data)
+        print('')
+        print('test_name: %s' % test_name)
+        print('test_result: %s' % test_result)
+        print('')
+
     return 0
