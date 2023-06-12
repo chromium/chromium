50cfe5c3b7021ed8d5e7f81aef64b4be1f09d6f1
Dibyajyoti Pal <dibyapal@google.com>
2023-06-12 20:50:58 +0000

    [dPWA] Enable Service Worker related tests for ML promotion
    This CL enables the Service worker related tests for ML promotion since
    crrev.com/c/4584992 has landed.
    
    Bug: 1450421
    Change-Id: I1d17ace35b449927bae1d2255c5b360ee8a7d46b
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608801
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156417}

--- chrome/browser/web_applications/ml_promotion_browsertest.cc
+++ chrome/browser/web_applications/ml_promotion_browsertest.cc
@@ -419,17 +419,8 @@ IN_PROC_BROWSER_TEST_F(MLPromotionBrowsertest,
       entry, InstallUkmEntry::kIsPartiallyInstalledName, true);
 }
 
-// SiteQualityMetrics tests.
-#if BUILDFLAG(IS_MAC)
-// TODO(crbug.com/1450786): Fix the flakiness of the test.
-#define MAYBE_SiteQualityMetrics_ServiceWorker_FetchHandler \
-  DISABLED_SiteQualityMetrics_ServiceWorker_FetchHandler
-#else
-#define MAYBE_SiteQualityMetrics_ServiceWorker_FetchHandler \
-  SiteQualityMetrics_ServiceWorker_FetchHandler
-#endif
 IN_PROC_BROWSER_TEST_F(MLPromotionBrowsertest,
-                       MAYBE_SiteQualityMetrics_ServiceWorker_FetchHandler) {
+                       SiteQualityMetrics_ServiceWorker_FetchHandler) {
   NavigateAndAwaitMetricsCollectionPending(GetInstallableAppURL());
   AwaitServiceWorkerRegistrationAndPendingDelayedTask(GetInstallableAppURL());
   task_runner_->RunPendingTasks();
@@ -467,17 +458,8 @@ IN_PROC_BROWSER_TEST_F(MLPromotionBrowsertest,
   EXPECT_EQ(entry.metrics[QualityUkmEntry::kServiceWorkerScriptSizeName], 0);
 }
 
-#if BUILDFLAG(IS_MAC)
-// TODO(crbug.com/1450786): Fix the flakiness of the test.
-#define MAYBE_SiteQualityMetrics_ServiceWorker_EmptyFetchHandler \
-  DISABLED_SiteQualityMetrics_ServiceWorker_EmptyFetchHandler
-#else
-#define MAYBE_SiteQualityMetrics_ServiceWorker_EmptyFetchHandler \
-  SiteQualityMetrics_ServiceWorker_EmptyFetchHandler
-#endif
-IN_PROC_BROWSER_TEST_F(
-    MLPromotionBrowsertest,
-    MAYBE_SiteQualityMetrics_ServiceWorker_EmptyFetchHandler) {
+IN_PROC_BROWSER_TEST_F(MLPromotionBrowsertest,
+                       SiteQualityMetrics_ServiceWorker_EmptyFetchHandler) {
   NavigateAndAwaitMetricsCollectionPending(GetUrlWithSWEmptyFetchHandler());
   AwaitServiceWorkerRegistrationAndPendingDelayedTask(
       GetUrlWithSWEmptyFetchHandler());
@@ -497,16 +479,8 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_GT(entry.metrics[QualityUkmEntry::kServiceWorkerScriptSizeName], 0);
 }
 
-#if BUILDFLAG(IS_MAC)
-// TODO(crbug.com/1450786): Fix the flakiness of the test.
-#define MAYBE_SiteQualityMetrics_ServiceWorker_NoFetchHandler \
-  DISABLED_SiteQualityMetrics_ServiceWorker_NoFetchHandler
-#else
-#define MAYBE_SiteQualityMetrics_ServiceWorker_NoFetchHandler \
-  SiteQualityMetrics_ServiceWorker_NoFetchHandler
-#endif
 IN_PROC_BROWSER_TEST_F(MLPromotionBrowsertest,
-                       MAYBE_SiteQualityMetrics_ServiceWorker_NoFetchHandler) {
+                       SiteQualityMetrics_ServiceWorker_NoFetchHandler) {
   NavigateAndAwaitMetricsCollectionPending(GetUrlWithSwNoFetchHandler());
   AwaitServiceWorkerRegistrationAndPendingDelayedTask(
       GetUrlWithSwNoFetchHandler());
