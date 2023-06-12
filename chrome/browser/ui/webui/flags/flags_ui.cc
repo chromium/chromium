af821dc00f3bdd4e082d3cee4fe83b461359e57a
Cole Horvitz <colehorvitz@chromium.org>
2023-06-12 18:36:12 +0000

    Flags UI: Introduce flags-experiment web component.
    This CL
     - Adds a new <flags-experiment> web component (not using Polymer)
       to encapsulate the UI for each entry in chrome://flags.
     - Implements the `set data() {...}` method to correctly render the
       contents of the element.
     - Hooks up the new <flags-experiment> to the existing jstemplate
       system (jsselect=...", jsvalues="...") as an incremental step for
       eventually fully removing jstemplate usage.
     - Rewrites flags logic to account for the flags-experiment shadow DOM.
     - Removes CSS and HTML that was rendered obsolete by the above changes.
    
    Performance improvements to this UI will be made in subsequent CL.
    
    Based on https://crrev.com/c/4569468.
    
    Bug: 1376313
    Change-Id: Ia8cc0ac0c261ce535fdc6d4c59f822fc2f06da87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4582413
    Commit-Queue: Cole Horvitz <colehorvitz@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1156328}

--- chrome/browser/ui/webui/flags/flags_ui.cc
+++ chrome/browser/ui/webui/flags/flags_ui.cc
@@ -65,12 +65,13 @@ namespace {
 content::WebUIDataSource* CreateAndAddFlagsUIHTMLSource(Profile* profile) {
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       profile, chrome::kChromeUIFlagsHost);
+  source->EnableReplaceI18nInJS();
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc,
       "script-src chrome://resources 'self' 'unsafe-eval';");
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::TrustedTypes,
-      "trusted-types jstemplate;");
+      "trusted-types jstemplate static-types;");
   source->AddString(flags_ui::kVersion,
                     std::string(version_info::GetVersionNumber()));
 
