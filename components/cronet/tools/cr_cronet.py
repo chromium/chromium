aaaaa95254c07443492acee96a201a0f6b45fb7c
Prashanth Swaminathan <prashanthsw@google.com>
2023-06-12 10:00:19 +0000

    Replace deprecated 'pipes' module with 'shlex'
    The quote-escape functions in 'pipes' have been moved to 'shlex' and
    'pipes' is now being deprecated. This requires clients to have at least
    Python 3.3+ and documentation confirms that the minimum assumption is at
    least Python 3.8.
    
    Bug: chromium:1453653
    Test: Verified running cr_cronet.py escapes and generates GN properly.
    Change-Id: I295ac546d0465e24d0248069b6e92b456fb8e21b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4605495
    Auto-Submit: Prashanth Swaminathan <prashanthsw@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1156115}

--- components/cronet/tools/cr_cronet.py
+++ components/cronet/tools/cr_cronet.py
@@ -9,14 +9,14 @@ cr_cronet.py - cr - like helper tool for cronet developers
 
 import argparse
 import os
-import pipes
 import re
+import shlex
 import subprocess
 import sys
 
 
 def quoted_args(args):
-  return ' '.join([pipes.quote(arg) for arg in args])
+  return ' '.join([shlex.quote(arg) for arg in args])
 
 
 def run(command, **kwargs):
@@ -85,7 +85,7 @@ def debug(extra_options):
 
 def stack(out_dir):
   return run_shell(
-      'adb logcat -d | CHROMIUM_OUTPUT_DIR=' + pipes.quote(out_dir) +
+      'adb logcat -d | CHROMIUM_OUTPUT_DIR=' + shlex.quote(out_dir) +
       ' third_party/android_platform/development/scripts/stack')
 
 
