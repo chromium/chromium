1b0c577a7abc10700ed2dab9f542d40c6f7a9ce1
Keren Zhu <kerenzhu@chromium.org>
2023-06-13 15:14:52 +0000

    [CR2023] update primary background color
    This CL updates the background color of bubbles, dialogs, menu,
    tooltips and some other UIs to kColorSysSurface.
    
    Bug: 1422125
    Change-Id: Ied72b9ee4807319aa1c5672d34fd43cfc69e694a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603356
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156928}

--- ui/color/material_ui_color_mixer.cc
+++ ui/color/material_ui_color_mixer.cc
@@ -74,6 +74,7 @@ void AddMaterialUiColorMixer(ColorProvider* provider,
   mixer[kColorMenuIcon] = {kColorSysOnSurfaceSubtle};
   mixer[kColorMenuItemForegroundSecondary] = {kColorSysOnSurfaceSubtle};
   mixer[kColorMenuItemForeground] = {kColorSysOnSurface};
+  mixer[kColorPrimaryBackground] = {kColorSysSurface};
   mixer[kColorProgressBar] = {ui::kColorSysPrimary};
   mixer[kColorProgressBarBackground] = {ui::kColorSysNeutralOutline};
   mixer[kColorProgressBarPaused] = {ui::kColorSysStateDisabled};
