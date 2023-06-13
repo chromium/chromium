44efbca5c05435751107e5fc89af0c87e0997d28
Ted Choc <tedchoc@chromium.org>
2023-06-13 12:45:24 +0000

    Delete testHideBrowserControlsAfterFlingBoosting
    The test has been disabled since 2018.
    
    When attempting to re-eanble, the following break:
    1.) Adding an observer for the GestureListenerManager requires
        being on the UI thread
        https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java;drc=17b782a721f7f657ff7f81fc36732cda16c5d5c4;l=416
    2.) Fling end is getting called before the second fling starts
        (seemingly part of the initial fling event causes the fling end
         to be broadcast). Resulting in this to fail 100% of the time:
         https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java;drc=17b782a721f7f657ff7f81fc36732cda16c5d5c4;l=439
    3.) The viewport callback logic was removed and is no longer called
        ever:
        https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java;drc=17b782a721f7f657ff7f81fc36732cda16c5d5c4;l=418
    
    Rationale for deleting the test is that this is really aiming
    to test ordering of scroll events, and doesn't need to be so
    tightly coupled with the fullscreen manager concept at all. If
    we need to revive this test, we should write it as an integration
    test of Flings and drop the omnibox scrolling testing out of it.
    
    But since we haven't had any issues in 5 years since this test
    was disabled, simply deleting it seems justified.
    
    BUG=901280,1331535
    
    Change-Id: I2e5c446e9b05a0905e0f9bbdc17a16162bf617d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608574
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156862}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -24,7 +24,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
@@ -49,8 +48,6 @@ import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.FullscreenTestUtils;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
-import org.chromium.content_public.browser.GestureListenerManager;
-import org.chromium.content_public.browser.GestureStateListener;
 import org.chromium.content_public.browser.SelectionPopupController;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
@@ -58,10 +55,8 @@ import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.browser.test.util.TestTouchUtils;
 import org.chromium.content_public.browser.test.util.TouchCommon;
 import org.chromium.content_public.browser.test.util.UiUtils;
-import org.chromium.content_public.browser.test.util.WebContentsUtils;
 
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -380,82 +375,6 @@ public class FullscreenManagerTest {
         // Check that the URL bar has not grabbed focus (http://crbug/236365)
         UrlBar urlBar = (UrlBar) mActivityTestRule.getActivity().findViewById(R.id.url_bar);
         Assert.assertFalse("Url bar grabbed focus", urlBar.hasFocus());
-    }
-
-    @Test
-    //@LargeTest
-    @DisabledTest(message = "crbug.com/901280")
-    public void testHideBrowserControlsAfterFlingBoosting() {
-        // Test that fling boosting doesn't break the scroll state management
-        // that's used by the BrowserControlsManager to dispatch URL bar based
-        // resizes to the renderer.
-        FullscreenManagerTestUtils.disableBrowserOverrides();
-        mActivityTestRule.startMainActivityWithURL(LONG_HTML_TEST_PAGE);
-
-        FullscreenManagerTestUtils.waitForBrowserControlsToBeMoveable(
-                mActivityTestRule, mActivityTestRule.getActivity().getActivityTab());
-
-        final CallbackHelper flingEndCallback = new CallbackHelper();
-        final CallbackHelper scrollStartCallback = new CallbackHelper();
-        GestureStateListener scrollListener = new GestureStateListener() {
-            @Override
-            public void onScrollStarted(
-                    int scrollOffsetY, int scrollExtentY, boolean isDirectionUp) {
-                scrollStartCallback.notifyCalled();
-            }
-
-            @Override
-            public void onFlingEndGesture(int scrollOffsetY, int scrollExtentY) {
-                flingEndCallback.notifyCalled();
-            }
-        };
-
-        Tab tab = mActivityTestRule.getActivity().getActivityTab();
-        GestureListenerManager gestureListenerManager =
-                WebContentsUtils.getGestureListenerManager(tab.getWebContents());
-        gestureListenerManager.addListener(scrollListener);
-
-        final CallbackHelper viewportCallback = new CallbackHelper();
-
-        Assert.assertEquals(0, scrollStartCallback.getCallCount());
-        Assert.assertEquals(0, viewportCallback.getCallCount());
-
-        // Start the first fling.
-        FullscreenManagerTestUtils.fling(mActivityTestRule, 0, -2000);
-
-        // Wait until we hear the gesture scroll begin before we try to fling
-        // again since we'll hit DCHECKs in the fling controller state
-        // management.
-        try {
-            scrollStartCallback.waitForCallback(0, 1, 1000, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            Assert.fail("Timeout waiting for scroll to start");
-        }
-
-        // Fling again while the first fling is still active. This will boost
-        // the first fling.
-        FullscreenManagerTestUtils.fling(mActivityTestRule, 0, -2000);
-
-        Assert.assertEquals(0, flingEndCallback.getCallCount());
-        Assert.assertTrue(gestureListenerManager.isScrollInProgress());
-
-        try {
-            flingEndCallback.waitForCallback(0, 1, 5000, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            Assert.fail("Timeout waiting for scroll to end");
-        }
-
-        // Make sure we call the viewport changed callback since the URL bar was hidden.
-        // Can be called once for the FlingEnd and once for the ScrollEnd.
-        try {
-            viewportCallback.waitForCallback(0, 1, 500, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            Assert.fail("Failed to update viewport");
-        }
-
-        // Ensure we don't still think we're scrolling.
-        Assert.assertFalse(
-                "Failed to reset scrolling state", gestureListenerManager.isScrollInProgress());
     }
 
     @Test
