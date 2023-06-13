58fb08e665f5d7a636765287b48ce1a402a63870
Calder Kitagawa <ckitagawa@chromium.org>
2023-06-13 22:25:39 +0000

    [Fixit] UndoBarController replace DisableIf with Restriction
    Should be a restriction rather than a DisableIf.
    
    Fixed: 1199023
    Change-Id: Ic9448ba2bb4a6e6f44c77550b7a77f7d0573d347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610779
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157180}

--- chrome/android/javatests/src/org/chromium/chrome/browser/undo_tab_close_snackbar/UndoBarControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/undo_tab_close_snackbar/UndoBarControllerTest.java
@@ -16,7 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -30,7 +30,7 @@ import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
-import org.chromium.ui.test.util.UiDisableIf;
+import org.chromium.ui.test.util.UiRestriction;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -172,7 +172,7 @@ public class UndoBarControllerTest {
 
     @Test
     @SmallTest
-    @DisableIf.Device(type = {UiDisableIf.TABLET}) // crbug/1199248
+    @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     @Features.EnableFeatures({ChromeFeatureList.START_SURFACE_WITH_ACCESSIBILITY})
     // clang-format off
     @CommandLineFlags.Add({
@@ -183,7 +183,6 @@ public class UndoBarControllerTest {
     // clang-format on
     public void
     testUndoSnackbarEnabled_AccessibilityEnabledWithGroupM5() throws Exception {
-        // clang-format on
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> ChromeAccessibilityUtil.get().setAccessibilityEnabledForTesting(true));
 
