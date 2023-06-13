f1e88a8a881fac01fd72145d0e96204f14b92540
Zain Afzal <zafzal@google.com>
2023-06-13 06:30:02 +0000

    [Sherrif] Disabled ServiceWorkersDoNotTimeOutWithPolicy on Linux.
    ServiceWorkersDoNotTimeOutWithPolicy Has failed 11 times in the last 200 runs.
    
    Bug: 1454339
    Change-Id: I17986a7b265bb59095cb078d70ef23511e7fa929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4609825
    Owners-Override: Zain Afzal <zafzal@google.com>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156742}

--- chrome/browser/extensions/service_worker_lifetime_strong_keepalive_browsertest.cc
+++ chrome/browser/extensions/service_worker_lifetime_strong_keepalive_browsertest.cc
@@ -201,8 +201,16 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerLifetimeStrongKeepaliveBrowsertest,
 
 // Tests that the service workers will not stop if both extensions are
 // allowlisted via policy and the port is not closed.
+// TODO(https://crbug.com/1454339): Flakes on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_ServiceWorkersDoNotTimeOutWithPolicy \
+  DISABLED_ServiceWorkersDoNotTimeOutWithPolicy
+#else
+#define MAYBE_ServiceWorkersDoNotTimeOutWithPolicy \
+  ServiceWorkersDoNotTimeOutWithPolicy
+#endif
 IN_PROC_BROWSER_TEST_F(ServiceWorkerLifetimeStrongKeepaliveBrowsertest,
-                       ServiceWorkersDoNotTimeOutWithPolicy) {
+                       MAYBE_ServiceWorkersDoNotTimeOutWithPolicy) {
   base::Value::List urls;
   // Both extensions receive extended lifetime.
   urls.Append(kTestOpenerExtensionUrl);
