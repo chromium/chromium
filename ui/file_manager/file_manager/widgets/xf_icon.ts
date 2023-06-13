013c2ba2e4ebaa3d924054527197ba61d277e2e9
Ben Reich <benreich@chromium.org>
2023-06-13 06:43:11 +0000

    [FilesBulkPinning] Make the cant-pin icon be less bulky
    The cant-pin icon actually has a border around the outside to be used to
    differentiate the dash from a background in the grid mode. Unfortunately
    in list view this has been implemented using a mask and thus both
    outline and inside are the same color, This updates to differentiate the
    outside and inside colors.
    
    Fixed: b:285210022
    Change-Id: I4dbb672d63dbaeccbd97f2d5635618a08a1eaa2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4603447
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156747}

--- ui/file_manager/file_manager/widgets/xf_icon.ts
+++ ui/file_manager/file_manager/widgets/xf_icon.ts
@@ -39,8 +39,8 @@ export class XfIcon extends XfBase {
 
   static get multiColor() {
     return {
-      [constants.ICON_TYPES.OFFLINE]:
-          svg`<use xlink:href="foreground/images/files/ui/offline.svg#offline"></use>`,
+      [constants.ICON_TYPES.CANT_PIN]:
+          svg`<use xlink:href="foreground/images/files/ui/cant_pin.svg#cant_pin"></use>`,
       [constants.ICON_TYPES.CLOUD_DONE]:
           svg`<use xlink:href="foreground/images/files/ui/cloud_done.svg#cloud_done"></use>`,
       [constants.ICON_TYPES.CLOUD_ERROR]:
@@ -55,6 +55,8 @@ export class XfIcon extends XfBase {
           svg`<use xlink:href="foreground/images/files/ui/encrypted.svg#encrypted"></use>`,
       [constants.ICON_TYPES.ERROR]:
           svg`<use xlink:href="foreground/images/files/ui/error.svg#error"></use>`,
+      [constants.ICON_TYPES.OFFLINE]:
+          svg`<use xlink:href="foreground/images/files/ui/offline.svg#offline"></use>`,
     };
   }
 
@@ -192,10 +194,6 @@ function getCSS() {
 
     :host([type="camera-folder"]) span {
       -webkit-mask-image: url(../foreground/images/volumes/camera.svg);
-    }
-
-    :host([type="cant-pin"]) span {
-      -webkit-mask-image: url(../foreground/images/files/ui/cant_pin.svg);
     }
 
     :host([type="computer"]) span {
