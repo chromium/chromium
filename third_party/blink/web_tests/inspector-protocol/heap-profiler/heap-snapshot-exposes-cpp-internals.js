8f4016b65f65e4dc092cc2cddb44c42aea77ceec
Michael Lippautz <mlippautz@chromium.org>
2023-06-13 16:18:33 +0000

    CfT: Add test for exposing internals in heap snapshots
    The test ensures that on regular builds a snapshot will see
    InternalNode objects but no Blink C++ objects. For CfT we ensure the
    exact opposite.
    
    Depends on: https://crrev.com/c/4547806
    
    Cq-Include-Trybots: luci.chromium.try:win-rel-cft,mac-rel-cft,linux-rel-cft
    Change-Id: Idd52cd34c4c3a4af126270a5e2cd13b91da8f459
    Bug: 1446931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4538742
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1156968}

---
+++ third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-exposes-cpp-internals.js
@@ -0,0 +1,42 @@
+(async function(testRunner) {
+    const {page, session, dp} = await testRunner.startBlank(
+      'Tests whether a heap snapshot contains any "InternalNode" or "blink::" objects. This test is useful to check that cppgc_enable_object_names gn arg is enabled.');
+
+    await dp.Profiler.enable();
+
+    // Take a heap snapshot.
+    let snapshot_string = '';
+    function onChunk(message) {
+      snapshot_string += message.params.chunk;
+    }
+    dp.HeapProfiler.onAddHeapSnapshotChunk(onChunk)
+    await dp.HeapProfiler.takeHeapSnapshot({ reportProgress: false, exposeInternals: true });
+    const s = JSON.parse(snapshot_string);
+
+    // Iterate all nodes and check the name against "InternalNode" and Blink C++ namespace "blink::".
+    const nameIndex = s.snapshot.meta.node_fields.indexOf('name');
+    const nodeLength = s.snapshot.meta.node_fields.length;
+    let foundInternalNode = false;
+    let foundBlinkNameSpace = false;
+    for (let i = 0; i < (nodeLength * s.snapshot.node_count); i += nodeLength) {
+      let nodeName = s.strings[s.nodes[i + nameIndex]];
+      if (nodeName === 'InternalNode') {
+        foundInternalNode = true;
+      } else if (nodeName.startsWith('blink::')) {
+        foundBlinkNameSpace = true;
+      }
+    }
+
+    if (foundInternalNode) {
+      testRunner.log('InternalNode: Found');
+    } else {
+      testRunner.log('InternalNode: Not found');
+    }
+    if (foundBlinkNameSpace) {
+      testRunner.log('Blink namespace: Found');
+    } else {
+      testRunner.log('Blink namespace: Not found');
+    }
+
+    testRunner.completeTest();
+  })
