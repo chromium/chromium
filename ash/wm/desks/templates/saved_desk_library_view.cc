8c93e386c862d497094c52e8c5d124af3fa15a67
Judy Wang <shidi@chromium.org>
2023-06-13 17:47:26 +0000

    CrosNext: Add shadow to Desk Library Label.
    Apply shadow to the text label to increase the visibility to users.
    
    Test: Manual
    Change-Id: I7cc516ec37b487b18be9266bf96735555eaf2d32
    Fixed: b/284210964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608885
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157043}

--- ash/wm/desks/templates/saved_desk_library_view.cc
+++ ash/wm/desks/templates/saved_desk_library_view.cc
@@ -63,6 +63,9 @@ constexpr int kGroupContentsBetweenChildSpacingDp = 20;
 // The size of the gradient applied to the top and bottom of the scroll view.
 constexpr int kScrollViewGradientSize = 32;
 
+// Elevation for the grid label text's shadow.
+constexpr int kLabelTextShadowElevation = 4;
+
 // Insets of Library page scroll content view. Note: the bottom inset is there
 // to slightly adjust the otherwise vertically centered scroll content up a tad.
 constexpr gfx::Insets kLibraryPageScrollContentsInsets =
@@ -129,6 +132,10 @@ std::unique_ptr<views::View> GetLabelAndGridGroupContents() {
 std::unique_ptr<views::Label> MakeGridLabel(int label_string_id) {
   auto label = std::make_unique<views::Label>(
       l10n_util::GetStringUTF16(label_string_id));
+  gfx::ShadowValues shadows =
+      gfx::ShadowValue::MakeChromeOSSystemUIShadowValues(
+          kLabelTextShadowElevation);
+  label->SetShadows(shadows);
   TypographyProvider::Get()->StyleLabel(TypographyToken::kCrosTitle1, *label);
   label->SetEnabledColorId(cros_tokens::kCrosSysOnSurface);
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
@@ -564,7 +571,6 @@ void SavedDeskLibraryView::Layout() {
     grid_labels_[i]->SetVisible(!grid_views_[i]->grid_items().empty());
     grid_labels_[i]->SetPreferredSize(landscape ? kLabelSizeLandscape
                                                 : kLabelSizePortrait);
-
     total_saved_desks += grid_views_[i]->grid_items().size();
   }
 
