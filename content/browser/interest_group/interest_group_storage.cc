151b8e2ec713a5acf9135c4c537f52ffbeb258c2
Russ Hamilton <behamilton@google.com>
2023-06-14 19:17:54 +0000

    Revert "interest_group_storage.cc Migrate database code to base::Time serialization in sql//"
    This reverts commit bf5185f73ab3b8178c97eb91b2a0ecbc064baf2e.
    
    Reason for revert: The CL changed functionality by storing the full precision of the time instead of truncating it to a 24h period. Note that we were truncating the time to reduce database size by consolidating records.
    
    Original change's description:
    > interest_group_storage.cc Migrate database code to base::Time serialization in sql//
    >
    > This CL migrates code in interest_group_storage.cc to the BindTime() method provided by sql::Transaction.
    >
    > This CL does not introduce any behavior changes.
    >
    > Bug: 1195962
    > Change-Id: I1822d9821e9a4d89f9aa7e6578977b061e614baf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3900200
    > Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    > Commit-Queue: Christine Smith <christinesm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1048046}
    
    Bug: 1195962
    Change-Id: I36fa1976480b498f5c22c20549ac7238a2d72115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615905
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157705}

--- content/browser/interest_group/interest_group_storage.cc
+++ content/browser/interest_group/interest_group_storage.cc
@@ -1209,6 +1209,10 @@ bool DoRecordInterestGroupJoin(sql::Database& db,
   // enclose them in a transaction since only one will actually modify the
   // database.
 
+  int64_t join_day = join_time.ToDeltaSinceWindowsEpoch()
+                         .FloorToMultiple(base::Days(1))
+                         .InMicroseconds();
+
   // clang-format off
   sql::Statement insert_join_hist(
       db.GetCachedStatement(SQL_FROM_HERE,
@@ -1221,7 +1225,7 @@ bool DoRecordInterestGroupJoin(sql::Database& db,
   insert_join_hist.Reset(true);
   insert_join_hist.BindString(0, Serialize(owner));
   insert_join_hist.BindString(1, name);
-  insert_join_hist.BindTime(2, join_time);
+  insert_join_hist.BindInt64(2, join_day);
   if (!insert_join_hist.Run())
     return false;
 
@@ -1242,7 +1246,7 @@ bool DoRecordInterestGroupJoin(sql::Database& db,
   update_join_hist.Reset(true);
   update_join_hist.BindString(0, Serialize(owner));
   update_join_hist.BindString(1, name);
-  update_join_hist.BindTime(2, join_time);
+  update_join_hist.BindInt64(2, join_day);
 
   return update_join_hist.Run();
 }
@@ -1522,6 +1526,10 @@ bool DoRecordInterestGroupBid(sql::Database& db,
   // enclose them in a transaction since only one will actually modify the
   // database.
 
+  int64_t bid_day = bid_time.ToDeltaSinceWindowsEpoch()
+                        .FloorToMultiple(base::Days(1))
+                        .InMicroseconds();
+
   // clang-format off
   sql::Statement insert_bid_hist(
       db.GetCachedStatement(SQL_FROM_HERE,
@@ -1534,7 +1542,7 @@ bool DoRecordInterestGroupBid(sql::Database& db,
   insert_bid_hist.Reset(true);
   insert_bid_hist.BindString(0, Serialize(group_key.owner));
   insert_bid_hist.BindString(1, group_key.name);
-  insert_bid_hist.BindTime(2, bid_time);
+  insert_bid_hist.BindInt64(2, bid_day);
   if (!insert_bid_hist.Run())
     return false;
 
@@ -1555,7 +1563,7 @@ bool DoRecordInterestGroupBid(sql::Database& db,
   update_bid_hist.Reset(true);
   update_bid_hist.BindString(0, Serialize(group_key.owner));
   update_bid_hist.BindString(1, group_key.name);
-  update_bid_hist.BindTime(2, bid_time);
+  update_bid_hist.BindInt64(2, bid_day);
 
   return update_bid_hist.Run();
 }
