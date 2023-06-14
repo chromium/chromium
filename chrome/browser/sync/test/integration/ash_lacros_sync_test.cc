f53ba1e8b0278808caaf75c9601509b881fb7e7b
Hidehiko Abe <hidehiko@chromium.org>
2023-06-14 04:49:09 +0000

    Remove LacrosPrimaryAshSyncTest.
    LacrosPrimary mode is going to be removed.
    This is the preparation.
    
    BUG=1448575
    TEST=Tryjob
    
    Change-Id: I6db7d9a8b345f86abd843b57a08f8482f35cd2cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610182
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157358}

--- chrome/browser/sync/test/integration/ash_lacros_sync_test.cc
+++ chrome/browser/sync/test/integration/ash_lacros_sync_test.cc
@@ -14,19 +14,6 @@
 
 namespace {
 
-// Test fixture running in Ash, with LacrosPrimary enabled.
-class LacrosPrimaryAshSyncTest : public SyncTest {
- public:
-  LacrosPrimaryAshSyncTest() : SyncTest(SINGLE_CLIENT) {
-    feature_list_.InitWithFeatures(
-        {ash::features::kLacrosSupport, ash::features::kLacrosPrimary}, {});
-  }
-  ~LacrosPrimaryAshSyncTest() override = default;
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
 // Test fixture running in Ash, with LacrosOnly enabled.
 class LacrosOnlyAshSyncTest : public SyncTest {
  public:
@@ -41,100 +28,6 @@ class LacrosOnlyAshSyncTest : public SyncTest {
  private:
   base::test::ScopedFeatureList feature_list_;
 };
-
-IN_PROC_BROWSER_TEST_F(LacrosPrimaryAshSyncTest, AshSyncsAllTypes) {
-  // With LacrosPrimary (but not LacrosOnly), Sync in the Ash browser is still
-  // enabled.
-  ASSERT_TRUE(crosapi::browser_util::IsAshBrowserSyncEnabled());
-
-  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-
-  syncer::SyncService* sync_service = GetSyncService(0);
-  syncer::SyncUserSettings* user_settings = sync_service->GetUserSettings();
-
-  // Both browser types and OS types should be selectable in Ash.
-  // NOTE: For now, while Lacros is in development, we check the full set
-  // equality here. Longer term, it's probably sufficient (and more robust) to
-  // just spot-check a few "representative" types.
-
-  // All the browser types should be selectable in Ash, except for Apps and
-  // WifiConfigurations which are part of the selectable *OS* types.
-  EXPECT_EQ(
-      user_settings->GetRegisteredSelectableTypes(),
-      syncer::UserSelectableTypeSet({syncer::UserSelectableType::kBookmarks,
-                                     syncer::UserSelectableType::kPreferences,
-                                     syncer::UserSelectableType::kPasswords,
-                                     syncer::UserSelectableType::kAutofill,
-                                     syncer::UserSelectableType::kThemes,
-                                     syncer::UserSelectableType::kHistory,
-                                     syncer::UserSelectableType::kExtensions,
-                                     syncer::UserSelectableType::kReadingList,
-                                     syncer::UserSelectableType::kTabs}))
-      << "Actual: "
-      << syncer::UserSelectableTypeSetToString(
-             user_settings->GetRegisteredSelectableTypes());
-
-  // All the OS types should be selectable in Ash too.
-  EXPECT_EQ(user_settings->GetRegisteredSelectableOsTypes(),
-            syncer::UserSelectableOsTypeSet(
-                {syncer::UserSelectableOsType::kOsApps,
-                 syncer::UserSelectableOsType::kOsPreferences,
-                 syncer::UserSelectableOsType::kOsWifiConfigurations}))
-      << "Actual: "
-      << syncer::UserSelectableOsTypeSetToString(
-             user_settings->GetRegisteredSelectableOsTypes());
-
-  syncer::ModelTypeSet expected_active_types = {
-      syncer::BOOKMARKS,
-      syncer::PREFERENCES,
-      syncer::PASSWORDS,
-      syncer::AUTOFILL_PROFILE,
-      syncer::AUTOFILL,
-      syncer::AUTOFILL_WALLET_DATA,
-      syncer::AUTOFILL_WALLET_METADATA,
-      syncer::AUTOFILL_WALLET_OFFER,
-      syncer::THEMES,
-      syncer::EXTENSIONS,
-      syncer::SEARCH_ENGINES,
-      syncer::SESSIONS,
-      syncer::APPS,
-      syncer::APP_SETTINGS,
-      syncer::EXTENSION_SETTINGS,
-      syncer::HISTORY_DELETE_DIRECTIVES,
-      syncer::DICTIONARY,
-      syncer::DEVICE_INFO,
-      syncer::PRIORITY_PREFERENCES,
-      syncer::APP_LIST,
-      syncer::ARC_PACKAGE,
-      syncer::PRINTERS,
-      syncer::READING_LIST,
-      syncer::USER_EVENTS,
-      syncer::USER_CONSENTS,
-      syncer::SEND_TAB_TO_SELF,
-      syncer::SECURITY_EVENTS,
-      syncer::WIFI_CONFIGURATIONS,
-      syncer::OS_PREFERENCES,
-      syncer::OS_PRIORITY_PREFERENCES,
-      syncer::SHARING_MESSAGE,
-      syncer::WORKSPACE_DESK,
-      syncer::PROXY_TABS,
-      syncer::NIGORI};
-  if (base::FeatureList::IsEnabled(syncer::kSyncEnableHistoryDataType)) {
-    expected_active_types.Put(syncer::HISTORY);
-  } else {
-    expected_active_types.Put(syncer::TYPED_URLS);
-  }
-  if (base::FeatureList::IsEnabled(syncer::kSyncEnableContactInfoDataType)) {
-    expected_active_types.Put(syncer::CONTACT_INFO);
-  }
-
-  if (base::FeatureList::IsEnabled(power_bookmarks::kPowerBookmarkBackend)) {
-    expected_active_types.Put(syncer::POWER_BOOKMARK);
-  }
-
-  // All of the model types, both browser and OS, should be active.
-  EXPECT_EQ(sync_service->GetActiveDataTypes(), expected_active_types);
-}
 
 IN_PROC_BROWSER_TEST_F(LacrosOnlyAshSyncTest, AshSyncsOnlyOSTypes) {
   // With LacrosOnly, Sync in the Ash browser is not enabled anymore.
