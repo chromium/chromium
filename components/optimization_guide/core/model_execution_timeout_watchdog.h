39144dce89c8cd2bbeb88a430a7387d0c458619f
Robert Ogden <robertogden@chromium.org>
2023-06-14 22:20:33 +0000

    Generalize the Model Execution Watchdog
    Instead of relying on a specific method on a specific class, use a
    OnceClosure instead to be more generic and in order to support different
    task libraries.
    
    Also has the benefit of no longer being templated so moves the impl to
    its own cc file.
    
    Bug: b/283522287
    Change-Id: I6b3faa470b06330ac985a57d256a4dbd277e81ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615354
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1157836}

--- components/optimization_guide/core/model_execution_timeout_watchdog.h
+++ components/optimization_guide/core/model_execution_timeout_watchdog.h
@@ -5,26 +5,15 @@
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TIMEOUT_WATCHDOG_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TIMEOUT_WATCHDOG_H_
 
-#include "base/metrics/histogram_functions.h"
+#include "base/functional/callback.h"
 #include "base/synchronization/lock.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "components/optimization_guide/core/model_util.h"
 #include "components/optimization_guide/proto/models.pb.h"
-#include "third_party/tflite_support/src/tensorflow_lite_support/cc/task/core/base_task_api.h"
 
 namespace optimization_guide {
-
-namespace {
-void RecordDidTimeoutHistogram(proto::OptimizationTarget optimization_target,
-                               bool did_timeout) {
-  base::UmaHistogramBoolean(
-      "OptimizationGuide.ModelExecutor.DidTimeout." +
-          GetStringNameForOptimizationTarget(optimization_target),
-      did_timeout);
-}
-}  // namespace
 
 // This is a helper class to |TFLiteModelExecutor| that watches for a model
 // execution that runs for too long.
@@ -60,90 +49,21 @@ void RecordDidTimeoutHistogram(proto::OptimizationTarget optimization_target,
 // |AlarmOnWatchdogSequence| on the watchdog sequence. The deletion of this
 // class must happen on the watchdog sequence to ensure that timer task can be
 // cancelled on the right sequence.
-
-template <class OutputType, class... InputTypes>
 class ModelExecutionTimeoutWatchdog {
  public:
   explicit ModelExecutionTimeoutWatchdog(
       scoped_refptr<base::SequencedTaskRunner> watchdog_task_runner,
       proto::OptimizationTarget optimization_target,
-      base::TimeDelta duration)
-      : watchdog_task_runner_(watchdog_task_runner),
-        optimization_target_(optimization_target),
-        duration_(duration) {
-    DCHECK_GE(duration, base::TimeDelta());
-  }
+      base::TimeDelta duration);
+  ~ModelExecutionTimeoutWatchdog();
 
-  ~ModelExecutionTimeoutWatchdog() {
-    DCHECK(watchdog_task_runner_->RunsTasksInCurrentSequence());
-  }
-
-  void ArmWithTask(
-      tflite::task::core::BaseTaskApi<OutputType, InputTypes...>* task) {
-    {
-      base::AutoLock lock(task_lock_);
-      task_ = task;
-    }
-
-    // Arm the watchdog timer. Since the dtor is on the watchdog sequence,
-    // using base::Unretained is safe.
-    watchdog_task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce(&ModelExecutionTimeoutWatchdog::ArmOnWatchdogSequence,
-                       base::Unretained(this)));
-  }
-
-  void DisarmOnExecutionComplete() {
-    {
-      base::AutoLock lock(task_lock_);
-      if (!task_) {
-        // This could happen in a race where Disarm was called at the same time
-        // as the alarm going off.
-        return;
-      }
-      task_ = nullptr;
-    }
-
-    // Disarm the watchdog timer. Since the dtor is on the watchdog sequence,
-    // using base::Unretained is safe.
-    watchdog_task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce(&ModelExecutionTimeoutWatchdog::DisarmOnWatchdogSequence,
-                       base::Unretained(this)));
-    RecordDidTimeoutHistogram(optimization_target_, false);
-  }
+  void ArmWithTask(base::OnceClosure cancel_closure);
+  void DisarmOnExecutionComplete();
 
  private:
-  void ArmOnWatchdogSequence() {
-    DCHECK(watchdog_task_runner_->RunsTasksInCurrentSequence());
-    // Since the dtor is on the watchdog sequence, using base::Unretained is
-    // safe. If the timer is released, the pending task will be canceled.
-    watchdog_timer_.Start(
-        FROM_HERE, duration_,
-        base::BindOnce(&ModelExecutionTimeoutWatchdog::AlarmOnWatchdogSequence,
-                       base::Unretained(this)));
-  }
-
-  void DisarmOnWatchdogSequence() {
-    DCHECK(watchdog_task_runner_->RunsTasksInCurrentSequence());
-    watchdog_timer_.Stop();
-  }
-
-  void AlarmOnWatchdogSequence() {
-    DCHECK(watchdog_task_runner_->RunsTasksInCurrentSequence());
-    {
-      base::AutoLock lock(task_lock_);
-      if (!task_) {
-        // This could happen in a race where Disarm was called at the same time
-        // as the alarm going off.
-        return;
-      }
-      task_->Cancel();
-      task_ = nullptr;
-    }
-
-    RecordDidTimeoutHistogram(optimization_target_, true);
-  }
+  void ArmOnWatchdogSequence();
+  void DisarmOnWatchdogSequence();
+  void AlarmOnWatchdogSequence();
 
   scoped_refptr<base::SequencedTaskRunner> watchdog_task_runner_;
   base::OneShotTimer watchdog_timer_;
@@ -151,9 +71,8 @@ class ModelExecutionTimeoutWatchdog {
   const proto::OptimizationTarget optimization_target_;
   const base::TimeDelta duration_;
 
-  base::Lock task_lock_;
-  raw_ptr<tflite::task::core::BaseTaskApi<OutputType, InputTypes...>> task_
-      GUARDED_BY(task_lock_);
+  base::Lock cancel_lock_;
+  base::OnceClosure cancel_closure_ GUARDED_BY(cancel_lock_);
 };
 
 }  // namespace optimization_guide
