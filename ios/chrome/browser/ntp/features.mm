3ebe9a5cdf8c06895f6a3c9afc2f1adcd77ce660
Tina Wang <tinazwang@google.com>
2023-06-15 16:42:34 +0000

    [FeedBottomSyncPromo] Remove fieldtrial and enable the flag by default
    Remove fieldtrial and enable the flag by default since this feature
    is 100% launch.
    
    Change-Id: I17c96d47131c1075e64a1f182e7327b3e951861c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617051
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158229}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -64,7 +64,7 @@ BASE_FEATURE(kCreateDiscoverFeedServiceEarly,
 
 BASE_FEATURE(kEnableFeedBottomSignInPromo,
              "EnableFeedBottomSignInPromo",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableFeedCardMenuSignInPromo,
              "EnableFeedCardMenuSignInPromo",
