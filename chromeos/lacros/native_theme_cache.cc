a01a5eab7613e20650a8373255ca07deb804489f
Sean Kau <skau@chromium.org>
2023-06-15 17:40:40 +0000

    Propagate Color information from Ash to Lacros
    Read color information from Ash to seed the Lacros theme. Color
    information will only be sent if Jelly is enabled and its only used
    when ChromeRefresh2023 is enabled.
    
    Color information is received via mojo. Update to the NativeThemeInfo struct included here.
    
    Bug: b:285907362, b:285907359
    Change-Id: Ib4500f203bac3879a4822c0f7865af849721d2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595806
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158288}

--- chromeos/lacros/native_theme_cache.cc
+++ chromeos/lacros/native_theme_cache.cc
@@ -5,9 +5,50 @@
 #include "chromeos/lacros/native_theme_cache.h"
 
 #include "chromeos/lacros/lacros_service.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "ui/color/color_provider_manager.h"
 #include "ui/native_theme/native_theme_aura.h"
 
 namespace chromeos {
+namespace {
+
+ui::ColorProviderManager::SchemeVariant ConvertSchemeVariant(
+    color::mojom::SchemeVariant scheme) {
+  switch (scheme) {
+    case color::mojom::SchemeVariant::kTonalSpot:
+      return ui::ColorProviderManager::SchemeVariant::kTonalSpot;
+    case color::mojom::SchemeVariant::kNeutral:
+      return ui::ColorProviderManager::SchemeVariant::kNeutral;
+    case color::mojom::SchemeVariant::kVibrant:
+      return ui::ColorProviderManager::SchemeVariant::kVibrant;
+    case color::mojom::SchemeVariant::kExpressive:
+      return ui::ColorProviderManager::SchemeVariant::kExpressive;
+  }
+  // not reached
+}
+
+void SetSeedOnTheme(ui::NativeTheme* theme,
+                    const crosapi::mojom::NativeThemeInfoPtr& info) {
+  if (!info->seed_color.has_value()) {
+    return;
+  }
+
+  theme->set_user_color(*info->seed_color);
+}
+
+void SetVariantOnTheme(ui::NativeTheme* theme,
+                       const crosapi::mojom::NativeThemeInfoPtr& info) {
+  if (!info->scheme_variant.has_value()) {
+    return;
+  }
+
+  // TODO(b/286891722): Convert to an EnumTrait after circular dependency is
+  // resolved.
+  theme->set_scheme_variant(ConvertSchemeVariant(*info->scheme_variant));
+}
+
+}  // namespace
 
 NativeThemeCache::NativeThemeCache(const crosapi::mojom::NativeThemeInfo& info)
     : info_(info.Clone()) {
@@ -32,8 +73,11 @@ void NativeThemeCache::OnNativeThemeInfoChanged(
 
 void NativeThemeCache::SetNativeThemeInfo() {
   bool dark_mode = info_->dark_mode || ui::NativeTheme::IsForcedDarkMode();
+
   auto* native_theme = ui::NativeTheme::GetInstanceForNativeUi();
   native_theme->set_use_dark_colors(dark_mode);
+  SetSeedOnTheme(native_theme, info_);
+  SetVariantOnTheme(native_theme, info_);
   native_theme->NotifyOnNativeThemeUpdated();
 
   auto* native_theme_web = ui::NativeTheme::GetInstanceForWeb();
@@ -41,6 +85,8 @@ void NativeThemeCache::SetNativeThemeInfo() {
   native_theme_web->set_preferred_color_scheme(
       dark_mode ? ui::NativeTheme::PreferredColorScheme::kDark
                 : ui::NativeTheme::PreferredColorScheme::kLight);
+  SetSeedOnTheme(native_theme_web, info_);
+  SetVariantOnTheme(native_theme_web, info_);
   native_theme_web->NotifyOnNativeThemeUpdated();
 }
 
