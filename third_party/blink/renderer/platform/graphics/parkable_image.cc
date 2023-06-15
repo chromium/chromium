1f2214139f855c83b6cbc27c442fecac9afe033b
Thiabaud Engelbrecht <thiabaud@google.com>
2023-06-15 19:05:07 +0000

    [parkable_image] Enable by default
    Follow-up to https://chromium-review.googlesource.com/c/chromium/src/+/4608920, enabling the last feature we need.
    
    Change-Id: I6324cc3f15981f4f2e4200938bbf931c02e77e4d
    Bug: 1142834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614290
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158346}

--- third_party/blink/renderer/platform/graphics/parkable_image.cc
+++ third_party/blink/renderer/platform/graphics/parkable_image.cc
@@ -100,7 +100,7 @@ void AsanUnpoisonBuffer(RWBuffer* rw_buffer) {
 
 BASE_FEATURE(kUseParkableImageSegmentReader,
              "UseParkableImageSegmentReader",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 constexpr base::TimeDelta ParkableImageImpl::kParkingDelay;
 
