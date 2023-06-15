edce8902c6eefb7a8185fb4f6cd08774b942eb73
Benoit Lize <lizeb@chromium.org>
2023-06-15 16:08:24 +0000

    [cc] Fix device scale factor on HiDPI screens for MSAA computation
    We want to use 4x MSAA on HiDPI screens (since crbug.com/520243), as
    8x is wasteful and likely not visibly different. However, as pointed out
    by vasilyt@, we've been using 8x ~all the time on macOS, which is not
    what we intend.
    
    This is because the device scale factor computation was not updated to
    take into account that only one of device_scale_factor or
    painted_device_scale_factor are updated. This CL fixes that, which as a
    result moves HiDPI screens back to 4x MSAA, as we wanted.
    
    To assess real-life impact, gated the change behind a feature flag.
    
    (cherry picked from commit 1663cd244bbbab1e91e9de5392dc53d26f0ec031)
    
    Change-Id: I2d28db50edef7f18325721a9a6db6815fa3c5ba1
    Bug: 1451299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583519
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1153206}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615566
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5790@{#795}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -116,4 +116,9 @@ BASE_FEATURE(kMoreAggressiveSolidColorDetection,
 BASE_FEATURE(kReducedFrameRateEstimation,
              "kReducedFrameRateEstimation",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kDetectHiDpiForMsaa,
+             "DetectHiDpiForMsaa",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
