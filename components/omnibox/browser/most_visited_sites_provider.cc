d753c00dcb87f1ef2dd0c1f3de9cf2283cad9673
Tomasz Wiszkowski <ender@google.com>
2023-06-15 21:59:31 +0000

    Clean up unnecessary logging.
    An unfortunate leftover submitted with http://crrev/c/4616670, that
    i used to understand the issues with most visited sites provider test.
    
    Change-Id: Idf1f5e94205539fc32a55ec345a6ed194de9711c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617871
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158452}

--- components/omnibox/browser/most_visited_sites_provider.cc
+++ components/omnibox/browser/most_visited_sites_provider.cc
@@ -88,7 +88,6 @@ bool BuildTileSuggest(AutocompleteProvider* provider,
   }
 
   if (base::FeatureList::IsEnabled(omnibox::kMostVisitedTiles)) {
-    LOG(ERROR) << "<><><> HERE";
     AutocompleteMatch match = BuildMatch(
         provider, client, std::u16string(), GURL::EmptyGURL(),
         kMostVisitedTilesRelevance, AutocompleteMatchType::TILE_NAVSUGGEST);
