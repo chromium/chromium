ec8f26e18f191c72fa49b9ce9e787ecb7ada0a69
Thiago Perrotta <tperrotta@chromium.org>
2023-06-16 11:45:58 +0000

    BiDi mapper: pull.sh: use latest revision from 'main' by default
    Bug: chromedriver:4226
    Change-Id: If970824c92c9c4ebbef838fa3ae533665765e333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615791
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Thiago Perrotta <tperrotta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158763}

--- third_party/bidimapper/build.sh
+++ third_party/bidimapper/build.sh
@@ -4,31 +4,31 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-if [ ! -f 'revision.info' ]
+if [[ ! -f 'revision.info' ]]
 then
   echo "File not found: revision.info" >&2
   echo "Execute pull.sh first" >&2
   exit 1
 fi
 
-dt=$(cut --fields=1 --delimiter=',' "revision.info")
-revision=$(cut --fields=2 --delimiter=',' "revision.info")
-sha512=$(cut --fields=3 --delimiter=',' "revision.info")
+dt="$(cut --fields=1 --delimiter=',' "revision.info")"
+revision="$(cut --fields=2 --delimiter=',' "revision.info")"
+sha512="$(cut --fields=3 --delimiter=',' "revision.info")"
 
-if [ -z "$dt" -o -z "$revision" -o -z "$sha512" ]
+if [[ -z "$dt" || -z "$revision" || -z "$sha512" ]]
 then
   echo "Incorrect format of revision.info" >&2
   echo "Execute pull.sh first" >&2
   exit 1
 fi
 
-
 npm --prefix src install
 npm --prefix src run build
 
 cp src/lib/iife/mapperTab.js ./mapper.js
 
-cp README.chromium.in README.chromium
-sed --in-place --regexp-extended "s/\\$\\{DATE\\}/$dt/gi" README.chromium
-sed --in-place --regexp-extended "s/\\$\\{REVISION\\}/$revision/gi" README.chromium
-sed --in-place --regexp-extended "s/\\$\\{TAR-SHA512\\}/$sha512/gi" README.chromium
+sed --regexp-extended \
+  --expression "s/\\$\\{DATE\\}/$dt/gi" \
+  --expression "s/\\$\\{REVISION\\}/$revision/gi" \
+  --expression "s/\\$\\{TAR-SHA512\\}/$sha512/gi" \
+  README.chromium.in > README.chromium
--- third_party/bidimapper/pull.sh
+++ third_party/bidimapper/pull.sh
@@ -8,23 +8,19 @@
 set -e
 
 revision="$1"
-
-if [ -z "$revision" ]
-then
-  echo "Usage: $0 git-revision-full-sha1" >&2
-  exit 1
+if [[ -z "$revision" ]]; then
+  # Fall back to latest revision when it is unspecified
+  revision="$(curl -H "Accept: application/vnd.github.VERSION.sha" https://api.github.com/repos/GoogleChromeLabs/chromium-bidi/commits/main)"
 fi
 
-[ -f "chromium-bidi.zip" ] && rm "chromium-bidi.zip"
-[ -d "src" ] && rm --recursive --force "src"
-
+[[ -f "chromium-bidi.zip" ]] && rm "chromium-bidi.zip"
+[[ -d "src" ]] && rm -rf "src"
 
 wget --output-document="chromium-bidi.zip" "https://github.com/GoogleChromeLabs/chromium-bidi/archive/$revision.zip"
 unzip "chromium-bidi.zip"
 mv "chromium-bidi-$revision" src
 
-date=$(date "+%Y-%m-%d")
-sha512=$(sha512sum --binary "chromium-bidi.zip" | cut --fields=1 --delimiter=' ')
+date="$(date "+%Y-%m-%d")"
+sha512="$(sha512sum --binary "chromium-bidi.zip" | cut --fields=1 --delimiter=' ')"
 rm "chromium-bidi.zip"
 echo "$date,$revision,$sha512" > "revision.info"
-
