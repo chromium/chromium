c8e133da88014a31801502bc6fb13e71e0a0c00f
Avi Drissman <avi@chromium.org>
2023-06-16 01:10:12 +0000

    Extend /base/apple/owned_objc to have weak types as well
    These will be useful for gfx::NativeView/NativeWidget work.
    
    Bug: 1280317
    Change-Id: Id9dda131844b61194209690c56b99921d70b3b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4610554
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1158555}

--- base/apple/owned_objc.h
+++ base/apple/owned_objc.h
@@ -10,19 +10,26 @@
 #include "base/base_export.h"
 #include "build/build_config.h"
 
-// This file defines wrappers to allow C++ code to own Objective-C objects
-// without being Objective-C++ code themselves. These should not be used for
-// pure Objective-C++ code, in which the underlying Objective-C types should be
-// used, nor should they be used for the case where the pimpl idiom would work
+// This file defines wrappers to allow C++ code to hold references to
+// Objective-C objects (either strong or weak) without being Objective-C++ code
+// themselves. These should not be used for pure Objective-C++ code, in which
+// the underlying Objective-C types should be used, nor should they be used for
+// the case where the pimpl idiom would work
 // (https://chromium.googlesource.com/chromium/src/+/main/docs/mac/mixing_cpp_and_objc.md).
 
 #if __OBJC__
 
-#define GENERATE_OWNED_OBJC_TYPE(name) @class name;
-#define GENERATE_OWNED_OBJC_PROTOCOL(name) @protocol name;
+#define GENERATE_STRONG_OBJC_TYPE(name) @class name;
+#define GENERATE_STRONG_OBJC_PROTOCOL(name) @protocol name;
+#define GENERATE_WEAK_OBJC_TYPE(name) @class name;
+#define GENERATE_WEAK_OBJC_PROTOCOL(name) @protocol name;
+
 #include "base/apple/owned_objc_types.h"
-#undef GENERATE_OWNED_OBJC_TYPE
-#undef GENERATE_OWNED_OBJC_PROTOCOL
+
+#undef GENERATE_STRONG_OBJC_TYPE
+#undef GENERATE_STRONG_OBJC_PROTOCOL
+#undef GENERATE_WEAK_OBJC_TYPE
+#undef GENERATE_WEAK_OBJC_PROTOCOL
 
 #endif  // __OBJC__
 
@@ -31,43 +38,48 @@
 // C++-compatible way that does not expose any Objective-C code and only allows
 // default construction and validity checking.
 #if __OBJC__
-#define OWNED_TYPE_DECL_OBJC_ADDITIONS(name, objctype) \
-  explicit Owned##name(objctype obj);                  \
+#define OWNED_TYPE_DECL_OBJC_ADDITIONS(classname, objctype) \
+  explicit classname(objctype obj);                         \
   objctype Get() const;
 #else
-#define OWNED_TYPE_DECL_OBJC_ADDITIONS(name, objctype)
+#define OWNED_TYPE_DECL_OBJC_ADDITIONS(classname, objctype)
 #endif  // __OBJC__
 
-#define OWNED_OBJC_DECL(name, objctype)                       \
-  namespace base::apple {                                     \
-  class BASE_EXPORT Owned##name {                             \
-   public:                                                    \
-    /* Default-construct in a null state. */                  \
-    Owned##name();                                            \
-    ~Owned##name();                                           \
-    Owned##name(const Owned##name&);                          \
-    Owned##name& operator=(const Owned##name&);               \
-    /* Returns whether the object contains a valid object. */ \
-    explicit operator bool() const;                           \
-    /* Comparisons. */                                        \
-    bool operator==(const Owned##name& other) const;          \
-    bool operator!=(const Owned##name& other) const;          \
-    /* Objective-C-only constructor and getter. */            \
-    OWNED_TYPE_DECL_OBJC_ADDITIONS(name, objctype)            \
-                                                              \
-   private:                                                   \
-    struct ObjCStorage;                                       \
-    std::unique_ptr<ObjCStorage> objc_storage_;               \
-  };                                                          \
+#define OWNED_OBJC_DECL(classname, objctype)                 \
+  namespace base::apple {                                    \
+  class BASE_EXPORT classname {                              \
+   public:                                                   \
+    /* Default-construct in a null state. */                 \
+    classname();                                             \
+    ~classname();                                            \
+    classname(const classname&);                             \
+    classname& operator=(const classname&);                  \
+    /* Returns whether the object contains a valid object.*/ \
+    explicit operator bool() const;                          \
+    /* Comparisons. */                                       \
+    bool operator==(const classname& other) const;           \
+    bool operator!=(const classname& other) const;           \
+    /* Objective-C-only constructor and getter. */           \
+    OWNED_TYPE_DECL_OBJC_ADDITIONS(classname, objctype)      \
+                                                             \
+   private:                                                  \
+    struct ObjCStorage;                                      \
+    std::unique_ptr<ObjCStorage> objc_storage_;              \
+  };                                                         \
   }  // namespace base::apple
 
-#define GENERATE_OWNED_OBJC_TYPE(name) OWNED_OBJC_DECL(name, name*)
-#define GENERATE_OWNED_OBJC_PROTOCOL(name) OWNED_OBJC_DECL(name, id<name>)
+#define GENERATE_STRONG_OBJC_TYPE(name) OWNED_OBJC_DECL(Owned##name, name*)
+#define GENERATE_STRONG_OBJC_PROTOCOL(name) \
+  OWNED_OBJC_DECL(Owned##name, id<name>)
+#define GENERATE_WEAK_OBJC_TYPE(name) OWNED_OBJC_DECL(Weak##name, name*)
+#define GENERATE_WEAK_OBJC_PROTOCOL(name) OWNED_OBJC_DECL(Weak##name, id<name>)
 
 #include "base/apple/owned_objc_types.h"
 
-#undef GENERATE_OWNED_OBJC_TYPE
-#undef GENERATE_OWNED_OBJC_PROTOCOL
+#undef GENERATE_STRONG_OBJC_TYPE
+#undef GENERATE_STRONG_OBJC_PROTOCOL
+#undef GENERATE_WEAK_OBJC_TYPE
+#undef GENERATE_WEAK_OBJC_PROTOCOL
 #undef OWNED_OBJC_DECL
 #undef OWNED_TYPE_DECL_OBJC_ADDITIONS
 
--- base/apple/owned_objc.mm
+++ base/apple/owned_objc.mm
@@ -12,43 +12,49 @@
 #error "This file requires ARC support."
 #endif
 
-#define OWNED_OBJC_IMPL(name, objctype)                                \
-  namespace base::apple {                                              \
-  struct Owned##name::ObjCStorage {                                    \
-    objctype __strong obj;                                             \
-  };                                                                   \
-  Owned##name::Owned##name()                                           \
-      : objc_storage_(std::make_unique<ObjCStorage>()) {}              \
-  Owned##name::~Owned##name() = default;                               \
-  Owned##name::Owned##name(objctype obj) : Owned##name() {             \
-    objc_storage_->obj = obj;                                          \
-  }                                                                    \
-  Owned##name::Owned##name(const Owned##name& other) : Owned##name() { \
-    objc_storage_->obj = other.objc_storage_->obj;                     \
-  }                                                                    \
-  Owned##name& Owned##name::operator=(const Owned##name& other) {      \
-    objc_storage_->obj = other.objc_storage_->obj;                     \
-    return *this;                                                      \
-  }                                                                    \
-  Owned##name::operator bool() const {                                 \
-    return objc_storage_->obj != nil;                                  \
-  }                                                                    \
-  bool Owned##name::operator==(const Owned##name& other) const {       \
-    return objc_storage_->obj == other.objc_storage_->obj;             \
-  }                                                                    \
-  bool Owned##name::operator!=(const Owned##name& other) const {       \
-    return !this->operator==(other);                                   \
-  }                                                                    \
-  objctype Owned##name::Get() const {                                  \
-    return objc_storage_->obj;                                         \
-  }                                                                    \
+#define OWNED_OBJC_IMPL(classname, objctype, ownership)                      \
+  namespace base::apple {                                                    \
+  struct classname::ObjCStorage {                                            \
+    objctype ownership obj;                                                  \
+  };                                                                         \
+  classname::classname() : objc_storage_(std::make_unique<ObjCStorage>()) {} \
+  classname::~classname() = default;                                         \
+  classname::classname(objctype obj) : classname() {                         \
+    objc_storage_->obj = obj;                                                \
+  }                                                                          \
+  classname::classname(const classname& other) : classname() {               \
+    objc_storage_->obj = other.objc_storage_->obj;                           \
+  }                                                                          \
+  classname& classname::operator=(const classname& other) {                  \
+    objc_storage_->obj = other.objc_storage_->obj;                           \
+    return *this;                                                            \
+  }                                                                          \
+  classname::operator bool() const {                                         \
+    return objc_storage_->obj != nil;                                        \
+  }                                                                          \
+  bool classname::operator==(const classname& other) const {                 \
+    return objc_storage_->obj == other.objc_storage_->obj;                   \
+  }                                                                          \
+  bool classname::operator!=(const classname& other) const {                 \
+    return !this->operator==(other);                                         \
+  }                                                                          \
+  objctype classname::Get() const {                                          \
+    return objc_storage_->obj;                                               \
+  }                                                                          \
   }  // namespace base::apple
 
-#define GENERATE_OWNED_OBJC_TYPE(name) OWNED_OBJC_IMPL(name, name*)
-#define GENERATE_OWNED_OBJC_PROTOCOL(name) OWNED_OBJC_IMPL(name, id<name>)
+#define GENERATE_STRONG_OBJC_TYPE(name) \
+  OWNED_OBJC_IMPL(Owned##name, name*, __strong)
+#define GENERATE_STRONG_OBJC_PROTOCOL(name) \
+  OWNED_OBJC_IMPL(Owned##name, id<name>, __strong)
+#define GENERATE_WEAK_OBJC_TYPE(name) OWNED_OBJC_IMPL(Weak##name, name*, __weak)
+#define GENERATE_WEAK_OBJC_PROTOCOL(name) \
+  OWNED_OBJC_IMPL(Weak##name, id<name>, __weak)
 
 #include "base/apple/owned_objc_types.h"
 
-#undef GENERATE_OWNED_OBJC_TYPE
-#undef GENERATE_OWNED_OBJC_PROTOCOL
+#undef GENERATE_STRONG_OBJC_TYPE
+#undef GENERATE_STRONG_OBJC_PROTOCOL
+#undef GENERATE_WEAK_OBJC_TYPE
+#undef GENERATE_WEAK_OBJC_PROTOCOL
 #undef OWNED_OBJC_IMPL
