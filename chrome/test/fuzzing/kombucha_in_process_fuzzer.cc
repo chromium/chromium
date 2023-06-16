18c265baded7c1b01459d1d6025bae4b12631049
xrosado <xrosado@google.com>
2023-06-16 17:30:04 +0000

    Refactor kombucha fuzzer to use libprotomutator
    protobuf file now contains element identifiers to target
    
    Bug: 1451928
    Change-Id: I4887dc61c3470bbcfd20388d0e192ec4f81c3e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4615749
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Xavier Rosado <xrosado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1158934}

--- chrome/test/fuzzing/kombucha_in_process_fuzzer.cc
+++ chrome/test/fuzzing/kombucha_in_process_fuzzer.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuzzer/FuzzedDataProvider.h>
+#include <google/protobuf/descriptor.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <cstdint>
@@ -18,11 +18,13 @@
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/fuzzing/in_process_fuzzer.h"
+#include "chrome/test/fuzzing/kombucha_in_process_fuzzer.pb.h"
 #include "chrome/test/interaction/interaction_test_util_browser.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/libfuzzer/proto/lpm_interface.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/interactive_test.h"
 
@@ -86,34 +88,25 @@ class KombuchaInProcessFuzzer
     InteractiveBrowserTestT::SetUp();
   }
   void SetUpOnMainThread() override;
+
 #if BUILDFLAG(IS_WIN)
   void TearDown() override {
     InteractiveBrowserTestT::TearDown();
     com_initializer_.reset();
   }
+
 #endif
+  using FuzzCase = test::fuzzing::ui_fuzzing::FuzzCase;
   int Fuzz(const uint8_t* data, size_t size) override;
   static std::unique_ptr<net::test_server::HttpResponse> HandleHTTPRequest(
       base::WeakPtr<KombuchaInProcessFuzzer> fuzzer_weak,
       const net::test_server::HttpRequest& request);
 
-  std::string current_fuzz_case_;
+  FuzzCase current_fuzz_case_;
 
-  // ElementIdentifiers that can be targeted by PressButton
-  std::vector<ui::ElementIdentifier> button_elements = {
-      kBackButtonElementId,           kDownloadToolbarButtonElementId,
-      kForwardButtonElementId,        kMediaButtonElementId,
-      kNewTabButtonElementId,         kSidePanelButtonElementId,
-      kSidePanelCloseButtonElementId, kSidePanelOpenInNewTabButtonElementId,
-      kSidePanelPinButtonElementId,   kTabAlertIndicatorButtonElementId,
-      kTabCounterButtonElementId,     kTabSearchButtonElementId};
-
-  // Element Identifiers that can be targeted by SelectMenuItem
-  std::vector<ui::ElementIdentifier> menu_elements = {
-      AppMenuModel::kBookmarksMenuItem, AppMenuModel::kDownloadsMenuItem,
-      AppMenuModel::kHistoryMenuItem,   AppMenuModel::kExtensionsMenuItem,
-      AppMenuModel::kMoreToolsMenuItem, AppMenuModel::kPasswordManagerMenuItem};
-
+  // TODO(xrosado) Replace EnsurePresent with non crashing variant
+  // EnsurePresent hits check if item isn't present
+  // Pollutes corpus with useless crashes
   auto CheckAndSelectMenuItem(ui::ElementIdentifier item) {
     return Steps(EnsurePresent(item), SelectMenuItem(item));
   }
@@ -127,6 +120,18 @@ class KombuchaInProcessFuzzer
                  SelectMenuItem(BookmarkSubMenuModel::kShowBookmarkBarMenuItem),
                  WaitForShow(kBookmarkBarElementId));
   }
+
+  // Enum descriptors for protobuf messages
+  // Allows for a kombucha verb to function independent of what element
+  // it's targeting
+  raw_ptr<const google::protobuf::EnumDescriptor> button_descriptor =
+      raw_ptr(test::fuzzing::ui_fuzzing::Button_descriptor());
+
+  raw_ptr<const google::protobuf::EnumDescriptor> menu_item_descriptor =
+      raw_ptr(test::fuzzing::ui_fuzzing::MenuItem_descriptor());
+
+  raw_ptr<const google::protobuf::EnumDescriptor> accelerator_descriptor =
+      raw_ptr(test::fuzzing::ui_fuzzing::Accelerator_descriptor());
 
   ui::Accelerator fullscreen_accelerator_;
   ui::Accelerator close_tab_accelerator_;
@@ -167,7 +172,7 @@ KombuchaInProcessFuzzer::HandleHTTPRequest(
   std::unique_ptr<net::test_server::BasicHttpResponse> response;
   response = std::make_unique<net::test_server::BasicHttpResponse>();
   response->set_content_type("text/html");
-  std::string response_body = "";
+  FuzzCase response_body;
   // We are running on the embedded test server's thread.
   // We want to ask the fuzzer thread for the latest payload,
   // but there's a risk of UaF if it's being destroyed.
@@ -184,18 +189,24 @@ KombuchaInProcessFuzzer::HandleHTTPRequest(
       });
   content::GetUIThreadTaskRunner()->PostTask(FROM_HERE, get_payload_lambda);
   run_loop.Run();
-  response->set_content(response_body);
+  std::string proto_debug_str = response_body.DebugString();
+
+  response->set_content(base::StringPrintf(
+      "<html><body><h1>hello world</h1><p>%s</p></body></html>",
+      proto_debug_str.c_str()));
   response->set_code(net::HTTP_OK);
   return response;
 }
 
 int KombuchaInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
-  std::string html_string(reinterpret_cast<const char*>(data), size);
-  current_fuzz_case_ = html_string;
+  FuzzCase fuzz_case;
+  fuzz_case.ParseFromArray(data, size);
+
+  current_fuzz_case_ = fuzz_case;
+
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kPrimaryTabElementId);
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondaryTabElementId);
   GURL test_url = embedded_test_server()->GetURL("/test.html");
-  FuzzedDataProvider data_provider = FuzzedDataProvider(data, size);
 
   // Base input always used in fuzzer
   // Start with three tabs
@@ -204,57 +215,74 @@ int KombuchaInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
             InstrumentTab(kPrimaryTabElementId, 0),
             AddInstrumentedTab(kSecondaryTabElementId, GURL("about:blank")),
             Log("Passed initial setup steps"));
-  // Always consume 3 bytes as operations
-  while (data_provider.remaining_bytes() >= 3) {
-    std::vector<uint8_t> ops = data_provider.ConsumeBytes<uint8_t>(3);
 
-    // Byte to determine which item to target
-    uint8_t action = ops.at(1);
-    ui::ElementIdentifier item;
+  auto input_buffer = Steps(Log("Began procedurally generated inputs"));
 
-    // TODO(xrosado): Use first byte as eventual decider between normal run or
-    // parallel
-    switch (action % 8) {
-      case 1:
-        item = button_elements[ops.at(2) % (button_elements.size())];
-        AddStep(ui_input,
-                Steps(CheckAndPressButton(item), Log("Hit pressbutton")));
+  // Action can have arbitrary number of steps
+  // Translate and append each step to run at once
+  test::fuzzing::ui_fuzzing::Action action = fuzz_case.action();
+  for (int j = 0; j < action.steps_size(); j++) {
+    test::fuzzing::ui_fuzzing::Step step = action.steps(j);
+
+    switch (step.step_choice_case()) {
+      case test::fuzzing::ui_fuzzing::Step::kPressButton: {
+        test::fuzzing::ui_fuzzing::Button button = step.press_button().target();
+        ui::ElementIdentifier target = ui::ElementIdentifier::FromName(
+            button_descriptor->FindValueByNumber(button)->name().c_str());
+        AddStep(input_buffer, CheckAndPressButton(target));
+
         break;
-      case 2:
-        AddStep(
-            ui_input,
-            Steps(SelectTab(kTabStripElementId,
-                            ops.at(2) % browser()->tab_strip_model()->count()),
-                  Log("Hit select tab")));
+      }
+      case test::fuzzing::ui_fuzzing::Step::kSelectTab:
+        AddStep(input_buffer,
+                Steps(SelectTab(kTabStripElementId,
+                                step.select_tab().target() %
+                                    browser()->tab_strip_model()->count())));
+
         break;
-      case 3:
-        AddStep(ui_input, Steps(ShowBookmarksBar(), Log("Hit bookmarks bar")));
+      case test::fuzzing::ui_fuzzing::Step::kSelectMenuItem: {
+        test::fuzzing::ui_fuzzing::MenuItem item =
+            step.select_menu_item().target();
+        ui::ElementIdentifier target = ui::ElementIdentifier::FromName(
+            menu_item_descriptor->FindValueByNumber(item)->name().c_str());
+        AddStep(input_buffer,
+                Steps(CheckAndPressButton(kAppMenuButtonElementId),
+                      CheckAndSelectMenuItem(target)));
         break;
-      case 4:
-        item = menu_elements[ops.at(2) % (menu_elements.size())];
-        AddStep(ui_input, Steps(CheckAndPressButton(kAppMenuButtonElementId),
-                                CheckAndSelectMenuItem(item),
-                                Log("Hit select menu item")));
+      }
+      case test::fuzzing::ui_fuzzing::Step::kSendAccelerator: {
+        switch (step.send_accelerator().target()) {
+          case test::fuzzing::ui_fuzzing::Accelerator::fullscreen_accelerator_:
+            AddStep(input_buffer, SendAccelerator(kBrowserViewElementId,
+                                                  fullscreen_accelerator_));
+            break;
+          case test::fuzzing::ui_fuzzing::Accelerator::closetab_accelerator_:
+
+            AddStep(input_buffer, SendAccelerator(kBrowserViewElementId,
+                                                  close_tab_accelerator_));
+            break;
+          case test::fuzzing::ui_fuzzing::Accelerator::grouptab_accelerator_:
+            AddStep(input_buffer,
+                    SendAccelerator(kBrowserViewElementId,
+                                    group_target_tab_accelerator_));
+            break;
+          default:  // Unspecified Value
+            break;
+        }
         break;
-      case 5:
-        AddStep(ui_input, Steps(SendAccelerator(kBrowserViewElementId,
-                                                fullscreen_accelerator_),
-                                Log("Hit FullScreen accelerator")));
-        break;
-      case 6:
-        AddStep(ui_input, Steps(SendAccelerator(kBrowserViewElementId,
-                                                close_tab_accelerator_),
-                                Log("Hit Close Tab accelerator")));
-        break;
-      case 7:
-        AddStep(ui_input, Steps(SendAccelerator(kBrowserViewElementId,
-                                                group_target_tab_accelerator_),
-                                Log("Hit Group Tab accelerator")));
-        break;
-      default:
+      }
+      default:  // Unspecified Value
         break;
     }
   }
+
+  if (action.has_parallel_flag()) {
+    // TODO(xrosado) Add InParallel() and AnyOf() case in future
+  } else {
+    // Join ui_input with input_buffer to one input
+    ui_input = Steps(std::move(ui_input), std::move(input_buffer));
+  }
+
   // POC for JS execution
   // TODO(xrosado) Generalize to more inputs and convert to a case
   std::string key_event_js =
@@ -278,3 +306,5 @@ int KombuchaInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
 }
 
 REGISTER_IN_PROCESS_FUZZER(KombuchaInProcessFuzzer)
+DEFINE_BINARY_PROTO_IN_PROCESS_FUZZER(
+    KombuchaInProcessFuzzer::FuzzCase testcase)
