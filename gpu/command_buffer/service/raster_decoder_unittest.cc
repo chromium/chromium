b5194a35f2911846c78fbb7d5073175d85c86658
Colin Blundell <blundell@chromium.org>
2023-06-19 14:21:47 +0000

    [GPU] Port raster_decoder_unittest.cc away from ResourceFormat
    A straightforward conversion.
    
    Bug: 1378708
    Change-Id: I93f0fb3113d0032d1abaef954fd6dc97bc39ee53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622402
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159610}

--- gpu/command_buffer/service/raster_decoder_unittest.cc
+++ gpu/command_buffer/service/raster_decoder_unittest.cc
@@ -13,7 +13,6 @@
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
-#include "components/viz/common/resources/resource_format.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
 #include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/raster_cmd_format.h"
@@ -234,7 +233,7 @@ class RasterDecoderOOPTest : public testing::Test, DecoderClient {
         /*is_for_display_compositor=*/false);
 
     client_texture_mailbox_ =
-        CreateMailbox(viz::ResourceFormat::RGBA_8888, /*width=*/2,
+        CreateMailbox(viz::SinglePlaneFormat::kRGBA_8888, /*width=*/2,
                       /*height=*/2, /*cleared=*/false);
 
     // When creating the mailbox, we create a WrappedSkImage shared image which
@@ -289,17 +288,15 @@ class RasterDecoderOOPTest : public testing::Test, DecoderClient {
     return decoder;
   }
 
-  gpu::Mailbox CreateMailbox(viz::ResourceFormat resource_format,
+  gpu::Mailbox CreateMailbox(viz::SharedImageFormat format,
                              GLsizei width,
                              GLsizei height,
                              bool cleared) {
     gpu::Mailbox mailbox = gpu::Mailbox::GenerateForSharedImage();
     gfx::Size size(width, height);
     auto color_space = gfx::ColorSpace::CreateSRGB();
-    viz::SharedImageFormat si_format =
-        viz::SharedImageFormat::SinglePlane(resource_format);
     shared_image_factory_->CreateSharedImage(
-        mailbox, si_format, size, color_space, kTopLeft_GrSurfaceOrigin,
+        mailbox, format, size, color_space, kTopLeft_GrSurfaceOrigin,
         kPremul_SkAlphaType, gpu::kNullSurfaceHandle, SHARED_IMAGE_USAGE_RASTER,
         "TestLabel");
 
@@ -380,7 +377,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DSizeMismatch) {
   context_state_->set_need_context_state_reset(true);
   // Create uninitialized source texture mailbox.
   gpu::Mailbox source_texture_mailbox =
-      CreateMailbox(viz::ResourceFormat::RGBA_8888,
+      CreateMailbox(viz::SinglePlaneFormat::kRGBA_8888,
                     /*width=*/1, /*height=*/1,
                     /*cleared=*/true);
   GLbyte mailboxes[sizeof(gpu::Mailbox) * 2];
@@ -423,7 +420,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DTwiceClearsUnclearedTexture) {
   context_state_->set_need_context_state_reset(true);
   // Create uninitialized source texture mailbox.
   gpu::Mailbox source_texture_mailbox =
-      CreateMailbox(viz::ResourceFormat::RGBA_8888,
+      CreateMailbox(viz::SinglePlaneFormat::kRGBA_8888,
                     /*width=*/2, /*height=*/2,
                     /*cleared=*/true);
   GLbyte mailboxes[sizeof(gpu::Mailbox) * 2];
@@ -460,7 +457,7 @@ TEST_F(RasterDecoderOOPTest, CopyTexSubImage2DPartialFailsWithUnalignedRect) {
   context_state_->set_need_context_state_reset(true);
   // Create uninitialized source texture mailbox.
   gpu::Mailbox source_texture_mailbox =
-      CreateMailbox(viz::ResourceFormat::RGBA_8888,
+      CreateMailbox(viz::SinglePlaneFormat::kRGBA_8888,
                     /*width=*/2, /*height=*/2,
                     /*cleared=*/true);
   GLbyte mailboxes[sizeof(gpu::Mailbox) * 2];
