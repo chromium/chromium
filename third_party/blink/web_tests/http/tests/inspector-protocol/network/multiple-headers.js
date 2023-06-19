2cb06c4bab72f7e1580a98211a214b0645e0b19b
Danil Somsikov <dsv@chromium.org>
2023-06-19 09:30:17 +0000

    Delfake inspector-protocol/network/multiple-headers.js
    It looks like loadingFinished might be sent by the browser after
    it sends reponseReceivedExtraInfo but before reponseReceived get
    delievered from the renderer and the test misses it.
    
    Bug: 1415385
    Change-Id: Ide53d276a2a09cc6d1fb411423eea239de6b7373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4626218
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1159511}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/multiple-headers.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/multiple-headers.js
@@ -3,36 +3,26 @@
       '../resources/test-page.html',
       `Tests that multiple HTTP headers with same name are correctly folded into one LF-separated line.`);
 
-  window.onerror = (msg) => testRunner.log('onerror: ' + msg);
-  window.onunhandledrejection = (e) =>
-      testRunner.log('onunhandledrejection: ' + e.reason);
-  let errorForLog = new Error();
-  setTimeout(() => {
-    testRunner.die('Timeout', errorForLog);
-  }, 5000);
-
   const url = 'http://127.0.0.1:8000/inspector-protocol/network/resources/multiple-headers.php';
   await dp.Network.enable();
   await dp.Page.enable();
 
   session.evaluate(`fetch("${url}?fetch=1").then(r => r.text())`);
-  errorForLog = new Error();
   const [fetchResponse, fetchResponseExtraInfo] = await Promise.all([
     dp.Network.onceResponseReceived(),
-    dp.Network.onceResponseReceivedExtraInfo()]);
+    dp.Network.onceResponseReceivedExtraInfo(),
+    dp.Network.onceLoadingFinished()
+  ]);
   testRunner.log(`Pragma header of fetch of ${fetchResponse.params.response.url}:`);
   testRunner.log(`Network.responseReceived: ${fetchResponse.params.response.headers['Access-Control-Pragma']}`);
   testRunner.log(`Network.responseReceivedExtraInfo: ${fetchResponseExtraInfo.params.headers['Access-Control-Pragma']}`);
   testRunner.log('');
-  errorForLog = new Error();
-  await dp.Network.onceLoadingFinished();
 
   session.evaluate(`
     var f = document.createElement('frame');
     f.src = "${url}";
     document.body.appendChild(f);
   `);
-  errorForLog = new Error();
   const [navigationResponse, navigationResponseExtraInfo] = await Promise.all([
     dp.Network.onceResponseReceived(),
     dp.Network.onceResponseReceivedExtraInfo()]);
