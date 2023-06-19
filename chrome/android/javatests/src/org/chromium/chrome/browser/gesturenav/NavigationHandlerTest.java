bff72ead854e75b3708cf0cf56db554101c52456
Samar Chehade-Lepleux <samarchehade@google.com>
2023-06-19 10:25:02 +0000

    Revert "[Android] Re-enable NavigationHandlerTest#testLeft...Link_with..Transition"
    This reverts commit cfe883721e113645517ceeeca82882623029454e.
    
    Reason for revert: https://crbug.com/1455901
    
    Original change's description:
    > [Android] Re-enable NavigationHandlerTest#testLeft...Link_with..Transition
    >
    > The disabled test has passed lately. Internally the flow takes exactly
    > the same path as the other variation. We may safely re-enable this
    > test.
    >
    > Bug: 1426201
    > Change-Id: I8eeea98b5d9f4f67fd5a89fdd245703c1f3a3c68
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4608652
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    > Reviewed-by: Lijin Shen <lazzzis@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1158181}
    
    Bug: 1426201,1455901
    Change-Id: I5d2fa16af241a9cb16200b633a6df8f4df5daf2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4624569
    Owners-Override: Samar Chehade-Lepleux <samarchehade@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1159531}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
@@ -193,6 +193,7 @@ public class NavigationHandlerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/1426201")
     public void testLeftEdgeSwipeClosesTabLaunchedFromLink_withBackForwardTransition() {
         FeatureList.setTestFeatures(Map.of(ChromeFeatureList.BACK_FORWARD_TRANSITIONS, true));
         testLeftEdgeSwipeClosesTabLaunchedFromLinkInternal();
