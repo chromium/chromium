fd1fdb6080894eb97e5752b1e4e930109963b972
David Bokan <bokan@chromium.org>
2023-06-20 16:12:57 +0000

    Revert "Clear last_focused_node in AX manager after load event is received"
    This reverts commit 0a1a41b4ff6b816af7c51c1d75b9f2aaa776b8ad.
    
    Reason for revert: Broke screen readers starting position https://crbug.com/1453789
    
    Original change's description:
    > Clear last_focused_node in AX manager after load event is received
    >
    > This ensures that a focus event is fired, even if a previous partial
    > AX update contained the same focused node.
    >
    > Change-Id: I8708e592566087f3907b22f423fd4bf4f9c5a314
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4367844
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Commit-Queue: Stefan Zager <szager@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1127207}
    
    Change-Id: Ie02f0ae1ed0f9441818e324490c029c6532655d3
    Bug: 1453789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617714
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160068}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -572,10 +572,6 @@ bool BrowserAccessibilityManager::OnAccessibilityEvents(
   }
 
   if (received_load_complete_event) {
-    // Clearing the focused node first ensures that the focus event isn't
-    // suppressed in the case where focus was on the same node as in a previous
-    // update.
-    SetLastFocusedNode(nullptr);
     // Fire a focus event after the document has finished loading, but after all
     // the platform independent events have already fired, e.g. kLayoutComplete.
     // Some screen readers need a focus event in order to work properly.
