f3d270d713089849b654f088ab2791c37fb8ad93
Xiaocheng Hu <xiaochengh@chromium.org>
2023-06-21 22:11:29 +0000

    Revert "Add functionality to PolicyLoaderLocalTest"
    This reverts commit 58676c89605f20192352cff623abd1857dfbac4c.
    
    Reason for revert: It depends on a previous patch that has been
    reverted https://chromium-review.googlesource.com/c/chromium/src/+/4610117
    
    Original change's description:
    > Add functionality to PolicyLoaderLocalTest
    >
    > Wrote load function for policy loader. Policies are loaded from a json string in the loader class.
    >
    > Bug: b:286422730
    > Change-Id: Ifcd28a5bd5d1aaabaf4902a9e376d797a7cf7fc9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619664
    > Commit-Queue: Jayee Zheng <jayee@google.com>
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1160795}
    
    Bug: b:286422730
    Change-Id: I21327b1f877a09a4801c3a02916761d6927dead2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634528
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Owners-Override: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160893}

--- components/policy/core/common/policy_loader_local_test.cc
+++ components/policy/core/common/policy_loader_local_test.cc
@@ -4,10 +4,7 @@
 
 #include "components/policy/core/common/policy_loader_local_test.h"
 
-#include <string>
-
 #include "base/json/json_reader.h"
-#include "base/logging.h"
 #include "components/policy/core/common/policy_bundle.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_namespace.h"
@@ -21,60 +18,11 @@ PolicyLoaderLocalTest::PolicyLoaderLocalTest() = default;
 PolicyLoaderLocalTest::~PolicyLoaderLocalTest() = default;
 
 PolicyBundle PolicyLoaderLocalTest::Load() {
-  return bundle_.Clone();
-}
-
-void PolicyLoaderLocalTest::SetPolicyListJson(
-    const std::string& policy_list_json) {
   PolicyBundle bundle;
 
-  auto policies = base::JSONReader::ReadAndReturnValueWithError(
-      policy_list_json, base::JSONParserOptions::JSON_ALLOW_TRAILING_COMMAS);
+  // TODO (b:286422730): Load policies from json file.
 
-  CHECK(policies.has_value() && policies->is_list())
-      << "List of policies expected";
-
-  PolicyMap& policy_map =
-      bundle.Get(PolicyNamespace(POLICY_DOMAIN_CHROME, std::string()));
-
-  for (auto& policy : policies->GetList()) {
-    CHECK(policy.is_dict())
-        << "A dictionary is expected for each policy definition";
-    base::Value::Dict* policy_dict = &policy.GetDict();
-
-    VerifyJsonContents(policy_dict);
-
-    PolicyLevel level =
-        static_cast<PolicyLevel>(*policy_dict->FindInt("level"));
-    PolicyScope scope =
-        static_cast<PolicyScope>(*policy_dict->FindInt("scope"));
-    PolicySource source =
-        static_cast<PolicySource>(*policy_dict->FindInt("source"));
-    std::string name = *policy_dict->FindString("name");
-    base::Value value = policy_dict->Find("value")->Clone();
-
-    PolicyMap::Entry entry(level, scope, source, std::move(value), nullptr);
-    policy_map.Set(name, std::move(entry));
-  }
-
-  bundle_ = std::move(bundle);
-}
-
-void PolicyLoaderLocalTest::VerifyJsonContents(base::Value::Dict* policy_dict) {
-  auto level_int = policy_dict->FindInt("level");
-  CHECK(level_int.has_value() &&
-        *level_int <= static_cast<int>(PolicyLevel::POLICY_LEVEL_MAX))
-      << "Invalid level found";
-  auto scope_int = policy_dict->FindInt("scope");
-  CHECK(scope_int.has_value() &&
-        *scope_int <= static_cast<int>(PolicyScope::POLICY_SCOPE_MAX))
-      << "Invalid scope found";
-  auto source_int = policy_dict->FindInt("source");
-  CHECK(source_int.has_value() &&
-        *source_int < static_cast<int>(PolicySource::POLICY_SOURCE_COUNT))
-      << "Invalid source found";
-  CHECK(policy_dict->FindString("name")) << "Invalid name found";
-  CHECK(policy_dict->contains("value")) << "Invalid value found";
+  return bundle;
 }
 
 }  // namespace policy
