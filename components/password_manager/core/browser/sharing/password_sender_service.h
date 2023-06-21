b006e0aaaff1e4e65b2c18bb8eb047ee55c37828
markusheintz <markusheintz@chromium.org>
2023-06-21 16:55:28 +0000

    Adds PasswordSender interface
    Bug: 1455407
    Change-Id: I4778d592937dd8095942fa874db0e173ceccd73c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622673
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Markus Heintz <markusheintz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160712}

---
+++ components/password_manager/core/browser/sharing/password_sender_service.h
@@ -0,0 +1,37 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_PASSWORD_SENDER_SERVICE_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_PASSWORD_SENDER_SERVICE_H_
+
+#include "components/password_manager/core/browser/ui/credential_ui_entry.h"
+
+namespace password_manager {
+
+// Struct representing information about the recipient of a password.
+struct PasswordRecipient {
+  // The `user_id` of the receiver.
+  std::string user_id;
+
+  // TODO(crbug.com/1456309): Add a field for the public key of the receiver
+  // once the discussion concluded which type to use.
+};
+
+// The PasswordSenderService class defines the interface for sending passwords.
+class PasswordSenderService {
+ public:
+  PasswordSenderService() = default;
+  PasswordSenderService(const PasswordSenderService&) = delete;
+  PasswordSenderService& operator=(const PasswordSenderService&) = delete;
+  virtual ~PasswordSenderService() = default;
+
+  // Sends password entries for the given `credential_ui_entry` to the specified
+  // `recipient`.
+  virtual void SendPassword(const CredentialUIEntry& credential_ui_entry,
+                            const PasswordRecipient& recipient) = 0;
+};
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_SHARING_PASSWORD_SENDER_SERVICE_H_
