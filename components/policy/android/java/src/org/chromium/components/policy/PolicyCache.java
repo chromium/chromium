3315438e4cf4af87770c3204e168c47ae4ec471d
Andrew Grieve <agrieve@chromium.org>
2023-06-21 15:28:57 +0000

    De-flake PolicyCacheProviderTest
    This test failed my CQ, and it turns out it was because PolicyCache is
    caches its copy of SharedPreferences, but Robolectric recreates
    SharedPreferences between tests.
    
    This uses ResettersForTesting to ensure that the PolicyCache is
    recreated for every test.
    
    CQ link: https://ci.chromium.org/ui/p/chromium/builders/try/android-nougat-x86-rel/296842/overview
    
    Bug: None
    Change-Id: I5e89d3a6266c4464d1fb7764eed49d821dce7bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629502
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160658}

--- components/policy/android/java/src/org/chromium/components/policy/PolicyCache.java
+++ components/policy/android/java/src/org/chromium/components/policy/PolicyCache.java
@@ -15,8 +15,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
+import org.chromium.build.BuildConfig;
 
 import java.util.List;
 import java.util.Map;
@@ -31,7 +33,7 @@ public class PolicyCache {
     @VisibleForTesting
     static final String POLICY_PREF = "Components.Policy";
 
-    private static PolicyCache sPolicyCache;
+    private static PolicyCache sInstance;
 
     public enum Type {
         Integer,
@@ -79,8 +81,15 @@ public class PolicyCache {
     }
 
     public static PolicyCache get() {
-        if (sPolicyCache == null) sPolicyCache = new PolicyCache();
-        return sPolicyCache;
+        var ret = sInstance;
+        if (ret == null) {
+            ret = new PolicyCache();
+            sInstance = ret;
+            if (BuildConfig.IS_FOR_TEST) {
+                ResettersForTesting.register(() -> sInstance = null);
+            }
+        }
+        return ret;
     }
 
     /**
@@ -255,12 +264,6 @@ public class PolicyCache {
         mReadable = false;
     }
 
-    @VisibleForTesting
-    static void resetForTesting() {
-        sPolicyCache = null;
-    }
-
-    @VisibleForTesting
     public void setReadableForTesting(boolean readable) {
         mReadable = readable;
     }
