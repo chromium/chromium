8408d0a826c5ad343d696ab3ff22ec4cdc155438
Kent Tamura <tkent@chromium.org>
2023-06-21 14:49:32 +0000

    NoCopyBack: Do no use Location() in LayoutSVGRoot
    This is a preparation to remove LayoutBox::Location().
    
    This CL updates LayoutSVGRoot::PositionForPoint() and
    LocalToSVGParentTransform() so that they use PhysicalLocation()
    instead of Location(). This might change the behavior in the
    vertical-rl writing-mode, but we didn't find test cases showing such
    changes.
    
    This change is guarded by 'LayoutNGNoLocation' flag.
    
    Bug: 1353190
    Change-Id: Ia1cdd0917f223c52ecd87a6f9906344fc7b4f2f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630688
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160631}

--- third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
@@ -410,8 +410,13 @@ PositionWithAffinity LayoutSVGRoot::PositionForPoint(
 
   LayoutObject* layout_object = closest_descendant;
   AffineTransform transform = layout_object->LocalToSVGParentTransform();
-  transform.Translate(To<LayoutBox>(layout_object)->Location().X(),
-                      To<LayoutBox>(layout_object)->Location().Y());
+  if (RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()) {
+    PhysicalOffset location = To<LayoutBox>(layout_object)->PhysicalLocation();
+    transform.Translate(location.left, location.top);
+  } else {
+    transform.Translate(To<LayoutBox>(layout_object)->Location().X(),
+                        To<LayoutBox>(layout_object)->Location().Y());
+  }
   while (layout_object) {
     layout_object = layout_object->Parent();
     if (layout_object->IsSVGRoot())
@@ -449,6 +454,12 @@ SVGTransformChange LayoutSVGRoot::BuildLocalToBorderBoxTransform() {
 
 AffineTransform LayoutSVGRoot::LocalToSVGParentTransform() const {
   NOT_DESTROYED();
+  if (RuntimeEnabledFeatures::LayoutNGNoLocationEnabled()) {
+    PhysicalOffset location = PhysicalLocation();
+    return AffineTransform::Translation(RoundToInt(location.left),
+                                        RoundToInt(location.top)) *
+           local_to_border_box_transform_;
+  }
   return AffineTransform::Translation(RoundToInt(Location().X()),
                                       RoundToInt(Location().Y())) *
          local_to_border_box_transform_;
