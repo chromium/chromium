chromium/chromium

commit b5d1bad16cadae0a12ffc290aeff838b005fbf2a

Close other <details> elements linked by name attribute in tree order.
When <details> elements are linked by their common name attribute values
into an exclusive accordion, and one member of the group is closed, this
closes the linked <details> elements in tree order rather than insertion
order.  This reverts much of 34b2758acd53c3ab7ff32bf418334792ad13e523.
The code is simpler but slower, and it matches typical web platform
patterns more closely (even if those patterns may not be ideal in this
case).

This change is based on discussion in
https://github.com/whatwg/html/pull/9400/files#r1221692104

This change also stops treating the empty string as a valid name, and
adds a test to match.  This matches the above HTML PR.

Bug: 1444057
Change-Id: I5eed9e25ff7698e23738e53445c64c0fc3a1b035
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617028
Reviewed-by: Mason Freed <masonf@chromium.org>
Commit-Queue: David Baron <dbaron@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1159237}

third_party/blink/renderer/core/html/html_details_element.cc
@@ -180,17 +180,16 @@ void HTMLDetailsElement::ParseAttribute(
       // other ones with the same name.
       CHECK_NE(params.reason,
                AttributeModificationReason::kBySynchronizationOfLazyAttribute);
-      if (!name_.IsNull() && IsInTreeScope() &&
+      if (!GetName().empty() &&
           params.reason == AttributeModificationReason::kDirectly) {
         CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
-        // Copy the set of details elements, because the setAttribute
-        // call can trigger mutation events that change the set.
+        // It's important that we have a copy of the set of details
+        // elements, because the setAttribute call can trigger mutation
+        // events that change the set.
         HeapVector<Member<HTMLDetailsElement>> details_with_name(
-            *GetTreeScope().NamedDetailsElements().at(name_));
+            OtherElementsInNameGroup());
         for (HTMLDetailsElement* other_details : details_with_name) {
-          if (other_details == this) {
-            continue;
-          }
+          CHECK_NE(other_details, this);
           other_details->setAttribute(html_names::kOpenAttr, g_null_atom);
         }
       }
@@ -201,50 +200,32 @@ void HTMLDetailsElement::ParseAttribute(
                                       CSSValueID::kHidden);
       content->EnsureDisplayLockContext().SetIsDetailsSlotElement(true);
     }
-  } else if (params.name == html_names::kNameAttr &&
-             RuntimeEnabledFeatures::AccordionPatternEnabled()) {
-    RemoveNameFromMap(GetTreeScope());
-    name_ = params.new_value;
-    AddNameToMap(GetTreeScope());
   } else {
     HTMLElement::ParseAttribute(params);
   }
 }
 
-void HTMLDetailsElement::DidMoveToNewTreeScope(TreeScope& old_scope) {
-  if (!name_.IsNull()) {
-    CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
-
-    RemoveNameFromMap(old_scope);
-    AddNameToMap(GetTreeScope());
-  }
-}
-
-void HTMLDetailsElement::AddNameToMap(TreeScope& tree_scope) {
-  if (!name_.IsNull()) {
-    CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
-
-    const auto add_result =
-        tree_scope.NamedDetailsElements().insert(name_, nullptr);
-    Member<Document::DetailsSet>& details_set = add_result.stored_value->value;
-    DCHECK_EQ(add_result.is_new_entry, !details_set);
-    if (!details_set) {
-      details_set = MakeGarbageCollected<Document::DetailsSet>();
-    }
-    details_set->insert(this);
-  }
-}
-
-void HTMLDetailsElement::RemoveNameFromMap(TreeScope& tree_scope) {
-  if (!name_.IsNull()) {
-    CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
-
-    tree_scope.NamedDetailsElements().at(name_)->erase(this);
-  }
-}
-
 void HTMLDetailsElement::ToggleOpen() {
   setAttribute(html_names::kOpenAttr, is_open_ ? g_null_atom : g_empty_atom);
+}
+
+HeapVector<Member<HTMLDetailsElement>>
+HTMLDetailsElement::OtherElementsInNameGroup() {
+  CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
+  HeapVector<Member<HTMLDetailsElement>> result;
+  const AtomicString& name = GetName();
+  if (name.empty() || !IsInTreeScope()) {
+    return result;
+  }
+  HTMLDetailsElement* details =
+      Traversal<HTMLDetailsElement>::Next(GetTreeScope().RootNode());
+  while (details) {
+    if (details != this && details->GetName() == name) {
+      result.push_back(details);
+    }
+    details = Traversal<HTMLDetailsElement>::Next(*details);
+  }
+  return result;
 }
 
 bool HTMLDetailsElement::IsInteractiveContent() const {

third_party/blink/renderer/core/dom/tree_scope.cc
@@ -45,7 +45,6 @@
 #include "third_party/blink/renderer/core/frame/picture_in_picture_controller.h"
 #include "third_party/blink/renderer/core/fullscreen/fullscreen.h"
 #include "third_party/blink/renderer/core/html/html_anchor_element.h"
-#include "third_party/blink/renderer/core/html/html_details_element.h"
 #include "third_party/blink/renderer/core/html/html_frame_owner_element.h"
 #include "third_party/blink/renderer/core/html/html_map_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
@@ -724,7 +723,6 @@ void TreeScope::Trace(Visitor* visitor) const {
   visitor->Trace(selection_);
   visitor->Trace(elements_by_id_);
   visitor->Trace(image_maps_by_name_);
-  visitor->Trace(details_name_map_);
   visitor->Trace(scoped_style_resolver_);
   visitor->Trace(radio_button_group_scope_);
   visitor->Trace(svg_tree_scoped_resources_);

third_party/blink/renderer/core/dom/tree_scope.h
@@ -34,8 +34,6 @@
 #include "third_party/blink/renderer/core/html/forms/radio_button_group_scope.h"
 #include "third_party/blink/renderer/core/layout/hit_test_request.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
-#include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
-#include "third_party/blink/renderer/platform/heap/collection_support/heap_linked_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
@@ -48,7 +46,6 @@ class CSSStyleSheet;
 class DOMSelection;
 class Document;
 class Element;
-class HTMLDetailsElement;
 class HTMLMapElement;
 class HitTestResult;
 class IdTargetObserverRegistry;
@@ -117,17 +114,6 @@ class CORE_EXPORT TreeScope : public GarbageCollectedMixin {
   void AddImageMap(HTMLMapElement&);
   void RemoveImageMap(HTMLMapElement&);
   HTMLMapElement* GetImageMap(const String& url) const;
-
-  // DetailsSet is the set of details elements with a particular name
-  // attribute.  They are stored in a HeapLinkedHashSet since the
-  // iteration order is web-exposed (through deprecated Mutation
-  // events), so it should be something vaguely reasonable (in this
-  // case, insertion order) that doesn't expose anything potentially
-  // sensitive.
-  using DetailsSet = HeapLinkedHashSet<WeakMember<HTMLDetailsElement>>;
-  using DetailsNameMap = HeapHashMap<AtomicString, Member<DetailsSet>>;
-  // HTMLDetailsElement objects that have a name attribute.
-  DetailsNameMap& NamedDetailsElements() { return details_name_map_; }
 
   Element* ElementFromPoint(double x, double y) const;
   Element* HitTestPoint(double x, double y, const HitTestRequest&) const;
@@ -225,9 +211,6 @@ class CORE_EXPORT TreeScope : public GarbageCollectedMixin {
 
   Member<TreeOrderedMap> elements_by_id_;
   Member<TreeOrderedMap> image_maps_by_name_;
-
-  // HTMLDetailsElement objects that have a name attribute.
-  DetailsNameMap details_name_map_;
 
   Member<IdTargetObserverRegistry> id_target_observer_registry_;
 

third_party/blink/renderer/core/dom/tree_scope_adopter.cc
@@ -36,7 +36,6 @@
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element.h"
-#include "third_party/blink/renderer/core/html/html_details_element.h"
 
 namespace blink {
 
@@ -86,10 +85,6 @@ void TreeScopeAdopter::MoveTreeToNewScope(Node& root) const {
       shadow->SetParentTreeScope(NewScope());
       if (will_move_to_new_document)
         MoveShadowTreeToNewDocument(*shadow, old_document, new_document);
-    }
-
-    if (auto* details_element = DynamicTo<HTMLDetailsElement>(element)) {
-      details_element->DidMoveToNewTreeScope(OldScope());
     }
   }
 }

third_party/blink/renderer/core/html/html_details_element.h
@@ -41,8 +41,6 @@ class HTMLDetailsElement final : public HTMLElement {
 
   void Trace(Visitor*) const override;
 
-  void DidMoveToNewTreeScope(TreeScope& old_scope);
-
   // Walks up the ancestor chain and expands all <details> elements found along
   // the way by setting the open attribute. If any were expanded, returns true.
   // This method may run script because of the mutation events fired when
@@ -52,16 +50,21 @@ class HTMLDetailsElement final : public HTMLElement {
  private:
   void DispatchPendingEvent(const AttributeModificationReason);
 
+  const AtomicString& GetName() const {
+    return FastGetAttribute(html_names::kNameAttr);
+  }
+
+  // Return all the <details> elements in the group created by the name
+  // attribute, excluding |this|, in tree order.  If there is no such group
+  // (e.g., because there is no name attribute), returns an empty list.
+  HeapVector<Member<HTMLDetailsElement>> OtherElementsInNameGroup();
+
   LayoutObject* CreateLayoutObject(const ComputedStyle&) override;
   void ParseAttribute(const AttributeModificationParams&) override;
   void DidAddUserAgentShadowRoot(ShadowRoot&) override;
   bool IsInteractiveContent() const override;
 
-  void AddNameToMap(TreeScope& tree_scope);
-  void RemoveNameFromMap(TreeScope& tree_scope);
-
   bool is_open_ = false;
-  AtomicString name_;
   TaskHandle pending_event_;
   Member<HTMLSlotElement> summary_slot_;
   Member<HTMLSlotElement> content_slot_;

third_party/blink/web_tests/external/wpt/html/semantics/interactive-elements/the-details-element/name-attribute.tentative.html
@@ -153,14 +153,14 @@ promise_test(async t => {
   if (mutation_event_received_ids.length == 0) {
     // ok if mutation events are not supported
   } else {
-    assert_array_equals(mutation_event_received_ids, ["e0", "e3", "e2", "e1"],
-                        "removal events received in node insertion order, followed by addition event");
+    assert_array_equals(mutation_event_received_ids, ["e0", "e2", "e3", "e1"],
+                        "removal events received in tree order, followed by addition event");
   }
   assert_element_states(elements, [0, 1, 0, 0], "states after mutation");
   assert_array_equals(toggle_event_received_ids, [], "toggle events received before awaiting promises");
   await Promise.all(toggle_event_promises);
-  assert_array_equals(toggle_event_received_ids, ["e1", "e0", "e3", "e2"], "toggle events received after awaiting promises");
-}, "mutation event and toggle event order matches order of insertion in set of named elements");
+  assert_array_equals(toggle_event_received_ids, ["e1", "e0", "e2", "e3"], "toggle events received after awaiting promises");
+}, "mutation event and toggle event order matches tree order");
 
 // This function is used to guard tests that test behavior that is
 // relevant only because of Mutation Events.  If mutation events (for
@@ -206,8 +206,26 @@ promise_test(async t => {
   assert_element_states(elements, [1, 0, 1], "states before mutation");
   elements[1].open = true;
   assert_array_equals(received_ids, ["e0", "e2", "e1"],
-                      "removal events received in node insertion order, followed by addition event, despite changes to name during mutation event");
+                      "removal events received in tree order, followed by addition event, despite changes to name during mutation event");
   assert_element_states(elements, [0, 1, 0], "states after mutation");
 }, "interaction of open attribute changes with mutation events");
+
+promise_test(async t => {
+  container.innerHTML = `
+    <details></details>
+    <details></details>
+    <details name></details>
+    <details name></details>
+    <details name=""></details>
+    <details name=""></details>
+  `;
+  let elements = Array.from(container.querySelectorAll("details"));
+
+  assert_element_states(elements, [0, 0, 0, 0, 0, 0], "initial states from open attribute");
+  for (let i = 0; i < 6; ++i) {
+    elements[i].open = true;
+  }
+  assert_element_states(elements, [1, 1, 1, 1, 1, 1], "after setting all elements open");
+}, "empty and missing name attributes do not create groups");
 
 </script>

commit d9d9390166f5f731a0baaf55119d482a47c65977

Test AccordionPattern feature flag instead of CHECK()ing it.
This fixes a regression from b5d1bad16cadae0a12ffc290aeff838b005fbf2a .
To address a review comment during the development of that CL, I removed
the name_ member variable.  However, this particular use of name_ was
using it both to test for a non-empty name *and* to test for the feature
flag.  With the new structure, these tests need to be separate.

It doesn't seem worth adding a new virtual test suite (for
AccordionPattern disabled) just to test for this bug, so I haven't added
a test.

Fixed: 1455755
Bug: 1444057
Change-Id: Ifab44ba70e6a1ed60999ee9d5e31b6656497e399
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629611
Commit-Queue: Mason Freed <masonf@chromium.org>
Auto-Submit: David Baron <dbaron@chromium.org>
Reviewed-by: Mason Freed <masonf@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1160640}

third_party/blink/renderer/core/html/html_details_element.cc
@@ -180,9 +180,9 @@ void HTMLDetailsElement::ParseAttribute(
       // other ones with the same name.
       CHECK_NE(params.reason,
                AttributeModificationReason::kBySynchronizationOfLazyAttribute);
-      if (!GetName().empty() &&
+      if (RuntimeEnabledFeatures::AccordionPatternEnabled() &&
+          !GetName().empty() &&
           params.reason == AttributeModificationReason::kDirectly) {
-        CHECK(RuntimeEnabledFeatures::AccordionPatternEnabled());
         // It's important that we have a copy of the set of details
         // elements, because the setAttribute call can trigger mutation
         // events that change the set.