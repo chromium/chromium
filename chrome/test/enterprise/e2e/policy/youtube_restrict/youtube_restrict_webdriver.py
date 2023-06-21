d9514ea01a079305e83082ca6f07110d3689cfb2
Xiang Ji <jxiang@google.com>
2023-06-21 18:49:48 +0000

    Fix YouTube restrict test by decoupling from open_page.py
    Change-Id: Ia42999f24f10944132f718cfb87d75b74b7f196b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629930
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Reviewed-by: Arthur Wang <wuwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160773}

---
+++ chrome/test/enterprise/e2e/policy/youtube_restrict/youtube_restrict_webdriver.py
@@ -0,0 +1,38 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import time
+
+from absl import app, flags
+from selenium.common.exceptions import NoSuchElementException
+
+from test_util import create_chrome_webdriver
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('url', None, 'The url to open in Chrome.')
+flags.mark_flag_as_required('url')
+
+flags.DEFINE_integer('wait', 0, 'How many seconds to wait to load the page')
+
+
+def main(argv):
+  driver = create_chrome_webdriver()
+
+  try:
+    driver.get(FLAGS.url)
+    if FLAGS.wait > 0:
+      time.sleep(FLAGS.wait)
+
+    print(
+        driver.find_element_by_xpath(
+            '//*[@id="header"]/ytd-text-header-renderer').text)
+  except NoSuchElementException:
+    print("Restricted text not found")
+  finally:
+    driver.quit()
+
+
+if __name__ == '__main__':
+  app.run(main)
