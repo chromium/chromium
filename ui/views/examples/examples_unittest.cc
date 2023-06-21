f03564e2175439b402e84685844817c608a6f5f2
Vasilii Sukhanov <vasilii@chromium.org>
2023-06-21 15:18:48 +0000

    Disable flaky ExamplesTest.TestViewsExamplesLaunches on Win.
    Bug: 1456674
    Change-Id: Ia5bc22d1d2f9858b3e3b67232846563910aeb9c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629695
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Owners-Override: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1160651}

--- ui/views/examples/examples_unittest.cc
+++ ui/views/examples/examples_unittest.cc
@@ -13,7 +13,13 @@
 
 namespace views::examples {
 
-TEST(ExamplesTest, TestViewsExamplesLaunches) {
+// Fails on Win reliably: crbug.com/1456674
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_TestViewsExamplesLaunches DISABLED_TestViewsExamplesLaunches
+#else
+#define MAYBE_TestViewsExamplesLaunches TestViewsExamplesLaunches
+#endif
+TEST(ExamplesTest, MAYBE_TestViewsExamplesLaunches) {
 #if BUILDFLAG(IS_WIN)
   if (ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()) {
     GTEST_SKIP() << "Host is in dark mode; skipping test";
