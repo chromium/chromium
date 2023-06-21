c53f0e2e845304a7300e85f6e28f0d920540c807
Alex Gough <ajgo@chromium.org>
2023-06-21 16:27:10 +0000

    Call ProcessPrng not RtlGenRandom/SystemFunction036
    Calling RtlGenRandom causes advapi & cryptbase to load, ultimately
    causing a handle to \Device\KsecDD to be opened in the renderer
    that we do not need.
    
    This CL replaces Chrome's calls to RtlGenRandom / SystemFunction036
    with calls to bcryptprimitives!ProcessPrng or base:: functions.
    In Windows 10 and onwards RtlGenRandom is simply a direct call to
    ProcessPrng so this does not change any behavior. See doc[1] for
    details.
    
    Before this CL these functions got system randomness from calls to RtlGenRandom. This function is undocumented and unsupported,
    but has always been available by linking to SystemFunction036 in
    advadpi32.dll. In Windows 10 and later, this export simply forwards
    to cryptbase.dll!SystemFunction036 which calls ProcessPrng()
    directly.
    
    cryptbase!SystemFunction036 decompiled:
    
    ```
    BOOLEAN SystemFunction036(PVOID RandomBuffer,ULONG RandomBufferLength)
    {
      BOOL retval;
      retval = ProcessPrng(RandomBuffer,RandomBufferLength);
      return retval != 0;
    }
    ```
    
    In addition:
    
     - ProcessPrng takes a size_t length argument, making
       it easier to call in Chrome's wrappers.
     - ProcessPrng involves loading fewer DLLs.
     - Removing all calls to RtlGenRandom prevents cargo-culting it back
       in where it might be used in the renderer.
    
    Note: boringssl still uses RtlGenRandom. If UseBoringSSLForRandBytes
    is enabled base/rand_util.h functions use RtlGenRandom().
    boringsll will be switched to ProcessPrng in a following CL.
    
    [1] https://docs.google.com/document/d/13n1t5ak0yofzcadQCF7Ew5TewSUkNfQ3n-IYodjeRYc/edit
    
    Bug: 74242
    Change-Id: Ibd6e9ec72b03c6556e4a9283e4b5008209c045be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4606732
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160694}

--- base/rand_util_win.cc
+++ base/rand_util_win.cc
@@ -9,13 +9,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
-// "Community Additions" comment on MSDN here:
-// http://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx
-#define SystemFunction036 NTAPI SystemFunction036
-#include <NTSecAPI.h>
-#undef SystemFunction036
-
 #include <algorithm>
 #include <atomic>
 #include <limits>
@@ -24,6 +17,12 @@
 #include "base/feature_list.h"
 #include "third_party/boringssl/src/include/openssl/crypto.h"
 #include "third_party/boringssl/src/include/openssl/rand.h"
+
+// Prototype for ProcessPrng.
+// See: https://learn.microsoft.com/en-us/windows/win32/seccng/processprng
+extern "C" {
+BOOL WINAPI ProcessPrng(PBYTE pbData, SIZE_T cbData);
+}
 
 namespace base {
 
@@ -54,6 +53,18 @@ bool UseBoringSSLForRandBytes() {
 
 namespace {
 
+// Import bcryptprimitives!ProcessPrng rather than cryptbase!RtlGenRandom to
+// avoid opening a handle to \\Device\KsecDD in the renderer.
+decltype(&ProcessPrng) GetProcessPrng() {
+  HMODULE hmod = LoadLibraryW(L"bcryptprimitives.dll");
+  CHECK(hmod);
+  decltype(&ProcessPrng) process_prng_fn =
+      reinterpret_cast<decltype(&ProcessPrng)>(
+          GetProcAddress(hmod, "ProcessPrng"));
+  CHECK(process_prng_fn);
+  return process_prng_fn;
+}
+
 void RandBytes(void* output, size_t output_length, bool avoid_allocation) {
   if (!avoid_allocation && internal::UseBoringSSLForRandBytes()) {
     // Ensure BoringSSL is initialized so it can use things like RDRAND.
@@ -63,16 +74,10 @@ void RandBytes(void* output, size_t output_length, bool avoid_allocation) {
     return;
   }
 
-  char* output_ptr = static_cast<char*>(output);
-  while (output_length > 0) {
-    const ULONG output_bytes_this_pass = static_cast<ULONG>(std::min(
-        output_length, static_cast<size_t>(std::numeric_limits<ULONG>::max())));
-    const bool success =
-        RtlGenRandom(output_ptr, output_bytes_this_pass) != FALSE;
-    CHECK(success);
-    output_length -= output_bytes_this_pass;
-    output_ptr += output_bytes_this_pass;
-  }
+  static decltype(&ProcessPrng) process_prng_fn = GetProcessPrng();
+  BOOL success = process_prng_fn(static_cast<BYTE*>(output), output_length);
+  // ProcessPrng is documented to always return TRUE.
+  CHECK(success);
 }
 
 }  // namespace
