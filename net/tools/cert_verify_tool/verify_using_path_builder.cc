de60608c3756c5fca5e9c579745ae3fb80c53e66
Bob Beck <bbe@google.com>
2023-06-21 16:53:35 +0000

    Change cert-verify-tool to use EncodeTimeAsGeneralizedTime
    This removes the semi-hand rolled conversion of time to a
    Generalized time via Time::Exploded.
    
    Bug: 1455430
    Change-Id: I99b11aaad14d863d4da1c0f9b7bde0886cb14c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632218
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Bob Beck <bbe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160709}

--- net/tools/cert_verify_tool/verify_using_path_builder.cc
+++ net/tools/cert_verify_tool/verify_using_path_builder.cc
@@ -21,6 +21,7 @@
 #include "net/cert/pki/simple_path_builder_delegate.h"
 #include "net/cert/pki/trust_store_collection.h"
 #include "net/cert/pki/trust_store_in_memory.h"
+#include "net/cert/time_conversions.h"
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
 #include "net/tools/cert_verify_tool/cert_verify_tool_util.h"
@@ -28,20 +29,6 @@
 #include "third_party/boringssl/src/include/openssl/mem.h"
 
 namespace {
-
-// Converts a base::Time::Exploded to a net::der::GeneralizedTime.
-// TODO(mattm): This function exists in cast_cert_validator.cc also. Dedupe it?
-net::der::GeneralizedTime ConvertExplodedTime(
-    const base::Time::Exploded& exploded) {
-  net::der::GeneralizedTime result;
-  result.year = exploded.year;
-  result.month = exploded.month;
-  result.day = exploded.day_of_month;
-  result.hours = exploded.hour;
-  result.minutes = exploded.minute;
-  result.seconds = exploded.second;
-  return result;
-}
 
 bool AddPemEncodedCert(const net::ParsedCertificate* cert,
                        std::vector<std::string>* pem_encoded_chain) {
@@ -154,9 +141,10 @@ bool VerifyUsingPathBuilder(
     const base::FilePath& dump_prefix_path,
     scoped_refptr<net::CertNetFetcher> cert_net_fetcher,
     net::SystemTrustStore* system_trust_store) {
-  base::Time::Exploded exploded_time;
-  at_time.UTCExplode(&exploded_time);
-  net::der::GeneralizedTime time = ConvertExplodedTime(exploded_time);
+  net::der::GeneralizedTime time;
+  if (!net::EncodeTimeAsGeneralizedTime(at_time, &time)) {
+    return false;
+  }
 
   net::TrustStoreInMemory additional_roots;
   for (const auto& cert_input_with_trust : der_certs_with_trust_settings) {
