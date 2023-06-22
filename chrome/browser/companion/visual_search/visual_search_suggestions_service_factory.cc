3869bda882a9aa040575d4981566c0d8f54be6d7
Serena Pascual <srna@google.com>
2023-06-22 17:14:01 +0000

    CSC: Add VisualSearchSuggestionsServiceBrowserTest
    Also disables factory on Android - which was not meant to be enabled to begin with since this feature is built for Chrome desktop.
    
    Bug: 1455543
    Change-Id: I9165f534422e6a1615015207c6a924ce0d876405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4616895
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Serena Pascual <srna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161286}

--- chrome/browser/companion/visual_search/visual_search_suggestions_service_factory.cc
+++ chrome/browser/companion/visual_search/visual_search_suggestions_service_factory.cc
@@ -8,6 +8,7 @@
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "chrome/browser/companion/visual_search/features.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -28,7 +29,12 @@ VisualSearchSuggestionsServiceFactory::VisualSearchSuggestionsServiceFactory()
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOriginalOnly)
               .WithGuest(ProfileSelection::kOriginalOnly)
-              .Build()) {}
+              .Build()) {
+  if (base::FeatureList::IsEnabled(
+          companion::visual_search::features::kVisualSearchSuggestions)) {
+    DependsOn(OptimizationGuideKeyedServiceFactory::GetInstance());
+  }
+}
 
 // static
 VisualSearchSuggestionsServiceFactory*
@@ -39,6 +45,11 @@ VisualSearchSuggestionsServiceFactory::GetInstance() {
 
 KeyedService* VisualSearchSuggestionsServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
+  if (!base::FeatureList::IsEnabled(
+          companion::visual_search::features::kVisualSearchSuggestions)) {
+    return nullptr;
+  }
+
   // The optimization guide service must be available for the translate model
   // service to be created.
   auto* opt_guide = OptimizationGuideKeyedServiceFactory::GetForProfile(
