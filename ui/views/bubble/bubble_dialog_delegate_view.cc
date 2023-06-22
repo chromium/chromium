8a27a2da8a8285d9aa097952f8f1a703a1ed2f74
Sean Kau <skau@chromium.org>
2023-06-22 00:01:50 +0000

    Migrate /ui/views to ColorProviderKey
    ColorProviderManager::Key and its associated types are being
    moved into their own class, ColorProviderKey. As a result,
    class names are rewritten and includes are replaced.
    
    This CL was uploaded by git cl split.
    
    R=tluk@chromium.org
    
    Bug: b:286952053
    Change-Id: I49d0169af609b7edbbdd44120d5fdcc245b84772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4629860
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1160937}

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -27,7 +27,7 @@
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
-#include "ui/color/color_provider_manager.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animator.h"
@@ -97,7 +97,7 @@ class BubbleWidget : public Widget {
     return anchor ? anchor->GetThemeProvider() : Widget::GetThemeProvider();
   }
 
-  ui::ColorProviderManager::ThemeInitializerSupplier* GetCustomTheme()
+  ui::ColorProviderKey::ThemeInitializerSupplier* GetCustomTheme()
       const override {
     const Widget* const anchor = GetAnchorWidget();
     return anchor ? anchor->GetCustomTheme() : Widget::GetCustomTheme();
@@ -168,7 +168,7 @@ Widget* CreateBubbleWidget(BubbleDialogDelegate* bubble) {
     bubble_params.shadow_type = Widget::InitParams::ShadowType::kNone;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   bubble_params.background_elevation =
-      ui::ColorProviderManager::ElevationMode::kHigh;
+      ui::ColorProviderKey::ElevationMode::kHigh;
 #endif
   gfx::NativeView parent = gfx::NativeView();
   if (bubble->has_parent()) {
--- ui/views/examples/examples_color_mixer.h
+++ ui/views/examples/examples_color_mixer.h
@@ -6,14 +6,14 @@
 #define UI_VIEWS_EXAMPLES_EXAMPLES_COLOR_MIXER_H_
 
 #include "ui/color/color_provider.h"
-#include "ui/color/color_provider_manager.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/views/examples/views_examples_export.h"
 
 namespace views::examples {
 
 VIEWS_EXAMPLES_EXPORT void AddExamplesColorMixers(
     ui::ColorProvider* color_provider,
-    const ui::ColorProviderManager::Key& key);
+    const ui::ColorProviderKey& key);
 
 }  // namespace views::examples
 
