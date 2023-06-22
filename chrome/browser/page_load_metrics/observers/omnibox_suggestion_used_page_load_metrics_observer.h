e4d3e2ac9ccd21920d856c8d0dfb2e1c10bc8d48
Lingqi Chi <lingqi@chromium.org>
2023-06-22 02:51:05 +0000

    Omnibox: make OmniboxSuggestionUsedMetricsObserver align with UMAPLMO
    I6a6c1566d94554b6fe2e0ec6cc6fab9e17ea795c added this metric for LCP,
    but its behavior is a little different from
    UmaPageLoadMetricsObserver(UMAPLMO).
    
    UMAPLMO， which is well-defined and used for CWV,
    defines a different strategy of collecting LCP timing from
    OmniboxSuggestionUsedMetricsObserver.
    Specifically, when a page enters background:
    - UmaPageLoadMetricsObserver uses CONTINUE_OBSERVING
    - OmniboxSuggestionUsedMetricsObserver uses STOP_OBSERVING.
    
    This may make the comparison among different LCP metrics difficult.
    
    This CL tweaks the OmniboxSuggestionUsedMetricsObserver's logic to
    make it align with UmaPageLoadMetricsObserver.
    
    OBSOLETE_HISTOGRAM[Omnibox.SuggestionUsed.Search.NavigationToLargestContentfulPaint2]=replaced by Omnibox.SuggestionUsed.Search.NavigationToLargestContentfulPaint2.1 for changing the logging logic. The difference is whether to record LCP after the OnHidden event.
    OBSOLETE_HISTOGRAM[Omnibox.SuggestionUsed.URL.NavigationToLargestContentfulPaint2]=replaced by Omnibox.SuggestionUsed.URL.NavigationToLargestContentfulPaint2.1 for changing the logging logic. The difference is whether to record LCP after the OnHidden event.
    OBSOLETE_HISTOGRAM[Omnibox.SuggestionUsed.Search.NavigationToLargestContentfulPaint2Above2s]=replaced by Omnibox.SuggestionUsed.Search.NavigationToLargestContentfulPaint2.1Above2s for changing the logging logic. The difference is whether to record LCP after the OnHidden event.
    
    Bug: 1453559
    Change-Id: Ibf44b2c222b51c93871108b4d3eb7fcb60f825f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4618310
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161007}

--- chrome/browser/page_load_metrics/observers/omnibox_suggestion_used_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/omnibox_suggestion_used_page_load_metrics_observer.h
@@ -27,8 +27,6 @@ class OmniboxSuggestionUsedMetricsObserver
   ObservePolicy OnFencedFramesStart(
       content::NavigationHandle* navigation_handle,
       const GURL& currently_committed_url) override;
-  ObservePolicy OnHidden(
-      const page_load_metrics::mojom::PageLoadTiming& timing) override;
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle) override;
   void OnFirstContentfulPaintInPage(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
