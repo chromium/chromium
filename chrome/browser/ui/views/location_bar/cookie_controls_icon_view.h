5f1ca67528dad6e1b150d4797668fffa2dc619ef
Nicola Tommasi <tommasin@chromium.org>
2023-06-22 10:13:46 +0000

    [user bypass]: Create scaffolding MVC for bubble view
    This CL just creates the scaffolding Model-View-Controller for the new
    CookieControlsBubbleView. The new view will be used for User Bypass.
    (see more in: go/user-bypass-dd)
    
    Bug: 1446230, b:280779743
    
    Change-Id: Ib5a649ed8a2b67cd8a7ef41ebf93116d07be68c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620667
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161115}

--- chrome/browser/ui/views/location_bar/cookie_controls_icon_view.h
+++ chrome/browser/ui/views/location_bar/cookie_controls_icon_view.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include "base/scoped_observation.h"
+#include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_coordinator.h"
 #include "chrome/browser/ui/views/location_bar/old_cookie_controls_bubble_view.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "components/content_settings/browser/ui/cookie_controls_controller.h"
@@ -17,7 +18,8 @@
 // View for the cookie control icon in the Omnibox.
 class CookieControlsIconView
     : public PageActionIconView,
-      public content_settings::OldCookieControlsObserver {
+      public content_settings::OldCookieControlsObserver,
+      public content_settings::CookieControlsObserver {
  public:
   METADATA_HEADER(CookieControlsIconView);
   CookieControlsIconView(
@@ -35,6 +37,14 @@ class CookieControlsIconView
   void OnCookiesCountChanged(int allowed_cookies, int blocked_cookies) override;
   void OnStatefulBounceCountChanged(int bounce_count) override;
 
+  // CookieControlsObserver:
+  void OnStatusChanged(CookieControlsStatus status,
+                       CookieControlsEnforcement enforcement,
+                       absl::optional<base::Time> expiration) override;
+  void OnSitesCountChanged(int allowed_sites, int blocked_sites) override;
+  void OnBreakageConfidenceLevelChanged(
+      CookieControlsBreakageConfidenceLevel level) override;
+
   // PageActionIconView:
   views::BubbleDialogDelegate* GetBubble() const override;
   void UpdateImpl() override;
@@ -51,6 +61,8 @@ class CookieControlsIconView
   bool has_blocked_cookies_ = false;
 
   std::unique_ptr<content_settings::CookieControlsController> controller_;
+  std::unique_ptr<CookieControlsBubbleCoordinator> bubble_coordinator_ =
+      nullptr;
   base::ScopedObservation<content_settings::CookieControlsController,
                           content_settings::OldCookieControlsObserver>
       observation_{this};
