7503aa50206ea8f3213c1d32b6fa9cf9793473e0
Erik Chen <erikchen@chromium.org>
2023-06-22 22:30:15 +0000

    cr23: Add secondary field trial.
    This CL has no intended behavior change. The secondary field trial is
    functionally comparable to enabling both ChromeRefresh2023 and
    ChromeWebuiRefresh2023. This allows switching field trial deployment
    mechanism without shuffling users.
    
    Change-Id: Ie9bef5259ff09cf00e67c16ebdb46656e67642e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4638763
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161486}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -448,8 +448,13 @@ BASE_FEATURE(kChromeRefresh2023,
              "ChromeRefresh2023",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kChromeRefreshSecondary2023,
+             "ChromeRefreshSecondary2023",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsChromeRefresh2023() {
-  return base::FeatureList::IsEnabled(kChromeRefresh2023);
+  return base::FeatureList::IsEnabled(kChromeRefresh2023) ||
+         base::FeatureList::IsEnabled(kChromeRefreshSecondary2023);
 }
 
 BASE_FEATURE(kChromeWebuiRefresh2023,
@@ -458,7 +463,8 @@ BASE_FEATURE(kChromeWebuiRefresh2023,
 
 bool IsChromeWebuiRefresh2023() {
   return IsChromeRefresh2023() &&
-         base::FeatureList::IsEnabled(kChromeWebuiRefresh2023);
+         (base::FeatureList::IsEnabled(kChromeWebuiRefresh2023) ||
+          base::FeatureList::IsEnabled(kChromeRefreshSecondary2023));
 }
 
 constexpr base::FeatureParam<ChromeRefresh2023Level>::Option
