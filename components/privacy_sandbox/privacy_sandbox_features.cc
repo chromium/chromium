f0ce86d2b377a65007ed142d7e01b94ab9ad2e87
Nicola Tommasi <tommasin@chromium.org>
2023-06-22 16:08:06 +0000

    [PS] Suppress dialog when Chrome is launched from an external app
    This CL suppresses any Privacy Sandbox dialog if Chrome Tab is launched
    from an external app. We added a feature param to switch off the
    behavior in case metrics won't match the expectations.
    
    Bug: 1456352
    
    Change-Id: If0c8022c0d063fe757a02d09c37346400614e586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4628111
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161243}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -60,6 +60,11 @@ const base::FeatureParam<bool> kPrivacySandboxSettings4ShowSampleDataForTesting{
     &kPrivacySandboxSettings4,
     kPrivacySandboxSettings4ShowSampleDataForTestingName, false};
 
+const base::FeatureParam<bool>
+    kPrivacySandboxSettings4SuppressDialogForExternalAppLaunches{
+        &kPrivacySandboxSettings4, "suppress-dialog-for-external-app-launches",
+        true};
+
 BASE_FEATURE(kPrivacySandboxSettings3,
              "PrivacySandboxSettings3",
              base::FEATURE_ENABLED_BY_DEFAULT);
