f1a2a7b549651af745f9b689827864833552f115
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-06-22 11:07:14 +0000

    Revert "[user bypass]: Create scaffolding MVC for bubble view"
    This reverts commit 5f1ca67528dad6e1b150d4797668fffa2dc619ef.
    
    Reason for revert:
    LUCI Bisection identified this CL as the culprit of a build failure. See the analysis: https://luci-bisection.appspot.com/analysis/b/8777595370436819905
    
    Sample failed build: https://ci.chromium.org/b/8777595370436819905
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fluci-bisection.appspot.com%2Fanalysis%2Fb%2F8777595370436819905&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F4620667
    
    Original change's description:
    > [user bypass]: Create scaffolding MVC for bubble view
    >
    > This CL just creates the scaffolding Model-View-Controller for the new
    > CookieControlsBubbleView. The new view will be used for User Bypass.
    > (see more in: go/user-bypass-dd)
    >
    > Bug: 1446230, b:280779743
    >
    > Change-Id: Ib5a649ed8a2b67cd8a7ef41ebf93116d07be68c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4620667
    > Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    > Reviewed-by: Elaine Chien <elainechien@chromium.org>
    > Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1161115}
    >
    
    Bug: 1446230, b:280779743
    Change-Id: Ic64a85611390eaee76edab28607c3d46d99d31d3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634871
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1161127}

--- chrome/browser/ui/views/location_bar/cookie_controls_icon_view.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls_icon_view.cc
@@ -6,7 +6,6 @@
 
 #include <memory>
 
-#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -14,7 +13,6 @@
 #include "chrome/browser/ui/views/location_bar/old_cookie_controls_bubble_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/content_settings/browser/ui/cookie_controls_controller.h"
-#include "components/content_settings/core/common/features.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/vector_icons.h"
 #include "content/public/browser/browser_context.h"
@@ -37,11 +35,6 @@ CookieControlsIconView::CookieControlsIconView(
   SetAccessibilityProperties(
       /*role*/ absl::nullopt,
       l10n_util::GetStringUTF16(IDS_COOKIE_CONTROLS_TOOLTIP));
-
-  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
-    bubble_coordinator_ =
-        std::make_unique<CookieControlsBubbleCoordinator>(this);
-  }
 }
 
 CookieControlsIconView::~CookieControlsIconView() = default;
@@ -95,23 +88,6 @@ void CookieControlsIconView::OnStatefulBounceCountChanged(int bounce_count) {
   }
 }
 
-void CookieControlsIconView::OnStatusChanged(
-    CookieControlsStatus status,
-    CookieControlsEnforcement enforcement,
-    absl::optional<base::Time> expiration) {
-  // TODO(1446230): Implement OnStatusChanged.
-}
-
-void CookieControlsIconView::OnSitesCountChanged(int allowed_sites,
-                                                 int blocked_sites) {
-  // TODO(1446230): Implement OnSitesCountChanged.
-}
-
-void CookieControlsIconView::OnBreakageConfidenceLevelChanged(
-    CookieControlsBreakageConfidenceLevel level) {
-  // TODO(1446230): Implement OnBreakageConfidenceLevelChanged.
-}
-
 bool CookieControlsIconView::ShouldBeVisible() const {
   if (delegate()->ShouldHidePageActionIcons()) {
     return false;
@@ -123,12 +99,6 @@ bool CookieControlsIconView::ShouldBeVisible() const {
 
   if (!delegate()->GetWebContentsForPageActionIconView()) {
     return false;
-  }
-
-  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
-    // TODO(1446230): Remove this once the CookieControlsObserver methods have
-    // been implemented.
-    return true;
   }
 
   switch (status_) {
@@ -151,18 +121,12 @@ bool CookieControlsIconView::GetAssociatedBubble() const {
 
 void CookieControlsIconView::OnExecuting(
     PageActionIconView::ExecuteSource source) {
-  if (base::FeatureList::IsEnabled(content_settings::features::kUserBypassUI)) {
-    bubble_coordinator_->ShowBubble(
-        delegate()->GetWebContentsForPageActionIconView(), controller_.get());
-  } else {
-    OldCookieControlsBubbleView::ShowBubble(
-        this, this, delegate()->GetWebContentsForPageActionIconView(),
-        controller_.get(), status_);
-  }
+  OldCookieControlsBubbleView::ShowBubble(
+      this, this, delegate()->GetWebContentsForPageActionIconView(),
+      controller_.get(), status_);
 }
 
 views::BubbleDialogDelegate* CookieControlsIconView::GetBubble() const {
-  // TODO(crbug.com/1446230): Return new bubble when ready.
   return OldCookieControlsBubbleView::GetCookieBubble();
 }
 
