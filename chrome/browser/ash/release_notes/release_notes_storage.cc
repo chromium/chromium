d51604010fc866bf648dbf2c0262feddb36e34de
Callistus <callistus@google.com>
2023-06-22 06:13:23 +0000

    Enable the release notes notification for M117.
    Bug: b/286137911
    Change-Id: Iaccdfd3ef712aefe7373e9fa88b07fd95da5e52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4612805
    Reviewed-by: Zufeng Wang <zufeng@google.com>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/main@{#1161049}

--- chrome/browser/ash/release_notes/release_notes_storage.cc
+++ chrome/browser/ash/release_notes/release_notes_storage.cc
@@ -28,7 +28,7 @@ namespace {
 // This stores the latest milestone with new Release Notes content. If the last
 // milestone the user has seen the notification is before this, a new
 // notification will be shown.
-constexpr int kLastChromeVersionWithReleaseNotes = 114;
+constexpr int kLastChromeVersionWithReleaseNotes = 117;
 constexpr int kTimesToShowSuggestionChip = 3;
 
 int GetMilestone() {
