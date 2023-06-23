f72e78eae6fc57d933d4af510ef14a491aeaef2c
David Bokan <bokan@chromium.org>
2023-06-23 18:28:35 +0000

    [Merge-116] TextFinder annotations fail to find clipped content
    This CL makes it so that TextFinder type annotations will fail to attach
    to text if it's within an empty, overflow-clipping ancestor.
    TextIterator already applies a similar exception so that even if we
    attach the highlight isn't created.
    
    The motivation for this is that this is how collapsible sections on
    sites are often implemented and it's a failure if we scroll to a
    highlight that cannot be seen.
    
    This is a special-case fix for the TextFinder type. The issue applies
    more generally to scroll-to-text but changes there will require more
    thought and spec edits. In the mean time, this change will fix the issue
    for kTextFinder type annotations and we can tweak this easily to inform
      a more general solution.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/editing/iterators/fully_clipped_state_stack.cc;l=58;drc=047c7dc4ee1ce908d7fea38ca063fa2f80f92c77
    
    (cherry picked from commit aaa1419c98162841ad01f21400ee9ced81ffcf94)
    
    Bug: 1456392
    Change-Id: I9de381f1e1d34e8bf3d9141b56704e1af7fb80da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4630314
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1161265}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4637347
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5845@{#51}
    Cr-Branched-From: 5a5dff63a4a4c63b9b18589819bebb2566c85443-refs/heads/main@{#1160321}

--- third_party/blink/renderer/core/annotation/annotation_agent_impl.cc
+++ third_party/blink/renderer/core/annotation/annotation_agent_impl.cc
@@ -42,6 +42,50 @@ bool IsValidRange(const RangeInFlatTree* range) {
   return range && range->IsConnected() && !range->IsCollapsed() &&
          !range->ToEphemeralRange().IsCollapsed();
 }
+
+// It's common for collapsible sections to be implemented by hiding collapsed
+// text within a `height:0; overflow: hidden` box. However, FindBuffer does
+// find this text (as typically overflow: hidden can still be programmatically
+// scrolled). The TextFinder use case wants to prevent offering scrolls to
+// these sections as its confusing (in fact, document Markers will avoid
+// creating a highlight for these, despite the fact we can scroll to it). We
+// probably want to do this for general SharedHighlights as well but that will
+// require some more thought and spec changes but we can experiment with this
+// for TextFinder to see how it works.
+bool IsValidRangeForTextFinder(const RangeInFlatTree* range) {
+  if (!IsValidRange(range)) {
+    return false;
+  }
+
+  EphemeralRangeInFlatTree ephemeral_range = range->ToEphemeralRange();
+
+  // Technically, the text could span multiple Elements, each of which could
+  // hide overflow. However, that doesn't seem to be common so do the more
+  // performant thing and check the common ancestor.
+  Node* common_node = ephemeral_range.CommonAncestorContainer();
+
+  LayoutObject* object = common_node->GetLayoutObject();
+  CHECK(object);
+
+  for (; !object->IsLayoutView(); object = object->Parent()) {
+    LayoutBox* box = DynamicTo<LayoutBox>(object);
+    if (!box || !box->HasNonVisibleOverflow()) {
+      continue;
+    }
+
+    if (box->StyleRef().OverflowX() != EOverflow::kVisible &&
+        box->Size().Width().RawValue() <= 0) {
+      return false;
+    }
+
+    if (box->StyleRef().OverflowY() != EOverflow::kVisible &&
+        box->Size().Height().RawValue() <= 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 }  // namespace
 
 AnnotationAgentImpl::AnnotationAgentImpl(
@@ -279,7 +323,13 @@ void AnnotationAgentImpl::PerformPreAttachDOMMutation() {
 void AnnotationAgentImpl::ProcessAttachmentFinished() {
   CHECK(!attached_range_);
 
-  if (IsValidRange(pending_range_)) {
+  // See IsValidRangeForTextFinder for why we treat kTextFinder differently
+  // here.
+  bool pending_range_valid = type_ == mojom::blink::AnnotationType::kTextFinder
+                                 ? IsValidRangeForTextFinder(pending_range_)
+                                 : IsValidRange(pending_range_);
+
+  if (pending_range_valid) {
     attached_range_ = pending_range_;
 
     TRACE_EVENT_INSTANT("blink", "IsAttached");
