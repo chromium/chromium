01fd9fc58ac2ef49d4573447bb1461913ff862d0
Dave Tapuska <dtapuska@chromium.org>
2023-06-23 17:34:37 +0000

    [ios] Fix build failure
    The font loading is now being CHECKed which was causing some test
    failures. Have blink_unittests depend directly on the platform
    unit test data bundle.
    
    Bug: 1411704
    Change-Id: I340133c7a871cb6675e705b603cb96ce50985271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4640042
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161829}

--- third_party/blink/renderer/PRESUBMIT.py
+++ third_party/blink/renderer/PRESUBMIT.py
@@ -19,9 +19,6 @@ def CheckChange(input_api, output_api):
         # pylint: disable=no-name-in-module,import-outside-toplevel
         from build.ios import presubmit_support
         results += presubmit_support.CheckBundleData(
-            input_api, output_api, 'controller/blink_unittests_bundle_data',
-            'controller')
-        results += presubmit_support.CheckBundleData(
             input_api, output_api,
             'platform/blink_platform_unittests_bundle_data', 'platform')
         results += presubmit_support.CheckBundleData(
