83d8f41592ddff07f9007ab9c8020eef6dd3374d
Ewann Pelle <ewannpv@chromium.org>
2023-06-23 15:09:09 +0000

    [iOS][TabPickup] Retrieve last synced tab
    This CL retrieves the last synced tab from distant sessions.
    IT also adds observers to make sure that the `active_web_state_`
    is updated and realized.
    
    Bug: 1457175
    Change-Id: If840d8d51288108c586d531b27b14315d0549073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4632565
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1161755}

--- ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.h
+++ ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.h
@@ -6,8 +6,12 @@
 #define IOS_CHROME_BROWSER_TABS_TAB_PICKUP_TAB_PICKUP_BROWSER_AGENT_H_
 
 #import "base/callback_list.h"
+#import "base/scoped_multi_source_observation.h"
+#import "base/scoped_observation.h"
 #import "ios/chrome/browser/shared/model/browser/browser_observer.h"
 #import "ios/chrome/browser/shared/model/browser/browser_user_data.h"
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.h"
+#import "ios/web/public/web_state_observer.h"
 
 class Browser;
 
@@ -15,9 +19,15 @@ namespace sync_sessions {
 class SessionSyncService;
 }
 
+namespace web {
+class WebState;
+}
+
 // Service that creates/replaces tab pickup infobar.
 class TabPickupBrowserAgent : public BrowserObserver,
-                              public BrowserUserData<TabPickupBrowserAgent> {
+                              public BrowserUserData<TabPickupBrowserAgent>,
+                              public WebStateListObserver,
+                              public web::WebStateObserver {
  public:
   TabPickupBrowserAgent(const TabPickupBrowserAgent&) = delete;
   TabPickupBrowserAgent& operator=(const TabPickupBrowserAgent&) = delete;
@@ -35,19 +45,40 @@ class TabPickupBrowserAgent : public BrowserObserver,
   // Records the `IOS.TabPickup.TimeSinceLastCrossDeviceSync` metric.
   void RecordTransitionTime();
 
+  // Setups the infobar delegate before showing the infobar.
+  void SetupInfoBarDelegate();
+
   // BrowserObserver methods.
   void BrowserDestroyed(Browser* browser) override;
 
+  // WebStateListObserver methods.
+  void WebStateActivatedAt(WebStateList* web_state_list,
+                           web::WebState* old_web_state,
+                           web::WebState* new_web_state,
+                           int active_index,
+                           ActiveWebStateChangeReason reason) override;
+
+  // web::WebStateObserver methods.
+  void WebStateDestroyed(web::WebState* web_state) override;
+  void WebStateRealized(web::WebState* web_state) override;
+
+  // Track if an infobar will be displayed.
+  bool infobar_in_progress_ = false;
+  // Tracks if the `IOS.TabPickup.TimeSinceLastCrossDeviceSync` metric has been
+  // recorded.
+  static bool transition_time_metric_recorded;
+
   // The owning Browser.
   raw_ptr<Browser> browser_ = nullptr;
+  // The active webState.
+  raw_ptr<web::WebState> active_web_state_ = nullptr;
   // KeyedService responsible session sync.
   raw_ptr<sync_sessions::SessionSyncService> session_sync_service_ = nullptr;
   // CallbackListSubscription for the SessionSyncService method.
   base::CallbackListSubscription foreign_session_updated_subscription_;
-
-  // Tracks if the `IOS.TabPickup.TimeSinceLastCrossDeviceSync` metric has been
-  // recorded.
-  static bool transition_time_metric_recorded;
+  // Scoped observer observing unrealized WebStates.
+  base::ScopedMultiSourceObservation<web::WebState, web::WebStateObserver>
+      web_state_observations_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_TABS_TAB_PICKUP_TAB_PICKUP_BROWSER_AGENT_H_
--- ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.mm
+++ ios/chrome/browser/tabs/tab_pickup/tab_pickup_browser_agent.mm
@@ -12,6 +12,7 @@
 #import "ios/chrome/browser/synced_sessions/distant_session.h"
 #import "ios/chrome/browser/synced_sessions/synced_sessions.h"
 #import "ios/chrome/browser/tabs/tab_pickup/features.h"
+#import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -26,6 +27,7 @@ TabPickupBrowserAgent::TabPickupBrowserAgent(Browser* browser)
       session_sync_service_(SessionSyncServiceFactory::GetForBrowserState(
           browser_->GetBrowserState())) {
   browser_->AddObserver(this);
+  browser_->GetWebStateList()->AddObserver(this);
 
   // base::Unretained() is safe below because the subscription itself is a class
   // member field and handles destruction well.
@@ -43,8 +45,35 @@ TabPickupBrowserAgent::~TabPickupBrowserAgent() {
 
 void TabPickupBrowserAgent::BrowserDestroyed(Browser* browser) {
   DCHECK_EQ(browser, browser_);
+  browser->GetWebStateList()->RemoveObserver(this);
   browser->RemoveObserver(this);
   browser_ = nullptr;
+}
+
+#pragma mark - WebStateListObserver
+
+void TabPickupBrowserAgent::WebStateActivatedAt(
+    WebStateList* web_state_list,
+    web::WebState* old_web_state,
+    web::WebState* new_web_state,
+    int active_index,
+    ActiveWebStateChangeReason reason) {
+  DCHECK_EQ(active_web_state_, old_web_state);
+  active_web_state_ = new_web_state;
+}
+
+#pragma mark - web::WebStateObserver
+
+void TabPickupBrowserAgent::WebStateDestroyed(web::WebState* web_state) {
+  DCHECK_EQ(web_state, active_web_state_);
+  web_state_observations_.RemoveObservation(web_state);
+  active_web_state_ = nullptr;
+}
+
+void TabPickupBrowserAgent::WebStateRealized(web::WebState* web_state) {
+  DCHECK_EQ(web_state, active_web_state_);
+  web_state_observations_.RemoveObservation(web_state);
+  ForeignSessionsChanged();
 }
 
 #pragma mark - Private methods
@@ -55,6 +84,36 @@ void TabPickupBrowserAgent::ForeignSessionsChanged() {
   if (!IsTabPickupEnabled()) {
     return;
   }
+
+  if (!active_web_state_ || infobar_in_progress_) {
+    return;
+  }
+
+  if (!active_web_state_->IsRealized()) {
+    web_state_observations_.AddObservation(active_web_state_);
+    return;
+  }
+
+  auto const synced_sessions =
+      std::make_unique<synced_sessions::SyncedSessions>(session_sync_service_);
+  if (synced_sessions->GetSessionCount()) {
+    // Get the last synced tab.
+    const synced_sessions::DistantSession* session =
+        synced_sessions->GetSession(0);
+
+    // Check that the last synced tab is yougner than the tab pickup time
+    // threshold.
+    const base::TimeDelta modified_time =
+        base::Time::Now() - session->modified_time;
+    if (modified_time < TabPickupTimeThreshold()) {
+      SetupInfoBarDelegate();
+    }
+  }
+}
+
+void TabPickupBrowserAgent::SetupInfoBarDelegate() {
+  DCHECK(IsTabPickupEnabled());
+  infobar_in_progress_ = true;
   // TODO(crbug.com/1129482): Implement this.
 }
 
