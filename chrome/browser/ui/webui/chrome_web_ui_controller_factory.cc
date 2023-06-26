bc77c98b01bf2989ea085bd56560708c4517b382
Georg Neis <neis@chromium.org>
2023-06-26 15:25:23 +0000

    Lacros: Add chrome://management back to Ash WebUI allow-list
    This also got lost in the recent refactoring, my bad. This was actually
    partially broken before (the os:// variant of the URL didn't work).
    Thanks to the refactoring, adding chrome://management back to the list
    will automatically also fix os://management.
    
    Also add a regression test.
    
    (cherry picked from commit 1caf90deab7f4ed5bdb7cab51e8c95964350748e)
    
    Bug: chromium:1412673, chromium:1457064
    Change-Id: I62828ed79dd174457ced57a3d08f99c4ba1e1c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4627618
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1160422}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4643329
    Auto-Submit: Georg Neis <neis@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5845@{#88}
    Cr-Branched-From: 5a5dff63a4a4c63b9b18589819bebb2566c85443-refs/heads/main@{#1160321}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -1112,6 +1112,7 @@ ChromeWebUIControllerFactory::GetListOfAcceptableURLs() {
     GURL(chrome::kChromeUIGpuURL),
     GURL(chrome::kChromeUIHistogramsURL),
     GURL(chrome::kChromeUIInvalidationsUrl),
+    GURL(chrome::kChromeUIManagementURL),
     GURL(chrome::kChromeUIPrefsInternalsURL),
     GURL(chrome::kChromeUIRestartURL),
     GURL(chrome::kChromeUISignInInternalsUrl),
@@ -1211,7 +1212,6 @@ ChromeWebUIControllerFactory::GetListOfAcceptableURLs() {
     // Pages that only exist in Lacros, where they are reachable via chrome://.
     // TODO(neis): Some of these still exist in Ash (but are inaccessible) and
     // should be removed.
-    GURL(chrome::kChromeUIManagementURL),
     GURL(chrome::kChromeUIPolicyURL),
     GURL(chrome::kChromeUISettingsURL),
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
