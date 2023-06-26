c7ab769ae5e9f4773651357797eb4ca06c29f3ec
Joshua Peraza <jperaza@chromium.org>
2023-06-26 23:40:30 +0000

    Remove Crash server url from public repo
    Bug: 1452172
    Change-Id: I8abb77e6d181da3a4606b321a81618db3f3e6e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4595407
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162678}

--- components/crash/core/app/url_constants.cc
+++ components/crash/core/app/url_constants.cc
@@ -3,15 +3,9 @@
 // found in the LICENSE file.
 
 #include "components/crash/core/app/url_constants.h"
-#include "build/branding_buildflags.h"
-#include "build/build_config.h"
 
 namespace crash_reporter {
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && defined(OFFICIAL_BUILD)
-const char kDefaultUploadUrl[] = "https://clients2.google.com/cr/report";
-#else
 const char kDefaultUploadUrl[] = "";
-#endif
 
 }  // namespace crash_reporter
