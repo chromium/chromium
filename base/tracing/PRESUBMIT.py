7323e32c374985fe867cc2898a678ca7391f14c0
Rasika Navarange <rasikan@google.com>
2023-06-26 17:05:44 +0000

    Add presubmit check for trace processor chrome stdlib
    This presubmit calls the check_sql_modules.py script which checks that the stdlib is documented with the proper schema.
    
    Change-Id: I76aab531c997281a2ae07b6981d521f4ba6cfb55
    Bug: b/287233783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4645392
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1162473}

---
+++ base/tracing/PRESUBMIT.py
@@ -0,0 +1,23 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import subprocess
+
+PRESUBMIT_VERSION = '2.0.0'
+
+def CheckSqlModules(input_api, output_api):
+  stdlib_dir = input_api.PresubmitLocalPath()
+  chromium_src_dir = input_api.os_path.join(stdlib_dir, '..', '..')
+  tool = input_api.os_path.join(
+    chromium_src_dir,
+    'third_party', 'perfetto', 'tools', 'check_sql_modules.py')
+  cmd = [
+    input_api.python3_executable,
+    tool,
+    '--stdlib-sources',
+    './stdlib/chrome'
+    ]
+  if subprocess.call(cmd):
+    return [output_api.PresubmitError(tool + ' failed')]
+  return []
