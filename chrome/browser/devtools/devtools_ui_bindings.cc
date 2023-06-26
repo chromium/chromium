7be0efc5865928066550f47b0b3805cd00a2cd7e
Naoto Ono <onoto1998@gmail.com>
2023-06-26 08:31:23 +0000

    [console] Allow to enter `noJavaScriptCompletion=true`
    This is related to https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4063096. This change allows users to enter `noJavaScriptCompletion` as a query parameter.
    
    Fixed: chromium:1451084
    Change-Id: Ia25ccdddb799908c80b6316e6b718f115fc0b248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4623888
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162281}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -360,6 +360,10 @@ std::string SanitizeFrontendQueryParam(
     return value;
   }
 
+  if (key == "noJavaScriptCompletion" && value == "true") {
+    return value;
+  }
+
   return std::string();
 }
 
