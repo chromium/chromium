97bf2fec09c5004d8e7c0061e26a813d62bd5e61
Rakina Zata Amni <rakina@chromium.org>
2023-06-26 04:55:40 +0000

    Update RenderWidgetHostFoldableCSSTest to expect RenderWidgetHost change on reload
    With RenderDocument, cross-document main frame navigations will use new
    RenderFrameHosts, RenderViewHosts, and RenderWidgetHosts. This CL
    updates RenderWidgetHostFoldableCSSTest.FoldablesCSSWithReload that
    didn't expect RenderWidgetHost to change on navigation.
    
    Bug: 936696
    Change-Id: Ic116ce6b3cd1ce0ccad0de043f4659bcfcfdc228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4636257
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1162244}

--- content/browser/renderer_host/render_widget_host_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_browsertest.cc
@@ -23,6 +23,7 @@
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
 #include "content/browser/web_contents/web_contents_impl.h"
+#include "content/common/content_navigation_policy.h"
 #include "content/public/browser/render_widget_host_observer.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -904,9 +905,11 @@ IN_PROC_BROWSER_TEST_F(RenderWidgetHostFoldableCSSTest,
   DisplayFeature emulated_display_feature{
       DisplayFeature::Orientation::kVertical, offset,
       /* mask_length */ kDisplayFeatureLength};
-  MockDisplayFeature mock_display_feature(view());
-  mock_display_feature.SetDisplayFeature(&emulated_display_feature);
-  host()->SynchronizeVisualProperties();
+  {
+    MockDisplayFeature mock_display_feature(view());
+    mock_display_feature.SetDisplayFeature(&emulated_display_feature);
+    host()->SynchronizeVisualProperties();
+  }
 
   EXPECT_EQ(
       base::NumberToString(emulated_display_feature.offset) + "px",
@@ -914,9 +917,22 @@ IN_PROC_BROWSER_TEST_F(RenderWidgetHostFoldableCSSTest,
 
   // Ensure that the environment variables have the correct values in the new
   // document that is created on reloading the page.
-  LoadStopObserver load_stop_observer(shell()->web_contents());
+  TestNavigationManager navigation_manager(shell()->web_contents(),
+                                           GURL(kTestPageURL));
   shell()->Reload();
-  load_stop_observer.Wait();
+  EXPECT_TRUE(navigation_manager.WaitForResponse());
+  if (ShouldCreateNewHostForAllFrames()) {
+    // When RenderDocument is enabled, a new RenderWidgetHost will be created
+    // after the reload, so we need to call SynchronizeVisualProperties() again.
+    RenderWidgetHostImpl* target_rwh = static_cast<RenderWidgetHostImpl*>(
+        navigation_manager.GetNavigationHandle()
+            ->GetRenderFrameHost()
+            ->GetRenderWidgetHost());
+    MockDisplayFeature mock_display_feature(target_rwh->GetView());
+    mock_display_feature.SetDisplayFeature(&emulated_display_feature);
+    target_rwh->SynchronizeVisualProperties();
+  }
+  EXPECT_TRUE(navigation_manager.WaitForNavigationFinished());
 
   EXPECT_EQ(
       base::NumberToString(emulated_display_feature.offset) + "px",
