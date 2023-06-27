f4f568e3c22eabc84ff5fd5a0acdf8f23cad95b3
Marlon Facey <mfacey@chromium.org>
2023-06-27 21:55:03 +0000

    [Side Panel Journeys] Add sp scroller for GM3 updates
    Add themed vertical scroller for GM3 updates.
    
    Screenshot: http://screenshot/ciHLhvy9nkLCgQi
    
    Bug: 1454582
    Change-Id: I78c95e4c592a069cd448f237f0d4f46fb3b916fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642008
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163243}

--- chrome/browser/resources/side_panel/history_clusters/app.ts
+++ chrome/browser/resources/side_panel/history_clusters/app.ts
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import '../strings.m.js';
-import 'chrome://history-clusters-side-panel.top-chrome/shared/sp_shared_vars.css.js';
+import 'chrome://history-clusters-side-panel.top-chrome/shared/sp_shared_style.css.js';
 import 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
 import 'chrome://resources/cr_components/history_clusters/clusters.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
