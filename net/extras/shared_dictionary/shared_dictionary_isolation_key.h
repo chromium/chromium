d3e1a536ba21c1431cfc24b857e6cb5e30005877
Tsuyoshi Horo <horo@chromium.org>
2023-06-27 03:35:38 +0000

    Introduce network::mojom::SharedDictionaryIsolationKey
    This CL introduces network::mojom::SharedDictionaryIsolationKey struct
    which is mapped to net::SharedDictionaryIsolationKey struct.
    
    We will use this to notify the shared dictionary usage from the network
    process to the browser process.
    
    Also this CL refactors net/BUILD.gn to split shared dictionary related
    files from "extras" component to a new "shared_dictionary" component.
    
    Bug: 1413922
    Change-Id: Ie4c94c690f358cfcc07c9c1696a91ae690a303c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4634964
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162763}

--- net/extras/shared_dictionary/shared_dictionary_isolation_key.h
+++ net/extras/shared_dictionary/shared_dictionary_isolation_key.h
@@ -15,7 +15,7 @@ class IsolationInfo;
 class NetworkIsolationKey;
 
 // Key used to isolate shared dictionary storages.
-class COMPONENT_EXPORT(NET_EXTRAS) SharedDictionaryIsolationKey {
+class COMPONENT_EXPORT(NET_SHARED_DICTIONARY) SharedDictionaryIsolationKey {
  public:
   // Creates a SharedDictionaryIsolationKey. Returns nullopt when
   // `frame_origin` or `top_frame_origin` of `isolation_info` is not set or
@@ -30,11 +30,12 @@ class COMPONENT_EXPORT(NET_EXTRAS) SharedDictionaryIsolationKey {
       const NetworkIsolationKey& network_isolation_key,
       const absl::optional<url::Origin>& frame_origin);
 
+  SharedDictionaryIsolationKey() = default;
   SharedDictionaryIsolationKey(const url::Origin& frame_origin,
                                const net::SchemefulSite& top_frame_site);
 
   const url::Origin& frame_origin() const { return frame_origin_; }
-  const net::SchemefulSite top_frame_site() const { return top_frame_site_; }
+  const net::SchemefulSite& top_frame_site() const { return top_frame_site_; }
 
   ~SharedDictionaryIsolationKey();
 
