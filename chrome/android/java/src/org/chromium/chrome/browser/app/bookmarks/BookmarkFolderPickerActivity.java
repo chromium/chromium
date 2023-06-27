ea3e0a49a540f4f61cb6d8143259035a8135d0f1
Brandon Wylie <wylieb@chromium.org>
2023-06-27 13:28:38 +0000

    [bookmarks] Add scaffolding for new folder select activity
    Bug: 1448933
    Change-Id: Ic7c42bb63d6901716989049b47e5a79e18818510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4576450
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1162920}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/app/bookmarks/BookmarkFolderPickerActivity.java
@@ -0,0 +1,14 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.app.bookmarks;
+
+import org.chromium.chrome.browser.SynchronousInitializationActivity;
+
+/**
+ * The activity that enables the user to pick the parent folder for the given {@link BookmarkId}.
+ * Used for the improved android bookmarks manager.
+ */
+// TODO(crbug.com/1448933): Implement new folder picker activity.
+public class BookmarkFolderPickerActivity extends SynchronousInitializationActivity {}
