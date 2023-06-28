7f2bb76706578e284e572d7269d39e075faae99c
David Black <dmblack@google.com>
2023-06-28 00:11:15 +0000

    Add Ash support for `DownloadStatusUpdaterClient` commands.
    This CL adds Ash support for the following
    `DownloadStatusUpdaterClient` commands:
    
    * Cancel
    * Pause
    * Resume
    * ShowInBrowser
    
    These commands will be invoked from Ash Chrome in response to the user
    interacting with downloads rendered in ChromeOS System UI surfaces.
    
    Follow up CLs will will add the rendered downloads to ChromeOS System UI surfaces and invoke these APIs.
    
    Bug: b:279794441
    Change-Id: I89b4483220d7ec33e779b370962c374608338f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4575996
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163304}

--- chrome/browser/ash/crosapi/download_status_updater_ash.cc
+++ chrome/browser/ash/crosapi/download_status_updater_ash.cc
@@ -4,7 +4,15 @@
 
 #include "chrome/browser/ash/crosapi/download_status_updater_ash.h"
 
+#include <vector>
+
+#include "base/barrier_callback.h"
+#include "base/functional/callback.h"
+#include "base/functional/identity.h"
+#include "base/functional/invoke.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 
 namespace crosapi {
@@ -18,6 +26,33 @@ void DownloadStatusUpdaterAsh::BindReceiver(
   receivers_.Add(this, std::move(pending_receiver));
 }
 
+void DownloadStatusUpdaterAsh::Cancel(
+    const std::string& guid,
+    mojom::DownloadStatusUpdaterClient::CancelCallback callback) {
+  Invoke(&mojom::DownloadStatusUpdaterClient::Cancel, guid,
+         std::move(callback));
+}
+
+void DownloadStatusUpdaterAsh::Pause(
+    const std::string& guid,
+    mojom::DownloadStatusUpdaterClient::PauseCallback callback) {
+  Invoke(&mojom::DownloadStatusUpdaterClient::Pause, guid, std::move(callback));
+}
+
+void DownloadStatusUpdaterAsh::Resume(
+    const std::string& guid,
+    mojom::DownloadStatusUpdaterClient::ResumeCallback callback) {
+  Invoke(&mojom::DownloadStatusUpdaterClient::Resume, guid,
+         std::move(callback));
+}
+
+void DownloadStatusUpdaterAsh::ShowInBrowser(
+    const std::string& guid,
+    mojom::DownloadStatusUpdaterClient::ShowInBrowserCallback callback) {
+  Invoke(&mojom::DownloadStatusUpdaterClient::ShowInBrowser, guid,
+         std::move(callback));
+}
+
 void DownloadStatusUpdaterAsh::BindClient(
     mojo::PendingRemote<mojom::DownloadStatusUpdaterClient> client) {
   clients_.Add(std::move(client));
@@ -26,6 +61,33 @@ void DownloadStatusUpdaterAsh::BindClient(
 // TODO(http://b/279831939): Render in the appropriate System UI surface(s).
 void DownloadStatusUpdaterAsh::Update(mojom::DownloadStatusPtr status) {
   NOTIMPLEMENTED();
+}
+
+void DownloadStatusUpdaterAsh::Invoke(DownloadStatusUpdaterClientFunction func,
+                                      const std::string& guid,
+                                      HandledCallback callback) {
+  if (clients_.empty()) {
+    std::move(callback).Run(/*handled=*/false);
+    return;
+  }
+
+  // This callback is invoked by each client to indicate whether it handled the
+  // `func` call successfully. Once all clients have responded, the `callback`
+  // supplied to `this` method is run to indicate whether *any* client handled
+  // the `func` call successfully.
+  base::RepeatingCallback<void(bool)> handled_by_client_callback =
+      base::BarrierCallback<bool>(
+          clients_.size(),
+          base::BindOnce([](std::vector<bool> handled_by_client) {
+            return base::ranges::any_of(handled_by_client, base::identity());
+          }).Then(std::move(callback)));
+
+  for (auto& client : clients_) {
+    base::invoke(func, client.get(), guid,
+                 mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+                     base::BindOnce(handled_by_client_callback),
+                     /*handled_by_client=*/false));
+  }
 }
 
 }  // namespace crosapi
--- chrome/browser/ash/crosapi/download_status_updater_ash.h
+++ chrome/browser/ash/crosapi/download_status_updater_ash.h
@@ -5,6 +5,9 @@
 #ifndef CHROME_BROWSER_ASH_CROSAPI_DOWNLOAD_STATUS_UPDATER_ASH_H_
 #define CHROME_BROWSER_ASH_CROSAPI_DOWNLOAD_STATUS_UPDATER_ASH_H_
 
+#include <string>
+
+#include "base/functional/callback_forward.h"
 #include "chromeos/crosapi/mojom/download_status_updater.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -25,11 +28,44 @@ class DownloadStatusUpdaterAsh : public mojom::DownloadStatusUpdater {
   // Binds the specified pending receiver to `this` for use by crosapi.
   void BindReceiver(mojo::PendingReceiver<mojom::DownloadStatusUpdater>);
 
+  // Attempts to cancel/pause/resume/show the download associated with the
+  // specified `guid` in the browser, invoking `callback` to return whether the
+  // command was handled successfully. Note that `callback` may be invoked
+  // asynchronously.
+  void Cancel(const std::string& guid,
+              mojom::DownloadStatusUpdaterClient::CancelCallback callback);
+  void Pause(const std::string& guid,
+             mojom::DownloadStatusUpdaterClient::PauseCallback callback);
+  void Resume(const std::string& guid,
+              mojom::DownloadStatusUpdaterClient::ResumeCallback callback);
+  void ShowInBrowser(
+      const std::string& guid,
+      mojom::DownloadStatusUpdaterClient::ShowInBrowserCallback callback);
+
  private:
   // DownloadStatusUpdater:
   void BindClient(
       mojo::PendingRemote<mojom::DownloadStatusUpdaterClient> client) override;
   void Update(mojom::DownloadStatusPtr status) override;
+
+  // Callback which accepts whether a function call was `handled` successfully.
+  using HandledCallback = base::OnceCallback<void(bool handled)>;
+
+  // Pointer to a `mojom::DownloadStatusUpdaterClient` function which accepts a
+  // `guid` for a download and a `callback` accepting whether the function call
+  // was handled successfully. Note that `callback` may be invoked
+  // asynchronously.
+  using DownloadStatusUpdaterClientFunction =
+      void (mojom::DownloadStatusUpdaterClient::*)(const std::string& guid,
+                                                   HandledCallback callback);
+
+  // Invokes the specified `mojom::DownloadStatusUpdaterClient` function with
+  // the specified `guid`, invoking `callback` to return whether the function
+  // call was handled successfully. Note that `callback` may be invoked
+  // asynchronously.
+  void Invoke(DownloadStatusUpdaterClientFunction func,
+              const std::string& guid,
+              HandledCallback callback);
 
   // The set of receivers bound to `this` for use by crosapi.
   mojo::ReceiverSet<mojom::DownloadStatusUpdater> receivers_;
