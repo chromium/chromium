fda10f6305ad501f32b84de54df657aa1befea64
Colin Blundell <blundell@chromium.org>
2023-06-28 09:51:28 +0000

    [shared_image_format_utils] Convert CanCreateGMB() from ResourceFormat
    This CL converts CanCreateGpuMemoryBufferForSinglePlaneSharedImageFormat
    away from using ResourceFormat internally.
    
    Bug: 1378708
    Change-Id: Ie831b1986ee9e982758b2de37586ceea41a4e8d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4647330
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163471}

--- components/viz/common/resources/shared_image_format_utils.cc
+++ components/viz/common/resources/shared_image_format_utils.cc
@@ -238,50 +238,24 @@ SharedImageFormat SkColorTypeToSinglePlaneSharedImageFormat(
 bool CanCreateGpuMemoryBufferForSinglePlaneSharedImageFormat(
     SharedImageFormat format) {
   CHECK(format.is_single_plane());
-  switch (format.resource_format()) {
-    case BGRA_8888:
+  return (format == SinglePlaneFormat::kBGRA_8888 ||
 #if !BUILDFLAG(IS_CHROMEOS_LACROS)
-    // TODO(crbug.com/1307837): On ARM devices LaCrOS can't create RED_8
-    // GpuMemoryBuffer Objects with GBM device. This capability should be
-    // plumbed and known by clients requesting shared images as overlay
-    // candidate.
-    case RED_8:
+          // TODO(crbug.com/1307837): On ARM devices LaCrOS can't create RED_8
+          // GpuMemoryBuffer Objects with GBM device. This capability should be
+          // plumbed and known by clients requesting shared images as overlay
+          // candidate.
+          format == SinglePlaneFormat::kR_8 ||
 #endif
 #if BUILDFLAG(IS_APPLE)
-    case BGRX_8888:
-    case RGBX_8888:
+          format == SinglePlaneFormat::kBGRX_8888 ||
+          format == SinglePlaneFormat::kRGBX_8888 ||
 #endif
-    case R16_EXT:
-    case RGBA_4444:
-    case RGBA_8888:
-    case RGBA_1010102:
-    case BGRA_1010102:
-    case RGBA_F16:
-      return true;
-    // These formats have no BufferFormat equivalent or are only used
-    // for external textures, or have no GL equivalent formats.
-    case ETC1:
-    case ALPHA_8:
-    case LUMINANCE_8:
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-    case RED_8:
-#endif
-#if !BUILDFLAG(IS_APPLE)
-    case BGRX_8888:
-    case RGBX_8888:
-#endif
-    case RGB_565:
-    case LUMINANCE_F16:
-    case BGR_565:
-    case RG_88:
-    case RG16_EXT:
-    case YVU_420:
-    case YUV_420_BIPLANAR:
-    case YUVA_420_TRIPLANAR:
-    case P010:
-      return false;
-  }
-  NOTREACHED_NORETURN();
+          format == SinglePlaneFormat::kR_16 ||
+          format == SinglePlaneFormat::kRGBA_4444 ||
+          format == SinglePlaneFormat::kRGBA_8888 ||
+          format == SinglePlaneFormat::kRGBA_1010102 ||
+          format == SinglePlaneFormat::kBGRA_1010102 ||
+          format == SinglePlaneFormat::kRGBA_F16);
 }
 
 bool HasEquivalentBufferFormat(SharedImageFormat format) {
