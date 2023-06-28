e32f5356ed1f1b6e472e34711894f7026565a5cc
Sophie Wen <sophiewen@chromium.org>
2023-06-28 16:44:36 +0000

    float: Don't show menu if cursor isn't visible
    We won't start the pie animation if the cursor isn't visibly hovering
    over the size button. The user will need to manually move the cursor
    for it to be visible again.
    
    Test: existing
    Fixed: b/283478420
    Change-Id: I9a1097ef32739ecb9b71e2b5eb5f91490b23d5b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652411
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163610}

--- chromeos/ui/frame/caption_buttons/frame_size_button.cc
+++ chromeos/ui/frame/caption_buttons/frame_size_button.cc
@@ -16,6 +16,7 @@
 #include "chromeos/ui/frame/multitask_menu/multitask_menu.h"
 #include "chromeos/ui/frame/multitask_menu/multitask_menu_nudge_controller.h"
 #include "chromeos/ui/wm/features.h"
+#include "ui/aura/client/cursor_client.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/base/hit_test.h"
@@ -387,7 +388,10 @@ void FrameSizeButton::StateChanged(views::Button::ButtonState old_state) {
   }
 
   // Pie animation will start on both active/inactive window.
-  if (GetState() == views::Button::STATE_HOVERED) {
+  if (aura::client::CursorClient* cursor_client = aura::client::GetCursorClient(
+          GetWidget()->GetNativeWindow()->GetRootWindow());
+      cursor_client && cursor_client->IsCursorVisible() &&
+      GetState() == views::Button::STATE_HOVERED) {
     // On animation end we should show the multitask menu.
     // Note that if the window is not active, after the pie animation this will
     // activate the window.
