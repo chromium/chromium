c82f0debec840d84c2c888825893a3c29c8daf68
Jayee <jayee@google.com>
2023-06-28 16:57:40 +0000

    Reland "Add functionality to PolicyLoaderLocalTest"
    Remove string expectation from EXPECT_DEATH_IF_SUPPORTED.
    This is a reland of commit d87c44105e4ade4ee00ee144cd6a07cf96dd1eb3.
    
    Original change's description:
    > Reland "Add functionality to PolicyLoaderLocalTest"
    >
    > This is a reland of commit 58676c89605f20192352cff623abd1857dfbac4c
    >
    > Original change's description:
    > > Add functionality to PolicyLoaderLocalTest
    > >
    > > Wrote load function for policy loader. Policies are loaded from a json string in the loader class.
    > >
    > > Bug: b:286422730
    > > Change-Id: Ifcd28a5bd5d1aaabaf4902a9e376d797a7cf7fc9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4619664
    > > Commit-Queue: Jayee Zheng <jayee@google.com>
    > > Reviewed-by: Yann Dago <ydago@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1160795}
    >
    > Bug: b:286422730
    > Change-Id: I91297d68804c4e9b5f943478dfb49de306485824
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642032
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Jayee Zheng <jayee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1162954}
    
    Bug: b:286422730
    Change-Id: I0f754901a6012e2e8ba0fab2d90b7bd15f631dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652648
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Jayee Zheng <jayee@google.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163621}

---
+++ components/policy/core/common/policy_loader_local_test_unittest.cc
@@ -0,0 +1,221 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/policy/core/common/policy_loader_local_test.h"
+
+#include <string>
+
+#include "base/values.h"
+#include "components/policy/core/common/policy_bundle.h"
+#include "components/policy/core/common/policy_types.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace policy {
+
+class PolicyLoaderLocalTestTest : public ::testing::Test {
+ public:
+  void LoadAndVerifyPolicies(PolicyLoaderLocalTest* loader,
+                             const base::Value::List& expected_policies) {
+    std::unique_ptr<PolicyBundle> bundle =
+        std::make_unique<PolicyBundle>(loader->Load());
+    PolicyMap& map =
+        bundle->Get(PolicyNamespace(POLICY_DOMAIN_CHROME, std::string()));
+    EXPECT_EQ(map.size(), expected_policies.size());
+    for (auto& expected_policy : expected_policies) {
+      const base::Value::Dict* policy_dict = &expected_policy.GetDict();
+      const PolicyMap::Entry* actual_policy =
+          map.Get(*policy_dict->FindString("name"));
+      ASSERT_TRUE(actual_policy);
+
+      EXPECT_EQ(static_cast<PolicyLevel>(*policy_dict->FindInt("level")),
+                actual_policy->level);
+      EXPECT_EQ(static_cast<PolicyScope>(*policy_dict->FindInt("scope")),
+                actual_policy->scope);
+      EXPECT_EQ(static_cast<PolicySource>(*policy_dict->FindInt("source")),
+                actual_policy->source);
+
+      ASSERT_TRUE(actual_policy->value_unsafe());
+      EXPECT_EQ(*policy_dict->Find("value"), *actual_policy->value_unsafe());
+    }
+  }
+
+  base::Value::Dict PolicyAsDict(PolicyLevel level,
+                                 PolicyScope scope,
+                                 PolicySource source,
+                                 const std::string& name,
+                                 base::Value value) {
+    base::Value::Dict policy;
+    policy.Set("level", level);
+    policy.Set("scope", scope);
+    policy.Set("source", source);
+    policy.Set("name", name);
+    policy.Set("value", std::move(value));
+
+    return policy;
+  }
+};
+
+TEST_F(PolicyLoaderLocalTestTest, LoadFromJson) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+  policy_loader->SetPolicyListJson(R"(
+  [
+    {
+      "level": 0,
+      "scope": 0,
+      "source": 0,
+      "name": "a",
+      "value": 3
+    },
+    {
+      "level": 1,
+      "scope": 1,
+      "source": 2,
+      "name": "b",
+      "value": "test"
+    }
+  ])");
+  base::Value::List expected_policies;
+  expected_policies.Append(PolicyAsDict(
+      POLICY_LEVEL_RECOMMENDED, POLICY_SCOPE_USER,
+      POLICY_SOURCE_ENTERPRISE_DEFAULT, /*name=*/"a", base::Value(3)));
+  expected_policies.Append(
+      PolicyAsDict(POLICY_LEVEL_MANDATORY, POLICY_SCOPE_MACHINE,
+                   POLICY_SOURCE_CLOUD, /*name=*/"b", base::Value("test")));
+
+  LoadAndVerifyPolicies(policy_loader.get(), expected_policies);
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_MissingScope) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  EXPECT_DEATH_IF_SUPPORTED(
+      {
+        policy_loader->SetPolicyListJson(R"(
+    [
+      {
+        "level": 0,
+        "source": 0,
+        "name": "a",
+        "value": 3
+      },
+      {
+        "level": 1,
+        "scope": 1,
+        "source": 2,
+        "name": "b",
+        "value": "test"
+      }
+    ])");
+      },
+      "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_MissingName) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  EXPECT_DEATH_IF_SUPPORTED(
+      {
+        policy_loader->SetPolicyListJson(R"(
+    [
+      {
+        "level": 0,
+        "scope": 0,
+        "source": 0,
+        "name": "a",
+        "value": 3
+      },
+      {
+        "level": 1,
+        "scope": 1,
+        "source": 2,
+        "value": "test"
+      }
+    ])");
+      },
+      "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_NotList) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+  // Not a list
+  EXPECT_DEATH_IF_SUPPORTED({ policy_loader->SetPolicyListJson(R"({})"); }, "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_PolicyNotDict) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  // Entry not a dictionary
+  EXPECT_DEATH_IF_SUPPORTED({ policy_loader->SetPolicyListJson(R"([[]])"); },
+                            "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_PolicyLevel) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  // Invalid policy level
+  EXPECT_DEATH_IF_SUPPORTED(
+      {
+        policy_loader->SetPolicyListJson(R"(
+    [
+      {
+        "level": 4,
+        "scope": 0,
+        "source": 0,
+        "name": "a",
+        "value": 3
+      }
+    ])");
+      },
+      "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_PolicyScope) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  // Invalid policy scope
+  EXPECT_DEATH_IF_SUPPORTED(
+      {
+        policy_loader->SetPolicyListJson(R"(
+    [
+      {
+        "level": 0,
+        "scope": 5,
+        "source": 0,
+        "name": "a",
+        "value": 3
+      }
+    ])");
+      },
+      "");
+}
+
+TEST_F(PolicyLoaderLocalTestTest, InvalidInput_PolicySource) {
+  std::unique_ptr<PolicyLoaderLocalTest> policy_loader =
+      std::make_unique<PolicyLoaderLocalTest>();
+
+  // Invalid policy source
+  EXPECT_DEATH_IF_SUPPORTED(
+      {
+        policy_loader->SetPolicyListJson(R"(
+    [
+      {
+        "level": 0,
+        "scope": 0,
+        "source": 11,
+        "name": "a",
+        "value": 3
+      }
+    ])");
+      },
+      "");
+}
+
+}  // namespace policy
