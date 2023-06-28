3865b1bbaec3305659914a0961e448f009e877fd
Hidehiko Abe <hidehiko@chromium.org>
2023-06-28 00:41:38 +0000

    Not explicitly download stateful lacros on loading.
    The original intention of the code was, if there already exists
    the downloaded lacros, use it, otherwise, report "there doesn't".
    However, in some cases, current code unintentionally gives it try
    to download and so it blocks loading.
    
    This causes a problem that lacros is not usable for certain amount
    of time, while waiting for O(min) time will resolve the issue.
    
    This CL stops to download as originally intended. Because this
    still registers the component, download in background should keep
    working.
    
    There are more rooms to be cleaned up, but that will be done
    separately in later CLs.
    
    BUG=1455070
    TEST=Ran locally. Tryjob.
    
    (cherry picked from commit 60e4f9ece73c93bf06218341c286e3095ee88125)
    
    Change-Id: Iacb03ff179fec7ce9f37c9e29ff0c13cf28345ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4639328
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1161725}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4648509
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5845@{#166}
    Cr-Branched-From: 5a5dff63a4a4c63b9b18589819bebb2566c85443-refs/heads/main@{#1160321}

--- chrome/browser/ash/crosapi/stateful_lacros_loader.cc
+++ chrome/browser/ash/crosapi/stateful_lacros_loader.cc
@@ -73,10 +73,10 @@ void PreloadComponent(
     std::string component) {
   if (!component.empty()) {
     LOG(WARNING) << "Preloading stateful lacros. " << component;
-    manager->Load(
-        component, component_updater::CrOSComponentManager::MountPolicy::kMount,
-        component_updater::CrOSComponentManager::UpdatePolicy::kDontForce,
-        base::BindOnce(&DonePreloading));
+    manager->Load(component,
+                  component_updater::CrOSComponentManager::MountPolicy::kMount,
+                  component_updater::CrOSComponentManager::UpdatePolicy::kSkip,
+                  base::BindOnce(&DonePreloading));
   }
 }
 
@@ -190,6 +190,8 @@ void StatefulLacrosLoader::GetVersion(
     return;
   }
 
+  // TODO(crbug.com/1455070): There's KI that the current implementation
+  // occasionally wrongly identifies there exists. Fix the logic.
   // If there currently isn't a stateful lacros-chrome binary, set `verison_`
   // null to proceed to use the rootfs lacros-chrome binary and start the
   // installation of the stateful lacros-chrome binary in the background.
@@ -205,8 +207,9 @@ void StatefulLacrosLoader::LoadInternal(LoadCompletionCallback callback) {
       lacros_component_name_,
       component_updater::CrOSComponentManager::MountPolicy::kMount,
       // If a compatible installation exists, use that and download any updates
-      // in the background.
-      component_updater::CrOSComponentManager::UpdatePolicy::kDontForce,
+      // in the background. Otherwise, report just there is no available
+      // stateful lacros.
+      component_updater::CrOSComponentManager::UpdatePolicy::kSkip,
       // If `callback` is null, means stateful lacros-chrome should be
       // installed/updated but rootfs lacros-chrome will be used.
       base::BindOnce(&StatefulLacrosLoader::OnLoad, weak_factory_.GetWeakPtr(),
