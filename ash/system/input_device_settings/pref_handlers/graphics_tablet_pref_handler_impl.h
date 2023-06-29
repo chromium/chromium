819ab3b9c14cdc20c942f9dba771b4f8ccb4656a
Danny Wang <wangdanny@google.com>
2023-06-29 16:35:21 +0000

    customization: Initial commit for graphics tablet pref handler
    Bug: b/241965717
    Test: ash_unittests GraphicsTabletPrefHandlerTest
    Change-Id: Ia19dd072628ebfe1ba9bfd28092d89425a082e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4633599
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164168}

---
+++ ash/system/input_device_settings/pref_handlers/graphics_tablet_pref_handler_impl.h
@@ -0,0 +1,35 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_GRAPHICS_TABLET_PREF_HANDLER_IMPL_H_
+#define ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_GRAPHICS_TABLET_PREF_HANDLER_IMPL_H_
+
+#include "ash/ash_export.h"
+#include "ash/system/input_device_settings/pref_handlers/graphics_tablet_pref_handler.h"
+
+class PrefService;
+
+namespace ash {
+
+class ASH_EXPORT GraphicsTabletPrefHandlerImpl
+    : public GraphicsTabletPrefHandler {
+ public:
+  GraphicsTabletPrefHandlerImpl();
+  GraphicsTabletPrefHandlerImpl(const GraphicsTabletPrefHandlerImpl&) = delete;
+  GraphicsTabletPrefHandlerImpl& operator=(
+      const GraphicsTabletPrefHandlerImpl&) = delete;
+  ~GraphicsTabletPrefHandlerImpl() override;
+
+  // GraphicsTabletPrefHandler:
+  void InitializeGraphicsTabletSettings(
+      PrefService* pref_service,
+      mojom::GraphicsTablet* graphics_tablet) override;
+  void UpdateGraphicsTabletSettings(
+      PrefService* pref_service,
+      const mojom::GraphicsTablet& graphics_tablet) override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_INPUT_DEVICE_SETTINGS_PREF_HANDLERS_GRAPHICS_TABLET_PREF_HANDLER_IMPL_H_
