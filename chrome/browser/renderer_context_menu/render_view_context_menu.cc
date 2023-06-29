2f9f230e75c4239de380629b36c5910c32bd85d3
Xiaohan Wang <xhwang@chromium.org>
2023-06-29 00:32:36 +0000

    (merge) media: Mark "Copy video frame" menu item as "New"
    See screenshot at https://photos.app.goo.gl/mnAozeUra8ofaQvUA
    
    (cherry picked from commit 6e8c01d937c245792f7d253860d7fadfbbc83a1b)
    
    Bug: 1447318
    Test: Tested manually. See screenshot above.
    Change-Id: Id7d95bdc5dba20954843b9633c1465d2c5279f18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4617106
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1161854}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4652065
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5845@{#201}
    Cr-Branched-From: 5a5dff63a4a4c63b9b18589819bebb2566c85443-refs/heads/main@{#1160321}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1904,6 +1904,7 @@ void RenderViewContextMenu::AppendVideoItems() {
                                   IDS_CONTENT_CONTEXT_SAVEVIDEOAS);
   menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_COPYVIDEOFRAME,
                                   IDS_CONTENT_CONTEXT_COPYVIDEOFRAME);
+  menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
   menu_model_.AddItemWithStringId(IDC_CONTENT_CONTEXT_COPYAVLOCATION,
                                   IDS_CONTENT_CONTEXT_COPYVIDEOLOCATION);
   menu_model_.AddCheckItemWithStringId(IDC_CONTENT_CONTEXT_PICTUREINPICTURE,
