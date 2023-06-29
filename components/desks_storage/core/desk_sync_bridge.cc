14d79a863e6664e1bf490e38babcccdfbfca5b9b
zhumatthew <zhumatthew@google.com>
2023-06-29 23:11:44 +0000

    fws: Add garbage collection for stale fws template
    Let floating workspace service initiate the garbage collection
    once it has loaded the template it needs.
    
    BUG=b:274502821
    TEST=./out/Default/unit_tests --gtest_filter="FloatingWorkspace*
    ./out/Default/components_unittests --gtest_filter="*DeskSyncBridgeTest*"
    
    Change-Id: I16eb5c4e35ee13964a4ca47b519644f5020a9b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654949
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164407}

--- components/desks_storage/core/desk_sync_bridge.cc
+++ components/desks_storage/core/desk_sync_bridge.cc
@@ -417,8 +417,8 @@ void DeskSyncBridge::DeleteEntry(const base::Uuid& uuid,
     std::move(callback).Run(DeleteEntryStatus::kFailure);
     return;
   }
-
-  if (GetUserEntryByUUID(uuid) == nullptr) {
+  const DeskTemplate* desk_template = GetUserEntryByUUID(uuid);
+  if (desk_template == nullptr) {
     // Consider the deletion successful if the entry does not exist.
     std::move(callback).Run(DeleteEntryStatus::kOk);
     return;
@@ -431,6 +431,11 @@ void DeskSyncBridge::DeleteEntry(const base::Uuid& uuid,
                              batch->GetMetadataChangeList());
 
   desk_template_entries_.erase(uuid);
+
+  if (desk_template->type() == ash::DeskTemplateType::kFloatingWorkspace) {
+    floating_workspace_templates_uuid_.erase(
+        desk_template->client_cache_guid());
+  }
 
   batch->DeleteData(uuid.AsLowercaseString());
 
