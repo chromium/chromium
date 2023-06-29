fc558097e95faee463fab1e9dc547abd53dd532c
Brandon Fong <bjfong@google.com>
2023-06-29 18:10:07 +0000

    [X-Device Restore] Scrolling back up with fling dismisses bottom sheet
    Add a vertical scroll offset to prevent scroll flinging on the restore tabs review screen from dismissing the bottom sheet. Also addressed here is the ability to scroll on the promo screen if the bottom sheet size needs to be shrunken due to landscape/split screen, etc.
    
    Bug: 1458630
    Change-Id: Id0e3ddce9ff2586dc0fcce2b4abb4b7f7366dd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642269
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164234}

--- chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/RestoreTabsUiRenderTest.java
+++ chrome/browser/recent_tabs/internal/android/java/src/org/chromium/chrome/browser/recent_tabs/RestoreTabsUiRenderTest.java
@@ -79,7 +79,7 @@ public class RestoreTabsUiRenderTest {
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
                     .setBugComponent(RenderTestRule.Component.UI_BROWSER_MOBILE_RECENT_TABS)
-                    .setRevision(2)
+                    .setRevision(3)
                     .build();
     @Rule
     public BaseActivityTestRule<BlankUiTestActivity> mActivityTestRule =
