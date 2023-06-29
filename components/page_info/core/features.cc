9a0c5ad66f0a57fec7ca3f60bb4999443aba0b6b
Zaina Al-Mashni <zalmashni@google.com>
2023-06-29 20:36:06 +0000

    Enable PageInfoAboutThisSiteImprovedBottomSheet by default
    Enable feature by default
    
    Bug: 1358923
    Change-Id: I3914e7e413716a66306df812a6341db3aa2b5860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4645289
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1164327}

--- components/page_info/core/features.cc
+++ components/page_info/core/features.cc
@@ -18,7 +18,7 @@ BASE_FEATURE(kPageInfoStoreInfo,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kPageInfoAboutThisSiteImprovedBottomSheet,
              "PageInfoAboutThisSiteImprovedBottomSheet",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 const char* default_langs[]{"en", "pt", "fr", "it", "nl", "de", "es"};
