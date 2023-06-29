adb2a3bd4dbd7ec14dd1b9c164fd7616f20a72d2
Takumi Fujimoto <takumif@chromium.org>
2023-06-29 02:19:37 +0000

    [Code health] Remove the use of NotificationService from ChromePluginServiceFilter
    Replace observing for content::NOTIFICATION_RENDERER_PROCESS_CLOSED with
    RenderProcessHostObserver.
    
    Bug: 357627
    Change-Id: I28257e7b120edbf9ffa0026a508337b8de1df04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4623960
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1163911}

--- chrome/browser/plugins/chrome_plugin_service_filter.h
+++ chrome/browser/plugins/chrome_plugin_service_filter.h
@@ -11,11 +11,13 @@
 #include <string>
 
 #include "base/files/file_path.h"
+#include "base/functional/callback.h"
 #include "base/memory/singleton.h"
+#include "base/memory/weak_ptr.h"
+#include "base/scoped_multi_source_observation.h"
 #include "base/synchronization/lock.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/plugin_service_filter.h"
+#include "content/public/browser/render_process_host_observer.h"
 
 class Profile;
 
@@ -26,7 +28,7 @@ class WebContents;
 // This class must be created (by calling the |GetInstance| method) on the UI
 // thread, but is safe to use on any thread after that.
 class ChromePluginServiceFilter : public content::PluginServiceFilter,
-                                  public content::NotificationObserver {
+                                  public content::RenderProcessHostObserver {
  public:
   static ChromePluginServiceFilter* GetInstance();
 
@@ -56,6 +58,16 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
   bool CanLoadPlugin(int render_process_id,
                      const base::FilePath& path) override;
 
+  // RenderProcessHostObserver implementation.
+  void RenderProcessExited(
+      content::RenderProcessHost* host,
+      const content::ChildProcessTerminationInfo& info) override;
+
+  // Sets a callback to be called when `this` adds itself as an observer to a
+  // RenderProcessHost.
+  void NotifyIfObserverAddedForTesting(
+      base::RepeatingClosure observer_added_callback_for_testing);
+
  private:
   friend struct base::DefaultSingletonTraits<ChromePluginServiceFilter>;
   struct ContextInfo;
@@ -71,21 +83,23 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
   ChromePluginServiceFilter();
   ~ChromePluginServiceFilter() override;
 
-  // content::NotificationObserver implementation:
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
-
   ProcessDetails* GetOrRegisterProcess(int render_process_id);
   const ProcessDetails* GetProcess(int render_process_id) const;
-
-  content::NotificationRegistrar registrar_;
+  void ObserveRenderProcessHost(int render_process_id);
 
   base::Lock lock_;  // Guards access to member variables.
 
   std::map<const void*, std::unique_ptr<ContextInfo>> browser_context_map_;
 
   std::map<int, ProcessDetails> plugin_details_;
+
+  base::ScopedMultiSourceObservation<content::RenderProcessHost,
+                                     content::RenderProcessHostObserver>
+      host_observation_{this};
+
+  base::RepeatingClosure observer_added_callback_for_testing_;
+
+  base::WeakPtrFactory<ChromePluginServiceFilter> weak_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_PLUGINS_CHROME_PLUGIN_SERVICE_FILTER_H_
--- chrome/browser/plugins/chrome_plugin_service_filter_unittest.cc
+++ chrome/browser/plugins/chrome_plugin_service_filter_unittest.cc
@@ -8,6 +8,7 @@
 
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
+#include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/plugins/plugin_prefs.h"
 #include "chrome/browser/profiles/profile.h"
@@ -19,9 +20,24 @@
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/webplugininfo.h"
+#include "content/public/test/mock_render_process_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
+
+class RenderProcessHostObserverAddedWaiter {
+ public:
+  explicit RenderProcessHostObserverAddedWaiter(
+      ChromePluginServiceFilter* filter) {
+    filter->NotifyIfObserverAddedForTesting(run_loop_.QuitClosure());
+  }
+  ~RenderProcessHostObserverAddedWaiter() = default;
+
+  void Wait() { run_loop_.Run(); }
+
+ private:
+  base::RunLoop run_loop_;
+};
 
 class ChromePluginServiceFilterTest : public ChromeRenderViewHostTestHarness {
  protected:
@@ -61,4 +77,24 @@ TEST_F(ChromePluginServiceFilterTest, IsPluginAvailableForDisabledPlugin) {
 
   EXPECT_FALSE(
       filter_->IsPluginAvailable(browser_context(), GetFakePdfPluginInfo()));
+}
+
+TEST_F(ChromePluginServiceFilterTest, AuthorizePlugin) {
+  const int render_process_id = process()->GetID();
+  const base::FilePath path(ChromeContentClient::kPDFExtensionPluginPath);
+  RenderProcessHostObserverAddedWaiter waiter(filter_);
+
+  // Initially the plugin shouldn't be authorized.
+  EXPECT_FALSE(filter_->CanLoadPlugin(render_process_id, path));
+
+  filter_->AuthorizePlugin(render_process_id, path);
+  EXPECT_TRUE(filter_->CanLoadPlugin(render_process_id, path));
+
+  // Wait for `filter_` to add itself as an observer to the RenderProcessHost
+  // on the UI thread.
+  waiter.Wait();
+  process()->SimulateRenderProcessExit(
+      base::TerminationStatus::TERMINATION_STATUS_NORMAL_TERMINATION,
+      /*exit_code=*/0);
+  EXPECT_FALSE(filter_->CanLoadPlugin(render_process_id, path));
 }
