782802c1609db28c9e4595ffed012350a71e6e5c
Junji Watanabe <jwata@google.com>
2023-06-30 08:08:12 +0000

    Remove exclusion files for crbug.com/1051561
    The bug has already been resolved.
    Test https://crrev.com/c/4659637
    
    Bug: 1051561
    Change-Id: Ia013698118659b57f7838e2a9ce712a45ec92f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659278
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1164557}

--- build/toolchain/clang_code_coverage_wrapper.py
+++ build/toolchain/clang_code_coverage_wrapper.py
@@ -67,13 +67,7 @@ _COVERAGE_FLAGS = [
 ]
 
 # Files that should not be built with coverage flags by default.
-_DEFAULT_COVERAGE_EXCLUSION_LIST = [
-    # TODO(crbug.com/1051561): angle_unittests affected by coverage.
-    '../../base/message_loop/message_pump_default.cc',
-    '../../base/message_loop/message_pump_libevent.cc',
-    '../../base/message_loop/message_pump_win.cc',
-    '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
-]
+_DEFAULT_COVERAGE_EXCLUSION_LIST = []
 
 # Map of exclusion lists indexed by target OS.
 # If no target OS is defined, or one is defined that doesn't have a specific
@@ -100,11 +94,6 @@ _COVERAGE_EXCLUSION_LIST_MAP = {
         '../../components/media_router/common/providers/cast/channel/cast_message_util.cc',  #pylint: disable=line-too-long
         '../../components/media_router/common/providers/cast/cast_media_source.cc',  #pylint: disable=line-too-long
         '../../ui/events/keycodes/dom/keycode_converter.cc',
-        # TODO(crbug.com/1051561): angle_unittests affected by coverage.
-        '../../base/message_loop/message_pump_default.cc',
-        '../../base/message_loop/message_pump_libevent.cc',
-        '../../base/message_loop/message_pump_win.cc',
-        '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
     ],
     'chromeos': [
         # These files caused clang to crash while compiling them. They are
@@ -113,18 +102,6 @@ _COVERAGE_EXCLUSION_LIST_MAP = {
         '../../third_party/icu/source/common/uts46.cpp',
         '../../third_party/icu/source/common/ucnvmbcs.cpp',
         '../../base/android/android_image_reader_compat.cc',
-        # TODO(crbug.com/1051561): angle_unittests affected by coverage.
-        '../../base/message_loop/message_pump_default.cc',
-        '../../base/message_loop/message_pump_libevent.cc',
-        '../../base/message_loop/message_pump_win.cc',
-        '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
-    ],
-    'win': [
-        # TODO(crbug.com/1051561): angle_unittests affected by coverage.
-        '../../base/message_loop/message_pump_default.cc',
-        '../../base/message_loop/message_pump_libevent.cc',
-        '../../base/message_loop/message_pump_win.cc',
-        '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
     ],
 }
 
