63d6b8ba8126b16215d33670df8c67dcbc6c9bef
Taylor Bergquist <tbergquist@chromium.org>
2023-06-30 00:49:39 +0000

    Fix UAF when exiting a nested run loop in TabDragContextImpl::OnGestureEvent.
    OnGestureEvent may call ContinueDrag, which may run a nested run loop. After the nested run loop returns, multiple seconds of time may have passed, and the world may be in a very different state; in particular, the window that contains this TabDragContext may have closed.
    
    This CL checks if this has happened, and returns early in that case.
    
    Bug: 1453465
    Change-Id: I6095c0afeb5aa5f422717f1bbd93b96175e52afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4657527
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1164449}

--- chrome/browser/ui/views/tabs/fake_tab_slot_controller.h
+++ chrome/browser/ui/views/tabs/fake_tab_slot_controller.h
@@ -60,8 +60,8 @@ class FakeTabSlotController : public TabSlotController {
       TabSlotView* source,
       const ui::LocatedEvent& event,
       const ui::ListSelectionModel& original_selection) override {}
-  void ContinueDrag(views::View* view, const ui::LocatedEvent& event) override {
-  }
+  Liveness ContinueDrag(views::View* view,
+                        const ui::LocatedEvent& event) override;
   bool EndDrag(EndDragReason reason) override;
   Tab* GetTabAt(const gfx::Point& point) override;
   const Tab* GetAdjacentTab(const Tab* tab, int offset) override;
