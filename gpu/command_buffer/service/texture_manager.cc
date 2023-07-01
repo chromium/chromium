93b801ecb60e3a69daa7e53c253536211c8be3bd
Xiyuan Xia <xiyuan@chromium.org>
2023-07-01 17:28:46 +0000

    Revert "[GPU] Remove GLImageNativePixmap"
    This reverts commit 54424945d85153be56bbd06d2618dba5f97a67c5.
    
    Reason for revert: 
    Breaks chrome uprev:
    https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/4662610
    
    ninja: Entering directory `c/Release'
    ninja: error: unknown target 'ozone_gl_unittests', did you mean 'ozone_unittests'?
    
    Original change's description:
    > [GPU] Remove GLImageNativePixmap
    >
    > Following the deletion of V4L2SliceVDA, nothing in the codebase
    > references GLImageNativePixmap. This CL deletes it as well as all of the
    > code that directly references it. Further cleanup is left for followup
    > CLs.
    >
    > With this CL, GLImage definitively sails off into the sunset.
    >
    > Bug: 1310018
    > Change-Id: I1bf12aa95eaeadf3287b2d37c79ec53e16ce39ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4645468
    > Reviewed-by: ccameron chromium <ccameron@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1164579}
    
    Bug: 1310018
    Bug: b:289641973
    Change-Id: I04c204694ed922dd4cb81ded1c30a7e4243b2294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4662000
    Auto-Submit: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165062}

--- gpu/command_buffer/service/texture_manager.cc
+++ gpu/command_buffer/service/texture_manager.cc
@@ -37,6 +37,10 @@
 #include "ui/gl/progress_reporter.h"
 #include "ui/gl/trace_util.h"
 
+#if BUILDFLAG(IS_OZONE)
+#include "gpu/command_buffer/service/shared_image/gl_image_native_pixmap.h"
+#endif
+
 using base::trace_event::MemoryAllocatorDump;
 using base::trace_event::MemoryDumpLevelOfDetail;
 
@@ -69,6 +73,7 @@ struct TextureSignature {
   GLint max_level_;
   GLenum format_;
   GLenum type_;
+  bool has_image_;
   bool can_render_;
   bool can_render_to_;
   bool npot_;
@@ -90,6 +95,7 @@ struct TextureSignature {
                    GLint max_level,
                    GLenum format,
                    GLenum type,
+                   bool has_image,
                    bool can_render,
                    bool can_render_to,
                    bool npot) {
@@ -115,6 +121,7 @@ struct TextureSignature {
     max_level_ = max_level;
     format_ = format;
     type_ = type;
+    has_image_ = has_image;
     can_render_ = can_render;
     can_render_to_ = can_render_to;
     npot_ = npot;
@@ -560,6 +567,14 @@ void TexturePassthrough::MarkContextLost() {
   have_context_ = false;
 }
 
+#if BUILDFLAG(IS_OZONE)
+void TexturePassthrough::SetLevelImage(GLenum target,
+                                       GLint level,
+                                       GLImageNativePixmap* image) {
+  SetLevelImageInternal(target, level, image, owned_service_id_);
+}
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
 void TexturePassthrough::BindToServiceId(GLuint service_id) {
   if (service_id != 0 && service_id != service_id_) {
@@ -607,6 +622,20 @@ bool TexturePassthrough::LevelInfoExists(GLenum target,
   *out_face_idx = face_idx;
   return true;
 }
+
+#if BUILDFLAG(IS_OZONE)
+void TexturePassthrough::SetLevelImageInternal(GLenum target,
+                                               GLint level,
+                                               GLImageNativePixmap* image,
+                                               GLuint service_id) {
+  LevelInfo* level_info = GetLevelInfo(target, level);
+  level_info->image = image;
+
+  if (service_id != 0 && service_id != service_id_) {
+    service_id_ = service_id;
+  }
+}
+#endif
 
 TexturePassthrough::LevelInfo* TexturePassthrough::GetLevelInfo(GLenum target,
                                                                 GLint level) {
@@ -711,8 +740,17 @@ Texture::LevelInfo::LevelInfo(const LevelInfo& rhs)
       border(rhs.border),
       format(rhs.format),
       type(rhs.type),
+#if BUILDFLAG(IS_OZONE)
+      image(rhs.image),
+#endif
       estimated_size(rhs.estimated_size),
-      internal_workaround(rhs.internal_workaround) {}
+      internal_workaround(rhs.internal_workaround)
+#if BUILDFLAG(IS_OZONE)
+      ,
+      image_state(rhs.image_state)
+#endif
+{
+}
 
 Texture::LevelInfo::~LevelInfo() = default;
 
@@ -838,10 +876,17 @@ void Texture::AddToSignature(
   const Texture::LevelInfo& info =
       face_infos_[face_index].level_infos[level];
 
+  bool has_image =
+#if BUILDFLAG(IS_OZONE)
+      info.image.get();
+#else
+      false;
+#endif
+
   TextureSignature signature_data(
       target, level, sampler_state_, usage_, info.internal_format, info.width,
       info.height, info.depth, base_level_, info.border, max_level_,
-      info.format, info.type, CanRender(feature_info),
+      info.format, info.type, has_image, CanRender(feature_info),
       CanRenderTo(feature_info, level), npot_);
 
   signature->append(TextureTag, sizeof(TextureTag));
@@ -938,9 +983,16 @@ bool Texture::CanGenerateMipmaps(const FeatureInfo* feature_info) const {
 
   for (size_t ii = 0; ii < face_infos_.size(); ++ii) {
     const LevelInfo& info = face_infos_[ii].level_infos[base_level_];
+    bool has_image =
+#if BUILDFLAG(IS_OZONE)
+        info.image.get();
+#else
+        false;
+#endif
     if ((info.target == 0) ||
         feature_info->validators()->compressed_texture_format.IsValid(
-            info.internal_format)) {
+            info.internal_format) ||
+        has_image) {
       return false;
     }
   }
@@ -1118,13 +1170,25 @@ void Texture::UpdateCanRenderCondition() {
   can_render_condition_ = GetCanRenderCondition();
 }
 
-// TODO(crbug.com/1324249): Eliminate this function and `has_images_`
-// altogether.
 void Texture::UpdateHasImages() {
   if (face_infos_.empty())
     return;
 
   bool has_images = false;
+  // TODO(crbug.com/1324249): Investigate making `has_images` and
+  // everything around its maintenance Ozone-only.
+#if BUILDFLAG(IS_OZONE)
+  for (size_t ii = 0; ii < face_infos_.size(); ++ii) {
+    for (size_t jj = 0; jj < face_infos_[ii].level_infos.size(); ++jj) {
+      const Texture::LevelInfo& info = face_infos_[ii].level_infos[jj];
+      if (info.image.get() != nullptr) {
+        has_images = true;
+        break;
+      }
+    }
+  }
+#endif
+
   if (has_images_ == has_images)
     return;
   has_images_ = has_images;
@@ -1272,6 +1336,10 @@ void Texture::SetLevelInfo(GLenum target,
   info.border = border;
   info.format = format;
   info.type = type;
+#if BUILDFLAG(IS_OZONE)
+  info.image.reset();
+  info.image_state = NOIMAGE;
+#endif
   info.internal_workaround = false;
 
   UpdateMipCleared(&info, width, height, cleared_rect);
@@ -1814,6 +1882,41 @@ bool Texture::ClearLevel(DecoderContext* decoder, GLenum target, GLint level) {
                    gfx::Rect(info.width, info.height));
   return true;
 }
+
+#if BUILDFLAG(IS_OZONE)
+void Texture::SetLevelImageInternal(GLenum target,
+                                    GLint level,
+                                    GLImageNativePixmap* image,
+                                    ImageState state) {
+  DCHECK(image ? state != ImageState::NOIMAGE : state == ImageState::NOIMAGE);
+
+  DCHECK_GE(level, 0);
+  size_t face_index = GLES2Util::GLTargetToFaceIndex(target);
+  DCHECK_LT(face_index, face_infos_.size());
+  DCHECK_LT(static_cast<size_t>(level),
+            face_infos_[face_index].level_infos.size());
+  Texture::LevelInfo& info = face_infos_[face_index].level_infos[level];
+  DCHECK_EQ(info.target, target);
+  DCHECK_EQ(info.level, level);
+  info.image = image;
+  info.image_state = state;
+
+  UpdateCanRenderCondition();
+  UpdateHasImages();
+}
+
+void Texture::SetBoundLevelImage(GLenum target,
+                                 GLint level,
+                                 GLImageNativePixmap* image) {
+  SetStreamTextureServiceId(0);
+  SetLevelImageInternal(target, level, image, ImageState::BOUND);
+}
+
+void Texture::UnsetLevelImage(GLenum target, GLint level) {
+  SetStreamTextureServiceId(0);
+  SetLevelImageInternal(target, level, nullptr, ImageState::NOIMAGE);
+}
+#endif
 
 #if BUILDFLAG(IS_ANDROID)
 void Texture::BindToServiceId(GLuint service_id) {
@@ -2468,6 +2571,23 @@ GLsizei TextureManager::ComputeMipMapCount(GLenum target,
       return 1 + base::bits::Log2Floor(std::max(width, height));
   }
 }
+
+#if BUILDFLAG(IS_OZONE)
+void TextureManager::SetBoundLevelImage(TextureRef* ref,
+                                        GLenum target,
+                                        GLint level,
+                                        GLImageNativePixmap* image) {
+  DCHECK(ref);
+  ref->texture()->SetBoundLevelImage(target, level, image);
+}
+
+void TextureManager::UnsetLevelImage(TextureRef* ref,
+                                     GLenum target,
+                                     GLint level) {
+  DCHECK(ref);
+  ref->texture()->UnsetLevelImage(target, level);
+}
+#endif
 
 size_t TextureManager::GetSignatureSize() const {
   return sizeof(TextureTag) + sizeof(TextureSignature);
---
+++ ui/ozone/gl/gl_image_test_template.h
@@ -0,0 +1,157 @@
+// Copyright 2015 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file defines tests that implementations of GLImage should pass in order
+// to be conformant.
+
+#ifndef UI_OZONE_GL_GL_IMAGE_TEST_TEMPLATE_H_
+#define UI_OZONE_GL_GL_IMAGE_TEST_TEMPLATE_H_
+
+#include "base/memory/raw_ptr.h"
+#include "base/strings/stringize_macros.h"
+#include "build/build_config.h"
+#include "gpu/command_buffer/service/shared_image/gl_image_native_pixmap.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/gfx/buffer_format_util.h"
+#include "ui/gfx/buffer_types.h"
+#include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_context.h"
+#include "ui/gl/gl_display.h"
+#include "ui/gl/gl_helper.h"
+#include "ui/gl/gl_surface.h"
+#include "ui/gl/gl_version_info.h"
+#include "ui/gl/init/gl_factory.h"
+#include "ui/gl/test/gl_test_helper.h"
+#include "ui/gl/test/gl_test_support.h"
+
+#if BUILDFLAG(IS_APPLE)
+#include "base/mac/mac_util.h"
+#endif
+
+// TODO(crbug.com/969798): Fix memory leaks in tests and re-enable on LSAN.
+#ifdef LEAK_SANITIZER
+#define MAYBE_Create DISABLED_Create
+#else
+#define MAYBE_Create Create
+#endif
+
+// TYPED_TEST_P() and REGISTER_TYPED_TEST_SUITE_P() don't do macro expansion on
+// their parameters, making the MAYBE_ technique above not work -- these macros
+// are a workaround.
+#define TYPED_TEST_P_WITH_EXPANSION(SuiteName, TestName) \
+  TYPED_TEST_P(SuiteName, TestName)
+#define REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION(SuiteName, ...) \
+  REGISTER_TYPED_TEST_SUITE_P(SuiteName, __VA_ARGS__)
+
+namespace gl {
+
+namespace internal {
+
+void DrawTextureQuad(GLenum target, const gfx::Size& size);
+}
+
+class GLImageTestDelegateBase {
+ public:
+  virtual ~GLImageTestDelegateBase() {}
+
+  virtual void DidSetUp() {}
+  virtual void WillTearDown() {}
+
+  virtual absl::optional<GLImplementationParts> GetPreferedGLImplementation()
+      const;
+  virtual bool SkipTest(GLDisplay* display) const;
+};
+
+template <typename GLImageTestDelegate>
+class GLImageTest : public testing::Test {
+ protected:
+  // Overridden from testing::Test:
+  void SetUp() override {
+    auto prefered_impl = delegate_.GetPreferedGLImplementation();
+    display_ = GLTestSupport::InitializeGL(prefered_impl);
+    surface_ = gl::init::CreateOffscreenGLSurface(display_, gfx::Size());
+    context_ =
+        gl::init::CreateGLContext(nullptr, surface_.get(), GLContextAttribs());
+    context_->MakeCurrent(surface_.get());
+    delegate_.DidSetUp();
+  }
+  void TearDown() override {
+    delegate_.WillTearDown();
+    context_->ReleaseCurrent(surface_.get());
+    context_ = nullptr;
+    surface_ = nullptr;
+    GLTestSupport::CleanupGL(display_);
+  }
+
+ protected:
+  scoped_refptr<GLSurface> surface_;
+  scoped_refptr<GLContext> context_;
+  GLImageTestDelegate delegate_;
+  raw_ptr<GLDisplay> display_ = nullptr;
+};
+
+TYPED_TEST_SUITE_P(GLImageTest);
+
+TYPED_TEST_P_WITH_EXPANSION(GLImageTest, MAYBE_Create) {
+  if (this->delegate_.SkipTest(this->display_)) {
+    GTEST_SKIP() << "Skip because GL initialization failed";
+  }
+
+  // NOTE: On some drm devices (mediatek) the mininum width/height to add an fb
+  // for a bo must be 64, and YVU_420 in i915 requires at least 128 length.
+  const gfx::Size small_image_size(128, 128);
+  const gfx::Size large_image_size(512, 512);
+  const uint8_t* image_color = this->delegate_.GetImageColor();
+
+  // Create a small solid color green image of preferred format. This must
+  // succeed in order for a GLImage to be conformant.
+  scoped_refptr<gpu::GLImageNativePixmap> small_image =
+      this->delegate_.CreateSolidColorImage(small_image_size, image_color);
+  ASSERT_TRUE(small_image);
+
+  // Create a large solid color green image of preferred format. This must
+  // succeed in order for a GLImage to be conformant.
+  scoped_refptr<gpu::GLImageNativePixmap> large_image =
+      this->delegate_.CreateSolidColorImage(large_image_size, image_color);
+  ASSERT_TRUE(large_image);
+}
+
+// The GLImageTest test case verifies the behaviour that is expected from a
+// GLImage in order to be conformant.
+REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION(GLImageTest, MAYBE_Create);
+
+template <typename GLImageTestDelegate>
+class GLImageOddSizeTest : public GLImageTest<GLImageTestDelegate> {};
+
+// This test verifies that odd-sized GLImages can be created and destroyed.
+TYPED_TEST_SUITE_P(GLImageOddSizeTest);
+
+TYPED_TEST_P_WITH_EXPANSION(GLImageOddSizeTest, MAYBE_Create) {
+  if (this->delegate_.SkipTest(this->display_)) {
+    GTEST_SKIP() << "Skip because GL initialization failed";
+  }
+
+  const gfx::Size odd_image_size(17, 53);
+  const uint8_t* image_color = this->delegate_.GetImageColor();
+
+  // Create an odd-sized solid color green image of preferred format. This must
+  // succeed in order for a GLImage to be conformant.
+  scoped_refptr<gpu::GLImageNativePixmap> odd_image =
+      this->delegate_.CreateSolidColorImage(odd_image_size, image_color);
+  ASSERT_TRUE(odd_image);
+}
+
+// The GLImageTest test case verifies the behaviour that is expected from a
+// GLImage in order to be conformant.
+REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION(GLImageOddSizeTest, MAYBE_Create);
+
+}  // namespace gl
+
+// Avoid polluting source files that include this header.
+#undef MAYBE_Create
+#undef TYPED_TEST_P_WITH_EXPANSION
+#undef REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION
+
+#endif  // UI_OZONE_GL_GL_IMAGE_TEST_TEMPLATE_H_
