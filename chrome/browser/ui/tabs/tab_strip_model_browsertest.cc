10c710b7426bed151735238234b445fce5f60466
James Ye <jamesye@google.com>
2023-07-04 08:23:15 +0000

    Revert "Prevent non-closable windows from being closed"
    This reverts commit 34a3ae9bbb5b0fb479efec100440ee981edd5725.
    
    Reason for revert: Terminal app doesn't close b/288972458
    
    Original change's description:
    > Prevent non-closable windows from being closed
    >
    > This CL implements admin configurable non-closable apps.
    > These apps should neither be closable through closing the only web app
    > tab (past CL [1]) nor by closing the window containing the
    > app (this CL). The check is placed in the
    > `UnloadController::ShouldCloseWindow` oracle.
    >
    > DD: go/preventclose
    > Bug: b/278246367
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/4444147
    >
    > Change-Id: I980d039713704f6d6d2d73153102ff115d9dbc40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4594776
    > Reviewed-by: Louise Brett <loubrett@google.com>
    > Commit-Queue: Simon Hangl <simonha@google.com>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1164322}
    
    Bug: b/278246367
    Bug: b/288972458
    Change-Id: I37828aff27457b7fa5a88028971aad8aa99b156c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664425
    Auto-Submit: James Ye <jamesye@google.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165508}

--- chrome/browser/ui/tabs/tab_strip_model_browsertest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_browsertest.cc
@@ -10,7 +10,6 @@
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/prevent_close_test_base.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_id.h"
@@ -45,7 +44,70 @@ constexpr bool kShouldPreventClose = false;
 
 }  // namespace
 
-using TabStripModelPreventCloseTest = PreventCloseTestBase;
+class TabStripModelPreventCloseTest : public policy::PolicyTest {
+ public:
+  TabStripModelPreventCloseTest() {
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{features::kDesktopPWAsEnforceWebAppSettingsPolicy,
+                              features::kDesktopPWAsPreventClose},
+        /*disabled_features=*/{});
+  }
+  TabStripModelPreventCloseTest(const TabStripModelPreventCloseTest&) = delete;
+  TabStripModelPreventCloseTest& operator=(
+      const TabStripModelPreventCloseTest&) = delete;
+  ~TabStripModelPreventCloseTest() override = default;
+
+  void SetUpInProcessBrowserTestFixture() override {
+    provider_.SetDefaultReturns(
+        /*is_initialization_complete_return=*/true,
+        /*is_first_policy_load_complete_return=*/true);
+    policy::BrowserPolicyConnector::SetPolicyProviderForTesting(&provider_);
+  }
+
+  void TearDownInProcessBrowserTestFixture() override {
+    ClearWebAppSettings();
+    policy::PolicyTest::TearDownInProcessBrowserTestFixture();
+  }
+
+  void SetWebAppSettings(base::StringPiece config) {
+    policy::PolicyMap policies;
+    SetPolicy(&policies, policy::key::kWebAppSettings,
+              ReturnPolicyValueFromJson(config));
+    provider_.UpdateChromePolicy(policies);
+  }
+
+  void ClearWebAppSettings() { SetWebAppSettings(/*config=*/"[]"); }
+
+  void InstallPWA(const GURL& app_url, const web_app::AppId& app_id) {
+    auto web_app_info = std::make_unique<web_app::WebAppInstallInfo>();
+    web_app_info->start_url = app_url;
+    web_app_info->scope = app_url.GetWithoutFilename();
+    web_app::AppId installed_app_id = web_app::test::InstallWebApp(
+        browser()->profile(), std::move(web_app_info));
+    EXPECT_EQ(app_id, installed_app_id);
+  }
+
+  Browser* LaunchPWA(const web_app::AppId& app_id, bool launch_in_window) {
+    return launch_in_window
+               ? web_app::LaunchWebAppBrowserAndWait(
+                     profile(), app_id, WindowOpenDisposition::NEW_WINDOW)
+               : web_app::LaunchBrowserForWebAppInTab(profile(), app_id);
+  }
+
+  base::Value ReturnPolicyValueFromJson(base::StringPiece policy) {
+    auto result = base::JSONReader::ReadAndReturnValueWithError(
+        policy, base::JSONParserOptions::JSON_ALLOW_TRAILING_COMMAS);
+    DCHECK(result.has_value()) << result.error().message;
+    DCHECK(result->is_list());
+    return std::move(*result);
+  }
+
+  Profile* profile() { return browser()->profile(); }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+  testing::NiceMock<policy::MockConfigurationPolicyProvider> provider_;
+};
 
 IN_PROC_BROWSER_TEST_F(TabStripModelPreventCloseTest,
                        PreventCloseEnforedByPolicy) {
