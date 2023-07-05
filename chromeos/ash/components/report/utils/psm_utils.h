4a78c949eab8aefc4bcd7d65a9656d6cc81321c0
Hirthanan Subenderan <hirthanan@google.com>
2023-07-05 18:27:16 +0000

    Add PSM utility function for generating PSM identifiers.
    The function GeneratePsmIdentifier takes a high entropy seed,
    PSM use case string, and window ID as input, and returns an optional
    RlwePlaintextId representing the PSM identifier.
    
    BUG=chromium:1449132
    
    Change-Id: I569e8281011f3aec365156cc48175c2ea59cf414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4648117
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166047}

---
+++ chromeos/ash/components/report/utils/psm_utils.h
@@ -0,0 +1,24 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_PSM_UTILS_H_
+#define CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_PSM_UTILS_H_
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace private_membership::rlwe {
+class RlwePlaintextId;
+}  // namespace private_membership::rlwe
+
+namespace ash::report::utils {
+
+// Generate the PSM id for a use case and window id, with the high entropy seed.
+absl::optional<private_membership::rlwe::RlwePlaintextId> GeneratePsmIdentifier(
+    const std::string& high_entropy_seed,
+    const std::string& psm_use_case_str,
+    const std::string& window_id);
+
+}  // namespace ash::report::utils
+
+#endif  // CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_PSM_UTILS_H_
