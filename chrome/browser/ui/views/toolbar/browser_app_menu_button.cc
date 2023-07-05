47793a1feb03f958e7c35cd12227df7abe1b2eb2
Teresa Mao <temao@chromium.org>
2023-07-05 07:44:37 +0000

    [M115][metrics] Add a metrics tracking time from the app menu (3-dot menu) button pressed to menu shown.
    (cherry picked from commit 7e525bb5a0c1890af5f563838c5adb30a1852631)
    
    Bug: 1444532,1458608
    Change-Id: Id4653d85238e3303cbc2a6120021f6e97f4584c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4521620
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1155821}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4653012
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/5790@{#1390}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- chrome/browser/ui/views/toolbar/browser_app_menu_button.cc
+++ chrome/browser/ui/views/toolbar/browser_app_menu_button.cc
@@ -9,6 +9,7 @@
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "build/chromeos_buildflags.h"
@@ -33,6 +34,7 @@
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/compositor/compositor.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
@@ -257,6 +259,21 @@ void BrowserAppMenuButton::OnTouchUiChanged() {
 }
 
 void BrowserAppMenuButton::ButtonPressed(const ui::Event& event) {
+  // Registers a callback for logging time from app menu button pressed to menu
+  // shown to the compositor's callback. The callback will only be invoked after
+  // successful presentation of the next frame - app menu.
+  BrowserView::GetBrowserViewForBrowser(toolbar_view_->browser())
+      ->GetWidget()
+      ->GetCompositor()
+      ->RequestSuccessfulPresentationTimeForNextFrame(base::BindOnce(
+          [](base::TimeTicks menu_button_pressed_time,
+             base::TimeTicks presentation_time) {
+            UMA_HISTOGRAM_TIMES(
+                "Chrome.WrenchMenu.MenuButtonPressedToMenuShown",
+                presentation_time - menu_button_pressed_time);
+          },
+          base::TimeTicks::Now()));
+
   ShowMenu(event.IsKeyEvent() ? views::MenuRunner::SHOULD_SHOW_MNEMONICS
                               : views::MenuRunner::NO_FLAGS);
 }
