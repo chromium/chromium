66b2b1b1072d970d8410228f62ccd2d8489228a9
Joemer Ramos <joemerramos@chromium.org>
2023-07-05 14:56:15 +0000

    [iOS] Fix UIButtonConfiguration In PasswordsInOtherAppsViewController
    In the UI screenshots, you can see what the configuration looked like
    before and after the changes in this CL. This CL ensures that the
    title string looks as intended whether UIButtonConfiguration is enabled
    or disabled.
    
    UI Screenshot Before:
    https://drive.google.com/file/d/1IJwAgnQ9cLjeg8DgZi9xvxsu671kdE_T/view?usp=drive_link&resourcekey=0-sJMuKVzbh_LxHwIqxtsFvw
    
    After:
    https://drive.google.com/file/d/1EYh3r-JD74tlyF0dJNaCZ3dSnOrZPWbG/view?usp=drive_link&resourcekey=0-O-FL-YD_u0Jp2WIenYps_A
    
    Bug: 1432213
    Change-Id: I4af806c122c5f33c85de98dad7e3136ea5cb2681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4661852
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1165942}

--- ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_view_controller.mm
@@ -19,6 +19,7 @@
 #import "ios/chrome/common/ui/util/button_util.h"
 #import "ios/chrome/common/ui/util/image_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
+#import "ios/chrome/common/ui/util/sdk_forward_declares.h"
 #import "ios/chrome/common/ui/util/text_view_util.h"
 #import "ios/chrome/grit/ios_google_chrome_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -435,18 +436,23 @@ CGFloat const kContentOptimalWidth = 327;
   if (!_actionButton) {
     _actionButton = [[HighlightButton alloc] initWithFrame:CGRectZero];
 
-    // TODO(crbug.com/1418068): Simplify after minimum version required is >=
-    // iOS 15.
-    if (base::ios::IsRunningOnIOS15OrLater() &&
-        IsUIButtonConfigurationEnabled()) {
-      if (@available(iOS 15, *)) {
-        UIButtonConfiguration* buttonConfiguration =
-            [UIButtonConfiguration plainButtonConfiguration];
-        buttonConfiguration.contentInsets = NSDirectionalEdgeInsetsMake(
-            kButtonVerticalInsets, kButtonHorizontalMargin,
-            kButtonVerticalInsets, kButtonHorizontalMargin);
-        _actionButton.configuration = buttonConfiguration;
-      }
+    if (IsUIButtonConfigurationEnabled()) {
+      UIButtonConfiguration* buttonConfiguration =
+          [UIButtonConfiguration plainButtonConfiguration];
+      buttonConfiguration.contentInsets = NSDirectionalEdgeInsetsMake(
+          kButtonVerticalInsets, kButtonHorizontalMargin, kButtonVerticalInsets,
+          kButtonHorizontalMargin);
+      buttonConfiguration.background.backgroundColor =
+          [UIColor colorNamed:kGroupedSecondaryBackgroundColor];
+      buttonConfiguration.baseForegroundColor = [UIColor colorNamed:kBlueColor];
+
+      UIFont* font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+      NSAttributedString* attributedTitle = [[NSAttributedString alloc]
+          initWithString:self.actionString
+              attributes:@{NSFontAttributeName : font}];
+      buttonConfiguration.attributedTitle = attributedTitle;
+      buttonConfiguration.titleLineBreakMode = NSLineBreakByTruncatingTail;
+      _actionButton.configuration = buttonConfiguration;
     } else {
       UIEdgeInsets contentEdgeInsets =
           UIEdgeInsetsMake(kButtonVerticalInsets, 0, kButtonVerticalInsets, 0);
@@ -454,28 +460,26 @@ CGFloat const kContentOptimalWidth = 327;
           0, kButtonHorizontalMargin, 0, kButtonHorizontalMargin);
       SetContentEdgeInsets(_actionButton, contentEdgeInsets);
       SetTitleEdgeInsets(_actionButton, titleEdgeInsets);
+      [_actionButton
+          setBackgroundColor:[UIColor
+                                 colorNamed:kGroupedSecondaryBackgroundColor]];
+      UIColor* titleColor = [UIColor colorNamed:kBlueColor];
+      [_actionButton setTitleColor:titleColor forState:UIControlStateNormal];
+      _actionButton.titleLabel.font =
+          [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+      [_actionButton setTitle:self.actionString forState:UIControlStateNormal];
+      _actionButton.titleLabel.adjustsFontForContentSizeCategory = YES;
+      _actionButton.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     }
 
-    [_actionButton
-        setBackgroundColor:[UIColor
-                               colorNamed:kGroupedSecondaryBackgroundColor]];
-    UIColor* titleColor = [UIColor colorNamed:kBlueColor];
-    [_actionButton setTitleColor:titleColor forState:UIControlStateNormal];
-    _actionButton.titleLabel.font =
-        [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     _actionButton.layer.cornerRadius = kPrimaryButtonCornerRadius;
     _actionButton.translatesAutoresizingMaskIntoConstraints = NO;
-
     _actionButton.pointerInteractionEnabled = YES;
     _actionButton.pointerStyleProvider =
         CreateOpaqueButtonPointerStyleProvider();
-
-    [_actionButton setTitle:self.actionString forState:UIControlStateNormal];
-    _actionButton.titleLabel.adjustsFontForContentSizeCategory = YES;
     _actionButton.accessibilityIdentifier =
         kPasswordsInOtherAppsActionAccessibilityIdentifier;
 
-    _actionButton.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     [_actionButton addTarget:self
                       action:@selector(didTapActionButton)
             forControlEvents:UIControlEventTouchUpInside];
