5806a676ec2df42fac6bf5913345f7acadd7f364
Hirthanan Subenderan <hirthanan@google.com>
2023-07-05 16:38:04 +0000

    Introduce test utility functions for reporting device actives.
    The functions are used for testing and provide functionality
    to parse protocol buffer data from a file,
    retrieve test cases from regression test data,
    and obtain specific test data based on test names.
    
    BUG=chromium:1449132
    
    Change-Id: I3070e188b7912633824204a7000131562af65ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4648115
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165984}

---
+++ chromeos/ash/components/report/utils/test_utils.h
@@ -0,0 +1,52 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_TEST_UTILS_H_
+#define CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_TEST_UTILS_H_
+
+#include "base/files/file_path.h"
+#include "chromeos/ash/components/dbus/private_computing/private_computing_service.pb.h"
+#include "third_party/private_membership/src/internal/testing/regression_test_data/regression_test_data.pb.h"
+
+namespace ash::report::utils {
+
+// Number of test cases in the private membership regression test data library.
+static const int kPsmTestCaseSize = 10;
+
+// Number of test cases exist in the preserved file test data.
+static const int kPreservedFileTestCaseSize = 9;
+
+// This fake high entropy seed is used for testing purposes.
+static constexpr char kFakeHighEntropySeed[] =
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+// Used to initialize clock time to midnight GMT for unit tests.
+// Use cases are passed a GMT timestamp that is already adjusted to PST (GMT-8).
+// Report controller is passed the unadjusted GMT timestamp for a new PST day.
+static const char kFakeTimeNowString[] = "2023-01-01 00:00:00 GMT";
+static const char kFakeTimeNowUnadjustedString[] = "2023-01-01 08:00:00 GMT";
+
+// Fake UTC-based device activation date, formatted as YYYY-WW for privacy.
+// https://crsrc.org/o/src/third_party/chromiumos-overlay/chromeos-base/chromeos-activate-date/files/activate_date;l=67
+static const char kFakeFirstActivateDate[] = "2022-50";
+
+bool ParseProtoFromFile(const base::FilePath& file_path,
+                        google::protobuf::MessageLite* out_proto);
+
+private_membership::rlwe::PrivateMembershipRlweClientRegressionTestData::
+    TestCase
+    GetPsmTestCase(
+        private_membership::rlwe::PrivateMembershipRlweClientRegressionTestData*
+            test_data,
+        int test_idx);
+
+private_computing::PrivateComputingClientRegressionTestData::TestCase
+GetPreservedFileTestCase(
+    private_computing::PrivateComputingClientRegressionTestData* test_data,
+    private_computing::PrivateComputingClientRegressionTestData::TestName
+        test_name);
+
+}  // namespace ash::report::utils
+
+#endif  // CHROMEOS_ASH_COMPONENTS_REPORT_UTILS_TEST_UTILS_H_
