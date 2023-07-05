8e82ec6a5c5cc5346fe1df15189be4fb05503a07
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-07-05 13:58:29 +0000

    Reland "[ios] Integrate NTP set up list with sign-in sheet"
    This is a reland of commit 0afe7b96e7df7f4e98336473a73087c6704aa844.
    
    Original change's description:
    > Behavior changes are behind flag. Have the set up list lead to the
    > sign-in sheet instead of the sync consent dialog.
    >
    > Implementation screenshots: https://crbug.com/1447012#c18
    > Mocks:
    > https://docs.google.com/presentation/d/1OsOjLEeKmiaAmXtxoY_dNuA9AtY8qGpQW89JWmKglbw/edit?resourcekey=0-_Aj3Z2trrKwk9e002jik8g#slide=id.g253d770a5e9_3_22
    >
    > There's an ongoing discussion whether the string in the "See more"
    > view needs changing. It is left as is for now.
    >
    > Bug: 1447012
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4642088
    > Auto-Submit: Victor Vianna <victorvianna@google.com>
    > Reviewed-by: Scott Yoder <scottyoder@google.com>
    > Quick-Run: Victor Vianna <victorvianna@google.com>
    > Commit-Queue: Victor Vianna <victorvianna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1164321}
    
    Bug: 1447012
    Change-Id: Ie4e1697ba642f4f6e22aded463a560633375c035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4659500
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1165912}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_show_more_item_view.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_show_more_item_view.mm
@@ -4,7 +4,9 @@
 
 #import "ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_show_more_item_view.h"
 
+#import "base/feature_list.h"
 #import "base/notreached.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/ntp/set_up_list_item_type.h"
 #import "ios/chrome/browser/shared/ui/elements/crossfade_label.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
@@ -14,6 +16,7 @@
 #import "ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_view.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_google_chrome_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -160,7 +163,12 @@ NSAttributedString* Strikethrough(NSString* text) {
 - (NSString*)titleText {
   switch (_data.type) {
     case SetUpListItemType::kSignInSync:
-      return l10n_util::GetNSString(IDS_IOS_SET_UP_LIST_SIGN_IN_SYNC_TITLE);
+      return base::FeatureList::IsEnabled(
+                 syncer::kReplaceSyncPromosWithSignInPromos)
+                 ? l10n_util::GetNSString(
+                       IDS_IOS_CONSISTENCY_PROMO_DEFAULT_ACCOUNT_TITLE)
+                 : l10n_util::GetNSString(
+                       IDS_IOS_SET_UP_LIST_SIGN_IN_SYNC_TITLE);
     case SetUpListItemType::kDefaultBrowser:
       return l10n_util::GetNSString(IDS_IOS_SET_UP_LIST_DEFAULT_BROWSER_TITLE);
     case SetUpListItemType::kAutofill:
