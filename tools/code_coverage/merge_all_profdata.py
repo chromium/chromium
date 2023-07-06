2f7fe7135bd8469159e341fab9c12858d9d3d487
Julia Hansbrough <flowerhack@google.com>
2023-07-06 18:39:14 +0000

    [fuzz coverage] Remote coverage.py dependency from merge_all_fuzzers.py.
    The profdata file that coverage.py *exports* is different than what it
    uses to generate coverage locally, causing a divergence in behavior of
    the local coverage script and the recipe run for coverage in automation.
    
    Plus, coverage.py is far too heavyweight for what we need anyway.
    
    Replace coverage.py with a simple llvm-profdata merge command.
    
    Change-Id: Icd8c8f52abb214399395e4d82a34f71fa2bc1329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4657425
    Reviewed-by: Prakhar Asthana <pasthana@google.com>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166687}

--- tools/code_coverage/merge_all_profdata.py
+++ tools/code_coverage/merge_all_profdata.py
@@ -25,20 +25,18 @@ def _ParseCommandArguments():
   arg_parser = argparse.ArgumentParser()
   arg_parser.usage = __doc__
 
+  arg_parser.add_argument('--llvm-profdata',
+                          required=True,
+                          type=str,
+                          help='Location of LLVM profdata tool')
   arg_parser.add_argument('--profdata-dir',
                           required=True,
                           type=str,
                           help='Directory in which profdata files are stored.')
-  arg_parser.add_argument('--binaries-dir',
-                          required=True,
-                          type=str,
-                          help=('Directory where binaries have been built.'))
   arg_parser.add_argument(
-      '--report-dir',
+      '--outfile',
       type=str,
-      const='out/report',
-      default='out/report',
-      nargs='?',
+      required=True,
       help=('Directory where the coverage report should go. '
             'Default is out/report.'))
 
@@ -50,19 +48,12 @@ def _ParseCommandArguments():
 args = _ParseCommandArguments()
 targets_to_cover = []
 for profdata_file in os.listdir(args.profdata_dir):
-  target_name = profdata_file.split(".")[0]
-  target_path = os.path.join(args.binaries_dir, target_name)
-  profdata_file = target_name + ".profdata"
-  profdata_path = os.path.join(args.profdata_dir, profdata_file)
-  if os.path.isfile(target_path) and os.path.isfile(profdata_path):
-    targets_to_cover.append((target_name, profdata_path))
+  targets_to_cover.append(os.path.join(args.profdata_dir, profdata_file))
 
-subprocess_cmd = ['python3', 'tools/code_coverage/coverage.py']
+subprocess_cmd = [args.llvm_profdata, 'merge', '-o', args.outfile]
+
 for target in targets_to_cover:
-  subprocess_cmd.append(target[0])
-subprocess_cmd.extend(['-b', args.binaries_dir, '-o', args.report_dir])
-for target in targets_to_cover:
-  subprocess_cmd.extend(['-p', target[1]])
+  subprocess_cmd.append(target)
 try:
   subprocess.check_call(subprocess_cmd)
 except:
