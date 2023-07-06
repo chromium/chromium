6a34d3b975d25c6ebaaaed44e6248cbf5a7beb7b
Byron Lee <byronlee@google.com>
2023-07-06 07:57:03 +0000

    Move FakeBrowserContextHelperDelegate to another file
    Make it a shareable fake class so the clients of BrowserContextHelper
    can reuse it in their unit tests.
    
    BUG=None
    TEST=chromeos_unittests --gtest_filter="BrowserContextHelperTest.*"
    
    Change-Id: Id2efdfa3596f78a5abaf85b26def5ea6f1b75036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665881
    Reviewed-by: Chung-sheng Wu <chungsheng@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Byron Lee <byronlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166357}

---
+++ chromeos/ash/components/browser_context_helper/fake_browser_context_helper_delegate.h
@@ -0,0 +1,58 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_BROWSER_CONTEXT_HELPER_FAKE_BROWSER_CONTEXT_HELPER_DELEGATE_H_
+#define CHROMEOS_ASH_COMPONENTS_BROWSER_CONTEXT_HELPER_FAKE_BROWSER_CONTEXT_HELPER_DELEGATE_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/files/scoped_temp_dir.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
+
+namespace base {
+class FilePath;
+}  // namespace base
+
+namespace content {
+class BrowserContext;
+class TestBrowserContext;
+}  // namespace content
+
+namespace ash {
+
+// Helper class for unit tests. Used to construct the BrowserContextHelper
+// singleton.
+class FakeBrowserContextHelperDelegate : public BrowserContextHelper::Delegate {
+ public:
+  FakeBrowserContextHelperDelegate();
+  FakeBrowserContextHelperDelegate(const FakeBrowserContextHelperDelegate&) =
+      delete;
+  FakeBrowserContextHelperDelegate& operator=(
+      const FakeBrowserContextHelperDelegate&) = delete;
+  ~FakeBrowserContextHelperDelegate() override;
+
+  // Creates a BrowserContext object for the given |path|. The |path| should
+  // be a subpath of |user_data_dir_|.
+  content::BrowserContext* CreateBrowserContext(const base::FilePath& path,
+                                                bool is_off_the_record);
+
+  // BrowserContextHelper::Delegate overrides.
+  content::BrowserContext* GetBrowserContextByPath(
+      const base::FilePath& path) override;
+  content::BrowserContext* DeprecatedGetBrowserContext(
+      const base::FilePath& path) override;
+  content::BrowserContext* GetOrCreatePrimaryOTRBrowserContext(
+      content::BrowserContext* browser_context) override;
+  const base::FilePath* GetUserDataDir() override;
+
+ private:
+  base::ScopedTempDir user_data_dir_;
+  std::vector<std::unique_ptr<content::TestBrowserContext>>
+      browser_context_list_;
+};
+
+}  // namespace ash
+
+#endif  // CHROMEOS_ASH_COMPONENTS_BROWSER_CONTEXT_HELPER_FAKE_BROWSER_CONTEXT_HELPER_DELEGATE_H_
