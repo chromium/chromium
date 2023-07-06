f0fca50a5e8dc98be8541528d5f004bc7540202e
Olesia Marukhno <olesiamarukhno@google.com>
2023-07-06 15:21:54 +0000

    [user bypass] Display title and description in page info
    Display the title and the description in the new third-party cookies section in page info. The title and the description should be updated
    based on the third-party cookies status.
    
    Bug: 1446230, b:285315064
    Change-Id: I95c382e63fc8b081977fee8ddee08c5987ee24d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4666009
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1166547}

--- chrome/browser/ui/views/page_info/page_info_cookies_content_view.h
+++ chrome/browser/ui/views/page_info/page_info_cookies_content_view.h
@@ -13,6 +13,10 @@
 #include "ui/views/controls/button/toggle_button.h"
 #include "ui/views/view.h"
 
+namespace views {
+class Label;
+}  // namespace views
+
 // The view that is used as a content view of the Cookies subpage in page info.
 // It contains information about cookies (short description, how many sites
 // are allowed).
@@ -47,6 +51,9 @@ class PageInfoCookiesContentView : public views::View, public PageInfoUI {
   //  so does it  and sets its info.
   void SetBlockingThirdPartyCookiesInfo(const CookiesNewInfo& cookie_info);
 
+  // Updates the new third-party cookies section using |cookie_info|.
+  void SetThirdPartyCookiesInfo(const CookiesNewInfo& cookie_info);
+
   // Updates toggles state according to info.
   void UpdateBlockingThirdPartyCookiesToggle(bool are_cookies_blocked);
 
@@ -68,39 +75,50 @@ class PageInfoCookiesContentView : public views::View, public PageInfoUI {
   // placeholder information if necessary.
   void InitFpsButton(bool is_managed);
 
+  // Initializes the new third-party cookies section. The section starts out
+  // hidden and is only shown when third-party cookies are blocked or there is
+  // an active exception.
+  void AddThirdPartyCookiesContainer();
+
   base::OnceClosure initialized_callback_ = base::NullCallback();
 
   raw_ptr<PageInfo, DanglingUntriaged> presenter_;
 
   // The view that contains the fps_button and cookies_dialog_button.
-  raw_ptr<views::View> cookies_buttons_container_view_ = nullptr;
+  raw_ptr<views::View> cookies_buttons_container_view_;
 
   // The button that opens Cookie Dialog and displays a number of allowed sites.
-  raw_ptr<RichHoverButton> cookies_dialog_button_ = nullptr;
+  raw_ptr<RichHoverButton> cookies_dialog_button_;
 
   // The view that contains toggle for blocking third party cookies
   // and displays information with a number of blocked sites.
   // Only displayed when third party cookies are blocked in settings.
-  raw_ptr<PageInfoRowView> blocking_third_party_cookies_row_ = nullptr;
+  raw_ptr<PageInfoRowView> blocking_third_party_cookies_row_;
 
   // The Label which is a subtitle of |blocking_third_party_cookies_row|.
-  raw_ptr<views::Label> blocking_third_party_cookies_subtitle_label_ = nullptr;
+  raw_ptr<views::Label> blocking_third_party_cookies_subtitle_label_;
 
   // The toggle on |blocking_third_party_cookies_row| when state is managed by
   // the user.
-  raw_ptr<views::ToggleButton> blocking_third_party_cookies_toggle_ = nullptr;
+  raw_ptr<views::ToggleButton> blocking_third_party_cookies_toggle_;
 
   // The icon on |blocking_third_party_cookies_row| when state is enforced.
-  raw_ptr<NonAccessibleImageView> enforced_icon_ = nullptr;
+  raw_ptr<NonAccessibleImageView> enforced_icon_;
 
   // The button that displays First-Party-Set information with a link to
   // 'All sites' settings page.
-  raw_ptr<RichHoverButton> fps_button_ = nullptr;
+  raw_ptr<RichHoverButton> fps_button_;
 
   // Used to keep track if it's the first time for this instance recording the
   // FPS info histogram. Needed to not record the histogram each time page info
   // status changed.
   bool fps_histogram_recorded_ = false;
+
+  // Third-party cookies section which contains a title, a description and a
+  // toggle row view.
+  raw_ptr<views::View> third_party_cookies_container_;
+  raw_ptr<views::Label> third_party_cookies_title_;
+  raw_ptr<views::Label> third_party_cookies_description_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PAGE_INFO_PAGE_INFO_COOKIES_CONTENT_VIEW_H_
