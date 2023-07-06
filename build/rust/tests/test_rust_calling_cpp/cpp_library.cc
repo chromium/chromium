54589da9691a6526ac11d05ae1198b7b700c098a
danakj <danakj@chromium.org>
2023-07-06 20:02:00 +0000

    Add a Rust build test for Rust calling C++, in a C++-linked exe.
    R=collinbaker@chromium.org
    
    Bug: 1292038
    Change-Id: Ib7c3181e7738e0a86636e5f90b9f6ff02763d49d
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670208
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166726}

---
+++ build/rust/tests/test_rust_calling_cpp/cpp_library.cc
@@ -0,0 +1,9 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "build/rust/tests/test_rust_calling_cpp/cpp_library.h"
+
+int32_t mul_by_2_in_cpp_library(int32_t a) {
+  return a * 2;
+}
---
+++ build/rust/tests/test_rust_calling_cpp/rust_calling_cpp_rlib.rs
@@ -0,0 +1,22 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[allow(unsafe_op_in_unsafe_fn)]
+#[cxx::bridge]
+mod ffi {
+    extern "Rust" {
+        fn rust_calling_cpp();
+    }
+
+    unsafe extern "C++" {
+        include!("build/rust/tests/test_rust_calling_cpp/cpp_library.h");
+
+        fn mul_by_2_in_cpp_library(a: i32) -> i32;
+    }
+}
+
+#[no_mangle]
+pub fn rust_calling_cpp() {
+    assert_eq!(ffi::mul_by_2_in_cpp_library(3), 6);
+}
