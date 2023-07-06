778ebc7d879b50ba53b579795f450f32a736b323
Alexander Cooper <alcooper@chromium.org>
2023-07-06 19:19:55 +0000

    Reland "Implement VR tests for cardboard vr provider."
    This reverts commit ac3917bb8a6fb2eb46b99d151963d7745267a4b4.
    
    Reason for revert: https://crrev.com/c/4667167
    
    Original change's description:
    > Revert "Implement VR tests for cardboard vr provider."
    >
    > This reverts commit 9f4f9374a5962ce53ee53c776ed0037e24186b46.
    >
    > Reason for revert: failing on the "chrome_public_test_vr_apk-ddready-cardboard" suite
    >
    > Original change's description:
    > > Implement VR tests for cardboard vr provider.
    > >
    > > Bug: 1429384
    > > Change-Id: I71becb5bad70149a84fde5ab1e7f8824cb908db2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4505177
    > > Commit-Queue: Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
    > > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1163812}
    >
    > Bug: 1429384
    > Change-Id: Ic799ea9aea83455a0f4d4caeaa83bfa521bd69da
    > Fixed: 1462445
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667950
    > Owners-Override: Nate Fischer <ntfschr@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1166248}
    
    Bug: 1429384, 1462466
    Change-Id: Iba7c76fc8972774921defc699ef14426b4fb8402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667719
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1166711}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/rules/ChromeTabbedActivityVrCardboardTestRule.java
@@ -0,0 +1,41 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.vr.rules;
+
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import org.chromium.chrome.browser.vr.rules.XrActivityRestriction.SupportedActivity;
+import org.chromium.chrome.browser.vr.util.VrCardboardTestRuleUtils;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+
+/**
+ * Cardboard extension of ChromeTabbedActivityTestRule. Applies ChromeTabbedActivityTestRule
+ * then opens up a ChromeTabbedActivity to a blank page while performing some additional setup.
+ */
+public class ChromeTabbedActivityVrCardboardTestRule
+        extends ChromeTabbedActivityTestRule implements VrTestRule {
+    @Override
+    public Statement apply(final Statement base, final Description desc) {
+        return super.apply(new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                VrCardboardTestRuleUtils.evaluateVrTestRuleImpl(base, desc,
+                        ChromeTabbedActivityVrCardboardTestRule.this,
+                        () -> { startMainActivityOnBlankPage(); });
+            }
+        }, desc);
+    }
+
+    @Override
+    public @SupportedActivity int getRestriction() {
+        return SupportedActivity.CTA;
+    }
+
+    @Override
+    public boolean isDonEnabled() {
+        return false;
+    }
+}
