e148c5876bf61977943628f35f9f02f8c1e0a1c7
Daniel Rubery <drubery@chromium.org>
2023-07-06 00:55:23 +0000

    Allow LEARN_MORE_SCANNING in the download bubble
    This download command is used to show the deep scanning learn more link,
    but currently hits a NOTREACHED error. This CL fixes that by forwarding
    the command to `DownloadCommands`.
    
    Bug: 1424527
    Change-Id: I797da5ec8852c7dbfb962dca088ab3a600fa6aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667990
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166253}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -240,6 +240,7 @@ void DownloadBubbleUIController::ProcessDownloadButtonPress(
     case DownloadCommands::SHOW_IN_FOLDER:
     case DownloadCommands::ALWAYS_OPEN_TYPE:
     case DownloadCommands::CANCEL_DEEP_SCAN:
+    case DownloadCommands::LEARN_MORE_SCANNING:
       commands.ExecuteCommand(command);
       break;
     default:
