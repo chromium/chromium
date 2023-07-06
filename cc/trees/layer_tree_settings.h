2842bd956b93c4191e021e927aa11003fcd61eaf
Benoit Lize <lizeb@chromium.org>
2023-07-06 15:12:09 +0000

    [cc/trees] Remove unused setting in LayerTreeSettings
    Change-Id: I96cbb0bc4e476e2edf0e12c1ada3e98d942efb9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667471
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1166542}

--- cc/trees/layer_tree_settings.h
+++ cc/trees/layer_tree_settings.h
@@ -57,7 +57,6 @@ class CC_EXPORT LayerTreeSettings {
   bool scrollbar_flash_after_any_scroll_update = false;
   SkColor4f solid_color_scrollbar_color = SkColors::kWhite;
   base::TimeDelta scroll_animation_duration_for_testing;
-  bool timeout_and_draw_when_animation_checkerboards = true;
   bool layers_always_allowed_lcd_text = false;
   float low_res_contents_scale_factor = 0.25f;
   float top_controls_show_threshold = 0.5f;
