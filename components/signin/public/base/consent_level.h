ac82f374d3fd6bc54fcdc817d8b662f68482d549
Boris Sazonov <bsazonov@google.com>
2023-07-07 14:41:49 +0000

    Add a comment about ConsentLevel::kSync deprecation & migration
    Bug: 1462286
    Change-Id: Ie4ba75b0593ec21534ef9a5ef97d5cfb7d4cf912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4667402
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167429}

--- components/signin/public/base/consent_level.h
+++ components/signin/public/base/consent_level.h
@@ -18,9 +18,36 @@ enum class ConsentLevel {
   // See "unconsented primary account" in ./README.md.
   kSignin,
 
-  // Chrome browser sync consent is required. Historically (before DICE and
-  // Project Butter) most operations implicitly required this consent. See
-  // "primary account" in ./README.md.
+  // This value is deprecated and will be removed in multiple steps. Each
+  // platform will be migrated separately: iOS is expected to be migrated first,
+  // Android will follow soon, while Desktop/ChromeOS might take a bit longer.
+  // Expected migration phases:
+  // 1. Pre-migration state: sync opt-in flows move IdentityManager into kSync
+  //    state.
+  // 2. The majority of flows move IdentityManager into kSignin state, but there
+  //    are few legacy flows that still allow moving into kSync state (for
+  //    example, restoring from backups on platforms where this functionality
+  //    exists). The behavior for existing users in kSync state is unchanged (so
+  //    some code paths still check kSync state to keep the behavior
+  //    consistent).
+  // 3. None of the user-visible flows move IdentityManager into kSignin state.
+  //    The behavior for existing users in kSync state is unchanged (so some
+  //    code paths still check kSync state to keep the behavior consistent).
+  // 4. All users are migrated to kSignin state. The migration will happen
+  //    per-platform, so cross-platform code paths that check kSync should still
+  //    be kept around (even though they are no longer executed on migrated
+  //    platforms).
+  // 5. When all platforms get to step 4, kSync will be completely removed from
+  //    the codebase.
+  //
+  // Considering the upcoming deletion of kSync, features should not be adding
+  // new dependencies on this value. See
+  // docs.google.com/document/d/1waia7V_2SfK1vQpT40Gv1fKV_hqRQ1JgE86FRediGp0
+  // for the current migration state on different platforms.
+  //
+  // This value was used by features that needed Chrome browser sync consent to
+  // be enabled. Historically (before DICE and Project Butter) most operations
+  // implicitly required this consent. See "primary account" in ./README.md.
   kSync
 };
 
