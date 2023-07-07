2f6a76fb694cb2f76254660f6ad92561a9a8f58b
Noémie St-Onge <noemies@google.com>
2023-07-07 16:58:07 +0000

    [iOS] Fix crash on [ArchivableCredentialStore saveDataWithCompletion:]
    The crash was caused by
    [[NSFileManager defaultManager] createDirectoryAtURL:self.fileURL...]
    being called with a `nil` URL. The fact that the completion block of
    `saveDataWithCompletion` was holding a strong reference to `self`
    seemed to be the root cause of the issue. Hence, this CL uses a weak
    self in the completion block and early returns when `self` is `nil`.
    
    Since the crash couldn't be reproduced, it will be monitored to make
    sure this fix does fix the issue.
    
    Fixed: 1449638
    Change-Id: If114a2dfd5602afebffc6347ff60079ed631203f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4622007
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167502}

--- ios/chrome/common/credential_provider/archivable_credential_store.mm
+++ ios/chrome/common/credential_provider/archivable_credential_store.mm
@@ -17,7 +17,7 @@
 
 @interface ArchivableCredentialStore ()
 
-// The fileURL to the disk file, can be nil.
+// The fileURL to the disk file.
 @property(nonatomic, strong) NSURL* fileURL;
 
 @end
@@ -29,7 +29,7 @@
 - (instancetype)initWithFileURL:(NSURL*)fileURL {
   self = [super init];
   if (self) {
-    DCHECK(fileURL.isFileURL) << "URL must be a file URL.";
+    CHECK(fileURL.isFileURL) << "URL must be a file URL.";
     _fileURL = fileURL;
   }
   return self;
@@ -38,40 +38,20 @@
 #pragma mark - CredentialStore
 
 - (void)saveDataWithCompletion:(void (^)(NSError* error))completion {
+  __weak __typeof(self) weakSelf = self;
   dispatch_barrier_async(self.workingQueue, ^{
-    auto executeCompletionIfPresent = ^(NSError* error) {
-      if (completion) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-          completion(error);
-        });
-      }
-    };
-
-    NSError* error = nil;
-    NSData* data =
-        [NSKeyedArchiver archivedDataWithRootObject:self.memoryStorage
-                              requiringSecureCoding:YES
-                                              error:&error];
-    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
-    if (error) {
-      executeCompletionIfPresent(error);
-      return;
+    if (weakSelf) {
+      [weakSelf saveDataWithCompletionBlockBody:completion];
+    } else {
+      NSError* error =
+          [[NSError alloc] initWithDomain:@""
+                                     code:0
+                                 userInfo:@{
+                                   NSLocalizedDescriptionKey :
+                                       @"ArchivableCredentialStore is nil."
+                                 }];
+      completion(error);
     }
-
-    [[NSFileManager defaultManager]
-               createDirectoryAtURL:self.fileURL.URLByDeletingLastPathComponent
-        withIntermediateDirectories:YES
-                         attributes:nil
-                              error:&error];
-
-    if (error) {
-      executeCompletionIfPresent(error);
-      return;
-    }
-
-    [data writeToURL:self.fileURL options:NSDataWritingAtomic error:&error];
-    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
-    executeCompletionIfPresent(error);
   });
 }
 
@@ -107,6 +87,53 @@
                                              error:&error];
   DCHECK(!error) << base::SysNSStringToUTF8(error.description);
   return dictionary;
+}
+
+#pragma mark - Private
+
+// Body of the `saveDataWithCompletion`'s block. Body was extracted so that the
+// `self`/`weak self` management is easier. `saveDataWithCompletion` takes the
+// responsability of calling `saveDataWithCompletionBlockBody` on a weak
+// version of `self`. There is therefore no need to use a weak reference
+// everywhere in the method here.
+- (void)saveDataWithCompletionBlockBody:(void (^)(NSError* error))completion {
+  auto executeCompletionIfPresent = ^(NSError* error) {
+    if (completion) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+        completion(error);
+      });
+    }
+  };
+
+  NSError* error = nil;
+
+  if (self) {
+    NSData* data =
+        [NSKeyedArchiver archivedDataWithRootObject:self.memoryStorage
+                              requiringSecureCoding:YES
+                                              error:&error];
+    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+    if (error) {
+      executeCompletionIfPresent(error);
+      return;
+    }
+
+    [[NSFileManager defaultManager]
+               createDirectoryAtURL:self.fileURL.URLByDeletingLastPathComponent
+        withIntermediateDirectories:YES
+                         attributes:nil
+                              error:&error];
+
+    if (error) {
+      executeCompletionIfPresent(error);
+      return;
+    }
+
+    [data writeToURL:self.fileURL options:NSDataWritingAtomic error:&error];
+    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+  }
+
+  executeCompletionIfPresent(error);
 }
 
 @end
