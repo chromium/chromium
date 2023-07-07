0eaf16641933097087dd2b282d8c411666c02d7a
Daniel Cheng <dcheng@chromium.org>
2023-07-07 23:40:37 +0000

    Migrate promos pref string constants to inline constexpr.
    This is a set of changes to migrate various pref string constants to be
    defined using inline constexpr. The end goal here is to unblock
    migration of the syncable pref allowlist to a compile time FixedFlatMap
    rather than building the allowlist dynamically at run time.
    
    Bug: 1459785
    Change-Id: I1a39be5ff5c623a52c52c174fce14b1033abea4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673449
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167686}

--- chrome/browser/promos/promos_pref_names.h
+++ chrome/browser/promos/promos_pref_names.h
@@ -6,9 +6,21 @@
 #define CHROME_BROWSER_PROMOS_PROMOS_PREF_NAMES_H_
 
 namespace promos_prefs {
-extern const char kiOSPasswordPromoLastImpressionTimestamp[];
-extern const char kiOSPasswordPromoImpressionsCounter[];
-extern const char kiOSPasswordPromoOptOut[];
+
+// Int64 that keeps track of the last impression timestamp of the "iOS password
+// promo bubble on desktop" for a given user.
+inline constexpr char kiOSPasswordPromoLastImpressionTimestamp[] =
+    "promos.ios_password_last_impression_timestamp";
+
+// Integer that keeps track of impressions of the "iOS password promo bubble on
+// desktop" shown to a given user.
+inline constexpr char kiOSPasswordPromoImpressionsCounter[] =
+    "promos.ios_password_impressions_counter";
+
+// Boolean that keeps track whether a given user has opted-out of seeing the
+// "iOS password promo bubble on desktop" again.
+inline constexpr char kiOSPasswordPromoOptOut[] = "promos.ios_password_opt_out";
+
 }  // namespace promos_prefs
 
 #endif  // CHROME_BROWSER_PROMOS_PROMOS_PREF_NAMES_H_
