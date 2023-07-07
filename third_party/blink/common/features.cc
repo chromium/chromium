9cefeb61ddb1f0acbd51944a8531eb516da7224d
Hirokazu Honda <hiroh@chromium.org>
2023-07-07 05:22:55 +0000

    blink: Enable WebRtcInitializeEncoderOnFirstFrame by default
    Bug: 1439782
    Test: None
    Change-Id: Ifea083fc1b3f69dc550e4c327bd42355108eae9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4670931
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167225}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1880,7 +1880,7 @@ BASE_FEATURE(kWebRtcIgnoreUnspecifiedColorSpace,
 
 BASE_FEATURE(kWebRtcInitializeEncoderOnFirstFrame,
              "WebRtcInitializeEncoderOnFirstFrame",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kWebRtcMetronome,
              "WebRtcMetronome",
