fb55766ca133a8ab7900776d1ec4642f3488911c
Daniel Cheng <dcheng@chromium.org>
2023-07-07 23:41:46 +0000

    Migrate tethering pref string constants to inline constexpr.
    This is a set of changes to migrate various pref string constants to be
    defined using inline constexpr. The end goal here is to unblock
    migration of the syncable pref allowlist to a compile time FixedFlatMap
    rather than building the allowlist dynamically at run time.
    
    Bug: 1459785
    Change-Id: I859313da2ed2fb4ff350f9b72f646d591e2234b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673451
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167688}

--- chromeos/ash/components/tether/pref_names.h
+++ chromeos/ash/components/tether/pref_names.h
@@ -5,39 +5,37 @@
 #ifndef CHROMEOS_ASH_COMPONENTS_TETHER_PREF_NAMES_H_
 #define CHROMEOS_ASH_COMPONENTS_TETHER_PREF_NAMES_H_
 
-namespace ash {
-
-namespace tether {
-
-namespace prefs {
+namespace ash::tether::prefs {
 
 // Preference name for the preference which stores IDs corresponding to devices
 // which have most recently replied to a TetherAvailabilityRequest with a
 // response code indicating that tethering is available. The value stored is a
 // ListValue, with the most recent response residing at the start of the list.
-extern const char kMostRecentTetherAvailablilityResponderIds[];
+inline constexpr char kMostRecentTetherAvailablilityResponderIds[] =
+    "tether.most_recent_tether_availability_responder_ids";
 
 // Preference name for the preference which stores IDs corresponding to devices
 // which have most recently replied to a ConnectTetheringResponse with a
 // response code indicating that tethering is available. The value stored is a
 // ListValue, with the most recent response residing at the start of the list.
-extern const char kMostRecentConnectTetheringResponderIds[];
+inline constexpr char kMostRecentConnectTetheringResponderIds[] =
+    "tether.most_recent_connect_tethering_responder_ids";
 
 // The status of the active host. The value stored for this key is the integer
 // version of an ActiveHost::ActiveHostStatus enumeration value.
-extern const char kActiveHostStatus[];
+inline constexpr char kActiveHostStatus[] = "tether.active_host_status";
 
 // The device ID of the active host. If there is no active host, the value at
 // this key is "".
-extern const char kActiveHostDeviceId[];
+inline constexpr char kActiveHostDeviceId[] = "tether.active_host_device_id";
 
 // The tether network GUID of the active host. If there is no active host, the
 // value at this key is "".
-extern const char kTetherNetworkGuid[];
+inline constexpr char kTetherNetworkGuid[] = "tether.tether_network_id";
 
 // The Wi-Fi network GUID of the active host. If there is no active host, the
 // value at this key is "".
-extern const char kWifiNetworkGuid[];
+inline constexpr char kWifiNetworkGuid[] = "tether.wifi_network_id";
 
 // The Wi-Fi network path that is currently being disconnected. When
 // disconnecting under normal circumstances, this value is set when a
@@ -48,17 +46,14 @@ extern const char kWifiNetworkGuid[];
 // When the Tether component starts up again (the next time the user logs in),
 // this path is fetched, the associated network configuration is removed, and
 // the path is cleared from prefs.
-extern const char kDisconnectingWifiNetworkPath[];
+inline constexpr char kDisconnectingWifiNetworkPath[] =
+    "tether.disconnecting_wifi_network_path";
 
 // Scanned Tether host results. The value stored is a ListValue containing
 // DictionaryValues containing the scan results. See PersistentHostScanCache for
 // more details.
-extern const char kHostScanCache[];
+inline constexpr char kHostScanCache[] = "tether.host_scan_cache";
 
-}  // namespace prefs
-
-}  // namespace tether
-
-}  // namespace ash
+}  // namespace ash::tether::prefs
 
 #endif  // CHROMEOS_ASH_COMPONENTS_TETHER_PREF_NAMES_H_
