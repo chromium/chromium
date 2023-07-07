c7fb64a6b74b0d7899596c577d0a4714751632d1
Daniel Cheng <dcheng@chromium.org>
2023-07-07 23:50:42 +0000

    Migrate ash events pref string constants to inline constexpr.
    This is a set of changes to migrate various pref string constants to be
    defined using inline constexpr. The end goal here is to unblock
    migration of the syncable pref allowlist to a compile time FixedFlatMap
    rather than building the allowlist dynamically at run time.
    
    Bug: 1459785
    Change-Id: Icf294a9e00e748509466a66111efa4357238f7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4673459
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1167695}

--- ui/events/ash/pref_names.h
+++ ui/events/ash/pref_names.h
@@ -10,15 +10,32 @@ namespace prefs {
 // Constants for the names of various preferences, for easier changing.
 // TODO:: Those pref names probably need to live in a better place.
 
-extern const char kLanguageRemapCapsLockKeyTo[];
-extern const char kLanguageRemapSearchKeyTo[];
-extern const char kLanguageRemapControlKeyTo[];
-extern const char kLanguageRemapAltKeyTo[];
-extern const char kLanguageRemapEscapeKeyTo[];
-extern const char kLanguageRemapBackspaceKeyTo[];
-extern const char kLanguageRemapAssistantKeyTo[];
-extern const char kLanguageRemapExternalCommandKeyTo[];
-extern const char kLanguageRemapExternalMetaKeyTo[];
+// *************** PROFILE PREFS ***************
+// These are attached to the user profile
+
+// Integer prefs which determine how we remap modifier keys (e.g. swap Alt and
+// Control.) Possible values for these prefs are 0-7. See ModifierKey enum in
+// src/ui/events/ash/mojom/modifier_key.mojom
+inline constexpr char kLanguageRemapSearchKeyTo[] =
+    // Note: we no longer use XKB for remapping these keys, but we can't change
+    // the pref names since the names are already synced with the cloud.
+    "settings.language.xkb_remap_search_key_to";
+inline constexpr char kLanguageRemapControlKeyTo[] =
+    "settings.language.xkb_remap_control_key_to";
+inline constexpr char kLanguageRemapAltKeyTo[] =
+    "settings.language.xkb_remap_alt_key_to";
+inline constexpr char kLanguageRemapCapsLockKeyTo[] =
+    "settings.language.remap_caps_lock_key_to";
+inline constexpr char kLanguageRemapEscapeKeyTo[] =
+    "settings.language.remap_escape_key_to";
+inline constexpr char kLanguageRemapBackspaceKeyTo[] =
+    "settings.language.remap_backspace_key_to";
+inline constexpr char kLanguageRemapAssistantKeyTo[] =
+    "settings.language.xkb_remap_assistant_key_to";
+inline constexpr char kLanguageRemapExternalCommandKeyTo[] =
+    "settings.language.remap_external_command_key_to";
+inline constexpr char kLanguageRemapExternalMetaKeyTo[] =
+    "settings.language.remap_external_meta_key_to";
 
 }  // namespace prefs
 
