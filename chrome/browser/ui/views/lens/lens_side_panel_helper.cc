2e7dda7eaecc83327d753cd0d62aeb56a7039c92
Jason Hu <hujasonx@google.com>
2023-07-08 08:09:37 +0000

    [Lens]Send accurate side panel webview size
    Does this by moving the place where the viewport size url param is set
    from before the webview / side panel is created to after, when the url
    is about to be opened.
    
    Bug: 1463036
    Change-Id: Icdde18ba7ea7ace2f571aa8b01c86696ae608cee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4669784
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1167746}

--- chrome/browser/ui/views/lens/lens_side_panel_helper.cc
+++ chrome/browser/ui/views/lens/lens_side_panel_helper.cc
@@ -45,14 +45,12 @@ bool ShouldPageBeVisible(const GURL& url) {
 GURL CreateURLForNewTab(const GURL& original_url) {
   if (!IsValidLensResultUrl(original_url))
     return GURL();
-  // Set the side panel max size to zero, as this is not a side panel request.
-  gfx::Size side_panel_initial_size = gfx::Size();
 
   // Append or replace query parameters related to entry point.
   return AppendOrReplaceQueryParametersForLensRequest(
       original_url, EntryPoint::CHROME_OPEN_NEW_TAB_SIDE_PANEL,
       RenderingEnvironment::ONELENS_DESKTOP_WEB_FULLSCREEN,
-      /*is_side_panel_request=*/false, side_panel_initial_size);
+      /*is_side_panel_request=*/false);
 }
 
 void OpenLensSidePanel(Browser* browser,
