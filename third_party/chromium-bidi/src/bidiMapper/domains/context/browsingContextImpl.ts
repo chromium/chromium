883fb3f88db8e35a2b6750ffbb9c336be6ade250
Thiago Perrotta <tperrotta@chromium.org>
2023-07-10 14:41:23 -0400

    feat(print): throw unsupported operation when content area is empty (#992)
    Bug: #518
    Spec: https://github.com/w3c/webdriver-bidi/issues/473
    WPT: https://github.com/web-platform-tests/wpt/pull/40872

--- third_party/chromium-bidi/src/bidiMapper/domains/context/browsingContextImpl.ts
+++ third_party/chromium-bidi/src/bidiMapper/domains/context/browsingContextImpl.ts
@@ -816,14 +816,24 @@ export class BrowsingContextImpl {
       cdpParams.preferCSSPageSize = !params.shrinkToFit;
     }
 
-    const result = await this.#cdpTarget.cdpClient.sendCommand(
-      'Page.printToPDF',
-      cdpParams
-    );
-
-    return {
-      data: result.data,
-    };
+    try {
+      const result = await this.#cdpTarget.cdpClient.sendCommand(
+        'Page.printToPDF',
+        cdpParams
+      );
+      return {
+        data: result.data,
+      };
+    } catch (error: any) {
+      // Effectively zero dimensions.
+      if (
+        (error as Error).message ===
+        'invalid print parameters: content area is empty'
+      ) {
+        throw new UnsupportedOperationException(error.message);
+      }
+      throw error;
+    }
   }
 
   async close(): Promise<void> {
