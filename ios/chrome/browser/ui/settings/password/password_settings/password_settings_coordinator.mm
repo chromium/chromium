466dd7dfb6dc19321f54f0193ff613697e1148bd
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-10 14:57:08 +0000

    [iOS] dealloc checks stop was called
    Change-Id: I874c6f97aa492ddf80a5f846068cfe764cf419e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4671158
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168094}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
@@ -129,8 +129,12 @@
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
                                    browser:(Browser*)browser {
-  self = [super initWithBaseViewController:viewController browser:browser];
-  return self;
+  return [super initWithBaseViewController:viewController browser:browser];
+}
+
+- (void)dealloc {
+  // TODO(crbug.com/1454777)
+  DUMP_WILL_BE_CHECK(!_mediator);
 }
 
 #pragma mark - ChromeCoordinator
@@ -196,11 +200,19 @@
   self.passwordsInOtherAppsCoordinator.delegate = nil;
   self.passwordsInOtherAppsCoordinator = nil;
 
+  self.passwordSettingsViewController.presentationDelegate = nil;
+  self.passwordSettingsViewController.delegate = nil;
   self.passwordSettingsViewController = nil;
   self.settingsNavigationController = nil;
   _preparingPasswordsAlert = nil;
 
+  _dispatcher = nil;
+  _reauthModule = nil;
+
   [self.mediator disconnect];
+  self.mediator.consumer = nil;
+  self.mediator = nil;
+  _savedPasswordsPresenter.reset();
 }
 
 #pragma mark - PasswordSettingsPresentationDelegate
