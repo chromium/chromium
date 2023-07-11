9b40191c2ae027e604066358f1ea65e5713aa0e2
Dale Curtis <dalecurtis@chromium.org>
2023-07-11 21:09:47 +0000

    Revert "[Tailored security] Implement initialization of retry logic"
    This reverts commit 67e1a5f34e208f1e489100dbf5aed64890bbbef6.
    
    Reason for revert: https://crbug.com/1464056
    
    Original change's description:
    > [Tailored security] Implement initialization of retry logic
    >
    > Add logic to configure the start state for an iteration of the tailored
    > security retry logic.
    >
    > Change-Id: I7a19e27454ec6c126c2a89e6e12937a655fd52b7
    > Bug: 1346776
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654987
    > Commit-Queue: Javier Castro <jacastro@chromium.org>
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1168730}
    
    Bug: 1346776
    Change-Id: I0e163c25a946d40e4bd5d4a97ae0ed70bf63ef30
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679561
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168953}

--- components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.h
+++ components/safe_browsing/core/browser/tailored_security_service/tailored_security_service.h
@@ -13,7 +13,6 @@
 #include <vector>
 
 #include "base/functional/callback_forward.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -162,16 +161,6 @@ class TailoredSecurityService : public KeyedService {
   GetURLLoaderFactory() = 0;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(
-      TailoredSecurityServiceTest,
-      RetryEnabledTimestampUpdateCallbackSetsOutcomeToUnknown);
-  FRIEND_TEST_ALL_PREFIXES(TailoredSecurityServiceTest,
-                           RetryEnabledTimestampUpdateCallbackRecordsStartTime);
-  FRIEND_TEST_ALL_PREFIXES(
-      TailoredSecurityServiceTest,
-      RetryDisabledTimestampUpdateCallbackDoesNotRecordStartTime);
-  FRIEND_TEST_ALL_PREFIXES(TailoredSecurityServiceTest,
-                           RetryDisabledOutcomeRemainsUnset);
   friend class TailoredSecurityTabHelperTest;
 
   // Stores pointer to IdentityManager instance. It must outlive the
