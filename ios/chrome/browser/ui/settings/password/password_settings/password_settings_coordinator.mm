78c66f3b76aede409c13ddb1aba3053dc660a4af
Arthur Milchior <arthurmilchior@chromium.org>
2023-07-11 08:18:09 +0000

    [iOS] remove trivial method
    R=jlebel
    
    Bug: None
    Change-Id: Ib242985ac2cf5072cdb7787303ae80d3a14c7166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675823
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168575}

--- ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_settings/password_settings_coordinator.mm
@@ -127,11 +127,6 @@
 
 @implementation PasswordSettingsCoordinator
 
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser {
-  return [super initWithBaseViewController:viewController browser:browser];
-}
-
 - (void)dealloc {
   // TODO(crbug.com/1454777)
   DUMP_WILL_BE_CHECK(!_mediator);
