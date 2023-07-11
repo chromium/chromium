2f2e075c6537f059819fdc8f946d452317039932
Sean Maher <spvw@chromium.org>
2023-07-11 22:38:36 +0000

    Change startup_metrics_utils to a component to fix issues with linking
    This allows linking of this library into several different components
    without running into issues where it was statically linked into separate compilation units, leading to separate instances of translation-unit-
    local variables.
    
    Bug: 1350257
    Change-Id: I7ebe671879064e4529c3ebdf88104ea2fdef88a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678101
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168998}

--- components/startup_metric_utils/browser/startup_metric_utils.h
+++ components/startup_metric_utils/browser/startup_metric_utils.h
@@ -5,6 +5,7 @@
 #ifndef COMPONENTS_STARTUP_METRIC_UTILS_BROWSER_STARTUP_METRIC_UTILS_H_
 #define COMPONENTS_STARTUP_METRIC_UTILS_BROWSER_STARTUP_METRIC_UTILS_H_
 
+#include "base/component_export.h"
 #include "base/time/time.h"
 
 // Utility functions to support metric collection for browser startup. Timings
@@ -17,11 +18,11 @@ namespace startup_metric_utils {
 
 // Resets this process's session to allow recording one-time-only metrics again
 // when a process is reused for multiple tests.
-void ResetSessionForTesting();
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS) void ResetSessionForTesting();
 
 // Returns true when browser UI was not launched normally: some other UI was
 // shown first or browser was launched in background mode.
-bool WasMainWindowStartupInterrupted();
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS) bool WasMainWindowStartupInterrupted();
 
 // Call this when displaying UI that might potentially delay startup events.
 //
@@ -29,45 +30,53 @@ bool WasMainWindowStartupInterrupted();
 // in comparison with UI display that it's OK to call it on every
 // UI invocation regardless of whether the browser window has already
 // been displayed or not.
-void SetNonBrowserUIDisplayed();
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS) void SetNonBrowserUIDisplayed();
 
 // Call this when background mode gets enabled, as it might delay startup
 // events.
-void SetBackgroundModeEnabled();
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS) void SetBackgroundModeEnabled();
 
 // Call this with the creation time of the startup (initial/main) process.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordStartupProcessCreationTime(base::Time time);
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordStartupProcessCreationTime(base::TimeTicks ticks);
 
 // Call this with a time recorded as early as possible in the startup process.
 // On Android, the application start is the time at which the Java code starts.
 // On Windows, the application start is sampled from chrome.exe:main, before
 // chrome.dll is loaded.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordApplicationStartTime(base::TimeTicks ticks);
 
 // Call this with the time when the executable is loaded and the ChromeMain()
 // function is invoked.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordChromeMainEntryTime(base::TimeTicks ticks);
 
 // Call this with the time recorded just before the message loop is started.
 // Must be called after RecordApplicationStartTime(), because it computes time
 // deltas based on application start time.
 // |is_first_run| - is the current launch part of a first run.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordBrowserMainMessageLoopStart(base::TimeTicks ticks,
                                        bool is_first_run);
 
 // Call this with the time recorded just after the message loop first reaches
 // idle. Must be called after RecordApplicationStartTime(), because it computes
 // time deltas based on application start time.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordBrowserMainLoopFirstIdle(base::TimeTicks ticks);
 
 // Call this with the time when the first browser window became visible.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordBrowserWindowDisplay(base::TimeTicks ticks);
 
 // Call this with the time when the first web contents had a non-empty paint,
 // only if the first web contents was unimpeded in its attempt to do so. Must be
 // called after RecordApplicationStartTime(), because it computes time deltas
 // based on application start time.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordFirstWebContentsNonEmptyPaint(
     base::TimeTicks now,
     base::TimeTicks render_process_host_init_time);
@@ -76,18 +85,21 @@ void RecordFirstWebContentsNonEmptyPaint(
 // frame / successfully committed its navigation for the main frame. These
 // functions must be called after RecordApplicationStartTime(), because they
 // compute time deltas based on application start time.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordFirstWebContentsMainNavigationStart(base::TimeTicks ticks);
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordFirstWebContentsMainNavigationFinished(base::TimeTicks ticks);
 
 // Call this with the time when the Browser window painted its children for the
 // first time. Must be called after RecordApplicationStartTime(), because it
 // computes time deltas based on application start time.
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordBrowserWindowFirstPaint(base::TimeTicks ticks);
 
 // Returns the TimeTicks corresponding to main entry as recorded by
 // |RecordMainEntryPointTime|. Returns a null TimeTicks if a value has not been
 // recorded yet. This method is expected to be called from the UI thread.
-base::TimeTicks MainEntryPointTicks();
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS) base::TimeTicks MainEntryPointTicks();
 
 // Call this to record an arbitrary startup timing histogram with startup
 // temperature and a trace event. Records the time between `completion_ticks`
@@ -104,6 +116,7 @@ base::TimeTicks MainEntryPointTicks();
 // string literal) since the pointer will be stored for an indefinite amount of
 // time before being written to a trace (see the "Memory scoping note" in
 // base/trace_event/common/trace_event_common.h.)
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordExternalStartupMetric(const char* histogram_name,
                                  base::TimeTicks completion_ticks,
                                  bool set_non_browser_ui_displayed);
@@ -125,6 +138,7 @@ enum class FirstRunSentinelCreationResult {
   kFileSystemError = 3,
   kMaxValue = kFileSystemError,
 };
+COMPONENT_EXPORT(STARTUP_METRIC_UTILS)
 void RecordFirstRunSentinelCreation(FirstRunSentinelCreationResult result);
 
 }  // namespace startup_metric_utils
