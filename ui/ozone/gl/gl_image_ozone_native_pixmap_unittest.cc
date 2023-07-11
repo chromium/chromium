ab0b18e3fcd64c5376e08cb6a2838553d90dadc9
Colin Blundell <blundell@chromium.org>
2023-07-11 06:22:20 +0000

    [Ozone] Strip down GLImageNativePixmap unittest to what's actually run
    This unittest is carrying a lot of historical baggage. This CL strips it
    down to the test that's actually run:
    
    * We inline the test delegate into the test itself, since the
      indirection is no longer necessary
    * We hardcode the format and usage that are tested into
      CreateSolidColorImage()
    * We eliminate the test being parameterized, as it's only run with one
      concrete format/usage combination and there are no plans to extend
      that going forward (the test is quite old and not a source of
      active development)
    
    Bug: 1310018
    Change-Id: I5c9679b60cba0b73f22f19760a8937ad1f89af21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4660776
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168542}

--- ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
+++ ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
@@ -29,22 +29,27 @@
 #define MAYBE_Create Create
 #endif
 
-// TYPED_TEST_P() and REGISTER_TYPED_TEST_SUITE_P() don't do macro expansion on
-// their parameters, making the MAYBE_ technique above not work -- these macros
-// are a workaround.
-#define TYPED_TEST_P_WITH_EXPANSION(SuiteName, TestName) \
-  TYPED_TEST_P(SuiteName, TestName)
-#define REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION(SuiteName, ...) \
-  REGISTER_TYPED_TEST_SUITE_P(SuiteName, __VA_ARGS__)
-
 namespace gl {
 namespace {
 
-const uint8_t kRed[] = {0xF0, 0x0, 0x0, 0xFF};
-const uint8_t kYellow[] = {0xF0, 0xFF, 0x00, 0xFF};
+bool SkipTest(GLDisplay* display) {
+  ui::GLOzone* gl_ozone = ui::OzonePlatform::GetInstance()
+                              ->GetSurfaceFactoryOzone()
+                              ->GetCurrentGLOzone();
+  if (!gl_ozone || !gl_ozone->CanImportNativePixmap()) {
+    LOG(WARNING) << "Skip test, ozone implementation can't import native "
+                 << "pixmaps";
+    return true;
+  }
+  return false;
+}
 
-template <typename GLImageTestDelegate>
 class GLImageTest : public testing::Test {
+ public:
+  GLImageTest() {
+    client_native_pixmap_factory_ = ui::CreateClientNativePixmapFactoryOzone();
+  }
+
  protected:
   // Overridden from testing::Test:
   void SetUp() override {
@@ -55,7 +60,9 @@ class GLImageTest : public testing::Test {
     context_->MakeCurrent(surface_.get());
   }
   void TearDown() override {
-    delegate_.WillTearDown();
+    if (texture_id_) {
+      glDeleteTextures(1, &texture_id_);
+    }
     context_->ReleaseCurrent(surface_.get());
     context_ = nullptr;
     surface_ = nullptr;
@@ -63,97 +70,18 @@ class GLImageTest : public testing::Test {
   }
 
  protected:
-  scoped_refptr<GLSurface> surface_;
-  scoped_refptr<GLContext> context_;
-  GLImageTestDelegate delegate_;
-  raw_ptr<GLDisplay> display_ = nullptr;
-};
-
-TYPED_TEST_SUITE_P(GLImageTest);
-
-TYPED_TEST_P_WITH_EXPANSION(GLImageTest, MAYBE_Create) {
-  if (this->delegate_.SkipTest(this->display_)) {
-    GTEST_SKIP() << "Skip because GL initialization failed";
-  }
-
-  // NOTE: On some drm devices (mediatek) the minimum width/height to add an fb
-  // for a bo must be 64, and YVU_420 in i915 requires at least 128 length.
-  const gfx::Size small_image_size(128, 128);
-  const gfx::Size large_image_size(512, 512);
-  const uint8_t* image_color = this->delegate_.GetImageColor();
-
-  // Create a small solid color green image of preferred format.
-  auto small_image_binding =
-      this->delegate_.CreateSolidColorImage(small_image_size, image_color);
-  ASSERT_TRUE(small_image_binding);
-
-  // Create a large solid color green image of preferred format.
-  auto large_image_binding =
-      this->delegate_.CreateSolidColorImage(large_image_size, image_color);
-  ASSERT_TRUE(large_image_binding);
-}
-
-// The GLImageTest test case verifies the behaviour that is expected from a
-// GLImage in order to be conformant.
-REGISTER_TYPED_TEST_SUITE_P_WITH_EXPANSION(GLImageTest, MAYBE_Create);
-
-template <gfx::BufferUsage usage, gfx::BufferFormat format>
-class GLImageNativePixmapTestDelegate {
- public:
-  GLImageNativePixmapTestDelegate() {
-    client_native_pixmap_factory_ = ui::CreateClientNativePixmapFactoryOzone();
-  }
-
-  GLImageNativePixmapTestDelegate(const GLImageNativePixmapTestDelegate&) =
-      delete;
-  GLImageNativePixmapTestDelegate& operator=(
-      const GLImageNativePixmapTestDelegate&) = delete;
-
-  ~GLImageNativePixmapTestDelegate() = default;
-
-  void WillTearDown() {
-    if (texture_id_) {
-      glDeleteTextures(1, &texture_id_);
-    }
-  }
-
-  bool SkipTest(GLDisplay* display) const {
-    ui::GLOzone* gl_ozone = ui::OzonePlatform::GetInstance()
-                                ->GetSurfaceFactoryOzone()
-                                ->GetCurrentGLOzone();
-    if (!gl_ozone || !gl_ozone->CanImportNativePixmap()) {
-      LOG(WARNING) << "Skip test, ozone implementation can't import native "
-                   << "pixmaps";
-      return true;
-    }
-    return false;
-  }
-
   std::unique_ptr<ui::NativePixmapGLBinding> CreateSolidColorImage(
-      const gfx::Size& size,
-      const uint8_t color[4]) {
+      const gfx::Size& size) {
+    auto kUsage = gfx::BufferUsage::SCANOUT;
+    auto kFormat = gfx::BufferFormat::BGRA_8888;
+
     ui::SurfaceFactoryOzone* surface_factory =
         ui::OzonePlatform::GetInstance()->GetSurfaceFactoryOzone();
     scoped_refptr<gfx::NativePixmap> pixmap =
         surface_factory->CreateNativePixmap(gfx::kNullAcceleratedWidget,
-                                            nullptr, size, format, usage);
-    DCHECK(pixmap) << "Offending format: " << gfx::BufferFormatToString(format);
-    if (usage == gfx::BufferUsage::GPU_READ_CPU_READ_WRITE ||
-        usage == gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE ||
-        usage == gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE) {
-      auto client_pixmap = client_native_pixmap_factory_->ImportFromHandle(
-          pixmap->ExportHandle(), size, format, usage);
-      bool mapped = client_pixmap->Map();
-      EXPECT_TRUE(mapped);
-
-      for (size_t plane = 0; plane < pixmap->GetNumberOfPlanes(); ++plane) {
-        void* data = client_pixmap->GetMemoryAddress(plane);
-        GLTestSupport::SetBufferDataToColor(
-            size.width(), size.height(), pixmap->GetDmaBufPitch(plane), plane,
-            pixmap->GetBufferFormat(), color, static_cast<uint8_t*>(data));
-      }
-      client_pixmap->Unmap();
-    }
+                                            nullptr, size, kFormat, kUsage);
+    DCHECK(pixmap) << "Offending format: "
+                   << gfx::BufferFormatToString(kFormat);
 
     // Create a dummy texture ID to bind - these tests don't actually care about
     // binding.
@@ -161,58 +89,44 @@ class GLImageNativePixmapTestDelegate {
       glGenTextures(1, &texture_id_);
     }
 
-    auto binding = ui::OzonePlatform::GetInstance()
-                       ->GetSurfaceFactoryOzone()
-                       ->GetCurrentGLOzone()
-                       ->ImportNativePixmap(std::move(pixmap), format,
-                                            gfx::BufferPlane::DEFAULT, size,
-                                            gfx::ColorSpace(),
-                                            GetTextureTarget(), texture_id_);
+    auto binding =
+        ui::OzonePlatform::GetInstance()
+            ->GetSurfaceFactoryOzone()
+            ->GetCurrentGLOzone()
+            ->ImportNativePixmap(
+                std::move(pixmap), kFormat, gfx::BufferPlane::DEFAULT, size,
+                gfx::ColorSpace(), GL_TEXTURE_EXTERNAL_OES, texture_id_);
     EXPECT_TRUE(binding);
-    return std::move(binding);
+    return binding;
   }
 
-  unsigned GetTextureTarget() const { return GL_TEXTURE_EXTERNAL_OES; }
-
-  const uint8_t* GetImageColor() const {
-    return format == gfx::BufferFormat::R_8 ? kRed : kYellow;
-  }
-
-  int GetAdmissibleError() const {
-    if (format == gfx::BufferFormat::YVU_420 ||
-        format == gfx::BufferFormat::YUV_420_BIPLANAR) {
-      return 1;
-    }
-    if (format == gfx::BufferFormat::P010) {
-      return 3;
-    }
-    return 0;
-  }
-
- private:
+  scoped_refptr<GLSurface> surface_;
+  scoped_refptr<GLContext> context_;
   GLuint texture_id_ = 0;
   std::unique_ptr<gfx::ClientNativePixmapFactory> client_native_pixmap_factory_;
+  raw_ptr<GLDisplay> display_ = nullptr;
 };
 
-using GLImageScanoutType = testing::Types<
-    GLImageNativePixmapTestDelegate<gfx::BufferUsage::SCANOUT,
-                                    gfx::BufferFormat::BGRA_8888>>;
+TEST_F(GLImageTest, MAYBE_Create) {
+  if (SkipTest(this->display_)) {
+    GTEST_SKIP() << "Skip because GL initialization failed";
+  }
 
-INSTANTIATE_TYPED_TEST_SUITE_P(GLImageNativePixmapScanoutBGRA,
-                               GLImageTest,
-                               GLImageScanoutType);
+  // NOTE: On some drm devices (mediatek) the minimum width/height to add an fb
+  // for a bo must be 64, and YVU_420 in i915 requires at least 128 length.
+  const gfx::Size small_image_size(128, 128);
+  const gfx::Size large_image_size(512, 512);
 
-using GLImageScanoutTypeDisabled = testing::Types<
-    GLImageNativePixmapTestDelegate<gfx::BufferUsage::SCANOUT,
-                                    gfx::BufferFormat::RGBA_1010102>,
-    GLImageNativePixmapTestDelegate<gfx::BufferUsage::SCANOUT,
-                                    gfx::BufferFormat::BGRA_1010102>>;
+  // Create a small solid color green image of preferred format. This must
+  // succeed in order for a GLImage to be conformant.
+  auto small_image = CreateSolidColorImage(small_image_size);
+  ASSERT_TRUE(small_image);
 
-// This test is disabled since we need mesa support for AB30 that is not
-// available on many boards yet.
-INSTANTIATE_TYPED_TEST_SUITE_P(DISABLED_GLImageNativePixmapScanoutRGBA,
-                               GLImageTest,
-                               GLImageScanoutTypeDisabled);
+  // Create a large solid color green image of preferred format. This must
+  // succeed in order for a GLImage to be conformant.
+  auto large_image = CreateSolidColorImage(large_image_size);
+  ASSERT_TRUE(large_image);
+}
 
 }  // namespace
 }  // namespace gl
