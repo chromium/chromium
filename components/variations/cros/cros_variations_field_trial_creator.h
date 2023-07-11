5c1e82b9f55ff820dddfadad4c520c2d75a32343
Miriam Zimmerman <mutexlox@google.com>
2023-07-11 18:37:37 +0000

    variations: Add CrOSVariationsFieldTrialCreator
    Add a subclass of VariationsFieldTrialCreatorBase specialized for use in
    evaluate_seed on CrOS.
    
    BUG=b:263975722
    TEST=Builds
    
    Change-Id: I68bcaabe6b5c620a70ad94b90bc19fc6647250bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676002
    Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168840}

---
+++ components/variations/cros/cros_variations_field_trial_creator.h
@@ -0,0 +1,45 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VARIATIONS_CROS_CROS_VARIATIONS_FIELD_TRIAL_CREATOR_H_
+#define COMPONENTS_VARIATIONS_CROS_CROS_VARIATIONS_FIELD_TRIAL_CREATOR_H_
+
+#include "components/variations/service/variations_field_trial_creator_base.h"
+
+namespace variations {
+
+class VariationsSeedStore;
+class VariationsServiceClient;
+
+// Used to set up field trials for early-boot ChromeOS experiments based on
+// stored variations seed data.
+class CrOSVariationsFieldTrialCreator : public VariationsFieldTrialCreatorBase {
+ public:
+  // Caller is responsible for ensuring that the VariationsServiceClient passed
+  // to the constructor stays valid for the lifetime of this object.
+  CrOSVariationsFieldTrialCreator(
+      VariationsServiceClient* client,
+      std::unique_ptr<VariationsSeedStore> seed_store);
+
+  CrOSVariationsFieldTrialCreator(const CrOSVariationsFieldTrialCreator&) =
+      delete;
+  CrOSVariationsFieldTrialCreator& operator=(
+      const CrOSVariationsFieldTrialCreator&) = delete;
+
+  ~CrOSVariationsFieldTrialCreator() override;
+
+  // We do not support overriding UI strings, so override with no-op
+  // implementations.
+  void OverrideCachedUIStrings() override {}
+  bool IsOverrideResourceMapEmpty() override;
+
+ protected:
+  // We do not support overriding UI strings, so override with no-op
+  // implementation.
+  void OverrideUIString(uint32_t hash, const std::u16string& str) override {}
+};
+
+}  // namespace variations
+
+#endif  // COMPONENTS_VARIATIONS_CROS_CROS_VARIATIONS_FIELD_TRIAL_CREATOR_H_
