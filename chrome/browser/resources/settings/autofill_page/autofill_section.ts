4db18cb339276b17eb6cce912ee2634aa94e7cb8
Dmitry Vykochko <vykochko@google.com>
2023-07-11 20:51:47 +0000

    Settings: Don't show cloud off  icon when the roll out feature is off.
    Namely the kSyncEnableContactInfoDataTypeInTransportMode feature.
    
    Bug: 1455978
    Change-Id: I1704fec7c372b83d4bd1f53dd78edc5d363ae3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4627073
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1168946}

--- chrome/browser/resources/settings/autofill_page/autofill_section.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_section.ts
@@ -223,9 +223,29 @@ export class SettingsAutofillSectionElement extends
   private isCloudOffVisible_(
       address: chrome.autofillPrivate.AddressEntry,
       accountInfo?: chrome.autofillPrivate.AccountInfo): boolean {
-    return address.metadata?.source !==
-        chrome.autofillPrivate.AddressSource.ACCOUNT &&
-        !!accountInfo && !accountInfo?.isSyncEnabledForAutofillProfiles;
+    if (address.metadata?.source ===
+        chrome.autofillPrivate.AddressSource.ACCOUNT) {
+      return false;
+    }
+
+    if (!accountInfo) {
+      return false;
+    }
+
+    if (accountInfo.isSyncEnabledForAutofillProfiles) {
+      return false;
+    }
+
+    if (!loadTimeData.getBoolean('autofillAccountProfileStorage') ||
+        !loadTimeData.getBoolean('syncEnableContactInfoDataType') ||
+        !loadTimeData.getBoolean(
+            'syncEnableContactInfoDataTypeInTransportMode')) {
+      return false;
+    }
+
+    // Local profile of a logged-in user with disabled address sync and
+    // enabled feature.
+    return true;
   }
 
   /**
