00e2278eaa69bc2a6da5ce4931040373f1684ede
Yuwei Huang <yuweih@chromium.org>
2023-07-11 01:36:06 +0000

    Introduce NamedMojoMessagePipeServer
    NamedMojoIpcServer was used by Chromoting to handle incoming IPCs via
    the named pipe, however, we have recently changed our multi-process
    architecture a bit, such that the server is run on one process, while
    mojo binding happens on another process. NamedMojoIpcServer does both
    endpoint connection and mojo binding, so it no longer suits our use
    case.
    
    This CL factors out a NamedMojoMessagePipeServer class from
    NamedMojoIpcServer, which only does the endpoint connection job. Rather
    than binding the message pipe to an implementation, this class just
    passes the mojo message pipe via a callback. Chromoting code will then
    pass the message pipe to and bind it on the right process.
    
    Bug: 1378803
    Change-Id: I566bee861e1ad33671a7bea1211e33c8fd482673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675098
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1168472}

--- components/named_mojo_ipc_server/named_mojo_ipc_server.cc
+++ components/named_mojo_ipc_server/named_mojo_ipc_server.cc
@@ -4,80 +4,35 @@
 
 #include "components/named_mojo_ipc_server/named_mojo_ipc_server.h"
 
-#include <stdint.h>
-
 #include <memory>
 #include <utility>
 
 #include "base/check.h"
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
-#include "base/location.h"
-#include "base/logging.h"
-#include "base/memory/weak_ptr.h"
-#include "base/notreached.h"
-#include "base/process/process.h"
-#include "base/task/sequenced_task_runner.h"
-#include "base/task/thread_pool.h"
-#include "base/threading/sequence_bound.h"
-#include "base/time/time.h"
-#include "build/build_config.h"
 #include "components/named_mojo_ipc_server/connection_info.h"
 #include "components/named_mojo_ipc_server/endpoint_options.h"
-#include "components/named_mojo_ipc_server/named_mojo_server_endpoint_connector.h"
-#include "mojo/core/embedder/embedder.h"
+#include "components/named_mojo_ipc_server/named_mojo_message_pipe_server.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
-#include "mojo/public/cpp/platform/platform_channel_endpoint.h"
-#include "mojo/public/cpp/system/invitation.h"
 #include "mojo/public/cpp/system/isolated_connection.h"
 #include "mojo/public/cpp/system/message_pipe.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
-
-#if BUILDFLAG(IS_WIN)
-#include <windows.h>
-
-#include "base/strings/stringprintf.h"
-#include "base/win/win_util.h"
-#endif  // BUILDFLAG(IS_WIN)
 
 namespace named_mojo_ipc_server {
-
-// Forwards callbacks from a NamedMojoServerEndpointConnector to a
-// NamedMojoIpcServerBase. This allows the server to create a SequenceBound
-// interface to post callbacks from the IO sequence to the main sequence.
-class NamedMojoIpcServerBase::DelegateProxy final
-    : public NamedMojoServerEndpointConnector::Delegate {
- public:
-  explicit DelegateProxy(base::WeakPtr<NamedMojoIpcServerBase> server)
-      : server_(server) {}
-  ~DelegateProxy() override = default;
-
-  void OnClientConnected(mojo::PlatformChannelEndpoint endpoint,
-                         std::unique_ptr<ConnectionInfo> info) override {
-    if (server_) {
-      server_->OnClientConnected(std::move(endpoint), std::move(info));
-    }
-  }
-
-  void OnServerEndpointCreated() override {
-    if (server_) {
-      server_->OnServerEndpointCreated();
-    }
-  }
-
- private:
-  base::WeakPtr<NamedMojoIpcServerBase> server_;
-};
 
 NamedMojoIpcServerBase::NamedMojoIpcServerBase(
     const EndpointOptions& options,
     base::RepeatingCallback<void*(std::unique_ptr<ConnectionInfo>)>
         impl_provider)
-    : options_(options), impl_provider_(impl_provider) {
-  CHECK(!options.server_name.empty());
-  io_sequence_ =
-      base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()});
-}
+    : message_pipe_server_(
+          options,
+          impl_provider.Then(base::BindRepeating([](void* impl) {
+            return NamedMojoMessagePipeServer::ValidationResult{
+                .is_valid = impl != nullptr, .context = impl};
+          })),
+          // Safe to use Unretained(), since `message_pipe_server_` is destroyed
+          // when NamedMojoIpcServerBase is being destroyed, which stops
+          // callbacks from being called.
+          base::BindRepeating(&NamedMojoIpcServerBase::OnMessagePipeReady,
+                              base::Unretained(this))) {}
 
 NamedMojoIpcServerBase::~NamedMojoIpcServerBase() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -86,27 +41,13 @@ NamedMojoIpcServerBase::~NamedMojoIpcServerBase() {
 void NamedMojoIpcServerBase::StartServer() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (server_started_) {
-    return;
-  }
-
-  endpoint_connector_ = NamedMojoServerEndpointConnector::Create(
-      io_sequence_, options_,
-      base::SequenceBound<DelegateProxy>(
-          base::SequencedTaskRunner::GetCurrentDefault(),
-          weak_factory_.GetWeakPtr()));
-  endpoint_connector_.AsyncCall(&NamedMojoServerEndpointConnector::Start);
-  server_started_ = true;
+  message_pipe_server_.StartServer();
 }
 
 void NamedMojoIpcServerBase::StopServer() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (!server_started_) {
-    return;
-  }
-  server_started_ = false;
-  endpoint_connector_.Reset();
+  message_pipe_server_.StopServer();
   UntrackAllMessagePipes();
   active_connections_.clear();
 }
@@ -126,69 +67,15 @@ void NamedMojoIpcServerBase::OnIpcDisconnected() {
   Close(current_receiver());
 }
 
-void NamedMojoIpcServerBase::OnClientConnected(
-    mojo::PlatformChannelEndpoint endpoint,
-    std::unique_ptr<ConnectionInfo> info) {
-  base::ProcessId peer_pid = info->pid;
-  void* impl = impl_provider_.Run(std::move(info));
-  if (!impl) {
-    LOG(ERROR) << "Process " << peer_pid
-               << " is not a trusted mojo endpoint. Connection refused.";
-    return;
-  }
-
-  bool is_isolated = !options_.message_pipe_id.has_value();
-
-  base::Process peer_process;
-  // A peer process is not needed to open a non-MojoIpcz isolated connection,
-  // and in fact some callers don't have the right ACL to open the peer process
-  // yet, so we only open the peer process if the connection is non-isolated, or
-  // MojoIpcz is enabled.
-  if (!is_isolated || mojo::core::IsMojoIpczEnabled()) {
-#if BUILDFLAG(IS_WIN)
-    // Open process with minimum permissions since the client process might have
-    // restricted its access with DACL.
-    peer_process = base::Process::OpenWithAccess(peer_pid, PROCESS_DUP_HANDLE);
-// Windows opens the process with a system call so we use PLOG to extract more
-// info. Other OSes (i.e. POSIX) don't do that.
-#define INVALID_PROCESS_LOG PLOG
-#else
-    peer_process = base::Process::Open(peer_pid);
-#define INVALID_PROCESS_LOG LOG
-#endif
-    if (!peer_process.IsValid()) {
-      // With MojoIpcz, connections can be made without a process handle to the
-      // client, as long as the client has a process handle to the server, so we
-      // don't return here.
-      INVALID_PROCESS_LOG(WARNING) << "Failed to open peer process";
-    }
-#undef INVALID_PROCESS_LOG
-  }
-
-  if (is_isolated) {
-    // Create isolated connection.
-    auto connection = std::make_unique<mojo::IsolatedConnection>();
-    mojo::ScopedMessagePipeHandle message_pipe =
-        connection->Connect(std::move(endpoint), std::move(peer_process));
-    mojo::ReceiverId receiver_id =
-        TrackMessagePipe(std::move(message_pipe), impl, peer_pid);
+void NamedMojoIpcServerBase::OnMessagePipeReady(
+    mojo::ScopedMessagePipeHandle message_pipe,
+    base::ProcessId peer_pid,
+    void* context,
+    std::unique_ptr<mojo::IsolatedConnection> connection) {
+  mojo::ReceiverId receiver_id =
+      TrackMessagePipe(std::move(message_pipe), context, peer_pid);
+  if (connection) {
     active_connections_[receiver_id] = std::move(connection);
-    return;
-  }
-
-  // Create non-isolated connection.
-  mojo::OutgoingInvitation invitation;
-  invitation.set_extra_flags(options_.extra_send_invitation_flags);
-  mojo::ScopedMessagePipeHandle message_pipe =
-      invitation.AttachMessagePipe(*options_.message_pipe_id);
-  mojo::OutgoingInvitation::Send(std::move(invitation), peer_process.Handle(),
-                                 std::move(endpoint));
-  TrackMessagePipe(std::move(message_pipe), impl, peer_pid);
-}
-
-void NamedMojoIpcServerBase::OnServerEndpointCreated() {
-  if (on_server_endpoint_created_callback_for_testing_) {
-    on_server_endpoint_created_callback_for_testing_.Run();
   }
 }
 
---
+++ components/named_mojo_ipc_server/named_mojo_message_pipe_server.h
@@ -0,0 +1,114 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_NAMED_MOJO_IPC_SERVER_NAMED_MOJO_MESSAGE_PIPE_SERVER_H_
+#define COMPONENTS_NAMED_MOJO_IPC_SERVER_NAMED_MOJO_MESSAGE_PIPE_SERVER_H_
+
+#include <memory>
+
+#include "base/functional/callback.h"
+#include "base/memory/raw_ptr.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "base/process/process_handle.h"
+#include "base/sequence_checker.h"
+#include "base/task/sequenced_task_runner.h"
+#include "base/threading/sequence_bound.h"
+#include "base/timer/timer.h"
+#include "components/named_mojo_ipc_server/endpoint_options.h"
+#include "components/named_mojo_ipc_server/named_mojo_server_endpoint_connector.h"
+#include "mojo/public/cpp/platform/named_platform_channel.h"
+#include "mojo/public/cpp/platform/platform_channel_endpoint.h"
+#include "mojo/public/cpp/system/message_pipe.h"
+
+namespace mojo {
+class IsolatedConnection;
+}
+
+namespace named_mojo_ipc_server {
+// A helper that uses a NamedPlatformChannel to send out mojo invitations and
+// maintains multiple concurrent mojo message pipes. It keeps one outgoing
+// invitation at a time and will send a new invitation whenever the previous one
+// has been accepted by the client. This is similar to NamedMojoIpcServer, but
+// it returns the mojo message pipe without binding it to an implementation.
+class NamedMojoMessagePipeServer {
+ public:
+  struct ValidationResult {
+    // The client connection will be dropped and OnMessagePipeReady() won't be
+    // called, if `is_valid` is false.
+    bool is_valid;
+    // The value will be passed to OnMessagePipeReady() as-is, if `is_valid` is
+    // true.
+    raw_ptr<void> context;
+  };
+
+  // Called when a client has just connected to the server. The implementation
+  // should check whether the connection is valid and return the
+  // ValidationResult.
+  using Validator = base::RepeatingCallback<ValidationResult(
+      std::unique_ptr<ConnectionInfo>)>;
+
+  // Called once the message pipe is open. This happens after Validator is
+  // called, with `is_valid` set to true in ValidationResult.
+  // Note that IsolatedConnection is nullable. If it is non-null, the
+  // implementation should store it to keep the connection alive, otherwise, the
+  // implementation can ignore it.
+  using OnMessagePipeReady =
+      base::RepeatingCallback<void(mojo::ScopedMessagePipeHandle,
+                                   base::ProcessId,
+                                   /* context= */ void*,
+                                   std::unique_ptr<mojo::IsolatedConnection>)>;
+
+  // Internal use only.
+  struct PendingConnection;
+
+  // Callbacks won't be called once the instance is destroyed.
+  NamedMojoMessagePipeServer(const EndpointOptions& options,
+                             Validator validator,
+                             OnMessagePipeReady on_message_pipe_ready);
+
+  ~NamedMojoMessagePipeServer();
+
+  void StartServer();
+  void StopServer();
+
+  // Sets a callback to be run when an invitation is sent. Used by unit tests
+  // only.
+  void set_on_server_endpoint_created_callback_for_testing(
+      const base::RepeatingClosure& callback) {
+    on_server_endpoint_created_callback_for_testing_ = callback;
+  }
+
+ private:
+  class DelegateProxy;
+
+  void OnEndpointConnectorStarted(
+      base::SequenceBound<NamedMojoServerEndpointConnector> endpoint_connector);
+  void OnClientConnected(mojo::PlatformChannelEndpoint endpoint,
+                         std::unique_ptr<ConnectionInfo> info);
+  void OnServerEndpointCreated();
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  EndpointOptions options_;
+  Validator validator_;
+  OnMessagePipeReady on_message_pipe_ready_;
+
+  bool server_started_ = false;
+
+  // A task runner to run blocking jobs.
+  scoped_refptr<base::SequencedTaskRunner> io_sequence_;
+
+  base::SequenceBound<NamedMojoServerEndpointConnector> endpoint_connector_;
+
+  base::OneShotTimer restart_endpoint_timer_;
+
+  base::RepeatingClosure on_server_endpoint_created_callback_for_testing_;
+
+  base::WeakPtrFactory<NamedMojoMessagePipeServer> weak_factory_{this};
+};
+
+}  // namespace named_mojo_ipc_server
+
+#endif  // COMPONENTS_NAMED_MOJO_IPC_SERVER_NAMED_MOJO_MESSAGE_PIPE_SERVER_H_
