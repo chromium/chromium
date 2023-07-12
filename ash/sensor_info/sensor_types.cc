740572bb1839c85654d4ea4ae94e441db83fb98f
Catherine Zhang <cgzhang@google.com>
2023-07-12 01:35:58 +0000

    Add SensorProvider class
    SensorProvider can get lid_angle, accelerometer, gyroscope devices in a chromeOS device and return sample updates to observers.
    
    Bug: b/265357935
    Change-Id: Ia73426c16b51b8448a37b26e5117ca2cfc41c0e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4483335
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Catherine Zhang <cgzhang@google.com>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169078}

--- ash/sensor_info/sensor_types.cc
+++ ash/sensor_info/sensor_types.cc
@@ -32,8 +32,9 @@ SensorUpdate& SensorUpdate::operator=(const SensorUpdate& update) = default;
 SensorUpdate::~SensorUpdate() = default;
 
 std::vector<float> SensorUpdate::GetReadingAsVector(SensorType source) const {
-  const absl::optional<SensorReading>& reading = data_[source];
-  if (source == SensorType::LID_ANGLE) {
+  const absl::optional<SensorReading>& reading =
+      data_[static_cast<int>(source)];
+  if (source == SensorType::kLidAngle) {
     return reading.has_value() ? std::vector<float>{reading->x}
                                : std::vector<float>{0.0};
   }
@@ -43,13 +44,13 @@ std::vector<float> SensorUpdate::GetReadingAsVector(SensorType source) const {
 }
 
 void SensorUpdate::Set(SensorType source, float x, float y, float z) {
-  CHECK_NE(source, SensorType::LID_ANGLE);
-  data_[source] = SensorReading(x, y, z);
+  CHECK_NE(source, SensorType::kLidAngle);
+  data_[static_cast<int>(source)] = SensorReading(x, y, z);
 }
 
 void SensorUpdate::Set(SensorType source, float x) {
-  DCHECK_EQ(source, SensorType::LID_ANGLE);
-  data_[source] = SensorReading(x);
+  DCHECK_EQ(source, SensorType::kLidAngle);
+  data_[static_cast<int>(source)] = SensorReading(x);
 }
 
 void SensorUpdate::Reset() {
