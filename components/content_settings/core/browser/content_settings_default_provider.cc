0b37c4a6f255e0d34ceb3c8afb7feee98949e780
Jeffrey Kardatzke <jkardatzke@google.com>
2023-07-12 19:23:34 +0000

    content_settings: Remove migration logic for kEnableDrm
    Cleaning up old migration logic that was allowed to be removed in M100.
    
    BUG=1191642
    TEST=CQ
    
    Change-Id: I711bcd40d7d7b32e888d3cc78be17e96f0ce49f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4583388
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Auto-Submit: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1169498}

--- components/content_settings/core/browser/content_settings_default_provider.cc
+++ components/content_settings/core/browser/content_settings_default_provider.cc
@@ -57,11 +57,6 @@ const char kObsoletePpapiBrokerDefaultPref[] =
 #endif  // !BUILDFLAG(IS_ANDROID)
 #endif  // !BUILDFLAG(IS_IOS)
 
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-// This setting was moved and should be migrated on profile startup.
-const char kDeprecatedEnableDRM[] = "settings.privacy.drm_enabled";
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-
 ContentSetting GetDefaultValue(const WebsiteSettingsInfo* info) {
   const base::Value& initial_default = info->initial_default_value();
   if (initial_default.is_none())
@@ -138,10 +133,6 @@ void DefaultProvider::RegisterProfilePrefs(
   registry->RegisterIntegerPref(kObsoletePpapiBrokerDefaultPref, 0);
 #endif  // !BUILDFLAG(IS_ANDROID)
 #endif  // !BUILDFLAG(IS_IOS)
-
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-  registry->RegisterBooleanPref(kDeprecatedEnableDRM, true);
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
 }
 
 DefaultProvider::DefaultProvider(PrefService* prefs,
@@ -344,22 +335,6 @@ void DefaultProvider::DiscardOrMigrateObsoletePreferences() {
   prefs_->ClearPref(kObsoletePpapiBrokerDefaultPref);
 #endif  // !BUILDFLAG(IS_ANDROID)
 #endif  // !BUILDFLAG(IS_IOS)
-
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
-  // TODO(crbug.com/1191642): Remove this migration logic in M100.
-  WebsiteSettingsRegistry* website_settings =
-      WebsiteSettingsRegistry::GetInstance();
-  const base::Value* deprecated_enable_drm_value =
-      prefs_->GetUserPrefValue(kDeprecatedEnableDRM);
-  if (deprecated_enable_drm_value) {
-    prefs_->SetInteger(
-        website_settings->Get(ContentSettingsType::PROTECTED_MEDIA_IDENTIFIER)
-            ->default_value_pref_name(),
-        deprecated_enable_drm_value->GetBool() ? CONTENT_SETTING_ALLOW
-                                               : CONTENT_SETTING_BLOCK);
-  }
-  prefs_->ClearPref(kDeprecatedEnableDRM);
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
 }
 
 void DefaultProvider::RecordHistogramMetrics() {
