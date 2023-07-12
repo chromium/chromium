d9112555e291d0bf358fa24264b138549a33a759
Andrés Olivares <andoli@chromium.org>
2023-07-12 07:57:19 +0000

    Use unique trace ids for console timings with different names
    Console timing events can be used to measure the duration of a task in
    JS and the recorded data is added to the performance panel timeline data
    via trace events. A console.time('name') event dispatches a trace begin
    event and a console.end('name') dispatches a trace end event.
    
    Before this cl, the trace id used to dispatch console timing events was
    a pointer to the process and, as a consequence, was shared within
    console timing events in the same process, regardless of the console
    timing name. This was problematic because the trace id is used to match
    trace begin and end events. Meaning that the trace end event dispatched for a given console.end will always be matched with the latest trace begin event that was dispatched. However, there is no guarantee that a console.end call is intended to end the last console.time call. Instead they should be matched by name, which this CL achieves by using the timing name the API was called with in the id of the trace event.
    
    Co-authored-by: jacktfranklin@chromium.org
    Fixed: 1455582
    
    Change-Id: I3a4d463254e4137861e7065cb3e4ea9e11476ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4665408
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169185}

--- third_party/blink/renderer/core/inspector/thread_debugger_common_impl.cc
+++ third_party/blink/renderer/core/inspector/thread_debugger_common_impl.cc
@@ -947,7 +947,9 @@ void ThreadDebuggerCommonImpl::consoleTime(
   // TODO(dgozman): we can save on a copy here if trace macro would take a
   // pointer with length.
   TRACE_EVENT_COPY_NESTABLE_ASYNC_BEGIN0(
-      "blink.console", ToCoreString(title).Utf8().c_str(), this);
+      "blink.console", ToCoreString(title).Utf8().c_str(),
+      TRACE_ID_WITH_SCOPE(ToCoreString(title).Utf8().c_str(),
+                          TRACE_ID_LOCAL(this)));
 }
 
 void ThreadDebuggerCommonImpl::consoleTimeEnd(
@@ -955,7 +957,9 @@ void ThreadDebuggerCommonImpl::consoleTimeEnd(
   // TODO(dgozman): we can save on a copy here if trace macro would take a
   // pointer with length.
   TRACE_EVENT_COPY_NESTABLE_ASYNC_END0(
-      "blink.console", ToCoreString(title).Utf8().c_str(), this);
+      "blink.console", ToCoreString(title).Utf8().c_str(),
+      TRACE_ID_WITH_SCOPE(ToCoreString(title).Utf8().c_str(),
+                          TRACE_ID_LOCAL(this)));
 }
 
 void ThreadDebuggerCommonImpl::consoleTimeStamp(
