bc0a38cba4bf78c5fd5f5f35aea21226fb241fa5
Dustin J. Mitchell <djmitche@chromium.org>
2023-07-12 16:12:24 +0000

    Improve reporting of errors running live sign-in tests
    This would have saved me a few minutes trying to figure out why these
    tests were failing.
    
    Change-Id: Icff320c5868bfe2d59d85164bb49b90bb00d1c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4678261
    Auto-Submit: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1169377}

--- chrome/browser/signin/e2e_tests/live_test.cc
+++ chrome/browser/signin/e2e_tests/live_test.cc
@@ -40,6 +40,9 @@ void LiveTest::SetUp() {
   base::PathService::Get(base::BasePathKey::DIR_SOURCE_ROOT, &root_path);
   base::FilePath config_path =
       base::MakeAbsoluteFilePath(root_path.Append(kTestAccountFilePath));
+  CHECK(!config_path.empty()) << kTestAccountFilePath
+                              << " does not exist. This file is only available "
+                                 "in Google-internal checkouts.";
   test_accounts_.Init(config_path);
   InProcessBrowserTest::SetUp();
 }
