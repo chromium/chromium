b89e55ded07a5a2ccff7f087d634fb089cfbe259
dpapad <dpapad@chromium.org>
2023-07-13 19:57:43 +0000

    WebUI: Migrate c/b/r/tab_search tests to WebUIMochaBrowserTest.
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Also doing minor renaming of tests, now that previous js2gtest
    constraint of having a dedicated C++ class for each test case does not
    exist and and removing the somewhat meaningless '*.All' naming pattern.
    Example renames:
     - TabSearchAppTest.All   -> TabSearchTest.App
     - TabSearchBiMapTest.All -> TabSearchTest.BiMap
    
    which also makes running such tests easier by specifying
    --gtest_filter=TabSearchTest.* instead of
    --gtest_filter=TabSearch*Test.All
    
    Bug: 1457360
    Change-Id: I6759290953aa9ec33ac0f1b9895041c19baa2ec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4682386
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170094}

---
+++ chrome/test/data/webui/tab_search/tab_search_browsertest.cc
@@ -0,0 +1,36 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+class TabSearchTest : public WebUIMochaBrowserTest {
+ protected:
+  TabSearchTest() { set_test_loader_host(chrome::kChromeUITabSearchHost); }
+};
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, App) {
+  RunTest("tab_search/tab_search_app_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, BiMap) {
+  RunTest("tab_search/bimap_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, FuzzySearch) {
+  RunTest("tab_search/fuzzy_search_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, InfiniteList) {
+  RunTest("tab_search/infinite_list_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, Item) {
+  RunTest("tab_search/tab_search_item_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(TabSearchTest, MediaTabs) {
+  RunTest("tab_search/tab_search_media_tabs_test.js", "mocha.run()");
+}
---
+++ chrome/test/data/webui/tab_search/tab_search_focus_test.cc
@@ -0,0 +1,23 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+class TabSearchFocusTest : public WebUIMochaBrowserTest {
+ protected:
+  TabSearchFocusTest() { set_test_loader_host(chrome::kChromeUITabSearchHost); }
+};
+
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_App DISABLED_App
+#else
+#define MAYBE_App App
+#endif
+IN_PROC_BROWSER_TEST_F(TabSearchFocusTest, MAYBE_App) {
+  RunTest("tab_search/tab_search_app_focus_test.js", "mocha.run()");
+}
