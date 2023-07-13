6a1a3d5205932e4c1c8312617855f6ed0d36cd61
Richard Chen <richche@chromium.org>
2023-07-13 17:57:21 +0000

    CRX Telemetry: declarativeNetRequest 100% Stable launch.
    - Enable feature by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1426891
    Change-Id: I1a8bc4bc8044e833d85726914288540cffffe907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4683468
    Auto-Submit: Richard Chen <richche@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170039}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -165,7 +165,7 @@ BASE_FEATURE(kExtensionTelemetryCookiesGetSignal,
 
 BASE_FEATURE(kExtensionTelemetryDeclarativeNetRequestSignal,
              "SafeBrowsingExtensionTelemetryDeclarativeNetRequestSignal",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFileTypePoliciesTag,
              "FileTypePoliciesTag",
