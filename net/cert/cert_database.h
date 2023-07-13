33285cb7a5c1363a1b8256860411b7fec4f9f178
Matt Mueller <mattm@chromium.org>
2023-07-13 16:12:44 +0000

    Merge to 116: Simplify cert_database_mac notifier implementation
    There can only be one, so don't have an overly complicated
    implementation that pretends otherwise.
    
    (cherry picked from commit 33d6b57a777b6a678fe0ca67008e88884fd2fa06)
    
    Bug: 1463342
    Change-Id: I95872be22a7609de23e43394685a46e628d4f3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676148
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1168427}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4682165
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5845@{#466}
    Cr-Branched-From: 5a5dff63a4a4c63b9b18589819bebb2566c85443-refs/heads/main@{#1160321}

--- net/cert/cert_database.h
+++ net/cert/cert_database.h
@@ -5,7 +5,6 @@
 #ifndef NET_CERT_CERT_DATABASE_H_
 #define NET_CERT_CERT_DATABASE_H_
 
-#include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/no_destructor.h"
 #include "build/build_config.h"
@@ -82,10 +81,9 @@ class NET_EXPORT CertDatabase {
   void RemoveObserver(Observer* observer);
 
 #if BUILDFLAG(IS_MAC)
-  // Start observing and forwarding events from Keychain services on the
-  // current thread. Current thread must have an associated CFRunLoop,
-  // which means that this must be called from a MessageLoop of TYPE_UI.
-  void StartListeningForKeychainEvents();
+  // Start observing and forwarding events from Keychain services. May be
+  // called multiple times, and may be called on any thread.
+  static void StartListeningForKeychainEvents();
 #endif
 
   // Synthetically injects notifications to all observers. In general, this
@@ -100,14 +98,6 @@ class NET_EXPORT CertDatabase {
   CertDatabase();
 
   const scoped_refptr<base::ObserverListThreadSafe<Observer>> observer_list_;
-
-#if BUILDFLAG(IS_MAC)
-  void ReleaseNotifier();
-
-  class Notifier;
-  friend class Notifier;
-  raw_ptr<Notifier, DanglingUntriaged> notifier_ = nullptr;
-#endif
 };
 
 }  // namespace net
