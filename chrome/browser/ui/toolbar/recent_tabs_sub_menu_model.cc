e369dfecc4026a606cf4e2f3a2b5c49b38860d13
Allen Bauer <kylixrd@chromium.org>
2023-07-13 14:55:18 +0000

    [CR2023] Renamed 'Recently Closed' to 'Recent Tabs'. Added separator to tab sub-menus.
    Updated history icon to a "cleaner" version.
    
    Change-Id: Ice1a03ee4604d1ae1b4c846a644ab3fa60b2a7dd
    Bug: 1456805,1463049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4679890
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1169929}

--- chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.cc
+++ chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.cc
@@ -349,13 +349,16 @@ void RecentTabsSubMenuModel::BuildLocalEntries() {
   if (!service || service->entries().empty()) {
     // This is to show a disabled restore tab entry with the accelerator to
     // teach users about this command.
-    InsertItemWithStringIdAt(++last_local_model_index_,
-                             kDisabledRecentlyClosedHeaderCommandId,
-                             IDS_RECENTLY_CLOSED);
+    InsertItemWithStringIdAt(
+        ++last_local_model_index_, kDisabledRecentlyClosedHeaderCommandId,
+        features::IsChromeRefresh2023() ? IDS_RECENT_TABS
+                                        : IDS_RECENTLY_CLOSED);
   } else {
     recently_closed_title_index_ = ++last_local_model_index_;
     InsertTitleWithStringIdAt(recently_closed_title_index_.value(),
-                              IDS_RECENTLY_CLOSED);
+                              features::IsChromeRefresh2023()
+                                  ? IDS_RECENT_TABS
+                                  : IDS_RECENTLY_CLOSED);
     if (!features::IsChromeRefresh2023()) {
       SetIcon(last_local_model_index_, CreateFavicon(kTabIcon));
     }
@@ -586,6 +589,9 @@ RecentTabsSubMenuModel::CreateWindowSubMenuModel(
       restore_all_command_id, IDS_RESTORE_WINDOW,
       ui::ImageModel::FromVectorIcon(vector_icons::kLaunchIcon));
   local_window_items_.emplace(restore_all_command_id, window.id);
+  if (features::IsChromeRefresh2023()) {
+    window_model->AddSeparator(ui::NORMAL_SEPARATOR);
+  }
 
   absl::optional<tab_groups::TabGroupId> last_group;
   tab_groups::TabGroupVisualData current_group_visual_data;
