35c8571d119190272d734773ee5ec128ac7f50e9
Fredrik Hernqvist <fhernqvist@google.com>
2023-07-14 08:01:55 +0000

    Add typified tracing for audio output on Linux
    This is intended to track down some issues with weird delay values.
    
    This will be submitted once the corresponding internal CL has been
    submitted, but the internal CL can only be submitted once this CL is
    approved.
    
    Sample Alsa trace: https://ui.perfetto.dev/#!/?s=3c77451e627e8cbb480f8ed9afa74b0a1f43c3f6df39f4e40e913c20de20da7e
    
    Sample Pulse trace: https://ui.perfetto.dev/#!/?s=44dfc4c623627e03807406ad0daab7f503b965a874591514e1127d9c0aa0911b
    
    Bug: 1459263
    Change-Id: Iec6ed8a509ca4585d6cafa176d344373e41d978f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4681384
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1170386}

--- media/audio/alsa/alsa_output.cc
+++ media/audio/alsa/alsa_output.cc
@@ -46,7 +46,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/default_tick_clock.h"
-#include "base/trace_event/trace_event.h"
+#include "base/trace_event/typed_macros.h"
 #include "media/audio/alsa/alsa_util.h"
 #include "media/audio/alsa/alsa_wrapper.h"
 #include "media/audio/alsa/audio_manager_alsa.h"
@@ -362,6 +362,14 @@ void AlsaPcmOutputStream::SetTickClockForTesting(
 
 void AlsaPcmOutputStream::BufferPacket(bool* source_exhausted) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  TRACE_EVENT("audio", "AlsaPcmOutputStream::BufferPacket",
+              [&](perfetto::EventContext ctx) {
+                auto* event =
+                    ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+                auto* data = event->set_linux_alsa_output();
+                data->set_forward_bytes(buffer_->forward_bytes());
+                data->set_sample_rate(sample_rate_);
+              });
 
   // If stopped, simulate a 0-length packet.
   if (stop_stream_) {
@@ -436,6 +444,7 @@ void AlsaPcmOutputStream::BufferPacket(bool* source_exhausted) {
 
 void AlsaPcmOutputStream::WritePacket() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  TRACE_EVENT("audio", "AlsaPcmOutputStream::WritePacket");
 
   // If the device is in error, just eat the bytes.
   if (stop_stream_) {
@@ -495,6 +504,7 @@ void AlsaPcmOutputStream::WritePacket() {
 
 void AlsaPcmOutputStream::WriteTask() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  TRACE_EVENT("audio", "AlsaPcmOutputStream::WriteTask");
 
   if (stop_stream_)
     return;
@@ -600,6 +610,12 @@ std::string AlsaPcmOutputStream::FindDeviceForChannels(uint32_t channels) {
 }
 
 snd_pcm_sframes_t AlsaPcmOutputStream::GetCurrentDelay() {
+  TRACE_EVENT_BEGIN(TRACE_DISABLED_BY_DEFAULT("audio"),
+                    "AlsaPcmOutputStream::GetCurrentDelay");
+  // Intermediate values saved for tracing.
+  absl::optional<snd_pcm_sframes_t> pcm_delay;
+  absl::optional<snd_pcm_sframes_t> available_frames;
+
   snd_pcm_sframes_t delay = -1;
   // Don't query ALSA's delay if we have underrun since it'll be jammed at some
   // non-zero value and potentially even negative!
@@ -620,6 +636,7 @@ snd_pcm_sframes_t AlsaPcmOutputStream::GetCurrentDelay() {
         LOG(ERROR) << "Failed querying delay: " << wrapper_->StrError(error);
       }
     }
+    pcm_delay = delay;
   }
 
   // snd_pcm_delay() sometimes returns crazy values.  In this case return delay
@@ -629,13 +646,26 @@ snd_pcm_sframes_t AlsaPcmOutputStream::GetCurrentDelay() {
   // clip if delay is truly crazy (> 10x expected).
   if (delay < 0 ||
       static_cast<snd_pcm_uframes_t>(delay) > alsa_buffer_frames_ * 10) {
-    delay = alsa_buffer_frames_ - GetAvailableFrames();
+    available_frames = GetAvailableFrames();
+    delay = alsa_buffer_frames_ - *available_frames;
   }
 
   if (delay < 0) {
     delay = 0;
   }
-
+  TRACE_EVENT_END(
+      TRACE_DISABLED_BY_DEFAULT("audio"), [&](perfetto::EventContext ctx) {
+        auto* event = ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+        auto* data = event->set_linux_alsa_output();
+        data->set_getcurrentdelay_alsa_buffer_frames(alsa_buffer_frames_);
+        data->set_getcurrentdelay_final_delay_frames(delay);
+        if (pcm_delay) {
+          data->set_getcurrentdelay_pcm_delay_frames(*pcm_delay);
+        }
+        if (available_frames) {
+          data->set_getcurrentdelay_available_frames(*available_frames);
+        }
+      });
   return delay;
 }
 
@@ -789,7 +819,13 @@ AlsaPcmOutputStream::InternalState AlsaPcmOutputStream::state() {
 int AlsaPcmOutputStream::RunDataCallback(base::TimeDelta delay,
                                          base::TimeTicks delay_timestamp,
                                          AudioBus* audio_bus) {
-  TRACE_EVENT0("audio", "AlsaPcmOutputStream::RunDataCallback");
+  TRACE_EVENT(
+      "audio", "AlsaPcmOutputStream::RunDataCallback",
+      [&](perfetto::EventContext ctx) {
+        auto* event = ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+        auto* data = event->set_linux_alsa_output();
+        data->set_source_request_playout_delay_us(delay.InMicroseconds());
+      });
 
   if (source_callback_) {
     UMA_HISTOGRAM_COUNTS_1000("Media.Audio.Render.SystemDelay",
--- media/audio/pulse/pulse_output.cc
+++ media/audio/pulse/pulse_output.cc
@@ -12,6 +12,7 @@
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "base/trace_event/typed_macros.h"
 #include "media/audio/audio_device_description.h"
 #include "media/audio/audio_manager_base.h"
 #include "media/audio/pulse/pulse_util.h"
@@ -157,6 +158,14 @@ void PulseAudioOutputStream::SendLogMessage(const char* format, ...) {
 }
 
 void PulseAudioOutputStream::FulfillWriteRequest(size_t requested_bytes) {
+  TRACE_EVENT("audio", "PulseAudioOutputStream::FulfillWriteRequest",
+              [&](perfetto::EventContext ctx) {
+                auto* event =
+                    ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+                auto* data = event->set_linux_pulse_output();
+                data->set_stream_request_bytes(requested_bytes);
+                data->set_sample_rate(params_.sample_rate());
+              });
   int bytes_remaining = requested_bytes;
   while (bytes_remaining > 0) {
     void* pa_buffer = nullptr;
@@ -172,9 +181,17 @@ void PulseAudioOutputStream::FulfillWriteRequest(size_t requested_bytes) {
     }
 
     size_t unwritten_frames_in_bus = audio_bus_->frames();
+    size_t frame_size = buffer_size_ / unwritten_frames_in_bus;
     const base::TimeDelta delay = pulse::GetHardwareLatency(pa_stream_);
     UMA_HISTOGRAM_COUNTS_1000("Media.Audio.Render.SystemDelay",
                               delay.InMilliseconds());
+    TRACE_EVENT("audio", "source request", [&](perfetto::EventContext ctx) {
+      auto* event = ctx.event<perfetto::protos::pbzero::ChromeTrackEvent>();
+      auto* data = event->set_linux_pulse_output();
+      data->set_source_request_playout_delay_us(delay.InMicroseconds());
+      data->set_input_buffer_size_frames(params_.frames_per_buffer());
+      data->set_frame_size_bytes(frame_size);
+    });
     size_t frames_filled = source_callback_->OnMoreData(
         BoundedDelay(delay), base::TimeTicks::Now(), {}, audio_bus_.get());
 
@@ -191,7 +208,6 @@ void PulseAudioOutputStream::FulfillWriteRequest(size_t requested_bytes) {
 
     audio_bus_->Scale(volume_);
 
-    size_t frame_size = buffer_size_ / unwritten_frames_in_bus;
     size_t frames_to_copy = pa_buffer_size / frame_size;
     size_t frame_offset_in_bus = 0;
     do {
