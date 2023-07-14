8ad61f9c781e87d3e82ca32dccc3d1ec3164b2f5
Tim Sergeant <tsergeant@chromium.org>
2023-07-14 02:18:40 +0000

    App Service: Add comment about replacement to AppPublisher::LoadIcon
    In most cases, App Service running on Ash will not call LoadIcon, and
    will instead call GetCompressedIconData. This adds a comment clarifying
    this to the documentation for LoadIcon.
    
    Note that LoadIcon _is_ still used on non-Ash platforms, which means
    we're maintaining two separate icon pipelines, so we still need to
    revisit this and figure out how to actually remove or simplify LoadIcon.
    
    Bug: 1380608
    Change-Id: Iefebde2d18332c6a22574c2f396b5ea6c4450e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680053
    Reviewed-by: Emil Mikulic <easy@google.com>
    Commit-Queue: Emil Mikulic <easy@google.com>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1170300}

--- chrome/browser/apps/app_service/publishers/app_publisher.h
+++ chrome/browser/apps/app_service/publishers/app_publisher.h
@@ -76,6 +76,10 @@ class AppPublisher {
   //    is true and when no icon is available for the app (or an icon for the
   //    app cannot be efficiently provided). Otherwise, a null icon should be
   //    returned.
+  //
+  // NOTE: On Ash, App Service will not call this method, and instead will call
+  // `GetCompressedIconData()` to load raw icon data from the Publisher.
+  // TODO(crbug.com/1380608): Clean up/simplify remaining usages of LoadIcon.
   virtual void LoadIcon(const std::string& app_id,
                         const IconKey& icon_key,
                         apps::IconType icon_type,
