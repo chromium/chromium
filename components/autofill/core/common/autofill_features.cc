66d0d632e022a86060ad9e1e8d0836dfa13c8819
Christoph Schwering <schwering@google.com>
2023-07-14 13:55:07 +0000

    Revert "[Autofill] Enable AutofillSplitCreditCardNumbersCautiously by default"
    This reverts commit 5ea7c542be6494b7d45a4aa81e2a6610ac0602e0.
    
    Reason for revert: special 10% + 50% stages are necessary due
    to WebView.
    
    Original change's description:
    > [Autofill] Enable AutofillSplitCreditCardNumbersCautiously by default
    >
    > Bug: 1419578
    > Change-Id: I14c04874a515a5852a219bae7e16f5dab6c7dfc7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4672930
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Dominic Battre <battre@chromium.org>
    > Commit-Queue: Christoph Schwering <schwering@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1170434}
    
    Bug: 1419578
    Change-Id: I9c03f03fc47f4a25da1fb10b6bbe8fd477640b8b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4686055
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1170488}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -526,7 +526,7 @@ BASE_FEATURE(kAutofillSkipComparingInferredLabels,
 // TODO(crbug.com/1419578): Remove when launched.
 BASE_FEATURE(kAutofillSplitCreditCardNumbersCautiously,
              "AutofillSplitCreditCardNumbersCautiously",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Controls whether Autofill should search prefixes of all words/tokens when
 // filtering profiles, or only on prefixes of the whole string.
