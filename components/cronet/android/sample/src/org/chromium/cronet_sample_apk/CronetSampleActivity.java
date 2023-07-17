bd46fa7fa428a9b732da1899f1d1c90ae8805833
Mohannad Farrag <aymanm@google.com>
2023-07-17 17:10:13 +0000

    Revamp CronetSample App
    * This is a change towards improving the Cronet sample app to improve the debugging process. Cronet supports a lot of options that are hard to debug so this app will provide an interface to enable/disable options and change the logic of how Cronet work to facilitate debugging.
    * Added two different fragments:
      * First fragment is responsible for the interaction with Cronet Engine, sending a request and restarting the engine.
      * Second fragment is responsible for enabling/disabling options.
    * Moved initialization of CronetEngine to the application-level.
    
    Change-Id: I7c9df35e1c761ef1669d6bab29305447f5eab666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675872
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171238}

--- components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleActivity.java
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleActivity.java
@@ -4,197 +4,90 @@
 
 package org.chromium.cronet_sample_apk;
 
-import android.app.Activity;
-import android.content.DialogInterface;
+import static org.chromium.cronet_sample_apk.SampleActivityViewModel.FRAGMENT_ID_FLAGS;
+import static org.chromium.cronet_sample_apk.SampleActivityViewModel.FRAGMENT_ID_HOME;
+
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
-import org.chromium.net.CronetEngine;
-import org.chromium.net.CronetException;
-import org.chromium.net.UploadDataProviders;
-import org.chromium.net.UrlRequest;
-import org.chromium.net.UrlResponseInfo;
+import org.chromium.base.ApiCompatibilityUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.WritableByteChannel;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Activity for managing the Cronet Sample.
  */
-public class CronetSampleActivity extends Activity {
+public class CronetSampleActivity extends FragmentActivity {
     private static final String TAG = CronetSampleActivity.class.getSimpleName();
 
-    private CronetEngine mCronetEngine;
+    private final Map<Integer, Integer> mFragmentIdMap = new HashMap<>();
 
-    private String mUrl;
-    private TextView mResultText;
-    private TextView mReceiveDataText;
+    private LinearLayout mBottomNav;
+    private SampleActivityViewModel mActivityViewModel;
 
-    class SimpleUrlRequestCallback extends UrlRequest.Callback {
-        private ByteArrayOutputStream mBytesReceived = new ByteArrayOutputStream();
-        private WritableByteChannel mReceiveChannel = Channels.newChannel(mBytesReceived);
-
-        @Override
-        public void onRedirectReceived(
-                UrlRequest request, UrlResponseInfo info, String newLocationUrl) {
-            Log.i(TAG, "****** onRedirectReceived ******");
-            request.followRedirect();
+    private void init() {
+        setContentView(R.layout.main_activity);
+        // Set up bottom navigation bar:
+        mBottomNav = findViewById(R.id.nav_view);
+        mFragmentIdMap.put(R.id.navigation_home, FRAGMENT_ID_HOME);
+        mFragmentIdMap.put(R.id.navigation_options_ui, FRAGMENT_ID_FLAGS);
+        final int childCount = mBottomNav.getChildCount();
+        View.OnClickListener switchFragmentListener = view -> {
+            assert mFragmentIdMap.containsKey(view.getId());
+            int fragmentId = mFragmentIdMap.get(view.getId());
+            switchFragment(fragmentId);
+        };
+        for (int i = 0; i < childCount; i++) {
+            mBottomNav.getChildAt(i).setOnClickListener(switchFragmentListener);
         }
-
-        @Override
-        public void onResponseStarted(UrlRequest request, UrlResponseInfo info) {
-            Log.i(TAG, "****** Response Started ******");
-            Log.i(TAG, "*** Headers Are *** " + info.getAllHeaders());
-
-            request.read(ByteBuffer.allocateDirect(32 * 1024));
-        }
-
-        @Override
-        public void onReadCompleted(
-                UrlRequest request, UrlResponseInfo info, ByteBuffer byteBuffer) {
-            byteBuffer.flip();
-            Log.i(TAG, "****** onReadCompleted ******" + byteBuffer);
-
-            try {
-                mReceiveChannel.write(byteBuffer);
-            } catch (IOException e) {
-                Log.i(TAG, "IOException during ByteBuffer read. Details: ", e);
-            }
-            byteBuffer.clear();
-            request.read(byteBuffer);
-        }
-
-        @Override
-        public void onSucceeded(UrlRequest request, UrlResponseInfo info) {
-            Log.i(TAG, "****** Request Completed, status code is " + info.getHttpStatusCode()
-                            + ", total received bytes is " + info.getReceivedByteCount());
-
-            final String receivedData = mBytesReceived.toString();
-            final String url = info.getUrl();
-            final String text = "Completed " + url + " (" + info.getHttpStatusCode() + ")";
-            CronetSampleActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mResultText.setText(text);
-                    mReceiveDataText.setText(receivedData);
-                    promptForURL(url);
-                }
-            });
-        }
-
-        @Override
-        public void onFailed(UrlRequest request, UrlResponseInfo info, CronetException error) {
-            Log.i(TAG, "****** onFailed, error is: " + error.getMessage());
-
-            final String url = mUrl;
-            final String text = "Failed " + mUrl + " (" + error.getMessage() + ")";
-            CronetSampleActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mResultText.setText(text);
-                    promptForURL(url);
-                }
-            });
-        }
+        mActivityViewModel = new ViewModelProvider(this).get(SampleActivityViewModel.class);
     }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mResultText = (TextView) findViewById(R.id.resultView);
-        mReceiveDataText = (TextView) findViewById(R.id.dataView);
-        mReceiveDataText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                promptForURL(mUrl);
+        init();
+        switchFragment(FRAGMENT_ID_HOME);
+    }
+
+    private void updateNavigationBarUI(int chosenFragmentId) {
+        final int childCount = mBottomNav.getChildCount();
+        for (int i = 0; i < childCount; ++i) {
+            View view = mBottomNav.getChildAt(i);
+            int fragmentId = mFragmentIdMap.get(view.getId());
+            assert view instanceof TextView : "Bottom bar must have TextViews as direct children";
+            TextView textView = (TextView) view;
+
+            boolean isSelectedFragment = chosenFragmentId == fragmentId;
+            ApiCompatibilityUtils.setTextAppearance(textView,
+                    isSelectedFragment ? R.style.SelectedNavigationButton
+                                       : R.style.UnselectedNavigationButton);
+            int color = isSelectedFragment ? getResources().getColor(R.color.navigation_selected)
+                                           : getResources().getColor(R.color.navigation_unselected);
+            for (Drawable drawable : textView.getCompoundDrawables()) {
+                if (drawable != null) {
+                    drawable.mutate();
+                    drawable.setColorFilter(
+                            new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN));
+                }
             }
-        });
-
-        CronetEngine.Builder myBuilder = new CronetEngine.Builder(this);
-        myBuilder.enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
-                .enableHttp2(true)
-                .enableQuic(true);
-
-        mCronetEngine = myBuilder.build();
-
-        String appUrl = (getIntent() != null ? getIntent().getDataString() : null);
-        if (appUrl == null) {
-            promptForURL("https://");
-        } else {
-            startWithURL(appUrl);
         }
     }
 
-    private void promptForURL(String url) {
-        Log.i(TAG, "No URL provided via intent, prompting user...");
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Enter a URL");
-        LayoutInflater inflater = getLayoutInflater();
-        View alertView = inflater.inflate(R.layout.dialog_url, null);
-        final EditText urlInput = (EditText) alertView.findViewById(R.id.urlText);
-        urlInput.setText(url);
-        final EditText postInput = (EditText) alertView.findViewById(R.id.postText);
-        alert.setView(alertView);
-
-        alert.setPositiveButton("Load", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int button) {
-                String url = urlInput.getText().toString();
-                String postData = postInput.getText().toString();
-                startWithURL(url, postData);
-            }
-        });
-        alert.show();
-    }
-
-    private void applyPostDataToUrlRequestBuilder(
-            UrlRequest.Builder builder, Executor executor, String postData) {
-        if (postData != null && postData.length() > 0) {
-            builder.setHttpMethod("POST");
-            builder.addHeader("Content-Type", "application/x-www-form-urlencoded");
-            builder.setUploadDataProvider(
-                    UploadDataProviders.create(postData.getBytes()), executor);
-        }
-    }
-
-    private void startWithURL(String url) {
-        startWithURL(url, null);
-    }
-
-    private void startWithURL(String url, String postData) {
-        Log.i(TAG, "Cronet started: " + url);
-        mUrl = url;
-
-        Executor executor = Executors.newSingleThreadExecutor();
-        UrlRequest.Callback callback = new SimpleUrlRequestCallback();
-        UrlRequest.Builder builder = mCronetEngine.newUrlRequestBuilder(url, callback, executor);
-        applyPostDataToUrlRequestBuilder(builder, executor, postData);
-        builder.build().start();
-    }
-
-    // Starts writing NetLog to disk. startNetLog() should be called afterwards.
-    private void startNetLog() {
-        mCronetEngine.startNetLogToFile(getCacheDir().getPath() + "/netlog.json", false);
-    }
-
-    // Stops writing NetLog to disk. Should be called after calling startNetLog().
-    // NetLog can be downloaded afterwards via:
-    //   adb root
-    //   adb pull /data/data/org.chromium.cronet_sample_apk/cache/netlog.json
-    // netlog.json can then be viewed in a Chrome tab navigated to chrome://net-internals/#import
-    private void stopNetLog() {
-        mCronetEngine.stopNetLog();
+    private void switchFragment(int chosenFragmentId) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, mActivityViewModel.getFragment(chosenFragmentId))
+                .commit();
+        updateNavigationBarUI(chosenFragmentId);
     }
 }
--- components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
@@ -8,19 +8,68 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 
+import androidx.annotation.OptIn;
 import androidx.multidex.MultiDex;
+
+import org.chromium.net.ConnectionMigrationOptions;
+import org.chromium.net.CronetEngine;
+import org.chromium.net.DnsOptions;
+import org.chromium.net.QuicOptions;
 
 /**
  * Application for managing the Cronet Sample.
  */
 public class CronetSampleApplication extends Application {
+    private CronetEngine mCronetEngine;
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-
         // install multidex for Kitkat - crbug/1393424
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
             MultiDex.install(this);
         }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CronetEngine.Builder myBuilder = new CronetEngine.Builder(this);
+        myBuilder.enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
+                .enableHttp2(true)
+                .enableQuic(true);
+        mCronetEngine = myBuilder.build();
+    }
+
+    public CronetEngine getCronetEngine() {
+        return mCronetEngine;
+    }
+
+    @OptIn(markerClass = {org.chromium.net.QuicOptions.Experimental.class,
+                   org.chromium.net.ConnectionMigrationOptions.Experimental.class})
+    public void
+    restartCronetEngine() {
+        mCronetEngine.shutdown();
+        CronetEngine.Builder myBuilder = new CronetEngine.Builder(this);
+        ConnectionMigrationOptions.Builder connecMigrationBuilder =
+                ConnectionMigrationOptions.builder();
+        QuicOptions.Builder quicOptionsBuilder = QuicOptions.builder();
+        DnsOptions.Builder dnsOptionsBuilder = DnsOptions.builder();
+        ActionData actionData = (new ActionData.Builder())
+                                        .setCronetEngineBuilder(myBuilder)
+                                        .setMigrationBuilder(connecMigrationBuilder)
+                                        .setQuicBuilder(quicOptionsBuilder)
+                                        .setDnsBuilder(dnsOptionsBuilder)
+                                        .build();
+        for (Options.Option<?> option : Options.getOptions()) {
+            option.configure(actionData);
+        }
+        mCronetEngine =
+                myBuilder.setConnectionMigrationOptions(connecMigrationBuilder)
+                        .setQuicOptions(quicOptionsBuilder)
+                        .setDnsOptions(dnsOptionsBuilder)
+                        .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
+                        .enableHttp2(true)
+                        .enableQuic(true)
+                        .build();
     }
 }
