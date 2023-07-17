f50d3d914156ae82bff03838f6a27f5d4411f2b5
Alex Rudenko <alexrudenko@chromium.org>
2023-07-17 09:08:28 +0000

    Fix the disabled-cache-navigation.js test
    The extra info events should only arrive if the responseReceived
    event says so. Adjusted the test to reflect that.
    
    Fixed: 1465022
    Change-Id: I42e5aeb7ce1f88728959c7879f2fbb513e109c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4687586
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171080}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/disabled-cache-navigation.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/disabled-cache-navigation.js
@@ -1,38 +1,46 @@
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(
+  const {dp} = await testRunner.startBlank(
       `Tests that browser-initiated navigation honors Network.setCacheDisabled\n`);
 
   async function navigateAndGetResponse() {
     dp.Page.navigate({url: testRunner.url('resources/cached.php')});
-    const [responseReceived, responseReceivedExtraInfo] = await Promise.all([
+    const responseReceivedExtraInfoEvents = [];
+    dp.Network.onResponseReceivedExtraInfo(event => {
+      responseReceivedExtraInfoEvents.push(event);
+    });
+    const [responseReceived] = await Promise.all([
       dp.Network.onceResponseReceived(),
-      dp.Network.onceResponseReceivedExtraInfo()]);
-    var response = responseReceived.params;
-    await dp.Network.onceLoadingFinished();
-    var content = (await dp.Network.getResponseBody({requestId: response.requestId})).result.body;
+      dp.Network.onceLoadingFinished()]
+    );
+    const response = responseReceived.params;
+    if (response.hasExtraInfo && responseReceivedExtraInfoEvents.length !== 1) {
+      testRunner.fail(`ResponseReceivedExtraInfo is missing`);
+    }
+    const [responseReceivedExtraInfo] = responseReceivedExtraInfoEvents;
+    const content = (await dp.Network.getResponseBody({requestId: response.requestId})).result.body;
     if (typeof content != 'string' || !content.startsWith('<html>'))
       testRunner.fail(`Invalid response: ${content}`);
     return {
       status: response.response.status,
-      extraInfoStatus: responseReceivedExtraInfo.params.statusCode,
+      extraInfoStatus: responseReceivedExtraInfo ? responseReceivedExtraInfo.params.statusCode : undefined,
       content: content
     };
   }
   await dp.Page.enable();
   await dp.Network.enable();
-  let response = await navigateAndGetResponse();
+  const response = await navigateAndGetResponse();
   testRunner.log(`Original navigation, should not be cached:`);
   testRunner.log(`  responseReceived status: ${response.status}`);
   testRunner.log(`  responseReceivedExtraInfo status: ${response.extraInfoStatus}\n`);
 
-  let response2 = await navigateAndGetResponse();
+  const response2 = await navigateAndGetResponse();
   testRunner.log(`Second navigation, should be cached:`);
   testRunner.log(`  responseReceived status: ${response2.status}`);
   testRunner.log(`  responseReceivedExtraInfo status: ${response2.extraInfoStatus}`);
   testRunner.log(`  cached: ${response.content === response2.content}\n`);
 
   await dp.Network.setCacheDisabled({cacheDisabled: true});
-  let response3 = await navigateAndGetResponse();
+  const response3 = await navigateAndGetResponse();
 
   testRunner.log(`Navigation with cache disabled:`);
   testRunner.log(`  responseReceived status: ${response3.status}`);
