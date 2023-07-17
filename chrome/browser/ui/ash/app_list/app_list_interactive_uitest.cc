00d3d11a41314c1c6704457e641c9ad10f181942
James Cook <jamescook@chromium.org>
2023-07-17 20:43:17 +0000

    ash: Update AppListInteractiveTest.OpenAndClose
    Use Kombucha SetContextOverrideCallback() to simplify element lookup by
    giving all widgets the same context. Add coverage for using mouse clicks
    and key presses to open and close the bubble. This provides the same
    coverage as Tast launcher.BubbleSmoke and will allow us to delete the
    Tast test.
    
    Bug: none
    Test: interactive_ui_tests --gtest_filter=AppListInteractiveTest.OpenAndClose
    Change-Id: Idec2b29e8fddf4ad4c4737bf843ade2b9ab99bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4691204
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171361}

--- chrome/browser/ui/ash/app_list/app_list_interactive_uitest.cc
+++ chrome/browser/ui/ash/app_list/app_list_interactive_uitest.cc
@@ -7,47 +7,69 @@
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_navigation_widget.h"
 #include "ash/shell.h"
+#include "base/functional/bind.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "content/public/test/browser_test.h"
+#include "ui/base/accelerators/accelerator.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/base/test/ui_controls.h"
+#include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/views/interaction/element_tracker_views.h"
 
 namespace ash {
 namespace {
-constexpr char kAppListElementName[] = "App List Element";
-}
 
 class AppListInteractiveTest : public InteractiveBrowserTest {
  public:
   AppListInteractiveTest() {
     // This test suite does not require a browser window.
     set_launch_browser_for_testing(nullptr);
+
+    // Give all widgets the same Kombucha context.This is useful for ash system
+    // UI because the UI uses a variety of small widgets. Note that if this test
+    // used multiple displays we would need to provide a different context per
+    // display (i.e. the widget's native window's root window). Elements like
+    // the home button, shelf, etc. appear once per display.
+    views::ElementTrackerViews::SetContextOverrideCallback(
+        base::BindRepeating([](views::Widget* widget) {
+          return ui::ElementContext(Shell::GetPrimaryRootWindow());
+        }));
   }
 };
 
 IN_PROC_BROWSER_TEST_F(AppListInteractiveTest, OpenAndClose) {
+  // Kombucha needs a widget to be able to click on things.
   views::Widget* navigation_widget =
       Shell::GetPrimaryRootWindowController()->shelf()->navigation_widget();
   SetContextWidget(navigation_widget);
 
+  const gfx::Point screen_origin(0, 0);
+  const ui::Accelerator escape_key(ui::VKEY_ESCAPE, ui::EF_NONE);
+
   RunTestSequence(
       // Clicking the home (launcher) button once opens the bubble widget, which
       // shows the bubble view.
-      PressButton(kHomeButtonElementId),
+      Log("Clicking home button"), PressButton(kHomeButtonElementId),
+      WaitForShow(kAppListBubbleViewElementId),
 
-      // Verify the view is shown and name the element to capture its context.
-      InAnyContext(AfterShow(
-          kAppListBubbleViewElementId,
-          [](ui::InteractionSequence* sequence, ui::TrackedElement* el) {
-            sequence->NameElement(el, kAppListElementName);
-          })),
+      // Clicking in the corner of the screen closes the bubble widget and view.
+      Log("Clicking screen corner"), MoveMouseTo(screen_origin), ClickMouse(),
+      WaitForHide(kAppListBubbleViewElementId),
 
-      // Clicking the home (launcher) button again hides the bubble widget,
-      // which hides the bubble view.
-      PressButton(kHomeButtonElementId),
+      // Pressing the Search key opens the bubble. Don't use SendAccelerator()
+      // because there's no target element.
+      Log("Pressing search key"), Do([]() {
+        ui_controls::SendKeyPress(/*window=*/nullptr, ui::VKEY_COMMAND,
+                                  /*control=*/false, /*shift=*/false,
+                                  /*alt=*/false, /*command=*/false);
+      }),
+      WaitForShow(kAppListBubbleViewElementId),
 
-      // Use the element name because we're not in the same context as the home
-      // button.
-      WaitForHide(kAppListElementName));
+      // Typing the escape key closes the bubble.
+      Log("Pressing escape"),
+      SendAccelerator(kAppListBubbleViewElementId, escape_key),
+      WaitForHide(kAppListBubbleViewElementId));
 }
 
+}  // namespace
 }  // namespace ash
