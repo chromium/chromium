6f7554469402fd3e943f2fa285f45e5438c33999
Victor Gomes <victorgomes@chromium.org>
2023-07-17 15:22:37 +0000

    [maglev] Enable gin flag by default
    Bug: v8:7700
    Change-Id: Iee30975f469e592d7fc2e391b7f2445fba81c385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4684051
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171195}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -94,8 +94,9 @@ BASE_FEATURE(kV8Turbofan, "V8Turbofan", base::FEATURE_ENABLED_BY_DEFAULT);
 // Enables Turbofan's new compiler IR Turboshaft.
 BASE_FEATURE(kV8Turboshaft, "V8Turboshaft", base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Enables experimental Maglev compiler.
-BASE_FEATURE(kV8Maglev, "V8Maglev", base::FEATURE_DISABLED_BY_DEFAULT);
+// Enables Maglev compiler. Note that this only sets the V8 flag when
+// manually overridden; otherwise it defers to whatever the V8 default is.
+BASE_FEATURE(kV8Maglev, "V8Maglev", base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables MinorMC young generation garbage collector.
 BASE_FEATURE(kV8MinorMC, "V8MinorMC", base::FEATURE_DISABLED_BY_DEFAULT);
