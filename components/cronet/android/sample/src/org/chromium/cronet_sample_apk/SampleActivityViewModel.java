bf1ab752d088dd31252b4bba6b0bbde699b777ad
Mohannad Farrag <aymanm@google.com>
2023-07-17 14:51:11 +0000

    Add MainActivityViewModel
    This is responsible for holding data between fragment transaction to maintain consistency and business logic.
    
    Change-Id: Ibeaeb3f1c68bcba4c468ee99ea640a446308ff2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675876
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171183}

---
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/SampleActivityViewModel.java
@@ -0,0 +1,38 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.cronet_sample_apk;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SampleActivityViewModel extends ViewModel {
+    private Map<Integer, Fragment> mFragmentMap = new HashMap<>();
+
+    public static final int FRAGMENT_ID_HOME = 0;
+    public static final int FRAGMENT_ID_FLAGS = 1;
+
+    public Fragment getFragment(int fragmentId) {
+        if (mFragmentMap.containsKey(fragmentId)) {
+            return mFragmentMap.get(fragmentId);
+        }
+        Fragment fragment;
+        switch (fragmentId) {
+            case FRAGMENT_ID_HOME:
+                fragment = new MainFragment();
+                break;
+            case FRAGMENT_ID_FLAGS:
+                fragment = new OptionsFragment();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "Fragment %d does not map to any implementation.", fragmentId));
+        }
+        mFragmentMap.put(fragmentId, fragment);
+        return fragment;
+    }
+}
