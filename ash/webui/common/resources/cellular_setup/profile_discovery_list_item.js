736d002242ff62ccfcc32be973c83944c497ec25
Michael Rygiel <michaelrygiel@google.com>
2023-07-17 21:46:39 +0000

    WebUI: CrOS Settings: Duplicate profile-discovery-list-item
    This CL duplicates the profile-discovery-list-item from the legacy
    and updates it to use the ESimProfileProperties.
    
    Manual Verification completed in final CL in chain.
    
    Bug: b/286600714
    Test: no unit tests, will be run in child CL
    Change-Id: I55fd3c992f6251570a2f2043f60763a56c709e72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4655373
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Michael Rygiel <michaelrygiel@google.com>
    Auto-Submit: Michael Rygiel <michaelrygiel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1171398}

---
+++ ash/webui/common/resources/cellular_setup/profile_discovery_list_item.js
@@ -0,0 +1,66 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * Item in the profile-discovery-list-page list displaying details of an eSIM
+ * profile.
+ */
+
+import '//resources/cr_elements/cr_shared_vars.css.js';
+import '//resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
+import '//resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
+import './cellular_setup_icons.html.js';
+
+import {I18nBehavior} from '//resources/ash/common/i18n_behavior.js';
+import {Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {ESimProfileProperties} from 'chrome://resources/mojo/chromeos/ash/services/cellular_setup/public/mojom/esim_manager.mojom-webui.js';
+
+import {getTemplate} from './profile_discovery_list_item.html.js';
+
+Polymer({
+  _template: getTemplate(),
+  is: 'profile-discovery-list-item',
+
+  behaviors: [I18nBehavior],
+
+  properties: {
+    /**
+     * @type {?ESimProfileProperties}
+     */
+    profileProperties: {
+      type: Object,
+      value: null,
+      notify: true,
+    },
+
+    selected: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
+
+    showLoadingIndicator: {
+      type: Boolean,
+    },
+
+    /**
+     * @type {boolean}
+     * @private
+     */
+    isDarkModeActive_: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /** @private */
+  getProfileName_() {
+    if (!this.profileProperties) {
+      return '';
+    }
+    return String.fromCharCode(...this.profileProperties.name.data);
+  },
+});
