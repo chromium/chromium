7296b65df09841eaca4107bbf3ff6feee71f5acd
Zack Han <zackhan@chromium.org>
2023-07-18 19:00:43 +0000

    CRX Telemetry: PotentialPasswordTheft 100% stable launch.
    - Enable feature by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1351484
    Change-Id: If5e7fbde3fc9b371bfaa125cc8ed89b7a9c1495b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4695774
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1171908}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -145,7 +145,7 @@ const base::FeatureParam<int> kExtensionTelemetryFileDataStartupDelaySeconds{
 
 BASE_FEATURE(kExtensionTelemetryPotentialPasswordTheft,
              "SafeBrowsingExtensionTelemetryPotentialPasswordTheft",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryReportContactedHosts,
              "SafeBrowsingExtensionTelemetryReportContactedHosts",
