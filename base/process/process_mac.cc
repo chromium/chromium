27413048a2b6de0fd7fa4abaf74c6f72651eba46
Patrick Monette <pmonette@chromium.org>
2023-07-19 02:41:00 +0000

    Revert "Make MacSetDefaultTaskRole enabled by default"
    This reverts commit fc66b4a11332620aa574c0e60aabf25faccb0602.
    
    Reason for revert: Regression in number of audio glitches
    https://bugs.chromium.org/p/chromium/issues/detail?id=1465403#c7
    
    Original change's description:
    > Make MacSetDefaultTaskRole enabled by default
    >
    > Bug: 1407290
    > Change-Id: Ie42d24a95343ed4616b3a4e78755d25a3a3f8049
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4544544
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1145940}
    
    Bug: 1407290
    Change-Id: Ic937d46fbd797a6373640830043db91012a6137f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4697093
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172130}

--- base/process/process_mac.cc
+++ base/process/process_mac.cc
@@ -27,7 +27,7 @@ namespace {
 // TASK_DEFAULT_APPLICATION.
 BASE_FEATURE(kMacSetDefaultTaskRole,
              "MacSetDefaultTaskRole",
-             FEATURE_ENABLED_BY_DEFAULT);
+             FEATURE_DISABLED_BY_DEFAULT);
 
 // Returns the `task_role_t` of the process whose process ID is `pid`.
 absl::optional<task_role_t> GetTaskCategoryPolicyRole(
