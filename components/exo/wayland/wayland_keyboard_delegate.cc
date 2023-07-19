5f36d5f8aaff218e35532fae6b7989f9a26835b5
Nergi Rahardi <nergi@google.com>
2023-07-19 01:05:27 +0000

    Flush wl_client when sending key repeat info
    Bug: b/291713951
    Test: Verify event sent
    Change-Id: Ie940a799a5127593900b4f8b01424fb49f570726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694281
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Nergi Rahardi <nergi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1172103}

--- components/exo/wayland/wayland_keyboard_delegate.cc
+++ components/exo/wayland/wayland_keyboard_delegate.cc
@@ -168,6 +168,7 @@ void WaylandKeyboardDelegate::OnKeyRepeatSettingsChanged(
     wl_keyboard_send_repeat_info(keyboard_resource_,
                                  GetWaylandRepeatRate(enabled, interval),
                                  static_cast<int32_t>(delay.InMilliseconds()));
+    wl_client_flush(client());
   }
 }
 
