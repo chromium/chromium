685d3f765691328f8b918d710f165eb0375f328e
Robert Mader <robert.mader@collabora.com>
2023-07-19 16:19:54 +0000

    exo: Work around SkRegion size limit
    SkRegion forbids 0x7FFFFFFF (INT32_MAX) as width or height, see
    SkRegion_kRunTypeSentinel, and would mark the resulting region from the
    union in Surface::Damage as empty.
    
    This breaks damage propagation for many Wayland clients as e.g. Mesa uses exactly that value by default if wl_surface_damage_buffer() is not
    supported - which is the case in Exo for now.
    
    Thus ensure to limit the damage rect to supported values.
    
    Bug: b:290384406
    Change-Id: I45334740a34b196177ea8c3330e5d0f4125edcc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4675388
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Robert Mader <robert.mader@collabora.com>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172413}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -394,7 +394,12 @@ bool Surface::HasPendingAttachedBuffer() const {
 void Surface::Damage(const gfx::Rect& damage) {
   TRACE_EVENT1("exo", "Surface::Damage", "damage", damage.ToString());
 
-  pending_state_.damage.Union(damage);
+  // SkRegion forbids 0x7FFFFFFF (INT32_MAX) as width or height, see
+  // SkRegion_kRunTypeSentinel, and would mark the resulting region from the
+  // union below as empty. See https://crbug.com/1463905
+  gfx::Rect intersected_damage = gfx::Rect(0x7FFFFFFE, 0x7FFFFFFE);
+  intersected_damage.Intersect(damage);
+  pending_state_.damage.Union(intersected_damage);
 }
 
 void Surface::RequestFrameCallback(const FrameCallback& callback) {
