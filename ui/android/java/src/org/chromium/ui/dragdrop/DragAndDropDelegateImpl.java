19032273ad5863324c47f0ddf0c8568ff5489c52
Mark Schillaci <mschillaci@google.com>
2023-07-19 00:48:03 +0000

    [Auto-disable Accessibility] Update DragAndDrop Impl to use AccessibilityState
    This CL continues the work to consolidate accessibility usage in Clank
    by moving the DragAndDrop logic to use AccessibilityState.
    
    
    AX-Relnotes: N/A
    Bug: 1430202, b/265493191
    Change-Id: I293ea83604c58ccbf97e565b0cee94acb6797195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4614408
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172073}

--- ui/android/java/src/org/chromium/ui/dragdrop/DragAndDropDelegateImpl.java
+++ ui/android/java/src/org/chromium/ui/dragdrop/DragAndDropDelegateImpl.java
@@ -23,7 +23,6 @@ import android.view.DragEvent;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.ViewGroup;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.ImageView;
 
 import androidx.annotation.IntDef;
@@ -40,6 +39,7 @@ import org.chromium.base.compat.ApiHelperForN;
 import org.chromium.base.compat.ApiHelperForO;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.ui.R;
+import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.dragdrop.AnimatedImageDragShadowBuilder.CursorOffset;
 import org.chromium.ui.dragdrop.AnimatedImageDragShadowBuilder.DragShadowSpec;
 
@@ -110,10 +110,10 @@ public class DragAndDropDelegateImpl implements DragAndDropDelegate, DragStateTr
             int dragObjRectWidth, int dragObjRectHeight) {
         // Drag and drop is disabled when gesture related a11y service is enabled.
         // See https://crbug.com/1250067.
-        AccessibilityManager a11yManager =
-                (AccessibilityManager) containerView.getContext().getSystemService(
-                        Context.ACCESSIBILITY_SERVICE);
-        if (a11yManager.isEnabled() && a11yManager.isTouchExplorationEnabled()) return false;
+        if (AccessibilityState.isTouchExplorationEnabled()
+                || AccessibilityState.isPerformGesturesEnabled()) {
+            return false;
+        }
 
         ClipData clipdata = buildClipData(dropData);
         if (clipdata == null) {
