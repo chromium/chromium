58a3c40eba9c7b1636457643d9e8ecb4c9958bdb
Colin Blundell <blundell@chromium.org>
2023-07-19 15:59:25 +0000

    [GPU][Mac] Remove no-longer-needed workaround
    This CL removes an OS X-specific GPU workaround that is used only in the
    validating command decoder, which is no longer used on Mac.
    
    Change-Id: Ic4d2d4951c412e1c270e95af3546bb3e0e554525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4694489
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172400}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -14429,14 +14429,9 @@ void GLES2DecoderImpl::DoCopyTexImage2D(
     // vs. "actual" internal format, so this will have to do for now. See
     // Table 3.12 and associated explanatory text in the OpenGL ES 3.0.6
     // spec for more information.
-    //
-    // Unfortunately, changing the internal format here conflicts with a
-    // macos workaround flag, so don't do it if the workaround applies.
     bool attempt_sized_upgrade =
-        (!workarounds().use_intermediary_for_copy_texture_image ||
-         target == GL_TEXTURE_2D) &&
-        (read_format == GL_RGB || read_format == GL_RGB8 ||
-         read_format == GL_RGBA || read_format == GL_RGBA8);
+        read_format == GL_RGB || read_format == GL_RGB8 ||
+        read_format == GL_RGBA || read_format == GL_RGBA8;
     switch (internal_format) {
       case GL_RGB:
         if (attempt_sized_upgrade) {
