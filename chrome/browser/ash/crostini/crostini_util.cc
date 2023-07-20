2bf0f1ec34170d5ce9ec892f5b89e1014b4b85cb
Trent Apted <tapted@chromium.org>
2023-07-20 03:47:19 +0000

    Revert "crostini: Deprecate buster containers"
    This reverts commit b7f51fa3a5b9274cb7a995436fb636f8777e0268.
    
    Reason for revert: gardener suspect for multiple crostini.*.buster_stable failures on chromeos-eve-chrome "failed to install Crostini from UI: installation doesn't finish in time"
    
    Original change's description:
    > crostini: Deprecate buster containers
    >
    > Show the upgrade popup for buster containers. Testing and bug fixes for
    > buster will end in the near future. Users are encouraged to upgrade to
    > bullseye.
    >
    > Auto-formatter changes contained.
    >
    > Bug: b:283027529
    > Test: deploy_chrome to DUT
    > Change-Id: Ieef4099d28773b8259981847755174d74a6e70f6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4680343
    > Reviewed-by: David Munro <davidmunro@google.com>
    > Commit-Queue: James Ye <jamesye@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1172216}
    
    Bug: b:283027529, b:292012155
    Change-Id: Idc799be0d632f8a3939aaacc9189b2baa11fadce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4702725
    Owners-Override: Trent Apted <tapted@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172765}

--- chrome/browser/ash/crostini/crostini_util.cc
+++ chrome/browser/ash/crostini/crostini_util.cc
@@ -203,9 +203,8 @@ bool IsUninstallable(Profile* profile, const std::string& app_id) {
       guest_os::GuestOsRegistryServiceFactory::GetForProfile(profile);
   absl::optional<guest_os::GuestOsRegistryService::Registration> registration =
       registry_service->GetRegistration(app_id);
-  if (registration) {
+  if (registration)
     return registration->CanUninstall();
-  }
   return false;
 }
 
@@ -444,13 +443,11 @@ bool IsContainerVersionExpired(Profile* profile,
                                const guest_os::GuestId& container_id) {
   auto* value = GetContainerPrefValue(profile, container_id,
                                       guest_os::prefs::kContainerOsVersionKey);
-  if (!value) {
+  if (!value)
     return false;
-  }
 
   auto version = static_cast<ContainerOsVersion>(value->GetInt());
-  return version == ContainerOsVersion::kDebianStretch ||
-         version == ContainerOsVersion::kDebianBuster;
+  return version == ContainerOsVersion::kDebianStretch;
 }
 
 std::u16string GetTimeRemainingMessage(base::TimeTicks start, int percent) {
