9c2d265d93575946a00e9d7254f6e1100b01bc30
Xiaodan Zhu <zxdan@chromium.org>
2023-07-20 00:04:08 +0000

    cros_next: add blurred background to pill button
    Sometimes the pill buttons need the background blur effect to make
    the label more clear. Directly setting background blur to pill
    button's layer will make all the rectangle area blur. Setting
    rounded corner radius to the layer will clip the contents of the
    button and its children. For example, the focus ring will be
    clipped. Therefore, we should set a separate background shield to
    the button for the background blur effect.
    
    Background blur is performance consuming so we should make it
    optional and only enable it as needed.
    
    This CL adds a BlurredBackgroundShield variable to the PillButton.
    When background blur is enabled, the blurred background shield will
    replace the button's background.
    
    The final effects are shown in the bug comments.
    
    Bug: b:277915834
    Change-Id: I51bf1c53f0128eea5d1e0b5509076c1ec9a5e08f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4643088
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1172675}

--- ash/style/pill_button.cc
+++ ash/style/pill_button.cc
@@ -5,7 +5,9 @@
 #include "ash/style/pill_button.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/public/cpp/style/color_provider.h"
 #include "ash/style/ash_color_id.h"
+#include "ash/style/blurred_background_shield.h"
 #include "ash/style/color_util.h"
 #include "ash/style/style_util.h"
 #include "ash/style/typography.h"
@@ -60,6 +62,26 @@ bool IsIconPillButton(PillButton::Type type) {
 int GetButtonHeight(PillButton::Type type) {
   return (type & PillButton::kLarge) ? kPillButtonLargeHeight
                                      : kPillButtonHeight;
+}
+
+// Checks if the color variant is assigned a color/color ID.
+bool IsAssignedColorVariant(PillButton::ColorVariant color_variant) {
+  // The color variant is assigned as long as it is not equal to
+  // `gfx::kPlaceholderColor`.
+  return !(absl::holds_alternative<SkColor>(color_variant) &&
+           absl::get<SkColor>(color_variant) == gfx::kPlaceholderColor);
+}
+
+// Updates the target color variant with given color variant if they are not
+// equal.
+bool MaybeUpdateColorVariant(PillButton::ColorVariant& target_color_variant,
+                             PillButton::ColorVariant color_variant) {
+  if (target_color_variant == color_variant) {
+    return false;
+  }
+
+  target_color_variant = color_variant;
+  return true;
 }
 
 absl::optional<ui::ColorId> GetDefaultBackgroundColorId(PillButton::Type type) {
@@ -177,16 +199,13 @@ PillButton::PillButton(PressedCallback callback,
   // Initialize image and icon spacing.
   SetImageLabelSpacing(kIconPillButtonImageLabelSpacingDp);
 
+  Init();
+
   enabled_changed_subscription_ = AddEnabledChangedCallback(base::BindRepeating(
       &PillButton::UpdateBackgroundColor, base::Unretained(this)));
 }
 
 PillButton::~PillButton() = default;
-
-void PillButton::AddedToWidget() {
-  // Only initialize the button after the button is added to a widget.
-  Init();
-}
 
 gfx::Size PillButton::CalculatePreferredSize() const {
   int button_width = label()->GetPreferredSize().width();
@@ -225,29 +244,60 @@ gfx::Insets PillButton::GetInsets() const {
 }
 
 void PillButton::UpdateBackgroundColor() {
-  if (IsFloatingPillButton(type_))
+  if (IsFloatingPillButton(type_)) {
     return;
+  }
 
-  const int height = GetButtonHeight(type_);
+  // Resolve the expected background color.
+  ColorVariant background_color;
   if (!GetEnabled()) {
-    SetBackground(views::CreateThemedRoundedRectBackground(
-        cros_tokens::kCrosSysDisabledContainer, height / 2.f));
-    return;
+    background_color = cros_tokens::kCrosSysDisabledContainer;
+  } else if (IsAssignedColorVariant(background_color_)) {
+    background_color = background_color_;
+  } else {
+    auto default_color_id = GetDefaultBackgroundColorId(type_);
+    DCHECK(default_color_id);
+    background_color = default_color_id.value();
   }
 
-  // If custom color is set, use it to create a solid background.
-  if (background_color_) {
-    SetBackground(views::CreateRoundedRectBackground(background_color_.value(),
-                                                     height / 2.f));
-    return;
+  // Replace the background with blurred background shield if the background
+  // blur is enabled. Otherwise, remove the blurred background shield.
+  const float corner_radius = GetButtonHeight(type_) / 2.0f;
+  if (enable_background_blur_) {
+    if (background()) {
+      SetBackground(nullptr);
+    }
+
+    if (!blurred_background_) {
+      blurred_background_ = std::make_unique<BlurredBackgroundShield>(
+          this, background_color, ColorProvider::kBackgroundBlurSigma,
+          gfx::RoundedCornersF(corner_radius),
+          /*add_layer_to_region=*/false);
+      return;
+    }
+  } else if (blurred_background_) {
+    blurred_background_.reset();
   }
 
-  // Otherwise, use custom ID if set or default color ID to create a themed
-  // background.
-  auto default_color_id = GetDefaultBackgroundColorId(type_);
-  DCHECK(default_color_id);
-  SetBackground(views::CreateThemedRoundedRectBackground(
-      background_color_id_.value_or(default_color_id.value()), height / 2.f));
+  // Create the background with expected color or update the colors of blurred
+  // background shield.
+  if (absl::holds_alternative<SkColor>(background_color)) {
+    SkColor color_value = absl::get<SkColor>(background_color);
+    if (enable_background_blur_) {
+      blurred_background_->SetColor(color_value);
+    } else {
+      SetBackground(
+          views::CreateRoundedRectBackground(color_value, corner_radius));
+    }
+  } else {
+    ui::ColorId color_id = absl::get<ui::ColorId>(background_color);
+    if (enable_background_blur_) {
+      blurred_background_->SetColorId(color_id);
+    } else {
+      SetBackground(
+          views::CreateThemedRoundedRectBackground(color_id, corner_radius));
+    }
+  }
 }
 
 views::PropertyEffects PillButton::UpdateStyleToIndicateDefaultStatus() {
@@ -257,64 +307,39 @@ views::PropertyEffects PillButton::UpdateStyleToIndicateDefaultStatus() {
 }
 
 void PillButton::SetBackgroundColor(const SkColor background_color) {
-  if (background_color_ && background_color_.value() == background_color) {
-    return;
+  if (MaybeUpdateColorVariant(background_color_, background_color)) {
+    UpdateBackgroundColor();
   }
-
-  background_color_ = background_color;
-  background_color_id_ = absl::nullopt;
-  UpdateBackgroundColor();
 }
 
 void PillButton::SetBackgroundColorId(ui::ColorId background_color_id) {
-  if (background_color_id_ &&
-      background_color_id_.value() == background_color_id) {
-    return;
+  if (MaybeUpdateColorVariant(background_color_, background_color_id)) {
+    UpdateBackgroundColor();
   }
-
-  background_color_id_ = background_color_id;
-  background_color_ = absl::nullopt;
-  UpdateBackgroundColor();
 }
 
 void PillButton::SetButtonTextColor(const SkColor text_color) {
-  if (text_color_ && text_color_.value() == text_color) {
-    return;
+  if (MaybeUpdateColorVariant(text_color_, text_color)) {
+    UpdateTextColor();
   }
-
-  text_color_ = text_color;
-  text_color_id_ = absl::nullopt;
-  UpdateTextColor();
 }
 
 void PillButton::SetButtonTextColorId(ui::ColorId text_color_id) {
-  if (text_color_id_ && text_color_id_.value() == text_color_id) {
-    return;
+  if (MaybeUpdateColorVariant(text_color_, text_color_id)) {
+    UpdateTextColor();
   }
-
-  text_color_id_ = text_color_id;
-  text_color_ = absl::nullopt;
-  UpdateTextColor();
 }
 
 void PillButton::SetIconColor(const SkColor icon_color) {
-  if (icon_color_ && icon_color_.value() == icon_color) {
-    return;
+  if (MaybeUpdateColorVariant(icon_color_, icon_color)) {
+    UpdateIconColor();
   }
-
-  icon_color_ = icon_color;
-  icon_color_id_ = absl::nullopt;
-  UpdateIconColor();
 }
 
 void PillButton::SetIconColorId(ui::ColorId icon_color_id) {
-  if (icon_color_id_ && icon_color_id_.value() == icon_color_id) {
-    return;
+  if (MaybeUpdateColorVariant(icon_color_, icon_color_id)) {
+    UpdateIconColor();
   }
-
-  icon_color_id_ = icon_color_id;
-  icon_color_ = absl::nullopt;
-  UpdateIconColor();
 }
 
 void PillButton::SetPillButtonType(Type type) {
@@ -328,6 +353,15 @@ void PillButton::SetPillButtonType(Type type) {
 void PillButton::SetUseDefaultLabelFont() {
   label()->SetFontList(TypographyProvider::Get()->ResolveTypographyToken(
       TypographyToken::kLegacyBody2));
+}
+
+void PillButton::SetEnableBackgroundBlur(bool enable) {
+  if (enable_background_blur_ == enable) {
+    return;
+  }
+
+  enable_background_blur_ = enable;
+  UpdateBackgroundColor();
 }
 
 void PillButton::Init() {
@@ -357,23 +391,21 @@ void PillButton::Init() {
 }
 
 void PillButton::UpdateTextColor() {
-  // Only update text color when the button is added to a widget.
-  if (!GetWidget())
-    return;
-
   SetTextColorId(views::Button::STATE_DISABLED, cros_tokens::kCrosSysDisabled);
 
   // If custom text color is set, use it to set text color.
-  if (text_color_) {
-    SetEnabledTextColors(text_color_.value());
-    return;
+  if (IsAssignedColorVariant(text_color_)) {
+    if (absl::holds_alternative<SkColor>(text_color_)) {
+      SetEnabledTextColors(absl::get<SkColor>(text_color_));
+    } else {
+      SetEnabledTextColorIds(absl::get<ui::ColorId>(text_color_));
+    }
+  } else {
+    // Otherwise, use default color ID to set text color.
+    auto default_color_id = GetDefaultButtonTextIconColorId(type_);
+    DCHECK(default_color_id);
+    SetEnabledTextColorIds(default_color_id.value());
   }
-
-  // Otherwise, use custom color ID if set or default color ID to set text
-  // color.
-  auto default_color_id = GetDefaultButtonTextIconColorId(type_);
-  DCHECK(default_color_id);
-  SetEnabledTextColorIds(text_color_id_.value_or(default_color_id.value()));
 }
 
 void PillButton::UpdateIconColor() {
@@ -389,20 +421,25 @@ void PillButton::UpdateIconColor() {
                     *icon_, cros_tokens::kCrosSysDisabled, kIconSize));
 
   // If custom icon color is set, use it to set icon color.
-  if (icon_color_) {
-    SetImage(views::Button::STATE_NORMAL,
-             gfx::CreateVectorIcon(*icon_, kIconSize, icon_color_.value()));
-    return;
+  if (IsAssignedColorVariant(icon_color_)) {
+    if (absl::holds_alternative<SkColor>(icon_color_)) {
+      SetImage(views::Button::STATE_NORMAL,
+               gfx::CreateVectorIcon(*icon_, kIconSize,
+                                     absl::get<SkColor>(icon_color_)));
+    } else {
+      SetImageModel(
+          views::Button::STATE_NORMAL,
+          ui::ImageModel::FromVectorIcon(
+              *icon_, absl::get<ui::ColorId>(icon_color_), kIconSize));
+    }
+  } else {
+    // Otherwise, use default color ID to set icon color.
+    auto default_color_id = GetDefaultButtonTextIconColorId(type_);
+    DCHECK(default_color_id);
+    SetImageModel(views::Button::STATE_NORMAL,
+                  ui::ImageModel::FromVectorIcon(
+                      *icon_, default_color_id.value(), kIconSize));
   }
-
-  // Otherwise, use custom color ID if set or default color ID to set icon
-  // color.
-  auto default_color_id = GetDefaultButtonTextIconColorId(type_);
-  DCHECK(default_color_id);
-  SetImageModel(views::Button::STATE_NORMAL,
-                ui::ImageModel::FromVectorIcon(
-                    *icon_, icon_color_id_.value_or(default_color_id.value()),
-                    kIconSize));
 }
 
 int PillButton::GetHorizontalSpacingWithIcon() const {
