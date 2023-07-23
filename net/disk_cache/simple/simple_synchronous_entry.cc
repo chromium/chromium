a4d201dfe7e05c8213fc4e22c31d35bf3e31433e
Adam Rice <ricea@chromium.org>
2023-07-23 14:51:33 +0000

    Use InlinedVector in /net/disk_cache
    Replace usages of base::StackVector with absl::InlinedVector. This is
    not a pure rename, as the semantics are slightly different:
    
    1. Methods like push_back() which were accessed with the -> operator in
       StackVector are instead accessed with . with InlinedVector.
    
    See also discussion thread in cxx@chromium.org:
    https://groups.google.com/a/chromium.org/g/cxx/c/jTfqVfU-Ka0/m/caaal90NCgAJ
    
    This CL was uploaded by git cl split.
    
    R=morlovich@chromium.org
    
    Bug: 1465736
    Change-Id: I9d7544e9a67ca8c3761ca3e4c745ad2064894c58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705627
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1173962}

--- net/disk_cache/simple/simple_synchronous_entry.cc
+++ net/disk_cache/simple/simple_synchronous_entry.cc
@@ -9,7 +9,6 @@
 #include <limits>
 
 #include "base/compiler_specific.h"
-#include "base/containers/stack_container.h"
 #include "base/files/file_util.h"
 #include "base/hash/hash.h"
 #include "base/location.h"
@@ -33,6 +32,7 @@
 #include "net/disk_cache/simple/simple_histogram_enums.h"
 #include "net/disk_cache/simple/simple_histogram_macros.h"
 #include "net/disk_cache/simple/simple_util.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 #include "third_party/zlib/zlib.h"
 
 using base::FilePath;
@@ -127,7 +127,7 @@ class SimpleSynchronousEntry::PrefetchData final {
       return false;
     UpdateEarliestOffset(offset);
     return offset >= offset_in_file_ &&
-           end <= (offset_in_file_ + buffer_->size());
+           end <= (offset_in_file_ + buffer_.size());
   }
 
   // Read the given range out of the prefetch buffer into the target
@@ -142,7 +142,7 @@ class SimpleSynchronousEntry::PrefetchData final {
       return false;
     DCHECK(offset >= offset_in_file_);
     size_t buffer_offset = offset - offset_in_file_;
-    memcpy(dest, buffer_->data() + buffer_offset, length);
+    memcpy(dest, buffer_.data() + buffer_offset, length);
     return true;
   }
 
@@ -152,12 +152,13 @@ class SimpleSynchronousEntry::PrefetchData final {
                         size_t offset,
                         size_t length) {
     DCHECK(file);
-    if (!buffer_->empty())
+    if (!buffer_.empty()) {
       return false;
-    buffer_->resize(length);
-    if (file->get()->Read(offset, buffer_->data(), length) !=
+    }
+    buffer_.resize(length);
+    if (file->get()->Read(offset, buffer_.data(), length) !=
         static_cast<int>(length)) {
-      buffer_->resize(0);
+      buffer_.resize(0);
       return false;
     }
     offset_in_file_ = offset;
@@ -183,7 +184,7 @@ class SimpleSynchronousEntry::PrefetchData final {
 
   // Prefer to read the prefetch data into a stack buffer to minimize
   // memory pressure on the OS disk cache.
-  base::StackVector<char, 1024> buffer_;
+  absl::InlinedVector<char, 1024> buffer_;
   size_t offset_in_file_ = 0;
 
   size_t earliest_requested_offset_;
