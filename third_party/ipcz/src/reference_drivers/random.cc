f10a5501f6fed15e64f1c6f14a4e6c615f2863cf
Alex Gough <ajgo@chromium.org>
2023-07-24 20:04:37 +0000

    Use ProcessPrng instead of RtlGenRandom
    Update ipcz reference driver to call ProcessPrng instead of
    RtlGenRandom.
    
    Bug: 74242
    Change-Id: I5a56e918694e15285ade1309ae43fe1b64761f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712686
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174395}

--- third_party/ipcz/src/reference_drivers/random.cc
+++ third_party/ipcz/src/reference_drivers/random.cc
@@ -12,7 +12,6 @@
 
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
-#include <limits>
 #elif BUILDFLAG(IS_FUCHSIA)
 #include <zircon/syscalls.h>
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
@@ -33,17 +32,28 @@
 #endif
 
 #if BUILDFLAG(IS_WIN)
-// #define needed to properly link in RtlGenRandom(), a.k.a. SystemFunction036.
-// See the documentation here:
-// https://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx
-#define SystemFunction036 NTAPI SystemFunction036
-#include <NTSecAPI.h>
-#undef SystemFunction036
+// Prototype for ProcessPrng.
+// See: https://learn.microsoft.com/en-us/windows/win32/seccng/processprng
+extern "C" {
+BOOL WINAPI ProcessPrng(PBYTE pbData, SIZE_T cbData);
+}
 #endif
 
 namespace ipcz::reference_drivers {
 
 namespace {
+
+#if BUILDFLAG(IS_WIN)
+decltype(&ProcessPrng) GetProcessPrng() {
+  HMODULE hmod = LoadLibraryW(L"bcryptprimitives.dll");
+  ABSL_ASSERT(hmod);
+  decltype(&ProcessPrng) process_prng_fn =
+      reinterpret_cast<decltype(&ProcessPrng)>(
+          GetProcAddress(hmod, "ProcessPrng"));
+  ABSL_ASSERT(process_prng_fn);
+  return process_prng_fn;
+}
+#endif
 
 #if defined(OS_POSIX) && !BUILDFLAG(IS_MAC)
 void RandomBytesFromDevUrandom(absl::Span<uint8_t> destination) {
@@ -72,10 +82,8 @@ void RandomBytesFromDevUrandom(absl::Span<uint8_t> destination) {
 
 void RandomBytes(absl::Span<uint8_t> destination) {
 #if BUILDFLAG(IS_WIN)
-  ABSL_ASSERT(destination.size() <= std::numeric_limits<ULONG>::max());
-  const bool ok =
-      RtlGenRandom(destination.data(), static_cast<ULONG>(destination.size()));
-  ABSL_ASSERT(ok);
+  static decltype(&ProcessPrng) process_prng_fn = GetProcessPrng();
+  process_prng_fn(destination.data(), destination.size());
 #elif BUILDFLAG(IS_FUCHSIA)
   zx_cprng_draw(destination.data(), destination.size());
 #elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
