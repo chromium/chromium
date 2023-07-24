1c47959b4395790c8c00d6f26a733ec15b904b6f
Sean Kau <skau@chromium.org>
2023-07-24 19:32:26 +0000

    Enable Jelly in FullscreenPixelTest
    Enable the flag ahead of default enable so we can review the updated
    screenshot.
    
    Change-Id: Ia1312197a04dd00258001fc39cece18f80cf8228
    Bug: b:292294480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4706799
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174374}

--- ash/fullscreen_pixeltest.cc
+++ ash/fullscreen_pixeltest.cc
@@ -6,22 +6,29 @@
 #include "ash/test/ash_test_base.h"
 #include "ash/test/pixel/ash_pixel_differ.h"
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
+#include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 
 namespace ash {
 
 class FullscreenPixelTest : public AshTestBase {
  public:
+  FullscreenPixelTest() : scoped_features_(chromeos::features::kJelly) {}
+
   // AshTestBase:
   absl::optional<pixel_test::InitParams> CreatePixelTestInitParams()
       const override {
     return pixel_test::InitParams();
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 // Verifies the primary fullscreen of an active user session.
 TEST_F(FullscreenPixelTest, VerifyDefaultPrimaryDisplay) {
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "primary_display", /*revision_number=*/2, Shell::GetPrimaryRootWindow()));
+      "primary_display", /*revision_number=*/3, Shell::GetPrimaryRootWindow()));
 }
 
 }  // namespace ash
