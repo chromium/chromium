80b8984c7a4a045c131fb5fc16a745eb538d652a
Shu Yang <shuyng@google.com>
2023-07-24 18:17:37 +0000

    Update ViewportTest#testDefaultViewportSize
    When flag DefaultViewportIsDeviceWidth is enabled, Viewport is default
    to device width.
    
    Bug: 1466336
    Change-Id: I62c045fd57b9c1a518a918538b93ecec8c6f6de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4709567
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174322}

--- content/public/android/javatests/src/org/chromium/content/browser/ViewportTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ViewportTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_shell_apk.ContentShellActivityTestRule;
@@ -45,7 +46,20 @@ public class ViewportTest {
     @Test
     @MediumTest
     @Feature({"Viewport", "InitialViewportSize"})
-    public void testDefaultViewportSize() throws Throwable {
+    @CommandLineFlags.Add({"enable-features=DefaultViewportIsDeviceWidth"})
+    public void testDefaultViewportSize_DefaultViewportIsDeviceWidth() throws Throwable {
+        testDefaultViewportSize(/*isDefaultViewportDeviceWidth*/ true);
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"Viewport", "InitialViewportSize"})
+    @CommandLineFlags.Add({"disable-features=DefaultViewportIsDeviceWidth"})
+    public void testDefaultViewportSize_DefaultViewportIs980() throws Throwable {
+        testDefaultViewportSize(/*isDefaultViewportDeviceWidth*/ false);
+    }
+
+    private void testDefaultViewportSize(boolean isDefaultViewportDeviceWidth) throws Throwable {
         mActivityTestRule.launchContentShellWithUrl("about:blank");
         mActivityTestRule.waitForActiveShellToBeDoneLoading();
 
@@ -61,7 +75,11 @@ public class ViewportTest {
         // Check that the viewport width is vaguely sensible.
         int viewportWidth = evaluateIntegerValue("document.documentElement.clientWidth");
         Assert.assertTrue(Math.abs(evaluateIntegerValue("window.innerWidth") - viewportWidth) <= 1);
-        Assert.assertTrue(viewportWidth >= 979);
+        if (isDefaultViewportDeviceWidth) {
+            Assert.assertTrue(viewportWidth >= metrics.widthPixels / metrics.density - 1);
+        } else {
+            Assert.assertTrue(viewportWidth >= 979);
+        }
         Assert.assertTrue(
                 viewportWidth <= Math.max(981, metrics.widthPixels / metrics.density + 1));
     }
