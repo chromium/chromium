34a5257c19d58ee2a406a6b2676a5bd7891f8cde
Joel Hockey <joelhockey@chromium.org>
2023-07-24 23:40:30 +0000

    Files app: Fix an HTML injection
    BUG=b:292448652, b:292431339
    
    Change-Id: Ice8aac1d2a363e96b756a8e9c9d7956b16e011a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4710386
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174542}

--- ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
+++ ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
@@ -3,9 +3,10 @@
 // found in the LICENSE file.
 
 import {assert, assertNotReached} from 'chrome://resources/ash/common/assert.js';
+import {sanitizeInnerHtml} from 'chrome://resources/js/parse_html_subset.js';
 
 import {getDisallowedTransfers, startIOTask} from '../../common/js/api.js';
-import {getFocusedTreeItem, isDirectoryTree, isDirectoryTreeItem, queryRequiredElement} from '../../common/js/dom_utils.js';
+import {getFocusedTreeItem, htmlEscape, isDirectoryTree, isDirectoryTreeItem, queryRequiredElement} from '../../common/js/dom_utils.js';
 import {FileType} from '../../common/js/file_type.js';
 import {ProgressCenterItem, ProgressItemState, ProgressItemType} from '../../common/js/progress_center_common.js';
 import {getEnabledTrashVolumeURLs, isAllTrashEntries, TrashEntry} from '../../common/js/trash.js';
@@ -679,13 +680,15 @@ export class FileTransferController {
     const container = /** @type {!HTMLElement} */ (
         this.document_.body.querySelector('#drag-container'));
     const multiple = items > 1 ? 'block' : 'none';
-    container.innerHTML = `
-      <div class='drag-box drag-multiple' style='display:${multiple}'></div>
+    const html = `
+      ${items > 1 ? `<div class='drag-box drag-multiple'></div>` : ''}
       <div class='drag-box drag-contents'>
-        <div class='detail-icon'></div><div class='label'>${entry.name}</div>
+        <div class='detail-icon'></div>
+        <div class='label'>${htmlEscape(entry.name)}</div>
       </div>
-      <div class='drag-bubble' style='display:${multiple}'>${items}</div>
+      ${items > 1 ? `<div class='drag-bubble'>${items}</div>` : ''}
     `;
+    container.innerHTML = sanitizeInnerHtml(html, {attrs: ['class']});
 
     const icon = container.querySelector('.detail-icon');
     const thumbnail = this.listContainer_.currentView.getThumbnail(index);
