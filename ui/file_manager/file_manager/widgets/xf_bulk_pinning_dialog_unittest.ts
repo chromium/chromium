4608ade394c7f35446af017378c671d444d64078
François Degros <fdegros@chromium.org>
2023-07-25 05:03:59 +0000

    XfBulkPinningDialog: Unit tests
    BUG=b:290556321
    TEST=xvfb-run out/Default/browser_tests --gtest_filter=FileManagerJsTest.XfBulkPinningDialog
    
    Change-Id: I4e01447311a284d7f65ff17151ddccc08b9210d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4699702
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174637}

--- ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog_unittest.ts
+++ ui/file_manager/file_manager/widgets/xf_bulk_pinning_dialog_unittest.ts
@@ -2,22 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertEquals, assertNotEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
+import {assertEquals, assertNotEquals, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 import {waitForElementUpdate} from '../common/js/unittest_util.js';
 
 import {XfBulkPinningDialog} from './xf_bulk_pinning_dialog.js';
 
-/**
- * Creates new <xf-bulk-pinning-dialog> for each test.
- */
 export function setUp() {
   document.body.innerHTML = '<xf-bulk-pinning-dialog></xf-bulk-pinning-dialog>';
 }
 
-/**
- * Returns the <xf-bulk-pinning-dialog> element.
- */
+/** Gets the <xf-bulk-pinning-dialog> element. */
 async function getDialog(): Promise<XfBulkPinningDialog> {
   const dialog =
       document.querySelector<XfBulkPinningDialog>('xf-bulk-pinning-dialog');
@@ -27,13 +22,50 @@ async function getDialog(): Promise<XfBulkPinningDialog> {
   return dialog!;
 }
 
-/**
- * Tests that the initial XfBulkPinningDialog exists.
- */
+
+/** Gets a footer of the given dialog. */
+function getFooter(dialog: XfBulkPinningDialog, id: string): HTMLDivElement {
+  return dialog.shadowRoot!.querySelector(`#${id}`)!;
+}
+
+/** Gets the "Continue" button of the given dialog. */
+function getButton(dialog: XfBulkPinningDialog): HTMLButtonElement {
+  return dialog.shadowRoot!.querySelector('#continue-button')!;
+}
+
+/** Tests that the initial XfBulkPinningDialog exists. */
 export async function testInit(done: () => void) {
   const dialog = await getDialog();
   assertNotEquals(null, dialog);
   dialog.show();
-  await waitForElementUpdate(dialog);
+
+  // At first, listing-footer should be displayed.
+  assertEquals('none', getFooter(dialog, 'offline-footer').style.display, '1');
+  assertEquals(
+      'none', getFooter(dialog, 'battery-saver-footer').style.display, '2');
+  assertEquals('flex', getFooter(dialog, 'listing-footer').style.display, '3');
+  assertEquals('none', getFooter(dialog, 'error-footer').style.display, '4');
+  assertEquals(
+      'none', getFooter(dialog, 'not-enough-space-footer').style.display, '5');
+  assertEquals('none', getFooter(dialog, 'ready-footer').style.display, '6');
+  assertTrue(getButton(dialog).disabled);
+
+  // But eventually, listing-footer gets replaced.
+  while (getFooter(dialog, 'listing-footer').style.display !== 'none') {
+    console.log('Waiting for listing-footer to disappear...');
+    await waitForElementUpdate(dialog);
+  }
+
+  // The error-footer should now be visible.
+  assertEquals('none', getFooter(dialog, 'offline-footer').style.display, '1');
+  assertEquals(
+      'none', getFooter(dialog, 'battery-saver-footer').style.display, '2');
+  assertEquals('none', getFooter(dialog, 'listing-footer').style.display, '3');
+  assertEquals('initial', getFooter(dialog, 'error-footer').style.display, '4');
+  assertEquals(
+      'none', getFooter(dialog, 'not-enough-space-footer').style.display, '5');
+  assertEquals('none', getFooter(dialog, 'ready-footer').style.display, '6');
+  assertTrue(getButton(dialog).disabled);
+
   done();
 }
