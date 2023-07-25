158ca8e8048f85c04a1c1a1e2137b76920f4fdae
keno <kenoss@chromium.org>
2023-07-25 06:00:16 +0000

    LinkPreview: Introduce feature flag `LinkPreview`
    Design doc: https://docs.google.com/document/d/1hrWfVIDrPkrBlf8A576dDBH7Q34ESMLvOObt0j9i0SU
    
    Bug: b/291842539
    Change-Id: If06ba79188780ab723620cdf15311df256916112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705803
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1174645}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1014,6 +1014,8 @@ BASE_FEATURE(kLightweightNoStatePrefetch,
 #endif
 );
 
+BASE_FEATURE(kLinkPreview, "LinkPreview", base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Makes network loading tasks unfreezable so that they can be processed while
 // the page is frozen.
 BASE_FEATURE(kLoadingTasksUnfreezable,
