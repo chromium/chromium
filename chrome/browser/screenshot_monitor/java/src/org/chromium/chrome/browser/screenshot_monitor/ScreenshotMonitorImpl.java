ce746e2dca0ebf85f8acc58286218dadc369779f
Sophie Chang <sophiechang@chromium.org>
2023-07-25 20:28:12 +0000

    Move Screenshot monitor out of feature_engagement to remove a future circular dep
    Bug: b:280355631
    Change-Id: I543043cb6dea25183478f056e87a108792c07c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4708059
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175051}

--- chrome/browser/screenshot_monitor/java/src/org/chromium/chrome/browser/screenshot_monitor/ScreenshotMonitorImpl.java
+++ chrome/browser/screenshot_monitor/java/src/org/chromium/chrome/browser/screenshot_monitor/ScreenshotMonitorImpl.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.feature_engagement;
+package org.chromium.chrome.browser.screenshot_monitor;
 
 import android.app.Activity;
 import android.content.ContentResolver;
