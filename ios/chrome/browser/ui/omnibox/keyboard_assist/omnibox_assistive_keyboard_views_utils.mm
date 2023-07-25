b1c7c3d56cf634c5d715827e334e41703638c5e9
Jason Hu <hujasonx@google.com>
2023-07-25 13:54:14 +0000

    [Lens] Update omnibox keyboard icon dark mode appearance
    Symbol based buttons in the omnibox keyboard will have monochrome
    symbols with dark backgrounds, with colors matching that of the
    voice search icon which uses pngs.
    
    Bug: 1467280
    Change-Id: I45183a035e780ed5cb84c39de143fac42ffd8ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4712746
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1174810}

--- ios/chrome/browser/ui/omnibox/keyboard_assist/omnibox_assistive_keyboard_views_utils.mm
+++ ios/chrome/browser/ui/omnibox/keyboard_assist/omnibox_assistive_keyboard_views_utils.mm
@@ -9,6 +9,7 @@
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/omnibox/keyboard_assist/omnibox_assistive_keyboard_delegate.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_ui_features.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/voice_search/voice_search_api.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -51,10 +52,17 @@ void SetUpButtonWithSymbol(UIButton* button, NSString* symbolName) {
                           weight:UIImageSymbolWeightSemibold
                            scale:UIImageSymbolScaleMedium];
 
-  UIImage* icon = MakeSymbolMulticolor(
-      CustomSymbolWithConfiguration(symbolName, configuration));
+  UIImage* icon = CustomSymbolWithConfiguration(symbolName, configuration);
+  if (UITraitCollection.currentTraitCollection.userInterfaceStyle ==
+      UIUserInterfaceStyleDark) {
+    icon = MakeSymbolMonochrome(icon);
+    button.tintColor = [UIColor whiteColor];
+  } else {
+    icon = MakeSymbolMulticolor(icon);
+  }
+
+  button.backgroundColor = [UIColor colorNamed:kOmniboxKeyboardButtonColor];
   [button setImage:icon forState:UIControlStateNormal];
-  button.backgroundColor = [UIColor whiteColor];
   button.layer.cornerRadius = kSymbolButtonSize / 2;
 
   button.layer.shadowColor = [UIColor blackColor].CGColor;
