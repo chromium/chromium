985e20f7e023b781380cee10f803604eb425f471
Wenbo Jie <wenbojie@chromium.org>
2023-07-26 05:35:00 +0000

    [FilesRefresh] Add additional margin for grid group heading
    Demo:
    * directory headings: http://screen/5EEJw7iQHYj3nhw.png
    * time period headings: http://screen/Ae28fniJQJsrDDy.png
    
    Bug: b:242938211
    Change-Id: I62c36e1eddfb6a34a22501ebbee6d56503755222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4713813
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175246}

--- ui/file_manager/file_manager/foreground/js/ui/file_grid.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_grid.js
@@ -30,6 +30,8 @@ import {ListSelectionModel} from './list_selection_model.js';
 const MODIFICATION_TIME_GROUP_HEADING_HEIGHT = 57;
 // Align with CSS .grid-title.group-by-isDirectory.
 const DIRECTORY_GROUP_HEADING_HEIGHT = 40;
+// Align with CSS .grid-title ~ .grid-title
+const GROUP_MARGIN_TOP = 16;
 
 /**
  * FileGrid constructor.
@@ -334,13 +336,16 @@ export class FileGrid extends Grid {
 
     let top = 0;
     let totalItemCount = 0;
-    for (const group of groupBySnapshot) {
+    for (let groupIndex = 0; groupIndex < groupBySnapshot.length;
+         groupIndex++) {
+      const group = groupBySnapshot[groupIndex];
       if (index <= group.endIndex) {
         // The index falls into the current group. Calculates how many rows
         // we have in the current group up until this index.
         const indexInCurGroup = index - totalItemCount;
         const rowsInCurGroup = Math.floor(indexInCurGroup / this.columns);
-        top += (rowsInCurGroup > 0 ? this.getGroupHeadingHeight_() : 0) +
+        top +=
+            (rowsInCurGroup > 0 ? this.getGroupHeadingHeight_(groupIndex) : 0) +
             rowsInCurGroup * this.getGroupItemHeight_(group.group);
         break;
       } else {
@@ -348,7 +353,7 @@ export class FileGrid extends Grid {
         // group to the final result.
         const groupItemCount = group.endIndex - group.startIndex + 1;
         const groupRowCount = Math.ceil(groupItemCount / this.columns);
-        top += this.getGroupHeadingHeight_() +
+        top += this.getGroupHeadingHeight_(groupIndex) +
             groupRowCount * this.getGroupItemHeight_(group.group);
         totalItemCount += groupItemCount;
       }
@@ -546,11 +551,13 @@ export class FileGrid extends Grid {
     let totalItemCount = 0;
     let shouldAdd = false;
     // Find the group of "index" and accumulate the height after that group.
-    for (const group of groupBySnapshot) {
+    for (let groupIndex = 0; groupIndex < groupBySnapshot.length;
+         groupIndex++) {
+      const group = groupBySnapshot[groupIndex];
       const groupItemCount = group.endIndex - group.startIndex + 1;
       const groupRowCount = Math.ceil(groupItemCount / this.columns);
       if (shouldAdd) {
-        afterFillerHeight += this.getGroupHeadingHeight_() +
+        afterFillerHeight += this.getGroupHeadingHeight_(groupIndex) +
             groupRowCount * this.getGroupItemHeight_(group.group);
       } else if (index <= group.endIndex) {
         // index falls into the current group. Starting from this group we need
@@ -590,16 +597,23 @@ export class FileGrid extends Grid {
 
   /**
    * Returns the height of group heading.
+   *
+   * @param {number} groupIndex
    * @return {number}
    * @private
    */
-  getGroupHeadingHeight_() {
+  getGroupHeadingHeight_(groupIndex) {
     const fileListModel = /** @type {FileListModel} */ (this.dataModel);
+    // For FilesRefresh, we have an additional margin for non-first group, check
+    // the CSS rule ".grid-title ~ .grid-title" for more information in the CSS
+    // file.
+    const groupMarginTop =
+        util.isJellyEnabled() && groupIndex > 0 ? GROUP_MARGIN_TOP : 0;
     switch (fileListModel.groupByField) {
       case GROUP_BY_FIELD_DIRECTORY:
-        return DIRECTORY_GROUP_HEADING_HEIGHT;
+        return DIRECTORY_GROUP_HEADING_HEIGHT + groupMarginTop;
       case GROUP_BY_FIELD_MODIFICATION_TIME:
-        return MODIFICATION_TIME_GROUP_HEADING_HEIGHT;
+        return MODIFICATION_TIME_GROUP_HEADING_HEIGHT + groupMarginTop;
       default:
         return 0;
     }
@@ -692,17 +706,21 @@ export class FileGrid extends Grid {
     // offset.
     let currentHeight = 0;
     let curRow = 0;
-    for (const group of groupBySnapshot) {
+    for (let groupIndex = 0; groupIndex < groupBySnapshot.length;
+         groupIndex++) {
+      const group = groupBySnapshot[groupIndex];
       const groupItemCount = group.endIndex - group.startIndex + 1;
       const groupRowCount = Math.ceil(groupItemCount / this.columns);
-      const groupHeight = this.getGroupHeadingHeight_() +
+      const groupHeight = this.getGroupHeadingHeight_(groupIndex) +
           groupRowCount * this.getGroupItemHeight_(group.group);
 
       if (currentHeight + groupHeight > innerOffset) {
         // Current offset falls into the current group. Calculates how many
         // rows in the offset within the group.
         const offsetInCurGroup = Math.max(
-            0, innerOffset - currentHeight - this.getGroupHeadingHeight_());
+            0,
+            innerOffset - currentHeight -
+                this.getGroupHeadingHeight_(groupIndex));
         return curRow +
             Math.floor(
                 offsetInCurGroup / this.getGroupItemHeight_(group.group));
@@ -1065,15 +1083,17 @@ export class FileGrid extends Grid {
     let curRow = 0;
     const shift = isStart ? 0 : -this.getItemMarginTop_();
     const yAfterShift = y + shift;
-    for (const group of groupBySnapshot) {
+    for (let groupIndex = 0; groupIndex < groupBySnapshot.length;
+         groupIndex++) {
+      const group = groupBySnapshot[groupIndex];
       const groupItemCount = group.endIndex - group.startIndex + 1;
       const groupRowCount = Math.ceil(groupItemCount / this.columns);
-      const groupHeight = this.getGroupHeadingHeight_() +
+      const groupHeight = this.getGroupHeadingHeight_(groupIndex) +
           groupRowCount * this.getGroupItemHeight_(group.group);
       if (yAfterShift < currentHeight + groupHeight) {
         // The y falls into the current group.
-        const yInCurGroup =
-            yAfterShift - currentHeight - this.getGroupHeadingHeight_();
+        const yInCurGroup = yAfterShift - currentHeight -
+            this.getGroupHeadingHeight_(groupIndex);
         if (yInCurGroup < 0) {
           // The remaining y in this group can't cover the current group
           // heading height.
