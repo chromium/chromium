ce623e01ca1c258986523c61b0cc1110808d0abe
Sean Kau <skau@chromium.org>
2023-07-26 16:17:06 +0000

    Temporarily disable Jelly in HasEnterpriseIconWhenDeviceIsBlockedByPolicy
    Expected icon needs to be updated. Temporarily run with Jelly disabled
    only to unblock flag flip.
    
    Change-Id: I4e02ddcd79cd9f7c4fc3c20b9e0a7713f9fc4943
    Bug: b:289445902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718312
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175484}

--- ash/system/bluetooth/bluetooth_device_list_item_view_unittest.cc
+++ ash/system/bluetooth/bluetooth_device_list_item_view_unittest.cc
@@ -19,7 +19,9 @@
 #include "base/memory/raw_ptr.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "chromeos/ash/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "chromeos/ui/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -378,6 +380,10 @@ TEST_F(BluetoothDeviceListItemViewTest, HasCorrectIcon) {
 
 TEST_F(BluetoothDeviceListItemViewTest,
        HasEnterpriseIconWhenDeviceIsBlockedByPolicy) {
+  // TODO(b/289445902): Fix for Jelly then remove the ScopedFeatureList.
+  base::test::ScopedFeatureList scoped_features;
+  scoped_features.InitAndDisableFeature(chromeos::features::kJelly);
+
   PairedBluetoothDevicePropertiesPtr paired_device_properties =
       CreatePairedDeviceProperties();
 
