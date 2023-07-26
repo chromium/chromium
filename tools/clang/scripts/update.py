46462b9b9f52602ae2e67e1319b87631f4d52fde
Alan Zhao <ayzhao@google.com>
2023-07-26 20:14:00 +0000

    [clang] Update for tip-of-tree version bump to 18.0.0
    Bug: 1467585
    Change-Id: Ia7e2f836c412258b8adff06ad1432fca9d9eaf75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4717326
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: Alan Zhao <ayzhao@google.com>
    Commit-Queue: Alan Zhao <ayzhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1175642}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -40,6 +40,7 @@ CLANG_SUB_REVISION = 3
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
 RELEASE_VERSION = '17'
+# TODO(crbug.com/1467585): Bump to 18 in next Clang roll.
 
 CDS_URL = os.environ.get('CDS_CLANG_BUCKET_OVERRIDE',
     'https://commondatastorage.googleapis.com/chromium-browser-clang')
@@ -333,6 +334,11 @@ def main():
   parser.add_argument('--verify-version',
                       help='Verify that clang has the passed-in version.')
   args = parser.parse_args()
+
+  # TODO(crbug.com/1467585): Remove in next Clang roll.
+  if args.llvm_force_head_revision:
+    global RELEASE_VERSION
+    RELEASE_VERSION = '18'
 
   if args.verify_version and args.verify_version != RELEASE_VERSION:
     print('RELEASE_VERSION is %s but --verify-version argument was %s.' % (
