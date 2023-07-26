2689f302668a6cdc5d68960cca68b84ffee3d70a
conniekxu <conniekxu@chromium.org>
2023-07-26 17:27:22 +0000

    cros_next: Initializing button icons properly
    My last CL https://chromium-review.googlesource.com/c/chromium/src/+/4659684 makes chrome frame
    caption buttons not shown on Linux. It's caused by that the browser
    frame view tries to initialize the buttons before the frame view is
    added to a widget. And we early early in FrameCaptionButton::SetImage if
    the widget is not there yet.
    
    In order to fix it, we remove the widget check in the SetImage function
    to let the button icons and icon definition to be initialized properly.
    Also in SetImage, we don't trigger the paint until the button is added
    to a widget.
    
    
    Fixed: 1467467
    Change-Id: Ife3fb2bfbc6d52ac128e2fcb191907cd1ec2f2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4718194
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1175525}

--- chrome/browser/ui/views/frame/tab_search_frame_caption_button.h
+++ chrome/browser/ui/views/frame/tab_search_frame_caption_button.h
@@ -24,7 +24,6 @@ class TabSearchFrameCaptionButton : public views::FrameCaptionButton {
   static bool IsTabSearchCaptionButtonEnabled(Browser* browser);
 
   // views::FrameCaptionButton:
-  void AddedToWidget() override;
   gfx::Rect GetAnchorBoundsInScreen() const override;
 
   TabSearchBubbleHost* tab_search_bubble_host() {
