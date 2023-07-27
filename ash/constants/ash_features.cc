a49fb6c1684b184239c3e32327c1f890cb194bd4
Xiaohui Chen <xiaohuic@google.com>
2023-07-27 22:31:23 +0000

    os settings: enable sync metrics toggle deprecation
    Bug: b/283174742
    Change-Id: I7712d7cf6f36021347773b51eed97242e8675af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727661
    Auto-Submit: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1176318}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1845,7 +1845,7 @@ BASE_FEATURE(kOsSettingsAppBadgingToggle,
 // deprecated.
 BASE_FEATURE(kOsSettingsDeprecateSyncMetricsToggle,
              "OsSettingsDeprecateSyncMetricsToggle",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables the wayfinding improvements for the ChromeOS Settings revamp
 BASE_FEATURE(kOsSettingsRevampWayfinding,
