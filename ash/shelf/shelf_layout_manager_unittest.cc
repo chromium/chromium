81a7859512e613f1bd401244677f3d78ea2dbf4c
Sean Kau <skau@chromium.org>
2023-07-27 18:09:35 +0000

    Temporarily disable Jelly in test
    Test is crashing in ShellTestApi.  Behavior of test is the same with
    Jelly disabled. Keep Jelly off in test until ShellTestApi bug is
    fixed.
    
    Bug: b:293400777
    Change-Id: Iee14ad051b0de7cbccb828b82a37e21eaa1a2704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722910
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176172}

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -82,6 +82,7 @@
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "components/prefs/pref_service.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -252,7 +253,11 @@ class AutoHideStateDetector : public ShelfLayoutManagerObserver {
 
 class ShelfLayoutManagerTest : public ShelfLayoutManagerTestBase {
  public:
-  ShelfLayoutManagerTest() = default;
+  ShelfLayoutManagerTest() {
+    // TODO(b/293400777): Test currently crashes when Jelly is enabled because
+    // of a crash in ShellTestApi. Remove when that is fixed.
+    scoped_features_.InitAndDisableFeature(chromeos::features::kJelly);
+  }
 
   void SetUpKioskSession() {
     SessionInfo info;
@@ -260,6 +265,9 @@ class ShelfLayoutManagerTest : public ShelfLayoutManagerTestBase {
     info.state = session_manager::SessionState::ACTIVE;
     Shell::Get()->session_controller()->SetSessionInfo(info);
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 // Makes sure SetVisible updates work area and widget appropriately.
