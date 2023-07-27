06c8d43eb8efc0a84dcf123428ffdf9d562af8b5
Matt Mueller <mattm@chromium.org>
2023-07-27 19:05:00 +0000

    If CertVerifyProcInternalTest.TestKnownRoot fails, log the current time
    CertVerifyProcInternalTest.TestKnownRoot is flaking on android bots,
    apparently because the system clock is sometimes wrong. Log the current
    time to confirm this.
    
    Bug: 1468330
    Change-Id: Ief42c78277d93fba6315677bc65982f03b736a61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724951
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Auto-Submit: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176210}

--- net/cert/cert_verify_proc_unittest.cc
+++ net/cert/cert_verify_proc_unittest.cc
@@ -1483,10 +1483,11 @@ TEST_P(CertVerifyProcInternalTest, TestKnownRoot) {
   CertVerifyResult verify_result;
   int error = Verify(cert_chain.get(), "tntpowerwashservices.com", flags,
                      CertificateList(), &verify_result);
-  EXPECT_THAT(error, IsOk()) << "This test relies on a real certificate that "
-                             << "expires on Aug 24 2024. If failing on/after "
-                             << "that date, please disable and file a bug "
-                             << "against mattm.";
+  EXPECT_THAT(error, IsOk())
+      << "This test relies on a real certificate that "
+      << "expires on Aug 24 2024. If failing on/after "
+      << "that date, please disable and file a bug "
+      << "against mattm. Current time: " << base::Time::Now();
   EXPECT_TRUE(verify_result.is_issued_by_known_root);
 #if BUILDFLAG(IS_MAC)
   if (verify_proc_type() == CERT_VERIFY_PROC_BUILTIN) {
