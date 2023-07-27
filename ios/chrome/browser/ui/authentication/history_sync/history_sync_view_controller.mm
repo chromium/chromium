cfea111454222ede1a8a4206de53da83d5ade9e9
Menghan YANG <myuu@google.com>
2023-07-27 15:17:32 +0000

    [iOS][FRE] Add history sync opt-in strings
    Add the string resources for the history sync opt-in screen.
    
    Bug: 1447014
    Change-Id: Ie941071a75f1e11ba7ccc11ed1f0d3b84a56f47d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4722008
    Auto-Submit: Menghan Yang <myuu@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176066}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
@@ -6,6 +6,8 @@
 
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_constants.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -21,13 +23,12 @@
   self.hasAvatarImage = YES;
   self.avatarBackgroundImage =
       [UIImage imageNamed:@"history_sync_opt_in_background"];
-  // TODO(crbug.com/1442218): Replace these temporary strings with the
-  // definitive ones.
-  self.titleText = @"** Save Time, Type Less **";
-  self.subtitleText = @"** To quickly get back to sites you've visited, sync "
-                      @"your tabs and history **";
-  self.primaryActionString = @"** Yes, I'm in **";
-  self.secondaryActionString = @"** No, thanks **";
+  self.titleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_TITLE);
+  self.subtitleText = l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_SUBTITLE);
+  self.primaryActionString =
+      l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_PRIMARY_ACTION);
+  self.secondaryActionString =
+      l10n_util::GetNSString(IDS_IOS_HISTORY_SYNC_SECONDARY_ACTION);
   [super viewDidLoad];
 }
 
