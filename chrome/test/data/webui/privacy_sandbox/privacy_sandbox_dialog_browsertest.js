1046df3df8629294a06de140d7378efe32f8a080
Christos Froussios <cfroussios@google.com>
2023-07-28 17:32:18 +0000

    [Gardener] Disable PrivacySandboxDialogSmallWindowTest on mac
    Merge to M115: Flaky on Mac.
    Renamed PrivacySandboxDialogSmallWindowTest.All to PrivacySandboxDialogSmallWindowTest.SmallAll to avoid a collision with
    PrivacySandboxDialogBigWindowTest.All
    
    (cherry picked from commit 7bb561217cdd82e9026a31410126573fa619bc3f)
    
    Bug: 1446188
    Change-Id: I00fd06c94663baaf29b955bb9322f176858367eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4596459
    Auto-Submit: Christos Froussios <cfroussios@chromium.org>
    Owners-Override: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1154253}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724126
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Owners-Override: Robert Liao <robliao@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5790@{#1872}
    Cr-Branched-From: 1d71a337b1f6e707a13ae074dca1e2c34905eb9f-refs/heads/main@{#1148114}

--- chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.js
+++ chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.js
@@ -28,7 +28,14 @@ var PrivacySandboxDialogSmallWindowTest = class extends PolymerTest {
   }
 };
 
-TEST_F('PrivacySandboxDialogSmallWindowTest', 'All', function() {
+// TODO(https://crbug.com/1446188): Re-enable the test.
+GEN('#if BUILDFLAG(IS_MAC)');
+GEN('# define MAYBE_SmallAll DISABLED_SmallAll');
+GEN('#else');
+GEN('# define MAYBE_SmallAll SmallAll');
+GEN('#endif');
+
+TEST_F('PrivacySandboxDialogSmallWindowTest', 'MAYBE_SmallAll', function() {
   mocha.run();
 });
 
