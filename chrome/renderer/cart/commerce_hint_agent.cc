8f257d23592836ff3f2dee1b73b1034179a584d7
Mei Liang <meiliang@chromium.org>
2023-07-28 23:08:58 +0000

    Add user actions for different commerce events
    Change-Id: Idf0fe42df5c43c56b753c82e9562d4171bc9de5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727094
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176859}

--- chrome/renderer/cart/commerce_hint_agent.cc
+++ chrome/renderer/cart/commerce_hint_agent.cc
@@ -10,6 +10,7 @@
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros_local.h"
+#include "base/metrics/user_metrics.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -237,26 +238,32 @@ void RecordCommerceEvent(CommerceEvent event) {
     case CommerceEvent::kAddToCartByForm:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.AddToCartByPOST", true);
       DVLOG(1) << "Commerce.AddToCart by POST form";
+      base::RecordAction(base::UserMetricsAction("Commerce.AddToCart"));
       break;
     case CommerceEvent::kAddToCartByURL:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.AddToCartByURL", true);
       DVLOG(1) << "Commerce.AddToCart by URL";
+      base::RecordAction(base::UserMetricsAction("Commerce.AddToCart"));
       break;
     case CommerceEvent::kVisitCart:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.VisitCart", true);
       DVLOG(1) << "Commerce.VisitCart";
+      base::RecordAction(base::UserMetricsAction("Commerce.VisitCart"));
       break;
     case CommerceEvent::kVisitCheckout:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.VisitCheckout", true);
       DVLOG(1) << "Commerce.VisitCheckout";
+      base::RecordAction(base::UserMetricsAction("Commerce.VisitCheckout"));
       break;
     case CommerceEvent::kPurchaseByForm:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.PurchaseByPOST", true);
       DVLOG(1) << "Commerce.Purchase by POST form";
+      base::RecordAction(base::UserMetricsAction("Commerce.Purchase"));
       break;
     case CommerceEvent::kPurchaseByURL:
       LOCAL_HISTOGRAM_BOOLEAN("Commerce.Carts.PurchaseByURL", true);
       DVLOG(1) << "Commerce.Purchase by URL";
+      base::RecordAction(base::UserMetricsAction("Commerce.Purchase"));
       break;
     default:
       NOTREACHED();
