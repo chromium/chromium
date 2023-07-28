aad76180bd48b78818efdb1f3fd0509c14142e44
Domenic Denicola <domenic@chromium.org>
2023-07-28 01:36:22 +0000

    CloseWatcher: factor out and expand Esc key tests
    Previously, tests which assumed the Esc key was the close signal were
    mixed up with tests that were generic for any close signal. Factor out
    those tests from basic.html and after-other-listeners.html, and expand
    them to cover keyup and keypress in addition to keydown.
    
    Bug: 1171318
    Change-Id: I09a62b087f5f2b0b7032adbd9f987dca711950a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4721668
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176390}

--- third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/close-watcher/resources/helpers.js
@@ -15,9 +15,16 @@ window.createBlessedRecordingCloseWatcher = (t, events, name) => {
   return test_driver.bless("create " + name, () => createRecordingCloseWatcher(t, events, name));
 };
 
-window.sendCloseSignal = () => {
+window.sendEscKey = () => {
   // *not* \uu001B; see https://w3c.github.io/webdriver/#keyboard-actions
   const ESC = '\uE00C';
 
   return test_driver.send_keys(document.getElementById("d"), ESC);
 };
+
+// For now, we always use the Esc keypress as our close signal. In
+// theory, in the future, we could add a WebDriver command or similar
+// for the close signal, which would allow different tests on platforms
+// with different close signals. In that case, we'd update this
+// function, but not update the sendEscKey function above.
+window.sendCloseSignal = window.sendEscKey;
