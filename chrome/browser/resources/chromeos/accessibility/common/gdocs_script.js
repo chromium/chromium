4c7eced6f781612530681475f9c8f3db2a10f1a4
Katie Dektar <katie@chromium.org>
2023-07-28 22:04:46 +0000

    [Lacros A11y] Fix STS and Switch Access content script injection in Lacros
    Google Docs was refusing to allow the EmbeddedA11yHelper extension
    to inject the <script> tag with an error that "This document requires
    'TrustedHTML' assignment". This only happened in Lacros, which uses
    MV3 to inject the script, and not Ash, which uses MV2. Updates the
    injected script to work properly in Lacros by using a TrustedScript.
    
    Note that using "Eval" in the Ash extensions does not seem to work,
    so we continue to add a <script> tag in Ash.
    
    Test: Manual in Lacros, Ash on Chrome OS on Linux
    Bug: b/293466039
    Change-Id: Ic3c91926e3aab36a4718dd68ea0a5df01ef832a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4730018
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1176830}

--- chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
+++ chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
@@ -3,23 +3,30 @@
 // found in the LICENSE file.
 
 function triggerDocsCanvasAnnotationMode() {
-  let extensionId = '';
   if (!chrome.runtime || !chrome.runtime.id) {
     // Manifest v3: Not running in extension's runtime.
     // Parse this script's URL to determine extension ID.
     // The URL will look like
     // `chrome-extension://extensionId/common/gdocs_script.js.
-    extensionId = document.currentScript.src.split('/')[2];
+    const extensionId = document.currentScript.src.split('/')[2];
+    const scriptContents = `
+      window['_docs_annotate_canvas_by_ext'] = "${extensionId}";
+    `;
+    const policy = trustedTypes.createPolicy('gdocsPolicy', {
+      createScript: (text) => text,
+    });
+    const sanitized = policy.createScript(scriptContents);
+    eval(sanitized);
   } else {
     // Manifest v2.
-    extensionId = chrome.runtime.id;
+    const extensionId = chrome.runtime.id;
+    const scriptContents = `
+      window['_docs_annotate_canvas_by_ext'] = "${extensionId}";
+    `;
+    const script = document.createElement('script');
+    script.innerHTML = scriptContents;
+    document.documentElement.appendChild(script);
   }
-  const scriptContents = `
-    window['_docs_annotate_canvas_by_ext'] = "${extensionId}";
-  `;
-  const script = document.createElement('script');
-  script.innerHTML = scriptContents;
-  document.documentElement.appendChild(script);
 }
 
 // Docs renders content in Canvas without annotations by default. This script is
