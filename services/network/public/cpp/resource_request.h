9b98e028d5a19faa077177fff893cbcbbacf079a
Dominic Farolino <dom@chromium.org>
2023-07-28 00:10:36 +0000

    Add kNoCors security disclaimer in network::ResourceRequest
    The "no-cors" fetch mode is highly discouraged for security reasons, however it is the default for both the Fetch Standard [1] and in the
    network service. This CL adds documentation encouraging those setting up
    new network::ResourceRequests to re-consider using `kNoCors`, and only
    do so with deep justification and security team buy-in.
    
    R=jyasskin@google.com
    
    Bug: 1383340
    Change-Id: Ib5fcf54e25e7b0cd2bf458c322fc172110633abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4724926
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Jeffrey Yasskin <jyasskin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1176354}

--- services/network/public/cpp/resource_request.h
+++ services/network/public/cpp/resource_request.h
@@ -158,6 +158,11 @@ struct COMPONENT_EXPORT(NETWORK_CPP_BASE) ResourceRequest {
   bool originated_from_service_worker = false;
   bool skip_service_worker = false;
   bool corb_detachable = false;
+  // `kNoCors` mode is the default request mode for legacy reasons, however this
+  // mode is highly discouraged for new requests made on the web platform;
+  // please consider using another mode like `kCors` instead, and only use
+  // `kNoCors` with strong rationale and approval from security experts. See
+  // https://fetch.spec.whatwg.org/#concept-request-mode.
   mojom::RequestMode mode = mojom::RequestMode::kNoCors;
   mojom::IPAddressSpace target_address_space = mojom::IPAddressSpace::kUnknown;
   mojom::CredentialsMode credentials_mode = mojom::CredentialsMode::kInclude;
