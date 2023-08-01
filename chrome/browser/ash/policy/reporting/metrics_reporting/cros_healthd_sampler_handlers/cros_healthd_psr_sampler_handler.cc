c2d188808f38c3f80d91940f883d488b8fdcf4ac
Hong Xu <xuhong@chromium.org>
2023-08-01 19:00:07 +0000

    Collect the result of getting PSR info from healthd
    Bug: b:280068539
    Change-Id: I96514edcdd8cd921a469d6945e475621cfa425a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735600
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177976}

--- chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_psr_sampler_handler.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_psr_sampler_handler.cc
@@ -8,12 +8,32 @@
 
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/types/cxx23_to_underlying.h"
 #include "components/reporting/metrics/sampler.h"
 #include "components/reporting/proto/synced/metric_data.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace reporting {
+
+namespace {
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class EnterpriseReportingPsrResult {
+  kOk = 0,
+  kErrorGettingPsr = 1,
+  kUnknownSystemResultType = 2,
+  kNullPsrInfo = 3,
+  kPsrUnsupported = 4,
+  kMaxValue = kPsrUnsupported
+};
+
+// Records PSR result to UMA.
+void RecordPsrResult(EnterpriseReportingPsrResult result) {
+  base::UmaHistogramEnumeration("Browser.ERP.PsrResult", result);
+}
+
+}  // namespace
 
 namespace cros_healthd = ::ash::cros_healthd::mojom;
 
@@ -38,10 +58,12 @@ void CrosHealthdPsrSamplerHandler::HandleResult(
   if (system_result->which() != cros_healthd::SystemResult::Tag::kSystemInfo) {
     switch (system_result->which()) {
       case cros_healthd::SystemResult::Tag::kError:
+        RecordPsrResult(EnterpriseReportingPsrResult::kErrorGettingPsr);
         LOG(ERROR) << "cros_healthd: Error getting PSR info: "
                    << system_result->get_error()->msg;
         return;
       default:
+        RecordPsrResult(EnterpriseReportingPsrResult::kUnknownSystemResultType);
         LOG(ERROR) << "cros_healthd: Unknown system result type: "
                    << base::to_underlying(system_result->which());
         return;
@@ -50,11 +72,13 @@ void CrosHealthdPsrSamplerHandler::HandleResult(
 
   const auto& psr_info = system_result->get_system_info()->psr_info;
   if (psr_info.is_null()) {
+    RecordPsrResult(EnterpriseReportingPsrResult::kNullPsrInfo);
     LOG(ERROR) << "Null PsrInfo from cros_healthd";
     return;
   }
 
   if (!psr_info->is_supported) {
+    RecordPsrResult(EnterpriseReportingPsrResult::kPsrUnsupported);
     return;
   }
 
@@ -70,5 +94,6 @@ void CrosHealthdPsrSamplerHandler::HandleResult(
   runtime_counters_telemetry->set_counter_enter_hibernation(
       psr_info->s4_counter);
   runtime_counters_telemetry->set_counter_enter_poweroff(psr_info->s5_counter);
+  RecordPsrResult(EnterpriseReportingPsrResult::kOk);
 }
 }  // namespace reporting
