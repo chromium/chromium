56db3888bf16627dd470713f97698a28de25c167
Asami Doi <asamidoi@chromium.org>
2023-08-01 14:24:16 +0000

    [ios] Remove webStateList:didChangeActiveWebState:oldWebState:atIndex:reason: from WebScrollViewMainContentUIForwarder
    This CL updates WebScrollViewMainContentUIForwarder to handle the activation of WebState in didChangeWebStateList:change:status:.
    
    Bug: 1442546
    Change-Id: I240bbc83ef06f681b60087047ab0c98a180e3104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4705250
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177806}

--- ios/chrome/browser/ui/main_content/web_scroll_view_main_content_ui_forwarder.mm
+++ ios/chrome/browser/ui/main_content/web_scroll_view_main_content_ui_forwarder.mm
@@ -182,40 +182,9 @@ void UpdateStateWithProxy(MainContentUIStateUpdater* updater,
 - (void)didChangeWebStateList:(WebStateList*)webStateList
                        change:(const WebStateListChange&)change
                        status:(const WebStateListStatus&)status {
-  switch (change.type()) {
-    case WebStateListChange::Type::kStatusOnly:
-      // TODO(crbug.com/1442546): Move the implementation from
-      // webStateList:didChangeActiveWebState:oldWebState:atIndex:reason to
-      // here. Note that here is reachable only when `reason` ==
-      // ActiveWebStateChangeReason::Activated.
-      break;
-    case WebStateListChange::Type::kDetach:
-      // Do nothing when a WebState is detached.
-      break;
-    case WebStateListChange::Type::kMove:
-      // Do nothing when a WebState is moved.
-      break;
-    case WebStateListChange::Type::kReplace: {
-      const WebStateListChangeReplace& replaceChange =
-          change.As<WebStateListChangeReplace>();
-      web::WebState* insertedWebState = replaceChange.inserted_web_state();
-      if (insertedWebState == webStateList->GetActiveWebState()) {
-        self.webState = insertedWebState;
-      }
-      break;
-    }
-    case WebStateListChange::Type::kInsert:
-      // Do nothing when a new WebState is inserted.
-      break;
+  if (status.active_web_state_change()) {
+    self.webState = status.new_active_web_state;
   }
-}
-
-- (void)webStateList:(WebStateList*)webStateList
-    didChangeActiveWebState:(web::WebState*)newWebState
-                oldWebState:(web::WebState*)oldWebState
-                    atIndex:(int)atIndex
-                     reason:(ActiveWebStateChangeReason)reason {
-  self.webState = newWebState;
 }
 
 #pragma mark - Private
