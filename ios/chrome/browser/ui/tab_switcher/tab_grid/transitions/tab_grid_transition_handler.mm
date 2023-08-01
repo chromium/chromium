865a9cba1130392f9a906f4b9ce4a53e91447d9b
Petro Akzhygitov <pakzhygitov@google.com>
2023-08-01 10:44:26 +0000

    [Part 1] Disabled animation case
    Change-Id: I32f1a963f7696ff11fdf02a2dce79d192da0e51b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4701563
    Commit-Queue: Petro Akzhygitov <pakzhygitov@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1177718}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/tab_grid_transition_handler.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/tab_grid_transition_handler.mm
@@ -5,6 +5,9 @@
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/tab_grid_transition_handler.h"
 
 #import "base/ios/block_types.h"
+#import "ios/chrome/browser/shared/ui/util/named_guide.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/transitions/animations/tab_grid_transition_animation.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -16,6 +19,8 @@
 
   UIViewController* _tabGridViewController;
   BVCContainerViewController* _bvcContainerViewController;
+
+  id<TabGridTransitionAnimation> _animation;
 }
 
 #pragma mark - Public
@@ -52,13 +57,35 @@
 // Performs the Browser to Tab Grid transition with a `completion` block.
 - (void)performBrowserToTabGridTransitionWithCompletion:
     (ProceduralBlock)completion {
+  __weak __typeof(self) weakSelf = self;
+  ProceduralBlock animationCompletion = ^{
+    [weakSelf finalizeBrowserToTabGridTransition];
+
+    if (completion) {
+      completion();
+    }
+  };
+
   [self prepareBrowserToTabGridTransition];
+  [self performBrowserToTabGridTransitionAnimationWithCompletion:
+            animationCompletion];
 }
 
 // Performs the Tab Grid to Browser transition with a `completion` block.
 - (void)performTabGridToBrowserTransitionWithCompletion:
     (ProceduralBlock)completion {
+  __weak __typeof(self) weakSelf = self;
+  ProceduralBlock animationCompletion = ^{
+    [weakSelf finalizeTabGridToBrowserTransition];
+
+    if (completion) {
+      completion();
+    }
+  };
+
   [self prepareTabGridToBrowserTransition];
+  [self performTabGridToBrowserTransitionAnimationWithCompletion:
+            animationCompletion];
 }
 
 // Prepares items for the Browser to Tab Grid transition.
@@ -75,6 +102,61 @@
 
   _bvcContainerViewController.view.accessibilityViewIsModal = YES;
   _bvcContainerViewController.view.alpha = 0;
+}
+
+// Performs Browser to TabGrid transition animation.
+- (void)performBrowserToTabGridTransitionAnimationWithCompletion:
+    (ProceduralBlock)completion {
+  _animation = [self determineAnimationForBrowserToTabGridTransition];
+  [self performTransitionAnimationWithCompletion:completion];
+}
+
+// Performs TabGrid to Browser transition animation.
+- (void)performTabGridToBrowserTransitionAnimationWithCompletion:
+    (ProceduralBlock)completion {
+  _animation = [self determineAnimationForTabGridToBrowserTransition];
+  [self performTransitionAnimationWithCompletion:completion];
+}
+
+// Takes all necessary actions to finish Browser to TabGrid transition.
+- (void)finalizeBrowserToTabGridTransition {
+  [_bvcContainerViewController.view removeFromSuperview];
+  [_bvcContainerViewController removeFromParentViewController];
+
+  [_tabGridViewController setNeedsStatusBarAppearanceUpdate];
+}
+
+// Takes all necessary actions to finish TabGrid to Browser transition.
+- (void)finalizeTabGridToBrowserTransition {
+  _bvcContainerViewController.view.alpha = 1;
+  [_bvcContainerViewController
+      didMoveToParentViewController:_tabGridViewController];
+
+  [_bvcContainerViewController setNeedsStatusBarAppearanceUpdate];
+}
+
+// Performs transition animation.
+- (void)performTransitionAnimationWithCompletion:(ProceduralBlock)completion {
+  if (_animation) {
+    [_animation animateWithCompletion:completion];
+
+  } else if (completion) {
+    completion();
+  }
+}
+
+// Determines the proper animation that should be used in Browser to TabGrid
+// transition.
+- (id<TabGridTransitionAnimation>)
+    determineAnimationForBrowserToTabGridTransition {
+  return nil;
+}
+
+// Determines the proper animation that should be used in TabGrid to Browser
+// transition.
+- (id<TabGridTransitionAnimation>)
+    determineAnimationForTabGridToBrowserTransition {
+  return nil;
 }
 
 @end
