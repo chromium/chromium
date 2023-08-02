30fb83577427d557368ee651581f0f9157031bc6
Brahim Chikhaoui <bchikhaoui@google.com>
2023-08-02 04:02:16 +0000

    [OOBE] Enable OobeSOftwareUpdate feature flag
    enable the feature flag for consumer update screen by default.
    
    Bug: b:292904894
    Change-Id: I42cc6760583310807cd47612ea80ef3bc17edae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4727699
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178236}

--- chrome/browser/ash/login/screens/update_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/update_screen_browsertest.cc
@@ -52,8 +52,8 @@ const test::UIPath kCellularPermissionBack = {"oobe-update",
 const test::UIPath kLowBatteryWarningMessage = {"oobe-update",
                                                 "battery-warning"};
 const test::UIPath kErrorMessage = {"error-message"};
-const test::UIPath kBetterUpdateCheckingForUpdatesDialog = {
-    "oobe-update", "checking-for-updates-dialog"};
+const test::UIPath kBetterUpdateCheckingForUpdatesDialog = {"oobe-update",
+                                                            "checking-update"};
 const test::UIPath kUpdateInProgressDialog = {"oobe-update",
                                               "update-in-progress-dialog"};
 const test::UIPath kRestartingDialog = {"oobe-update", "restarting-dialog"};
