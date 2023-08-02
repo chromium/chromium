7ac0eea5146e1a7e30abe4f0cc2bbd5dad5eb748
dpapad <dpapad@chromium.org>
2023-08-02 06:49:49 +0000

    Settings: Migrate tests to WebUIMochaBrowserTest, part 14 (last).
    In this part migrating Gtests triggering the Mocha tests in:
     - payments_section_focus_test.js
     - payments_section_interactive_test.js
     - people_page_sync_page_interactive_test.js
     - review_notification_permissions_interactive_ui_test.js
     - secure_dns_interactive_test.js
     - settings_animated_pages_test.js
     - settings_menu_interactive_ui_test.js
     - settings_ui_test.js
     - unused_site_permissions_interactive_ui_test.js
    
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Bug: 1457360
    Change-Id: I51924ec9dd4de2c6b1ece82fb4c561183e311331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4735092
    Commit-Queue: John Lee <johntlee@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1178269}

---
+++ chrome/test/data/webui/settings/settings_focus_test.cc
@@ -0,0 +1,82 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/chrome_features.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "components/content_settings/core/common/features.h"
+#include "content/public/test/browser_test.h"
+
+class SettingsFocusTest : public WebUIMochaFocusTest {
+ protected:
+  SettingsFocusTest() { set_test_loader_host(chrome::kChromeUISettingsHost); }
+};
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, AnimatedPages) {
+  RunTest("settings/settings_animated_pages_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, PaymentsSectionInteractive) {
+  RunTest("settings/payments_section_interactive_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, PaymentsSectionFocus) {
+  RunTest("settings/payments_section_focus_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, SyncPage) {
+  RunTest("settings/people_page_sync_page_interactive_test.js", "mocha.run()");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, SecureDns) {
+  RunTest("settings/secure_dns_interactive_test.js", "mocha.run()");
+}
+
+// Times out on Mac. See https://crbug.com/1060981.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_SettingsUIToolbarAndDrawer DISABLED_SettingsUIToolbarAndDrawer
+#else
+#define MAYBE_SettingsUIToolbarAndDrawer SettingsUIToolbarAndDrawer
+#endif
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, MAYBE_SettingsUIToolbarAndDrawer) {
+  RunTest("settings/settings_ui_test.js",
+          "runMochaSuite('SettingsUIToolbarAndDrawer')");
+}
+
+// Times out on Mac. See https://crbug.com/1060981.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_SettingsUISearch DISABLED_SettingsUISearch
+#else
+#define MAYBE_SettingsUISearch SettingsUISearch
+#endif
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, MAYBE_SettingsUISearch) {
+  RunTest("settings/settings_ui_test.js", "runMochaSuite('SettingsUISearch')");
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsFocusTest, Menu) {
+  RunTest("settings/settings_menu_interactive_ui_test.js", "mocha.run()");
+}
+
+class SettingsReviewNotificationPermissionsFocusTest
+    : public SettingsFocusTest {
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kSafetyCheckNotificationPermissions};
+};
+
+IN_PROC_BROWSER_TEST_F(SettingsReviewNotificationPermissionsFocusTest, All) {
+  RunTest("settings/review_notification_permissions_interactive_ui_test.js",
+          "mocha.run()");
+}
+
+class SettingsUnusedSitePermissionsFocusTest : public SettingsFocusTest {
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      content_settings::features::kSafetyCheckUnusedSitePermissions};
+};
+
+IN_PROC_BROWSER_TEST_F(SettingsUnusedSitePermissionsFocusTest, All) {
+  RunTest("settings/unused_site_permissions_interactive_ui_test.js",
+          "mocha.run()");
+}
