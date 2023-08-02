adaba96cccebfe3a8e183e9d02f045c93bb26992
Theresa Sullivan <twellington@chromium.org>
2023-08-02 16:04:22 +0000

    Add automotive as type in DeviceInfoFeedbackSource
    Bug: 1469472
    Low-Coverage-Reason: Simple if-else block, not interesting to test
    Change-Id: Ie9a3f6bdb638aad736e7d7fd9e03e745b1d7d7cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740096
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178455}

--- chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/DeviceInfoFeedbackSource.java
+++ chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/DeviceInfoFeedbackSource.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.feedback;
 import android.os.Build;
 import android.util.Pair;
 
+import org.chromium.base.BuildInfo;
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.ContextUtils;
 import org.chromium.ui.base.DeviceFormFactor;
@@ -19,6 +20,7 @@ class DeviceInfoFeedbackSource implements FeedbackSource {
     private static final String DEVICE_TYPE_KEY = "device_type";
     private static final String TYPE_PHONE = "phone";
     private static final String TYPE_TABLET = "tablet";
+    private static final String TYPE_AUTO = "automotive";
 
     @Override
     public Map<String, String> getFeedback() {
@@ -27,10 +29,15 @@ class DeviceInfoFeedbackSource implements FeedbackSource {
         // (column 'device'). This can be obtained from system property 'ro.product.device'
         // via android.os.Build.DEVICE.
         String name = Build.DEVICE;
-        String type = DeviceFormFactor.isNonMultiDisplayContextOnTablet(
-                              ContextUtils.getApplicationContext())
-                ? TYPE_TABLET
-                : TYPE_PHONE;
+        String type;
+        if (BuildInfo.getInstance().isAutomotive) {
+            type = TYPE_AUTO;
+        } else if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(
+                           ContextUtils.getApplicationContext())) {
+            type = TYPE_TABLET;
+        } else {
+            type = TYPE_PHONE;
+        }
 
         return CollectionUtil.newHashMap(
                 Pair.create(DEVICE_NAME_KEY, name), Pair.create(DEVICE_TYPE_KEY, type));
