225bdca6bb07031a60dc60897e182088e90b9246
Bryan Yu <yuholong@google.com>
2023-08-02 15:46:12 +0000

    Modify ARC on Demand metrics
    The number of users that should have been affected by ARC on Demand in
    the control group cannot be retrieved under the current implementation.
    Adding a "ShouldBeDelayed" metric to track this group of users.
    
    Bug: b:293081292
    Test: unittest
    Change-Id: I4087713dc4d5c5eae8ead59fd8013122f449a982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4719368
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Bryan Yu <yuholong@google.com>
    Reviewed-by: Youkichi Hosoi <youkichihosoi@chromium.org>
    Auto-Submit: Bryan Yu <yuholong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1178425}

--- chrome/browser/ash/arc/session/arc_activation_necessity_checker.cc
+++ chrome/browser/ash/arc/session/arc_activation_necessity_checker.cc
@@ -10,6 +10,7 @@
 #include "ash/components/arc/arc_prefs.h"
 #include "ash/components/arc/arc_util.h"
 #include "ash/components/arc/session/adb_sideloading_availability_delegate.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ash/arc/policy/arc_policy_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -30,26 +31,20 @@ void ArcActivationNecessityChecker::Check(CheckCallback callback) {
   // If ARC is running in a container, it's already started on the login screen
   // as a mini instance. In that case, just activate it.
   if (!IsArcVmEnabled()) {
-    std::move(callback).Run(true);
-    return;
-  }
-
-  // If the ARC on Demand feature is disabled, always activate ARC.
-  if (!base::FeatureList::IsEnabled(kArcOnDemandFeature)) {
-    std::move(callback).Run(true);
+    OnChecked(std::move(callback), true);
     return;
   }
 
   // Always activate ARC for unmanaged users who will get some applications
   // installed by Play Auto Install (PAI).
   if (!policy_util::IsAccountManaged(profile_)) {
-    std::move(callback).Run(true);
+    OnChecked(std::move(callback), true);
     return;
   }
 
   // Activate ARC if the package list held by ArcAppListPrefs is not up to date.
   if (!profile_->GetPrefs()->GetBoolean(arc::prefs::kArcPackagesIsUpToDate)) {
-    std::move(callback).Run(true);
+    OnChecked(std::move(callback), true);
     return;
   }
 
@@ -60,7 +55,7 @@ void ArcActivationNecessityChecker::Check(CheckCallback callback) {
   for (const auto& package_name : package_names) {
     auto package = app_list->GetPackage(package_name);
     if (package && !package->preinstalled) {
-      std::move(callback).Run(true);
+      OnChecked(std::move(callback), true);
       return;
     }
   }
@@ -68,7 +63,19 @@ void ArcActivationNecessityChecker::Check(CheckCallback callback) {
   // If ADB sideloading is enabled, activate ARC. Otherwise, no need to
   // activate.
   adb_sideloading_availability_delegate_->CanChangeAdbSideloading(
-      std::move(callback));
+      base::BindOnce(&ArcActivationNecessityChecker::OnChecked,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void ArcActivationNecessityChecker::OnChecked(CheckCallback callback,
+                                              bool result) {
+  base::UmaHistogramBoolean("Arc.DelayedActivation.ActivationShouldBeDelayed",
+                            !result);
+  // If the ARC on Demand feature is disabled, always activate ARC.
+  if (!base::FeatureList::IsEnabled(kArcOnDemandFeature)) {
+    result = true;
+  }
+  std::move(callback).Run(result);
 }
 
 }  // namespace arc
