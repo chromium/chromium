19fdec19317884bacf8f48fce2f0c222d0497caa
Haxatron Sec <haxatron1@gmail.com>
2023-08-03 17:44:55 +0000

    Account for scroll in cursor checks
    Use the correct transform for frame rect to account for scroll.
    
    Video: https://bugs.chromium.org/p/chromium/issues/detail?id=1454515#c18
    
    Fixed: 1454515
    Change-Id: I996bfaaaddc22055f7f350c40357625c420141e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4728774
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179139}

--- third_party/blink/renderer/core/input/event_handler.cc
+++ third_party/blink/renderer/core/input/event_handler.cc
@@ -597,8 +597,10 @@ absl::optional<ui::Cursor> EventHandler::SelectCursor(
             PhysicalOffset(hot_spot);
         PhysicalRect cursor_rect(cursor_offset,
                                  PhysicalSize::FromSizeFFloor(size));
-        if (!PhysicalRect(page->GetVisualViewport().VisibleContentRect())
-                 .Contains(cursor_rect)) {
+        PhysicalRect frame_rect(page->GetVisualViewport().VisibleContentRect());
+        frame_->ContentLayoutObject()->MapToVisualRectInAncestorSpace(
+            nullptr, frame_rect);
+        if (!frame_rect.Contains(cursor_rect)) {
           continue;
         }
       }
