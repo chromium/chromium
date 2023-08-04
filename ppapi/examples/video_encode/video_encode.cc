192c07cdd60cf53311f36ce47d00bfbe782412b3
Peter Kasting <pkasting@chromium.org>
2023-08-04 23:09:52 +0000

    Ban [w]ctype.h: ppapi/
    Bug: 1361094
    Change-Id: I308905037bed0b2403da6dc5f589b86d2c30f9b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749599
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Owners-Override: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179913}

--- ppapi/examples/video_encode/video_encode.cc
+++ ppapi/examples/video_encode/video_encode.cc
@@ -53,8 +53,10 @@ double clamp(double min, double max, double value) {
 
 std::string ToUpperString(const std::string& str) {
   std::string ret;
-  for (uint32_t i = 0; i < str.size(); i++)
-    ret.push_back(static_cast<char>(toupper(str[i])));
+  ret.reserve(str.size());
+  std::transform(str.cbegin(), str.cend(), std::back_inserter(ret), [](char c) {
+    return (c < 'a' || c > 'z') ? c : (c + 'A' - 'a');
+  });
   return ret;
 }
 
