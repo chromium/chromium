a19b91642882c83a17d60f03229f3910861bfd1a
Nigel Tao <nigeltao@chromium.org>
2023-08-04 01:32:38 +0000

    [File tasks] tweak IsFilesAppUrlOpener arg types
    Bug: None
    Test: CQ
    Change-Id: Iba9845fc58df24371d4acd1810960e16f0d8725b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4746639
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Auto-Submit: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179385}

--- chrome/browser/ash/file_manager/app_service_file_tasks.cc
+++ chrome/browser/ash/file_manager/app_service_file_tasks.cc
@@ -183,19 +183,18 @@ std::string ToSwaActionId(const std::string& action_id) {
          action_id;
 }
 
-// True if |launch_entry| represents a task which opens the file by getting the
-// URL for a file rather than by opening the local contents of the file.
-bool IsFilesAppUrlOpener(const apps::IntentLaunchInfo& launch_entry) {
-  if (launch_entry.app_id != kFileManagerSwaAppId) {
+// True if |app_id| and |action_id| represent a task which opens the file by
+// getting the URL for a file rather than by opening the local contents of the
+// file.
+bool IsFilesAppUrlOpener(const std::string& app_id,
+                         const std::string& action_id) {
+  if (app_id != kFileManagerSwaAppId) {
     return false;
   }
-  return launch_entry.activity_name == ToSwaActionId(kActionIdOpenInOffice) ||
-         launch_entry.activity_name ==
-             ToSwaActionId(kActionIdWebDriveOfficeWord) ||
-         launch_entry.activity_name ==
-             ToSwaActionId(kActionIdWebDriveOfficeExcel) ||
-         launch_entry.activity_name ==
-             ToSwaActionId(kActionIdWebDriveOfficePowerPoint);
+  return action_id == ToSwaActionId(kActionIdOpenInOffice) ||
+         action_id == ToSwaActionId(kActionIdWebDriveOfficeWord) ||
+         action_id == ToSwaActionId(kActionIdWebDriveOfficeExcel) ||
+         action_id == ToSwaActionId(kActionIdWebDriveOfficePowerPoint);
 }
 
 bool IsSystemAppIdWithFileHandlers(base::StringPiece id) {
@@ -280,7 +279,8 @@ void FindAppServiceTasks(Profile* profile,
       // tasks which only access the file via URL.
       if (has_non_native_file &&
           !(IsSystemAppIdWithFileHandlers(launch_entry.app_id) ||
-            IsFilesAppUrlOpener(launch_entry))) {
+            IsFilesAppUrlOpener(launch_entry.app_id,
+                                launch_entry.activity_name))) {
         continue;
       }
 
