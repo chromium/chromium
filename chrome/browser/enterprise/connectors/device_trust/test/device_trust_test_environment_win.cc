693befc28b7f0b20b9ef2d2b66b00b674ab97ae6
Paul Adedeji <pauladedeji@google.com>
2023-08-04 22:01:09 +0000

    Revert "Do not override HKLM for browser test in DeviceTrustTests"
    This reverts commit 6773d8873ec72c25b64b755d75ad765342a35561.
    
    Reason for revert: Breaks builder "chromium/ci/Win10 Tests x64" and builder "chromium/ci/Win10 Tests x64"
    
    First failing build:https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/83356/overview
    
    Original change's description:
    > Do not override HKLM for browser test in DeviceTrustTests
    >
    > It is not permitted to override HKLM in a browser_test as this can
    > interfere with browser child process launches.
    >
    > This CL changes the test to require running as Admin in order
    > to pass, otherwise the test is skipped.
    >
    > BUG=1470075,1470095
    >
    > Change-Id: I7801e0062b98c9e463c6a889e347b555bea2e0eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4749725
    > Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1179670}
    
    BUG=1470075,1470095
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: Iba255516fb4b0e267eebe1c0bd821cc1f1bd51bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752258
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Owners-Override: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1179855}

--- chrome/browser/enterprise/connectors/device_trust/test/device_trust_test_environment_win.cc
+++ chrome/browser/enterprise/connectors/device_trust/test/device_trust_test_environment_win.cc
@@ -13,6 +13,7 @@
 #include "base/command_line.h"
 #include "base/notreached.h"
 #include "base/task/thread_pool.h"
+#include "base/win/registry.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/win_key_rotation_command.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/network/mock_key_network_delegate.h"
 #include "chrome/browser/enterprise/connectors/device_trust/key_management/core/persistence/key_persistence_delegate.h"
@@ -93,6 +94,7 @@ DeviceTrustTestEnvironmentWin::DeviceTrustTestEnvironmentWin()
     : DeviceTrustTestEnvironment("device_trust_test_environment_win",
                                  kSuccessCode),
       install_details_(true) {
+  registry_override_manager_.OverrideRegistry(HKEY_LOCAL_MACHINE);
   KeyRotationCommandFactory::SetFactoryInstanceForTesting(this);
 }
 
