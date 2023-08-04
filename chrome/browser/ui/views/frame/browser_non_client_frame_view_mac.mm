0cf0e3be4579e03f598f7c1c8f674417ab1b2701
David Pennington <dpenning@chromium.org>
2023-08-04 21:54:42 +0000

    fix MacOS catalina and older stoplight sizes
    10.15 is still supported. Adding back in the original measurements from
    ccrev.com/4675108 for frame buttons.
    
    Bug: 1469094
    Change-Id: I397f7d3a59296f5587f0cf183157ddf67a1bfd15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4738755
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1179844}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.mm
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.mm
@@ -53,6 +53,7 @@ constexpr int kResizeHandleHeight = 1;
 
 // Empirical measurements of the traffic lights.
 constexpr int kCaptionButtonsWidth = 52;
+constexpr int kCaptionButtonsInsetsCatalinaOrOlder = 70;
 constexpr int kCaptionButtonsLeadingPadding = 20;
 
 FullscreenToolbarStyle GetUserPreferredToolbarStyle(bool always_show) {
@@ -451,9 +452,11 @@ void BrowserNonClientFrameViewMac::PaintChildren(const views::PaintInfo& info) {
 }
 
 gfx::Insets BrowserNonClientFrameViewMac::GetCaptionButtonInsets() const {
-  const int kCaptionButtonInset = kCaptionButtonsWidth +
-                                  (kCaptionButtonsLeadingPadding * 2) -
-                                  TabStyle::Get()->GetBottomCornerRadius();
+  const int kCaptionButtonInset =
+      base::mac::IsOS10_15()
+          ? kCaptionButtonsInsetsCatalinaOrOlder
+          : (kCaptionButtonsWidth + (kCaptionButtonsLeadingPadding * 2) -
+             TabStyle::Get()->GetBottomCornerRadius());
   if (CaptionButtonsOnLeadingEdge()) {
     return gfx::Insets::TLBR(0, kCaptionButtonInset, 0, 0);
   } else {
