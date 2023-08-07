67c8295db3e82fb62ec4f057df07e843ab5f8056
Peter Kasting <pkasting@chromium.org>
2023-08-07 17:23:19 +0000

    Use new facilities for base::expected: components/services/
    Bug: 1424513
    Change-Id: I7907f1d33db06c2a7a7021f8599c68795ebff98b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754779
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180379}

--- components/services/storage/public/cpp/filesystem/filesystem_proxy.cc
+++ components/services/storage/public/cpp/filesystem/filesystem_proxy.cc
@@ -13,6 +13,7 @@
 #include "base/files/important_file_writer.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/types/expected.h"
+#include "base/types/expected_macros.h"
 #include "build/build_config.h"
 #include "components/services/storage/public/cpp/filesystem/filesystem_impl.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -303,12 +304,10 @@ base::FileErrorOr<std::unique_ptr<FilesystemProxy::FileLock>>
 FilesystemProxy::LockFile(const base::FilePath& path) {
   if (!remote_directory_) {
     base::FilePath full_path = MaybeMakeAbsolute(path);
-    base::FileErrorOr<base::File> result =
-        FilesystemImpl::LockFileLocal(full_path);
-    if (!result.has_value())
-      return base::unexpected(result.error());
+    ASSIGN_OR_RETURN(base::File result,
+                     FilesystemImpl::LockFileLocal(full_path));
     return std::make_unique<LocalFileLockImpl>(std::move(full_path),
-                                               std::move(result.value()));
+                                               std::move(result));
   }
 
   mojo::PendingRemote<mojom::FileLock> remote_lock;
