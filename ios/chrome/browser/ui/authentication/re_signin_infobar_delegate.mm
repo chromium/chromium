90ee4d4798d5145966f799dcc7e1d80fea93acab
Jérôme Lebel <jlebel@chromium.org>
2023-08-07 22:38:08 +0000

    [iOS][UNO] Show new infobar when account removed
    Update the infobar message when kReplaceSyncPromosWithSignInPromos is
    enabled.
    This patch adds: IDS_IOS_SYNC_LOGIN_INFO_OUT_OF_DATE_WITH_UNO
    
    Change-Id: Idf940bd5cadf35ad0051628400df7c7d3cbd5f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752867
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180579}

--- ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
+++ ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
@@ -9,10 +9,12 @@
 #import <memory>
 #import <utility>
 
+#import "base/feature_list.h"
 #import "base/logging.h"
 #import "components/infobars/core/infobar_manager.h"
 #import "components/signin/public/base/signin_metrics.h"
 #import "components/strings/grit/components_strings.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/infobars/infobar_ios.h"
 #import "ios/chrome/browser/infobars/infobar_manager_impl.h"
 #import "ios/chrome/browser/infobars/infobar_utils.h"
@@ -22,6 +24,7 @@
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/signin_presenter.h"
+#import "ios/chrome/grit/ios_chromium_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 #import "ui/base/models/image_model.h"
@@ -111,7 +114,11 @@ ReSignInInfoBarDelegate::GetIdentifier() const {
 }
 
 std::u16string ReSignInInfoBarDelegate::GetMessageText() const {
-  return l10n_util::GetStringUTF16(IDS_IOS_SYNC_LOGIN_INFO_OUT_OF_DATE);
+  return base::FeatureList::IsEnabled(
+             syncer::kReplaceSyncPromosWithSignInPromos)
+             ? l10n_util::GetStringUTF16(
+                   IDS_IOS_SYNC_LOGIN_INFO_OUT_OF_DATE_WITH_UNO)
+             : l10n_util::GetStringUTF16(IDS_IOS_SYNC_LOGIN_INFO_OUT_OF_DATE);
 }
 
 int ReSignInInfoBarDelegate::GetButtons() const {
