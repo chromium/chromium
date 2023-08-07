da025accf65748c547c7384d6cc2d8fa7fd6bb51
Roman Arora <romanarora@chromium.org>
2023-08-07 20:51:59 +0000

    [ntp-journeys] V2 Quests integration tests
    Updates history clusters tile navigation test logic to work with the
    Modules V2 redesigned feature. Additional tests will follow.
    
    Bug: 1445233
    Change-Id: Iaf93570fa48bbfb81100774d6ba2bc955869fcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4664095
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180529}

--- chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
+++ chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
@@ -13,42 +13,22 @@
 namespace {
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewTabPageElementId);
 DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementReadyEvent);
+DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementChildrenReadyEvent);
 
 const char kMainHistoryClusterTileLinkUrl[] =
     "https://store.google.com/product/pixel_7?hl=en-US";
 const char kHistoryClustersCartTileLinkUrl[] = "https://store.google.com/cart";
+const std::string kHistoryClusterSuggestTileLinkUrl =
+    "https://www.google.com/search?q=new%20google%20products";
 }  // namespace
 
-class NewTabPageModulesInteractiveUiTest : public InteractiveBrowserTest {
+class NewTabPageModulesInteractiveUiBaseTest : public InteractiveBrowserTest {
  public:
-  NewTabPageModulesInteractiveUiTest() = default;
-  ~NewTabPageModulesInteractiveUiTest() override = default;
-  NewTabPageModulesInteractiveUiTest(
-      const NewTabPageModulesInteractiveUiTest&) = delete;
-  void operator=(const NewTabPageModulesInteractiveUiTest&) = delete;
-
-  void SetUp() override {
-    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
-    // TODO(crbug.com/1430493): This test should include signing into an
-    // account.
-    base::CommandLine::ForCurrentProcess()->AppendSwitch(
-        switches::kSignedOutNtpModulesSwitch);
-    const unsigned long kNumClusters = 1;
-    const unsigned long kNumVisits = 2;
-    const unsigned long kNumVisitsWithImages = 2;
-    features.InitWithFeaturesAndParameters(
-        {
-            {ntp_features::kNtpHistoryClustersModule,
-             {{ntp_features::kNtpHistoryClustersModuleDataParam,
-               base::StringPrintf("%lu,%lu,%lu", kNumClusters, kNumVisits,
-                                  kNumVisitsWithImages)}}},
-            {ntp_features::kNtpChromeCartInHistoryClusterModule,
-             {{ntp_features::kNtpChromeCartInHistoryClustersModuleDataParam,
-               "6"}}},
-        },
-        {});
-    InteractiveBrowserTest::SetUp();
-  }
+  NewTabPageModulesInteractiveUiBaseTest() = default;
+  ~NewTabPageModulesInteractiveUiBaseTest() override = default;
+  NewTabPageModulesInteractiveUiBaseTest(
+      const NewTabPageModulesInteractiveUiBaseTest&) = delete;
+  void operator=(const NewTabPageModulesInteractiveUiBaseTest&) = delete;
 
   void SetUpOnMainThread() override {
     InteractiveBrowserTest::SetUpOnMainThread();
@@ -81,48 +61,179 @@ class NewTabPageModulesInteractiveUiTest : public InteractiveBrowserTest {
     return WaitForStateChange(kNewTabPageElementId, std::move(tile_loaded));
   }
 
+  InteractiveTestApi::MultiStep WaitForElementToLoad(const DeepQuery& element) {
+    StateChange element_loaded;
+    element_loaded.event = kElementReadyEvent;
+    element_loaded.type = StateChange::Type::kExists;
+    element_loaded.where = element;
+    return WaitForStateChange(kNewTabPageElementId, std::move(element_loaded));
+  }
+
+  InteractiveTestApi::MultiStep WaitForElementChildElementCount(
+      const DeepQuery& element,
+      int count) {
+    StateChange element_loaded;
+    element_loaded.event = kElementChildrenReadyEvent;
+    element_loaded.type = StateChange::Type::kExistsAndConditionTrue;
+    element_loaded.where = element;
+    element_loaded.test_function = base::StringPrintf(
+        "(el) => { return el.childElementCount == %d; }", count);
+    return WaitForStateChange(kNewTabPageElementId, std::move(element_loaded));
+  }
+
+  InteractiveTestApi::MultiStep WaitForElementStyleSet(
+      const DeepQuery& element) {
+    StateChange element_loaded;
+    element_loaded.event = kElementReadyEvent;
+    element_loaded.type = StateChange::Type::kExistsAndConditionTrue;
+    element_loaded.where = element;
+    element_loaded.test_function = "(el) => { return el.style.length > 0; }";
+    return WaitForStateChange(kNewTabPageElementId, std::move(element_loaded));
+  }
+
   InteractiveTestApi::MultiStep ClickElement(
       const ui::ElementIdentifier& contents_id,
       const DeepQuery& element) {
     return Steps(MoveMouseTo(contents_id, element), ClickMouse());
   }
 
- private:
+ protected:
   base::test::ScopedFeatureList features;
 };
 
-IN_PROC_BROWSER_TEST_F(NewTabPageModulesInteractiveUiTest,
+class NewTabPageModulesInteractiveUiTest
+    : public NewTabPageModulesInteractiveUiBaseTest,
+      public testing::WithParamInterface<bool> {
+ public:
+  NewTabPageModulesInteractiveUiTest() = default;
+  ~NewTabPageModulesInteractiveUiTest() override = default;
+  NewTabPageModulesInteractiveUiTest(
+      const NewTabPageModulesInteractiveUiTest&) = delete;
+  void operator=(const NewTabPageModulesInteractiveUiTest&) = delete;
+
+  void SetUp() override {
+    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
+    // TODO(crbug.com/1430493): This test should include signing into an
+    // account.
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kSignedOutNtpModulesSwitch);
+    const unsigned long kNumClusters = 1;
+    const unsigned long kNumVisits = 2;
+    const unsigned long kNumVisitsWithImages = 2;
+
+    std::vector<base::test::FeatureRefAndParams> enabled_features;
+    std::vector<base::test::FeatureRef> disabled_features;
+
+    enabled_features.push_back(
+        {ntp_features::kNtpHistoryClustersModule,
+         {{ntp_features::kNtpHistoryClustersModuleDataParam,
+           base::StringPrintf("%lu,%lu,%lu", kNumClusters, kNumVisits,
+                              kNumVisitsWithImages)}}});
+    enabled_features.push_back(
+        {ntp_features::kNtpChromeCartInHistoryClusterModule,
+         {{ntp_features::kNtpChromeCartInHistoryClustersModuleDataParam,
+           "6"}}});
+
+    if (Redesigned()) {
+      enabled_features.push_back({ntp_features::kNtpModulesRedesigned, {}});
+    } else {
+      disabled_features.push_back(ntp_features::kNtpModulesRedesigned);
+    }
+
+    features.InitWithFeaturesAndParameters(std::move(enabled_features),
+                                           std::move(disabled_features));
+    InteractiveBrowserTest::SetUp();
+  }
+
+  bool Redesigned() const { return GetParam(); }
+};
+
+IN_PROC_BROWSER_TEST_P(NewTabPageModulesInteractiveUiTest,
                        ClickingHistoryClustersTileNavigatesToCorrectPage) {
-  const DeepQuery kMainHistoryClusterTileLink = {"ntp-app",
-                                                 "ntp-modules",
-                                                 "ntp-module-wrapper",
-                                                 "ntp-history-clusters",
-                                                 ".main-tile",
-                                                 "a"};
+  const DeepQuery tileLink = {"ntp-app",
+                              "ntp-modules",
+                              "ntp-module-wrapper",
+                              "ntp-history-clusters",
+                              ".main-tile",
+                              "a"};
+  const DeepQuery tileLinkRedesigned = {"ntp-app",
+                                        "ntp-modules-v2",
+                                        "ntp-module-wrapper",
+                                        "ntp-history-clusters-redesigned",
+                                        "ntp-history-clusters-visit-tile",
+                                        "a"};
+  const DeepQuery kHistoryClusterVisitTileLink =
+      Redesigned() ? tileLinkRedesigned : tileLink;
+
   RunTestSequence(
       // 1. Wait for new tab page to load.
       LoadNewTabPage(),
       // 2. Wait for tile to load.
-      WaitForLinkToLoad(kMainHistoryClusterTileLink,
+      WaitForLinkToLoad(kHistoryClusterVisitTileLink,
                         kMainHistoryClusterTileLinkUrl),
       // 3. Scroll to tile. Modules may sometimes load below the fold.
-      ScrollIntoView(kNewTabPageElementId, kMainHistoryClusterTileLink),
+      ScrollIntoView(kNewTabPageElementId, kHistoryClusterVisitTileLink),
       // 4. Click the tile.
-      ClickElement(kNewTabPageElementId, kMainHistoryClusterTileLink),
+      ClickElement(kNewTabPageElementId, kHistoryClusterVisitTileLink),
       // 5. Verify that the tab navigates to the tile's link.
       WaitForWebContentsNavigation(kNewTabPageElementId,
                                    GURL(kMainHistoryClusterTileLinkUrl)));
 }
 
-IN_PROC_BROWSER_TEST_F(NewTabPageModulesInteractiveUiTest,
-                       ClickingHistoryClustersCartTileNavigatesToCorrectPage) {
-  const DeepQuery kHistoryClustersCartTileLink = {
+IN_PROC_BROWSER_TEST_P(
+    NewTabPageModulesInteractiveUiTest,
+    ClickingHistoryClustersRelatedSearchNavigatesToCorrectPage) {
+  const DeepQuery relatedSearchLink = {"ntp-app",
+                                       "ntp-modules",
+                                       "ntp-module-wrapper",
+                                       "ntp-history-clusters",
+                                       "ntp-history-clusters-suggest-tile",
+                                       "a"};
+  const DeepQuery relatedSearchLinkRedesigned = {
       "ntp-app",
-      "ntp-modules",
+      "ntp-modules-v2",
       "ntp-module-wrapper",
-      "ntp-history-clusters",
-      "ntp-history-clusters-cart-tile",
+      "ntp-history-clusters-redesigned",
+      "ntp-history-clusters-suggest-tile-v2",
       "a"};
+  const DeepQuery kHistoryClusterRelatedSearchLink =
+      Redesigned() ? relatedSearchLinkRedesigned : relatedSearchLink;
+
+  RunTestSequence(
+      // 1. Wait for new tab page to load.
+      LoadNewTabPage(),
+      // 2. Wait for tile to load.
+      WaitForLinkToLoad(kHistoryClusterRelatedSearchLink,
+                        kHistoryClusterSuggestTileLinkUrl.c_str()),
+      // 3. Scroll to tile. Modules may sometimes load below the fold.
+      ScrollIntoView(kNewTabPageElementId, kHistoryClusterRelatedSearchLink),
+      // 4. Click the tile.
+      ClickElement(kNewTabPageElementId, kHistoryClusterRelatedSearchLink),
+      // 5. Verify that the tab navigates to the tile's link.
+      WaitForWebContentsNavigation(
+          kNewTabPageElementId,
+          GURL(kHistoryClusterSuggestTileLinkUrl.c_str())));
+}
+
+IN_PROC_BROWSER_TEST_P(NewTabPageModulesInteractiveUiTest,
+                       ClickingHistoryClustersCartTileNavigatesToCorrectPage) {
+  const DeepQuery tileLink = {"ntp-app",
+                              "ntp-modules",
+                              "ntp-module-wrapper",
+                              "ntp-history-clusters",
+                              "ntp-history-clusters-cart-tile",
+                              "a"};
+
+  const DeepQuery kHistoryClustersCartTileLinkRedesigned = {
+      "ntp-app",
+      "ntp-modules-v2",
+      "ntp-module-wrapper",
+      "ntp-history-clusters-redesigned",
+      "ntp-history-clusters-cart-tile-v2",
+      "a"};
+
+  const DeepQuery kHistoryClustersCartTileLink =
+      Redesigned() ? kHistoryClustersCartTileLinkRedesigned : tileLink;
 
   RunTestSequence(
       // 1. Wait for new tab page to load.
@@ -138,3 +249,103 @@ IN_PROC_BROWSER_TEST_F(NewTabPageModulesInteractiveUiTest,
       WaitForWebContentsNavigation(kNewTabPageElementId,
                                    GURL(kHistoryClustersCartTileLinkUrl)));
 }
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         NewTabPageModulesInteractiveUiTest,
+                         ::testing::Bool());
+
+constexpr unsigned long kSampleNumClusters = 3;
+
+class NewTabPageModulesRedesignedInteractiveUiTest
+    : public NewTabPageModulesInteractiveUiBaseTest {
+ public:
+  NewTabPageModulesRedesignedInteractiveUiTest() = default;
+  ~NewTabPageModulesRedesignedInteractiveUiTest() override = default;
+  NewTabPageModulesRedesignedInteractiveUiTest(
+      const NewTabPageModulesRedesignedInteractiveUiTest&) = delete;
+  void operator=(const NewTabPageModulesRedesignedInteractiveUiTest&) = delete;
+
+  void SetUp() override {
+    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kSignedOutNtpModulesSwitch);
+
+    const unsigned long kNumVisits = 2;
+    const unsigned long kNumVisitsWithImages = 2;
+    features.InitWithFeaturesAndParameters(
+        {{ntp_features::kNtpHistoryClustersModule,
+          {{ntp_features::kNtpHistoryClustersModuleDataParam,
+            base::StringPrintf("%lu,%lu,%lu", kSampleNumClusters, kNumVisits,
+                               kNumVisitsWithImages)}}},
+         {ntp_features::kNtpChromeCartInHistoryClusterModule,
+          {{ntp_features::kNtpChromeCartInHistoryClustersModuleDataParam,
+            "6"}}},
+         {ntp_features::kNtpModulesRedesigned, {}}},
+        {});
+    InteractiveBrowserTest::SetUp();
+  }
+};
+
+// TODO(crbug.com/1470367): Enable the test on a compatible version skew.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_ClickingHideButtonDismissesCluster \
+  DISABLED_ClickingHideButtonDismissesCluster
+#else
+#define MAYBE_ClickingHideButtonDismissesCluster \
+  ClickingHideButtonDismissesCluster
+#endif
+IN_PROC_BROWSER_TEST_F(NewTabPageModulesRedesignedInteractiveUiTest,
+                       MAYBE_ClickingHideButtonDismissesCluster) {
+  const DeepQuery kModulesContainer = {"ntp-app", "ntp-modules-v2",
+                                       "#container"};
+  const DeepQuery kHistoryClustersMoreButton = {
+      "ntp-app",
+      "ntp-modules-v2",
+      "ntp-module-wrapper",
+      "ntp-history-clusters-redesigned",
+      "history-clusters-header-v2",
+      "ntp-module-header-v2",
+      "#menuButton"};
+  const DeepQuery kHistoryClustersMoreActionMenu = {
+      "ntp-app",
+      "ntp-modules-v2",
+      "ntp-module-wrapper",
+      "ntp-history-clusters-redesigned",
+      "history-clusters-header-v2",
+      "cr-action-menu",
+      "dialog"};
+  const DeepQuery kHistoryClustersHideButton = {
+      "ntp-app",
+      "ntp-modules-v2",
+      "ntp-module-wrapper",
+      "ntp-history-clusters-redesigned",
+      "history-clusters-header-v2",
+      "#dismissButton"};
+
+  RunTestSequence(
+      // 1. Wait for new tab page to load.
+      LoadNewTabPage(),
+      // 2. Wait for modules container to have an expected child count matching
+      // the test setup.
+      WaitForElementChildElementCount(kModulesContainer, kSampleNumClusters),
+      // 3. Ensure the NTP history clusters module "hide" button exists, and
+      // thus, that the module loaded successfully.
+      WaitForElementToLoad(kHistoryClustersHideButton),
+      // 4. Scroll to the "hide" element of the NTP history clusters module.
+      // Modules may sometimes load below the fold.
+      ScrollIntoView(kNewTabPageElementId, kHistoryClustersHideButton),
+      // 5. Click the "more actions" menu button of the NTP history clusters
+      // module.
+      ClickElement(kNewTabPageElementId, kHistoryClustersMoreButton),
+      // 6. Wait for history clusters menu dialog to be anchored.
+      WaitForElementStyleSet(kHistoryClustersMoreActionMenu),
+      // 7. Click the "hide" element of the NTP history clusters module.
+      ClickElement(kNewTabPageElementId, kHistoryClustersHideButton),
+      // 8. Wait for modules container to reflect an updated element count that
+      // resulted from dismissing a module.
+      WaitForElementChildElementCount(kModulesContainer,
+                                      kSampleNumClusters - 1));
+}
+
+// TODO(crbug.com/1434428): Add a test that asserts the 'Done' button dismisses
+// the card.
