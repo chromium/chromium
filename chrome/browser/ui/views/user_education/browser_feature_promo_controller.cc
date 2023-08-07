b723e78942f8f4e88525d663aa30d8e970e1f2d7
Dana Fried <dfried@chromium.org>
2023-08-07 19:07:07 +0000

    [User Education] Disable IPH during required privacy/legal interstitial
    Previously, an NTP promo for Panorama could show at the same time as a
    Privacy Sandbox modal dialog, which was creating a bad/confusing user
    experience. This change prevents all spontaneous IPH from showing when
    a required Privacy notice/consent dialog is queued or showing.
    
    This is a temporary workaround until Desktop User Education 2.0 brings
    a more full-featured contention model for IPH, required legal notices,
    and other non-IPH promotions. See go/desktop-user-education-2.0 for
    information about future plans in this area.
    
    (But note that the behavior *is* consistent with what is proposed in
    the 2.0 doc.)
    
    Bug: 1470839
    Change-Id: Ib4ea8cf88e3e0b063220c1c61cc7f73ce1c05a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4754618
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180462}

--- chrome/browser/ui/views/user_education/browser_feature_promo_controller.cc
+++ chrome/browser/ui/views/user_education/browser_feature_promo_controller.cc
@@ -8,6 +8,8 @@
 
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/grit/generated_resources.h"
@@ -66,6 +68,18 @@ bool BrowserFeaturePromoController::CanShowPromo(
   // See https://bugs.chromium.org/p/chromium/issues/detail?id=1128728#c30
   if (browser_view_->GetProfile()->IsIncognitoProfile())
     return false;
+
+  // Turn off IPH while a required privacy interstitial is visible or pending.
+  // TODO(dfried): with Desktop User Education 2.0, filtering of IPH may need to
+  // be more nuanced; also a contention scheme between required popups may be
+  // required. See go/desktop-user-education-2.0 for details.
+  auto* const privacy_sandbox_service =
+      PrivacySandboxServiceFactory::GetForProfile(browser_view_->GetProfile());
+  if (privacy_sandbox_service &&
+      privacy_sandbox_service->GetRequiredPromptType() !=
+          PrivacySandboxService::PromptType::kNone) {
+    return false;
+  }
 
   // Don't show IPH if the anchor view is in an inactive window.
   auto* const anchor_view = anchor_element->AsA<views::TrackedElementViews>();
