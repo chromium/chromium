a89dca27405903f50a6c7113206704645e899ea0
Bo Liu <boliu@chromium.org>
2023-08-07 01:10:39 +0000

    Invalidate shader cache when binary arch changes
    (cherry picked from commit 32f821edb4808d978022e7631c13918694fb945a)
    
    Bug: 1470074
    Change-Id: I52772206dfd25abe44968c24f4ed59378902b3a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752875
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1179858}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4756084
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5931@{#5}
    Cr-Branched-From: afeb22acfbd4017230c0a77a75b4e5eeabdf0bc3-refs/heads/main@{#1179742}

--- components/viz/host/gpu_host_impl.cc
+++ components/viz/host/gpu_host_impl.cc
@@ -13,6 +13,7 @@
 #include "base/no_destructor.h"
 #include "base/process/process_handle.h"
 #include "base/strings/strcat.h"
+#include "base/system/sys_info.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/thread_checker.h"
 #include "base/trace_event/trace_event.h"
@@ -31,10 +32,6 @@
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/build_info.h"
-#endif
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "base/system/sys_info.h"
 #endif
 
 #if BUILDFLAG(IS_WIN)
@@ -418,7 +415,8 @@ std::string GpuHostImpl::GetShaderPrefixKey() {
 
     shader_prefix_key_ = params_.product + "-" + info.gl_vendor + "-" +
                          info.gl_renderer + "-" + active_gpu.driver_version +
-                         "-" + active_gpu.driver_vendor;
+                         "-" + active_gpu.driver_vendor + "-" +
+                         base::SysInfo::ProcessCPUArchitecture();
 
 #if BUILDFLAG(IS_ANDROID)
     std::string build_fp =
