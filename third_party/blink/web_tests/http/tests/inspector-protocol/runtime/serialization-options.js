9250e8443bdf423947aabfdcd145bbbd7a02d3ef
Mathias Bynens <mathias@chromium.org>
2023-08-07 12:40:10 +0000

    Asynchronously kick off all serialization tests
    Instead of waiting for each individual test to complete before
    starting the next one, we now eagerly start all the tests.
    
    This reduces the test run time by a factor of 1.6× (from 729 ms
    down to 454 ms) as observed on my workstation using these
    commands:
    
    ```
    autoninja -C out/Release chrome blink_tests && \
    third_party/blink/tools/run_web_tests.py -t Release http/tests/inspector-protocol/runtime/serialization-options.js && \
    cat out/Release/layout-test-results/times_ms.json
    ```
    
    Before:
    
    ```
    {"http":{"tests":{"inspector-protocol":{"runtime":{"serialization-options.js":729}}}}}
    ```
    
    After:
    
    ```
    {"http":{"tests":{"inspector-protocol":{"runtime":{"serialization-options.js":454}}}}}
    ```
    
    Bug: chromium:1457850
    Change-Id: Ife22ef75384959730f61307b411dca97140c6a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4755526
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180235}

--- third_party/blink/web_tests/http/tests/inspector-protocol/runtime/serialization-options.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/runtime/serialization-options.js
@@ -1,76 +1,93 @@
-(async function (testRunner) {
-  const { dp, session } = await testRunner.startHTML(
+(async function(testRunner) {
+  const {dp} = await testRunner.startHTML(
     '<div some_attr_name="some_attr_value">some text<h2>some another text</h2></div>',
     'Tests `serialization` options.');
 
-  await dp.Runtime.enable();
-
-  // Test V8 primitives.
-  await testExpression("undefined");
-  await testExpression("null");
-  await testExpression("'some_string'");
-  await testExpression("'2'");
-  await testExpression("Number.NaN");
-  await testExpression("-0");
-  await testExpression("Infinity");
-  await testExpression("-Infinity");
-  await testExpression("3");
-  await testExpression("1.4");
-  await testExpression("true");
-  await testExpression("false");
-  await testExpression("42n");
-  // Test V8 non-primitives.
-  await testExpression("(Symbol('foo'))");
-  await testExpression("[1, 'foo', true, new RegExp(/foo/g), [1]]",);
-  await testExpression("({'foo': {'bar': 'baz'}, 'qux': 'quux'})",);
-  await testExpression("(()=>{})");
-  await testExpression("(function(){})");
-  await testExpression("(async ()=>{})");
-  await testExpression("(async function(){})");
-  await testExpression("new RegExp(/foo/g)");
-  await testExpression("new Date(1654004849000)");
-  await testExpression("new Map([[1, 2], ['foo', 'bar'], [true, false], ['baz', [1]]])",);
-  await testExpression("new Set([1, 'foo', true, [1], new Map([[1,2]])])");
-  await testExpression("new WeakMap()");
-  await testExpression("new WeakSet()");
-  await testExpression("new Error('SOME_ERROR_TEXT')");
-  await testExpression("Promise.resolve()");
-  await testExpression("new Int32Array()");
-  await testExpression("new ArrayBuffer()");
-  // Test DOM objects.
-  await testExpression("document.body")
-  await testExpression("window")
-  await testExpression("document.querySelector('body > div')")
-  await testExpression("document.querySelector('body > div').attributes[0]")
-  await testExpression("new URL('http://example.com')")
-
-  testRunner.completeTest();
-
-  async function testExpression(expression) {
-    await test(expression, {
-      serialization: "deep"
-    });
-    await test(expression, {
-      serialization: "deep",
-      maxDepth: 0
-    });
-    await test(expression, {
-      serialization: "deep",
-      maxDepth: 1
-    });
-    await test(expression, {
-      serialization: "deep",
-      maxDepth: 99
-    });
+  const ALL_TEST_LOGS = [];
+  function test(expression, serializationOptions) {
+    ALL_TEST_LOGS.push(runTest(expression, serializationOptions));
   }
 
-  async function test(expression, serializationOptions) {
-    testRunner.log(`Testing '${expression}' with ${JSON.stringify(serializationOptions)}`);
-
+  async function runTest(expression, serializationOptions) {
     const evalResult = await dp.Runtime.evaluate({
       expression,
-      serializationOptions
-    })
-    testRunner.log(evalResult?.result?.result?.deepSerializedValue ?? evalResult);
+      serializationOptions,
+    });
+    const logs = [
+      `Testing \`${expression}\` with ${JSON.stringify(serializationOptions)}`,
+      evalResult?.result?.result?.deepSerializedValue ?? evalResult,
+    ];
+    return logs;
   }
-})
+
+  async function runTests() {
+    for await (const logs of ALL_TEST_LOGS) {
+      const [description, result] = logs;
+      testRunner.log(description);
+      testRunner.log(result);
+    }
+  }
+
+  await dp.Runtime.enable();
+
+  // Test ECMAScript primitives.
+  testExpression('undefined');
+  testExpression('null');
+  testExpression('"some_string"');
+  testExpression('"2"');
+  testExpression('Number.NaN');
+  testExpression('-0');
+  testExpression('Infinity');
+  testExpression('-Infinity');
+  testExpression('3');
+  testExpression('1.4');
+  testExpression('true');
+  testExpression('false');
+  testExpression('42n');
+  // Test ECMAScript non-primitives.
+  testExpression('(Symbol("foo"))');
+  testExpression('[1, "foo", true, new RegExp(/foo/g), [1]]',);
+  testExpression('({"foo": {"bar": "baz"}, "qux": "quux"})',);
+  testExpression('(()=>{})');
+  testExpression('(function(){})');
+  testExpression('(async ()=>{})');
+  testExpression('(async function(){})');
+  testExpression('new RegExp(/foo/g)');
+  testExpression('new Date(1654004849000)');
+  testExpression('new Map([[1, 2], ["foo", "bar"], [true, false], ["baz", [1]]])',);
+  testExpression('new Set([1, "foo", true, [1], new Map([[1,2]])])');
+  testExpression('new WeakMap()');
+  testExpression('new WeakSet()');
+  testExpression('new Error("SOME_ERROR_TEXT")');
+  testExpression('Promise.resolve()');
+  testExpression('new Int32Array()');
+  testExpression('new ArrayBuffer()');
+  // Test non-ECMAScript platform objects.
+  testExpression('document.body')
+  testExpression('window')
+  testExpression('document.querySelector("body > div")')
+  testExpression('document.querySelector("body > div").attributes[0]')
+  testExpression('new URL("http://example.com/")')
+
+  function testExpression(expression) {
+    test(expression, {
+      serialization: 'deep',
+    });
+    test(expression, {
+      serialization: 'deep',
+      maxDepth: 0,
+    });
+    test(expression, {
+      serialization: 'deep',
+      maxDepth: 1,
+    });
+    test(expression, {
+      serialization: 'deep',
+      maxDepth: 99,
+    });
+  }
+
+  await runTests();
+
+  testRunner.completeTest();
+});
