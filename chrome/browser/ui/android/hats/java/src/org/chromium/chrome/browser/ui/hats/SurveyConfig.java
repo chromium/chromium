312b48c9b5a11df06662087412c62404c9930cde
Wenyu Fu <wenyufu@google.com>
2023-08-08 17:10:24 +0000

    [Survey] Add SurveyConfig for Android
    Bug: 1466724
    Change-Id: Ideef6ee0d6baff5ea6dc63545e09acf42f8c32d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4740560
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181001}

---
+++ chrome/browser/ui/android/hats/java/src/org/chromium/chrome/browser/ui/hats/SurveyConfig.java
@@ -0,0 +1,120 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.hats;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Java equivalent of C++ survey::SurveyConfig. All member variable are kept package protected,
+ * as they should not be access outside of survey code.
+ * To add a new SurveyConfig, see //chrome/browser/ui/hats/survey_config.*
+ */
+@JNINamespace("hats")
+public class SurveyConfig {
+    /**
+     * Unique key associate with the config.
+     */
+    final String mTrigger;
+    /**
+     * TriggerId used to download & present the associated survey from HaTS service.
+     */
+    final String mTriggerId;
+
+    /** Probability [0,1] of how likely a chosen user will see the survey. */
+    final double mProbability;
+    /**
+     * Whether the survey will prompt every time because the user has explicitly decided to take the
+     * survey e.g. clicking a link.
+     */
+    final boolean mUserPrompted;
+    /**
+     * Product Specific Bit Data fields which are sent with the survey response.
+     */
+    final String[] mPsdBitDataFields;
+    /**
+     * Product Specific String Data fields which are sent with the survey response.
+     */
+    final String[] mPsdStringDataFields;
+
+    /**
+     * Get the survey config with the associate trigger if the survey config exists and enabled;
+     * returns null otherwise.
+     *
+     * @param trigger The trigger associated with the SurveyConfig.
+     * @return SurveyConfig if the survey exists and is enabled.
+     */
+    @Nullable
+    public static SurveyConfig get(String trigger) {
+        return Holder.getInstance().getSurveyConfig(trigger);
+    }
+
+    /** Clear all the initialized configs. */
+    public static void clearAll() {
+        Holder.getInstance().destroy();
+    }
+
+    /** Not generated from java. */
+    private SurveyConfig(String trigger, String triggerId, double probability, boolean userPrompted,
+            String[] psdBitDataFields, String[] psdStringDataFields) {
+        mTrigger = trigger;
+        mTriggerId = triggerId;
+        mProbability = probability;
+        mUserPrompted = userPrompted;
+        mPsdBitDataFields = psdBitDataFields;
+        mPsdStringDataFields = psdStringDataFields;
+    }
+
+    @CalledByNative
+    private static void addActiveSurveyConfigToHolder(Holder holder, String trigger,
+            String triggerId, double probability, boolean userPrompted, String[] psdBitDataFields,
+            String[] psdStringDataFields) {
+        holder.mTriggers.put(trigger,
+                new SurveyConfig(trigger, triggerId, probability, userPrompted, psdBitDataFields,
+                        psdStringDataFields));
+    }
+
+    /**
+     * Holder that stores all the active surveys for Android.
+     */
+    static class Holder {
+        private static Holder sInstance;
+        private long mNativeInstance;
+        private Map<String, SurveyConfig> mTriggers;
+
+        static Holder getInstance() {
+            if (sInstance == null) {
+                sInstance = new Holder();
+            }
+            return sInstance;
+        }
+
+        private Holder() {
+            mTriggers = new HashMap<>();
+            mNativeInstance = SurveyConfigJni.get().initHolder(this);
+        }
+
+        SurveyConfig getSurveyConfig(String trigger) {
+            return mTriggers.get(trigger);
+        }
+
+        void destroy() {
+            SurveyConfigJni.get().destroy(mNativeInstance);
+            mNativeInstance = 0;
+        }
+    }
+
+    @NativeMethods
+    interface Natives {
+        long initHolder(Holder caller);
+        void destroy(long nativeSurveyConfigHolder);
+    }
+}
---
+++ chrome/browser/ui/android/hats/survey_config_android.cc
@@ -0,0 +1,66 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/android/hats/survey_config_android.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_array.h"
+#include "base/android/jni_string.h"
+#include "chrome/browser/ui/android/hats/jni_headers/SurveyConfig_jni.h"
+
+using base::android::AttachCurrentThread;
+using base::android::ConvertUTF8ToJavaString;
+using base::android::JavaParamRef;
+using base::android::ScopedJavaLocalRef;
+
+namespace hats {
+
+SurveyConfigHolder::SurveyConfigHolder(JNIEnv* env,
+                                       const JavaParamRef<jobject>& obj) {
+  jobj_.Reset(env, obj);
+  GetActiveSurveyConfigs(survey_configs_by_triggers_);
+  InitJavaHolder();
+}
+
+SurveyConfigHolder::~SurveyConfigHolder() = default;
+
+// Initialize the holder on the java side.
+void SurveyConfigHolder::InitJavaHolder() {
+  JNIEnv* env = AttachCurrentThread();
+
+  for (auto entry : survey_configs_by_triggers_) {
+    auto survey_config = entry.second;
+
+    ScopedJavaLocalRef<jstring> jtrigger =
+        ConvertUTF8ToJavaString(env, survey_config.trigger);
+    ScopedJavaLocalRef<jstring> jtrigger_id =
+        ConvertUTF8ToJavaString(env, survey_config.trigger_id);
+    ScopedJavaLocalRef<jobjectArray> jpsd_bits_data_fields =
+        base::android::ToJavaArrayOfStrings(
+            env, survey_config.product_specific_bits_data_fields);
+    ScopedJavaLocalRef<jobjectArray> jpsd_string_data_fields =
+        base::android::ToJavaArrayOfStrings(
+            env, survey_config.product_specific_string_data_fields);
+    jboolean juser_prompted = survey_config.user_prompted;
+    jdouble jprobability = survey_config.probability;
+    Java_SurveyConfig_addActiveSurveyConfigToHolder(
+        env, jobj_, jtrigger, jtrigger_id, jprobability, juser_prompted,
+        jpsd_bits_data_fields, jpsd_string_data_fields);
+  }
+}
+
+void SurveyConfigHolder::Destroy(JNIEnv* env) {
+  survey_configs_by_triggers_.clear();
+  jobj_.Reset();
+}
+
+// static
+jlong JNI_SurveyConfig_InitHolder(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& caller) {
+  SurveyConfigHolder* holder = new SurveyConfigHolder(env, caller);
+  return reinterpret_cast<intptr_t>(holder);
+}
+
+}  // namespace hats
