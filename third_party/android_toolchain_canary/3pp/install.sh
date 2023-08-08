607f22f1b0e1157b316571d89403b4fe16ef0b89
Nathaniel Manista <nathaniel@google.com>
2023-08-08 08:04:17 +0000

    Create //third_party/android_toolchain_canary
    As a 3pp this is mostly a fork of //third_party/android_toolchain but
    we aim the pointer at the latest tip-of-source-control
    (fresh-out-of-the-ci.android.com-oven just a few hours ago!) Android
    NDK rather than the latest *LTS* (stable) Android NDK.
    
    Bug: b/283156595
    Change-Id: I662b171fde5c8774d31ad95c995bd33fec345e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4716779
    Auto-Submit: Nathaniel Manista <nathaniel@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nathaniel Manista <nathaniel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1180734}

--- third_party/android_toolchain_canary/3pp/install.sh
+++ third_party/android_toolchain_canary/3pp/install.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# THIS MUST BE KEPT IN SYNC WITH ../../android_toolchain_canary/3pp/install.sh.
+# THIS MUST BE KEPT IN SYNC WITH ../../android_toolchain/3pp/install.sh.
 
 set -e
 set -x
