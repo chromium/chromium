6bb386bf2a6462eb2a33100833d564a2fd0f8225
Tsuyoshi Horo <horo@chromium.org>
2023-08-08 01:33:05 +0000

    Fix ExtensionLocalizationThrottle::WillProcessResponse
    Synchronous call of `delegate_->CancelWithError()` inside
    blink::URLLoaderThrottle::WillProcessResponse() in Blink can cause UAF.
    To fix this, this CL change ExtensionLocalizationThrottle::
    WillProcessResponse() to set `defer` to true, and asynchronously call
    `delegate_->CancelWithError()`.
    
    Bug: 1469754
    Change-Id: Ic129ba4b39a9c1ab00eb6609db824b065296e66d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4751579
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1180638}

--- extensions/renderer/extension_localization_throttle.cc
+++ extensions/renderer/extension_localization_throttle.cc
@@ -217,7 +217,14 @@ void ExtensionLocalizationThrottle::WillProcessResponse(
       mojo::CreateDataPipe(/*options=*/nullptr, producer_handle, body);
 
   if (create_pipe_result != MOJO_RESULT_OK || force_error_for_test_) {
-    delegate_->CancelWithError(net::ERR_INSUFFICIENT_RESOURCES, kCancelReason);
+    // Synchronous call of `delegate_->CancelWithError` can cause a UAF error.
+    // So defer the request here.
+    *defer = true;
+    base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE,
+        base::BindOnce(base::BindOnce(
+            &ExtensionLocalizationThrottle::DeferredCancelWithError,
+            weak_factory_.GetWeakPtr(), net::ERR_INSUFFICIENT_RESOURCES)));
     return;
   }
 
@@ -246,6 +253,12 @@ void ExtensionLocalizationThrottle::WillProcessResponse(
   loader_rawptr->Start(std::move(source_loader),
                        std::move(source_client_receiver), std::move(body),
                        std::move(producer_handle));
+}
+
+void ExtensionLocalizationThrottle::DeferredCancelWithError(int error_code) {
+  if (delegate_) {
+    delegate_->CancelWithError(error_code, kCancelReason);
+  }
 }
 
 }  // namespace extensions
