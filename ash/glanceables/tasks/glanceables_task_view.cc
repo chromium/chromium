4e8b200f102a9b4669e36d008ee464a7f79c30b2
Artsiom Mitrokhin <amitrokhin@google.com>
2023-08-09 14:31:16 +0000

    Add an icon indicating that a glanceables task has attached notes
    https://screenshot.googleplex.com/9umfXcCGDtrWfkx.png
    
    Bug: b/293887995
    Test: ash_pixeltests --gtest_filter="*GlanceablesTaskViewPixelTest*"
    Change-Id: I9af5fa3e1ea90421514b79ac5012dd92cceb27b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4760045
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181466}

--- ash/glanceables/tasks/glanceables_task_view.cc
+++ ash/glanceables/tasks/glanceables_task_view.cc
@@ -4,36 +4,36 @@
 
 #include "ash/glanceables/tasks/glanceables_task_view.h"
 
+#include <memory>
+#include <string>
+
 #include "ash/glanceables/glanceables_v2_controller.h"
 #include "ash/glanceables/tasks/glanceables_tasks_client.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
-#include "ash/style/ash_color_provider.h"
 #include "ash/style/typography.h"
 #include "ash/system/time/date_helper.h"
 #include "base/strings/string_util.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/color/color_provider.h"
 #include "ui/gfx/font.h"
-#include "ui/gfx/paint_vector_icon.h"
+#include "ui/gfx/vector_icon_types.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/button_controller.h"
 #include "ui/views/controls/image_view.h"
 
+namespace ash {
 namespace {
 
 constexpr int kIconSize = 20;
 constexpr char kFormatterPattern[] = "EEE, MMM d";  // "Wed, Feb 28"
 
 constexpr int kBackgroundRadius = 4;
-constexpr auto kTimeIconMargin = gfx::Insets::TLBR(0, 0, 0, 4);
-constexpr auto kSubtaskIconMargin = gfx::Insets::TLBR(0, 4, 0, 0);
+constexpr auto kSecondRowItemsMargin = gfx::Insets::TLBR(0, 0, 0, 4);
 
 constexpr auto kSingleRowButtonMargin = gfx::Insets::VH(13, 18);
 constexpr auto kDoubleRowButtonMargin = gfx::Insets::VH(16, 18);
@@ -60,16 +60,28 @@ std::u16string GetFormattedDueDate(const base::Time& due) {
     return l10n_util::GetStringUTF16(IDS_GLANCEABLES_DUE_TODAY);
   }
 
-  auto* const date_helper = ash::DateHelper::GetInstance();
+  auto* const date_helper = DateHelper::GetInstance();
   CHECK(date_helper);
   const auto formatter =
       date_helper->CreateSimpleDateFormatter(kFormatterPattern);
   return date_helper->GetFormattedTime(&formatter, due);
 }
 
-}  // namespace
+std::unique_ptr<views::ImageView> CreateSecondRowIcon(
+    const gfx::VectorIcon& icon) {
+  auto icon_view = std::make_unique<views::ImageView>();
+  icon_view->SetProperty(views::kMarginsKey, kSecondRowItemsMargin);
+  if (chromeos::features::IsJellyEnabled()) {
+    icon_view->SetImage(ui::ImageModel::FromVectorIcon(
+        icon, cros_tokens::kCrosSysOnSurfaceVariant));
+  } else {
+    icon_view->SetImage(
+        ui::ImageModel::FromVectorIcon(icon, kColorAshTextColorSecondary));
+  }
+  return icon_view;
+}
 
-namespace ash {
+}  // namespace
 
 class GlanceablesTaskView::CheckButton : public views::ImageButton {
  public:
@@ -148,12 +160,12 @@ GlanceablesTaskView::GlanceablesTaskView(const std::string& task_list_id,
 
   std::vector<std::u16string> details;
   if (task->due.has_value()) {
-    views::ImageView* time_icon_view =
-        tasks_details_view_->AddChildView(std::make_unique<views::ImageView>());
-    time_icon_view->SetProperty(views::kMarginsKey, kTimeIconMargin);
+    tasks_details_view_->AddChildView(
+        CreateSecondRowIcon(kGlanceablesTasksDueDateIcon));
 
     views::Label* due_date_label = SetupLabel(tasks_details_view_);
     due_date_label->SetText(GetFormattedDueDate(task->due.value()));
+    due_date_label->SetProperty(views::kMarginsKey, kSecondRowItemsMargin);
     // TODO(b/294681832): Finalize, and then localize strings.
     details.push_back(u"Due " + GetFormattedDueDate(task->due.value()));
     due_date_label->SetFontList(
@@ -163,12 +175,8 @@ GlanceablesTaskView::GlanceablesTaskView(const std::string& task_list_id,
         TypographyToken::kCrosAnnotation1));
 
     if (chromeos::features::IsJellyEnabled()) {
-      time_icon_view->SetImage(ui::ImageModel::FromVectorIcon(
-          kGlanceablesTasksDueDateIcon, cros_tokens::kCrosSysOnSurfaceVariant));
       due_date_label->SetEnabledColorId(cros_tokens::kCrosSysOnSurfaceVariant);
     } else {
-      time_icon_view->SetImage(ui::ImageModel::FromVectorIcon(
-          kGlanceablesTasksDueDateIcon, kColorAshTextColorSecondary));
       due_date_label->SetEnabledColorId(kColorAshTextColorSecondary);
     }
   }
@@ -176,16 +184,15 @@ GlanceablesTaskView::GlanceablesTaskView(const std::string& task_list_id,
   if (task->has_subtasks) {
     // TODO(b/294681832): Finalize, and then localize strings.
     details.push_back(u"Has subtasks");
-    views::ImageView* has_subtask_icon_view =
-        tasks_details_view_->AddChildView(std::make_unique<views::ImageView>());
-    has_subtask_icon_view->SetProperty(views::kMarginsKey, kSubtaskIconMargin);
-    if (chromeos::features::IsJellyEnabled()) {
-      has_subtask_icon_view->SetImage(ui::ImageModel::FromVectorIcon(
-          kGlanceablesSubtaskIcon, cros_tokens::kCrosSysOnSurfaceVariant));
-    } else {
-      has_subtask_icon_view->SetImage(ui::ImageModel::FromVectorIcon(
-          kGlanceablesSubtaskIcon, kColorAshTextColorSecondary));
-    }
+    tasks_details_view_->AddChildView(
+        CreateSecondRowIcon(kGlanceablesSubtaskIcon));
+  }
+
+  if (task->has_notes) {
+    // TODO(b/294681832): Finalize, and then localize strings.
+    details.push_back(u"Has notes");
+    tasks_details_view_->AddChildView(
+        CreateSecondRowIcon(kGlanceablesTasksNotesIcon));
   }
 
   // Use different margins depending on the number of
