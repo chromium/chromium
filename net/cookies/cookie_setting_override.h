16507a63fbd64436b15f3c9b32c5f13e3ab1d769
Joshua Hood <jdh@chromium.org>
2023-08-09 19:54:01 +0000

    Remove unused content setting from user bypass impl (3/3)
    This CL removes unused/abandoned code for a content setting that was
    introduced during the initial implementation work for UserBypass.
    
    This is CL 3 out of 3.
    
    Bug: 1386190
    Change-Id: I2f8fd0c21de9527c92f5ada2a1b738ae45f45495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4752763
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181650}

--- net/cookies/cookie_setting_override.h
+++ net/cookies/cookie_setting_override.h
@@ -15,19 +15,16 @@ namespace net {
 enum class CookieSettingOverride {
   kMinValue = 0,
 
-  // When specified, the user has indicated to force allowing third-party
-  // cookies.
-  kForceThirdPartyByUser = kMinValue,
   // When specified, third-party cookies may be allowed based on existence of
   // TopLevelStorageAccess grants.
-  kTopLevelStorageAccessGrantEligible = 1,
+  kTopLevelStorageAccessGrantEligible = kMinValue,
   // When present, the caller may use an existing Storage Access API grant (if
   // a matching grant exists) to access third-party cookies. Otherwise, Storage
   // Access API grants do not apply.
-  kStorageAccessGrantEligible = 2,
+  kStorageAccessGrantEligible = 1,
   // When present, 3PCD support settings (ContentSettingsType::TPCD_SUPPORT)
   // will be respected.
-  k3pcdSupport = 3,
+  k3pcdSupport = 2,
   kMaxValue = k3pcdSupport,
 };
 
