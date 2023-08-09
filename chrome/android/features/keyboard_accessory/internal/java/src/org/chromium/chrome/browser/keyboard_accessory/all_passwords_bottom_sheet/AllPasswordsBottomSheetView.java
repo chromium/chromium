66e724b0067b168a31ae14f3bcc96f7a00b02092
Ioana Pandele <ioanap@chromium.org>
2023-08-09 14:27:41 +0000

    [UPMCleanup] Remove branding checks from the select password sheet
    This is the bottom sheet that allows users to fill passwords saved for
    other websites.
    
    Bug: 1439191
    Change-Id: Id64d4f187634d24aa2e91587ec14350dc0870663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4765347
    Code-Coverage: Findit <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181464}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetView.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetView.java
@@ -4,8 +4,6 @@
 
 package org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet;
 
-import static org.chromium.chrome.browser.password_manager.PasswordManagerHelper.usesUnifiedPasswordManagerBranding;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -71,11 +69,6 @@ class AllPasswordsBottomSheetView implements BottomSheetContent {
         mSheetItemListView.setLayoutManager(new LinearLayoutManager(
                 mSheetItemListView.getContext(), LinearLayoutManager.VERTICAL, false));
         mSheetItemListView.setItemAnimator(null);
-        if (usesUnifiedPasswordManagerBranding()) {
-            // TODO(crbug.com/1217070): update the layout xml once feature is rolled out
-            final TextView titleTextView = mContentView.findViewById(R.id.sheet_title);
-            titleTextView.setText(R.string.all_passwords_bottom_sheet_title_gpm);
-        }
     }
 
     /**
@@ -106,12 +99,6 @@ class AllPasswordsBottomSheetView implements BottomSheetContent {
     void setWarning(CharSequence warningMessage) {
         final TextView warningTextView = mContentView.findViewById(R.id.sheet_warning);
         warningTextView.setText(warningMessage);
-        if (usesUnifiedPasswordManagerBranding()) {
-            // TODO(crbug.com/1217070): remove from the layout xml once feature roll out
-            final TextView warningSecondTextView =
-                    mContentView.findViewById(R.id.sheet_warning_second);
-            warningSecondTextView.setVisibility(View.GONE);
-        }
     }
 
     void setSheetItemListAdapter(RecyclerView.Adapter adapter) {
--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewTest.java
@@ -122,22 +122,6 @@ public class AllPasswordsBottomSheetViewTest {
 
     @Test
     @MediumTest
-    @Features.DisableFeatures({ChromeFeatureList.UNIFIED_PASSWORD_MANAGER_ANDROID,
-            ChromeFeatureList.UNIFIED_PASSWORD_MANAGER_ANDROID_BRANDING})
-    public void
-    testShowsWarningWithOriginByDefaultWithUpmDisabled() {
-        TestThreadUtils.runOnUiThreadBlocking(() -> mModel.set(VISIBLE, true));
-        pollUiThread(() -> getBottomSheetState() == SheetState.FULL);
-        assertThat(mAllPasswordsBottomSheetView.getContentView().isShown(), is(true));
-        assertEquals(mAllPasswordsBottomSheetView.getWarningText().toString(),
-                String.format(
-                        getString(R.string.all_passwords_bottom_sheet_warning_dialog_message_first),
-                        "m.example.com"));
-    }
-
-    @Test
-    @MediumTest
-    @Features.EnableFeatures(ChromeFeatureList.UNIFIED_PASSWORD_MANAGER_ANDROID)
     public void testShowsWarningWithOriginByDefaultWithUpmEnabled() {
         TestThreadUtils.runOnUiThreadBlocking(() -> mModel.set(VISIBLE, true));
         pollUiThread(() -> getBottomSheetState() == SheetState.FULL);
