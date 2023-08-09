30b484e483c198afdebd575e36ea14dd825485d2
Ashutosh Singhal <macinashutosh@google.com>
2023-08-09 16:34:10 +0000

    Browser test for persistent display settings in Managed guest session
    This test verifies that display properties persist for managed guest
    session. This test verifies that when enabled by the policy
    `DeviceAllowMGSToStoreDisplayProperties`, display settings will
    persist even after a session is restarted.
    This test covers a CUJ that MGS users can enable persistent display
    settings. More info: go/cros-cuj-tracker
    
    Bug: b/263339976
    Change-Id: Ieebd5e3f78b1eb636b601dbf4c54181431cddef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742509
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181527}

--- ash/display/display_prefs.cc
+++ ash/display/display_prefs.cc
@@ -13,6 +13,7 @@
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "base/check_is_test.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/strings/string_number_conversions.h"
@@ -886,6 +887,11 @@ void DisplayPrefs::StoreDisplayPowerStateForTest(
 
 void DisplayPrefs::LoadTouchAssociationPreferenceForTest() {
   LoadDisplayTouchAssociations(local_state_);
+}
+
+void DisplayPrefs::LoadDisplayPrefsForTest() {
+  CHECK_IS_TEST();
+  LoadDisplayPreferences();
 }
 
 void DisplayPrefs::StoreLegacyTouchDataForTest(
