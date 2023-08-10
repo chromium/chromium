6724da61e874ad44e581f4a34e827a649a0c6769
Ruma Kesh <rkesh@google.com>
2023-08-10 21:41:28 +0000

    CrOS Settings: Update import for CrInputElement in the test files.
    The element should be accessed from the 'os_settings' file in order to
    avoid the element from potentially being registered twice.
    
    Bug: b/270728282
    Test: browser_tests --gtest_filter="OSSettings*"
    Change-Id: Ic6bec0d818e68f394201c8d937dacf76dc647787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766836
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Ruma Kesh <rkesh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1182298}

--- chrome/test/data/webui/settings/chromeos/kerberos_page/kerberos_add_account_dialog_test.ts
+++ chrome/test/data/webui/settings/chromeos/kerberos_page/kerberos_add_account_dialog_test.ts
@@ -5,8 +5,7 @@
 import 'chrome://os-settings/lazy_load.js';
 
 import {KerberosAccount, KerberosAccountsBrowserProxyImpl, KerberosAddAccountDialogElement, KerberosConfigErrorCode, KerberosErrorType} from 'chrome://os-settings/lazy_load.js';
-import {CrButtonElement, CrCheckboxElement, CrDialogElement, CrTextareaElement} from 'chrome://os-settings/os_settings.js';
-import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import {CrButtonElement, CrCheckboxElement, CrDialogElement, CrInputElement, CrTextareaElement} from 'chrome://os-settings/os_settings.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertFalse, assertNotEquals, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
--- chrome/test/data/webui/settings/chromeos/os_files_page/smb_shares_page_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_files_page/smb_shares_page_test.ts
@@ -5,8 +5,7 @@
 import 'chrome://os-settings/lazy_load.js';
 
 import {AddSmbShareDialogElement, SettingsSmbSharesPageElement, SmbBrowserProxy, SmbBrowserProxyImpl, SmbMountResult} from 'chrome://os-settings/lazy_load.js';
-import {CrButtonElement} from 'chrome://os-settings/os_settings.js';
-import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import {CrButtonElement, CrInputElement} from 'chrome://os-settings/os_settings.js';
 import {assert} from 'chrome://resources/js/assert_ts.js';
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
