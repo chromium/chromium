b0c464cb6cb7665995a6c3edc4487ce039cd1201
Pierre St Juste <pstjuste@google.com>
2023-08-10 05:41:44 +0000

    Add additional UMA metrics + reliability fix.
    This CL adds UMA metrics for the side panel loader and adds a reliability fix which retries visual classification aftert 2 seconds.
    
    Bug: b/290980069
    Change-Id: Ic0540e76b4b05594a8efd20fafb20e269cf780f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4753328
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Pierre St Juste <pstjuste@google.com>
    Cr-Commit-Position: refs/heads/main@{#1181887}

--- chrome/browser/companion/visual_search/visual_search_classifier_host_unittest.cc
+++ chrome/browser/companion/visual_search/visual_search_classifier_host_unittest.cc
@@ -134,8 +134,8 @@ class VisualSearchClassifierHostTest : public ChromeRenderViewHostTestHarness {
 TEST_F(VisualSearchClassifierHostTest, StartClassification) {
   SetModelPath();
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {});
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {});
   visual_search_host_->StartClassification(
       web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
   base::RunLoop().RunUntilIdle();
@@ -157,8 +157,8 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_WithOverride) {
   base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
       switches::kVisualSearchConfigForCompanion, config_string);
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {});
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {});
   visual_search_host_->StartClassification(
       web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
   base::RunLoop().RunUntilIdle();
@@ -176,8 +176,8 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_WithOverride) {
 
 TEST_F(VisualSearchClassifierHostTest, StartClassification_NoModelSet) {
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {});
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {});
   visual_search_host_->StartClassification(
       web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
   base::RunLoop().RunUntilIdle();
@@ -197,8 +197,8 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_NoModelSet) {
 TEST_F(VisualSearchClassifierHostTest, StartClassification_WithInvalidModel) {
   SetInvalidModelPath();
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {});
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {});
   visual_search_host_->StartClassification(
       web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
   base::RunLoop().RunUntilIdle();
@@ -221,8 +221,8 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_WithInvalidModel) {
 TEST_F(VisualSearchClassifierHostTest, StartClassification_WithCancellation) {
   SetModelPath();
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {});
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {});
   visual_search_host_->StartClassification(
       web_contents()->GetPrimaryMainFrame(), url_, std::move(callback));
   GURL url("https://foo.bar");
@@ -243,8 +243,8 @@ TEST_F(VisualSearchClassifierHostTest, StartClassification_WithCancellation) {
 TEST_F(VisualSearchClassifierHostTest, HandleClassification) {
   SetModelPath();
   VisualSearchClassifierHost::ResultCallback callback =
-      base::BindOnce([](std::vector<std::string> results,
-                        const VisualSuggestionsMetrics& stats) {
+      base::BindOnce([](const std::vector<std::string> results,
+                        const VisualSuggestionsMetrics stats) {
         EXPECT_EQ(results.size(), 1U);
       });
   visual_search_host_->StartClassification(
