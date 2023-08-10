d08dbbaa926275ac0106d3ec9ca975310f5e297c
Daniel Andersson <dandersson@chromium.org>
2023-08-10 23:21:27 +0000

    saved_desks: Remove HighlightBorder on SavedDeskItem
    This border is covered by the background. Removing it results in zero
    visual difference, so let's do that. Same results regardless of whether
    Jelly is enabled or not.
    
    Change-Id: I3b57afaf9aafb44a13ca0da66a84e36078a2bce3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766939
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1182359}

--- ash/wm/desks/templates/saved_desk_item_view.cc
+++ ash/wm/desks/templates/saved_desk_item_view.cc
@@ -32,7 +32,6 @@
 #include "base/i18n/time_formatting.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/vector_icons/vector_icons.h"
 #include "ui/accessibility/ax_enums.mojom-shared.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -47,7 +46,6 @@
 #include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/highlight_border.h"
 #include "ui/views/layout/flex_layout_view.h"
 #include "ui/views/metadata/view_factory_internal.h"
 #include "ui/views/view.h"
@@ -130,11 +128,6 @@ SavedDeskItemView::SavedDeskItemView(std::unique_ptr<DeskTemplate> saved_desk)
       .SetUseDefaultFillLayout(true)
       .SetAccessibleName(saved_desk_name)
       .SetCallback(std::move(launch_template_callback))
-      .SetBorder(std::make_unique<views::HighlightBorder>(
-          kSaveDeskCornerRadius,
-          chromeos::features::IsJellyrollEnabled()
-              ? views::HighlightBorder::Type::kHighlightBorderOnShadow
-              : views::HighlightBorder::Type::kHighlightBorder1))
       .AddChildren(
           views::Builder<View>()
               .CopyAddressTo(&background_view)
