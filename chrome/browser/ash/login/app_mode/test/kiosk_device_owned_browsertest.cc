9b88f07f634a303c1840e10f1b657c24fecc020b
Solomon Kinard <solomonkinard@chromium.org>
2023-08-10 09:28:08 +0000

    Extensions: ExtensionId: Use alias instead of std::string
    Context:
    crrev.com/c/4763085/1..2/extensions/browser/api/api_resource.h#b22
    
    This CL was uploaded by git cl split.
    
    Bug: chromium:1471741
    Change-Id: I0f00012a42fa8b6c10ef5f48c4e4d939638d5c7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766920
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1181957}

--- chrome/browser/ash/login/app_mode/test/kiosk_device_owned_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_device_owned_browsertest.cc
@@ -55,6 +55,7 @@
 #include "extensions/browser/browsertest_util.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
+#include "extensions/common/extension_id.h"
 #include "extensions/common/mojom/manifest.mojom-shared.h"
 #include "extensions/components/native_app_window/native_app_window_views.h"
 #include "extensions/test/extension_test_message_listener.h"
@@ -127,7 +128,7 @@ class ScopedSettingsPages {
 class ExtensionReadyObserver : public extensions::ExtensionRegistryObserver {
  public:
   ExtensionReadyObserver(extensions::ExtensionRegistry* registry,
-                         const std::string& extension_id)
+                         const extensions::ExtensionId& extension_id)
       : extension_id_(extension_id) {
     extension_registry_observation_.Observe(registry);
   }
@@ -148,7 +149,7 @@ class ExtensionReadyObserver : public extensions::ExtensionRegistryObserver {
   base::ScopedObservation<extensions::ExtensionRegistry,
                           ExtensionRegistryObserver>
       extension_registry_observation_{this};
-  const std::string extension_id_;
+  const extensions::ExtensionId extension_id_;
 };
 
 }  // namespace
