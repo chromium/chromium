ff157a9d0e38a19536383191e485c2cd5cdd60f0
Vishwas Uppoor <vishwasuppoor@google.com>
2023-08-14 18:51:10 +0000

    [Offers][Messages] Default enable the experiment
    Default enable MessagesForAndroidOfferNotification experiment.
    
    Change-Id: I04025e4cf699641e94a17876469c4e9220646de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774545
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183221}

--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -37,7 +37,7 @@ BASE_FEATURE(kMessagesForAndroidInfrastructure,
 
 BASE_FEATURE(kMessagesForAndroidOfferNotification,
              "MessagesForAndroidOfferNotification",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMessagesForAndroidPermissionUpdate,
              "MessagesForAndroidPermissionUpdate",
