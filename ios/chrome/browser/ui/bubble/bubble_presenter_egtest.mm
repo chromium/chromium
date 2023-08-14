98048c8f44d9950a4f462a8d7996eebb755b3318
Huiting Yu <huitingyu@google.com>
2023-08-14 14:43:47 +0000

    Mark the test case testTabGridIPH flaky on device.
    Reland "[ios] Add egtest for tab grid IPH and new tab IPH."
    
    This is a reland of commit be2b40e8db459f77390f72bbc9b74a18d71fad3f
    
    Original change's description:
    > [ios] Add egtest for tab grid IPH and new tab IPH.
    >
    > Bug: 1439920
    >
    > Change-Id: I1cbc150be563c934ef9e6808a9c6c7382780bcfa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4676456
    > Commit-Queue: Huiting Yu <huitingyu@google.com>
    > Reviewed-by: Scott Yoder <scottyoder@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1180969}
    
    Bug: 1439920
    Change-Id: I2961ee902606473a4fb8bae032a19f7d4740631b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762345
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183099}

---
+++ ios/chrome/browser/ui/bubble/bubble_presenter_egtest.mm
@@ -0,0 +1,101 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/ios/ios_util.h"
+#import "base/strings/stringprintf.h"
+#import "components/feature_engagement/public/feature_constants.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/app_launch_manager.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
+
+@interface BubblePresenterTestCase : ChromeTestCase
+@end
+
+@implementation BubblePresenterTestCase
+
+// Open a random url from omnibox. `isAfterNewAppLaunch` is used for deciding
+// whether the step of tapping the fake omnibox is needed.
+- (void)openURLFromOmniboxWithIsAfterNewAppLaunch:(BOOL)isAfterNewAppLaunch {
+  if (isAfterNewAppLaunch) {
+    [[EarlGrey selectElementWithMatcher:chrome_test_util::FakeOmnibox()]
+        performAction:grey_tap()];
+    [ChromeEarlGrey waitForSufficientlyVisibleElementWithMatcher:
+                        chrome_test_util::Omnibox()];
+  }
+
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::Omnibox()]
+      performAction:grey_replaceText(@"chrome://version")];
+  // TODO(crbug.com/1454516): Use simulatePhysicalKeyboardEvent until
+  // replaceText can properly handle \n.
+  [ChromeEarlGrey simulatePhysicalKeyboardEvent:@"\n" flags:0];
+}
+
+#pragma mark - Tests
+
+// Tests that the New Tab IPH can be displayed when opening an URL from omnibox.
+- (void)testNewTabIPH {
+  // Enable the IPH Demo Mode feature to ensure the IPH triggers
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.additional_args.push_back(
+      base::StringPrintf("--enable-features=%s:chosen_feature/"
+                         "IPH_iOSNewTabToolbarItemFeature,IPHForSafariSwitcher",
+                         feature_engagement::kIPHDemoMode.name));
+  // Force the conditions that allow the iph to show.
+  config.additional_args.push_back("-ForceExperienceForDeviceSwitcher");
+  config.additional_args.push_back("SyncedAndFirstDevice");
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  // TODO(crbug.com/1470901): remove after fixing the bug on iphone.
+  if (![ChromeEarlGrey isIPadIdiom]) {
+    config.features_disabled.push_back(kBottomOmniboxDefaultSetting);
+    config.features_disabled.push_back(kBottomOmniboxSteadyState);
+  }
+
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
+  // Open an URL from NTP is likely not triggering showing the IPH due to
+  // failing the check `BubblePresenter::isTabScrolledToTop`, hence need to open
+  // an URL from non-NTP page below.
+  [self openURLFromOmniboxWithIsAfterNewAppLaunch:YES];
+
+  // Open an URL from non-NTP page.
+  [self openURLFromOmniboxWithIsAfterNewAppLaunch:NO];
+
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:grey_accessibilityID(
+                                              @"BubbleViewLabelIdentifier")];
+}
+
+// Tests that the Tab Grid IPH can be displayed when opening a new tab and there
+// are multiple tabs.
+// TODO(crbug.com/1471222): Test is flaky on device. Re-enable the test.
+#if !TARGET_OS_SIMULATOR
+#define MAYBE_testTabGridIPH FLAKY_testTabGridIPH
+#else
+#define MAYBE_testTabGridIPH testTabGridIPH
+#endif
+- (void)testTabGridIPH {
+  // Enable the IPH Demo Mode feature to ensure the IPH triggers
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.additional_args.push_back(base::StringPrintf(
+      "--enable-features=%s:chosen_feature/"
+      "IPH_iOSTabGridToolbarItemFeature,IPHForSafariSwitcher",
+      feature_engagement::kIPHDemoMode.name));
+  // Force the conditions that allow the iph to show.
+  config.additional_args.push_back("-ForceExperienceForDeviceSwitcher");
+  config.additional_args.push_back("SyncedAndFirstDevice");
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
+  [ChromeEarlGrey openNewTab];
+  [ChromeEarlGrey waitForMainTabCount:2];
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:grey_accessibilityID(
+                                              @"BubbleViewLabelIdentifier")];
+}
+
+@end
