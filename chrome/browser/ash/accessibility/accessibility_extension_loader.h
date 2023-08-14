d4183dc3201b597febaf650fce4451723b8c9eac
Solomon Kinard <solomonkinard@chromium.org>
2023-08-14 15:26:06 +0000

    Extensions: ExtensionId: Use alias instead of std::string
    Context:
    crrev.com/c/4763085/1..2/extensions/browser/api/api_resource.h#b22
    
    This CL was uploaded by git cl split.
    
    R=akihiroota@chromium.org
    
    Bug: chromium:1471741
    Change-Id: I312180a017a07873cafa9a6f1a4c86a8ed6a34da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4767418
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183115}

--- chrome/browser/ash/accessibility/accessibility_extension_loader.h
+++ chrome/browser/ash/accessibility/accessibility_extension_loader.h
@@ -9,6 +9,7 @@
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "extensions/common/extension_id.h"
 
 namespace content {
 class BrowserContext;
@@ -50,7 +51,7 @@ class AccessibilityExtensionLoader {
                                   base::OnceClosure done_cb);
   void UnloadExtension(content::BrowserContext* browser_context);
 
-  std::string extension_id_;
+  extensions::ExtensionId extension_id_;
   base::FilePath extension_path_;
   const base::FilePath::CharType* manifest_filename_;
   const base::FilePath::CharType* guest_manifest_filename_;
