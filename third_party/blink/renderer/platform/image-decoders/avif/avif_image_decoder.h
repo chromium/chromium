9710af96019ab544ee361c3b691fb680a4c449e6
Vignesh Venkatasubramanian <vigneshv@google.com>
2023-08-14 16:59:47 +0000

    Revert "Revert "avif_image_decoder: Use libavif's yuv to rgb function""
    Relanding the CL with workaround for the CFI failure.
    
    There seems to be an issue with CFI when referencing C++ function
    pointers from C libraries (libyuv is a C++ library and libavif is a C
    library). So work around that by adding
    "-fsanitize-cfi-icall-generalize-pointers" until the underlying CFI
    issue is figured out.
    
    Test: blink_platform_unittests pass with cfi turned on.
    Change-Id: Ib8679d11c72b128fc4ffebd871b00afa2119b443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4769822
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Vignesh Venkat <vigneshv@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183144}

--- third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder.h
+++ third_party/blink/renderer/platform/image-decoders/avif/avif_image_decoder.h
@@ -15,7 +15,6 @@
 #include "third_party/libavif/src/include/avif/avif.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "ui/gfx/color_space.h"
-#include "ui/gfx/color_transform.h"
 #include "ui/gfx/geometry/point.h"
 
 namespace blink {
@@ -59,7 +58,7 @@ class PLATFORM_EXPORT AVIFImageDecoder final : public ImageDecoder {
   // (ftyp) that supports the brand 'avif' or 'avis'.
   static bool MatchesAVIFSignature(const FastSharedBufferReader& fast_reader);
 
-  gfx::ColorTransform* GetColorTransformForTesting();
+  gfx::ColorSpace GetColorSpaceForTesting() const;
 
  private:
   // If the AVIF image has a clean aperture ('clap') property, what kind of
@@ -104,9 +103,6 @@ class PLATFORM_EXPORT AVIFImageDecoder final : public ImageDecoder {
   // depth, and YUV format matches those reported by the container. The decoded
   // frame is available in decoded_image_.
   avifResult DecodeImage(wtf_size_t index);
-
-  // Updates or creates |color_transform_| for YUV-to-RGB conversion.
-  void UpdateColorTransform(const gfx::ColorSpace& frame_cs, int bit_depth);
 
   // Crops |decoded_image_|.
   void CropDecodedImage();
@@ -168,8 +164,6 @@ class PLATFORM_EXPORT AVIFImageDecoder final : public ImageDecoder {
       nullptr, avifDecoderDestroy};
   avifIO avif_io_ = {};
   AvifIOData avif_io_data_;
-
-  std::unique_ptr<gfx::ColorTransform> color_transform_;
 
   const AnimationOption animation_option_;
 
