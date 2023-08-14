d1d38bc6907ee360f92f9c889ef0e118dfe11981
Anne Redulla <aredulla@google.com>
2023-08-14 23:59:58 +0000

    Delete //third_party/blanketjs
    I believe this dependency is no longer used.
    
    Bug: b:290275121
    Change-Id: I8c206834c1879f57190378eb99d12171a8b2148d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4772354
    Commit-Queue: Anne Redulla <aredulla@google.com>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1183422}

--- tools/licenses/licenses.py
+++ tools/licenses/licenses.py
@@ -57,7 +57,6 @@ PRUNE_PATHS = set([
     # Used for development and test, not in the shipping product.
     os.path.join('build', 'secondary'),
     os.path.join('third_party', 'bison'),
-    os.path.join('third_party', 'blanketjs'),
     os.path.join('third_party', 'chromite'),
     os.path.join('third_party', 'cygwin'),
     os.path.join('third_party', 'gles2_conform'),
