5b7b9f90ac0a9bb23aa80ed08e3e51ef26b51d1a
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-08-15 17:37:13 +0000

    [Extensions] Clean up EventMatcher
    Make a few small improvements to EventMatcher:
    * Make methods that are only called by EventMatcher private
    * Rename `GetRoutingID()` to `routing_id()` since it's a trivial
      accessor
    
    This CL has no behavior change, and merely makes the public surface
    of EventMatcher more targeted (which will be valuable if we look to
    change its implementation or structure).
    
    Bug: 1470045
    Change-Id: I680bdfa90d196a9ee196b9cc5761ca06d8b16432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763525
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183688}

--- extensions/common/event_matcher.cc
+++ extensions/common/event_matcher.cc
@@ -46,8 +46,9 @@ bool EventMatcher::MatchNonURLCriteria(
   if (event_info.has_window_exposed_by_default) {
     // An event with a |window_exposed_by_default| set is only
     // relevant to the listener if no window type filter is set.
-    if (HasWindowTypes())
+    if (GetWindowTypeCount() > 0) {
       return false;
+    }
     return event_info.window_exposed_by_default;
   }
 
@@ -106,14 +107,6 @@ bool EventMatcher::GetWindowType(int i, std::string* window_type_out) const {
     }
   }
   return false;
-}
-
-bool EventMatcher::HasWindowTypes() const {
-  return GetWindowTypeCount() != 0;
-}
-
-int EventMatcher::GetRoutingID() const {
-  return routing_id_;
 }
 
 }  // namespace extensions
