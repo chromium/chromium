f5fdc20c52f6e174904e42997955cfa8a77f037a
Camillia Smith Barnes <cammie@chromium.org>
2023-08-15 17:58:23 +0000

    Shared Storage: Add SharedStorageAPIM118 RuntimeEnabledFeature
    We add a RuntimeEnabledFeature "SharedStorageAPIM118" to correspond to
    `blink::features::kSharedStorageAPIM118` and correct when the
    `sharedStorageWritable` attribute is exposed for `RequestInit`.
    
    
    Bug: 1434529,1218540
    Change-Id: Ic2eaf3465fb95a84215da98d479c27e578b6c4d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4777832
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183702}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -233,6 +233,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
      raw_ref(features::kPrivacySandboxAdsAPIsOverride), kSetOnlyIfOverridden},
     {wf::EnableSharedStorageAPI,
      raw_ref(features::kPrivacySandboxAdsAPIsM1Override), kSetOnlyIfOverridden},
+    {wf::EnableSharedStorageAPIM118,
+     raw_ref(blink::features::kSharedStorageAPIM118), kSetOnlyIfOverridden},
     {wf::EnableFedCmMultipleIdentityProviders,
      raw_ref(features::kFedCmMultipleIdentityProviders), kDefault},
     {wf::EnableFedCmSelectiveDisclosure,
@@ -241,8 +243,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
      kSetOnlyIfOverridden},
     {wf::EnableFencedFrames,
      raw_ref(features::kPrivacySandboxAdsAPIsM1Override), kSetOnlyIfOverridden},
-    {wf::EnableSharedStorageAPI,
-     raw_ref(features::kPrivacySandboxAdsAPIsOverride), kSetOnlyIfOverridden},
     {wf::EnableForcedColors, raw_ref(features::kForcedColors)},
     {wf::EnableFractionalScrollOffsets,
      raw_ref(features::kFractionalScrollOffsets)},
@@ -698,6 +698,17 @@ void ResolveInvalidConfigurations() {
         << switches::kEnableFeatures << "="
         << blink::features::kSharedStorageAPI.name << " in addition.";
     WebRuntimeFeatures::EnableSharedStorageAPI(false);
+  }
+
+  if (!base::FeatureList::IsEnabled(blink::features::kSharedStorageAPIM118) ||
+      !base::FeatureList::IsEnabled(blink::features::kSharedStorageAPI)) {
+    LOG_IF(WARNING, WebRuntimeFeatures::IsSharedStorageAPIM118Enabled())
+        << "SharedStorage for M118+ cannot be enabled in this "
+           "configuration. Use --"
+        << switches::kEnableFeatures << "="
+        << blink::features::kSharedStorageAPI.name << ","
+        << blink::features::kSharedStorageAPIM118.name << " in addition.";
+    WebRuntimeFeatures::EnableSharedStorageAPIM118(false);
   }
 
   if (!base::FeatureList::IsEnabled(blink::features::kConversionMeasurement)) {
