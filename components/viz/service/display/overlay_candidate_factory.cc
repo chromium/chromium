10439084a805322cee326b76c5712e3108b42b6b
Antonio Gomes <tonikitoo@igalia.com>
2023-08-15 21:32:29 +0000

    Only set OverlayCandidate::rounded_corners when required
    When delegated compositing is enabled (eg lacros), rounded corners
    data (gfx::RRectF) were being unconditionally set to all
    OverlayCandidate instances, despite whether it'd be effectively applied
    on Ash side or not.
    At very least, this increases the data traffic between lacros and exo
    and adds up some extra processing on both client and server sides.
    
    This CL optimizes that verifying whether any of the rounded corners
    bounding rects (up/left, up/right, down/left, down/right) intersect
    with the respective OverlayCandidate::display_rect.
    
    R=petermcneeley@chromium.org
    
    Bug: 1462171
    Change-Id: I63bfd844b4c72c5a46690c59d7cad0dab5e91970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4743486
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1183830}

--- components/viz/service/display/overlay_candidate_factory.cc
+++ components/viz/service/display/overlay_candidate_factory.cc
@@ -110,6 +110,35 @@ OverlayCandidate::CandidateStatus GetReasonForTransformNotAxisAligned(
   return OverlayCandidate::CandidateStatus::kFailNotAxisAligned2dRotation;
 }
 
+// Returns true if the overlay candidate bounds rect overlap with at least one
+// of the rounded corners bounding rects.
+//
+// TODO(crbug.com/1462171): This method shares some logic with
+// DirectRenderer::ShouldApplyRoundedCorner(). Try to move it
+// to a shared location.
+bool ShouldApplyRoundedCorner(OverlayCandidate& candidate,
+                              const SharedQuadState* sqs) {
+  const gfx::MaskFilterInfo& mask_filter_info = sqs->mask_filter_info;
+  if (!mask_filter_info.HasRoundedCorners()) {
+    return false;
+  }
+
+  const gfx::RRectF& rounded_corner_bounds =
+      mask_filter_info.rounded_corner_bounds();
+
+  const gfx::RectF target_rect = candidate.display_rect;
+
+  const gfx::RRectF::Corner corners[] = {
+      gfx::RRectF::Corner::kUpperLeft, gfx::RRectF::Corner::kUpperRight,
+      gfx::RRectF::Corner::kLowerRight, gfx::RRectF::Corner::kLowerLeft};
+  for (auto c : corners) {
+    if (rounded_corner_bounds.CornerBoundingRect(c).Intersects(target_rect)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }  // namespace
 
 OverlayCandidate::CandidateStatus OverlayCandidateFactory::FromDrawQuad(
@@ -145,10 +174,6 @@ OverlayCandidate::CandidateStatus OverlayCandidateFactory::FromDrawQuad(
     return CandidateStatus::kFailMaskFilterNotSupported;
   }
 
-  candidate.has_mask_filter =
-      !quad->shared_quad_state->mask_filter_info.IsEmpty();
-  candidate.rounded_corners = sqs->mask_filter_info.rounded_corner_bounds();
-
   candidate.requires_overlay = OverlayCandidate::RequiresOverlay(quad);
   candidate.overlay_damage_index =
       sqs->overlay_damage_index.value_or(OverlayCandidate::kInvalidDamageIndex);
@@ -181,6 +206,17 @@ OverlayCandidate::CandidateStatus OverlayCandidateFactory::FromDrawQuad(
       break;
     default:
       break;
+  }
+
+  candidate.has_mask_filter =
+      !quad->shared_quad_state->mask_filter_info.IsEmpty();
+
+  // Conditionally set the rounded corners once the candidate's |display_rect|
+  // is known.
+  // TODO(https://crbug.com/1462171): Consider moving this code to
+  // FromDrawQuadResource() that covers all of delegated compositing.
+  if (ShouldApplyRoundedCorner(candidate, sqs)) {
+    candidate.rounded_corners = sqs->mask_filter_info.rounded_corner_bounds();
   }
 
   return status;
