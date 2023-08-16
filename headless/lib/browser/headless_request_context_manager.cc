e66f8bf7435c63c235e4f3442d7cb407d699dba1
Will Harris <wfh@chromium.org>
2023-08-16 18:00:51 +0000

    Enable network data migration for headless network data on Windows.
    This brings headless to the same configuration as headful and allows the
    sandbox to be enabled on Windows.
    
    See bug for full justification and explanation.
    
    BUG=1473097
    
    Change-Id: If8d7b7b0a890e6416ce798b9c60f44c1195ab8d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4781219
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184257}

--- headless/lib/browser/headless_request_context_manager.cc
+++ headless/lib/browser/headless_request_context_manager.cc
@@ -275,10 +275,36 @@ void HeadlessRequestContextManager::ConfigureNetworkContextParamsInternal(
     context_params->file_paths->unsandboxed_data_path = user_data_path_;
     context_params->file_paths->cookie_database_name =
         base::FilePath(FILE_PATH_LITERAL("Cookies"));
-    // Headless should never perform a migration leaving it to the network
-    // service to decide which data directory (sandboxed or unsandboxed)
-    // it should pick.
+#if BUILDFLAG(IS_WIN)
+    // For the network sandbox to operate, the network data must be in the
+    // 'Network' directory and not the `unsandboxed_data_path`.
+    //
+    // On Windows, the majority of data dir is already residing in the 'Network'
+    // data dir. This is because there are three possible cases:
+    // 1. A data dir from headful is being used, and headful has migrated any
+    // data since M98 (Feb 2022). (data is in 'Network')
+    // 2. Headless has been using the 'Network' data dir since M96, since
+    // although headless never opted into migration, any new data was always in
+    // the 'Network' dir since ba2eb47b. (data is in 'Network')
+    // 3. Headless is using a data dir from before M96, and since migration was
+    // never enabled, it has been continuing to use this directory up until now.
+    // (data is in `unsandboxed_data_path` and sandbox will not function).
+    //
+    // The majority of users are in 1, or 2. For the small number of users in 3,
+    // setting `trigger_migration` will migrate their data dirs to 'Network' but
+    // this data will still interop between headless and headful as long as they
+    // are running M96 or later that understands both directories, the only
+    // noticeable difference will be that the files will move, as they have
+    // already been doing in headful.
+    context_params->file_paths->trigger_migration = true;
+#else
+    // On non-Windows platforms, trigger migration is not set, so there is no
+    // point (but equally no harm) in doing a migration since headful does not
+    // perform the migration. See
+    // ProfileNetworkContextService::ConfigureNetworkContextParamsInternal in
+    // src/chrome.
     context_params->file_paths->trigger_migration = false;
+#endif  // BUILDFLAG(IS_WIN)
   }
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kDiskCacheDir)) {
