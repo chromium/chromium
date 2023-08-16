73fabaee0ba821178ba25726708473656d83c3fa
Rasika Navarange <rasikan@google.com>
2023-08-16 12:40:53 +0000

    Add presubmit to add PERFETTO_TESTS to cl description
    When making changes to the trace processor chrome standard library, developers should run the Perfetto Diff Tests and ensure they pass before submitting.
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests`
    
    Change-Id: Ief2d38f05a204c5b64055cbf53f9084ce6b4476e
    Bug: b/287233796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4774631
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184099}

--- base/tracing/PRESUBMIT.py
+++ base/tracing/PRESUBMIT.py
@@ -23,3 +23,31 @@ def CheckSqlModules(input_api, output_api):
     # is complete.
     return []
   return []
+
+_STDLIB_PATHS = (
+  r"^base/tracing/stdlib/",
+  r"^base/tracing/test/",
+)
+
+def CheckPerfettoTestsTag(input_api, output_api):
+  """Checks that commits to the trace processor chrome stdlib or the
+  Perfetto diff tests contain a PERFETTO_TESTS tag in their commit
+  message."""
+  def FileFilter(affected_file):
+    return input_api.FilterSourceFile(affected_file,
+                                      files_to_check=_STDLIB_PATHS)
+
+  # Only consider changes to chrome stdlib or tests paths
+  if not any (input_api.AffectedFiles(file_filter=FileFilter)):
+    return []
+
+  if input_api.change.PERFETTO_TESTS:
+    return []
+
+  message = (
+    'Must provide PERFETTO_TESTS='
+    '`autoninja -C out/Default perfetto_diff_tests && '
+    'out/Default/bin/run_perfetto_diff_tests` line in CL description.'
+    '\nPlease ensure the Perfetto diff tests pass before submitting.'
+  )
+  return [output_api.PresubmitError(message)]
