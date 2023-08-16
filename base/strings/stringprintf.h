b081fa890cac71095d544b44ce657d56f164b7f4
Peter Kasting <pkasting@chromium.org>
2023-08-16 17:18:24 +0000

    Revert "Remove Windows-specific string16 variants of StringPrintf() etc."
    This reverts commit 09d0b586a8b0c48a9651a33664315c8cdbb3bf5c.
    
    Reason for revert: Breaks official build: https://luci-milo.appspot.com/ui/p/chrome/builders/official/win64-clang/6825/overview
    
    Original change's description:
    > Remove Windows-specific string16 variants of StringPrintf() etc.
    >
    > These are little-used and can be worked around.
    >
    > Bug: 1371963
    > Change-Id: I29292f701aac4f21c7247569aed3b33ab63fe2c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771905
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Owners-Override: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1183930}
    
    (cherry picked from commit b8307cc6f40c0e4cbafb7ff63ccd69479e4db9e9)
    
    Bug: 1371963
    Change-Id: Id35c0e96e022e243e2f320553ba2c935964816de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784100
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1184182}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784708
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/5952@{#4}
    Cr-Branched-From: a2270a38ec1a78c48ebdfb549710919b3cee97cb-refs/heads/main@{#1184085}

--- base/strings/stringprintf.h
+++ base/strings/stringprintf.h
@@ -24,6 +24,8 @@ namespace base {
 // carefully and sparingly. Also applies to StringAppendV below.
 [[nodiscard]] BASE_EXPORT std::wstring StringPrintf(const wchar_t* format, ...)
     WPRINTF_FORMAT(1, 2);
+[[nodiscard]] BASE_EXPORT std::u16string StringPrintf(const char16_t* format,
+                                                      ...) WPRINTF_FORMAT(1, 2);
 #endif
 
 // Return a C++ string given vprintf-like input.
@@ -37,6 +39,8 @@ BASE_EXPORT void StringAppendF(std::string* dst, const char* format, ...)
 #if BUILDFLAG(IS_WIN)
 BASE_EXPORT void StringAppendF(std::wstring* dst, const wchar_t* format, ...)
     WPRINTF_FORMAT(2, 3);
+BASE_EXPORT void StringAppendF(std::u16string* dst, const char16_t* format, ...)
+    WPRINTF_FORMAT(2, 3);
 #endif
 
 // Lower-level routine that takes a va_list and appends to a specified
@@ -46,6 +50,9 @@ BASE_EXPORT void StringAppendV(std::string* dst, const char* format, va_list ap)
 #if BUILDFLAG(IS_WIN)
 BASE_EXPORT void StringAppendV(std::wstring* dst,
                                const wchar_t* format,
+                               va_list ap) WPRINTF_FORMAT(2, 0);
+BASE_EXPORT void StringAppendV(std::u16string* dst,
+                               const char16_t* format,
                                va_list ap) WPRINTF_FORMAT(2, 0);
 #endif
 
