ee2dfb18fa61a4eb774557dadd09856069cb5495
Josiah Krutz <josiahk@google.com>
2023-08-16 22:23:33 +0000

    [ChromeVox] Remove unused helper kbexplorer_loader.js
    Cleans up unused helper kbexplorer_loader.js and references in chrome/
    browser/resources/chromeos/accessibility/chromevox.
    
    This change is a pure refactor.
    
    AX-Relnotes: N/A
    Bug: b/265481751
    Change-Id: I7815628eaea02e8807fede4085d43fa71cd439c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4776165
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184420}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/kbexplorer_loader.js
+++
@@ -1,8 +0,0 @@
-// Copyright 2014 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * @fileoverview This file is empty, but the closure compiler requires an entry
- * point. As such this file is that entry point.
- */
