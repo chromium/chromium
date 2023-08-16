07b34b08ca3bdc49c2107b0c89f0e6437973ee9a
Dana Fried <dfried@chromium.org>
2023-08-16 20:35:25 +0000

    [User Education] Add flag for v2 experience
    No behavior is changed as part of this CL.
    
    Bug: 1468395
    Change-Id: I7e909dcb39419f9347482d9327a376dfc924931b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762289
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184355}

---
+++ components/user_education/common/user_education_features.h
@@ -0,0 +1,16 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_USER_EDUCATION_COMMON_USER_EDUCATION_FEATURES_H_
+#define COMPONENTS_USER_EDUCATION_COMMON_USER_EDUCATION_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace user_education::features {
+
+BASE_DECLARE_FEATURE(kUserEducationExperienceVersion2);
+
+}  // namespace user_education::features
+
+#endif  // COMPONENTS_USER_EDUCATION_COMMON_USER_EDUCATION_FEATURES_H_
