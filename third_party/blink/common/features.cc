7a0408f992d14320616f70c485e5306ae7b7fef9
W. James MacLean <wjmaclean@chromium.org>
2023-08-16 22:33:20 +0000

    Enable NewBaseUrlInheritanceBehavior by default.
    Enables the feature by default in preparation for advancing the
    launch trial.
    
    Bug: 1356658
    Change-Id: I1064e9234290cf6e6bd90ec3568b9042b8e8315f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4779689
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184427}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1158,7 +1158,7 @@ BASE_FEATURE(kNavigationPredictor,
 
 BASE_FEATURE(kNewBaseUrlInheritanceBehavior,
              "NewBaseUrlInheritanceBehavior",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNoForcedFrameUpdatesForWebTests,
              "NoForcedFrameUpdatesForWebTests",
