b92bd477f1d8805778830bacb0c79f0ddae20cb6
Victor Hugo Vianna Silva <victorvianna@google.com>
2023-08-17 10:02:00 +0000

    Ensure raw_ptrs are initialized in chrome/browser/ui/webui/sync_internals/
    Details in https://crbug.com/1473266#c2.
    
    Bug: 1473266
    Change-Id: I7fc9c5defa27a6e68c8d20bfc965de72b60c56ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783451
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1184628}

--- chrome/browser/ui/webui/sync_internals/sync_internals_message_handler_unittest.cc
+++ chrome/browser/ui/webui/sync_internals/sync_internals_message_handler_unittest.cc
@@ -188,8 +188,9 @@ class SyncInternalsMessageHandlerTest : public ChromeRenderViewHostTestHarness {
 
  private:
   content::TestWebUI web_ui_;
-  raw_ptr<TestSyncService, DanglingUntriaged> test_sync_service_;
-  raw_ptr<FakeUserEventService, DanglingUntriaged> fake_user_event_service_;
+  raw_ptr<TestSyncService, DanglingUntriaged> test_sync_service_ = nullptr;
+  raw_ptr<FakeUserEventService, DanglingUntriaged> fake_user_event_service_ =
+      nullptr;
   std::unique_ptr<TestableSyncInternalsMessageHandler> handler_;
   int about_sync_data_delegate_call_count_ = 0;
   raw_ptr<SyncService, DanglingUntriaged> last_delegate_sync_service_ = nullptr;
