21fff7f6528ac5c96beb6cee9468ddf9ba5dc854
Nan Lin <linnan@chromium.org>
2023-08-17 15:48:56 +0000

    Add explicit enum for background caller error for Conversions.AndroidRegistrationResult.* metrics
    OBSOLETE_HISTOGRAMS=Replaced with new versions
    
    Bug: 1473393
    Change-Id: Ida781ce8fdb21adc5294875276e13a5c0ab1e9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4784011
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184738}

--- content/public/android/java/src/org/chromium/content/browser/AttributionOsLevelManager.java
+++ content/public/android/java/src/org/chromium/content/browser/AttributionOsLevelManager.java
@@ -38,6 +38,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -66,7 +67,8 @@ public class AttributionOsLevelManager {
             RegistrationResult.ERROR_ILLEGAL_ARGUMENT, RegistrationResult.ERROR_IO,
             RegistrationResult.ERROR_ILLEGAL_STATE, RegistrationResult.ERROR_SECURITY,
             RegistrationResult.ERROR_TIMEOUT, RegistrationResult.ERROR_LIMIT_EXCEEDED,
-            RegistrationResult.ERROR_INTERNAL, RegistrationResult.COUNT})
+            RegistrationResult.ERROR_INTERNAL, RegistrationResult.ERROR_BACKGROUND_CALLER,
+            RegistrationResult.COUNT})
     @Retention(RetentionPolicy.SOURCE)
     public @interface RegistrationResult {
         int SUCCESS = 0;
@@ -78,7 +80,8 @@ public class AttributionOsLevelManager {
         int ERROR_TIMEOUT = 6;
         int ERROR_LIMIT_EXCEEDED = 7;
         int ERROR_INTERNAL = 8;
-        int COUNT = 9;
+        int ERROR_BACKGROUND_CALLER = 9;
+        int COUNT = 10;
     }
 
     @CalledByNative
@@ -100,12 +103,12 @@ public class AttributionOsLevelManager {
         switch (type) {
             case RegistrationType.SOURCE:
                 RecordHistogram.recordEnumeratedHistogram(
-                        "Conversions.AndroidRegistrationResult.Source", result,
+                        "Conversions.AndroidRegistrationResult.Source2", result,
                         RegistrationResult.COUNT);
                 break;
             case RegistrationType.TRIGGER:
                 RecordHistogram.recordEnumeratedHistogram(
-                        "Conversions.AndroidRegistrationResult.Trigger", result,
+                        "Conversions.AndroidRegistrationResult.Trigger2", result,
                         RegistrationResult.COUNT);
 
                 break;
@@ -137,7 +140,13 @@ public class AttributionOsLevelManager {
                 } else if (thrown instanceof IOException) {
                     result = RegistrationResult.ERROR_IO;
                 } else if (thrown instanceof IllegalStateException) {
-                    result = RegistrationResult.ERROR_ILLEGAL_STATE;
+                    // The Android API doesn't break out this error as a separate exception so we
+                    // are forced to inspect the message for now.
+                    if (thrown.getMessage().toLowerCase(Locale.US).contains("background")) {
+                        result = RegistrationResult.ERROR_BACKGROUND_CALLER;
+                    } else {
+                        result = RegistrationResult.ERROR_ILLEGAL_STATE;
+                    }
                 } else if (thrown instanceof SecurityException) {
                     result = RegistrationResult.ERROR_SECURITY;
                 } else if (thrown instanceof TimeoutException) {
