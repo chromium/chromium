3612b64601836b6b03bf4a99d71fc5637e1074db
Andrew Williams <awillia@chromium.org>
2023-08-17 13:31:11 +0000

    [BlobURL] Re-enable new blob URL architecture by default
    This was enabled and then disabled due to an increase in
    renderer hangs, but after investigating further it seems
    plausible that this higher (but stable) number of
    renderer hangs is inherent in using a synchronous Mojo
    interface method of a navigation-associated interface
    between the renderer and the browser process UI thread
    (or at least is a side effect of some unknown issue that
    causes the browser process to not respond to the messages
    in a timely manner).
    
    This requires temporarily disabling
    kRestrictCanAccessDataForOriginToUIThread due to issues
    that arise when both flags are enabled. For more info,
    see:
     - https://chromium-review.googlesource.com/c/chromium/src/+/4764617/comments/850fbb6b_b25fed3c
     - https://bugs.chromium.org/p/chromium/issues/detail?id=764958#c54
    
    Bug: 764958, 1407944
    Change-Id: If287568c4beb9ee8faabd4ac166ebe4594095734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4762807
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184679}

--- content/common/features.cc
+++ content/common/features.cc
@@ -41,7 +41,7 @@ BASE_FEATURE(kGpuInfoCollectionSeparatePrefetch,
 
 BASE_FEATURE(kRestrictCanAccessDataForOriginToUIThread,
              "RestrictCanAccessDataForOriginToUIThread",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kServiceWorkerAutoPreload,
              "ServiceWorkerAutoPreload",
