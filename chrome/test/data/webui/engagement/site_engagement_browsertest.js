4d3e4fcd0597151b4f78b6195de2b6f4087b606c
dpapad <dpapad@chromium.org>
2023-08-17 07:31:12 +0000

    Separate Mocha and js2gtest code in chrome://site-engagement tests.
    This is in preparation of migrating the tests to TypeScript, and then
    moving off of js2gtest completely.
    
    Bug: 1457360,1158471
    Change-Id: Ie27e822dae54950d8ac083a5f150bc28f80f4196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4782372
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1184582}

--- chrome/test/data/webui/engagement/site_engagement_browsertest.js
+++ chrome/test/data/webui/engagement/site_engagement_browsertest.js
@@ -19,7 +19,8 @@ function SiteEngagementBrowserTest() {}
 SiteEngagementBrowserTest.prototype = {
   __proto__: testing.Test.prototype,
 
-  browsePreload: 'chrome://site-engagement',
+  browsePreload:
+      'chrome://site-engagement/test_loader.html?module=engagement/site_engagement_test.js',
 
   isAsync: true,
 
@@ -30,67 +31,8 @@ SiteEngagementBrowserTest.prototype = {
     GEN('service->ResetBaseScoreForURL(GURL("' + EXAMPLE_URL_2 +
         '"), 3.14159);');
   },
-
-  extraLibraries: [
-    '//third_party/mocha/mocha.js',
-    '//chrome/test/data/webui/mocha_adapter.js',
-  ],
 };
 
 TEST_F('SiteEngagementBrowserTest', 'All', function() {
-  let app;
-  let cells;
-
-  function getCells() {
-    const originCells =
-        Array.from(app.shadowRoot.querySelectorAll('.origin-cell'));
-    const scoreInputs =
-        Array.from(app.shadowRoot.querySelectorAll('.base-score-input'));
-    const bonusScoreCells =
-        Array.from(app.shadowRoot.querySelectorAll('.bonus-score-cell'));
-    const totalScoreCells =
-        Array.from(app.shadowRoot.querySelectorAll('.total-score-cell'));
-    return originCells.map((c, i) => {
-      return {
-        origin: c,
-        scoreInput: scoreInputs[i],
-        bonusScore: bonusScoreCells[i],
-        totalScore: totalScoreCells[i],
-      };
-    });
-  }
-
-  setup(async function() {
-    await import('chrome://webui-test/mojo_webui_test_support.js');
-    document.body.innerHTML = window.trustedTypes.emptyHTML;
-    app = document.createElement('site-engagement-app');
-    document.body.appendChild(app);
-    await app.whenPopulatedForTest();
-    app.disableAutoupdate();
-    cells = getCells();
-  });
-
-  test('check engagement values are loaded', function() {
-    assertDeepEquals(
-        [EXAMPLE_URL_1, EXAMPLE_URL_2], cells.map((c) => c.origin.textContent));
-  });
-
-  test('scores rounded to 2 decimal places', function() {
-    assertDeepEquals(['10', '3.14'], cells.map((x) => x.scoreInput.value));
-    assertDeepEquals(['0', '0'], cells.map((x) => x.bonusScore.textContent));
-    assertDeepEquals(
-        ['10', '3.14'], cells.map((x) => x.totalScore.textContent));
-  });
-
-  test('change score', async function() {
-    const firstRow = cells[0];
-    firstRow.scoreInput.value = 50;
-    firstRow.scoreInput.dispatchEvent(new Event('change'));
-
-    const {info} =
-        await app.engagementDetailsProvider.getSiteEngagementDetails();
-    assertEquals(firstRow.origin.textContent, info[0].origin.url);
-    assertEquals(50, info[0].baseScore);
-  });
   mocha.run();
 });
---
+++ chrome/test/data/webui/engagement/site_engagement_test.js
@@ -0,0 +1,67 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://webui-test/mojo_webui_test_support.js';
+import 'chrome://site-engagement/app.js';
+
+import {assertDeepEquals, assertEquals} from 'chrome://webui-test/chai_assert.js';
+
+suite('SiteEngagement', function() {
+  const EXAMPLE_URL_1 = 'http://example.com/';
+  const EXAMPLE_URL_2 = 'http://shmlexample.com/';
+
+  let app;
+  let cells;
+
+  function getCells() {
+    const originCells =
+        Array.from(app.shadowRoot.querySelectorAll('.origin-cell'));
+    const scoreInputs =
+        Array.from(app.shadowRoot.querySelectorAll('.base-score-input'));
+    const bonusScoreCells =
+        Array.from(app.shadowRoot.querySelectorAll('.bonus-score-cell'));
+    const totalScoreCells =
+        Array.from(app.shadowRoot.querySelectorAll('.total-score-cell'));
+    return originCells.map((c, i) => {
+      return {
+        origin: c,
+        scoreInput: scoreInputs[i],
+        bonusScore: bonusScoreCells[i],
+        totalScore: totalScoreCells[i],
+      };
+    });
+  }
+
+  setup(async function() {
+    document.body.innerHTML = window.trustedTypes.emptyHTML;
+    app = document.createElement('site-engagement-app');
+    document.body.appendChild(app);
+    await app.whenPopulatedForTest();
+    app.disableAutoupdate();
+    cells = getCells();
+  });
+
+  test('check engagement values are loaded', function() {
+    assertDeepEquals(
+        [EXAMPLE_URL_1, EXAMPLE_URL_2], cells.map((c) => c.origin.textContent));
+  });
+
+  test('scores rounded to 2 decimal places', function() {
+    assertDeepEquals(['10', '3.14'], cells.map((x) => x.scoreInput.value));
+    assertDeepEquals(['0', '0'], cells.map((x) => x.bonusScore.textContent));
+    assertDeepEquals(
+        ['10', '3.14'], cells.map((x) => x.totalScore.textContent));
+  });
+
+  test('change score', async function() {
+    const firstRow = cells[0];
+    firstRow.scoreInput.value = 50;
+    firstRow.scoreInput.dispatchEvent(new Event('change'));
+
+    const {info} =
+        await app.engagementDetailsProvider.getSiteEngagementDetails();
+    assertEquals(firstRow.origin.textContent, info[0].origin.url);
+    assertEquals(50, info[0].baseScore);
+  });
+});
