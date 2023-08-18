d3f7168238616e65d05cee26b54f5429711d8d79
Kendrake Tsui <kendraketsui@google.com>
2023-08-18 20:39:30 +0000

    featured: Upload active early-boot trials to UMA
    There is currently no existing logic to record active early-boot trials
    in UMA.
    
    Add logic to record active early-boot trials to UMA by watching for
    new active trial files created by featured. This includes a helper
    method that parses the filename so the trial can be added and activated
    in base::FieldTrialList, which marks the trial as active in UMA.
    
    BUG=b:294278740
    TEST=unit tests
    
    Change-Id: I4e0683c4f58aa184df38c9972cf8565fae728acb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771078
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kendrake Tsui <kendraketsui@google.com>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185368}

--- chromeos/ash/components/dbus/featured/featured_client.cc
+++ chromeos/ash/components/dbus/featured/featured_client.cc
@@ -6,14 +6,21 @@
 
 #include <string>
 
-#include <base/logging.h>
+#include "base/check_is_test.h"
+#include "base/files/file_path.h"
+#include "base/files/file_path_watcher.h"
 #include "base/functional/callback.h"
+#include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/field_trial.h"
+#include "base/strings/escape.h"
+#include "base/strings/string_split.h"
 #include "chromeos/ash/components/dbus/featured/fake_featured_client.h"
 #include "chromeos/ash/components/dbus/featured/featured.pb.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
+#include "third_party/cros_system_api/constants/featured.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace ash::featured {
@@ -36,6 +43,23 @@ class FeaturedClientImpl : public FeaturedClient {
     featured_service_proxy_ =
         bus->GetObjectProxy(::featured::kFeaturedServiceName,
                             dbus::ObjectPath(::featured::kFeaturedServicePath));
+
+    listen_callback_ =
+        base::BindRepeating(&FeaturedClientImpl::RecordEarlyBootTrialInUMA,
+                            weak_ptr_factory_.GetWeakPtr());
+    ListenForActiveEarlyBootTrials();
+  }
+
+  void InitWithCallbackForTesting(dbus::Bus* const bus,  // IN-TEST
+                                  const base::FilePath& expected_dir,
+                                  ListenForTrialCallback callback) {
+    CHECK_IS_TEST();
+    featured_service_proxy_ =
+        bus->GetObjectProxy(::featured::kFeaturedServiceName,
+                            dbus::ObjectPath(::featured::kFeaturedServicePath));
+    expected_dir_ = expected_dir;
+    listen_callback_ = callback;
+    ListenForActiveEarlyBootTrials();
   }
 
   void HandleSeedFetchedResponse(
@@ -66,6 +90,52 @@ class FeaturedClientImpl : public FeaturedClient {
   }
 
  private:
+  void RecordEarlyBootTrialInUMA(const std::string& trial_name,
+                                 const std::string& group_name) {
+    base::FieldTrial* trial =
+        base::FieldTrialList::CreateFieldTrial(trial_name, group_name);
+    // This records the trial in UMA.
+    trial->Activate();
+  }
+
+  void RecordEarlyBootTrialInChrome(const base::FilePath& path, bool error) {
+    if (error || path.DirName() != expected_dir_) {
+      // TODO(b/296394808): Add UMA metric if we enter this code path since it
+      // is not expected.
+      return;
+    }
+
+    base::FieldTrial::ActiveGroup active_group;
+    if (ParseTrialFilename(path, active_group)) {
+      listen_callback_.Run(active_group.trial_name, active_group.group_name);
+    }
+  }
+
+  void ListenForActiveEarlyBootTrials() {
+    base::FilePathWatcher::WatchOptions options = {
+        // Watches for changes in a directory.
+        .type = base::FilePathWatcher::Type::kRecursive,
+        // Reports the path of modified files in the directory.
+        .report_modified_path = true};
+
+    watcher_.WatchWithOptions(
+        expected_dir_, options,
+        base::BindRepeating(&FeaturedClientImpl::RecordEarlyBootTrialInChrome,
+                            weak_ptr_factory_.GetWeakPtr()));
+  }
+
+  // Watches for early-boot trial files written to `expected_dir_`.
+  base::FilePathWatcher watcher_;
+
+  // Callback used when early-boot trial files are written to `expected_dir_`.
+  FeaturedClient::ListenForTrialCallback listen_callback_;
+
+  // Directory where active trial files on platform are written to, defaulting
+  // to feature::kActiveTrialFileDirectory. The default value can be overridden
+  // with InitializeForTesting().
+  base::FilePath expected_dir_ =
+      base::FilePath(feature::kActiveTrialFileDirectory);
+
   raw_ptr<dbus::ObjectProxy, ExperimentalAsh> featured_service_proxy_ = nullptr;
 
   // Note: This should remain the last member so it'll be destroyed and
@@ -97,6 +167,15 @@ void FeaturedClient::InitializeFake() {
 }
 
 // static
+void FeaturedClient::InitializeForTesting(dbus::Bus* bus,
+                                          const base::FilePath& expected_dir,
+                                          ListenForTrialCallback callback) {
+  DCHECK(bus);
+  (new FeaturedClientImpl())
+      ->InitWithCallbackForTesting(bus, expected_dir, callback);  // IN-TEST
+}
+
+// static
 void FeaturedClient::Shutdown() {
   DCHECK(g_instance);
   delete g_instance;
@@ -105,6 +184,34 @@ void FeaturedClient::Shutdown() {
 // static
 FeaturedClient* FeaturedClient::Get() {
   return g_instance;
+}
+
+// static
+bool FeaturedClient::ParseTrialFilename(
+    const base::FilePath& path,
+    base::FieldTrial::ActiveGroup& active_group) {
+  std::string filename = path.BaseName().value();
+  std::vector<base::StringPiece> components =
+      base::SplitStringPiece(filename, feature::kTrialGroupSeparator,
+                             base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
+  if (components.size() != 2) {
+    LOG(ERROR) << "Active trial filename not of the form TrialName,GroupName: "
+               << filename;
+    return false;
+  }
+
+  std::string trial_name = base::UnescapeURLComponent(
+      components[0],
+      base::UnescapeRule::SPACES | base::UnescapeRule::PATH_SEPARATORS |
+          base::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS);
+  std::string group_name = base::UnescapeURLComponent(
+      components[1],
+      base::UnescapeRule::SPACES | base::UnescapeRule::PATH_SEPARATORS |
+          base::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS);
+  active_group.trial_name = trial_name;
+  active_group.group_name = group_name;
+
+  return true;
 }
 
 }  // namespace ash::featured
--- chromeos/ash/components/dbus/featured/featured_client.h
+++ chromeos/ash/components/dbus/featured/featured_client.h
@@ -6,7 +6,10 @@
 #define CHROMEOS_ASH_COMPONENTS_DBUS_FEATURED_FEATURED_CLIENT_H_
 
 #include "base/component_export.h"
+#include "base/files/file_path.h"
 #include "base/functional/callback.h"
+#include "base/gtest_prod_util.h"
+#include "base/metrics/field_trial.h"
 #include "chromeos/ash/components/dbus/featured/featured.pb.h"
 
 namespace dbus {
@@ -15,13 +18,29 @@ class Bus;
 
 namespace ash::featured {
 
-// FeaturedClient is used to communicate seed state with featured, which is used
-// for enabling and managing platform specific features. Its main user is Chrome
-// field trials.
+// FeaturedClient is used to:
+//  * communicate seed state with featured, which is used for enabling and
+//  managing platform specific features.
+//  * record early-boot trials to UMA by listening for early-boot trial files
+//  written by featured.
+//
+// Its main user is Chrome field trials.
 class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DBUS_FEATURED) FeaturedClient {
  public:
+  // Callback type for ListenForActiveEarlyBootTrials(), which takes in the
+  // trial name and group name of a field trial.
+  using ListenForTrialCallback =
+      base::RepeatingCallback<void(const std::string& trial_name,
+                                   const std::string& group_name)>;
+
   // Creates and initializes the global instance. |bus| must not be null.
   static void Initialize(dbus::Bus* bus);
+
+  // Creates and initializes the global instance for testing. |bus| must not be
+  // null.
+  static void InitializeForTesting(dbus::Bus* bus,
+                                   const base::FilePath& expected_dir,
+                                   ListenForTrialCallback callback);
 
   // Creates and initializes a fake global instance.
   static void InitializeFake();
@@ -44,6 +63,19 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DBUS_FEATURED) FeaturedClient {
   // Initialize/Shutdown should be used instead.
   FeaturedClient();
   virtual ~FeaturedClient();
+
+  // Returns true if the base component of |path| is of the format
+  // `TrialName,GroupName`. |active_group| will contain the trial name and group
+  // name specified by the filename. If the return value is false,
+  // |active_group| will not be modified.
+  //
+  // The platform-side logic that writes these files can be found in
+  // feature::PlatformFeatures::RecordActiveTrial() in chromiumos.
+  static bool ParseTrialFilename(const base::FilePath& path,
+                                 base::FieldTrial::ActiveGroup& active_group);
+
+ private:
+  friend class FeaturedClientTest;
 };
 }  // namespace ash::featured
 
