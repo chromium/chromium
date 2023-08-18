5213f936daa9680372e0e5a18c2b3a720f7d6c27
Fredrik Hernqvist <fhernqvist@google.com>
2023-08-18 16:34:12 +0000

    Update privacy filtered fields to keep CrasUnified
    This was generated with the script.
    
    The tracing was added in
    https://chromium-review.googlesource.com/c/chromium/src/+/4777252
    and cl/557767168
    
    Bug: 1459263
    
    Change-Id: I1190145419339b7174ba3ca5a1017493cfe57322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4790083
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Fredrik Hernqvist <fhernqvist@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185228}

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -439,6 +439,10 @@ constexpr MessageInfo const* kChromeGraphicsPipelineComplexMessages[] = {
 constexpr MessageInfo kChromeGraphicsPipeline = {
     kChromeGraphicsPipelineIndices, kChromeGraphicsPipelineComplexMessages};
 
+// Proto Message: CrasUnified
+constexpr int kCrasUnifiedIndices[] = {1, 2, 3, 4, 5, 6, 7, -1};
+constexpr MessageInfo kCrasUnified = {kCrasUnifiedIndices, nullptr};
+
 // Proto Message: TrackEvent
 constexpr int kTrackEventIndices[] = {
     1,    2,    3,    5,    6,    9,    10,   11,   12,   16,   17,   22,
@@ -447,7 +451,7 @@ constexpr int kTrackEventIndices[] = {
     1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014,
     1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1028,
     1031, 1032, 1033, 1034, 1036, 1038, 1039, 1040, 1041, 1042, 1046, 1047,
-    1048, 1049, 1050, 1051, 1052, -1};
+    1048, 1049, 1050, 1051, 1052, 1053, -1};
 constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     nullptr,
     nullptr,
@@ -525,7 +529,8 @@ constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     &kMacAUHALStream,
     &kLinuxAlsaOutput,
     &kLinuxPulseOutput,
-    &kChromeGraphicsPipeline};
+    &kChromeGraphicsPipeline,
+    &kCrasUnified};
 constexpr MessageInfo kTrackEvent = {kTrackEventIndices,
                                      kTrackEventComplexMessages};
 
