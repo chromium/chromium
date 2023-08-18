22ce2aa5b02f7638bfc8856658c958897d260e73
Mikhail Khokhlov <khokhlov@google.com>
2023-08-18 09:37:15 +0000

    [Tracing] Set shared memory size hint for all backends
    Right now the shared memory size configuration relies on default values
    defined in multiple places. This causes inconsistencies (e.g. the
    default in Perfetto IPC producer is different from the one in Chrome
    producer backend).
    
    This CL specifies an explicit size hint for all backends during
    Perfetto initialization.
    
    Bug: b/295340301
    Change-Id: Iab8529112713df7ab1ce06180df16f29aacebbd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4789048
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185093}

--- services/tracing/public/cpp/perfetto/perfetto_traced_process.cc
+++ services/tracing/public/cpp/perfetto/perfetto_traced_process.cc
@@ -305,6 +305,7 @@ void PerfettoTracedProcess::SetupClientLibrary(bool enable_consumer) {
   init_args.custom_backend = tracing_backend_.get();
   init_args.backends |= perfetto::kCustomBackend;
   init_args.shmem_batch_commits_duration_ms = 1000;
+  init_args.shmem_size_hint_kb = 4 * 1024;
   init_args.use_monotonic_clock = true;
   init_args.disallow_merging_with_system_tracks = true;
 #if BUILDFLAG(IS_POSIX)
