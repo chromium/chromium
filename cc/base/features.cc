fbea606ae01e255cd140c9e41fa8f1c3f3ccb948
Vikas Soni <vikassoni@chromium.org>
2023-08-21 20:01:04 +0000

    [UseDMSAAForTiles] Enable by default on Vulkan.
    Enabling DMSAA by default on Android. Note that this
    flag only controls Vulkan launch. GL launch will be
    controlled by a separate flag.
    
    Bug: 1366068
    Change-Id: If9a4e8df3952dd9c16a83d11d4124dd1ca52d165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798853
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186032}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -69,9 +69,11 @@ BASE_FEATURE(kNormalPriorityImageDecoding,
              "NormalPriorityImageDecoding",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Note that kUseDMSAAForTiles only controls vulkan launch on android. We will
+// be using a separate flag to control the launch on GL.
 BASE_FEATURE(kUseDMSAAForTiles,
              "UseDMSAAForTiles",
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_ANDROID)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
