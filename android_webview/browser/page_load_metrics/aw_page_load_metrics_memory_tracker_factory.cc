00e23dbc64f6e03a126a01795276b01611601512
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-21 12:20:38 +0000

    [Android] Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated, this CL
    replaces it by the first one.
    This also ensures that, instead of applying `WrapUnique` to a `new` raw pointer, it directly uses `make_unique`.
    
    Change-Id: I617d5e005deed406560c07fddb75756aa542eb07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4796312
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185808}

--- android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.cc
+++ android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.cc
@@ -33,9 +33,10 @@ bool AwPageLoadMetricsMemoryTrackerFactory::ServiceIsCreatedWithBrowserContext()
   return base::FeatureList::IsEnabled(features::kV8PerFrameMemoryMonitoring);
 }
 
-KeyedService* AwPageLoadMetricsMemoryTrackerFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+AwPageLoadMetricsMemoryTrackerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new page_load_metrics::PageLoadMetricsMemoryTracker();
+  return std::make_unique<page_load_metrics::PageLoadMetricsMemoryTracker>();
 }
 
 content::BrowserContext*
--- android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h
+++ android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h
@@ -27,7 +27,7 @@ class AwPageLoadMetricsMemoryTrackerFactory
   // BrowserContextKeyedServiceFactory:
   bool ServiceIsCreatedWithBrowserContext() const override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 
   content::BrowserContext* GetBrowserContextToUse(
