3bf3dd21371199aa391d2bd70dc15882ac7e633d
Paul Adedeji <pauladedeji@google.com>
2023-08-21 21:52:38 +0000

    [ntp][modules] Disable First Run Experience for Desktop NTP Modules ToT
    Change-Id: Ic45de4a1689f4c2cee9ddf9505f6f8c55e804dca
    Bug: 1474067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799429
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186096}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -171,7 +171,7 @@ BASE_FEATURE(kNtpModulesDragAndDrop,
 // If enabled, the first run experience for Modular NTP Desktop v1 will show.
 BASE_FEATURE(kNtpModulesFirstRunExperience,
              "NtpModulesFirstRunExperience",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, modules will be loaded but not shown. This is useful to determine
 // if a user would have seen modules in order to counterfactually log or
