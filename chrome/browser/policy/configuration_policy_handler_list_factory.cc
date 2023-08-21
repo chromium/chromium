cec72c034fd816a457fb445489462a2d3577ad05
Mingyu Lei <leimy@chromium.org>
2023-08-21 08:50:20 +0000

    CCNS BFCache: add Fuchsia in the supported platform of enterprise policy
    When adding the policy, we put Fuchsia under "future_on" and skip the
    platform when handling the policy.
    
    This CL adds the platform back since there is no reason to exclude it.
    
    Bug: 1444300
    Change-Id: I1b83b5c2675694a49157074edfe4d97d629ce06f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4741554
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185750}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1961,14 +1961,12 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     prefs::kGoogleSearchSidePanelEnabled,
     base::Value::Type::BOOLEAN },
 #endif  // BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_FUCHSIA)
-  { key::kBeforeunloadEventCancelByPreventDefaultEnabled,
-    policy_prefs::kBeforeunloadEventCancelByPreventDefaultEnabled,
-    base::Value::Type::BOOLEAN},
-#if !BUILDFLAG(IS_FUCHSIA)
   { key::kAllowBackForwardCacheForCacheControlNoStorePageEnabled,
     policy_prefs::kAllowBackForwardCacheForCacheControlNoStorePageEnabled,
     base::Value::Type::BOOLEAN},
-#endif  // !BUILDFLAG(IS_FUCHSIA)
+  { key::kBeforeunloadEventCancelByPreventDefaultEnabled,
+    policy_prefs::kBeforeunloadEventCancelByPreventDefaultEnabled,
+    base::Value::Type::BOOLEAN},
   { key::kDefaultMidiSetting,
     prefs::kManagedDefaultMidi,
     base::Value::Type::INTEGER },
