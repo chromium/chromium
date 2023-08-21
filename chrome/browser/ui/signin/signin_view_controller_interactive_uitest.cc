86bcb53bb9e198c0058a2bb9e65b4aaf91d2fe7c
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2023-08-21 07:27:54 +0000

    gardening: Disable flaky SignInViewControllerBrowserTest.SyncConfirmationDefaultFocus
    Bug: 1474306
    Change-Id: I5d7251ab1ca29fb98ce4a45718e28e2a5f35bcc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4796932
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Owners-Override: Yoshisato Yanagisawa <yyanagisawa@google.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1185729}

--- chrome/browser/ui/signin/signin_view_controller_interactive_uitest.cc
+++ chrome/browser/ui/signin/signin_view_controller_interactive_uitest.cc
@@ -118,7 +118,8 @@ IN_PROC_BROWSER_TEST_F(SignInViewControllerBrowserTest, Accelerators) {
 // Tests that the confirm button is focused by default in the sync confirmation
 // dialog.
 IN_PROC_BROWSER_TEST_F(SignInViewControllerBrowserTest,
-                       SyncConfirmationDefaultFocus) {
+                       // TODO(crbug.com/1474306): Re-enable this test
+                       DISABLED_SyncConfirmationDefaultFocus) {
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   // This test runs in the main profile.
   EXPECT_TRUE(
