b6fef9e9bdfb4785a87efb91f97e80abd4055c6b
Tanmoy Mollik <triploblastic@chromium.org>
2023-08-21 13:40:51 +0000

    [iOS] Remove kFinchIosFre flag
    This experiment is no longer needed. Cleanup cl for the flag and the
    histogram that this feature records.
    
    Fixed: 1468059
    Bug: 1447028
    Change-Id: I654406d82217ecd6fe4f7713fde0ad91db046aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4793790
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1185837}

--- ios/chrome/browser/ui/first_run/first_run_coordinator.mm
+++ ios/chrome/browser/ui/first_run/first_run_coordinator.mm
@@ -11,7 +11,6 @@
 #import "base/notreached.h"
 #import "base/time/time.h"
 #import "components/signin/public/base/signin_metrics.h"
-#import "components/signin/public/base/signin_switches.h"
 #import "ios/chrome/browser/first_run/first_run_metrics.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -55,10 +54,6 @@
 }
 
 - (void)start {
-  bool isFinchFreEnabled = base::FeatureList::IsEnabled(switches::kFinchIosFre);
-  // TODO(crbug.com/1447028): Remove this histogram when the finch experiment
-  // is finished.
-  base::UmaHistogramBoolean("FirstRun.iOSFreFinchEnabled", isFinchFreEnabled);
   [self presentScreen:[self.screenProvider nextScreenType]];
   void (^completion)(void) = ^{
     base::UmaHistogramEnumeration("FirstRun.Stage", first_run::kStart);
