76cbe541935b9c95cea1564e6de8b204f100f81f
dpapad <dpapad@chromium.org>
2023-08-21 11:09:47 +0000

    WebUI: Migrate c/b/r/support_tool tests to WebUIMochaBrowserTest.
    WebUIMochaBrowserTest is the new way of running WebUI tests without any
    dependency to the obsolete js2gtest and test_api.js mechanism.
    
    Bug: 1457360
    Change-Id: Iea4e74463dab7ab0f6f7506d2bc8d5736a16a992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4794009
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Reviewed-by: Irem Uguz <iremuguz@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1185792}

---
+++ chrome/test/data/webui/support_tool/support_tool_focus_test.cc
@@ -0,0 +1,30 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/common/chrome_features.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "content/public/test/browser_test.h"
+
+class SupportToolTest : public WebUIMochaFocusTest {
+ protected:
+  SupportToolTest() {
+    scoped_feature_list_.InitWithFeatures(
+        {
+            features::kSupportTool,
+            features::kSupportToolScreenshot,
+            features::kSupportToolCopyTokenButton,
+        },
+        {});
+    set_test_loader_host(chrome::kChromeUISupportToolHost);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(SupportToolTest, All) {
+  RunTest("support_tool/support_tool_test.js", "mocha.run()");
+}
