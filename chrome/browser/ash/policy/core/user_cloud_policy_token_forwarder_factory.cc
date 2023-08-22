151c63e64909598835bdd08e49b176626147a046
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-22 12:26:28 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    This also ensures that, instead of applying `WrapUnique` to a `new` raw
    ptr, it directly uses `make_unique`.
    
    Change-Id: If1bf9774340cec61898ea9cd4dd71cbc785650de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798099
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186432}

--- chrome/browser/ash/policy/core/user_cloud_policy_token_forwarder_factory.cc
+++ chrome/browser/ash/policy/core/user_cloud_policy_token_forwarder_factory.cc
@@ -34,7 +34,8 @@ UserCloudPolicyTokenForwarderFactory::UserCloudPolicyTokenForwarderFactory()
 UserCloudPolicyTokenForwarderFactory::~UserCloudPolicyTokenForwarderFactory() =
     default;
 
-KeyedService* UserCloudPolicyTokenForwarderFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+UserCloudPolicyTokenForwarderFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   Profile* profile = static_cast<Profile*>(context);
   UserCloudPolicyManagerAsh* manager = profile->GetUserCloudPolicyManagerAsh();
@@ -42,7 +43,8 @@ KeyedService* UserCloudPolicyTokenForwarderFactory::BuildServiceInstanceFor(
       IdentityManagerFactory::GetForProfile(profile);
   if (!manager || !identity_manager)
     return nullptr;
-  return new UserCloudPolicyTokenForwarder(manager, identity_manager);
+  return std::make_unique<UserCloudPolicyTokenForwarder>(manager,
+                                                         identity_manager);
 }
 
 bool UserCloudPolicyTokenForwarderFactory::ServiceIsCreatedWithBrowserContext()
--- chrome/browser/ash/policy/core/user_cloud_policy_token_forwarder_factory.h
+++ chrome/browser/ash/policy/core/user_cloud_policy_token_forwarder_factory.h
@@ -33,7 +33,7 @@ class UserCloudPolicyTokenForwarderFactory : public ProfileKeyedServiceFactory {
   ~UserCloudPolicyTokenForwarderFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
