ec863f413cd4d748a09d06fecd83c32dcf484034
Greg Thompson <grt@chromium.org>
2023-08-22 21:06:46 +0000

    [windows] Use ordinary imports for Ro{I,Uni}nitialize
    Bug: 1384167
    Change-Id: I12d3b3a8b99788e889098c67cd646217a35991d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800454
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186814}

--- base/win/scoped_winrt_initializer.cc
+++ base/win/scoped_winrt_initializer.cc
@@ -5,61 +5,14 @@
 #include "base/win/scoped_winrt_initializer.h"
 
 #include <roapi.h>
-#include <windows.h>
-
-#include <ostream>
 
 #include "base/check_op.h"
-#include "base/threading/scoped_thread_priority.h"
 #include "base/win/com_init_util.h"
-#include "base/win/core_winrt_util.h"
 
 namespace base::win {
 
-namespace {
-
-FARPROC LoadComBaseFunction(const char* function_name) {
-  static HMODULE const handle = []() {
-    // Mitigate the issues caused by loading DLLs on a background thread
-    // (http://crbug/973868).
-    SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY();
-    return ::LoadLibraryEx(L"combase.dll", nullptr,
-                           LOAD_LIBRARY_SEARCH_SYSTEM32);
-  }();
-  return handle ? ::GetProcAddress(handle, function_name) : nullptr;
-}
-
-decltype(&::RoInitialize) GetRoInitializeFunction() {
-  static decltype(&::RoInitialize) const function =
-      reinterpret_cast<decltype(&::RoInitialize)>(
-          LoadComBaseFunction("RoInitialize"));
-  return function;
-}
-
-decltype(&::RoUninitialize) GetRoUninitializeFunction() {
-  static decltype(&::RoUninitialize) const function =
-      reinterpret_cast<decltype(&::RoUninitialize)>(
-          LoadComBaseFunction("RoUninitialize"));
-  return function;
-}
-
-HRESULT CallRoInitialize(RO_INIT_TYPE init_type) {
-  auto ro_initialize_func = GetRoInitializeFunction();
-  if (!ro_initialize_func)
-    return E_FAIL;
-  return ro_initialize_func(init_type);
-}
-
-void CallRoUninitialize() {
-  auto ro_uninitialize_func = GetRoUninitializeFunction();
-  if (ro_uninitialize_func)
-    ro_uninitialize_func();
-}
-
-}  // namespace
-
 ScopedWinrtInitializer::ScopedWinrtInitializer()
-    : hr_(CallRoInitialize(RO_INIT_MULTITHREADED)) {
+    : hr_(::RoInitialize(RO_INIT_MULTITHREADED)) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 #if DCHECK_IS_ON()
   if (SUCCEEDED(hr_))
@@ -72,7 +25,7 @@ ScopedWinrtInitializer::ScopedWinrtInitializer()
 ScopedWinrtInitializer::~ScopedWinrtInitializer() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (SUCCEEDED(hr_))
-    CallRoUninitialize();
+    ::RoUninitialize();
 }
 
 bool ScopedWinrtInitializer::Succeeded() const {
