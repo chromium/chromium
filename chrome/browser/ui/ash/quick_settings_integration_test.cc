5d557e13697e44d4aa59b96a60b579df3c46fd42
James Cook <jamescook@chromium.org>
2023-08-22 18:23:10 +0000

    crosier: Add QuickSettingsLacrosIntegrationTest.ManagedDeviceInfo
    The test verifies that lacros is launched when the managed information
    text is clicked. It does not (yet) verify the URL. Doing that will
    require more test helpers to be added (perhaps remote Kombucha).
    
    Include-Ci-Only-Tests: true
    Bug: b/291732302
    Test: chromeos_integration_tests
    Change-Id: Ice8f3367e3f2a17732cbb80dc1453601178143c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803345
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186680}

--- chrome/browser/ui/ash/quick_settings_integration_test.cc
+++ chrome/browser/ui/ash/quick_settings_integration_test.cc
@@ -9,9 +9,12 @@
 #include "ash/system/model/system_tray_model.h"
 #include "base/test/gtest_tags.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ash/crosapi/browser_manager_observer.h"
+#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/chromeos/crosier/interactive_ash_test.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/base/interaction/state_observer.h"
 
 namespace ash {
 namespace {
@@ -88,6 +91,79 @@ IN_PROC_BROWSER_TEST_F(QuickSettingsIntegrationTest, ManagedDeviceInfo) {
 
                   Log("Test complete"));
 }
+
+// Testing with Lacros requires a VM or DUT.
+#if BUILDFLAG(IS_CHROMEOS_DEVICE)
+
+// Observes the crosapi browser manager to detect Lacros startup. Signals with a
+// boolean that is true if lacros is running, false otherwise.
+class TestBrowserManagerObserver : public ui::test::ObservationStateObserver<
+                                       bool,
+                                       crosapi::BrowserManager,
+                                       crosapi::BrowserManagerObserver> {
+ public:
+  TestBrowserManagerObserver()
+      : ObservationStateObserver(crosapi::BrowserManager::Get()) {}
+
+  // ui::test::ObservationStateObserver:
+  bool GetStateObserverInitialState() const override {
+    // Tests in this suite do not have a lacros browser window open at start.
+    return false;
+  }
+
+  // crosapi::BrowserManagerObserver:
+  void OnStateChanged() override {
+    const bool is_lacros_running =
+        crosapi::BrowserManager::Get()->IsRunningOrWillRun();
+    OnStateObserverStateChanged(is_lacros_running);
+  }
+};
+
+// Tests of ash quick settings that assume the primary browser is Lacros.
+class QuickSettingsLacrosIntegrationTest : public QuickSettingsIntegrationTest {
+ public:
+  QuickSettingsLacrosIntegrationTest() {
+    feature_list_.InitAndEnableFeature(features::kLacrosOnly);
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(QuickSettingsLacrosIntegrationTest, ManagedDeviceInfo) {
+  ASSERT_TRUE(crosapi::browser_util::IsLacrosEnabled());
+
+  base::AddFeatureIdTagToTestResult(
+      "screenplay-f0a4bd11-a2bc-4596-a4fc-2a62c7277965");
+
+  SetupContextWidget();
+
+  // Simulate enterprise information being available.
+  GetEnterpriseDomainModel()->SetDeviceEnterpriseInfo(
+      DeviceEnterpriseInfo{"example.com", /*active_directory_managed=*/false,
+                           ManagementDeviceMode::kChromeEnterprise});
+
+  DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(TestBrowserManagerObserver, kLacrosState);
+
+  RunTestSequence(
+      Log("Opening quick settings bubble"),
+      PressButton(kUnifiedSystemTrayElementId),
+      WaitForShow(kQuickSettingsViewElementId),
+
+      Log("Pressing enterprise managed view"),
+      ObserveState(kLacrosState,
+                   std::make_unique<TestBrowserManagerObserver>()),
+      PressButton(kEnterpriseManagedView),
+
+      Log("Waiting for the lacros browser to start"),
+      WaitForState(kLacrosState, true),
+
+      // TODO(jamescook): Verify that Lacros loaded the management URL.
+
+      Log("Test complete"));
+}
+
+#endif  // BUILDFLAG(IS_CHROMEOS_DEVICE)
 
 }  // namespace
 }  // namespace ash
