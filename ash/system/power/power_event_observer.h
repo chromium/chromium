1a360d1cf8539ec61d65dcfa44111d9fc6c6f365
Prashant Malani <pmalani@chromium.org>
2023-08-22 23:16:40 +0000

    Introduce external display timeout deferral for lock screen
    When a Thunderbolt-enabled Chromebook is connected to a Thunderbolt
    display with the lid closed and on the lock/login screen, the display
    enters DisplayPort alternate mode. A subsequent unlock/login causes
    the display to switch to Thunderbolt alternate mode.
    
    This switch leads to a transient disconnection of connected displays.
    The PowerEventObserver, seeing no displays, immediately throws the
    system back to the lock screen, which is a bad UX.
    
    So, introduce a timer which starts on unlock and login, and during
    which, the number of displays going to 0 *does not* generate a lock
    screen. This gives enough time for the display stack to re-enumerate
    the displays. At the end of the timeout, we can attempt to lock
    the screen again (if the system state allows it).
    
    The value of the timer timeout is taken from cros-config, and is the
    same as what is used by Chrome OS power_manager to control similar
    behaviour, but for suspend/resume instead of the lock screen.
    
    Devices which don't have the cros-config setting will remain unaffected
    by this change.
    
    Bug: b/280752163
    Change-Id: I839869f8f4e82c62ed25e5041d6956e76aa1f522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4654993
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186907}

--- ash/system/power/power_event_observer.h
+++ ash/system/power/power_event_observer.h
@@ -11,6 +11,7 @@
 #include "ash/login_status.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "base/compiler_specific.h"
+#include "base/timer/timer.h"
 #include "base/unguessable_token.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 
@@ -116,6 +117,9 @@ class ASH_EXPORT PowerEventObserver
   // can be stopped for all root windows when device suspends.
   void OnCompositorsReadyForSuspend();
 
+  // Starts |wait_for_external_display_timer_|.
+  void StartExternalDisplayTimer();
+
   LockState lock_state_ = LockState::kUnlocked;
   chromeos::PowerManagerClient::LidState lid_state_ =
       chromeos::PowerManagerClient::LidState::OPEN;
@@ -138,6 +142,11 @@ class ASH_EXPORT PowerEventObserver
   base::UnguessableToken block_suspend_token_;
 
   std::unique_ptr<LockOnSuspendUsage> lock_on_suspend_usage_;
+
+  // Amount of time (in seconds) to wait for external displays when a display
+  // mode change occurs and the lid is closed.
+  int defer_external_display_timeout_s_ = 0;
+  base::OneShotTimer wait_for_external_display_timer_;
 
   base::WeakPtrFactory<PowerEventObserver> weak_factory_{this};
 };
