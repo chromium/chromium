2fa345bbd4ab35da936a4f919e15173c6f4e28d2
Jood Hajeer <jood@google.com>
2023-08-22 17:05:37 +0000

    [iOS] Add EGTest for the unified settings row for UNO users.
    This CL introduces appConfigurationForTestCase in the
    ManageSyncSettingsTestCase to allow enabling the UNO flag for tests.
    And introduces the first test; which covers verifying the entry point
    for the unified settings page in the main settings menu.
    
    Bug: 1462123
    Change-Id: I73c23da2d26a7ab1c2b6b4aee7e523aaae5d542b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798513
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186630}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_egtest.mm
@@ -2,17 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/strings/sys_string_conversions.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
+#import "ios/chrome/browser/ui/authentication/signin_matchers.h"
+#import "ios/chrome/browser/ui/authentication/views/views_constants.h"
 #import "ios/chrome/browser/ui/settings/google_services/google_services_settings_constants.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_constants.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#import "ui/base/l10n/l10n_util.h"
 
 using chrome_test_util::ManageSyncSettingsButton;
+using chrome_test_util::SettingsAccountButton;
+using chrome_test_util::SettingsSignInRowMatcher;
 
 // Integration tests using the Google services settings screen.
 @interface ManageSyncSettingsTestCase : WebHttpServerChromeTestCase
@@ -20,7 +28,23 @@ using chrome_test_util::ManageSyncSettingsButton;
 
 @implementation ManageSyncSettingsTestCase
 
-// Tests that Sync settings is dismissed when the primary account is removed.
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  if ([self
+          isRunningTest:@selector(testSignoutWhileManageSyncSettingsOpened)]) {
+    config.features_disabled.push_back(
+        syncer::kReplaceSyncPromosWithSignInPromos);
+  }
+  if ([self isRunningTest:@selector
+            (testShowingUnifiedAccountSettings_SyncToSigninEnabled)]) {
+    config.features_enabled.push_back(
+        syncer::kReplaceSyncPromosWithSignInPromos);
+  }
+  return config;
+}
+
+// Tests that Sync settings is dismissed when the primary account is removed and
+// kReplaceSyncPromosWithSignInPromos is disabled.
 - (void)testSignoutWhileManageSyncSettingsOpened {
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
@@ -34,6 +58,47 @@ using chrome_test_util::ManageSyncSettingsButton;
   [ChromeEarlGreyUI waitForAppToIdle];
   [[EarlGrey selectElementWithMatcher:scrollViewMatcher]
       assertWithMatcher:grey_nil()];
+}
+
+// Tests that unified account settings row is showing, and the Sync row is not
+// showing when kReplaceSyncPromosWithSignInPromos enabled.
+- (void)testShowingUnifiedAccountSettings_SyncToSigninEnabled {
+  [super setUp];
+
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+
+  [ChromeEarlGreyUI openSettingsMenu];
+
+  // Sign in with fake identity using the settings sign-in promo.
+  [ChromeEarlGreyUI
+      tapSettingsMenuButton:chrome_test_util::SettingsSignInRowMatcher()];
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                          kIdentityButtonControlIdentifier)]
+      performAction:grey_tap()];
+  [[EarlGrey
+      selectElementWithMatcher:chrome_test_util::IdentityCellMatcherForEmail(
+                                   fakeIdentity.userEmail)]
+      performAction:grey_tap()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(
+                                   grey_accessibilityLabel(
+                                       l10n_util::GetNSStringF(
+                                           IDS_IOS_FIRST_RUN_SIGNIN_CONTINUE_AS,
+                                           base::SysNSStringToUTF16(
+                                               fakeIdentity.userGivenName))),
+                                   grey_sufficientlyVisible(), nil)]
+      performAction:grey_tap()];
+  [ChromeEarlGreyUI waitForAppToIdle];
+
+  // Verify the Sync settings row is not showing.
+  [SigninEarlGrey verifySyncUIIsHidden];
+
+  // Verify the account settings row is showing.
+  [[EarlGrey selectElementWithMatcher:SettingsAccountButton()]
+      assertWithMatcher:grey_notNil()];
+
+  [super tearDown];
 }
 
 @end
