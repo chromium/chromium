0b4bc9bec78950650b91a28fb563e247be4f5215
Josip Sokcevic <sokcevic@chromium.org>
2023-08-22 19:40:12 +0000

    [checkperms] Ignore gitlinks in permission checks
    Once git submodules are added to this repository, checkperms script
    would start scanning content of all git submodules, and it would result
    in checking more than two orders of magnitude files (total of 60M vs
    428k).
    
    This patch will retain current functionality after git submodules are
    added.
    
    R=brucedawson
    
    Change-Id: Idbdc39a8017d12164bdf41ac98224f723f79831d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4795589
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Josip Sokcevic <sokcevic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186748}

--- tools/checkperms/checkperms.py
+++ tools/checkperms/checkperms.py
@@ -412,7 +412,16 @@ class ApiAllFilesAtOnceBase(ApiBase):
 
 class ApiGit(ApiAllFilesAtOnceBase):
   def _get_all_files(self):
-    return capture([git_name, 'ls-files'], cwd=self.root_dir).splitlines()
+    # ls-files -s outputs in the following format:
+    # <mode> <SP> <object> <SP> <stage> <TAB> <file>
+    # Example output:
+    # 100644 08f1a0445babd612aab0a9934eabc0a7ae3d48ef 0<TAB>README.md
+    # 160000 e9f9f56b0dee9032936d23c81c8246ae0ffe36bd 0<TAB>build
+    out = capture([git_name, 'ls-files', '-s'], cwd=self.root_dir).splitlines()
+
+    # Return only actual files, which start with 100, and ignore anything else.
+    # See: https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
+    return [x.split(maxsplit=3)[-1] for x in out if x.startswith('100')]
 
 
 def get_scm(dir_path, bare):
