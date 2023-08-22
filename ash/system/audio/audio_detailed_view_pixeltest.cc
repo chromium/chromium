fe57e2a1643b84435a5bbe23b0cd89b41c688b06
Jiaming Cheng <jiamingc@google.com>
2023-08-22 22:38:44 +0000

    Aligned noise cancellation toggle in the qs detailed page
    Also aligned agc info row.
    
    The corresponding pixel test is added.
    
    Screenshot:
    https://screenshot.googleplex.com/AUD3CBzX2VxPHEe
    
    Bug: b/295392848
    Change-Id: I93e3303c46182b45e6d84328863aeac6ee4a709c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4800573
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186882}

--- ash/system/audio/audio_detailed_view_pixeltest.cc
+++ ash/system/audio/audio_detailed_view_pixeltest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
+
 #include "ash/constants/ash_features.h"
 #include "ash/system/tray/tray_detailed_view.h"
 #include "ash/system/unified/unified_system_tray.h"
@@ -10,11 +12,16 @@
 #include "ash/test/pixel/ash_pixel_differ.h"
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
 #include "base/test/scoped_feature_list.h"
+#include "chromeos/ash/components/audio/cras_audio_handler.h"
+#include "chromeos/ash/components/dbus/audio/audio_node.h"
+#include "chromeos/ash/components/dbus/audio/fake_cras_audio_client.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
+
+constexpr uint64_t kInternalMicId = 10003;
 
 // Pixel tests for the quick settings audio detailed view.
 class AudioDetailedViewPixelTest : public AshTestBase {
@@ -50,6 +57,39 @@ TEST_F(AudioDetailedViewPixelTest, Basics) {
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "qs_audio_detailed_view",
       /*revision_number=*/6, detailed_view));
+}
+
+TEST_F(AudioDetailedViewPixelTest, ShowNoiseCancellationButton) {
+  // Setup for showing noise cancellation button.
+  auto* client = FakeCrasAudioClient::Get();
+  auto* audio_handler = CrasAudioHandler::Get();
+  auto internal_mic_node = AudioNode(
+      true, kInternalMicId, false, kInternalMicId, 0, "Fake Mic",
+      "INTERNAL_MIC", "Internal Mic", false /* is_active*/, 0 /* pluged_time */,
+      1, cras::EFFECT_TYPE_NOISE_CANCELLATION, 0);
+  AudioNodeList node_list;
+  node_list.push_back(internal_mic_node);
+  client->SetAudioNodesAndNotifyObserversForTesting(node_list);
+  client->SetNoiseCancellationSupported(true);
+  audio_handler->RequestNoiseCancellationSupported(base::DoNothing());
+  audio_handler->SwitchToDevice(AudioDevice(internal_mic_node), true,
+                                CrasAudioHandler::ACTIVATE_BY_USER);
+
+  UnifiedSystemTray* system_tray = GetPrimaryUnifiedSystemTray();
+  system_tray->ShowBubble();
+  ASSERT_TRUE(system_tray->bubble());
+
+  system_tray->bubble()
+      ->unified_system_tray_controller()
+      ->ShowAudioDetailedView();
+
+  TrayDetailedView* detailed_view =
+      system_tray->bubble()->quick_settings_view()->GetDetailedViewForTest();
+  ASSERT_TRUE(detailed_view);
+
+  EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
+      "qs_audio_detailed_view",
+      /*revision_number=*/0, detailed_view));
 }
 
 }  // namespace ash
