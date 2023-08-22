9eca0044e17a3594d4719f6b57362b1e3448d310
Federica Germinario <fedegermi@google.com>
2023-08-22 08:50:39 +0000

    [ios] Check that observerList has no observer on destruction
    This CL checks that observer list in UrlLoadingNotifierBrowserAgent is
    empty when it is destroyed.
    
    Bug: 1430392
    Change-Id: Ib76a13ea4494c35ca23b880f6a392faec97f5753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798094
    Commit-Queue: David Jean <djean@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186357}

--- ios/chrome/browser/url_loading/url_loading_notifier_browser_agent.h
+++ ios/chrome/browser/url_loading/url_loading_notifier_browser_agent.h
@@ -71,7 +71,7 @@ class UrlLoadingNotifierBrowserAgent
 
   explicit UrlLoadingNotifierBrowserAgent(Browser* browser);
 
-  base::ObserverList<UrlLoadingObserverBridge> observers_;
+  base::ObserverList<UrlLoadingObserverBridge, true> observers_;
 };
 
 #endif  // IOS_CHROME_BROWSER_URL_LOADING_URL_LOADING_NOTIFIER_BROWSER_AGENT_H_
