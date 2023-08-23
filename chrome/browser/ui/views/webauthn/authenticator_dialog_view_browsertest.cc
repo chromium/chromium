a3ab5618c0c515e2d2a08fef931609d8a7e9ebce
Lingqi Chi <lingqi@chromium.org>
2023-08-23 02:30:16 +0000

    gardening: disable AuthenticatorDialogViewTest.InvokeUi_manage_devices
    Test is flaky on Mac since https://ci.chromium.org/ui/p/chromium/builders/ci/Mac12%20Tests/12170/overview
    
    Bug: 1474278
    Change-Id: Id5f94709a414728b66308b033eab71d61f63613b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4805443
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1186992}

--- chrome/browser/ui/views/webauthn/authenticator_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/webauthn/authenticator_dialog_view_browsertest.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -172,7 +173,13 @@ IN_PROC_BROWSER_TEST_F(AuthenticatorDialogViewTest, InvokeUi_default) {
 
 // Test that the models decide to show the "Manage devices" button when a phone
 // is listed.
-// TODO(crbug.com/1474278): Re-enable this test
-IN_PROC_BROWSER_TEST_F(AuthenticatorDialogViewTest, InvokeUi_manage_devices) {
+// TODO(crbug.com/1474278): Flaky on Mac, Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_InvokeUi_manage_devices DISABLED_InvokeUi_manage_devices
+#else
+#define MAYBE_InvokeUi_manage_devices InvokeUi_manage_devices
+#endif
+IN_PROC_BROWSER_TEST_F(AuthenticatorDialogViewTest,
+                       MAYBE_InvokeUi_manage_devices) {
   ShowAndVerifyUi();
 }
