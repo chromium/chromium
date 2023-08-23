e6c70a0d4c6f5957f69102a98c8d1198bc1635f0
Daniel Rubery <drubery@chromium.org>
2023-08-23 23:33:37 +0000

    Create DownloadBubblePasswordPromptView
    This CL creates a new view encapsulating the password textfield and
    associated labels that are shown when prompting the user for a password
    with their deep scan.
    
    Bug: 1466284
    Change-Id: If3bf58b7e14b57f6cd58c07053607ce411afff93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4792602
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187558}

---
+++ chrome/browser/ui/views/download/bubble/download_bubble_password_prompt_view.h
@@ -0,0 +1,39 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_BUBBLE_PASSWORD_PROMPT_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_BUBBLE_PASSWORD_PROMPT_VIEW_H_
+
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/layout/table_layout_view.h"
+
+namespace views {
+class Label;
+class Textfield;
+}  // namespace views
+
+class DownloadBubblePasswordPromptView : public views::TableLayoutView {
+ public:
+  enum class State {
+    kValid,
+    kInvalid,
+    kInvalidEmpty,
+  };
+
+  METADATA_HEADER(DownloadBubblePasswordPromptView);
+  DownloadBubblePasswordPromptView();
+  ~DownloadBubblePasswordPromptView() override;
+
+  void SetState(State state);
+  const std::u16string& GetText() const;
+
+ private:
+  bool IsError(State state) const;
+  std::u16string GetErrorMessage(State state) const;
+
+  raw_ptr<views::Label> error_message_ = nullptr;
+  raw_ptr<views::Textfield> password_field_ = nullptr;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_DOWNLOAD_BUBBLE_DOWNLOAD_BUBBLE_PASSWORD_PROMPT_VIEW_H_
