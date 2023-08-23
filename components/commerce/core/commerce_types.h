4656a455f2b8ff881bcb0461f8cc3370d84dbd1d
Mei Liang <meiliang@chromium.org>
2023-08-23 22:25:12 +0000

    Move shopping service types to separate file
    This patch moves the public structs and type definitions that are used
    in the commerce component to a separate file and target. This will
    allow features that pass this kind of information around to do so
    while only adding a limited dependency chain (versus the very large one
    required by the shopping service).
    
    Bug: b:297191826
    Change-Id: I7c5e05f6d9df9f669efa5b1a2403b3954ee76af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807244
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187527}

---
+++ components/commerce/core/commerce_types.h
@@ -0,0 +1,123 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_COMMERCE_CORE_COMMERCE_TYPES_H_
+#define COMPONENTS_COMMERCE_CORE_COMMERCE_TYPES_H_
+
+#include <string>
+#include <vector>
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "url/gurl.h"
+
+namespace commerce {
+
+// Data containers that are provided by the above callbacks:
+
+// Information returned by the discount APIs.
+struct DiscountInfo {
+  DiscountInfo();
+  DiscountInfo(const DiscountInfo&);
+  DiscountInfo& operator=(const DiscountInfo&);
+  ~DiscountInfo();
+
+  std::string description_detail;
+  absl::optional<std::string> terms_and_conditions;
+  std::string value_in_text;
+  std::string discount_code;
+  int64_t id;
+  bool is_merchant_wide;
+  double expiry_time_sec;
+};
+
+// Information returned by the merchant info APIs.
+struct MerchantInfo {
+  MerchantInfo();
+  MerchantInfo(const MerchantInfo&);
+  MerchantInfo& operator=(const MerchantInfo&);
+  MerchantInfo(MerchantInfo&&);
+  MerchantInfo& operator=(MerchantInfo&&) = default;
+  ~MerchantInfo();
+
+  float star_rating;
+  uint32_t count_rating;
+  GURL details_page_url;
+  bool has_return_policy;
+  float non_personalized_familiarity_score;
+  bool contains_sensitive_content;
+  bool proactive_message_disabled;
+};
+
+// Position of current price with respect to the typical price range.
+enum class PriceBucket {
+  kUnknown = 0,
+  kLowPrice = 1,
+  kTypicalPrice = 2,
+  kHighPrice = 3,
+  kMaxValue = kHighPrice,
+};
+
+// Information returned by the price insights APIs.
+struct PriceInsightsInfo {
+  PriceInsightsInfo();
+  PriceInsightsInfo(const PriceInsightsInfo&);
+  PriceInsightsInfo& operator=(const PriceInsightsInfo&);
+  ~PriceInsightsInfo();
+
+  absl::optional<uint64_t> product_cluster_id;
+  std::string currency_code;
+  absl::optional<int64_t> typical_low_price_micros;
+  absl::optional<int64_t> typical_high_price_micros;
+  absl::optional<std::string> catalog_attributes;
+  std::vector<std::tuple<std::string, int64_t>> catalog_history_prices;
+  absl::optional<GURL> jackpot_url;
+  PriceBucket price_bucket;
+  bool has_multiple_catalogs;
+};
+
+// Information returned by the product info APIs.
+struct ProductInfo {
+ public:
+  ProductInfo();
+  ProductInfo(const ProductInfo&);
+  ProductInfo& operator=(const ProductInfo&);
+  ~ProductInfo();
+
+  std::string title;
+  std::string product_cluster_title;
+  GURL image_url;
+  absl::optional<uint64_t> product_cluster_id;
+  absl::optional<uint64_t> offer_id;
+  std::string currency_code;
+  int64_t amount_micros{0};
+  absl::optional<int64_t> previous_amount_micros;
+  std::string country_code;
+
+ private:
+  friend class ShoppingService;
+
+  // This is used to track whether the server provided an image with the rest
+  // of the product info. This value being |true| does not necessarily mean an
+  // image is available in the ProductInfo struct (as it is flag gated) and is
+  // primarily used for recording metrics.
+  bool server_image_available{false};
+};
+
+// Callbacks and typedefs for various accessors in the shopping service.
+using DiscountsMap = std::map<GURL, std::vector<DiscountInfo>>;
+using DiscountInfoCallback = base::OnceCallback<void(const DiscountsMap&)>;
+using MerchantInfoCallback =
+    base::OnceCallback<void(const GURL&, absl::optional<MerchantInfo>)>;
+using PriceInsightsInfoCallback =
+    base::OnceCallback<void(const GURL&,
+                            const absl::optional<PriceInsightsInfo>&)>;
+using ProductInfoCallback =
+    base::OnceCallback<void(const GURL&,
+                            const absl::optional<const ProductInfo>&)>;
+using IsShoppingPageCallback =
+    base::OnceCallback<void(const GURL&, absl::optional<bool>)>;
+
+}  // namespace commerce
+
+#endif  // COMPONENTS_COMMERCE_CORE_COMMERCE_TYPES_H_
--- components/commerce/core/shopping_service.h
+++ components/commerce/core/shopping_service.h
@@ -22,6 +22,7 @@
 #include "base/supports_user_data.h"
 #include "base/uuid.h"
 #include "components/commerce/core/account_checker.h"
+#include "components/commerce/core/commerce_types.h"
 #include "components/commerce/core/proto/commerce_subscription_db_content.pb.h"
 #include "components/commerce/core/subscriptions/commerce_subscription.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -122,34 +123,6 @@ class WebWrapper;
 enum class SubscriptionType;
 struct CommerceSubscription;
 
-// Information returned by the product info APIs.
-struct ProductInfo {
- public:
-  ProductInfo();
-  ProductInfo(const ProductInfo&);
-  ProductInfo& operator=(const ProductInfo&);
-  ~ProductInfo();
-
-  std::string title;
-  std::string product_cluster_title;
-  GURL image_url;
-  absl::optional<uint64_t> product_cluster_id;
-  absl::optional<uint64_t> offer_id;
-  std::string currency_code;
-  int64_t amount_micros{0};
-  absl::optional<int64_t> previous_amount_micros;
-  std::string country_code;
-
- private:
-  friend class ShoppingService;
-
-  // This is used to track whether the server provided an image with the rest
-  // of the product info. This value being |true| does not necessarily mean an
-  // image is available in the ProductInfo struct (as it is flag gated) and is
-  // primarily used for recording metrics.
-  bool server_image_available{false};
-};
-
 // A struct that keeps track of cached product info related data about a url.
 struct ProductInfoCacheEntry {
  public:
@@ -174,67 +147,6 @@ struct ProductInfoCacheEntry {
   std::unique_ptr<ProductInfo> product_info;
 };
 
-// Information returned by the merchant info APIs.
-struct MerchantInfo {
-  MerchantInfo();
-  MerchantInfo(const MerchantInfo&);
-  MerchantInfo& operator=(const MerchantInfo&);
-  MerchantInfo(MerchantInfo&&);
-  MerchantInfo& operator=(MerchantInfo&&) = default;
-  ~MerchantInfo();
-
-  float star_rating;
-  uint32_t count_rating;
-  GURL details_page_url;
-  bool has_return_policy;
-  float non_personalized_familiarity_score;
-  bool contains_sensitive_content;
-  bool proactive_message_disabled;
-};
-
-// Position of current price with respect to the typical price range.
-enum class PriceBucket {
-  kUnknown = 0,
-  kLowPrice = 1,
-  kTypicalPrice = 2,
-  kHighPrice = 3,
-  kMaxValue = kHighPrice,
-};
-
-// Information returned by the price insights APIs.
-struct PriceInsightsInfo {
-  PriceInsightsInfo();
-  PriceInsightsInfo(const PriceInsightsInfo&);
-  PriceInsightsInfo& operator=(const PriceInsightsInfo&);
-  ~PriceInsightsInfo();
-
-  absl::optional<uint64_t> product_cluster_id;
-  std::string currency_code;
-  absl::optional<int64_t> typical_low_price_micros;
-  absl::optional<int64_t> typical_high_price_micros;
-  absl::optional<std::string> catalog_attributes;
-  std::vector<std::tuple<std::string, int64_t>> catalog_history_prices;
-  absl::optional<GURL> jackpot_url;
-  PriceBucket price_bucket;
-  bool has_multiple_catalogs;
-};
-
-// Information returned by the discount APIs.
-struct DiscountInfo {
-  DiscountInfo();
-  DiscountInfo(const DiscountInfo&);
-  DiscountInfo& operator=(const DiscountInfo&);
-  ~DiscountInfo();
-
-  std::string description_detail;
-  absl::optional<std::string> terms_and_conditions;
-  std::string value_in_text;
-  std::string discount_code;
-  int64_t id;
-  bool is_merchant_wide;
-  double expiry_time_sec;
-};
-
 // Types of shopping pages from backend.
 enum class ShoppingPageType {
   kUnknown = 0,
@@ -249,22 +161,6 @@ enum class ShoppingPageType {
   kBuyingGuidePage = 9,
   kMaxValue = kBuyingGuidePage,
 };
-
-// Callbacks for querying a single URL or observing information from all
-// navigated urls.
-using ProductInfoCallback =
-    base::OnceCallback<void(const GURL&,
-                            const absl::optional<const ProductInfo>&)>;
-using MerchantInfoCallback =
-    base::OnceCallback<void(const GURL&, absl::optional<MerchantInfo>)>;
-using PriceInsightsInfoCallback =
-    base::OnceCallback<void(const GURL&,
-                            const absl::optional<PriceInsightsInfo>&)>;
-using IsShoppingPageCallback =
-    base::OnceCallback<void(const GURL&, absl::optional<bool>)>;
-
-using DiscountsMap = std::map<GURL, std::vector<DiscountInfo>>;
-using DiscountInfoCallback = base::OnceCallback<void(const DiscountsMap&)>;
 
 // A callback for getting updated ProductInfo for a bookmark. This provides the
 // bookmark ID being updated, the URL, and the product info.
