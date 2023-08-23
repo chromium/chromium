91d266d6a74db4b70208ec87a54b2e251572511d
Arthur Milchior <arthurmilchior@chromium.org>
2023-08-23 20:45:03 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I3ae2194a8d89d3460a93b3c833cc6916a3a0266c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799915
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187479}

--- chrome/browser/ash/login/security_token_session_controller_factory.cc
+++ chrome/browser/ash/login/security_token_session_controller_factory.cc
@@ -49,7 +49,8 @@ SecurityTokenSessionControllerFactory::GetInstance() {
   return instance.get();
 }
 
-KeyedService* SecurityTokenSessionControllerFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+SecurityTokenSessionControllerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   // The service should only exist for the primary and the sign-in profiles.
   Profile* profile = Profile::FromBrowserContext(context);
@@ -75,9 +76,9 @@ KeyedService* SecurityTokenSessionControllerFactory::BuildServiceInstanceFor(
   chromeos::CertificateProviderService* certificate_provider_service =
       chromeos::CertificateProviderServiceFactory::GetForBrowserContext(
           context);
-  return new SecurityTokenSessionController(is_primary_profile, local_state,
-                                            primary_user,
-                                            certificate_provider_service);
+  return std::make_unique<SecurityTokenSessionController>(
+      is_primary_profile, local_state, primary_user,
+      certificate_provider_service);
 }
 
 bool SecurityTokenSessionControllerFactory::ServiceIsCreatedWithBrowserContext()
--- chrome/browser/ash/login/security_token_session_controller_factory.h
+++ chrome/browser/ash/login/security_token_session_controller_factory.h
@@ -33,7 +33,7 @@ class SecurityTokenSessionControllerFactory
   ~SecurityTokenSessionControllerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   bool ServiceIsCreatedWithBrowserContext() const override;
 };
