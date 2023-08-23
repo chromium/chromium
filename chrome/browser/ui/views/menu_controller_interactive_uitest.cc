6edee64d60fe8b4a55935216f146c6c6fcd020aa
Jenny Zhang <jennyz@chromium.org>
2023-08-23 17:40:57 +0000

    Enable MenuControllerMnemonicTestNoMatch.NoMatch for lacros memory test.
    I have enabled the test running on linux-lacros-asan-lsan-rel
    for 100 times, and all passed. See:
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-lacros-asan-lsan-rel/294/overview
    
    Therefore, re-enabled the test for running on builder
    "chromium/ci/linux-lacros-asan-lsan-rel".
    
    Bug: 1444104
    Change-Id: I6b9b06ffc70af6f4ef71817668852fda37cf63eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4804080
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187364}

--- chrome/browser/ui/views/menu_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/menu_controller_interactive_uitest.cc
@@ -78,10 +78,4 @@ VIEW_TEST(MenuControllerMnemonicTestTitleMatch, MAYBE_TitleMatch)
 typedef MenuControllerMnemonicTest<ui::VKEY_A,0>
     MenuControllerMnemonicTestNoMatch;
 
-// TODO(https://crbug.com/1444104): Re-enable the test once fixed.
-#if BUILDFLAG(IS_CHROMEOS_LACROS) && defined(ADDRESS_SANITIZER)
-#define MAYBE_NoMatch DISABLED_NoMatch
-#else
-#define MAYBE_NoMatch NoMatch
-#endif
-VIEW_TEST(MenuControllerMnemonicTestNoMatch, MAYBE_NoMatch)
+VIEW_TEST(MenuControllerMnemonicTestNoMatch, NoMatch)
