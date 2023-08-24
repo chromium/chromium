421e4f80a4313849156863b055d259987cb67b88
Nohemi Fernandez <fernandex@chromium.org>
2023-08-24 16:47:45 +0000

    [iOS] Use camel case for "Learn More" link in incognito tab grid.
    Fixed: b/295143684
    Change-Id: I73e85c7b5d635f09f6f2e07db412fa3d6543cf4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808110
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187890}

--- ios/chrome/browser/supervised_user/supervised_user_incognito_mode_egtest.mm
+++ ios/chrome/browser/supervised_user/supervised_user_incognito_mode_egtest.mm
@@ -29,8 +29,8 @@ namespace {
 NSString* const kTestSupervisedIncognitoMessage =
     @"Your account is managed by your parent.";
 
-// Label used to find the 'Learn more' link.
-NSString* const kTestLearnMoreLabel = @"Learn more";
+// Label used to find the 'Learn More' link.
+NSString* const kTestLearnMoreLabel = @"Learn More";
 
 }  // namespace
 
@@ -131,7 +131,7 @@ NSString* const kTestLearnMoreLabel = @"Learn more";
                                           kTestSupervisedIncognitoMessage)]
       assertWithMatcher:grey_notNil()];
 
-  // Check that the "Learn more" link works.
+  // Check that the "Learn More" link works.
   [[EarlGrey
       selectElementWithMatcher:grey_allOf(
                                    grey_accessibilityLabel(kTestLearnMoreLabel),
