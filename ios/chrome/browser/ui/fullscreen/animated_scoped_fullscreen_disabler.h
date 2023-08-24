7ccd1c80040ac0dc35735457a8d924d97152bec1
Federica Germinario <fedegermi@google.com>
2023-08-24 15:19:53 +0000

    [ios] Cleanup inside AnimatedScopedFullscreenDisabler
    AnimatedScopedFullscreenDisabler implements methods addObserver and
    removeObserver. However, none of the two methods is ever called in
    the code, this CL removes the dead code.
    
    Bug: 1430392
    Change-Id: Ifa58ed074d1aaf9b82ce6e6bc8939f4dbe4c625f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797737
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187831}

--- ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.h
+++ ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.h
@@ -6,13 +6,9 @@
 #define IOS_CHROME_BROWSER_UI_FULLSCREEN_ANIMATED_SCOPED_FULLSCREEN_DISABLER_H_
 
 #include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
-#include "ios/chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #include "ios/chrome/browser/ui/fullscreen/fullscreen_controller_observer.h"
 
 class FullscreenController;
-class AnimatedScopedFullscreenDisablerObserver;
-@class AnimatedScopedFullscreenDisablerObserverListContainer;
 
 // A helper object that increments FullscrenController's disabled counter for
 // its entire lifetime after calling StartAnimation().  Any UI updates resulting
@@ -21,10 +17,6 @@ class AnimatedScopedFullscreenDisabler : public FullscreenControllerObserver {
  public:
   explicit AnimatedScopedFullscreenDisabler(FullscreenController* controller);
   ~AnimatedScopedFullscreenDisabler() override;
-
-  // Adds and removes AnimatedScopedFullscreenDisablerObservers.
-  void AddObserver(AnimatedScopedFullscreenDisablerObserver* observer);
-  void RemoveObserver(AnimatedScopedFullscreenDisablerObserver* observer);
 
   // Starts the disabling the FullscreenController, animating any resulting UI
   // changes.  The FullscreenController will then remain disabled until this
@@ -36,40 +28,16 @@ class AnimatedScopedFullscreenDisabler : public FullscreenControllerObserver {
       FullscreenController* controller) override;
 
  private:
-  // Helper methods used to implement the animations.
+  // Helper method used to implement the animations.
   void OnAnimationStart();
-  void OnAnimationCompletion();
 
   // The FullscreenController being disabled by this object.
   FullscreenController* controller_ = nullptr;
-  // A container object for the list of observers.
-  __strong AnimatedScopedFullscreenDisablerObserverListContainer*
-      observer_list_container_ = nil;
+
   // Whether this disabler is contributing to `controller_`'s disabled counter.
   bool disabling_ = false;
   // Used to implement animation blocks safely.
   base::WeakPtrFactory<AnimatedScopedFullscreenDisabler> weak_factory_{this};
-};
-
-// Obsever class for listening to animated fullscreen disabling events.
-class AnimatedScopedFullscreenDisablerObserver {
- public:
-  explicit AnimatedScopedFullscreenDisablerObserver() = default;
-  virtual ~AnimatedScopedFullscreenDisablerObserver() = default;
-
-  // Called when the fullscreen disabling animation begins and ends.  If
-  // AnimatedScopedFullscreenDisabler::StartAnimation() is called and for a
-  // FullscreenController that is already disabled, these callbacks will not be
-  // sent.  If the disabler is destroyed before the animation can finish,
-  // FullscreenDisablingAnimationDidFinish() will not be received.
-  virtual void FullscreenDisablingAnimationDidStart(
-      AnimatedScopedFullscreenDisabler* disabler) {}
-  virtual void FullscreenDisablingAnimationDidFinish(
-      AnimatedScopedFullscreenDisabler* disabler) {}
-
-  // Called before the disabler is destructed.
-  virtual void AnimatedFullscreenDisablerDestroyed(
-      AnimatedScopedFullscreenDisabler* disabler) {}
 };
 
 #endif  // IOS_CHROME_BROWSER_UI_FULLSCREEN_ANIMATED_SCOPED_FULLSCREEN_DISABLER_H_
--- ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.mm
+++ ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.mm
@@ -9,122 +9,34 @@
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #import "ios/chrome/common/material_timing.h"
 
-#pragma mark - AnimatedScopedFullscreenDisablerObserverListContainer
-
-// An Objective-C container used to store observers.  This is used to allow
-// correct memory management for use in UIView animation blocks.
-@interface AnimatedScopedFullscreenDisablerObserverListContainer : NSObject {
-  // The AnimatedScopedFullscreenDisablerObservers.
-  base::ObserverList<AnimatedScopedFullscreenDisablerObserver>::Unchecked
-      _observers;
-}
-
-// The disabler passed on initialization.
-@property(nonatomic, readonly) AnimatedScopedFullscreenDisabler* disabler;
-
-// Designated initializer for a container containing `disabler`'s observer list.
-- (instancetype)initWithDisabler:(AnimatedScopedFullscreenDisabler*)disabler
-    NS_DESIGNATED_INITIALIZER;
-- (instancetype)init NS_UNAVAILABLE;
-
-// Adds and removes observers.
-- (void)addObserver:(AnimatedScopedFullscreenDisablerObserver*)observer;
-- (void)removeObserver:(AnimatedScopedFullscreenDisablerObserver*)observer;
-
-// Notifies observers when the animation starts and finishes.
-- (void)onAnimationStarted;
-- (void)onAnimationFinished;
-- (void)onDisablerDestroyed;
-
-@end
-
-@implementation AnimatedScopedFullscreenDisablerObserverListContainer
-@synthesize disabler = _disabler;
-
-- (instancetype)initWithDisabler:(AnimatedScopedFullscreenDisabler*)disabler {
-  if (self = [super init]) {
-    _disabler = disabler;
-    DCHECK(_disabler);
-  }
-  return self;
-}
-
-- (const base::ObserverList<
-    AnimatedScopedFullscreenDisablerObserver>::Unchecked&)observers {
-  return _observers;
-}
-
-- (void)addObserver:(AnimatedScopedFullscreenDisablerObserver*)observer {
-  _observers.AddObserver(observer);
-}
-
-- (void)removeObserver:(AnimatedScopedFullscreenDisablerObserver*)observer {
-  _observers.RemoveObserver(observer);
-}
-
-- (void)onAnimationStarted {
-  for (auto& observer : _observers) {
-    observer.FullscreenDisablingAnimationDidStart(_disabler);
-  }
-}
-
-- (void)onAnimationFinished {
-  for (auto& observer : _observers) {
-    observer.FullscreenDisablingAnimationDidFinish(_disabler);
-  }
-}
-
-- (void)onDisablerDestroyed {
-  for (auto& observer : _observers) {
-    observer.AnimatedFullscreenDisablerDestroyed(_disabler);
-  }
-}
-
-@end
-
-#pragma mark - AnimatedScopedFullscreenDisabler
-
 AnimatedScopedFullscreenDisabler::AnimatedScopedFullscreenDisabler(
     FullscreenController* controller)
     : controller_(controller) {
   DCHECK(controller_);
   controller_->AddObserver(this);
-  observer_list_container_ =
-      [[AnimatedScopedFullscreenDisablerObserverListContainer alloc]
-          initWithDisabler:this];
 }
 
 AnimatedScopedFullscreenDisabler::~AnimatedScopedFullscreenDisabler() {
   if (controller_) {
-    if (disabling_)
+    if (disabling_) {
       controller_->DecrementDisabledCounter();
+    }
     controller_->RemoveObserver(this);
     controller_ = nullptr;
   }
-  [observer_list_container_ onDisablerDestroyed];
-}
-
-void AnimatedScopedFullscreenDisabler::AddObserver(
-    AnimatedScopedFullscreenDisablerObserver* observer) {
-  [observer_list_container_ addObserver:observer];
-}
-
-void AnimatedScopedFullscreenDisabler::RemoveObserver(
-    AnimatedScopedFullscreenDisablerObserver* observer) {
-  [observer_list_container_ removeObserver:observer];
 }
 
 void AnimatedScopedFullscreenDisabler::StartAnimation() {
   // StartAnimation() should be idempotent, so early return if this disabler has
   // already incremented the disabled counter.
-  if (disabling_ || !controller_)
+  if (disabling_ || !controller_) {
     return;
+  }
   disabling_ = true;
 
   if (controller_->IsEnabled()) {
     // Increment the disabled counter in an animation block if the controller is
     // not already disabled.
-    [observer_list_container_ onAnimationStarted];
 
     base::WeakPtr<AnimatedScopedFullscreenDisabler> weak_ptr =
         weak_factory_.GetWeakPtr();
@@ -132,18 +44,13 @@ void AnimatedScopedFullscreenDisabler::StartAnimation() {
     base::RepeatingClosure animation_started = base::BindRepeating(
         &AnimatedScopedFullscreenDisabler::OnAnimationStart, weak_ptr);
 
-    base::RepeatingClosure animation_completed = base::BindRepeating(
-        &AnimatedScopedFullscreenDisabler::OnAnimationCompletion, weak_ptr);
-
     [UIView animateWithDuration:kMaterialDuration1
-        animations:^{
-          if (!animation_started.IsCancelled())
-            animation_started.Run();
-        }
-        completion:^(BOOL finished) {
-          if (!animation_completed.IsCancelled())
-            animation_completed.Run();
-        }];
+                     animations:^{
+                       if (!animation_started.IsCancelled()) {
+                         animation_started.Run();
+                       }
+                     }
+                     completion:nil];
   } else {
     // If `controller_` is already disabled, no animation is necessary.
     controller_->IncrementDisabledCounter();
@@ -160,8 +67,4 @@ void AnimatedScopedFullscreenDisabler::FullscreenControllerWillShutDown(
 void AnimatedScopedFullscreenDisabler::OnAnimationStart() {
   if (controller_)
     controller_->IncrementDisabledCounter();
-}
-
-void AnimatedScopedFullscreenDisabler::OnAnimationCompletion() {
-  [observer_list_container_ onAnimationFinished];
 }
