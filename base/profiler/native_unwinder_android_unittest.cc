f116f8dfd29c9c12a9f3a6680562feb7d6fcf688
Thiabaud Engelbrecht <thiabaud@google.com>
2023-08-24 17:37:42 +0000

    [ssm] Reenable Java test for NativeUnwinderAndroid
    This test was previously disabled in 2020 due to the test failing on
    certain older devices. We believe these devices are no longer in use,
    so we should be safe to re-enable the test.
    
    Change-Id: I14a6baf3cc35306603e0eb8e003ae01d46c01457
    Bug: 1076997, 1475718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808990
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187921}

--- base/profiler/native_unwinder_android_unittest.cc
+++ base/profiler/native_unwinder_android_unittest.cc
@@ -365,8 +365,7 @@ TEST(NativeUnwinderAndroidTest, MAYBE_ResumeUnwinding) {
 }
 
 // Checks that java frames can be unwound through.
-// Disabled, see: https://crbug.com/1076997
-TEST(NativeUnwinderAndroidTest, DISABLED_JavaFunction) {
+TEST(NativeUnwinderAndroidTest, JavaFunction) {
   auto* build_info = base::android::BuildInfo::GetInstance();
   // Due to varying availability of compiled java unwind tables, unwinding is
   // only expected to succeed on > SDK_VERSION_MARSHMALLOW.
