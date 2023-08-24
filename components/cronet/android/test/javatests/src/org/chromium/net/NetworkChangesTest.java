baf10d0036ce8ebfa4cce664e318d8c8c3d67dc1
Stefano Duo <stefanoduo@google.com>
2023-08-24 12:39:05 +0000

    Refactor NetworkChangesTest's test
    Bug: b:290583020
    Change-Id: I4df0e3286c6107303d4cffa34de2948aea581711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4806348
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1187765}

--- components/cronet/android/test/javatests/src/org/chromium/net/NetworkChangesTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/NetworkChangesTest.java
@@ -9,52 +9,55 @@ import static android.system.OsConstants.SOCK_STREAM;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import android.os.ConditionVariable;
 import android.system.Os;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.net.CronetTestRule.OnlyRunNativeCronet;
 import org.chromium.net.impl.CronetLibraryLoader;
 
 import java.io.FileDescriptor;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Test Cronet under different network change scenarios.
  */
 @RunWith(AndroidJUnit4.class)
+@DoNotBatch(reason = "crbug/1459563")
 @OnlyRunNativeCronet
 public class NetworkChangesTest {
     @Rule
     public final CronetTestRule mTestRule = CronetTestRule.withAutomaticEngineStartup();
 
-    /**
-     * Verify NetworkChangeNotifier signals trigger appropriate action, like
-     * aborting pending connect() jobs.
-     */
-    @Test
-    @SmallTest
-    public void testDefaultNetworkChange() throws Exception {
+    private FileDescriptor mSocket;
+
+    @Before
+    public void setUp() throws Exception {
         // Bind a listening socket to a local port. The socket won't be used to accept any
         // connections, but rather to get connection stuck waiting to connect.
-        FileDescriptor s = Os.socket(AF_INET6, SOCK_STREAM, 0);
+        mSocket = Os.socket(AF_INET6, SOCK_STREAM, 0);
         // Bind to 127.0.0.1 and a random port (indicated by special 0 value).
-        Os.bind(s, InetAddress.getByAddress(null, new byte[] {127, 0, 0, 1}), 0);
+        Os.bind(mSocket, InetAddress.getByAddress(null, new byte[] {127, 0, 0, 1}), 0);
         // Set backlog to 0 so connections end up stuck waiting to connect().
-        Os.listen(s, 0);
+        Os.listen(mSocket, 0);
+    }
 
-        // Make URL pointing at this local port, where requests will get stuck connecting.
-        String url = "https://127.0.0.1:" + ((InetSocketAddress) Os.getsockname(s)).getPort();
-
+    @Test
+    @SmallTest
+    public void testDefaultNetworkChangeBeforeConnect_failsWithErrNetChanged() throws Exception {
+        // URL pointing at the local socket, where requests will get stuck connecting.
+        String url = "https://127.0.0.1:" + ((InetSocketAddress) Os.getsockname(mSocket)).getPort();
         // Launch a few requests at this local port.  Four seems to be the magic number where
-        // that request and any further request get stuck connecting.
+        // the last request (and any further request) get stuck connecting.
         TestUrlRequestCallback callback = null;
         UrlRequest request = null;
         for (int i = 0; i < 4; i++) {
@@ -66,17 +69,7 @@ public class NetworkChangesTest {
             request.start();
         }
 
-        // Wait for request to get to connecting stage
-        final AtomicBoolean requestConnecting = new AtomicBoolean();
-        while (!requestConnecting.get()) {
-            request.getStatus(new UrlRequest.StatusListener() {
-                @Override
-                public void onStatus(int status) {
-                    requestConnecting.set(status == UrlRequest.Status.CONNECTING);
-                }
-            });
-            Thread.sleep(100);
-        }
+        waitForConnectingStatus(request);
 
         // Simulate network change which should abort connect jobs
         CronetLibraryLoader.postToInitThread(new Runnable() {
@@ -96,5 +89,18 @@ public class NetworkChangesTest {
                 .contains("Exception in CronetUrlRequest: net::ERR_NETWORK_CHANGED");
         assertThat(((NetworkException) callback.mError).getCronetInternalErrorCode())
                 .isEqualTo(NetError.ERR_NETWORK_CHANGED);
+    }
+
+    private static void waitForConnectingStatus(UrlRequest request) {
+        final ConditionVariable cv = new ConditionVariable(false /* closed */);
+        request.getStatus(new UrlRequest.StatusListener() {
+            @Override
+            public void onStatus(int status) {
+                if (status == UrlRequest.Status.CONNECTING) {
+                    cv.open();
+                }
+            }
+        });
+        cv.block();
     }
 }
