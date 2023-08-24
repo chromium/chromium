dda3d1723fecfc0e9d07f3608053e40a3dbe0f72
Mike Taylor <miketaylr@chromium.org>
2023-08-24 20:27:53 +0000

    Remove unintentional testing of patch_version
    This CL restores the previous state of this test - it was accidentally added when trying to de-duplicate tests that were no longer needed when removing the ForceMajorVersionToMinorPositionInUserAgent policy and feature.
    
    (Also add back a test for the UserAgentReduction policy kForceDisabled state.)
    
    Bug: 1474818
    Change-Id: I06e3638f327a1b924103f7dfa6ea15e0fe7c73a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807408
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1187998}

--- components/embedder_support/user_agent_utils_unittest.cc
+++ components/embedder_support/user_agent_utils_unittest.cc
@@ -1163,11 +1163,11 @@ TEST_F(UserAgentUtilsTest, GetProductAndVersion) {
       GetProductAndVersion(UserAgentReductionEnterprisePolicyState::kDefault);
   EXPECT_TRUE(re2::RE2::FullMatch(product, kChromeProductVersionRegex,
                                   &major_version, &minor_version,
-                                  &build_version, &patch_version));
+                                  &build_version));
   EXPECT_EQ(major_version, version_info::GetMajorVersionNumber());
   EXPECT_EQ(minor_version, "0");
   EXPECT_NE(build_version, "0");
-  EXPECT_EQ(patch_version, "0");
+  // Patch version cannot be tested as it would be set in a release branch.
 
   // (1b) Policies: UserAgentReduction force enabled.
   product = GetProductAndVersion(
@@ -1179,6 +1179,17 @@ TEST_F(UserAgentUtilsTest, GetProductAndVersion) {
   EXPECT_EQ(minor_version, "0");
   EXPECT_EQ(build_version, "0");
   EXPECT_EQ(patch_version, "0");
+
+  // (1c) Policies:: UserAgentReduction force disabled.
+  product = GetProductAndVersion(
+      UserAgentReductionEnterprisePolicyState::kForceDisabled);
+  EXPECT_TRUE(re2::RE2::FullMatch(product, kChromeProductVersionRegex,
+                                  &major_version, &minor_version,
+                                  &build_version));
+  EXPECT_EQ(major_version, version_info::GetMajorVersionNumber());
+  EXPECT_EQ(minor_version, "0");
+  EXPECT_NE(build_version, "0");
+  // Patch version cannot be tested as it would be set in a release branch.
 
   // (2) Features: UserAgentReduction enabled with version.
   scoped_feature_list.Reset();
