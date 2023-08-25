febe344def58fc2f21ab76b0432492bf3fea99f3
Kannan Vijayan <kv-github@kvijayan.com>
2023-08-25 13:15:15 -0400

    Rollback getAppliedStyle changes due to command crashes.

--- third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
+++ third_party/blink/renderer/bindings/core/v8/record_replay_interface.cc
@@ -2236,7 +2236,7 @@ function CSS_getAppliedRules({ node: nodeRrpId }) {
   let rules = gCssRulesByNodeRrpId.get(nodeRrpId);
   const data = {};
 
-  if (!rules && isBlinkInstanceOf(nodeObj, Node)) {
+  if (!rules && isBlinkInstanceOf(nodeObj, Element)) {
     const nodeId = getBlinkNodeIdByRrpId(nodeRrpId);
 
     // NOTE: CSS domain commands are not accessible via `sendMessage`, so we have to get the data indirectly.
@@ -2244,11 +2244,6 @@ function CSS_getAppliedRules({ node: nodeRrpId }) {
     const cdpMatchedStyles = fromJsGetMatchedStylesForNode(nodeId);
     rules = convertCdpToRrpCssRules(nodeObj, cdpMatchedStyles);
     gCssRulesByNodeRrpId.set(nodeRrpId, rules);
-  }
-  else {
-    // The target is not a node.
-    log(`[RuntimeWarning] CSS.getAppliedRules called with non-node: ${nodeRrpId} ${isBlinkObject(nodeObj)} ${nodeObj?.constructor?.name} ${nodeObj?.constructor}.`);
-    rules = [];
   }
 
   return { rules, data };
