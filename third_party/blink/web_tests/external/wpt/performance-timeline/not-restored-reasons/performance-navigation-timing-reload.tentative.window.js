742e7ec59602dcff56d24a09e51dcbeb52163656
Yuki Shiino <yukishiino@chromium.org>
2023-08-25 08:53:11 +0000

    Revert "[bfcache] Introduce NotRestoredReasons interface"
    This reverts commit c5d373e378134b9594f80045f747f5b5b8966402.
    
    Reason for revert: https://crbug.com/1474767
    
    Original change's description:
    > [bfcache] Introduce NotRestoredReasons interface
    >
    > This CL introduces NotRestoredReasons interface to make the implementation match the spec draft[1].
    > This also changes NotRestoredReasons attribute's name from `blocked` to `preventedBackForwardCache` and its return value, thus changing WPT as well.
    >
    > [1]: https://github.com/whatwg/html/pull/9360
    >
    > Bug: 1472637
    > Change-Id: I4dcecd5a012c36c883933ed3d04c776bfe844f6c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4770594
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1185723}
    
    (cherry picked from commit f71c5c4c6a42eba6026e5f93241215189dfdd820)
    
    Bug: 1472637
    Change-Id: Ie2e4322abd062a4cdff14f9f78c8651c06453ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4805368
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1187060}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812798
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5938@{#550}
    Cr-Branched-From: 2b50cb4bcc2318034581a816714d9535dc38966d-refs/heads/main@{#1181205}

--- third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-reload.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/not-restored-reasons/performance-navigation-timing-reload.tentative.window.js
@@ -27,7 +27,7 @@ promise_test(async t => {
   await assertBFCacheEligibility(rc1, /*shouldRestoreFromBFCache=*/ false);
   await assertNotRestoredReasonsEquals(
       rc1,
-      /*preventedBackForwardCache=*/ "yes",
+      /*blocked=*/ true,
       /*url=*/ rc1_url,
       /*src=*/ null,
       /*id=*/ null,
