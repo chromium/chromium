c6c8e0dfd9e552ea2e4bd6a8714a210b515e5904
Chris Lu <thegreenfrog@chromium.org>
2023-08-25 18:27:54 +0000

    [ios] Fix Set Up List Icon Magic Stack Polish Bugs
    When Set Up List is in the Magic Stack, the icons should be a smaller
    size.
    
    Fixed: 1474006
    Change-Id: I2f60123eef18ec95821ec1e0d0dfbc42c6fc9f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812015
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1188440}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_icon.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_icon.mm
@@ -10,6 +10,7 @@
 #import "components/sync/base/features.h"
 #import "ios/chrome/browser/ntp/set_up_list_item_type.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
@@ -17,10 +18,13 @@ namespace {
 
 // Constants related to icon sizing.
 constexpr CGFloat kIconSize = 36;
+constexpr CGFloat kMagicStackIconSize = 30;
 constexpr CGFloat kCompactIconSize = 26;
 constexpr CGFloat kSymbolPointSize = 18;
 constexpr CGFloat kSparkleSize = 72;
 constexpr CGFloat kSparkleOffset = (kSparkleSize - kIconSize) / 2;
+constexpr CGFloat kMagicStackSparkleOffset =
+    (kSparkleSize - kMagicStackIconSize) / 2;
 constexpr CGFloat kIconSquareContainerRadius = 7.0f;
 
 // The amount of rotation for the icons, during the animation.
@@ -45,9 +49,11 @@ UIImageView* IconForSymbol(NSString* symbol,
   UIImage* image = DefaultSymbolWithConfiguration(symbol, config);
   UIImageView* icon = [[UIImageView alloc] initWithImage:image];
   icon.translatesAutoresizingMaskIntoConstraints = NO;
-  CGFloat icon_width = compact_layout ? kCompactIconSize : kIconSize;
+  CGFloat nonCompactIconWidth =
+      IsMagicStackEnabled() ? kMagicStackIconSize : kIconSize;
+  CGFloat iconWidth = compact_layout ? kCompactIconSize : nonCompactIconWidth;
   [NSLayoutConstraint activateConstraints:@[
-    [icon.widthAnchor constraintEqualToConstant:icon_width],
+    [icon.widthAnchor constraintEqualToConstant:iconWidth],
     [icon.heightAnchor constraintEqualToAnchor:icon.widthAnchor],
   ]];
   return icon;
@@ -61,8 +67,9 @@ UIView* IconInSquareContainer(UIImageView* icon, NSString* color) {
 
   [square_view addSubview:icon];
   AddSameCenterConstraints(icon, square_view);
+  CGFloat iconWidth = IsMagicStackEnabled() ? kMagicStackIconSize : kIconSize;
   [NSLayoutConstraint activateConstraints:@[
-    [square_view.widthAnchor constraintEqualToConstant:kIconSize],
+    [square_view.widthAnchor constraintEqualToConstant:iconWidth],
     [square_view.heightAnchor constraintEqualToAnchor:square_view.widthAnchor],
   ]];
   return square_view;
@@ -90,7 +97,9 @@ UIImageView* DefaultBrowserIcon(BOOL compact_layout) {
   [container addSubview:icon];
   AddSameConstraints(icon, container);
   // Set the widths.
-  CGFloat icon_width = compact_layout ? kCompactIconSize : kIconSize;
+  CGFloat nonCompactIconWidth =
+      IsMagicStackEnabled() ? kMagicStackIconSize : kIconSize;
+  CGFloat icon_width = compact_layout ? kCompactIconSize : nonCompactIconWidth;
   // The white background circle must be smaller so that the edges don't show.
   CGFloat white_background_width = icon_width - 2;
   [NSLayoutConstraint activateConstraints:@[
@@ -154,8 +163,9 @@ UIView* IconInSquare(NSString* symbol,
   symbol_view.translatesAutoresizingMaskIntoConstraints = NO;
   [square_view addSubview:symbol_view];
   AddSameCenterConstraints(symbol_view, square_view);
+  CGFloat iconWidth = IsMagicStackEnabled() ? kMagicStackIconSize : kIconSize;
   [NSLayoutConstraint activateConstraints:@[
-    [square_view.widthAnchor constraintEqualToConstant:kIconSize],
+    [square_view.widthAnchor constraintEqualToConstant:iconWidth],
     [square_view.heightAnchor constraintEqualToAnchor:square_view.widthAnchor],
   ]];
   return square_view;
@@ -273,7 +283,7 @@ UIView* IconInSquare(NSString* symbol,
       }
 
       UIImageView* iconImage = IconForSymbol(
-          kSyncCircleSymbol, _compactLayout || _inSquare,
+          kSyncCircleSymbol, _compactLayout,
           @[ [UIColor whiteColor], [UIColor colorNamed:kGreen500Color] ]);
       return _inSquare ? IconInSquareContainer(iconImage, kGreen500Color)
                        : iconImage;
@@ -308,8 +318,10 @@ UIView* IconInSquare(NSString* symbol,
   // This image view does not initially have an image. The animation frames
   // are loaded on demand.
   UIImageView* imageView = [[UIImageView alloc] initWithImage:nil];
+  CGFloat sparkleOffset =
+      IsMagicStackEnabled() ? kMagicStackSparkleOffset : kSparkleOffset;
   imageView.frame =
-      CGRectMake(-kSparkleOffset, -kSparkleOffset, kSparkleSize, kSparkleSize);
+      CGRectMake(-sparkleOffset, -sparkleOffset, kSparkleSize, kSparkleSize);
   return imageView;
 }
 
