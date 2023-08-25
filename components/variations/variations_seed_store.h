e60c2c522a14e95798037aa7d011cccc1739577e
Chan Li <chanli@chromium.org>
2023-08-25 00:42:18 +0000

    Revert "variations: Add SeedStore override for CrOS."
    This reverts commit ba0105e3fcee8ca37509c772bf53d62c6e7e9ae5.
    
    Reason for revert: test `EarlyBootSeedStoreTest.LoadSafeSeed_Unspecified` added by this CL is failing on https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome
    
    First failed build https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/35356/overview
    
    Original change's description:
    > variations: Add SeedStore override for CrOS.
    >
    > CrOS early boot needs to have its own notion of what the "safe seed" is
    > (and when to use it), so override the VariationsSeedStore class's
    > LoadSafeSeed method to load a distinct seed from the one in local state.
    >
    > BUG=b:263975722
    > TEST=included unit tests
    >
    > Change-Id: I87c5135bb6438056f5018c722c888c430da8c35d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4685411
    > Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    > Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1187997}
    
    BUG=b:263975722
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: If31ab38b4955881eaa791abb74b6a7cd031f2c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812159
    Owners-Override: Chan Li <chanli@chromium.org>
    Commit-Queue: Chan Li <chanli@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188113}

--- components/variations/variations_seed_store.h
+++ components/variations/variations_seed_store.h
@@ -17,7 +17,6 @@
 #include "components/variations/metrics.h"
 #include "components/variations/proto/variations_seed.pb.h"
 #include "components/variations/seed_response.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class PrefService;
 class PrefRegistrySimple;
@@ -102,8 +101,7 @@ class COMPONENT_EXPORT(VARIATIONS) VariationsSeedStore {
   // Side effect: Upon failing to read or validate the safe seed, clears all
   // of the safe seed pref values.
   //
-  // Virtual for testing and for early-boot CrOS experiments to use a different
-  // safe seed.
+  // Virtual for testing.
   [[nodiscard]] virtual bool LoadSafeSeed(VariationsSeed* seed,
                                           ClientFilterableState* client_state);
 
@@ -155,15 +153,6 @@ class COMPONENT_EXPORT(VARIATIONS) VariationsSeedStore {
   static VerifySignatureResult VerifySeedSignatureForTesting(
       const std::string& seed_bytes,
       const std::string& base64_seed_signature);
-
- protected:
-  // Verify an already-loaded |seed_data| along with its |base64_seed_signature|
-  // and, if verification passes, parse it into |*seed|.
-  [[nodiscard]] LoadSeedResult VerifyAndParseSeed(
-      VariationsSeed* seed,
-      const std::string& seed_data,
-      const std::string& base64_seed_signature,
-      absl::optional<VerifySignatureResult>* verify_signature_result);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(VariationsSeedStoreTest, VerifySeedSignature);
