1293667fe4263ee9b1a546ab5a6916c5fc356390
Collin Baker <collinbaker@chromium.org>
2023-08-28 17:09:40 +0000

    Remove third_party/rust gnrt dependencies
    gnrt was moved to a Cargo-managed build and no longer uses vendored
    crates in third_party/rust. Remove the crates not used elsewhere.
    
    Bug: 1475069
    Change-Id: I102ae8deeced53656fe08ea9a65732df31399d0b
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Change-Id: I102ae8deeced53656fe08ea9a65732df31399d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815127
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189025}

--- third_party/rust/indexmap/v1/crate/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // If "std" is explicitly requested, don't bother probing the target for it.
-    match std::env::var_os("CARGO_FEATURE_STD") {
-        Some(_) => autocfg::emit("has_std"),
-        None => autocfg::new().emit_sysroot_crate("std"),
-    }
-    autocfg::new().emit_rustc_version(1, 51);
-    autocfg::rerun_path("build.rs");
-}
