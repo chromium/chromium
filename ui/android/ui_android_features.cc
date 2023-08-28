195c45455799b8fa1458ece8a751f0391edac1ca
Ted Choc <tedchoc@chromium.org>
2023-08-28 16:41:14 +0000

    [M117 branch 5938] [Android] Disable new select file path that breaks non-audio/image mime types.
    Original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/4545876
    
    (cherry picked from commit 9ca485cdb8e648f82d1be6cc946e8ca3b282737e)
    
    Bug: 1475995
    Change-Id: Ied770b43b85239e6d8642766f602f27eff11779e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4815007
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Auto-Submit: Ted Choc <tedchoc@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1188613}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4818106
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5938@{#669}
    Cr-Branched-From: 2b50cb4bcc2318034581a816714d9535dc38966d-refs/heads/main@{#1181205}

--- ui/android/ui_android_features.cc
+++ ui/android/ui_android_features.cc
@@ -12,7 +12,7 @@ BASE_FEATURE(kConvertTrackpadEventsToMouse,
 
 BASE_FEATURE(kDeprecatedExternalPickerFunction,
              "DeprecatedExternalPickerFunction",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kReportAllAvailablePointerTypes,
              "ReportAllAvailablePointerTypes",
