a419963fe89b8c5cc6811ac271f35fae2485a7f9
Jack Yammine <jyammine@google.com>
2023-08-28 13:39:48 +0000

    [waffle]Enable CR2023 style for search engine choice
    Bug: b/280753754
    Change-Id: Ib421cf91a878ff9778a0cd265cda828683a76dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813244
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1188941}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui.cc
@@ -84,6 +84,8 @@ SearchEngineChoiceUI::SearchEngineChoiceUI(content::WebUI* web_ui)
 
   source->AddString("choiceList", GetChoiceListJSON(profile_.get()));
 
+  webui::SetupChromeRefresh2023(source);
+
   webui::SetupWebUIDataSource(
       source,
       base::make_span(kSearchEngineChoiceResources,
