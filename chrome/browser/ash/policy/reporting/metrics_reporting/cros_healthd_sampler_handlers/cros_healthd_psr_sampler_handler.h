a0967d1bfa907b39ddf0c2a52517e39f4cfc35b5
Hong Xu <xuhong@chromium.org>
2023-08-29 21:41:16 +0000

    When fetching PSR info has failed, try again
    Fetching PSR info may be racey, e.g., b/297413010. Try again if it
    fails.
    
    This CL is expected to work after crrev.com/c/4813187
    
    Bug: b:280068539
    Change-Id: Id5ac977fa9627c969007da06cccba32ca16a24f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4820433
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189767}

--- chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_psr_sampler_handler.h
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_psr_sampler_handler.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_HEALTHD_SAMPLER_HANDLERS_CROS_HEALTHD_PSR_SAMPLER_HANDLER_H_
 #define CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_HEALTHD_SAMPLER_HANDLERS_CROS_HEALTHD_PSR_SAMPLER_HANDLER_H_
 
+#include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_sampler_handler.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "components/reporting/metrics/sampler.h"
@@ -18,7 +20,7 @@ namespace cros_healthd = ::ash::cros_healthd::mojom;
 // PSR.
 class CrosHealthdPsrSamplerHandler : public CrosHealthdSamplerHandler {
  public:
-  CrosHealthdPsrSamplerHandler() = default;
+  CrosHealthdPsrSamplerHandler();
 
   CrosHealthdPsrSamplerHandler(const CrosHealthdPsrSamplerHandler&) = delete;
   CrosHealthdPsrSamplerHandler& operator=(const CrosHealthdPsrSamplerHandler&) =
@@ -29,8 +31,20 @@ class CrosHealthdPsrSamplerHandler : public CrosHealthdSamplerHandler {
   // CrosHealthdSamplerHandler override
   void HandleResult(OptionalMetricCallback callback,
                     cros_healthd::TelemetryInfoPtr result) const override;
-};
 
+ private:
+  // Implementation of `HandleResult`. If it fails to obtain PSR info, it will
+  // get PSR info from healthd again for at most `num_retries_left` times with
+  // itself as the callback. A zero `num_retries_left` implies no retry on
+  // failure.
+  void HandleResultImpl(OptionalMetricCallback callback,
+                        size_t num_retries_left,
+                        cros_healthd::TelemetryInfoPtr result) const;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+  base::WeakPtrFactory<CrosHealthdPsrSamplerHandler> weak_ptr_factory_
+      GUARDED_BY_CONTEXT(sequence_checker_){this};
+};
 }  // namespace reporting
 
 #endif  // CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_HEALTHD_SAMPLER_HANDLERS_CROS_HEALTHD_PSR_SAMPLER_HANDLER_H_
