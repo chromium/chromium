7fd8f61b0c987e8fb9479c06a1f4730f11036e3f
mark a. foltz <mfoltz@chromium.org>
2023-08-29 22:52:57 +0000

    [Copyright] Fix copyright headers in Chromium.
    This CL updates copyright headers in /device
    to the current format:
    
    // Copyright $year The Chromium Authors
    // Use of this source code is governed by a BSD-style license that can be
    // found in the LICENSE file.
    
    Generated with:
    
    ./tools/git/mffr.py -g "*.cc" -g "*.h" -g "*.mm" -g "*.py" -g "*.js" -g "*.java" -g "*.mojom" -g "*.xml" -g "*.jinja" -g "*.gn" -g "*.gni" -g "*.star" -g "*.icon" -g "*.ts" -g "*.css" -g "*.json" -g "*.html" -g "*.proto" -g "*.idl" -g "*.pins" -g "*.yml" -g "*.sh" 'Chromium Authors\. All rights reserved\.' 'Chromium Authors'
    
    Additional manual fixes were made to files that have extra "(C)"
    strings, or where line breaks were wrong.
    
    This CL was uploaded by git cl split.
    
    R=michaelfsun@google.com
    
    Bug: 1457007
    Change-Id: I2bde92090de98526ccf3cb774016412ad8f8a9ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824313
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Michael Sun <michaelfsun@google.com>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189803}

--- device/bluetooth/floss/bluetooth_remote_gatt_service_floss.cc
+++ device/bluetooth/floss/bluetooth_remote_gatt_service_floss.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #include "device/bluetooth/floss/bluetooth_remote_gatt_service_floss.h"
