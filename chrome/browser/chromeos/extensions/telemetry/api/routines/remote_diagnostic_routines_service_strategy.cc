33f7b74562d20ecdf8a5a7f81f2498c928a2cecb
Bastian Kersting <bkersting@google.com>
2023-08-29 07:26:08 +0000

    [Telemetry] Add a strategy for routines service
    This change adds a strategy type for accessing the
    TelemetryDiagnosticRoutinesService mojo connection.
    This prepares for the different ways of accessing
    the remote in Ash vs Lacros Chrome.
    
    This is part of a bigger effort to add a new diagnostics API, please
    visit go/dd-telemetry-extension-diag-revisit for more context.
    
    Bug: b/268125453, b/268125594
    Change-Id: I8a907e4ca726d11d28943e642c1c20808527e81c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4783813
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1189379}

---
+++ chrome/browser/chromeos/extensions/telemetry/api/routines/remote_diagnostic_routines_service_strategy.cc
@@ -0,0 +1,93 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/extensions/telemetry/api/routines/remote_diagnostic_routines_service_strategy.h"
+
+#include <memory>
+
+#include "base/notreached.h"
+#include "build/chromeos_buildflags.h"
+#include "chromeos/crosapi/mojom/telemetry_diagnostic_routine_service.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/telemetry_extension/routines/telemetry_diagnostic_routine_service_ash.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/lacros/lacros_service.h"
+#endif  // BUILDFLAG (IS_CHROMEOS_LACROS)
+
+namespace chromeos {
+
+namespace {
+
+namespace crosapi = ::crosapi::mojom;
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+class RemoteDiagnosticRoutineServiceStrategyAsh
+    : public RemoteDiagnosticRoutineServiceStrategy {
+ public:
+  RemoteDiagnosticRoutineServiceStrategyAsh()
+      : routine_service_(
+            ash::TelemetryDiagnosticsRoutineServiceAsh::Factory::Create(
+                remote_diagnostic_service_.BindNewPipeAndPassReceiver())) {}
+
+  ~RemoteDiagnosticRoutineServiceStrategyAsh() override = default;
+
+  // `RemoteDiagnosticRoutineServiceStrategy`:
+  mojo::Remote<crosapi::TelemetryDiagnosticRoutinesService>& GetRemoteService()
+      override {
+    return remote_diagnostic_service_;
+  }
+
+ private:
+  mojo::Remote<crosapi::TelemetryDiagnosticRoutinesService>
+      remote_diagnostic_service_;
+
+  std::unique_ptr<crosapi::TelemetryDiagnosticRoutinesService> routine_service_;
+};
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+class RemoteDiagnosticRoutineServiceStrategyLacros
+    : public RemoteDiagnosticRoutineServiceStrategy {
+ public:
+  RemoteDiagnosticRoutineServiceStrategyLacros() = default;
+
+  ~RemoteDiagnosticRoutineServiceStrategyLacros() override = default;
+
+  // `RemoteDiagnosticRoutineServiceStrategy`:
+  mojo::Remote<crosapi::TelemetryDiagnosticRoutinesService>& GetRemoteService()
+      override {
+    return LacrosService::Get()
+        ->GetRemote<crosapi::TelemetryDiagnosticRoutinesService>();
+  }
+};
+#endif  // BUILDFLAG (IS_CHROMEOS_LACROS)
+
+}  // namespace
+
+// static
+std::unique_ptr<RemoteDiagnosticRoutineServiceStrategy>
+RemoteDiagnosticRoutineServiceStrategy::Create() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  return std::make_unique<RemoteDiagnosticRoutineServiceStrategyAsh>();
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  if (!LacrosService::Get()
+           ->IsAvailable<crosapi::TelemetryDiagnosticRoutinesService>()) {
+    return nullptr;
+  }
+  return std::make_unique<RemoteDiagnosticRoutineServiceStrategyLacros>();
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+  NOTREACHED_NORETURN();
+}
+
+RemoteDiagnosticRoutineServiceStrategy::
+    RemoteDiagnosticRoutineServiceStrategy() = default;
+RemoteDiagnosticRoutineServiceStrategy::
+    ~RemoteDiagnosticRoutineServiceStrategy() = default;
+
+}  // namespace chromeos
