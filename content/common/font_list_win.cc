d175d77f05aed39e46bee88c28c9ad1123f849f2
David Yeung <dayeung@chromium.org>
2023-08-30 21:16:16 +0000

    [Font] Fix font dropdown list in chrome://settings/fonts page.
    Font list was not sorted. This was only reported for Windows.
    
    https://screenshot.googleplex.com/DfbnTRo2TJ8u3uq
    
    Bug: 1205900
    Change-Id: I26ee2133bd5444c65090664a8edc33e163993239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827468
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190321}

--- content/common/font_list_win.cc
+++ content/common/font_list_win.cc
@@ -65,7 +65,7 @@ base::Value::List GetFontList_SlowBlocking() {
     font_item.Append(localized_name.value());
     font_list.Append(std::move(font_item));
   }
-
+  std::sort(font_list.begin(), font_list.end());
   return font_list;
 }
 
