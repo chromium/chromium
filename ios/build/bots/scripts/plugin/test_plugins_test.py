568d0b46dd779a03b59b4e045e84c77b209c19f6
mark a. foltz <mfoltz@chromium.org>
2023-08-30 03:29:59 +0000

    [Copyright] Fix copyright headers in Chromium.
    This CL updates copyright headers in /ios
    to the current format:
    
    // Copyright $year The Chromium Authors
    // Use of this source code is governed by a BSD-style license that can be
    // found in the LICENSE file.
    
    Generated with:
    
    ./tools/git/mffr.py -g "*.cc" -g "*.h" -g "*.mm" -g "*.py" -g "*.js" -g "*.java" -g "*.mojom" -g "*.xml" -g "*.jinja" -g "*.gn" -g "*.gni" -g "*.star" -g "*.icon" -g "*.ts" -g "*.css" -g "*.json" -g "*.html" -g "*.proto" -g "*.idl" -g "*.pins" -g "*.yml" -g "*.sh" 'Chromium Authors\. All rights reserved\.' 'Chromium Authors'
    
    Additional manual fixes were made to files that have extra "(C)"
    strings, or where line breaks were wrong.
    
    This CL was uploaded by git cl split.
    
    R=cameronhiggins@google.com, rohitrao@chromium.org
    
    Bug: 1457007
    Change-Id: I38053efc940856ebcdac60844aff93c97214c77b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824410
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1189905}

--- ios/build/bots/scripts/plugin/test_plugins_test.py
+++ ios/build/bots/scripts/plugin/test_plugins_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env vpython3
-# Copyright 2022 The Chromium Authors. All rights reserved.
+# Copyright 2022 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
