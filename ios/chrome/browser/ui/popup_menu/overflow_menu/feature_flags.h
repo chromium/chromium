477372b011c3e285c8596d8e12e8160257129069
Robbie Gibson <rkgibson@google.com>
2023-08-30 13:28:37 +0000

    [iOS] Remove dead code for overflow menu flag
    Bug: none
    Change-Id: I273264e6a68f360300f36b2b0ec6289123ffa9e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4823429
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190085}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.h
@@ -23,9 +23,6 @@ BASE_DECLARE_FEATURE(kOverflowMenuCustomization);
 // Whether the NewOverflowMenu feature is enabled.
 bool IsNewOverflowMenuEnabled();
 
-// Whether or not the NewOverflowMenuReorderSettings feature is enabled.
-bool IsNewOverflowMenuReorderSettingsEnabled();
-
 // Whether or not the NewOverflowMenuShareChromeAction is enabled.
 bool IsNewOverflowMenuShareChromeActionEnabled();
 
