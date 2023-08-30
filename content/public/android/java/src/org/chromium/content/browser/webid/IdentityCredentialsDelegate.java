b8eb95fe500c776a9198b34bc3ac6b142d89f881
Sam Goto <goto@chromium.org>
2023-08-30 22:21:05 +0000

    [wallets] Integrate the Wallet API with the Identity Credential module
    In this CL, we integrate the request for identity credentials with
    the recently introduced android API.
    
    More details on the architectural choices and the use cases in the
    Intent-to-Prototype here:
    
    https://groups.google.com/a/chromium.org/g/blink-dev/c/O9A9fq-0IdI?pli=1
    
    Change-Id: Iaf227174695d4b43c663506ecf81a35b05502a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803076
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190361}

--- content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegate.java
+++ content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegate.java
@@ -4,14 +4,65 @@
 
 package org.chromium.content.browser.webid;
 
+import android.app.Activity;
+
 import org.chromium.base.Promise;
 
 /**
  * Delegate interface for calling into GMSCore's private identity credentials.
  *
+ * There are two implementations of this interface, in two different repositories.
+ * To update this interface without breaking the builds independently, you have to:
+ *
+ * Step 0) Current state
+ *
+ *   Upstream:
+ *     Interface: get(int)
+ *     PublicImpl: get(int)
+ *
+ *   Downstream:
+ *     Impl: get(int)
+ *
+ * Step 1) CL#1 in chromium
+ *
+ *   Upstream:
+ *     Interface: default get(int), default get(int, string)
+ *     PublicImpl: get(int, string)
+ *
+ *   Downstream:
+ *     Impl: get(int)
+ *
+ * Step 2) CL#2 in //clank
+ *
+ *   Upstream:
+ *     Interface: default get(int), default get(int, string)
+ *     PublicImpl: get(int, string)
+ *
+ *   Downstream:
+ *     Impl: get(int, string)
+ *
+ * Step 3) CL#3 in chromium
+ *
+ *    Upstream:
+ *     Interface: get(int, string)
+ *     PublicImpl: get(int, string)
+ *
+ *   Downstream:
+ *     Impl: get(int, string)
+ *
+ * Once GMSCore publishes this API publicly, we can have a single implementation.
+ *
  * TODO(crbug.com/1475970) delete this once GMSCore publishes this API.
+ *
  */
 public interface IdentityCredentialsDelegate {
-    // TODO(goto): replace with the actual input/output parameters
     public Promise<String> get(String origin, String request);
+
+    public default Promise<byte[]> get(Activity activity, String origin, String request) {
+        return Promise.rejected();
+    }
+
+    public default Promise<Void> register(Activity activity, byte[] credential, byte[] matcher) {
+        return Promise.rejected();
+    }
 }
