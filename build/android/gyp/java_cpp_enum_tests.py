bd63a122fd17bb0abcc5e9383718bb7a5511b617
mark a. foltz <mfoltz@chromium.org>
2023-08-30 14:07:35 +0000

    [Copyright] Fix copyright headers in Chromium.
    This CL updates copyright headers in /build
    to the current format:
    
    // Copyright $year The Chromium Authors
    // Use of this source code is governed by a BSD-style license that can be
    // found in the LICENSE file.
    
    Generated with:
    
    ./tools/git/mffr.py -g "*.cc" -g "*.h" -g "*.mm" -g "*.py" -g "*.js" -g "*.java" -g "*.mojom" -g "*.xml" -g "*.jinja" -g "*.gn" -g "*.gni" -g "*.star" -g "*.icon" -g "*.ts" -g "*.css" -g "*.json" -g "*.html" -g "*.proto" -g "*.idl" -g "*.pins" -g "*.yml" -g "*.sh" 'Chromium Authors\. All rights reserved\.' 'Chromium Authors'
    
    Additional manual fixes were made to files that have extra "(C)"
    strings, or where line breaks were wrong.
    
    This CL was uploaded by git cl split.
    
    R=smaier@chromium.org
    
    Bug: 1457007
    Change-Id: I5a85899b7e507fa1cbe3c4a7cc6db9e4affc3438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4824408
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1190101}

--- build/android/gyp/java_cpp_enum_tests.py
+++ build/android/gyp/java_cpp_enum_tests.py
@@ -31,7 +31,7 @@ class TestPreprocess(unittest.TestCase):
                                                  'really long.')])
     output = GenerateOutput('path/to/file', definition)
     expected = """
-// Copyright %d The Chromium Authors. All rights reserved.
+// Copyright %d The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
