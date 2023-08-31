09a2029915002e28a955613242aa81fbdf8a0ece
Benjamin Williams <bwwilliams@google.com>
2023-08-31 17:01:55 +0000

    Disable flaky ClipboardRecentContentIOSTest tests.
    ClipboardRecentContentIOSTest.* are flaky on iOS. Recent flake seems to
    suggest ClipboardRecentContentIOSTest.SuppressedPasteboardImage and
    ClipboardRecentContentIOSTest.AddingNonStringRemovesCachedString are
    responsible for most of the recent flake, though many tests in the suite
    fail.
    
    Bug: 1462734
    Change-Id: Ibdb424501d68f8b30b6a69b1141b4f021726a5a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4827663
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Owners-Override: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190825}

--- components/open_from_clipboard/clipboard_recent_content_ios_unittest.mm
+++ components/open_from_clipboard/clipboard_recent_content_ios_unittest.mm
@@ -357,9 +357,10 @@ TEST_F(ClipboardRecentContentIOSTest, SuppressedPasteboardContent) {
   VerifyClipboardURLExists(kRecognizedURL2);
 }
 
+// TODO(crbug.com/1462734): This test is flaky.
 // Checks that if the user suppresses content, no image will be returned,
 // and if the image changes, the new image will be returned again.
-TEST_F(ClipboardRecentContentIOSTest, SuppressedPasteboardImage) {
+TEST_F(ClipboardRecentContentIOSTest, DISABLED_SuppressedPasteboardImage) {
   SetPasteboardImage(TestUIImage());
 
   // Test that recent pasteboard data is provided.
@@ -388,9 +389,11 @@ TEST_F(ClipboardRecentContentIOSTest, SuppressedPasteboardImage) {
   VerifyClipboardTypeExists(ClipboardContentType::Image, true);
 }
 
+// TODO(crbug.com/1462734): This test is flaky.
 // Checks that if user copies something other than a string we don't cache the
 // string in pasteboard.
-TEST_F(ClipboardRecentContentIOSTest, AddingNonStringRemovesCachedString) {
+TEST_F(ClipboardRecentContentIOSTest,
+       DISABLED_AddingNonStringRemovesCachedString) {
   SetPasteboardContent(kRecognizedURL);
 
   // Test that recent pasteboard data is provided as url.
