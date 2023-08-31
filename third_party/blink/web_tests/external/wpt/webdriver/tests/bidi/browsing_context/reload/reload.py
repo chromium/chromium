0a216ad78099ca4dd93a6b53cf3a3d88148d067e
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-08-31 16:49:02 +0000

    Import wpt@05f75638b00d12693b50474a97f7caf4c41a7f55
    Using wpt-import in Chromium a75196381e94d8cd796be982ced8089b3dcb09f3.
    With Chromium commits locally applied on WPT:
    a75196381e "Fix an overly aggressive DCHECKs for whitespace in custom properties."
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ieb8c831cebe30d7f0639386d3a135cb3a422b0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4831472
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190804}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/reload.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -21,27 +21,31 @@ async def test_return_value(bidi_session, inline, new_tab):
     "initial_url",
     [
         "about:blank",
-        "https://example.com/#foo",
+        "https://{host}",
+        "https://{host}/#foo",
         "data:text/html,<p>foo</p>",
         f'file://{Path(__file__).parent.parent.resolve() / "support/empty.html"}',
     ],
     ids=[
         "about:blank",
-        "hash",
+        "without hash",
+        "with hash",
         "data url",
         "file url",
     ],
 )
-async def test_reload(bidi_session, new_tab, initial_url):
+async def test_reload(bidi_session, server_config, new_tab, initial_url):
+    target_url = initial_url.format(host=server_config["domains"][""][""])
+
     await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=initial_url)
+                                                 url=target_url)
     await bidi_session.browsing_context.reload(context=new_tab["context"],
                                                wait="complete")
 
     contexts = await bidi_session.browsing_context.get_tree(
         root=new_tab['context'])
     assert len(contexts) == 1
-    assert contexts[0]["url"] == initial_url
+    assert contexts[0]["url"] == target_url
 
 
 async def test_image(bidi_session, new_tab, url):
