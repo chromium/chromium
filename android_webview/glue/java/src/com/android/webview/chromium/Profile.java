6638ba5b967b95144460395883914e5b4d2b60cb
Wayne Jackson Jr. <wbjacksonjr@google.com>
2023-08-31 22:22:39 +0000

    [webview][multi-profile] Implement Glue Profile
    This CL adds the Profile class to the android webview glue layer that
    will be used by the support library for the WebView multi-profile API.
    
    Bug: 1446913
    Change-Id: I05244b5427ae5fdbf3655dd4c8fe7a78c2ac1477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4766416
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191017}

---
+++ android_webview/glue/java/src/com/android/webview/chromium/Profile.java
@@ -0,0 +1,83 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.android.webview.chromium;
+
+import android.webkit.CookieManager;
+import android.webkit.GeolocationPermissions;
+import android.webkit.ServiceWorkerController;
+import android.webkit.WebStorage;
+
+import androidx.annotation.NonNull;
+
+import org.chromium.android_webview.AwBrowserContext;
+import org.chromium.android_webview.common.Lifetime;
+import org.chromium.base.ThreadUtils;
+
+/**
+ * An abstraction of {@link AwBrowserContext}, this class reflects
+ * the state needed for the multi-profile public API.
+ */
+@Lifetime.Profile
+public class Profile {
+    @NonNull
+    private final String mName;
+
+    @NonNull
+    private final CookieManager mCookieManager;
+
+    @NonNull
+    private final WebStorage mWebStorage;
+
+    @NonNull
+    private final GeolocationPermissions mGeolocationPermissions;
+
+    @NonNull
+    private final ServiceWorkerController mServiceWorkerController;
+
+    public Profile(@NonNull final AwBrowserContext browserContext) {
+        assert ThreadUtils.runningOnUiThread();
+        WebViewChromiumFactoryProvider factory = WebViewChromiumFactoryProvider.getSingleton();
+        mName = browserContext.getName();
+
+        if (browserContext.isDefaultAwBrowserContext()) {
+            mCookieManager = factory.getCookieManager();
+            mWebStorage = factory.getWebStorage();
+            mGeolocationPermissions = factory.getGeolocationPermissions();
+            mServiceWorkerController = factory.getServiceWorkerController();
+        } else {
+            mCookieManager = new CookieManagerAdapter(browserContext.getCookieManager());
+            mWebStorage = new WebStorageAdapter(factory, browserContext.getQuotaManagerBridge());
+            mGeolocationPermissions = new GeolocationPermissionsAdapter(
+                    factory, browserContext.getGeolocationPermissions());
+            mServiceWorkerController =
+                    new ServiceWorkerControllerAdapter(browserContext.getServiceWorkerController());
+        }
+    }
+
+    @NonNull
+    public String getName() {
+        return mName;
+    }
+
+    @NonNull
+    public CookieManager getCookieManager() {
+        return mCookieManager;
+    }
+
+    @NonNull
+    public WebStorage getWebStorage() {
+        return mWebStorage;
+    }
+
+    @NonNull
+    public GeolocationPermissions getGeolocationPermissions() {
+        return mGeolocationPermissions;
+    }
+
+    @NonNull
+    public ServiceWorkerController getServiceWorkerController() {
+        return mServiceWorkerController;
+    }
+}
