7df4589fcf08b0de65183694b64954a132b57efc
Nikhil Nayunigari <nikhilcn@google.com>
2023-08-31 07:36:58 +0000

    [CrOS Cellular] Make the SIM Profile name consistent across all UIs
    When a custom name is given to eSIM, the custom name is appended with
    the carrier name in OS Settings. This change makes that happen in Quick
    settings as well.
    
    Bug: b/267515202
    Change-Id: Id8ddcf75418db996848aed142367882f6fa6e65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4816227
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1190571}

--- ash/system/network/network_list_item_view.cc
+++ ash/system/network/network_list_item_view.cc
@@ -29,9 +29,23 @@ std::u16string NetworkListItemView::GetLabel() {
     ActivationStateType activation_state =
         network_properties_->type_state->get_cellular()->activation_state;
     if (activation_state == ActivationStateType::kActivating) {
+      if (network_properties_->type_state->get_cellular()->has_nick_name) {
+        return l10n_util::GetStringFUTF16(
+            IDS_ASH_STATUS_TRAY_NETWORK_LIST_ACTIVATING_WITH_NICK_NAME,
+            base::UTF8ToUTF16(network_properties_->name),
+            base::UTF8ToUTF16(network_properties_->type_state->get_cellular()
+                                  ->network_operator));
+      }
       return l10n_util::GetStringFUTF16(
           IDS_ASH_STATUS_TRAY_NETWORK_LIST_ACTIVATING,
           base::UTF8ToUTF16(network_properties_->name));
+    }
+    if (network_properties_->type_state->get_cellular()->has_nick_name) {
+      return l10n_util::GetStringFUTF16(
+          IDS_ASH_STATUS_TRAY_NETWORK_LIST_ITEM_TITLE,
+          base::UTF8ToUTF16(network_properties_->name),
+          base::UTF8ToUTF16(network_properties_->type_state->get_cellular()
+                                ->network_operator));
     }
   }
   // Otherwise just show the network name or 'Ethernet'.
