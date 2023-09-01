051c143176ad781622521849a372624237ed4076
Aaron Leventhal <aleventhal@chromium.org>
2023-09-01 00:17:01 +0000

    Revert "[A11y] Workaround for AccessibilityFatalError() issues from stale data"
    This reverts commit 99d3f3b65f8e2f221ddeb69046c60d9bcd4aae3d.
    
    Reason for revert: Was a band-aid, and not temporary fix for the bug.
    
    Original change's description:
    > [A11y] Workaround for AccessibilityFatalError() issues from stale data
    >
    > Occasionally an in-flight serialization for an old AXTree collides with
    > a new AXTree. For now, simply toss these.
    >
    > Upcoming CLs (CL:4814368 and CL:4811427) will work to find a proper fix.
    >
    > Bug: 1471373
    > Change-Id: I36a295e06694138b08cc8301df7b140758d15209
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4813553
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1188565}
    
    Bug: 1471373
    Change-Id: I164bcd9719530af3a39a10b928ce8226a02b86da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833516
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1191082}

--- ui/accessibility/ax_tree_unittest.cc
+++ ui/accessibility/ax_tree_unittest.cc
@@ -5239,8 +5239,7 @@ TEST(AXTreeTest, UnserializeErrors) {
       tree.Unserialize(tree_update_3),
       "2 will not be in the tree and is not the new root");
 #else
-  // TODO(crbug.com/1471373) Restore to returning false.
-  EXPECT_TRUE(tree.Unserialize(tree_update_3));
+  EXPECT_FALSE(tree.Unserialize(tree_update_3));
   EXPECT_EQ("2 will not be in the tree and is not the new root", tree.error());
   histogram_tester.ExpectUniqueSample(
       "Accessibility.Reliability.Tree.UnserializeError",
