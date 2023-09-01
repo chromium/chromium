a14f74c98a04356df7a050e6e7fa726b8029c9a5
Tony <legendmastertony@gmail.com>
2023-09-01 17:27:26 +0000

    Add a feature to control calculating scale factor
    On calling getDisplayMedia without size constraints,
    the size blink sends to screen capture component
    is smaller than the screen size
    when device scale factor is larger than 1
    
    The feature is enabled by default on all platforms except ChromeOS due to timeouts observed on some devices.
    
    Bug: 1429161
    Change-Id: I939fd0913f0878a1626749db7864411ca27db85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4826746
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191439}

--- third_party/blink/renderer/modules/mediastream/media_stream_utils.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_utils.h
@@ -5,6 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_UTILS_H_
 
+#include "base/feature_list.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom-blink.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -17,6 +18,8 @@ class ExecutionContext;
 class LocalFrame;
 class MediaStreamSource;
 class MediaStreamTrack;
+
+MODULES_EXPORT BASE_DECLARE_FEATURE(kGetDisplayMediaScreenScaleFactor);
 
 class MODULES_EXPORT MediaStreamUtils {
   STATIC_ONLY(MediaStreamUtils);
