c362b35b5bc726237a7a13c8fd22deca6f67d78d
Arthur Milchior <arthurmilchior@chromium.org>
2023-09-03 20:05:25 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I746f287b1ba17e47aadbd82500a2aecd5f858404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4797957
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1191862}

--- chrome/browser/commerce/merchant_viewer/merchant_viewer_data_manager_factory.h
+++ chrome/browser/commerce/merchant_viewer/merchant_viewer_data_manager_factory.h
@@ -36,7 +36,7 @@ class MerchantViewerDataManagerFactory : public ProfileKeyedServiceFactory {
   ~MerchantViewerDataManagerFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
