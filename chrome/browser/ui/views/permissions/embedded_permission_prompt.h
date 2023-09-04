b13e7d9d5170ee7de67982d88aed132f6e9bdcaf
Kamila <hkamila@google.com>
2023-09-04 13:29:26 +0000

    [PEPC] Add EmbeddedPromptViews enum
    Lists possible views to be shown after the user clicks on the embedded
    permission prompt.
    
    Bug: 1462930
    Change-Id: I9994fac469844932c07f71d75b5070b801871cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4838736
    Auto-Submit: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192084}

--- chrome/browser/ui/views/permissions/embedded_permission_prompt.h
+++ chrome/browser/ui/views/permissions/embedded_permission_prompt.h
@@ -22,6 +22,27 @@ class EmbeddedPermissionPrompt : public PermissionPromptDesktop {
   EmbeddedPermissionPrompt(const EmbeddedPermissionPrompt&) = delete;
   EmbeddedPermissionPrompt& operator=(const EmbeddedPermissionPrompt&) = delete;
 
+  // Prompt views shown after the user clicks on the embedded permission prompt.
+  enum Variant {
+    // Permission prompt that asks the user for site-level permission.
+    kAsk,
+    // Permission prompt that informs the user they already granted permission.
+    // Offers additional options to modify the permission decision.
+    kPreviouslyGranted,
+    // Permission prompt that additionally informs the user that they have
+    // previously denied permission to the site. May offer different options
+    // (buttons) to the site-level prompt |kAsk|.
+    kPreviouslyDenied,
+    // Informs the user that the permission was blocked by their administrator.
+    kBlockedByAdministrator,
+    // Informs the user that Chrome needs permission from the OS level, in order
+    // for the site to be able to access a permission.
+    kOsPrompt,
+    // Informs the user that they need to go to OS system settings to grant
+    // access to Chrome.
+    kOsSystemSettings,
+  };
+
   // permissions::PermissionPrompt:
   permissions::PermissionPromptDisposition GetPromptDisposition()
       const override;
