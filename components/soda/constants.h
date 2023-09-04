ab3cfeecba4e65f7437d53548bad2b5f0a3f1755
Rob Schonberger <robsc@chromium.org>
2023-09-04 04:28:13 +0000

    soda: Add a number of unused language constants.
    Bug: b:257166700
    Test: All unit tests pass.
    Change-Id: I28223867b437bab094aa541c230eed21d22a6ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4826369
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1191938}

--- components/soda/constants.h
+++ components/soda/constants.h
@@ -33,7 +33,20 @@ enum class LanguageCode {
   kEsEs = 4,
   kFrFr = 5,
   kItIt = 6,
-  kMaxValue = kItIt,
+  kEnCa = 7,
+  kEnAu = 8,
+  kEnGb = 9,
+  kEnIe = 10,
+  kEnSg = 11,
+  kFrBe = 12,
+  kFrCh = 13,
+  kEnIn = 14,
+  kItCh = 15,
+  kDeAt = 16,
+  kDeBe = 17,
+  kDeCh = 18,
+  kEsUs = 19,
+  kMaxValue = kEsUs,
 };
 
 // Describes all metadata needed to dynamically install SODA language pack
