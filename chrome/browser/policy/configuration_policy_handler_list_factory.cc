d64e4860ae833b6d666368d010da5fc28f6abc9d
Martin Bidlingmaier <mbid@google.com>
2023-09-04 14:55:45 +0000

    Add FullRestoreMode policy
    See the linked bug for a link to the PRD. The new policy corresponds
    directly to the existing settings.restore_apps_and_pages pref.
    
    Bug: b:298381356
    Change-Id: I9981a8d3d1a5a2487fbfb1cabaf62a93a7ae9578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4832212
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Reviewed-by: Roman Sorokin <rsorokin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192126}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1527,6 +1527,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kGlanceablesEnabled,
     ash::prefs::kGlanceablesEnabled,
     base::Value::Type::BOOLEAN },
+  { key::kFullRestoreMode,
+    ash::full_restore::kRestoreAppsAndPagesPrefName,
+    base::Value::Type::INTEGER },
 #endif // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_LINUX)
