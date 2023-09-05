0379063cec28cc8ca5aa35937c47f5a268fe8766
Vidhan Jain <vidhanj@google.com>
2023-09-05 10:09:55 +0000

    [iOS] Add test for migrating the profile from local to account
    Bug: 1464547
    Change-Id: Ib55fbfe0b7576839cf60f578f2a87f5b3824da83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833762
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1192352}

--- ios/chrome/browser/ui/autofill/save_profile_egtest.mm
+++ ios/chrome/browser/ui/autofill/save_profile_egtest.mm
@@ -5,7 +5,9 @@
 #import <memory>
 
 #import "base/test/ios/wait_util.h"
+#import "components/autofill/core/common/autofill_features.h"
 #import "components/strings/grit/components_strings.h"
+#import "components/sync/base/features.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
@@ -14,6 +16,7 @@
 #import "ios/chrome/browser/ui/infobars/infobar_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/ui/infobars/modals/infobar_address_profile_modal_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
+#import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
@@ -23,10 +26,17 @@
 #import "testing/gtest/include/gtest/gtest.h"
 #import "ui/base/l10n/l10n_util.h"
 
+using base::test::ios::kWaitForActionTimeout;
+
 namespace {
 
 // URLs of the test pages.
-const char kProfileForm[] = "/autofill_smoke_test.html";
+constexpr char kProfileForm[] = "/autofill_smoke_test.html";
+constexpr char kFormHTMLFile[] = "/profile_form.html";
+
+// Ids of fields in the form.
+constexpr char kFormElementName[] = "name";
+constexpr char kFormElementSubmit[] = "submit_profile";
 
 // Matcher for the banner button.
 id<GREYMatcher> BannerButtonMatcher() {
@@ -41,10 +51,28 @@ id<GREYMatcher> ModalButtonMatcher() {
                     grey_accessibilityTrait(UIAccessibilityTraitButton), nil);
 }
 
-// Matcher for the banner button.
+// Matcher for the modal button.
 id<GREYMatcher> ModalEditButtonMatcher() {
   return grey_allOf(grey_accessibilityID(kInfobarSaveAddressModalEditButton),
                     grey_accessibilityTrait(UIAccessibilityTraitButton), nil);
+}
+
+// Matcher for the migration button in modal view.
+id<GREYMatcher> ModalMigrationButtonMatcher() {
+  return grey_allOf(
+      grey_accessibilityLabel(l10n_util::GetNSString(
+          IDS_AUTOFILL_ADDRESS_MIGRATION_TO_ACCOUNT_PROMPT_OK_BUTTON_LABEL)),
+      grey_accessibilityTrait(UIAccessibilityTraitButton), nil);
+}
+
+// Waits for the keyboard to appear. Returns NO on timeout.
+BOOL WaitForKeyboardToAppear() {
+  GREYCondition* waitForKeyboard = [GREYCondition
+      conditionWithName:@"Wait for keyboard"
+                  block:^BOOL {
+                    return [EarlGrey isKeyboardShownWithError:nil];
+                  }];
+  return [waitForKeyboard waitWithTimeout:kWaitForActionTimeout.InSecondsF()];
 }
 
 }  // namepsace
@@ -60,6 +88,19 @@ id<GREYMatcher> ModalEditButtonMatcher() {
   [AutofillAppInterface clearProfilesStore];
 
   [super tearDown];
+}
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+
+  if ([self isRunningTest:@selector(testUserData_MigrationToAccount)]) {
+    config.features_enabled.push_back(
+        autofill::features::kAutofillAccountProfileStorage);
+    config.features_enabled.push_back(
+        syncer::kSyncEnableContactInfoDataTypeInTransportMode);
+  }
+
+  return config;
 }
 
 #pragma mark - Page interaction helper methods
@@ -161,6 +202,71 @@ id<GREYMatcher> ModalEditButtonMatcher() {
 
   // Save the profile.
   [[EarlGrey selectElementWithMatcher:ModalButtonMatcher()]
+      performAction:grey_tap()];
+
+  // Ensure profile is saved locally.
+  GREYAssertEqual(1U, [AutofillAppInterface profilesCount],
+                  @"Profile should have been saved.");
+
+  [SigninEarlGrey signOut];
+}
+
+// Ensures that if a local profile is filled in a form and submitted, the user
+// is asked for a migration prompt.
+- (void)testUserData_MigrationToAccount {
+  [AutofillAppInterface clearProfilesStore];
+
+  // Store one address.
+  [AutofillAppInterface saveExampleProfile];
+
+  [SigninEarlGreyUI signinWithFakeIdentity:[FakeSystemIdentity fakeIdentity1]
+                                enableSync:NO];
+
+  GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kFormHTMLFile)];
+
+  // Ensure there are no saved profiles.
+  GREYAssertEqual(1U, [AutofillAppInterface profilesCount],
+                  @"There should be no saved profile.");
+
+  // Tap on a field to trigger form activity.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
+      performAction:chrome_test_util::TapWebElementWithId(kFormElementName)];
+
+  // Wait for the keyboard to appear.
+  WaitForKeyboardToAppear();
+
+  // Tap on the suggestion.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::
+                                          AutofillSuggestionViewMatcher()]
+      performAction:grey_tap()];
+
+  // Verify Web Content was filled.
+  NSString* name = [AutofillAppInterface exampleProfileName];
+  NSString* javaScriptCondition = [NSString
+      stringWithFormat:@"document.getElementById('%s').value === '%@'",
+                       kFormElementName, name];
+  [ChromeEarlGrey waitForJavaScriptCondition:javaScriptCondition];
+
+  // Submit the form.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
+      performAction:chrome_test_util::TapWebElementWithId(kFormElementSubmit)];
+
+  [InfobarEarlGreyUI waitUntilInfobarBannerVisibleOrTimeout:YES];
+
+  // Accept the banner.
+  [[EarlGrey selectElementWithMatcher:BannerButtonMatcher()]
+      performAction:grey_tap()];
+  [InfobarEarlGreyUI waitUntilInfobarBannerVisibleOrTimeout:NO];
+
+  id<GREYMatcher> footerMatcher = grey_text(l10n_util::GetNSStringF(
+      IDS_IOS_AUTOFILL_ADDRESS_MIGRATE_IN_ACCOUNT_FOOTER, u"foo1@gmail.com"));
+  // Check if there is footer suggesting it's a migration prompt.
+  [[EarlGrey selectElementWithMatcher:footerMatcher]
+      assertWithMatcher:grey_sufficientlyVisible()];
+
+  // Save the profile.
+  [[EarlGrey selectElementWithMatcher:ModalMigrationButtonMatcher()]
       performAction:grey_tap()];
 
   // Ensure profile is saved locally.
