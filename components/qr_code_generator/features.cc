b185db5d502d4995627e09d62c6934590031a5f2
Łukasz Anforowicz <lukasza@chromium.org>
2023-09-06 23:42:12 +0000

    Revert "Enable by default the `RustyQrCodeGenerator` feature on most platforms."
    This reverts commit 4b7aea0f323fb3bf14c31d6d1099d6fc637a58bc.
    
    Reason for revert: I guess I was mistaken when I assumed that making the feature enabled by default is a prerequisite for Stable experiments.  Finch presubmit are complaining... (see cl/563248796).
    
    Original change's description:
    > Enable by default the `RustyQrCodeGenerator` feature on most platforms.
    >
    > This CL enables the `RustyQrCodeGenerator` feature on all platforms
    > except ChromeOS.
    >
    > Based on the results from the Canary/Dev/Beta experiments (see
    > Google-internal go/rusty-qr-experiments for more details if needed) the
    > feature team plans to experiment with (and hopefully launch) the feature
    > into the Stable release channel within the M117 timeframe (on all
    > relevant non-ChromeOS platforms).  This CL is one of prerequisite step
    > for such Stable experiments.
    >
    > Bug: 1431991
    > Change-Id: Idf14660e3de705fbf14c9d4b971231d1de2c0e61
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4843524
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1193286}
    
    Bug: 1431991
    Change-Id: I4fb6fefdd3a6dc9ec3ae6a5296b12bf52583b469
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4848232
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1193323}

--- components/qr_code_generator/features.cc
+++ components/qr_code_generator/features.cc
@@ -5,19 +5,11 @@
 #include "components/qr_code_generator/features.h"
 
 #include "base/feature_list.h"
-#include "build/build_config.h"
 
 namespace qr_code_generator {
 
 BASE_FEATURE(kRustyQrCodeGeneratorFeature,
              "RustyQrCodeGenerator",
-#if BUILDFLAG(IS_CHROMEOS)
-             // TODO(https://crbug.com/1431991): Enable on all platforms.
-             // (http://crbug.com/1467360 is the only known blocker on CrOS.)
-             base::FEATURE_DISABLED_BY_DEFAULT
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace qr_code_generator
