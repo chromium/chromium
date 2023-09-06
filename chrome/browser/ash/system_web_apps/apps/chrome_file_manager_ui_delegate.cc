83517477017e456728134d82548d91c85189538a
Ben Reich <benreich@chromium.org>
2023-09-06 01:32:51 +0000

    [Files app] Record the Docs offline hosted state when polling
    Ensures when the hosted pin state is polled, we also record the
    percentage of files that are available offline. Along with this, polling
    hosted docs doesn't necessarily need to only happen when bulk pinning is
    enabled. Poll docs on a slow frequency when bulk pinning is disabled
    though.
    
    Bug: b/296340490
    Change-Id: Ib50663b9d0e7dee5673d2b9aa0a5644fb8206c3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4787970
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1192813}

--- chrome/browser/ash/system_web_apps/apps/chrome_file_manager_ui_delegate.cc
+++ chrome/browser/ash/system_web_apps/apps/chrome_file_manager_ui_delegate.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/ash/system_web_apps/apps/chrome_file_manager_ui_delegate.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
 #include "chrome/browser/ash/drive/file_system_util.h"
@@ -14,6 +15,7 @@
 #include "chrome/browser/ash/policy/dlp/files_policy_notification_manager_factory.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/drive/file_errors.h"
 #include "content/public/browser/web_ui.h"
 
 ChromeFileManagerUIDelegate::ChromeFileManagerUIDelegate(content::WebUI* web_ui)
@@ -41,8 +43,7 @@ void ChromeFileManagerUIDelegate::ProgressPausedTasks() const {
 }
 
 void ChromeFileManagerUIDelegate::ShouldPollDriveHostedPinStates(bool enabled) {
-  if (!drive::util::IsDriveFsBulkPinningEnabled(Profile::FromWebUI(web_ui_)) ||
-      poll_hosted_pin_states_ == enabled) {
+  if (poll_hosted_pin_states_ == enabled) {
     return;
   }
   poll_hosted_pin_states_ = enabled;
@@ -64,14 +65,61 @@ void ChromeFileManagerUIDelegate::PollHostedPinStates() {
   if (!poll_hosted_pin_states_) {
     return;
   }
-  drive::DriveIntegrationService* const service =
-      drive::util::GetIntegrationServiceByProfile(Profile::FromWebUI(web_ui_));
-  if (service && service->IsMounted()) {
+  if (drive::DriveIntegrationService* const service =
+          drive::util::GetIntegrationServiceByProfile(
+              Profile::FromWebUI(web_ui_))) {
+    VLOG(1) << "Polling hosted file pin states";
     service->PollHostedFilePinStates();
   }
+
+  base::TimeDelta poll_delay =
+      drive::util::IsDriveFsBulkPinningEnabled(Profile::FromWebUI(web_ui_))
+          ? base::Seconds(15)
+          : base::Minutes(3);
+
+  // After the `poll_delay` call `GetDocsOfflineStats`, the
+  // `PollHostedFilePinStates` function caches the number of items pinned /
+  // available offline, ensure there's enough time for that data to be
+  // retrieved.
+  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&ChromeFileManagerUIDelegate::PollDocsOfflineStats,
+                     weak_ptr_factory_.GetWeakPtr(), poll_delay),
+      poll_delay);
+}
+
+void ChromeFileManagerUIDelegate::PollDocsOfflineStats(
+    const base::TimeDelta poll_delay) {
+  if (drive::DriveIntegrationService* const service =
+          drive::util::GetIntegrationServiceByProfile(
+              Profile::FromWebUI(web_ui_))) {
+    VLOG(1) << "Getting docs offline stats";
+    service->GetDocsOfflineStats(
+        base::BindOnce(&ChromeFileManagerUIDelegate::RecordDocsOfflineStats,
+                       weak_ptr_factory_.GetWeakPtr()));
+  }
+
   base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
       FROM_HERE,
       base::BindOnce(&ChromeFileManagerUIDelegate::PollHostedPinStates,
                      weak_ptr_factory_.GetWeakPtr()),
-      base::Seconds(30));
+      poll_delay);
+}
+
+void ChromeFileManagerUIDelegate::RecordDocsOfflineStats(
+    drive::FileError error,
+    drivefs::mojom::DocsOfflineStatsPtr stats) {
+  if (error != drive::FileError::FILE_ERROR_OK || !stats ||
+      stats->total == -1 || stats->available_offline == -1 ||
+      (total_available_offline_hosted_files_ == stats->available_offline &&
+       total_hosted_files_ == stats->total)) {
+    VLOG(1) << "Not recording the Docs offline UMA stat";
+    return;
+  }
+
+  base::UmaHistogramPercentage(
+      "FileBrowser.GoogleDrive.DSSAvailabilityPercentage",
+      stats->total == 0 ? 0 : (stats->available_offline * 100 / stats->total));
+  total_available_offline_hosted_files_ = stats->available_offline;
+  total_hosted_files_ = stats->total;
 }
