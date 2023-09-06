f479830f1cd7c9f14a363314f4742281958b471f
Robert Phillips <robertphillips@google.com>
2023-09-06 14:55:45 +0000

    Disable SkImage::makeWithFilter usage in Chromium
    After:
    https://chromium-review.googlesource.com/c/chromium/src/+/4831473
    
    Chromium should no longer be using this API call. This CL will block
    any new uses from popping up before the call is deleted in Skia.
    
    Bug: b/293326072
    Change-Id: I695d01fc1a40ed1e55ee0f9aede2ddce3be66c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4831474
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1193037}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -231,6 +231,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_SUPPORT_LEGACY_CONIC_CHOP
 
+#define SK_DISABLE_LEGACY_MAKEWITHFILTER
+
 #define SK_ENABLE_SKSL_IN_RASTER_PIPELINE
 
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
