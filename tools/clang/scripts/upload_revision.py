9d89fd56b4e1ba45cc2fc26c4db6c3d2d76500bc
Hans Wennborg <hans@chromium.org>
2023-09-08 13:55:55 +0000

    [clang] Add win-arm64-rel to Cq-Include-Trybots for rolls
    Bug: 1472681
    Change-Id: Ie6942417ad7c8e6a15e413fdf64815adc8a93f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852185
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194072}

--- tools/clang/scripts/upload_revision.py
+++ tools/clang/scripts/upload_revision.py
@@ -72,7 +72,7 @@ Cq-Include-Trybots: chromium/try:mac-official,linux-official
 Cq-Include-Trybots: chromium/try:win-official,win32-official
 Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
 Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
-Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
+Cq-Include-Trybots: chrome/try:linux-chromeos-chrome,win-arm64-rel
 Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
 Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo'''
 
