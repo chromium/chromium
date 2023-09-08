678eb5c08e4fc85fe28f100168e37f4c849ef007
Jood Hajeer <jood@google.com>
2023-09-08 15:21:43 +0000

    [iOS] EG tests for unified settings with policies.
    This CL adds 3 tests for the unified settings page.
    With SyncTypesListDisabled, with SyncDisabled policy, and with a passphrase needed error.
    
    Fixed: 1462123
    Change-Id: I30b92897d7bceeb49873b4b30c4bfdd159f0dbb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846509
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194123}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_egtest.mm
@@ -3,7 +3,11 @@
 // found in the LICENSE file.
 
 #import "base/strings/sys_string_conversions.h"
+#import "components/policy/policy_constants.h"
 #import "components/sync/base/features.h"
+#import "ios/chrome/browser/policy/policy_app_interface.h"
+#import "ios/chrome/browser/policy/policy_earl_grey_utils.h"
+#import "ios/chrome/browser/policy/policy_util.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
@@ -27,6 +31,8 @@ using chrome_test_util::SettingsAccountButton;
 using chrome_test_util::SettingsSignInRowMatcher;
 
 namespace {
+
+NSString* const kPassphrase = @"hello";
 
 void SignInWithPromoFromAccountSettings(FakeSystemIdentity* fake_identity,
                                         BOOL expect_history_sync) {
@@ -352,6 +358,106 @@ void DismissSignOutSnackbar() {
                                           /*is_toggled_on=*/YES,
                                           /*enabled=*/YES)]
       assertWithMatcher:grey_sufficientlyVisible()];
+}
+
+// Tests the account settings is reflecting the SyncTypesListDisabled
+// policy.
+- (void)testAccountSettingsWithSyncTypesListDisabled_SyncToSigninEnabled {
+  base::Value::List list;
+  list.Append("passwords");
+  policy_test_utils::SetPolicy(base::Value(std::move(list)),
+                               policy::key::kSyncTypesListDisabled);
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+
+  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [ChromeEarlGreyUI openSettingsMenu];
+  [ChromeEarlGreyUI tapSettingsMenuButton:SettingsAccountButton()];
+
+  // Verify that for Passwords an "Off" button is shown instead of a toggle.
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityLabel(
+                                   l10n_util::GetNSString(IDS_IOS_SETTING_OFF))]
+      assertWithMatcher:grey_sufficientlyVisible()];
+}
+
+// Tests the account settings is reflecting the SyncDisabled policy.
+- (void)testAccountSettingsWithSyncDisabled_SyncToSigninEnabled {
+  policy_test_utils::SetPolicy(true, policy::key::kSyncDisabled);
+  [ChromeEarlGreyUI waitForAppToIdle];
+
+  // Dismiss the sync disabled popup.
+  [[EarlGrey selectElementWithMatcher:
+                 grey_allOf(grey_accessibilityLabel(l10n_util::GetNSString(
+                                IDS_IOS_SYNC_SYNC_DISABLED_CONTINUE)),
+                            grey_userInteractionEnabled(), nil)]
+      performAction:grey_tap()];
+
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+
+  [ChromeEarlGreyUI openSettingsMenu];
+  SignInWithPromoFromAccountSettings(fakeIdentity, /*expect_history_sync=*/NO);
+  [ChromeEarlGreyUI tapSettingsMenuButton:SettingsAccountButton()];
+
+  // Scroll to the bottom to view all section.
+  id<GREYMatcher> scroll_view_matcher =
+      grey_accessibilityID(kManageSyncTableViewAccessibilityIdentifier);
+  [[EarlGrey selectElementWithMatcher:scroll_view_matcher]
+      performAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)];
+
+  // Verify that the advanced settings items are not shown.
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityLabel(l10n_util::GetNSString(
+                                   IDS_IOS_MANAGE_SYNC_ENCRYPTION))]
+      assertWithMatcher:grey_notVisible()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityLabel(l10n_util::GetNSString(
+                                   IDS_IOS_MANAGE_DATA_IN_YOUR_ACCOUNT_TITLE))]
+      assertWithMatcher:grey_notVisible()];
+  [[EarlGrey selectElementWithMatcher:
+                 grey_accessibilityLabel(l10n_util::GetNSString(
+                     IDS_IOS_MANAGE_SYNC_GOOGLE_ACTIVITY_CONTROLS_TITLE))]
+      assertWithMatcher:grey_notVisible()];
+}
+
+// Tests the account settings is with a user actionable error; enter
+// passphrase error.
+- (void)testAccountSettingsWithError_SyncToSigninEnabled {
+  [ChromeEarlGrey addBookmarkWithSyncPassphrase:kPassphrase];
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity];
+
+  [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
+  [ChromeEarlGreyUI openSettingsMenu];
+  [ChromeEarlGreyUI tapSettingsMenuButton:SettingsAccountButton()];
+
+  // Verify the error section is showing.
+  [[EarlGrey selectElementWithMatcher:
+                 grey_accessibilityLabel(l10n_util::GetNSString(
+                     IDS_IOS_ACCOUNT_TABLE_ERROR_ENTER_PASSPHRASE_BUTTON))]
+      assertWithMatcher:grey_sufficientlyVisible()];
+
+  // Tap "Enter Passphrase" button.
+  [[EarlGrey selectElementWithMatcher:
+                 grey_accessibilityLabel(l10n_util::GetNSString(
+                     IDS_IOS_ACCOUNT_TABLE_ERROR_ENTER_PASSPHRASE_BUTTON))]
+      performAction:grey_tap()];
+
+  // Enter the passphrase.
+  [SigninEarlGreyUI submitSyncPassphrase:kPassphrase];
+
+  // Verify it goes back to "manage sync" UI.
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(
+                                   kManageSyncTableViewAccessibilityIdentifier)]
+      assertWithMatcher:grey_sufficientlyVisible()];
+
+  // Verify the error section is not showing anymore.
+  [[EarlGrey selectElementWithMatcher:
+                 grey_accessibilityLabel(l10n_util::GetNSString(
+                     IDS_IOS_ACCOUNT_TABLE_ERROR_ENTER_PASSPHRASE_BUTTON))]
+      assertWithMatcher:grey_notVisible()];
 }
 
 @end
