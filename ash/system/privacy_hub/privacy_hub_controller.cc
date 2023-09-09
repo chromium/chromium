6ff038bc470d280f14cebba6e568ad7fd501e551
CJ Huang <chenjih@google.com>
2023-09-09 07:51:41 +0000

    speak-on-mute: Sync pref across devices
    As requested by PWG, we want the speak-on-mute prefs to sync across devices.
    
    Bug: b/296885742
    Change-Id: I0a4ba27d9c28868f677a024125637b4b3251143a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4841078
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194454}

--- ash/system/privacy_hub/privacy_hub_controller.cc
+++ ash/system/privacy_hub/privacy_hub_controller.cc
@@ -15,6 +15,7 @@
 #include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/types/pass_key.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 
 namespace ash {
@@ -86,9 +87,15 @@ void PrivacyHubController::RegisterLocalStatePrefs(
 void PrivacyHubController::RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kUserCameraAllowed, true);
   registry->RegisterBooleanPref(prefs::kUserMicrophoneAllowed, true);
-  registry->RegisterBooleanPref(prefs::kUserSpeakOnMuteDetectionEnabled, false);
-  registry->RegisterBooleanPref(prefs::kShouldShowSpeakOnMuteOptInNudge, true);
-  registry->RegisterIntegerPref(prefs::kSpeakOnMuteOptInNudgeShownCount, 0);
+  registry->RegisterBooleanPref(
+      prefs::kUserSpeakOnMuteDetectionEnabled, false,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  registry->RegisterBooleanPref(
+      prefs::kShouldShowSpeakOnMuteOptInNudge, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  registry->RegisterIntegerPref(
+      prefs::kSpeakOnMuteOptInNudgeShownCount, 0,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
   registry->RegisterBooleanPref(prefs::kUserGeolocationAllowed, true);
 }
 
