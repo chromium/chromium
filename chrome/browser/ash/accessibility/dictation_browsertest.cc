aae630f799017d544f9e7c2ffe7fe81ed2c6b61a
Mark Pearson <mpearson@chromium.org>
2023-09-09 01:23:49 +0000

    Disable Flaky DictationKeyboardImprovementsTest
    In particular, disable NetworkDictationKeyboardImprovementsTest/DictationKeyboardImprovementsTest.ToggledWithNoFocusTriggersSpeech/0
    
    This test is flaky on the linux-chromeos-dbg bot.
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg
    
    I see 5 failures in the page of 25, with two consecutive failures recently (hence the alert in sheriff-o-matic).  It's flaky.
    
    This is confirmed by the test history.
    https://screenshot.googleplex.com/8BMjZLQZTwtVb27
    
    Interestingly, it seems to be less flaky on the -rel bot and most of the other Linux ChromeOS bots.
    
    The test is already disabled on the MSAN and ASAN bots due to flakiness.
    
    Given these observations above, I suspect the test is flaky when things are slower.  Perhaps there's a race condition or a time deadline.
    
    As for this particular change, I considered disabling this test just on Linux ChromeOS bots, including the linux-chromeos-dbg one.  However, it appears to be only running on Linux ChromeOS, so disabling on Linux ChromeOS is the same as disabling entirely.  So that's what I did.
    
    Bug: b:259352600
    Change-Id: Ib4b6b82c718be18b4b2c5f5015157765d38e6db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4855039
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Auto-Submit: Mark Pearson <mpearson@chromium.org>
    Owners-Override: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194406}

--- chrome/browser/ash/accessibility/dictation_browsertest.cc
+++ chrome/browser/ash/accessibility/dictation_browsertest.cc
@@ -2318,15 +2318,9 @@ IN_PROC_BROWSER_TEST_P(DictationKeyboardImprovementsTest,
 
 // Verifies that ChromeVox announces a message when when Dictation is toggled
 // when there is no focused editable.
-// TODO(b:259352600): Re-enable this test on MSAN & ASAN.
-#if defined(MEMORY_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define MAYBE_ToggledWithNoFocusTriggersSpeech \
-  DISABLED_ToggledWithNoFocusTriggersSpeech
-#else
-#define MAYBE_ToggledWithNoFocusTriggersSpeech ToggledWithNoFocusTriggersSpeech
-#endif
+// TODO(b:259352600): Flaky on MSAN & ASAN and Linux ChromiumOS in general.
 IN_PROC_BROWSER_TEST_P(DictationKeyboardImprovementsTest,
-                       MAYBE_ToggledWithNoFocusTriggersSpeech) {
+                       DISABLED_ToggledWithNoFocusTriggersSpeech) {
   // Setup ChromeVox.
   test::SpeechMonitor sm;
   EXPECT_FALSE(GetManager()->IsSpokenFeedbackEnabled());
