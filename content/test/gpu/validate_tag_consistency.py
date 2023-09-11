c23e81d375102e4d151bb68564f9f865c2fbfa4f
Zijie He <zijiehe@google.com>
2023-09-11 19:29:47 +0000

    [Fuchsia] Ignore known failing gpu tests on nelson
    Change-Id: I87a85452f8e2ac583d7a5b507ce3b772cd6a162e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854667
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Zijie He <zijiehe@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194990}

--- content/test/gpu/validate_tag_consistency.py
+++ content/test/gpu/validate_tag_consistency.py
@@ -173,7 +173,8 @@ TAG_HEADER = """\
 #             android-sm-a235m
 #         chromeos-board-amd64-generic chromeos-board-kevin chromeos-board-eve
 #             chromeos-board-jacuzzi chromeos-board-octopus
-#         fuchsia-board-astro fuchsia-board-sherlock fuchsia-board-qemu-x64 ]
+#         fuchsia-board-astro fuchsia-board-nelson fuchsia-board-sherlock
+#             fuchsia-board-qemu-x64 ]
 # Platform
 # tags: [ desktop
 #         mobile ]
