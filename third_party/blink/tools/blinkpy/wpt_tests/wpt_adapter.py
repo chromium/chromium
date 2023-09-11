d3ff6eb33bc2b1954a88e398852da1e954fb1a61
Weizhong Xia <weizhong@google.com>
2023-09-11 20:12:15 +0000

    Fix Wptrunner logging
    Fix the function name so that we can log test
    names the same way as RWT, i.e. virtual/*/external/wpt/...
    
    Bug: None
    Change-Id: I472530a06cce178c9e1011d0825a5849d1a560fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852648
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195017}

--- third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter.py
+++ third_party/blink/tools/blinkpy/wpt_tests/wpt_adapter.py
@@ -49,7 +49,7 @@ class GroupingFormatter(mozlog.formatters.GroupingFormatter):
         self.show_logs = True
         self._start = datetime.now()
 
-    def generate_test_name_output(self, subsuite, test_name):
+    def get_test_name_output(self, subsuite, test_name):
         if not test_name.startswith('/wpt_internal/'):
             test_name = '/external/wpt' + test_name
         return f'virtual/{subsuite}{test_name}' if subsuite else test_name[1:]
