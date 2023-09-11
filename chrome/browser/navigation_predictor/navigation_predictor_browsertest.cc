020bc90be051f8a85dbd5af7369083bd9a7e78b6
Kevin McNee <mcnee@chromium.org>
2023-09-11 16:50:10 +0000

    Reland "Allow anchor interaction tracking on localhost"
    This is a reland of commit bf798ff87519d500f82b4238d4cb69dffaf67074
    
    The original CL caused NavigationPredictorBrowserTest.PipelineHttp to
    start failing, as the test uses a loopback address with http. The
    reland updates the test to use another hostname which doesn't get
    the special treatment that localhost does.
    
    Furthermore, the original CL passed the CQ because it does not
    reliably fail. The reland addresses this by waiting for the
    navigation to finish instead of relying just on RunUntilIdle.
    
    Original change's description:
    > Allow anchor interaction tracking on localhost
    >
    > There's an explicit check that a document's URL is HTTPS before
    > computing anchor element metrics. This notably excludes
    > http://localhost. We now check for HTTP/HTTPS and potential
    > trustworthiness of the HTTP case.
    >
    > Bug: None
    > Change-Id: I6307808792d8ff60059f1de2c54287ad83e33d2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846664
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Auto-Submit: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1193952}
    
    Bug: None
    Change-Id: Ia4f6bc4d918a222635463fcd30ad46ae69c992a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852020
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194902}

--- chrome/browser/navigation_predictor/navigation_predictor_browsertest.cc
+++ chrome/browser/navigation_predictor/navigation_predictor_browsertest.cc
@@ -30,6 +30,7 @@
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/fenced_frame_test_util.h"
 #include "content/public/test/prerender_test_util.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "net/base/features.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -79,6 +80,7 @@ class NavigationPredictorBrowserTest
   void SetUpOnMainThread() override {
     subresource_filter::SubresourceFilterBrowserTest::SetUpOnMainThread();
     host_resolver()->ClearRules();
+    host_resolver()->AddRule("a.test", "127.0.0.1");
     ukm_recorder_ = std::make_unique<ukm::TestAutoSetUkmRecorder>();
   }
 
@@ -90,8 +92,8 @@ class NavigationPredictorBrowserTest
     return https_server_->GetURL(hostname, file);
   }
 
-  const GURL GetHttpTestURL(const char* file) const {
-    return http_server_->GetURL(file);
+  const GURL GetHttpTestURL(const char* hostname, const char* file) const {
+    return http_server_->GetURL(hostname, file);
   }
 
   // Wait until at least |num_links| are reported as having entered the viewport
@@ -245,13 +247,17 @@ IN_PROC_BROWSER_TEST_F(NavigationPredictorBrowserTest, PipelineHttp) {
   auto test_ukm_recorder = std::make_unique<ukm::TestAutoSetUkmRecorder>();
   ResetUKM();
 
-  const GURL& url = GetHttpTestURL("/simple_page_with_anchors.html");
+  // We don't use localhost for this test, as http localhost is trusted.
+  const GURL& url = GetHttpTestURL("a.test", "/simple_page_with_anchors.html");
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
   base::RunLoop().RunUntilIdle();
 
-  EXPECT_TRUE(
-      content::ExecJs(browser()->tab_strip_model()->GetActiveWebContents(),
-                      "document.getElementById('google').click();"));
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  content::TestNavigationObserver click_nav_observer(web_contents);
+  EXPECT_TRUE(content::ExecJs(web_contents,
+                              "document.getElementById('google').click();"));
+  click_nav_observer.Wait();
   base::RunLoop().RunUntilIdle();
 
   auto entries = test_ukm_recorder->GetMergedEntriesByName(
