36cb0300c6822123fed0b8072b2106325853d1de
Brahim Chikhaoui <bchikhaoui@google.com>
2023-09-11 12:49:58 +0000

    [OOBE] Add skip button to the TpmErrorScreen
    add skip button to the TpmErrorScreen tpm-owned step
    to be able to skip the screen in chromeOS flex devices
    without clearing tpm needed. Issue occurred after some
    refactoring of resuming screen in OOBE. this issue occur
    when a user try to enroll the device and TPM already owned.
    the user is stuck in this screen unable to login unless TPM
    is cleared. this will allow user to skip enroll to login.
    for enrollment only possible after clearing the TPM ownership.
    
    Bug: b:293146563
    Change-Id: Icdfd2d25a28433019eb1cbfff6b20303e28ef26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854021
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1194797}

--- chrome/browser/ash/login/screens/tpm_error_screen.h
+++ chrome/browser/ash/login/screens/tpm_error_screen.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_ASH_LOGIN_SCREENS_TPM_ERROR_SCREEN_H_
 #define CHROME_BROWSER_ASH_LOGIN_SCREENS_TPM_ERROR_SCREEN_H_
 
+#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/screens/base_screen.h"
 
@@ -15,10 +16,17 @@ class TpmErrorView;
 // Controller for the tpm error screen.
 class TpmErrorScreen : public BaseScreen {
  public:
-  explicit TpmErrorScreen(base::WeakPtr<TpmErrorView> view);
+  enum class Result { kSkip };
+
+  using ScreenExitCallback = base::RepeatingCallback<void(Result result)>;
+
+  explicit TpmErrorScreen(base::WeakPtr<TpmErrorView> view,
+                          const ScreenExitCallback& exit_callback);
   TpmErrorScreen(const TpmErrorScreen&) = delete;
   TpmErrorScreen& operator=(const TpmErrorScreen&) = delete;
   ~TpmErrorScreen() override;
+
+  static std::string GetResultString(Result result);
 
  private:
   // BaseScreen:
@@ -27,6 +35,7 @@ class TpmErrorScreen : public BaseScreen {
   void OnUserAction(const base::Value::List& args) override;
 
   base::WeakPtr<TpmErrorView> view_;
+  ScreenExitCallback exit_callback_;
 };
 
 }  // namespace ash
--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -762,7 +762,9 @@ WizardController::CreateScreens() {
                           weak_factory_.GetWeakPtr())));
 
   append(std::make_unique<TpmErrorScreen>(
-      oobe_ui->GetView<TpmErrorScreenHandler>()->AsWeakPtr()));
+      oobe_ui->GetView<TpmErrorScreenHandler>()->AsWeakPtr(),
+      base::BindRepeating(&WizardController::OnTpmErrorScreenExit,
+                          weak_factory_.GetWeakPtr())));
 
   if (ash::features::IsCryptohomeRecoveryEnabled()) {
     auto gaia_password_change_screen =
@@ -1321,6 +1323,17 @@ void WizardController::OnPasswordSelectionScreenExit(
       AdvanceToSigninScreen();
       return;
   }
+}
+
+void WizardController::OnTpmErrorScreenExit(TpmErrorScreen::Result result) {
+  OnScreenExit(TpmErrorView::kScreenId,
+               TpmErrorScreen::GetResultString(result));
+
+  // On ChromeOS Flex devices TpmErrorScreen is shown when users try to enroll,
+  // but the TPM is owned by a previous OS. Users can either try restarting the
+  // device to reconfigure TPM in BIOS or continue using the device without
+  // enrollment. This is a non-blocking error.
+  AdvanceToScreen(UserCreationView::kScreenId);
 }
 
 void WizardController::OnGaiaScreenExit(GaiaScreen::Result result) {
