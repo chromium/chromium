62db5ab357486bfb0b97f9a8a9669371503f8cf6
Chris Lu <thegreenfrog@chromium.org>
2023-09-11 17:19:09 +0000

    [ios] Only add MVTs to Magic Stack Order if it is in the Magic Stack
    Most Visited Tiles module will only be shown if
    ShouldPutMostVisitedSitesInMagicStack() is true, so apply the same rule
    when constructing the ranking order from the list fetched from
    Segmentation.
    
    This change also moves the -segmentationMagicStackOrder construction
    logic into a switch statement.
    
    Bug: 1481058
    Change-Id: I6e1ea39860b153add8aaa18463ecb48709ec4872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852644
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194915}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -1089,25 +1089,39 @@ bool CredentialProviderPromoDismissed(PrefService* local_state) {
   for (NSNumber* moduleNumber : _magicStackOrderFromSegmentation) {
     ContentSuggestionsModuleType moduleType =
         (ContentSuggestionsModuleType)[moduleNumber intValue];
-    if (moduleType == ContentSuggestionsModuleType::kSafetyCheck) {
-      if (!IsSafetyCheckMagicStackEnabled() ||
-          safety_check_prefs::IsSafetyCheckInMagicStackDisabled(_localState)) {
-        continue;
-      }
-      // If ShouldHideIrrelevantModules() is enabled and it is not the first
-      // ranked module, do not add it to the Magic Stack.
-      if (!ShouldHideIrrelevantModules() || [magicStackOrder count] == 0) {
-        [self addSafetyCheckToMagicStackOrder:magicStackOrder];
-      }
-    } else if (moduleType == ContentSuggestionsModuleType::kTabResumption) {
-      if (IsTabResumptionEnabled() &&
-          !tab_resumption_prefs::IsTabResumptionDisabled(_localState) &&
-          _tabResumptionItem) {
+    switch (moduleType) {
+      case ContentSuggestionsModuleType::kMostVisited:
+        if (ShouldPutMostVisitedSitesInMagicStack()) {
+          [magicStackOrder addObject:moduleNumber];
+        }
+        break;
+      case ContentSuggestionsModuleType::kTabResumption:
+        if (IsTabResumptionEnabled() &&
+            !tab_resumption_prefs::IsTabResumptionDisabled(_localState) &&
+            _tabResumptionItem) {
+          [magicStackOrder addObject:moduleNumber];
+        }
+        break;
+      case ContentSuggestionsModuleType::kSafetyCheck:
+        if (!IsSafetyCheckMagicStackEnabled() ||
+            safety_check_prefs::IsSafetyCheckInMagicStackDisabled(
+                _localState)) {
+          break;
+        }
+        // If ShouldHideIrrelevantModules() is enabled and it is not the first
+        // ranked module, do not add it to the Magic Stack.
+        if (!ShouldHideIrrelevantModules() || [magicStackOrder count] == 0) {
+          [self addSafetyCheckToMagicStackOrder:magicStackOrder];
+        }
+        break;
+      case ContentSuggestionsModuleType::kShortcuts:
         [magicStackOrder addObject:moduleNumber];
-      }
-
-    } else {
-      [magicStackOrder addObject:moduleNumber];
+        break;
+      default:
+        // These module types should not have been added by the logic receiving
+        // the order list from Segmentation.
+        NOTREACHED();
+        break;
     }
   }
   return magicStackOrder;
