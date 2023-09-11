400c5c8bfc6149176799a95294124f41278cb22a
S Ganesh <ganesh@chromium.org>
2023-09-11 18:25:34 +0000

    Revert "[gardening] Revert "Changes to allow cross-compilation for the test MSI installer""
    This reverts commit b33b38cd1483f9fe527d9017d1204b4050ae09c0.
    
    Reason for revert: As @vollick correctly pointed out, the `--output_dir` includes the path to the `out` directory, for instance `c$:/src/chromium/src/out/Default/test_installer/`. I'm fixing this issue in this revert.
    
    Original change's description:
    > [gardening] Revert "Changes to allow cross-compilation for the test MSI installer"
    >
    > This reverts commit d9c98aac5035935ed081333b8e2beb749a7d3b70.
    >
    > Reason for revert: Appears to be causing failures on
    > chromium/ci/Windows deterministic
    >
    > Original change's description:
    > > Changes to allow cross-compilation for the test MSI installer
    > >
    > > This CL changes the python MSI generating scripts to use the checked-in
    > > artifacts when cross-compiling, and to (re)build the MSIs on Windows
    > > when required.
    > >
    > > Bug: 1480089
    > > Change-Id: Ideb80d1815862e9209582abbd05623a0e932f683
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854612
    > > Commit-Queue: S Ganesh <ganesh@chromium.org>
    > > Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    > > Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    > > Auto-Submit: S Ganesh <ganesh@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1194411}
    >
    > Bug: 1480089, 1481016
    > Change-Id: I90225053be189e2382fd08421ab13f89927e925a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4852811
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1194867}
    
    Bug: 1480089, 1481016
    Change-Id: I3ba2b2912b2100a34b0db741ae51744c692c021c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4856357
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1194955}

--- chrome/updater/test/test_installer/create_test_msi_installer.py
+++ chrome/updater/test/test_installer/create_test_msi_installer.py
@@ -23,9 +23,12 @@ python3 chrome/updater/test/test_installer/create_test_msi_installer.py
 import binascii
 from datetime import date
 import argparse
+import filecmp
 import os
 import shutil
 import subprocess
+import sys
+import tempfile
 import uuid
 
 
@@ -37,7 +40,8 @@ class TestInstaller:
     """Creates a test installer."""
     def __init__(self, candle_path, light_path, product_name, product_version,
                  appid, msi_base_name, msi_template_path, company_name,
-                 company_full_name, per_user_install, output_dir):
+                 company_full_name, per_user_install, checked_in_msi,
+                 output_dir):
         self._candle_path = candle_path
         self._light_path = light_path
         self._product_name = product_name
@@ -48,88 +52,104 @@ class TestInstaller:
         self._company_name = company_name
         self._company_full_name = company_full_name
         self._per_user_install = per_user_install
+        self._checked_in_msi = checked_in_msi
         self._output_dir = output_dir
 
     def BuildInstaller(self):
-        test_installer_namespace = '{A2091DEA-AF86-4C00-8AE0-ECC38FDE6533}'
-        namespace_uuid = uuid.UUID(test_installer_namespace)
-        names_plus_version = '%s %s %s' % (
-            self._product_name, self._msi_base_name, self._product_version)
-
         output_directory_name = os.path.join(
             self._output_dir, self._appid + '.' + self._product_version)
         if not os.path.exists(output_directory_name):
             os.makedirs(output_directory_name)
-
-        target_wxs = os.path.join(output_directory_name,
-                                  self._msi_base_name + '.wxs')
-        shutil.copyfile(self._msi_template_path, target_wxs)
-
-        wix_candle_flags = [
-            *optional_flag('ProductName', self._product_name),
-            *optional_flag('ProductNameLegalIdentifier',
-                           self._product_name.replace(' ', '')),
-            *optional_flag('ProductVersion', self._product_version),
-            *optional_flag('ProductOriginalVersionString',
-                           self._product_version),
-            *optional_flag('ProductBuildYear', str(date.today().year)),
-            *optional_flag('ProductGuid', self._appid),
-            *optional_flag('CompanyName', self._company_name),
-            *optional_flag('CompanyFullName', self._company_full_name),
-            *optional_flag('PerUserInstall', self._per_user_install),
-            *optional_flag(
-                'MsiProductId',
-                str(
-                    uuid.uuid5(namespace_uuid,
-                               'Product %s' % names_plus_version)).upper()),
-            *optional_flag(
-                'MsiUpgradeCode',
-                str(uuid.uuid5(namespace_uuid,
-                               'Upgrade ' + self._product_name)).upper()),
-            *optional_flag(
-                'ComponentGuidInstallerResultSet',
-                str(
-                    uuid.uuid5(
-                        namespace_uuid, 'Component InstallerResult Set %s' %
-                        names_plus_version)).upper()),
-            *optional_flag(
-                'ComponentGuidInstallerErrorSet',
-                str(
-                    uuid.uuid5(
-                        namespace_uuid, 'Component InstallerError Set %s' %
-                        names_plus_version)).upper()),
-            *optional_flag(
-                'ComponentGuidInstallerResultUIStringSet',
-                str(
-                    uuid.uuid5(
-                        namespace_uuid,
-                        'Component InstallerResultUIString Set %s' %
-                        names_plus_version)).upper()),
-            *optional_flag(
-                'ComponentGuidRegisterLaunchCommandSet',
-                str(
-                    uuid.uuid5(
-                        namespace_uuid,
-                        'Component RegisterLaunchCommand Set %s' %
-                        names_plus_version)).upper()),
-        ]
-
-        # Disable warning LGHT1076 and internal check ICE61 on light.exe.
-        wix_light_flags = ['-sw1076', '-sice:ICE61']
-
         msi_output_path = os.path.join(output_directory_name,
                                        self._msi_base_name + '.msi')
-        msi_output_path_wixobj = os.path.splitext(
-            msi_output_path)[0] + '.wixobj'
+        msi_base_file_path = os.path.splitext(self._checked_in_msi)[0]
+        target_wxs = msi_base_file_path + '.wxs'
 
-        candle_cmd = [self._candle_path] + wix_candle_flags + [
-            '-nologo', '-o', msi_output_path_wixobj, target_wxs
-        ]
-        subprocess.run(candle_cmd, check=True)
-        light_cmd = [self._light_path] + wix_light_flags + [
-            '-nologo', '-out', msi_output_path, msi_output_path_wixobj
-        ]
-        subprocess.run(light_cmd, check=True)
+        if sys.platform == 'win32' and os.path.isfile(
+                self._candle_path) and os.path.isfile(self._light_path) and (
+                    not os.path.isfile(target_wxs) or not filecmp.cmp(
+                        self._msi_template_path, target_wxs, shallow=False)):
+            checked_in_dir = os.path.dirname(self._checked_in_msi)
+            if not os.path.exists(checked_in_dir):
+                os.makedirs(checked_in_dir)
+
+            test_installer_namespace = '{A2091DEA-AF86-4C00-8AE0-ECC38FDE6533}'
+            namespace_uuid = uuid.UUID(test_installer_namespace)
+            names_plus_version = '%s %s %s' % (
+                self._product_name, self._msi_base_name, self._product_version)
+
+            wix_candle_flags = [
+                *optional_flag('ProductName', self._product_name),
+                *optional_flag('ProductNameLegalIdentifier',
+                               self._product_name.replace(' ', '')),
+                *optional_flag('ProductVersion', self._product_version),
+                *optional_flag('ProductOriginalVersionString',
+                               self._product_version),
+                *optional_flag('ProductBuildYear', str(date.today().year)),
+                *optional_flag('ProductGuid', self._appid),
+                *optional_flag('CompanyName', self._company_name),
+                *optional_flag('CompanyFullName', self._company_full_name),
+                *optional_flag('PerUserInstall', self._per_user_install),
+                *optional_flag(
+                    'MsiProductId',
+                    str(
+                        uuid.uuid5(namespace_uuid, 'Product %s' %
+                                   names_plus_version)).upper()),
+                *optional_flag(
+                    'MsiUpgradeCode',
+                    str(
+                        uuid.uuid5(namespace_uuid,
+                                   'Upgrade ' + self._product_name)).upper()),
+                *optional_flag(
+                    'ComponentGuidInstallerResultSet',
+                    str(
+                        uuid.uuid5(
+                            namespace_uuid,
+                            'Component InstallerResult Set %s' %
+                            names_plus_version)).upper()),
+                *optional_flag(
+                    'ComponentGuidInstallerErrorSet',
+                    str(
+                        uuid.uuid5(
+                            namespace_uuid, 'Component InstallerError Set %s' %
+                            names_plus_version)).upper()),
+                *optional_flag(
+                    'ComponentGuidInstallerResultUIStringSet',
+                    str(
+                        uuid.uuid5(
+                            namespace_uuid,
+                            'Component InstallerResultUIString Set %s' %
+                            names_plus_version)).upper()),
+                *optional_flag(
+                    'ComponentGuidRegisterLaunchCommandSet',
+                    str(
+                        uuid.uuid5(
+                            namespace_uuid,
+                            'Component RegisterLaunchCommand Set %s' %
+                            names_plus_version)).upper()),
+            ]
+
+            # Disable warning LGHT1076 and internal check ICE61 on light.exe.
+            wix_light_flags = ['-sw1076', '-sice:ICE61']
+
+            shutil.copyfile(self._msi_template_path, target_wxs)
+            with tempfile.TemporaryDirectory() as temp_dir:
+                msi_wixobj = os.path.join(temp_dir,
+                                          self._msi_base_name + '.wixobj')
+                msi_pdb = os.path.join(temp_dir,
+                                       self._msi_base_name + '.wixpdb')
+                candle_cmd = [self._candle_path] + wix_candle_flags + [
+                    '-nologo', '-o', msi_wixobj, target_wxs
+                ]
+                subprocess.run(candle_cmd, check=True)
+                light_cmd = [self._light_path] + wix_light_flags + [
+                    '-nologo', '-pdbout', msi_pdb, '-out',
+                    self._checked_in_msi, msi_wixobj
+                ]
+                subprocess.run(light_cmd, check=True)
+
+        # Copy the checked-in files to the final output path.
+        shutil.copyfile(self._checked_in_msi, msi_output_path)
 
 
 def main():
@@ -167,6 +187,11 @@ def main():
                         action='store_true',
                         help='specifies that the MSI is a per-user installer')
     parser.add_argument(
+        '--checked_in_msi',
+        required=True,
+        help='specifies the location where the MSI will live in the source tree'
+    )
+    parser.add_argument(
         '--output_dir',
         required=True,
         help='path to the directory that will contain the resulting MSI')
@@ -175,7 +200,7 @@ def main():
                   args.product_version, args.appid, args.msi_base_name,
                   args.msi_template_path, args.company_name,
                   args.company_full_name, args.per_user_install,
-                  args.output_dir).BuildInstaller()
+                  args.checked_in_msi, args.output_dir).BuildInstaller()
 
 
 if __name__ == '__main__':
