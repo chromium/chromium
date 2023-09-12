f6404181d84980eaac60b89ebb44b12566c754bc
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-09-12 06:35:05 +0000

    Import wpt@61f49db98626648c2b218835dd1df2152c6d8dfd
    Using wpt-import in Chromium 3f80cdb13961f340aee5d2f2d15a8980ebb063d6.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    andruud@chromium.org:
      external/wpt/css/css-properties-values-api
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I18a9442ef9ff0e33ad40a5a2cdf4eb8494b5582f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4857858
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195231}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/reload.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/reload/reload.py
@@ -2,41 +2,32 @@ from pathlib import Path
 
 import pytest
 
+from . import reload_and_assert
+
 
 pytestmark = pytest.mark.asyncio
 
 PNG_BLACK_DOT = "/webdriver/tests/bidi/browsing_context/support/black_dot.png"
 
 
-async def test_return_value(bidi_session, inline, new_tab):
-    url = inline("<div>foo</div>")
-    await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=url,
-                                                 wait="complete")
-
-    result = await bidi_session.browsing_context.reload(
-        context=new_tab["context"],
-        wait="complete"
-    )
-    assert result == {}
-
-
 @pytest.mark.parametrize("hash", [False, True], ids=["without hash", "with hash"])
 async def test_reload(bidi_session, inline, new_tab, hash):
-    target_url = inline("""<div id="foo""")
+    url = inline("""<div id="foo""")
     if hash:
-        target_url += "#foo"
+        url += "#foo"
 
-    await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=target_url,
-                                                 wait="complete")
-    await bidi_session.browsing_context.reload(context=new_tab["context"],
-                                               wait="complete")
+    navigate_result = await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=url,
+        wait="complete"
+    )
 
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab['context'])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == target_url
+    reload_and_assert(
+        bidi_session,
+        new_tab,
+        last_navigation=navigate_result["navigation"],
+        url=url
+    )
 
 
 @pytest.mark.parametrize(
@@ -53,27 +44,30 @@ async def test_reload(bidi_session, inline, new_tab, hash):
     ],
 )
 async def test_reload_special_protocols(bidi_session, new_tab, url):
-    await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=url,
-                                                 wait="complete")
-    await bidi_session.browsing_context.reload(context=new_tab["context"],
-                                               wait="complete")
+    navigate_result = await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=url,
+        wait="complete"
+    )
 
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab['context'])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == url
+    reload_and_assert(
+        bidi_session,
+        new_tab,
+        last_navigation=navigate_result["navigation"],
+        url=url
+    )
 
 
 async def test_image(bidi_session, new_tab, url):
-    initial_url = url(PNG_BLACK_DOT)
-    await bidi_session.browsing_context.navigate(context=new_tab["context"],
-                                                 url=initial_url,
-                                                 wait="complete")
-    await bidi_session.browsing_context.reload(context=new_tab["context"],
-                                               wait="complete")
+    navigate_result = await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=url(PNG_BLACK_DOT),
+        wait="complete"
+    )
 
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab['context'])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == initial_url
+    reload_and_assert(
+        bidi_session,
+        new_tab,
+        last_navigation=navigate_result["navigation"],
+        url=url
+    )
