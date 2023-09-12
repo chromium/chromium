1c32ebac3bf27ee3cf52c50d0945dc4616ca78cd
Alexander Alekseev <alemate@chromium.org>
2023-09-12 02:21:29 +0000

    Add ChromeOS trace events in /ui/gfx/image
    Investigating ChromeOS login performance revealed that many
    potentially slow UI thread tasks are not instrumented.
    This Cl adds some events around slow tasks that we saw in
    our tests.
    
    Bug: b:293198304
    
    CC=​achuith@chromium.org
    
    This CL was uploaded by git cl split.
    
    R=oshima@chromium.org
    
    Change-Id: Ie23a06d56563030a3b05d1efe0e641cd95883a84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4763835
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195172}

--- ui/gfx/image/image_skia.cc
+++ ui/gfx/image/image_skia.cc
@@ -18,6 +18,7 @@
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
 #include "base/sequence_checker.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/resource/resource_scale_factor.h"
@@ -201,6 +202,7 @@ bool ImageSkiaStorage::HasRepresentationAtAllScales() const {
 std::vector<ImageSkiaRep>::const_iterator ImageSkiaStorage::FindRepresentation(
     float scale,
     bool fetch_new_image) const {
+  TRACE_EVENT0("ui", "ImageSkiaStorage::FindRepresentation");
   auto closest_iter = image_reps_.end();
   auto exact_iter = image_reps_.end();
   float smallest_diff = std::numeric_limits<float>::max();
@@ -323,6 +325,7 @@ ImageSkia ImageSkia::CreateFrom1xBitmap(const SkBitmap& bitmap) {
 }
 
 ImageSkia ImageSkia::DeepCopy() const {
+  TRACE_EVENT0("ui", "ImageSkia::DeepCopy");
   ImageSkia copy;
   if (isNull())
     return copy;
@@ -394,6 +397,7 @@ bool ImageSkia::HasRepresentation(float scale) const {
 }
 
 const ImageSkiaRep& ImageSkia::GetRepresentation(float scale) const {
+  TRACE_EVENT0("ui", "ImageSkia::GetRepresentation");
   if (isNull())
     return NullImageRep();
 
@@ -413,6 +417,7 @@ void ImageSkia::SetReadOnly() {
 }
 
 void ImageSkia::MakeThreadSafe() {
+  TRACE_EVENT0("ui", "ImageSkia::MakeThreadSafe");
   CHECK(storage_.get());
   EnsureRepsForSupportedScales();
   // Delete source as we no longer needs it.
@@ -458,6 +463,7 @@ std::vector<ImageSkiaRep> ImageSkia::image_reps() const {
 }
 
 void ImageSkia::EnsureRepsForSupportedScales() const {
+  TRACE_EVENT0("ui", "ImageSkia::EnsureRepsForSupportedScales");
   const std::vector<ui::ResourceScaleFactor>& supported_scales =
       ui::GetSupportedResourceScaleFactors();
 
@@ -496,6 +502,7 @@ void ImageSkia::Init(const ImageSkiaRep& image_rep) {
 }
 
 const SkBitmap& ImageSkia::GetBitmap() const {
+  TRACE_EVENT0("ui", "ImageSkia::GetBitmap");
   if (isNull()) {
     // Callers expect a ImageSkiaRep even if it is |isNull()|.
     // TODO(pkotwicz): Fix this.
