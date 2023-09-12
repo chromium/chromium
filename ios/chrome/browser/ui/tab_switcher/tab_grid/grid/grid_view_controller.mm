5be989fee95388b9ce37ff62ad6ddb26a29eac71
Aliona DANGLA <alionadangla@google.com>
2023-09-12 17:31:54 +0000

    [iOS] Return a cell when index are wrong in GridViewController
    Due to a potential race condition, cells can be refreshed at a wrong
    timing causing an integer overflow crash (self.items.count - 1 when
    self.items.count is equal to 0 result to max NSUInteger and the crash
    is caused by the impossible memory allocation).
    
    This CL adds a workaround when this happens.
    
    Low-Coverage-Reason: We don't know how to reproduce.
    Bug: 1457146
    Change-Id: I93b3c07b97d354d964aacddbdab0a8a8f77f786a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853731
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195508}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -701,17 +701,17 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
     suggestedActionsCell.suggestedActionsView =
         self.suggestedActionsViewController.view;
   } else {
-    // In some cases this is called with an indexPath.item that's beyond (by 1)
-    // the bounds of self.items -- see crbug.com/1068136. Presumably this is a
-    // race condition where an item has been deleted at the same time as the
-    // collection is doing layout (potentially during rotation?). Fudge by
-    // duplicating the last cell. The assumption is that there will be another,
-    // correct layout shortly after the incorrect one.
-    // Keep array bounds valid.
-    if (itemIndex >= self.items.count) {
-      itemIndex = self.items.count - 1;
+    // In some cases, this is called with an index path that doesn't match the
+    // data source -- see crbug.com/1068136. Presumably this is a race condition
+    // where an item has been deleted at the same time as the collection is
+    // doing layout (potentially during rotation?). Fudge by returning an
+    // unconfigured cell. The assumption is that there will be another – correct
+    // – layout shortly after the incorrect one. Keep `items`' bounds valid.
+    if (self.items.count == 0 || itemIndex >= self.items.count) {
+      return
+          [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier
+                                                    forIndexPath:indexPath];
     }
-
     TabSwitcherItem* item = self.items[itemIndex];
     cell =
         [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier
@@ -955,15 +955,16 @@ NSString* GridCellAccessibilityIdentifier(NSUInteger index) {
 
   // Handle GridCell-s.
   NSUInteger itemIndex = base::checked_cast<NSUInteger>(indexPath.item);
-  // In some cases this is called with an indexPath.item that's beyond (by 1)
-  // the bounds of self.items -- see crbug.com/1068136. Presumably this is a
-  // race condition where an item has been deleted at the same time as the
-  // collection is doing layout (potentially during rotation?). Fudge by
-  // duplicating the last cell. The assumption is that there will be another,
-  // correct layout shortly after the incorrect one.
-  // Keep array bounds valid.
-  if (itemIndex >= self.items.count) {
-    itemIndex = self.items.count - 1;
+  // In some cases, this is called with an index path that doesn't match the
+  // data source -- see crbug.com/1068136. Presumably this is a race condition
+  // where an item has been deleted at the same time as the collection is doing
+  // layout (potentially during rotation?). Fudge by returning an unconfigured
+  // cell. The assumption is that there will be another – correct – layout
+  // shortly after the incorrect one. Keep `items`' bounds valid.
+  if (self.items.count == 0 || itemIndex >= self.items.count) {
+    return [self.collectionView
+        dequeueReusableCellWithReuseIdentifier:kCellIdentifier
+                                  forIndexPath:indexPath];
   }
 
   TabSwitcherItem* item = self.items[itemIndex];
