c2120d3641b600796857d5c3e7659173cc2f40c8
Noureldien Refae <noureldien@google.com>
2023-09-12 04:51:21 +0000

    Privacy Guide: Add preload card standard explanation
    Add standard explanation sheet to the preload card with its logic implementation.
    
    Animated GIF: https://crbug.com/1215630#c280
    
    Low-Coverage-Reason: Tests are on the last relation chain CL.
    
    Bug: 1215630
    Change-Id: I72fe5eb29b4ac5c81f473e39b7a791a9e3df866a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4844857
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Nour Eldien Ayman <noureldien@google.com>
    Cr-Commit-Position: refs/heads/main@{#1195215}

--- chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PreloadFragment.java
+++ chrome/browser/privacy_guide/android/java/src/org/chromium/chrome/browser/privacy_guide/PreloadFragment.java
@@ -11,13 +11,61 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import org.chromium.base.supplier.OneshotSupplier;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.components.browser_ui.widget.RadioButtonWithDescriptionAndAuxButton;
+
 /**
  * Controls the behaviour of the Preload privacy guide page.
  */
-public class PreloadFragment extends Fragment {
+public class PreloadFragment extends Fragment
+        implements RadioButtonWithDescriptionAndAuxButton.OnAuxButtonClickedListener {
+    private RadioButtonWithDescriptionAndAuxButton mStandardPreloading;
+    private BottomSheetController mBottomSheetController;
+    private PrivacyGuideBottomSheetView mBottomSheetView;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.privacy_guide_preload_step, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mStandardPreloading =
+                (RadioButtonWithDescriptionAndAuxButton) view.findViewById(R.id.standard_option);
+
+        mStandardPreloading.setAuxButtonClickedListener(this);
+    }
+
+    @Override
+    public void onAuxButtonClicked(int clickedButtonId) {
+        LayoutInflater inflater = LayoutInflater.from(getView().getContext());
+        if (clickedButtonId == mStandardPreloading.getId()) {
+            displayBottomSheet(
+                    inflater.inflate(R.layout.privacy_guide_preload_standard_explanation, null));
+        } else {
+            assert false : "Unknown Aux clickedButtonId " + clickedButtonId;
+        }
+    }
+
+    private void displayBottomSheet(View sheetContent) {
+        mBottomSheetView = new PrivacyGuideBottomSheetView(sheetContent, this::closeBottomSheet);
+        // TODO(crbug.com/1287979): Re-enable animation once bug is fixed
+        if (mBottomSheetController != null) {
+            mBottomSheetController.requestShowContent(mBottomSheetView, false);
+        }
+    }
+
+    private void closeBottomSheet() {
+        if (mBottomSheetController != null && mBottomSheetView != null) {
+            mBottomSheetController.hideContent(mBottomSheetView, true);
+        }
+    }
+
+    void setBottomSheetControllerSupplier(
+            OneshotSupplier<BottomSheetController> bottomSheetControllerSupplier) {
+        bottomSheetControllerSupplier.onAvailable(
+                (bottomSheetController) -> mBottomSheetController = bottomSheetController);
     }
 }
