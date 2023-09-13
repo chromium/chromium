397323385e97824bd069e125f16e4db427c413c4
John Palmer <jopalmer@chromium.org>
2023-09-13 00:24:54 +0000

    Add guard to editor menu
    BUG: b:296976482
    Change-Id: I0da8d61b369e5cc062c1b25d7298f4e0eb566ae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4812330
    Reviewed-by: Yuki Awano <yawano@google.com>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195730}

--- chrome/browser/chromeos/read_write_cards/read_write_cards_manager.cc
+++ chrome/browser/chromeos/read_write_cards/read_write_cards_manager.cc
@@ -5,7 +5,11 @@
 #include "chrome/browser/chromeos/read_write_cards/read_write_cards_manager.h"
 
 #include <memory>
+#include <string>
 
+#include "base/command_line.h"
+#include "base/hash/sha1.h"
+#include "base/strings/string_piece.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/quick_answers/quick_answers_controller_impl.h"
 #include "chrome/browser/ui/views/editor_menu/editor_menu_controller_impl.h"
@@ -18,6 +22,27 @@
 #include "third_party/blink/public/mojom/context_menu/context_menu.mojom-shared.h"
 
 namespace chromeos {
+
+namespace {
+
+constexpr base::StringPiece kOrcaKey = "orca-key";
+constexpr char kOrcaKeyHash[] =
+    "\x7a\xf3\xa1\x57\x28\x48\xc4\x14\x27\x13\x53\x5a\x09\xf3\x0e\xfc\xee\xa6"
+    "\xbb\xa4";
+
+void CheckOrcaKey() {
+  const std::string& debug_key_hash = base::SHA1HashString(
+      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          /*ash::switches::kOrcaKey=*/kOrcaKey));
+  // See go/orca-key for the key.
+  // Commandline looks like:
+  //  out/Default/chrome --user-data-dir=/tmp/auuf123 --orca-key="INSERT KEY
+  //  HERE" --enable-features=Orca
+  CHECK_EQ(debug_key_hash, kOrcaKeyHash)
+      << "Provided debug key does not match with the expected one.";
+}
+
+}  // namespace
 
 ReadWriteCardsManager::ReadWriteCardsManager()
     : quick_answers_controller_(
@@ -52,6 +77,7 @@ ReadWriteCardController* ReadWriteCardsManager::GetController(
 
   if (chromeos::features::IsOrcaEnabled()) {
     if (params.is_editable) {
+      CheckOrcaKey();
       return editor_menu_controller_.get();
     }
   }
