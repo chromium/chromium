0894375da82464373aa72ff5cf10fc32e6d5ad3e
David Bienvenu <davidbienvenu@chromium.org>
2023-09-13 12:54:41 +0000

    win: Fix dir override for AppHomePageHandlerTests
    Change the dir override from DIR_USER_STARTUP to DIR_START_MENU.
    SHORTCUT_LOCATION_START_MENU_CHROME_APPS_DIR maps to DIR_START_MENU,
    not DIR_USER_STARTUP in  ShellUtil::GetShortcutPath.
    
    Original cl was crrev.com/c/4826392
    
    Bug: 1457508
    Change-Id: Iac6b8ae33013a4f8551f75a20e5d19aef835c7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861024
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1195949}

--- chrome/browser/ui/webui/app_home/app_home_page_handler_browsertest.cc
+++ chrome/browser/ui/webui/app_home/app_home_page_handler_browsertest.cc
@@ -272,7 +272,7 @@ class AppHomePageHandlerTest : public InProcessBrowserTest {
 #if BUILDFLAG(IS_WIN)
   // This is used as a fallback to prevent creating shortcuts in the startup
   // dir if tasks are still running when `override_registration_` is reset.
-  base::ScopedPathOverride override_start_dir_{base::DIR_USER_STARTUP};
+  base::ScopedPathOverride override_start_dir_{base::DIR_START_MENU};
 #endif  // BUILDFLAG(IS_WIN)
 };
 
