23df362d731f1790f922c523e75c6a2f70f89b25
John Lee <johntlee@chromium.org>
2023-09-13 22:25:10 +0000

    WebUI Gallery: Update demo for cr-action-menu
    Change-Id: Ifd4c17a38bc71535d84a2d191cad5feeaf2187d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4854004
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196288}

--- chrome/browser/resources/webui_gallery/demos/cr_action_menu/cr_action_menu_demo.ts
+++ chrome/browser/resources/webui_gallery/demos/cr_action_menu/cr_action_menu_demo.ts
@@ -2,25 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_icons.css.js';
 import '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
-import '//resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
-import '//resources/cr_elements/cr_input/cr_input.js';
 import '//resources/cr_elements/icons.html.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
 import '../demo.css.js';
 
-import {AnchorAlignment, CrActionMenuElement, ShowAtPositionConfig} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
-import {CrInputElement} from '//resources/cr_elements/cr_input/cr_input.js';
-import {DomRepeatEvent, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {AnchorAlignment, CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
+import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './cr_action_menu_demo.html.js';
 
 interface CrActionMenuDemoElement {
   $: {
     menu: CrActionMenuElement,
+    minMaxContainer: HTMLDivElement,
+    anchorAlignmentDemo: HTMLButtonElement,
   };
 }
 
@@ -36,9 +34,14 @@ class CrActionMenuDemoElement extends PolymerElement {
   static get properties() {
     return {
       alignmentOptions_: Array,
-      enableCustomContextMenu_: Boolean,
-      numberInputs_: Array,
-      showAtPositionConfig_: Object,
+      customAlignmentX_: {
+        type: String,
+        value: 'CENTER',
+      },
+      customAlignmentY_: {
+        type: String,
+        value: 'CENTER',
+      },
       statusText_: String,
     };
   }
@@ -50,84 +53,40 @@ class CrActionMenuDemoElement extends PolymerElement {
     'BEFORE_END',
     'AFTER_END',
   ];
-  private enableCustomContextMenu_: boolean = false;
-  private numberInputs_: Array<keyof ShowAtPositionConfig> = [
-    'top',
-    'left',
-    'width',
-    'height',
-    'minX',
-    'minY',
-    'maxX',
-    'maxY',
-  ];
-  private showAtPositionConfig_: ShowAtPositionConfig = {top: 0, left: 0};
-  private statusText_: string;
-
-  override ready() {
-    super.ready();
-    this.addEventListener('contextmenu', this.onContextMenu_.bind(this));
-  }
-
-  private onAlignmentChangedX_(event: Event) {
-    const select = event.target as HTMLSelectElement;
-    if (!select.value) {
-      delete this.showAtPositionConfig_.anchorAlignmentX;
-      return;
-    }
-    const key = select.value as keyof typeof AnchorAlignment;
-    this.showAtPositionConfig_.anchorAlignmentX = AnchorAlignment[key];
-  }
-
-  private onAlignmentChangedY_(event: Event) {
-    const select = event.target as HTMLSelectElement;
-    if (!select.value) {
-      delete this.showAtPositionConfig_.anchorAlignmentY;
-      return;
-    }
-    const key = select.value as keyof typeof AnchorAlignment;
-    this.showAtPositionConfig_.anchorAlignmentY = AnchorAlignment[key];
-  }
-
-  private onContextMenu_(event: MouseEvent) {
-    if (!this.enableCustomContextMenu_) {
-      return;
-    }
-
-    event.preventDefault();
-    this.$.menu.close();
-    this.$.menu.showAtPosition({top: event.clientY, left: event.clientX});
-  }
-
-  private onMenuItem1Click_() {
-    this.$.menu.close();
-    this.statusText_ = 'Clicked item 1';
-  }
-
-  private onMenuItem2Click_() {
-    this.$.menu.close();
-    this.statusText_ = 'Clicked item 2';
-  }
+  private customAlignmentX_: keyof typeof AnchorAlignment;
+  private customAlignmentY_: keyof typeof AnchorAlignment;
 
   private onShowAnchoredMenuClick_(event: MouseEvent) {
     this.$.menu.showAt(event.target as HTMLElement);
   }
 
-  private onShowMenuClick_() {
-    this.$.menu.showAtPosition(this.showAtPositionConfig_);
+  private onContextMenu_(event: MouseEvent) {
+    event.preventDefault();
+    this.$.menu.close();
+    this.$.menu.showAtPosition({top: event.clientY, left: event.clientX});
   }
 
-  private onNumberInputChanged_(
-      event: DomRepeatEvent<keyof ShowAtPositionConfig>) {
-    const crInput = event.target as CrInputElement;
-    const key = event.model.item;
-    // Inputs have values that are strings, so convert them to numbers or
-    // delete them from the config if undefined.
-    if (crInput.value) {
-      (this.showAtPositionConfig_[key] as number) = Number(crInput.value);
-    } else {
-      delete this.showAtPositionConfig_[key];
-    }
+  private onShowMinMaxMenu_(event: MouseEvent) {
+    const minMaxContainerRect = this.$.minMaxContainer.getBoundingClientRect();
+    const config = {
+      minX: minMaxContainerRect.left,
+      maxX: minMaxContainerRect.right,
+      minY: minMaxContainerRect.top,
+      maxY: minMaxContainerRect.bottom,
+    };
+    this.$.menu.showAt(event.target as HTMLElement, config);
+  }
+
+  private onAnchorAlignmentDemoClick_() {
+    this.$.menu.showAt(this.$.anchorAlignmentDemo, {
+      anchorAlignmentX: AnchorAlignment[this.customAlignmentX_],
+      anchorAlignmentY: AnchorAlignment[this.customAlignmentY_],
+    });
+  }
+
+  private isSelectedAlignment_(
+      selectedAlignment: AnchorAlignment, option: AnchorAlignment) {
+    return selectedAlignment === option;
   }
 }
 
