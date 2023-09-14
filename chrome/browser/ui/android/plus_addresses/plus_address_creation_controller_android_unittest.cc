fd00d2536294bb7e02e6f90f7bfae0f58ffc07bd
Matt Reichhoff <mreichhoff@chromium.org>
2023-09-14 15:38:29 +0000

    Add more plus_address android controller tests
    This path was previously untested due to concerns with flakiness in
    crbug.com/1479967. In theory that has resolved, so adding the tests.
    
    Change-Id: I77fb9255e17e48fec3d7261232f5165fe099fce2
    Bug: 1467623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864805
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196618}

--- chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
+++ chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
@@ -9,23 +9,92 @@
 #include "base/test/gmock_callback_support.h"
 #include "base/test/mock_callback.h"
 #include "chrome/browser/plus_addresses/plus_address_service_factory.h"
+#include "chrome/browser/profiles/profile_test_util.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/plus_addresses/features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/web_contents_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+// TODO(crbug.com/1467623): Consolidate this and the desktop version. Splitting
+// them was a mechanism to reduce dependencies during implementation, but isn't
+// the ideal long-term state.
 namespace plus_addresses {
 
-class PlusAddressCreationControllerAndroidTest : public ::testing::Test {
+namespace {
+// Used to control the behavior of the controller's `plus_address_service_`
+// (though mocking would also be fine). Most importantly, this avoids the
+// requirement to mock the identity portions of the `PlusAddressService`.
+class MockPlusAddressService : public PlusAddressService {
+ public:
+  MockPlusAddressService() = default;
+
+  void OfferPlusAddressCreation(const url::Origin& origin,
+                                PlusAddressCallback callback) override {
+    std::move(callback).Run("plus+plus@plus.plus");
+  }
+};
+}  // namespace
+
+// Testing very basic functionality for now. As UI complexity increases, this
+// class will grow and mutate.
+class PlusAddressCreationControllerAndroidEnabledTest : public testing::Test {
+ public:
+  std::unique_ptr<KeyedService> PlusAddressServiceTestFactory(
+      content::BrowserContext* context) {
+    return std::make_unique<MockPlusAddressService>();
+  }
+
+ protected:
+  content::BrowserTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList features_{kFeature};
+};
+
+TEST_F(PlusAddressCreationControllerAndroidEnabledTest, DirectCallback) {
+  // Ensure that the feature is known to be enabled, such that
+  // `PlusAddressServiceFactory` doesn't bail early with a null return.
+  profiles::testing::ScopedProfileSelectionsForFactoryTesting
+      overide_profile_selections(
+          PlusAddressServiceFactory::GetInstance(),
+          PlusAddressServiceFactory::CreateProfileSelections());
+
+  TestingProfile test_profile;
+  std::unique_ptr<content::WebContents> web_contents =
+      content::WebContentsTester::CreateTestWebContents(&test_profile, nullptr);
+  PlusAddressServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+      &test_profile,
+      base::BindRepeating(&PlusAddressCreationControllerAndroidEnabledTest::
+                              PlusAddressServiceTestFactory,
+                          base::Unretained(this)));
+
+  PlusAddressCreationControllerAndroid::CreateForWebContents(
+      web_contents.get());
+  PlusAddressCreationControllerAndroid* controller =
+      PlusAddressCreationControllerAndroid::FromWebContents(web_contents.get());
+  controller->set_suppress_ui_for_testing(true);
+
+  base::MockOnceCallback<void(const std::string&)> callback;
+  EXPECT_CALL(callback, Run).Times(1);
+  controller->OfferCreation(
+      url::Origin::Create(GURL("https://mattwashere.example")), callback.Get());
+  controller->OnConfirmed();
+}
+
+class PlusAddressCreationControllerAndroidDisabledTest
+    : public ::testing::Test {
+ public:
+  void SetUp() override { features_.InitAndDisableFeature(kFeature); }
+
  private:
   content::BrowserTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList features_;
 };
 
 // With the feature disabled, the `KeyedService` is not present; ensure this is
 // handled. While this code path should not be called in that case, it is
 // validated here for safety.
-TEST_F(PlusAddressCreationControllerAndroidTest, ConfirmedNullService) {
+TEST_F(PlusAddressCreationControllerAndroidDisabledTest, ConfirmedNullService) {
   TestingProfile profile;
   std::unique_ptr<content::WebContents> web_contents =
       content::WebContentsTester::CreateTestWebContents(&profile, nullptr);
