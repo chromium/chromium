f911e960d9d2953e7cf99adc175dcc9d42db1c5c
Jonathan Lee <jonathanjlee@google.com>
2023-09-14 23:52:40 +0000

    Import wpt@738006798d5285c6db5893d4b8edd41dcc94cb96
    Using wpt-import in Chromium 3253306ce6e6af351ce4a17b102a9fc9207cd626.
    With Chromium commits locally applied on WPT:
    9836daebaf "[text-autospace] Refactor tests"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rego@igalia.com:
      external/wpt/css/css-contain
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: Ic342f5098d829a630d3ebeb804709f02b4560af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864486
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1196897}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/user_prompt_opened/user_prompt_opened.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/user_prompt_opened/user_prompt_opened.py
@@ -56,6 +56,29 @@ async def test_prompt_type(
     }
 
 
+async def test_prompt_default_value(
+    bidi_session, inline, new_tab, subscribe_events, wait_for_event
+):
+    await subscribe_events(events=[USER_PROMPT_OPENED_EVENT])
+    on_entry = wait_for_event(USER_PROMPT_OPENED_EVENT)
+
+    text = "test"
+    default = "default"
+
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"],
+        url=inline(f"<script>window.prompt('{text}', '{default}')</script>"),
+    )
+
+    event = await on_entry
+
+    assert event == {
+        "context": new_tab["context"],
+        "type": "prompt",
+        "message": text,
+        "defaultValue": default,
+    }
+
 @pytest.mark.parametrize("type_hint", ["tab", "window"])
 async def test_subscribe_to_one_context(
     bidi_session, subscribe_events, inline, wait_for_event, type_hint
