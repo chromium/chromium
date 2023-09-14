f1ef886204e41ebfbf15bce2903d0988d510037c
Brad Triebwasser <btriebw@chromium.org>
2023-09-14 20:20:51 +0000

    Configure FullscreenPopupWindows flag for origin trial.
    Adds the required FullscreenPopupWindowsEnabled overload (with context).
    Enables the base flag by default, but not the blink flag.
    
    Bug: 1142516
    Change-Id: I1cfe7cf983a9bab0a5205ed4eb499a10495cf217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861185
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1196773}

--- third_party/blink/renderer/core/page/create_window.cc
+++ third_party/blink/renderer/core/page/create_window.cc
@@ -209,7 +209,8 @@ WebWindowFeatures GetWindowFeaturesFromString(const String& feature_string,
     } else if (key_string == "persistent") {
       window_features.persistent = true;
     } else if (key_string == "fullscreen" &&
-               RuntimeEnabledFeatures::FullscreenPopupWindowsEnabled()) {
+               RuntimeEnabledFeatures::FullscreenPopupWindowsEnabled(
+                   dom_window)) {
       // TODO(crbug.com/1142516): Add permission check to give earlier
       // feedback / console warning if permission isn't granted, and/or just
       // silently drop the flag. Currently the browser will block the popup
