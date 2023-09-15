7e56853e7d8b6662daa2c3df9312d0f2967b4c79
Andrew Grieve <agrieve@chromium.org>
2023-09-15 16:06:12 +0000

    Reland "Android: Fix appComponentFactory not being used when incremental_install=true"
    This reverts commit 18b69067074658a17e4cc266c081e2cd9cac312d.
    
    Reason for reland: Updated test runner
    
    Original change's description:
    > Revert "Android: Fix appComponentFactory not being used when incremental_install=true"
    >
    > This reverts commit c5017fb5c3ba0120f25b264769dfaa97459ec6c9.
    >
    > Reason for revert: Tests are showing:
    > Support not implemented for incremental_install=true on tests that do not use //base's test runner.
    >
    > Original change's description:
    > > Android: Fix appComponentFactory not being used when incremental_install=true
    > >
    > > Bug: None
    > > Change-Id: Id7617c76bc364466d6e9f2b040d03df8a3ba4597
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866629
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1196658}
    >
    > Bug: None
    > Change-Id: If231bf98f95928f5fb0adb6f07048b5352d315ba
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865368
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1196760}
    
    Bug: None
    Change-Id: I14c7ad1f6f0f75915c73b47008c1be4a02b82f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868051
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197187}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -751,10 +751,11 @@ class InstrumentationTestInstance(test_instance.TestInstance):
 
     if self._junit4_runner_class:
       if self._test_apk_incremental_install_json:
-        self._junit4_runner_supports_listing = next(
-            (True for x in self._test_apk.GetAllMetadata()
-             if 'real-instr' in x[0] and x[1] in _TEST_LIST_JUNIT4_RUNNERS),
-            False)
+        for name, value in self._test_apk.GetAllMetadata():
+          if (name.startswith('incremental-install-instrumentation-')
+              and value in _TEST_LIST_JUNIT4_RUNNERS):
+            self._junit4_runner_supports_listing = True
+            break
       else:
         self._junit4_runner_supports_listing = (
             self._junit4_runner_class in _TEST_LIST_JUNIT4_RUNNERS)
