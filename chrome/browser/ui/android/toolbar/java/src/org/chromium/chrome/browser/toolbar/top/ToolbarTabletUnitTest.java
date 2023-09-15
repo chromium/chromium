81ade951bf873a1381d8d1593a4b6b691b193348
Zhe Li <zheliooo@google.com>
2023-09-15 18:33:04 +0000

    [M118][APS] Update tooltip visibility and remove hover highlight for StatusView
    - Remove tooltip when StatusIconView is not visible.
    - remove hover highlight when StatusIconView is not visible(e.g. default engine is not Google).
    - remove hover highlight when VerboseStatusText is visible(e.g. offline).
    
    demo(default engine not Google): https://drive.google.com/file/d/1DPIhHtWN4RnjPvkysyCSfV_gd73ZMjPb/view?usp=sharing
    
    demo(offline)
    https://drive.google.com/file/d/1-1Y_t59i8AcxFv6sLpBE-kG2tsSGoi4s/view?usp=sharing
    
    TODO: relocate the logic in StatusView to StatusViewBinder or StatusProperties.
    
    (cherry picked from commit 37d04b2089c15cb152a8a559887a7d10c66572be)
    
    Bug: 1476816, 1476802
    Change-Id: I005fec4cb75dfea3f674439651b526009dbe6d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4833418
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1194960}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864404
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5993@{#377}
    Cr-Branched-From: 511350718e646be62331ae9d7213d10ec320d514-refs/heads/main@{#1192594}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -37,6 +38,7 @@ import org.chromium.chrome.browser.omnibox.LocationBarCoordinator;
 import org.chromium.chrome.browser.omnibox.LocationBarCoordinatorTablet;
 import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
+import org.chromium.chrome.browser.omnibox.status.StatusView;
 import org.chromium.chrome.browser.toolbar.HomeButton;
 import org.chromium.chrome.browser.toolbar.R;
 import org.chromium.chrome.browser.toolbar.menu_button.MenuButtonCoordinator;
@@ -339,6 +341,35 @@ public final class ToolbarTabletUnitTest {
         Assert.assertEquals("Tooltip text for Save Offline button is not as expected",
                 mActivity.getResources().getString(R.string.download_page),
                 mSaveOfflineButton.getTooltipText());
+    }
+
+    @Test
+    public void testStatusViewHoverAction() {
+        StatusView statusViewSpy = spy(mToolbarTablet.findViewById(R.id.location_bar_status));
+        // Do NOT show status view tooltip and background when status icon is NOT visible.
+        when(statusViewSpy.isSearchEngineStatusIconVisible()).thenReturn(false);
+        statusViewSpy.setHoverActionOnVisibilityChange();
+        Assert.assertEquals("Tooltip text for Status view is not as expected", null,
+                statusViewSpy.getTooltipText());
+        Assert.assertEquals("Background for Status view is not as expected", null,
+                statusViewSpy.getBackground());
+
+        // Show status view tooltip and background when status icon is visible.
+        when(statusViewSpy.isSearchEngineStatusIconVisible()).thenReturn(true);
+        statusViewSpy.setHoverActionOnVisibilityChange();
+        Assert.assertEquals("Tooltip text for Status view is not as expected",
+                mActivity.getResources().getString(R.string.accessibility_menu_info),
+                statusViewSpy.getTooltipText());
+        Assert.assertNotEquals("Background for Status view is not as expected", null,
+                statusViewSpy.getBackground());
+
+        // Only show status view tooltip when verbose status text is visible.
+        statusViewSpy.setVerboseStatusTextVisible(true);
+        Assert.assertEquals("Tooltip text for Status view is not as expected",
+                mActivity.getResources().getString(R.string.accessibility_menu_info),
+                statusViewSpy.getTooltipText());
+        Assert.assertEquals("Background for Status view is not as expected", null,
+                statusViewSpy.getBackground());
     }
 
     @Test
