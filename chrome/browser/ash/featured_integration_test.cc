17f18e761f7b0c300fc9ccbe3a2a44d705b79a7c
James Cook <jamescook@chromium.org>
2023-09-15 18:23:04 +0000

    Port featured.FeatureLibraryLateBoot Tast test to Crosier
    This test exercises the ChromeOS platform-level experiments framework,
    which is implemented in a helper library called "featured" used by
    platform components that want to handle Finch experiments.
    
    - Launch featured.FeatureLibraryLateBoot.check helper binary to get its
      view of feature state
    - Collect app output on a worker thread, because the helper binary calls
      back into chromeos_integration_tests over D-Bus, so collecting output
      on the main thread causes a deadlock
    - Use a list of test cases to describe the expected JSON output from the
      helper binary for each experiment scenario
    
    Tast source:
    https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/tast-tests/src/go.chromium.org/tast-tests/cros/local/bundles/cros/featured/feature_library_late_boot.go
    
    Bug: b/300649094
    Test: chromeos_integration_tests
    Change-Id: Id67658907ab97fbe3f56b777a35b1f95aa425a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868647
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197267}

---
+++ chrome/browser/ash/featured_integration_test.cc
@@ -0,0 +1,200 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+#include <vector>
+
+#include "base/functional/bind.h"
+#include "base/json/json_reader.h"
+#include "base/process/launch.h"
+#include "base/run_loop.h"
+#include "base/strings/string_split.h"
+#include "base/task/task_traits.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/values.h"
+#include "chrome/test/base/chromeos/crosier/interactive_ash_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace ash {
+namespace {
+
+constexpr char kHelperBinary[] =
+    "/usr/local/libexec/tast/helpers/local/cros/"
+    "featured.FeatureLibraryLateBoot.check";
+
+// The tests start the chromeos_integration_tests binary with two features
+// enabled or disabled, with or without parameters, simulating a Finch
+// experiment. One feature is default-enabled; the other feature is
+// default-disabled. The helper binary (a simulation of a platform binary)
+// returns its view of the feature state as a JSON object. The test cases have
+// expected output for the default-enabled and default-disabled features.
+struct TestCase {
+  const char* test_name = "";
+  const char* enabled_features = "";
+  const char* disabled_features = "";
+  const char* expected_default_enabled = "";
+  const char* expected_default_disabled = "";
+};
+
+class FeaturedIntegrationTest : public InteractiveAshTest,
+                                public ::testing::WithParamInterface<TestCase> {
+ public:
+  FeaturedIntegrationTest() {
+    feature_list_.InitFromCommandLine(GetParam().enabled_features,
+                                      GetParam().disabled_features);
+  }
+
+  base::test::ScopedFeatureList feature_list_;
+};
+
+constexpr TestCase kTestCases[] = {
+    {.test_name = "experiment_enabled_without_params",
+     .enabled_features =
+         "CrOSLateBootTestDefaultEnabled,CrOSLateBootTestDefaultDisabled",
+     .expected_default_enabled = R"(
+        {
+          "EnabledCallbackEnabledResult": true,
+          "FeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackEnabledResult": true,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )",
+     .expected_default_disabled = R"(
+        {
+          "EnabledCallbackEnabledResult": true,
+          "FeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackEnabledResult": true,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )"},
+    {.test_name = "experiment_disabled_without_params",
+     .disabled_features =
+         "CrOSLateBootTestDefaultEnabled,CrOSLateBootTestDefaultDisabled",
+     .expected_default_enabled = R"(
+        {
+          "EnabledCallbackEnabledResult": false,
+          "FeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackEnabledResult": false,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )",
+     .expected_default_disabled = R"(
+        {
+          "EnabledCallbackEnabledResult": false,
+          "FeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackEnabledResult": false,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )"},
+    {.test_name = "experiment_enabled_with_params",
+     .enabled_features = "CrOSLateBootTestDefaultEnabled:k1/v1/k2/v2,"
+                         "CrOSLateBootTestDefaultDisabled:k3/v3/k4/v4",
+     .expected_default_enabled = R"(
+        {
+          "EnabledCallbackEnabledResult": true,
+          "FeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackEnabledResult": true,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackParamsResult": {
+            "k1":"v1",
+            "k2":"v2"
+          }
+        }
+      )",
+     .expected_default_disabled = R"(
+        {
+          "EnabledCallbackEnabledResult": true,
+          "FeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackEnabledResult": true,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackParamsResult": {
+            "k3":"v3",
+            "k4":"v4"
+          }
+        }
+      )"},
+    {.test_name = "experiment_default",
+     .enabled_features = "",
+     .expected_default_enabled = R"(
+        {
+          "EnabledCallbackEnabledResult": true,
+          "FeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackEnabledResult": true,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultEnabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )",
+     .expected_default_disabled = R"(
+        {
+          "EnabledCallbackEnabledResult": false,
+          "FeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackEnabledResult": false,
+          "ParamsCallbackFeatureName": "CrOSLateBootTestDefaultDisabled",
+          "ParamsCallbackParamsResult": {}
+        }
+      )"},
+
+};
+
+const char* NameFromTestCase(::testing::TestParamInfo<TestCase> info) {
+  return info.param.test_name;
+}
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         FeaturedIntegrationTest,
+                         ::testing::ValuesIn(kTestCases),
+                         NameFromTestCase);
+
+IN_PROC_BROWSER_TEST_P(FeaturedIntegrationTest, FeatureLibraryLateBoot) {
+  // Collect stdout from the helper binary. The binary will call back via D-Bus
+  // into chromeos_integration_tests, so collect the binary's output off the
+  // main thread.
+  std::string output;
+  base::RunLoop run_loop;
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock()},
+      base::BindOnce(
+          [](std::string* output, base::RunLoop* run_loop) {
+            // Run the helper binary to get its feature state.
+            ASSERT_TRUE(base::GetAppOutput({kHelperBinary}, output));
+            run_loop->Quit();
+          },
+          &output, &run_loop));
+  run_loop.Run();
+
+  // The helper binary returns two lines of output.
+  std::vector<std::string> split_out = base::SplitString(
+      output, "\n", base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  ASSERT_EQ(split_out.size(), 2u);
+
+  // The first line of the output is the state of the default enabled feature.
+  absl::optional<base::Value> default_enabled =
+      base::JSONReader::Read(split_out[0]);
+  ASSERT_TRUE(default_enabled.has_value());
+
+  // The test case has the expected JSON for the default enabled feature.
+  absl::optional<base::Value> expected_default_enabled =
+      base::JSONReader::Read(GetParam().expected_default_enabled);
+  ASSERT_TRUE(expected_default_enabled.has_value());
+  EXPECT_EQ(default_enabled, expected_default_enabled);
+
+  // The second line of the output is the default disabled feature.
+  absl::optional<base::Value> default_disabled =
+      base::JSONReader::Read(split_out[1]);
+  ASSERT_TRUE(default_disabled.has_value());
+
+  // The test case has the expected JSON for the default disabled feature.
+  absl::optional<base::Value> expected_default_disabled =
+      base::JSONReader::Read(GetParam().expected_default_disabled);
+  ASSERT_TRUE(expected_default_disabled.has_value());
+  EXPECT_EQ(default_disabled, expected_default_disabled);
+}
+
+}  // namespace
+}  // namespace ash
