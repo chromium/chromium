6b25ea37616df5b9e5f7a7a1d3d8bfaa5616188d
Timofey Chudakov <tchudakov@google.com>
2023-09-17 07:30:32 +0000

    [mullet m2] Split EditAddressProfileDialogController tests.
    edit_address_profile_dialog_controller_impl_browsertest.cc is used
    both to verify both UI and business logic. Instead we should have separate unit and integration tests. This CL implements this split by converting existing browser tests into unit tests and introducing
    dedicated interactive UI tests.
    
    Bug: 1459990
    Change-Id: I242b2150ddc4dc4e86e641a21c2d5f7ff8a91937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4846629
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197602}

--- chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl_unittest.cc
@@ -9,30 +9,31 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
-#include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace autofill {
 
 using ::testing::_;
 
-class EditAddressProfileDialogControllerImplTest : public DialogBrowserTest {
+class EditAddressProfileDialogControllerImplTest
+    : public BrowserWithTestWindowTest {
  public:
   EditAddressProfileDialogControllerImplTest() = default;
 
-  // DialogBrowserTest:
-  void ShowUi(const std::string& name) override {
+  void SetUp() override {
+    BrowserWithTestWindowTest::SetUp();
+    AddTab(browser(), GURL("about:blank"));
     EditAddressProfileDialogControllerImpl::CreateForWebContents(
         web_contents());
-    EditAddressProfileDialogControllerImpl* dialog_controller = controller();
-    ASSERT_THAT(dialog_controller, ::testing::NotNull());
-    dialog_controller->OfferEdit(profile_, /*original_profile=*/nullptr,
-                                 /*footer_message=*/u"", save_callback_.Get(),
-                                 cancel_callback_.Get(),
-                                 /*is_migration_to_account=*/false);
+    ASSERT_THAT(controller(), ::testing::NotNull());
+    controller()->OfferEdit(profile_, /*original_profile=*/nullptr,
+                            /*footer_message=*/u"", save_callback_.Get(),
+                            cancel_callback_.Get(),
+                            /*is_migration_to_account=*/false);
   }
 
  protected:
@@ -53,51 +54,42 @@ class EditAddressProfileDialogControllerImplTest : public DialogBrowserTest {
   base::MockOnceClosure cancel_callback_;
 };
 
-IN_PROC_BROWSER_TEST_F(EditAddressProfileDialogControllerImplTest,
-                       InvokeUi_Edit) {
-  ShowAndVerifyUi();
-}
-
-IN_PROC_BROWSER_TEST_F(EditAddressProfileDialogControllerImplTest,
-                       CloseTab_CancelCallbackInvoked) {
+TEST_F(EditAddressProfileDialogControllerImplTest,
+       CloseTab_CancelCallbackInvoked) {
   EXPECT_CALL(save_callback_, Run).Times(0);
   EXPECT_CALL(cancel_callback_, Run).Times(0);
-  ShowUi("CloseTab_NoCallbacksInvoked");
 
   content::WebContents* tab =
       browser()->tab_strip_model()->GetActiveWebContents();
   tab->Close();
 }
 
-IN_PROC_BROWSER_TEST_F(EditAddressProfileDialogControllerImplTest,
-                       IgnoreDialog_CancelCallbackInvoked) {
+TEST_F(EditAddressProfileDialogControllerImplTest,
+       IgnoreDialog_CancelCallbackInvoked) {
   EXPECT_CALL(save_callback_, Run).Times(0);
   EXPECT_CALL(cancel_callback_, Run);
-  ShowUi("IgnoreDialog_CancelCallbackInvoked");
 
   controller()->OnUserDecision(
       AutofillClient::SaveAddressProfileOfferUserDecision::kIgnored, profile_);
 }
 
-IN_PROC_BROWSER_TEST_F(EditAddressProfileDialogControllerImplTest,
-                       CancelEditing_CancelCallbackInvoked) {
+TEST_F(EditAddressProfileDialogControllerImplTest,
+       CancelEditing_CancelCallbackInvoked) {
   EXPECT_CALL(save_callback_, Run).Times(0);
   EXPECT_CALL(cancel_callback_, Run);
-  ShowUi("CancelEditing_CancelCallbackInvoked");
 
   controller()->OnUserDecision(
       AutofillClient::SaveAddressProfileOfferUserDecision::kEditDeclined,
       profile_);
 }
 
-IN_PROC_BROWSER_TEST_F(EditAddressProfileDialogControllerImplTest,
-                       SaveAddress_SaveCallbackInvoked) {
+TEST_F(EditAddressProfileDialogControllerImplTest,
+       SaveAddress_SaveCallbackInvoked) {
   EXPECT_CALL(
       save_callback_,
       Run(AutofillClient::SaveAddressProfileOfferUserDecision::kEditAccepted,
           profile_));
   EXPECT_CALL(cancel_callback_, Run).Times(0);
-  ShowUi("SaveAddress_SaveCallbackInvoked");
 
   controller()->OnUserDecision(
       AutofillClient::SaveAddressProfileOfferUserDecision::kEditAccepted,
