d989d3292993d4ee665aa91a4035e29d0d7dc14d
Michael Thiessen <mthiesse@chromium.org>
2023-09-18 18:18:51 +0000

    Fix cold start detection for CustomTabs.Startup.TimeTo*
    We can use the AppComponentFactory to figure out if Chrome was started
    for an Activity or for something in the background, and ignore when
    Chrome was already started for something in the background.
    
    ContentProviders are somewhat complicated as Android always
    initializes them when initializing the Application, but if we post a
    task to set the reason to provider Android seems to always create the
    Activity/Service, if one is being started, first.
    
    how tests work. This is just for metrics so not worth the effort.
    
    Low-Coverage-Reason: Cold start cannot be meaningfully tested given
    Bug: 1476038
    Change-Id: I7ee6427d3e0f9566b60b04f5f531f3474070bb05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863059
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197915}

--- chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatAppComponentFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatAppComponentFactory.java
@@ -8,17 +8,25 @@ import static org.chromium.chrome.browser.base.SplitCompatApplication.CHROME_SPL
 
 import android.app.Activity;
 import android.app.AppComponentFactory;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ContentProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.RequiresApi;
 
 import org.chromium.base.BundleUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.task.PostTask;
+import org.chromium.base.task.TaskTraits;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * There are some cases where the ClassLoader for components in the chrome split does not match the
@@ -33,9 +41,29 @@ import org.chromium.base.Log;
 public class SplitCompatAppComponentFactory extends AppComponentFactory {
     private static final String TAG = "SplitCompat";
 
+    @IntDef({ProcessCreationReason.UNINITIALIZED, ProcessCreationReason.PENDING,
+            ProcessCreationReason.ACTIVITY, ProcessCreationReason.SERVICE,
+            ProcessCreationReason.CONTENT_PROVIDER, ProcessCreationReason.BROADCAST_RECEIVER,
+            ProcessCreationReason.NUM_ENTRIES})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ProcessCreationReason {
+        int UNINITIALIZED = -2;
+        int PENDING = -1;
+        int ACTIVITY = 0;
+        int SERVICE = 1;
+        int CONTENT_PROVIDER = 2;
+        int BROADCAST_RECEIVER = 3;
+        int NUM_ENTRIES = 4;
+    }
+
+    private static @ProcessCreationReason int sProcessCreationReason =
+            ProcessCreationReason.UNINITIALIZED;
+
     @Override
     public Activity instantiateActivity(ClassLoader cl, String className, Intent intent)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        setProcessCreationReason(ProcessCreationReason.ACTIVITY);
+
         // Activities will not call createContextForSplit() which will normally ensure the preload
         // is finished, so we have to manually ensure that here.
         SplitChromeApplication.finishPreload(CHROME_SPLIT_NAME);
@@ -45,16 +73,45 @@ public class SplitCompatAppComponentFactory extends AppComponentFactory {
     @Override
     public ContentProvider instantiateProvider(ClassLoader cl, String className)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        // Android always initializes all ContentProviders when it initializes the Application, so
+        // post a task to set the reason asynchronously which will run after Android creates any
+        // other components during the launch.
+        if (sProcessCreationReason == ProcessCreationReason.UNINITIALIZED) {
+            sProcessCreationReason = ProcessCreationReason.PENDING;
+            PostTask.postTask(TaskTraits.UI_DEFAULT,
+                    () -> { setProcessCreationReason(ProcessCreationReason.CONTENT_PROVIDER); });
+        }
         return super.instantiateProvider(getComponentClassLoader(cl, className), className);
     }
 
     @Override
     public BroadcastReceiver instantiateReceiver(ClassLoader cl, String className, Intent intent)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        setProcessCreationReason(ProcessCreationReason.BROADCAST_RECEIVER);
+
         // Receivers call createContextForSplit() on the ContextImpl, which will not run the logic
         // in SplitChromeApplication which makes sure the preload has finished.
         SplitChromeApplication.finishPreload(CHROME_SPLIT_NAME);
         return super.instantiateReceiver(getComponentClassLoader(cl, className), className, intent);
+    }
+
+    @Override
+    public Service instantiateService(ClassLoader cl, String className, Intent intent)
+            throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        setProcessCreationReason(ProcessCreationReason.SERVICE);
+        return super.instantiateService(cl, className, intent);
+    }
+
+    private static void setProcessCreationReason(@ProcessCreationReason int reason) {
+        if (sProcessCreationReason > ProcessCreationReason.PENDING) return;
+        sProcessCreationReason = reason;
+        if (!SplitCompatApplication.isBrowserProcess()) return;
+        RecordHistogram.recordEnumeratedHistogram("Startup.Android.BrowserProcessCreationReason",
+                reason, ProcessCreationReason.NUM_ENTRIES);
+    }
+
+    public static @ProcessCreationReason int getProcessCreationReason() {
+        return sProcessCreationReason;
     }
 
     private static ClassLoader getComponentClassLoader(ClassLoader cl, String className) {
--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabObserver.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Process;
 import android.os.SystemClock;
 import android.text.TextUtils;
@@ -21,6 +22,8 @@ import androidx.browser.customtabs.CustomTabsSessionToken;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.base.SplitCompatAppComponentFactory;
+import org.chromium.chrome.browser.base.SplitCompatAppComponentFactory.ProcessCreationReason;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.customtabs.ClientManager.CalledWarmup;
 import org.chromium.chrome.browser.customtabs.features.TabInteractionRecorder;
@@ -259,10 +262,14 @@ public class CustomTabObserver extends EmptyTabObserver {
 
         mFirstCommitRealtimeMillis = SystemClock.elapsedRealtime();
 
-        if (mCustomTabsConnection == null) return;
+        // ProcessCreationReason is only available on P+.
+        if (mCustomTabsConnection == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.P) return;
 
         String histogram = null;
         long duration = 0;
+        @ProcessCreationReason
+        int processCreationReason = SplitCompatAppComponentFactory.getProcessCreationReason();
+
         // Note that this will exclude Webapp launches in all cases due to either
         // mUsedHiddenTabSpeculation being null, or mIntentReceivedTimestamp being 0.
         if (mUsedHiddenTabSpeculation != null && mUsedHiddenTabSpeculation) {
@@ -275,7 +282,8 @@ public class CustomTabObserver extends EmptyTabObserver {
             if (wasWarmedUp()) {
                 duration = mFirstCommitRealtimeMillis - mIntentReceivedRealtimeMillis;
                 histogram = "CustomTabs.Startup.TimeToFirstCommitNavigation.WarmedUp";
-            } else if (SimpleStartupForegroundSessionDetector.runningCleanForegroundSession()) {
+            } else if (processCreationReason == ProcessCreationReason.ACTIVITY
+                    && SimpleStartupForegroundSessionDetector.runningCleanForegroundSession()) {
                 duration = mFirstCommitRealtimeMillis - Process.getStartElapsedRealtime();
                 histogram = "CustomTabs.Startup.TimeToFirstCommitNavigation.Cold";
             } else {
@@ -290,10 +298,14 @@ public class CustomTabObserver extends EmptyTabObserver {
     }
 
     public void recordLargestContentfulPaint(long lcpUptimeMillis) {
-        if (mCustomTabsConnection == null) return;
+        // ProcessCreationReason is only available on P+.
+        if (mCustomTabsConnection == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.P) return;
 
         String histogram = null;
         long duration = 0;
+        @ProcessCreationReason
+        int processCreationReason = SplitCompatAppComponentFactory.getProcessCreationReason();
+
         // Note that this will exclude Webapp launches in all cases due to either
         // mUsedHiddenTabSpeculation being null, or mIntentReceivedTimestamp being 0.
         if (mUsedHiddenTabSpeculation != null && mUsedHiddenTabSpeculation) {
@@ -306,7 +318,8 @@ public class CustomTabObserver extends EmptyTabObserver {
             if (wasWarmedUp()) {
                 duration = lcpUptimeMillis - mIntentReceivedUptimeMillis;
                 histogram = "CustomTabs.Startup.TimeToLargestContentfulPaint.WarmedUp";
-            } else if (SimpleStartupForegroundSessionDetector.runningCleanForegroundSession()) {
+            } else if (processCreationReason == ProcessCreationReason.ACTIVITY
+                    && SimpleStartupForegroundSessionDetector.runningCleanForegroundSession()) {
                 duration = lcpUptimeMillis - Process.getStartUptimeMillis();
                 histogram = "CustomTabs.Startup.TimeToLargestContentfulPaint.Cold";
             } else {
