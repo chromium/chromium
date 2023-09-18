8738a1dccc233f6b51d7d24649b6519f6dc74a96
Ewann Pelle <ewannpv@chromium.org>
2023-09-18 09:59:42 +0000

    [iOS][1/3] Move ios/chrome/browser/text_selection to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/text_selection will be moved to
    ios/c/b/text_selection/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480365
    Change-Id: Idf91ea80d845c76dcacab0acbb5636566046ba6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4868213
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197751}

---
+++ ios/chrome/browser/text_selection/model/text_classifier_model_service.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TEXT_SELECTION_MODEL_TEXT_CLASSIFIER_MODEL_SERVICE_H_
+#define IOS_CHROME_BROWSER_TEXT_SELECTION_MODEL_TEXT_CLASSIFIER_MODEL_SERVICE_H_
+
+#import "ios/chrome/browser/text_selection/text_classifier_model_service.h"
+
+#endif  // IOS_CHROME_BROWSER_TEXT_SELECTION_MODEL_TEXT_CLASSIFIER_MODEL_SERVICE_H_
