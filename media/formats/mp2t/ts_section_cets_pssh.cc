9bef7b22b2e7df5733755ec1d21059ab4d38110e
Ted Meyer <tmathmeyer@chromium.org>
2023-09-18 22:21:13 +0000

    Check read size
    BitReader.ReadString uses DCHECK to check to ensure num_bits is a valid
    number of bits to read into a string, but DCHECK isn't turned on in
    either release builds or the fuzzer builds. The checks for valid bits
    available needs to be done in TsSectionCetsPssh::Parse.
    
    R=dalecurtis
    
    Fixed: 1478470
    Bug: 1478470
    Change-Id: Iab09ec601a5eb350508101f13a75f54fca246a4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875295
    Auto-Submit: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198042}

--- media/formats/mp2t/ts_section_cets_pssh.cc
+++ media/formats/mp2t/ts_section_cets_pssh.cc
@@ -32,6 +32,11 @@ bool TsSectionCetsPssh::Parse(bool payload_unit_start_indicator,
   RCHECK(bit_reader.SkipBits(31));
   int box_length_bits = bit_reader.bits_available();
   std::string pssh;
+  if (!box_length_bits) {
+    // Ignore if there are no bits to read.
+    return false;
+  }
+  RCHECK(box_length_bits % 8 == 0);
   RCHECK(bit_reader.ReadString(box_length_bits, &pssh));
   // Now check that the first 4 bytes are of the form {0x00, 0x00, 0x00, X},
   // where X is the box length in bytes.
