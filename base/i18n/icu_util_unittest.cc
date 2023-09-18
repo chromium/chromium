f3454e9b25c0a22818ea101b23b642ad8e319fd4
Peter Kasting <pkasting@chromium.org>
2023-09-18 15:44:31 +0000

    Combine duplicate test implementations.
    This also combines nested preprocessor checks.
    
    Bug: none
    Change-Id: I048f528b023b7a9f31a1aec4d92b5f24166a2697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863001
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197852}

--- base/i18n/icu_util_unittest.cc
+++ base/i18n/icu_util_unittest.cc
@@ -7,8 +7,8 @@
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if !BUILDFLAG(IS_NACL)
-#if ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE
+#if !BUILDFLAG(IS_NACL) && (ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE) && \
+    (BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_ANDROID))
 
 namespace base::i18n {
 
@@ -17,25 +17,12 @@ class IcuUtilTest : public testing::Test {
   void SetUp() override { ResetGlobalsForTesting(); }
 };
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
 TEST_F(IcuUtilTest, InitializeIcuSucceeds) {
   bool success = InitializeICU();
 
   ASSERT_TRUE(success);
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
-
-#if BUILDFLAG(IS_ANDROID)
-
-TEST_F(IcuUtilTest, InitializeIcuSucceeds) {
-  bool success = InitializeICU();
-
-  ASSERT_TRUE(success);
-}
-
-#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace base::i18n
 
-#endif  // ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE
-#endif  // !BUILDFLAG(IS_NACL)
+#endif
