ead63e18b24f447d95d9a3e3b8ca66562b570a02
Daniel Vogelheim <vogelheim@chromium.org>
2023-09-18 18:15:27 +0000

    Default enable OriginAgentClusterDefaultEnable.
    This follows the rollout of of the flag via server-side configs.
    
    Intent-to-ship:
    - https://groups.google.com/a/chromium.org/g/blink-dev/c/_oRc19PjpFo
    - https://groups.google.com/a/chromium.org/g/blink-dev/c/nrLl0IxSxSI
    
    Bug: 1259920
    Change-Id: Iba7e1dfb4a106a55de8913dbe37fc5247683dd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4835892
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1197912}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1204,7 +1204,7 @@ BASE_FEATURE(kNoForcedFrameUpdatesForWebTests,
 
 BASE_FEATURE(kOriginAgentClusterDefaultEnabled,
              "OriginAgentClusterDefaultEnable",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOriginAgentClusterDefaultWarning,
              "OriginAgentClusterDefaultWarning",
