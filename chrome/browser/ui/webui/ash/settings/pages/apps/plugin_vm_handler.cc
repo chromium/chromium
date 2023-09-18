dbe40aeb8b9cd22eb0495bbeeb2d258cb720184b
Xiaohui Chen <xiaohuic@google.com>
2023-09-18 22:39:58 +0000

    os settings: move cpp files to new location (13/N)
    * Move some additional apps page related files missed previously
    
    Bug: b/284187997
    Change-Id: Iaa5ce91e4f8d1e4c49ef19866080e1d86701f1ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873871
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198051}

--- chrome/browser/ui/webui/ash/settings/pages/apps/plugin_vm_handler.cc
+++ chrome/browser/ui/webui/ash/settings/pages/apps/plugin_vm_handler.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/settings/ash/plugin_vm_handler.h"
+#include "chrome/browser/ui/webui/ash/settings/pages/apps/plugin_vm_handler.h"
 
 #include <string>
 #include <utility>
