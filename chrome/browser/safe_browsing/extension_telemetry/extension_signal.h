117432cb65a922bb7fb81df1490c3e9428569788
Anunoy Ghosh <anunoy@chromium.org>
2023-09-18 19:09:07 +0000

    Add histogram enum entry for new extension telemetry signal type
    Bug: 1465947
    Change-Id: Ic959ff572a2d6127be8e3fa1563b67e7efdebc18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874616
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1197944}

--- chrome/browser/safe_browsing/extension_telemetry/extension_signal.h
+++ chrome/browser/safe_browsing/extension_telemetry/extension_signal.h
@@ -11,7 +11,9 @@ namespace safe_browsing {
 
 // Signal types.
 // These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
+// numeric values should never be reused. They should be kept in sync with
+// SBExtensionTelemetrySignalsSignalType enum definition in
+// /tools/metrics/histograms/enums.xml
 enum class ExtensionSignalType {
   kTabsExecuteScript = 0,
   kRemoteHostContacted = 1,
