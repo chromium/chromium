db3e9a6b8c712c8863bc67ae308062cd528b01ec
Ashley Prasad <ashleydp@google.com>
2023-09-19 20:05:21 +0000

    scanning: Convert color-mode-select to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Remove underscores from private properties and functions.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: Iee4ddde64e029c37f72a51f3cec1aaa0d1e64121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866755
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198570}

--- ash/webui/scanning/resources/color_mode_select.ts
+++ ash/webui/scanning/resources/color_mode_select.ts
@@ -6,7 +6,7 @@ import './scan_settings_section.js';
 import './strings.m.js';
 
 import {assert} from 'chrome://resources/ash/common/assert.js';
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
+import {I18nBehavior} from 'chrome://resources/ash/common/i18n_behavior.js';
 import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './color_mode_select.html.js';
@@ -22,41 +22,28 @@ const DEFAULT_COLOR_MODE = ColorMode.kColor;
  * 'color-mode-select' displays the available scanner color modes in a dropdown.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- * @implements {SelectBehaviorInterface}
- */
+// TODO(b/300484132): Replace mixinBehavior with mixin implementation once a
+// Mixin version of SelectBehavior is available.
 const ColorModeSelectElementBase =
-    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement);
+    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement) as
+    {new (): PolymerElement & I18nBehavior & SelectBehaviorInterface};
 
-/** @polymer */
 class ColorModeSelectElement extends ColorModeSelectElementBase {
   static get is() {
-    return 'color-mode-select';
+    return 'color-mode-select' as const;
   }
 
   static get template() {
     return getTemplate();
   }
 
-  /**
-   * @param {number} index
-   * @return {string}
-   */
-  getOptionAtIndex(index) {
+  getOptionAtIndex(index: number): string {
     assert(index < this.options.length);
 
     return this.options[index].toString();
   }
 
-  /**
-   * @param {!ColorMode} mojoColorMode
-   * @return {string}
-   * @private
-   */
-  getColorModeString_(mojoColorMode) {
+  getColorModeAsString(mojoColorMode: ColorMode): string {
     return getColorModeString(mojoColorMode);
   }
 
@@ -66,12 +53,14 @@ class ColorModeSelectElement extends ColorModeSelectElementBase {
     });
   }
 
-  /**
-   * @param {!ColorMode} option
-   * @return {boolean}
-   */
-  isDefaultOption(option) {
+  isDefaultOption(option: ColorMode): boolean {
     return option === DEFAULT_COLOR_MODE;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ColorModeSelectElement.is]: ColorModeSelectElement;
   }
 }
 
