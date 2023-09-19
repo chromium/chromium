862065a892023bc53d78df6465678bbe57652fae
Maksim Sadym <sadym@chromium.org>
2023-09-19 13:05:23 +0000

    Speed up and re-enable deep dom serialization tests
    Analogue of crrev.com/c/4755526.
    
    For deep dom serialization tests, instead of waiting for each individual test to complete before starting the next one, we now eagerly start all the tests.
    
    This reduces the test run time by a factor of 10× (from 7053 ms down to 668 ms) as observed on my workstation using these commands:
    
    ```
    autoninja -C out/Default chrome blink_tests && \
    third_party/blink/tools/run_web_tests.py -t Default http/tests/inspector-protocol/runtime/serialization-dom-types.js --timeout-ms 30000 && \
    cat out/Default/layout-test-results/times_ms.json
    ```
    
    Before:
    ```
    {"http":{"tests":{"inspector-protocol":{"runtime":{"serialization-dom-types.js":7053}}}}}
    ```
    
    After:
    ```
    {"http":{"tests":{"inspector-protocol":{"runtime":{"serialization-dom-types.js":668}}}}}
    ```
    
    Bug: chromium:1478928
    Change-Id: I814d29fca5159b277e215a8566454a9010671c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876676
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198330}

--- third_party/blink/web_tests/http/tests/inspector-protocol/runtime/serialization-dom-types.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/runtime/serialization-dom-types.js
@@ -21,72 +21,90 @@
     `,
     'Tests DOM objects serialization');
 
-  await dp.Runtime.enable();
+  const ALL_TEST_LOGS = [];
 
-  // Node.
-  await testExpression("document.querySelector('body > div')")
-  // NodeList.
-  await testExpression("document.querySelector('body > div').childNodes")
-  // HTMLCollection.
-  await testExpression("document.getElementsByTagName('div')")
-  await testExpression("window")
-  await testExpression("new URL('http://example.com')");
-  await testExpression("window.openShadowContainer")
-  await testExpression("window.closedShadowContainer")
-
-  testRunner.completeTest();
-
-  async function testExpression(expression) {
-    await test(expression);
-    await test(expression, { UNKNOWN_PARAMETER_NAME: 'SOME_VALUE' });
-    await test(expression, { maxNodeDepth: 'STRING_INSTEAD_OF_INT' });
-    await test(expression, { INVALID_PARAMETER_TYPE: {} });
-    await test(expression, { maxNodeDepth:0 });
-    await test(expression, { maxNodeDepth:1 });
-    await test(expression, { maxNodeDepth:99 });
-    await test(expression, {
-      maxNodeDepth: 99,
-      includeShadowTree: "none"
-    }
-    );
-    await test(expression, {
-      maxNodeDepth:99 ,
-      includeShadowTree:"open"
-    });
-    await test(expression, {
-      includeShadowTree: "all"
-    }    );
-    await test(expression, {
-      maxNodeDepth: 0,
-      includeShadowTree: "all"
-
-    }    );
-    await test(expression, {
-      maxNodeDepth: 1,
-      includeShadowTree: "all"
-
-    });
-    await test(expression, {
-      maxNodeDepth: 99,
-      includeShadowTree: "all"
-    });
+  function scheduleTest(expression, additionalParameters) {
+    ALL_TEST_LOGS.push(runTest(expression, additionalParameters));
   }
 
-  async function test(expression, additionalParameters) {
+  async function runTest(expression, additionalParameters) {
     const serializationOptions = {
       serialization: "deep",
-        additionalParameters
+      additionalParameters
     }
-    testRunner.log(`Testing '${expression}' with ${JSON.stringify(serializationOptions)}`);
 
     const evalResult = await dp.Runtime.evaluate({
       expression,
       serializationOptions
     })
-    testRunner.log(
-      evalResult?.result?.result?.deepSerializedValue ?? evalResult,
-      undefined,
-      TestRunner.extendStabilizeNames(['context']),
+
+    return [
+      `Testing '${expression}' with ${JSON.stringify(serializationOptions)}`,
+      evalResult?.result?.result?.deepSerializedValue ?? evalResult]
+  }
+
+  async function runTests() {
+    for await (const logs of ALL_TEST_LOGS) {
+      const [description, result] = logs;
+      testRunner.log(description);
+      testRunner.log(
+        result,
+        undefined,
+        TestRunner.extendStabilizeNames(['context']),
+      );
+    }
+  }
+
+  await dp.Runtime.enable();
+
+  // Node.
+  testExpression("document.querySelector('body > div')")
+  // NodeList.
+  testExpression("document.querySelector('body > div').childNodes")
+  // HTMLCollection.
+  testExpression("document.getElementsByTagName('div')")
+  testExpression("window")
+  testExpression("new URL('http://example.com')");
+  testExpression("window.openShadowContainer")
+  testExpression("window.closedShadowContainer")
+
+  await runTests();
+
+  testRunner.completeTest();
+
+  function testExpression(expression) {
+    scheduleTest(expression);
+    scheduleTest(expression, { UNKNOWN_PARAMETER_NAME: 'SOME_VALUE' });
+    scheduleTest(expression, { maxNodeDepth: 'STRING_INSTEAD_OF_INT' });
+    scheduleTest(expression, { INVALID_PARAMETER_TYPE: {} });
+    scheduleTest(expression, { maxNodeDepth:0 });
+    scheduleTest(expression, { maxNodeDepth:1 });
+    scheduleTest(expression, { maxNodeDepth:99 });
+    scheduleTest(expression, {
+      maxNodeDepth: 99,
+      includeShadowTree: "none"
+    }
     );
+    scheduleTest(expression, {
+      maxNodeDepth:99 ,
+      includeShadowTree:"open"
+    });
+    scheduleTest(expression, {
+      includeShadowTree: "all"
+    }    );
+    scheduleTest(expression, {
+      maxNodeDepth: 0,
+      includeShadowTree: "all"
+
+    }    );
+    scheduleTest(expression, {
+      maxNodeDepth: 1,
+      includeShadowTree: "all"
+
+    });
+    scheduleTest(expression, {
+      maxNodeDepth: 99,
+      includeShadowTree: "all"
+    });
   }
 })
