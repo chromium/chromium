88c728e51729538f7a230877e8ee5c2e7d065ae5
Ankush Singh <ankushkush@google.com>
2023-09-19 11:07:57 +0000

    [sync] Add field in SyncablePrefsMetadata for pref merge behavior
    This CL introduces a new field in SyncablePrefsMetadata to specify the
    merge behavior for each syncable preference.
    
    Currently there exist methods in PrefModelAssociatorClient which help
    check whether a pref is mergeable or not but they are not convenient to
    use. The syncable prefs database would later be the source of truth for
    this info.
    
    Bug: 1474375
    Change-Id: Icfe70884b56eaaa51047512617467800b52e1419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874056
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198290}

--- components/sync_preferences/syncable_prefs_database.h
+++ components/sync_preferences/syncable_prefs_database.h
@@ -14,15 +14,37 @@
 
 namespace sync_preferences {
 
+enum class MergeBehavior {
+  // The account value wins. This is the default behavior. Any pref update is
+  // applied to both - the local value as well as the account value.
+  kNone,
+  // For dictionary values, all entries in `account_value` and `local_value` are
+  // merged recursively. In case of a conflict, the entry from `account_value`
+  // wins. With a DualLayerUserPrefStore, pref updates are split between the
+  // account value and the local value and only the relevant updates are applied
+  // to each.
+  kMergeableDict,
+  // For list values, all entries of `account_value` come first, followed by all
+  // entries in the `local_value`. Any repeating entry in `local_value` is
+  // omitted. Any pref update overwrites both - the local value as well as the
+  // account value.
+  kMergeableListWithRewriteOnUpdate,
+  // A custom merge logic has been implemented for this pref.
+  kCustom
+};
+
 // This class represents the metadata corresponding to a syncable preference.
 class SyncablePrefMetadata {
  public:
-  constexpr SyncablePrefMetadata(int syncable_pref_id,
-                                 syncer::ModelType model_type,
-                                 bool is_history_opt_in_required)
+  constexpr SyncablePrefMetadata(
+      int syncable_pref_id,
+      syncer::ModelType model_type,
+      bool is_history_opt_in_required,
+      MergeBehavior merge_behavior = MergeBehavior::kNone)
       : syncable_pref_id_(syncable_pref_id),
         model_type_(model_type),
-        is_history_opt_in_required_(is_history_opt_in_required) {
+        is_history_opt_in_required_(is_history_opt_in_required),
+        merge_behaviour_(merge_behavior) {
     // TODO(crbug.com/1424774): Allow OS_* types only if IS_CHROMEOS_ASH is
     // true. This isn't the case now because of an outlier entry in
     // common_syncable_prefs_database.
@@ -45,10 +67,13 @@ class SyncablePrefMetadata {
     return is_history_opt_in_required_;
   }
 
+  MergeBehavior merge_behavior() const { return merge_behaviour_; }
+
  private:
   int syncable_pref_id_;
   syncer::ModelType model_type_;
   bool is_history_opt_in_required_;
+  MergeBehavior merge_behaviour_;
 };
 
 // This class provides an interface to define the list of syncable
@@ -74,6 +99,10 @@ class SyncablePrefsDatabase {
   // Returns true if `pref_name` is part of the allowlist of syncable
   // preferences.
   bool IsPreferenceSyncable(const std::string& pref_name) const;
+
+  // Return true if `pref_name` is a mergeable syncable preference.
+  // Note: `pref_name` must be syncable.
+  bool IsPreferenceMergeable(const std::string& pref_name) const;
 };
 
 }  // namespace sync_preferences
