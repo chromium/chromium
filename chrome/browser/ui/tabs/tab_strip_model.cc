fc8566af925288f7a1ae4db2dd6d3c5d74b37802
Wei4 Wang <wei4.wang@intel.com>
2023-09-19 01:02:10 +0000

    [TabStripModel] Remove useless codes for failing fast in the Lacros platform.
    The codes are added in [1] to fail fast if TabStripModel adds same
    observer twice in the Lacros platform. However, the bug is fixed and
    the codes are useless now. So this CL removes the useless codes.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4483421
    
    Bug: 1427990
    Change-Id: I072aad8b4565ba08e4f33f1fb0136b992b3985d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4870710
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1198124}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -313,10 +313,6 @@ void TabStripModel::SetTabStripUI(TabStripModelObserver* observer) {
 }
 
 void TabStripModel::AddObserver(TabStripModelObserver* observer) {
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  // TODO(crbug.com/1427990): Remove this after fixing the bug.
-  CHECK(!observers_.HasObserver(observer));
-#endif
   observers_.AddObserver(observer);
   observer->StartedObserving(TabStripModelObserver::ModelPasskey(), this);
 }
