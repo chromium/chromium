00943465811796515f074eb355f8ec4236f03b40
Ashley Prasad <ashleydp@google.com>
2023-09-19 21:15:24 +0000

    scanning: Convert file_type_select to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Replace mixinBehavior with I18nMixin.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run $DUT scanapp.* excluding (hardware dependent tests)
    Change-Id: I27767a2877e21481e3c4f854e0be7d2c8075e344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865723
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198607}

--- ash/webui/scanning/resources/file_type_select.ts
+++ ash/webui/scanning/resources/file_type_select.ts
@@ -5,8 +5,8 @@
 import './scan_settings_section.js';
 import './strings.m.js';
 
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
-import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './file_type_select.html.js';
 
@@ -15,18 +15,11 @@ import {getTemplate} from './file_type_select.html.js';
  * 'file-type-select' displays the available file types in a dropdown.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- */
-const FileTypeSelectElementBase =
-    mixinBehaviors([I18nBehavior], PolymerElement);
+const FileTypeSelectElementBase = I18nMixin(PolymerElement);
 
-/** @polymer */
 class FileTypeSelectElement extends FileTypeSelectElementBase {
   static get is() {
-    return 'file-type-select';
+    return 'file-type-select' as const;
   }
 
   static get template() {
@@ -35,15 +28,22 @@ class FileTypeSelectElement extends FileTypeSelectElementBase {
 
   static get properties() {
     return {
-      /** @type {boolean} */
       disabled: Boolean,
 
-      /** @type {string} */
       selectedFileType: {
         type: String,
         notify: true,
       },
     };
+  }
+
+  disabled: boolean;
+  selectedFileType: string;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [FileTypeSelectElement.is]: FileTypeSelectElement;
   }
 }
 
