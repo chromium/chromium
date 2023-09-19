c814fcdf9bac724ac71ee6a17467b2f1b9e0f1b0
Ashley Prasad <ashleydp@google.com>
2023-09-19 18:59:47 +0000

    scanning: Convert page_size_select to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    - Add TODO for SelectBehavior.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: Ifb42b9bd36c89cebceba8088f7b8a7b1e83e3631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4867150
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198529}

--- ash/webui/scanning/resources/page_size_select.ts
+++ ash/webui/scanning/resources/page_size_select.ts
@@ -6,7 +6,7 @@ import './scan_settings_section.js';
 import './strings.m.js';
 
 import {assert} from 'chrome://resources/ash/common/assert.js';
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
+import {I18nBehavior} from 'chrome://resources/ash/common/i18n_behavior.js';
 import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './page_size_select.html.js';
@@ -22,54 +22,42 @@ const DEFAULT_PAGE_SIZE = PageSize.kNaLetter;
  * 'page-size-select' displays the available page sizes in a dropdown.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- * @implements {SelectBehaviorInterface}
- */
+// TODO(b/300484132): Replace mixinBehavior with mixin implementation once a
+// Mixin version of SelectBehavior is available.
 const PageSizeSelectElementBase =
-    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement);
+    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement) as
+    {new (): PolymerElement & I18nBehavior & SelectBehaviorInterface};
 
-/** @polymer */
 class PageSizeSelectElement extends PageSizeSelectElementBase {
   static get is() {
-    return 'page-size-select';
+    return 'page-size-select' as const;
   }
 
   static get template() {
     return getTemplate();
   }
 
-  /**
-   * @param {number} index
-   * @return {string}
-   */
-  getOptionAtIndex(index) {
+  getOptionAtIndex(index: number): string {
     assert(index < this.options.length);
 
     return this.options[index].toString();
   }
 
-  /**
-   * @param {!PageSize} pageSize
-   * @return {string}
-   * @private
-   */
-  getPageSizeString_(pageSize) {
+  getPageSizeAsString(pageSize: PageSize): string {
     return getPageSizeString(pageSize);
   }
 
-  sortOptions() {
-    this.options.sort((a, b) => {
+  sortOptions(): void {
+    this.options.sort((a: PageSize, b: PageSize) => {
       return alphabeticalCompare(getPageSizeString(a), getPageSizeString(b));
     });
 
     // If the fit to scan area option exists, move it to the end of the page
     // sizes array.
-    const fitToScanAreaIndex = this.options.findIndex((pageSize) => {
-      return pageSize === PageSize.kMax;
-    });
+    const fitToScanAreaIndex =
+        this.options.findIndex((pageSize: PageSize): boolean => {
+          return pageSize === PageSize.kMax;
+        });
 
 
     if (fitToScanAreaIndex !== -1) {
@@ -77,12 +65,14 @@ class PageSizeSelectElement extends PageSizeSelectElementBase {
     }
   }
 
-  /**
-   * @param {!PageSize} option
-   * @return {boolean}
-   */
-  isDefaultOption(option) {
+  isDefaultOption(option: PageSize): boolean {
     return option === DEFAULT_PAGE_SIZE;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [PageSizeSelectElement.is]: PageSizeSelectElement;
   }
 }
 
