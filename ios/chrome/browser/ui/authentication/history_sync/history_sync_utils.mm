aae562bf3678a2d4b1fc7e1ceba957276fdf6611
Menghan YANG <myuu@google.com>
2023-09-19 11:29:29 +0000

    [iOS][M118] Create prefs to record declined History Sync prompts
    The history sync promo should not be shown too frequently to the users.
    In the flow for which history sync is optional, the opt-in screen should
    be skipped if any user declined it in the past 2 weeks, or twice in a
    row.
    
    This CL creates the related preferences and utils.
    
    (cherry picked from commit de62c80da23e837efb617618fc981315c340003f)
    
    Bug: 1450867
    Change-Id: I86aa52f07ffac3548343c2070dc931807b80170c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4853626
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1194857}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863433
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5993@{#514}
    Cr-Branched-From: 511350718e646be62331ae9d7213d10ec320d514-refs/heads/main@{#1192594}

---
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_utils.mm
@@ -0,0 +1,60 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/authentication/history_sync/history_sync_utils.h"
+
+#import "base/time/time.h"
+#import "components/pref_registry/pref_registry_syncable.h"
+#import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/ui/authentication/history_sync/pref_names.h"
+
+namespace {
+
+// Number of times the History Sync should be successively declined until the
+// Opt-In prompt is automatically dismissed.
+constexpr int kMaxSuccessiveDeclineCount = 2;
+
+// Delay before reshowing an optional History Sync Opt-In prompt to the user.
+constexpr base::TimeDelta kDelayBeforeReshowPrompt = base::Days(14);
+
+}  // namespace
+
+namespace history_sync {
+
+void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterTimePref(
+      history_sync_prefs::kHistorySyncLastDeclinedTimestamp, base::Time());
+  registry->RegisterIntegerPref(
+      history_sync_prefs::kHistorySyncSuccessiveDeclineCount, 0);
+}
+
+void ResetDeclinePrefs(PrefService* pref_service) {
+  pref_service->ClearPref(
+      history_sync_prefs::kHistorySyncLastDeclinedTimestamp);
+  pref_service->ClearPref(
+      history_sync_prefs::kHistorySyncSuccessiveDeclineCount);
+}
+
+void RecordDeclinePrefs(PrefService* pref_service) {
+  pref_service->SetTime(history_sync_prefs::kHistorySyncLastDeclinedTimestamp,
+                        base::Time::Now());
+  const int old_count = pref_service->GetInteger(
+      history_sync_prefs::kHistorySyncSuccessiveDeclineCount);
+  pref_service->SetInteger(
+      history_sync_prefs::kHistorySyncSuccessiveDeclineCount, old_count + 1);
+}
+
+bool IsDeclinedTooOften(PrefService* pref_service) {
+  const int decline_count = pref_service->GetInteger(
+      history_sync_prefs::kHistorySyncSuccessiveDeclineCount);
+  if (decline_count >= kMaxSuccessiveDeclineCount) {
+    return true;
+  }
+
+  const base::Time last_declined = pref_service->GetTime(
+      history_sync_prefs::kHistorySyncLastDeclinedTimestamp);
+  return (base::Time::Now() - last_declined) < kDelayBeforeReshowPrompt;
+}
+
+}  // namespace history_sync
---
+++ ios/chrome/browser/ui/authentication/history_sync/pref_names.h
@@ -0,0 +1,15 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_HISTORY_SYNC_PREF_NAMES_H_
+#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_HISTORY_SYNC_PREF_NAMES_H_
+
+namespace history_sync_prefs {
+
+extern const char kHistorySyncSuccessiveDeclineCount[];
+extern const char kHistorySyncLastDeclinedTimestamp[];
+
+}  // namespace history_sync_prefs
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_HISTORY_SYNC_PREF_NAMES_H_
