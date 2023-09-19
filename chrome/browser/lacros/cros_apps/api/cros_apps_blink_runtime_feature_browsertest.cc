c5d405b3636195de80364bba0fa108a0212da574
Jiewei Qian <qjw@chromium.org>
2023-09-19 03:59:51 +0000

    cros-apps: allow the browser to control chromeos.diagnostics exposure
    This CL marks BlinkExtensionChromeOSDiagnostics as a browser controlled
    blink runtime feature, and verifies that runtime feature state can
    control its exposure.
    
    Bug: b/294479032
    Change-Id: I900685237d89c3e85c9bd73cfd8ff2a1e25f7ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4855868
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198180}

--- chrome/browser/lacros/cros_apps/api/cros_apps_blink_runtime_feature_browsertest.cc
+++ chrome/browser/lacros/cros_apps/api/cros_apps_blink_runtime_feature_browsertest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/strings/stringprintf.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/navigation_handle.h"
@@ -44,22 +45,31 @@ class CrosAppsBlinkRuntimeFeatureBrowsertest
                                                /*num_of_navigations*/ 1);
   }
 
+  // This method checks the given identifier is defined in web_content's
+  // JavaScript.
+  [[nodiscard]] content::EvalJsResult IsIdentifierDefined(
+      content::WebContents* web_contents,
+      const char* identifier) {
+    return content::EvalJs(
+        web_contents,
+        base::StringPrintf("typeof %s !== 'undefined';", identifier));
+  }
+
  private:
   base::RepeatingCallback<void(content::NavigationHandle*)>
       on_ready_to_commit_navigation_ = base::DoNothing();
 };
 
 IN_PROC_BROWSER_TEST_F(CrosAppsBlinkRuntimeFeatureBrowsertest,
-                       DefaultDisabled) {
+                       DefaultDisabled_GlobalChromeOS) {
   auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
   NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
 
-  EXPECT_EQ(true, content::EvalJs(web_contents,
-                                  "typeof window.chromeos === 'undefined';"));
+  EXPECT_EQ(false, IsIdentifierDefined(web_contents, "window.chromeos"));
 }
 
 IN_PROC_BROWSER_TEST_F(CrosAppsBlinkRuntimeFeatureBrowsertest,
-                       SelectivelyEnabledByFeatureState) {
+                       SelectivelyEnabledByFeatureState_GlobalChromeOS) {
   content::WebContents* web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
 
@@ -71,13 +81,59 @@ IN_PROC_BROWSER_TEST_F(CrosAppsBlinkRuntimeFeatureBrowsertest,
             .SetBlinkExtensionChromeOSEnabled(true);
       }));
   NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
-  EXPECT_EQ(true, content::EvalJs(web_contents,
-                                  "typeof window.chromeos !== 'undefined';"));
+  EXPECT_EQ(true, IsIdentifierDefined(web_contents, "window.chromeos"));
 
   // Stop enabling the feature and navigate again. The global object isn't
   // exposed to the new page.
   SetReadyToCommitNavigationCallback(base::DoNothing());
   NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
-  EXPECT_EQ(true, content::EvalJs(web_contents,
-                                  "typeof window.chromeos === 'undefined';"));
+  EXPECT_EQ(false, IsIdentifierDefined(web_contents, "window.chromeos"));
+}
+
+IN_PROC_BROWSER_TEST_F(CrosAppsBlinkRuntimeFeatureBrowsertest,
+                       DefaultDisabled_ChromeOS_Diagnostics) {
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
+  EXPECT_EQ(false, IsIdentifierDefined(web_contents, "window.chromeos"));
+
+  // Enable diagnostic feature without the global chromeos shouldn't expose the
+  // feature.
+  SetReadyToCommitNavigationCallback(
+      base::BindRepeating([](content::NavigationHandle* navigation_handle) {
+        navigation_handle->GetMutableRuntimeFeatureStateContext()
+            .SetBlinkExtensionDiagnosticsEnabled(true);
+      }));
+  EXPECT_EQ(false, IsIdentifierDefined(web_contents, "window.chromeos"));
+}
+
+IN_PROC_BROWSER_TEST_F(CrosAppsBlinkRuntimeFeatureBrowsertest,
+                       SelectivelyEnabledByFeatureState_ChromeOS_Diagnostics) {
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  // Start enabling the feature and the feature attribute is exposed.
+  SetReadyToCommitNavigationCallback(
+      base::BindRepeating([](content::NavigationHandle* navigation_handle) {
+        navigation_handle->GetMutableRuntimeFeatureStateContext()
+            .SetBlinkExtensionChromeOSEnabled(true);
+        navigation_handle->GetMutableRuntimeFeatureStateContext()
+            .SetBlinkExtensionDiagnosticsEnabled(true);
+      }));
+  NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
+  EXPECT_EQ(true, IsIdentifierDefined(web_contents, "window.chromeos"));
+  EXPECT_EQ(true,
+            IsIdentifierDefined(web_contents, "window.chromeos.diagnostics"));
+
+  // Stop enabling the feature, but still enable the global object.
+  SetReadyToCommitNavigationCallback(
+      base::BindRepeating([](content::NavigationHandle* navigation_handle) {
+        navigation_handle->GetMutableRuntimeFeatureStateContext()
+            .SetBlinkExtensionChromeOSEnabled(true);
+      }));
+  NavigateAndWait(web_contents, embedded_test_server()->GetURL("/empty.html"));
+  EXPECT_EQ(true, IsIdentifierDefined(web_contents, "window.chromeos"));
+  EXPECT_EQ(false,
+            IsIdentifierDefined(web_contents, "window.chromeos.diagnostics"));
 }
