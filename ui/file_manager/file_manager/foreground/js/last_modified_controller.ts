408833456490d04cc87eabec097933fcb4aa54ec
Ben Reich <benreich@chromium.org>
2023-09-19 06:00:03 +0000

    [Files app] Convert last_modified_controller to TypeScript
    Bug: b/289003444
    Change-Id: Ic0771d65cc5175cf84a5c9848bb97b9c3aca590d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876333
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198201}

--- ui/file_manager/file_manager/foreground/js/last_modified_controller.ts
+++ ui/file_manager/file_manager/foreground/js/last_modified_controller.ts
@@ -11,26 +11,16 @@ import {FileTable} from './ui/file_table.js';
  * Controls last modified column in the file table.
  */
 export class LastModifiedController {
-  /**
-   * @param {!FileTable} fileTable File table UI.
-   * @param {!DirectoryModel} directoryModel Directory model.
-   */
-  constructor(fileTable, directoryModel) {
-    /** @private @const {!FileTable} */
-    this.fileTable_ = fileTable;
-
-    /** @private @const {!DirectoryModel} */
-    this.directoryModel_ = directoryModel;
-
+  constructor(
+      private fileTable_: FileTable, private directoryModel_: DirectoryModel) {
     this.directoryModel_.addEventListener(
         'scan-started', this.onScanStarted_.bind(this));
   }
 
   /**
    * Handles directory scan start.
-   * @private
    */
-  onScanStarted_() {
+  private onScanStarted_() {
     // If the current directory is Recent root, request FileTable to use
     // modificationByMeTime instead of modificationTime in last modified column.
     const useModificationByMeTime =
