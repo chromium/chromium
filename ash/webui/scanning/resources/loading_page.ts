c2ab65ef2d70cddd32eb4b46c991c870d1b02e74
Ashley Prasad <ashleydp@google.com>
2023-09-19 19:02:08 +0000

    scanning: Convert landing-page to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    - Replace mixinBehavior with I18nMixin
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: If4acfab8fdd4281e1281af40d3554c4e52109a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4865364
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198533}

--- ash/webui/scanning/resources/loading_page.ts
+++ ash/webui/scanning/resources/loading_page.ts
@@ -7,9 +7,9 @@ import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import 'chrome://resources/polymer/v3_0/paper-progress/paper-progress.js';
 import './strings.m.js';
 
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
-import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './loading_page.html.js';
 import {AppState} from './scanning_app_types.js';
@@ -19,17 +19,11 @@ import {AppState} from './scanning_app_types.js';
  * 'loading-page' is shown while searching for available scanners.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- */
-const LoadingPageElementBase = mixinBehaviors([I18nBehavior], PolymerElement);
+const LoadingPageElementBase = I18nMixin(PolymerElement);
 
-/** @polymer */
 class LoadingPageElement extends LoadingPageElementBase {
   static get is() {
-    return 'loading-page';
+    return 'loading-page' as const;
   }
 
   static get template() {
@@ -38,76 +32,79 @@ class LoadingPageElement extends LoadingPageElementBase {
 
   static get properties() {
     return {
-      /** @type {!AppState} */
       appState: {
         type: Number,
-        observer: 'onAppStateChange_',
+        observer: 'appStateChanged',
       },
 
-      /** @protected {boolean} */
-      isDarkModeEnabled_: {
+      isDarkModeEnabled: {
         type: Boolean,
       },
 
-      /** @protected {boolean} */
-      isJellyEnabled_: {
+      isJellyEnabled: {
         type: Boolean,
         value: () => {
           return loadTimeData.getBoolean('isJellyEnabledForScanningApp');
         },
       },
 
-      /** @private {boolean} */
-      noScannersAvailable_: {
+      noScannersAvailable: {
         type: Boolean,
         value: false,
       },
     };
   }
 
-
+  appState: AppState;
+  private isDarkModeEnabled: boolean;
+  private isJellyEnabled: boolean;
+  private noScannersAvailable: boolean;
 
   /**
    * Determines correct SVG for "no scanners" based on dark mode.
-   * @protected
-   * @return {string}
    */
-  getNoScannersSvgSrc_() {
-    return this.isDarkModeEnabled_ ? 'svg/no_scanners_dark.svg' :
-                                     'svg/no_scanners.svg';
+  private getNoScannersSvgSrc(): string {
+    return this.isDarkModeEnabled ? 'svg/no_scanners_dark.svg' :
+                                    'svg/no_scanners.svg';
   }
 
   /**
    * Determines correct SVG for "scanners loading" based on dark mode.
-   * @protected
-   * @return {string}
    */
-  getScannersLoadingSvgSrc_() {
-    return this.isDarkModeEnabled_ ? 'svg/scanners_loading_dark.svg' :
-                                     'svg/scanners_loading.svg';
+  private getScannersLoadingSvgSrc(): string {
+    return this.isDarkModeEnabled ? 'svg/scanners_loading_dark.svg' :
+                                    'svg/scanners_loading.svg';
   }
 
-  /** @private */
-  onAppStateChange_() {
-    this.noScannersAvailable_ = this.appState === AppState.NO_SCANNERS;
+  private appStateChanged(): void {
+    this.noScannersAvailable = this.appState === AppState.NO_SCANNERS;
   }
 
-  /** @private */
-  onRetryClick_() {
+  private onRetryClick(): void {
     this.dispatchEvent(
         new CustomEvent('retry-click', {bubbles: true, composed: true}));
   }
 
-  /** @private */
-  onLearnMoreClick_() {
+  private onLearnMoreClick(): void {
     this.dispatchEvent(
         new CustomEvent('learn-more-click', {bubbles: true, composed: true}));
   }
 
-  /** @param {boolean} enabled */
-  setIsJellyEnabledForTesting(enabled) {
-    this.isJellyEnabled_ = enabled;
+  setIsJellyEnabledForTesting(enabled: boolean): void {
+    this.isJellyEnabled = enabled;
   }
 }
+
+declare global {
+  interface HTMLElementEventMap {
+    'learn-more-click': CustomEvent<void>;
+    'retry-click': CustomEvent<void>;
+  }
+
+  interface HTMLElementTagNameMap {
+    [LoadingPageElement.is]: LoadingPageElement;
+  }
+}
+
 
 customElements.define(LoadingPageElement.is, LoadingPageElement);
