36274b780dadf43ed863f999e137a34a4353974f
Gang Wu <gangwu@chromium.org>
2023-09-19 16:54:05 +0000

    Enable AndroidAppIntegration by default
    Bug:1445112
    
    Change-Id: I3bec7bafa23799df110c69d13492f618e51b9c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875457
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198439}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -469,7 +469,7 @@ BASE_FEATURE(kFocusOmniboxInIncognitoTabIntents,
 
 BASE_FEATURE(kAndroidAppIntegration,
              "AndroidAppIntegration",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidAppIntegrationSafeSearch,
              "AndroidAppIntegrationSafeSearch",
