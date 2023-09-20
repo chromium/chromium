c31471efdb960c6c452510eb16cbc9a1a9cc4e1d
Ernesto Izquierdo Clua <eic@google.com>
2023-09-20 14:44:23 +0000

    [ios][auth-on-entry2] Passwords in Other Apps Auth on Entry
    Bug: 1462419
    Change-Id: Ifbda1a273518ac0de9cd72731536f4244bb7bfa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873675
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198989}

--- ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_coordinator.mm
@@ -5,16 +5,24 @@
 #import "ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_coordinator.h"
 
 #import "base/check.h"
+#import "ios/chrome/browser/ui/settings/password/password_manager_ui_features.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_mediator.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_in_other_apps/passwords_in_other_apps_view_controller.h"
+#import "ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_coordinator.h"
 
-@interface PasswordsInOtherAppsCoordinator () <PasswordsInOtherAppsPresenter>
+@interface PasswordsInOtherAppsCoordinator () <
+    PasswordsInOtherAppsPresenter,
+    ReauthenticationCoordinatorDelegate>
 
 // Main mediator for this coordinator.
 @property(nonatomic, strong) PasswordsInOtherAppsMediator* mediator;
 
 // Main view controller for this coordinator.
 @property(nonatomic, strong) PasswordsInOtherAppsViewController* viewController;
+
+// Used for requiring Local Authentication when the app is
+// backgrounded/foregrounded with Password in Other Apps opened.
+@property(nonatomic, strong) ReauthenticationCoordinator* reauthCoordinator;
 
 @end
 
@@ -48,17 +56,51 @@
 
   [self.baseNavigationController pushViewController:self.viewController
                                            animated:YES];
+
+  if (password_manager::features::IsAuthOnEntryV2Enabled()) {
+    [self startReauthCoordinator];
+  }
 }
 
 - (void)stop {
   self.mediator = nil;
   self.viewController = nil;
+  [_reauthCoordinator stop];
+  _reauthCoordinator.delegate = nil;
+  _reauthCoordinator = nil;
 }
 
 #pragma mark - PasswordsInOtherAppsPresenter
 
 - (void)passwordsInOtherAppsViewControllerDidDismiss {
   [self.delegate passwordsInOtherAppsCoordinatorDidRemove:self];
+}
+
+#pragma mark - ReauthenticationCoordinatorDelegate
+
+- (void)successfulReauthenticationWithCoordinator:
+    (ReauthenticationCoordinator*)coordinator {
+  // No-op.
+}
+
+- (void)willPushReauthenticationViewController {
+  // No-op.
+}
+
+#pragma mark - Private
+
+// Starts reauthCoordinator. Once started
+// reauthCoordinator observes scene state changes and requires authentication
+// when the scene is backgrounded and then foregrounded while Password in Other
+// Apps is opened.
+- (void)startReauthCoordinator {
+  _reauthCoordinator = [[ReauthenticationCoordinator alloc]
+      initWithBaseNavigationController:_baseNavigationController
+                               browser:self.browser
+                reauthenticationModule:nil
+                           authOnStart:NO];
+  _reauthCoordinator.delegate = self;
+  [_reauthCoordinator start];
 }
 
 @end
