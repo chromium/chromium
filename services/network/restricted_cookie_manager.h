f38b08c7ba545970cca822f954d4108638a01f3c
Clark DuVall <cduvall@chromium.org>
2023-09-20 23:52:44 +0000

    Revert "Add a high priority queue for sync cookie calls in the network service"
    This reverts commit a922565736a5f5f691c3596215fe9e6cdc365940.
    
    Reason for revert: This did not show any improvement.
    
    Original change's description:
    > Add a high priority queue for sync cookie calls in the network service
    >
    > This change adds a minimal scheduler for the network service thread
    > which has a default task queue and high priority task queue. The
    > RestrictedCookieManager interface will be bound using the high priority
    > task queue, which should make the sync cookie accesses/writes from the
    > renderer faster if there are other tasks that need to be run in the
    > network service.
    >
    > The hope is that re-ordering these tasks with other network tasks will
    > not be a problem since they are generally called as sync calls from the
    > renderer. If there turns out to be problems with this later we can
    > revisit how tasks are prioritized.
    >
    > Bug: 1448685
    > Change-Id: I42f7f4e895a0b7d8708c49db707aab6286ef18bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4561626
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: Scott Haseley <shaseley@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1149519}
    
    Bug: 1448685
    Change-Id: I23d627113a8697bc14521d4673588c226b97c573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880044
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199314}

--- services/network/restricted_cookie_manager.h
+++ services/network/restricted_cookie_manager.h
@@ -162,7 +162,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) RestrictedCookieManager
   // |pending_receiver|.
   void InstallReceiver(
       mojo::PendingReceiver<mojom::RestrictedCookieManager> pending_receiver,
-      scoped_refptr<base::SequencedTaskRunner> task_runner,
       base::OnceClosure on_disconnect_callback);
 
   // Computes the First-Party Set metadata corresponding to the given `origin`,
