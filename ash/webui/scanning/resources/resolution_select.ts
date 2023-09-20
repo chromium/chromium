caa070b0185dd44e98f1e21df8f769861a76e639
Ashley Prasad <ashleydp@google.com>
2023-09-20 00:40:56 +0000

    scanning: Convert resolution_select to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    - Add TODO for SelectBehavior.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: I4ee00563b4b17c357bdd2fc5f53415858cb4d939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866681
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198714}

--- ash/webui/scanning/resources/resolution_select.ts
+++ ash/webui/scanning/resources/resolution_select.ts
@@ -6,71 +6,59 @@ import './scan_settings_section.js';
 import './strings.m.js';
 
 import {assert} from 'chrome://resources/ash/common/assert.js';
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
+import {I18nBehavior} from 'chrome://resources/ash/common/i18n_behavior.js';
 import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './resolution_select.html.js';
 import {SelectBehavior, SelectBehaviorInterface} from './select_behavior.js';
 
-/** @type {number} */
-const DEFAULT_RESOLUTION = 300;
+const DEFAULT_RESOLUTION: number = 300;
 
 /**
  * @fileoverview
  * 'resolution-select' displays the available scan resolutions in a dropdown.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- * @implements {SelectBehaviorInterface}
- */
+// TODO(b/300484132): Replace mixinBehavior with mixin implementation once a
+// Mixin version of SelectBehavior is available.
 const ResolutionSelectElementBase =
-    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement);
+    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement) as
+    {new (): PolymerElement & I18nBehavior & SelectBehaviorInterface};
 
-/** @polymer */
 class ResolutionSelectElement extends ResolutionSelectElementBase {
   static get is() {
-    return 'resolution-select';
+    return 'resolution-select' as const;
   }
 
   static get template() {
     return getTemplate();
   }
 
-  /**
-   * @param {number} index
-   * @return {string}
-   */
-  getOptionAtIndex(index) {
+  getOptionAtIndex(index: number): string {
     assert(index < this.options.length);
 
     return this.options[index].toString();
   }
 
-  /**
-   * @param {number} resolution
-   * @return {string}
-   * @private
-   */
-  getResolutionString_(resolution) {
+  getResolutionString(resolution: number): string {
     return this.i18n('resolutionOptionText', resolution);
   }
 
-  sortOptions() {
+  sortOptions(): void {
     // Sort the resolutions in descending order.
-    this.options.sort(function(a, b) {
+    this.options.sort(function(a: number, b: number): number {
       return b - a;
     });
   }
 
-  /**
-   * @param {number} option
-   * @return {boolean}
-   */
-  isDefaultOption(option) {
+  isDefaultOption(option: number): boolean {
     return option === DEFAULT_RESOLUTION;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ResolutionSelectElement.is]: ResolutionSelectElement;
   }
 }
 
