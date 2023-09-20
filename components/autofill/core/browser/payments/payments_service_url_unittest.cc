5e2fdceb0be65172f3c828acb24cbf2f8750cad1
Verina Armanyous <averina@google.com>
2023-09-20 17:14:12 +0000

    Update chrome settings link to gpay
    Bug: b/281595962
    Change-Id: I22ae62d0d87c73521c2ba90a00a37f030b5509e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4859302
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199082}

--- components/autofill/core/browser/payments/payments_service_url_unittest.cc
+++ components/autofill/core/browser/payments/payments_service_url_unittest.cc
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "components/autofill/core/browser/payments/payments_service_url.h"
+#include "base/command_line.h"
+#include "base/test/scoped_feature_list.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/autofill_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -32,6 +34,40 @@ TEST(PaymentsServiceSandboxUrl, CheckProdUrls) {
       "https://pay.google.com/payments/"
       "home?utm_source=chrome&utm_medium=settings&utm_campaign=payment-methods#"
       "paymentMethods";
+
+  EXPECT_EQ(kExpectedURL, GetManageInstrumentsUrl().spec());
+  EXPECT_EQ(kExpectedURL, GetManageAddressesUrl().spec());
+}
+
+TEST(PaymentsServiceSandboxUrl,
+     CheckSandboxUrls_UpdateChromeSettingLinkToGPayWebEnabled) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kWalletServiceUseSandbox, "1");
+  base::test::ScopedFeatureList feature_list_;
+  feature_list_.InitAndEnableFeature(
+      features::kAutofillUpdateChromeSettingsLinkToGPayWeb);
+
+  const char kExpectedURL[] =
+      "https://pay.sandbox.google.com/"
+      "pay?p=paymentmethods&utm_source=chrome&utm_medium=settings&utm_campaign="
+      "payment_methods";
+
+  EXPECT_EQ(kExpectedURL, GetManageInstrumentsUrl().spec());
+  EXPECT_EQ(kExpectedURL, GetManageAddressesUrl().spec());
+}
+
+TEST(PaymentsServiceSandboxUrl,
+     CheckProdUrls_UpdateChromeSettingLinkToGPayWebEnabled) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kWalletServiceUseSandbox, "0");
+  base::test::ScopedFeatureList feature_list_;
+  feature_list_.InitAndEnableFeature(
+      features::kAutofillUpdateChromeSettingsLinkToGPayWeb);
+
+  const char kExpectedURL[] =
+      "https://pay.google.com/"
+      "pay?p=paymentmethods&utm_source=chrome&utm_medium=settings&utm_campaign="
+      "payment_methods";
 
   EXPECT_EQ(kExpectedURL, GetManageInstrumentsUrl().spec());
   EXPECT_EQ(kExpectedURL, GetManageAddressesUrl().spec());
