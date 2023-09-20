2f9944acba44463462f29dbcd405643a8a8f8dab
sisidovski <sisidovski@chromium.org>
2023-09-20 07:38:34 +0000

    Add tests to confirm the network error in ServiceWorkerAutoPreload
    This CL adds a unit test for the network error case in
    ServiceWorkerAutoPreload.  We had a crash bug due to the unexpected
    state transition from State::kResponseReceived to State::kCompleted.
    This was fixed in [1], but the test was not added. The new unit test in
    this CL confirms the transition works as intended.
    
    This transition could happen when below conditions are all met:
    1) SWAutoPreload is enabled.
    2) The fetch handler result is fallback.
    3) The initial response is successfully received.
    4) The response is not transered into the data pipe yet for teeing.
    5) The response is completed with the network error.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4835496
    
    Bug: 1480081, 1478382
    Change-Id: Ibb844a47a1484a15a8ecd195d933aa5c5182ab0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876506
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198845}

--- content/common/service_worker/race_network_request_url_loader_client_unittest.cc
+++ content/common/service_worker/race_network_request_url_loader_client_unittest.cc
@@ -236,8 +236,8 @@ class ServiceWorkerRaceNetworkRequestURLLoaderClientTest
   }
 
   // Tells |clients_| to the completion of the response.
-  void CompleteResponse() {
-    const network::URLLoaderCompletionStatus status;
+  void CompleteResponse(const net::Error& error_code) {
+    const network::URLLoaderCompletionStatus status(error_code);
     client_->OnComplete(status);
   }
 
@@ -273,6 +273,10 @@ class ServiceWorkerRaceNetworkRequestURLLoaderClientTest
 
   void SetOnCompletedCallback(OnCompletedCallback callback) {
     owner_->SetOnCompletedCallback(std::move(callback));
+  }
+
+  ServiceWorkerRaceNetworkRequestURLLoaderClient::State client_state() {
+    return client_->state();
   }
 
  private:
@@ -313,7 +317,7 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest, Basic) {
         task_runner->PostTask(FROM_HERE, std::move(done));
       },
       run_loop.QuitClosure(), base::SequencedTaskRunner::GetCurrentDefault()));
-  CompleteResponse();
+  CompleteResponse(net::OK);
   run_loop.Run();
 
   // Check the response for fetch handler
@@ -341,7 +345,7 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
   SetOnCompletedCallback(base::BindOnce([](int error_code, const char* reason) {
     EXPECT_EQ(error_code, net::OK);
   }));
-  CompleteResponse();
+  CompleteResponse(net::OK);
 
   // Waiting for the first data chunk is received. The first chunk is the
   // fragment of the expected input, which is splitted per the data pipe size.
@@ -394,7 +398,7 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
   SetOnCompletedCallback(base::BindOnce([](int error_code, const char* reason) {
     EXPECT_EQ(error_code, net::OK);
   }));
-  CompleteResponse();
+  CompleteResponse(net::OK);
 
   // Waiting for the first data chunk is received. The first chunk is the
   // fragment of the expected input, which is splitted per the data pipe size.
@@ -443,7 +447,7 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
   SetOnCompletedCallback(base::BindOnce([](int error_code, const char* reason) {
     EXPECT_EQ(error_code, net::OK);
   }));
-  CompleteResponse();
+  CompleteResponse(net::OK);
 
   // Waiting for the first data chunk is received.
   RunUntilStateChange(/*resume_state=*/false);
@@ -481,7 +485,7 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
   SetOnCompletedCallback(base::BindOnce([](int error_code, const char* reason) {
     EXPECT_EQ(error_code, net::OK);
   }));
-  CompleteResponse();
+  CompleteResponse(net::OK);
 
   // Waiting for the first data chunk is received.
   client_for_fetch_handler()->RunUntilStateChange(/*resume_state=*/false);
@@ -496,5 +500,37 @@ TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
   // data pipe is also closed.
   ConsumeChunk();
   EXPECT_TRUE(IsDisconnected());
+}
+
+TEST_F(ServiceWorkerRaceNetworkRequestURLLoaderClientTest,
+       NetworkError_AfterInitialResponse) {
+  const uint32_t data_pipe_capacity_num_bytes = 8;
+  SetUpURLLoaderClient(data_pipe_capacity_num_bytes);
+
+  const std::string kExpectedBody = "abcdefghijklmnopqrstu";
+  ASSERT_GT(kExpectedBody.size(), data_pipe_capacity_num_bytes);
+
+  // Set the callback for the commit completion.
+  SetOnCompletedCallback(base::BindOnce([](int error_code, const char* reason) {
+    EXPECT_EQ(error_code, net::ERR_FAILED);
+  }));
+
+  // |client_| receives the response and expect |state_| is changed to
+  // kResponseReceived.
+  WriteData(kExpectedBody);
+  EXPECT_EQ(
+      client_state(),
+      ServiceWorkerRaceNetworkRequestURLLoaderClient::State::kResponseReceived);
+
+  // Set kWithoutServiceWorker. This imitates the fetch handler fallback case.
+  owner()->SetCommitResponsibility(
+      ServiceWorkerRaceNetworkRequestURLLoaderClient::FetchResponseFrom::
+          kWithoutServiceWorker);
+
+  // |client_| suddenly receives the network error, and expect |state_| is
+  // changed to kCompleted directly from kResponseReceived.
+  CompleteResponse(net::ERR_FAILED);
+  EXPECT_EQ(client_state(),
+            ServiceWorkerRaceNetworkRequestURLLoaderClient::State::kCompleted);
 }
 }  // namespace content
