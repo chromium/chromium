bb2d422e2188f8a90e1398abe0280fd8c8f370ad
Salvador Guerrero <salg@google.com>
2023-09-20 22:37:40 +0000

    [CPA Reader mode] Re-enabled distillabillity service on Android
    This CL adds back the distillability service flag to Android. This is
    used by CPA reader mode to detect compatible pages. This flag was
    accidentally removed on
    https://chromium-review.googlesource.com/c/chromium/src/+/4821106
    
    It also adds a browser test to ensure the reader mode signal works as
    expected.
    
    Bug: 1485268, b/300980031
    Change-Id: Id55a719625dcd807a7fe6fceb79995db12edebec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4873251
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199275}

--- chrome/android/javatests/src/org/chromium/chrome/browser/segmentation_platform/ContextualPageActionControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/segmentation_platform/ContextualPageActionControllerTest.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.segmentation_platform;
 
 import androidx.test.filters.MediumTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,9 +18,11 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarButtonVariant;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
+import org.chromium.net.test.EmbeddedTestServer;
 
 @RunWith(BaseJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
@@ -28,12 +31,24 @@ public class ContextualPageActionControllerTest {
     private static final String CONTEXTUAL_PAGE_ACTION_DEFAULT_MODEL_HISTOGRAM =
             "SegmentationPlatform.ModelExecution.DefaultProvider.Status."
             + "ContextualPageActionPriceTracking";
+    private static final String CONTEXTUAL_PAGE_ACTION_SHOWN_BUTTON_HISTOGRAM =
+            "Android.AdaptiveToolbarButton.Variant.OnPageLoad";
+    private static final String TEST_PAGE = "/chrome/test/data/dom_distiller/simple_article.html";
 
     @Rule
     public Features.JUnitProcessor mFeaturesProcessor = new Features.JUnitProcessor();
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+
+    private EmbeddedTestServer mTestServer;
+    private String mReaderModePageUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestServer = mActivityTestRule.getTestServer();
+        mReaderModePageUrl = mTestServer.getURL(TEST_PAGE);
+    }
 
     @Test
     @MediumTest
@@ -52,5 +67,26 @@ public class ContextualPageActionControllerTest {
         mActivityTestRule.startMainActivityOnBlankPage();
 
         histogram.pollInstrumentationThreadUntilSatisfied();
+    }
+
+    @Test
+    @MediumTest
+    @EnableFeatures({ChromeFeatureList.CONTEXTUAL_PAGE_ACTIONS,
+            ChromeFeatureList.CONTEXTUAL_PAGE_ACTION_READER_MODE})
+    public void
+    testContextualPageModelExecution_OnReaderModePage() {
+        LibraryLoader.getInstance().ensureInitialized();
+
+        var histograms =
+                HistogramWatcher.newBuilder()
+                        .expectIntRecord(CONTEXTUAL_PAGE_ACTION_DEFAULT_MODEL_HISTOGRAM,
+                                /* value= kSuccess*/ 0)
+                        .expectIntRecord(CONTEXTUAL_PAGE_ACTION_SHOWN_BUTTON_HISTOGRAM, /* value= */
+                                AdaptiveToolbarButtonVariant.READER_MODE)
+                        .build();
+
+        mActivityTestRule.startMainActivityWithURL(mReaderModePageUrl);
+
+        histograms.pollInstrumentationThreadUntilSatisfied();
     }
 }
