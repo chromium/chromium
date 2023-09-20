1f28d7aecb0aa6862eed0aaad67678f633ba0a2d
Ashley Prasad <ashleydp@google.com>
2023-09-20 00:38:59 +0000

    scanning: Convert multi-page-checkbox to TS
    - Remove closure types.
    - Update property and function names to match TS style guide.
    - Use i18nMixin instead of behavior.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: I121cc5d9b6e7a5b5578e72743514ed20bc915778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4863674
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1198713}

--- ash/webui/scanning/resources/multi_page_checkbox.ts
+++ ash/webui/scanning/resources/multi_page_checkbox.ts
@@ -6,8 +6,8 @@ import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import './scan_settings_section.js';
 import './strings.m.js';
 
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
-import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './multi_page_checkbox.html.js';
 
@@ -16,18 +16,11 @@ import {getTemplate} from './multi_page_checkbox.html.js';
  * 'multi-page-checkbox' displays the checkbox for starting a multi-page scan.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- */
-const MultiPageCheckboxElementBase =
-    mixinBehaviors([I18nBehavior], PolymerElement);
+const MultiPageCheckboxElementBase = I18nMixin(PolymerElement);
 
-/** @polymer */
 class MultiPageCheckboxElement extends MultiPageCheckboxElementBase {
   static get is() {
-    return 'multi-page-checkbox';
+    return 'multi-page-checkbox' as const;
   }
 
   static get template() {
@@ -36,24 +29,30 @@ class MultiPageCheckboxElement extends MultiPageCheckboxElementBase {
 
   static get properties() {
     return {
-      /** {boolean} */
       multiPageScanChecked: {
         type: Boolean,
         notify: true,
       },
 
-      /** @type {boolean} */
       disabled: Boolean,
     };
   }
 
-  /** @private */
-  onCheckboxClick_() {
+  multiPageScanChecked: boolean;
+  disabled: boolean;
+
+  private onCheckboxClick(): void {
     if (this.disabled) {
       return;
     }
 
     this.multiPageScanChecked = !this.multiPageScanChecked;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [MultiPageCheckboxElement.is]: MultiPageCheckboxElement;
   }
 }
 
