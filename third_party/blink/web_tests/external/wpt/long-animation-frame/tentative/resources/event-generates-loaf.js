940d0d4ea3bc965aaaa85079b097362db60ac72c
Noam Rosenthal <nrosenthal@chromium.org>
2023-09-20 07:27:59 +0000

    LoAF: Add tests that we don't expose post-redirect script URLs
    Bug: 1392685
    Change-Id: I16ba6c7cdd2b8cfe0eb957700d7ce7ea3efb73d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874411
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1198842}

---
+++ third_party/blink/web_tests/external/wpt/long-animation-frame/tentative/resources/event-generates-loaf.js
@@ -0,0 +1,8 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/common/dummy.xml');
+xhr.addEventListener('load', () => {
+  const deadline = performance.now() + 360;
+  while (performance.now() < deadline) {
+  }
+});
+xhr.send();
