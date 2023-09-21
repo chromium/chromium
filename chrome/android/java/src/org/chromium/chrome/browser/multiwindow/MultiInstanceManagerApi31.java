cefb87f082acae9ed81d1dc6e7d26cd8fef4c664
Jinsuk Kim <jinsukkim@chromium.org>
2023-09-21 16:54:02 +0000

    [MultiInstance] Fix the assertion error
    Fixes a corner case where the assert is not valid. When Chrome
    Window is switched to another one via menu, with window-task
    mapping still present but the corresponding Activity having been
    already killed, the assert was triggered, causing confusion.
    Now that we know this is a valid case, the assert statement may
    be removed.
    
    Bug: 1481331
    Change-Id: I1b6ee26ee59c1161481353a74c8282d8ff7507c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4876456
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199673}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -238,12 +238,12 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
     public int allocInstanceId(int windowId, int taskId, boolean preferNew) {
         removeInvalidInstanceData();
 
+        int instanceId = getInstanceByTask(taskId);
+
         // Explicitly specified window ID should be preferred. This comes from user selecting
-        // a certain instance on UI. This method would never be called if there were an instance
-        // already mapped to the task. Check it with an assert. When out of range, ignore the ID
-        // and apply the normal allocation logic below.
-        if (windowId >= 0 && windowId < mMaxInstances) {
-            assert getInstanceByTask(taskId) == INVALID_INSTANCE_ID;
+        // a certain instance on UI when no task is present for it.
+        // When out of range, ignore the ID and apply the normal allocation logic below.
+        if (windowId >= 0 && windowId < mMaxInstances && instanceId == INVALID_INSTANCE_ID) {
             Log.i(TAG_MULTI_INSTANCE, "Existing Instance - selected Id allocated: " + windowId);
             return windowId;
         }
@@ -251,7 +251,6 @@ class MultiInstanceManagerApi31 extends MultiInstanceManager implements Activity
         // First, see if we have instance-task ID mapping. If we do, use the instance id. This
         // takes care of a task that had its activity destroyed and comes back to create a
         // new one. We pair them again.
-        int instanceId = getInstanceByTask(taskId);
         if (instanceId != INVALID_INSTANCE_ID) {
             Log.i(TAG_MULTI_INSTANCE, "Existing Instance - mapped Id allocated: " + instanceId);
             return instanceId;
