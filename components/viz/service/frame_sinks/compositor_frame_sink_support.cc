756cb6ffdf8c9862e5dff57116859b066094287b
Jonathan Ross <jonross@chromium.org>
2023-09-21 21:29:48 +0000

    Reland "Remove separate kOnBeginFrameAllowLateAcks"
    This reverts commit 165aedb8ba97a7e1c0ba7a9554e5acdbc6ebcc45.
    
    This relands https://chromium-review.googlesource.com/c/chromium/src/+/4867009
    which was reverted when a newer test failed:
    OnBeginFrameAcksSurfaceSynchronizationTest.FallbackSurfacesClosed/BeginFrameAcks_AutoNeedsBeginFrame
    
    This test was added in parallel in:
    https://chromium-review.googlesource.com/c/chromium/src/+/4867009
    
    We need to reland the two, with the test updated to match the expectation
    changes caused by this feature
    
    Bug: 1399880
    Change-Id: I26ba4c17b1174c765740be51d829670b35e07ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880182
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199844}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -876,8 +876,7 @@ void CompositorFrameSinkSupport::DidReceiveCompositorFrameAck() {
   // send a separate Ack, so they can unthrottle and begin frame production.
   if (ShouldMergeBeginFrameWithAcks() &&
       !was_pending_manual_begin_frame_source_ &&
-      (!base::FeatureList::IsEnabled(features::kOnBeginFrameAllowLateAcks) ||
-       !ack_pending_during_on_begin_frame_)) {
+      !ack_pending_during_on_begin_frame_) {
     ack_queued_for_client_count_++;
     UpdateNeedsBeginFramesInternal();
     return;
