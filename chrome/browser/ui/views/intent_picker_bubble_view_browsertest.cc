3025fb792cf04b6529fc792b1e2030e4f7f23751
Frank Liberato <liberato@chromium.org>
2023-09-21 00:25:41 +0000

    [Gardener] Turn off some flaky IntentPickerIconBrowserTests on Mac
    Bug: 1478654
    Change-Id: I715ddf5da8e8ffc32c6721dfef3a4e6b53e863cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880619
    Owners-Override: Frank Liberato <liberato@google.com>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199334}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -265,8 +265,16 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
 // Test that navigating to service pages (chrome://) will hide the intent picker
 // icon.
+// TODO(crbug.com/1478654): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_DoNotShowIconAndBubbleOnServicePages \
+  DISABLED_DoNotShowIconAndBubbleOnServicePages
+#else
+#define MAYBE_DoNotShowIconAndBubbleOnServicePages \
+  DoNotShowIconAndBubbleOnServicePages
+#endif
 IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
-                       DoNotShowIconAndBubbleOnServicePages) {
+                       MAYBE_DoNotShowIconAndBubbleOnServicePages) {
   InstallTestWebApp();
 
   const GURL in_scope_url =
@@ -356,7 +364,13 @@ IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest, PushStateURLChangeTest) {
   EXPECT_FALSE(intent_picker_view->GetVisible());
 }
 
-IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest, OpenBubbleOnClick) {
+// TODO(crbug.com/1478654): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_OpenBubbleOnClick DISABLED_OpenBubbleOnClick
+#else
+#define MAYBE_OpenBubbleOnClick OpenBubbleOnClick
+#endif
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest, MAYBE_OpenBubbleOnClick) {
   InstallTestWebApp();
   views::NamedWidgetShownWaiter waiter(views::test::AnyWidgetTestPasskey{},
                                        IntentPickerBubbleView::kViewClassName);
