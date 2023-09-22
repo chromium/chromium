f35839cfc9f12a246fdb9415d2cf228eb0b93e54
Riley Tatum <rtatum@google.com>
2023-09-22 17:20:46 +0000

    Wallpaper search simple form with styling
    * Copied over the input and button from wallpaper-search-simple for now
      while there are more design decisions being discussed. That way result
      functionality can be started.
    * Followed the designs for the submit/search button, unlike
      wallpaper-search-simple.
    
    Screenshot: http://screenshot/7BJUjLSGtF7UAh7
    
    Bug: b/300970975
    Change-Id: I3912ddbfaa186f999a0d041480bad3943bbba7a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885232
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1200315}

--- chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
+++ chrome/browser/resources/side_panel/customize_chrome/wallpaper_search.ts
@@ -4,15 +4,22 @@
 
 import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
 import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_shared_style.css.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 
 import {SpHeading} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
+import {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {CustomizeChromeApiProxy} from './customize_chrome_api_proxy.js';
 import {getTemplate} from './wallpaper_search.html.js';
 
 export interface WallpaperSearchElement {
   $: {
     heading: SpHeading,
+    queryInput: CrInputElement,
+    submitButton: CrButtonElement,
   };
 }
 
@@ -26,8 +33,12 @@ export class WallpaperSearchElement extends PolymerElement {
   }
 
   static get properties() {
-    return {};
+    return {
+      query_: String,
+    };
   }
+
+  private query_: string;
 
   focusOnBackButton() {
     this.$.heading.getBackButton().focus();
@@ -35,6 +46,14 @@ export class WallpaperSearchElement extends PolymerElement {
 
   private async onBackClick_() {
     this.dispatchEvent(new Event('back-click'));
+  }
+
+  private async onSearchClick_() {
+    const {success} =
+        await CustomizeChromeApiProxy.getInstance().handler.searchWallpaper(
+            this.query_);
+    this.$.queryInput.invalid = !success;
+    this.$.queryInput.errorMessage = 'Error';
   }
 }
 
--- chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/wallpaper_search_test.ts
@@ -2,17 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://webui-test/mojo_webui_test_support.js';
 import 'chrome://customize-chrome-side-panel.top-chrome/wallpaper_search.js';
 
+import {CustomizeChromePageCallbackRouter, CustomizeChromePageHandlerRemote} from 'chrome://customize-chrome-side-panel.top-chrome/customize_chrome.mojom-webui.js';
+import {CustomizeChromeApiProxy} from 'chrome://customize-chrome-side-panel.top-chrome/customize_chrome_api_proxy.js';
 import {WallpaperSearchElement} from 'chrome://customize-chrome-side-panel.top-chrome/wallpaper_search.js';
-import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {TestMock} from 'chrome://webui-test/test_mock.js';
 import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
+import {installMock} from './test_support.js';
+
 suite('WallpaperSearchTest', () => {
+  let handler: TestMock<CustomizeChromePageHandlerRemote>;
   let wallpaperSearchElement: WallpaperSearchElement;
 
   setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    handler = installMock(
+        CustomizeChromePageHandlerRemote,
+        (mock: CustomizeChromePageHandlerRemote) =>
+            CustomizeChromeApiProxy.setInstance(
+                mock, new CustomizeChromePageCallbackRouter()));
     wallpaperSearchElement =
         document.createElement('customize-chrome-wallpaper-search');
     document.body.appendChild(wallpaperSearchElement);
@@ -27,5 +39,21 @@ suite('WallpaperSearchTest', () => {
     wallpaperSearchElement.$.heading.getBackButton().click();
     const event = await eventPromise;
     assertTrue(!!event);
+  });
+
+  test('clicking search invokes backend', () => {
+    wallpaperSearchElement.$.queryInput.value = 'foo';
+    wallpaperSearchElement.$.submitButton.click();
+    assertEquals(1, handler.getCallCount('searchWallpaper'));
+    assertEquals('foo', handler.getArgs('searchWallpaper')[0]);
+  });
+
+  test('search failure shows error message', async () => {
+    const failurePromise = Promise.resolve({success: false});
+    handler.setResultFor('searchWallpaper', failurePromise);
+    wallpaperSearchElement.$.submitButton.click();
+    await failurePromise;
+    assertTrue(wallpaperSearchElement.$.queryInput.invalid);
+    assertEquals('Error', wallpaperSearchElement.$.queryInput.errorMessage);
   });
 });
