0f61b06bb71f967cb8dd4fa051d9dea795bdcf0d
Florian Leimgruber <fleimgruber@google.com>
2023-09-22 21:57:45 +0000

    Replace AutocompleteUnrecognizedFallbackEventLogger CHECK with if.
    See http://crbug.com/1483883#c2.
    
    (cherry picked from commit 089ce670026f3f8a98ae1c46535ce7c8ae4c1a9d)
    
    Fixed: 1483883
    Change-Id: Ic40b60016d962a62ae304efbaf71b8f74f028244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4872718
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1197816}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887738
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Owners-Override: Harry Souders <harrysouders@google.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Auto-Submit: Harry Souders <harrysouders@google.com>
    Cr-Commit-Position: refs/branch-heads/5938@{#1400}
    Cr-Branched-From: 2b50cb4bcc2318034581a816714d9535dc38966d-refs/heads/main@{#1181205}

--- components/autofill/core/browser/metrics/fallback_autocomplete_unrecognized_metrics.cc
+++ components/autofill/core/browser/metrics/fallback_autocomplete_unrecognized_metrics.cc
@@ -29,8 +29,12 @@ void AutocompleteUnrecognizedFallbackEventLogger::OnDidShowSuggestions() {
 }
 
 void AutocompleteUnrecognizedFallbackEventLogger::OnDidFillSuggestion() {
-  CHECK_NE(suggestion_state_, SuggestionState::kNotShown);
-  if (suggestion_state_ != SuggestionState::kFilled) {
+  // Since the `AutocompleteUnrecognizedFallbackEventLogger` is only notified
+  // about autocomplete=unrecognized fields, it is possible that to reach
+  // `OnDidFillSuggestion()` with `suggestion_state_` kNotShown. This happens
+  // when the website dynamically changes the autocomplete attribute to
+  // unrecognized after triggering (regular) suggestions.
+  if (suggestion_state_ == SuggestionState::kShown) {
     suggestion_state_ = SuggestionState::kFilled;
   }
 }
