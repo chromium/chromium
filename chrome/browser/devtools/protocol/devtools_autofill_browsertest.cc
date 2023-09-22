9dbb370c4106be7b1736cfc958e657902d101712
Justin Lulejian <jlulejian@chromium.org>
2023-09-22 20:48:28 +0000

    [Gardener] Disable DevToolsAutofillTest.TriggerCreditCardInOOPIFIframe.
    Due to segfaults.
    
    Bug: 1486043
    Change-Id: Ie98578bf19a096e33277ea21c44469a2562e3f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885072
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Owners-Override: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200451}

--- chrome/browser/devtools/protocol/devtools_autofill_browsertest.cc
+++ chrome/browser/devtools/protocol/devtools_autofill_browsertest.cc
@@ -332,7 +332,9 @@ IN_PROC_BROWSER_TEST_F(DevToolsAutofillTest, TriggerCreditCardInIframe) {
   EXPECT_EQ(GetFilledOutForm(unique_context_id), GetTestCreditCard());
 }
 
-IN_PROC_BROWSER_TEST_F(DevToolsAutofillTest, TriggerCreditCardInOOPIFIframe) {
+// TODO(crbug.com/1486043): Disabled due to segfaults across platforms.
+IN_PROC_BROWSER_TEST_F(DevToolsAutofillTest,
+                       DISABLED_TriggerCreditCardInOOPIFIframe) {
   embedded_test_server()->ServeFilesFromSourceDirectory(
       "chrome/test/data/autofill");
   ASSERT_TRUE(embedded_test_server()->Start());
