f2066b34cc19c30f0bb7f6c464e46ed940963605
Ashley Prasad <ashleydp@google.com>
2023-09-22 00:15:12 +0000

    scanning: Convert scan-settings-section to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: I095318eddee85983e4a2fc8e76986f48d570150e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4874821
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199940}

--- ash/webui/scanning/resources/scan_settings_section.ts
+++ ash/webui/scanning/resources/scan_settings_section.ts
@@ -9,14 +9,19 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 
 import {getTemplate} from './scan_settings_section.html.js';
 
-/** @polymer */
 class ScanSettingsSectionElement extends PolymerElement {
   static get is() {
-    return 'scan-settings-section';
+    return 'scan-settings-section' as const;
   }
 
   static get template() {
     return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ScanSettingsSectionElement.is]: ScanSettingsSectionElement;
   }
 }
 
