dcaae4fedb6609763ed009630a961d6372185525
Rasika Navarange <rasikan@google.com>
2023-09-22 13:34:34 +0000

    Add Chrome test suites to perfetto_diff_tests target
    Add the ChromeArgs, ChromeMemorySnapshots, ChromeProcesses, ChromeRailModes, ChromeTouchGesture test suites to the perfetto_diff_tests target, so we can run these tests when developing the stdlib.
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests`
    
    Change-Id: I827a401a2315ef7c633ef05673c0e63d219e2d60
    Bug: b/300448047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4882666
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200197}

--- base/tracing/test/trace_processor/diff_tests/include_index.py
+++ base/tracing/test/trace_processor/diff_tests/include_index.py
@@ -7,9 +7,20 @@ from typing import List
 from python.generators.diff_tests import testing
 from chrome.tests import Chrome
 from chrome.tests_scroll_jank import ChromeScrollJank
+from chrome.tests_args import ChromeArgs
+from chrome.tests_memory_snapshots import ChromeMemorySnapshots
+from chrome.tests_processes import ChromeProcesses
+from chrome.tests_rail_modes import ChromeRailModes
+from chrome.tests_touch_gesture import ChromeTouchGesture
 
 def fetch_all_diff_tests(index_path: str) -> List['testing.TestCase']:
   return [
       *ChromeScrollJank(index_path, 'chrome', 'ChromeScrollJank').fetch(),
       *Chrome(index_path, 'chrome', 'Chrome').fetch(),
+      *ChromeArgs(index_path, 'chrome', 'ChromeArgs').fetch(),
+      *ChromeMemorySnapshots(index_path, 'chrome', 'ChromeMemorySnapshots')
+              .fetch(),
+      *ChromeProcesses(index_path, 'chrome', 'ChromeProcesses').fetch(),
+      *ChromeRailModes(index_path, 'chrome', 'ChromeRailModes').fetch(),
+      *ChromeTouchGesture(index_path, 'chrome', 'ChromeTouchGesture').fetch(),
       ]
