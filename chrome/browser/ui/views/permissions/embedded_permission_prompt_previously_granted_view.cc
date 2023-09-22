92568044f0e9d030cbb3c23ec9ea76dcecb28104
Kamila <hkamila@google.com>
2023-09-22 15:57:55 +0000

    [PEPC]Add previously granted view
    Adds view letting the user know that the permission prompt triggered through them clicking on the permission element was previously granted.
    
    The user is given the options to continue allowing or stop allowing the given permission.
    
    Bug: 1462930
    Change-Id: I7d47e7dd07d49c9b2fd2e5e6e0d1d0679883de46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875375
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1200256}

---
+++ chrome/browser/ui/views/permissions/embedded_permission_prompt_previously_granted_view.cc
@@ -0,0 +1,87 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/permissions/embedded_permission_prompt_previously_granted_view.h"
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/url_identity.h"
+#include "components/strings/grit/components_strings.h"
+#include "components/vector_icons/vector_icons.h"
+#include "ui/base/l10n/l10n_util.h"
+
+EmbeddedPermissionPromptPreviouslyGrantedView::
+    EmbeddedPermissionPromptPreviouslyGrantedView(
+        Browser* browser,
+        base::WeakPtr<permissions::PermissionPrompt::Delegate> delegate,
+        bool is_permission_allowed)
+    : EmbeddedPermissionPromptBaseView(browser, delegate) {}
+
+EmbeddedPermissionPromptPreviouslyGrantedView::
+    ~EmbeddedPermissionPromptPreviouslyGrantedView() = default;
+
+std::u16string
+EmbeddedPermissionPromptPreviouslyGrantedView::GetAccessibleWindowTitle()
+    const {
+  return GetMessageText();
+}
+
+std::u16string EmbeddedPermissionPromptPreviouslyGrantedView::GetWindowTitle()
+    const {
+  return std::u16string();
+}
+
+void EmbeddedPermissionPromptPreviouslyGrantedView::RunButtonCallback(
+    int button_id) {
+  if (!delegate()) {
+    return;
+  }
+
+  ButtonType button = GetButtonType(button_id);
+  if (button == ButtonType::kContinueAllowing) {
+    // The permission is already allowed.
+    return;
+  }
+
+  if (button == ButtonType::kStopAllowing) {
+    delegate()->Deny();
+  }
+}
+
+std::vector<
+    EmbeddedPermissionPromptPreviouslyGrantedView::RequestLineConfiguration>
+EmbeddedPermissionPromptPreviouslyGrantedView::GetRequestLinesConfiguration() {
+  return {{/*icon=*/nullptr, GetMessageText()}};
+}
+
+std::vector<EmbeddedPermissionPromptPreviouslyGrantedView::ButtonConfiguration>
+EmbeddedPermissionPromptPreviouslyGrantedView::GetButtonsConfiguration() {
+  std::vector<ButtonConfiguration> buttons;
+  buttons.emplace_back(
+      l10n_util::GetStringUTF16(IDS_EMBEDDED_PROMPT_CONTINUE_ALLOWING),
+      ButtonType::kContinueAllowing, ui::ButtonStyle::kTonal);
+
+  buttons.emplace_back(
+      l10n_util::GetStringUTF16(IDS_EMBEDDED_PROMPT_STOP_ALLOWING),
+      ButtonType::kStopAllowing, ui::ButtonStyle::kTonal);
+  return buttons;
+}
+
+std::u16string EmbeddedPermissionPromptPreviouslyGrantedView::GetMessageText()
+    const {
+  const auto& requests = delegate()->Requests();
+  CHECK_GT(requests.size(), 0U);
+
+  std::u16string permission_name;
+  if (requests.size() == 2) {
+    permission_name = l10n_util::GetStringUTF16(
+        IDS_CAMERA_AND_MICROPHONE_PERMISSION_NAME_FRAGMENT);
+  } else {
+    permission_name = requests[0]->GetPermissionNameTextFragment();
+  }
+
+  return l10n_util::GetStringFUTF16(IDS_EMBEDDED_PROMPT_PREVIOUSLY_ALLOWED,
+                                    permission_name,
+                                    GetUrlIdentityObject().name);
+}
