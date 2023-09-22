56991b95e4edfa5f8aeae8e71a1ff9353cedf259
David Bokan <bokan@chromium.org>
2023-09-22 20:00:03 +0000

    Revert "Add crash keys in subresource filter"
    This reverts commit a82cb60b76fc5f017a3833a3abc4451db09b271e.
    
    Reason for revert: Crash Keys no longer needed
    
    Original change's description:
    > Add crash keys in subresource filter
    >
    > The crash is occurring when activating page from the BFCache. We make a
    > call on the subresource filter throttle manager from the restoring page
    > but it's unexpectedly null.
    >
    > I can't tell how this can happen - every page should have a
    > ThrottleManager stored on it. The ThrottleManager is handed off to the
    > Page from the NavigationRequest at DidFinishNavigation so,
    > theoretically, there's a short (async) window when the Page doesn't have
    > a ThrottleManager set. However, AFAICT BFCache shouldn't ever store such
    > a page.
    >
    > This CL adds a few diagnostics in a crash key to help point further
    > debugging efforts.
    >
    > Bug: 1264667
    > Change-Id: I328150387d6c313d7e85d9ed32d08dd4457494ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257292
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#964654}
    
    Bug: 1264667
    Change-Id: Ic3de50e5ed1a004fe4c6956fb94c7c61a0214b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887807
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200410}

--- components/subresource_filter/content/browser/content_subresource_filter_web_contents_helper.h
+++ components/subresource_filter/content/browser/content_subresource_filter_web_contents_helper.h
@@ -5,14 +5,12 @@
 #ifndef COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_CONTENT_SUBRESOURCE_FILTER_WEB_CONTENTS_HELPER_H_
 #define COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_CONTENT_SUBRESOURCE_FILTER_WEB_CONTENTS_HELPER_H_
 
-#include <map>
 #include <set>
 
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/scoped_observation.h"
-#include "components/crash/core/common/crash_key.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer_manager.h"
 #include "components/subresource_filter/content/browser/verified_ruleset_dealer.h"
@@ -34,60 +32,6 @@ namespace subresource_filter {
 
 class ContentSubresourceFilterThrottleManager;
 class SubresourceFilterProfileContext;
-
-// TODO(bokan): Temporary to help debug crash in crbug.com/1264667.  Tracks
-// main frame RFHs and observes navigations/events relating to them, recording
-// some potentially interesting data that'll be stored as a crash key at the
-// crash site in
-// ContentSubresourceFilterWebContentsHelper::DidFinishNavigation.
-class DebugCrashWebContentsObserver : public content::WebContentsObserver {
-  struct CrashKeyData {
-    CrashKeyData();
-    ~CrashKeyData();
-    CrashKeyData(const CrashKeyData& other);
-
-    int num_commits = 0;
-
-    bool last_nav_had_manager = false;
-    bool last_nav_manager_moved_to_page = false;
-    bool entered_bf_cache = false;
-    bool is_bfcache_restore = false;
-
-    base::WeakPtr<content::Page> page_at_last_nav_finish;
-    base::WeakPtr<content::Page> page_at_bf_cache_entry;
-    base::WeakPtr<content::Page> page_at_restore;
-  };
-
- public:
-  explicit DebugCrashWebContentsObserver(content::WebContents* web_contents);
-  ~DebugCrashWebContentsObserver() override;
-
-  void RenderFrameCreated(content::RenderFrameHost* render_frame_host) override;
-  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
-
-  void ReadyToCommitNavigation(content::NavigationHandle* handle) override;
-  // Not an override since it relies on running after the WebContentsHelper's
-  // DidFinishNavigation.
-  void DidFinishNonActivatingNavigation(content::NavigationHandle* handle,
-                                        content::Page* page);
-
-  void RenderFrameHostStateChanged(
-      content::RenderFrameHost* render_frame_host,
-      content::RenderFrameHost::LifecycleState old_state,
-      content::RenderFrameHost::LifecycleState new_state) override;
-
-  // Not an override since it relies on running before the crash site in the
-  // WebContentsHelper's DidFinishNavigation.
-  void DidFinishPageActivatingNavigation(content::NavigationHandle* handle);
-  void GetScopedCrashKeyStrings(
-      content::RenderFrameHost* rfh,
-      std::vector<std::unique_ptr<crash_reporter::ScopedCrashKeyString>>& keys);
-
- private:
-  // Tracks useful data for each main frame RFH that's passed through
-  // ReadyToCommit/DidFinishNavigation.
-  std::map<content::RenderFrameHost*, CrashKeyData> rfh_crash_data_;
-};
 
 // This class manages the lifetime and storage of
 // ContentSubresourceFilterThrottleManager instances. This helper is attached
@@ -172,7 +116,6 @@ class ContentSubresourceFilterWebContentsHelper
       LoadPolicy load_policy) override;
 
  private:
-  DebugCrashWebContentsObserver debug_crash_observer_;
   raw_ptr<SubresourceFilterProfileContext, DanglingUntriaged> profile_context_;
 
   scoped_refptr<safe_browsing::SafeBrowsingDatabaseManager> database_manager_;
