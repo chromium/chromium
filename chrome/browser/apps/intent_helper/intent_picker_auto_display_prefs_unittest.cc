d5c0447d9f5d2a5d292f5e4ae0c75e54b34ad151
Tim Sergeant <tsergeant@chromium.org>
2023-09-22 01:47:06 +0000

    Intents: Clean up obsolete code in IntentPickerAutoDisplayPrefs
    These methods have been unused since the rollout of the new Link
    Capturing UI on CrOS in M108.
    
    Obsolete prefs values in storage will be cleared whenever the value for
    that site is updated (and also when doing Clear Browsing Data).
    
    Still TODO: Updating the name of this class to something more suitable
    for its remaining Intent Chip use-case.
    
    Bug: 1416434
    Change-Id: I96b4e3c97823c9f65236cd1f9c413288dffcf9cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885669
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1199987}

--- chrome/browser/apps/intent_helper/intent_picker_auto_display_prefs_unittest.cc
+++ chrome/browser/apps/intent_helper/intent_picker_auto_display_prefs_unittest.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/apps/intent_helper/intent_picker_auto_display_prefs.h"
 
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -20,92 +19,6 @@ class IntentPickerAutoDisplayPrefsTest : public testing::Test {
  protected:
   content::BrowserTaskEnvironment task_environment_;
 };
-
-TEST_F(IntentPickerAutoDisplayPrefsTest, GetPlatform) {
-  GURL url1("https://www.google.com/abcde");
-  GURL url2("https://www.google.com/hi");
-  GURL url3("https://www.boogle.com/a");
-
-  TestingProfile profile;
-
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url1));
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url2));
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url3));
-
-  // Update platform to a value and check value has updated.
-  PickerPrefs::UpdatePlatformForTablets(&profile, url1,
-                                        PickerPrefs::Platform::kArc);
-  EXPECT_EQ(PickerPrefs::Platform::kArc,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url1));
-  // Url with the same host should also have updated value.
-  EXPECT_EQ(PickerPrefs::Platform::kArc,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url2));
-
-  // Url with a different host should have original value.
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url3));
-
-  // Update platform and check value has updated.
-  PickerPrefs::UpdatePlatformForTablets(&profile, url2,
-                                        PickerPrefs::Platform::kChrome);
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url1));
-  // Url with the same host should also have updated value.
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url2));
-
-  // Url with a different host should have original value.
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url3));
-
-  // Update platform and check value has updated.
-  PickerPrefs::UpdatePlatformForTablets(&profile, url3,
-                                        PickerPrefs::Platform::kArc);
-  // Url with different hosts should have original value.
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url1));
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url2));
-
-  // Url value should be updated.
-  EXPECT_EQ(PickerPrefs::Platform::kArc,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url3));
-
-  PickerPrefs::UpdatePlatformForTablets(&profile, url3,
-                                        PickerPrefs::Platform::kNone);
-  // Url value should be updated.
-  EXPECT_EQ(PickerPrefs::Platform::kNone,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url3));
-  // Url with different hosts should have original value.
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url1));
-  EXPECT_EQ(PickerPrefs::Platform::kChrome,
-            PickerPrefs::GetLastUsedPlatformForTablets(&profile, url2));
-}
-
-// Checks that calling IncrementCounter twice for a particular URL causes
-// the UI to no longer be auto-displayed for that URL.
-TEST_F(IntentPickerAutoDisplayPrefsTest, ShouldAutoDisplayUi) {
-  GURL url1("https://www.google.com/abcde");
-  GURL url2("https://www.google.com/hi");
-  GURL url3("https://www.boogle.com/a");
-
-  TestingProfile profile;
-
-  EXPECT_TRUE(PickerPrefs::ShouldAutoDisplayUi(&profile, url1));
-  PickerPrefs::IncrementPickerUICounter(&profile, url1);
-  EXPECT_TRUE(PickerPrefs::ShouldAutoDisplayUi(&profile, url1));
-  PickerPrefs::IncrementPickerUICounter(&profile, url1);
-  EXPECT_FALSE(PickerPrefs::ShouldAutoDisplayUi(&profile, url1));
-
-  // Should return false for a different URL on the same host.
-  EXPECT_FALSE(PickerPrefs::ShouldAutoDisplayUi(&profile, url2));
-  // Should return true for a different host.
-  EXPECT_TRUE(PickerPrefs::ShouldAutoDisplayUi(&profile, url3));
-}
 
 TEST_F(IntentPickerAutoDisplayPrefsTest, ResetIntentChipCounter) {
   GURL url("https://www.google.com/abcde");
