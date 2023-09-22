d3f7c0bd3cdcac4b61415523634b6c8d0df22057
François Degros <fdegros@chromium.org>
2023-09-22 03:12:13 +0000

    Use ScopedObservation in GoogleDrivePageHandler
    BUG=b:301024841
    
    Change-Id: I6346d2da0c466fb72d5a7891c273facfe66f1166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885931
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200019}

--- chrome/browser/ui/webui/ash/settings/pages/files/google_drive_page_handler.h
+++ chrome/browser/ui/webui/ash/settings/pages/files/google_drive_page_handler.h
@@ -7,6 +7,7 @@
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/drive/drive_integration_service.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/files/mojom/google_drive_handler.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -20,7 +21,7 @@ namespace ash::settings {
 
 // ChromeOS "Google Drive" settings page UI handler.
 class GoogleDrivePageHandler : public google_drive::mojom::PageHandler,
-                               public drive::DriveIntegrationServiceObserver {
+                               drive::DriveIntegrationService::Observer {
  public:
   GoogleDrivePageHandler(
       mojo::PendingReceiver<google_drive::mojom::PageHandler> receiver,
@@ -39,7 +40,8 @@ class GoogleDrivePageHandler : public google_drive::mojom::PageHandler,
   void ClearPinnedFiles(ClearPinnedFilesCallback callback) override;
   void RecordBulkPinningEnabledMetric() override;
 
-  // drive::DriveIntegrationServiceObserver
+  // DriveIntegrationService::Observer implementation.
+  void OnDriveIntegrationServiceDestroyed() override;
   void OnBulkPinProgress(const drivefs::pinning::Progress& progress) override;
 
   void NotifyServiceUnavailable();
@@ -52,10 +54,14 @@ class GoogleDrivePageHandler : public google_drive::mojom::PageHandler,
 
   drivefs::pinning::PinManager* GetPinManager();
   drive::DriveIntegrationService* GetDriveService();
-  raw_ptr<Profile> profile_;
+  const raw_ptr<Profile> profile_;
 
   mojo::Remote<google_drive::mojom::Page> page_;
   mojo::Receiver<google_drive::mojom::PageHandler> receiver_{this};
+
+  base::ScopedObservation<drive::DriveIntegrationService,
+                          drive::DriveIntegrationService::Observer>
+      drive_observer_{this};
 
   base::WeakPtrFactory<GoogleDrivePageHandler> weak_ptr_factory_{this};
 };
