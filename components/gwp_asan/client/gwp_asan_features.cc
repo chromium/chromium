d9040052402ec13fa729fd9841ba94f6a8bbdc8c
Matthew Denton <mpdenton@chromium.org>
2023-09-22 21:01:45 +0000

    GWP-ASan on Lacros by default
    Change-Id: Id06b9b1ad2942fc42324b11993e587f86eb79423
    Bug: 1233008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4883575
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1200466}

--- components/gwp_asan/client/gwp_asan_features.cc
+++ components/gwp_asan/client/gwp_asan_features.cc
@@ -9,7 +9,7 @@
 namespace gwp_asan::internal {
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS_ASH)
+    BUILDFLAG(IS_CHROMEOS)
 constexpr base::FeatureState kDefaultEnabled = base::FEATURE_ENABLED_BY_DEFAULT;
 #else
 constexpr base::FeatureState kDefaultEnabled =
