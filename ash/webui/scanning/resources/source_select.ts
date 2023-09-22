ab0c7a7207bd89eb65c0134c6b7a3586e045ec83
Ashley Prasad <ashleydp@google.com>
2023-09-22 00:24:03 +0000

    scanning: Convert source_select to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    - Add TODO for select_behavior mixin.
    
    Bug: b:299520240
    Test: browser_tests --gtest_filter=*ScanningApp*
    Test: tast run -build=true ${DUT} scanapp.* (excluding hardware tests)
    Change-Id: Iecdbff8b4cf0c297705b2e270d05abd688fedc52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4880719
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1199947}

--- ash/webui/scanning/resources/source_select.ts
+++ ash/webui/scanning/resources/source_select.ts
@@ -6,7 +6,7 @@ import './scan_settings_section.js';
 import './strings.m.js';
 
 import {assert} from 'chrome://resources/ash/common/assert.js';
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
+import {I18nBehavior} from 'chrome://resources/ash/common/i18n_behavior.js';
 import {mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {ScanSource, SourceType} from './scanning.mojom-webui.js';
@@ -14,65 +14,53 @@ import {alphabeticalCompare, getSourceTypeString} from './scanning_app_util.js';
 import {SelectBehavior, SelectBehaviorInterface} from './select_behavior.js';
 import {getTemplate} from './source_select.html.js';
 
-/** @type {SourceType} */
-const DEFAULT_SOURCE_TYPE = SourceType.kFlatbed;
+const DEFAULT_SOURCE_TYPE: SourceType = SourceType.kFlatbed;
 
 /**
  * @fileoverview
  * 'source-select' displays the available scanner sources in a dropdown.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- * @implements {SelectBehaviorInterface}
- */
+// TODO(b/300484132): Replace mixinBehavior with mixin implementation once a
+// Mixin version of SelectBehavior is available.
 const SourceSelectElementBase =
-    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement);
+    mixinBehaviors([I18nBehavior, SelectBehavior], PolymerElement) as
+    {new (): PolymerElement & I18nBehavior & SelectBehaviorInterface};
 
-/** @polymer */
 class SourceSelectElement extends SourceSelectElementBase {
   static get is() {
-    return 'source-select';
+    return 'source-select' as const;
   }
 
   static get template() {
     return getTemplate();
   }
 
-  /**
-   * @param {number} index
-   * @return {string}
-   */
-  getOptionAtIndex(index) {
+  getOptionAtIndex(index: number): string {
     assert(index < this.options.length);
 
     return this.options[index].name;
   }
 
-  /**
-   * @param {SourceType} mojoSourceType
-   * @return {string}
-   * @private
-   */
-  getSourceTypeString_(mojoSourceType) {
+  private getSourceTypeAsString(mojoSourceType: SourceType): string {
     return getSourceTypeString(mojoSourceType);
   }
 
-  sortOptions() {
-    this.options.sort((a, b) => {
+  sortOptions(): void {
+    this.options.sort((a: ScanSource, b: ScanSource) => {
       return alphabeticalCompare(
           getSourceTypeString(a.type), getSourceTypeString(b.type));
     });
   }
 
-  /**
-   * @param {!ScanSource} option
-   * @return {boolean}
-   */
-  isDefaultOption(option) {
+  isDefaultOption(option: ScanSource): boolean {
     return option.type === DEFAULT_SOURCE_TYPE;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [SourceSelectElement.is]: SourceSelectElement;
   }
 }
 
