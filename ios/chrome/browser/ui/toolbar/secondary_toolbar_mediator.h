983beef7677d11061d78c15d9579b25bf0344dfc
Christian Xu <christianxu@google.com>
2023-09-26 14:09:11 +0000

    [iOS][Toolbar] Add SecondaryToolbarMediator
    Add SecondaryToolbarMediator and move
    SecondaryToolbarKeyboardStateProvider to it.
    
    Bug: 1462578
    Change-Id: I7c8b98cdde5cf595efcd9e46f52812b88db1dba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4889653
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201538}

---
+++ ios/chrome/browser/ui/toolbar/secondary_toolbar_mediator.h
@@ -0,0 +1,26 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/ui/toolbar/secondary_toolbar_keyboard_state_provider.h"
+
+class WebStateList;
+
+/// Mediator providing web state information to SecondaryToolbarViewController.
+@interface SecondaryToolbarMediator
+    : NSObject <SecondaryToolbarKeyboardStateProvider>
+
+/// Creates an instance of the mediator.
+- (instancetype)initWithWebStateList:(WebStateList*)webStateList;
+
+/// Disconnects web state list reference.
+- (void)disconnect;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_SECONDARY_TOOLBAR_MEDIATOR_H_
