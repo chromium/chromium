11065a962f6a15212ac1b021d19a0072c62108bc
Tony Herre <toprice@chromium.org>
2023-09-26 10:45:01 +0000

    Switch to AVCaptureDeviceTypeExternal in MacOS 14.0+
    Tested on local builds on MBPs with external USB cameras on both Ventura
    and on Sonoma built targeting 14.0.
    
    Bug: 1484830
    Change-Id: Ifc1dfa2bdf6c233096b135531bf7afe207fd315e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887175
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201442}

--- media/base/mac/video_capture_device_avfoundation_helpers.mm
+++ media/base/mac/video_capture_device_avfoundation_helpers.mm
@@ -4,23 +4,42 @@
 
 #include "media/base/mac/video_capture_device_avfoundation_helpers.h"
 
+#include "base/feature_list.h"
 #include "build/build_config.h"
 
 namespace media {
 
+BASE_FEATURE(kUseAVCaptureDeviceTypeExternal,
+             "UseAVCaptureDeviceTypeExternal",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 NSArray<AVCaptureDevice*>* GetVideoCaptureDevices() {
-  // Query for all camera device types available on apple platform. The
-  // others in the enum are only supported on iOS/iPadOS.
-  NSArray* captureDeviceType = @[
-    AVCaptureDeviceTypeBuiltInWideAngleCamera,
+  // Camera device types available on all apple platforms.
+  NSArray* captureDeviceTypes = @[ AVCaptureDeviceTypeBuiltInWideAngleCamera ];
+
 #if BUILDFLAG(IS_MAC)
-    AVCaptureDeviceTypeExternalUnknown
+  // MacOS has an additional 'external' device type we want to include.
+  // AVCaptureDeviceTypeExternal since 14.0, AVCaptureDeviceTypeExternalUnknown
+  // before. See crbug.com/1484830.
+  if (@available(macOS 14.0, *)) {
+    if (base::FeatureList::IsEnabled(kUseAVCaptureDeviceTypeExternal)) {
+      captureDeviceTypes =
+          [captureDeviceTypes arrayByAddingObject:AVCaptureDeviceTypeExternal];
+    } else {
+      // @available needs to be alone in an if statement, so we need to
+      // duplicate the else case here.
+      captureDeviceTypes = [captureDeviceTypes
+          arrayByAddingObject:AVCaptureDeviceTypeExternalUnknown];
+    }
+  } else {
+    captureDeviceTypes = [captureDeviceTypes
+        arrayByAddingObject:AVCaptureDeviceTypeExternalUnknown];
+  }
 #endif
-  ];
 
   AVCaptureDeviceDiscoverySession* deviceDiscoverySession =
       [AVCaptureDeviceDiscoverySession
-          discoverySessionWithDeviceTypes:captureDeviceType
+          discoverySessionWithDeviceTypes:captureDeviceTypes
                                 mediaType:AVMediaTypeVideo
                                  position:AVCaptureDevicePositionUnspecified];
   return deviceDiscoverySession.devices;
