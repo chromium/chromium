533f4e02f4b42fa37859653b5b519e447375f2ae
Glenn Hartmann <hartmanng@chromium.org>
2023-09-26 14:36:27 +0000

    Move web_app::AppId and web_app::ManifestId to a more public place
    So that Android and Desktop implementations can share the types.
    
    Note that this is part of a multi-CL change. This CL only touches
    references in /chrome/browser/ash/crosapi.
    
    BUG=1486624
    
    This CL was uploaded by git cl split.
    
    R=rbock@google.com
    
    Change-Id: I6074aa2ebf06181e33c769b1d167f8a2bbf4e6a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4884676
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201558}

--- chrome/browser/ash/crosapi/web_app_provider_bridge_browsertest.cc
+++ chrome/browser/ash/crosapi/web_app_provider_bridge_browsertest.cc
@@ -9,7 +9,7 @@
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/ash/crosapi/web_app_service_ash.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/web_applications/web_app_id.h"
+#include "components/webapps/common/web_app_id.h"
 #include "content/public/test/browser_test.h"
 
 namespace crosapi {
@@ -24,7 +24,7 @@ class WebAppProviderBridgeBrowserTest
     AshRequiresLacrosBrowserTestBase::SetUp();
   }
 
-  web_app::AppId InstallWebApp(const std::string& start_url,
+  webapps::AppId InstallWebApp(const std::string& start_url,
                                apps::WindowMode mode) {
     base::test::TestFuture<const std::string&> future;
     GetStandaloneBrowserTestController()->InstallWebApp(start_url, mode,
@@ -35,7 +35,7 @@ class WebAppProviderBridgeBrowserTest
     return app_id;
   }
 
-  web_app::AppId InstallSubApp(const web_app::AppId& parent_app_id,
+  webapps::AppId InstallSubApp(const webapps::AppId& parent_app_id,
                                std::string sub_app_start_url) {
     base::test::TestFuture<const std::string&> future;
     GetStandaloneBrowserTestController()->InstallSubApp(
@@ -48,14 +48,14 @@ class WebAppProviderBridgeBrowserTest
 };
 
 IN_PROC_BROWSER_TEST_F(WebAppProviderBridgeBrowserTest, GetSubAppIds) {
-  web_app::AppId parent_app_id =
+  webapps::AppId parent_app_id =
       InstallWebApp("https://www.parent-app.com", apps::WindowMode::kWindow);
-  web_app::AppId sub_app_id_1 =
+  webapps::AppId sub_app_id_1 =
       InstallSubApp(parent_app_id, "https://www.parent-app.com/sub-app-1");
-  web_app::AppId sub_app_id_2 =
+  webapps::AppId sub_app_id_2 =
       InstallSubApp(parent_app_id, "https://www.parent-app.com/sub-app-2");
 
-  base::flat_set<web_app::AppId> expected;
+  base::flat_set<webapps::AppId> expected;
   expected.emplace(sub_app_id_1);
   expected.emplace(sub_app_id_2);
 
@@ -66,29 +66,29 @@ IN_PROC_BROWSER_TEST_F(WebAppProviderBridgeBrowserTest, GetSubAppIds) {
           ->GetWebAppProviderBridge();
   ASSERT_TRUE(web_app_provider_bridge);
 
-  base::test::TestFuture<const std::vector<web_app::AppId>&>
+  base::test::TestFuture<const std::vector<webapps::AppId>&>
       get_sub_apps_future;
 
   web_app_provider_bridge->GetSubAppIds(parent_app_id,
                                         get_sub_apps_future.GetCallback());
 
-  base::flat_set<web_app::AppId> results_set{get_sub_apps_future.Get()};
+  base::flat_set<webapps::AppId> results_set{get_sub_apps_future.Get()};
   EXPECT_EQ(2u, results_set.size());
   EXPECT_EQ(results_set, expected);
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppProviderBridgeBrowserTest, GetSubAppToParentMap) {
-  web_app::AppId parent_app_id =
+  webapps::AppId parent_app_id =
       InstallWebApp("https://www.parent-app.com", apps::WindowMode::kWindow);
-  web_app::AppId sub_app_id_1 =
+  webapps::AppId sub_app_id_1 =
       InstallSubApp(parent_app_id, "https://www.parent-app.com/sub-app-1");
-  web_app::AppId sub_app_id_2 =
+  webapps::AppId sub_app_id_2 =
       InstallSubApp(parent_app_id, "https://www.parent-app.com/sub-app-2");
   // This app should not appear at all in the result map.
-  web_app::AppId unrelated_app_id =
+  webapps::AppId unrelated_app_id =
       InstallWebApp("https://www.unrelated-app.com", apps::WindowMode::kWindow);
 
-  base::flat_map<web_app::AppId, web_app::AppId> expected;
+  base::flat_map<webapps::AppId, webapps::AppId> expected;
   expected[sub_app_id_1] = parent_app_id;
   expected[sub_app_id_2] = parent_app_id;
 
@@ -99,13 +99,13 @@ IN_PROC_BROWSER_TEST_F(WebAppProviderBridgeBrowserTest, GetSubAppToParentMap) {
           ->GetWebAppProviderBridge();
   ASSERT_TRUE(web_app_provider_bridge);
 
-  base::test::TestFuture<const base::flat_map<web_app::AppId, web_app::AppId>&>
+  base::test::TestFuture<const base::flat_map<webapps::AppId, webapps::AppId>&>
       get_sub_apps_to_parent_map_future;
 
   web_app_provider_bridge->GetSubAppToParentMap(
       get_sub_apps_to_parent_map_future.GetCallback());
 
-  base::flat_map<web_app::AppId, web_app::AppId> results{
+  base::flat_map<webapps::AppId, webapps::AppId> results{
       get_sub_apps_to_parent_map_future.Get()};
   EXPECT_EQ(2u, results.size());
   EXPECT_EQ(results, expected);
