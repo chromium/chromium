1b9f546cf600df393254080a8761aac3a68087ae
Koji Ishii <kojii@chromium.org>
2023-09-27 05:49:52 +0000

    [text-spacing-trim] Remove redundant copying of variants to links
    As a minor follow to crrev.com/c/4886653, it turned out that
    wpt variants automatically adds the variants to ref links as
    well, so copying to href links isn't necessary. This patch
    removes that.
    
    crbug.com/1486901 has some info from the investigation.
    
    This patch has no behavior changes.
    
    Bug: 1463891
    Change-Id: Ie60fc6343491e751faa833dfcf5e07cefdd0155d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891235
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1201933}

--- third_party/blink/web_tests/external/wpt/css/css-text/text-spacing-trim/support/variant-class.js
+++ third_party/blink/web_tests/external/wpt/css/css-text/text-spacing-trim/support/variant-class.js
@@ -2,23 +2,12 @@
 
 /**
  * Add `class` search params to the `class` attribute of the root element.
- *
- * They are also added to `<link rel="match">` so that ref files have the same
- * `class` search parameters.
  */
 if (window.location.search) {
   const params = new URLSearchParams(window.location.search);
   if (params.has('class')) {
-    const classes = document.documentElement.classList;
-    const links = document.querySelectorAll('link[rel="match"]');
-    for (const value of params.getAll('class')) {
-      classes.add(...value.split(','));
-
-      for (const link of links) {
-        const url = new URL(link.href);
-        url.searchParams.append('class', value);
-        link.href = url;
-      }
-    }
+    const values = params.getAll('class')
+                         .flatMap(value => value.split(','));
+    document.documentElement.classList.add(...values);
   }
 }
