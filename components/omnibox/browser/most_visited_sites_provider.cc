89de16222a9ef3dfeb1eba7076a7514907dbae55
Tomasz Wiszkowski <ender@google.com>
2023-09-27 20:59:46 +0000

    Plumb Most Visited URL / Query type information from Top Sites.
    This change enables us to determine whether Most Visited site is a URL
    or Search based on the origin of the information, bypassing the cost
    of decomposing the URL and looking for a query.
    
    Change-Id: Id6c49580a8e54421d5b828a08bff9824d31846c1
    Fixed: 1370151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875303
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202088}

--- components/omnibox/browser/most_visited_sites_provider.cc
+++ components/omnibox/browser/most_visited_sites_provider.cc
@@ -99,15 +99,11 @@ bool BuildTileSuggest(AutocompleteProvider* provider,
     auto* const dse = url_service->GetDefaultSearchProvider();
     int relevance = kMostVisitedTilesIndividualRelevance;
     for (const auto& tile : container) {
-      // TODO(crbug/1474087): pass this information from History layer via
-      // history::MostVisitedURL.
-      bool is_search =
-          url_service->IsSearchResultsPageFromDefaultSearchProvider(tile.url);
-      auto match =
-          BuildMatch(provider, client, tile.title, tile.url, relevance,
-                     is_search ? AutocompleteMatchType::TILE_REPEATABLE_QUERY
-                               : AutocompleteMatchType::TILE_MOST_VISITED_SITE);
-      if (is_search) {
+      auto match = BuildMatch(
+          provider, client, tile.title, tile.url, relevance,
+          tile.is_srp ? AutocompleteMatchType::TILE_REPEATABLE_QUERY
+                      : AutocompleteMatchType::TILE_MOST_VISITED_SITE);
+      if (tile.is_srp) {
         match.keyword = dse->keyword();
         std::u16string query = tile.title;
 
@@ -134,22 +130,17 @@ bool BuildTileSuggest(AutocompleteProvider* provider,
                    AutocompleteMatchType::TILE_NAVSUGGEST);
 
     match.suggest_tiles.reserve(container.size());
-    auto* const url_service = client->GetTemplateURLService();
-
     size_t num_search_tiles = 0;
     size_t num_url_tiles = 0;
 
     for (const auto& tile : container) {
-      bool is_search =
-          url_service->IsSearchResultsPageFromDefaultSearchProvider(tile.url);
-
       match.suggest_tiles.push_back({
           .url = tile.url,
           .title = tile.title,
-          .is_search = is_search,
+          .is_search = tile.is_srp,
       });
 
-      if (is_search) {
+      if (tile.is_srp) {
         num_search_tiles++;
       } else {
         num_url_tiles++;
