41709f02db4e76178a989be386a78bb0c84d76b8
Keren Zhu <kerenzhu@chromium.org>
2023-09-28 23:01:28 +0000

    immersive fs mac: ensure that the overlay widget is observed only once
    The overlay widget is observed twice and causes DCHECK failure in UI
    devtools because both the overlay widget's own NSWindow and AppKit's
    native NSToolbarFullScreenWindow maps to the overlay widget. See
    https://crrev.com/c/3750424.
    
    This CL fixes the DCHECK failure by ensuring each widget is observed
    only once in UI devtools.
    
    Bug: 1487692
    Change-Id: Ic99a2a9d3c0c8621d6d4102d5f83e15d80cf813b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902221
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1202921}

--- components/ui_devtools/views/overlay_agent_mac.mm
+++ components/ui_devtools/views/overlay_agent_mac.mm
@@ -80,7 +80,9 @@ void OverlayAgentMac::InstallPreTargetHandlerOnWidget(views::Widget* widget) {
     return;
   widget->GetRootView()->AddPreTargetHandler(
       this, ui::EventTarget::Priority::kSystem);
-  widget->AddObserver(this);
+  if (!widget->HasObserver(this)) {
+    widget->AddObserver(this);
+  }
 }
 void OverlayAgentMac::RemovePreTargetHandlerOnWidget(views::Widget* widget) {
   if (!widget)
