2a057132dab23f8bbbeb132a692b77edece86b68
cassycc <cassycc@google.com>
2023-09-28 07:36:46 +0000

    Files app: Convert volume_info_list_impl to TS
    Bug: b/289003444
    Change-Id: I4e8b57fdb2a04a297252a6e422b4d76b231b06e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4886590
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202393}

--- ui/file_manager/file_manager/background/js/volume_info_list_impl.ts
+++ ui/file_manager/file_manager/background/js/volume_info_list_impl.ts
@@ -2,38 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @fileoverview The container of the VolumeInfo for each mounted volume.
+ * Disable type checking for closure, as it is done by the typescript compiler.
+ * @suppress {checkTypes}
+ */
+
 import {ArrayDataModel} from '../../common/js/array_data_model.js';
 import {VolumeInfo} from '../../externs/volume_info.js';
 import {VolumeInfoList} from '../../externs/volume_info_list.js';
 
+// To avoid the import being elided, closure requires this name here because of
+// the @implements.
+export const _unused = VolumeInfoList;
+
 /**
- * The container of the VolumeInfo for each mounted volume.
- * @final
  * @implements {VolumeInfoList}
  */
-export class VolumeInfoListImpl {
+export class VolumeInfoListImpl implements VolumeInfoList {
+  /**
+   * Holds VolumeInfo instances.
+   */
+  private model_ = new ArrayDataModel([]);
   constructor() {
-    /**
-     * Holds VolumeInfo instances.
-     * @private @const {ArrayDataModel}
-     */
-    this.model_ = new ArrayDataModel([]);
     Object.freeze(this);
   }
 
-  get length() {
+  get length(): number {
     return this.model_.length;
   }
 
-  addEventListener(type, handler) {
+  addEventListener(
+      type: string, handler: EventListenerOrEventListenerObject|null) {
     this.model_.addEventListener(type, handler);
   }
 
-  removeEventListener(type, handler) {
+  removeEventListener(
+      type: string, handler: EventListenerOrEventListenerObject|null) {
     this.model_.removeEventListener(type, handler);
   }
 
-  add(volumeInfo) {
+  add(volumeInfo: VolumeInfo) {
     const index = this.findIndex(volumeInfo.volumeId);
     if (index !== -1) {
       this.model_.splice(index, 1, volumeInfo);
@@ -42,23 +51,22 @@ export class VolumeInfoListImpl {
     }
   }
 
-  remove(volumeId) {
+  remove(volumeId: string) {
     const index = this.findIndex(volumeId);
     if (index !== -1) {
+      // @ts-ignore: TS doesn't recognize Closure {...*} type.
       this.model_.splice(index, 1);
     }
   }
 
-  item(index) {
-    return /** @type {!VolumeInfo} */ (this.model_.item(index));
+  item(index: number): VolumeInfo {
+    return (this.model_.item(index)) as VolumeInfo;
   }
 
   /**
    * Obtains an index from the volume ID.
-   * @param {string} volumeId Volume ID.
-   * @return {number} Index of the volume.
    */
-  findIndex(volumeId) {
+  findIndex(volumeId: string): number {
     for (let i = 0; i < this.model_.length; i++) {
       if (this.model_.item(i).volumeId === volumeId) {
         return i;
