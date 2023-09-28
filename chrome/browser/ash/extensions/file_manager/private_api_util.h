63955e03950e28cb30aceedf0856a34a636e21b3
Austin Tankiang <austinct@chromium.org>
2023-09-28 00:27:12 +0000

    [M117] Get the local paths before force resyncing in SelectFile
    This means we no longer need to bind the render frame host. The local
    paths are already required to resync the file anyway, so this also
    removes some duplicated work. There should be no change in behaviour.
    
    (cherry picked from commit 716ddb369e8956d342d1c00730fcf2a90e8707c4)
    
    Bug: b:301111340
    Change-Id: Id8f9180fdef610ae0e8f3305c22eb6baa0c36993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875920
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1199479}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891975
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5938@{#1485}
    Cr-Branched-From: 2b50cb4bcc2318034581a816714d9535dc38966d-refs/heads/main@{#1181205}

--- chrome/browser/ash/extensions/file_manager/private_api_util.h
+++ chrome/browser/ash/extensions/file_manager/private_api_util.h
@@ -148,10 +148,9 @@ enum GetSelectedFileInfoLocalPathOption {
   NEED_LOCAL_PATH_FOR_SAVING,
 };
 
-// Gets the information for |file_urls|.
-void GetSelectedFileInfo(content::RenderFrameHost* render_frame_host,
-                         Profile* profile,
-                         const std::vector<GURL>& file_urls,
+// Gets the information for |local_paths|.
+void GetSelectedFileInfo(Profile* profile,
+                         std::vector<base::FilePath> local_paths,
                          GetSelectedFileInfoLocalPathOption local_path_option,
                          GetSelectedFileInfoCallback callback);
 
