66a5bc2cd2eba68a1fec2b355086ac72556ee89f
aliaaborhamy <aliaaborhamy@google.com>
2023-09-28 02:05:24 +0000

    [iOS] Add Open in Chrome extension's body
    Bug: 1478760
    Change-Id: I72696c9f7c822144c8ea5de8037cd456771affdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4887045
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202223}

--- ios/chrome/open_extension/open_view_controller.mm
+++ ios/chrome/open_extension/open_view_controller.mm
@@ -4,24 +4,130 @@
 
 #import "ios/chrome/open_extension/open_view_controller.h"
 
-#import <Foundation/Foundation.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 
-@implementation OpenViewController
+#import "base/apple/bundle_locations.h"
+#import "base/apple/foundation_util.h"
+#import "base/ios/block_types.h"
+#import "base/strings/sys_string_conversions.h"
+#import "ios/chrome/common/app_group/app_group_command.h"
+#import "ios/chrome/common/app_group/app_group_constants.h"
+
+// Type for completion handler to fetch the components of the share items.
+// `idResponse` type depends on the element beeing fetched.
+using ItemBlock = void (^)(id idResponse, NSError* error);
+
+@implementation OpenViewController {
+  NSURL* _openInURL;
+  NSExtensionItem* _openInItem;
+}
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{{
-        [self.extensionContext
-         cancelRequestWithError:[NSError errorWithDomain:@"Open in Chrome"
-                                                    code:0
-                                                userInfo:@{
-            NSLocalizedDescriptionKey :
-                @"Extension is not supported"
-         }]];
+  [self loadElementsFromContext];
 }
-});
+
+- (void)loadElementsFromContext {
+  NSString* typeURL = UTTypeURL.identifier;
+  BOOL foundMatch = false;
+  for (NSExtensionItem* item in self.extensionContext.inputItems) {
+    for (NSItemProvider* itemProvider in item.attachments) {
+      if ([itemProvider hasItemConformingToTypeIdentifier:typeURL]) {
+        foundMatch = true;
+        __weak __typeof(self) weakSelf = self;
+        ItemBlock URLCompletion = ^(id idURL, NSError* error) {
+          NSURL* URL = base::apple::ObjCCast<NSURL>(idURL);
+          if (!URL) {
+            [self displayErrorView];
+            return;
+          }
+          dispatch_async(dispatch_get_main_queue(), ^{
+            [weakSelf shareItem:item url:URL];
+          });
+        };
+        [itemProvider loadItemForTypeIdentifier:typeURL
+                                        options:nil
+                              completionHandler:URLCompletion];
+      }
+    }
+  }
+  // Display the error view when no match have been found.
+  if (!foundMatch) {
+    [self displayErrorView];
+  }
+}
+
+- (void)shareItem:(NSExtensionItem*)item url:(NSURL*)URL {
+  _openInItem = [item copy];
+  _openInURL = [URL copy];
+  if ([[_openInURL scheme] isEqualToString:@"http"] ||
+      [[_openInURL scheme] isEqualToString:@"https"]) {
+    [self openInChrome];
+  } else {
+    [self displayErrorView];
+  }
+}
+
+- (void)openInChrome {
+  UIResponder* responder = self;
+
+  while ((responder = responder.nextResponder)) {
+    if ([responder respondsToSelector:@selector(openURL:)]) {
+      [self prepareCommand:responder];
+      [self.extensionContext completeRequestReturningItems:@[ _openInItem ]
+                                         completionHandler:nil];
+      return;
+    }
+  }
+  [self.extensionContext
+      cancelRequestWithError:[NSError errorWithDomain:NSURLErrorDomain
+                                                 code:NSURLErrorUnknown
+                                             userInfo:nil]];
+}
+
+- (void)prepareCommand:(UIResponder*)responder {
+  AppGroupCommand* command = [[AppGroupCommand alloc]
+      initWithSourceApp:app_group::kOpenCommandSourceOpenExtension
+         URLOpenerBlock:^(NSURL* openURL) {
+           [responder performSelector:@selector(openURL:) withObject:openURL];
+         }];
+  [command prepareToOpenURL:_openInURL];
+  [command executeInApp];
+}
+
+- (void)displayErrorView {
+  __weak OpenViewController* weakSelf = self;
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [weakSelf displayErrorViewMainThread];
+  });
+}
+
+- (void)displayErrorViewMainThread {
+  NSString* errorMessage =
+      NSLocalizedString(@"IDS_IOS_ERROR_MESSAGE_OPEN_IN_EXTENSION",
+                        @"The error message to display to the user.");
+  NSString* okButton =
+      NSLocalizedString(@"IDS_IOS_OK_BUTTON_OPEN_IN_EXTENSION",
+                        @"The label of the OK button in open in extension.");
+  UIAlertController* alert =
+      [UIAlertController alertControllerWithTitle:errorMessage
+                                          message:[_openInURL absoluteString]
+                                   preferredStyle:UIAlertControllerStyleAlert];
+  __weak __typeof(self) weakSelf = self;
+  UIAlertAction* defaultAction =
+      [UIAlertAction actionWithTitle:okButton
+                               style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction* action) {
+                               NSError* unsupportedURLError = [NSError
+                                   errorWithDomain:NSURLErrorDomain
+                                              code:NSURLErrorUnsupportedURL
+                                          userInfo:nil];
+                               [weakSelf.extensionContext
+                                   cancelRequestWithError:unsupportedURLError];
+                             }];
+  [alert addAction:defaultAction];
+  [self presentViewController:alert animated:YES completion:nil];
 }
 @end
