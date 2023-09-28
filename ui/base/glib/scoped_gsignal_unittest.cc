52ce3e124f457b9f062ff5c1d9fc0382959b591e
Tom Anderson <thomasanderson@chromium.org>
2023-09-28 22:16:39 +0000

    Reland "Add ScopedGSignal for managing GLib signal lifetime"
    This is a reland of commit 15eaed5fd66be5a76ea620b08ab08ad5e6e1d712
    
    The reland fixes CFI tests, and adds 2 changes I noticed while
    converting signal usage in //ui/gtk to ScopedGSignal:
    1. Adds missing return in callback function
    2. Adds a raw_ptr constructor overload
    
    Original change's description:
    > Add ScopedGSignal for managing GLib signal lifetime
    >
    > Currently, g_signal_connect() is used in //remoting, //ui/gtk,
    > //net //third_party/webrtc, and //content/browser/accessibility.
    > In almost all cases, the signal isn't disconnected when receiving
    > objects are destroyed, which can lead to use-after-frees.
    >
    > This CL adds ScopedGSignal for safer management of GLib signal
    > lifetime since proper management of GLib signals is surprisingly
    > tricky: g_signal_handler_disconnect() should be called when the
    > signal is no longer needed, but if the sender is deallocated
    > before disconnecting, a crash will occur in GLib.  Therefore we
    > also need to listen for sender destruction.  However, this
    > creates some more problems since GLib may run the destruction
    > callback asynchronously after we've disconnected from the signal,
    > so we also need to carefully remove the destruction callback
    > in the appropriate places.
    >
    > Followup CLs will convert usages of g_signal_connect() to
    > ScopedGSignal, and a presubmit check will be added to prevent
    > new usages of g_signal_connect().
    >
    > Change-Id: I485a188c915ea79910dad181eddda5eed075e9c0
    > Bug: None
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4885026
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1202336}
    
    Bug: None
    Change-Id: I31aede53904a117d03626e2839efd98804990288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4901584
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202880}

---
+++ ui/base/glib/scoped_gsignal_unittest.cc
@@ -0,0 +1,150 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/glib/scoped_gsignal.h"
+
+#include "base/functional/bind.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/glib/scoped_gobject.h"
+
+namespace ui {
+
+namespace {
+
+G_DECLARE_FINAL_TYPE(TestObject, test_object, TEST, OBJECT, GObject)
+
+// Used by G_DECLARE_FINAL_TYPE above.
+struct _TestObject {
+  GObject parent_instance;
+};
+
+G_DEFINE_TYPE(TestObject, test_object, G_TYPE_OBJECT)
+
+// Used by G_DEFINE_TYPE above.
+void test_object_class_init(TestObjectClass*) {
+  g_signal_newv("some-signal", test_object_get_type(),
+                (GSignalFlags)(G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE |
+                               G_SIGNAL_NO_HOOKS),
+                nullptr, nullptr, nullptr, nullptr, G_TYPE_NONE, 0, nullptr);
+}
+
+// Used by G_DEFINE_TYPE above.
+void test_object_init(TestObject* self) {}
+
+ScopedGObject<GObject> CreateGObject() {
+  return TakeGObject(G_OBJECT(g_object_new(test_object_get_type(), nullptr)));
+}
+
+void EmitSignal(GObject* instance, const gchar* detailed_signal) {
+  g_signal_emit_by_name(instance, detailed_signal);
+}
+
+}  // namespace
+
+TEST(ScopedGSignalTest, Empty) {
+  ScopedGSignal signal;
+  EXPECT_FALSE(signal.Connected());
+}
+
+TEST(ScopedGSignalTest, Construction) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal(instance, "some-signal",
+                       base::BindRepeating([](GObject* obj) {}));
+  EXPECT_TRUE(signal.Connected());
+}
+
+TEST(ScopedGSignalTest, DisconnectsOnDestruction) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  bool signal_fired = false;
+  {
+    ScopedGSignal signal(
+        instance, "some-signal",
+        base::BindRepeating([](bool* fired, GObject* obj) { *fired = true; },
+                            &signal_fired));
+    EXPECT_TRUE(signal.Connected());
+  }
+
+  EmitSignal(instance, "some-signal");
+  EXPECT_FALSE(signal_fired);
+}
+
+TEST(ScopedGSignalTest, DisconnectsOnGClosureFinalize) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal(instance, "some-signal",
+                       base::BindRepeating([](GObject* obj) {}));
+  EXPECT_TRUE(signal.Connected());
+
+  instance.Reset();
+  EXPECT_FALSE(signal.Connected());
+}
+
+TEST(ScopedGSignalTest, DisconnectsOnReset) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal(instance, "some-signal",
+                       base::BindRepeating([](GObject* obj) {}));
+  EXPECT_TRUE(signal.Connected());
+
+  signal.Reset();
+  EXPECT_FALSE(signal.Connected());
+}
+
+TEST(ScopedGSignalTest, MoveConstruct) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal1(instance, "some-signal",
+                        base::BindRepeating([](GObject* obj) {}));
+  EXPECT_TRUE(signal1.Connected());
+
+  ScopedGSignal signal2{std::move(signal1)};
+  EXPECT_FALSE(signal1.Connected());
+  EXPECT_TRUE(signal2.Connected());
+}
+
+TEST(ScopedGSignalTest, MoveAssign) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal1(instance, "some-signal",
+                        base::BindRepeating([](GObject* obj) {}));
+  EXPECT_TRUE(signal1.Connected());
+
+  ScopedGSignal signal2 = std::move(signal1);
+  EXPECT_FALSE(signal1.Connected());
+  EXPECT_TRUE(signal2.Connected());
+}
+
+TEST(ScopedGSignalTest, SignalHandlerCalled) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  bool signal_fired = false;
+  ScopedGSignal signal(
+      instance, "some-signal",
+      base::BindRepeating([](bool* fired, GObject* obj) { *fired = true; },
+                          &signal_fired));
+  EXPECT_TRUE(signal.Connected());
+
+  EmitSignal(instance, "some-signal");
+  EXPECT_TRUE(signal_fired);
+}
+
+TEST(ScopedGSignalTest, InvalidSignal) {
+  auto instance = CreateGObject();
+  ASSERT_TRUE(instance.get());
+
+  ScopedGSignal signal(instance, "invalid-signal",
+                       base::BindRepeating([](GObject* obj) {}));
+  EXPECT_FALSE(signal.Connected());
+}
+
+}  // namespace ui
