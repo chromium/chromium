962f8abab6f28f51c19b53b3dbbca47aa5c8c6f1
Peter Marshall <petermarshall@chromium.org>
2023-09-28 00:16:50 +0000

    Disable flaky test NTPTilesTest.NavigateAfterSettingObserver
    Bug: 1487047
    Change-Id: Idecfc6f97182231db83390ec8e49249634995ac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891795
    Owners-Override: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1202149}

--- chrome/browser/ntp_tiles/ntp_tiles_browsertest.cc
+++ chrome/browser/ntp_tiles/ntp_tiles_browsertest.cc
@@ -136,7 +136,8 @@ IN_PROC_BROWSER_TEST_F(NTPTilesTest, ServerRedirect) {
 
 // Tests usage of MostVisitedSites mimicking Chrome Home, where an observer is
 // installed early and once and navigations follow afterwards.
-IN_PROC_BROWSER_TEST_F(NTPTilesTest, NavigateAfterSettingObserver) {
+// Flaky on several platforms: https://crbug.com/1487047.
+IN_PROC_BROWSER_TEST_F(NTPTilesTest, DISABLED_NavigateAfterSettingObserver) {
   ASSERT_TRUE(embedded_test_server()->Start());
   const GURL page_url = embedded_test_server()->GetURL("/simple.html");
 
