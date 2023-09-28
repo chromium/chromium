57dec6e355475d68dbd2627d261517173e3d28ad
Siddhartha S <ssid@chromium.org>
2023-09-28 02:12:44 +0000

    Enable UKM engine by default
    Change-Id: I6b4b2b60e0bcc21955a3c12e950db32dbc8068ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898421
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202234}

--- components/segmentation_platform/public/features.cc
+++ components/segmentation_platform/public/features.cc
@@ -14,7 +14,7 @@ BASE_FEATURE(kSegmentationPlatformFeature,
 
 BASE_FEATURE(kSegmentationPlatformUkmEngine,
              "SegmentationPlatformUkmEngine",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSegmentationPlatformUserVisibleTaskRunner,
              "SegmentationPlatformUserVisibleTaskRunner",
