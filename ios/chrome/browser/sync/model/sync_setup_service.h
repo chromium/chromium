0f46720388a84910ace1c079adfd63db2332873d
Mahmoud Rashad <mmrashad@google.com>
2023-09-28 15:47:11 +0000

    [Code Health][iOS][1/3] Move ios/chrome/browser/sync to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/sync will be moved to
    ios/c/b/sync/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480361
    Change-Id: Iff990d7e320a77f7dc7afccddc043cbe11570654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4900707
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1202661}

---
+++ ios/chrome/browser/sync/model/sync_setup_service.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SYNC_MODEL_SYNC_SETUP_SERVICE_H_
+#define IOS_CHROME_BROWSER_SYNC_MODEL_SYNC_SETUP_SERVICE_H_
+
+#import "ios/chrome/browser/sync/sync_setup_service.h"
+
+#endif  // IOS_CHROME_BROWSER_SYNC_MODEL_SYNC_SETUP_SERVICE_H_
