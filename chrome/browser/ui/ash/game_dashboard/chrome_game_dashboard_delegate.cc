a569146b71a56526b52ba2090e5bdea666938e98
lgcheng <lgcheng@google.com>
2023-09-29 16:37:47 +0000

    Pass package name to get app category
    Android can only get related information by package name.
    
    Bug: b:299995398
    Test: Manual
    Test: ash unit tests
    Change-Id: Id6f1cba17080120fce9ffba1145a8ae3e166567d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903142
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203316}

--- chrome/browser/ui/ash/game_dashboard/chrome_game_dashboard_delegate.cc
+++ chrome/browser/ui/ash/game_dashboard/chrome_game_dashboard_delegate.cc
@@ -4,8 +4,8 @@
 
 #include "chrome/browser/ui/ash/game_dashboard/chrome_game_dashboard_delegate.h"
 
+#include "ash/components/arc/session/connection_holder.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
-#include "chrome/browser/ash/app_list/arc/arc_app_list_prefs_factory.h"
 #include "chrome/browser/profiles/profile_manager.h"
 
 ChromeGameDashboardDelegate::ChromeGameDashboardDelegate() {}
@@ -17,9 +17,8 @@ void ChromeGameDashboardDelegate::GetIsGame(const std::string& app_id,
   // Get the app category from ArcAppListPrefs.
   auto* profile = ProfileManager::GetPrimaryUserProfile();
   CHECK(profile);
-  const auto app_category =
-      ArcAppListPrefsFactory::GetForBrowserContext(profile)->GetAppCategory(
-          app_id);
+  auto* arc_app_list_prefs = ArcAppListPrefs::Get(profile);
+  const auto app_category = arc_app_list_prefs->GetAppCategory(app_id);
   // If the category is anything except `kUndefined`, fire the callback,
   // otherwise, retrieve the category from ARC.
   if (app_category != arc::mojom::AppCategory::kUndefined) {
@@ -27,15 +26,16 @@ void ChromeGameDashboardDelegate::GetIsGame(const std::string& app_id,
     return;
   }
 
-  auto* connection = ArcAppListPrefs::Get(profile)->app_connection_holder();
-  auto* app_instance = ARC_GET_INSTANCE_FOR_METHOD(connection, GetAppCategory);
+  auto* app_instance = ARC_GET_INSTANCE_FOR_METHOD(
+      arc_app_list_prefs->app_connection_holder(), GetAppCategory);
   if (!app_instance) {
     // If there's no app instance, assume the app is not a game.
     std::move(callback).Run(/*is_game=*/false);
     return;
   }
+
   app_instance->GetAppCategory(
-      app_id,
+      arc_app_list_prefs->GetAppPackageName(app_id),
       base::BindOnce(&ChromeGameDashboardDelegate::OnReceiveAppCategory,
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
@@ -46,8 +46,7 @@ std::string ChromeGameDashboardDelegate::GetArcAppName(
   auto* profile = ProfileManager::GetPrimaryUserProfile();
   CHECK(profile);
 
-  auto app_info =
-      ArcAppListPrefsFactory::GetForBrowserContext(profile)->GetApp(app_id);
+  auto app_info = ArcAppListPrefs::Get(profile)->GetApp(app_id);
   if (!app_info) {
     LOG(ERROR) << "Failed to get app info: " << app_id << ".";
     return std::string();
