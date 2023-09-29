c4997a348a3ac738fafa94cd4e3a2ef61979d3b6
Juan Mojica <juanmojica@google.com>
2023-09-29 23:56:54 +0000

    [CSC] New badges feature should be able to turn off CSC badges.
    Bug: b:302746897
    Change-Id: I30587a697cb3ee6d50c92c5e455e51928eb2a614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905267
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203538}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1896,8 +1896,7 @@ void RenderViewContextMenu::AppendSearchWebForImageItems() {
   }
 
   menu_model_.AddItem(GetSearchForImageIdc(), menu_string);
-  if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser()) ||
-      companion::IsSearchImageInCompanionSidePanelSupported(GetBrowser())) {
+  if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser())) {
     menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
   }
 
@@ -2208,7 +2207,6 @@ void RenderViewContextMenu::AppendSearchProvider() {
         menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
       }
       if (companion::IsSearchWebInCompanionSidePanelSupported(GetBrowser())) {
-        menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
         // Add an "in new tab" item performing the non-side panel behavior.
         if (base::FeatureList::IsEnabled(
                 companion::features::
@@ -2500,8 +2498,7 @@ void RenderViewContextMenu::AppendRegionSearchItem() {
     menu_model_.AddItem(GetRegionSearchIdc(),
                         l10n_util::GetStringFUTF16(
                             resource_id, GetImageSearchProviderName(provider)));
-    if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser()) ||
-        companion::IsSearchImageInCompanionSidePanelSupported(GetBrowser())) {
+    if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser())) {
       menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
     }
 
