aa1b5df6e13678e7450ca13dc3cbdc61f4ede406
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-09-29 18:40:59 +0000

    Import wpt@5c8a89d4951c6f44c40c99af5a9c37d647e0eb38
    Using wpt-import in Chromium 12ce68197c10a57064e05ad143ceed795299ce22.
    With Chromium commits locally applied on WPT:
    3edfc57206 "[bfcache] Add NotRestoredReasons WPT coverage"
    29b0e704e6 "Test added for navigation timing bits dependent on DocumentLoadTiming"
    
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    hongchan@chromium.org, mjwilson@chromium.org, alvinji@chromium.org, sinafirooz@chromium.org:
      external/wpt/webaudio
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    pcupp@microsoft.com:
      external/wpt/editing
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I59c4b3e909772f6f3d137e675bc9229461cda76d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905705
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203382}

--- third_party/blink/web_tests/external/wpt/streams/transform-streams/reentrant-strategies.any.js
+++ third_party/blink/web_tests/external/wpt/streams/transform-streams/reentrant-strategies.any.js
@@ -314,6 +314,10 @@ promise_test(t => {
   // call to TransformStreamDefaultSink.
   return delay(0).then(() => {
     controller.enqueue('a');
+    return reader.read();
+  }).then(({ value, done }) => {
+    assert_false(done, 'done should be false');
+    assert_equals(value, 'a', 'value should be correct');
     return Promise.all([promise_rejects_exactly(t, error1, reader.read(), 'read() should reject'), abortPromise]);
   });
 }, 'writer.abort() inside size() should work');
