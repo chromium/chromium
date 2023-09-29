7f51c71c5cceb6c141819558ac64f72ad906ced2
mmrashad <mmrashad@google.com>
2023-09-29 10:45:32 +0000

    [Code Health][iOS][1/3] Move ios/c/b/reading_list to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/reading_list/ will be moved to
    ios/c/b/reading_list/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480340
    Change-Id: Icc4edfbeb80a5a3a4cf705761261e16db1702528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902244
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1203171}

---
+++ ios/chrome/browser/reading_list/model/reading_list_browser_agent.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_READING_LIST_MODEL_READING_LIST_BROWSER_AGENT_H_
+#define IOS_CHROME_BROWSER_READING_LIST_MODEL_READING_LIST_BROWSER_AGENT_H_
+
+#import "ios/chrome/browser/reading_list/reading_list_browser_agent.h"
+
+#endif  // IOS_CHROME_BROWSER_READING_LIST_MODEL_READING_LIST_BROWSER_AGENT_H_
