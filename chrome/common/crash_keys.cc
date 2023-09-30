460111ca5b8a855359eabe3e9743f196de3ba4fd
Ian Barkley-Yeung <iby@chromium.org>
2023-09-30 01:42:16 +0000

    Ensure CrashKeyWithName is not copied, moved, or deleted
    CrashKeyWithName should never be copied, moved, or deleted since that
    will break the CrashKey contract. Spell this out explicitly. (The class
    was already not copyable or moveable due to the member variables; this
    CL adds not destructable.)
    
    I don't think this will fix the referenced bug but it makes it a little
    easier to reason about.
    
    BUG=b:296821415
    
    Change-Id: Id7d179cb90977da7584424099a7fe5132e291788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4906401
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1203592}

--- chrome/common/crash_keys.cc
+++ chrome/common/crash_keys.cc
@@ -32,10 +32,20 @@ namespace crash_keys {
 namespace {
 
 // A convenient wrapper around a crash key and its name.
+//
+// The CrashKey contract requires that CrashKeyStrings are never
+// moved, copied, or deleted (see
+// third_party/crashpad/crashpad/client/annotation.h); since this class holds
+// a CrashKeyString, it likewise cannot be moved, copied, or deleted.
 class CrashKeyWithName {
  public:
   explicit CrashKeyWithName(std::string name)
       : name_(std::move(name)), crash_key_(name_.c_str()) {}
+  CrashKeyWithName(const CrashKeyWithName&) = delete;
+  CrashKeyWithName& operator=(const CrashKeyWithName&) = delete;
+  CrashKeyWithName(CrashKeyWithName&&) = delete;
+  CrashKeyWithName& operator=(CrashKeyWithName&&) = delete;
+  ~CrashKeyWithName() = delete;
 
   void Clear() { crash_key_.Clear(); }
   void Set(base::StringPiece value) { crash_key_.Set(value); }
