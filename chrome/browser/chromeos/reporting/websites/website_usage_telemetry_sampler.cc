891bfeabf30bc1800c3a42d0511a951fd983bc3f
Vignesh Shenvi <vshenvi@google.com>
2023-10-02 22:23:16 +0000

    Introducing a new website usage telemetry sampler
    This change introduces a new website usage telemetry sampler for
    collecting website usage telemetry data that was persisted by the
    WebsiteUsageObserver in the user pref store.
    
    Bug: b:302033398
    Change-Id: Id116edf5094dfce942681b1ed7a29edb54db0981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905801
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204306}

---
+++ chrome/browser/chromeos/reporting/websites/website_usage_telemetry_sampler.cc
@@ -0,0 +1,100 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/reporting/websites/website_usage_telemetry_sampler.h"
+
+#include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
+#include "base/json/values_util.h"
+#include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
+#include "chrome/browser/chromeos/reporting/metric_reporting_prefs.h"
+#include "components/prefs/pref_service.h"
+#include "components/prefs/scoped_user_pref_update.h"
+#include "components/reporting/metrics/sampler.h"
+#include "components/reporting/proto/synced/metric_data.pb.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace reporting {
+
+WebsiteUsageTelemetrySampler::WebsiteUsageTelemetrySampler(
+    base::WeakPtr<Profile> profile)
+    : profile_(profile) {}
+
+WebsiteUsageTelemetrySampler::~WebsiteUsageTelemetrySampler() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+void WebsiteUsageTelemetrySampler::MaybeCollect(
+    OptionalMetricCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  absl::optional<MetricData> metric_data;
+  base::ScopedClosureRunner run_callback_on_return(base::BindOnce(
+      [](base::WeakPtr<WebsiteUsageTelemetrySampler> self,
+         OptionalMetricCallback callback,
+         absl::optional<MetricData>* metric_data) {
+        DCHECK_CALLED_ON_VALID_SEQUENCE(self->sequence_checker_);
+        if (!metric_data->has_value()) {
+          std::move(callback).Run(std::move(*metric_data));
+          return;
+        }
+
+        // Report metric data and delete tracked website usage data from the
+        // pref store.
+        const auto& website_usage_data = metric_data->value()
+                                             .telemetry_data()
+                                             .website_telemetry()
+                                             .website_usage_data();
+        CHECK(!website_usage_data.website_usage().empty());
+        std::move(callback).Run(std::move(*metric_data));
+        self->DeleteWebsiteUsageDataFromPrefStore(&website_usage_data);
+      },
+      weak_ptr_factory_.GetWeakPtr(), std::move(callback), &metric_data));
+  if (!profile_) {
+    // Profile has been destructed. Return.
+    return;
+  }
+  const PrefService* const user_prefs = profile_->GetPrefs();
+  if (!user_prefs->HasPrefPath(kWebsiteUsage)) {
+    // No usage data being tracked in the pref store. Return.
+    return;
+  }
+  const base::Value::Dict& usage_dict = user_prefs->GetDict(kWebsiteUsage);
+  if (usage_dict.empty()) {
+    // No website usage data to report. Return.
+    return;
+  }
+
+  // Parse website usage across URLs from the pref store and populate
+  // website usage data.
+  metric_data = absl::make_optional<MetricData>();
+  auto* const website_usage_data = metric_data->mutable_telemetry_data()
+                                       ->mutable_website_telemetry()
+                                       ->mutable_website_usage_data();
+  for (auto usage_it : usage_dict) {
+    const absl::optional<const base::TimeDelta> saved_usage_time =
+        base::ValueToTimeDelta(usage_it.second);
+    CHECK(saved_usage_time.has_value());
+    WebsiteUsageData::WebsiteUsage* const website_usage =
+        website_usage_data->mutable_website_usage()->Add();
+    website_usage->set_url(usage_it.first);
+    website_usage->set_running_time_ms(
+        saved_usage_time.value().InMilliseconds());
+  }
+}
+
+void WebsiteUsageTelemetrySampler::DeleteWebsiteUsageDataFromPrefStore(
+    const WebsiteUsageData* website_usage_data) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  CHECK(profile_);
+  ScopedDictPrefUpdate usage_dict_pref(profile_->GetPrefs(), kWebsiteUsage);
+  for (const auto& website_usage : website_usage_data->website_usage()) {
+    const std::string& url = website_usage.url();
+    CHECK(usage_dict_pref->contains(url))
+        << "Missing website usage data for URL: " << url;
+    usage_dict_pref->Remove(url);
+  }
+}
+
+}  // namespace reporting
