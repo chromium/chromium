21f3600044b24cb7f43de2233957a7ea3ea7a596
Daniel White <danieltwhite@google.com>
2023-10-02 15:16:54 +0000

    [iOS] WebThreadTaskExecutor::GetTaskRunner Crash Fix
    In rare cases, for example when a user obtains a new device and restores
    it from a previous backup, iOS invokes the
    [application didRegisterForRemoteNotificationsWithDeviceToken:] function
    potentially before Chrome threads have been initialized, which is the
    root cause of the crash. In this case, iOS' invocation is ignored and
    the device is registered for push notifications through the normal
    startup process.
    
    (cherry picked from commit d58e29d73bc74668edf67270ab307b61e71b121f)
    
    Bug: 1486463
    Change-Id: I8463e9d69d250039ae8af81ec33d3ee0855ec20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4892839
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1202028}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902551
    Auto-Submit: Daniel White <danieltwhite@google.com>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/branch-heads/5993@{#1045}
    Cr-Branched-From: 511350718e646be62331ae9d7213d10ec320d514-refs/heads/main@{#1192594}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -227,6 +227,16 @@ const int kMainIntentCheckDelay = 1;
 
 - (void)application:(UIApplication*)application
     didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken {
+  // In rare cases, for example when a user obtains a new device and restores it
+  // from a previous backup, iOS invokes the [application
+  // didRegisterForRemoteNotificationsWithDeviceToken:] function potentially
+  // before Chrome threads have been initialized. In this case, iOS'
+  // invocation is ignored and the device is registered for push notifications
+  // through the normal startup process.
+  if (!web::WebThread::IsThreadInitialized(web::WebThread::UI)) {
+    return;
+  }
+
   // This method is invoked by iOS on the successful registration of the app to
   // APNS and retrieval of the device's APNS token.
   _didRegisterDeviceWithAPNS = YES;
