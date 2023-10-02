d32dc3635147aa9cb4638864865aa6f86f42f2f2
Ming-Ying Chung <mych@chromium.org>
2023-10-02 17:01:24 +0000

    [fetch-later] Force sending when BackgroundSync permission is off
    The [privacy decision][1] suggests: after user navigates away from
    a page, any pending requests left in the page should not be sent
    if [BackgroundSync permission][2] is off (default is on in Chrome).
    
    This CL forces sending out deferred requests on entering
    BackForwardCache if the page's BackgroundSync permission is off.
    
    [1]: https://github.com/WICG/pending-beacon/issues/3#issuecomment-1286397825
    [2]: https://wicg.github.io/background-sync/spec/
    
    Bug: 1465781
    Change-Id: I120fbbc13294c40fcf13162d5d786f97e2131367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4845750
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204080}

--- third_party/blink/web_tests/wpt_internal/fetch/fetch-later/send-on-deactivate-with-background-sync.tentative.https.window.js
+++ third_party/blink/web_tests/wpt_internal/fetch/fetch-later/send-on-deactivate-with-background-sync.tentative.https.window.js
@@ -9,12 +9,26 @@
 
 'use strict';
 
-// NOTE: Due to the restriction of WPT runner, the following tests are all run
-// with BackgroundSync off, which is different from some browser's,
-// e.g. Chrome, default behavior, as the testing infra does not support enabling
-// it.
+/**
+ * This file cannot be upstreamed to external/wpt/ until:
+ * `internals.setPermission()` usage is replaced with a WebDriver API.
+ */
+
+// BackgroundSync needs to be explicitly enabled in Web Tests, as the test
+// runner uses a different permission manager. See
+// https://source.chromium.org/chromium/chromium/src/+/main:content/web_test/browser/web_test_permission_manager.h;l=138-140;drc=f616c54d73c8eea9db5f7e567611711897651b66
+async function setBackgroundSyncEnabled(enabled) {
+  const status = enabled ? 'granted' : 'denied';
+  const origin = location.origin;
+  await internals.setPermission(
+      {name: 'background-sync'}, status, origin, origin);
+}
 
 parallelPromiseTest(async t => {
+  // Enables BackgroundSync permission such that deferred request won't be
+  // immediately sent out on entering BFCache.
+  await setBackgroundSyncEnabled(true);
+
   const uuid = token();
   const url = generateSetBeaconURL(uuid);
   // Sets no option to test the default behavior when a document enters BFCache.
@@ -41,81 +55,19 @@ parallelPromiseTest(async t => {
     return window.pageshowEvent.persisted;
   }));
 
-  // However, we expect 1 request sent, as by default the content_shell run by
-  // WPT does not enable BackgroundSync permission.
-  await expectBeacon(uuid, {count: 1});
-}, `fetchLater() sends on page entering BFCache if BackgroundSync is off.`);
+  await expectBeacon(uuid, {count: 0});
+}, `fetchLater() does not send on page entering BFCache if BackgroundSync is on.`);
 
 parallelPromiseTest(async t => {
+  // Enables BackgroundSync permission such that deferred request won't be
+  // immediately sent out on entering BFCache.
+  await setBackgroundSyncEnabled(true);
+
   const uuid = token();
   const url = generateSetBeaconURL(uuid);
-  const helper = new RemoteContextHelper();
-  // Opens a window with noopener so that BFCache will work.
-  const rc1 = await helper.addWindow(
-      /*config=*/ null, /*options=*/ {features: 'noopener'});
-
-  // When the remote is BFCached, creates a fetchLater request w/
-  // activationTimeout = 0s. It should be sent out immediately.
-  await rc1.executeScript(url => {
-    window.addEventListener('pagehide', e => {
-      if (e.persisted) {
-        fetchLater(url, {activationTimeout: 0});
-      }
-    });
-    // Add a pageshow listener to stash the BFCache event.
-    window.addEventListener('pageshow', e => {
-      window.pageshowEvent = e;
-    });
-  }, [url]);
-  // Navigates away to trigger request sending.
-  const rc2 = await rc1.navigateToNew();
-  // Navigate back.
-  await rc2.historyBack();
-  // Verify that the page was BFCached.
-  assert_true(await rc1.executeScript(() => {
-    return window.pageshowEvent.persisted;
-  }));
-
-  // Note: In this case, it does not matter if BackgroundSync is on or off.
-  await expectBeacon(uuid, {count: 1});
-}, `Call fetchLater() when BFCached with activationTimeout=0 sends immediately.`);
-
-parallelPromiseTest(async t => {
-  const uuid = token();
-  const url = generateSetBeaconURL(uuid);
-  // Sets no option to test the default behavior when a document gets discarded
-  // on navigated away.
-  const helper = new RemoteContextHelper();
-  // Opens a window without BFCache.
-  const rc1 = await helper.addWindow();
-
-  // Creates a fetchLater request in remote which should only be sent on
-  // navigating away.
-  await rc1.executeScript(url => {
-    fetchLater(url);
-    // Add a pageshow listener to stash the BFCache event.
-    window.addEventListener('pageshow', e => {
-      window.pageshowEvent = e;
-    });
-  }, [url]);
-  // Navigates away to trigger request sending.
-  const rc2 = await rc1.navigateToNew();
-  // Navigate back.
-  await rc2.historyBack();
-  // Verify that the page was NOT BFCached.
-  assert_equals(undefined, await rc1.executeScript(() => {
-    return window.pageshowEvent;
-  }));
-
-  await expectBeacon(uuid, {count: 1});
-}, `fetchLater() sends on navigating away a page w/o BFCache.`);
-
-parallelPromiseTest(async t => {
-  const uuid = token();
-  const url = generateSetBeaconURL(uuid);
-  // backgroundTimeout = 1m means the request should NOT be sent out on
-  // document becoming deactivated (BFCached or frozen) until after 1 minute.
-  const options = {backgroundTimeout: 60000};
+  // activationTimeout = 0s means the request should be sent out right on
+  // document becoming deactivated (BFCached or frozen) after navigating away.
+  const options = {activationTimeout: 0};
   const helper = new RemoteContextHelper();
   // Opens a window with noopener so that BFCache will work.
   const rc1 = await helper.addWindow(
@@ -125,6 +77,7 @@ parallelPromiseTest(async t => {
   // navigating away.
   await rc1.executeScript((url, options) => {
     fetchLater(url, options);
+
     // Add a pageshow listener to stash the BFCache event.
     window.addEventListener('pageshow', e => {
       window.pageshowEvent = e;
@@ -139,7 +92,42 @@ parallelPromiseTest(async t => {
     return window.pageshowEvent.persisted;
   }));
 
-  // However, as BackgroundSync is off by default, the request gets sent out
-  // immediately after entering BFCache.
   await expectBeacon(uuid, {count: 1});
-}, `fetchLater() with backgroundTimeout=1m sends on page entering BFCache if BackgroundSync is off.`);
+}, `fetchLater() with activationTimeout=0 sends on page entering BFCache if BackgroundSync is on.`);
+
+parallelPromiseTest(async t => {
+  // Enables BackgroundSync permission such that deferred request won't be
+  // immediately sent out on entering BFCache.
+  await setBackgroundSyncEnabled(true);
+
+  const uuid = token();
+  const url = generateSetBeaconURL(uuid);
+  // activationTimeout = 1m means the request should NOT be sent out on
+  // document becoming deactivated (BFCached or frozen) until after 1 minute.
+  const options = {activationTimeout: 60000};
+  const helper = new RemoteContextHelper();
+  // Opens a window with noopener so that BFCache will work.
+  const rc1 = await helper.addWindow(
+      /*config=*/ null, /*options=*/ {features: 'noopener'});
+
+  // Creates a fetchLater request in remote which should only be sent on
+  // navigating away.
+  await rc1.executeScript((url, options) => {
+    fetchLater(url, options);
+
+    // Add a pageshow listener to stash the BFCache event.
+    window.addEventListener('pageshow', e => {
+      window.pageshowEvent = e;
+    });
+  }, [url, options]);
+  // Navigates away to trigger request sending.
+  const rc2 = await rc1.navigateToNew();
+  // Navigate back.
+  await rc2.historyBack();
+  // Verify that the page was BFCached.
+  assert_true(await rc1.executeScript(() => {
+    return window.pageshowEvent.persisted;
+  }));
+
+  await expectBeacon(uuid, {count: 0});
+}, `fetchLater() with activationTimeout=1m does not send on page entering BFCache if BackgroundSync is on.`);
