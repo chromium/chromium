12cd99f50876a544abc242540ca7467d48c84a46
Juan Mojica <juanmojica@google.com>
2023-10-02 17:15:51 +0000

    [118][CSC] New badges feature should be able to turn off CSC badges.
    (cherry picked from commit c4997a348a3ac738fafa94cd4e3a2ef61979d3b6)
    
    Bug: b:302746897, 1449021
    Change-Id: I30587a697cb3ee6d50c92c5e455e51928eb2a614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905267
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1203538}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904341
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/5993@{#1054}
    Cr-Branched-From: 511350718e646be62331ae9d7213d10ec320d514-refs/heads/main@{#1192594}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1902,8 +1902,7 @@ void RenderViewContextMenu::AppendSearchWebForImageItems() {
   }
 
   menu_model_.AddItem(GetSearchForImageIdc(), menu_string);
-  if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser()) ||
-      companion::IsSearchImageInCompanionSidePanelSupported(GetBrowser())) {
+  if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser())) {
     menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
   }
 
@@ -2214,7 +2213,6 @@ void RenderViewContextMenu::AppendSearchProvider() {
         menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
       }
       if (companion::IsSearchWebInCompanionSidePanelSupported(GetBrowser())) {
-        menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
         // Add an "in new tab" item performing the non-side panel behavior.
         if (base::FeatureList::IsEnabled(
                 companion::features::
@@ -2513,8 +2511,7 @@ void RenderViewContextMenu::AppendRegionSearchItem() {
     menu_model_.AddItem(GetRegionSearchIdc(),
                         l10n_util::GetStringFUTF16(
                             resource_id, GetImageSearchProviderName(provider)));
-    if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser()) ||
-        companion::IsSearchImageInCompanionSidePanelSupported(GetBrowser())) {
+    if (companion::IsNewBadgeEnabledForSearchMenuItem(GetBrowser())) {
       menu_model_.SetIsNewFeatureAt(menu_model_.GetItemCount() - 1, true);
     }
 
