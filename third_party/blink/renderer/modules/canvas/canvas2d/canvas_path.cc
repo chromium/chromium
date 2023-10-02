4e00deb162b3be1c2eae1f5474dd309098ea16b7
Yi Xu <yiyix@google.com>
2023-10-02 19:12:03 +0000

    Skip line caps for closed paths
    As discussed in the github issue,
    https://github.com/whatwg/html/issues/1079, it's generally agreed to
    skip drawing line caps for closed paths. This will also fix the test
    2d.path.stroke.prune.closed.html.
    
    Bug: 644067
    
    Change-Id: I6e1ae0c31384e98c39e22f315214a14d9ce791d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4864061
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Yi Xu <yiyix@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204192}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.cc
@@ -51,6 +51,22 @@ void CanvasPath::closePath() {
   if (UNLIKELY(IsEmpty())) {
     return;
   }
+  // If the current path is a zero lengthed path (ex: moveTo p1 and lineTo p1),
+  // then closePath is no op.
+  if (UNLIKELY(path_.BoundingRect().height() == 0 &&
+               path_.BoundingRect().width() == 0 &&
+               (IsLine() && line_builder_.BoundingRect().height() == 0 &&
+                line_builder_.BoundingRect().width() == 0))) {
+    if (!path_.HasCurrentPoint()) {
+      Clear();
+      return;
+    }
+    auto p = path_.CurrentPoint();
+    Clear();
+    moveTo(p.x(), p.y());
+    return;
+  }
+
   UpdatePathFromLineIfNecessaryForMutation();
   if (UNLIKELY(identifiability_study_helper_.ShouldUpdateBuilder())) {
     identifiability_study_helper_.UpdateBuilder(CanvasOps::kClosePath);
