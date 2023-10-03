d583de75b0963bf5889e7065de3513d3a236639f
Xiyuan Xia <xiyuan@chromium.org>
2023-10-03 15:25:00 +0000

    crosier: Add reset dut script
    Add a helper script to reset DUT state. For now, it only nukes
    system state files.
    
    Bug: b:292023082
    Change-Id: I41af025601afabca05d92c5252734d889d633fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4903906
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1204668}

--- chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client_test.cc
+++ chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client_test.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client.h"
 
+#include "base/files/file_util.h"
 #include "base/strings/string_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -11,4 +12,21 @@ TEST(TestSudoHelperClientTest, WhoAmI) {
   auto result = TestSudoHelperClient().RunCommand("whoami");
   EXPECT_EQ(result.return_code, 0);
   EXPECT_EQ(base::TrimString(result.output, " \n", base::TRIM_ALL), "root");
+}
+
+TEST(TestSudoHelperClientTest, ResetSystemStateFiles) {
+  TestSudoHelperClient().RunCommand("touch /home/chronos/.oobe_completed");
+  TestSudoHelperClient().RunCommand("touch /var/lib/devicesettings/test");
+
+  ASSERT_TRUE(
+      base::PathExists(base::FilePath("/home/chronos/.oobe_completed")));
+  ASSERT_TRUE(base::PathExists(base::FilePath("/var/lib/devicesettings/test")));
+
+  auto result = TestSudoHelperClient().RunCommand("./reset_dut.py");
+  EXPECT_EQ(result.return_code, 0);
+
+  EXPECT_FALSE(
+      base::PathExists(base::FilePath("/home/chronos/.oobe_completed")));
+  EXPECT_FALSE(
+      base::PathExists(base::FilePath("/var/lib/devicesettings/test")));
 }
