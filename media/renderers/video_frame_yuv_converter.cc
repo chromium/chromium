761f68b51c21a8bbf821d8e0d083021e7a919419
Colin Blundell <blundell@chromium.org>
2023-10-03 07:37:17 +0000

    [//media] Handle `use_visible_rect` in ConvertYUVVideoFrame()
    Handling is straightforward now that predecessor CLs have enabled
    ConvertYUVAMailboxesToRGB() to take in a sub-rect of the source image.
    
    Bug: 1410164
    Change-Id: I0bdb17d3ee5d1cc8af7c420533973efcf08c59c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905194
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204491}

--- media/renderers/video_frame_yuv_converter.cc
+++ media/renderers/video_frame_yuv_converter.cc
@@ -139,15 +139,18 @@ bool VideoFrameYUVConverter::ConvertYUVVideoFrame(
         gr_params.value_or(GrParams()));
   }
 
-  // The RasterInterface path does not support flip_y or use_visible_rect.
+  // The RasterInterface path does not support flip_y.
   if (gr_params) {
     DCHECK(!gr_params->flip_y);
-    DCHECK(!gr_params->use_visible_rect);
   }
 
   auto* ri = raster_context_provider->RasterInterface();
   DCHECK(ri);
   ri->WaitSyncTokenCHROMIUM(dest_mailbox_holder.sync_token.GetConstData());
+
+  auto source_rect = gr_params && gr_params->use_visible_rect
+                         ? video_frame->visible_rect()
+                         : gfx::Rect(video_frame->coded_size());
 
   if (!video_frame->HasTextures() && IsWritePixelsYUVEnabled()) {
     // For pure software pixel upload paths with video frames that don't have
@@ -158,8 +161,8 @@ bool VideoFrameYUVConverter::ConvertYUVVideoFrame(
                                    /*allow_multiplanar_for_upload=*/true);
     gpu::Mailbox src_mailbox = mailboxes[0];
     ri->CopySharedImage(src_mailbox, dest_mailbox_holder.mailbox, GL_TEXTURE_2D,
-                        0, 0, 0, 0, video_frame->coded_size().width(),
-                        video_frame->coded_size().height(),
+                        0, 0, source_rect.x(), source_rect.y(),
+                        source_rect.width(), source_rect.height(),
                         /*unpack_flip_y=*/false,
                         /*unpack_premultiply_alpha=*/false);
   } else if (video_frame->shared_image_format_type() !=
@@ -169,8 +172,8 @@ bool VideoFrameYUVConverter::ConvertYUVVideoFrame(
     // textures.
     gpu::Mailbox src_mailbox = video_frame->mailbox_holder(0).mailbox;
     ri->CopySharedImage(src_mailbox, dest_mailbox_holder.mailbox, GL_TEXTURE_2D,
-                        0, 0, 0, 0, video_frame->coded_size().width(),
-                        video_frame->coded_size().height(),
+                        0, 0, source_rect.x(), source_rect.y(),
+                        source_rect.width(), source_rect.height(),
                         /*unpack_flip_y=*/false,
                         /*unpack_premultiply_alpha=*/false);
   } else {
@@ -181,8 +184,8 @@ bool VideoFrameYUVConverter::ConvertYUVVideoFrame(
                                    mailboxes,
                                    /*allow_multiplanar_for_upload=*/false);
     ri->ConvertYUVAMailboxesToRGB(
-        dest_mailbox_holder.mailbox, 0, 0, video_frame->coded_size().width(),
-        video_frame->coded_size().height(),
+        dest_mailbox_holder.mailbox, source_rect.x(), source_rect.y(),
+        source_rect.width(), source_rect.height(),
         holder_->yuva_info().yuvColorSpace(), nullptr,
         holder_->yuva_info().planeConfig(), holder_->yuva_info().subsampling(),
         mailboxes);
