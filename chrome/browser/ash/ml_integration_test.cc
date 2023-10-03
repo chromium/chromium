447ab5f62028b08c7e53c1fe69539f3fc320f9e7
Eriko Kurimoto <elkurin@chromium.org>
2023-10-03 14:14:36 +0000

    gardener: Disable MLIntegrationTest.Bootstrap
    Has been failing on chromeos-betty-pi-arc-chrome since the test had
    introduced.
    
    Bug: b:302983380
    Change-Id: I09abbb2d9ad5eee1abdcb35b6047be43b2ea79a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907460
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204625}

--- chrome/browser/ash/ml_integration_test.cc
+++ chrome/browser/ash/ml_integration_test.cc
@@ -16,7 +16,9 @@ using MLIntegrationTest = InteractiveAshTest;
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(MLIntegrationTest, Bootstrap) {
+// Failing on betty-pi-arc-chrome builder.
+// TODO(b/302983380): Re-enable the test.
+IN_PROC_BROWSER_TEST_F(MLIntegrationTest, DISABLED_Bootstrap) {
   std::string ml_service = "ml-service";
   std::vector<std::string> extra_args{"TASK=mojo_service"};
 
