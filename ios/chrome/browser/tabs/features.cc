e4e0cedeea5857fe07d899a606ab71ef30ead930
Sylvain Defresne <sdefresne@chromium.org>
2023-10-03 08:06:43 +0000

    [ios] Cleanup //ios/chrome/browser/tabs:features target
    Remove unnecessary includes and convert the implementation file
    from .mm to .cc (since it does not use any Objective-C).
    
    Bug: none
    Change-Id: Iab009b91707b5b11381360ab9794fb8f47ca3724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4907653
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204517}

--- ios/chrome/browser/tabs/features.cc
+++ ios/chrome/browser/tabs/features.cc
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/tabs/features.h"
+#include "ios/chrome/browser/tabs/features.h"
 
-#import "base/apple/foundation_util.h"
-#import "base/metrics/field_trial_params.h"
-#import "ui/base/device_form_factor.h"
+#include "ui/base/device_form_factor.h"
 
 bool IsPinnedTabsEnabled() {
   return ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_TABLET;
--- ios/chrome/browser/tabs/features.h
+++ ios/chrome/browser/tabs/features.h
@@ -5,8 +5,6 @@
 #ifndef IOS_CHROME_BROWSER_TABS_FEATURES_H_
 #define IOS_CHROME_BROWSER_TABS_FEATURES_H_
 
-#import <Foundation/Foundation.h>
-
 // Convenience method for determining if Pinned Tabs is enabled.
 // The Pinned Tabs feature is fully enabled on iPhone and disabled on iPad.
 bool IsPinnedTabsEnabled();
