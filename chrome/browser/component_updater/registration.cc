e52181c168429452125e652d07b82c318efd7ea4
Jonathan Njeunje <njeunje@chromium.org>
2023-10-03 14:10:00 +0000

    Registers the TPCD Metadata Component Installer
    Bug: b:290039143, 1488772
    Change-Id: Ie36981bacefe187a3caad8f78806d4e7c2610d44
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4742351
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Jonathan Njeunje <njeunje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204619}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -33,6 +33,7 @@
 #include "chrome/browser/component_updater/privacy_sandbox_attestations_component_installer.h"
 #include "chrome/browser/component_updater/ssl_error_assistant_component_installer.h"
 #include "chrome/browser/component_updater/subresource_filter_component_installer.h"
+#include "chrome/browser/component_updater/tpcd_metadata_component_installer.h"
 #include "chrome/browser/component_updater/trust_token_key_commitments_component_installer.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_paths.h"
@@ -212,6 +213,8 @@ void RegisterComponentsForUpdate() {
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE) && !BUILDFLAG(IS_CHROMEOS)
 
   RegisterCommerceHeuristicsComponent(cus);
+
+  RegisterTpcdMetadataComponent(cus);
 }
 
 }  // namespace component_updater
