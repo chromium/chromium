23ac8cbb62be574d5191dd998c95edd9aba8b4ed
dpapad <dpapad@chromium.org>
2023-10-03 16:57:00 +0000

    [Reland] WebUI: Update several toolchain dependencies to latest versions.
    Reland notes: Fixed presubmit test failures in
     - tools/resources/svgo_presubmit_test.py
     - ui/webui/resources/tools/bundle_js_test.py
    
    Specifically updating:
     - svgo:   2.8.0  -> 3.0.2
     - terser: 5.16.2 -> 5.20.0
     - rollup: 3.12.0 -> 3.29.4
     - eslint: 8.33.0 -> 8.50.0
    
    rollup notes:
    With the latest version of Rollup, bundled files are unfortunately a
    bit larger, due to fixing a previous bug [1], which incorrectly only
    kept the first line of all license header comments. The new behavior
    is correct (see updated tests), so perhaps we should look into ways of
    deduplicating license header comments in the future, to further reduce
    the file size.
    
    svgo notes:
     - Latest version includes a fix for [2] which was affecting some SVG
       files, and has resulted in using 'minifyStyles: false' in
       tools/resources/svgo.config.js as a workaround, see [3].
     - Updated svgo.config.js to account for the breaking change at [4].
     - Updated svgo_presubmit_test.py since it now re-arranges
       the order of SVG attributes as part of the optimization.
    
    [1] https://github.com/rollup/rollup/issues/4953
    [2] https://github.com/svg/svgo/issues/1672
    [3] https://source.chromium.org/chromium/chromium/src/+/main:tools/resources/svgo.config.js;l=17-18;drc=4046a6ad0dfa25c3ec4ff3a0f2019badcd4ede70
    [4] https://github.com/svg/svgo/commit/6295c60fbfcc8325bf8c089ecd27cad7e5567f3d
    
    Bug: None
    Change-Id: I12ca6f1204868a8592d03b24852c12c97e4e5b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908394
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1204712}

--- tools/resources/svgo_presubmit_test.py
+++ tools/resources/svgo_presubmit_test.py
@@ -16,8 +16,8 @@ from resources import svgo_presubmit
 from PRESUBMIT_test_mocks import MockInputApi, MockOutputApi, MockFile
 
 _OPTIMIZED_SVG = (
-    b'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" ' +
-    b'id="EXPORT_preserved_id" viewBox="0 0 24 24" fill="#757575"><path ' +
+    b'<svg xmlns="http://www.w3.org/2000/svg" id="EXPORT_preserved_id" ' +
+    b'width="24" height="24" fill="#757575" viewBox="0 0 24 24"><path ' +
     b'd="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 ' +
     b'2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg>')
 
