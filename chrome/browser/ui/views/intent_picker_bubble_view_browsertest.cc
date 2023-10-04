571ba56d11a133072431bdfde6db8ed3544a77ca
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2023-10-04 04:17:37 +0000

    sheriff: Disable IntentPickerIconBrowserTest.PushStateURLChangeTest
    Test is really flaky on macOS:
    
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FIntentPickerIconBrowserTest.PushStateURLChangeTest
    
    Bug: 1484208
    Change-Id: Ib35164330bedbb4734ebf76dc934cf142dfbd7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905019
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205035}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -338,7 +338,14 @@ IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
 
 // Test that loading a page with pushState() call that changes URL updates the
 // intent picker view.
-IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest, PushStateURLChangeTest) {
+// TODO(crbug.com/1484208): Re-enable this test
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_PushStateURLChangeTest DISABLED_PushStateURLChangeTest
+#else
+#define MAYBE_PushStateURLChangeTest PushStateURLChangeTest
+#endif
+IN_PROC_BROWSER_TEST_F(IntentPickerIconBrowserTest,
+                       MAYBE_PushStateURLChangeTest) {
   // Note: The test page is served from embedded_test_server() as https_server()
   // always returns empty responses.
   ASSERT_TRUE(embedded_test_server()->Start());
