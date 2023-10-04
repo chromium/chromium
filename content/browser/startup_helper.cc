9b63ea55c9c817cb5b6318af20463d4712b4da9a
Minoru Chikamune <chikamune@chromium.org>
2023-10-04 05:27:30 +0000

    MiracleParameter: Make the browser's thread pool count configurable with miracle parameter
    This CL replaces base::FeatureParam with MIRACLE_PARAMETER_FOR_* so that
    we can configure the parameter based on the physical memory.
    
    Bug: 1475915
    Change-Id: Ie3d178bb97886d63701c72b47c2f54fb9eea9269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4896045
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205056}

--- content/browser/startup_helper.cc
+++ content/browser/startup_helper.cc
@@ -11,11 +11,11 @@
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
-#include "base/metrics/field_trial_params.h"
 #include "base/system/sys_info.h"
 #include "base/task/thread_pool/initialization_util.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "build/build_config.h"
+#include "components/miracle_parameter/common/public/miracle_parameter.h"
 #include "content/common/thread_pool_util.h"
 #include "content/public/common/content_switch_dependent_feature_overrides.h"
 #include "content/public/common/content_switches.h"
@@ -53,26 +53,35 @@ BASE_FEATURE(kBrowserThreadPoolAdjustment,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // For iOS see ios/web/app/web_main_loop.cc.
-constexpr base::FeatureParam<int> kBrowserThreadPoolMin {
-  &kBrowserThreadPoolAdjustment, "BrowserThreadPoolMin",
+MIRACLE_PARAMETER_FOR_INT(GetBrowserThreadPoolMin,
+                          kBrowserThreadPoolAdjustment,
+                          "BrowserThreadPoolMin",
 #if BUILDFLAG(IS_ANDROID)
-      6
+                          6
 #else
-      16
+                          16
 #endif
-};
-constexpr base::FeatureParam<int> kBrowserThreadPoolMax {
-  &kBrowserThreadPoolAdjustment, "BrowserThreadPoolMax",
+)
+
+MIRACLE_PARAMETER_FOR_INT(GetBrowserThreadPoolMax,
+                          kBrowserThreadPoolAdjustment,
+                          "BrowserThreadPoolMax",
 #if BUILDFLAG(IS_ANDROID)
-      8
+                          8
 #else
-      32
+                          32
 #endif
-};
-constexpr base::FeatureParam<double> kBrowserThreadPoolCoresMultiplier{
-    &kBrowserThreadPoolAdjustment, "BrowserThreadPoolCoresMultiplier", 0.6};
-constexpr base::FeatureParam<int> kBrowserThreadPoolOffset{
-    &kBrowserThreadPoolAdjustment, "BrowserThreadPoolOffset", 0};
+)
+
+MIRACLE_PARAMETER_FOR_DOUBLE(GetBrowserThreadPoolCoresMultiplier,
+                             kBrowserThreadPoolAdjustment,
+                             "BrowserThreadPoolCoresMultiplier",
+                             0.6)
+
+MIRACLE_PARAMETER_FOR_INT(GetBrowserThreadPoolOffset,
+                          kBrowserThreadPoolAdjustment,
+                          "BrowserThreadPoolOffset",
+                          0)
 
 }  // namespace
 
@@ -81,12 +90,11 @@ constexpr base::FeatureParam<int> kBrowserThreadPoolOffset{
 void StartBrowserThreadPool() {
   // Ensure we always support at least one thread regardless of the field trial
   // param setting.
-  auto min = static_cast<size_t>(std::max(kBrowserThreadPoolMin.Get(), 1));
+  auto min = static_cast<size_t>(std::max(GetBrowserThreadPoolMin(), 1));
   base::ThreadPoolInstance::InitParams thread_pool_init_params = {
       base::RecommendedMaxNumberOfThreadsInThreadGroup(
-          min, kBrowserThreadPoolMax.Get(),
-          kBrowserThreadPoolCoresMultiplier.Get(),
-          kBrowserThreadPoolOffset.Get())};
+          min, GetBrowserThreadPoolMax(), GetBrowserThreadPoolCoresMultiplier(),
+          GetBrowserThreadPoolOffset())};
 
 #if BUILDFLAG(IS_WIN)
   thread_pool_init_params.common_thread_pool_environment = base::
