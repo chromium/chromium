f670ee27cebb65a61d7188f9f7d57efda1894f2b
Maggie Cai <mxcai@chromium.org>
2023-10-05 04:34:56 +0000

    Revert "[LCPP] Record NavigationToLargestContentfulPaint UMA when prerendered"
    This reverts commit d4e4ecbff0ee64b3b6eb0f6311b4448c087d3596.
    
    Reason for revert: This CL is likely the cause of test failure LcpCriticalPathPredictorPageLoadMetricsObserverTest.MetricsNotRecordedWithoutHint in builder https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20(Wayland) starting from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(Wayland)/44195/overview
    
    Original change's description:
    > [LCPP] Record NavigationToLargestContentfulPaint UMA when prerendered
    >
    > This CL lets LcpCriticalPathPredictorPageLoadMetricsObserver
    > - not learn LCPP when the page is prerendered to avoid data skew.
    > - report the UMA when the page is prerendered and activated.
    >
    > Bug: 1468188
    > Change-Id: I99b864b941dc7281cde471bd4bf5239a80391c55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4891302
    > Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    > Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1205610}
    
    Bug: 1468188
    Change-Id: I11de80f5d8e47a95aa15b7e2dd637882a41ef47b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914833
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1205645}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.h
@@ -81,9 +81,6 @@ class LcpCriticalPathPredictorPageLoadMetricsObserver
   void OnFirstContentfulPaintInPage(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
 
-  // True if the page is prerendered.
-  bool is_prerender_ = false;
-
   // The URL of the last navigation commit.
   absl::optional<GURL> commit_url_;
 
