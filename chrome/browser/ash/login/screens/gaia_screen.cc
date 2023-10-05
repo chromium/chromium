258f83e0252548e2b0775fc4e15cde9984f3b880
Yunke Zhou <yunkez@google.com>
2023-10-05 14:58:33 +0000

    Disallow passwordless login when Gaia password is configured
    Bug: b/238181338
    Change-Id: I7c7833bfe5d9acdf4a8d09b1a50d8b8d8c3aeb97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914382
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205834}

--- chrome/browser/ash/login/screens/gaia_screen.cc
+++ chrome/browser/ash/login/screens/gaia_screen.cc
@@ -22,6 +22,7 @@
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/enterprise/util/managed_browser_utils.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_screen_handler.h"
+#include "chromeos/ash/services/auth_factor_config/auth_factor_config_utils.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/known_user.h"
 #include "components/user_manager/user.h"
@@ -173,6 +174,7 @@ void GaiaScreen::Reset() {
     return;
   context()->gaia_config.gaia_path = WizardContext::GaiaPath::kDefault;
   context()->gaia_config.prefilled_account = EmptyAccountId();
+  view_->SetIsGaiaPasswordRequired(false);
   view_->Reset();
 }
 
@@ -305,16 +307,25 @@ void GaiaScreen::HandleIdentifierEntered(const std::string& user_email) {
 void GaiaScreen::OnGetAuthFactorsConfiguration(
     std::unique_ptr<UserContext> user_context,
     absl::optional<AuthenticationError> error) {
-  bool is_recovery_configured;
+  bool is_recovery_configured = false;
+  bool is_gaia_password_configured = true;
   if (error.has_value()) {
     LOG(WARNING) << "Failed to get auth factors configuration, code "
                  << error->get_cryptohome_code()
                  << ", skip fetching reauth request token";
-    is_recovery_configured = false;
   } else {
     const auto& config = user_context->GetAuthFactorsConfiguration();
     is_recovery_configured =
         config.HasConfiguredFactor(cryptohome::AuthFactorType::kRecovery);
+    auto* password_factor =
+        config.FindFactorByType(cryptohome::AuthFactorType::kPassword);
+    is_gaia_password_configured =
+        password_factor && auth::IsGaiaPassword(*password_factor);
+  }
+
+  if (is_gaia_password_configured) {
+    // Disallow passwordless login when Gaia password is configured.
+    view_->SetIsGaiaPasswordRequired(true);
   }
 
   const AccountId& account_id = user_context->GetAccountId();
