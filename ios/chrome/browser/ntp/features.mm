57de97996bd0a3d4bea6f2dc51dd92f43a5e5f02
adamta <adamta@google.com>
2023-10-05 16:43:50 +0000

    [iOS] Enable ACR by default and extend flag expiry
    Bug: 1489651
    Change-Id: I06eb6d10db27fb5dd87c63e69fd741bd89ed9442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916430
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205886}

--- ios/chrome/browser/ntp/features.mm
+++ ios/chrome/browser/ntp/features.mm
@@ -54,7 +54,7 @@ BASE_FEATURE(kEnableFeedBackgroundRefresh,
 
 BASE_FEATURE(kEnableFeedInvisibleForegroundRefresh,
              "EnableFeedInvisibleForegroundRefresh",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCreateDiscoverFeedServiceEarly,
              "CreateDiscoverFeedServiceEarly",
@@ -270,7 +270,7 @@ bool IsFeedAppCloseForegroundRefreshEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kEnableFeedInvisibleForegroundRefresh,
       kEnableFeedAppCloseForegroundRefresh,
-      /*default=*/false);
+      /*default=*/true);
 }
 
 bool IsFeedAppCloseBackgroundRefreshEnabled() {
