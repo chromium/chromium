09fd68a507bcdc09d7723daecb25f03a5c203e96
mmrashad <mmrashad@google.com>
2023-10-05 13:30:35 +0000

    [Code Health][iOS][1/3] Move ios/c/b/sessions to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480351
    Change-Id: I717035b4e248f4aa89277c2eedf8bdccefd3b9d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915611
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1205798}

---
+++ ios/chrome/browser/sessions/model/test_session_service.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SESSIONS_MODEL_TEST_SESSION_SERVICE_H_
+#define IOS_CHROME_BROWSER_SESSIONS_MODEL_TEST_SESSION_SERVICE_H_
+
+#import "ios/chrome/browser/sessions/test_session_service.h"
+
+#endif  // IOS_CHROME_BROWSER_SESSIONS_MODEL_TEST_SESSION_SERVICE_H_
