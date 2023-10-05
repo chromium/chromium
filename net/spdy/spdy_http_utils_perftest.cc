39a6684997e175d4e1f7dee973f2af5d91f541fe
Adam Rice <ricea@chromium.org>
2023-10-05 09:05:44 +0000

    Add benchmark for CreateSpdyHeadersFromHttpRequest()
    of HTTP/2 and HTTP/3. Create a benchmark for it.
    
    net: :CreateSpdyHeadersFromHttpRequest() is important to the performance
    Change-Id: I1f364f3ad62d2260bcdec0e8b43d2d000d6da35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902677
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205723}

--- net/spdy/spdy_http_utils_perftest.cc
+++ net/spdy/spdy_http_utils_perftest.cc
@@ -5,9 +5,12 @@
 #include "net/spdy/spdy_http_utils.h"
 
 #include "base/memory/ref_counted.h"
+#include "net/http/http_request_headers.h"
+#include "net/http/http_request_info.h"
 #include "net/http/http_response_headers.h"
 #include "net/third_party/quiche/src/quiche/spdy/core/http2_header_block.h"
 #include "third_party/google_benchmark/src/include/benchmark/benchmark.h"
+#include "url/gurl.h"
 
 namespace net {
 
@@ -72,6 +75,49 @@ BENCHMARK(Benchmark<SpdyHeadersToHttpResponseHeadersUsingRawString>)
     ->MinWarmUpTime(1.0);
 BENCHMARK(Benchmark<SpdyHeadersToHttpResponseHeadersUsingBuilder>)
     ->MinWarmUpTime(1.0);
+
+void BM_CreateSpdyHeadersFromHttpRequest(::benchmark::State& state) {
+  HttpRequestInfo info;
+  info.url = GURL("https://en.wikipedia.org/wiki/HTTP");
+  info.method = "GET";
+  HttpRequestHeaders http_request_headers;
+  http_request_headers.SetHeader(
+      "Accept",
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/"
+      "webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+  http_request_headers.SetHeader("Accept-Encoding", "gzip, deflate, br");
+  http_request_headers.SetHeader("Accept-Language", "en-GB,en;q=0.9");
+  http_request_headers.SetHeader("Cache-Control", "max-age=0");
+  http_request_headers.SetHeader(
+      "Cookie",
+      "WMF-Last-Access=xxxxxxxxxxx; WMF-Last-Access-Global=xxxxxxxxxxx; "
+      "GeoIP=xxxxxxxxxxxxxxxxxxxxxxxxxxx; NetworkProbeLimit=0.001; "
+      "enwikimwuser-sessionId=xxxxxxxxxxxxxxxxxxxx");
+  http_request_headers.SetHeader(
+      "Sec-Ch-Ua",
+      "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", "
+      "\"Chromium\";v=\"117\"");
+  http_request_headers.SetHeader("Sec-Ch-Ua-Mobile", "?0");
+  http_request_headers.SetHeader("Sec-Ch-Ua-Platform", "\"Linux\"");
+  http_request_headers.SetHeader("Sec-Fetch-Dest", "document");
+  http_request_headers.SetHeader("Sec-Fetch-Mode", "navigate");
+  http_request_headers.SetHeader("Sec-Fetch-Site", "none");
+  http_request_headers.SetHeader("Sec-Fetch-User", "?1");
+  http_request_headers.SetHeader("Upgrade-Insecure-Requests", "1");
+  http_request_headers.SetHeader(
+      "User-Agent",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+      "Chrome/117.0.0.0 Safari/537.36");
+
+  for (auto _ : state) {
+    spdy::Http2HeaderBlock headers;
+    CreateSpdyHeadersFromHttpRequest(info, RequestPriority::DEFAULT_PRIORITY,
+                                     http_request_headers, &headers);
+    ::benchmark::DoNotOptimize(headers);
+  }
+}
+
+BENCHMARK(BM_CreateSpdyHeadersFromHttpRequest)->MinWarmUpTime(1.0);
 
 }  // namespace
 
