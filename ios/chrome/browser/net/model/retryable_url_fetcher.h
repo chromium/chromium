79ee29462e66a91b1badb1f030ceaac56dc7fd0e
mmrashad <mmrashad@google.com>
2023-10-05 13:30:59 +0000

    [Code Health][iOS][1/3] Move ios/c/b/net to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480322
    Change-Id: Ic274de01a01808713bac14d0c88467a12806e9b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912921
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205799}

---
+++ ios/chrome/browser/net/model/retryable_url_fetcher.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NET_MODEL_RETRYABLE_URL_FETCHER_H_
+#define IOS_CHROME_BROWSER_NET_MODEL_RETRYABLE_URL_FETCHER_H_
+
+#import "ios/chrome/browser/net/retryable_url_fetcher.h"
+
+#endif  // IOS_CHROME_BROWSER_NET_MODEL_RETRYABLE_URL_FETCHER_H_
