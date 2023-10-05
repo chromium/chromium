34c2b34be987c248a60d9b71f54ac9673fdc9bf3
Haiyang Pan <hypan@google.com>
2023-10-05 21:54:09 +0000

    Revert "[CIC] Account height of content view for omnibox suggestion list"
    This reverts commit a3ac64b545154a8dfff89c8334e761f9756fa023.
    
    Reason for revert: Break tests on android-oreo-x86-rel.
    Example build: https://ci.chromium.org/ui/p/chromium/builders/try/android-x86-rel/1650/overview
    
    Original change's description:
    > [CIC] Account height of content view for omnibox suggestion list
    >
    > Omnibox suggestion used to assume that the content view is always at the top of the window in #recalculateOmniboxAlignment. This is not the case on automotive since there's a back button bar on the top.
    >
    > This CL fix the issue by checking the available space left in window v.s. the size of the content view.
    >
    > Screenshots for preview see b/290104585#comment12
    >
    > Bug: b/290104585
    > Change-Id: Icf7c321522f1732c9a3c419a6e2afdf100695113
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914084
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Charles Hager <clhager@google.com>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1206016}
    
    Bug: b/290104585
    Change-Id: Idcfd53c7395a8146def83b07432c6d8e140836c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916204
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1206069}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxSuggestionsDropdownEmbedderImpl.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxSuggestionsDropdownEmbedderImpl.java
@@ -215,9 +215,8 @@ class OmniboxSuggestionsDropdownEmbedderImpl implements OmniboxSuggestionsDropdo
         int mKeyboardHeight = mDeferredIMEWindowInsetApplicationCallback != null
                 ? mDeferredIMEWindowInsetApplicationCallback.getCurrentKeyboardHeight()
                 : 0;
-        int windowSpace =
-                DisplayUtil.dpToPx(mWindowAndroid.getDisplay(), mWindowHeightDp) - mKeyboardHeight;
-        int height = Math.min(windowSpace, contentView.getMeasuredHeight()) - top;
+        int height = DisplayUtil.dpToPx(mWindowAndroid.getDisplay(), mWindowHeightDp) - top
+                - mKeyboardHeight;
 
         // TODO(pnoland@, https://crbug.com/1416985): avoid pushing changes that are identical to
         // the previous alignment value.
