f935d237563a49dad3348a1981053ae5785ce2b6
Adrian Taylor <adetaylor@chromium.org>
2023-10-05 20:25:28 +0000

    Fuzz FormatURL.
    This approximately doubles the coverage of the existing fuzzer.
    
    Bug: 1485103
    Change-Id: I8eedf7e734d297f480a2079945395774865d51c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915613
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206022}

--- components/url_formatter/url_fixer_fuzzer.cc
+++ components/url_formatter/url_fixer_fuzzer.cc
@@ -14,9 +14,11 @@
 #include "base/files/file_path.h"
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
+#include "base/strings/escape.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/url_formatter/url_fixer.h"
+#include "components/url_formatter/url_formatter.h"
 #include "url/third_party/mozilla/url_parse.h"
 
 namespace {
@@ -48,7 +50,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static Environment env;
   FuzzedDataProvider provider(data, size);
 
-  switch (provider.ConsumeIntegralInRange<int>(0, 3)) {
+  switch (provider.ConsumeIntegralInRange<int>(0, 4)) {
     case 0: {
       std::string text = provider.ConsumeRandomLengthString();
       url::Parsed parts;
@@ -71,6 +73,25 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     case 3: {
       std::ignore = url_formatter::FixupRelativeFile(
           GenerateFuzzedFilePath(provider), GenerateFuzzedFilePath(provider));
+      break;
+    }
+    case 4: {
+      url::Parsed parsed;
+      GURL unparsed(provider.ConsumeRandomLengthString());
+      // Get any valid bitmask of the FormatUrlType options
+      const uint32_t kCurrentMaxFormatUrlType =
+          url_formatter::kFormatUrlOmitMobilePrefix;
+      url_formatter::FormatUrlType format_url_type =
+          provider.ConsumeIntegralInRange(
+              0U, ((kCurrentMaxFormatUrlType << 1) - 1));
+      // Get any valid bitmask of the UnescapeRule types
+      const uint32_t kCurrentMaxUnescapeRuleType =
+          base::UnescapeRule::REPLACE_PLUS_WITH_SPACE;
+      base::UnescapeRule::Type unescape_rule_type =
+          provider.ConsumeIntegralInRange(
+              0U, ((kCurrentMaxUnescapeRuleType << 1) - 1));
+      url_formatter::FormatUrl(unparsed, format_url_type, unescape_rule_type,
+                               &parsed, nullptr, nullptr);
       break;
     }
   }
