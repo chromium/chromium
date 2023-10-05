aa89570dfcca14297802650d7c2aabf1f022e563
cassycc <cassycc@google.com>
2023-10-05 03:57:34 +0000

    Files app: Convert volume_manager_factory to TS
    Bug: b/289003444
    Change-Id: I5b204cb5f1638ad61f5d1a6c77b3a25ba23dd903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4914534
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205641}

--- ui/file_manager/file_manager/background/js/volume_manager_factory.ts
+++ ui/file_manager/file_manager/background/js/volume_manager_factory.ts
@@ -10,23 +10,17 @@ const volumeManagerFactory = (() => {
   /**
    * The singleton instance of VolumeManager. Initialized by the first
    * invocation of getInstance().
-   * @type {?VolumeManagerImpl}
    */
-  let instance = null;
+  let instance: VolumeManagerImpl|null = null;
 
-  /**
-   * @type {?Promise<void>}
-   */
-  let instanceInitialized = null;
+  let instanceInitialized: Promise<void>|null = null;
 
   /**
    * Returns the VolumeManager instance asynchronously. If it has not been
    * created or is under initialization, it will waits for the finish of the
    * initialization.
-   * @return {!Promise<!VolumeManager>} Promise to be fulfilled with the volume
-   *     manager.
    */
-  async function getInstance() {
+  async function getInstance(): Promise<VolumeManager> {
     if (!instance) {
       instance = new VolumeManagerImpl();
       instanceInitialized = instance.initialize();
@@ -39,9 +33,8 @@ const volumeManagerFactory = (() => {
    * Returns instance of VolumeManager for debug purpose.
    * This method returns VolumeManager.instance which may not be initialized.
    *
-   * @return {VolumeManager} Volume manager.
    */
-  function getInstanceForDebug() {
+  function getInstanceForDebug(): VolumeManager|null {
     return instance;
   }
 
