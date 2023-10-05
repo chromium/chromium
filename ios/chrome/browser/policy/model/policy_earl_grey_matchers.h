265dd332536d793a648764f0afe6cb05c85e8b85
mmrashad <mmrashad@google.com>
2023-10-05 14:07:17 +0000

    [Code Health][iOS][1/3] Move ios/c/b/policy to model subfolder
    Creates upstream headers to move downstream deps before the move.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852, 1480332
    Change-Id: I7a9eed8fb28ab3226b111766d778f69a104357f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912941
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1205820}

---
+++ ios/chrome/browser/policy/model/policy_earl_grey_matchers.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_EARL_GREY_MATCHERS_H_
+#define IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_EARL_GREY_MATCHERS_H_
+
+#import "ios/chrome/browser/policy/policy_earl_grey_matchers.h"
+
+#endif  // IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_EARL_GREY_MATCHERS_H_
---
+++ ios/chrome/browser/policy/model/policy_util.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_UTIL_H_
+#define IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_UTIL_H_
+
+#import "ios/chrome/browser/policy/policy_util.h"
+
+#endif  // IOS_CHROME_BROWSER_POLICY_MODEL_POLICY_UTIL_H_
