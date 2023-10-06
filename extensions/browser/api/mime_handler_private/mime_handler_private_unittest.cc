8ea6a291e86f73005b5e09c06e8b18e90d839dfa
Andy Phan <andyphan@chromium.org>
2023-10-06 21:35:55 +0000

    [PDF Viewer] Remove unused use_skia field
    Remove an unused use_skia field from PdfPluginAttributes.
    
    PdfPluginAttributes is used to send attributes to generate a PDF plugin
    load response from the PDF extension to the browser. However, the
    use_skia field is never set or used. The browser determines whether
    Skia should be used, and directly writes it into the response in
    pdf::PluginResponseWriter, without using PdfPluginAttributes for
    use_skia.
    
    There should be no change in behavior.
    
    Change-Id: I24e25df7e572e0ef392dabd895c3ce0d05e01f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917154
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Nigi <nigi@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1206648}

--- extensions/browser/api/mime_handler_private/mime_handler_private_unittest.cc
+++ extensions/browser/api/mime_handler_private/mime_handler_private_unittest.cc
@@ -51,38 +51,32 @@ TEST_F(MimeHandlerServiceImplTest, SetValidPdfPluginAttributes) {
   {
     const double kBackgroundColor = 4292533472.0f;
     service_->SetPdfPluginAttributes(mime_handler::PdfPluginAttributes::New(
-        /*background_color=*/kBackgroundColor, /*allow_javascript=*/true,
-        /*use_skia=*/true));
+        /*background_color=*/kBackgroundColor, /*allow_javascript=*/true));
     ASSERT_TRUE(stream_container_->pdf_plugin_attributes());
 
     EXPECT_EQ(kBackgroundColor,
               stream_container_->pdf_plugin_attributes()->background_color);
     EXPECT_TRUE(stream_container_->pdf_plugin_attributes()->allow_javascript);
-    EXPECT_TRUE(stream_container_->pdf_plugin_attributes()->use_skia);
   }
 
   {
     service_->SetPdfPluginAttributes(mime_handler::PdfPluginAttributes::New(
-        /*background_color=*/0.0f, /*allow_javascript=*/true,
-        /*use_skia=*/false));
+        /*background_color=*/0.0f, /*allow_javascript=*/true));
     ASSERT_TRUE(stream_container_->pdf_plugin_attributes());
 
     EXPECT_EQ(0.0f,
               stream_container_->pdf_plugin_attributes()->background_color);
     EXPECT_TRUE(stream_container_->pdf_plugin_attributes()->allow_javascript);
-    EXPECT_FALSE(stream_container_->pdf_plugin_attributes()->use_skia);
   }
 
   {
     service_->SetPdfPluginAttributes(mime_handler::PdfPluginAttributes::New(
-        /*background_color=*/UINT32_MAX, /*allow_javascript=*/false,
-        /*use_skia=*/true));
+        /*background_color=*/UINT32_MAX, /*allow_javascript=*/false));
     ASSERT_TRUE(stream_container_->pdf_plugin_attributes());
 
     EXPECT_EQ(static_cast<double>(UINT32_MAX),
               stream_container_->pdf_plugin_attributes()->background_color);
     EXPECT_FALSE(stream_container_->pdf_plugin_attributes()->allow_javascript);
-    EXPECT_TRUE(stream_container_->pdf_plugin_attributes()->use_skia);
   }
 }
 
@@ -91,8 +85,7 @@ TEST_F(MimeHandlerServiceImplTest,
   {
     // Background is not an integer.
     service_->SetPdfPluginAttributes(mime_handler::PdfPluginAttributes::New(
-        /*background_color=*/12.34, /*allow_javascript=*/true,
-        /*use_skia=*/true));
+        /*background_color=*/12.34, /*allow_javascript=*/true));
     EXPECT_FALSE(stream_container_->pdf_plugin_attributes());
   }
 
@@ -100,8 +93,7 @@ TEST_F(MimeHandlerServiceImplTest,
     // Background color is beyond the range of an uint32_t.
     uint64_t color_beyond_range = UINT32_MAX + static_cast<uint64_t>(1);
     service_->SetPdfPluginAttributes(mime_handler::PdfPluginAttributes::New(
-        static_cast<double>(color_beyond_range), /*allow_javascript=*/true,
-        /*use_skia=*/true));
+        static_cast<double>(color_beyond_range), /*allow_javascript=*/true));
     EXPECT_FALSE(stream_container_->pdf_plugin_attributes());
   }
 }
