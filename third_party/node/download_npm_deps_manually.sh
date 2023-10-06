95bb60bf7fd3d18f469f050b60663b3dbdfa0402
dpapad <dpapad@chromium.org>
2023-10-06 16:59:32 +0000

    WebUI: Add ability to manually download NPM deps, without `npm install`.
     - Add new download_npm_deps_manually.sh script.
     - Hook it up to third_party/node/update_npm_deps
     - Update npm_include.txt, npm_exclude.txt accordingly.
     - Port third_party/chaijs/chromium.patch.
     - Fix minor typo in third_party/node/typescript.patch (left-over
       from a previous CL).
    
    This is in preparation of removing third_party/{chaijs, mocha} folders
    in favor of third_party/node/node_modules/. The stand-alone folders
    provided no automated way of updating to newer versions, and whoever
    rolled a new version had to manually find, download and check-in the
    new files, which was cumbersome and error-prone.
    
    By moving these dependencies to third_party/node/node_modules, they
    can be updated similarly to other WebUI toolchain dependencies using the
    third_party/node/update_npm_deps mechanism.
    
    The updated third_party/node/update_npm_deps script is intentionally not
    triggered in the same CL, and will be done in a follow-up.
    
    Bug: 1490291
    Change-Id: I807d3ebd6aac809a5dacb6d74731b8abf881128d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917178
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206471}

---
+++ third_party/node/download_npm_deps_manually.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script for downloading some WebUI's NPM deps without using `npm install`.
+# Unfortunately `npm install` downloads a lot of unnecessary dependencies in
+# many cases. This is especially true for cases like 'mocha'and 'chai', which
+# even though come already bundled, NPM still downloads about ~8MB of
+# unnecessary dependencies.
+#
+# To address the problem above and avoid blowing up the size of the generated
+# node_modules.tar.gz file, this script directly downloads just the necessary
+# packages, since NPM does not provide such functionality built-in, see
+# https://github.com/npm/npm/issues/340.
+
+set -eu
+
+tmp_dir="node_modules_manual"
+mkdir -p "${tmp_dir}"
+
+CHAI_VERSION="4.2.0"
+MOCHA_VERSION="10.0.0"
+
+download_package() {
+  local PACKAGE="$1"
+  local VERSION="$2"
+
+  # Manually download from NPM.
+  npm pack --silent ${PACKAGE}@${VERSION} --pack-destination ${tmp_dir}
+
+  # Extract.
+  tar xfz ${tmp_dir}/${PACKAGE}-${VERSION}.tgz -C ${tmp_dir}
+
+  # Delete destination folder if it already exists.
+  rm -rf node_modules/${PACKAGE}
+
+  # Place in node_modules/ folder. Any further filtering of files happens in the
+  # parent script.
+  mv ${tmp_dir}/package node_modules/${PACKAGE}
+}
+
+download_package "chai" ${CHAI_VERSION}
+rm -rf ${tmp_dir}/package
+download_package "mocha" ${MOCHA_VERSION}
+
+# Clean up.
+rm -rf "$tmp_dir"
