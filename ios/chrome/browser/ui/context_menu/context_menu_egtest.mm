b5133d383b86af65225d98e157c63d25c6e8958c
Olivier Robin <olivierrobin@chromium.org>
2023-10-06 07:39:02 +0000

    Revert "[iOS] Disable ContextMenuTestCase/testOpenLinkInNewWindow"
    This reverts commit 9ba356bdb97228a59d6973f3216acdb4a6cb9628.
    
    Reason for revert: Underlying cause is fixed (crbug/1488446)
    
    Original change's description:
    > [iOS] Disable ContextMenuTestCase/testOpenLinkInNewWindow
    >
    > This test is flaky on both device and simulator.
    >
    > Bug: 1487586
    > Change-Id: Ib764f1c3f560bec79f6aa5994be9d3d5be16b7c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4898882
    > Auto-Submit: Ali Juma <ajuma@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Owners-Override: Ali Juma <ajuma@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1202655}
    
    Bug: 1487586
    Change-Id: I15340117e4c434e2ce9f7e0abe894a0dce98e269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4915932
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1206266}

--- ios/chrome/browser/ui/context_menu/context_menu_egtest.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_egtest.mm
@@ -512,9 +512,7 @@ void TapOnContextMenuButton(id<GREYMatcher> context_menu_item_button) {
 
 // Checks that "open in new window" shows up on a long press of a url link
 // and that it actually opens in a new window.
-// TODO(crbug.com/1487586): This test is very flaky on both device and
-// simulator.
-- (void)DISABLED_testOpenLinkInNewWindow {
+- (void)testOpenLinkInNewWindow {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
 
