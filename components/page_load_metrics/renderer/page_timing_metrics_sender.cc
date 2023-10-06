027ad38d4c6118c2e9cbfdc267a7a8cd647f16ed
Ian Clelland <iclelland@chromium.org>
2023-10-06 05:43:58 +0000

    Fix SPA navigation CHECKs
    Bug: 1489583
    Change-Id: Ib08d55f053c63678acdb2fe65b6c21da8ccc3574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916975
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206238}

--- components/page_load_metrics/renderer/page_timing_metrics_sender.cc
+++ components/page_load_metrics/renderer/page_timing_metrics_sender.cc
@@ -117,13 +117,14 @@ void PageTimingMetricsSender::DidObserveNewFeatureUsage(
 void PageTimingMetricsSender::DidObserveSoftNavigation(
     blink::SoftNavigationMetrics new_metrics) {
   // The start_time is a TimeDelta, and its resolution is in microseconds.
-  // Therefore each soft navigation would have an effectively larger start_time
-  // than the one that came before it. Each soft navigation should also have a
-  // larger count and a different navigation id.
+  // Successive soft navigations could have the same start time, if the inputs
+  // which triggered them started at the same time, but we should not see an
+  // earlier start time for a later soft navigation. Each soft navigation should
+  // also have a larger count and a different navigation id than the previous
+  // one.
   CHECK(new_metrics.count > soft_navigation_metrics_->count);
   CHECK(!new_metrics.start_time.is_zero());
-  CHECK(new_metrics.start_time != soft_navigation_metrics_->start_time);
-  CHECK(new_metrics.start_time > soft_navigation_metrics_->start_time);
+  CHECK(new_metrics.start_time >= soft_navigation_metrics_->start_time);
   CHECK(new_metrics.navigation_id != soft_navigation_metrics_->navigation_id);
 
   soft_navigation_metrics_->count = new_metrics.count;
