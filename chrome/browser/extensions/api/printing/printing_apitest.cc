f5234bd83373e670767cebb8e58b7ce6255e4894
Tim Judkins <tjudkins@chromium.org>
2023-10-07 01:34:14 +0000

    [Extensions] Refactor printing API tests to include MV3 extension type
    This is mostly setup for cleanly adding promise based API tests in a
    followup.
    
    Note: There are no CQ builders that properly run this test as it
    requires a chromeos builder with "use_cups = true". It has been tested
    locally on a build with those args using the browser_tests target and
    --gtest_filter="PrintingApiTest.*"
    
    Bug: 1422837
    Change-Id: I9ee259310fd08d7a572005fae377edb955851102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4916969
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1206721}

--- chrome/browser/extensions/api/printing/printing_apitest.cc
+++ chrome/browser/extensions/api/printing/printing_apitest.cc
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
+#include "base/containers/fixed_flat_map.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
@@ -51,6 +54,23 @@ constexpr int kMediaSizeWidth = 210000;
 constexpr int kMediaSizeHeight = 297000;
 constexpr char kMediaSizeVendorId[] = "iso_a4_210x297mm";
 
+// Enum used to initialize the parameterized test with different types of
+// extensions.
+enum class ExtensionType {
+  kChromeApp,
+  kExtensionMV2,
+  kExtensionMV3,
+};
+
+// Mapping of the different extension types used in the test to the specific
+// manifest file names to create an extension of that type. The actual location
+// of these files is at //chrome/test/data/extensions/api_test/printing/.
+static constexpr auto kManifestFileNames =
+    base::MakeFixedFlatMap<ExtensionType, const char*>(
+        {{ExtensionType::kChromeApp, "manifest_chrome_app.json"},
+         {ExtensionType::kExtensionMV2, "manifest_extension.json"},
+         {ExtensionType::kExtensionMV3, "manifest_v3_extension.json"}});
+
 std::unique_ptr<KeyedService> BuildTestCupsPrintJobManager(
     content::BrowserContext* context) {
   return std::make_unique<ash::TestCupsPrintJobManager>(
@@ -81,7 +101,7 @@ ConstructPrinterCapabilities() {
 }  // namespace
 
 class PrintingApiTest : public ExtensionApiTest,
-                        public testing::WithParamInterface<bool> {
+                        public testing::WithParamInterface<ExtensionType> {
  public:
   PrintingApiTest() = default;
   ~PrintingApiTest() override = default;
@@ -153,12 +173,11 @@ class PrintingApiTest : public ExtensionApiTest,
                           dir.UnpackedPath(), /*recursive=*/false);
       base::CopyFile(
           test_data_dir_.AppendASCII("printing")
-              .AppendASCII(IsChromeApp() ? "manifest_chrome_app.json"
-                                         : "manifest_extension.json"),
+              .AppendASCII(kManifestFileNames.at(GetExtensionType())),
           dir.UnpackedPath().AppendASCII(extensions::kManifestFilename));
     }
 
-    auto run_options = IsChromeApp()
+    auto run_options = GetExtensionType() == ExtensionType::kChromeApp
                            ? RunOptions{.custom_arg = html_test_page,
                                         .launch_as_platform_app = true}
                            : RunOptions({.extension_url = html_test_page});
@@ -166,7 +185,7 @@ class PrintingApiTest : public ExtensionApiTest,
   }
 
  private:
-  bool IsChromeApp() const { return GetParam(); }
+  ExtensionType GetExtensionType() const { return GetParam(); }
 
   void OnWillCreateBrowserContextServices(content::BrowserContext* context) {
     ash::CupsPrintJobManagerFactory::GetInstance()->SetTestingFactory(
@@ -238,7 +257,10 @@ IN_PROC_BROWSER_TEST_P(PrintingApiTest, CancelJob) {
   RunTest("cancel_job.html");
 }
 
-// |true| for Chrome App, |false| for Extension.
-INSTANTIATE_TEST_SUITE_P(/**/, PrintingApiTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(/**/,
+                         PrintingApiTest,
+                         testing::Values(ExtensionType::kChromeApp,
+                                         ExtensionType::kExtensionMV2,
+                                         ExtensionType::kExtensionMV3));
 
 }  // namespace extensions
