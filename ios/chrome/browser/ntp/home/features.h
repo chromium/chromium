c8e0d079d2be1696189601952cb2081fee50a866
Ewann Pelle <ewannpv@chromium.org>
2023-10-09 13:56:51 +0000

    [iOS][[1/3] Move content suggestions features
    Creates upstream headers to move downstream deps before the move.
    
    Moves i/c/b/ui/content_suggestions/content_suggestions_feature
    in i/c/b/ntp/home/features.
    
    Bug: 1491019
    Change-Id: If3e15161325d4c981dd09ef77c92bfad6218d3db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4921769
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207056}

---
+++ ios/chrome/browser/ntp/home/features.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NTP_HOME_FEATURES_H_
+#define IOS_CHROME_BROWSER_NTP_HOME_FEATURES_H_
+
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
+
+#endif  // IOS_CHROME_BROWSER_NTP_HOME_FEATURES_H_
