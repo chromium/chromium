74987c85ea5c507b1d8f186b995d2c990e00c236
Mahmoud Rashad <mmrashad@google.com>
2023-10-09 15:45:37 +0000

    Revert "customization: Identify mice which should disable key event rewrite"
    This reverts commit 0d69b56144a7c66c1381f5646474670ee3166c63.
    
    Reason for revert: Build failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/34356/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/53927/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/38836/overview
    
    Original change's description:
    > customization: Identify mice which should disable key event rewrite
    >
    > Add DisableKeyEventRewrites enum. Implement disabling key event rewrite
    > from a mouse if a keyboard with the same id exists.
    >
    > Bug: b/241965717
    > Test: ash_unittests
    > Change-Id: I7fe96efa6a12ca106106212ca8cb5577686d71ff
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4908242
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Longbo Wei <longbowei@google.com>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Commit-Queue: Danny Wang <wangdanny@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1206650}
    
    Bug: b/241965717, 1491040
    Change-Id: I9206881b7efc54f145371fbe0aad4a5890bb326b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4922469
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207092}

--- ash/system/input_device_settings/input_device_settings_utils.cc
+++ ash/system/input_device_settings/input_device_settings_utils.cc
@@ -164,13 +164,16 @@ const base::Value::List* GetLoginScreenButtonRemappingList(
   return &list_value->GetList();
 }
 
-bool IsMouseCustomizable(const ui::InputDevice& device) {
+mojom::CustomizationRestriction GetCustomizationRestriction(
+    const ui::InputDevice& device) {
   // TODO(wangdanny): Update uncustomizable mice set with devices' vid and pid.
   static constexpr auto kUncustomizableMice =
       base::MakeFixedFlatSet<VendorProductId>({
           {0xffff, 0xffff},  // Fake data for testing.
       });
-  return !kUncustomizableMice.contains({device.vendor_id, device.product_id});
+  return kUncustomizableMice.contains({device.vendor_id, device.product_id})
+             ? mojom::CustomizationRestriction::kDisallowCustomizations
+             : mojom::CustomizationRestriction::kAllowCustomizations;
 }
 
 bool IsKeyboardPretendingToBeMouse(const ui::InputDevice& device) {
