85380041276fe15a849cdf6a150b95bfd8a44a8f
Noémie St-Onge <noemies@google.com>
2023-10-10 19:10:55 +0000

    [iOS] Add test coverage for `settingsWillBeDismissed` crash fix
    This CL adds a test to prevent future regressions related to a crash fix
    that was made in SettingsRootTableViewController. See crbug.com/1480694
    for reference.
    
    Fixed: 1486840
    Change-Id: Ia42a0b11c5fc3a4de9a93ef3c92e9ec98e73f45b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4926194
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1207824}

--- ios/chrome/browser/ui/settings/settings_root_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/settings_root_table_view_controller_unittest.mm
@@ -4,9 +4,11 @@
 
 #import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
 
+#import "base/test/scoped_feature_list.h"
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest/include/gtest/gtest.h"
@@ -15,6 +17,32 @@
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
 #import "ui/base/l10n/l10n_util_mac.h"
+
+// Test SettingsRootTableViewController subclass that conformfs to
+// SettingsControllerProtocol. Used to test that 'settingsWillBeDismissed' is
+// called at the right time.
+@interface FakeSettingsRootTableViewController
+    : SettingsRootTableViewController <SettingsControllerProtocol>
+
+@property(nonatomic, assign) BOOL settingsWillBeDismissedCalled;
+
+@end
+
+@implementation FakeSettingsRootTableViewController
+
+#pragma mark - SettingsControllerProtocol
+
+- (void)reportDismissalUserAction {
+}
+
+- (void)reportBackUserAction {
+}
+
+- (void)settingsWillBeDismissed {
+  _settingsWillBeDismissedCalled = YES;
+}
+
+@end
 
 class SettingsRootTableViewControllerTest : public PlatformTest {
  public:
@@ -132,4 +160,29 @@ TEST_F(SettingsRootTableViewControllerTest, TestDeleteToolbar) {
       didDeselectRowAtIndexPath:testIndexPath];
   EXPECT_TRUE(navigationController.toolbarHidden);
   [navigationController cleanUpSettings];
+}
+
+// Tests that a subclass of SettingsRootViewController that implements the
+// SettingsControllerProtocol does not have its implementation of
+// 'settingsWillBeDismissed' called when 'willMoveToParentViewController' is
+// triggered. 'settingsWillBeDismissed' should only be called when
+// 'didMoveToParentViewController' is triggered. Otherwise, a crash may occur as
+// some of the subclass objects can be reset before the subclass is deallocated.
+// A call to 'willMoveToParentViewController' does not necessarily mean that the
+// view controller will be removed from the navigation stack.
+TEST_F(SettingsRootTableViewControllerTest,
+       TestSettingsWillBeDismissedCallTiming) {
+  // Make sure kill switch is enabled.
+  base::test::ScopedFeatureList feature_list(
+      kSettingsWillBeDismissedBugFixKillSwitch);
+
+  FakeSettingsRootTableViewController* controller =
+      [[FakeSettingsRootTableViewController alloc]
+          initWithStyle:UITableViewStylePlain];
+
+  [controller willMoveToParentViewController:nil];
+  EXPECT_FALSE(controller.settingsWillBeDismissedCalled);
+
+  [controller didMoveToParentViewController:nil];
+  EXPECT_TRUE(controller.settingsWillBeDismissedCalled);
 }
