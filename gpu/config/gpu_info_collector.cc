5d2a79eb4a007b53139a7294224ea879f110a774
Austin Eng <enga@chromium.org>
2023-10-10 18:33:18 +0000

    Disable the flag CollectWebGPUSupportMetrics for Windows
    The info collection may hang while loading the driver on Windows.
    This is the step 1 of go/finch-killswitch#process.
    
    (cherry picked from commit abed7ebb93f1b10c3c8a623ba78940d336c9aa86)
    
    Bug: 1487488
    Change-Id: Iff3630d191fce7dcb1acc47fc5f2c69a46a34617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4904998
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1203467}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4919936
    Cr-Commit-Position: refs/branch-heads/5993@{#1235}
    Cr-Branched-From: 511350718e646be62331ae9d7213d10ec320d514-refs/heads/main@{#1192594}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -346,7 +346,11 @@ void ReportWebGPUAdapterMetrics(dawn::native::Instance* instance) {
 void ReportWebGPUSupportMetrics(dawn::native::Instance* instance) {
   static BASE_FEATURE(kCollectWebGPUSupportMetrics,
                       "CollectWebGPUSupportMetrics",
+#if BUILDFLAG(IS_WIN)
+                      base::FEATURE_DISABLED_BY_DEFAULT);
+#else
                       base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
   if (!base::FeatureList::IsEnabled(kCollectWebGPUSupportMetrics)) {
     return;
   }
