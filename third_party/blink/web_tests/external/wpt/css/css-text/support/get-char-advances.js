5fb3032bc63bf3369afe555d38993cf013cd7f5d
Koji Ishii <kojii@chromium.org>
2023-10-10 03:42:37 +0000

    [text-autospace][text-spacing-trim] Update `get-char-advances.js`
    This patch updates `getCharAdvances` function used by tests
    for `text-autospace` and `text-spacing-trim`, so that it uses
    the differences between right edges of bounding boxes instead
    of the widths of bounding boxes.
    
    There are two ways to adjust spacing; one is to adjust width,
    and another is to move the glyph. When the latter happens,
    the width of the bounding box doesn't change, and the logic
    before this change could not detect.
    
    The new logic is needed for future patches, but this patch is
    split to ensure the change doesn't affect existing tests.
    
    Bug: 1463890, 1463891
    Change-Id: I2619510b59ff8732f8416d44d676d4808127610d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918498
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207441}

--- third_party/blink/web_tests/external/wpt/css/css-text/support/get-char-advances.js
+++ third_party/blink/web_tests/external/wpt/css/css-text/support/get-char-advances.js
@@ -1,25 +1,43 @@
 'use strict';
 
 /**
- * Returns an array of advances (widths) for all characters in the descendants
+ * Returns an array of advances for all characters in the descendants
  * of the specified element.
+ *
+ * Technically speaking, advances and glyph bounding boxes are different things,
+ * and advances are not exposed. This function computes approximate values from
+ * bounding boxes.
  */
 function getCharAdvances(element) {
   const range = document.createRange();
-  let advances = [];
-  for (const node of element.childNodes) {
-    const nodeType = node.nodeType;
-    if (nodeType === Node.TEXT_NODE) {
-      const text = node.nodeValue;
-      for (let i = 0; i < text.length; ++i) {
-        range.setStart(node, i);
-        range.setEnd(node, i + 1);
-        const bounds = range.getBoundingClientRect();
-        advances.push(bounds.width);
+  const advances = [];
+  let origin = undefined;
+  let blockEnd = -1;
+  function walk(element) {
+    for (const node of element.childNodes) {
+      const nodeType = node.nodeType;
+      if (nodeType === Node.TEXT_NODE) {
+        const text = node.nodeValue;
+        for (let i = 0; i < text.length; ++i) {
+          range.setStart(node, i);
+          range.setEnd(node, i + 1);
+          const bounds = range.getBoundingClientRect();
+          // Check if this is on the same line.
+          if (bounds.top >= blockEnd) {
+            origin = undefined;
+            blockEnd = bounds.bottom;
+          }
+          // For the first character, the left bound is closest to the origin.
+          if (origin === undefined) origin = bounds.left;
+          // The right bound is a good approximation of the next origin.
+          advances.push(bounds.right - origin);
+          origin = bounds.right;
+        }
+      } else if (nodeType === Node.ELEMENT_NODE) {
+        walk(node);
       }
-    } else if (nodeType === Node.ELEMENT_NODE) {
-      advances = advances.concat(getCharAdvances(node));
     }
   }
+  walk(element);
   return advances;
 }
