17bcd26da201cd4a93579b4205c3756c2cd8750d
Nico Hartmann <nicohartmann@chromium.org>
2023-10-10 10:57:24 +0000

    [Turboshaft] Enable Turboshaft gin feature by default
    Bug: v8:12783
    Change-Id: I30e81f59f2afe497dd5d63913b9f47d3a14a859d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4923751
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Nico Hartmann <nicohartmann@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207559}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -92,7 +92,7 @@ BASE_FEATURE(kV8ExperimentalRegexpEngine,
 BASE_FEATURE(kV8Turbofan, "V8Turbofan", base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables Turbofan's new compiler IR Turboshaft.
-BASE_FEATURE(kV8Turboshaft, "V8Turboshaft", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kV8Turboshaft, "V8Turboshaft", base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables Maglev compiler. Note that this only sets the V8 flag when
 // manually overridden; otherwise it defers to whatever the V8 default is.
