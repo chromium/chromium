33b7e2b5de1a1d0a130c9d6404772bc9ba202d78
manukh <manukh@chromium.org>
2023-10-10 18:29:08 +0000

    [omnibox][post-ac] Prefer url-what-you-typed over preserved default
    Currently, preserve default prefers the previous default suggestion if
    it still matches the current input and is allowed to be default.
    
    That's problematic when the user is typing 'crates.io' and the input
    'crates.i' has a default 'crates.io' search suggestion. When the user
    types the last character 'o', the previous search default is preserved,
    even though the user likely wants to navigate to 'crates.io' as a URL.
    
    This is probably somewhat rare, since
    - If the user had visited crates.io previously, it would have likely
      outscored the search.
    - For popular sites, e.g. facebook.com, we would have had a default
      navsuggest
    - For unpopular sites, e.g. randomwebsite.com, there won't be a search
      suggestion for 'randomwebsite.com'.
    
    Bug: 1340291, 1488804
    Change-Id: Id08849a003b4bbd16f3e0cf9ab57b75df35b766e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4917431
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1207792}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -305,17 +305,16 @@ void AutocompleteResult::SortAndCull(
   std::sort(matches_.begin(), matches_.end(), comparing_object);
 
   // Find the best match and rotate it to the front to become the default match.
+  // TODO(manukh) Ranking and preserving the default suggestion should be done
+  //   by the grouping framework.
   {
-    ACMatches::iterator top_match = matches_.end();
-
-    // TODO(manukh) Ranking and preserving the default suggestion should be done
-    //  by the grouping framework.
-    // If we are trying to keep a default match from a previous pass stable,
-    // search the current results for it, and if found, make it the top match.
-    if (default_match_to_preserve.has_value()) {
+    auto top_match = FindTopMatch(input, &matches_);
+    if (default_match_to_preserve &&
+        (top_match == matches_.end() ||
+         top_match->type != AutocompleteMatchType::URL_WHAT_YOU_TYPED)) {
       const auto default_match_fields =
           GetMatchComparisonFields(default_match_to_preserve.value());
-      top_match =
+      const auto preserved_default_match =
           base::ranges::find_if(matches_, [&](const AutocompleteMatch& match) {
             // Find a match that is a duplicate AND has the same fill_into_edit.
             // Don't preserve suggestions that are not default-able; e.g.,
@@ -325,12 +324,8 @@ void AutocompleteResult::SortAndCull(
                        match.fill_into_edit &&
                    match.allowed_to_be_default_match;
           });
-    }
-
-    // Otherwise, if there's no default match from a previous pass to preserve,
-    // find the top match based on our normal undemoted scoring method.
-    if (top_match == matches_.end()) {
-      top_match = FindTopMatch(input, &matches_);
+      if (preserved_default_match != matches_.end())
+        top_match = preserved_default_match;
     }
 
     RotateMatchToFront(top_match, &matches_);
