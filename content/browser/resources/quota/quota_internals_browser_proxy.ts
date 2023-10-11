db8d6a841907e73a5a9ed8cb1e6a4ac4d55c0e41
Rebekah Potter <rbpotter@chromium.org>
2023-10-11 22:49:21 +0000

    WebUI: Migrate origin mojo bindings to TypeScript
    Bug: 1002798
    Change-Id: I10d68c230a6a928e05edee6ab69b89ea84e8f578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4932559
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208537}

--- content/browser/resources/quota/quota_internals_browser_proxy.ts
+++ content/browser/resources/quota/quota_internals_browser_proxy.ts
@@ -63,10 +63,11 @@ export class QuotaInternalsBrowserProxy {
     const originToTest = (document.body.querySelector<HTMLInputElement>(
         '#origin-to-test'))!.value;
     const originUrl = new URL(originToTest);
-    const newOrigin = new Origin();
-    newOrigin.scheme = originUrl.protocol.replace(/:$/, '');
-    newOrigin.host = originUrl.host;
-    newOrigin.port = urlPort(originUrl);
+    const newOrigin: Origin = {
+      scheme: originUrl.protocol.replace(/:$/, ''),
+      host: originUrl.host,
+      port: urlPort(originUrl),
+    };
 
     this.handler.simulateStoragePressure(newOrigin);
   }
