0bcbaade93960b42617b3e1a12ded84105a6369d
An Sung <ansung@google.com>
2023-10-11 22:21:28 +0000

    [blink] un-suppress RunTest test class in run_web_tests_unittest
    The annotation "@unittest.removeHandler" led to the RunTest test class not being run.
    This change removes the annotation to resurrect the tests (103 passing tests, 2 skipped tests, 21 failing tests).
    
    For the tests that are failing due to staleness,
      tests related to "test-list" have been fixed according to up to date definition of the test-list functionality,
      tests with missing result field has fixed to include newly introduced fields,
      rest of the tests are marked as "skipped" to be fixed in future CL.
    
    Some background on "test-list":
    The argument "test-list" used to represent test filter before, the argument was renamed to "filter" in https://crrev.com/c/2719071.
    "test-list" was later re-introduced in https://crrev.com/c/3866568 to represent a collection of tests, which only does positive matching on local tests and does NOT have filtering capability.
    
    Bug: 1466198
    Change-Id: I072931130ef7d378868f9c7f2c3ad23c6d0e1f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4707085
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: An Sung <ansung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208527}

--- third_party/blink/tools/blinkpy/web_tests/run_web_tests_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/run_web_tests_unittest.py
@@ -164,7 +164,6 @@ class StreamTestingMixin(object):
         self.assertTrue(stream.getvalue())
 
 
-@unittest.removeHandler
 class RunTest(unittest.TestCase, StreamTestingMixin):
     def setUp(self):
         # A real PlatformInfo object is used here instead of a
@@ -172,6 +171,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         # Windows and Mac to skip some tests.
         self._platform = SystemHost().platform
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_basic(self):
         options, args = parse_args(
             tests_included=True)
@@ -645,6 +645,8 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertNotIn('flag_expectations', test_results)
         self.assertNotIn('base_expectations', test_results)
 
+    @unittest.skip(
+        "TODO: Need fix for this test - flag_name is missing from object")
     def test_no_flag_expectations_found_json_results(self):
         host = MockHost()
         port = host.port_factory.get('test-win-win7')
@@ -672,6 +674,8 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertNotIn('flag_expectations', test_results)
         self.assertNotIn('base_expectations', test_results)
 
+    @unittest.skip(
+        "TODO: Need fix for this test - flag_name is missing from object")
     def test_pass_flag_expectations_in_json_results(self):
         host = MockHost()
         port = host.port_factory.get('test-win-win7')
@@ -704,6 +708,8 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(sorted(test_results['base_expectations']),
                          ['FAIL', 'TIMEOUT'])
 
+    @unittest.skip(
+        "TODO: Need fix for this test - flag_name is missing from object")
     def test_slow_flag_expectations_in_json_results(self):
         host = MockHost()
         port = host.port_factory.get('test-win-win7')
@@ -748,6 +754,8 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(sorted(image_results['base_expectations']),
                          ['CRASH', 'FAIL'])
 
+    @unittest.skip(
+        "TODO: Need fix for this test - flag_name is missing from object")
     def test_flag_and_base_expectations_in_json_results(self):
         host = MockHost()
         port = host.port_factory.get('test-win-win7')
@@ -782,6 +790,8 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(sorted(test_results['base_expectations']),
                          ['FAIL', 'TIMEOUT'])
 
+    @unittest.skip(
+        "TODO: Need fix for this test - flag_name is missing from object")
     def test_flag_and_default_base_expectations_in_json_results(self):
         host = MockHost()
         port = host.port_factory.get('test-win-win7')
@@ -808,6 +818,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(test_results['flag_expectations'], ['FAIL'])
         self.assertEqual(test_results['base_expectations'], ['PASS'])
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_stderr_is_saved(self):
         host = MockHost()
         self.assertTrue(passing_run(host=host))
@@ -1004,23 +1015,42 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(details.exit_code, exit_codes.NO_TESTS_EXIT_STATUS)
         self.assert_not_empty(err)
 
-    def test_test_list_filter_glob(self):
+    def test_test_list_with_args(self):
+        # Tests from test list and tests in argument are added together
+        host = MockHost()
+        filename = '/tmp/foo.txt'
+        host.filesystem.write_text_file(filename, 'passes/image.html')
+        args = ['passes/text.html']
+        tests_run = get_tests_run(['--test-list=%s' % filename] + args,
+                                  host=host)
+        self.assertEqual(tests_run, ['passes/text.html', 'passes/image.html'])
+
+    def test_test_list_wildcard(self):
+        host = MockHost()
+        filename = '/tmp/foo.txt'
+        host.filesystem.write_text_file(filename, 'passes/i*')
+        tests_run = get_tests_run(['--test-list=%s' % filename], host=host)
+        self.assertEqual(tests_run, ['passes/image.html'])
+
+    def test_filter(self):
+        host = MockHost()
+        filename = '/tmp/foo.txt'
+        host.filesystem.write_text_file(filename, '-passes/text.html')
+        args = ['passes/text.html', 'passes/image.html']
+        tests_run = get_tests_run(
+            ['--isolated-script-test-filter-file=%s' % filename] + args,
+            host=host)
+        self.assertEqual(tests_run, ['passes/image.html'])
+
+    def test_filter_wildcard(self):
         host = MockHost()
         filename = '/tmp/foo.txt'
         host.filesystem.write_text_file(filename, '-passes/t*')
         args = ['passes/text.html', 'passes/image.html']
-        tests_run = get_tests_run(['--test-list=%s' % filename] + args,
-                                  host=host)
+        tests_run = get_tests_run(
+            ['--isolated-script-test-filter-file=%s' % filename] + args,
+            host=host)
         self.assertEqual(tests_run, ['passes/image.html'])
-
-    def test_test_list_filter(self):
-        host = MockHost()
-        filename = '/tmp/foo.txt'
-        host.filesystem.write_text_file(filename, '-passes/image.html')
-        args = ['passes/text.html', 'passes/image.html']
-        tests_run = get_tests_run(['--test-list=%s' % filename] + args,
-                                  host=host)
-        self.assertEqual(tests_run, ['passes/text.html'])
 
     def test_test_list_union(self):
         host = MockHost()
@@ -1162,6 +1192,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
             'passes/text.html'
         ])
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_smoke_test(self):
         host = MockHost()
         smoke_test_filename = test.MOCK_WEB_TESTS + 'SmokeTests'
@@ -1202,6 +1233,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
             port_obj=port_obj)
         self.assertNotIn('passes/text.html', tests_run)
 
+    @unittest.skip("TODO: Fix failing test case")
     def test_smoke_test_default_retry(self):
         host = MockHost()
         smoke_test_filename = test.MOCK_WEB_TESTS + 'SmokeTests'
@@ -1241,6 +1273,11 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
                 'is_unexpected': True,
                 'is_regression': True,
                 'text_mismatch': 'general text mismatch',
+                'image_diff_stats': {
+                    'maxDifference': 100,
+                    'maxPixels': 54
+                },
+                'shard': None,
             })
         results['tests']['failures']['unexpected']['missing_text.html'].pop(
             'artifacts')
@@ -1251,12 +1288,15 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
                 'is_unexpected': True,
                 'is_regression': True,
                 'is_missing_text': True,
+                'shard': None,
             })
-        self.assertEqual(results['tests']['passes']['slow.html'], {
-            'expected': 'PASS',
-            'actual': 'PASS',
-            'is_slow_test': True,
-        })
+        self.assertEqual(
+            results['tests']['passes']['slow.html'], {
+                'expected': 'PASS',
+                'actual': 'PASS',
+                'is_slow_test': True,
+                'shard': None,
+            })
         self.assertEqual(results['num_passes'], 1)
         self.assertEqual(results['num_regressions'], 2)
         self.assertEqual(results['num_flaky'], 0)
@@ -1831,6 +1871,9 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
                                  host=host)
         self.assertIn('Retrying', err.getvalue())
 
+    @unittest.skip(
+        "TODO: Need fix for this test - Old result re-appear after clobber event and before new artifact creation"
+    )
     def test_clobber_old_results(self):
         host = MockHost()
         details, _, _ = logging_run([
@@ -1862,6 +1905,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertFalse(
             host.filesystem.exists('/tmp/layout-test-results/retry_3'))
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_run_order__inline(self):
         # These next tests test that we run the tests in ascending alphabetical
         # order per directory. HTTP tests are sharded separately from other tests,
@@ -1873,6 +1917,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         tests_run = get_tests_run(['--order', 'natural', 'http/tests/passes'])
         self.assertEqual(tests_run, sorted(tests_run))
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_virtual(self):
         self.assertTrue(
             passing_run([
@@ -1923,6 +1968,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         # The list of references should be empty since the test crashed and we didn't run any references.
         self.assertEqual(test_results[0].references, [])
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_reftest_with_virtual_reference(self):
         _, err, _ = logging_run(
             ['--details', 'virtual/virtual_passes/passes/reftest.html'],
@@ -2169,6 +2215,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertEqual(run_details.exit_code, 0)
         self.assertNotIn('Please remove', log_stream.getvalue())
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_testharness_expected_txt(self):
         host = MockHost()
         test_name = '/failures/unexpected/testharness.html'
@@ -2200,6 +2247,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
                 test_failures.FailureTestHarnessAssertion,
                 test_result.failures))
 
+    @unittest.skip("TODO: Need fix for this test")
     def test_additional_platform_directory(self):
         self.assertTrue(
             passing_run([
@@ -2302,6 +2350,7 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
                                 host=host)
         self.assertIn('OUT:', err.getvalue())
 
+    @unittest.skip("TODO: Need fix for this test")
     def _check_json_test_results(self, host, details):
         self.assertEqual(details.exit_code, 0)
         self.assertTrue(host.filesystem.exists('/tmp/json_results.json'))
