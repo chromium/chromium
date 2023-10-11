643c4545f0e4269ac62fb8708a1285a4b8d11e74
Andrew Grieve <agrieve@chromium.org>
2023-10-11 14:12:43 +0000

    Android: Allow tests of fragments that use BlankUiTestActivity to use InMemorySharedPreferences
    Bug: None
    Change-Id: Ib7a41ddb1d5a108cbf756f6f7ba6f6696f37afa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930089
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208237}

--- ui/android/javatests/src/org/chromium/ui/test/util/BlankUiTestActivity.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/BlankUiTestActivity.java
@@ -4,6 +4,7 @@
 
 package org.chromium.ui.test.util;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -13,6 +14,7 @@ import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.Callback;
+import org.chromium.base.ContextUtils;
 import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
@@ -45,6 +47,12 @@ public class BlankUiTestActivity extends AppCompatActivity implements ModalDialo
     public void onDestroy() {
         mModalDialogManager.destroy();
         super.onDestroy();
+    }
+
+    /** Required to make preference fragments use InMemorySharedPreferences in tests. */
+    @Override
+    public SharedPreferences getSharedPreferences(String name, int mode) {
+        return ContextUtils.getApplicationContext().getSharedPreferences(name, mode);
     }
 
     /**
