cbabb7968a285a417cd21fded23a9c9f500fef3c
Lei Zhang <thestig@chromium.org>
2023-10-11 19:05:11 +0000

    Modernize PdfNupConverterClientBrowserTest
    - Change PdfNupConverterClientBrowserTest::Convert() to avoid using out
      parameters.
    - Rename Convert() to ConvertDocument() to better describe what it does.
    - Replace some std::vector constants with C-arrays and base::spans.
    - Replace base::RunLoop with base::test::TestFuture.
    
    Change-Id: I5a60f1f11606413bff0f89d8b045951323035366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928232
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208379}

--- chrome/browser/printing/pdf_nup_converter_client_browsertest.cc
+++ chrome/browser/printing/pdf_nup_converter_client_browsertest.cc
@@ -2,14 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <utility>
+#include "chrome/browser/printing/pdf_nup_converter_client.h"
 
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
+#include "base/notreached.h"
 #include "base/path_service.h"
-#include "base/run_loop.h"
+#include "base/test/test_future.h"
 #include "base/threading/thread_restrictions.h"
-#include "chrome/browser/printing/pdf_nup_converter_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_paths.h"
@@ -23,18 +28,6 @@
 namespace printing {
 
 namespace {
-
-void ResultCallbackImpl(mojom::PdfNupConverter::Status* status_out,
-                        base::ReadOnlySharedMemoryRegion* nup_pdf_region_out,
-                        bool* called,
-                        base::OnceClosure quit_closure,
-                        mojom::PdfNupConverter::Status status_in,
-                        base::ReadOnlySharedMemoryRegion nup_pdf_region_in) {
-  *status_out = status_in;
-  *nup_pdf_region_out = std::move(nup_pdf_region_in);
-  *called = true;
-  std::move(quit_closure).Run();
-}
 
 base::FilePath GetTestDir() {
   base::FilePath dir;
@@ -97,16 +90,16 @@ std::vector<gfx::SizeF> GetPdfPageSizes(base::span<const uint8_t> pdf_data) {
 }
 
 void VerifyPdf(base::span<const uint8_t> pdf_data,
-               const std::vector<gfx::SizeF>& expected_sizes) {
+               base::span<const gfx::SizeF> expected_sizes) {
   std::vector<gfx::SizeF> page_sizes = GetPdfPageSizes(pdf_data);
   ASSERT_EQ(expected_sizes.size(), page_sizes.size());
   for (size_t i = 0; i < expected_sizes.size(); ++i)
     EXPECT_EQ(expected_sizes[i], page_sizes[i]);
 }
 
-std::vector<gfx::SizeF> GetExpectedPdfSizes(base::StringPiece pdf_name) {
+base::span<const gfx::SizeF> GetExpectedPdfSizes(base::StringPiece pdf_name) {
   if (pdf_name == "pdf_converter_basic.pdf") {
-    static const std::vector<gfx::SizeF> kSizes = {
+    static constexpr gfx::SizeF kSizes[] = {
         {612.0f, 792.0f},
         {612.0f, 792.0f},
         {612.0f, 792.0f},
@@ -114,43 +107,45 @@ std::vector<gfx::SizeF> GetExpectedPdfSizes(base::StringPiece pdf_name) {
     return kSizes;
   }
 
-  NOTREACHED();
-  return {};
+  NOTREACHED_NORETURN();
 }
 
 }  // namespace
 
 class PdfNupConverterClientBrowserTest : public InProcessBrowserTest {
  public:
+  struct ConvertResult {
+    mojom::PdfNupConverter::Status status;
+    base::ReadOnlySharedMemoryRegion nup_pdf_region;
+  };
+
   PdfNupConverterClientBrowserTest() = default;
   ~PdfNupConverterClientBrowserTest() override = default;
 
-  absl::optional<mojom::PdfNupConverter::Status> Convert(
+  absl::optional<ConvertResult> ConvertDocument(
       base::ReadOnlySharedMemoryRegion pdf_region,
-      int pages_per_sheet,
-      base::ReadOnlySharedMemoryRegion* out_nup_pdf_region) {
-    mojom::PdfNupConverter::Status status;
-    base::ReadOnlySharedMemoryRegion nup_pdf_region;
-    bool called = false;
+      int pages_per_sheet) {
     auto converter = std::make_unique<PdfNupConverterClient>(
         browser()->tab_strip_model()->GetActiveWebContents());
 
-    {
-      base::RunLoop run_loop;
-      converter->DoNupPdfDocumentConvert(
-          /*document_cookie=*/PrintSettings::NewCookie(), pages_per_sheet,
-          /*page_size=*/gfx::Size(612, 792),
-          /*printable_area=*/gfx::Rect(612, 792), std::move(pdf_region),
-          base::BindOnce(&ResultCallbackImpl, &status, &nup_pdf_region, &called,
-                         run_loop.QuitClosure()));
-      run_loop.Run();
+    base::test::TestFuture<mojom::PdfNupConverter::Status,
+                           base::ReadOnlySharedMemoryRegion>
+        future;
+    converter->DoNupPdfDocumentConvert(
+        /*document_cookie=*/PrintSettings::NewCookie(), pages_per_sheet,
+        /*page_size=*/gfx::Size(612, 792),
+        /*printable_area=*/gfx::Rect(612, 792), std::move(pdf_region),
+        future.GetCallback());
+
+    if (!future.Wait()) {
+      // Give the caller a chance to fail gracefully. Whereas calling
+      // `future.Take()` without handling the Wait() failure will result in a
+      // CHECK() crash.
+      return absl::nullopt;
     }
 
-    if (!called)
-      return absl::nullopt;
-
-    *out_nup_pdf_region = std::move(nup_pdf_region);
-    return status;
+    auto result = future.Take();
+    return ConvertResult{std::get<0>(result), std::move(std::get<1>(result))};
   }
 };
 
@@ -164,17 +159,17 @@ IN_PROC_BROWSER_TEST_F(PdfNupConverterClientBrowserTest,
   VerifyPdf(pdf_region.mapping.GetMemoryAsSpan<uint8_t>(),
             GetExpectedPdfSizes("pdf_converter_basic.pdf"));
 
-  base::ReadOnlySharedMemoryRegion nup_pdf_region;
-  absl::optional<mojom::PdfNupConverter::Status> status = Convert(
-      std::move(pdf_region.region), /*pages_per_sheet=*/2, &nup_pdf_region);
+  absl::optional<ConvertResult> result =
+      ConvertDocument(std::move(pdf_region.region), /*pages_per_sheet=*/2);
 
-  ASSERT_TRUE(status.has_value());
-  EXPECT_EQ(mojom::PdfNupConverter::Status::SUCCESS, status.value());
-  base::ReadOnlySharedMemoryMapping nup_pdf_mapping = nup_pdf_region.Map();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(mojom::PdfNupConverter::Status::SUCCESS, result.value().status);
+  base::ReadOnlySharedMemoryMapping nup_pdf_mapping =
+      result.value().nup_pdf_region.Map();
   ASSERT_TRUE(nup_pdf_mapping.IsValid());
 
   // For 2-up, a 3 page portrait document fits on 2 landscape-oriented pages.
-  const std::vector<gfx::SizeF> kExpectedSizes = {
+  static constexpr gfx::SizeF kExpectedSizes[] = {
       {792.0f, 612.0f},
       {792.0f, 612.0f},
   };
@@ -191,17 +186,17 @@ IN_PROC_BROWSER_TEST_F(PdfNupConverterClientBrowserTest,
   VerifyPdf(pdf_region.mapping.GetMemoryAsSpan<uint8_t>(),
             GetExpectedPdfSizes("pdf_converter_basic.pdf"));
 
-  base::ReadOnlySharedMemoryRegion nup_pdf_region;
-  absl::optional<mojom::PdfNupConverter::Status> status = Convert(
-      std::move(pdf_region.region), /*pages_per_sheet=*/4, &nup_pdf_region);
+  absl::optional<ConvertResult> result =
+      ConvertDocument(std::move(pdf_region.region), /*pages_per_sheet=*/4);
 
-  ASSERT_TRUE(status.has_value());
-  EXPECT_EQ(mojom::PdfNupConverter::Status::SUCCESS, status.value());
-  base::ReadOnlySharedMemoryMapping nup_pdf_mapping = nup_pdf_region.Map();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(mojom::PdfNupConverter::Status::SUCCESS, result.value().status);
+  base::ReadOnlySharedMemoryMapping nup_pdf_mapping =
+      result.value().nup_pdf_region.Map();
   ASSERT_TRUE(nup_pdf_mapping.IsValid());
 
   // For 4-up, a 3 page portrait document fits on 1 portrait-oriented page.
-  const std::vector<gfx::SizeF> kExpectedSizes = {
+  static constexpr gfx::SizeF kExpectedSizes[] = {
       {612.0f, 792.0f},
   };
   VerifyPdf(nup_pdf_mapping.GetMemoryAsSpan<uint8_t>(), kExpectedSizes);
@@ -212,13 +207,14 @@ IN_PROC_BROWSER_TEST_F(PdfNupConverterClientBrowserTest,
   base::MappedReadOnlyRegion pdf_region = GetBadDataRegion();
   ASSERT_TRUE(pdf_region.IsValid());
 
-  base::ReadOnlySharedMemoryRegion nup_pdf_region;
-  absl::optional<mojom::PdfNupConverter::Status> status = Convert(
-      std::move(pdf_region.region), /*pages_per_sheet=*/2, &nup_pdf_region);
+  absl::optional<ConvertResult> result =
+      ConvertDocument(std::move(pdf_region.region), /*pages_per_sheet=*/2);
 
-  ASSERT_TRUE(status.has_value());
-  EXPECT_EQ(mojom::PdfNupConverter::Status::CONVERSION_FAILURE, status.value());
-  base::ReadOnlySharedMemoryMapping nup_pdf_mapping = nup_pdf_region.Map();
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(mojom::PdfNupConverter::Status::CONVERSION_FAILURE,
+            result.value().status);
+  base::ReadOnlySharedMemoryMapping nup_pdf_mapping =
+      result.value().nup_pdf_region.Map();
   EXPECT_FALSE(nup_pdf_mapping.IsValid());
 }
 
