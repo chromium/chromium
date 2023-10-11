7d1744cb68e9f84aa91929e069170370a0a1fa53
Ashley Prasad <ashleydp@google.com>
2023-10-11 16:49:44 +0000

    Firmware UI: Convert update_card to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    - Replace mixinBehavior with I18nMixin.
    
    Bug: b:302140708
    Test: browser_tests --gtest_filter=*FirmwareUpdate*
    Test: tast run -build=true ${DUT} firmwareupdate.*
    Change-Id: I018c96f16a307c1dadbd007ab1204b149d800718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4912463
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1208323}

--- ash/webui/firmware_update_ui/resources/update_card.ts
+++ ash/webui/firmware_update_ui/resources/update_card.ts
@@ -4,18 +4,19 @@
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/big_buffer.mojom-webui.js';
-import 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-webui.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/file_path.mojom-webui.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-webui.js';
 import './firmware_shared.css.js';
 import './firmware_shared_fonts.css.js';
 import './firmware_update.mojom-webui.js';
 import './strings.m.js';
 
-import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/ash/common/i18n_behavior.js';
+import {I18nMixin, I18nMixinInterface} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {mojoString16ToString} from 'chrome://resources/js/mojo_type_util.js';
-import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {FirmwareUpdate, UpdatePriority} from './firmware_update.mojom-webui.js';
+import {OpenConfirmationDialogEventDetail} from './firmware_update_types.js';
 import {getTemplate} from './update_card.html.js';
 
 /**
@@ -23,17 +24,12 @@ import {getTemplate} from './update_card.html.js';
  * 'update-card' displays information about a peripheral update.
  */
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {I18nBehaviorInterface}
- */
-const UpdateCardElementBase = mixinBehaviors([I18nBehavior], PolymerElement);
+const UpdateCardElementBase =
+    I18nMixin(PolymerElement) as {new (): PolymerElement & I18nMixinInterface};
 
-/** @polymer */
 export class UpdateCardElement extends UpdateCardElementBase {
   static get is() {
-    return 'update-card';
+    return 'update-card' as const;
   }
 
   static get template() {
@@ -42,7 +38,6 @@ export class UpdateCardElement extends UpdateCardElementBase {
 
   static get properties() {
     return {
-      /** @type {!FirmwareUpdate} */
       update: {
         type: Object,
       },
@@ -53,26 +48,20 @@ export class UpdateCardElement extends UpdateCardElementBase {
     };
   }
 
-  /**
-   * @protected
-   * @return {boolean}
-   */
-  isCriticalUpdate_() {
+  update: FirmwareUpdate;
+  disabled: boolean;
+
+  protected isCriticalUpdate(): boolean {
     return this.update.priority === UpdatePriority.kCritical;
   }
 
-  /** @protected */
-  onUpdateButtonClicked_() {
-    this.dispatchEvent(new CustomEvent(
+  protected onUpdateButtonClicked(): void {
+    this.dispatchEvent(new CustomEvent<OpenConfirmationDialogEventDetail>(
         'open-confirmation-dialog',
         {bubbles: true, composed: true, detail: {update: this.update}}));
   }
 
-  /**
-   * @protected
-   * @return {string}
-   */
-  computeVersionText_() {
+  protected computeVersionText(): string {
     if (!this.update.deviceVersion) {
       return '';
     }
@@ -80,20 +69,22 @@ export class UpdateCardElement extends UpdateCardElementBase {
     return this.i18n('versionText', this.update.deviceVersion);
   }
 
-  /**
-   * @protected
-   * @return {string}
-   */
-  computeDeviceName_() {
+  protected computeDeviceName(): string {
     return mojoString16ToString(this.update.deviceName);
   }
 
-  /**
-   * @protected
-   * @return {string}
-   */
-  getUpdateButtonA11yLabel_() {
-    return this.i18n('updateButtonA11yLabel', this.computeDeviceName_());
+  protected getUpdateButtonA11yLabel(): string {
+    return this.i18n('updateButtonA11yLabel', this.computeDeviceName());
+  }
+}
+
+declare global {
+  interface HTMLElementEventMap {
+    'open-confirmation-dialog': CustomEvent<OpenConfirmationDialogEventDetail>;
+  }
+
+  interface HTMLElementTagNameMap {
+    [UpdateCardElement.is]: UpdateCardElement;
   }
 }
 
