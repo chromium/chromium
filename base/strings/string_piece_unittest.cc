d1e98a4c6c6130ff7ca1dffc29c03fb8e8a69a63
David Benjamin <davidben@chromium.org>
2023-10-12 19:23:26 +0000

    Revert "Do not build unit tests depending on safe libc++ if it is not enabled"
    This reverts commit ef08fb754f940a894c60010623176466c6bf73bb.
    
    Reason for revert: These unit tests should run unconditionally. The
    reason is two-fold.
    
    First, the tests exist to make sure we didn't make a mistake in our
    build and accidentally disable safe mode for a supported
    configuration. Whatever the right accommodation is for libstdc++,
    this is not the right one as it introduces a security risk for
    Chromium projects. Thus, it should be reverted and a new
    accommodation attempted post-revert.
    
    Second, this test reflects the Chromium project's development
    assumptions. As documented in the security FAQ below, we treat
    crashes from libc++ safe mode as functional bugs, not security bugs.
    These tests reflect those assumptions. It is desirable for them to
    fail in embedders whose STLs don't meet this. Embedders should either
    fix their STLs to pass the tests, or go out of their way to ignore
    the test failures. If they pick the latter, they are taking on a
    security risk, so needing a manual step is desirable.
    
    https://chromium.googlesource.com/chromium/src/+/master/docs/security/faq.md#indexing-a-container-out-of-bounds-hits-a-libcpp_verbose_abort_is-this-a-security-bug
    
    For libstdc++, probably the best option is to, as the original CL
    description suggests, define _GLIBCXX_ASSERTIONS=1, not to suppress
    the tests.
    
    Original change's description:
    > Do not build unit tests depending on safe libc++ if it is not enabled
    >
    > When use_safe_libcxx is enabled, then we build defining
    > _LIBCPP_ENABLE_ASSERTIONS, that adds extra checks to libc++. This
    > adds several tests that fail when using libstdc++.
    >
    > In the future we could be interested in building libstdc++ with its
    > own assertions defining _GLIBCXX_ASSERTIONS=1. But for now, we remove
    > flaky tests when they are not supported.
    >
    > Bug: 957519
    > Change-Id: I8da050d93d18817833996266bebc8305c45cdb52
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4807532
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1188924}
    
    Bug: 957519
    Change-Id: I9fbb2616e0e103b02e496e85f88d0d906f1ffe67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936192
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209013}

--- base/strings/string_piece_unittest.cc
+++ base/strings/string_piece_unittest.cc
@@ -684,7 +684,14 @@ TEST(StringPieceTest, ConstexprCtor) {
   }
 }
 
-#if defined(_LIBCPP_ENABLE_ASSERTIONS)
+// Chromium development assumes StringPiece (which is std::string_view) is
+// implemented with an STL that enables hardening checks. We treat bugs that
+// trigger one of these conditions as functional rather than security bugs. If
+// this test fails on some embedder, it should not be disabled. Instead, the
+// embedder should fix their STL or build configuration to enable corresponding
+// hardening checks.
+//
+// See https://chromium.googlesource.com/chromium/src/+/main/docs/security/faq.md#indexing-a-container-out-of-bounds-hits-a-libcpp_verbose_abort_is-this-a-security-bug
 TEST(StringPieceTest, OutOfBoundsDeath) {
   {
     constexpr StringPiece piece;
@@ -726,7 +733,6 @@ TEST(StringPieceTest, InvalidLengthDeath) {
   int length = -1;
   ASSERT_DEATH_IF_SUPPORTED({ StringPiece piece("hello", length); }, "");
 }
-#endif  // defined(_LIBCPP_ENABLE_ASSERTIONS)
 
 TEST(StringPieceTest, ConstexprData) {
   {
