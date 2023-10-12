dbd697a8cc8da30ef04319f005b0047fc2f87296
Louise Brett <loubrett@google.com>
2023-10-12 06:09:33 +0000

    Add install and cancel buttons to install dialog
    The install button when clicked changes to an installing state with a
    spinner, and then to an installed state.
    
    Screencast: https://crbug.com/1488697#c4
    
    Bug: 1488697, b/302413269
    Change-Id: I3a92b804edd38db994331181b0313cb9abb20779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4918680
    Commit-Queue: Louise Brett <loubrett@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208672}

--- chrome/browser/ui/webui/ash/web_app_install/web_app_install_ui.cc
+++ chrome/browser/ui/webui/ash/web_app_install/web_app_install_ui.cc
@@ -8,9 +8,11 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/web_app_install_resources.h"
 #include "chrome/grit/web_app_install_resources_map.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/url_constants.h"
@@ -21,6 +23,14 @@ WebAppInstallDialogUI::WebAppInstallDialogUI(content::WebUI* web_ui)
     : ui::WebDialogUI(web_ui) {
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       Profile::FromWebUI(web_ui), chrome::kChromeUIWebAppInstallDialogHost);
+
+  static constexpr webui::LocalizedString kStrings[] = {
+      {"cancel", IDS_CANCEL},
+      {"install", IDS_INSTALL},
+      {"installing", IDS_OFFICE_INSTALL_PWA_INSTALLING_BUTTON},
+  };
+
+  source->AddLocalizedStrings(kStrings);
   webui::SetupWebUIDataSource(
       source,
       base::make_span(kWebAppInstallResources, kWebAppInstallResourcesSize),
