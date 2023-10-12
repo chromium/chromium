7ea8c5a87a77322e859cabf473636ccc0146dd06
Zonghan Xu <xzonghan@google.com>
2023-10-12 16:57:43 +0000

    [Gardener] Disable ChromeVoxBackgroundTest.NewWindowWebSpeech on ChromeOS.
    Bug: 1491964
    Change-Id: Id0ed2e8cecbbfcec63ba2e311c6315f4146b2cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4936211
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208908}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
@@ -3864,7 +3864,7 @@ AX_TEST_F(
     });
 
 // TODO(https://crbug.com/1491964): Failing on Linux ChromeOS bots.
-GEN('#if BUILDFLAG(IS_LINUX)');
+GEN('#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)');
 GEN('#define MAYBE_NewWindowWebSpeech DISABLED_NewWindowWebSpeech');
 GEN('#else');
 GEN('#define MAYBE_NewWindowWebSpeech NewWindowWebSpeech');
