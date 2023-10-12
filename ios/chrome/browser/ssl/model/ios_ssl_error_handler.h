ebe102b877e2bee310bddb819ca57e9a0f749192
nihardamar <nihardamar@google.com>
2023-10-12 17:35:29 +0000

    [Code Health][iOS] Move ios/c/b/ssl to model subfolder [1/3]
    This will be done in 3 parts:
    1. Add temporary header files that just include corresponding files
    and create BUILD.gn with the correct target names
    2. Update downstream references
    3. move the folder (which will overwrite the temp files)
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/<foo>/ will be moved to
    ios/c/b/<foo>/model/ to showcase that they are model files.
    
    Bug: 1319852,1480358
    Change-Id: Ibf4cd7a4fcfdfd28976e05a95b8a16eaa3704816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4929938
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1208941}

---
+++ ios/chrome/browser/ssl/model/ios_ssl_error_handler.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SSL_MODEL_IOS_SSL_ERROR_HANDLER_H_
+#define IOS_CHROME_BROWSER_SSL_MODEL_IOS_SSL_ERROR_HANDLER_H_
+
+#include "ios/chrome/browser/ssl/ios_ssl_error_handler.h"
+
+#endif  // IOS_CHROME_BROWSER_SSL_MODEL_IOS_SSL_ERROR_HANDLER_H_
