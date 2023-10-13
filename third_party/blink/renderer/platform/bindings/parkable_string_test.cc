71c96c9e3ad560d4fc441fd21400ff6832c19e0a
nihardamar <nihardamar@google.com>
2023-10-13 17:04:01 +0000

    [Code Health] clean up expired histograms
    Clean up expired histograms.
    Savings.Kb looks like it was already cleaned up so just remove the
    histogram for this one.
    
    Bug: 1384691
    Change-Id: Ib919630394f5e04c58545ee40a4c0f495957388e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4929957
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209504}

--- third_party/blink/renderer/platform/bindings/parkable_string_test.cc
+++ third_party/blink/renderer/platform/bindings/parkable_string_test.cc
@@ -1205,8 +1205,6 @@ TEST_P(ParkableStringTest, DISABLED_OnlyOneAgingTask) {
 }
 
 TEST_P(ParkableStringTest, ReportTotalUnparkingTime) {
-  const size_t kCompressedSize = GetExpectedCompressedSize();
-
   base::ScopedMockElapsedTimersForTest mock_elapsed_timers;
   base::HistogramTester histogram_tester;
 
@@ -1229,14 +1227,6 @@ TEST_P(ParkableStringTest, ReportTotalUnparkingTime) {
   }
 
   task_environment_.FastForwardUntilNoTasksRemain();
-
-  histogram_tester.ExpectUniqueSample("Memory.ParkableString.TotalSizeKb.5min",
-                                      kSizeKb, 1);
-  histogram_tester.ExpectUniqueSample(
-      "Memory.ParkableString.CompressedSizeKb.5min", kCompressedSize / 1000, 1);
-  histogram_tester.ExpectUniqueSample(
-      "Memory.ParkableString.CompressionRatio.5min",
-      (100 * kCompressedSize) / (kSizeKb * 1000), 1);
 }
 
 TEST_P(ParkableStringTest, ReportTotalDiskTime) {
@@ -1272,10 +1262,6 @@ TEST_P(ParkableStringTest, ReportTotalDiskTime) {
   histogram_tester.ExpectUniqueSample(
       "Memory.ParkableString.DiskWriteTime.5min", mock_elapsed_time_ms, 1);
 
-  histogram_tester.ExpectUniqueSample("Memory.ParkableString.TotalSizeKb.5min",
-                                      kSizeKb, 1);
-  histogram_tester.ExpectUniqueSample(
-      "Memory.ParkableString.CompressedSizeKb.5min", 0, 1);
   histogram_tester.ExpectUniqueSample("Memory.ParkableString.OnDiskSizeKb.5min",
                                       kCompressedSize / 1000, 1);
 }
