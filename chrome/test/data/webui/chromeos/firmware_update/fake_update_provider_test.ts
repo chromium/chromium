bcd60207917c2eb3c62dbbacb3b223215ddeb0cf
Ashley Prasad <ashleydp@google.com>
2023-10-13 21:49:37 +0000

    Firmware UI: Convert fake_update_provider_test to TS
    - Rename file from JS to TS and update BUILD.gn.
    - Remove closure type comments.
    - Update property and function names to match TS style guide.
    
    Bug: b:304299225
    Test: browser_tests --gtest_filter=*FirmwareUpdate*
    Change-Id: I0519db4f9c2bdc63347b698d56349d038068b3e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4928675
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209695}

--- chrome/test/data/webui/chromeos/firmware_update/fake_update_provider_test.ts
+++ chrome/test/data/webui/chromeos/firmware_update/fake_update_provider_test.ts
@@ -2,30 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assert} from 'chrome://resources/js/assert.js';
 import {fakeFirmwareUpdates} from 'chrome://accessory-update/fake_data.js';
 import {FakeUpdateProvider} from 'chrome://accessory-update/fake_update_provider.js';
 import {UpdateObserverRemote} from 'chrome://accessory-update/firmware_update.mojom-webui.js';
 import {assertDeepEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
 
 export function fakeUpdateProviderTest() {
-  /** @type {?FakeUpdateProvider} */
-  let provider = null;
+  let provider: FakeUpdateProvider|null = null;
 
   setup(() => provider = new FakeUpdateProvider());
 
   teardown(() => {
-    provider.reset();
+    provider?.reset();
     provider = null;
   });
 
   test('ObservePeripheralUpdates', () => {
+    assert(provider);
     provider.setFakeFirmwareUpdates(fakeFirmwareUpdates);
 
-    const updateObserverRemote = /** @type {!UpdateObserverRemote} */ ({
+    const updateObserverRemote ={
       onUpdateListChanged: (firmwareUpdates) => {
         assertDeepEquals(fakeFirmwareUpdates[0], firmwareUpdates);
       },
-    });
+    } as UpdateObserverRemote;
 
     provider.observePeripheralUpdates(updateObserverRemote);
     return provider.getObservePeripheralUpdatesPromiseForTesting();
