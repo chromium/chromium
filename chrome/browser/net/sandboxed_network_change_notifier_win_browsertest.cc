ab9634b597869692029b58df488295e8e6f390a6
Will Harris <wfh@chromium.org>
2023-10-13 16:45:44 +0000

    Correctly uninstall the KM-TEST Loopback Adapter after using it.
    The SandboxedNetworkChangeNotifierBrowserTest adds a test
    "Microsoft KM-TEST Loopback Adapter" to verify network change
    notifier behavior on Windows.
    
    However, on certain platform configurations, simply removing the
    device node did not correctly trigger the PnP manager to spot that
    the device was no longer present, causing the network interface to
    remain after the test had concluded (until the machine rebooted).
    
    This CL updates the test to correctly call DiUninstallDevice to
    match the SetupDiCallClassInstaller call, to correctly uninstall
    the device completely.
    
    This was tested manually by remoting into the bots, and verifying
    that the network interface always gets removed after test finishes,
    so as a result, this CL also re-enables the test.
    
    BUG=1487750
    
    Change-Id: I8ffb88ceeab65434dc2dfba7836ab44eae56bf87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935947
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209492}

--- chrome/browser/net/sandboxed_network_change_notifier_win_browsertest.cc
+++ chrome/browser/net/sandboxed_network_change_notifier_win_browsertest.cc
@@ -43,6 +43,22 @@ namespace content {
 
 namespace {
 
+void UninstallAllMatchingDevices(base::win::ScopedDevInfo dev_info) {
+  SP_DEVINFO_DATA device_info_data = {};
+  device_info_data.cbSize = sizeof(SP_DEVINFO_DATA);
+
+  DWORD member_index = 0;
+  while (::SetupDiEnumDeviceInfo(dev_info.get(), member_index,
+                                 &device_info_data)) {
+    // Explicitly continue on failure, to make sure that all devices are
+    // correctly removed.
+    std::ignore = ::DiUninstallDevice(/*hwndParent=*/nullptr, dev_info.get(),
+                                      &device_info_data, /*Flags=*/0,
+                                      /*NeedReboot=*/nullptr);
+    member_index++;
+  }
+}
+
 absl::optional<base::ScopedClosureRunner> InstallAdapter(
     const base::FilePath& inf,
     const std::wstring hwid) {
@@ -92,9 +108,8 @@ absl::optional<base::ScopedClosureRunner> InstallAdapter(
     return absl::nullopt;
   }
 
-  return base::ScopedClosureRunner(base::BindOnce(
-      [](DEVINST devinst) { std::ignore = ::CM_Uninstall_DevNode(devinst, 0); },
-      deviceInfoData.DevInst));
+  return base::ScopedClosureRunner(
+      base::BindOnce(&UninstallAllMatchingDevices, std::move(dev_info)));
 }
 
 class MockNetworkChangeManagerClient
@@ -160,11 +175,8 @@ class SandboxedNetworkChangeNotifierBrowserTest
 // The network service is able to see these network adapter changes, as it is
 // created with the LPAC "internetClient" capability. See
 // https://learn.microsoft.com/en-us/windows/uwp/packaging/app-capability-declarations
-// TODO(crbug.com/1487750): This test install a new network interface that is
-// causing wrong route path added to bots and blocks the DNS lookup or causing
-// bot died.
 IN_PROC_BROWSER_TEST_P(SandboxedNetworkChangeNotifierBrowserTest,
-                       DISABLED_AddNetworkAdapter) {
+                       AddNetworkAdapter) {
   if (!::IsUserAnAdmin()) {
     GTEST_SKIP() << "This test requires running elevated.";
   }
