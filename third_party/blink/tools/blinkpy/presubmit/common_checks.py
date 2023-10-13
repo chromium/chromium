e2ffccf0891353b9d70c48b871369fe69442c66e
Jonathan Lee <jonathanjlee@google.com>
2023-10-13 18:36:23 +0000

    Reland "[web-tests] Clean up obsolete WPT metadata checks"
    This is a reland of commit 63e5deadc1768b8f13bf004f3dd1525ffa02da14
    
    Changes since original: Restore `--ignore-glob=*.ini` option [1].
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/4425277/3/third_party/blink/web_tests/external/PRESUBMIT.py
    
    Original change's description:
    > [web-tests] Clean up obsolete WPT metadata checks
    >
    > These aren't necessary anymore since we plan to abandon the `.ini`
    > format. Two of the new checks regressed performance significantly:
    > * `lint-wpt`: Unnecessarily regenerates the manifest.
    > * `_check_no_wpt_lines(...)` in `lint_test_expectations.py`: Needs to
    >   iterate the entire manifest to map URLs to files; this is a lazy
    >   operation that was not called before.
    >
    > With this change, [0] now takes 40s, not 65s.
    >
    > [0]: git cl presubmit --upload -v
    >   --files='third_party/blink/web_tests/external/wpt/badging*'
    >
    > Bug: 1492264, 1492238
    > Change-Id: Ifd8e11718af9314f54d335b483034a63c619a9cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935624
    > Reviewed-by: Weizhong Xia <weizhong@google.com>
    > Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1209171}
    
    Bug: 1492264, 1492238
    Change-Id: Ia3adcb22b07bf857d656a90a0a5a6a226a03d75e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935456
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209559}

--- third_party/blink/tools/blinkpy/presubmit/common_checks.py
+++ third_party/blink/tools/blinkpy/presubmit/common_checks.py
@@ -8,29 +8,23 @@ from typing import Optional
 
 
 def lint_wpt_root(input_api, output_api, repo_root: Optional[str] = None):
-    """Run `blink_tool.py lint-wpt` against the specified directory."""
+    """Run `wpt lint` against the specified directory."""
     repo_root = repo_root or input_api.PresubmitLocalPath()
-    tool_path = input_api.os_path.join(input_api.os_path.dirname(__file__),
-                                       input_api.os_path.pardir,
-                                       input_api.os_path.pardir,
-                                       'blink_tool.py')
+    wpt_executable = input_api.os_path.join(input_api.change.RepositoryRoot(),
+                                            'third_party', 'wpt_tools', 'wpt',
+                                            'wpt')
 
-    # TODO(crbug.com/1406669): After switching to wptrunner, changing a test
-    # file should also lint its corresponding metadata file, if any, because the
-    # test-side change may invalidate the metadata file's contents. For example,
-    # removing a test variant will orphan its expectations, which the linter
-    # should flag for cleanup.
+    # TODO(crbug.com/1406669): Changing a test file should also lint its
+    # corresponding reference/*-expected.txt file, if any, because the
+    # test-side change may invalidate the other files' contents. For example,
+    # removing a test variant will orphan its expectations.
     paths = []
     for abs_path in input_api.AbsoluteLocalPaths():
-        # For now, skip checking metadata files in `presubmit --{files,all}` for
-        # the invalidation reason mentioned above.
-        if input_api.no_diffs and abs_path.endswith('.ini'):
-            continue
         if abs_path.endswith(input_api.os_path.relpath(abs_path, repo_root)):
             paths.append(abs_path)
 
-    # Without an explicit file list, `lint-wpt` will lint all files in the root,
-    # which is slow.
+    # Without an explicit file list, `wpt lint` will lint all files in the
+    # root, which is slow.
     if not paths:
         return []
 
@@ -42,12 +36,13 @@ def lint_wpt_root(input_api, output_api, repo_root: Optional[str] = None):
         paths_name = f.name
     args = [
         input_api.python3_executable,
-        tool_path,
-        'lint-wpt',
+        wpt_executable,
+        'lint',
         '--repo-root=%s' % repo_root,
         # To avoid false positives, do not lint files not upstreamed from
         # Chromium.
         '--ignore-glob=*-expected.txt',
+        '--ignore-glob=*.ini',
         '--ignore-glob=*DIR_METADATA',
         '--ignore-glob=*OWNERS',
         '--ignore-glob=config.json',
@@ -62,7 +57,7 @@ def lint_wpt_root(input_api, output_api, repo_root: Optional[str] = None):
 
     if proc.returncode != 0:
         return [
-            output_api.PresubmitError('`blink_tool.py lint-wpt` failed:',
+            output_api.PresubmitError('`wpt lint` failed:',
                                       long_text=(stdout + stderr).decode())
         ]
     return []
