02bb14c1b9545cc8e9d5c675bbd5b2a93a4bf2e8
Elias Khsheibun <eliaskh@google.com>
2023-10-13 09:05:39 +0000

    Revert "Reland "[Incognito] Field trial for HideIncognitoMediaMetadata""
    This reverts commit 8d804edf7920564e902c7c1f983b5e51fbb65d37.
    
    Reason for revert: Failing tests: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/43664/overview
    
    Original change's description:
    > Reland "[Incognito] Field trial for HideIncognitoMediaMetadata"
    >
    > This is a reland of commit e788935ccc7b6e027fbed391103b45961d9d0f1b
    >
    > Original change's description:
    > > [Incognito] Field trial for HideIncognitoMediaMetadata
    > >
    > > Bug: 1447545
    > > Change-Id: Ie067fc9878fd3031ce2b31cdbdffbc979ef08afe
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4866390
    > > Auto-Submit: Filipa Senra <fsenra@google.com>
    > > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1204757}
    >
    > Bug: 1447545
    > Change-Id: I50cea57116c9f9fe156033543d58593449e3fb3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4909596
    > Auto-Submit: Filipa Senra <fsenra@google.com>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1208832}
    
    Bug: 1447545
    Change-Id: Ib64b161d1aa3bc8beb768e23b51746d5c4ba7325
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4935355
    Owners-Override: Elias Khsheibun <eliaskh@google.com>
    Auto-Submit: Elias Khsheibun <eliaskh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Elias Khsheibun <eliaskh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1209304}

--- chrome/browser/media/history/media_history_browsertest.cc
+++ chrome/browser/media/history/media_history_browsertest.cc
@@ -31,7 +31,6 @@
 #include "content/public/browser/browsing_data_filter_builder.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/browser/media_session.h"
-#include "content/public/browser/media_session_client.h"
 #include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browsing_data_remover_test_util.h"
@@ -67,8 +66,7 @@ class MediaHistoryBrowserTest : public InProcessBrowserTest,
                                 public testing::WithParamInterface<TestState> {
  public:
   MediaHistoryBrowserTest() {
-    scoped_feature_list_.InitWithFeatures(
-        {media::kUseMediaHistoryStore, media::kHideIncognitoMediaMetadata}, {});
+    scoped_feature_list_.InitAndEnableFeature(media::kUseMediaHistoryStore);
   }
 
   ~MediaHistoryBrowserTest() override = default;
@@ -210,14 +208,7 @@ class MediaHistoryBrowserTest : public InProcessBrowserTest,
 
   media_session::MediaMetadata GetExpectedMetadata() {
     media_session::MediaMetadata expected_metadata =
-        GetExpectedDefaultActiveMetadata();
-
-#if BUILDFLAG(IS_CHROMEOS)
-    if (GetParam() == TestState::kIncognito) {
-      return expected_metadata;
-    }
-#endif  // BUILDFLAG(IS_CHROMEOS)
-
+        GetExpectedDefaultMetadata();
     expected_metadata.title = u"Big Buck Bunny";
     expected_metadata.artist = u"Test Footage";
     expected_metadata.album = u"The Chrome Collection";
@@ -226,13 +217,6 @@ class MediaHistoryBrowserTest : public InProcessBrowserTest,
 
   std::vector<media_session::MediaImage> GetExpectedArtwork() {
     std::vector<media_session::MediaImage> images;
-
-#if BUILDFLAG(IS_CHROMEOS)
-    if (GetParam() == TestState::kIncognito) {
-      images.push_back(media_session::MediaImage());
-      return images;
-    }
-#endif  // BUILDFLAG(IS_CHROMEOS)
 
     {
       media_session::MediaImage image;
@@ -284,30 +268,8 @@ class MediaHistoryBrowserTest : public InProcessBrowserTest,
     return images;
   }
 
-  media_session::MediaMetadata GetExpectedDefaultActiveMetadata() {
-    media_session::MediaMetadata expected_metadata =
-        GetExpectedDefaultMetadata();
-
-#if BUILDFLAG(IS_CHROMEOS)
-    if (GetParam() == TestState::kIncognito) {
-      content::MediaSessionClient* media_session_client =
-          content::MediaSessionClient::Get();
-
-      expected_metadata.title = media_session_client->GetTitlePlaceholder();
-      expected_metadata.source_title =
-          media_session_client->GetSourceTitlePlaceholder();
-      expected_metadata.album = media_session_client->GetAlbumPlaceholder();
-      expected_metadata.artist = media_session_client->GetArtistPlaceholder();
-      return expected_metadata;
-    }
-#endif  // BUILDFLAG(IS_CHROMEOS)
-
-    return expected_metadata;
-  }
-
   media_session::MediaMetadata GetExpectedDefaultMetadata() {
     media_session::MediaMetadata expected_metadata;
-
     expected_metadata.title = u"Media History";
     expected_metadata.source_title = base::ASCIIToUTF16(base::StringPrintf(
         "%s:%u", embedded_test_server()->GetIPLiteralString().c_str(),
@@ -460,8 +422,7 @@ IN_PROC_BROWSER_TEST_P(MediaHistoryBrowserTest,
 
   EXPECT_TRUE(SetupPageAndStartPlaying(browser, GetTestURL()));
 
-  media_session::MediaMetadata expected_metadata =
-      GetExpectedDefaultActiveMetadata();
+  media_session::MediaMetadata expected_metadata = GetExpectedDefaultMetadata();
 
   {
     media_session::test::MockMediaSessionMojoObserver observer(
@@ -506,8 +467,7 @@ IN_PROC_BROWSER_TEST_P(MediaHistoryBrowserTest,
   EXPECT_TRUE(SetupPageAndStartPlaying(browser, GetTestURL()));
   EXPECT_TRUE(FinishPlaying(browser));
 
-  media_session::MediaMetadata expected_metadata =
-      GetExpectedDefaultActiveMetadata();
+  media_session::MediaMetadata expected_metadata = GetExpectedDefaultMetadata();
 
   {
     media_session::test::MockMediaSessionMojoObserver observer(
@@ -584,7 +544,7 @@ IN_PROC_BROWSER_TEST_P(MediaHistoryBrowserTest, DoNotRecordSessionIfNotActive) {
 // Flaky failures: crbug.com/1066853
 IN_PROC_BROWSER_TEST_P(MediaHistoryBrowserTest, DISABLED_GetPlaybackSessions) {
   auto* browser = CreateBrowserFromParam();
-  auto expected_default_metadata = GetExpectedDefaultActiveMetadata();
+  auto expected_default_metadata = GetExpectedDefaultMetadata();
 
   {
     // Start a session.
