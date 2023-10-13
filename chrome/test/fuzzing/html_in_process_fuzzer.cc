e6778d77d011391d03ddbb3d93961da7d130582c
Adrian Taylor <adetaylor@chromium.org>
2023-10-13 20:58:31 +0000

    Add page_load_in_process_fuzzer
    Overall, this CL splits html_in_process_fuzzer into two fuzzers:
    * html_in_process_fuzzer becomes simpler and no longer involves network
      navigations
    * a new page_load_in_process_fuzzer is able to explore network loads
      with custom headers, status codes, etc.
    
    Specifically, html_in_proces_fuzzer now loads pages as simple data:
    URIs so there's no call through to the http(s) test server.
    
    The new page_load_in_process_fuzzer is a protobuf-based fuzzer such that
    we can later feed it custom protos (as requested by security shepherds,
    sort of). It can simulate network loads over HTTP and HTTPS with custom
    headers and response codes, as well as simulating custom HTML.
    
    Bug: 1361343, 1471958
    Change-Id: Iaa61c7470f258f52640b3329bef900b58af68daa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4902486
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1209665}

--- chrome/test/fuzzing/html_in_process_fuzzer.cc
+++ chrome/test/fuzzing/html_in_process_fuzzer.cc
@@ -1,93 +1,39 @@
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#include "base/functional/bind.h"
-#include "base/memory/weak_ptr.h"
-#include "base/test/bind.h"
+
+#include "base/strings/escape.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/fuzzing/in_process_fuzzer.h"
-#include "content/public/browser/browser_thread.h"
-#include "net/dns/mock_host_resolver.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
 
 // This is an example use of the InProcessFuzzer framework.
 // It fetches arbitrary HTML from an HTTPS server. It's not really
 // intended to be an effective fuzzer, but just to show an example
 // of how this framework can be used.
 
-class HtmlInProcessFuzzer : virtual public InProcessFuzzer {
+class HtmlInProcessFuzzer : public InProcessFuzzer {
  public:
-  HtmlInProcessFuzzer()
-      : http_test_server_(net::EmbeddedTestServer::TYPE_HTTP) {}
+  HtmlInProcessFuzzer() = default;
 
-  void SetUpOnMainThread() override;
   int Fuzz(const uint8_t* data, size_t size) override;
-  static std::unique_ptr<net::test_server::HttpResponse> HandleHTTPRequest(
-      base::WeakPtr<HtmlInProcessFuzzer> fuzzer_weak,
-      const net::test_server::HttpRequest& request);
 
-  net::EmbeddedTestServer http_test_server_;
+ private:
   std::string current_fuzz_case_;
-  base::WeakPtrFactory<HtmlInProcessFuzzer> weak_ptr_factory_{this};
 };
 
 REGISTER_IN_PROCESS_FUZZER(HtmlInProcessFuzzer)
 
-void HtmlInProcessFuzzer::SetUpOnMainThread() {
-  InProcessFuzzer::SetUpOnMainThread();
-  host_resolver()->AddRule("*", "127.0.0.1");
-  // Previous versions of this fuzzer used HTTPS, but on ClusterFuzz,
-  // data_deps are not available and thus the SSL config did not work.
-  // For now, use simple HTTP.
-  // TODO(crbug.com/1463972)
-  // http_test_server_.SetSSLConfig(net::EmbeddedTestServer::CERT_OK);
-  http_test_server_.RegisterRequestHandler(base::BindRepeating(
-      &HtmlInProcessFuzzer::HandleHTTPRequest, weak_ptr_factory_.GetWeakPtr()));
-  ASSERT_TRUE(http_test_server_.Start());
-}
-
-std::unique_ptr<net::test_server::HttpResponse>
-HtmlInProcessFuzzer::HandleHTTPRequest(
-    base::WeakPtr<HtmlInProcessFuzzer> fuzzer_weak,
-    const net::test_server::HttpRequest& request) {
-  std::unique_ptr<net::test_server::BasicHttpResponse> response;
-  response = std::make_unique<net::test_server::BasicHttpResponse>();
-  response->set_content_type("text/html");
-  std::string response_body = "";
-  // We are running on the embedded test server's thread.
-  // We want to ask the fuzzer thread for the latest HTML payload,
-  // but there's a risk of UaF if it's being destroyed.
-  // We use a weak pointer, but we have to dereference that on the originating
-  // thread.
-  base::RunLoop run_loop;
-  base::RepeatingCallback<void()> get_payload_lambda =
-      base::BindLambdaForTesting([&]() {
-        HtmlInProcessFuzzer* fuzzer = fuzzer_weak.get();
-        if (fuzzer) {
-          response_body = fuzzer->current_fuzz_case_;
-        }
-        run_loop.Quit();
-      });
-  content::GetUIThreadTaskRunner()->PostTask(FROM_HERE, get_payload_lambda);
-  run_loop.Run();
-  response->set_content(response_body);
-  response->set_code(net::HTTP_OK);
-  return response;
-}
-
 int HtmlInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
   std::string html_string(reinterpret_cast<const char*>(data), size);
-  current_fuzz_case_ = html_string;
-  GURL test_url = http_test_server_.GetURL("/test.html");
-
-  base::RunLoop run_loop;
-  base::RepeatingCallback<void()> run_fuzz_case_lambda =
-      base::BindLambdaForTesting([&]() {
-        base::IgnoreResult(ui_test_utils::NavigateToURL(browser(), test_url));
-        run_loop.Quit();
-      });
-  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, run_fuzz_case_lambda);
-  run_loop.Run();
+  // See
+  // docs/security/url_display_guidelines/url_display_guidelines.md#url-length
+  const size_t kMaxUrlLength = 2 * 1024 * 1024;
+  std::string url_string = "data:text/html;charset=utf-8,";
+  const bool kUsePlus = false;
+  url_string.append(base::EscapeQueryParamValue(html_string, kUsePlus));
+  if (url_string.length() > kMaxUrlLength) {
+    return -1;
+  }
+  base::IgnoreResult(ui_test_utils::NavigateToURL(browser(), GURL(url_string)));
   return 0;
 }
---
+++ chrome/test/fuzzing/page_load_in_process_fuzzer.cc
@@ -0,0 +1,138 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include <fuzzer/FuzzedDataProvider.h>
+#include <google/protobuf/descriptor.h>
+#include "base/functional/bind.h"
+#include "base/memory/weak_ptr.h"
+#include "base/strings/escape.h"
+#include "base/strings/strcat.h"
+#include "base/test/bind.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/fuzzing/in_process_proto_fuzzer.h"
+#include "chrome/test/fuzzing/page_load_in_process_fuzzer.pb.h"
+#include "content/public/browser/browser_thread.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/libfuzzer/proto/lpm_interface.h"
+
+// A fuzzer which can test the interaction of HTTP response parameters
+// and HTML content. This is a large search space and it's unlikely that
+// this fuzzer will presently find interesting results, but future
+// technologies that can better explore a search space like this may
+// successfully do so. Meanwhile, it may be useful to aid reproduction
+// of human-crafted test cases.
+//
+// In the future we might want to extend this fuzzer to:
+// * support different HTTPS parameters too
+// * support multiple, different, HTTP(S) responses in order to
+//   handle iframes or other types of navigation.
+//   (We'd need to provide a corpus designed to exercise these).
+// * run servers on 3+ different ports to support cross-origin navigations
+
+class PageLoadInProcessFuzzer : public InProcessFuzzer {
+ public:
+  using FuzzCase = test::fuzzing::page_load_fuzzing::FuzzCase;
+  PageLoadInProcessFuzzer();
+
+  void SetUpOnMainThread() override;
+  int Fuzz(const uint8_t* data, size_t size) override;
+
+ private:
+  static std::unique_ptr<net::test_server::HttpResponse> HandleHTTPRequest(
+      base::WeakPtr<PageLoadInProcessFuzzer> fuzzer_weak,
+      const net::test_server::HttpRequest& request);
+
+ private:
+  net::EmbeddedTestServer http_test_server_;
+  net::EmbeddedTestServer https_test_server_;
+  std::unique_ptr<net::test_server::BasicHttpResponse> http_response_;
+  base::WeakPtrFactory<PageLoadInProcessFuzzer> weak_ptr_factory_{this};
+};
+
+REGISTER_TEXT_PROTO_IN_PROCESS_FUZZER(PageLoadInProcessFuzzer);
+
+PageLoadInProcessFuzzer::PageLoadInProcessFuzzer()
+    : http_test_server_(net::EmbeddedTestServer::TYPE_HTTP),
+      https_test_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
+  https_test_server_.SetSSLConfig(net::EmbeddedTestServer::CERT_OK);
+  http_test_server_.RegisterRequestHandler(
+      base::BindRepeating(&PageLoadInProcessFuzzer::HandleHTTPRequest,
+                          weak_ptr_factory_.GetWeakPtr()));
+  https_test_server_.RegisterRequestHandler(
+      base::BindRepeating(&PageLoadInProcessFuzzer::HandleHTTPRequest,
+                          weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PageLoadInProcessFuzzer::SetUpOnMainThread() {
+  InProcessFuzzer::SetUpOnMainThread();
+  host_resolver()->AddRule("*", "127.0.0.1");
+  ASSERT_TRUE(http_test_server_.Start());
+  ASSERT_TRUE(https_test_server_.Start());
+}
+
+std::unique_ptr<net::test_server::HttpResponse>
+PageLoadInProcessFuzzer::HandleHTTPRequest(
+    base::WeakPtr<PageLoadInProcessFuzzer> fuzzer_weak,
+    const net::test_server::HttpRequest& request) {
+  std::unique_ptr<net::test_server::BasicHttpResponse> response;
+  // We are running on the embedded test server's thread.
+  // We want to ask the fuzzer thread for the fuzz case.
+  // We use a weak pointer, but we have to dereference that on the originating
+  // thread.
+  base::RunLoop run_loop;
+  base::RepeatingCallback<void()> get_payload_lambda =
+      base::BindLambdaForTesting([&]() {
+        PageLoadInProcessFuzzer* fuzzer = fuzzer_weak.get();
+        if (fuzzer) {
+          response = std::move(fuzzer->http_response_);
+        }
+        run_loop.Quit();
+      });
+  content::GetUIThreadTaskRunner()->PostTask(FROM_HERE, get_payload_lambda);
+  run_loop.Run();
+  return response;
+}
+
+int PageLoadInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
+  FuzzCase fuzz_case;
+  fuzz_case.ParseFromArray(data, size);
+
+  GURL test_url;
+
+  std::string content_type = fuzz_case.content_type();
+  std::string body = fuzz_case.body();
+
+  if (fuzz_case.has_network_load()) {
+    // Load the page over a network. Slow but we can provide lots of options
+    // and realism.
+    const auto& network_load = fuzz_case.network_load();
+    std::string path = network_load.path();
+    if (network_load.https()) {
+      test_url = https_test_server_.GetURL(path);
+    } else {
+      test_url = http_test_server_.GetURL(path);
+    }
+
+    std::unique_ptr<net::test_server::BasicHttpResponse> response =
+        std::make_unique<net::test_server::BasicHttpResponse>();
+
+    // Now figure out what sort of response the server will provide
+    response->set_code(
+        static_cast<net::HttpStatusCode>(network_load.http_status()));
+    response->set_content_type(content_type);
+    for (const auto& header : network_load.custom_headers()) {
+      response->AddCustomHeader(header.key(), header.value());
+    }
+    if (network_load.has_reason()) {
+      response->set_reason(network_load.reason());
+    }
+    http_response_ = std::move(response);
+  } else {
+    // Request via a data: URI which should be quicker.
+    test_url = GURL(base::StrCat({"data:", content_type, ";charset=utf-8,",
+                                  base::EscapeQueryParamValue(body, false)}));
+  }
+  base::IgnoreResult(ui_test_utils::NavigateToURL(browser(), test_url));
+  return 0;
+}
