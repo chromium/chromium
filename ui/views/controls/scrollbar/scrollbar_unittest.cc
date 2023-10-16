593aa35a762987eaf3b32549cacbc0f872811fee
Jan Keitel <jkeitel@google.com>
2023-10-16 17:18:32 +0000

    Avoid dangling pointers in ScrollBar test.
    Bug: 1469136
    Change-Id: I663c5064108186fa83d37e11a008b9f1137d6d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4938215
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210268}

--- ui/views/controls/scrollbar/scrollbar_unittest.cc
+++ ui/views/controls/scrollbar/scrollbar_unittest.cc
@@ -20,20 +20,22 @@
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_utils.h"
 
+namespace views {
+
 namespace {
 
 // The Scrollbar controller. This is the widget that should do the real
 // scrolling of contents.
-class TestScrollBarController : public views::ScrollBarController {
+class TestScrollBarController : public ScrollBarController {
  public:
   virtual ~TestScrollBarController() = default;
 
-  void ScrollToPosition(views::ScrollBar* source, int position) override {
+  void ScrollToPosition(ScrollBar* source, int position) override {
     last_source = source;
     last_position = position;
   }
 
-  int GetScrollIncrement(views::ScrollBar* source,
+  int GetScrollIncrement(ScrollBar* source,
                          bool is_page,
                          bool is_positive) override {
     last_source = source;
@@ -47,15 +49,15 @@ class TestScrollBarController : public views::ScrollBarController {
 
   // We save the last values in order to assert the correctness of the scroll
   // operation.
-  raw_ptr<views::ScrollBar, AcrossTasksDanglingUntriaged> last_source;
-  bool last_is_positive;
-  bool last_is_page;
-  int last_position;
+  raw_ptr<ScrollBar> last_source = nullptr;
+  bool last_is_positive = false;
+  bool last_is_page = false;
+  int last_position = 0;
 };
 
 // This container is used to forward gesture events to the scrollbar for
 // testing fling and other gestures.
-class TestScrollbarContainer : public views::View {
+class TestScrollbarContainer : public View {
  public:
   TestScrollbarContainer() = default;
   ~TestScrollbarContainer() override = default;
@@ -69,10 +71,10 @@ class TestScrollbarContainer : public views::View {
 
 }  // namespace
 
-namespace views {
-
 class ScrollBarViewsTest : public ViewsTestBase {
  public:
+  static constexpr int kScrollBarID = 123;
+
   ScrollBarViewsTest() = default;
 
   void SetUp() override {
@@ -87,24 +89,30 @@ class ScrollBarViewsTest : public ViewsTestBase {
     auto* container =
         widget_->SetContentsView(std::make_unique<TestScrollbarContainer>());
 
-    scrollbar_ = container->AddChildView(std::make_unique<ScrollBarViews>());
-    scrollbar_->SetBounds(0, 0, 100, 100);
-    scrollbar_->Update(100, 1000, 0);
-    scrollbar_->set_controller(controller_.get());
+    auto* bar = container->AddChildView(std::make_unique<ScrollBarViews>());
+    bar->SetBounds(0, 0, 100, 100);
+    bar->Update(100, 1000, 0);
+    bar->set_controller(controller_.get());
+    bar->SetID(kScrollBarID);
 
-    track_size_ = scrollbar_->GetTrackBounds().width();
+    track_size_ = scrollbar()->GetTrackBounds().width();
   }
 
   void TearDown() override {
+    scrollbar()->set_controller(nullptr);
+    controller_.reset();
     widget_.reset();
     ViewsTestBase::TearDown();
   }
 
  protected:
+  ScrollBar* scrollbar() {
+    return static_cast<ScrollBar*>(
+        widget_->GetContentsView()->GetViewByID(kScrollBarID));
+  }
+
   UniqueWidgetPtr widget_;
 
-  // This is the Views scrollbar.
-  raw_ptr<ScrollBar, AcrossTasksDanglingUntriaged> scrollbar_ = nullptr;
 
   // Keep track of the size of the track. This is how we can tell when we
   // scroll to the middle.
@@ -115,181 +123,181 @@ class ScrollBarViewsTest : public ViewsTestBase {
 
 // Verify properties are accessible via metadata.
 TEST_F(ScrollBarViewsTest, MetaDataTest) {
-  test::TestViewMetadata(scrollbar_);
+  test::TestViewMetadata(scrollbar());
 }
 
 TEST_F(ScrollBarViewsTest, Scrolling) {
-  EXPECT_EQ(0, scrollbar_->GetPosition());
-  EXPECT_EQ(900, scrollbar_->GetMaxPosition());
-  EXPECT_EQ(0, scrollbar_->GetMinPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
+  EXPECT_EQ(900, scrollbar()->GetMaxPosition());
+  EXPECT_EQ(0, scrollbar()->GetMinPosition());
 
   // Scroll to middle.
-  scrollbar_->ScrollToThumbPosition(track_size_ / 2, true);
+  scrollbar()->ScrollToThumbPosition(track_size_ / 2, true);
   EXPECT_EQ(450, controller_->last_position);
-  EXPECT_EQ(scrollbar_, controller_->last_source);
+  EXPECT_EQ(scrollbar(), controller_->last_source);
 
   // Scroll to the end.
-  scrollbar_->ScrollToThumbPosition(track_size_, true);
+  scrollbar()->ScrollToThumbPosition(track_size_, true);
   EXPECT_EQ(900, controller_->last_position);
 
   // Overscroll. Last position should be the maximum position.
-  scrollbar_->ScrollToThumbPosition(track_size_ + 100, true);
+  scrollbar()->ScrollToThumbPosition(track_size_ + 100, true);
   EXPECT_EQ(900, controller_->last_position);
 
   // Underscroll. Last position should be the minimum position.
-  scrollbar_->ScrollToThumbPosition(-10, false);
+  scrollbar()->ScrollToThumbPosition(-10, false);
   EXPECT_EQ(0, controller_->last_position);
 
   // Test the different fixed scrolling amounts. Generally used by buttons,
   // or click on track.
-  scrollbar_->ScrollToThumbPosition(0, false);
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
+  scrollbar()->ScrollToThumbPosition(0, false);
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
   EXPECT_EQ(10, controller_->last_position);
 
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kPrevLine);
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kPrevLine);
   EXPECT_EQ(0, controller_->last_position);
 
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kNextPage);
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kNextPage);
   EXPECT_EQ(20, controller_->last_position);
 
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kPrevPage);
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kPrevPage);
   EXPECT_EQ(0, controller_->last_position);
 }
 
 TEST_F(ScrollBarViewsTest, ScrollBarFitsToBottom) {
-  scrollbar_->Update(100, 1999, 0);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
-  EXPECT_EQ(1899, scrollbar_->GetMaxPosition());
-  EXPECT_EQ(0, scrollbar_->GetMinPosition());
+  scrollbar()->Update(100, 1999, 0);
+  EXPECT_EQ(0, scrollbar()->GetPosition());
+  EXPECT_EQ(1899, scrollbar()->GetMaxPosition());
+  EXPECT_EQ(0, scrollbar()->GetMinPosition());
 
   // Scroll to the midpoint of the document.
-  scrollbar_->Update(100, 1999, 950);
-  EXPECT_EQ((scrollbar_->GetTrackBounds().width() -
-             scrollbar_->GetThumbLengthForTesting()) /
+  scrollbar()->Update(100, 1999, 950);
+  EXPECT_EQ((scrollbar()->GetTrackBounds().width() -
+             scrollbar()->GetThumbLengthForTesting()) /
                 2,
-            scrollbar_->GetPosition());
+            scrollbar()->GetPosition());
 
   // Scroll to the end of the document.
-  scrollbar_->Update(100, 1999, 1899);
-  EXPECT_EQ(scrollbar_->GetTrackBounds().width() -
-                scrollbar_->GetThumbLengthForTesting(),
-            scrollbar_->GetPosition());
+  scrollbar()->Update(100, 1999, 1899);
+  EXPECT_EQ(scrollbar()->GetTrackBounds().width() -
+                scrollbar()->GetThumbLengthForTesting(),
+            scrollbar()->GetPosition());
 }
 
 TEST_F(ScrollBarViewsTest, ScrollToEndAfterShrinkAndExpand) {
   // Scroll to the end of the content.
-  scrollbar_->Update(100, 1001, 0);
-  EXPECT_TRUE(scrollbar_->ScrollByContentsOffset(-1));
+  scrollbar()->Update(100, 1001, 0);
+  EXPECT_TRUE(scrollbar()->ScrollByContentsOffset(-1));
   // Shrink and then re-expand the content.
-  scrollbar_->Update(100, 1000, 0);
-  scrollbar_->Update(100, 1001, 0);
+  scrollbar()->Update(100, 1000, 0);
+  scrollbar()->Update(100, 1001, 0);
   // Ensure the scrollbar allows scrolling to the end.
-  EXPECT_TRUE(scrollbar_->ScrollByContentsOffset(-1));
+  EXPECT_TRUE(scrollbar()->ScrollByContentsOffset(-1));
 }
 
 TEST_F(ScrollBarViewsTest, ThumbFullLengthOfTrack) {
   // Shrink content so that it fits within the viewport.
-  scrollbar_->Update(100, 10, 0);
-  EXPECT_EQ(scrollbar_->GetTrackBounds().width(),
-            scrollbar_->GetThumbLengthForTesting());
+  scrollbar()->Update(100, 10, 0);
+  EXPECT_EQ(scrollbar()->GetTrackBounds().width(),
+            scrollbar()->GetThumbLengthForTesting());
   // Emulate a click on the full size scroll bar.
-  scrollbar_->ScrollToThumbPosition(0, false);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  scrollbar()->ScrollToThumbPosition(0, false);
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   // Emulate a key down.
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
+  EXPECT_EQ(0, scrollbar()->GetPosition());
 
   // Expand content so that it fits *exactly* within the viewport.
-  scrollbar_->Update(100, 100, 0);
-  EXPECT_EQ(scrollbar_->GetTrackBounds().width(),
-            scrollbar_->GetThumbLengthForTesting());
+  scrollbar()->Update(100, 100, 0);
+  EXPECT_EQ(scrollbar()->GetTrackBounds().width(),
+            scrollbar()->GetThumbLengthForTesting());
   // Emulate a click on the full size scroll bar.
-  scrollbar_->ScrollToThumbPosition(0, false);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  scrollbar()->ScrollToThumbPosition(0, false);
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   // Emulate a key down.
-  scrollbar_->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  scrollbar()->ScrollByAmount(ScrollBar::ScrollAmount::kNextLine);
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   // Emulate a programmatic scroll to a non-zero offset. See crbug.com/1447967.
-  scrollbar_->Update(100, 100, 10);
-  EXPECT_EQ(scrollbar_->GetTrackBounds().width(),
-            scrollbar_->GetThumbLengthForTesting());
+  scrollbar()->Update(100, 100, 10);
+  EXPECT_EQ(scrollbar()->GetTrackBounds().width(),
+            scrollbar()->GetThumbLengthForTesting());
 }
 
 TEST_F(ScrollBarViewsTest, AccessibleRole) {
   ui::AXNodeData data;
-  scrollbar_->GetAccessibleNodeData(&data);
+  scrollbar()->GetAccessibleNodeData(&data);
   EXPECT_EQ(data.role, ax::mojom::Role::kScrollBar);
-  EXPECT_EQ(scrollbar_->GetAccessibleRole(), ax::mojom::Role::kScrollBar);
+  EXPECT_EQ(scrollbar()->GetAccessibleRole(), ax::mojom::Role::kScrollBar);
 
   data = ui::AXNodeData();
-  scrollbar_->SetAccessibleRole(ax::mojom::Role::kButton);
-  scrollbar_->GetAccessibleNodeData(&data);
+  scrollbar()->SetAccessibleRole(ax::mojom::Role::kButton);
+  scrollbar()->GetAccessibleNodeData(&data);
   EXPECT_EQ(data.role, ax::mojom::Role::kButton);
-  EXPECT_EQ(scrollbar_->GetAccessibleRole(), ax::mojom::Role::kButton);
+  EXPECT_EQ(scrollbar()->GetAccessibleRole(), ax::mojom::Role::kButton);
 }
 
 #if !BUILDFLAG(IS_MAC)
 TEST_F(ScrollBarViewsTest, RightClickOpensMenu) {
-  EXPECT_EQ(nullptr, scrollbar_->menu_model_);
-  EXPECT_EQ(nullptr, scrollbar_->menu_runner_);
-  scrollbar_->set_context_menu_controller(scrollbar_);
+  EXPECT_EQ(nullptr, scrollbar()->menu_model_);
+  EXPECT_EQ(nullptr, scrollbar()->menu_runner_);
+  scrollbar()->set_context_menu_controller(scrollbar());
   // Disabled on Mac because Mac's native menu is synchronous.
-  scrollbar_->ShowContextMenu(scrollbar_->GetBoundsInScreen().CenterPoint(),
-                              ui::MENU_SOURCE_MOUSE);
-  EXPECT_NE(nullptr, scrollbar_->menu_model_);
-  EXPECT_NE(nullptr, scrollbar_->menu_runner_);
+  scrollbar()->ShowContextMenu(scrollbar()->GetBoundsInScreen().CenterPoint(),
+                               ui::MENU_SOURCE_MOUSE);
+  EXPECT_NE(nullptr, scrollbar()->menu_model_);
+  EXPECT_NE(nullptr, scrollbar()->menu_runner_);
 }
 
 TEST_F(ScrollBarViewsTest, TestPageScrollingByPress) {
   ui::test::EventGenerator generator(GetRootWindow(widget_.get()));
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   generator.MoveMouseTo(
-      scrollbar_->GetThumb()->GetBoundsInScreen().right_center() +
+      scrollbar()->GetThumb()->GetBoundsInScreen().right_center() +
       gfx::Vector2d(4, 0));
   generator.ClickLeftButton();
   generator.ClickLeftButton();
-  EXPECT_GT(scrollbar_->GetPosition(), 0);
+  EXPECT_GT(scrollbar()->GetPosition(), 0);
 }
 
 TEST_F(ScrollBarViewsTest, DragThumbScrollsContent) {
   ui::test::EventGenerator generator(GetRootWindow(widget_.get()));
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   generator.MoveMouseTo(
-      scrollbar_->GetThumb()->GetBoundsInScreen().CenterPoint());
+      scrollbar()->GetThumb()->GetBoundsInScreen().CenterPoint());
   generator.PressLeftButton();
   generator.MoveMouseBy(15, 0);
-  EXPECT_GE(scrollbar_->GetPosition(), 10);
+  EXPECT_GE(scrollbar()->GetPosition(), 10);
 
   // Dragging the mouse somewhat outside the thumb maintains scroll.
   generator.MoveMouseBy(0, 100);
-  EXPECT_GE(scrollbar_->GetPosition(), 10);
+  EXPECT_GE(scrollbar()->GetPosition(), 10);
 
   // Dragging the mouse far outside the thumb snaps back to the initial
   // scroll position.
   generator.MoveMouseBy(0, 100);
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
 }
 
 TEST_F(ScrollBarViewsTest, DragThumbScrollsContentWhenSnapBackDisabled) {
-  scrollbar_->GetThumb()->SetSnapBackOnDragOutside(false);
+  scrollbar()->GetThumb()->SetSnapBackOnDragOutside(false);
   ui::test::EventGenerator generator(GetRootWindow(widget_.get()));
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   generator.MoveMouseTo(
-      scrollbar_->GetThumb()->GetBoundsInScreen().CenterPoint());
+      scrollbar()->GetThumb()->GetBoundsInScreen().CenterPoint());
   generator.PressLeftButton();
   generator.MoveMouseBy(10, 0);
-  EXPECT_GT(scrollbar_->GetPosition(), 0);
+  EXPECT_GT(scrollbar()->GetPosition(), 0);
   // Move the mouse far down, outside the thumb.
   generator.MoveMouseBy(0, 200);
   // Position does not snap back to zero.
-  EXPECT_GT(scrollbar_->GetPosition(), 0);
+  EXPECT_GT(scrollbar()->GetPosition(), 0);
 }
 
 TEST_F(ScrollBarViewsTest, FlingGestureScrollsView) {
   constexpr int kNumScrollSteps = 100;
   constexpr int kScrollVelocity = 10;
   ui::test::EventGenerator generator(GetRootWindow(widget_.get()));
-  EXPECT_EQ(0, scrollbar_->GetPosition());
+  EXPECT_EQ(0, scrollbar()->GetPosition());
   const gfx::Point start_pos =
       widget_->GetContentsView()->GetBoundsInScreen().CenterPoint();
   const gfx::Point end_pos = start_pos + gfx::Vector2d(-100, 0);
@@ -299,7 +307,7 @@ TEST_F(ScrollBarViewsTest, FlingGestureScrollsView) {
           start_pos, end_pos, kScrollVelocity, kNumScrollSteps),
       kNumScrollSteps);
   // Just make sure the view scrolled
-  EXPECT_GT(scrollbar_->GetPosition(), 0);
+  EXPECT_GT(scrollbar()->GetPosition(), 0);
 }
 #endif
 
