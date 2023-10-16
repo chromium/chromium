a707a20abc3d9558da923c38bf8add9344a37ddb
Gauthier Ambard <gambard@chromium.org>
2023-10-16 09:39:53 +0000

    [iOS] Handle WebState changes and replacement in TabStrip
    This CL makes sure that when the WebState status (title, loading...) is
    updated, the view is updated.
    It also makes sure to handle when a WebState is replaced by another.
    
    Bug: 1490555
    Change-Id: I8a60e97277d081e6dd30ae98a48bf0848e67ed2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4931680
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210084}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_consumer.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_consumer.swift
@@ -2,16 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// Consumer protocol for the TabStrip.
 @objc protocol TabStripConsumer {
 
-  /// Tells the consumer to replace its current set of items with `items` and
-  /// update the selected item to be `selectedItem`.
+  /// Tells the consumer to replace its current set of items with `items` and updates the selected
+  /// item to be `selectedItem`. The selected item must be in the `items`.
   func populate(items: [TabSwitcherItem]?, selectedItem: TabSwitcherItem?)
+
+  /// Tells the consumer to select `item`.
+  func selectItem(_ item: TabSwitcherItem?)
 
   /// Reloads `item`'s content.
   func reloadItem(_ item: TabSwitcherItem?)
 
-  /// Tells the consumer to select `item`.
-  func selectItem(_ item: TabSwitcherItem?)
+  /// Replaces `oldItem` by `newItem`.
+  /// The nullability is here for Objective-C compatibility. If one of them is nil, the consumer will do nothing.
+  func replaceItem(_ oldItem: TabSwitcherItem?, withItem newItem: TabSwitcherItem?)
 
 }
