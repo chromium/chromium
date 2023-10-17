86dca3cab61e9fc3959a5322e46725ca4fd26c0e
Min Qin <qinmin@chromium.org>
2023-10-17 23:03:24 +0000

    Use std::move() to reduce memory pressure when creating download db entry
    BUG=1492408
    
    Change-Id: I0d68650c562c72e8acbf3aa71d844643f271cc09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4948891
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211173}

--- components/download/internal/common/download_utils.cc
+++ components/download/internal/common/download_utils.cc
@@ -552,11 +552,11 @@ DownloadDBEntry CreateDownloadDBEntryFromItem(const DownloadItemImpl& item) {
   in_progress_info.range_request_from = range_request_offset.first;
   in_progress_info.range_request_to = range_request_offset.second;
 
-  download_info.in_progress_info = in_progress_info;
+  download_info.in_progress_info = std::move(in_progress_info);
 
   download_info.ukm_info =
       UkmInfo(item.GetDownloadSource(), item.ukm_download_id());
-  entry.download_info = download_info;
+  entry.download_info = std::move(download_info);
   return entry;
 }
 
