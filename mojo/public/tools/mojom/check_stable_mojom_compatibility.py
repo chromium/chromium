91f5d9419adf819cfb6c8b15d2f2b24d4a7c14ce
Jiewei Qian <qjw@chromium.org>
2023-10-17 04:54:08 +0000

    mojo: remove exemption for deleted generated files
    This is a followup for https://crrev.com/c/4891399.
    
    This CL removes the Mojo compatibility check exemption for the
    removed generated mojom files (runtime_feature_state.mojom).
    
    Bug: 1377000
    Change-Id: Ia2e64b35d29b89d9c54aa3315a319e84574cd9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4905005
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210647}

--- mojo/public/tools/mojom/check_stable_mojom_compatibility.py
+++ mojo/public/tools/mojom/check_stable_mojom_compatibility.py
@@ -81,11 +81,6 @@ def _ValidateDelta(root, delta):
     generated_files_to_skip = {
         ('third_party/blink/public/mojom/runtime_feature_state/'
          'runtime_feature.mojom'),
-        # TODO: Remove the following file. We need to skip the old file because
-        # it's still in the rename delta. Compatibility checker will try to
-        # read it and fail.
-        ('third_party/blink/public/mojom/runtime_feature_state/'
-         'runtime_feature_state.mojom'),
         ('third_party/blink/public/mojom/origin_trial_feature/'
          'origin_trial_feature.mojom'),
     }
