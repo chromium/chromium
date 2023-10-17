f14a25035d8a486ec9c7f708d805fa4d697af32e
Mark Polner <polner@google.com>
2023-10-17 01:19:05 +0000

    [Nearby] Add platform implementation of GetPublicCredentials.
    Added the platform implementation of the Nearby Library's
    GetPublicCredentials method. This method calls upon the
    NearbyPresenceCredentialStorage GetPublicCredential
    interface to retrieve stored shared credentials.
    
    Test: Added unit tests.
    Change-Id: Iaba5dfb7af8ac5d653fa1f46cd0ad2d2f4d911b8
    Bug: b:287334195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4851770
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Mark Polner <polner@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1210577}

--- chrome/services/sharing/nearby/platform/credential_storage.cc
+++ chrome/services/sharing/nearby/platform/credential_storage.cc
@@ -62,7 +62,15 @@ void CredentialStorage::GetLocalCredentials(
 void CredentialStorage::GetPublicCredentials(
     const CredentialSelector& credential_selector,
     PublicCredentialType public_credential_type,
-    GetPublicCredentialsResultCallback callback) {}
+    GetPublicCredentialsResultCallback callback) {
+  // TODO(b/299300880): Extract identity_type from credential_selector
+  // and pass to the browser process to filter SharedCredentials.
+  nearby_presence_credential_storage_->GetPublicCredentials(
+      ash::nearby::presence::proto::PublicCredentialTypeToMojom(
+          public_credential_type),
+      base::BindOnce(&CredentialStorage::OnPublicCredentialsRetrieved,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+}
 
 void CredentialStorage::OnCredentialsSaved(
     nearby::presence::SaveCredentialsResultCallback
@@ -76,6 +84,34 @@ void CredentialStorage::OnCredentialsSaved(
         .credentials_saved_cb(absl::Status(absl::StatusCode::kUnknown,
                                            "Failed to save to database."));
   }
+}
+
+void CredentialStorage::OnPublicCredentialsRetrieved(
+    nearby::presence::GetPublicCredentialsResultCallback
+        on_public_credentials_retrieved_callback,
+    mojo_base::mojom::AbslStatusCode retrieved_status,
+    absl::optional<
+        std::vector<ash::nearby::presence::mojom::SharedCredentialPtr>>
+        shared_credentials_mojom) {
+  if (retrieved_status != mojo_base::mojom::AbslStatusCode::kOk) {
+    std::move(on_public_credentials_retrieved_callback)
+        .credentials_fetched_cb(absl::Status(
+            absl::StatusCode::kAborted,
+            "Failed to retrieve public credentials from database."));
+    return;
+  }
+
+  CHECK(shared_credentials_mojom.has_value());
+
+  std::vector<SharedCredential> shared_credentials;
+  for (const auto& shared_credential_mojom : *shared_credentials_mojom) {
+    shared_credentials.push_back(
+        ash::nearby::presence::proto::SharedCredentialFromMojom(
+            shared_credential_mojom.get()));
+  }
+
+  std::move(on_public_credentials_retrieved_callback)
+      .credentials_fetched_cb(shared_credentials);
 }
 
 }  // namespace nearby::chrome
