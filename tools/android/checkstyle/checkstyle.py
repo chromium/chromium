6e81e0ae3b4ca2cfe4cf37c78b0d3cb144defc6d
Andrew Grieve <agrieve@chromium.org>
2023-10-17 15:31:50 +0000

    Reland "Improve error message when checkstyle cannot parse a .java file"
    This reverts commit 055914dfa22d2907fbe13d981fa86f39c18dc9e9.
    
    Reason for reland: Fixed check.
    
    Original change's description:
    > Revert "Improve error message when checkstyle cannot parse a .java file"
    >
    > This reverts commit cd228a148ca7e8db1f379dbc5256d4f1cf8800fd.
    >
    > Reason for revert: exit code checking logic is wrong
    >
    > Turns out the exit code is the number of errors rather than "1"
    > as I had assumed when testing.
    >
    > Original change's description:
    > > Improve error message when checkstyle cannot parse a .java file
    > >
    > > Also adds a main() to checkstyle.py to make it easy to run it without
    > > having to go through "git cl presubmit".
    > >
    > > Bug: None
    > > Change-Id: I462335b9b5e8f1f13e9a090c5d74d8f52449806e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943930
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > > Commit-Queue: Peter Wen <wnwen@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1210269}
    >
    > Bug: None
    > Change-Id: I60178885511fc64be5f0641dd8f412cd41d591e9
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4947771
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1210830}
    
    Bug: None
    Change-Id: I76d5a74b4d4ee9186ded02317d73a081dd52bbcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4947612
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210868}

--- tools/android/checkstyle/checkstyle.py
+++ tools/android/checkstyle/checkstyle.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 # Copyright 2013 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 """Script that is used by PRESUBMIT.py to run style checks on Java files."""
 
+import argparse
 import collections
 import os
 import subprocess
@@ -42,22 +44,29 @@ def run_checkstyle(local_path, style_file, java_files):
         _JAVA_PATH, '-cp', _CHECKSTYLE_ROOT,
         'com.puppycrawl.tools.checkstyle.Main', '-c', style_file, '-f', 'xml'
     ] + java_files
-    check = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    stdout = check.communicate()[0].decode('utf-8')
-    results = []
+    result = subprocess.run(cmd, capture_output=True, check=False, text=True)
 
-    lines = stdout.splitlines(keepends=True)
-    if 'Checkstyle ends with' in lines[-1]:
-        stdout = ''.join(lines[:-1])
+    stderr_lines = result.stderr.splitlines()
+    # One line is always: "Checkstyle ends with # warnings/errors".
+    if len(stderr_lines) > 1 or (stderr_lines
+                                 and 'ends with' not in stderr_lines[0]):
+        sys.stderr.write(result.stderr)
+        sys.stderr.write(
+            f'\nCheckstyle failed with returncode={result.returncode}.\n')
+        sys.stderr.write('This might mean you have a syntax error\n')
+        sys.exit(-1)
+
     try:
-        root = xml.dom.minidom.parseString(stdout)
+        root = xml.dom.minidom.parseString(result.stdout)
     except Exception:
-        print('Tried to parse:')
-        print(stdout)
+        sys.stderr.write('Tried to parse:\n')
+        sys.stderr.write(result.stdout)
+        sys.stderr.write('\n')
         raise
 
     inclusive_files = []
     inclusive_warning = ''
+    results = []
     for fileElement in root.getElementsByTagName('file'):
         filename = fileElement.attributes['name'].value
         if filename.startswith(local_path):
@@ -120,3 +129,20 @@ To remove unused imports: """ + input_api.os_path.relpath(
                 _REMOVE_UNUSED_IMPORTS_PATH, local_path)
         ret.append(output_api.PresubmitError(msg))
     return ret
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('java_files', nargs='+')
+    args = parser.parse_args()
+
+    violations = run_checkstyle(CHROMIUM_SRC, _STYLE_FILE, args.java_files)
+    for v in violations:
+        print(f'{v} ({v.severity})')
+
+    if any(v.is_error() for v in violations):
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
