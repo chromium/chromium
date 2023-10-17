c4fd76d747c5dafb3109dd72fef8ff733882e3da
Matt Menke <mmenke@chromium.org>
2023-10-17 14:18:25 +0000

    [FLEDGE] Fix SharedStorageBindings reference-after-free bug.
    BidderWorklet::V8State has a
    mojo::Remote<mojom::AuctionSharedStorageHost> member below a pair of
    ContextRecycler objects. The ContextRecyclers own
    SharedStorageBindings objects that have raw pointers to the
    mojo::Remote<mojom::AuctionSharedStorageHost>. This results in the
    Remove being destroyed before raw_ptrs to it, which triggers memory
    tools to complain.
    
    It's likely that this wasn't caught before because of insufficient
    integration testing around executionMode="group-by-origin" interest
    groups - we have update tests, but no browser or WPT tests, I
    believe.
    
    Fixed: 1492732
    Change-Id: Iec6de7bb97b5be81d20f70ef5479e5efdcb6f2d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943730
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210820}

--- content/services/auction_worklet/bidder_worklet.h
+++ content/services/auction_worklet/bidder_worklet.h
@@ -565,11 +565,13 @@ class CONTENT_EXPORT BidderWorklet : public mojom::BidderWorklet,
     const absl::optional<GURL> wasm_helper_url_;
     const absl::optional<GURL> trusted_bidding_signals_url_;
 
+    // This must be above the ContextRecyclers, since they own
+    // SharedStorageBindings, which have raw pointers to it.
+    mojo::Remote<mojom::AuctionSharedStorageHost> shared_storage_host_remote_;
+
     std::unique_ptr<ContextRecycler> context_recycler_for_origin_group_mode_;
     url::Origin join_origin_for_origin_group_mode_;
     std::unique_ptr<ContextRecycler> context_recycler_for_frozen_context_;
-
-    mojo::Remote<mojom::AuctionSharedStorageHost> shared_storage_host_remote_;
 
     SEQUENCE_CHECKER(v8_sequence_checker_);
   };
