9a8f1ce9ababb2ef11c65aec1cb1cb7eb586f485
Lingqi Chi <lingqi@chromium.org>
2023-10-17 01:30:09 +0000

    [text-autospace] sort bounds by physical order before calculating advance
    If the text contains RTL text, iterating over characters in strings
    would be in a different order from the physical order. So then, some
    advances might be negative.
    So this CL sorts the bounds by physical orders first, then calculate
    the advance.
    It also makes a minor change to transpose part, to ensure that
    - bounds.bottom is always greater than bounds.top
    - bounds.right is always greater than bounds.left,
    so then the array of bounds gets sortable.
    
    Bug: 1463890
    Change-Id: Ie12ddf0b34397ac17fbfb7bd1084e7c4ff674d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4942910
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1210583}

--- third_party/blink/web_tests/external/wpt/css/css-text/support/get-char-advances.js
+++ third_party/blink/web_tests/external/wpt/css/css-text/support/get-char-advances.js
@@ -12,9 +12,8 @@ function getCharAdvances(element) {
   const style = getComputedStyle(element);
   const is_vertical = style.writingMode.startsWith('vertical');
   const range = document.createRange();
-  const advances = [];
-  let origin = undefined;
-  let blockEnd = -1;
+  const all_bounds = []
+
   function walk(element) {
     for (const node of element.childNodes) {
       const nodeType = node.nodeType;
@@ -24,21 +23,17 @@ function getCharAdvances(element) {
           range.setStart(node, i);
           range.setEnd(node, i + 1);
           let bounds = range.getBoundingClientRect();
-          // Transpose if it's in vertical flow.
+          // Transpose if it's in vertical flow. Guarantee that top < bottom
+          // and left < right are always true.
           if (is_vertical) {
-            bounds = {left: bounds.top, top: bounds.right,
-                      right: bounds.bottom, bottom: bounds.left};
+            bounds = {
+              left: bounds.top,
+              top: bounds.left,
+              right: bounds.bottom,
+              bottom: bounds.right
+            };
           }
-          // Check if this is on the same line.
-          if (bounds.top >= blockEnd) {
-            origin = undefined;
-            blockEnd = bounds.bottom;
-          }
-          // For the first character, the left bound is closest to the origin.
-          if (origin === undefined) origin = bounds.left;
-          // The right bound is a good approximation of the next origin.
-          advances.push(bounds.right - origin);
-          origin = bounds.right;
+          all_bounds.push(bounds);
         }
       } else if (nodeType === Node.ELEMENT_NODE) {
         walk(node);
@@ -46,5 +41,30 @@ function getCharAdvances(element) {
     }
   }
   walk(element);
+  all_bounds.sort(function(bound_a, bound_b) {
+    if (bound_a.bottom <= bound_b.top) {
+      return -1;
+    }
+    if (bound_b.bottom <= bound_a.top) {
+      return 1;
+    }
+    return bound_a.left - bound_b.left;
+  });
+  let origin = undefined;
+  let blockEnd = -1;
+  const advances = [];
+  for (let bounds of all_bounds) {
+    // Check if this is on the same line.
+    if (bounds.top >= blockEnd) {
+      origin = undefined;
+      blockEnd = bounds.bottom;
+    }
+    // For the first character, the left bound is closest to the origin.
+    if (origin === undefined)
+      origin = bounds.left;
+    // The right bound is a good approximation of the next origin.
+    advances.push(bounds.right - origin);
+    origin = bounds.right;
+  }
   return advances;
 }
