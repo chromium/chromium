ae19210ed682a9e4f8c4ba74b816ca77b6ce1724
Alexei Svitkine <asvitkine@chromium.org>
2023-10-18 23:22:25 +0000

    Pass --disable-field-trial-config to tests that need it.
    Affected tests already run without the testing config applied,
    so this CL makes it explicit.
    
    Context: Browser tests that use ScopedFeatureList and pass
    params presently cause the field trial testing config to be
    entirely disabled. (details: crbug.com/1491463#c8)
    We plan to fix this, but some existing tests rely on this
    (fail with the testing config). This CL annotates those
    tests explicitly by making them specify the flag for that.
    
    Once all the CLs updating such tests land (sharded via git
    cl split), the following CL to make ScopedFeatureList not
    disable the testing config can land:
    https://chromium-review.googlesource.com/c/chromium/src/+/4935093
    
    As many tests are affected, it will be up to their owners
    to do any necessary follow up if the tests should be made to
    pass with the testing config (per the comments this CL adds).
    
    Note: I did a best effort to have the new flag only apply to the
    subset of tests that needed it when it was a single or a small
    number of tests that were affected, by introducing subclasses
    for the affected tests. In other cases, where lots of tests in
    the same file fail, the flag is added to the original test class.
    
    Note: The --disable-field-trial-config flag is passed simply
    by name intentionally, to avoid additional complexity from
    depending on components/variations and includes to reference
    the existing constant for it. The name is unlikely to change and
    this way, we need less complex code to add this to the tests.
    
    This CL was uploaded by git cl split.
    
    R=stanfield@google.com
    
    Bug: 1491463
    Change-Id: I98106d76cc73458eedbb4333671e9985ce029080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4943935
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211828}

--- chrome/browser/ui/views/side_panel/search_companion/companion_page_browsertest.cc
+++ chrome/browser/ui/views/side_panel/search_companion/companion_page_browsertest.cc
@@ -2216,8 +2216,19 @@ IN_PROC_BROWSER_TEST_F(SidePanelCompanion2BrowserDisabledTest,
   EXPECT_EQ(0u, requests_received_on_server());
 }
 
+// TODO(crbug.com/1491942): This fails with the field trial testing config.
+class SidePanelCompanion2BrowserEnabledTestNoTestingConfig
+    : public SidePanelCompanion2BrowserEnabledTest {
+ public:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    SidePanelCompanion2BrowserEnabledTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch("disable-field-trial-config");
+  }
+};
+
 // Verify that Companion is enabled when `kSidePanelCompanion2` is enabled.
-IN_PROC_BROWSER_TEST_F(SidePanelCompanion2BrowserEnabledTest, FeatureEnabled) {
+IN_PROC_BROWSER_TEST_F(SidePanelCompanion2BrowserEnabledTestNoTestingConfig,
+                       FeatureEnabled) {
   EXPECT_TRUE(companion::IsCompanionFeatureEnabled());
 
   // Load a page on the active tab and open companion side panel
