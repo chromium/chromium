24dfd5f913d01db144efc2e5ab775856ce9df958
Xinghui Lu <xinghuilu@chromium.org>
2023-10-18 22:47:44 +0000

    Enable SafeBrowsingSkipSubResources by default on iOS
    Bug: 1486144
    Change-Id: I7f127a9e08691cbf6c27587f8f3b1f8c2f95cc4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4945415
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211804}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -258,12 +258,7 @@ BASE_FEATURE(kSafeBrowsingSkipImageCssFont,
 
 BASE_FEATURE(kSafeBrowsingSkipSubresources,
              "SafeBrowsingSkipSubResources",
-#if BUILDFLAG(IS_IOS)
-             base::FEATURE_DISABLED_BY_DEFAULT
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSafeBrowsingSkipSubresources2,
              "SafeBrowsingSkipSubResources2",
