44ceb42924fbc5f551d2031a7613533b1490355d
Dave Tapuska <dtapuska@chromium.org>
2023-10-18 17:05:59 +0000

    [extensions] Return early if not app window
    Prior to https://crrev.com/c/4915599 the messages were dropped on the
    floor if they weren't an app window. Enable that to see if this addresses the crashes reported. If it does we probably really
    shouldn't send these messages in the first place if the ViewType
    isn't AppWindow.
    
    Bug: 1493704
    Change-Id: I20defadb31dadc2700306f489fd7cec32ae4224b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953253
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211578}

--- extensions/browser/extension_frame_host.cc
+++ extensions/browser/extension_frame_host.cc
@@ -118,14 +118,9 @@ void ExtensionFrameHost::UpdateDraggableRegions(
     std::vector<mojom::DraggableRegionPtr> regions) {
   content::RenderFrameHost* render_frame_host =
       receivers_.GetCurrentTargetFrame();
-  // This message should come from a primary main frame.
-  if (!render_frame_host->IsInPrimaryMainFrame()) {
-    bad_message::ReceivedBadMessage(
-        render_frame_host->GetProcess(),
-        bad_message::AWCI_INVALID_CALL_FROM_NOT_PRIMARY_MAIN_FRAME);
-    return;
-  }
 
+  // TODO(dtapuska): We should restrict sending the draggable region
+  // only to AppWindows.
   AppWindowRegistry* registry =
       AppWindowRegistry::Get(render_frame_host->GetBrowserContext());
   if (!registry) {
@@ -133,6 +128,14 @@ void ExtensionFrameHost::UpdateDraggableRegions(
   }
   AppWindow* app_window = registry->GetAppWindowForWebContents(web_contents_);
   if (!app_window) {
+    return;
+  }
+
+  // This message should come from a primary main frame.
+  if (!render_frame_host->IsInPrimaryMainFrame()) {
+    bad_message::ReceivedBadMessage(
+        render_frame_host->GetProcess(),
+        bad_message::AWCI_INVALID_CALL_FROM_NOT_PRIMARY_MAIN_FRAME);
     return;
   }
   app_window->UpdateDraggableRegions(std::move(regions));
