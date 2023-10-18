05125ae47f03bb3af47db11fd99a0c7d085e406a
Stefan Kuhne <skuhne@google.com>
2023-10-18 19:12:35 +0000

    [Lacros] Blocking to load Lacros for -v1 (pre-SSE4) devices
    Some very old Flex devices (pre 2006) did not support
    SSE4.2. As Lacros gets shipped to all platforms with the
    same binary, we disable Lacros for these old devices.
    
    Bug: 1489710
    Change-Id: I95596de7e455282a7cf22caf8febb63c341f6f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4944000
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1211667}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -54,6 +54,7 @@ namespace crosapi::browser_util {
 namespace {
 
 bool g_profile_migration_completed_for_test = false;
+absl::optional<bool> g_cpu_override_for_test = absl::nullopt;
 
 // At session start the value for LacrosAvailability logic is applied and the
 // result is stored in this variable which is used after that as a cache.
@@ -186,11 +187,27 @@ bool IsLacrosDisallowedByCommand() {
          !cmdline->HasSwitch(ash::switches::kDisableDisallowLacros);
 }
 
+// Return true if the CPU of this system is capable to run Lacros.
+bool IsCPUSupported() {
+  if (g_cpu_override_for_test.has_value()) {
+    return g_cpu_override_for_test.value();
+  }
+#ifdef ARCH_CPU_X86_64
+  // Some very old Flex devices are not capable to support the -v2 instruction
+  // set. Those CPU's should not use Lacros as Lacros has only one binary
+  // for all intel platforms. As 'x86-64-v2' does not work on the build server,
+  // we check against SSE4.2 here which should be as as good.
+  return __builtin_cpu_supports("sse4.2");
+#else
+  return true;
+#endif
+}
+
 // Returns whether or not lacros is allowed for the Primary user,
 // with given LacrosAvailability policy.
 bool IsLacrosAllowedInternal(const User* user,
                              LacrosAvailability lacros_availability) {
-  if (IsLacrosDisallowedByCommand()) {
+  if (IsLacrosDisallowedByCommand() || !IsCPUSupported()) {
     // This happens when Ash is restarted in multi-user session, meaning there
     // are more than two users logged in to the device. This will not cause an
     // accidental removal of Lacros data because for the primary user, the fact
@@ -1090,6 +1107,10 @@ bool ShouldEnforceAshExtensionKeepList() {
 bool IsAshDevToolEnabled() {
   return IsAshWebBrowserEnabled() ||
          base::FeatureList::IsEnabled(ash::features::kAllowDevtoolsInSystemUI);
+}
+
+void SetCpuAvailabilityForTesting(absl::optional<bool> value) {
+  g_cpu_override_for_test = value;
 }
 
 }  // namespace crosapi::browser_util
