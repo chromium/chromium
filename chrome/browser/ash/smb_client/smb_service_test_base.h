84eec031c42bc279c9876d644d3ef4d9ef330899
Moteva <moteva@google.com>
2023-10-19 23:36:57 +0000

    CrOS Settings: SMB Service Test Refactoring
    Refactoring the file smb_service_unittest.cc into a base class and
    separating the smb_handler tests into their own file. This change is
    done because smb_handler_unittest will be sharing many functions that
    were already implemented in smb_service_unittest. Now the shared
    functions are enclosed in the class SmbServiceBaseTest.
    
    BUG=b/299160727
    TEST= unit_test:
    --gtest_filter="*SmbHandlerTest*"
    --gtest_filter="*SmbServiceWithSmbfsTest*"
    
    Change-Id: If402403d83dc5659084b33246ffaf83d873335a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950591
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212459}

---
+++ chrome/browser/ash/smb_client/smb_service_test_base.h
@@ -0,0 +1,134 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_SMB_CLIENT_SMB_SERVICE_TEST_BASE_H_
+#define CHROME_BROWSER_ASH_SMB_CLIENT_SMB_SERVICE_TEST_BASE_H_
+
+#include "chrome/browser/ash/smb_client/smb_service.h"
+
+#include <stddef.h>
+
+#include "base/test/bind.h"
+#include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
+#include "chromeos/ash/components/dbus/concierge/concierge_client.h"
+#include "chromeos/ash/components/disks/disk_mount_manager.h"
+#include "chromeos/ash/components/disks/fake_disk_mount_manager.h"
+#include "chromeos/ash/components/smbfs/smbfs_mounter.h"
+#include "components/user_manager/scoped_user_manager.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::_;
+using ::testing::NiceMock;
+
+namespace ash::smb_client {
+
+namespace {
+
+inline constexpr char kSharePath[] = "\\\\server\\foobar";
+inline constexpr char kMountPath[] = "/share/mount/path";
+inline constexpr char kDisplayName[] = "My Share";
+
+}  // namespace
+
+class MockSmbFsMounter : public smbfs::SmbFsMounter {
+ public:
+  MockSmbFsMounter();
+  ~MockSmbFsMounter() override;
+
+  MOCK_METHOD(void,
+              Mount,
+              (smbfs::SmbFsMounter::DoneCallback callback),
+              (override));
+};
+
+class MockSmbFsImpl : public smbfs::mojom::SmbFs {
+ public:
+  explicit MockSmbFsImpl(mojo::PendingReceiver<smbfs::mojom::SmbFs> pending);
+  ~MockSmbFsImpl() override;
+
+  MockSmbFsImpl(const MockSmbFsImpl&) = delete;
+  MockSmbFsImpl& operator=(const MockSmbFsImpl&) = delete;
+
+  // Mojo disconnection handler.
+  MOCK_METHOD(void, OnDisconnect, (), ());
+
+  // smbfs::mojom::SmbFs overrides.
+  MOCK_METHOD(void,
+              RemoveSavedCredentials,
+              (RemoveSavedCredentialsCallback),
+              (override));
+
+  MOCK_METHOD(void,
+              DeleteRecursively,
+              (const base::FilePath&, DeleteRecursivelyCallback),
+              (override));
+
+ private:
+  mojo::Receiver<smbfs::mojom::SmbFs> receiver_;
+};
+
+class SmbServiceBaseTest : public testing::Test {
+ public:
+  SmbServiceBaseTest();
+
+  SmbServiceBaseTest(const SmbServiceBaseTest&) = delete;
+  SmbServiceBaseTest& operator=(const SmbServiceBaseTest&) = delete;
+  ~SmbServiceBaseTest() override;
+
+ protected:
+  // Mojo endpoints owned by the smbfs instance.
+  struct TestSmbFsInstance {
+    explicit TestSmbFsInstance(
+        mojo::PendingReceiver<smbfs::mojom::SmbFs> pending);
+    ~TestSmbFsInstance();
+
+    MockSmbFsImpl mock_smbfs;
+    mojo::Remote<smbfs::mojom::SmbFsDelegate> delegate;
+  };
+
+  // Create smb service.
+  void CreateService(TestingProfile* profile);
+
+  // Checks that the correct result status is returned when an SMB attempts
+  // mount with an invalid URL.
+  void ExpectInvalidUrl(const std::string& url);
+
+  // Checks that the correct result status is returned when an SMB attempts
+  // mount with an invalid SSO URL.
+  void ExpectInvalidSsoUrl(const std::string& url);
+
+  void WaitForSetupComplete();
+
+  std::unique_ptr<disks::MountPoint> MakeMountPoint(const base::FilePath& path);
+
+  // Helper function for creating a basic smbfs mount with an empty
+  // username/password.
+  std::unique_ptr<TestSmbFsInstance> MountBasicShare(
+      const std::string& share_path,
+      const std::string& mount_path,
+      SmbService::MountResponse callback);
+
+  std::unique_ptr<SmbService> smb_service;
+
+  // Not owned.
+  raw_ptr<TestingProfile, DanglingUntriaged | ExperimentalAsh> profile =
+      nullptr;
+
+ private:
+  content::BrowserTaskEnvironment task_environment_{
+      content::BrowserTaskEnvironment::REAL_IO_THREAD};
+  base::test::ScopedFeatureList scoped_feature_list_;
+  raw_ptr<disks::FakeDiskMountManager, DanglingUntriaged | ExperimentalAsh>
+      disk_mount_manager_;
+  std::unique_ptr<TestingProfileManager> profile_manager_;
+  std::unique_ptr<user_manager::ScopedUserManager> user_manager_enabler_;
+};
+
+}  // namespace ash::smb_client
+
+#endif  // CHROME_BROWSER_ASH_SMB_CLIENT_SMB_SERVICE_TEST_BASE_H_
