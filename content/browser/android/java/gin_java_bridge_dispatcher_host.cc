96f01f9f63e4d6c4b0661509160811c45243695d
Nate Fischer <ntfschr@chromium.org>
2023-10-19 03:21:58 +0000

    Revert "WebView: Release its reference when RenderFrameHost is destroyed"
    This reverts commit bf9fb13fbfe37004ef9c0d27cf33a57119096ec1.
    
    Reason for revert: broke the Google account flow (crbug.com/1493848)
    
    Original change's description:
    > WebView: Release its reference when RenderFrameHost is destroyed
    >
    > The iframes within the page can be dynamically removed, triggering the
    > destruction of the RenderFrameHost(RFH). However, the 'host_' and
    > 'hosts_is_in_primary_main_frame_' within GinJavaBridgeMessageFilter
    > still reference the released RFH. Frequent iframe operations may result
    > in a continuous increase in the size of 'host_' and
    > 'hosts_is_in_primary_main_frame_'
    >
    > R=torne@chromium.org
    >
    > Low-Coverage-Reason: HARD_TO_TEST manually tested on crbug.com/1481603
    > Bug: 1481603,1481037
    > Change-Id: I517ab2e0245d05db83ed6f45a6f221714028cbde
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4861403
    > Commit-Queue: Richard (Torne) Coles <torne@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1208297}
    
    (cherry picked from commit e1aedc824ad46a1d410dacadc2ab3ba54475e83f)
    
    Bug: 1481603,1481037
    Change-Id: If674fb53bd864ecef118d6fe240a1486a6d79d39
    Fixed: 1493848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4954035
    Owners-Override: Harry Souders <harrysouders@google.com>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1211883}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4953510
    Cr-Commit-Position: refs/branch-heads/6076@{#3}
    Cr-Branched-From: 65ba1ef22591eac46df5da4cd43887b774f79e39-refs/heads/main@{#1211682}

--- content/browser/android/java/gin_java_bridge_dispatcher_host.cc
+++ content/browser/android/java/gin_java_bridge_dispatcher_host.cc
@@ -55,33 +55,28 @@ void GinJavaBridgeDispatcherHost::InstallFilterAndRegisterAllRoutingIds() {
     return;
   }
 
-  web_contents()->GetPrimaryMainFrame()->ForEachRenderFrameHost(
-      [this](RenderFrameHostImpl* frame) {
-        if (frame->IsRenderFrameLive()) {
-          InstallFilterAndRegisterRoutingId(frame);
-        }
-      });
-}
+  web_contents()
+      ->GetPrimaryMainFrame()
+      ->ForEachRenderFrameHost(
+          [this](RenderFrameHostImpl* frame) {
+            AgentSchedulingGroupHost& agent_scheduling_group =
+                frame->GetAgentSchedulingGroup();
 
-void GinJavaBridgeDispatcherHost::InstallFilterAndRegisterRoutingId(
-    RenderFrameHost* render_frame_host) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  AgentSchedulingGroupHost& agent_scheduling_group =
-      static_cast<RenderFrameHostImpl*>(render_frame_host)
-          ->GetAgentSchedulingGroup();
-  scoped_refptr<GinJavaBridgeMessageFilter> per_asg_filter =
-      GinJavaBridgeMessageFilter::FromHost(agent_scheduling_group,
-                                           /*create_if_not_exists=*/true);
-  if (base::FeatureList::IsEnabled(features::kMBIMode)) {
-    scoped_refptr<GinJavaBridgeObjectDeletionMessageFilter>
-        process_global_filter =
-            GinJavaBridgeObjectDeletionMessageFilter::FromHost(
-                agent_scheduling_group.GetProcess(),
-                /*create_if_not_exists=*/true);
-    process_global_filter->AddRoutingIdForHost(this, render_frame_host);
-  }
+            scoped_refptr<GinJavaBridgeMessageFilter> per_asg_filter =
+                GinJavaBridgeMessageFilter::FromHost(
+                    agent_scheduling_group,
+                    /*create_if_not_exists=*/true);
+            if (base::FeatureList::IsEnabled(features::kMBIMode)) {
+              scoped_refptr<GinJavaBridgeObjectDeletionMessageFilter>
+                  process_global_filter =
+                      GinJavaBridgeObjectDeletionMessageFilter::FromHost(
+                          agent_scheduling_group.GetProcess(),
+                          /*create_if_not_exists=*/true);
+              process_global_filter->AddRoutingIdForHost(this, frame);
+            }
 
-  per_asg_filter->AddRoutingIdForHost(this, render_frame_host);
+            per_asg_filter->AddRoutingIdForHost(this, frame);
+          });
 }
 
 WebContentsImpl* GinJavaBridgeDispatcherHost::web_contents() const {
@@ -91,11 +86,16 @@ WebContentsImpl* GinJavaBridgeDispatcherHost::web_contents() const {
 void GinJavaBridgeDispatcherHost::RenderFrameCreated(
     RenderFrameHost* render_frame_host) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (named_objects_.empty()) {
-    return;
+  AgentSchedulingGroupHost& agent_scheduling_group =
+      static_cast<RenderFrameHostImpl*>(render_frame_host)
+          ->GetAgentSchedulingGroup();
+  if (scoped_refptr<GinJavaBridgeMessageFilter> filter =
+          GinJavaBridgeMessageFilter::FromHost(
+              agent_scheduling_group, /*create_if_not_exists=*/false)) {
+    filter->AddRoutingIdForHost(this, render_frame_host);
+  } else {
+    InstallFilterAndRegisterAllRoutingIds();
   }
-
-  InstallFilterAndRegisterRoutingId(render_frame_host);
   for (NamedObjectMap::const_iterator iter = named_objects_.begin();
        iter != named_objects_.end();
        ++iter) {
@@ -104,18 +104,19 @@ void GinJavaBridgeDispatcherHost::RenderFrameCreated(
   }
 }
 
-void GinJavaBridgeDispatcherHost::RenderFrameDeleted(
-    RenderFrameHost* render_frame_host) {
-  AgentSchedulingGroupHost& agent_scheduling_group =
-      static_cast<RenderFrameHostImpl*>(render_frame_host)
-          ->GetAgentSchedulingGroup();
-  scoped_refptr<GinJavaBridgeMessageFilter> filter =
-      GinJavaBridgeMessageFilter::FromHost(agent_scheduling_group,
-                                           /*create_if_not_exists=*/false);
+void GinJavaBridgeDispatcherHost::WebContentsDestroyed() {
+  // Unretained() is safe because ForEachRenderFrameHost() is synchronous.
+  web_contents()->GetPrimaryMainFrame()->ForEachRenderFrameHost(
+      [this](RenderFrameHostImpl* frame) {
+        AgentSchedulingGroupHost& agent_scheduling_group =
+            frame->GetAgentSchedulingGroup();
+        scoped_refptr<GinJavaBridgeMessageFilter> filter =
+            GinJavaBridgeMessageFilter::FromHost(
+                agent_scheduling_group, /*create_if_not_exists=*/false);
 
-  if (filter) {
-    filter->RemoveHost(this);
-  }
+        if (filter)
+          filter->RemoveHost(this);
+      });
 }
 
 void GinJavaBridgeDispatcherHost::PrimaryPageChanged(Page& page) {
