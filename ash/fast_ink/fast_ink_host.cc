6e2028db44b1a1110c293205bd85e80efdfd571e
Colin Blundell <blundell@chromium.org>
2023-10-20 07:35:06 +0000

    [//ash] Enable usage of single SharedImage in FastInkHost by default
    Can be reverted in case of problems.
    
    Bug: 1431257
    Change-Id: If9abee786c3152a858f515d4487a8b9a3f820a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4946613
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212604}

--- ash/fast_ink/fast_ink_host.cc
+++ ash/fast_ink/fast_ink_host.cc
@@ -32,7 +32,7 @@ namespace ash {
 namespace {
 BASE_FEATURE(kUseOneSharedImageForFastInkHostResources,
              "UseOneSharedImageForFastInkHostResources",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 }
 
 // -----------------------------------------------------------------------------
