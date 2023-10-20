79fcc6cdc74866d4db62622ebb7990b44535633d
Reilly Grant <reillyg@chromium.org>
2023-10-20 00:52:49 +0000

    [Gardener] Disable SwitchAccessTest.TypeIntoVirtualKeyboard for flake
    See logs attached to bug.
    
    Bug: 1472440
    Change-Id: I8436a264e897ac2bf83344e128eba038e8df3a06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956583
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Owners-Override: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212500}

--- chrome/browser/ash/accessibility/switch_access_browsertest.cc
+++ chrome/browser/ash/accessibility/switch_access_browsertest.cc
@@ -208,14 +208,8 @@ IN_PROC_BROWSER_TEST_F(SwitchAccessTest, NavigateButtonsInTextFieldMenu) {
   utils()->WaitForFocusRing("primary", "button", "Keyboard");
 }
 
-// TODO(crbug.com/1472440): Enable after fixing flakiness on Linux ChromiumOS
-// MSAN.
-#if BUILDFLAG(IS_CHROMEOS) && defined(MEMORY_SANITIZER)
-#define MAYBE_TypeIntoVirtualKeyboard DISABLED_TypeIntoVirtualKeyboard
-#else
-#define MAYBE_TypeIntoVirtualKeyboard TypeIntoVirtualKeyboard
-#endif
-IN_PROC_BROWSER_TEST_F(SwitchAccessTest, MAYBE_TypeIntoVirtualKeyboard) {
+// TODO(crbug.com/1472440): Enable after fixing flakiness.
+IN_PROC_BROWSER_TEST_F(SwitchAccessTest, DISABLED_TypeIntoVirtualKeyboard) {
   utils()->EnableSwitchAccess({'1', 'A'} /* select */, {'2', 'B'} /* next */,
                               {'3', 'C'} /* previous */);
 
