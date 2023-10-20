ac26b6d3cfb491169e7de92baceef4e70e07698d
Benedikt Meurer <bmeurer@chromium.org>
2023-10-20 11:32:14 +0000

    [DevTools] Deflake and re-enable dom-getFrameOwner.js test.
    The test was racy wrt. the target creation and catching the event, but
    it was also relying on the `nodeId` return value of `DOM.getFrameOwner`,
    which is not guaranteed to be there.
    
    Fixed: chromium:1367372, chromium:1179857
    Change-Id: I6f4b3f237cb175acfceb7facbb96c24707ce41d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958774
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1212668}

--- third_party/blink/web_tests/http/tests/inspector-protocol/dom/dom-getFrameOwner.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/dom/dom-getFrameOwner.js
@@ -1,18 +1,19 @@
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank('Tests DOM.getFrameOwner method.');
+  var {page, session, dp} =
+      await testRunner.startBlank('Tests DOM.getFrameOwner method.');
   await dp.Target.setDiscoverTargets({discover: true});
-  await session.evaluate(`
+  var [r] = await Promise.all([
+    dp.Target.onceTargetCreated(),
+    session.evaluate(`
     var iframe = document.createElement('iframe');
     iframe.id = 'outer_frame';
     iframe.src = 'data:text/html,<iframe id=inner_frame src="http://devtools.oopif.test:8000/resources/dummy.html">';
     document.body.appendChild(iframe);
-  `);
-
-  var frameId = (await dp.Target.onceTargetCreated()).params.targetInfo.targetId;
+  `),
+  ]);
   await dp.DOM.enable();
-  await dp.DOM.getDocument();
-  var r = await dp.DOM.getFrameOwner({frameId});
-  r = await dp.DOM.describeNode({nodeId: r.result.nodeId});
+  r = await dp.DOM.getFrameOwner({frameId: r.params.targetInfo.targetId});
+  r = await dp.DOM.describeNode({backendNodeId: r.result.backendNodeId});
   testRunner.log(r.result.node.nodeName + ' ' + r.result.node.attributes);
   testRunner.completeTest();
 })
