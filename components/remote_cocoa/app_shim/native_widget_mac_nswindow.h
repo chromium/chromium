b24fe65dee8d4131bf24775592d293e2dd5dec23
Leonard Grey <lgrey@chromium.org>
2023-10-20 20:47:27 +0000

    Revert "[Mac Immersive Fullscreen] Reland of prevent spaces switch on child window removal"
    This reverts commit 50361d0485ffaac5fa504ad4c38a68c00e8344a0 (but leaves the test file
    moving from browser_tests to interactive_ui_tests to reduce disruption)
    
    Also reverts 3b927a9dbb080ca137fd8da0a45acad2cf4ae554 which depends
    on this.
    
    (cherry picked from commit fae31a511ab4e049793131bc277413378d59dda7)
    
    Bug: 1492984
    Change-Id: I1aff7e38a374c8db61bf59a2ad62578747000702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960653
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1212948}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4960714
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6079@{#4}
    Cr-Branched-From: 06e5fef6d68b53d8ee8736490d832b20b67214b4-refs/heads/main@{#1212715}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.h
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.h
@@ -70,21 +70,6 @@ REMOTE_COCOA_APP_SHIM_EXPORT
 // screen bounds.
 - (NSRect)constrainFrameRect:(NSRect)frameRect toScreen:(NSScreen*)screen;
 
-// Returns YES if the window is in fullscreen mode.
-- (BOOL)isFullScreen;
-
-// Defers the removal of `childWindow` until the window is the active window
-// or ordered out, to avoid triggering a space change.
-- (void)removeChildWindowOnActivation:(NSWindow*)childWindow;
-
-// Returns YES if `aWindow` is a child window that will be removed when the
-// window activates or orders out.
-- (BOOL)willRemoveChildWindowOnActivation:(NSWindow*)aWindow;
-
-- (BOOL)hasDeferredChildWindowRemovalsForTesting;
-
-- (BOOL)hasDeferredChildWindowOrderingCommandsForTesting;
-
 // Identifier for the NativeWidgetMac from which this window was created. This
 // may be used to look up the NativeWidgetMacNSWindowHost in the browser process
 // or the NativeWidgetNSWindowBridge in a display process.
@@ -106,7 +91,7 @@ REMOTE_COCOA_APP_SHIM_EXPORT
 // Called whenever a child window is added to the receiver.
 @property(nonatomic, copy) void (^childWindowAddedHandler)(NSWindow* child);
 
-// Called whenever a child window is removed from the receiver.
+// Called whenever a child window is removed to the receiver.
 @property(nonatomic, copy) void (^childWindowRemovedHandler)(NSWindow* child);
 
 // Window to dispatch commands to. Needed for situations where the window that
