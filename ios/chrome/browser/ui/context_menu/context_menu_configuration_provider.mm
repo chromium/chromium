c5149353d5fc653a51ead1ae179ad979a20c7c51
Quentin Pubert <qpubert@google.com>
2023-10-20 09:11:43 +0000

    [iOS] Add context menu action histogram for Save to Photos
    This CL adds a new IOS.SaveToPhotos.ContextMenu histogram to determine
    what action is selected when the user long presses an image in a web
    page when the "Save in Google Photos" action is presented, and when it
    is not presented.
    
    A new user action MobileWebContextMenuImageWithSaveToPhotosImpression is
    added too so to count when the image context menu is impressed WITH the
    "Save in Google Photos" action.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE logging statements
    Bug: 1492924
    Change-Id: I6f0720b563424d574c04f5ab4ec09c215ea84620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4941832
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1212633}

--- ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.mm
@@ -12,6 +12,7 @@
 #import "ios/chrome/browser/favicon/favicon_loader.h"
 #import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #import "ios/chrome/browser/photos/photos_availability.h"
+#import "ios/chrome/browser/photos/photos_metrics.h"
 #import "ios/chrome/browser/policy/policy_util.h"
 #import "ios/chrome/browser/reading_list/model/reading_list_browser_agent.h"
 #import "ios/chrome/browser/search_engines/model/search_engines_util.h"
@@ -153,6 +154,8 @@ const NSUInteger kContextMenuMaxTitleLength = 30;
   const bool isLink = linkURL.is_valid();
   const GURL imageURL = params.src_url;
   const bool isImage = imageURL.is_valid();
+  const bool saveToPhotosAvailable =
+      IsSaveToPhotosAvailable(self.browser->GetBrowserState());
 
   DCHECK(self.browser->GetBrowserState());
   const bool isOffTheRecord = self.browser->GetBrowserState()->IsOffTheRecord();
@@ -256,17 +259,29 @@ const NSUInteger kContextMenuMaxTitleLength = 30;
                                  referrer:referrer
                                  webState:weakSelf.currentWebState
                        baseViewController:weakBaseViewController];
+      base::UmaHistogramEnumeration(
+          kSaveToPhotosContextMenuActionsHistogram,
+          saveToPhotosAvailable
+              ? SaveToPhotosContextMenuActions::kAvailableDidSaveImageLocally
+              : SaveToPhotosContextMenuActions::
+                    kUnavailableDidSaveImageLocally);
     }];
     [menuElements addObject:saveImage];
 
     // Save Image to Photos.
-    const BOOL saveToPhotosAvailable =
-        IsSaveToPhotosAvailable(self.browser->GetBrowserState());
     if (saveToPhotosAvailable) {
-      UIAction* saveImageToPhotosAction =
-          [actionFactory actionToSaveToPhotosWithImageURL:imageURL
-                                                 referrer:referrer
-                                                 webState:webState];
+      base::RecordAction(base::UserMetricsAction(
+          "MobileWebContextMenuImageWithSaveToPhotosImpression"));
+      UIAction* saveImageToPhotosAction = [actionFactory
+          actionToSaveToPhotosWithImageURL:imageURL
+                                  referrer:referrer
+                                  webState:webState
+                                     block:^{
+                                       base::UmaHistogramEnumeration(
+                                           kSaveToPhotosContextMenuActionsHistogram,
+                                           SaveToPhotosContextMenuActions::
+                                               kAvailableDidSaveImageToGooglePhotos);
+                                     }];
       [menuElements addObject:saveImageToPhotosAction];
     }
 
