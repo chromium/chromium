5bfd4f255398f5b5d6d64590cb0a613ddf3ed5f6
Filipa Senra <fsenra@google.com>
2023-10-23 14:06:56 +0000

    [Android][SA] Add custom label to site settings category page toggle
    Fixed: b:306386624
    Bug: 1478113
    Change-Id: I804e34d801883b78d2be5cc58c575f1d5b07497f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965601
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213471}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
@@ -279,9 +279,13 @@ public class ContentSettingsResources {
                         R.string.website_settings_category_sound_blocked);
 
             case ContentSettingsType.STORAGE_ACCESS:
-                return new ResourceItem(R.drawable.ic_storage_access_24,
-                        R.string.storage_access_permission_title, ContentSettingValues.ASK,
-                        ContentSettingValues.BLOCK, 0, 0);
+                return new ResourceItem(
+                        R.drawable.ic_storage_access_24,
+                        R.string.storage_access_permission_title,
+                        ContentSettingValues.ASK,
+                        ContentSettingValues.BLOCK,
+                        R.string.website_settings_category_storage_access_allowed,
+                        R.string.website_settings_category_storage_access_blocked);
 
             case ContentSettingsType.USB_CHOOSER_DATA:
                 return new ResourceItem(R.drawable.gm_filled_usb_24, 0, ContentSettingValues.ASK,
