9c093584851f4fe43f845cbb43663b0fae7a82ac
Kramer Ge <fangzhoug@chromium.org>
2023-10-23 17:08:24 +0000

    [Exo]Animation fix w.r.t RecreateLayers
    When an animation causes the `host_window` to `RecreateLayers`, the
    animation treats the new `host_window->layer` as if it's ready to be
    shown. Before the client has time to ack to the config event for that
    request, the `old_layer` prior to the cloning should be hosting the
    incoming wl_surface commits, i.e. it's the `commit_target_layer`.
    
    If incoming commits cause the surface hierarchy to change bounds, it
    should be changing bounds of the `commit_target_layer`, instead of the
    `host_window->layer`.
    
    This CL ensures that the bounds changes from the client goes to its
    `commit_target_layer`, and updates the `viz::SurfaceId` range for the
    `host_window->layer` s.t. fallback looks correct via stretching.
    
    Changes:
    1) `Config`: stores an extra field `old_layer` to track the Layer
       prior to cloning.
    2) `AllocateLocalSurfaceId`: `host_window`'s parent seq_number
       increments will be suppressed if they're originated from the client
       modifying the wayland surface hierarchy.
    3) `GetCommitTargetLayer()`: returns the ui::Layer that can host
       client's surface commits. Due to animation cancelling destroying the
       old_layer with it, this may return nullptr.
    4) `MaybeActivateSurface()`: for ShellSurface that has a config queue,
       it updates the surface range of `old_layer`s, and `host_window`'s
       layer, to be:
         [current_surface_id, max(layer_surface_id, current_surface_id)].
       And rely on stretching fallback surface when the deadline expires.
    
    Bug: 1251778, 1468344
    Change-Id: I4c7c95aead4a04037826abf94dac1fa247972f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4739372
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213576}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -1893,6 +1893,12 @@ void ShellSurfaceBase::UpdateWidgetBounds() {
 }
 
 void ShellSurfaceBase::UpdateHostWindowOrigin() {
+  // There's an animation happening on cloned layers, `host_window()` layer may
+  // be "ahead" of client's commit_target_layer. Do not update its origin,
+  // instead, rely on SurfaceLayer stretching until the client catches up.
+  if (GetCommitTargetLayer() != host_window()->layer()) {
+    return;
+  }
   gfx::Point origin = GetClientViewBounds().origin();
 
   origin += GetSurfaceOrigin().OffsetFromOrigin();
@@ -1946,7 +1952,15 @@ void ShellSurfaceBase::UpdateShadow() {
   aura::Window* window = widget_->GetNativeWindow();
 
   // Window shadows should be disabled if a window shape has been set.
-  if (!shadow_bounds_ || shape_dp_.has_value()) {
+  //
+  // Or if `host_window()`'s layer is not commit_target_layer, `shadow_bounds_`
+  // committed by the client should not go to current `widget_`'s shadow, but to
+  // the old widget's shadow prior to layer clone. Don't show the new shadow for
+  // now.
+  // TODO(crbug.com/1491604): Find the old widget's shadow layer and update it,
+  // and maybe show new widget's shadow by predicting its dimensions.
+  if (!shadow_bounds_ || shape_dp_.has_value() ||
+      GetCommitTargetLayer() != host_window()->layer()) {
     wm::SetShadowElevation(window, wm::kShadowElevationNone);
   } else {
     // Use a small style shadow for popup surface.
@@ -2199,7 +2213,7 @@ void ShellSurfaceBase::CommitWidget() {
   // type (e.g. caption height).
   UpdateFrameType();
   UpdateWidgetBounds();
-  UpdateHostWindowSizeAndRootSurfaceOrigin();
+  UpdateSurfaceLayerSizeAndRootSurfaceOrigin();
 
   // System modal container is used by clients to implement overlay
   // windows using a single ShellSurface instance.  If hit-test
