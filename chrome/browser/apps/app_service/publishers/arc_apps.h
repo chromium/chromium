1187d7120d528942e4b31e047d190ab0c2a1b5b4
Sharmin Zaman <sharminzaman@google.com>
2023-10-23 04:01:58 +0000

    [App Details] Add UpdateAppSize function for arc apps
    To solve a current discrepancy in app sizes between app detail UIs,
    a function has been created to notify ARC from app service, and app
    service from ARC, when any apps have updated to recalculate app
    sizes and ensure that all data is up-to-date
    
    See ag/25014922 for equivalent ARC changes.
    
    Bug: b/302254352
    Change-Id: I4d566d6cd1bc21846a8fa4c5b1e265cf9ce72218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930374
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Sharmin Zaman <sharminzaman@google.com>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213321}

--- chrome/browser/apps/app_service/publishers/arc_apps.h
+++ chrome/browser/apps/app_service/publishers/arc_apps.h
@@ -126,6 +126,7 @@ class ArcApps : public KeyedService,
   void PauseApp(const std::string& app_id) override;
   void UnpauseApp(const std::string& app_id) override;
   void StopApp(const std::string& app_id) override;
+  void UpdateAppSize(const std::string& app_id) override;
   void ExecuteContextMenuCommand(const std::string& app_id,
                                  int command_id,
                                  const std::string& shortcut_id,
