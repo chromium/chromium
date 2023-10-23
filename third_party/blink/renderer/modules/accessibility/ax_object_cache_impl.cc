22d7802c4c9dd349ebde83c83cb2c0d04dd20cbf
Aaron Leventhal <aleventhal@google.com>
2023-10-23 19:59:32 +0000

    M119: [A11y] When marking entire document dirty, don't repair parents
    This fixes a crash that can occur when marking a document dirty.
    
    Description of crash:
    MarkDocumentDirtyWithCleanLayout() iterates all nodes and
    calls SetNeedsToUpdateChildren(). This performs additional work
    to ensure that each node, all the way to the root, has a
    "has dirty descendants" flag set, and walks the parent chain,
    repairing missing parents if necessary. This extra loop is unnecessary
    because each node in the tree will already be visited by
    MarkDocumentDirtyWithCleanLayout(). The code path gets out of
    control because the calls to RepairMissingParent() end up creating
    objects as we are marking them all dirty, as well as updating
    cached values on objects (this was not expected to occur until
    UpdateTreeIfNeeded()). Finally, children changed events are fired
    because of the newly created objects. All of these unexpected
    occurrences result in methods being called on detached objects where
    that is not expected.
    
    Description of fix:
    Avoid walking parent chains while marking the document dirty.
    This fix may also be ported to later branches, however, these specific crashes have not been seen there yet. Either way, it is an improvement
    to avoid the unnecessary parent chain loops while visiting each node.
    
    (cherry picked from commit 30602f573445deea880c10fd2ad9d4cd0ecd77e5)
    
    Bug: 1480675, b/305236511, b/305967411, 1493459, 1475004
    Change-Id: Ifc1c803962bfcb98e49a986793d50470754a3580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4950098
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1212870}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4967816
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6045@{#850}
    Cr-Branched-From: 905e8bdd32d891451d94d1ec71682e989da2b0a1-refs/heads/main@{#1204232}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -2770,6 +2770,8 @@ void AXObjectCacheImpl::ProcessDeferredAccessibilityEvents(Document& document) {
     }
 #endif
 
+    mark_all_dirty_ = false;
+
     // Build out tree, such that each node has computed its children.
     if (RuntimeEnabledFeatures::AccessibilityEagerAXTreeUpdateEnabled()) {
       UpdateTreeIfNeeded();
@@ -4292,7 +4294,6 @@ void AXObjectCacheImpl::MarkDocumentDirtyWithCleanLayout() {
   // but will not create new AXObjects, which avoids resetting the user's
   // position in the content.
   DCHECK(mark_all_dirty_);
-  mark_all_dirty_ = false;
 
   // Assume all nodes in the tree need to recompute their properties.
   // Note that objects can remain in the tree without being re-created.
