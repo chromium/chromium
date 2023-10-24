743e3e5d2341b5854a47cea630109feaaadaded2
Leonid Baraz <lbaraz@chromium.org>
2023-10-24 00:28:27 +0000

    Add PRESUBMIT check: all #include use full path
    Bug: b:290829951
    Change-Id: Iefc571adeacf7e1a8aa25b2328439ab1455ea417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968935
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Alberto Juarez <albertojuarez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1213852}

---
+++ components/reporting/PRESUBMIT.py
@@ -0,0 +1,35 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit checks for //components/reporting
+
+See https://www.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+PRESUBMIT_VERSION = '2.0.0'
+
+from pathlib import Path
+
+def CheckIncludeForFullPath(input_api, output_api):
+  """Checks to make sure every .h file has a full path."""
+  errors = []
+  pattern = input_api.re.compile(r'^#include\s*[\"<](\S*)[\">]',
+                                  input_api.re.MULTILINE)
+  for f in input_api.AffectedSourceFiles(input_api.FilterSourceFile):
+    if not f.LocalPath().casefold().endswith(('.h', '.cc')):
+      continue
+    contents = input_api.ReadFile(f)
+    for include in pattern.finditer(contents):
+      header = include.group(1)
+      if not header.casefold().endswith('.h'):
+        continue
+      if not header.startswith('/'):
+        header = '../../' + header
+      if not Path(header).is_file():
+        errors.append(f.LocalPath() + ': ' + include.group(1))
+  if errors:
+    return [ output_api.PresubmitError(
+        'Some #include(s) in source errors do not have a full path. ',
+        errors) ]
+  return []
