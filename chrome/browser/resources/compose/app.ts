a9ee7dd3181bef149c6ebf33e7bb681f156bf0a6
John Lee <johntlee@chromium.org>
2023-10-24 01:26:20 +0000

    Update icons for Compose
    Bug: b:302741003
    Change-Id: I2d0f1aff812ee7bf755895967d61c03b2e833946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4968714
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213878}

--- chrome/browser/resources/compose/app.ts
+++ chrome/browser/resources/compose/app.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './icons.html.js';
 import './textarea.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
 import '//resources/cr_elements/cr_hidden_style.css.js';
