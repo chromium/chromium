3b83c12d66c560c5ada037a4e6b14f36b91bf457
dljames <dljames@google.com>
2023-10-24 21:46:03 +0000

    [ Extensions ] Fix ReadingList observer update bug
    Fixes a bug where updating the read status of a reading list entry would
    not notify the observers correctly or at all in some cases.
    
    Added a TODO to clean up this fix once MoveEntry and UpdateEntry
    functions are merged together.
    
    Bug: 1495466, 1424750
    Change-Id: I922e8d9864b2d629e633b7feaf381828959deaf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973417
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214462}

--- chrome/browser/extensions/api/reading_list/reading_list_api_unittest.cc
+++ chrome/browser/extensions/api/reading_list/reading_list_api_unittest.cc
@@ -527,6 +527,13 @@ TEST_F(ReadingListApiUnitTest, ReadingListOnEntryUpdated) {
 
   TestEventRouterObserver event_observer(EventRouter::Get(browser_context()));
 
+  reading_list_model->SetReadStatusIfExists(url, /*read=*/true);
+  EXPECT_TRUE(reading_list_model->GetEntryByURL(url)->IsRead());
+
+  EXPECT_EQ(event_observer.events().size(), 1u);
+  EXPECT_TRUE(base::Contains(event_observer.events(),
+                             api::reading_list::OnEntryUpdated::kEventName));
+
   reading_list_model->SetEntryTitleIfExists(url, "New title");
   EXPECT_EQ(reading_list_model->GetEntryByURL(url)->Title(), "New title");
 
