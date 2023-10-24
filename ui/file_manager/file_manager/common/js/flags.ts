73f8dfeb79417d2c63cbd78ceb30cb6108dfd833
Ben Reich <benreich@chromium.org>
2023-10-24 02:50:42 +0000

    [FilesTS] Create common `isFlagEnabled` function
    Bug: b/289003444
    Change-Id: I3e93a6902b0c44c5f391f9aba8e2b1a318f4ca53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969254
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1213914}

--- ui/file_manager/file_manager/common/js/flags.ts
+++ ui/file_manager/file_manager/common/js/flags.ts
@@ -4,108 +4,100 @@
 
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
 
+function isFlagEnabled(flagName: string) {
+  return loadTimeData.isInitialized() && loadTimeData.valueExists(flagName) &&
+      loadTimeData.getBoolean(flagName);
+}
+
 /**
  * Whether the Files app integration with DLP (Data Loss Prevention) is enabled.
  */
 export function isDlpEnabled() {
-  return loadTimeData.valueExists('DLP_ENABLED') &&
-      loadTimeData.getBoolean('DLP_ENABLED');
+  return isFlagEnabled('DLP_ENABLED');
 }
 
 /**
  * Returns true if FuseBoxDebug flag is enabled.
  */
 export function isFuseBoxDebugEnabled() {
-  return loadTimeData.isInitialized() &&
-      loadTimeData.valueExists('FUSEBOX_DEBUG') &&
-      loadTimeData.getBoolean('FUSEBOX_DEBUG');
+  return isFlagEnabled('FUSEBOX_DEBUG');
 }
 
 /**
  * Returns true if GuestOsFiles flag is enabled.
  */
 export function isGuestOsEnabled() {
-  return loadTimeData.getBoolean('GUEST_OS');
+  return isFlagEnabled('GUEST_OS');
 }
 
 /**
  * Returns true if Jelly flag is enabled.
  */
 export function isJellyEnabled() {
-  return loadTimeData.getBoolean('JELLY');
+  return isFlagEnabled('JELLY');
 }
 
 /**
  * Returns true if the cros-components flag is enabled.
  */
 export function isCrosComponentsEnabled() {
-  return loadTimeData.getBoolean('CROS_COMPONENTS');
+  return isFlagEnabled('CROS_COMPONENTS');
 }
 
 /**
  * Returns true if DriveFsMirroring flag is enabled.
  */
 export function isMirrorSyncEnabled() {
-  return loadTimeData.isInitialized() &&
-      loadTimeData.valueExists('DRIVEFS_MIRRORING') &&
-      loadTimeData.getBoolean('DRIVEFS_MIRRORING');
+  return isFlagEnabled('DRIVEFS_MIRRORING');
 }
 
 export function isGoogleOneOfferFilesBannerEligibleAndEnabled() {
-  return loadTimeData.getBoolean(
-      'ELIGIBLE_AND_ENABLED_GOOGLE_ONE_OFFER_FILES_BANNER');
+  return isFlagEnabled('ELIGIBLE_AND_ENABLED_GOOGLE_ONE_OFFER_FILES_BANNER');
 }
 
 /**
  * Returns true if FilesSinglePartitionFormat flag is enabled.
  */
 export function isSinglePartitionFormatEnabled() {
-  return loadTimeData.getBoolean('FILES_SINGLE_PARTITION_FORMAT_ENABLED');
+  return isFlagEnabled('FILES_SINGLE_PARTITION_FORMAT_ENABLED');
 }
 
 /**
  * Returns true if InlineSyncStatus feature flag is enabled.
  */
 export function isInlineSyncStatusEnabled() {
-  return loadTimeData.valueExists('INLINE_SYNC_STATUS') &&
-      loadTimeData.getBoolean('INLINE_SYNC_STATUS');
+  return isFlagEnabled('INLINE_SYNC_STATUS');
 }
 
 /**
  * Returns true if FilesDriveShortcuts flag is enabled.
  */
 export function isDriveShortcutsEnabled() {
-  return loadTimeData.isInitialized() &&
-      loadTimeData.valueExists('DRIVE_SHORTCUTS') &&
-      loadTimeData.getBoolean('DRIVE_SHORTCUTS');
+  return isFlagEnabled('DRIVE_SHORTCUTS');
 }
 
 /**
  * Returns whether the DriveFsBulkPinning feature flag is enabled.
  */
 export function isDriveFsBulkPinningEnabled() {
-  return loadTimeData.getBoolean('DRIVE_FS_BULK_PINNING');
+  return isFlagEnabled('DRIVE_FS_BULK_PINNING');
 }
 
 /**
  * Whether the new directory tree flag is enabled.
  */
 export function isNewDirectoryTreeEnabled() {
-  return loadTimeData.valueExists('NEW_DIRECTORY_TREE') &&
-      loadTimeData.getBoolean('NEW_DIRECTORY_TREE');
+  return isFlagEnabled('NEW_DIRECTORY_TREE');
 }
 
 export function isArcUsbStorageUIEnabled() {
-  return loadTimeData.valueExists('ARC_USB_STORAGE_UI_ENABLED') &&
-      loadTimeData.getBoolean('ARC_USB_STORAGE_UI_ENABLED');
+  return isFlagEnabled('ARC_USB_STORAGE_UI_ENABLED');
 }
 
 export function isArcVmEnabled() {
-  return loadTimeData.valueExists('ARC_VM_ENABLED') &&
-      loadTimeData.getBoolean('ARC_VM_ENABLED');
+  return isFlagEnabled('ARC_VM_ENABLED');
 }
 
 export function isPluginVmEnabled() {
-  return loadTimeData.valueExists('PLUGIN_VM_ENABLED') &&
-      loadTimeData.getBoolean('PLUGIN_VM_ENABLED');
+  return isFlagEnabled('PLUGIN_VM_ENABLED');
 }
