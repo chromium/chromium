f1f4e6d89bb45abba0052626ab060ac42b312bed
Leonard Grey <lgrey@chromium.org>
2023-10-24 19:02:22 +0000

    Mac: remove EnergyImpact
    Bug: 1493137
    Change-Id: I491cbe0e511287be88bd2317ddfd45cf891d81d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4955884
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214368}

--- base/process/process_metrics_mac.cc
+++ base/process/process_metrics_mac.cc
@@ -4,39 +4,12 @@
 
 #include "base/process/process_metrics.h"
 
-#include <AvailabilityMacros.h>
-#include <mach/mach.h>
-#include <mach/mach_time.h>
-#include <mach/mach_vm.h>
-#include <mach/shared_region.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <sys/sysctl.h>
 #include <memory>
 
-#include "base/apple/mach_logging.h"
-#include "base/logging.h"
-#include "base/mac/mac_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/process/process_metrics_iocounters.h"
-#include "base/time/time.h"
-#include "build/build_config.h"
 
 namespace base {
-
-namespace {
-
-double GetEnergyImpactInternal(mach_port_t task, uint64_t mach_time) {
-  return 0;
-}
-
-}  // namespace
-
-// Getting a mach task from a pid for another process requires permissions in
-// general, so there doesn't really seem to be a way to do these (and spinning
-// up ps to fetch each stats seems dangerous to put in a base api for anyone to
-// call). Child processes ipc their port, so return something if available,
-// otherwise return 0.
 
 // static
 std::unique_ptr<ProcessMetrics> ProcessMetrics::CreateProcessMetrics(
@@ -50,34 +23,7 @@ ProcessMetrics::ProcessMetrics(ProcessHandle process,
     : process_(process),
       last_absolute_idle_wakeups_(0),
       last_absolute_package_idle_wakeups_(0),
-      last_energy_impact_(0),
       port_provider_(port_provider) {}
-
-int ProcessMetrics::GetEnergyImpact() {
-  uint64_t now = mach_absolute_time();
-  if (last_energy_impact_ == 0) {
-    last_energy_impact_ = GetEnergyImpactInternal(TaskForPid(process_), now);
-    last_energy_impact_time_ = now;
-    return 0;
-  }
-
-  double total_energy_impact =
-      GetEnergyImpactInternal(TaskForPid(process_), now);
-  uint64_t delta = now - last_energy_impact_time_;
-  if (delta == 0) {
-    return 0;
-  }
-
-  // Scale by 100 since the histogram is integral.
-  double seconds_since_last_measurement =
-      base::TimeTicks::FromMachAbsoluteTime(delta).since_origin().InSecondsF();
-  int energy_impact = 100 * (total_energy_impact - last_energy_impact_) /
-                      seconds_since_last_measurement;
-  last_energy_impact_ = total_energy_impact;
-  last_energy_impact_time_ = now;
-
-  return energy_impact;
-}
 
 bool ProcessMetrics::GetIOCounters(IoCounters* io_counters) const {
   return false;
