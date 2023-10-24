2f845c3c093601e13d5a961b9ac48714bde6a6ce
Elmehdi Rahmaoui <erahmaoui@google.com>
2023-10-24 12:57:38 +0000

    [iOS]  Unit Conversion EG tests
    Add unit conversion eg test, update unit conversion provider to mimic
    the internal provider's behaviour.
    
    Bug: 1468905
    Change-Id: I95bb1b969013cfc9dd6b6e5e5cd2ea27f4faef30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4958679
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Éric Noyau <noyau@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214117}

--- ios/chrome/browser/providers/unit_conversion/chromium_unit_conversion.mm
+++ ios/chrome/browser/providers/unit_conversion/chromium_unit_conversion.mm
@@ -7,22 +7,52 @@
 namespace ios::provider {
 
 NSUnit* GetDefaultUnitForType(UnitType unitType) {
+  if (unitType == kUnitTypeMass) {
+    return [NSUnitMass kilograms];
+  }
+  if (unitType == kUnitTypeLength) {
+    return [NSUnitLength miles];
+  }
   return nil;
 }
 
 std::vector<UnitType> GetSupportedUnitTypes() {
-  return {};
+  return {kUnitTypeMass, kUnitTypeLength};
 }
 
 NSUnit* GetDefaultTargetUnit(NSUnit* unit) {
+  if ([unit isEqual:[NSUnitMass kilograms]]) {
+    return [NSUnitMass poundsMass];
+  }
+  if ([unit isEqual:[NSUnitLength miles]]) {
+    return [NSUnitLength yards];
+  }
   return nil;
 }
 
 const NSArray<NSArray<NSUnit*>*>* GetUnitsForType(UnitType unitType) {
+  if (unitType == kUnitTypeMass) {
+    return @[ @[ [NSUnitMass kilograms] ], @[ [NSUnitMass poundsMass] ] ];
+  }
+  if (unitType == kUnitTypeLength) {
+    return @[ @[ [NSUnitLength miles], [NSUnitLength yards] ] ];
+  }
   return nil;
 }
 
 NSString* GetFormattedUnit(NSUnit* unit) {
+  if ([unit isEqual:[NSUnitMass kilograms]]) {
+    return @"Kilograms (kg)";
+  }
+  if ([unit isEqual:[NSUnitMass poundsMass]]) {
+    return @"Pounds (lb)";
+  }
+  if ([unit isEqual:[NSUnitLength miles]]) {
+    return @"Miles (mi)";
+  }
+  if ([unit isEqual:[NSUnitLength yards]]) {
+    return @"Yards (yd)";
+  }
   return nil;
 }
 
