b2c3c254ed1e525e306617a72ba4bbcce816073b
Devlin Cronin <rdevlin.cronin@chromium.org>
2023-10-25 20:55:45 +0000

    [Extensions] Mark `userScripts` as restricted to dev mode
    Mark the `userScripts` API as restricted to dev mode. Update the
    current API tests to always have dev mode enabled, and add a new API
    test that verifies the API is unavailable when dev mode is not enabled.
    
    Bug: 1495451
    Change-Id: Ia74de0c32c4aad2732523eacd203ce05e6ad46d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974855
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215091}

--- chrome/browser/extensions/api/user_scripts/user_scripts_apitest.cc
+++ chrome/browser/extensions/api/user_scripts/user_scripts_apitest.cc
@@ -4,11 +4,13 @@
 
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/extensions/extension_util.h"
 #include "components/version_info/channel.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/common/extension_features.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
+#include "extensions/test/test_extension_dir.h"
 #include "net/dns/mock_host_resolver.h"
 
 namespace extensions {
@@ -22,6 +24,10 @@ class UserScriptsAPITest : public ExtensionApiTest {
 
   void SetUpOnMainThread() override {
     ExtensionApiTest::SetUpOnMainThread();
+
+    // The userScripts API is only available to users in developer mode.
+    util::SetDeveloperModeForProfile(profile(), true);
+
     host_resolver()->AddRule("*", "127.0.0.1");
     ASSERT_TRUE(StartEmbeddedTestServer());
   }
@@ -36,8 +42,14 @@ class UserScriptsAPITest : public ExtensionApiTest {
 };
 
 UserScriptsAPITest::UserScriptsAPITest() {
-  scoped_feature_list_.InitAndEnableFeature(
-      extensions_features::kApiUserScripts);
+  scoped_feature_list_.InitWithFeatures(
+      {extensions_features::kApiUserScripts,
+       // Also enable the dev mode restriction feature to gate the API on
+       // developer mode.
+       // TODO(https://crbug.com/1495451): Remove this when the feature is
+       // enabled by default.
+       extensions_features::kRestrictDeveloperModeAPIs},
+      /*disabled_features=*/{});
 }
 
 // TODO(crbug.com/1491361): Flaky on Linux debug.
@@ -124,6 +136,60 @@ IN_PROC_BROWSER_TEST_F(PersistentUserScriptsAPITest, PersistentScripts) {
   listener_->Reply(
       testing::UnitTest::GetInstance()->current_test_info()->name());
   EXPECT_TRUE(result_catcher_.GetNextResult()) << result_catcher_.message();
+}
+
+// A test suite that runs without developer mode enabled.
+class UserScriptsAPITestWithoutDeveloperMode : public UserScriptsAPITest {
+ public:
+  UserScriptsAPITestWithoutDeveloperMode() = default;
+  UserScriptsAPITestWithoutDeveloperMode(
+      const UserScriptsAPITestWithoutDeveloperMode&) = delete;
+  UserScriptsAPITestWithoutDeveloperMode& operator=(
+      const UserScriptsAPITestWithoutDeveloperMode&) = delete;
+  ~UserScriptsAPITestWithoutDeveloperMode() override = default;
+
+  void SetUpOnMainThread() override {
+    // Note: We explicitly do *not* call UserScriptsAPITest::SetUpOnMainThread()
+    // here. This ensures the user is not in developer mode.
+    ExtensionApiTest::SetUpOnMainThread();
+  }
+};
+
+// Verifies that the `chrome.userScripts` API is unavailable if the user doesn't
+// have dev mode turned on.
+IN_PROC_BROWSER_TEST_F(UserScriptsAPITestWithoutDeveloperMode,
+                       UserScriptsAPIIsUnavailableWithoutDeveloperMode) {
+  static constexpr char kManifest[] =
+      R"({
+           "name": "user scripts",
+           "manifest_version": 3,
+           "version": "0.1",
+           "background": {"service_worker": "background.js"},
+           "permissions": ["userScripts"]
+         })";
+  static constexpr char kBackgroundJs[] =
+      R"(chrome.test.runTests([
+           function userScriptsIsUnavailable() {
+             let caught = false;
+             try {
+               chrome.userScripts;
+             } catch (e) {
+               caught = true;
+               const expectedError =
+                   `The 'userScripts' API is only available for ` +
+                   `users in developer mode.`;
+               chrome.test.assertEq(expectedError, e.message);
+             }
+             chrome.test.assertTrue(caught);
+             chrome.test.succeed();
+           },
+         ]);)";
+
+  TestExtensionDir test_dir;
+  test_dir.WriteManifest(kManifest);
+  test_dir.WriteFile(FILE_PATH_LITERAL("background.js"), kBackgroundJs);
+
+  ASSERT_TRUE(RunExtensionTest(test_dir.UnpackedPath(), {}, {})) << message_;
 }
 
 }  // namespace extensions
