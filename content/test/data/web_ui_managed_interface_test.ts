24617acae55c5b10c89938168b09c317808e7c6b
Rebekah Potter <rbpotter@chromium.org>
2023-10-25 17:08:36 +0000

    WebUI: Migrate managed interface test to TS bindings
    Bug: 1002798
    Change-Id: Ifd36da076c8b3804bef4c60768b1273b781f6704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973421
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214923}

--- content/test/data/web_ui_managed_interface_test.ts
+++ content/test/data/web_ui_managed_interface_test.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {BarCallbackRouter, BazCallbackRouter, FooRemote, TestWebUIJsBridge} from '/web_ui_managed_interface_test.test-mojom-webui.js';
+import {BarCallbackRouter, BazCallbackRouter, FooRemote, TestWebUIJsBridge} from './web_ui_managed_interface_test.test-mojom-webui.js';
 
 Object.assign(
     window,
