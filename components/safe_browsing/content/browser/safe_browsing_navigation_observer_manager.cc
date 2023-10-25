5b744a490cd03900b5c7959ee00542dc252bc2d6
Nwokedi Idika <nwokedi@chromium.org>
2023-10-25 18:50:35 +0000

    [Notifications] Track validity of script urls
    Bug: b/307509406
    Change-Id: Iaf3904086bf20d03e4e543aa09dec87d482836c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973906
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Nwokedi Idika <nwokedi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214989}

--- components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.cc
+++ components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.cc
@@ -11,6 +11,7 @@
 #include "base/containers/cxx20_erase.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "base/time/time.h"
@@ -901,6 +902,8 @@ void SafeBrowsingNavigationObserverManager::RecordNotificationNavigationEvent(
           std::move(nav_event);
   UMA_HISTOGRAM_BOOLEAN(
       "SafeBrowsing.NavigationObserver.NotificationNavigationEventAdded", true);
+  base::UmaHistogramBoolean("SafeBrowsing.NavigationObserver.IsScriptUrlValid",
+                            script_url.is_valid());
 }
 
 void SafeBrowsingNavigationObserverManager::MaybeAddToReferrerChain(
