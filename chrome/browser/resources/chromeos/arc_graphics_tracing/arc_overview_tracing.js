aab622dc9e94d888dac3924a26ef1945ef75aac6
Matthew DeVore <matvore@chromium.org>
2023-10-25 00:17:13 +0000

    arc-overview-tracing: tweak max time input props
    Remove max value for the max-time, since 30 seconds is too low, and
    setting it to a true maximum in the thousands of seconds would be
    pointless because no one will use the up/down arrows to set the time
    then. Even with a maximum specified the user can type in any number.
    
    Turn off autocomplete so that we don't get uninterceptable updates of
    the value after the page loads.
    
    Test: log-out with arc-overview-tracing open and max time set to > 5, re-open browser, and confirm 5 is in input box
    Test: use arrow portions of input box to set profiling time to 70 and do a trace that stops automatically at that time
    Bug: b/302421589
    Change-Id: Ic04e2889902c6fbff76822ee6a8ced60a649bdcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4956840
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Matthew DeVore <matvore@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1214551}

--- chrome/browser/resources/chromeos/arc_graphics_tracing/arc_overview_tracing.js
+++ chrome/browser/resources/chromeos/arc_graphics_tracing/arc_overview_tracing.js
@@ -22,7 +22,15 @@ cr.define('cr.ArcOverviewTracing', function() {
         }
         chrome.send('setMaxTime', [value]);
       }, false);
+
+      // Note autocomplete for this input element must be *off* for this to
+      // do the right thing. Otherwise, the last-entered value may be restored
+      // automatically sometime after window.onload completes. I could not find
+      // the right event to intercept to capture the restored value. A vanilla
+      // HTML page I threw together did not have this problem, and the load
+      // event fired after the value was restored.
       chrome.send('setMaxTime', [parseFloat(maxTime.value)]);
+
       initializeOverviewUi();
     },
 
