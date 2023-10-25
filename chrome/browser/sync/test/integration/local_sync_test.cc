e912502f4369a25113408af7da4cdbea7d3fb583
Brahim Chikhaoui <bchikhaoui@google.com>
2023-10-25 12:54:36 +0000

    [Sync][Lacros] Fix LocalSyncTest.ShouldStart sync_integration_tests
    Fix LocalSyncTest.ShouldStart sync_integration_tests when
    kSyncChromeOSAppsToggleSharing is enabled. On Lacros,
    if kSyncChromeOSAppsToggleSharing is enabled, syncing of Apps
    is controlled via the OS, not the browser and we remove all related
    data types.
    
    Bug: 1330894 , b:307483579
    Change-Id: I36c3af78575ad72fefbff3dfb9939fb88626b4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4971139
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1214786}

--- chrome/browser/sync/test/integration/local_sync_test.cc
+++ chrome/browser/sync/test/integration/local_sync_test.cc
@@ -130,6 +130,16 @@ IN_PROC_BROWSER_TEST_F(LocalSyncTest, ShouldStart) {
     expected_active_data_types.Put(syncer::WEBAUTHN_CREDENTIAL);
   }
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // Apps sync is controlled by a dedicated preference on Lacros,
+  // corresponding to the Apps toggle in OS Sync settings. we remove
+  // data types related to the Apps sync toggle.
+  if (base::FeatureList::IsEnabled(syncer::kSyncChromeOSAppsToggleSharing)) {
+    expected_active_data_types.RemoveAll(
+        {syncer::APPS, syncer::APP_SETTINGS, syncer::WEB_APPS});
+  }
+#endif
+
   // The dictionary is currently only synced on Windows, Linux, and Lacros.
   // TODO(crbug.com/1052397): Reassess whether the following block needs to be
   // included in lacros-chrome once build flag switch of lacros-chrome is
@@ -147,6 +157,20 @@ IN_PROC_BROWSER_TEST_F(LocalSyncTest, ShouldStart) {
   EXPECT_FALSE(service->GetActiveDataTypes().Has(syncer::SHARING_MESSAGE));
   EXPECT_FALSE(service->GetActiveDataTypes().Has(syncer::SEND_TAB_TO_SELF));
   EXPECT_FALSE(service->GetActiveDataTypes().Has(syncer::HISTORY));
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // Apps sync is controlled by a dedicated preference on Lacros,
+  // corresponding to the Apps toggle in OS Sync settings.
+  if (base::FeatureList::IsEnabled(syncer::kSyncChromeOSAppsToggleSharing)) {
+    // Enable the Apps Toggle from OS level
+    service->GetUserSettings()->SetAppsSyncEnabledByOs(true);
+    // Wait until Sync has reconfigured itself and becomes active again.
+    ASSERT_TRUE(SyncTransportActiveChecker(service).Wait());
+    expected_active_data_types.PutAll(
+        {syncer::APPS, syncer::APP_SETTINGS, syncer::WEB_APPS});
+    EXPECT_EQ(service->GetActiveDataTypes(), expected_active_data_types);
+  }
+#endif
 }
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS_LACROS))
