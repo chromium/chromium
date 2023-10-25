12cca851becbab2f5c2bbdf9f6f3adc60832864d
Leonid Baraz <lbaraz@chromium.org>
2023-10-25 22:56:09 +0000

    Sync up PRESUBMIT.py to changes in missive
    Also add enumeration and allowlists of header files and prefixes.
    
    Bug: b:929082995
    Change-Id: Icbbaad599313e7ac2e51081f94babdadc24ed30f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973773
    Reviewed-by: Hong Xu <xuhong@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215163}

--- components/reporting/PRESUBMIT.py
+++ components/reporting/PRESUBMIT.py
@@ -9,31 +9,51 @@ for more details about the presubmit API built into depot_tools.
 
 PRESUBMIT_VERSION = '2.0.0'
 
-from pathlib import Path, PurePath
+from pathlib import Path
+from pathlib import PurePath
+
+
+# Header file names that do not need to be verified.
+ALLOWED_HEADERS = {
+    "errno.h",
+    "snappy.h",
+    "unistd.h",
+}
+
+ALLOWED_PREFIXES = {
+    "build",
+}
 
 def CheckIncludeForFullPath(input_api, output_api):
   """Checks to make sure every .h file has a full path."""
   errors = []
-  pattern = input_api.re.compile(r'^#include\s*\"(\S*)\"',
-                                  input_api.re.MULTILINE)
-  for f in input_api.AffectedSourceFiles(input_api.FilterSourceFile):
-    if not f.LocalPath().casefold().endswith(('.h', '.cc')):
+  pattern = input_api.re.compile(r'^#include\s*[\"<](\S*)[\">]')
+  for filename in input_api.AffectedSourceFiles(input_api.FilterSourceFile):
+    if not filename.LocalPath().endswith((".h", ".cc")):
       continue
-    contents = input_api.ReadFile(f)
-    for include in pattern.finditer(contents):
-      header = PurePath(include.group(1))
-      if header.suffix.casefold() != '.h':
+    contents = input_api.ReadFile(filename)
+    for lineno, line in enumerate(contents.splitlines(), start=1):
+      m = pattern.match(line)
+      if not m:
         continue
-      if header.parts[0] == 'build':
+
+      header = PurePath(m.group(1))
+      if header.suffix != ".h":
+        continue
+      if header.name in ALLOWED_HEADERS:
+        continue
+      if header.parts[0] in ALLOWED_PREFIXES:
         continue
       if not header.is_absolute():
         header = '../../' / header
-      if tuple(header.suffixes) == ('.pb', '.h'):
+      if tuple(header.suffixes) == (".pb", ".h"):
         # *.pb.h files don't exist in source code, replace the suffix with
-        # *.proto.
-        header = header.with_suffix('').with_suffix('.proto')
+        # *.proto
+        header = header.with_suffix("").with_suffix(".proto")
       if not Path(header).is_file():
-        errors.append(f.LocalPath() + ': ' + include.group(1))
+        errors.append(
+            filename.LocalPath() + ":" + str(lineno) + " " + m.group(1))
+
   if errors:
     return [ output_api.PresubmitError(
         'Some #include(s) in source errors do not have a full path. ',
