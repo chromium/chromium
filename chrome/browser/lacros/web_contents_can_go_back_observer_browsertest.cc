07121cb851015ca37372efb41d66e2cd8c6ff914
Tim Sergeant <tsergeant@chromium.org>
2023-10-26 22:53:46 +0000

    Sheriff: Disable flaky WebContentsCanGoBackObserverTest case
    This is a partial revert of https://crrev.com/c/4957497, which only
    recently re-enabled this test.
    
    Bug: b/257291972
    Change-Id: I8e9ecbb7a2781f1cf3668e40f8fcaaacc156b490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974804
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Vivian Pao <vpao@google.com>
    Commit-Queue: Vivian Pao <vpao@google.com>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215809}

--- chrome/browser/lacros/web_contents_can_go_back_observer_browsertest.cc
+++ chrome/browser/lacros/web_contents_can_go_back_observer_browsertest.cc
@@ -103,8 +103,15 @@ IN_PROC_BROWSER_TEST_F(WebContentsCanGoBackObserverTest, CanGoBack_ServerSide) {
   CheckCanGoBackOnServer(id, false /* expected_value */);
 }
 
+// TODO(b/257291972): This test is flaky on Lacros asan builder.
+#if BUILDFLAG(IS_CHROMEOS_LACROS) && defined(ADDRESS_SANITIZER)
+#define MAYBE_CanGoBackMultipleTabs_ServerSide \
+  DISABLED_CanGoBackMultipleTabs_ServerSide
+#else
+#define MAYBE_CanGoBackMultipleTabs_ServerSide CanGoBackMultipleTabs_ServerSide
+#endif
 IN_PROC_BROWSER_TEST_F(WebContentsCanGoBackObserverTest,
-                       CanGoBackMultipleTabs_ServerSide) {
+                       MAYBE_CanGoBackMultipleTabs_ServerSide) {
   auto* lacros_service = chromeos::LacrosService::Get();
   ASSERT_TRUE(lacros_service);
   ASSERT_TRUE(lacros_service->IsAvailable<crosapi::mojom::TestController>());
