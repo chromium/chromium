c3dc330b8797c6e8580e0e79df6bcde4945a5a30
Adam Walls <avvall@google.com>
2023-10-26 17:20:00 +0000

    [Canary branch 6091] Revert "WebView: Remove client-side package name allowlist"
    This reverts commit 7f92f6e0ee35b2a3b6c85f074108e5cff1294352.
    Note: Due to the deprecation of NO_SERVER_SIDE_FILTER_REQUIRED_DUE_TO_CLIENT_FILTERING enum value, records will now default to SERVER_SIDE_FILTER_UNSPECIFIED. Tests were modified to reflect that expectation.
    
    Reason for revert: Regression detected in crbug/1495877
    
    Original change's description:
    > WebView: Remove client-side package name allowlist
    >
    > WebView currently supports server-side app package name allowlist
    > filtering of app package names. The current on-device functionality
    > allows for client-side allowlist filtering via the server-side
    > allowlist flag.
    > This will remove the allowlist flag and all of the client-side
    > package name allowlist filtering code that uses the allowlist to determine whether to record app package names and the non-embedded WebView service that stores, caches, and updates the app package name allowlist filter via the component updater.
    >
    > Bug: 1484262
    > Change-Id: I2e47e2b15a5c7aebdbb65c22532306cef1d1eeb2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4875196
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Commit-Queue: Adam Walls <avvall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1198412}
    
    Bug: 1495877
    Change-Id: I63e1460358e036d0d9ee50e2ab25cdfedf36061c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979827
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Adam Walls <avvall@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6091@{#4}
    Cr-Branched-From: 655262c0626e76317d75d8b771c668706c9747ac-refs/heads/main@{#1215318}

--- android_webview/browser/component_updater/registration.cc
+++ android_webview/browser/component_updater/registration.cc
@@ -4,6 +4,7 @@
 
 #include "android_webview/browser/component_updater/registration.h"
 
+#include "android_webview/browser/component_updater/loader_policies/aw_apps_package_names_allowlist_component_loader_policy.h"
 #include "android_webview/browser/component_updater/loader_policies/empty_component_loader_policy.h"
 #include "android_webview/browser/component_updater/origin_trials_component_loader.h"
 #include "android_webview/browser/component_updater/trust_token_key_commitments_component_loader.h"
@@ -15,6 +16,8 @@ component_updater::ComponentLoaderPolicyVector GetComponentLoaderPolicies() {
   component_updater::ComponentLoaderPolicyVector policies;
   LoadTrustTokenKeyCommitmentsComponent(policies);
   LoadOriginTrialsComponent(policies);
+  LoadPackageNamesAllowlistComponent(policies,
+                                     AwMetricsServiceClient::GetInstance());
   LoadEmptyComponent(policies);
   return policies;
 }
