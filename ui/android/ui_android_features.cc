4d1d636c462da4587af843480779eb999c9df776
Robert Flack <flackr@chromium.org>
2023-10-26 21:19:22 +0000

    Enable report all pointer types feature.
    Bug: 1384181
    Change-Id: I85804a9cd1e660dd042ab15f3ba2b02dcc3b1457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979667
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215744}

--- ui/android/ui_android_features.cc
+++ ui/android/ui_android_features.cc
@@ -21,6 +21,6 @@ BASE_FEATURE(kPwaRestoreUi, "PwaRestoreUi", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kReportAllAvailablePointerTypes,
              "ReportAllAvailablePointerTypes",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace ui
