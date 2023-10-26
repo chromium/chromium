907d3fb7123557bc8ec922de829315808fb8989d
Matt Jones <mdjones@chromium.org>
2023-10-26 20:53:30 +0000

    [commerce] Move type implementations to cc file
    This patch moves the type implementations for the definitions in
    commerce_types.h to a corresponding cc file. This wasn't an issue in
    the past since users of the types also depended on the shopping
    service which isn't guaranteed to be the case.
    
    Change-Id: I09a2bad6a10cf960df538bd5274ec027f79b4f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4980165
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215728}

--- components/commerce/core/shopping_service.cc
+++ components/commerce/core/shopping_service.cc
@@ -82,48 +82,11 @@ const char kProductInfoLocalExtractionTime[] =
 
 const uint64_t kProductInfoLocalExtractionDelayMs = 2000;
 
-ProductInfo::ProductInfo() = default;
-ProductInfo::ProductInfo(const ProductInfo&) = default;
-ProductInfo& ProductInfo::operator=(const ProductInfo&) = default;
-ProductInfo::~ProductInfo() = default;
-
 ProductInfoCacheEntry::ProductInfoCacheEntry() = default;
 ProductInfoCacheEntry::~ProductInfoCacheEntry() = default;
 
 PriceInsightsInfoCacheEntry::PriceInsightsInfoCacheEntry() = default;
 PriceInsightsInfoCacheEntry::~PriceInsightsInfoCacheEntry() = default;
-
-MerchantInfo::MerchantInfo() = default;
-MerchantInfo::MerchantInfo(const MerchantInfo&) = default;
-MerchantInfo& MerchantInfo::operator=(const MerchantInfo&) = default;
-MerchantInfo::MerchantInfo(MerchantInfo&&) = default;
-MerchantInfo::~MerchantInfo() = default;
-
-PriceInsightsInfo::PriceInsightsInfo() = default;
-PriceInsightsInfo::PriceInsightsInfo(const PriceInsightsInfo&) = default;
-PriceInsightsInfo& PriceInsightsInfo::operator=(const PriceInsightsInfo&) =
-    default;
-PriceInsightsInfo::~PriceInsightsInfo() = default;
-
-DiscountInfo::DiscountInfo() = default;
-DiscountInfo::DiscountInfo(const DiscountInfo&) = default;
-DiscountInfo& DiscountInfo::operator=(const DiscountInfo&) = default;
-DiscountInfo::~DiscountInfo() = default;
-
-ParcelTrackingStatus::ParcelTrackingStatus() = default;
-ParcelTrackingStatus::ParcelTrackingStatus(const ParcelTrackingStatus&) =
-    default;
-ParcelTrackingStatus& ParcelTrackingStatus::operator=(
-    const ParcelTrackingStatus&) = default;
-ParcelTrackingStatus::~ParcelTrackingStatus() = default;
-ParcelTrackingStatus::ParcelTrackingStatus(const ParcelStatus& parcel_status) {
-  carrier = parcel_status.parcel_identifier().carrier();
-  tracking_id = parcel_status.parcel_identifier().tracking_id();
-  state = parcel_status.parcel_state();
-  tracking_url = GURL(parcel_status.tracking_url());
-  estimated_delivery_time = base::Time::FromDeltaSinceWindowsEpoch(
-      base::Microseconds(parcel_status.estimated_delivery_time_usec()));
-}
 
 ShoppingService::ShoppingService(
     const std::string& country_on_startup,
