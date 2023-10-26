f39a1ee2d839e280deb7aeb849d47116e6d2d058
Eugene Zemtsov <eugene@chromium.org>
2023-10-26 06:17:49 +0000

    media: Fix the race between MF VEA::Flush() and its outputs
    Sometimes when the client isn't in time to return output buffers to VEA,
    Flush() can signal completion before all outputs were actually transferred
    to the client.
    
    Bug: 1493841
    Change-Id: If7b9386ebff65d3d5daba2ad27c81d2f2d607792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4974147
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215341}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.h
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.h
@@ -125,6 +125,9 @@ class MEDIA_GPU_EXPORT MediaFoundationVideoEncodeAccelerator
     // We issued a drain message to the MF encoder want wait for the drain
     // to complete.
     kFlushing,
+    // We wait to return all encoded outputs from `encoder_output_queue_`
+    // before signaling that Flush() has finished.
+    kPostFlushing,
     kError,
   };
 
