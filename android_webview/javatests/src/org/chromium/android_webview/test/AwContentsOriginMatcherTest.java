e26d13a160b9b8cca1501fa1ea012eed2f2fc672
Rupert Ben Wiser <bewise@chromium.org>
2023-10-26 12:01:48 +0000

    webview: Expose AwContentsOriginMatcher to Java
    We want to make use of this class in Java land so that we can
    have consistent matching rules.
    Rather than re-implement this behaviour in Java, we are adding a
    simple wrapper class to call into a native implementation.
    
    The Java class doesn't keep onto any state.
    
    The one unfortunate detail is that callers of this API will have
    to call the destroy method to clear the native memory.
    
    Bug: 1493963
    Change-Id: Ib1735303350017074b5c2f6b1a288a19999f4ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973035
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215442}

---
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsOriginMatcherTest.java
@@ -0,0 +1,103 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import android.net.Uri;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.android_webview.AwContentsOriginMatcher;
+import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.Feature;
+
+import java.util.Arrays;
+
+/** AwContentsOriginMatcher tests. */
+@RunWith(AwJUnit4ClassRunner.class)
+@DoNotBatch(reason = "Shared dependencies among the tests cause conflicts during batch testing.")
+public class AwContentsOriginMatcherTest {
+    @Rule public AwActivityTestRule mActivityTestRule = new AwActivityTestRule();
+
+    private AwContentsOriginMatcher mMatcher;
+
+    @Before
+    public void setup() {
+        mMatcher = new AwContentsOriginMatcher();
+    }
+
+    @After
+    public void tearDown() {
+        mMatcher.destroy();
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testMatchesOrigin_returnsFalse_whenEmpty() {
+        Assert.assertFalse(mMatcher.matchesOrigin(Uri.parse("http://webview.com")));
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testMatchesOrigin_returnsFalse_whenNoMatching() {
+        mMatcher.updateRuleList(Arrays.asList("http://somesite.com", "http://webviewalmost.com"));
+        Assert.assertFalse(mMatcher.matchesOrigin(Uri.parse("http://webview.com")));
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testMatchesOrigin_returnsTrue_whenMatches() {
+        mMatcher.updateRuleList(Arrays.asList("http://somesite.com", "http://webview.com"));
+        Assert.assertTrue(mMatcher.matchesOrigin(Uri.parse("http://webview.com")));
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testMatchesOrigin_returnsTrue_whenSubDomainMatches() {
+        mMatcher.updateRuleList(Arrays.asList("http://somesite.com", "http://*.webview.com"));
+        Assert.assertTrue(mMatcher.matchesOrigin(Uri.parse("http://sub.webview.com")));
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testUpdateRuleList() {
+        String[] badRules =
+                mMatcher.updateRuleList(
+                        Arrays.asList("http://somesite.com", "oh no", "http://*.webview.com"));
+        // Still doesn't match even though that list had a rule that should match.
+        // Because updateRuleList won't update the rules if there was a bad one.
+        Assert.assertFalse(mMatcher.matchesOrigin(Uri.parse("http://sub.webview.com")));
+        Assert.assertEquals(new String[] {"oh no"}, badRules);
+
+        // After removing the bad rule, we should no longer get any rules returned and things should
+        // match
+        badRules =
+                mMatcher.updateRuleList(
+                        Arrays.asList("http://somesite.com", "http://*.webview.com"));
+
+        Assert.assertTrue(mMatcher.matchesOrigin(Uri.parse("http://sub.webview.com")));
+        Assert.assertEquals(new String[] {}, badRules);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testThrowsAfterDestroy() {
+        AwContentsOriginMatcher matcher = new AwContentsOriginMatcher();
+        matcher.destroy();
+        matcher.matchesOrigin(Uri.parse("http://sub.webview.com"));
+    }
+}
