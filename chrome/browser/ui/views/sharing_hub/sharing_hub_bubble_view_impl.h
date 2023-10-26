a14b5bd464c698e0f0bbb46f6990cd4a6859a23b
Elly <ellyjones@chromium.org>
2023-10-26 22:45:38 +0000

    desktop-sharing-hub: tidy code
    This change:
    * Removes some unused member variables and methods
    * Removes some unnecessary overrides and replaces them with uses of setters
    
    Bug: None
    Change-Id: I5955262c0d54a074daebd65f9b000effc39d57d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981175
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1215800}

--- chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.h
+++ chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.h
@@ -38,18 +38,11 @@ class SharingHubBubbleViewImpl : public SharingHubBubbleView,
   // SharingHubBubbleView:
   void Hide() override;
 
-  // views::WidgetDelegateView:
-  bool ShouldShowCloseButton() const override;
-  bool ShouldShowWindowTitle() const override;
-  void WindowClosing() override;
-
   // LocationBarBubbleDelegateView:
-  std::u16string GetAccessibleWindowTitle() const override;
   void OnThemeChanged() override;
 
+  // Public for testing.
   void OnActionSelected(SharingHubBubbleActionButton* button);
-
-  const views::View* GetButtonContainerForTesting() const;
 
  private:
   // views::BubbleDialogDelegateView:
@@ -66,6 +59,10 @@ class SharingHubBubbleViewImpl : public SharingHubBubbleView,
   // size.
   void MaybeSizeToContents();
 
+  // Callback when the window is about to close, either from external causes
+  // (parent window closed) or because the web contents requested it.
+  void OnWindowClosing();
+
   // A raw pointer is *not* safe here; the controller can be torn down before
   // the bubble during the window close path, since the bubble will be closed
   // asynchronously during browser window teardown but the controller will be
@@ -75,14 +72,8 @@ class SharingHubBubbleViewImpl : public SharingHubBubbleView,
   // ScrollView containing the list of share/save actions.
   raw_ptr<views::ScrollView> scroll_view_ = nullptr;
 
-  // The "Share link to" annotation text, which indicates to the user what
-  // the 3P target options do.
-  raw_ptr<views::Label> share_link_label_ = nullptr;
-
   // The share attempt this bubble was opened for.
   const share::ShareAttempt attempt_;
-
-  base::WeakPtrFactory<SharingHubBubbleViewImpl> weak_factory_{this};
 };
 
 }  // namespace sharing_hub
