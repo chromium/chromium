a6212d6f67074db15c81771e94cb7fa14fe3a7a8
punithnayak <npunith125@gmail.com>
2023-10-26 19:10:35 +0000

    Removing dangling pointer from SignalsAggregatorImplTest
    Change-Id: I7226d2d1141acf99930e71c1efd4669def11b64c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969921
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1215662}

--- components/device_signals/core/browser/signals_aggregator_impl_unittest.cc
+++ components/device_signals/core/browser/signals_aggregator_impl_unittest.cc
@@ -81,10 +81,10 @@ class SignalsAggregatorImplTest : public testing::Test {
   }
 
   base::test::TaskEnvironment task_environment_;
-  raw_ptr<MockSignalsCollector, DanglingUntriaged> av_signal_collector_;
-  raw_ptr<MockSignalsCollector, DanglingUntriaged> hotfix_signal_collector_;
   testing::StrictMock<MockUserPermissionService> mock_permission_service_;
   std::unique_ptr<SignalsAggregatorImpl> aggregator_;
+  raw_ptr<MockSignalsCollector> av_signal_collector_;
+  raw_ptr<MockSignalsCollector> hotfix_signal_collector_;
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   UserContext user_context_{kGaiaId};
