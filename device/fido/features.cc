38ca2c4ae23d4ea69ad49c9e8c4035cafac241f7
Adem Derinel <derinel@google.com>
2023-10-27 15:37:30 +0000

    [Android][CredMan] Enable CredMan by default and bump GMSCore version
    The GMSCore version that will include the password selection
    confirmation is coming in W46.
    The version is not bumped for canary & dev in order to get testing
    coverage.
    
    Bug: 1434278, 1474805
    Change-Id: Ie6c07c2c55b9d048d4603f9ae93971662175b9d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4983929
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216153}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -62,7 +62,7 @@ BASE_FEATURE(kWebAuthnGoogleCorpRemoteDesktopClientPrivilege,
 // Added in M114. Not yet enabled by default.
 BASE_FEATURE(kWebAuthnAndroidCredMan,
              "WebAuthenticationAndroidCredMan",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enabled in M118. Remove in or after M121.
 BASE_FEATURE(kWebAuthnAndroidCredManForHybrid,
