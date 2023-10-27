a1201da4ec6783d6ded002784a683d9b58e0ca5e
Aliona DANGLA <alionadangla@google.com>
2023-10-27 10:29:22 +0000

    [iOS] Stop fullscreen animation only if there is one running
    When a user overscroll, it disable fullcreen. When fullscreen is
    disabled, it stop animating. In this case, fullscreen is not all the
    time animated, which is causing crash. This CL put a guard to avoid
    to call stop animating function when there is no animation.
    
    Fixed: 1432220
    Change-Id: If7aa1843701ae5db4fcc1d0f7f95bb62946ab39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4972715
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216044}

--- ios/chrome/browser/ui/fullscreen/fullscreen_mediator.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_mediator.mm
@@ -121,7 +121,10 @@ void FullscreenMediator::FullscreenModelProgressUpdated(
 void FullscreenMediator::FullscreenModelEnabledStateChanged(
     FullscreenModel* model) {
   DCHECK_EQ(model_, model);
-  StopAnimating(true /* update_model */);
+  // Stops the animation only if there is a current animation running.
+  if (animator_ && animator_.state == UIViewAnimatingStateActive) {
+    StopAnimating(true /* update_model */);
+  }
   for (auto& observer : observers_) {
     observer.FullscreenEnabledStateChanged(controller_, model->enabled());
   }
