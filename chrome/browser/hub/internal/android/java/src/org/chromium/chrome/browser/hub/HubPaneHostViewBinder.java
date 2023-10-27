2852034c3a24d426282e7bef0856a8c89bec416d
Sky Malice <skym@chromium.org>
2023-10-27 18:15:25 +0000

    Initial view properties for pane host.
    Bug: 1496708, 1496700
    Change-Id: I499399dd6d2a7a4c90a817003becfa8b98c295a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4982418
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216270}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostViewBinder.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostViewBinder.java
@@ -4,11 +4,20 @@
 
 package org.chromium.chrome.browser.hub;
 
+import static org.chromium.chrome.browser.hub.HubPaneHostProperties.ACTION_BUTTON_DATA;
+import static org.chromium.chrome.browser.hub.HubPaneHostProperties.PANE_ROOT_VIEW;
+
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /** Applies properties to the view that holds one pane at a time. */
 public class HubPaneHostViewBinder {
     /** Stateless propagation of properties. */
-    public static void bind(PropertyModel model, HubPaneHostView view, PropertyKey propertyKey) {}
+    public static void bind(PropertyModel model, HubPaneHostView view, PropertyKey key) {
+        if (key == PANE_ROOT_VIEW) {
+            view.setRootView(model.get(PANE_ROOT_VIEW));
+        } else if (key == ACTION_BUTTON_DATA) {
+            view.setActionButtonData(model.get(ACTION_BUTTON_DATA));
+        }
+    }
 }
