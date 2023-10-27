8e838a5f992a74a47d4d1da27f72a2089aabd6e5
Benedikt Meurer <bmeurer@chromium.org>
2023-10-27 08:06:57 +0000

    [DevTools] Remove bogus test for "array like" objects.
    This test cements the expectation that "array like" objects in
    JavaScript have to have a "splice" property that is callable,
    which has nothing to do with any common definition of "array
    like" objects in JavaScript.
    
    Bug: chromium:1495781
    Change-Id: I709b5201e0d6756e020bbdd8b2e64ea65bab1210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4979771
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1216001}

--- third_party/blink/web_tests/http/tests/devtools/console/console-format-collections.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-format-collections.js
@@ -73,17 +73,6 @@ import {ConsoleTestRunner} from 'console_test_runner';
         // DOMTokenList
         var div = document.getElementsByTagName("div")[0];
         console.log(div.classList);
-
-        // Array-like's.
-        console.log(new ArrayLike(5));
-        console.log(new ArrayLike(0xFFFFFFFF));
-        // Array-like's with wrong length.
-        console.log(new ArrayLike(-5));
-        console.log(new ArrayLike(5.6));
-        console.log(new ArrayLike(NaN));
-        console.log(new ArrayLike(Infinity));
-        console.log(new ArrayLike(-0));
-        console.log(new ArrayLike(0xFFFFFFFF + 1));
     }
 
     logToConsole();
@@ -97,12 +86,6 @@ import {ConsoleTestRunner} from 'console_test_runner';
         console.log("FAIL: 'length' should not be called");
         return this.keys.length;
     });
-
-    function ArrayLike(length)
-    {
-        this.length = length;
-    }
-    ArrayLike.prototype.splice = function() {};
   `);
 
   TestRunner.evaluateInPage('logToConsole()', callback);
