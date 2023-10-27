e5c90cd1afeb249a85e5d4fc6c9476db0e1a9227
François Beaufort <beaufort.francois@gmail.com>
2023-10-27 15:05:06 +0000

    WebGPU: Add the maxBindGroupsPlusVertexBuffers limit
    Intent to ship: https://groups.google.com/a/chromium.org/g/blink-dev/c/jIevLHrxejE
    
    Bug: dawn:1849
    Change-Id: Ic0c44cee63fb804d0c327569de9391e4757ab3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4952541
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1216115}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
@@ -16,6 +16,7 @@
   X(maxTextureDimension3D)                     \
   X(maxTextureArrayLayers)                     \
   X(maxBindGroups)                             \
+  X(maxBindGroupsPlusVertexBuffers)            \
   X(maxBindingsPerBindGroup)                   \
   X(maxDynamicUniformBuffersPerPipelineLayout) \
   X(maxDynamicStorageBuffersPerPipelineLayout) \
