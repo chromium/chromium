8b3158fe16d392d551ff760608213a9c9bb4048d
Rafał Godlewski <rgod@google.com>
2023-10-30 15:57:41 +0000

    [iOS] Hide the share button with the enterprise policy disabled
    Bug: 1463882
    Change-Id: If006be7d193056d26e7a458163ef1d8a7f860ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4983911
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1216966}

--- ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_egtest.mm
+++ ios/chrome/browser/ui/settings/password/password_sharing/password_sharing_egtest.mm
@@ -5,6 +5,7 @@
 #import "base/test/ios/wait_util.h"
 #import "components/password_manager/core/browser/features/password_features.h"
 #import "components/password_manager/core/common/password_manager_features.h"
+#import "components/password_manager/core/common/password_manager_pref_names.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/signin/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
@@ -120,10 +121,14 @@ void SignInAndEnableSync() {
 - (void)setUp {
   [super setUp];
 
-  // Make sure the pref is in its non-default state (which should be the case
-  // for all tests that do not test the first run experience flow).
+  // Make sure the following pref is in its non-default state (which should be
+  // the case for all tests that do not test the first run experience flow).
   [ChromeEarlGrey setBoolValue:YES
                    forUserPref:prefs::kPasswordSharingFlowHasBeenEntered];
+  // Make sure the password sharing pref is in its default state.
+  [ChromeEarlGrey
+      setBoolValue:YES
+       forUserPref:password_manager::prefs::kPasswordSharingEnabled];
 }
 
 - (void)tearDown {
@@ -133,6 +138,10 @@ void SignInAndEnableSync() {
   // for all tests that do not test the first run experience flow).
   [ChromeEarlGrey setBoolValue:YES
                    forUserPref:prefs::kPasswordSharingFlowHasBeenEntered];
+  // Reset the password sharing pref to its default state.
+  [ChromeEarlGrey
+      setBoolValue:YES
+       forUserPref:password_manager::prefs::kPasswordSharingEnabled];
 
   [super tearDown];
 }
@@ -172,6 +181,19 @@ void SignInAndEnableSync() {
   [[EarlGrey
       selectElementWithMatcher:grey_accessibilityID(kPasswordShareButtonId)]
       assertWithMatcher:grey_sufficientlyVisible()];
+}
+
+- (void)testShareButtonVisibilityWithSharingPolicyDisabled {
+  [ChromeEarlGrey
+      setBoolValue:NO
+       forUserPref:password_manager::prefs::kPasswordSharingEnabled];
+
+  SignInAndEnableSync();
+  [self saveExamplePasswordAndOpenDetails];
+
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(kPasswordShareButtonId)]
+      assertWithMatcher:grey_not(grey_sufficientlyVisible())];
 }
 
 - (void)testFamilyPickerCancelFlow {
