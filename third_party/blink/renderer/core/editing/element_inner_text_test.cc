5cf863d08897051721dc1130618c5e5892684136
Scott Violet <sky@chromium.org>
2023-10-30 23:28:24 +0000

    blink: adds test to ensure TextVisitor called for <select>
    Specifically to ensure the descendants are called correctly.
    
    This also moves allowing include of stack_allocated to
    third_party/blink/DEPS.
    
    Bug: b:304353698
    Change-Id: Ie16e4ac425e214bfbf9dbc8521e76d9c3da572e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977256
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217267}

--- third_party/blink/renderer/core/editing/element_inner_text_test.cc
+++ third_party/blink/renderer/core/editing/element_inner_text_test.cc
@@ -4,13 +4,17 @@
 
 #include "third_party/blink/renderer/core/dom/element.h"
 
+#include "base/memory/stack_allocated.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/text_visitor.h"
 #include "third_party/blink/renderer/core/editing/testing/editing_test_base.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
 
-class ElementInnerTest : public EditingTestBase {};
+using ElementInnerTest = EditingTestBase;
 
 // http://crbug.com/877498
 TEST_F(ElementInnerTest, ListItemWithLeadingWhiteSpace) {
@@ -55,6 +59,45 @@ TEST_F(ElementInnerTest, GetInnerTextWithoutUpdate) {
   Element& target = *GetDocument().getElementById(AtomicString("target"));
   EXPECT_EQ("abc", target.innerText());
   EXPECT_EQ("abc", target.GetInnerTextWithoutUpdate());
+}
+
+using VisitedNodes = HeapHashSet<Member<const Node>>;
+class TextVisitorImpl : public TextVisitor {
+  STACK_ALLOCATED();
+
+ public:
+  explicit TextVisitorImpl(VisitedNodes& nodes) : nodes_(nodes) {}
+
+  // TextVisitor:
+  void WillVisit(const Node& element, unsigned offset) override {
+    nodes_.insert(&element);
+  }
+
+ private:
+  VisitedNodes& nodes_;
+};
+
+// Ensures TextVisitor is called for all children of a <select>.
+TEST_F(ElementInnerTest, VisitAllChildrenOfSelect) {
+  SetBodyContent(
+      "<select id='0'><optgroup id='1'><option "
+      "id='2'></option></optgroup><option id='3'></option></select>");
+  VisitedNodes visited_nodes;
+  TextVisitorImpl visitor(visited_nodes);
+  GetDocument().body()->getElementById(AtomicString("0"))->innerText(&visitor);
+
+  // The select and all its descendants should be visited. Each one has an
+  // id from 0-4.
+  for (int i = 0; i < 4; ++i) {
+    Element* element =
+        GetDocument().getElementById(AtomicString(String::Number(i)));
+    ASSERT_TRUE(element) << i;
+    EXPECT_TRUE(visited_nodes.Contains(element)) << i;
+    visited_nodes.erase(element);
+  }
+
+  // Nothing else should remain.
+  EXPECT_TRUE(visited_nodes.empty());
 }
 
 }  // namespace blink
