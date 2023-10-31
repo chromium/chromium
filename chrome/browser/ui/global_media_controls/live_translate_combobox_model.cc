aced02fea191782a3c9a7c6be032a0ac816b9003
Evan Liu <evliu@google.com>
2023-10-31 00:13:55 +0000

    Update languages in the Live Translate dropdown
    This CL updates the languages in the Live Translate dropdown in the global media control to match the languages in the dropdown on the caption settings page.
    
    Bug: b/307626460
    Change-Id: I3ec751c08d094c26423b202abeafa6510683b6e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977571
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1217309}

--- chrome/browser/ui/global_media_controls/live_translate_combobox_model.cc
+++ chrome/browser/ui/global_media_controls/live_translate_combobox_model.cc
@@ -13,6 +13,36 @@
 #include "components/prefs/pref_service.h"
 #include "ui/base/l10n/l10n_util.h"
 
+namespace {
+bool LanguageSupportsTranslate(translate::TranslateLanguageInfo language) {
+  // The language must be marked as translatable.
+  if (!language.supports_translate) {
+    return false;
+  }
+
+  if (language.code == "zh-CN" || language.code == "zh-TW") {
+    // In Translate, general Chinese is not used, and the sub code is
+    // necessary as a language code for the Translate server.
+    return true;
+  }
+
+  if (language.code == "mni-Mtei") {
+    // Translate uses the Meitei Mayek script for Manipuri.
+    return true;
+  }
+
+  const auto& base_language = l10n_util::GetLanguage(language.code);
+  if (base_language == "nb") {
+    // Norwegian Bokmål (nb) is listed as supporting translate but the
+    // Translate server only supports Norwegian (no).
+    return false;
+  }
+
+  // For all other languages only base languages are supported.
+  return language.code == base_language;
+}
+}  // namespace
+
 LiveTranslateComboboxModel::LiveTranslateComboboxModel(Profile* profile)
     : profile_(profile) {
   std::vector<translate::TranslateLanguageInfo> language_list;
@@ -20,8 +50,7 @@ LiveTranslateComboboxModel::LiveTranslateComboboxModel(Profile* profile)
       g_browser_process->GetApplicationLocale(), true, &language_list);
 
   for (const auto& language : language_list) {
-    if (language.supports_translate &&
-        language.code == l10n_util::GetLanguage(language.code)) {
+    if (LanguageSupportsTranslate(language)) {
       languages_.push_back(language);
     }
   }
