ddd30a3e80f37cae78c9050ce71c966ca5dc374f
Andrew Paseltiner <apaseltiner@chromium.org>
2023-10-31 13:47:35 +0000

    Move Attribution Reporting proto serialization to sql_utils
    To reduce the complexity of attribution_storage_sql.cc and enable
    more comprehensive testing.
    
    I will relocate existing tests and add further coverage in a followup
    change.
    
    Bug: 1496390
    Change-Id: Ib2d0903f5ba744f689381b48b7f4b2eb2e2ad34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4985510
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217540}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -21,7 +21,6 @@
 #include "base/check_op.h"
 #include "base/containers/enum_set.h"
 #include "base/containers/flat_set.h"
-#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/overloaded.h"
@@ -36,7 +35,6 @@
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
 #include "base/uuid.h"
-#include "components/aggregation_service/features.h"
 #include "components/attribution_reporting/aggregatable_dedup_key.h"
 #include "components/attribution_reporting/aggregatable_trigger_data.h"
 #include "components/attribution_reporting/aggregation_keys.h"
@@ -47,7 +45,6 @@
 #include "components/attribution_reporting/features.h"
 #include "components/attribution_reporting/filters.h"
 #include "components/attribution_reporting/source_registration.h"
-#include "components/attribution_reporting/source_registration_time_config.mojom.h"
 #include "components/attribution_reporting/source_type.mojom.h"
 #include "components/attribution_reporting/suitable_origin.h"
 #include "components/attribution_reporting/trigger_config.h"
@@ -97,7 +94,6 @@ using EventLevelResult = ::content::AttributionTrigger::EventLevelResult;
 
 using ::attribution_reporting::EventReportWindows;
 using ::attribution_reporting::SuitableOrigin;
-using ::attribution_reporting::mojom::SourceRegistrationTimeConfig;
 using ::attribution_reporting::mojom::SourceType;
 using ::attribution_reporting::mojom::TriggerDataMatching;
 
@@ -185,266 +181,6 @@ absl::optional<AttributionReport::Type> DeserializeReportType(int val) {
     default:
       return absl::nullopt;
   }
-}
-
-std::string SerializeFilterData(
-    const attribution_reporting::FilterData& filter_data) {
-  proto::AttributionFilterData msg;
-
-  for (const auto& [filter, values] : filter_data.filter_values()) {
-    proto::AttributionFilterValues filter_values_msg;
-    filter_values_msg.mutable_values()->Add(values.begin(), values.end());
-    (*msg.mutable_filter_values())[filter] = std::move(filter_values_msg);
-  }
-
-  return msg.SerializeAsString();
-}
-
-absl::optional<attribution_reporting::FilterData> DeserializeFilterData(
-    sql::Statement& stmt,
-    int col) {
-  std::string string;
-  if (!stmt.ColumnBlobAsString(col, &string)) {
-    return absl::nullopt;
-  }
-
-  proto::AttributionFilterData msg;
-  if (!msg.ParseFromString(string)) {
-    return absl::nullopt;
-  }
-
-  attribution_reporting::FilterValues::container_type filter_values;
-  filter_values.reserve(msg.filter_values().size());
-
-  for (google::protobuf::MapPair<std::string, proto::AttributionFilterValues>&
-           entry : *msg.mutable_filter_values()) {
-    // Serialized source filter data can only contain these keys due to DB
-    // corruption or deliberate modification.
-    if (entry.first ==
-            attribution_reporting::FilterData::kSourceTypeFilterKey ||
-        entry.first ==
-            attribution_reporting::FilterConfig::kLookbackWindowKey) {
-      continue;
-    }
-
-    google::protobuf::RepeatedPtrField<std::string>* values =
-        entry.second.mutable_values();
-
-    filter_values.emplace_back(
-        entry.first,
-        std::vector<std::string>(std::make_move_iterator(values->begin()),
-                                 std::make_move_iterator(values->end())));
-  }
-
-  return attribution_reporting::FilterData::Create(std::move(filter_values));
-}
-
-std::string SerializeAggregationKeys(
-    const attribution_reporting::AggregationKeys& keys) {
-  proto::AttributionAggregatableSource msg;
-
-  for (const auto& [id, key] : keys.keys()) {
-    proto::AttributionAggregationKey key_msg;
-    key_msg.set_high_bits(absl::Uint128High64(key));
-    key_msg.set_low_bits(absl::Uint128Low64(key));
-    (*msg.mutable_keys())[id] = std::move(key_msg);
-  }
-
-  return msg.SerializeAsString();
-}
-
-bool IsValid(const proto::AttributionAggregationKey& key) {
-  return key.has_high_bits() && key.has_low_bits();
-}
-
-absl::optional<attribution_reporting::AggregationKeys>
-DeserializeAggregationKeys(sql::Statement& stmt, int col) {
-  std::string str;
-  if (!stmt.ColumnBlobAsString(col, &str)) {
-    return absl::nullopt;
-  }
-
-  proto::AttributionAggregatableSource msg;
-  if (!msg.ParseFromString(str)) {
-    return absl::nullopt;
-  }
-
-  attribution_reporting::AggregationKeys::Keys::container_type keys;
-  keys.reserve(msg.keys().size());
-
-  for (const auto& [id, key] : msg.keys()) {
-    if (!IsValid(key)) {
-      return absl::nullopt;
-    }
-
-    keys.emplace_back(id, absl::MakeUint128(key.high_bits(), key.low_bits()));
-  }
-
-  return attribution_reporting::AggregationKeys::FromKeys(std::move(keys));
-}
-
-std::string SerializeReportMetadata(
-    const AttributionReport::EventLevelData& data) {
-  proto::AttributionEventLevelMetadata msg;
-  msg.set_trigger_data(data.trigger_data);
-  msg.set_priority(data.priority);
-
-  return msg.SerializeAsString();
-}
-
-[[nodiscard]] bool DeserializeReportMetadata(const std::string& str,
-                                             uint64_t& trigger_data,
-                                             int64_t& priority) {
-  proto::AttributionEventLevelMetadata msg;
-  if (!msg.ParseFromString(str) || !msg.has_trigger_data() ||
-      !msg.has_priority()) {
-    return false;
-  }
-
-  trigger_data = msg.trigger_data();
-  priority = msg.priority();
-  return true;
-}
-
-void SerializeCommonAggregatableData(
-    const AttributionReport::CommonAggregatableData& data,
-    proto::AttributionCommonAggregatableMetadata& msg) {
-  if (base::FeatureList::IsEnabled(
-          aggregation_service::kAggregationServiceMultipleCloudProviders) &&
-      data.aggregation_coordinator_origin.has_value()) {
-    msg.set_coordinator_origin(
-        data.aggregation_coordinator_origin->Serialize());
-  }
-
-  if (const auto& verification_token = data.verification_token;
-      verification_token.has_value()) {
-    msg.set_verification_token(*verification_token);
-  }
-
-  switch (data.source_registration_time_config) {
-    case SourceRegistrationTimeConfig::kInclude:
-      msg.set_source_registration_time_config(
-          proto::AttributionCommonAggregatableMetadata::INCLUDE);
-      break;
-    case SourceRegistrationTimeConfig::kExclude:
-      msg.set_source_registration_time_config(
-          proto::AttributionCommonAggregatableMetadata::EXCLUDE);
-      break;
-  }
-}
-
-[[nodiscard]] bool DeserializeCommonAggregatableData(
-    const proto::AttributionCommonAggregatableMetadata& msg,
-    AttributionReport::CommonAggregatableData& data) {
-  if (!msg.has_source_registration_time_config()) {
-    return false;
-  }
-
-  if (base::FeatureList::IsEnabled(
-          ::aggregation_service::kAggregationServiceMultipleCloudProviders) &&
-      msg.has_coordinator_origin()) {
-    absl::optional<SuitableOrigin> aggregation_coordinator_origin =
-        SuitableOrigin::Deserialize(msg.coordinator_origin());
-    if (!aggregation_coordinator_origin.has_value()) {
-      return false;
-    }
-    data.aggregation_coordinator_origin =
-        std::move(aggregation_coordinator_origin);
-  }
-
-  switch (msg.source_registration_time_config()) {
-    case proto::AttributionCommonAggregatableMetadata::INCLUDE:
-      data.source_registration_time_config =
-          SourceRegistrationTimeConfig::kInclude;
-      break;
-    case proto::AttributionCommonAggregatableMetadata::EXCLUDE:
-      data.source_registration_time_config =
-          SourceRegistrationTimeConfig::kExclude;
-      break;
-    default:
-      return false;
-  }
-
-  if (msg.has_verification_token()) {
-    data.verification_token = msg.verification_token();
-  }
-
-  return true;
-}
-
-std::string SerializeReportMetadata(
-    const AttributionReport::AggregatableAttributionData& data) {
-  proto::AttributionAggregatableMetadata msg;
-
-  SerializeCommonAggregatableData(data.common_data, *msg.mutable_common_data());
-
-  msg.mutable_contributions()->Reserve(data.contributions.size());
-  for (const auto& contribution : data.contributions) {
-    proto::AttributionAggregatableMetadata_Contribution* contribution_msg =
-        msg.add_contributions();
-    contribution_msg->mutable_key()->set_high_bits(
-        absl::Uint128High64(contribution.key()));
-    contribution_msg->mutable_key()->set_low_bits(
-        absl::Uint128Low64(contribution.key()));
-    contribution_msg->set_value(contribution.value());
-  }
-
-  return msg.SerializeAsString();
-}
-
-[[nodiscard]] bool DeserializeReportMetadata(
-    const std::string& str,
-    AttributionReport::AggregatableAttributionData& data) {
-  proto::AttributionAggregatableMetadata msg;
-  if (!msg.ParseFromString(str) || msg.contributions().empty() ||
-      !msg.has_common_data() ||
-      !DeserializeCommonAggregatableData(msg.common_data(), data.common_data)) {
-    return false;
-  }
-
-  data.contributions.reserve(msg.contributions().size());
-  for (const auto& contribution_msg : msg.contributions()) {
-    if (!contribution_msg.has_key() || !contribution_msg.has_value() ||
-        !IsValid(contribution_msg.key()) || contribution_msg.value() == 0 ||
-        contribution_msg.value() >
-            attribution_reporting::kMaxAggregatableValue) {
-      return false;
-    }
-    data.contributions.emplace_back(
-        absl::MakeUint128(contribution_msg.key().high_bits(),
-                          contribution_msg.key().low_bits()),
-        contribution_msg.value());
-  }
-
-  return true;
-}
-
-std::string SerializeReportMetadata(
-    const AttributionReport::NullAggregatableData& data) {
-  proto::AttributionNullAggregatableMetadata msg;
-
-  SerializeCommonAggregatableData(data.common_data, *msg.mutable_common_data());
-
-  msg.set_fake_source_time(
-      data.fake_source_time.ToDeltaSinceWindowsEpoch().InMicroseconds());
-
-  return msg.SerializeAsString();
-}
-
-[[nodiscard]] bool DeserializeReportMetadata(
-    const std::string& str,
-    AttributionReport::NullAggregatableData& data) {
-  proto::AttributionNullAggregatableMetadata msg;
-  if (!msg.ParseFromString(str) || !msg.has_fake_source_time() ||
-      !msg.has_common_data() ||
-      !DeserializeCommonAggregatableData(msg.common_data(), data.common_data)) {
-    return false;
-  }
-
-  data.fake_source_time = base::Time::FromDeltaSinceWindowsEpoch(
-      base::Microseconds(msg.fake_source_time()));
-
-  return true;
 }
 
 absl::optional<StoredSource::ActiveState> GetSourceActiveState(
@@ -571,17 +307,8 @@ AttributionStorageSql::ReadSourceFromStatement(sql::Statement& statement) {
   int max_event_level_reports =
       read_only_source_data_msg->max_event_level_reports();
 
-  std::vector<base::TimeDelta> end_times;
-  for (int64_t time :
-       read_only_source_data_msg->event_level_report_window_end_times()) {
-    end_times.push_back(base::Microseconds(time));
-  }
-
   absl::optional<EventReportWindows> event_report_windows =
-      EventReportWindows::Create(
-          base::Microseconds(read_only_source_data_msg
-                                 ->event_level_report_window_start_time()),
-          std::move(end_times));
+      DeserializeEventReportWindows(*read_only_source_data_msg);
   if (!event_report_windows.has_value()) {
     return absl::nullopt;
   }
--- content/browser/attribution_reporting/sql_utils.h
+++ content/browser/attribution_reporting/sql_utils.h
@@ -5,15 +5,19 @@
 #ifndef CONTENT_BROWSER_ATTRIBUTION_REPORTING_SQL_UTILS_H_
 #define CONTENT_BROWSER_ATTRIBUTION_REPORTING_SQL_UTILS_H_
 
+#include <stdint.h>
+
 #include <string>
 
 #include "components/attribution_reporting/source_type.mojom-forward.h"
-#include "content/browser/attribution_reporting/attribution_reporting.pb.h"
+#include "content/browser/attribution_reporting/attribution_report.h"
 #include "content/common/content_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace attribution_reporting {
+class AggregationKeys;
 class EventReportWindows;
+class FilterData;
 class TriggerConfig;
 }  // namespace attribution_reporting
 
@@ -26,6 +30,10 @@ class Origin;
 }  // namespace url
 
 namespace content {
+
+namespace proto {
+class AttributionReadOnlySourceData;
+}  // namespace proto
 
 url::Origin DeserializeOrigin(const std::string& origin);
 
@@ -43,7 +51,42 @@ CONTENT_EXPORT std::string SerializeReadOnlySourceData(
     const bool* debug_cookie_set);
 
 CONTENT_EXPORT absl::optional<proto::AttributionReadOnlySourceData>
-DeserializeReadOnlySourceDataAsProto(sql::Statement& stmt, int col);
+DeserializeReadOnlySourceDataAsProto(sql::Statement&, int col);
+
+std::string SerializeFilterData(const attribution_reporting::FilterData&);
+
+absl::optional<attribution_reporting::FilterData> DeserializeFilterData(
+    sql::Statement&,
+    int col);
+
+absl::optional<attribution_reporting::EventReportWindows>
+DeserializeEventReportWindows(const proto::AttributionReadOnlySourceData&);
+
+std::string SerializeAggregationKeys(
+    const attribution_reporting::AggregationKeys&);
+
+absl::optional<attribution_reporting::AggregationKeys>
+DeserializeAggregationKeys(sql::Statement&, int col);
+
+std::string SerializeReportMetadata(const AttributionReport::EventLevelData&);
+
+std::string SerializeReportMetadata(
+    const AttributionReport::AggregatableAttributionData&);
+
+std::string SerializeReportMetadata(
+    const AttributionReport::NullAggregatableData&);
+
+[[nodiscard]] bool DeserializeReportMetadata(const std::string&,
+                                             uint64_t& trigger_data,
+                                             int64_t& priority);
+
+[[nodiscard]] bool DeserializeReportMetadata(
+    const std::string&,
+    AttributionReport::AggregatableAttributionData&);
+
+[[nodiscard]] bool DeserializeReportMetadata(
+    const std::string&,
+    AttributionReport::NullAggregatableData&);
 
 }  // namespace content
 
