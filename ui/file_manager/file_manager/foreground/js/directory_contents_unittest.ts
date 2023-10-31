b1086f84b5c297e4ed3f4db24fa725cd066648b1
Bo Majewski <majewski@chromium.org>
2023-10-31 02:09:12 +0000

    [Files,TS]: Converts directory_contents_unitest to TypeScript.
    Bug: b:289003444
    Change-Id: I50c37c204378dbdf728d6929468cfe6b9c0f9c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4988710
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217366}

--- ui/file_manager/file_manager/foreground/js/directory_contents_unittest.ts
+++ ui/file_manager/file_manager/foreground/js/directory_contents_unittest.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,26 +6,24 @@ import {assertEquals, assertFalse} from 'chrome://webui-test/chromeos/chai_asser
 
 import {installMockChrome} from '../../common/js/mock_chrome.js';
 import {VolumeManagerCommon} from '../../common/js/volume_manager_types.js';
-import {EntryLocation} from '../../externs/entry_location.js';
 import {VolumeManager} from '../../externs/volume_manager.js';
+import type {VolumeInfo} from '../../externs/volume_info.js';
 
 import {FileFilter, RecentContentScanner} from './directory_contents.js';
 
 /**
  * Mock chrome APIs.
- * @type {!Object}
  */
 const mockChrome = {
   fileManagerPrivate: {
     getRecentFiles:
-        // @ts-ignore: error TS7006: Parameter 'callback' implicitly has an
-        // 'any' type.
-        (sourceRestriction, query, fileType, invalidateCache, callback) => {
-          /** @type {!Array<!FileEntry>} */
-          const entries = [
-            /** @type {!FileEntry} */ ({name: '1.txt'}),
-            /** @type {!FileEntry} */ ({name: '2.txt'}),
-            /** @type {!FileEntry} */ ({name: '3.png'}),
+        (_1: chrome.fileManagerPrivate.SourceRestriction,
+         query: string, _2: chrome.fileManagerPrivate.FileCategory,
+         _3: boolean, callback: (entries: FileEntry[]) => void) => {
+          const entries: FileEntry[] = [
+            {name: '1.txt'} as FileEntry,
+            {name: '2.txt'} as FileEntry,
+            {name: '3.png'} as FileEntry,
           ];
           callback(entries.filter(e => e.name.indexOf(query) !== -1));
         },
@@ -46,28 +44,24 @@ export function setUp() {
 export function testHiddenFiles() {
   let volumeManagerRootType = VolumeManagerCommon.RootType.DOWNLOADS;
   // Create a fake volume manager that provides entry location info.
-  const volumeManager = /** @type {!VolumeManager} */ ({
-    // @ts-ignore: error TS6133: 'entry' is declared but its value is never
-    // read.
-    getLocationInfo: (entry) => {
-      return /** @type {!EntryLocation} */ ({
+  const volumeManager = {
+    getLocationInfo: (_: FileEntry) => {
+      return {
         rootType: volumeManagerRootType,
-      });
+      };
     },
-  });
+  } as VolumeManager;
 
-  // @ts-ignore: error TS7006: Parameter 'fullPath' implicitly has an 'any'
-  // type.
-  const entry = (fullPath) =>
-      // @ts-ignore: error TS2352: Conversion of type '{ name: any; fullPath:
-      // any; filesystem: string; }' to type 'FileSystemEntry' may be a mistake
-      // because neither type sufficiently overlaps with the other. If this was
-      // intentional, convert the expression to 'unknown' first.
-      /** @type {!Entry} */ (
-          {name: fullPath.split('/').pop(), fullPath, filesystem: 'test'});
+  const entry = (fullPath: string): FileEntry => {
+    return {
+      name: fullPath.split('/').pop()!,
+      fullPath,
+      filesystem: {name: 'test'} as FileSystem,
+    } as FileEntry;
+  };
 
   // Create a set of entries in root dir, and in /PvmDefault dir.
-  const entries = [];
+  const entries: FileEntry[] = [];
   for (const e of ['.test', 'test', '$RECYCLE.BIN', '$okForNow', '~okForNow']) {
     entries.push(entry('/' + e));
     entries.push(entry('/PvmDefault/' + e));
@@ -80,12 +74,9 @@ export function testHiddenFiles() {
   assertFalse(filter.isHiddenFilesVisible());
   let hidden = entries.filter(entry => !filter.filter(entry));
   assertEquals(3, hidden.length);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('/.test', hidden[0].fullPath);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('/PvmDefault/.test', hidden[1].fullPath);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('/PvmDefault/$RECYCLE.BIN', hidden[2].fullPath);
+  assertEquals('/.test', hidden[0]!.fullPath);
+  assertEquals('/PvmDefault/.test', hidden[1]!.fullPath);
+  assertEquals('/PvmDefault/$RECYCLE.BIN', hidden[2]!.fullPath);
 
   // No files hidden when we show hidden files.
   filter.setHiddenFilesVisible(true);
@@ -97,10 +88,8 @@ export function testHiddenFiles() {
   filter.setHiddenFilesVisible(false);
   hidden = entries.filter(entry => !filter.filter(entry));
   assertEquals(2, hidden.length);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('/.test', hidden[0].fullPath);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('/PvmDefault/.test', hidden[1].fullPath);
+  assertEquals('/.test', hidden[0]!.fullPath);
+  assertEquals('/PvmDefault/.test', hidden[1]!.fullPath);
 
   // Still no files hidden when we show hidden files.
   filter.setHiddenFilesVisible(true);
@@ -114,28 +103,24 @@ export function testHiddenFiles() {
  */
 export async function testRecentScannerFilter() {
   // Mock volume manager
-  const volumeManager = /** @type {!VolumeManager} */ ({
+  const volumeManager = {
     getVolumeInfo: (entry) => {
       if (entry.name === '1.txt') {
         return null;
       }
-      return /** @type {!import("../../externs/volume_info.js").VolumeInfo} */ (
-          {
-            volumeId: 'fakeId',
-          });
+      return {
+        volumeId: 'fakeId',
+      } as VolumeInfo;
     },
-  });
+  } as VolumeManager;
   const scanner = new RecentContentScanner('txt', volumeManager);
-  /** @type {!Array<!FileEntry>} */
-  let entriesCallbackResult = [];
-  // @ts-ignore: error TS7006: Parameter 'entries' implicitly has an 'any' type.
-  function entriesCallback(entries) {
+  let entriesCallbackResult: FileEntry[] = [];
+  function entriesCallback(entries: FileEntry[]) {
     entriesCallbackResult = entries;
   }
   function otherCallback() {}
   await scanner.scan(entriesCallback, otherCallback, otherCallback);
   // 1.txt: volume is not allowed; 3.png: query is not matched.
   assertEquals(1, entriesCallbackResult.length);
-  // @ts-ignore: error TS2532: Object is possibly 'undefined'.
-  assertEquals('2.txt', entriesCallbackResult[0].name);
+  assertEquals('2.txt', entriesCallbackResult[0]!.name);
 }
