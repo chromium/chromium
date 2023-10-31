f66ce96fdd24ccb0788430f8c18478b648c36dbe
Dan McArdle <dmcardle@chromium.org>
2023-10-31 16:25:34 +0000

    Update query count expectations in DohHttpsProtocolUpgradeBrowserTest
    These browser tests have been failing intermittently due to unexpected
    numbers of DoH queries served (see the linked bug).
    
    I had trouble reproducing this behavior locally until I added a
    four-second sleep to TestDohServer::HandleRequest(). Then, I caught
    TestDohServer handling several queries for www.gstatic.com while I ran
    the browser tests. I think it was tricky to reproduce because my
    workstation is faster than the trybots; it probably started and finished
    each test before the service triggering the www.gstatic.com lookups had
    a chance to start up.
    
    This CL adds TestDohServer::QueriesServedForSubdomains() and uses it to
    update the flaky browser tests' expectations.
    
    Fixed: 1493570
    Change-Id: I3325951481fa0e704fd97a0e139b1db75023848f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990303
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1217646}

--- net/test/test_doh_server.cc
+++ net/test/test_doh_server.cc
@@ -7,15 +7,17 @@
 #include <string.h>
 
 #include <memory>
+#include <string_view>
 
 #include "base/base64url.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece.h"
 #include "base/synchronization/lock.h"
+#include "base/time/time.h"
 #include "net/base/io_buffer.h"
 #include "net/base/url_util.h"
 #include "net/dns/dns_names_util.h"
@@ -37,7 +39,7 @@ const char kPath[] = "/dns-query";
 
 std::unique_ptr<test_server::HttpResponse> MakeHttpErrorResponse(
     HttpStatusCode status,
-    base::StringPiece error) {
+    std::string_view error) {
   auto response = std::make_unique<test_server::BasicHttpResponse>();
   response->set_code(status);
   response->set_content(std::string(error));
@@ -69,7 +71,7 @@ TestDohServer::TestDohServer() {
 
 TestDohServer::~TestDohServer() = default;
 
-void TestDohServer::SetHostname(base::StringPiece name) {
+void TestDohServer::SetHostname(std::string_view name) {
   DCHECK(!server_.Started());
   hostname_ = std::string(name);
 }
@@ -79,7 +81,7 @@ void TestDohServer::SetFailRequests(bool fail_requests) {
   fail_requests_ = fail_requests;
 }
 
-void TestDohServer::AddAddressRecord(base::StringPiece name,
+void TestDohServer::AddAddressRecord(std::string_view name,
                                      const IPAddress& address,
                                      base::TimeDelta ttl) {
   AddRecord(BuildTestAddressRecord(std::string(name), address, ttl));
@@ -136,6 +138,15 @@ int TestDohServer::QueriesServed() {
   return queries_served_;
 }
 
+int TestDohServer::QueriesServedForSubdomains(std::string_view domain) {
+  CHECK(net::dns_names_util::IsValidDnsName(domain));
+  auto is_subdomain = [&domain](std::string_view candidate) {
+    return net::IsSubdomainOf(candidate, domain);
+  };
+  base::AutoLock lock(lock_);
+  return base::ranges::count_if(query_qnames_, is_subdomain);
+}
+
 std::unique_ptr<test_server::HttpResponse> TestDohServer::HandleRequest(
     const test_server::HttpRequest& request) {
   GURL request_url = request.GetURL();
@@ -189,6 +200,7 @@ std::unique_ptr<test_server::HttpResponse> TestDohServer::HandleRequest(
                          dns_protocol::kRcodeFORMERR);
     return MakeHttpResponseFromDns(response);
   }
+  query_qnames_.push_back(*name);
 
   auto range = records_.equal_range(std::make_pair(*name, dns_query.qtype()));
   std::vector<DnsResourceRecord> answers;
