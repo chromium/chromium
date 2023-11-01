02eaf1b43c8f632a1106ab832f1e6ad23a678af6
Sangbaek Park <sangbaekpark@chromium.org>
2023-11-01 19:32:27 +0000

    Support MediaFoundation encrypted playback for x86_64 only
    MediaFoundation encrypted playback is supported only on x86_64 (AMD64
    Windows) while Chrome bitness matches OS bitness. We won't let a x86_64
    Chrome build be running on ARM64 with the WOW emulation.
    
    Bug: b/302732731
    Change-Id: Id70d8c6323b476349f48d78e26ec25960960912c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4990842
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218381}

--- media/base/win/mf_feature_checks.cc
+++ media/base/win/mf_feature_checks.cc
@@ -5,6 +5,7 @@
 #include "media/base/win/mf_feature_checks.h"
 
 #include "base/win/windows_version.h"
+#include "build/build_config.h"
 #include "media/base/media_switches.h"
 
 namespace media {
@@ -20,10 +21,22 @@ bool SupportMediaFoundationClearPlayback() {
 }
 
 bool SupportMediaFoundationEncryptedPlayback() {
+#if defined(ARCH_CPU_X86_64)
+  // MediaFoundation encrypted playback is supported only on x86_64 (AMD64
+  // Windows) while Chrome bitness matches OS bitness. We won't let a x86_64
+  // Chrome build be running on ARM64 with the WOW emulation.
+  auto is_running_emulated_on_arm64 =
+      base::win::OSInfo::IsRunningEmulatedOnArm64();
+  DVLOG(3) << __func__
+           << ": is_running_emulated_on_arm64=" << is_running_emulated_on_arm64;
+
   // TODO(xhwang): Also check whether software secure decryption is enabled and
   // supported by MediaFoundationCdm in the future.
   return base::win::GetVersion() >= base::win::Version::WIN10_20H1 &&
-         IsHardwareSecureDecryptionEnabled();
+         !is_running_emulated_on_arm64 && IsHardwareSecureDecryptionEnabled();
+#else
+  return false;
+#endif  // defined(ARCH_CPU_X86_64)
 }
 
 }  // namespace media
