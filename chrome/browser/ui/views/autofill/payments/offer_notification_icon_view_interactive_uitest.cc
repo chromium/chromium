c8f01d42d954514ad88272c0b73ef6471fc5a3e6
Marc Treib <treib@chromium.org>
2023-11-02 16:22:32 +0000

    Disable IconCollapseAfterBubbleWidgetIsClosed on Linux MSan
    OfferNotificationIconViewInteractiveTest.IconCollapseAfterBubbleWidgetIsClosed
    is flaky on Linux MSan.
    
    Bug: 1498588
    Change-Id: I0a0348b32190af39d7d848c06435facbf75c11a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001505
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1218873}

--- chrome/browser/ui/views/autofill/payments/offer_notification_icon_view_interactive_uitest.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_icon_view_interactive_uitest.cc
@@ -170,8 +170,16 @@ IN_PROC_BROWSER_TEST_F(OfferNotificationIconViewInteractiveTest,
                         &OfferNotificationIconView::ShouldShowLabel, true));
 }
 
+// TODO(crbug.com/1498588): Flaky on Linux MSAN.
+#if BUILDFLAG(IS_LINUX) && defined(MEMORY_SANITIZER)
+#define MAYBE_IconCollapseAfterBubbleWidgetIsClosed \
+  DISABLED_IconCollapseAfterBubbleWidgetIsClosed
+#else
+#define MAYBE_IconCollapseAfterBubbleWidgetIsClosed \
+  IconCollapseAfterBubbleWidgetIsClosed
+#endif
 IN_PROC_BROWSER_TEST_F(OfferNotificationIconViewInteractiveTest,
-                       IconCollapseAfterBubbleWidgetIsClosed) {
+                       MAYBE_IconCollapseAfterBubbleWidgetIsClosed) {
   SetUpShoppingServiceToReturnDiscounts();
 
   RunTestSequence(
