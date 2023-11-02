fa680810c071d55d958a68e52d4b23757f903fca
Fritz Koenig <frkoenig@chromium.org>
2023-11-02 05:28:42 +0000

    media/gpu/v4l2: Increase max number of /dev/video*
    Some devices produce a large number of /dev/video* nodes.
    
    Bug: b:307870877
    Change-Id: Ia4138f350a113c88383bce70488664c0a38a910e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4996517
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>
    Commit-Queue: Chen-Yu Tsai <wenst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218638}

--- media/gpu/v4l2/test/v4l2_ioctl_shim.cc
+++ media/gpu/v4l2/test/v4l2_ioctl_shim.cc
@@ -35,7 +35,7 @@ constexpr int kMaxRetryCount = 1 << 24;
 #define V4L2_REQUEST_CODE_AND_STRING(x) \
   { x, #x }
 
-constexpr uint32_t kMaximumDeviceNumber = 40;
+constexpr uint32_t kMaximumDeviceNumber = 150;
 
 constexpr base::StringPiece kDecoderDevicePrefix = "/dev/video";
 constexpr base::StringPiece kMediaDevicePrefix = "/dev/media";
