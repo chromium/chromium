b95a064554a1cbaa2c01a339a480436d3dd80fc2
Oleg Davydov <burunduk@chromium.org>
2023-11-02 14:57:33 +0000

    [storage] Update comments for CopyOrMoveHookDelegate
    Just a cosmetic update: move comments to the corresponding methods,
    minor changes here and there.
    
    Bug: b:304956302
    Change-Id: I7340083403b9fbaf7cf17d4023cf0065832ffb7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4992365
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218800}

--- storage/browser/file_system/copy_or_move_hook_delegate.h
+++ storage/browser/file_system/copy_or_move_hook_delegate.h
@@ -15,12 +15,14 @@ namespace storage {
 class FileSystemURL;
 
 // A delegate to handle different hooks of the CopyOrMove operation.
-// Its OnBegin* functions take a callback that is called when the function
-// finishes, potentially notifying of any errors during the execution of
-// the function.
+// Its OnBegin* functions take a callback that should be called when the
+// function finishes, potentially notifying of any errors during the execution
+// of the function. Similarly, OnError takes a callback that is called when a
+// error happens and might be used to skip the error and continue the operation.
+//
 // The other functions do not take a callback and can asynchronously notify of
-// any progress or errors.
-// Used for progress updates, etc. in FileSystemOperation::Copy() and Move().
+// any progress. They are used for progress updates, etc. in
+// FileSystemOperation::Copy() and Move().
 //
 // Note that Move() has both a same-filesystem (1) and a cross-filesystem (2)
 // implementation.
@@ -32,58 +34,16 @@ class FileSystemURL;
 // 2) Degrades into copy + delete: each entry is copied and deleted
 // recursively.
 //
-// OnBeginProcessFile, resp. OnBeginProcessDirectory is called at the start of
-// each copy or move operation for a file, resp. a directory. The `source_url`
-// and the `destination_url` are the URLs of the source and the destination
-// entries. Note that for the root directory, OnBeginProcessFile is called
-// instead of OnBeginProcessDirectory. This resembles the call order of the
-// RecursiveOperationDelegate.
-//
-// OnProgress is called periodically during file transfer (not called for
-// same-filesystem move and directory copy/move).
-// The `source_url` and the `destination_url` are the URLs of the source and
-// the destination entries. `size` is the number of cumulative copied bytes
-// for the currently copied file. Both at beginning and ending of file
-// transfer, PROGRESS event should be called. At beginning, `size` should be
-// 0. At ending, `size` should be the size of the file.
-//
-// OnError is called for any occurring error.
-// The `source_url` and the `destination_url` are the URLs of the source and
-// the destination entries. `error` is the base::File::Error that was noticed.
-// `callback` might be used to skip/suppress the error (when invoked with
-// kSkip), otherwise should be just called with kDefault.
-// Note that:
-//  * OnError is also called if an OnBeginProcessFile or
-//  OnBeginProcessDirectory function passes an error to their respective
-//  callbacks.
-//  * The operation is paused until the `callback` gets called.
-//  * Using kSkip will continue the operation even if it was run with
-//  ERROR_BEHAVIOR_ABORT, which makes possible to make abort/skip
-//  decision per every error encountered.
-//
-// OnEndCopy is called for each destination entry that has been successfully
-// copied (for both file and directory). The `source_url` and the
-// `destination_url` are the URLs of the source and the destination entries.
-//
-// OnEndMove is called for each entry that has been successfully moved (for
-// both file and directory), in the case of a same-filesystem move. The
-// `source_url` and the `destination_url` are the URLs of the source and the
-// destination entries.
-//
-// OnEndRemoveSource, applies in the Move() case only, and is called for
-// each source entry that has been successfully removed from its source location
-// (for both file and directory). The `source_url` is the URL of the source
-// entry.
-//
-// When moving files, the expected events are as follows.
+// When transferring files, the expected events are as follows:
 // Copy: OnBeginProcessFile -> OnProgress -> ... -> OnProgress ->
-// OnEndCopy. Move (same-filesystem): OnBeginProcessFile -> OnEndMove.
+//   OnEndCopy.
+// Move (same-filesystem): OnBeginProcessFile -> OnEndMove.
 // Move (cross-filesystem): OnBeginProcessFile -> OnProgress -> ... ->
-// OnProgress -> OnEndCopy -> OnEndRemoveSource.
+//   OnProgress -> OnEndCopy -> OnEndRemoveSource.
 //
 // Here is an example callback sequence of for a copy or a cross-filesystem
 // move. Suppose there are a/b/c.txt (100 bytes) and a/b/d.txt (200 bytes),
-// and trying to transfer a to x recursively, then the progress update
+// and trying to transfer a/ to x/ recursively, then the progress update
 // sequence will be (Note that for the root directory, OnBeginProcessFile is
 // called instead of OnBeginProcessDirectory):
 //
@@ -139,26 +99,59 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) CopyOrMoveHookDelegate
 
   virtual ~CopyOrMoveHookDelegate();
 
+  // OnBeginProcessFile, resp. OnBeginProcessDirectory is called at the start of
+  // each copy or move operation for a file, resp. a directory. Note that for
+  // the root directory, OnBeginProcessFile is called instead of
+  // OnBeginProcessDirectory. This resembles the call order of the
+  // RecursiveOperationDelegate.
   virtual void OnBeginProcessFile(const FileSystemURL& source_url,
                                   const FileSystemURL& destination_url,
                                   StatusCallback callback);
-
   virtual void OnBeginProcessDirectory(const FileSystemURL& source_url,
                                        const FileSystemURL& destination_url,
                                        StatusCallback callback);
 
+  // OnProgress is called periodically during file transfer (not called for
+  // same-filesystem move and directory copy/move).
+  // `size` is the number of cumulative copied bytes for the currently copied
+  // file. Both at beginning and ending of file transfer, PROGRESS event should
+  // be called. At beginning, `size` should be 0. At ending, `size` should be
+  // the size of the file.
   virtual void OnProgress(const FileSystemURL& source_url,
                           const FileSystemURL& destination_url,
                           int64_t size);
+
+  // OnError is called for any occurring error.
+  // `error` is the base::File::Error that was noticed.
+  // `callback` might be used to skip/suppress the error (when invoked with
+  // kSkip), otherwise should be just called with kDefault.
+  // Note that:
+  //  * OnError is also called if an OnBeginProcessFile or
+  //  OnBeginProcessDirectory function passes an error to their respective
+  //  callbacks.
+  //  * The operation is paused until the `callback` gets called.
+  //  * Using kSkip will continue the operation even if it was run with
+  //  ERROR_BEHAVIOR_ABORT, which makes possible to make abort/skip
+  //  decision per every error encountered.
   virtual void OnError(const FileSystemURL& source_url,
                        const FileSystemURL& destination_url,
                        base::File::Error error,
                        ErrorCallback callback);
 
+  // OnEndCopy is called for each destination entry that has been successfully
+  // copied (for both file and directory). As mentioned before, it is also
+  // called for a cross-filesystem move.
   virtual void OnEndCopy(const FileSystemURL& source_url,
                          const FileSystemURL& destination_url);
+
+  // OnEndMove is called for each entry that has been successfully moved (for
+  // both file and directory), in the case of a same-filesystem move.
   virtual void OnEndMove(const FileSystemURL& source_url,
                          const FileSystemURL& destination_url);
+
+  // OnEndRemoveSource, applies in the Move() case only, and is called for
+  // each source entry that has been successfully removed from its source
+  // location (for both file and directory).
   virtual void OnEndRemoveSource(const FileSystemURL& source_url);
 
  protected:
