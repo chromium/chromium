b78a0bf0bfd71c700136caf01d7b14180da8df70
Claudio DeSouza <cdesouza@chromium.org>
2023-11-02 19:55:11 +0000

    [Extensions] Enum modernisation for safe_browsing_private.idl
    This CL updates the codegen attribute for the enum types generated by
    safe_browsing_private.idl. This attribute generates enum class types
    with kCamelCase key names.
    
    Note: This CL is purely mechanical and should have no effect on existing
    behaviour.
    
    Bug: 1421546
    Change-Id: I2aafcbe8c0a9a2dea730efb2fecc421af8c7ab4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994401
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219053}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_util_unittest.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_util_unittest.cc
@@ -51,15 +51,14 @@ void ValidateReferrerChain(
     const safe_browsing_private::ReferrerChainEntry& referrer) {
   ASSERT_FALSE(referrer.url.empty());
   EXPECT_EQ(*referrer.main_frame_url, kMainFrameUrl);
-  EXPECT_EQ(referrer.url_type,
-            safe_browsing_private::URLType::URL_TYPE_LANDING_PAGE);
+  EXPECT_EQ(referrer.url_type, safe_browsing_private::URLType::kLandingPage);
   EXPECT_EQ(referrer.ip_addresses->at(0), kIpAddress);
   EXPECT_EQ(*referrer.referrer_url, kReferrerUrl);
   EXPECT_EQ(*referrer.is_retargeting, kIsRetargeting);
   EXPECT_EQ(*referrer.navigation_time_ms, kNavTime);
   EXPECT_EQ(*referrer.server_redirect_chain->at(0).url, kServerRedirectUrl);
   EXPECT_EQ(referrer.navigation_initiation,
-            safe_browsing_private::NAVIGATION_INITIATION_BROWSER_INITIATED);
+            safe_browsing_private::NavigationInitiation::kBrowserInitiated);
   EXPECT_EQ(*referrer.maybe_launched_by_external_app, kMaybeLaunched);
   EXPECT_EQ(*referrer.is_subframe_url_removed, kIsSubframeUrlRemoved);
   EXPECT_EQ(*referrer.is_subframe_referrer_url_removed,
