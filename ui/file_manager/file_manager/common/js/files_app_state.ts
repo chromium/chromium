84bb400ea9830c9e0ea527b026638885c94671dc
Bo Majewski <majewski@chromium.org>
2023-11-02 08:28:10 +0000

    [Files,F2]: Convert files_app_state and to TS
    Bug: b:228112698
    Change-Id: I797b6abd1f90f02326a64831dfb60e20fdaff11e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4882058
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218686}

---
+++ ui/file_manager/file_manager/common/js/files_app_state.ts
@@ -0,0 +1,87 @@
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {DialogType} from './dialog_type.js';
+import {AllowedPaths} from './volume_manager_types.js';
+
+/**
+ * FilesAppState is used in 2 ways:
+ *
+ * 1. Persist in the localStorage the some state, like current directory,
+ * sorting column options, etc.
+ *
+ * 2. To open a new window:
+ * 2.1, Requests to open a new window set part of these options to configure the
+ * how the new window should behave.
+ * 2.2  When the Files app extension is restarted, the background page retrieves
+ * the last state from localStorage for each opened window and re-spawn the
+ * windows with their state.
+ */
+export class FilesAppState {
+  /**
+   * The desired target directory when opening a new window.
+   */
+  currentDirectoryURL: string|null|undefined;
+
+  /**
+   * The URL for a file or directory to be selected once a new window is
+   * spawned.
+   */
+  selectionURL: string|undefined;
+
+  /**
+   * For SaveAs dialog it prefills the <input> for the file name with this
+   * value.
+   * For FilePicker it pre-selects the file in the file list.
+   */
+  targetName: string|undefined;
+
+  /**
+   * Search term to initialize the Files app directly in a search results.
+   */
+  searchQuery: string|undefined;
+
+  /**
+   * The type of the window being opened, when it's undefined it defaults to
+   * the normal Files app window (non-dialog version).
+   */
+  type: DialogType|undefined;
+
+  /**
+   * List of file extensions (.txt, .zip, etc) that will be used by
+   * AndroidAppListModel, when displaying Files app as FilePicker for ARC++.
+   * Files app displays Android apps that can handle such extensions in the
+   * DirectoryTree.
+   * TODO(lucmult): Add type for the Object below:
+   */
+  typeList: Object[]|undefined;
+
+  /**
+   * For FilePicker indicates that the "All files" should be displayed in the
+   * file type dropdown in the footer.
+   */
+  includeAllFiles: boolean|undefined;
+
+  /**
+   * Defines what volumes are available in the Files app, when NATIVE_PATH is
+   * used, any virtual volume (FSPs) is hidden.
+   *
+   * Defaults to `ANY_PATH_OR_URL` when undefined.
+   */
+  allowedPaths: AllowedPaths|undefined;
+
+  /**
+   * If the Android apps should be shown in the DirectoryTree for FilePicker.
+   */
+  showAndroidPickerApps: boolean|undefined;
+
+  /**
+   * Array of Files app mode dependent volume filter names. Defaults to an
+   * empty Array when undefined, and is the normal case (no filters).
+   *
+   * See filtered_volume_manager.js for details about the available volume
+   * filter names and their volume filter effects.
+   */
+  volumeFilter: string[]|undefined;
+}
