aeff0b7e9f9431cc6f2915380b08c96dc54dc2a7
Tushar Singh <tusharsinghnx@gmail.com>
2023-11-02 19:44:46 +0000

    Improve caption button appearance on Windows 11
    On Windows 11's Restore button, the top and right edges of the top
    rounded square should be drawn with half-rounded linecaps, rather than
    being flat at the ends.
    
    Also, the stroke width applied to the caption buttons is too thin when
    the current fractional scale is close to the next higher integer
    scaling, E.g. 175% and 275%, but not 150% or 125%.
    
    Bug: 1411070, 1436835
    Change-Id: Iebc91bc3594cb8b4f16fa40ba8d397554abb0c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4428171
    Auto-Submit: Tushar Singh <tusharsinghnx@gmail.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219050}

--- chrome/browser/ui/views/frame/windows_icon_painter.cc
+++ chrome/browser/ui/views/frame/windows_icon_painter.cc
@@ -8,46 +8,26 @@
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/geometry/rrect_f.h"
 #include "ui/gfx/geometry/skia_conversions.h"
 #include "ui/gfx/scoped_canvas.h"
 
 namespace {
 
-// Canvas::DrawRect's stroke can bleed out of |rect|'s bounds, so this draws a
-// rectangle inset such that the result is constrained to |rect|'s size.
+// The rounded rect corner radius for MaximizeIcon and RestoreIcon in
+// Windows 11.
+constexpr float kWin11RoundedCornerRadius = 1.5f;
+
 void DrawRect(gfx::Canvas* canvas,
               const gfx::Rect& rect,
               const cc::PaintFlags& flags) {
   gfx::RectF rect_f(rect);
+
+  // The rect is used as a bounding box, and the stroke is kept within.
   float stroke_half_width = flags.getStrokeWidth() / 2;
   rect_f.Inset(stroke_half_width);
+
   canvas->DrawRect(rect_f, flags);
-}
-
-void DrawRoundRect(gfx::Canvas* canvas,
-                   const gfx::Rect& rect,
-                   float radius,
-                   const cc::PaintFlags& flags) {
-  gfx::RectF rect_f(rect);
-  float stroke_half_width = flags.getStrokeWidth() / 2;
-  rect_f.Inset(stroke_half_width);
-  canvas->DrawRoundRect(rect_f, radius, flags);
-}
-
-// Draws a path containing the top and right edges of a rounded rectangle.
-void DrawRoundRectEdges(gfx::Canvas* canvas,
-                        const gfx::Rect& rect,
-                        float radius,
-                        const cc::PaintFlags& flags) {
-  gfx::RectF symbol_rect_f(rect);
-  float stroke_half_width = flags.getStrokeWidth() / 2;
-  symbol_rect_f.Inset(stroke_half_width);
-  SkPath path;
-  path.moveTo(symbol_rect_f.x(), symbol_rect_f.y());
-  path.arcTo(symbol_rect_f.right(), symbol_rect_f.y(), symbol_rect_f.right(),
-             symbol_rect_f.y() + radius, radius);
-  path.lineTo(symbol_rect_f.right(), symbol_rect_f.bottom());
-  canvas->DrawPath(path, flags);
 }
 
 }  // namespace
@@ -121,25 +101,55 @@ Windows11IconPainter::~Windows11IconPainter() = default;
 void Windows11IconPainter::PaintMaximizeIcon(gfx::Canvas* canvas,
                                              gfx::Rect& symbol_rect,
                                              cc::PaintFlags& flags) {
+  gfx::RectF rect_f(symbol_rect);
   flags.setAntiAlias(true);
-  const float corner_radius = canvas->image_scale();
-  DrawRoundRect(canvas, symbol_rect, corner_radius, flags);
+  const float corner_radius =
+      base::ClampFloor(kWin11RoundedCornerRadius * canvas->image_scale());
+
+  // The symbol rect is used as a bounding box, and the stroke is kept within.
+  float stroke_half_width = flags.getStrokeWidth() / 2;
+  rect_f.Inset(stroke_half_width);
+
+  canvas->DrawRoundRect(rect_f, corner_radius, flags);
 }
 
 void Windows11IconPainter::PaintRestoreIcon(gfx::Canvas* canvas,
                                             gfx::Rect& symbol_rect,
                                             cc::PaintFlags& flags) {
-  const int separation = base::ClampFloor(2 * canvas->image_scale());
-  symbol_rect.Inset(gfx::Insets::TLBR(separation, 0, 0, separation));
-
+  gfx::RectF rect_f(symbol_rect);
+  const float separation = 2.0f * canvas->image_scale();
+  const int round_rect_radius =
+      base::ClampFloor(kWin11RoundedCornerRadius * canvas->image_scale());
+  const int top_rect_upper_right_radius = 2 * round_rect_radius;
   flags.setAntiAlias(true);
-  // Bottom left ("in front") rounded square.
-  const float bottom_rect_radius = canvas->image_scale();
-  DrawRoundRect(canvas, symbol_rect, bottom_rect_radius, flags);
 
-  // Top right ("behind") top+right edges of rounded square (2.5x).
-  symbol_rect.Offset(separation, -separation);
+  // The symbol rect is used as a bounding box, and the stroke is kept within.
+  const float stroke_half_width = flags.getStrokeWidth() / 2;
+  rect_f.Inset(stroke_half_width);
 
-  const float top_rect_radius = 2.5f * canvas->image_scale();
-  DrawRoundRectEdges(canvas, symbol_rect, top_rect_radius, flags);
+  // Shrink the rect to make space for the top rect.
+  rect_f.Inset(gfx::InsetsF::TLBR(separation, 0, 0, separation));
+
+  gfx::RRectF rrect(rect_f, round_rect_radius);
+
+  // Bottom ("in front") rounded square.
+  canvas->sk_canvas()->drawRRect(SkRRect(rrect), flags);
+
+  // The top rounded square is clipped to only draw the top and right edges,
+  // and give corners a flat base. The clip rect sits right below the bottom
+  // half of the stroke.
+  gfx::RRectF clip_rrect(rrect);
+  const float clip_rect_growth = separation - stroke_half_width;
+  // The upper-right corner radius doesn't need to be updated because |Outset|
+  // does that for us.
+  clip_rrect.Outset(clip_rect_growth);
+  canvas->sk_canvas()->clipRRect(SkRRect(clip_rrect), SkClipOp::kDifference,
+                                 true);
+
+  // Top ("behind") rounded square.
+  rrect.Offset(separation, -separation);
+  rrect.SetCornerRadii(gfx::RRectF::Corner::kUpperRight,
+                       top_rect_upper_right_radius,
+                       top_rect_upper_right_radius);
+  canvas->sk_canvas()->drawRRect(SkRRect(rrect), flags);
 }
