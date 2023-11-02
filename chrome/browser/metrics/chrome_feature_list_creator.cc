015aead3780f69576242ccb0a271ea21c7a0b3f3
Daniel Rubery <drubery@chromium.org>
2023-11-02 02:49:01 +0000

    Add SafeBrowsingEncryptedArchivesMetadata to chrome://flags
    We will re-use the existing #download-warning-improvements flag for
    this, since it's meant to cover all upcoming download warning features.
    
    Bug: 11491184
    Change-Id: I7c01aa90e32218415633c3fa8e0dd6cb3dafdb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4989887
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218610}

--- chrome/browser/metrics/chrome_feature_list_creator.cc
+++ chrome/browser/metrics/chrome_feature_list_creator.cc
@@ -105,6 +105,9 @@ GetSwitchDependentFeatureOverrides(const base::CommandLine& command_line) {
       {switches::kEnableDownloadWarningImprovements,
        std::cref(safe_browsing::kImprovedDownloadPageWarnings),
        base::FeatureList::OVERRIDE_ENABLE_FEATURE},
+      {switches::kEnableDownloadWarningImprovements,
+       std::cref(safe_browsing::kEncryptedArchivesMetadata),
+       base::FeatureList::OVERRIDE_ENABLE_FEATURE},
 
       // Override for --privacy-sandbox-ads-apis.
       {switches::kEnablePrivacySandboxAdsApis,
