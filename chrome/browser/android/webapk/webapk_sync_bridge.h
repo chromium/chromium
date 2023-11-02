e1fff855a32025051dcc1ba48e1b0506497907ce
Glenn Hartmann <hartmanng@chromium.org>
2023-11-02 16:22:22 +0000

    Add partial sync bridge MergeFullSyncData() implementation.
    This CL adds 2 key helper functions that implement roughly
    2/3 of the complexity of MergeFullSyncData(). This is parts
    1 and 2 of the full plan at
    https://docs.google.com/document/d/1Pce17EEuIs0dIbw-L1RZVf2HA4H8-Lu8RqVxHGmdJds.
    
    Bug: 1425518
    Change-Id: I901282f5c40d0e80e1acd0f965cfd170fca206cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4998326
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1218872}

--- chrome/browser/android/webapk/webapk_sync_bridge.h
+++ chrome/browser/android/webapk/webapk_sync_bridge.h
@@ -8,6 +8,7 @@
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
+#include "base/time/clock.h"
 #include "chrome/browser/android/webapk/webapk_database.h"
 #include "components/sync/model/entity_change.h"
 #include "components/sync/model/model_type_sync_bridge.h"
@@ -43,7 +44,8 @@ class WebApkSyncBridge : public syncer::ModelTypeSyncBridge {
   WebApkSyncBridge(
       AbstractWebApkDatabaseFactory* database_factory,
       base::OnceClosure on_initialized,
-      std::unique_ptr<syncer::ModelTypeChangeProcessor> change_processor);
+      std::unique_ptr<syncer::ModelTypeChangeProcessor> change_processor,
+      std::unique_ptr<base::Clock> clock);
   WebApkSyncBridge(const WebApkSyncBridge&) = delete;
   WebApkSyncBridge& operator=(const WebApkSyncBridge&) = delete;
   ~WebApkSyncBridge() override;
@@ -62,6 +64,53 @@ class WebApkSyncBridge : public syncer::ModelTypeSyncBridge {
   std::string GetClientTag(const syncer::EntityData& entity_data) override;
   std::string GetStorageKey(const syncer::EntityData& entity_data) override;
 
+  // internal helpers, exposed for testing.
+  bool AppWasUsedRecently(const sync_pb::WebApkSpecifics* specifics) const;
+
+  // PrepareSyncUpdateFromInstalledApps compiles a vector of changes that need
+  // to be applied to the remote sync data, based on the apps that are installed
+  // on the device. This is "Step 1" of MergeFullSyncData() (see
+  // https://docs.google.com/document/d/1Pce17EEuIs0dIbw-L1RZVf2HA4H8-Lu8RqVxHGmdJds).
+  // We don't need to consider apps from the db/registry yet - that will be
+  // covered in "Step 3".
+  //
+  // Being more specific, this step gathers all the apps that are installed
+  // locally that have been used or modified recently enough _except_ the ones
+  // that are already in the remote sync data and have been used more recently
+  // than the local versions (ie, we want only the latest, most up-to-date
+  // version of the metadata).
+  //
+  // |installed_apps| and |sync_changes| are inputs to this function, and
+  // |sync_update_from_installed| is the output.
+  void PrepareSyncUpdateFromInstalledApps(
+      const std::vector<std::unique_ptr<sync_pb::WebApkSpecifics>>&
+          installed_apps,
+      const syncer::EntityChangeList& sync_changes,
+      std::vector<const sync_pb::WebApkSpecifics*>* sync_update_from_installed)
+      const;
+
+  // PrepareRegistryUpdateFromInstalledAndSyncApps creates a collection of apps
+  // that need to be added to or removed from the db and registry, based on a
+  // combination of the app list gathered in
+  // PrepareSyncUpdateFromInstalledApps(), and the changes from Sync. This is
+  // "Step 2" of
+  // https://docs.google.com/document/d/1Pce17EEuIs0dIbw-L1RZVf2HA4H8-Lu8RqVxHGmdJds.
+  //
+  // Here, we want to gather all the apps from |sync_update_from_installed|,
+  // plus everything from the remote sync data except the ones that have already
+  // been covered by |sync_update_from_installed|. Effectively this means that
+  // we're gathering everything from Sync, plus all the relevant
+  // locally-installed apps, so we can bring the registry up to date with both
+  // data sources.
+  //
+  // |sync_update_from_installed| and |sync_changes| are inputs, and
+  // |registry_update_from_installed_and_sync| is the output.
+  void PrepareRegistryUpdateFromInstalledAndSyncApps(
+      const std::vector<const sync_pb::WebApkSpecifics*>&
+          sync_update_from_installed,
+      const syncer::EntityChangeList& sync_changes,
+      RegistryUpdateData* registry_update_from_installed_and_sync) const;
+
  private:
   void OnDatabaseOpened(base::OnceClosure callback,
                         Registry registry,
@@ -70,12 +119,14 @@ class WebApkSyncBridge : public syncer::ModelTypeSyncBridge {
 
   std::unique_ptr<WebApkDatabase> database_;
   Registry registry_;
+  std::unique_ptr<base::Clock> clock_;
 
   base::WeakPtrFactory<WebApkSyncBridge> weak_ptr_factory_{this};
 };
 
 std::unique_ptr<syncer::EntityData> CreateSyncEntityData(
     const WebApkProto& app);
+webapps::AppId ManifestIdStrToAppId(const std::string& manifest_id);
 
 }  // namespace webapk
 
