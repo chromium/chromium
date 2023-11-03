43d34cad87648aa8bfb723198ab014913d869bf3
Matthew Denton <mpdenton@chromium.org>
2023-11-03 16:53:44 +0000

    Linux/Cros: Add feature to disable Spectre v2 mitigations in net service
    Applying a seccomp sandbox in Chrome normally automatically enables
    Spectre v2 mitigations. This can cause performance regressions so this
    CL adds a feature to disable those mitigations, which should help debug
    any performance regressions when enabling the network service sandbox.
    
    (cherry picked from commit 55bb25a643cba469a00258e987074399db4c2ffb)
    
    Change-Id: Iaadf6b71ff65d7cd0116d9fd7e2df8cb21facb81
    Bug: 1494823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4965340
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1213702}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4969552
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/6045@{#1191}
    Cr-Branched-From: 905e8bdd32d891451d94d1ec71682e989da2b0a1-refs/heads/main@{#1204232}

--- sandbox/policy/features.cc
+++ sandbox/policy/features.cc
@@ -126,6 +126,20 @@ BASE_FEATURE(kForceSpectreVariant2Mitigation,
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+// Enabling the kNetworkServiceSandbox feature automatically enables Spectre
+// variant 2 mitigations in the network service. This can lead to performance
+// regressions, so enabling this feature will turn off the Spectre Variant 2
+// mitigations.
+//
+// On ChromeOS Ash, this overrides the system-wide kSpectreVariant2Mitigation
+// feature above, but not the user-controlled kForceSpectreVariant2Mitigation
+// feature.
+BASE_FEATURE(kForceDisableSpectreVariant2MitigationInNetworkService,
+             "kForceDisableSpectreVariant2MitigationInNetworkService",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+
 #if BUILDFLAG(IS_MAC)
 // Enables caching compiled sandbox profiles. Only some profiles support this,
 // as controlled by CanCacheSandboxPolicy().
