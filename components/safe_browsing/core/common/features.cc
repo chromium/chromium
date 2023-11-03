dfea2141e4d94f4897bb5eaac5d19eca3570a3fc
Richard Chen <richche@chromium.org>
2023-11-03 17:54:24 +0000

    InterceptRemoteHostsContactedInRenderer 100% Stable launch.
    - Enable feature by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1447587
    Change-Id: I7cf33a450b08b433d9cfed54e00adb48cc57dc49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002906
    Commit-Queue: Richard Chen <richche@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219597}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -132,7 +132,7 @@ BASE_FEATURE(kExtensionTelemetryReportHostsContactedViaWebSocket,
 BASE_FEATURE(
     kExtensionTelemetryInterceptRemoteHostsContactedInRenderer,
     "SafeBrowsingExtensionTelmetryInterceptRemoteHostsContactedInRenderer",
-    base::FEATURE_DISABLED_BY_DEFAULT);
+    base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryTabsApiSignal,
              "SafeBrowsingExtensionTelemetryTabsApiSignal",
