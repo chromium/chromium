70731d027c9382173ec03d26fadb572ff5256ef1
Ho Cheung <uioptt24@gmail.com>
2023-11-03 09:52:11 +0000

    [widget] Add a new assertion for TextViewWithLeading
    Add assertion `checkForLineSpacingAttributes` and its associated code
    to handle `android:lineSpacingExtra` or `android:lineSpacingMultiplier`.
    
    Bug: None
    Change-Id: I8afd99b801477852f93fa94c948fb9c7d8308a39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4994817
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Ho Cheung <uioptt24@gmail.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219382}

--- ui/android/java/src/org/chromium/ui/widget/TextViewWithLeading.java
+++ ui/android/java/src/org/chromium/ui/widget/TextViewWithLeading.java
@@ -12,10 +12,10 @@ import android.widget.TextView;
 import org.chromium.ui.R;
 
 /**
- * A TextView with the added leading property.
- * Leading is the distance between the baselines of successive lines of text (so the space between
- * rules on ruled paper). This class performs the calculation to setup leading correctly and allows
- * it to be set in XML. It overwrites android:lineSpacingExtra and android:lineSpacingMultiplier.
+ * A TextView with the added leading property. Leading is the distance between the baselines of
+ * successive lines of text (so the space between rules on ruled paper). This class performs the
+ * calculation to set up leading correctly and allows it to be set in XML. It overwrites
+ * android:lineSpacingExtra and android:lineSpacingMultiplier.
  */
 public class TextViewWithLeading extends TextView {
     /**
@@ -27,11 +27,11 @@ public class TextViewWithLeading extends TextView {
         super(context);
     }
 
-    /** Constructor for use from XML. */
+    /** Constructor for use from XML with checkForLineSpacingAttributes assertion. */
     public TextViewWithLeading(Context context, AttributeSet attrs) {
         super(context, attrs);
+        checkForLineSpacingAttributes(attrs);
 
-        // TODO(peconn): Add a lint check to ensure no lineSpacingExtr or lineSpacingMultiplier.
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextViewWithLeading, 0, 0);
         if (a.hasValue(R.styleable.TextViewWithLeading_leading)) {
             final float leading = a.getDimension(R.styleable.TextViewWithLeading_leading, 0f);
@@ -40,5 +40,12 @@ public class TextViewWithLeading extends TextView {
         }
 
         a.recycle();
+    }
+
+    private void checkForLineSpacingAttributes(AttributeSet attrs) {
+        assert attrs.getAttributeValue(null, "android:lineSpacingExtra") == null
+                        && attrs.getAttributeValue(null, "android:lineSpacingMultiplier") == null
+                : "Do not use android:lineSpacingExtra or android:lineSpacingMultiplier in"
+                        + " TextViewWithLeading.";
     }
 }
