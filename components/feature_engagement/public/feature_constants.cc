9f187b396cc207230fb08f0611675a85e68e1d66
Gayane Petrosyan <gayane@google.com>
2023-11-03 17:40:12 +0000

    [iOS] Enable What's New promos
    It was disabled by mistake here https://chromium-review.googlesource.com/c/chromium/src/+/4904150/10/components/feature_engagement/public/feature_constants.cc
    
    but it has always been enabled since it was added in M113
    https://chromium-review.googlesource.com/c/chromium/src/+/4321946/8/components/feature_engagement/public/feature_constants.cc
    
    Bug: 1499232
    Change-Id: I30af6313ade8234399518f46c8a94ebc43cb2448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004107
    Auto-Submit: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1219585}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -509,7 +509,7 @@ BASE_FEATURE(kIPHiOSPromoAppStoreFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHiOSPromoWhatsNewFeature,
              "IPH_iOSPromoWhatsNew",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHiOSPromoPostRestoreFeature,
              "IPH_iOSPromoPostRestore",
              base::FEATURE_ENABLED_BY_DEFAULT);
