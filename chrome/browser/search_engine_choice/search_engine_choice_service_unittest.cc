9b313e690b953afd53343af8f4d269d617850121
Jack Yammine <jyammine@google.com>
2023-11-06 15:36:20 +0000

    [waffle]Fix crash caused by trying to show the dialog when we shouldn't
    Fix crash caused by trying to display the search engine choice dialog
    when the dialog feature is not enabled.
    
    Fixed: 1497266
    Change-Id: Iaca1372dcab5f6e14455e0e8641fe0ec1dbbf7c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007614
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220297}

--- chrome/browser/search_engine_choice/search_engine_choice_service_unittest.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_service_unittest.cc
@@ -4,80 +4,90 @@
 
 #include "chrome/browser/search_engine_choice/search_engine_choice_service.h"
 #include "base/functional/bind.h"
-#include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "chrome/browser/ui/webui/settings/search_engines_handler.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_browser_process.h"
-#include "chrome/test/base/testing_profile_manager.h"
 #include "components/country_codes/country_codes.h"
 #include "components/search_engines/search_engine_choice_utils.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/signin/public/base/signin_switches.h"
-#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class SearchEngineChoiceServiceTest : public testing::Test {
+class SearchEngineChoiceServiceTest : public BrowserWithTestWindowTest {
  public:
-  SearchEngineChoiceServiceTest()
-      : profile_manager_(TestingBrowserProcess::GetGlobal()) {}
+  SearchEngineChoiceServiceTest() {
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{switches::kSearchEngineChoice,
+                              switches::kSearchEngineChoiceFre},
+        /*disabled_features=*/{});
+
+    scoped_chrome_build_override_ = std::make_unique<base::AutoReset<bool>>(
+        SearchEngineChoiceServiceFactory::ScopedChromeBuildOverrideForTesting(
+            /*force_chrome_build=*/true));
+  }
 
   void SetUp() override {
-    testing::Test::SetUp();
-    ASSERT_TRUE(profile_manager_.SetUp());
-    profile_ = profile_manager_.CreateTestingProfile("Profile 1");
-
+    BrowserWithTestWindowTest::SetUp();
     TemplateURLServiceFactory::GetInstance()->SetTestingFactoryAndUse(
         profile(),
         base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
 
     PrefService* pref_service = profile()->GetPrefs();
-    // The search engine choice feature is only enabled for countries in the EEA
-    // region.
+    // The search engine choice feature is only enabled for countries in the
+    // EEA region.
     const int kBelgiumCountryId =
         country_codes::CountryCharsToCountryID('B', 'E');
     pref_service->SetInteger(country_codes::kCountryIDAtInstall,
                              kBelgiumCountryId);
-
-    TemplateURLService* template_url_service =
-        TemplateURLServiceFactory::GetForProfile(profile());
-    search_engine_choice_service_ = std::make_unique<SearchEngineChoiceService>(
-        *profile_, *template_url_service);
-  }
-
-  Profile* profile() const { return profile_; }
-
-  SearchEngineChoiceService* search_engine_choice_service() {
-    return search_engine_choice_service_.get();
   }
 
   const base::HistogramTester& histogram_tester() const {
     return histogram_tester_;
   }
 
+  base::test::ScopedFeatureList& feature_list() { return feature_list_; }
+
  private:
-  base::test::ScopedFeatureList feature_list_{switches::kSearchEngineChoice};
-  content::BrowserTaskEnvironment task_environment_;
-  TestingProfileManager profile_manager_;
-  raw_ptr<Profile> profile_ = nullptr;
-  std::unique_ptr<SearchEngineChoiceService> search_engine_choice_service_;
+  base::test::ScopedFeatureList feature_list_;
   base::HistogramTester histogram_tester_;
+  std::unique_ptr<base::AutoReset<bool>> scoped_chrome_build_override_;
 };
 
 TEST_F(SearchEngineChoiceServiceTest, HandleLearnMoreLinkClicked) {
-  search_engine_choice_service()->NotifyLearnMoreLinkClicked(
+  SearchEngineChoiceService* search_engine_choice_service =
+      SearchEngineChoiceServiceFactory::GetForProfile(profile());
+
+  search_engine_choice_service->NotifyLearnMoreLinkClicked(
       SearchEngineChoiceService::EntryPoint::kDialog);
   histogram_tester().ExpectBucketCount(
       search_engines::kSearchEngineChoiceScreenEventsHistogram,
       search_engines::SearchEngineChoiceScreenEvents::kLearnMoreWasDisplayed,
       1);
 
-  search_engine_choice_service()->NotifyLearnMoreLinkClicked(
+  search_engine_choice_service->NotifyLearnMoreLinkClicked(
       SearchEngineChoiceService::EntryPoint::kProfilePicker);
   histogram_tester().ExpectBucketCount(
       search_engines::kSearchEngineChoiceScreenEventsHistogram,
       search_engines::SearchEngineChoiceScreenEvents::kFreLearnMoreWasDisplayed,
+      1);
+}
+
+TEST_F(SearchEngineChoiceServiceTest, CanShowDialog) {
+  feature_list().Reset();
+  feature_list().InitWithFeatures(
+      /*enabled_features=*/{switches::kSearchEngineChoiceFre},
+      /*disabled_features=*/{switches::kSearchEngineChoice});
+
+  SearchEngineChoiceService* search_engine_choice_service =
+      SearchEngineChoiceServiceFactory::GetForProfile(profile());
+
+  EXPECT_FALSE(search_engine_choice_service->CanShowDialog(*browser()));
+  histogram_tester().ExpectUniqueSample(
+      search_engines::kSearchEngineChoiceScreenNavigationConditionsHistogram,
+      search_engines::SearchEngineChoiceScreenConditions::kFeatureSuppressed,
       1);
 }
