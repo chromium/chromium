8dc31fe125ec54c9d75831750697af383d634601
Joemer Ramos <joemerramos@chromium.org>
2023-11-06 15:07:50 +0000

    [iOS] Clean Up UIButtonConfiguration in BubbleView
    Clean up for UIButtonConfigurationfeature flag. Creating small CLs to
    add transparency to relevant code owners. To ensure that there
    aren't conflicting UI elements, we are asking the reviewers to look at
    the files in the CL and any relevant files where the modified
    UIButtons are seen in other UI components. If any relevant files are
    missing that the feature guard IsUIButtonConfigurationEnabled(),
    please leave a comment to add those files. Feel free to reach out if
    this is confusing.
    
    Bug: 1423432
    Change-Id: I956068c12aa12d5b946756a761a0bf7d2b3772dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002753
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220270}

--- ios/chrome/browser/ui/bubble/bubble_view.mm
+++ ios/chrome/browser/ui/bubble/bubble_view.mm
@@ -187,35 +187,17 @@ UIButton* BubbleCloseButton() {
   const CGFloat closeButtonLeadingPadding = kCloseButtonSize -
                                             kCloseButtonTopTrailingPadding -
                                             buttonImage.size.width;
-  UIButton* button;
 
-  // TODO(crbug.com/1418068): Simplify after minimum version required is >=
-  // iOS 15.
-  if (base::ios::IsRunningOnIOS15OrLater() &&
-      IsUIButtonConfigurationEnabled()) {
-    if (@available(iOS 15, *)) {
-      UIButtonConfiguration* buttonConfiguration =
-          [UIButtonConfiguration plainButtonConfiguration];
-      [buttonConfiguration setImage:buttonImage];
-      [buttonConfiguration
-          setContentInsets:NSDirectionalEdgeInsetsMake(
-                               kCloseButtonTopTrailingPadding,
-                               closeButtonLeadingPadding,
-                               closeButtonBottomPadding,
-                               kCloseButtonTopTrailingPadding)];
-      button = [UIButton buttonWithConfiguration:buttonConfiguration
-                                   primaryAction:nil];
-    }
-  } else {
-    button = [UIButton buttonWithType:UIButtonTypeSystem];
-    [button setImage:buttonImage forState:UIControlStateNormal];
-    [button.imageView setBounds:CGRectZero];
-    [button.imageView setContentMode:UIViewContentModeScaleAspectFit];
-    UIEdgeInsets contentEdgeInsets = UIEdgeInsetsMakeDirected(
-        kCloseButtonTopTrailingPadding, closeButtonLeadingPadding,
-        closeButtonBottomPadding, kCloseButtonTopTrailingPadding);
-    SetImageEdgeInsets(button, contentEdgeInsets);
-  }
+  UIButtonConfiguration* buttonConfiguration =
+      [UIButtonConfiguration plainButtonConfiguration];
+  [buttonConfiguration setImage:buttonImage];
+  [buttonConfiguration
+      setContentInsets:NSDirectionalEdgeInsetsMake(
+                           kCloseButtonTopTrailingPadding,
+                           closeButtonLeadingPadding, closeButtonBottomPadding,
+                           kCloseButtonTopTrailingPadding)];
+  UIButton* button = [UIButton buttonWithConfiguration:buttonConfiguration
+                                         primaryAction:nil];
   [button setTintColor:[UIColor colorNamed:kSolidButtonTextColor]];
   [button setAccessibilityLabel:l10n_util::GetNSString(IDS_IOS_ICON_CLOSE)];
   [button setAccessibilityIdentifier:kBubbleViewCloseButtonIdentifier];
