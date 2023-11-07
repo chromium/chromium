2f65594f55ee21b03bc89627ff6146781a792a41
Joemer Ramos <joemerramos@chromium.org>
2023-11-07 19:14:56 +0000

    [iOS] BadgeMediator iOS 15 Available Guard Clean Up
    Clean up for iOS 15 available guards since the minimum deployment
    target is now 15.0.
    
    Bug: 1418068
    Change-Id: I85f166ba21564d6d4fdce25f7ba6400ed1edff3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006961
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1221080}

--- ios/chrome/browser/ui/badges/badge_mediator.mm
+++ ios/chrome/browser/ui/badges/badge_mediator.mm
@@ -74,8 +74,7 @@ const char kInfobarOverflowBadgeShownUserAction[] =
 @property(nonatomic, strong, readonly) NSArray<id<BadgeItem>>* badges;
 
 // The correct badge type for permissions infobar.
-@property(nonatomic, assign, readonly)
-    BadgeType permissionsBadgeType API_AVAILABLE(ios(15.0));
+@property(nonatomic, assign, readonly) BadgeType permissionsBadgeType;
 
 @end
 
@@ -162,11 +161,9 @@ const char kInfobarOverflowBadgeShownUserAction[] =
         BadgeTypeForInfobarType(infobarTypeBadgeStatePair.first);
     // Update BadgeType for permissions to align with current permission states
     // of the web state.
-    if (@available(iOS 15.0, *)) {
-      if (infobarTypeBadgeStatePair.first ==
-          InfobarType::kInfobarTypePermissions) {
-        badgeType = self.permissionsBadgeType;
-      }
+    if (infobarTypeBadgeStatePair.first ==
+        InfobarType::kInfobarTypePermissions) {
+      badgeType = self.permissionsBadgeType;
     }
     BadgeTappableItem* item =
         [[BadgeTappableItem alloc] initWithBadgeType:badgeType];
