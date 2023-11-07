be7bb6cd032adf41861faf2aa6566ae7073a4c13
Stepan Khapugin <stkhapugin@chromium.org>
2023-11-07 12:00:46 +0000

    [iOS] Prepare move push_notification to model subdirectory
    Creates a dummy file to update the downstream usage.
    
    Bug: 1480339
    Change-Id: I47115eb73cb5979079ba0a163201fb2193d3eba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007972
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220849}

---
+++ ios/chrome/browser/push_notification/model/push_notification_service.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PUSH_NOTIFICATION_MODEL_PUSH_NOTIFICATION_SERVICE_H_
+#define IOS_CHROME_BROWSER_PUSH_NOTIFICATION_MODEL_PUSH_NOTIFICATION_SERVICE_H_
+
+#import "ios/chrome/browser/push_notification/push_notification_service.h"
+
+#endif  // IOS_CHROME_BROWSER_PUSH_NOTIFICATION_MODEL_PUSH_NOTIFICATION_SERVICE_H_
