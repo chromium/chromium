9f8fc91cd1208a3d6c58689b1f72dfd922eecc56
Colin Blundell <blundell@chromium.org>
2023-11-07 11:52:22 +0000

    [//ash] Enable UseMappableSIForFastInkHost by default
    Bug: 1431257
    Change-Id: If37261feba9e262158f6293844a4ad1f4b45f820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4976227
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220846}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2970,7 +2970,7 @@ BASE_FEATURE(kEnableFastInkForSoftwareCursor,
 // ShouldUseMappableSharedImage() below.
 BASE_FEATURE(kEnableMappableSIForFastInkHost,
              "EnableMappableSIForFastInkHost",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 ////////////////////////////////////////////////////////////////////////////////
 
