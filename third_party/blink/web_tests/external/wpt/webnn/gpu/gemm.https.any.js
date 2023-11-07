47091d2006baf4d2b5f0b05c13b7fa23c16eaff7
Ningxin Hu <ningxin.hu@intel.com>
2023-11-07 23:49:08 +0000

    WebNN: Run WPT operator conformance tests for GPU devices
    This CL adds WebNN operator conformance tests for GPU devices into WPT
    webnn/gpu folder.
    
    WebNN service for GPU devices is behind a feature flag, this CL creates
    a virtual test suite that runs the WebNN operator test cases for GPU by
    enabling the "MachineLearningNeuralNetworkService" flag.
    
    WebNN GPU backend is supported on Win11. This CL also updates the
    baseline regarding the test results.
    
    Bug: 1273291
    Change-Id: Idb4ca8d6eeee9eb207e359c4b20cf04c55c08afc
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4997551
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221277}

--- third_party/blink/web_tests/external/wpt/webnn/gpu/gemm.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/gpu/gemm.https.any.js
@@ -1,10 +1,10 @@
 // META: title=test WebNN API gemm operation
 // META: global=window,dedicatedworker
-// META: script=./resources/utils.js
+// META: script=../resources/utils.js
 // META: timeout=long
 
 'use strict';
 
 // https://webmachinelearning.github.io/webnn/#api-mlgraphbuilder-gemm
 
-testWebNNOperation('gemm', buildGemm);
+testWebNNOperation('gemm', buildGemm, 'gpu');
