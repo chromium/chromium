9ded58e32583f02d38ffd8ac81f27a1c5118520b
Viktor Semeniuk <vsemeniuk@google.com>
2023-11-07 09:22:23 +0000

    Moving protos and unified_password_manager_proto_utils.h to android
    Since they are used (and can be used) only on Android this CL moves
    them from components/ to chrome/browser/password_manager/android/.
    
    Bug: 1479425
    Change-Id: Ia3df5f4f106c255fa317aac5cc2b929e346f85b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5007599
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220795}

--- chrome/browser/password_manager/android/unified_password_manager_proto_utils.cc
+++ chrome/browser/password_manager/android/unified_password_manager_proto_utils.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/password_manager/core/browser/password_store/unified_password_manager_proto_utils.h"
+#include "chrome/browser/password_manager/android/unified_password_manager_proto_utils.h"
 
 #include <string>
 
