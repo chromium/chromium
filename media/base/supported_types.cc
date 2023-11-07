5161dc2e76ac76af7afc3c18c900478a7c165436
Dale Curtis <dalecurtis@chromium.org>
2023-11-07 05:12:55 +0000

    Fix Theora support detection when codec string is not specified.
    This ensures we report Theora as unsupported when Theora is
    tried without going through canPlayType().
    
    - Also fixes a similar issue for MPEG4.
    - Also removes a test which was expecting MPEG4 playback to work
    on non-ChromeOS platforms.
    
    Bug: 1500061
    Change-Id: I0c011704de663baf9dc41cb9e90e69c8de6859cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008061
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1220733}

--- media/base/supported_types.cc
+++ media/base/supported_types.cc
@@ -296,8 +296,8 @@ bool IsAV1Supported(const VideoType& type) {
 }
 
 bool IsMPEG4Supported() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  return true;
+#if BUILDFLAG(IS_CHROMEOS) && BUILDFLAG(USE_PROPRIETARY_CODECS)
+  return base::FeatureList::IsEnabled(kCrOSLegacyMediaFormats);
 #else
   return false;
 #endif
@@ -355,9 +355,10 @@ bool IsDefaultSupportedVideoType(const VideoType& type) {
 #endif
 
   switch (type.codec) {
+    case VideoCodec::kTheora:
+      return IsBuiltInVideoCodec(type.codec);
     case VideoCodec::kH264:
     case VideoCodec::kVP8:
-    case VideoCodec::kTheora:
       return true;
     case VideoCodec::kAV1:
       return IsAV1Supported(type);
@@ -427,7 +428,7 @@ bool IsDefaultSupportedAudioType(const AudioType& type) {
 bool IsBuiltInVideoCodec(VideoCodec codec) {
 #if BUILDFLAG(ENABLE_FFMPEG_VIDEO_DECODERS)
   if (codec == VideoCodec::kTheora)
-    return true;
+    return base::FeatureList::IsEnabled(kTheoraVideoCodec);
   if (codec == VideoCodec::kVP8)
     return true;
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
