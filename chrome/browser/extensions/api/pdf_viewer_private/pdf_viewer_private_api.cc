2048d38a20787facd8274f5f43316ee9608d6c85
Andy Phan <andyphan@chromium.org>
2023-11-07 20:42:35 +0000

    [OOPIF PDF] Add PdfViewerPrivate extension API endpoints
    Add extension functions GetStreamInfo() and SetPdfPluginAttributes().
    These will be used to load the PDF Viewer in OOPIF without a GuestView.
    
    Functionality is very similar to extensions::MimeHandlerServiceImpl's
    GetStreamInfo() and SetPdfPluginAttributes(), but instead of using
    mime_handler::MimeHandlerService, use an extension API function instead.
    
    pdfViewerPrivate.StreamInfo and pdfViewerPrivate.PdfPluginAttributes are
    based on mimeHandlerPrivate.StreamInfo and
    mimeHandlerPrivate.PdfPluginAttributes. However, for the PDF viewer, the
    mime type and response headers fields in StreamInfo are unused, so don't
    include those fields.
    
    Currently, these functions are unused in this CL. Future CLs will make
    use of the new API endpoints. See https://crrev.com/c/4691287.
    
    Bug: 1445746
    Change-Id: I7b0439a0b65393c5e651dc8e06b0bd21cfe0ad64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4682380
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1221164}

--- chrome/browser/extensions/api/pdf_viewer_private/pdf_viewer_private_api.cc
+++ chrome/browser/extensions/api/pdf_viewer_private/pdf_viewer_private_api.cc
@@ -4,12 +4,18 @@
 
 #include "chrome/browser/extensions/api/pdf_viewer_private/pdf_viewer_private_api.h"
 
+#include <cmath>
+
+#include "base/memory/weak_ptr.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/pdf/pdf_pref_names.h"
+#include "chrome/browser/pdf/pdf_viewer_stream_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/pdf_viewer_private.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "url/url_constants.h"
 
 namespace extensions {
@@ -20,6 +26,9 @@ namespace IsAllowedLocalFileAccess =
     api::pdf_viewer_private::IsAllowedLocalFileAccess;
 
 namespace SetPdfOcrPref = api::pdf_viewer_private::SetPdfOcrPref;
+
+namespace SetPdfPluginAttributes =
+    api::pdf_viewer_private::SetPdfPluginAttributes;
 
 // Check if the current URL is allowed based on a list of allowlisted domains.
 bool IsUrlAllowedToEmbedLocalFiles(
@@ -42,7 +51,46 @@ bool IsUrlAllowedToEmbedLocalFiles(
   return false;
 }
 
+// Get the `StreamContainer` associated with the `extension_host`.
+base::WeakPtr<StreamContainer> GetStreamContainer(
+    content::RenderFrameHost* extension_host) {
+  content::RenderFrameHost* embedder_host = extension_host->GetParent();
+  if (!embedder_host) {
+    return nullptr;
+  }
+
+  auto* pdf_viewer_stream_manager =
+      pdf::PdfViewerStreamManager::FromWebContents(
+          content::WebContents::FromRenderFrameHost(embedder_host));
+  if (!pdf_viewer_stream_manager) {
+    return nullptr;
+  }
+
+  return pdf_viewer_stream_manager->GetStreamContainer(embedder_host);
+}
+
 }  // namespace
+
+PdfViewerPrivateGetStreamInfoFunction::PdfViewerPrivateGetStreamInfoFunction() =
+    default;
+
+PdfViewerPrivateGetStreamInfoFunction::
+    ~PdfViewerPrivateGetStreamInfoFunction() = default;
+
+ExtensionFunction::ResponseAction PdfViewerPrivateGetStreamInfoFunction::Run() {
+  base::WeakPtr<StreamContainer> stream =
+      GetStreamContainer(render_frame_host());
+  if (!stream) {
+    return RespondNow(Error("Failed to get StreamContainer"));
+  }
+
+  api::pdf_viewer_private::StreamInfo stream_info;
+  stream_info.original_url = stream->original_url().spec();
+  stream_info.stream_url = stream->stream_url().spec();
+  stream_info.tab_id = stream->tab_id();
+  stream_info.embedded = stream->embedded();
+  return RespondNow(WithArguments(stream_info.ToValue()));
+}
 
 PdfViewerPrivateIsAllowedLocalFileAccessFunction::
     PdfViewerPrivateIsAllowedLocalFileAccessFunction() = default;
@@ -113,6 +161,44 @@ ExtensionFunction::ResponseAction PdfViewerPrivateSetPdfOcrPrefFunction::Run() {
   DCHECK(pref->GetValue()->is_bool());
   prefs->SetBoolean(prefs::kAccessibilityPdfOcrAlwaysActive, params->value);
   return RespondNow(WithArguments(true));
+}
+
+PdfViewerPrivateSetPdfPluginAttributesFunction::
+    PdfViewerPrivateSetPdfPluginAttributesFunction() = default;
+
+PdfViewerPrivateSetPdfPluginAttributesFunction::
+    ~PdfViewerPrivateSetPdfPluginAttributesFunction() = default;
+
+ExtensionFunction::ResponseAction
+PdfViewerPrivateSetPdfPluginAttributesFunction::Run() {
+  absl::optional<SetPdfPluginAttributes::Params> params =
+      SetPdfPluginAttributes::Params::Create(args());
+  EXTENSION_FUNCTION_VALIDATE(params);
+
+  base::WeakPtr<StreamContainer> stream =
+      GetStreamContainer(render_frame_host());
+  if (!stream) {
+    return RespondNow(Error("Failed to get StreamContainer"));
+  }
+
+  const api::pdf_viewer_private::PdfPluginAttributes& attributes =
+      params->attributes;
+  // Check the `background_color` is an integer.
+  double whole = 0.0;
+  if (std::modf(attributes.background_color, &whole) != 0.0) {
+    return RespondNow(Error("Background color is not an integer"));
+  }
+
+  // Check the `background_color` is within the range of a uint32_t.
+  if (!base::IsValueInRangeForNumericType<uint32_t>(
+          attributes.background_color)) {
+    return RespondNow(Error("Background color out of bounds"));
+  }
+
+  stream->set_pdf_plugin_attributes(mime_handler::PdfPluginAttributes::New(
+      /*background_color=*/attributes.background_color,
+      /*allow_javascript=*/attributes.allow_javascript));
+  return RespondNow(NoArguments());
 }
 
 }  // namespace extensions
