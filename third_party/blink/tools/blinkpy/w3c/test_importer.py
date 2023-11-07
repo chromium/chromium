5ad6a864e88081425f700e0fa106524a0ede9a5c
Jonathan Lee <jonathanjlee@google.com>
2023-11-07 03:31:57 +0000

    [wpt-importer] Always upload TestExpectations
    This speculatively fixes [0], where only TestExpectations should be
    updated (e.g., only reftest failures), but PS2 with the new expectations
    is not uploaded. In such a scenario, it's possible to arrive at the
    check removed here with no working changes because TestExpectations are
    committed earlier to satisfy `rebaseline-cl` [1].
    
    This check is not necessary because uploading an empty PS1..2 in
    response to one or more try build WPT failures should never be valid.
    (Those try builders, if run, would just fail again.)
    
    This issue may have been masked by the frequent changes staged by
    `update-metadata`.
    
    [0]: https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket/8765246270296581105/+/u/Import_changes_from_WPT_to_Chromium/stdout
    [1]: crrev.com/c/3900005
    
    Bug: None
    Change-Id: Ic9cda8384876712af3e3d6cc7b7f67860ca0e19f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5008935
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220705}

--- third_party/blink/tools/blinkpy/w3c/test_importer.py
+++ third_party/blink/tools/blinkpy/w3c/test_importer.py
@@ -245,17 +245,16 @@ class TestImporter(object):
 
         if try_results and self.git_cl.some_failed(try_results):
             self.fetch_new_expectations_and_baselines()
-            if self.project_git.has_working_directory_changes():
-                # Skip slow and timeout tests so that presubmit check passes
-                port = self.host.port_factory.get()
-                if self.expectations_updater.skip_slow_timeout_tests(port):
-                    path = port.path_to_generic_test_expectations_file()
-                    self.project_git.add_list([path])
+            # Skip slow and timeout tests so that presubmit check passes
+            port = self.host.port_factory.get()
+            if self.expectations_updater.skip_slow_timeout_tests(port):
+                path = port.path_to_generic_test_expectations_file()
+                self.project_git.add_list([path])
 
-                self._generate_manifest()
-                message = 'Update test expectations and baselines.'
-                self._commit_changes(message)
-                self._upload_patchset(message)
+            self._generate_manifest()
+            message = 'Update test expectations and baselines.'
+            self._commit_changes(message)
+            self._upload_patchset(message)
         return True
 
     def _trigger_try_jobs(self):
