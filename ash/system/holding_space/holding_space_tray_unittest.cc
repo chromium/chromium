c5a13d04770f135efc3ef89c6157f4fedcbb998a
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-11-08 06:19:00 +0000

    Revert "Revert "tote tour: Add destruction method to HoldingSpaceControllerObserver""
    This reverts commit 406c706f31e95b6bec61e9a3cdbc9ded5343bdbd.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8765019503283233665
    
    Sample failed build: https://ci.chromium.org/b/8765019503283233665
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8765019503283233665&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5004362
    
    Original change's description:
    > Revert "tote tour: Add destruction method to HoldingSpaceControllerObserver"
    >
    > This reverts commit 2132506e6b571f8c2d72080a2ba45af3ced03cd3.
    >
    > Reason for revert: Introduces a use-after-free asan error in HoldingSpaceControllerObserverTest.Destruction (ref: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/54510/test-results?sortby=&groupby=)
    >
    > Original change's description:
    > > tote tour: Add destruction method to HoldingSpaceControllerObserver
    > >
    > > This will be used to prevent lifetime issues with observers.
    > > Specifically, the HoldingSpaceWallpaperNudgeController observes the
    > > controller to hide the bubble when the holding space opens, but may be
    > > destroyed after the HoldingSpaceController.
    > >
    > > Bug: b:283171784
    > > Change-Id: I68db5d10d282cd0dea8f91477f72d069d56f70f7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5005316
    > > Commit-Queue: Angus McLean <angusmclean@google.com>
    > > Reviewed-by: James Cook <jamescook@chromium.org>
    > > Reviewed-by: David Black <dmblack@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1221230}
    >
    > Bug: b:283171784
    > Change-Id: If68625db1e2fbc135cf9c672d08cc484b3fa2119
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004362
    > Auto-Submit: Justin Huang <justinhuang@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Justin Huang <justinhuang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1221407}
    >
    
    Bug: b:283171784
    Change-Id: I4607adce79745610f773de914a9e56ed69089549
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012252
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1221417}

--- ash/system/holding_space/holding_space_tray_unittest.cc
+++ ash/system/holding_space/holding_space_tray_unittest.cc
@@ -23,6 +23,7 @@
 #include "ash/public/cpp/holding_space/holding_space_test_api.h"
 #include "ash/public/cpp/holding_space/holding_space_util.h"
 #include "ash/public/cpp/holding_space/mock_holding_space_client.h"
+#include "ash/public/cpp/holding_space/scoped_mock_holding_space_controller_observer.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
@@ -263,30 +264,6 @@ const views::MenuItemView* GetMenuItemByCommandId(HoldingSpaceCommandId id) {
   }
   return nullptr;
 }
-
-// ScopedMockHoldingSpaceControllerObserver ------------------------------------
-
-// An implementation of `HoldingSpaceControllerObserver` that enables its
-// methods to be mocked.
-class ScopedMockHoldingSpaceControllerObserver
-    : public HoldingSpaceControllerObserver {
- public:
-  explicit ScopedMockHoldingSpaceControllerObserver(
-      HoldingSpaceController* controller) {
-    observation_.Observe(controller);
-  }
-
-  // HoldingSpaceControllerObserver:
-  MOCK_METHOD(void,
-              OnHoldingSpaceTrayBubbleVisibilityChanged,
-              (const HoldingSpaceTray*, bool),
-              (override));
-
- private:
-  base::ScopedObservation<HoldingSpaceController,
-                          HoldingSpaceControllerObserver>
-      observation_{this};
-};
 
 // ViewVisibilityChangedWaiter -------------------------------------------------
 
