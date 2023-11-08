49fee66e92c356c0fca180a5f7d9e41720e71f4a
Shuming Hao <shuminghao@google.com>
2023-11-08 22:14:35 +0000

    Sync render-arc-notifications-by-chrome Chrome flag to ARC
    ARC side CL: ag/24592130
    
    Bug: b:298088396
    Test: launch and post notifications using go/notify-apk
    Change-Id: I422c3066233ad1498dfc595840aa134965d20c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5010987
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Shuming Hao <shuminghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1221912}

--- ash/components/arc/chrome_feature_flags/arc_chrome_feature_flags_bridge.cc
+++ ash/components/arc/chrome_feature_flags/arc_chrome_feature_flags_bridge.cc
@@ -90,6 +90,8 @@ void ArcChromeFeatureFlagsBridge::NotifyFeatureFlags() {
   flags->rounded_window_radius = chromeos::features::RoundedWindowsRadius();
   flags->xdg_mode = base::FeatureList::IsEnabled(kXdgMode);
   flags->enable_pip_double_tap = ash::features::IsPipDoubleTapToResizeEnabled();
+  flags->render_arc_notifications_by_chrome =
+      ash::features::IsRenderArcNotificationsByChromeEnabled();
 
   chrome_feature_flags_instance->NotifyFeatureFlags(std::move(flags));
 }
