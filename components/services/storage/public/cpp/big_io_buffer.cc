01360f8143290116e80ca18ade5f9518245d2c96
Tom Sepez <tsepez@chromium.org>
2023-11-09 18:25:47 +0000

    Separate owning and non-owning net:IOBuffers by class.
    Currently, this is determined based upon constructor overload.
    Instead, simplify by making ownership depend upon the class itself.
    Re-use the existing IOBufferWithSize class for the owning class, just
    to cut down on the number of places touched by the code change. Once
    functional, this can be mass renamed to something more meaningful.
    
    Bug: 1493389
    Change-Id: I864f248b275f747ee9d6fbbb36f6f07ac9041d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4977740
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222411}

--- components/services/storage/public/cpp/big_io_buffer.cc
+++ components/services/storage/public/cpp/big_io_buffer.cc
@@ -10,21 +10,18 @@
 namespace storage {
 
 BigIOBuffer::BigIOBuffer(mojo_base::BigBuffer buffer)
-    : net::IOBufferWithSize(nullptr, buffer.size()),
-      buffer_(std::move(buffer)) {
+    : net::IOBuffer(nullptr, buffer.size()), buffer_(std::move(buffer)) {
   data_ = reinterpret_cast<char*>(buffer_.data());
 }
 
 BigIOBuffer::BigIOBuffer(size_t size)
-    : net::IOBufferWithSize(nullptr, size),
-      buffer_(mojo_base::BigBuffer(size)) {
+    : net::IOBuffer(nullptr, size), buffer_(mojo_base::BigBuffer(size)) {
   data_ = reinterpret_cast<char*>(buffer_.data());
   DCHECK(data_);
 }
 
 BigIOBuffer::~BigIOBuffer() {
-  // Must clear `data_` so base class doesn't attempt to delete[] a pointer
-  // it doesn't own.
+  // Must clear `data_` so base class doesn't hold a dangling ptr.
   data_ = nullptr;
   size_ = 0UL;
 }
