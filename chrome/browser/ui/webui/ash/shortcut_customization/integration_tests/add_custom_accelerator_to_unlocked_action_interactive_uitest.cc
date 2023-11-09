3e6e0ab19363a2994065b3bf336d1515c8d631ab
Michael Checo <michaelcheco@google.com>
2023-11-09 23:46:06 +0000

    Port AddCustomAcceleratorToUnlockedAction to interactive_ui_test
    Tast test: https://crsrc.org/o/src/platform/tast-tests/src/go.chromium.org/tast-tests/cros/local/bundles/cros/shortcutcustomization/add_custom_accelerator_to_unlocked_action.go
    
    Bug: b:307385730
    Change-Id: I64c6ca16f1828e7827255bfee5d0ece73db95f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4995040
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222633}

---
+++ chrome/browser/ui/webui/ash/shortcut_customization/integration_tests/add_custom_accelerator_to_unlocked_action_interactive_uitest.cc
@@ -0,0 +1,78 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <linux/input-event-codes.h>
+
+#include "ash/ash_element_identifiers.h"
+#include "chrome/browser/ui/settings_window_manager_chromeos.h"
+#include "chrome/browser/ui/webui/ash/shortcut_customization/shortcut_customization_test_base.h"
+#include "chrome/test/base/chromeos/crosier/interactive_ash_test.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "ui/base/interaction/element_identifier.h"
+#include "ui/events/event_constants.h"
+#include "ui/events/keycodes/keyboard_codes_posix.h"
+
+namespace ash {
+
+namespace {
+
+class AddCustomAcceleratorToUnlockedActionInteractiveUiTest
+    : public ShortcutCustomizationInteractiveUiTestBase {
+ public:
+  const DeepQuery kCustomAcceleratorViewQuery{
+      "shortcut-customization-app",
+      "navigation-view-panel#navigationPanel",
+      "#category-0",
+      "#container",
+      "accelerator-subsection",
+      "tbody#rowList",
+      // Action 93 corresponds to the "Open/Close Calendar" shortcut.
+      "accelerator-row[action='93']",
+      "#container > td > accelerator-view:nth-child(2)",
+  };
+};
+
+IN_PROC_BROWSER_TEST_F(AddCustomAcceleratorToUnlockedActionInteractiveUiTest,
+                       AddCustomAcceleratorToUnlockedAction) {
+  ui::Accelerator default_accel(ui::VKEY_C, ui::EF_COMMAND_DOWN);
+  ui::Accelerator new_accel(ui::VKEY_N,
+                            ui::EF_COMMAND_DOWN | ui::EF_CONTROL_DOWN);
+
+  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kShortcutAppWebContentsId);
+  webcontents_id_ = kShortcutAppWebContentsId;
+  RunTestSequence(
+      InstrumentNextTab(kShortcutAppWebContentsId, AnyBrowser()),
+      LaunchShortcutCustomizationApp(),
+      WaitForWebContentsReady(kShortcutAppWebContentsId,
+                              GURL("chrome://shortcut-customization")),
+      InAnyContext(Steps(
+          SendAccelerator(webcontents_id_, new_accel),
+          EnsureNotPresent(kCalendarViewElementId),
+          Log("Verify that the custom shortcut does not open the calendar "
+              "before it's added as a shortcut"),
+          OpenCalendarShortcutDialog(), AddCustomCalendarShortcut(new_accel),
+          Log("Adding Search + Ctrl + n as a custom open/close calendar "
+              "shortcut"),
+          EnsurePresent(webcontents_id_, kCustomAcceleratorViewQuery),
+          Log("New shortcut is present in the UI"),
+          SendAccelerator(webcontents_id_, new_accel), FlushEvents(),
+          WaitForShow(kCalendarViewElementId),
+          Log("Custom shortcut opens calendar"),
+          SendAccelerator(webcontents_id_, new_accel), FlushEvents(),
+          WaitForHide(kCalendarViewElementId),
+          Log("Custom shortcut closes calendar"), ResetCalendarShortcuts(),
+          Log("Open/Close calendar shortcut reset to defaults"),
+          FocusWebContents(webcontents_id_), EnsureAcceleratorsAreProcessed(),
+          SendAccelerator(webcontents_id_, default_accel), FlushEvents(),
+          WaitForShow(kCalendarViewElementId),
+          SendAccelerator(webcontents_id_, default_accel), FlushEvents(),
+          WaitForHide(kCalendarViewElementId),
+          Log("Default shortcut still works"),
+          SendAccelerator(webcontents_id_, new_accel), FlushEvents(),
+          EnsureNotPresent(kCalendarViewElementId),
+          Log("Custom shortcut no longer works"))));
+}
+
+}  // namespace
+}  // namespace ash
