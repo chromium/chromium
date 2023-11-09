656961493e3990d935ff060aa2a340bc0f26f6fe
Ameur Hosni <ameurhosni@google.com>
2023-11-09 14:49:11 +0000

    [IOS][1/3] Move ios/chrome/browser/optimization_guide to model subfolder
    Creates upstream headers to update downstream deps before moving.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/optimization_guide will be moved to
    ios/c/b/optimization_guide/model/ to showcase that they are model files.
    
    Bug: 1480327
    Change-Id: Id522a5a538b6e3900c613914ce024b7979c1737a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013104
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222263}

---
+++ ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_MODEL_OPTIMIZATION_GUIDE_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_MODEL_OPTIMIZATION_GUIDE_SERVICE_FACTORY_H_
+
+#import "ios/chrome/browser/optimization_guide/optimization_guide_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_MODEL_OPTIMIZATION_GUIDE_SERVICE_FACTORY_H_
