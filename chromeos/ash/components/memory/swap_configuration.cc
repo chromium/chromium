5ce1b3c6a7e2175aab92473ec76df1ae16ef48f5
Erik Chen <erikchen@chromium.org>
2023-11-09 00:28:19 +0000

    chromeos: Enable new thresholds for non-arc memory pressure signals.
    As we will be running a long-term holdback, continue to keep a
    fieldtrial testing config for the disabled path.
    
    Bug: b/226425011
    Change-Id: Iaed4b5b3fb37911f24c5fd5136db675493d6c6e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009143
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1221985}

--- chromeos/ash/components/memory/swap_configuration.cc
+++ chromeos/ash/components/memory/swap_configuration.cc
@@ -16,11 +16,11 @@ namespace ash {
 // for ARC enabled users and one for ARC disabled users.
 BASE_FEATURE(kCrOSMemoryPressureSignalStudyNonArc,
              "ChromeOSMemoryPressureSignalStudyNonArc",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kCrOSMemoryPressureSignalStudyNonArcCriticalBps{
     &kCrOSMemoryPressureSignalStudyNonArc, "critical_threshold_percentage",
-    520};
+    1500};
 
 const base::FeatureParam<int> kCrOSMemoryPressureSignalStudyNonArcModerateBps{
     &kCrOSMemoryPressureSignalStudyNonArc, "moderate_threshold_percentage",
