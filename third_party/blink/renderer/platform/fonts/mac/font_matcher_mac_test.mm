9e4e2dc59f67417419fc550b34f359f51d856972
Munira Tursunova <moonira@google.com>
2023-11-09 19:01:59 +0000

    Added unit tests for font family matching on Mac
    Added unit tests to cover different cases of MatchNSFontFamily() calls:
    system ui fonts, matching fonts by postscript and family names, matching
    fonts with different weights and styles.
    
    Bug: 1019112
    Change-Id: I6ad5a757667ae94b053b284645a9c15ab2ef720c
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel,mac13-blink-rel,mac12.0-blink-rel,mac12.0.arm64-blink-rel,mac11.0-blink-rel,mac11.0.arm64-blink-rel,mac10.15-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5001708
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222435}

--- third_party/blink/renderer/platform/fonts/mac/font_matcher_mac_test.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_matcher_mac_test.mm
@@ -7,21 +7,155 @@
 #import <AppKit/AppKit.h>
 #import <CoreText/CoreText.h>
 
+#include "base/apple/foundation_util.h"
+#include "base/apple/scoped_cftyperef.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/font_family_names.h"
+#include "third_party/blink/renderer/platform/fonts/font_selection_types.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
+using base::apple::NSToCFPtrCast;
 using base::apple::ScopedCFTypeRef;
 
 namespace blink {
 
-void TestSystemFontContainsString(FontSelectionValue desired_weight,
-                                  NSString* substring) {
-  NSFont* font =
-      MatchNSFontFamily(font_family_names::kSystemUi, 0, desired_weight, 11);
-  EXPECT_TRUE([font.description containsString:substring]);
+namespace {
+
+struct FontName {
+  const char* full_font_name;
+  const char* postscript_name;
+  const char* family_name;
+};
+
+// If these font names are unavailable on future Mac OS versions, please try to
+// find replacements or remove individual lines.
+const FontName FontNames[] = {
+    {"American Typewriter Condensed Light", "AmericanTypewriter-CondensedLight",
+     "American Typewriter"},
+    {"Apple Braille Outline 6 Dot", "AppleBraille-Outline6Dot",
+     "Apple Braille"},
+    {"Arial Narrow Bold Italic", "ArialNarrow-BoldItalic", "Arial Narrow"},
+    {"Baskerville SemiBold Italic", "Baskerville-SemiBoldItalic",
+     "Baskerville"},
+    {"Devanagari MT", "DevanagariMT", "Devanagari MT"},
+    {"DIN Alternate Bold", "DINAlternate-Bold", "DIN Alternate"},
+    {"Gill Sans Light Italic", "GillSans-LightItalic", "Gill Sans"},
+    {"Malayalam Sangam MN", "MalayalamSangamMN", "Malayalam Sangam MN"},
+    {"Hiragino Maru Gothic ProN W4", "HiraMaruProN-W4",
+     "Hiragino Maru Gothic ProN"},
+    {"Hiragino Sans W3", "HiraginoSans-W3", "Hiragino Sans"},
+};
+
+const FontName CommonFontNames[] = {
+    {"Avenir-Roman", "Avenir-Roman", "Avenir"},
+    {"CourierNewPS-BoldMT", "CourierNewPS-BoldMT", "Courier New"},
+    {"Helvetica-Light", "Helvetica-Light", "Helvetica"},
+    {"HelveticaNeue-CondensedBlack", "HelveticaNeue-CondensedBlack",
+     "Helvetica Neue"},
+    {"Menlo-Bold", "Menlo-Bold", "Menlo"},
+    {"Tahoma", "Tahoma", "Tahoma"},
+    {"TimesNewRomanPS-BoldItalicMT", "TimesNewRomanPS-BoldItalicMT",
+     "Times New Roman"},
+};
+
+const char* FamiliesWithBoldItalicFaces[] = {"Baskerville", "Cochin", "Georgia",
+                                             "GillSans"};
+
+void TestFontWithWeights(const AtomicString& font_name) {
+  float ns_weights[] = {
+      -0.80,  // NSFontWeightUltraLight
+      -0.60,  // NSFontWeightThin
+      -0.40,  // NSFontWeightLight
+      0.0,    // NSFontWeightRegular
+      0.23,   // NSFontWeightMedium
+      0.30,   // NSFontWeightSemibold
+      0.40,   // NSFontWeightBold
+      0.56,   // NSFontWeightHeavy
+      0.62,   // NSFontWeightBlack
+  };
+  for (size_t i = 0; i < 9; i++) {
+    @autoreleasepool {
+      int weight = (i + 1) * 100;
+      NSFont* font =
+          MatchNSFontFamily(font_name, 0, FontSelectionValue(weight), 11);
+      EXPECT_TRUE(font);
+
+      ScopedCFTypeRef<CFDictionaryRef> traits(
+          CTFontCopyTraits(NSToCFPtrCast(font)));
+      CFNumberRef actual_weight_cf_num =
+          base::apple::GetValueFromDictionary<CFNumberRef>(traits.get(),
+                                                           kCTFontWeightTrait);
+      float actual_weight = 0.0;
+      CFNumberGetValue(actual_weight_cf_num, kCFNumberFloatType,
+                       &actual_weight);
+      EXPECT_EQ(actual_weight, ns_weights[i]);
+    }
+  }
 }
 
-TEST(FontMatcherMacTest, NoUniqueFontMatchOnUnavailableFont) {
+void TestFontWithBoldAndItalicTraits(const AtomicString& font_name) {
+  NSFont* font_italic =
+      MatchNSFontFamily(font_name, NSFontItalicTrait, kNormalWeightValue, 11);
+  EXPECT_TRUE(font_italic);
+
+  CTFontSymbolicTraits italic_font_traits =
+      CTFontGetSymbolicTraits(NSToCFPtrCast(font_italic));
+  EXPECT_TRUE(italic_font_traits & NSFontItalicTrait);
+
+  NSFont* font_bold_italic =
+      MatchNSFontFamily(font_name, NSFontItalicTrait, kBoldWeightValue, 11);
+  EXPECT_TRUE(font_bold_italic);
+
+  CTFontSymbolicTraits bold_italic_font_traits =
+      CTFontGetSymbolicTraits(NSToCFPtrCast(font_bold_italic));
+  EXPECT_TRUE(bold_italic_font_traits & NSFontItalicTrait);
+  EXPECT_TRUE(bold_italic_font_traits & NSFontBoldTrait);
+}
+
+void TestFontMatchingByNameByFamilyName(const char* font_name) {
+  NSFont* font =
+      MatchNSFontFamily(AtomicString(font_name), 0, kNormalWeightValue, 11);
+  EXPECT_TRUE(font);
+  ScopedCFTypeRef<CFStringRef> matched_family_name(
+      CTFontCopyFamilyName(base::apple::NSToCFPtrCast(font)));
+  ScopedCFTypeRef<CFStringRef> expected_family_name(
+      CFStringCreateWithCString(nullptr, font_name, kCFStringEncodingUTF8));
+  EXPECT_EQ(
+      CFStringCompare(matched_family_name.get(), expected_family_name.get(),
+                      kCFCompareCaseInsensitive),
+      kCFCompareEqualTo);
+}
+
+void TestFontMatchingByNameByPostscriptName(const char* font_name) {
+  NSFont* font =
+      MatchNSFontFamily(AtomicString(font_name), 0, kNormalWeightValue, 11);
+  EXPECT_TRUE(font);
+  ScopedCFTypeRef<CFStringRef> matched_postscript_name(
+      CTFontCopyPostScriptName(base::apple::NSToCFPtrCast(font)));
+  ScopedCFTypeRef<CFStringRef> expected_postscript_name(
+      CFStringCreateWithCString(nullptr, font_name, kCFStringEncodingUTF8));
+  EXPECT_EQ(CFStringCompare(matched_postscript_name.get(),
+                            expected_postscript_name.get(),
+                            kCFCompareCaseInsensitive),
+            kCFCompareEqualTo);
+}
+
+}  // namespace
+
+TEST(FontMatcherMacTest, MatchSystemFont) {
+  TestFontWithWeights(font_family_names::kSystemUi);
+  TestFontWithBoldAndItalicTraits(font_family_names::kSystemUi);
+}
+
+TEST(FontMatcherMacTest, FontFamilyMatchingUnavailableFont) {
+  NSFont* font = MatchNSFontFamily(
+      AtomicString(
+          "ThisFontNameDoesNotExist07F444B9-4DDF-4A41-8F30-C80D4ED4CCA2"),
+      0, kNormalWeightValue, 12);
+  EXPECT_FALSE(font);
+}
+
+TEST(FontMatcherMacTest, MatchUniqueUnavailableFont) {
   ScopedCFTypeRef<CTFontRef> font = MatchUniqueFont(
       AtomicString(
           "ThisFontNameDoesNotExist07F444B9-4DDF-4A41-8F30-C80D4ED4CCA2"),
@@ -29,52 +163,53 @@ TEST(FontMatcherMacTest, NoUniqueFontMatchOnUnavailableFont) {
   EXPECT_FALSE(font);
 }
 
-// If these font names are unavailable on future Mac OS versions, please try to
-// find replacements or remove individual lines.
-TEST(FontMatcherMacTest, MatchFullFontName) {
-  const char* font_names[] = {"American Typewriter Condensed Light",
-                              "Arial Narrow Bold Italic",
-                              "Baskerville SemiBold Italic",
-                              "Devanagari MT",
-                              "DIN Alternate Bold",
-                              "Gill Sans Light Italic",
-                              "Iowan Old Style Titling",
-                              "Malayalam Sangam MN",
-                              "Hiragino Maru Gothic Pro W4",
-                              "Hiragino Kaku Gothic StdN W8"};
+class TestFontMatchingByName : public testing::TestWithParam<FontName> {};
 
-  for (const char* font_name : font_names) {
-    @autoreleasepool {
-      ScopedCFTypeRef<CTFontRef> font =
-          MatchUniqueFont(AtomicString(font_name), 12);
-      EXPECT_TRUE(font);
-    }
-  }
+INSTANTIATE_TEST_SUITE_P(FontMatcherMacTest,
+                         TestFontMatchingByName,
+                         testing::ValuesIn(FontNames));
+
+INSTANTIATE_TEST_SUITE_P(CommonFontMatcherMacTest,
+                         TestFontMatchingByName,
+                         testing::ValuesIn(CommonFontNames));
+
+// We perform matching by PostScript name for legacy and compatibility reasons
+// (Safari also does it), although CSS specs do not require that, see
+// crbug.com/641861.
+TEST_P(TestFontMatchingByName, MatchingByFamilyName) {
+  const FontName font_name = TestFontMatchingByName::GetParam();
+  TestFontMatchingByNameByFamilyName(font_name.family_name);
 }
 
-// If these font names are unavailable on future Mac OS versions, please try to
-// find replacements or remove individual lines.
-TEST(FontMatcherMacTest, MatchPostscriptName) {
-  const char* font_names[] = {
-      "AmericanTypewriter-CondensedLight",
-      "ArialNarrow-BoldItalic",
-      "Baskerville-SemiBoldItalic",
-      "DevanagariMT",
-      "DINAlternate-Bold",
-      "GillSans-LightItalic",
-      "IowanOldStyle-Titling",
-      "MalayalamSangamMN",
-      "HiraMaruPro-W4",
-      "HiraKakuStdN-W8",
-  };
+TEST_P(TestFontMatchingByName, MatchingByPostscriptName) {
+  const FontName font_name = TestFontMatchingByName::GetParam();
+  TestFontMatchingByNameByPostscriptName(font_name.postscript_name);
+}
 
-  for (const char* font_name : font_names) {
-    @autoreleasepool {
-      ScopedCFTypeRef<CTFontRef> font =
-          MatchUniqueFont(AtomicString(font_name), 12);
-      EXPECT_TRUE(font);
-    }
-  }
+TEST_P(TestFontMatchingByName, MatchUniqueFontByFullFontName) {
+  const FontName font_name = TestFontMatchingByName::GetParam();
+  ScopedCFTypeRef<CTFontRef> font =
+      MatchUniqueFont(AtomicString(font_name.full_font_name), 12);
+  EXPECT_TRUE(font);
+}
+
+TEST_P(TestFontMatchingByName, MatchUniqueFontByPostscriptName) {
+  const FontName font_name = TestFontMatchingByName::GetParam();
+  ScopedCFTypeRef<CTFontRef> font =
+      MatchUniqueFont(AtomicString(font_name.postscript_name), 12);
+  EXPECT_TRUE(font);
+}
+
+class TestFontWithTraitsMatching : public testing::TestWithParam<const char*> {
+};
+
+INSTANTIATE_TEST_SUITE_P(FontMatcherMacTest,
+                         TestFontWithTraitsMatching,
+                         testing::ValuesIn(FamiliesWithBoldItalicFaces));
+
+TEST_P(TestFontWithTraitsMatching, FontFamilyMatchingWithBoldItalicTraits) {
+  const char* font_name = TestFontWithTraitsMatching::GetParam();
+  TestFontWithBoldAndItalicTraits(AtomicString(font_name));
 }
 
 }  // namespace blink
