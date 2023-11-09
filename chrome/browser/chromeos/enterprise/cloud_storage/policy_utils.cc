2f2e6d0fe7d9ae226f796177c63ff37b397e6ea3
Leon Masopust <lmasopust@google.com>
2023-11-09 08:50:53 +0000

    [Clippy] Add defaults for MicrosoftOneDriveAccountRestrictions
    Add explicit defaults for MicrosoftOneDriveAccountRestrictions
    (no restrictions for consumers, accounts restricted to commercial
    accounts for commercial users) so that this don't need to be
    handled by the extension API or the ODFS extension.
    
    Bug: b:299077762
    Change-Id: I636720d7c127134dab3c5921684d2b92ce284280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5009855
    Reviewed-by: Cris Guerrero Romero <crisguerrero@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Commit-Queue: Leon Masopust <lmasopust@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222139}

--- chrome/browser/chromeos/enterprise/cloud_storage/policy_utils.cc
+++ chrome/browser/chromeos/enterprise/cloud_storage/policy_utils.cc
@@ -4,11 +4,14 @@
 
 #include "chrome/browser/chromeos/enterprise/cloud_storage/policy_utils.h"
 
+#include "base/values.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 
 namespace {
 constexpr char MicrosoftOneDriveMountAllowed[] = "allowed";
+constexpr char MicrosoftOneDriveNoAccountRestriction[] = "common";
+
 }  // namespace
 
 namespace chromeos::cloud_storage {
@@ -16,7 +19,11 @@ namespace chromeos::cloud_storage {
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterStringPref(prefs::kMicrosoftOneDriveMount,
                                MicrosoftOneDriveMountAllowed);
-  registry->RegisterListPref(prefs::kMicrosoftOneDriveAccountRestrictions);
+
+  base::Value::List account_restrictions_default;
+  account_restrictions_default.Append(MicrosoftOneDriveNoAccountRestriction);
+  registry->RegisterListPref(prefs::kMicrosoftOneDriveAccountRestrictions,
+                             std::move(account_restrictions_default));
 }
 
 }  // namespace chromeos::cloud_storage
