87c2920d274df42d54e0eeebe861c3c7961fce77
Kevin Radtke <kradtke@chromium.org>
2023-11-10 18:25:08 +0000

    Migrate ToastManager's SystemToastStyle usages to new SystemToastView
    https://goto.google.com/system-toast-view
    
    Use the new SystemToastView added in http://crrev/c/4894945 to be used
    by the ToastOverlay object created through the ToastManager.
    
    Using this new view fixes an issue with the current SystemToastStyle
    view where the shadows go out of bounds.
    
    screenshot comparisons old vs. new component:
    one-line: https://screenshot.googleplex.com/8ru3aj5mEUPzAiT
    multi-line: https://screenshot.googleplex.com/B6iHEtWT8AjtiSX
    
    Also made the following changes:
    - Pass the `toast_data` parameter from ToastManager instead of sending
    every contained parameter individually.
    - Added `ToggleA11yFocus` and related member variables and functions to
    SystemToastView to match behavior implemented in http://crrev/c/3692579.
    
    Bug: b:309842498
    Change-Id: I306e2da7812eb2e352be2513e485ba0791ce05c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4913276
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223037}

--- ash/system/toast/system_toast_view.h
+++ ash/system/toast/system_toast_view.h
@@ -10,9 +10,14 @@
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/flex_layout_view.h"
 
+namespace views {
+class LabelButton;
+}  // namespace views
+
 namespace ash {
 
 struct ToastData;
+class ScopedA11yOverrideWindowSetter;
 class SystemShadow;
 
 // The System Toast view. (go/toast-style-spec)
@@ -28,12 +33,30 @@ class ASH_EXPORT SystemToastView : public views::FlexLayoutView {
   SystemToastView& operator=(const SystemToastView&) = delete;
   ~SystemToastView() override;
 
+  bool is_dismiss_button_highlighted() const {
+    return is_dismiss_button_highlighted_;
+  }
+
+  views::LabelButton* dismiss_button() const { return dismiss_button_; }
+
+  // Returns true if there's a button and it was highlighted for accessibility.
+  bool ToggleA11yFocus();
+
  private:
+  // Owned by the views hierarchy.
+  raw_ptr<views::LabelButton> dismiss_button_ = nullptr;
+
   std::unique_ptr<SystemShadow> shadow_;
 
   // views::View:
   void AddedToWidget() override;
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
+
+  bool is_dismiss_button_highlighted_ = false;
+
+  // Updates the current a11y override window when the dismiss button is being
+  // highlighted.
+  std::unique_ptr<ScopedA11yOverrideWindowSetter> scoped_a11y_overrider_;
 };
 
 }  // namespace ash
--- ash/system/toast/toast_overlay.cc
+++ ash/system/toast/toast_overlay.cc
@@ -18,7 +18,7 @@
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/pill_button.h"
-#include "ash/style/system_toast_style.h"
+#include "ash/system/toast/system_toast_view.h"
 #include "ash/wm/work_area_insets.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
@@ -146,26 +146,22 @@ class ToastOverlay::ToastHoverObserver : public ui::EventObserver {
 ///////////////////////////////////////////////////////////////////////////////
 //  ToastOverlay
 ToastOverlay::ToastOverlay(Delegate* delegate,
-                           const std::u16string& text,
-                           const std::u16string& dismiss_text,
-                           const gfx::VectorIcon& leading_icon,
-                           base::TimeDelta duration,
-                           bool persist_on_hover,
-                           aura::Window* root_window,
-                           base::RepeatingClosure dismiss_callback)
+                           ToastData& toast_data,
+                           aura::Window* root_window)
     : delegate_(delegate),
-      text_(text),
-      dismiss_text_(dismiss_text),
+      text_(toast_data.text),
+      dismiss_text_(toast_data.dismiss_text),
       overlay_widget_(new views::Widget),
-      overlay_view_(new SystemToastStyle(
-          base::BindRepeating(&ToastOverlay::OnButtonClicked,
-                              base::Unretained(this)),
-          text,
-          dismiss_text,
-          leading_icon)),
       display_observer_(std::make_unique<ToastDisplayObserver>(this)),
       root_window_(root_window),
-      dismiss_callback_(std::move(dismiss_callback)) {
+      dismiss_callback_(std::move(toast_data.dismiss_callback)) {
+  // TODO(b/303253656): Refactor toast dismiss callback so it does not have to
+  // bounce from the view back to ToastOverlay.
+  // The provided `toast_data`'s callback was stored in `dismiss_callback_`.
+  toast_data.dismiss_callback = base::BindRepeating(
+      &ToastOverlay::OnButtonClicked, base::Unretained(this));
+  overlay_view_ = std::make_unique<SystemToastView>(toast_data);
+
   views::Widget::InitParams params;
   params.type = views::Widget::InitParams::TYPE_POPUP;
   params.name = "ToastOverlay";
@@ -189,7 +185,8 @@ ToastOverlay::ToastOverlay(Delegate* delegate,
 
   // Only toasts that expire should be able to persist on hover (i.e. toasts
   // with infinite duration persist regardless of hover).
-  if (persist_on_hover && (duration != ToastData::kInfiniteDuration)) {
+  if (toast_data.persist_on_hover &&
+      (toast_data.duration != ToastData::kInfiniteDuration)) {
     hover_observer_ = std::make_unique<ToastHoverObserver>(
         overlay_window, base::BindRepeating(&ToastOverlay::OnHoverStateChanged,
                                             base::Unretained(this)));
