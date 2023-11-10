134002f0374471b397f0025060987e8e09552c69
Clemens Backes <clemensb@chromium.org>
2023-11-10 13:54:44 +0000

    [wasm] Remove caching threshold on wire byte size
    V8 already has a threshold for when to trigger caching (currently if 1MB
    of TurboFan code was generated). Thus the threshold on the wire byte
    size is redundant.
    As we want to experiment with smaller thresholds in V8 (see linked bug)
    this chromium-side threshold could potentially interfere.
    
    R=ahaas@chromium.org
    
    Bug: v8:14411
    Change-Id: I7e919883ab0722caba84a0ba2564ef3eb764f7da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018581
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1222899}

--- third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
@@ -94,17 +94,6 @@ class WasmCodeCachingCallback {
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"),
                          "v8.wasm.compiledModule", TRACE_EVENT_SCOPE_THREAD,
                          "url", response_url_.Utf8());
-    v8::MemorySpan<const uint8_t> wire_bytes =
-        compiled_module.GetWireBytesRef();
-    // Our heuristic for whether it's worthwhile to cache is that the module
-    // was fully compiled and the size is such that loading from the cache will
-    // improve startup time. Use wire bytes size since it should be correlated
-    // with module size.
-    // TODO(bbudge) This is set very low to compare performance of caching with
-    // baseline compilation. Adjust this test once we know which sizes benefit.
-    const size_t kWireBytesSizeThresholdBytes = 1UL << 10;  // 1 KB.
-    if (wire_bytes.size() < kWireBytesSizeThresholdBytes)
-      return;
     v8::OwnedBuffer serialized_module;
     {
       // Use a standard milliseconds based timer (up to 10 seconds, 50 buckets),
@@ -120,6 +109,8 @@ class WasmCodeCachingCallback {
                          "v8.wasm.cachedModule", TRACE_EVENT_SCOPE_THREAD,
                          "producedCacheSize", serialized_module.size);
 
+    v8::MemorySpan<const uint8_t> wire_bytes =
+        compiled_module.GetWireBytesRef();
     DigestValue wire_bytes_digest;
     {
       TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"),
