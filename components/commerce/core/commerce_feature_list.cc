0fc75000e71b20e4b2383b4078063b2b32710f79
Matt Jones <mdjones@chromium.org>
2023-11-10 22:49:29 +0000

    [iOS] Enable price tracking by default
    This patch enables the region-based "shopping list" flag for iOS.
    
    Bug: b:296799795
    Change-Id: I320ef8a3da686f51eeb5ea763be4db47269ded31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021213
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223175}

--- components/commerce/core/commerce_feature_list.cc
+++ components/commerce/core/commerce_feature_list.cc
@@ -225,7 +225,7 @@ BASE_FEATURE(kShoppingCollection,
 
 BASE_FEATURE(kShoppingList, "ShoppingList", base::FEATURE_DISABLED_BY_DEFAULT);
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || \
-    BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+    BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
 BASE_FEATURE(kShoppingListRegionLaunched,
              "ShoppingListRegionLaunched",
              base::FEATURE_ENABLED_BY_DEFAULT);
