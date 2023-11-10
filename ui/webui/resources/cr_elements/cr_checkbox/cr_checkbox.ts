324df1c405ffc1f17e78126d8ffa16669a19cabe
Wes Okuhara <wesokuhara@google.com>
2023-11-10 18:22:58 +0000

    CrOS cr-elements: Update cr-checkbox styles
    Context: go/cr2023-cros-styles
    
    Light mode:
    - Checked, unchecked: http://screen/9iJesZshAbze2Gx.png
    - Disabled: http://screen/9iJesZshAbze2Gx.png
    - Focused: http://screen/9iJesZshAbze2Gx.png
    - Hovered: http://screen/8g3QMr5jnaoztZG.png
    
    Dark mode:
    - Checked, unchecked: http://screen/5STzEdhngtZjEzM.png
    - Disabled: http://screen/5STzEdhngtZjEzM.png
    - Focused: http://screen/5STzEdhngtZjEzM.png
    - Hovered: http://screen/AD8xuuxbuEGjGfi.png
    
    Bug: b:309170780
    Test: Manually verify no regressions in cr-checkbox style
    Change-Id: I7ece116cc1a82d8113da72bfb0bc93a6890bc20b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5015163
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223036}

--- ui/webui/resources/cr_elements/cr_checkbox/cr_checkbox.ts
+++ ui/webui/resources/cr_elements/cr_checkbox/cr_checkbox.ts
@@ -92,6 +92,17 @@ export class CrCheckboxElement extends CrCheckboxElementBase {
 
   override ready() {
     super.ready();
+
+    // <if expr="chromeos_ash">
+    // TODO(b/309689294) Remove this once CrOS UIs migrate to Jellybean
+    // components and no longer use cr-elements.
+    // Force stamp the ripple element to enable CrOS focus styles. Ripple
+    // visibility is controlled by the event listeners below.
+    if (document.documentElement.hasAttribute('chrome-refresh-2023')) {
+      this.getRipple();
+    }
+    // </if>
+
     this.removeAttribute('unresolved');
     this.addEventListener('click', this.onClick_.bind(this));
     this.addEventListener('pointerup', this.hideRipple_.bind(this));
