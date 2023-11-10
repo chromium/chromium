ffd081d09b51e8956dec8041c0082811af49abcc
Ameur Hosni <ameurhosni@google.com>
2023-11-10 10:25:40 +0000

    [IOS][1/3] Move ios/chrome/browser/history to model subfolder
    Creates upstream headers to update downstream deps before moving.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/history will be moved to
    ios/c/b/history/model/ to showcase that they are model files.
    
    Bug: 1480308
    Change-Id: Ib983271edf492c1854e02b599bc2ed9535a0db10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5016585
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1222834}

---
+++ ios/chrome/browser/history/model/history_service_factory.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_HISTORY_MODEL_HISTORY_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_HISTORY_MODEL_HISTORY_SERVICE_FACTORY_H_
+
+#import "ios/chrome/browser/history/history_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_HISTORY_MODEL_HISTORY_SERVICE_FACTORY_H_
