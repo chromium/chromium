5aaf90352b5b1b787d6ca61e6dccb5dcb771578e
Hsin-chen Chuang <chharry@chromium.org>
2023-11-11 04:29:21 +0000

    [M120][floss] Block the callbacks related to pairing in LaCrOS
    Block the callbacks in LaCrOS for now so it won't race with AshChrome.
    
    BUG=b:308988818
    TEST=Start PWA and pair device
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5004605
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Commit-Queue: Michael Sun <michaelfsun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1220735}
    (cherry picked from commit ff8ce4b5195103b956cb27179450ceee07f15129)
    
    Change-Id: I47601187d39e790a1a0020bf0d9361be5e18add8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5019186
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>
    Cr-Commit-Position: refs/branch-heads/6099@{#526}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- device/bluetooth/floss/floss_adapter_client.cc
+++ device/bluetooth/floss/floss_adapter_client.cc
@@ -492,10 +492,15 @@ void FlossAdapterClient::OnSspRequest(
     return;
   }
 
+  // Block the event in LaCrOS so it won't race with AshChrome. See b/308988818.
+  // TODO(b/274706838): Redesign DBus API so it's only received by the correct
+  // client.
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
   for (auto& observer : observers_) {
     observer.AdapterSspRequest(
         device, cod, static_cast<BluetoothSspVariant>(variant), passkey);
   }
+#endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
   std::move(response_sender).Run(dbus::Response::FromMethodCall(method_call));
 }
@@ -514,9 +519,14 @@ void FlossAdapterClient::OnPinDisplay(
     return;
   }
 
+  // Block the event in LaCrOS so it won't race with AshChrome. See b/308988818.
+  // TODO(b/274706838): Redesign DBus API so it's only received by the correct
+  // client.
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
   for (auto& observer : observers_) {
     observer.AdapterPinDisplay(device, pincode);
   }
+#endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
   std::move(response_sender).Run(dbus::Response::FromMethodCall(method_call));
 }
@@ -536,9 +546,14 @@ void FlossAdapterClient::OnPinRequest(
     return;
   }
 
+  // Block the event in LaCrOS so it won't race with AshChrome. See b/308988818.
+  // TODO(b/274706838): Redesign DBus API so it's only received by the correct
+  // client.
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
   for (auto& observer : observers_) {
     observer.AdapterPinRequest(device, cod, min_16_digit);
   }
+#endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
   std::move(response_sender).Run(dbus::Response::FromMethodCall(method_call));
 }
