df7e4920f413f02a73e5d4c81cc41fadfca09107
James Cook <jamescook@chromium.org>
2023-11-13 18:33:32 +0000

    chromeos: Add test CameraAppIntegrationTest.CCAAPI
    As part of the go/crosier effort we're removing Tast tests from the
    chromium commit queue. This CL ports the Tast test camera.CCAAPI, which
    verifies that private JavaScript APIs are available to the Chrome Camera
    App. This does not appear to have any OS/platform dependencies, so add
    the test to an existing browser_tests suite for the camera app.
    
    Bug: b:310037161
    Test: browser_tests --gtest_filter=*CameraAppIntegrationTest*
    Change-Id: I168794572ecb6bca011822afa70da8ddc4d6be8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018514
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223750}

--- chrome/browser/ash/system_web_apps/apps/camera_app/camera_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/camera_app/camera_app_integration_browsertest.cc
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/constants/ash_features.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/system_web_apps/test_support/system_web_app_browsertest_base.h"
 #include "chrome/browser/ash/system_web_apps/test_support/system_web_app_integration_test.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_navigation_observer.h"
 
@@ -50,6 +49,45 @@ IN_PROC_BROWSER_TEST_P(CameraAppIntegrationTest, OtherPageUrlNavigation) {
   EXPECT_EQ(
       other_page_camera_app_url,
       browser()->tab_strip_model()->GetActiveWebContents()->GetVisibleURL());
+}
+
+// CCAAPI verifies whether the private JavaScript APIs CCA (Chrome camera app)
+// relies on work as expected. The APIs under testing are not owned by CCA team.
+// This test prevents changes to those APIs' implementations from silently
+// breaking CCA.
+// Contacts: chromeos-camera-eng@google.com, wtlee@chromium.org
+// Ported this test from Tast: jamescook@chromium.org
+// Bug Component: 978428
+// Bug Component: ChromeOS > Platform > Technologies > Camera > App & Framework
+IN_PROC_BROWSER_TEST_P(CameraAppIntegrationTest, CCAAPI) {
+  WaitForTestSystemAppInstall();
+
+  // Load the test page.
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(), GURL("chrome://camera-app/test/test.html")));
+  EXPECT_EQ(1u, chrome::GetTotalBrowserCount());
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  ASSERT_TRUE(web_contents);
+
+  // Assert that the window.FileSystemHandle API exists.
+  auto result =
+      content::EvalJs(web_contents, "window.FileSystemHandle !== undefined");
+  bool has_window_file_system_handle = result.ExtractBool();
+  EXPECT_TRUE(has_window_file_system_handle);
+
+  // Load the untrusted script loader.
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(
+      browser(),
+      GURL("chrome://camera-app/views/untrusted_script_loader.html")));
+  EXPECT_EQ(1u, chrome::GetTotalBrowserCount());
+  web_contents = browser()->tab_strip_model()->GetActiveWebContents();
+
+  // Assert that the window.launchQueue API exists.
+  auto result2 =
+      content::EvalJs(web_contents, "window.launchQueue !== undefined");
+  bool has_window_launch_queue = result2.ExtractBool();
+  EXPECT_TRUE(has_window_launch_queue);
 }
 
 INSTANTIATE_SYSTEM_WEB_APP_MANAGER_TEST_SUITE_REGULAR_PROFILE_P(
