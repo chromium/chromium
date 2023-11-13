069e2e3338e9a2610b5143e506eedbbbf24b463f
Peter Boström <pbos@chromium.org>
2023-11-13 22:47:23 +0000

    Downgrade NOTREACHEDs in OnControlleeRemoved
    This downgrades NOTREACHEDs inside
    performance_manager::ServiceWorkerContextAdapter::OnControlleeRemoved
    which is one of the top report signatures on Windows M119.
    
    Downgrading a NOTREACHED to DUMP_WILL_BE_NOTREACHED_NORETURN excludes it
    from the kNotReachedIsFatal experiment and avoids stability risk during
    experiment rollout.
    
    Bug: 851128, 1501964
    Change-Id: I4dadc1605a326b3767c11e505ff7201bf6e01425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026499
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223927}

--- components/performance_manager/service_worker_context_adapter.cc
+++ components/performance_manager/service_worker_context_adapter.cc
@@ -351,13 +351,13 @@ void ServiceWorkerContextAdapter::OnControlleeRemoved(
   // notification is dropped.
   auto it = service_worker_clients_.find(version_id);
   if (it == service_worker_clients_.end()) {
-    NOTREACHED();
+    DUMP_WILL_BE_NOTREACHED_NORETURN();
     return;
   }
 
   size_t removed = it->second.erase(client_uuid);
   if (!removed) {
-    NOTREACHED();
+    DUMP_WILL_BE_NOTREACHED_NORETURN();
     return;
   }
 
