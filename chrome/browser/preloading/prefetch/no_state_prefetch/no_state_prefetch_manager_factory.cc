49b16882835e3817979866314fd4bd51784966a6
David Bertoni <dbertoni@chromium.org>
2023-11-13 20:27:36 +0000

    [Extensions] Fix indirect KeyedService dependency.
    NoStatePrefetchManagerFactory indirectly depends on the
    extensions::RulesRegistryService through extension::TabHelper.
    
    This fixes a UAF in the RulesRegistryService during profile shutdown.
    
    (cherry picked from commit 03cb3ef4523655b1f9cd87039c60b95fa88366a5)
    
    Bug: 1494580
    Change-Id: I434702083f4d4e8be1d139510b275f2aebaf0d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5006127
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1220090}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024489
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6099@{#608}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.cc
+++ chrome/browser/preloading/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.cc
@@ -14,6 +14,7 @@
 #include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "extensions/browser/api/declarative/rules_registry_service.h"
 #include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extensions_browser_client.h"
 #endif
@@ -46,6 +47,9 @@ NoStatePrefetchManagerFactory::NoStatePrefetchManagerFactory()
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   DependsOn(
       extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
+  // NoStatePrefetchService has an indirect dependency on the
+  // RulesRegistryService through extensions::TabHelper::WebContentsDestroyed.
+  DependsOn(extensions::RulesRegistryService::GetFactoryInstance());
 #endif
   // PrerenderLocalPredictor observers the history visit DB.
   DependsOn(HistoryServiceFactory::GetInstance());
