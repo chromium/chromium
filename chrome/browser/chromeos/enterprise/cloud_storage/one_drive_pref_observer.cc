72b10b0ce1f976af169034ae0cc3fa5489939b43
Simon Hangl <simonha@google.com>
2023-11-13 15:26:54 +0000

    Add OneDrivePrefObserver keyed service skeleton
    This CL adds the skeleton of the keyed service that will be
    used for the enterprise integration of Microsoft OneDrive.
    It will be used to listen to OneDrive related policies and
    forward this information to the ODFS extension.
    
    Bug: b/294983416
    Change-Id: I60d3263ba196998ca07efbd9fdc23862b133ad5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5023999
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223634}

---
+++ chrome/browser/chromeos/enterprise/cloud_storage/one_drive_pref_observer.cc
@@ -0,0 +1,106 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/enterprise/cloud_storage/one_drive_pref_observer.h"
+
+#include "base/notreached.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_selections.h"
+#include "chrome/common/pref_names.h"
+#include "chromeos/constants/chromeos_features.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "components/prefs/pref_change_registrar.h"
+#include "content/public/browser/browser_context.h"
+#include "extensions/browser/extensions_browser_client.h"
+
+namespace chromeos::cloud_storage {
+
+namespace {
+
+// This class is responsible for watching the prefs for a particular profile.
+class OneDrivePrefObserver : public KeyedService {
+ public:
+  ~OneDrivePrefObserver() override;
+
+  static std::unique_ptr<OneDrivePrefObserver> Create(Profile* profile);
+
+  // KeyedService:
+  void Shutdown() override;
+
+ private:
+  explicit OneDrivePrefObserver(Profile* profile);
+
+  // Sets up watchers.
+  void Init();
+
+  // Serves as callback for pref changes.
+  void OnMicrosoftOneDriveMountPrefChanged();
+
+  raw_ptr<Profile> profile_ = nullptr;
+
+  // The registrar used to watch prefs changes.
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
+};
+
+OneDrivePrefObserver::OneDrivePrefObserver(Profile* profile)
+    : profile_(profile),
+      pref_change_registrar_(std::make_unique<PrefChangeRegistrar>()) {}
+
+OneDrivePrefObserver::~OneDrivePrefObserver() = default;
+
+std::unique_ptr<OneDrivePrefObserver> OneDrivePrefObserver::Create(
+    Profile* profile) {
+  auto watcher = base::WrapUnique(new OneDrivePrefObserver(profile));
+  watcher->Init();
+  return watcher;
+}
+
+void OneDrivePrefObserver::Init() {
+  pref_change_registrar_->Init(profile_->GetPrefs());
+  pref_change_registrar_->Add(
+      prefs::kMicrosoftOneDriveMount,
+      base::BindRepeating(
+          &OneDrivePrefObserver::OnMicrosoftOneDriveMountPrefChanged,
+          base::Unretained(this)));
+}
+
+void OneDrivePrefObserver::Shutdown() {
+  pref_change_registrar_.reset();
+}
+
+void OneDrivePrefObserver::OnMicrosoftOneDriveMountPrefChanged() {
+  // TODO(b/294983416): Implement this change listener.
+  NOTREACHED();
+}
+
+}  // namespace
+
+OneDrivePrefObserverFactory* OneDrivePrefObserverFactory::GetInstance() {
+  static base::NoDestructor<OneDrivePrefObserverFactory> instance;
+  return instance.get();
+}
+
+OneDrivePrefObserverFactory::OneDrivePrefObserverFactory()
+    : ProfileKeyedServiceFactory("OneDrivePrefObserverFactory",
+                                 ProfileSelections::BuildForRegularProfile()) {}
+
+OneDrivePrefObserverFactory::~OneDrivePrefObserverFactory() = default;
+
+std::unique_ptr<KeyedService>
+OneDrivePrefObserverFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  if (!chromeos::features::IsUploadOfficeToCloudEnabled() ||
+      !chromeos::features::
+          IsMicrosoftOneDriveIntegrationForEnterpriseEnabled()) {
+    return nullptr;
+  }
+  return OneDrivePrefObserver::Create(Profile::FromBrowserContext(context));
+}
+
+bool OneDrivePrefObserverFactory::ServiceIsCreatedWithBrowserContext() const {
+  return true;
+}
+
+}  // namespace chromeos::cloud_storage
