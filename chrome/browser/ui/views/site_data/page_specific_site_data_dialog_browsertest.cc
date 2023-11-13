bac5913594a1e141aaef8af2adabfbd038e49898
Fiona Macintosh <fmacintosh@google.com>
2023-11-13 17:06:03 +0000

    Disable 3PCD for PageSpecificSiteDataDialogBrowserTest 3PC allowed case
    Rename to PartitionedCookiesAndAllowedThirdParty to better indicate
    difference from subsequent PartitionedCookiesAndBlockedThirdParty
    
    Bug: b:306414714
    Change-Id: Ia9a34dd0368ae6ea854685a4234184cd39b1d0ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5025295
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223694}

--- chrome/browser/ui/views/site_data/page_specific_site_data_dialog_browsertest.cc
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog_browsertest.cc
@@ -6,6 +6,7 @@
 
 #include "base/memory/raw_ptr.h"
 #include "base/test/metrics/user_action_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -17,6 +18,7 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/browsing_data/core/browsing_data_utils.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/page_info/core/features.h"
@@ -415,8 +417,20 @@ IN_PROC_BROWSER_TEST_F(PageSpecificSiteDataDialogBrowserTest,
   histograms.ExpectTotalCount(kCookiesDialogHistogramName, 2);
 }
 
-IN_PROC_BROWSER_TEST_F(PageSpecificSiteDataDialogBrowserTest,
-                       PartitionedCookies) {
+class PageSpecificSiteDataDialogPre3pcdBrowserTest
+    : public PageSpecificSiteDataDialogBrowserTest {
+ public:
+  PageSpecificSiteDataDialogPre3pcdBrowserTest() {
+    feature_list_.InitAndDisableFeature(
+        content_settings::features::kTrackingProtection3pcd);
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(PageSpecificSiteDataDialogPre3pcdBrowserTest,
+                       PartitionedCookiesAndAllowedThirdParty) {
   content::CookieChangeObserver observer(
       browser()->tab_strip_model()->GetActiveWebContents(), 8);
 
