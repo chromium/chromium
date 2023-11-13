85f9ad7ec2ab299df024ee7442b8429b9efc0ad2
Richard Chen <richche@chromium.org>
2023-11-13 21:11:12 +0000

    DisableOffstoreExtensions 100% Stable launch
    - Enable feature by default
    - Remove entry in fieldtrial_testing_config.json
    
    Bug: 1466149
    Change-Id: If6329fc070bdb7883fcc079da4d1fbfdb79fa25f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5024682
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223848}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -144,7 +144,7 @@ BASE_FEATURE(kExtensionTelemetryTabsExecuteScriptSignal,
 
 BASE_FEATURE(kExtensionTelemetryDisableOffstoreExtensions,
              "SafeBrowsingExtensionTelemetryDisableOffstoreExtensions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFriendlierSafeBrowsingSettingsEnhancedProtection,
              "FriendlierSafeBrowsingSettingsEnhancedProtection",
