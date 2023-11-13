d32aa042138eb77cae6b306172f36d73a9e9c277
Cheick Cisse <cheickcisse@google.com>
2023-11-13 16:22:37 +0000

    [WhatsNew] Add git commands to script
    This CL adds git commands to create and upload a CL with the new What's New features.
    
    Change-Id: Ifdfeb686f5e9c8a595e40ebbc20b2669009db2b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5002685
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1223664}

--- tools/whats_new/upload_whats_new_features.py
+++ tools/whats_new/upload_whats_new_features.py
@@ -18,10 +18,80 @@ import pandas as pd
 import sys
 import whats_new_util
 import argparse
+import time
 
 
-def _AddFeature(feature_dict: dict[str, str],
+def _CreateBranch(milestone: str) -> None:
+    try:
+        start = time.time()
+        command_git = ['git', 'checkout', '-b', f'whats_new_{milestone}']
+        subprocess.run(command_git, check=True)
+        end = time.time()
+        execution_time = _GetExecutionTime(start, end)
+        print(f'The time of execution to create a branch: {execution_time}')
+    except subprocess.CalledProcessError as exc:
+        raise Exception(f'Failed to create a branch: {exc}')
+
+
+def _FormatChanges() -> None:
+    try:
+        start = time.time()
+        command_git = ['git', 'cl', 'format']
+        subprocess.run(command_git, check=True)
+        end = time.time()
+        execution_time = _GetExecutionTime(start, end)
+        print(f'The time of execution to format the changes: {execution_time}')
+    except subprocess.CalledProcessError as exc:
+        raise Exception(f'Failed to format changes: {exc}')
+
+
+def _AddChangesToBranch() -> None:
+    command_git = ['git', 'add', '-A']
+    try:
+        start = time.time()
+        subprocess.run(command_git, check=True)
+        end = time.time()
+        execution_time = _GetExecutionTime(start, end)
+        print('The time of execution to add the changes to the branch: '
+              f'{execution_time}')
+    except subprocess.CalledProcessError as exc:
+        raise Exception(f'Failed to add changes to a branch: {exc}')
+
+
+def _CommitChangesToBranch() -> None:
+    try:
+        start = time.time()
+        command_git = [
+            'git', 'commit', '-m', 'Add new features to What\'s New'
+        ]
+        subprocess.run(command_git, check=True)
+        end = time.time()
+        execution_time = _GetExecutionTime(start, end)
+        print('The time of execution to commit the changes to the branch: '
+              f'{execution_time}')
+    except subprocess.CalledProcessError as exc:
+        raise Exception(f'Failed to commit the changes to a branch: {exc}')
+
+
+def _UploadChangesToCL(message: str) -> None:
+    try:
+        start = time.time()
+        command_git = [
+            'git', 'cl', 'upload', '--bypass-hooks', '--bypass-watchlists',
+            '--force', '--message', message, '--cq-dry-run', '--auto-submit'
+        ]
+        subprocess.run(command_git, check=True)
+        end = time.time()
+        execution_time = _GetExecutionTime(start, end)
+        print('The time of execution to upload the changes to a cl: '
+              f'{execution_time}')
+    except subprocess.CalledProcessError as exc:
+        raise Exception(f'Failed to upload the changes to a branch: {exc}')
+
+
+def _AddFeature(index: int, feature_dict: dict[str, str],
                 path_to_milestone_folder: str) -> None:
+    start = time.time()
     new_enum = whats_new_util.UpdateWhatsNewItemAndGetNewTypeValue(
         feature_dict)
     whats_new_util.UpdateWhatsNewPlist(feature_dict, new_enum)
@@ -31,9 +101,18 @@ def _AddFeature(feature_dict: dict[str, str],
     whats_new_util.UpdateResourcesBuildFile(feature_dict)
     whats_new_util.AddStrings(feature_dict, path_to_milestone_folder)
     whats_new_util.UploadScreenshots(feature_dict, path_to_milestone_folder)
+    end = time.time()
+    execution_time = _GetExecutionTime(start, end)
+    print(f'The time of execution to add feature {index}: {execution_time}')
+
+
+def _GetExecutionTime(start: float, end: float) -> str:
+    return f'{(end-start)*10**3:.03f}ms'
 
 
 def main():
+    start = time.time()
+    print('Start...')
     parser = parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -49,17 +128,34 @@ def main():
     xlsx_file_path = os.path.join(milestone_folder_absolute_path,
                                   'whats_new_features.xlsx')
     xlsx_content = pd.read_excel(xlsx_file_path)
+    milestone = xlsx_content.iloc[0]['Milestone']
+
+    #Create branch
+    _CreateBranch(milestone.lower())
+
     # Delete existing features from the plist
     whats_new_util.CleanUpFeaturesPlist()
+    features_name = []
     for index, feature_row in pd.DataFrame(xlsx_content).iterrows():
-        _AddFeature(feature_row, milestone_folder_absolute_path)
-    command_git_format = ['git', 'cl', 'format']
-    error = subprocess.Popen(command_git_format,
-                             universal_newlines=True,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE).communicate()
-    if error:
-        print(error)
+        _AddFeature(index, feature_row, milestone_folder_absolute_path)
+        features_name.append(feature_row['Feature name'])
+
+    # Prepare and upload changes
+    _FormatChanges()
+    _AddChangesToBranch()
+    _CommitChangesToBranch()
+
+    comment_builder = []
+    comment_builder.append(
+        f'[iOS][WhatsNew] Add new features for {milestone}\n')
+    comment_builder.append(
+        'This CL adds the following features to What\'s New:')
+    comment_builder.append('\n'.join(features_name))
+    _UploadChangesToCL('\n'.join(comment_builder))
+
+    end = time.time()
+    execution_time = _GetExecutionTime(start, end)
+    print(f'The total time of execution: {execution_time}')
 
 
 if __name__ == '__main__':
