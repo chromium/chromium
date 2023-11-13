3325fccb680295570ab3332ec5a5857e2e36a610
Fiona Macintosh <fmacintosh@google.com>
2023-11-13 06:29:43 +0000

    Explicitly disable 3PCD in HintsFetcherDoesntFetchOnNSP test case
    This test uses no state prefetching, which doesn't work without 3PC:
    https://source.chromium.org/chromium/chromium/src/+/main:components/no_state_prefetch/browser/no_state_prefetch_manager.cc;l=593;drc=af1d46cb4d0220b6f042d7c741c54fc4de1dd965
    
    Bug: b:306414714
    Change-Id: I3a66eeb602e4eb5946142f2a46c8070f1f26cfa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5022555
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1223481}

--- chrome/browser/optimization_guide/hints_fetcher_browsertest.cc
+++ chrome/browser/optimization_guide/hints_fetcher_browsertest.cc
@@ -30,6 +30,7 @@
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/google/core/common/google_switches.h"
 #include "components/google/core/common/google_util.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
@@ -538,7 +539,8 @@ class HintsFetcherBrowserTest : public HintsFetcherDisabledBrowserTest {
     PopulateEnabledFeatures(&enabled_features);
 
     // Enable OptimizationHintsFetching with |kRemoteOptimizationGuideFetching|.
-    scoped_feature_list_.InitWithFeaturesAndParameters(enabled_features, {});
+    scoped_feature_list_.InitWithFeaturesAndParameters(enabled_features,
+                                                       disabled_features_);
     // Call to inherited class to match same set up with feature flags added.
     HintsFetcherDisabledBrowserTest::SetUp();
   }
@@ -574,6 +576,9 @@ class HintsFetcherBrowserTest : public HintsFetcherDisabledBrowserTest {
             urls, optimization_types,
             optimization_guide::proto::CONTEXT_BOOKMARKS, callback);
   }
+
+ protected:
+  std::vector<base::test::FeatureRef> disabled_features_;
 };
 
 // This test creates new browser with no profile and loads a random page with
@@ -1264,7 +1269,16 @@ IN_PROC_BROWSER_TEST_F(
   }
 }
 
-IN_PROC_BROWSER_TEST_F(HintsFetcherBrowserTest, HintsFetcherDoesntFetchOnNSP) {
+class HintsFetcherPre3pcdBrowserTest : public HintsFetcherBrowserTest {
+ public:
+  HintsFetcherPre3pcdBrowserTest() {
+    disabled_features_.push_back(
+        content_settings::features::kTrackingProtection3pcd);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(HintsFetcherPre3pcdBrowserTest,
+                       HintsFetcherDoesntFetchOnNSP) {
   const base::HistogramTester* histogram_tester = GetHistogramTester();
 
   // Allowlist NoScript for https_url()'s' host.
