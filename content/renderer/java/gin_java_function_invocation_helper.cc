69f6d21137961510e9fde48a3707795ca439c3a2
Dave Tapuska <dtapuska@chromium.org>
2023-11-14 18:38:29 +0000

    [gin_java_bridge] Deal with the fact the remote object closed
    Code prior to mojom based bridge did not initialize error value
    correctly which caused a CheckError when the mojo object failed
    to send the InvokeMethod call. Check the return code and assign
    the correct initialization value to error.
    
    Bug: 1501450
    Change-Id: I001bb6dc370dab1835e7b649eed5b9b2bbccef86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029575
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224410}

--- content/renderer/java/gin_java_function_invocation_helper.cc
+++ content/renderer/java/gin_java_function_invocation_helper.cc
@@ -73,15 +73,19 @@ v8::Local<v8::Value> GinJavaFunctionInvocationHelper::Invoke(
     }
   }
 
-  mojom::GinJavaBridgeError error;
+  mojom::GinJavaBridgeError error =
+      mojom::GinJavaBridgeError::kGinJavaBridgeNoError;
 
   std::unique_ptr<base::Value> result;
   if (auto* remote = object->GetRemote()) {
     base::Value::List result_wrapper;
-    remote->InvokeMethod(method_name_, std::move(arguments), &error,
-                         &result_wrapper);
-    if (!result_wrapper.empty()) {
-      result = base::Value::ToUniquePtrValue(result_wrapper[0].Clone());
+    if (remote->InvokeMethod(method_name_, std::move(arguments), &error,
+                             &result_wrapper)) {
+      if (!result_wrapper.empty()) {
+        result = base::Value::ToUniquePtrValue(result_wrapper[0].Clone());
+      }
+    } else {
+      error = mojom::GinJavaBridgeError::kGinJavaBridgeObjectIsGone;
     }
   } else {
     result = dispatcher_->InvokeJavaMethod(object->object_id(), method_name_,
