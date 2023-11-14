6acedb4c296553696dcb4d4ec8310df951084c3b
Ameur Hosni <ameurhosni@google.com>
2023-11-14 17:19:27 +0000

    [IOS][1/3] Move ios/chrome/browser/favicon to model subfolder
    Creates upstream headers to update downstream deps before moving.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/favicon will be moved to
    ios/c/b/favicon/model/ to showcase that they are model files.
    
    Bug: 1480299
    Change-Id: Ice18fec5c12987af8d8a19d4957f8713a5e79423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012794
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224358}

---
+++ ios/chrome/browser/favicon/model/ios_chrome_favicon_loader_factory.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_FAVICON_LOADER_FACTORY_H_
+#define IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_FAVICON_LOADER_FACTORY_H_
+
+#import "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_FAVICON_LOADER_FACTORY_H_
---
+++ ios/chrome/browser/favicon/model/ios_chrome_large_icon_service_factory.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_LARGE_ICON_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_LARGE_ICON_SERVICE_FACTORY_H_
+
+#import "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_FAVICON_MODEL_IOS_CHROME_LARGE_ICON_SERVICE_FACTORY_H_
