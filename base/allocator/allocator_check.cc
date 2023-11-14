09403a8abb9a2bb4c99d1161cdcb5b56a305c387
Sylvain Defresne <sdefresne@chromium.org>
2023-11-14 11:35:38 +0000

    [apple] Fix compilation when `use_allocator_shim=false`
    The namespace `allocator_shim` is not-defined if `use_allocator_shim`
    gn variable is set to false, causing failure when compiling the file
    //base/allocator/allocator_check.cc.
    
    The issue was introduced by https://crrev.com/c/5020203 and cause
    some internal bots to fail as they set `use_allocator_shim=false`.
    
    Add a check for `BUILDFLAG(USE_ALLOCATOR_SHIM)` on Apple platform (as
    is done on Windows platform).
    
    Bug: none
    Change-Id: Ibddbaf59c3758d6f0655451783822d3facfe22a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028358
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224200}

--- base/allocator/allocator_check.cc
+++ base/allocator/allocator_check.cc
@@ -27,7 +27,7 @@ bool IsAllocatorInitialized() {
   // shimmed _set_new_mode() is called.
   return allocator_shim::g_is_win_shim_layer_initialized;
 #elif BUILDFLAG(IS_APPLE) && !defined(MEMORY_TOOL_REPLACES_ALLOCATOR) && \
-    !BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+    !BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && BUILDFLAG(USE_ALLOCATOR_SHIM)
   // From allocator_interception_mac.mm.
   return allocator_shim::g_replaced_default_zone;
 #else
