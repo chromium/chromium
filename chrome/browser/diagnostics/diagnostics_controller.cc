a7b235ee80c2204557750ecc1b3a014cabf28cd5
Jack Yammine <jyammine@google.com>
2023-11-14 14:07:13 +0000

    [CodeHealth] Remove expired histogram Diagnostics.RecoveryRun
    OBSOLETE_HISTOGRAM[Diagnostics.RecoveryRun]= The
    histogram is expired as of M77.
    
    Fixed: 1448176
    Change-Id: Ie4de217b0770c502994bdb48f6fb147e96949da3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018125
    Reviewed-by: Carlos Pizano <cpu@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224252}

--- chrome/browser/diagnostics/diagnostics_controller.cc
+++ chrome/browser/diagnostics/diagnostics_controller.cc
@@ -8,14 +8,12 @@
 #include <string>
 
 #include "base/command_line.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/diagnostics/diagnostics_model.h"
 #include "chrome/browser/diagnostics/diagnostics_test.h"
 #include "chrome/browser/diagnostics/diagnostics_writer.h"
-#include "chrome/common/chrome_switches.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_switches.h"
@@ -43,11 +41,6 @@ void DiagnosticsController::ClearResults() { model_.reset(); }
 
 void DiagnosticsController::RecordRegularStartup() {
 #if BUILDFLAG(IS_CHROMEOS_ASH)  // Only collecting UMA stats on ChromeOS
-  // Count the number of normal starts, so we can compare that with the number
-  // of recovery runs to get a percentage.
-  UMA_HISTOGRAM_ENUMERATION(
-      "Diagnostics.RecoveryRun", RECOVERY_NOT_RUN, RECOVERY_RUN_METRICS_COUNT);
-
   // For each of the test types, record a normal start (no diagnostics run), so
   // we have a common denominator.
   for (int i = 0; i < DIAGNOSTICS_TEST_ID_COUNT; ++i) {
@@ -73,20 +66,6 @@ int DiagnosticsController::Run(const base::CommandLine& command_line,
 // been initialized, so be careful what you use.
 int DiagnosticsController::RunRecovery(const base::CommandLine& command_line,
                                        DiagnosticsWriter* writer) {
-// Separate out recoveries that we execute automatically as a result of a
-// crash from user-run recoveries.
-#if BUILDFLAG(IS_CHROMEOS_ASH)  // Only collecting UMA stats on ChromeOS
-  if (command_line.HasSwitch(ash::switches::kLoginUser)) {
-    UMA_HISTOGRAM_ENUMERATION("Diagnostics.RecoveryRun",
-                              diagnostics::RECOVERY_CRASH_RUN,
-                              diagnostics::RECOVERY_RUN_METRICS_COUNT);
-  } else {
-    UMA_HISTOGRAM_ENUMERATION("Diagnostics.RecoveryRun",
-                              diagnostics::RECOVERY_USER_RUN,
-                              diagnostics::RECOVERY_RUN_METRICS_COUNT);
-  }
-#endif
-
   if (!HasResults()) {
     if (writer) {
       writer->WriteInfoLine("No diagnostics have been run.");
