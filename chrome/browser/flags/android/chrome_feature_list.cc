61536bc6bcee61ea084932aefeb9ce051298e6b8
Brandon Fong <bjfong@google.com>
2023-11-14 21:19:40 +0000

    [X-Device Restore] Launch feature to 100% stable
    Change the feature flag config to enabled by default for launch to 100% stable.
    
    Bug: 1426921
    Change-Id: I5d431df999a93de8311276d5a331044f7e37e2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5029700
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224538}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1003,7 +1003,7 @@ BASE_FEATURE(kRequestDesktopSiteDefaultsLogging,
 
 BASE_FEATURE(kRestoreTabsOnFRE,
              "RestoreTabsOnFRE",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kScrollToTLDOptimization,
              "ScrollToTLDOptimization",
