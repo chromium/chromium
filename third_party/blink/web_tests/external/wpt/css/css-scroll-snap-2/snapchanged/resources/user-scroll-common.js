23f4079ea7cc56643c2558e47345095e4f711520
David Awogbemila <awogbemila@chromium.org>
2023-11-14 16:09:28 +0000

    Pull common snap event wpt code together (part 1)
    Much of the test code written for snapchanged can be reused for
    snapchanging. This is the first patch in a 2-patch attempt to put the
    common code in one place: css/css-scroll-snap-2/resources/common.js so
    that the functions can simply be called with either "snapchanged" or
    "snapchanging" when we are adding tests for snapchanging.
    
    The attempt is separated into two parts so it's easier to see what changes are being made to the functions (part 1) vs code that's just being moved to a common location (part 2).
    
    Bug: 1494892, 1456339
    Change-Id: Ia93cf8edf73e321a5a45a20b93c25f288eaab945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026336
    Commit-Queue: David Awogbemila <awogbemila@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224317}

--- third_party/blink/web_tests/external/wpt/css/css-scroll-snap-2/snapchanged/resources/user-scroll-common.js
+++ third_party/blink/web_tests/external/wpt/css/css-scroll-snap-2/snapchanged/resources/user-scroll-common.js
@@ -2,9 +2,9 @@
 
 // This performs a touch scroll on |scroller| using the coordinates provided
 // in |start_pos| and |end_pos|.
-// It is meant for use in snapchanged tests for triggering snapchanged events
-// when touch scrolling from |start_pos| to |end_pos|.
-function snapchanged_touch_scroll_helper(start_pos, end_pos) {
+// It is meant for use in snapchanged & snapchanging tests for triggering snap
+// events when touch scrolling from |start_pos| to |end_pos|.
+function snap_event_touch_scroll_helper(start_pos, end_pos) {
   return new test_driver.Actions()
     .addPointer("TestPointer", "touch")
     .pointerMove(start_pos.x, start_pos.y)
@@ -18,10 +18,10 @@ function snapchanged_touch_scroll_helper(start_pos, end_pos) {
 }
 
 // This drags the provided |scroller|'s scrollbar  vertically by |drag_amt|.
-// Snapchanged tests should provide a |drag_amt| that would result in a
-// snapchanged event being triggered.
+// Snap event tests should provide a |drag_amt| that would result in a
+// the desired snap event being triggered.
 const vertical_offset_into_scrollbar = 30;
-function snapchanged_scrollbar_drag_helper(scroller, scrollbar_width, drag_amt) {
+function snap_event_scrollbar_drag_helper(scroller, scrollbar_width, drag_amt) {
   let x, y, bounds;
   if (scroller == document.scrollingElement) {
     bounds = document.documentElement.getBoundingClientRect();
@@ -41,22 +41,26 @@ function snapchanged_scrollbar_drag_helper(scroller, scrollbar_width, drag_amt) 
     .send();
 }
 
-// This tests that snapchanged doesn't fire for a user (wheel) scroll that
-// snaps back to the same element. snapchanged tests should provide a |delta|
-// small enough that no change in |scroller|'s snap targets occurs at the end of
-// the scroll.
-async function test_no_snapchanged(test, scroller, delta) {
+// This tests that snap event of type |event_type| don't fire for a user (wheel)
+// scroll that snaps back to the same element. Snap events tests should provide
+// a |delta| small enough that no change in |scroller|'s snap targets occurs at
+// the end of the scroll.
+async function test_no_snap_event(test, scroller, delta, event_type) {
   const listening_element = scroller == document.scrollingElement
       ? document : scroller;
-  checkSnapchangedSupport(test);
+  checkSnapEventSupport(event_type);
   await waitForScrollReset(test, scroller);
   await waitForCompositorCommit();
-  let snapchanged_promise = waitForSnapChangedEvent(listening_element);
+  let snap_event_promise = waitForSnapEvent(listening_element, event_type);
   // Set the scroll destination to just a little off (0, 0) top so we snap
   // back to the top box.
   await new test_driver.Actions().scroll(0, 0, delta, delta).send();
-  let evt = await snapchanged_promise;
-  assert_equals(evt, null, "no snapchanged since scroller is back to top");
+  let evt = await snap_event_promise;
+  assert_equals(evt, null, "no snap event since scroller is back to top");
   assert_equals(scroller.scrollTop, 0, "scroller snaps back to the top");
   assert_equals(scroller.scrollLeft, 0, "scroller snaps back to the left");
+}
+
+async function test_no_snapchanged(t, scroller, delta) {
+  await test_no_snap_event(t, scroller, delta, "snapchanged");
 }
