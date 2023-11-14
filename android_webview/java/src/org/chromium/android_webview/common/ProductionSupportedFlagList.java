fa824cbff22fe8991f7a00be904950b56736d98d
Dan Harrington <harringtond@chromium.org>
2023-11-14 17:29:56 +0000

    Add AutofillTextAreaChangeEvents to ProductionSupportedFlagList
    Bug: b:300942022
    Change-Id: I8c5723059102e7d6130d8002b365718041b168ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5021049
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1224372}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -303,6 +303,9 @@ public final class ProductionSupportedFlagList {
                 AutofillFeatures.AUTOFILL_ENABLE_EMAIL_HEURISTIC_ONLY_ADDRESS_FORMS,
                 "When enabled, Autofill supports forms consisting of only email fields."),
         Flag.baseFeature(
+                AutofillFeatures.AUTOFILL_TEXT_AREA_CHANGE_EVENTS,
+                "When enabled, autofill responds to textarea change events."),
+        Flag.baseFeature(
                 FeatureConstants.KEYBOARD_ACCESSORY_PAYMENT_VIRTUAL_CARD_FEATURE,
                 "When enabled, merchant bound virtual cards will be offered in the keyboard "
                         + "accessory."),
