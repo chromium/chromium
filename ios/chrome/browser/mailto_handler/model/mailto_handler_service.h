02cb59597f59d4066c1955e27d7d0d2cf96f8305
Robbie Gibson <rkgibson@google.com>
2023-11-14 09:32:29 +0000

    [iOS] Prepare to move ios/c/b/mailto_handler to model subfolder
    iOS is refactoring this code to move these files into a subfolder. This
    prepares for that migration.
    
    This is part of a multi-step CL:
    1. This
    2. crrev.com/i/6665307
    3. TBD
    
    Bug: 1480318
    Change-Id: I1dba2e8c3382d932f4e7a88b7ebec2d74eb89c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5026640
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1224157}

---
+++ ios/chrome/browser/mailto_handler/model/mailto_handler_service.h
@@ -0,0 +1,10 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_MAILTO_HANDLER_MODEL_MAILTO_HANDLER_SERVICE_H_
+#define IOS_CHROME_BROWSER_MAILTO_HANDLER_MODEL_MAILTO_HANDLER_SERVICE_H_
+
+#import "ios/chrome/browser/mailto_handler/mailto_handler_service.h"
+
+#endif  // IOS_CHROME_BROWSER_MAILTO_HANDLER_MODEL_MAILTO_HANDLER_SERVICE_H_
