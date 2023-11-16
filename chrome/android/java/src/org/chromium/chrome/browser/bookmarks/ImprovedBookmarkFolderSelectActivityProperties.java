e49b3f0963526dc5a30bc18126a412dcbcaf5f40
Sky Malice <skym@chromium.org>
2023-11-16 17:48:30 +0000

    Delete unused ImprovedBookmarkFolderSelectActivity*
    These are unused, not referenced by code of build files. And have the
    wrong class names, confusing android studio.
    
    Change-Id: Ic149b058ff33e829e46170d84ef9b2e28818c4cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018440
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1225577}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkFolderSelectActivityProperties.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package org.chromium.chrome.browser.bookmarks;
-
-/** Properties for the folder select activity. */
-// TODO(crbug.com/1448933): Implement new folder select activity.
-class BookmarkFolderPickerProperties {}
