15dd6ab2e14ff8d202d6298d11efbdf19348924a
Jian Li <jianli@chromium.org>
2023-11-16 05:36:38 +0000

    M120: Hide WebFeedAwareness IPH when the feed follow UI update is enabled
    (cherry picked from commit ffc26a888ea56385d550ac9258d8a4ec63e2f81f)
    
    Bug: 1500430
    Change-Id: Idf570a9374d0a8976075ac2417e7714682a854eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5011263
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1221280}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5035705
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6099@{#743}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -580,7 +580,9 @@ public class FeedSurfaceCoordinator
     }
 
     public void maybeShowWebFeedAwarenessIph() {
-        if (mWebFeedHasContent && FeedFeatures.shouldUseWebFeedAwarenessIPH()) {
+        if (mWebFeedHasContent
+                && FeedFeatures.shouldUseWebFeedAwarenessIPH()
+                && !ChromeFeatureList.isEnabled(ChromeFeatureList.FEED_FOLLOW_UI_UPDATE)) {
             UserEducationHelper helper = new UserEducationHelper(mActivity, mHandler);
             mSectionHeaderView.showWebFeedAwarenessIph(
                     helper, StreamTabId.FOLLOWING, new Scroller());
