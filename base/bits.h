6f303a54c58db0567939403e7fd9211beaa0afd4
Avi Drissman <avi@chromium.org>
2023-11-17 15:09:58 +0000

    Remove bit counting functions
    Uses of CountLeadingZeroBits and CountTrailingZeroBits have been
    updated to use std:: functions, so remove those functions.
    
    Bug: 1414634
    Change-Id: I2b2a1f901b6f132be842f15501dd6a7750645dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5028837
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226098}

--- base/bits.h
+++ base/bits.h
@@ -10,18 +10,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <bit>
 #include <type_traits>
 
 #include "base/check.h"
-#include "base/compiler_specific.h"
-#include "build/build_config.h"
 
-namespace base {
-namespace bits {
+namespace base::bits {
 
 // Returns true iff |value| is a power of 2.
 //
-// TODO(pkasting): When C++20 is available, replace with std::has_single_bit().
+// TODO(https://crbug.com/1414634): Replace with std::has_single_bit().
 template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
 constexpr bool IsPowerOfTwo(T value) {
   // From "Hacker's Delight": Section 2.1 Manipulating Rightmost Bits.
@@ -63,57 +61,15 @@ inline T* AlignUp(T* ptr, uintptr_t alignment) {
       AlignUp(reinterpret_cast<uintptr_t>(ptr), alignment));
 }
 
-// CountLeadingZeroBits(value) returns the number of zero bits following the
-// most significant 1 bit in |value| if |value| is non-zero, otherwise it
-// returns {sizeof(T) * 8}.
-// Example: 00100010 -> 2
-//
-// CountTrailingZeroBits(value) returns the number of zero bits preceding the
-// least significant 1 bit in |value| if |value| is non-zero, otherwise it
-// returns {sizeof(T) * 8}.
-// Example: 00100010 -> 1
-//
-// C does not have an operator to do this, but fortunately the various
-// compilers have built-ins that map to fast underlying processor instructions.
-//
-// TODO(pkasting): When C++20 is available, replace with std::countl_zero() and
-// similar.
-
-// __builtin_clz has undefined behaviour for an input of 0, even though there's
-// clearly a return value that makes sense, and even though some processor clz
-// instructions have defined behaviour for 0. We could drop to raw __asm__ to
-// do better, but we'll avoid doing that unless we see proof that we need to.
-template <typename T, int bits = sizeof(T) * 8>
-ALWAYS_INLINE constexpr
-    typename std::enable_if<std::is_unsigned_v<T> && sizeof(T) <= 8, int>::type
-    CountLeadingZeroBits(T value) {
-  static_assert(bits > 0, "invalid instantiation");
-  return LIKELY(value)
-             ? bits == 64
-                   ? __builtin_clzll(static_cast<uint64_t>(value))
-                   : __builtin_clz(static_cast<uint32_t>(value)) - (32 - bits)
-             : bits;
-}
-
-template <typename T, int bits = sizeof(T) * 8>
-ALWAYS_INLINE constexpr
-    typename std::enable_if<std::is_unsigned_v<T> && sizeof(T) <= 8, int>::type
-    CountTrailingZeroBits(T value) {
-  return LIKELY(value) ? bits == 64
-                             ? __builtin_ctzll(static_cast<uint64_t>(value))
-                             : __builtin_ctz(static_cast<uint32_t>(value))
-                       : bits;
-}
-
 // Returns the integer i such as 2^i <= n < 2^(i+1).
 //
 // There is a common `BitLength` function, which returns the number of bits
 // required to represent a value. Rather than implement that function,
 // use `Log2Floor` and add 1 to the result.
 //
-// TODO(pkasting): When C++20 is available, replace with std::bit_xxx().
+// TODO(https://crbug.com/1414634): Replace with std::bit_xxx().
 constexpr int Log2Floor(uint32_t n) {
-  return 31 - CountLeadingZeroBits(n);
+  return 31 - std::countl_zero(n);
 }
 
 // Returns the integer i such as 2^(i-1) < n <= 2^i.
@@ -121,7 +77,7 @@ constexpr int Log2Ceiling(uint32_t n) {
   // When n == 0, we want the function to return -1.
   // When n == 0, (n - 1) will underflow to 0xFFFFFFFF, which is
   // why the statement below starts with (n ? 32 : -1).
-  return (n ? 32 : -1) - CountLeadingZeroBits(n - 1);
+  return (n ? 32 : -1) - std::countl_zero(n - 1);
 }
 
 // Returns a value of type T with a single bit set in the left-most position.
@@ -134,7 +90,6 @@ constexpr T LeftmostBit() {
   return one << (8 * sizeof(T) - 1);
 }
 
-}  // namespace bits
-}  // namespace base
+}  // namespace base::bits
 
 #endif  // BASE_BITS_H_
