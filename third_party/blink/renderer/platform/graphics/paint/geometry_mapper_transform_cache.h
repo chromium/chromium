07c4ca86e2b37054429787eb6d1509c5883294bf
Kalvin Lee <kdlee@chromium.org>
2023-11-17 06:52:30 +0000

    MiraclePtr: Unrewrite for MotionMark
    The `motionmark_ramp_leaves` story in `rendering.desktop` regresses
    ~3% when MiraclePtr is enabled for the renderer process. CPU profiling
    suggests that we can downgrade several members in `StateEntry` and
    `GeometryMapperTransformCache` to `T*` and recover about 1%.
    
    See also: crbug.com/1495275#c116 and the Pinpoint runs associated
    with this CL.
    
    (cherry picked from commit e773c835116f7a7f81c82ab1b044a252bf0ccb40)
    
    Change-Id: I72d8a8103882be05557643fb58eb097568a99341
    Bug: 1495275, 1500071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5012414
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Owners-Override: Kalvin Lee <kdlee@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1222801}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5040290
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6099@{#789}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- third_party/blink/renderer/platform/graphics/paint/geometry_mapper_transform_cache.h
+++ third_party/blink/renderer/platform/graphics/paint/geometry_mapper_transform_cache.h
@@ -7,7 +7,7 @@
 
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -149,8 +149,10 @@ class PLATFORM_EXPORT GeometryMapperTransformCache {
   // The parent of the root of consecutive identity or 2d translations from the
   // transform node, or the root of the tree if the whole path from the
   // transform node to the root contains identity or 2d translations only.
-  raw_ptr<const TransformPaintPropertyNode, ExperimentalRenderer>
-      root_of_2d_translation_;
+  //
+  // Excluded from being a `raw_ptr` for visible regression in
+  // MotionMark (crbug.com/1495275#c116).
+  RAW_PTR_EXCLUSION const TransformPaintPropertyNode* root_of_2d_translation_;
 
   // The cached values here can be categorized in two logical groups:
   //
