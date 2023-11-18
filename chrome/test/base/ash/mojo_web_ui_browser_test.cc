1881adadd3cf16e3d852079d12124a5eded5369e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2023-11-18 04:40:14 +0000

    Revert "WebUI: Move web_ui_test.mojom and related code to CrOS-only folder."
    This reverts commit 0ebe4e09fa2f5cb542fe068c275d04b50852083f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8764123015430851969
    
    Sample failed build: https://ci.chromium.org/b/8764123015430851969
    
    If this is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?comment=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8764123015430851969&components=Tools%3ETest%3EFindit&labels=LUCI-Bisection-Wrong%2CPri-3%2CType-Bug&status=Available&summary=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5034588
    
    Original change's description:
    > WebUI: Move web_ui_test.mojom and related code to CrOS-only folder.
    >
    > These files are only used in legacy js2gtest tests, which are no longer
    > used on Desktop.
    >
    > Note that chrome/test/base/ash/mojo_web_ui_browser_test.h which
    > consumes such bindings on the C++ side, is already limited to CrOS.
    >
    > Bug: 1347740
    > Change-Id: I24baefaa539d0a69d2d3aacc7af48b81ee4f03e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5034588
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1226485}
    >
    
    Bug: 1347740
    Change-Id: I079158092734eff9ae9d2b7762100330a83e7be2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5042363
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1226505}

--- chrome/test/base/ash/mojo_web_ui_browser_test.cc
+++ chrome/test/base/ash/mojo_web_ui_browser_test.cc
@@ -139,7 +139,7 @@ void MojoWebUIBrowserTest::BrowsePreload(const GURL& browse_to) {
       browser()->tab_strip_model()->GetActiveWebContents();
   std::string test_mojo_lite_js =
       ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
-          IDR_WEBUI_CHROMEOS_TEST_WEB_UI_TEST_MOJOM_LITE_JS);
+          IDR_WEBUI_TEST_WEB_UI_TEST_MOJOM_LITE_JS);
   web_contents->GetPrimaryMainFrame()->ExecuteJavaScriptForTests(
       base::UTF8ToUTF16(test_mojo_lite_js), base::NullCallback());
 }
