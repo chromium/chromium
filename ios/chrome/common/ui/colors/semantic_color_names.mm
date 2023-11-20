a316289a60956b09d953c76eb1f9cdce6542ecf1
Hira Mahmood <hiramahmood@google.com>
2023-11-20 21:14:20 +0000

    [iOS] Add default image for parcel tracking magic stack module
    This CL:
    * adds tints for the default icon image for the magic stack module
    * removes use of the USPS logo (to use default image instead)
    
    Red status: https://screenshot.googleplex.com/7UR4dK4XEVdBGuu
    Green status: https://screenshot.googleplex.com/odVywAXNu8isof6
    Gray status: https://screenshot.googleplex.com/A6DUZfZSULNVtc3
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL in fast follow
    Bug: None
    Change-Id: I611624b2274a439e2377670a5b10062a93038476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038656
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227014}

--- ios/chrome/common/ui/colors/semantic_color_names.mm
+++ ios/chrome/common/ui/colors/semantic_color_names.mm
@@ -55,13 +55,16 @@ NSString* const kStaticBlue400Color = @"static_blue_400_color";
 NSString* const kGreenColor = @"green_color";
 NSString* const kGreen50Color = @"green_50_color";
 NSString* const kGreen100Color = @"green_100_color";
+NSString* const kGreen300Color = @"green_300_color";
 NSString* const kGreen400Color = @"green_400_color";
 NSString* const kGreen500Color = @"green_500_color";
 NSString* const kGreen600Color = @"green_600_color";
 NSString* const kGreen700Color = @"green_700_color";
 NSString* const kGreen800Color = @"green_800_color";
 
+NSString* const kRed50Color = @"red_50_color";
 NSString* const kRed100Color = @"red_100_color";
+NSString* const kRed300Color = @"red_300_color";
 NSString* const kRed400Color = @"red_400_color";
 NSString* const kRed500Color = @"red_500_color";
 NSString* const kRed600Color = @"red_600_color";
