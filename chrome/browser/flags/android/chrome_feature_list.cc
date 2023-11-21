481d9ea7aa19178dd401761a100d35fa770692c9
Gang Wu <gangwu@chromium.org>
2023-11-21 19:53:39 +0000

    Enable AndroidAppIntegrationSafeSearch by default
    Bug: 1445112
    Change-Id: I7627ff000b321ced02922f2b47cb1af33f1ba9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5050961
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227543}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -478,7 +478,7 @@ BASE_FEATURE(kAndroidAppIntegration,
 
 BASE_FEATURE(kAndroidAppIntegrationSafeSearch,
              "AndroidAppIntegrationSafeSearch",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidHatsRefactor,
              "AndroidHatsRefactor",
