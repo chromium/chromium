928d642e3777ddce67b8397953c7e26e2a8b2eba
Hira Mahmood <hiramahmood@google.com>
2023-11-21 15:57:47 +0000

    [iOS] Fix accessibility bugs for parcel tracking opt-in sheet
    This CL:
    * fixes a bug where the subtitle text was being announced as "editable"
      in VoiceOver
    * adds VoiceOver support to the radio buttons to convey their state
      and control
    
    an egtest for this component
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED reports as 0 but there is
    Fixed: 1503990, 1503989
    Change-Id: I3c14a15fc1057ae7426c21af7b7feab9854dea33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046986
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227400}

--- ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
+++ ios/chrome/browser/ui/parcel_tracking/parcel_tracking_opt_in_view_controller.mm
@@ -103,7 +103,6 @@ CGFloat const kRadioButtonSize = 20;
       l10n_util::GetNSString(IDS_IOS_PARCEL_TRACKING_OPT_IN_SUBTITLE),
       textAttributes, linkAttributes);
   subtitle.delegate = self;
-  subtitle.editable = YES;
   subtitle.selectable = YES;
 }
 
@@ -162,7 +161,10 @@ CGFloat const kRadioButtonSize = 20;
   cell.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
   cell.userInteractionEnabled = YES;
   cell.textLabel.text = title;
-  cell.textLabel.isAccessibilityElement = YES;
+  cell.isAccessibilityElement = YES;
+  cell.accessibilityLabel = cell.textLabel.text;
+  cell.accessibilityTraits =
+      [self accessibilityTraitsForButton:/*selected=*/NO];
 
   cell.accessoryView =
       [[UIImageView alloc] initWithImage:DefaultSymbolTemplateWithPointSize(
@@ -196,6 +198,8 @@ CGFloat const kRadioButtonSize = 20;
   cell.accessoryView = [[UIImageView alloc] initWithImage:icon];
   cell.accessoryView.tintColor = [UIColor colorNamed:kBlueColor];
   self.primaryActionButton.enabled = YES;
+  cell.accessibilityTraits =
+      [self accessibilityTraitsForButton:/*selected=*/YES];
 }
 
 - (void)tableView:(UITableView*)tableView
@@ -205,6 +209,8 @@ CGFloat const kRadioButtonSize = 20;
       initWithImage:DefaultSymbolTemplateWithPointSize(
                         kCircleSymbol, kSymbolAccessoryPointSize)];
   cell.accessoryView.tintColor = [UIColor colorNamed:kGrey500Color];
+  cell.accessibilityTraits =
+      [self accessibilityTraitsForButton:/*selected=*/NO];
 }
 
 #pragma mark - Private
@@ -267,6 +273,16 @@ CGFloat const kRadioButtonSize = 20;
     }
     incomingButton.configuration = updatedConfig;
   };
+}
+
+// Returns the accessibility traits for the radio button options. `selected`
+// should be true if the radio button is selected.
+- (UIAccessibilityTraits)accessibilityTraitsForButton:(BOOL)selected {
+  UIAccessibilityTraits accessibilityTraits = UIAccessibilityTraitButton;
+  if (selected) {
+    accessibilityTraits |= UIAccessibilityTraitSelected;
+  }
+  return accessibilityTraits;
 }
 
 @end
