82e30bcba4b93b42acde08ff7d815878875f6dc0
Wes Okuhara <wesokuhara@google.com>
2023-11-21 01:24:03 +0000

    CrOS Settings: Enable CR2023 styles with revamp
    CR2023 styles for cr-elements will be included in the Wayfinding Revamp
    for ChromeOS Settings. The "OsSettingsTestChromeRefresh" feature flag
    will be removed in a follow-up CL.
    
    Bug: b/312300730
    Test: Manually test
    Change-Id: I976a0a03078a68a434c0aee3f04a8000613955f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046159
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227161}

--- chrome/browser/ui/webui/ash/settings/pages/main/main_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/main/main_section.cc
@@ -179,8 +179,11 @@ void MainSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
   html_source->AddBoolean("isOSSettings", true);
 
   // Add app-wide feature flags
-  html_source->AddBoolean("isRevampWayfindingEnabled",
-                          ash::features::IsOsSettingsRevampWayfindingEnabled());
+  const bool kIsRevampEnabled =
+      ash::features::IsOsSettingsRevampWayfindingEnabled();
+  html_source->AddBoolean("isRevampWayfindingEnabled", kIsRevampEnabled);
+  html_source->AddString("chromeRefresh2023Attribute",
+                         kIsRevampEnabled ? "chrome-refresh-2023" : "");
 
   html_source->AddBoolean("isGuest", IsGuestModeActive());
   html_source->AddBoolean(
@@ -199,10 +202,6 @@ void MainSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
   html_source->AddInteger(
       "entryPointEnumSize",
       static_cast<int>(PersonalizationEntryPoint::kMaxValue) + 1);
-
-  html_source->AddString(
-      "chromeRefresh2023Attribute",
-      features::IsOsSettingsTestChromeRefresh() ? "chrome-refresh-2023" : "");
 
   AddSearchInSettingsStrings(html_source);
   AddChromeOSUserStrings(html_source);
