f866ed291230b2be670bebc916ff6163f0ca1356
ahmedmoussa <ahmedmoussa@google.com>
2023-11-21 20:02:15 +0000

    Reland "[live_preview] Page Info sub-pages supports camera and mic preview"
    This is a reland of commit 7944db0d1833a91bfc6e29b2062adc9962668cf1
    
    Original change's description:
    > [live_preview] Page Info sub-pages supports camera and mic preview
    >
    > This CL integrates MediaPreview into Page Info media sub-pages.
    >
    > Disabled-by-default behind feature flag `kCameraMicPreview`.
    >
    > Bug: b:296951351
    > Change-Id: I8c03aeac67635d8bee6732aee3987c98ecbe984e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4639352
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1226426}
    
    Bug: b:296951351
    Change-Id: I435b764f558f29830bd79d3773175adb62699a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037627
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227553}

--- chrome/browser/ui/views/page_info/page_info_permission_content_view.h
+++ chrome/browser/ui/views/page_info/page_info_permission_content_view.h
@@ -9,6 +9,10 @@
 #include "components/page_info/page_info_ui.h"
 #include "ui/views/view.h"
 
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_FUCHSIA)
+#include "chrome/browser/ui/views/media_preview/media_coordinator.h"
+#endif
+
 class ChromePageInfoUiDelegate;
 class NonAccessibleImageView;
 
@@ -42,9 +46,15 @@ class PageInfoPermissionContentView : public views::View, public PageInfoUI {
                          ChosenObjectInfoList chosen_object_info_list) override;
 
  private:
+  // views::View overrides
+  void ChildPreferredSizeChanged(views::View* child) override;
+
   void OnToggleButtonPressed();
   void OnRememberSettingPressed();
   void PermissionChanged();
+
+  // Adds Media (Camera or Mic) live preview feeds.
+  void MaybeAddMediaPreview();
 
   raw_ptr<PageInfo> presenter_ = nullptr;
   ContentSettingsType type_;
@@ -56,6 +66,10 @@ class PageInfoPermissionContentView : public views::View, public PageInfoUI {
   raw_ptr<views::Label> state_label_ = nullptr;
   raw_ptr<views::ToggleButton> toggle_button_ = nullptr;
   raw_ptr<views::Checkbox> remember_setting_ = nullptr;
+
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_FUCHSIA)
+  std::optional<MediaCoordinator> media_preview_coordinator_;
+#endif
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PAGE_INFO_PAGE_INFO_PERMISSION_CONTENT_VIEW_H_
