bc8370fb22dec55a7831f38e90b2e0ed6234c4ee
Maggie Cai <mxcai@google.com>
2023-11-21 01:00:06 +0000

    [Shortcut] Use Shortname instead of Name for the parent app name.
    This CL updates the shortcut context menu and removal dialog to use the
    Shortname instead of Name for the parent app of the shortcut.
    
    BUG=1412708
    
    Change-Id: I57372b203debdbb6e599727adccb9639a7dd3619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5043445
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227141}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shortcut_shelf_item_controller_browsertest.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shortcut_shelf_item_controller_browsertest.cc
@@ -116,7 +116,7 @@ IN_PROC_BROWSER_TEST_F(AppServiceShortcutShelfItemControllerBrowserTest,
       ->AppRegistryCache()
       .ForOneApp(app_constants::kChromeAppId,
                  [&host_app_name](const apps::AppUpdate& update) {
-                   host_app_name = base::UTF8ToUTF16(update.Name());
+                   host_app_name = base::UTF8ToUTF16(update.ShortName());
                  });
 
   EXPECT_EQ(u"Open " + shortcut_name + u" - " + host_app_name,
