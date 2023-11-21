7fd761993e0442803215db7d63c1359be0b572a7
Etienne Pierre-doray <etiennep@chromium.org>
2023-11-21 23:35:35 +0000

    [blink] Implement MainThreadIsolate for blink_unittests_v2
    Bug: 1315595
    Change-Id: I7d2a93ed3a041138bfa77b7bd16a63bc72b2522a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5046374
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227676}

---
+++ third_party/blink/renderer/platform/testing/main_thread_isolate.cc
@@ -0,0 +1,27 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/testing/main_thread_isolate.h"
+
+#include "third_party/blink/public/web/blink.h"
+#include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
+#include "third_party/blink/renderer/platform/heap/thread_state.h"
+#include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
+
+namespace blink::test {
+
+MainThreadIsolate::MainThreadIsolate() {
+  isolate_ = CreateMainThreadIsolate();
+}
+
+MainThreadIsolate::~MainThreadIsolate() {
+  CHECK_NE(nullptr, isolate_);
+  ThreadState::Current()->CollectAllGarbageForTesting();
+  ThreadScheduler::Current()->SetV8Isolate(nullptr);
+  V8PerIsolateData::WillBeDestroyed(isolate());
+  V8PerIsolateData::Destroy(isolate());
+  isolate_ = nullptr;
+}
+
+}  // namespace blink::test
