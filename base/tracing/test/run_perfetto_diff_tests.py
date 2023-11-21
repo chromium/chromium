e3b6a95421c7bd265c16c19c358618b5921ce1a6
Rasika Navarange <rasikan@google.com>
2023-11-21 16:19:38 +0000

    Use Chrome test/data directory for diff tests
    Pass the local base/tracing/test/data directory to the diff test TestSuite. This allows developers to update test data locally.
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests`
    
    Change-Id: If5a0a1d97bbea7d0bfab6d33c1fec0ab1d44f8ee
    Bug: b/308929429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049421
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1227413}

--- base/tracing/test/run_perfetto_diff_tests.py
+++ base/tracing/test/run_perfetto_diff_tests.py
@@ -48,6 +48,7 @@ def main():
   completed_process = subprocess.run(cmd, capture_output=True)
 
   sys.stderr.buffer.write(completed_process.stderr)
+  sys.stdout.buffer.write(completed_process.stdout)
   return completed_process.returncode
 
 if __name__ == '__main__':
--- base/tracing/test/trace_processor/diff_tests/include_index.py
+++ base/tracing/test/trace_processor/diff_tests/include_index.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 from typing import List
 
 from python.generators.diff_tests import testing
@@ -11,8 +12,12 @@ from chrome.tests_scroll_jank import ChromeScrollJankStdlib
 from chrome.tests_chrome_interactions import ChromeInteractions
 
 def fetch_all_diff_tests(index_path: str) -> List['testing.TestCase']:
+  test_data_path = os.path.abspath(os.path.join(__file__, '../../../data'))
   return [
-      *ChromeScrollJankStdlib(index_path, 'chrome', 'ChromeScrollJankStdlib').fetch(),
-      *ChromeStdlib(index_path, 'chrome', 'Chrome').fetch(),
-      *ChromeInteractions(index_path, 'chrome', 'ChromeInteractions').fetch(),
+      *ChromeScrollJankStdlib(index_path, 'chrome', 'ChromeScrollJankStdlib',
+                              test_data_path).fetch(),
+      *ChromeStdlib(index_path, 'chrome', 'ChromeStdlib',
+                    test_data_path).fetch(),
+      *ChromeInteractions(index_path, 'chrome', 'ChromeInteractions',
+                          test_data_path).fetch(),
       ]
