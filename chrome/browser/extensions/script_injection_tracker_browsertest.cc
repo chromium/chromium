8f21a9e763ea281e391e2d7230a591bbcb9ef6d6
Lukasz Anforowicz <lukasza@chromium.org>
2023-11-21 23:37:12 +0000

    Deflake and reenable `ContentScriptViaScriptingApiWhileIdle` test.
    The
    `DynamicScriptsTrackerBrowserTest.ContentScriptViaScriptingApiWhileIdle`
    test injects a content script at `document_idle` time.  The test has
    been flakily timing out while waiting until the content script signals
    that is has been run (test logs from the bots show that the test was
    timing out during the `catcher.GetNextResult()` call).
    
    Based on reading the code it seems that:
    
    * The test properly waits for `DidFinishNavigation` for the navigation
      in the main frame.
    * The test properly controls the timing of the `onload` event for one of
      the subframes.  The affected subframe is one of two subframes and the
      `ControllableHttpResponse` somewhat randomly catches one or the other.
      This is okay though - the `onload` event won't happen until *both*
      subframes have finished navigating.
    
    Based on the above, it seems that the content script should indeed get
    injected at `document_idle` time - after the `onload` event.  Based on
    reading additional test code, it seems that the problem is that the
    content script *also* tries to wait for the `onload` event - this
    doesn't take into the account that this event might have already been
    raised.
    
    Based on the above this CL:
    
    * Attempts to speculatively fix the flakiness, by removing the
      `window.onload = function()` part of the content script
    * Removes the secondary and ultimately unnecessary test assertions
      around `document.readyState` (which changes around `onload` event)
    * Reenables the test
    * Adds additional `console.log` statements to the content script
      to help with diagnosing any remaining flakiness (if any; we
      speculatively assume that the test should not be flaky going forward).
    
    Bug: 1502769
    Change-Id: I363ca624f22d2d3b471e1b67fcc5ee286f21b248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038655
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227678}

--- chrome/browser/extensions/script_injection_tracker_browsertest.cc
+++ chrome/browser/extensions/script_injection_tracker_browsertest.cc
@@ -1264,9 +1264,8 @@ IN_PROC_BROWSER_TEST_F(DynamicScriptsTrackerBrowserTest,
 }
 
 // Regression test for https://crbug.com/1439642.
-// TODO(crbug.com/1502769): Test is flaky.
 IN_PROC_BROWSER_TEST_F(DynamicScriptsTrackerBrowserTest,
-                       DISABLED_ContentScriptViaScriptingApiWhileIdle) {
+                       ContentScriptViaScriptingApiWhileIdle) {
   // The test orchestrates the following sequence of events.
   //
   // Step 1: `DidFinishNavigation` for a.com/iframe_cross_site.html
@@ -1321,11 +1320,21 @@ IN_PROC_BROWSER_TEST_F(DynamicScriptsTrackerBrowserTest,
   dir.WriteManifest(kManifestTemplate);
   dir.WriteFile(FILE_PATH_LITERAL("page.html"), "<p>Extension page</p>");
   const char kContentScript[] = R"(
-      window.onload = function() {
-          chrome.test.assertEq('complete', document.readyState);
-          document.body.innerText = 'content script has run';
-          chrome.test.notifyPass();
-      }
+      // TODO(https://crbug.com/1502769): Remove `console.log` after confirming
+      // that the test is no longer flaky
+      console.log('CONTENT SCRIPT: running...');
+
+      // `document_idle` waits for either `onload` or 200ms after
+      // `DOMContentLoaded` (ish).  In both of these cases `document.body`
+      // should be already available.
+      chrome.test.assertTrue(!!document.body);
+
+      document.body.innerText = 'content script has run';
+      chrome.test.notifyPass();
+
+      // TODO(https://crbug.com/1502769): Remove `console.log` after confirming
+      // that the test is no longer flaky
+      console.log('CONTENT SCRIPT: running... DONE.');
   )";
   dir.WriteFile(FILE_PATH_LITERAL("content_script.js"), kContentScript);
   const Extension* extension = LoadExtension(dir.UnpackedPath());
