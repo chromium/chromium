7baba4eee6a9dae87a4bea8fc0f68c581afd96fa
Tom Sepez <tsepez@chromium.org>
2023-11-22 04:52:15 +0000

    Correct a comment in base/pickle.h
    The comment that claims that pickle has no other virtual methods is
    out-of-date, as these are now present a few lines below.
    
    Change-Id: I1f2de9380c2045046207f38bd2e161ef3118925d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5051626
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227796}

--- base/pickle.h
+++ base/pickle.h
@@ -168,11 +168,9 @@ class BASE_EXPORT Pickle {
   // Initializes a Pickle as a deep copy of another Pickle.
   Pickle(const Pickle& other);
 
-  // Note: There are no virtual methods in this class.  This destructor is
-  // virtual as an element of defensive coding.  Other classes have derived from
-  // this class, and there is a *chance* that they will cast into this base
-  // class before destruction.  At least one such class does have a virtual
-  // destructor, suggesting at least some need to call more derived destructors.
+  // Note: Other classes are derived from this class, and they may well
+  // delete through this parent class, e.g. std::uniuqe_ptr<Pickle> exists
+  // in several places the code.
   virtual ~Pickle();
 
   // Performs a deep copy.
