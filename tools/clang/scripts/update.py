f13eca9f30c80fc49b54115a83d321a361dda83b
Arthur Eubanks <aeubanks@google.com>
2023-11-22 08:23:12 +0000

    Roll clang+rust llvmorg-18-init-9505-g10664813-1 : llvmorg-18-init-11962-g73e96337-1 / 2e4e2a8f288f642cafcc41fff211955ceddc453d-1 : 820f06b21f8373060ff7b515715b8440a6a6c197-1
    https://chromium.googlesource.com/external/github.com/llvm/llvm-project/+log/10664813..73e96337
    
    https://chromium.googlesource.com/external/github.com/rust-lang/rust/+log/2e4e2a8f288f..820f06b21f83
    
    Ran: ./tools/clang/scripts/upload_revision.py
    
    Bug: 1498542, 1497464, 1503571
    Change-Id: I054c85b0a8cc6981e9db6d0b492ceaba0841f1fe
    Disable-Rts: True
    Fuchsia-Binary-Size: recent LLVM improvement was reverted, https://crbug.com/1498542#c9
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:lacros-arm64-generic-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac11-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:ios-catalyst
    Cq-Include-Trybots: chromium/try:win-asan
    Cq-Include-Trybots: chromium/try:android-official,fuchsia-official
    Cq-Include-Trybots: chromium/try:mac-official,linux-official
    Cq-Include-Trybots: chromium/try:win-official,win32-official
    Cq-Include-Trybots: chromium/try:linux-swangle-try-x64,win-swangle-try-x86
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-arm64-rel
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-x86-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-mainline-clang-x86-rel
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-dbg
    Cq-Include-Trybots: chromium/try:android-cronet-riscv64-rel
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,linux-chrome,mac-chrome
    Cq-Include-Trybots: chrome/try:linux-pgo,mac-pgo,win32-pgo,win64-pgo
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:mac-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038007
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1227858}

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,8 +35,8 @@ import zlib
 # https://chromium.googlesource.com/chromium/src/+/main/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
 # This is the output of `git describe` and is usable as a commit-ish.
-CLANG_REVISION = 'llvmorg-18-init-9505-g10664813'
-CLANG_SUB_REVISION = 2
+CLANG_REVISION = 'llvmorg-18-init-11962-g73e96337'
+CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s' % (CLANG_REVISION, CLANG_SUB_REVISION)
 RELEASE_VERSION = '18'
--- tools/rust/update_rust.py
+++ tools/rust/update_rust.py
@@ -34,8 +34,8 @@ sys.path.append(
 # In the case that a Rust roll fails and you want to roll Clang alone, reset
 # this back to its previous value _AND_ set `OVERRIDE_CLANG_REVISION` below
 # to the `CLANG_REVISION` that was in place before the roll.
-RUST_REVISION = '2e4e2a8f288f642cafcc41fff211955ceddc453d'
-RUST_SUB_REVISION = 2
+RUST_REVISION = '820f06b21f8373060ff7b515715b8440a6a6c197'
+RUST_SUB_REVISION = 1
 
 # If not None, this overrides the `CLANG_REVISION` in
 # //tools/clang/scripts/update.py in order to download a Rust toolchain that
@@ -56,7 +56,7 @@ CRUBIT_SUB_REVISION = 1
 # Hash of src/stage0.json, which itself contains the stage0 toolchain hashes.
 # We trust the Rust build system checks, but to ensure it is not tampered with
 # itself check the hash.
-STAGE0_JSON_SHA256 = '0bad56c08e3fc22901944ceb5a2bc9c51ec1f9df57be981f70dac53288f55411'
+STAGE0_JSON_SHA256 = '432afb4d9ab10d74d70d735ac01727a6e0d6b8fdd1e651303edec667aec57a0e'
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 CHROMIUM_DIR = os.path.abspath(os.path.join(THIS_DIR, '..', '..'))
