40c906ca2a7428abc4dedd74e8292ea08dd59798
Colin Blundell <blundell@chromium.org>
2023-11-22 20:11:37 +0000

    [//gpu] Default to Graphite-Vulkan backend on Android
    Right now, Graphite doesn't have any default backend on Android and
    hence hits a NOTREACHED() when Graphite is enabled. This CL changes
    Graphite to default to Vulkan as is done for Linux and ChromeOS.
    
    Note: There is then immediately a different GPU crash to work through
    :).
    
    Bug: 1504543
    Change-Id: I6d2ede64c3ac350ae946326bdfd502cb0251cd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053982
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228159}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -186,7 +186,7 @@ wgpu::BackendType DawnContextProvider::GetDefaultBackendType() {
   return base::FeatureList::IsEnabled(features::kSkiaGraphiteDawnUseD3D12)
              ? wgpu::BackendType::D3D12
              : wgpu::BackendType::D3D11;
-#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   return wgpu::BackendType::Vulkan;
 #elif BUILDFLAG(IS_APPLE)
   return wgpu::BackendType::Metal;
