6d4479c052ddee8c545aeb5db06731a654165cfc
Colin Blundell <blundell@chromium.org>
2023-11-23 15:20:14 +0000

    [//gpu] Create DawnContextProvider for DrDC SharedContextState
    When using Graphite-Dawn and DrDC, the DrDC SharedContextState instance
    needs a DawnContextProvider. This CL adds one.
    
    Bug: 1504543
    Change-Id: If146d2430b5f89a7d097f5c758f53104a04de24b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5053992
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1228463}

--- components/viz/service/display_embedder/compositor_gpu_thread.cc
+++ components/viz/service/display_embedder/compositor_gpu_thread.cc
@@ -31,6 +31,10 @@
 #include "gpu/vulkan/vulkan_implementation.h"
 #endif
 
+#if BUILDFLAG(SKIA_USE_DAWN)
+#include "gpu/command_buffer/service/dawn_context_provider.h"
+#endif
+
 namespace viz {
 
 // static
@@ -174,6 +178,18 @@ CompositorGpuThread::GetSharedContextState() {
     return nullptr;
   }
 
+  const auto& workarounds = gpu_channel_manager_->gpu_driver_bug_workarounds();
+
+#if BUILDFLAG(SKIA_USE_DAWN)
+  // TODO(1504543): Determine if we need to set up a DawnCachingInterfaceFactory
+  // and/or a cache blob callback.
+  dawn_context_provider_ =
+      gpu::DawnContextProvider::Create(gpu_preferences, workarounds);
+  if (!dawn_context_provider_) {
+    DLOG(ERROR) << "Failed to create Dawn context provider for Graphite.";
+  }
+#endif
+
   // Create a SharedContextState.
   auto shared_context_state = base::MakeRefCounted<gpu::SharedContextState>(
       std::move(share_group), std::move(surface), std::move(context),
@@ -186,11 +202,14 @@ CompositorGpuThread::GetSharedContextState() {
       /*vulkan_context_provider=*/nullptr,
 #endif
       /*metal_context_provider=*/nullptr,
+#if BUILDFLAG(SKIA_USE_DAWN)
+      dawn_context_provider_.get(),
+#else
       /*dawn_context_provider=*/nullptr,
+#endif
       /*peak_memory_monitor=*/weak_ptr_factory_.GetWeakPtr(),
       /*created_on_compositor_gpu_thread=*/true);
 
-  const auto& workarounds = gpu_channel_manager_->gpu_driver_bug_workarounds();
   auto gles2_feature_info = base::MakeRefCounted<gpu::gles2::FeatureInfo>(
       workarounds, gpu_feature_info);
 
