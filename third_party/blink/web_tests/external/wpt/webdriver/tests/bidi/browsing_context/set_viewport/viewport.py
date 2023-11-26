fabb430888b432883924f72fd937646a718d1c1c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-11-26 20:39:52 +0000

    Import wpt@d993813c8a2fd7b655bd29d292eeff5dddc1c93b
    Using wpt-import in Chromium 70d5c057459cbb31b4d4ae58f3b8c6b1f2267424.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-chromium-rel
    Change-Id: I329d18c21288533c936f5a0997c728bd7e4cebbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5060759
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1229098}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/set_viewport/viewport.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browsing_context/set_viewport/viewport.py
@@ -3,6 +3,7 @@ from webdriver.bidi.undefined import UNDEFINED
 
 from ... import get_viewport_dimensions
 
+
 @pytest.mark.asyncio
 async def test_set_viewport(bidi_session, new_tab):
     test_viewport = {"width": 250, "height": 300}
@@ -17,10 +18,16 @@ async def test_set_viewport(bidi_session, new_tab):
 
 
 @pytest.mark.asyncio
-async def test_set_viewport_same_dimensions(bidi_session, new_tab):
-    test_viewport = {"width": 250, "height": 300}
+async def test_undefined_viewport(bidi_session, inline, new_tab):
+    test_viewport = {"width": 499, "height": 599}
 
     assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
+
+    # Load a page so that reflow is triggered when changing the viewport
+    url = inline("<div>foo</div>")
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url, wait="complete"
+    )
 
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
@@ -30,18 +37,56 @@ async def test_set_viewport_same_dimensions(bidi_session, new_tab):
 
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
-        viewport=test_viewport)
+        viewport=UNDEFINED)
 
     assert await get_viewport_dimensions(bidi_session, new_tab) == test_viewport
 
 
 @pytest.mark.asyncio
-async def test_set_viewport_reset(bidi_session, new_tab):
+@pytest.mark.parametrize("width, height", [
+    (250, 300),
+    (500, 300),
+    (250, 600),
+    (500, 600)
+], ids=["none", "width", "height", "both"])
+async def test_modified_dimensions(bidi_session, inline, new_tab, width, height):
+    start_viewport = {"width": 250, "height": 300}
+
+    assert await get_viewport_dimensions(bidi_session, new_tab) != start_viewport
+
+    # Load a page so that reflow is triggered when changing the viewport
+    url = inline("<div>foo</div>")
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url, wait="complete"
+    )
+
+    await bidi_session.browsing_context.set_viewport(
+        context=new_tab["context"],
+        viewport=start_viewport)
+
+    assert await get_viewport_dimensions(bidi_session, new_tab) == start_viewport
+
+    modified_viewport = {"width": width, "height": height}
+    await bidi_session.browsing_context.set_viewport(
+        context=new_tab["context"],
+        viewport=modified_viewport)
+
+    assert await get_viewport_dimensions(bidi_session, new_tab) == modified_viewport
+
+
+@pytest.mark.asyncio
+async def test_reset_to_default(bidi_session, inline, new_tab):
     original_viewport = await get_viewport_dimensions(bidi_session, new_tab)
 
     test_viewport = {"width": 666, "height": 333}
 
     assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
+
+    # Load a page so that reflow is triggered when changing the viewport
+    url = inline("<div>foo</div>")
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url, wait="complete"
+    )
 
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
@@ -58,12 +103,18 @@ async def test_set_viewport_reset(bidi_session, new_tab):
 
 
 @pytest.mark.asyncio
-async def test_set_viewport_affects_specific_context(bidi_session, top_context, new_tab):
+async def test_specific_context(bidi_session, inline, new_tab, top_context):
     original_viewport = await get_viewport_dimensions(bidi_session, top_context)
 
     test_viewport = {"width": 333, "height": 666}
 
     assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
+
+    # Load a page so that reflow is triggered when changing the viewport
+    url = inline("<div>foo</div>")
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url, wait="complete"
+    )
 
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
@@ -84,7 +135,7 @@ async def test_set_viewport_affects_specific_context(bidi_session, top_context, 
     "https coop"
 ])
 @pytest.mark.asyncio
-async def test_set_viewport_persists_on_navigation(bidi_session, new_tab, inline, protocol, parameters):
+async def test_persists_on_navigation(bidi_session, new_tab, inline, protocol, parameters):
     test_viewport = {"width": 499, "height": 599}
 
     assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
@@ -111,10 +162,16 @@ async def test_set_viewport_persists_on_navigation(bidi_session, new_tab, inline
 
 
 @pytest.mark.asyncio
-async def test_set_viewport_persists_on_reload(bidi_session, new_tab):
+async def test_persists_on_reload(bidi_session, inline, new_tab):
     test_viewport = {"width": 499, "height": 599}
 
     assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
+
+    # Load a page so that reflow is triggered when changing the viewport
+    url = inline("<div>foo</div>")
+    await bidi_session.browsing_context.navigate(
+        context=new_tab["context"], url=url, wait="complete"
+    )
 
     await bidi_session.browsing_context.set_viewport(
         context=new_tab["context"],
@@ -125,24 +182,5 @@ async def test_set_viewport_persists_on_reload(bidi_session, new_tab):
     await bidi_session.browsing_context.reload(
         context=new_tab["context"], wait="complete"
     )
-
-    assert await get_viewport_dimensions(bidi_session, new_tab) == test_viewport
-
-
-@pytest.mark.asyncio
-async def test_set_viewport_undefined(bidi_session, new_tab):
-    test_viewport = {"width": 499, "height": 599}
-
-    assert await get_viewport_dimensions(bidi_session, new_tab) != test_viewport
-
-    await bidi_session.browsing_context.set_viewport(
-        context=new_tab["context"],
-        viewport=test_viewport)
-
-    assert await get_viewport_dimensions(bidi_session, new_tab) == test_viewport
-
-    await bidi_session.browsing_context.set_viewport(
-        context=new_tab["context"],
-        viewport=UNDEFINED)
 
     assert await get_viewport_dimensions(bidi_session, new_tab) == test_viewport
