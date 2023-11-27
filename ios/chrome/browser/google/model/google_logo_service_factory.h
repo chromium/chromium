5b017cbc01d0a8cf90130e7fb3599215a5e7e346
Saeid Eid <saeideid@chromium.org>
2023-11-27 14:12:34 +0000

    [IOS][1/3] Move ios/chrome/browser/google to model subfolder
    Creates upstream headers to update downstream deps before moving.
    
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/google will be moved to
    ios/c/b/google/model/ to showcase that they are model files.
    
    Bug: 1480307
    Change-Id: Ib7a67cb2b33008c8f100703dafb2197dfacc6a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5055850
    Commit-Queue: Saeid Eid <saeideid@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229294}

---
+++ ios/chrome/browser/google/model/google_logo_service_factory.h
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_GOOGLE_MODEL_GOOGLE_LOGO_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_GOOGLE_MODEL_GOOGLE_LOGO_SERVICE_FACTORY_H_
+
+#import "ios/chrome/browser/google/google_logo_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_GOOGLE_MODEL_GOOGLE_LOGO_SERVICE_FACTORY_H_
