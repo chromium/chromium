73b51f4c2b1894f72dafa8b0fa092d911d10f95c
Elias Klim <elklm@google.com>
2023-11-27 13:31:17 +0000

    Enable ImprovedSemanticsActivityIndicators flag by default
    Bug: 1412449
    Change-Id: I17ed2588c581565ea258dd0331b199878e3d7831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5062289
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229276}

--- components/content_settings/core/common/features.cc
+++ components/content_settings/core/common/features.cc
@@ -72,7 +72,7 @@ BASE_FEATURE(kUserBypassFeedback,
 
 BASE_FEATURE(kImprovedSemanticsActivityIndicators,
              "ImprovedSemanticsActivityIndicators",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTrackingProtection3pcd,
              "TrackingProtection3pcd",
