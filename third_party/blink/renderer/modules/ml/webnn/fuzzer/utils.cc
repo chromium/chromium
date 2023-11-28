00133e6030d615c0447e5a5ff7d802fe50ae1548
Ningxin Hu <ningxin.hu@intel.com>
2023-11-28 10:27:35 +0000

    WebNN: Fix conv2d_fuzzer and pool2d_fuzzer
    There is an issue that conv2d_fuzzer and pool2d_fuzzer are not built and
    tested because third_party/blink/renderer/modules/ml/webnn/BUILD.gn is
    never referred from anywhere thus getting ignored. This CL fixes is
    issue by moving them into
    third_party/blink/renderer/modules/ml/BUILD.gn.
    
    This CL also fixes the compilation issue of conv2d_fuzzer and
    pool2d_fuzzer.
    
    With this fix, now the fuzzer bots can find and build the two targets,
    such as win-libfuzzer-asan-rel:
    
    https://ci.chromium.org/ui/p/chromium/builders/try/win-libfuzzer-asan-rel/1600856/overview
    
    Bug: 1504404, 1273291
    Change-Id: I83da182309ffed7e638602a36a89b871ba3cc31a
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,win-libfuzzer-asan-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056649
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1229822}

--- third_party/blink/renderer/modules/ml/webnn/fuzzer/utils.cc
+++ third_party/blink/renderer/modules/ml/webnn/fuzzer/utils.cc
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/modules/ml/webnn/fuzzer/utils.h"
+#include "base/functional/callback_helpers.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
@@ -43,6 +45,23 @@ V8MLInputOperandLayout::Enum ToV8MLInputOperandLayout(
     case webnn_proto::MLInputOperandLayout::NHWC:
       return V8MLInputOperandLayout::Enum::kNhwc;
   }
+}
+
+base::ScopedClosureRunner MakeScopedGarbageCollectionRequest(
+    v8::Isolate* isolate) {
+  return base::ScopedClosureRunner(WTF::BindOnce(
+      [](v8::Isolate* isolate) {
+        // Request a V8 GC. Oilpan will be invoked by the GC epilogue.
+        //
+        // Multiple GCs may be required to ensure everything is collected (due
+        // to a chain of persistent handles), so some objects may not be
+        // collected until a subsequent iteration. This is slow enough as is, so
+        // we compromise on one major GC, as opposed to the 5 used in
+        // V8GCController for unit tests.
+        isolate->RequestGarbageCollectionForTesting(
+            v8::Isolate::kFullGarbageCollection);
+      },
+      WTF::Unretained(isolate)));
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/ml/webnn/fuzzer/utils.h
+++ third_party/blink/renderer/modules/ml/webnn/fuzzer/utils.h
@@ -5,9 +5,15 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_FUZZER_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_FUZZER_UTILS_H_
 
+#include "base/functional/callback_helpers.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_conv_2d_options.h"
 #include "third_party/blink/renderer/modules/ml/webnn/fuzzer/webnn.pb.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_operand.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+
+namespace base {
+class ScopedClosureRunner;
+}
 
 namespace blink {
 
@@ -18,6 +24,18 @@ V8MLAutoPad::Enum ToV8MLAutoPad(webnn_proto::MLAutoPad auto_pad);
 
 V8MLInputOperandLayout::Enum ToV8MLInputOperandLayout(
     webnn_proto::MLInputOperandLayout input_layout);
+
+template <typename T>
+Vector<T> RepeatedFieldToVector(
+    const ::google::protobuf::RepeatedField<T>& repeated_field) {
+  Vector<T> result;
+  for (auto& field : repeated_field) {
+    result.push_back(field);
+  }
+  return result;
+}
+
+base::ScopedClosureRunner MakeScopedGarbageCollectionRequest(v8::Isolate*);
 
 }  // namespace blink
 
