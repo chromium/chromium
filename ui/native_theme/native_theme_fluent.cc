0c0c0b555c3ad229c73db212216d3bdbdd2c61d4
Gaston Rodriguez Lopez <gastonr@microsoft.com>
2023-11-28 00:23:49 +0000

    Check NativeThemeFluent doesn't have css colors when HC is on.
    Painting functions in NativeThemeFluent depend on the correct input
    from its callers to get the colors for the scrollbars' parts. If
    CSS colors are enabled at the same time as forced colors, the function
    would override the forced colors in favor of the css ones.
    
    This CL adds CHECKS in the painting functions to make sure that this
    situation doesn't occur.
    
    Bug: 1495415
    Change-Id: I42d520739d0cb822ce18e00b0b15ec57a4f306da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038216
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1229642}

--- ui/native_theme/native_theme_fluent.cc
+++ ui/native_theme/native_theme_fluent.cc
@@ -47,10 +47,17 @@ void NativeThemeFluent::PaintArrowButton(
     Part direction,
     State state,
     ColorScheme color_scheme,
-    const ScrollbarArrowExtraParams& arrow) const {
-  PaintButton(canvas, color_provider, rect, direction, color_scheme, arrow);
+    const ScrollbarArrowExtraParams& extra_params) const {
+  // If Forced Colors are enabled, css colors should not be present.
+  if (InForcedColorsMode()) {
+    CHECK(!extra_params.thumb_color.has_value());
+    CHECK(!extra_params.track_color.has_value());
+  }
+
+  PaintButton(canvas, color_provider, rect, direction, color_scheme,
+              extra_params);
   PaintArrow(canvas, color_provider, rect, direction, state, color_scheme,
-             arrow);
+             extra_params);
 }
 
 void NativeThemeFluent::PaintScrollbarTrack(
@@ -61,6 +68,8 @@ void NativeThemeFluent::PaintScrollbarTrack(
     const ScrollbarTrackExtraParams& extra_params,
     const gfx::Rect& rect,
     ColorScheme color_scheme) const {
+  // If Forced Colors are enabled, css colors should not be present.
+  CHECK(!InForcedColorsMode() || !extra_params.track_color.has_value());
   gfx::Rect track_fill_rect = rect;
   if (InForcedColorsMode()) {
     gfx::Insets edge_insets;
@@ -89,10 +98,8 @@ void NativeThemeFluent::PaintScrollbarTrack(
     constexpr gfx::Insets fill_insets(kFluentScrollbarTrackOutlineWidth);
     track_fill_rect.Inset(fill_insets + edge_insets);
   }
-  const SkColor track_color =
-      extra_params.track_color.has_value()
-          ? extra_params.track_color.value()
-          : color_provider->GetColor(kColorWebNativeControlScrollbarTrack);
+  const SkColor track_color = extra_params.track_color.value_or(
+      color_provider->GetColor(kColorWebNativeControlScrollbarTrack));
   cc::PaintFlags flags;
   flags.setColor(track_color);
   canvas->drawIRect(gfx::RectToSkIRect(track_fill_rect), flags);
@@ -106,6 +113,8 @@ void NativeThemeFluent::PaintScrollbarThumb(
     const gfx::Rect& rect,
     const ScrollbarThumbExtraParams& extra_params,
     ColorScheme color_scheme) const {
+  // If Forced Colors are enabled, css colors should not be present.
+  CHECK(!InForcedColorsMode() || !extra_params.thumb_color.has_value());
   DCHECK_NE(state, NativeTheme::kDisabled);
 
   cc::PaintCanvasAutoRestore auto_restore(canvas, true);
@@ -144,10 +153,8 @@ void NativeThemeFluent::PaintScrollbarCorner(
     const ScrollbarTrackExtraParams& extra_params,
     ColorScheme color_scheme) const {
   cc::PaintFlags flags;
-  const SkColor corner_color =
-      extra_params.track_color.has_value()
-          ? extra_params.track_color.value()
-          : color_provider->GetColor(kColorWebNativeControlScrollbarCorner);
+  const SkColor corner_color = extra_params.track_color.value_or(
+      color_provider->GetColor(kColorWebNativeControlScrollbarCorner));
   flags.setColor(corner_color);
   canvas->drawIRect(RectToSkIRect(rect), flags);
 }
@@ -185,12 +192,10 @@ void NativeThemeFluent::PaintButton(
     const gfx::Rect& rect,
     Part direction,
     ColorScheme color_scheme,
-    const ScrollbarArrowExtraParams& arrow) const {
+    const ScrollbarArrowExtraParams& extra_params) const {
   cc::PaintFlags flags;
-  const SkColor button_color =
-      arrow.track_color.has_value()
-          ? arrow.track_color.value()
-          : color_provider->GetColor(kColorWebNativeControlScrollbarTrack);
+  const SkColor button_color = extra_params.track_color.value_or(
+      color_provider->GetColor(kColorWebNativeControlScrollbarTrack));
   flags.setColor(button_color);
   gfx::Rect button_fill_rect = rect;
   if (InForcedColorsMode()) {
@@ -243,15 +248,14 @@ void NativeThemeFluent::PaintArrow(
     Part part,
     State state,
     ColorScheme color_scheme,
-    const ScrollbarArrowExtraParams& arrow) const {
+    const ScrollbarArrowExtraParams& extra_params) const {
   const ColorId arrow_color_id =
       state == NativeTheme::kPressed || state == NativeTheme::kHovered
           ? kColorWebNativeControlScrollbarArrowForegroundPressed
           : kColorWebNativeControlScrollbarArrowForeground;
   // TODO(crbug.com/891944): Adjust thumb_color based on `state`.
-  const SkColor arrow_color = arrow.thumb_color.has_value()
-                                  ? arrow.thumb_color.value()
-                                  : color_provider->GetColor(arrow_color_id);
+  const SkColor arrow_color = extra_params.thumb_color.value_or(
+      color_provider->GetColor(arrow_color_id));
   cc::PaintFlags flags;
   flags.setColor(arrow_color);
 
