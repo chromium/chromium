84f3b69bec4d16c4bdf929af8f273d33e6503b14
Arthur Milchior <arthurmilchior@chromium.org>
2023-11-28 09:54:25 +0000

    [iOS] Disable a death test on iOS
    The test consistently fails on ios-dcheck-tests.
    
    This seems to be because death tests requires multiprocess, which is
    not supported by iOS. If dcheck is enabled, and a DCHECK fails, it
    is not possible to resume the test.
    
    This CL should ensure the test is essentially ignored on ios.
    
    I leave to people who know more about the testing frameworks or the
    goal of those tests the task to re-enable it.
    
    Fixed: 1505315
    Change-Id: Id6130a5e1703041f61aa51aa1a65cf2c791077f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5058138
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229814}

--- base/check_unittest.cc
+++ base/check_unittest.cc
@@ -28,6 +28,7 @@ class ScopedExpectDumpWithoutCrashing {
     g_dump_without_crashing_count = 0;
     base::debug::SetDumpWithoutCrashingFunction(&DumpWithoutCrashing);
   }
+
   ~ScopedExpectDumpWithoutCrashing() {
     EXPECT_EQ(1, g_dump_without_crashing_count);
     base::debug::SetDumpWithoutCrashingFunction(nullptr);
@@ -77,16 +78,19 @@ MATCHER_P2(LogErrorMatches, line, expected_msg, "") {
 // Macro which expects a DCHECK to fire if DCHECKs are enabled.
 //
 // Note: Please use the `CheckDeathTest` fixture when using this check.
-#define EXPECT_DCHECK(msg, check_expr)                                         \
-  do {                                                                         \
-    if (DCHECK_IS_ON() && logging::LOGGING_DCHECK == logging::LOGGING_FATAL) { \
-      EXPECT_DEATH_IF_SUPPORTED(check_expr, CHECK_MATCHER(__LINE__, msg));     \
-    } else if (DCHECK_IS_ON()) {                                               \
-      ScopedExpectDumpWithoutCrashing expect_dump;                             \
-      check_expr;                                                              \
-    } else {                                                                   \
-      check_expr;                                                              \
-    }                                                                          \
+// TODO(crbug.com/1505315) Port test to iOS if possible.
+#define EXPECT_DCHECK(msg, check_expr)                                     \
+  do {                                                                     \
+    if (DCHECK_IS_ON() &&                                                  \
+        (logging::LOGGING_DCHECK == logging::LOGGING_FATAL ||              \
+         BUILDFLAG(IS_IOS))) {                                             \
+      EXPECT_DEATH_IF_SUPPORTED(check_expr, CHECK_MATCHER(__LINE__, msg)); \
+    } else if (DCHECK_IS_ON()) {                                           \
+      ScopedExpectDumpWithoutCrashing expect_dump;                         \
+      check_expr;                                                          \
+    } else {                                                               \
+      check_expr;                                                          \
+    }                                                                      \
   } while (0)
 
 #define EXPECT_LOG_ERROR_WITH_FILENAME(expected_file, expected_line, expr,     \
@@ -130,7 +134,8 @@ MATCHER_P2(LogErrorMatches, line, expected_msg, "") {
     logging::SetLogMessageHandler(nullptr);                                    \
   } while (0)
 
-#if DCHECK_IS_ON()
+// TODO(crbug.com/1505315) Port test to iOS if possible.
+#if DCHECK_IS_ON() || BUILDFLAG(IS_IOS)
 #define EXPECT_DUMP_WILL_BE_CHECK EXPECT_DCHECK
 #else
 #define EXPECT_DUMP_WILL_BE_CHECK(expected_string, statement)               \
