bcdf42cc26369576d1176ecec1f60e3a66a43f5d
Rune Lillesveen <futhark@chromium.org>
2023-11-28 18:04:43 +0000

    [inset-area] Implement resolution from computed to physical
    Resolve inset-area from logical regions to ordered physical spans for
    use during layout based on writing-mode, direction, and deduced axes.
    
    Bug: 1477314
    Change-Id: I3cb5b5218396b66422bf89d8ee16771c9ba241a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5056916
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1229989}

--- third_party/blink/renderer/core/style/inset_area.h
+++ third_party/blink/renderer/core/style/inset_area.h
@@ -5,11 +5,14 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_INSET_AREA_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_INSET_AREA_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
 
-// Possibly end points for a computed <inset-area-span>
+class WritingDirectionMode;
+
+// Possible region end points for a computed <inset-area-span>
 enum class InsetAreaRegion {
   kNone,
   kAll,
@@ -32,7 +35,17 @@ enum class InsetAreaRegion {
   kYSelfEnd,
 };
 
-// Represents the computed value for the inset-area property
+// Represents the computed value for the inset-area property. Each span is
+// represented by two end points in the spec order for that axis. That is:
+//
+//   "all" -> (kStart, kEnd)
+//   "center" -> (kCenter, kCenter)
+//   "right left" -> (kLeft, kRight)
+//   "top center bottom" -> (kTop, kBottom)
+//
+// The axes are not ordered in a particular block/inline or vertical/
+// horizontal order because the axes will be resolved at layout time (see
+// ToPhysical() below).
 class InsetArea {
   DISALLOW_NEW();
 
@@ -58,6 +71,15 @@ class InsetArea {
            span2_start_ == other.span2_start_ && span2_end_ == other.span2_end_;
   }
   bool operator!=(const InsetArea& other) const { return !(*this == other); }
+  bool IsNone() const { return span1_start_ == InsetAreaRegion::kNone; }
+
+  // Convert the computed inset-area into a physical representation where the
+  // first span is always a top/center/bottom span, and the second is a
+  // left/center/right span. If the inset-area is not valid, all regions will be
+  // InsetAreaRegion::kNone.
+  CORE_EXPORT InsetArea
+  ToPhysical(const WritingDirectionMode& container_writing_direction,
+             const WritingDirectionMode& self_writing_direction) const;
 
  private:
   InsetAreaRegion span1_start_ = InsetAreaRegion::kNone;
---
+++ third_party/blink/renderer/core/style/inset_area_test.cc
@@ -0,0 +1,93 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/style/inset_area.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/text/writing_direction_mode.h"
+
+namespace blink {
+
+const WritingDirectionMode vertical_lr_rtl(WritingMode::kVerticalLr,
+                                           TextDirection::kRtl);
+const WritingDirectionMode vertical_rl_ltr(WritingMode::kVerticalRl,
+                                           TextDirection::kLtr);
+const WritingDirectionMode horizontal_tb_ltr(WritingMode::kHorizontalTb,
+                                             TextDirection::kLtr);
+const WritingDirectionMode horizontal_tb_rtl(WritingMode::kHorizontalTb,
+                                             TextDirection::kRtl);
+
+struct ToPhysicalTestCase {
+  InsetArea logical;
+  InsetArea expected_physical;
+  WritingDirectionMode container_writing_direction;
+  WritingDirectionMode self_writing_direction;
+};
+
+ToPhysicalTestCase to_physical_test_cases[] = {
+    {
+        {InsetAreaRegion::kAll, InsetAreaRegion::kAll, InsetAreaRegion::kTop,
+         InsetAreaRegion::kCenter},
+        {InsetAreaRegion::kTop, InsetAreaRegion::kCenter,
+         InsetAreaRegion::kLeft, InsetAreaRegion::kRight},
+        horizontal_tb_ltr,
+        horizontal_tb_ltr,
+    },
+    {
+        {InsetAreaRegion::kXStart, InsetAreaRegion::kXStart,
+         InsetAreaRegion::kYStart, InsetAreaRegion::kYStart},
+        {InsetAreaRegion::kTop, InsetAreaRegion::kTop, InsetAreaRegion::kRight,
+         InsetAreaRegion::kRight},
+        horizontal_tb_rtl,
+        horizontal_tb_ltr,
+    },
+    {
+        {InsetAreaRegion::kXSelfEnd, InsetAreaRegion::kXSelfEnd,
+         InsetAreaRegion::kYSelfEnd, InsetAreaRegion::kYSelfEnd},
+        {InsetAreaRegion::kBottom, InsetAreaRegion::kBottom,
+         InsetAreaRegion::kLeft, InsetAreaRegion::kLeft},
+        horizontal_tb_ltr,
+        horizontal_tb_rtl,
+    },
+    {
+        // block-axis (containing block) / inline-axis (containing block) since
+        // both are neutral. First span becomes physical "center right" because
+        // of vertical-rl / ltr. Second becomes "center bottom" because of
+        // horizontal-tb / rtl.
+        {InsetAreaRegion::kStart, InsetAreaRegion::kCenter,
+         InsetAreaRegion::kCenter, InsetAreaRegion::kSelfEnd},
+        {InsetAreaRegion::kCenter, InsetAreaRegion::kBottom,
+         InsetAreaRegion::kCenter, InsetAreaRegion::kRight},
+        vertical_rl_ltr,
+        horizontal_tb_rtl,
+    },
+    {
+        // block-axis (self) / inline-axis (self) since both are neutral. First
+        // span becomes physical "right" because of vertical-lr. Second becomes
+        // "bottom" because of rtl.
+        {InsetAreaRegion::kSelfStart, InsetAreaRegion::kSelfStart,
+         InsetAreaRegion::kSelfEnd, InsetAreaRegion::kSelfEnd},
+        {InsetAreaRegion::kBottom, InsetAreaRegion::kBottom,
+         InsetAreaRegion::kRight, InsetAreaRegion::kRight},
+        horizontal_tb_ltr,
+        vertical_lr_rtl,
+    },
+};
+
+class InsetAreaToPhysicalTest
+    : public testing::Test,
+      public testing::WithParamInterface<ToPhysicalTestCase> {};
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         InsetAreaToPhysicalTest,
+                         testing::ValuesIn(to_physical_test_cases));
+
+TEST_P(InsetAreaToPhysicalTest, All) {
+  const ToPhysicalTestCase& test_case = GetParam();
+  EXPECT_EQ(test_case.logical.ToPhysical(test_case.container_writing_direction,
+                                         test_case.self_writing_direction),
+            test_case.expected_physical);
+}
+
+}  // namespace blink
