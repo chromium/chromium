836c8023fdf6242545fd41e2ed1fcd20f56cbbc9
Dave Tapuska <dtapuska@chromium.org>
2023-11-29 19:55:22 +0000

    [extensions] Fix issue with MessagePort sending
    One crash report indicates that ShouldSkipFrameForBFCache may call
    RenderFrameDeleted which could mutate frames_ and pending_frames_
    while we are iterating on them. So clone the keys before iterating
    on the structures.
    
    Bug: 1504066
    Change-Id: I6dcad41d484f7c99c3457259c14d4a054c8aa8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5070893
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1230848}

--- extensions/browser/api/messaging/extension_message_port.cc
+++ extensions/browser/api/messaging/extension_message_port.cc
@@ -435,7 +435,10 @@ void ExtensionMessagePort::DispatchOnConnect(
   info->guest_process_id = guest_process_id;
   info->guest_render_frame_routing_id = guest_render_frame_routing_id;
 
-  for (const auto& frame_token : pending_frames_) {
+  // `ShouldSkipFrameForBFCache` could mutate `pending_frames_` so we
+  // take it before iterating on it.
+  auto pending_frames = std::move(pending_frames_);
+  for (const auto& frame_token : pending_frames) {
     auto* frame = content::RenderFrameHost::FromFrameToken(frame_token);
     if (!frame || ShouldSkipFrameForBFCache(frame)) {
       continue;
@@ -835,26 +838,27 @@ void ExtensionMessagePort::SendToPort(SendCallback send_callback) {
   // We should have called OnConnect before SentToPort.
   CHECK(pending_frames_.empty());
   CHECK(pending_service_workers_.empty());
-  std::vector<mojom::MessagePort*> targets;
+  std::vector<content::GlobalRenderFrameHostToken> frame_targets;
   // Build the list of targets.
   for (const auto& item : frames_) {
-    auto* frame = content::RenderFrameHost::FromFrameToken(item.first);
-    if (!frame || ShouldSkipFrameForBFCache(frame)) {
+    frame_targets.push_back(item.first);
+  }
+
+  for (const auto& target : frame_targets) {
+    auto item = frames_.find(target);
+    // `ShouldSkipFrameForBFCache` can mutate `frames_`, so verify the frame
+    // still exists.
+    if (item == frames_.end()) {
       continue;
     }
-    if (item.second.is_bound()) {
-      targets.push_back(item.second.get());
+    auto* frame = content::RenderFrameHost::FromFrameToken(item->first);
+    if (frame && !ShouldSkipFrameForBFCache(frame)) {
+      send_callback.Run(item->second.get());
     }
   }
 
   for (const auto& running_worker : service_workers_) {
-    if (running_worker.second.is_bound()) {
-      targets.push_back(running_worker.second.get());
-    }
-  }
-
-  for (auto* target : targets) {
-    send_callback.Run(target);
+    send_callback.Run(running_worker.second.get());
   }
 }
 #endif
