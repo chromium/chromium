6afcb4bb85841c16a38f958c84efbec3284b6b9a
Richard Chen <richche@chromium.org>
2023-11-30 03:38:41 +0000

    [CodeHealth] Remove Enterprise.ONC.PolicyValidation expired histogram
    As part of the Chrome code health rotation effort this CL removes the
    expired Enterprise.ONC.PolicyValidation histogram and
    cleans up the code using it.
    
    OBSOLETE_HISTOGRAM[Enterprise.ONC.PolicyValidation]=expired histogram.
    
    Fixed: 1499377
    Change-Id: Iae8adfbb6a04d0ee479f779bbc68f26c3ead0377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5049606
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231120}

--- chromeos/components/onc/onc_utils.cc
+++ chromeos/components/onc/onc_utils.cc
@@ -10,7 +10,6 @@
 #include "base/base64.h"
 #include "base/json/json_reader.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
@@ -686,11 +685,6 @@ bool ParseAndValidateOncForImport(const std::string& onc_blob,
       validator.ValidateAndRepairObject(&kToplevelConfigurationSignature,
                                         toplevel_onc.value(),
                                         &validation_result);
-
-  if (from_policy) {
-    UMA_HISTOGRAM_BOOLEAN("Enterprise.ONC.PolicyValidation",
-                          validation_result == Validator::VALID);
-  }
 
   bool success = true;
   if (validation_result == Validator::VALID_WITH_WARNINGS) {
