e60aa95cfa68fd1c61b9c8e8d67b2df8d517c719
Clark DuVall <cduvall@chromium.org>
2023-11-30 18:42:57 +0000

    Launch MmapSafeBrowsingDatabase and related features
    Bug: 1380507
    Change-Id: Id70be35eca33fc32a222bb5216e0f1266e852267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072254
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231423}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -186,11 +186,23 @@ BASE_FEATURE(kLogAccountEnhancedProtectionStateInProtegoPings,
 
 BASE_FEATURE(kMmapSafeBrowsingDatabase,
              "MmapSafeBrowsingDatabase",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+// TODO(crbug.com/1380507): Fix iOS tests with this enabled.
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_DISABLED_BY_DEFAULT
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+);
 
 constexpr base::FeatureParam<bool> kMmapSafeBrowsingDatabaseAsync{
     &kMmapSafeBrowsingDatabase, "MmapSafeBrowsingDatabaseAsync",
-    /*default_value=*/false};
+// TODO(crbug.com/1380507): Fix iOS tests with this enabled.
+#if BUILDFLAG(IS_IOS)
+    /*default_value=*/false
+#else
+    /*default_value=*/true
+#endif
+};
 
 BASE_FEATURE(kNestedArchives,
              "SafeBrowsingArchiveImprovements",
@@ -250,7 +262,13 @@ BASE_FEATURE(kSafeBrowsingNewGmsApiForBrowseUrlDatabaseCheck,
 
 BASE_FEATURE(kSafeBrowsingOnUIThread,
              "SafeBrowsingOnUIThread",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+// TODO(crbug.com/1380507): Fix iOS tests with this enabled.
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_DISABLED_BY_DEFAULT
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kSafeBrowsingReferrerChainWithCopyPasteNavigation,
              "SafeBrowsingReferrerChainWithCopyPasteNavigation",
