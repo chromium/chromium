4a7009a1bd5a846ea19aa6d6fe8659a75fc75d48
Evan Stade <estade@chromium.org>
2023-11-30 00:08:13 +0000

    IndexedDB: simplify IndexedDBControlWrapper
    IndexedDBControlWrapper was an IndexedDBControl implementation that
    simply passed through all operations to an underlying IndexedDBControl
    (IndexedDBContextImpl). The only functionality that it added was to
    interact with the SpecialStoragePolicy whenever a new IndexedDBFactory
    connection is established.
    
    It's simpler and more idiomatic to accomplish this by being an
    observer of the underlying IndexedDBContextImpl rather intercepting
    messages meant for the IndexedDBContextImpl. Luckily, such an observer
    interface already exists.
    
    Bug: 717812
    Change-Id: If7adf390cb34dc52e35fa4a27983f3d8a283fb00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5069675
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231011}

--- content/browser/indexed_db/indexed_db_control_wrapper.cc
+++ content/browser/indexed_db/indexed_db_control_wrapper.cc
@@ -29,8 +29,9 @@ IndexedDBControlWrapper::IndexedDBControlWrapper(
 
   if (special_storage_policy) {
     storage_policy_observer_.emplace(
-        base::BindRepeating(&IndexedDBControlWrapper::ApplyPolicyUpdates,
-                            weak_factory_.GetWeakPtr()),
+        base::BindRepeating(
+            &IndexedDBControlWrapper::OnSpecialStoragePolicyUpdated,
+            base::Unretained(this)),
         io_task_runner, std::move(special_storage_policy));
   }
 }
@@ -42,103 +43,42 @@ IndexedDBControlWrapper::~IndexedDBControlWrapper() {
   IndexedDBContextImpl::ReleaseOnIDBSequence(std::move(context_));
 }
 
-void IndexedDBControlWrapper::BindIndexedDB(
-    const storage::BucketLocator& bucket_locator,
-    mojo::PendingRemote<storage::mojom::IndexedDBClientStateChecker>
-        client_state_checker_remote,
-    mojo::PendingReceiver<blink::mojom::IDBFactory> receiver) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  if (storage_policy_observer_) {
-    // TODO(https://crbug.com/1199077): Pass the real StorageKey once
-    // StoragePolicyObserver is migrated.
-    storage_policy_observer_->StartTrackingOrigin(
-        bucket_locator.storage_key.origin());
-  }
-  indexed_db_control_->BindIndexedDB(bucket_locator,
-                                     std::move(client_state_checker_remote),
-                                     std::move(receiver));
-}
-
-void IndexedDBControlWrapper::DeleteForStorageKey(
-    const blink::StorageKey& storage_key,
-    DeleteForStorageKeyCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->DeleteForStorageKey(storage_key, std::move(callback));
-}
-
-void IndexedDBControlWrapper::ForceClose(
-    storage::BucketId bucket_id,
-    storage::mojom::ForceCloseReason reason,
-    base::OnceClosure callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->ForceClose(bucket_id, reason, std::move(callback));
-}
-
-void IndexedDBControlWrapper::GetConnectionCount(
-    storage::BucketId bucket_id,
-    GetConnectionCountCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->GetConnectionCount(bucket_id, std::move(callback));
-}
-
-void IndexedDBControlWrapper::DownloadBucketData(
-    storage::BucketId bucket_id,
-    DownloadBucketDataCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->DownloadBucketData(bucket_id, std::move(callback));
-}
-
-void IndexedDBControlWrapper::GetAllBucketsDetails(
-    GetAllBucketsDetailsCallback callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->GetAllBucketsDetails(std::move(callback));
-}
-
-void IndexedDBControlWrapper::SetForceKeepSessionState() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->SetForceKeepSessionState();
-}
-
-void IndexedDBControlWrapper::ApplyPolicyUpdates(
-    std::vector<storage::mojom::StoragePolicyUpdatePtr> policy_updates) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->ApplyPolicyUpdates(std::move(policy_updates));
-}
-
-void IndexedDBControlWrapper::BindTestInterface(
-    mojo::PendingReceiver<storage::mojom::IndexedDBControlTest> receiver) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->BindTestInterface(std::move(receiver));
-}
-
-void IndexedDBControlWrapper::AddObserver(
-    mojo::PendingRemote<storage::mojom::IndexedDBObserver> observer) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  BindRemoteIfNeeded();
-  indexed_db_control_->AddObserver(std::move(observer));
-}
-
-void IndexedDBControlWrapper::BindRemoteIfNeeded() {
+storage::mojom::IndexedDBControl&
+IndexedDBControlWrapper::GetIndexedDBControl() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(
       !(indexed_db_control_.is_bound() && !indexed_db_control_.is_connected()))
       << "Rebinding is not supported yet.";
 
-  if (indexed_db_control_.is_bound())
-    return;
-  context_->IDBTaskRunner()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&IndexedDBContextImpl::Bind, context_,
-                     indexed_db_control_.BindNewPipeAndPassReceiver()));
+  if (!indexed_db_control_.is_bound()) {
+    context_->IDBTaskRunner()->PostTask(
+        FROM_HERE,
+        base::BindOnce(&IndexedDBContextImpl::Bind, context_,
+                       indexed_db_control_.BindNewPipeAndPassReceiver()));
+
+    if (storage_policy_observer_) {
+      mojo::PendingRemote<storage::mojom::IndexedDBObserver> remote;
+      receiver_.Bind(remote.InitWithNewPipeAndPassReceiver());
+      indexed_db_control_->AddObserver(std::move(remote));
+    }
+  }
+
+  return *indexed_db_control_;
+}
+
+void IndexedDBControlWrapper::OnIndexedDBListChanged(
+    const storage::BucketLocator& bucket_locator) {
+  CHECK(storage_policy_observer_);
+  // TODO(https://crbug.com/1199077): Pass the real StorageKey once
+  // StoragePolicyObserver is migrated.
+  storage_policy_observer_->StartTrackingOrigin(
+      bucket_locator.storage_key.origin());
+}
+
+void IndexedDBControlWrapper::OnSpecialStoragePolicyUpdated(
+    std::vector<storage::mojom::StoragePolicyUpdatePtr> policy_updates) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  GetIndexedDBControl().ApplyPolicyUpdates(std::move(policy_updates));
 }
 
 }  // namespace content
