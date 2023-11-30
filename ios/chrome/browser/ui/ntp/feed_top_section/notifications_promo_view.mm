66c34522ef1d7526e7949496b7e1053e5b9bf503
Guillem Perez <guiperez@google.com>
2023-11-30 22:04:20 +0000

    [iOS] Add text to the notifications promo.
    Adding a UILabel to the notifications promo to display the main content.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Change-Id: I15248d17a48ed1f5a5da8e1b89c22a309857ce1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5073196
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1231533}

--- ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
+++ ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view.mm
@@ -9,9 +9,16 @@
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/ntp/feed_top_section/notifications_promo_view_constants.h"
 #import "ios/chrome/common/button_configuration_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 namespace {
+// Horizontal spacing between stackView and cell contentView.
+constexpr CGFloat kStackViewPadding = 16.0;
+constexpr CGFloat kStackViewTrailingMargin = 19.0;
+constexpr CGFloat kStackViewSubViewSpacing = 12.0;
 // Margins for the close button.
 constexpr CGFloat kCloseButtonTrailingMargin = -8.0;
 constexpr CGFloat kCloseButtonTopMargin = 8.0;
@@ -20,8 +27,12 @@ constexpr CGFloat kCloseButtonWidthHeight = 24;
 }  // namespace
 
 @interface NotificationsPromoView ()
-// Redeclare as read-write.
+
+@property(nonatomic, strong) UILabel* textLabel;
 @property(nonatomic, strong, readwrite) UIButton* closeButton;
+
+// Stack View containing all internal views on the promo.
+@property(nonatomic, strong) UIStackView* promoStackView;
 
 @end
 
@@ -31,30 +42,66 @@ constexpr CGFloat kCloseButtonWidthHeight = 24;
 - (instancetype)initWithFrame:(CGRect)frame {
   self = [super initWithFrame:frame];
   if (self) {
+    _textLabel = [self createTextLabel];
+    _promoStackView = [self createPromoStackWithViewsArray:@[ _textLabel ]];
     _closeButton = [self createCloseButton];
+    [self addSubview:_promoStackView];
     [self addSubview:_closeButton];
-
-    // Constraints that apply to all styles.
-    [NSLayoutConstraint activateConstraints:@[
-      // Close button size constraints.
-      [_closeButton.heightAnchor
-          constraintEqualToConstant:kCloseButtonWidthHeight],
-      [_closeButton.widthAnchor
-          constraintEqualToConstant:kCloseButtonWidthHeight],
-      [_closeButton.trailingAnchor
-          constraintEqualToAnchor:self.trailingAnchor
-                         constant:kCloseButtonTrailingMargin],
-      [_closeButton.topAnchor constraintEqualToAnchor:self.topAnchor
-                                             constant:kCloseButtonTopMargin],
-      [_closeButton.bottomAnchor
-          constraintEqualToAnchor:self.bottomAnchor
-                         constant:-kCloseButtonTopMargin],
-    ]];
+    [self activateConstraints];
   }
   return self;
 }
 
 #pragma mark - Private
+
+- (void)activateConstraints {
+  [NSLayoutConstraint activateConstraints:@[
+    // Stack View Constraints.
+    [self.promoStackView.leadingAnchor
+        constraintEqualToAnchor:self.leadingAnchor
+                       constant:kStackViewPadding],
+    [self.promoStackView.topAnchor constraintEqualToAnchor:self.topAnchor
+                                                  constant:kStackViewPadding],
+    [self.promoStackView.bottomAnchor
+        constraintEqualToAnchor:self.bottomAnchor
+                       constant:-kStackViewPadding],
+    [self.promoStackView.trailingAnchor
+        constraintEqualToAnchor:self.trailingAnchor
+                       constant:-kStackViewTrailingMargin],
+    // Close button size constraints.
+    [self.closeButton.heightAnchor
+        constraintEqualToConstant:kCloseButtonWidthHeight],
+    [self.closeButton.widthAnchor
+        constraintEqualToConstant:kCloseButtonWidthHeight],
+    [self.closeButton.trailingAnchor
+        constraintEqualToAnchor:self.trailingAnchor
+                       constant:kCloseButtonTrailingMargin],
+    [self.closeButton.topAnchor constraintEqualToAnchor:self.topAnchor
+                                               constant:kCloseButtonTopMargin]
+  ]];
+}
+
+- (UIStackView*)createPromoStackWithViewsArray:(NSArray*)views {
+  UIStackView* stackView = [[UIStackView alloc] initWithArrangedSubviews:views];
+  stackView.alignment = UIStackViewAlignmentCenter;
+  stackView.axis = UILayoutConstraintAxisVertical;
+  stackView.translatesAutoresizingMaskIntoConstraints = NO;
+  stackView.spacing = kStackViewSubViewSpacing;
+  return stackView;
+}
+
+- (UILabel*)createTextLabel {
+  UILabel* textLabel = [[UILabel alloc] init];
+  textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+  textLabel.numberOfLines = 0;
+  textLabel.textAlignment = NSTextAlignmentCenter;
+  textLabel.lineBreakMode = NSLineBreakByWordWrapping;
+  textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+  textLabel.textColor = [UIColor colorNamed:kGrey800Color];
+  textLabel.text =
+      l10n_util::GetNSString(IDS_IOS_CONTENT_NOTIFICATIONS_PROMO_TEXT);
+  return textLabel;
+}
 
 // Creates the close button for the Promo.
 - (UIButton*)createCloseButton {
