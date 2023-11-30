bde76fd7be535364963be1a9d9ea30f564916cfe
Bryan Cain <bryancain@chromium.org>
2023-11-30 22:02:15 +0000

    printing: Turn on EnableBorderlessPrinting feature flag by default
    The feature has been fully approved now.
    
    Bug: b:314146164
    Change-Id: I89972bb210452f5ad6485a1af8561750b4c2ff18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076603
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Bryan Cain <bryancain@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231530}

--- printing/printing_features.cc
+++ printing/printing_features.cc
@@ -18,7 +18,7 @@ namespace features {
 // Enable support for borderless printing and media type.
 BASE_FEATURE(kEnableBorderlessPrinting,
              "EnableBorderlessPrinting",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(IS_MAC)
