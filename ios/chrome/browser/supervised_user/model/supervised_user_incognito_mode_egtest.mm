0e4d3e8b4487ae43ec1ffec8fb7cd17075b788a8
Duong Dac <ddac@google.com>
2023-11-30 18:23:43 +0000

    [iOS] Supervised incognito egtest cleanup
    Change-Id: I0be60af82781d69654d47d91efbc5b569adbdd18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076960
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231402}

--- ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_incognito_mode_egtest.mm
@@ -3,10 +3,8 @@
 // found in the LICENSE file.
 
 #import "base/feature_list.h"
-#import "base/strings/sys_string_conversions.h"
 #import "components/supervised_user/core/common/features.h"
 #import "ios/chrome/browser/policy/policy_earl_grey_matchers.h"
-#import "ios/chrome/browser/signin/model/capabilities_types.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.h"
@@ -19,7 +17,6 @@
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/app_launch_configuration.h"
-#import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
 using chrome_test_util::ContainsPartialText;
@@ -49,14 +46,6 @@ NSString* const kTestLearnMoreLabel = @"Learn more";
   config.features_enabled.push_back(
       supervised_user::kFilterWebsitesForSupervisedUsersOnDesktopAndIOS);
   return config;
-}
-
-- (void)setUp {
-  [super setUp];
-}
-
-- (void)tearDown {
-  [super tearDown];
 }
 
 // Signs in with a supervised account.
