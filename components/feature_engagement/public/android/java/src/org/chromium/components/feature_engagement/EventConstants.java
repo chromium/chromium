036708da1192cb67de52fc3c19c8b1f9e6d98547
Wenyu Fu <wenyufu@google.com>
2023-11-30 19:13:47 +0000

    Remove unused EventConstants in code
    Bug: N/A
    Change-Id: I3ca4a4fe5f8365a39590c240f51b97331a4d55f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5074896
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231439}

--- components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/EventConstants.java
+++ components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/EventConstants.java
@@ -22,74 +22,14 @@ public final class EventConstants {
     public static final String SCREENSHOT_TAKEN_CHROME_IN_FOREGROUND =
             "screenshot_taken_chrome_in_foreground";
 
-    /** The data saver preview infobar was shown. */
-    public static final String DATA_SAVER_PREVIEW_INFOBAR_SHOWN = "data_saver_preview_opened";
-
-    /** Data was saved when page loaded. */
-    public static final String DATA_SAVED_ON_PAGE_LOAD = "data_saved_page_load";
-
-    /** The overflow menu was opened. */
-    public static final String OVERFLOW_OPENED_WITH_DATA_SAVER_SHOWN =
-            "overflow_opened_data_saver_shown";
-
-    /** The data saver footer was used (tapped). */
-    public static final String DATA_SAVER_DETAIL_OPENED = "data_saver_overview_opened";
-
-    /** The data saver milestone promo was used (tapped). */
-    public static final String DATA_SAVER_MILESTONE_PROMO_OPENED = "data_saver_milestone_promo";
-
-    /** The previews verbose status view was opened. */
-    public static final String PREVIEWS_VERBOSE_STATUS_OPENED = "previews_verbose_status_opened";
-
-    /** A page load used a preview. */
-    public static final String PREVIEWS_PAGE_LOADED = "preview_page_load";
-
     /** Add to homescreen events. */
     public static final String ADD_TO_HOMESCREEN_DIALOG_SHOWN = "add_to_homescreen_dialog_shown";
 
     /** The user clicked the minimize button on the toolbar. */
     public static final String CCT_MINIMIZE_BUTTON_CLICKED = "cct_minimize_button_clicked";
 
-    /** Contextual Search panel was opened. */
-    public static final String CONTEXTUAL_SEARCH_PANEL_OPENED = "contextual_search_panel_opened";
-
-    /** Contextual Search panel was opened after it was triggered by tapping. */
-    public static final String CONTEXTUAL_SEARCH_PANEL_OPENED_AFTER_TAP =
-            "contextual_search_panel_opened_after_tap";
-
-    /** Contextual Search panel was opened after it was triggered by longpressing. */
-    public static final String CONTEXTUAL_SEARCH_PANEL_OPENED_AFTER_LONGPRESS =
-            "contextual_search_panel_opened_after_longpress";
-
-    /** Contextual Search panel was opened after receiving entity data. */
-    public static final String CONTEXTUAL_SEARCH_PANEL_OPENED_FOR_ENTITY =
-            "contextual_search_panel_opened_for_entity";
-
     /** User performed a web search for a query by choosing the Web Search option on the popup menu. */
     public static final String WEB_SEARCH_PERFORMED = "web_search_performed";
-
-    /** Contextual Search showed an entity result for the searched query. */
-    public static final String CONTEXTUAL_SEARCH_ENTITY_RESULT = "contextual_search_entity_result";
-
-    /** Contextual Search was triggered by a tap. */
-    public static final String CONTEXTUAL_SEARCH_TRIGGERED_BY_TAP =
-            "contextual_search_triggered_by_tap";
-
-    /** Contextual Search was triggered by longpressing. */
-    public static final String CONTEXTUAL_SEARCH_TRIGGERED_BY_LONGPRESS =
-            "contextual_search_triggered_by_longpress";
-
-    /** Contextual Search attempted-trigger by Tap when user should Long-press. */
-    public static final String CONTEXTUAL_SEARCH_TAPPED_BUT_SHOULD_LONGPRESS =
-            "contextual_search_tapped_but_should_longpress";
-
-    /** Contextual Search acknowledged the suggestion that they should longpress instead of tap. */
-    public static final String CONTEXTUAL_SEARCH_ACKNOWLEDGED_IN_PANEL_HELP =
-            "contextual_search_acknowledged_in_panel_help";
-
-    /** Contextual Search user fully enabled access to page content through the opt-in. */
-    public static final String CONTEXTUAL_SEARCH_ENABLED_OPT_IN =
-            "contextual_search_enabled_opt_in";
 
     /** The partner homepage was pressed. */
     public static final String PARTNER_HOME_PAGE_BUTTON_PRESSED =
@@ -97,9 +37,6 @@ public final class EventConstants {
 
     /** The homepage button in the toolbar was clicked. */
     public static final String HOMEPAGE_BUTTON_CLICKED = "homepage_button_clicked";
-
-    /** The clear tab button in the toolbar was clicked. */
-    public static final String CLEAR_TAB_BUTTON_CLICKED = "clear_tab_button_clicked";
 
     /** The pinned homepage tile in MV tiles was clicked. */
     public static final String HOMEPAGE_TILE_CLICKED = "homepage_tile_clicked";
@@ -121,10 +58,6 @@ public final class EventConstants {
 
     /** The keyboard accessory was swiped to reveal more suggestions. */
     public static final String KEYBOARD_ACCESSORY_BAR_SWIPED = "keyboard_accessory_bar_swiped";
-
-    /** The keyboard accessory was used to fill virtual card CVC data into a form. */
-    public static final String KEYBOARD_ACCESSORY_VIRTUAL_CARD_CVC_AUTOFILLED =
-            "autofill_virtual_card_cvc_suggestion_accepted";
 
     /** User has finished drop-to-merge to create a group. */
     public static final String TAB_DRAG_AND_DROP_TO_GROUP = "tab_drag_and_drop_to_group";
@@ -148,36 +81,12 @@ public final class EventConstants {
 
     public static final String READ_LATER_CONTEXT_MENU_TAPPED = "read_later_context_menu_tapped";
     public static final String READ_LATER_ARTICLE_SAVED = "read_later_article_saved";
-    public static final String READ_LATER_BOTTOM_SHEET_FOLDER_SEEN =
-            "read_later_bottom_sheet_folder_seen";
     public static final String READ_LATER_BOOKMARK_FOLDER_OPENED =
             "read_later_bookmark_folder_opened";
 
     /** The request desktop site window setting IPH was shown. */
     public static final String REQUEST_DESKTOP_SITE_WINDOW_SETTING_IPH_SHOWN =
             "request_desktop_site_window_setting_iph_shown";
-
-    /** Video tutorial related events. */
-    public static final String VIDEO_TUTORIAL_DISMISSED_SUMMARY =
-            "video_tutorial_iph_dismissed_summary";
-
-    public static final String VIDEO_TUTORIAL_DISMISSED_CHROME_INTRO =
-            "video_tutorial_iph_dismissed_chrome_intro";
-    public static final String VIDEO_TUTORIAL_DISMISSED_DOWNLOAD =
-            "video_tutorial_iph_dismissed_download";
-    public static final String VIDEO_TUTORIAL_DISMISSED_SEARCH =
-            "video_tutorial_iph_dismissed_search";
-    public static final String VIDEO_TUTORIAL_DISMISSED_VOICE_SEARCH =
-            "video_tutorial_iph_dismissed_voice_search";
-    public static final String VIDEO_TUTORIAL_CLICKED_SUMMARY =
-            "video_tutorial_iph_clicked_summary";
-    public static final String VIDEO_TUTORIAL_CLICKED_CHROME_INTRO =
-            "video_tutorial_iph_clicked_chrome_intro";
-    public static final String VIDEO_TUTORIAL_CLICKED_DOWNLOAD =
-            "video_tutorial_iph_clicked_download";
-    public static final String VIDEO_TUTORIAL_CLICKED_SEARCH = "video_tutorial_iph_clicked_search";
-    public static final String VIDEO_TUTORIAL_CLICKED_VOICE_SEARCH =
-            "video_tutorial_iph_clicked_voice_search";
 
     /** Reengagement events. */
     public static final String STARTED_FROM_MAIN_INTENT = "started_from_main_intent";
@@ -203,12 +112,6 @@ public final class EventConstants {
 
     /** Sharing Hub link toggle events. */
     public static final String SHARING_HUB_LINK_TOGGLE_CLICKED = "sharing_hub_link_toggle_clicked";
-
-    /** Crow events. */
-    public static final String CROW_TAB_MENU_ITEM_CLICKED = "crow_tab_menu_item_clicked";
-
-    /** Mic toolbar IPH event */
-    public static final String SUCCESSFUL_VOICE_SEARCH = "successful_voice_search";
 
     /** AdaptiveButtonInTopToolbarCustomization new tab events. */
     public static final String ADAPTIVE_TOOLBAR_CUSTOMIZATION_NEW_TAB_OPENED =
@@ -267,10 +170,6 @@ public final class EventConstants {
     /** A tap on the folder icon in the enhanced bookmark save flow. */
     public static final String SHOPPING_LIST_SAVE_FLOW_FOLDER_TAP =
             "shopping_list_save_flow_folder_tap";
-
-    /** An app menu (tab-level) desktop site setting update event. */
-    public static final String APP_MENU_DESKTOP_SITE_FOR_TAB_CLICKED =
-            "app_menu_desktop_site_for_tab_clicked";
 
     /** Desktop site settings page opened. */
     public static final String DESKTOP_SITE_SETTINGS_PAGE_OPENED =
