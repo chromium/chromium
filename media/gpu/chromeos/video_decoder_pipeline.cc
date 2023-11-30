4bef4bc4388aa0cb37cede9c2ea21366a434dda4
Miguel Casas <mcasas@chromium.org>
2023-11-30 03:53:21 +0000

    media/gpu/chromeos: Use less in-flight buffers for V4L2StatefulVD
    V4L2StatefulVD doesn't need to have up to 8 DecoderBuffers in flight,
    and also since those are processed slowly and one at a time on Hana,
    this leads to large average decode delay. This CL uses 4 instead.
    It also improves on a TODO comment.
    
    Bug: b:279980150
    Change-Id: Ib35c501800768f669af8313609f8d5acc159bcd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075272
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231121}

--- media/gpu/chromeos/video_decoder_pipeline.cc
+++ media/gpu/chromeos/video_decoder_pipeline.cc
@@ -391,7 +391,9 @@ void VideoDecoderPipeline::DestroyAsync(
 
 VideoDecoderType VideoDecoderPipeline::GetDecoderType() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(client_sequence_checker_);
-  // TODO(mcasas): query |decoder_| instead.
+  // TODO(mcasas): query |decoder_| instead. This is difficult because it can
+  // only be accessed on |decoder_sequence_checker_|: this method is supposed
+  // to be synchronous.
 
   if (uses_oop_video_decoder_) {
     return VideoDecoderType::kOutOfProcess;
@@ -415,8 +417,16 @@ bool VideoDecoderPipeline::IsPlatformDecoder() const {
 int VideoDecoderPipeline::GetMaxDecodeRequests() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(client_sequence_checker_);
 
-  // TODO(mcasas): query |decoder_| instead.
-  // This value comes from the number of buffers in the input queue in
+  // TODO(mcasas): query |decoder_| instead. This is difficult because it can
+  // only be accessed on |decoder_sequence_checker_|: this method is supposed
+  // to be synchronous.
+
+#if BUILDFLAG(USE_V4L2_CODEC)
+  if (base::FeatureList::IsEnabled(kV4L2FlatStatefulVideoDecoder)) {
+    return 4;
+  }
+#endif
+  // This value comes from the (large) number of buffers in the input queue in
   // V4L2VideoDecoder.
   return 8;
 }
