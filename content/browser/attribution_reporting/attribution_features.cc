670bbc1ba5eaa413dc684a194a540b8c79a53292
Thomas Quintanilla <tquintanilla@google.com>
2023-12-01 19:44:37 +0000

    Fix typo in feature name, adds to field trial experiment
    Bug: 1458510
    Change-Id: If1049b0c02aa97f5b0d65309e5852c4cf86f9cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5079511
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232116}

--- content/browser/attribution_reporting/attribution_features.cc
+++ content/browser/attribution_reporting/attribution_features.cc
@@ -20,7 +20,7 @@ BASE_FEATURE(kAttributionVerboseDebugReporting,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAttributionReportingDeactivateAfterFilterMatch,
-             "kAttributionReportingDeactivateAfterFilterMatch",
+             "AttributionReportingDeactivateAfterFilterMatch",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace content
