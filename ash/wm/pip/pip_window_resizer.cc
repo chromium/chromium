bde6f7b4e841dcaf153fea498e260f485765e3e0
Kazuki Takise <takise@google.com>
2023-12-01 00:34:17 +0000

    pip: Use proper coordinate system in CompleteDrag
    - `intended_bounds` must be in screen coordinates, but we forgot
     to convert it at one place.
    - `PipPositioner::SaveSnapFraction` expects bounds in screen
     coordinates, but `resting_bounds` is in the parent coordinates
     when it's passed to it.
    
    Bug: b/313944158
    Bug: b/313024926
    Test: ash_unittests
    Change-Id: Ie5962da940979318d777bb6ce364f6cc3ecb572e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076333
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1231644}

--- ash/wm/pip/pip_window_resizer.cc
+++ ash/wm/pip/pip_window_resizer.cc
@@ -370,6 +370,8 @@ void PipWindowResizer::CompleteDrag() {
         intended_bounds = last_event_was_pinch_
                               ? CalculateBoundsForPinch(location_in_parent)
                               : CalculateBoundsForDrag(location_in_parent);
+        wm::ConvertRectToScreen(window_state()->window()->GetRootWindow(),
+                                &intended_bounds);
       } else {
         intended_bounds = GetTarget()->GetBoundsInScreen();
       }
@@ -412,6 +414,8 @@ void PipWindowResizer::CompleteDrag() {
     // TODO(edcourtney): This may not be the best place for this. Consider
     // doing this a different way or saving these bounds at a later point when
     // the work area changes.
+    wm::ConvertRectToScreen(window_state()->window()->GetRootWindow(),
+                                &resting_bounds);
     PipPositioner::SaveSnapFraction(window_state(), resting_bounds);
   }
 }
