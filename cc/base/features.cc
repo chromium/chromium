52b71a6a94d4fb5854a94aaae6eb05769a291b29
Vikas Soni <vikassoni@chromium.org>
2023-12-01 19:29:09 +0000

    [UseDMSAAForTiles] Enable UseDMSAAForTilesAndroidGL by default.
    Enable UseDMSAAForTilesAndroidGL by default on android in preparation to
    launch.
    
    Bug: 1366068
    Change-Id: I8a6b36e1d5bd13d94c8e58745663822ff83777d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5076523
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232102}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -100,7 +100,7 @@ BASE_FEATURE(kUseDMSAAForTiles,
 // above flag UseDMSAAForTiles controls the launch on Vulkan backend.
 BASE_FEATURE(kUseDMSAAForTilesAndroidGL,
              "UseDMSAAForTilesAndroidGL",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kUpdateBrowserControlsWithoutProxy,
