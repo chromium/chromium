4f242d073f1be62e4cbd6cc6d89a4b4ed4c8d39f
Brandon Wylie <wylieb@chromium.org>
2023-12-04 22:23:06 +0000

    [bookmarks] Enable AndroidImprovedBookmarks by default
    Bug: 1508129
    Change-Id: I3a04cc9ee44b3061d6b52b5814f5708418d64d46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5085032
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233007}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -491,7 +491,7 @@ BASE_FEATURE(kAndroidSearchEngineChoiceNotification,
 
 BASE_FEATURE(kAndroidImprovedBookmarks,
              "AndroidImprovedBookmarks",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidNoVisibleHintForTablets,
              "AndroidNoVisibleHintForTablets",
