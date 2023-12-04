872eb460552b6de2b583238c69899adcd1aa8bf9
Solomon Kinard <solomonkinard@chromium.org>
2023-12-04 20:09:57 +0000

    Extensions: Prefer base::flat_map in some cases for space savings
    Bug: None
    Change-Id: I1fa40122c6574e2e9e0b5bd03a74a9a62b9ace0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080328
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1232921}

--- extensions/common/extension_icon_set.h
+++ extensions/common/extension_icon_set.h
@@ -5,10 +5,10 @@
 #ifndef EXTENSIONS_COMMON_EXTENSION_ICON_SET_H_
 #define EXTENSIONS_COMMON_EXTENSION_ICON_SET_H_
 
-#include <map>
 #include <set>
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/strings/string_piece.h"
 
 namespace base {
@@ -27,7 +27,7 @@ class ExtensionIconSet {
   };
 
   // Access to the underlying map from icon size->{path, bitmap}.
-  typedef std::map<int, std::string> IconMap;
+  typedef base::flat_map<int, std::string> IconMap;
 
   ExtensionIconSet();
   ExtensionIconSet(const ExtensionIconSet& other);
