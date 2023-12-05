df76d07bfcdfa3291597f88e77b70e0435e28931
Josiah Krutz <josiahk@google.com>
2023-12-05 00:20:33 +0000

    [TS] Migrate c/b/r/c/a/c/array_util.js to Typescript
    Migrate c/b/r/chromeos/accessibility/common/array_util.js to Typescript.
    
    AX-Relnotes: N/A.
    Bug: 267329383
    Change-Id: I36649d5d0acae9954b1609b51bf098bdf8e6a0a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5085936
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233089}

--- chrome/browser/resources/chromeos/accessibility/common/array_util.ts
+++ chrome/browser/resources/chromeos/accessibility/common/array_util.ts
@@ -1,15 +1,10 @@
-// Copyright 2020 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 /** A collection of helper functions when dealing with arrays. */
 export const ArrayUtil = {
-  /**
-   * @param {Array=} array1
-   * @param {Array=} array2
-   * @return {boolean}
-   */
-  contentsAreEqual: (array1, array2) => {
+  contentsAreEqual: <T>(array1?: T[], array2?: T[]): boolean => {
     if (!array1 || !array2 || array1.length !== array2.length) {
       return false;
     }
