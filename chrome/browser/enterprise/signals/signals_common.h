b953483ea01fabeb4b5f6ec1c87637da1a8644ad
Philippe Leblanc <pleblanc@google.com>
2023-12-05 19:47:01 +0000

    Remove dead code that was flagged as missing test coverage.
    Change-Id: I4d896789622ba0dfd6dd39f18a908c20ce17e3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075434
    Auto-Submit: Philippe Leblanc <pleblanc@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233524}

--- chrome/browser/enterprise/signals/signals_common.h
+++ chrome/browser/enterprise/signals/signals_common.h
@@ -15,11 +15,6 @@ enum class SettingValue {
   ENABLED,
 };
 
-// Converts |setting_value| to an optional boolean value. ENABLED and DISABLED
-// will be converted to true and false respectively. Other values will be
-// treated as missing, and nullopt will be returned instead.
-absl::optional<bool> SettingValueToBool(SettingValue setting_value);
-
 }  // namespace enterprise_signals
 
 #endif  // CHROME_BROWSER_ENTERPRISE_SIGNALS_SIGNALS_COMMON_H_
