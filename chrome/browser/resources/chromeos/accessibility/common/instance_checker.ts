9ab2206332b0e58b2da5ad89f4c5c236c9766ee4
Josiah Krutz <josiahk@google.com>
2023-12-05 22:05:57 +0000

    [TS] Migrate c/b/r/c/a/c/instance_checker.js to Typescript
    Migrate c/b/r/c/accessibility/common/instance_checker.js to Typescript.
    
    Add Chrome API Typescript definitions.
    
    AX-Relnotes: N/A.
    Bug: 267329383
    Test: Run Switch Access with an incognito and regular window and no double actions happen.
    Change-Id: I629dd4f63f1337a68d9403c7da36941ab643075d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5086298
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1233595}

--- chrome/browser/resources/chromeos/accessibility/common/instance_checker.ts
+++ chrome/browser/resources/chromeos/accessibility/common/instance_checker.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,19 +7,18 @@
  * simultaneously.
  */
 export class InstanceChecker {
-  static isActiveInstance() {
+  static isActiveInstance(): boolean {
     // In 'split' manifest mode, the extension system runs two copies of the
     // extension. One in an incognito context; the other not. In guest mode, the
     // extension system runs only the extension in an incognito context. To
     // prevent doubling of this extension, only continue for one context.
     const manifest =
-        /** @type {{incognito: (string|undefined)}} */ (
-            chrome.runtime.getManifest());
-    return manifest.incognito !== 'split' ||
+            chrome.runtime.getManifest();
+    return manifest['incognito'] !== 'split' ||
         chrome.extension.inIncognitoContext;
   }
 
-  static closeExtraInstances() {
+  static closeExtraInstances(): void {
     if (!InstanceChecker.isActiveInstance()) {
       window.close();
     }
