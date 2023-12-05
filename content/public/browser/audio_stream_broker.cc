23e9721fb945243fdf342235e9bec24df12c6288
Patrick Monette <pmonette@chromium.org>
2023-12-05 22:13:46 +0000

    Move NotifyHostOfStarted/StoppedStream out of content/public/
    These 2 functions are only ever used inside of content/.
    
    Change-Id: I3235637929ce96663c4f3c026067e29c95f91880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5089868
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233607}

--- content/public/browser/audio_stream_broker.cc
+++ content/public/browser/audio_stream_broker.cc
@@ -4,14 +4,6 @@
 
 #include "content/public/browser/audio_stream_broker.h"
 
-#include <utility>
-
-#include "base/functional/bind.h"
-#include "base/location.h"
-#include "content/browser/renderer_host/render_frame_host_impl.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
-
 namespace content {
 
 AudioStreamBroker::LoopbackSink::LoopbackSink() = default;
@@ -24,28 +16,6 @@ AudioStreamBroker::AudioStreamBroker(int render_process_id, int render_frame_id)
     : render_process_id_(render_process_id),
       render_frame_id_(render_frame_id) {}
 AudioStreamBroker::~AudioStreamBroker() = default;
-
-void AudioStreamBroker::NotifyHostOfStartedStream() {
-  auto impl = [](int render_process_id, int render_frame_id) {
-    if (auto* host =
-            RenderFrameHostImpl::FromID(render_process_id, render_frame_id)) {
-      host->OnMediaStreamAdded();
-    }
-  };
-  GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(impl, render_process_id(), render_frame_id()));
-}
-
-void AudioStreamBroker::NotifyHostOfStoppedStream() {
-  auto impl = [](int render_process_id, int render_frame_id) {
-    if (auto* host =
-            RenderFrameHostImpl::FromID(render_process_id, render_frame_id)) {
-      host->OnMediaStreamRemoved();
-    }
-  };
-  GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(impl, render_process_id(), render_frame_id()));
-}
 
 AudioStreamBrokerFactory::AudioStreamBrokerFactory() = default;
 AudioStreamBrokerFactory::~AudioStreamBrokerFactory() = default;
