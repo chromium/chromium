f0c241b8cfcfad480692e83f504d8911371545df
Julie Jeongeun Kim <jkim@igalia.com>
2023-12-05 12:26:28 +0000

    [CodeHealth] Fix dangling raw_ptr in QuitWithAppsController
    This CL fixes dangling raw_ptr by resetting `notification_profile_`
    when ProfileManager is destroyed.
    
    Bug: 1462042
    Change-Id: I68d36c83ac02d2849beb717b6d45d473e6a11133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5047614
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233274}

--- chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
+++ chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac.cc
@@ -77,9 +77,19 @@ QuitWithAppsController::QuitWithAppsController() {
       message_center::NotifierId(message_center::NotifierType::SYSTEM_COMPONENT,
                                  kQuitWithAppsNotificationID),
       rich_notification_data, this);
+  if (ProfileManager* profile_manager = g_browser_process->profile_manager()) {
+    profile_manager_observation_.Observe(profile_manager);
+  }
 }
 
 QuitWithAppsController::~QuitWithAppsController() {}
+
+void QuitWithAppsController::OnProfileManagerDestroying() {
+  // Set `notification_profile_` to null to avoid danling pointer detection when
+  // ProfileManager is destroyed.
+  notification_profile_ = nullptr;
+  profile_manager_observation_.Reset();
+}
 
 void QuitWithAppsController::Close(bool by_user) {
   if (by_user)
