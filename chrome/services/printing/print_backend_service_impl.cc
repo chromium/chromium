242cf8cd2603b9617955625095114b2cec750249
Alan Screen <awscreen@chromium.org>
2023-12-05 23:08:54 +0000

    Set crash keys before start of OOP printing
    Ensure that crash keys are set before the start of printing a document
    in the PrintBackend service.  This is done once the print settings are
    finally known, which varies depending upon the configuration and print
    scenario.
    
    Bug: 1508178
    Change-Id: Idf18db269d850c74c405c3167f720f7f0699962d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087733
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233634}

--- chrome/services/printing/print_backend_service_impl.cc
+++ chrome/services/printing/print_backend_service_impl.cc
@@ -15,6 +15,7 @@
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -743,6 +744,11 @@ void PrintBackendServiceImpl::StartPrinting(
 
 #if !BUILDFLAG(ENABLE_OOP_BASIC_PRINT_DIALOG)
   if (settings) {
+    // Now have the printer name for the print job for use with crash keys.
+    std::string printer_name = base::UTF16ToUTF8(settings->device_name());
+    crash_keys_ = std::make_unique<crash_keys::ScopedPrinterInfo>(
+        printer_name, print_backend_->GetPrinterDriverInfo(printer_name));
+
     // Apply the settings from the in-browser system dialog to the context.
     context_container->context->SetPrintSettings(*settings);
   }
@@ -853,6 +859,13 @@ void PrintBackendServiceImpl::OnDidAskUserForSettings(
     std::move(callback).Run(mojom::PrintSettingsResult::NewResultCode(result));
     return;
   }
+
+  // Set the crash keys for the document that should start printing next.
+  std::string printer_name =
+      base::UTF16ToUTF8(context->settings().device_name());
+  crash_keys_ = std::make_unique<crash_keys::ScopedPrinterInfo>(
+      printer_name, print_backend_->GetPrinterDriverInfo(printer_name));
+
   std::move(callback).Run(mojom::PrintSettingsResult::NewSettings(
       *context->TakeAndResetSettings()));
 }
