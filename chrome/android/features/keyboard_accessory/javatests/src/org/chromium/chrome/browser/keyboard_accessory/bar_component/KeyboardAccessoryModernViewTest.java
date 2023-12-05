05a1ad0755f33f2ea150de9c06cc9047536f4b64
Friedrich Horschig <fhorschig@chromium.org>
2023-12-05 16:16:32 +0000

    [Android][Mfill] Revive former flaky testDismissesSwipingEducationBubbleOnTap
    The test reviver says flakiness is gone as sudden as it came.
    
    Fixed: 1432551, 1504886
    Change-Id: Ie6342133fa88cd04b7f2a651f685781fe00c071f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088410
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Auto-Submit: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233372}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryModernViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryModernViewTest.java
@@ -66,7 +66,6 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -514,7 +513,6 @@ public class KeyboardAccessoryModernViewTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1432551")
     public void testDismissesSwipingEducationBubbleOnTap() {
         TestTracker tracker =
                 new TestTracker() {
