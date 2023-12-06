afec46c737c5547302fa35a3944624df44fad755
Yoav Weiss <yoavweiss@chromium.org>
2023-12-06 06:34:46 +0000

    Fix crash by adding v8::HandleScope in SameDocumentCommit
    This CL fixes a crash in a scenario where the a SameDocumentCommit
    was done outside of a JS task. I'll add tests shortly as a followup, but
    manually validated that this fixes the reported crash.
    
    Bug: 1508696
    Change-Id: Ic777b8defa88dd68978a6de7024e88f3157add90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5092767
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1233792}

--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
@@ -289,6 +289,7 @@ void SoftNavigationHeuristics::CheckSoftNavigationConditions(
   }
 
   if (paint_conditions_met_) {
+    v8::HandleScope handle_scope(script_state->GetIsolate());
     LocalFrame* frame = ToLocalFrameIfNotDetached(script_state->GetContext());
     if (frame && frame->IsOutermostMainFrame()) {
       EmitSoftNavigationEntry(frame);
