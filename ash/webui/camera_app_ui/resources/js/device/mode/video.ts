729f136643ead75d0e4f8cf511bfb8459061f693
seannli <seannli@google.com>
2023-12-07 14:05:07 +0000

    CCA: Add a callback for gif mode to stop the capturing loop.
    When minimizing CCA, the callback registered to
    requestVideoFrameCallback will never run. In this case, the capturing
    loop doesn't know the recording state is changed and will not
    leave the loop then resolve the promise. The CL lets video mode
    controller holds a callback which can stop the loop and it will be
    invoked when stop() is invoked.
    
    Bug: b:309901983
    Test: Manually click stop button, wait for timer while capturing gif.
    Test: Manually unplug external camera while while capturing gif.
    Change-Id: I9c9a6655608b6f543c4c1af488adcf678da11535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072612
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234458}

--- ash/webui/camera_app_ui/resources/js/device/mode/video.ts
+++ ash/webui/camera_app_ui/resources/js/device/mode/video.ts
@@ -268,6 +268,11 @@ export class Video extends ModeBase {
   private stopped = false;
 
   /**
+   * Callback to stop the loop of requesting video frames in the gif mode.
+   */
+  private stopCapturingGifCallback: (() => void)|null = null;
+
+  /**
    * HTMLElement displaying warning about low storage.
    */
   private readonly lowStorageWarningNudge = dom.get('#nudge', HTMLDivElement);
@@ -712,6 +717,9 @@ export class Video extends ModeBase {
       state.set(state.State.RECORDING, false);
       state.set(state.State.RECORDING_PAUSED, false);
       state.set(state.State.RECORDING_UI_PAUSED, false);
+      if (this.recordingType === RecordType.GIF) {
+        this.stopCapturingGifCallback?.();
+      }
     } else {
       sound.cancel('recordStart');
 
@@ -745,23 +753,21 @@ export class Video extends ModeBase {
     const context = assertInstanceof(
         canvas.getContext('2d', {willReadFrequently: true}),
         OffscreenCanvasRenderingContext2D);
-    if (videoTrack.readyState === 'ended') {
+    if (videoTrack.readyState === 'ended' ||
+        !state.get(state.State.RECORDING)) {
       throw new NoFrameError();
     }
     const frames = await new Promise<number>((resolve) => {
       let encodedFrames = 0;
       let writtenFrames = 0;
       let handle: number;
-      function stopRecording() {
+      const stopRecording = () => {
+        this.stopCapturingGifCallback = null;
         video.cancelVideoFrameCallback(handle);
         videoTrack.removeEventListener('ended', stopRecording);
         resolve(writtenFrames);
-      }
+      };
       function updateCanvas() {
-        if (!state.get(state.State.RECORDING)) {
-          stopRecording();
-          return;
-        }
         encodedFrames++;
         if (encodedFrames % GRAB_GIF_FRAME_RATIO === 0) {
           writtenFrames++;
@@ -771,6 +777,7 @@ export class Video extends ModeBase {
         handle = video.requestVideoFrameCallback(updateCanvas);
       }
       videoTrack.addEventListener('ended', stopRecording);
+      this.stopCapturingGifCallback = stopRecording;
       handle = video.requestVideoFrameCallback(updateCanvas);
     });
     if (frames === 0) {
