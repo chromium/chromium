e1b8b541e7e8b0a4ec78dad4108cf872e9ab81aa
Mikel Astiz <mastiz@chromium.org>
2023-12-07 18:35:49 +0000

    [m120] [Passwords] Add metrics for the outdated GMS dialog
    This dialog is displayed for users syncing passwords whenever they want
    to open the password management UI, but the GMS Core version on the
    device is lower than the minimum supported version for UPM.
    
    (cherry picked from commit 5e189ac374de1378afac44e37592fd0eee94d676)
    
    Bug: 1505730
    Change-Id: I8a7b43ad2ada22d7fdd1c71462e2a730aac70771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5067313
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1230593}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080881
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6099@{#1430}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/OutdatedGmsCoreDialog.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/OutdatedGmsCoreDialog.java
@@ -6,7 +6,10 @@ package org.chromium.chrome.browser.password_manager;
 
 import android.content.Context;
 
+import androidx.annotation.IntDef;
+
 import org.chromium.base.Callback;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
@@ -18,9 +21,27 @@ import org.chromium.ui.modelutil.PropertyModel;
  * agrees to proceed with the update.
  */
 class OutdatedGmsCoreDialog {
+    static final String DISMISSAL_REASON_HISTOGRAM =
+            "PasswordManager.OutdatedGMSDialogDismissalReason";
     private final ModalDialogManager mModalDialogManager;
     private final Context mContext;
     private final Callback<Boolean> mResultHandler;
+
+    // These values are persisted to logs. Entries should not be renumbered and
+    // numeric values should never be reused.
+    @IntDef({
+        GmsUpdateDialogDismissalReason.OTHER,
+        GmsUpdateDialogDismissalReason.ACCEPTED,
+        GmsUpdateDialogDismissalReason.REJECTED,
+        GmsUpdateDialogDismissalReason.COUNT
+    })
+    @interface GmsUpdateDialogDismissalReason {
+        int OTHER = 0;
+        int ACCEPTED = 1;
+        int REJECTED = 2;
+
+        int COUNT = 3;
+    }
 
     /**
      * Constructor for {@link OutdatedGmsCoreDialog}.
@@ -40,9 +61,7 @@ class OutdatedGmsCoreDialog {
     /** Shows the dialog. */
     void show() {
         SimpleModalDialogController modalDialogController =
-                new SimpleModalDialogController(mModalDialogManager, result -> {
-                    mResultHandler.onResult(result == DialogDismissalCause.POSITIVE_BUTTON_CLICKED);
-                });
+                new SimpleModalDialogController(mModalDialogManager, this::onDismissedWithReason);
 
         PropertyModel dialogModel =
                 new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
@@ -61,5 +80,27 @@ class OutdatedGmsCoreDialog {
                         .build();
 
         mModalDialogManager.showDialog(dialogModel, ModalDialogManager.ModalDialogType.APP);
+    }
+
+    private void onDismissedWithReason(int dismissalReason) {
+        // The dismissal reason should be recorded before the result handler is called, because
+        // the latter might open a new activity, which can cause the current one to be destroyed.
+        RecordHistogram.recordEnumeratedHistogram(
+                DISMISSAL_REASON_HISTOGRAM,
+                getDismissalReasonForMetrics(dismissalReason),
+                GmsUpdateDialogDismissalReason.COUNT);
+        mResultHandler.onResult(dismissalReason == DialogDismissalCause.POSITIVE_BUTTON_CLICKED);
+    }
+
+    private @GmsUpdateDialogDismissalReason int getDismissalReasonForMetrics(
+            int dialogDismissalReason) {
+        if (dialogDismissalReason == DialogDismissalCause.POSITIVE_BUTTON_CLICKED) {
+            return GmsUpdateDialogDismissalReason.ACCEPTED;
+        }
+        if (dialogDismissalReason == DialogDismissalCause.NEGATIVE_BUTTON_CLICKED
+                || dialogDismissalReason == DialogDismissalCause.NAVIGATE_BACK_OR_TOUCH_OUTSIDE) {
+            return GmsUpdateDialogDismissalReason.REJECTED;
+        }
+        return GmsUpdateDialogDismissalReason.OTHER;
     }
 }
