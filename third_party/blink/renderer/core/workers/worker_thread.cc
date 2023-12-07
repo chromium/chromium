525d82fa074241b57649b5bc3200151c416b08fc
punithnayak <npunith125@gmail.com>
2023-12-07 16:30:37 +0000

    Remove expired histograms WorkerThread.ExitCode
    Bug: 1508106
    Change-Id: I1f7141193a3f72744b44a9d889b27e3037c4f8ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096823
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Punith Nayak <npunith125@gmail.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234532}

--- third_party/blink/renderer/core/workers/worker_thread.cc
+++ third_party/blink/renderer/core/workers/worker_thread.cc
@@ -163,7 +163,6 @@ WorkerThread::~WorkerThread() {
 
   DCHECK(child_threads_.empty());
   DCHECK_NE(ExitCode::kNotTerminated, exit_code_);
-  base::UmaHistogramEnumeration("WorkerThread.ExitCode", exit_code_);
 }
 
 void WorkerThread::Start(
