2cf6db676cb678de6e9359262bc15def1d5727b2
Wes Okuhara <wesokuhara@google.com>
2023-12-07 18:05:28 +0000

    Set Time Dialog: Convert entry point JS to TS
    Bug: b:308829256
    Test: browser_tests --gtest_filter="SetTimeDialogBrowserTest*"
    Test: webui_closure_compile
    Change-Id: I9a977ba3675b7c4189e66f3e6fa65339e5c0f273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096577
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234584}

--- chrome/browser/resources/chromeos/set_time_dialog/set_time.ts
+++ chrome/browser/resources/chromeos/set_time_dialog/set_time.ts
@@ -10,7 +10,7 @@
 import './strings.m.js';
 import './set_time_dialog.js';
 
-import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 
 // TODO(b/257329722) After the Jelly experiment is launched, add the CSS link
