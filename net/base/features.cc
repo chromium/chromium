3f0d6447ad5cf9d4099e5eb3b5664d94b414516e
Joshua Hood <jdh@chromium.org>
2023-12-07 16:23:47 +0000

    Enable the TpcdSupportSettings by default
    This CL enables the TpcdSupportSettings feature by default and removes
    the associated fieldtrial testing config entry. This is in preparation
    for the launch of the ThirdPartyCookieDeprecationTrialService Finch
    study to 100% of Stable channel.
    
    Bug: b/305014952
    Change-Id: Idc55ef83676c075862592119a97a25246f40f4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094779
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234528}

--- net/base/features.cc
+++ net/base/features.cc
@@ -277,7 +277,7 @@ BASE_FEATURE(kSupportPartitionedBlobUrl,
 
 BASE_FEATURE(kTpcdSupportSettings,
              "TpcdSupportSettings",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTpcdMetadataGrants,
              "TpcdMetadataGrants",
