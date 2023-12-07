f27ada0c9ec8fa951f1950b6bbe4463ad1628f5c
Wes Okuhara <wesokuhara@google.com>
2023-12-07 18:40:26 +0000

    Set Time Dialog: Incorporate Jelly directly into HTML
    Jelly is launched and the conditional logic can be incorporated
    directly into the HTML.
    
    Light mode: http://screen/AAFcefMuzMHP2pK.png
    Dark mode: http://screen/BTg6MM47qmTpxsU.png
    
    Bug: b:281116317, b:257329722
    Test: Manually test UI in dark and light mode
    Change-Id: I8b5457bf9d9f7b7e48b1ef6974c2134fc20ff316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5096643
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1234607}

--- chrome/browser/resources/chromeos/set_time_dialog/set_time.ts
+++ chrome/browser/resources/chromeos/set_time_dialog/set_time.ts
@@ -10,22 +10,8 @@
 import './strings.m.js';
 import './set_time_dialog.js';
 
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 
-// TODO(b/257329722) After the Jelly experiment is launched, add the CSS link
-// element directly to the HTML.
-const jellyEnabled = loadTimeData.getBoolean('isJellyEnabled');
-if (jellyEnabled) {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'chrome://theme/colors.css?sets=legacy,sys';
-  document.head.appendChild(link);
-  document.body.classList.add('jelly-enabled');
-}
-
 window.onload = () => {
-  if (jellyEnabled) {
-    ColorChangeUpdater.forDocument().start();
-  }
+  ColorChangeUpdater.forDocument().start();
 };
