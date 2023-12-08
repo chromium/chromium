7d324a83ce03f1015fb11b0c468ffac653e26b27
nihardamar <nihardamar@google.com>
2023-12-08 16:47:01 +0000

    Fix http test
    Should be last one that needs to be fixed based on latest win-arm64-rel
    results.
    
    Bug: 1464614
    Change-Id: I0a7cf0edca1ed0e4f408b7df2a0e3439928e9cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5080519
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Nihar Damarasingu <nihardamar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235064}

--- third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/resources/sandbox.php
+++ third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/resources/sandbox.php
@@ -1,5 +1,5 @@
 <?php
-if ($_GET["report-only"]) {
+if (isset($_GET["report-only"])) {
     header("Content-Security-Policy-Report-Only: sandbox " . $_GET["sandbox"]);
 } else {
     header("Content-Security-Policy: sandbox " . $_GET["sandbox"]);
