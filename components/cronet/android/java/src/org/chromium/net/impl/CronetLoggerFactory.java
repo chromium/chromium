276ceaa45423bcacbe94fa8d72a9eae0fcdee52d
Etienne Dechamps <edechamps@google.com>
2023-12-08 16:22:11 +0000

    Clean up CronetLoggerFactory
    Bug: b:313418339
    Change-Id: Idfc4d12ff9fdc8e87daad0be5e8dbdf44f849dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5104788
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235054}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetLoggerFactory.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetLoggerFactory.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.util.Log; // TODO(crbug/1394709): use org.chromium.base.Log instead
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.net.impl.CronetLogger.CronetSource;
 import org.chromium.net.telemetry.CronetLoggerImpl;
@@ -20,46 +21,43 @@ public final class CronetLoggerFactory {
 
     private CronetLoggerFactory() {}
 
-    private static final CronetLogger sDefaultLogger = new NoOpLogger();
-    private static CronetLogger sTestingLogger;
+    private static CronetLogger sLogger;
 
     /**
-     * Bypasses CronetLoggerFactory logic and always creates a NoOpLogger.
-     * To be used only as a kill-switch for logging.
-     * @return a NoOpLogger instance.
+     * @return The correct CronetLogger to be used for logging. Never null - returns a no-op logger
+     *     on failure.
      */
-    public static CronetLogger createNoOpLogger() {
-        return sDefaultLogger;
-    }
-
-    /** @return The correct CronetLogger to be used for logging. */
     public static CronetLogger createLogger(Context ctx, CronetSource source) {
-        if (sTestingLogger != null) return sTestingLogger;
-
-        // The CronetLoggerImpl only works from apiLevel 30
-        if (!CronetManifest.isAppOptedInForTelemetry(ctx, source)
-                || Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-            return sDefaultLogger;
+        synchronized (CronetLoggerFactory.class) {
+            if (sLogger == null
+                    && CronetManifest.isAppOptedInForTelemetry(ctx, source)
+                    // The CronetLoggerImpl only works from apiLevel 30
+                    && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                try {
+                    sLogger = new CronetLoggerImpl(SAMPLE_RATE_PER_SECOND);
+                } catch (Exception e) {
+                    // Pass - since we dont want any failure, catch any exception that might
+                    // arise.
+                    Log.e(TAG, "Exception creating an instance of CronetLoggerImpl", e);
+                }
+            }
+            if (sLogger == null) sLogger = new NoOpLogger();
+            return sLogger;
         }
-
-        try {
-            return new CronetLoggerImpl(SAMPLE_RATE_PER_SECOND);
-        } catch (Exception e) {
-            // Pass - since we dont want any failure, catch any exception that might arise.
-            Log.e(TAG, "Exception creating an instance of CronetLoggerImpl", e);
-        }
-        return sDefaultLogger;
     }
 
     private static void setLoggerForTesting(@Nullable CronetLogger testingLogger) {
-        sTestingLogger = testingLogger;
+        synchronized (CronetLoggerFactory.class) {
+            sLogger = testingLogger;
+        }
     }
 
     /**
-     * Utility class to safely use a custom CronetLogger for the duration of a test.
-     * To be used within a try-with-resources statement within the test.
+     * Utility class to safely use a custom CronetLogger for the duration of a test. To be used
+     * within a try-with-resources statement within the test.
      */
-    public static class SwapLoggerForTesting implements AutoCloseable {
+    @VisibleForTesting
+    public static final class SwapLoggerForTesting implements AutoCloseable {
         /**
          * Forces {@code CronetLoggerFactory#createLogger} to return @param testLogger instead of
          * what it would have normally returned.
