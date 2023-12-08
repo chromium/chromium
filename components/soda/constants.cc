8c9bba6b900c3cf1ee23ee9205e73c6c3e0faea9
Evan Liu <evliu@google.com>
2023-12-08 20:53:29 +0000

    Switch to using the cmn-ha* language code for Chinese
    This CL switches the zh-CN and zh-TW language codes over to cmn-Hans-CN
    and cmn-Hant-TW. While most of the language codes used by the Speech
    On-Device API (SODA) are in the BCP47 format, there's a bug where the
    zh-* language codes aren't recognized by langid (b/314005589).
    
    Bug: 1161569
    Change-Id: I57e85fcc4acb29d5ca0acd09ec5d49ddc82d6013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5105735
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1235213}

--- components/soda/constants.cc
+++ components/soda/constants.cc
@@ -28,7 +28,7 @@ const constexpr char* const kDefaultEnabledLanguages[] = {"fr-FR", "it-IT",
 const char kUsEnglishLocale[] = "en-US";
 
 const char kEnglishLocaleNoCountry[] = "en";
-const char kChineseLocaleNoCountry[] = "zh";
+const char kChineseLocaleNoCountry[] = "cmn";
 
 const char kSodaBinaryInstallationResult[] =
     "SodaInstaller.BinaryInstallationResult";
@@ -195,9 +195,9 @@ LanguageCode GetLanguageCode(const std::string& language_name) {
 
 const std::u16string GetLanguageDisplayName(const std::string& language_name,
                                             const std::string& display_locale) {
-  if (language_name.substr(0, 2) == kChineseLocaleNoCountry) {
-    return l10n_util::GetDisplayNameForLocale(language_name, display_locale,
-                                              true);
+  if (language_name.substr(0, 3) == kChineseLocaleNoCountry) {
+    return l10n_util::GetDisplayNameForLocale(language_name.substr(0, 8),
+                                              display_locale, true);
 
   } else {
     return l10n_util::GetDisplayNameForLocaleWithoutCountry(
