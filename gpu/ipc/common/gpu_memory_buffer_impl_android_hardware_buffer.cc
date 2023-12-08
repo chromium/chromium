5546a66704fd5c48252b475a735ed00c1b5025ab
vikas soni <vikassoni@chromium.org>
2023-12-08 18:03:33 +0000

    Move Android Hardware Buffer related utility to new files.
    1. Add a new method to create ScopedHardwareBufferHandle and add it to a
       new utility file.
    2. Refactor GpuMemoryBufferImplAndroidHardwareBuffer to use method from
       utility files.
    
    Bug: 1509564
    Change-Id: I13c2337c5453af28abe24961f15f98f7c641cd8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5101289
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235100}

--- gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.cc
+++ gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.cc
@@ -6,64 +6,15 @@
 
 #include <utility>
 
-#include "base/android/android_hardware_buffer_compat.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "gpu/ipc/common/android/android_hardware_buffer_utils.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
-#include "ui/gfx/android/android_surface_control_compat.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace gpu {
-
-namespace {
-
-AHardwareBuffer_Desc GetBufferDescription(const gfx::Size& size,
-                                          gfx::BufferFormat format,
-                                          gfx::BufferUsage usage) {
-  // On create, all elements must be initialized, including setting the
-  // "reserved for future use" (rfu) fields to zero.
-  AHardwareBuffer_Desc desc = {};
-  desc.width = size.width();
-  desc.height = size.height();
-  desc.layers = 1;  // number of images
-
-  switch (format) {
-    case gfx::BufferFormat::RGBA_8888:
-      desc.format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM;
-      break;
-    case gfx::BufferFormat::RGBX_8888:
-      desc.format = AHARDWAREBUFFER_FORMAT_R8G8B8X8_UNORM;
-      break;
-    case gfx::BufferFormat::BGR_565:
-      desc.format = AHARDWAREBUFFER_FORMAT_R5G6B5_UNORM;
-      break;
-    case gfx::BufferFormat::RGBA_F16:
-      desc.format = AHARDWAREBUFFER_FORMAT_R16G16B16A16_FLOAT;
-      break;
-    case gfx::BufferFormat::RGBA_1010102:
-      desc.format = AHARDWAREBUFFER_FORMAT_R10G10B10A2_UNORM;
-      break;
-    default:
-      NOTREACHED();
-  }
-
-  switch (usage) {
-    case gfx::BufferUsage::GPU_READ:
-    case gfx::BufferUsage::SCANOUT:
-      desc.usage = AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE |
-                   AHARDWAREBUFFER_USAGE_GPU_COLOR_OUTPUT;
-      if (usage == gfx::BufferUsage::SCANOUT)
-        desc.usage |= gfx::SurfaceControl::RequiredUsage();
-      break;
-    default:
-      NOTREACHED();
-  }
-  return desc;
-}
-
-}  // namespace
 
 GpuMemoryBufferImplAndroidHardwareBuffer::
     GpuMemoryBufferImplAndroidHardwareBuffer(
@@ -85,19 +36,13 @@ GpuMemoryBufferImplAndroidHardwareBuffer::Create(gfx::GpuMemoryBufferId id,
                                                  gfx::BufferFormat format,
                                                  gfx::BufferUsage usage,
                                                  DestructionCallback callback) {
-  DCHECK(base::AndroidHardwareBufferCompat::IsSupportAvailable());
-
-  AHardwareBuffer* buffer = nullptr;
-  AHardwareBuffer_Desc desc = GetBufferDescription(size, format, usage);
-  base::AndroidHardwareBufferCompat::GetInstance().Allocate(&desc, &buffer);
-  if (!buffer) {
-    LOG(ERROR) << "Failed to allocate AHardwareBuffer";
+  auto scoped_buffer_handle =
+      CreateScopedHardwareBufferHandle(size, format, usage);
+  if (!scoped_buffer_handle.is_valid()) {
     return nullptr;
   }
-
   return base::WrapUnique(new GpuMemoryBufferImplAndroidHardwareBuffer(
-      id, size, format, std::move(callback),
-      base::android::ScopedHardwareBufferHandle::Adopt(buffer)));
+      id, size, format, std::move(callback), std::move(scoped_buffer_handle)));
 }
 
 // static
@@ -151,12 +96,10 @@ base::OnceClosure GpuMemoryBufferImplAndroidHardwareBuffer::AllocateForTesting(
   gfx::GpuMemoryBufferId kBufferId(1);
   handle->type = gfx::ANDROID_HARDWARE_BUFFER;
   handle->id = kBufferId;
-  AHardwareBuffer* buffer = nullptr;
-  AHardwareBuffer_Desc desc = GetBufferDescription(size, format, usage);
-  base::AndroidHardwareBufferCompat::GetInstance().Allocate(&desc, &buffer);
-  DCHECK(buffer);
-  handle->android_hardware_buffer =
-      base::android::ScopedHardwareBufferHandle::Adopt(buffer);
+  auto scoped_buffer_handle =
+      CreateScopedHardwareBufferHandle(size, format, usage);
+  CHECK(scoped_buffer_handle.is_valid());
+  handle->android_hardware_buffer = std::move(scoped_buffer_handle);
   return base::DoNothing();
 }
 
