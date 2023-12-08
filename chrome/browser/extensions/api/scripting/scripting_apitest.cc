9198fa3eef9de6ca3103d43e2763f9fd75ece194
Zaina Al-Mashni <zalmashni@google.com>
2023-12-08 13:57:37 +0000

    Disable flaky ScriptingAPIPrerenderingTest.Basic test
    Bug: 1351648
    Change-Id: I18a37f813ef7f3dd4880897f5f3a8fc9113115cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5103033
    Owners-Override: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1234995}

--- chrome/browser/extensions/api/scripting/scripting_apitest.cc
+++ chrome/browser/extensions/api/scripting/scripting_apitest.cc
@@ -727,7 +727,8 @@ class ScriptingAPIPrerenderingTest : public ScriptingAPITest {
   content::test::ScopedPrerenderFeatureList scoped_feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(ScriptingAPIPrerenderingTest, Basic) {
+// TODO(crbug.com/1351648): disabled due to flakiness.
+IN_PROC_BROWSER_TEST_F(ScriptingAPIPrerenderingTest, DISABLED_Basic) {
   ASSERT_TRUE(RunExtensionTest("scripting/prerendering")) << message_;
 }
 
