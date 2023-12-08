75dfa186b11605d18240ea18f6ec646eaef9ac54
Sky Malice <skym@chromium.org>
2023-12-08 21:44:07 +0000

    Avoid using bookmark title in format string.
    Bug: 1509959
    Change-Id: I862802182e6685312cc1dd9eb2916c375644fd83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106671
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235240}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/ImprovedBookmarkRowCoordinator.java
@@ -63,7 +63,8 @@ public class ImprovedBookmarkRowCoordinator {
             propertyModel.set(
                     ImprovedBookmarkRowProperties.TITLE,
                     String.format(
-                            bookmarkItem.getTitle() + " (%s)",
+                            "%s (%s)",
+                            bookmarkItem.getTitle(),
                             BookmarkUtils.getChildCountForDisplay(bookmarkId, mBookmarkModel)));
         } else {
             propertyModel.set(ImprovedBookmarkRowProperties.TITLE, bookmarkItem.getTitle());
