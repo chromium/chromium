f8eda9a68566ac2f5837f7234a62df52173d477b
Louis Romero <lpromero@google.com>
2023-12-11 09:06:00 +0000

    Revert "Update TopAlignedImageView to clip the bottom instead of pillarbox"
    This reverts commit d6ce042960703d271b2350bce3491bbb33c27020.
    
    Reason for revert: Breaks landscape snapshots in portrait grid with the existing layout. Will reland with the correct logic.
    
    Bug: 1508874
    
    Original change's description:
    > Update TopAlignedImageView to clip the bottom instead of pillarbox
    >
    > Prior to this CL, when a landscape image was displayed, its height
    > matched the view height, and its width was smaller than the view's
    > width, ending up with a pillarbox effect (vertical bands on each side of
    > the image).
    > With this CL, preparing the way for landscape grid cells, the image is
    > now always matching the image width, even if that means clipping the
    > bottom or adding an horizontal band at the bottom.
    >
    > For example, with the upcoming new layout, we will get this:
    > https://screenshot.googleplex.com/96o97eEY2AJsF8q
    > instead of this:
    > https://screenshot.googleplex.com/Aq5tG7RjioQSqty (notice the
    > vertical white bands on the sides of the screenshots.)
    >
    > Bug: 1485895
    > Change-Id: I3b71a8977bce6376dc44cac467db25633e05efa9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5033729
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Louis Romero <lpromero@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1225980}
    
    Bug: 1485895
    Change-Id: Iee467a902fd13825b2f49e929b29437f9fd90cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5103971
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235632}

--- ios/chrome/browser/shared/ui/elements/top_aligned_image_view.h
+++ ios/chrome/browser/shared/ui/elements/top_aligned_image_view.h
@@ -8,17 +8,17 @@
 #import <UIKit/UIKit.h>
 
 // The standard UIImageView zooms to the center of the image when it aspect
-// fills. TopAlignedImageView aligns to the top and width of the image in all
-// cases.
+// fills. TopAlignedImageView aligns to the top of the image in all cases.
+// When the image is portrait, the image matches the width of the view in all
+// cases. When the image is landscape, the image matches the height of the view
+// in all cases.
 @interface TopAlignedImageView : UIView
-
 // The image displayed in the image view.
 @property(nonatomic) UIImage* image;
 
 - (instancetype)init NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_SHARED_UI_ELEMENTS_TOP_ALIGNED_IMAGE_VIEW_H_
