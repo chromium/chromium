90ed311b6d261a179981d9e3a93a57e69680d818
Dana Fried <dfried@chromium.org>
2023-12-11 22:37:09 +0000

    [Test Automation] Minor fixes.
    Add missing verb to documentation.
    
    Fix macro to preserve line and file from call site.
    
    Change-Id: Ie657e532d3519cfbdd34e84062df7f9585e6bee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5106830
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235977}

--- ui/base/interaction/state_observer.h
+++ ui/base/interaction/state_observer.h
@@ -176,8 +176,8 @@ extern std::ostream& operator<<(std::ostream& os,
   DEFINE_ELEMENT_IDENTIFIER_VALUE(Name##Impl);            \
   constexpr ui::test::StateIdentifier<ObserverType> Name(Name##Impl)
 
-#define DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ObserverType, Name) \
-  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(Name##Impl);            \
+#define DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ObserverType, Name)          \
+  DEFINE_MACRO_ELEMENT_IDENTIFIER_VALUE(__FILE__, __LINE__, Name##Impl); \
   constexpr ui::test::StateIdentifier<ObserverType> Name(Name##Impl)
 
 #endif  // UI_BASE_INTERACTION_STATE_OBSERVER_H_
