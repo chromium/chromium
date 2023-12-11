4f23a98a38f3e26174dc4ba63102b80251476a33
David Benjamin <davidben@chromium.org>
2023-12-11 18:36:11 +0000

    Consistently default-initial members of PasswordGenerationUIData
    https://crrev.com/c/5058541 introduced pile UBSan failure:
    https://luci-milo.appspot.com/ui/inv/build-8762360020685235185/test-results?q=AutofillTypeTraitsTestImpl.PassPasswordGenerationUIData&sortby=&groupby=
    
    This is because PasswordGenerationUIData's default constructor leaves a
    bunch of fields uninitialized, and the test wasn't initializing it. Just
    default-initialize all of them so we don't need to worry about this.
    
    Bug: 1394755
    Change-Id: I2110b89d314730e4eb3bbcf5d2da09792f99646d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5100552
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235843}

--- components/autofill/core/common/password_generation_util.h
+++ components/autofill/core/common/password_generation_util.h
@@ -138,7 +138,7 @@ struct PasswordGenerationUIData {
   gfx::RectF bounds;
 
   // Maximum length of the generated password.
-  int max_length;
+  int max_length = 0;
 
   // Name of the password field to which the generation popup is attached.
   std::u16string generation_element;
@@ -147,16 +147,17 @@ struct PasswordGenerationUIData {
   FieldRendererId generation_element_id;
 
   // Is the generation element |type=password|.
-  bool is_generation_element_password_type;
+  bool is_generation_element_password_type = false;
 
   // Direction of the text for |generation_element|.
-  base::i18n::TextDirection text_direction;
+  base::i18n::TextDirection text_direction =
+      base::i18n::TextDirection::UNKNOWN_DIRECTION;
 
   // The form associated with the password field.
   FormData form_data;
 
   // Whether the password input field is empty.
-  bool input_field_empty;
+  bool input_field_empty = false;
 };
 
 void LogPasswordGenerationEvent(PasswordGenerationEvent event);
