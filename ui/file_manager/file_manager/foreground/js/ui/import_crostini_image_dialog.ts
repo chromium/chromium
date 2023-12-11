0d36f9df48a520f91ab5890d01e878cf305f9267
Bo Majewski <majewski@chromium.org>
2023-12-11 06:20:20 +0000

    [Files,TS]: Converts import_crostini_image_dialog to TypeScript.
    Bug: b:289003444
    Change-Id: I6eb9feb9bab00b18d78b8007c2a790075efe76cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5107453
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1235598}

--- ui/file_manager/file_manager/foreground/js/ui/import_crostini_image_dialog.ts
+++ ui/file_manager/file_manager/foreground/js/ui/import_crostini_image_dialog.ts
@@ -14,26 +14,25 @@ import {ConfirmDialog} from './dialogs.js';
  */
 export class ImportCrostiniImageDialog extends ConfirmDialog {
   /**
-   * @param {HTMLElement} parentNode Node to be parent for this dialog.
+   * @param parentNode Node to be parent for this dialog.
    */
-  constructor(parentNode) {
+  constructor(parentNode: HTMLElement) {
     super(parentNode);
     super.setOkLabel(str('IMPORT_CROSTINI_IMAGE_DIALOG_OK_LABEL'));
 
-    // @ts-ignore: error TS2531: Object is possibly 'null'.
-    this.container.classList.add('files-ng');
+    this.container!.classList.add('files-ng');
   }
 
   /**
    * Shows the dialog.
    *
-   * @param {!Entry} entry
    */
-  showImportCrostiniImageDialog(entry) {
+  showImportCrostiniImageDialog(entry: Entry) {
     super.showWithTitle(
         str('IMPORT_CROSTINI_IMAGE_DIALOG_TITLE'),
         str('IMPORT_CROSTINI_IMAGE_DIALOG_DESCRIPTION'),
         chrome.fileManagerPrivate.importCrostiniImage.bind(
-            null, /** @type {!Entry} */ (unwrapEntry(entry))));
+            null, unwrapEntry(entry) as Entry));
   }
 }
+
