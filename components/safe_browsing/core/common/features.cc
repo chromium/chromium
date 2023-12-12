661b64d1ae4d2b611d0d7f4bda8da164589e81b1
Javier Castro <jacastro@chromium.org>
2023-12-12 17:53:09 +0000

    Enable TailoredSecurityRetryForSyncUsers by default
    This feature is ready to rollout to 100% Stable.
    
    This change also removes the associated entry from
    fieldtrial_testing_config.json
    
    Change-Id: I90e6925818134128bee2e5706711bb0fc67e2d92
    Bug: 1346776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112831
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236415}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -303,7 +303,7 @@ BASE_FEATURE(kSuspiciousSiteTriggerQuotaFeature,
 
 BASE_FEATURE(kTailoredSecurityRetryForSyncUsers,
              "TailoredSecurityRetryForSyncUsers",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kTailoredSecurityObserverRetries,
