0e74ba1f1660a4ec9126b5144c494729f14385ac
Hong Xu <xuhong@chromium.org>
2023-12-12 18:52:37 +0000

    Use Destination_ARRAYSIZE instead of *_MAX for UMA stats
    `base::UmaHistogramEnumeration` requires that all possible metrics
    collected must be strictly smaller than the given enum size. Since
    casting an integer that is out of range of an enum to the enum is
    undefined behavior, Use `base::UmaHistogramExactLinear` instead to work
    around, along with `Destination_ARRAYSIZE`.
    
    (cherry picked from commit 7f90b77e5da55736aa195cc4998428eb2b68c1a5)
    
    Bug: b:310095358, b:315882219
    Fixed: b:315882219
    Change-Id: I206cd30d5934431e5774d5859b8ee99d4c5f963f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113455
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1236096}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112997
    Cr-Commit-Position: refs/branch-heads/6167@{#290}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- components/reporting/metrics/periodic_collector.cc
+++ components/reporting/metrics/periodic_collector.cc
@@ -87,9 +87,10 @@ void PeriodicCollector::OnMetricDataCollected(
   CheckOnSequence();
   CHECK(metric_report_queue_);
   if (!metric_data.has_value()) {
-    base::UmaHistogramEnumeration(PeriodicCollector::kNoMetricDataMetricsName,
-                                  metric_report_queue_->GetDestination(),
-                                  Destination_MAX);
+    base::UmaHistogramExactLinear(
+        PeriodicCollector::kNoMetricDataMetricsName,
+        static_cast<int>(metric_report_queue_->GetDestination()),
+        Destination_ARRAYSIZE);
     return;
   }
 
