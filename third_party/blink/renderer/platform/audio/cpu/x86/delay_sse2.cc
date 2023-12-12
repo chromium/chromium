1a8e198de5afc3914f85d4de8195a92008114ff5
Hongchan Choi <hongchan@chromium.org>
2023-12-12 02:36:08 +0000

    Wrap buffer read index in delay kernel
    The current code assumes that the first buffer read index in the delay
    kernel does not go out of bound. This CL applies the wrapping function
    to the read index array.
    
    (cherry picked from commit fb96fd5f41bec823dbb208d9a7d53fbbf4d16ce4)
    
    Bug: 1505086
    Test: Locally confirmed the repro does not crash anymore
    Change-Id: Idca3dfc7dec5b5a7f9b22d87135e2d775729631a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5072113
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1231040}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112992
    Auto-Submit: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6099@{#1498}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- third_party/blink/renderer/platform/audio/cpu/x86/delay_sse2.cc
+++ third_party/blink/renderer/platform/audio/cpu/x86/delay_sse2.cc
@@ -56,10 +56,10 @@ std::tuple<unsigned, int> Delay::ProcessARateVector(
 
   const float sample_rate = sample_rate_;
   const float* delay_times = delay_times_.Data();
-
   int w_index = write_index_;
 
   const __m128 v_sample_rate = _mm_set1_ps(sample_rate);
+  const __m128 v_all_zeros = _mm_setzero_ps();
 
   // The buffer length as a float and as an int so we don't need to constant
   // convert from one to the other.
@@ -82,7 +82,10 @@ std::tuple<unsigned, int> Delay::ProcessARateVector(
   int k = 0;
 
   for (int n = 0; n < number_of_loops; ++n, k += 4) {
-    const __m128 v_delay_time = _mm_loadu_ps(delay_times + k);
+    // It's possible that `delay_time` contains negative values. Make sure
+    // they are greater than zero.
+    const __m128 v_delay_time = _mm_max_ps(_mm_loadu_ps(delay_times + k),
+                                           v_all_zeros);
     const __m128 v_desired_delay_frames =
         _mm_mul_ps(v_delay_time, v_sample_rate);
 
@@ -95,7 +98,8 @@ std::tuple<unsigned, int> Delay::ProcessARateVector(
         WrapPositionVector(v_read_position, v_buffer_length_float);
 
     // Get indices into the buffer for the samples we need for interpolation.
-    const __m128i v_read_index1 = _mm_cvttps_epi32(v_read_position);
+    const __m128i v_read_index1 = WrapIndexVector(
+        _mm_cvttps_epi32(v_read_position), v_buffer_length_int);
     const __m128i v_read_index2 = WrapIndexVector(
         _mm_add_epi32(v_read_index1, _mm_set1_epi32(1)), v_buffer_length_int);
 
