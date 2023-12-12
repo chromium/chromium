22f1e521e3b6ee11d5df39e483b1ee5a2a368a5b
Boris Sazonov <bsazonov@chromium.org>
2023-12-12 19:30:09 +0000

    [M121]🧇 Add search engine logos in settings page
    This cl adds logic on search engine settings page to fetch favicons for
    the search engines from google servers.
    
    (cherry picked from commit 133e8a5d49081b5ae0328fab392fd62ae184d558)
    
    Bug: b:307240685, 1510478
    Change-Id: Ief179252a634c0b61022416df633ddcf34c4751f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5094334
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1235680}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110783
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6167@{#292}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- chrome/android/javatests/src/org/chromium/chrome/browser/search_engines/settings/SearchEngineSettingsRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/search_engines/settings/SearchEngineSettingsRenderTest.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.doReturn;
 
 import static org.chromium.components.search_engines.TemplateUrlTestHelpers.buildMockTemplateUrl;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.view.View;
 
 import androidx.fragment.app.FragmentManager;
@@ -23,20 +25,31 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.components.favicon.GoogleFaviconServerRequestStatus;
+import org.chromium.components.favicon.IconType;
+import org.chromium.components.favicon.LargeIconBridge;
+import org.chromium.components.favicon.LargeIconBridgeJni;
 import org.chromium.components.search_engines.TemplateUrl;
 import org.chromium.components.search_engines.TemplateUrlService;
+import org.chromium.content_public.browser.BrowserContextHandle;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.net.NetworkTrafficAnnotationTag;
 import org.chromium.ui.test.util.BlankUiTestActivity;
+import org.chromium.url.GURL;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** Tests for Search Engine Settings. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -55,15 +68,18 @@ public class SearchEngineSettingsRenderTest {
 
     public final @Rule MockitoRule mMocks = MockitoJUnit.rule();
 
+    public final @Rule JniMocker mJniMocker = new JniMocker();
+
     private @Mock TemplateUrlService mMockTemplateUrlService;
     private @Mock Profile mProfile;
+    private @Mock LargeIconBridge.Natives mLargeIconBridgeNativeMock;
 
     @Test
     @MediumTest
     @Feature({"RenderTest"})
     @Features.EnableFeatures(ChromeFeatureList.SEARCH_ENGINE_CHOICE)
     public void testRenderWithSecFeature() throws Exception {
-        testRender("search_engine_settings");
+        testRender("search_engine_settings", true);
     }
 
     @Test
@@ -71,24 +87,34 @@ public class SearchEngineSettingsRenderTest {
     @Feature({"RenderTest"})
     @Features.DisableFeatures(ChromeFeatureList.SEARCH_ENGINE_CHOICE)
     public void testRenderWithoutSecFeature() throws Exception {
-        testRender("search_engine_settings_flag_off");
+        testRender("search_engine_settings_flag_off", false);
     }
 
-    private void testRender(String screenshotId) throws Exception {
-        TemplateUrl defaultSearchEngine = buildTemplateUrl("Custom Engine", 0);
-        List<TemplateUrl> templateUrls =
-                List.of(defaultSearchEngine, buildTemplateUrl("Prepopulated Engine", 2));
+    private void testRender(String screenshotId, boolean needsIcons) throws Exception {
+        TemplateUrl engine1 = buildTemplateUrl("Custom Engine", 0);
+        GURL engine1Gurl = new GURL("https://gurl1.example.com");
+        TemplateUrl engine2 = buildTemplateUrl("Prepopulated Engine", 2);
+        GURL engine2Gurl = new GURL("https://gurl2.example.com");
+        List<TemplateUrl> templateUrls = List.of(engine1, engine2);
 
         doReturn(new ArrayList<>(templateUrls)).when(mMockTemplateUrlService).getTemplateUrls();
-        doReturn(defaultSearchEngine)
-                .when(mMockTemplateUrlService)
-                .getDefaultSearchEngineTemplateUrl();
+        doReturn(engine1).when(mMockTemplateUrlService).getDefaultSearchEngineTemplateUrl();
         doReturn(true).when(mMockTemplateUrlService).isEeaChoiceCountry();
         doReturn(true).when(mMockTemplateUrlService).isLoaded();
+        String engine1Keyword = engine1.getKeyword();
+        doReturn(engine1Gurl.getSpec())
+                .when(mMockTemplateUrlService)
+                .getSearchEngineUrlFromTemplateUrl(engine1Keyword);
+        String engine2Keyword = engine2.getKeyword();
+        doReturn(engine2Gurl.getSpec())
+                .when(mMockTemplateUrlService)
+                .getSearchEngineUrlFromTemplateUrl(engine2Keyword);
 
         TemplateUrlServiceFactory.setInstanceForTesting(mMockTemplateUrlService);
+        mJniMocker.mock(LargeIconBridgeJni.TEST_HOOKS, mLargeIconBridgeNativeMock);
 
         mActivityTestRule.launchActivity(null);
+        TestLargeIconBridge largeIconBridge = new TestLargeIconBridge(mProfile);
 
         View view =
                 TestThreadUtils.runOnUiThreadBlocking(
@@ -104,12 +130,40 @@ public class SearchEngineSettingsRenderTest {
                                                                     .getClassLoader(),
                                                             SearchEngineSettings.class.getName());
                             fragment.setProfile(mProfile);
+
+                            SearchEngineAdapter adapter =
+                                    new SearchEngineAdapter(
+                                            mActivityTestRule.getActivity(), mProfile) {
+                                        @Override
+                                        LargeIconBridge createLargeIconBridge() {
+                                            return largeIconBridge;
+                                        }
+                                    };
+                            fragment.overrideSearchEngineAdapterForTesting(adapter);
+
                             fragmentManager
                                     .beginTransaction()
                                     .replace(android.R.id.content, fragment)
                                     .commitNow();
+
                             return fragment.getView();
                         });
+
+        if (needsIcons) {
+            // Wait for icons to be requested.
+            CriteriaHelper.pollUiThread(() -> largeIconBridge.getCallbackCount() == 2);
+
+            TestThreadUtils.runOnUiThreadBlocking(
+                    () -> {
+                        Bitmap bitmap1 = Bitmap.createBitmap(32, 32, Bitmap.Config.ARGB_8888);
+                        bitmap1.eraseColor(Color.GREEN);
+                        largeIconBridge.provideFaviconForUrl(engine1Gurl, bitmap1);
+
+                        Bitmap bitmap2 = Bitmap.createBitmap(32, 32, Bitmap.Config.ARGB_8888);
+                        bitmap2.eraseColor(Color.BLUE);
+                        largeIconBridge.provideFaviconForUrl(engine2Gurl, bitmap2);
+                    });
+        }
         mRenderTestRule.render(view, screenshotId);
     }
 
@@ -118,5 +172,46 @@ public class SearchEngineSettingsRenderTest {
                 buildMockTemplateUrl("prepopulatedId=" + prepopulatedId, prepopulatedId);
         doReturn(shortName).when(templateUrl).getShortName();
         return templateUrl;
+    }
+
+    private class TestLargeIconBridge extends LargeIconBridge {
+        private final Map<GURL, LargeIconCallback> mCallbacks = new HashMap<>();
+
+        TestLargeIconBridge(BrowserContextHandle browserContextHandle) {
+            super(browserContextHandle);
+        }
+
+        @Override
+        public boolean getLargeIconForUrl(
+                final GURL pageUrl,
+                int minSizePx,
+                int desiredSizePx,
+                final LargeIconCallback callback) {
+            mCallbacks.put(pageUrl, callback);
+            return true;
+        }
+
+        public void provideFaviconForUrl(GURL pageUrl, Bitmap bitmap) {
+            LargeIconCallback callback = mCallbacks.get(pageUrl);
+            callback.onLargeIconAvailable(bitmap, Color.BLACK, false, IconType.INVALID);
+            mCallbacks.remove(pageUrl);
+        }
+
+        public int getCallbackCount() {
+            return mCallbacks.size();
+        }
+
+        @Override
+        public void getLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache(
+                GURL pageUrl,
+                boolean mayPageUrlBePrivate,
+                boolean shouldTrimPageUrlPath,
+                NetworkTrafficAnnotationTag trafficAnnotation,
+                GoogleFaviconServerCallback callback) {
+            callback.onRequestComplete(GoogleFaviconServerRequestStatus.SUCCESS);
+        }
+
+        @Override
+        public void touchIconFromGoogleServer(GURL iconUrl) {}
     }
 }
