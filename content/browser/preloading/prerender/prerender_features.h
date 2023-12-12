5107590a6c50e18d16b331e173a7d0c667a3bdac
Hiroki Nakagawa <nhiroki@chromium.org>
2023-12-12 03:02:56 +0000

    [Merge to M121] Prerender: Add a separate feature flag for embedder_blocked_hosts
    This CL introduces a new feature flag specific to
    embedder_blocked_hosts. This enables us to more flexibly coordinate
    fieldtrial configurations.
    
    (cherry picked from commit 58104f74170de20d6179e1ad7c823f56e9b684ee)
    
    Bug: 1509271
    Change-Id: I1e42303910e8b15f9abae90ee443604a30ebc4df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5095540
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1234466}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5113584
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6167@{#253}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- content/browser/preloading/prerender/prerender_features.h
+++ content/browser/preloading/prerender/prerender_features.h
@@ -6,6 +6,7 @@
 #define CONTENT_BROWSER_PRELOADING_PRERENDER_PRERENDER_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "content/common/content_export.h"
 
 namespace content::features {
@@ -13,6 +14,10 @@ namespace content::features {
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2BypassMemoryLimitCheck);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2NewLimitAndScheduler);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2AllowActivationInBackground);
+
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kPrerender2EmbedderBlockedHosts);
+CONTENT_EXPORT extern const base::FeatureParam<std::string>
+    kPrerender2EmbedderBlockedHostsParam;
 
 }  // namespace content::features
 
