6f71491dccb1e94630f304c38cf2faf36a673d2f
Yue Zhang <yuezhanggg@chromium.org>
2023-12-13 18:36:36 +0000

    [PriceDrop] Add view for price change module
    Add the basic layout for the price change module. More polishes will be
    done in later patches.
    
    Demo: https://screenshot.googleplex.com/C8TPgYyT9t3icZm
    
    Low-Coverage-Reason: Tests are added in the following change https://crrev.com/c/5113265.
    Bug: 1501138
    Change-Id: Ic7d86757727214a70bd70293dca868f29bb95079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5101890
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237070}

---
+++ chrome/browser/commerce/price_change/android/java/src/org/chromium/chrome/browser/price_change/PriceChangeModuleView.java
@@ -0,0 +1,74 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.price_change;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+/** View for the price change module. */
+public class PriceChangeModuleView extends FrameLayout {
+    private TextView mProductTitleView;
+    private TextView mPriceChangeDomainView;
+    private ImageView mProductImageView;
+    private ImageView mFaviconImageView;
+    private TextView mPreviousPriceView;
+    private TextView mCurrentPriceView;
+
+    public PriceChangeModuleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mProductTitleView = findViewById(R.id.product_title);
+        mProductImageView = findViewById(R.id.product_image);
+        mPriceChangeDomainView = findViewById(R.id.price_drop_domain);
+        mFaviconImageView = findViewById(R.id.favicon_image);
+        mPreviousPriceView = findViewById(R.id.previous_price);
+        mCurrentPriceView = findViewById(R.id.current_price);
+    }
+
+    void setProductTitle(String header) {
+        mProductTitleView.setText(header);
+    }
+
+    void setPriceChangeDomain(String domain) {
+        mPriceChangeDomainView.setText(domain);
+    }
+
+    void setProductImage(Bitmap bitmap) {
+        mProductImageView.setImageBitmap(bitmap);
+    }
+
+    void setFaviconImage(Bitmap bitmap) {
+        mFaviconImageView.setImageBitmap(bitmap);
+    }
+
+    void setCurrentPrice(String price) {
+        // TODO(crbug.com/1501138): Add the color to pre-defined text appearance styles when it's
+        // finalized.
+        mPreviousPriceView.setTextColor(
+                getContext().getColor(R.color.price_drop_annotation_text_green));
+        mCurrentPriceView.setText(price);
+    }
+
+    void setPreviousPrice(String price) {
+        // TODO(crbug.com/1501138): Add the color to pre-defined text appearance styles when it's
+        // finalized.
+        mPreviousPriceView.setTextColor(
+                getContext().getColor(R.color.chip_text_color_secondary_list));
+        mPreviousPriceView.setPaintFlags(
+                mPreviousPriceView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        mPreviousPriceView.setText(price);
+    }
+}
