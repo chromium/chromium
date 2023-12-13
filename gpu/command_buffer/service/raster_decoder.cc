ae6d92651bf5cb304fc1d534cff83fd68959c5bb
Chia-I Wu <olv@google.com>
2023-12-13 01:27:48 +0000

    Populate DRM capabilities for RasterDecoder
    exo depends on DRM capabilities to advertise zwp_linux_dmabuf v4.
    
    Bug: b/314143782
    Test: exo reports zwp_linux_dmabuf v4 again
    Change-Id: Iadfca9ccbb33fca801ab5436c7d30385e0c5c32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5075028
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Chia-I Wu <olv@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1236719}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -94,6 +94,7 @@
 #if BUILDFLAG(ENABLE_VULKAN)
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #include "gpu/vulkan/vulkan_device_queue.h"
+#include "gpu/vulkan/vulkan_util.h"
 #endif  // BUILDFLAG(ENABLE_VULKAN)
 
 #if BUILDFLAG(IS_WIN)
@@ -1304,6 +1305,24 @@ Capabilities RasterDecoderImpl::GetCapabilities() {
     caps.supports_yuv_rgb_conversion = true;
     caps.supports_yuv_readback = true;
   }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (shared_context_state_->GrContextIsGL()) {
+    PopulateDRMCapabilities(&caps, feature_info());
+  }
+#if BUILDFLAG(ENABLE_VULKAN)
+  else if (shared_context_state_->GrContextIsVulkan()) {
+    auto* device_queue =
+        shared_context_state_->vk_context_provider()->GetDeviceQueue();
+    caps.drm_device_id = device_queue->drm_device_id();
+    gpu::PopulateVkDrmFormatsAndModifiers(device_queue,
+                                          caps.drm_formats_and_modifiers);
+  }
+#endif  // BUILDFLAG(ENABLE_VULKAN)
+  else {
+    NOTREACHED();
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   return caps;
 }
