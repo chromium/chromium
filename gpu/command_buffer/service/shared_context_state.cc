00dc98c7d473e380cdc897bd85b6d954c9a42935
Le Hoang Quyen <lehoangquyen@chromium.org>
2023-12-13 20:29:53 +0000

    Only crash GPU process if the faulty shader was cached.
    When Skia's shader's compilerError happens:
    - If the faulty shader was cached, crash the GPU process to signal the
    browser process to clear the cache.
    - Otherwise just send a error report via DumpWithoutCrashing(). This
    won't crash the browser.
    
    Bug: 1442633
    Change-Id: I946128e5a0998a070c927919258ada644920e650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5118038
    Reviewed-by: Vasiliy Telezhnikov <vtelezhnikov@google.com>
    Auto-Submit: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237138}

--- gpu/command_buffer/service/shared_context_state.cc
+++ gpu/command_buffer/service/shared_context_state.cc
@@ -4,6 +4,7 @@
 
 #include "gpu/command_buffer/service/shared_context_state.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/immediate_crash.h"
 #include "base/observer_list.h"
 #include "base/strings/stringprintf.h"
@@ -105,23 +106,31 @@ int32_t GetDawnMaxTextureSize(gpu::DawnContextProvider* context_provider) {
 
 namespace gpu {
 
-void SharedContextState::compileError(const char* shader, const char* errors) {
+void SharedContextState::compileError(const char* shader,
+                                      const char* errors,
+                                      bool shaderWasCached) {
   if (!context_lost()) {
-    LOG(ERROR) << "Skia shader compilation error\n"
+    LOG(ERROR) << "Skia shader compilation error (was cached = "
+               << shaderWasCached << ")" << "\n"
                << "------------------------\n"
                << shader << "\nErrors:\n"
                << errors;
 
     static crash_reporter::CrashKeyString<2048> error_key("skia-compile-error");
     error_key.Set(errors);
-    // https://crbug.com/1442633 Sometimes we would fail to compile a cached
-    // GLSL shader because of GL driver change. Increase shader cache shm
-    // count and crash the GPU process so that the browser process would clear
-    // the cache.
-    GpuProcessShmCount::ScopedIncrement increment(
-        use_shader_cache_shm_count_.get());
 
-    base::ImmediateCrash();
+    if (shaderWasCached) {
+      // https://crbug.com/1442633 Sometimes we would fail to compile a cached
+      // GLSL shader because of GL driver change. Increase shader cache shm
+      // count and crash the GPU process so that the browser process would clear
+      // the cache.
+      GpuProcessShmCount::ScopedIncrement increment(
+          use_shader_cache_shm_count_.get());
+
+      base::ImmediateCrash();
+    } else {
+      base::debug::DumpWithoutCrashing();
+    }
   }
 }
 
