e674b5467eaf6434d359d235fbb45d633f4ca47b
Peter Kotwicz <pkotwicz@chromium.org>
2023-12-13 16:22:14 +0000

    Add test for form_tracker.cc
    BUG=None
    
    Change-Id: Ibae667fb5f753be21469a3f66047e0210e840dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5117278
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1236975}

---
+++ components/autofill/content/renderer/form_tracker_browsertest.cc
@@ -0,0 +1,103 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/content/renderer/form_tracker.h"
+
+#include "content/public/test/render_view_test.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/web/web_form_control_element.h"
+#include "third_party/blink/public/web/web_local_frame.h"
+
+namespace autofill {
+namespace {
+
+class MockFormTrackerObserver : public FormTracker::Observer {
+ public:
+  // Not a mock method so that gmock ignores calls to this method.
+  void OnProvisionallySaveForm(const blink::WebFormElement&,
+                               const blink::WebFormControlElement&,
+                               ElementChangeSource) override {}
+
+  MOCK_METHOD0(OnProbablyFormSubmitted, void());
+  MOCK_METHOD1(OnFormSubmitted, void(const blink::WebFormElement&));
+  MOCK_METHOD1(OnInferredFormSubmission, void(mojom::SubmissionSource));
+};
+
+class FormTrackerTest : public content::RenderViewTest {
+ public:
+  blink::WebFormControlElement GetFormControlById(const std::string& id) {
+    return GetMainFrame()
+        ->GetDocument()
+        .GetElementById(blink::WebString::FromUTF8(id))
+        .DynamicTo<blink::WebFormControlElement>();
+  }
+};
+
+// Check that submission is detected on a page with no <form> when in sequence:
+// 1) User types into a field.
+// 2) Page does an XHR.
+// 3) Page hides all of the inputs.
+TEST_F(FormTrackerTest, FormlessXHRThenHide) {
+  LoadHTML("<!DOCTYPE HTML><input id='input1'><input id='input2'/>");
+
+  blink::WebFormControlElement input1 = GetFormControlById("input1");
+
+  FormTracker tracker(GetMainRenderFrame());
+  testing::StrictMock<MockFormTrackerObserver> observer;
+  tracker.AddObserver(&observer);
+
+  GetMainFrame()->NotifyUserActivation(
+      blink::mojom::UserActivationNotificationType::kTest);
+  ExecuteJavaScriptForTests("document.getElementById('input1').focus();");
+  tracker.TextFieldDidChange(input1);
+
+  base::RunLoop().RunUntilIdle();
+
+  tracker.AjaxSucceeded();
+  base::RunLoop().RunUntilIdle();
+  // FormTracker should not think there is a submission because the <input>s are
+  // still visible.
+
+  // FormTracker should detect a submission after the <input>s are hidden.
+  EXPECT_CALL(observer, OnInferredFormSubmission).Times(1);
+  ExecuteJavaScriptForTests(
+      "document.getElementById('input1').style.display = 'none';"
+      "document.getElementById('input2').style.display = 'none';");
+}
+
+// Check that submission is detected on a page with no <form> when in sequence:
+// 1) User types into a field.
+// 2) Page hides all of the inputs.
+// 3) Page does an XHR.
+TEST_F(FormTrackerTest, FormlessHideThenXhr) {
+  LoadHTML("<!DOCTYPE HTML><input id='input1'><input id='input2'/>");
+
+  blink::WebFormControlElement input1 = GetFormControlById("input1");
+
+  FormTracker tracker(GetMainRenderFrame());
+  testing::StrictMock<MockFormTrackerObserver> observer;
+  tracker.AddObserver(&observer);
+
+  GetMainFrame()->NotifyUserActivation(
+      blink::mojom::UserActivationNotificationType::kTest);
+  ExecuteJavaScriptForTests("document.getElementById('input1').focus();");
+  tracker.TextFieldDidChange(input1);
+  base::RunLoop().RunUntilIdle();
+
+  ExecuteJavaScriptForTests(
+      "document.getElementById('input1').style.display = 'none';"
+      "document.getElementById('input2').style.display = 'none';");
+  base::RunLoop().RunUntilIdle();
+  // FormTracker should not think there is a submission because the page has not
+  // done any XHRs.
+
+  // FormTracker should detect a submission when the XHR succeeds.
+  EXPECT_CALL(observer, OnInferredFormSubmission).Times(1);
+  tracker.AjaxSucceeded();
+  base::RunLoop().RunUntilIdle();
+}
+
+}  // anonymous namespace
+}  // namespace autofill
