de4f66a34efb902bf77b381b230bc64583c1a758
Hubert Chao <hchao@chromium.org>
2023-12-14 19:59:43 +0000

    EmbeddedTestServer: add option for generated intermediate to not be served via TLS handshake or via AIA
    Change-Id: I0b827162f7840243d1b83a2560794ecac2aa36f7
    Bug: 1477317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5110443
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1237691}

--- net/test/embedded_test_server/embedded_test_server.cc
+++ net/test/embedded_test_server/embedded_test_server.cc
@@ -515,12 +515,17 @@ bool EmbeddedTestServer::GenerateCertAndKey() {
     leaf->SetCaIssuersAndOCSPUrls(leaf_ca_issuers_urls, leaf_ocsp_urls);
   }
 
-  if (cert_config_.intermediate == IntermediateType::kByAIA) {
+  if (cert_config_.intermediate == IntermediateType::kByAIA ||
+      cert_config_.intermediate == IntermediateType::kMissing) {
     // Server certificate chain does not include the intermediate.
     x509_cert_ = leaf->GetX509Certificate();
   } else {
     // Server certificate chain will include the intermediate, if there is one.
     x509_cert_ = leaf->GetX509CertificateChain();
+  }
+
+  if (intermediate) {
+    intermediate_ = intermediate->GetX509Certificate();
   }
 
   private_key_ = bssl::UpRef(leaf->GetKey());
@@ -834,6 +839,12 @@ scoped_refptr<X509Certificate> EmbeddedTestServer::GetCertificate() {
     CHECK(InitializeCertAndKeyFromFile());
   }
   return x509_cert_;
+}
+
+scoped_refptr<X509Certificate> EmbeddedTestServer::GetGeneratedIntermediate() {
+  DCHECK(is_using_ssl_);
+  DCHECK(!UsingStaticCert());
+  return intermediate_;
 }
 
 void EmbeddedTestServer::ServeFilesFromDirectory(
