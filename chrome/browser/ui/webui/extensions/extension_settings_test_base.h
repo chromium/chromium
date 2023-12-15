b327147ccd4a246d341076ea6170153a637971fa
David Bienvenu <davidbienvenu@chromium.org>
2023-12-15 20:47:49 +0000

    Stop ExtensionSettings tests from adding shortcut to start menu
    ScopedPathOverride override_start_dir{base::DIR_START_MENU} to stop
    tests inheriting from ExtensionSettingsTestBase from creating
    shortcuts in the Windows start menu.
    
    Bug: 1405159
    Change-Id: I04811d03605ad3f6630a5379b18575b1caf42cf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125294
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238233}

--- chrome/browser/ui/webui/extensions/extension_settings_test_base.h
+++ chrome/browser/ui/webui/extensions/extension_settings_test_base.h
@@ -12,6 +12,11 @@
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "extensions/browser/scoped_ignore_content_verifier_for_test.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "base/base_paths_win.h"
+#include "base/test/scoped_path_override.h"
+#endif  // BUILDFLAG(IS_WIN)
+
 namespace extensions {
 class Extension;
 class ScopedTestDialogAutoConfirm;
@@ -60,6 +65,14 @@ class ExtensionSettingsTestBase : public WebUIMochaBrowserTest {
   const extensions::Extension* InstallExtension(const base::FilePath& path);
 
   const base::FilePath test_data_dir_;
+
+#if BUILDFLAG(IS_WIN)
+  // This is needed to stop tests creating a shortcut in the Windows start menu.
+  // The override needs to last until the test is destroyed, because Windows
+  // shortcut tasks which create the shortcut can run after the test body
+  // returns.
+  base::ScopedPathOverride override_start_dir{base::DIR_START_MENU};
+#endif  // BUILDFLAG(IS_WIN)
 
   // Disable extension content verification.
   extensions::ScopedIgnoreContentVerifierForTest ignore_content_verification_;
