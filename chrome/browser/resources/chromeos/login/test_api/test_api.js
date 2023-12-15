cfa2ee60fd96d064685dbc0ef56d082e784d6df5
Anastasiia N <anastasiian@chromium.org>
2023-12-15 13:54:57 +0000

    cros: Add PasswordFactorSuccessScreen to OobeTestApi
    The screen was split from the LocalPasswordSetupScreen.
    
    Bug: b:304962593
    Change-Id: Ic601570d8e016a01fb2a46aa3e4dadb40ebed253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116250
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238036}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -975,8 +975,6 @@ class LocalPasswordSetupScreenTester extends ScreenElementApi {
     this.firstInput = new TextFieldApi(this.passwordInput, '#firstInput');
     this.confirmInput = new TextFieldApi(this.passwordInput, '#confirmInput');
     this.nextButton = new PolymerElementApi(this, '#nextButton');
-    this.doneDialog = new PolymerElementApi(this, '#doneDialog');
-    this.doneButton = new PolymerElementApi(this, '#doneButton');
   }
 
   /** @return {boolean} */
@@ -994,14 +992,29 @@ class LocalPasswordSetupScreenTester extends ScreenElementApi {
       });
     });
   }
+}
+
+class PasswordFactorSuccessScreenTester extends ScreenElementApi {
+  constructor() {
+    super('factor-setup-success');
+    this.doneButton = new PolymerElementApi(this, '#doneButton');
+    this.nextButton = new PolymerElementApi(this, '#nextButton');
+  }
 
   /** @return {boolean} */
   isDone() {
-    return this.doneDialog.isVisible();
+    return this.isVisible() &&
+        (this.doneButton.isVisible() || this.nextButton.isVisible());
   }
 
   clickDone() {
-    this.doneButton.click();
+    if (this.doneButton.isVisible()) {
+      this.doneButton.click();
+      return;
+    }
+    if (this.nextButton.isVisible()) {
+      this.nextButton.click();
+    }
   }
 }
 
@@ -1198,6 +1211,7 @@ export class OobeApiProvider {
       SmartPrivacyProtectionScreen: new SmartPrivacyProtectionScreenTester(),
       CryptohomeRecoverySetupScreen: new CryptohomeRecoverySetupScreenTester(),
       LocalPasswordSetupScreen: new LocalPasswordSetupScreenTester(),
+      PasswordFactorSuccessScreen: new PasswordFactorSuccessScreenTester(),
       GaiaInfoScreen: new GaiaInfoScreenTester(),
       ConsumerUpdateScreen: new ConsumerUpdateScreenTester(),
       ChoobeScreen: new ChoobeScreenTester(),
