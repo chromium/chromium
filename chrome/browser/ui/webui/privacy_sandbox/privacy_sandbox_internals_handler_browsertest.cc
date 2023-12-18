8b2e5532db3714e9d127f7d454329b8f90fa9b6a
Kevin Graney <kmg@google.com>
2023-12-18 19:37:38 +0000

    Revert "[gardener] Revert "Add mojo bindings for Privacy Sandbox DevUI""
    This reverts commit 479fb4d4f2d67860f22bca0109d49853362136a0.
    
    Reason for revert: Fixed test expectation to account for changes to the default content settings list
    
    Original change's description:
    > [gardener] Revert "Add mojo bindings for Privacy Sandbox DevUI"
    >
    > This reverts commit f1e7579a504f46db076cd761500ed1283f268e12.
    >
    > Reason for revert: Mostly consistently failing test on  linux-chromeos-chrome:
    > PrivacySandboxInternalsMojoTest.GetCookieContentSettings
    >
    > First failure:
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37612/test-results
    >
    > Another example failure:
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/37616/test-results
    >
    > Original change's description:
    > > Add mojo bindings for Privacy Sandbox DevUI
    > >
    > > Add initial mojo bindings for the Privacy Sandbox DevUI page.  These are
    > > expected to be only accessed by DevUI (chrome://) pages and to be
    > > read-only methods for the foreseeable future that are not able to mutate
    > > browser state.
    > >
    > > Content settings data is already exposed to WebUI pages and ContentSettingsPattern::ToString has some fuzzing coverage in
    > > content_settings_pattern_parser_fuzzer.cc.
    > >
    > > Bug: b:308167671
    > > Change-Id: I7bda8ebdfcc32f03cb40c260333b9484915223b3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5041330
    > > Reviewed-by: Emily Stark <estark@chromium.org>
    > > Commit-Queue: Kevin Graney <kmg@google.com>
    > > Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1233137}
    >
    > Bug: b:308167671
    > Change-Id: I6dc3a23d8f4ed2f3bd07a81ad6a706a561f60e4a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5087928
    > Auto-Submit: Pavol Marko <pmarko@chromium.org>
    > Owners-Override: Pavol Marko <pmarko@chromium.org>
    > Commit-Queue: Pavol Marko <pmarko@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1233279}
    
    Bug: b:308167671
    Change-Id: I477e5c0b5bb66aa54fb77d32d4d97f81c2026aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088328
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1238655}

---
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_handler_browsertest.cc
@@ -0,0 +1,151 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "base/functional/bind.h"
+#include "base/run_loop.h"
+#include "base/test/bind.h"
+#include "chrome/browser/content_settings/cookie_settings_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals.mojom.h"
+#include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_handler.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/content_settings_pattern_parser.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/test_utils.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+using ::privacy_sandbox_internals::mojom::PageHandler;
+using ::testing::AllOf;
+using ::testing::Ge;
+using ::testing::SizeIs;
+using ::testing::StrEq;
+using ::testing::UnorderedElementsAreArray;
+
+// Helper to aid in waiting for mojo callbacks to happen.
+class CallbackWaiter {
+ public:
+  void Notify() {
+    waiting_ = false;
+    if (runner_.get()) {
+      runner_->Quit();
+    }
+  }
+
+  void Wait() {
+    if (waiting_) {
+      runner_ = std::make_unique<base::RunLoop>();
+      runner_->Run();
+      runner_.reset();
+    }
+  }
+
+  void Reset() {
+    waiting_ = true;
+    if (runner_.get()) {
+      runner_->Quit();
+    }
+    runner_.reset();
+  }
+
+ private:
+  bool waiting_{true};
+  std::unique_ptr<base::RunLoop> runner_;
+};
+
+// Given an implementation `Handler` of a mojo handler for `Interface`, will
+// wire up a pipe with this object acting as the remote (client) side.
+template <typename Handler, typename Interface>
+class MojoPiping {
+ public:
+  explicit MojoPiping(Handler handler)
+      : handler_(std::move(handler)),
+        // Connect both ends to the mojo pipe.  The `Remote` is the client end
+        // and the `Receiver` is the end connected to the handler.
+        remote_(mojo::PendingRemote<Interface>(std::move(pipe_.handle0), 0)),
+        receiver_(&handler_,
+                  mojo::PendingReceiver<Interface>(std::move(pipe_.handle1))) {}
+
+  // For this test we only care about access to the `Remote` object since the
+  // test is acting as a client.
+  typename Interface::Proxy_* get() const { return remote_.get(); }
+  typename Interface::Proxy_* operator->() const { return get(); }
+  typename Interface::Proxy_& operator*() const { return *get(); }
+
+ private:
+  mojo::MessagePipe pipe_;
+  Handler handler_;
+  mojo::Remote<PageHandler> remote_;
+  mojo::Receiver<PageHandler> receiver_;
+};
+
+class PrivacySandboxInternalsMojoTest : public InProcessBrowserTest {
+ public:
+  PrivacySandboxInternalsMojoTest() = default;
+
+  MojoPiping<PrivacySandboxInternalsHandler, PageHandler> MakeMojoPiping() {
+    return MojoPiping<PrivacySandboxInternalsHandler, PageHandler>(
+        PrivacySandboxInternalsHandler(browser()->profile()));
+  }
+
+  void GetCookieContentSettingsCallback(
+      const std::vector<ContentSettingPatternSource>& vec) {
+    get_cookie_content_settings_cb_data_ = vec;
+    waiter_.Notify();
+  }
+
+  void ContentSettingsPatternToStringCallback(const std::string& s) {
+    content_settings_pattern_to_string_cb_data_ = s;
+    waiter_.Notify();
+  }
+
+ protected:
+  // Notified when _any_ callback from the mojo interface is made.
+  CallbackWaiter waiter_;
+
+  std::vector<ContentSettingPatternSource> get_cookie_content_settings_cb_data_;
+  std::string content_settings_pattern_to_string_cb_data_;
+};
+
+IN_PROC_BROWSER_TEST_F(PrivacySandboxInternalsMojoTest,
+                       GetCookieContentSettings) {
+  auto mojo = MakeMojoPiping();
+  content_settings::CookieSettings* settings =
+      CookieSettingsFactory::GetForProfile(browser()->profile()).get();
+  settings->SetCookieSetting(GURL("https://example.com"),
+                             CONTENT_SETTING_ALLOW);
+
+  mojo->GetCookieContentSettings(base::BindOnce(
+      &PrivacySandboxInternalsMojoTest::GetCookieContentSettingsCallback,
+      base::Unretained(this)));
+  waiter_.Wait();
+  EXPECT_THAT(
+      get_cookie_content_settings_cb_data_,
+      AllOf(
+          UnorderedElementsAreArray(settings->GetCookieSettings()),
+          SizeIs(Ge(1u))));  // Don't check exact size (default list may change)
+}
+
+IN_PROC_BROWSER_TEST_F(PrivacySandboxInternalsMojoTest, PatternPartsToString) {
+  auto mojo = MakeMojoPiping();
+
+  for (const std::string& regex :
+       {"[*.]example.com", "http://example.net", "example.org"}) {
+    ContentSettingsPattern pattern = ContentSettingsPattern::FromString(regex);
+    mojo->ContentSettingsPatternToString(
+        pattern, base::BindOnce(&PrivacySandboxInternalsMojoTest::
+                                    ContentSettingsPatternToStringCallback,
+                                base::Unretained(this)));
+    waiter_.Wait();
+    waiter_.Reset();
+    EXPECT_THAT(content_settings_pattern_to_string_cb_data_,
+                StrEq(pattern.ToString()));
+  }
+}
+}  // namespace
