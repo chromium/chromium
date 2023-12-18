58aa3e22a2fb88025f11be570c0d434c03a4a0f1
Danila Kuzmin <dkuzmin@google.com>
2023-12-18 12:13:49 +0000

    [oobe] Migrate demo_setup to TS
    Bug: b:314762334
    Change-Id: I02be7591ea11387c097fadc461e92a7bf595148a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5116382
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1238559}

--- chrome/browser/resources/chromeos/login/screens/oobe/demo_setup.ts
+++ chrome/browser/resources/chromeos/login/screens/oobe/demo_setup.ts
@@ -16,62 +16,54 @@ import '../../components/common_styles/oobe_dialog_host_styles.css.js';
 import '../../components/dialogs/oobe_adaptive_dialog.js';
 import '../../components/progress_list_item.js';
 
-import {loadTimeData} from '//resources/ash/common/load_time_data.m.js';
+import {loadTimeData} from '//resources/js/load_time_data.js';
+import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
 import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.js';
-import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.js';
+import {OobeDialogHostBehavior, OobeDialogHostBehaviorInterface} from '../../components/behaviors/oobe_dialog_host_behavior.js';
 import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.js';
 
 import {getTemplate} from './demo_setup.html.js';
 
-
 /**
  * UI mode for the dialog.
- * @enum {string}
  */
-const DemoSetupUIState = {
-  PROGRESS: 'progress',
-  ERROR: 'error',
+enum DemoSetupUiState {
+  PROGRESS = 'progress',
+  ERROR = 'error',
+}
+
+export const DemoSetupScreenBase = mixinBehaviors(
+                                       [
+                                         OobeI18nBehavior,
+                                         OobeDialogHostBehavior,
+                                         LoginScreenBehavior,
+                                         MultiStepBehavior,
+                                       ],
+                                       PolymerElement) as {
+  new (): PolymerElement & OobeI18nBehaviorInterface &
+      OobeDialogHostBehaviorInterface & LoginScreenBehaviorInterface &
+      MultiStepBehaviorInterface,
 };
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {LoginScreenBehaviorInterface}
- * @implements {MultiStepBehaviorInterface}
- * @implements {OobeI18nBehaviorInterface}
- */
-const DemoSetupScreenBase = mixinBehaviors(
-    [
-      OobeI18nBehavior,
-      OobeDialogHostBehavior,
-      LoginScreenBehavior,
-      MultiStepBehavior,
-    ],
-    PolymerElement);
-
-/**
- * @polymer
- */
 class DemoSetupScreen extends DemoSetupScreenBase {
   static get is() {
-    return 'demo-setup-element';
+    return 'demo-setup-element' as const;
   }
 
-  static get template() {
+  static get template(): HTMLTemplateElement {
     return getTemplate();
   }
 
-  static get properties() {
+  static get properties(): PolymerElementProperties {
     return {
       /** Object mapping step strings to step indices */
       setupSteps_: {
         type: Object,
         value() {
-          return /** @type {!Object} */ (
-              loadTimeData.getValue('demoSetupSteps'));
+          return loadTimeData.getValue('demoSetupSteps');
         },
       },
 
@@ -95,54 +87,54 @@ class DemoSetupScreen extends DemoSetupScreenBase {
     };
   }
 
+  private setupSteps_: Record<string, number>;
+  private currentStepIndex_: number;
+  private errorMessage_: string;
+  private isPowerwashRequired_: boolean;
+
   constructor() {
     super();
   }
 
-  /** @override */
-  ready() {
+  override ready(): void {
     super.ready();
     this.initializeLoginScreen('DemoSetupScreen');
   }
 
-  defaultUIStep() {
-    return DemoSetupUIState.PROGRESS;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override defaultUIStep() {
+    return DemoSetupUiState.PROGRESS;
   }
 
-  get UI_STEPS() {
-    return DemoSetupUIState;
+  override get UI_STEPS() {
+    return DemoSetupUiState;
   }
 
   /** Overridden from LoginScreenBehavior. */
-  // clang-format off
-  get EXTERNAL_API() {
-    return ['setCurrentSetupStep',
-            'onSetupSucceeded',
-            'onSetupFailed'];
+  override get EXTERNAL_API(): string[] {
+    return ['setCurrentSetupStep', 'onSetupSucceeded', 'onSetupFailed'];
   }
-  // clang-format on
 
-
-  onBeforeShow() {
+  override onBeforeShow(): void {
     this.reset();
   }
 
   /** Resets demo setup flow to the initial screen and starts setup. */
-  reset() {
-    this.setUIStep(DemoSetupUIState.PROGRESS);
+  reset(): void {
+    this.setUIStep(DemoSetupUiState.PROGRESS);
     this.userActed('start-setup');
   }
 
   /** Called after resources are updated. */
-  updateLocalizedContent() {
+  override updateLocalizedContent(): void {
     this.i18nUpdateLocale();
   }
 
   /**
    * Called at the beginning of a setup step.
-   * @param {string} currentStep The new step name.
+   * @param currentStep The new step name.
    */
-  setCurrentSetupStep(currentStep) {
+  setCurrentSetupStep(currentStep: string): void {
     // If new step index not specified, remain unchanged.
     if (this.setupSteps_.hasOwnProperty(currentStep)) {
       this.currentStepIndex_ = this.setupSteps_[currentStep];
@@ -150,43 +142,40 @@ class DemoSetupScreen extends DemoSetupScreenBase {
   }
 
   /** Called when demo mode setup succeeded. */
-  onSetupSucceeded() {
+  onSetupSucceeded(): void {
     this.errorMessage_ = '';
   }
 
   /**
    * Called when demo mode setup failed.
-   * @param {string} message Error message to be displayed to the user.
-   * @param {boolean} isPowerwashRequired Whether powerwash is required to
+   * @param message Error message to be displayed to the user.
+   * @param isPowerwashRequired Whether powerwash is required to
    *     recover from the error.
    */
-  onSetupFailed(message, isPowerwashRequired) {
+  onSetupFailed(message: string, isPowerwashRequired: boolean): void {
     this.errorMessage_ = message;
     this.isPowerwashRequired_ = isPowerwashRequired;
-    this.setUIStep(DemoSetupUIState.ERROR);
+    this.setUIStep(DemoSetupUiState.ERROR);
   }
 
   /**
    * Retry button click handler.
-   * @private
    */
-  onRetryClicked_() {
+  private onRetryClicked_(): void {
     this.reset();
   }
 
   /**
    * Powerwash button click handler.
-   * @private
    */
-  onPowerwashClicked_() {
+  private onPowerwashClicked_(): void {
     this.userActed('powerwash');
   }
 
   /**
    * Close button click handler.
-   * @private
    */
-  onCloseClicked_() {
+  private onCloseClicked_(): void {
     // TODO(wzang): Remove this after crbug.com/900640 is fixed.
     if (this.isPowerwashRequired_) {
       return;
@@ -196,34 +185,36 @@ class DemoSetupScreen extends DemoSetupScreenBase {
 
   /**
    * Whether a given step should be rendered on the UI.
-   * @param {string} stepName The name of the step (from the enum).
-   * @param {!Object} setupSteps
-   * @private
+   * @param stepName The name of the step (from the enum).
    */
-  shouldShowStep_(stepName, setupSteps) {
+  private shouldShowStep_(stepName: string, setupSteps: Object): boolean {
     return setupSteps.hasOwnProperty(stepName);
   }
 
   /**
    * Whether a given step is active.
-   * @param {string} stepName The name of the step (from the enum).
-   * @param {!Object} setupSteps
-   * @param {number} currentStepIndex
-   * @private
+   * @param stepName The name of the step (from the enum).
    */
-  stepIsActive_(stepName, setupSteps, currentStepIndex) {
+  private stepIsActive_(
+      stepName: string, setupSteps: Record<string, number>,
+      currentStepIndex: number): boolean {
     return currentStepIndex === setupSteps[stepName];
   }
 
   /**
    * Whether a given step is completed.
-   * @param {string} stepName The name of the step (from the enum).
-   * @param {!Object} setupSteps
-   * @param {number} currentStepIndex
-   * @private
+   * @param stepName The name of the step (from the enum).
    */
-  stepIsCompleted_(stepName, setupSteps, currentStepIndex) {
+  private stepIsCompleted_(
+      stepName: string, setupSteps: Record<string, number>,
+      currentStepIndex: number): boolean {
     return currentStepIndex > setupSteps[stepName];
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [DemoSetupScreen.is]: DemoSetupScreen;
   }
 }
 
