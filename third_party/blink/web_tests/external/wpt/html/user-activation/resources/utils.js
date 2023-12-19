fc20e44c837595900e20b4c607ba95f2c773ed82
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2023-12-19 18:18:57 +0000

    Import wpt@1cea438aed9655c42ed5476912476eb221c04daf
    Using wpt-import in Chromium f71c923f5eebd448494ba4efa6d87411bf5bd689.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mustaq@chromium.org:
      external/wpt/html/user-activation
    nsatragno@chromium.org:
      external/wpt/webauthn
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-chromium-rel
    Change-Id: Ide144afcbeced98b7d7ab4031d9ab245fbe1d3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5135236
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1239242}

--- third_party/blink/web_tests/external/wpt/html/user-activation/resources/utils.js
+++ third_party/blink/web_tests/external/wpt/html/user-activation/resources/utils.js
@@ -1,37 +1,56 @@
+/**
+ * Calls the function (f) after N number of frames have passed.
+ *
+ * @param {Function} f
+ * @param {number} num_frames
+ */
 function delayByFrames(f, num_frames) {
   function recurse(depth) {
-    if (depth == 0)
-      f();
-    else
-      requestAnimationFrame(() => recurse(depth-1));
+    if (depth == 0) f();
+    else requestAnimationFrame(() => recurse(depth - 1));
   }
   recurse(num_frames);
 }
 
-// Returns a Promise which is resolved with the event object when the event is
-// fired.
-function getEvent(eventType) {
-  return new Promise(resolve => {
-    document.body.addEventListener(eventType, e => resolve(e), {once: true});
+/**
+ * @param {string} eventType
+ * @param {object} options EventListenerOptions
+ * @returns {Promise<Event>} Resolved when the event is fired.
+ */
+function getEvent(eventType, options = { once: true }) {
+  return new Promise((resolve) => {
+    document.body.addEventListener(eventType, resolve, options);
   });
 }
 
-
-// Returns a Promise which is resolved with a "true" iff transient activation
-// was available and successfully consumed.
-//
-// This function relies on Fullscreen API to check/consume user activation
-// state.
-async function consumeTransientActivation() {
-  try {
-    await document.body.requestFullscreen();
-    await document.exitFullscreen();
-    return true;
-  } catch(e) {
-    return false;
+/**
+ *
+ * @param {Window} context
+ * @returns {Promise<Boolean>} resolved with a true if transient activation is consumed.
+ */
+async function consumeTransientActivation(context = window) {
+  if (!context.navigator.userActivation.isActive) {
+    throw new Error(
+      "User activation is not active so can't be consumed. Something is probably wrong with the test."
+    );
   }
+  if (test_driver?.consume_user_activation) {
+    return test_driver.consume_user_activation(context);
+  }
+  // fallback to Fullscreen API.
+  if (!context.document.fullscreenElement) {
+    await context.document.documentElement.requestFullscreen();
+  }
+  await context.document.exitFullscreen();
+  return !context.navigator.userActivation.isActive;
 }
 
+/**
+ * Waits for a message to be sent from an iframe.
+ *
+ * @param {string} type
+ * @returns {Promise<Object>}
+ */
 function receiveMessage(type) {
   return new Promise((resolve) => {
     window.addEventListener("message", function listener(event) {
@@ -45,4 +64,20 @@ function receiveMessage(type) {
       }
     });
   });
+}
+
+/**
+ * Creates an iframe and waits for it to load...
+ *
+ * @param {String} src
+ * @returns {Promise<HTMLIFrameElement>}
+ */
+async function attachIframe(src, document = window.document) {
+  const iframe = document.createElement("iframe");
+  await new Promise((resolve) => {
+    iframe.addEventListener("load", resolve, { once: true });
+    document.body.appendChild(iframe);
+    iframe.src = src;
+  });
+  return iframe;
 }
