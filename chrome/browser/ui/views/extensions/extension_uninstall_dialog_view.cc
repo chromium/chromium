ad7e729ce97796c707de63abf56d1c0501a061bb
EmiliaPaz <emiliapaz@chromium.org>
2023-12-19 22:12:16 +0000

    [Extensions] Remove unnecessary extra button
    Remove extra button added by mistake on crrev.com/c/5121173
    
    Bug: n/a
    Change-Id: I0c4f308967d6207d63e9ba25677cd63416868b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136881
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239377}

--- chrome/browser/ui/views/extensions/extension_uninstall_dialog_view.cc
+++ chrome/browser/ui/views/extensions/extension_uninstall_dialog_view.cc
@@ -81,9 +81,6 @@ void ExtensionUninstallDialogViews::Show() {
               icon(), skia::ImageOperations::ResizeMethod::RESIZE_GOOD,
               gfx::Size(extension_misc::EXTENSION_ICON_SMALL,
                         extension_misc::EXTENSION_ICON_SMALL))))
-      .AddExtraButton(
-          base::DoNothing(),
-          ui::DialogModel::Button::Params().SetLabel(u"extra button"))
       .AddOkButton(
           base::BindOnce(&ExtensionUninstallDialogViews::DialogAccepted,
                          weak_ptr_factory_.GetWeakPtr()),
