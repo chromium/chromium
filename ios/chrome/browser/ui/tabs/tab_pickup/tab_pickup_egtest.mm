02418225c7b223528dd297307dc7e83453ed1857
Arthur Milchior <arthurmilchior@chromium.org>
2023-12-19 11:14:27 +0000

    [iOS] Do not display search engine choice in tab pickup egtests
    Locally, the test testBannerDisplayedAfterBackground failed because, when
    the browser restarted, the test engine choice selection appeared.
    
    This change apply to all tests. As those tests do not relate to the choice screen, it will make them quicker and not break anything.
    
    Bug: 1513072
    Change-Id: I09a1f5013128c8fd476614607e74b5e4e2fd3f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132775
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239028}

--- ios/chrome/browser/ui/tabs/tab_pickup/tab_pickup_egtest.mm
+++ ios/chrome/browser/ui/tabs/tab_pickup/tab_pickup_egtest.mm
@@ -4,6 +4,7 @@
 
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "components/search_engines/search_engines_switches.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
@@ -82,6 +83,9 @@ id<GREYMatcher> TabPickupSwitchItem(bool is_toggled_on, bool enabled) {
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(kTabPickupThreshold);
+
+  config.additional_args.push_back(std::string("--") +
+                                   switches::kDisableSearchEngineChoiceScreen);
 
   // In order to present banners on the NTP, the tab resumption feature must be
   // disabled.
