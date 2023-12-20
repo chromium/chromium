abd21e00a31ca7a539bd1d676250f0d4ad7f2395
Abe Boujane <boujane@google.com>
2023-12-20 02:31:03 +0000

    Add dependency on Silent Onboarding for the TrackingProtection Control Group
    This ensures that there are no differences in timing and groups between
    Control and Treatment.
    This CL also moves the CookieDeprecationTestingEnableSilentOnboarding to
    content/public so it can be depended on from components.
    
    Bug: 1513308
    Change-Id: Ibfeeac8b34a8a4e09ec92d7feeb867c52773cfc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137977
    Auto-Submit: Abe Boujane <boujane@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239513}

--- chrome/browser/privacy_sandbox/tracking_protection_onboarding_factory.cc
+++ chrome/browser/privacy_sandbox/tracking_protection_onboarding_factory.cc
@@ -7,6 +7,7 @@
 #include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
+#include "chrome/browser/tpcd/experiment/tpcd_experiment_features.h"
 #include "chrome/common/channel_info.h"
 
 TrackingProtectionOnboardingFactory*
@@ -35,5 +36,6 @@ TrackingProtectionOnboardingFactory::BuildServiceInstanceForBrowserContext(
   Profile* profile = Profile::FromBrowserContext(context);
 
   return std::make_unique<privacy_sandbox::TrackingProtectionOnboarding>(
-      profile->GetPrefs(), chrome::GetChannel());
+      profile->GetPrefs(), chrome::GetChannel(),
+      tpcd::experiment::kEnableSilentOnboarding.Get());
 }
