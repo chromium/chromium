1e15a765396f156b7653aaf5ac4d75f0d967a736
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-12-20 18:03:23 +0000

    Merge back to M120 of [Privacy Sandbox] Do not convert attestation list directory using AsUTF8Unsafe().
    NOTE: This CL had merge conflicts because:
    1. There was a fuzzer test for the parser merged in M121.
    2. The implementation of LoadAttestationInternal was changed in M121.
    
    The merge conflicts have been resolved.
    
    Original CL description:
    
    From histograms, we found there is a Windows specific issue that the
    std::ifstream fails to open. I tested the existing implementation and
    found it fails to open the file when the directory contains combining
    characters, for example "Å" and "ö". See crrev.com/c/5089910 for the
    try bot failures.
    
    I realize that the converted directory returned by
    `base::FilePath::AsUTF8Unsafe()` is not guaranteed to work across
    platforms. We should stick to `base::FilePath`.
    
    Changed to `ReadFileToString`, then `ParseFromString`. It seems most
    existing proto parsing implementation in Chromium is done this way.
    
    Although there is no crash reports that we can use to investigate if
    the combining character is actually the root cause, this is a fix we
    should make.
    
    (cherry picked from commit bed5fa2c1633b2fcacd653286e3df952ff31b646)
    
    Bug: 1509845
    Change-Id: I5d11b4823585c39dc0bd22c24e164a45e9762493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5090610
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1235828}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5131737
    Cr-Commit-Position: refs/branch-heads/6099@{#1567}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
+++ components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.cc
@@ -4,9 +4,9 @@
 
 #include "components/privacy_sandbox/privacy_sandbox_attestations/privacy_sandbox_attestations.h"
 
-#include <fstream>
 #include <ios>
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -113,10 +113,12 @@ absl::optional<PrivacySandboxAttestationsMap> LoadAttestationsInternal(
   CHECK(base::FeatureList::IsEnabled(
       privacy_sandbox::kEnforcePrivacySandboxAttestations));
 
-  std::ifstream stream(installed_file_path.AsUTF8Unsafe(),
-                       std::ios::binary | std::ios::in);
-  if (!stream.is_open()) {
-    // File does not exist.
+  std::string proto_str;
+  // When reading the file, the `base::FilePath` directory should be used to
+  // make sure it works across platforms. If using the converted directory
+  // returned by `base::FilePath::AsUTF8Unsafe()`, it fails on Windows when the
+  // directory contains combining characters.
+  if (!base::ReadFileToString(installed_file_path, &proto_str)) {
     return absl::nullopt;
   }
 
@@ -136,7 +138,7 @@ absl::optional<PrivacySandboxAttestationsMap> LoadAttestationsInternal(
   // the attestations file from being parsed again.
   base::ElapsedTimer parsing_timer;
   absl::optional<PrivacySandboxAttestationsMap> attestations_map =
-      ParseAttestationsFromStream(stream);
+      ParseAttestationsFromString(proto_str);
   if (!attestations_map.has_value()) {
     // The parsing failed.
     return absl::nullopt;
