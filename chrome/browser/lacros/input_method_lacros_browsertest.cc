0d7a205f6da5b610064b5dfe8e8e76e6bab54811
Georg Neis <neis@chromium.org>
2023-12-20 09:00:12 +0000

    Fix issue in InputMethodLacrosBrowserTest
    Start the text input observation only after the test page has received
    focus. This makes sure that we don't accidentally observe events
    caused by the automatic omnibox focus.
    
    Get rid of a previous workaround and reenable a test.
    
    Bug: chromium:1510768, b:315079554
    Change-Id: I5fc4e00368604b59f36a1164b5dba96085a4e4e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119976
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239592}

--- chrome/browser/lacros/input_method_lacros_browsertest.cc
+++ chrome/browser/lacros/input_method_lacros_browsertest.cc
@@ -101,6 +101,9 @@ bool RenderHtmlInLacros(Browser* browser, const std::string& html) {
           ->GetNativeWindow()
           ->GetRootWindow());
   EXPECT_TRUE(browser_test_util::WaitForWindowCreation(window_id));
+  EXPECT_TRUE(BrowserView::GetBrowserViewForBrowser(browser)
+                  ->contents_web_view()
+                  ->HasFocus());
   return true;
 }
 
@@ -581,12 +584,14 @@ IN_PROC_BROWSER_TEST_P(InputMethodLacrosBrowserTest,
 
 IN_PROC_BROWSER_TEST_P(InputMethodLacrosBrowserTest,
                        CommitTextUpdatesSurroundingText) {
+  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+
   mojo::Remote<InputMethodTestInterface> input_method =
       BindInputMethodTestInterface(GetParam());
   if (!input_method.is_bound()) {
     GTEST_SKIP() << "Unsupported ash version";
   }
-  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+
   InputMethodTestInterfaceAsyncWaiter input_method_async_waiter(
       input_method.get());
   input_method_async_waiter.WaitForFocus();
@@ -874,15 +879,16 @@ IN_PROC_BROWSER_TEST_P(InputMethodLacrosBrowserTest,
   EXPECT_FALSE(event_listener.HasMessages());
 }
 
-// TODO(crbug.com/1510768): Reenable once fixed.
 IN_PROC_BROWSER_TEST_P(InputMethodLacrosBrowserTest,
-                       DISABLED_SetCompositionUpdatesSurroundingText) {
+                       SetCompositionUpdatesSurroundingText) {
+  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+
   mojo::Remote<InputMethodTestInterface> input_method =
       BindInputMethodTestInterface(GetParam());
   if (!input_method.is_bound()) {
     GTEST_SKIP() << "Unsupported ash version";
   }
-  const std::string id = RenderAutofocusedInputFieldInLacros(browser());
+
   InputMethodTestInterfaceAsyncWaiter input_method_async_waiter(
       input_method.get());
   input_method_async_waiter.WaitForFocus();
