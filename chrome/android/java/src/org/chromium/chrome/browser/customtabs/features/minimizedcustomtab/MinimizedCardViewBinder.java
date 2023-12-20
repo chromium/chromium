9c46c2056f1be9360c9663f8f6bac0905becf4b1
Sinan Sahin <sinansahin@google.com>
2023-12-20 18:14:47 +0000

    [MCT] Round the corners of the favicon
    Bug: 1511383
    Change-Id: Iaca596a53cebecab50762089b042bbde9130e0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5138359
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239754}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedCardViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/minimizedcustomtab/MinimizedCardViewBinder.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.ui.UiUtils;
+import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -39,7 +40,11 @@ public class MinimizedCardViewBinder {
                                 R.color.default_icon_color_tint_list);
                 favicon.setImageDrawable(icon);
             } else {
-                favicon.setImageBitmap(model.get(FAVICON));
+                var res = view.getResources();
+                int radius = res.getDimensionPixelSize(R.dimen.default_favicon_corner_radius);
+                var roundedIcon = ViewUtils.createRoundedBitmapDrawable(res, bitmap, radius);
+
+                favicon.setImageDrawable(roundedIcon);
             }
         }
     }
