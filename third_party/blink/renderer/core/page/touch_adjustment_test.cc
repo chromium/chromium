13de1d469fe7727aa3bb5ee5830aba0c6d0ec876
Chris Harrelson <chrishtr@chromium.org>
2023-12-20 17:27:25 +0000

    Set device scale factor to 1 when an inspector overrides scales.
    This was inadvertently broken in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3561740
    
    Bug: 1342049
    
    Change-Id: I58da4e4f5f94832f8cbf6712e04bc2b36a3c6676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136585
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239730}

--- third_party/blink/renderer/core/page/touch_adjustment_test.cc
+++ third_party/blink/renderer/core/page/touch_adjustment_test.cc
@@ -96,6 +96,11 @@ TEST_F(TouchAdjustmentTest, AdjustmentRangeUpperboundScale) {
   SetZoomAndScale(0.5 /* dsf */, 1 /* browser_zoom */, 1 /* page_scale */);
   result = GetHitTestRectForAdjustment(GetFrame(), touch_area);
   EXPECT_EQ(result, max_touch_area_dip_unscaled * 0.5f);
+
+  SetZoomAndScale(2 /* dsf */, 1 /* browser_zoom */, 1 /* page_scale */);
+  GetPage().SetInspectorDeviceScaleFactorOverride(0.5);
+  result = GetHitTestRectForAdjustment(GetFrame(), touch_area);
+  EXPECT_EQ(result, max_touch_area_dip_unscaled);
 }
 
 TEST_F(TouchAdjustmentTest, AdjustmentRangeLowerboundScale) {
