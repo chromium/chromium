ca7a53131c64aab10c780841f2fcc46ce686a478
Sylvain Defresne <sdefresne@chromium.org>
2023-12-21 13:17:53 +0000

    [ios][web] Mark Referrer related conversion function as public
    Move the function to convert Referrer to/from protobuf message
    to the public file //ios/web/public/session/proto/proto_util.h
    to allow code outside of //ios/web to use those functions.
    
    Bug: 1383087
    Change-Id: I609f6e3dac0f2e933e9fb4ea8632fc2ddc4c13c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136155
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1240105}

--- ios/web/navigation/proto_util.h
+++ ios/web/navigation/proto_util.h
@@ -7,23 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
-#include "ios/web/public/navigation/referrer.h"
 #include "ios/web/public/session/proto/navigation.pb.h"
 
 namespace web {
-
-// Converts a web::proto::ReferrerPolicy to a web::ReferrerPolicy.
-ReferrerPolicy ReferrerPolicyFromProto(proto::ReferrerPolicy value);
-
-// Converts a web::ReferrerPolicy to a web::proto::ReferrerPolicy.
-proto::ReferrerPolicy ReferrerPolicyToProto(ReferrerPolicy value);
-
-// Creates a Referrer from serialized `storage`.
-Referrer ReferrerFromProto(const proto::ReferrerStorage& storage);
-
-// Serializes `referrer` into `storage`.
-void SerializeReferrerToProto(const Referrer& referrer,
-                              proto::ReferrerStorage& storage);
 
 // Creates a mutable HTTP request headers dictionary from serialized `storage`.
 NSMutableDictionary<NSString*, NSString*>* HttpRequestHeadersFromProto(
