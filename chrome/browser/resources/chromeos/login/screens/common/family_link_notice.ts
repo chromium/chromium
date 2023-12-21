67a518b6e9750e93136db52226e27e04e7cb6f04
Brahim Chikhaoui <bchikhaoui@google.com>
2023-12-21 13:07:28 +0000

    [OOBE] Migrate family_link to TS
    Bug: b:314761865
    Change-Id: I77e0120d90e889d57a31c374f91d3bb1c0dc0f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5134514
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240104}

--- chrome/browser/resources/chromeos/login/screens/common/family_link_notice.ts
+++ chrome/browser/resources/chromeos/login/screens/common/family_link_notice.ts
@@ -12,48 +12,35 @@ import '../../components/oobe_icons.html.js';
 import '../../components/common_styles/oobe_common_styles.css.js';
 import '../../components/common_styles/oobe_dialog_host_styles.css.js';
 
-import {I18nBehavior} from '//resources/ash/common/i18n_behavior.js';
-import {html, mixinBehaviors, Polymer, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
+import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
-import {OobeDialogHostBehavior} from '../../components/behaviors/oobe_dialog_host_behavior.js';
+import {OobeDialogHostBehavior, OobeDialogHostBehaviorInterface} from '../../components/behaviors/oobe_dialog_host_behavior.js';
 import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.js';
-import {OobeNextButton} from '../../components/buttons/oobe_next_button.js';
-import {OobeAdaptiveDialog} from '../../components/dialogs/oobe_adaptive_dialog.js';
 
 import {getTemplate} from './family_link_notice.html.js';
 
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {LoginScreenBehaviorInterface}
- * @implements {OobeI18nBehaviorInterface}
- */
-const FamilyLinkScreenElementBase = mixinBehaviors(
-    [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
-    PolymerElement);
+export const FamilyLinkScreenElementBase =
+    mixinBehaviors(
+        [OobeI18nBehavior, OobeDialogHostBehavior, LoginScreenBehavior],
+        PolymerElement) as {
+      new (): PolymerElement & OobeI18nBehaviorInterface &
+          OobeDialogHostBehaviorInterface & LoginScreenBehaviorInterface,
+    };
 
-/**
- * @typedef {{
- *   familyLinkDialog:  OobeAdaptiveDialog,
- * }}
- */
-FamilyLinkScreenElementBase.$;
 
-/**
- * @polymer
- */
-class FamilyLinkNotice extends FamilyLinkScreenElementBase {
+export class FamilyLinkNotice extends FamilyLinkScreenElementBase {
   static get is() {
-    return 'family-link-notice-element';
+    return 'family-link-notice-element' as const;
   }
 
-  static get template() {
+  static get template(): HTMLTemplateElement {
     return getTemplate();
   }
 
-  static get properties() {
+  static get properties(): PolymerElementProperties {
     return {
       /**
        * If the gaia account is newly created
@@ -81,9 +68,11 @@ class FamilyLinkNotice extends FamilyLinkScreenElementBase {
     };
   }
 
-  /** Overridden from LoginScreenBehavior. */
-  // clang-format off
-  get EXTERNAL_API() {
+  private isNewGaiaAccount_: boolean;
+  private email_: string;
+  private domain_: string;
+
+  override get EXTERNAL_API(): string[] {
     return [
       'setDisplayEmail',
       'setDomain',
@@ -91,43 +80,36 @@ class FamilyLinkNotice extends FamilyLinkScreenElementBase {
     ];
   }
 
-  // clang-format on
-
-  /** @override */
-  ready() {
+  override ready(): void {
     super.ready();
     this.initializeLoginScreen('FamilyLinkNoticeScreen');
   }
 
   /**
    * Returns default event target element.
-   * @type {Object}
    */
-  get defaultControl() {
-    return this.$.familyLinkDialog;
+  override get defaultControl(): HTMLElement|null {
+    return this.shadowRoot!.querySelector('#familyLinkDialog');
   }
 
   /**
    * Sets email address.
-   * @param {string} email
    */
-  setDisplayEmail(email) {
+  setDisplayEmail(email: string): void {
     this.email_ = email;
   }
 
   /**
    * Sets enterprise domain.
-   * @param {string} domain
    */
-  setDomain(domain) {
+  setDomain(domain: string): void {
     this.domain_ = domain;
   }
 
   /**
    * Sets if the gaia account is newly created.
-   * @param {boolean} isNewGaiaAccount
    */
-  setIsNewGaiaAccount(isNewGaiaAccount) {
+  setIsNewGaiaAccount(isNewGaiaAccount: boolean): void {
     this.isNewGaiaAccount_ = isNewGaiaAccount;
   }
 
@@ -135,14 +117,13 @@ class FamilyLinkNotice extends FamilyLinkScreenElementBase {
    * Returns the title of the dialog based on if account is managed. Account is
    * managed when email or domain field is not empty and we show parental
    * controls is not eligible.
-   *
-   * @private
    */
-  getDialogTitle_(locale, email, domain) {
+  private getDialogTitle_(locale: string, email: string, domain: string):
+      string {
     if (email || domain) {
-      return this.i18n('familyLinkDialogNotEligibleTitle');
+      return this.i18nDynamic(locale, 'familyLinkDialogNotEligibleTitle');
     } else {
-      return this.i18n('familyLinkDialogTitle');
+      return this.i18nDynamic(locale, 'familyLinkDialogTitle');
     }
   }
 
@@ -150,17 +131,19 @@ class FamilyLinkNotice extends FamilyLinkScreenElementBase {
    * Formats and returns the subtitle of the dialog based on if account is
    * managed or if account is newly created. Account is managed when email or
    * domain field is not empty and we show parental controls is not eligible.
-   *
-   * @private
    */
-  getDialogSubtitle_(locale, isNewGaiaAccount, email, domain) {
+  private getDialogSubtitle_(
+      locale: string, isNewGaiaAccount: boolean, email: string,
+      domain: string): string {
     if (email || domain) {
       return this.i18n('familyLinkDialogNotEligibleSubtitle', email, domain);
     } else {
       if (isNewGaiaAccount) {
-        return this.i18n('familyLinkDialogNewGaiaAccountSubtitle');
+        return this.i18nDynamic(
+            locale, 'familyLinkDialogNewGaiaAccountSubtitle');
       } else {
-        return this.i18n('familyLinkDialogExistingGaiaAccountSubtitle');
+        return this.i18nDynamic(
+            locale, 'familyLinkDialogExistingGaiaAccountSubtitle');
       }
     }
   }
@@ -168,10 +151,15 @@ class FamilyLinkNotice extends FamilyLinkScreenElementBase {
   /**
    * On-tap event handler for Continue button.
    *
-   * @private
    */
-  onContinueButtonPressed_() {
+  private onContinueButtonPressed_(): void {
     this.userActed('continue');
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [FamilyLinkNotice.is]: FamilyLinkNotice;
   }
 }
 
