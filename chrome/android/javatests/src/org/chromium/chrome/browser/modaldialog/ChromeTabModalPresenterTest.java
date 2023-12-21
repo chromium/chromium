3c1d5499cd643326361049057dd094ba65679583
Lijin Shen <lazzzis@google.com>
2023-12-21 01:38:30 +0000

    Do not dismiss dialog when back press interception fails
    Bug: 1512277
    Change-Id: I98ede09f25e2860aa6a18654dab505bb2acc92d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142531
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1239953}

--- chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenterTest.java
@@ -5,7 +5,6 @@
 package org.chromium.chrome.browser.modaldialog;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -31,7 +30,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.Espresso;
 import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -53,6 +51,7 @@ import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.cc.input.BrowserControlsState;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.omnibox.UrlBar;
 import org.chromium.chrome.browser.omnibox.UrlFocusChangeListener;
@@ -64,6 +63,7 @@ import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.batch.BlankCTATabInitialStateRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
+import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.browser_ui.modaldialog.ModalDialogTestUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.EmbeddedTestServer;
@@ -141,7 +141,7 @@ public class ChromeTabModalPresenterTest {
     public void tearDown() {
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    mManager.destroy();
+                    mManager.dismissAllDialogs(DialogDismissalCause.UNKNOWN);
                 });
     }
 
@@ -421,7 +421,8 @@ public class ChromeTabModalPresenterTest {
     @Test
     @SmallTest
     @Feature({"ModalDialog"})
-    public void testDismiss_BackPressed() throws Exception {
+    @Features.DisableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
+    public void testDismiss_BackPressed() {
         PropertyModel dialog1 = createDialog(mActivity, mManager, "1", null);
         PropertyModel dialog2 = createDialog(mActivity, mManager, "2", null);
 
@@ -439,7 +440,7 @@ public class ChromeTabModalPresenterTest {
         checkCurrentPresenter(mManager, ModalDialogType.TAB);
 
         // Perform a back press. The first tab modal dialog should be dismissed.
-        Espresso.pressBack();
+        pressBack();
         checkPendingSize(mManager, ModalDialogType.TAB, 0);
         onView(withId(R.id.tab_modal_dialog_container))
                 .check(
@@ -451,7 +452,7 @@ public class ChromeTabModalPresenterTest {
         checkCurrentPresenter(mManager, ModalDialogType.TAB);
 
         // Perform a second back press. The second tab modal dialog should be dismissed.
-        Espresso.pressBack();
+        pressBack();
         checkPendingSize(mManager, ModalDialogType.APP, 0);
         checkPendingSize(mManager, ModalDialogType.TAB, 0);
         onView(withId(R.id.tab_modal_dialog_container))
@@ -462,6 +463,14 @@ public class ChromeTabModalPresenterTest {
                                         not(hasDescendant(withText("2"))))));
         ChromeModalDialogTestUtils.checkBrowserControls(mActivity, false);
         checkCurrentPresenter(mManager, null);
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"ModalDialog"})
+    @Features.EnableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
+    public void testDismiss_BackPressed_BackPressRefactor() {
+        testDismiss_BackPressed();
     }
 
     @Test
@@ -497,6 +506,7 @@ public class ChromeTabModalPresenterTest {
     @Test
     @SmallTest
     @Feature({"ModalDialog"})
+    @Features.DisableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
     public void testDismiss_DismissalCause_BackPressed() throws Exception {
         PropertyModel dialog1 = createDialog(mActivity, mManager, "1", mTestObserver);
         mExpectedDismissalCause = DialogDismissalCause.NAVIGATE_BACK_OR_TOUCH_OUTSIDE;
@@ -505,10 +515,18 @@ public class ChromeTabModalPresenterTest {
 
         // Dismiss the tab modal dialog and verify dismissal cause.
         int callCount = mTestObserver.onDialogDismissedCallback.getCallCount();
-        Espresso.pressBack();
+        pressBack();
         mTestObserver.onDialogDismissedCallback.waitForCallback(callCount);
 
         mExpectedDismissalCause = null;
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"ModalDialog"})
+    @Features.EnableFeatures({ChromeFeatureList.BACK_GESTURE_REFACTOR})
+    public void testDismiss_DismissalCause_BackPressed_BackPressRefactor() throws Exception {
+        testDismiss_DismissalCause_BackPressed();
     }
 
     @Test
@@ -609,5 +627,10 @@ public class ChromeTabModalPresenterTest {
     private void ensureDialogContainerVisible() {
         final View dialogContainer = mTabModalPresenter.getDialogContainerForTest();
         onViewWaiting(allOf(is(dialogContainer), isDisplayed()));
+    }
+
+    private void pressBack() {
+        TestThreadUtils.runOnUiThreadBlocking(
+                mActivity.getOnBackPressedDispatcher()::onBackPressed);
     }
 }
