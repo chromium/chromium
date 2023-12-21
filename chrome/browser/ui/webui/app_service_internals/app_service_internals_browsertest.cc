7c13f847acbad236d74906107b0811db59723282
David Bienvenu <davidbienvenu@chromium.org>
2023-12-21 15:00:42 +0000

    Stop AppServiceInternals tests from modifying desktop
    Use OsIntegrationManager::ScopedSuppressForTesting to stop
    AppServiceInternalBrowserTests pinning shortcut to
    windows taskbar and other web app os integrations.
    
    Remove ScopedPathOverride(DIR_START_MENU) since ScopedSuppressForTesting
    will prevent writing to the actual start menu.
    
    Bug: 1405159
    Change-Id: I207d1808893a015cb95adbc10d8cc759b87da680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142587
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240146}

--- chrome/browser/ui/webui/app_service_internals/app_service_internals_browsertest.cc
+++ chrome/browser/ui/webui/app_service_internals/app_service_internals_browsertest.cc
@@ -5,23 +5,17 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
+#include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
-#if BUILDFLAG(IS_WIN)
-#include "base/base_paths_win.h"
-#include "base/test/scoped_path_override.h"
-#endif  // BUILDFLAG(IS_WIN)
-
 class AppServiceInternalsBrowserTest : public InProcessBrowserTest {
-#if BUILDFLAG(IS_WIN)
  private:
-  // This is used to prevent creating shortcuts in the start menu dir.
-  base::ScopedPathOverride override_start_dir_{base::DIR_START_MENU};
-#endif  // BUILDFLAG(IS_WIN)
+  // Stop test from installing OS hooks.
+  web_app::OsIntegrationManager::ScopedSuppressForTesting os_hooks_suppress_;
 };
 
 IN_PROC_BROWSER_TEST_F(AppServiceInternalsBrowserTest, LoadsWebUiPage) {
