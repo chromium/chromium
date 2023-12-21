742a8296f33cbd28cfff8a76c66aa8d6b53709d3
Hyowon Kim <hyowon@igalia.com>
2023-12-21 03:05:48 +0000

    [DanglingPtr] Fix dangling pointer in ContextTestBase
    This CL resets raw_ptrs before gpu::GLInProcessContext is destroyed.
    
    Bug: 1291138
    Change-Id: I73c7269717510a08198e07d7a84f3b08fdd37049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5133016
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1239983}

--- gpu/ipc/client/gpu_in_process_context_tests.cc
+++ gpu/ipc/client/gpu_in_process_context_tests.cc
@@ -40,11 +40,15 @@ class ContextTestBase : public testing::Test {
     context_support_ = context_->GetImplementation();
   }
 
-  void TearDown() override { context_.reset(); }
+  void TearDown() override {
+    gl_ = nullptr;
+    context_support_ = nullptr;
+    context_.reset();
+  }
 
  protected:
-  raw_ptr<gpu::gles2::GLES2Interface, DanglingUntriaged> gl_;
-  raw_ptr<gpu::ContextSupport, DanglingUntriaged> context_support_;
+  raw_ptr<gpu::gles2::GLES2Interface> gl_;
+  raw_ptr<gpu::ContextSupport> context_support_;
 
  private:
   gpu::InProcessGpuThreadHolder gpu_thread_holder_;
