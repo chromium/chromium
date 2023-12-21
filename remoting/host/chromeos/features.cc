b64a165c4c7d29fe2d3bb0bdb9a84353c6e5961f
Jeroen Dhollander <jeroendh@google.com>
2023-12-21 12:33:14 +0000

    Cleanup ForceEnableEnterpriseCrdFileTransfer feature flag
    This was only there for testing purpose
    
    Bug: N/A
    Change-Id: I644fa651518592003161a514356b51965e56142a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5119892
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240096}

--- remoting/host/chromeos/features.cc
+++ remoting/host/chromeos/features.cc
@@ -20,8 +20,4 @@ BASE_FEATURE(kEnableCrdFileTransferForKiosk,
              "EnableCrdFileTransferForKiosk",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kForceEnableEnterpriseCrdFileTransfer,
-             "ForceEnableEnterpriseCrdFileTransfer",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 }  // namespace remoting::features
