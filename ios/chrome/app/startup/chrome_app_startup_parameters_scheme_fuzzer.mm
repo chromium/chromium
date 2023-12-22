8830ca021d511ab46f75e29fdc41866e6987e2a2
Nicolas MacBeth <nicolasmacbeth@google.com>
2023-12-22 15:14:21 +0000

    [iOS external action] Fuzzer for external action scheme
    Fuzzer for the chrome_app_startup_parameters including for the new
    external action scheme.
    
    Bug: b:311412541
    Change-Id: I617863491dcf11aaf2a3d73282d2eb9de7de3a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5037393
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1240572}

---
+++ ios/chrome/app/startup/chrome_app_startup_parameters_scheme_fuzzer.mm
@@ -0,0 +1,16 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/app/startup/chrome_app_startup_parameters.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string str = std::string(reinterpret_cast<const char*>(data), size);
+
+  NSString* formatted_string = [NSString stringWithFormat:@"%s", str.c_str()];
+  NSURL* url = [NSURL URLWithString:formatted_string];
+  [ChromeAppStartupParameters startupParametersWithURL:url
+                                     sourceApplication:nil];
+
+  return 0;
+}
