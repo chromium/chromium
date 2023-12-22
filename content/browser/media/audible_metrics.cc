875faf44ba966b13a7f4fa6239260fb432c3a413
Christine Smith <christinesm@chromium.org>
2023-12-22 09:23:03 +0000

    [Code Health] Remove expired histogram Media.Audible.CloseNewestToExitConcurrentPlayback*
    Removes an expired histogram.
    According to code health rotation instructions,
    an obsoletion message is not required for this change.
    
    Bug: 960055
    Change-Id: Ib931dac1d07460084c9624c16391832401795d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5141766
    Auto-Submit: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240490}

--- content/browser/media/audible_metrics.cc
+++ content/browser/media/audible_metrics.cc
@@ -35,22 +35,6 @@ void AudibleMetrics::WebContentsDestroyed(const WebContents* web_contents,
                                           bool recently_audible) {
   if (base::Contains(audible_web_contents_, web_contents))
     RemoveAudibleWebContents(web_contents);
-
-  // If we have two web contents and we go down to one, we should record
-  // whether we destroyed the most recent one. This is used to determine
-  // whether a user closes a new or old tab after starting playback if
-  // they have multiple tabs.
-  if (audible_web_contents_.size() == 1 && recently_audible) {
-    ExitConcurrentPlaybackContents value =
-        last_audible_web_contents_.back() == web_contents
-            ? ExitConcurrentPlaybackContents::kMostRecent
-            : ExitConcurrentPlaybackContents::kOlder;
-
-    UMA_HISTOGRAM_ENUMERATION(
-        "Media.Audible.CloseNewestToExitConcurrentPlayback", value);
-  }
-
-  last_audible_web_contents_.remove(web_contents);
 }
 
 void AudibleMetrics::SetClockForTest(const base::TickClock* test_clock) {
@@ -63,11 +47,6 @@ void AudibleMetrics::AddAudibleWebContents(const WebContents* web_contents) {
       1, 10, 11);
 
   audible_web_contents_.insert(web_contents);
-
-  // Since the web contents is newly audible then move it to the back of the
-  // last audible web contents list.
-  last_audible_web_contents_.remove(web_contents);
-  last_audible_web_contents_.push_back(web_contents);
 
   if (audible_web_contents_.size() > 1 &&
       concurrent_web_contents_start_time_.is_null()) {
