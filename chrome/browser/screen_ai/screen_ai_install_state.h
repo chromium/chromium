c99abbc12a4ea8549ea1f9938359ae13cc2d6b9b
Taiyo Mizuhashi <taiyo@chromium.org>
2023-12-22 03:41:29 +0000

    Revert "Add a screen-ai CIPD package for PDF OCR E2E tests on Linux"
    This reverts commit 8c005672e75f619a1f2df8241fe996f9f03cbe7d.
    
    Reason for revert: 
    
    The test |PDFOCRIntegrationTest.EnsureScreenAIInitializes| introduced 
    in this CL consistently fails in Linux MSan Tests.
    
    Please see crbug.com/1513808 for more information.
    
    
    Original change's description:
    > Add a screen-ai CIPD package for PDF OCR E2E tests on Linux
    >
    > To have E2E tests for PDF OCR on Windows, macOS, and Linux, we need to
    > be able to download and use the screen-ai library in those tests.
    > Pursuing this goal, we upload the screen-ai library for Linux to CIPD
    > and download it from CIPD to //third_party/screen-ai. This downloaded
    > library is then loaded in E2E tests (i.e. browser tests).
    >
    > For more information, visit go/chrome_screen_ai_tests and read the
    > Uploading the library files to CIPD section.
    >
    > AX-Relnotes: n/a.
    > Bug: 1443346
    > Change-Id: I7d55f9ae970a8e39aafbc4273fae2d654b4af17e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4803348
    > Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1240288}
    
    Bug: 1443346
    Change-Id: If2f6d956e634872f9e0816dfd8c1c9bbcbda9e16
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148588
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Owners-Override: Taiyo Mizuhashi <taiyo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1240444}

--- chrome/browser/screen_ai/screen_ai_install_state.h
+++ chrome/browser/screen_ai/screen_ai_install_state.h
@@ -103,7 +103,6 @@ class ScreenAIInstallState {
   State get_state() { return state_; }
 
   void ResetForTesting();
-  void SetComponentFolderForTesting();
   void SetStateForTesting(State state);
 
  private:
