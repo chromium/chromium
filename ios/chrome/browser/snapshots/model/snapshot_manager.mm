a7a77903add76f057d79c002c663fd95b31a3c85
Asami Doi <asamidoi@chromium.org>
2023-12-22 10:34:21 +0000

    [ios] Move the logic to select API to SnapshotGenerator
    This CL moves the logic to select which APIs should be used for
    taking snapshot images from
    SnapshotTabHelper::UpdateSnapshotWithCallback to SnapshotGenerator.
    The callers of SnapshotGenerator don't have to take care of which
    APIs are used.
    
    Bug: 1485474
    Change-Id: I26698acc9c33cd4e822cf47b7a227f5b70306db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5145109
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1240507}

--- ios/chrome/browser/snapshots/model/snapshot_manager.mm
+++ ios/chrome/browser/snapshots/model/snapshot_manager.mm
@@ -59,7 +59,7 @@
   }
 }
 
-- (void)updateWKWebViewSnapshotWithCompletion:(void (^)(UIImage*))completion {
+- (void)updateSnapshotWithCompletion:(void (^)(UIImage*))completion {
   DCHECK(_snapshotGenerator);
 
   __weak SnapshotManager* weakSelf = self;
@@ -72,23 +72,7 @@
       completion(image);
     }
   };
-  [_snapshotGenerator
-      generateWKWebViewSnapshotWithCompletion:wrappedCompletion];
-}
-
-- (void)updateUIViewSnapshotWithCompletion:(void (^)(UIImage*))completion {
-  DCHECK(_snapshotGenerator);
-  UIImage* image = [_snapshotGenerator generateUIViewSnapshotWithOverlays];
-
-  // Update the snapshot storage with the latest snapshot. The old image is
-  // deleted it if `image` is nil.
-  [self updateSnapshotStorageWithImage:image];
-
-  // Post a task to the current thread (UI thread).
-  if (completion) {
-    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-        FROM_HERE, base::BindOnce(completion, image));
-  }
+  [_snapshotGenerator generateSnapshotWithCompletion:wrappedCompletion];
 }
 
 - (UIImage*)generateUIViewSnapshot {
