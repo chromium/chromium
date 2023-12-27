329d1e0341fe1fa5c23de7ae8f25c643841dcae3
Joey Arhar <jarhar@chromium.org>
2023-12-27 22:07:19 +0000

    Revert "Autoselect in sub-popups open with LEFT/RIGHT keys only."
    This reverts commit 2a37071a1c0b0d8577dba1213e36b06692d85b08.
    
    Reason for revert: The new/modified test is failing on linux-chromeos-chrome: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/38141/overview
    I was able to reproduce the failure on my linux machine.
    
    Original change's description:
    > Autoselect in sub-popups open with LEFT/RIGHT keys only.
    >
    > Instead of autoselection by any keyboard key, which doesn't work
    > for navigating up/down in a parent popup with a sub-popup open.
    >
    > Bug: 1512898
    > Change-Id: I7e18229c95dac48dbfe8543f3323742cc154d173
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5136177
    > Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Reviewed-by: Bruno Braga <brunobraga@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1241188}
    
    Bug: 1512898
    Change-Id: Ia340e92bfac83c44cd9046fcd2ce551699b3d6f6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154190
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joey Arhar <jarhar@google.com>
    Reviewed-by: Joey Arhar <jarhar@google.com>
    Commit-Queue: Joey Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241249}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.h
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.h
@@ -111,10 +111,6 @@ class PopupViewViews : public PopupBaseView,
  private:
   friend class PopupViewViewsTestApi;
 
-  void SetSelectedCell(std::optional<CellIndex> cell_index,
-                       PopupCellSelectionSource source,
-                       AutoselectFirstSuggestion autoselect_first_suggestion);
-
   // Returns the `PopupRowView` at line number `index`. Assumes that there is
   // such a view at that line number - otherwise the underlying variant will
   // check false.
@@ -185,10 +181,8 @@ class PopupViewViews : public PopupBaseView,
 
   // Opens a sub-popup on a new row (and closes the open one if any), or just
   // closes the existing if `std::nullopt` is passed.
-  void SetRowWithOpenSubPopup(
-      std::optional<size_t> row_index,
-      AutoselectFirstSuggestion autoselect_first_suggestion =
-          AutoselectFirstSuggestion(false));
+  void SetRowWithOpenSubPopup(std::optional<size_t> row_index,
+                              PopupCellSelectionSource selection_source);
 
   // Controller for this view.
   base::WeakPtr<AutofillPopupController> controller_ = nullptr;
--- chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
@@ -945,28 +945,19 @@ TEST_F(PopupViewViewsTest, CellSubPopupResetAfterSuggestionsUpdates) {
       << "The cell's sub-popup should be closed.";
 }
 
-// `PopupViewViewsTest` is not used in death tests because it sets up a complex
-// environment (namely creates a `TestingProfile`) that fails to be created in
-// the sub-process (see `ASSERT_DEATH` doc for details). This fail hides
-// the real death reason to be tested.
-using PopupViewViewsDeathTest = ChromeViewsTestBase;
-TEST_F(PopupViewViewsDeathTest, OpenSubPopupWithNoChildrenCheckCrash) {
-  NiceMock<MockAutofillPopupController> controller;
-  controller.set_suggestions({
+TEST_F(PopupViewViewsTest, NoSubPopupOpenIfNotEligible) {
+  controller().set_suggestions({
       // Regular suggestion with no children,
       Suggestion(u"Suggestion #1"),
       Suggestion(u"Suggestion #2"),
   });
-  std::unique_ptr<views::Widget> widget = CreateTestWidget();
-  std::unique_ptr<PopupViewViews> view =
-      std::make_unique<PopupViewViews>(controller.GetWeakPtr());
-  raw_ptr<PopupViewViews> view_ptr = widget->SetContentsView(std::move(view));
-  view_ptr->Show(AutoselectFirstSuggestion(false));
+  CreateAndShowView();
 
-  ASSERT_DEATH(
-      view_ptr->SetSelectedCell(CellIndex{0, CellType::kControl},
-                                PopupCellSelectionSource::kNonUserInput),
-      "can_open_sub_popup");
+  view().SetSelectedCell(CellIndex{0, CellType::kControl},
+                         PopupCellSelectionSource::kNonUserInput);
+  task_environment()->FastForwardBy(PopupViewViews::kNonMouseOpenSubPopupDelay);
+  EXPECT_EQ(test_api(view()).GetOpenSubPopupRow(), std::nullopt)
+      << "Opening a sub-popup should happen.";
 }
 
 TEST_F(PopupViewViewsTest, SubPopupHidingOnNoSelection) {
@@ -1048,34 +1039,6 @@ TEST_F(PopupViewViewsTest, SubPopupOwnSelectionPreventsHiding) {
 
   EXPECT_NE(test_api(view()).GetOpenSubPopupRow(), std::nullopt);
   EXPECT_EQ(test_api(*sub_view).GetOpenSubPopupRow(), std::nullopt);
-}
-
-TEST_F(PopupViewViewsTest, SubPopupOpensWithNoAutoselectByMouse) {
-  controller().set_suggestions({
-      CreateSuggestionWithChildren({Suggestion(u"Child #1")}),
-  });
-  CreateAndShowView();
-
-  EXPECT_CALL(controller(),
-              OpenSubPopup(_, _, AutoselectFirstSuggestion(false)));
-
-  view().SetSelectedCell(CellIndex{0, CellType::kControl},
-                         PopupCellSelectionSource::kMouse);
-  task_environment()->FastForwardBy(PopupViewViews::kMouseOpenSubPopupDelay);
-}
-
-TEST_F(PopupViewViewsTest, SubPopupOpensWithAutoselectByRightKey) {
-  controller().set_suggestions({
-      CreateSuggestionWithChildren({Suggestion(u"Child #1")}),
-  });
-  CreateAndShowView();
-
-  EXPECT_CALL(controller(),
-              OpenSubPopup(_, _, AutoselectFirstSuggestion(true)));
-
-  SimulateKeyPress(ui::VKEY_DOWN);
-  SimulateKeyPress(ui::VKEY_RIGHT);
-  task_environment()->FastForwardBy(PopupViewViews::kNonMouseOpenSubPopupDelay);
 }
 
 // TODO(crbug.com/1489673): Enable once the view shows itself properly.
