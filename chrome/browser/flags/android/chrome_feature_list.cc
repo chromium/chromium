141556d3b0e25b8357e9a26aea63457835776c95
Donn Denman <donnd@chromium.org>
2023-12-28 19:40:21 +0000

    [Homepage] Default-disable Histograms flag->Delete
    This CL default-disables the Homepage Histograms UMA flag
    in preparation for removal of the flag and associated code.
    
    BUG=1433079
    
    Change-Id: Ib1cb7b196464f0e73e43a68af4b5c7bc9fdc96fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154340
    Commit-Queue: Geoff Huang <geoffhuang@google.com>
    Reviewed-by: Geoff Huang <geoffhuang@google.com>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241390}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -688,7 +688,7 @@ BASE_FEATURE(kOmniboxNoopEditUrlSuggestionClicks,
 
 BASE_FEATURE(kPartnerCustomizationsUma,
              "PartnerCustomizationsUma",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kQuickDeleteForAndroid,
              "QuickDeleteForAndroid",
