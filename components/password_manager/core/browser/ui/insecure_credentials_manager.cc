8ed921c358818c1381a7496b2b1dfdbb293ecbd4
Viktor Semeniuk <vsemeniuk@google.com>
2023-12-28 14:32:35 +0000

    [Password Manager] Avoid calling IsWeak on UI thread.
    Fixed: 1511231
    Change-Id: Idfc79870b2cadf3d4e05f55144e180580da9ba76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5150733
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241353}

--- components/password_manager/core/browser/ui/insecure_credentials_manager.cc
+++ components/password_manager/core/browser/ui/insecure_credentials_manager.cc
@@ -49,13 +49,19 @@ base::flat_set<std::u16string> ExtractPasswords(
                                            &CredentialUIEntry::password);
 }
 
-bool ChangesRequireRerunningCheck(const PasswordStoreChangeList& changes) {
+bool ChangesRequireRerunningReuseCheck(const PasswordStoreChangeList& changes) {
   return base::ranges::any_of(changes, [](const auto& change) {
     return change.type() == PasswordStoreChange::ADD ||
            change.type() == PasswordStoreChange::REMOVE ||
            (change.type() == PasswordStoreChange::UPDATE &&
             change.password_changed());
   });
+}
+
+bool ChangeRequiresRerunningWeakCheck(const PasswordStoreChange& change) {
+  return change.type() == PasswordStoreChange::ADD ||
+         (change.type() == PasswordStoreChange::UPDATE &&
+          change.password_changed());
 }
 
 #endif  // !BUILDFLAG(IS_ANDROID)
@@ -206,23 +212,37 @@ void InsecureCredentialsManager::OnWeakCheckDone(
   NotifyInsecureCredentialsChanged();
 }
 
+void InsecureCredentialsManager::OnPartialWeakCheckDone(
+    base::flat_set<std::u16string> weak_passwords) {
+  if (weak_passwords.empty()) {
+    return;
+  }
+
+  weak_passwords_.insert(weak_passwords.begin(), weak_passwords.end());
+  NotifyInsecureCredentialsChanged();
+}
+
 // Re-computes the list of insecure credentials with passwords after obtaining a
 // new list of saved passwords.
 void InsecureCredentialsManager::OnSavedPasswordsChanged(
     const PasswordStoreChangeList& changes) {
   // Disable on Android  to avoid pulling in a big dependency on zxcvbn.
 #if !BUILDFLAG(IS_ANDROID)
+  base::flat_set<std::u16string> passwords_to_recheck;
   for (const auto& change : changes) {
-    if (change.type() == PasswordStoreChange::ADD ||
-        (change.type() == PasswordStoreChange::UPDATE &&
-         change.password_changed())) {
-      const std::u16string& password = change.form().password_value;
-      if (!weak_passwords_.contains(password) && IsWeak(password)) {
-        weak_passwords_.insert(password);
-      }
+    if (ChangeRequiresRerunningWeakCheck(change)) {
+      passwords_to_recheck.insert(change.form().password_value);
     }
   }
-  if (ChangesRequireRerunningCheck(changes)) {
+  if (!passwords_to_recheck.empty()) {
+    base::ThreadPool::PostTaskAndReplyWithResult(
+        FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
+        base::BindOnce(&BulkWeakCheck, std::move(passwords_to_recheck)),
+        base::BindOnce(&InsecureCredentialsManager::OnPartialWeakCheckDone,
+                       weak_ptr_factory_.GetWeakPtr()));
+  }
+
+  if (ChangesRequireRerunningReuseCheck(changes)) {
     // Re-run reused check since user might have changed reused password. Don't
     // notify observers yet, as they'll be notified on OnReuseCheckDone()
     // anyway.
