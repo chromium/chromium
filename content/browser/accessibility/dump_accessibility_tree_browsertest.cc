4b4b91cc7b485b11f39ef0f53a02292f2c33f28e
Joanmarie Diggs <jdiggs@igalia.com>
2023-12-30 15:03:22 +0000

    [A11y] Re-enable AccessibilityCSSDisplayContents on mac
    It isn't flaking on the try jobs, and work to improve flakes has
    occurred. Re-enable this test to determine if any flakiness remains.
    
    Bug: 1367886
    Change-Id: I8e29161a1ae9d4a5a3b6426ac5e1f32d49ec1b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5150469
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1241604}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -252,16 +252,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
   RunCSSTest(FILE_PATH_LITERAL("display-contents.html"));
 }
 
-// TODO(crbug.com/1367886): De-flake and reenable.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_AccessibilityCSSDisplayContents \
-  DISABLED_AccessibilityCSSDisplayContents
-#else
-#define MAYBE_AccessibilityCSSDisplayContents \
-  AccessibilityCSSDisplayContents
-#endif
 IN_PROC_BROWSER_TEST_P(YieldingParserDumpAccessibilityTreeTest,
-                       MAYBE_AccessibilityCSSDisplayContents) {
+                       AccessibilityCSSDisplayContents) {
   RunCSSTest(FILE_PATH_LITERAL("display-contents.html"));
 }
 
