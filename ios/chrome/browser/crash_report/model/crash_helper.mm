81047fe91b262f655cc89467d4af01905218d5ad
Justin Cohen <justincohen@google.com>
2024-01-02 19:15:32 +0000

    ios: Add kill switch for disabling crash uploads.
    Fixed: b/317214674
    Change-Id: Ifc1d08a647bd05894ec8d1c87faa5eb4a55387cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5146327
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242071}

--- ios/chrome/browser/crash_report/model/crash_helper.mm
+++ ios/chrome/browser/crash_report/model/crash_helper.mm
@@ -44,6 +44,15 @@ namespace crash_helper {
 
 namespace {
 
+// Disable all crash uploading (including during safe mode) if the
+// kIOSCrashUploadKillSwitch is enabled. By revoking upload consent Crashpad
+// will mark any pending reports as skipped. By disabling UserEnabledUploading
+// safe mode crashes will be ignored. This also disables the main thread freeze
+// detector.
+BASE_FEATURE(kIOSCrashUploadKillSwitch,
+             "IOSCrashUploadKillSwitch",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 const char kUptimeAtRestoreInMs[] = "uptime_at_restore_in_ms";
 const char kUploadedInRecoveryMode[] = "uploaded_in_recovery_mode";
 
@@ -163,6 +172,9 @@ void Start() {
 }
 
 void SetEnabled(bool enabled) {
+  if (base::FeatureList::IsEnabled(kIOSCrashUploadKillSwitch)) {
+    enabled = false;
+  }
   // Caches the uploading flag in NSUserDefaults, so that we can access the
   // value immediately on startup, such as in safe mode or extensions.
   crash_helper::common::SetUserEnabledUploading(enabled);
