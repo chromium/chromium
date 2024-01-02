8109e70b8c8fb100d4194e69b71648ccb0615f11
Jeffrey Young <cowmoo@google.com>
2024-01-02 16:42:07 +0000

    p13n: wallpaper_grid_item_element check icon fix
    When wallpaper_grid_item_element was moved to
    //ash/webui/common, the new personalization-shared icons
    were not yet ready. Update to new circle-checkmark icon
    definition.
    
    Bug: b:312208348
    Change-Id: I85fd116a03fc3de08b6a61bf2278485141c38a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154355
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241962}

--- ash/webui/common/resources/wallpaper_grid_item_element.ts
+++ ash/webui/common/resources/wallpaper_grid_item_element.ts
@@ -6,6 +6,7 @@
  * @fileoverview Polymer element that displays a single grid item.
  */
 
+import 'chrome://resources/ash/common/personalization_shared_icons.html.js';
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
