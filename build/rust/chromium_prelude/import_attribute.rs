52ee534619c5389ecfcb93bfb5513bf176b42dc4
Lukasz Anforowicz <lukasza@chromium.org>
2024-01-02 22:25:35 +0000

    Ensure that `chromium::import!` escaping uses an *injective* function.
    The main change in this CL is ensuring that the escaping mechanism used
    by the `chromium::import!` is "injective" (see
    https://en.wikipedia.org/wiki/Injective_function). Before this CL,
    different inputs could be mapped to the same output (i.e. the function
    wasn't injective):
    
    * //foo:bar_baz => foo_bar_baz
    * //foo/bar:baz => foo_bar_baz
    * //foo_bar:baz => foo_bar_baz
    
    After this CL, each of the special characters (and the `_` escape
    character itself) is mapped to a unique sequence of characters that
    begins with the `_` escape character: `_` => `_u`, `/` => `_s`, `:` =>
    `_c`, etc.:
    
    * //foo:bar_baz => foo_cbar_ubaz
    * //foo/bar:baz => foo_sbar_cbaz
    * //foo_bar:baz => foo_ubar_cbaz
    
    Other minor changes:
    
    * Added support for escaping the `-` character (found for example in the
      `//third_party/blink/renderer/platform/image-decoders` path)
    * Ensured that all escaping implementations are in-sync (before this CL
      `rust_static_library.gni` wouldn't take `invoker.crate_name` into
      account).  (Unfortunately some duplication remains and so there is
      still some risk that things will get out of sync in the future.
      Fixing this is hard without an ability to define reusable functions in
      `.gni` files, similarly to how Bazel's Skylark allows this.)
    * Removed `crate_name` from
      `//build/rust/chromium_prelude:import_test_lib` and added a comment
      to `rust_static_library.gni` to discourage using this property.
    * Using `get_label_info(..., 'dir')` instead of inferring this
      information via `target_out_dir` and hardcoded knowledge about `/obj`
      subdirectory.
    * Gracefully returning an error if the GN target name is not a valid
      Rust identifier (e.g. the following target has a dot character inside:
      `//chrome/test/chromedriver:chromedriver_server.unstripped`)
    * Reverting https://crrev.com/c/4185300 under
      `//build/rust/tests/test_aliased_deps` and *not* using the
      `chromium::import!` macro in this test, because `aliased_deps` is
      agnostic to `chromium::import!` and its mangling algorithm.
    
    Bug: 1512174
    Change-Id: I57da7ac077e82b50c6b57e25a489bc817236ed9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5132119
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242165}

--- build/rust/chromium_prelude/import_attribute.rs
+++ build/rust/chromium_prelude/import_attribute.rs
@@ -5,7 +5,7 @@
 use proc_macro2::Span;
 use quote::quote;
 use syn::parse::{Parse, ParseStream};
-use syn::{parse_macro_input, Error, Ident, Lit, Result, Token};
+use syn::{parse_macro_input, Error, Ident, Lit, Token};
 
 #[proc_macro]
 pub fn import(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -39,14 +39,14 @@ struct GnTarget {
 }
 
 impl GnTarget {
-    fn parse(s: &str, span: Span) -> std::result::Result<GnTarget, String> {
+    fn parse(s: &str, span: Span) -> Result<GnTarget, String> {
         if !s.starts_with("//") {
             return Err(String::from("expected absolute GN path (should start with //)"));
         }
 
         let mut path: Vec<&str> = s[2..].split('/').collect();
 
-        let (mangled_crate_name, gn_name) = {
+        let gn_name = {
             if path[0..2] == ["third_party", "rust"] {
                 return Err(String::from(
                     "import! macro should not be used for third_party crates",
@@ -60,40 +60,30 @@ impl GnTarget {
             };
             path.push(split_last);
 
-            for p in &path {
-                if p.contains(':') {
-                    return Err(String::from("unexpected ':' in GN path component"));
-                }
-                if p.is_empty() {
-                    return Err(String::from("unexpected empty GN path component"));
-                }
-                // Valid GN indentifiers:
-                // https://gn.googlesource.com/gn/+/main/docs/reference.md#identifiers
-                //
-                // We assume all path components are also valid GN identifiers, which
-                // we believe they are in Chromium. This could be loosened if needed.
-                if !p.chars().all(|c| c.is_ascii_alphanumeric() || c == '_') {
-                    return Err(String::from(
-                        "unexpected character in GN path component, \
-                         expected characters in [A-Za-z0-9_]",
-                    ));
-                }
-            }
-
-            let mangled_crate_name: Vec<&str> =
-                path.into_iter().chain(std::iter::once(gn_name)).collect();
-            (mangled_crate_name.join("_"), gn_name)
+            gn_name
         };
+
+        for p in &path {
+            if p.contains(':') {
+                return Err(String::from("unexpected ':' in GN path component"));
+            }
+            if p.is_empty() {
+                return Err(String::from("unexpected empty GN path component"));
+            }
+        }
+
+        let mangled_crate_name =
+            escape_non_identifier_chars(&format!("{}:{gn_name}", path.join("/")))?;
 
         Ok(GnTarget {
             mangled_crate_name: Ident::new(&mangled_crate_name, span),
-            gn_name: Ident::new(gn_name, span),
+            gn_name: syn::parse_str::<Ident>(gn_name).map_err(|e| format!("{e}"))?,
         })
     }
 }
 
 impl Parse for ImportList {
-    fn parse(input: ParseStream) -> Result<Self> {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut imports: Vec<Import> = Vec::new();
 
         while !input.is_empty() {
@@ -135,4 +125,48 @@ impl Parse for ImportList {
 
         Ok(Self { imports })
     }
+}
+
+/// Escapes non-identifier characters in `symbol`.
+///
+/// Importantly, this is
+/// [an injective function](https://en.wikipedia.org/wiki/Injective_function)
+/// which means that different inputs are never mapped to the same output.
+///
+/// This is based on a similar function in
+/// https://github.com/google/crubit/blob/22ab04aef9f7cc56d8600c310c7fe20999ffc41b/common/code_gen_utils.rs#L59-L71
+/// The main differences are:
+///
+/// * Only a limited set of special characters is supported, because this makes
+///   it easier to replicate the escaping algorithm in `.gni` files, using just
+///   `string_replace` calls.
+/// * No dependency on `unicode_ident` crate means that instead of
+///   `is_xid_continue` a more restricted call to `char::is_ascii_alphanumeric`
+///   is used.
+/// * No support for escaping leading digits.
+/// * The escapes are slightly different (e.g. `/` frequently appears in GN
+///   paths and therefore here we map it to a nice `_s` rather than to `_x002f`)
+fn escape_non_identifier_chars(symbol: &str) -> Result<String, String> {
+    assert!(!symbol.is_empty()); // Caller is expected to verify.
+    if symbol.chars().next().unwrap().is_ascii_digit() {
+        return Err("Leading digits are not supported".to_string());
+    }
+
+    // Escaping every character can at most double the size of the string.
+    let mut result = String::with_capacity(symbol.len() * 2);
+    for c in symbol.chars() {
+        // NOTE: TargetName=>CrateName mangling algorithm should be updated
+        // simultaneously in 3 places: here, //build/rust/rust_target.gni,
+        // //build/rust/rust_static_library.gni.
+        match c {
+            '_' => result.push_str("_u"),
+            '/' => result.push_str("_s"),
+            ':' => result.push_str("_c"),
+            '-' => result.push_str("_d"),
+            c if c.is_ascii_alphanumeric() => result.push(c),
+            _ => return Err(format!("Unsupported character in GN path component: `{c}`")),
+        }
+    }
+
+    Ok(result)
 }
