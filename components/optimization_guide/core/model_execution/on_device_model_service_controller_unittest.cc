03c7a3c293a558f2f98630a7011f14c775c18c82
Scott Violet <sky@chromium.org>
2024-01-02 15:12:07 +0000

    on-device: extends result histogram to include unsafe output
    This patch adds whether the output is considered unsafe to
    OptimizationGuideOnDeviceExecuteModelResult.
    
    (cherry picked from commit 1da9da1d554756335a337ce0fcc65d8cfcd26499)
    
    Bug: b:315807854
    Change-Id: I06c439668b6f3435326a39234ba2ecb541457a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5120130
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1237187}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154253
    Owners-Override: Daniel Yip <danielyip@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Auto-Submit: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/branch-heads/6167@{#928}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller_unittest.cc
@@ -35,6 +35,11 @@ using ExecuteModelResult = SessionImpl::ExecuteModelResult;
 namespace {
 // If non-zero this amount of delay is added before the response is sent.
 base::TimeDelta g_execute_delay = base::TimeDelta();
+
+// Status code supplied to OnComplete().
+on_device_model::mojom::ResponseStatus g_on_complete_response_type =
+    on_device_model::mojom::ResponseStatus::kOk;
+
 }  // namespace
 
 std::vector<std::string> ConcatResponses(
@@ -89,7 +94,7 @@ class FakeOnDeviceSession : public base::SupportsWeakPtr<FakeOnDeviceSession>,
       remote->OnResponse("Context: " + context + "\n");
     }
     remote->OnResponse("Input: " + input->text + "\n");
-    remote->OnComplete(on_device_model::mojom::ResponseStatus::kOk);
+    remote->OnComplete(g_on_complete_response_type);
   }
 
   void AddContextInternal(
@@ -216,6 +221,7 @@ class OnDeviceModelServiceControllerTest : public testing::Test {
  public:
   void SetUp() override {
     g_execute_delay = base::TimeDelta();
+    g_on_complete_response_type = on_device_model::mojom::ResponseStatus::kOk;
     feature_list_.InitAndEnableFeatureWithParameters(
         features::kOptimizationGuideOnDeviceModel,
         {{"on_device_model_min_tokens_for_context", "10"},
@@ -982,6 +988,20 @@ TEST_F(OnDeviceModelServiceControllerTest, UseServerWithRepeatedDelays) {
   // next session should use the server.
   EXPECT_EQ(nullptr, test_controller_->CreateSession(
                          kFeature, base::DoNothing(), &logger_));
+}
+
+TEST_F(OnDeviceModelServiceControllerTest, UsedOnDeviceOutputUnsafe) {
+  g_on_complete_response_type =
+      on_device_model::mojom::ResponseStatus::kRetracted;
+  auto session =
+      test_controller_->CreateSession(kFeature, base::DoNothing(), &logger_);
+  ASSERT_TRUE(session);
+  ExecuteModel(*session, "foo");
+  base::HistogramTester histogram_tester;
+  task_environment_.RunUntilIdle();
+  histogram_tester.ExpectUniqueSample(
+      "OptimizationGuide.ModelExecution.OnDeviceExecuteModelResult.Compose",
+      ExecuteModelResult::kUsedOnDeviceOutputUnsafe, 1);
 }
 
 }  // namespace optimization_guide
