942a3bf2d0ba6f5afaefb512fdd00be4542951ee
Negeen Daudi <negeend@google.com>
2024-01-02 00:08:59 +0000

    Fix GetWordSimilarity to use cosine similarity.
    Test: out/Default/unit_tests --gtest_filter="*ManateeTest*"
    Bug: b/314868265
    Change-Id: Ib797137e3debd7e79ef995bd6f50729cb005f190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5150539
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: Negeen Daudi <negeend@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241786}

--- chrome/browser/ash/app_list/search/util/manatee.cc
+++ chrome/browser/ash/app_list/search/util/manatee.cc
@@ -15,7 +15,20 @@ std::optional<double> GetWordSimilarity(const std::vector<double>& vector1,
   if (vector1.size() != vector2.size()) {
     return std::nullopt;
   }
-  return std::inner_product(vector1.begin(), vector1.end(), vector2.begin(), 0);
+  // Calculating cosine similarity.
+  double inner_prod =
+      std::inner_product(vector1.begin(), vector1.end(), vector2.begin(), 0);
+
+  double magnitude1 = std::sqrt(
+      std::inner_product(vector1.begin(), vector1.end(), vector1.begin(), 0));
+  double magnitude2 = std::sqrt(
+      std::inner_product(vector2.begin(), vector2.end(), vector2.begin(), 0));
+
+  if (magnitude1 == 0 || magnitude2 == 0) {
+    return 0.0;
+  }
+
+  return inner_prod / (magnitude1 * magnitude2);
 }
 
 }  // namespace app_list
--- chrome/browser/ash/app_list/search/util/manatee_unittest.cc
+++ chrome/browser/ash/app_list/search/util/manatee_unittest.cc
@@ -11,30 +11,26 @@
 
 namespace app_list::test {
 
+// Test for successful cosine similarity calculation of two positive input
+// vectors.
 TEST(ManateeTest, GetWordSimilarityPositiveVectors) {
-  // Test for successful dot product calculation of two
-  // positive input vectors
-
   std::vector<double> v1{1.0, 2.0, 3.0};
   std::vector<double> v2{4.0, 5.0, 6.0};
   absl::optional<double> result = GetWordSimilarity(v1, v2);
   ASSERT_TRUE(result.has_value());
-  EXPECT_DOUBLE_EQ(32.0, result.value());
+  EXPECT_NEAR(0.974632, result.value(), 1e-5);
 }
 
+// Test for correct handling of input vectors with mismatching size.
 TEST(ManateeTest, GetWordSimilarityMismatchingSize) {
-  // Test for correct handling of input vectors
-  // with mismatching size
-
   std::vector<double> v1{1.0, 2.0, 3.0};
   std::vector<double> v2{4.0, 5.0, 6.0, 7.0};
   absl::optional<double> result = GetWordSimilarity(v1, v2);
   ASSERT_FALSE(result.has_value());
 }
 
+// Test for correct handling of empty input vectors.
 TEST(ManateeTest, GetWordSimilarityEmptyVectors) {
-  // Test for correct handling of empty input vectors
-
   std::vector<double> v1{};
   std::vector<double> v2{};
   absl::optional<double> result = GetWordSimilarity(v1, v2);
@@ -42,10 +38,8 @@ TEST(ManateeTest, GetWordSimilarityEmptyVectors) {
   EXPECT_DOUBLE_EQ(0.0, result.value());
 }
 
+// Test for correct cosine similarity with a vector of all zero values.
 TEST(ManateeTest, GetWordSimilarityZeroVectors) {
-  // Test for correct dot product with a vector of
-  // all zero values
-
   std::vector<double> v1{1.0, 2.0, 3.0};
   std::vector<double> v2{0.0, 0.0, 0.0};
   absl::optional<double> result = GetWordSimilarity(v1, v2);
