c71abe495cdabdd93a760cd7d40f245e7b1d31f3
Peter Marshall <petermarshall@chromium.org>
2024-01-02 02:08:55 +0000

    [Office upload] Remove dots (again)
    Remove the unused code for displaying progress dots.
    
    This time, make sure the buttons don't move.
    
    Bug: none
    Change-Id: I21a42776c7e4f7e48baa9b67b5ef6239900544eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5143368
    Auto-Submit: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1241800}

--- chrome/browser/resources/chromeos/cloud_upload/base_setup_page.ts
+++ chrome/browser/resources/chromeos/cloud_upload/base_setup_page.ts
@@ -9,8 +9,6 @@
  * to switch pages or exit.
  */
 
-import {assert} from 'chrome://resources/js/assert.js';
-
 import {getTemplate} from './base_setup_page.html.js';
 
 /**
@@ -35,10 +33,6 @@ declare global {
  * layout via a common shadow DOM.
  */
 export class BaseSetupPageElement extends HTMLElement {
-  static get observedAttributes() {
-    return ['page-number', 'total-pages'];
-  }
-
   constructor() {
     super();
 
@@ -60,25 +54,6 @@ export class BaseSetupPageElement extends HTMLElement {
         {passive: true});
     // Focus the dialog so that the screen reader reads the title.
     this.shadowRoot!.querySelector<HTMLElement>('#dialog')!.focus();
-  }
-
-  attributeChangedCallback(name: string, _oldValue: string, _newValue: string) {
-    assert(name === 'page-number' || name === 'total-pages');
-
-    const dotsElement =
-        this.shadowRoot?.querySelector('#dots') as HTMLDivElement;
-    assert(window.trustedTypes);
-    dotsElement.innerHTML = window.trustedTypes.emptyHTML;
-    const pages = parseInt(this.getAttribute('total-pages')!) || 0;
-    const curPage = parseInt(this.getAttribute('page-number')!) || 0;
-
-    for (let i = 0; i < pages; i++) {
-      const dot = document.createElement('div');
-      if (i === curPage) {
-        dot.classList.add('active');
-      }
-      dotsElement.appendChild(dot);
-    }
   }
 
   updateContentFade(contentElement: HTMLElement) {
