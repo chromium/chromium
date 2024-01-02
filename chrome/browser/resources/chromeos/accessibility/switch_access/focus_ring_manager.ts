09608d7aaef78fd5c4042890acf55e6113360d67
Anastasia Helfinstein <anastasi@google.com>
2024-01-02 17:27:08 +0000

    [Switch Access] Convert FocusRingManager to TypeScript
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: Ic282b168e3fd08145b2d7ac50dded757d5e76c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5154072
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241991}

--- chrome/browser/resources/chromeos/accessibility/switch_access/focus_ring_manager.ts
+++ chrome/browser/resources/chromeos/accessibility/switch_access/focus_ring_manager.ts
@@ -5,43 +5,41 @@
 import {RectUtil} from '../common/rect_util.js';
 
 import {MenuManager} from './menu_manager.js';
-import {SAChildNode, SANode, SARootNode} from './nodes/switch_access_node.js';
+import {SAChildNode, SANode} from './nodes/switch_access_node.js';
 import {SwitchAccess} from './switch_access.js';
 import {ErrorType, Mode} from './switch_access_constants.js';
 
-const FocusRingInfo = chrome.accessibilityPrivate.FocusRingInfo;
-const FocusType = chrome.accessibilityPrivate.FocusType;
+import FocusRingInfo = chrome.accessibilityPrivate.FocusRingInfo;
+import FocusType = chrome.accessibilityPrivate.FocusType;
+import ScreenRect = chrome.accessibilityPrivate.ScreenRect;
 
+type Observer = (primary: SANode | null, preview: SANode | null) => void;
 
 /** Class to handle focus rings. */
 export class FocusRingManager {
-  /** @private */
-  constructor() {
-    /**
-     * A map of all the focus rings.
-     * @private {!Object<RingId, FocusRingInfo>}
-     */
-    this.rings_ = this.createRings_();
+  private observer_?: Observer;
+  /** A map of all the focus rings. */
+  private rings_: Record<RingId, FocusRingInfo>;
+  private ringNodesForTesting_: Record<RingId, SANode | null> = {
+    [RingId.PRIMARY]: null,
+    [RingId.PREVIEW]: null,
+  };
 
-    /** @private {!Object<RingId, ?SANode>} */
-    this.ringNodesForTesting_ = {
-      [RingId.PRIMARY]: null,
-      [RingId.PREVIEW]: null,
-    };
+  private static instance_?: FocusRingManager;
+
+  private constructor() {
+    this.rings_ = this.createRings_();
   }
 
-  static get instance() {
+  static get instance(): FocusRingManager {
     if (!FocusRingManager.instance_) {
       FocusRingManager.instance_ = new FocusRingManager();
     }
     return FocusRingManager.instance_;
   }
 
-  /**
-   * Sets the focus ring color.
-   * @param {!string} color
-   */
-  static setColor(color) {
+  /** Sets the focus ring color. */
+  static setColor(color: string): void {
     if (!COLOR_PATTERN.test(color)) {
       console.error(SwitchAccess.error(
           ErrorType.INVALID_COLOR,
@@ -52,11 +50,8 @@ export class FocusRingManager {
     FocusRingManager.instance.setColorValidated_(color);
   }
 
-  /**
-   * Sets the primary and preview focus rings based on the provided node.
-   * @param {!SAChildNode} node
-   */
-  static setFocusedNode(node) {
+  /** Sets the primary and preview focus rings based on the provided node. */
+  static setFocusedNode(node: SAChildNode): void {
     if (node.ignoreWhenComputingUnionOfBoundingBoxes()) {
       FocusRingManager.instance.setFocusedNodeIgnorePrimary_(node);
       return;
@@ -81,7 +76,7 @@ export class FocusRingManager {
   }
 
   /** Clears all focus rings. */
-  static clearAll() {
+  static clearAll(): void {
     FocusRingManager.instance.clearAll_();
   }
 
@@ -90,27 +85,21 @@ export class FocusRingManager {
    * are updated. It will be called with two arguments: the node for
    * the primary ring, and the node for the preview ring. Either may
    * be null.
-   * @param {function(?SANode, ?SANode)} observer
    */
-  static setObserver(observer) {
+  static setObserver(observer: Observer): void {
     FocusRingManager.instance.observer_ = observer;
   }
 
   // ======== Private methods ========
 
-  /** @private */
-  clearAll_() {
+  private clearAll_(): void {
     this.forEachRing_(ring => ring.rects = []);
     this.updateNodesForTesting_(null, null);
     this.updateFocusRings_();
   }
 
-  /**
-   * Creates the map of focus rings.
-   * @return {!Object<RingId, FocusRingInfo>}
-   * @private
-   */
-  createRings_() {
+  /** Creates the map of focus rings. */
+  private createRings_(): Record<RingId, FocusRingInfo> {
     const primaryRing = {
       id: RingId.PRIMARY,
       rects: [],
@@ -133,44 +122,34 @@ export class FocusRingManager {
     };
   }
 
-  /**
-   * Calls a function for each focus ring.
-   * @param {!function(!FocusRingInfo)} callback
-   * @private
-   */
-  forEachRing_(callback) {
+  /** Calls a function for each focus ring. */
+  private forEachRing_(callback: (info: FocusRingInfo) => void): void {
     Object.values(this.rings_).forEach(ring => callback(ring));
   }
 
-  /**
-   * Sets the focus ring color. Assumes the color has already been validated.
-   * @param {!string} color
-   * @private
-   */
-  setColorValidated_(color) {
+  /** Sets the focus ring color. Assumes the color has been validated. */
+  private setColorValidated_(color: string): void {
     this.forEachRing_(ring => ring.color = color);
   }
 
   /**
    * Sets the primary focus ring to |node|, and the preview focus ring to
    * |firstChild|.
-   * @param {!SAChildNode} group
-   * @param {!SAChildNode} firstChild
-   * @private
    */
-  setFocusedNodeGroup_(group, firstChild) {
+  private setFocusedNodeGroup_(
+      group: SAChildNode, firstChild: SAChildNode): void {
     // Clear the dashed ring between transitions, as the animation is
     // distracting.
     this.rings_[RingId.PREVIEW].rects = [];
 
-    let focusRect = group.location;
+    let focusRect: ScreenRect = group.location!;
     const childRect = firstChild ? firstChild.location : null;
     if (childRect) {
       // If the current element is not specialized in location handling, e.g.
       // the back button, the focus rect should expand to contain the child
       // rect.
       focusRect =
-          RectUtil.expandToFitWithPadding(GROUP_BUFFER, focusRect, childRect);
+          RectUtil.expandToFitWithPadding(GROUP_BUFFER, focusRect, childRect)!;
       this.rings_[RingId.PREVIEW].rects = [childRect];
     }
     this.rings_[RingId.PRIMARY].rects = [focusRect];
@@ -180,11 +159,9 @@ export class FocusRingManager {
 
   /**
    * Clears the primary focus ring and sets the preview focus ring based on the
-   *     provided node.
-   * @param {!SAChildNode} node
-   * @private
+   * provided node.
    */
-  setFocusedNodeIgnorePrimary_(node) {
+  private setFocusedNodeIgnorePrimary_(node: SAChildNode): void {
     // Nodes of this type, e.g. the back button node, handles setting its own
     // focus, as it has special requirements (a round focus ring that has no
     // gap with the edges of the view).
@@ -203,13 +180,11 @@ export class FocusRingManager {
     this.updateFocusRings_();
   }
 
-  /**
-   * Sets the primary focus to |node| and clears the secondary focus.
-   * @param {!SAChildNode} node
-   * @private
-   */
-  setFocusedNodeLeaf_(node) {
-    this.rings_[RingId.PRIMARY].rects = [node.location];
+  /** Sets the primary focus to |node| and clears the secondary focus. */
+  private setFocusedNodeLeaf_(node: SAChildNode): void {
+    // TODO(b/314203187): Not nulls asserted, check these to make sure
+    // this is correct.
+    this.rings_[RingId.PRIMARY].rects = [node.location!];
     this.rings_[RingId.PREVIEW].rects = [];
     this.updateNodesForTesting_(node, null);
     this.updateFocusRings_();
@@ -218,9 +193,8 @@ export class FocusRingManager {
   /**
    * Updates all focus rings to reflect new location, color, style, or other
    * changes. Enables observers to monitor what's focused.
-   * @private
    */
-  updateFocusRings_() {
+  private updateFocusRings_(): void {
     if (SwitchAccess.mode === Mode.POINT_SCAN && !MenuManager.isMenuOpen()) {
       return;
     }
@@ -231,13 +205,9 @@ export class FocusRingManager {
         chrome.accessibilityPrivate.AssistiveTechnologyType.SWITCH_ACCESS);
   }
 
-  /**
-   * Saves the primary/preview focus for testing.
-   * @param {?SANode} primary
-   * @param {?SANode} preview
-   * @private
-   */
-  updateNodesForTesting_(primary, preview) {
+  /** Saves the primary/preview focus for testing. */
+  private updateNodesForTesting_(
+      primary: SANode | null, preview: SANode | null): void {
     // Keep track of the nodes associated with each focus ring for testing
     // purposes, since focus ring locations are not guaranteed to exactly match
     // node locations.
@@ -255,44 +225,32 @@ export class FocusRingManager {
  * Regex pattern to verify valid colors. Checks that the first character
  * is '#', followed by 3, 4, 6, or 8 valid hex characters, and no other
  * characters (ignoring case).
- * @const {RegExp}
  */
 const COLOR_PATTERN = /^#([0-9A-F]{3,4}|[0-9A-F]{6}|[0-9A-F]{8})$/i;
 
 /**
  * The buffer (in dip) between a child's focus ring and its parent's focus
  * ring.
- * @const {number}
  */
 const GROUP_BUFFER = 2;
 
 /**
  * The focus ring IDs used by Switch Access.
  * Exported for testing.
- * @enum {string}
  */
-export const RingId = {
+export enum RingId {
   // The ID for the ring showing the user's current focus.
-  PRIMARY: 'primary',
+  PRIMARY = 'primary',
   // The ID for the ring showing a preview of the next focus, if the user
   // selects the current element.
-  PREVIEW: 'preview',
-};
+  PREVIEW = 'preview',
+}
 
-/**
- * The secondary color for both rings.
- * @const {string|undefined}
- */
+/** The secondary color for both rings. */
 const OUTER_COLOR = '#174EA6';  // Google Blue 900
 
-/**
- * The inner color of the preview focus ring
- * @const {string}
- */
+/** The inner color of the preview focus ring. */
 const PREVIEW_COLOR = '#8AB4F880';  // Google Blue 300, 50% opacity
 
-/**
- * The inner color of the primary focus ring.
- * @const {string}
- */
+/** The inner color of the primary focus ring. */
 const PRIMARY_COLOR = '#8AB4F8';  // Google Blue 300
