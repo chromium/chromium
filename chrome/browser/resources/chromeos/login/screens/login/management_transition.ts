71148df8ea9ea60498b40d342d0040c11866672f
Simon Ziegltrum <ziegltrum@google.com>
2024-01-02 02:04:51 +0000

    [OOBE] management_transition TS migration
    Bug: b/314761941
    Change-Id: Ib5714959d25755fd2ca1d473e72d4c9cfc296614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149087
    Auto-Submit: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1241799}

---
+++ chrome/browser/resources/chromeos/login/screens/login/management_transition.ts
@@ -0,0 +1,188 @@
+// Copyright 2018 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Polymer element for displaying material design management
+ * transition screen.
+ */
+
+import '//resources/cr_elements/cr_shared_vars.css.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/paper-progress/paper-progress.js';
+import '../../components/buttons/oobe_text_button.js';
+import '../../components/common_styles/oobe_common_styles.css.js';
+import '../../components/common_styles/oobe_dialog_host_styles.css.js';
+import '../../components/dialogs/oobe_adaptive_dialog.js';
+
+import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
+import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.js';
+
+import {getTemplate} from './management_transition.html.js';
+
+
+enum ManagementTransitionUiState {
+  PROGRESS = 'progress',
+  ERROR = 'error',
+}
+
+/**
+ * Possible transition types. Must be in the same order as
+ * ArcSupervisionTransition enum values.
+ */
+enum ArcSupervisionTransition {
+  NO_TRANSITION = 0,
+  CHILD_TO_REGULAR = 1,
+  REGULAR_TO_CHILD = 2,
+  UNMANAGED_TO_MANAGED=  3,
+}
+
+const ManagementTransitionScreenBase = mixinBehaviors(
+    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior],
+        PolymerElement) as { new (): PolymerElement
+      & OobeI18nBehaviorInterface
+      & LoginScreenBehaviorInterface
+      & MultiStepBehaviorInterface,
+    };
+
+interface ManagementTransitionScreenData {
+  arcTransition: ArcSupervisionTransition;
+  managementEntity: string;
+}
+
+class ManagementTransitionScreen extends ManagementTransitionScreenBase {
+  static get is() {
+    return 'management-transition-element'as const;
+  }
+
+  static get template(): HTMLTemplateElement {
+    return getTemplate();
+  }
+
+  static get properties(): PolymerElementProperties {
+    return {
+      /**
+       * Property that determines transition direction.
+       */
+      arcTransition: {
+        type: Number,
+        value: ArcSupervisionTransition.NO_TRANSITION,
+      },
+
+      /**
+       * String that represents management entity for the user. Can be domain or
+       * admin name.
+       */
+      managementEntity: {
+        type: String,
+        value: '',
+      },
+    };
+  }
+
+  private arcTransition: number;
+  private managementEntity: string;
+
+  constructor() {
+    super();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override defaultUIStep(): string {
+    return ManagementTransitionUiState.PROGRESS;
+  }
+
+  override get UI_STEPS() {
+    return ManagementTransitionUiState;
+  }
+
+  override get EXTERNAL_API(): string[] {
+    return ['showStep'];
+  }
+
+  override ready() {
+    super.ready();
+    this.initializeLoginScreen('ManagementTransitionScreen');
+  }
+
+  onBeforeShow(data: ManagementTransitionScreenData): void {
+    this.setArcTransition(data['arcTransition']);
+    this.setManagementEntity(data['managementEntity']);
+  }
+
+  /**
+   * Switches between different steps.
+   * @param step the steps to show
+   */
+  private showStep(step: string): void {
+    this.setUIStep(step);
+  }
+
+  /**
+   * Sets arc transition type.
+   * @param arc_transition enum element indicating
+   *     transition type
+   */
+  private setArcTransition(arcTransition: ArcSupervisionTransition): void {
+    switch (arcTransition) {
+      case ArcSupervisionTransition.CHILD_TO_REGULAR:
+      case ArcSupervisionTransition.REGULAR_TO_CHILD:
+      case ArcSupervisionTransition.UNMANAGED_TO_MANAGED:
+        this.arcTransition = arcTransition;
+        break;
+      case ArcSupervisionTransition.NO_TRANSITION:
+        console.error(
+            'Screen should not appear for ' +
+            'ARC_SUPERIVISION_TRANSITION.NO_TRANSITION');
+        break;
+      default:
+        console.error('Not handled transition type: ' + arcTransition);
+    }
+  }
+
+  private setManagementEntity(managementEntity: string): void {
+    this.managementEntity = managementEntity;
+  }
+
+  private getDialogTitle(locale: string, arcTransition: number,
+      managementEntity: string): string {
+    switch (arcTransition) {
+      case ArcSupervisionTransition.CHILD_TO_REGULAR:
+        return this.i18nDynamic(locale, 'removingSupervisionTitle');
+      case ArcSupervisionTransition.REGULAR_TO_CHILD:
+        return this.i18nDynamic(locale, 'addingSupervisionTitle');
+      case ArcSupervisionTransition.UNMANAGED_TO_MANAGED:
+        if (managementEntity) {
+          return this.i18nDynamic(locale, 'addingManagementTitle',
+              managementEntity);
+        } else {
+          return this.i18nDynamic(locale, 'addingManagementTitleUnknownAdmin');
+        }
+    }
+    return '';
+  }
+
+  private isChildTransition(arcTransition: number): boolean {
+    return arcTransition != ArcSupervisionTransition.UNMANAGED_TO_MANAGED;
+  }
+
+  /**
+   * On-tap event handler for OK button.
+   */
+  private onAcceptAndContinue(): void {
+    this.userActed(['finish-management-transition']);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ManagementTransitionScreen.is]: ManagementTransitionScreen;
+  }
+}
+
+customElements.define(
+    ManagementTransitionScreen.is, ManagementTransitionScreen);
