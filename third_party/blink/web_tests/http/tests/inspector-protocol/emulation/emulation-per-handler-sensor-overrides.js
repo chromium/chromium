f6f0b1b1d5f6e146773965832118528a4265d233
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2024-01-03 10:13:46 +0000

    sensors: Remove some setFocusEmulationEnabled() calls in web test
    These calls are no longer needed now that https://crrev.com/c/5151324
    has fixed the underlying issue.
    
    Bug: 1492129
    Change-Id: Iafeb593e13fa5317690686f128a710de562db01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5163367
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Auto-Submit: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242347}

--- third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-per-handler-sensor-overrides.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-per-handler-sensor-overrides.js
@@ -63,8 +63,6 @@
   // created by other handlers.
   testRunner.log('\nDisconnecting |session3|');
   await session3.disconnect();
-  // TODO(crbug.com/1492129): This call can be removed once the bug is fixed.
-  await dp.Emulation.setFocusEmulationEnabled({enabled: true});
 
   await assertSensorIsOverridden(dp2, 'accelerometer');
   await assertSensorIsOverridden(dp2, 'gravity');
@@ -82,8 +80,6 @@
 
   testRunner.log('\nDisconnecting |session2|');
   await session2.disconnect();
-  // TODO(crbug.com/1492129): This call can be removed once the bug is fixed.
-  await dp.Emulation.setFocusEmulationEnabled({enabled: true});
 
   await assertSensorIsNotOverridden(dp2, 'accelerometer');
   await assertSensorIsNotOverridden(dp2, 'gravity');
@@ -98,8 +94,6 @@
 
   testRunner.log('\nDisconnection session');
   await session.disconnect();
-  // TODO(crbug.com/1492129): This call can be removed once the bug is fixed.
-  await dp.Emulation.setFocusEmulationEnabled({enabled: true});
 
   await assertSensorIsNotOverridden(dp, 'accelerometer');
   await assertSensorIsNotOverridden(dp2, 'gravity');
