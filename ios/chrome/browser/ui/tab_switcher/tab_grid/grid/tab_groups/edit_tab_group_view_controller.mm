d53fe006a67d90fd7b5ffb1dba93abaf0a6f8260
Aliona DANGLA <alionadangla@google.com>
2024-01-03 12:06:38 +0000

    [iOS] Edit tab group view user input view
    Add the tab group name input style which contains the colored dot
    (currently static but in a future will be the selected color), the
    background view of the title and the user input with the keyboard open.
    
    See video:
    https://drive.google.com/file/d/1eoVUMpN9FXD75hlC_itz6WgO5rISJjuN/view?usp=drive_link
    
    Bug: 1501837
    Change-Id: I630b3c79a45c0f735ffffd44f92a91c1b0712e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149630
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242369}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/edit_tab_group_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/edit_tab_group_view_controller.mm
@@ -9,12 +9,24 @@
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_groups_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 namespace {
 constexpr CGFloat kBackgroundAlpha = 0.6;
+constexpr CGFloat kColoredDotSize = 21;
+constexpr CGFloat kTitleHorizontalMargin = 16;
+constexpr CGFloat kTitleVerticalMargin = 10;
+constexpr CGFloat kHorizontalMargin = 32;
+constexpr CGFloat kdotAndFieldContainerMargin = 44;
+constexpr CGFloat kDotTitleSeparationMargin = 12;
+constexpr CGFloat kTitleBackgroundCornerRadius = 17;
 }
 
-@implementation EditTabGroupViewController
+@implementation EditTabGroupViewController {
+  // Text input to name the group.
+  UITextField* _tabGroupTextField;
+}
 
 - (instancetype)init {
   CHECK(base::FeatureList::IsEnabled(kTabGroupsInGrid))
@@ -41,6 +53,105 @@ constexpr CGFloat kBackgroundAlpha = 0.6;
   } else {
     self.view.backgroundColor = [UIColor blackColor];
   }
+
+  UIView* dotAndFieldContainer = [self configuredDotAndFieldContainer];
+  [self.view addSubview:dotAndFieldContainer];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [dotAndFieldContainer.leadingAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor
+                       constant:kHorizontalMargin],
+    [dotAndFieldContainer.topAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor
+                       constant:kdotAndFieldContainerMargin],
+    [dotAndFieldContainer.trailingAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
+                       constant:-kHorizontalMargin],
+  ]];
+
+  // To force display the keyboard when the view is shown.
+  [_tabGroupTextField becomeFirstResponder];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+  return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Private helpers
+
+// Configures the text input dedicated for the group name.
+- (UITextField*)configuredTabGroupNameTextFieldInput {
+  UITextField* tabGroupTextField = [[UITextField alloc] init];
+  tabGroupTextField.textColor = [UIColor colorNamed:kSolidWhiteColor];
+  tabGroupTextField.font =
+      [UIFont preferredFontForTextStyle:UIFontTextStyleLargeTitle];
+  tabGroupTextField.adjustsFontForContentSizeCategory = YES;
+  tabGroupTextField.translatesAutoresizingMaskIntoConstraints = NO;
+
+  UITraitCollection* interfaceStyleDarkTraitCollection = [UITraitCollection
+      traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark];
+  UIColor* placeholderTextColor = [[UIColor colorNamed:kTextSecondaryColor]
+      resolvedColorWithTraitCollection:interfaceStyleDarkTraitCollection];
+
+  tabGroupTextField.attributedPlaceholder = [[NSAttributedString alloc]
+      initWithString:l10n_util::GetNSString(
+                         IDS_IOS_TAB_GROUP_CREATION_PLACEHOLDER)
+          attributes:@{NSForegroundColorAttributeName : placeholderTextColor}];
+  return tabGroupTextField;
+}
+
+// Returns the group color dot view.
+- (UIView*)groupDotViewWithColor:(UIColor*)color {
+  UIView* dotView = [[UIView alloc] initWithFrame:CGRectZero];
+  dotView.translatesAutoresizingMaskIntoConstraints = NO;
+  dotView.layer.cornerRadius = kColoredDotSize / 2;
+  dotView.backgroundColor = color;
+
+  [NSLayoutConstraint activateConstraints:@[
+    [dotView.heightAnchor constraintEqualToConstant:kColoredDotSize],
+    [dotView.widthAnchor constraintEqualToConstant:kColoredDotSize],
+  ]];
+
+  return dotView;
+}
+
+// Returns the configured full primary title (colored dot and text title).
+- (UIView*)configuredDotAndFieldContainer {
+  UIView* titleBackground = [[UIView alloc] initWithFrame:CGRectZero];
+  titleBackground.translatesAutoresizingMaskIntoConstraints = NO;
+  titleBackground.backgroundColor = [UIColor colorWithWhite:1 alpha:0.1];
+  titleBackground.layer.cornerRadius = kTitleBackgroundCornerRadius;
+  titleBackground.opaque = NO;
+
+  // TODO(crbug.com/1501837): Save the view to be able to change the color of
+  // the dot.
+  UIView* coloredDotView =
+      [self groupDotViewWithColor:[UIColor colorNamed:kYellow500Color]];
+  _tabGroupTextField = [self configuredTabGroupNameTextFieldInput];
+
+  [titleBackground addSubview:coloredDotView];
+  [titleBackground addSubview:_tabGroupTextField];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [_tabGroupTextField.leadingAnchor
+        constraintEqualToAnchor:coloredDotView.trailingAnchor
+                       constant:kDotTitleSeparationMargin],
+    [coloredDotView.centerYAnchor
+        constraintEqualToAnchor:_tabGroupTextField.centerYAnchor],
+    [coloredDotView.leadingAnchor
+        constraintEqualToAnchor:titleBackground.leadingAnchor
+                       constant:kTitleHorizontalMargin],
+    [titleBackground.trailingAnchor
+        constraintEqualToAnchor:_tabGroupTextField.trailingAnchor
+                       constant:kTitleHorizontalMargin],
+    [_tabGroupTextField.topAnchor
+        constraintEqualToAnchor:titleBackground.topAnchor
+                       constant:kTitleVerticalMargin],
+    [titleBackground.bottomAnchor
+        constraintEqualToAnchor:_tabGroupTextField.bottomAnchor
+                       constant:kTitleVerticalMargin],
+  ]];
+  return titleBackground;
 }
 
 @end
