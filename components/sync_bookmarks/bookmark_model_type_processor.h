c1a7d80a734da1c5b9312082cc6402af1585f7ae
Ali Hijazi <ahijazi@chromium.org>
2024-01-03 21:36:32 +0000

    [CodeHealth] Fix dangling ptr in BookmarkModelTypeProcessor
    Change-Id: Icff97e8feb2f2333939aed440e633a4ba9dc4b50
    Bug: 1512703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5149768
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1242637}

--- components/sync_bookmarks/bookmark_model_type_processor.h
+++ components/sync_bookmarks/bookmark_model_type_processor.h
@@ -161,8 +161,7 @@ class BookmarkModelTypeProcessor : public syncer::ModelTypeProcessor,
   // The bookmark model we are processing local changes from and forwarding
   // remote changes to. It is set during ModelReadyToSync(), which is called
   // during startup, as part of the bookmark-loading process.
-  raw_ptr<BookmarkModelView, AcrossTasksDanglingUntriaged> bookmark_model_ =
-      nullptr;
+  raw_ptr<BookmarkModelView> bookmark_model_ = nullptr;
 
   // Used to when processing remote updates to apply favicon information. It's
   // not set at start up because it's only avialable after the bookmark model
