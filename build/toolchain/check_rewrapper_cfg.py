fc343fc274ff0525aa5593d75ce107dc62b64b2f
Ben Segall <bentekkie@google.com>
2024-01-04 18:14:54 +0000

    Show googler vs non-googler instructions when reclient cfgs are missing
    Imported shutil under the new error message function to ensure the happy path is not impacted by the additional import.
    
    Did not add logic to differentiate between googler and non googler on non corp machine as that requires importing the google.auth library which would require vpython for this script.
    If this improvement in wording is not sufficient I will look into differentiating that case as well.
    
    Change-Id: I41632a824c80d6f6d88aa2b5e60d8f12d5cd5dd8
    Bug: b/318536549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5164093
    Reviewed-by: Michael Savigny <msavigny@google.com>
    Commit-Queue: Ben Segall <bentekkie@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242989}

--- build/toolchain/check_rewrapper_cfg.py
+++ build/toolchain/check_rewrapper_cfg.py
@@ -7,6 +7,48 @@ import os
 import sys
 
 
+def missing_cfg_error_message():
+  """This assumes that corp machine has gcert binary in known location."""
+  import shutil
+  if shutil.which("gcert") is not None:
+    return """
+To build with gn arg 'use_remoteexec=true' as a googler on a corp machine
+set "download_remoteexec_cfg" in .gclient like
+
+solutions = [
+    {
+        "name"        : "src",
+        # ...
+        "custom_vars" : {
+            "download_remoteexec_cfg": True,
+        },
+    },
+]
+
+and re-run `gclient sync`.
+
+See http://go/chrome-linux-build#setup-remote-execution
+for more details."""
+  elif sys.platform == 'linux':
+    return """
+To build with gn arg 'use_remoteexec=true' as a googler on a non corp machine
+see http://go/chrome-linux-build#setup-remote-execution for setup instructions.
+
+To build with gn arg 'use_remoteexec=true' as a non-googler set the appropriate
+`rbe_cfg_dir` value in args.gn.
+See
+https://chromium.googlesource.com/chromium/src/+/main/docs/linux/build_instructions.md#use-reclient
+for more details."""
+  else:
+    return """
+To build with gn arg 'use_remoteexec=true' as a googler on a non corp machine
+see http://go/chrome-linux-build#setup-remote-execution for setup instructions.
+
+Building with gn arg 'use_remoteexec=true' as a non-googler is not currently
+supported on your os (%s).
+""" % sys.platform
+
+
 def main():
   if len(sys.argv) != 2:
     print("This should have a path to reclient config file in its args.",
@@ -17,29 +59,10 @@ def main():
   if os.path.isfile(sys.argv[1]):
     return 0
 
-  print("""
-  reclient config file "%s" doesn't exist, you may need to set
-  "download_remoteexec_cfg" in .gclient like
-  ```
-  solutions = [
-    {
-      "name"        : "src",
-      # ...
-      "custom_vars" : {
-        "download_remoteexec_cfg": True,
-      },
-    },
-  ]
-  ```
-  and re-run `gclient sync`.
-
-  Or you may not set appropriate `rbe_cfg_dir` value in args.gn.
-
-  See
-  https://chromium.googlesource.com/chromium/src/+/main/docs/linux/build_instructions.md#use-reclient
-  for more details.
-  """ % (sys.argv[1]),
+  print("reclient config file '%s' doesn't exist" %
+        (os.path.abspath(sys.argv[1])),
         file=sys.stderr)
+  print(missing_cfg_error_message(), file=sys.stderr)
 
   return 1
 
