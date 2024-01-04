8749e198834b8b1dc7d9cdf922c9ff0ba2bdafad
Hongyu Long <hongyulong@chromium.org>
2024-01-04 20:06:02 +0000

    CrOS Settings: Hide two power sounds toggle row if no battery
    This CL will update the condition to hide the low battery sound and the
    charging sounds toggle row if no battery status exists or if the feature
    is disabled.
    
    Bug: b/318037405
    Change-Id: I1e11f7dbdcfbcba452e59bf5d42f9e633a81aaa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166375
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1243059}

--- chrome/test/data/webui/settings/chromeos/device_page/audio_page_test.ts
+++ chrome/test/data/webui/settings/chromeos/device_page/audio_page_test.ts
@@ -6,6 +6,7 @@ import 'chrome://os-settings/lazy_load.js';
 
 import {Router, routes, SettingsAudioElement, SettingsToggleButtonElement} from 'chrome://os-settings/os_settings.js';
 import {assert} from 'chrome://resources/js/assert.js';
+import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
@@ -51,44 +52,88 @@ suite('<settings-audio>', () => {
     Router.getInstance().resetRouteForTesting();
   });
 
-  test(
-      'low battery sound toggle button should reflect pref value', async () => {
-        loadTimeData.overrideValues({
-          areSystemSoundsEnabled: true,
-        });
-        await initPage();
-
-        const lowBatterySoundToggle =
-            page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
-                '#lowBatterySoundToggle');
-        assert(lowBatterySoundToggle);
-        assertTrue(isVisible(lowBatterySoundToggle));
-
-        assertTrue(lowBatterySoundToggle.checked);
-        assertTrue(page.prefs.ash.low_battery_sound.enabled.value);
-
-        lowBatterySoundToggle.click();
-        assertFalse(lowBatterySoundToggle.checked);
-        assertFalse(page.prefs.ash.low_battery_sound.enabled.value);
-      });
-
-  test('charging sounds toggle button should reflect pref value', async () => {
-    loadTimeData.overrideValues({
-      areSystemSoundsEnabled: true,
+  suite('When battery status is present', () => {
+    setup(async () => {
+      const batteryStatus = {
+        present: true,
+        charging: false,
+        calculating: false,
+        percent: 50,
+        statusText: '5 hours left',
+      };
+      webUIListenerCallback('battery-status-changed', batteryStatus);
+      await flushTasks();
     });
-    await initPage();
 
-    const chargingSoundsToggle =
-        page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
-            '#chargingSoundsToggle');
-    assert(chargingSoundsToggle);
-    assertTrue(isVisible(chargingSoundsToggle));
+    test(
+        'low battery sound toggle button should reflect pref value',
+        async () => {
+          loadTimeData.overrideValues({
+            areSystemSoundsEnabled: true,
+          });
+          await initPage();
 
-    assertFalse(chargingSoundsToggle.checked);
-    assertFalse(page.prefs.ash.charging_sounds.enabled.value);
+          const lowBatterySoundToggle =
+              page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+                  '#lowBatterySoundToggle');
+          assert(lowBatterySoundToggle);
+          assertTrue(isVisible(lowBatterySoundToggle));
 
-    chargingSoundsToggle.click();
-    assertTrue(chargingSoundsToggle.checked);
-    assertTrue(page.prefs.ash.charging_sounds.enabled.value);
+          assertTrue(lowBatterySoundToggle.checked);
+          assertTrue(page.prefs.ash.low_battery_sound.enabled.value);
+
+          lowBatterySoundToggle.click();
+          assertFalse(lowBatterySoundToggle.checked);
+          assertFalse(page.prefs.ash.low_battery_sound.enabled.value);
+        });
+
+    test(
+        'charging sounds toggle button should reflect pref value', async () => {
+          loadTimeData.overrideValues({
+            areSystemSoundsEnabled: true,
+          });
+          await initPage();
+
+          const chargingSoundsToggle =
+              page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+                  '#chargingSoundsToggle');
+          assert(chargingSoundsToggle);
+          assertTrue(isVisible(chargingSoundsToggle));
+
+          assertFalse(chargingSoundsToggle.checked);
+          assertFalse(page.prefs.ash.charging_sounds.enabled.value);
+
+          chargingSoundsToggle.click();
+          assertTrue(chargingSoundsToggle.checked);
+          assertTrue(page.prefs.ash.charging_sounds.enabled.value);
+        });
+  });
+
+  suite('When battery status is not present', () => {
+    setup(async () => {
+      const batteryStatus = {
+        present: false,
+        charging: false,
+        calculating: false,
+        percent: 100,
+        statusText: '',
+      };
+      webUIListenerCallback('battery-status-changed', batteryStatus);
+      await flushTasks();
+    });
+
+    test(
+        'charging sounds toggle and low battery sound are not visible',
+        async () => {
+          const chargingSoundsToggle =
+              page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+                  '#chargingSoundsToggle');
+          assertFalse(isVisible(chargingSoundsToggle));
+
+          const lowBatterySoundToggle =
+              page.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+                  '#lowBatterySoundToggle');
+          assertFalse(isVisible(lowBatterySoundToggle));
+        });
   });
 });
