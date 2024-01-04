c13b3b0704bf79b1bed25b1d1705c393c7747fd1
Greg Thompson <grt@chromium.org>
2024-01-04 13:25:39 +0000

    [Windows] Logging to diagnose flaky build failure
    generate_previous_version_mini_installer.py has started failing with
    what appears to be ERROR_FILE_NOT_FOUND from CreateProcessW. This error
    is expected when the target executable doesn't exist, except that
    there's no reason it wouldn't. Add some extra diagnostics in case this
    happens again to help rule out some obvious problems.
    
    Also reformat the file as per current style.
    
    Bug: 1515472
    Change-Id: I595dc8f45ce96e26bdf0e6dfcc3e9ffdd7ba1208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5168608
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1242884}

--- chrome/installer/mini_installer/generate_previous_version_mini_installer.py
+++ chrome/installer/mini_installer/generate_previous_version_mini_installer.py
@@ -1,44 +1,61 @@
 # Copyright 2016 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Generates a mini_installer with a lower version than an existing one."""
 
 import argparse
+import os
 import subprocess
 import sys
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--alternate_version_generator',
-      help='Path to alternate_version_generator.')
-  parser.add_argument('--mini_installer', help='Path to input mini_installer')
-  parser.add_argument('--out', help='Path to the generated mini_installer.')
-  parser.add_argument('--path_7za', help='Path to 7za.exe')
-  args = parser.parse_args()
-  assert args.alternate_version_generator
-  assert args.mini_installer
-  assert args.out
-  assert args.path_7za
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--alternate_version_generator',
+                        help='Path to alternate_version_generator.')
+    parser.add_argument('--mini_installer',
+                        help='Path to input mini_installer')
+    parser.add_argument('--out', help='Path to the generated mini_installer.')
+    parser.add_argument('--path_7za', help='Path to 7za.exe')
+    args = parser.parse_args()
+    assert args.alternate_version_generator
+    assert args.mini_installer
+    assert args.out
+    assert args.path_7za
 
-  cmd = [args.alternate_version_generator,
-         '--force',
-         '--previous',
-         '--mini_installer=' + args.mini_installer,
-         '--out=' + args.out,
-         '--7za_path=' + args.path_7za,]
+    cmd = [
+        args.alternate_version_generator,
+        '--force',
+        '--previous',
+        '--mini_installer=' + args.mini_installer,
+        '--out=' + args.out,
+        '--7za_path=' + args.path_7za,
+    ]
 
-  try:
-    # Run |cmd|, redirecting stderr to stdout in order for captured errors to be
-    # inline with corresponding stdout.
-    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-  except subprocess.CalledProcessError as e:
-    raise Exception("Error while running cmd: %s\n"
-                    "Exit code: %s\n"
-                    "Command output:\n%s" %
-                    (e.cmd, e.returncode, e.output))
+    try:
+        # Run |cmd|, redirecting stderr to stdout in order for captured errors
+        # to be inline with corresponding stdout.
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise Exception("Error while running cmd: %s\n"
+                        "Exit code: %s\n"
+                        "Command output:\n%s" %
+                        (e.cmd, e.returncode, e.output))
+    except FileNotFoundError as e:
+        # In https://crbug.com/1515472, we see cases where CreateProcess fails
+        # with ERROR_FILE_NOT_FOUND (2). Add some logging to try to understand
+        # what's going on.
+        cwd = os.getcwd()
+        msg = (
+            e.strerror + '\n  Diagnostics for https://crbug.com/1515472:\n' +
+            '  CreateProcess failed with last error code %d\n' % e.winerror +
+            '  CWD is %s and it %s\n' %
+            (cwd, 'exists' if os.path.isdir(cwd) else 'does not exist') +
+            '  executable is %s and it %s' %
+            (args.alternate_version_generator, 'exists' if os.path.isfile(
+                args.alternate_version_generator) else 'does not exist'))
+        raise OSError(e.errno, msg, e.filename, e.winerror, e.filename2) from e
 
 
 if '__main__' == __name__:
-  sys.exit(main())
+    sys.exit(main())
