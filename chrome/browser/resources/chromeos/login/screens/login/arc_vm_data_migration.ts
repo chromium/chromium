c6d54d48450f0ef92c3dfbfc2d2f69aba505ae24
Simon Ziegltrum <ziegltrum@google.com>
2024-01-08 17:25:11 +0000

    [OOBE] arc_vm_data_migration TS migration
    Bug: b/314761941
    Change-Id: Iff532b829d64ee9726fb15e9a1b3306d177b482e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148808
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Auto-Submit: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244118}

---
+++ chrome/browser/resources/chromeos/login/screens/login/arc_vm_data_migration.ts
@@ -0,0 +1,215 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Polymer element for ARCVM /data migration screen.
+ */
+
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/polymer/v3_0/paper-progress/paper-progress.js';
+import '//resources/polymer/v3_0/paper-styles/color.js';
+import '../../components/common_styles/oobe_dialog_host_styles.css.js';
+import '../../components/dialogs/oobe_adaptive_dialog.js';
+import '../../components/dialogs/oobe_loading_dialog.js';
+import '../../components/oobe_icons.html.js';
+
+import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
+import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
+import {MultiStepBehavior, MultiStepBehaviorInterface} from '../../components/behaviors/multi_step_behavior.js';
+import {OobeDialogHostBehavior, OobeDialogHostBehaviorInterface} from '../../components/behaviors/oobe_dialog_host_behavior.js';
+import {OobeI18nBehavior, OobeI18nBehaviorInterface} from '../../components/behaviors/oobe_i18n_behavior.js';
+import {OOBE_UI_STATE} from '../../components/display_manager_types.js';
+
+import {getTemplate} from './arc_vm_data_migration.html.js';
+
+// Keep in sync with ArcVmDataMigrationScreenView::UIState.
+enum ArcVmDataMigrationUiState {
+  LOADING = 'loading',
+  WELCOME = 'welcome',
+  RESUM = 'resume',
+  PROGRESS = 'progress',
+  SUCCESS = 'success',
+  FAILURE = 'failure',
+}
+
+// Keep in sync with kUserAction* in arc_vm_data_migration_screen.cc.
+enum ArcVmDataMigrationUserAction {
+  SKIP = 'skip',
+  UPDATE = 'update',
+  RESUME = 'resume',
+  FINISH = 'finish',
+  REPORT = 'report',
+}
+
+const ArcVmDataMigrationScreenElementBase = mixinBehaviors(
+                                                [
+                                                  OobeDialogHostBehavior,
+                                                  OobeI18nBehavior,
+                                                  LoginScreenBehavior,
+                                                  MultiStepBehavior,
+                                                ],
+                                                PolymerElement) as {
+  new (): PolymerElement & OobeDialogHostBehaviorInterface &
+      OobeI18nBehaviorInterface & LoginScreenBehaviorInterface &
+      MultiStepBehaviorInterface,
+};
+
+export class ArcVmDataMigrationScreen extends
+    ArcVmDataMigrationScreenElementBase {
+  static get is() {
+    return 'arc-vm-data-migration-element' as const;
+  }
+
+  static get template(): HTMLTemplateElement {
+    return getTemplate();
+  }
+
+  static get properties(): PolymerElementProperties {
+    return {
+      hasEnoughFreeDiskSpace: {
+        type: Boolean,
+        value: true,
+      },
+
+      requiredFreeDiskSpaceInString: {
+        type: String,
+        value: '',
+      },
+
+      minimumBatteryPercent: {
+        type: Number,
+        value: 0,
+      },
+
+      hasEnoughBattery: {
+        type: Boolean,
+        value: true,
+      },
+
+      isConnectedToCharger: {
+        type: Boolean,
+        value: true,
+      },
+
+      migrationProgress: {
+        type: Number,
+        value: -1,
+      },
+
+      estimatedRemainingTimeInString: {
+        type: String,
+        value: '',
+      },
+    };
+  }
+
+  private hasEnoughFreeDiskSpace: boolean;
+  private requiredFreeDiskSpaceInString: string;
+  private minimumBatteryPercent: number;
+  private hasEnoughBattery: boolean;
+  private isConnectedToCharger: boolean;
+  private migrationProgress: number;
+  private estimatedRemainingTimeInString: string;
+
+  constructor() {
+    super();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override defaultUIStep(): string {
+    return ArcVmDataMigrationUiState.LOADING;
+  }
+
+  override get UI_STEPS() {
+    return ArcVmDataMigrationUiState;
+  }
+
+  override get EXTERNAL_API(): string[] {
+    return [
+      'setUIState',
+      'setRequiredFreeDiskSpace',
+      'setMinimumBatteryPercent',
+      'setBatteryState',
+      'setMigrationProgress',
+      'setEstimatedRemainingTime',
+    ];
+  }
+
+  override ready(): void {
+    super.ready();
+    this.initializeLoginScreen('ArcVmDataMigrationScreen');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override getOobeUIInitialState(): OOBE_UI_STATE {
+    return OOBE_UI_STATE.MIGRATION;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  setUIState(state: number): void {
+    this.setUIStep(Object.values(ArcVmDataMigrationUiState)[state]);
+  }
+
+  setRequiredFreeDiskSpace(requiredFreeDiskSpaceInString: string): void {
+    this.hasEnoughFreeDiskSpace = false;
+    this.requiredFreeDiskSpaceInString = requiredFreeDiskSpaceInString;
+  }
+
+  setMinimumBatteryPercent(minimumBatteryPercent: number): void {
+    this.minimumBatteryPercent = Math.floor(minimumBatteryPercent);
+  }
+
+  setBatteryState(hasEnoughBattery: boolean, isConnectedToCharger: boolean):
+      void {
+    this.hasEnoughBattery = hasEnoughBattery;
+    this.isConnectedToCharger = isConnectedToCharger;
+  }
+
+  setMigrationProgress(migrationProgress: number): void {
+    this.migrationProgress = Math.floor(migrationProgress);
+  }
+
+  setEstimatedRemainingTime(estimatedRemainingTimeInString: string): void {
+    this.estimatedRemainingTimeInString = estimatedRemainingTimeInString;
+  }
+
+  private shouldDisableUpdateButton(
+      hasEnoughFreeDiskSpace: boolean, hasEnoughBattery: boolean): boolean {
+    return !hasEnoughFreeDiskSpace || !hasEnoughBattery;
+  }
+
+  private isProgressIndeterminate(migrationProgress: number): boolean {
+    return migrationProgress < 0;
+  }
+
+  private onSkipButtonClicked(): void {
+    this.userActed(ArcVmDataMigrationUserAction.SKIP);
+  }
+
+  private onUpdateButtonClicked(): void {
+    this.userActed(ArcVmDataMigrationUserAction.UPDATE);
+  }
+
+  private onResumeButtonClicked(): void {
+    this.userActed(ArcVmDataMigrationUserAction.RESUME);
+  }
+
+  private onFinishButtonClicked(): void {
+    this.userActed(ArcVmDataMigrationUserAction.FINISH);
+  }
+
+  private onReportButtonClicked(): void {
+    this.userActed(ArcVmDataMigrationUserAction.REPORT);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ArcVmDataMigrationScreen.is]: ArcVmDataMigrationScreen;
+  }
+}
+
+customElements.define(ArcVmDataMigrationScreen.is, ArcVmDataMigrationScreen);
