7539efca364d895763265443a7b43cca54fe152d
Tim Sergeant <tsergeant@chromium.org>
2024-01-08 21:56:28 +0000

    Revert "Link Capturing: Speculatively re-enable flaky test on Mac"
    This reverts commit 345cea900e0b62955cff037ace5f51f3f5597fd3.
    
    Reason for revert: The test case is still flaky, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/24120/overview
    
    Original change's description:
    > Link Capturing: Speculatively re-enable flaky test on Mac
    >
    > Several test cases have been disabled in this file for a long time on
    > Mac. We've recently changed things around a bit, so I'd like to
    > re-enable this simple test case to determine whether flakiness is still
    > present, and gather up-to-date logs if it is.
    >
    > Bug: 1515480
    > Change-Id: I968ca16468933086897e58b8aafa3e6082f4d876
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5167728
    > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1243875}
    
    Bug: 1515480
    Change-Id: I5fd749801a31b9441ed8ed3a93b6bb654b1346c4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5179026
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244292}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -250,9 +250,16 @@ IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
 
 // Tests that the intent picker icon is not visible if the navigation redirects
 // to a URL that doesn't have an installed PWA.
-// Gardeners: If this test is still flaky, please update crbug.com/1515480.
+// TODO(crbug.com/1515480): This test is flaky. Re-enable this test.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_DoesNotShowIntentPickerWhenRedirectedOutOfScope \
+  DISABLED_DoesNotShowIntentPickerWhenRedirectedOutOfScope
+#else
+#define MAYBE_DoesNotShowIntentPickerWhenRedirectedOutOfScope \
+  DoesNotShowIntentPickerWhenRedirectedOutOfScope
+#endif
 IN_PROC_BROWSER_TEST_P(IntentPickerIconBrowserTest,
-                       DoesNotShowIntentPickerWhenRedirectedOutOfScope) {
+                       MAYBE_DoesNotShowIntentPickerWhenRedirectedOutOfScope) {
   InstallTestWebApp(GetOtherAppUrlHost(), /*app_scope=*/"/");
 
   const GURL out_of_scope_url =
