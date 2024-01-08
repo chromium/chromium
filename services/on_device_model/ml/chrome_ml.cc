9ce3211b8dd022b4fdb248a02bdfe6fe43508ad6
Clark DuVall <cduvall@chromium.org>
2024-01-08 18:20:37 +0000

    [Merge 121] Log histograms for common errors instead of generating crash reports
    This switches to just logging a histogram and exiting cleanly for
    DXGI_ERROR_DEVICE_HUNG and DXGI_ERROR_DEVICE_REMOVED since we're not
    getting much info from the crash reports.
    
    This merge also includes the fix for crrev.com/c/5172921.
    
    (cherry picked from commit b72b22bcbcc0ffd23fe1e19cd5a07343c4a7177d)
    
    Bug: 1512318
    Change-Id: I113563718e441cc065b3e93d74a59f789fbbc041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169808
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1242981}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177269
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6167@{#1133}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- services/on_device_model/ml/chrome_ml.cc
+++ services/on_device_model/ml/chrome_ml.cc
@@ -17,6 +17,7 @@
 #include "base/native_library.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
+#include "base/process/process.h"
 #include "build/build_config.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "gpu/config/gpu_info_collector.h"
@@ -57,9 +58,31 @@ void LogGpuBlocked(GpuBlockedReason reason) {
   base::UmaHistogramEnumeration("OnDeviceModel.GpuBlockedReason", reason);
 }
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class GpuErrorReason {
+  kOther = 0,
+  kDxgiErrorDeviceHung = 1,
+  kDxgiErrorDeviceRemoved = 2,
+  kMaxValue = kDxgiErrorDeviceRemoved,
+};
+
 void FatalErrorFn(const char* msg) {
   SCOPED_CRASH_KEY_STRING1024("ChromeML", "error_msg", msg);
-  CHECK(false) << "ChromeML Error: " << msg;
+  std::string msg_str(msg);
+  GpuErrorReason error_reason = GpuErrorReason::kOther;
+  if (msg_str.find("DXGI_ERROR_DEVICE_HUNG") != std::string::npos) {
+    error_reason = GpuErrorReason::kDxgiErrorDeviceHung;
+  } else if (msg_str.find("DXGI_ERROR_DEVICE_REMOVED") != std::string::npos) {
+    error_reason = GpuErrorReason::kDxgiErrorDeviceRemoved;
+  }
+  base::UmaHistogramEnumeration("OnDeviceModel.GpuErrorReason", error_reason);
+  if (error_reason == GpuErrorReason::kOther) {
+    // Collect crash reports on unknown errors.
+    CHECK(false) << "ChromeML Error: " << msg;
+  } else {
+    base::Process::TerminateCurrentProcessImmediately(0);
+  }
 }
 
 }  // namespace
