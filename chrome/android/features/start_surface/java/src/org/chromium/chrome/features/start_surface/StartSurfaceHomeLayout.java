a88d7ef55649943b89873be01de2b46e64e69bfa
Xinyi Ji <xinyiji@google.com>
2024-01-08 18:16:32 +0000

    [MagicStack] Fix bug with flag "instant start" been enabled
    When both the 'instant start' and 'magic stack' features are activated and the system is constructed with a signing keys build, only the start surface toolbar appears on the page upon rebooting with Start Surface. This CL fixes this problem by adding a check for initialization in StartSurfaceHomeLayout#onTabSelecting().
    
    Bug: 1512962, 1516654
    Change-Id: Iaaf7bb239ba89589769000c4deade8920f4c34f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5177866
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244149}

--- chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceHomeLayout.java
+++ chrome/android/features/start_surface/java/src/org/chromium/chrome/features/start_surface/StartSurfaceHomeLayout.java
@@ -171,6 +171,8 @@ public class StartSurfaceHomeLayout extends Layout {
     }
 
     private void onTabSelecting(int tabId) {
+        if (!mIsInitialized) return;
+
         TabModelUtils.selectTabById(mTabModelSelector, tabId, TabSelectionType.FROM_USER, false);
         startHiding();
     }
