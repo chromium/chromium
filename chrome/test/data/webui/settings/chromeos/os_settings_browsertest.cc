bd50d7347a9f13c8105d2de7bc02955a2bc8333e
Moteva <moteva@google.com>
2024-01-08 23:57:20 +0000

    CrOS Settings: js2gtest migration - Refactor RunTest()
    Overload Mocha's RunTest() locally so we can avoid repeating the parent
    directory in the path each time.
    
    Bug:b/318889922, b/315704162
    Test:
    browser_test: run all tests in the file.
    
    Change-Id: I99e65648ab7563e9fecf7cd18a276684e120868a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176838
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244364}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.cc
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "ash/constants/ash_features.h"
+#include "base/strings/strcat.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/crostini/fake_crostini_features.h"
 #include "chrome/common/webui_url_constants.h"
@@ -15,6 +16,14 @@ class OSSettingsMochaTest : public WebUIMochaBrowserTest {
  protected:
   OSSettingsMochaTest() {
     set_test_loader_host(chrome::kChromeUIOSSettingsHost);
+  }
+
+  void RunSettingsTest(const std::string& current_path) {
+    // All OS Settings test files are located in the directory
+    // settings/chromeos/.
+    const std::string path_with_parent_directory =
+        base::StrCat({std::string("settings/chromeos/"), current_path});
+    RunTest(path_with_parent_directory, "mocha.run()");
   }
 
   base::test::ScopedFeatureList scoped_feature_list_{
@@ -44,35 +53,31 @@ class OSSettingsMochaTestRevampDisabled : public OSSettingsMochaTest {
 };
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, ApnDetailDialog) {
-  RunTest("settings/chromeos/apn_detail_dialog_test.js", "mocha.run()");
+  RunSettingsTest("apn_detail_dialog_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, AppLanguageSelectionDialog) {
-  RunTest(
-      "settings/chromeos/common/app_language_selection_dialog/"
-      "app_language_selection_dialog_test.js",
-      "mocha.run()");
+  RunSettingsTest(
+      "common/app_language_selection_dialog/"
+      "app_language_selection_dialog_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, AppLanguageSelectionItem) {
-  RunTest(
-      "settings/chromeos/common/app_language_selection_dialog/"
-      "app_language_selection_item_test.js",
-      "mocha.run()");
+  RunSettingsTest(
+      "common/app_language_selection_dialog/"
+      "app_language_selection_item_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, AppManagementFileHandlingItem) {
-  RunTest("settings/chromeos/app_management/file_handling_item_test.js",
-          "mocha.run()");
+  RunSettingsTest("app_management/file_handling_item_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, AppManagementManagedApps) {
-  RunTest("settings/chromeos/app_management/managed_apps_test.js",
-          "mocha.run()");
+  RunSettingsTest("app_management/managed_apps_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, AppManagementToggleRow) {
-  RunTest("settings/chromeos/app_management/toggle_row_test.js", "mocha.run()");
+  RunSettingsTest("app_management/toggle_row_test.js");
 }
 
 class OSSettingsCrostiniTestRevampEnabled
@@ -99,143 +104,116 @@ class OSSettingsCrostiniTestRevampDisabled
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageBruschettaSubpageTest) {
-  RunTest("settings/chromeos/crostini_page/bruschetta_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/bruschetta_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsCrostiniPageBruschettaSubpageRevampTest) {
-  RunTest("settings/chromeos/crostini_page/bruschetta_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/bruschetta_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniArcAdbTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_arc_adb_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_arc_adb_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsCrostiniPageCrostiniArcAdbRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_arc_adb_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_arc_adb_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniExportImportTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_export_import_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_export_import_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsCrostiniPageCrostiniExportImportRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_export_import_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_export_import_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(
     OSSettingsCrostiniTestRevampDisabled,
     OSSettingsCrostiniPageCrostiniExtraContainersSubpageTest) {
-  RunTest(
-      "settings/chromeos/crostini_page/"
-      "crostini_extra_containers_subpage_test.js",
-      "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_extra_containers_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(
     OSSettingsCrostiniTestRevampEnabled,
     OSSettingsCrostiniPageCrostiniExtraContainersSubpageRevampTest) {
-  RunTest(
-      "settings/chromeos/crostini_page/"
-      "crostini_extra_containers_subpage_test.js",
-      "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_extra_containers_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_page_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_page_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniPortForwardingTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_port_forwarding_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_port_forwarding_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsCrostiniPageCrostiniPortForwardingRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_port_forwarding_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_port_forwarding_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniSettingsCardTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_settings_card_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_settings_card_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsAboutPageCrostiniSettingsCardRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_settings_card_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_settings_card_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniSharedUsbDevicesTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_shared_usb_devices_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_shared_usb_devices_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(
     OSSettingsCrostiniTestRevampEnabled,
     OSSettingsCrostiniPageCrostiniSharedUsbDevicesRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_shared_usb_devices_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_shared_usb_devices_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampDisabled,
                        OSSettingsCrostiniPageCrostiniSubpageTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsCrostiniTestRevampEnabled,
                        OSSettingsCrostiniPageCrostiniSubpageRevampTest) {
-  RunTest("settings/chromeos/crostini_page/crostini_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("crostini_page/crostini_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DateTimePage) {
-  RunTest("settings/chromeos/date_time_page/date_time_page_test.js",
-          "mocha.run()");
+  RunSettingsTest("date_time_page/date_time_page_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTestRevampDisabled,
                        DateTimePageDateTimeSettingsCard) {
-  RunTest("settings/chromeos/date_time_page/date_time_settings_card_test.js",
-          "mocha.run()");
+  RunSettingsTest("date_time_page/date_time_settings_card_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DateTimePageTimezoneSelector) {
-  RunTest("settings/chromeos/date_time_page/timezone_selector_test.js",
-          "mocha.run()");
+  RunSettingsTest("date_time_page/timezone_selector_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DateTimePageTimezoneSubpage) {
-  RunTest("settings/chromeos/date_time_page/timezone_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("date_time_page/timezone_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DevicePageAudioPage) {
-  RunTest("settings/chromeos/device_page/audio_page_test.js", "mocha.run()");
+  RunSettingsTest("device_page/audio_page_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest,
                        DevicePageCustomizeButtonDropdownItem) {
-  RunTest(
-      "settings/chromeos/device_page/customize_button_dropdown_item_test.js",
-      "mocha.run()");
+  RunSettingsTest("device_page/customize_button_dropdown_item_test.js");
 }
 
 class OSSettingsDevicePeripheralAndInputEnabled : public OSSettingsMochaTest {
@@ -256,75 +234,60 @@ class OSSettingsDevicePeripheralAndInputEnabled : public OSSettingsMochaTest {
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageCustomizeButtonRow) {
-  RunTest("settings/chromeos/device_page/customize_button_row_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/customize_button_row_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DevicePageCustomizeButtonSelect) {
-  RunTest("settings/chromeos/device_page/customize_button_select_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/customize_button_select_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageCustomizeButtonsSubsection) {
-  RunTest("settings/chromeos/device_page/customize_buttons_subsection_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/customize_buttons_subsection_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageCustomizeMouseButtonsSubpage) {
-  RunTest(
-      "settings/chromeos/device_page/customize_mouse_buttons_subpage_test.js",
-      "mocha.run()");
+  RunSettingsTest("device_page/customize_mouse_buttons_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageCustomizePenButtonsSubpage) {
-  RunTest("settings/chromeos/device_page/customize_pen_buttons_subpage_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/customize_pen_buttons_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageCustomizeTabletButtonsSubpage) {
-  RunTest(
-      "settings/chromeos/device_page/customize_tablet_buttons_subpage_test.js",
-      "mocha.run()");
+  RunSettingsTest("device_page/customize_tablet_buttons_subpage_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTestRevampDisabled,
                        DevicePageDisplayPage) {
-  RunTest("settings/chromeos/device_page/display_page_test.js", "mocha.run()");
+  RunSettingsTest("device_page/display_page_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTestRevampEnabled,
                        DevicePageDisplayPageRevamp) {
-  RunTest("settings/chromeos/device_page/display_page_test.js", "mocha.run()");
+  RunSettingsTest("device_page/display_page_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest,
                        DevicePageDisplaySettingsMojoInterfaceProvider) {
-  RunTest(
-      "settings/chromeos/device_page/"
-      "display_settings_mojo_interface_provider_test.js",
-      "mocha.run()");
+  RunSettingsTest(
+      "device_page/display_settings_mojo_interface_provider_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsDevicePeripheralAndInputEnabled,
                        DevicePageDragAndDropManager) {
-  RunTest("settings/chromeos/device_page/drag_and_drop_manager_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/drag_and_drop_manager_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest, DevicePageFakeCrosAudioConfig) {
-  RunTest("settings/chromeos/device_page/fake_cros_audio_config_test.js",
-          "mocha.run()");
+  RunSettingsTest("device_page/fake_cros_audio_config_test.js");
 }
 
 IN_PROC_BROWSER_TEST_F(OSSettingsMochaTest,
                        DevicePageFakeInputDeviceSettingsProvider) {
-  RunTest(
-      "settings/chromeos/device_page/"
-      "fake_input_device_settings_provider_test.js",
-      "mocha.run()");
+  RunSettingsTest("device_page/fake_input_device_settings_provider_test.js");
 }
 }  // namespace ash::settings
