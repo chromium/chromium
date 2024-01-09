7c5d378ec4b9bf25566b660ded3bc92ebbc65e84
Kameron Lutes <kalutes@google.com>
2024-01-09 03:50:02 +0000

    ChromeOS: Change ReclaimTarget to use std::optional
    absl::optional will soon be removed, so switch ReclaimTaret to use
    std::optional instead.
    
    Bug: b:309901919
    Change-Id: I77b0edd21ceedf15811643ee329399a1b6ac7644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5148589
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Kameron Lutes <kalutes@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244447}

--- components/memory_pressure/reclaim_target.h
+++ components/memory_pressure/reclaim_target.h
@@ -5,8 +5,9 @@
 #ifndef COMPONENTS_MEMORY_PRESSURE_RECLAIM_TARGET_H_
 #define COMPONENTS_MEMORY_PRESSURE_RECLAIM_TARGET_H_
 
+#include <optional>
+
 #include "base/time/time.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace memory_pressure {
 
@@ -15,13 +16,13 @@ struct ReclaimTarget {
   ReclaimTarget() = default;
   ~ReclaimTarget() = default;
   explicit ReclaimTarget(uint64_t target_kb) : target_kb(target_kb) {}
-  ReclaimTarget(uint64_t target_kb, absl::optional<base::TimeTicks> origin_time)
+  ReclaimTarget(uint64_t target_kb, std::optional<base::TimeTicks> origin_time)
       : target_kb(target_kb), origin_time(origin_time) {}
 
   // The number of KiB that should be reclaimed.
   uint64_t target_kb = 0;
   // The time at which this reclaim target was calculated.
-  absl::optional<base::TimeTicks> origin_time;
+  std::optional<base::TimeTicks> origin_time;
 };
 
 }  // namespace memory_pressure
