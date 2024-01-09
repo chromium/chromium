0a7532844504cea5abfd9595cdb1a9a4045ffc85
Nan Lin <linnan@chromium.org>
2024-01-09 22:19:30 +0000

    Not creating experiment manager for ash internal profiles
    Bug: 1516997
    Change-Id: Ifbfffcd523d9f2a21d7cbca6ad1fed218e9382e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182037
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244913}

--- chrome/browser/tpcd/experiment/experiment_manager_impl.cc
+++ chrome/browser/tpcd/experiment/experiment_manager_impl.cc
@@ -16,6 +16,8 @@
 #include "base/no_destructor.h"
 #include "base/sequence_checker.h"
 #include "base/task/single_thread_task_runner.h"
+#include "build/buildflag.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/profiles/profile.h"
@@ -27,6 +29,10 @@
 #include "components/variations/synthetic_trials.h"
 #include "content/public/common/content_features.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/profiles/profile_types_ash.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 namespace tpcd::experiment {
 namespace {
@@ -57,6 +63,14 @@ ExperimentManagerImpl* ExperimentManagerImpl::GetForProfile(Profile* profile) {
           features::kCookieDeprecationFacilitatedTesting)) {
     return nullptr;
   }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // Ash internal profile should not be accounted for the experiment
+  // eligibility, and therefore should not create the experiment manager.
+  if (!IsUserProfile(profile)) {
+    return nullptr;
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   if (!features::kCookieDeprecationFacilitatedTestingEnableOTRProfiles.Get() &&
       (profile->IsOffTheRecord() || profile->IsGuestSession())) {
