a972e219dbe862e16282c0c8737b05b1b71bc887
Vasilii Sukhanov <vasilii@chromium.org>
2024-01-09 08:45:14 +0000

    Revert "iwa: integrate Isolated Web Apps installer with ChromeOS Shelf"
    This reverts commit cfd4a70f1402883544909f8b2634a5de34a4fda9.
    
    Reason for revert: The new test reliably crashes on ASAN and MSAN https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/55570/overview
    
    Original change's description:
    > iwa: integrate Isolated Web Apps installer with ChromeOS Shelf
    >
    > Bug: 1479140
    > Change-Id: I9072e400d22adfa3b68aebd619fd60dc2d96b5af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5142214
    > Commit-Queue: Zelin Liu <zelin@chromium.org>
    > Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    > Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1244435}
    
    Bug: 1479140
    Change-Id: I1cce21663991cb8ec195ba56b564c14cc9efd6b6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5176997
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244528}

--- chrome/browser/ash/crosapi/lacros_shelf_item_tracker.cc
+++ chrome/browser/ash/crosapi/lacros_shelf_item_tracker.cc
@@ -13,7 +13,6 @@
 #include "ash/public/cpp/shelf_types.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chrome/browser/ui/ash/shelf/isolated_web_app_installer_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/lacros_shelf_item_controller.h"
 #include "chromeos/crosapi/mojom/lacros_shelf_item_tracker.mojom.h"
 #include "components/exo/shell_surface_util.h"
@@ -154,8 +153,8 @@ LacrosShelfItemTracker::CreateDelegateByInstanceType(
     mojom::InstanceType instance_type) {
   switch (instance_type) {
     case mojom::InstanceType::kIsolatedWebAppInstaller: {
-      return std::make_unique<IsolatedWebAppInstallerShelfItemController>(
-          shelf_id);
+      // TODO(crbug.com/1479140): implement.
+      return nullptr;
     }
   }
   return nullptr;
