5cce8148a4e4d60b7ee6b4e377d80803b9be7e06
Shuran Huang <shuuran@chromium.org>
2024-01-09 23:28:10 +0000

    Don't warn about 3PCD when the cookie has been excluded by other reasons
    Before: https://screenshot.googleplex.com/9pBiSHnjdWJuQhY.png
    
    After: https://screenshot.googleplex.com/7UGs8dZ2HJWKEpF.png
    
    Bug: 1515699
    Change-Id: I2cfadda5a36c45f87fe3c394b40e297efd245cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169712
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1244952}

--- net/cookies/cookie_inclusion_status_unittest.cc
+++ net/cookies/cookie_inclusion_status_unittest.cc
@@ -50,12 +50,78 @@ TEST(CookieInclusionStatusTest, ExcludeStatus) {
       CookieInclusionStatus status_two_reasons = status_one_reason;
       status_two_reasons.AddExclusionReason(reason2);
       EXPECT_FALSE(status_two_reasons.IsInclude());
-      EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason1));
-      EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason2));
-      EXPECT_FALSE(status_two_reasons.HasOnlyExclusionReason(reason1));
-      EXPECT_FALSE(status_two_reasons.HasOnlyExclusionReason(reason2));
+
+      if (reason1 != CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT &&
+          reason2 != CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT) {
+        EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason1));
+        EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason2));
+      }
     }
   }
+}
+
+TEST(CookieInclusionStatusTest,
+     ExcludeStatus_MaybeClearThirdPartyPhaseoutReason) {
+  int num_exclusion_reasons =
+      static_cast<int>(CookieInclusionStatus::NUM_EXCLUSION_REASONS);
+  CookieInclusionStatus::ExclusionReason reason1 =
+      CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT;
+  const CookieInclusionStatus status_one_reason(reason1);
+  ASSERT_FALSE(status_one_reason.IsInclude());
+  ASSERT_TRUE(status_one_reason.HasOnlyExclusionReason(reason1));
+
+  for (int j = 0; j < num_exclusion_reasons; ++j) {
+    auto reason2 = static_cast<CookieInclusionStatus::ExclusionReason>(j);
+    if (reason1 == reason2) {
+      continue;
+    }
+    EXPECT_FALSE(status_one_reason.HasExclusionReason(reason2)) << reason2;
+
+    CookieInclusionStatus status_two_reasons = status_one_reason;
+    status_two_reasons.AddExclusionReason(reason2);
+    EXPECT_FALSE(status_two_reasons.IsInclude());
+
+    if (reason2 == CookieInclusionStatus::
+                       EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET) {
+      EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason1));
+      EXPECT_TRUE(status_two_reasons.HasExclusionReason(reason2));
+    } else {
+      EXPECT_TRUE(status_two_reasons.HasOnlyExclusionReason(reason2));
+    }
+  }
+}
+
+TEST(CookieInclusionStatusTest,
+     AddExclusionReason_MaybeClearThirdPartyPhaseoutReason) {
+  CookieInclusionStatus status;
+  status.AddWarningReason(CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT);
+  ASSERT_TRUE(status.ShouldWarn());
+  ASSERT_TRUE(status.HasExactlyWarningReasonsForTesting(
+      {CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT}));
+  // Adding an exclusion reason should clear 3PCD warning reason.
+  status.AddExclusionReason(
+      CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT);
+  EXPECT_TRUE(status.HasExactlyExclusionReasonsForTesting(
+      {CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT}));
+  EXPECT_FALSE(status.ShouldWarn());
+
+  status.AddExclusionReason(
+      CookieInclusionStatus::
+          EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET);
+  EXPECT_TRUE(status.HasExactlyExclusionReasonsForTesting(
+      {CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT,
+       CookieInclusionStatus::
+           EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET}));
+  // Adding an exclusion reason unrelated with 3PCD should clear 3PCD related
+  // exclusion reasons.
+  status.AddExclusionReason(
+      CookieInclusionStatus::EXCLUDE_SAMESITE_NONE_INSECURE);
+  EXPECT_TRUE(status.HasExactlyExclusionReasonsForTesting(
+      {CookieInclusionStatus::EXCLUDE_SAMESITE_NONE_INSECURE,
+       // TODO(crbug.com/1516673): This should also be removed.
+       CookieInclusionStatus::
+           EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET}));
+  EXPECT_FALSE(status.IsInclude());
 }
 
 TEST(CookieInclusionStatusTest, AddExclusionReason) {
