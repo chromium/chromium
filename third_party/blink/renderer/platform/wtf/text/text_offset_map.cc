013bf7f2cc1ce1b8230807b97e3d0da0e172fcb3
Kent Tamura <tkent@chromium.org>
2024-01-09 08:09:08 +0000

    WTF: Add another constructor of TextOffsetMap
    It merges two TextOffsetMap instances.
    
    This CL has no production behavior changes yet, and is a preparation
    to fix crbug.com/486880.
    
    Bug: 486880
    Change-Id: I56272e6470ec0aaf1a5b925718ec7933c0216fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5179851
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244512}

--- third_party/blink/renderer/platform/wtf/text/text_offset_map.cc
+++ third_party/blink/renderer/platform/wtf/text/text_offset_map.cc
@@ -13,6 +13,62 @@ std::ostream& operator<<(std::ostream& stream,
   return stream << "{" << entry.source << ", " << entry.target << "}";
 }
 
+std::ostream& operator<<(std::ostream& stream,
+                         const Vector<TextOffsetMap::Entry>& entries) {
+  stream << "{";
+  for (wtf_size_t i = 0; i < entries.size(); ++i) {
+    if (i > 0) {
+      stream << ", ";
+    }
+    stream << entries[i];
+  }
+  return stream << "}";
+}
+
+TextOffsetMap::TextOffsetMap(const TextOffsetMap& map12,
+                             const TextOffsetMap& map23) {
+  if (map12.IsEmpty()) {
+    entries_ = map23.entries_;
+    return;
+  }
+  if (map23.IsEmpty()) {
+    entries_ = map12.entries_;
+    return;
+  }
+
+  const wtf_size_t size12 = map12.entries_.size();
+  const wtf_size_t size23 = map23.entries_.size();
+  wtf_size_t index12 = 0, index23 = 0;
+  int length_diff_12 = 0, length_diff_23 = 0;
+  while (index12 < size12 && index23 < size23) {
+    const Entry& entry12 = map12.entries_[index12];
+    const Entry& entry23 = map23.entries_[index23];
+    if (entry12.target < entry23.source) {
+      Append(entry12.source, entry12.target + length_diff_23);
+      length_diff_12 = entry12.target - entry12.source;
+      ++index12;
+    } else if (entry12.target == entry23.source) {
+      Append(entry12.source, entry23.target);
+      length_diff_12 = entry12.target - entry12.source;
+      length_diff_23 = entry23.target - entry23.source;
+      ++index12;
+      ++index23;
+    } else {
+      Append(entry23.source - length_diff_12, entry23.target);
+      length_diff_23 = entry23.target - entry23.source;
+      ++index23;
+    }
+  }
+  for (; index12 < size12; ++index12) {
+    const Entry& entry12 = map12.entries_[index12];
+    Append(entry12.source, entry12.target + length_diff_23);
+  }
+  for (; index23 < size23; ++index23) {
+    const Entry& entry23 = map23.entries_[index23];
+    Append(entry23.source - length_diff_12, entry23.target);
+  }
+}
+
 void TextOffsetMap::Append(wtf_size_t source, wtf_size_t target) {
   DCHECK(IsEmpty() ||
          (source > entries_.back().source && target > entries_.back().target));
