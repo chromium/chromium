8fae0f920ae347f57a66032da6a1359a098c1fea
Edgar Arriaga <edgararriaga@google.com>
2024-01-09 20:32:41 +0000

    Launch OnBeginFrameThrottle feature
    Bug: b/284356269
    Change-Id: I88334c0dd3a63ce9cdebc2e4459514c47d6eb625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5161249
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Edgar Arriaga García <edgararriaga@google.com>
    Auto-Submit: Edgar Arriaga García <edgararriaga@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244845}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -261,7 +261,12 @@ BASE_FEATURE(kOnBeginFrameAcks,
 // callbacks to match the preferred framerate.
 BASE_FEATURE(kOnBeginFrameThrottleVideo,
              "OnBeginFrameThrottleVideo",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_ANDROID)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+             );
 
 BASE_FEATURE(kSharedBitmapToSharedImage,
              "SharedBitmapToSharedImage",
