347521e55e90ad1f804527ad051c6008a57944c8
Mohamed Heikal <mheikal@chromium.org>
2024-01-10 23:38:13 +0000

    Check both 32bit and 64bit static initializers
    Bug: 1516670
    Change-Id: I4655e8fc39a86d79602640c02c7f02a250c313cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182914
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245601}

--- build/android/gyp/assert_static_initializers.py
+++ build/android/gyp/assert_static_initializers.py
@@ -11,8 +11,6 @@ import os
 import re
 import subprocess
 import sys
-import tempfile
-import zipfile
 
 from util import build_utils
 
@@ -27,38 +25,14 @@ def _RunReadelf(so_path, options, tool_prefix=''):
       [so_path]).decode('utf8')
 
 
-def _ParseLibBuildId(so_path, tool_prefix):
-  """Returns the Build ID of the given native library."""
-  stdout = _RunReadelf(so_path, ['-n'], tool_prefix)
-  match = re.search(r'Build ID: (\w+)', stdout)
-  return match.group(1) if match else None
+def _DumpStaticInitializers(so_path):
+  subprocess.check_call([_DUMP_STATIC_INITIALIZERS_PATH, so_path])
 
 
-def _VerifyLibBuildIdsMatch(tool_prefix, *so_files):
-  if len(set(_ParseLibBuildId(f, tool_prefix) for f in so_files)) > 1:
-    raise Exception('Found differing build ids in output directory and apk. '
-                    'Your output directory is likely stale.')
-
-
-def _DumpStaticInitializers(apk_so_name, unzipped_so, out_dir, tool_prefix):
-  so_with_symbols_path = os.path.join(out_dir, 'lib.unstripped',
-                                      os.path.basename(apk_so_name))
-  if not os.path.exists(so_with_symbols_path):
-    raise Exception('Unstripped .so not found. Looked here: %s' %
-                    so_with_symbols_path)
-  _VerifyLibBuildIdsMatch(tool_prefix, unzipped_so, so_with_symbols_path)
-  subprocess.check_call([_DUMP_STATIC_INITIALIZERS_PATH, so_with_symbols_path])
-
-
-def _ReadInitArray(so_path, tool_prefix, expect_no_initializers):
+def _ReadInitArray(so_path, tool_prefix):
   stdout = _RunReadelf(so_path, ['-SW'], tool_prefix)
   # Matches: .init_array INIT_ARRAY 000000000516add0 5169dd0 000010 00 WA 0 0 8
   match = re.search(r'\.init_array.*$', stdout, re.MULTILINE)
-  if expect_no_initializers:
-    if match:
-      raise Exception(
-          'Expected no initializers for %s, yet some were found' % so_path)
-    return 0
   if not match:
     raise Exception('Did not find section: .init_array in {}:\n{}'.format(
         so_path, stdout))
@@ -66,7 +40,7 @@ def _ReadInitArray(so_path, tool_prefix, expect_no_initializers):
   return int(size_str, 16)
 
 
-def _CountStaticInitializers(so_path, tool_prefix, expect_no_initializers):
+def _CountStaticInitializers(so_path, tool_prefix):
   # Find the number of files with at least one static initializer.
   # First determine if we're 32 or 64 bit
   stdout = _RunReadelf(so_path, ['-h'], tool_prefix)
@@ -80,43 +54,9 @@ def _CountStaticInitializers(so_path, tool_prefix, expect_no_initializers):
   # Then find the number of files with global static initializers.
   # NOTE: this is very implementation-specific and makes assumptions
   # about how compiler and linker implement global static initializers.
-  init_array_size = _ReadInitArray(so_path, tool_prefix, expect_no_initializers)
+  init_array_size = _ReadInitArray(so_path, tool_prefix)
   assert init_array_size % word_size == 0
   return init_array_size // word_size
-
-
-def _AnalyzeStaticInitializers(apk_or_aab, tool_prefix, dump_sis, out_dir,
-                               ignored_libs, no_initializers_libs):
-  with zipfile.ZipFile(apk_or_aab) as z:
-    so_files = [
-        f for f in z.infolist() if f.filename.endswith('.so')
-        and f.file_size > 0 and os.path.basename(f.filename) not in ignored_libs
-    ]
-    # Skip checking static initializers for secondary abi libs. They will be
-    # checked by 32-bit bots. This avoids the complexity of finding 32 bit .so
-    # files in the output directory in 64 bit builds.
-    has_64 = any('64' in f.filename for f in so_files)
-    files_to_check = [f for f in so_files if not has_64 or '64' in f.filename]
-
-    # Do not check partitioned libs. They have no ".init_array" section since
-    # all SIs are considered "roots" by the linker, and so end up in the base
-    # module.
-    files_to_check = [
-        f for f in files_to_check if not f.filename.endswith('_partition.so')
-    ]
-
-    si_count = 0
-    for f in files_to_check:
-      lib_basename = os.path.basename(f.filename)
-      expect_no_initializers = lib_basename in no_initializers_libs
-      with tempfile.NamedTemporaryFile(prefix=lib_basename) as temp:
-        temp.write(z.read(f))
-        temp.flush()
-        si_count += _CountStaticInitializers(temp.name, tool_prefix,
-                                             expect_no_initializers)
-        if dump_sis:
-          _DumpStaticInitializers(f.filename, temp.name, out_dir, tool_prefix)
-  return si_count
 
 
 def main():
@@ -127,21 +67,13 @@ def main():
   parser.add_argument('--expected-count', required=True, type=int,
                       help='Fail if number of static initializers is not '
                            'equal to this value.')
-  parser.add_argument('apk_or_aab', help='Path to .apk or .aab file.')
+  parser.add_argument('--unstripped-so-path',
+                      help='Path to the unstripped version of the .so '
+                      'file if needed for better dumps.')
+  parser.add_argument('so_path', help='Path to .so file.')
   args = parser.parse_args()
 
-  # TODO(crbug.com/838414): add support for files included via loadable_modules.
-  ignored_libs = {
-      'libarcore_sdk_c.so', 'libcrashpad_handler_trampoline.so',
-      'libsketchology_native.so'
-  }
-  # The chromium linker doesn't have static initializers, which makes the
-  # regular check throw. It should not have any.
-  no_initializers_libs = ['libchromium_android_linker.so']
-
-  si_count = _AnalyzeStaticInitializers(args.apk_or_aab, args.tool_prefix,
-                                        False, '.', ignored_libs,
-                                        no_initializers_libs)
+  si_count = _CountStaticInitializers(args.so_path, args.tool_prefix)
   if si_count != args.expected_count:
     print('Expected {} static initializers, but found {}.'.format(
         args.expected_count, si_count))
@@ -153,8 +85,10 @@ def main():
 
     print('Dumping static initializers via dump-static-initializers.py:')
     sys.stdout.flush()
-    _AnalyzeStaticInitializers(args.apk_or_aab, args.tool_prefix, True, '.',
-                               ignored_libs, no_initializers_libs)
+    dump_so_path = args.so_path
+    if args.unstripped_so_path:
+      dump_so_path = args.unstripped_so_path
+    _DumpStaticInitializers(dump_so_path)
     print()
     print('For more information:')
     print('    https://chromium.googlesource.com/chromium/src/+/main/docs/'
