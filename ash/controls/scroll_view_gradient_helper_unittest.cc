a35a94fabf307f1f8c7bd2c6279d21a9c9a0cb03
Mitsuru Oshima <oshima@chromium.org>
2024-01-10 21:06:57 +0000

    Remove unused GradientLayerDelegate
    Bug: None
    Change-Id: I185fd1d034bc8d6265edca8f7b030a785725d44c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5186734
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245503}

--- ash/controls/scroll_view_gradient_helper_unittest.cc
+++ ash/controls/scroll_view_gradient_helper_unittest.cc
@@ -4,7 +4,6 @@
 
 #include <memory>
 
-#include "ash/controls/gradient_layer_delegate.h"
 #include "ash/controls/scroll_view_gradient_helper.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/compositor/layer.h"
