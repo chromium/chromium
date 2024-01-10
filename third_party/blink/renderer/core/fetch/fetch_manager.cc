fb94946d1579a11fe89d124f1ff236610c4038e2
Ming-Ying Chung <mych@chromium.org>
2024-01-10 09:04:55 +0000

    Move duration UMA logging for fetch keepalive requests to proper place.
    The current implementation logs nothing after loader is detached.
    It should happen in the detached ThreadableLoader instead of
    FetchManager Loader.
    
    Bug: 1356128
    Change-Id: Ib618b3ff93f5bbe10ad6d1fe07170e5c566a4f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5137541
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245158}

--- third_party/blink/renderer/core/fetch/fetch_manager.cc
+++ third_party/blink/renderer/core/fetch/fetch_manager.cc
@@ -506,7 +506,6 @@ class FetchManager::Loader final
   Member<ScriptCachedMetadataHandler> cached_metadata_handler_;
   TraceWrapperV8Reference<v8::Value> exception_;
   base::TimeTicks request_started_time_;
-  absl::optional<base::TimeTicks> detached_time_;
 };
 
 FetchManager::Loader::Loader(ExecutionContext* execution_context,
@@ -871,7 +870,6 @@ void FetchManager::Loader::Dispose() {
   fetch_manager_ = nullptr;
   if (threadable_loader_) {
     if (GetFetchRequestData()->Keepalive()) {
-      detached_time_ = base::TimeTicks::Now();
       threadable_loader_->Detach();
     } else {
       threadable_loader_->Cancel();
@@ -1204,36 +1202,17 @@ void FetchManager::Loader::LogIfKeepalive(
 
   base::UmaHistogramEnumeration("FetchKeepAlive.Renderer.Metrics", type);
 
-  auto now = base::TimeTicks::Now();
-  base::TimeDelta duration = now - request_started_time_;
-  absl::optional<base::TimeDelta> duration_after_detached =
-      detached_time_.has_value() ? absl::make_optional(now - *detached_time_)
-                                 : absl::nullopt;
+  base::TimeDelta duration = base::TimeTicks::Now() - request_started_time_;
   if (type == FetchKeepAliveRendererMetricType::kLoadingSuceeded ||
       type == FetchKeepAliveRendererMetricType::kLoadingFailed) {
     base::UmaHistogramMediumTimes("FetchKeepAlive.Renderer.Duration", duration);
-    if (duration_after_detached.has_value()) {
-      base::UmaHistogramMediumTimes(
-          "FetchKeepAlive.Renderer.DurationAfterDetached",
-          *duration_after_detached);
-    }
 
     if (type == FetchKeepAliveRendererMetricType::kLoadingSuceeded) {
       base::UmaHistogramMediumTimes(
           "FetchKeepAlive.Renderer.Duration.Succeeded", duration);
-      if (duration_after_detached.has_value()) {
-        base::UmaHistogramMediumTimes(
-            "FetchKeepAlive.Renderer.DurationAfterDetached.Succeeded",
-            *duration_after_detached);
-      }
     } else {
       base::UmaHistogramMediumTimes("FetchKeepAlive.Renderer.Duration.Failed",
                                     duration);
-      if (duration_after_detached.has_value()) {
-        base::UmaHistogramMediumTimes(
-            "FetchKeepAlive.Renderer.DurationAfterDetached.Failed",
-            *duration_after_detached);
-      }
     }
   }
 }
