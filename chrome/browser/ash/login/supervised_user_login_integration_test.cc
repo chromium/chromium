687a19c58196f794a1fb8319793fe97a53bc8587
Courtney Wong <courtneywong@chromium.org>
2024-01-10 00:21:49 +0000

    families: Check that the expected policies are applied for SU login
    Checks that chrome://policy shows the expected policies for a supervised
    user. These policies should not change regularly for test accounts.
    
    Tested: Ran test 100 times
    
    Bug: b:319295181
    Change-Id: Iea2b78b78ea0221b04642d3a5046201acb991dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178563
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1244990}

--- chrome/browser/ash/login/supervised_user_login_integration_test.cc
+++ chrome/browser/ash/login/supervised_user_login_integration_test.cc
@@ -8,10 +8,15 @@
 #include "chrome/browser/ash/login/test/session_manager_state_waiter.h"
 #include "chrome/test/base/chromeos/crosier/ash_integration_test.h"
 #include "chrome/test/base/chromeos/crosier/chromeos_integration_login_mixin.h"
+#include "chrome/test/base/chromeos/crosier/interactive_ash_test.h"
 #include "chrome/test/base/chromeos/crosier/supervised_user_login_delegate.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "url/gurl.h"
 
 // Tests using production GAIA can only run on branded builds.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
+constexpr char kPolicyUrl[] = "chrome://policy";
 
 // This class implements CrOS login using prod GAIA with the different types of
 // supervised accounts.
@@ -28,36 +33,90 @@ class SupervisedUserLoginIntegrationTest : public AshIntegrationTest {
     login_mixin().set_custom_gaia_login_delegate(&delegate_);
   }
 
+  auto OpenPolicyPage() {
+    return Do([&]() { CreateBrowserWindow(GURL(kPolicyUrl)); });
+  }
+
+  // Checks that chrome://policy pages shows policies applied for the child
+  // user.
+  void VerifyPolicies() {
+    DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kPolicyTabId);
+    DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kPoliciesLoaded);
+    DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kVerifyPolicies);
+    const DeepQuery kPolicyTableQuery{"policy-table", ".main"};
+
+    StateChange policies_loaded;
+    policies_loaded.event = kPoliciesLoaded;
+    policies_loaded.where = kPolicyTableQuery;
+    policies_loaded.type = StateChange::Type::kExistsAndConditionTrue;
+    policies_loaded.test_function =
+        "(el) => el.querySelectorAll('policy-row:not([hidden])').length > 0";
+
+    StateChange verify_policies;
+    verify_policies.event = kVerifyPolicies;
+    verify_policies.where = kPolicyTableQuery;
+    verify_policies.type = StateChange::Type::kExistsAndConditionTrue;
+    verify_policies.test_function = R"(
+      (el) => {
+        const expectedPolicies = ['ArcPolicy', 'DeveloperToolsAvailability',
+          'EduCoexistenceToSVersion', 'ForceGoogleSafeSearch',
+          'LacrosSecondaryProfilesAllowed', 'ParentAccessCodeConfig',
+          'PerAppTimeLimits', 'PerAppTimeLimitsAllowlist',
+          'ReportArcStatusEnabled', 'URLBlocklist', 'UsageTimeLimit'];
+        const policyNodes = el.querySelectorAll('policy-row:not([hidden])');
+        const policies = [...policyNodes].map((node) =>
+          node.shadowRoot.querySelector('.name').innerText)
+        return expectedPolicies.every((policy) => policies.includes(policy));
+      })";
+
+    RunTestSequence(Log("Navigate to chrome://policy page"),
+                    InstrumentNextTab(kPolicyTabId, AnyBrowser()),
+                    OpenPolicyPage(),
+
+                    Log("Wait for policies to load"),
+                    WaitForWebContentsReady(kPolicyTabId, GURL(kPolicyUrl)),
+                    WaitForStateChange(kPolicyTabId, policies_loaded),
+
+                    Log("Check that all expected policies loaded"),
+                    WaitForStateChange(kPolicyTabId, verify_policies));
+  }
+
  protected:
   SupervisedUserLoginDelegate delegate_;
 };
 
 IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestUnicornLogin) {
+  SetupContextWidget();
+
   login_mixin().Login();
 
   ash::test::WaitForPrimaryUserSessionStart();
-
   EXPECT_TRUE(login_mixin().IsCryptohomeMounted());
+  VerifyPolicies();
 }
 
 IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGellerLogin) {
+  SetupContextWidget();
+
   delegate_.set_user_type(
       SupervisedUserLoginDelegate::SupervisedUserType::kGeller);
   login_mixin().Login();
 
   ash::test::WaitForPrimaryUserSessionStart();
-
   EXPECT_TRUE(login_mixin().IsCryptohomeMounted());
+  VerifyPolicies();
 }
 
 IN_PROC_BROWSER_TEST_F(SupervisedUserLoginIntegrationTest, TestGriffinLogin) {
+  SetupContextWidget();
+
   delegate_.set_user_type(
       SupervisedUserLoginDelegate::SupervisedUserType::kGriffin);
   login_mixin().Login();
 
   ash::test::WaitForPrimaryUserSessionStart();
-
   EXPECT_TRUE(login_mixin().IsCryptohomeMounted());
+  VerifyPolicies();
 }
 
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
