7a43d2fc379bf75e433b989da6875f53e76363a2
Emily Shack <emshack@chromium.org>
2024-01-11 01:06:36 +0000

    [M121] Tab Organization: Announce header on state change
    (cherry picked from commit 9a6bf8e8070187d745b5cf2e1426697eeb0fe691)
    
    Bug: 1510476
    Change-Id: I1bb7a1b9b5f68ba021d7620c72e35154ba738e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5181717
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1245015}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5187475
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6167@{#1234}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- chrome/browser/resources/tab_search/tab_organization_in_progress.ts
+++ chrome/browser/resources/tab_search/tab_organization_in_progress.ts
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './strings.m.js';
 import './tab_organization_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_loading_gradient/cr_loading_gradient.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './tab_organization_in_progress.html.js';
+
+export interface TabOrganizationInProgressElement {
+  $: {
+    header: HTMLElement,
+  };
+}
 
 // Loading state for the tab organization UI.
 export class TabOrganizationInProgressElement extends PolymerElement {
@@ -17,6 +25,15 @@ export class TabOrganizationInProgressElement extends PolymerElement {
 
   static get template() {
     return getTemplate();
+  }
+
+  announceHeader() {
+    this.$.header.textContent = '';
+    this.$.header.textContent = this.getTitle_();
+  }
+
+  private getTitle_(): string {
+    return loadTimeData.getString('inProgressTitle');
   }
 }
 
