7b901a8d66dfa103f433ee868b378962aa3a126c
Jiewei Qian <qjw@chromium.org>
2024-01-11 00:51:51 +0000

    cros-apps: treat blob: and filesystem: schemes as ineligible for ChromeOS Apps APIs
    Moves the blob: and filesystem: exclusion to
    IsUrlEligibleForCrosAppsApis predicate.
    
    Change-Id: I0b30235d5c8170c3b2e12666e9cfe84c756bd169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183062
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1245633}

--- chrome/browser/chromeos/cros_apps/api/cros_apps_api_utils.cc
+++ chrome/browser/chromeos/cros_apps/api/cros_apps_api_utils.cc
@@ -8,5 +8,17 @@
 #include "url/gurl.h"
 
 bool IsUrlEligibleForCrosAppsApis(const GURL& url) {
+  // TODO(b/311528206): Decide if this scheme check should be removed.
+  //
+  // The following schemes because they share the same origin as their creator
+  // (i.e. the App), and could cause problem during origin matching.
+  //
+  // The app could inadvertently create these URLs that serve third-party (from
+  // the App's perspective) untrustworthy content. Said third-party content
+  // probably shouldn't be treated as same origin as the app.
+  if (url.SchemeIs(url::kBlobScheme) || url.SchemeIs(url::kFileSystemScheme)) {
+    return false;
+  }
+
   return network::IsUrlPotentiallyTrustworthy(url);
 }
