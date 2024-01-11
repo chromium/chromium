fd26c5ac0b2dde91e551b79ab0c9b1f4e6b81532
David Schinazi <dschinazi@chromium.org>
2024-01-11 16:53:34 +0000

    Add spec link for blocked port list
    Change-Id: I69cc8714c850e216b80651f681875c9276936dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5189847
    Auto-Submit: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1245910}

--- net/base/port_util.cc
+++ net/base/port_util.cc
@@ -23,7 +23,7 @@ namespace {
 // The general list of blocked ports. Will be blocked unless a specific
 // protocol overrides it. (Ex: ftp can use port 21)
 // When adding a port to the list, consider also adding it to kAllowablePorts,
-// below.
+// below. See <https://fetch.spec.whatwg.org/#port-blocking>.
 const int kRestrictedPorts[] = {
     1,      // tcpmux
     7,      // echo
