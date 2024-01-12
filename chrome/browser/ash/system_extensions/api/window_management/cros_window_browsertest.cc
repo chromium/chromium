5ec273daf25a26294ce7abca0f2c23e551b8a1a5
Maggie Cai <mxcai@google.com>
2024-01-12 07:56:11 +0000

    [Shortstand] Delete obsolete CrosWindowManagementBrowserTest.
    With shortstand enabled, web app open in browser should no longer be a
    case, so we should remove tests that involve web app in browser mode in
    CrosWindowManagementBrowserTest if shorstand is enabled. Futuremore,
    because we are planning to remove all the code in system extentions,
    instead of skipping the tests, we just simply delete them.
    
    BUG=b/319546142
    
    Change-Id: Ic1a151ee1d054c6dd01b4c1851fc826ef8554634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5192043
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246316}

--- chrome/browser/ash/system_extensions/api/window_management/cros_window_browsertest.cc
+++ chrome/browser/ash/system_extensions/api/window_management/cros_window_browsertest.cc
@@ -242,32 +242,6 @@ class CrosWindowManagementBrowserTest : public SystemExtensionsApiBrowserTest {
  protected:
   TestSystemWebAppInstallation& swa_installation() { return *installation_; }
 
-  // Installs and launches a Web App that opens in a tab.
-  void InstallAndLaunchBrowserWebApp() {
-    ASSERT_TRUE(embedded_test_server()->Start());
-
-    const GURL start_url =
-        embedded_test_server()->GetURL("/web_apps/basic.html");
-
-    // Install a web app with `browser` display mode, so that it launched
-    // in a tab in regular browser window.
-    auto web_app_info = std::make_unique<web_app::WebAppInstallInfo>();
-    web_app_info->start_url = start_url;
-    web_app_info->user_display_mode = web_app::mojom::UserDisplayMode::kBrowser;
-    const webapps::AppId app_id = web_app::test::InstallWebApp(
-        browser()->profile(), std::move(web_app_info));
-    apps::AppReadinessWaiter(browser()->profile(), app_id).Await();
-
-    // Launch app through App Service proxy and wait for it to open.
-    auto* const proxy =
-        apps::AppServiceProxyFactory::GetForProfile(browser()->profile());
-
-    ui_test_utils::TabAddedWaiter waiter(browser());
-    proxy->Launch(app_id,
-                  /*event_flags=*/0, apps::LaunchSource::kFromAppListGrid);
-    waiter.Wait();
-  }
-
  private:
   mojo::UniqueReceiverSet<
       system_extensions_test::mojom::CrosWindowManagementTestHelper>
@@ -349,22 +323,6 @@ IN_PROC_BROWSER_TEST_F(CrosWindowManagementBrowserTest, CrosWindowClose) {
 
 IN_PROC_BROWSER_TEST_F(CrosWindowManagementBrowserTest, WindowOpenedEvent) {
   RunTest("window_opened_event.js");
-}
-
-IN_PROC_BROWSER_TEST_F(CrosWindowManagementBrowserTest, CrosWindowWebAppTab) {
-  InstallAndLaunchBrowserWebApp();
-
-  // Unfocus the web app.
-  chrome::SelectPreviousTab(browser());
-
-  // Run test which calls .getWindows().
-  RunTest("cros_window_web_app_tab.js");
-}
-
-IN_PROC_BROWSER_TEST_F(CrosWindowManagementBrowserTest, IgnoreWebAppTabs) {
-  InstallAndLaunchBrowserWebApp();
-
-  RunTest("cros_ignore_web_app_tabs.js");
 }
 
 IN_PROC_BROWSER_TEST_F(CrosWindowManagementBrowserTest,
