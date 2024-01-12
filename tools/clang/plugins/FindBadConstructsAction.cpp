d973c590e143ca26d6700d6358e1adfc189fbab5
mikt <mikt@google.com>
2024-01-12 07:06:29 +0000

    [BRP/clang-plugin] Add a flag to disable `raw_ptr<StackAllocated>` error
    We want to disable errors to `raw_ptr<StackAllocated>` due to issues
    around templates. However, a feature flag
    `check_raw_ptr_to_stack_allocated` is also associated with a feature
    to allow `StackAllocated` types to bypass `check_raw_ptr_fields` check.
    Disabling `check_raw_ptr_to_stack_allocated` can result in new errors
    for `StackAllocated*` types in the codebase, so adding a new flag that
    disables the `raw_ptr<StackAllocated>` error without affecting anything
    else.
    
    Change-Id: Ida73ace3da64b35f5d2fb13d3ae8fadefa5e48b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191475
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246302}

--- tools/clang/plugins/FindBadConstructsAction.cpp
+++ tools/clang/plugins/FindBadConstructsAction.cpp
@@ -100,6 +100,8 @@ bool FindBadConstructsAction::ParseArgs(const CompilerInstance& instance,
       options_.check_raw_ptr_fields = true;
     } else if (arg == "check-raw-ptr-to-stack-allocated") {
       options_.check_raw_ptr_to_stack_allocated = true;
+    } else if (arg == "disable-check-raw-ptr-to-stack-allocated-error") {
+      options_.disable_check_raw_ptr_to_stack_allocated_error = true;
     } else if (arg == "check-stack-allocated") {
       options_.check_stack_allocated = true;
     } else if (arg == "check-raw-ref-fields") {
