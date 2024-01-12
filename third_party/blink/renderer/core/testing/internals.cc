fdc633c072d985d8fac9a32241d71ab046b87de0
Yoichi Osato <yoichio@chromium.org>
2024-01-12 04:00:58 +0000

    Reapply "[LCPP] Call TimingPredictor fallback at window.onload or later LCP"
    This reverts commit c7968c41e6cf810bed0bf5046c2f266345889bf8.
    The original CL was reverted due to failing on builder
    "chromium/ci/WebKit Linux MSAN".
    
    The root cause is in msan or some slow environment, image load can
    finish earlier than expected.
    In this patch, I fix that by moving LCPPrediction promise before img
    element and also checking the img element is loaded already.
    (See diff between patch 1 and the latest)
    
    TEST: $ third_party/blink/tools/run_web_tests.py --release -t try_webkit_linux_msan virtual/lcpp/http/tests/lcp_critical_path_predictor/ --repeat-each=20
    
    Bug: 1493255,1508884
    Change-Id: I728fe4a23bdb1cb5ba7e8883642e7e074d1e52f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5183135
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246229}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -127,6 +127,7 @@
 #include "third_party/blink/renderer/core/layout/layout_tree_as_text.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/lcp_critical_path_predictor/element_locator.h"
+#include "third_party/blink/renderer/core/lcp_critical_path_predictor/lcp_critical_path_predictor.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_loader.h"
 #include "third_party/blink/renderer/core/loader/history_item.h"
@@ -623,8 +624,9 @@ TestWritableStreamSink::Optimizer::PerformInProcessOptimization(
 }
 
 void OnLCPPredicted(ScriptPromiseResolver* resolver,
-                    const Element& lcp_element) {
-  const ElementLocator locator = element_locator::OfElement(lcp_element);
+                    const Element* lcp_element) {
+  const ElementLocator locator =
+      lcp_element ? element_locator::OfElement(*lcp_element) : ElementLocator();
   resolver->Resolve(element_locator::ToStringForTesting(locator));
 }
 
@@ -4023,7 +4025,9 @@ ScriptPromise Internals::LCPPrediction(ScriptState* script_state,
       MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
 
-  document->AddLCPPredictedCallback(
+  LCPCriticalPathPredictor* lcpp = document->GetFrame()->GetLCPP();
+  CHECK(lcpp);
+  lcpp->AddLCPPredictedCallback(
       WTF::BindOnce(&OnLCPPredicted, WrapPersistent(resolver)));
   return promise;
 }
---
+++ third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/resources/common.js
@@ -0,0 +1,38 @@
+import {NonAssociatedWebTestControlHostRemote} from '/gen/content/web_test/common/web_test.mojom.m.js';
+import {mojo} from '/gen/mojo/public/js/bindings.js';
+import {ByteString} from '/gen/mojo/public/mojom/base/byte_string.mojom.m.js';
+import {LCPCriticalPathPredictorNavigationTimeHint} from '/gen/third_party/blink/public/mojom/lcp_critical_path_predictor/lcp_critical_path_predictor.mojom.m.js';
+
+export async function setupLCPTest(lcp_locator_protos) {
+  if (window.location.search != '?start') {
+    const hint = new LCPCriticalPathPredictorNavigationTimeHint();
+    // All fields are non-nullable.
+    hint.lcpElementLocators = [];
+    hint.lcpInfluencerScripts = [];
+    hint.fetchedFonts = [];
+
+    let getLCPBytes = async function(proto_file) {
+      const resp = await fetch(
+          '/gen/third_party/blink/renderer/core/lcp_critical_path_predictor/test_proto/' +
+          proto_file);
+      console.assert(resp.status == 200, `${resp.url} is not found.`);
+      const bytes = new ByteString;
+      bytes.data = new Uint8Array(await resp.arrayBuffer());
+      return bytes;
+    };
+
+    if (lcp_locator_protos) {
+      for (let proto of lcp_locator_protos) {
+        hint.lcpElementLocators.push(await getLCPBytes(proto));
+      }
+    }
+
+    const web_test_control_host_remote =
+        new NonAssociatedWebTestControlHostRemote();
+    web_test_control_host_remote.$.bindNewPipeAndPassReceiver().bindInBrowser(
+        'process');
+    web_test_control_host_remote.setLCPPNavigationHint(hint);
+
+    window.location.search = '?start';
+  }
+}
