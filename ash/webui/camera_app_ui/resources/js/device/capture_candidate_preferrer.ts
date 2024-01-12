99edb14154f6bf9c9ddafa08750885016b1064d5
Chu-Hsuan Yang <chuhsuan@chromium.org>
2024-01-12 09:31:31 +0000

    CCA: Change Rex's default aspect ratio
    CCA uses 4:3 as the default photo aspect ratio but Screebo devices have
    larger resolution in 16:9 (3840x2160) than 4:3 (2560x1920). This CL sets
    the default photo aspect ratio of Rex board to 16:9, allowing Screebo
    devices to use the largest resolution by default.
    
    Test: Verify that Screebo devices use 16:9 by default
    Bug: b:319035959
    Change-Id: Iae712c8a0d06f216a75a3648ec20061b392cd9e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5192331
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246347}

--- ash/webui/camera_app_ui/resources/js/device/capture_candidate_preferrer.ts
+++ ash/webui/camera_app_ui/resources/js/device/capture_candidate_preferrer.ts
@@ -4,6 +4,7 @@
 
 import {assert, assertNotReached} from '../assert.js';
 import * as expert from '../expert.js';
+import {getBoard} from '../models/load_time_data.js';
 import * as localStorage from '../models/local_storage.js';
 import {
   AspectRatioSet,
@@ -531,21 +532,18 @@ export class CaptureCandidatePreferrer {
   }
 
   private buildPhotoOptions(deviceId: string, resolutions: Resolution[]): void {
-    const defaultPreferOrder = [
-      AspectRatioSet.RATIO_4_3,
-      AspectRatioSet.RATIO_16_9,
-      AspectRatioSet.RATIO_OTHER,
-    ];
+    const aspectRatioSetPreferOrder = getAspectRatioSetPreferOrder();
+
     // Making sure that the prefer aspect ratio has resolution which is equal to
     // or larger than 720p.
     const prioritizedAspectRatioSet =
-        defaultPreferOrder.find(
+        aspectRatioSetPreferOrder.find(
             (ratio) => resolutions.some(
                 (r) => toAspectRatioSet(r) === ratio && r.height >= 720)) ??
-        defaultPreferOrder[0];
+        aspectRatioSetPreferOrder[0];
     this.preferPhotoAspectRatioOrder = [
       prioritizedAspectRatioSet,
-      ...defaultPreferOrder.filter(
+      ...aspectRatioSetPreferOrder.filter(
           (ratio) => ratio !== prioritizedAspectRatioSet),
     ];
 
@@ -966,4 +964,22 @@ function getFallbackVideoResolutionLevel(options: VideoResolutionOption[]):
     }
   }
   assertNotReached();
+}
+
+function getAspectRatioSetPreferOrder() {
+  const board = getBoard();
+  switch (board) {
+    case 'rex':
+      return [
+        AspectRatioSet.RATIO_16_9,
+        AspectRatioSet.RATIO_4_3,
+        AspectRatioSet.RATIO_OTHER,
+      ];
+    default:
+      return [
+        AspectRatioSet.RATIO_4_3,
+        AspectRatioSet.RATIO_16_9,
+        AspectRatioSet.RATIO_OTHER,
+      ];
+  }
 }
