fcb0a17d97f43cb0e2c8bdc2fcca314f18414f6e
Jose Dapena Paz <jdapena@igalia.com>
2024-01-12 08:06:10 +0000

    IWYU: missing include for unique_ptr use on push_notification_service.h
    Bug: 957519
    Change-Id: Ib6a0fbc8a97df4b969d9ff37f62d4ba459710d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190665
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1246322}

--- components/push_notification/push_notification_service.h
+++ components/push_notification/push_notification_service.h
@@ -5,6 +5,8 @@
 #ifndef COMPONENTS_PUSH_NOTIFICATION_PUSH_NOTIFICATION_SERVICE_H_
 #define COMPONENTS_PUSH_NOTIFICATION_PUSH_NOTIFICATION_SERVICE_H_
 
+#include <memory>
+
 #include "components/push_notification/push_notification_client_manager.h"
 
 namespace push_notification {
