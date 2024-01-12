8328793719b7e07681c6b05cc32b0d7ab24de36d
Vincent Boisselle <vincb@google.com>
2024-01-12 20:34:38 +0000

    [M121][ios] Use the IOS_BROWSER type for iOS user policy registration
    (cherry picked from commit 3b4f93c479dc3513e936a0f1bfeb30a3733ee89f)
    
    Bug: 1312263, b/319315090
    Change-Id: I8b7e2dbb81ad0fb3e6b60cb93a5cb739923cf73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5184976
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1245486}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190124
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/branch-heads/6167@{#1293}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- components/policy/core/browser/cloud/user_policy_signin_service_base.cc
+++ components/policy/core/browser/cloud/user_policy_signin_service_base.cc
@@ -32,12 +32,8 @@ namespace {
 const em::DeviceRegisterRequest::Type kCloudPolicyRegistrationType =
     em::DeviceRegisterRequest::ANDROID_BROWSER;
 #elif BUILDFLAG(IS_IOS)
-// TODO(crbug.com/1312263): Use em::DeviceRegisterRequest::IOS_BROWSER when
-// supported in the dmserver. The type for Desktop is temporarily used on iOS
-// to allow early testing of the feature before the DMServer can support iOS
-// User Policy.
 const em::DeviceRegisterRequest::Type kCloudPolicyRegistrationType =
-    em::DeviceRegisterRequest::BROWSER;
+    em::DeviceRegisterRequest::IOS_BROWSER;
 #else
 const em::DeviceRegisterRequest::Type kCloudPolicyRegistrationType =
     em::DeviceRegisterRequest::BROWSER;
