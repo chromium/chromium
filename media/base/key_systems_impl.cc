83c7497a4fb9c4fbbee2cbdf955e72bde2035eaf
Feras Aldahlawi <feras@chromium.org>
2024-01-13 02:25:45 +0000

    Move KeySystemsImpl class to its own file
    This is a refactoring CL to follow the common pattern of separating
    an interface from it's implementation.
    
    Bug: b/267698934
    Change-Id: I9768a283f5dcf3e53de009782a2740c7f4c75b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5173626
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Feras Aldahlawi <feras@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1246800}

--- media/base/key_systems_impl.cc
+++ media/base/key_systems_impl.cc
@@ -1,8 +1,8 @@
-// Copyright 2013 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/base/key_systems.h"
+#include "media/base/key_systems_impl.h"
 
 #include <stddef.h>
 
@@ -10,17 +10,13 @@
 #include <memory>
 #include <unordered_map>
 
-#include "base/callback_list.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
-#include "base/no_destructor.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
-#include "base/threading/thread_checker.h"
 #include "build/build_config.h"
 #include "media/base/eme_constants.h"
-#include "media/base/key_system_info.h"
 #include "media/base/key_system_names.h"
 #include "media/base/media.h"
 #include "media/base/media_client.h"
@@ -280,106 +276,7 @@ static bool CanBlock(const KeySystemInfo& key_system_info) {
 #endif
 }
 
-class KeySystemsImpl : public KeySystems {
- public:
-  static KeySystemsImpl* GetInstance();
-
-  KeySystemsImpl(const KeySystemsImpl&) = delete;
-  KeySystemsImpl& operator=(const KeySystemsImpl&) = delete;
-
-  // Implementation of KeySystems interface.
-  void UpdateIfNeeded(base::OnceClosure done_cb) override;
-  std::string GetBaseKeySystemName(
-      const std::string& key_system) const override;
-  bool IsSupportedKeySystem(const std::string& key_system) const override;
-  bool ShouldUseBaseKeySystemName(const std::string& key_system) const override;
-  bool CanUseAesDecryptor(const std::string& key_system) const override;
-  bool IsSupportedInitDataType(const std::string& key_system,
-                               EmeInitDataType init_data_type) const override;
-  EmeConfig::Rule GetEncryptionSchemeConfigRule(
-      const std::string& key_system,
-      EncryptionScheme encryption_scheme) const override;
-  EmeConfig::Rule GetContentTypeConfigRule(
-      const std::string& key_system,
-      EmeMediaType media_type,
-      const std::string& container_mime_type,
-      const std::vector<std::string>& codecs) const override;
-  EmeConfig::Rule GetRobustnessConfigRule(
-      const std::string& key_system,
-      EmeMediaType media_type,
-      const std::string& requested_robustness,
-      const bool* hw_secure_requirement) const override;
-  EmeConfig::Rule GetPersistentLicenseSessionSupport(
-      const std::string& key_system) const override;
-  EmeFeatureSupport GetPersistentStateSupport(
-      const std::string& key_system) const override;
-  EmeFeatureSupport GetDistinctiveIdentifierSupport(
-      const std::string& key_system) const override;
-
-  // These functions are for testing purpose only.
-  void AddCodecMaskForTesting(EmeMediaType media_type,
-                              const std::string& codec,
-                              uint32_t mask);
-  void AddMimeTypeCodecMaskForTesting(const std::string& mime_type,
-                                      uint32_t mask);
-  void ResetForTesting();
-
- private:
-  friend class base::NoDestructor<KeySystemsImpl>;
-
-  using MimeTypeToCodecsMap = std::unordered_map<std::string, SupportedCodecs>;
-  using CodecMap = std::unordered_map<std::string, EmeCodec>;
-  using InitDataTypesMap = std::unordered_map<std::string, EmeInitDataType>;
-
-  KeySystemsImpl();
-  ~KeySystemsImpl() override;
-
-  void Initialize();
-
-  void UpdateSupportedKeySystems();
-  void OnSupportedKeySystemsUpdated(KeySystemInfos key_systems);
-  void ProcessSupportedKeySystems(KeySystemInfos key_systems);
-
-  const KeySystemInfo* GetKeySystemInfo(const std::string& key_system) const;
-
-  void RegisterMimeType(const std::string& mime_type, SupportedCodecs codecs);
-  bool IsValidMimeTypeCodecsCombination(const std::string& mime_type,
-                                        SupportedCodecs codecs) const;
-
-  // TODO(crbug.com/417440): Separate container enum from codec mask value.
-  // Potentially pass EmeMediaType and a container enum.
-  SupportedCodecs GetCodecMaskForMimeType(
-      const std::string& container_mime_type) const;
-
-  // Converts a full `codec_string` (e.g. vp09.02.10.10) to an EmeCodec. Returns
-  // EME_CODEC_NONE is the |codec_string| is invalid or not supported by EME.
-  EmeCodec GetEmeCodecForString(EmeMediaType media_type,
-                                const std::string& container_mime_type,
-                                const std::string& codec_string) const;
-
-  // Whether the supported key systems are still up to date.
-  bool is_updating_ = false;
-
-  // Pending callbacks for UpdateIfNeeded() calls.
-  base::OnceClosureList update_callbacks_;
-
-  // Vector of KeySystemInfo .
-  KeySystemInfos key_system_info_vector_;
-
-  // This member should only be modified by RegisterMimeType().
-  MimeTypeToCodecsMap mime_type_to_codecs_map_;
-
-  // For unit test only.
-  CodecMap codec_map_for_testing_;
-
-  SupportedCodecs audio_codec_mask_ = EME_CODEC_AUDIO_ALL;
-  SupportedCodecs video_codec_mask_ = EME_CODEC_VIDEO_ALL;
-
-  // Makes sure all methods are called from the same thread.
-  base::ThreadChecker thread_checker_;
-
-  base::WeakPtrFactory<KeySystemsImpl> weak_factory_{this};
-};
+}  // namespace
 
 KeySystemsImpl* KeySystemsImpl::GetInstance() {
   static base::NoDestructor<KeySystemsImpl> key_systems;
@@ -819,7 +716,6 @@ EmeFeatureSupport KeySystemsImpl::GetDistinctiveIdentifierSupport(
   return key_system_info->GetDistinctiveIdentifierSupport();
 }
 
-}  // namespace
 
 KeySystems* KeySystems::GetInstance() {
   return KeySystemsImpl::GetInstance();
