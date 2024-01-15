45f74fc0b2018c6a4fa25ad02a03e666d4cf2efe
Olivier Flückiger <olivf@chromium.org>
2024-01-15 17:03:50 +0000

    gin: explicitly set thread policy
    We need to explicitly set the thread policy to avoid running into
    `DCHECK(transaction.traits().thread_policy_set_explicitly())` in
    `base/task/thread_pool/thread_pool_impl.cc` when updating the priority.
    
    This explicitly initializes the policy to the default value.
    
    Bug: chromium:1514341
    Change-Id: Iee41103f8f5e7514db7fbce27cc03ed04e88e71b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5191990
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247221}

--- gin/v8_platform.cc
+++ gin/v8_platform.cc
@@ -420,20 +420,21 @@ std::unique_ptr<v8::JobHandle> V8Platform::CreateJobImpl(
   // Ownership of |job_task| is assumed by |worker_task|, while
   // |max_concurrency_callback| uses an unretained pointer.
   auto* job_task_ptr = job_task.get();
-  auto handle =
-      base::CreateJob(ToBaseLocation(location), {ToBaseTaskPriority(priority)},
-                      base::BindRepeating(
-                          [](const std::unique_ptr<v8::JobTask>& job_task,
-                             base::JobDelegate* delegate) {
-                            JobDelegateImpl delegate_impl(delegate);
-                            job_task->Run(&delegate_impl);
-                          },
-                          std::move(job_task)),
-                      base::BindRepeating(
-                          [](v8::JobTask* job_task, size_t worker_count) {
-                            return job_task->GetMaxConcurrency(worker_count);
-                          },
-                          base::Unretained(job_task_ptr)));
+  auto handle = base::CreateJob(
+      ToBaseLocation(location),
+      {ToBaseTaskPriority(priority), base::ThreadPolicy::PREFER_BACKGROUND},
+      base::BindRepeating(
+          [](const std::unique_ptr<v8::JobTask>& job_task,
+             base::JobDelegate* delegate) {
+            JobDelegateImpl delegate_impl(delegate);
+            job_task->Run(&delegate_impl);
+          },
+          std::move(job_task)),
+      base::BindRepeating(
+          [](v8::JobTask* job_task, size_t worker_count) {
+            return job_task->GetMaxConcurrency(worker_count);
+          },
+          base::Unretained(job_task_ptr)));
 
   return std::make_unique<JobHandleImpl>(std::move(handle));
 }
