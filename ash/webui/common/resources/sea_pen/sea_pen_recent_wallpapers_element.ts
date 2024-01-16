774b807a70e873fbcfd0f37ff85ec7d3cfdad4c6
Jason Thai <jasontt@chromium.org>
2024-01-16 21:04:43 +0000

    p13n: recently used menu icons
    https://screenshot.googleplex.com/9BmtEBtUsWwRjnD
    
    BUG=b:317281058
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I38eb1cd39a70bc6e4ff924405e001c0d85e883d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5193173
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247720}

--- ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
@@ -10,7 +10,9 @@
 import 'chrome://resources/ash/common/personalization/common.css.js';
 import 'chrome://resources/ash/common/personalization/wallpaper.css.js';
 import 'chrome://resources/ash/common/sea_pen/sea_pen.css.js';
+import 'chrome://resources/ash/common/sea_pen/sea_pen_icons.html.js';
 import 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 import {AnchorAlignment} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {assert} from 'chrome://resources/js/assert.js';
@@ -20,11 +22,11 @@ import {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
 import {WallpaperGridItemSelectedEvent} from '../personalization/wallpaper_grid_item_element.js';
 
 import {RecentSeaPenData} from './constants.js';
-import {isImageDataUrl, isNonEmptyArray, isNonEmptyFilePath} from './sea_pen_utils.js';
 import {deleteRecentSeaPenImage, fetchRecentSeaPenData, selectRecentSeaPenImage} from './sea_pen_controller.js';
 import {getSeaPenProvider} from './sea_pen_interface_provider.js';
 import {getTemplate} from './sea_pen_recent_wallpapers_element.html.js';
 import {WithSeaPenStore} from './sea_pen_store.js';
+import {isImageDataUrl, isNonEmptyArray, isNonEmptyFilePath} from './sea_pen_utils.js';
 
 export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
   static get is() {
