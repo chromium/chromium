ee28fdaf692add2fd6114a0167c9b1df5ee718c1
Anton Bershanskyi <bershanskyi@gmail.com>
2024-01-16 22:11:20 +0000

    [Extensions] Check that manifest.json devtools_page exists
    Bug: 1514855
    Change-Id: I2464c335644334fa3913147a7b3e3e9e44562658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5151172
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1247774}

--- chrome/common/extensions/chrome_manifest_url_handlers.cc
+++ chrome/common/extensions/chrome_manifest_url_handlers.cc
@@ -83,6 +83,23 @@ base::span<const char* const> DevToolsPageHandler::Keys() const {
   return kKeys;
 }
 
+bool DevToolsPageHandler::Validate(
+    const Extension* extension,
+    std::string* error,
+    std::vector<InstallWarning>* warnings) const {
+  const GURL& url = chrome_manifest_urls::GetDevToolsPage(extension);
+  const base::FilePath relative_path =
+      file_util::ExtensionURLToRelativeFilePath(url);
+  const base::FilePath resource_path =
+      extension->GetResource(relative_path).GetFilePath();
+  if (resource_path.empty() || !base::PathExists(resource_path)) {
+    const std::string message = ErrorUtils::FormatErrorMessage(
+        errors::kFileNotFound, relative_path.AsUTF8Unsafe());
+    warnings->emplace_back(message, keys::kDevToolsPage);
+  }
+  return true;
+}
+
 URLOverridesHandler::URLOverridesHandler() = default;
 URLOverridesHandler::~URLOverridesHandler() = default;
 
