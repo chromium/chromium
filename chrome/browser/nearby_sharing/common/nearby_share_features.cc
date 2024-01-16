00b7b90e13d46abb5947b13d540a9b8cd9939c11
Daniel Classon <dclasson@google.com>
2024-01-16 18:31:56 +0000

    [Nearby Share][Self Share] Enable Self Share flag by default
    Enables Self Share flag by default as part of the Self Share rollout.
    
    Test: Unit tests.
    Bug: b/314371388
    Change-Id: Ie658882e5caf13c3634a9f8f47ff76bb28987199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5200761
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1247613}

--- chrome/browser/nearby_sharing/common/nearby_share_features.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_features.cc
@@ -31,7 +31,7 @@ BASE_FEATURE(kNearbySharingOnePageOnboarding,
 // own devices.
 BASE_FEATURE(kNearbySharingSelfShare,
              "NearbySharingSelfShare",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables use of WebRTC in Nearby Share.
 BASE_FEATURE(kNearbySharingWebRtc,
