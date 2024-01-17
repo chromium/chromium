353474394af2c5f742df04b3a41fa0f23c63e516
James Lee <ljjlee@google.com>
2024-01-17 16:07:49 +0000

    [MERGE] Fix formatting
    (cherry picked from commit 1604ee48e930368d65631e3ac121a9f51b419f39)
    
    Bug: b/317853352, 1518728
    Change-Id: I6765dd98e6c46fde805f35fbd8b0706c6788f543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5151692
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1241142}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5205690
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6167@{#1457}
    Cr-Branched-From: 222e786949e76e342d325ea0d008b4b6273f3a89-refs/heads/main@{#1233107}

--- chrome/browser/metrics/family_link_user_metrics_provider.cc
+++ chrome/browser/metrics/family_link_user_metrics_provider.cc
@@ -21,27 +21,26 @@ FamilyLinkUserMetricsProvider::~FamilyLinkUserMetricsProvider() = default;
 bool FamilyLinkUserMetricsProvider::ProvideHistograms() {
   // This function is called at unpredictable intervals throughout the Chrome
   // session, so guarantee it will never crash.
-
-    ProfileManager* profile_manager = g_browser_process->profile_manager();
-    std::vector<Profile*> profile_list = profile_manager->GetLoadedProfiles();
-    std::vector<AccountInfo> primary_accounts;
-    for (Profile* profile : profile_list) {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  std::vector<Profile*> profile_list = profile_manager->GetLoadedProfiles();
+  std::vector<AccountInfo> primary_accounts;
+  for (Profile* profile : profile_list) {
 #if !BUILDFLAG(IS_ANDROID)
-      // TODO(b/274889379): Mock call to GetBrowserCount().
-      if (!FamilyLinkUserMetricsProvider::
-              skip_active_browser_count_for_unittesting_ &&
-          chrome::GetBrowserCount(profile) == 0) {
-        // The profile is loaded, but there's no opened browser for this
-        // profile.
-        continue;
-      }
-#endif
-      signin::IdentityManager* identity_manager_ =
-          IdentityManagerFactory::GetForProfile(profile);
-      AccountInfo account_info = identity_manager_->FindExtendedAccountInfo(
-          identity_manager_->GetPrimaryAccountInfo(
-              signin::ConsentLevel::kSignin));
-      primary_accounts.push_back(std::move(account_info));
+    // TODO(b/274889379): Mock call to GetBrowserCount().
+    if (!FamilyLinkUserMetricsProvider::
+            skip_active_browser_count_for_unittesting_ &&
+        chrome::GetBrowserCount(profile) == 0) {
+      // The profile is loaded, but there's no opened browser for this
+      // profile.
+      continue;
     }
-    return supervised_user::EmitLogSegmentHistogram(primary_accounts);
+#endif
+    signin::IdentityManager* identity_manager_ =
+        IdentityManagerFactory::GetForProfile(profile);
+    AccountInfo account_info = identity_manager_->FindExtendedAccountInfo(
+        identity_manager_->GetPrimaryAccountInfo(
+            signin::ConsentLevel::kSignin));
+    primary_accounts.push_back(std::move(account_info));
+  }
+  return supervised_user::EmitLogSegmentHistogram(primary_accounts);
 }
