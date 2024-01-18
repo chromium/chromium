df4f86b86f2387d7a0c11852b3f2f0e2e65e2551
Nan Lin <linnan@chromium.org>
2024-01-18 16:13:05 +0000

    Add override feature to runtime enable cross app web Attribution Reporting
    This CL adds a flag which will be used to move the feature out of OT and
    ship.
    
    Bug: 1518936
    Change-Id: Icf05d16ab5895fefefe4671c33f92c8c03b5eb71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5202347
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248803}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -382,6 +382,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {"AttributionReportingCrossAppWeb",
            raw_ref(features::kPrivacySandboxAdsAPIsOverride),
            kSetOnlyIfOverridden},
+          {"AttributionReportingCrossAppWeb",
+           raw_ref(features::kAttributionReportingCrossAppWebOverride)},
           {"AndroidDownloadableFontsMatching",
            raw_ref(features::kAndroidDownloadableFontsMatching)},
 #if BUILDFLAG(IS_ANDROID)
@@ -704,6 +706,21 @@ void ResolveInvalidConfigurations() {
         << attribution_reporting::features::kConversionMeasurement.name
         << " in addition.";
     WebRuntimeFeatures::EnableAttributionReporting(false);
+  }
+
+  if (!base::FeatureList::IsEnabled(
+          attribution_reporting::features::kConversionMeasurement) ||
+      !base::FeatureList::IsEnabled(
+          network::features::kAttributionReportingCrossAppWeb)) {
+    LOG_IF(WARNING,
+           WebRuntimeFeatures::IsAttributionReportingCrossAppWebEnabled())
+        << "AttributionReportingCrossAppWeb cannot be enabled in this "
+           "configuration. Use --"
+        << switches::kEnableFeatures << "="
+        << attribution_reporting::features::kConversionMeasurement.name << ","
+        << network::features::kAttributionReportingCrossAppWeb.name
+        << " in addition.";
+    WebRuntimeFeatures::EnableAttributionReportingCrossAppWeb(false);
   }
 
   if (!base::FeatureList::IsEnabled(blink::features::kInterestGroupStorage)) {
