d1c4e1613de833132cd040641b3b506c3298f108
Asami Doi <asamidoi@chromium.org>
2024-01-18 10:48:36 +0000

    [ios] Add unit tests for SnapshotGenerator
    This CL adds unit tests for SnapshotGenerator. There are 2 classes for
    testing; one is for a page without an overlay and the other is for a
    page with an overlay. The snapshot is always overridden by overlays if
    they exist.
    
    Change-Id: I838ef18d6d711a6074f02e26427adbe38c9396c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5190295
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248678}

---
+++ ios/chrome/browser/snapshots/model/snapshot_generator_unittest.mm
@@ -0,0 +1,163 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/snapshots/model/snapshot_generator.h"
+
+#import <UIKit/UIKit.h>
+
+#import "base/run_loop.h"
+#import "ios/chrome/browser/shared/ui/util/image/image_util.h"
+#import "ios/chrome/browser/snapshots/model/fake_snapshot_generator_delegate.h"
+#import "ios/chrome/browser/snapshots/model/snapshot_tab_helper.h"
+#import "ios/web/public/test/fakes/fake_web_state.h"
+#import "ios/web/public/test/web_task_environment.h"
+#import "testing/platform_test.h"
+#import "ui/base/test/ios/ui_image_test_utils.h"
+#import "ui/gfx/image/image.h"
+
+namespace {
+
+// Dimension of the WebState's view.
+constexpr CGSize kWebStateViewSize = {300, 400};
+
+// Returns whether the `image` dominant color is `color`.
+bool IsDominantColorForImage(UIImage* image, UIColor* color) {
+  return
+      [color isEqual:DominantColorForImage(gfx::Image(image), /*opacity=*/1.0)];
+}
+
+// Overrides `TakeSnapshot()` to return a placeholder UIImage.
+class FakeWebStateWithSnapshot : public web::FakeWebState {
+  void TakeSnapshot(const CGRect rect, SnapshotCallback callback) override {
+    std::move(callback).Run(
+        ui::test::uiimage_utils::UIImageWithSizeAndSolidColor(
+            kWebStateViewSize, [UIColor blueColor]));
+  }
+};
+
+}  // namespace
+
+class SnapshotGeneratorTest : public PlatformTest {
+ public:
+  SnapshotGeneratorTest() {
+    // Create the SnapshotGenerator with a fake delegate.
+    delegate_ = [[FakeSnapshotGeneratorDelegate alloc] init];
+    generator_ = [[SnapshotGenerator alloc] initWithWebState:&web_state_];
+    generator_.delegate = delegate_;
+
+    // Add a base view to the web state.
+    CGRect frame = {CGPointZero, kWebStateViewSize};
+    UIView* view = [[UIView alloc] initWithFrame:frame];
+    view.backgroundColor = [UIColor redColor];
+    delegate_.view = view;
+  }
+
+ protected:
+  web::WebTaskEnvironment task_environment_;
+  SnapshotGenerator* generator_ = nil;
+  FakeSnapshotGeneratorDelegate* delegate_ = nil;
+  FakeWebStateWithSnapshot web_state_;
+};
+
+// Tests the snapshot taken by UIKit-based API.
+TEST_F(SnapshotGeneratorTest, GenerateUIViewSnapshot) {
+  UIImage* snapshot = [generator_ generateUIViewSnapshot];
+
+  ASSERT_TRUE(snapshot);
+  EXPECT_TRUE(CGSizeEqualToSize(snapshot.size, kWebStateViewSize));
+  EXPECT_TRUE(IsDominantColorForImage(snapshot, [UIColor redColor]));
+}
+
+// Tests the snapshot taken by WebKit-based API.
+TEST_F(SnapshotGeneratorTest, GenerateWebViewSnapshot) {
+  // Enable the flag to take a snapshot with WebKit-based API.
+  web_state_.SetCanTakeSnapshot(true);
+
+  base::RunLoop run_loop;
+  base::RunLoop* run_loop_ptr = &run_loop;
+
+  __block UIImage* snapshot = nil;
+  [generator_ generateSnapshotWithCompletion:^(UIImage* image) {
+    snapshot = image;
+    run_loop_ptr->Quit();
+  }];
+
+  run_loop.Run();
+
+  ASSERT_TRUE(snapshot);
+  EXPECT_TRUE(CGSizeEqualToSize(snapshot.size, kWebStateViewSize));
+  EXPECT_TRUE(IsDominantColorForImage(snapshot, [UIColor blueColor]));
+}
+
+@interface FakeOverlaysSnapshotGeneratorDelegate : FakeSnapshotGeneratorDelegate
+@end
+
+@implementation FakeOverlaysSnapshotGeneratorDelegate
+
+#pragma mark - SnapshotGeneratorDelegate
+
+- (NSArray<UIView*>*)snapshotGenerator:(SnapshotGenerator*)snapshotGenerator
+           snapshotOverlaysForWebState:(web::WebState*)webState {
+  CGRect frame = {CGPointZero, kWebStateViewSize};
+  UIView* overlay = [[UIView alloc] initWithFrame:frame];
+  overlay.backgroundColor = [UIColor greenColor];
+  return @[ overlay ];
+}
+
+@end
+
+class SnapshotGeneratorWithOverlaysTest : public PlatformTest {
+ public:
+  SnapshotGeneratorWithOverlaysTest() {
+    // Create the SnapshotGenerator with a fake delegate. The fake delegate
+    // returns an overlay.
+    delegate_ = [[FakeOverlaysSnapshotGeneratorDelegate alloc] init];
+    generator_ = [[SnapshotGenerator alloc] initWithWebState:&web_state_];
+    generator_.delegate = delegate_;
+
+    // Add a base view to the web state.
+    CGRect frame = {CGPointZero, kWebStateViewSize};
+    UIView* view = [[UIView alloc] initWithFrame:frame];
+    view.backgroundColor = [UIColor redColor];
+    delegate_.view = view;
+  }
+
+ protected:
+  web::WebTaskEnvironment task_environment_;
+  SnapshotGenerator* generator_ = nil;
+  FakeOverlaysSnapshotGeneratorDelegate* delegate_ = nil;
+  FakeWebStateWithSnapshot web_state_;
+};
+
+// Tests the snapshot taken by UIKit-based API. The page has an overlay.
+TEST_F(SnapshotGeneratorWithOverlaysTest, GenerateUIViewSnapshot) {
+  UIImage* snapshot = [generator_ generateUIViewSnapshotWithOverlays];
+
+  // The base color of UIView is red, but it's overriden by an overlay.
+  ASSERT_TRUE(snapshot);
+  EXPECT_TRUE(CGSizeEqualToSize(snapshot.size, kWebStateViewSize));
+  EXPECT_TRUE(IsDominantColorForImage(snapshot, [UIColor greenColor]));
+}
+
+// Tests the snapshot taken by UIKit-based API. The page has an overlay.
+TEST_F(SnapshotGeneratorWithOverlaysTest, GenerateWebViewSnapshot) {
+  // Enable the flag to take a snapshot with WebKit-based API.
+  web_state_.SetCanTakeSnapshot(true);
+
+  base::RunLoop run_loop;
+  base::RunLoop* run_loop_ptr = &run_loop;
+
+  __block UIImage* snapshot = nil;
+  [generator_ generateSnapshotWithCompletion:^(UIImage* image) {
+    snapshot = image;
+    run_loop_ptr->Quit();
+  }];
+
+  run_loop.Run();
+
+  // The color should be green because there is an overlay filled with green.
+  ASSERT_TRUE(snapshot);
+  EXPECT_TRUE(CGSizeEqualToSize(snapshot.size, kWebStateViewSize));
+  EXPECT_TRUE(IsDominantColorForImage(snapshot, [UIColor greenColor]));
+}
