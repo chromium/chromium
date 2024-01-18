8dde4374a2812a64db45eebcbb5a512ab5e34fb5
Marc Treib <treib@chromium.org>
2024-01-18 15:10:24 +0000

    Bookmarks: Make all pref names `constexpr`
    This will allow using them in `consteval` contexts, specifically
    `MakeFixedFlatMap()` - see linked bug for more context.
    (It's also arguably preferable anyway - they're now all in one place,
    the header, rather than split across .h and .cc.)
    
    Bug: 1512537
    Change-Id: Ie600ed80db96d42c8b43a393701b251285741095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210275
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248762}

--- components/bookmarks/common/bookmark_pref_names.h
+++ components/bookmarks/common/bookmark_pref_names.h
@@ -7,19 +7,36 @@
 #ifndef COMPONENTS_BOOKMARKS_COMMON_BOOKMARK_PREF_NAMES_H_
 #define COMPONENTS_BOOKMARKS_COMMON_BOOKMARK_PREF_NAMES_H_
 
-namespace bookmarks {
-namespace prefs {
+namespace bookmarks::prefs {
 
-extern const char kAddedBookmarkSincePowerBookmarksLaunch[];
-extern const char kBookmarkEditorExpandedNodes[];
-extern const char kEditBookmarksEnabled[];
-extern const char kManagedBookmarks[];
-extern const char kManagedBookmarksFolderName[];
-extern const char kShowAppsShortcutInBookmarkBar[];
-extern const char kShowManagedBookmarksInBookmarkBar[];
-extern const char kShowBookmarkBar[];
+// Boolean which specifies whether the user has added any new bookmarks
+// following the launch of the power bookmarks feature.
+inline constexpr char kAddedBookmarkSincePowerBookmarksLaunch[] =
+    "bookmarks.added_since_power_bookmarks_launch";
+// Boolean which specifies the ids of the bookmark nodes that are expanded in
+// the bookmark editor.
+inline constexpr char kBookmarkEditorExpandedNodes[] =
+    "bookmark_editor.expanded_nodes";
+// Modifying bookmarks is completely disabled when this is set to false.
+inline constexpr char kEditBookmarksEnabled[] = "bookmarks.editing_enabled";
+// A list of bookmarks to include in a Managed Bookmarks root node. Each
+// list item is a dictionary containing a "name" and an "url" entry, detailing
+// the bookmark name and target URL respectively.
+inline constexpr char kManagedBookmarks[] = "bookmarks.managed_bookmarks";
+// String which specifies the Managed Bookmarks folder name
+inline constexpr char kManagedBookmarksFolderName[] =
+    "bookmarks.managed_bookmarks_folder_name";
+// Boolean which specifies whether the apps shortcut is visible on the bookmark
+// bar.
+inline constexpr char kShowAppsShortcutInBookmarkBar[] =
+    "bookmark_bar.show_apps_shortcut";
+// Boolean which specifies whether the Managed Bookmarks folder is visible on
+// the bookmark bar.
+inline constexpr char kShowManagedBookmarksInBookmarkBar[] =
+    "bookmark_bar.show_managed_bookmarks";
+// Boolean which specifies whether the bookmark bar is visible on all tabs.
+inline constexpr char kShowBookmarkBar[] = "bookmark_bar.show_on_all_tabs";
 
-}  // namespace prefs
-}  // namespace bookmarks
+}  // namespace bookmarks::prefs
 
 #endif  // COMPONENTS_BOOKMARKS_COMMON_BOOKMARK_PREF_NAMES_H_
