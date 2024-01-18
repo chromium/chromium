d6a9fb595226b61cee501df9384fe952068acb10
Marc Treib <treib@chromium.org>
2024-01-18 18:26:28 +0000

    BrowsingData: Make all pref names `constexpr`
    This will allow using them in `consteval` contexts, specifically
    `MakeFixedFlatMap()` - see linked bug for more context.
    (It's also arguably preferable anyway - they're now all in one place,
    the header, rather than split across .h and .cc.)
    
    Bug: 1512537
    Change-Id: I7dfcfda19527114ad9ad5bc895e56c239e1e5bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5210595
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1248908}

--- components/browsing_data/core/pref_names.h
+++ components/browsing_data/core/pref_names.h
@@ -9,43 +9,72 @@ namespace user_prefs {
 class PrefRegistrySyncable;
 }
 
-namespace browsing_data {
+namespace browsing_data::prefs {
 
-namespace prefs {
+// JSON config to periodically delete some browsing data as specified by
+// the BrowsingDataLifetime policy.
+inline constexpr char kBrowsingDataLifetime[] =
+    "browser.clear_data.browsing_data_lifetime";
 
-extern const char kBrowsingDataLifetime[];
-extern const char kClearBrowsingDataOnExitDeletionPending[];
-extern const char kClearBrowsingDataOnExitList[];
+// Boolean set to true while browsing data needs to be deleted per
+// ClearBrowsingDataOnExit policy.
+// TODO (crbug/1026442): Consider setting this pref to true during fast
+// shutdown if the ClearBrowsingDataOnExit policy is set.
+inline constexpr char kClearBrowsingDataOnExitDeletionPending[] =
+    "browser.clear_data.clear_on_exit_pending";
 
-extern const char kDeleteTimePeriod[];
-extern const char kDeleteTimePeriodBasic[];
+// List of browsing data, specified by the ClearBrowsingDataOnExit policy, to
+// delete just before browser shutdown.
+inline constexpr char kClearBrowsingDataOnExitList[] =
+    "browser.clear_data.clear_on_exit";
 
-extern const char kDeleteTimePeriodV2[];
-extern const char kDeleteTimePeriodV2Basic[];
+// Clear browsing data deletion time period.
+inline constexpr char kDeleteTimePeriod[] = "browser.clear_data.time_period";
+inline constexpr char kDeleteTimePeriodBasic[] =
+    "browser.clear_data.time_period_basic";
 
-extern const char kDeleteBrowsingHistory[];
-extern const char kDeleteBrowsingHistoryBasic[];
-extern const char kDeleteDownloadHistory[];
-extern const char kDeleteCache[];
-extern const char kDeleteCacheBasic[];
-extern const char kDeleteCookies[];
-extern const char kDeleteCookiesBasic[];
-extern const char kDeletePasswords[];
-extern const char kDeleteFormData[];
-extern const char kDeleteHostedAppsData[];
-extern const char kDeleteSiteSettings[];
+// Clear browsing data deletion time period experiment. This experiment requires
+// users to interact with timeframe drop down menu in the clear browsing data
+// dialog. It also adds a new 'Last 15 minutes' value to the list. Until the
+// user has made their 1st time period selection, the UI shows 'Select a time
+// range'.
+inline constexpr char kDeleteTimePeriodV2[] =
+    "browser.clear_data.time_period_v2";
+inline constexpr char kDeleteTimePeriodV2Basic[] =
+    "browser.clear_data.time_period_v2_basic";
 
-extern const char kLastClearBrowsingDataTime[];
-extern const char kClearBrowsingDataHistoryNoticeShownTimes[];
+// Clear Browsing Data dialog datatype preferences.
+inline constexpr char kDeleteBrowsingHistory[] =
+    "browser.clear_data.browsing_history";
+inline constexpr char kDeleteBrowsingHistoryBasic[] =
+    "browser.clear_data.browsing_history_basic";
+inline constexpr char kDeleteDownloadHistory[] =
+    "browser.clear_data.download_history";
+inline constexpr char kDeleteCache[] = "browser.clear_data.cache";
+inline constexpr char kDeleteCacheBasic[] = "browser.clear_data.cache_basic";
+inline constexpr char kDeleteCookies[] = "browser.clear_data.cookies";
+inline constexpr char kDeleteCookiesBasic[] =
+    "browser.clear_data.cookies_basic";
+inline constexpr char kDeletePasswords[] = "browser.clear_data.passwords";
+inline constexpr char kDeleteFormData[] = "browser.clear_data.form_data";
+inline constexpr char kDeleteHostedAppsData[] =
+    "browser.clear_data.hosted_apps_data";
+inline constexpr char kDeleteSiteSettings[] =
+    "browser.clear_data.site_settings";
 
-extern const char kLastClearBrowsingDataTab[];
-extern const char kPreferencesMigratedToBasic[];
+// Other Clear Browsing Data preferences.
+inline constexpr char kLastClearBrowsingDataTime[] =
+    "browser.last_clear_browsing_data_time";
+inline constexpr char kClearBrowsingDataHistoryNoticeShownTimes[] =
+    "browser.clear_data.history_notice_shown_times";
+inline constexpr char kLastClearBrowsingDataTab[] =
+    "browser.last_clear_browsing_data_tab";
+inline constexpr char kPreferencesMigratedToBasic[] =
+    "browser.clear_data.preferences_migrated_to_basic";
 
 // Registers the Clear Browsing Data UI prefs.
 void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry);
 
-}  // namespace prefs
-
-}  // namespace browsing_data
+}  // namespace browsing_data::prefs
 
 #endif  // COMPONENTS_BROWSING_DATA_CORE_PREF_NAMES_H_
