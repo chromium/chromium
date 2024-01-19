70a3b2f72c55b47a895d40725b0366fe11b31b54
Saifuddin Hitawala <hitawala@chromium.org>
2024-01-19 18:13:24 +0000

    Multiplanar: Enable hardware video decode with multiplanar SI on Linux
    UMA data looks good iwth 50% finch stable on Linux:
    https://uma.googleplex.com/p/chrome/variations?sid=adcced7f3bc7ea03474105a6d025291c
    
    Enable UseMultiPlaneFormatForHardwareVideo on Linux.
    
    Bug: 1366495
    Change-Id: Iaa574b21287349f96b56b91e20a90d7be81d21dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5213126
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1249399}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -604,7 +604,8 @@ BASE_FEATURE(kUseWritePixelsYUV,
 // hardware video decoders.
 BASE_FEATURE(kUseMultiPlaneFormatForHardwareVideo,
              "UseMultiPlaneFormatForHardwareVideo",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_FUCHSIA)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_FUCHSIA) || \
+    BUILDFLAG(IS_LINUX)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
