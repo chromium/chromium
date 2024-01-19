e749c7221383564d65c21c55f1030f80fdbdf98f
Etienne Pierre-Doray <etiennep@chromium.org>
2024-01-19 20:40:06 +0000

    [ETW] Add an escape hatch to disable ETW exports
    A spike in crash suggests ETW Chrome GUID is being enabled we have no
    control over otherwise. This CL controls enabling ETW exports with
    a base feature, disabled by default until we investigate the root
    cause of the crash.
    
    Bug: 1519796
    Change-Id: I3c82612fda869db120a1739a6cd517bfaffdc324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216804
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/6258@{#7}
    Cr-Branched-From: 5426893f247cf914e7ef0eb4e4a0d3c76e613ff1-refs/heads/main@{#1249342}

--- components/tracing/common/etw_export_win.cc
+++ components/tracing/common/etw_export_win.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "base/trace_event/etw_interceptor_win.h"
 #include "base/trace_event/trace_event_etw_export_win.h"
@@ -17,6 +18,10 @@
 
 namespace tracing {
 namespace {
+
+BASE_FEATURE(kEnableEtwExports,
+             "EnableEtwExports",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Used to protect the upper 16 bits reserved by winmeta.xml as they
 // should not be used but older logging code and tools incorrectly used
@@ -116,6 +121,9 @@ void ETWExportController::OnUpdate(TlmProvider::EventControlCode event) {
 
 // static
 void EnableETWExport() {
+  if (!base::FeatureList::IsEnabled(kEnableEtwExports)) {
+    return;
+  }
   static base::NoDestructor<ETWExportController> instance{};
 }
 
