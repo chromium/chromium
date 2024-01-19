3def21f58ea33cd79320a51ad623493849617da9
Simon Ziegltrum <ziegltrum@google.com>
2024-01-19 10:21:58 +0000

    [OOBE] consumer_update TS migration
    Bug: b/314762334
    Change-Id: I0413c00242733b7e0fb67dbfb9c7188636b19ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214045
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249293}

--- chrome/browser/resources/chromeos/login/screens/oobe/consumer_update.ts
+++ chrome/browser/resources/chromeos/login/screens/oobe/consumer_update.ts
@@ -9,7 +9,7 @@
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import '//resources/polymer/v3_0/paper-progress/paper-progress.js';
 import '//resources/polymer/v3_0/paper-styles/color.js';
-import '../../components/oobe_cr_lottie.js';
+import {OobeCrLottie} from '../../components/oobe_cr_lottie.js';
 import '../../components/oobe_icons.html.js';
 import '../../components/buttons/oobe_back_button.js';
 import '../../components/buttons/oobe_next_button.js';
@@ -19,6 +19,7 @@ import '../../components/dialogs/oobe_loading_dialog.js';
 import '../../components/oobe_carousel.js';
 import '../../components/oobe_slide.js';
 
+import {PolymerElementProperties} from '//resources/polymer/v3_0/polymer/interfaces.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {LoginScreenBehavior, LoginScreenBehaviorInterface} from '../../components/behaviors/login_screen_behavior.js';
@@ -30,13 +31,14 @@ import {getTemplate} from './consumer_update.html.js';
 
 /**
  * @constructor
- * @extends {PolymerElement}
- * @implements {LoginScreenBehaviorInterface}
- * @implements {OobeI18nBehaviorInterface}
- * @implements {MultiStepBehaviorInterface}
  */
-const ConsumerUpdateScreenElementBase = mixinBehaviors(
-    [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior], PolymerElement);
+const ConsumerUpdateScreenElementBase =
+    mixinBehaviors(
+        [OobeI18nBehavior, LoginScreenBehavior, MultiStepBehavior],
+        PolymerElement) as {
+      new (): PolymerElement & OobeI18nBehaviorInterface
+      & LoginScreenBehaviorInterface & MultiStepBehaviorInterface,
+    };
 
 const UNREACHABLE_PERCENT = 1000;
 // Thresholds which are used to determine when update status announcement should
@@ -61,49 +63,39 @@ const PERCENT_THRESHOLDS = [
 
 /**
  * Enum to represent steps on the consumer update screen.
- * @enum {string}
  */
-const ConsumerUpdateStep = {
-  CHECKING: 'checking',
-  UPDATE: 'update',
-  RESTART: 'restart',
-  REBOOT: 'reboot',
-  CELLULAR: 'cellular',
-};
+enum ConsumerUpdateStep {
+  CHECKING = 'checking',
+  UPDATE = 'update',
+  RESTART = 'restart',
+  REBOOT = 'reboot',
+  CELLULAR = 'cellular',
+}
 
 
 /**
  * Available user actions.
- * @enum {string}
  */
-const UserAction = {
-  BACK: 'back',
-  SKIP: 'skip-consumer-update',
-  DECLINE_CELLULAR: 'consumer-update-reject-cellular',
-  ACCEPT_CELLULAR: 'consumer-update-accept-cellular',
-};
-
-/**
- * @typedef {{
- *   betterUpdatePercent:  HTMLDivElement,
- *   betterUpdateTimeleft:  HTMLDivElement,
- * }}
- */
-ConsumerUpdateScreenElementBase.$;
+enum UserAction {
+  BACK = 'back',
+  SKIP = 'skip-consumer-update',
+  DECLINE_CELLULAR = 'consumer-update-reject-cellular',
+  ACCEPT_CELLULAR = 'consumer-update-accept-cellular',
+}
 
 /**
  * @polymer
  */
 class ConsumerUpdateScreen extends ConsumerUpdateScreenElementBase {
   static get is() {
-    return 'consumer-update-element';
+    return 'consumer-update-element' as const;
   }
 
-  static get template() {
+  static get template(): HTMLTemplateElement {
     return getTemplate();
   }
 
-  static get properties() {
+  static get properties(): PolymerElementProperties {
     return {
       /**
        * True if update is forced.
@@ -165,11 +157,19 @@ class ConsumerUpdateScreen extends ConsumerUpdateScreenElementBase {
     };
   }
 
-  static get observers() {
-    return ['playAnimation_(uiStep)'];
+  private isUpdateMandatory: boolean;
+  private showLowBatteryWarning: boolean;
+  private updateStatusMessagePercent: string;
+  private updateStatusMessageTimeLeft: string;
+  private betterUpdateProgressValue: number;
+  private autoTransition: boolean;
+  private thresholdIndex: number;
+
+  static get observers(): string[] {
+    return ['playAnimation(uiStep)'];
   }
 
-  get EXTERNAL_API() {
+  override get EXTERNAL_API(): string[] {
     return [
       'setIsUpdateMandatory',
       'showLowBatteryWarningMessage',
@@ -179,73 +179,75 @@ class ConsumerUpdateScreen extends ConsumerUpdateScreenElementBase {
     ];
   }
 
-  get UI_STEPS() {
+  override get UI_STEPS() {
     return ConsumerUpdateStep;
   }
 
-  defaultUIStep() {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override defaultUIStep() {
     return ConsumerUpdateStep.CHECKING;
   }
 
-  /** @override */
-  ready() {
+  override ready() {
     super.ready();
     this.initializeLoginScreen('ConsumerUpdateScreen');
   }
 
-  getOobeUIInitialState() {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  override getOobeUIInitialState() {
     return OOBE_UI_STATE.ONBOARDING;
   }
 
   /**
    * Shows or hides skip button while update in progress.
-   * @param {boolean} visible Is skip button visible?
+   * @param visible Is skip button visible?
    */
-  setIsUpdateMandatory(visible) {
+  setIsUpdateMandatory(visible: boolean): void {
     this.isUpdateMandatory = visible;
   }
 
   /**
    * Decline to use cellular data.
    */
-  onDeclineCellularClicked_() {
+  private onDeclineCellularClicked(): void {
     this.userActed(UserAction.DECLINE_CELLULAR);
   }
 
   /**
    * Accept to use cellular data.
    */
-  onAcceptCellularClicked_() {
+  private onAcceptCellularClicked(): void {
     this.userActed(UserAction.ACCEPT_CELLULAR);
   }
 
-  onSkip_() {
+  private onSkip(): void {
     this.userActed(UserAction.SKIP);
   }
 
   /**
    * Shows or hides battery warning message.
-   * @param {boolean} visible Is message visible?
+   * @param visible Is message visible?
    */
-  showLowBatteryWarningMessage(visible) {
+  showLowBatteryWarningMessage(visible: boolean): void {
     this.showLowBatteryWarning = visible;
   }
 
   /**
    * Sets which dialog should be shown.
-   * @param {ConsumerUpdateStep} value Current update state.
+   * @param value Current update state.
    */
-  setUpdateState(value) {
+  setUpdateState(value: ConsumerUpdateStep): void {
     this.setUIStep(value);
   }
 
   /**
    * Sets percent to be shown in progress bar.
-   * @param {number} percent Current progress
-   * @param {string} messagePercent Message describing current progress.
-   * @param {string} messageTimeLeft Message describing time left.
+   * @param percent Current progress
+   * @param messagePercent Message describing current progress.
+   * @param messageTimeLeft Message describing time left.
    */
-  setUpdateStatus(percent, messagePercent, messageTimeLeft) {
+  setUpdateStatus(percent: number, messagePercent: string,
+      messageTimeLeft: string): void {
     // Sets aria-live polite on percent and timeleft container every time
     // new threshold has been achieved otherwise do not initiate spoken
     // feedback update by setting aria-live off.
@@ -253,11 +255,25 @@ class ConsumerUpdateScreen extends ConsumerUpdateScreenElementBase {
       while (percent >= PERCENT_THRESHOLDS[this.thresholdIndex]) {
         this.thresholdIndex = this.thresholdIndex + 1;
       }
-      this.$.betterUpdatePercent.setAttribute('aria-live', 'polite');
-      this.$.betterUpdateTimeleft.setAttribute('aria-live', 'polite');
+      const betterUpdatePercent = this.shadowRoot?.
+          querySelector('#betterUpdatePercent');
+      const betterUpdateTimeleft = this.shadowRoot?.
+          querySelector('#betterUpdateTimeleft');
+      if (betterUpdatePercent instanceof HTMLElement
+          && betterUpdateTimeleft instanceof HTMLElement) {
+        betterUpdatePercent.setAttribute('aria-live', 'polite');
+        betterUpdateTimeleft.setAttribute('aria-live', 'polite');
+      }
     } else {
-      this.$.betterUpdateTimeleft.setAttribute('aria-live', 'off');
-      this.$.betterUpdatePercent.setAttribute('aria-live', 'off');
+      const betterUpdatePercent = this.shadowRoot?.
+          querySelector('#betterUpdatePercent');
+      const betterUpdateTimeleft = this.shadowRoot?.
+          querySelector('#betterUpdateTimeleft');
+      if (betterUpdatePercent instanceof HTMLElement
+          && betterUpdateTimeleft instanceof HTMLElement) {
+        betterUpdatePercent.setAttribute('aria-live', 'off');
+        betterUpdateTimeleft.setAttribute('aria-live', 'off');
+      }
     }
     this.betterUpdateProgressValue = percent;
     this.updateStatusMessagePercent = messagePercent;
@@ -267,30 +283,39 @@ class ConsumerUpdateScreen extends ConsumerUpdateScreenElementBase {
   /**
    * Sets whether carousel should auto transit slides.
    */
-  setAutoTransition(value) {
+  setAutoTransition(value: boolean): void {
     this.autoTransition = value;
   }
 
   /**
    * Gets whether carousel should auto transit slides.
-   * @private
-   * @param {ConsumerUpdateStep} step Which UIState is shown now.
-   * @param {boolean} autoTransition Is auto transition allowed.
+   * @param step Which UIState is shown now.
+   * @param autoTransition Is auto transition allowed.
    */
-  getAutoTransition_(step, autoTransition) {
+  private getAutoTransition(step: ConsumerUpdateStep,
+      autoTransition: boolean): boolean {
     return step == ConsumerUpdateStep.UPDATE && autoTransition;
   }
 
-  onBackClicked_() {
+  private onBackClicked(): void {
     this.userActed(UserAction.BACK);
   }
 
   /**
-   * @private
-   * @param {ConsumerUpdateStep} uiStep which UIState is shown now.
+   * @param uiStep which UIState is shown now.
    */
-  playAnimation_(uiStep) {
-    this.$.checkingAnimation.playing = (uiStep === ConsumerUpdateStep.CHECKING);
+  private playAnimation(uiStep: ConsumerUpdateStep): void {
+    const animation = this.shadowRoot?.querySelector('#checkingAnimation');
+    if (animation instanceof OobeCrLottie) {
+      animation.playing = (uiStep === ConsumerUpdateStep.CHECKING);
+    }
   }
 }
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ConsumerUpdateScreen.is]: ConsumerUpdateScreen;
+  }
+}
+
 customElements.define(ConsumerUpdateScreen.is, ConsumerUpdateScreen);
