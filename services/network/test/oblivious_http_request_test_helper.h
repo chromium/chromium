394b6d1c2fdcb440d0dafb29d3b5aea1ce2f7cdc
Russ Hamilton <behamilton@google.com>
2024-01-19 21:19:43 +0000

    Re-Reland "Add browser test for KAnonymityService Join and Query"
    This reverts commit 77a4f12d24620477182f3f154472ed7156c4b9d7.
    
    Original change's description:
    > Revert "Reland "Add browser test for KAnonymityService Join and Query""
    >
    > This reverts commit 4b4b0f2dbf81bff44523fccf84132aba080297ad.
    >
    > Reason for revert: Looks like the reland also broke some tests:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/23978/test-results?sortby=&groupby=
    >
    > [26649:27915:0103/105330.657222:ERROR:checkin_request.cc(243)] Check-in request got net error: -105
    > ../../chrome/browser/k_anonymity_service/k_anonymity_service_client_browsertest.cc:361: Failure
    > Value of: result
    >   Actual: false
    > Expected: true
    >
    > Original change's description:
    > > Reland "Add browser test for KAnonymityService Join and Query"
    > >
    > > This is a reland of commit 8f276be81536819509b79fbec2883d00665fa8d8
    > >
    > > Original change's description:
    > > > Add browser test for KAnonymityService Join and Query
    > > >
    > > > Adds browser tests for the JoinSet and QuerySets functions of the
    > > > k-anonymity service. These tests differ from the unit tests in that
    > > > they also exercise the network service with a fake test server. In
    > > > particular these tests exercise Trust Token redemption as part of
    > > > Oblivious HTTP requests which is not otherwise tested.
    > > >
    > > > Bug: b:310667960
    > > > Change-Id: I3bb760d3538231b0f2279df041a3eb102263dc7f
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5095130
    > > > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > > > Commit-Queue: Russ Hamilton <behamilton@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1237655}
    > >
    > > Bug: b:310667960
    > > Change-Id: I9e8bf560e3edfb4313994d4e2f933255b3a77206
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5131473
    > > Commit-Queue: Russ Hamilton <behamilton@google.com>
    > > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1242533}
    >
    > Bug: b:310667960
    > Change-Id: I314d7cc3038317eff68fa62f0bdcb2a7a1d97589
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5166151
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Peter Williamson <petewil@chromium.org>
    > Commit-Queue: Peter Williamson <petewil@chromium.org>
    > Owners-Override: Peter Williamson <petewil@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1242604}
    
    Bug: b:310667960
    Change-Id: I378694151c758bc399151172d4e2faf4262f9178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5169186
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1249514}

---
+++ services/network/test/oblivious_http_request_test_helper.h
@@ -0,0 +1,40 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_TEST_OBLIVIOUS_HTTP_REQUEST_TEST_HELPER_H_
+#define SERVICES_NETWORK_TEST_OBLIVIOUS_HTTP_REQUEST_TEST_HELPER_H_
+
+#include <memory>
+#include <string>
+
+#include "net/third_party/quiche/src/quiche/oblivious_http/buffers/oblivious_http_request.h"
+#include "net/third_party/quiche/src/quiche/oblivious_http/common/oblivious_http_header_key_config.h"
+#include "net/third_party/quiche/src/quiche/oblivious_http/oblivious_http_gateway.h"
+
+namespace network {
+namespace test {
+
+// ObliviousHttpRequestTestHelper encapsulates server-side oblivious HTTP key
+// management and crypto logic.
+//
+class ObliviousHttpRequestTestHelper {
+ public:
+  ObliviousHttpRequestTestHelper();
+  ~ObliviousHttpRequestTestHelper();
+
+  std::string GetPublicKeyConfigs();
+  std::pair<std::string, quiche::ObliviousHttpRequest::Context> DecryptRequest(
+      std::string_view ciphertext_request);
+  std::string EncryptResponse(std::string plaintext_response,
+                              quiche::ObliviousHttpRequest::Context& context);
+
+ private:
+  std::unique_ptr<quiche::ObliviousHttpGateway> ohttp_gateway_;
+  quiche::ObliviousHttpHeaderKeyConfig key_config_;
+};
+
+}  // namespace test
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_TEST_OBLIVIOUS_HTTP_REQUEST_SERVER_H_
