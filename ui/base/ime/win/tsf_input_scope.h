d8ea6f3e14936c27d8a51dac42b8021326640f19
Anupam Snigdha <snianu@microsoft.com>
2024-01-20 00:42:59 +0000

    Remove SetInputScope call.
    Continuation of crrev.com/c/5217731. Since this code is unused,
    remove it from Chromium.
    
    Bug: 1519933
    Change-Id: Ib8d0122732afb8b02f817bc68f560b73639e8d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5219113
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1249715}

--- ui/base/ime/win/tsf_input_scope.h
+++ ui/base/ime/win/tsf_input_scope.h
@@ -16,10 +16,6 @@
 namespace ui {
 namespace tsf_inputscope {
 
-// Initializes the TSF for input scopes. It loads the tsf dll and get the
-// function pointer for setting the input scopes in TSF.
-COMPONENT_EXPORT(UI_BASE_IME_WIN) void InitializeTsfForInputScopes();
-
 // Returns InputScope list corresoponding to ui::TextInputType and
 // ui::TextInputMode.
 // This function is only used from following functions but declared for test.
@@ -37,15 +33,6 @@ COMPONENT_EXPORT(UI_BASE_IME_WIN)
 ITfInputScope* CreateInputScope(TextInputType text_input_type,
                                 TextInputMode text_input_mode,
                                 bool should_do_learning);
-
-// A wrapper of the SetInputScopes API exported by msctf.dll.
-// http://msdn.microsoft.com/en-us/library/windows/desktop/ms629026.aspx
-// Does nothing on Windows XP in case TSF is disabled.
-// NOTE: For TSF-aware window, you should use ITfInputScope instead.
-COMPONENT_EXPORT(UI_BASE_IME_WIN)
-void SetInputScopeForTsfUnawareWindow(HWND window_handle,
-                                      TextInputType text_input_type,
-                                      TextInputMode text_input_mode);
 
 }  // namespace tsf_inputscope
 }  // namespace ui
