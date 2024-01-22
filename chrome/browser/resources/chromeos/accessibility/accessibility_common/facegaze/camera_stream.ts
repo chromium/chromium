f12e07751da06072db007f739ad586a0adbe6f3d
Katie Dektar <katie@chromium.org>
2024-01-22 22:59:58 +0000

    [FaceGaze] More fun face landmark colors
    Now bright transluscent pink for the face outline and
    yellow for the forehead dot. The white was a little creepy
    according to feedback from teammates, hopefully this will
    be so loud that it won't look as much like a creepy mask.
    
    See https://screenshot.googleplex.com/3rynfPn5bXZ9jJg
    
    AX-Relnotes: N/A
    Bug: N/A
    Change-Id: Ib4553247d561b82f5d33fde154f8918de5115cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5214728
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1250511}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/camera_stream.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/camera_stream.ts
@@ -93,13 +93,13 @@ export class WebCamFaceLandmarker {
     for (const landmarks of result.faceLandmarks) {
       this.drawingUtils_.drawConnectors(
           landmarks, FaceLandmarker.FACE_LANDMARKS_CONTOURS,
-          {color: '#C0C0C060', lineWidth: 2});
+          {color: '#F5005760', lineWidth: 2});
     }
 
     // Center point.
     ctx.beginPath();
     ctx.arc(overlay.width / 2, overlay.height / 2, 2, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'blue';
+    ctx.strokeStyle = '#3D5AFE';
     ctx.stroke();
 
     // Forehead point.
@@ -109,7 +109,7 @@ export class WebCamFaceLandmarker {
     const y = foreheadLocation.y;
     ctx.beginPath();
     ctx.arc(x * overlay.width, y * overlay.height, 2, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'red';
+    ctx.strokeStyle = '#FFEA00';
     ctx.stroke();
   }
 }
