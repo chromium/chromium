5a132262dae7410b168f41bcb004ea608965949d
Joemer Ramos <joemerramos@chromium.org>
2024-01-23 22:54:12 +0000

    [iOS] Cleanup iOS 15 Guard in ErrorPageInttest
    Bug: 1418068, 1153261
    Change-Id: Ib3e2d3c4770bd63a95dafbb09369488d9ddae112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229800
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251109}

--- ios/web/web_state/error_page_inttest.mm
+++ ios/web/web_state/error_page_inttest.mm
@@ -158,12 +158,6 @@ class ErrorPageTest : public WebTestWithWebState {
 // Tests that the error page is correctly displayed after navigating back to it
 // multiple times. See http://crbug.com/944037 .
 TEST_F(ErrorPageTest, BackForwardErrorPage) {
-  // TODO(crbug.com/1153261): this test should be fixed in newer versions of
-  // WebKit.
-  if (@available(iOS 15, *)) {
-  } else {
-    return;
-  }
   test::LoadUrl(web_state(), server_.GetURL("/close-socket"));
   ASSERT_TRUE(WaitForErrorText(web_state(), server_.GetURL("/close-socket")));
 
