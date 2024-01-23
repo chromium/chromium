a3e4ebdc597984495a37358374b47304e29870d5
Jeff Gour <jegour@microsoft.com>
2024-01-23 21:04:49 +0000

    Restore base::ProtectedMemory
    This PR restores parts of the base::ProtectedMemory code removed in
    https://crrev.com/c/1891114 . This is done as part of a larger effort
    to harden MojoJS bindings against data-only attacks. Out of multiple
    proposed approaches for preventing MojoJS bindings from being enabled
    by a data-only attack, using protected memory appears to provide the
    best balance of security versus cost/maintenance burden. A follow-up
    PR will be made to actually use base::ProtectedMemory for this purpose.
    
    Since there are challenges with using base::ProtectedMemory on Linux and
    MacOS (originating from changes that make it so that a memory section
    that is going to be made writable during runtime cannot be made
    read-only at compile/link time), this PR only restores the functionality
    at Windows for now. If the use of it to harden the MojoJS binding
    enablement proves effective, restoring it for Linux and MacOS can be
    revisited.
    
    Bug: 976506
    Change-Id: I5a8dbecd695d642cc46ebc68fc5f20d0f2d152b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5161001
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Jeffrey Gour <jegour@microsoft.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251044}

---
+++ base/memory/protected_memory_win.cc
@@ -0,0 +1,52 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/memory/protected_memory.h"
+
+#include <windows.h>
+
+#include <stdint.h>
+
+#include "base/memory/page_size.h"
+#include "base/process/process_metrics.h"
+#include "base/synchronization/lock.h"
+#include "build/build_config.h"
+
+namespace base {
+namespace internal {
+
+namespace {
+
+bool SetMemory(void* start, void* end, DWORD prot) {
+  CHECK(end > start);
+  const uintptr_t page_mask = ~(base::GetPageSize() - 1);
+  const uintptr_t page_start = reinterpret_cast<uintptr_t>(start) & page_mask;
+  DWORD old_prot;
+  return VirtualProtect(reinterpret_cast<void*>(page_start),
+                        reinterpret_cast<uintptr_t>(end) - page_start, prot,
+                        &old_prot) != 0;
+}
+
+}  // namespace
+
+bool SetMemoryReadWrite(void* start, void* end) {
+  return SetMemory(start, end, PAGE_READWRITE);
+}
+
+bool SetMemoryReadOnly(void* start, void* end) {
+  return SetMemory(start, end, PAGE_READONLY);
+}
+
+void AssertMemoryIsReadOnly(const void* ptr) {
+  const uintptr_t page_mask = ~(base::GetPageSize() - 1);
+  const uintptr_t page_start = reinterpret_cast<uintptr_t>(ptr) & page_mask;
+
+  MEMORY_BASIC_INFORMATION info;
+  SIZE_T result =
+      VirtualQuery(reinterpret_cast<LPCVOID>(page_start), &info, sizeof(info));
+  CHECK_GT(result, 0U);
+  CHECK(info.Protect == PAGE_READONLY);
+}
+}  // namespace internal
+}  // namespace base
