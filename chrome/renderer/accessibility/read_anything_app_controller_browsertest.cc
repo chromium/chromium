3af5ff4888d0655c96bf49c172300a266f2aee64
Lauren Winston <lwinston@google.com>
2024-01-24 18:16:22 +0000

    [Read Aloud] Add a test for backwards nav with a split sentence.
    Bug: 1474951
    Change-Id: I1d9ea68a96cf964c0917345dc022ab59b6ff8a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229082
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251527}

--- chrome/renderer/accessibility/read_anything_app_controller_browsertest.cc
+++ chrome/renderer/accessibility/read_anything_app_controller_browsertest.cc
@@ -3042,3 +3042,61 @@ TEST_F(ReadAnythingAppControllerTest,
   std::vector<ui::AXNodeID> next_node_ids = GetNextText();
   EXPECT_EQ((int)next_node_ids.size(), 0);
 }
+
+TEST_F(ReadAnythingAppControllerTest,
+       GetPreviousText_SentenceSplitAcrossTwoNodes) {
+  std::u16string sentence1 = u"And I am almost ";
+  std::u16string sentence2 = u"there. ";
+  std::u16string sentence3 = u"I am almost there.";
+  ui::AXTreeUpdate update;
+  SetUpdateTreeID(&update);
+  ui::AXNodeData static_text1;
+  static_text1.id = 2;
+  static_text1.role = ax::mojom::Role::kStaticText;
+  static_text1.SetNameChecked(sentence1);
+
+  ui::AXNodeData static_text2;
+  static_text2.id = 3;
+  static_text2.role = ax::mojom::Role::kStaticText;
+  static_text2.SetNameChecked(sentence2);
+
+  ui::AXNodeData static_text3;
+  static_text3.id = 4;
+  static_text3.role = ax::mojom::Role::kStaticText;
+  static_text3.SetNameChecked(sentence3);
+  update.nodes = {static_text1, static_text2, static_text3};
+
+  AccessibilityEventReceived({update});
+  OnAXTreeDistilled({static_text1.id, static_text2.id, static_text3.id});
+  InitAXPosition(update.nodes[0].id);
+
+  // Move to the end of the tree.
+  GetNextText();
+  GetNextText();
+  GetNextText();
+
+  std::vector<ui::AXNodeID> previous_node_ids = GetPreviousText();
+  EXPECT_EQ((int)previous_node_ids.size(), 2);
+
+  // Returns the 2nd segment correctly.
+  EXPECT_EQ(previous_node_ids[1], static_text2.id);
+  EXPECT_EQ(GetNextTextStartIndex(previous_node_ids[1]), 0);
+  EXPECT_EQ(GetNextTextEndIndex(previous_node_ids[1]), (int)sentence2.length());
+
+  // Returns the 1st segment correctly.
+  EXPECT_EQ(previous_node_ids[0], static_text1.id);
+  EXPECT_EQ(GetNextTextStartIndex(previous_node_ids[0]), 0);
+  EXPECT_EQ(GetNextTextEndIndex(previous_node_ids[0]), (int)sentence1.length());
+
+  // After moving forward again, the third segment was returned correctly.
+  // The third segment was returned correctly after getting the next text.
+  std::vector<ui::AXNodeID> next_node_ids = GetNextText();
+  EXPECT_EQ((int)next_node_ids.size(), 1);
+  EXPECT_EQ(next_node_ids[0], static_text3.id);
+  EXPECT_EQ(GetNextTextStartIndex(next_node_ids[0]), 0);
+  EXPECT_EQ(GetNextTextEndIndex(next_node_ids[0]), (int)sentence3.length());
+
+  // Nodes are empty at the end of the new tree.
+  next_node_ids = GetNextText();
+  EXPECT_EQ((int)next_node_ids.size(), 0);
+}
