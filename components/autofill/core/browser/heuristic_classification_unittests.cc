ccfa6ef2ce0a08d2e3cd29aa894860ebaf696a91
Norge Vizcay <vizcay@google.com>
2024-01-24 08:29:41 +0000

    Enable kAutofillUseI18nAddressModel in HeuristicClassificationTests.
    The CL enables the flag kAutofillUseI18nAddressModel in
    HeuristicClassificationTests. This fixes the discrepancies seen after
    crrev/c/5222621 landed.
    
    Change-Id: I640182a3a3e9e7298fee55b66996d7aa344fcdab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229059
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1251288}

--- components/autofill/core/browser/heuristic_classification_unittests.cc
+++ components/autofill/core/browser/heuristic_classification_unittests.cc
@@ -521,6 +521,7 @@ TEST_P(HeuristicClassificationTests, EndToEnd) {
 
   std::vector<base::test::FeatureRef> enabled_features = {
       // Support for new field types.
+      features::kAutofillUseI18nAddressModel,
       features::kAutofillEnableSupportForBetweenStreets,
       features::kAutofillEnableSupportForAdminLevel2,
       features::kAutofillEnableSupportForAddressOverflow,
