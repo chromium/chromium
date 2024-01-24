75ea292bee2dcb569b475c1cef8cf14f58d40b22
Crisrael Lucero <crisrael@google.com>
2024-01-24 20:40:04 +0000

    [Phone Hub] Update Learn More pill button type
    Previously, the Learn More pill button was using kFloatingWithoutIcon.
    During the Jelly migration, this was not updated as the appropriate Type
    (kSecondaryWithoutIcon) had contrast ratio issues. That has since been
    resolved and we are now migrating this pill button.
    
    Before Screenshots
    BT Disabled: https://screenshot.googleplex.com/7gEXTCmHfE96yDj.png
    Disconnected: https://screenshot.googleplex.com/BAPfZH52g2rp5z2.png
    
    After Screenshots
    BT Disabled: https://screenshot.googleplex.com/AwPg5TrKZusvLFu.png
    Disconnectd: https://screenshot.googleplex.com/4jEPJb4ijQyTkE7.png
    
    Test: manually verified, provided screenshots
    Bug: b/281844561
    Change-Id: I037ee6753b34dedc6409a5838941227574720be8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5234562
    Auto-Submit: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251655}

--- ash/system/phonehub/bluetooth_disabled_view.cc
+++ ash/system/phonehub/bluetooth_disabled_view.cc
@@ -50,7 +50,7 @@ BluetoothDisabledView::BluetoothDisabledView() {
                           base::Unretained(this)),
       l10n_util::GetStringUTF16(
           IDS_ASH_PHONE_HUB_BLUETOOTH_DISABLED_DIALOG_LEARN_MORE_BUTTON),
-      PillButton::Type::kFloatingWithoutIcon, /*icon=*/nullptr);
+      PillButton::Type::kSecondaryWithoutIcon, /*icon=*/nullptr);
   learn_more->SetID(PhoneHubViewID::kBluetoothDisabledLearnMoreButton);
   content_view->AddButton(std::move(learn_more));
 
