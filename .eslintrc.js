e4b476dafacf363d081d502d1308c8b4007d85df
dpapad <dpapad@chromium.org>
2024-01-25 21:56:21 +0000

    WebUI: Enforce querySelector<Type>() pattern via ESLint, part 5 (last)
    A common misuse of querySelector/querySelectorAll is to use a type cast,
    like
    this.shadowRoot.querySelector(...) as Type
    
    instead of leveraging the type parameter, like
    this.shadowRoot.querySelector<Type>(...)
    
    In this part enabling an ESLint check leveraging the
    'no-restricted-syntax' rule, that catches such patterns, with an error
    as follows:
    
    "Don't use 'querySelector(...) as Type'. Use the type parameter,
    'querySelector<Type>(...)' instead"
    
    All violations across the codebase, should have been fixed or
    suppressed in previous CLs.
    
    Bug: 1521107
    Change-Id: I81280129d2a1f74b897c3fcde296185278aeb40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230741
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252346}

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,6 +66,16 @@ module.exports = {
     'no-restricted-syntax': ['error', {
       'selector': 'CallExpression[callee.object.name=JSON][callee.property.name=parse] > CallExpression[callee.object.name=JSON][callee.property.name=stringify]',
       'message': 'Don\'t use JSON.parse(JSON.stringify(...)) to clone objects. Use structuredClone() instead.',
+    },
+    {
+      // https://google.github.io/styleguide/tsguide.html#return-type-only-generics
+      'selector': 'TSAsExpression > CallExpression > MemberExpression[property.name=/^querySelector$/]',
+      'message': 'Don\'t use \'querySelector(...) as Type\'. Use the type parameter, \'querySelector<Type>(...)\' instead',
+    },
+    {
+      // https://google.github.io/styleguide/tsguide.html#return-type-only-generics
+      'selector': 'TSAsExpression > CallExpression > MemberExpression[property.name=/^querySelectorAll$/]',
+      'message': 'Don\'t use \'querySelectorAll(...) as Type\'. Use the type parameter, \'querySelectorAll<Type>(...)\' instead',
     }],
     'no-throw-literal': 'error',
     'no-trailing-spaces': 'error',
