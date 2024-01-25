2162b5c1bd27768dd138b5f8f735cf9a0971144e
sisidovski <sisidovski@chromium.org>
2024-01-25 15:29:22 +0000

    Split requests/errors handlings in sw.js to a sub script
    We'd like to add the test case when there is no fetch handler in SW.
    But we'd like to use the same mechanism to monitor errors. So this CL
    moves some code into a sub script, and that will be imported from
    another service worker file in the next CL.
    
    Change-Id: Ie7663ef3a7c7e614d063abc521e4a4d1f9f791a5
    Bug: 1519727, 1511459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232748
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252095}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/static-router-sw.js
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/tentative/static-router/resources/static-router-sw.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import {routerRules} from './router-rules.js';
-
-var requests = [];
-var errors = [];
+import {
+  recordRequest,
+  recordError,
+  getRecords,
+  resetRecords } from './static-router-sw.sub.js';
 
 self.addEventListener('install', async e => {
   e.waitUntil(caches.open('v1').then(
@@ -14,7 +16,7 @@ self.addEventListener('install', async e => {
   try {
     await e.addRoutes(routerRules[key]);
   } catch (e) {
-    errors.push(e);
+    recordError(e);
   }
   self.skipWaiting();
 });
@@ -24,20 +26,18 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', function(event) {
-  requests.push({url: event.request.url, mode: event.request.mode});
+  recordRequest(event.request);
   const url = new URL(event.request.url);
   const nonce = url.searchParams.get('nonce');
   event.respondWith(new Response(nonce));
 });
 
 self.addEventListener('message', function(event) {
-  let r = requests;
-  let e = errors;
   if (event.data.reset) {
-    requests = [];
-    errors = [];
+    resetRecords();
   }
   if (event.data.port) {
-    event.data.port.postMessage({requests: r, errors: e});
+    const {requests, errors} = getRecords();
+    event.data.port.postMessage({requests, errors});
   }
 });
