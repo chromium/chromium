a268f642be2fe2ae47374d9aca6071d29cc53beb
Colin Blundell <blundell@chromium.org>
2024-01-25 08:18:30 +0000

    [//media] Unconditionally disable VideoFrame->WebGL 1c upload on Android
    [1] disabled this codepath on Android with a "reverse killswitch" to
    re-enable. That CL landed in 120 [2], which has now been in stable for
    almost 2 months with no problems reported. This CL removes the reverse
    killswitch to unconditionally disable the codepath on Android.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/4958678
    [2] https://chromiumdash.appspot.com/commits?commit=61898050faa88383ae75edd8e1f9ee07425126fd&platform=Windows
    
    Bug: 1410164, 1494365
    Change-Id: Ia6965a8b235ddf87e7221ab1eb1f0c42f2b1743e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5233664
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1251944}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -681,6 +681,7 @@ void ConvertVideoFrameToRGBPixelsTask(const VideoFrame* video_frame,
   done->Run();
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 // Valid gl texture internal format that can try to use direct uploading path.
 bool ValidFormatForDirectUploading(
     viz::RasterContextProvider* raster_context_provider,
@@ -705,6 +706,7 @@ bool ValidFormatForDirectUploading(
       return false;
   }
 }
+#endif
 
 std::tuple<SkYUVAInfo::PlaneConfig, SkYUVAInfo::Subsampling>
 VideoPixelFormatAsSkYUVAInfoValues(VideoPixelFormat format) {
@@ -727,26 +729,14 @@ VideoPixelFormatAsSkYUVAInfoValues(VideoPixelFormat format) {
   }
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 // Controls whether the one-copy path when copying a VideoFrame to a GL texture
-// is enabled or disabled. This codepath is disabled on Android by default
-// pending the full transition of the codebase to the passthrough decoder and
-// the transition of this function away from using legacy mailboxes to do 1-copy
-// upload (see crbug.com/1494365 for details). On Android, this Feature serves
-// as a reverse-killswitch while we roll out the complete disabling of this
-// codepath.
-// TODO(crbug.com/1494365): Remove the usage of this feature for Android once
-// explicit disabling has safely rolled out and make
-// UploadOfVideoFrameToGLTexture() unconditionally disabled on Android.
-// On all other platforms, the one-copy path being enabled is the default
+// is enabled or disabled. The one-copy path being enabled is the default
 // production state, with this Feature being used to be able to disable this
 // path for performance testing.
 BASE_FEATURE(kOneCopyUploadOfVideoFrameToGLTexture,
              "OneCopyUploadOfVideoFrameToGLTexture",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 // Controls whether the one-copy path when copying a pure software VideoFrame
 // (i.e., a VideoFrame with no textures) to a GL texture is enabled or disabled.
@@ -773,6 +763,7 @@ BASE_FEATURE(kOneCopyUploadOfPureSoftwareVideoFrameToGLTexture,
 BASE_FEATURE(kOneCopyLegacyMPVideoFrameUploadViaSI,
              "OneCopyLegacyMPVideoFrameUploadViaSI",
              base::FEATURE_ENABLED_BY_DEFAULT);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // anonymous namespace
 
@@ -1521,6 +1512,10 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameTexturesToGLTexture(
     // alpha been requested. And dst texture mipLevel must be 0.
     // TODO(crbug.com/1155003): Figure out whether premultiply options here are
     // accurate.
+    // NOTE: The direct upload path is not supported on Android (see comment on
+    // UploadVideoFrameToGLTexture()).
+    // TODO(crbug.com/1494365): Enable on Android.
+#if !BUILDFLAG(IS_ANDROID)
     if ((media::IsOpaque(video_frame->format()) || premultiply_alpha) &&
         level == 0 &&
         (video_frame->NumTextures() > 1 ||
@@ -1535,6 +1530,7 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameTexturesToGLTexture(
         }
       }
     }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
     if (!UpdateLastImage(video_frame, raster_context_provider,
                          true /* allow_wrap_texture */)) {
@@ -1601,6 +1597,7 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameTexturesToGLTexture(
   return true;
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 bool PaintCanvasVideoRenderer::UploadVideoFrameToGLTexture(
     viz::RasterContextProvider* raster_context_provider,
     gpu::gles2::GLES2Interface* destination_gl,
@@ -1769,6 +1766,7 @@ bool PaintCanvasVideoRenderer::UploadVideoFrameToGLTexture(
 
   return true;
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 bool PaintCanvasVideoRenderer::CopyVideoFrameYUVDataToGLTexture(
     viz::RasterContextProvider* raster_context_provider,
@@ -1811,6 +1809,10 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameYUVDataToGLTexture(
   // alpha been requested. And dst texture mipLevel must be 0.
   // TODO(crbug.com/1155003): Figure out whether premultiply options here are
   // accurate.
+  // NOTE: The direct upload path is not supported on Android (see comment on
+  // UploadVideoFrameToGLTexture()).
+  // TODO(crbug.com/1494365): Enable on Android.
+#if !BUILDFLAG(IS_ANDROID)
   if ((media::IsOpaque(video_frame->format()) || premultiply_alpha) &&
       level == 0) {
     if (base::FeatureList::IsEnabled(kOneCopyUploadOfVideoFrameToGLTexture)) {
@@ -1822,6 +1824,7 @@ bool PaintCanvasVideoRenderer::CopyVideoFrameYUVDataToGLTexture(
       }
     }
   }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
   auto* sii = raster_context_provider->SharedImageInterface();
   gpu::raster::RasterInterface* source_ri =
