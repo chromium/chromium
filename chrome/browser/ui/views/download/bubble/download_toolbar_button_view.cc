61df42c0e5f1e549ea186fc374a7c7e7378dc094
Awad Osman <awado@google.com>
2024-01-25 19:58:27 +0000

    [M122] Don't show IPH if safe browsing is controlled by policy
    Previously the IPH would show if the user had a chrome policy
    controlling their safe browsing level which was a bug because the user
    wouldn't be able to switch their safe browsing level even if they
    accepted the IPH promo.
    
    Screencast(Before):
    http://go/scrcast/NDc0Mzk3MzY2NjAyOTU2OHxhYTIzYjlhYi0zYg
    Screencast(After):
    http://go/scrcast/NjY0NTI0MjQwODIwNjMzNnxlNTA4NzgxZi04ZA
    
    (cherry picked from commit 1503e8e94adfb88f6b22a28ecba5cdf40c82a093)
    
    Bug: 1520909
    Change-Id: I6846414af9b38c7298ce1eaf8d6a159bb76ff981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5229782
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1251135}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238786
    Cr-Commit-Position: refs/branch-heads/6261@{#95}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
@@ -42,6 +42,7 @@
 #include "chrome/grit/generated_resources.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/safe_browsing/core/common/features.h"
+#include "components/safe_browsing/core/common/safe_browsing_policy_handler.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/user_education/common/user_education_class_properties.h"
 #include "content/public/browser/browser_thread.h"
@@ -765,6 +766,11 @@ void DownloadToolbarButtonView::BubbleCloser::OnEvent(const ui::Event& event) {
 void DownloadToolbarButtonView::ShowIphPromo() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   Profile* profile = browser_->profile();
+  // Don't show IPH Promo if safe browsing level is set by policy.
+  if (safe_browsing::SafeBrowsingPolicyHandler::
+          IsSafeBrowsingProtectionLevelSetByPolicy(profile->GetPrefs())) {
+    return;
+  }
   if (safe_browsing::GetSafeBrowsingState(*profile->GetPrefs()) ==
           safe_browsing::SafeBrowsingState::STANDARD_PROTECTION &&
       !profile->IsOffTheRecord() &&
