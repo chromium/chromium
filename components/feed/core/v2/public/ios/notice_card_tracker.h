fba8b1cfd093925cbc154a877fd0f822b47da8a8
Justin Cohen <justincohen@google.com>
2024-01-25 20:30:03 +0000

    ios: Use raw_ptr for iOS builds.
    This CL was uploaded by git cl split.
    
    R=dewittj@chromium.org
    
    Change-Id: I214edb6640aad3f3129fc674845d20aa29b09558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5232010
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252302}

--- components/feed/core/v2/public/ios/notice_card_tracker.h
+++ components/feed/core/v2/public/ios/notice_card_tracker.h
@@ -5,6 +5,7 @@
 #ifndef COMPONENTS_FEED_CORE_V2_PUBLIC_IOS_NOTICE_CARD_TRACKER_H_
 #define COMPONENTS_FEED_CORE_V2_PUBLIC_IOS_NOTICE_CARD_TRACKER_H_
 
+#include "base/memory/raw_ptr.h"
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
 // TODO(crbug.com/1213474): Remove this include:
@@ -40,7 +41,7 @@ class NoticeCardTracker {
   void MaybeUpdateNoticeCardViewsCount(int index);
   void MaybeUpdateNoticeCardClicksCount(int index);
 
-  PrefService* profile_prefs_;
+  raw_ptr<PrefService> profile_prefs_;
 
   // The number of views of the notice card.
   mutable base::Lock views_count_lock_;
