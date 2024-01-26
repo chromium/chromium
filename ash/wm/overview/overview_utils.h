50496c61002596cd8240e61d12b764471621f82e
Michele Fan <michelefan@chromium.org>
2024-01-26 22:27:57 +0000

    snap-group: Use `GetGridBoundsInScreen()` directly in test util
    This cl optimizes the logics in `GetGridBoundsInScreen()` in
    overview_utils.cc and applies the logic directly in test util to
    avoid duplication.
    
    Minor renaming `StartOverview()` and `EndOverview()` input params.
    
    Fixed: b/322448111
    Test: Run existing
    Change-Id: I43e7c74d57977cc6f8f9e0f0c0ea8d6ac6eee6a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239871
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252893}

--- ash/wm/overview/overview_utils.h
+++ ash/wm/overview/overview_utils.h
@@ -72,7 +72,7 @@ void MaximizeIfSnapped(aura::Window* window);
 // `divider_changed` is true, maybe clamp the bounds to a minimum size and shift
 // the bounds offscreen. If `account_for_hotseat` is true and we are in tablet
 // mode, inset the bounds by the hotseat size.
-gfx::Rect GetGridBoundsInScreen(aura::Window* target_root);
+ASH_EXPORT gfx::Rect GetGridBoundsInScreen(aura::Window* target_root);
 gfx::Rect GetGridBoundsInScreen(
     aura::Window* target_root,
     std::optional<SplitViewDragIndicators::WindowDraggingState>
