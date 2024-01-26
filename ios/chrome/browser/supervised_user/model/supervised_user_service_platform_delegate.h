a88bbb3caff924629ec89563a5ddbc189fcffb6a
Justin Cohen <justincohen@google.com>
2024-01-26 17:33:02 +0000

    ios: Use raw_ptr for ios/ directory for iOS builds.
    This CL was uploaded by git cl split.
    
    R=agawronska@chromium.org
    
    Change-Id: I1f11949b4fc841dfd768a73c56faa185c923367a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231590
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1252720}

--- ios/chrome/browser/supervised_user/model/supervised_user_service_platform_delegate.h
+++ ios/chrome/browser/supervised_user/model/supervised_user_service_platform_delegate.h
@@ -5,6 +5,7 @@
 #ifndef IOS_CHROME_BROWSER_SUPERVISED_USER_MODEL_SUPERVISED_USER_SERVICE_PLATFORM_DELEGATE_H_
 #define IOS_CHROME_BROWSER_SUPERVISED_USER_MODEL_SUPERVISED_USER_SERVICE_PLATFORM_DELEGATE_H_
 
+#import "base/memory/raw_ptr.h"
 #include "components/supervised_user/core/browser/supervised_user_service.h"
 
 class ChromeBrowserState;
@@ -20,7 +21,7 @@ class SupervisedUserServicePlatformDelegate
   void CloseIncognitoTabs() override;
 
  private:
-  ChromeBrowserState* browser_state_;
+  raw_ptr<ChromeBrowserState> browser_state_;
 };
 
 #endif  // IOS_CHROME_BROWSER_SUPERVISED_USER_MODEL_SUPERVISED_USER_SERVICE_PLATFORM_DELEGATE_H_
