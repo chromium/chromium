c545dc8060ccbd76266b1fdacfac751db12b46da
Gauthier Ambard <gambard@chromium.org>
2024-01-29 17:48:33 +0000

    [iOS] Don't use WebUsageEnabler when browser is nil
    Looking at the crashes, in some cases the Browser is nil when the
    browsing data are cleared.
    Previously the code was using Objective-C calls which are Ok with
    sending message to nil, so moving it back to a no-op.
    CL introducing the issue: https://chromium-review.googlesource.com/c/chromium/src/+/5215853
    
    Fixed: 1522188
    Change-Id: I75a04f280d8bf3d41fc2496574b37c6e045155ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5238372
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253353}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -1483,12 +1483,16 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
           [handler hideActivityOverlay];
         }
       }
-      WebUsageEnablerBrowserAgent::FromBrowser(
-          browserProviderInterface.mainBrowserProvider.browser)
-          ->SetWebUsageEnabled(true);
-      WebUsageEnablerBrowserAgent::FromBrowser(
-          browserProviderInterface.incognitoBrowserProvider.browser)
-          ->SetWebUsageEnabled(true);
+      if (browserProviderInterface.mainBrowserProvider.browser) {
+        WebUsageEnablerBrowserAgent::FromBrowser(
+            browserProviderInterface.mainBrowserProvider.browser)
+            ->SetWebUsageEnabled(true);
+      }
+      if (browserProviderInterface.incognitoBrowserProvider.browser) {
+        WebUsageEnablerBrowserAgent::FromBrowser(
+            browserProviderInterface.incognitoBrowserProvider.browser)
+            ->SetWebUsageEnabled(true);
+      }
       [browserProviderInterface.currentBrowserProvider setPrimary:YES];
     }
     // `completionBlock` is run once, not once per scene.
