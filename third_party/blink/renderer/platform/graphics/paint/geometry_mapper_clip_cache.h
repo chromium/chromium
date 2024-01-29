491dc771d9ffe77e0a3da326f77a259e4811bffc
Kalvin Lee <kdlee@chromium.org>
2024-01-29 06:37:11 +0000

    MiraclePtr: Unrewrite `GeometryMapperClipCache`
    Looking at dev channel stack samples (admittedly from Windows), these
    appear to be hot paths. Try unrewriting them to see if we can recover
    any performance.
    
    Bug: 1521616
    
    (cherry picked from commit a8635cc5e37ed2e82892be93230c81bb081068a4)
    
    Change-Id: I99421ef17b307ec224f583e39b34bde95a74504c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5216686
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1251279}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240085
    Owners-Override: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6261@{#207}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- third_party/blink/renderer/platform/graphics/paint/geometry_mapper_clip_cache.h
+++ third_party/blink/renderer/platform/graphics/paint/geometry_mapper_clip_cache.h
@@ -6,6 +6,7 @@
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_GEOMETRY_MAPPER_CLIP_CACHE_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
 #include "third_party/blink/renderer/platform/graphics/overlay_scrollbar_clip_behavior.h"
 #include "third_party/blink/renderer/platform/graphics/paint/float_clip_rect.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -32,9 +33,13 @@ class PLATFORM_EXPORT GeometryMapperClipCache {
     DISALLOW_NEW();
 
    public:
-    raw_ptr<const ClipPaintPropertyNode, DanglingUntriaged> ancestor_clip;
-    raw_ptr<const TransformPaintPropertyNode, ExperimentalRenderer>
-        ancestor_transform;
+    // RAW_PTR_EXCLUSION: found prominently in profiled stack samples
+    // on Windows Dev.
+    //
+    // TODO(crbug.com/1489080): `ancestor_clip` was marked
+    // `DanglingUntriaged` before being unrewritten.
+    RAW_PTR_EXCLUSION const ClipPaintPropertyNode* ancestor_clip;
+    RAW_PTR_EXCLUSION const TransformPaintPropertyNode* ancestor_transform;
     OverlayScrollbarClipBehavior clip_behavior;
     bool operator==(const ClipAndTransform& other) const {
       return ancestor_clip == other.ancestor_clip &&
