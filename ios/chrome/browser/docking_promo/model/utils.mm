361ecade0e34f67a598d422b2356d146840cd0e2
Benjamin Williams <bwwilliams@google.com>
2024-01-29 15:36:47 +0000

    [iOS] Add Docking Promo to Experimental Settings.
    This change enables the Docking Promo to be forced via Experimental
    Settings, making manual testing possible.
    
    This change also updates CanShowDockingPromo() to respect the forced
    Docking Promo state, if it exists.
    
    Change-Id: I4b8c8957f2da0413dc967efc53d71fbc0ea1e4e7
    Fixed: 1521351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5231466
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1253290}

--- ios/chrome/browser/docking_promo/model/utils.mm
+++ ios/chrome/browser/docking_promo/model/utils.mm
@@ -4,10 +4,29 @@
 
 #import "ios/chrome/browser/docking_promo/model/utils.h"
 
+#import <optional>
+
+#import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
+#import "ios/chrome/browser/promos_manager/constants.h"
 #import "ios/chrome/browser/shared/model/utils/first_run_util.h"
+#import "ios/chrome/browser/shared/public/features/system_flags.h"
 
 BOOL CanShowDockingPromo(base::TimeDelta time_since_last_foreground) {
+  NSString* forced_promo_name = experimental_flags::GetForcedPromoToDisplay();
+
+  if ([forced_promo_name length] > 0) {
+    std::optional<promos_manager::Promo> forced_promo =
+        promos_manager::PromoForName(
+            base::SysNSStringToUTF8(forced_promo_name));
+
+    if (!forced_promo.has_value()) {
+      return NO;
+    }
+
+    return forced_promo.value() == promos_manager::Promo::DockingPromo;
+  }
+
   if (IsChromeLikelyDefaultBrowser()) {
     return NO;
   }
