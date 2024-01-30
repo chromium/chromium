c6952591ca4bd4711594012060176429b92530d8
dpapad <dpapad@chromium.org>
2024-01-30 02:42:52 +0000

    CrOS Cellular Setup: Move to a dedicated build_webui() invocation.
    Now that this folder is fully migrated to TS, it should have its own
    build_webui() invocation, instead of having its build configuration in
    its parent folder.
    
    Also deleting obsolete js_library() targets, which were already not
    hooked up anywhere in the build, as well as unnecessary entries to
    generate TS definitions files.
    
    Bug: b/314833224
    Change-Id: I23f1c58ece410bb938b1b60fe87c5f1b15edbede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241903
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1253700}

--- ash/webui/common/resources/cellular_setup/base_page.ts
+++ ash/webui/common/resources/cellular_setup/base_page.ts
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 /** Base template with elements common to all Cellular Setup flow sub-pages. */
-import '//resources/ash/common/cellular_setup/cellular_setup_icons.html.js';
+import './cellular_setup_icons.html.js';
 import '//resources/cr_elements/chromeos/cros_color_overrides.css.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
 import '//resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
