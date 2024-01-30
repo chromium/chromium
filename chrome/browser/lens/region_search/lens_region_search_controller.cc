chromium/chromium

commit 9f8bea8be72e92e13b8cc5f9c637a2b16dc58b3e

[M114-LTS] Fix region search controller using dangling browser pointer.
Gets the browser on every start instead of storing it as a raw_ptr.

(cherry picked from commit 444f737100c497f9ba3433b3a474b3c0f2cac23a)

Bug: 1494565, b:307537320
Change-Id: I1844ecaa0a66e439d49c8dd4c1c64ad28d796f7b
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4973698
Commit-Queue: Juan Mojica <juanmojica@google.com>
Cr-Original-Commit-Position: refs/heads/main@{#1214562}
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5207785
Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
Reviewed-by: Michael Ershov <miersh@google.com>
Owners-Override: Michael Ershov <miersh@google.com>
Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
Cr-Commit-Position: refs/branch-heads/5735@{#1672}
Cr-Branched-From: 2f562e4ddbaf79a3f3cb338b4d1bd4398d49eb67-refs/heads/main@{#1135570}

chrome/browser/lens/region_search/lens_region_search_controller.cc
@@ -11,6 +11,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/image_editor/screenshot_flow.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/lens/lens_side_panel_helper.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "components/lens/lens_entrypoints.h"
@@ -32,8 +33,7 @@ LensRegionSearchControllerData::~LensRegionSearchControllerData() = default;
 RegionSearchCapturedData::RegionSearchCapturedData() = default;
 RegionSearchCapturedData::~RegionSearchCapturedData() = default;
 
-LensRegionSearchController::LensRegionSearchController(Browser* browser)
-    : browser_(browser) {
+LensRegionSearchController::LensRegionSearchController() {
   weak_this_ = weak_factory_.GetWeakPtr();
 }
 
@@ -47,7 +47,11 @@ void LensRegionSearchController::Start(content::WebContents* web_contents,
   is_google_default_search_provider_ = is_google_default_search_provider;
   // Return early if web contents/browser don't exist and if capture mode is
   // already active.
-  if (!web_contents || !browser_ || in_capture_mode_) {
+  if (!web_contents || in_capture_mode_) {
+    return;
+  }
+  Browser* browser = chrome::FindBrowserWithTab(web_contents);
+  if (!browser) {
     return;
   }
 
@@ -66,7 +70,7 @@ void LensRegionSearchController::Start(content::WebContents* web_contents,
     // Create user education bubble anchored to the toolbar container.
     // This is only done for non-fulllscreen capture.
     bubble_widget_ = lens::OpenLensRegionSearchInstructions(
-        browser_,
+        browser,
         base::BindOnce(&LensRegionSearchController::Close,
                        base::Unretained(this)),
         base::BindOnce(&LensRegionSearchController::Escape,

chrome/browser/lens/region_search/lens_region_search_controller.h
@@ -13,8 +13,6 @@
 #include "ui/gfx/image/image.h"
 #include "ui/views/widget/widget.h"
 
-class Browser;
-
 namespace content {
 class WebContents;
 enum class Visibility;
@@ -28,7 +26,7 @@ namespace lens {
 
 class LensRegionSearchController : public content::WebContentsObserver {
  public:
-  explicit LensRegionSearchController(Browser* browser);
+  LensRegionSearchController();
   ~LensRegionSearchController() override;
 
   // Creates and runs the drag and capture flow. When run, the user enters into
@@ -97,8 +95,6 @@ class LensRegionSearchController : public content::WebContentsObserver {
   bool in_capture_mode_ = false;
 
   std::unique_ptr<image_editor::ScreenshotFlow> screenshot_flow_;
-
-  raw_ptr<Browser> browser_ = nullptr;
 
   raw_ptr<views::Widget> bubble_widget_ = nullptr;
 

chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3795,7 +3795,7 @@ void RenderViewContextMenu::ExecRegionSearch(
 
   if (!lens_region_search_controller_) {
     lens_region_search_controller_ =
-        std::make_unique<lens::LensRegionSearchController>(browser);
+        std::make_unique<lens::LensRegionSearchController>();
   }
   lens_region_search_controller_->Start(web_contents, use_fullscreen_capture,
                                         is_google_default_search_provider);

chrome/browser/ui/browser_commands.cc
@@ -2045,7 +2045,7 @@ void ExecLensRegionSearch(Browser* browser) {
     auto lens_region_search_controller_data =
         std::make_unique<lens::LensRegionSearchControllerData>();
     lens_region_search_controller_data->lens_region_search_controller =
-        std::make_unique<lens::LensRegionSearchController>(browser);
+        std::make_unique<lens::LensRegionSearchController>();
     lens_region_search_controller_data->lens_region_search_controller->Start(
         contents, lens::features::IsLensFullscreenSearchEnabled(),
         search::DefaultSearchProviderIsGoogle(profile));

chrome/browser/ui/side_panel/companion/companion_tab_helper.cc
@@ -156,12 +156,9 @@ std::string CompanionTabHelper::GetTextQueryFromSearchUrl(
 void CompanionTabHelper::StartRegionSearch(content::WebContents* web_contents,
                                            bool use_fullscreen_capture) {
 #if BUILDFLAG(ENABLE_LENS_DESKTOP_GOOGLE_BRANDED_FEATURES)
-  // TODO(shaktisahu): Pass a UI entry point for accurate metrics.
-  Browser* browser = companion::GetBrowserForWebContents(web_contents);
-  CHECK(browser);
   if (!lens_region_search_controller_) {
     lens_region_search_controller_ =
-        std::make_unique<lens::LensRegionSearchController>(browser);
+        std::make_unique<lens::LensRegionSearchController>();
   }
   lens_region_search_controller_->Start(web_contents, use_fullscreen_capture,
                                         /*is_google_default_search_provider=*/

chrome/browser/ui/views/lens/lens_region_search_controller_unittest.cc
@@ -25,8 +25,7 @@ class LensRegionSearchControllerTest : public TestWithBrowserView {
 
     // Create an active web contents.
     AddTab(browser_view()->browser(), GURL("about:blank"));
-    controller_ =
-        std::make_unique<LensRegionSearchController>(browser_view()->browser());
+    controller_ = std::make_unique<LensRegionSearchController>();
     controller_->SetWebContentsForTesting(
         browser_view()->GetActiveWebContents());
   }

chrome/browser/ui/views/lens/lens_side_panel_helper.cc
@@ -65,10 +65,11 @@ views::Widget* OpenLensRegionSearchInstructions(
     Browser* browser,
     base::OnceClosure close_callback,
     base::OnceClosure escape_callback) {
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
+  CHECK(browser_view);
   // Our anchor should be the browser view's top container view. This makes sure
   // that we account for side panel width and the top container view.
-  views::View* anchor =
-      BrowserView::GetBrowserViewForBrowser(browser)->contents_web_view();
+  views::View* anchor = browser_view->contents_web_view();
   return views::BubbleDialogDelegateView::CreateBubble(
       std::make_unique<LensRegionSearchInstructionsView>(
           anchor, std::move(close_callback), std::move(escape_callback)));

chrome/browser/ui/views/lens/lens_static_page_controller.cc
@@ -80,7 +80,7 @@ void LensStaticPageController::StartRegionSearch(
   DCHECK(browser_);
   if (!lens_region_search_controller_) {
     lens_region_search_controller_ =
-        std::make_unique<lens::LensRegionSearchController>(browser_);
+        std::make_unique<lens::LensRegionSearchController>();
   }
   lens_region_search_controller_->Start(
       contents,

commit a414fe89a44d1ef872aa0b05272065e03a8b3e65

[M114-LTS] FIXUP: Fix region search controller using dangling browser pointer
The issue happens because commit 9f8bea8be72e92e13b8cc5f9c637a2b16dc58b3e
introduced  a line with chrome::FindBrowserWithTab, that is named
FindBrowserWithWebContents in the 114 branch.

The tryjobs didn't catch the build issue because it only happens with
the enable_lens_desktop_google_branded_features gn arg set.

Bug: b:322867570
Change-Id: I64390ef130c5417d34a425b191b027cca6b94966
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250118
Reviewed-by: Duncan Mercer <mercerd@google.com>
Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
Reviewed-by: Juan Mojica <juanmojica@google.com>
Cr-Commit-Position: refs/branch-heads/5735@{#1679}
Cr-Branched-From: 2f562e4ddbaf79a3f3cb338b4d1bd4398d49eb67-refs/heads/main@{#1135570}

chrome/browser/lens/region_search/lens_region_search_controller.cc
@@ -50,7 +50,7 @@ void LensRegionSearchController::Start(content::WebContents* web_contents,
   if (!web_contents || in_capture_mode_) {
     return;
   }
-  Browser* browser = chrome::FindBrowserWithTab(web_contents);
+  Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
   if (!browser) {
     return;
   }