baaadc72ca7bafb6b0a825e65b8bf7763d9fc58b
Nick Diego Yamane <nickdiego@igalia.com>
2024-01-30 03:33:30 +0000

    lacros: disable delegated out-of-window clip rect
    Delegated out-of-bounds clip rect does still have issues at Exo side, so
    disable it for now at Lacros to unblock beta release (see the linked
    crbug for further details), until the Exo-side issues get fixed.
    
    Bug: 1515452, 1457444
    Test: Full scroll down a 5+ pages long Google doc breaks tab drag.
    Change-Id: Ibf9813bacaf1b188438de93652ae02ca10897960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247723
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1253715}

--- ui/ozone/platform/wayland/ozone_platform_wayland.cc
+++ ui/ozone/platform/wayland/ozone_platform_wayland.cc
@@ -410,8 +410,10 @@ class OzonePlatformWayland : public OzonePlatform,
       properties.supports_clip_rect = buffer_manager_->supports_clip_rect();
       properties.supports_affine_transform =
           buffer_manager_->supports_affine_transform();
-      properties.supports_out_of_window_clip_rect =
-          buffer_manager_->supports_out_of_window_clip_rect();
+      // TODO(crbug.com/1515452): Hardcoded to false due to Exo-side DnD events
+      // target lookup issues. This should be re-enabled once they're fixed, via
+      // |buffer_manager_->supports_out_of_window_clip_rect()|.
+      properties.supports_out_of_window_clip_rect = false;
       properties.has_transformation_fix =
           buffer_manager_->has_transformation_fix();
     }
