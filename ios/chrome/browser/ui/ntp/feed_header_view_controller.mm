19c8d8e3c8ae8daae5d07adc7893748a69cdf2f5
Joemer Ramos <joemerramos@chromium.org>
2024-01-30 19:02:47 +0000

    Reland "[iOS] Refactor Methods With Suppressed Deprecation Warnings"
    This is a reland of commit 65285d39fef4f8dbdde212d9ec1cb41643a93010.
    UIButton.configuration needs to be set in order to use
    configurationHandler.
    
    Original change's description:
    > [iOS] Refactor Methods With Suppressed Deprecation Warnings
    >
    > These methods were used during the UIButtonConfiguration refactor and
    > should be replaced with non-deprecated properties. We ask that the
    > reviewer CPs this patchset and validates UI works as intended. We were
    > unsure of which specific UIButtons were touched.
    >
    > Bug: 1418068
    > Change-Id: Ie0288aaba60d239c88cecf63d705159b5161fa5d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5245293
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1253878}
    
    Bug: 1418068
    Fixed: 1523251
    Change-Id: Ib46742bc803bba2b018e6a6bd10eed01ddbf4dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250587
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254070}

--- ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
+++ ios/chrome/browser/ui/ntp/feed_header_view_controller.mm
@@ -37,8 +37,7 @@ const CGFloat kHiddenFeedLabelFontSize = 16;
 // The width of the label for when the feed is hidden.
 const CGFloat kHiddenFeedLabelWidth = 250;
 // Insets for header menu button.
-const CGFloat kHeaderMenuButtonInsetTopAndBottom = 2;
-const CGFloat kHeaderMenuButtonInsetSides = 2;
+const CGFloat kHeaderMenuButtonInset = 2;
 // The height of the header container. The content is unaffected.
 const CGFloat kDiscoverFeedHeaderHeight = 40;
 const CGFloat kCustomSearchEngineLabelHeight = 18;
@@ -339,28 +338,28 @@ NSInteger kFeedSymbolPointSize = 17;
 
 // Configures the feed header's menu button.
 - (void)configureMenuButton:(UIButton*)menuButton {
+  UIButtonConfiguration* buttonConfiguration =
+      [UIButtonConfiguration plainButtonConfiguration];
   menuButton.translatesAutoresizingMaskIntoConstraints = NO;
   menuButton.accessibilityIdentifier = kNTPFeedHeaderMenuButtonIdentifier;
   menuButton.accessibilityLabel =
       l10n_util::GetNSString(IDS_IOS_DISCOVER_FEED_MENU_ACCESSIBILITY_LABEL);
   if ([self.feedControlDelegate isFollowingFeedAvailable]) {
-    [menuButton setImage:DefaultSymbolTemplateWithPointSize(
-                             kMenuSymbol, kFeedSymbolPointSize)
-                forState:UIControlStateNormal];
-    menuButton.backgroundColor =
+    buttonConfiguration.image =
+        DefaultSymbolTemplateWithPointSize(kMenuSymbol, kFeedSymbolPointSize);
+    buttonConfiguration.background.backgroundColor =
         [[UIColor colorNamed:kGrey200Color] colorWithAlphaComponent:0.8];
     menuButton.layer.cornerRadius = kButtonSize / 2;
     menuButton.clipsToBounds = YES;
   } else {
     UIImage* menuIcon = DefaultSymbolTemplateWithPointSize(
         kSettingsFilledSymbol, kFeedSymbolPointSize);
-    [menuButton setImage:menuIcon forState:UIControlStateNormal];
-    menuButton.tintColor = [UIColor colorNamed:kGrey600Color];
-    UIEdgeInsets imageInsets = UIEdgeInsetsMake(
-        kHeaderMenuButtonInsetTopAndBottom, kHeaderMenuButtonInsetSides,
-        kHeaderMenuButtonInsetTopAndBottom, kHeaderMenuButtonInsetSides);
-    SetImageEdgeInsets(menuButton, imageInsets);
+    buttonConfiguration.image = menuIcon;
+    buttonConfiguration.baseForegroundColor =
+        [UIColor colorNamed:kGrey600Color];
+    buttonConfiguration.imagePadding = kHeaderMenuButtonInset;
   }
+  menuButton.configuration = buttonConfiguration;
   [menuButton addTarget:self
                  action:@selector(didTouchMenuButton)
        forControlEvents:UIControlEventTouchUpInside];
