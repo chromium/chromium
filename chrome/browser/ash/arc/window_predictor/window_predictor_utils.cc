e5471cfebf6f0115cc7eeb913995361d6b5d76f5
Shengsong Tan <sstan@chromium.org>
2024-01-31 01:32:11 +0000

    [M122]arc: Use tablet size as default launch bound type for google apps
    This CL change the launch bounds policy when the ARC size app
    pre-defined bound have not been synced to chrome. The reason is
    google app already optimized on tablet, and almost all chromeos devices
    are landscape style.
    
    BUG=b:320684570
    TEST=manuall test on ToT, print log.
    
    (cherry picked from commit 71f8b7573578f298dcf767dfccab00b5d18d7f40)
    
    Change-Id: Ibcb6f6a3e73cdc225ddce4e57e49c875ab6f8364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239894
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1253296}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251588
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Shengsong Tan <sstan@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6261@{#316}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
+++ chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
@@ -4,10 +4,17 @@
 
 #include "chrome/browser/ash/arc/window_predictor/window_predictor_utils.h"
 
+#include "base/strings/string_util.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "components/app_restore/app_restore_data.h"
 
 namespace arc {
+
+namespace {
+const char kPlayStorePrefix[] = "com.android.vending";
+const char kGoogleOfficialPrefix1[] = "com.android.google";
+const char kGoogleOfficialPrefix2[] = "com.google.android";
+}  // namespace
 
 bool CanLaunchGhostWindowByRestoreData(
     const app_restore::AppRestoreData& restore_data) {
@@ -16,6 +23,12 @@ bool CanLaunchGhostWindowByRestoreData(
       restore_data.window_state_type == chromeos::WindowStateType::kFullscreen;
   return not_need_bounds || restore_data.bounds_in_root.has_value() ||
          restore_data.current_bounds.has_value();
+}
+
+bool IsGoogleSeriesPackage(const std::string& package_name) {
+  return base::StartsWith(package_name, kPlayStorePrefix) ||
+         base::StartsWith(package_name, kGoogleOfficialPrefix1) ||
+         base::StartsWith(package_name, kGoogleOfficialPrefix2);
 }
 
 }  // namespace arc
