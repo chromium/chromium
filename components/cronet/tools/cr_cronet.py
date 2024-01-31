007862fcdcc49b0a3d1770abaf4691bfce642484
Mohannad Farrag <aymanm@google.com>
2024-01-31 11:36:00 +0000

    Separate methods in cr_cronet into utils
    * Those methods are util-like method which will be re-used by the gn2bp script
    in the following order: (1) gn gen args, (2) run command to get desc_*.json files. So
    instead of duplicating the methods over two different python files, this is a clean-up
    to start clean.
    
    Bug: 1523214
    Change-Id: Icbe797878d351fd3c0402aff28dfed8da8bd3037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5246290
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254445}

--- components/cronet/tools/cr_cronet.py
+++ components/cronet/tools/cr_cronet.py
@@ -2,47 +2,21 @@
 # Copyright 2014 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """
 cr_cronet.py - cr - like helper tool for cronet developers
 """
 
 import argparse
-import os
-import re
-import shlex
-import subprocess
 import sys
+import os
+import shlex
 from datetime import datetime
 
+REPOSITORY_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
 
-def quoted_args(args):
-  return ' '.join([shlex.quote(arg) for arg in args])
-
-
-def run(command, **kwargs):
-  print(command, kwargs)
-  return subprocess.call(command, **kwargs)
-
-
-def run_shell(command, extra_options=''):
-  command = command + ' ' + extra_options
-  print(command)
-  return os.system(command)
-
-
-def gn(out_dir, gn_args, gn_extra=None):
-  cmd = ['gn', 'gen', out_dir, '--args=%s' % gn_args]
-  if gn_extra:
-    cmd += gn_extra
-  return run(cmd)
-
-
-def build(out_dir, build_target, extra_options=None):
-  cmd = ['ninja', '-C', out_dir, build_target] + get_ninja_jobs_options()
-  if extra_options:
-    cmd += extra_options
-  return run(cmd)
+sys.path.insert(0, REPOSITORY_ROOT)
+from components.cronet.tools.utils import run, run_shell, android_gn_gen, build  # pylint: disable=wrong-import-position
 
 
 def install(out_dir):
@@ -54,6 +28,7 @@ def install(out_dir):
       'HOME': os.environ.get('HOME', '')
   }
   return run(cmd + ['CronetTestInstrumentation.apk'], env=env)
+
 
 def test(out_dir, extra_options):
   # Ideally we would fetch this path from somewhere. Though, that's not trivial
@@ -68,16 +43,14 @@ def test(out_dir, extra_options):
 
 
 def unittest(out_dir, extra_options):
-  return run([out_dir + '/bin/run_cronet_unittests_android'] +
-             extra_options)
+  return run([out_dir + '/bin/run_cronet_unittests_android'] + extra_options)
 
 
 def debug(extra_options):
-  return run(['build/android/adb_gdb', '--start',
-             '--activity=.CronetTestActivity',
-             '--program-name=CronetTest',
-             '--package-name=org.chromium.net'] +
-             extra_options)
+  return run([
+      'build/android/adb_gdb', '--start', '--activity=.CronetTestActivity',
+      '--program-name=CronetTest', '--package-name=org.chromium.net'
+  ] + extra_options)
 
 
 def stack(out_dir):
@@ -86,89 +59,21 @@ def stack(out_dir):
                    ' third_party/android_platform/development/scripts/stack')
 
 
-def use_goma():
-  goma_dir = (subprocess.check_output(['goma_ctl', 'goma_dir'])
-                        .decode('utf-8')
-                        .strip())
-  result = run(['goma_ctl', 'ensure_start'])
-  if not result:
-    return 'use_goma=true goma_dir="' + goma_dir + '" '
-  return ''
-
-
-def get_ninja_jobs_options():
-  if use_goma():
-    return ["-j1000"]
-  return []
-
-
-def map_config_to_android_builder(is_release, target_cpu):
-  target_cpu_to_base_builder = {
-      'x86': 'android-cronet-x86',
-      'x64': 'android-cronet-x64',
-      'arm': 'android-cronet-arm',
-      'arm64': 'android-cronet-arm64',
-      'riscv64': 'android-cronet-riscv64',
-  }
-  if target_cpu not in target_cpu_to_base_builder:
-    raise ValueError('Unsupported target CPU')
-
-  builder_name = target_cpu_to_base_builder[target_cpu]
-  if is_release:
-    builder_name += '-rel'
-  else:
-    builder_name += '-dbg'
-  return builder_name
-
-
-def filter_gn_args(gn_args):
-  gn_arg_matcher = re.compile("^.*=.*$")
-  # `mb_py lookup` prints out a bunch of metadata lines which we don't
-  # care about, we only want the GN args.
-  assert len(gn_args) > 4
-  actual_gn_args = gn_args[1:-3]
-  for line in gn_args:
-    if line in actual_gn_args:
-      assert gn_arg_matcher.match(line), \
-             f'Not dropping {line}, which does not look like a GN arg'
-    else:
-      assert not gn_arg_matcher.match(line), \
-             f'Dropping {line}, which looks like a GN arg'
-
-  return list(filter(lambda string: "remoteexec" not in string, actual_gn_args))
-
-
-def android_gn_gen(is_release, target_cpu, out_dir):
-  group_name = 'chromium.android'
-  mb_script = 'tools/mb/mb.py'
-  builder_name = map_config_to_android_builder(is_release, target_cpu)
-  # Ideally we would call `mb_py gen` directly, but we need to filter out the
-  # use_remoteexec arg, as that cannot be used in a local environment.
-  gn_args = subprocess.check_output([
-      'python3', mb_script, 'lookup', '-m', group_name, '-b', builder_name
-  ]).decode('utf-8').strip()
-  gn_args = filter_gn_args(gn_args.split("\n"))
-  return gn(out_dir, ' '.join(gn_args))
-
-
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('command',
-                      choices=['gn',
-                               'sync',
-                               'build',
-                               'install',
-                               'proguard',
-                               'test',
-                               'build-test',
-                               'unit',
-                               'build-unit',
-                               'stack',
-                               'debug',
-                               'build-debug'])
-  parser.add_argument('-d', '--out_dir', action='store',
+                      choices=[
+                          'gn', 'sync', 'build', 'install', 'proguard', 'test',
+                          'build-test', 'unit', 'build-unit', 'stack', 'debug',
+                          'build-debug'
+                      ])
+  parser.add_argument('-d',
+                      '--out_dir',
+                      action='store',
                       help='name of the build directory')
-  parser.add_argument('-x', '--x86', action='store_true',
+  parser.add_argument('-x',
+                      '--x86',
+                      action='store_true',
                       help='build for Intel x86 architecture')
   parser.add_argument('--x64',
                       action='store_true',
@@ -177,9 +82,13 @@ def main():
                       '--riscv64',
                       action='store_true',
                       help='build for riscv64 architecture')
-  parser.add_argument('-r', '--release', action='store_true',
+  parser.add_argument('-r',
+                      '--release',
+                      action='store_true',
                       help='use release configuration')
-  parser.add_argument('-a', '--asan', action='store_true',
+  parser.add_argument('-a',
+                      '--asan',
+                      action='store_true',
                       help='use address sanitizer')
 
   options, extra_options = parser.parse_known_args()
@@ -214,11 +123,11 @@ def main():
     else:
       out_dir = 'out/Debug' + out_dir_suffix
 
-  if (options.command=='gn'):
+  if (options.command == 'gn'):
     return android_gn_gen(options.release, target_cpu, out_dir)
-  if (options.command=='sync'):
+  if (options.command == 'sync'):
     return run(['git', 'pull', '--rebase']) or run(['gclient', 'sync'])
-  if (options.command=='build'):
+  if (options.command == 'build'):
     return build(out_dir, test_target, extra_options)
   if (options.command == 'install'):
     return install(out_dir)
