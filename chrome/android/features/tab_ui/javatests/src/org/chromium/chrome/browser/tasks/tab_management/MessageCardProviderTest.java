ccdeb5c5d0edc0c5bf51c8dbbdc1ad8a9867dd7a
Henrique Nakashima <hnakashima@chromium.org>
2024-01-31 15:37:58 +0000

    Revert "Android: Fix check for correct @RunsWith()"
    This reverts commit fa7c505eb236acc27c82aed8de754eedad8a71a0.
    
    Reason for revert: Breaks WebRTC: b/322449368
    
    Original change's description:
    > Android: Fix check for correct @RunsWith()
    >
    > We did already have a check for this, but it turns out it didn't
    > actually fail test runs. This change makes tests runs fail, and fixes
    > the few tests that were in violation.
    >
    > Bug: None
    > Change-Id: I83b606f429c727577e8cd3d00cc2c67a28b1edcf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237294
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1252796}
    
    Bug: None
    Change-Id: I6a4b1c0976f0b379d54821dad2ca81fb49343ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250094
    Owners-Override: Sam Maier <smaier@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254525}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardProviderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardProviderTest.java
@@ -24,7 +24,6 @@ import androidx.test.filters.SmallTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -34,7 +33,6 @@ import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tasks.tab_management.suggestions.TabSuggestion;
 import org.chromium.chrome.tab_ui.R;
-import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.MVCListAdapter;
@@ -46,7 +44,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /** Integration tests for MessageCardProvider component. */
 @DisableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION)
-@RunWith(ChromeJUnit4ClassRunner.class)
 public class MessageCardProviderTest extends BlankUiTestActivityTestCase {
     private static final int SUGGESTED_TAB_COUNT = 2;
 
