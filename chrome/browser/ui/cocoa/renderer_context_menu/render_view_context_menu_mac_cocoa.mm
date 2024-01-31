be036e90290c89cc5b03f0f0e73f45d5d7938684
Marijn Kruisselbrink <mek@chromium.org>
2024-01-31 02:49:10 +0000

    Don't rely on +load in ChromeSwizzleServiceMenuUpdater.
    Instead have an explicit +install method that is called
    from both the browser and app shim processes.
    Also move this interface to a separate file rather than
    as part of render_view_context_menu_mac_cocoa.mm.
    
    Bug: none
    Change-Id: I722be3f0b51d03231234f4a58fea07c60be0e12c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250881
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254330}

--- chrome/browser/ui/cocoa/renderer_context_menu/render_view_context_menu_mac_cocoa.mm
+++ chrome/browser/ui/cocoa/renderer_context_menu/render_view_context_menu_mac_cocoa.mm
@@ -7,16 +7,13 @@
 
 #include <utility>
 
-#import "base/apple/scoped_objc_class_swizzler.h"
 #include "base/compiler_specific.h"
 #include "base/mac/mac_util.h"
 #import "base/mac/scoped_sending_event.h"
 #import "base/message_loop/message_pump_apple.h"
-#include "base/no_destructor.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/current_thread.h"
 #include "chrome/browser/headless/headless_mode_util.h"
-#import "chrome/browser/mac/nsprocessinfo_additions.h"
 #include "content/public/browser/web_contents.h"
 #import "ui/base/cocoa/menu_controller.h"
 #include "ui/base/interaction/element_tracker_mac.h"
@@ -26,13 +23,6 @@
 #include "ui/views/widget/widget.h"
 
 namespace {
-
-base::apple::ScopedObjCClassSwizzler* g_populatemenu_swizzler = nullptr;
-
-// |g_filtered_entries_array| is only set during testing (see
-// +[ChromeSwizzleServicesMenuUpdater storeFilteredEntriesForTestingInArray:]).
-// Otherwise it remains nil.
-NSMutableArray* g_filtered_entries_array = nil;
 
 // Retrieves an NSMenuItem which has the specified command_id. This function
 // traverses the given |model| in the depth-first order. When this function
@@ -59,103 +49,6 @@ NSMenuItem* GetMenuItemByID(ui::MenuModel* model,
 }
 
 }  // namespace
-
-// An AppKit-private class that adds Services items to contextual menus and
-// the application Services menu.
-@interface _NSServicesMenuUpdater : NSObject
-- (void)populateMenu:(NSMenu*)menu
-    withServiceEntries:(NSArray*)entries
-            forDisplay:(BOOL)display;
-@end
-
-// An AppKit-private class representing a Services menu entry.
-@interface _NSServiceEntry : NSObject
-- (NSString*)bundleIdentifier;
-@end
-
-@implementation ChromeSwizzleServicesMenuUpdater
-
-- (void)populateMenu:(NSMenu*)menu
-    withServiceEntries:(NSArray*)entries
-            forDisplay:(BOOL)display {
-  NSMutableArray* remainingEntries = [NSMutableArray array];
-  [g_filtered_entries_array removeAllObjects];
-
-  // Remove some services.
-  //   - Remove the ones from Safari, as they are redundant to the ones provided
-  //     by Chromium, and confusing to the user due to them switching apps
-  //     upon their selection.
-  //   - Remove the "Open URL" one provided by SystemUIServer, as it is
-  //     redundant to the one provided by Chromium and has other serious issues.
-  //     (https://crbug.com/960209)
-
-  for (_NSServiceEntry* nextEntry in entries) {
-    NSString* bundleIdentifier = [nextEntry bundleIdentifier];
-    NSString* message = [nextEntry valueForKey:@"message"];
-    bool shouldRemove =
-        ([bundleIdentifier isEqualToString:@"com.apple.Safari"]) ||
-        ([bundleIdentifier isEqualToString:@"com.apple.systemuiserver"] &&
-         [message isEqualToString:@"openURL"]);
-
-    if (!shouldRemove) {
-      [remainingEntries addObject:nextEntry];
-    } else {
-      [g_filtered_entries_array addObject:nextEntry];
-    }
-  }
-
-  // Pass the filtered array along to the _NSServicesMenuUpdater.
-  g_populatemenu_swizzler->InvokeOriginal<void, NSMenu*, NSArray*, BOOL>(
-      self, _cmd, menu, remainingEntries, display);
-}
-
-+ (void)storeFilteredEntriesForTestingInArray:(NSMutableArray*)array {
-  g_filtered_entries_array = array;
-}
-
-+ (void)load {
-  // Swizzling should not happen in renderer processes.
-  if (![[NSProcessInfo processInfo] cr_isMainBrowserOrTestProcess])
-    return;
-
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    // Confirm that the AppKit's private _NSServiceEntry class exists. This
-    // class cannot be accessed at link time and is not guaranteed to exist in
-    // past or future AppKits so use NSClassFromString() to locate it. Also
-    // check that the class implements the bundleIdentifier method. The browser
-    // test checks for all of this as well, but the checks here ensure that we
-    // don't crash out in the wild when running on some future version of OS X.
-    // Odds are a developer will be running a newer version of OS X sooner than
-    // the bots - NOTREACHED() will get them to tell us if compatibility breaks.
-    if (![NSClassFromString(@"_NSServiceEntry")
-            instancesRespondToSelector:@selector(bundleIdentifier)]) {
-      NOTREACHED();
-      return;
-    }
-
-    // Perform similar checks on the AppKit's private _NSServicesMenuUpdater
-    // class.
-    SEL targetSelector = @selector(populateMenu:withServiceEntries:forDisplay:);
-    Class targetClass = NSClassFromString(@"_NSServicesMenuUpdater");
-    if (![targetClass instancesRespondToSelector:targetSelector]) {
-      NOTREACHED();
-      return;
-    }
-
-    // Replace the populateMenu:withServiceEntries:forDisplay: method in
-    // _NSServicesMenuUpdater with an implementation that can filter Services
-    // menu entries from contextual menus and elsewhere. Place the swizzler into
-    // a static so that it never goes out of scope, because the scoper's
-    // destructor undoes the swizzling.
-    Class swizzleClass = [ChromeSwizzleServicesMenuUpdater class];
-    static base::NoDestructor<base::apple::ScopedObjCClassSwizzler>
-        servicesMenuFilter(targetClass, swizzleClass, targetSelector);
-    g_populatemenu_swizzler = servicesMenuFilter.get();
-  });
-}
-
-@end
 
 // macOS implementation of the ToolkitDelegate.
 // This simply (re)delegates calls to RVContextMenuMac because they do not
