d6f682e5051eb9f26707241cd619ea1a58290b71
mark a. foltz <mfoltz@chromium.org>
2024-01-31 18:47:52 +0000

    [Camera] Add histograms to track effects support on Win/Mac.
    This CL adds three histograms to track support for OS level camera effects on Win/Mac, namely:
    - Background Blur
    - Face framing
    - Eye gaze correction
    
    Bug: b/322047922
    Change-Id: I75c8854f5f0fc57a212eb26c0b59ef244a0d92f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5230634
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254633}

--- services/video_capture/device_media_to_mojo_adapter.cc
+++ services/video_capture/device_media_to_mojo_adapter.cc
@@ -16,6 +16,7 @@
 #include "media/capture/video/video_capture_buffer_pool_impl.h"
 #include "media/capture/video/video_capture_buffer_pool_util.h"
 #include "media/capture/video/video_capture_buffer_tracker_factory_impl.h"
+#include "media/capture/video/video_capture_metrics.h"
 #include "media/capture/video/video_frame_receiver_on_task_runner.h"
 #include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "services/video_capture/public/cpp/receiver_mojo_to_media_adapter.h"
@@ -192,6 +193,10 @@ void DeviceMediaToMojoAdapter::StartInternal(
 
   device_->AllocateAndStart(requested_settings, std::move(device_client));
   device_started_ = true;
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+  device_->GetPhotoState(base::BindOnce(&media::LogCaptureDeviceEffects));
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 }
 
 void DeviceMediaToMojoAdapter::StopInProcess() {
