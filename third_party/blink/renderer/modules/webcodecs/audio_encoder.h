6feb06f520dc9e99ea8582f77b97adfa0e5028ac
Dale Curtis <dalecurtis@chromium.org>
2024-01-31 03:12:31 +0000

    Improve web exposed error messages for software audio encoders.
    There's no need to sanitize messages coming from our software
    encoders since they're all compile time generated (and don't
    include information like pointer addresses, etc).
    
    R=tguilbert
    
    Bug: 1522959
    Change-Id: Ib72f2343304e758d45fd41ad6c8211432afda8ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5251728
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1254335}

--- third_party/blink/renderer/modules/webcodecs/audio_encoder.h
+++ third_party/blink/renderer/modules/webcodecs/audio_encoder.h
@@ -94,6 +94,13 @@ class MODULES_EXPORT AudioEncoder final
       uint32_t reset_count,
       media::EncodedAudioBuffer encoded_buffer,
       absl::optional<media::AudioEncoder::CodecDescription> codec_desc);
+  DOMException* MakeOperationError(std::string error_msg,
+                                   media::EncoderStatus status);
+  DOMException* MakeEncodingError(std::string error_msg,
+                                  media::EncoderStatus status);
+
+  // True if MojoAudioEncoder is being used.
+  bool is_platform_encoder_ = false;
 };
 
 }  // namespace blink
