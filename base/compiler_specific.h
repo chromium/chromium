59f56d97fff01d8fed1de28f4a7865d62f80ad1a
danakj <danakj@chromium.org>
2024-02-01 15:31:35 +0000

    Add UNSAFE_BUFFERS and UNSAFE_BUFFER_USAGE macros for C++ safe buffers
    Functions annotated with UNSAFE_BUFFER_USAGE can not be called
    from code that is opted into C++ safe buffers, unless the callsite is
    wrapped with UNSAFE_BUFFERS(). All instances of UNSAFE_BUFFER_USAGE
    should come with a Safety comment explaining the requirements of the
    function that all callers must meet. All callers using the function
    inside UNSAFE_BUFFERS() should explain in a SAFETY comment how they
    meet the safety requirements of the unsafe function.
    
    Pointer arithmetic/indexing can not be done in code that is opted into
    C++ safe buffers either, unless the usage is wrapped with
    UNSAFE_BUFFERS(). A SAFETY comment should be written explaining how the
    preconditions of the unsafe pointer usage are met.
    
    **USE OF UNSAFE_BUFFERS() SHOULD BE VERY RARE** and should come with
    a "// SAFETY: " comment that explains how it is guaranteed to not cause
    an out-of-bounds security bug that is based on local analysis only, such
    as via CHECK()s. If the explanation requires cooperation of code that is
    not fully encapsulated close to the `UNSAFE_BUFFERS()` usage, it should
    be rejected and replaced with safer coding patterns or stronger
    guarantees.
    
    Using UNSAFE_BUFFERS() and getting it wrong gives a forever-path for
    attackers to write exploits against Chrome. Avoid it almost always. Use
    base::span<T> instead of T*.
    
    The UNSAFE_BUFFERS() macro can be used to narrowly scope a single
    expression or can be used to wrap a whole block.
    
    Wrapping an expression:
    
    ```
    // Does spooky stuff.
    //
    // # Safety
    // The pointer must be valid and point to an array of at least 5 `int`s.
    UNSAFE_BUFFER_USAGE int spooky_function(int* pointer);
    
    void f(span<int, 5> s) {
      // SAFETY: The spooky_function requires the pointer has 5 ints,
      // which is true because we have a span with at least 5 as
      // specified in the type.
      int x = UNSAFE_BUFFERS(spooky_function(s.data()));
      ...
    }
    ```
    
    Wrapping a block:
    
    ```
    // Does spooky stuff.
    //
    // # Safety
    // The pointer must be valid and point to an array of at least 5 `int`s.
    UNSAFE_BUFFER_USAGE int* spooky_function(int* pointer);
    
    void f(span<int, 5> s) {
      UNSAFE_BUFFERS({
        // SAFETY: The spooky_function requires the pointer has 5 ints,
        // which is true because we have a span with at least 5 as
        // specified in the type.
        int* x = UNSAFE_BUFFERS(spooky_function(pointer));
        hope_we_got_things_right(x[6u]);
      })
    }
    ```
    
    Adds a `unsafe_buffer_warning_header_allowlist` list of paths that
    will be considered system headers so that unsafe buffer usage in
    those headers does not prevent us from enabling the warning on our
    own code.
    
    R=dcheng@chromium.org
    
    Bug: 1490484
    Change-Id: Id5a808054f265674638e8236fa18a5e28e569dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249877
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255055}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -454,4 +454,64 @@ inline constexpr bool AnalyzerAssumeTrue(bool arg) {
 #define PRESERVE_MOST
 #endif
 
+// Functions should be marked with UNSAFE_BUFFER_USAGE when they lead to
+// out-of-bounds bugs when called with incorrect inputs.
+//
+// Ideally such functions should be paired with a safer version that works with
+// safe primitives like `base::span`. Otherwise, another safer coding pattern
+// should be documented along side the use of `UNSAFE_BUFFER_USAGE`.
+//
+// All functions marked with UNSAFE_BUFFER_USAGE should come with a safety
+// comment that explains the requirements of the function to prevent any chance
+// of an out-of-bounds bug. For example:
+// ```
+// // Function to do things between `input` and `end`.
+// //
+// // # Safety
+// // The `input` must point to an array with size at least 5. The `end` must
+// // point within the same allocation of `input` and not come before `input`.
+// ```
+#if defined(__clang__) && HAS_ATTRIBUTE(unsafe_buffer_usage)
+#define UNSAFE_BUFFER_USAGE [[clang::unsafe_buffer_usage]]
+#else
+#define UNSAFE_BUFFER_USAGE
+#endif
+
+// UNSAFE_BUFFERS() wraps code that violates the -Wunsafe-buffer-usage warning,
+// such as:
+// - pointer arithmetic,
+// - pointer subscripting, and
+// - calls to functions annotated with UNSAFE_BUFFER_USAGE.
+//
+// ** USE OF THIS MACRO SHOULD BE VERY RARE.** Reviewers should push back when
+// it is not strictly necessary. Prefer to use `base::span` instead of pointers,
+// or other safer coding patterns (like std containers) that avoid the
+// opportunity for out-of-bounds bugs to creep into the code. Any use of
+// UNSAFE_BUFFERS() can lead to a critical security bug if any assumptions are
+// wrong, or ever become wrong in the future.
+//
+// The macro should be used to wrap the minimum necessary code, to make it clear
+// what is unsafe, and prevent accidentally opting extra things out of the
+// warning.
+//
+// All usage of UNSAFE_BUFFERS() should come with a `// SAFETY: ...` comment
+// that explains how we have guaranteed (ideally directly above, with conditions
+// or CHECKs) that the pointer usage can never go out-of-bounds, or that the
+// requirements of the UNSAFE_BUFFER_USAGE function are met. If the safety
+// explanation requires cooperation of code that is not fully encapsulated close
+// to the UNSAFE_BUFFERS() usage, it should be rejected and replaced with safer
+// coding patterns or stronger guarantees.
+#if defined(__clang__)
+// clang-format off
+// Formatting is off so that we can put each _Pragma on its own line, as
+// recommended by the gcc docs.
+#define UNSAFE_BUFFERS(...)                  \
+  _Pragma("clang unsafe_buffer_usage begin") \
+  __VA_ARGS__                                \
+  _Pragma("clang unsafe_buffer_usage end")
+// clang-format on
+#else
+#define UNSAFE_BUFFERS(...) __VA_ARGS__
+#endif
+
 #endif  // BASE_COMPILER_SPECIFIC_H_
