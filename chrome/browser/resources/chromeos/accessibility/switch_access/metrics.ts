a657d2ae882f40ce4354d5d9f77080d0fa6d586a
Anastasia Helfinstein <anastasi@google.com>
2024-02-01 00:17:28 +0000

    [Switch Access] Convert Metrics to TypeScript
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: Ifbd47d3440811a3208c0f94b45254ed90d926a19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5182976
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Jacob Francis <francisjp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1254835}

--- chrome/browser/resources/chromeos/accessibility/switch_access/metrics.ts
+++ chrome/browser/resources/chromeos/accessibility/switch_access/metrics.ts
@@ -2,18 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {StringUtil} from '../common/string_util.js';
+import {StringUtil} from '/common/string_util.js';
 
 /**
  * Class to record metrics for Switch Access.
  */
-export const SwitchAccessMetrics = {
-  /**
-   * @param {string} menuAction
-   */
-  recordMenuAction: menuAction => {
+export namespace SwitchAccessMetrics {
+  export function recordMenuAction(menuAction: string): void {
     const metricName = 'Accessibility.CrosSwitchAccess.MenuAction.' +
         StringUtil.toUpperCamelCase(menuAction);
     chrome.metricsPrivate.recordUserAction(metricName);
-  },
-};
+  }
+}
