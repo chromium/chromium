cca330538b0cb21a7d12bbe9ed3461b258cf1c0c
Fritz Koenig <frkoenig@chromium.org>
2024-02-01 17:52:34 +0000

    v4l2/stateless: Move blocking dequeue to dedicated threads
    VIDIOC_DQBUF calls are blocking. They wait for a buffer to be
    done processing. |CreateSequencedTaskRunner| can share a thread.
    When thread usage goes up, these blocking calls prevent other
    task runners from working.
    
    Cq-Include-Trybots: luci.chromium.try:linux-v4l2-codec-rel
    Bug: b:278935312
    Change-Id: Ie2717591c75cddc304483490283dfd82f3ab6514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5253818
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255126}

--- media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.cc
+++ media/gpu/v4l2/stateless/v4l2_stateless_video_decoder.cc
@@ -182,16 +182,22 @@ void V4L2StatelessVideoDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
   const int32_t bitstream_id =
       bitstream_id_generator_.GenerateNextId().GetUnsafeValue();
 
+  // |input_queue_task_runner_| and |output_queue_task_runner_| block on
+  // dequeuing a kernel ioctl call (VIDIOC_DQBUF). These don't need to be true
+  // task runners as there is never anything posted to those runners. They wait
+  // for an event and then post messages to the main task runner. Using task
+  // runners requires having a dedicated thread to prevent other runners that
+  // are put on the same thread from being blocked unintentionally.
   if (!input_queue_task_runner_) {
-    input_queue_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
-        {base::MayBlock(), base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
-    CHECK(input_queue_task_runner_);
+    input_queue_task_runner_ = base::ThreadPool::CreateSingleThreadTaskRunner(
+        {base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+        base::SingleThreadTaskRunnerThreadMode::DEDICATED);
   }
 
   if (!output_queue_task_runner_) {
-    output_queue_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
-        {base::MayBlock(), base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
-    CHECK(output_queue_task_runner_);
+    output_queue_task_runner_ = base::ThreadPool::CreateSingleThreadTaskRunner(
+        {base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+        base::SingleThreadTaskRunnerThreadMode::DEDICATED);
   }
 
   decode_request_queue_.push(
