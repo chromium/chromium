3e0ed95772c79c1ee2b6fd267b4ec750912aac8e
Jack Yammine <jyammine@google.com>
2024-02-01 17:13:44 +0000

    Reland "[waffle]Do not display the search engine choice page if policies are set"
    This is a reland of commit 084d8728e2d8b0f4551d60dd5557703eb298d438
    
    Original change's description:
    > [waffle]Do not display the search engine choice page if policies are set
    >
    > We check whether the default search provider is null (that means the
    > kDefaultSearchProviderEnabled is set to false) or that the default
    > search provider is set by a policy
    >
    > Screen recording: http://b/322958716#comment6
    >
    > Bug: b:322958716
    > Fixed: 1523304
    > Change-Id: Iceaab542e3f281afce423c32ed7416b460dca8f4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249792
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Auto-Submit: Jack Yammine <jyammine@google.com>
    > Commit-Queue: Jack Yammine <jyammine@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1254109}
    
    The build fail was caused by an unused function in CFT. We put that
    function behind a build flag to fix the issue.
    
    (cherry picked from commit 1abad73dc2ec2a2d0f688f7428e095f83f064a03)
    
    Change-Id: I4c2a3e632023ded28322433ae5d6cda77499d82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5250490
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1254453}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258317
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6261@{#378}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- components/search_engines/search_engine_choice/search_engine_choice_service.cc
+++ components/search_engines/search_engine_choice/search_engine_choice_service.cc
@@ -65,6 +65,16 @@ bool IsSearchEngineChoiceScreenAllowedByPolicy(
   }
   return false;
 }
+
+bool IsDefaultSearchProviderSetOrBlockedByPolicy(
+    const TemplateURLService& template_url_service) {
+  const TemplateURL* default_search_engine =
+      template_url_service.GetDefaultSearchProvider();
+
+  return !default_search_engine ||
+         default_search_engine->created_by_policy() ==
+             TemplateURLData::CreatedByPolicy::kDefaultSearchProvider;
+}
 #endif
 
 SearchEngineType GetDefaultSearchEngineType(
@@ -212,7 +222,8 @@ SearchEngineChoiceService::GetStaticChoiceScreenConditions(
     return SearchEngineChoiceScreenConditions::kSearchProviderOverride;
   }
 
-  if (!IsSearchEngineChoiceScreenAllowedByPolicy(policy_service)) {
+  if (!IsSearchEngineChoiceScreenAllowedByPolicy(policy_service) ||
+      IsDefaultSearchProviderSetOrBlockedByPolicy(template_url_service)) {
     return SearchEngineChoiceScreenConditions::kControlledByPolicy;
   }
 
@@ -231,6 +242,10 @@ SearchEngineChoiceService::GetDynamicChoiceScreenConditions(
   // Don't show the dialog if the default search engine is set by an extension.
   if (template_url_service.IsExtensionControlledDefaultSearch()) {
     return SearchEngineChoiceScreenConditions::kExtensionControlled;
+  }
+
+  if (IsDefaultSearchProviderSetOrBlockedByPolicy(template_url_service)) {
+    return SearchEngineChoiceScreenConditions::kControlledByPolicy;
   }
 
   // Don't show the dialog if the user has a custom search engine set as
