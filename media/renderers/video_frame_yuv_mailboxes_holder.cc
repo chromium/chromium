b1f5b97f28a8838c4d511af9718e4a4ee7af1b57
Colin Blundell <blundell@chromium.org>
2024-02-02 08:05:32 +0000

    [//media] Add RASTER usage to VideoFrameYUVMailboxesHolder for !OOP-R
    This class should have RASTER SharedImage usage even if OOP-R is not
    enabled. This CL adds it under a killswitch to guard against any
    unexpected functional changes.
    
    Bug: 1524009
    Change-Id: I34eed5a752c6c6bda68c71329aece41c37ec8a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258177
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255469}

--- media/renderers/video_frame_yuv_mailboxes_holder.cc
+++ media/renderers/video_frame_yuv_mailboxes_holder.cc
@@ -31,6 +31,10 @@ namespace media {
 
 namespace {
 
+BASE_FEATURE(kAddSharedImageRasterUsageWithNonOOPR,
+             "AddSharedImageRasterUsageWithNonOOPR",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 viz::SharedImageFormat PlaneSharedImageFormat(int num_channels,
                                               bool supports_red) {
   switch (num_channels) {
@@ -163,6 +167,14 @@ void VideoFrameYUVMailboxesHolder::VideoFrameToMailboxes(
   } else {
     mailbox_usage = gpu::SHARED_IMAGE_USAGE_GLES2_READ |
                     gpu::SHARED_IMAGE_USAGE_GLES2_WRITE;
+    // RASTER_{READ, WRITE} usages should be included as these SharedImages are
+    // both read and written via raster, but historically these usages were not
+    // included. Currently in the process of adding with a killswitch.
+    // TODO(crbug.com/1524009): Remove this killswitch post-safe rollout.
+    if (base::FeatureList::IsEnabled(kAddSharedImageRasterUsageWithNonOOPR)) {
+      mailbox_usage = mailbox_usage | gpu::SHARED_IMAGE_USAGE_RASTER_READ |
+                      gpu::SHARED_IMAGE_USAGE_RASTER_WRITE;
+    }
   }
 
   // Enabled with flags UseWritePixelsYUV and
