44741f68aeaf8b46c8dfded47db00af6b5e4eca4
Colin Blundell <blundell@chromium.org>
2024-02-02 10:04:30 +0000

    [//gpu] Refine egl_image_backing_factory_unittest RASTER SI usage
    Bug: 1519074
    Change-Id: I47ffe2ba46c39e9764d9386b3fdb6c8127bab823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258420
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255492}

--- gpu/command_buffer/service/shared_image/egl_image_backing_factory_unittest.cc
+++ gpu/command_buffer/service/shared_image/egl_image_backing_factory_unittest.cc
@@ -671,12 +671,11 @@ CreateAndValidateSharedImageRepresentations::
   SkAlphaType alpha_type = kPremul_SkAlphaType;
   gpu::SurfaceHandle surface_handle = gpu::kNullSurfaceHandle;
 
-  // SHARED_IMAGE_USAGE_DISPLAY_READ for skia read and
-  // SHARED_IMAGE_USAGE_RASTER_READ | SHARED_IMAGE_USAGE_RASTER_WRITE for skia
-  // write. Tests that use this class also write to the created SharedImage via
+  // SHARED_IMAGE_USAGE_DISPLAY_READ for modeling skia read via the display
+  // compositor and SHARED_IMAGE_USAGE_RASTER_WRITE for modeling skia write via
+  // raster. Tests that use this class also write to the created SharedImage via
   // GL.
   uint32_t usage = SHARED_IMAGE_USAGE_GLES2_WRITE |
-                   SHARED_IMAGE_USAGE_RASTER_READ |
                    SHARED_IMAGE_USAGE_RASTER_WRITE;
   if (!is_thread_safe)
     usage |= SHARED_IMAGE_USAGE_DISPLAY_READ;
