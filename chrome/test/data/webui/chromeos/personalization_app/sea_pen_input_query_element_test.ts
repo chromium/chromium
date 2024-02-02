dfd9765ca6c941db831d8a95c7fc34ec6495b92b
Jason Thai <jasontt@chromium.org>
2024-02-02 00:21:18 +0000

    p13n: SeaPen search button update #2
    Apply the same treatment as https://crrev.com/c/5239205 for free form
    text input.
    
    BUG=b:321301133
    TEST=browser_tests --gtest_filter="*SeaPenTextInput*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I655cfbb88a0a7a085ab3652bef18dbc48b9b382b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5260468
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1255369}

--- chrome/test/data/webui/chromeos/personalization_app/sea_pen_input_query_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/sea_pen_input_query_element_test.ts
@@ -9,14 +9,21 @@ import {SeaPenInputQueryElement, SeaPenPaths} from 'chrome://personalization/js/
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
+import {TestPersonalizationStore} from 'test_personalization_store.js';
+import {TestSeaPenProvider} from 'test_sea_pen_interface_provider.js';
 
-import {initElement, teardownElement} from './personalization_app_test_utils.js';
+import {baseSetup, initElement, teardownElement} from './personalization_app_test_utils.js';
 
 suite('SeaPenInputQueryElementTest', function() {
   let seaPenInputQueryElement: SeaPenInputQueryElement|null;
+  let personalizationStore: TestPersonalizationStore;
+  let seaPenProvider: TestSeaPenProvider;
 
   setup(function() {
     loadTimeData.overrideValues({isSeaPenTextInputEnabled: true});
+    const mocks = baseSetup();
+    personalizationStore = mocks.personalizationStore;
+    seaPenProvider = mocks.seaPenProvider;
   });
 
   teardown(async () => {
@@ -24,7 +31,7 @@ suite('SeaPenInputQueryElementTest', function() {
     seaPenInputQueryElement = null;
   });
 
-  test('displays search buttonon root page', async () => {
+  test('displays search button on root page', async () => {
     seaPenInputQueryElement =
         initElement(SeaPenInputQueryElement, {path: SeaPenPaths.ROOT});
     await waitAfterNextRender(seaPenInputQueryElement);
@@ -38,15 +45,34 @@ suite('SeaPenInputQueryElementTest', function() {
   });
 
   test('displays search again button on results page', async () => {
+    personalizationStore.data.wallpaper.seaPen.thumbnails =
+        seaPenProvider.images;
     seaPenInputQueryElement =
         initElement(SeaPenInputQueryElement, {path: SeaPenPaths.RESULTS});
     await waitAfterNextRender(seaPenInputQueryElement);
 
-    const searchButton = seaPenInputQueryElement.shadowRoot!.querySelector(
-                             '#searchButton') as HTMLElement;
-
+    const searchButton =
+        seaPenInputQueryElement.shadowRoot!.querySelector<HTMLElement>(
+            '#searchButton');
+    const icon = searchButton!.querySelector<HTMLElement>('iron-icon');
     assertEquals(
         seaPenInputQueryElement.i18n('seaPenRecreateButton'),
         searchButton!.innerText);
+    assertEquals('personalization-shared:refresh', icon!.getAttribute('icon'));
+  });
+
+  test('displays create button when no thumbnails are generated', async () => {
+    seaPenInputQueryElement =
+        initElement(SeaPenInputQueryElement, {path: SeaPenPaths.RESULTS});
+    await waitAfterNextRender(seaPenInputQueryElement);
+
+    const searchButton =
+        seaPenInputQueryElement.shadowRoot!.querySelector<HTMLElement>(
+            '#searchButton');
+    const icon = searchButton!.querySelector<HTMLElement>('iron-icon');
+    assertEquals(
+        seaPenInputQueryElement.i18n('seaPenCreateButton'),
+        searchButton!.innerText);
+    assertEquals('sea-pen:photo-spark', icon!.getAttribute('icon'));
   });
 });
