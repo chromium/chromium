302e66d83cbe98e04911b0bc25c5108f88fd6237
Sandor Major <sandormajor@google.com>
2024-02-05 07:36:42 +0000

    Check if LegacyTechService exists before using
    (cherry picked from commit 70eae2526a18a1822c7448b950bcced757e62004)
    
    Bug: b/41496058, 1523104
    Change-Id: I90a6fadaa51f3a3683193b22bdfcc2a6a2a10764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5254599
    Auto-Submit: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Johann Hofmann <johannhof@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Johann Hofmann <johannhof@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1254992}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5267166
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6261@{#460}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -6988,9 +6988,13 @@ void ChromeContentBrowserClient::ReportLegacyTechEvent(
   if (!profile) {
     return;
   }
-  enterprise_reporting::LegacyTechServiceFactory::GetForProfile(profile)
-      ->ReportEvent(type, url, frame_url, filename, line, column,
-                    cookie_issue_details);
+  enterprise_reporting::LegacyTechService* service =
+      enterprise_reporting::LegacyTechServiceFactory::GetForProfile(profile);
+  if (!service) {
+    return;
+  }
+  service->ReportEvent(type, url, frame_url, filename, line, column,
+                       cookie_issue_details);
 }
 
 bool ChromeContentBrowserClient::CanAcceptUntrustedExchangesIfNeeded() {
