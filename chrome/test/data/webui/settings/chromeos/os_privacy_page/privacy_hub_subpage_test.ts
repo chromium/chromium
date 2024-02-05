a3fb70f94deb56c2052b4406f49765bca8bd0679
Md Shahadat Hossain Shahin <shahinmd@google.com>
2024-02-05 10:22:27 +0000

    privacy-hub: Fix camera toggle aria description issue
    This CL fixes the issue of wrong aria description for the camera toggle
    in the Privacy controls page and also adds tests to avoid future
    regression.
    
    Bug: b:323378073
    Change-Id: I0df705108d2b79ca0e259f8b3b368a449eeaf8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5261890
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Md Shahadat Hossain Shahin <shahinmd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256106}

--- chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_subpage_test.ts
+++ chrome/test/data/webui/settings/chromeos/os_privacy_page/privacy_hub_subpage_test.ts
@@ -893,6 +893,34 @@ suite('<settings-privacy-hub-subpage> AllBuilds app permissions', () => {
         getMicrophoneRowSubtext());
   });
 
+  function getMicrophoneToggleAriaLabel(): string {
+    return getMicrophoneCrToggle().getAttribute('aria-label')!.trim();
+  }
+
+  function getMicrophoneToggleAriaDescription(): string {
+    return getMicrophoneCrToggle().getAttribute('aria-description')!.trim();
+  }
+
+  test('Microphone toggle aria label and description', async () => {
+    mediaDevices.addDevice('audioinput', 'Fake Mic');
+    await flushTasks();
+
+    assertEquals(
+        privacyHubSubpage.i18n('microphoneToggleTitle'),
+        getMicrophoneToggleAriaLabel());
+    assertEquals(
+        getMicrophoneRowSubtext(), getMicrophoneToggleAriaDescription());
+
+    getMicrophoneCrToggle().click();
+    flush();
+
+    assertEquals(
+        privacyHubSubpage.i18n('microphoneToggleTitle'),
+        getMicrophoneToggleAriaLabel());
+    assertEquals(
+        getMicrophoneRowSubtext(), getMicrophoneToggleAriaDescription());
+  });
+
   test('Camera row subtext', async () => {
     mediaDevices.addDevice('videoinput', 'Fake Camera');
     await flushTasks();
@@ -925,6 +953,32 @@ suite('<settings-privacy-hub-subpage> AllBuilds app permissions', () => {
     assertEquals(
         privacyHubSubpage.i18n('privacyHubPageCameraRowFallbackSubtext'),
         getCameraRowSubtext());
+  });
+
+  function getCameraToggleAriaLabel(): string {
+    return getCameraCrToggle().getAttribute('aria-label')!.trim();
+  }
+
+  function getCameraToggleAriaDescription(): string {
+    return getCameraCrToggle().getAttribute('aria-description')!.trim();
+  }
+
+  test('Camera toggle aria label and description', async () => {
+    mediaDevices.addDevice('videoinput', 'Fake Camera');
+    await flushTasks();
+
+    assertEquals(
+        privacyHubSubpage.i18n('cameraToggleTitle'),
+        getCameraToggleAriaLabel());
+    assertEquals(getCameraRowSubtext(), getCameraToggleAriaDescription());
+
+    getCameraCrToggle().click();
+    flush();
+
+    assertEquals(
+        privacyHubSubpage.i18n('cameraToggleTitle'),
+        getCameraToggleAriaLabel());
+    assertEquals(getCameraRowSubtext(), getCameraToggleAriaDescription());
   });
 });
 
