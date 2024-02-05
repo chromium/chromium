86256041ebeb3dfa80b7f21c3da7304c307d2785
Akihiro Ota <akihiroota@chromium.org>
2024-02-05 20:41:14 +0000

    A11y: Convert pumpkin constants to TS
    Bug: b:267329383
    Test: Manually with dictation on ChromeOS (emulated on linux).
    Change-Id: I0793f88a7a36b07a0b67a2eca63d08e53f27f3ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263737
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256388}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/parse/pumpkin/pumpkin_constants.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/parse/pumpkin/pumpkin_constants.ts
@@ -12,77 +12,61 @@
  * third_party/closure_compiler/externs/accessibility_private.js
  * TODO(crbug.com/1258190): Consider creating a python script that would pull
  * this definition in at build time.
- * @typedef {{
- *   js_pumpkin_tagger_bin_js: ArrayBuffer,
- *   tagger_wasm_main_js: ArrayBuffer,
- *   tagger_wasm_main_wasm: ArrayBuffer,
- *   en_us_action_config_binarypb: ArrayBuffer,
- *   en_us_pumpkin_config_binarypb: ArrayBuffer,
- *   fr_fr_action_config_binarypb: ArrayBuffer,
- *   fr_fr_pumpkin_config_binarypb: ArrayBuffer,
- *   it_it_action_config_binarypb: ArrayBuffer,
- *   it_it_pumpkin_config_binarypb: ArrayBuffer,
- *   de_de_action_config_binarypb: ArrayBuffer,
- *   de_de_pumpkin_config_binarypb: ArrayBuffer,
- *   es_es_action_config_binarypb: ArrayBuffer,
- *   es_es_pumpkin_config_binarypb: ArrayBuffer
- * }}
  */
-export let PumpkinData;
+export interface PumpkinData {
+  js_pumpkin_tagger_bin_js: ArrayBuffer;
+  tagger_wasm_main_js: ArrayBuffer;
+  tagger_wasm_main_wasm: ArrayBuffer;
+  en_us_action_config_binarypb: ArrayBuffer;
+  en_us_pumpkin_config_binarypb: ArrayBuffer;
+  fr_fr_action_config_binarypb: ArrayBuffer;
+  fr_fr_pumpkin_config_binarypb: ArrayBuffer;
+  it_it_action_config_binarypb: ArrayBuffer;
+  it_it_pumpkin_config_binarypb: ArrayBuffer;
+  de_de_action_config_binarypb: ArrayBuffer;
+  de_de_pumpkin_config_binarypb: ArrayBuffer;
+  es_es_action_config_binarypb: ArrayBuffer;
+  es_es_pumpkin_config_binarypb: ArrayBuffer;
+}
 
-/**
- * The types of commands that can come from SandboxedPumpkinTagger.
- * @enum {string}
- */
-export const FromPumpkinTaggerCommand = {
-  READY: 'ready',
-  FULLY_INITIALIZED: 'fullyInitialized',
-  TAG_RESULTS: 'tagResults',
-  REFRESHED: 'refreshed',
-};
+/** The types of commands that can come from SandboxedPumpkinTagger. */
+export enum FromPumpkinTaggerCommand {
+  READY = 'ready',
+  FULLY_INITIALIZED = 'fullyInitialized',
+  TAG_RESULTS = 'tagResults',
+  REFRESHED = 'refreshed',
+}
 
-/**
- * The types of commands that can be sent to SandboxedPumpkinTagger.
- * @enum {string}
- */
-export const ToPumpkinTaggerCommand = {
-  LOAD: 'load',
-  TAG: 'tagAndGetNBestHypotheses',
-  REFRESH: 'refresh',
-};
+/** The types of commands that can be sent to SandboxedPumpkinTagger. */
+export enum ToPumpkinTaggerCommand {
+  LOAD = 'load',
+  TAG = 'tagAndGetNBestHypotheses',
+  REFRESH ='refresh',
+}
 
-/**
- * Defines the message data received from SandboxedPumpkinTagger.
- * @typedef {{
- *  results: (!Object|null|undefined),
- *  type: !FromPumpkinTaggerCommand,
- * }}
- */
-export let FromPumpkinTagger;
+/** Defines the message data received from SandboxedPumpkinTagger. */
+export interface FromPumpkinTagger {
+  results?: Object|null;
+  type: FromPumpkinTaggerCommand;
+}
 
-/**
- * Defines the message data sent to SandboxedPumpkinTagger.
- * @typedef {{
- *  locale: (!PumpkinLocale|undefined),
- *  numResults: (number|undefined),
- *  pumpkinData: (!PumpkinData|null|undefined),
- *  text: (string|undefined),
- *  type: !ToPumpkinTaggerCommand,
- * }}
- */
-export let ToPumpkinTagger;
+/** Defines the message data sent to SandboxedPumpkinTagger. */
+export interface ToPumpkinTagger {
+  locale?: PumpkinLocale;
+  numResults?: number;
+  pumpkinData?: PumpkinData|null;
+  text?: string;
+  type: ToPumpkinTaggerCommand;
+}
 
-/**
- * Supported Pumpkin locales.
- * @enum {string}
- */
-export const PumpkinLocale = {
-  EN_US: 'en_us',
-  FR_FR: 'fr_fr',
-  IT_IT: 'it_it',
-  DE_DE: 'de_de',
-  ES_ES: 'es_es',
-};
+/** Supported Pumpkin locales. */
+export enum PumpkinLocale {
+  EN_US = 'en_us',
+  FR_FR = 'fr_fr',
+  IT_IT = 'it_it',
+  DE_DE = 'de_de',
+  ES_ES = 'es_es',
+}
 
 /**
  * Map from BCP-47 locale code (see dictation.cc) to directory name in
@@ -90,7 +74,6 @@ export const PumpkinLocale = {
  * TODO(crbug.com/1264544): Determine if all en* languages can be mapped to
  * en_us. Possible locales are listed in dictation.cc,
  * kWebSpeechSupportedLocales.
- * @const {!Object<string, PumpkinLocale>}
  */
 export const SUPPORTED_LOCALES = {
   // English.
@@ -148,16 +131,14 @@ export const SUPPORTED_LOCALES = {
  * PumpkinTagger Hypothesis argument names. These should match the variable
  * argument placeholders in voiceaccess.patterns_template and the static strings
  * defined in voiceaccess/utils/PumpkinUtils.java in google3.
- * @enum {string}
  */
-export const HypothesisArgumentName = {
-  SEM_TAG: 'SEM_TAG',
-  NUM_ARG: 'NUM_ARG',
-  OPEN_ENDED_TEXT: 'OPEN_ENDED_TEXT',
-  BEGIN_PHRASE: 'BEGIN_PHRASE',
-  END_PHRASE: 'END_PHRASE',
-};
+export enum HypothesisArgumentName {
+  SEM_TAG = 'SEM_TAG',
+  NUM_ARG = 'NUM_ARG',
+  OPEN_ENDED_TEXT = 'OPEN_ENDED_TEXT',
+  BEGIN_PHRASE = 'BEGIN_PHRASE',
+  END_PHRASE = 'END_PHRASE',
+}
 
-/** @const {string} */
 export const SANDBOXED_PUMPKIN_TAGGER_JS_FILE =
     'dictation/parse/sandboxed_pumpkin_tagger.js';
