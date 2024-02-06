414dcf0f33de8bece74d1aed0114f65b19fb40d1
Huiting Yu <huitingyu@google.com>
2024-02-06 14:54:05 +0000

    Reland "[ios] Fix EGTest set up and checks for bottom/top omnibox positions."
    This is a reland of commit 34304be62b42604313775e535c19dcd42095afc5
    The previous failure was due to the wrong Setup/Teardown used for
    cleaning up. So the tests were flaky on bots.
    
    Original change's description:
    > [ios] Fix EGTest set up and checks for bottom/top omnibox positions.
    >
    > Before the bottom omnibox wasn't set up correctly.
    >
    > Refactor the code so that the top/bottom matchers can be re-used.
    >
    > Change-Id: Id55183264473295ca73786eb227a4f0ae9105330
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5018540
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Commit-Queue: Huiting Yu <huitingyu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1255551}
    
    Change-Id: Ib95c68beaad8b514e729acbb79bc365a7be7facd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5263482
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1256739}

--- ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_egtest.mm
@@ -14,6 +14,8 @@
 #import "build/build_config.h"
 #import "components/feature_engagement/public/feature_constants.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/iph_for_new_chrome_user/model/features.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_app_interface.h"
@@ -287,6 +289,14 @@ void FocusFakebox() {
 
   [ChromeEarlGrey clearPasteboard];
   [ChromeEarlGrey clearBrowsingHistory];
+
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kBottomOmnibox];
+}
+
+- (void)tearDown {
+  [super tearDown];
+
+  [ChromeEarlGrey setBoolValue:NO forUserPref:prefs::kBottomOmnibox];
 }
 
 #pragma mark - Helpers
@@ -558,6 +568,9 @@ void FocusFakebox() {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (no bottom omnibox in tablet)");
   }
+
+  [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kBottomOmnibox];
+
   // Enable the IPH Demo Mode feature to ensure the IPH triggers
   AppLaunchConfiguration config = [self appConfigurationForTestCase];
   config.additional_args.push_back(
@@ -629,6 +642,10 @@ void FocusFakebox() {
   [ChromeEarlGrey verifyStringCopied:base::SysUTF8ToNSString(_URL1.spec())];
 
   DefocusOmnibox();
+
+  // Verify the omnibox is at the top.
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxOnTop()];
 
   // Verify the share button IPH is shown.
   [ChromeEarlGrey
@@ -766,22 +783,26 @@ void FocusFakebox() {
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Skipped for iPad (no bottom omnibox in tablet)");
   }
+
+  [ChromeEarlGrey setBoolValue:YES forUserPref:prefs::kBottomOmnibox];
+
   // Enable the IPH Demo Mode feature to ensure the IPH triggers
   AppLaunchConfiguration config = [self appConfigurationForTestCase];
   config.additional_args.push_back(
       base::StringPrintf("--enable-features=%s:chosen_feature/"
-                         "IPH_iOSShareToolbarItemFeature,IPHForSafariSwitcher",
-                         feature_engagement::kIPHDemoMode.name));
+                         "IPH_iOSShareToolbarItemFeature",
+                         feature_engagement::kIPHDemoMode.name) +
+      "," + std::string(kIPHForSafariSwitcher.name));
+
   // Force the conditions that allow the iph to show.
   config.additional_args.push_back("-ForceExperienceForDeviceSwitcher");
   config.additional_args.push_back("SyncedAndFirstDevice");
 
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
 
-  GREYAssertTrue([ChromeEarlGrey isBottomOmniboxSteadyStateEnabled],
-                 @"Test case should have ran in bottom omnibox environment");
-
   [self openPage1];
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxAtBottom()];
 
   // Long pressing should allow copying.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::DefocusedLocationView()]
@@ -834,6 +855,10 @@ void FocusFakebox() {
   [[EarlGrey selectElementWithMatcher:CopyContextMenuButton()]
       performAction:grey_tap()];
   [ChromeEarlGrey verifyStringCopied:base::SysUTF8ToNSString(_URL1.spec())];
+
+  // Verify the omnibox is at the top.
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:chrome_test_util::OmniboxOnTop()];
 
   // Verify the share button IPH is shown.
   GREYAssert([ChromeEarlGrey testUIElementAppearanceWithMatcher:
