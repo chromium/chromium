2f9196f5b8767eac83ef12b56b84f35ef60cf717
Tsuyoshi Horo <horo@chromium.org>
2024-02-06 09:03:58 +0000

    Don't use test::RunUntil in SharedDictionaryAccessReportedCorrectly test
    The CL https://crrev.com/c/5205190 introduced test::RunUntil() in
    SharedDictionaryAccessReportedCorrectly test. But according to
    b/41492531, test::RunUntil is problematic on Mac. And the test went
    flaky on Mac.
    
    So this CL revives `while` loop.
    
    Bug: 41496998
    Change-Id: I4b96198fab40078fe508ce0a5f06c5fc4abaff36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269202
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256656}

--- chrome/browser/browsing_data/browsing_data_model_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_model_browsertest.cc
@@ -1452,16 +1452,8 @@ IN_PROC_BROWSER_TEST_P(BrowsingDataModelBrowserTest,
   ASSERT_EQ(browsing_data_model->size(), 0u);
 }
 
-// TODO(crbug.com/1524052): Flaky on at least Mac11 and Mac13.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_SharedDictionaryAccessReportedCorrectly \
-  DISABLED_SharedDictionaryAccessReportedCorrectly
-#else
-#define MAYBE_SharedDictionaryAccessReportedCorrectly \
-  SharedDictionaryAccessReportedCorrectly
-#endif  // BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_P(BrowsingDataModelBrowserTest,
-                       MAYBE_SharedDictionaryAccessReportedCorrectly) {
+                       SharedDictionaryAccessReportedCorrectly) {
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
       https_test_server()->GetURL(kTestHost, "/browsing_data/site_data.html")));
@@ -1510,8 +1502,9 @@ IN_PROC_BROWSER_TEST_P(BrowsingDataModelBrowserTest,
 
   // Need this polling because the shared dictionary is used only if the
   // metadata database has been read when sending the HTTP request.
-  EXPECT_TRUE(base::test::RunUntil(
-      [&]() { return HasDataForType("SharedDictionary", web_contents()); }));
+  while (!HasDataForType("SharedDictionary", web_contents())) {
+    base::PlatformThread::Sleep(TestTimeouts::tiny_timeout());
+  }
 
   // Checking HasDataForType("SharedDictionary") accesses the registered
   // shared dictionary. This must be reported to the data model.
