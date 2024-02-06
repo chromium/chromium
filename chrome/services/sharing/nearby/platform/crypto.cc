d2a64fa73fcae01d88ac4e320b382bd662177473
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-06 09:22:33 +0000

    Use `span`-based `base::MD5Sum`: //chrome/services.
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Bug: 1490484
    Change-Id: I1c03d6dd88a7d407942b39dbcd12d0489be1cba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5269779
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1256662}

--- chrome/services/sharing/nearby/platform/crypto.cc
+++ chrome/services/sharing/nearby/platform/crypto.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/nearby/src/internal/platform/implementation/crypto.h"
 
+#include "base/containers/span.h"
 #include "base/hash/md5.h"
 #include "base/memory/ptr_util.h"
 #include "crypto/sha2.h"
@@ -20,7 +21,7 @@ ByteArray Crypto::Md5(absl::string_view input) {
     return ByteArray();
 
   base::MD5Digest digest;
-  base::MD5Sum(input.data(), input.length(), &digest);
+  base::MD5Sum(base::as_byte_span(input), &digest);
   return ByteArray(std::string(std::begin(digest.a), std::end(digest.a)));
 }
 
