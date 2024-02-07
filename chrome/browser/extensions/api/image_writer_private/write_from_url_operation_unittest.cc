d21e2276715c6c4d593a89c3a577ff8174aa657f
Lukasz Anforowicz <lukasza@chromium.org>
2024-02-07 19:05:48 +0000

    Add missing `#include "base/containers/span.h"`.
    The call to `base::make_span` requires the `#include`.  (Everything
    compiles even before this CL, but per IWYU this `#include` should have
    been included in https://crrev.com/c/5270836.  Ooops.)
    
    Bug: None
    Change-Id: If383d452142fbdf0833a65dfdf5442deb7ab41f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276932
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257477}

--- chrome/browser/extensions/api/image_writer_private/write_from_url_operation_unittest.cc
+++ chrome/browser/extensions/api/image_writer_private/write_from_url_operation_unittest.cc
@@ -6,6 +6,7 @@
 
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "chrome/browser/extensions/api/image_writer_private/error_constants.h"
