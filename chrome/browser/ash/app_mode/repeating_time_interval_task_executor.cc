4ccd1adfd966e8de05591894023e11f3cc1567b4
Fahad Mansoor <fahadmansoor@google.com>
2024-02-07 19:19:10 +0000

    autosleep: Add constructor for RepeatingTimeIntervalTaskExecutor
    The constructor takes a time interval and a set of repeating callbacks.
    It will use the time interval to schedule and execute the start and end
    callbacks for when the system time enters/exits the specified time
    interval.
    
    Bug: b:319210159, b:319083748
    Change-Id: I75c356e9bf5ffc9512edac05d9c2ec186e888c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5222622
    Reviewed-by: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257492}

--- chrome/browser/ash/app_mode/repeating_time_interval_task_executor.cc
+++ chrome/browser/ash/app_mode/repeating_time_interval_task_executor.cc
@@ -4,10 +4,17 @@
 
 #include "chrome/browser/ash/app_mode/repeating_time_interval_task_executor.h"
 
+#include "base/functional/callback.h"
+
 namespace ash {
 
-RepeatingTimeIntervalTaskExecutor::RepeatingTimeIntervalTaskExecutor() =
-    default;
+RepeatingTimeIntervalTaskExecutor::RepeatingTimeIntervalTaskExecutor(
+    const policy::WeeklyTimeInterval& time_interval,
+    base::RepeatingClosure interval_start_callback,
+    base::RepeatingClosure interval_end_callback)
+    : time_interval_(time_interval),
+      interval_start_callback_(interval_start_callback),
+      interval_end_callback_(interval_end_callback) {}
 
 RepeatingTimeIntervalTaskExecutor::~RepeatingTimeIntervalTaskExecutor() =
     default;
--- chrome/browser/ash/app_mode/repeating_time_interval_task_executor.h
+++ chrome/browser/ash/app_mode/repeating_time_interval_task_executor.h
@@ -5,20 +5,27 @@
 #ifndef CHROME_BROWSER_ASH_APP_MODE_REPEATING_TIME_INTERVAL_TASK_EXECUTOR_H_
 #define CHROME_BROWSER_ASH_APP_MODE_REPEATING_TIME_INTERVAL_TASK_EXECUTOR_H_
 
+#include "base/functional/callback.h"
+#include "chromeos/ash/components/policy/weekly_time/weekly_time_interval.h"
+
 namespace ash {
 
-// `RepeatingTimeIntervalTaskExecutor` takes a time interval and a set of
-// repeating callbacks. When the device enters and exits the specified time
-// interval, this class invokes the provided start and end callbacks. This class
-// schedules the time interval using the system timezone. Changes to the system
-// timezone will make it reprogram the time interval.
-// TODO(b/319083748) Add constructor to accept intervals and callbacks.
+// When the device enters and exits the specified time interval, this class
+// invokes the provided `interval_start_callback` callback and
+// `interval_end_callback` callback respectively. This class schedules the time
+// interval using the system timezone. Changes to the system timezone will make
+// it reprogram the time interval.
 // TODO(b/319087271) Implement case when current time falls in interval.
 // TODO(b/319086751) Implement case when next interval is in the future.
 // TODO(b/319083880) Observe time zone changes and cancel pending executors.
 class RepeatingTimeIntervalTaskExecutor {
  public:
-  RepeatingTimeIntervalTaskExecutor();
+  RepeatingTimeIntervalTaskExecutor() = delete;
+
+  RepeatingTimeIntervalTaskExecutor(
+      const policy::WeeklyTimeInterval& time_interval,
+      base::RepeatingClosure interval_start_callback,
+      base::RepeatingClosure interval_end_callback);
 
   RepeatingTimeIntervalTaskExecutor(const RepeatingTimeIntervalTaskExecutor&) =
       delete;
@@ -26,6 +33,11 @@ class RepeatingTimeIntervalTaskExecutor {
       const RepeatingTimeIntervalTaskExecutor&) = delete;
 
   ~RepeatingTimeIntervalTaskExecutor();
+
+ private:
+  const policy::WeeklyTimeInterval time_interval_;
+  const base::RepeatingClosure interval_start_callback_;
+  const base::RepeatingClosure interval_end_callback_;
 };
 
 }  // namespace ash
