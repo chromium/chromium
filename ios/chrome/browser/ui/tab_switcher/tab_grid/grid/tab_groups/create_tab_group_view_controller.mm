cee37f4edfb3a7bea1af7db97a08f9df33774e97
Aliona DANGLA <alionadangla@google.com>
2024-02-07 10:48:43 +0000

    [iOS] Handle landscape view for tab group creation view
    This CL creates a compact view and use it when there is vertically not
    enough space to display everything in one column. It listen for any
    rotation to ensure that the view is always adapted.
    
    See video on iPhone:
    https://drive.google.com/file/d/1O_UnHtZsMJvBIFxtYaCHMPXEmxA53KsN/view?usp=drive_link
    
    iPad pro (12.9 inch):
    https://drive.google.com/file/d/1U7HBQR-0TYDM4N8kPOKhtRixx-c1Xsdg/view?usp=drive_link
    
    iPad mini:
    https://drive.google.com/file/d/1r-ar5VItXKVz1l0wAlUD4Gp4D_yP3F9Y/view?usp=drive_link
    
    Bug: 1501837
    Change-Id: I1ccdc393befc6279dbe644bb081fae662d7eb964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258410
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257271}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_view_controller.mm
@@ -40,6 +40,7 @@ constexpr CGFloat kSnapshotViewMaxHeight = 190;
 constexpr CGFloat kSnapshotViewCornerRadius = 18;
 constexpr CGFloat kSnapshotViewVerticalMargin = 25;
 constexpr CGFloat kSingleSnapshotRatio = 0.75;
+constexpr CGFloat kContainersMaxWidth = 400;
 }  // namespace
 
 @implementation CreateTabGroupViewController {
@@ -61,9 +62,12 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
   tab_groups::TabGroupColorId _defaultColor;
   // StackView which contains all bottom views.
   UIStackView* _bottomStackView;
+  // List of snapshots.
   NSArray<UIImage*>* _snapshots;
   // List of favicons.
   NSArray<UIImage*>* _favicons;
+  // Snapshots views container.
+  UIView* _snapshotsContainer;
 }
 
 - (instancetype)initWithHandler:(id<TabGroupsCommands>)handler {
@@ -137,82 +141,12 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
   _selectedButton = _colorSelectionButtons[defaultButtonIndex];
   [_selectedButton setSelected:YES];
 
-  if (!UIAccessibilityIsReduceTransparencyEnabled()) {
-    self.view.backgroundColor = [[UIColor colorNamed:kGrey900Color]
-        colorWithAlphaComponent:kBackgroundAlpha];
-    UIBlurEffect* blurEffect =
-        [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-    UIVisualEffectView* blurEffectView =
-        [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blurEffectView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:blurEffectView];
-    AddSameConstraints(self.view, blurEffectView);
-  } else {
-    self.view.backgroundColor = [UIColor blackColor];
+  [self updateViews];
+
+  if (@available(iOS 17, *)) {
+    [self registerForTraitChanges:@[ UITraitVerticalSizeClass.self ]
+                       withAction:@selector(updateViews)];
   }
-
-  UIView* dotAndFieldContainer = [self configuredDotAndFieldContainer];
-  UILayoutGuide* snapshotsContainerLayoutGuide = [[UILayoutGuide alloc] init];
-  UIView* snapshotsContainer = [self configuredSnapshotsContainer];
-  UIView* colorsView = [self listOfColorView];
-  UIButton* creationButton = [self configuredCreateGroupButton];
-  UIButton* cancelButton = [self configuredCancelButton];
-
-  _bottomStackView = [[UIStackView alloc]
-      initWithArrangedSubviews:@[ colorsView, creationButton, cancelButton ]];
-  _bottomStackView.translatesAutoresizingMaskIntoConstraints = NO;
-  _bottomStackView.axis = UILayoutConstraintAxisVertical;
-  [_bottomStackView setCustomSpacing:kColorListBottomMargin
-                           afterView:colorsView];
-
-  [self.view addSubview:dotAndFieldContainer];
-  [self.view addSubview:snapshotsContainer];
-  [self.view addLayoutGuide:snapshotsContainerLayoutGuide];
-  [self.view addSubview:_bottomStackView];
-
-  [NSLayoutConstraint activateConstraints:@[
-    [dotAndFieldContainer.leadingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor
-                       constant:kHorizontalMargin],
-    [dotAndFieldContainer.topAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor
-                       constant:kdotAndFieldContainerMargin],
-    [dotAndFieldContainer.trailingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
-                       constant:-kHorizontalMargin],
-    [_bottomStackView.bottomAnchor
-        constraintEqualToAnchor:self.view.keyboardLayoutGuide.topAnchor
-                       constant:-kButtonsMargin],
-    [_bottomStackView.leadingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor
-                       constant:kHorizontalMargin],
-    [_bottomStackView.trailingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
-                       constant:-kHorizontalMargin],
-    [snapshotsContainerLayoutGuide.topAnchor
-        constraintEqualToAnchor:dotAndFieldContainer.bottomAnchor
-                       constant:kSnapshotViewVerticalMargin],
-    [snapshotsContainerLayoutGuide.bottomAnchor
-        constraintEqualToAnchor:colorsView.topAnchor
-                       constant:-kSnapshotViewVerticalMargin],
-    [snapshotsContainerLayoutGuide.leadingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor
-                       constant:kHorizontalMargin],
-    [snapshotsContainerLayoutGuide.trailingAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
-                       constant:-kHorizontalMargin],
-
-    [snapshotsContainer.centerXAnchor
-        constraintEqualToAnchor:self.view.centerXAnchor],
-    [snapshotsContainer.centerYAnchor
-        constraintEqualToAnchor:snapshotsContainerLayoutGuide.centerYAnchor],
-    [snapshotsContainer.topAnchor
-        constraintGreaterThanOrEqualToAnchor:snapshotsContainerLayoutGuide
-                                                 .topAnchor],
-    [snapshotsContainer.bottomAnchor
-        constraintLessThanOrEqualToAnchor:snapshotsContainerLayoutGuide
-                                              .bottomAnchor],
-  ]];
 
   // To force display the keyboard when the view is shown.
   [_tabGroupTextField becomeFirstResponder];
@@ -220,6 +154,17 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
   return UIStatusBarStyleLightContent;
+}
+
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+  if (self.traitCollection.verticalSizeClass !=
+      previousTraitCollection.verticalSizeClass) {
+    [self updateViews];
+  }
 }
 
 #pragma mark - Private helpers
@@ -232,6 +177,8 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
       [UIFont preferredFontForTextStyle:UIFontTextStyleLargeTitle];
   tabGroupTextField.adjustsFontForContentSizeCategory = YES;
   tabGroupTextField.translatesAutoresizingMaskIntoConstraints = NO;
+  tabGroupTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+  tabGroupTextField.spellCheckingType = UITextSpellCheckingTypeNo;
 
   UITraitCollection* interfaceStyleDarkTraitCollection = [UITraitCollection
       traitCollectionWithUserInterfaceStyle:UIUserInterfaceStyleDark];
@@ -300,16 +247,19 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
 }
 
 // Returns the cancel button.
-- (UIButton*)configuredCancelButton {
+- (UIButton*)configuredCancelButton:(BOOL)isCompact {
   UIButton* cancelButton = [[UIButton alloc] init];
   cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
+
+  UIColor* textColor = isCompact ? [UIColor colorNamed:kBlue600Color]
+                                 : [UIColor colorNamed:kSolidWhiteColor];
 
   UIButtonConfiguration* buttonConfiguration =
       [UIButtonConfiguration plainButtonConfiguration];
   NSDictionary* attributes = @{
     NSFontAttributeName :
         [UIFont preferredFontForTextStyle:UIFontTextStyleBody],
-    NSForegroundColorAttributeName : [UIColor colorNamed:kSolidWhiteColor]
+    NSForegroundColorAttributeName : textColor
   };
   NSMutableAttributedString* attributedString =
       [[NSMutableAttributedString alloc]
@@ -331,18 +281,31 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
 }
 
 // Returns the cancel button.
-- (UIButton*)configuredCreateGroupButton {
+- (UIButton*)configuredCreateGroupButton:(BOOL)isCompact {
   UIButton* creationButton = [[UIButton alloc] init];
   creationButton.translatesAutoresizingMaskIntoConstraints = NO;
 
   UIButtonConfiguration* buttonConfiguration =
       [UIButtonConfiguration filledButtonConfiguration];
-  buttonConfiguration.baseBackgroundColor = [UIColor colorNamed:kBlue600Color];
+  if (isCompact) {
+    buttonConfiguration.baseBackgroundColor = [UIColor clearColor];
+  } else {
+    buttonConfiguration.baseBackgroundColor =
+        [UIColor colorNamed:kBlue600Color];
+  }
   buttonConfiguration.background.cornerRadius = kButtonBackgroundCornerRadius;
+
+  UIColor* textColor = isCompact ? [UIColor colorNamed:kBlue600Color]
+                                 : [UIColor colorNamed:kSolidWhiteColor];
+  UIFontDescriptor* boldDescriptor = [[UIFontDescriptor
+      preferredFontDescriptorWithTextStyle:UIFontTextStyleBody]
+      fontDescriptorWithSymbolicTraits:UIFontDescriptorTraitBold];
+  UIFont* fontAttribute =
+      isCompact ? [UIFont fontWithDescriptor:boldDescriptor size:0.0]
+                : [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
   NSDictionary* attributes = @{
-    NSFontAttributeName :
-        [UIFont preferredFontForTextStyle:UIFontTextStyleBody],
-    NSForegroundColorAttributeName : [UIColor colorNamed:kSolidWhiteColor]
+    NSFontAttributeName : fontAttribute,
+    NSForegroundColorAttributeName : textColor
   };
   NSMutableAttributedString* attributedString =
       [[NSMutableAttributedString alloc]
@@ -474,14 +437,178 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
   return _UIColorList[static_cast<NSUInteger>(colorID)];
 }
 
+// YES if the given button is the default one.
 - (BOOL)isDefaultButton:(UIButton*)button {
   return static_cast<tab_groups::TabGroupColorId>(button.tag) == _defaultColor;
 }
 
-#pragma mark - TabGroupCreationConsumer
+// Updates all the view and subviews depending on space available.
+- (void)updateViews {
+  [self.view.subviews
+      makeObjectsPerformSelector:@selector(removeFromSuperview)];
+  [self setupBackground];
+  BOOL isVerticallyCompacted =
+      self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassCompact;
+  if (isVerticallyCompacted) {
+    [self compactConfiguration];
+  } else {
+    [self regularConfiguration];
+  }
 
-- (void)setDefaultGroupColor:(tab_groups::TabGroupColorId)color {
-  _defaultColor = color;
+  [self.view layoutIfNeeded];
+  if ([_snapshotsContainer superview] &&
+      _snapshotsContainer.frame.size.height < 40) {
+    [_snapshotsContainer removeFromSuperview];
+  }
+}
+
+// Configures the view and all subviews when there is enough space.
+- (void)regularConfiguration {
+  UIView* dotAndFieldContainer = [self configuredDotAndFieldContainer];
+  UILayoutGuide* snapshotsContainerLayoutGuide = [[UILayoutGuide alloc] init];
+  _snapshotsContainer = [self configuredSnapshotsContainer];
+  UIView* colorsView = [self listOfColorView];
+  UIButton* creationButton = [self configuredCreateGroupButton:NO];
+  UIButton* cancelButton = [self configuredCancelButton:NO];
+
+  _bottomStackView = [[UIStackView alloc]
+      initWithArrangedSubviews:@[ colorsView, creationButton, cancelButton ]];
+  _bottomStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  _bottomStackView.axis = UILayoutConstraintAxisVertical;
+  [_bottomStackView setCustomSpacing:kColorListBottomMargin
+                           afterView:colorsView];
+
+  [self.view addSubview:dotAndFieldContainer];
+  [self.view addSubview:_snapshotsContainer];
+  [self.view addLayoutGuide:snapshotsContainerLayoutGuide];
+  [self.view addSubview:_bottomStackView];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [dotAndFieldContainer.leadingAnchor
+        constraintGreaterThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                                 .leadingAnchor
+                                    constant:kHorizontalMargin],
+    [dotAndFieldContainer.topAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor
+                       constant:kdotAndFieldContainerMargin],
+    [dotAndFieldContainer.trailingAnchor
+        constraintLessThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                              .trailingAnchor
+                                 constant:-kHorizontalMargin],
+    [dotAndFieldContainer.heightAnchor
+        constraintGreaterThanOrEqualToConstant:kButtonsHeight],
+    [_bottomStackView.bottomAnchor
+        constraintEqualToAnchor:self.view.keyboardLayoutGuide.topAnchor
+                       constant:-kButtonsMargin],
+    [_bottomStackView.leadingAnchor
+        constraintGreaterThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                                 .leadingAnchor
+                                    constant:kHorizontalMargin],
+    [_bottomStackView.trailingAnchor
+        constraintLessThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                              .trailingAnchor
+                                 constant:-kHorizontalMargin],
+    [snapshotsContainerLayoutGuide.topAnchor
+        constraintEqualToAnchor:dotAndFieldContainer.bottomAnchor
+                       constant:kSnapshotViewVerticalMargin],
+    [snapshotsContainerLayoutGuide.bottomAnchor
+        constraintEqualToAnchor:colorsView.topAnchor
+                       constant:-kSnapshotViewVerticalMargin],
+    [snapshotsContainerLayoutGuide.leadingAnchor
+        constraintGreaterThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                                 .leadingAnchor
+                                    constant:kHorizontalMargin],
+    [snapshotsContainerLayoutGuide.trailingAnchor
+        constraintLessThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                              .trailingAnchor
+                                 constant:-kHorizontalMargin],
+
+    [_snapshotsContainer.centerXAnchor
+        constraintEqualToAnchor:snapshotsContainerLayoutGuide.centerXAnchor],
+    [_snapshotsContainer.centerYAnchor
+        constraintEqualToAnchor:snapshotsContainerLayoutGuide.centerYAnchor],
+    [_snapshotsContainer.topAnchor
+        constraintGreaterThanOrEqualToAnchor:snapshotsContainerLayoutGuide
+                                                 .topAnchor],
+    [_snapshotsContainer.bottomAnchor
+        constraintLessThanOrEqualToAnchor:snapshotsContainerLayoutGuide
+                                              .bottomAnchor],
+    [dotAndFieldContainer.widthAnchor
+        constraintLessThanOrEqualToConstant:kContainersMaxWidth],
+    [snapshotsContainerLayoutGuide.widthAnchor
+        constraintEqualToAnchor:dotAndFieldContainer.widthAnchor],
+    [_bottomStackView.widthAnchor
+        constraintEqualToAnchor:dotAndFieldContainer.widthAnchor],
+
+    [dotAndFieldContainer.centerXAnchor
+        constraintEqualToAnchor:self.view.centerXAnchor],
+    [snapshotsContainerLayoutGuide.centerXAnchor
+        constraintEqualToAnchor:self.view.centerXAnchor],
+    [_bottomStackView.centerXAnchor
+        constraintEqualToAnchor:self.view.centerXAnchor],
+  ]];
+}
+
+// Configures the view and subviews when the screen is small.
+- (void)compactConfiguration {
+  UIView* dotAndFieldContainer = [self configuredDotAndFieldContainer];
+  UIView* colorsView = [self listOfColorView];
+  UIButton* creationButton = [self configuredCreateGroupButton:YES];
+  UIButton* cancelButton = [self configuredCancelButton:YES];
+
+  UIStackView* containedStackView = [[UIStackView alloc]
+      initWithArrangedSubviews:@[ dotAndFieldContainer, colorsView ]];
+  containedStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  containedStackView.axis = UILayoutConstraintAxisVertical;
+  containedStackView.distribution = UIStackViewDistributionEqualSpacing;
+
+  _bottomStackView = [[UIStackView alloc] initWithArrangedSubviews:@[
+    cancelButton, containedStackView, creationButton
+  ]];
+  _bottomStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  _bottomStackView.distribution = UIStackViewDistributionEqualSpacing;
+  _bottomStackView.alignment = UIStackViewAlignmentTop;
+
+  [self.view addSubview:_bottomStackView];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [_bottomStackView.leadingAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor
+                       constant:kHorizontalMargin],
+    [_bottomStackView.topAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor
+                       constant:kdotAndFieldContainerMargin],
+    [_bottomStackView.trailingAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor
+                       constant:-kHorizontalMargin],
+    [_bottomStackView.bottomAnchor
+        constraintEqualToAnchor:self.view.keyboardLayoutGuide.topAnchor
+                       constant:-kButtonsMargin],
+    [containedStackView.bottomAnchor
+        constraintEqualToAnchor:_bottomStackView.bottomAnchor],
+
+    [dotAndFieldContainer.widthAnchor
+        constraintLessThanOrEqualToConstant:kContainersMaxWidth],
+    [containedStackView.widthAnchor
+        constraintEqualToAnchor:dotAndFieldContainer.widthAnchor],
+  ]];
+}
+
+// Configures the view background.
+- (void)setupBackground {
+  if (!UIAccessibilityIsReduceTransparencyEnabled()) {
+    self.view.backgroundColor = [[UIColor colorNamed:kGrey900Color]
+        colorWithAlphaComponent:kBackgroundAlpha];
+    UIBlurEffect* blurEffect =
+        [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView* blurEffectView =
+        [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    blurEffectView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:blurEffectView];
+    AddSameConstraints(self.view, blurEffectView);
+  } else {
+    self.view.backgroundColor = [UIColor blackColor];
+  }
 }
 
 // Returns the view which contains all the selected tabs' snapshot which will be
@@ -528,6 +655,10 @@ constexpr CGFloat kSingleSnapshotRatio = 0.75;
 }
 
 #pragma mark - TabGroupCreationConsumer
+
+- (void)setDefaultGroupColor:(tab_groups::TabGroupColorId)color {
+  _defaultColor = color;
+}
 
 - (void)setSnapshots:(NSArray<UIImage*>*)snapshots
             favicons:(NSArray<UIImage*>*)favicons {
