dfdca148939274f10e4ea472eb2b65ace400434b
Ben Franz <bfranz@google.com>
2024-02-08 22:43:44 +0000

    [M122] kiosk: Close new browsers by closing tabs instead of window
    Currently we close new browsers with `browser->window()->Close()`. Turns
    out this can fail in certain conditions, e.g. if the user is currently
    dragging the window. This leads to the possibility that a new window
    does not get closed.
    
    This change uses `browser->tab_strip_model()->CloseAllTabs()` that seems
    to be more resilient and closes the browser window during drag events.
    
    Note this is a bandaid fix, the ideal solution is to prevent the window
    from opening on the first place, not to close it after the fact.
    
    This is behind the `kKioskCloseAllTabs` feature flag. Enable it with the
    `--enable-features=KioskCloseAllTabs` arg in /etc/chrome_dev.conf.
    
          and the window still closes during a drag.
    
    (cherry picked from commit e090938556780999db8cca6dfde8aab52b57300b)
    
    Bug: b:322954900
    Test: manual - do the steps in b/322775376, verify step 11 does not work
    Change-Id: Iae6beba499e66d7849551331bc10e55179051024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5249989
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1254484}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279442
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Auto-Submit: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6261@{#604}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -152,6 +152,11 @@ BASE_FEATURE(kJelly, "Jelly", base::FEATURE_ENABLED_BY_DEFAULT);
 // controls all system UI updates and new system components. go/jelly-flags
 BASE_FEATURE(kJellyroll, "Jellyroll", base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Makes Kiosk close all tabs instead of closing the window.
+BASE_FEATURE(kKioskCloseAllTabs,
+             "KioskCloseAllTabs",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 // Enables Kiosk Heartbeats to be sent via Encrypted Reporting Pipeline
 BASE_FEATURE(kKioskHeartbeatsViaERP,
