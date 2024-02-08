e6fe0580b7d34fc200154d2542e165999a52558d
Florian Leimgruber <fleimgruber@google.com>
2024-02-08 10:41:52 +0000

    Revert "Roll wpt tooling"
    This reverts commit 3039df172e23b66cf3a574334edbd619daac7ecc.
    
    Reason for revert: Causes consistent webdriver_wpt_tests failures
    starting at https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/118558/overview.
    
    Original change's description:
    > Roll wpt tooling
    >
    > This rolls wpt to latest commit at
    > https://github.com/web-platform-tests/wpt.
    > REMOTE-WPT-HEAD: a68f313a50899795e1e6660b7398544d340f1652
    >
    > Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    > Bug: None
    > Change-Id: I67d47ad33ea3c99140e14c65305ea30b8239eb0e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274210
    > Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    > Reviewed-by: Weizhong Xia <weizhong@google.com>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1257156}
    
    Bug: None
    Change-Id: I3c5f6869e8e99b306bb7d7164155fd79116ee435
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273148
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Owners-Override: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1257849}

--- third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorservodriver.py
+++ third_party/wpt_tools/wpt/tools/wptrunner/wptrunner/executors/executorservodriver.py
@@ -201,7 +201,7 @@ class ServoWebDriverTestharnessExecutor(TestharnessExecutor):
         if success:
             return self.convert_result(test, data)
 
-        return (test.make_result(*data), [])
+        return (test.result_cls(*data), [])
 
     def do_testharness(self, session, url, timeout):
         session.url = url
@@ -257,15 +257,15 @@ class ServoWebDriverRefTestExecutor(RefTestExecutor):
             result = self.implementation.run_test(test)
             return self.convert_result(test, result)
         except OSError:
-            return test.make_result("CRASH", None), []
+            return test.result_cls("CRASH", None), []
         except TimeoutError:
-            return test.make_result("TIMEOUT", None), []
+            return test.result_cls("TIMEOUT", None), []
         except Exception as e:
             message = getattr(e, "message", "")
             if message:
                 message += "\n"
             message += traceback.format_exc()
-            return test.make_result("INTERNAL-ERROR", message), []
+            return test.result_cls("INTERNAL-ERROR", message), []
 
     def screenshot(self, test, viewport_size, dpi, page_ranges):
         # https://github.com/web-platform-tests/wpt/issues/7135
