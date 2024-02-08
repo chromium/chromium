8a9c719a6f0b1ba675c1e353a60945a2e57d9ef8
Jong Ahn <jongahn@google.com>
2024-02-08 00:04:59 +0000

    [Structured Metrics] Change to StructuredMetricsClient::Record to deprecate recording from events directly for //ash/display.
    Bug: b/322037443
    Change-Id: Ie4344348acfbfe9f45e1edee2d4653a0919ce676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274661
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257681}

--- ash/display/display_prefs.cc
+++ ash/display/display_prefs.cc
@@ -7,6 +7,7 @@
 #include <stddef.h>
 
 #include <string>
+#include <utility>
 
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
@@ -21,6 +22,7 @@
 #include "base/system/sys_info.h"
 #include "base/values.h"
 #include "components/metrics/structured/structured_events.h"
+#include "components/metrics/structured/structured_metrics_client.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -813,18 +815,19 @@ void ReportToPopularityMetricsAndStore(PrefService* pref_service) {
     int product_id;
     base::StringToInt(display.product_id(), &product_id);
 
-    metrics::structured::events::v2::popular_displays::MonitorInfo()
-        .SetDisplayName(display.name())
-        .SetManufacturerId(display.manufacturer_id())
-        .SetProductId(product_id)
-        .SetNativeModeSize(native_mode->size().ToString())
-        .SetNativeModeRefreshRate(native_mode->refresh_rate())
-        .SetPhysicalSize(display.physical_size().ToString())
-        .SetConnectionType(
-            display::DisplayConnectionTypeString(display.connection_type()))
-        .SetIsVrrCapable(display.variable_refresh_rate_state() <
-                         display::VariableRefreshRateState::kVrrNotCapable)
-        .Record();
+    metrics::structured::StructuredMetricsClient::Record(std::move(
+        metrics::structured::events::v2::popular_displays::MonitorInfo()
+            .SetDisplayName(display.name())
+            .SetManufacturerId(display.manufacturer_id())
+            .SetProductId(product_id)
+            .SetNativeModeSize(native_mode->size().ToString())
+            .SetNativeModeRefreshRate(native_mode->refresh_rate())
+            .SetPhysicalSize(display.physical_size().ToString())
+            .SetConnectionType(
+                display::DisplayConnectionTypeString(display.connection_type()))
+            .SetIsVrrCapable(
+                display.variable_refresh_rate_state() <
+                display::VariableRefreshRateState::kVrrNotCapable)));
 
     cached_list.Append(display_id);
   }
