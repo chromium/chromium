4887b37639e8dddb2282dd8589127f1dc5b54e75
Joseph Park <josephjoopark@chromium.org>
2024-02-08 22:12:14 +0000

    Fix leftmost tab corner alignment on fullscreen Mac
    Bug: 1513245
    Change-Id: I06146164cbce9aec7169fd04eeeb40f3711982d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5258791
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258191}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.mm
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.mm
@@ -175,8 +175,13 @@ gfx::Rect BrowserNonClientFrameViewMac::GetBoundsForTabStripRegion(
   gfx::Rect bounds(0, GetTopInset(restored), width(),
                    tabstrip_minimum_size.height());
 
-  // Do not draw caption buttons on fullscreen.
-  if (!frame()->IsFullscreen()) {
+  // If we do not inset, the leftmost tab doesn't blend well with the bottom of
+  // the tab strip. Normally, we would naturally have an inset from either the
+  // caption buttons or the tab search button.
+  if (frame()->IsFullscreen() && features::IsChromeRefresh2023()) {
+    bounds.Inset(
+        gfx::Insets::TLBR(0, GetLayoutConstant(TOOLBAR_CORNER_RADIUS), 0, 0));
+  } else {
     bounds.Inset(GetCaptionButtonInsets());
   }
 
