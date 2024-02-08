62625943d6b96b20a6a8f837a4bf16fb56bd3a0a
Sylvain Defresne <sdefresne@chromium.org>
2024-02-08 16:50:44 +0000

    Save SessionIdGenerator seed to prefs on increment
    The preferences are only saved to disk periodically, which can lead
    to them not being saved if the app crashes soon after returning a
    new id.
    
    This can lead to duplicate id if the code that uses the new id save
    it to another location with less delay and then the application crash
    before the prefs has been saved. This has been observed on iOS in the
    wild.
    
    To limit the risk of this happening, store the value to prefs when
    it is incremented, including when adding kCautionaryIdPadding. On
    iOS, this solves the issue on iOS since on that system, the prefs
    are saved unconditionally as part of the application startup after
    calling SessionIdGenerator::Init().
    
    Bug: 41495439
    Change-Id: I2538c328dced802787928577c5ceccd48f2f35e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5272360
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257977}

--- components/sessions/core/session_id_generator.cc
+++ components/sessions/core/session_id_generator.cc
@@ -61,16 +61,7 @@ void SessionIdGenerator::Shutdown() {
 
 SessionID SessionIdGenerator::NewUnique() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // Init() should have been called in production (which initializes
-  // |local_state_|), but for test convenience, we allow operating even without
-  // underlying prefs.
-  if (local_state_) {
-    IncrementValueBy(1);
-    local_state_->SetInt64(kLastValuePref, last_value_);
-  } else {
-    // Test-only path. Will CHECK-fail if Init() is called later.
-    ++last_value_;
-  }
+  IncrementValueBy(1);
   DCHECK(SessionID::IsValidValue(last_value_));
   return SessionID::FromSerializedValue(last_value_);
 }
@@ -100,6 +91,12 @@ void SessionIdGenerator::IncrementValueBy(int increment) {
     last_value_ = 0;
   }
   last_value_ += increment;
+  // Init() should have been called in production (which initializes
+  // |local_state_|), but for test convenience, we allow operating even without
+  // underlying prefs.
+  if (local_state_) {
+    local_state_->SetInt64(kLastValuePref, last_value_);
+  }
 }
 
 bool SessionIdGenerator::IsInitializedForTest() const {
