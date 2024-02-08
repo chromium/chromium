344524e55d0213c2531983dfa95dbc47417eb997
Andre Le <leandre@chromium.org>
2024-02-08 20:01:37 +0000

    Mahi: Add menu views hierarchy and children views
    Define the views hierarchy and layout for the Mahi menu view. This CL
    also add the settings icon and textfield view to complete the list of
    children views of this UI, and add the styling necessary for each
    component.
    
    Screenshot: https://screenshot.googleplex.com/7yLjAZHYkeHgEx2
    
    Fixed: b:318733118, b:318733491
    Change-Id: I511ae1f15ddef2c728c75b33e1bde7a1b6fe2695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5274893
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258107}

--- chrome/browser/ui/views/mahi/mahi_menu_view.cc
+++ chrome/browser/ui/views/mahi/mahi_menu_view.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/ui/views/mahi/mahi_menu_view.h"
 
+#include <algorithm>
 #include <memory>
 
 #include "base/functional/bind.h"
@@ -11,22 +12,33 @@
 #include "chrome/browser/ui/views/editor_menu/utils/utils.h"
 #include "chromeos/components/mahi/public/cpp/mahi_manager.h"
 #include "chromeos/components/mahi/public/cpp/views/experiment_badge.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
+#include "chromeos/ui/vector_icons/vector_icons.h"
 #include "components/vector_icons/vector_icons.h"
+#include "ui/base/ime/text_input_type.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/color/color_id.h"
 #include "ui/display/screen.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/background.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/flex_layout_view.h"
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/layout/layout_types.h"
+#include "ui/views/style/typography.h"
 #include "ui/views/view.h"
+#include "ui/views/view_class_properties.h"
 #include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
 
@@ -36,36 +48,96 @@ namespace {
 
 constexpr char kWidgetName[] = "MahiMenuViewWidget";
 
-constexpr int kButtonHeight = 20;
+constexpr gfx::Insets kMenuPadding = gfx::Insets::TLBR(12, 16, 12, 14);
+constexpr int kButtonHeight = 16;
+constexpr int kButtonCornerRadius = 8;
+constexpr gfx::Insets kButtonPadding = gfx::Insets::VH(6, 8);
+constexpr gfx::Insets kHeaderRowPadding = gfx::Insets::TLBR(0, 0, 12, 0);
+constexpr int kHeaderRowSpacing = 8;
+constexpr int kButtonTextfieldSpacing = 16;
+constexpr int kButtonImageLabelSpacing = 4;
+constexpr int kButtonBorderThickness = 1;
 
 }  // namespace
 
 MahiMenuView::MahiMenuView() {
-  auto* layout = SetLayoutManager(std::make_unique<views::FlexLayout>());
-  layout->SetOrientation(views::LayoutOrientation::kVertical);
-  layout->SetCrossAxisAlignment(views::LayoutAlignment::kStart);
-
   SetBackground(views::CreateThemedRoundedRectBackground(
       ui::kColorPrimaryBackground,
       views::LayoutProvider::Get()->GetCornerRadiusMetric(
           views::ShapeContextTokens::kMenuRadius)));
 
+  auto* layout = SetLayoutManager(std::make_unique<views::FlexLayout>());
+  layout->SetOrientation(views::LayoutOrientation::kVertical);
+  layout->SetInteriorMargin(kMenuPadding);
+
+  auto header_row = std::make_unique<views::FlexLayoutView>();
+  header_row->SetOrientation(views::LayoutOrientation::kHorizontal);
+  header_row->SetInteriorMargin(kHeaderRowPadding);
+
+  auto header_left_container = std::make_unique<views::FlexLayoutView>();
+  header_left_container->SetOrientation(views::LayoutOrientation::kHorizontal);
+  header_left_container->SetMainAxisAlignment(views::LayoutAlignment::kStart);
+  header_left_container->SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
+  header_left_container->SetDefault(
+      views::kMarginsKey, gfx::Insets::TLBR(0, 0, 0, kHeaderRowSpacing));
+  header_left_container->SetProperty(
+      views::kFlexBehaviorKey,
+      views::FlexSpecification(
+          views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
+                                   views::MaximumFlexSizeRule::kUnbounded)));
+
   // TODO(b/318733118): Finish building the menu UI.
   // TODO(b/319264190): Replace the strings here with real strings.
-  AddChildView(std::make_unique<views::Label>(u"Mahi Menu"));
+  auto* header_label =
+      header_left_container->AddChildView(std::make_unique<views::Label>(
+          u"Mahi Menu", views::style::CONTEXT_DIALOG_TITLE,
+          views::style::STYLE_HEADLINE_5));
+  header_label->SetEnabledColorId(ui::kColorSysOnSurface);
+  header_label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
 
-  AddChildView(std::make_unique<chromeos::mahi::ExperimentBadge>());
+  header_left_container->AddChildView(
+      std::make_unique<chromeos::mahi::ExperimentBadge>());
 
-  auto* button = AddChildView(std::make_unique<views::LabelButton>(
+  header_row->AddChildView(std::move(header_left_container));
+
+  header_row->AddChildView(views::ImageButton::CreateIconButton(
+      views::Button::PressedCallback(), vector_icons::kSettingsOutlineIcon,
+      l10n_util::GetStringUTF16(IDS_EDITOR_MENU_SETTINGS_TOOLTIP)));
+
+  AddChildView(std::move(header_row));
+
+  auto button = std::make_unique<views::LabelButton>(
       /*callback=*/base::BindRepeating(&MahiMenuView::OnSummaryButtonPressed,
                                        weak_ptr_factory_.GetWeakPtr()),
-      /*text=*/u"Show"));
-  button->SetBackground(views::CreateThemedRoundedRectBackground(
-      cros_tokens::kCrosSysSystemBase, 4));
+      /*text=*/l10n_util::GetStringUTF16(IDS_MAHI_SUMMARIZE_BUTTON_LABEL_TEXT));
+  button->SetLabelStyle(views::style::STYLE_BODY_4_EMPHASIS);
   button->SetImageModel(
       views::Button::ButtonState::STATE_NORMAL,
-      ui::ImageModel::FromVectorIcon(vector_icons::kInfoOutlineIcon,
-                                     ui::kColorMenuIcon, kButtonHeight));
+      ui::ImageModel::FromVectorIcon(chromeos::kMahiSummarizeIcon,
+                                     ui::kColorSysOnSurface, kButtonHeight));
+  button->SetTextColorId(views::LabelButton::ButtonState::STATE_NORMAL,
+                         ui::kColorSysOnSurface);
+  button->SetImageLabelSpacing(kButtonImageLabelSpacing);
+  button->SetBorder(views::CreatePaddedBorder(
+      views::CreateThemedRoundedRectBorder(kButtonBorderThickness,
+                                           kButtonCornerRadius,
+                                           ui::kColorSysTonalOutline),
+      kButtonPadding));
+  button->SetProperty(views::kCrossAxisAlignmentKey,
+                      views::LayoutAlignment::kStart);
+  AddChildView(std::move(button));
+
+  auto textfield = std::make_unique<views::Textfield>();
+  textfield->SetTextInputType(ui::TEXT_INPUT_TYPE_TEXT);
+  textfield->SetPlaceholderText(
+      l10n_util::GetStringUTF16(IDS_MAHI_MENU_INPUT_TEXTHOLDER));
+  textfield->SetProperty(views::kFlexBehaviorKey,
+                         views::FlexSpecification(views::FlexSpecification(
+                             views::MinimumFlexSizeRule::kPreferred,
+                             views::MaximumFlexSizeRule::kUnbounded)));
+  textfield->SetProperty(views::kMarginsKey,
+                         gfx::Insets::TLBR(kButtonTextfieldSpacing, 0, 0, 0));
+  AddChildView(std::move(textfield));
 }
 
 MahiMenuView::~MahiMenuView() = default;
