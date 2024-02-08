64b3424518934236fc5b1f1c54e4a13cac4305fd
Thomas Lukaszewicz <tluk@chromium.org>
2024-02-08 01:15:05 +0000

    [exo] Implement zaura_output_manager_v2 protocol server-side
    See follow-up CL for exo tests crrev.com/c/5247526.
    
    This CL introduces the zaura_output_manager_v2 protocol to the
    aura-shell extension and implements this server-side.
    
    At a high level the protocol uses the global output name to
    communicate atomic output config updates without excessive
    server<->client round-trips.
    
    Key additions
    
      - OutputConfigurationChange
          Struct wrapping added/removed/changed output globals
    
      - AuraOutputManagerV2
          Responsible for sending zaura_output_manager_v2 events to bound
          clients.
          OutputController composes this class to coordinate event
          dispatch in response to output configuration changes.
    
    Output configuration updates are communicated to bound clients via
      wl_registry.global - output additions
      wl_registry.global_remove - output removals
      zaura_output_manager_v2.* - output metric changes
      zaura_output_manager_v2.done - end of atomic config change
    
    There are two important cases to consider
      - Steady state (output changes after client has bound global)
          Add / remove / modify events communicated normally
      - Initial bind
          The client will have already received add events for
          active outputs. To complete the atomic config update
          AuraOutputManagerV2 sends all active output metrics
          immediately after bind.
    
    See go/atomic-output-configuration for more details.
    
    Bug: 1427019, 1408304, 1518335
    Change-Id: I1421b34d06373b5e5be1536cd8738f5316265002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5199307
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1257715}

--- components/exo/wayland/clients/client_helper.cc
+++ components/exo/wayland/clients/client_helper.cc
@@ -56,6 +56,7 @@ DEFAULT_DELETER(wp_presentation, wp_presentation_destroy)
 DEFAULT_DELETER(struct wp_presentation_feedback,
                 wp_presentation_feedback_destroy)
 DEFAULT_DELETER(zaura_output_manager, zaura_output_manager_destroy)
+DEFAULT_DELETER(zaura_output_manager_v2, zaura_output_manager_v2_destroy)
 DEFAULT_DELETER(zaura_shell, zaura_shell_destroy)
 DEFAULT_DELETER(zaura_surface, zaura_surface_destroy)
 DEFAULT_DELETER(zaura_toplevel, zaura_toplevel_destroy)
