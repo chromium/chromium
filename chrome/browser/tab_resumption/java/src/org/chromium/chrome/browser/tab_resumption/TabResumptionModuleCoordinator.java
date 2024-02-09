5e268bdf7d9129052a3023c19f4d77cd9e02c366
Samuel Huang <huangs@chromium.org>
2024-02-09 23:52:32 +0000

    [Android TRM] Refactor to prepare for Magic Stack integration.
    Currently the Tab Resumption Module only appears on the NTP as a
    standalone module. We'd like to integrate it into the Magic Stack, so it
    can appear alongside other modules, and show up on the Start Surface as
    well.
    
    This CL restructures the code to make the above integration easier.
    There is no feature changes. Details:
    * Remove DATA_PROVIDER from Model: TabResumptionDataProvider is now
      directly passed to Mediator by CTOR (but still owned by Coordinator).
    * Add TITLE to Model:
      * This makes title accessible by Coordinator, to prepare for usage by
        overridden ModuleProvider.getModuleTitle() in Coordinator.
      * Computed (moved from View) and set by Mediator on suggestion fetch.
        * Computation needs Resources. Before we got it from View. Now we
          get it from Context (more on this later).
      * Binds to newly added setTitle() in View.
    * Render triggering in View:
      * Rendering needs {URL_IMAGE_PROVIDER, CLICK_CALLBACK,
        SUGGESTION_BUNDLE}. Previously the first 2 were set in Coordinator
        CTOR, which binds to View setters. Then SUGGESTION_BUNDLE is bound
        to View.setSuggestionBundleThenRender(), which then renders,
        assuming all data are available.
      * The assumption breaks down for Magic Stack, which can set all
        properties in one setting in arbitrary order.
      * To fix: Rename the above to setSuggestionBundle(), and move
        rendering calls to renderIfReady(), which is called when setting
        all 3 properties, but only actually renders when all required data
        {|mUrlImageProvider|, |mClickCallback|, |mBundle|} are available
        (noting that |mBundle| can be null).
    * Mediator:
      * Add CTOR param |context|: Used to compute title (referred to above).
      * Add CTOR param |dataProvider|: For direct usage.
      * Add CTOR params {|urlImageProvider|, |suggestionClickCallback|}, to
        be assigned into Model. This moves business logic from Coordinator,
        and improves testing.
      * Rename reload() to loadModule().
    * Coordinator:
      * Still own data / image providers.
      * Duplicate reload() to showModule(), which will be a Magic Stack
        override. Keeping reload() for semantic accuracy.
    
    Test details:
    * TabResumptionModuleMediatorTest:
      * Replace Coordinator usage with direct Mediator usage, now that we
        can pass in more CTOR params.
      * Stop test that relies on bind() (now that Coordinator is not used).
        Specifically, remove URL image load (covered by View test) tests
        and simulated clicks (moved to View test).
      * Test title (singular and plural) via TITLE in Model.
    * TabResumptionModuleViewUnitTest:
      * Add 1 test: testSetTitle(), to test title assignment in View.
      * testRender{Single,Double}(): Remove title test, since TITLE is now
        assigned by Mediator via Model, and tested there.
      * Add tile click tests, now using performClick().
    
    Bug: 1515325
    Change-Id: I7b8b371a740f9c4b4d7f688adfb60630f1361c58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281359
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258810}

--- chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleCoordinator.java
+++ chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleCoordinator.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.tab_resumption;
 
+import android.content.Context;
 import android.view.ViewStub;
 
 import org.chromium.chrome.browser.tab_resumption.TabResumptionModuleUtils.SuggestionClickCallback;
@@ -16,6 +17,7 @@ import org.chromium.url.GURL;
  * Start surface.
  */
 public class TabResumptionModuleCoordinator {
+    protected final Context mContext;
     protected final TabResumptionDataProvider mDataProvider;
     protected final UrlImageProvider mUrlImageProvider;
     protected final PropertyModel mModel;
@@ -24,34 +26,28 @@ public class TabResumptionModuleCoordinator {
     protected final TabResumptionModuleMediator mMediator;
 
     public TabResumptionModuleCoordinator(
+            Context context,
             TabResumptionDataProvider dataProvider,
             UrlImageProvider urlImageProvider,
             SuggestionClickCallback suggestionClickCallback,
             ViewStub viewStub) {
+        mContext = context;
         mDataProvider = dataProvider;
         mUrlImageProvider = urlImageProvider;
-
-        SuggestionClickCallback wrappedCallback =
-                (GURL gurl) -> {
-                    suggestionClickCallback.onSuggestionClick(gurl);
-                    // TODO(crbug.com/1515325): Record metrics here.
-                };
-        mModel =
-                new PropertyModel.Builder(TabResumptionModuleProperties.ALL_KEYS)
-                        .with(TabResumptionModuleProperties.DATA_PROVIDER, mDataProvider)
-                        .with(TabResumptionModuleProperties.URL_IMAGE_PROVIDER, mUrlImageProvider)
-                        .with(TabResumptionModuleProperties.CLICK_CALLBACK, wrappedCallback)
-                        .build();
+        mModel = new PropertyModel(TabResumptionModuleProperties.ALL_KEYS);
         mModuleView = (TabResumptionModuleView) viewStub.inflate();
         mPropertyModelChangeProcessor =
                 PropertyModelChangeProcessor.create(
                         mModel, mModuleView, new TabResumptionModuleViewBinder());
-        mMediator = createMediator();
+        SuggestionClickCallback wrappedClickCallback =
+                (GURL url) -> {
+                    suggestionClickCallback.onSuggestionClick(url);
+                    // TODO(crbug.com/1515325): Record metrics here.
+                };
+        mMediator =
+                new TabResumptionModuleMediator(
+                        mContext, mModel, mDataProvider, mUrlImageProvider, wrappedClickCallback);
         mDataProvider.setStatusChangedCallback(this::reload);
-    }
-
-    protected TabResumptionModuleMediator createMediator() {
-        return new TabResumptionModuleMediator(mModel);
     }
 
     public void destroy() {
@@ -64,12 +60,13 @@ public class TabResumptionModuleCoordinator {
         mDataProvider.destroy();
     }
 
-    /** Reloads the most up-to-date data and rerenders the module. */
-    public void reload() {
-        mMediator.reload();
+    /** Show tab resumption module. */
+    public void showModule() {
+        mMediator.loadModule();
     }
 
-    public PropertyModel getModelForTesting() {
-        return mModel;
+    /** Reloads tab resumption module in response to UI or data updates. */
+    public void reload() {
+        mMediator.loadModule();
     }
 }
