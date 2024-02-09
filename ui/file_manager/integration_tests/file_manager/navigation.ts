319b9c50af5180cbbd5af5105ca1c242d0539e80
François Degros <fdegros@chromium.org>
2024-02-09 05:23:48 +0000

    Files app: Convert navigation.js to TypeScript
    BUG=b:319189127
    
    Change-Id: I104df46d73e547cae4d4317c1d8280e8c4039e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5276343
    Auto-Submit: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258359}

--- ui/file_manager/integration_tests/file_manager/navigation.ts
+++ ui/file_manager/integration_tests/file_manager/navigation.ts
@@ -3,18 +3,13 @@
 // found in the LICENSE file.
 
 import {ENTRIES, RootPath} from '../test_util.js';
-import {testcase} from '../testcase.js';
 
 import {remoteCall, setupAndWaitUntilReady} from './background.js';
 
 /** Tests that the Backspace key navigates to parent directory.  */
-// @ts-ignore: error TS4111: Property 'navigateToParent' comes from an index
-// signature, so it must be accessed with ['navigateToParent'].
-testcase.navigateToParent = async () => {
+export async function navigateToParent() {
   // Open Files app on local Downloads.
   const appId =
-      // @ts-ignore: error TS4111: Property 'beautiful' comes from an index
-      // signature, so it must be accessed with ['beautiful'].
       await setupAndWaitUntilReady(RootPath.DOWNLOADS, [ENTRIES.beautiful], []);
 
   // It should start in Downloads.
@@ -22,11 +17,9 @@ testcase.navigateToParent = async () => {
       appId, '/My files/Downloads');
 
   // Send the Backspace key to the file list.
-  const backspaceKey = ['#file-list', 'Backspace', false, false, false];
-  // @ts-ignore: error TS2556: A spread argument must either have a tuple type
-  // or be passed to a rest parameter.
-  await remoteCall.fakeKeyDown(appId, ...backspaceKey);
+  await remoteCall.fakeKeyDown(
+      appId, '#file-list', 'Backspace', false, false, false);
 
   // It should navigate to the parent.
   await remoteCall.waitUntilCurrentDirectoryIsChanged(appId, '/My files');
-};
+}
