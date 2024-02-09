014f6e8ae67ae9e14b9b6fffc9b16abd12e1593b
Ted Choc <tedchoc@chromium.org>
2024-02-09 15:37:58 +0000

    Remove getLastUsedRegularProfile from PermissionSettingsBridge.
    Bug: 40254448
    Change-Id: Iccc5b6e94f12b9084d130becb744a022e569282a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273630
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258505}

--- chrome/android/java/src/org/chromium/chrome/browser/permissions/PermissionSettingsBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/permissions/PermissionSettingsBridge.java
@@ -13,15 +13,12 @@ import org.chromium.content_public.browser.WebContents;
 public class PermissionSettingsBridge {
     public static boolean shouldShowNotificationsPromo(WebContents webContents) {
         return PermissionSettingsBridgeJni.get()
-                .shouldShowNotificationsPromo(getProfile(), webContents);
+                .shouldShowNotificationsPromo(
+                        Profile.fromWebContents(webContents).getOriginalProfile(), webContents);
     }
 
-    public static void didShowNotificationsPromo() {
-        PermissionSettingsBridgeJni.get().didShowNotificationsPromo(getProfile());
-    }
-
-    private static Profile getProfile() {
-        return Profile.getLastUsedRegularProfile();
+    public static void didShowNotificationsPromo(Profile profile) {
+        PermissionSettingsBridgeJni.get().didShowNotificationsPromo(profile.getOriginalProfile());
     }
 
     @NativeMethods
