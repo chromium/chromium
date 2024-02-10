964d7c69a11bab36d0b622211292eb653b4426d2
dpapad <dpapad@chromium.org>
2024-02-10 00:05:23 +0000

    WebUI: Script for migrating a Polymer element to Lit.
    The provided script performs several mechanical code transformations
    that are needed when migrating a PolymerElement to a LitElement. It
    does not handle all cases, and there is more stuff that can be possibly
    automated as follow-ups.
    
    See lit_migration.md file for a list of transformations, some of which
    are already automated by the provided script (annotated with the [Automated] prefix).
    
    Step1: Install jscodeshift locally
    npm install --prefix ui/webui/resources/tools/codemods/ jscodeshift
    
    Step2: Invoke migration script for a single Polymer element, for example
    ./ui/webui/resources/tools/codemods/lit_migration.py \
        --file ui/webui/resources/cr_elements/cr_grid/cr_grid.ts
    
    Step3: Re-format
    git cl format --js ui/webui/resources/
    
    Step4: Run `git add` for the newly created '.css' file.
    Step5: Continue migration by manually performing remaining
    non-automated steps.
    
    Bug: 40943652
    Change-Id: I61a3134d0c52bd061d3cd9127fe8bb2e76b0a3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277628
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1258814}

---
+++ ui/webui/resources/tools/codemods/lit_migration_templates.mjs
@@ -0,0 +1,72 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A helper script that performs some Polymer->Lit migration steps. See details
+// below.
+
+import fs from 'node:fs';
+import path from 'node:path';
+import {parseArgs} from 'node:util';
+
+// Regular expression to extract CSS Content from within <style>...</style>
+// tags. The 'd' flag is needed to obtain the start/end indices of the match.
+const CSS_REGEX = /<style(?<deps>[\s\S]{0,})>(?<content>[\s\S]+)<\/style>/d;
+
+// Header to place on top of the newly created CSS file.
+const CSS_FILE_HEADER = `/* Copyright 2024 The Chromium Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+/* #css_wrapper_metadata_start
+ * #type=style-lit
+ * #scheme=relative
+ * #css_wrapper_metadata_end */
+`;
+
+const LISTENER_BINDING_REGEX =
+    /on-(?<eventName>[a-zA-Z]+)="(?<listenerName>[a-zA-Z_]+)"/g;
+
+function processFile(file) {
+  const basename = path.basename(file, '.ts');
+  const htmlFile = path.join(path.dirname(file), basename + '.html');
+  const cssFile = path.join(path.dirname(file), basename + '.css');
+
+  // Step 1: Extract a standalone CSS file and write to disk.
+  let htmlContent = fs.readFileSync(htmlFile, 'utf8');
+  const match = htmlContent.match(CSS_REGEX);
+
+  if (match !== null) {
+    fs.writeFileSync(
+        cssFile, CSS_FILE_HEADER + match.groups['content'], 'utf8');
+
+    // Step 2: Remove <style>...</style> CSS content from HTML template file.
+    htmlContent = htmlContent.substring(match.indices[0][1]);
+  }
+
+  // Step 3: Update event listeners syntax in HTML template
+  htmlContent = htmlContent.replaceAll(
+      LISTENER_BINDING_REGEX, function(_a, _b, _c, _d, _e, groups) {
+        return `@${groups.eventName}="\${this.${groups.listenerName}}"`;
+      });
+
+  // Step 4: Update property access syntax in HTML template
+  // TODO(crbug.com/40943652): Implement this.
+
+  // Step 5: Write updated HTML content to disk
+  fs.writeFileSync(htmlFile, htmlContent, 'utf8');
+}
+
+function main() {
+  const args = parseArgs({
+                 options: {
+                   file: {
+                     type: 'string',
+                   },
+                 },
+               }).values;
+
+  processFile(args.file);
+  console.log('DONE');
+}
+main();
