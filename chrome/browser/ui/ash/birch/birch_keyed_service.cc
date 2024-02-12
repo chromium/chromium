54ed336026bd7676f5be5f105175ee4af70cf33f
Matthew Mourgos <mmourgos@chromium.org>
2024-02-12 23:06:23 +0000

    Birch: Fix crash on shutdown due to BirchKeyedService
    Add a browsertest case to ensure that no crash occurs on shutdown.
    
    Bug: b/324629812
    Change-Id: Ia8ce9e4ed56c41cfe0ca42bbd3c15a44646c0c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5282549
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259513}

--- chrome/browser/ui/ash/birch/birch_keyed_service.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service.cc
@@ -23,9 +23,23 @@ BirchKeyedService::BirchKeyedService(Profile* profile)
           std::make_unique<BirchRecentTabsProvider>(profile)) {
   birch_client_impl_ = std::make_unique<BirchClientImpl>(profile);
   Shell::Get()->birch_model()->SetClient(birch_client_impl_.get());
+  shell_observation_.Observe(Shell::Get());
 }
 
 BirchKeyedService::~BirchKeyedService() {
+  ShutdownBirch();
+}
+
+void BirchKeyedService::OnShellDestroying() {
+  ShutdownBirch();
+}
+
+void BirchKeyedService::ShutdownBirch() {
+  if (is_shutdown_) {
+    return;
+  }
+  is_shutdown_ = true;
+  shell_observation_.Reset();
   Shell::Get()->birch_model()->SetClient(nullptr);
 }
 
