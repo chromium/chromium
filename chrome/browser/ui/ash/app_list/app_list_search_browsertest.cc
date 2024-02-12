247d23d3345cb7d28bf971b17f56e63d3268357a
xiangdong kong <xiangdongkong@google.com>
2024-02-12 19:00:33 +0000

    Port LaunchFeedbackFromLauncher tast test to browser tests
    Tast test: http://cs/h/chromium/chromiumos/codesearch/+/main:src/platform/tast-tests/src/go.chromium.org/tast-tests/cros/local/bundles/cros/feedback/launch_feedback_from_launcher.go
    
    - Remove set_launch_browser_for_testing(nullptr), for browser is needed
      for the feedback app.
    - Refactor the SearchBuiltInApps test to reuse some code.
    
    Bug: b:307385730
    Test: browser_tests --gtest_filter=AppListSearchBrowserTest.OpenFeedbackApp
    Change-Id: I28b30ffafa06ef423146da9eafd85fcfd40f6f18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5280583
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259324}

--- chrome/browser/ui/ash/app_list/app_list_search_browsertest.cc
+++ chrome/browser/ui/ash/app_list/app_list_search_browsertest.cc
@@ -12,6 +12,7 @@
 #include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/test/active_window_waiter.h"
+#include "ash/webui/os_feedback_ui/url_constants.h"
 #include "base/run_loop.h"
 #include "chrome/browser/ash/app_list/app_list_client_impl.h"
 #include "chrome/browser/ash/app_list/search/test/app_list_search_test_helper.h"
@@ -20,6 +21,7 @@
 #include "chrome/browser/ash/app_list/test/chrome_app_list_test_support.h"
 #include "chrome/browser/ash/system_web_apps/system_web_app_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -32,10 +34,64 @@ namespace ash {
 namespace {
 
 class AppListSearchBrowserTest : public InProcessBrowserTest {
- public:
-  AppListSearchBrowserTest() {
-    // No need for a browser window.
-    set_launch_browser_for_testing(nullptr);
+ protected:
+  void SearchForSystemApp(aura::Window* primary_root_window,
+                          const std::u16string app_query,
+                          const std::string app_id) {
+    // Ensure the System app is installed.
+    Profile* profile = ProfileManager::GetActiveUserProfile();
+    ASSERT_TRUE(profile);
+    SystemWebAppManager::GetForTest(profile)->InstallSystemAppsForTesting();
+
+    // Associate `client` with the current profile.
+    AppListClientImpl* client = AppListClientImpl::GetInstance();
+    ASSERT_TRUE(client);
+    client->UpdateProfile();
+
+    // Show the launcher.
+    client->ShowAppList(ash::AppListShowSource::kSearchKey);
+    AppListTestApi().WaitForBubbleWindowInRootWindow(
+        primary_root_window,
+        /*wait_for_opening_animation=*/true);
+
+    // The search box should be active.
+    SearchBoxView* search_box_view = GetSearchBoxView();
+    ASSERT_TRUE(search_box_view);
+    EXPECT_TRUE(search_box_view->is_search_box_active());
+
+    // Search for the app and wait for the result.
+    app_list::SearchResultsChangedWaiter results_changed_waiter(
+        AppListClientImpl::GetInstance()->search_controller(),
+        {app_list::ResultType::kInstalledApp});
+    app_list::ResultsWaiter results_waiter(app_query);
+
+    AppListTestApi().SimulateSearch(app_query);
+    results_changed_waiter.Wait();
+    results_waiter.Wait();
+
+    // Search UI updates are scheduled by posting a task on the main thread, run
+    // loop to run scheduled result update tasks.
+    base::RunLoop().RunUntilIdle();
+  }
+
+  void ClickTopSearchResult(aura::Window* primary_root_window,
+                            const std::u16string app_title) {
+    SearchResultListView* top_result_list =
+        AppListTestApi().GetTopVisibleSearchResultListView();
+    ASSERT_TRUE(top_result_list);
+    EXPECT_EQ(top_result_list->list_type_for_test(),
+              SearchResultListView::SearchResultListType::kApps);
+    SearchResultView* top_result_view = top_result_list->GetResultViewAt(0);
+    ASSERT_TRUE(top_result_view);
+    ASSERT_TRUE(top_result_view->result());
+
+    EXPECT_EQ(app_title, top_result_view->result()->title());
+
+    // Open the search result by clicking on it.
+    ui::test::EventGenerator event_generator(primary_root_window);
+    event_generator.MoveMouseTo(
+        top_result_view->GetBoundsInScreen().CenterPoint());
+    event_generator.ClickLeftButton();
   }
 };
 
@@ -46,63 +102,14 @@ class AppListSearchWithAppShortcutsBrowserTest
 };
 
 IN_PROC_BROWSER_TEST_F(AppListSearchBrowserTest, SearchBuiltInApps) {
-  // Ensure the OS Settings app is installed.
-  Profile* profile = ProfileManager::GetActiveUserProfile();
-  ASSERT_TRUE(profile);
-  SystemWebAppManager::GetForTest(profile)->InstallSystemAppsForTesting();
-
-  // Associate `client` with the current profile.
-  AppListClientImpl* client = AppListClientImpl::GetInstance();
-  ASSERT_TRUE(client);
-  client->UpdateProfile();
-
-  // Show the launcher.
-  aura::Window* const primary_root_window = Shell::GetPrimaryRootWindow();
-  client->ShowAppList(ash::AppListShowSource::kSearchKey);
-  AppListTestApi().WaitForBubbleWindowInRootWindow(
-      primary_root_window,
-      /*wait_for_opening_animation=*/true);
-
-  // The search box should be active.
-  SearchBoxView* search_box_view = GetSearchBoxView();
-  ASSERT_TRUE(search_box_view);
-  EXPECT_TRUE(search_box_view->is_search_box_active());
-
-  // Search for OS Settings and wait for the result.
-  const std::u16string app_query = u"Settings";
   const std::string app_id = web_app::kOsSettingsAppId;
-  app_list::SearchResultsChangedWaiter results_changed_waiter(
-      AppListClientImpl::GetInstance()->search_controller(),
-      {app_list::ResultType::kInstalledApp});
-  app_list::ResultsWaiter results_waiter(app_query);
+  aura::Window* const primary_root_window = Shell::GetPrimaryRootWindow();
 
-  AppListTestApi().SimulateSearch(app_query);
-
-  results_changed_waiter.Wait();
-  results_waiter.Wait();
-
-  // Search UI updates are scheduled by posting a task on the main thread, run
-  // loop to run scheduled result update tasks.
-  base::RunLoop().RunUntilIdle();
-
-  SearchResultListView* top_result_list =
-      AppListTestApi().GetTopVisibleSearchResultListView();
-  ASSERT_TRUE(top_result_list);
-  EXPECT_EQ(top_result_list->list_type_for_test(),
-            SearchResultListView::SearchResultListType::kApps);
-  SearchResultView* top_result_view = top_result_list->GetResultViewAt(0);
-  ASSERT_TRUE(top_result_view);
-  ASSERT_TRUE(top_result_view->result());
-
-  EXPECT_EQ(u"Settings", top_result_view->result()->title());
+  SearchForSystemApp(primary_root_window, u"Settings", app_id);
 
   ActiveWindowWaiter window_waiter(primary_root_window);
 
-  // Open the search result by clicking on it.
-  ui::test::EventGenerator event_generator(primary_root_window);
-  event_generator.MoveMouseTo(
-      top_result_view->GetBoundsInScreen().CenterPoint());
-  event_generator.ClickLeftButton();
+  ClickTopSearchResult(primary_root_window, u"Settings");
 
   // Wait for the OS Settings window to activate.
   aura::Window* app_window = window_waiter.Wait();
@@ -110,6 +117,24 @@ IN_PROC_BROWSER_TEST_F(AppListSearchBrowserTest, SearchBuiltInApps) {
   EXPECT_EQ(
       app_id,
       ShelfID::Deserialize(app_window->GetProperty(ash::kShelfIDKey)).app_id);
+}
+
+IN_PROC_BROWSER_TEST_F(AppListSearchBrowserTest, OpenFeedbackApp) {
+  aura::Window* const primary_root_window = Shell::GetPrimaryRootWindow();
+  SearchForSystemApp(primary_root_window, u"Feedback",
+                     web_app::kOsFeedbackAppId);
+
+  GURL feedback_url = GURL(kChromeUIOSFeedbackUrl);
+  content::TestNavigationObserver navigation_observer(feedback_url);
+  navigation_observer.StartWatchingNewWebContents();
+
+  ClickTopSearchResult(primary_root_window, u"Feedback");
+
+  // Wait for the Feedback app to launch.
+  navigation_observer.Wait();
+  Browser* feedback_browser = FindSystemWebAppBrowser(
+      browser()->profile(), SystemWebAppType::OS_FEEDBACK);
+  EXPECT_TRUE(feedback_browser);
 }
 
 IN_PROC_BROWSER_TEST_F(AppListSearchWithAppShortcutsBrowserTest,
