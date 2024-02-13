9f0c18c86091cf6b458945aaca3eb549c15a3db9
Joshua Hood <jdh@chromium.org>
2024-02-13 21:14:21 +0000

    [M122][3PCD] Add origin trials observer for top-level 3PC deprecation trial
    This CL adds a OriginTrialsControllerDelegate::Observer to listen to
    status changes for the TopLevelTpcd and create/remove
    TOP_LEVEL_TPCD_TRIAL content settings accordingly.
    
    NOTREECHECKS=true
    
    (cherry picked from commit 4795399e45efb89d9931feae8d319b262057ea30)
    
    Bug: 1513317
    Change-Id: I1f742cdee2d15e6a4900c940a914b70f42ac4541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279476
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1258106}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291756
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Auto-Submit: Joshua Hood <jdh@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6261@{#741}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -181,6 +181,7 @@
 #include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
 #include "chrome/browser/tpcd/heuristics/opener_heuristic_service_factory.h"
 #include "chrome/browser/tpcd/metadata/updater_service_factory.h"
+#include "chrome/browser/tpcd/support/top_level_trial_service_factory.h"
 #include "chrome/browser/tpcd/support/tpcd_support_service_factory.h"
 #include "chrome/browser/translate/translate_model_service_factory.h"
 #include "chrome/browser/translate/translate_ranker_factory.h"
@@ -1217,6 +1218,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   tpcd::experiment::EligibilityServiceFactory::GetInstance();
   tpcd::metadata::UpdaterServiceFactory::GetInstance();
   tpcd::trial::TpcdTrialServiceFactory::GetInstance();
+  tpcd::trial::TopLevelTrialServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   TrackingProtectionNoticeFactory::GetInstance();
 #endif
