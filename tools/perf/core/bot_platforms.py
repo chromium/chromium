3b293fe7c2c321fc97662d877af55973570e4b4a
Wenbin Zhang <wenbinzhang@google.com>
2024-02-13 20:10:54 +0000

    [chromeperf] update the shard number for the intel macbooks as the missing bots are recovered
    Now we have 29 bots each pool and will use ~80% of them for perf waterfall.
    
    Bug: b/306679218
    Change-Id: I42acff26ed141ead93a5e8cb517b5670d1b73c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293154
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260007}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -617,8 +617,7 @@ MAC_HIGH_END_LAPTOP = PerfPlatform(
     'mac-laptop_high_end-perf',
     'MacBook Pro, Core i7 2.8 GHz, 16GB RAM, 256GB SSD, Radeon 55',
     _MAC_HIGH_END_BENCHMARK_CONFIGS,
-    # crbug/1522221. Reduce it from 26 to 20 until missing bots are recovered.
-    20,
+    23,
     'mac',
     executables=_MAC_HIGH_END_EXECUTABLE_CONFIGS)
 MAC_HIGH_END_LAPTOP_PGO = PerfPlatform(
@@ -633,7 +632,7 @@ MAC_LOW_END_LAPTOP = PerfPlatform(
     'mac-laptop_low_end-perf',
     'MacBook Air, Core i5 1.8 GHz, 8GB RAM, 128GB SSD, HD Graphics',
     _MAC_LOW_END_BENCHMARK_CONFIGS,
-    20,
+    23,
     'mac',
     executables=_MAC_LOW_END_EXECUTABLE_CONFIGS)
 MAC_LOW_END_LAPTOP_PGO = PerfPlatform(
