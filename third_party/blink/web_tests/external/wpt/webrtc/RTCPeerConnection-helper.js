2247db5ddfc6896085bdd39568d1ca912b33ee8b
Philipp Hancke <phancke@microsoft.com>
2024-02-13 14:33:57 +0000

    webrtc wpt: update more tests to use receiver capabilities for setCodecPreferences
    BUG=webrtc:15396
    
    Change-Id: Icb67eacc4554c5cf356b12ff7e7306ac795c4f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287360
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1259818}

--- third_party/blink/web_tests/external/wpt/webrtc/RTCPeerConnection-helper.js
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCPeerConnection-helper.js
@@ -651,7 +651,7 @@ function findTransceiverForSender(pc, sender) {
 }
 
 function preferCodec(transceiver, mimeType, sdpFmtpLine) {
-  const {codecs} = RTCRtpSender.getCapabilities(transceiver.receiver.track.kind);
+  const {codecs} = RTCRtpReceiver.getCapabilities(transceiver.receiver.track.kind);
   // sdpFmtpLine is optional, pick the first partial match if not given.
   const selectedCodecIndex = codecs.findIndex(c => {
     return c.mimeType === mimeType && (c.sdpFmtpLine === sdpFmtpLine || !sdpFmtpLine);
