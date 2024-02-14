13f8692eb2812ffc13c48361fbaf309991be27c4
Timofey Chudakov <tchudakov@google.com>
2024-02-14 16:17:55 +0000

    [mullhet] Fetch passwords in the passwords manual fallback flow.
    This CL implements PasswordManualFallbackFlow::RunFlow. Since passwords
    are read from disk asynchronously, the suggestion generation and
    displaying can be delayed.
    
    This CL corresponds to "Accessing all user passwords" section in
    go/chrome-mullhet-suggestion-generation
    
    Bug: b:321678448
    Change-Id: I2392dcdade86372f32c9974385a03973aae33d3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5279440
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1260495}

--- components/password_manager/core/browser/password_manual_fallback_flow.cc
+++ components/password_manager/core/browser/password_manual_fallback_flow.cc
@@ -13,11 +13,16 @@ using autofill::Suggestion;
 PasswordManualFallbackFlow::PasswordManualFallbackFlow(
     PasswordManagerDriver* password_manager_driver,
     autofill::AutofillClient* autofill_client,
-    PasswordManagerClient* password_client)
-    : password_manager_driver_(password_manager_driver),
+    PasswordManagerClient* password_client,
+    std::unique_ptr<SavedPasswordsPresenter> passwords_presenter)
+    : suggestion_generator_(password_manager_driver, password_client),
+      password_manager_driver_(password_manager_driver),
       autofill_client_(autofill_client),
       password_client_(password_client),
-      suggestion_generator_(password_manager_driver, password_client) {}
+      passwords_presenter_(std::move(passwords_presenter)) {
+  passwords_presenter_observation_.Observe(passwords_presenter_.get());
+  passwords_presenter_->Init();
+}
 
 PasswordManualFallbackFlow::~PasswordManualFallbackFlow() {
   if (deletion_callback_) {
@@ -40,8 +45,29 @@ bool PasswordManualFallbackFlow::SupportsSuggestionType(
   }
 }
 
-void PasswordManualFallbackFlow::RunFlow() {
-  // TODO(b/321678448): Generate suggestions and run the popup.
+void PasswordManualFallbackFlow::OnSavedPasswordsChanged(
+    const PasswordStoreChangeList& changes) {
+  FlowState old_state =
+      std::exchange(flow_state_, FlowState::kPasswordsRetrived);
+  if (old_state == FlowState::kInvokedWithoutPasswords) {
+    CHECK(saved_bounds_);
+    CHECK(saved_text_direction_);
+    RunFlowImpl(saved_bounds_.value(), saved_text_direction_.value());
+    saved_bounds_.reset();
+    saved_text_direction_.reset();
+  }
+}
+
+void PasswordManualFallbackFlow::RunFlow(
+    const gfx::RectF& bounds,
+    base::i18n::TextDirection text_direction) {
+  if (flow_state_ != FlowState::kPasswordsRetrived) {
+    flow_state_ = FlowState::kInvokedWithoutPasswords;
+    saved_bounds_ = bounds;
+    saved_text_direction_ = text_direction;
+    return;
+  }
+  RunFlowImpl(bounds, text_direction);
 }
 
 void PasswordManualFallbackFlow::OnPopupShown() {}
@@ -130,6 +156,19 @@ int32_t PasswordManualFallbackFlow::GetWebContentsPopupControllerAxId() const {
 void PasswordManualFallbackFlow::RegisterDeletionCallback(
     base::OnceClosure deletion_callback) {
   deletion_callback_ = std::move(deletion_callback);
+}
+
+void PasswordManualFallbackFlow::RunFlowImpl(
+    const gfx::RectF& bounds,
+    base::i18n::TextDirection text_direction) {
+  std::vector<Suggestion> suggestions =
+      suggestion_generator_.GetManualFallbackSuggestions(
+          passwords_presenter_->GetSavedPasswords());
+  autofill::AutofillClient::PopupOpenArgs open_args(
+      bounds, text_direction, std::move(suggestions),
+      autofill::AutofillSuggestionTriggerSource::kManualFallbackPasswords);
+  autofill_client_->ShowAutofillPopup(open_args,
+                                      weak_ptr_factory_.GetWeakPtr());
 }
 
 }  // namespace password_manager
