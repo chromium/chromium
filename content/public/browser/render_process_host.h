c2455cc8196c6f20acb449bcd8c015b1002b4b18
David Stevens <stevensd@chromium.org>
2024-02-14 12:11:40 +0000

    Clean up incorrectly formatted comments
    Make the documentation of RenderProcessHost::IsDeletingSoon consistent
    with chromium comment style.
    
    Bug: None
    Change-Id: If73bf50d9205e8bfda0540dc8d2f74c746c57329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293856
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260398}

--- content/public/browser/render_process_host.h
+++ content/public/browser/render_process_host.h
@@ -305,10 +305,10 @@ class CONTENT_EXPORT RenderProcessHost : public IPC::Sender,
   // still return an invalid process with a null handle.
   virtual bool IsInitializedAndNotDead() = 0;
 
-  /// Returns true iff the decision has been made to delete `this`.
-  ///
-  /// If this returns true, then no new child processes will be associated
-  /// with `this`.
+  // Returns true iff the decision has been made to delete `this`.
+  //
+  // If this returns true, then no new child processes will be associated
+  // with `this`.
   virtual bool IsDeletingSoon() = 0;
 
   // Returns the renderer channel.
