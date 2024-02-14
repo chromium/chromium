043e69e24786c831e9070c7734e7853cce93a9c0
Jood Hajeer <jood@google.com>
2024-02-14 11:46:07 +0000

    [sync] Enable kSyncRememberCustomPassphraseAfterSignout
    This CL enables the feature kSyncRememberCustomPassphraseAfterSignout by
    default, which controls the w/r and migration from the old passphrase
    pref to the new gaia-keyed pref.
    
    This change affects all users on all platforms.
    
    Fixed: 40069260
    Change-Id: I4b451853d41c339226f0aa2a7e3c0123f0cf29d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5285758
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260391}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -241,7 +241,7 @@ BASE_FEATURE(kSyncShowIdentityErrorsForSignedInUsers,
 
 BASE_FEATURE(kSyncRememberCustomPassphraseAfterSignout,
              "SyncRememberCustomPassphraseAfterSignout",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kWebApkBackupAndRestoreBackend,
