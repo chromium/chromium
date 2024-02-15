c329bb1f8fdfddda306b480d73ac3f1d52e10c5e
Thomas Lukaszewicz <tluk@chromium.org>
2024-02-15 01:59:03 +0000

    [lacros] Update os://settings label for browsing data
    Currently the os://settings label for browsing data opens a browser
    window to the primary user's browser profile data management, however
    this does not cover management of any other browser profiles the user
    may be signed into.
    
    This CL makes the existing os://setting more clear as directed by
    ChromeOS PM. Future work will explore improving the setting to
    allow management of browsing data for all signed in profiles.
    
    Bug: 41484354
    Change-Id: I450411100c31376aeb343e27cbe0d91fc2676bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5287248
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1260847}

--- chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/storage/storage_section.cc
@@ -7,10 +7,14 @@
 #include "ash/constants/ash_features.h"
 #include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/ash/crosapi/browser_util.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ui/webui/ash/settings/os_settings_features_util.h"
 #include "chrome/browser/ui/webui/ash/settings/search/search_tag_registry.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -87,7 +91,6 @@ void StorageSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
                                            : IDS_SETTINGS_STORAGE_ITEM_APPS},
       {"storageItemOffline", IDS_SETTINGS_STORAGE_ITEM_OFFLINE},
       {"storageItemAvailable", IDS_SETTINGS_STORAGE_ITEM_AVAILABLE},
-      {"storageItemBrowsingData", IDS_SETTINGS_STORAGE_ITEM_BROWSING_DATA},
       {"storageItemCrostini", IDS_SETTINGS_STORAGE_ITEM_CROSTINI},
       {"storageItemInUse", IDS_SETTINGS_STORAGE_ITEM_IN_USE},
       {"storageItemMyFiles", IDS_SETTINGS_STORAGE_ITEM_MY_FILES},
@@ -117,6 +120,26 @@ void StorageSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
       l10n_util::GetStringFUTF16(
           IDS_SETTINGS_STORAGE_ANDROID_APPS_ACCESS_EXTERNAL_DRIVES_NOTE,
           chrome::kArcExternalStorageLearnMoreURL));
+
+  // If Lacros is enabled the browsing data settings control will open a Lacros
+  // window for browsing data belonging to the Lacros primary profile. The
+  // signed-in ash user profile corresponds directly to the Lacros primary
+  // profile so use the ash user's display name.
+  // TODO(crbug.com/41484354): Explore exposing a better setting that allows
+  // browsing data management for all browser profiles.
+  if (crosapi::browser_util::IsLacrosEnabled()) {
+    const user_manager::User* user =
+        ProfileHelper::Get()->GetUserByProfile(profile());
+    CHECK(user);
+    html_source->AddString("storageItemBrowsingData",
+                           l10n_util::GetStringFUTF16(
+                               IDS_SETTINGS_STORAGE_ITEM_BROWSING_DATA_LACROS,
+                               user->GetDisplayName()));
+  } else {
+    html_source->AddString(
+        "storageItemBrowsingData",
+        l10n_util::GetStringUTF16(IDS_SETTINGS_STORAGE_ITEM_BROWSING_DATA));
+  }
 
   html_source->AddBoolean("isExternalStorageEnabled",
                           IsExternalStorageEnabled(profile()));
