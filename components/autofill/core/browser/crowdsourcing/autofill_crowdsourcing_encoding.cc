ff05807b75d2c4e27cbb35045c00fba457d476a3
Norge Vizcay <vizcay@google.com>
2024-02-16 14:19:18 +0000

    Remove repeated IsCheckable check.
    We missed a repeated IsCheckable check in crrev/c/5145389/.
    
    Change-Id: Ie650e40b9f540f81175baf8075dc9c503900b5bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300247
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261616}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.cc
@@ -313,10 +313,6 @@ void EncodeFormFieldsForUpload(const FormStructure& form,
     if (IsCheckable(field->check_status)) {
       continue;
     }
-    //  Add the same field elements as the query and a few more below.
-    if (IsCheckable(field->check_status)) {
-      continue;
-    }
     // Do not upload fields that were filled with a fallback type, as this would
     // introduce unnecessary noise in the field votes.
     if (field->WasAutofilledWithFallback()) {
