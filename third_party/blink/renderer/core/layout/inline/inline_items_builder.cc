ac2823849527cbfe4a0c6a0f0308d529c141f05c
Kent Tamura <tkent@chromium.org>
2024-02-16 07:58:53 +0000

    Layout: Workaround for crashes in TransformedString
    We hit the following CHECK:
      CHECK_EQ(layout_text->TransformedText().length(), transformed.length());
    
    * This CL updates InlineItemsBuilder so that it records an on-the-fly
      transformed text, instead of a transformed text stored in LayoutText.
    
    * Collect information about the problematic case.
    
    This CL has no tests.  We don't know how to reproduce the crash.
    
    Bug: 324813558
    Change-Id: I5facdcfc16b3e78d865248985d76b4aaa6f73896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5302095
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261523}

--- third_party/blink/renderer/core/layout/inline/inline_items_builder.cc
+++ third_party/blink/renderer/core/layout/inline/inline_items_builder.cc
@@ -529,13 +529,17 @@ void InlineItemsBuilderTemplate<MappingBuilder>::AppendText(
   TextOffsetMap offset_map;
   String transformed =
       layout_text->TransformAndSecureText(original, offset_map);
-  CHECK_EQ(layout_text->TransformedText().length(), transformed.length());
+  if (layout_text->TransformedText().length() != transformed.length()) {
+    NOTREACHED() << "Mismatch; class=" << layout_text->GetName()
+                 << " stored=" << layout_text->TransformedText()
+                 << " live=" << transformed;
+  }
   const Vector<unsigned> length_map = TransformedString::CreateLengthMap(
       original.length(), transformed.length(), offset_map);
   CHECK(transformed.length() == length_map.size() || length_map.size() == 0);
-  AppendText(TransformedString(layout_text->TransformedText(),
-                               {length_map.data(), length_map.size()}),
-             *layout_text);
+  AppendText(
+      TransformedString(transformed, {length_map.data(), length_map.size()}),
+      *layout_text);
 }
 
 template <typename MappingBuilder>
