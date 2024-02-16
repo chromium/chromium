1f7d2f00e133990a90ccc83c6c02b65ad041c7c9
Andrew Grieve <agrieve@chromium.org>
2024-02-16 15:19:55 +0000

    JNI Zero: Remove .cr.so loading fallback in sample app
    No longer necessary
    
    Bug: 324916799
    Change-Id: Ib786905810c077a45cdd108761e955846e6a28ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301575
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1261644}

--- third_party/jni_zero/samples/java/src/org/jni_zero/samples/SampleActivity.java
+++ third_party/jni_zero/samples/java/src/org/jni_zero/samples/SampleActivity.java
@@ -8,14 +8,7 @@ import android.os.Bundle;
 
 public class SampleActivity extends Activity {
     static {
-        try {
-            System.loadLibrary("sample_lib");
-        } catch (UnsatisfiedLinkError e) {
-            // This is a hacky workaround for the Chromium build system - in some configurations,
-            // we use "component build", and instead of properly supporting this, we can just
-            // try/catch for this sample app.
-            System.loadLibrary("sample_lib.cr");
-        }
+        System.loadLibrary("sample_lib");
     }
 
     public void onCreate(Bundle b) {
