6b29e7f446db1eda8ed9f6904cd3719f1cef5ece
Theo Cristea <theocristea@google.com>
2024-02-16 15:15:22 +0000

    Create autofill field promo controller & view
    These classes are responsible for showing and hiding IPH bubbles which
    need to be attached directly to DOM elements.
    
    The controller will be owned (in a future CL) by the autofill client.
    The controller manages hiding and showing the view.
    
    The view is actually an invisible view which completely covers the
    bottom boundary of the element. The IPH is then anchored to this view.
    This gives the visual impression that the IPH is anchored to the DOM
    element.
    
    This code can be used to display multiple IPH's. For every IPH, a new
    controller has to be created. The `ElementIdentifier` passed to the
    controller dictates which IPH will be shown.
    
    Tested the view by setting a red background to it:
    https://screenshot.googleplex.com/5KTWrUrHkyzr5vL
    https://screenshot.googleplex.com/9F3YBNubowcdG2c
    
    No matter how I moved the window within the screen, the view was always
    positioned correctly.
    
    Bug: b/320633297
    Change-Id: Ib41227f2eeb4668cbd6669a305e7aa966ab110ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5281925
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261640}

---
+++ chrome/browser/ui/autofill/autofill_field_promo_controller_impl.cc
@@ -0,0 +1,45 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill/autofill_field_promo_controller_impl.h"
+
+#include "base/functional/bind.h"
+#include "components/autofill/core/browser/ui/popup_hiding_reasons.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/gfx/geometry/rect_f.h"
+
+namespace autofill {
+
+AutofillFieldPromoControllerImpl::AutofillFieldPromoControllerImpl(
+    content::WebContents* web_contents,
+    ui::ElementIdentifier promo_element_identifier)
+    : web_contents_(web_contents),
+      promo_element_identifier_(std::move(promo_element_identifier)) {}
+
+AutofillFieldPromoControllerImpl::~AutofillFieldPromoControllerImpl() {
+  Hide();
+}
+
+void AutofillFieldPromoControllerImpl::Show(const gfx::RectF& bounds) {
+  Hide();
+  promo_view_ = AutofillFieldPromoView::CreateAndShow(
+      web_contents_, bounds, promo_element_identifier_);
+  // The hide helper is destroyed on hide, so it cannot outlive the popup
+  // controller.
+  promo_hide_helper_ = std::make_unique<AutofillPopupHideHelper>(
+      web_contents_, AutofillPopupHideHelper::HidingParams{},
+      base::BindRepeating([](AutofillFieldPromoControllerImpl& controller,
+                             PopupHidingReason) { controller.Hide(); },
+                          std::ref(*this)));
+}
+
+void AutofillFieldPromoControllerImpl::Hide() {
+  promo_hide_helper_.reset();
+  if (promo_view_) {
+    promo_view_->Close();
+    promo_view_ = nullptr;
+  }
+}
+
+}  // namespace autofill
