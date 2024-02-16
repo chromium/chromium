62d8f5ce96976ac392112d9ef03fc85f344ea1bd
Fiona Macintosh <fmacintosh@google.com>
2024-02-16 22:17:22 +0000

    [Settings] Turn on aligned 3PC settings UI by default
    Bug: b:320711396
    Change-Id: I8e052f31eae90e8bf629317a8291a41252ab3c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293281
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1261925}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -159,7 +159,7 @@ BASE_FEATURE(kIpProtectionUx,
 
 BASE_FEATURE(kCookieSettingsUiAlignment,
              "CookieSettingsUiAlignment",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kTrackingProtectionNoticeRequestTracking,
